4723098affb23d15db83dcbc31d066dff6eb9f1d
mellon <mellon>
1998-03-05 08:52:06 +0000

    Moved to net/

--- athena/zephyr/files/zhm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-PREFIX=${PREFIX-@prefix@}
-
-if [ -x ${PREFIX}/sbin/zhm -a -f ${PREFIX}/etc/zephyr/server.list ] ; then
-     PATH=${PREFIX}/sbin:${PREFIX}/bin:$PATH ; export PATH
-     zhm `cat ${PREFIX}/etc/zephyr/server.list` && echo -n ' zhm'
-fi
