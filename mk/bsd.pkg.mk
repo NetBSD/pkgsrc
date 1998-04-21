e5ba589749e07aed3babd559d94a0c4433b12116
agc <agc>
1998-04-21 15:50:27 +0000

    Pass the environment down when using xmkmf to configure.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,7 +1,7 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4
 #
-#	$NetBSD: bsd.pkg.mk,v 1.69 1998/04/20 10:16:24 agc Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.70 1998/04/21 15:50:27 agc Exp $
 #
 #	This file is derived from bsd.port.mk - 940820 Jordan K. Hubbard.
 #	This file is in the public domain.
@@ -1202,7 +1202,7 @@ do-configure:
 	    ${CONFIGURE_ENV} ./${CONFIGURE_SCRIPT} ${CONFIGURE_ARGS})
 .endif
 .if defined(USE_IMAKE)
-	@(cd ${WRKSRC} && ${XMKMF})
+	@(cd ${WRKSRC} && ${SETENV} ${SCRIPTS_ENV} XPROJECTROOT=${X11BASE} ${XMKMF})
 .endif
 .endif
 
