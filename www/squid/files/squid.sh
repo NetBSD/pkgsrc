3c9dc3de69414bea873048acbe1e83c121885377
tron <tron@pkgsrc.org>
1998-05-24 19:59:28 +0000

    "RunCache" doesn't put its logfile in the current directory any more. So we don't need to change it.

--- www/squid/files/squid.sh
+++ www/squid/files/squid.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-# $NetBSD: squid.sh,v 1.1 1998/05/23 21:27:21 tron Exp $
+# $NetBSD: squid.sh,v 1.2 1998/05/24 19:59:28 tron Exp $
 #
 if [ -x @PREFIX@/sbin/RunCache -a -f @PREFIX@/etc/squid/squid.conf ];
 then
- (cd /var/run; @PREFIX@/sbin/RunCache >/dev/null 2>&1 &)
+ @PREFIX@/sbin/RunCache >/dev/null 2>&1 &
  echo -n ' squid'
 fi
 exit 0
