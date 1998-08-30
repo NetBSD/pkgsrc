320f9dd21529e1de6c1b2369488dbcda0e3a0548
lukem <lukem>
1998-08-30 04:44:56 +0000

    it's now bsd.pkg.mk (not bsd.port.mk)

--- mk/bsd.own.mk
+++ mk/bsd.own.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.own.mk,v 1.6 1998/07/31 14:55:07 tv Exp $
+#	$NetBSD: bsd.own.mk,v 1.7 1998/08/30 04:44:56 lukem Exp $
 
 .if !defined(_BSD_OWN_MK_)
 _BSD_OWN_MK_=1
@@ -166,7 +166,7 @@ TARGETS+=	all clean cleandir depend includes install lint obj regress \
 		realinstall
 
 # set NEED_OWN_INSTALL_TARGET, if it's not already set, to yes
-# this is used by bsd.port.mk to stop "install" being defined
+# this is used by bsd.pkg.mk to stop "install" being defined
 NEED_OWN_INSTALL_TARGET?=	yes
 
 .if (${NEED_OWN_INSTALL_TARGET} == "yes")
