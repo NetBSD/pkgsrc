d5872bbb3fa834f231477b90d0d4a7363475df72
explorer <explorer>
1998-09-04 22:00:33 +0000

    Import of SSLeay, which uses RSAREF 2.0

---
+++ security/ssleay/files/NetBSD.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+perl util/perlpath.pl /usr/pkg/bin
+perl util/ssldir.pl /usr/pkg
+
+case `uname -m` in
+    i386)
+	conf=NetBSD-x86
+	;;
+    sparc)
+	conf=NetBSD-sparc
+	;;
+    m68k)
+	conf=NetBSD-m86	# yes, this really IS m86, not m68!
+	;;
+    *)
+	echo "Not supported on this architecture, sorry."
+	;;
+esac
+perl Configure $conf
