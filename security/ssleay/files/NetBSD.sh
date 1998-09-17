19e686e50d4cdb8ec30cde97232ca5e619f3d29b
frueauf <frueauf>
1998-09-17 12:34:28 +0000

    Don't hardcode install path in NetBSD.sh to /usr/pkg. Run perl from ${PREFIX}/bin/perl.

--- security/ssleay/files/NetBSD.sh
+++ security/ssleay/files/NetBSD.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+# $NetBSD: NetBSD.sh,v 1.3 1998/09/17 12:34:29 frueauf Exp $
 
-perl util/perlpath.pl /usr/pkg/bin
-perl util/ssldir.pl /usr/pkg
+@@@PREFIX@@@/bin/perl util/perlpath.pl @@@PREFIX@@@/bin
+@@@PREFIX@@@/bin/perl util/ssldir.pl @@@PREFIX@@@
 
 case "$1" in
     i386)
@@ -17,4 +18,4 @@ case "$1" in
 	echo "Not supported on this architecture, sorry."
 	;;
 esac
-perl Configure $conf
+@@@PREFIX@@@/bin/perl Configure $conf
