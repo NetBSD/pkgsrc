f29eea23f751007593f7948cd0857d4de71cc743
tv <tv>
1998-10-02 22:02:25 +0000

    Clean up:
    - make this work on all arches (use C where no equivalent asm is
      available).  Since the arch specifics are determined by `uname', set
      ONLY_FOR_ARCHS to the full list of current architectures.
    - don't use various methods of editing (patch, sed) where pulling values
      from the environment and using CONFIGURE_ENV/MAKE_ENV will suffice.
    - use %D to get the value of $PREFIX in the PLIST; the PLIST doesn't need
      to be sed edited to get this as part of an @exec!
    - use pmake's += construct to add rsaref-specific stuff, instead of adding
      "extra" patches.
    
    Now works on arm32, but probably not on alpha and sparc64 (tests fail on
    alpha; likely type-sizing problems, and still looking at that).

--- security/ssleay/files/NetBSD.sh
+++ security/ssleay/files/NetBSD.sh
@@ -1,24 +1,33 @@
 #!/bin/sh
-# $NetBSD: NetBSD.sh,v 1.4 1998/09/21 18:30:34 garbled Exp $
+# $NetBSD: NetBSD.sh,v 1.5 1998/10/02 22:02:26 tv Exp $
 
-@@@PREFIX@@@/bin/perl util/perlpath.pl @@@PREFIX@@@/bin
-@@@PREFIX@@@/bin/perl util/ssldir.pl @@@PREFIX@@@
+$LOCALBASE/bin/perl util/perlpath.pl $LOCALBASE/bin
+$LOCALBASE/bin/perl util/ssldir.pl $PREFIX
 
 case "$1" in
+    arm32|mips|mipsel|ns32k|vax)
+	conf=NetBSD-gle32 # use C routines for little-endian, 32 bit pointers
+	;;
+    mipseb|powerpc)
+	conf=NetBSD-gbe32 # use C routines for big-endian, 32 bit pointers
+	;;
+    alpha)
+	conf=NetBSD-alpha
+	;;
     i386)
 	conf=NetBSD-x86
-	;;
-    sparc)
-	conf=NetBSD-sparc
 	;;
     m68k)
 	conf=NetBSD-m86	# yes, this really IS m86, not m68!
 	;;
-    alpha)
-	conf=NetBSD-alpha
+    sparc)
+	conf=NetBSD-sparc
+	;;
+    sparc64)
+	conf=NetBSD-sparc64
 	;;
     *)
 	echo "Not supported on this architecture, sorry."
 	;;
 esac
-@@@PREFIX@@@/bin/perl Configure $conf
+$LOCALBASE/bin/perl Configure $conf
