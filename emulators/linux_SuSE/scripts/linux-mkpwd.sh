92cf84c114190cdcb9449b68a9d7f103b9922e1c
tron <tron@pkgsrc.org>
1998-10-31 00:18:47 +0000

    New "linux_SuSE-5.3" package: Linux compatibility package based on the SuSE Linux distribution, take a look at "http://www.suse.com/" for more information about it.
    This package supports aout and ELF binaries linked with libc 5 or libc 6.
    However, there are no aout libraries included. You can add Linux RPM
    packages to this package with "linux-rpm -i ..." and remove them with
    "linux-rpm -e ..." again.

---
+++ emulators/linux_SuSE/scripts/linux-mkpwd.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# $Id: linux-mkpwd.sh,v 1.1.1.1 1998/10/31 00:18:47 tron Exp $
+
+SRCDIR=/etc
+DESTDIR=@@EMULDIR@@/etc
+
+umask 22
+cd $DESTDIR
+cp $SRCDIR/passwd .
+sed s/^wheel:/root:/ <$SRCDIR/group >group
+
+umask 77
+awk -F: '{print($1":"$2":0:0:10000::::")}' <$SRCDIR/master.passwd >shadow
+sed s/^wheel:/root:/ <$SRCDIR/group |
+awk -F: '{print($1":*:root:root")}' >gshadow
+
+exit 0
