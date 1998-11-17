aaf4adf450b1162c6b5ff1410fd7a2701fb1e55e
agc <agc@pkgsrc.org>
1998-11-17 16:27:25 +0000

    Initial import of tripwire-1.2, a file and directory integrity checker, into the NetBSD Packages Collection.

---
+++ security/tripwire/files/conf-netbsd.h
@@ -0,0 +1,54 @@
+/* Original Id: conf-bsd.h,v 1.2 1993/08/19 05:26:52 genek Exp */
+
+/*
+ * conf-bsd.h
+ *
+ *	Tripwire configuration file
+ *
+ * Gene Kim
+ * Purdue University
+ */
+
+/***
+ *** Operating System specifics
+ ***	
+ ***	If the answer to a question in the comment is "Yes", then
+ ***	change the corresponding "#undef" to a "#define"
+ ***/
+
+/*
+ * is your OS a System V derivitive?  if so, what version?
+ *			(e.g., define SYSV 4)
+ */
+
+#undef SYSV
+
+/* 
+ * does your system have a <malloc.h> like System V? 
+ */
+
+#undef MALLOCH 	
+
+/* 
+ * does your system have a <stdlib.h> like POSIX says you should? 
+ */
+
+#define STDLIBH
+
+/*
+ * does your system use readdir(3) that returns (struct dirent *)?
+ */
+
+#define DIRENT
+
+/*
+ * is #include <string.h> ok?  (as opposed to <strings.h>)
+ */
+
+#define STRINGH
+ 
+/* 
+ * does your system have gethostname(2) (instead of uname(2))?
+ */
+
+#define GETHOSTNAME
