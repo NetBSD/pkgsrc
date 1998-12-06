d938c182b571e5c99b8e1c1cf008514bf91b4d9c
tron <tron@pkgsrc.org>
1998-12-06 12:42:00 +0000

    Sync with "squid-current" package.

--- www/squid/files/squid.sh
+++ www/squid/files/squid.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# $NetBSD: squid.sh,v 1.2 1998/05/24 19:59:28 tron Exp $
+# $NetBSD: squid.sh,v 1.3 1998/12/06 12:42:00 tron Exp $
 #
-if [ -x @PREFIX@/sbin/RunCache -a -f @PREFIX@/etc/squid/squid.conf ];
+if [ -x @PREFIX@/sbin/RunCache -a -f /etc/squid.conf ];
 then
  @PREFIX@/sbin/RunCache >/dev/null 2>&1 &
  echo -n ' squid'
