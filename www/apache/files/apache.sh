63816e098812efd5b9a6dba751db77c3c35093ff
tron <tron>
1999-05-11 18:38:31 +0000

    Redirect output of "apachectl" to "/dev/null".

--- www/apache/files/apache.sh
+++ www/apache/files/apache.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-# $NetBSD: apache.sh,v 1.1 1999/05/11 18:32:24 tron Exp $
+# $NetBSD: apache.sh,v 1.2 1999/05/11 18:38:31 tron Exp $
 #
 if [ -x @PREFIX@/sbin/httpd -a -f @PREFIX@/etc/httpd/httpd.conf ]
 then
- @PREFIX@/sbin/apachectl start
+ @PREFIX@/sbin/apachectl start >/dev/null
  echo -n ' apache'
 fi
 exit 0
