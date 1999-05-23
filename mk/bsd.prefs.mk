6e915e0f3f83908d4de1453336d4a204480fb34d
tv <tv@pkgsrc.org>
1999-05-23 22:10:01 +0000

    - Move the inclusion of bsd.own.mk to bsd.prefs.mk, and multiple-include   protect bsd.prefs.mk.  Now there's no multiple inclusion of mk.conf,   clobbering variables modified between bsd.prefs.mk and bsd.pkg.mk, when   set with VARNAME=value. - Let bsd.own.mk include mk.conf/MAKECONF; we don't need to do that   manually. - Don't define BSD_PKG_MK or OPSYS in bsd.pkg.mk, as bsd.prefs.mk does   precisely that just a couple lines below.

--- mk/bsd.prefs.mk
+++ mk/bsd.prefs.mk
@@ -1,9 +1,12 @@
-# $NetBSD: bsd.prefs.mk,v 1.4 1998/11/26 23:39:15 hubertf Exp $
+# $NetBSD: bsd.prefs.mk,v 1.5 1999/05/23 22:10:01 tv Exp $
 #
 # Make file, included to get the site preferences, if any.  Should
 # only be included by package Makefiles before any .if defined()
 # statements, to make sure any variables defined in /etc/mk.conf or
 # $MAKECONF are used.
+
+# Do not recursively include mk.conf, redefine OPSYS, include bsd.own.mk, etc.
+.ifndef BSD_PKG_MK
 
 # Let people know this is bsd.pkg.mk, so they can set up their
 # /etc/mk.conf accordingly  
@@ -16,12 +19,21 @@ OPSYS!= /usr/bin/uname -s
 # Don't complain about environment settings on recursive makes.
 __PREFIX_SET__=${PREFIX}
 
-.if defined(MAKECONF) && exists(${MAKECONF})
-.include "${MAKECONF}"
-.elif ${OPSYS} == "FreeBSD" && exists(/etc/make.conf)
-.include "/etc/make.conf"
-.elif exists(/etc/mk.conf)
-.include "/etc/mk.conf"
+.if (${OPSYS} == "NetBSD")
+NEED_OWN_INSTALL_TARGET=	no
+.include <bsd.own.mk>
+SHAREOWN = ${DOCOWN}
+SHAREGRP = ${DOCGRP}
+SHAREMODE = ${DOCMODE}
+.elif (${OPSYS} == "SunOS")
+NEED_OWN_INSTALL_TARGET=	no
+.include <bsd.own.mk>
+SHAREOWN = ${DOCOWN}
+SHAREGRP = ${DOCGRP}
+SHAREMODE = ${DOCMODE}
+.elif (${OPSYS} == "OpenBSD")
+.include <bsd.own.mk>
+MAKE_ENV+=	EXTRA_SYS_MK_INCLUDES="<bsd.own.mk>"
 .endif
 
 .if defined(PREFIX) && (${PREFIX} != ${__PREFIX_SET__})
@@ -29,4 +41,6 @@ __PREFIX_SET__=${PREFIX}
 	@${ECHO_MSG} "You can NOT set PREFIX manually or in mk.conf.  Set LOCALBASE or X11BASE"
 	@${ECHO_MSG} "depending on your needs.  See the pkg system documentation for more info."
 	@${FALSE}
+.endif
+
 .endif
