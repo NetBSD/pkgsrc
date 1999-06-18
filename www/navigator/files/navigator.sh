7659c244b951558707486d31116a73791d8773af
abs <abs>
1999-06-18 23:14:17 +0000

    Use "" around $@ to handle argument expansion correctly. From jtk.

--- www/navigator/files/navigator.sh
+++ www/navigator/files/navigator.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# $NetBSD: navigator.sh,v 1.1.1.1 1999/02/22 07:45:22 abs Exp $
+# $NetBSD: navigator.sh,v 1.2 1999/06/18 23:14:17 abs Exp $
 #
 MOZILLA_HOME=@@MOZILLA_HOME@@ ;	export MOZILLA_HOME
 XNLSPATH=$MOZILLA_HOME/nls ;		export XNLSPATH
 XKEYSYMDB=$MOZILLA_HOME/XKeysymDB ;	export XKEYSYMDB
-exec $MOZILLA_HOME/netscape ${1+$@}      
+exec $MOZILLA_HOME/netscape "$@"
