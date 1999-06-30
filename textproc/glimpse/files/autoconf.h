88311e2580421d3872a89545d016c4b70cf26d42
agc <agc>
1999-06-30 13:13:03 +0000

    Make this package work on Solaris as well as NetBSD - Solaris doesn't have a decent flock(3).

--- textproc/glimpse/files/autoconf.h
+++ textproc/glimpse/files/autoconf.h
@@ -1,11 +1,15 @@
-/*	$NetBSD: autoconf.h,v 1.1 1998/12/03 12:39:43 frueauf Exp $	*/
+/*	$NetBSD: autoconf.h,v 1.2 1999/06/30 13:13:03 agc Exp $	*/
 
 /*
  * preprocessor variables used during build of glimpse-4.1
  */
 
 #define HAVE_DIRENT_H 1
+#ifdef __NetBSD__
 #define HAVE_FLOCK 1
+#else
+#undef HAVE_FLOCK
+#endif
 #define HAVE_GETDTABLESIZE 1
 #define HAVE_SETLINEBUF 1
 #define HAVE_SETRLIMIT 1
