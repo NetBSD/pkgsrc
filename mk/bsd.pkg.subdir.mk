13803785db5131d4a8206cac9ffba457c7a89d2a
agc <agc@pkgsrc.org>
1999-07-02 12:56:32 +0000

    Add a show-downlevel target, which shows output of the form:
            ...
            ===> package security/sudo
            sudo package: sudo-1.5.7p4 installed, pkgsrc version sudo-1.5.9p1
            ===> package security/tripwire
            ===> category shells
            ...
    
    for installed packages whose version does not match that in pkgsrc. No
    output is produced if the package is not installed. This allows an easy
    overview of the packages installed on a machine which need to be upgraded.
    
    Also make minor modifications to the SUBDIRUSE target for Solaris.

--- mk/bsd.pkg.subdir.mk
+++ mk/bsd.pkg.subdir.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.subdir.mk,v 1.29 1999/06/28 11:42:29 agc Exp $
+#	$NetBSD: bsd.pkg.subdir.mk,v 1.30 1999/07/02 12:56:32 agc Exp $
 #	Derived from: FreeBSD Id: bsd.port.subdir.mk,v 1.19 1997/03/09 23:10:56 wosch Exp 
 #	from: @(#)bsd.subdir.mk	5.9 (Berkeley) 2/1/91
 #
@@ -61,9 +61,11 @@ SED?=		/usr/bin/sed
 SORT?=		/usr/bin/sort
 
 _SUBDIRUSE: .USE
-	@for entry in ${SUBDIR}; do \
+	@for entry in "" ${SUBDIR}; do \
+		if [ "X$$entry" = "X" ]; then continue; fi; \
 		OK=""; \
-		for dud in ${DUDS}; do \
+		for dud in "" ${DUDS}; do \
+			if [ "X$$dud" = "X" ]; then continue; fi; \
 			if [ $${dud} = $${entry} ]; then \
 				OK="false"; \
 				${ECHO_MSG} "===> ${_THISDIR_}$${entry} skipped"; \
@@ -101,7 +103,7 @@ ${SUBDIR}::
 
 .for __target in all fetch fetch-list package extract configure build clean \
 		cleandir distclean depend describe reinstall tags checksum \
-		mirror-distfiles deinstall ${PKG_MISC_TARGETS}
+		mirror-distfiles deinstall show-downlevel ${PKG_MISC_TARGETS}
 .if !target(__target)
 ${__target}: _SUBDIRUSE
 .endif
