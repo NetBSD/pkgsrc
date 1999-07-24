c2daedb31b1e00846d12b7e3b1f4663a6395bdc2
tron <tron>
1999-07-24 17:07:27 +0000

    "rc.news" should be started with "su news" of course.

--- news/inn/files/innd.sh
+++ news/inn/files/innd.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-# $NetBSD: innd.sh,v 1.2 1999/07/21 22:50:31 tron Exp $
+# $NetBSD: innd.sh,v 1.3 1999/07/24 17:07:27 tron Exp $
 #
 if [ -x @@PREFIX@@/bin/rc.news -a -s @@INN_DATA_DIR@@/db/active ];
 then
- @@PREFIX@@/bin/rc.news >/dev/null
+ su news @@PREFIX@@/bin/rc.news >/dev/null
  echo -n ' innd'
 fi
 exit 0
