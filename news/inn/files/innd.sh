c7db87b3dcf4dabfe8a651d67e2604b0cde1b11d
tron <tron@pkgsrc.org>
1999-07-24 17:38:14 +0000

    Use "su" with "-c" so that it works for accounts with login shell other than "/bin/sh", too.

--- news/inn/files/innd.sh
+++ news/inn/files/innd.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-# $NetBSD: innd.sh,v 1.3 1999/07/24 17:07:27 tron Exp $
+# $NetBSD: innd.sh,v 1.4 1999/07/24 17:38:14 tron Exp $
 #
 if [ -x @@PREFIX@@/bin/rc.news -a -s @@INN_DATA_DIR@@/db/active ];
 then
- su news @@PREFIX@@/bin/rc.news >/dev/null
+ su news -c @@PREFIX@@/bin/rc.news >/dev/null
  echo -n ' innd'
 fi
 exit 0
