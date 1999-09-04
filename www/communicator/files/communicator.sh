7c8da2adfe5b9b8d58a369c4c456c02805ffcb02
jlam <jlam>
1999-09-04 16:37:53 +0000

    Share plugins directory with www/navigator.

--- www/communicator/files/communicator.sh
+++ www/communicator/files/communicator.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 #
-# $NetBSD: communicator.sh,v 1.2 1999/06/18 23:14:17 abs Exp $
+# $NetBSD: communicator.sh,v 1.3 1999/09/04 16:37:53 jlam Exp $
 #
 MOZILLA_HOME=@@MOZILLA_HOME@@ ;	export MOZILLA_HOME
-XNLSPATH=$MOZILLA_HOME/nls ;		export XNLSPATH
-XKEYSYMDB=$MOZILLA_HOME/XKeysymDB ;	export XKEYSYMDB
-exec $MOZILLA_HOME/netscape "$@"
+NS_PLUGINS_DIR=@@NS_PLUGINS_DIR@@;	export NS_PLUGINS_DIR
+XNLSPATH=${MOZILLA_HOME}/nls ;		export XNLSPATH
+XKEYSYMDB=${MOZILLA_HOME}/XKeysymDB ;	export XKEYSYMDB
+NPX_PLUGIN_PATH=${NS_PLUGINS_DIR}:${MOZILLA_HOME}/plugins:${HOME}/.netscape/plugins ;	export NPX_PLUGIN_PATH
+
+exec ${MOZILLA_HOME}/netscape "$@"
