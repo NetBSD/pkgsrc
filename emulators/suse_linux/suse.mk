9485175b9fc5926745b16fa399316f7c8119b9be
tron <tron>
1999-09-20 00:51:41 +0000

    Assign "DIST_SUBDIR", "MIRROR_DISTFILE", "WRKSRC" and "MANCOMPRESSED" with "?=" so that they can be overwritten by a package.

--- emulators/suse_linux/suse.mk
+++ emulators/suse_linux/suse.mk
@@ -1,17 +1,16 @@
-# $NetBSD: suse.mk,v 1.1 1999/09/19 23:58:42 tron Exp $
+# $NetBSD: suse.mk,v 1.2 1999/09/20 00:51:41 tron Exp $
 
 MASTER_SITE_SUSE=	ftp://ftp.suse.com/pub/suse/i386/6.1/suse/ \
 			ftp://ftp.cs.unm.edu/mirrors/SuSE61/suse/
 
-DIST_SUBDIR=		suse
-MIRROR_DISTFILE=	no
+DIST_SUBDIR?=		suse
+MIRROR_DISTFILE?=	no
 
-WRKSRC=			${WRKDIR}
-MANCOMPRESSED=		yes
+WRKSRC?=		${WRKDIR}
+MANCOMPRESSED?=		yes
 
 EMULSUBDIR=		emul/linux
 EMULDIR=		${PREFIX}/${EMULSUBDIR}
-EMULLDCONFIG=		
 
 RPM2PKG=		${PREFIX}/sbin/rpm2pkg
 
@@ -25,7 +24,11 @@ RPM2PKGARGS+=		${DISTDIR}/${DIST_SUBDIR}/${TEMP}
 
 .if !target(do-install)
 do-install:
-	@${RM} -f ${PLIST_SRC}
+	@if [ -f ${PKGDIR}/PLIST ]; then \
+	  ${CP} ${PKGDIR}/PLIST ${PLIST_SRC}; \
+	else \
+	  ${RM} -f ${PLIST_SRC}; \
+	fi
 	${RPM2PKG} ${RPM2PKGARGS}
 	@if ${GREP} -q 'lib.*\.so' ${PLIST_SRC}; then \
 	  ${ECHO_MSG} "===>   [Automatic Linux shared object handling]"; \
