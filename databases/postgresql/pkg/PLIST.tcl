745377f7028f13bed9820723467387361277d1bc
jlam <jlam@pkgsrc.org>
1999-09-20 23:38:45 +0000

    Update PostgreSQL to 6.5.2.  The pgaccess binary is no longer available from the main PostgreSQL distribution, and must be installed separately (databases/pgaccess).  Relevant changes from 6.5.1:
           subselect+CASE fixes(Tom)
           Add SHLIB_LINK setting for solaris_i386 and solaris_sparc ports(Daren
           Sefcik)
           Fixes for CASE in WHERE join clauses(Tom)
           Fix BTScan abort(Tom)
           Repair the check for redundant UNIQUE and PRIMARY KEY indices(Thomas)
           Improve it so that it checks for multi-column constraints(Tom)
           Fix for Win32 making problem with MB enabled(Hiroki Kataoka)
           Allow BSD yacc and bison to compile pl code(Bruce)
           Fix SET NAMES
           int8 fixes(Thomas)
           Fix vacuum's memory consumption(Hiroshi,Tatsuo)
           Reduce the total memory consumption of vacuum(Tom)
           Fix for timestamp(datetime)
           Rule deparsing bugfixes(Tom)
           Fix quoting problems in mkMakefile.tcldefs.sh.in and
           mkMakefile.tkdefs.sh.in(Tom)
           This is to re-use space on index pages freed by vacuum(Vadim)
           document -x for pg_dump(Bruce)
           Fix for unary operators in rule deparser(Tom)
           Comment out FileUnlink of excess segments during mdtruncate()(Tom)
           Irix linking fix from Yu Cao <yucao@falcon.kla-tencor.com>
           Repair logic error in LIKE: should not return LIKE_ABORT
           when reach end of pattern before end of text(Tom)
           Repair incorrect cleanup of heap memory allocation during transaction
           abort(Tom)

--- databases/postgresql/pkg/PLIST.tcl
+++ databases/postgresql/pkg/PLIST.tcl
@@ -1,5 +1,4 @@
-@comment $NetBSD: PLIST.tcl,v 1.1 1999/09/19 04:24:56 jlam Exp $
-${PGUSER}/bin/pgaccess
+@comment $NetBSD: PLIST.tcl,v 1.2 1999/09/20 23:38:51 jlam Exp $
 ${PGUSER}/bin/pgtclsh
 ${PGUSER}/bin/pgtksh
 ${PGUSER}/include/libpgtcl.h
