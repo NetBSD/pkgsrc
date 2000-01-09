1782475f3f3e2fb3d3e2c458bdd3520b348da944
tls <tls>
2000-01-09 07:10:37 +0000

    Update this package to SuSE-6.3 so that glibc-2.1 applications are supported.

--- emulators/suse_linux/suse.mk
+++ emulators/suse_linux/suse.mk
@@ -1,9 +1,9 @@
-# $NetBSD: suse.mk,v 1.3 1999/10/04 21:06:58 tron Exp $
+# $NetBSD: suse.mk,v 1.4 2000/01/09 07:11:21 tls Exp $
 
-MASTER_SITE_SUSE=	ftp://ftp.suse.com/pub/suse/i386/6.1/suse/ \
-			ftp://ftp.cs.unm.edu/mirrors/SuSE61/suse/
+MASTER_SITE_SUSE=	ftp://ftp.suse.com/pub/suse/i386/6.3/suse/ \
+			ftp://ftp.varesearch.com/pub/mirrors/suse/suse/i386/6.3/suse/ 
 
-DIST_SUBDIR?=		suse
+DIST_SUBDIR?=		suse63
 
 WRKSRC?=		${WRKDIR}
 MANCOMPRESSED?=		yes
