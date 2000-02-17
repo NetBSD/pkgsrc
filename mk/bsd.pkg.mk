e29c26fdcbdab632568655edb558dc7cc41369d8
hubertf <hubertf@pkgsrc.org>
2000-02-17 17:30:15 +0000

    Extend MASTER_SITE_OVERRIDE properly (like MASTER_SITE_BACKUP), when DIST_SUBDIR is set.
    Noted by Bill Squier (groo@cs.stevens-tech.edu) in private mail.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.405 2000/02/12 23:46:05 wiz Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.406 2000/02/17 17:30:15 hubertf Exp $
 #
 # This file is in the public domain.
 #
@@ -709,6 +709,7 @@ MASTER_SITE_BACKUP?=	\
 	ftp://ftp.freebsd.org/pub/FreeBSD/distfiles/
 .if defined(DIST_SUBDIR)
 MASTER_SITE_BACKUP:=	${MASTER_SITE_BACKUP:=${DIST_SUBDIR}/}
+MASTER_SITE_OVERRIDE:=	${MASTER_SITE_OVERRIDE:=${DIST_SUBDIR}/}
 .endif
 
 # Where to put distfiles that don't have any other master site
