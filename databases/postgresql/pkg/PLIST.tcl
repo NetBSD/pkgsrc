73ad669f44bcd5c54801f1283910ff9bc8f1f5df
jwise <jwise>
2000-02-20 00:11:19 +0000

    Hmmm.  I thought I'd committed this a long time ago...
    Split PGSQL_USE_TCL into PGSQL_USE_TCL and PGSQL_USE_TK so that postgres
    can be built with tcl support on a machine without tk (such as a machine
    without X installed...)

--- databases/postgresql/pkg/PLIST.tcl
+++ databases/postgresql/pkg/PLIST.tcl
@@ -1,6 +1,5 @@
-@comment $NetBSD: PLIST.tcl,v 1.2 1999/09/20 23:38:51 jlam Exp $
+@comment $NetBSD: PLIST.tcl,v 1.3 2000/02/20 00:11:20 jwise Exp $
 ${PGUSER}/bin/pgtclsh
-${PGUSER}/bin/pgtksh
 ${PGUSER}/include/libpgtcl.h
 ${PGUSER}/lib/libpgtcl.a
 ${PGUSER}/lib/libpgtcl.so.2.0
