9983612216f1d1e5c31772065738fd49fcf09ff0
jlam <jlam@pkgsrc.org>
2000-03-18 17:43:16 +0000

    Change structure of postgresql package.  Shared library build is now unlibtoolized.  Things now install into ${PREFIX} instead of ${PREFIX}/${PGSQL_USER} to match hier(7) expectations.  The Tcl and Tk interfaces are now split out into separate packages.
    This closes the following PRs: 7384, 8747, 8789, 9272, 9461.

--- databases/postgresql/pkg/PLIST.tcl
+++
@@ -1,6 +0,0 @@
-@comment $NetBSD: PLIST.tcl,v 1.3 2000/02/20 00:11:20 jwise Exp $
-${PGUSER}/bin/pgtclsh
-${PGUSER}/include/libpgtcl.h
-${PGUSER}/lib/libpgtcl.a
-${PGUSER}/lib/libpgtcl.so.2.0
-${PGUSER}/lib/pltcl.so
