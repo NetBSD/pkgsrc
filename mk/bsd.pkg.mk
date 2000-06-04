5acc4510dc51cb0d4f7275253ff226cab43c8934
rh <rh@pkgsrc.org>
2000-06-04 17:56:15 +0000

    Fix aout brokenness.  Bump libtool to 1.3.5nb3.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.475 2000/06/04 16:47:37 rh Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.476 2000/06/04 17:56:15 rh Exp $
 #
 # This file is in the public domain.
 #
@@ -153,9 +153,9 @@ DEPENDS+=		gtexinfo-3.12:../../devel/gtexinfo
 .if defined(USE_LIBTOOL)
 LIBTOOL=		${LOCALBASE}/bin/libtool
 # XXX: actually, here we would need something like
-# BUILD_DEPENDS+=libtool>1.3.5nb1:../../devel/libtool
+# BUILD_DEPENDS+=libtool>1.3.5nb2:../../devel/libtool
 .if make(misc-depends)
-DEPENDS+=		libtool>1.3.5nb1:../../devel/libtool
+DEPENDS+=		libtool>1.3.5nb2:../../devel/libtool
 .endif
 .elif defined(USE_PKGLIBTOOL)
 .if ${OPSYS} == "NetBSD" || ${OPSYS} == "SunOS"
