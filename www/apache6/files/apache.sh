e0673da205157e836e346069f2b6b13544d9f324
hubertf <hubertf>
2000-07-10 12:22:27 +0000

    Make the rc.d script(s) react to something else then 'start', i.e. one can now stop the daemon.

--- www/apache6/files/apache.sh
+++ www/apache6/files/apache.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 #
-# $NetBSD: apache.sh,v 1.1.1.1 1999/07/23 17:53:29 itojun Exp $
+# $NetBSD: apache.sh,v 1.2 2000/07/10 12:22:30 hubertf Exp $
 #
+
+CMD=${1:-start}
+
 if [ -x @PREFIX@/sbin/httpd -a -f @PREFIX@/etc/httpd/httpd.conf ]
 then
- @PREFIX@/sbin/apachectl start >/dev/null
+ @PREFIX@/sbin/apachectl $CMD >/dev/null
  echo -n ' apache'
 fi
 exit 0
