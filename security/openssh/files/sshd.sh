16019f161ce5cfd6b6c5aba2e2b99b2582d6021f
jlam <jlam@pkgsrc.org>
2000-07-22 08:21:59 +0000

    Update openssh to 2.1.1p4. Package changes:     * Factor out common post-install code from PLIST and package Makefile       into files/INSTALL.     * Enhance files/sshd.sh to handle start/stop/restart/status.     * Check for usable installed version of OpenSSL.  This bit possibly       closes the following PRs: 10404, 10501, 10593
    Changes from 2.1.1p3:
        * allow multiple whitespace but only one '=' between tokens
        * close can fail on AFS
        * allow leading whitespace in configuration files
        * Always create ~/.ssh with mode 700

--- security/openssh/files/sshd.sh
+++ security/openssh/files/sshd.sh
@@ -1,14 +1,48 @@
 #!/bin/sh
 #
-# $NetBSD: sshd.sh,v 1.1 2000/01/27 17:37:19 hubertf Exp $
+# $NetBSD: sshd.sh,v 1.2 2000/07/22 08:22:02 jlam Exp $
 #
-if [ ! -f @SSH_CONF_DIR@/ssh_host_key ]
-then
- @PREFIX@/bin/ssh-keygen -f @SSH_CONF_DIR@/ssh_host_key -N ''
-fi
-if [ -x @PREFIX@/sbin/sshd -a -f @SSH_CONF_DIR@/sshd_config ]
-then
- @PREFIX@/sbin/sshd
- echo -n ' sshd'
-fi
+
+name="sshd"
+pidfile="/var/run/${name}.pid"
+
+case $1 in
+start)
+	if [ ! -f @SSH_CONF_DIR@/ssh_host_key ]
+	then
+		@PREFIX@/bin/ssh-keygen -b 1024 -N "" -f /etc/ssh_host_key
+	fi
+	if [ ! -f @SSH_CONF_DIR@/ssh_host_dsa_key ]
+	then
+		/usr/pkg/bin/ssh-keygen -d -N "" -f /etc/ssh_host_dsa_key
+	fi
+	if [ -x @PREFIX@/sbin/sshd -a -f @SSH_CONF_DIR@/sshd_config ]
+	then
+		echo "Starting ${name}."
+		@PREFIX@/sbin/sshd
+	fi
+	;;
+stop)
+	if [ -f ${pidfile} ]; then
+		pid=`head -1 ${pidfile}`
+		echo "Stopping ${name}."
+		kill -TERM ${pid}
+	else
+		echo "${name} not running?"
+	fi
+	;;
+restart)
+	( $0 stop )
+	sleep 1
+	$0 start
+	;;
+status)
+	if [ -f ${pidfile} ]; then
+		pid=`head -1 ${pidfile}`
+		echo "${name} is running as pid ${pid}."
+	else
+		echo "${name} is not running."
+	fi
+	;;
+esac
 exit 0
