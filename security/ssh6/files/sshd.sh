d6b3864d13e0d0a40dd820b82aef62d59def3340
jlam <jlam@pkgsrc.org>
2000-07-24 16:44:52 +0000

    Make all the installed /usr/pkg/etc/rc.d/sshd.sh files match. Also default to "start" command if run with no arguments.

--- security/ssh6/files/sshd.sh
+++ security/ssh6/files/sshd.sh
@@ -1,14 +1,50 @@
 #!/bin/sh
 #
-# $NetBSD: sshd.sh,v 1.1 2000/03/20 02:25:32 itojun Exp $
+# $NetBSD: sshd.sh,v 1.2 2000/07/24 16:44:53 jlam Exp $
 #
-if [ ! -f @SSH_CONF_DIR@/ssh_host_key ]
-then
- @PREFIX@/bin/ssh-keygen -f @SSH_CONF_DIR@/ssh_host_key -N ''
-fi
-if [ -x @PREFIX@/sbin/sshd -a -f @SSH_CONF_DIR@/sshd_config ]
-then
- @PREFIX@/sbin/sshd
- echo -n ' sshd'
-fi
+
+name="sshd"
+pidfile="/var/run/${name}.pid"
+
+command=${1:-start}
+
+case ${command} in
+start)
+	if [ ! -f @SSH_CONF_DIR@/ssh_host_key ]
+	then
+		@PREFIX@/bin/ssh-keygen -b 1024 -N "" -f /etc/ssh_host_key
+	fi
+	if [ ! -f @SSH_CONF_DIR@/ssh_host_dsa_key ]
+	then
+		/usr/pkg/bin/ssh-keygen -d -N "" -f /etc/ssh_host_dsa_key
+	fi
+	if [ -x @PREFIX@/sbin/sshd -a -f @SSH_CONF_DIR@/sshd_config ]
+	then
+		echo "Starting ${name}."
+		@PREFIX@/sbin/sshd
+	fi
+	;;
+stop)
+	if [ -f ${pidfile} ]; then
+		pid=`head -1 ${pidfile}`
+		echo "Stopping ${name}."
+		kill -TERM ${pid}
+	else
+		echo "${name} not running?"
+	fi
+	;;
+restart)
+	( $0 stop )
+	sleep 1
+	$0 start
+	;;
+status)
+	if [ -f ${pidfile} ]; then
+		pid=`head -1 ${pidfile}`
+		echo "${name} is running as pid ${pid}."
+	else
+		echo "${name} is not running."
+	fi
+	;;
+esac
 exit 0
