83be209e854c21da5f3089f4d34f5b0e85656fb2
hubertf <hubertf>
2000-08-09 21:00:09 +0000

    Update to dhis client R4. Changes:  * improved security  * server can detect when client goes down, and remove entry

--- net/dhid/files/dhid.sh
+++ net/dhid/files/dhid.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
 #
-# $NetBSD: dhid.sh,v 1.1 1999/08/04 14:04:36 jlam Exp $
+# $NetBSD: dhid.sh,v 1.2 2000/08/09 21:00:10 hubertf Exp $
 
-DHID=@PREFIX@/libexec/dhid
-if [ -f ${DHID} ]; then
-	@ECHO@ -n " dhid"
-	${DHID}
-fi
+DHID=@PREFIX@/sbin/dhid
+
+case "$1" in
+start)
+	if [ ! -f "@PREFIX@/etc/dhid.conf" ]; then
+		pkg_info -D dhid
+		echo "@PREFIX@/etc/dhid.conf not found, exiting."
+		exit 1
+	fi
+	if [ -f ${DHID} ]; then
+		@ECHO@ -n " dhid"
+		${DHID}
+	fi
+	;;
+stop)
+	if [ -f /var/run/dhid.pid ]; then
+		kill `cat /var/run/dhid.pid`
+		rm -f /var/run/dhid.pid
+	fi
+	;;
+*)
+	echo "Usage: $0 {start,stop}"
+	exit 1
+	;;
+esac
