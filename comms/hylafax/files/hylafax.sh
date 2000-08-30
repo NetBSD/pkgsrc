b881dfe2def1f026c72825a9964b1ba1110af94f
abs <abs@pkgsrc.org>
2000-08-30 09:50:44 +0000

    - Install template config files in share/hylafax/*_template/, and then copy   into /var/spool/fax, rather than trying to use symlinks which break under   chroot(). - Add basic rc.d/hylafax.sh startup script - Use tiff-3.5-interfaces.patch, pointed out by Bernd Proissl <news@proissl.de> Update to hylafax-4.1b2nb2

---
+++ comms/hylafax/files/hylafax.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# $NetBSD: hylafax.sh,v 1.1 2000/08/30 09:50:45 abs Exp $
+#
+
+command=${1:-start}
+
+case ${command} in
+start)
+	if [ -x @PREFIX@/libexec/hfaxd -a -f /var/spool/fax/etc/config ] ; then
+	    echo -n 'Starting hfaxd'
+	    @PREFIX@/libexec/hfaxd -i 4559 -o 4557 -s 444 
+
+	    echo ' faxq.'
+	    @PREFIX@/sbin/faxq
+	fi
+	;;
+stop)
+	PIDS=`ps -ax -o pid,command|egrep '[ /](faxq|hfaxd) '|awk '{printf " " $1}'`
+	if [ -n "$PIDS" ];then
+		echo "Stopping hylafax pids$PIDS."
+		kill -TERM $PIDS
+	else
+		echo "hylafax not running?"
+	fi
+	;;
+restart)
+	( $0 stop )
+	sleep 1
+	$0 start
+	;;
+esac
+exit 0
