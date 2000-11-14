05483985c5c737abe94b5e63e42d94730d878db8
itojun <itojun>
2000-11-14 19:58:05 +0000

    upgrade to 1.3.14 + IPv6 patch.  apache side fixed security hole between 1.3.12 -> 1.3.14.

--- www/apache6/files/apache.sh
+++ www/apache6/files/apache.sh
@@ -1,15 +1,47 @@
-#!/bin/sh
+#! /bin/sh
 #
-# $NetBSD: apache.sh,v 1.3 2000/09/20 05:14:12 jlam Exp $
+# $NetBSD: apache.sh,v 1.4 2000/11/14 19:58:05 itojun Exp $
 #
 # PROVIDE: apache
 # REQUIRE: DAEMON
 
-CMD=${1:-start}
+name="apache"
+command="@PREFIX@/sbin/apachectl"
+pidfile="/var/run/httpd.pid"
+conffile="@PREFIX@/etc/httpd/httpd.conf"
 
-if [ -x @PREFIX@/sbin/httpd -a -f @PREFIX@/etc/httpd/httpd.conf ]
+apache_start="start"
+
+if [ -f @PREFIX@/etc/httpd/mod_ssl.conf ]
 then
- @PREFIX@/sbin/apachectl $CMD >/dev/null
- echo -n ' apache'
+	# This file can reset apache_start to "startssl"
+	. @PREFIX@/etc/httpd/mod_ssl.conf
 fi
+
+cmd=${1:-start}
+
+case ${cmd} in
+start)
+	if [ -x ${command} -a -f ${conffile} ]; then
+		echo "Starting ${name}."
+		${command} ${apache_start} > /dev/null
+	fi
+	;;
+stop)
+	if [ -x ${command} -a -f ${pidfile} ]; then
+		echo "Stopping ${name}."
+		${command} stop > /dev/null
+	fi
+	;;
+restart)
+	( $0 stop )
+	sleep 5
+	$0 start
+	;;
+*)
+	if [ -x ${command} ]; then
+		${command} ${cmd}
+	fi
+	;;
+esac
 exit 0
