2bee6139ba0a08d7817d21514570630d5fe89d55
sakamoto <sakamoto@pkgsrc.org>
2000-11-27 04:42:48 +0000

    escape RE special chars in package names.

--- pkgtools/pkgdep/files/pkgdep.pl
+++ pkgtools/pkgdep/files/pkgdep.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-# $NetBSD: pkgdep.pl,v 1.1.1.1 1999/09/21 02:26:13 sakamoto Exp $
+# $NetBSD: pkgdep.pl,v 1.2 2000/11/27 04:42:48 sakamoto Exp $
 
 require 'getopts.pl';
 use strict;
@@ -128,6 +128,7 @@ sub mkreqd {
 	for (keys %deps) {
 		my ($p) = $_;
 		for (split(/ /, $deps{$p})) {
+			$_ =~ s/(\+)/\\\1/g;
 			if (defined($reqd{$_})) {
 				if (!($reqd{$_} =~ /$_/)) {
 					$reqd{$_} .= " " . $p;
