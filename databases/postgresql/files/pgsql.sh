891f1a485e2a6c8037d29a4a52b8bf8f956efa90
jlam <jlam>
2000-12-11 20:18:56 +0000

    Rename this script to pgsql.sh, make this script a bit more complete and robust, and add example command_args setting to allow TCP/IP connections to the PostgreSQL database.

---
+++ databases/postgresql/files/pgsql.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+#
+# $NetBSD: pgsql.sh,v 1.1 2000/12/11 20:18:57 jlam Exp $
+#
+# PostgreSQL database rc.d control script
+#
+# PROVIDE: pgsql
+# REQUIRE: DAEMON
+
+name="postgres"
+ctl_command="@PREFIX@/bin/pg_ctl"
+command="@PREFIX@/bin/postmaster"
+command_args="-S -o -F"			# start in silent mode and disable
+					#   automatic fsync after each
+					#   transaction
+#command_args="-S -i -o -F"		# as above, but also allows TCP/IP
+					#   connections
+
+USER="@PGUSER@"
+PGHOME="@PREFIX@/${USER}"
+SU="@SU@"
+
+cmd=${1:-start}
+
+if [ -x ${ctl_command} -a -x ${command} ]
+then
+	case ${cmd} in
+	restart)
+		echo "Restarting ${name}."
+		echo "exec ${ctl_command} restart >> ${PGHOME}/errlog" \
+			| ${SU} -l ${USER}
+		;;
+
+	start)
+		echo "Starting ${name}."
+		echo "exec ${ctl_command} \
+			-p ${command} -o \"${command_args}\" start" \
+			| ${SU} -l ${USER}
+		;;
+
+	stop)
+		echo "Stopping ${name}."
+		echo "exec ${ctl_command} stop >> ${PGHOME}/errlog" \
+			| ${SU} -l ${USER}
+		;;
+
+	status)
+		echo "exec ${ctl_command} status" \
+			| ${SU} -l ${USER}
+		;;
+
+	*)
+		echo 1>&2 "Usage: $0 [restart|start|stop|status]"
+		exit 1
+		;;
+	esac
+fi
