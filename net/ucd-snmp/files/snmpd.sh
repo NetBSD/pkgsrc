4beff6001aca45e2bf66d46fb7c0c02b30b72bcc
hubertf <hubertf@pkgsrc.org>
2000-12-16 09:04:40 +0000

    Make rc.d script handle stop/start/restart

--- net/ucd-snmp/files/snmpd.sh
+++ net/ucd-snmp/files/snmpd.sh
@@ -1,13 +1,35 @@
 #!/bin/sh
-# $NetBSD: snmpd.sh,v 1.1 2000/10/13 18:16:00 kim Exp $
+# $NetBSD: snmpd.sh,v 1.2 2000/12/16 09:04:41 hubertf Exp $
+
+# PID file:
+PF=/var/run/snmpd.pid
+
 
 if [ -f /etc/snmp/snmpd.conf ]
 then
     SNMPD_CFG="-c /etc/snmp/snmpd.conf"
 fi
 
-if [ -x @PREFIX@/sbin/snmpd ]
-then
-    echo -n ' snmpd'
-    @PREFIX@/sbin/snmpd -s -P /var/run/snmpd.pid -A -l /dev/null ${SNMPD_CFG}
-fi
+case $1 in
+start)
+	if [ -x @PREFIX@/sbin/snmpd ]
+	then
+	    echo -n ' snmpd'
+	    @PREFIX@/sbin/snmpd -s -P ${PF} -A -l /dev/null ${SNMPD_CFG}
+	fi
+	;;
+stop)
+	if [ -f ${PF} ]; then
+		kill `cat ${PF}`
+		rm -f ${PF}
+	fi
+	;;
+restart)
+	sh $0 stop
+	sh $0 start
+	;;
+*)
+	echo "Usage: $0 {start|stop|restart}"
+	exit 1
+esac
+
