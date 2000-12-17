d4dbd6d843c09cf75624d33d2d9761aee1611fbf
wiz <wiz@pkgsrc.org>
2000-12-17 23:32:09 +0000

    New category: time. Move many packages here: misc/cal, misc/cardboard-schedule, misc/ical, misc/plan, misc/py-mxDateTime, misc/remind, sysutils/logtime, and 19 more from x11.

---
+++ time/cardboard-schedule/files/csched.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+@JAVA_HOME@/bin/jre -cp @PREFIX@/lib/cardboard-schedule/stage/cs.jar:@PREFIX@/lib/cardboard-schedule/stage/CsHelp.jar:@PREFIX@/lib/cardboard-schedule/stage/jhbasic.jar:@LOCALBASE@/lib/java/swingall.jar:${CLASSPATH} avg.cs.gui.CsMainApp
