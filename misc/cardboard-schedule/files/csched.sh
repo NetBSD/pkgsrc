57efd1b49eb686d5d0305eb66c33a852005ced96
wiz <wiz@pkgsrc.org>
2000-12-17 23:40:53 +0000

    Move lots of packages to new time category; fix links; add time subcategory to main Makefile.

--- misc/cardboard-schedule/files/csched.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-@JAVA_HOME@/bin/jre -cp @PREFIX@/lib/cardboard-schedule/stage/cs.jar:@PREFIX@/lib/cardboard-schedule/stage/CsHelp.jar:@PREFIX@/lib/cardboard-schedule/stage/jhbasic.jar:@LOCALBASE@/lib/java/swingall.jar:${CLASSPATH} avg.cs.gui.CsMainApp
