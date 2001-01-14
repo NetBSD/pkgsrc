dcb74d0bc5d3392d0a5752e0ad9984e93920244c
jlam <jlam@pkgsrc.org>
2001-01-14 06:14:13 +0000

    Install cgi-bin programs with executable bit set, and modify thttpd rc.d script to check for the pid instead of requiring a pidfile that may not exist.

--- www/thttpd/files/thttpd.sh
+++ www/thttpd/files/thttpd.sh
@@ -1,45 +1,54 @@
 #!/bin/sh
 #
-# $NetBSD: thttpd.sh,v 1.3 2000/09/20 05:14:13 jlam Exp $
+# $NetBSD: thttpd.sh,v 1.4 2001/01/14 06:14:13 jlam Exp $
 #
 # PROVIDE: thttpd
 # REQUIRE: DAEMON
 
 name="thttpd"
 command=@PREFIX@/sbin/${name}
-pidfile="/var/run/${name}.pid"
+
+pid=`ps -ax | awk '{print $1,$5}' | grep ${name} | awk '{print $1}'`
 
 cmd=${1:-start}
 
 case ${cmd} in
 start)
-	if [ -x ${command} -a -f /etc/${name}.conf ]
+	if [ "$pid" = "" -a -x ${command} -a -f /etc/${name}.conf ]
 	then
 		echo "Starting ${name}."
-		${command} -i ${pidfile} -C /etc/${name}.conf
+		${command} -C /etc/${name}.conf
 	fi
 	;;
+
 stop)
-	if [ -f ${pidfile} ]; then
-		pid=`head -1 ${pidfile}`
+	if [ "$pid" != "" ]
+	then
 		echo "Stopping ${name}."
 		kill -TERM ${pid}
 	else
 		echo "${name} not running?"
 	fi
 	;;
+
 restart)
 	( $0 stop )
 	sleep 1
 	$0 start
 	;;
+
 status)
-	if [ -f ${pidfile} ]; then
-		pid=`head -1 ${pidfile}`
+	if [ "$pid" != "" ]
+	then
 		echo "${name} is running as pid ${pid}."
 	else
 		echo "${name} is not running."
 	fi
+	;;
+
+*)
+	echo 1>&2 "Usage: ${name} [restart|start|stop|status]"
+	exit 1
 	;;
 esac
 exit 0
