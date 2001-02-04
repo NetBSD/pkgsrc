2b329e6a51ce7f391f7f2e564b6a9ac7b26acd39
tron <tron@pkgsrc.org>
2001-02-04 16:55:47 +0000

    Provide a new style startup script with backwards compatibility.

--- net/ra-rtsp-proxy/files/rtspd.sh
+++ net/ra-rtsp-proxy/files/rtspd.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 #
-# $NetBSD: rtspd.sh,v 1.2 1999/09/03 13:52:17 tron Exp $
+# $NetBSD: rtspd.sh,v 1.3 2001/02/04 16:55:47 tron Exp $
 #
-if [ -x @PREFIX@/sbin/rtspd ]
+# PROVIDE: rtspd
+# REQUIRE: DAEMON
+
+name="rtspd"
+command="@PREFIX@/sbin/${name}"
+command_args="-u nobody"
+
+if [ ! -d /etc/rc.d ]
 then
- @PREFIX@/sbin/rtspd
- echo -n ' rtspd'
+	@ECHO@ -n ' ${name}'
+	exec ${command} ${command_args}
 fi
-exit 0
+
+. /etc/rc.subr
+
+load_rc_config $name
+run_rc_command "$1"
