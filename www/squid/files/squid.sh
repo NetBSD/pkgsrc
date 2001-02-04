94d79346b2e4250204940bafae91987f37100be1
tron <tron@pkgsrc.org>
2001-02-04 20:46:14 +0000

    Provide a new style startup script with backwards compatibility.

--- www/squid/files/squid.sh
+++ www/squid/files/squid.sh
@@ -1,35 +1,28 @@
 #!/bin/sh
 #
-# $NetBSD: squid.sh,v 1.5 2001/01/30 19:24:22 tron Exp $
+# $NetBSD: squid.sh,v 1.6 2001/02/04 20:46:14 tron Exp $
 #
 # REQUIRE: DAEMON
 
-case $1 in
-start)
-	if [ `ps -auxwww | grep RunCache | grep -v grep | wc -l'` -ne 0 ]; then
-		echo "squid already running."
-		exit 1
-	fi
+name="squid"
+command="@PREFIX@/sbin/${name}"
+required_files="/etc/squid.conf"
+command_args="-Y -f $required_files"
 
-	if [ -x @PREFIX@/sbin/RunCache -a -f /etc/squid.conf ];
-	then
-		@PREFIX@/sbin/RunCache >/dev/null 2>&1 &
-		echo -n ' squid'
-	fi
-	;;
-stop)
-	if [ `ps -auxwww | grep RunCache | grep -v grep | wc -l'` -ne 0 ]; then
-		kill `ps -auxwww | grep RunCache | awk '{print $2}'`
-		@PREFIX@/sbin/squid -k shutdown
-	else
-		echo "squid not running."
-	fi
-	;;
-restart)
-	sh $0 stop
-	sh $0 start
-	;;
-*)
-	echo "Usage: $0 {start|stop|restart}"
-	exit 1
-esac
+if [ ! -d /etc/rc.d ]
+then
+	@ECHO@ -n ' ${name}'
+	exec ${command} ${command_args}
+fi
+
+. /etc/rc.subr
+
+extra_commands="reload"
+
+if [ "$1" = rotate ]
+then
+ exec ${command} -k rotate
+fi
+
+load_rc_config $name
+run_rc_command "$1"
