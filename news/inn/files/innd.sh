836e192d591f5079042879be8cf8d9fadf477ad1
tron <tron@pkgsrc.org>
2001-02-08 16:23:06 +0000

    Update "inn" package to version 2.3.1. Changes since version 2.2.3:  INN 2.3.0 represents a significant architectural change to INN, with a  completely new internal overview interface, three new overview mechanisms,  two new article storage mechanisms, and the elimination of quite a few old  interfaces and old code. The NetBSD package furthermore includes IPv6 support and a new style startup script with backwards compatibility.

--- news/inn/files/innd.sh
+++ news/inn/files/innd.sh
@@ -1,25 +1,45 @@
 #!/bin/sh
 #
-# $NetBSD: innd.sh,v 1.7 1999/07/29 21:15:46 tron Exp $
-#
-if [ -x @@PREFIX@@/inn/bin/rc.news -a -s @@INN_DATA_DIR@@/db/active ];
+# $NetBSD: innd.sh,v 1.8 2001/02/08 16:23:06 tron Exp $
+
+if [ -x @@PREFIX@@/inn/bin/rc.news -a -s @@INN_DATA_DIR@@/db/active ]
 then
- su news -c @@PREFIX@@/inn/bin/rc.news >/dev/null
+	if [ ! -f @@PREFIX@@/etc/nntp/server ]
+	then
+		hostname >@@PREFIX@@/etc/nntp/server
+	fi
 
- if [ ! -f @@PREFIX@@/etc/nntp/server ]
- then
-  hostname >@@PREFIX@@/etc/nntp/server
- fi
+	if [ ! -f @@PREFIX@@/etc/nntp/domainname ]
+	then
+		(set - X `grep ^fromhost: @@INN_DATA_DIR@@/etc/inn.conf`
+		if [ $# -eq 3 ]
+		then
+			echo $3 >@@PREFIX@@/etc/nntp/domainname
+		fi)
+	fi
 
- if [ ! -f @@PREFIX@@/etc/nntp/domainname ]
- then
-  set - X `grep -w ^fromhost: @@INN_DATA_DIR@@/etc/inn.conf | head -1`
-  if [ $# -eq 3 ]
-  then
-   echo $3 >@@PREFIX@@/etc/nntp/domainname
-  fi
- fi
+	if [ $# -eq 0 ]
+	then
+		echo -n ' innd'
+		su news -c "@@PREFIX@@/inn/bin/rc.news start" >/dev/null
+		exit 0
+	fi
 
- echo -n ' innd'
+	case "$1" in
+	start | stop)
+		su news -c "@@PREFIX@@/inn/bin/rc.news $1"
+		exit 0
+		;;
+	restart )
+		$0 stop
+		sleep 5
+		exec $0 start
+		;;
+	* )
+		echo "Usage: $0 (start|stop|restart)"
+		exit 1
+		;;
+	esac
 fi
+
 exit 0
