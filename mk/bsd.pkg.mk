bddb9c20acfcb83050e5b243e146835830963316
tron <tron>
2001-03-18 19:27:02 +0000

    Replace two dead German GNOME mirrors by a working one.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.688 2001/03/18 17:12:51 skrll Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.689 2001/03/18 19:27:02 tron Exp $
 #
 # This file is in the public domain.
 #
@@ -885,9 +885,8 @@ MASTER_SITE_SUNSITE+=	\
 MASTER_SITE_GNOME+=	\
 	ftp://ftp.gnome.org/pub/GNOME/ \
 	ftp://ftp.sunet.se/pub/X11/GNOME/ \
-	ftp://ftp.archive.de.uu.net/pub/X11/GNOME/ \
 	ftp://ftp.tuwien.ac.at/hci/gnome.org/GNOME/ \
-	ftp://ftp.informatik.uni-bonn.de/pub/os/unix/gnome/
+	ftp://ftp.uni-trier.de/pub/unix2/gnome/
 
 MASTER_SITE_SOURCEFORGE+=	\
 	http://download.sourceforge.net/ \
