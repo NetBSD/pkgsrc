edb3eae1f378cde1e8bdb9692fdafbe7dd7598ce
jlam <jlam@pkgsrc.org>
2001-05-08 18:13:51 +0000

    Improve rc.d-style startup script.  It now respects settings in /etc/rc.conf (apache=YES/NO and apache_start=startssl) if it exists.

--- www/apache6/files/apache.sh
+++ www/apache6/files/apache.sh
@@ -1,16 +1,17 @@
-#! /bin/sh
+#!/bin/sh
 #
-# $NetBSD: apache.sh,v 1.7 2001/04/29 21:21:13 jlam Exp $
+# $NetBSD: apache.sh,v 1.8 2001/05/08 18:13:52 jlam Exp $
 #
 # PROVIDE: apache
 # REQUIRE: DAEMON
+# KEYWORD: shutdown
+#
+# To start apache at startup, copy this script to /etc/rc.d and set
+# apache=YES in /etc/rc.conf.
 
 name="apache"
-ctl_command="@PREFIX@/sbin/apachectl"
-command="@PREFIX@/sbin/httpd"
-pidfile="/var/run/httpd.pid"
-
-apache_start="start"
+rcvar=$name
+command="@PREFIX@/sbin/apachectl"
 
 if [ -f @APACHE_SYSCONFDIR@/apache_start.conf ]
 then
@@ -18,38 +19,53 @@ then
 	. @APACHE_SYSCONFDIR@/apache_start.conf
 fi
 
-cmd=${1:-start}
+apache_doit()
+{
+	action=$1
 
-if [ -x ${ctl_command} -a -x ${command} ]
-then
-	case ${cmd} in
-	start)
-		echo "Starting ${name}."
-		${ctl_command} ${apache_start} > /dev/null
-		;;
+	case ${action} in
+	start)		echo -n "Starting ${name}: "; action=${apache_start} ;;
+	stop)		echo -n "Stopping ${name}: " ;;
+	restart)	echo -n "Restarting ${name}: " ;;
+	esac
 
-	stop)
-		if [ -f ${pidfile} ]
-		then
-			echo "Stopping ${name}."
-			${ctl_command} ${cmd} > /dev/null
-		fi
-		;;
+	${command} ${command_args} ${action}
+}
 
-	restart)
-		( $0 stop )
-		sleep 5
-		$0 start
-		;;
-
-	status)
-		${ctl_command} ${cmd}
-		;;
-
+checkyesno()
+{
+	eval _value=\$${1}
+	case $_value in
+	[Yy][Ee][Ss]|[Tt][Rr][Uu][Ee]|[Oo][Nn]|1)	return 0 ;;
+	[Nn][Oo]|[Ff][Aa][Ll][Ss][Ee]|[Oo][Ff][Ff]|0)	return 1 ;;
 	*)
-		echo 1>&2 "Usage: $0 [restart|start|stop|status]"
-		exit 1
+		echo "\$${1} is not set properly."
+		return 1
 		;;
 	esac
+}
+
+if [ -r /etc/rc.conf ]
+then
+	. /etc/rc.conf
+else
+	eval ${rcvar}=YES
 fi
-exit 0
+
+if checkyesno ${rcvar}
+then
+	cmd=${1:-start}
+
+	if [ -x ${command} ]
+	then
+		case ${cmd} in
+		restart|start|stop|status)
+			${rcvar}_doit ${cmd}
+			;;
+		*)
+			echo 1>&2 "Usage: $0 [restart|start|stop|status]"
+			exit 1
+			;;
+		esac
+	fi
+fi
