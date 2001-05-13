e7ac4ced79479f60a0023688cdccfed74adb1cd9
wiz <wiz>
2001-05-13 00:44:50 +0000

    Don't set $command_args, set ${smbd,nmbd}_flags instead. Patch provided in pkg/12921 by Paul Goyette.

--- net/samba/files/nmbd.sh
+++ net/samba/files/nmbd.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 #
-# $NetBSD: nmbd.sh,v 1.1 2001/02/04 15:29:58 tron Exp $
+# $NetBSD: nmbd.sh,v 1.2 2001/05/13 00:44:50 wiz Exp $
 #
 # PROVIDE: nmbd
 # REQUIRE: DAEMON
 
 name="nmbd"
 command="@PREFIX@/sbin/${name}"
-command_args="-D"
+nmbd_flags="-D"
 
 if [ ! -d /etc/rc.d ]
 then
 	@ECHO@ -n ' ${name}'
-	exec ${command} ${command_args}
+	exec ${command} ${nmbd_flags} ${command_args}
 fi
 
 . /etc/rc.subr
