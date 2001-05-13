682b75ccce1e2eafcae2d10428c000a5d85f9c5c
wiz <wiz@pkgsrc.org>
2001-05-13 00:44:50 +0000

    Don't set $command_args, set ${smbd,nmbd}_flags instead. Patch provided in pkg/12921 by Paul Goyette.

--- net/samba/files/smbd.sh
+++ net/samba/files/smbd.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 #
-# $NetBSD: smbd.sh,v 1.1 2001/02/04 15:29:59 tron Exp $
+# $NetBSD: smbd.sh,v 1.2 2001/05/13 00:44:50 wiz Exp $
 #
 # PROVIDE: smbd
 # REQUIRE: nmbd
 
 name="smbd"
 command="@PREFIX@/sbin/${name}"
-command_args="-D"
+smbd_flags="-D"
 
 if [ ! -d /etc/rc.d ]
 then
 	@ECHO@ -n ' ${name}'
-	exec ${command} ${command_args}
+	exec ${command} ${smbd_flags} ${command_args}
 fi
 
 . /etc/rc.subr
