cbca8155a068c256717db61c8ac5e675117fd611
wiz <wiz@pkgsrc.org>
2001-06-09 15:32:16 +0000

    '        ' -> '\t'

--- graphics/tiff/buildlink.mk
+++ graphics/tiff/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.2 2001/05/28 02:50:24 jlam Exp $
+# $NetBSD: buildlink.mk,v 1.3 2001/06/09 15:32:16 wiz Exp $
 #
 # This Makefile fragment is included by packages that use libtiff.
 #
@@ -45,7 +45,7 @@ link-tiff-headers:
 			${RM} -f $${dest};				\
 			${LN} -sf $${inc} $${dest};			\
 		fi;							\
-        done
+	done
 
 # This target links the libraries into ${BUILDLINK_LIBDIR}, which should
 # be searched first by the linker.
