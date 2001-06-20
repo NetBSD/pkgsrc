22e90132f5662b528646dc1301a1c5d533f9ff60
jlam <jlam@pkgsrc.org>
2001-06-20 23:46:23 +0000

    Whitespace chnages, and include bsd.buildlink.mk(!) missed in last commit.

--- databases/gdbm/buildlink.mk
+++ databases/gdbm/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.3 2001/06/11 01:59:34 jlam Exp $
+# $NetBSD: buildlink.mk,v 1.4 2001/06/20 23:46:23 jlam Exp $
 #
 # This Makefile fragment is included by packages that use gdbm.
 #
@@ -17,14 +17,16 @@ GDBM_BUILDLINK_MK=	# defined
 GDBM_REQD?=		1.7.3
 DEPENDS+=		gdbm>=${GDBM_REQD}:../../databases/gdbm
 
-BUILDLINK_PREFIX.gdbm=		${LOCALBASE}
-BUILDLINK_FILES.gdbm=		include/gdbm.h
-BUILDLINK_FILES.gdbm+=		lib/libgdbm.*
+BUILDLINK_PREFIX.gdbm=	${LOCALBASE}
+BUILDLINK_FILES.gdbm=	include/gdbm.h
+BUILDLINK_FILES.gdbm+=	lib/libgdbm.*
 
-BUILDLINK_TARGETS.gdbm=		gdbm-buildlink
-BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.gdbm}
+BUILDLINK_TARGETS.gdbm=	gdbm-buildlink
+BUILDLINK_TARGETS+=	${BUILDLINK_TARGETS.gdbm}
 
 pre-configure: ${BUILDLINK_TARGETS.gdbm}
 gdbm-buildlink: _BUILDLINK_USE
+
+.include "../../mk/bsd.buildlink.mk"
 
 .endif	# GDBM_BUILDLINK_MK
