afe2249f014c72ca4cf465f8d41b8e8524a87825
jlam <jlam@pkgsrc.org>
2001-06-26 03:07:34 +0000

    Add buildlink.mk file for use by other package Makefiles.

---
+++ audio/nas/buildlink.mk
@@ -0,0 +1,33 @@
+# $NetBSD: buildlink.mk,v 1.1 2001/06/26 03:07:34 jlam Exp $
+#
+# This Makefile fragment is included by packages that use nas.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.nas to the dependency pattern
+#     for the version of nas desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(NAS_BUILDLINK_MK)
+NAS_BUILDLINK_MK=	# defined
+
+BUILDLINK_DEPENDS.nas?=	nas>=1.4
+DEPENDS+=	${BUILDLINK_DEPENDS.nas}:../../audio/nas
+
+BUILDLINK_PREFIX.nas=	${X11PREFIX}
+BUILDLINK_FILES.nas=	include/audio/*
+BUILDLINK_FILES.nas+=	lib/libaudio.*
+
+BUILDLINK_TARGETS.nas=	nas-buildlink
+BUILDLINK_TARGETS+=	${BUILDLINK_TARGETS.nas}
+
+pre-configure: ${BUILDLINK_TARGETS.nas}
+nas-buildlink: _BUILDLINK_USE
+
+.include "../../mk/bsd.buildlink.mk"
+
+.endif	# NAS_BUILDLINK_MK
