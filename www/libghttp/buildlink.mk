95b509930d062b9e07a58a0e3b1951b65c4f88da
zuntum <zuntum>
2001-06-30 10:34:42 +0000

    Add buildlink.mk file for use by other packages

---
+++ www/libghttp/buildlink.mk
@@ -0,0 +1,33 @@
+# $NetBSD: buildlink.mk,v 1.1 2001/06/30 10:34:42 zuntum Exp $
+#
+# This Makefile fragment is included by packages that use libghttp.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.libghttp to the dependency pattern
+#     for the version of libghttp desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(LIBGHTTP_BUILDLINK_MK)
+LIBGHTTP_BUILDLINK_MK=	# defined
+
+BUILDLINK_DEPENDS.libghttp?=	libghttp>=1.0.9
+DEPENDS+=	${BUILDLINK_DEPENDS.libghttp}:../../www/libghttp
+
+BUILDLINK_PREFIX.libghttp=	${LOCALBASE}
+BUILDLINK_FILES.libghttp=	include/ghttp*
+BUILDLINK_FILES.libghttp+=	lib/libghttp.*
+
+BUILDLINK_TARGETS.libghttp=	libghttp-buildlink
+BUILDLINK_TARGETS+=	${BUILDLINK_TARGETS.libghttp}
+
+pre-configure: ${BUILDLINK_TARGETS.libghttp}
+libghttp-buildlink: _BUILDLINK_USE
+
+.include "../../mk/bsd.buildlink.mk"
+
+.endif	# LIBGHTTP_BUILDLINK_MK
