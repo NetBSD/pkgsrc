cea2f2364149f563a3a2b44ad27a35bb10fb1627
zuntum <zuntum>
2001-07-03 16:30:19 +0000

    o convert to use buildlink.mk files
    o add buildlink.mk file for use by other packages
    
      Fixes enlightenment build problem noted by David Brownlee <abs@netbsd.org>
      in private mail.

---
+++ graphics/fnlib/buildlink.mk
@@ -0,0 +1,35 @@
+# $NetBSD: buildlink.mk,v 1.1 2001/07/03 16:30:19 zuntum Exp $
+#
+# This Makefile fragment is included by packages that use fnlib.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.fnlib to the dependency pattern
+#     for the version of fnlib desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(FNLIB_BUILDLINK_MK)
+FNLIB_BUILDLINK_MK=	# defined
+
+BUILDLINK_DEPENDS.fnlib?=	fnlib>=0.5
+DEPENDS+=	${BUILDLINK_DEPENDS.fnlib}:../../graphics/fnlib
+
+BUILDLINK_PREFIX.fnlib=	${LOCALBASE}
+BUILDLINK_FILES.fnlib=	include/Fnlib*
+BUILDLINK_FILES.fnlib+=	lib/libFnlib.*
+
+.include "../../graphics/imlib/buildlink.mk"
+
+BUILDLINK_TARGETS.fnlib=	fnlib-buildlink
+BUILDLINK_TARGETS+=	${BUILDLINK_TARGETS.fnlib}
+
+pre-configure: ${BUILDLINK_TARGETS.fnlib}
+fnlib-buildlink: _BUILDLINK_USE
+
+.include "../../mk/bsd.buildlink.mk"
+
+.endif	# FNLIB_BUILDLINK_MK
