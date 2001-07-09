d416d673e54e18b9e0a6c85e4fca5d6fe764e6bd
zuntum <zuntum@pkgsrc.org>
2001-07-09 18:59:48 +0000

    Add buildlink.mk file for use by other packages

---
+++ graphics/libwmf/buildlink.mk
@@ -0,0 +1,45 @@
+# $NetBSD: buildlink.mk,v 1.1 2001/07/09 18:59:48 zuntum Exp $
+#
+# This Makefile fragment is included by packages that use libwmf.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.libwmf to the dependency pattern
+#     for the version of libwmf desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(LIBWMF_BUILDLINK_MK)
+LIBWMF_BUILDLINK_MK=	# defined
+
+.include "../../mk/bsd.buildlink.mk"
+
+BUILDLINK_DEPENDS.libwmf?=	libwmf>=0.1.21
+DEPENDS+=	${BUILDLINK_DEPENDS.libwmf}:../../graphics/libwmf
+
+BUILDLINK_PREFIX.libwmf=	${LOCALBASE}
+BUILDLINK_FILES.libwmf=		include/libwmf/*.h
+BUILDLINK_FILES.libwmf+=	lib/libXwmf.a
+BUILDLINK_FILES.libwmf+=	lib/libdib.a
+BUILDLINK_FILES.libwmf+=	lib/libeps.a
+BUILDLINK_FILES.libwmf+=	lib/libepswmf.a
+BUILDLINK_FILES.libwmf+=	lib/libgdwmf.a
+BUILDLINK_FILES.libwmf+=	lib/libwmf.a
+BUILDLINK_FILES.libwmf+=	lib/libxfig.a
+BUILDLINK_FILES.libwmf+=	lib/libxfwmf.a
+BUILDLINK_FILES.libwmf+=	lib/libxgd.a
+
+.include "../../graphics/freetype-lib/buildlink.mk"
+.include "../../graphics/png/buildlink.mk"
+.include "../../graphics/xpm/buildlink.mk"
+
+BUILDLINK_TARGETS.libwmf=	libwmf-buildlink
+BUILDLINK_TARGETS+=	${BUILDLINK_TARGETS.libwmf}
+
+pre-configure: ${BUILDLINK_TARGETS.libwmf}
+libwmf-buildlink: _BUILDLINK_USE
+
+.endif	# LIBWMF_BUILDLINK_MK
