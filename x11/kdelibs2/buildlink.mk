7fc577c4646c0c55867f2215d396b7949ddce739
jlam <jlam>
2001-07-15 11:06:10 +0000

    Add bin/dcopserver to list of files linked into ${BUILDLINK_DIR} as some programs (qt2-designer-kde) test for that file to determine the presence of KDE2.

--- x11/kdelibs2/buildlink.mk
+++ x11/kdelibs2/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.3 2001/07/09 13:37:08 skrll Exp $
+# $NetBSD: buildlink.mk,v 1.4 2001/07/15 11:06:10 jlam Exp $
 #
 # This Makefile fragment is included by packages that use kdelibs2.
 #
@@ -24,6 +24,7 @@ DEPENDS+=	${BUILDLINK_DEPENDS.kdelibs2}:../../x11/kdelibs2
 
 BUILDLINK_PREFIX.kdelibs2=	${X11PREFIX}
 BUILDLINK_FILES.kdelibs2!=	${GREP} "^\(include\|lib\)" ${.CURDIR}/../../x11/kdelibs2/pkg/PLIST
+BUILDLINK_FILES.kdelibs2+=	bin/dcopserver
 
 .if (defined(KJS_USE_PCRE) && ${KJS_USE_PCRE} == yes)
 .include "../../devel/pcre/buildlink.mk"
