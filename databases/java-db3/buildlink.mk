95d8d10e1f29ba3bfd435422dbe69376cddce3c1
jlam <jlam>
2001-07-28 17:49:00 +0000

    java-db3: Java interface to the Sleepcat DB3 library.
    Idea provided in pkg/13512 and pkg/13518 by Martti Kuparinen
    <martti.kuparinen@iki.fi>.

---
+++ databases/java-db3/buildlink.mk
@@ -0,0 +1,36 @@
+# $NetBSD: buildlink.mk,v 1.1.1.1 2001/07/28 17:49:00 jlam Exp $
+#
+# This Makefile fragment is included by packages that use java-db3.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.java-db3 to the dependency pattern
+#     for the version of java-db3 desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(JAVA_DB3_BUILDLINK_MK)
+JAVA_DB3_BUILDLINK_MK=	# defined
+
+.include "../../mk/bsd.buildlink.mk"
+
+BUILDLINK_DEPENDS.java-db3?=	java-db3>=3.2.9
+DEPENDS+=		${BUILDLINK_DEPENDS.java-db3}:../../databases/java-db3
+
+EVAL_PREFIX+=			BUILDLINK_PREFIX.java-db3=java-db3
+BUILDLINK_PREFIX.java-db3_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.java-db3=	lib/db3.jar
+BUILDLINK_FILES.java-db3+=	lib/libdb3_java.*
+
+.include "../../databases/db3/buildlink.mk"
+
+BUILDLINK_TARGETS.java-db3=	java-db3-buildlink
+BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.java-db3}
+
+pre-configure: ${BUILDLINK_TARGETS.java-db3}
+java-db3-buildlink: _BUILDLINK_USE
+
+.endif	# JAVA_DB3_BUILDLINK_MK
