7a4299d009e1e8b4f9e172a776cdf4cc9b584826
abs <abs>
2001-07-29 01:18:33 +0000

    Import speedtouch-21062001:     Driver for the ALCATEL SpeedTouch USB ADSL modem.     It currently only supports PPPoA encapsulation.

---
+++ net/speedtouch/files/adsl.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+# $NetBSD: adsl.sh,v 1.1.1.1 2001/07/29 01:18:33 abs Exp $
+#
+# PROVIDE: adsl
+# REQUIRE: DAEMON
+#
+# On systems with rc.d you will need to copy this to /etc/rc.d and set
+# 'adsl=YES' in /etc/rc.conf
+
+adsl_start()
+{
+	echo "Starting speedtouch adsl."
+	@PREFIX@/sbin/modem_run -f @PREFIX@/libdata/alcaudsl.sys -m
+	@PREFIX@/sbin/ppp -ddial adsl
+}
+
+adsl_stop()
+{
+	PID=`ps ax | awk '/ppp -ddial adsl $/{print $1}'`
+	if [ -n "$PID" ]; then
+	    echo "Stopping speedtouch adsl."
+	    kill $PID
+	fi
+}
+
+if [ ! -d /etc/rc.d ]
+then
+	case "$1" in
+	    start)	adsl_start		;;
+	    stop)	adsl_stop		;;
+	    rstart)	adsl_stop ; adsl_start	;;
+	esac
+	exit
+fi
+
+. /etc/rc.subr
+
+name="adsl"
+rcvar=$name
+start_cmd="adsl_start"
+stop_cmd="adsl_stop"
+
+load_rc_config $name
+run_rc_command "$1"
