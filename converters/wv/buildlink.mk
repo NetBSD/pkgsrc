d75237de09a3c4d75e86290ba1cb3ed3167b034c
wiz <wiz@pkgsrc.org>
2001-08-06 13:55:18 +0000

    Add buildlink.mk, provided by Martti Kuparinen in pkg/13637.

---
+++ converters/wv/buildlink.mk
@@ -0,0 +1,35 @@
+# $NetBSD: buildlink.mk,v 1.1 2001/08/06 13:55:18 wiz Exp $
+#
+# This Makefile fragment is included by packages that use wv.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.wv to the dependency pattern
+#     for the version of wv desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(WV_BUILDLINK_MK)
+WV_BUILDLINK_MK=	# defined
+
+.include "../../mk/bsd.buildlink.mk"
+
+BUILDLINK_DEPENDS.wv?=	wv>=0.6.5
+DEPENDS+=		${BUILDLINK_DEPENDS.wv}:../../converters/wv
+
+EVAL_PREFIX+=			BUILDLINK_PREFIX.wv=wv
+BUILDLINK_PREFIX.wv_DEFAULT=	${LOCALBASE}
+
+BUILDLINK_FILES.wv=	include/wv.h
+BUILDLINK_FILES.wv+=	lib/libwv.*
+
+BUILDLINK_TARGETS.wv=	wv-buildlink
+BUILDLINK_TARGETS+=	${BUILDLINK_TARGETS.wv}
+
+pre-configure: ${BUILDLINK_TARGETS.wv}
+wv-buildlink: _BUILDLINK_USE
+
+.endif	# WV_BUILDLINK_MK
