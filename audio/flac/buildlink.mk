638aa91496fbc5d3ad95593a5d967ec3c3155036
nra <nra@pkgsrc.org>
2001-08-13 18:46:10 +0000

    buildlink-ify.  PR 13684 by Ben Collver.

---
+++ audio/flac/buildlink.mk
@@ -0,0 +1,34 @@
+# $NetBSD: buildlink.mk,v 1.1 2001/08/13 18:46:10 nra Exp $
+#
+# This Makefile fragment is included by packages that use flac.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.flac to the dependency pattern
+#     for the version of flac desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(FLAC_BUILDLINK_MK)
+FLAC_BUILDLINK_MK=	# defined
+
+.include "../../mk/bsd.buildlink.mk"
+
+BUILDLINK_DEPENDS.flac?=	flac>=1.0
+DEPENDS+=	${BUILDLINK_DEPENDS.flac}:../../audio/flac
+
+EVAL_PREFIX+=			BUILDLINK_PREFIX.flac=flac
+BUILDLINK_PREFIX.flac_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.flac=	include/FLAC/*
+BUILDLINK_FILES.flac+=	lib/libFLAC.*
+
+BUILDLINK_TARGETS.flac=	flac-buildlink
+BUILDLINK_TARGETS+=	${BUILDLINK_TARGETS.flac}
+
+pre-configure: ${BUILDLINK_TARGETS.flac}
+flac-buildlink: _BUILDLINK_USE
+
+.endif	# LIBVORBIS_BUILDLINK_MK
