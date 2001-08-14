9ec7eee25cc9e6058999df27f25cb6eb6bdca66f
lukem <lukem@pkgsrc.org>
2001-08-14 14:53:20 +0000

    update from libvorbis-1.0rc1 to libvorbis-1.0rc2.
    changes:
    * Powerful channel coupling system takes advantage of similarities between
      channels to reduce bitrate.
    * Additional encoding modes give more flexibility in bitrate selection.
      64,80,96,128,160,192,256,350 stereo; 32,48,64,80,112,140 mono modes.
    * Only 44.1KHz/48KHz is officially supported in this release. Lower
      sample rates will work, but with much lower quality.
    * Cascaded encoding makes libvorbis ready for future bitrate reduction tools.
    * Decode engine bugfix ensures better compatibility with future Vorbis codecs.

--- audio/libvorbis/buildlink.mk
+++ audio/libvorbis/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.6 2001/08/08 09:55:28 lukem Exp $
+# $NetBSD: buildlink.mk,v 1.7 2001/08/14 14:53:20 lukem Exp $
 #
 # This Makefile fragment is included by packages that use libvorbis.
 #
@@ -17,7 +17,7 @@ LIBVORBIS_BUILDLINK_MK=	# defined
 
 .include "../../mk/bsd.buildlink.mk"
 
-BUILDLINK_DEPENDS.libvorbis?=	libvorbis>=1.0.0.5
+BUILDLINK_DEPENDS.libvorbis?=	libvorbis>=1.0.0.6
 DEPENDS+=	${BUILDLINK_DEPENDS.libvorbis}:../../audio/libvorbis
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.libvorbis=libvorbis
