8cae1af440380dd903f5661bed1fe11c11f69a9f
jlam <jlam>
2001-08-16 05:09:21 +0000

    * This package doesn't need suse_libc5 anymore...suse_compat is enough. * Remove make for loop and replace with shell for loop * Add an RCS ID and comment to uvscan.sh. * Remove unnecessary whitespace from DESCR.

--- security/uvscan/files/uvscan.sh
+++ security/uvscan/files/uvscan.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-@@PREFIX@@/libexec/uvscan/uvscan $*
+#
+# $NetBSD: uvscan.sh,v 1.2 2001/08/16 05:09:22 jlam Exp $
+#
+# Wrapper script to execute McAfee(R) VirusScan
+
+@PREFIX@/libexec/uvscan/uvscan $*
