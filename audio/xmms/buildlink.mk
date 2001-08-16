1af0ab43824708eda473e063da15f7650f896aa0
zuntum <zuntum@pkgsrc.org>
2001-08-16 04:25:23 +0000

    o add xmms-config support o change package name from "libxmms" to "xmms" in description

--- audio/xmms/buildlink.mk
+++ audio/xmms/buildlink.mk
@@ -1,6 +1,6 @@
-# $NetBSD: buildlink.mk,v 1.2 2001/08/16 03:06:31 hubertf Exp $
+# $NetBSD: buildlink.mk,v 1.3 2001/08/16 04:25:23 zuntum Exp $
 #
-# This Makefile fragment is included by packages that use libxmms.
+# This Makefile fragment is included by packages that use xmms.
 #
 # To use this Makefile fragment, simply:
 #
@@ -33,9 +33,19 @@ BUILDLINK_FILES.xmms+=	lib/libxmms.*
 .include "../../x11/gtk/buildlink.mk"
 
 BUILDLINK_TARGETS.xmms=	xmms-buildlink
+BUILDLINK_TARGETS.xmms+=xmms-buildlink-config-wrapper
 BUILDLINK_TARGETS+=	${BUILDLINK_TARGETS.xmms}
+
+BUILDLINK_CONFIG.xmms=		${BUILDLINK_PREFIX.xmms}/bin/xmms-config
+BUILDLINK_CONFIG_WRAPPER.xmms=	${BUILDLINK_DIR}/bin/xmms-config
+
+.if defined(USE_CONFIG_WRAPPER) && defined(GNU_CONFIGURE)
+XMMS_CONFIG?=		${BUILDLINK_CONFIG_WRAPPER.xmms}
+CONFIGURE_ENV+=		XMMS_CONFIG="${XMMS_CONFIG}"
+.endif
 
 pre-configure: ${BUILDLINK_TARGETS.xmms}
 xmms-buildlink: _BUILDLINK_USE
+xmms-buildlink-config-wrapper: _BUILDLINK_CONFIG_WRAPPER_USE
 
 .endif	# XMMS_BUILDLINK_MK
