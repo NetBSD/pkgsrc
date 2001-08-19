c3b1a82135d85776c28b02da3fe3bfa1f5acbbef
jlam <jlam@pkgsrc.org>
2001-08-19 19:38:37 +0000

    Mark as USE_BUILDLINK_ONLY and add a buildlink.mk file for use by other package Makefiles.

---
+++ converters/uulib/buildlink.mk
@@ -0,0 +1,34 @@
+# $NetBSD: buildlink.mk,v 1.1 2001/08/19 19:38:37 jlam Exp $
+#
+# This Makefile fragment is included by packages that use uulib.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.uulib to the dependency pattern
+#     for the version of uulib desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(UULIB_BUILDLINK_MK)
+UULIB_BUILDLINK_MK=	# defined
+
+.include "../../mk/bsd.buildlink.mk"
+
+BUILDLINK_DEPENDS.uulib?=	uulib>=0.5.15
+DEPENDS+=	${BUILDLINK_DEPENDS.uulib}:../../databases/uulib
+
+EVAL_PREFIX+=		BUILDLINK_PREFIX.uulib=uulib
+BUILDLINK_PREFIX.uulib_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.uulib=	include/uu.h
+BUILDLINK_FILES.uulib+=	lib/libuu.*
+
+BUILDLINK_TARGETS.uulib=	uulib-buildlink
+BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.uulib}
+
+pre-configure: ${BUILDLINK_TARGETS.uulib}
+uulib-buildlink: _BUILDLINK_USE
+
+.endif	# UULIB_BUILDLINK_MK
