38b2235b9dc91f14e1a23d5cececbe9e6c904f64
zuntum <zuntum>
2001-08-19 09:02:53 +0000

    It's AVIFILE_CONFIG, not AVIFILE-DEVEL_CONFIG what needs to be passed to MAKE_ENV and CONFIGURE_ENV
    Not apla gets AVIFILE_CONFIG via MAKE_ENV and uses it, doesn't have to
    fall back to ${LOCALBASE}/bin/avifile-config as specified in AVIFILE_CONFIG?=
    in apla's Makefile.

--- graphics/avifile-devel/buildlink.mk
+++ graphics/avifile-devel/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.4 2001/08/17 21:14:08 jlam Exp $
+# $NetBSD: buildlink.mk,v 1.5 2001/08/19 09:02:53 zuntum Exp $
 #
 # This Makefile fragment is included by packages that use avifile-devel.
 #
@@ -35,9 +35,9 @@ BUILDLINK_CONFIG.avifile-devel=	${BUILDLINK_PREFIX.avifile-devel}/bin/avifile-co
 BUILDLINK_CONFIG_WRAPPER.avifile-devel=${BUILDLINK_DIR}/bin/avifile-config
 
 .if defined(USE_CONFIG_WRAPPER)
-AVIFILE-DEVEL_CONFIG?=		${BUILDLINK_CONFIG_WRAPPER.avifile-devel}
-CONFIGURE_ENV+=			AVIFILE-DEVEL_CONFIG="${AVIFILE-DEVEL_CONFIG}"
-MAKE_ENV+=			AVIFILE-DEVEL_CONFIG="${AVIFILE-DEVEL_CONFIG}"
+AVIFILE_CONFIG?=		${BUILDLINK_CONFIG_WRAPPER.avifile-devel}
+CONFIGURE_ENV+=			AVIFILE_CONFIG="${AVIFILE_CONFIG}"
+MAKE_ENV+=			AVIFILE_CONFIG="${AVIFILE_CONFIG}"
 .endif
 
 pre-configure: ${BUILDLINK_TARGETS.avifile-devel}
