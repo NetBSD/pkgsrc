d109bf48acc68e8f2ff2c65185070ccc728b14a1
jlam <jlam@pkgsrc.org>
2001-08-20 03:54:48 +0000

    Don't automatically add -lgetopt to LIBS, as we usually only want -lgetopt to be linked into just the programs, not everything being linked including libraries.  Also some changes to set _NEED_GETOPT instead of a direct check for /usr/include/getopt.h.

--- devel/libgetopt/buildlink.mk
+++ devel/libgetopt/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.12 2001/07/27 13:33:23 jlam Exp $
+# $NetBSD: buildlink.mk,v 1.13 2001/08/20 03:54:48 jlam Exp $
 #
 # This Makefile fragment is included by packages that use getopt_long().
 #
@@ -19,8 +19,15 @@ GETOPT_BUILDLINK_MK=	# defined
 BUILDLINK_DEPENDS.getopt?=	libgetopt>=1.3
 
 .if exists(/usr/include/getopt.h)
+_NEED_GETOPT=		NO
+.else
+_NEED_GETOPT=		YES
+.endif
+
+.if ${_NEED_GETOPT} == "NO"
 BUILDLINK_PREFIX.getopt=	/usr
 BUILDLINK_FILES.getopt=		include/getopt.h
+LIBGETOPT=			# empty
 .else
 DEPENDS+=	${BUILDLINK_DEPENDS.getopt}:../../devel/libgetopt
 EVAL_PREFIX+=			BUILDLINK_PREFIX.getopt=libgetopt
@@ -28,11 +35,11 @@ BUILDLINK_PREFIX.getopt_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.getopt=		include/getopt.h
 BUILDLINK_FILES.getopt+=	lib/libgetopt.*
 BUILDLINK_FILES.getopt+=	lib/libgetopt_pic.a
+LIBGETOPT=			-lgetopt
+.endif
 
-.if defined(GNU_CONFIGURE)
-LIBS+=				-lgetopt
-.endif
-.endif
+CONFIGURE_ENV+=			LIBGETOPT="${LIBGETOPT}"
+MAKE_ENV+=			LIBGETOPT="${LIBGETOPT}"
 
 BUILDLINK_TARGETS.getopt=	getopt-buildlink
 BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.getopt}
