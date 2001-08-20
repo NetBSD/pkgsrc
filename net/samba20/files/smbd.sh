55d8038c4856dd7091713144ebf5e9c41e39ea9b
jlam <jlam@pkgsrc.org>
2001-08-20 14:01:46 +0000

    smbd responds to SIGHUP by reloading its configuration file, so add "reload" to the rc.d command listing.  Noted by Luke Mewburn <lukem@wasabisystems.com>.

--- net/samba20/files/smbd.sh
+++ net/samba20/files/smbd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: smbd.sh,v 1.1.1.1 2001/08/03 13:24:37 jlam Exp $
+# $NetBSD: smbd.sh,v 1.2 2001/08/20 14:01:46 jlam Exp $
 #
 # PROVIDE: smbd
 # REQUIRE: nmbd
@@ -9,6 +9,7 @@ name="smbd"
 command="@PREFIX@/sbin/${name}"
 command_args="-D"		# _must_ start as daemon from rc.d;
 				# add more flags through ${smbd_flags}
+extra_commands="reload"
 
 if [ ! -d /etc/rc.d ]
 then
