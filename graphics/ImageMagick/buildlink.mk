96098338d44acb737404b37788244e9fadd9c042
jlam <jlam@pkgsrc.org>
2001-08-23 21:32:32 +0000

    Add wrappers for Magick-config and Magick++-config.

--- graphics/ImageMagick/buildlink.mk
+++ graphics/ImageMagick/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.5 2001/07/27 13:33:27 jlam Exp $
+# $NetBSD: buildlink.mk,v 1.6 2001/08/23 21:32:32 jlam Exp $
 #
 # This Makefile fragment is included by packages that use ImageMagick.
 #
@@ -34,9 +34,31 @@ BUILDLINK_FILES.ImageMagick+=	lib/libMagick++.*
 .include "../../graphics/tiff/buildlink.mk"
 
 BUILDLINK_TARGETS.ImageMagick=	ImageMagick-buildlink
+BUILDLINK_TARGETS.ImageMagick+=	magick-buildlink-config-wrapper
+BUILDLINK_TARGETS.ImageMagick+=	magickpp-buildlink-config-wrapper
 BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.ImageMagick}
+
+BUILDLINK_CONFIG.magick=	\
+	${BUILDLINK_PREFIX.ImageMagick}/bin/Magick-config
+BUILDLINK_CONFIG_WRAPPER.magick=	\
+	${BUILDLINK_DIR}/bin/Magick-config
+BUILDLINK_CONFIG.magickpp=	\
+	${BUILDLINK_PREFIX.ImageMagick}/bin/Magick++-config
+BUILDLINK_CONFIG_WRAPPER.magickpp=	\
+	${BUILDLINK_DIR}/bin/Magick++-config
+
+.if defined(USE_CONFIG_WRAPPER)
+MAGICK_CONFIG?=		${BUILDLINK_CONFIG_WRAPPER.magick}
+MAGICKPP_CONFIG?=	${BUILDLINK_CONFIG_WRAPPER.magickpp}
+CONFIGURE_ENV+=		MAGICK_CONFIG="${MAGICK_CONFIG}"
+CONFIGURE_ENV+=		MAGICKPP_CONFIG="${MAGICKPP_CONFIG}"
+MAKE_ENV+=		MAGICK_CONFIG="${MAGICK_CONFIG}"
+MAKE_ENV+=		MAGICKPP_CONFIG="${MAGICKPP_CONFIG}"
+.endif
 
 pre-configure: ${BUILDLINK_TARGETS.ImageMagick}
 ImageMagick-buildlink: _BUILDLINK_USE
+magick-buildlink-config-wrapper: _BUILDLINK_CONFIG_WRAPPER_USE
+magickpp-buildlink-config-wrapper: _BUILDLINK_CONFIG_WRAPPER_USE
 
 .endif	# IMAGEMAGICK_BUILDLINK_MK
