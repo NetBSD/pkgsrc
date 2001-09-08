7e2f8eddd2344a907fb5d8c30f64e6881feaaabb
jlam <jlam@pkgsrc.org>
2001-09-08 20:03:43 +0000

    Fix OpenMotif package to not overwrite X11R6 files when installed without xpkgwedge.  Also, OpenMotif can actually be installed into ${LOCALBASE} if xpkgwedge is installed.  Use x11.buildlink.mk and mark as USE_BUILDLINK_ONLY, and add a buildlink.mk file for use by other package Makefiles.

---
+++ x11/openmotif/buildlink.mk
@@ -0,0 +1,40 @@
+# $NetBSD: buildlink.mk,v 1.1 2001/09/08 20:03:43 jlam Exp $
+#
+# This Makefile fragment is included by packages that use openmotif.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.openmotif to the dependency pattern
+#     for the version of openmotif desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(OPENMOTIF_BUILDLINK_MK)
+OPENMOTIF_BUILDLINK_MK=	# defined
+
+.include "../../mk/bsd.buildlink.mk"
+
+BUILDLINK_DEPENDS.openmotif?=	openmotif>=2.1.30
+DEPENDS+=	${BUILDLINK_DEPENDS.openmotif}:../../x11/openmotif
+
+EVAL_PREFIX+=			BUILDLINK_PREFIX.openmotif=openmotif
+BUILDLINK_PREFIX.openmotif_DEFAULT=	${X11PREFIX}
+BUILDLINK_FILES.openmotif=	include/Mrm/*
+BUILDLINK_FILES.openmotif+=	include/Xm/*
+BUILDLINK_FILES.openmotif+=	include/uil/*
+BUILDLINK_FILES.openmotif+=	lib/libMrm.*
+BUILDLINK_FILES.openmotif+=	lib/libUil.*
+BUILDLINK_FILES.openmotif+=	lib/libXm.*
+
+BUILDLINK_TARGETS.openmotif=	openmotif-buildlink
+BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.openmotif}
+
+MOTIFBASE=			${BUILDLINK_PREFIX.openmotif}
+
+pre-configure: ${BUILDLINK_TARGETS.openmotif}
+openmotif-buildlink: _BUILDLINK_USE
+
+.endif	# OPENMOTIF_BUILDLINK_MK
