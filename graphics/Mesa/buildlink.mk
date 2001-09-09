c4a0ff39222fbd3481d97af0bc5ba9ce5704c4aa
jlam <jlam>
2001-09-09 03:19:42 +0000

    Create an empty xmesa.h file in ${BUILDLINK_DIR}/include if it doesn't already exist.  This file is in Mesa-3.4.2, but not in the Mesa included with XFree86-4.x where the definitions in the file have migrated elsewhere. We create an empty one in this case to prevent compile errors caused by source files including xmesa.h and not finding it.

--- graphics/Mesa/buildlink.mk
+++ graphics/Mesa/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.6 2001/07/01 22:59:21 jlam Exp $
+# $NetBSD: buildlink.mk,v 1.7 2001/09/09 03:19:42 jlam Exp $
 #
 # This Makefile fragment is included by packages that use Mesa.
 #
@@ -35,11 +35,25 @@ BUILDLINK_FILES.Mesa-localbase+=	lib/libGLU.*
 BUILDLINK_FILES.Mesa-localbase+=	lib/libglut.*
 
 BUILDLINK_TARGETS.Mesa+=	Mesa-localbase-buildlink
+BUILDLINK_TARGETS.Mesa+=	Mesa-extra-includes-buildlink
 
 BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.Mesa}
 
 pre-configure: ${BUILDLINK_TARGETS.Mesa}
 Mesa-x11base-buildlink: _BUILDLINK_USE
 Mesa-localbase-buildlink: _BUILDLINK_USE
+
+Mesa-extra-includes-buildlink:
+	${_PKG_SILENT}${_PKG_DEBUG}					\
+	${ECHO_MSG} "Touching extra Mesa headers in ${BUILDLINK_DIR}."; \
+	extra_includes="						\
+		include/GL/xmesa.h					\
+	";								\
+	for file in $${extra_includes}; do				\
+		header=${BUILDLINK_DIR}/$${file};			\
+		if [ ! -e $${header} ]; then				\
+			${TOUCH} ${TOUCH_FLAGS} $${header};		\
+		fi;							\
+	done
 
 .endif	# MESA_BUILDLINK_MK
