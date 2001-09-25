f0c5d6feafe68d057379bd6a649c7f37e1d6aa10
jlam <jlam@pkgsrc.org>
2001-09-25 23:36:49 +0000

    Add missing shared library from XFree86-3.3.x (libXext.so.6.3), noted by Thomas-Peter Klug <thomas-peter.klug@db.com> on port-amiga.

--- pkgtools/x11-links/buildlink.mk
+++ pkgtools/x11-links/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.1.1.1 2001/08/29 22:24:51 jlam Exp $
+# $NetBSD: buildlink.mk,v 1.2 2001/09/25 23:36:49 jlam Exp $
 #
 # This Makefile fragment is included by packages that use X11.
 #
@@ -427,6 +427,7 @@ BUILDLINK_FILES.X11+=	lib/libXdmcp.a
 BUILDLINK_FILES.X11+=	lib/libXext.a
 BUILDLINK_FILES.X11+=	lib/libXext.so
 BUILDLINK_FILES.X11+=	lib/libXext.so.6
+BUILDLINK_FILES.X11+=	lib/libXext.so.6.3
 BUILDLINK_FILES.X11+=	lib/libXext.so.6.4
 BUILDLINK_FILES.X11+=	lib/libXfont.a
 BUILDLINK_FILES.X11+=	lib/libXfont.so
