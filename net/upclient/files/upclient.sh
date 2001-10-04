d9aeaae03b4c5366fbc2be7117c4d85246a4c38c
abs <abs@pkgsrc.org>
2001-10-04 11:05:00 +0000

    Deleted net/upclient - the uptimes project has been suspended, and if it resumes it will use a new client and protocol.

--- net/upclient/files/upclient.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -x @PREFIX@/bin/upclient ]
-then
-	@INSTALL@ -o nobody -m 644 /dev/null /var/run/upclient.pid
-	@SU@ -m nobody -c @PREFIX@/bin/upclient >/dev/null
-	echo -n ' upclient'
-fi
