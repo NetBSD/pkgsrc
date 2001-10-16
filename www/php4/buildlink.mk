344ece92822d4d09fbb2c60de2192af5bc91c589
jlam <jlam@pkgsrc.org>
2001-10-16 04:54:23 +0000

    Update ap-php and php to 4.0.6.  Changes from version 4.0.5 include:
    - Bug fixes (memory leaks and other errors)
    - Made $HTTP_SESSION_VARS['foo'] and $foo be references to the same value
      when register_globals is on. (Andrei)
    - Added is_callable() function that can be used to find out whether
      its argument is a valid callable construct. (Andrei)
    - Added pg_last_notice() function. (Rasmus from suggestion by Dirk@rackspace.com)
    - Added support to getimagesize to return dimensions of BMP and PSD
      files. (Derick)
    - Added Japanese multibyte string functions support. (Rui)
    - Added key_exists() to check if a given key or index exists in an
      array or object. (David Croft)
    - Added -C command-line option to avoid chdir to the script's directory. (Stig)
    - printf argnum (parameter swapping) support. (Morten Poulsen, Rasmus)
    - Modified get_parent_class() and get_class_methods() to accept a class name as
      well as a class instance. (Andrei, Zend Engine)
    - Added array_map() function that applies a callback to the elements
      of given arrays and returns the result. It can also be used with a
      null callback to transpose arrays. (Andrei)
    - Added array_filter(), which allows filtering of array elements via
      the specified callback. (Andrei)

---
+++ www/php4/buildlink.mk
@@ -0,0 +1,49 @@
+# $NetBSD: buildlink.mk,v 1.1 2001/10/16 04:54:24 jlam Exp $
+#
+# This Makefile fragment is included by packages that use php4.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.php4 to the dependency pattern
+#     for the version of php4 desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(PHP4_BUILDLINK_MK)
+PHP4_BUILDLINK_MK=	# defined
+
+.include "../../mk/bsd.buildlink.mk"
+
+# This adds a build-dependency as php4 has no libraries.
+BUILDLINK_DEPENDS.php4?=	php>=4.0.6
+BUILD_DEPENDS+=			${BUILDLINK_DEPENDS.php4}:../../www/php4
+
+EVAL_PREFIX+=			BUILDLINK_PREFIX.php4=php
+BUILDLINK_PREFIX.php4_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.php4=		include/php/*/*/*.h
+BUILDLINK_FILES.php4+=		include/php/*/*.h
+BUILDLINK_FILES.php4+=		include/php/*.h
+
+BUILDLINK_TARGETS.php4=		php4-buildlink
+BUILDLINK_TARGETS.php4+=	php4-buildlink-config-wrapper
+BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.php4}
+
+BUILDLINK_CONFIG.php4=		${BUILDLINK_PREFIX.php4}/bin/php-config
+BUILDLINK_CONFIG_WRAPPER.php4=	${BUILDLINK_DIR}/bin/php-config
+REPLACE_BUILDLINK_SED+=	\
+	-e "s|${BUILDLINK_CONFIG_WRAPPER.php4}|${BUILDLINK_CONFIG.php4}|g"
+
+.if defined(USE_CONFIG_WRAPPER)
+PHP_CONFIG?=		${BUILDLINK_CONFIG_WRAPPER.php4}
+CONFIGURE_ENV+=		PHP_CONFIG="${PHP_CONFIG}"
+MAKE_ENV+=		PHP_CONFIG="${PHP_CONFIG}"
+.endif
+
+pre-configure: ${BUILDLINK_TARGETS.php4}
+php4-buildlink: _BUILDLINK_USE
+php4-buildlink-config-wrapper: _BUILDLINK_CONFIG_WRAPPER_USE
+
+.endif	# PHP4_BUILDLINK_MK
