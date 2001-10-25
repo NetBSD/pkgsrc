7206d91640e6fa24a677e67fcbc43657916f3ba7
agc <agc>
2001-10-25 20:58:50 +0000

    Move the linux-mkpwd.sh script from scripts/ to files/

--- emulators/suse_base/files/linux-mkpwd.sh
+++ emulators/suse_base/files/linux-mkpwd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: linux-mkpwd.sh,v 1.1.1.1 1999/05/17 00:14:50 tron Exp $
+# $NetBSD: linux-mkpwd.sh,v 1.1 2001/10/25 20:58:51 agc Exp $
 
 SRCDIR=/etc
 DESTDIR=@@EMULDIR@@/etc
