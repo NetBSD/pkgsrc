fd673633f27996b8d5dd81f4e1b0950d704b39b8
hubertf <hubertf@pkgsrc.org>
2001-11-03 21:34:35 +0000

    Bump to 3.11: warn about scripts/* and pkg/* dirs and contents

--- pkgtools/pkglint/files/pkglint.pl
+++ pkgtools/pkglint/files/pkglint.pl
@@ -12,7 +12,7 @@
 # Freely redistributable.  Absolutely no warranty.
 #
 # From Id: portlint.pl,v 1.64 1998/02/28 02:34:05 itojun Exp
-# $NetBSD: pkglint.pl,v 1.59 2001/11/03 21:14:18 wiz Exp $
+# $NetBSD: pkglint.pl,v 1.60 2001/11/03 21:34:36 hubertf Exp $
 #
 # This version contains lots of changes necessary for NetBSD packages
 # done by Hubert Feyrer <hubertf@netbsd.org>,
@@ -203,6 +203,14 @@ if (-e <$portdir/$filesdir/patch-sum> ) {
 }
 if (-e <$pkgdir/COMMENT> ) {
 	&perror("FATAL: $pkgdir/COMMENT is deprecated -- please use a COMMENT variable instead.");
+}
+if (-d "$portdir/pkg" ) {
+	&perror("FATAL: $portdir/pkg and it's contents are deprecated!\n".
+		"\tPlease 'mv $portdir/pkg/* $portdir' and 'rmdir $portdir/pkg'.");
+}
+if (-d "$portdir/scripts" ) {
+	&perror("WARN: $portdir/scripts and it's contents are deprecated! Please call the script(s)\n".
+		"\texplicitly from the corresponding target(s) in the pkg's Makefile.");
 }
 if (! -f "$portdir/$pkgdir/PLIST"
     and ! -f "$portdir/$pkgdir/PLIST-mi"
