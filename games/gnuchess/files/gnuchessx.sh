43329a34d73c50a01f9e44c306ebc67873e6bdd0
jlam <jlam@pkgsrc.org>
2001-11-11 07:54:55 +0000

    Apply a patch from Tim Mann's chess site that fixes the following problem:
        The "edit" function is not implemented in GNU Chess 5.00. This means
        that XBoard's "Edit Position" feature will not work with it.  Loading a
        position from a file, loading a PGN game that starts from a nonstandard
        position, or playing an ICS wild game in Zippy mode, etc., will also not
        work.
    
    Also fix the gnuchessx script to accept options and to pass them through to
    the gnuchess binary.
    
    Bump package version number to 5.00nb2.

--- games/gnuchess/files/gnuchessx.sh
+++ games/gnuchess/files/gnuchessx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-# $NetBSD: gnuchessx.sh,v 1.1 2000/03/26 08:29:25 jlam Exp $
+# $NetBSD: gnuchessx.sh,v 1.2 2001/11/11 07:54:55 jlam Exp $
 #
 # Wrapper script for gnuchess to interface with xboard.
 
-exec @PREFIX@/bin/gnuchess xboard
+exec @PREFIX@/bin/gnuchess xboard $*
