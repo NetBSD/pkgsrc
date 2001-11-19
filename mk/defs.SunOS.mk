5831dfc44e785c21792788aede23f0127c652a75
jlam <jlam>
2001-11-19 16:17:51 +0000

    Add definitions for USERADD and GROUPADD, used to create new users and groups.  These commands follow the basic interface of the NetBSD/Solaris useradd and groupadd programs.  For platforms on which these commands don't exist, either sysutils/user is added as a dependency, or these are set to ${FALSE}, and the package admin is responsible for creating them himself prior to the installation of a package.

--- mk/defs.SunOS.mk
+++ mk/defs.SunOS.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.SunOS.mk,v 1.5 2001/07/15 12:40:03 jlam Exp $
+# $NetBSD: defs.SunOS.mk,v 1.6 2001/11/19 16:17:51 jlam Exp $
 #
 # Variable definitions for the SunOS/Solaris operating system.
 
@@ -61,3 +61,7 @@ TRUE?=		/usr/bin/true
 TYPE?=		/usr/bin/type
 WC?=		/usr/bin/wc
 XARGS?=		/usr/bin/xargs
+
+USERADD?=	/usr/sbin/useradd
+GROUPADD?=	/usr/sbin/groupadd
+NOLOGIN?=	${FALSE}
