3ed625524ca59dadd0371a344be472da0cd18a14
jlam <jlam>
2001-11-21 21:38:50 +0000

    These rc.d scripts are copied to ${RCD_SCRIPTS_DIR} == /etc/rc.d, so change the test for pre-rc.d systems to just whether /etc/rc.subr is present.

--- sysutils/ups-nut/files/upsd.sh
+++ sysutils/ups-nut/files/upsd.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 #
-# $NetBSD: upsd.sh,v 1.1 2001/11/21 15:50:56 jlam Exp $
+# $NetBSD: upsd.sh,v 1.2 2001/11/21 21:38:55 jlam Exp $
 #
 # PROVIDE: upsd
 # REQUIRE: upsdriver
 
-if [ -d /etc/rc.d -a -f /etc/rc.subr ]
+if [ -e /etc/rc.subr ]
 then
 	. /etc/rc.subr
 fi
@@ -16,11 +16,11 @@ command="@PREFIX@/sbin/${name}"
 required_files="@NUT_CONFDIR@/${name}.conf"
 pidfile="@NUT_STATEDIR@/${name}.pid"
 
-if [ ! -d /etc/rc.d ]
+if [ -e /etc/rc.d ]
 then
-	@ECHO@ -n " ${name}"
-	${command} ${upsd_flags} ${command_args}
-else
 	load_rc_config $name
 	run_rc_command "$1"
+else
+	@ECHO@ -n " ${name}"
+	${command} ${upsd_flags} ${command_args}
 fi
