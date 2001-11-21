6155a451394fb1b2658a3a60a54eef7752f36c61
jlam <jlam@pkgsrc.org>
2001-11-21 16:10:03 +0000

    Refer to /etc/rc.d as ${RCD_SCRIPTS_DIR} more thoroughly.

--- mk/bsd.pkg.install.mk
+++ mk/bsd.pkg.install.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.pkg.install.mk,v 1.3 2001/11/21 15:43:01 jlam Exp $
+# $NetBSD: bsd.pkg.install.mk,v 1.4 2001/11/21 16:10:03 jlam Exp $
 #
 # This Makefile fragment is included by package Makefiles to use the common
 # INSTALL/DEINSTALL scripts.  To use this Makefile fragment, simply:
@@ -134,7 +134,7 @@ FILES_SUBST+=		RCD_SCRIPTS_DIR=${RCD_SCRIPTS_DIR}
 #       created and should attempt to be destroyed by the INSTALL/DEINSTALL
 #	scripts.  MAKE_DIRS is used the same way, but the package admin
 #	isn't prompted to remove the directory at post-deinstall time if it
-#	isn't empty.  For convenience, ${CONFDIR} and /etc/rc.d are
+#	isn't empty.  For convenience, ${CONFDIR} and ${RCD_SCRIPTS_DIR} are
 #	automatically added to MAKE_DIRS.
 #
 # OWN_DIRS_PERMS contains a list of "directory owner group mode" sublists
@@ -145,7 +145,7 @@ FILES_SUBST+=		RCD_SCRIPTS_DIR=${RCD_SCRIPTS_DIR}
 #
 MAKE_DIRS?=		# empty
 MAKE_DIRS_PERMS?=	# empty
-_MAKE_DIRS=		${CONFDIR} /etc/rc.d ${MAKE_DIRS}
+_MAKE_DIRS=		${CONFDIR} ${RCD_SCRIPTS_DIR} ${MAKE_DIRS}
 OWN_DIRS?=		# empty
 OWN_DIRS_PERMS?=	# empty
 FILES_SUBST+=		MAKE_DIRS=${_MAKE_DIRS:Q}
