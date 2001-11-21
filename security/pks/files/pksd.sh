aba0d94753759bda6caf16aec96199b1ef93ca0d
jlam <jlam@pkgsrc.org>
2001-11-21 15:14:01 +0000

    Buildlinkify and use general INSTALL/DEINSTALL scripts.  Also modify rc.d script to respect ${CONFDIR} and look more like other 1.5-release rc.d scripts.

--- security/pks/files/pksd.sh
+++ security/pks/files/pksd.sh
@@ -1,52 +1,78 @@
 #!/bin/sh
 #
-# $NetBSD: pksd.sh,v 1.2 2000/09/20 05:14:12 jlam Exp $
+# $NetBSD: pksd.sh,v 1.3 2001/11/21 15:14:02 jlam Exp $
 #
 # PROVIDE: pksd
 # REQUIRE: DAEMON
 
-name="pksd"
-command=${1:-start}
+if [ -d /etc/rc.d -a -f /etc/rc.subr ]
+then
+	. /etc/rc.subr
+fi
 
-case ${command} in
-start)
-	if [ ! -f /etc/${name}.conf ]
+name="pksd"
+rcvar=${name}
+
+command="@PREFIX@/bin/pksd"
+run_command="@PREFIX@/bin/pks-queue-run.sh"
+ctl_command="@PREFIX@/bin/pksdctl"
+client_command="@PREFIX@/bin/pksclient"
+
+conf_file="@CONFDIR@/${name}.conf"
+required_files="${conf_file}"
+extra_commands="dbinit"
+
+pksd_dbinit()
+{
+	(
+	umask 022
+	if [ -r ${conf_file} ]
 	then
-		exit 0
-	else
-		dbdir=`awk '/db_dir/ { print $2 }' < /etc/${name}.conf`
+		dbdir=`@AWK@ '/db_dir/ { print $2 }' < ${conf_file}`
+		if [ ! -f ${dbdir}/keydb000 -a -x ${client_command} ]
+		then
+			${client_command} ${dbdir} create
+		fi
 	fi
-	if [ ! -f ${dbdir}/keydb000 -a -x @PREFIX@/bin/pksclient ]
+	)
+}
+
+pksd_start()
+{
+	if [ -r ${conf_file} -a -x ${command} -a -x ${run_command} ]
 	then
-		@PREFIX@/bin/pksclient ${dbdir} create
-	fi
-	if [ -x @PREFIX@/bin/${name} -a -x @PREFIX@/bin/pks-queue-run.sh ]
-	then
-		echo "Starting ${name}."
-		@PREFIX@/bin/${name} /etc/${name}.conf &
+		@ECHO@ "Starting ${name} (local)."
+		@ECHO@ "${command} ${conf_file}"
+		${command} ${conf_file} &
 		sleep 5
-		@PREFIX@/bin/pks-queue-run.sh /etc/${name}.conf
+		${run_command} ${conf_file}
 	fi
-	;;
-stop)
-	if [ ! -f /etc/${name}.conf ]
+}
+
+pksd_stop()
+{
+	if [ -r ${conf_file} -a -x ${ctl_command} ]
 	then
-		exit 0
-	else
-		socket=`awk '/socket_name/ { print $2 }' < /etc/${name}.conf`
+		socket=`@AWK@ '/socket_name/ { print $2 }' < ${conf_file}`
+		if [ -S ${socket} ]
+		then
+			@ECHO@ "Stopping ${name} (local)."
+			${ctl_command} ${socket} shutdown
+		fi
 	fi
-	if [ -x @PREFIX@/bin/${name}ctl -a -S ${socket} ]
-	then
-		echo "Stopping ${name}."
-		@PREFIX@/bin/${name}ctl ${socket} shutdown
-	else
-		echo "couldn't stop ${name}."
-	fi
-	;;
-restart)
-	( $0 stop )
-	sleep 1
-	$0 start
-	;;
-esac
-exit 0
+} 
+
+dbinit_cmd=pksd_dbinit
+start_precmd=pksd_dbinit
+start_cmd=pksd_start
+stop_cmd=pksd_stop
+
+if [ ! -d /etc/rc.d ]
+then
+	@ECHO@ -n " ${name}"
+	pksd_dbinit
+	pksd_start
+else
+	load_rc_config $name
+	run_rc_command "$1"
+fi
