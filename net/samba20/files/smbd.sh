83536bb6ee068534930395b255ff3b0f1fa7865f
jlam <jlam@pkgsrc.org>
2001-11-21 17:17:43 +0000

    Use the general INSTALL/DEINSTALL scripts, and minimize the differences between this package and the net/samba package.

---
+++ net/samba20/files/smbd.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# $NetBSD: smbd.sh,v 1.4 2001/11/21 17:17:44 jlam Exp $
+#
+# PROVIDE: smbd
+# REQUIRE: DAEMON
+
+if [ -d /etc/rc.d -a -f /etc/rc.subr ]
+then
+	. /etc/rc.subr
+fi
+
+name="smbd"
+rcvar=$name
+command="@PREFIX@/sbin/${name}"
+required_files="@SAMBA_ETCDIR@/smb.conf"
+extra_commands="reload"
+command_args="-D"		# _must_ start as daemon from rc.d;
+				# add more flags through ${${name}_flags}
+
+if [ ! -d /etc/rc.d ]
+then
+	@ECHO@ -n ' ${name}'
+	${command} ${smbd_flags} ${command_args}
+else
+	load_rc_config $name
+	run_rc_command "$1"
+fi
