3a1a0965440cf67d4da924a7c0f13c0d7e287ba6
jlam <jlam>
2001-11-21 17:55:54 +0000

    Remove unneeded INSTALL script and update the rplayd.sh to look like an rc.d script.

--- audio/rplay/files/rplayd.sh
+++ audio/rplay/files/rplayd.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 #
-# $NetBSD: rplayd.sh,v 1.1 2000/01/26 22:11:36 jlam Exp $
+# $NetBSD: rplayd.sh,v 1.2 2001/11/21 17:55:55 jlam Exp $
+#
+# PROVIDE: rplayd
+# REQUIRE: DAEMON
 
-if [ -x @PREFIX@/sbin/rplayd ]; then
-	echo -n ' rplayd';	@PREFIX@/sbin/rplayd
+if [ -d /etc/rc.d -a -f /etc/rc.subr ]
+then
+	. /etc/rc.subr
+fi
+
+name="rplayd"
+rcvar=$name
+command="@PREFIX@/sbin/${name}"
+required_files="@CONFDIR@/rplay.conf"
+
+if [ ! -d /etc/rc.d ]
+then
+	@ECHO@ -n ' ${name}'
+	${command} ${rplayd_flags} ${command_args}
+else
+	load_rc_config $name
+	run_rc_command "$1"
 fi
