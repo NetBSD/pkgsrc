9aef11dbb155afb6f9b24e4b9102541e50a5fa24
jlam <jlam@pkgsrc.org>
2001-11-26 05:18:01 +0000

    p5-HTTPD-User-Manage also used APACHE_SYSCONFDIR (implicitly).  Also re-indent for aesthetic considerations.

--- mk/bsd.pkg.obsolete.mk
+++ mk/bsd.pkg.obsolete.mk
@@ -1,24 +1,25 @@
-# $NetBSD: bsd.pkg.obsolete.mk,v 1.2 2001/11/25 19:33:45 jlam Exp $
+# $NetBSD: bsd.pkg.obsolete.mk,v 1.3 2001/11/26 05:18:01 jlam Exp $
 #
 # This file holds make(1) logic to allow obsolete or deprecated variables
 # still to be used.  These may eventually disappear over time as the contents
 # are, by definition, obsolete and deprecated.
 
 .if defined(APACHE_SYSCONFDIR)
-PKG_SYSCONFDIR.ap-ssl?=		${APACHE_SYSCONFDIR}
-PKG_SYSCONFDIR.apache?=		${APACHE_SYSCONFDIR}
-PKG_SYSCONFDIR.apache6?=	${APACHE_SYSCONFDIR}
-PKG_SYSCONFDIR.apc-gui?=	${APACHE_SYSCONFDIR}
-PKG_SYSCONFDIR.imp?=		${APACHE_SYSCONFDIR}
-PKG_SYSCONFDIR.horde?=		${APACHE_SYSCONFDIR}
-PKG_SYSCONFDIR.squirrelmail?=	${APACHE_SYSCONFDIR}
-PKG_SYSCONFDIR.stripes?=	${APACHE_SYSCONFDIR}
+PKG_SYSCONFDIR.ap-ssl?=			${APACHE_SYSCONFDIR}
+PKG_SYSCONFDIR.apache?=			${APACHE_SYSCONFDIR}
+PKG_SYSCONFDIR.apache6?=		${APACHE_SYSCONFDIR}
+PKG_SYSCONFDIR.apc-gui?=		${APACHE_SYSCONFDIR}
+PKG_SYSCONFDIR.imp?=			${APACHE_SYSCONFDIR}
+PKG_SYSCONFDIR.horde?=			${APACHE_SYSCONFDIR}
+PKG_SYSCONFDIR.p5-HTTPD-User-Manage?=	${APACHE_SYSCONFDIR}
+PKG_SYSCONFDIR.squirrelmail?=		${APACHE_SYSCONFDIR}
+PKG_SYSCONFDIR.stripes?=		${APACHE_SYSCONFDIR}
 .endif
 
 .if defined(PHP_CONFDIR)
-PKG_SYSCONFDIR.php?=		${PHP_CONFDIR}
+PKG_SYSCONFDIR.php?=			${PHP_CONFDIR}
 .endif
 
 .if defined(VTUN_SYSCONFDIR)
-PKG_SYSCONFDIR.vtun?=		${VTUN_SYSCONFDIR}
+PKG_SYSCONFDIR.vtun?=			${VTUN_SYSCONFDIR}
 .endif
