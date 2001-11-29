69faace063681b2c0feac360168f9a110d6f8d78
seb <seb@pkgsrc.org>
2001-11-29 11:41:28 +0000

    Initial import of hesiod-3.0.2.
    Provided in PR 14649 by Kevin P. Neal <kpneal@pobox.com>.
    
    Minor enhancements and buildlink.mk made by seb.
    A package builder can force the use of this package by defining NEED_HESIOD in
    case the base system already provides Hesiod API in the C library.
    Note: NetBSD's C library provides minimum Hesiod support, the library included
    in this package is the MIT original one and so provides the whole Hesiod API.
    
    Hesiod is a name service library that can provide general name service for a
    variety of applications. It is based on the Domain Name system (DNS).
    The Hesiod library and hesinfo tool allow easy queries to Hesiod name service.

---
+++ net/hesiod/buildlink.mk
@@ -0,0 +1,53 @@
+# $NetBSD: buildlink.mk,v 1.1.1.1 2001/11/29 11:41:28 seb Exp $
+#
+# This Makefile fragment is included by packages that use the Hesiod name 
+# service library. 
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define USE_HESIOD to force use of the hesiod library
+#     included in this package.
+# (2) Optionally define BUILDLINK_DEPENDS.hesiod to the dependency pattern
+#     for the version of hesiod library desired.
+# (3) Include this Makefile fragment in the package Makefile,
+# (4) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (5) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(HESIOD_BUILDLINK_MK)
+HESIOD_BUILDLINK_MK=	# defined
+
+.include "../../mk/bsd.buildlink.mk"
+
+BUILDLINK_DEPENDS.hesiod?=	hesiod>=3.0.2
+
+.if defined(USE_HESIOD)
+_NEED_HESIOD=		YES
+.else
+.  if exists(/usr/include/hesiod.h)
+_NEED_HESIOD=		NO
+.  else
+_NEED_HESIOD=		YES
+.  endif
+.endif
+
+.if ${_NEED_HESIOD} == "YES"
+DEPENDS+=		${BUILDLINK_DEPENDS.hesiod}:../../net/hesiod
+EVAL_PREFIX+=		BUILDLINK_PREFIX.hesiod=hesiod
+BUILDLINK_PREFIX.hesiod_DEFAULT=	${LOCALBASE}
+.else
+BUILDLINK_PREFIX.hesiod=	/usr
+.endif
+
+BUILDLINK_FILES.hesiod=		include/hesiod.h
+# if lib/libhesiod.* does not exists assume hesiod service is included in libc
+BUILDLINK_FILES.hesiod+=	lib/libhesiod.*
+
+BUILDLINK_TARGETS.hesiod=	hesiod-buildlink
+BUILDLINK_TARGETS+=	${BUILDLINK_TARGETS.hesiod}
+
+pre-configure: ${BUILDLINK_TARGETS.hesiod}
+hesiod-buildlink: _BUILDLINK_USE
+
+.endif	# HESIOD_BUILDLINK_MK
