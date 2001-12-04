a1ffed43fc392a6b2cd3dc255835ff76bacc4b7e
jlam <jlam@pkgsrc.org>
2001-12-04 05:52:22 +0000

    * Strongly buildlink'ify. * Reduce the number of patches needed by passing flags down to the make(1)   process instead of patching the Makefiles and sources in unnecessary ways. * Remove unnecessary endian.sh script as the computation is down directly   in the package Makefile. * Preliminary support for platforms other than NetBSD. * Generalize the package Makefile enough to make it usable if/when xmess   compiles again on UNIX platforms * Use the general INSTALL scripts to manage the spool directory during   installation/deinstallation.
    Approved by kristerw@netbsd.org.

--- emulators/xmame/files/endian.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# $Id: endian.sh,v 1.1 2000/12/07 12:58:14 abs Exp $
-# Trivial script to output BIG_ENDIAN or LITTLE_ENDIAN
-printf '#include <machine/endian.h>\n#if BYTE_ORDER == BIG_ENDIAN\nORDER_BIG_ENDIAN\n#else\nORDER_LITTLE_ENDIAN\n#endif\n' | cpp -P | sed -n 's/^ORDER_//p'
