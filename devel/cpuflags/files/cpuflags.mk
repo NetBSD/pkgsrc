26d74908a6f48beb2019e49274154eef2fdd5631
abs <abs@pkgsrc.org>
2001-12-24 13:25:47 +0000

    Updated cpuflags to 0.15: 	Determine the appropriate flags for the Alpha 433a I now have. 	(Insert appropriate grumble about there being no sysctl way to tell 	 the cputype on an Alpha)

--- devel/cpuflags/files/cpuflags.mk
+++ devel/cpuflags/files/cpuflags.mk
@@ -1,20 +1,23 @@
-# $Id: cpuflags.mk,v 1.1 2001/06/29 11:24:51 abs Exp $
+# $Id: cpuflags.mk,v 1.2 2001/12/24 13:25:47 abs Exp $
 # Makefile include fragment to simplify use of cpuflags in pkgsrc
 # abs@netbsd.org - freely distributable, no warrenties, stick no bills.
 
 # Try to optimise based on CPU
 # Does not affect Imakefiles, and any package that overrides CFLAGS/CXXFLAGS
-# Sets four variables:
+# Sets five variables:
 #
 #	CPU_FLAGS	The output of cpuflags
-#	CPU_DIR		CPU_FLAGS with spaces stripped (eg: for use in PACKAGES)
 #	CFLAGS		Has CPU_FLAGS appended
 #	CXXFLAGS	Has CPU_FLAGS appended
+#
+#	CPU_DIR		CPU_FLAGS with spaces stripped (eg: for use in PACKAGES)
+#	LOWER_CPU_DIR	As CPU_FLAGS but all lower case
 #
 
 .if ! defined(CPU_FLAGS)
 CPU_FLAGS!=/usr/pkg/bin/cpuflags
 CPU_DIR!=echo ${CPU_FLAGS} | sed 's/ //'
+LOWER_CPU_DIR!=echo ${CPU_DIR} | tr A-Z a-z
 MAKE_ENV+=CPU_FLAGS=${CPU_FLAGS} CPU_DIR=${CPU_DIR}
 CFLAGS+=${CPU_FLAGS}
 CXXFLAGS+=${CPU_FLAGS}
