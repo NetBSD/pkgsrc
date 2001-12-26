e782bc15da11425360cd61d4dfff0cf50806944e
jlam <jlam>
2001-12-26 22:03:38 +0000

    Vastly simplify this script by using /etc/rc.subr.  Provide minimal start functionality on pre-1.5 platforms, as apachectl may always be used instead for the full control interface.

--- www/apache6/files/apache.sh
+++ www/apache6/files/apache.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: apache.sh,v 1.10 2001/11/26 07:34:07 jlam Exp $
+# $NetBSD: apache.sh,v 1.11 2001/12/26 22:03:38 jlam Exp $
 #
 # PROVIDE: apache
 # REQUIRE: DAEMON
@@ -9,9 +9,15 @@
 # To start apache at startup, copy this script to /etc/rc.d and set
 # apache=YES in /etc/rc.conf.
 
+if [ -e /etc/rc.subr ]
+then
+	. /etc/rc.subr
+fi
+
 name="apache"
 rcvar=$name
-command="@PREFIX@/sbin/apachectl"
+command="@PREFIX@/sbin/httpd"
+ctl_command="@PREFIX@/sbin/apachectl"
 
 apache_start=start
 if [ -f @PKG_SYSCONFDIR@/apache_start.conf ]
@@ -20,66 +26,15 @@ then
 	. @PKG_SYSCONFDIR@/apache_start.conf
 fi
 
-# set defaults
-if [ -r /etc/rc.conf ]
+required_files="@PKG_SYSCONFDIR@/httpd.conf"
+start_cmd="${ctl_command} ${apache_start}"
+stop_cmd="${ctl_command} stop"
+restart_cmd="${ctl_command} restart"
+
+if [ -e /etc/rc.subr ]
 then
-	. /etc/rc.conf
+	load_rc_config $name
+	run_rc_command "$1"
 else
-	eval ${rcvar}=YES
-fi
-
-# $flags from environment overrides ${rcvar}_flags
-if [ -n "${flags}" ]
-then
-	eval ${rcvar}_flags="${flags}"
-fi
-
-apache_doit()
-{
-	action=$1
-
-	case ${action} in
-	start)		echo -n "Starting ${name}: "; action=${apache_start} ;;
-	stop)		echo -n "Stopping ${name}: " ;;
-	restart)	echo -n "Restarting ${name}: " ;;
-	esac
-
-	${command} ${apache_flags} ${command_args} ${action}
-}
-
-checkyesno()
-{
-	eval _value=\$${1}
-	case $_value in
-	[Yy][Ee][Ss]|[Tt][Rr][Uu][Ee]|[Oo][Nn]|1)	return 0 ;;
-	[Nn][Oo]|[Ff][Aa][Ll][Ss][Ee]|[Oo][Ff][Ff]|0)	return 1 ;;
-	*)
-		echo "\$${1} is not set properly."
-		return 1
-		;;
-	esac
-}
-
-cmd=${1:-start}
-case ${cmd} in
-force*)
-	cmd=${cmd#force}
-	eval ${rcvar}=YES
-	;;
-esac
-
-if checkyesno ${rcvar}
-then
-	if [ -x ${command} ]
-	then
-		case ${cmd} in
-		restart|start|stop|status)
-			${rcvar}_doit ${cmd}
-			;;
-		*)
-			echo 1>&2 "Usage: $0 [restart|start|stop|status]"
-			exit 1
-			;;
-		esac
-	fi
+	${start_cmd}
 fi
