6d9eb2fdd6e6ea8632557b979176125620a7e944
jlam <jlam>
2002-01-02 19:26:01 +0000

    Also look for maildirmake.*.  Bump to version 0.3.

--- mail/courier-maildirmake/files/maildirmake.sh
+++ mail/courier-maildirmake/files/maildirmake.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
-# $NetBSD: maildirmake.sh,v 1.2 2002/01/01 08:00:45 jlam Exp $
+# $NetBSD: maildirmake.sh,v 1.3 2002/01/02 19:26:02 jlam Exp $
 #
 # Wrapper to execute maildirmake from amongst any installed maildirmake
 # executable.
 
 maildirmake=
-for file in @LOCALBASE@/bin/*.maildirmake
+for file in @LOCALBASE@/bin/maildirmake.* @LOCALBASE@/bin/*.maildirmake
 do
 	if [ -x ${file} ]
 	then
