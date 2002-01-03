453bb6fe234ca46b7c9fa40bd08ef1fec0618a0e
tron <tron>
2002-01-03 20:40:37 +0000

    Update "gcc" package to version 2.95.3. This is a bug fix release of version 2.95.2. This package includes all changes to "gcc" from the new toolchain in NetBSD-current. The only tested (and enabled) platform is "NetBSD-*-i386" so far.

--- lang/gcc/files/netbsd.ml
+++
@@ -1,8 +0,0 @@
-# a.out with shared libm, so we can link it into the shared libstdc++.
-
-ARLIB   = libstdc++.a.`echo $(VERSION) | sed 's/\([0-9]*[.][0-9]*\).*/\1/'`
-SHLIB   = libstdc++.so.`echo $(VERSION) | sed 's/\([0-9]*[.][0-9]*\).*/\1/'`
-
-LIBS    = $(ARLIB) $(ARLINK) $(SHLIB)
-SHDEPS  = -lm
-DEPLIBS = ../$(SHLIB)
