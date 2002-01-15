fc6b971a19e241bf29c8d1f89fd490e393de1aad
drochner <drochner@pkgsrc.org>
2002-01-15 18:38:46 +0000

    allow to override the required version just like other buildlink.mks do

--- math/py-Numeric/buildlink.mk
+++ math/py-Numeric/buildlink.mk
@@ -1,5 +1,6 @@
-# $NetBSD: buildlink.mk,v 1.1 2002/01/10 20:47:51 drochner Exp $
+# $NetBSD: buildlink.mk,v 1.2 2002/01/15 18:38:46 drochner Exp $
 
 .include "../../lang/python/pyversion.mk"
 
-DEPENDS+=	${PYPKGPREFIX}-Numeric-*:../math/py-Numeric
+BUILDLINK_DEPENDS.pynumeric?= ${PYPKGPREFIX}-Numeric-*
+DEPENDS+= ${BUILDLINK_DEPENDS.pynumeric}:../math/py-Numeric
