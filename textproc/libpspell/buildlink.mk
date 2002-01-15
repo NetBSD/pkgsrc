dd9ae92aa7621ac12ee98a50035cba40582746e1
wiz <wiz>
2002-01-15 01:05:14 +0000

    Update to 0.12.2; also buildlink'ify and add buildlink.mk. Changes since 0.11.2: * Added a length parameter to many Pspell functions which should cut   down on unnecessary copying of words with some programs that use   Pspell. Unfortunately this also breaks sourcecode compatibility. But   it is easy to fix, see section 2.3.4 for what to do. * Added support for mapping the county code to a spelling for the   English language. For example the language tag ``en_GB'' will now get   the British dictionary if it is available. * Rewrote the ``Usage'' section of the manual to use the C interface   instead of the C++ one as I recommend using the C interface even in   C++ code to avoid some of the nasty issues associated with C++   linkage. * Libltdl is now enabled by default. * Switched to the multi-language branch of libtool. * Added support for iso-8859-13 and 14. And some of bugfixes.

---
+++ textproc/libpspell/buildlink.mk
@@ -0,0 +1,34 @@
+# $NetBSD: buildlink.mk,v 1.1 2002/01/15 01:05:14 wiz Exp $
+#
+# This Makefile fragment is included by packages that use libpspell.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.libpspell to the dependency pattern
+#     for the version of libpspell desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(LIBPSPELL_BUILDLINK_MK)
+LIBPSPELL_BUILDLINK_MK=	# defined
+
+.include "../../mk/bsd.buildlink.mk"
+
+BUILDLINK_DEPENDS.libpspell?=	libpspell>=0.12.2
+DEPENDS+=	${BUILDLINK_DEPENDS.libpspell}:../../textproc/libpspell
+
+EVAL_PREFIX+=			BUILDLINK_PREFIX.libpspell=libpspell
+BUILDLINK_PREFIX.libpspell_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.libpspell=		include/pspell/*
+BUILDLINK_FILES.libpspell+=		lib/libpspell*
+
+BUILDLINK_TARGETS.libpspell=	libpspell-buildlink
+BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.libpspell}
+
+pre-configure: ${BUILDLINK_TARGETS.libpspell}
+libpspell-buildlink: _BUILDLINK_USE
+
+.endif	# LIBPSPELL_BUILDLINK_MK
