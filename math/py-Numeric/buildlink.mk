52fb261baf281402386a7871f9c64d246c0e28a5
drochner <drochner@pkgsrc.org>
2002-01-16 20:01:52 +0000

    fix typo

--- math/py-Numeric/buildlink.mk
+++ math/py-Numeric/buildlink.mk
@@ -1,6 +1,6 @@
-# $NetBSD: buildlink.mk,v 1.2 2002/01/15 18:38:46 drochner Exp $
+# $NetBSD: buildlink.mk,v 1.3 2002/01/16 20:01:52 drochner Exp $
 
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_DEPENDS.pynumeric?= ${PYPKGPREFIX}-Numeric-*
-DEPENDS+= ${BUILDLINK_DEPENDS.pynumeric}:../math/py-Numeric
+DEPENDS+= ${BUILDLINK_DEPENDS.pynumeric}:../../math/py-Numeric
