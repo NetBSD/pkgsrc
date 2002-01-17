f284514b32f865fb78487429fa511df58b12b46e
drochner <drochner@pkgsrc.org>
2002-01-17 11:17:54 +0000

    add a rule to patch #!/the/right/path/to/python into the first line of scripts

--- lang/python/application.buildlink.mk
+++ lang/python/application.buildlink.mk
@@ -1,3 +1,15 @@
-# $NetBSD: application.buildlink.mk,v 1.1 2002/01/15 17:23:24 drochner Exp $
+# $NetBSD: application.buildlink.mk,v 1.2 2002/01/17 11:17:54 drochner Exp $
 
 .include "../../lang/python/pyversion.mk"
+
+# replace shell magic by python version specific path
+
+_PYTHON_PATCH_SCRIPT_SED=	-e "1s;^\#!.*python;\#!${PYTHONBIN};"
+
+.if defined(PYTHON_PATCH_SCRIPTS)
+post-build:
+.for s in ${PYTHON_PATCH_SCRIPTS}
+	${SED} ${_PYTHON_PATCH_SCRIPT_SED} <${WRKSRC}/$s >${WRKSRC}/$s.patched
+	${MV} ${WRKSRC}/$s.patched ${WRKSRC}/$s
+.endfor
+.endif
