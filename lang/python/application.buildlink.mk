a78f4e6701cd2a47c943868479cdd4e55ea2ee58
drochner <drochner@pkgsrc.org>
2002-01-18 14:16:19 +0000

    do the script patching pre-install rather than post-build, to make it work is NO_BUILD is set

--- lang/python/application.buildlink.mk
+++ lang/python/application.buildlink.mk
@@ -1,13 +1,15 @@
-# $NetBSD: application.buildlink.mk,v 1.3 2002/01/18 11:00:56 drochner Exp $
+# $NetBSD: application.buildlink.mk,v 1.4 2002/01/18 14:16:19 drochner Exp $
 
 .include "../../lang/python/pyversion.mk"
 
 # replace shell magic by python version specific path
+# PYTHON_PATCH_SCRIPTS is a list of Python scripts to be installed,
+#  relative to ${WRKSRC}
 
 _PYTHON_PATCH_SCRIPT_SED=	-e "1s;^\#!.*python;\#!${PYTHONBIN};"
 
 .if defined(PYTHON_PATCH_SCRIPTS)
-post-build:
+pre-install:
 .for s in ${PYTHON_PATCH_SCRIPTS}
 	${SED} ${_PYTHON_PATCH_SCRIPT_SED} <${WRKSRC}/$s >${WRKSRC}/$s.patched
 	${MV} -f ${WRKSRC}/$s.patched ${WRKSRC}/$s
