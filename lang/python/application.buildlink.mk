c4e7f6757c19a6da09ed831c44c52e1b0cf6ed85
drochner <drochner@pkgsrc.org>
2002-01-18 11:00:56 +0000

    use "-f" flag do "mv"

--- lang/python/application.buildlink.mk
+++ lang/python/application.buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: application.buildlink.mk,v 1.2 2002/01/17 11:17:54 drochner Exp $
+# $NetBSD: application.buildlink.mk,v 1.3 2002/01/18 11:00:56 drochner Exp $
 
 .include "../../lang/python/pyversion.mk"
 
@@ -10,6 +10,6 @@ _PYTHON_PATCH_SCRIPT_SED=	-e "1s;^\#!.*python;\#!${PYTHONBIN};"
 post-build:
 .for s in ${PYTHON_PATCH_SCRIPTS}
 	${SED} ${_PYTHON_PATCH_SCRIPT_SED} <${WRKSRC}/$s >${WRKSRC}/$s.patched
-	${MV} ${WRKSRC}/$s.patched ${WRKSRC}/$s
+	${MV} -f ${WRKSRC}/$s.patched ${WRKSRC}/$s
 .endfor
 .endif
