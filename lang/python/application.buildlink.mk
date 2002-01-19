2142e937dd4d4c00e4ea24701903805f04b5557d
drochner <drochner>
2002-01-19 13:39:58 +0000

    some changes for scripts which get run during build: -rearrange sed call so that an execute permission doesn't get lost -correct python path earlier (post-patch)

--- lang/python/application.buildlink.mk
+++ lang/python/application.buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: application.buildlink.mk,v 1.4 2002/01/18 14:16:19 drochner Exp $
+# $NetBSD: application.buildlink.mk,v 1.5 2002/01/19 13:39:58 drochner Exp $
 
 .include "../../lang/python/pyversion.mk"
 
@@ -9,9 +9,12 @@
 _PYTHON_PATCH_SCRIPT_SED=	-e "1s;^\#!.*python;\#!${PYTHONBIN};"
 
 .if defined(PYTHON_PATCH_SCRIPTS)
-pre-install:
+python-patch-scripts:
 .for s in ${PYTHON_PATCH_SCRIPTS}
-	${SED} ${_PYTHON_PATCH_SCRIPT_SED} <${WRKSRC}/$s >${WRKSRC}/$s.patched
-	${MV} -f ${WRKSRC}/$s.patched ${WRKSRC}/$s
+	${CP} ${WRKSRC}/$s ${WRKSRC}/$s.tmp
+	${CHMOD} +w ${WRKSRC}/$s
+	${SED} ${_PYTHON_PATCH_SCRIPT_SED} <${WRKSRC}/$s.tmp >${WRKSRC}/$s
 .endfor
+
+post-patch: python-patch-scripts
 .endif
