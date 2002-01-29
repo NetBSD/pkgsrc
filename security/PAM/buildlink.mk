8fc6698a3998d932d3b5b6ac333a4b63d3bc610d
jlam <jlam@pkgsrc.org>
2002-01-29 17:10:10 +0000

    Update security/PAM to 0.75.  Note that this release contains backwardly incompatible changes to libpam.so; prior versions were buggy so upgrading is highly recommended.
    Pkgsrc changes from version 0.72 include:
    
      * Honor ${PKG_SYSCONFDIR}: the config files are now found in /etc/pam.conf
        and /etc/pam/*.conf, or in the appropriate ${PKG_SYSCONFBASE} directory.
      * Convert to use the general INSTALL/DEINSTALL scripts.
    
    Changes from version 0.72 include:
    
      * bug fixes to almost every PAM module
      * pam_pwdb replaced with pam_unix
      * fixed a small security hole (more of a user confusion issue) with
        the unix and pwdb password helper binaries.
      * improved handling of the setcred/close_session and update chauthtok
        stack.  *Warning* This is a backwardly incompatible change, but 'more
        sane' than before. (Bug 129775 - agmorgan)
      * added support for '/' symbols in pam_time and pam_group config files
        (support for modern terminal devices).  Fixed infinite loop problem
        with '\\[^\n]' in these files.
      * added accessconf=<filename> feature to pam_access

--- security/PAM/buildlink.mk
+++ security/PAM/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.10 2001/07/27 13:33:34 jlam Exp $
+# $NetBSD: buildlink.mk,v 1.11 2002/01/29 17:10:10 jlam Exp $
 #
 # This Makefile fragment is included by packages that use libpam.
 #
@@ -17,11 +17,20 @@ PAM_BUILDLINK_MK=	# defined
 
 .include "../../mk/bsd.buildlink.mk"
 
-BUILDLINK_DEPENDS.pam?=	PAM>=0.72
-DEPENDS+=		${BUILDLINK_DEPENDS.pam}:../../security/PAM
+BUILDLINK_DEPENDS.pam?=	PAM>=0.75
 
+.if exists(/usr/include/security/pam_appl.h)
+_NEED_PAM=		NO
+.else
+_NEED_PAM=		YES
+.endif
+
+.if ${_NEED_PAM} == "YES"
+
+DEPENDS+=		${BUILDLINK_DEPENDS.pam}:../../security/PAM
 EVAL_PREFIX+=		BUILDLINK_PREFIX.pam=PAM
 BUILDLINK_PREFIX.pam_DEFAULT=	${LOCALBASE}
+
 BUILDLINK_FILES.pam=	include/security/*
 BUILDLINK_FILES.pam+=	lib/libpam.*
 BUILDLINK_FILES.pam+=	lib/libpamc.*
@@ -30,7 +39,26 @@ BUILDLINK_FILES.pam+=	lib/libpam_misc.*
 BUILDLINK_TARGETS.pam=	pam-buildlink
 BUILDLINK_TARGETS+=	${BUILDLINK_TARGETS.pam}
 
+.else
+
+# The PAM libraries are usually in /lib on Linux systems while the headers
+# are in /usr/include.
+#
+BUILDLINK_PREFIX.pam-inc=	/usr
+BUILDLINK_FILES.pam-inc=	include/security/*
+BUILDLINK_PREFIX.pam-lib=	/
+BUILDLINK_FILES.pam-lib+=	lib/libpam.*
+BUILDLINK_FILES.pam-lib+=	lib/libpamc.*
+BUILDLINK_FILES.pam-lib+=	lib/libpam_misc.*
+
+BUILDLINK_TARGETS.pam=		pam-inc-buildlink
+BUILDLINK_TARGETS.pam+=		pam-lib-buildlink
+
+.endif
+
 pre-configure: ${BUILDLINK_TARGETS.pam}
 pam-buildlink: _BUILDLINK_USE
+pam-inc-buildlink: _BUILDLINK_USE
+pam-lib-buildlink: _BUILDLINK_USE
 
 .endif	# PAM_BUILDLINK_MK
