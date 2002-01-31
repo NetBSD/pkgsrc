062a120f2c5eb16c65bd82310d8e5b2508220854
drochner <drochner@pkgsrc.org>
2002-01-31 15:48:10 +0000

    uhh - EVAL_PREFIX sucks. Do it right now and use buildlink files. Should fix a building problem reported by Kent Polk.

--- x11/py-Tk/files/setup.py
+++ x11/py-Tk/files/setup.py
@@ -1,15 +1,20 @@
-# $NetBSD: setup.py,v 1.1 2002/01/28 17:34:30 drochner Exp $
+# $NetBSD: setup.py,v 1.2 2002/01/31 15:48:11 drochner Exp $
 
 import distutils
 from distutils.core import setup, Extension
 
-tclprefix = '@TCLPREFIX@'
-tclincl = tclprefix + '/include'
-tcllib = tclprefix + '/lib'
-
+blprefix= '@BLPREFIX@'
+blincl = blprefix + '/include'
+bllib = blprefix + '/lib'
+tclrtprefix = '@TCLRTPREFIX@'
+tclrtlib = tclrtprefix + '/lib'
+tkrtprefix = '@TKRTPREFIX@'
+tkrtlib = tkrtprefix + '/lib'
 x11prefix = '@X11PREFIX@'
 x11incl = x11prefix + '/include'
 x11lib = x11prefix + '/lib'
+x11rtprefix = '@X11RTPREFIX@'
+x11rtlib = x11rtprefix + '/lib'
 
 setup(
 	ext_modules = [
@@ -17,9 +22,9 @@ setup(
 			'_tkinter',
 			['Modules/_tkinter.c', 'Modules/tkappinit.c'],
 			define_macros=[('WITH_APPINIT', None)],
-			include_dirs=[tclincl, x11incl],
-			library_dirs=[tcllib, x11lib],
-			runtime_library_dirs=[tcllib, x11lib],
+			include_dirs=[blincl, x11incl],
+			library_dirs=[bllib, x11lib],
+			runtime_library_dirs=[tclrtlib, tkrtlib, x11rtlib],
 			libraries=['tk83', 'tcl83', 'X11']
 		)
 	]
