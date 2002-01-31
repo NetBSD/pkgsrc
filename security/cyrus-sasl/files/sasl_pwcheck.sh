7fad60566f57cd0ee20640074796a51fb68d5466
jlam <jlam@pkgsrc.org>
2002-01-31 21:41:18 +0000

    Remove the dbinit command as it's not needed by the pwcheck daemon. pwcheck only checks against the /etc/passwd database.  Users that need CRAM-MD5 or SCRAM-MD5 authentication can initialize the sasldb and add themselves in the process by running saslpasswd.

--- security/cyrus-sasl/files/sasl_pwcheck.sh
+++ security/cyrus-sasl/files/sasl_pwcheck.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 #
-# $NetBSD: sasl_pwcheck.sh,v 1.5 2002/01/31 19:55:07 jlam Exp $
+# $NetBSD: sasl_pwcheck.sh,v 1.6 2002/01/31 21:41:18 jlam Exp $
 #
 # The pwcheck daemon allows UNIX password authentication with Cyrus SASL.
 #
@@ -12,44 +12,10 @@ then
 	. /etc/rc.subr
 fi
 
-rcd_dir=`@DIRNAME@ $0`
-
 name="sasl_pwcheck"
 rcvar="${name}"
 command="@PREFIX@/sbin/pwcheck"
 command_args="& sleep 2"
-extra_commands="dbinit"
-
-sasldb=@PKG_SYSCONFDIR@/sasldb.db
-
-sasl_pwcheck_dbinit()
-{
-	(
-	saslpasswd=@PREFIX@/sbin/saslpasswd
-	umask 002
-	if [ -e ${sasldb} ]
-	then
-		@ECHO@ "You already have an existing SASL password database"
-		@ECHO@ "Skipping empty database generation"
-	else
-		@ECHO@ password | ${saslpasswd} -p user
-		${saslpasswd} -d user
-		@CHOWN@ @CYRUS_USER@ ${sasldb}
-		@CHMOD@ 0600 ${sasldb}
-	fi
-	)
-}
-
-sasl_pwcheck_precmd()
-{
-	if [ ! -e ${sasldb} ]
-	then
-		$rcd_dir/sasl_pwcheck dbinit
-	fi
-}
-
-dbinit_cmd=sasl_pwcheck_dbinit
-start_precmd=sasl_pwcheck_precmd
 
 if [ -e /etc/rc.subr ]
 then
@@ -57,6 +23,5 @@ then
 	run_rc_command "$1"
 else
 	@ECHO@ -n " ${name}"
-	start_precmd
 	${command} ${sasl_pwcheck_flags} ${command_args}
 fi
