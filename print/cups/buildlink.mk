2b3ad9bcb29de88c78990c3c74b60d2ea30ce0b3
jlam <jlam@pkgsrc.org>
2002-01-31 17:51:26 +0000

    Update print/cups to 1.1.13:
    CUPS 1.1.13 adds support for the KOI8-R and KOI8-U encodings, message
    catalogs for several Russian locales and for Simplified Chinese,
    improvements for MacOS X (Darwin) and IRIX, improvements to the PDF,
    PostScript, and text filters, status reporting for IPP-based printers
    (paper out, etc.), and improvements to the SAMBA driver export facility.
    The new release also fixes bugs in the scheduler, the lpstat command, the
    CUPS API, and the pstoraster filter.

--- print/cups/buildlink.mk
+++ print/cups/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.10 2001/07/27 13:33:33 jlam Exp $
+# $NetBSD: buildlink.mk,v 1.11 2002/01/31 17:51:27 jlam Exp $
 #
 # This Makefile fragment is included by packages that use libcups.
 #
@@ -26,10 +26,23 @@ BUILDLINK_FILES.cups=	include/cups/*
 BUILDLINK_FILES.cups+=	lib/libcups.*
 BUILDLINK_FILES.cups+=	lib/libcupsimage.*
 
-BUILDLINK_TARGETS.cups=	cups-buildlink
-BUILDLINK_TARGETS+=	${BUILDLINK_TARGETS.cups}
+BUILDLINK_TARGETS.cups=		cups-buildlink
+BUILDLINK_TARGETS.cups+=	cups-buildlink-config-wrapper
+BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.cups}
+
+BUILDLINK_CONFIG.cups=		${BUILDLINK_PREFIX.cups}/bin/cups-config
+BUILDLINK_CONFIG_WRAPPER.cups=	${BUILDLINK_DIR}/bin/cups-config
+REPLACE_BUILDLINK_SED+=	\
+	-e "s|${BUILDLINK_CONFIG_WRAPPER.cups}|${BUILDLINK_CONFIG.cups}|g"
+
+.if defined(USE_CONFIG_WRAPPER)
+CUPS_CONFIG?=		${BUILDLINK_CONFIG_WRAPPER.cups}
+CONFIGURE_ENV+=		CUPS_CONFIG="${CUPS_CONFIG}"
+MAKE_ENV+=		CUPS_CONFIG="${CUPS_CONFIG}"
+.endif
 
 pre-configure: ${BUILDLINK_TARGETS.cups}
 cups-buildlink: _BUILDLINK_USE
+cups-buildlink-config-wrapper: _BUILDLINK_CONFIG_WRAPPER_USE
 
 .endif	# CUPS_BUILDLINK_MK
