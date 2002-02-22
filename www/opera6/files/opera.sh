1f9657cc1f02e29c4358f1779b5cb2ab4d992b89
tron <tron@pkgsrc.org>
2002-02-22 22:22:21 +0000

    Import Opera 6.0 for Linux Technology Preview 3 as "opera6": Small, fast and customizable WWW client

---
+++ www/opera6/files/opera.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+OPERADIR=$HOME/.opera
+
+mkdir -p $OPERADIR || exit 1
+for FILE in $OPERADIR/opera.ini $OPERADIR/opera5.ini
+do
+ if [ -e ${FILE} ]
+ then
+  if grep -q '^Synchronous DNS Lookup=0$' ${FILE}
+  then
+   cp -p ${FILE} ${FILE}.patch
+   sed -e 's#^\(Synchronous DNS Lookup\)=0$#\1=1#' ${FILE}.patch >${FILE}
+   rm -f ${FILE}.patch
+  fi
+ else
+  echo >${FILE} "Synchronous DNS Lookup=1"
+ fi
+done
+
+exec @EMULDIR@/bin/bash /usr/bin/opera "$@"
