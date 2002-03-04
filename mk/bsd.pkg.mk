f354afd4b8f2161c8744c2688b57346d0a1da5e2
jmc <jmc@pkgsrc.org>
2002-03-04 17:21:46 +0000

    Redirect errors from grep in fake-pkg to /dev/null so complaints about not being able to grep on a dir don't fly by and confuse people.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.939 2002/03/04 11:56:54 agc Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.940 2002/03/04 17:21:46 jmc Exp $
 #
 # This file is in the public domain.
 #
@@ -3645,7 +3645,7 @@ fake-pkg: ${PLIST} ${DESCR} ${MESSAGE}
 	else								\
 		files="";						\
 	fi;								\
-	${GREP} '\$$NetBSD' $$files ${PKGDIR}/* | ${SED} -e 's|^${_PKGSRCDIR}/||' > ${BUILD_VERSION_FILE}; \
+	${GREP} '\$$NetBSD' $$files ${PKGDIR}/* 2> /dev/null | ${SED} -e 's|^${_PKGSRCDIR}/||' > ${BUILD_VERSION_FILE}; \
 	files="";							\
 	if [ -f ${DISTINFO_FILE} ]; then				\
 		for f in `${AWK} 'NF == 4 && $$3 == "=" { gsub("[()]", "", $$2); print $$2 }' < ${DISTINFO_FILE}`; do \
