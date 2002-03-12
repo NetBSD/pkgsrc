cd270deb80f94f68818ab476e7be60bfe5c0dee2
abs <abs@pkgsrc.org>
2002-03-12 15:01:34 +0000

    quote $@ correctly

--- games/civctp-demo/files/wrapper.sh
+++ games/civctp-demo/files/wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-# $NetBSD: wrapper.sh,v 1.1.1.1 2000/10/16 12:24:02 abs Exp $
+# $NetBSD: wrapper.sh,v 1.2 2002/03/12 15:01:34 abs Exp $
 # Simple wrapper for lokigames @@BINNAME@@
 
 # linux binary calls mkdir for this with a trailing '/'
@@ -7,4 +7,4 @@ if [ ! -d $HOME/.loki ];then
     mkdir $HOME/.loki
 fi
 cd @@LIBDIR@@
-exec ./@@BINNAME@@ $@
+exec ./@@BINNAME@@ "$@"
