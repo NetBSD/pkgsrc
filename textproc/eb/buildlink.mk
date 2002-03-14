a1124ca0b8cb2b158a50ea8d7219d7fb95da5228
uebayasi <uebayasi>
2002-03-14 13:54:04 +0000

    Update EB from 3.0 to 3.2.1.
    	* Fix a bug in text read routine when using 'appendix'.
    	* Import zlib-1.1.4.
    	* If the EB_DEBUG environment variable is defined, EB Library outputs
    	  messages for debugging.
    	  `--enable-debug' option of configure is removed.
    	* Fix a critical bug in decompression of EBZIP compression format.
    	* The maximum compression level of ebzip is changed from 3 to 5.
    	* Implement eb_backward_text() and revise eb_forward_text().
    	* The EB Library programming document (Japanese edition) is back.
    	* Fix bugs in compressed S-EBXA support.
    	* Add debug codes.
    	  Those codes are enabled by `configure --enable-debug'.
    	* Rename eb_initialize_all_subbooks() to
    	  eb_load_all_subbooks(), and
    	  eb_initialize_all_appendix_subbooks() to
    	  eb_load_all_appendix_subbooks().  For backward
    	  compatibility, the old function names are defined as cpp
    	  macros.
    	* Support for DUDEN EBG CD-ROMs is back.
    	* Fix a bug in handling of CD-ROM mounted on the root directory.
    	* Overhaul initialization and finalization routines in the library.
    	* Support compressed S-EBXA CD-ROM book.
    	  Support Nippon daihyakka zensho, accessories to Sony
    	  DataDiscMan DP-S1000.
    	* New appendices (crownfj2, crowngj).
    	* And many bug fixes.
    
    Besides, add a brand new buildlink.mk.

---
+++ textproc/eb/buildlink.mk
@@ -0,0 +1,36 @@
+# $NetBSD: buildlink.mk,v 1.1 2002/03/14 13:54:04 uebayasi Exp $
+#
+# This Makefile fragment is included by packages that use EB.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.eb to the dependency pattern
+#     for the version of eb desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(EB_BUILDLINK_MK)
+EB_BUILDLINK_MK=	# defined
+
+.include "../../mk/bsd.buildlink.mk"
+
+# 3.2.0 had an 'appendix' bug, so bump the base version.
+BUILDLINK_DEPENDS.eb?=	eb>=3.2.1
+
+EVAL_PREFIX+=		BUILDLINK_PREFIX.eb=eb
+#BUILDLINK_PREFIX.png_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.eb=	include/eb/*.h
+BUILDLINK_FILES.eb+=	lib/libeb.*
+
+BUILDLINK_TARGETS.eb=	eb-buildlink
+BUILDLINK_TARGETS+=	${BUILDLINK_TARGETS.eb}
+
+.include "../../mk/bsd.prefs.mk"
+
+pre-configure: ${BUILDLINK_TARGETS.eb}
+eb-buildlink: _BUILDLINK_USE
+
+.endif	# EB_BUILDLINK_MK
