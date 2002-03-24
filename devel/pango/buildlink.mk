d1b1ac3968521f81529a455938e0e76719be201e
dmcmahill <dmcmahill@pkgsrc.org>
2002-03-24 20:29:35 +0000

    import pango-1.0.0
    Pango is a library for layout and rendering of text, with an emphasis
    on internationalization. Pango can be used anywhere that text layout
    is needed; however, most of the work on Pango-1.0 was done using the
    GTK+ widget toolkit as a test platform. Pango forms the core of text
    and font handling for GTK+-2.0.
    
    Pango is designed to be modular; the core Pango layout can be used
    with four different font backends:
    
     - Core X windowing system fonts
     - Client-side fonts on X using the Xft library
     - Direct rendering of scalable fonts using the FreeType library
     - Native fonts on Microsoft backends
    
    Dynamically loaded modules then handle text layout for particular
    combinations of script and font backend.
    
    As well as the low level layout rendering routines, Pango includes
    PangoLayout, a high level driver for laying out entire blocks of text,
    and routines to assist in editing internationalized text.

---
+++ devel/pango/buildlink.mk
@@ -0,0 +1,37 @@
+# $NetBSD: buildlink.mk,v 1.1.1.1 2002/03/24 20:29:35 dmcmahill Exp $
+#
+# This Makefile fragment is included by packages that use pango.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.pango to the dependency pattern
+#     for the version of pango desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(PANGO_BUILDLINK_MK)
+PANGO_BUILDLINK_MK=	# defined
+
+.include "../../mk/bsd.buildlink.mk"
+
+BUILDLINK_DEPENDS.pango?=	pango>=1.0.0
+DEPENDS+=	${BUILDLINK_DEPENDS.pango}:../../devel/pango
+
+EVAL_PREFIX+=		BUILDLINK_PREFIX.pango=pango
+BUILDLINK_PREFIX.pango_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.pango=	include/pango-1.0/*/*
+BUILDLINK_FILES.pango+=	include/pango-1.0/*
+BUILDLINK_FILES.pango+=	lib/libpango*-1.0.*
+
+.include "../../devel/pkgconfig/buildlink.mk"
+
+BUILDLINK_TARGETS.pango=	pango-buildlink
+BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.pango}
+
+pre-configure: ${BUILDLINK_TARGETS.pango}
+pango-buildlink: _BUILDLINK_USE
+
+.endif	# PANGO_BUILDLINK_MK
