e6fc08903f4dc47aef26ea0cfdf791cfcdf12690
dmcmahill <dmcmahill@pkgsrc.org>
2002-03-24 20:28:05 +0000

    import atk-1.0.0
    The ATK library provides a set of interfaces for accessibility.
    By supporting the ATK interfaces, an application or toolkit can
    be used with such tools as screen readers, magnifiers,
    and alternative input devices.
    
    Atk provides a core set of interfaces which are common to all
    widgets and "additional" interfaces that are appropriate to certain
    classes of widgets and whose existence can be queried at run time.
    It also provides interfaces which an application can use to provide
    additional accessibility information to assistive technology tools.

---
+++ devel/atk/buildlink.mk
@@ -0,0 +1,37 @@
+# $NetBSD: buildlink.mk,v 1.1.1.1 2002/03/24 20:28:05 dmcmahill Exp $
+#
+# This Makefile fragment is included by packages that use atk.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.atk to the dependency pattern
+#     for the version of atk desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(ATK_BUILDLINK_MK)
+ATK_BUILDLINK_MK=	# defined
+
+.include "../../mk/bsd.buildlink.mk"
+
+BUILDLINK_DEPENDS.atk?=	atk>=1.0.0
+DEPENDS+=	${BUILDLINK_DEPENDS.atk}:../../devel/atk
+
+EVAL_PREFIX+=		BUILDLINK_PREFIX.atk=atk
+BUILDLINK_PREFIX.atk_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.atk=	include/atk-1.0/*/*
+BUILDLINK_FILES.atk+=	include/atk-1.0/*
+BUILDLINK_FILES.atk+=	lib/libatk-1.0.*
+
+.include "../../devel/pkgconfig/buildlink.mk"
+
+BUILDLINK_TARGETS.atk=		atk-buildlink
+BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.atk}
+
+pre-configure: ${BUILDLINK_TARGETS.atk}
+atk-buildlink: _BUILDLINK_USE
+
+.endif	# ATK_BUILDLINK_MK
