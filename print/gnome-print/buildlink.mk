900343a9dde26bc726c7d1632370b17a136789e0
rh <rh@pkgsrc.org>
2002-03-26 09:20:44 +0000

    Update gnome-print to 0.35.  This is mainly a bug-fix release, but also includes the following changes:
    - Print preview clips now all graphics, including text and bitmaps -
      it makes gnumeric and sodipodi previews much nicer
    
    - Fontmaps can contain per-locale default entries now, so unknown
      font names do not fall uniformly back to Helvetica any more
    
    - Added --dir flag to gnome-font-install (Tambet)

--- print/gnome-print/buildlink.mk
+++ print/gnome-print/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.4 2002/03/13 17:37:35 fredb Exp $
+# $NetBSD: buildlink.mk,v 1.5 2002/03/26 09:20:44 rh Exp $
 #
 # This Makefile fragment is included by packages that use gnome-print.
 #
@@ -22,7 +22,7 @@ DEPENDS+=	${BUILDLINK_DEPENDS.gnome-print}:../../print/gnome-print
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.gnome-print=gnome-print
 BUILDLINK_PREFIX.gnome-print_DEFAULT=	${X11PREFIX}
-BUILDLINK_FILES.gnome-print+=	include/libgnomeprint/*
+BUILDLINK_FILES.gnome-print+=	include/gnome-1.0/libgnomeprint/*
 BUILDLINK_FILES.gnome-print+=	lib/libgnomeprint.*
 
 .include "../../graphics/gdk-pixbuf-gnome/buildlink.mk"
@@ -35,7 +35,10 @@ BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.gnome-print}
 BUILDLINK_CONFIG.gnome-print=		${BUILDLINK_PREFIX.gnome-print}/lib/printConf.sh
 BUILDLINK_CONFIG_WRAPPER.gnome-print=	${BUILDLINK_DIR}/lib/printConf.sh
 REPLACE_BUILDLINK_SED+=	\
-	-e "s|${BUILDLINK_CONFIG_WRAPPER.gnome-print}|${BUILDLINK_CONFIG.gnome-print}|g"
+	-e "s|${BUILDLINK_CONFIG_WRAPPER.gnome-print}|${BUILDLINK_CONFIG.gnome-print}|g"	\
+	-e "s|-I${BUILDLINK_DIR}/\(include/gnome-1.0\)|-I${BUILDLINK_PREFIX.gnome-print}/\1|g"
+BUILDLINK_CONFIG_WRAPPER_SED+=	\
+	-e "s|-I${BUILDLINK_PREFIX.gnome-print}/\(include/gnome-1.0\)|-I${BUILDLINK_DIR}/\1|g"
 
 .if defined(USE_CONFIG_WRAPPER)
 GNOME_PRINT_CONFIG?=	${BUILDLINK_CONFIG_WRAPPER.gnome-print}
