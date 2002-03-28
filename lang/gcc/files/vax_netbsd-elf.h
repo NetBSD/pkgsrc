eea3b9a2bc72341e9b76f5f7d9fda289537be12e
jmc <jmc>
2002-03-28 10:11:50 +0000

    Update patch set against a sync of source from gnusrc/dist/toolchain.
    Not very many overall changes. Main ones include
    
    1. Support for powerpc, arm32 and vax
    2. Makefile.gcc can now be included by anything which depends on gcc versions.
       If the version installed isn't 2.95.3 it'll add itself as a BUILD_DEPENDS.
       (XXX: any of the makefile's in pkgsrc should be checked and change to use
        this)
    
    3. Remove special PLIST.NetBSD-sparc as it's no longer needed
    4. Change post-extract loop to pick up any arch files from FILESDIR without
       having to hardcode all the archs
    5. Remove arch restrictions as this should work on any arch supported by the
       main source tree as of 03/28/02
    6. Add PKGREVISION as this clearly isn't stock 2.95.3 (it doesn't change
       gcc --version so version checks won't care).

--- lang/gcc/files/vax_netbsd-elf.h
+++ lang/gcc/files/vax_netbsd-elf.h
@@ -27,6 +27,13 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #define NETBSD_ELF
 #include "vax/netbsd.h"
 
+#undef REGISTER_PREFIX
+#define REGISTER_PREFIX "%"
+
+/* Redefine this with register prefixes.  */
+#undef VAX_ISTREAM_SYNC
+#define	VAX_ISTREAM_SYNC	"movpsl -(%sp)\n\tpushal 1(%pc)\n\trei"
+
 #undef SIZE_TYPE
 #define SIZE_TYPE "long unsigned int"
 
@@ -41,6 +48,8 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #define	TARGET_DEFAULT	0
 
 #define	TARGET_MEM_FUNCTIONS		/* include mem* calls */
+
+#undef	PCC_STATIC_STRUCT_RETURN	/* let's be reentrant */
 
 #if 1
 #undef  PREFERRED_DEBUGGING_TYPE
@@ -61,7 +70,6 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 /* Profiling routines */
 
-/* Redefine this to use %eax instead of %edx.  */
 #undef  FUNCTION_PROFILER
 #define FUNCTION_PROFILER(FILE, LABELNO)  \
   fprintf (FILE, "\tmovab .LP%d,r0\n\tjsb __mcount+2\n", (LABELNO))
