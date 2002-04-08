da76d1658436ac84fea1838d3697dcb8626866ed
wiz <wiz@pkgsrc.org>
2002-04-08 13:57:12 +0000

    Recognize ${INSTALL_INFO} in PLISTs as install-info. Bump to 3.25.

--- pkgtools/pkglint/files/pkglint.pl
+++ pkgtools/pkglint/files/pkglint.pl
@@ -12,7 +12,7 @@
 # Freely redistributable.  Absolutely no warranty.
 #
 # From Id: portlint.pl,v 1.64 1998/02/28 02:34:05 itojun Exp
-# $NetBSD: pkglint.pl,v 1.65 2002/02/27 11:20:59 abs Exp $
+# $NetBSD: pkglint.pl,v 1.66 2002/04/08 13:57:14 wiz Exp $
 #
 # This version contains lots of changes necessary for NetBSD packages
 # done by Hubert Feyrer <hubertf@netbsd.org>,
@@ -359,7 +359,11 @@ sub checkplist {
 					"instead of \"\@unexec rmdir\".");
 			} elsif ($_ =~ /^\@exec[ \t]+(.*\/)?install-info/) {
 				$infoinstallseen = $.
+			} elsif ($_ =~ /^\@exec[ \t]+(.*\/)?\$\{INSTALL_INFO\}/) {
+				$infoinstallseen = $.
 			} elsif ($_ =~ /^\@unexec[ \t]+(.*\/)?install-info[ \t]+--delete/) {
+				$inforemoveseen = $.
+			} elsif ($_ =~ /^\@unexec[ \t]+(.*\/)?\$\{INSTALL_INFO\}[ \t]+--delete/) {
 				$inforemoveseen = $.
 			} elsif ($_ =~ /^\@(exec|unexec)/) {
 				if (/ldconfig/
