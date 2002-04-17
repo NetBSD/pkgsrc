b1a6611a47543b150528d8a4987d06d80dbda961
tsarna <tsarna@pkgsrc.org>
2002-04-17 20:47:20 +0000

    Plex, a very nice lexical analysis module for Python

---
+++ devel/py-plex/files/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="Plex",
+    version="1.1.2",
+    description="A very nice lexical analysis module for Python",
+    
+    author="Greg Ewing",
+    author_email="greg@cosc.canterbury.ac.nz",
+    
+    url="http://www.cosc.canterbury.ac.nz/~greg/python/Pyrex/",
+    
+    packages = ['Plex'],
+)
