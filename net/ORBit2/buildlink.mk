c9cc2849d61486e86f55666bc5919ebd68f43b72
rh <rh>
2002-04-27 07:55:59 +0000

    Add orbit2-buildlink to BUILDLINK_TARGETS

--- net/ORBit2/buildlink.mk
+++ net/ORBit2/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.1.1.1 2002/04/23 12:24:48 rh Exp $
+# $NetBSD: buildlink.mk,v 1.2 2002/04/27 07:55:59 rh Exp $
 #
 # This Makefile fragment is included by packages that use orbit2.
 #
@@ -22,7 +22,8 @@ DEPENDS+=	${BUILDLINK_DEPENDS.orbit2}:../../net/ORBit2
 
 EVAL_PREFIX+=		BUILDLINK_PREFIX.orbit2=orbit2
 BUILDLINK_PREFIX.orbit2_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.orbit2=	include/orbit-2.0/orbit/*
+BUILDLINK_FILES.orbit2=	include/orbit-2.0/orbit/*/*
+BUILDLINK_FILES.orbit2+=include/orbit-2.0/orbit/*
 BUILDLINK_FILES.orbit2+=include/orbit-2.0/ORBitservices/*
 BUILDLINK_FILES.orbit2+=lib/libORBit-2.*
 BUILDLINK_FILES.orbit2+=lib/libORBitCosNaming-2.*
@@ -34,11 +35,7 @@ BUILDLINK_FILES.orbit2+=lib/orbit-2.0/*
 .include "../../net/libIDL/buildlink.mk"
 .include "../../net/linc/buildlink.mk"
 
-BUILDLINK_FILES.orbit2+=	lib/libORBit-2.*
-BUILDLINK_FILES.orbit2+=	lib/libORBitCosNaming-2.*
-BUILDLINK_FILES.orbit2+=	lib/libname-server-2.*
-BUILDLINK_FILES.orbit2+=	lib/orbit-2.0/*
-
+BUILDLINK_TARGETS.orbit2=	orbit2-buildlink
 BUILDLINK_TARGETS.orbit2+=	orbit2-buildlink-config-wrapper
 BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.orbit2}
 BUILDLINK_TARGETS+=		${BUILDLINK_PKG_CONFIG}
@@ -54,7 +51,7 @@ CONFIGURE_ENV+=	ORBIT2_CONFIG="${ORBIT2_CONFIG}"
 MAKE_ENV+=	ORBIT2_CONFIG="${ORBIT2_CONFIG}"
 .endif
 
-pre-configure: ${BUILDLINK_TARGETS}
+pre-configure: ${BUILDLINK_TARGETS.orbit2}
 orbit2-buildlink: _BUILDLINK_USE
 orbit2-buildlink-config-wrapper: _BUILDLINK_CONFIG_WRAPPER_USE
 
