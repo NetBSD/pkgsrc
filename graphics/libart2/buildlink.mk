d1f58a492bf3565c2394ca8002274759cfce822c
agc <agc@pkgsrc.org>
2002-05-22 21:30:31 +0000

    Initial import of libart2-2.3.8, a high-performance 2D graphics library, into the NetBSD packages collection.
    Provided in PR 16932 by jmmv@hispabsd.org (Julio Merino).
    
    Libart is a library for high-performance 2D graphics. It is currently
    being used as the antialiased rendering engine for the Gnome Canvas.
    It is also the rendering engine for Gill, the Gnome Illustration app.
    
    Libart supports a very powerful imaging model, basically the same as
    SVG and the Java 2D API. It includes all PostScript imaging
    operations, and adds antialiasing and alpha-transparency.
    
    Libart is also highly tuned for incremental rendering. It contains
    data structures and algorithms suited to rapid, precise computation
    of Region of Interest, as well as a two-phase rendering pipeline
    optimized for interactive display.

---
+++ graphics/libart2/buildlink.mk
@@ -0,0 +1,36 @@
+# $NetBSD: buildlink.mk,v 1.1.1.1 2002/05/22 21:30:31 agc Exp $
+#
+# This Makefile fragment is included by packages that use the library
+# provided by libart2.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Optionally define BUILDLINK_DEPENDS.libart2 to the dependency
+#     pattern for the version of libart2 desired.
+# (2) Include this Makefile fragment in the package Makefile,
+# (3) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (4) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
+
+.if !defined(LIBART2_BUILDLINK_MK)
+LIBART2_BUILDLINK_MK=	# defined
+
+.include "../../mk/bsd.buildlink.mk"
+
+BUILDLINK_DEPENDS.libart2?=	libart2>=2.3.8
+DEPENDS+=	${BUILDLINK_DEPENDS.libart2}:../../graphics/libart2
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.libart2=libart2
+BUILDLINK_PREFIX.libart2=	${LOCALBASE}
+BUILDLINK_FILES.libart2=	bin/libart22-config
+BUILDLINK_FILES.libart2+=	include/libart2-2.0/libart2_lgpl/*
+BUILDLINK_FILES.libart2+=	lib/libart2_lgpl_2.*
+
+BUILDLINK_TARGETS.libart2=	libart2-buildlink
+BUILDLINK_TARGETS+=		${BUILDLINK_TARGETS.libart2}
+
+pre-configure: ${BUILDLINK_TARGETS.libart2}
+libart2-buildlink: _BUILDLINK_USE
+
+.endif	# LIBART2_BUILDLINK_MK
