c6bb94a065ac2ceed0fd857a683ee17be8b48d9d
schmonz <schmonz@pkgsrc.org>
2002-06-10 13:28:23 +0000

    If ${_USE_RPATH} == "no", strip any rpaths that may have managed to sneak into LDFLAGS. From jlam, tested a bit by billc, approved by agc.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.988 2002/06/10 11:40:53 skrll Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.989 2002/06/10 13:28:23 schmonz Exp $
 #
 # This file is in the public domain.
 #
@@ -355,7 +355,10 @@ LDFLAGS+=		-L${X11BASE}/lib
 .endif
 .if ${_USE_RPATH} == "yes"
 LDFLAGS+=		-Wl,-R${LOCALBASE}/lib
+.else
+LDFLAGS:=		${LDFLAGS:N*-Wl,-R*:N*-rpath*}
 .endif
+
 .if !defined(USE_BUILDLINK_ONLY)
 LDFLAGS+=		-L${LOCALBASE}/lib
 .endif
