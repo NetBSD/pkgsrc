3ecceb56b93d2cf5d8010957d6c6b4ee6af9d3a6
jlam <jlam@pkgsrc.org>
2002-06-21 21:43:20 +0000

    * Split the libtool script into a main script and a helper script.  The   helper script is separated to prevent parsing until its needed.  Also   specially handle the --mode=install case for libtool as we don't really   care to cache the arguments in those cases.
    * Use a more correct strategy for properly quoting and caching arguments
      in the wrapper scripts.  This fixes building php4, kdelibs2, and imlib.

--- mk/buildlink2/wrapper.sh
+++ mk/buildlink2/wrapper.sh
@@ -1,34 +1,28 @@
 #!/bin/sh
+#
+# $NetBSD: wrapper.sh,v 1.1.2.2 2002/06/21 21:43:20 jlam Exp $
+
 Xsed='@SED@ -e 1s/^X//'
 sed_quote_subst='s/\([\\`\\"$\\\\]\)/\\\1/g'
-sed_whitespace_subst='s/\([ 	]\)/\\\1/g'
-precache="@_BLNK_WRAP_PRE_CACHE@"
+
+pre_cache="@_BLNK_WRAP_PRE_CACHE@"
 cache="@_BLNK_WRAP_CACHE@"
-postcache="@_BLNK_WRAP_POST_CACHE@"
+post_cache="@_BLNK_WRAP_POST_CACHE@"
 logic="@_BLNK_WRAP_LOGIC@"
 wrapperlog="@BUILDLINK_DIR@/.wrapper.log"
-for arg
-do
+
+cmd="@WRAPPEE@"
+for arg; do
 	cacheupdated=
-	case "$arg" in
-	-[IL]*@BUILDLINK_DIR@*|-[IL]*@BUILDLINK_X11_DIR@*|-[IL]*@_BLNK_X11PKG_DIR@*)
-		args="$args $arg"
-		;;
-	-[IL]*|-[Dl]*|@LOCALBASE@/*/lib*|@X11BASE@/*/lib*)
-		. $logic
-		case "$cacheupdated" in
-		yes) @CAT@ $precache $cache $postcache > $logic ;;
-		esac
-		;;
-	# Double-quote args containing other shell metacharacters.
-	*[\[\~\#\^\&\*\(\)\{\}\|\;\<\>\?\'\ \	]*|*]*|"")
-		args="$args \"$arg\""
-		;;
-	*)
-		args="$args $arg"
-		;;
+	. $logic
+	case "$cacheupdated" in
+	yes) @CAT@ $pre_cache $cache $post_cache > $logic ;;
 	esac
+	args="$args $arg"
 done
+cmd="$cmd $args"
+
 @_BLNK_WRAP_SANITIZE_PATH@
-eval @ECHO@ @WRAPPEE@ "$args" >> $wrapperlog
-eval exec @WRAPPEE@ "$args"
+
+@ECHO@ $cmd >> $wrapperlog
+eval exec $cmd
