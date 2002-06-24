61d0c7d98e9543b9e408b4f855c515fc159677b1
jlam <jlam@pkgsrc.org>
2002-06-24 15:48:33 +0000

    Update to match more recent buildlink.mk.

--- graphics/gdk-pixbuf/buildlink2.mk
+++ graphics/gdk-pixbuf/buildlink2.mk
@@ -1,17 +1,36 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.3 2002/06/21 23:00:30 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.1.2.4 2002/06/24 15:48:33 jlam Exp $
 
 .if !defined(GDK_PIXBUF_BUILDLINK2_MK)
 GDK_PIXBUF_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gdk-pixbuf
-BUILDLINK_DEPENDS.gdk-pixbuf?=		gdk-pixbuf>=0.11.0nb1
+BUILDLINK_DEPENDS.gdk-pixbuf?=		gdk-pixbuf>=0.17.0
 BUILDLINK_PKGSRCDIR.gdk-pixbuf?=	../../graphics/gdk-pixbuf
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gdk-pixbuf=gdk-pixbuf
 BUILDLINK_PREFIX.gdk-pixbuf_DEFAULT=	${X11PREFIX}
-BUILDLINK_FILES.gdk-pixbuf=	include/gdk-pixbuf/*
+BUILDLINK_FILES.gdk-pixbuf=	include/gdk-pixbuf-1.0/gdk-pixbuf/gdk-pixbuf-features.h
+BUILDLINK_FILES.gdk-pixbuf+=	include/gdk-pixbuf-1.0/gdk-pixbuf/gdk-pixbuf-loader.h
+BUILDLINK_FILES.gdk-pixbuf+=    include/gdk-pixbuf-1.0/gdk-pixbuf/gdk-pixbuf-xlib.h
+BUILDLINK_FILES.gdk-pixbuf+=	include/gdk-pixbuf-1.0/gdk-pixbuf/gdk-pixbuf-xlibrgb.h
+BUILDLINK_FILES.gdk-pixbuf+=	include/gdk-pixbuf-1.0/gdk-pixbuf/gdk-pixbuf.h
+BUILDLINK_FILES.gdk-pixbuf+=	lib/gdk-pixbuf/loaders/libpixbufloader-bmp.*
+BUILDLINK_FILES.gdk-pixbuf+=	lib/gdk-pixbuf/loaders/libpixbufloader-gif.*
+BUILDLINK_FILES.gdk-pixbuf+=	lib/gdk-pixbuf/loaders/libpixbufloader-ico.*
+BUILDLINK_FILES.gdk-pixbuf+=	lib/gdk-pixbuf/loaders/libpixbufloader-jpeg.*
+BUILDLINK_FILES.gdk-pixbuf+=	lib/gdk-pixbuf/loaders/libpixbufloader-png.*
+BUILDLINK_FILES.gdk-pixbuf+=	lib/gdk-pixbuf/loaders/libpixbufloader-pnm.*
+BUILDLINK_FILES.gdk-pixbuf+=	lib/gdk-pixbuf/loaders/libpixbufloader-ras.*
+BUILDLINK_FILES.gdk-pixbuf+=	lib/gdk-pixbuf/loaders/libpixbufloader-tiff.*
+BUILDLINK_FILES.gdk-pixbuf+=	lib/gdk-pixbuf/loaders/libpixbufloader-xbm.*
+BUILDLINK_FILES.gdk-pixbuf+=	lib/gdk-pixbuf/loaders/libpixbufloader-xpm.*
+BUILDLINK_FILES.gdk-pixbuf+=	lib/gdk_pixbufConf.sh
+BUILDLINK_FILES.gdk-pixbuf+=	lib/gdk_pixbuf_xlibConf.sh
 BUILDLINK_FILES.gdk-pixbuf+=	lib/libgdk_pixbuf.*
 BUILDLINK_FILES.gdk-pixbuf+=	lib/libgdk_pixbuf_xlib.*
+
+# The gdk-pixbuf headers should be found at the usual location.
+BUILDLINK_TRANSFORM.gdk-pixbuf=	-e "s|/include/gdk-pixbuf-1.0/|/include/|g"
 
 .include "../../graphics/jpeg/buildlink2.mk"
 .include "../../graphics/png/buildlink2.mk"
