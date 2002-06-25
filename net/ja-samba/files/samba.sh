41aa7800e3527a003020540265bcf0057937085b
taca <taca>
2002-06-25 15:31:32 +0000

    Importing ja-samba 2.2.2.1.1 package.
    This is modified version deveopled by Samba Users Group Japan.
    
    o I18N for SWAT.
    
    	Select display language based on information supplied by
    	Web browser.
    
    o L10N for Japanese.
    
    	Support various use of Japanese name; share name, computer
    	name, workgroup name, user name.
    
    	Solve some problems when different version of Windows are
    	mixed.  Especially, vanilla samba dosen't support vendor
    	depended characters completely.  (Those characters are
    	differently handled among Windows 95, Windows 98, Windows NT
    	and something changed in Windows XP.)  This cause practical
    	problem on real world.
    
    o Some Bugfix go ahead to original samba.
    
    o Some original feature
    
    	Supoprt a recycle bin.
    
    Currently it is samba 2.2.2 base, and they are developing newer
    version based on samba 2.2.4.

---
+++ net/ja-samba/files/samba.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#
+# $NetBSD: samba.sh,v 1.1.1.1 2002/06/25 15:31:34 taca Exp $
+#
+# KEYWORD: nostart
+
+if [ -f /etc/rc.subr ]
+then
+	. /etc/rc.subr
+fi
+
+rcd_dir=`@DIRNAME@ $0`
+
+# NOTE: run_rc_command sets $rc_arg
+#
+forward_commands()
+{
+	# Backward compat with NetBSD <1.6:
+	[ -z "$rc_arg" ] && rc_arg=$_arg
+
+	for file in $COMMAND_LIST; do
+		$rcd_dir/$file $rc_arg
+	done
+}
+
+reverse_commands()
+{
+	# Backward compat with NetBSD <1.6:
+	[ -z "$rc_arg" ] && rc_arg=$_arg
+
+	REVCOMMAND_LIST=
+	for file in $COMMAND_LIST; do
+		REVCOMMAND_LIST="$file $REVCOMMAND_LIST"
+	done
+	for file in $REVCOMMAND_LIST; do
+		$rcd_dir/$file $rc_arg
+	done
+}
+
+COMMAND_LIST="nmbd smbd"
+
+name="samba"
+start_cmd="forward_commands"
+stop_cmd="reverse_commands"
+reload_cmd="forward_commands"
+status_cmd="forward_commands"
+extra_commands="reload status"
+
+if [ -f /etc/rc.subr ]
+then
+	run_rc_command "$1"
+else
+	@ECHO@ -n ' ${name}'
+	_arg="$1"
+	${start_cmd}
+fi
