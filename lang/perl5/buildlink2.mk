a9f7158fff7d8afe42d20bd354b900b2cc52d457
jlam <jlam@pkgsrc.org>
2002-06-27 21:40:10 +0000

    Let bsd.pkg.mk handle setting the PERL5_* variables by defining USE_PERL5. I'll figure out a way to move all that to the buildlink2.mk file later.

--- lang/perl5/buildlink2.mk
+++ lang/perl5/buildlink2.mk
@@ -1,7 +1,9 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.3 2002/06/21 23:00:34 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.1.2.4 2002/06/27 21:40:10 jlam Exp $
 
 .if !defined(PERL5_BUILDLINK2_MK)
 PERL5_BUILDLINK2_MK=  # defined
+
+USE_PERL5=	# defined
 
 .include "../../mk/bsd.prefs.mk"
 
@@ -21,19 +23,3 @@ BUILDLINK_TARGETS+=	perl-buildlink
 perl-buildlink: _BUILDLINK_USE
 
 .endif  # PERL5_BUILDLINK2_MK
-
-.if exists(${PERL5})
-.  if exists(${BUILDLINK_PREFIX.perl}/share/mk/bsd.perl.mk)
-.    include "${BUILDLINK_PREFIX.perl}/share/mk/bsd.perl.mk"
-.  elif !defined(PERL5_SITELIB) || !defined(PERL5_SITEARCH) || !defined(PERL5_ARCHLIB)
-PERL5_SITELIB!=		eval `${PERL5} -V:installsitelib 2>/dev/null`;	\
-			${ECHO} $${installsitelib}
-PERL5_SITEARCH!=	eval `${PERL5} -V:installsitearch 2>/dev/null`;	\
-			${ECHO} $${installsitearch}
-PERL5_ARCHLIB!=		eval `${PERL5} -V:installarchlib 2>/dev/null`;	\
-			${ECHO} $${installarchlib}
-MAKEFLAGS+=		PERL5_SITELIB=${PERL5_SITELIB}
-MAKEFLAGS+=		PERL5_SITEARCH=${PERL5_SITEARCH}
-MAKEFLAGS+=		PERL5_ARCHLIB=${PERL5_ARCHLIB}
-.  endif # !exists(bsd.perl.mk) && !defined(PERL5_*)
-.endif # exists($PERL5)
