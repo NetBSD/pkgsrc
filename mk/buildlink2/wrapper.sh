0ced05028c771b7a3d3d51172520308b2d829c20
jlam <jlam>
2002-06-29 20:02:40 +0000

    Provide way to prevent updating the wrapper argument cache by setting BUILDLINK_UPDATE_CACHE=no in the environment.  We typically want to do this during the configure process to prevent poisoning the cache with stuff we don't want or with stuff that might break the wrapper scripts. Also add a way to clear the cache by doing "make clear-buildlink-cache".

--- mk/buildlink2/wrapper.sh
+++ mk/buildlink2/wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: wrapper.sh,v 1.1.2.3 2002/06/28 06:26:58 jlam Exp $
+# $NetBSD: wrapper.sh,v 1.1.2.4 2002/06/29 20:02:42 jlam Exp $
 
 Xsed='@SED@ -e 1s/^X//'
 sed_quote_subst='s/\([\\`\\"$\\\\]\)/\\\1/g'
@@ -10,10 +10,11 @@ cache="@_BLNK_WRAP_CACHE@"
 post_cache="@_BLNK_WRAP_POST_CACHE@"
 logic="@_BLNK_WRAP_LOGIC@"
 wrapperlog="@_BLNK_WRAP_LOG@"
+updatecache=${BUILDLINK_UPDATE_CACHE-yes}
 
 cmd="@WRAPPEE@"
 for arg; do
-	cacheupdated=
+	cacheupdated=no
 	. $logic
 	case "$cacheupdated" in
 	yes) @CAT@ $pre_cache $cache $post_cache > $logic ;;
