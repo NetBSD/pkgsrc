a1710d765832b6bbf30e170603c30b3dbe1a2e0d
jlam <jlam@pkgsrc.org>
2002-06-30 20:27:23 +0000

    typo

--- lang/tcl/buildlink2.mk
+++ lang/tcl/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.1 2002/06/23 23:04:32 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.1.2.2 2002/06/30 20:27:23 jlam Exp $
 
 .if !defined(TCL_BUILDLINK2_MK)
 TCL_BUILDLINK2_MK=	# defined
@@ -16,7 +16,7 @@ BUILDLINK_FILES.tcl+=	include/tcl/*/*.h
 BUILDLINK_FILES.tcl+=	lib/libtcl83.*
 BUILDLINK_FILES.tcl+=	lib/libtclstub83.*
 
-BUILDLINK_TARGETS==	tcl-buildlink
+BUILDLINK_TARGETS+=	tcl-buildlink
 
 tcl-buildlink: _BUILDLINK_USE
 
