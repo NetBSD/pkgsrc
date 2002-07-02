93091852b6d808b1150f7be8c3332044004f89c4
wiz <wiz@pkgsrc.org>
2002-07-02 12:56:29 +0000

    Remove duplicate empty line.

--- textproc/libxml/buildlink.mk
+++ textproc/libxml/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.10 2002/03/26 06:25:16 rh Exp $
+# $NetBSD: buildlink.mk,v 1.11 2002/07/02 12:56:29 wiz Exp $
 #
 # This Makefile fragment is included by packages that use libxml.
 #
@@ -39,7 +39,6 @@ REPLACE_BUILDLINK_SED+=	\
 	-e "s|-I${BUILDLINK_DIR}/\(include/gnome-xml\)|-I${BUILDLINK_PREFIX.libxml}/\1|g"
 BUILDLINK_CONFIG_WRAPPER_SED+=	\
 	-e "s|-I${BUILDLINK_PREFIX.libxml}/\(include/gnome-xml\)|-I${BUILDLINK_DIR}/\1 -I${BUILDLINK_DIR}/include|g"
-
 
 .if defined(USE_CONFIG_WRAPPER)
 XML_CONFIG?=		${BUILDLINK_CONFIG_WRAPPER.libxml}
