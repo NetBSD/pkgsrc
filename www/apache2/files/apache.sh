1da1091257aef06b3fdda2de9b34af60d3edf9ec
tv <tv@pkgsrc.org>
2002-07-07 00:45:18 +0000

    Provide the common rc.d directive "reload" as an alias for "apachectl graceful", which reloads the server without killing transfers in progress.

--- www/apache2/files/apache.sh
+++ www/apache2/files/apache.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: apache.sh,v 1.3 2002/04/13 21:35:51 jlam Exp $
+# $NetBSD: apache.sh,v 1.4 2002/07/07 00:45:19 tv Exp $
 #
 # PROVIDE: apache
 # REQUIRE: DAEMON
@@ -19,13 +19,16 @@ rcvar=$name
 command="@PREFIX@/sbin/httpd"
 ctl_command="@PREFIX@/sbin/apachectl"
 required_files="@PKG_SYSCONFDIR@/httpd.conf"
+extra_commands="reload"
 start_cmd="apache_doit start"
 stop_cmd="apache_doit stop"
 restart_cmd="apache_doit restart"
+reload_cmd="apache_doit reload"
 
 # "${apache_start}" is the subcommand sent to apachectl to control how
 # httpd is started.  It's value may be overridden in:
 #
+#	@PKG_SYSCONFDIR@/apache_start.conf
 #	/etc/rc.conf
 #	/etc/rc.conf.d/apache,
 #
@@ -33,17 +36,22 @@ restart_cmd="apache_doit restart"
 # and "startssl", and defaults to "start".
 #
 apache_start=start
+if [ -f @PKG_SYSCONFDIR@/apache_start.conf ]
+then
+	. @PKG_SYSCONFDIR@/apache_start.conf
+fi
 
 apache_doit ()
 {
 	case $1 in
 	start)	action=${apache_start} ;;
+	reload)	action=graceful ;;
 	*)	action=$1 ;;
 	esac
 	${ctl_command} ${action}
 }
 
-if [ -f /etc/rc.subr ]
+if [ -f /etc/rc.subr -a -f /etc/rc.conf -a -d /etc/rc.d -a -f /etc/rc.d/DAEMON ]
 then
 	load_rc_config $name
 	run_rc_command "$1"
