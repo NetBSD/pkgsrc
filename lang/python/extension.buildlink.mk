bb0a2cc268070adb24f6c44304d0a8f55462a658
wiz <wiz@pkgsrc.org>
2002-07-14 13:05:23 +0000

    echo -> ${ECHO}

--- lang/python/extension.buildlink.mk
+++ lang/python/extension.buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: extension.buildlink.mk,v 1.8 2002/01/29 19:22:26 drochner Exp $
+# $NetBSD: extension.buildlink.mk,v 1.9 2002/07/14 13:05:23 wiz Exp $
 
 # derive a python version from the package name if possible
 .if defined(PKGNAME_REQD)
@@ -19,11 +19,11 @@ IGNORE="${PKGNAME} needs dynamic loading"
 
 .if exists(${PYTHONBIN})
 PYINC!=	${PYTHONBIN} -c "import distutils.sysconfig; \
-	print distutils.sysconfig.get_python_inc(0, \"\")" || echo ""
+	print distutils.sysconfig.get_python_inc(0, \"\")" || ${ECHO} ""
 PYLIB!=	${PYTHONBIN} -c "import distutils.sysconfig; \
-	print distutils.sysconfig.get_python_lib(0, 1, \"\")" || echo ""
+	print distutils.sysconfig.get_python_lib(0, 1, \"\")" || ${ECHO} ""
 PYSITELIB!= ${PYTHONBIN} -c "import distutils.sysconfig; \
-	print distutils.sysconfig.get_python_lib(0, 0, \"\")" || echo ""
+	print distutils.sysconfig.get_python_lib(0, 0, \"\")" || ${ECHO} ""
 .endif
 
 .if defined(PYDISTUTILSPKG)
