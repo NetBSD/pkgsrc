ff445de485c782e6cde50b5851bfb367c1228f52
schmonz <schmonz>
2002-07-15 16:24:01 +0000

    Provide per-${OPSYS} defaults for how to run a command as root.
    Darwin ships with a disabled root account and a working "sudo".

--- mk/defs.Darwin.mk
+++ mk/defs.Darwin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.Darwin.mk,v 1.25 2002/06/02 19:44:32 schmonz Exp $
+# $NetBSD: defs.Darwin.mk,v 1.26 2002/07/15 16:24:01 schmonz Exp $
 #
 # Variable definitions for the Darwin operating system.
 
@@ -63,6 +63,7 @@ MOTIF_TYPE_DEFAULT?=	openmotif	# default 2.0 compatible libs type
 MOTIF12_TYPE_DEFAULT?=	lesstif12	# default 1.2 compatible libs type
 NOLOGIN?=		${FALSE}
 PKG_TOOLS_BIN?=		${ZOULARISBASE}/sbin
+ROOT_CMD?=		sudo sh -c
 ROOT_GROUP?=		wheel
 ROOT_USER?=		root
 SERIAL_DEVICES?=	/dev/null
