ab5c21642732d43c5c32492b283d89d05b3dc963
martti <martti@pkgsrc.org>
2002-07-22 11:51:35 +0000

    Added POSTFIX_USE_TLS=YES

--- mk/bsd.pkg.defaults.mk
+++ mk/bsd.pkg.defaults.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.pkg.defaults.mk,v 1.71 2002/07/20 12:42:35 tron Exp $
+# $NetBSD: bsd.pkg.defaults.mk,v 1.72 2002/07/22 11:51:35 martti Exp $
 #
 
 # A file providing defaults for pkgsrc and the packages collection.
@@ -1103,6 +1103,11 @@ PILRC_USE_GTK?=	YES
 
 #POSTFIX_USE_PCRE=YES
 # Add support for Perl Compatible Regular Expressions.
+# Possible: YES, not defined
+# Default: not defined
+
+#POSTFIX_USE_TLS=YES
+# Add support for TLS.
 # Possible: YES, not defined
 # Default: not defined
 
