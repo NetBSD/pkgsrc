53fafaeb45663d46e9ea19c03c2489fb6778d689
joda <joda>
2002-07-29 11:55:26 +0000

    rc.d-ify

--- net/zephyr/files/zhm.sh
+++ net/zephyr/files/zhm.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 #
-# $NetBSD: zhm.sh,v 1.2 1998/08/07 11:11:06 agc Exp $
+# $NetBSD: zhm.sh,v 1.3 2002/07/29 11:55:26 joda Exp $
 #
+# PROVIDE: zhm
+# REQUIRE: DAEMON
+
+. /etc/rc.subr
+
 PREFIX=${PREFIX-@prefix@}
 
-if [ -x ${PREFIX}/sbin/zhm -a -f ${PREFIX}/etc/zephyr/server.list ] ; then
-     PATH=${PREFIX}/sbin:${PREFIX}/bin:$PATH ; export PATH
-     zhm `cat ${PREFIX}/etc/zephyr/server.list` && echo -n ' zhm'
-fi
+name="zhm"
+rcvar="$name"
+command="${PREFIX}/sbin/${name}"
+pidfile="/var/run/${name}.pid"
+required_files="${PREFIX}/etc/zephyr/server.list"
+start_precmd="zhm_precmd"
+
+zhm_precmd()
+{
+	_flags="`cat ${PREFIX}/etc/zephyr/server.list`"
+}
+load_rc_config "$name"
+run_rc_command "$1"
