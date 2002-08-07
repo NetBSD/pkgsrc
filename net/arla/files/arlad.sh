398443620bd91447a798d1af4a1cf951ec0c0063
jlam <jlam>
2002-08-07 08:30:50 +0000

    Merge arlad and arlad.sh scripts into one, following pkgsrc convention for rc.d scripts.

--- net/arla/files/arlad.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# $NetBSD: arlad.sh,v 1.2 2001/08/09 13:48:59 wennmach Exp $
-#
-# start script for arlad (for NetBSD earlier than 1.5)
-
-if [ -f @PREFIX@/libexec/arlad ]; then
-	echo -n " arlad"
-	@PREFIX@/libexec/arlad -z /dev/xfs0
-fi
