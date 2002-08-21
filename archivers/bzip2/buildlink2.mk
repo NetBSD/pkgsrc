ea5c5346684438e0db7a2c57862e6d7dd97f8582
jlam <jlam>
2002-08-21 22:50:29 +0000

    Mirror changes to buildlink.mk in pkgsrc-current that allowing more precise specification of incompatible system versions of libbz.*.

--- archivers/bzip2/buildlink2.mk
+++ archivers/bzip2/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.3 2002/06/21 23:00:24 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.1.2.4 2002/08/21 22:50:29 jlam Exp $
 
 .if !defined(BZIP2_BUILDLINK2_MK)
 BZIP2_BUILDLINK2_MK=	# defined
@@ -8,19 +8,34 @@ BZIP2_BUILDLINK2_MK=	# defined
 BUILDLINK_DEPENDS.bzip2?=	bzip2>=1.0.1
 BUILDLINK_PKGSRCDIR.bzip2?=	../../archivers/bzip2
 
-.if exists(/usr/include/bzlib.h)
+.if defined(USE_BZIP2)
+_NEED_BZIP2=		YES
+.else
+.  if exists(/usr/include/bzlib.h)
 #
 # Recent versions of the libbz2 API prefix all functions with "BZ2_".
 #
 _BUILTIN_BZIP2!=	${EGREP} -c "BZ2_" /usr/include/bzlib.h || ${TRUE}
-.else
+.  else
 _BUILTIN_BZIP2=		0
-.endif
-
-.if ${_BUILTIN_BZIP2} == "0"
+.  endif
+.  if ${_BUILTIN_BZIP2} == "0"
 _NEED_BZIP2=		YES
-.else
+.  else
 _NEED_BZIP2=		NO
+.  endif
+#
+# This catch-all for SunOS is probably too broad, but better to err on
+# the safe side.  We can narrow down the match when we have better
+# information.
+#
+_INCOMPAT_BZIP2=	SunOS-*-*
+INCOMPAT_BZIP2?=	# empty
+.  for _pattern_ in ${_INCOMPAT_BZIP2} ${INCOMPAT_BZIP2}
+.    if !empty(MACHINE_PLATFORM:M${_pattern_})
+_NEED_BZIP2=		YES
+.    endif
+.  endfor
 .endif
 
 .if ${_NEED_BZIP2} == "YES"
