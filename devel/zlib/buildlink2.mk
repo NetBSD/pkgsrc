2870e8d2c74582caeee2a819b6c9d4b3e00630d7
jlam <jlam>
2002-08-22 19:48:44 +0000

    Mirror changes in buildlink.mk to support more precisely specifying when the system zlib routines are usable in place of zlib.

--- devel/zlib/buildlink2.mk
+++ devel/zlib/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.3 2002/06/21 23:00:29 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.1.2.4 2002/08/22 19:48:44 jlam Exp $
 
 .if !defined(ZLIB_BUILDLINK2_MK)
 ZLIB_BUILDLINK2_MK=	# defined
@@ -8,10 +8,31 @@ ZLIB_BUILDLINK2_MK=	# defined
 BUILDLINK_DEPENDS.zlib?=	zlib>=1.1.3
 BUILDLINK_PKGSRCDIR.zlib?=	../../devel/zlib
 
-.if exists(/usr/include/zlib.h)
-_NEED_ZLIB=		NO
-.else
+.if defined(USE_ZLIB)
 _NEED_ZLIB=		YES
+.else
+.  if exists(/usr/include/zlib.h)
+_NEED_ZLIB=		NO
+.  else
+_NEED_ZLIB=		YES
+.  endif
+#
+# Solaris has a broken (for the purposes of pkgsrc) version of zlib.
+#
+_INCOMPAT_ZLIB=		SunOS-*-*
+
+#
+# Some NetBSD versions shipped with versions lower than 1.1.3.
+#
+_INCOMPAT_ZLIB+=	NetBSD-0.*-* NetBSD-1.[012]*-*
+_INCOMPAT_ZLIB+=	NetBSD-1.3-* NetBSD-1.3.*-* NetBSD-1.3[A-H]-*
+
+INCOMPAT_ZLIB?=		# empty
+.  for _pattern_ in ${_INCOMPAT_ZLIB} ${INCOMPAT_ZLIB}
+.    if !empty(MACHINE_PLATFORM:M${_pattern_})
+_NEED_ZLIB=		YES
+.    endif
+.  endfor
 .endif
 
 .if ${_NEED_ZLIB} == "YES"
@@ -19,7 +40,7 @@ BUILDLINK_PACKAGES+=		zlib
 EVAL_PREFIX+=	BUILDLINK_PREFIX.zlib=zlib
 BUILDLINK_PREFIX.zlib_DEFAULT=	${LOCALBASE}
 .else
-BUILDLINK_PREFIX.zlib=	/usr
+BUILDLINK_PREFIX.zlib=		/usr
 .endif
 
 BUILDLINK_FILES.zlib=	include/zconf.h
