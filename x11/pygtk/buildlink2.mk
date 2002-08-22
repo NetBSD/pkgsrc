ef42739e31c230a0efcb3ccb7343bc965bcf0834
jlam <jlam>
2002-08-22 20:44:41 +0000

    Fix typo: s/DEPENDS/PKGSRCDIR/

--- x11/pygtk/buildlink2.mk
+++ x11/pygtk/buildlink2.mk
@@ -1,7 +1,7 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.1 2002/06/23 23:04:55 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.1.2.2 2002/08/22 20:45:57 jlam Exp $
 
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_PACKAGES+=		pygtk
 BUILDLINK_DEPENDS.pygtk?=	${PYPKGPREFIX}-gtk-*
-BUILDLINK_DEPENDS.pygtk?=	../../x11/pygtk
+BUILDLINK_PKGSRCDIR.pygtk?=	../../x11/pygtk
