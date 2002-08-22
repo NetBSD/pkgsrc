cc29901e2ed2ff33ac658ecaf99533e353d1ec8a
jlam <jlam@pkgsrc.org>
2002-08-22 20:44:41 +0000

    Fix typo: s/DEPENDS/PKGSRCDIR/

--- x11/py-qt2-sip/buildlink2.mk
+++ x11/py-qt2-sip/buildlink2.mk
@@ -1,7 +1,7 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.1 2002/06/23 23:04:54 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.1.2.2 2002/08/22 20:44:41 jlam Exp $
 
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_PACKAGES+=		pyqt2sip
 BUILDLINK_DEPENDS.pyqt2sip?=	${PYPKGPREFIX}-qt2-sip-*
-BUILDLINK_DEPENDS.pyqt2sip?=	../../x11/py-qt2-sip
+BUILDLINK_PKGSRCDIR.pyqt2sip?=	../../x11/py-qt2-sip
--- x11/py-qt2/buildlink2.mk
+++ x11/py-qt2/buildlink2.mk
@@ -1,7 +1,7 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.1 2002/06/23 23:04:54 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.1.2.2 2002/08/22 20:45:22 jlam Exp $
 
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_PACKAGES+=		pyqt2
 BUILDLINK_DEPENDS.pyqt2?=	${PYPKGPREFIX}-qt2-[0-9]*
-BUILDLINK_DEPENDS.pyqt2?=	../../x11/py-qt2
+BUILDLINK_PKGSRCDIR.pyqt2?=	../../x11/py-qt2
