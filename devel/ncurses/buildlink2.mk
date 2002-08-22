c65049d602b6acd8974b2d1905310af013f0552f
jlam <jlam@pkgsrc.org>
2002-08-22 19:35:13 +0000

    Mirror changes in buildlink.mk to support more precisely specifying when the system curses routines are usable in place of ncurses.

--- devel/ncurses/buildlink2.mk
+++ devel/ncurses/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.3 2002/06/21 23:00:28 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.1.2.4 2002/08/22 19:35:13 jlam Exp $
 #
 # Optionally define:
 #
 # USE_NCURSES		force use of ncurses
-# INCOMPAT_CURSES	specify NetBSD versions that are missing some
-#			needed functions.
+# INCOMPAT_CURSES	specify MACHINE_PLATFORM versions that are missing
+#			some needed functions.
 
 .if !defined(NCURSES_BUILDLINK2_MK)
 NCURSES_BUILDLINK2_MK=     # defined
@@ -18,23 +18,36 @@ BUILDLINK_PKGSRCDIR.ncurses?=	../../devel/ncurses
 _NEED_NCURSES=		YES
 .else
 _NEED_NCURSES=		NO
-.  if ${OPSYS} == "NetBSD"
-_INCOMPAT_CURSES=	0.* 1.[0123]* 1.4.* 1.4[A-X] ${INCOMPAT_CURSES}
-.    for PATTERN in ${_INCOMPAT_CURSES}
-.      if ${OS_VERSION:M${PATTERN}} != ""
+#
+# These versions of NetBSD didn't have a curses library that was capable of
+# replacing ncurses.
+#
+_INCOMPAT_CURSES=       NetBSD-0.*-* NetBSD-1.[0123]*-*
+_INCOMPAT_CURSES+=      NetBSD-1.4.*-* NetBSD-1.4[A-X]-*
+#
+# This catch-all for SunOS is probably too broad, but better to err on
+# the safe side.  We can narrow down the match when we have better
+# information.
+#
+_INCOMPAT_CURSES+=	SunOS-*-*
+INCOMPAT_CURSES?=	# empty
+.  for _pattern_ in ${_INCOMPAT_CURSES} ${INCOMPAT_CURSES}
+.    if !empty(MACHINE_PLATFORM:M${_pattern_})
 _NEED_NCURSES=		YES
-.      endif
-.    endfor
-.  elif ${OPSYS} != "SunOS"
-_NEED_NCURSES=		YES
-.  endif
+.    endif
+.  endfor
 .endif
 
 .if ${_NEED_NCURSES} == "YES"
 BUILDLINK_PACKAGES+=		ncurses
-EVAL_PREFIX+=	BUILDLINK_PREFIX.ncurses=ncurses
-BUILDLINK_PREFIX.ncurses_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.ncurses=	include/cursesapp.h
+EVAL_PREFIX+=			BUILDLINK_PREFIX.ncurses=ncurses
+BUILDLINK_PREFIX.ncurses_DEFAULT= ${LOCALBASE}
+.else
+BUILDLINK_PREFIX.ncurses=	/usr
+.endif
+
+BUILDLINK_FILES.ncurses+=	include/curses.h
+BUILDLINK_FILES.ncurses+=	include/cursesapp.h
 BUILDLINK_FILES.ncurses+=	include/cursesf.h
 BUILDLINK_FILES.ncurses+=	include/cursesm.h
 BUILDLINK_FILES.ncurses+=	include/cursesp.h
@@ -54,43 +67,39 @@ BUILDLINK_FILES.ncurses+=	lib/libmenu.*
 BUILDLINK_FILES.ncurses+=	lib/libncurses++.*
 BUILDLINK_FILES.ncurses+=	lib/libncurses.*
 BUILDLINK_FILES.ncurses+=	lib/libpanel.*
-.else
-BUILDLINK_PREFIX.ncurses=	/usr
-BUILDLINK_FILES.ncurses=	include/curses.h
-BUILDLINK_FILES.ncurses+=	include/eti.h
-BUILDLINK_FILES.ncurses+=	include/form.h
-BUILDLINK_FILES.ncurses+=	include/menu.h
-BUILDLINK_FILES.ncurses+=	include/termcap.h
-BUILDLINK_FILES.ncurses+=	include/unctrl.h
+
+.if ${_NEED_NCURSES} == "NO"
+_BLNK_LIBNCURSES_LIST!=		${ECHO} /usr/lib/libncurses.*
+.  if ${_BLNK_LIBNCURSES_LIST} == "/usr/lib/libncurses.*"
 BUILDLINK_FILES.ncurses+=	lib/libcurses.*
-BUILDLINK_FILES.ncurses+=	lib/libform.*
-BUILDLINK_FILES.ncurses+=	lib/libmenu.*
 BUILDLINK_TRANSFORM.ncurses+=	-e "s|/curses.h|/ncurses.h|g"
 BUILDLINK_TRANSFORM+=		l:ncurses:curses
+.  endif
 .endif
 
 BUILDLINK_TARGETS+=	ncurses-buildlink
 BUILDLINK_TARGETS+=	ncurses-curses-h
-
-.if ${_NEED_NCURSES} == "NO"
 BUILDLINK_TARGETS+=	ncurses-extra-includes-buildlink
-.endif
 
 ncurses-buildlink: _BUILDLINK_USE
 
 ncurses-extra-includes-buildlink:
 	${_PKG_SILENT}${_PKG_DEBUG}					\
-	${ECHO_BUILDLINK_MSG} "Touching extra ncurses headers in ${BUILDLINK_DIR}."; \
 	extra_includes="						\
 		include/term.h						\
 	";								\
 	for file in $${extra_includes}; do				\
-		${TOUCH} ${TOUCH_FLAGS} ${BUILDLINK_DIR}/$${file};	\
+		if [ ! -f ${BUILDLINK_DIR}/$${file} ]; then		\
+			${ECHO_BUILDLINK_MSG} "Touching extra ncurses header ($${file}) in ${BUILDLINK_DIR}."; \
+			${TOUCH} ${TOUCH_FLAGS} ${BUILDLINK_DIR}/$${file}; \
+		fi;							\
 	done
 
 ncurses-curses-h:
 	${_PKG_SILENT}${_PKG_DEBUG}					\
-	${ECHO_BUILDLINK_MSG} "Linking curses.h to ncurses.h in ${BUILDLINK_DIR}."; \
-	${LN} -s ${BUILDLINK_PREFIX.ncurses}/include/ncurses.h ${BUILDLINK_DIR}/include/curses.h
+	if [ ! -f ${BUILDLINK_DIR}/include/curses.h ]; then		\
+		${ECHO_BUILDLINK_MSG} "Linking curses.h to ncurses.h in ${BUILDLINK_DIR}."; \
+		${LN} -s ${BUILDLINK_PREFIX.ncurses}/include/ncurses.h ${BUILDLINK_DIR}/include/curses.h; \
+	fi
 
 .endif  # NCURSES_BUILDLINK2_MK
