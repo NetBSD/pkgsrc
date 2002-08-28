3d024da9a85201dbf7ee9c298840886852308302
jlam <jlam@pkgsrc.org>
2002-08-28 06:39:19 +0000

    Add the lame shared libraries to the BUILDLINK_FILES.

--- audio/lame/buildlink2.mk
+++ audio/lame/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:21:49 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/08/28 06:39:19 jlam Exp $
 
 .if !defined(LAME_BUILDLINK2_MK)
 LAME_BUILDLINK2_MK=	# defined
@@ -7,9 +7,10 @@ BUILDLINK_PACKAGES+=		lame
 BUILDLINK_DEPENDS.lame?=	lame>=3.89
 BUILDLINK_PKGSRCDIR.lame?=	../../audio/lame
 
-EVAL_PREFIX+=		BUILDLINK_PREFIX.lame=lame
-BUILDLINK_PREFIX.lame=	${LOCALBASE}
-BUILDLINK_FILES.lame=	include/lame/lame.h
+EVAL_PREFIX+=			BUILDLINK_PREFIX.lame=lame
+BUILDLINK_PREFIX.lame_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.lame=		include/lame/lame.h
+BUILDLINK_FILES.lame+=		lib/libmp3lame.*
 
 BUILDLINK_TARGETS+=	lame-buildlink
 
