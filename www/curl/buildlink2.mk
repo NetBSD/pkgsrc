4b1659c4340a964051ecf1b39f3a499fc7741185
jlam <jlam>
2002-09-05 20:48:17 +0000

    libcurl.so needs the openssl libraries.

--- www/curl/buildlink2.mk
+++ www/curl/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1 2002/09/05 20:40:01 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/09/05 20:48:17 jlam Exp $
 
 .if !defined(CURL_BUILDLINK2_MK)
 CURL_BUILDLINK2_MK=	# defined
@@ -11,6 +11,8 @@ EVAL_PREFIX+=	BUILDLINK_PREFIX.curl=curl
 BUILDLINK_PREFIX.curl_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.curl=	include/curl/*
 BUILDLINK_FILES.curl+=	lib/libcurl.*
+
+.include "../../security/openssl/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	curl-buildlink
 
