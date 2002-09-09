22fa89c9fcfeec868da4f44d1884c047cc5663f6
jlam <jlam@pkgsrc.org>
2002-09-09 19:59:40 +0000

    We don't symlink binaries into ${BUILDLINK_DIR}/bin because they may conflict with wrapper scripts.  Also modify INCOMPAT_BINUTILS to take ${MACHINE_PLATFORM}-style wildcards.

--- devel/binutils/buildlink2.mk
+++ devel/binutils/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:22:22 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/09/09 19:59:40 jlam Exp $
 
 .if !defined(BINUTILS_BUILDLINK2_MK)
 BINUTILS_BUILDLINK2_MK=	# defined
@@ -9,16 +9,26 @@ BUILDLINK_DEPENDS.binutils?=		binutils>=2.11.2
 BUILDLINK_PKGSRCDIR.binutils?=		../../devel/binutils
 BUILDLINK_DEPMETHOD.binutils?=		build
 
-_NEED_BINUTILS=          NO
-.if ${OPSYS} == "NetBSD"
-_INCOMPAT_BINUTILS=       0.* 1.[01234]* 1.5.* 1.5[A-X]
-.  for PATTERN in ${_INCOMPAT_BINUTILS}
-.    if ${OS_VERSION:M${PATTERN}} != ""
+.if defined(USE_BINUTILS)
+_NEED_BINUTILS=		YES
+.else
+_NEED_BINUTILS=		NO
+#
+# These versions of NetBSD didn't have a curses library that was capable of
+# replacing binutils.
+#
+_INCOMPAT_BINUTILS=	NetBSD-0.*-* NetBSD-1.[01234]*-*
+_INCOMPAT_BINUTILS+=	NetBSD-1.5.*-* NetBSD-1.5[A-X]-*
+#
+# XXX: _INCOMPAT_BINUTILS settings for other operating systems possibly
+# XXX: needed here
+#
+INCOMPAT_BINUTILS?=       # empty
+.  for _pattern_ in ${_INCOMPAT_BINUTILS} ${INCOMPAT_BINUTILS}
+.    if !empty(MACHINE_PLATFORM:M${_pattern_})
 _NEED_BINUTILS=          YES
 .    endif
 .  endfor
-.else
-# XXX: logic for other operating systems needed here
 .endif
 
 .if ${_NEED_BINUTILS} == "YES"
@@ -31,25 +41,12 @@ BUILDLINK_FILES.binutils+=	include/bfdlink.h
 BUILDLINK_FILES.binutils+=	lib/libbfd.*
 BUILDLINK_FILES.binutils+=	lib/libiberty.*
 BUILDLINK_FILES.binutils+=	lib/libopcodes.*
-BUILDLINK_FILES.binutils+=	bin/addr2line
-BUILDLINK_FILES.binutils+=	bin/ar
-BUILDLINK_FILES.binutils+=	bin/as
-BUILDLINK_FILES.binutils+=	bin/c++filt
-BUILDLINK_FILES.binutils+=	bin/gasp
-BUILDLINK_FILES.binutils+=	bin/gprof
-BUILDLINK_FILES.binutils+=	bin/ld
-BUILDLINK_FILES.binutils+=	bin/nm
-BUILDLINK_FILES.binutils+=	bin/objcopy
-BUILDLINK_FILES.binutils+=	bin/objdump
-BUILDLINK_FILES.binutils+=	bin/ranlib
-BUILDLINK_FILES.binutils+=	bin/readelf
-BUILDLINK_FILES.binutils+=	bin/size
-BUILDLINK_FILES.binutils+=	bin/strings
-BUILDLINK_FILES.binutils+=	bin/strip
 
 BUILDLINK_TARGETS+=	binutils-buildlink
 
+PATH:=	${BUILDLINK_PREFIX.binutils}/bin:${PATH}
+
 binutils-buildlink: _BUILDLINK_USE
-.endif	# _NEED_BINUTILS
+.endif	# _NEED_BINUTILS == YES
 
 .endif	# BINUTILS_BUILDLINK2_MK
