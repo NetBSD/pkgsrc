39748e570b0ab4b8d678fd6e0c39a13d85b292a9
jlam <jlam@pkgsrc.org>
2002-09-10 19:50:36 +0000

    USE_PTHREAD died a long time ago; match pthread handling from buildlink.mk and add the PTL header directory to CPPFLAGS so that the headers for the pthread implementation used by glib2 may be found by other packages.

--- devel/glib2/buildlink2.mk
+++ devel/glib2/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/10 16:06:39 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2002/09/10 19:50:36 jlam Exp $
 
 .if !defined(GLIB2_BUILDLINK2_MK)
 GLIB2_BUILDLINK2_MK=	# defined
@@ -17,14 +17,18 @@ BUILDLINK_FILES.glib2+=	lib/libgmodule-2.0.*
 BUILDLINK_FILES.glib2+=	lib/libgobject-2.0.*
 BUILDLINK_FILES.glib2+=	lib/libgthread-2.0.*
 
-USE_PTHREAD=	native ptl2
+.include "../../converters/libiconv/buildlink.mk"
+.include "../../devel/gettext-lib/buildlink.mk"
+.include "../../devel/pkgconfig/buildlink.mk"
 
-.include "../../mk/bsd.prefs.mk"
-.include "../../mk/pthread.buildlink2.mk"
+PTHREAD_OPTS+=	native
 
-.include "../../converters/libiconv/buildlink2.mk"
-.include "../../devel/gettext-lib/buildlink2.mk"
-.include "../../devel/pkgconfig/buildlink2.mk"
+.include "../../mk/pthread.buildlink.mk"
+
+.if defined(PTHREAD_TYPE) && (${PTHREAD_TYPE} == "none")
+CPPFLAGS+=	-I${BUILDLINK_PREFIX.ptl2}/PTL/include
+.  include "../../devel/ptl2/buildlink.mk"
+.endif
 
 BUILDLINK_TARGETS+=	glib2-buildlink
 
