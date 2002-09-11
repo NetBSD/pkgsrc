dd70a189f2254c37ef3e05c49a582632511c8ff9
wiz <wiz@pkgsrc.org>
2002-09-11 09:11:33 +0000

    Remove oaf buildlink2.mk.

--- devel/GConf2/buildlink2.mk
+++ devel/GConf2/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/10 16:06:37 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2002/09/11 09:11:33 wiz Exp $
 
 .if !defined(GCONF2_BUILDLINK2_MK)
 GCONF2_BUILDLINK2_MK=	# defined
@@ -14,7 +14,6 @@ BUILDLINK_FILES.gconf2+=		lib/libgconf-2*
 
 .include "../../databases/db3/buildlink2.mk"
 .include "../../devel/gettext-lib/buildlink2.mk"
-.include "../../devel/oaf/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	gconf2-buildlink
 
