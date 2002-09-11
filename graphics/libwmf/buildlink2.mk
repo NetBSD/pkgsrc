e6e640f131a8ea46669a38f943a2dc83b583033a
wiz <wiz@pkgsrc.org>
2002-09-11 11:19:56 +0000

    Update to 0.2.6. Convert to buildlink2. Changes: Split libwmf in two, libwmflite (with the parser) and libwmf (utility functions, device layers, etc.). Should also better support ImageMagick. Other changes unknown.

--- graphics/libwmf/buildlink2.mk
+++ graphics/libwmf/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/10 16:06:42 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2002/09/11 11:19:56 wiz Exp $
 
 .if !defined(LIBWMF_BUILDLINK2_MK)
 LIBWMF_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libwmf
-BUILDLINK_DEPENDS.libwmf?=	libwmf>=0.2.2nb2
+BUILDLINK_DEPENDS.libwmf?=	libwmf>=0.2.6
 BUILDLINK_PKGSRCDIR.libwmf?=	../../graphics/libwmf
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.libwmf=libwmf
@@ -13,6 +13,8 @@ BUILDLINK_FILES.libwmf=		include/libwmf/libwmf/*.h
 BUILDLINK_FILES.libwmf+=	include/libwmf/libwmf/gd/*.h
 BUILDLINK_FILES.libwmf+=	lib/libwmf-0.2.so*
 BUILDLINK_FILES.libwmf+=	lib/libwmf.*
+BUILDLINK_FILES.libwmf+=	lib/libwmflite-0.2.so*
+BUILDLINK_FILES.libwmf+=	lib/libwmflite.*
 
 .include "../../graphics/freetype2/buildlink2.mk"
 .include "../../graphics/png/buildlink2.mk"
