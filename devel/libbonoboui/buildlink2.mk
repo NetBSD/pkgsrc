282f5003c65fcd8854886a1156afc3b7bf29b68d
rh <rh>
2002-09-11 23:57:02 +0000

    Initial import of libbonoboui-2.0.3, the user interface components of the CORBA object activtion framework for GNOME2. Provided by Julio Merino <jmmv@hispabsd.org>.  Closes PR pkg/18198.

---
+++ devel/libbonoboui/buildlink2.mk
@@ -0,0 +1,32 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2002/09/11 23:57:02 rh Exp $
+
+.if !defined(LIBBONOBOUI_BUILDLINK2_MK)
+LIBBONOBOUI_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=		libbonoboui
+BUILDLINK_DEPENDS.libbonoboui?=	libbonoboui>=2.0.3
+BUILDLINK_PKGSRCDIR.libbonoboui?=	../../devel/libbonoboui
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.libbonoboui=libbonoboui
+BUILDLINK_PREFIX.libbonoboui_DEFAULT=	${X11PREFIX}
+BUILDLINK_FILES.libbonoboui+=	include/libbonoboui-2.0/bonobo/*
+BUILDLINK_FILES.libbonoboui+=	include/libbonoboui-2.0/*
+BUILDLINK_FILES.libbonoboui+=	lib/libbonoboui-2.*
+BUILDLINK_FILES.libbonoboui+=	lib/libglade/2.0/libbonobo.*
+BUILDLINK_FILES.libbonoboui+=	lib/pkgconfig/libbonoboui-2.0.pc
+
+.include "../../devel/pkgconfig/buildlink2.mk"
+.include "../../devel/GConf2/buildlink2.mk"
+.include "../../devel/libbonobo/buildlink2.mk"
+.include "../../devel/libglade2/buildlink2.mk"
+.include "../../devel/libgnome/buildlink2.mk"
+.include "../../graphics/libart2/buildlink2.mk"
+.include "../../graphics/libgnomecanvas/buildlink2.mk"
+.include "../../sysutils/gnome-vfs2/buildlink2.mk"
+.include "../../x11/gtk2/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	libbonoboui-buildlink
+
+libbonoboui-buildlink: _BUILDLINK_USE
+
+.endif	# LIBBONOBOUI_BUILDLINK2_MK
