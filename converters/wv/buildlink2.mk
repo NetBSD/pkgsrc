00a0a207e96e88b84de4b7503d095603426bc2d7
wiz <wiz@pkgsrc.org>
2002-09-11 11:23:14 +0000

    Update to 0.7.2, changes unknown.

--- converters/wv/buildlink2.mk
+++ converters/wv/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/10 16:06:37 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2002/09/11 11:23:15 wiz Exp $
 
 .if !defined(WV_BUILDLINK2_MK)
 WV_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		wv
-BUILDLINK_DEPENDS.wv?=		wv>=0.7.0nb2
+BUILDLINK_DEPENDS.wv?=		wv>=0.7.2
 BUILDLINK_PKGSRCDIR.wv?=	../../converters/wv
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.wv=wv
