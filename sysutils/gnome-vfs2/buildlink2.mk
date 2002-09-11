102b75a5c86189191165d3c1ee7df4080dad422c
rh <rh@pkgsrc.org>
2002-09-11 23:07:50 +0000

    Bump gnome-vfs2 to 2.0.4nb1: 	- Properly compile pluggable fs modules (pkg/18220) 	- Add missing dependencies (pkg/18220) 	- Make this more pkglint-friendly. 	- Don't try to tweak LTCONFIG_OVERRIDE towards ltmain.sh, use 	  LIBTOOL_OVERRIDE instead

--- sysutils/gnome-vfs2/buildlink2.mk
+++ sysutils/gnome-vfs2/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2002/09/07 03:04:06 rh Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/09/11 23:07:50 rh Exp $
 
 .if !defined(GNOME_VFS2_BUILDLINK2_MK)
 GNOME_VFS2_BUILDLINK2_MK=	# defined
@@ -12,15 +12,25 @@ BUILDLINK_PREFIX.gnome-vfs2_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.gnome-vfs2=	include/gnome-vfs-2.0/libgnomevfs/*
 BUILDLINK_FILES.gnome-vfs2+=	include/gnome-vfs-module-2.0/libgnomevfs/*
 BUILDLINK_FILES.gnome-vfs2+=	lib/gnome-vfs-2.0/include/gnome-vfs-file-size.h
+BUILDLINK_FILES.gnome-vfs2+=	lib/gnome-vfs-2.0/modules/*
 BUILDLINK_FILES.gnome-vfs2+=	lib/libgnomevfs-2.*
 BUILDLINK_FILES.gnome-vfs2+=	lib/pkgconfig/gnome-vfs-2.0.pc
 BUILDLINK_FILES.gnome-vfs2+=	lib/pkgconfig/gnome-vfs-module-2.0.pc
 
+.include "../../archivers/bzip2/buildlink2.mk"
+.include "../../audio/cdparanoia/buildlink2.mk"
 .include "../../databases/gnome-mime-data/buildlink2.mk"
 .include "../../devel/gettext-lib/buildlink2.mk"
 .include "../../devel/GConf2/buildlink2.mk"
+.include "../../devel/glib2/buildlink2.mk"
 .include "../../devel/libbonobo/buildlink2.mk"
+.include "../../devel/bonobo-activation/buildlink2.mk"
 .include "../../devel/ptl2/buildlink2.mk"
+.include "../../devel/popt/buildlink2.mk"
+.include "../../devel/zlib/buildlink2.mk"
+.include "../../net/ORBit2/buildlink2.mk"
+.include "../../textproc/libxml2/buildlink2.mk"
+.include "../../security/openssl/buildlink2.mk"
 .include "../../sysutils/fam/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	gnome-vfs2-buildlink
