0ae25ebf2292b88be46d79c6faad1e30bbd3fefb
rh <rh@pkgsrc.org>
2002-09-12 04:28:27 +0000

    Initial import of libgnomeprintui-1.116.0, the user interface dependent part of the GNOME2 printing infrastructure. Provided by Julio Merino <jmmv@hispabsd.org>.  This closes PR pkg/18204.

---
+++ print/libgnomeprintui/buildlink2.mk
@@ -0,0 +1,30 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2002/09/12 04:28:27 rh Exp $
+
+.if !defined(LIBGNOMEPRINTUI_BUILDLINK2_MK)
+LIBGNOMEPRINTUI_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			libgnomeprintui
+BUILDLINK_DEPENDS.libgnomeprintui?=	libgnomeprintui>=1.116.0
+BUILDLINK_PKGSRCDIR.libgnomeprintui?=	../../print/libgnomeprintui
+
+EVAL_PREFIX+=		BUILDLINK_PREFIX.libgnomeprintui=libgnomeprintui
+BUILDLINK_PREFIX.libgnomeprintui_DEFAULT=	${X11PREFIX}
+BUILDLINK_FILES.libgnomeprintui=	include/libgnomeprintui-2.0/libgnomeprintui/*
+BUILDLINK_FILES.libgnomeprintui+=	lib/libgnomeprintui-2.*
+BUILDLINK_FILES.libgnomeprintui+=	lib/pkgconfig/libgnomeprintui-2.0.pc
+
+.include "../../devel/GConf2/buildlink2.mk"
+.include "../../devel/bonobo-activation/buildlink2.mk"
+.include "../../devel/libbonobo/buildlink2.mk"
+.include "../../devel/libglade2/buildlink2.mk"
+.include "../../devel/zlib/buildlink2.mk"
+.include "../../graphics/libart2/buildlink2.mk"
+.include "../../graphics/libgnomecanvas/buildlink2.mk"
+.include "../../print/libgnomeprint/buildlink2.mk"
+.include "../../textproc/libxml2/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	libgnomeprintui-buildlink
+
+libgnomeprintui-buildlink: _BUILDLINK_USE
+
+.endif	# LIBGNOMEPRINTUI_BUILDLINK2_MK
