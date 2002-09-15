fcc6ab8b1ccf8948ac886558030a6d809172ee7a
rh <rh@pkgsrc.org>
2002-09-15 05:36:56 +0000

    s/USE_X11BASE/USE_X11/ so this works on non-xpkgwedge systems Rename the package to gnome2-pixmaps and let it co-exist with gnome-pixmaps if LOCALBASE != X11BASE

--- graphics/gnome2-pixmaps/buildlink2.mk
+++ graphics/gnome2-pixmaps/buildlink2.mk
@@ -1,22 +1,22 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2002/09/14 06:57:33 rh Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/09/15 05:36:56 rh Exp $
 #
-# This Makefile fragment is included by packages that use gnome-pixmaps.
+# This Makefile fragment is included by packages that use gnome2-pixmaps.
 #
 # This file was created automatically using createbuildlink 2.0.
 #
 
-.if !defined(GNOME_PIXMAPS_BUILDLINK2_MK)
-GNOME_PIXMAPS_BUILDLINK2_MK=	# defined
+.if !defined(GNOME2_PIXMAPS_BUILDLINK2_MK)
+GNOME2_PIXMAPS_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=			gnome-pixmaps
-BUILDLINK_DEPENDS.gnome-pixmaps?=		gnome-pixmaps>=2.0.8
-BUILDLINK_PKGSRCDIR.gnome-pixmaps?=		../../graphics/gnome2-pixmaps
+BUILDLINK_PACKAGES+=			gnome2-pixmaps
+BUILDLINK_DEPENDS.gnome2-pixmaps?=		gnome2-pixmaps>=2.0.8
+BUILDLINK_PKGSRCDIR.gnome2-pixmaps?=		../../graphics/gnome2-pixmaps
 
-EVAL_PREFIX+=	BUILDLINK_PREFIX.gnome-pixmaps=gnome-pixmaps
-BUILDLINK_PREFIX.gnome-pixmaps_DEFAULT=	${X11PREFIX}
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gnome2-pixmaps=gnome2-pixmaps
+BUILDLINK_PREFIX.gnome2-pixmaps_DEFAULT=	${LOCALBASE}
 
-BUILDLINK_TARGETS+=	gnome-pixmaps-buildlink
+BUILDLINK_TARGETS+=	gnome2-pixmaps-buildlink
 
-gnome-pixmaps-buildlink: _BUILDLINK_USE
+gnome2-pixmaps-buildlink: _BUILDLINK_USE
 
-.endif	# GNOME_PIXMAPS_BUILDLINK2_MK
+.endif	# GNOME2_PIXMAPS_BUILDLINK2_MK
