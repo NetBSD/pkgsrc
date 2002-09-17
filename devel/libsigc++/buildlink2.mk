3a44ff569ada19c3174fae57999998f2e8aaad94
jlam <jlam@pkgsrc.org>
2002-09-17 05:46:53 +0000

    Require 1.0.4nb1 and include pthread.buildlink*.mk now that pthread support is activated in libsigc++.

--- devel/libsigc++/buildlink2.mk
+++ devel/libsigc++/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/11 21:29:28 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2002/09/17 05:46:54 jlam Exp $
 
 .if !defined(LIBSIGCXX_BUILDLINK2_MK)
 LIBSIGCXX_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libsigcxx
-BUILDLINK_DEPENDS.libsigcxx?=	libsigc++>=1.0.4
+BUILDLINK_DEPENDS.libsigcxx?=	libsigc++>=1.0.4nb1
 BUILDLINK_PKGSRCDIR.libsigcxx?=	../../devel/libsigc++
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.libsigcxx=libsigc++
@@ -12,6 +12,8 @@ BUILDLINK_PREFIX.libsigcxx_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.libsigcxx=	include/sigc++-1.0/sigc++/*
 BUILDLINK_FILES.libsigcxx+=	lib/sigc++-1.0/include/sigc++config.h
 BUILDLINK_FILES.libsigcxx+=	lib/libsigc.*
+
+.include "../../mk/pthread.buildlink2.mk"
 
 BUILDLINK_TARGETS+=	libsigcxx-buildlink
 
