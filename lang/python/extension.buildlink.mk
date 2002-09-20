cf40cdad23252af11b30083ee06c27adf907801a
jlam <jlam>
2002-09-20 22:53:21 +0000

    Move the definitions for PYINC, PYLIB, and PYSITELIB to pyversion.mk so that they may be used by buildlink2.mk files.

--- lang/python/extension.buildlink.mk
+++ lang/python/extension.buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: extension.buildlink.mk,v 1.11 2002/09/04 14:28:37 drochner Exp $
+# $NetBSD: extension.buildlink.mk,v 1.12 2002/09/20 22:53:21 jlam Exp $
 
 # derive a python version from the package name if possible
 .if defined(PKGNAME_REQD)
@@ -7,23 +7,10 @@ PYTHON_VERSION_REQD?= ${PKGNAME_REQD:C/^py([^-]*)-.*/\1/}
 
 .include "../../lang/python/pyversion.mk"
 
-#
-# below is what used to be in bsd.python.mk
-#
-
 .if defined(PYBINMODULE)
 .if ${MACHINE_ARCH} == "mips" || ${MACHINE_ARCH} == "vax"
 IGNORE="${PKGNAME} needs dynamic loading"
 .endif
-.endif
-
-.if exists(${PYTHONBIN})
-PYINC!=	${PYTHONBIN} -c "import distutils.sysconfig; \
-	print distutils.sysconfig.get_python_inc(0, \"\")" || ${ECHO} ""
-PYLIB!=	${PYTHONBIN} -c "import distutils.sysconfig; \
-	print distutils.sysconfig.get_python_lib(0, 1, \"\")" || ${ECHO} ""
-PYSITELIB!= ${PYTHONBIN} -c "import distutils.sysconfig; \
-	print distutils.sysconfig.get_python_lib(0, 0, \"\")" || ${ECHO} ""
 .endif
 
 .if defined(PYDISTUTILSPKG)
