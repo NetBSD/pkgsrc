905291a92c9612fd330a18ccfbd150532affbe5f
jlam <jlam@pkgsrc.org>
2002-09-20 02:04:33 +0000

    Use BUILDLINK_PKGSRCDIR.kdelibs2 instead of referring to the pkgsrc directory for kdelibs2 directly.  Also some minor whitespacing.

--- x11/kdelibs2/buildlink2.mk
+++ x11/kdelibs2/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/10 16:06:54 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2002/09/20 02:04:33 jlam Exp $
 
 .if !defined(KDELIBS2_BUILDLINK2_MK)
 KDELIBS2_BUILDLINK2_MK=	# defined
@@ -11,7 +11,9 @@ BUILDLINK_PKGSRCDIR.kdelibs2?=	../../x11/kdelibs2
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.kdelibs2=kdelibs
 BUILDLINK_PREFIX.kdelibs2_DEFAULT=	${X11PREFIX}
-BUILDLINK_FILES.kdelibs2!=	${GREP} "^\(include\|lib\)" ${.CURDIR}/../../x11/kdelibs2/PLIST
+BUILDLINK_FILES.kdelibs2!=						\
+	${GREP} "^\(include\|lib\)"					\
+		${.CURDIR}/${BUILDLINK_PKGSRCDIR.kdelibs2}/PLIST
 
 KDEDIR=				${BUILDLINK_PREFIX.kdelibs2}
 
@@ -26,7 +28,7 @@ USE_OPENSSL_VERSION=		${OPENSSL_VERSION_096}
 .include "../../mk/ossaudio.buildlink2.mk"
 
 .if defined(USE_CUPS) && (${USE_CUPS} == "YES")
-.include "../../print/cups/buildlink2.mk"
+.  include "../../print/cups/buildlink2.mk"
 .endif
 
 BUILDLINK_TARGETS+=	kdelibs2-buildlink
