c932d01fb5131898f08112a716b45dac31471d36
jlam <jlam@pkgsrc.org>
2002-09-20 22:44:18 +0000

    Python extensions also install headers into ${PYINC}, so we can't just assume all of those headers belong to the python package.  We use the trick for kdelibs2 and generate the BUILDLINK_FILES list from the PLIST.

--- lang/python21-pth/buildlink2.mk
+++ lang/python21-pth/buildlink2.mk
@@ -1,7 +1,9 @@
-# $NetBSD: buildlink2.mk,v 1.1 2002/09/20 22:33:30 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/09/20 22:44:19 jlam Exp $
 
 .if !defined(PYTHON21_BUILDLINK2_MK)
 PYTHON21_BUILDLINK2_MK=	# defined
+
+.include "../../mk/bsd.prefs.mk"
 
 BUILDLINK_PACKAGES+=			python21-pth
 BUILDLINK_DEPENDS.python21-pth?=	python21-pth>=2.1
@@ -13,7 +15,8 @@ BUILDLINK_DEPMETHOD.python21-pth?=	${BUILDLINK_DEPMETHOD.python}
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.python21-pth=python21-pth
 BUILDLINK_PREFIX.python21-pth_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.python21-pth=	include/python2p1/*
+BUILDLINK_FILES.python21-pth!= \
+	${GREP} "^include/" ${.CURDIR}/${BUILDLINK_PKGSRCDIR.python21-pth}/PLIST
 BUILDLINK_FILES.python21-pth+=	lib/python2p1/config/libpython2p1.*
 
 BUILDLINK_TARGETS+=	python21-pth-buildlink
