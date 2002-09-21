b41ccc12d220926dc4c24e21ddfeb6567d42adf7
jlam <jlam>
2002-09-21 22:32:25 +0000

    Need to include bsd.prefs.mk for GREP definition.  From pkg/18362 by Julio Merino.

--- audio/arts/buildlink2.mk
+++ audio/arts/buildlink2.mk
@@ -1,7 +1,9 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:21:43 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/09/21 22:32:25 jlam Exp $
 
 .if !defined(ARTS_BUILDLINK2_MK)
 ARTS_BUILDLINK2_MK=	# defined
+
+.include "../../mk/bsd.prefs.mk"
 
 BUILDLINK_PACKAGES+=		arts
 BUILDLINK_DEPENDS.arts?=	arts>=1.0.0
