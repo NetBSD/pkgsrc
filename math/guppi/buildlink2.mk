46a236b6c72d5ecb2e1b2a5b2ed9d6b4c6475085
jlam <jlam@pkgsrc.org>
2002-09-24 10:24:59 +0000

    Add a buildlink2.mk file for use by other package Makefiles.

---
+++ math/guppi/buildlink2.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink2.mk,v 1.1 2002/09/24 10:24:59 jlam Exp $
+
+.if !defined(GUPPI_BUILDLINK2_MK)
+GUPPI_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=		guppi
+BUILDLINK_DEPENDS.guppi?=	guppi>=0.40.3nb1
+BUILDLINK_PKGSRCDIR.guppi?=	../../math/guppi
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.guppi=guppi
+BUILDLINK_PREFIX.guppi_DEFAULT=	${X11PREFIX}
+BUILDLINK_FILES.guppi=	include/gnome-1.0/libguppi/*
+BUILDLINK_FILES.guppi+=	include/gnome-1.0/libguppitank/*
+BUILDLINK_FILES.guppi+=	lib/libguppi.*
+BUILDLINK_FILES.guppi+=	lib/libguppitank.*
+
+.include "../../devel/libglade/buildlink2.mk"
+.include "../../devel/readline/buildlink2.mk"
+.include "../../x11/gnome-core/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	guppi-buildlink
+
+guppi-buildlink: _BUILDLINK_USE
+
+.endif	# GUPPI_BUILDLINK2_MK
