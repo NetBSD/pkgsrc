bc36e3accf49606ed6304389dc40f9e8a79e7248
jlam <jlam@pkgsrc.org>
2002-09-29 06:55:55 +0000

    udunits installs static libraries, so by default, use a build dependency.

--- math/udunits/buildlink2.mk
+++ math/udunits/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:23:08 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/09/29 06:55:55 jlam Exp $
 
 .if !defined(UDUNITS_BUILDLINK2_MK)
 UDUNITS_BUILDLINK2_MK=		# defined
@@ -6,6 +6,7 @@ UDUNITS_BUILDLINK2_MK=		# defined
 BUILDLINK_PACKAGES+=		udunits
 BUILDLINK_DEPENDS.udunits?=	udunits>=1.11.7
 BUILDLINK_PKGSRCDIR.udunits?=	../../math/udunits
+BUILDLINK_DEPMETHOD.udunits?=	build
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.udunits=udunits
 BUILDLINK_PREFIX.udunits_DEFAULT=${LOCALBASE}
