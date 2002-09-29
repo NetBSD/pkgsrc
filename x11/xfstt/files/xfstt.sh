3ea8862051c444bb335f2ebc99e84009584e8db3
wiz <wiz@pkgsrc.org>
2002-09-29 12:58:37 +0000

    Two fixes from Urban Boquist. Closes PR 18197.

--- x11/xfstt/files/xfstt.sh
+++ x11/xfstt/files/xfstt.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: xfstt.sh,v 1.3 2002/09/27 14:47:06 grant Exp $
+# $NetBSD: xfstt.sh,v 1.4 2002/09/29 12:58:37 wiz Exp $
 #
 # PROVIDE: xfstt
 # REQUIRE: DAEMON
@@ -9,7 +9,7 @@
 
 name="xfstt"
 command="@PREFIX@/bin/${name}"
-command_flags="--user nobody --notcp --daemon &"
+command_args="--user nobody --notcp --daemon"
 pidfile="/var/run/${name}.pid"
 start_precmd="${command} --sync >/dev/null"
 
