a4a7e5b91eb7f0ccfe4cad7d04a007f18a2d2a26
jlam <jlam>
2002-10-02 20:46:46 +0000

    Make this script more verbose when starting/stopping OSS.

--- audio/oss/files/oss.sh
+++ audio/oss/files/oss.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: oss.sh,v 1.1.1.1 2002/10/02 20:33:31 jlam Exp $
+# $NetBSD: oss.sh,v 1.2 2002/10/02 20:46:46 jlam Exp $
 #
 # PROVIDE: oss
 # REQUIRE: aftermountlkm
@@ -13,8 +13,25 @@ fi
 name="oss"
 rcvar=$name
 required_files="/etc/oss.conf"
-start_cmd="@PREFIX@/bin/soundon"
-stop_cmd="@PREFIX@/bin/soundoff"
+start_cmd="oss_start"
+stop_cmd="oss_stop"
+
+oss_start()
+{
+	if [ -x @PREFIX@/bin/soundon ]; then
+		@ECHO@ "Starting ${name}."
+		@PREFIX@/bin/soundon
+	fi
+}
+
+oss_stop()
+{
+	if [ -x @PREFIX@/bin/soundoff ]; then
+		@ECHO@ "Stopping ${name}."
+		@PREFIX@/bin/soundoff
+	fi
+}
+
 
 if [ -f /etc/rc.subr -a -f /etc/rc.conf -a -d /etc/rc.d -a -f /etc/rc.d/DAEMON ]
 then
@@ -23,4 +40,3 @@ then
 else
 	$start_cmd
 fi
-
