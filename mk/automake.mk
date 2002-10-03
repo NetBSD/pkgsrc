3cdae317ec8b4cb7b80406e8f86aafebac20bfbf
wiz <wiz>
2002-10-03 19:36:36 +0000

    Sync with new autoconf213, automake14 packages and autoconf, automake updates.

--- mk/automake.mk
+++ mk/automake.mk
@@ -1,11 +1,11 @@
-# $NetBSD: automake.mk,v 1.1 2002/10/02 18:10:16 dillo Exp $
+# $NetBSD: automake.mk,v 1.2 2002/10/03 19:36:36 wiz Exp $
 #
 # makefile fragment for packages that use automake
 # AUTOMAKE_REQD can be set to the minimum version required.
 # It adds a build dependency on the appropriate automake and autoconf
 # packages and sets the following variables:
-#       AUTOMAKE:       automake binary to use
-#       ACLOCAL:	aclocal binary to use
+#	AUTOMAKE:	automake binary to use
+#	ACLOCAL:	aclocal binary to use
 # autoconf required version will default to 2.13 if automake 1.4* is used,
 # and to 2.50 otherwise.
 #
@@ -17,18 +17,17 @@ AUTOMAKE_MK=	# defined
 AUTOMAKE_REQD?= 1.7
 
 .if ${AUTOMAKE_REQD:M1.4*} == ""
-BUILD_DEPENDS+=		automake>=${AUTOMAKE_REQD}:../../devel/automake-devel
+BUILD_DEPENDS+=		automake>=${AUTOMAKE_REQD}:../../devel/automake
 _AUTOMAKE_API_VERSION=	1.7
 AUTOCONF_REQD?=		2.50
 .else
-BUILD_DEPENDS+=		automake>=${AUTOMAKE_REQD}:../../devel/automake
+BUILD_DEPENDS+=		automake14>=${AUTOMAKE_REQD}:../../devel/automake14
 _AUTOMAKE_API_VERSION=	1.4
 AUTOCONF_REQD?=		2.13
 .endif
 
 AUTOMAKE=		${LOCALBASE}/bin/automake-${_AUTOMAKE_API_VERSION}
 ACLOCAL=		${LOCALBASE}/bin/aclocal-${_AUTOMAKE_API_VERSION}
-#AUTOMAKE_SHAREPATH=	${LOCALBASE}/share/automake-${_AUTOMAKE_API_VERSION}
 
 .include "../../mk/autoconf.mk"
 .endif # AUTOMAKE_MK
