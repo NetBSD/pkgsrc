e19a91d0f0dc48c32cdd006d4b231e6701c23331
jlam <jlam>
2002-10-03 08:04:28 +0000

    Create "DEVOSSSOUND" which is used where we'd like for programs that use the ossaudio emulation to use /dev/sound instead of /dev/audio.  For OSS, DEVOSSSOUND == DEVOSSAUDIO == /dev/dsp.

--- audio/oss/buildlink2.mk
+++ audio/oss/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1 2002/10/03 02:51:12 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/10/03 08:04:28 jlam Exp $
 
 .if !defined(OSS_BUILDLINK2_MK)
 OSS_BUILDLINK2_MK=	# defined
@@ -29,6 +29,7 @@ BUILDLINK_TARGETS+=	oss-buildlink
 
 LIBOSSAUDIO=		# empty
 DEVOSSAUDIO=		/dev/dsp
+DEVOSSSOUND=		${DEVOSSAUDIO}
 
 oss-buildlink: _BUILDLINK_USE
 
