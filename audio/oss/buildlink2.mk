98c1daf130626bf1197da3f3dce820544e877568
jlam <jlam@pkgsrc.org>
2002-10-03 02:51:12 +0000

    Add a buildlink2.mk file.  This file shouldn't ever really need to be included because it will be included indirectly by ossaudio.buildlink2.mk, which should be the only file included by packages that want OSS.

---
+++ audio/oss/buildlink2.mk
@@ -0,0 +1,35 @@
+# $NetBSD: buildlink2.mk,v 1.1 2002/10/03 02:51:12 jlam Exp $
+
+.if !defined(OSS_BUILDLINK2_MK)
+OSS_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=		oss
+BUILDLINK_DEPENDS.oss?=		oss>=3.97d
+BUILDLINK_PKGSRCDIR.oss?=	../../audio/oss
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.oss=oss
+BUILDLINK_PREFIX.oss_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.oss+=	lib/oss/include/sys/awe_voice.h
+BUILDLINK_FILES.oss+=	lib/oss/include/sys/dm.h
+BUILDLINK_FILES.oss+=	lib/oss/include/sys/soundcard.h
+BUILDLINK_FILES.oss+=	lib/oss/include/sys/ultrasound.h
+BUILDLINK_FILES.oss+=	lib/oss/libOSSlib.*
+
+BUILDLINK_CPPFLAGS.oss=	-I${BUILDLINK_PREFIX.oss}/lib/oss/include
+BUILDLINK_LDFLAGS.oss=	-L${BUILDLINK_PREFIX.oss}/lib/oss		\
+			-Wl,-R${BUILDLINK_PREFIX.oss}/lib/oss
+
+BUILDLINK_PREFIX.ossaudio=	${BUILDLINK_PREFIX.oss}/lib/oss
+BUILDLINK_FILES.ossaudio=	include/sys/awe_voice.h
+BUILDLINK_FILES.ossaudio+=	include/sys/dm.h
+BUILDLINK_FILES.ossaudio+=	include/sys/soundcard.h
+BUILDLINK_FILES.ossaudio+=	include/sys/ultrasound.h
+
+BUILDLINK_TARGETS+=	oss-buildlink
+
+LIBOSSAUDIO=		# empty
+DEVOSSAUDIO=		/dev/dsp
+
+oss-buildlink: _BUILDLINK_USE
+
+.endif	# OSS_BUILDLINK2_MK
