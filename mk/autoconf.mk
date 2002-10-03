734a299936c59a91cff0764c02ef21ace8d49871
wiz <wiz@pkgsrc.org>
2002-10-03 19:36:36 +0000

    Sync with new autoconf213, automake14 packages and autoconf, automake updates.

--- mk/autoconf.mk
+++ mk/autoconf.mk
@@ -1,4 +1,4 @@
-# $NetBSD: autoconf.mk,v 1.2 2002/10/02 18:56:47 dillo Exp $
+# $NetBSD: autoconf.mk,v 1.3 2002/10/03 19:36:36 wiz Exp $
 #
 # makefile fragment for packages that use autoconf
 # AUTOCONF_REQD can be set to the minimum version required.
@@ -6,7 +6,7 @@
 # and sets the following variables:
 #	AUTOCONF:	autoconf binary to use
 #	AUTORECONF:	autoreconf binary to use
-#	AUTOHEADER;	autoheader binary to use
+#	AUTOHEADER:	autoheader binary to use
 #
 
 .if !defined(AUTOCONF_MK)
@@ -16,15 +16,15 @@ AUTOCONF_MK=	# defined
 AUTOCONF_REQD?= 2.50
 
 .if ${AUTOCONF_REQD:M2.1[0-9]*} == ""
-BUILD_DEPENDS+=		autoconf>=${AUTOCONF_REQD}:../../devel/autoconf-devel
-AUTOCONF=		${LOCALBASE}/bin/autoconf
-AUTORECONF=		${LOCALBASE}/bin/autoreconf
-AUTOHEADER=		${LOCALBASE}/bin/autoheader
-.else
 BUILD_DEPENDS+=		autoconf>=${AUTOCONF_REQD}:../../devel/autoconf
 AUTOCONF=		${LOCALBASE}/bin/autoconf
 AUTORECONF=		${LOCALBASE}/bin/autoreconf
 AUTOHEADER=		${LOCALBASE}/bin/autoheader
+.else
+BUILD_DEPENDS+=		autoconf213>=${AUTOCONF_REQD}:../../devel/autoconf213
+AUTOCONF=		${LOCALBASE}/bin/autoconf-2.13
+AUTORECONF=		${LOCALBASE}/bin/autoreconf-2.13
+AUTOHEADER=		${LOCALBASE}/bin/autoheader-2.13
 .endif
 
 .endif # AUTOCONF_MK
