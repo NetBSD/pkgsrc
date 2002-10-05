9e5c71c1aa818328df9df0c34dd11186136fcc83
wiz <wiz>
2002-10-05 11:26:59 +0000

    Unused.

--- x11/gnome-python/buildlink.mk
+++
@@ -1,6 +0,0 @@
-# $NetBSD: buildlink.mk,v 1.2 2002/09/10 16:06:53 wiz Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_DEPENDS.pygnome?= ${PYPKGPREFIX}-gnome>=1.4.0nb2
-DEPENDS+= ${BUILDLINK_DEPENDS.pygnome}:../../x11/gnome-python
