d1281e9b9f28e91483db8d360533e36fbef5b123
wiz <wiz@pkgsrc.org>
2002-10-07 21:47:50 +0000

    Move BUILD_USES_GETTEXT_M4 logic to automake.mk.

---
+++ mk/automake.mk
@@ -0,0 +1,37 @@
+# $NetBSD: automake.mk,v 1.3.4.2 2002/10/07 21:47:50 wiz Exp $
+#
+# makefile fragment for packages that use automake
+# AUTOMAKE_REQD can be set to the minimum version required.
+# It adds a build dependency on the appropriate automake and autoconf
+# packages and sets the following variables:
+#	AUTOMAKE:	automake binary to use
+#	ACLOCAL:	aclocal binary to use
+# autoconf required version will default to 2.13 if automake 1.4* is used,
+# and to 2.50 otherwise.
+#
+
+.if !defined(AUTOMAKE_MK)
+AUTOMAKE_MK=	# defined
+
+# minimal required version
+AUTOMAKE_REQD?= 1.7
+
+.if ${AUTOMAKE_REQD:M1.4*} == ""
+BUILD_DEPENDS+=		automake>=${AUTOMAKE_REQD}:../../devel/automake
+_AUTOMAKE_API_VERSION=	1.7
+AUTOCONF_REQD?=		2.50
+.else
+BUILD_DEPENDS+=		automake14>=${AUTOMAKE_REQD}:../../devel/automake14
+_AUTOMAKE_API_VERSION=	1.4
+AUTOCONF_REQD?=		2.13
+.endif
+
+AUTOMAKE=		${LOCALBASE}/bin/automake-${_AUTOMAKE_API_VERSION}
+ACLOCAL=		${LOCALBASE}/bin/aclocal-${_AUTOMAKE_API_VERSION}
+
+.if defined(BUILD_USES_GETTEXT_M4)
+BUILD_DEPENDS+=		{gettext-0.10.35nb1,gettext-m4-[0-9]*}:../../devel/gettext-m4
+.endif
+
+.include "../../mk/autoconf.mk"
+.endif # AUTOMAKE_MK
