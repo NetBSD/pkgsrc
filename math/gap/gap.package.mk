7d4ec9c039fa99a1dbbcaa6124965d121deee1d6
cjep <cjep@pkgsrc.org>
2002-10-08 04:49:41 +0000

    Update of GAP-4.3 (math/gap) to bug fix level 3.
    Corrections since bug fix level 1 include:
     * A problem with the inversion routine for quaternions.
     * A segmentation fault when appending to a length 0 compressed vector
       over GF2.
     * An error in the computation of inverses in quaternion algebras with
       non-standard parameters.
     * A problem in backtrack routines using `Suborbits' if the group has
       fixed points in the range [1..max(Omega)].
     * A problem with the linear algebra methods for Lie algebra cohomology.
     * A Problem with inverting lists of compressed vectors over fields
       of order greater than 2.
     * An error in computing whether an element is in a Green's D equivalence
       class or not.
     * Problems with various functions (including GQuotient, IntScalarProduct,
                    NonnegIntScalarProducts, CharacterTableDirectProduct)
     * Problems with various methods (including IsGreensLessThanOrEqual and
    	GroupHClassOfGreensDClass, AssociatedReesMatrixSemigroupOfDClass,
    	IsZeroSimpleSemigroup, IsomorphismReesMatrixSemigroup and
    	SandwichMatrixOfReesZeroMatrixSemigroup)
    
    XXX A bug has been introduced into GAP which prevents the test suite
        (zmodnz.tst) from working in this version. This will be fixed in
        bug fix 4.
    
    For full details, please see the "description3" file that comes with
    this package in ${PREFIX}/lib/gap4.3.

--- math/gap/gap.package.mk
+++ math/gap/gap.package.mk
@@ -1,7 +1,7 @@
-# $NetBSD: gap.package.mk,v 1.1.1.1 2002/08/21 08:04:05 cjep Exp $
+# $NetBSD: gap.package.mk,v 1.2 2002/10/08 04:49:41 cjep Exp $
 #
 # Set GAP_PKG_NAME in your Makefile. More may appear here later.
 
-DEPENDS+=       	gap-4.3:../../math/gap
+DEPENDS+=       	gap-4.3nb1:../../math/gap
 GAP_DIRECTORY=  	${LOCALBASE}/lib/gap4.3
 GAP_PKG_DIRECTORY=	${GAP_DIRECTORY}/pkg/${GAP_PKG_NAME}
