b32eec7631165b6c7bf5ed93d507f663a63d55fe
jlam <jlam@pkgsrc.org>
2002-10-09 11:40:40 +0000

    Create BUILDLINK_CPPFLAGS and BUILDLINK_LDFLAGS for the python packages so as to find their headers and libraries.  Also allow "-lpython" to resolve to the versioned libpython (-lpython2.2, etc.).

--- lang/python15/buildlink2.mk
+++ lang/python15/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/09/20 22:44:18 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/10/09 11:40:40 jlam Exp $
 
 .if !defined(PYTHON15_BUILDLINK2_MK)
 PYTHON15_BUILDLINK2_MK=	# defined
@@ -18,6 +18,13 @@ BUILDLINK_PREFIX.python15_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.python15!= \
 	${GREP} "^include/" ${.CURDIR}/${BUILDLINK_PKGSRCDIR.python15}/PLIST
 BUILDLINK_FILES.python15+=	lib/python1.5/config/libpython1.5.*
+BUILDLINK_TRANSFORM+=		l:python:python1.5
+
+BUILDLINK_CPPFLAGS.python15+= \
+	-I${BUILDLINK_PREFIX.python15}/include/python1.5
+BUILDLINK_LDFLAGS.python15+= \
+	-L${BUILDLINK_PREFIX.python15}/lib/python1.5/config		\
+	-Wl,-R${BUILDLINK_PREFIX.python15}/lib/python1.5/config
 
 BUILDLINK_TARGETS+=	python15-buildlink
 
