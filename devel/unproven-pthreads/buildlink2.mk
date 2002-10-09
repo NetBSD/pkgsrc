26139e21fa5ddcb0c6f6b1e55d006dca5d3c0acb
jlam <jlam@pkgsrc.org>
2002-10-09 10:27:12 +0000

    Rather than creating wrapper scripts for pgcc and pg++, use them directly and rely on the gcc and g++ called internally to use the buildlink wrappers.  This correctly forces all references to headers and libraries outside of /usr/include and /usr/lib to be within ${BUILDLINK_DIR}.

--- devel/unproven-pthreads/buildlink2.mk
+++ devel/unproven-pthreads/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.4 2002/10/09 09:49:32 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.5 2002/10/09 10:27:12 jlam Exp $
 
 .if !defined(UNPROVEN_PTHREADS_BUILDLINK2_MK)
 UNPROVEN_PTHREADS_BUILDLINK2_MK=	# defined
@@ -11,13 +11,23 @@ BUILDLINK_PKGSRCDIR.unproven-pthreads?=	../../devel/unproven-pthreads
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.unproven-pthreads=unproven-pthreads
 BUILDLINK_PREFIX.unproven-pthreads_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.unproven-pthreads=	pthreads/include/*.h
+BUILDLINK_FILES.unproven-pthreads+=	pthreads/include/*/*.h
+BUILDLINK_FILES.unproven-pthreads+=	pthreads/lib/libpthread.*
 
 BUILDLINK_CPPFLAGS.unproven-pthreads= \
 	-I${BUILDLINK_PREFIX.unproven-pthreads}/pthreads/include
 BUILDLINK_LDFLAGS.unproven-pthreads= \
 	-L${BUILDLINK_PREFIX.unproven-pthreads}/pthreads/lib
 
-CC=	${BUILDLINK_PREFIX.unproven-pthreads}/pthreads/bin/pgcc
-CXX=	${BUILDLINK_PREFIX.unproven-pthreads}/pthreads/bin/pg++
+PGCC=	${BUILDLINK_PREFIX.unproven-pthreads}/pthreads/bin/pgcc
+PGXX=	${BUILDLINK_PREFIX.unproven-pthreads}/pthreads/bin/pg++
+
+BUILDLINK_ENV+=	CC="${PGCC}"
+BUILDLINK_ENV+=	CXX="${PGXX}"
+
+BUILDLINK_TARGETS+=	unproven-pthreads-buildlink
+
+unproven-pthreads-buildlink: _BUILDLINK_USE
 
 .endif	# UNPROVEN_PTHREADS_BUILDLINK2_MK
