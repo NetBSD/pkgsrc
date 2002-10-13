34db2068c4420ebd95c6849dd3f9d734b09d107f
jlam <jlam@pkgsrc.org>
2002-10-13 09:08:15 +0000

    Note that using this package implies needing datasize >= 131204 by adding datasize to UNLIMIT_RESOURCES.

--- lang/sun-jre14/buildlink2.mk
+++ lang/sun-jre14/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1 2002/10/13 07:51:40 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/10/13 09:08:15 jlam Exp $
 
 .if !defined(SUN_JRE14_BUILDLINK2_MK)
 SUN_JRE14_BUILDLINK2_MK=	# defined
@@ -18,6 +18,7 @@ EVAL_PREFIX+=	BUILDLINK_JAVA_PREFIX.sun-jre14=sun-jre14
 BUILDLINK_JAVA_PREFIX.sun-jre14_DEFAULT= \
 	${BUILDLINK_PREFIX.sun-jre14}/java/sun-1.4.0
 
+UNLIMIT_RESOURCES+=	datasize	# must be at least 131204
 BUILDLINK_TARGETS+=	sun-jre14-buildlink
 
 sun-jre14-buildlink: _BUILDLINK_USE
