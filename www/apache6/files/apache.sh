cbccc3827bcace658604042228d0746f5d5c29b5
lukem <lukem@pkgsrc.org>
2002-10-14 00:29:50 +0000

    Only set apache_start to "start" if it's not already set.
    Previously, if apache_start was set in /etc/rc.conf and /etc/rc.d/apache was
    loaded as part of the /etc/rc start sequence, apache_start's value would
    be overridden by "apache_start=start" in this script, because /etc/rc.conf
    would have already been loaded and load_rc_config() would not reload it again.
    
    This problem would not have been seen if /etc/rc.d/apache was started
    manually, or /etc/rc.conf.d/apache or @PKG_SYSCONFDIR@/apache_start.conf
    was used to set apache_start.
    
    (I am using /etc/rc.conf, and was wondering why apache wasn't starting
    with ssl support at boot, but worked after a manual restart...)

--- www/apache6/files/apache.sh
+++ www/apache6/files/apache.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: apache.sh,v 1.16 2002/09/20 02:02:01 grant Exp $
+# $NetBSD: apache.sh,v 1.17 2002/10/14 00:29:51 lukem Exp $
 #
 # PROVIDE: apache
 # REQUIRE: DAEMON
@@ -33,9 +33,9 @@ reload_cmd="apache_doit reload"
 #	/etc/rc.conf.d/apache,
 #
 # in order of increasing precedence.  Its possible values are "start"
-# and "startssl", and defaults to "start".
+# and "startssl", and defaults to "start" unless it's already set.
 #
-apache_start=start
+: ${apache_start:=start}
 if [ -f @PKG_SYSCONFDIR@/apache_start.conf ]
 then
 	. @PKG_SYSCONFDIR@/apache_start.conf
