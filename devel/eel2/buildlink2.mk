59a228c908d9044238f56c1e1e2ba75f6846889f
rh <rh@pkgsrc.org>
2002-10-16 09:37:26 +0000

    Initial import of eel2-2.0.7, the eazel extentions library for GNOME2.

---
+++ devel/eel2/buildlink2.mk
@@ -0,0 +1,31 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2002/10/16 09:37:26 rh Exp $
+#
+# This Makefile fragment is included by packages that use eel2.
+#
+# This file was created automatically using createbuildlink 2.2.
+#
+
+.if !defined(EEL2_BUILDLINK2_MK)
+EEL2_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			eel2
+BUILDLINK_DEPENDS.eel2?=		eel2>=2.0.7
+BUILDLINK_PKGSRCDIR.eel2?=		../../devel/eel2
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.eel2=eel2
+BUILDLINK_PREFIX.eel2_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.eel2+=	include/eel-2/eel/*.h
+BUILDLINK_FILES.eel2+=	lib/libeel-2.*
+BUILDLINK_FILES.eel2+=	lib/pkgconfig/eel-2.0.pc
+
+.include "../../sysutils/gnome-vfs2/buildlink2.mk"
+.include "../../devel/gettext-lib/buildlink2.mk"
+.include "../../devel/libgnomeui/buildlink2.mk"
+.include "../../devel/popt/buildlink2.mk"
+.include "../../devel/gail/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	eel2-buildlink
+
+eel2-buildlink: _BUILDLINK_USE
+
+.endif	# EEL2_BUILDLINK2_MK
