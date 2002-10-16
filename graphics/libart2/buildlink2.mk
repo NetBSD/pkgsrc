59d0831d3b764488845df16e7a74f7b4b051cd6d
rh <rh>
2002-10-16 10:14:38 +0000

    s/USE_X11BASE/USE_X11/ Bump PKGREVISION

--- graphics/libart2/buildlink2.mk
+++ graphics/libart2/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/07 03:58:47 rh Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2002/10/16 10:14:38 rh Exp $
 
 .if !defined(LIBART2_BUILDLINK2_MK)
 LIBART2_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libart2
-BUILDLINK_DEPENDS.libart2?=	libart2>=2.3.10
+BUILDLINK_DEPENDS.libart2?=	libart2>=2.3.10nb1
 BUILDLINK_PKGSRCDIR.libart2?=	../../graphics/libart2
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libart2=libart2
