cb0effb7e678eb06c6b7e3a6591624943bc04e30
rh <rh>
2002-10-16 10:32:39 +0000

    Add buildlink2.mk

---
+++ graphics/librsvg2/buildlink2.mk
@@ -0,0 +1,33 @@
+# $NetBSD: buildlink2.mk,v 1.1 2002/10/16 10:32:39 rh Exp $
+#
+# This Makefile fragment is included by packages that use librsvg2.
+#
+# This file was created automatically using createbuildlink 2.2.
+#
+
+.if !defined(LIBRSVG2_BUILDLINK2_MK)
+LIBRSVG2_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			librsvg2
+BUILDLINK_DEPENDS.librsvg2?=		librsvg2>=2.0.1
+BUILDLINK_PKGSRCDIR.librsvg2?=		../../graphics/librsvg2
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.librsvg2=librsvg2
+BUILDLINK_PREFIX.librsvg2_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.librsvg2+=	include/librsvg-2/librsvg/*.h
+BUILDLINK_FILES.librsvg2+=	lib/librsvg-2.*
+BUILDLINK_FILES.librsvg2+=	lib/pkgconfig/librsvg-2.0.pc
+
+.include "../../graphics/freetype2/buildlink2.mk"
+.include "../../graphics/libart2/buildlink2.mk"
+.include "../../textproc/libxml2/buildlink2.mk"
+.include "../../devel/glib2/buildlink2.mk"
+.include "../../devel/pango/buildlink2.mk"
+.include "../../devel/popt/buildlink2.mk"
+.include "../../x11/gtk2/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	librsvg2-buildlink
+
+librsvg2-buildlink: _BUILDLINK_USE
+
+.endif	# LIBRSVG2_BUILDLINK2_MK
