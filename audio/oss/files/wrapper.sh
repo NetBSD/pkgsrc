bfc2aaa7fee6401ba0d8c2399a732673fe90b584
jlam <jlam@pkgsrc.org>
2002-10-18 13:43:37 +0000

    ossxmix also needs to be wrapped as it may also use ui_X.so.

--- audio/oss/files/wrapper.sh
+++ audio/oss/files/wrapper.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#
+# $NetBSD: wrapper.sh,v 1.1 2002/10/18 13:43:38 jlam Exp $
 
 # ui_X.so expects the GTK+-1.x libraries to be found in /usr/pkg/lib
 if [ -z "${LD_LIBRARY_PATH}" ]; then
@@ -8,4 +10,4 @@ else
 fi
 export LD_LIBRARY_PATH
 
-exec @OSSLIBDIR@/soundconf "$@"
+exec @OSS_WRAPPEE@ "$@"
