d1df701bfb748da138962384acf7d81041c07d34
jlam <jlam>
2002-10-20 07:57:57 +0000

    Honor PKG_SYSCONFDIR.

--- net/openslp/files/slpd.sh
+++ net/openslp/files/slpd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: slpd.sh,v 1.8 2002/10/20 07:56:29 jlam Exp $
+# $NetBSD: slpd.sh,v 1.9 2002/10/20 07:57:57 jlam Exp $
 #
 # Service Location Protocol daemon
 #
@@ -16,7 +16,7 @@ name="slpd"
 rcvar=${name}
 command="@PREFIX@/sbin/${name}"
 pidfile="/var/run/${name}.pid"
-required_files="/etc/slp.conf"
+required_files="@PKG_SYSCONFDIR@/slp.conf"
 
 if [ -f /etc/rc.subr ]
 then
