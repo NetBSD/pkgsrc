38efd55e7708c8e07863d7329ee94dbe84a61b36
jlam <jlam@pkgsrc.org>
2002-10-24 09:54:43 +0000

    If a package uses the UCD-SNMP interface to Net-SNMP, then first check if ucd-snmp is installed.  If it is, then use it, otherwise, use the net-snmp package with the ucd-snmp-compatibility headers and libraries.
    A package signals that it wants to use the UCD-SNMP interface by defining
    USE_UCD_SNMP in the package Makefile.

--- net/net-snmp/buildlink2.mk
+++ net/net-snmp/buildlink2.mk
@@ -1,7 +1,17 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/10/24 09:34:08 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/10/24 09:54:43 jlam Exp $
 
 .if !defined(NET_SNMP_BUILDLINK2_MK)
 NET_SNMP_BUILDLINK2_MK=	# defined
+
+# If a package uses the UCD-SNMP interface to Net-SNMP, then first check
+# if ucd-snmp is installed.  If it is, then use it, otherwise, use the
+# net-snmp package with the ucd-snmp-compatibility headers and libraries.
+#
+.if defined(USE_UCD_SNMP)
+_UCD_SNMP_INSTALLED!=	${PKG_INFO} -e ucd-snmp || ${ECHO}
+.  if !empty(_UCD_SNMP_INSTALLED)
+.    include "../../net/ucd-snmp/buildlink2.mk"
+.  else
 
 BUILDLINK_PACKAGES+=		net-snmp
 BUILDLINK_PKGBASE.net-snmp=	net-snmp
@@ -22,5 +32,8 @@ BUILDLINK_FILES.net-snmp+=		lib/libsnmp.*
 BUILDLINK_TARGETS+=	net-snmp-buildlink
 
 net-snmp-buildlink: _BUILDLINK_USE
+
+.  endif  # _UCD_SNMP_INSTALLED
+.endif	  # USE_UCD_SNMP
 
 .endif	# NET_SNMP_BUILDLINK2_MK
