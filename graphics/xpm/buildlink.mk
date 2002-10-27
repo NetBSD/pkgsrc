3b4dfe284d377ab6cf399a122daa4303844162df
bouyer <bouyer@pkgsrc.org>
2002-10-27 18:00:07 +0000

    On Solaris, if ${X11BASE} != "/usr/openwin" use the builtin XPM if it exists.

--- graphics/xpm/buildlink.mk
+++ graphics/xpm/buildlink.mk
@@ -1,12 +1,21 @@
-# $NetBSD: buildlink2.mk,v 1.4 2002/10/27 18:00:07 bouyer Exp $
+# $NetBSD: buildlink.mk,v 1.15 2002/10/27 18:00:07 bouyer Exp $
+#
+# This Makefile fragment is included by packages that use xpm.
+#
+# To use this Makefile fragment, simply:
+#
+# (1) Include this Makefile fragment in the package Makefile,
+# (2) Add ${BUILDLINK_DIR}/include to the front of the C preprocessor's header
+#     search path, and
+# (3) Add ${BUILDLINK_DIR}/lib to the front of the linker's library search
+#     path.
 
-.if !defined(XPM_BUILDLINK2_MK)
-XPM_BUILDLINK2_MK=	# defined
+.if !defined(XPM_BUILDLINK_MK)
+XPM_BUILDLINK_MK=	# defined
 
-.include "../../mk/bsd.prefs.mk"
+.include "../../mk/bsd.buildlink.mk"
 
-BUILDLINK_DEPENDS.xpm?=		xpm-3.4k
-BUILDLINK_PKGSRCDIR.xpm?=	../../graphics/xpm
+BUILDLINK_DEPENDS.xpm?=	xpm-3.4k
 
 # Check if we got Xpm distributed with XFree86 4.x or Solaris 9 or if we need
 # to depend on the Xpm package.
@@ -29,7 +38,7 @@ _NEED_XPM=		NO
 .endif
 
 .if ${_NEED_XPM} == "YES"
-BUILDLINK_PACKAGES+=	xpm
+DEPENDS+=		${BUILDLINK_DEPENDS.xpm}:../../graphics/xpm
 EVAL_PREFIX+=		BUILDLINK_PREFIX.xpm=xpm
 BUILDLINK_PREFIX.xpm_DEFAULT=	${X11PREFIX}
 .else
@@ -39,8 +48,10 @@ BUILDLINK_PREFIX.xpm=	${X11BASE}
 BUILDLINK_FILES.xpm+=	include/X11/xpm.h
 BUILDLINK_FILES.xpm+=	lib/libXpm.*
 
-BUILDLINK_TARGETS+=	xpm-buildlink
+BUILDLINK_TARGETS.xpm=	xpm-buildlink
+BUILDLINK_TARGETS+=	${BUILDLINK_TARGETS.xpm}
 
+pre-configure: ${BUILDLINK_TARGETS.xpm}
 xpm-buildlink: _BUILDLINK_USE
 
-.endif	# XPM_BUILDLINK2_MK
+.endif	# XPM_BUILDLINK_MK
