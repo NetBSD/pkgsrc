16fc98d715e034e5a598040f2e1a951965db2150
rh <rh@pkgsrc.org>
2002-10-30 04:13:54 +0000

    Update libgnomecanvas to 2.0.4nb1.  Changes include: 	- USE_X11 instead of USE_X11BASE 	- add missing PKGCONFIG_OVERRIDE

--- graphics/libgnomecanvas/buildlink2.mk
+++ graphics/libgnomecanvas/buildlink2.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/09/22 09:52:36 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/10/30 04:13:54 rh Exp $
 
 .if !defined(LIBGNOMECANVAS_BUILDLINK2_MK)
 LIBGNOMECANVAS_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libgnomecanvas
-BUILDLINK_DEPENDS.libgnomecanvas?=	libgnomecanvas>=2.0.4
+BUILDLINK_DEPENDS.libgnomecanvas?=	libgnomecanvas>2.0.4
 BUILDLINK_PKGSRCDIR.libgnomecanvas?=	../../graphics/libgnomecanvas
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libgnomecanvas=libgnomecanvas
-BUILDLINK_PREFIX.libgnomecanvas_DEFAULT=	${X11PREFIX}
+BUILDLINK_PREFIX.libgnomecanvas_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.libgnomecanvas=		include/libgnomecanvas-2.0/libgnomecanvas/*
 BUILDLINK_FILES.libgnomecanvas+=	lib/libglade/2.0/libcanvas.*
 BUILDLINK_FILES.libgnomecanvas+=	lib/libgnomecanvas-2.*
