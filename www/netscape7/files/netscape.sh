720c1a61be9e9ce5ac8f638ae5f5d46a9e017e30
grant <grant>
2002-10-31 03:58:12 +0000

    exec netscape7 directly, as we don't need to return.

--- www/netscape7/files/netscape.sh
+++ www/netscape7/files/netscape.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# $NetBSD: netscape.sh,v 1.2 2002/10/07 09:32:45 grant Exp $
-@PREFIX@/lib/netscape/netscape7/netscape "$@"
+# $NetBSD: netscape.sh,v 1.3 2002/10/31 03:58:12 grant Exp $
+exec @PREFIX@/lib/netscape/netscape7/netscape "$@"
