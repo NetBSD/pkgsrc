8dea8d71809162a54732a1683b40d6db5f29b527
jlam <jlam>
2002-11-01 06:09:36 +0000

    Bump the pth dependency to pth>=1.4.1nb5, which ensures that we have the latest set of fixes and changes to pth to make it look more like a real pthreads package.

--- devel/pth/buildlink2.mk
+++ devel/pth/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 18:38:44 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/11/01 06:09:37 jlam Exp $
 
 .if !defined(PTH_BUILDLINK2_MK)
 PTH_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		pth
-BUILDLINK_DEPENDS.pth?=		pth>=1.4.0
+BUILDLINK_DEPENDS.pth?=		pth>=1.4.1.nb5
 BUILDLINK_PKGSRCDIR.pth?=	../../devel/pth
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.pth=pth
