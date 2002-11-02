2226b9db7e6f5b1299151ae9635a25dba2067751
rh <rh@pkgsrc.org>
2002-11-02 02:08:01 +0000

    Add SCROLLKEEPER_DATADIR definition for use by dependent packages.

--- textproc/scrollkeeper/buildlink2.mk
+++ textproc/scrollkeeper/buildlink2.mk
@@ -1,7 +1,7 @@
-# $NetBSD: buildlink2.mk,v 1.1 2002/08/28 16:35:37 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/11/02 02:08:01 rh Exp $
 
 .if !defined(SCROLLKEEPER_BUILDLINK2_MK)
-SCROLLKEEPER_BUILDLINK2_MK=	# defined
+SCROLLKEEPER_BUILDLINK2_MK=		# defined
 
 BUILDLINK_PACKAGES+=			scrollkeeper
 BUILDLINK_DEPENDS.scrollkeeper?=	scrollkeeper>=0.2
@@ -9,9 +9,15 @@ BUILDLINK_PKGSRCDIR.scrollkeeper?=	../../textproc/scrollkeeper
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.scrollkeeper=scrollkeeper
 BUILDLINK_PREFIX.scrollkeeper_DEFAULT=	${X11PREFIX}
-BUILDLINK_FILES.scrollkeeper+=	lib/libscrollkeeper.*
+BUILDLINK_FILES.scrollkeeper+=		lib/libscrollkeeper.*
 
-BUILDLINK_TARGETS+=	scrollkeeper-buildlink
+BUILDLINK_TARGETS+=			scrollkeeper-buildlink
+
+.if exists(${LOCALBASE}/libdata/scrollkeeper)
+SCROLLKEEPER_DATADIR=			${LOCALBASE}/libdata
+.else
+SCROLLKEEPER_DATADIR=			${BUILDLINK_PREFIX.scrollkeeper}/var/lib
+.endif
 
 scrollkeeper-buildlink: _BUILDLINK_USE
 
