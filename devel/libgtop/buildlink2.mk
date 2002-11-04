02196e2a96314872f328a7f20e1f62e61f11f585
rh <rh@pkgsrc.org>
2002-11-04 00:48:32 +0000

    Update libgtop to 1.0.13.  Changes include: 	- a security fix we already had in patch-af 	- some additional localisations 	- moved header files to allow co-existence with GNOME2 	- some minor pkglint cleanups

--- devel/libgtop/buildlink2.mk
+++ devel/libgtop/buildlink2.mk
@@ -1,20 +1,21 @@
-# $NetBSD: buildlink2.mk,v 1.1 2002/10/06 13:59:22 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/11/04 00:48:32 rh Exp $
+#
+# This Makefile fragment is included by packages that use libgtop.
+#
+# This file was created automatically using createbuildlink 2.2.
 #
 
 .if !defined(LIBGTOP_BUILDLINK2_MK)
 LIBGTOP_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			libgtop
-BUILDLINK_DEPENDS.libgtop?=		libgtop>=1.0.12nb3
+BUILDLINK_DEPENDS.libgtop?=		libgtop>=1.0.13
 BUILDLINK_PKGSRCDIR.libgtop?=		../../devel/libgtop
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libgtop=libgtop
 BUILDLINK_PREFIX.libgtop_DEFAULT=	${X11PREFIX}
-BUILDLINK_FILES.libgtop+=	include/glibtop.h
-BUILDLINK_FILES.libgtop+=	include/glibtop/*.h
-BUILDLINK_FILES.libgtop+=	include/glibtop_machine.h
-BUILDLINK_FILES.libgtop+=	include/glibtop_server.h
-BUILDLINK_FILES.libgtop+=	include/glibtop_suid.h
+BUILDLINK_FILES.libgtop+=	include/libgtop-1.0/*.h
+BUILDLINK_FILES.libgtop+=	include/libgtop-1.0/glibtop/*.h
 BUILDLINK_FILES.libgtop+=	lib/libgtop-features.def
 BUILDLINK_FILES.libgtop+=	lib/libgtop.*
 BUILDLINK_FILES.libgtop+=	lib/libgtopConf.sh
@@ -23,6 +24,9 @@ BUILDLINK_FILES.libgtop+=	lib/libgtop_names.*
 BUILDLINK_FILES.libgtop+=	lib/libgtop_suid_common.*
 BUILDLINK_FILES.libgtop+=	lib/libgtop_sysdeps.*
 BUILDLINK_FILES.libgtop+=	lib/libgtop_sysdeps_suid.*
+
+.include "../../devel/gettext-lib/buildlink2.mk"
+.include "../../x11/gnome-libs/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	libgtop-buildlink
 
