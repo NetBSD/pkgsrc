6a92172f988bb7bc636521896c331bed45743e17
seb <seb@pkgsrc.org>
2002-11-04 17:07:24 +0000

    Fix USE_UCD_SNMP behaviour: previously net-snmp package was never considered even with USE_UCD_SNMP not defined. Include bsd.prefs.mk so PKG_INFO is defined.

--- net/net-snmp/buildlink2.mk
+++ net/net-snmp/buildlink2.mk
@@ -1,7 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/10/24 09:54:43 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2002/11/04 17:07:24 seb Exp $
 
 .if !defined(NET_SNMP_BUILDLINK2_MK)
 NET_SNMP_BUILDLINK2_MK=	# defined
+
+_NEED_NET_SNMP=	YES
+
+.include "../../mk/bsd.prefs.mk"
 
 # If a package uses the UCD-SNMP interface to Net-SNMP, then first check
 # if ucd-snmp is installed.  If it is, then use it, otherwise, use the
@@ -11,8 +15,11 @@ NET_SNMP_BUILDLINK2_MK=	# defined
 _UCD_SNMP_INSTALLED!=	${PKG_INFO} -e ucd-snmp || ${ECHO}
 .  if !empty(_UCD_SNMP_INSTALLED)
 .    include "../../net/ucd-snmp/buildlink2.mk"
-.  else
+_NEED_NET_SNMP=	NO
+.  endif  # _UCD_SNMP_INSTALLED
+.endif		
 
+.if ${_NEED_NET_SNMP} == "YES"
 BUILDLINK_PACKAGES+=		net-snmp
 BUILDLINK_PKGBASE.net-snmp=	net-snmp
 BUILDLINK_DEPENDS.net-snmp?=	net-snmp>=5.0.6
@@ -32,8 +39,6 @@ BUILDLINK_FILES.net-snmp+=		lib/libsnmp.*
 BUILDLINK_TARGETS+=	net-snmp-buildlink
 
 net-snmp-buildlink: _BUILDLINK_USE
-
-.  endif  # _UCD_SNMP_INSTALLED
-.endif	  # USE_UCD_SNMP
+.endif	  # _NEED_NET_SNMP
 
 .endif	# NET_SNMP_BUILDLINK2_MK
