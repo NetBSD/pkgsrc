a9d177aa4659226e609cf182610d0a2bc9e60d99
wiz <wiz>
2002-11-13 14:14:27 +0000

    Fix DEPENDS line (5.5.1.6 instead of 5.5.6.1, oops).

--- graphics/ImageMagick/buildlink2.mk
+++ graphics/ImageMagick/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/11/13 14:01:20 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2002/11/13 14:14:27 wiz Exp $
 
 .if !defined(IMAGEMAGICK_BUILDLINK2_MK)
 IMAGEMAGICK_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			ImageMagick
-BUILDLINK_DEPENDS.ImageMagick?=		ImageMagick>=5.5.6.1
+BUILDLINK_DEPENDS.ImageMagick?=		ImageMagick>=5.5.1.6
 BUILDLINK_PKGSRCDIR.ImageMagick?=	../../graphics/ImageMagick
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.ImageMagick=ImageMagick
