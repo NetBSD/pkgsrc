a35c786e99664b71dd996c0061666a0cd3a287ba
wiz <wiz@pkgsrc.org>
2002-11-13 14:01:20 +0000

    Depend on latest, and add jasper buildlink2.

--- graphics/ImageMagick/buildlink2.mk
+++ graphics/ImageMagick/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:22:44 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/11/13 14:01:20 wiz Exp $
 
 .if !defined(IMAGEMAGICK_BUILDLINK2_MK)
 IMAGEMAGICK_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			ImageMagick
-BUILDLINK_DEPENDS.ImageMagick?=		ImageMagick>=5.3.9nb1
+BUILDLINK_DEPENDS.ImageMagick?=		ImageMagick>=5.5.6.1
 BUILDLINK_PKGSRCDIR.ImageMagick?=	../../graphics/ImageMagick
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.ImageMagick=ImageMagick
@@ -16,6 +16,7 @@ BUILDLINK_FILES.ImageMagick+=	lib/libMagick.*
 BUILDLINK_FILES.ImageMagick+=	lib/libMagick++.*
 
 .include "../../archivers/bzip2/buildlink2.mk"
+.include "../../graphics/jasper/buildlink2.mk"
 .include "../../graphics/jpeg/buildlink2.mk"
 .include "../../graphics/png/buildlink2.mk"
 .include "../../graphics/tiff/buildlink2.mk"
