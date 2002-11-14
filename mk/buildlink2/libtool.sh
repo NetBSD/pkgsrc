b717574b2cc4eee6e8712a0a929e3a19e3f3b39d
jlam <jlam>
2002-11-14 21:45:21 +0000

    Allow for common case where --mode=install is not specified when using libtool to install libraries or programs.

--- mk/buildlink2/libtool.sh
+++ mk/buildlink2/libtool.sh
@@ -1,6 +1,6 @@
 #!@BUILDLINK_SHELL@
 #
-# $NetBSD: libtool.sh,v 1.5 2002/09/28 23:46:42 jlam Exp $
+# $NetBSD: libtool.sh,v 1.6 2002/11/14 21:45:21 jlam Exp $
 
 Xsed='@SED@ -e 1s/^X//'
 sed_quote_subst='s/\([\\`\\"$\\\\]\)/\\\1/g'
@@ -16,57 +16,69 @@ updatecache=${BUILDLINK_UPDATE_CACHE-yes}
 
 cmd="@WRAPPEE@"
 lafile=
-while [ $# -gt 0 ]; do
+doinstall=
+case "$1" in
+*install|*cp|*install-sh|*install.sh)
 	arg="$1"; shift
-	case $arg in
-	--fix-la)
-		case "$1" in
-		*.la)
-			lafile="$1"
-		 	. $libtool_fix_la
-			exit
+	doinstall=yes
+	;;
+*)
+	while [ $# -gt 0 ]; do
+		arg="$1"; shift
+		case $arg in
+		--fix-la)
+			case "$1" in
+			*.la)
+				lafile="$1"
+			 	. $libtool_fix_la
+				exit
+				;;
+			esac
+			;;
+		--mode|--mode=install)
+			if [ "$arg" = "--mode=install" ] || \
+			   [ "$arg" = "--mode" -a "$1" = "install" ]; then
+				doinstall=yes
+				break
+			fi
+			;;
+		-o)
+			case "$1" in
+			*.la) lafile="$1" ;;
+			esac
+			;;
+		*)
+			cacheupdated=
+			. $logic
+			case "$cacheupdated" in
+			yes) @CAT@ $pre_cache $cache $post_cache > $logic ;;
+			esac
 			;;
 		esac
-		;;
-	--mode|--mode=install)
-		if [ "$arg" = "--mode=install" ] || \
-		   [ "$arg" = "--mode" -a "$1" = "install" ]; then
-			args="$args $arg"
-			while [ $# -gt 0 ]; do
-				arg="$1"; shift
-				case $arg in
-				*[\`\"\$\\]*)
-					arg=`@ECHO@ X$arg | \
-						$Xsed -e "$sed_quote_subst" \
-					`
-					;;
-				esac
-				case $arg in
-				*[\[\~\#\^\&\*\(\)\{\}\|\;\<\>\?\'\ \	]*|*]*|"")
-					arg="\"$arg\""
-					;;
-				esac
-				args="$args $arg"
-			done
-			break
-		fi
-		;;
-	-o)
-		case "$1" in
-		*.la) lafile="$1" ;;
-		esac
-		;;
-	*)
-		cacheupdated=
-		. $logic
-		case "$cacheupdated" in
-		yes) @CAT@ $pre_cache $cache $post_cache > $logic ;;
-		esac
-		;;
-	esac
-	. $specificlogic
+		. $specificlogic
+		args="$args $arg"
+	done
+	;;
+esac
+if [ -n "$doinstall" ]; then
 	args="$args $arg"
-done
+	while [ $# -gt 0 ]; do
+		arg="$1"; shift
+		case $arg in
+		*[\`\"\$\\]*)
+			arg=`@ECHO@ X$arg | \
+				$Xsed -e "$sed_quote_subst" \
+			`
+			;;
+		esac
+		case $arg in
+		*[\[\~\#\^\&\*\(\)\{\}\|\;\<\>\?\'\ \	]*|*]*|"")
+			arg="\"$arg\""
+			;;
+		esac
+		args="$args $arg"
+	done
+fi
 cmd="$cmd $args"
 
 @_BLNK_WRAP_ENV@
