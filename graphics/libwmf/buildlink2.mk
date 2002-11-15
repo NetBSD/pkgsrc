9138b8064a2c553b7af04863f368ad2b795451d1
wiz <wiz@pkgsrc.org>
2002-11-15 17:50:24 +0000

    Update to 0.2.7. Changes: * update Fontmap[.GS] detection (guesswork, really) (fjf) * don't include trio.h when --disable-heavy is used (fjf) * remember to update both version numbers in configure.ac! * move player.h's defs into metadefs.h for recorder.c (fjf) * add some missing "extern"s (c. Jeremy Shane Sanders) * added recorder.h & libwmf/canvas.h; call it Canvas now (fjf)

--- graphics/libwmf/buildlink2.mk
+++ graphics/libwmf/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.4 2002/09/11 11:19:56 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.5 2002/11/15 17:50:24 wiz Exp $
 
 .if !defined(LIBWMF_BUILDLINK2_MK)
 LIBWMF_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libwmf
-BUILDLINK_DEPENDS.libwmf?=	libwmf>=0.2.6
+BUILDLINK_DEPENDS.libwmf?=	libwmf>=0.2.7
 BUILDLINK_PKGSRCDIR.libwmf?=	../../graphics/libwmf
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.libwmf=libwmf
