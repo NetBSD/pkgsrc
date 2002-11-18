53c2a76c7c00725a00dee908e1ef72d49e924cc9
jlam <jlam>
2002-11-18 07:49:24 +0000

    Alter Mesa/GL packages so that they may always be installed if X11PREFIX != X11BASE (xpkgwedge is installed).  Introduce a new variable MESA_REQD that defaults to "3.4.2" and represents the version of Mesa/GL needed by a package.  MESA_REQD is intended to be used by package Makefiles or by buildlink2.mk files.
    It should now be possible to update this package to the latest release
    (5.0), and have it work on:
    
    	* XF86-3.x with or without xpkgwedge
    	* XF86-4.x with xpkgwedge

--- graphics/Mesa/buildlink2.mk
+++ graphics/Mesa/buildlink2.mk
@@ -1,8 +1,9 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 18:38:55 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/11/18 07:49:24 jlam Exp $
 
 .if !defined(MESA_BUILDLINK2_MK)
 MESA_BUILDLINK2_MK=	# defined
 
+MESA_REQD?=		3.4.2
 BUILDLINK_PREFIX.Mesa=	${BUILDLINK_PREFIX.MesaLib}
 
 .include "../../graphics/MesaLib/buildlink2.mk"
