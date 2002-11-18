571e507c6ca4f36cb77ccc72489f507b4cc49ef2
schmonz <schmonz@pkgsrc.org>
2002-11-18 06:25:51 +0000

    Update to 0.1.7. Changes from 0.1.6a:
    Fix up error codes for darwin port.
    Make sure short transfers are allowed on BSD port.
    Fix bug when finding new busses during usb_find_busses().
    Added two new functions: usb_get_string() and usb_get_string_simple().
    Documentation updates.
    Added LICENSE file.
    Various other cleanups.

--- devel/libusb/buildlink2.mk
+++ devel/libusb/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:22:33 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/11/18 06:25:51 schmonz Exp $
 
 .if !defined(LIBUSB_BUILDLINK2_MK)
 LIBUSB_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libusb
-BUILDLINK_DEPENDS.libusb?=	libusb>=0.1.5
+BUILDLINK_DEPENDS.libusb?=	libusb>=0.1.7
 BUILDLINK_PKGSRCDIR.libusb?=	../../devel/libusb
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.libusb=libusb
