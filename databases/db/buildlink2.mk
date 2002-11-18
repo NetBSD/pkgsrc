07d35ec942253833ebd9571cc1aa9f720dc8a7f5
jlam <jlam@pkgsrc.org>
2002-11-18 20:49:17 +0000

    Allow this file to be used to mean "we want DB-1.85" if the inclusion of this file is preceded by defining "USE_DB185".  On NetBSD (and probably Linux), where we already have DB-1.85 in the base system, this defaults to using the system libraries and headers.

--- databases/db/buildlink2.mk
+++ databases/db/buildlink2.mk
@@ -1,18 +1,33 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/26 22:34:09 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2002/11/18 20:49:17 jlam Exp $
 
 .if !defined(DB_BUILDLINK2_MK)
 DB_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=		db
 BUILDLINK_DEPENDS.db?=		db>=2.7.3
 BUILDLINK_PKGSRCDIR.db?=	../../databases/db
 
-EVAL_PREFIX+=		BUILDLINK_PREFIX.db=db
+.if defined(USE_DB185)
+.  if exists(/usr/include/db.h)		# NetBSD
+BUILDLINK_PREFIX.db=		/usr
+BUILDLINK_FILES.db=		include/db.h
+BUILDLINK_FILES.db+=		include/mpool.h
+BUILDLINK_CPPFLAGS.db=		# empty
+.  elif exists(/usr/include/db1/db.h)	# Linux
+BUILDLINK_PREFIX.db=		/usr
+BUILDLINK_FILES.db=		include/db1/*
+BUILDLINK_CPPFLAGS.db=		-I${BUILDLINK_PREFIX.db}/include/db1
+BUILDLINK_TRANSFORM+=		l:db:db1
+.  else
+BUILDLINK_PACKAGES+=		db
+EVAL_PREFIX+=			BUILDLINK_PREFIX.db=db
 BUILDLINK_PREFIX.db_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.db=	include/db2/*
-BUILDLINK_FILES.db+=	lib/libdb2.*
-
-BUILDLINK_CPPFLAGS.db=	-I${BUILDLINK_PREFIX.db}/include/db2
+BUILDLINK_FILES.db=		include/db2/*
+BUILDLINK_FILES.db+=		lib/libdb2.*
+BUILDLINK_CPPFLAGS.db=		-I${BUILDLINK_PREFIX.db}/include/db2
+BUILDLINK_TRANSFORM+=		l:db:db2
+.  endif
+CPPFLAGS+=			${BUILDLINK_CPPFLAGS.db}
+.endif
 
 BUILDLINK_TARGETS+=	db-buildlink
 
