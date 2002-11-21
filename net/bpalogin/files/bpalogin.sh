6537848186cb52af2d5fb979890f152260011acf
grant <grant@pkgsrc.org>
2002-11-21 10:02:56 +0000

    - fix to run as daemon properly (from Sam Uong in pkg/19086) - convert to bsd.pkg.install.mk rc.d script and example   config installation - bump PKGREVISION

--- net/bpalogin/files/bpalogin.sh
+++ net/bpalogin/files/bpalogin.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!@RCD_SCRIPTS_SHELL@
 #
-#	$NetBSD: bpalogin,v 1.1.1.1 2002/04/12 08:20:32 rh Exp $
+#	$NetBSD: bpalogin.sh,v 1.1 2002/11/21 10:02:57 grant Exp $
 #
 # bpalogin startup script
 #
@@ -16,7 +16,7 @@ name="bpalogin"
 rcvar=${name}
 command="@PREFIX@/sbin/${name}"
 command_args="-c @PKG_SYSCONFDIR@/${name}.conf & sleep 1"
-required_files="${command} @PKG_SYSCONFDIR@/${name}.conf"
+required_files="@PKG_SYSCONFDIR@/${name}.conf"
 
 if [ -f /etc/rc.subr ]
 then
