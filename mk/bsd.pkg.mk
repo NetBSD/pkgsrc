57bff61e16ace8edb0a48e7767e081c0489c0659
jschauma <jschauma@pkgsrc.org>
2002-11-23 22:53:38 +0000

    Bump PKGTOOLS_REQD to 20021123 to prevent users from using tools that could not correctly de-install packages if cwd is part of PLIST of the to be deleted package.  This closes PR pkg/18384.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1087 2002/11/23 10:19:29 dillo Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1088 2002/11/23 22:53:38 jschauma Exp $
 #
 # This file is in the public domain.
 #
@@ -606,7 +606,7 @@ uptodate-digest:
 	fi
 
 # Latest version of pkgtools required for this file.
-PKGTOOLS_REQD=		20020402
+PKGTOOLS_REQD=		20021123
 
 # Check that we are using up-to-date pkg_* tools with this file.
 .if defined(ZOULARIS_VERSION)
