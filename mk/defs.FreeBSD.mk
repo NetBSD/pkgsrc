323ac6541561ac5c17b830657d25cedae52fe40f
jdolecek <jdolecek@pkgsrc.org>
2002-11-24 09:17:44 +0000

    introduce PKG_HAVE_KQUEUE variable - defined if OS supports kqueue(2)/kevent(2) interface

--- mk/defs.FreeBSD.mk
+++ mk/defs.FreeBSD.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.FreeBSD.mk,v 1.2 2002/11/17 15:14:18 grant Exp $
+# $NetBSD: defs.FreeBSD.mk,v 1.3 2002/11/24 09:17:44 jdolecek Exp $
 #
 # Variable definitions for the FreeBSD operating system.
 
@@ -106,3 +106,7 @@ _STRIPFLAG_INSTALL?=	-s	# install(1) option to strip
 DEFAULT_SERIAL_DEVICE?=	/dev/null
 SERIAL_DEVICES?=	/dev/null
 
+# check for kqueue(2) support
+.if exists(/usr/include/sys/event.h)
+PKG_HAVE_KQUEUE=	# defined
+.endif
