9a50e4c655bf3134d8b9a7cbbc488f8bb9224655
jlam <jlam@pkgsrc.org>
2002-11-25 18:21:02 +0000

    FLTK headers should also be found under <include/Fl/*>.

--- x11/fltk/buildlink2.mk
+++ x11/fltk/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 18:40:29 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/11/25 18:21:02 jlam Exp $
 
 .if !defined(FLTK_BUILDLINK2_MK)
 FLTK_BUILDLINK2_MK=	# defined
@@ -10,6 +10,7 @@ BUILDLINK_PKGSRCDIR.fltk?=	../../x11/fltk
 EVAL_PREFIX+=		BUILDLINK_PREFIX.fltk=fltk
 BUILDLINK_PREFIX.fltk_DEFAULT=	${X11PREFIX}
 BUILDLINK_FILES.fltk=	include/FL/*
+BUILDLINK_FILES.fltk+=	include/Fl/*
 BUILDLINK_FILES.fltk+=	lib/libfltk.*
 
 .include "../../graphics/Mesa/buildlink2.mk"
