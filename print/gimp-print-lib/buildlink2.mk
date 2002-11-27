b66a98a50c8ca88a05ca9bfd3b4d9a5d37e904c8
drochner <drochner@pkgsrc.org>
2002-11-27 16:12:35 +0000

    update gimp-print to 2.4.2 changes: -support for HP DesignJet large-format plotters -support fot Epson multi-function devices and MJ-930C -manpage improvements -support for transverse versions of the US CAD standard paper sizes -bugfixes

--- print/gimp-print-lib/buildlink2.mk
+++ print/gimp-print-lib/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/10/24 14:26:28 drochner Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/11/27 16:12:37 drochner Exp $
 
 .if !defined(GIMP_PRINT_BUILDLINK2_MK)
 GIMP_PRINT_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=	gimp-print-lib
-BUILDLINK_DEPENDS.gimp-print-lib?=	gimp-print-lib-4.2.3
+BUILDLINK_DEPENDS.gimp-print-lib?=	gimp-print-lib-4.2.4
 BUILDLINK_PKGSRCDIR.gimp-print-lib?=	../../print/gimp-print-lib
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gimp-print-lib=gimp-print-lib
