38e34b3b98fa11123ce15a210aee32f4411ace2d
agc <agc>
2002-12-03 18:57:52 +0000

    Abstract out the base value of libtool required into _OPSYS_LIBTOOL_REQD, and use that rather than a hardcoded ${OPSYS} default in bsd.pkg.mk

--- mk/defs.Darwin.mk
+++ mk/defs.Darwin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.Darwin.mk,v 1.32 2002/11/23 04:43:05 schmonz Exp $
+# $NetBSD: defs.Darwin.mk,v 1.33 2002/12/03 18:57:52 agc Exp $
 #
 # Variable definitions for the Darwin operating system.
 
@@ -82,6 +82,7 @@ _IMAKE_MAKE=		${MAKE}	# program which gets invoked by imake
 _OPSYS_HAS_GMAKE=	yes	# GNU make is standard
 _OPSYS_HAS_MANZ=	yes	# MANZ controls gzipping of man pages
 _OPSYS_HAS_OSSAUDIO=	no	# libossaudio is available
+_OPSYS_LIBTOOL_REQD=	1.4.20010614nb9 # base version of libtool required
 _PATCH_BACKUP_ARG=	-V simple -b -z	# switch to patch(1) for backup suffix
 _PREFORMATTED_MAN_DIR=	cat	# directory where catman pages are
 _USE_RPATH=		no	# don't add rpath to LDFLAGS
