c03f0654dbe0c6b9ebb797a01697d96cc29fd7f6
drochner <drochner@pkgsrc.org>
2002-12-05 19:10:42 +0000

    update to 0.93.36 changes to the pkg: -don't bother sharing patches with the Motif-1.2 compatible version  anymore -- most patches were only necessary to be able to share  patches, so it got ridicoulous -kick out use of autoconf et al. - just unneeded complexity -comply to Motif-2.1 instead of 2.0 lesstif changes: -added XmSimpleSpinBox implementation -Initial implementation of the CareParentVisual Trait -Start implementing XmRenderTable, XmRendition, XmTabList -misc fixes/cleanups

--- x11/lesstif/buildlink2.mk
+++ x11/lesstif/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 18:40:38 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/12/05 19:10:42 drochner Exp $
 
 .if !defined(LESSTIF_BUILDLINK2_MK)
 LESSTIF_BUILDLINK2_MK=	# defined
@@ -9,9 +9,11 @@ BUILDLINK_PKGSRCDIR.lesstif?=	../../x11/lesstif
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.lesstif=lesstif
 BUILDLINK_PREFIX.lesstif_DEFAULT=	${X11PREFIX}
-BUILDLINK_FILES.lesstif=	include/Mrm/*
+BUILDLINK_FILES.lesstif=	include/Dt/*
+BUILDLINK_FILES.lesstif+=	include/Mrm/*
 BUILDLINK_FILES.lesstif+=	include/Xm/*
 BUILDLINK_FILES.lesstif+=	include/uil/*
+BUILDLINK_FILES.lesstif+=	lib/libDtPrint.*
 BUILDLINK_FILES.lesstif+=	lib/libMrm.*
 BUILDLINK_FILES.lesstif+=	lib/libUil.*
 BUILDLINK_FILES.lesstif+=	lib/libXm.*
