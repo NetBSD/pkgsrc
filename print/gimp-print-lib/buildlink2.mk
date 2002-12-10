d29ddd8fcd25bfa088bc9b11311d1d95d578f934
drochner <drochner>
2002-12-10 18:37:45 +0000

    pull in gettext-lib/buildlink2.mk; this fixes the build problem for gimp-print-ijs (PR pkg/19323)

--- print/gimp-print-lib/buildlink2.mk
+++ print/gimp-print-lib/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/11/27 16:12:37 drochner Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2002/12/10 18:37:45 drochner Exp $
 
 .if !defined(GIMP_PRINT_BUILDLINK2_MK)
 GIMP_PRINT_BUILDLINK2_MK=	# defined
@@ -11,6 +11,8 @@ EVAL_PREFIX+=	BUILDLINK_PREFIX.gimp-print-lib=gimp-print-lib
 BUILDLINK_PREFIX.gimp-print-lib_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.gimp-print-lib=		include/gimp-print/*
 BUILDLINK_FILES.gimp-print-lib+=	lib/libgimpprint.*
+
+.include "../../devel/gettext-lib/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	gimp-print-lib-buildlink
 
