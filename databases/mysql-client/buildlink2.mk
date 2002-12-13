03c6e6a40091df08ecaeddd21fcc2869e689c46f
tron <tron@pkgsrc.org>
2002-12-13 14:19:54 +0000

    Fix security problem in MySQL client library and server which were recently discovered by e-matters.

--- databases/mysql-client/buildlink2.mk
+++ databases/mysql-client/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/10/08 23:00:51 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2002/12/13 14:19:54 tron Exp $
 
 .if !defined(MYSQL_CLIENT_BUILDLINK2_MK)
 MYSQL_CLIENT_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			mysql-client
-BUILDLINK_DEPENDS.mysql-client?=	mysql-client>=3.22
+BUILDLINK_DEPENDS.mysql-client?=	mysql-client>=3.23.49nb2
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql-client
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.mysql-client=mysql-client
