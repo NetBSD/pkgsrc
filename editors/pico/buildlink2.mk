0c9230096e6266a44508229acc11385dd10a9aa4
tron <tron@pkgsrc.org>
2002-12-14 10:28:41 +0000

    Update "pico" package to version 4.4. Changes since version 4.2: - Pico only preserves the start-stop characters (^Q/^S) if given the -p - New pico option -Q <quotestr>

--- editors/pico/buildlink2.mk
+++ editors/pico/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.1 2002/10/08 23:31:02 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/12/14 10:28:42 tron Exp $
 
 .if !defined(PICO_BUILDLINK2_MK)
 PICO_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		pico
-BUILDLINK_DEPENDS.pico?=	pico>=4.2
+BUILDLINK_DEPENDS.pico?=	pico>=4.4
 BUILDLINK_PKGSRCDIR.pico?=	../../editors/pico
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.pico=pico
