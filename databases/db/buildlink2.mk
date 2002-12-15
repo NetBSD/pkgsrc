73d1139431aa03fba89fbcc6e76edadfb4532556
seb <seb>
2002-12-15 01:48:45 +0000

    If USE_DB185 is _not_ defined we actually want db2: introduce _NEED_DB2 to help sort this out.

--- databases/db/buildlink2.mk
+++ databases/db/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.4 2002/11/18 20:49:17 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.5 2002/12/15 01:48:45 seb Exp $
 
 .if !defined(DB_BUILDLINK2_MK)
 DB_BUILDLINK2_MK=	# defined
@@ -6,18 +6,25 @@ DB_BUILDLINK2_MK=	# defined
 BUILDLINK_DEPENDS.db?=		db>=2.7.3
 BUILDLINK_PKGSRCDIR.db?=	../../databases/db
 
+_NEED_DB2=			YES
+
 .if defined(USE_DB185)
 .  if exists(/usr/include/db.h)		# NetBSD
 BUILDLINK_PREFIX.db=		/usr
 BUILDLINK_FILES.db=		include/db.h
 BUILDLINK_FILES.db+=		include/mpool.h
 BUILDLINK_CPPFLAGS.db=		# empty
+_NEED_DB2=			NO
 .  elif exists(/usr/include/db1/db.h)	# Linux
 BUILDLINK_PREFIX.db=		/usr
 BUILDLINK_FILES.db=		include/db1/*
 BUILDLINK_CPPFLAGS.db=		-I${BUILDLINK_PREFIX.db}/include/db1
 BUILDLINK_TRANSFORM+=		l:db:db1
-.  else
+_NEED_DB2=			NO
+.  endif
+.endif
+
+.if ${_NEED_DB2} == "YES"
 BUILDLINK_PACKAGES+=		db
 EVAL_PREFIX+=			BUILDLINK_PREFIX.db=db
 BUILDLINK_PREFIX.db_DEFAULT=	${LOCALBASE}
@@ -25,9 +32,8 @@ BUILDLINK_FILES.db=		include/db2/*
 BUILDLINK_FILES.db+=		lib/libdb2.*
 BUILDLINK_CPPFLAGS.db=		-I${BUILDLINK_PREFIX.db}/include/db2
 BUILDLINK_TRANSFORM+=		l:db:db2
-.  endif
-CPPFLAGS+=			${BUILDLINK_CPPFLAGS.db}
 .endif
+CPPFLAGS+=			${BUILDLINK_CPPFLAGS.db}
 
 BUILDLINK_TARGETS+=	db-buildlink
 
