93cbbef16094dca071f4fe1375be17bb6265dace
schmonz <schmonz>
2002-12-20 18:13:52 +0000

    Import autoconf'd pkg_install source from othersrc/bootstrap-pkgsrc/pkg_install into ${FILESDIR}.
    Using autoconf greatly simplifies maintenance of this package across
    multiple platforms. Placing the extracted source under ${FILESDIR}
    will eventually let us use the same source for bootstrapping as
    well, though we're not ready for this yet.

---
+++ pkgtools/pkg_install/files/lib/setenv.c
@@ -0,0 +1,74 @@
+/*	$NetBSD: setenv.c,v 1.1.1.1 2002/12/20 18:14:06 schmonz Exp $	*/
+
+/*-
+ * Copyright (c) 1998 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Christos Zoulas.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <syslog.h>
+
+#ifndef HAVE_SETENV
+int
+setenv(name, value, overwrite)
+	const char *name;
+	const char *value;
+	int overwrite;
+{
+	size_t  len;
+	int     ret;
+	char   *p;
+
+	if (overwrite == 0 && getenv(name))
+		return 0;
+
+	if (value == NULL)
+	    value = "";
+
+	len = strlen(name) + strlen(value) + 2;
+	p = (char *) malloc(len);
+	if (p == NULL)
+		return -1;
+
+	(void) sprintf(p, "%s=%s", name, value);
+	ret = putenv(p);
+	return ret;
+}
+#endif
