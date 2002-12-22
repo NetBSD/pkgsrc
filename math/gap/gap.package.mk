883c8d629a52f8d39dca703e2505051704cda532
cjep <cjep@pkgsrc.org>
2002-12-22 16:06:33 +0000

    Update of math/gap (GAP 4.3) to bug fix 4.
    Changes since bug fix 3:
    * Interface changes affecting the AutPGrp and ANUPQ packages (these
      packages will need reinstallation).
    * 24 computational problems (please see the description4 file supplied
      with the GAP distribution and the NetBSD package).

--- math/gap/gap.package.mk
+++ math/gap/gap.package.mk
@@ -1,7 +1,10 @@
-# $NetBSD: gap.package.mk,v 1.2 2002/10/08 04:49:41 cjep Exp $
+# $NetBSD: gap.package.mk,v 1.3 2002/12/22 16:06:33 cjep Exp $
 #
 # Set GAP_PKG_NAME in your Makefile. More may appear here later.
 
-DEPENDS+=       	gap-4.3nb1:../../math/gap
+# Note that between nb1 == bug fix 3 and nb2 == bug fix 4 there are
+# some interface changes
+
+DEPENDS+=       	gap-4.3nb2:../../math/gap
 GAP_DIRECTORY=  	${LOCALBASE}/lib/gap4.3
 GAP_PKG_DIRECTORY=	${GAP_DIRECTORY}/pkg/${GAP_PKG_NAME}
