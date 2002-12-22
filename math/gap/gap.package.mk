79c4e0eb4c26d9108042afa3985f6f6317b4c792
cjep <cjep@pkgsrc.org>
2002-12-22 17:40:03 +0000

    Change gap.package.mk slightly for ease on adding gp-autpgrp. The gap.package.mk file needs reworking for it to be of use to all GAP packages. Bump revision.

--- math/gap/gap.package.mk
+++ math/gap/gap.package.mk
@@ -1,10 +1,11 @@
-# $NetBSD: gap.package.mk,v 1.3 2002/12/22 16:06:33 cjep Exp $
+# $NetBSD: gap.package.mk,v 1.4 2002/12/22 17:40:03 cjep Exp $
 #
 # Set GAP_PKG_NAME in your Makefile. More may appear here later.
 
 # Note that between nb1 == bug fix 3 and nb2 == bug fix 4 there are
 # some interface changes
 
-DEPENDS+=       	gap-4.3nb2:../../math/gap
+DEPENDS+=       	gap-4.3nb3:../../math/gap
 GAP_DIRECTORY=  	${LOCALBASE}/lib/gap4.3
-GAP_PKG_DIRECTORY=	${GAP_DIRECTORY}/pkg/${GAP_PKG_NAME}
+GAP_PKG_ROOT=		${GAP_DIRECTORY}/pkg
+GAP_PKG_DIRECTORY=	${GAP_PKG_ROOT}/${GAP_PKG_NAME}
