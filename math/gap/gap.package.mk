180364be98bd7a83ef9d977fe32f85a1ad0b51a1
cjep <cjep@pkgsrc.org>
2002-12-22 19:20:21 +0000

    Fix PLIST again. Bump revision for gap.package.mk ready for xgap (coming soon).

--- math/gap/gap.package.mk
+++ math/gap/gap.package.mk
@@ -1,11 +1,12 @@
-# $NetBSD: gap.package.mk,v 1.4 2002/12/22 17:40:03 cjep Exp $
+# $NetBSD: gap.package.mk,v 1.5 2002/12/22 19:20:22 cjep Exp $
 #
 # Set GAP_PKG_NAME in your Makefile. More may appear here later.
 
 # Note that between nb1 == bug fix 3 and nb2 == bug fix 4 there are
 # some interface changes
 
-DEPENDS+=       	gap-4.3nb3:../../math/gap
+DEPENDS+=       	gap-4.3nb4:../../math/gap
 GAP_DIRECTORY=  	${LOCALBASE}/lib/gap4.3
 GAP_PKG_ROOT=		${GAP_DIRECTORY}/pkg
 GAP_PKG_DIRECTORY=	${GAP_PKG_ROOT}/${GAP_PKG_NAME}
+GAP_PLATFORM=   	${MACHINE_GNU_PLATFORM}-cc
