d3bbfc37201af2cc102d923011e4977090d41b88
jschauma <jschauma@pkgsrc.org>
2002-12-23 22:50:29 +0000

    Don't test for OPSYS to set rpath et al, but rather use _OPSYS_RPATH_NAME. Suggested by Stoned Elipot.  Thanks.

--- mk/xaw.buildlink2.mk
+++ mk/xaw.buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: xaw.buildlink2.mk,v 1.7 2002/12/23 22:33:14 jschauma Exp $
+# $NetBSD: xaw.buildlink2.mk,v 1.8 2002/12/23 22:50:29 jschauma Exp $
 
 .if !defined(XAW_BUILDLINK2_MK)
 XAW_BUILDLINK2_MK=	# defined
@@ -19,11 +19,7 @@ BUILDLINK_PREFIX.Xaw?=	${BUILDLINK_PREFIX.Xaw3d}
 BUILDLINK_PREFIX.Xaw?=	${BUILDLINK_PREFIX.neXtaw}
 .else
 BUILDLINK_PREFIX.Xaw?=	${X11BASE}
-.  if ${OPSYS} == "IRIX"
-LIBXAW?=		-L${X11BASE}/lib -Wl,-rpath,${X11BASE}/lib -lXaw
-.  else
-LIBXAW?=		-L${X11BASE}/lib -Wl,-R${X11BASE}/lib -lXaw
-.  endif
+LIBXAW?=		-L${X11BASE}/lib -Wl,${_OPSYS_RPATH_NAME}${X11BASE}/lib -lXaw
 .endif
 
 MAKE_ENV+=	LIBXAW="${LIBXAW}"
