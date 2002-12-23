9c809260f3e534e38dabfa4cdd37a611c5529baf
wiz <wiz@pkgsrc.org>
2002-12-23 01:42:08 +0000

    Install headers and API example. Bump PKGREVISION to 2.

--- graphics/ffmpeg/buildlink2.mk
+++ graphics/ffmpeg/buildlink2.mk
@@ -1,15 +1,19 @@
-# $NetBSD: buildlink2.mk,v 1.1 2002/11/06 10:27:24 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/12/23 01:42:08 wiz Exp $
 #
 
 .if !defined(FFMPEG_BUILDLINK2_MK)
 FFMPEG_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			ffmpeg
-BUILDLINK_DEPENDS.ffmpeg?=		ffmpeg>=0.4.5nb1
+BUILDLINK_DEPENDS.ffmpeg?=		ffmpeg>=0.4.5nb2
 BUILDLINK_PKGSRCDIR.ffmpeg?=		../../graphics/ffmpeg
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.ffmpeg=ffmpeg
 BUILDLINK_PREFIX.ffmpeg_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.ffmpeg+=	include/libav/*.h
+BUILDLINK_FILES.ffmpeg+=	include/libavcodec/*.h
+BUILDLINK_FILES.ffmpeg+=	include/libavcodec/i386/*.h
+BUILDLINK_FILES.ffmpeg+=	include/libavcodec/libac3/*.h
 BUILDLINK_FILES.ffmpeg+=	lib/libav.*
 BUILDLINK_FILES.ffmpeg+=	lib/libavcodec.*
 
