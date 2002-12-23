001879746439fac02e3c24d47cd840552b785add
jlam <jlam>
2002-12-23 04:25:01 +0000

    Bump PKGREVISION for lang/librep and update the buildlink2.mk dependency to the latest librep.  librep installs and uses its own libtool for use by rep-* packages.  We replace librep's libtool with a symlink to ${LOCALBASE}/bin/libtool and add a dependency on libtool-base.  This addresses PR 19495 by Bill Sommerfeld.

--- lang/librep/buildlink2.mk
+++ lang/librep/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.1 2002/10/08 13:12:25 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/12/23 04:25:01 jlam Exp $
 #
 
 .if !defined(LIBREP_BUILDLINK2_MK)
 LIBREP_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			librep
-BUILDLINK_DEPENDS.librep?=		librep>=0.15.1nb1
+BUILDLINK_DEPENDS.librep?=		librep>=0.15.1nb3
 BUILDLINK_PKGSRCDIR.librep?=		../../lang/librep
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.librep=librep
