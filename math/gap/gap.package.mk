cd3e5cb6ca2304bc0e5cda6cbffd6cd2f86b0b6d
cjep <cjep@pkgsrc.org>
2002-12-31 13:02:33 +0000

    Install the prim directory (noted by Bettina Eick in private e-mail). Bump pkg revision.

--- math/gap/gap.package.mk
+++ math/gap/gap.package.mk
@@ -1,11 +1,11 @@
-# $NetBSD: gap.package.mk,v 1.5 2002/12/22 19:20:22 cjep Exp $
+# $NetBSD: gap.package.mk,v 1.6 2002/12/31 13:02:33 cjep Exp $
 #
 # Set GAP_PKG_NAME in your Makefile. More may appear here later.
 
 # Note that between nb1 == bug fix 3 and nb2 == bug fix 4 there are
 # some interface changes
 
-DEPENDS+=       	gap-4.3nb4:../../math/gap
+DEPENDS+=       	gap>=4.3nb5:../../math/gap
 GAP_DIRECTORY=  	${LOCALBASE}/lib/gap4.3
 GAP_PKG_ROOT=		${GAP_DIRECTORY}/pkg
 GAP_PKG_DIRECTORY=	${GAP_PKG_ROOT}/${GAP_PKG_NAME}
