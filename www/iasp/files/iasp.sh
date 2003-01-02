fd515dfdb11e34886fae8285bbc4d5fe4ee5c38a
jlam <jlam@pkgsrc.org>
2003-01-02 06:40:32 +0000

    Require a iasp user and group (defaulting to APACHE_{USER,GROUP}) and run the iASP proxy server as that user for improved security.  Bump PKGREVISION on www/iasp to 1.

--- www/iasp/files/iasp.sh
+++ www/iasp/files/iasp.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: iasp.sh,v 1.1.1.1 2003/01/01 07:03:42 jlam Exp $
+# $NetBSD: iasp.sh,v 1.2 2003/01/02 06:40:33 jlam Exp $
 #
 # PROVIDE: iasp
 # REQUIRE: DAEMON
@@ -12,12 +12,13 @@ fi
 
 name="iasp"
 rcvar=$name
+iasp_user="@IASP_USER@"
 start_cmd=iasp_start
 stop_cmd=iasp_stop
 
 iasp_start()
 {
-	@IASP@/start-server.sh &
+	@SU@ -m ${iasp_user} -c "exec @IASP@/start-server.sh" &
 	sleep 2
 }
 
