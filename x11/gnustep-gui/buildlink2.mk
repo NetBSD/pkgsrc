e17c89775cfadaab86f2681ffb85f8af0a2a5513
rh <rh@pkgsrc.org>
2003-01-03 04:06:13 +0000

    Update gnustep-gui to 0.8.3.
    Noteworthy changes in version `0.8.3'
    =====================================
       * Additions for Gorm support.
       * Alpha support for OpenGL
       * Better ruler support - dragging of tab markers.
       * Document support, recent files, etc.
       * Simple printing to printer and print previewing.
       * Window focus fixes
       * Key view handling rewritten.

--- x11/gnustep-gui/buildlink2.mk
+++ x11/gnustep-gui/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/10/18 09:23:26 rh Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/01/03 04:06:15 rh Exp $
 #
 
 .if !defined(GNUSTEP_GUI_BUILDLINK2_MK)
 GNUSTEP_GUI_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gnustep-gui
-BUILDLINK_DEPENDS.gnustep-gui?=		gnustep-gui>=0.8.2
+BUILDLINK_DEPENDS.gnustep-gui?=		gnustep-gui>=0.8.3
 BUILDLINK_PKGSRCDIR.gnustep-gui?=	../../x11/gnustep-gui
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gnustep-gui=gnustep-gui
