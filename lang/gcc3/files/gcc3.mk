c3d0fc5ae4f5117a15b41692753e9afb7c530395
uebayasi <uebayasi@pkgsrc.org>
2003-01-06 07:42:26 +0000

    Initial import of GCC 3.2.1, the latest release of GNU Compiler Collection, which currently contains front ends for C, C++, Objective-C, Fortran, Java, and Ada.
    For more detail, see
    
    	http://gcc.gnu.org/gcc-3.2/
    
    XXX Only tested on NetBSD/i386 -current.  Tests on Linux and Solaris
    are very encouraged.
    
    XXX buildlink2.mk is not provided, meaning no other packages can
    depend on this for now.

---
+++ lang/gcc3/files/gcc3.mk
@@ -0,0 +1,7 @@
+# $NetBSD: gcc3.mk,v 1.1.1.1 2003/01/06 07:42:27 uebayasi Exp $
+#
+# make configuration file for @PKGNAME@
+
+CC=	@GCC_PREFIX@/bin/cc
+CPP=	@GCC_PREFIX@/bin/cpp
+CXX=	@GCC_PREFIX@/bin/c++
