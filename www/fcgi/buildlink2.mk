8236f0a4cbe091551f7145f4e2f9e9cc59558447
wiz <wiz@pkgsrc.org>
2003-01-07 12:45:51 +0000

    Remove duplicate empty line.

--- www/fcgi/buildlink2.mk
+++ www/fcgi/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1 2003/01/07 11:35:33 uebayasi Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/01/07 12:45:51 wiz Exp $
 #
 # This Makefile fragment is included by packages that use fcgi.
 #
@@ -6,9 +6,9 @@
 .if !defined(FCGI_BUILDLINK2_MK)
 FCGI_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=			fcgi
-BUILDLINK_DEPENDS.fcgi?=		fcgi>=2.2.2
-BUILDLINK_PKGSRCDIR.fcgi?=		../../www/fcgi
+BUILDLINK_PACKAGES+=		fcgi
+BUILDLINK_DEPENDS.fcgi?=	fcgi>=2.2.2
+BUILDLINK_PKGSRCDIR.fcgi?=	../../www/fcgi
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.fcgi=fcgi
 BUILDLINK_PREFIX.fcgi_DEFAULT=	${LOCALBASE}
@@ -16,7 +16,6 @@ BUILDLINK_FILES.fcgi+=	include/fastcgi.h
 BUILDLINK_FILES.fcgi+=	include/fcgi*.h
 BUILDLINK_FILES.fcgi+=	lib/libfcgi++.*
 BUILDLINK_FILES.fcgi+=	lib/libfcgi.*
-
 
 BUILDLINK_TARGETS+=	fcgi-buildlink
 
