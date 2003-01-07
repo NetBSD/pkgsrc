1d4d4b252e208f0093fa893f949dadd5719c88dd
uebayasi <uebayasi>
2003-01-07 11:47:29 +0000

    Thank createbuildlink; initial buildlink2.mk.

---
+++ devel/gtl/buildlink2.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink2.mk,v 1.1 2003/01/07 11:47:29 uebayasi Exp $
+#
+# This Makefile fragment is included by packages that use gtl.
+#
+
+.if !defined(GTL_BUILDLINK2_MK)
+GTL_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=		gtl
+BUILDLINK_DEPENDS.gtl?=		gtl>=0.3.3
+BUILDLINK_PKGSRCDIR.gtl?=	../../devel/gtl
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gtl=gtl
+BUILDLINK_PREFIX.gtl_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.gtl+=	include/GTL/*
+BUILDLINK_FILES.gtl+=	lib/libGTL.*
+
+
+BUILDLINK_TARGETS+=	gtl-buildlink
+
+gtl-buildlink: _BUILDLINK_USE
+
+.endif	# GTL_BUILDLINK2_MK
