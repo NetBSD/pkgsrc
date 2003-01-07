b0fc40bb8dae13b34a50d3b58dc06912433915cf
uebayasi <uebayasi@pkgsrc.org>
2003-01-07 12:24:51 +0000

    Initial buildlink2.mk.

---
+++ math/cassowary/buildlink2.mk
@@ -0,0 +1,29 @@
+# $NetBSD: buildlink2.mk,v 1.1 2003/01/07 12:24:51 uebayasi Exp $
+#
+# This Makefile fragment is included by packages that use cassowary.
+#
+
+.if !defined(CASSOWARY_BUILDLINK2_MK)
+CASSOWARY_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			cassowary
+BUILDLINK_DEPENDS.cassowary?=		cassowary>=0.60
+BUILDLINK_PKGSRCDIR.cassowary?=		../../math/cassowary
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.cassowary=cassowary
+BUILDLINK_PREFIX.cassowary_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.cassowary+=	include/Cl.h
+BUILDLINK_FILES.cassowary+=	include/ClC.h
+BUILDLINK_FILES.cassowary+=	include/cassowary/*
+BUILDLINK_FILES.cassowary+=	include/cassowary*.h
+BUILDLINK_FILES.cassowary+=	include/cl-snarf.h
+BUILDLINK_FILES.cassowary+=	lib/libcassowary.*
+
+.include "../../devel/gtl/buildlink2.mk"
+.include "../../lang/guile14/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	cassowary-buildlink
+
+cassowary-buildlink: _BUILDLINK_USE
+
+.endif	# CASSOWARY_BUILDLINK2_MK
