8456659b56755141133b19b2b25550c39f4dbaa6
rh <rh>
2003-01-10 04:30:16 +0000

    Update pilot-link packages to 0.11.7.  Changes are too numerous to list here.  They include 	* tons of bug fixes, 	* USB support, 	* C++ support, 	* restructured libraries; and 	* tons of other new features.

--- comms/pilot-link-libs/buildlink2.mk
+++ comms/pilot-link-libs/buildlink2.mk
@@ -1,19 +1,21 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/10 16:06:36 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/01/10 04:30:21 rh Exp $
 
 .if !defined(PILOT_LINK_LIBS_BUILDLINK2_MK)
 PILOT_LINK_LIBS_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			pilot-link-libs
-BUILDLINK_DEPENDS.pilot-link-libs?=	pilot-link-libs>=0.9.5nb3
+BUILDLINK_DEPENDS.pilot-link-libs?=	pilot-link-libs>=0.11.7
 BUILDLINK_PKGSRCDIR.pilot-link-libs?=	../../comms/pilot-link-libs
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.pilot-link-libs=pilot-link-libs
 BUILDLINK_PREFIX.pilot-link-libs_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.pilot-link-libs=	include/pi-*.*
-BUILDLINK_FILES.pilot-link-libs+=	lib/libpicc.*
-BUILDLINK_FILES.pilot-link-libs+=	lib/libpisock.*
+BUILDLINK_FILES.pilot-link-libs+=       lib/libpisock++.*
+BUILDLINK_FILES.pilot-link-libs+=       lib/libpisock.*
+BUILDLINK_FILES.pilot-link-libs+=       lib/libpisync.*
 
 .include "../../converters/libiconv/buildlink2.mk"
+.include "../../devel/readline/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	pilot-link-libs-buildlink
 
