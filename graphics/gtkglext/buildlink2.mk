b2cf731735e860a6b7b42add72ced2f575e0a05f
rh <rh@pkgsrc.org>
2003-01-14 05:35:53 +0000

    Update gtkglext to 0.6.1.  Changes include: 	- works with gtk2>=2.2.0 	- some API changes 	- several feature additions and bug fixes

--- graphics/gtkglext/buildlink2.mk
+++ graphics/gtkglext/buildlink2.mk
@@ -1,48 +1,30 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/12/24 06:10:06 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/01/14 05:35:53 rh Exp $
 #
 # This Makefile fragment is included by packages that use gtkglext.
 #
-# This file was created automatically using createbuildlink 2.1.
+# This file was created automatically using createbuildlink 2.4.
 #
 
 .if !defined(GTKGLEXT_BUILDLINK2_MK)
 GTKGLEXT_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gtkglext
-BUILDLINK_DEPENDS.gtkglext?=		gtkglext>=0.5.1nb1
+BUILDLINK_DEPENDS.gtkglext?=		gtkglext>=0.6.1
 BUILDLINK_PKGSRCDIR.gtkglext?=		../../graphics/gtkglext
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gtkglext=gtkglext
 BUILDLINK_PREFIX.gtkglext_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkgl.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkglconfig.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkglcontext.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkgldebug.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkgldefs.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkgldrawable.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkglenumtypes.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkglfont.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkglpixmap.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkglprivate.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkglquery.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkgltokens.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkgltypes.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkglversion.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkglwindow.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gdk/gdkglx.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gtk/gtkgl.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gtk/gtkgldebug.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gtk/gtkgldefs.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gtk/gtkglprivate.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gtk/gtkglversion.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gtk/gtkglwidget.h
-BUILDLINK_FILES.gtkglext+=	include/gtkglext-0.x/gtk/gtkglwidgetparam.h
-BUILDLINK_FILES.gtkglext+=	lib/libgdkglext-x11-0.x.*
-BUILDLINK_FILES.gtkglext+=	lib/libgtkglext-x11-0.x.*
-BUILDLINK_FILES.gtkglext+=	lib/pkgconfig/gdkglext-0.x.pc
-BUILDLINK_FILES.gtkglext+=	lib/pkgconfig/gdkglext-x11-0.x.pc
-BUILDLINK_FILES.gtkglext+=	lib/pkgconfig/gtkglext-0.x.pc
-BUILDLINK_FILES.gtkglext+=	lib/pkgconfig/gtkglext-x11-0.x.pc
+BUILDLINK_FILES.gtkglext+=	include/gtkglext-1.0/gdk/*.h
+BUILDLINK_FILES.gtkglext+=	include/gtkglext-1.0/gdk/GL/*.h
+BUILDLINK_FILES.gtkglext+=	include/gtkglext-1.0/gdk/x11/*.h
+BUILDLINK_FILES.gtkglext+=	include/gtkglext-1.0/gtk/*.h
+BUILDLINK_FILES.gtkglext+=	lib/gtkglext-1.0/include/gdkglext-config.h
+BUILDLINK_FILES.gtkglext+=	lib/libgdkglext-x11-1.0.*
+BUILDLINK_FILES.gtkglext+=	lib/libgtkglext-x11-1.0.*
+BUILDLINK_FILES.gtkglext+=	lib/pkgconfig/gdkglext-1.0.pc
+BUILDLINK_FILES.gtkglext+=	lib/pkgconfig/gdkglext-x11-1.0.pc
+BUILDLINK_FILES.gtkglext+=	lib/pkgconfig/gtkglext-1.0.pc
+BUILDLINK_FILES.gtkglext+=	lib/pkgconfig/gtkglext-x11-1.0.pc
 
 .include "../../textproc/gtk-doc/buildlink2.mk"
 .include "../../devel/pkgconfig/buildlink2.mk"
