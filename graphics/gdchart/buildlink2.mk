c89ac7e2457bc753c2b9111adf3d6eb3b8a1b8cf
jmmv <jmmv@pkgsrc.org>
2003-01-17 13:49:07 +0000

    Libtoolize this package so we get shared libraries (they are required by net/ntop2).  While here, also enable jpeg support.  Bump PKGREVISION to 2. Reviewed by wiz.

--- graphics/gdchart/buildlink2.mk
+++ graphics/gdchart/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.1 2002/10/07 20:12:13 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/01/17 13:49:07 jmmv Exp $
 #
 
 .if !defined(GDCHART_BUILDLINK2_MK)
 GDCHART_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gdchart
-BUILDLINK_DEPENDS.gdchart?=		gdchart>=0.10.1nb1
+BUILDLINK_DEPENDS.gdchart?=		gdchart>=0.10.1nb2
 BUILDLINK_PKGSRCDIR.gdchart?=		../../graphics/gdchart
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gdchart=gdchart
