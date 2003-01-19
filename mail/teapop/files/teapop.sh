f560d4ac7834a0c9539de4bffc8f40a970511c12
grant <grant@pkgsrc.org>
2003-01-19 13:04:48 +0000

    use automatic rc script handling.

--- mail/teapop/files/teapop.sh
+++ mail/teapop/files/teapop.sh
@@ -1,6 +1,6 @@
-#!/bin/sh 
+#!@RCD_SCRIPTS_SHELL@
 #
-#       $NetBSD: teapop,v 1.1 2002/05/07 23:46:01 hubertf Exp $  
+#       $NetBSD: teapop.sh,v 1.1 2003/01/19 13:04:52 grant Exp $  
 #
 # PROVIDE: teapop 
 # REQUIRE: DAEMON
@@ -12,7 +12,7 @@ rcvar=$name
 command="@PREFIX@/libexec/${name}"
 command_args="-s"
 pidfile="/var/run/${name}.pid"
-required_files="@PREFIX@/etc/teapop.passwd"
+required_files="@PKG_SYSCONFDIR@/teapop.passwd"
 sig_stop="INT"
 
 load_rc_config $name
