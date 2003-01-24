df5ad629fab80e14c3430784a2e91c3a0daffb9f
jlam <jlam@pkgsrc.org>
2003-01-24 04:22:42 +0000

    Fix the installed g-wrap-config to invoke ${GUILE14_PREFIX}/bin/guile instead of the one in the work directory.  Also ensure that we install guile library files into ${GUILE14_PREFIX}/share/guile instead of into the same place under ${LOCALBASE}, since we are using guile14.  Bump PKGREVISION of devel/g-wrap to 1.

--- devel/g-wrap/buildlink2.mk
+++ devel/g-wrap/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/01/19 04:43:38 uebayasi Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/01/24 04:22:42 jlam Exp $
 
 .if !defined(G_WRAP_BUILDLINK2_MK)
 G_WRAP_BUILDLINK2_MK=	# defined
@@ -9,8 +9,7 @@ BUILDLINK_PKGSRCDIR.g-wrap?=	../../devel/g-wrap
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.g-wrap=g-wrap
 BUILDLINK_PREFIX.g-wrap_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.g-wrap+=	bin/g-wrap-config
-BUILDLINK_FILES.g-wrap+=	include/g-wrap-runtime-guile.h
+BUILDLINK_FILES.g-wrap=		include/g-wrap-runtime-guile.h
 BUILDLINK_FILES.g-wrap+=	lib/libg-wrap-runtime-guile.*
 
 .include "../../lang/guile14/buildlink2.mk"
