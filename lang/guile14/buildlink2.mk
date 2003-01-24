07212bde902d7174cbf6c87c1bd5d12985124174
jlam <jlam@pkgsrc.org>
2003-01-24 04:22:16 +0000

    * Add GUILE14, GUILE14_CONFIG, and GUILE14_SNARF that may be used by packages   to invoke the guile, guile-config, and guile-snarf binaries from the   guile14 installation.
    * GUILE14_PREFIX, the result of "pkg_info -p guile14".
    
    * GUILE14_SUBDIR, the subdir relative to ${LOCALBASE} where guile14 is
      installed.
    
    * Replace for ${BUILDLINK_DIR}/bin/guile in config files when unbuildinking.
    
    * Symlink the guile14 libtool archives into ${BUILDLINK_DIR}/lib to suppress
      find the libtool archives from a guile package installed into ${LOCALBASE}.
      We can no longer include both guile/buildlink2.mk and guile14/buildlink2.mk
      as a result of this change.

--- lang/guile14/buildlink2.mk
+++ lang/guile14/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.7 2003/01/19 04:38:01 uebayasi Exp $
+# $NetBSD: buildlink2.mk,v 1.8 2003/01/24 04:22:16 jlam Exp $
 
 .if !defined(GUILE14_BUILDLINK2_MK)
 GUILE14_BUILDLINK2_MK=	# defined
@@ -7,15 +7,16 @@ BUILDLINK_PACKAGES+=		guile14
 BUILDLINK_DEPENDS.guile14?=	guile14>=1.4nb2
 BUILDLINK_PKGSRCDIR.guile14?=	../../lang/guile14
 
-EVAL_PREFIX+=				BUILDLINK_PREFIX.guile14=guile14
-BUILDLINK_PREFIX.guile14=		${LOCALBASE}
-BUILDLINK_PREFIX.guile14_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.guile14+=		guile/1.4/include/libguile.*
-BUILDLINK_FILES.guile14+=		guile/1.4/include/guile/*
-BUILDLINK_FILES.guile14+=		guile/1.4/include/guile-readline/*
-BUILDLINK_FILES.guile14+=		guile/1.4/include/libguile/*
-BUILDLINK_FILES.guile14+=		guile/1.4/lib/libguile.*
-BUILDLINK_FILES.guile14+=		guile/1.4/lib/libguilereadline.*
+EVAL_PREFIX+=			GUILE14_PREFIX=guile14
+GUILE14_PREFIX_DEFAULT=		${LOCALBASE}/guile/1.4
+BUILDLINK_PREFIX.guile14=	${LOCALBASE}
+GUILE14_SUBDIR=			${GUILE14_PREFIX:S/^${BUILDLINK_PREFIX.guile14}\///}
+BUILDLINK_FILES.guile14+=	${GUILE14_SUBDIR}/include/libguile.*
+BUILDLINK_FILES.guile14+=	${GUILE14_SUBDIR}/include/guile/*
+BUILDLINK_FILES.guile14+=	${GUILE14_SUBDIR}/include/guile-readline/*
+BUILDLINK_FILES.guile14+=	${GUILE14_SUBDIR}/include/libguile/*
+BUILDLINK_FILES.guile14+=	${GUILE14_SUBDIR}/lib/libguile.*
+BUILDLINK_FILES.guile14+=	${GUILE14_SUBDIR}/lib/libguilereadline.*
 
 USE_GNU_READLINE=	# defined
 
@@ -24,14 +25,34 @@ USE_GNU_READLINE=	# defined
 
 BUILDLINK_TARGETS+=	guile14-buildlink
 BUILDLINK_TARGETS+=	guile14-buildlink-bin
+BUILDLINK_TARGETS+=	guile14-buildlink-la
+
+GUILE14=		${GUILE14_PREFIX}/bin/guile
+GUILE14_CONFIG=		${GUILE14_PREFIX}/bin/guile-config
+GUILE14_SNARF=		${GUILE14_PREFIX}/bin/guile-snarf
 
 guile14-buildlink: _BUILDLINK_USE
 
 # Put only bin/guile, bin/guile-config, and bin/guile-snarf into
 # ${BUILDLINK_DIR}/bin.  Other files (headers/libraries) are put in
 # ${BUILDLINK_DIR}/guile/1.4/...
+#
 guile14-buildlink-bin:
 	${_PKG_SILENT}${_PKG_DEBUG}${ECHO_BUILDLINK_MSG} "Linking Guile executables."
-	${_PKG_SILENT}${_PKG_DEBUG}${LN} -s ${BUILDLINK_PREFIX.guile14}/guile/1.4/bin/guile* ${BUILDLINK_DIR}/bin
+	${_PKG_SILENT}${_PKG_DEBUG}${LN} -s ${GUILE14_PREFIX}/bin/guile* ${BUILDLINK_DIR}/bin
+
+REPLACE_BUILDLINK_SED+=	\
+	-e "s|${BUILDLINK_DIR}/bin/guile|${GUILE14_PREFIX}/bin/guile|g"
+
+guile14-buildlink-la:
+	${_PKG_SILENT}${_PKG_DEBUG}					\
+	for file in libguile.la libguilereadline.la; do			\
+		src="${GUILE14_PREFIX}/lib/$$file";			\
+		dest="${BUILDLINK_DIR}/lib/$$file";			\
+		if [ -f $$src ]; then					\
+			${ECHO_BUILDLINK_MSG} "Linking $$src to $$dest."; \
+			${LN} -sf $$src $$dest;				\
+		fi;							\
+	done
 
 .endif	# GUILE14_BUILDLINK2_MK
