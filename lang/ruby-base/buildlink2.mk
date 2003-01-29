e85cfbbd73223429bae4fd9eb2ae8d677bc71d20
kei <kei>
2003-01-29 09:00:25 +0000

    replaced hardcoded i386-netbsdelf to ${MACHINE_GNU_PLATFORM:C/--/-/}. Problem shown in the pkgsrc/arm bulk build by Chris Pinnock.  Fix suggested by Masao Uebayashi.
    This hopefully should fix non-i386-netbsd-elf buld.

--- lang/ruby-base/buildlink2.mk
+++ lang/ruby-base/buildlink2.mk
@@ -1,8 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.1 2002/10/09 17:17:13 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/01/29 09:00:25 kei Exp $
 #
 
 .if !defined(RUBY_BASE_BUILDLINK2_MK)
 RUBY_BASE_BUILDLINK2_MK=	# defined
+
+RUBY_MD_LIBDIR=			${MACHINE_GNU_PLATFORM:C/--/-/}
 
 BUILDLINK_PACKAGES+=			ruby-base
 BUILDLINK_DEPENDS.ruby-base?=		ruby-base>=1.6.7nb3
@@ -10,31 +12,31 @@ BUILDLINK_PKGSRCDIR.ruby-base?=		../../lang/ruby-base
 EVAL_PREFIX+=	BUILDLINK_PREFIX.ruby-base=ruby-base
 BUILDLINK_PREFIX.ruby-base_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.ruby-base=	lib/libruby.*
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/config.h
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/curses.so
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/dbm.so
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/defines.h
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/dln.h
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/env.h
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/etc.so
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/fcntl.so
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/intern.h
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/libruby.a
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/nkf.so
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/node.h
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/pty.so
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/rbconfig.rb
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/re.h
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/regex.h
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/ruby.h
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/rubyio.h
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/rubysig.h
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/sdbm.so
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/socket.so
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/st.h
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/syslog.so
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/util.h
-BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/i386-netbsdelf/version.h
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/config.h
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/curses.so
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/dbm.so
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/defines.h
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/dln.h
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/env.h
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/etc.so
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/fcntl.so
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/intern.h
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/libruby.a
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/nkf.so
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/node.h
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/pty.so
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/rbconfig.rb
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/re.h
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/regex.h
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/ruby.h
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/rubyio.h
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/rubysig.h
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/sdbm.so
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/socket.so
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/st.h
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/syslog.so
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/util.h
+BUILDLINK_FILES.ruby-base+=	lib/ruby/1.6/${RUBY_MD_LIBDIR}/version.h
 
 BUILDLINK_TARGETS+=	ruby-base-buildlink
 
