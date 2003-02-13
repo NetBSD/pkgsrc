0a17dce2f0280f9333a572a3a432f77bf6e85828
jmmv <jmmv@pkgsrc.org>
2003-02-13 22:33:12 +0000

    Update to 2.2.1.  Changes since 0.17:
    * libwnck/tasklist.c: (wnck_tasklist_check_end_sequence): Actually end the
      sequnce if we match the wmclass against a window.
    * libwnck/tasklist.c: time out startup sequences if we don't see them end, and
      end them if a window is mapped with StartupWMClass.
    * libwnck/window.c (_wnck_window_get_resource_class)
      (_wnck_window_get_resource_name): add internal API to get window classes
    * configure.in: added 'ro' to ALL_LINGUAS
    * configure.in: Added "mn" to ALL_LINGUAS.
    * configure.in: Added Amharic (am), Farsi (fa), and Macedonian (mk) to
      ALL_LINGUAS
    * README: update README
    * libwnck/application.c (get_icons): don't add an extra reference to all
      icons, that was an unfortunate memory leak. Patch from

--- devel/libwnck/buildlink2.mk
+++ devel/libwnck/buildlink2.mk
@@ -1,17 +1,19 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/12/24 06:09:57 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/02/13 22:33:12 jmmv Exp $
 
 .if !defined(LIBWNCK_BUILDLINK2_MK)
 LIBWNCK_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libwnck
-BUILDLINK_DEPENDS.libwnck?=	libwnck>=0.17nb1
+BUILDLINK_DEPENDS.libwnck?=	libwnck>=2.2.1
 BUILDLINK_PKGSRCDIR.libwnck?=	../../devel/libwnck
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.libwnck=libwnck
 BUILDLINK_PREFIX.libwnck_DEFAULT=	${X11PREFIX}
 BUILDLINK_FILES.libwnck=	include/libwnck-1.0/libwnck/*
 BUILDLINK_FILES.libwnck+=	lib/libwnck-1.*
+BUILDLINK_FILES.libwnck+=	lib/pkgconfig/libwnck-1.0.pc
 
+.include "../../devel/gettext-lib/buildlink2.mk"
 .include "../../x11/gtk2/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	libwnck-buildlink
