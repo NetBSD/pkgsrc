eb8cbc990e6587f2fd73cd3e950f8d5978fd3f07
wiz <wiz@pkgsrc.org>
2003-02-14 23:25:32 +0000

    Update to 1.1.0:  This release adds ReplayGain and cue sheet support, and improves 24-bit compression, tag editing in the plugins, and decoding to AIFF.

--- audio/flac/buildlink2.mk
+++ audio/flac/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.5 2002/11/10 23:55:14 lukem Exp $
+# $NetBSD: buildlink2.mk,v 1.6 2003/02/14 23:25:33 wiz Exp $
 
 .if !defined(FLAC_BUILDLINK2_MK)
 FLAC_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		flac
-BUILDLINK_DEPENDS.flac?=	flac>=1.0.4
+BUILDLINK_DEPENDS.flac?=	flac>=1.1.0
 BUILDLINK_PKGSRCDIR.flac?=	../../audio/flac
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.flac=flac
