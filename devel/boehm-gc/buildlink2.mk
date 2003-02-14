c5ade8fb8a84f29b4d55793493dad108f1448c09
wiz <wiz@pkgsrc.org>
2003-02-14 18:50:50 +0000

    Update to 6.1nb1, from Marc Recht in PR 20008: Install shared libs.

--- devel/boehm-gc/buildlink2.mk
+++ devel/boehm-gc/buildlink2.mk
@@ -1,19 +1,19 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 18:38:26 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/02/14 18:50:50 wiz Exp $
 
 .if !defined(BOEHM_GC_BUILDLINK2_MK)
 BOEHM_GC_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=		boehm-gc
-BUILDLINK_DEPENDS.boehm-gc?=	boehm-gc>=5.3
-BUILDLINK_PKGSRCDIR.boehm-gc?=	../../devel/boehm-gc
-BUILDLINK_DEPMETHOD.boehm-gc?=	build
+BUILDLINK_PACKAGES+=			boehm-gc
+BUILDLINK_DEPENDS.boehm-gc?=		boehm-gc>=6.1nb1
+BUILDLINK_PKGSRCDIR.boehm-gc?=		../../devel/boehm-gc
 
-EVAL_PREFIX+=				BUILDLINK_PREFIX.boehm-gc=boehm-gc
+EVAL_PREFIX+=	BUILDLINK_PREFIX.boehm-gc=boehm-gc
 BUILDLINK_PREFIX.boehm-gc_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.boehm-gc=		include/gc.h
-BUILDLINK_FILES.boehm-gc+=		include/gc_cpp.h
-BUILDLINK_FILES.boehm-gc+=		lib/libgc.a
-BUILDLINK_FILES.boehm-gc+=		lib/libleak.a
+BUILDLINK_FILES.boehm-gc+=	include/gc.h
+BUILDLINK_FILES.boehm-gc+=	include/gc_backptr.h
+BUILDLINK_FILES.boehm-gc+=	include/gc_cpp.h
+BUILDLINK_FILES.boehm-gc+=	lib/libgc.*
+BUILDLINK_FILES.boehm-gc+=	lib/libleak.*
 
 BUILDLINK_TARGETS+=	boehm-gc-buildlink
 
