1aa0f7d756e713c662e9adc8408264a306e448b2
jmmv <jmmv@pkgsrc.org>
2003-02-14 20:14:27 +0000

    Updated to 2.2.0.1.  Changes since 2.0.5:
    * libgnomecanvas/Makefile.am (libgnomecanvas_2_la_LDFLAGS): Correct version
      number error.
    * configure.in: Add EXTRAVERSION and set to .1.  Added missing languages to
      ALL_LINGUAS.
    * libgnomecanvas/gnome-canvas.c (gnome_canvas_request_redraw_uta): Comment
      out assert that caused lots of crashes.
    * configure.in: Added "mn" to ALL_LINGUAS.
    * configure.in: added 'ro' to ALL_LINGUAS
    * libgnomecanvas/gnome-canvas-shape.c (gnome_canvas_shape_init): Initialize
      shape->priv->scale to 1.0.
    * configure.in: Similar setup for Win32 build as DLL as in glib etc: Call
      AC_LIBTOOL_WIN32_DLL. Check for native Win32 or Win32 platform in general
      (including Cygwin). Set automake conditionals OS_WIN32 and PLATFORM_WIN32.
      Use gcc -mms-bitfields for binary compatibility with GTK+ DLLs.
    * libgnomecanvas/Makefile.am: [Win32] Use -no-undefined. Install import
      libraries.
    * glade/Makefile.am: [Win32] Use -no-undefined.
    * libgnomecanvas/gnome-canvas-util.c (gnome_canvas_get_miter_points): Use
      G_PI and G_PI_2 instead of M_PI and M_PI_2.

--- graphics/libgnomecanvas/buildlink2.mk
+++ graphics/libgnomecanvas/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.4 2002/12/24 06:10:07 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.5 2003/02/14 20:14:28 jmmv Exp $
 
 .if !defined(LIBGNOMECANVAS_BUILDLINK2_MK)
 LIBGNOMECANVAS_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libgnomecanvas
-BUILDLINK_DEPENDS.libgnomecanvas?=	libgnomecanvas>=2.0.4nb2
+BUILDLINK_DEPENDS.libgnomecanvas?=	libgnomecanvas>=2.2.0.1
 BUILDLINK_PKGSRCDIR.libgnomecanvas?=	../../graphics/libgnomecanvas
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libgnomecanvas=libgnomecanvas
@@ -12,6 +12,12 @@ BUILDLINK_PREFIX.libgnomecanvas_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.libgnomecanvas=		include/libgnomecanvas-2.0/libgnomecanvas/*
 BUILDLINK_FILES.libgnomecanvas+=	lib/libglade/2.0/libcanvas.*
 BUILDLINK_FILES.libgnomecanvas+=	lib/libgnomecanvas-2.*
+BUILDLINK_FILES.libgnomecanvas+=	lib/pkgconfig/libgnomecanvas-2.0.pc
+
+.include "../../devel/gettext-lib/buildlink2.mk"
+.include "../../devel/libglade2/buildlink2.mk"
+.include "../../graphics/libart2/buildlink2.mk"
+.include "../../x11/gtk2/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	libgnomecanvas-buildlink
 
