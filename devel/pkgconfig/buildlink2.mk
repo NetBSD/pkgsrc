0132a8778424e41ffb289cfbf9b98274f7e80ac6
salo <salo>
2003-02-14 21:14:37 +0000

    Update to version 0.15.
    Patch from Juan RP via PR pkg/20236.
    
    Changes:
    
    0.15
    ====
     - add PKG_CONFIG_LIBDIR for cross-compiling (David Schleef)
     - add --libs-only-other/--cflags-only-other (Zack Rusin)
     - apply man page fixes (Pter Breitenlohner)
     - C portability fix (David Robins)
     - fix to win32 build (Tor Lillqvist)

--- devel/pkgconfig/buildlink2.mk
+++ devel/pkgconfig/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/01/04 22:08:05 rh Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/02/14 21:14:38 salo Exp $
 
 .if !defined(PKGCONFIG_BUILDLINK2_MK)
 PKGCONFIG_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		pkgconfig
-BUILDLINK_DEPENDS.pkgconfig?=	pkgconfig>=0.14.0
+BUILDLINK_DEPENDS.pkgconfig?=	pkgconfig>=0.15.0
 BUILDLINK_PKGSRCDIR.pkgconfig?=	../../devel/pkgconfig
 BUILDLINK_DEPMETHOD.pkgconfig?=	build
 
