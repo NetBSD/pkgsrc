61f56e6628102b9685803b5a970281c0341ebd69
jtb <jtb>
2003-02-17 01:01:38 +0000

    Update to gsl-1.3.
    Changes since version 1.1.1 include:
    
    * Addition of new functions
    * Many bugfixes
    * Homepage and distribution moved from sources.redhat.com to GNU.
    
    See NEWS for more info.

--- math/gsl/buildlink2.mk
+++ math/gsl/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/01/01 06:07:51 dmcmahill Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/02/17 01:01:38 jtb Exp $
 
 .if !defined(GSL_BUILDLINK2_MK)
 GSL_BUILDLINK2_MK=              # defined
 
 BUILDLINK_PACKAGES+=		gsl
-BUILDLINK_DEPENDS.gsl?=		gsl>=1.1.1
+BUILDLINK_DEPENDS.gsl?=		gsl>=1.3
 BUILDLINK_PKGSRCDIR.gsl?=	../../math/gsl
 BUILDLINK_DEPMETHOD.gsl?=	build
 
@@ -13,7 +13,7 @@ BUILDLINK_PREFIX.gsl_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.gsl=		include/gsl/*
 BUILDLINK_FILES.gsl+=		lib/libgsl.*
 BUILDLINK_FILES.gsl+=		lib/libgslcblas.*
-          
+
 BUILDLINK_TARGETS+=	gsl-buildlink
 
 gsl-buildlink: _BUILDLINK_USE
