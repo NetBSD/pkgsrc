2f3e683279c7e6e156b233e196acc56348ee609f
jmmv <jmmv@pkgsrc.org>
2003-02-18 14:39:02 +0000

    Update to 0.3.12.  There have been lots of changes since 0.2 (this version is almost a rewrite of the program).  Note that this is still a development version but includes new features that may be required by packages using scrollkeeper (which won't work with the old 0.2).
    Provide a framework to automatically rebuild scrollkeeper's database from
    packages installing OMF files under share/omf/;  they only need to include
    omf.mk in their Makefile and take care to install OMF files in the said
    directory.
    
    Reviewed by rh and wiz.

--- textproc/scrollkeeper/buildlink2.mk
+++ textproc/scrollkeeper/buildlink2.mk
@@ -1,24 +1,40 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/11/02 02:08:01 rh Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/02/18 14:39:02 jmmv Exp $
 
 .if !defined(SCROLLKEEPER_BUILDLINK2_MK)
 SCROLLKEEPER_BUILDLINK2_MK=		# defined
 
 BUILDLINK_PACKAGES+=			scrollkeeper
-BUILDLINK_DEPENDS.scrollkeeper?=	scrollkeeper>=0.2
+BUILDLINK_DEPENDS.scrollkeeper?=	scrollkeeper>=0.3.12
 BUILDLINK_PKGSRCDIR.scrollkeeper?=	../../textproc/scrollkeeper
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.scrollkeeper=scrollkeeper
-BUILDLINK_PREFIX.scrollkeeper_DEFAULT=	${X11PREFIX}
+BUILDLINK_PREFIX.scrollkeeper_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.scrollkeeper+=		lib/libscrollkeeper.*
 
 BUILDLINK_TARGETS+=			scrollkeeper-buildlink
+BUILDLINK_TARGETS+=			scrollkeeper-buildlink-fake
 
-.if exists(${LOCALBASE}/libdata/scrollkeeper)
-SCROLLKEEPER_DATADIR=			${LOCALBASE}/libdata
-.else
-SCROLLKEEPER_DATADIR=			${BUILDLINK_PREFIX.scrollkeeper}/var/lib
-.endif
+_SK_FAKE_UPDATE=	${BUILDLINK_DIR}/bin/scrollkeeper-update
+_SK_FAKE_REBUILDDB=	${BUILDLINK_DIR}/bin/scrollkeeper-rebuilddb
 
 scrollkeeper-buildlink: _BUILDLINK_USE
+
+scrollkeeper-buildlink-fake:
+	${_PKG_SILENT}${_PKG_DEBUG}					\
+	if [ ! -f ${_SK_FAKE_UPDATE} ]; then				\
+		${ECHO_BUILDLINK_MSG} "Creating ${_SK_FAKE_UPDATE}";	\
+		${MKDIR} ${_SK_FAKE_UPDATE:H};			\
+		${ECHO} "#!/bin/sh" > ${_SK_FAKE_UPDATE};		\
+		${ECHO} "echo '[pkgsrc] Doing nothing for scrollkeeper-update' \$$*" >> ${_SK_FAKE_UPDATE};		\
+		${CHMOD} +x ${_SK_FAKE_UPDATE};			\
+	fi
+	${_PKG_SILENT}${_PKG_DEBUG}					\
+	if [ ! -f ${_SK_FAKE_REBUILDDB} ]; then				\
+		${ECHO_BUILDLINK_MSG} "Creating ${_SK_FAKE_REBUILDDB}";	\
+		${MKDIR} ${_SK_FAKE_REBUILDDB:H};			\
+		${ECHO} "#!/bin/sh" > ${_SK_FAKE_REBUILDDB};		\
+		${ECHO} "echo '[pkgsrc] Doing nothing for scrollkeeper-rebuilddb' \$$*" >> ${_SK_FAKE_REBUILDDB};		\
+		${CHMOD} +x ${_SK_FAKE_REBUILDDB};			\
+	fi
 
 .endif	# SCROLLKEEPER_BUILDLINK2_MK
