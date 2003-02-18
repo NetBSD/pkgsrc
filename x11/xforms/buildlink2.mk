8f9c320d150dc3703fabd90426f738b953ac2f42
jmmv <jmmv@pkgsrc.org>
2003-02-18 16:18:48 +0000

    Sync with reality.  Patch provided by Marc Recht in PR pkg/20412.

--- x11/xforms/buildlink2.mk
+++ x11/xforms/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/12/28 19:49:07 jschauma Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/02/18 16:18:48 jmmv Exp $
 
 .if !defined(XFORMS_BUILDLINK2_MK)
 XFORMS_BUILDLINK2_MK=	# defined
@@ -9,10 +9,13 @@ BUILDLINK_PKGSRCDIR.xforms?=	../../x11/xforms
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.xforms=xforms
 BUILDLINK_PREFIX.xforms_DEFAULT=	${X11PREFIX}
-BUILDLINK_FILES.xforms=		include/X11/forms.h
+BUILDLINK_FILES.xforms+=	include/X11/flimage.h
+BUILDLINK_FILES.xforms+=	include/X11/forms.h
+BUILDLINK_FILES.xforms+=	lib/libflimage.*
 BUILDLINK_FILES.xforms+=	lib/libforms.*
-BUILDLINK_FILES.xforms+=	lib/libxforms.*
 
+.include "../../graphics/jpeg/buildlink2.mk"
+.include "../../graphics/tiff/buildlink2.mk"
 .include "../../graphics/xpm/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	xforms-buildlink
