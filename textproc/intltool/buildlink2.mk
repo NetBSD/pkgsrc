65aa83c7f0e748a54f721c5fc0c7c0ef74e2f3cd
jmmv <jmmv@pkgsrc.org>
2003-02-18 14:12:15 +0000

    When using this file, set a build dependancy on intltool by default. Usually it is only required during the build of a package.

--- textproc/intltool/buildlink2.mk
+++ textproc/intltool/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 18:40:02 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/02/18 14:12:15 jmmv Exp $
 
 .if !defined(INTLTOOL_BUILDLINK2_MK)
 INTLTOOL_BUILDLINK2_MK=	# defined
@@ -6,10 +6,12 @@ INTLTOOL_BUILDLINK2_MK=	# defined
 BUILDLINK_PACKAGES+=		intltool
 BUILDLINK_DEPENDS.intltool?=	intltool>=0.18
 BUILDLINK_PKGSRCDIR.intltool?=	../../textproc/intltool
+BUILDLINK_DEPMETHOD.intltool?=	build
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.intltool=intltool
 BUILDLINK_PREFIX.intltool_DEFAULT=	${LOCALBASE}
 
+USE_PERL5?=	build
 .include "../../lang/perl5/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	intltool-buildlink
