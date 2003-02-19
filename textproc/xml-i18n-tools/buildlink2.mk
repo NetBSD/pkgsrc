5452db964b4b2e31f69e5683a3fec2a64ed4da17
jmmv <jmmv>
2003-02-19 14:20:02 +0000

    Bump versions to avoid conflicts between intltool and xml-i18n-tools packages as suggested by tron.

--- textproc/xml-i18n-tools/buildlink2.mk
+++ textproc/xml-i18n-tools/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1 2003/01/05 05:01:21 rh Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/02/19 14:20:02 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use xml-i18n-tools.
 #
@@ -7,7 +7,7 @@
 XML_I18N_TOOLS_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			xml-i18n-tools
-BUILDLINK_DEPENDS.xml-i18n-tools?=	xml-i18n-tools>=0.9
+BUILDLINK_DEPENDS.xml-i18n-tools?=	xml-i18n-tools>=0.9nb1
 BUILDLINK_PKGSRCDIR.xml-i18n-tools?=	../../textproc/xml-i18n-tools
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.xml-i18n-tools=xml-i18n-tools
