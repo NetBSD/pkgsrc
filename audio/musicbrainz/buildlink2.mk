a88c01f62f0372cd3f587c250efcfb71140c4ab1
salo <salo@pkgsrc.org>
2003-02-20 16:02:34 +0000

    Updated to version 2.0.0.
    Addresses PR pkg/20446 by Matthias Friedrich.
    
    Changes:
    
    2.0.0:
    ======
    - Removed some spurious C++ comments from mb_c.h
    - Fixes and improvements to the python language bindings and tools
    - Updated NetBSD cdrom reading code
    
    2.0.0-pre4:
    ===========
    - Two new functions have been added:
        mb_GetFragmentFromURL - Retrieve a fragment from a URI
        mb_GetOrdinalFromList - Retrieve a list ordinal from a given list --
                                this is sometimes needed to determine the
                                location of an item in a list. (e.g. a track
                                in an album in order to find the track number)
    - The TRM code has been updated for TRM B#er.
    - New queries have been added to queries.h in order to support the improved
      FileLookupInterface.
    - The client library now uses the mm-2.1 and mq-1.1 namespaces.
    - The client library will now be referred to as libmusicbrainz
    
    2.0.0-pre1:
    ===========
    - Added authentication support
    - Added new queries
    - Added support for submitting client version to the server during TRM
      submits
    - Updated client code for TRM B#er

--- audio/musicbrainz/buildlink2.mk
+++ audio/musicbrainz/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:21:55 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/02/20 16:02:35 salo Exp $
 
 .if !defined(MUSICBRAINZ_BUILDLINK2_MK)
 MUSICBRAINZ_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			musicbrainz
-BUILDLINK_DEPENDS.musicbrainz?=		musicbrainz>=1.1.0
+BUILDLINK_DEPENDS.musicbrainz?=		musicbrainz>=2.0.0
 BUILDLINK_PKGSRCDIR.musicbrainz?=	../../audio/musicbrainz
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.musicbrainz=musicbrainz
