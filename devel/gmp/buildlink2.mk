f23641db11c4fabfd1aa26c093681e27905cf5a3
mycroft <mycroft@pkgsrc.org>
2003-02-21 12:25:22 +0000

    Update to gmp 4.1.2.  Many changes, but the relevant bug fixes seems to be:
    	* mpn/generic/rootrem.c: In Newton loop, pad qp with leading zero.
    
    	* mpn/generic/rootrem.c: Allocate 1.585 (log2(3)) times more space
            for pp temporary to allow for worst case overestimate of root.
    	Add some asserts.
    
    	* tests/mpz/t-root.c: Generalize and speed up.
    
            * mpfr/pow.c: Fixed bug (infinite loop) for exact powers.
    
    	* mpfr/sub.c: Fixed wrong inexact flag for a - b where a and b are of
            different signs and EXP(a) < EXP(b).
    
    	* printf/printffuns.c (gmp_fprintf_reps): Make it actually work
    	for padding > 256.
    
            * gmp-impl.h (USE_LEADING_REGPARM): Disable for PIC code generation.
    
    	* mpn/generic/tdiv_qr.c: Remove a bogus assert.
    
    	* mpn/generic/mode1o.c, mpn/alpha/ev5/mode1o.c: Correction to ASSERTs.
    	Reported by Christoph Ludwig.
    
    	* mpf/urandomb.c: Truncate nbits to precision of rop.
    
    	* configure.in (sparc v9 *bsd*): Add NetBSD and OpenBSD sparc64.
    	(x86 openbsd*): Extra underscore for _GLOBAL_OFFSET_TABLE_.
    
    	* mpn/generic/rootrem.c: Avoid overflow in xnb calculation.
    	* mpz/root.c: Avoid overflow in rootnb calculation.
    
    	* gmpxx.h (__gmp_binary_equal, __gmp_binary_not_equal): Fix broken
    	mpq/double functions.
    
    	* mpn/m68k/lshift.asm: Fix typo in !scale_available_p code.
            * mpn/m68k/rshift.asm: Likewise.
    
    	* mpz/remove.c: Make src==0 return 0, not do DIVIDE_BY_ZERO.
    
    	* mpfr/acinclude.m4 (MPFR_CONFIGS): Patch by Vincent for an apparent
    	float rounding gremlin on powerpc.
    
    	* mpf/inp_str.c: Fix returned count of chars read, reported by Paul
    	Zimmermann.  Also fix a memory leak for invalid input.
    
    	* mpfr/set_q.c: Allow for 1 bit numerator or denominator.

--- devel/gmp/buildlink2.mk
+++ devel/gmp/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.1 2002/08/27 06:23:53 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/02/21 12:25:22 mycroft Exp $
 
 .if !defined(GMP_BUILDLINK2_MK)
 GMP_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		gmp
-BUILDLINK_DEPENDS.gmp?=		gmp>=4.1
+BUILDLINK_DEPENDS.gmp?=		gmp>=4.1.2
 BUILDLINK_PKGSRCDIR.gmp?=	../../devel/gmp
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.gmp=gmp
