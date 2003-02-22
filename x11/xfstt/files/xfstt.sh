870ecab533c9b5a900cc03afd2454e628a5b8fda
salo <salo@pkgsrc.org>
2003-02-22 13:33:31 +0000

    Updated to version 1.4.
    Changes:
    
    - added MESSAGE file with post-install instructions
    - minor cleanups
    
    1.4:
    ====
    - Auto* tools
    - Initial i18n support (not used yet)
    - Fixed slow down with some programs using FS_ListFontsWithXInfo
    - Fixed childs unlinking unix socket when father uid == childs uid
    - Added an option to specify pidfile location
    - Added endpwent() to prevent leaking one fd
    - Changed usage output
    
    1.3.1:
    ======
    - Added --cache description to man page

--- x11/xfstt/files/xfstt.sh
+++ x11/xfstt/files/xfstt.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: xfstt.sh,v 1.4 2002/09/29 12:58:37 wiz Exp $
+# $NetBSD: xfstt.sh,v 1.5 2003/02/22 13:33:31 salo Exp $
 #
 # PROVIDE: xfstt
 # REQUIRE: DAEMON
@@ -8,6 +8,7 @@
 . /etc/rc.subr
 
 name="xfstt"
+rcvar=$name
 command="@PREFIX@/bin/${name}"
 command_args="--user nobody --notcp --daemon"
 pidfile="/var/run/${name}.pid"
