25b58baba55f3e2f44496dc64840bc37c0167549
jtb <jtb@pkgsrc.org>
2003-02-22 00:51:58 +0000

    Update to version 4.0.1.
    Added my own makefiles to create shared versions of the libraries
    using bsd.lib.mk.
    
    Changes in Lua itself:
    
    lua/src/lapi.c
    lua/src/lstring.c
    	Fixed a bug in lua_pushuserdata(L, NULL)
    lua/src/ldo.c
    lua/src/lgc.c
    lua/src/lgc.h
    	Give a good chance for GC before parsing
    lua/src/lparser.c
    	Fixed a bug (did not accept `;' after a `return')
    lua/src/lvm.c
    	Fixed a bug (linehook off by 1)
    lua/src/lib/lbaselib.c
    	Fixed a bug in rawget and rawset (seg. fault if given extra arguments)
    	Fixed a bug in dostring (identification of precompiled chunks)

--- lang/lua/buildlink2.mk
+++ lang/lua/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1 2003/01/26 21:38:23 salo Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/02/22 00:51:58 jtb Exp $
 #
 # This Makefile fragment is included by packages that use lua.
 #
@@ -7,7 +7,7 @@
 LUA_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		lua
-BUILDLINK_DEPENDS.lua?=		lua>=4.0
+BUILDLINK_DEPENDS.lua?=		lua>=4.0.1
 BUILDLINK_PKGSRCDIR.lua?=	../../lang/lua
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.lua=lua
