29a9a901bb054d06b155c90e080caa5ed1da2340
grant <grant@pkgsrc.org>
2003-02-28 23:29:34 +0000

    mark Linux zlib as incompatible to avoid using base zlib which can cause problems with pkgsrc libtool, eg.
    libtool: link: AGE `4' is greater than the current interface number `1'
    libtool: link: `1:1:4' is not valid version information
    libtool: install: `libz.la' is not a valid libtool archive

--- devel/zlib/buildlink2.mk
+++ devel/zlib/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.5 2003/02/16 17:33:16 grant Exp $
+# $NetBSD: buildlink2.mk,v 1.6 2003/02/28 23:29:34 grant Exp $
 
 .if !defined(ZLIB_BUILDLINK2_MK)
 ZLIB_BUILDLINK2_MK=	# defined
@@ -26,6 +26,9 @@ _INCOMPAT_ZLIB+=	IRIX-*-*
 
 # So does Darwin
 _INCOMPAT_ZLIB+=	Darwin-*-*
+
+# So does Linux
+_INCOMPAT_ZLIB+=	Linux-*-*
 
 #
 # Some NetBSD versions shipped with versions lower than 1.1.3.
