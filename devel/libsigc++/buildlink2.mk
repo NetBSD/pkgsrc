eb13939fd6cdbef5d6191795f0a087561be93005
jmmv <jmmv>
2003-03-01 11:54:07 +0000

    Update to 1.2.3.  This version is almost a rewrite of the previous 1.0.x branch (incompatible with it).

--- devel/libsigc++/buildlink2.mk
+++ devel/libsigc++/buildlink2.mk
@@ -1,19 +1,24 @@
-# $NetBSD: buildlink2.mk,v 1.4 2002/09/17 05:46:54 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.5 2003/03/01 11:54:07 jmmv Exp $
+#
+# This Makefile fragment is included by packages that use libsigc++.
+#
+# This file was created automatically using createbuildlink 2.4.
+#
 
 .if !defined(LIBSIGCXX_BUILDLINK2_MK)
 LIBSIGCXX_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=		libsigcxx
-BUILDLINK_DEPENDS.libsigcxx?=	libsigc++>=1.0.4nb1
-BUILDLINK_PKGSRCDIR.libsigcxx?=	../../devel/libsigc++
+BUILDLINK_PACKAGES+=			libsigcxx
+BUILDLINK_DEPENDS.libsigcxx?=		libsigc++>=1.2.3
+BUILDLINK_PKGSRCDIR.libsigcxx?=		../../devel/libsigc++
 
-EVAL_PREFIX+=			BUILDLINK_PREFIX.libsigcxx=libsigc++
+EVAL_PREFIX+=	BUILDLINK_PREFIX.libsigcxx=libsigcxx
 BUILDLINK_PREFIX.libsigcxx_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.libsigcxx=	include/sigc++-1.0/sigc++/*
-BUILDLINK_FILES.libsigcxx+=	lib/sigc++-1.0/include/sigc++config.h
-BUILDLINK_FILES.libsigcxx+=	lib/libsigc.*
-
-.include "../../mk/pthread.buildlink2.mk"
+BUILDLINK_FILES.libsigcxx+=	include/sigc++-1.2/sigc++/*
+BUILDLINK_FILES.libsigcxx+=	include/sigc++-1.2/sigc++/macros/*
+BUILDLINK_FILES.libsigcxx+=	lib/libsigc-1.2.*
+BUILDLINK_FILES.libsigcxx+=	lib/pkgconfig/sigc++-1.2.pc
+BUILDLINK_FILES.libsigcxx+=	lib/sigc++-1.2/include/*
 
 BUILDLINK_TARGETS+=	libsigcxx-buildlink
 
