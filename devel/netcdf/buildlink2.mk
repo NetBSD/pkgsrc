6d0b9f629f60bde9dc770f42e8ab24266893eb6b
jtb <jtb>
2003-03-02 08:26:09 +0000

    Update to 3.5.0.
    Some relevant changes:
    
     Fixed miswriting of netCDF header when exiting define
     mode.  Because the header was always written correctly
     later, this was only a problem if there was another
     reader of the netCDF file.
    
     Fixed explicit synchronizing between netCDF writer and
     readers via the nc_sync(), nf_sync(), and ncsync()
     functions.
    
     Fixed a number of bugs related to attempts to support
     shrinking the header in netCDF files when attributes
     are rewritten or deleted.  Also fixed the problem that
     nc__endef() did not work as intended in reserving
     extra space in the file header, since the extra space
     would be compacted again on calling nc_close().
    
     Fixed the "redef bug" that occurred when nc_enddef()
     or nf_enddef() is called after nc_redef() or
     nf_redef(), the file is growing such that the new
     beginning of a record variable is in the next "chunk",
     and the size of at least one record variable exceeds
     the chunk size (see netcdf.3 man page for a
     description of this tuning parameter and how to set
     it).  This bug resulted in corruption of some values
     in other variables than the one being added.
    
     The "__" tuning functions for the Fortran interface,
     nf__create, nf__open, and nf__enddef, are now
     documented in the Fortran interface man pages.
    
     Changed the extension of C++ files from ".cc" to ".cpp".
     Renamed the C++ interface header file "netcdfcpp.h"
     instead of "netcdf.hh", changing "netcdf.hh" to
     include "netcdfcpp.h" for backward compatibility.
    
     Fixed bug in ncdump using same CDL header name when
     called with multiple files.
    
     Added new NULL data type  NC_NAT (Not A Type) to
     facilitate checking whether a variable object has had
     its type defined yet, for example when working with
     packed values.
    
     Fixed use of compile-time macro NO_NETCDF_2 so it
     really doesn't include old netCDF-2 interfaces, as
     intended.
    
     Fixed C++ friend declarations to conform to C++ standard.

--- devel/netcdf/buildlink2.mk
+++ devel/netcdf/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:22:34 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/03/02 08:26:09 jtb Exp $
 
 .if !defined(NETCDF_BUILDLINK2_MK)
 NETCDF_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		netcdf
-BUILDLINK_DEPENDS.netcdf?=	netcdf>=3.4
+BUILDLINK_DEPENDS.netcdf?=	netcdf>=3.5
 BUILDLINK_PKGSRCDIR.netcdf?=	../../devel/netcdf
 
 EVAL_PREFIX+=				BUILDLINK_PREFIX.netcdf=netcdf
@@ -12,7 +12,7 @@ BUILDLINK_PREFIX.netcdf_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.netcdf=			include/netcdf.h
 BUILDLINK_FILES.netcdf+=		include/netcdf.inc
 BUILDLINK_FILES.netcdf+=		include/netcdf.hh
-BUILDLINK_FILES.netcdf+=		include/ncvalues.hh
+BUILDLINK_FILES.netcdf+=		include/ncvalues.h
 BUILDLINK_FILES.netcdf+=		lib/libnetcdf.*
 BUILDLINK_FILES.netcdf+=		lib/libnetcdf_c++.*
 
