00e799491cdc6a967d8777e1a37d6344e0ec92d6
seb <seb@pkgsrc.org>
2003-03-03 14:22:41 +0000

    Use ${_OPSYS_RPATH_NAME} instead of bare `-R'

--- x11/Xaw3d/buildlink2.mk
+++ x11/Xaw3d/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/23 08:29:59 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/03/03 14:22:41 seb Exp $
 
 .if !defined(XAW3D_BUILDLINK2_MK)
 XAW3D_BUILDLINK2_MK=	# defined
@@ -6,6 +6,8 @@ XAW3D_BUILDLINK2_MK=	# defined
 BUILDLINK_PACKAGES+=		Xaw3d
 BUILDLINK_DEPENDS.Xaw3d?=	Xaw3d-1.5
 BUILDLINK_PKGSRCDIR.Xaw3d?=	../../x11/Xaw3d
+
+.include "../../mk/bsd.prefs.mk"
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.Xaw3d=Xaw3d
 BUILDLINK_PREFIX.Xaw3d_DEFAULT=	${X11PREFIX}
@@ -16,7 +18,7 @@ BUILDLINK_FILES.Xaw3d+=		lib/libXaw3d.*
 BUILDLINK_TARGETS+=	Xaw3d-buildlink
 
 LIBXAW?=	-L${BUILDLINK_PREFIX.Xaw3d}/lib				\
-		-Wl,-R${BUILDLINK_PREFIX.Xaw3d}/lib			\
+		-Wl,${_OPSYS_RPATH_NAME}${BUILDLINK_PREFIX.Xaw3d}/lib	\
 		-lXaw3d
 
 Xaw3d-buildlink: _BUILDLINK_USE
