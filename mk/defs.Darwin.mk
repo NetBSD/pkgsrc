241f2f19a8ae4ca0239649c4c21c61b0b086f5e2
seb <seb>
2003-03-04 14:50:09 +0000

    Move M4 default definition into the defs.$OPSYS.mk files: unconditionally set it to /usr/bin/m4 for everyone, at least for now.

--- mk/defs.Darwin.mk
+++ mk/defs.Darwin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.Darwin.mk,v 1.47 2003/02/28 03:41:39 jschauma Exp $
+# $NetBSD: defs.Darwin.mk,v 1.48 2003/03/04 14:50:10 seb Exp $
 #
 # Variable definitions for the Darwin operating system.
 
@@ -34,6 +34,7 @@ ID?=		/usr/bin/id
 LDCONFIG?=	/sbin/ldconfig
 LN?=		/bin/ln
 LS?=		/bin/ls
+M4?=		/usr/bin/m4 
 MKDIR?=		/bin/mkdir -p
 MTREE?=		/usr/sbin/mtree
 MV?=		/bin/mv
