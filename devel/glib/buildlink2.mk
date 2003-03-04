6d25b2f983fb41898c765f5fe14d01a4e8354d1d
salo <salo@pkgsrc.org>
2003-03-04 19:53:26 +0000

    Make the last fix conditional, fix typo, delint. Bump PKGREVISION.

--- devel/glib/buildlink2.mk
+++ devel/glib/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/10 19:44:48 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/03/04 19:53:26 salo Exp $
 
 .if !defined(GLIB_BUILDLINK2_MK)
 GLIB_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		glib
-BUILDLINK_DEPENDS.glib?=	glib>=1.2.8
+BUILDLINK_DEPENDS.glib?=	glib>=1.2.10nb4
 BUILDLINK_PKGSRCDIR.glib?=	../../devel/glib
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.glib=glib
