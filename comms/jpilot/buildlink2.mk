e99332c58358d5e4198baf7573423dd06e595022
wiz <wiz@pkgsrc.org>
2003-03-04 16:29:28 +0000

    Update to 0.99.5, now using GTK2.
    0.99.5 = 02/21/03
     Fixed serious bug resulting in overwriting appointments on the palm (Serious bug)
     Restore now restores latest files instead of ones from last backup (Critical bug)
     Added -export-dynamic to Makefile to resolve jp_logf unresolved errors
     Made home key go to today in datebook
     Added dialog error windows instead of logging them
     If files are to be installed a prompt window opens before a sync/backup
     Browser windows open from jpilot menu
     When app button is pressed it will cycle through categories
    
    0.99.4 - 01/13/03
     Ported code to GTK-2 (I used GTK-2.2)
     Added random password generation to KeyRing
     Added a dialer program and GUI launcher
     Added Syncronization of categories and editing categories on desktop
     Added Export of iCalendar format from ToDo and Datebook
     Added Export of vCard format from Address book
     Added Export of ldif format from Address book
     Added Russian Language Support
     Removed jpilot-upgrade-99

--- comms/jpilot/buildlink2.mk
+++ comms/jpilot/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/10 16:06:35 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/03/04 16:29:29 wiz Exp $
 
 .if !defined(JPILOT_BUILDLINK2_MK)
 JPILOT_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		jpilot
-BUILDLINK_DEPENDS.jpilot?=	jpilot>=0.99nb2
+BUILDLINK_DEPENDS.jpilot?=	jpilot>=0.99.5
 BUILDLINK_PKGSRCDIR.jpilot?=	../../comms/jpilot
 BUILDLINK_DEPMETHOD.jpilot?=	build
 
@@ -12,7 +12,7 @@ EVAL_PREFIX+=			BUILDLINK_PREFIX.jpilot=jpilot
 BUILDLINK_PREFIX.jpilot_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.jpilot=		include/jpilot/*
 
-.include "../../x11/gtk/buildlink2.mk"
+.include "../../x11/gtk2/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	jpilot-buildlink
 
