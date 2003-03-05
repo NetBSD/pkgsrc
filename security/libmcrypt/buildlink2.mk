5bc6010865b82f553a642562929b2de49d5b919a
wiz <wiz@pkgsrc.org>
2003-03-05 08:42:55 +0000

    Update to 2.5.6. January 19 2003: (version 2.5.6) - Fixes in win32 detection and DLL building. - Fixes for solaris (a symbol was not exported) - Corrected bug which made algorithm symbols to be inserted   twice in symbol table.
    December 22 2002: (version 2.5.5)
    - Better win32 detection and DLL building.
    - Changed some variables names in gost.c to allow compiling
      with gcc and K6 optimizations.
    - Some buffer overrun checks on input. Patches and suggestions
      by Ilia A.
    - Made the default behaviour to include all algorithms into
      the main library, instead of using dynamic modules. Dynamic
      loading is disabled by default. The --enable-dynamic-loading flag
      can be used in the configure script, to get the old behaviour.
    - Some fixes in ECB mode.
    
    August 16 2002: (version 2.5.3)
    - The const keyword is now used in the exported functions
    - Corrected problem in libltdl's configure script
    
    June 22 2002: (version 2.5.2)
    - Fixed bug in the returned IV size of ARCFOUR
    - Fixed bug in mcrypt_readdir() which prevented the test programs
      to work.
    
    May 30 2002: (version 2.5.1)
    - Corrected the license. COPYING.LIB (LGPL) is now included
      instead of COPYING (GPL)
    
    Mar 09th 2002: (version 2.5.0)
    - Several corrections in stream and block cipher modes.
    - Added --disable-dynamic-loading configure option
    - The IV modifications in Arcfour and Wake have been disabled
      by default.
    - Added CTR mode for block ciphers.
    - Fixes in nCFB and nOFB modes.
    - Added mcrypt_enc_get_state() function.
    - Added test for nCFB, CFB, nOFB and CTR with AES
    
    Feb 14th 2002:
    - nOFB and nCFB modes can now encrypt and decrypt plaintext
      of size less than block size.

--- security/libmcrypt/buildlink2.mk
+++ security/libmcrypt/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 18:39:53 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/03/05 08:42:56 wiz Exp $
 
 .if !defined(LIBMCRYPT_BUILDLINK2_MK)
 LIBMCRYPT_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libmcrypt
-BUILDLINK_DEPENDS.libmcrypt?=	libmcrypt>=2.4.17
+BUILDLINK_DEPENDS.libmcrypt?=	libmcrypt>=2.5.6
 BUILDLINK_PKGSRCDIR.libmcrypt?=	../../security/libmcrypt
 
 EVAL_PREFIX+=				BUILDLINK_PREFIX.libmcrypt=libmcrypt
@@ -12,8 +12,6 @@ BUILDLINK_PREFIX.libmcrypt_DEFAULT=	${LOCALBASE}
 
 BUILDLINK_FILES.libmcrypt=	include/mcrypt.h
 BUILDLINK_FILES.libmcrypt+=	lib/libmcrypt.*
-
-.include "../../devel/libtool/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	libmcrypt-buildlink
 
