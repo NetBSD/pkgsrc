b4ddb66d7e7d3f17e6dd0871ba5390890e6dadbf
dmcmahill <dmcmahill@pkgsrc.org>
2003-03-09 06:05:57 +0000

    update to 2.0.9
    detailed list of changes not available, but this update is needed to support
    the 20030223 release of the gEDA suite.

--- graphics/libgdgeda/buildlink2.mk
+++ graphics/libgdgeda/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/12/13 03:18:00 dmcmahill Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/03/09 06:05:58 dmcmahill Exp $
 #
 # This Makefile fragment is included by packages that use libgdgeda.
 #
@@ -9,7 +9,7 @@
 LIBGDGEDA_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			libgdgeda
-BUILDLINK_DEPENDS.libgdgeda?=		libgdgeda>=1.8nb1
+BUILDLINK_DEPENDS.libgdgeda?=		libgdgeda>=2.0.6
 BUILDLINK_PKGSRCDIR.libgdgeda?=		../../graphics/libgdgeda
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libgdgeda=libgdgeda
@@ -22,6 +22,7 @@ BUILDLINK_FILES.libgdgeda+=	include/gdgeda/gdfontmb.h
 BUILDLINK_FILES.libgdgeda+=	include/gdgeda/gdfonts.h
 BUILDLINK_FILES.libgdgeda+=	include/gdgeda/gdfontt.h
 BUILDLINK_FILES.libgdgeda+=	include/gdgeda/gdcache.h
+BUILDLINK_FILES.libgdgeda+=	include/gdgeda/gdhelpers.h
 BUILDLINK_FILES.libgdgeda+=	include/gdgeda/jisx0208.h
 BUILDLINK_FILES.libgdgeda+=	include/gdgeda/wbmp.h
 BUILDLINK_FILES.libgdgeda+=	lib/libgdgeda.*
