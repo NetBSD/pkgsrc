1f5126c04eb3b074bcfb4fd30650a615c3edf2b5
markd <markd@pkgsrc.org>
2003-03-11 13:22:19 +0000

    Update KDE to 3.1.
    Many bugfixes and functionality enhancements including:
    
    kdeaddons
    
     NEW IN KDE: Noatun plugin to search for lyrics of the current song.
    
    kdeadmin
    
     kcmlinuz: is now a standalone application
     kcmlinuz: Added capability to search for configuration options
    
    
    kdeedu
    
     NEW IN KDE: Kiten, a Japanese Reference Tool.
     NEW IN KDE: FlashKard, a vocabulary tool. Is able to read KVTML files which
     were written by KVocTrain.
     NEW IN KDE: started a libkdeedu, which allows interopability and code
     sharing between KDE Educational applications.
    
    
    kdepim
    
     libkabc
       Plugin interface for resources and formats.
       Control module for configuring multiple resources.
       Optional directory resource which stores each entry as own file.
       LDAP resource.
       Optional binary storage format for better performance with large
       addressbooks.
       Completed support for vCard entities: sound, geo, key and some more.
       vCard 2.1 import.
       LDAP aware mail address input field.
     KAddressbook
       New card and icon views.
       Multiple configurable views.
       "kab-style" details view for selected entry.
       Customizable view filters.
       Customizable categories for addressbook entries.
       Support for transparently accessing multiple addressbooks.
       Redesigned entry editor widget.
       Printing option supporting two different styles.
       Optional embedded entry and ditribution list editor.
       "Jump bar" for quick alphabetical access of entries.
       Fetch addresses from LDAP servers, including support for multiple servers.
       Importing of vCard 2.1 files.
       Improved CSV import (including template for import from Outlook 2000).
       vCard export.
     KAlarm
       Added option to execute commands in alarms.
       Implemented daily, weekly, monthly and annual recurrences.
       Optionally play audio notification when an alarm triggers.
     KPilot
       Added time conduit
       VCal conduits (TODO and Calendar) have been improved to function properly.
       Addressbook conduit has been ported to use libkabc.
     KOrganizer
       General
         User definable templates for events and TODOs.
         Alarms for TODOs.
         Added support for automatic HTML export on save of a calendar file.
         Time table print view.
         New "location" attribute for events.
         Experimental "Get Hot New Stuff" button for downloading and uploading
         calendar files of common interest.
       Views
         Added new "Next 3 days" view.
         Added selection of time span for a new event in day and week views.
         Direct manipulation of priority, completion status and categories of
         TODOs by context menus added.
         Deletion of individual instances of recurring events.
         Rewritten month view.
         Coloring of events in month view based on categories.
         Coloring of due and overdue todos.
         Improved "What's Next" view.
         Configurable cell height in week and day views
       Group scheduling
         iMIP group scheduling functions for TODOs.
         Publishing of Free/Busy information by iMIP conformant email.
         Improved automatisation of group scheduling.
       Interopability and integration
         Support for "webcal" URLs in Konqueror (known from Apple iCal).
         Support for iCalendar based drag&drop.
         KOrganizer now shares the dialogs for categories with KAddressBook.
         Improved iCalendar conformance.
         Improved right-to-left languages support.
         Bug fixes for non-latin1 encodings.
       Plugins
         New plugin for importing Birthdays of contacts in the KDE address book.
         New plugin for accessing calendar data stored on an Exchange 2000 server.
    
    
    kdeutils
    
     Partially ACPI support in KLaptop.

--- misc/kdeutils3/buildlink2.mk
+++ misc/kdeutils3/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1 2003/01/09 11:21:42 uebayasi Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/03/11 13:22:31 markd Exp $
 #
 # This Makefile fragment is included by packages that use kdeutils.
 #
@@ -9,7 +9,7 @@
 KDEUTILS_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			kdeutils
-BUILDLINK_DEPENDS.kdeutils?=		kdeutils>=3.0.5.1
+BUILDLINK_DEPENDS.kdeutils?=		kdeutils>=3.1
 BUILDLINK_PKGSRCDIR.kdeutils?=		../../misc/kdeutils3
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.kdeutils=kdeutils
