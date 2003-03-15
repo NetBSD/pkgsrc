12f467f77fe5965965b57248acc8c0eb5e212971
kei <kei@pkgsrc.org>
2003-03-15 08:17:07 +0000

    update teTeX2 and related packages to version 2.0.2.  Changes from announce are following:
    with the dvipdfm -> dvipdfm/config change, dvipdfmx package can easily
    coexist with dvipdfm of teTeX2.
    
    ----------------
    
    ===========================================================================
      Changes in this release:
    ===========================================================================
    
    The main change is the new pdftex which has a few important fixes. For
    details, please look at tetex-src-2.0.2/texk/web2c/pdftexdir/NEWS.
    
    ChangeLog entries:
    
    In the src tarball:
            * portability fix for texk/kpathsea/cnf.c
            * pdftex version 1.10b
            * add eomega support to fmtutil
            * ttf2afm: FontFamily patch by Siep Kroonenberg
            * update config.guess / config.sub
            * handle manpages for kpsepath / kpsexpand properly
            * fix two bugs in texdoctk
            * mktexmf fix for CJK fonts
            * adjust cxx-runtime-hack a little
            * add search paths for non"k"-xdvi to texmf.in
            * fix bug in texk/etc/autoconf/acspecific.m4
    
    In the texmf tarball:
            * add doc/latex/styles/multiple-span.txt
            * move dvipdfm/* to dvipdfm/config
            * update ragged2e
            * support uppercase mathtime fonts via aliases
            * remove g-brief: it is not free software
            * update shadow.sty
            * add package mparhack and jurabib
            * listings package is back (license *is* ok!)
            * SIunits.cfg updated (only comments have changed in a minor way)
            * replace old README by new readme in doc/latex/pdfpages
            * added sihyph23.tex (Slovenian hyphenation patterns)
    
    ----------------

--- print/teTeX2-bin/kpathsea.buildlink2.mk
+++ print/teTeX2-bin/kpathsea.buildlink2.mk
@@ -1,14 +1,14 @@
-# $NetBSD: kpathsea.buildlink2.mk,v 1.1.1.1 2003/02/23 04:59:25 kei Exp $
+# $NetBSD: kpathsea.buildlink2.mk,v 1.2 2003/03/15 08:17:08 kei Exp $
 
 .if !defined(KPATHSEA_BUILDLINK2_MK)
 KPATHSEA_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		kpathsea
-BUILDLINK_DEPENDS.kpathsea?=	teTeX-bin>=2.0.1
+BUILDLINK_DEPENDS.kpathsea?=	teTeX2-bin>=2.0.1
 BUILDLINK_PKGSRCDIR.kpathsea?=	../../print/teTeX2-bin
 BUILDLINK_DEPMETHOD.kpathsea?=	build
 
-EVAL_PREFIX+=		BUILDLINK_PREFIX.kpathsea=teTeX-bin
+EVAL_PREFIX+=		BUILDLINK_PREFIX.kpathsea=teTeX2-bin
 BUILDLINK_PREFIX.kpathsea_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.kpathsea=	include/kpathsea/*
 BUILDLINK_FILES.kpathsea+=	lib/libkpathsea.*
