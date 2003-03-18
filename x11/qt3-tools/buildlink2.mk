f196647017d17ee63a5b6627e80ef97f3a6469bc
skrll <skrll@pkgsrc.org>
2003-03-18 08:36:22 +0000

    Update qt3 to 3.1.2. This update fixes a large number of bugs.

--- x11/qt3-tools/buildlink2.mk
+++ x11/qt3-tools/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.7 2003/03/11 13:02:09 markd Exp $
+# $NetBSD: buildlink2.mk,v 1.8 2003/03/18 08:36:26 skrll Exp $
 
 .if !defined(QT3_TOOLS_BUILDLINK2_MK)
 QT3_TOOLS_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		qt3-tools
-BUILDLINK_DEPENDS.qt3-tools?=	qt3-tools>=3.1.1
+BUILDLINK_DEPENDS.qt3-tools?=	qt3-tools>=3.1.2
 BUILDLINK_PKGSRCDIR.qt3-tools?=	../../x11/qt3-tools
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.qt3-tools=qt3-tools
