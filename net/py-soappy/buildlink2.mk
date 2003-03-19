eef4739e242c406e65f218f7ff5af9ecb1dcb08d
drochner <drochner@pkgsrc.org>
2003-03-19 11:51:08 +0000

    update to 0.9.8, provided by Marc Recht (PR pkg/20787) Notes: This is SOAPpy 0.9.8.    It contains substantial improvements over the last release. 1) SOAPpy now uses the standard python    package installation tools.  Executing the commands      $ python setup.py build      $ python setup.py install    (the second as root) should install SOAPpy into the appropriate Python    library directory. 2) There has been inconsistency in how  SOAPpy library components have been    imported.  This new release standardizes on:      from SOAPpy import SOAP 3) See the changelog for further enhancements.

--- net/py-soappy/buildlink2.mk
+++ net/py-soappy/buildlink2.mk
@@ -1,7 +1,14 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/10/24 13:05:34 drochner Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/03/19 11:51:09 drochner Exp $
+
+.if !defined(SOAPPY_BUILDLINK2_MK)
+SOAPPY_BUILDLINK2_MK=	# defined
 
 .include "../../lang/python/pyversion.mk"
 
-BUILDLINK_PACKAGES+=		pysoappy
-BUILDLINK_DEPENDS.pysoappy?=	${PYPKGPREFIX}-SOAPpy-*
-BUILDLINK_PKGSRCDIR.pysoappy?=	../../net/py-soappy
+BUILDLINK_PACKAGES+=			SOAPpy
+BUILDLINK_DEPENDS.SOAPpy?=		${PYPKGPREFIX}-SOAPpy>=0.9.8
+BUILDLINK_PKGSRCDIR.SOAPpy?=		../../net/py-soappy
+
+.include "../../textproc/pyxml/buildlink2.mk"
+
+.endif	# SOAPPY_BUILDLINK2_MK
