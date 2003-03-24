162832714e28f98a1bb3eb3b8a96716b3bef223e
wiz <wiz@pkgsrc.org>
2003-03-24 09:21:58 +0000

    Sync with package rename and update.

--- audio/speex/buildlink2.mk
+++ audio/speex/buildlink2.mk
@@ -1,31 +1,28 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/03/24 08:58:53 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/03/24 09:21:58 wiz Exp $
 #
-# This Makefile fragment is included by packages that use Speex.
-#
-# This file was created automatically using createbuildlink 2.2.
+# This Makefile fragment is included by packages that use speex.
 #
 
 .if !defined(SPEEX_BUILDLINK2_MK)
 SPEEX_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=			Speex
-BUILDLINK_DEPENDS.Speex?=		Speex>=0.99.3
-BUILDLINK_PKGSRCDIR.Speex?=		../../audio/Speex
+BUILDLINK_PACKAGES+=			speex
+BUILDLINK_DEPENDS.speex?=		speex>=1.0
+BUILDLINK_PKGSRCDIR.speex?=		../../audio/speex
 
-EVAL_PREFIX+=	BUILDLINK_PREFIX.Speex=Speex
-BUILDLINK_PREFIX.Speex_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.Speex+=	include/speex.h
-BUILDLINK_FILES.Speex+=	include/speex_bits.h
-BUILDLINK_FILES.Speex+=	include/speex_callbacks.h
-BUILDLINK_FILES.Speex+=	include/speex_header.h
-BUILDLINK_FILES.Speex+=	include/speex_stereo.h
-BUILDLINK_FILES.Speex+=	lib/libspeex-0.99.*
-BUILDLINK_FILES.Speex+=	lib/libspeex.*
+EVAL_PREFIX+=	BUILDLINK_PREFIX.speex=speex
+BUILDLINK_PREFIX.speex_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.speex+=	include/speex.h
+BUILDLINK_FILES.speex+=	include/speex_bits.h
+BUILDLINK_FILES.speex+=	include/speex_callbacks.h
+BUILDLINK_FILES.speex+=	include/speex_header.h
+BUILDLINK_FILES.speex+=	include/speex_stereo.h
+BUILDLINK_FILES.speex+=	lib/libspeex.*
 
 .include "../../audio/libogg/buildlink2.mk"
 
-BUILDLINK_TARGETS+=	Speex-buildlink
+BUILDLINK_TARGETS+=	speex-buildlink
 
-Speex-buildlink: _BUILDLINK_USE
+speex-buildlink: _BUILDLINK_USE
 
 .endif	# SPEEX_BUILDLINK2_MK
