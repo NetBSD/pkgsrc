49c204a621cb1694a65bcce9645ed2b0a5ceae9c
wiz <wiz>
2003-03-28 20:28:56 +0000

    Remove superfluous whitespace.

--- math/blas/buildlink2.mk
+++ math/blas/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/03/11 01:49:49 jmc Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/03/28 20:29:43 wiz Exp $
 
 .if !defined(BLAS_BUILDLINK2_MK)
 BLAS_BUILDLINK2_MK=	# defined
@@ -10,7 +10,7 @@ BUILDLINK_DEPMETHOD.blas?=	build
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.blas=blas
 BUILDLINK_PREFIX.blas_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.blas=		lib/libblas* 
+BUILDLINK_FILES.blas=		lib/libblas*
 
 BUILDLINK_TARGETS+=		blas-buildlink
 
