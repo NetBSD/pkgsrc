6f2588575825fab606b6f4f456b89a97b677f5ee
grant <grant>
2003-03-31 08:51:06 +0000

    Initial import of mtree-20030331 into The NetBSD Packages Collection.
    The mtree utility compares the file hierarchy rooted in the current
    directory against a specification read from the standard input.
    Messages are written to the standard output for any files whose
    characteristics do not match the specification, or which are missing
    from either the file hierarchy or the specification.

---
+++ pkgtools/mtree/files/acconfig.h
@@ -0,0 +1,8 @@
+/* $Id: acconfig.h,v 1.1.1.1 2003/03/31 08:51:33 grant Exp $ */
+
+/* Define if *printf() uses %qd to print `long long' (otherwise uses %lld) */
+#undef HAVE_PRINTF_QD
+
+/* Define if `long long' is supported and sizeof(off_t) >= 8 */
+#undef HAVE_QUAD_SUPPORT
+
