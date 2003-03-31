d5782ace09f71244d557ddc97db557365dc377ea
grant <grant>
2003-03-31 05:56:35 +0000

    s/nb-types.h/nbtypes.h/

--- pkgtools/libnbcompat/buildlink2.mk
+++ pkgtools/libnbcompat/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/03/31 05:07:52 grant Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/03/31 05:56:35 grant Exp $
 #
 
 .if !defined(LIBNBCOMPAT_BUILDLINK2_MK)
@@ -22,7 +22,7 @@ BUILDLINK_FILES.libnbcompat+=	include/libnbcompat/md5.h
 BUILDLINK_FILES.libnbcompat+=	include/libnbcompat/mtree.h
 BUILDLINK_FILES.libnbcompat+=	include/libnbcompat/namespace.h
 BUILDLINK_FILES.libnbcompat+=	include/libnbcompat/nbcompat.h
-BUILDLINK_FILES.libnbcompat+=	include/libnbcompat/nb-types.h
+BUILDLINK_FILES.libnbcompat+=	include/libnbcompat/nbtypes.h
 BUILDLINK_FILES.libnbcompat+=	include/libnbcompat/pack_dev.h
 BUILDLINK_FILES.libnbcompat+=	include/libnbcompat/pwcache.h
 BUILDLINK_FILES.libnbcompat+=	include/libnbcompat/rmd160.h
