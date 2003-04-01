6e6e3a820ec744317a991092dbcd9d78a4382a40
rh <rh>
2003-04-01 08:29:12 +0000

    Update gnustep-make to 1.6.0.  Changes include:
       Make now configures by default for only one system. To compile and
    run GNUstep for multiple platforms from the same directory, use the
    configure argument -enable-multi-platform.
    
       * Auto-build def file for DLLs
    
       * Work natively on MacOSX systems. Now use apple-apple-apple instead
         of nx-nx-nx on OSX systems.
    
       * More efficient checks for re-making a project.

--- devel/gnustep-make/buildlink2.mk
+++ devel/gnustep-make/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.5 2003/02/06 11:50:01 dmcmahill Exp $
+# $NetBSD: buildlink2.mk,v 1.6 2003/04/01 08:29:12 rh Exp $
 #
 
 .if !defined(GNUSTEP_MAKE_BUILDLINK2_MK)
 GNUSTEP_MAKE_BUILDLINK2_MK=		# defined
 
 BUILDLINK_PACKAGES+=			gnustep-make
-BUILDLINK_DEPENDS.gnustep-make?=	gnustep-make>=1.5.1nb1
+BUILDLINK_DEPENDS.gnustep-make?=	gnustep-make>=1.6.0
 BUILDLINK_PKGSRCDIR.gnustep-make?=	../../devel/gnustep-make
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gnustep-make=gnustep-make
