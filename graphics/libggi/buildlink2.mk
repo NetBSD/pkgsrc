b94d468d47c3675e06618018f35f7661b0e09dbd
wiz <wiz@pkgsrc.org>
2003-04-02 12:05:25 +0000

    Update to 2.0.3:  New in libggi 2.0.3:  * X-target:      * Fixed a bug, where a ggiFlush() call was missed, which makes        directbuffer content visible when releasing it.      * Fixed a clipping bug, causing to let disappear characters or        whole strings.      * Fixed clipping on multiple frames.      * Add -keepcursor option, which keeps the default cursor.      * Fixed crash when used 8bit characters      * Fixed endless loop when X11 server provides only and only        the same sort of X visuals  * Demo: Improved pageflip demo to test all primitives in a    multi-frame environment  * merged some compiler fixes from the OpenBSD ports tree  * fixed a crash, when a drawing primitive (i.e. a line) crossed    two borders of the clipping rectangle  * added 64bit fixes to wrap and cube3d demos  * minor improvements to checkmode and struct check tool    (important for target developers though)  * manpage fixes
    New in 2:2:0 (2.0.2): 2002-12-26
    * Misc bugfixes (see ChangeLog)
    * Huge documentation updates
    * Major build system update
      - adds builds fixes for many platforms
    * memory-target
      - add -pixfmt option
      - added missing support for reporting physical screen sizes.
    * X-target
      - rewritten (x/xlib splitting is obselete now)
      - added dirty region management
    * xlib-target
      - disabled. Will be removed later
    * fbdev-target
      - DirectFB driver works with directfb 0.9.13
      - added mach64 accelerator
    * KGI-target
      - big update (works with KGI-0.9 now)
      - added mach64 accelerator
      - added radeon accelerator
      - still experimental, thus disabled in configure
    
    New in 2:1:0 (2.0.1): 2001-08-26
    * More documentation updates.
    * Prelimiary support for using DirectFB drivers under display-fbdev
    * Many corrections/improvements to build files
    * Started move towards using libtool versioning, as interface is stable.
    
    New in 2.0: 2001-08-11
    * Huge documentation updates.
    * svgalib-target
      - added frame support
      - misc bug-fixes
    
    New in 2.0beta4: 2001-07-10
    * Misc bugfixes (see ChangeLog).
    * Huge documentation updates.
    * finished support for reporting physical screen sizes.
    * splitted libggimisc off
    * New API: ggiDetachInput()
    * fbdev-target
      - Acceleration of Matrox G400 (matrox fb)
    * directX-target
      - Update
      - Highly experimental target for Win ... Use at your own risk !
    * ipc-target
      - New target for interprocess communication (experimental)
    
    New in 2.0beta3: 2000-12-18
    * Misc bugfixes (see ChangeLog).
    * Minor documentation updates.
    * Major rewrite of the internal sublib handling.
    * Some fixes for PowerPC compatibility.
    * preliminary support for reporting physical screen sizes.
    * Made all headers C++ compatible.
    
    * lcd823-target
      - Target for the Motorola MPC823 builtin LCD controler, using
        the /dev/lcd823 Linux kernel device.
    * fbdev-target
      - Better detection of kgicon-type fb-devices and old fb implementations.
      - Better usage of fbdev mode infos in fb.modes
      - Better acceleration for the Matrox fb.
    * glide-target
      - Added directbuffer support.
    * aalib-target
      - Better mode handling.
    * X-target
      - Propagated X dpi values to give physical screen dimensions.
      - Various fixes to properly init several values.
    * tele-target
      - Updated much to make it work again.
    * vgl-target
      - New target for FreeBSD's VGL library.
    * directX-target
      - Highly experimental target for Win ... Use at your own risk !

--- graphics/libggi/buildlink2.mk
+++ graphics/libggi/buildlink2.mk
@@ -1,35 +1,15 @@
-# $NetBSD: buildlink2.mk,v 1.1 2002/11/25 20:50:26 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/04/02 12:05:26 wiz Exp $
 
 .if !defined(LIBGGI_BUILDLINK2_MK)
 LIBGGI_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libggi
-BUILDLINK_DEPENDS.libggi?=	libggi>=2.0b2.1
+BUILDLINK_DEPENDS.libggi?=	libggi>=2.0.3
 BUILDLINK_PKGSRCDIR.libggi?=	../../graphics/libggi
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libggi=libggi
 BUILDLINK_PREFIX.libggi_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.libggi+=	include/ggi/display/aa.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/fbdev.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/file.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/glide.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/linvtsw.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/mansync.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/memory.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/modelist.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/monotext.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/multi.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/palemu.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/sub.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/svgalib.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/tele.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/tile.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/trueemu.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/vcsa.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/x.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/xcommon.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/xf86dga.h
-BUILDLINK_FILES.libggi+=	include/ggi/display/xlib.h
+BUILDLINK_FILES.libggi+=	include/ggi/display/*
 BUILDLINK_FILES.libggi+=	include/ggi/ggi-unix.h
 BUILDLINK_FILES.libggi+=	include/ggi/ggi.h
 BUILDLINK_FILES.libggi+=	include/ggi/ggi_ext.h
@@ -39,13 +19,11 @@ BUILDLINK_FILES.libggi+=	include/ggi/internal/ggi-dl.h
 BUILDLINK_FILES.libggi+=	include/ggi/internal/ggidlinit.h
 BUILDLINK_FILES.libggi+=	include/ggi/internal/ggilibinit.h
 BUILDLINK_FILES.libggi+=	include/ggi/internal/internal.h
-BUILDLINK_FILES.libggi+=	include/ggi/internal/misc_int.h
 BUILDLINK_FILES.libggi+=	include/ggi/internal/plat.h
 BUILDLINK_FILES.libggi+=	include/ggi/internal/structs.h
-BUILDLINK_FILES.libggi+=	include/ggi/misc.h
+BUILDLINK_FILES.libggi+=	include/ggi/ll.h
 BUILDLINK_FILES.libggi+=	include/ggi/types.h
 BUILDLINK_FILES.libggi+=	lib/libggi.*
-BUILDLINK_FILES.libggi+=	lib/libggimisc.*
 
 USE_X11=		# defined
 
