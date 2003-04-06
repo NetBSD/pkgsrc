fa9d267e476aecc6902884805b79b4562eb785d9
hubertf <hubertf@pkgsrc.org>
2003-04-06 12:44:22 +0000

    ../../local/pciutils, eh?

--- sysutils/pciutils/buildlink2.mk
+++ sysutils/pciutils/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/02/26 00:12:43 kristerw Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/04/06 12:44:22 hubertf Exp $
 
 .if !defined(PCIUTILS_BUILDLINK2_MK)
 PCIUTILS_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		pciutils
 BUILDLINK_DEPENDS.pciutils?=	pciutils>=2.1.11
-BUILDLINK_PKGSRCDIR.pciutils?=	../../local/pciutils
+BUILDLINK_PKGSRCDIR.pciutils?=	../../sysutils/pciutils
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.pciutils=pciutils
 BUILDLINK_PREFIX.pciutils_DEFAULT=	${LOCALBASE}
