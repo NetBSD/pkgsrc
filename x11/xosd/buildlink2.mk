cab5f614c4397c7e18c87487db942b4313a5b104
jmmv <jmmv@pkgsrc.org>
2003-04-12 12:14:15 +0000

    Do not build nor install the example xmms plugin.  This dramatically shrinks the dependancy tree for this simple library.  Bump PKGREVISION to 1. Take maintainership.

--- x11/xosd/buildlink2.mk
+++ x11/xosd/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/04/12 09:21:24 jmmv Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/04/12 12:14:16 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use xosd.
 #
@@ -9,14 +9,13 @@
 XOSD_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			xosd
-BUILDLINK_DEPENDS.xosd?=		xosd>=2.2.0
+BUILDLINK_DEPENDS.xosd?=		xosd>=2.2.0nb1
 BUILDLINK_PKGSRCDIR.xosd?=		../../x11/xosd
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.xosd=xosd
 BUILDLINK_PREFIX.xosd_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.xosd+=	include/xosd.h
 BUILDLINK_FILES.xosd+=	lib/libxosd.*
-BUILDLINK_FILES.xosd+=	lib/xmms/General/libxmms_osd.*
 
 .include "../../audio/xmms/buildlink2.mk"
 .include "../../graphics/gdk-pixbuf/buildlink2.mk"
