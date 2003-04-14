781ce71694711f9b3da1aa405c601fcefa87e2b2
salo <salo@pkgsrc.org>
2003-04-14 04:30:21 +0000

    GNU texinfo >= 4.1 is required to build the info files shipped, fix wrong check for MAKEINFO version too.  Addresses PR pkg/20930. Make it PKGREVISION resilent. G/C unused variables, delint.

--- lang/gcc3/files/gcc3.mk
+++ lang/gcc3/files/gcc3.mk
@@ -1,6 +1,6 @@
-# $NetBSD: gcc3.mk,v 1.1.1.1 2003/01/06 07:42:27 uebayasi Exp $
+# $NetBSD: gcc3.mk,v 1.2 2003/04/14 04:30:22 salo Exp $
 #
-# make configuration file for @PKGNAME@
+# make configuration file for @PKGNAME_NOREV@
 
 CC=	@GCC_PREFIX@/bin/cc
 CPP=	@GCC_PREFIX@/bin/cpp
