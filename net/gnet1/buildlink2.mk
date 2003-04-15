1ae3248dd31caff9d03949a6e84dc813b44d5391
wiz <wiz@pkgsrc.org>
2003-04-15 12:17:19 +0000

    Rename to gnet1, since it doesn't conflict with gnet-2.0.0.

--- net/gnet1/buildlink2.mk
+++ net/gnet1/buildlink2.mk
@@ -1,42 +1,42 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/04/15 11:49:02 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/04/15 12:17:19 wiz Exp $
 #
-# This Makefile fragment is included by packages that use gnet.
+# This Makefile fragment is included by packages that use gnet1.
 #
 # This file was created automatically using createbuildlink 2.1.
 #
 
-.if !defined(GNET_BUILDLINK2_MK)
-GNET_BUILDLINK2_MK=	# defined
+.if !defined(GNET1_BUILDLINK2_MK)
+GNET1_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=			gnet
-BUILDLINK_DEPENDS.gnet?=		gnet>=1.1.7nb1
-BUILDLINK_PKGSRCDIR.gnet?=		../../net/gnet1
+BUILDLINK_PACKAGES+=			gnet1
+BUILDLINK_DEPENDS.gnet1?=		gnet1>=1.1.7nb1
+BUILDLINK_PKGSRCDIR.gnet1?=		../../net/gnet1
 
-EVAL_PREFIX+=	BUILDLINK_PREFIX.gnet=gnet
-BUILDLINK_PREFIX.gnet_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.gnet+=	include/gnet/conn.h
-BUILDLINK_FILES.gnet+=	include/gnet/gnet.h
-BUILDLINK_FILES.gnet+=	include/gnet/inetaddr.h
-BUILDLINK_FILES.gnet+=	include/gnet/iochannel.h
-BUILDLINK_FILES.gnet+=	include/gnet/mcast.h
-BUILDLINK_FILES.gnet+=	include/gnet/md5.h
-BUILDLINK_FILES.gnet+=	include/gnet/pack.h
-BUILDLINK_FILES.gnet+=	include/gnet/server.h
-BUILDLINK_FILES.gnet+=	include/gnet/sha.h
-BUILDLINK_FILES.gnet+=	include/gnet/socks.h
-BUILDLINK_FILES.gnet+=	include/gnet/tcp.h
-BUILDLINK_FILES.gnet+=	include/gnet/udp.h
-BUILDLINK_FILES.gnet+=	include/gnet/unix.h
-BUILDLINK_FILES.gnet+=	include/gnet/url.h
-BUILDLINK_FILES.gnet+=	lib/gnet/include/gnetconfig.h
-BUILDLINK_FILES.gnet+=	lib/libgnet-1.1.*
-BUILDLINK_FILES.gnet+=	lib/libgnet.*
-BUILDLINK_FILES.gnet+=	lib/pkgconfig/gnet.pc
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gnet1=gnet1
+BUILDLINK_PREFIX.gnet1_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.gnet1+=	include/gnet/conn.h
+BUILDLINK_FILES.gnet1+=	include/gnet/gnet.h
+BUILDLINK_FILES.gnet1+=	include/gnet/inetaddr.h
+BUILDLINK_FILES.gnet1+=	include/gnet/iochannel.h
+BUILDLINK_FILES.gnet1+=	include/gnet/mcast.h
+BUILDLINK_FILES.gnet1+=	include/gnet/md5.h
+BUILDLINK_FILES.gnet1+=	include/gnet/pack.h
+BUILDLINK_FILES.gnet1+=	include/gnet/server.h
+BUILDLINK_FILES.gnet1+=	include/gnet/sha.h
+BUILDLINK_FILES.gnet1+=	include/gnet/socks.h
+BUILDLINK_FILES.gnet1+=	include/gnet/tcp.h
+BUILDLINK_FILES.gnet1+=	include/gnet/udp.h
+BUILDLINK_FILES.gnet1+=	include/gnet/unix.h
+BUILDLINK_FILES.gnet1+=	include/gnet/url.h
+BUILDLINK_FILES.gnet1+=	lib/gnet/include/gnetconfig.h
+BUILDLINK_FILES.gnet1+=	lib/libgnet-1.1.*
+BUILDLINK_FILES.gnet1+=	lib/libgnet.*
+BUILDLINK_FILES.gnet1+=	lib/pkgconfig/gnet.pc
 
 .include "../../devel/glib2/buildlink2.mk"
 
-BUILDLINK_TARGETS+=	gnet-buildlink
+BUILDLINK_TARGETS+=	gnet1-buildlink
 
-gnet-buildlink: _BUILDLINK_USE
+gnet1-buildlink: _BUILDLINK_USE
 
-.endif	# GNET_BUILDLINK2_MK
+.endif	# GNET1_BUILDLINK2_MK
