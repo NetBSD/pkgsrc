bd32e7a493d37011379e5948296ba21e33c7c9ab
rh <rh@pkgsrc.org>
2003-04-20 10:43:51 +0000

    Complete the split of gnome-core into multiple packages. Bump PKGREVISION.

--- x11/gnome-core/buildlink2.mk
+++ x11/gnome-core/buildlink2.mk
@@ -1,31 +1,32 @@
-# $NetBSD: buildlink2.mk,v 1.5 2002/12/24 06:10:30 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.6 2003/04/20 10:43:51 rh Exp $
+#
+# This Makefile fragment is included by packages that use gnome-core.
+#
+# This file was created automatically using createbuildlink 2.4.
+#
 
 .if !defined(GNOME_CORE_BUILDLINK2_MK)
 GNOME_CORE_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gnome-core
-BUILDLINK_DEPENDS.gnome-core?=		gnome-core>=1.4.2nb1
-BUILDLINK_PKGSRCDIR.gnome-core?=	../../x11/gnome-core
+BUILDLINK_DEPENDS.gnome-core?=		gnome-core>=1.4.2nb4
+BUILDLINK_PKGSRCDIR.gnome-core?=		../../x11/gnome-core
 
-EVAL_PREFIX+=			BUILDLINK_PREFIX.gnome-core=gnome-core
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gnome-core=gnome-core
 BUILDLINK_PREFIX.gnome-core_DEFAULT=	${X11PREFIX}
-BUILDLINK_FILES.gnome-core=	include/applet-widget.h
-BUILDLINK_FILES.gnome-core+=	include/gnome-panel.h
-BUILDLINK_FILES.gnome-core+=	include/status-docklet.h
 BUILDLINK_FILES.gnome-core+=	lib/libfish_applet.*
 BUILDLINK_FILES.gnome-core+=	lib/libgen_util_applet.*
-BUILDLINK_FILES.gnome-core+=	lib/libpanel_applet.*
-BUILDLINK_FILES.gnome-core+=	lib/libpanel_status.*
-BUILDLINK_FILES.gnome-core+=	share/idl/Terminal.idl
-BUILDLINK_FILES.gnome-core+=	share/idl/gnome-panel.idl
-BUILDLINK_FILES.gnome-core+=	share/idl/help-browser.idl
 
+.include "../../archivers/bzip2/buildlink2.mk"
 .include "../../devel/gettext-lib/buildlink2.mk"
 .include "../../devel/libglade/buildlink2.mk"
 .include "../../graphics/gdk-pixbuf-gnome/buildlink2.mk"
+.include "../../graphics/gnome-pixmaps/buildlink2.mk"
+.include "../../lang/perl5/buildlink2.mk"
 .include "../../textproc/libxml/buildlink2.mk"
 .include "../../www/gtkhtml/buildlink2.mk"
 .include "../../www/libghttp/buildlink2.mk"
+.include "../../x11/gnome1-panel/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	gnome-core-buildlink
 
