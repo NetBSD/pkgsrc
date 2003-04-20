3f3c871ee06a411d60cd29f30f774bc9a5498655
rh <rh@pkgsrc.org>
2003-04-20 06:58:19 +0000

    Install the Helpers tools and a couple of scripts to actually get them working. Bump PKGREVISION.

--- graphics/PanoTools/files/ptfile.sh
+++ graphics/PanoTools/files/ptfile.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 #
-#	$Id: ptfile.sh,v 1.1.1.1 2003/04/19 07:55:34 rh Exp $
+#	$Id: ptfile.sh,v 1.2 2003/04/20 06:58:20 rh Exp $
 #
 export CLASSPATH=@@prefix@@/share/PanoTools:${CLASSPATH}
 export JAVA_HOME=@@javahome@@
 export PATH=$JAVA_HOME/bin:$PATH
+@@prefix@@/bin/mkpthelpers
 ulimit -d `ulimit -d -H`
 ulimit -s `ulimit -s -H`
 ulimit -m `ulimit -m -H`
 ulimit -p `ulimit -p -H`
 ulimit -n `ulimit -n -H`
-java -jar @@prefix@@/share/PanoTools/@@ptfile@@.jar "$@"
+exec java -jar @@prefix@@/share/PanoTools/@@ptfile@@.jar "$@"
