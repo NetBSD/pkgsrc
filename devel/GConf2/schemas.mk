d0ee251c9b6968b4332eee138124d8afbbd3371a
rh <rh@pkgsrc.org>
2003-04-21 00:12:33 +0000

    Don't crash if GCONF2_SCHEMAS is undefined.

--- devel/GConf2/schemas.mk
+++ devel/GConf2/schemas.mk
@@ -1,4 +1,4 @@
-# $NetBSD: schemas.mk,v 1.2 2003/02/18 15:54:13 jmmv Exp $
+# $NetBSD: schemas.mk,v 1.3 2003/04/21 00:12:33 rh Exp $
 #
 # This Makefile fragment is intended to be included by packages that install
 # GConf2 schema files.  It takes care of registering them in the GConf2
@@ -37,7 +37,7 @@ CONFIGURE_ARGS+= 	--disable-schemas-install
 CONFIGURE_ARGS+=	--with-gconf-schema-file-dir=${GCONF2_SCHEMAS_DIR}
 .endif
 
-.if !empty(GCONF2_SCHEMAS)
+.if defined(GCONF2_SCHEMAS) && !empty(GCONF2_SCHEMAS)
 FILES_SUBST+=		GCONFTOOL2="${GCONFTOOL2}"
 FILES_SUBST+=		GCONF2_CONFIG_SOURCE="${GCONF2_CONFIG_SOURCE}"
 FILES_SUBST+=		GCONF2_SCHEMAS="${GCONF2_SCHEMAS}"
