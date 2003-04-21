4219638759891f2aa274044aff155f0638d71e76
rh <rh>
2003-04-21 02:28:56 +0000

    Initial import of gnome-games-2.2.1, the GNOME2 games collection.

---
+++ games/gnome2-games/buildlink2.mk
@@ -0,0 +1,31 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/04/21 02:28:57 rh Exp $
+#
+# This Makefile fragment is included by packages that use gnome-games.
+#
+# This file was created automatically using createbuildlink 2.4.
+#
+
+.if !defined(GNOME_GAMES_BUILDLINK2_MK)
+GNOME_GAMES_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			gnome-games
+BUILDLINK_DEPENDS.gnome-games?=		gnome-games>=1.4.0.4nb3
+BUILDLINK_PKGSRCDIR.gnome-games?=		../../games/gnome-games
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gnome-games=gnome-games
+BUILDLINK_PREFIX.gnome-games_DEFAULT=	${X11PREFIX}
+BUILDLINK_FILES.gnome-games+=	include/gdkcardimage/gdk-card-image.h
+BUILDLINK_FILES.gnome-games+=	lib/gnome-stones/objects/libgnome-stones.*
+BUILDLINK_FILES.gnome-games+=	lib/gnome-stones/objects/libgnomekoban.*
+BUILDLINK_FILES.gnome-games+=	lib/libgdkcardimage.*
+
+.include "../../devel/gettext-lib/buildlink2.mk"
+.include "../../devel/ncurses/buildlink2.mk"
+.include "../../lang/guile14/buildlink2.mk"
+.include "../../x11/gnome-core/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	gnome-games-buildlink
+
+gnome-games-buildlink: _BUILDLINK_USE
+
+.endif	# GNOME_GAMES_BUILDLINK2_MK
