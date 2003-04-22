3ce9980ce6cbf36dfc98637f7c0fe2b1682f0c68
jmmv <jmmv@pkgsrc.org>
2003-04-22 19:06:11 +0000

    Update to 2.2.1:
    * Made the underscores disappear correctly (James Roberts-Thomson)
    * xmms plugin breakdown and breakup (Philipp Matthias Hahn)
    * configure.in xinerama patch (Julio Merino)
    * configure.in -> configure.ac conversion (Philipp Matthias Hahn)

--- x11/xosd/buildlink2.mk
+++ x11/xosd/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.4 2003/04/12 13:23:13 jmmv Exp $
+# $NetBSD: buildlink2.mk,v 1.5 2003/04/22 19:06:12 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use xosd.
 #
@@ -9,7 +9,7 @@
 XOSD_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			xosd
-BUILDLINK_DEPENDS.xosd?=		xosd>=2.2.0nb1
+BUILDLINK_DEPENDS.xosd?=		xosd>=2.2.1
 BUILDLINK_PKGSRCDIR.xosd?=		../../x11/xosd
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.xosd=xosd
