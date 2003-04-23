82bb7439832f21ab4f148287bd39e236afba7e50
drochner <drochner>
2003-04-23 17:32:59 +0000

    update to 4.2.5 changes: -bugfixes -If Foomatic 2.9.x is installed, the Foomatic data will be  generated with additional features -automatic paper cutter is now functional on the  Epson Stylus Photo 925, 950/960, and 2100/2200 -Quality Improvements on Epson Stylus Photo 950/960/PM-950C and 2100/2200 -i18n

--- print/gimp-print-lib/buildlink2.mk
+++ print/gimp-print-lib/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.4 2002/12/10 18:37:45 drochner Exp $
+# $NetBSD: buildlink2.mk,v 1.5 2003/04/23 17:33:00 drochner Exp $
 
 .if !defined(GIMP_PRINT_BUILDLINK2_MK)
 GIMP_PRINT_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=	gimp-print-lib
-BUILDLINK_DEPENDS.gimp-print-lib?=	gimp-print-lib-4.2.4
+BUILDLINK_DEPENDS.gimp-print-lib?=	gimp-print-lib-4.2.5
 BUILDLINK_PKGSRCDIR.gimp-print-lib?=	../../print/gimp-print-lib
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gimp-print-lib=gimp-print-lib
