842e29cba43a34dc16cff768eeda8d8c59ec1f3f
jtb <jtb@pkgsrc.org>
2003-04-29 22:48:45 +0000

    Update to version 3.0.
    Major goals of this release:
    
    * Speed: often 20% or more faster than FFTW 2.x, even without SIMD (see below).
    
    * Complete rewrite, to make it easier to add new algorithms and transforms.
    
    * New API, to support more general semantics.
    
    Other enhancements:
    
    * SIMD acceleration on supporting CPUs (SSE, SSE2, 3DNow!, and AltiVec).
     (With special thanks to Franz Franchetti for many experimental prototypes
      and to Stefan Kral for the vectorizing generator from fftwgel.)
    
    * True in-place 1d transforms of large sizes (as well as compressed
      twiddle tables for additional memory/cache savings).
    
    * More arbitrary placement of real & imaginary data, e.g. including
      interleaved (as in FFTW 2.x) as well as separate real/imag arrays.
    
    * Efficient prime-size transforms of real data.
    
    * Multidimensional transforms can operate on a subset of a larger matrix,
      and/or transform selected dimensions of a multidimensional array.
    
    * By popular demand, simultaneous linking to double precision (fftw),
      single precision (fftwf), and long-double precision (fftwl) versions
      of FFTW is now supported.
    
    * Cycle counters (on all modern CPUs) are exploited to speed planning.
    
    * Efficient transforms of real even/odd arrays, a.k.a. discrete
      cosine/sine transforms (types I-IV).  (Currently work via pre/post
      processing of real transforms, ala FFTPACK, so are not optimal.)
    
    * DHTs (Discrete Hartley Transforms), again via post-processing
      of real transforms (and thus suboptimal, for now).
    
    * Support for linking to just those parts of FFTW that you need,
      greatly reducing the size of statically linked programs when
      only a limited set of transform sizes/types are required.
    
    * Canonical global wisdom file (/etc/fftw/wisdom) on Unix, along
      with a command-line tool (fftw-wisdom) to generate/update it.
    
    * Fortran API can be used with both g77 and non-g77 compilers
      simultaneously.
    
    * Multi-threaded version has optional OpenMP support.
    
    * Authors' good looks have greatly improved with age.

--- math/fftw/buildlink2.mk
+++ math/fftw/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/12/14 18:14:02 jmmv Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/04/29 22:48:46 jtb Exp $
 #
 # This Makefile fragment is included by packages that use fftw.
 #
@@ -9,14 +9,14 @@
 FFTW_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			fftw
-BUILDLINK_DEPENDS.fftw?=		fftw>=2.1.3
+BUILDLINK_DEPENDS.fftw?=		fftw>=3.0
 BUILDLINK_PKGSRCDIR.fftw?=		../../math/fftw
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.fftw=fftw
 BUILDLINK_PREFIX.fftw_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.fftw+=	include/fftw.h
-BUILDLINK_FILES.fftw+=	include/rfftw.h
-BUILDLINK_FILES.fftw+=	lib/libfftw.*
+BUILDLINK_FILES.fftw+=	include/fftw3.f
+BUILDLINK_FILES.fftw+=	include/fftw3.h
+BUILDLINK_FILES.fftw+=	lib/libfftw3.*
 BUILDLINK_FILES.fftw+=	lib/librfftw.*
 
 BUILDLINK_TARGETS+=	fftw-buildlink
