3b9b9405cb6fe1796bdf02d62300e349422658f1
jtb <jtb@pkgsrc.org>
2003-04-29 23:33:56 +0000

    Update to version 5.0.
    * Changes from version 4.0 to 5.0
      -------------------------------
      Language:
      + lexical scoping.
      + Lua coroutines.
      + standard libraries now packaged in tables.
      + tags replaced by metatables and tag methods replaced by metamethods,
        stored in metatables.
      + proper tail calls.
      + each function can have its own global table, which can be shared.
      + new __newindex metamethod, called when we insert a new key into a table.
      + new block comments: --[[ ... ]].
      + new generic for.
      + new weak tables.
      + new boolean type.
      + new syntax "local function".
      + (f()) returns the first value returned by f.
      + {f()} fills a table with all values returned by f.
      + \n ignored in [[\n .
      + fixed and-or priorities.
      + more general syntax for function definition (e.g. function a.x.y:f()...end).
      + more general syntax for function calls (e.g. (print or write)(9)).
      + new functions (time/date, tmpfile, unpack, require, load*, etc.).
      API:
      + chunks are loaded by using lua_load; new luaL_loadfile and luaL_loadbuffer.
      + introduced lightweight userdata, a simple "void*" without a metatable.
      + new error handling protocol: the core no longer prints error messages;
        all errors are reported to the caller on the stack.
      + new lua_atpanic for host cleanup.
      + new, signal-safe, hook scheme.
      Implementation:
      + new license: MIT.
      + new, faster, register-based virtual machine.
      + support for external multithreading and coroutines.
      + new and consistent error message format.
      + the core no longer needs "stdio.h" for anything (except for a single
        use of sprintf to convert numbers to strings).
      + lua.c now runs the environment variable LUA_INIT, if present. It can
        be "@filename", to run a file, or the chunk itself.
      + support for user extensions in lua.c.
        sample implementation given for command line editing.
      + new dynamic loading library, active by default on several platforms.
      + safe garbage-collector metamethods.
      + precompiled bytecodes checked for integrity (secure binary dostring).
      + strings are fully aligned.
      + position capture in string.find.
      + read('*l') can read lines with embedded zeros.

--- lang/lua/buildlink2.mk
+++ lang/lua/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/02/22 00:51:58 jtb Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/04/29 23:33:56 jtb Exp $
 #
 # This Makefile fragment is included by packages that use lua.
 #
@@ -7,14 +7,13 @@
 LUA_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		lua
-BUILDLINK_DEPENDS.lua?=		lua>=4.0.1
+BUILDLINK_DEPENDS.lua?=		lua>=5.0
 BUILDLINK_PKGSRCDIR.lua?=	../../lang/lua
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.lua=lua
 BUILDLINK_PREFIX.lua_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.lua+=	include/lauxlib.h
 BUILDLINK_FILES.lua+=	include/lua.h
-BUILDLINK_FILES.lua+=	include/luadebug.h
 BUILDLINK_FILES.lua+=	include/lualib.h
 BUILDLINK_FILES.lua+=	lib/liblua.*
 BUILDLINK_FILES.lua+=	lib/liblualib.*
