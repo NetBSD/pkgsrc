e8409bafa1cf86de8935c4a2393183b5c2263ffb
drochner <drochner@pkgsrc.org>
2003-05-09 14:40:17 +0000

    pull in gtkglarea2 from pkgsrc-wip, provided by Marc Recht

---
+++ x11/gtkglarea2/buildlink2.mk
@@ -0,0 +1,29 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/05/09 14:40:17 drochner Exp $
+#
+# This Makefile fragment is included by packages that use gtkglarea2.
+#
+# This file was created automatically using createbuildlink 2.4.
+#
+
+.if !defined(GTKGLAREA2_BUILDLINK2_MK)
+GTKGLAREA2_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			gtkglarea2
+BUILDLINK_DEPENDS.gtkglarea2?=		gtkglarea2>=1.99.0
+BUILDLINK_PKGSRCDIR.gtkglarea2?=		../../wip/gtkglarea2
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gtkglarea2=gtkglarea2
+BUILDLINK_PREFIX.gtkglarea2_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.gtkglarea2+=	include/gtkgl-2.0/gtkgl/gdkgl.h
+BUILDLINK_FILES.gtkglarea2+=	include/gtkgl-2.0/gtkgl/gtkglarea.h
+BUILDLINK_FILES.gtkglarea2+=	lib/libgtkgl-2.0.*
+BUILDLINK_FILES.gtkglarea2+=	lib/pkgconfig/gtkgl-2.0.pc
+
+.include "../../graphics/Mesa/buildlink2.mk"
+.include "../../x11/gtk2/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	gtkglarea2-buildlink
+
+gtkglarea2-buildlink: _BUILDLINK_USE
+
+.endif	# GTKGLAREA2_BUILDLINK2_MK
