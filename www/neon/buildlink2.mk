feffb1605c40d89ff2d77bb35bb1bbe57ea40677
drochner <drochner@pkgsrc.org>
2003-05-09 15:44:09 +0000

    update to 0.23.9, provided by Marc Recht per PR pkg/20987 Changes are basically fixes, among them a security fix.

--- www/neon/buildlink2.mk
+++ www/neon/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.5 2003/05/02 11:56:45 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.6 2003/05/09 15:44:10 drochner Exp $
 
 .if !defined(NEON_BUILDLINK2_MK)
 NEON_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		neon
-BUILDLINK_DEPENDS.neon?=	neon>=0.23.7nb1
+BUILDLINK_DEPENDS.neon?=	neon>=0.23.9
 BUILDLINK_PKGSRCDIR.neon?=	../../www/neon
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.neon=neon
