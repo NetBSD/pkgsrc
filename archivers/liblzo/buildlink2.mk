7d3012fa4ee0073d9fe0230b44c674f914045dc9
salo <salo@pkgsrc.org>
2003-05-14 02:11:25 +0000

    - sync buildlink2.mk with reality - DESCR has 80 culumns, no need for saying about license here - style nits

--- archivers/liblzo/buildlink2.mk
+++ archivers/liblzo/buildlink2.mk
@@ -1,15 +1,20 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 21:49:14 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/05/14 02:11:25 salo Exp $
+#
+# This Makefile fragment is included by packages that use liblzo.
+#
+# This file was created automatically using createbuildlink 2.6.
+#
 
 .if !defined(LIBLZO_BUILDLINK2_MK)
 LIBLZO_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=		liblzo
-BUILDLINK_DEPENDS.liblzo?=	liblzo>=1.06
-BUILDLINK_PKGSRCDIR.liblzo?=	../../archivers/liblzo
+BUILDLINK_PACKAGES+=			liblzo
+BUILDLINK_DEPENDS.liblzo?=		liblzo>=1.08
+BUILDLINK_PKGSRCDIR.liblzo?=		../../archivers/liblzo
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.liblzo=liblzo
 BUILDLINK_PREFIX.liblzo_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.liblzo=		include/lzo1.h
+BUILDLINK_FILES.liblzo+=	include/lzo1.h
 BUILDLINK_FILES.liblzo+=	include/lzo16bit.h
 BUILDLINK_FILES.liblzo+=	include/lzo1a.h
 BUILDLINK_FILES.liblzo+=	include/lzo1b.h
@@ -23,7 +28,7 @@ BUILDLINK_FILES.liblzo+=	include/lzoconf.h
 BUILDLINK_FILES.liblzo+=	include/lzoutil.h
 BUILDLINK_FILES.liblzo+=	lib/liblzo.*
 
-BUILDLINK_TARGETS+=		liblzo-buildlink
+BUILDLINK_TARGETS+=	liblzo-buildlink
 
 liblzo-buildlink: _BUILDLINK_USE
 
