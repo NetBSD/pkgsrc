23349eef7cdc9d729db652fe598ed6673eab5dff
grant <grant@pkgsrc.org>
2003-05-18 03:43:01 +0000

    Update phoenix-bin to 0.6.
        * New default theme
          Mozilla Firebird 0.6 sports a crisp, fresh and attractive theme, based on the amazing Qute theme by Arvid Axelsson. Also, the new theme has icons for the Bookmark Manager toolbar.
    
        * Redesigned Preferences window
          The new Options window puts the most often used preferences into seven convenient panels, each with a descriptive icon selector.
    
        * Improved Privacy Options
          With a single click (and a confirmation) you can clear all privacy data including form data, history, cache, cookies, etc.
    
        * Improved Bookmarks
          Among other bookmark enhancements, the Bookmarks menu now features a context menu. Try it by right-clicking on a bookmark in the menu.
    
        * Talkback enabled
          You are now able to submit Talkback information when Mozilla Firebird crashes, which makes it much easier for developers to fix critical bugs. For more information read about Talkback in the FAQ.
    
        * Automatic Image Resizing
          With this feature, Mozilla Firebird shrinks any image that is bigger than the window to make the whole image visible. When this is done, the cursor over the image changes to tell you that if you click, the image is restored to full size. Instructions on how to disable this feature can be found here.
    
        * Smooth Scrolling
          Instructions on how to disable this feature can be found here.
    
        * Access to more preferences
          You are now able to edit lots of advanced preferences that are not exposed in the Options window by entering about:config in the location bar and press Enter.
    
        * Profile Chrome
          You can now install Mozilla Firebird extensions in your own profile folder instead of in the actual program folder. This means that you will be able to upgrade to new versions of Mozilla Firebird without losing all your extensions. Note that it's up to the extension authors to allow their extensions to use this functionality. Some extensions may not offer this as an option yet.
    
        * Mac OS X
          Mozilla Firebird is available for Mac OS X. It's still quite rough around the edges but it's a start.
    
        * Lots of bug fixes
          There have been too many bug fixes since Phoenix 0.5 to keep track of them all. It's better. Trust us ;-)
    
    The full release notes can be found at:
    
    	http://www.mozilla.org/projects/firebird/release-notes.html
    
    Note that this package is still named 'phoenix-bin' and the script
    installed into ${PREFIX}/bin is still 'phoenix-*' until all phoenix*
    packages are updated for the 0.6 release.

--- www/phoenix-bin/files/phoenix.sh
+++ www/phoenix-bin/files/phoenix.sh
@@ -1,3 +1,3 @@
 #!@SH@
-# $NetBSD: phoenix.sh,v 1.2 2003/04/14 22:59:32 grant Exp $
-exec @PREFIX@/lib/phoenix-@PLATFORM@/phoenix "$@"
+# $NetBSD: phoenix.sh,v 1.3 2003/05/18 03:43:02 grant Exp $
+exec @PREFIX@/lib/MozillaFirebird-@PLATFORM@/MozillaFirebird "$@"
