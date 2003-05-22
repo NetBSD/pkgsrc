e92d61064c99953b2fe9bb67e5b11de41338a12a
markd <markd@pkgsrc.org>
2003-05-22 03:05:38 +0000

    Update KDE to 3.1.2.
    Changes:
    
    kdemultimedia:
    * Ogg Vorbis Info List view column titles were incorrect.
    * kmix: Vertical label widget to sliders in replacement of large horizontal labels
    * kmix: Fixed alsa 0.9x to restore current volumes
    * kmix: Fixed multiple alsa 0.9x cards detection

--- audio/arts/buildlink2.mk
+++ audio/arts/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.6 2003/05/02 11:53:37 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.7 2003/05/22 03:05:38 markd Exp $
 
 .if !defined(ARTS_BUILDLINK2_MK)
 ARTS_BUILDLINK2_MK=	# defined
@@ -6,7 +6,7 @@ ARTS_BUILDLINK2_MK=	# defined
 .include "../../mk/bsd.prefs.mk"
 
 BUILDLINK_PACKAGES+=		arts
-BUILDLINK_DEPENDS.arts?=	arts>=1.1.1nb1
+BUILDLINK_DEPENDS.arts?=	arts>=1.1.2
 BUILDLINK_PKGSRCDIR.arts?=	../../audio/arts
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.arts=arts
