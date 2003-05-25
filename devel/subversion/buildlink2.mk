3d7242efcea9c7045595c65174e1bdc4324e14b5
epg <epg>
2003-05-25 09:31:28 +0000

    Turn this into a meta-package for Subversion 0.23.0.  Also, it contains the infrastructure (Makefile.common, buildlink2.mk, distinfo, and patches).

--- devel/subversion/buildlink2.mk
+++ devel/subversion/buildlink2.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/05/02 11:54:42 wiz Exp $
-#
+# $NetBSD: buildlink2.mk,v 1.4 2003/05/25 09:31:29 epg Exp $
 
 .if !defined(SUBVERSION_BUILDLINK2_MK)
-SUBVERSION_BUILDLINK2_MK=          #defined
+SUBVERSION_BUILDLINK2_MK=		#defined
 
 BUILDLINK_PACKAGES+=			subversion-base
-BUILDLINK_DEPENDS.subversion-base?=	subversion>=0.20.1nb1
-BUILDLINK_PKGSRCDIR.subversion-base?=	../../devel/subversion
+BUILDLINK_DEPENDS.subversion-base?=	subversion-base>=${SVNVER}
+BUILDLINK_PKGSRCDIR.subversion-base?=	../../devel/subversion-base
 
-EVAL_PREFIX+=	BUILDLINK_PREFIX.subversion-base=subversion
+EVAL_PREFIX+=			BUILDLINK_PREFIX.subversion-base=subversion-base
 BUILDLINK_PREFIX.subversion-base_DEFAULT=	${LOCALBASE}
 
 BUILDLINK_FILES.subversion-base+=	bin/svn-config
@@ -16,12 +15,13 @@ BUILDLINK_FILES.subversion-base+=	include/subversion-1/*
 BUILDLINK_FILES.subversion-base+=	lib/libsvn*
 
 .include "../../databases/db4/buildlink2.mk"
-.include "../../textproc/expat/buildlink2.mk"
-.include "../../www/apache2/buildlink2.mk"
+.include "../../devel/apr/buildlink2.mk"
 .include "../../www/neon/buildlink2.mk"
 
-BUILDLINK_TARGETS+=             subversion-base-buildlink
+.include "../../devel/subversion/Makefile.version"
 
-subversion-base-buildlink:	_BUILDLINK_USE
+BUILDLINK_TARGETS+=		subversion-base-buildlink
 
-.endif  # SUBVERSION_BUILDLINK2_MK
+subversion-base-buildlink: _BUILDLINK_USE
+
+.endif	# SUBVERSION_BUILDLINK2_MK
