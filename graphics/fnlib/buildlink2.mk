34ef7645fb9cae4e67a8881662ed6fe0c4752fea
salo <salo@pkgsrc.org>
2003-05-25 09:01:46 +0000

    PKGREVISION++ - s/USE_X11BASE/USE_X11/ - new MASTER_SITES

--- graphics/fnlib/buildlink2.mk
+++ graphics/fnlib/buildlink2.mk
@@ -1,15 +1,20 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/05/02 11:55:15 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/05/25 09:01:46 salo Exp $
+#
+# This Makefile fragment is included by packages that use fnlib.
+#
+# This file was created automatically using createbuildlink 2.6.
+#
 
 .if !defined(FNLIB_BUILDLINK2_MK)
 FNLIB_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=		fnlib
-BUILDLINK_DEPENDS.fnlib?=	fnlib>=0.5nb3
-BUILDLINK_PKGSRCDIR.fnlib?=	../../graphics/fnlib
+BUILDLINK_PACKAGES+=			fnlib
+BUILDLINK_DEPENDS.fnlib?=		fnlib>=0.5nb4
+BUILDLINK_PKGSRCDIR.fnlib?=		../../graphics/fnlib
 
-EVAL_PREFIX+=		BUILDLINK_PREFIX.fnlib=fnlib
-BUILDLINK_PREFIX.fnlib_DEFAULT=	${X11PREFIX}
-BUILDLINK_FILES.fnlib=	include/Fnlib*
+EVAL_PREFIX+=	BUILDLINK_PREFIX.fnlib=fnlib
+BUILDLINK_PREFIX.fnlib_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.fnlib+=	include/Fnlib*
 BUILDLINK_FILES.fnlib+=	lib/libFnlib.*
 
 .include "../../graphics/imlib/buildlink2.mk"
