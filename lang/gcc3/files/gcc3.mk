acf83f9bca6af381ba77e2e249b97beedf5383e0
grant <grant@pkgsrc.org>
2003-05-27 07:12:34 +0000

    define USE_GCC3, so that when this file is included, we use gcc3's buildlink files.

--- lang/gcc3/files/gcc3.mk
+++ lang/gcc3/files/gcc3.mk
@@ -1,7 +1,8 @@
-# $NetBSD: gcc3.mk,v 1.2 2003/04/14 04:30:22 salo Exp $
+# $NetBSD: gcc3.mk,v 1.3 2003/05/27 07:12:34 grant Exp $
 #
 # make configuration file for @PKGNAME_NOREV@
 
-CC=	@GCC_PREFIX@/bin/cc
-CPP=	@GCC_PREFIX@/bin/cpp
-CXX=	@GCC_PREFIX@/bin/c++
+USE_GCC3=	# defined
+CC=		@GCC_PREFIX@/bin/cc
+CPP=		@GCC_PREFIX@/bin/cpp
+CXX=		@GCC_PREFIX@/bin/c++
