b67e03ba9bd10f3d7850edc41677c26fd56b7fb1
grant <grant@pkgsrc.org>
2003-05-29 23:14:07 +0000

    s/USE_CXX/USE_GCC_SHLIB/ - there are more shared libraries than just libstdc++ in gcc3.
    when defined, USE_GCC_SHLIB ensures that the correct rpath is passed
    to the linker, and a full dependency on the compiler package is
    registered.
    
    packages which define USE_GCC_SHLIB should not include
    mk/gcc.buildlink2.mk (or gcc{,3}/buildlink2.mk) as it is handled
    automatically.

--- lang/gcc3/buildlink2.mk
+++ lang/gcc3/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/05/28 13:24:35 grant Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/05/29 23:14:12 grant Exp $
 
 .if !defined(GCC3_BUILDLINK2_MK)
 GCC3_BUILDLINK2_MK=	# defined
@@ -9,10 +9,10 @@ GCC_REQD?=			3.3
 BUILDLINK_DEPENDS.gcc?=		gcc3>=${GCC_REQD}
 BUILDLINK_PKGSRCDIR.gcc?=	../../lang/gcc3
 
-# Packages that use the C++ compiler and subsequently link against the
-# shared libstdc++ from the package need a full dependency.
+# Packages that link against gcc shared libraries need a full
+# dependency.
 #
-.if defined(USE_CXX)
+.if defined(USE_GCC_SHLIB)
 BUILDLINK_DEPMETHOD.gcc+=	full
 .else
 BUILDLINK_DEPMETHOD.gcc?=	build
@@ -57,7 +57,7 @@ CXX=		${_GCC_PREFIX}bin/g++
 F77=		${_GCC_PREFIX}bin/g77
 PKG_FC=		${F77}
 
-.  if defined(USE_CXX)
+.  if defined(USE_GCC_SHLIB)
 LDFLAGS+=		${BUILDLINK_LDFLAGS.gcc}
 .  endif
 BUILDLINK_WRAPPER_ENV+=	\
