0324de6b5dadc7b4498c36f038a89b62332fc018
hubertf <hubertf@pkgsrc.org>
2003-06-01 23:56:50 +0000

    Improve message when vulnerability file is not there.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1186 2003/05/29 23:08:01 grant Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1187 2003/06/01 23:56:50 hubertf Exp $
 #
 # This file is in the public domain.
 #
@@ -1443,7 +1443,9 @@ do-fetch:
 			${FALSE} ;;					\
 		esac;							\
 	else								\
-		${ECHO_MSG} "${_PKGSRC_IN}> *** No ${PKGVULNDIR}/vulnerabilities file found - skipping vulnerability checks ***"; \
+		${ECHO_MSG} "${_PKGSRC_IN}> *** No ${PKGVULNDIR}/vulnerabilities file found,"; \
+		${ECHO_MSG} "${_PKGSRC_IN}> *** skipping vulnerability checks. To fix, install"; \
+		${ECHO_MSG} "${_PKGSRC_IN}> *** pkgsrc/security/audit-packages and run 'download-vulnerability-list'."; \
 	fi
 .  endif
 .  if !empty(_ALLFILES)
