441ef24d3af3052870dbbab8f5c4e87739e7ded5
drochner <drochner>
2003-06-04 19:55:13 +0000

    use the canonical way to depend on Python and allow 22-pth too patch by Marc Recht (PR pkg/21773)

--- misc/gstreamer/buildlink2.mk
+++ misc/gstreamer/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/05/02 11:55:50 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/06/04 19:55:13 drochner Exp $
 #
 # This Makefile fragment is included by packages that use gstreamer.
 #
@@ -46,7 +46,7 @@ BUILDLINK_FILES.gstreamer+=	lib/pkgconfig/gstreamer-control-0.6.pc
 .include "../../devel/libgnomeui/buildlink2.mk"
 .include "../../devel/pkgconfig/buildlink2.mk"
 .include "../../devel/popt/buildlink2.mk"
-.include "../../lang/python22/buildlink2.mk"
+.include "../../lang/python/application.mk"
 .include "../../textproc/gtk-doc/buildlink2.mk"
 .include "../../textproc/libxml2/buildlink2.mk"
 .include "../../textproc/libxslt/buildlink2.mk"
