dcb7b6a5952e51a6c443488488c8e785fb5896ac
abs <abs@pkgsrc.org>
2003-06-06 15:26:48 +0000

    Update cpuflags to 0.45:     - Detect fpuless mac68k boxes and add -msoft-float     - Check ${USETOOLS} to avoid setting gcc3 flags when compiling NetBSD       userland with built in tools, but with gcc3 installed     - Default to -O3 again for all except perl5 and perl58 under gcc3

--- devel/cpuflags/files/optimize_gcc.mk
+++ devel/cpuflags/files/optimize_gcc.mk
@@ -1,4 +1,4 @@
-# $Id: optimize_gcc.mk,v 1.1 2003/05/29 11:45:50 abs Exp $
+# $Id: optimize_gcc.mk,v 1.2 2003/06/06 15:26:50 abs Exp $
 
 # This file is 'experimental' - which is doublespeak for unspeakably
 # ugly, and probably quite broken by design.
@@ -8,13 +8,10 @@
 # compile some code, and even generate curdled binaries. It is completely
 # unsupported. Any questions should be directed to <abs@netbsd.org>.
 
-.if (${MACHINE} == sparc64)
-COPT_FLAGS=
-.elif defined(USE_GCC3)
-# With at least -march=k6-2, gcc 3.3 breaks with -frename-registers (in -O3)
-COPT_FLAGS=-O2 -finline-functions
-.else
+.if defined(USE_GCC3) || (${MACHINE} != sparc64)
 COPT_FLAGS=-O3
+.else
+COPT_FLAGS=
 .endif
 
 .ifdef BSD_PKG_MK			# Try to catch various package opts
@@ -28,15 +25,20 @@ PKGBASE=${.CURDIR:C:.*/::}
 .endif
 .endif
 
-COPT_FLAGS+=-ffast-math
+COPT_FLAGS+=-ffast-math -fomit-frame-pointer
 
 PKG_EXCLUDE_OMIT_FRAME_POINTER=lua4
 .if defined(USE_GCC3)
 PKG_EXCLUDE_OMIT_FRAME_POINTER+=mozilla phoenix qt3-libs kdeedu3
+PKG_EXCLUDE_O3+=perl5 perl58
 .endif
 
 .if empty(PKG_EXCLUDE_OMIT_FRAME_POINTER:M${PKGBASE})
 COPT_FLAGS+=-fomit-frame-pointer
+.endif
+
+.if !empty(PKG_EXCLUDE_O3:M${PKGBASE})
+COPT_FLAGS:=	${COPT_FLAGS:S/-O3//}
 .endif
 
 CFLAGS+=${COPT_FLAGS}
@@ -45,6 +47,7 @@ MAKE_FLAGS+=CCOPTIONS="${COPT_FLAGS}"	# Override CCOPTIONS for imake
 
 .else					# Assume in base system, only COPTS
 
+.if ${USETOOLS:Uyes} == "no"
 COPT_FLAGS+=-fomit-frame-pointer
 # Include ${DEFCOPTS} and set ?= to allow overriding in kernel builds
 .if !defined(COPTS)
@@ -52,6 +55,8 @@ COPTS=${DEFCOPTS} ${COPT_FLAGS}
 .else
 COPTS+=${COPT_FLAGS}
 .endif
+.endif
+
 .if defined(USE_GCC3)
 DEFWARNINGS=no
 .endif
