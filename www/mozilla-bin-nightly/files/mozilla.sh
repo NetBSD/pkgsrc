84d24750c1a9f9e7118c321edd5cc3008cd9655f
grant <grant>
2003-06-11 17:43:15 +0000

    add Solaris/sparc binary release to this package.

--- www/mozilla-bin-nightly/files/mozilla.sh
+++ www/mozilla-bin-nightly/files/mozilla.sh
@@ -1,3 +1,7 @@
 #!@SH@
-# $NetBSD: mozilla.sh,v 1.1.1.1 2003/05/20 14:25:14 grant Exp $
+# $NetBSD: mozilla.sh,v 1.2 2003/06/11 17:43:16 grant Exp $
+
+LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:@PREFIX@/lib/mozilla-@PLATFORM@"
+export LD_LIBRARY_PATH
+
 exec @PREFIX@/lib/mozilla-@PLATFORM@/mozilla "$@"
