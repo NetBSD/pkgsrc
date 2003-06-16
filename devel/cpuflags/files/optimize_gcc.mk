780469084d20dcf6526c62a378a06876c4970349
abs <abs>
2003-06-16 13:00:40 +0000

    Update cpuflags to 0.50:     mozilla and phoenix still need PKG_EXCLUDE_OMIT_FRAME_POINTER with gcc3.3.     (galeon has its own set of issues with this file - still looking into)

--- devel/cpuflags/files/optimize_gcc.mk
+++ devel/cpuflags/files/optimize_gcc.mk
@@ -1,4 +1,4 @@
-# $Id: optimize_gcc.mk,v 1.6 2003/06/11 07:47:40 abs Exp $
+# $Id: optimize_gcc.mk,v 1.7 2003/06/16 13:00:40 abs Exp $
 
 # This file is 'experimental' - which is doublespeak for unspeakably
 # ugly, and probably quite broken by design.
@@ -28,9 +28,9 @@ PKGBASE=${.CURDIR:C:.*/::}
 COPT_FLAGS+=-ffast-math -fomit-frame-pointer
 
 PKG_EXCLUDE_RENAME_REGISTERS+=
-PKG_EXCLUDE_OMIT_FRAME_POINTER+=lua4
+PKG_EXCLUDE_OMIT_FRAME_POINTER+=lua4 mozilla phoenix
 .if !defined(USE_GCC3)
-PKG_EXCLUDE_OMIT_FRAME_POINTER+=mozilla phoenix qt3-libs kdeedu3
+PKG_EXCLUDE_OMIT_FRAME_POINTER+=qt3-libs kdeedu3
 .endif
 
 .if !empty(PKG_EXCLUDE_OMIT_FRAME_POINTER:M${PKGBASE})
