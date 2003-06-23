067c67a3f7752160ff0d82f53d9feb4bb53ef765
wiz <wiz@pkgsrc.org>
2003-06-23 13:28:53 +0000

    Do not add dependency if /usr/include/event.h exists. Based on patch by Juan RP, slightly modified by me.

--- devel/libevent/buildlink2.mk
+++ devel/libevent/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1 2003/06/04 17:15:35 jmmv Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/06/23 13:28:53 wiz Exp $
 #
 # This Makefile fragment is included by packages that use libevent.
 #
@@ -8,12 +8,23 @@
 .if !defined(LIBEVENT_BUILDLINK2_MK)
 LIBEVENT_BUILDLINK2_MK=	# defined
 
+.if exists(/usr/include/event.h)
+_NEED_LIBEVENT=		NO
+.else
+_NEED_LIBEVENT=		YES
+.endif
+
+.if ${_NEED_LIBEVENT} == "YES"
 BUILDLINK_PACKAGES+=			libevent
 BUILDLINK_DEPENDS.libevent?=		libevent>=0.6
 BUILDLINK_PKGSRCDIR.libevent?=		../../devel/libevent
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libevent=libevent
 BUILDLINK_PREFIX.libevent_DEFAULT=	${LOCALBASE}
+.else
+BUILDLINK_PREFIX.libevent=		/usr
+.endif
+
 BUILDLINK_FILES.libevent+=	include/event.h
 BUILDLINK_FILES.libevent+=	lib/libevent.*
 
