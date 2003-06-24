efa139f6f556d2cc65c2c0dd90240f3942de13e2
wiz <wiz>
2003-06-24 08:20:59 +0000

    Bump PKGREVISION, since previous version compiled but was unusable.

--- graphics/gdchart/buildlink2.mk
+++ graphics/gdchart/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.5 2003/06/23 10:19:32 adam Exp $
+# $NetBSD: buildlink2.mk,v 1.6 2003/06/24 08:20:59 wiz Exp $
 #
 
 .if !defined(GDCHART_BUILDLINK2_MK)
 GDCHART_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gdchart
-BUILDLINK_DEPENDS.gdchart?=		gdchart>=0.11.4
+BUILDLINK_DEPENDS.gdchart?=		gdchart>=0.11.4nb1
 BUILDLINK_PKGSRCDIR.gdchart?=		../../graphics/gdchart
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gdchart=gdchart
