98966e8146c32c729076a97ec5e3fe9fdaeaaedd
jmmv <jmmv@pkgsrc.org>
2003-06-27 23:02:51 +0000

    Update to 2.2.5:
    - Gimp Import/Load plugin
    - Bugfixes
    - Few bugfixes
    - All sorts of build and installation fixes
    - "Bugfix" for a regression in GTK+'s pixbuf-io loader scheme
    - Handle SVGZ files (Gzipped SVG)
    - Handle objectBoundingBox coordinates on gradients
    - API extensions
    - Various bugfixes and crash fixes
    - Better CSS style handling
    - Fix for a crasher exploited by Sodipodi. Improved gradient handling
    - RSVG allows you to specify the JPEG quality
    - Handles Kontour's conicalGradients to some extent
    - Better CSS style handling
    - Works in "international" locales (non-C locales) in a threadsafe manner
    - Handle rounded rects
    - Add manpage
    - Handle "ex" units better
    - Missed files in tarball added.
    - Fixed memory leak
    - Better handling of display and visibility styles.
    - Better font handling (stretch, style, variant, weight supported, inherited)
    - Fixed crash with non-utf8 characters.
    - Better ellipse rendering
    - New functions to set rendering DPI
    - Better handling of SVGs colors, gradients and images
    - support dashes
    - Includes a new gdk-pixbuf svg loader
    - Includes a new gimp svg loader
    - Now installs a binary that can rasterize svgs.
    - New co-maintainer Dom Lachowicz
    - Added new svg based gtk+ engine
    - Handles SVGs a lot better. Especially with regards to units.

--- graphics/librsvg2/buildlink2.mk
+++ graphics/librsvg2/buildlink2.mk
@@ -1,20 +1,22 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/05/02 11:55:25 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/06/27 23:02:51 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use librsvg2.
 #
-# This file was created automatically using createbuildlink 2.2.
+# This file was created automatically using createbuildlink 2.6.
 #
 
 .if !defined(LIBRSVG2_BUILDLINK2_MK)
 LIBRSVG2_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			librsvg2
-BUILDLINK_DEPENDS.librsvg2?=		librsvg2>=2.0.1nb2
+BUILDLINK_DEPENDS.librsvg2?=		librsvg2>=2.2.5
 BUILDLINK_PKGSRCDIR.librsvg2?=		../../graphics/librsvg2
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.librsvg2=librsvg2
 BUILDLINK_PREFIX.librsvg2_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.librsvg2+=	include/librsvg-2/librsvg/*.h
+BUILDLINK_FILES.librsvg2+=	include/librsvg-2/librsvg/*
+BUILDLINK_FILES.librsvg2+=	lib/gtk-2.0/2.2.0/engines/libsvg.*
+BUILDLINK_FILES.librsvg2+=	lib/gtk-2.0/2.2.0/loaders/svg_loader.*
 BUILDLINK_FILES.librsvg2+=	lib/librsvg-2.*
 BUILDLINK_FILES.librsvg2+=	lib/pkgconfig/librsvg-2.0.pc
 
@@ -22,6 +24,7 @@ BUILDLINK_FILES.librsvg2+=	lib/pkgconfig/librsvg-2.0.pc
 .include "../../graphics/libart2/buildlink2.mk"
 .include "../../textproc/libxml2/buildlink2.mk"
 .include "../../devel/glib2/buildlink2.mk"
+.include "../../devel/libgsf/buildlink2.mk"
 .include "../../devel/pango/buildlink2.mk"
 .include "../../devel/popt/buildlink2.mk"
 .include "../../x11/gtk2/buildlink2.mk"
