f5471bd6f037c8bfaf868d987b2dbacdcd10c173
drochner <drochner@pkgsrc.org>
2003-07-03 15:56:37 +0000

    It seems that splitting gcc3 is more work than I expected. Until this is done, put everything back but java.

--- lang/gcc3/files/gcc3.mk
+++ lang/gcc3/files/gcc3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: gcc3.mk,v 1.5 2003/07/03 11:58:05 drochner Exp $
+# $NetBSD: gcc3.mk,v 1.6 2003/07/03 15:56:38 drochner Exp $
 #
 # make configuration file for @PKGNAME_NOREV@
 
@@ -6,3 +6,5 @@ USE_GCC3=	# defined
 CC=		@GCC_PREFIX@/bin/cc
 CPP=		@GCC_PREFIX@/bin/cpp
 CXX=		@GCC_PREFIX@/bin/c++
+F77=		@GCC_PREFIX@/bin/g77
+PKG_FC=		@GCC_PREFIX@/bin/g77
