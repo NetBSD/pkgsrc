fd3d92443f92ba5111a8c8b58a3a6b88f775ba51
seb <seb>
2003-07-04 16:27:24 +0000

    Convert to USE_NEW_TEXINFO. Remove or trim "info related" patch files. Install info files in package specific directory. Hence the PLIST files are now nearly empty...
    This should finish making these two packages really non-conflicting.
    
    Bump PKGREVISION.

--- lang/gcc3/PLIST.java
+++
@@ -1,7 +0,0 @@
-@comment $NetBSD: PLIST.java,v 1.1 2003/07/03 16:16:30 drochner Exp $
-@unexec ${INSTALL_INFO} --delete %D/info/fastjar.info %D/info/dir
-info/fastjar.info
-@exec ${INSTALL_INFO} %D/info/fastjar.info %D/info/dir
-@unexec ${INSTALL_INFO} --delete %D/info/gcj.info %D/info/dir
-info/gcj.info
-@exec ${INSTALL_INFO} %D/info/gcj.info %D/info/dir
