6d279aed24b70c1bb4bd5fddc40bc2ff6eb8a394
drochner <drochner>
2003-07-08 15:04:53 +0000

    some cleanup: -USE_NEW_TEXINFO, saves some patches -symlink libiberty.a into build tree instead of buildlink/lib,  saves some patches -put common make rules into Makefile.common (for all) and  buildaddon.mk (for all but the base "c" pkg)

--- lang/gcc3-c/files/gcc3.mk
+++ lang/gcc3-c/files/gcc3.mk
@@ -1,10 +1,7 @@
-# $NetBSD: gcc3.mk,v 1.1 2003/07/06 12:15:08 drochner Exp $
+# $NetBSD: gcc3.mk,v 1.2 2003/07/08 15:04:57 drochner Exp $
 #
-# make configuration file for @PKGNAME_NOREV@
+# make configuration file for @PKGNAME@
 
 USE_GCC3=	# defined
 CC=		@GCC_PREFIX@/bin/cc
 CPP=		@GCC_PREFIX@/bin/cpp
-CXX=		@GCC_PREFIX@/bin/c++
-F77=		@GCC_PREFIX@/bin/g77
-PKG_FC=		@GCC_PREFIX@/bin/g77
