d750aba60714e0d9cea99f9db0eedba304b13c15
salo <salo@pkgsrc.org>
2003-07-09 16:07:21 +0000

    More support bits for native Sun compilers (on Solaris).
    Sun's C++ compiler (CC) doesn't support passing arguments to ld via -Wl flag,
    remove this flag from the buildlink2 environment.

--- mk/buildlink2/gen-transform.sh
+++ mk/buildlink2/gen-transform.sh
@@ -1,6 +1,6 @@
 #!@BUILDLINK_SHELL@
 #
-# $NetBSD: gen-transform.sh,v 1.11 2003/04/15 17:28:05 grant Exp $
+# $NetBSD: gen-transform.sh,v 1.12 2003/07/09 16:07:22 salo Exp $
 
 transform="@_BLNK_TRANSFORM_SEDFILE@"
 untransform="@_BLNK_UNTRANSFORM_SEDFILE@"
@@ -18,6 +18,8 @@ untransform="@_BLNK_UNTRANSFORM_SEDFILE@"
 #       s:foo:bar		translates "foo" into "bar"
 #	static:src:dst		translates "src/libfoo.a" into "dst/libfoo.a"
 #	no-rpath		removes "-R*", "-Wl,-R", and "-Wl,-rpath,*"
+#	sanitize-rpath		translates "-Wl,-R", and "-Wl,-rpath,*"
+#					to whatever is appropriate
 
 gen() {
 	action=$1; shift
@@ -128,6 +130,16 @@ EOF
 s|-Wl,-R[^ 	"':;]*||g
 s|-Wl,-rpath,[^ 	"':;]*||g
 s|-R[^ 	"':;]*||g
+EOF
+			;;
+		esac
+		;;
+	sanitize-rpath)
+		case "$action" in
+		transform|untransform)
+			@CAT@ >> $sedfile << EOF
+s|-Wl,-R\([^ 	"':;]*\)|@_COMPILER_LD_FLAG@@_OPSYS_RPATH_NAME@\1|g
+s|-Wl,-rpath,\([^ 	"':;]*\)|@_COMPILER_LD_FLAG@@_OPSYS_RPATH_NAME@\1|g
 EOF
 			;;
 		esac
