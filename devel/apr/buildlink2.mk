607499afbc6f1f5c7c2baf4c08d172481106366e
itojun <itojun>
2003-07-09 08:09:04 +0000

    upgrade to apache-2.0.47/apr-0.9.4.2.0.47.
    Changes with Apache 2.0.47
      *) SECURITY [CAN-2003-0192]: Fixed a bug whereby certain sequences
         of per-directory renegotiations and the SSLCipherSuite directive
         being used to upgrade from a weak ciphersuite to a strong one
         could result in the weak ciphersuite being used in place of the
         strong one.  [Ben Laurie]
      *) SECURITY [CAN-2003-0253]: Fixed a bug in prefork MPM causing
         temporary denial of service when accept() on a rarely accessed port
         returns certain errors.  Reported by Saheed Akhtar
         <S.Akhtar@talis.com>.  [Jeff Trawick]
      *) SECURITY [CAN-2003-0254]: Fixed a bug in ftp proxy causing denial
         of service when target host is IPv6 but proxy server can't create
         IPv6 socket.  Fixed by the reporter.  [Yoshioka Tsuneo
         <tsuneo.yoshioka@f-secure.com>]
      *) SECURITY [VU#379828] Prevent the server from crashing when entering
         infinite loops. The new LimitInternalRecursion directive configures
         limits of subsequent internal redirects and nested subrequests, after
         which the request will be aborted.  PR 19753 (and probably others).
         [William Rowe, Jeff Trawick, André Malo]
      *) core_output_filter: don't split the brigade after a FLUSH bucket if
         it's the last bucket.  This prevents creating unneccessary empty
         brigades which may not be destroyed until the end of a keepalive
         connection.
         [Juan Rivera <Juan.Rivera@citrix.com>]
      *) Add support for "streamy" PROPFIND responses.
         [Ben Collins-Sussman <sussman@collab.net>]
      *) mod_cgid: Eliminate a double-close of a socket.  This resolves
         various operational problems in a threaded MPM, since on the
         second attempt to close the socket, the same descriptor was
         often already in use by another thread for another purpose.
         [Jeff Trawick]
      *) mod_negotiation: Introduce "prefer-language" environment variable,
         which allows to influence the negotiation process on request basis
         to prefer a certain language.  [André Malo]
      *) Make mod_expires' ExpiresByType work properly, including for
         dynamically-generated documents.  [Ken Coar, Bill Stoddard]

--- devel/apr/buildlink2.mk
+++ devel/apr/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/05/29 09:37:08 jklos Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/07/09 08:09:04 itojun Exp $
 #
 
 .if !defined(APR_BUILDLINK2_MK)
 APR_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		apr
-BUILDLINK_DEPENDS.apr?=		apr>=0.9.4.2.0.46
+BUILDLINK_DEPENDS.apr?=		apr>=0.9.4.2.0.47
 BUILDLINK_PKGSRCDIR.apr?=	../../devel/apr
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.apr=apr
