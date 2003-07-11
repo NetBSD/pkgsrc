b2d6642c9889715151e5a3f9a647e28754881155
jschauma <jschauma@pkgsrc.org>
2003-07-11 15:40:18 +0000

    Rather than hardcoding IRIX into bsd.pkg.mk when checking if xpkgwedge is needed let's use _OPSYS_NEEDS_XPKGWEDGE to be set in defs.${OPSYS}.mk

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1209 2003/07/10 23:10:07 grant Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1210 2003/07/11 15:40:18 jschauma Exp $
 #
 # This file is in the public domain.
 #
@@ -81,7 +81,8 @@ USE_X11?=		implied
 
 # IRIX *always* needs xpkgwedge
 .if defined(USE_X11BASE)
-.  if ${OPSYS} == "IRIX" ||						\
+.  if ( defined(_OPSYS_NEEDS_XPKGWEDGE) && 				\
+	!empty(_OPSYS_NEEDS_XPKGWEDGE:M[yY][eE][sS]) ) ||
       exists(${LOCALBASE}/lib/X11/config/xpkgwedge.def) ||		\
       exists(${X11BASE}/lib/X11/config/xpkgwedge.def)
 BUILD_DEPENDS+=		xpkgwedge>=1.5:../../pkgtools/xpkgwedge
