5e4225a2a71b84cc6e0473db6c9891fdf1e6c681
abs <abs@pkgsrc.org>
2003-07-17 09:07:54 +0000

    Update:         gcc3 to 3.3nb5         gcc3-c to 3.3nb1         gcc3-c++ to 3.3nb1 In gcc3.mk set CPP to '.../cpp -gcc' rather than '.../cpp' Ensure __GNUC__ and similar are defined.

--- lang/gcc3-c++/files/gcc3.mk
+++ lang/gcc3-c++/files/gcc3.mk
@@ -1,8 +1,8 @@
-# $NetBSD: gcc3.mk,v 1.2 2003/07/08 15:04:59 drochner Exp $
+# $NetBSD: gcc3.mk,v 1.3 2003/07/17 09:07:57 abs Exp $
 #
 # make configuration file for @PKGNAME@
 
 USE_GCC3=	# defined
 CC=		@GCC_PREFIX@/bin/cc
-CPP=		@GCC_PREFIX@/bin/cpp
+CPP=		@GCC_PREFIX@/bin/cpp -gcc
 CXX=		@GCC_PREFIX@/bin/c++
