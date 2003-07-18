48dbe1740e38d58ac8e6f3d06df11a2fb89eda07
grant <grant@pkgsrc.org>
2003-07-18 07:31:13 +0000

    allow these to work with -current glib/gtk on Solaris, add links for libgmodule and handle symlinked libraries more cleanly.

--- www/phoenix-bin/files/phoenix.sh
+++ www/phoenix-bin/files/phoenix.sh
@@ -1,3 +1,7 @@
 #!@SH@
-# $NetBSD: phoenix.sh,v 1.3 2003/05/18 03:43:02 grant Exp $
+# $NetBSD: phoenix.sh,v 1.4 2003/07/18 07:31:14 grant Exp $
+
+LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:@PREFIX@/lib/MozillaFirebird-@PLATFORM@:@PREFIX@/lib"
+export LD_LIBRARY_PATH
+
 exec @PREFIX@/lib/MozillaFirebird-@PLATFORM@/MozillaFirebird "$@"
