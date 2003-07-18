2bd1ce0772a0f0bf03b4dfbc7a25cbb13d927d78
grant <grant>
2003-07-18 07:31:13 +0000

    allow these to work with -current glib/gtk on Solaris, add links for libgmodule and handle symlinked libraries more cleanly.

--- www/mozilla-bin/files/mozilla.sh
+++ www/mozilla-bin/files/mozilla.sh
@@ -1,3 +1,7 @@
 #!@SH@
-# $NetBSD: mozilla.sh,v 1.2 2003/04/14 22:50:19 grant Exp $
+# $NetBSD: mozilla.sh,v 1.3 2003/07/18 07:31:16 grant Exp $
+
+LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:@PREFIX@/lib/mozilla-@PLATFORM@:@PREFIX@/lib"
+export LD_LIBRARY_PATH
+
 exec @PREFIX@/lib/mozilla-@PLATFORM@/mozilla "$@"
