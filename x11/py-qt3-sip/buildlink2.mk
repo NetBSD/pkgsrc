e85ff06350484fa15a8bb1e1daa3a8ea0c1111d8
drochner <drochner@pkgsrc.org>
2003-07-22 17:19:59 +0000

    Add the links and LDFLAGS to build PyQt properly.

--- x11/py-qt3-sip/buildlink2.mk
+++ x11/py-qt3-sip/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/07/15 16:40:45 drochner Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/07/22 17:19:59 drochner Exp $
 
 .if !defined(PY_QT3_SIP_BUILDLINK2_MK)
 PY_QT3_SIP_BUILDLINK2_MK=	# defined
@@ -12,14 +12,17 @@ BUILDLINK_PKGSRCDIR.pyqt3sip?=	../../x11/py-qt3-sip
 EVAL_PREFIX+=	BUILDLINK_PREFIX.pyqt3sip=pyqt3sip
 BUILDLINK_PREFIX.pyqt3sip_DEFAULT=	${LOCALBASE}
 
-#BUILDLINK_FILES.pyqt3sip=	${PYINC}/sip.h
-#BUILDLINK_FILES.pyqt3sip+=	${PYINC}/sipQt.h
-#BUILDLINK_FILES.pyqt3sip+=	${PYSITELIB}/libsip.*
+BUILDLINK_FILES.pyqt3sip=	${PYINC}/sip.h
+BUILDLINK_FILES.pyqt3sip+=	${PYINC}/sipQt.h
+BUILDLINK_FILES.pyqt3sip+=	${PYSITELIB}/libsip.*
+
+SIPBIN=	${BUILDLINK_PREFIX.pyqt3sip}/bin/sip${PYVERSSUFFIX}
+
+BUILDLINK_LDFLAGS.pyqt3sip=	-L${PREFIX}/${PYSITELIB} -Wl,-R${PREFIX}/${PYSITELIB}
+LDFLAGS+=	-Wl,-R${PREFIX}/${PYSITELIB}
 
 BUILDLINK_TARGETS+=	pyqt3sip-buildlink
 
 pyqt3sip-buildlink: _BUILDLINK_USE
-
-SIPBIN=	${BUILDLINK_PREFIX.pyqt3sip}/bin/sip${PYVERSSUFFIX}
 
 .endif	# PY_QT3_SIP_BUILDLINK2_MK
