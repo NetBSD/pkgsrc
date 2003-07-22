94c75c232a692acd0e6d4447060a772e0455ceab
drochner <drochner>
2003-07-22 17:22:48 +0000

    Add PyQt-3.7 for Qt3, the base module and tools.

---
+++ x11/py-qt3-base/buildlink2.mk
@@ -0,0 +1,27 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/07/22 17:22:48 drochner Exp $
+
+.if !defined(PY_QT3_BASE_BUILDLINK2_MK)
+PY_QT3_BASE_BUILDLINK2_MK=	# defined
+
+.include "../../lang/python/pyversion.mk"
+
+BUILDLINK_PACKAGES+=		pyqt3base
+BUILDLINK_DEPENDS.pyqt3base?=	${PYPKGPREFIX}-qt3-base-[0-9]*
+BUILDLINK_PKGSRCDIR.pyqt3base?=	../../x11/py-qt3-base
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.pyqt3base=pyqt3base
+BUILDLINK_PREFIX.pyqt3base_DEFAULT=	${LOCALBASE}
+
+# LDFLAGS for this are inherited from py-qt3-sip
+BUILDLINK_FILES.pyqt3base+=	${PYSITELIB}/libqtcmodule.*
+
+BUILDLINK_TARGETS+=	pyqt3base-buildlink
+
+pyqt3base-buildlink: _BUILDLINK_USE
+
+.include "../../x11/py-qt3-sip/buildlink2.mk"
+.include "../../x11/qt3-libs/buildlink2.mk"
+BUILDLINK_DEPENDS.qt3-tools=	qt3-tools>=3.1.2nb2
+.include "../../x11/qt3-tools/buildlink2.mk"
+
+.endif	# PY_QT3_BASE_BUILDLINK2_MK
