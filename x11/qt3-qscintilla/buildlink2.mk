266a0430bec701ca846596ac16db7f9f8f7235a9
drochner <drochner@pkgsrc.org>
2003-07-22 17:25:25 +0000

    add one

---
+++ x11/qt3-qscintilla/buildlink2.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink2.mk,v 1.1 2003/07/22 17:25:25 drochner Exp $
+
+.if !defined(QT3_SCINTILLA_BUILDLINK2_MK)
+QT3_SCINTILLA_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=		qt3-scintilla
+BUILDLINK_DEPENDS.qt3-scintilla?=	qt3-qscintilla-[0-9]*
+BUILDLINK_PKGSRCDIR.qt3-scintilla?=	../../x11/qt3-qscintilla
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.qt3-scintilla=qt3-qscintilla
+BUILDLINK_PREFIX.qt3-scintilla_DEFAULT=	${X11PREFIX}
+BUILDLINK_FILES.qt3-scintilla+=	qt3/include/qextscintilla*.h
+BUILDLINK_FILES.qt3-scintilla+=	qt3/lib/libqscintilla.*
+
+.include "../../x11/qt3-libs/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	qt3-scintilla-buildlink
+
+qt3-scintilla-buildlink: _BUILDLINK_USE
+
+.endif	# QT3_SCINTILLA_BUILDLINK2_MK
