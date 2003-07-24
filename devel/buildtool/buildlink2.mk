f616762b629c54fcd124bc0a0cf40ca37cea188c
jmmv <jmmv@pkgsrc.org>
2003-07-24 10:12:30 +0000

    Update to 0.12:
         Released on 2003/07/24, its state was alpha.
    
         o   The bt_wizard module was added, which creates the basic structure
             needed for a project based on a serie of interactive questions.
    
         o   Added cache support for many checks in bt_config.  This makes the
             program run a lot faster if checks are duplicated, and more impor-
             tant, lets the administrator define check results beforehand in the
             system wide bt_config.conf configuration file.
    
         o   The bt_swcgen module was added, which aids the system administrator
             in the creation of system-wide configuration files for bt_config
             (that is, files that store check results to avoid rechecking the same
             thing over and over again).
    
         o   The buildtool.d/config_init file was deprecated.  The entire configu-
             ration script is now read from a single file, buildtool.d/config, as
             the old framework was confusing and error prone.  Compatibility will
             be kept for some time.
    
         o   The TeXinfo documentation was removed.  The manual is now distributed
             in its own `buildtool-doc' package, written using Docbook/XML.
    
         o   The buildtool.spec file was added, needed to create RPM packages.  It
             actually supports building binary packages for RedHat Linux and SuSE
             Linux.
    
         o   Improved user experience: better usage messages and support for long
             options added.
    
         o   A developer mode was added to all configure scripts (disabled by de-
             fault).  It enables extra functionality during the build process,
             ideal for developers but which may annoy end users.
    
         o   Removed license description files.  bt_dist will suggest a COPYING
             file instead, as this is the most common practice suggested by li-
             censes.
    
         o   New standard checks in the bt_config module, specially
             bt_check_prog_m4() and bt_check_attribute().
    
         o   Code cleanup and simplification (using M4 in some places).

---
+++ devel/buildtool/buildlink2.mk
@@ -0,0 +1,41 @@
+# $NetBSD: buildlink2.mk,v 1.1 2003/07/24 10:12:30 jmmv Exp $
+
+.if !defined(BUILDTOOL_BUILDLINK2_MK)
+BUILDTOOL_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=		buildtool
+BUILDLINK_DEPENDS.buildtool?=	buildtool>=0.12
+BUILDLINK_PKGSRCDIR.buildtool?=	../../devel/buildtool
+BUILDLINK_DEPMETHOD.buildtool?=	build
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.buildtool=buildtool
+BUILDLINK_PREFIX.buildtool_DEFAULT=	${LOCALBASE}
+
+BUILDTOOL=		${BUILDLINK_PREFIX.buildtool}/bin/buildtool
+
+BUILD_TARGET=		all
+INSTALL_TARGET=		install
+
+HAS_CONFIGURE=		yes
+CONFIGURE_SCRIPT=	${BUILDTOOL} config
+CONFIGURE_ENV+=		BT_FLAGS_CC="${CFLAGS}"
+CONFIGURE_ENV+=		BT_FLAGS_CPP="${CPPFLAGS}"
+CONFIGURE_ENV+=		BT_FLAGS_CXX="${CXXFLAGS}"
+CONFIGURE_ENV+=		BT_PROG_AWK="${AWK}"
+CONFIGURE_ENV+=		BT_PROG_CC="${CC}"
+CONFIGURE_ENV+=		BT_PROG_CXX="${CXX}"
+CONFIGURE_ENV+=		BT_PROG_LD="${LD}"
+CONFIGURE_ENV+=		BT_PROG_M4="${M4}"
+CONFIGURE_ARGS+=	--prefix=${PREFIX}
+
+.if !target(do-build)
+do-build:
+	${_PKG_SILENT}${_PKG_DEBUG}${_ULIMIT_CMD}cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} ${BUILDTOOL} make all
+.endif
+
+.if !target(do-install)
+do-install:
+	${_PKG_SILENT}${_PKG_DEBUG}${_ULIMIT_CMD}cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} ${BUILDTOOL} make install
+.endif
+
+.endif	# BUILDTOOL_BUILDLINK2_MK
