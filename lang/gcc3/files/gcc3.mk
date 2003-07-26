fb0fdaefef68bf194ec5d47d367b3c783d2bb829
martin <martin@pkgsrc.org>
2003-07-26 17:37:04 +0000

    Bring in a patch from the base system gcc3 and adapt example gcc3.mk file accordingly.
      From: matthew green <mrg@netbsd.org>
      To: source-changes@NetBSD.org
      Subject: CVS commit: src/gnu/dist/gcc/gcc
      Message-Id: <20030725175744.B6FC4B004@cvs.netbsd.org>
    
      Module Name:    src
      Committed By:   mrg
      Date:           Fri Jul 25 17:57:44 UTC 2003
    
      Modified Files:
              src/gnu/dist/gcc/gcc: cppspec.c
              src/gnu/dist/gcc/gcc/config: netbsd.h
    
      Log Message:
      make `cpp -gcc' the default on NetBSD.
    
    
      To generate a diff of this commit:
      cvs rdiff -r1.1.1.1 -r1.2 src/gnu/dist/gcc/gcc/cppspec.c
      cvs rdiff -r1.1.1.1 -r1.2 src/gnu/dist/gcc/gcc/config/netbsd.h

--- lang/gcc3/files/gcc3.mk
+++ lang/gcc3/files/gcc3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: gcc3.mk,v 1.10 2003/07/17 09:07:55 abs Exp $
+# $NetBSD: gcc3.mk,v 1.11 2003/07/26 17:37:06 martin Exp $
 #
 # make configuration file for @DISTNAME@
 
 CC=		@GCC_PREFIX@/bin/gcc
-CPP=		@GCC_PREFIX@/bin/cpp -gcc
+CPP=		@GCC_PREFIX@/bin/cpp
 CXX=		@GCC_PREFIX@/bin/g++
 F77=		@GCC_PREFIX@/bin/g77
 PKG_FC=		@GCC_PREFIX@/bin/g77
