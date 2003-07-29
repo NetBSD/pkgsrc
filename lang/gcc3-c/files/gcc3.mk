bcee6bd52c3ba10093fdbbcd3d25299013c61826
martin <martin@pkgsrc.org>
2003-07-29 09:05:54 +0000

    Sync with ../gcc3 (cpp defaults to -gcc).

--- lang/gcc3-c/files/gcc3.mk
+++ lang/gcc3-c/files/gcc3.mk
@@ -1,7 +1,7 @@
-# $NetBSD: gcc3.mk,v 1.3 2003/07/17 09:07:56 abs Exp $
+# $NetBSD: gcc3.mk,v 1.4 2003/07/29 09:05:55 martin Exp $
 #
 # make configuration file for @PKGNAME@
 
 USE_GCC3=	# defined
 CC=		@GCC_PREFIX@/bin/cc
-CPP=		@GCC_PREFIX@/bin/cpp -gcc
+CPP=		@GCC_PREFIX@/bin/cpp
