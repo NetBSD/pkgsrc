221b5682d6aec58a2dc12feef12c03398d7388a3
jmmv <jmmv@pkgsrc.org>
2003-07-29 18:55:56 +0000

    Convert to the bsd.pkg.install.mk infrastructure: - Install configuration files under the examples hierarchy. - Honour PKG_SYSCONFDIR. - Use RCD_SCRIPTS to handle rc.d script. As a result, bump PKGREVISION to 1.

--- chat/zephyr-current/files/zhm.sh
+++ chat/zephyr-current/files/zhm.sh
@@ -1,24 +1,22 @@
-#!/bin/sh
+#!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: zhm.sh,v 1.1.1.1 2002/12/30 17:39:27 sommerfeld Exp $
+# $NetBSD: zhm.sh,v 1.2 2003/07/29 18:55:58 jmmv Exp $
 #
 # PROVIDE: zhm
 # REQUIRE: DAEMON
 
 . /etc/rc.subr
 
-PREFIX=${PREFIX-@prefix@}
-
 name="zhm"
 rcvar="$name"
-command="${PREFIX}/sbin/${name}"
+command="@PREFIX@/sbin/${name}"
 pidfile="/var/run/${name}.pid"
-required_files="${PREFIX}/etc/zephyr/server.list"
+required_files="@PKG_SYSCONFDIR@/server.list"
 start_precmd="zhm_precmd"
 
 zhm_precmd()
 {
-	rc_flags="`cat ${PREFIX}/etc/zephyr/server.list`"
+	rc_flags="`cat @PKG_SYSCONFDIR@/server.list`"
 }
 load_rc_config "$name"
 run_rc_command "$1"
