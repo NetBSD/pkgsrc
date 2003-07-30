ca28fe2f05649898b46fa326fc99df7c95983bf3
markd <markd@pkgsrc.org>
2003-07-30 13:16:56 +0000

    Update kdelibs to 3.1.3.
    Changes:
    * build system updates, gcc 3.4+ compilation fixes (Dirk Mueller)
    * ensure that each installed header file can be compiled on its own (has all
      necessary includes/forward declarations)
    * SECURITY: kio/khtml: Improve referer handling, always strip out username and
      password.
    * Roaming user support (Helge Deller)
    * kabc: add quotes around names from LDAP which contain non-alphanumeric characters
    * kabc: unterminated string handling fixes
    * kbugreport: only refer to http://bugs.kde.org if the bug-address is
      submit@bugs.kde.org
    * kcookiejar: don't deliver cookies when the url is "strange"
    * kcookiejar: unbreak cookies for https sites once again
    * kcookiejar: make kcookiejar --remove <domain> work
    * kdecore: never unload dante socks library, it crashes when doing so
    * kdecore: Adding KMacroExpander, class for shell-safe expansion of filenames in
      shellcode
    * kdeinit: Handle X errors instead of just crashing when they occur.
    * kdeprint: improvements to handling of cups network printers
    * kdeprint/smbview: use nmblookup -M -- to make it work with samba 3.0
    * kdeui: dragging from the "konqueror wheel" button no longer duplicates the window
    * kdeui: rewritten mouse cursor-autohide code (hopefully much more stable now)
    * kdeui/kfontdialog: Try very hard to match the font name that was passed with one
      that actually exists
    * kdeui/kmainwindow: fix for saving statusbar state when autosave is enabled
    * kdewidgets: Add some new widgets
    * keramik: Major fixes to toolbar gradient alignment, as well as some miscellaneous
      fixes.
    * khtml: avoid endless-relayout-loop
    * khtml: several backports of fixes from 3.2 development branch
    * khtml: several fixes to bugs exposed by DOM2 testsuite
    * khtml jpeg loader: use memmove in places where memory regions overlap (valgrind
      we love you)
    * kio: crash/memory leak fixes
    * kio: added ability to add host:port to the no-proxy list
    * kio: speedup handling local files
    * kio: improvements to the layout of the HTTP Authentication password dialog
    * kio: allow rtsp:// protocol redirecting to mms://
    * kio: KIOSK restriction fixes
    * kio: signal handling fixes (SIGPIPE etc)
    * kio_http: text/html mimetype in Accept: gets preference over other text/* types
    * kio_http: improvements in HTTP referer handling
    * kiconloader: memory leak fix
    * kjava: thread handling fixes
    * kjava: don't send text to the console window if its not visible (saves huge
      amount of memory on some pages)
    * kjava: catch exceptions thrown inside Applet.destroy
    * klocale: Support Macedonian plural forms

--- x11/kdelibs3/buildlink2.mk
+++ x11/kdelibs3/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.17 2003/07/13 13:54:25 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.18 2003/07/30 13:16:56 markd Exp $
 
 .if !defined(KDELIBS3_BUILDLINK2_MK)
 KDELIBS3_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		kdelibs3
 BUILDLINK_PKGBASE.kdelibs3?=	kdelibs
-BUILDLINK_DEPENDS.kdelibs3?=	kdelibs>=3.1.2nb1
+BUILDLINK_DEPENDS.kdelibs3?=	kdelibs>=3.1.3
 BUILDLINK_PKGSRCDIR.kdelibs3?=	../../x11/kdelibs3
 
 .include "../../mk/bsd.prefs.mk"
