f2ddfd00de5b038a2d19cfd22b91f241ed91c839
jklos <jklos@pkgsrc.org>
2003-07-31 09:31:04 +0000

    Pine 4.56 requires Pico 4.6; 4.4 will not do. Fixed so updates won't fail.

--- editors/pico/buildlink2.mk
+++ editors/pico/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/07/13 13:51:35 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/07/31 09:31:04 jklos Exp $
 
 .if !defined(PICO_BUILDLINK2_MK)
 PICO_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		pico
-BUILDLINK_DEPENDS.pico?=	pico>=4.4nb2
+BUILDLINK_DEPENDS.pico?=	pico>=4.6
 BUILDLINK_PKGSRCDIR.pico?=	../../editors/pico
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.pico=pico
