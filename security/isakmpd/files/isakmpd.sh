26e4f981fa7d2c345b33825b393ede91dd47371f
jmmv <jmmv@pkgsrc.org>
2003-07-31 10:38:32 +0000

    Convert to the bsd.pkg.install.mk framework by honouring PKG_SYSCONFDIR and using RCD_SCRIPTS to handle generation and installation of the rc.d script. Convert the rc.d script to the rc.subr framework too. Bump PKGREVISION to 1.

--- security/isakmpd/files/isakmpd.sh
+++ security/isakmpd/files/isakmpd.sh
@@ -1,23 +1,25 @@
-#!/bin/sh
+#!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: isakmpd.sh,v 1.1 2002/04/04 09:42:30 martti Exp $
+# $NetBSD: isakmpd.sh,v 1.2 2003/07/31 10:38:33 jmmv Exp $
+#
+# PROVIDE: isakmpd
+# REQUIRE: DAEMON NETWORK
 #
 
-case "$1" in
-	start)
-		if [ -r /etc/isakmpd/isakmpd.conf ]; then
-			echo 'Starting isakmpd'
-			@PREFIX@/sbin/isakmpd
-		fi
-		;;
-	stop)
-		if [ -f /var/run/isakmpd.pid ]; then
-			echo 'Stopping isakmpd'
-			kill `cat /var/run/isakmpd.pid`
-			rm -f /var/run/isakmpd.pid
-		fi
-		;;
-	*)
-		echo "$0 start | stop"
-		;;
-esac
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
+fi
+
+name="isakmpd"
+rcvar="${name}"
+command="@PREFIX@/sbin/isakmpd"
+pidfile="/var/run/isakmpd.pid"
+required_files="@PKG_SYSCONFDIR@/isakmpd.conf"
+
+if [ -f /etc/rc.subr ]; then
+	load_rc_config "${name}"
+	run_rc_command "$1"
+else
+	printf " ${name}"
+	eval ${command} ${isakmpd_flags} ${command_args}
+fi
