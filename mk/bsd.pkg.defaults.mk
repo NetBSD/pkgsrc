740cb57f9034e52b6ecc468e51fa96da8574732d
jmmv <jmmv@pkgsrc.org>
2003-08-04 11:43:47 +0000

    Remove 'faad' support from MPLAYER_DISABLE_DRIVERS.  It is an input driver needed to play files as pointed out by wiz@ (sorry for the delay) and salo@.

--- mk/bsd.pkg.defaults.mk
+++ mk/bsd.pkg.defaults.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.pkg.defaults.mk,v 1.168 2003/08/04 11:14:07 jmmv Exp $
+# $NetBSD: bsd.pkg.defaults.mk,v 1.169 2003/08/04 11:43:47 jmmv Exp $
 #
 
 # A file providing defaults for pkgsrc and the packages collection.
@@ -1193,7 +1193,7 @@ MOZILLA_USE_XFT?=	NO
 MPLAYER_DISABLE_DRIVERS?=
 # Whitespace separated list of output drivers that are not wanted in
 # mplayer/gmplayer's binary.
-# Possible: arts esd faad nas sdl
+# Possible: arts esd nas sdl
 # Default: empty
 
 MPLAYER_ENABLE_RUNTIME_CPU_DETECTION?= YES
