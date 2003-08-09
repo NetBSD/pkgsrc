cb2ff4dccd9858bd92a6b481b67da058df30a19f
recht <recht@pkgsrc.org>
2003-08-09 13:07:40 +0000

    Update to 3.17.1
    The NetBSD project now has the permission to download the archive from
    the official Spread site and to mirror it, so remove the restrictions.
    
    changes:
    *) Fix memory corruption and crash with groups of large size.
    *) Correct make install so it installs header files.
    *) Fix syntax error in build.xml file for Java/Ant.
    *) Cleanup prototypes to remove compiler warnings.
    *) Fix parser to correctly recognize upper, lower, and mixed case command options.
    *) During make install, remove old symlinks.
    *) Change setgroups call to be more portable. (fixes MacOSX)
    *) Change name of r and s to sprecv and spsend, and add as make targets.
       They can be built by "make testprog" (not built by default).
    *) Work on making long group names possible.
    *) Increase listen backlog for accepting client connections.
    *) Fix Win32 project files to have correct path to source files.
       (note CVS was always ok, but 3.17.0 release had incorrect path)
    *) Fix bug where large groups overflow Mess_buf in groups.c.
    *) Fix memory corruption bug when a message header is received in
       several separate packets in session.c. Thanks to Ryan Caudy for
       many, many hours tracking this down.
    *) Change order of build in Makefile so binaries are built before
       documentation.
    *) Fix Java bug where connection objects cannot be disconnected and
       then reconnected, but must be created anew. They can now be reused.
    *) Fix compile error on AIX for struct if_info.
    *) Fix security issue with buffer checks in the C library.
    *) Fix obscure off-by-one buffer error with the parser.

--- net/spread/buildlink2.mk
+++ net/spread/buildlink2.mk
@@ -1,22 +1,23 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/05/02 11:56:12 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/08/09 13:07:41 recht Exp $
+#
 
 .if !defined(SPREAD_BUILDLINK2_MK)
-SPREAD_BUILDLINK2_MK=		# defined
+SPREAD_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=		spread
-BUILDLINK_DEPENDS.spread?=	spread>=3.17.0nb1
-BUILDLINK_PKGSRCDIR.spread?=	../../net/spread
+BUILDLINK_PACKAGES+=			spread
+BUILDLINK_DEPENDS.spread?=		spread>=3.17.1
+BUILDLINK_PKGSRCDIR.spread?=		../../net/spread
 
-EVAL_PREFIX+=			BUILDLINK_PREFIX.spread=spread
-BUILDLINK_PREFIX.spread_DEFAULT=${LOCALBASE}
+EVAL_PREFIX+=	BUILDLINK_PREFIX.spread=spread
+BUILDLINK_PREFIX.spread_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.spread+=	include/spread/sp.h
 BUILDLINK_FILES.spread+=	include/spread/sp_events.h
 BUILDLINK_FILES.spread+=	include/spread/sp_func.h
 BUILDLINK_FILES.spread+=	lib/libspread.*
 BUILDLINK_FILES.spread+=	lib/libtspread.*
 
-BUILDLINK_TARGETS+=		spread-buildlink
+BUILDLINK_TARGETS+=	spread-buildlink
 
 spread-buildlink: _BUILDLINK_USE
 
-.endif # SPREAD_BUILDLINK2_MK
+.endif	# SPREAD_BUILDLINK2_MK
