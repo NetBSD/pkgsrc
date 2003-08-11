51bdc56be4d66a84b96899b886ee55befe5bf24d
recht <recht@pkgsrc.org>
2003-08-11 22:07:17 +0000

    Update to 0.10.1. This version contains many changes/fixes. among them: - Major Change: The huge file SOAPpy/SOAP.py (4,122 lines, 131K) has   been split into 10 separate files:         Client.py       NS.py           SOAPBuilder.py  Utilities.py         Config.py       Parser.py       Server.py         Errors.py       SOAP.py         Types.py   This should ease navigation and maintenance. - Added client support for WSDL, ported from ZSI by Mark Bucciarelli   <mark@hubcapconsulting.com>

--- net/py-soappy/buildlink2.mk
+++ net/py-soappy/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/03/19 11:51:09 drochner Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/08/11 22:07:17 recht Exp $
 
 .if !defined(SOAPPY_BUILDLINK2_MK)
 SOAPPY_BUILDLINK2_MK=	# defined
@@ -6,7 +6,7 @@ SOAPPY_BUILDLINK2_MK=	# defined
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_PACKAGES+=			SOAPpy
-BUILDLINK_DEPENDS.SOAPpy?=		${PYPKGPREFIX}-SOAPpy>=0.9.8
+BUILDLINK_DEPENDS.SOAPpy?=		${PYPKGPREFIX}-SOAPpy>=0.10.1
 BUILDLINK_PKGSRCDIR.SOAPpy?=		../../net/py-soappy
 
 .include "../../textproc/pyxml/buildlink2.mk"
