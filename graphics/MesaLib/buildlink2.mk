54a07c83c0cfdcf3ad78b5742c66e9067d2de2f0
jschauma <jschauma@pkgsrc.org>
2003-08-26 01:43:48 +0000

    Finally remove support of Mesa 3.4.1 completely as discussed at length on tech-pkg@ at various times.  This means that regardless of what kind of GL support comes with X11, if a package depends on GL, Mesa 5.0.1 (or higher) will be installed into ${LOCALBASE}.
    Some troubleshooting after the latest patches by Krister Walfridsson.

--- graphics/MesaLib/buildlink2.mk
+++ graphics/MesaLib/buildlink2.mk
@@ -1,81 +1,19 @@
-# $NetBSD: buildlink2.mk,v 1.14 2003/07/15 23:31:22 jschauma Exp $
+# $NetBSD: buildlink2.mk,v 1.15 2003/08/26 01:43:52 jschauma Exp $
 
 .if !defined(MESALIB_BUILDLINK2_MK)
 MESALIB_BUILDLINK2_MK=	# defined
 
 .include "../../mk/bsd.prefs.mk"
 
-MESA_REQD?=			3.4.2
-
-.if exists(${LOCALBASE}/lib/libGL.so.5) && empty(MESA_REQD:M5.[0-9]*)
-MESA_REQD=			5.0.1
-.endif
-
-BUILDLINK_DEPENDS.MesaLib?=	MesaLib>=${MESA_REQD}
+BUILDLINK_DEPENDS.MesaLib?=	MesaLib>=${MESA_VERSION}
 BUILDLINK_PKGSRCDIR.MesaLib?=	../../graphics/MesaLib
 
-_REQUIRE_BUILTIN_MESALIB?=	NO
-
-MAKEFLAGS+=			MESA_REQD="${MESA_REQD}"
-
-# XXX should be >= 3.4.2
-.if !empty(MESA_REQD:M5.[0-9]*)
-_IS_BUILTIN_MESALIB=	0
-CPPFLAGS+=		-DGLX_GLXEXT_LEGACY
-CFLAGS+=		-DGLX_GLXEXT_LEGACY
-.else
-
-# Check if we got Mesa distributed with XFree86 4.x or if we need to
-# depend on the Mesa package.
-#
-_GL_GLX_H=		${X11BASE}/include/GL/glx.h
-_X11_TMPL=		${X11BASE}/lib/X11/config/X11.tmpl
-.if exists(${_GL_GLX_H}) && exists(${_X11_TMPL})
-_IS_BUILTIN_MESALIB!=	${EGREP} -c BuildGLXLibrary ${_X11_TMPL} || ${TRUE}
-.else
-_IS_BUILTIN_MESALIB=	0
-.endif
-
-.endif  # MESA_REQD
-
-.if !empty(_REQUIRE_BUILTIN_MESALIB:M[yY][eE][sS])
-_NEED_MESALIB=		NO
-.else
-.  if ${_IS_BUILTIN_MESALIB} == "0"
-_NEED_MESALIB=		YES
-.  else
-#
-# Create an appropriate package name for the built-in MesaLib distributed
-# with XFree86 4.x.  This package name can be used to check against
-# BUILDLINK_DEPENDS.MesaLib to see if we need to install the pkgsrc
-# Mesa or if the built-in one is sufficient.
-#
-.    include "../../graphics/Mesa/version.mk"
-_MESALIB_PKG=		MesaLib-${_MESA_VERSION}
-_MESALIB_DEPENDS=	${BUILDLINK_DEPENDS.MesaLib}
-_NEED_MESALIB!= \
-	if ${PKG_ADMIN} pmatch '${_MESALIB_DEPENDS}' ${_MESALIB_PKG}; then \
-		${ECHO} "NO";						\
-	else								\
-		${ECHO} "YES";						\
-	fi
-.  endif
-.endif
-
-.if ${_NEED_MESALIB} == "YES"
 BUILDLINK_PACKAGES+=		MesaLib
 EVAL_PREFIX+=			BUILDLINK_PREFIX.MesaLib=MesaLib
 BUILDLINK_PREFIX.MesaLib_DEFAULT=	${LOCALBASE}
-.else
-BUILDLINK_PREFIX.MesaLib=	${X11BASE}
-.endif
 
-.if ${_NEED_MESALIB} == "NO"
-_BLNK_MESALIB_LDFLAGS=		-L${BUILDLINK_PREFIX.MesaLib}/lib -lGL
-LIBTOOL_ARCHIVE_UNTRANSFORM_SED+=	\
-	-e "s|${BUILDLINK_PREFIX.MesaLib}/lib/libGL.la|${_BLNK_MESALIB_LDFLAGS}|g" \
-	-e "s|${LOCALBASE}/lib/libGL.la|${_BLNK_MESALIB_LDFLAGS}|g"
-.endif
+CPPFLAGS+=		-DGLX_GLXEXT_LEGACY
+CFLAGS+=		-DGLX_GLXEXT_LEGACY
 
 BUILDLINK_FILES.MesaLib+=	include/GL/GL*.h
 BUILDLINK_FILES.MesaLib+=	include/GL/gl.h
