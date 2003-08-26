4972dd834892e31fdfafb49be29db9eedcd65358
jlam <jlam@pkgsrc.org>
2003-08-26 20:15:23 +0000

    Create a symlink from the "make" requested by the package into ${TOOLS_DIR}/bin/make.  If a package wants GNU make, then it should set: 	USE_GNU_TOOLS+=	make
    in the package Makefile.  Allow for USE_GMAKE to continue to work
    until we get a chance to sweep through pkgsrc and remove the usage
    of this now-redundant variable.
    
    This change allows us to avoid patching makefiles that use a bare
    "make" command to invoke sub-make processes.  Idea suggested by
    salo@netbsd.org in pkg/22509.

--- mk/defs.Darwin.mk
+++ mk/defs.Darwin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.Darwin.mk,v 1.51 2003/08/23 09:07:07 grant Exp $
+# $NetBSD: defs.Darwin.mk,v 1.52 2003/08/26 20:15:25 jlam Exp $
 #
 # Variable definitions for the Darwin operating system.
 
@@ -103,7 +103,6 @@ IMAKE_MANNEWSUFFIX=    ${IMAKE_MAN_SUFFIX}
 
 _DO_SHLIB_CHECKS=	yes	# on installation, fixup PLIST for shared libs
 _IMAKE_MAKE=		${MAKE}	# program which gets invoked by imake
-_OPSYS_HAS_GMAKE=	yes	# GNU make is standard
 .if ${OS_VERSION} >= 6.0
 _OPSYS_HAS_INET6=	yes	# IPv6 is standard
 .else
