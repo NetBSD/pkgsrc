f95d0695631c1526beb1e69a5a78f2eb8a93cfe7
jmmv <jmmv>
2003-08-27 12:25:40 +0000

    Drop advertising clause from UCB licensed code, following changes done in NetBSD's source files.

--- pkgtools/libnbcompat/files/stat_flags.c
+++ pkgtools/libnbcompat/files/stat_flags.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: stat_flags.c,v 1.1.1.1 2003/03/31 05:03:04 grant Exp $	*/
+/*	$NetBSD: stat_flags.c,v 1.2 2003/08/27 12:25:40 jmmv Exp $	*/
 
 /*-
  * Copyright (c) 1993
@@ -12,11 +12,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -45,7 +41,7 @@
 #if 0
 static char sccsid[] = "@(#)stat_flags.c	8.2 (Berkeley) 7/28/94";
 #else
-__RCSID("$NetBSD: stat_flags.c,v 1.1.1.1 2003/03/31 05:03:04 grant Exp $");
+__RCSID("$NetBSD: stat_flags.c,v 1.2 2003/08/27 12:25:40 jmmv Exp $");
 #endif
 #endif /* not lint */
 
