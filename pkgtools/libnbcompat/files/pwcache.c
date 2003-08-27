9df63dbfb4a3023c65e4f5885ba3787b1bdd8cf7
jmmv <jmmv@pkgsrc.org>
2003-08-27 12:25:40 +0000

    Drop advertising clause from UCB licensed code, following changes done in NetBSD's source files.

--- pkgtools/libnbcompat/files/pwcache.c
+++ pkgtools/libnbcompat/files/pwcache.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pwcache.c,v 1.1.1.1 2003/03/31 05:02:58 grant Exp $	*/
+/*	$NetBSD: pwcache.c,v 1.2 2003/08/27 12:25:40 jmmv Exp $	*/
 
 /*-
  * Copyright (c) 1992 Keith Muller.
@@ -16,11 +16,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -80,7 +76,7 @@
 #if 0
 static char sccsid[] = "@(#)cache.c	8.1 (Berkeley) 5/31/93";
 #else
-__RCSID("$NetBSD: pwcache.c,v 1.1.1.1 2003/03/31 05:02:58 grant Exp $");
+__RCSID("$NetBSD: pwcache.c,v 1.2 2003/08/27 12:25:40 jmmv Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
