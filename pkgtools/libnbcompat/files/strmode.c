90a54ec28f3aa6414ccbf18159d7da8901cae390
jlam <jlam@pkgsrc.org>
2003-09-01 15:31:14 +0000

    Bump version to 20030823:
    * Add mkdtemp, TAILQ_*, and statfs.
    
    * Install a nbconfig.h file containing the discovered system information
      and include it directly in nbcompat.h.  Make all files use nbconfig.h
      instead of config.h.  Ensure that all installed headers don't include
      any local header files.  This makes libnbcompat more self-contained.
    
    * Consistently use "#if HAVE_HEADER_H" instead of "#ifdef HAVE_HEADER_H"
      since sometimes people put -DHAVE_HEADER_H=0 on their command lines.

--- pkgtools/libnbcompat/files/strmode.c
+++ pkgtools/libnbcompat/files/strmode.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: strmode.c,v 1.2 2003/08/27 12:25:40 jmmv Exp $	*/
+/*	$NetBSD: strmode.c,v 1.3 2003/09/01 15:31:21 jlam Exp $	*/
 
 /*-
  * Copyright (c) 1990, 1993
@@ -31,11 +31,7 @@
 
 #include "nbcompat.h"
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
-#ifdef HAVE_SYS_CDEFS_H
+#if HAVE_SYS_CDEFS_H
 #include <sys/cdefs.h>
 #endif
 
@@ -43,7 +39,7 @@
 #if 0
 static char sccsid[] = "@(#)strmode.c	8.3 (Berkeley) 8/15/94";
 #else
-__RCSID("$NetBSD: strmode.c,v 1.2 2003/08/27 12:25:40 jmmv Exp $");
+__RCSID("$NetBSD: strmode.c,v 1.3 2003/09/01 15:31:21 jlam Exp $");
 #endif
 #endif /* LIBC_SCCS and not lint */
 
@@ -51,7 +47,7 @@ __RCSID("$NetBSD: strmode.c,v 1.2 2003/08/27 12:25:40 jmmv Exp $");
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#ifdef HAVE_ASSERT_H
+#if HAVE_ASSERT_H
 #include <assert.h>
 #endif
 
