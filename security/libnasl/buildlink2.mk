85068e5720e48c5d2e38794eb5ca5e928a0ccc71
frueauf <frueauf>
2003-09-02 09:29:47 +0000

    Update libnasl, nessus{-core,-libraries,-plugins} to 2.0.7.
    Based on pr pkg/22356 by Adrian Portelli.
    
    Changes since 2.0.6a:
    
    . changes by Renaud Deraison (deraison@cvs.nessus.org)
    - Fixed bad performances issues when pinging dead hosts
    - Fixed a bug which would prevent to store items larger than 2kb in the KB
    - NFS and SMB file-related functions completed (open, read and cwd are
      implemented)
    - Plugins support for Windows 2003
    - Network IPs can now be evenly sliced instead of being scanned
      sequentially
    - User-definable source-IP(s) for the checks (nessusd -S)
    - Fixed a possible message corruption problem if a plugin was to send a too
      long message back to nessusd
    - Fixed a possible plugin corruption problem when the client overwrites
      existing plugins
    - Fixed various false positives and wording issues in several plugins

--- security/libnasl/buildlink2.mk
+++ security/libnasl/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/06/09 17:01:24 frueauf Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/09/02 09:29:47 frueauf Exp $
 #
 
 .if !defined(LIBNASL_BUILDLINK2_MK)
 LIBNASL_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			libnasl
-BUILDLINK_DEPENDS.libnasl?=		libnasl>=2.0.6a
+BUILDLINK_DEPENDS.libnasl?=		libnasl>=2.0.7
 BUILDLINK_PKGSRCDIR.libnasl?=		../../security/libnasl
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libnasl=libnasl
