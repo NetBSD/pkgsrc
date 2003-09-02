f7e3ee93fd5bddda86495d0fce9d0333fdd8f18c
jlam <jlam@pkgsrc.org>
2003-09-02 07:14:39 +0000

    Update pkgtools/x11-links to 0.13.  This package now uses buildlink3 to build.  I'm bumping the version number just in case the installed package is different from version 0.12 (I can't test on different systems).

---
+++ pkgtools/x11-links/openwin.mk
@@ -0,0 +1,3 @@
+# $NetBSD: openwin.mk,v 1.1 2003/09/02 07:14:42 jlam Exp $
+
+FILES_LIST=	${FILESDIR}/openwin
---
+++ pkgtools/x11-links/xsgi.mk
@@ -0,0 +1,3 @@
+# $NetBSD: xsgi.mk,v 1.1 2003/09/02 07:14:45 jlam Exp $
+
+FILES_LIST=	${FILESDIR}/xsgi
