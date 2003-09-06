cd4c91e7da89784f700368735f3a30fea1a8f499
jlam <jlam>
2003-09-06 01:35:07 +0000

    Update pkgtools/libnbcompat to 20030830.  Changes from the previous version are removing sha2* object files from libnbcompat.a.  It turns out that the functions defined there aren't provided by NetBSD, so it makes no sense for libnbcompat to define them, and none of the utilities in pkgsrc that link against libnbcompat actually use these functions anyway.

--- pkgtools/libnbcompat/buildlink2.mk
+++ pkgtools/libnbcompat/buildlink2.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink2.mk,v 1.12 2003/09/05 18:35:58 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.13 2003/09/06 01:35:07 jlam Exp $
 
 .if !defined(LIBNBCOMPAT_BUILDLINK2_MK)
 LIBNBCOMPAT_BUILDLINK2_MK=     # defined
 
-BUILDLINK_DEPENDS.libnbcompat?=		libnbcompat>=20030829
+BUILDLINK_DEPENDS.libnbcompat?=		libnbcompat>=20030830
 BUILDLINK_PKGSRCDIR.libnbcompat?=	../../pkgtools/libnbcompat
 BUILDLINK_DEPMETHOD.libnbcompat?=	build
 
