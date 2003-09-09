6b899f8cc197d810ce2778ea80e8e4f43bf79a4f
recht <recht@pkgsrc.org>
2003-09-09 13:06:20 +0000

    Initial import of py-gnome2 2.0.0.
    The gnome-python package contains the source packages for the Python
    bindings for GNOME called PyGNOME.
    
    PyGNOME is an extension module for Python that provides access to the
    base GNOME libraries, so you have access to more widgets, a simple
    configuration interface, and metadata support.

---
+++ x11/py-gnome2/buildlink2.mk
@@ -0,0 +1,29 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/09/09 13:06:20 recht Exp $
+
+.if !defined(PYGNOME2_BUILDLINK2_MK)
+PYGNOME2_BUILDLINK2_MK=	# defined
+
+PYTHON_VERSIONS_ACCEPTED=	23pth 22pth
+.include "../../lang/python/pyversion.mk"
+
+BUILDLINK_PACKAGES+=			pygnome2
+BUILDLINK_DEPENDS.pygnome2?=		${PYPKGPREFIX}-gnome2>=2.0.0
+BUILDLINK_PKGSRCDIR.pygnome2?=		../../x11/py-gnome2
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.pygnome2=${PYPKGPREFIX}-gnome2
+BUILDLINK_PREFIX.pygnome2_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.pygnome2+=	lib/pkgconfig/gnome-python-2.0.pc
+
+.include "../../www/libgtkhtml/buildlink2.mk"
+.include "../../net/py-ORBit/buildlink2.mk"
+.include "../../print/libgnomeprintui/buildlink2.mk"
+.include "../../sysutils/nautilus/buildlink2.mk"
+.include "../../x11/libzvt/buildlink2.mk"
+.include "../../x11/gnome-panel/buildlink2.mk"
+.include "../../x11/py-gtk2/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	pygnome2-buildlink
+
+pygnome2-buildlink: _BUILDLINK_USE
+
+.endif	# PYGNOME2_BUILDLINK2_MK
