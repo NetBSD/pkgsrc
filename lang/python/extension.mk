13d692511892ffdcc245e61cfc047931948ac864
recht <recht@pkgsrc.org>
2003-09-11 13:13:47 +0000

    Adapt a change from FreeBSD's ports Mk/bsd.python.mk. Change the order of PYSETUP*AGS and the corresponding target. Eg.: s/${PYSETUPINSTALLARGS} install/install ${PYSETUPINSTALLARGS}/
    This allows us to automatically install .pyo files with a
    PYSETUPINSTALLARGS=-O1 -c and thus drop the compileall.py stuff in the
    post-install target.
    
    ok'ed by drochner@

--- lang/python/extension.mk
+++ lang/python/extension.mk
@@ -1,4 +1,4 @@
-# $NetBSD: extension.mk,v 1.3 2003/05/09 09:24:17 jdolecek Exp $
+# $NetBSD: extension.mk,v 1.4 2003/09/11 13:13:47 recht Exp $
 
 # derive a python version from the package name if possible
 .if defined(PKGNAME_REQD)
@@ -22,11 +22,11 @@ PYSETUPSUBDIR?=		#empty
 
 do-build:
 	(cd ${WRKSRC}/${PYSETUPSUBDIR} && ${SETENV} ${MAKE_ENV} ${PYTHONBIN} \
-	 ${PYSETUP} ${PYSETUPBUILDARGS} build)
+	 ${PYSETUP} build ${PYSETUPBUILDARGS})
 
 do-install:
 	(cd ${WRKSRC}/${PYSETUPSUBDIR} && ${SETENV} ${MAKE_ENV} ${PYTHONBIN} \
-	 ${PYSETUP} ${PYSETUPINSTALLARGS} install)
+	 ${PYSETUP} install ${PYSETUPINSTALLARGS})
 .endif
 
 .if defined(PY_PATCHPLIST)
