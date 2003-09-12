bba8dfb0b57df69fcecc6ce7992ea712959b6588
grant <grant@pkgsrc.org>
2003-09-12 13:03:38 +0000

    start of improved compiler handling, mostly derived from the existing gcc buildlink2.mk files and splitting the non-buildlink2.mk bits out into compiler.mk.
    this allows USE_GCC2 and USE_GCC3 to work with non-buildlink2 packages
    again.

--- lang/gcc/buildlink2.mk
+++ lang/gcc/buildlink2.mk
@@ -1,16 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.13 2003/07/13 13:04:09 grant Exp $
+# $NetBSD: buildlink2.mk,v 1.14 2003/09/12 13:03:40 grant Exp $
 
-# do not directly include this file. .include "../../mk/gcc.buildlink2.mk"
-# to ensure the correct gcc package is selected based on USE_GCC2 and
-# USE_GCC3.
+# Do not directly include this file in package Makefiles. It is
+# automatically included when required based on USE_GCC2.
 
 .if !defined(GCC2_BUILDLINK2_MK)
 GCC2_BUILDLINK2_MK=	# defined
 
-# If you want to allow EGCS to satisfy the GCC requirement, then set
-# GCC_REQD to "2.8.0".
-#
-GCC_REQD?=			2.95.3
 BUILDLINK_DEPENDS.gcc?=		gcc>=${GCC_REQD}
 BUILDLINK_PKGSRCDIR.gcc?=	../../lang/gcc
 
@@ -25,20 +20,11 @@ BUILDLINK_DEPMETHOD.gcc?=	build
 
 BUILDLINK_PREFIX.gcc=	${LOCALBASE}
 
-_GCC_SUBPREFIX=		gcc-2.95.3/
 _GCC_PREFIX=		${BUILDLINK_PREFIX.gcc}/${_GCC_SUBPREFIX}
-_GCC_ARCHSUBDIR=	lib/gcc-lib/${MACHINE_GNU_PLATFORM}/2.95.3
-_GCC_ARCHDIR=		${_GCC_PREFIX}${_GCC_ARCHSUBDIR}
 
 BUILDLINK_LDFLAGS.gcc=	-L${_GCC_ARCHDIR} -Wl,${RPATH_FLAG}${_GCC_ARCHDIR} -L${_GCC_PREFIX}lib -Wl,${RPATH_FLAG}${_GCC_PREFIX}lib
 
 BUILDLINK_PACKAGES+=	gcc
-PATH:=		${_GCC_PREFIX}bin:${PATH}
-CC=		${_GCC_PREFIX}bin/gcc
-CPP=		${_GCC_PREFIX}bin/cpp
-CXX=		${_GCC_PREFIX}bin/g++
-F77=		${_GCC_PREFIX}bin/g77
-PKG_FC=		${F77}
 
 .if defined(USE_GCC_SHLIB)
 LDFLAGS+=		${BUILDLINK_LDFLAGS.gcc}
