f94c96e733b807f4c75fd2191094c0b86c074eca
jlam <jlam@pkgsrc.org>
2003-09-13 09:24:07 +0000

    Update pkg_install to 20030913.  Changes from version 20020912 are quieting the file removal when deleting linkfarms if the expected file doesn't exist.  Since the symlink isn't there, the mission is already accomplished, so why bother with the warning?

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.13 2003/09/13 05:50:26 jlam Exp $	*/
+/*	$NetBSD: version.h,v 1.14 2003/09/13 09:24:22 jlam Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20030912"
+#define PKGTOOLS_VERSION "20030913"
 
 #endif /* _INST_LIB_VERSION_H_ */
