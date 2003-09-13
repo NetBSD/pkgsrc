c74a02ee07512d9a9b91904ed9e6fba09596ebab
jlam <jlam@pkgsrc.org>
2003-09-13 23:21:19 +0000

    Need to use :C instead of :S to get the "pop" action to work properly.

--- graphics/glu/buildlink3.mk
+++ graphics/glu/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2003/09/07 19:43:36 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2003/09/13 23:21:21 jlam Exp $
 
 .if !defined(GLU_BUILDLINK3_MK)
 GLU_BUILDLINK3_MK=	# defined
@@ -65,5 +65,5 @@ BUILDLINK_DEPENDS+=	glu
 BUILDLINK_PACKAGES+=	glu
 .endif
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/\+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:C/\+$//}
 .endif	# GLU_BUILDLINK3_MK
