fc7611e4af0c532d3922e01607a2c89e2656c2fc
jlam <jlam>
2003-09-14 22:25:25 +0000

    Update net/samba to 2.2.8nb4.  Changes from 2.2.8nb3 are installing adduser and deluser scripts into ${PKG_SYSCONFDIR} that are capable of dealing with usernames containing a "$".  These scripts basically accept the same options as useradd/userdel.  They're meant to be used in "add user script" and "delete user script" to deal with samba machine accounts.

---
+++ net/samba/files/deluser.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+#
+# $NetBSD: deluser.sh,v 1.1 2003/09/14 22:25:26 jlam Exp $
+#
+# This is a deluser script for NetBSD systems whose userdel(8) doesn't
+# accept "$" in the username.
+
+awkprog="@AWK@"
+mktempprog="@MKTEMP@"
+pwdmkdbprog="@PWD_MKDB@"
+rmprog="@RM@"
+
+progname=deluser
+
+usage() {
+	echo "Usage: $progname [-n] name"
+	exit 1
+}
+
+doit=""
+
+while getopts n flag; do
+	case $flag in
+	n)	doit=":" ;;
+	esac
+done
+shift `expr $OPTIND - 1`
+
+if [ $# -lt 1 ]; then
+	usage
+fi
+
+name="$1"
+
+case "$name" in
+*:*)
+	echo "$progname: \`$name' is not a valid user name" 1>&2
+	exit 1
+esac
+
+ptmp=`$mktempprog -q /etc/ptmp`
+case "$ptmp" in
+/etc/ptmp)
+	;;
+*)
+	echo "$progname: can't create /etc/ptmp" 1>&2
+	exit 1
+	;;
+esac
+
+if [ ! -f /etc/master.passwd ]; then
+	echo "$progname: /etc/master.passwd not found" 1>&2
+	$rmprog -f $ptmp
+	exit 1
+fi
+
+$awkprog -v name="${name}" '						\
+	BEGIN { FS = ":" }						\
+	{								\
+		if ($1 != name)						\
+			print $0;					\
+	}' /etc/master.passwd > $ptmp
+
+$doit $pwdmkdbprog -p $ptmp
+rc=$?
+
+$rmprog -f $ptmp
+exit $rc
