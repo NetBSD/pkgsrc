6535f577fcbbd23fc2c961aae9b6337d2ac707f0
drochner <drochner>
2003-09-16 15:47:19 +0000

    the USE_GCC3 make variable causes conflicts with the monolithic gcc3 pkg. Until we have a sane solution, delete it for now. Addresses PR pkg/22773 by John R. Shannon.

--- lang/gcc3-f77/files/gcc3.mk
+++ lang/gcc3-f77/files/gcc3.mk
@@ -1,7 +1,6 @@
-# $NetBSD: gcc3.mk,v 1.2 2003/07/08 15:05:00 drochner Exp $
+# $NetBSD: gcc3.mk,v 1.3 2003/09/16 15:47:21 drochner Exp $
 #
 # make configuration file for @PKGNAME@
 
-USE_GCC3=	# defined
 F77=		@GCC_PREFIX@/bin/g77
 PKG_FC=		@GCC_PREFIX@/bin/g77
