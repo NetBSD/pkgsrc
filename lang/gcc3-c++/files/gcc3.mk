e703e7f91b332a0adf23c70600789b2501b005d9
drochner <drochner@pkgsrc.org>
2003-09-16 15:47:19 +0000

    the USE_GCC3 make variable causes conflicts with the monolithic gcc3 pkg. Until we have a sane solution, delete it for now. Addresses PR pkg/22773 by John R. Shannon.

--- lang/gcc3-c++/files/gcc3.mk
+++ lang/gcc3-c++/files/gcc3.mk
@@ -1,8 +1,7 @@
-# $NetBSD: gcc3.mk,v 1.4 2003/07/29 09:02:50 martin Exp $
+# $NetBSD: gcc3.mk,v 1.5 2003/09/16 15:47:19 drochner Exp $
 #
 # make configuration file for @PKGNAME@
 
-USE_GCC3=	# defined
 CC=		@GCC_PREFIX@/bin/cc
 CPP=		@GCC_PREFIX@/bin/cpp
 CXX=		@GCC_PREFIX@/bin/c++
