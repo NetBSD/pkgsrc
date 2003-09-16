6865175e09cb35bcad3e3bd1b7bb656efc257926
grant <grant@pkgsrc.org>
2003-09-16 16:49:51 +0000

    bump BUILDLINK_DEPENDS to 20030916.

--- pkgtools/libnbcompat/buildlink2.mk
+++ pkgtools/libnbcompat/buildlink2.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink2.mk,v 1.13 2003/09/06 01:35:07 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.14 2003/09/16 16:49:51 grant Exp $
 
 .if !defined(LIBNBCOMPAT_BUILDLINK2_MK)
 LIBNBCOMPAT_BUILDLINK2_MK=     # defined
 
-BUILDLINK_DEPENDS.libnbcompat?=		libnbcompat>=20030830
+BUILDLINK_DEPENDS.libnbcompat?=		libnbcompat>=20030916
 BUILDLINK_PKGSRCDIR.libnbcompat?=	../../pkgtools/libnbcompat
 BUILDLINK_DEPMETHOD.libnbcompat?=	build
 
