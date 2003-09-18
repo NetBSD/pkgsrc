d9fce77b3b3991338dda5a28565484de4b3ebc4a
markd <markd@pkgsrc.org>
2003-09-18 05:37:15 +0000

    Update openldap to version 2.1.22.
        This release contains the following major enhancements:
    
            * Transactional backend
            * Improved Unicode handling
            * Improved DN handlng
            * Improved Referral handling
            * SASL authentication/authorization mapping
            * SASL in-directory storage of authentication secrets
            * Enhanced administrative limits/access controls
            * Meta backend (experimental)
            * Monitor backend (experimental)
            * Virtual "glue" backend (experimental)
            * LDAP C++ API
            * Updated LDAP C and TCL APIs
            * LDAPv3 extensions, including:
                - Enhanced Language Tag/Range Support
                - 'Who am i?' Extended Operation
                - 'Matched Values' Control
                - 'NOOP' Control
    
       plus lots of bug fixes.
    
    Update (to 2.1.20) from Juan RP in PR pkg/21682 with some tidy up and
    update to 2.1.22 by Marc Recht and me.
    
    Also closes PR pkg/21217 by Jean-Luc Wasmer and PR pkg/20972 by Jeremy
    Reed.

--- databases/openldap/buildlink2.mk
+++ databases/openldap/buildlink2.mk
@@ -1,34 +1,34 @@
-# $NetBSD: buildlink2.mk,v 1.5 2003/05/08 12:30:37 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.6 2003/09/18 05:37:15 markd Exp $
 
 .if !defined(OPENLDAP_BUILDLINK2_MK)
 OPENLDAP_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		openldap
-BUILDLINK_DEPENDS.openldap?=	openldap>=2.0.27nb3
+BUILDLINK_DEPENDS.openldap?=	openldap>=2.1.22
 BUILDLINK_PKGSRCDIR.openldap?=	../../databases/openldap
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.openldap=openldap
 BUILDLINK_PREFIX.openldap_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.openldap=	include/disptmpl.h
 BUILDLINK_FILES.openldap+=	include/lber.h
 BUILDLINK_FILES.openldap+=	include/lber_types.h
 BUILDLINK_FILES.openldap+=	include/ldap.h
 BUILDLINK_FILES.openldap+=	include/ldap_cdefs.h
 BUILDLINK_FILES.openldap+=	include/ldap_features.h
 BUILDLINK_FILES.openldap+=	include/ldap_schema.h
-BUILDLINK_FILES.openldap+=	include/srchpref.h
+BUILDLINK_FILES.openldap+=	include/ldap_utf8.h
 BUILDLINK_FILES.openldap+=	lib/liblber.*
 BUILDLINK_FILES.openldap+=	lib/libldap.*
+BUILDLINK_FILES.openldap+=	lib/libldap_r.*
 
 .include "../../mk/bsd.prefs.mk"
 
-.if ${OPSYS} == SunOS
-.  include "../../databases/db/buildlink2.mk"
-.endif
-
-.if defined(USE_SASL) && (${USE_SASL} == "YES")
+.if defined(USE_SASL2) && (${USE_SASL2} == "YES")
+.  include "../../security/cyrus-sasl2/buildlink2.mk"
+.elif defined(USE_SASL) && (${USE_SASL} == "YES")
 .  include "../../security/cyrus-sasl/buildlink2.mk"
 .endif
+
+.include "../../security/openssl/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	openldap-buildlink
 
