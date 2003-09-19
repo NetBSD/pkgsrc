d6d3906685314fbebda4dd35e94c85c5e4f15bab
wiz <wiz@pkgsrc.org>
2003-09-19 17:22:14 +0000

    Update to 0.8.3. Remove Xext dependency; split in Xrender and render; use automake; add animate cursor support; some other fixes.

--- x11/Xrender/buildlink2.mk
+++ x11/Xrender/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/01/14 09:16:36 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/09/19 17:22:15 wiz Exp $
 
 .if !defined(XRENDER_BUILDLINK2_MK)
 XRENDER_BUILDLINK2_MK=	# defined
@@ -49,6 +49,9 @@ _NEED_XRENDER!= \
 .  endif
 .endif
 
+BUILDLINK_FILES.Xrender=	include/X11/extensions/Xrender.h
+BUILDLINK_FILES.Xrender+=	lib/libXrender.*
+
 .if ${_NEED_XRENDER} == "YES"
 BUILDLINK_PACKAGES+=			Xrender
 EVAL_PREFIX+=	BUILDLINK_PREFIX.Xrender=Xrender
@@ -57,19 +60,18 @@ BUILDLINK_PREFIX.Xrender_DEFAULT=	${X11PREFIX}
 # If we depend on the package, depend on the latest version
 # with a library major bump.
 #
-BUILDLINK_DEPENDS.Xrender=	Xrender>=0.7
+BUILDLINK_DEPENDS.Xrender=	Xrender>=0.8.3
+
+.include "../../x11/render/buildlink2.mk"
 .else
 BUILDLINK_PREFIX.Xrender=	${X11BASE}
+BUILDLINK_FILES.Xrender+=      include/X11/extensions/extutil.h
+BUILDLINK_FILES.Xrender+=      include/X11/extensions/region.h
+BUILDLINK_FILES.Xrender+=      include/X11/extensions/render.h
+BUILDLINK_FILES.Xrender+=      include/X11/extensions/renderproto.h
 .endif
 
 USE_X11=			# defined
-
-BUILDLINK_FILES.Xrender=	include/X11/extensions/Xrender.h
-BUILDLINK_FILES.Xrender+=	include/X11/extensions/extutil.h
-BUILDLINK_FILES.Xrender+=	include/X11/extensions/region.h
-BUILDLINK_FILES.Xrender+=	include/X11/extensions/render.h
-BUILDLINK_FILES.Xrender+=	include/X11/extensions/renderproto.h
-BUILDLINK_FILES.Xrender+=	lib/libXrender.*
 
 BUILDLINK_TARGETS+=		Xrender-buildlink
 
