e6c34a9fb4e3b860819d878e07dd0427a7dcb437
gavan <gavan@pkgsrc.org>
2003-09-22 18:26:37 +0000

    Java programs should be executed with the java command, not jre.

--- time/cardboard-schedule/files/csched.sh
+++ time/cardboard-schedule/files/csched.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-@JAVA_HOME@/bin/jre -cp @PREFIX@/lib/cardboard-schedule/stage/cs.jar:@PREFIX@/lib/cardboard-schedule/stage/CsHelp.jar:@PREFIX@/lib/cardboard-schedule/stage/jhbasic.jar:@LOCALBASE@/lib/java/swingall.jar:${CLASSPATH} avg.cs.gui.CsMainApp
+@JAVA_HOME@/bin/java -cp @PREFIX@/lib/cardboard-schedule/stage/cs.jar:@PREFIX@/lib/cardboard-schedule/stage/CsHelp.jar:@PREFIX@/lib/cardboard-schedule/stage/jhbasic.jar:@LOCALBASE@/lib/java/swingall.jar:${CLASSPATH} avg.cs.gui.CsMainApp
