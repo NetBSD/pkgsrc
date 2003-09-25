56d57c8df5893be4091ae933a46d944b87089996
xtraeme <xtraeme@pkgsrc.org>
2003-09-25 14:04:54 +0000

    Initial import of libcroco 0.2. Package imported from pkgsrc-wip.
    The Libcroco project is an effort to build a generic Cascading Style Sheet
    (CSS) parsing and manipulation toolkit that can be used by GNOME applications
    in need of CSS support.

---
+++ textproc/libcroco/buildlink2.mk
@@ -0,0 +1,35 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/09/25 14:04:55 xtraeme Exp $
+#
+# This Makefile fragment is included by packages that use libcroco.
+#
+# This file was created automatically using createbuildlink 2.6.
+#
+
+.if !defined(LIBCROCO_BUILDLINK2_MK)
+LIBCROCO_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			libcroco
+BUILDLINK_DEPENDS.libcroco?=		libcroco>=0.2.0
+BUILDLINK_PKGSRCDIR.libcroco?=		../../wip/libcroco
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.libcroco=libcroco
+BUILDLINK_PREFIX.libcroco_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.libcroco+=	include/libcroco/layeng/*.h
+BUILDLINK_FILES.libcroco+=	include/libcroco/libcroco.h
+BUILDLINK_FILES.libcroco+=	include/libcroco/parser/*.h
+BUILDLINK_FILES.libcroco+=	include/libcroco/seleng/*.h
+BUILDLINK_FILES.libcroco+=	lib/libcrlayeng.*
+BUILDLINK_FILES.libcroco+=	lib/libcroco.*
+BUILDLINK_FILES.libcroco+=	lib/libcrseleng.*
+BUILDLINK_FILES.libcroco+=	lib/pkgconfig/libcroco.pc
+
+.include "../../devel/glib2/buildlink2.mk"
+.include "../../devel/pango/buildlink2.mk"
+.include "../../devel/pkgconfig/buildlink2.mk"
+.include "../../textproc/libxml2/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	libcroco-buildlink
+
+libcroco-buildlink: _BUILDLINK_USE
+
+.endif	# LIBCROCO_BUILDLINK2_MK
