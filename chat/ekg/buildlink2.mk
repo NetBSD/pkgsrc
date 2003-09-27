30d7040bfa5963dcf8ff912ed903634fe15c1019
grant <grant>
2003-09-27 17:07:34 +0000

    move ncurses/buildlink2.mk to mk/curses.buildlink2.mk, as it provides support for base system curses/ncurses as well as ncurses itself.
    suggested by wiz.

--- chat/ekg/buildlink2.mk
+++ chat/ekg/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1 2003/07/22 10:41:42 adam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/09/27 17:07:35 grant Exp $
 
 .if !defined(EKG_BUILDLINK2_MK)
 EKG_BUILDLINK2_MK=	# defined
@@ -12,7 +12,7 @@ BUILDLINK_PREFIX.ekg_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.ekg=	include/libgadu*.h
 BUILDLINK_FILES.ekg+=	lib/libgadu.*
 
-.include "../../devel/ncurses/buildlink2.mk"
+.include "../../mk/curses.buildlink2.mk"
 .include "../../devel/pkgconfig/buildlink2.mk"
 .include "../../devel/readline/buildlink2.mk"
 .include "../../devel/zlib/buildlink2.mk"
