f25d0e5fde04c8a2e61e598e78906ebf4e2e0373
martti <martti@pkgsrc.org>
2003-09-27 06:52:41 +0000

    Require 4.0.0 instead of 3.99.4

--- graphics/xfce4-iconbox/buildlink2.mk
+++ graphics/xfce4-iconbox/buildlink2.mk
@@ -1,25 +1,23 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/09/26 10:28:20 martti Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/09/27 06:52:41 martti Exp $
 #
 # This Makefile fragment is included by packages that use xfce4-iconbox.
 #
-# This file was created automatically using createbuildlink 2.6.
+# This file was created automatically using createbuildlink 2.7.
 #
 
 .if !defined(XFCE4_ICONBOX_BUILDLINK2_MK)
 XFCE4_ICONBOX_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			xfce4-iconbox
-BUILDLINK_DEPENDS.xfce4-iconbox?=		xfce4-iconbox>=3.99.4
+BUILDLINK_DEPENDS.xfce4-iconbox?=		xfce4-iconbox>=4.0.0
 BUILDLINK_PKGSRCDIR.xfce4-iconbox?=		../../graphics/xfce4-iconbox
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.xfce4-iconbox=xfce4-iconbox
 BUILDLINK_PREFIX.xfce4-iconbox_DEFAULT=	${X11PREFIX}
 BUILDLINK_FILES.xfce4-iconbox+=	lib/xfce4/mcs-plugins/xfce4-iconbox_settings.*
 
-.include "../../devel/pkgconfig/buildlink2.mk"
 .include "../../x11/xfce4-mcs-plugins/buildlink2.mk"
 .include "../../devel/glib2/buildlink2.mk"
-.include "../../devel/pkgconfig/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	xfce4-iconbox-buildlink
 
