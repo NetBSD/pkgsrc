0d0353fc3564731786301a31b6cd27096f4ffefe
martti <martti@pkgsrc.org>
2003-09-27 06:57:16 +0000

    Regenerate (fix dependency to pkgconfig).

--- sysutils/xfce4-netload-plugin/buildlink2.mk
+++ sysutils/xfce4-netload-plugin/buildlink2.mk
@@ -1,8 +1,8 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/09/26 10:34:18 martti Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/09/27 06:57:16 martti Exp $
 #
 # This Makefile fragment is included by packages that use xfce4-netload-plugin.
 #
-# This file was created automatically using createbuildlink 2.6.
+# This file was created automatically using createbuildlink 2.7.
 #
 
 .if !defined(XFCE4_NETLOAD_PLUGIN_BUILDLINK2_MK)
@@ -18,7 +18,6 @@ BUILDLINK_FILES.xfce4-netload-plugin+=	lib/xfce4/panel-plugins/libnetload.*
 
 .include "../../x11/xfce4-panel/buildlink2.mk"
 .include "../../devel/glib2/buildlink2.mk"
-.include "../../devel/pkgconfig/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	xfce4-netload-plugin-buildlink
 
