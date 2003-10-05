08a315055052ed3f2f5b0f23ef400d2e445815e9
martti <martti@pkgsrc.org>
2003-10-05 17:55:13 +0000

    Update mysql-client to 3.23.58
    A lot of bug and security fixes since 3.23.51...

--- databases/mysql-client/buildlink2.mk
+++ databases/mysql-client/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.6 2003/09/28 09:03:52 jdolecek Exp $
+# $NetBSD: buildlink2.mk,v 1.7 2003/10/05 17:55:13 martti Exp $
 
 .if !defined(MYSQL_CLIENT_BUILDLINK2_MK)
 MYSQL_CLIENT_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			mysql-client
-BUILDLINK_DEPENDS.mysql-client?=	mysql-client>=3.23.51nb1
+BUILDLINK_DEPENDS.mysql-client?=	mysql-client>=3.23.58
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql-client
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.mysql-client=mysql-client
