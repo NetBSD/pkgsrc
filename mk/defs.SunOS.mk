4d388ba30189aa8aa4a564055b28603ba14d0da7
grant <grant@pkgsrc.org>
2003-10-11 02:44:17 +0000

    don't hardcode the -DHasGcc2... flags to imake, pass them only if we're actually using gcc on Solaris.
    fixes building of imake PIC code when the compiler is not gcc.

--- mk/defs.SunOS.mk
+++ mk/defs.SunOS.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.SunOS.mk,v 1.69 2003/09/24 07:40:03 grant Exp $
+# $NetBSD: defs.SunOS.mk,v 1.70 2003/10/11 02:44:17 grant Exp $
 #
 # Variable definitions for the SunOS/Solaris operating system.
 
@@ -42,7 +42,7 @@ GZIP_CMD?=	/usr/bin/gzip -nf ${GZIP}
 HEAD?=		/usr/bin/head
 HOSTNAME_CMD?=	/bin/hostname
 ID?=		/usr/xpg4/bin/id
-IMAKE?=		${X11BASE}/bin/imake -DHasGcc2=YES -DHasGcc2ForCplusplus=YES
+IMAKE?=		${X11BASE}/bin/imake ${IMAKEOPTS}
 LDCONFIG?=	/usr/bin/true
 LN?=		/usr/bin/ln
 LS?=		/usr/bin/ls
