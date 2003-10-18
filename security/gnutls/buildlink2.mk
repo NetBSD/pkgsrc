043c767d4ba8750168ca5c1a7dc1e159d9d08e50
jmmv <jmmv@pkgsrc.org>
2003-10-18 08:10:57 +0000

    Make this package use the libtasn library that comes with it, instead of our own security/libtasn1 package, which is too new to work fine with gnutls. While here, add missing dependency on devel/zlib. Fixes PR pkg/23172; reviewed by wiz@.  Bump PKGREVISION to 1.

--- security/gnutls/buildlink2.mk
+++ security/gnutls/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/05/14 03:46:44 salo Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/10/18 08:10:57 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use gnutls.
 #
@@ -9,7 +9,7 @@
 GNUTLS_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gnutls
-BUILDLINK_DEPENDS.gnutls?=		gnutls>=0.8.7
+BUILDLINK_DEPENDS.gnutls?=		gnutls>=0.8.7nb1
 BUILDLINK_PKGSRCDIR.gnutls?=		../../security/gnutls
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gnutls=gnutls
@@ -19,7 +19,7 @@ BUILDLINK_FILES.gnutls+=	lib/libgnutls-extra.*
 BUILDLINK_FILES.gnutls+=	lib/libgnutls.*
 
 .include "../../archivers/liblzo/buildlink2.mk"
-.include "../../security/libtasn1/buildlink2.mk"
+.include "../../devel/zlib/buildlink2.mk"
 .include "../../security/opencdk/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	gnutls-buildlink
