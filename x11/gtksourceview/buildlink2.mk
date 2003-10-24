8248d31ab2706c195a901d69b296d1e3d6c772a7
cube <cube@pkgsrc.org>
2003-10-24 21:55:34 +0000

    This package needs gmake.
    Update to 0.7.0.
    
    News in 0.7.0
    -------------
    
    * A bunch of small bugfixes
    * Replaced deprecated calls for glib 2.3
    * Multiline backward search works now
    * <keyword-item> patterns are now limited to 250 elements (more cause
      trouble with GNU regex)

--- x11/gtksourceview/buildlink2.mk
+++ x11/gtksourceview/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/09/11 11:11:34 jmmv Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/10/24 21:55:34 cube Exp $
 #
 # This Makefile fragment is included by packages that use gtksourceview.
 #
@@ -9,7 +9,7 @@
 GTKSOURCEVIEW_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gtksourceview
-BUILDLINK_DEPENDS.gtksourceview?=		gtksourceview>=0.6.0
+BUILDLINK_DEPENDS.gtksourceview?=		gtksourceview>=0.7.0
 BUILDLINK_PKGSRCDIR.gtksourceview?=		../../x11/gtksourceview
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gtksourceview=gtksourceview
