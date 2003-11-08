b29af19929d02f79955ea0352f7db51ee09a8546
xtraeme <xtraeme@pkgsrc.org>
2003-11-08 07:40:17 +0000

    Upgrade nessus and friends to 2.0.9.
    This closes PR pkg/23159 by Adrian Portelli.
    
    Changes since 2.0.7:
    
    	o The bpf sharing system now works fine on BSD systems, so Nessus
    	  now only requires one /dev/bpf to work correctly, no matter how
              many hosts are being tested
    	o A bug in tcp_ping() would make some probes have a source port set
              to 0
    	o Minor bug fixes
    	o Added functions in libnasl (join_multicast_group(), unixtime(),
              and more...)
    	o All SSL operations now use non-blocking sockets instead of the
              alarm() trick to handle timeouts
    	o Minimize the number of pixmaps that need to be created in the
              Nessus client by re-using them
    
    Review the Changelog file to see a complete list of changes.

--- security/libnasl/buildlink2.mk
+++ security/libnasl/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.4 2003/09/02 09:29:47 frueauf Exp $
+# $NetBSD: buildlink2.mk,v 1.5 2003/11/08 07:40:17 xtraeme Exp $
 #
 
 .if !defined(LIBNASL_BUILDLINK2_MK)
 LIBNASL_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			libnasl
-BUILDLINK_DEPENDS.libnasl?=		libnasl>=2.0.7
+BUILDLINK_DEPENDS.libnasl?=		libnasl>=2.0.9
 BUILDLINK_PKGSRCDIR.libnasl?=		../../security/libnasl
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libnasl=libnasl
