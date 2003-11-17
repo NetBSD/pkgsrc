692a4324baa988ff11b398bc9676ae3d3ed489ce
recht <recht@pkgsrc.org>
2003-11-17 00:45:58 +0000

    Fix typo: s/misc/graphics/

--- graphics/libdvbpsi/buildlink2.mk
+++ graphics/libdvbpsi/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/08/05 07:30:07 wulf Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/11/17 00:45:58 recht Exp $
 
 .if !defined(LIBDVBPSI_BUILDLINK2_MK)
 LIBDVBPSI_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			libdvbpsi
 BUILDLINK_DEPENDS.libdvbpsi?=		libdvbpsi>=0.1.3
-BUILDLINK_PKGSRCDIR.libdvbpsi?=	../../misc/libdvbpsi
+BUILDLINK_PKGSRCDIR.libdvbpsi?=	../../graphics/libdvbpsi
 
 EVAL_PREFIX+=		BUILDLINK_PREFIX.libdvbpsi=libdvbpsi
 BUILDLINK_PREFIX.libdvbpsi=	${LOCALBASE}
