00a8207bfc3efeaf5800b21a5d81c2aeec25f004
martin <martin>
2003-11-26 21:22:12 +0000

    Now that the toolchain is able to build this beast at all, replace the untested MD xpcom routines with working ones for sparc64 - mostly copied from the solaris sparc v9 versions.

--- www/mozilla/files/xptcinvoke_asm_sparc64_netbsd.s
+++ www/mozilla/files/xptcinvoke_asm_sparc64_netbsd.s
@@ -22,6 +22,7 @@
 
 /* Platform specific code to invoke XPCOM methods on native objects */
         .global XPTC_InvokeByIndex
+        .type   XPTC_InvokeByIndex, #function
 /*
     XPTC_InvokeByIndex(nsISupports* that, PRUint32 methodIndex,
                    PRUint32 paramCount, nsXPTCVariant* params);
@@ -49,50 +50,63 @@
  */
 	
 XPTC_InvokeByIndex:
-	save	%sp,-(128+8),%sp    ! room for the register window and this
-	sllx	%i2,3,%l0	    ! required stack size in bytes
+	save	%sp,-(128 + 64),%sp ! room for the register window and
+				    ! struct pointer, rounded up to 0 % 64
+	sll	%i2,4,%l0	    ! assume the worst case
+				    ! paramCount * 2 * 8 bytes
+	cmp	%l0, 0		    ! are there any args? If not,
+	be	.invoke 	    ! no need to copy args to stack
+	 nop
+
 	sub	%sp,%l0,%sp	    ! create the additional stack space
-	    
-	add	%sp,0x7ff+128,%l2   ! pointer to parameter save area
-	add	%l2,8,%o0	    ! %o1's parameter save area
-	mov	%i2,%o1		    ! paramCount
+	add	%sp,0x7ff+136,%o0   ! step past the register window, the
+				    ! struct result pointer and the 'this' slot
+	mov	%i2,%o1 	    ! paramCount
 	call	invoke_copy_to_stack
-	mov	%i3,%o2		    ! params
+	 mov	%i3,%o2 	    ! params
+
+!
+!   load arguments from stack into the outgoing registers
+!   BIAS is 0x7ff (2047)
+!
+
+!   load the %o1..5 64bit (extended word) output registers registers 
+	ldx	[%sp + 0x7ff + 136],%o1    ! %i1
+	ldx	[%sp + 0x7ff + 144],%o2    ! %i2
+	ldx	[%sp + 0x7ff + 152],%o3    ! %i3
+	ldx	[%sp + 0x7ff + 160],%o4    ! %i4
+	ldx	[%sp + 0x7ff + 168],%o5    ! %i5
+
+!   load the even number double registers starting with %f2
+	ldd	[%sp + 0x7ff + 136],%f2
+	ldd	[%sp + 0x7ff + 144],%f4
+	ldd	[%sp + 0x7ff + 152],%f6
+	ldd	[%sp + 0x7ff + 160],%f8
+	ldd	[%sp + 0x7ff + 168],%f10
+	ldd	[%sp + 0x7ff + 176],%f12
+	ldd	[%sp + 0x7ff + 184],%f14
+	ldd	[%sp + 0x7ff + 192],%f16
+	ldd	[%sp + 0x7ff + 200],%f18
+	ldd	[%sp + 0x7ff + 208],%f20
+	ldd	[%sp + 0x7ff + 216],%f22
+	ldd	[%sp + 0x7ff + 224],%f24
+	ldd	[%sp + 0x7ff + 232],%f26
+	ldd	[%sp + 0x7ff + 240],%f28
+	ldd	[%sp + 0x7ff + 248],%f30
+
 !
 !   calculate the target address from the vtable
 !
-	inc	%i1		    ! vTable is zero-based, index is 1 based (?)
-	ldx	[%i0],%l1	    ! *that --> vTable
-	sllx	%i1,4,%i1	    ! Each vtable entry is 16-bytes long
-	add	%i1,%l1,%l1
-	ldsh	[%l1],%l0	    ! this adjustor
-	add	%l0,%i0,%i0	    ! adjust this
-	ldx	[%l1 + 8],%l0	    ! target address
+.invoke:
+	sll	%i1,3,%l0	    ! index *= 8
+	ldx	[%i0],%l1	    ! *this --> address of vtable
+	ldx	[%l0 + %l1],%l0     ! this->vtable[index] --> address
 
-.L15:	ldd	[%sp + 0x7ff + 128 + 120],%f30
-.L14:	ldd	[%sp + 0x7ff + 128 + 112],%f28
-.L13:	ldd	[%sp + 0x7ff + 128 + 104],%f26
-.L12:	ldd	[%sp + 0x7ff + 128 + 96],%f24
-.L11:	ldd	[%sp + 0x7ff + 128 + 88],%f22
-.L10:	ldd	[%sp + 0x7ff + 128 + 80],%f20
-.L9:	ldd	[%sp + 0x7ff + 128 + 72],%f18
-.L8:	ldd	[%sp + 0x7ff + 128 + 64],%f16
-.L7:	ldd	[%sp + 0x7ff + 128 + 56],%f14
-.L6:	ldd	[%sp + 0x7ff + 128 + 48],%f12
-.L5:	ldx	[%sp + 0x7ff + 128 + 40],%o5
-	ldd	[%sp + 0x7ff + 128 + 40],%f10
-.L4:	ldx	[%sp + 0x7ff + 128 + 32],%o4
-	ldd	[%sp + 0x7ff + 128 + 32],%f8
-.L3:	ldx	[%sp + 0x7ff + 128 + 24],%o3
-	ldd	[%sp + 0x7ff + 128 + 24],%f6
-.L2:	ldx	[%sp + 0x7ff + 128 + 16],%o2
-	ldd	[%sp + 0x7ff + 128 + 16],%f4
-.L1:	ldx	[%sp + 0x7ff + 128 +  8],%o1
-	ldd	[%sp + 0x7ff + 128 +  8],%f2
-.L0:
-	jmpl	%l0,%o7		    ! call the routine
-! always have a 'this', from the incoming 'that'
-	 mov	%i0,%o0
-	
+	jmpl	%l0,%o7 	    ! call the routine
+	 mov	%i0,%o0 	    ! move 'this' pointer to out register
+
+	mov	%o0,%i0 	    ! propagate return value
 	ret
-	 restore	%o0,0,%o0   ! propogate return value
+	 restore
+
+        .size    XPTC_InvokeByIndex, .-XPTC_InvokeByIndex
