31c3cd247525d8f31b5dfb60e52bd3780ef67cf5
martti <martti>
2003-12-03 05:34:08 +0000

    Updated xfce4 to 4.0.1
      * Fixed panel crash with corrupted config file
      * Window management improvement
      * Improved handling of multiple selections in the file manager
      * French translations for the documentation
      * Translation updates
      * A new Gtk2 theme called "Xfce-winter"

--- sysutils/xfce4-systray/buildlink2.mk
+++ sysutils/xfce4-systray/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/09/27 06:57:16 martti Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/12/03 05:34:08 martti Exp $
 #
 # This Makefile fragment is included by packages that use xfce4-systray.
 #
@@ -9,7 +9,7 @@
 XFCE4_SYSTRAY_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			xfce4-systray
-BUILDLINK_DEPENDS.xfce4-systray?=		xfce4-systray>=4.0.0
+BUILDLINK_DEPENDS.xfce4-systray?=		xfce4-systray>=4.0.1
 BUILDLINK_PKGSRCDIR.xfce4-systray?=		../../sysutils/xfce4-systray
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.xfce4-systray=xfce4-systray
--- x11/xfce4-toys/buildlink2.mk
+++ x11/xfce4-toys/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/09/27 06:57:16 martti Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/12/03 05:34:09 martti Exp $
 #
 # This Makefile fragment is included by packages that use xfce4-toys.
 #
@@ -9,7 +9,7 @@
 XFCE4_TOYS_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			xfce4-toys
-BUILDLINK_DEPENDS.xfce4-toys?=		xfce4-toys>=4.0.0
+BUILDLINK_DEPENDS.xfce4-toys?=		xfce4-toys>=4.0.1
 BUILDLINK_PKGSRCDIR.xfce4-toys?=		../../x11/xfce4-toys
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.xfce4-toys=xfce4-toys
