682c104f718cb0c20f40f863cff76ae2ef409e24
xtraeme <xtraeme@pkgsrc.org>
2003-12-03 21:18:10 +0000

    s/wip/devel/

--- devel/ogre/buildlink2.mk
+++ devel/ogre/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/12/03 19:01:47 xtraeme Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/12/03 21:18:10 xtraeme Exp $
 #
 # This Makefile fragment is included by packages that use ogre.
 #
@@ -10,7 +10,7 @@ OGRE_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			ogre
 BUILDLINK_DEPENDS.ogre?=		ogre>=0.12.0
-BUILDLINK_PKGSRCDIR.ogre?=		../../wip/ogre
+BUILDLINK_PKGSRCDIR.ogre?=		../../devel/ogre
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.ogre=ogre
 BUILDLINK_PREFIX.ogre_DEFAULT=	${LOCALBASE}
@@ -20,10 +20,10 @@ BUILDLINK_FILES.ogre+=	lib/libOgreMain.*
 BUILDLINK_FILES.ogre+=	lib/libOgrePlatform.*
 BUILDLINK_FILES.ogre+=	lib/pkgconfig/OGRE.pc
 
+.include "../../devel/devIL/buildlink2.mk"
 .include "../../devel/SDL/buildlink2.mk"
 .include "../../devel/zlib/buildlink2.mk"
 .include "../../graphics/freetype2/buildlink2.mk"
-.include "../../wip/devIL/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	ogre-buildlink
 
