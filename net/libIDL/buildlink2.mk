fd8f0b166867e7a7949be3422b3da4e74fba992e
xtraeme <xtraeme@pkgsrc.org>
2003-12-04 17:07:48 +0000

    Require at least libIDL-0.8.2 to pick up the dependencies (missed in previous). Bump PKGREVISION.

--- net/libIDL/buildlink2.mk
+++ net/libIDL/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.6 2003/07/13 13:52:55 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.7 2003/12/04 17:07:48 xtraeme Exp $
 
 .if !defined(LIBIDL2_BUILDLINK2_MK)
 LIBIDL2_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		idl
-BUILDLINK_DEPENDS.idl?=		libIDL>=0.8.0nb2
+BUILDLINK_DEPENDS.idl?=		libIDL>=0.8.2
 BUILDLINK_PKGSRCDIR.idl?=	../../net/libIDL
 
 EVAL_PREFIX+=		BUILDLINK_PREFIX.idl=idl
@@ -12,9 +12,8 @@ BUILDLINK_PREFIX.idl_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.idl=	include/libIDL-2.0/libIDL/*
 BUILDLINK_FILES.idl+=	lib/libIDL-2.*
 
-.include "../../converters/libiconv/buildlink2.mk"
+.include "../../devel/gettext-lib/buildlink2.mk"
 .include "../../devel/glib2/buildlink2.mk"
-.include "../../devel/pkgconfig/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	idl-buildlink
 
