64b11434ce71aa79de87bb67d6bfc925b885cbc6
xtraeme <xtraeme@pkgsrc.org>
2003-12-04 19:37:52 +0000

    Updated to 1.1.0, provided by Min Sik Kim PR pkg/23630.
    Changes:
    
    	o imlib2-1.1.0 uses freetype2 instead of freetype1.
    	o More bugfixes, etc.

--- graphics/imlib2/buildlink2.mk
+++ graphics/imlib2/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.5 2003/12/03 01:32:08 reed Exp $
+# $NetBSD: buildlink2.mk,v 1.6 2003/12/04 19:37:52 xtraeme Exp $
 
 .if !defined(IMLIB2_BUILDLINK2_MK)
 IMLIB2_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		imlib2
-BUILDLINK_DEPENDS.imlib2?=	imlib2>=1.0.6nb2
+BUILDLINK_DEPENDS.imlib2?=	imlib2>=1.1.0
 BUILDLINK_PKGSRCDIR.imlib2?=	../../graphics/imlib2
 
 EVAL_PREFIX+=		BUILDLINK_PREFIX.imlib2=imlib2
@@ -12,16 +12,14 @@ BUILDLINK_PREFIX.imlib2_DEFAULT=	${X11PREFIX}
 BUILDLINK_FILES.imlib2=		bin/imlib2-config
 BUILDLINK_FILES.imlib2+=	include/Imlib2.h
 BUILDLINK_FILES.imlib2+=	lib/libImlib2.*
+BUILDLINK_FILES.imlib2+=	lib/loaders/filter/*
 BUILDLINK_FILES.imlib2+=	lib/loaders/image/*
 
-.include "../../devel/gettext-lib/buildlink2.mk"
-.include "../../graphics/freetype-lib/buildlink2.mk"
+.include "../../graphics/freetype2/buildlink2.mk"
 .include "../../graphics/jpeg/buildlink2.mk"
 .include "../../graphics/libungif/buildlink2.mk"
 .include "../../graphics/png/buildlink2.mk"
 .include "../../graphics/tiff/buildlink2.mk"
-.include "../../databases/edb/buildlink2.mk"
-.include "../../x11/gtk/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	imlib2-buildlink
 
