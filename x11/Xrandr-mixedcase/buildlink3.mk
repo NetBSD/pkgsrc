5c224110e58d46445dbcda19612c96347591b228
recht <recht@pkgsrc.org>
2003-12-04 14:40:39 +0000

    initial import of libXrandr-1.0.1
    This package contains the X RandR extension.

---
+++ x11/Xrandr-mixedcase/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2003/12/04 14:40:40 recht Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+XRANDR_BUILDLINK3_MK:=	${XRANDR_BUILDLINK3_MK}+
+
+.if !empty(XRANDR_BUILDLINK3_MK:M\+)
+.  include "../../mk/bsd.prefs.mk"
+
+BUILDLINK_DEPENDS.Xrandr?=	Xrandr>=1.0.1
+BUILDLINK_PKGSRCDIR.Xrandr?=	../../x11/Xrandr
+BUILDLINK_PACKAGES+=		Xrandr
+.endif	# XRANDR_BUILDLINK3_MK
+
+.if !defined(BUILDLINK_IS_BUILTIN.Xrender)
+BUILDLINK_IS_BUILTIN.Xrender=	NO
+.endif
+
+.if !empty(BUILDLINK_DEPTH:M\+)
+BUILDLINK_DEPENDS+=		Xrandr
+.endif
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:C/\+$//}
