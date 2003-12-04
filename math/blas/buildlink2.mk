fa03ed376be20ccb9bd8837f70dcdad6519dab0d
adam <adam@pkgsrc.org>
2003-12-04 11:26:42 +0000

    buildlink fix

--- math/blas/buildlink2.mk
+++ math/blas/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.5 2003/12/04 09:13:58 adam Exp $
+# $NetBSD: buildlink2.mk,v 1.6 2003/12/04 11:27:16 adam Exp $
 
 .if !defined(BLAS_BUILDLINK2_MK)
 BLAS_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		blas
-BUILDLINK_DEPENDS.blas?=	blas>=1.0
+BUILDLINK_DEPENDS.blas?=	blas>=1.0nb2
 BUILDLINK_PKGSRCDIR.blas?=	../../math/blas
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.blas=blas
--- math/lapack/buildlink2.mk
+++ math/lapack/buildlink2.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/06/12 19:08:34 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/12/04 11:26:42 adam Exp $
 
 .if !defined(LAPACK_BUILDLINK2_MK)
 LAPACK_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		lapack
-BUILDLINK_DEPENDS.lapack?=	lapack>=20010201
+BUILDLINK_DEPENDS.lapack?=	lapack>=20010201nb1
 BUILDLINK_PKGSRCDIR.lapack?=	../../math/lapack
-BUILDLINK_DEPMETHOD.lapack?=	build
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.lapack=lapack
 BUILDLINK_PREFIX.lapack_DEFAULT=	${LOCALBASE}
