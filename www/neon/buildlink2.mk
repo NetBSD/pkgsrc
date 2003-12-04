9cb63535538ccf0144fa70d3990251db6f3a3eb3
recht <recht@pkgsrc.org>
2003-12-04 17:00:58 +0000

    update to 0.24.4
    * Ignore unclean SSL closure when response body is delimited by EOF
      ("Could not read response body: Secure connection truncated" errors
      with some buggy SSL servers).
    * Fix test/ssl.c syntax errors with C89 compilers (Radu Greab).
    * Respect configure's --datadir argument (Max Bowsher).
    * Fix build on Windows when OpenSSL is not used.
    * Fix use of SSLv2 (spurious "Server did not present certificate" error).
    * When using SSL via a proxy, prevent leaking server auth credentials to the
      proxy, or proxy auth credentials to the server.
    * Fix name resolver with some old versions of glibc.
    * Fix problems with configure's "time_t format string" detection.
    * Fix problems when a broken Kerberos installation is found.
    * When verifying SSL certificates, check iPaddress names in the subjectAltName
      extension.
    
    Update BUILDLINK_DEPENDS to 0.24.4 since there was an XML API change in
    0.24.0.

--- www/neon/buildlink2.mk
+++ www/neon/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.7 2003/05/25 09:34:30 epg Exp $
+# $NetBSD: buildlink2.mk,v 1.8 2003/12/04 17:00:58 recht Exp $
 
 .if !defined(NEON_BUILDLINK2_MK)
 NEON_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		neon
-BUILDLINK_DEPENDS.neon?=	neon>=0.23.9
+BUILDLINK_DEPENDS.neon?=	neon>=0.24.4
 BUILDLINK_PKGSRCDIR.neon?=	../../www/neon
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.neon=neon
