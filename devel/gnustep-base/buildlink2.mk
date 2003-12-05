71e8816a8a53b911fb0efffcb216bbecd9dc6328
adam <adam@pkgsrc.org>
2003-12-05 09:55:46 +0000

    Noteworthy changes in version `1.8.0':    Read the NEWS file for a compete list of changes since the last stable version. The major change is that the header files (both in the package and when installed) are in different locations. Also, due to filesystem changes in gnustep-make, several components of gnustep-base are located in different locations. Generally this should not affect the compilation or running of applications and tools.

--- devel/gnustep-base/buildlink2.mk
+++ devel/gnustep-base/buildlink2.mk
@@ -1,27 +1,28 @@
-# $NetBSD: buildlink2.mk,v 1.4 2003/07/13 13:51:12 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.5 2003/12/05 09:55:46 adam Exp $
 #
 
 .if !defined(GNUSTEP_BASE_BUILDLINK2_MK)
 GNUSTEP_BASE_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gnustep-base
-BUILDLINK_DEPENDS.gnustep-base?=	gnustep-base>=1.6.0nb2
+BUILDLINK_DEPENDS.gnustep-base?=	gnustep-base>=1.8.0
 BUILDLINK_PKGSRCDIR.gnustep-base?=	../../devel/gnustep-base
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gnustep-base=gnustep-base
 BUILDLINK_PREFIX.gnustep-base_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.gnustep-base=	share/GNUstep/System/Headers/Foundation/*
-BUILDLINK_FILES.gnustep-base+=	share/GNUstep/System/Headers/gnustep/base/*
-BUILDLINK_FILES.gnustep-base+=	share/GNUstep/System/Headers/gnustep/unicode/*
-BUILDLINK_FILES.gnustep-base+=	share/GNUstep/System/Headers/${GNUSTEP_HOST_CPU}/${LOWER_OPSYS}${APPEND_ELF}/*
-BUILDLINK_FILES.gnustep-base+=	share/GNUstep/System/Libraries/${GNUSTEP_HOST_CPU}/${LOWER_OPSYS}${APPEND_ELF}/gnu-gnu-gnu/libgnustep-base.*
+BUILDLINK_FILES.gnustep-base+=	share/GNUstep/System/Library/Headers/*
+BUILDLINK_FILES.gnustep-base+=	share/GNUstep/System/Library/Headers/Foundation/*
+BUILDLINK_FILES.gnustep-base+=	share/GNUstep/System/Library/Headers/GNUstepBase/*
+BUILDLINK_FILES.gnustep-base+=	share/GNUstep/System/Library/Headers/gnustep/base/*
+BUILDLINK_FILES.gnustep-base+=	share/GNUstep/System/Library/Headers/gnustep/unicode/*
+BUILDLINK_FILES.gnustep-base+=	share/GNUstep/System/Library/Libraries/libgnustep-base.*
 
-.include "../../devel/gmp/buildlink2.mk"
-.include "../../devel/ffcall/buildlink2.mk"
-.include "../../devel/gnustep-objc/buildlink2.mk"
 .include "../../converters/libiconv/buildlink2.mk"
-.include "../../textproc/libxml2/buildlink2.mk"
+.include "../../devel/ffcall/buildlink2.mk"
+.include "../../devel/gmp/buildlink2.mk"
+.include "../../devel/gnustep-objc/buildlink2.mk"
 .include "../../security/openssl/buildlink2.mk"
+.include "../../textproc/libxml2/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	gnustep-base-buildlink
 
