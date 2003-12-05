cc51b9716e83101e0a6b522ea89562b09813a536
adam <adam@pkgsrc.org>
2003-12-05 10:15:23 +0000

    Noteworthy changes in version `0.9.0':    Some more improvements to copy/paste support following ICCCM specifications (like COMPOUND_TEXT support).

--- x11/gnustep-back/buildlink2.mk
+++ x11/gnustep-back/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.5 2003/07/13 13:54:00 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.6 2003/12/05 10:15:23 adam Exp $
 #
 
 .if !defined(GNUSTEP_BACK_BUILDLINK2_MK)
 GNUSTEP_BACK_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gnustep-back
-BUILDLINK_DEPENDS.gnustep-back?=	gnustep-back>=0.8.5nb2
+BUILDLINK_DEPENDS.gnustep-back?=	gnustep-back>=0.9.0
 BUILDLINK_PKGSRCDIR.gnustep-back?=	../../x11/gnustep-back
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gnustep-back=gnustep-back
