d501428092b13e781ed95efe8155ff9af69ae003
adam <adam@pkgsrc.org>
2003-12-05 09:22:26 +0000

    Changes 1.3.0:     Improvements and bug-fixes

--- devel/gnustep-objc/buildlink2.mk
+++ devel/gnustep-objc/buildlink2.mk
@@ -1,19 +1,20 @@
-# $NetBSD: buildlink2.mk,v 1.4 2003/05/02 11:54:29 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.5 2003/12/05 09:22:26 adam Exp $
 #
 
 .if !defined(GNUSTEP_OBJC_BUILDLINK2_MK)
 GNUSTEP_OBJC_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gnustep-objc
-BUILDLINK_DEPENDS.gnustep-objc?=		gnustep-objc>=1.2.3nb1
-BUILDLINK_PKGSRCDIR.gnustep-objc?=		../../devel/gnustep-objc
+BUILDLINK_DEPENDS.gnustep-objc?=	gnustep-objc>=1.3.0
+BUILDLINK_PKGSRCDIR.gnustep-objc?=	../../devel/gnustep-objc
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gnustep-objc=gnustep-objc
 BUILDLINK_PREFIX.gnustep-objc_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.gnustep-objc=	share/GNUstep/System/Headers/objc/*
-BUILDLINK_FILES.gnustep-objc+=	share/GNUstep/System/Libraries/${GNUSTEP_HOST_CPU}/${LOWER_OPSYS}${APPEND_ELF}/*
+BUILDLINK_FILES.gnustep-objc=	share/GNUstep/System/Library/Headers/objc/*
+BUILDLINK_FILES.gnustep-objc+=	share/GNUstep/System/Library/Libraries/*
 
-.include "../../devel/gnustep-make/buildlink2.mk"
+#.include "../../devel/gnustep-make/buildlink2.mk"
+.include "../gnustep-make/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	gnustep-objc-buildlink
 
