22262abeb23b6af54ecd08e8e1335cb21c97d937
xtraeme <xtraeme@pkgsrc.org>
2003-12-06 00:52:21 +0000

    Updated to 1.0.0, provided by Min Sik Kim PR pkg/23661.
    Changes:
    
    - Exported the static SRP group parameters.
    - Some fixes in the certificate authenticated SRP ciphersuites.
    - Improved the support for draft-ietf-tls-srp-05. The two-phase
      handshake is now fully supported without any interaction with
      the application layer (except for a callback).
    - Some fixes in the openpgp authentication.
    - Removed the Twofish cipher.
    - The openssl compatibility layer was moved to gnutls-openssl
      library instead of being included in the gnutls-extra library.
    - Added the RIPEMD ciphersuites defined in draft-ietf-tls-openpgp-keys-04.
    - Building with openpgp support is now mandatory.
    - gnutls4 compatibility header is no longer included by default in
      gnutls.h.
    - gnutls8 function usage yelds a deprecation warning in gcc3.
    - gnutls_x509_*_set_dn_by_oid() and gnutls_x509_*_get_*_dn_by_oid()
      functions have a raw_flag parameter added.
    - The certtool utility can now generate PKCS #12 structures
      without specifying a certificate.
    - Added capability to read CRLs to certtool.
    - Corrected some functions which return GNUTLS_E_SHORT_MEMORY_BUFFER
      to properly set the required buffer size.
    - Corrected a bug in libgcrypt detection.
    
    And more...

--- security/gnutls/buildlink2.mk
+++ security/gnutls/buildlink2.mk
@@ -1,26 +1,30 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/10/18 08:10:57 jmmv Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/12/06 00:52:21 xtraeme Exp $
 #
 # This Makefile fragment is included by packages that use gnutls.
 #
-# This file was created automatically using createbuildlink 2.6.
+# This file was created automatically using createbuildlink 2.8.
 #
 
 .if !defined(GNUTLS_BUILDLINK2_MK)
 GNUTLS_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gnutls
-BUILDLINK_DEPENDS.gnutls?=		gnutls>=0.8.7nb1
+BUILDLINK_DEPENDS.gnutls?=		gnutls>=1.0.0
 BUILDLINK_PKGSRCDIR.gnutls?=		../../security/gnutls
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gnutls=gnutls
 BUILDLINK_PREFIX.gnutls_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.gnutls+=	include/gnutls/*.h
 BUILDLINK_FILES.gnutls+=	lib/libgnutls-extra.*
+BUILDLINK_FILES.gnutls+=	lib/libgnutls-openssl.*
 BUILDLINK_FILES.gnutls+=	lib/libgnutls.*
 
 .include "../../archivers/liblzo/buildlink2.mk"
 .include "../../devel/zlib/buildlink2.mk"
+.include "../../security/libgcrypt/buildlink2.mk"
+.include "../../security/libtasn1/buildlink2.mk"
 .include "../../security/opencdk/buildlink2.mk"
+.include "../../security/openssl/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	gnutls-buildlink
 
