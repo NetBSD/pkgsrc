03db1d5c1456ee45f3d87b4daeee64413086c171
xtraeme <xtraeme@pkgsrc.org>
2003-12-06 00:26:59 +0000

    Updated to 1.1.90, provided by Min Sik Kim PR pkg/23322.
    Changes:
    
     * The use of the GCRY_WEAK_RANDOM level is now deprecated in favor of
       the new gcry_create_nonce function.
     * gcry_sexp_build now supports a "%b" format to include a memory buffer.
     * Minor configuration fixes.
     * Interface changes relative to the 1.1.44 release:
     * Bug fixes and more code cleanups.
     * Enhanced the prime API.
     * Support for the Serpent cipher algorithm.
    
    And more...

--- security/libgcrypt/buildlink2.mk
+++ security/libgcrypt/buildlink2.mk
@@ -1,17 +1,24 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/11/02 19:57:51 hubertf Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/12/06 00:26:59 xtraeme Exp $
+#
+# This Makefile fragment is included by packages that use libgcrypt.
+#
+# This file was created automatically using createbuildlink 2.8.
 #
 
 .if !defined(LIBGCRYPT_BUILDLINK2_MK)
 LIBGCRYPT_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			libgcrypt
-BUILDLINK_DEPENDS.libgcrypt?=		libgcrypt>=1.1.12nb1
+BUILDLINK_DEPENDS.libgcrypt?=		libgcrypt>=1.1.90
 BUILDLINK_PKGSRCDIR.libgcrypt?=		../../security/libgcrypt
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libgcrypt=libgcrypt
 BUILDLINK_PREFIX.libgcrypt_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.libgcrypt+=	include/gcrypt-module.h
 BUILDLINK_FILES.libgcrypt+=	include/gcrypt.h
-BUILDLINK_FILES.libgcrypt+=	lib/libgcrypt.*
+BUILDLINK_FILES.libgcrypt+=	lib/libgcrypt*
+
+.include "../../security/libgpg-error/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	libgcrypt-buildlink
 
