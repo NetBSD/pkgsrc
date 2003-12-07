ba7f811e8fd65372ae65a37345d82e61eab01a46
epg <epg@pkgsrc.org>
2003-12-07 00:51:07 +0000

    Update to 4.2.52.
    Clean up Makefile.
    
    Set MAINTAINER to tech-pkg@NetBSD.org (previous maintainer, Alex
    Newman, says he no longer maintains this).
    
    Changes are too many to list.

--- databases/db4/buildlink2.mk
+++ databases/db4/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/06/10 09:54:41 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/12/07 00:51:07 epg Exp $
 #
 
 .if !defined(DB4_BUILDLINK2_MK)
 DB4_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		db4
-BUILDLINK_DEPENDS.db4?=		db4>=4.1.25
+BUILDLINK_DEPENDS.db4?=		db4>=4.2.52
 BUILDLINK_PKGSRCDIR.db4?=	../../databases/db4
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.db4=db4
