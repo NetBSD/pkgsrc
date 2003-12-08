3cfbe5b778eb138d025ec6bb4d5c4f657b3d12ef
drochner <drochner@pkgsrc.org>
2003-12-08 18:33:52 +0000

    add tasking support, developed by John R. Shannon, submitted by PR pkg/23627 This requires native pthreads, so it is for -current only.

---
+++ lang/gcc3-ada/files/gcc3.mk
@@ -0,0 +1,8 @@
+# $NetBSD: gcc3.mk,v 1.1 2003/12/08 18:33:53 drochner Exp $
+#
+# make configuration file for @PKGNAME@
+
+CC=		@GCC_PREFIX@/bin/cc
+CPP=		@GCC_PREFIX@/bin/cpp
+CXX=		@GCC_PREFIX@/bin/c++
+ADAC=		@GCC_PREFIX@/bin/gcc
