80b898d704b309dbe6ebf3017ebe69b25568d15d
recht <recht@pkgsrc.org>
2003-12-08 21:13:56 +0000

    Update to Python 2.3.2
    Changes in Python:
    Quite a few fixes.. See NEWS for details.
    
    Changes in the pkg:
    - add FreeBSD patches from the FreeBSD port
    - add fix for a fatal bug in type's GC handling causes segfaults
      (via FreeBSD port)
      see http://cvs.sourceforge.net/viewcvs.py/python/python/dist/src/Misc/NEWS?r1=1.831.4.75&r2=1.831.4.76&diff_format=u
    - always build the db 1.85 module (on all platforms)

--- lang/python/srcdist.mk
+++ lang/python/srcdist.mk
@@ -1,16 +1,16 @@
-# $NetBSD: srcdist.mk,v 1.8 2003/08/04 08:15:47 drochner Exp $
+# $NetBSD: srcdist.mk,v 1.9 2003/12/08 21:13:56 recht Exp $
 
 .include "../../lang/python/pyversion.mk"
 
 .if ${_PYTHON_VERSION} == "23" || ${_PYTHON_VERSION} == "23pth"
 
-DISTNAME=	Python-2.3
+DISTNAME=	Python-2.3.2
 EXTRACT_SUFX=	.tgz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python23/distinfo
 PATCHDIR=	${.CURDIR}/../../lang/python23/patches
-PYSUBDIR=	Python-2.3
+PYSUBDIR=	Python-2.3.2
 WRKSRC=		${WRKDIR}/${PYSUBDIR}
-MASTER_SITES=	ftp://ftp.python.org/pub/python/2.3/
+MASTER_SITES=	ftp://ftp.python.org/pub/python/2.3.2/
 
 .elif ${_PYTHON_VERSION} == "22" || ${_PYTHON_VERSION} == "22pth"
 
