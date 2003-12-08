487699e0ba7bac7622afb151a67e923fd588806c
wiz <wiz@pkgsrc.org>
2003-12-08 14:42:44 +0000

    Bump PKGREVISION for libogg update, and fix bl2 file while here.

--- audio/flac/buildlink2.mk
+++ audio/flac/buildlink2.mk
@@ -1,18 +1,24 @@
-# $NetBSD: buildlink2.mk,v 1.6 2003/02/14 23:25:33 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.7 2003/12/08 14:42:44 wiz Exp $
 
 .if !defined(FLAC_BUILDLINK2_MK)
 FLAC_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=		flac
-BUILDLINK_DEPENDS.flac?=	flac>=1.1.0
-BUILDLINK_PKGSRCDIR.flac?=	../../audio/flac
+BUILDLINK_PACKAGES+=			flac
+BUILDLINK_DEPENDS.flac?=		flac>=1.1.0nb1
+BUILDLINK_PKGSRCDIR.flac?=		../../audio/flac
 
-EVAL_PREFIX+=			BUILDLINK_PREFIX.flac=flac
+EVAL_PREFIX+=	BUILDLINK_PREFIX.flac=flac
 BUILDLINK_PREFIX.flac_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.flac=		include/FLAC/*
-BUILDLINK_FILES.flac+=		include/FLAC++/*
-BUILDLINK_FILES.flac+=		lib/libFLAC.*
-BUILDLINK_FILES.flac+=		lib/libFLAC++.*
+BUILDLINK_FILES.flac+=	include/FLAC++/*.h
+BUILDLINK_FILES.flac+=	include/FLAC/*.h
+BUILDLINK_FILES.flac+=	include/OggFLAC++/*.h
+BUILDLINK_FILES.flac+=	include/OggFLAC/*.h
+BUILDLINK_FILES.flac+=	lib/libFLAC++.*
+BUILDLINK_FILES.flac+=	lib/libFLAC.*
+BUILDLINK_FILES.flac+=	lib/libOggFLAC++.*
+BUILDLINK_FILES.flac+=	lib/libOggFLAC.*
+
+.include "../../audio/libogg/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	flac-buildlink
 
