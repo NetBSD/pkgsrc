e6d2a377cc1705e972fbae32db159cd73d720c9e
agc <agc>
2003-12-12 11:44:05 +0000

    Pullup a typo fix to the pkgsrc-2003Q4 branch, requested by Grant Beattie.
    	Module Name:    pkgsrc
    	Committed By:   grant
    	Date:           Thu Dec 11 13:51:28 UTC 2003
    
    	Modified Files:
    		pkgsrc/mk: apache.mk
    
    	Log Message:
    	add missing }.
    
    	from Aidan Kehoe in PR pkg/23676.

--- mk/apache.mk
+++ mk/apache.mk
@@ -1,4 +1,4 @@
-# $NetBSD: apache.mk,v 1.3 2003/11/21 07:04:43 grant Exp $
+# $NetBSD: apache.mk,v 1.3.2.1 2003/12/12 11:44:05 agc Exp $
 #
 # This Makefile fragment handles Apache dependencies and make variables,
 # and is meant to be included by packages that require Apache either at
@@ -124,7 +124,7 @@ _APACHE_PKGSRCDIR=	../../www/apache6
 
 _APACHE_BL_SRCDIR?=	../../www/apache
 
-_APACHE_DEPENDENCY?=	${BUILDLINK_DEPENDS.${_PKG_APACHE}:${_APACHE_PKGSRCDIR}
+_APACHE_DEPENDENCY?=	${BUILDLINK_DEPENDS.${_PKG_APACHE}}:${_APACHE_PKGSRCDIR}
 
 # Add a runtime dependency on the apache server.
 # This may or may not create an actual dependency depending on
