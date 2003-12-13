8a18012636a47ea4ba589e5193df673880956fc0
jmmv <jmmv@pkgsrc.org>
2003-12-13 19:22:12 +0000

    Update to 4.14.  While here, change xscreensaver to not install the xscreensaver-demo program (which requires GTK).  This is done to allow xscreensaver coexist with xscreensaver-gnome, and to avoid a dependancy on GTK when installing kdeartwork3.
    Changes since 4.13:
    * New hacks, `fontglide', `apple2', `xanalogtv', `pong',
      `gleidescope', `mirrorblob', and `blinkbox'.
    * New version of `glsnake' (with many more models.)
    * Another Windows crash in `bsod'; also HVX/GCOS6/TPS6.
    * New version of `endgame'.
    * Screen grabbing works on MacOS X.
    * Various minor fixes.
    
    Ok'ed by wiz@.

--- x11/xscreensaver/buildlink2.mk
+++ x11/xscreensaver/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/12/13 00:45:27 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/12/13 19:22:13 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use xscreensaver.
 #
@@ -9,16 +9,14 @@
 XSCREENSAVER_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			xscreensaver
-BUILDLINK_DEPENDS.xscreensaver?=		xscreensaver{,-gnome}>=4.13nb2
+BUILDLINK_DEPENDS.xscreensaver?=		xscreensaver>=4.14
 BUILDLINK_PKGSRCDIR.xscreensaver?=		../../x11/xscreensaver
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.xscreensaver=xscreensaver
 BUILDLINK_PREFIX.xscreensaver_DEFAULT=	${X11PREFIX}
 BUILDLINK_FILES.xscreensaver+=	lib/X11/app-defaults/XScreenSaver
 
-.include "../../devel/libglade2/buildlink2.mk"
 .include "../../textproc/libxml2/buildlink2.mk"
-.include "../../x11/gtk2/buildlink2.mk"
 .include "../../graphics/gle/buildlink2.mk"
 .include "../../graphics/jpeg/buildlink2.mk"
 
