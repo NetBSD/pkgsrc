737db4ad3a3f7f73f1d2b27ea12bb2118b4537dc
jmmv <jmmv@pkgsrc.org>
2003-12-14 19:49:24 +0000

    Update to 2.4.1, based on work done by xtraeme@ and Min Sik Kim. Change log lists changes for the development branch (2.3); since then, there have been several bugfixes.
    libbonoboui 2.3.6
    
    	* bugs fixed
    		+ F1 help menu shortcut (Yuedong Du)
    		+ vicious K/B focus fixage (Federico)
    		+ dock grip fix (Padraig O'Briain)
    		+ doc. fix (Michael)
    
    libbonoboui 2.3.3
    
    	* bugs fixed
    		+ multi-head issues (Morten Welinder, Michael)
    		+ nasty focus hacks binned (Michael)
    		+ UI preferences fixes (Anders Carlsson)
    		+ toolbar layout fix (Gustavo Giraldez)
    		+ UI re-enterancy fix (Radek Doulik)
    		+ Tooltip fixes (Dave Camp)
    		+ build fixage (Hidetoshi Tajima, Masahiro Sakai,
    		  Malcolm Tredinnick, Padraig O'Briain, Alex Duggan)
    	* featurelets
    		+ new sample container (Sergey Oudaltsov)
    		+ nicer toolbar rendering (Rodney Dawes)
    		+ cygwin port (Masahiro Sakai)
    		+ toolbar widget direction (Jody Goldberg)
    		+ working canvas components (Bob Gibbs)
    		+ more documentation (Gustavo Carneiro)
    	* translations
    		+ kn, id, mn, yi (Christian Rose)
    		+ ro (Marius Andreiana)
    		+ fa (Roozbeh Pournader)
    		+ ml (Guntupalli Karunakar)
    		+ sr (Danilo Šegan)
    		+ cy (Telsa Gwynne et. al)

--- devel/libbonoboui/buildlink2.mk
+++ devel/libbonoboui/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.9 2003/12/13 00:45:22 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.10 2003/12/14 19:49:24 jmmv Exp $
 
 .if !defined(LIBBONOBOUI_BUILDLINK2_MK)
 LIBBONOBOUI_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libbonoboui
-BUILDLINK_DEPENDS.libbonoboui?=	libbonoboui>=2.2.4nb3
+BUILDLINK_DEPENDS.libbonoboui?=	libbonoboui>=2.4.1
 BUILDLINK_PKGSRCDIR.libbonoboui?=	../../devel/libbonoboui
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libbonoboui=libbonoboui
@@ -13,9 +13,7 @@ BUILDLINK_FILES.libbonoboui+=	include/libbonoboui-2.0/bonobo/*
 BUILDLINK_FILES.libbonoboui+=	include/libbonoboui-2.0/*
 BUILDLINK_FILES.libbonoboui+=	lib/libbonoboui-2.*
 BUILDLINK_FILES.libbonoboui+=	lib/libglade/2.0/libbonobo.*
-BUILDLINK_FILES.libbonoboui+=	lib/pkgconfig/libbonoboui-2.0.pc
 
-.include "../../devel/pkgconfig/buildlink2.mk"
 .include "../../devel/GConf2/buildlink2.mk"
 .include "../../devel/libbonobo/buildlink2.mk"
 .include "../../devel/libglade2/buildlink2.mk"
