2cb2ed4b30285d0d6e875a52636fa6d5d1d3e64d
jmmv <jmmv>
2003-12-14 19:41:40 +0000

    Update to 1.1: 	* much improved threading (Michael) 	* much improved docs (Gustavo Carneiro) 	* cygwin support (Masahiro Sakai) 	* Unix socket length fix (Masahiro) 	* OSF/1 True64 support (mmokrejs)
    While here, enable OpenSSL support.

--- net/linc/buildlink2.mk
+++ net/linc/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.8 2003/12/09 06:54:29 xtraeme Exp $
+# $NetBSD: buildlink2.mk,v 1.9 2003/12/14 19:41:40 jmmv Exp $
 
 .if !defined(LINC_BUILDLINK2_MK)
 LINC_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		linc
-BUILDLINK_DEPENDS.linc?=	linc>=1.0.1nb2
+BUILDLINK_DEPENDS.linc?=	linc>=1.1.1
 BUILDLINK_PKGSRCDIR.linc?=	../../net/linc
 
 EVAL_PREFIX+=		BUILDLINK_PREFIX.linc=linc
@@ -13,6 +13,7 @@ BUILDLINK_FILES.linc=	include/linc-1.0/linc/*
 BUILDLINK_FILES.linc+=	lib/liblinc.*
 
 .include "../../devel/glib2/buildlink2.mk"
+.include "../../security/openssl/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	linc-buildlink
 
