6d4299862d329a8c13b20ec7254dfa234af0ea93
jmmv <jmmv@pkgsrc.org>
2003-12-14 19:44:38 +0000

    Update to 2.4.0.1, from PR pkg/23697 by Min Sik Kim.
    2.4.0
     - print usage message when no options passed to gconftool
     - API docs updates
     - fix UTF-8 handling for command line output
     - fix reliability of writing out XML
     - ensure recursive unsets get synced
     - fix flag value that was "0"
     - fix recursive preloading
     - fix a memleak
     - fix some gconftool messages/segfaults
    
    2.3.3
     - new gconf_client_notify() API to fake a notification
     - fixes to gconftool --dump feature
     - implement schema uninstallation in gconftool so packages
       can avoid leaving junk
     - fix build issues with linc-free ORBit
     - new gconf_client_recursive_unset API
     - fix gcc 3.3 strict aliasing warnings
    
    2.3.2
     - fix build on Cygwin
     - don't complain about missing saved_state in syslog
     - improve gconf-sanity-check error text
     - added --get-type, --get-list-size and --get-list-element
     - exported gconf_entry_copy
     - cleaned up headers for C++
     - added missing GCONF_CLIENT_GET_CLASS
     - hackaround fix for 2.3.1 lockfile breakage

--- devel/GConf2/buildlink2.mk
+++ devel/GConf2/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.18 2003/11/12 01:31:47 salo Exp $
+# $NetBSD: buildlink2.mk,v 1.19 2003/12/14 19:44:38 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use GConf2.
 #
@@ -9,20 +9,12 @@
 GCONF2_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			GConf2
-BUILDLINK_DEPENDS.GConf2?=		GConf2>=2.2.0nb3
+BUILDLINK_DEPENDS.GConf2?=		GConf2>=2.4.0.1
 BUILDLINK_PKGSRCDIR.GConf2?=		../../devel/GConf2
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.GConf2=GConf2
 BUILDLINK_PREFIX.GConf2_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.GConf2+=	include/gconf/2/gconf/gconf-changeset.h
-BUILDLINK_FILES.GConf2+=	include/gconf/2/gconf/gconf-client.h
-BUILDLINK_FILES.GConf2+=	include/gconf/2/gconf/gconf-engine.h
-BUILDLINK_FILES.GConf2+=	include/gconf/2/gconf/gconf-enum-types.h
-BUILDLINK_FILES.GConf2+=	include/gconf/2/gconf/gconf-error.h
-BUILDLINK_FILES.GConf2+=	include/gconf/2/gconf/gconf-listeners.h
-BUILDLINK_FILES.GConf2+=	include/gconf/2/gconf/gconf-schema.h
-BUILDLINK_FILES.GConf2+=	include/gconf/2/gconf/gconf-value.h
-BUILDLINK_FILES.GConf2+=	include/gconf/2/gconf/gconf.h
+BUILDLINK_FILES.GConf2+=	include/gconf/2/gconf/*
 BUILDLINK_FILES.GConf2+=	lib/GConf/2/libgconfbackend-xml.*
 BUILDLINK_FILES.GConf2+=	lib/libgconf-2.*
 
@@ -30,7 +22,6 @@ BUILDLINK_FILES.GConf2+=	lib/libgconf-2.*
 .include "../../devel/glib2/buildlink2.mk"
 .include "../../devel/popt/buildlink2.mk"
 .include "../../net/ORBit2/buildlink2.mk"
-.include "../../net/linc/buildlink2.mk"
 .include "../../textproc/libxml2/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	GConf2-buildlink
