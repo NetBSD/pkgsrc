19eb9f9ecc5756f39e5dd18aa6aec97f2e11884d
wiz <wiz@pkgsrc.org>
2003-12-14 17:55:23 +0000

    Fix DEPENDS and PATH so that only the 0.3.x version is accepted.

--- security/gpgme03/buildlink2.mk
+++ security/gpgme03/buildlink2.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/12/14 17:51:04 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/12/14 17:55:23 wiz Exp $
 #
 
 .if !defined(GPGME_BUILDLINK2_MK)
 GPGME_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		gpgme
-BUILDLINK_DEPENDS.gpgme?=	gpgme>=0.3.15nb1
-BUILDLINK_PKGSRCDIR.gpgme?=	../../security/gpgme
+BUILDLINK_DEPENDS.gpgme?=	gpgme-0.3.[0-9]*
+BUILDLINK_PKGSRCDIR.gpgme?=	../../security/gpgme03
 
 EVAL_PREFIX+=		BUILDLINK_PREFIX.gpgme=gpgme
 BUILDLINK_PREFIX.gpgme_DEFAULT=	${LOCALBASE}
