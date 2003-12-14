78980d44698cdf116b03d2d61e723a082852723b
jmmv <jmmv>
2003-12-14 19:43:50 +0000

    Update to 2.8.0, based on work done by xtraeme@:
    ORBit2-2.8.0
    	* bug fixes
    		+ sequence_concat helper (Frank)
    		+ corbaloc fixes (Frank)
    
    ORBit2-2.7.6
    	* bug fixes
    		+ try_connection deadlock (Michael, Padraig O'Briain)
    		+ shutdown_orb fd leak (Michael, Morten Welinder)
    		+ don't export libIDL strangely (Michael, Morten)
    	* featurelets
    		+ define file specific foo in idlcompile (James H)
    
    
    ORBit2-2.7.5
    	* bug fixes
    		+ cmdline option parsing (Michael)
    		+ idl-compiler stderr fixes (Dan Winship)
    		+ object key hashing fix (Frank Rehberger)
    		+ ~always enable thread-safety (Michael)
    		+ parallel build fixes (Michael)
    		+ _is_a fixes for in-proc + transient poa (Michael)
    	* featurelets
    		+ RootPOA/POACurrent set guards (Frank)
    		+ corbaloc support (Frank)
    		+ more regression tests (Michael, Frank)
    
    ORBit2-2.7.3
    	* bug fixes
    		+ vicious union discriminator issue (Brian Cameron)
    		+ non_existent / try_connection fix (Michael)
    		+ 'broken' emission at idle (Michael, George Lebl)
    		+ DynAnyFactory registration (Michael)
    		+ no longer explicitely "entering a wold of pain"
    		+ ORBit_sequence fixes (Frank Rehberger)
    		+ linc source fix (Padraig O'Briain)
    		+ gthread linkage fix (Michael)
    		+ struct/union leakage on exceptions (Frank)
    	* featurelets
    		+ improved regression tests (Michael, Frank, Bowie Owens)
    		+ lots of documentation improvements (Gustavo Carneiro)
    		+ -ORBInitRef, -ORBNamingIOR (Frank)
    
    ORBit2-2.7.2
    	* bug fixes
    		+ fixed docs build (Gustavo Varneiro)
    		+ thread pool fix (Alex)
    		+ build fixes (Waschk, oGalaxy, Michael)
    	* features (Michael)
    		+ vastly improved thread safety (& Alex)
    		+ improved regression tests
    		+ much improved ABI
    		+ implemented POA thread policies
    		+ client side re-enterancy guard policies
    		+ internalized linc as 'linc2/link'
    		+ in-proc cross-thread invocation
    		+ poa copying API
    
    ORBit2-2.7.1
    	* bug fixes
    		+ tail align fix (Herbert Valerio Riedel)
    		+ request ID fix (Herbert)
    		+ thread hint fix (Gustavo Carneiro)
    		+ build fixes (Richard Kinder)
    		+ packed type marshal fix (Michael Haubenwallner)
    		+ purifing mode fixes (Michael)
    		+ use g_get_tmp_dir (Michael)
    	* features
    		+ much improved thread safey (Michael)
    		+ improved tests (Michael)
    		+ locate request impl. (Herbert)
    		+ GIOP cleans (Herbert)
    		+ configurable init msg limit (Michael)
    		+ cygwin support (Masahiro Sakai)
    		+ code cleans (Richard Kinder)

--- net/ORBit2/buildlink2.mk
+++ net/ORBit2/buildlink2.mk
@@ -1,31 +1,38 @@
-# $NetBSD: buildlink2.mk,v 1.10 2003/11/12 01:31:50 salo Exp $
+# $NetBSD: buildlink2.mk,v 1.11 2003/12/14 19:43:50 jmmv Exp $
+#
+# This Makefile fragment is included by packages that use ORBit2.
+#
+# This file was created automatically using createbuildlink 2.8.
+#
 
 .if !defined(ORBIT2_BUILDLINK2_MK)
 ORBIT2_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=		orbit2
-BUILDLINK_DEPENDS.orbit2?=	ORBit2>=2.6.0nb4
-BUILDLINK_PKGSRCDIR.orbit2?=	../../net/ORBit2
+BUILDLINK_PACKAGES+=			ORBit2
+BUILDLINK_DEPENDS.ORBit2?=		ORBit2>=2.8.2
+BUILDLINK_PKGSRCDIR.ORBit2?=		../../net/ORBit2
 
-EVAL_PREFIX+=				BUILDLINK_PREFIX.orbit2=orbit2
-BUILDLINK_PREFIX.orbit2_DEFAULT=	${LOCALBASE}
+EVAL_PREFIX+=	BUILDLINK_PREFIX.ORBit2=ORBit2
+BUILDLINK_PREFIX.ORBit2_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.ORBit2+=	include/orbit-2.0/ORBitservices/*
+BUILDLINK_FILES.ORBit2+=	include/orbit-2.0/orbit-idl/*
+BUILDLINK_FILES.ORBit2+=	include/orbit-2.0/orbit/dynamic/*
+BUILDLINK_FILES.ORBit2+=	include/orbit-2.0/orbit/orb-core/*
+BUILDLINK_FILES.ORBit2+=	include/orbit-2.0/orbit/*
+BUILDLINK_FILES.ORBit2+=	include/orbit-2.0/orbit/poa/*
+BUILDLINK_FILES.ORBit2+=	include/orbit-2.0/orbit/util/*
+BUILDLINK_FILES.ORBit2+=	lib/libORBit-2.*
+BUILDLINK_FILES.ORBit2+=	lib/libORBit-imodule-2.*
+BUILDLINK_FILES.ORBit2+=	lib/libORBitCosNaming-2.*
+BUILDLINK_FILES.ORBit2+=	lib/libname-server-2.*
+BUILDLINK_FILES.ORBit2+=	lib/orbit-2.0/Everything_module.*
 
-BUILDLINK_FILES.orbit2=		include/orbit-2.0/orbit/*/*
-BUILDLINK_FILES.orbit2+=	include/orbit-2.0/orbit/*
-BUILDLINK_FILES.orbit2+=	include/orbit-2.0/ORBitservices/*
-BUILDLINK_FILES.orbit2+=	lib/libORBit-2.*
-BUILDLINK_FILES.orbit2+=	lib/libORBitCosNaming-2.*
-BUILDLINK_FILES.orbit2+=	lib/libname-server-2.*
-BUILDLINK_FILES.orbit2+=	lib/orbit-2.0/*
-BUILDLINK_FILES.orbit2+=	share/idl/orbit-2.0/CosNaming.idl
-
-.include "../../devel/pkgconfig/buildlink2.mk"
+.include "../../devel/glib2/buildlink2.mk"
 .include "../../devel/popt/buildlink2.mk"
 .include "../../net/libIDL/buildlink2.mk"
-.include "../../net/linc/buildlink2.mk"
 
-BUILDLINK_TARGETS+=	orbit2-buildlink
+BUILDLINK_TARGETS+=	ORBit2-buildlink
 
-orbit2-buildlink: _BUILDLINK_USE
+ORBit2-buildlink: _BUILDLINK_USE
 
 .endif	# ORBIT2_BUILDLINK2_MK
