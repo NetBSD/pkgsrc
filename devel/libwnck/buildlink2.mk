0e2aca193f213a9a73e7568264543ccf49528aaa
jmmv <jmmv@pkgsrc.org>
2003-12-14 19:50:21 +0000

    Update to 2.4.0.1, based on work done by xtraeme@ and Min Sik Kim.
    2.4.0
    - fix workspace name property handling
    - fix updating of window/icon names
    - fixes to viewport handling
    - lots of new translations
    
    2.3.1
    - put -lX11 in link flags
    - add API to manipulate window fullscreen state
    - use an icon from one of the windows in a group, rather than
      the "white piece of paper" icon, if a group doesn't set an icon
    - fixes for Cygwin compilation
    - add support for viewports in window list and workspace switcher
    - fix multihead glitches
    - set desktop layout properly on 64-bit platforms
    - new/updated translations
    
    2.3.0
    - fixed fitt's law for window list applet (Mark McLoughlin)
    - incorporated fixes from stable branch

--- devel/libwnck/buildlink2.mk
+++ devel/libwnck/buildlink2.mk
@@ -1,19 +1,19 @@
-# $NetBSD: buildlink2.mk,v 1.7 2003/12/13 00:45:22 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.8 2003/12/14 19:50:21 jmmv Exp $
 
 .if !defined(LIBWNCK_BUILDLINK2_MK)
 LIBWNCK_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libwnck
-BUILDLINK_DEPENDS.libwnck?=	libwnck>=2.2.1nb4
+BUILDLINK_DEPENDS.libwnck?=	libwnck>=2.4.0.1
 BUILDLINK_PKGSRCDIR.libwnck?=	../../devel/libwnck
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.libwnck=libwnck
 BUILDLINK_PREFIX.libwnck_DEFAULT=	${X11PREFIX}
 BUILDLINK_FILES.libwnck=	include/libwnck-1.0/libwnck/*
 BUILDLINK_FILES.libwnck+=	lib/libwnck-1.*
-BUILDLINK_FILES.libwnck+=	lib/pkgconfig/libwnck-1.0.pc
 
 .include "../../devel/gettext-lib/buildlink2.mk"
+.include "../../x11/startup-notification/buildlink2.mk"
 .include "../../x11/gtk2/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	libwnck-buildlink
