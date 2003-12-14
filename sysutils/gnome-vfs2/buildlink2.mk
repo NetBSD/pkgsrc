e6c8d8548d1c57b1bc37c812e0579b610c3acab2
jmmv <jmmv@pkgsrc.org>
2003-12-14 19:47:26 +0000

    Update to 2.4.1, based on work done by xtraeme@ and Min Sik Kim.
    gnome-vfs-2.4.1
    	* Bugs fixed
    		+ Allow to create symlinks to huge files on
                      nearly full file systems (James M. Cape)
    		+ Fix compilation on HP/UX
    	* Translations
    		+ he (Gil "Dolfin" Osher), it (Luca Ferretti),
    	          lv (Peteris Krisjanis), mr (Jitendra Shah),
                      nl (Vincent van Adrighem), pt_BR (Augusta Marques da Silva),
     	          ro (Mi?u Moldovan), ta (Dinesh)
    
    gnome-vfs 2.4.0
    	* Featurelets:
    		Updated translations
    
    gnome-vfs 2.3.90
    	* Bugs fixed:
    	       + Just emit one mime db changed signal per change
    
    gnome-vfs 2.3.8
    	* Bugs fixed:
    	        + CDE menu fixes
    		+ IPV6 build fixes for non-linux
    		+ removed usage of ngettext (for now)
    		+ fix fam crash
    	* Featurelets:
    		+ add dav schema for webdav usage
    		+ Make gnome_vfs_make_uri_from_input be smarter about
    		  when to make http: uris
    
    gnome-vfs 2.3.7
    	* Bugs fixed:
    	        + Test fixes
    		+ translation fixes
    	* Featurelets:
    		+ ignore_hosts gconf key for proxies
    
    gnome-vfs 2.3.6
    	* Bugs fixed:
    		+ Fixes in CDE menu handling
    		+ Shrink default gnome-vfs stack size
    		+ Small API doc updates
    		+ Properly read .mime and .keys file not ending with \n
    		+ Compilation fixes when using gcc 3.3
    		+ Compilation fixes for Solaris
    		+ Leak fixes in URI and mime-type handling
    		+ When an unknown app is added to a mime-type short list,
    		  also add it to this mime-type global list
    	* Featurelets:
    		+ Added an Accessibility submenu in the Applications menu
    		+ Improve parsing of URIs (wrongly) containing UTF-8 chars
    		+ Translation updates
    
    gnome-vfs 2.3.5:
    	* Features:
    		+ added a startup notification application registry key
    	* Bugs fixed:
    		+ Fix memory handling in mime handlers.
    		+ Make job_private static in gnome-vfs-job.c
    		+ Fixed CDE menu crash
    
    gnome-vfs 2.3.4:
    	* Bugs fixed:
    	       + Get Names of vfolder directories right
    
    gnome-vfs 2.3.3:
    	* Bugs fixed:
    	       + Fix potential crashes for uris with %2F in them
    	       + solaris libnsl/libsocket build fixes
    	       + Fix fam threadsafe issue
    	       + Handle fam connection going away better
    	       + Fixed doc building
    
    gnome-vfs 2.3.2
            * Features:
    	       + ipv6 uris supported
    	       + add some command line applications for gnome-vfs
    
    	* Bugs fixed:
    	       + mp3 mimetype is audio/mpeg, not audio/x-mp3
    	       + build issues fixed
    	       + fix ncpfs filesystem
    
    gnome-vfs 2.3.1
            * Features:
    	       + New API gnome_vfs_url_show() and related calls
    	       + New function gnome_vfs_make_uri_from_input_with_dirs
    	       + Cygwin port
    	       + IPV6 support
    
    	* Bugs fixed:
    	       + Some mime fixes
    	       + Correctly handle symlinks to symlinks
    	       + hpux fixes
    	       + CDE menu translation fixes
    	       + Webdav PROPFIND details
    	       + Fixed locale aliases handling
    	       + call bindtextlocale to get translation working
    	       + Fixes to mtab handling

--- sysutils/gnome-vfs2/buildlink2.mk
+++ sysutils/gnome-vfs2/buildlink2.mk
@@ -1,18 +1,22 @@
-# $NetBSD: buildlink2.mk,v 1.11 2003/11/12 01:31:51 salo Exp $
+# $NetBSD: buildlink2.mk,v 1.12 2003/12/14 19:47:26 jmmv Exp $
+#
+# This Makefile fragment is included by packages that use gnome-vfs2.
+#
+# This file was created automatically using createbuildlink 2.8.
+#
 
 .if !defined(GNOME_VFS2_BUILDLINK2_MK)
 GNOME_VFS2_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=		gnome-vfs2
-BUILDLINK_DEPENDS.gnome-vfs2?=	gnome-vfs2>=2.2.4nb4
+BUILDLINK_PACKAGES+=			gnome-vfs2
+BUILDLINK_DEPENDS.gnome-vfs2?=		gnome-vfs2>=2.4.1
 BUILDLINK_PKGSRCDIR.gnome-vfs2?=	../../sysutils/gnome-vfs2
 
 EVAL_PREFIX+=				BUILDLINK_PREFIX.gnome-vfs2=gnome-vfs2
 BUILDLINK_PREFIX.gnome-vfs2_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.gnome-vfs2=	include/gnome-vfs-2.0/libgnomevfs/*
+BUILDLINK_FILES.gnome-vfs2+=	include/gnome-vfs-2.0/libgnomevfs/*
 BUILDLINK_FILES.gnome-vfs2+=	include/gnome-vfs-module-2.0/libgnomevfs/*
-BUILDLINK_FILES.gnome-vfs2+=	lib/gnome-vfs-2.0/include/gnome-vfs-file-size.h
-BUILDLINK_FILES.gnome-vfs2+=	lib/gnome-vfs-2.0/modules/*
+BUILDLINK_FILES.gnome-vfs2+=	lib/gnome-vfs-2.0/include/*
 BUILDLINK_FILES.gnome-vfs2+=	lib/libgnomevfs-2.*
 
 .include "../../archivers/bzip2/buildlink2.mk"
@@ -22,9 +26,7 @@ BUILDLINK_FILES.gnome-vfs2+=	lib/libgnomevfs-2.*
 .include "../../devel/GConf2/buildlink2.mk"
 .include "../../devel/glib2/buildlink2.mk"
 .include "../../devel/libbonobo/buildlink2.mk"
-.include "../../devel/bonobo-activation/buildlink2.mk"
 .include "../../devel/popt/buildlink2.mk"
-.include "../../devel/zlib/buildlink2.mk"
 .include "../../net/ORBit2/buildlink2.mk"
 .include "../../textproc/libxml2/buildlink2.mk"
 .include "../../security/openssl/buildlink2.mk"
