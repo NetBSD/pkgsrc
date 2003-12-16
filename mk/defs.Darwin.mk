6183188e494f0250eec41777e8bef8c118a24420
heinz <heinz@pkgsrc.org>
2003-12-16 23:07:42 +0000

    Introduce command MAIL. It will be used to send messages like this: echo "message" | ${MAIL} -s"subject" add@ress.example

--- mk/defs.Darwin.mk
+++ mk/defs.Darwin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.Darwin.mk,v 1.63 2003/12/16 22:54:13 heinz Exp $
+# $NetBSD: defs.Darwin.mk,v 1.64 2003/12/16 23:07:42 heinz Exp $
 #
 # Variable definitions for the Darwin operating system.
 
@@ -38,6 +38,7 @@ LDD?=		/usr/bin/otool -L
 LN?=		/bin/ln
 LS?=		/bin/ls
 M4?=		/usr/bin/m4 
+MAIL?=		/usr/bin/mail
 MKDIR?=		/bin/mkdir -p
 MTREE?=		/usr/sbin/mtree
 MV?=		/bin/mv
