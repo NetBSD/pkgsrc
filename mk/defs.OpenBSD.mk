34eb3489f6ae5e128a3f32f9b3fd993d2f4a45ef
heinz <heinz>
2003-12-16 23:07:42 +0000

    Introduce command MAIL. It will be used to send messages like this: echo "message" | ${MAIL} -s"subject" add@ress.example

--- mk/defs.OpenBSD.mk
+++ mk/defs.OpenBSD.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.OpenBSD.mk,v 1.26 2003/12/16 22:54:13 heinz Exp $
+# $NetBSD: defs.OpenBSD.mk,v 1.27 2003/12/16 23:07:42 heinz Exp $
 #
 # Variable definitions for the OpenBSD operating system.
 
@@ -39,6 +39,7 @@ LDCONFIG?=	/sbin/ldconfig
 LN?=		/bin/ln
 LS?=		/bin/ls
 M4?=		/usr/bin/m4
+MAIL?=		/usr/bin/mail
 MKDIR?=		/bin/mkdir -p
 MTREE?=		/usr/sbin/mtree
 MV?=		/bin/mv
