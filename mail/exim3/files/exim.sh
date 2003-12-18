56e16b26838c0c399d35ff5c11490a6c47271ed7
grant <grant@pkgsrc.org>
2003-12-18 02:30:54 +0000

    kill some trailing whitespace (mmm, cutnpaste).

--- mail/exim3/files/exim.sh
+++ mail/exim3/files/exim.sh
@@ -1,18 +1,18 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-#	$NetBSD: exim.sh,v 1.1.1.1 2003/12/04 04:24:49 grant Exp $
+#	$NetBSD: exim.sh,v 1.2 2003/12/18 02:30:54 grant Exp $
 #
 # PROVIDE: mail
 # REQUIRE: LOGIN
 
 . /etc/rc.subr
 
-name="exim"                      
+name="exim"
 exim_flags=${exim_flags-"-bd -q30m"}
-rcvar=$name         
+rcvar=$name
 command="@PREFIX@/sbin/${name}"
 pidfile="/var/run/${name}.pid"
 required_files="@PREFIX@/etc/exim/configure"
-              
+
 load_rc_config $name
 run_rc_command "$1"
