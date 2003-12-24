45fe95921455f6c434af932f123eb72f167a1487
recht <recht@pkgsrc.org>
2003-12-24 11:42:02 +0000

    update to 0.11.1
    changes:
    
    - Preliminary pyGlobus support (contributed by Ivan R. Judson)
    - Fixes for many of the test scripts in tests/*.py, as well as
      documentation in tests/README of what tests succeed and fail.
    - New/Changed configuration settings:
    - Added 'strict' option to the WSDL class constructor. If strict is
      true, a RuntimeException will be raised if an unrecogned message is
      recieved. If strict is false, a warning will be printed to the
      console, the message type will be added to the WSDL schema, and
      processing will continue.  This is in response to the second half of
      bug report [ 817331 ] "Some WSDL.py changes", submitted by Rudolf
      Ruland.
    - Config.simplify_objects=1 now converts all SOAPpy objects into basic
      Python types (list, dictionary, tuple, double, float, etc.).  By default,
      Config.simplify_objects=0 for backward compatibility.
    - Config.dict_encoding='ascii' converts the keys of dictionaries
      (e.g. created when Config.simplify_objects=1) to ascii == plain python
      strings instead of unicode strings.  This variable can be set to any
      encoding known to string.encode().
    - Config.strict_range=1 forces the SOAP parsing routines to perform
      range checks on recieved SOAP float and double objects.  When
    
    The following bugs have been fixed:
    
    [ 752882 ] "SSL SOAP Server no longer working."
    [ 792258 ] "SOAPBuilder.SOAPBuilder.dump can catch wrong exceptions"
    [ 792600 ] "SOAPBuilder.SOAPBuilder.dump possibly should not call gentag"
    [ 817331 ] "Some WSDL.py changes"
    [ 858168 ] 'xsi:nil="true" causes exception'
    
    In addtion, all of the outstanding bugs in the WSDL implementation
    have been fixed, so WSDLProxy should now function properly.

--- net/py-soappy/buildlink2.mk
+++ net/py-soappy/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.5 2003/08/24 10:20:34 recht Exp $
+# $NetBSD: buildlink2.mk,v 1.6 2003/12/24 11:42:02 recht Exp $
 
 .if !defined(SOAPPY_BUILDLINK2_MK)
 SOAPPY_BUILDLINK2_MK=	# defined
@@ -7,7 +7,7 @@ PYTHON_VERSIONS_ACCEPTED=	23pth 22pth 21pth
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_PACKAGES+=			SOAPpy
-BUILDLINK_DEPENDS.SOAPpy?=		${PYPKGPREFIX}-SOAPpy>=0.10.1
+BUILDLINK_DEPENDS.SOAPpy?=		${PYPKGPREFIX}-SOAPpy>=0.11.1
 BUILDLINK_PKGSRCDIR.SOAPpy?=		../../net/py-soappy
 
 .include "../../textproc/pyxml/buildlink2.mk"
