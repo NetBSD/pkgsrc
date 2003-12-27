9c904ef1adde8bfd525b48e8f5365451a7c2e396
tron <tron>
2003-12-27 13:39:01 +0000

    Override predefined "USE_INET6" if "USE_SOCKS" is defined.

--- mk/bsd.prefs.mk
+++ mk/bsd.prefs.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.prefs.mk,v 1.136 2003/12/27 13:28:46 tron Exp $
+# $NetBSD: bsd.prefs.mk,v 1.137 2003/12/27 13:39:01 tron Exp $
 #
 # Make file, included to get the site preferences, if any.  Should
 # only be included by package Makefiles before any .if defined()
@@ -240,7 +240,7 @@ SHAREMODE?=		${DOCMODE}
 
 # if the system is IPv6-ready, compile with IPv6 support turned on.
 .if defined(USE_INET6)
-.if empty(USE_INET6:M[Yy][Ee][Ss])
+.if empty(USE_INET6:M[Yy][Ee][Ss]) || defined(USE_SOCKS)
 USE_INET6=		NO
 .else
 USE_INET6=		YES
