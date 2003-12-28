0ed97a2759cff928dda1267a1b41924d11545beb
jmmv <jmmv@pkgsrc.org>
2003-12-28 12:31:42 +0000

    Update to 1.3.1.  Changes since 1.1.9:
    * Check for stdint.h in autoconf
    * prevent the geoipupdate man page from trying to install itself directly
      in the system directory
    * Fixed includes to compile on Windows
    * Removed the _addr_to_num() calls from GeoIP_*_by_name()
    * _seek_record() optimizations
    * Added GeoIP_assign_region_by_inetaddr which doesn't do any mallocs and
      made all other region APIs go thru it
    * Replaced _h_addr_to_num() with ntohl() and removed _h_addr_to_num()
    * Fixed bug when IP address not found in region rev1 database
    * Added added extern "C" to GeoIPCity.h fixes problems when included
      in C ++ library
    * Added support for GeoIP ISP Edition identifier
    * Fixed bug in GeoIP_database_info
    * Added support for GeoIP AS Number Edition
    * renamed GeoIP_org_by_* functions to GeoIP_name_by_* to reduce confusion
      since these functions are used by GeoIP ISP and GeoIP ASNum as well as
      GeoIP Organization
    * Added support for GeoIP Proxy Edition
    * renamed GeoIP_country_id_by_* functions to GeoIP_id_by_*
    * Added GeoIP_record_id_by_addr and GeoIP_next_record functions
    * Added support for new GeoIP Region format, including Canadian Provinces
    * Added support for GeoIP City format revision 1, including dma code and
      area code
    * geoiplookup returns results from GeoIP Region, City, ISP and Organization
      databases
    * Standardized location for GeoIP Region, City, ISP and Organization data bases
    * Added GeoIP_open_type to open database from default location for other
      dbs besides country
    * Added check to make sure that the appropriate database is loaded for each
      lookup method
    * Updated update code to check for first 12 characters of license key
    * Added GeoIP_country_continent array to lookup continent by country ID
    
    Closes PR pkg/23909.

--- net/GeoIP/buildlink2.mk
+++ net/GeoIP/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/06/01 10:24:42 jmmv Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/12/28 12:31:42 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use GeoIP.
 #
@@ -9,7 +9,7 @@
 GEOIP_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			GeoIP
-BUILDLINK_DEPENDS.GeoIP?=		GeoIP>=1.1.9
+BUILDLINK_DEPENDS.GeoIP?=		GeoIP>=1.3.1
 BUILDLINK_PKGSRCDIR.GeoIP?=		../../net/GeoIP
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.GeoIP=GeoIP
