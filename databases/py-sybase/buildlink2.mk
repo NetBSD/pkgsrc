ef35fa308fa40b51763d5712c98ffcd4e2a98bb4
cjep <cjep>
2003-12-30 23:14:17 +0000

    Whitespace fixes

--- databases/py-sybase/buildlink2.mk
+++ databases/py-sybase/buildlink2.mk
@@ -1,25 +1,25 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/07/25 11:31:15 drochner Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/12/30 23:14:17 cjep Exp $
 #
 # This Makefile fragment is included by packages that use py-sybase.
 #
 # This file was created automatically using createbuildlink 2.6.
 #
- 
+
 .if !defined(PY_SYBASE_BUILDLINK2_MK)
 PY_SYBASE_BUILDLINK2_MK=	# defined
- 
+
 .include "../../lang/python/pyversion.mk"
- 
+
 BUILDLINK_PACKAGES+=		py-sybase
 BUILDLINK_DEPENDS.py-sybase?=	${PYPKGPREFIX}-sybase>=0.36
 BUILDLINK_PKGSRCDIR.py-sybase?=	../../databases/py-sybase
- 
+
 EVAL_PREFIX+=	BUILDLINK_PREFIX.py-sybase=${PYPKGPREFIX}-sybase
 BUILDLINK_PREFIX.py-sybase_DEFAULT=	${LOCALBASE}
  
 .include "../../databases/freetds/buildlink2.mk"
- 
+
 BUILDLINK_TARGETS+=	py-sybase-buildlink
- 
+
 py-sybase-buildlink: _BUILDLINK_USE
 .endif # PY_SYBASE_BUILDLINK2_MK
