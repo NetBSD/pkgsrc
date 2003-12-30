26085c3e4f60eea33fc5099e708164fb0d7f90fb
cjep <cjep@pkgsrc.org>
2003-12-30 16:48:44 +0000

    Whitespace fixes

--- mail/libsnert/buildlink2.mk
+++ mail/libsnert/buildlink2.mk
@@ -2,7 +2,7 @@ BUILDLINK_PACKAGES+=		libsnert
 BUILDLINK_PKGBASE.libsnert=	libsnert
 BUILDLINK_DEPENDS.libsnert?=	libsnert>=1.23
 BUILDLINK_PKGSRCDIR.libsnert?=	../../mail/libsnert
-	
+
 EVAL_PREFIX+=			BUILDLINK_PREFIX.libsnert=libsnert
 BUILDLINK_PREFIX.libsnert_DEFAULT= ${LOCALBASE}
 BUILDLINK_FILES.libsnert=	include/com/snert/lib/*.h
@@ -11,7 +11,7 @@ BUILDLINK_FILES.libsnert+=	include/com/snert/lib/io/*.h
 BUILDLINK_FILES.libsnert+=	include/com/snert/lib/sys/*.h
 BUILDLINK_FILES.libsnert+=	include/com/snert/lib/util/*.h
 BUILDLINK_FILES.libsnert+=	lib/libsnert.a
-	
+
 BUILDLINK_TARGETS+=		libsnert-buildlink
-	
+
 libsnert-buildlink: 		_BUILDLINK_USE
