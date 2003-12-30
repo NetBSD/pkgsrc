802885d2e03b6beb0d896c20e79b7bdc6cfa8a2f
kristerw <kristerw@pkgsrc.org>
2003-12-30 23:24:45 +0000

    Add the gmpxx lib and .h files.

--- devel/gmp/buildlink2.mk
+++ devel/gmp/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/02/21 12:25:22 mycroft Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/12/30 23:24:45 kristerw Exp $
 
 .if !defined(GMP_BUILDLINK2_MK)
 GMP_BUILDLINK2_MK=	# defined
@@ -10,7 +10,9 @@ BUILDLINK_PKGSRCDIR.gmp?=	../../devel/gmp
 EVAL_PREFIX+=			BUILDLINK_PREFIX.gmp=gmp
 BUILDLINK_PREFIX.gmp_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.gmp=		include/gmp.h
+BUILDLINK_FILES.gmp+=		include/gmpxx.h
 BUILDLINK_FILES.gmp+=		lib/libgmp.*
+BUILDLINK_FILES.gmp+=		lib/libgmpxx.*
 
 BUILDLINK_TARGETS+=	gmp-buildlink
 
