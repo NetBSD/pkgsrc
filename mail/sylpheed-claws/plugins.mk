5e4b446ab93ebebe4e67461d4a8039aec24b791f
cube <cube@pkgsrc.org>
2004-01-01 21:39:02 +0000

    Organise the Makefile and add a framework to allow building the plugins found in sylpheed-claws distribution.  Note that this is _only_ meant for included plugins.  The image-viewer and dillo-viewer packages are in the pipe.
    From the sylpheed-claws package point of view, this is just a reordering
    of the Makefile.  It breaks pkglint'ing, but it's not like there really is
    a choice.

---
+++ mail/sylpheed-claws/plugins.mk
@@ -0,0 +1,29 @@
+# $NetBSD: plugins.mk,v 1.1 2004/01/01 21:39:02 cube Exp $
+#
+
+.include "../../mail/sylpheed-claws/Makefile.common"
+
+PKGNAME=	sylpheed-claws-${PLUGIN_NAME:S/_/-/}-${SYLPHEED_VERSION}
+
+WRKSRC=		${WRKDIR}/${DISTNAME}/src/plugins/${PLUGIN_NAME}
+CONFIGURE_DIRS=	${WRKDIR}/${DISTNAME}
+
+DEPENDS+=       sylpheed-claws>=${SYLPHEED_VERSION}:../../mail/sylpheed-claws
+
+LIBTOOL_OVERRIDE=	${WRKDIR}/${DISTNAME}/libtool
+
+CONFIGURE_ARGS+=        --disable-aspell
+CONFIGURE_ARGS+=	--disable-nls
+CONFIGURE_ARGS+=	--disable-openssl
+CONFIGURE_ARGS+=	--disable-gpgme
+CONFIGURE_ARGS+=	--disable-compface
+CONFIGURE_ARGS+=	--disable-ipv6
+CONFIGURE_ARGS+=	--disable-trayicon-plugin
+CONFIGURE_ARGS+=	--disable-image-viewer-plugin
+CONFIGURE_ARGS+=	--disable-clamav-plugin
+CONFIGURE_ARGS+=	--disable-dillo-viewer-plugin
+CONFIGURE_ARGS+=	--disable-spamassassin-plugin
+CONFIGURE_ARGS+=	--disable-mathml-viewer-plugin
+
+PLIST_SRC=	${.CURDIR}/../../mail/sylpheed-claws/PLIST.plugins
+PLIST_SUBST+=	PLUGIN_NAME=${PLUGIN_NAME}
