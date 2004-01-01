3e3e80b8b35e1d3a8e44fab1980bf1c6796667de
cjep <cjep@pkgsrc.org>
2004-01-01 18:49:22 +0000

    Update of math/gap to 4.3 bug fix 5.
    Pkgsrc fixes:
     1) Install trans directory.
    
    Official bug fixes since bug fix 4:
    
     1) A wrong return format for `IsomorphicSubgroups' applied to cyclic groups.
     2) A wrong `true' result of `IsSubset' for certain algebras.
     3) Segmentation faults and other strange behaviour when assigning
        finite field elements of different characteristics into compressed vectors
     4) A bug in the subgroup conjugation test for permutation groups that are
        not subgroups.
     5) A missing method for `BaseOrthogonalSpaceMat'.
     6) A missing `Set' call in the construction of the global variable
        `AUTOLOAD_PACKAGES'.
     7) A wrong display string of the numerator in rational functions returned
        by `MolienSeries' (in the case that the constant term of this numerator
        is zero).
     8) A strange behaviour of `Intersection' for the case that a strictly
        sorted list is the unique entry of the list that is given as the
        argument; in this situation, this entry itself was returned instead of
        a shallow copy.
     9) An error in the basis of a product space of algebras.
    10) Possibly wrong result of `Centre' for pc groups.
    11) Possibly wrong result of `DirectSumDecomposition' for matrix Lie algebras.
    12) An error in `LieNormalizer', `LieCentralizer' for zero subspaces.
    13) An error in the computation of matrices of adjoint modules.
    14) A strange error message when constructing the simple Lie algebra of type
        B1.
    15) An error in `ModuleByRestriction'.
    16) An error in `IrrBaumClausen' for the trivial group.
    17) An error with vector space bases of row spaces over fields which
        neither are prime fields nor contain all entries of the vectors.
    18) An error with `IsMonomial', when it uses the function
        `TestMonomialFromLattice' (i.e., in hard cases, likely for characters of
        nonsolvable groups).
    
    Errors 1, 3, 5, 6, 9, 12, 13, 14 and 16 may cause GAP to stop with an error
    message, errors 2, 4, 7, 10, 17, and 18 may cause wrong results.

--- math/gap/gap.package.mk
+++ math/gap/gap.package.mk
@@ -1,11 +1,11 @@
-# $NetBSD: gap.package.mk,v 1.9 2003/12/18 14:44:17 sketch Exp $
+# $NetBSD: gap.package.mk,v 1.10 2004/01/01 18:49:22 cjep Exp $
 #
 # Set GAP_PKG_NAME in your Makefile. More may appear here later.
 
-# Note that between nb1 == bug fix 3 and nb2 == bug fix 4 there are
+# Note that between 4.3nb1 == bug fix 3 and 4.3nb2 == bug fix 4 there are
 # some interface changes
 
-DEPENDS+=       	gap>=4.3nb6:../../math/gap
+DEPENDS+=       	gap>=4.3.5:../../math/gap
 GAP_DIRECTORY=  	${LOCALBASE}/lib/gap4.3
 GAP_PKG_ROOT=		${GAP_DIRECTORY}/pkg
 GAP_PKG_DIRECTORY=	${GAP_PKG_ROOT}/${GAP_PKG_NAME}
