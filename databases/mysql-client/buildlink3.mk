ec57e785948f02fdafc0a63409e816bd158715ef
jlam <jlam@pkgsrc.org>
2004-01-04 23:46:39 +0000

    Use BUILDLINK_{INCDIRS,LIBDIRS}.<pkg> instead of BUILDLINK_{CPPFLAGS,LDFLAGS}.<pkg> where we're adding search directories. Using the former should do the right thing for pkgviews if we do binary package upgrades of dependencies by inserting the right default view directories into the search path as well.

--- databases/mysql-client/buildlink3.mk
+++ databases/mysql-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/01/04 23:34:05 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/01/04 23:46:39 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MYSQL_CLIENT_BUILDLINK3_MK:=	${MYSQL_CLIENT_BUILDLINK3_MK}+
@@ -11,9 +11,7 @@ BUILDLINK_DEPENDS+=	mysql-client
 BUILDLINK_PACKAGES+=			mysql-client
 BUILDLINK_DEPENDS.mysql-client?=	mysql-client>=3.23.58
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql-client
-BUILDLINK_LDFLAGS.mysql-client?=	\
-	-L${BUILDLINK_PREFIX.mysql-client}/lib/mysql			\
-	-Wl,${RPATH_FLAG}${BUILDLINK_PREFIX.mysql-client}/lib/mysql
+BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
 
 .  include "../../devel/zlib/buildlink3.mk"
 .endif	# MYSQL_CLIENT_BUILDLINK3_MK
