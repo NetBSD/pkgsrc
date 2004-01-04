31dbec3a55fb3752ef34d7ce6e84b14abfe84227
jlam <jlam>
2004-01-04 23:34:04 +0000

    Re-arrange to match example buildlink3.mk file in bsd.buildlink3.mk.

--- print/cups/buildlink3.mk
+++ print/cups/buildlink3.mk
@@ -1,19 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/01/03 23:06:45 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/01/04 23:34:07 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CUPS_BUILDLINK3_MK:=	${CUPS_BUILDLINK3_MK}+
 
-.if !empty(CUPS_BUILDLINK3_MK:M\+)
-BUILDLINK_DEPENDS.cups?=	cups>=1.1.14nb1
-BUILDLINK_PKGSRCDIR.cups?=	../../print/cups
-.endif	# CUPS_BUILDLINK3_MK
-
-.if !empty(BUILDLINK_DEPTH:M\+)
+.if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	cups
 .endif
 
-.if !empty(CUPS_BUILDLINK3_MK:M\+)
-BUILDLINK_PACKAGES+=	cups
+.if !empty(CUPS_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=		cups
+BUILDLINK_DEPENDS.cups?=	cups>=1.1.14nb1
+BUILDLINK_PKGSRCDIR.cups?=	../../print/cups
 
 .  include "../../graphics/png/buildlink3.mk"
 .  include "../../graphics/tiff/buildlink3.mk"
