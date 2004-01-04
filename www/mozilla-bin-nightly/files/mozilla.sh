5cfd4f362781b8dbbc094201f91295bfb3d8083f
grant <grant@pkgsrc.org>
2004-01-04 05:01:02 +0000

    factor out the common bits of the various mozilla binary packages into mozilla-bin/Makefile.common and mozilla-bin-nightly/Makefile.common.
    the result is much less duplication and more consistent installations.
    tested on NetBSD-current only (for now).

--- www/mozilla-bin-nightly/files/mozilla.sh
+++
@@ -1,7 +0,0 @@
-#!@SH@
-# $NetBSD: mozilla.sh,v 1.3 2003/07/18 07:31:17 grant Exp $
-
-LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:@PREFIX@/lib/mozilla-@PLATFORM@:@PREFIX@/lib"
-export LD_LIBRARY_PATH
-
-exec @PREFIX@/lib/mozilla-@PLATFORM@/mozilla "$@"
