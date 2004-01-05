4cda707aa74d46d245011b38f233728b6cb1be3a
jlam <jlam>
2004-01-05 09:31:31 +0000

    Sow BUILDLINK_USE_BUILTIN.<pkg> and reap _NEED_<PKG> variables.

--- archivers/bzip2/buildlink3.mk
+++ archivers/bzip2/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/01/04 23:34:04 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/01/05 09:31:31 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 BZIP2_BUILDLINK3_MK:=	${BZIP2_BUILDLINK3_MK}+
 
-.if !empty(BZIP2_BUILDLINK3_MK:M+)
-.  include "../../mk/bsd.prefs.mk"
+.include "../../mk/bsd.prefs.mk"
 
+.if !empty(BZIP2_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=		bzip2
 BUILDLINK_DEPENDS.bzip2?=	bzip2>=1.0.1
 BUILDLINK_PKGSRCDIR.bzip2?=	../../archivers/bzip2
 .endif	# BZIP2_BUILDLINK3_MK
@@ -26,18 +27,18 @@ MAKEFLAGS+=	BUILDLINK_IS_BUILTIN.bzip2=${BUILDLINK_IS_BUILTIN.bzip2}
 .endif
 
 .if defined(USE_BZIP2)
-_NEED_BZIP2=	YES
+BUILDLINK_USE_BUILTIN.bzip2=	NO
 .endif
 
 .if !empty(BUILDLINK_CHECK_BUILTIN.bzip2:M[yY][eE][sS])
-_NEED_BZIP2=	NO
+BUILDLINK_USE_BUILTIN.bzip2=	YES
 .endif
 
-.if !defined(_NEED_BZIP2)
+.if !defined(BUILDLINK_USE_BUILTIN.bzip2)
 .  if !empty(BUILDLINK_IS_BUILTIN.bzip2:M[nN][oO])
-_NEED_BZIP2=	YES
+BUILDLINK_USE_BUILTIN.bzip2=	NO
 .  else
-_NEED_BZIP2=	NO
+BUILDLINK_USE_BUILTIN.bzip2=	YES
 #
 # Solaris 9 has bzip2 1.0.1, build it on older versions.
 # Darwin only has static libbz2.a, which can't be buildlinked.
@@ -46,25 +47,17 @@ _INCOMPAT_BZIP2=	SunOS-5.[678]-* Darwin-*
 INCOMPAT_BZIP2?=	# empty
 .    for _pattern_ in ${_INCOMPAT_BZIP2} ${INCOMPAT_BZIP2}
 .      if !empty(MACHINE_PLATFORM:M${_pattern_})
-_NEED_BZIP2=	YES
+BUILDLINK_USE_BUILTIN.bzip2=	NO
 .      endif
 .    endfor
 .  endif
-MAKEFLAGS+=	_NEED_BZIP2="${_NEED_BZIP2}"
+MAKEFLAGS+=	BUILDLINK_USE_BUILTIN.bzip2="${BUILDLINK_USE_BUILTIN.bzip2}"
 .endif
 
-.if ${_NEED_BZIP2} == "YES"
+.if !empty(BUILDLINK_USE_BUILTIN.bzip2:M[nN][oO])
 .  if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	bzip2
 .  endif
 .endif
-
-.if !empty(BZIP2_BUILDLINK3_MK:M+)
-.  if ${_NEED_BZIP2} == "YES"
-BUILDLINK_PACKAGES+=		bzip2
-.  else
-BUILDLINK_PREFIX.bzip2=		/usr
-.  endif
-.endif	# BZIP2_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:C/\+$//}
