4a30077723a2c69edb832016b9be86e34a0c27db
hubertf <hubertf@pkgsrc.org>
2004-01-06 15:53:41 +0000

    pkg_install now prints post-install message by default, if available.

--- pkgtools/pkg_install/files/info/main.c
+++ pkgtools/pkg_install/files/info/main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main.c,v 1.6 2003/09/23 07:13:51 grant Exp $	*/
+/*	$NetBSD: main.c,v 1.7 2004/01/06 15:53:41 hubertf Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -11,7 +11,7 @@
 #if 0
 static char *rcsid = "from FreeBSD Id: main.c,v 1.14 1997/10/08 07:47:26 charnier Exp";
 #else
-__RCSID("$NetBSD: main.c,v 1.6 2003/09/23 07:13:51 grant Exp $");
+__RCSID("$NetBSD: main.c,v 1.7 2004/01/06 15:53:41 hubertf Exp $");
 #endif
 #endif
 
@@ -217,7 +217,8 @@ main(int argc, char **argv)
 
 	/* Set some reasonable defaults */
 	if (!Flags)
-		Flags = SHOW_COMMENT | SHOW_DESC | SHOW_REQBY | SHOW_DEPENDS;
+		Flags = SHOW_COMMENT | SHOW_DESC | SHOW_REQBY 
+			| SHOW_DEPENDS | SHOW_DISPLAY;
 
 	/* -Fe /filename -> change CheckPkg to real packagename */
 	if (CheckPkg && File2Pkg) {
