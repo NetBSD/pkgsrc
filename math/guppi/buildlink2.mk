23c43b8e0df65396f5c54cd2e609fd3a405fe02d
jmmv <jmmv>
2004-01-06 18:52:33 +0000

    Adjust dependancies to only depend on required libraries, not the whole gnome-core package.  This lets gnucash be installed alongside GNOME2 again. Bump PKGREVISION to 8.

--- math/guppi/buildlink2.mk
+++ math/guppi/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.6 2004/01/03 18:49:46 reed Exp $
+# $NetBSD: buildlink2.mk,v 1.7 2004/01/06 18:52:33 jmmv Exp $
 
 .if !defined(GUPPI_BUILDLINK2_MK)
 GUPPI_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		guppi
-BUILDLINK_DEPENDS.guppi?=	guppi>=0.40.3nb7
+BUILDLINK_DEPENDS.guppi?=	guppi>=0.40.3nb8
 BUILDLINK_PKGSRCDIR.guppi?=	../../math/guppi
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.guppi=guppi
@@ -14,9 +14,13 @@ BUILDLINK_FILES.guppi+=	include/gnome-1.0/libguppitank/*
 BUILDLINK_FILES.guppi+=	lib/libguppi.*
 BUILDLINK_FILES.guppi+=	lib/libguppitank.*
 
+.include "../../devel/bonobo/buildlink2.mk"
+.include "../../devel/gal/buildlink2.mk"
 .include "../../devel/libglade/buildlink2.mk"
 .include "../../devel/readline/buildlink2.mk"
-.include "../../x11/gnome-core/buildlink2.mk"
+.include "../../lang/guile14/buildlink2.mk"
+.include "../../print/gnome-print/buildlink2.mk"
+.include "../../x11/gnome-libs/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	guppi-buildlink
 
