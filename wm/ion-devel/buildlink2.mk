5d94b384cd1a77080927816d1950472035c8a023
cube <cube@pkgsrc.org>
2004-01-09 23:05:01 +0000

    wip -> wm

--- wm/ion-devel/buildlink2.mk
+++ wm/ion-devel/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2004/01/09 23:03:20 cube Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2004/01/09 23:05:01 cube Exp $
 
 .if !defined(ION_BUILDLINK2_MK)
 ION_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		ion
 BUILDLINK_DEPENDS.ion?=		ion>=20031211
-BUILDLINK_PKGSRCDIR.ion?=	../../wip/ion-devel
+BUILDLINK_PKGSRCDIR.ion?=	../../wm/ion-devel
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.ion=ion
 BUILDLINK_PREFIX.ion_DEFAULT=	${LOCALBASE}
