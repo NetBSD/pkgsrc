2baa5de93f245fcaab22c7b479152099f2135c2b
cube <cube@pkgsrc.org>
2004-01-09 23:03:19 +0000

    Initial import of ion-devel, version 20031211 into the NetBSD Package Collection.  It is the development version of Ion, a light window manager. There are a lot of new features since the release of the last stable version.
    Ion is a window manager dividing the screen into frames rather than having
    overlapping windows, much like the windows in Emacs.  Navigation between
    clients can be done solely using the keyboard without need for a mouse.

---
+++ wm/ion-devel/buildlink2.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2004/01/09 23:03:20 cube Exp $
+
+.if !defined(ION_BUILDLINK2_MK)
+ION_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=		ion
+BUILDLINK_DEPENDS.ion?=		ion>=20031211
+BUILDLINK_PKGSRCDIR.ion?=	../../wip/ion-devel
+
+EVAL_PREFIX+=			BUILDLINK_PREFIX.ion=ion
+BUILDLINK_PREFIX.ion_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.ion=		include/ion/*.h
+BUILDLINK_FILES.ion+=		include/ion/mkexports.lua
+BUILDLINK_FILES.ion+=		include/ion/*.mk
+BUILDLINK_FILES.ion+=		include/ion/ioncore/*.h
+BUILDLINK_FILES.ion+=		include/ion/libtu/*.h
+BUILDLINK_FILES.ion+=		include/ion/luaextl/*.h
+
+BUILDLINK_TARGETS+=		ion-buildlink
+
+ion-buildlink: _BUILDLINK_USE
+
+.endif	# ION_BUILDLINK2_MK
