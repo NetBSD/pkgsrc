01e3c4734f412c410f8a10de24c9fa89bd151dbf
recht <recht@pkgsrc.org>
2004-01-10 14:23:52 +0000

    update to wxGTK 2.4.2
    Changes:
    Enable building of OpenGL support and bump buildlink dependecy because of
    that.
    
    2.4.2
    -----
    All:
    - always NUL-terminate the log messages
    - wxRegEx::Compile() now calculates the number of groups correctly
    - wxHTTP::GetHeader() didn't find headers which were not all in upper case
    - wxHTTP input stream didn't detect EOF correctly
    - wxString::find_last_of() ignored "start" parameter (Robert Vazan)
    - a bug in wxArrayString::Shrink() fixed (Gunnar Roth)
    - added wxIPV4address::IPAddress()
    
    All (GUI):
    - rare crash in wxFontMapper fixed (Robert Vazan)
    - added wxMenu::FindItemByPosition.
    - copy wxImage options when copying the images
    
    Generic:
    - wxListCtrl: respect wxLC_NO_HEADER style when inserting first column
    
    Unix:
    - fixed build problems with OpenGL when libGL(U) were in different directories
    - added --basename and --release flags to wx-config (Robert O'Connor)
    - added support for GNU/Hurd in configure
    - fixed wx-config --rezflags output on platforms different from Mac
    - changed wxNativeFontInfo::FromXFontName to treat names with all
      components being empty or '*' as default font (since default font is
      converted to such a string by GetXFontName).
    - wxLocale::Init now tries to set .utf8 locale in Unicode mode (Andreas Pflug)
    
    wxGTK
    - fixed compilation with --disable-compat-22
    - set the event object correctly for wxMenu events (Bob Balfour)
    - fixed memory leak in wxFileDialog (Christian Sturmlechner)
    
    wxXRC:
    - added wxNO_FULL_REPAINT_ON_RESIZE to "unknown" class handler (John Anderson)

--- x11/wxGTK/buildlink2.mk
+++ x11/wxGTK/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.8 2004/01/07 21:45:18 jmmv Exp $
+# $NetBSD: buildlink2.mk,v 1.9 2004/01/10 14:23:52 recht Exp $
 #
 # This Makefile fragment is included by packages that use wxGTK.
 #
@@ -9,7 +9,7 @@
 WXGTK_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		wxGTK
-BUILDLINK_DEPENDS.wxGTK?=	wxGTK>=2.4.1nb3
+BUILDLINK_DEPENDS.wxGTK?=	wxGTK>=2.4.2
 BUILDLINK_PKGSRCDIR.wxGTK?=	../../x11/wxGTK
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.wxGTK=wxGTK
@@ -25,15 +25,18 @@ BUILDLINK_FILES.wxGTK+=	include/wx/unix/*
 
 .if !empty(WXGTK_USE_GTK1:M[Yy][Ee][Ss])
 BUILDLINK_FILES.wxGTK+=	lib/libwx_gtk.*
+BUILDLINK_FILES.wxGTK+=	lib/libwx_gtk_gl-2.4.*
 BUILDLINK_FILES.wxGTK+=	lib/wx/include/gtk-2.4/wx/*
 .include "../../x11/gtk/buildlink2.mk"
 .else
 BUILDLINK_FILES.wxGTK+=	lib/libwx_gtk2.*
+BUILDLINK_FILES.wxGTK+=	lib/libwx_gtk2_gl-2.4.*
 BUILDLINK_FILES.wxGTK+=	lib/wx/include/gtk2-2.4/wx/*
 .include "../../x11/gtk2/buildlink2.mk"
 .endif
 
 .include "../../devel/zlib/buildlink2.mk"
+.include "../../graphics/MesaLib/buildlink2.mk"
 .include "../../graphics/jpeg/buildlink2.mk"
 .include "../../graphics/png/buildlink2.mk"
 .include "../../graphics/tiff/buildlink2.mk"
