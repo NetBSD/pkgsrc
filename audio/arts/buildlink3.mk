db2a7b350dd8e73fb567675b3d1c8aa7086d8884
jlam <jlam@pkgsrc.org>
2004-01-14 07:37:00 +0000

    Add some buildlink3.mk files.

---
+++ audio/arts/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/01/14 07:37:00 jlam Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+ARTS_BUILDLINK3_MK:=	${ARTS_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	arts
+.endif
+
+.if !empty(ARTS_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=		arts
+BUILDLINK_DEPENDS.arts?=	arts>=1.1.4nb1
+BUILDLINK_PKGSRCDIR.arts?=	../../audio/arts
+
+.  include "../../audio/libaudiofile/buildilnk3.mk"
+.  include "../../audio/libogg/buildilnk3.mk"
+.  include "../../audio/libvorbis/buildilnk3.mk"
+.  include "../../mk/ossaudio.buildilnk3.mk"
+.endif	# ARTS_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
