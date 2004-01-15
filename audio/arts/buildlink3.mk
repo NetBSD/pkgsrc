4936d7776c30bab2e9124c0099651fd32afc783f
markd <markd@pkgsrc.org>
2004-01-15 09:11:02 +0000

    s/buildilnk3/buildlink3/

--- audio/arts/buildlink3.mk
+++ audio/arts/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/01/14 07:37:00 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/01/15 09:11:02 markd Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 ARTS_BUILDLINK3_MK:=	${ARTS_BUILDLINK3_MK}+
@@ -12,10 +12,10 @@ BUILDLINK_PACKAGES+=		arts
 BUILDLINK_DEPENDS.arts?=	arts>=1.1.4nb1
 BUILDLINK_PKGSRCDIR.arts?=	../../audio/arts
 
-.  include "../../audio/libaudiofile/buildilnk3.mk"
-.  include "../../audio/libogg/buildilnk3.mk"
-.  include "../../audio/libvorbis/buildilnk3.mk"
-.  include "../../mk/ossaudio.buildilnk3.mk"
+.  include "../../audio/libaudiofile/buildlink3.mk"
+.  include "../../audio/libogg/buildlink3.mk"
+.  include "../../audio/libvorbis/buildlink3.mk"
+.  include "../../mk/ossaudio.buildlink3.mk"
 .endif	# ARTS_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
