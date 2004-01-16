5a0c981e9d435fa1383344a5571f8f7962855e19
xtraeme <xtraeme@pkgsrc.org>
2004-01-16 10:33:10 +0000

    Update to 0.4.0
    Changes:
    
    	o 4:2:2-profile decoding
    	o Better robustness
    	o Concatenated streams support
    	o Color conversion helper library
    	o sparc VIS MC optimizations
    	o Basic documentation, etc...

--- graphics/libmpeg2/buildlink2.mk
+++ graphics/libmpeg2/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/08/05 07:13:42 wulf Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2004/01/16 10:33:10 xtraeme Exp $
 #
 # This Makefile fragment is included by packages that use libmpeg2.
 #
@@ -9,7 +9,7 @@
 LIBMPEG2_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			libmpeg2
-BUILDLINK_DEPENDS.libmpeg2?=		libmpeg2>=20030612
+BUILDLINK_DEPENDS.libmpeg2?=		libmpeg2>=0.4.0
 BUILDLINK_PKGSRCDIR.libmpeg2?=		../../graphics/libmpeg2
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libmpeg2=libmpeg2
@@ -17,7 +17,6 @@ BUILDLINK_PREFIX.libmpeg2_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.libmpeg2+=	include/mpeg2dec/mpeg2.h
 BUILDLINK_FILES.libmpeg2+=	include/mpeg2dec/convert.h
 BUILDLINK_FILES.libmpeg2+=	lib/libmpeg2.*
-BUILDLINK_FILES.libmpeg2+=	lib/pkgconfig/libmpeg2.pc
 
 .include "../../devel/SDL/buildlink2.mk"
 
