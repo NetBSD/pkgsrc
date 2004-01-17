7df666924a2fb769c014b4e1d1b0587fd37eac34
recht <recht@pkgsrc.org>
2004-01-17 14:38:22 +0000

    initial import of libcdio-0.65
    This library is to encapsulate CD-ROM reading and
    control. Applications wishing to be oblivious of the OS- and
    device-dependant properties of a CD-ROM can use this library.
    
    Some support for disk image types like BIN/CUE and NRG is available,
    so applications that use this library also have the ability to read
    disc images as though they were CD's.

---
+++ misc/libcdio/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/01/17 14:38:22 recht Exp $
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBCDIO_BUILDLINK3_MK:=	${LIBCDIO_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libcdio
+.endif
+
+.if !empty(LIBCDIO_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			libcdio
+BUILDLINK_DEPENDS.libcdio?=		libcdio>=0.65
+BUILDLINK_PKGSRCDIR.libcdio?=		../../misc/libcdio.work
+
+.endif # LIBCDIO_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
