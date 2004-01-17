d67c7c83251e687f37cc62c36fa9ee1b6d4e3d3d
jlam <jlam@pkgsrc.org>
2004-01-17 15:46:27 +0000

    Add a buildlink3.mk file for audio/libvorbis.

---
+++ audio/libvorbis/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/01/17 15:46:27 jlam Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBVORBIS_BUILDLINK3_MK:=	${LIBVORBIS_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libvorbis
+.endif
+
+.if !empty(LIBVORBIS_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=		libvorbis
+BUILDLINK_DEPENDS.libvorbis?=	libvorbis>=1.0.1
+BUILDLINK_PKGSRCDIR.libvorbis?=	../../audio/libvorbis
+
+.  include "../../audio/libogg/buildlink3.mk"
+.endif	# LIBVORBIS_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
