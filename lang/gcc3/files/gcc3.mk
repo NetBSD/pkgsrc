df48ec9ad176c6309ed0af0e746500881bb4e00f
abs <abs>
2004-01-21 10:17:56 +0000

    Add USE_GCC3 to files/gcc3.mk to work better when included in mk.conf

--- lang/gcc3/files/gcc3.mk
+++ lang/gcc3/files/gcc3.mk
@@ -1,7 +1,8 @@
-# $NetBSD: gcc3.mk,v 1.11 2003/07/26 17:37:06 martin Exp $
+# $NetBSD: gcc3.mk,v 1.12 2004/01/21 10:17:56 abs Exp $
 #
 # make configuration file for @DISTNAME@
 
+USE_GCC3=	yes
 CC=		@GCC_PREFIX@/bin/gcc
 CPP=		@GCC_PREFIX@/bin/cpp
 CXX=		@GCC_PREFIX@/bin/g++
