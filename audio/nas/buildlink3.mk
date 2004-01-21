a5f99db9cf1d45ee3e2e8b22e52a203f5c02f478
jlam <jlam@pkgsrc.org>
2004-01-21 18:22:39 +0000

    nas is in the audio category, not graphics.

--- audio/nas/buildlink3.mk
+++ audio/nas/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/01/14 07:37:01 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/01/21 18:22:39 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 NAS_BUILDLINK3_MK:=	${NAS_BUILDLINK3_MK}+
@@ -10,7 +10,7 @@ BUILDLINK_DEPENDS+=	nas
 .if !empty(NAS_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=		nas
 BUILDLINK_DEPENDS.nas?=		nas>=1.4.2
-BUILDLINK_PKGSRCDIR.nas?=	../../graphics/nas
+BUILDLINK_PKGSRCDIR.nas?=	../../audio/nas
 .endif	# NAS_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
