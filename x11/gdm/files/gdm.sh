9780a994e880b006389a84a1928e26ec10763517
jmmv <jmmv@pkgsrc.org>
2004-01-22 09:16:59 +0000

    - Create /var/gdm and /var/log/gdm directories during installation.  These   are *REQUIRED* (in fact, only the former) to get gdm working. - Manually set 'procname' in the rc.d script so that 'gdm stop' works. - Allow the rc.d script to work w/o rc.subr. - Use PAM's buildlink2.mk file instead of a simple DEPENDS. - Remove useless stuff from PLIST. - Simplify Makefile a bit. - As a result, bump PKGREVISION to 1.

--- x11/gdm/files/gdm.sh
+++ x11/gdm/files/gdm.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 #
-# $NetBSD: gdm.sh,v 1.1 2004/01/20 13:41:06 xtraeme Exp $
+# $NetBSD: gdm.sh,v 1.2 2004/01/22 09:16:59 jmmv Exp $
 #
 
 # PROVIDE: gdm
 # REQUIRE: DAEMON LOGIN wscons
 # KEYWORD: shutdown
 
-. /etc/rc.subr
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
+fi
 
 name="gdm"
-rcvar=$name
+rcvar=${name}
 command="@PREFIX@/bin/${name}"
-pidfile="@LOCALSTATEDIR@/run/${name}.pid"
+pidfile="/var/run/${name}.pid"
+procname="@PREFIX@/bin/gdm-binary"
 required_files="@PKG_SYSCONFDIR@/gdm/gdm.conf"
 extra_commands="reload"
 
-load_rc_config $name
-run_rc_command "$1"
+if [ -f /etc/rc.subr ]; then
+	load_rc_config ${name}
+	run_rc_command "$1"
+else
+	echo -n " ${name}"
+	${command} ${gdm_flags} ${command_args}
+fi
