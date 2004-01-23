092dd6c04d407c44bc0ad451498940b39740289d
recht <recht@pkgsrc.org>
2004-01-23 15:36:13 +0000

    Catch up with pyxml -> py-xml name change in the Makefile and finally remove pyxml.

--- textproc/pyxml/buildlink2.mk
+++
@@ -1,7 +0,0 @@
-# $NetBSD: buildlink2.mk,v 1.1 2002/10/24 22:24:24 drochner Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_PACKAGES+=		pyxml
-BUILDLINK_DEPENDS.pyxml?=	${PYPKGPREFIX}-xml-*
-BUILDLINK_PKGSRCDIR.pyxml?=	../../textproc/pyxml
