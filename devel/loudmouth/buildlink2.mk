f6c036811afa0dac0093dbf4904ad77afb7d1940
xtraeme <xtraeme@pkgsrc.org>
2004-01-23 09:45:51 +0000

    Update loudmouth to 0.15.1
    Changes in 0.15.1:
    ------------------
    * Require GnuTLS >= 1.0.0
    
    Changes in 0.15:
    ----------------
    * Better Win32 instructions.
    * Fixed bug when disconnecting and then connecting again using the same
      LmConnection.
    * Documentation typos and errors fixed by Ross Burton.
    * Better SSL support, now gives the application the possibility to decide
      if it wants to connect on bogus SSL certificates.
    * The SSL API changed a bit, take a look at the API docs.
    * Fixed timestamp error on FreeBSD.

--- devel/loudmouth/buildlink2.mk
+++ devel/loudmouth/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/12/16 07:48:19 recht Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2004/01/23 09:45:51 xtraeme Exp $
 #
 # This Makefile fragment is included by packages that use loudmouth.
 #
@@ -15,13 +15,14 @@ BUILDLINK_PKGSRCDIR.loudmouth?=		../../devel/loudmouth
 EVAL_PREFIX+=	BUILDLINK_PREFIX.loudmouth=loudmouth
 BUILDLINK_PREFIX.loudmouth_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.loudmouth+=	include/loudmouth-1.0/loudmouth/*.h
-BUILDLINK_FILES.loudmouth+=	lib/libloudmouth.*
+BUILDLINK_FILES.loudmouth+=	lib/libloudmouth*
 BUILDLINK_FILES.loudmouth+=	lib/pkgconfig/loudmouth-1.0.pc
 
 .include "../../devel/glib2/buildlink2.mk"
 .include "../../devel/pkgconfig/buildlink2.mk"
 .include "../../converters/libiconv/buildlink2.mk"
 .if defined(LOUDMOUTH_USE_SSL) && !empty(LOUDMOUTH_USE_SSL:M[Yy][Ee][Ss])
+BUILDLINK_DEPENDS.gnutls=	gnutls>=1.0.0
 .include "../../security/gnutls/buildlink2.mk"
 .endif
 
