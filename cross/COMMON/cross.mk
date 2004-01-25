ac3294f043085679ec07be301cc15edbe4459103
grant <grant>
2004-01-25 02:36:11 +0000

    replace deprecated USE_GMAKE with USE_GNU_TOOLS+=make.

--- cross/COMMON/cross.mk
+++ cross/COMMON/cross.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: cross.mk,v 1.26 2003/09/30 22:46:42 kristerw Exp $
+#	$NetBSD: cross.mk,v 1.27 2004/01/25 02:36:11 grant Exp $
 
 # Shared definitions for building a cross-compile environment.
 
@@ -154,7 +154,7 @@ PLIST_PRE+=		${COMMON_DIR}/PLIST-egcs
 CROSS_DISTFILES+=	${EGCS_DISTNAME}.tar.gz ${EGCS_PATCHBUNDLE}
 MASTER_SITES+=		ftp://egcs.cygnus.com/pub/egcs/${EGCS_DISTDIR}/ \
 			${MASTER_SITE_LOCAL}
-USE_GMAKE=		yes
+USE_GNU_TOOLS+=		make
 
 CC_FOR_TARGET=		${EGCS_WRKSRC}/gcc/xgcc -B${EGCS_WRKSRC}/gcc/ ${CFLAGS_FOR_TARGET}
 CXX_FOR_TARGET=		${CC_FOR_TARGET}
