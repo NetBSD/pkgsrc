70f1bae4d90dffd350c4ca2272522f696dfa31b4
jlam <jlam@pkgsrc.org>
2004-01-25 17:15:12 +0000

    * Follow the new buildlink3 standard and append to BUILDLINK_DEPENDS.<pkg>.
    * Use BUILDLINK_{INCDIRS,LIBDIRS}.<pkg> instead of using
      BUILDLINK_{CPPFLAGS,LDFLAGS}.<pkg> since we're just dealing with adding
      directories to the various search paths.

--- lang/python15/buildlink3.mk
+++ lang/python15/buildlink3.mk
@@ -1,7 +1,6 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/01/25 14:23:39 recht Exp $
-#
+# $NetBSD: buildlink3.mk,v 1.2 2004/01/25 17:15:12 jlam Exp $
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PYTHON15_BUILDLINK3_MK:=	${PYTHON15_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
@@ -9,21 +8,17 @@ BUILDLINK_DEPENDS+=	python15
 .endif
 
 .if !empty(PYTHON15_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			python15
-BUILDLINK_DEPENDS.python15?=		python15>=1.5.2
-BUILDLINK_PKGSRCDIR.python15?=		../../lang/python15
+BUILDLINK_PACKAGES+=		python15
+BUILDLINK_DEPENDS.python15+=	python15>=1.5.2
+BUILDLINK_PKGSRCDIR.python15?=	../../lang/python15
 
-.if defined(BUILDLINK_DEPMETHOD.python)
+.  if defined(BUILDLINK_DEPMETHOD.python)
 BUILDLINK_DEPMETHOD.python15?=	${BUILDLINK_DEPMETHOD.python}
-.endif
+.  endif
 
+BUILDLINK_INCDIRS.python15=	include/python1.5
+BUILDLINK_LIBDIRS.python15=	lib/python1.5/config
 BUILDLINK_TRANSFORM+=		l:python:python1.5
-
-BUILDLINK_CPPFLAGS.python15+= \
-	-I${BUILDLINK_PREFIX.python15}/include/python1.5
-BUILDLINK_LDFLAGS.python15+= \
-	-L${BUILDLINK_PREFIX.python15}/lib/python1.5/config		\
-	-Wl,-R${BUILDLINK_PREFIX.python15}/lib/python1.5/config
 
 .endif # PYTHON15_BUILDLINK3_MK
 
