85656b82105e72a3aa4f9845a946e9ee129318b2
jmmv <jmmv@pkgsrc.org>
2004-01-26 12:44:05 +0000

    Move mjpegtools to the multimedia category (coming from graphics). This is version 1.6.1.93.

---
+++ multimedia/mjpegtools/buildlink2.mk
@@ -0,0 +1,35 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2004/01/26 12:44:06 jmmv Exp $
+#
+# This Makefile fragment is included by packages that use mjpegtools.
+#
+# This file was created automatically using createbuildlink 2.6.
+#
+
+.if !defined(MJPEGTOOLS_BUILDLINK2_MK)
+MJPEGTOOLS_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			mjpegtools
+BUILDLINK_DEPENDS.mjpegtools?=		mjpegtools>=1.6.1.90nb3
+BUILDLINK_PKGSRCDIR.mjpegtools?=		../../graphics/mjpegtools
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.mjpegtools=mjpegtools
+BUILDLINK_PREFIX.mjpegtools_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.mjpegtools+=	include/mjpegtools/*
+BUILDLINK_FILES.mjpegtools+=	include/mjpegtools/mpeg2enc/*
+BUILDLINK_FILES.mjpegtools+=	include/mjpegtools/mplex/*
+BUILDLINK_FILES.mjpegtools+=	lib/liblavfile-1.6.*
+BUILDLINK_FILES.mjpegtools+=	lib/liblavfile.*
+BUILDLINK_FILES.mjpegtools+=	lib/liblavjpeg-1.6.*
+BUILDLINK_FILES.mjpegtools+=	lib/liblavjpeg.*
+BUILDLINK_FILES.mjpegtools+=	lib/liblavplay-1.6.*
+BUILDLINK_FILES.mjpegtools+=	lib/liblavplay.*
+BUILDLINK_FILES.mjpegtools+=	lib/libmjpegutils.*
+BUILDLINK_FILES.mjpegtools+=	lib/pkgconfig/mjpegtools.pc
+
+.include "../../graphics/jpeg/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	mjpegtools-buildlink
+
+mjpegtools-buildlink: _BUILDLINK_USE
+
+.endif	# MJPEGTOOLS_BUILDLINK2_MK
