c664dc44d1371ebeddd736f6687d04e5a6b7a597
agc <agc>
2004-01-27 16:11:47 +0000

    Replace all occurrences of the "MAIL" definition with MAIL_CMD, since some shells can set MAIL to be the mailbox of the user, and environment variables will override assignments in make when using conditional assignments.

--- mk/defs.NetBSD.mk
+++ mk/defs.NetBSD.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.NetBSD.mk,v 1.58 2003/12/16 23:07:42 heinz Exp $
+# $NetBSD: defs.NetBSD.mk,v 1.59 2004/01/27 16:11:47 agc Exp $
 #
 # Variable definitions for the NetBSD operating system.
 
@@ -43,7 +43,7 @@ LDCONFIG?=	/sbin/ldconfig
 LN?=		/bin/ln
 LS?=		/bin/ls
 M4?=		/usr/bin/m4
-MAIL?=		/usr/bin/mail
+MAIL_CMD?=	/usr/bin/mail
 MKDIR?=		/bin/mkdir -p
 MTREE?=		/usr/sbin/mtree
 MV?=		/bin/mv
