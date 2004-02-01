b54d683461efa62a97667337f834e08ab56549f5
jlam <jlam@pkgsrc.org>
2004-02-01 10:09:26 +0000

    Append to BUILDLINK_DEPENDS.<pkg>.

--- graphics/gle/buildlink3.mk
+++ graphics/gle/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/01 10:04:12 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/01 10:09:26 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GLE_BUILDLINK3_MK:=	${GLE_BUILDLINK3_MK}+
@@ -9,7 +9,7 @@ BUILDLINK_DEPENDS+=	gle
 
 .if !empty(GLE_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=		gle
-BUILDLINK_DEPENDS.gle?=		gle>=3.0.3
+BUILDLINK_DEPENDS.gle+=		gle>=3.0.3
 BUILDLINK_PKGSRCDIR.gle?=	../../graphics/gle
 
 .  include "../../graphics/Mesa/buildlink3.mk"
