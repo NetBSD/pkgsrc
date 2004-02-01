05a06c15636740e58b11268ec3b3d2836f40789f
jlam <jlam>
2004-02-01 10:04:12 +0000

    bl3ify

---
+++ graphics/gle/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/01 10:04:12 jlam Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GLE_BUILDLINK3_MK:=	${GLE_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gle
+.endif
+
+.if !empty(GLE_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=		gle
+BUILDLINK_DEPENDS.gle?=		gle>=3.0.3
+BUILDLINK_PKGSRCDIR.gle?=	../../graphics/gle
+
+.  include "../../graphics/Mesa/buildlink3.mk"
+.endif # GLE_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
