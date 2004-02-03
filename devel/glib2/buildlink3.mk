15ccdc739fae3e705c58af49d6887896ae57c3fb
recht <recht@pkgsrc.org>
2004-02-03 10:49:53 +0000

    Add buildlink3.mk. (needed to bl3ify lang/mono) ok'ed by xtraeme@

---
+++ devel/glib2/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/03 10:49:53 recht Exp $
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GLIB2_BUILDLINK3_MK:=	${GLIB2_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	glib2
+.endif
+
+.if !empty(GLIB2_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			glib2
+BUILDLINK_DEPENDS.glib2?=		glib2>=2.2.3nb1
+BUILDLINK_PKGSRCDIR.glib2?=		../../devel/glib2
+
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
+
+.endif # GLIB2_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
