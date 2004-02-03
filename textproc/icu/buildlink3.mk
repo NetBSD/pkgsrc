8ae29298bbf72b131a0d5a3f15988586593fb19f
recht <recht@pkgsrc.org>
2004-02-03 10:44:28 +0000

    Add buildlink3.mk. (needed to bl3ify lang/mono)

---
+++ textproc/icu/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/03 10:44:28 recht Exp $
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+ICU_BUILDLINK3_MK:=	${ICU_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	icu
+.endif
+
+.if !empty(ICU_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			icu
+BUILDLINK_DEPENDS.icu?=		icu>=2.6.1
+BUILDLINK_PKGSRCDIR.icu?=		../../textproc/icu
+
+.endif # ICU_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
