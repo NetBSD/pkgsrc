3279e3a39a1119c1117ada54b4240f5736764d29
jmmv <jmmv>
2004-02-04 17:12:09 +0000

    Update to 0.15:
         Fifteenth alpha version, released on 2004/02/04.
    
         o   Finally dropped bt_make and bt_wrap, together with all compatibility
             code introduced around them.
    
         o   M4 is no longer needed.  It was introduced to clean up code, but it
             has proven to only ofuscate it.  As a side effect, bt_swcgen uses
             shell scripts instead of macro files to generate system wide configu-
             ration.
    
         o   Added some checks to bt_config: bt_check_hdr_time(),
             bt_check_prog_cc_flag() and bt_check_prog_cxx_flag().
    
         o   Support for the `+=' operator (append text to a variable) was added
             to bt_sh.  This can be used to simplify syntax when writting scripts.
    
         o   Support for pkgconfig files was added to bt_pkgflags.
    
         o   Support for pkgconfig command line syntax was added to bt_pkgflags
             (it is not exactly the same, but we now support operators in their
             real form, like `=' or `!=', plus spaces to separate keywords).
    
         o   Multiple portability problems fixed.

--- devel/buildtool/buildlink2.mk
+++ devel/buildtool/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.4 2003/09/05 18:50:33 jmmv Exp $
+# $NetBSD: buildlink2.mk,v 1.5 2004/02/04 17:12:09 jmmv Exp $
 
 .if !defined(BUILDTOOL_BUILDLINK2_MK)
 BUILDTOOL_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		buildtool
-BUILDLINK_DEPENDS.buildtool?=	buildtool>=0.14
+BUILDLINK_DEPENDS.buildtool?=	buildtool>=0.15
 BUILDLINK_PKGSRCDIR.buildtool?=	../../devel/buildtool
 BUILDLINK_DEPMETHOD.buildtool?=	build
 
