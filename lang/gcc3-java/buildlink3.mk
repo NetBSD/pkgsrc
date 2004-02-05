eee7fe372c00086f3b6472f4c4f8b26cf0412b68
jlam <jlam@pkgsrc.org>
2004-02-05 00:28:35 +0000

    List the correct directories for finding GCC libraries so that they will be added automatically to library and run-time library search paths.

--- lang/gcc3-java/buildlink3.mk
+++ lang/gcc3-java/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/01 14:05:47 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/05 00:28:35 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GCC3JAVA_BUILDLINK3_MK:=	${GCC3JAVA_BUILDLINK3_MK}+
@@ -13,6 +13,8 @@ BUILDLINK_PACKAGES+=		gcc3java
 BUILDLINK_DEPENDS+=		gcc3java
 BUILDLINK_DEPENDS.gcc3java+=	gcc3${GCC3_PKGMODIF}-java>=${_GCC_REQD}
 BUILDLINK_PKGSRCDIR.gcc3java?=	../../lang/gcc3-java
+BUILDLINK_LIBDIRS.gcc3java?=	\
+	lib ${_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc3java}\///}
 
 # Packages that link against shared libraries need a full dependency.
 .  if defined(USE_GCC_SHLIB)
