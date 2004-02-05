99834df6414218ced36f44a0dc09e6082461596e
jlam <jlam>
2004-02-05 03:45:13 +0000

    bsd.compiler.mk no longer adds dependencies or includes buildlink*.mk files if it's being included by bsd.prefs.mk, so we can safely move the BUILDLINK_DEPENDS line to its usual location.

--- lang/gcc3-c/buildlink3.mk
+++ lang/gcc3-c/buildlink3.mk
@@ -1,16 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/02/05 00:28:35 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/02/05 03:45:13 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GCC3C_BUILDLINK3_MK:=	${GCC3C_BUILDLINK3_MK}+
 
 .include "../../mk/bsd.prefs.mk"
 
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gcc3c
+.endif
+
 .if !empty(GCC3C_BUILDLINK3_MK:M+)
 .  if defined(GCC3_INSTALLTO_SUBPREFIX)
 GCC3_PKGMODIF=			_${GCC3_INSTALLTO_SUBPREFIX}
 .  endif
 BUILDLINK_PACKAGES+=		gcc3c
-BUILDLINK_DEPENDS+=		gcc3c
 BUILDLINK_DEPENDS.gcc3c+=	gcc3${GCC3_PKGMODIF}-c>=${_GCC_REQD}
 BUILDLINK_PKGSRCDIR.gcc3c?=	../../lang/gcc3-c
 BUILDLINK_LIBDIRS.gcc3c?=	\
