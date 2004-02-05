5edf1bbd520f174ff09090b6535a89ad7bb09050
markd <markd@pkgsrc.org>
2004-02-05 01:32:47 +0000

    Update to KDE 3.2
    Changes:
    Graphics
    
    
    KPDF
    * NEW IN KDE: PDF viewer based on XPDF Christophe Devriese
    
    KSVG
    * NEW IN KDE: Scalable Vector Graphics plugins for Konqueror Nikolas
      Zimmermann
    
    KFilePlugins
    * pcx: Displays width, height, bpp, dpi for PCX images Nadeem Hasan
    * dvi: show basic information Stefan Kebekus
    * pnm: Display format, dimension, bpp and comments of PBM, PGM and PPM
      images. Volker Krause
    
    KDVI
    * Function to embed external PostScript files into a DVI file Stefan Kebekus
    * support for colored fonts Stefan Kebekus
    * KDVI can now use Type1 and TrueType fonts in addition to PK
      fonts. This reduces waiting times for font generation to a minimum,
      and give easier access to fonts used in Asian languages Stefan Kebekus
    * massive performance improvement Stefan Kebekus
    * support for papersize specials Stefan Kebekus
    * usability enhancements (better statusbar display, reload button, 'read-up'
      feature, etc.) Stefan Kebekus
    
    kviewshell
    * FullScreen mode Stefan Kebekus
    * more intelligent handling of paper sizes/orientations Stefan Kebekus
    * usability enhancements Stefan Kebekus
    
    KView
    * autoscroll the image when creating a selection Matthias Kretz
    * drag and drop support Matthias Kretz
    * some basic image effects as a new plugin Matthias Kretz
    * open images from stdin Matthias Kretz
    
    KSnapshot
    * Window snapshots optionally don't include window decorations Lubos Lunak
    * Region snapshots. User interface makeover. Nadeem Hasan
    
    KGamma
    * NEW IN KDE: A KControl module for monitor gamma correction. Michael
      v.Ostheim
    
    KPovModeler
    * Basic plugin framework Andreas Zehender
    * The user is asked if pending changes should be saved before rendering
      Andreas Zehender
    * Control point selection in the properties view for bicubic patch, sor,
      lathe and prism Andreas Zehender
    * Export flag for graphical objects and the camera Andreas Zehender
    * Height field view structure Leon Pennington
    * Light object gained, parallel, circular and orient options Leon
      Pennington
    * Dispersion options are supported for interior Leon Pennington
    * Support for POV-Ray 3.5 noise generators Leon Pennington
    * New POV-Ray 3.5 warp types Leon Pennington
    * New POV-Ray 3.5 objects: isosurface, projected through, radiosity,
      global photons, photons, light groups, interior texture, mesh Leon
      Pennington, Andreas Zehender
    
    KGhostView
    * Thumbnail generation for all pages Albert Astals Cid, Luis Pedro
      Coelho

--- graphics/kdegraphics3/buildlink2.mk
+++ graphics/kdegraphics3/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.11 2004/01/03 18:49:43 reed Exp $
+# $NetBSD: buildlink2.mk,v 1.12 2004/02/05 01:32:47 markd Exp $
 #
 # This Makefile fragment is included by packages that use kdegraphics.
 #
@@ -7,29 +7,76 @@
 KDEGRAPHICS_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			kdegraphics
-BUILDLINK_DEPENDS.kdegraphics?=		kdegraphics>=3.1.4nb1
+BUILDLINK_DEPENDS.kdegraphics?=		kdegraphics>=3.2.0
 BUILDLINK_PKGSRCDIR.kdegraphics?=		../../graphics/kdegraphics3
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.kdegraphics=kdegraphics
 BUILDLINK_PREFIX.kdegraphics_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.kdegraphics+=	include/devselector.h
+BUILDLINK_FILES.kdegraphics+=	include/dispgamma.h
+BUILDLINK_FILES.kdegraphics+=	include/gammadialog.h
+BUILDLINK_FILES.kdegraphics+=	include/img_canvas.h
+BUILDLINK_FILES.kdegraphics+=	include/imgscaledialog.h
+BUILDLINK_FILES.kdegraphics+=	include/kgammatable.h
 BUILDLINK_FILES.kdegraphics+=	include/kmultipageInterface.h
-BUILDLINK_FILES.kdegraphics+=	lib/kcolorchooser.*
+BUILDLINK_FILES.kdegraphics+=	include/kscandevice.h
+BUILDLINK_FILES.kdegraphics+=	include/kscanoption.h
+BUILDLINK_FILES.kdegraphics+=	include/kscanoptset.h
+BUILDLINK_FILES.kdegraphics+=	include/kscanslider.h
+BUILDLINK_FILES.kdegraphics+=	include/massscandialog.h
+BUILDLINK_FILES.kdegraphics+=	include/previewer.h
+BUILDLINK_FILES.kdegraphics+=	include/scanparams.h
+BUILDLINK_FILES.kdegraphics+=	include/scansourcedialog.h
+BUILDLINK_FILES.kdegraphics+=	include/sizeindicator.h
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kcm_kgamma.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kcm_kmrml.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kcm_kviewcanvasconfig.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kcm_kviewgeneralconfig.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kcm_kviewpluginsconfig.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kcm_kviewpresenterconfig.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kcm_kviewviewerpluginsconfig.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kded_daemonwatcher.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kdvipart.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kfaxpart.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kfile_bmp.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kfile_dvi.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kfile_ico.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kfile_jpeg.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kfile_pcx.*
 BUILDLINK_FILES.kdegraphics+=	lib/kde3/kfile_pdf.*
 BUILDLINK_FILES.kdegraphics+=	lib/kde3/kfile_png.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kfile_pnm.*
 BUILDLINK_FILES.kdegraphics+=	lib/kde3/kfile_ps.*
-BUILDLINK_FILES.kdegraphics+=	lib/kuickshow.*
-BUILDLINK_FILES.kdegraphics+=	lib/kview.*
-BUILDLINK_FILES.kdegraphics+=	lib/libkdvi.*
-BUILDLINK_FILES.kdegraphics+=	lib/libkfax.*
-BUILDLINK_FILES.kdegraphics+=	lib/libkghostview.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kfile_tga.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kfile_tiff.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kfile_xbm.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kio_mrml.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kuickshow.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kview.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kview_browserplugin.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kview_effectsplugin.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kview_presenterplugin.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kview_scannerplugin.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/kviewerpart.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/libkghostviewpart.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/libkmrmlpart.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/libkpdfpart.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/libkpovmodelerpart.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/libkviewcanvas.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/libkviewviewer.*
+BUILDLINK_FILES.kdegraphics+=	lib/kde3/mrmlsearch.*
+BUILDLINK_FILES.kdegraphics+=	lib/libkdeinit_kuickshow.*
+BUILDLINK_FILES.kdegraphics+=	lib/libkdeinit_kview.*
+BUILDLINK_FILES.kdegraphics+=	lib/libkdeinit_mrmlsearch.*
+BUILDLINK_FILES.kdegraphics+=	lib/libkimageviewer.*
 BUILDLINK_FILES.kdegraphics+=	lib/libkmultipage.*
-BUILDLINK_FILES.kdegraphics+=	lib/libkpagetest.*
-BUILDLINK_FILES.kdegraphics+=	lib/libkuickshow_main.*
-BUILDLINK_FILES.kdegraphics+=	lib/libkview_main.*
-BUILDLINK_FILES.kdegraphics+=	lib/libkviewerpart.*
-BUILDLINK_FILES.kdegraphics+=	lib/libkviewpart.*
+BUILDLINK_FILES.kdegraphics+=	lib/libkpovmodeler.*
+BUILDLINK_FILES.kdegraphics+=	lib/libkscan.*
 
+.include "../../graphics/tiff/buildlink2.mk"
+.include "../../graphics/glut/buildlink2.mk"
 .include "../../graphics/imlib/buildlink2.mk"
+.include "../../graphics/sane-backends/buildlink2.mk"
 .include "../../meta-pkgs/kde3/buildlink2.mk"
 .include "../../x11/kdebase3/buildlink2.mk"
 
