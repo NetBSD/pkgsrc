57a48d9587db33980ce281df02987195870cf100
markd <markd>
2004-02-05 08:27:59 +0000

    Update to KDE 3.2
    Changes:
    SDK
    
    KUIViewer
    * NEW IN KDE: KUIViewer part that allows the preview of UI files. Ian
      Reinhart Geiser
    
    UI Thumbnail
    * NEW IN KDE: Thumbnail preview for UI files. Ian Reinhart Geiser
    
    KCachegrind
    * NEW IN KDE: KCachegrind, a profile data visualisation. The data is
      generated with the Valgrind derived tool Calltree (see homepage) Josef
      Weidendorfer
    
    Cervisia
    * Login support for pserver cvs servers Christian Loose
    * DCOP service that provides easy access to CVS functions (cvsservice)
      Christian Loose
    * Optimize and improve Cervisia's file and directory view (UpdateView)
      Andre Woebbeking
    * Better SSH support. Add ability to start and use ssh-agent.
      Christian Loose
    * More KDE standard compliant dialogs (QDialog -> KDialogBase) Andre
      Woebbeking
    * Remember last opened directory in "Open Sandbox..." dialog Christian
      Loose
    * Add new command line option (-log) to display a log dialog for a
      single file without starting Cervisia Christian Loose
    * Add "diff to HEAD" functionality to display changes made by other
      developers before updating the local working copy Andre Woebbeking
    * Make list views configurable (column order/widths, sorting) Andre
      Woebbeking
    * Add "Hide Empty Directories" functionality to display only
      directories which contain any "changes" Andre Woebbeking
    * Use user's settings (locale and timezone) to display dates and times
      Andre Woebbeking
    * Add splitters to resolve dialog Christian Loose
    * Add new view to log dialog that shows the cvs output in a similar
      form as the command-line client. Christian Loose
    
    KFilePlugins
    * ts: Displays the number of translated, untranslated and obsolete
      messages of Qt-Linguist files kfile_ts.tar.bz2 Carsten Niehaus
    * diffAdded support for recognizing Subversion diff output. Otto
      Bruggeman
    
    KBabel
    * Catalog Manager caching Stefan Asserhall
    * Bookmarks Marco Wegner
    * TMX dictionary plugin Stanislav Visnovsky
    * Highlighting code rewrite Marco Wegner
    * CVS integration in Catalog Manager Marco Wegner
    * GNU gettext plural forms Stanislav Visnovsky
    * Validation plugin-based tools Stanislav Visnovsky
    * Create plural form translation from single text Stanislav Visnovsky
    * Marking using mouse in Catalog Manager Stanislav Visnovsky
    * RegExp marking in Catalog Manager Marco Wegner
    * Plugin-based file import/export Stanislav Visnovsky
    * Autosave feature Marco Wegner
    * Whitespace translations validation plugin Dwayne Bailey
    * Translation length validation plugin Dwayne Bailey
    * Translations still in English validation plugin Dwayne Bailey
    * Qt Linguist import/export plugin Marco Wegner
    
    Kompare
    * Fixed some problems with the interaction between the navigation part
      and the komparepart, mostly preventing double emitted signals so it
      got a bit faster. Otto Bruggeman
    * Added the ability to load a file and diff output and to patch the
      file. Otto Bruggeman
    * A lot of internal cleanups so maintenance will be easier. Otto
      Bruggeman
    * Added an interface class/library you can link your application against
      to expose the extra methods the komparepart implements over a standard
      KPart. This is not guaranteed to be binary compatible yet. I might
      need to add support for the encoding in the interface. Otto Bruggeman
    
    Umbrello
    * NEW IN KDE: Umbrello UML Modeller Umbrello Developers
    * refactoring agent Umbrello Developers
    * improved code export Umbrello Developers
    * changing properties of multiple items at one time Sebastian Stein
    * refactor internal code (Widget and WidgetData classes) Oliver Kellogg
    * Add Datatype widget and internal representation Umbrello Developers

--- devel/kdesdk3/buildlink2.mk
+++ devel/kdesdk3/buildlink2.mk
@@ -1,38 +1,59 @@
-# $NetBSD: buildlink2.mk,v 1.9 2003/12/08 14:41:39 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.10 2004/02/05 08:27:59 markd Exp $
 #
 # This Makefile fragment is included by packages that use kdesdk.
 #
-# This file was created automatically using createbuildlink 2.4.
+# This file was created automatically using createbuildlink-3.0.
 #
 
 .if !defined(KDESDK_BUILDLINK2_MK)
 KDESDK_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			kdesdk
-BUILDLINK_DEPENDS.kdesdk?=		kdesdk>=3.1.4nb1
+BUILDLINK_DEPENDS.kdesdk?=		kdesdk>=3.2.0
 BUILDLINK_PKGSRCDIR.kdesdk?=		../../devel/kdesdk3
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.kdesdk=kdesdk
 BUILDLINK_PREFIX.kdesdk_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.kdesdk+=	include/cvsjob_stub.h
+BUILDLINK_FILES.kdesdk+=	include/cvsservice_stub.h
 BUILDLINK_FILES.kdesdk+=	include/kbabel/*
 BUILDLINK_FILES.kdesdk+=	include/kprofilemethod.h
 BUILDLINK_FILES.kdesdk+=	include/kspy.h
+BUILDLINK_FILES.kdesdk+=	include/repository_stub.h
 BUILDLINK_FILES.kdesdk+=	lib/kde3/kabcformat_kdeaccounts.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_accelstool.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_argstool.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_contexttool.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_equationstool.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_gettextexport.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_gettextimport.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_lengthtool.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_linguistexport.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_linguistimport.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_nottranslatedtool.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_pluraltool.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_punctuationtool.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_setfuzzytool.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_whitespacetool.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabel_xmltool.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabeldict_poauxiliary.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabeldict_pocompendium.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kbabeldict_tmxcompendium.*
 BUILDLINK_FILES.kdesdk+=	lib/kde3/kfile_cpp.*
 BUILDLINK_FILES.kdesdk+=	lib/kde3/kfile_diff.*
 BUILDLINK_FILES.kdesdk+=	lib/kde3/kfile_po.*
-BUILDLINK_FILES.kdesdk+=	lib/kde3/libdbsearchengine.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/kfile_ts.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/libcervisiapart.*
 BUILDLINK_FILES.kdesdk+=	lib/kde3/libkomparenavtreepart.*
 BUILDLINK_FILES.kdesdk+=	lib/kde3/libkomparepart.*
-BUILDLINK_FILES.kdesdk+=	lib/kde3/libpoauxiliary.*
-BUILDLINK_FILES.kdesdk+=	lib/kde3/libpocompendium.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/libkuiviewerpart.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/plugins/styles/scheck.*
 BUILDLINK_FILES.kdesdk+=	lib/kde3/pothumbnail.*
-BUILDLINK_FILES.kdesdk+=	lib/libcatalogmanager.*
-BUILDLINK_FILES.kdesdk+=	lib/libcervisia.*
-BUILDLINK_FILES.kdesdk+=	lib/libkbabel.*
+BUILDLINK_FILES.kdesdk+=	lib/kde3/quithumbnail.*
+BUILDLINK_FILES.kdesdk+=	lib/libcvsservice.*
 BUILDLINK_FILES.kdesdk+=	lib/libkbabelcommon.*
-BUILDLINK_FILES.kdesdk+=	lib/libkbabeldict.*
 BUILDLINK_FILES.kdesdk+=	lib/libkbabeldictplugin.*
+BUILDLINK_FILES.kdesdk+=	lib/libkompareinterface.*
 BUILDLINK_FILES.kdesdk+=	lib/libkspy.*
 
 .include "../../databases/db/buildlink2.mk"
