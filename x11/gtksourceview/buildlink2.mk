b0c3bc051345400a26cd25c90bdbafbe0767574b
jmmv <jmmv@pkgsrc.org>
2004-02-07 15:21:43 +0000

    Under a system without GNU regex, this package builds an internal copy of it.  This copy defines the standard regcomp and friends functions, which are also present in NetBSD.  The problem is that the copy gets linked into the libgtksourceview shared library and, when loaded into memory, shadows native regex functions.
    The problem exposes itself at least in gedit through gnome-vfs2.  The later
    executes regcomp, expecting the native one to be called, but instead the
    one from the libgtksourceview library appears, making the program crash.
    
    Apply a patch to ensure these internal GNU regex functions do not colish
    with native ones.  Bump PKGREVISION to 8.

--- x11/gtksourceview/buildlink2.mk
+++ x11/gtksourceview/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.5 2004/01/03 18:49:57 reed Exp $
+# $NetBSD: buildlink2.mk,v 1.6 2004/02/07 15:21:43 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use gtksourceview.
 #
@@ -9,7 +9,7 @@
 GTKSOURCEVIEW_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gtksourceview
-BUILDLINK_DEPENDS.gtksourceview?=		gtksourceview>=0.7.0nb5
+BUILDLINK_DEPENDS.gtksourceview?=		gtksourceview>=0.7.0nb8
 BUILDLINK_PKGSRCDIR.gtksourceview?=		../../x11/gtksourceview
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gtksourceview=gtksourceview
