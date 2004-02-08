0e74dc1fb22e34114500c2b373bbd4cb449bf709
chris <chris@pkgsrc.org>
2004-02-08 17:08:13 +0000

    Add missing buildlink3 glue code for this package.

---
+++ audio/id3lib/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/08 17:08:13 chris Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+ID3LIB_BUILDLINK3_MK:=	${ID3LIB_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	id3lib
+.endif
+
+.if !empty(ID3LIB_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			id3lib
+BUILDLINK_DEPENDS.id3lib?=		id3lib>=3.8.3
+BUILDLINK_PKGSRCDIR.id3lib?=		../../audio/id3lib
+
+.include "../../devel/zlib/buildlink3.mk"
+
+.endif # ID3LIB_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
