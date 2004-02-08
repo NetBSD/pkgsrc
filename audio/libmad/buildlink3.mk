7170eada862640a56bdf0b0e1ed792e6928e8dfe
tron <tron@pkgsrc.org>
2004-02-08 16:21:44 +0000

    Correct a few errors pointed out by Chris Gilbert.

--- audio/libmad/buildlink3.mk
+++ audio/libmad/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/08 15:59:58 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/08 16:21:44 tron Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBMAD_BUILDLINK3_MK:=		${LIBMAD_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=		libmad>=0.15nb1
+BUILDLINK_DEPENDS+=	libmad
 .endif
 
 .if !empty(LIBMAD_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=		libmad
-BUILDLINK_DEPENDS.libmad+=	libmad>=0.15nb1
+BUILDLINK_DEPENDS.libmad+=	libmad>=0.15.0bnb1
 BUILDLINK_PKGSRCDIR.libmad?=	../../audio/libmad
 .endif	# LIBMAD_BUILDLINK3_MK
 
