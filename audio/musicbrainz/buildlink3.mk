ce0993ae3aa100688918760de136d55c5812ccb3
chris <chris@pkgsrc.org>
2004-02-09 01:23:43 +0000

    Correct the name of this package used in buildlink3.mk I'd created this package originally as libmusicbrainz, till someone pointed it out as musicbrainz

--- audio/musicbrainz/buildlink3.mk
+++ audio/musicbrainz/buildlink3.mk
@@ -1,20 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/09 00:07:36 chris Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/09 01:23:43 chris Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
-LIBMUSICBRAINZ_BUILDLINK3_MK:=	${LIBMUSICBRAINZ_BUILDLINK3_MK}+
+MUSICBRAINZ_BUILDLINK3_MK:=	${MUSICBRAINZ_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	libmusicbrainz
+BUILDLINK_DEPENDS+=	musicbrainz
 .endif
 
-.if !empty(LIBMUSICBRAINZ_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			libmusicbrainz
-BUILDLINK_DEPENDS.libmusicbrainz?=		libmusicbrainz>=2.0.2
-BUILDLINK_PKGSRCDIR.libmusicbrainz?=		../../audio/libmusicbrainz
+.if !empty(MUSICBRAINZ_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			musicbrainz
+BUILDLINK_DEPENDS.musicbrainz?=		musicbrainz>=2.0.2
+BUILDLINK_PKGSRCDIR.musicbrainz?=		../../audio/musicbrainz
 
 .include "../../textproc/expat/buildlink3.mk"
 .include "../../devel/pkgconfig/buildlink3.mk"
 
-.endif # LIBMUSICBRAINZ_BUILDLINK3_MK
+.endif # MUSICBRAINZ_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
