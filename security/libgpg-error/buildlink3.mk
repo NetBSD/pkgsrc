6f6b3bf689425f3e5c4d9a93bc8a17923fa77290
jlam <jlam@pkgsrc.org>
2004-02-09 20:25:09 +0000

    bl3ify

---
+++ security/libgpg-error/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/09 20:28:16 jlam Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBGPG_ERROR_BUILDLINK3_MK:=	${LIBGPG_ERROR_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libgpg-error
+.endif
+
+.if !empty(LIBGPG_ERROR_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			libgpg-error
+BUILDLINK_DEPENDS.libgpg-error?=	libgpg-error>=0.6
+BUILDLINK_PKGSRCDIR.libgpg-error?=	../../security/libgpg-error
+
+.  include "../../devel/gettext-lib/buildlink3.mk"
+.endif # LIBGPG_ERROR_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
