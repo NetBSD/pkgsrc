509d18f30a14ac823f4b78cf0ddcabc6b79c0f83
jlam <jlam>
2004-02-09 23:56:32 +0000

    BUILDLINK_DEPENDS.<pkg> lines should _always_ use += to _append_ to the existing value.  This is critical to set the correct list of dependencies for a package.

--- graphics/cairo/buildlink3.mk
+++ graphics/cairo/buildlink3.mk
@@ -1,9 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/02/03 12:16:43 recht Exp $
-#
-# This Makefile fragment is included by packages that use cairo.
-#
-# This file was created automatically using createbuildlink-3.0.
-#
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/09 23:56:32 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CAIRO_BUILDLINK3_MK:=	${CAIRO_BUILDLINK3_MK}+
@@ -13,16 +8,15 @@ BUILDLINK_DEPENDS+=	cairo
 .endif
 
 .if !empty(CAIRO_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			cairo
-BUILDLINK_DEPENDS.cairo?=		cairo>=0.1.17
-BUILDLINK_PKGSRCDIR.cairo?=		../../graphics/cairo
+BUILDLINK_PACKAGES+=		cairo
+BUILDLINK_DEPENDS.cairo+=	cairo>=0.1.17
+BUILDLINK_PKGSRCDIR.cairo?=	../../graphics/cairo
 
-BUILDLINK_DEPENDS.freetype2=  freetype2>=2.1.5
+BUILDLINK_DEPENDS.freetype2+=  freetype2>=2.1.5
 
-.include "../../graphics/freetype2/buildlink3.mk"
-.include "../../graphics/libpixman/buildlink3.mk"
-.include "../../fonts/Xft2/buildlink3.mk"
-
+.  include "../../fonts/Xft2/buildlink3.mk"
+.  include "../../graphics/freetype2/buildlink3.mk"
+.  include "../../graphics/libpixman/buildlink3.mk"
 .endif # CAIRO_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
--- lang/mono/buildlink3.mk
+++ lang/mono/buildlink3.mk
@@ -1,9 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/03 12:30:11 recht Exp $
-#
-# This Makefile fragment is included by packages that use mono.
-#
-# This file was created automatically using createbuildlink-3.0.
-#
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/09 23:56:32 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 MONO_BUILDLINK3_MK:=	${MONO_BUILDLINK3_MK}+
@@ -13,13 +8,12 @@ BUILDLINK_DEPENDS+=	mono
 .endif
 
 .if !empty(MONO_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			mono
-BUILDLINK_DEPENDS.mono?=		mono>=0.30
-BUILDLINK_PKGSRCDIR.mono?=		../../lang/mono
+BUILDLINK_PACKAGES+=		mono
+BUILDLINK_DEPENDS.mono+=	mono>=0.30
+BUILDLINK_PKGSRCDIR.mono?=	../../lang/mono
 
-.include "../../devel/glib2/buildlink3.mk"
-.include "../../textproc/icu/buildlink3.mk"
-
+.  include "../../devel/glib2/buildlink3.mk"
+.  include "../../textproc/icu/buildlink3.mk"
 .endif # MONO_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
