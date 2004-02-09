068de68fd75782a2fbed2064df3dc2ebaf3381f8
seb <seb@pkgsrc.org>
2004-02-09 12:03:27 +0000

    Update to version 1.12.
    Package changes:
    	The include files moved from $PREFIX/include/lcms to
    	$PREFIX/include. Support added to buildlink*.mk to provide
    	compatibility symlinks include/lcms/*.h in BUILDLINK_DIR.
    
    No list of changes since the previously packaged version (1.06), I do not
    even know if there was any versions in between... Anyway changes
    in version 1.12 are (from the homepage):
    
    - Brightness/Contrast/Hue/Saturation/WhitePoint modification across abstract
    profile
    - License changed to MIT
    - pseq tag handling: cmsReadProfileSequenceDescription and cmsSEQ,
    cmsPSEQDESC structures
    - CRD generation now supports black point compensation, see
    cmsGetPostScriptCRDEx
    - cmsTakeManufacturer and cmsTakeModel for uncooked info on these tags
    - Writing 8 bit profiles is now supported
    - Named color profiles support. This turns lcms from a "wide subset" into
    a "full implementation" of ICC 3.4, with some ICC 4.0 support.
    - PostScript CSA, CRD generation
    - Ink-Limiting capabilities for CMYK
    - Devicelink profile generation.
    - Gray scale virtual profiles
    - Linearization virtual device link profiles
    - New ICCLINK and ICC2PS utilities
    - SWIG wrapper. This enables lcms from Python.
    - Floating-point formats are now accepted as well.
    - More ICC 4.0 compatibility. Some 4.0 profiles are now are fully understood
    (still experimental)
    - Profiles can now be saved to memory (thanks to Steven Greaves for providing
    the code)
    - Char Target data are now handled. Some profiles does store the data
    profiler has used. This is all information needed to rebuild the profile
    from scratch.
    - New low-resolution flag cmsFLAGS_LOWRESPRECALC to save memory.
    - User-defined encodings are now supported.
    - cmsChangeBuffersFormat() to change the encoding of buffers on runtime
    allows reuse of existing transforms.
    - Gamma estimation routines cmsEstimateGamma() and cmsEstimateGammaEx()
    - multilocalized unicode is now supported. Language and codepage is
    selected via cmsSetLanguage() (ICC 4.0 only)
    - LUT handling has been enhanced with enumerators. (SAMPLER_INSPECT)
    - Improved TIFFICC, JPEGICC and ICCTRANS utilities.
    - cmsOpenProfileFromMem() no longer creates temporary files.
    - Transforms does accept now a maximum of 8 channels on input and 16 on
    output. (last version did accept 6 on input)
    - 8 <-> 16 bits per sample are now always computed accurately.
    - Some minor bugs fixed

--- graphics/lcms/buildlink2.mk
+++ graphics/lcms/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/10/31 08:00:27 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2004/02/09 12:03:27 seb Exp $
 
 .if !defined(LCMS_BUILDLINK2_MK)
 LCMS_BUILDLINK2_MK=	# defined
@@ -9,13 +9,25 @@ BUILDLINK_PKGSRCDIR.lcms?=	../../graphics/lcms
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.lcms=lcms
 BUILDLINK_PREFIX.lcms_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.lcms=	include/lcms/*
+BUILDLINK_FILES.lcms=	include/icc34.h
+BUILDLINK_FILES.lcms+=	include/lcms.h
 BUILDLINK_FILES.lcms+=	lib/liblcms.*
 
-BUILDLINK_CPPFLAGS.lcms=	-I${BUILDLINK_PREFIX.lcms}/include/lcms
+BUILDLINK_CPPFLAGS.lcms=	-I${BUILDLINK_PREFIX.lcms}/include
 
 BUILDLINK_TARGETS+=	lcms-buildlink
+BUILDLINK_TARGETS+=	buildlink-include-lcms
 
 lcms-buildlink: _BUILDLINK_USE
+
+buildlink-include-lcms: .PHONY
+.for _h_ in lcms.h icc34.h
+	${_PKG_SILENT}${_PKG_DEBUG}					\
+	if [ ! -f ${BUILDLINK_DIR}/include/lcms/${_h_} ]; then		\
+		${INSTALL_DATA_DIR} ${BUILDLINK_DIR}/include/lcms;	\
+		${ECHO_BUILDLINK_MSG} "Linking lcms/${_h_} to ${_h_} in ${BUILDLINK_DIR}/include"; \
+		${LN} -s ${BUILDLINK_PREFIX.lcms}/include/${_h_} ${BUILDLINK_DIR}/include/lcms/${_h_}; \
+	fi
+.endfor
 
 .endif	# LCMS_BUILDLINK2_MK
--- graphics/lcms/buildlink3.mk
+++ graphics/lcms/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/01/24 03:26:47 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/02/09 12:03:27 seb Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LCMS_BUILDLINK3_MK:=	${LCMS_BUILDLINK3_MK}+
@@ -11,7 +11,18 @@ BUILDLINK_DEPENDS+=	lcms
 BUILDLINK_PACKAGES+=		lcms
 BUILDLINK_DEPENDS.lcms+=	lcms>=1.06
 BUILDLINK_PKGSRCDIR.lcms?=	../../graphics/lcms
-BUILDLINK_INCDIRS.lcms?=	include/lcms
 .endif # LCMS_BUILDLINK3_MK
+
+BUILDLINK_TARGETS+=	buildlink-include-lcms
+
+buildlink-include-lcms: .PHONY
+.for _h_ in lcms.h icc34.h
+	${_PKG_SILENT}${_PKG_DEBUG}					\
+	if [ ! -f ${BUILDLINK_DIR}/include/lcms/${_h_} ]; then		\
+		${INSTALL_DATA_DIR} ${BUILDLINK_DIR}/include/lcms;	\
+		${ECHO_BUILDLINK_MSG} "Linking lcms/${_h_} to ${_h_} in ${BUILDLINK_DIR}/include"; \
+		${LN} -s ${BUILDLINK_PREFIX.lcms}/include/${_h_} ${BUILDLINK_DIR}/include/lcms/${_h_}; \
+	fi
+.endfor
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
