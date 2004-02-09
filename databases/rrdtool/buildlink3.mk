0ef17764833da359153bd3e992e4754ceb9a83d2
jlam <jlam>
2004-02-09 23:56:32 +0000

    BUILDLINK_DEPENDS.<pkg> lines should _always_ use += to _append_ to the existing value.  This is critical to set the correct list of dependencies for a package.

--- databases/rrdtool/buildlink3.mk
+++ databases/rrdtool/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/07 17:44:10 seb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/09 23:56:32 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 RRDTOOL_BUILDLINK3_MK:=	${RRDTOOL_BUILDLINK3_MK}+
@@ -8,15 +8,14 @@ BUILDLINK_DEPENDS+=	rrdtool
 .endif
 
 .if !empty(RRDTOOL_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			rrdtool
-BUILDLINK_DEPENDS.rrdtool?=		rrdtool>=1.0.40nb2
-BUILDLINK_PKGSRCDIR.rrdtool?=		../../databases/rrdtool
+BUILDLINK_PACKAGES+=		rrdtool
+BUILDLINK_DEPENDS.rrdtool+=	rrdtool>=1.0.40nb2
+BUILDLINK_PKGSRCDIR.rrdtool?=	../../databases/rrdtool
 
-.include "../../graphics/freetype-lib/buildlink3.mk"
-.include "../../graphics/jpeg/buildlink3.mk"
-.include "../../graphics/gd/buildlink3.mk"
-.include "../../www/cgilib/buildlink3.mk"
-
+.  include "../../graphics/freetype-lib/buildlink3.mk"
+.  include "../../graphics/gd/buildlink3.mk"
+.  include "../../graphics/jpeg/buildlink3.mk"
+.  include "../../www/cgilib/buildlink3.mk"
 .endif # RRDTOOL_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
--- graphics/libgdiplus/buildlink3.mk
+++ graphics/libgdiplus/buildlink3.mk
@@ -1,7 +1,6 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/02/03 12:31:35 recht Exp $
-#
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/09 23:56:32 jlam Exp $
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGDIPLUS_BUILDLINK3_MK:=	${LIBGDIPLUS_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
@@ -10,13 +9,12 @@ BUILDLINK_DEPENDS+=	libgdiplus
 
 .if !empty(LIBGDIPLUS_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=			libgdiplus
-BUILDLINK_DEPENDS.libgdiplus?=		libgdiplus>=0.1
-BUILDLINK_PKGSRCDIR.libgdiplus?=		../../graphics/libgdiplus
+BUILDLINK_DEPENDS.libgdiplus+=		libgdiplus>=0.1
+BUILDLINK_PKGSRCDIR.libgdiplus?=	../../graphics/libgdiplus
 
-.include "../../graphics/cairo/buildlink3.mk"
-.include "../../lang/mono/buildlink3.mk"
-.include "../../x11/Xrender/buildlink3.mk"
-
+.  include "../../graphics/cairo/buildlink3.mk"
+.  include "../../lang/mono/buildlink3.mk"
+.  include "../../x11/Xrender/buildlink3.mk"
 .endif # LIBGDIPLUS_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
