7ce78d03ecee0cdfce1d4977590316137c3328d0
chris <chris>
2004-02-09 00:30:24 +0000

    Initial import of TagLib 1.0.
    TagLib is a library for reading and editing the meta-data of several popular
    audio formats. Currently it supports both ID3v1 and ID3v2 for MP3 files, Ogg
    Vorbis comments and ID3 tags and Vorbis comments in FLAC files.

---
+++ audio/taglib/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/02/09 00:30:24 chris Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+TAGLIB_BUILDLINK3_MK:=	${TAGLIB_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	taglib
+.endif
+
+.if !empty(TAGLIB_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			taglib
+BUILDLINK_DEPENDS.taglib?=		taglib>=1.0
+BUILDLINK_PKGSRCDIR.taglib?=		../../audio/TagLib
+
+.endif # TAGLIB_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
