6c6e05ac5608ab721d0a67e2ff209b7c5308569d
jlam <jlam>
2004-02-09 20:25:09 +0000

    bl3ify

---
+++ archivers/liblzo/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/09 20:25:09 jlam Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBLZO_BUILDLINK3_MK:=	${LIBLZO_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	liblzo
+.endif
+
+.if !empty(LIBLZO_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=		liblzo
+BUILDLINK_DEPENDS.liblzo?=	liblzo>=1.08
+BUILDLINK_PKGSRCDIR.liblzo?=	../../archivers/liblzo
+.endif # LIBLZO_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
