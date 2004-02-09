371122db34a6615ae2fbcbff93f47c05e53ceef8
jlam <jlam@pkgsrc.org>
2004-02-09 23:56:32 +0000

    BUILDLINK_DEPENDS.<pkg> lines should _always_ use += to _append_ to the existing value.  This is critical to set the correct list of dependencies for a package.

--- graphics/OpenRM/buildlink3.mk
+++ graphics/OpenRM/buildlink3.mk
@@ -1,11 +1,6 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/02/05 20:34:17 garbled Exp $
-#
-# This Makefile fragment is included by packages that use openrm-devel.
-#
-# This file was created automatically using createbuildlink-3.0.
-#
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/09 23:56:32 jlam Exp $
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 OPENRM_DEVEL_BUILDLINK3_MK:=	${OPENRM_DEVEL_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
@@ -14,13 +9,12 @@ BUILDLINK_DEPENDS+=	openrm-devel
 
 .if !empty(OPENRM_DEVEL_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=			openrm-devel
-BUILDLINK_DEPENDS.openrm-devel?=		openrm-devel>=1.5.1
-BUILDLINK_PKGSRCDIR.openrm-devel?=		../../graphics/OpenRM
+BUILDLINK_DEPENDS.openrm-devel+=	openrm-devel>=1.5.1
+BUILDLINK_PKGSRCDIR.openrm-devel?=	../../graphics/OpenRM
 
-.include "../../graphics/jpeg/buildlink3.mk"
-.include "../../graphics/Mesa/buildlink3.mk"
-.include "../../mk/pthread.buildlink3.mk"
-
+.  include "../../graphics/jpeg/buildlink3.mk"
+.  include "../../graphics/Mesa/buildlink3.mk"
+.  include "../../mk/pthread.buildlink3.mk"
 .endif # OPENRM_DEVEL_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
