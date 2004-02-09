a54ebc8064ae6f42867728c813f6f0b6b4bd308c
jlam <jlam@pkgsrc.org>
2004-02-09 23:56:32 +0000

    BUILDLINK_DEPENDS.<pkg> lines should _always_ use += to _append_ to the existing value.  This is critical to set the correct list of dependencies for a package.

--- devel/glib2/buildlink3.mk
+++ devel/glib2/buildlink3.mk
@@ -1,5 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/03 10:49:53 recht Exp $
-#
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/09 23:56:32 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GLIB2_BUILDLINK3_MK:=	${GLIB2_BUILDLINK3_MK}+
@@ -9,13 +8,12 @@ BUILDLINK_DEPENDS+=	glib2
 .endif
 
 .if !empty(GLIB2_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			glib2
-BUILDLINK_DEPENDS.glib2?=		glib2>=2.2.3nb1
-BUILDLINK_PKGSRCDIR.glib2?=		../../devel/glib2
+BUILDLINK_PACKAGES+=		glib2
+BUILDLINK_DEPENDS.glib2+=	glib2>=2.2.3nb1
+BUILDLINK_PKGSRCDIR.glib2?=	../../devel/glib2
 
-.include "../../converters/libiconv/buildlink3.mk"
-.include "../../devel/gettext-lib/buildlink3.mk"
-
+.  include "../../converters/libiconv/buildlink3.mk"
+.  include "../../devel/gettext-lib/buildlink3.mk"
 .endif # GLIB2_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
