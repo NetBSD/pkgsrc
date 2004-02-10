4ae73715a8d6bb4034e8c18a200d584e5f1c3bf4
xtraeme <xtraeme@pkgsrc.org>
2004-02-10 02:56:00 +0000

    bl3ify

---
+++ textproc/libxslt/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/10 02:56:00 xtraeme Exp $
+#
+# This Makefile fragment is included by packages that use libxslt.
+#
+# This file was created automatically using createbuildlink-3.0.
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBXSLT_BUILDLINK3_MK:=	${LIBXSLT_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libxslt
+.endif
+
+.if !empty(LIBXSLT_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			libxslt
+BUILDLINK_DEPENDS.libxslt+=		libxslt>=1.1.2nb1
+BUILDLINK_PKGSRCDIR.libxslt?=		../../textproc/libxslt
+
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../lang/perl5/buildlink3.mk"
+
+.endif # LIBXSLT_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
