1c33d171f5449eff503b085f06ebc1e954fd3f5e
wiz <wiz@pkgsrc.org>
2004-02-10 10:07:24 +0000

    Adapt for PKGNAME change.

--- graphics/OpenRM/buildlink3.mk
+++ graphics/OpenRM/buildlink3.mk
@@ -1,20 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/02/09 23:56:32 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/02/10 10:07:24 wiz Exp $
 
-BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-OPENRM_DEVEL_BUILDLINK3_MK:=	${OPENRM_DEVEL_BUILDLINK3_MK}+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+OPENRM_BUILDLINK3_MK:=			${OPENRM_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	openrm-devel
+BUILDLINK_DEPENDS+=			OpenRM
 .endif
 
-.if !empty(OPENRM_DEVEL_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			openrm-devel
-BUILDLINK_DEPENDS.openrm-devel+=	openrm-devel>=1.5.1
-BUILDLINK_PKGSRCDIR.openrm-devel?=	../../graphics/OpenRM
+.if !empty(OPENRM_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			OpenRM
+BUILDLINK_DEPENDS.OpenRM+=		OpenRM>=1.5.1
+BUILDLINK_PKGSRCDIR.OpenRM?=		../../graphics/OpenRM
 
 .  include "../../graphics/jpeg/buildlink3.mk"
 .  include "../../graphics/Mesa/buildlink3.mk"
 .  include "../../mk/pthread.buildlink3.mk"
-.endif # OPENRM_DEVEL_BUILDLINK3_MK
+.endif # OPENRM_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
