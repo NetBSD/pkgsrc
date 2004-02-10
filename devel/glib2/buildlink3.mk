b04cb645495f0ad5b381422ac48213568325d708
jlam <jlam@pkgsrc.org>
2004-02-10 20:15:09 +0000

    The BUILDLINK_DEPENDS.<pkg> should match the one in the buildlink2.mk file if it exists.

--- devel/glib2/buildlink3.mk
+++ devel/glib2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/02/09 23:56:32 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/02/10 20:15:09 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GLIB2_BUILDLINK3_MK:=	${GLIB2_BUILDLINK3_MK}+
@@ -9,7 +9,7 @@ BUILDLINK_DEPENDS+=	glib2
 
 .if !empty(GLIB2_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=		glib2
-BUILDLINK_DEPENDS.glib2+=	glib2>=2.2.3nb1
+BUILDLINK_DEPENDS.glib2+=	glib2>=2.2.2nb1
 BUILDLINK_PKGSRCDIR.glib2?=	../../devel/glib2
 
 .  include "../../converters/libiconv/buildlink3.mk"
