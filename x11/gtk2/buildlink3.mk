8f5a15435de472250a38f8cf2c0f2f3f75e62e49
xtraeme <xtraeme>
2004-02-10 02:04:54 +0000

    bl3ify

---
+++ x11/gtk2/buildlink3.mk
@@ -0,0 +1,31 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/10 02:04:54 xtraeme Exp $
+#
+# This Makefile fragment is included by packages that use gtk2+.
+#
+# This file was created automatically using createbuildlink-3.0.
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GTK2+_BUILDLINK3_MK:=	${GTK2+_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gtk2+
+.endif
+
+.if !empty(GTK2+_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			gtk2+
+BUILDLINK_DEPENDS.gtk2+?=		gtk2+>=2.2.4nb3
+BUILDLINK_PKGSRCDIR.gtk2+?=		../../x11/gtk2
+
+.include "../../devel/atk/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+
+.endif # GTK2+_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
