04040c4c082c81508172f9372bd7925dab512be7
jlam <jlam>
2004-02-10 02:18:04 +0000

    Turn PERL5_REQD into a list of values, the highest of which will be used to choose the correct Perl dependency.

--- lang/libperl/buildlink3.mk
+++ lang/libperl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/01/24 03:26:47 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/02/10 02:18:04 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBPERL_BUILDLINK3_MK:=	${LIBPERL_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=		libperl
 BUILDLINK_DEPENDS.libperl+=	{libperl>=${LIBPERL5_REQD},perl>=5.8.0}
 BUILDLINK_PKGSRCDIR.libperl?=	../../lang/libperl
 
-LIBPERL5_REQD?=		${PERL5_REQD}
+LIBPERL5_REQD?=		${_PERL5_REQD}
 
 .  include "../../lang/perl5/buildlink3.mk"
 .endif	# LIBPERL_BUILDLINK3_MK
