818e0987cbda285396cb80e09708e0c4186d34e3
jlam <jlam@pkgsrc.org>
2004-02-11 02:03:41 +0000

    Turn MESA_REQD back into a public variable, and make it take a list of values that MesaLib/buildlink3.mk converts into a single _MESA_REQD that is the highest version required.  If including the Mesa/buildlink3.mk file, then require the same version of Mesa as required by glut.

--- graphics/Mesa/buildlink3.mk
+++ graphics/Mesa/buildlink3.mk
@@ -1,12 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/01 10:01:08 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/11 02:03:41 jlam Exp $
 
 .if !defined(MESA_BUILDLINK3_MK)
 MESA_BUILDLINK3_MK=	defined
 
 BUILDLINK_PREFIX.Mesa=	${BUILDLINK_PREFIX.MesaLib}
 
-.include "../../graphics/MesaLib/buildlink3.mk"
-.include "../../graphics/glu/buildlink3.mk"
+# The ordering is important here as glut/buildlink3.mk sets the version
+# of Mesa required.
+#
 .include "../../graphics/glut/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
 
 .endif	# MESA_BUILDLINK3_MK
