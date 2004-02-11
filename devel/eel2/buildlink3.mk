b94763404ab5779b970807124ba582924f64be12
xtraeme <xtraeme@pkgsrc.org>
2004-02-11 04:47:10 +0000

    bl3ify

---
+++ devel/eel2/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/11 04:54:00 xtraeme Exp $
+#
+# This Makefile fragment is included by packages that use eel2.
+#
+# This file was created automatically using createbuildlink-3.1.
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+EEL2_BUILDLINK3_MK:=	${EEL2_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	eel2
+.endif
+
+.if !empty(EEL2_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			eel2
+BUILDLINK_DEPENDS.eel2+=		eel2>=2.4.1nb2
+BUILDLINK_PKGSRCDIR.eel2?=		../../devel/eel2
+
+.include "../../sysutils/gnome-vfs2/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/libgnomeui/buildlink3.mk"
+.include "../../devel/popt/buildlink3.mk"
+.include "../../devel/gail/buildlink3.mk"
+
+.endif # EEL2_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
