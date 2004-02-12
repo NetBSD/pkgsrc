729be5ebb9775b95be97cddadacfcc46472439f3
recht <recht@pkgsrc.org>
2004-02-12 16:50:53 +0000

    add buildlink{2,3}.mk needed for py-Scientific to build

---
+++ parallel/mpi-ch/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/12 16:50:53 recht Exp $
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+MPICH_BUILDLINK3_MK:=	${MPICH_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	mpich
+.endif
+
+.if !empty(MPICH_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			mpich
+BUILDLINK_DEPENDS.mpich+=		mpich>=1.2.5.2
+BUILDLINK_PKGSRCDIR.mpich?=		../../parallel/mpi-ch
+
+.endif # MPICH_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
