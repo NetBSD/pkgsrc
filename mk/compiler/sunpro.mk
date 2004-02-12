7f41613dcc55bcd15e253d1ed78666f8ca561528
jlam <jlam>
2004-02-12 08:54:48 +0000

    Generate a more informative string "CC_VERSION_STRING" that is passed to ccache as the compiler information to hash.

--- mk/compiler/sunpro.mk
+++ mk/compiler/sunpro.mk
@@ -1,4 +1,4 @@
-# $NetBSD: sunpro.mk,v 1.14 2004/02/10 00:37:53 jlam Exp $
+# $NetBSD: sunpro.mk,v 1.15 2004/02/12 08:54:48 jlam Exp $
 
 .if !defined(COMPILER_SUNPRO_MK)
 COMPILER_SUNPRO_MK=	one
@@ -35,9 +35,11 @@ CXX=		${PKG_CXX:T}
 _COMPILER_LD_FLAG=	# empty
 
 .  if exists(${SUNWSPROBASE}/bin/cc)
-CC_VERSION!=	${SUNWSPROBASE}/bin/cc -V 2>&1 | ${GREP} '^cc'
+CC_VERSION_STRING!=	${SUNWSPROBASE}/bin/cc -V 2>&1
+CC_VERSION!=		${SUNWSPROBASE}/bin/cc -V 2>&1 | ${GREP} '^cc'
 .  else
-CC_VERSION=	cc: Sun C
+CC_VERSION_STRING=	${CC_VERSION}
+CC_VERSION=		cc: Sun C
 .  endif
 .endif	# COMPILER_SUNPRO_MK
 
