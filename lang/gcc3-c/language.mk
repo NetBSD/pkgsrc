ff86c6e0d55fbdf5e37f6ef5d578c178d946f5da
jlam <jlam@pkgsrc.org>
2004-02-12 11:40:45 +0000

    Include gcc3-c/buildlink3.mk to get the gcc3-c library and header files symlinked into ${BUILDLINK_DIR}.

--- lang/gcc3-c/language.mk
+++ lang/gcc3-c/language.mk
@@ -1,10 +1,13 @@
-# $NetBSD: language.mk,v 1.3 2004/02/10 19:31:27 jlam Exp $
+# $NetBSD: language.mk,v 1.4 2004/02/12 11:40:45 jlam Exp $
+
+BUILDLINK_DEPENDS.gcc3c+=	gcc3${GCC3_PKGMODIF}-c>=${GCC_VERSION}
+BUILDLINK_DEPMETHOD.gcc3c+=	full
 
 .include "../../lang/gcc3-c/Makefile.common"
-
-DEPENDS+=	gcc3${GCC3_PKGMODIF}-c>=${GCC_VERSION}:../../lang/gcc3-c
 
 pre-build:
 	cd ${WRKSRC}/gcc && ${SETENV} ${MAKE_ENV} ${GMAKE} tconfig.h
 	${LN} -sf ${GCC_PREFIX}/lib/libiberty.a ${WRKSRC}/libiberty
 	${LN} -sf ${GCC_PREFIX}/bin/gcc ${WRKSRC}/gcc/xgcc
+
+.include "../gcc3-c/buildlink3.mk"
