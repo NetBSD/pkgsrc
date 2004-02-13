120bab67aa71dafaf68bd7416cab8a76f218f033
jlam <jlam@pkgsrc.org>
2004-02-13 15:13:00 +0000

    Bump the PKGREVISION after fixing up the installed *.la files to remove references to the buildlink work directories.

--- lang/gcc3-c/language.mk
+++ lang/gcc3-c/language.mk
@@ -1,9 +1,9 @@
-# $NetBSD: language.mk,v 1.4 2004/02/12 11:40:45 jlam Exp $
+# $NetBSD: language.mk,v 1.5 2004/02/13 15:13:01 jlam Exp $
 
 BUILDLINK_DEPENDS.gcc3c+=	gcc3${GCC3_PKGMODIF}-c>=${GCC_VERSION}
 BUILDLINK_DEPMETHOD.gcc3c+=	full
 
-.include "../../lang/gcc3-c/Makefile.common"
+.include "../gcc3-c/Makefile.common"
 
 pre-build:
 	cd ${WRKSRC}/gcc && ${SETENV} ${MAKE_ENV} ${GMAKE} tconfig.h
