da94dd61e17ef78b2ffcf2e01d7e0d2b5a0d78ed
jmmv <jmmv@pkgsrc.org>
2004-02-13 16:13:27 +0000

    Update to 1.3.9.  Changes in the package: - Fill missing dependancies. - Use pkgsrc intltool.
    What's new in at-spi-1.3.9:
    * New api:  AccessibleApplication_getLocale (app, type),
      returns a POSIX locale string for a locale category,
      for instance Accessibility_LOCALE_TYPE_MESSAGES.
    * New SPI_ROLE_EDITBAR.
    * New Accessible_getLocalizedRoleName.
    * New AccessibleText_getDefaultAttributes.
    * fixes to AccessibleStreamableContent, including a 'seek'
      implementation for cspi.  Thanks to Michael Meeks for some
      related bugfixes in StreamableContent.
    * now we emit key-released events to appropriate keylisteners.
    * new LINGUAS in configure.in: nn (Asmund Sjeveland), lt (Gediminas Paulauskas),
      no (Kjartan Masass), eu (Christian Rose).
    * bugfixes for: #120991, #120473, #118581, #127463, #116009, #114926,
      #125834, #111429, #121974.

--- devel/at-spi/buildlink2.mk
+++ devel/at-spi/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2004/01/03 18:49:37 reed Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2004/02/13 16:13:27 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use at-spi.
 #
@@ -16,15 +16,14 @@ EVAL_PREFIX+=	BUILDLINK_PREFIX.at-spi=at-spi
 BUILDLINK_PREFIX.at-spi_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.at-spi+=	include/at-spi-1.0/cspi/*.h
 BUILDLINK_FILES.at-spi+=	include/at-spi-1.0/libspi/*.h
-BUILDLINK_FILES.at-spi+=	lib/bonobo/servers/Accessibility_Registry.server
-BUILDLINK_FILES.at-spi+=	lib/gtk-2.0/modules/libatk-bridge.*
 BUILDLINK_FILES.at-spi+=	lib/libcspi.*
 BUILDLINK_FILES.at-spi+=	lib/libspi.*
-BUILDLINK_FILES.at-spi+=	lib/orbit-2.0/Accessibility_module.*
 
+.include "../../devel/atk/buildlink2.mk"
 .include "../../devel/gail/buildlink2.mk"
 .include "../../devel/libbonobo/buildlink2.mk"
 .include "../../devel/popt/buildlink2.mk"
+.include "../../x11/gtk2/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	at-spi-buildlink
 
