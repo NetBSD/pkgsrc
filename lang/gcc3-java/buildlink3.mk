0082c0531707ffb43534b61c231a9c5eeb07bb28
jlam <jlam@pkgsrc.org>
2004-02-13 00:27:28 +0000

    Check if a variable is defined before checking its value.

--- lang/gcc3-java/buildlink3.mk
+++ lang/gcc3-java/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/02/13 00:16:00 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/02/13 00:27:28 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GCC3JAVA_BUILDLINK3_MK:=	${GCC3JAVA_BUILDLINK3_MK}+
@@ -10,8 +10,10 @@ BUILDLINK_DEPENDS+=	gcc3java
 .endif
 
 .if !empty(GCC3JAVA_BUILDLINK3_MK:M+)
-.  if ${GCC3_INSTALLTO_SUBPREFIX} != "gcc3"
+.  if defined(GCC3_INSTALLTO_SUBPREFIX)
+.    if ${GCC3_INSTALLTO_SUBPREFIX} != "gcc3"
 GCC3_PKGMODIF=			_${GCC3_INSTALLTO_SUBPREFIX}
+.    endif
 .  endif
 BUILDLINK_PACKAGES+=		gcc3java
 BUILDLINK_DEPENDS.gcc3java+=	gcc3${GCC3_PKGMODIF}-java>=${_GCC_REQD}
