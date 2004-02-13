b93c03be5b92f4c3b7eb6d5716180b4460580e3f
adam <adam@pkgsrc.org>
2004-02-13 13:36:43 +0000

    Changes 0.2.1: * Transparent reading of bzipped and gzipped files.
    * Add support for writing emulator custom data to .rzx and .szx files.
    
    * Update to the more recent version of gcrypt; version 1.1.42 or
      later is now required.
    
    * Link libspectrum against gcrypt, glib and zlib; this means that
      client applications no longer need to do this.
    
    * Support for 'the last instruction was an EI' in the snapshot code.
    
    * Routines to check the versions of libspectrum and gcrypt in use.
    
    * Write .rzx files with a version number of 0.12 if we're not
      signing the data; improves backward compatability.
    
    * Fix a segfault when dealing with .slt files.
    
    * QNX6 compilation fixes.

--- emulators/libspectrum/buildlink2.mk
+++ emulators/libspectrum/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/09/16 09:46:51 adam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2004/02/13 13:36:43 adam Exp $
 
 .if !defined(LIBSPECTRUM_BUILDLINK2_MK)
 LIBSPECTRUM_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libspectrum
-BUILDLINK_DEPENDS.libspectrum?=		libspectrum>=0.1.0
+BUILDLINK_DEPENDS.libspectrum?=		libspectrum>=0.2.1
 BUILDLINK_PKGSRCDIR.libspectrum?=	../../emulators/libspectrum
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libspectrum=libspectrum
@@ -12,7 +12,9 @@ BUILDLINK_PREFIX.libspectrum_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.libspectrum=	include/libspectrum.h
 BUILDLINK_FILES.libspectrum+=	lib/libspectrum.*
 
-.include "../../devel/glib/buildlink2.mk"
+.include "../../devel/glib2/buildlink2.mk"
+.include "../../devel/zlib/buildlink2.mk"
+.include "../../security/libgcrypt/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	libspectrum-buildlink
 
