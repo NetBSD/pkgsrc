a256b48477e29d34b7f6fe1f79cb4c781247be8d
jmmv <jmmv@pkgsrc.org>
2004-02-13 23:29:17 +0000

    Add explicit dependancies on packages used by this one directly.  No revision bump since these were already used (but indirectly).

--- devel/eel2/buildlink3.mk
+++ devel/eel2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/11 04:54:00 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/13 23:29:17 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use eel2.
 #
@@ -17,11 +17,18 @@ BUILDLINK_PACKAGES+=			eel2
 BUILDLINK_DEPENDS.eel2+=		eel2>=2.4.1nb2
 BUILDLINK_PKGSRCDIR.eel2?=		../../devel/eel2
 
-.include "../../sysutils/gnome-vfs2/buildlink3.mk"
+.include "../../devel/GConf2/buildlink3.mk"
+.include "../../devel/gail/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/libglade2/buildlink3.mk"
+.include "../../devel/libgnome/buildlink3.mk"
 .include "../../devel/libgnomeui/buildlink3.mk"
 .include "../../devel/popt/buildlink3.mk"
-.include "../../devel/gail/buildlink3.mk"
+.include "../../graphics/libart2/buildlink3.mk"
+.include "../../sysutils/gnome-vfs2/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
 
 .endif # EEL2_BUILDLINK3_MK
 
