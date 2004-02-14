fab6b2f647187ebcc4851afc5f60d9e4ce8c5cac
jmmv <jmmv@pkgsrc.org>
2004-02-14 00:02:42 +0000

    Add explicit dependencies on all the packages this one checks for. No revision bump since these were already used (indirectly).

--- sysutils/nautilus/buildlink3.mk
+++ sysutils/nautilus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/11 06:30:13 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/14 00:02:42 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use nautilus.
 #
@@ -17,11 +17,25 @@ BUILDLINK_PACKAGES+=			nautilus
 BUILDLINK_DEPENDS.nautilus+=		nautilus>=2.4.2
 BUILDLINK_PKGSRCDIR.nautilus?=		../../sysutils/nautilus
 
-.include "../../devel/libgsf/buildlink3.mk"
+.include "../../audio/esound/buildlink3.mk"
+.include "../../devel/GConf2/schemas.mk"
 .include "../../devel/eel2/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/libbonobo/buildlink3.mk"
+.include "../../devel/libbonoboui/buildlink3.mk"
+.include "../../devel/libgnome/buildlink3.mk"
+.include "../../devel/libgnomeui/buildlink3.mk"
+.include "../../devel/libgsf/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
 .include "../../graphics/imlib2/buildlink3.mk"
+.include "../../graphics/libart2/buildlink3.mk"
 .include "../../graphics/librsvg2/buildlink3.mk"
+.include "../../net/ORBit2/buildlink3.mk"
+.include "../../sysutils/gnome-vfs2/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
 .include "../../x11/gnome-desktop/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/startup-notification/buildlink3.mk"
 
 .endif # NAUTILUS_BUILDLINK3_MK
 
