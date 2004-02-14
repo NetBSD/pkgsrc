776ddbd81bdf99e15ee4cdc5b1e2fba653112de2
recht <recht>
2004-02-14 20:39:38 +0000

    update to 0.30.1
    This release only includes a few smaller bug fixes to the Http client as
    well as minor fixes to the C# compiler.
    
    Specifically this release fixes some problems with Chunked encoding and a
    race condition on the Http client. The C# compiler would crash sometimes
    when reporting a delegate signature error.
    
    While add it g/c some whitespaces in the buildlink3.mk and remove the
    LIBTOOL_OVERRIDES.

--- lang/mono/buildlink3.mk
+++ lang/mono/buildlink3.mk
@@ -1,4 +1,5 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/02/09 23:56:32 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/02/14 20:39:38 recht Exp $
+#
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 MONO_BUILDLINK3_MK:=	${MONO_BUILDLINK3_MK}+
@@ -8,12 +9,13 @@ BUILDLINK_DEPENDS+=	mono
 .endif
 
 .if !empty(MONO_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=		mono
-BUILDLINK_DEPENDS.mono+=	mono>=0.30
-BUILDLINK_PKGSRCDIR.mono?=	../../lang/mono
+BUILDLINK_PACKAGES+=			mono
+BUILDLINK_DEPENDS.mono+=		mono>=0.30
+BUILDLINK_PKGSRCDIR.mono?=		../../lang/mono
 
-.  include "../../devel/glib2/buildlink3.mk"
-.  include "../../textproc/icu/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../textproc/icu/buildlink3.mk"
+
 .endif # MONO_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
