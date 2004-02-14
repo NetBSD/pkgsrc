d8857a73d3c3b37aacd64a38126b5ba0522c1e5d
jmmv <jmmv@pkgsrc.org>
2004-02-14 00:02:42 +0000

    Add explicit dependencies on all the packages this one checks for. No revision bump since these were already used (indirectly).

--- sysutils/nautilus/buildlink2.mk
+++ sysutils/nautilus/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.9 2004/02/06 22:31:14 jmmv Exp $
+# $NetBSD: buildlink2.mk,v 1.10 2004/02/14 00:02:42 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use nautilus.
 #
@@ -36,11 +36,25 @@ BUILDLINK_FILES.nautilus+=	lib/pkgconfig/libnautilus.pc
 
 BUILDLINK_DEPENDS.esound=	esound>=0.2.27
 
+.include "../../audio/esound/buildlink2.mk"
+.include "../../devel/GConf2/schemas.mk"
 .include "../../devel/eel2/buildlink2.mk"
+.include "../../devel/glib2/buildlink2.mk"
+.include "../../devel/libbonobo/buildlink2.mk"
+.include "../../devel/libbonoboui/buildlink2.mk"
+.include "../../devel/libgnome/buildlink2.mk"
+.include "../../devel/libgnomeui/buildlink2.mk"
+.include "../../devel/libgsf/buildlink2.mk"
+.include "../../devel/pango/buildlink2.mk"
 .include "../../graphics/imlib2/buildlink2.mk"
+.include "../../graphics/libart2/buildlink2.mk"
 .include "../../graphics/librsvg2/buildlink2.mk"
-.include "../../textproc/intltool/buildlink2.mk"
+.include "../../net/ORBit2/buildlink2.mk"
+.include "../../sysutils/gnome-vfs2/buildlink2.mk"
+.include "../../textproc/libxml2/buildlink2.mk"
 .include "../../x11/gnome-desktop/buildlink2.mk"
+.include "../../x11/gtk2/buildlink2.mk"
+.include "../../x11/startup-notification/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	nautilus-buildlink
 
