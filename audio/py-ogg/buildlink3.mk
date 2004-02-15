ff4690fb5033a20a9346bd795429693349f07be2
minskim <minskim@pkgsrc.org>
2004-02-15 02:04:05 +0000

    This package is in audio category.

--- audio/py-ogg/buildlink3.mk
+++ audio/py-ogg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/02/15 01:49:15 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/15 02:04:05 minskim Exp $
 #
 # This Makefile fragment is included by packages that use py23pth-ogg.
 #
@@ -15,7 +15,7 @@ BUILDLINK_DEPENDS+=	py23pth-ogg
 .if !empty(PY23PTH_OGG_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=			py23pth-ogg
 BUILDLINK_DEPENDS.py23pth-ogg+=		py23pth-ogg>=1.3
-BUILDLINK_PKGSRCDIR.py23pth-ogg?=		../../wip/py-ogg
+BUILDLINK_PKGSRCDIR.py23pth-ogg?=		../../audio/py-ogg
 
 .include "../../audio/libogg/buildlink3.mk"
 
