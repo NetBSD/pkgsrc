9f426f1821a35aea3939e83a7671e3982817c196
recht <recht>
2004-02-15 09:45:30 +0000

    add buildlink3.mks

---
+++ x11/fixesext/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/15 09:45:30 recht Exp $
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+FIXESEXT_BUILDLINK3_MK:=	${FIXESEXT_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	fixesext
+.endif
+
+.if !empty(FIXESEXT_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			fixesext
+BUILDLINK_DEPENDS.fixesext+=		fixesext>=2.0
+BUILDLINK_PKGSRCDIR.fixesext?=		../../x11/fixesext
+
+
+.endif # FIXESEXT_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
