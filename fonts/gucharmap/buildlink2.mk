9af0ccde223715537fee1f3fabbfdcf7efc0f3ec
jmmv <jmmv>
2004-02-16 00:11:32 +0000

    Explicitly depend on all packages used directly by this one. While here, use pkgsrc intltool, too.

--- fonts/gucharmap/buildlink2.mk
+++ fonts/gucharmap/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2004/02/15 23:48:28 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2004/02/16 00:11:32 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use gucharmap.
 #
@@ -23,8 +23,12 @@ BUILDLINK_FILES.gucharmap+=	include/gucharmap/gucharmap.h
 BUILDLINK_FILES.gucharmap+=	lib/gtk-2.0/2.2.0/immodules/im-gucharmap.*
 BUILDLINK_FILES.gucharmap+=	lib/libgucharmap.*
 
+.include "../../devel/gettext-lib/buildlink2.mk"
+.include "../../devel/libgnome/buildlink2.mk"
 .include "../../devel/libgnomeui/buildlink2.mk"
+.include "../../devel/pango/buildlink2.mk"
 .include "../../devel/popt/buildlink2.mk"
+.include "../../x11/gtk2/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	gucharmap-buildlink
 
