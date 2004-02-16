c6c3174e82613924c49f9295c1e768632426329e
jlam <jlam>
2004-02-16 23:39:36 +0000

    Update pkgtools/x11-links to 0.21.  Changes from version 0.20 include catching up to changes to xcursor, Xrender, render, Xrandr, and randrext by including their buildlink3.mk files.

--- pkgtools/x11-links/xfree.mk
+++ pkgtools/x11-links/xfree.mk
@@ -1,8 +1,13 @@
-# $NetBSD: xfree.mk,v 1.4 2003/09/28 12:45:04 jlam Exp $
+# $NetBSD: xfree.mk,v 1.5 2004/02/16 23:39:36 jlam Exp $
 
 FILES_LIST=	${FILESDIR}/xfree
 
-X11_MODULES=	MesaLib Xrender freetype2 glu xpm
+X11_MODULES=	MesaLib glu
+X11_MODULES+=	freetype2
+X11_MODULES+=	xpm
+X11_MODULES+=	Xrender fontconfig render
+X11_MODULES+=	Xrandr randrext
+X11_MODULES+=	xcursor
 
 # Check if any headers and libraries for ${X11_MODULES} found in
 # ${X11BASE} actually belong to the base XFree86 or not.
@@ -11,11 +16,16 @@ X11_MODULES=	MesaLib Xrender freetype2 glu xpm
 BUILDLINK_CHECK_BUILTIN.${_module_}=	YES
 .endfor
 
+.include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../graphics/glu/buildlink3.mk"
 .include "../../graphics/xpm/buildlink3.mk"
+.include "../../x11/Xrandr/buildlink3.mk"
 .include "../../x11/Xrender/buildlink3.mk"
+.include "../../x11/render/buildlink3.mk"
+.include "../../x11/randrext/buildlink3.mk"
+.include "../../x11/xcursor/buildlink3.mk"
 
 .for _module_ in ${X11_MODULES}
 .  if !empty(BUILDLINK_IS_BUILTIN.${_module_}:M[yY][eE][sS])
