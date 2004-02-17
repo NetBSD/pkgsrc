869432a9f1b875cb72e4781635759c3fafd573f5
jmmv <jmmv@pkgsrc.org>
2004-02-17 22:02:02 +0000

    bl3ify.

---
+++ audio/gnome-speech/buildlink3.mk
@@ -0,0 +1,26 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/17 22:02:02 jmmv Exp $
+#
+# This Makefile fragment is included by packages that use gnome-speech.
+#
+# This file was created automatically using createbuildlink-3.0.
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GNOME_SPEECH_BUILDLINK3_MK:=	${GNOME_SPEECH_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gnome-speech
+.endif
+
+.if !empty(GNOME_SPEECH_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			gnome-speech
+BUILDLINK_DEPENDS.gnome-speech?=		gnome-speech>=0.2.7
+BUILDLINK_PKGSRCDIR.gnome-speech?=		../../audio/gnome-speech
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/libbonobo/buildlink3.mk"
+.include "../../net/ORBit2/buildlink3.mk"
+
+.endif # GNOME_SPEECH_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
