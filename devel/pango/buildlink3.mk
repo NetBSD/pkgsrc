b984a0496eae29c5611d6f98160d40c7125aa894
jlam <jlam@pkgsrc.org>
2004-02-17 14:48:01 +0000

    Remove some unnecessary buildlink3.mk files and clean up some whitespace.

--- devel/pango/buildlink3.mk
+++ devel/pango/buildlink3.mk
@@ -1,9 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/10 01:56:42 xtraeme Exp $
-#
-# This Makefile fragment is included by packages that use pango.
-#
-# This file was created automatically using createbuildlink-3.0.
-#
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/17 14:48:01 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PANGO_BUILDLINK3_MK:=	${PANGO_BUILDLINK3_MK}+
@@ -13,15 +8,12 @@ BUILDLINK_DEPENDS+=	pango
 .endif
 
 .if !empty(PANGO_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			pango
-BUILDLINK_DEPENDS.pango?=		pango>=1.2.5nb1
-BUILDLINK_PKGSRCDIR.pango?=		../../devel/pango
+BUILDLINK_PACKAGES+=		pango
+BUILDLINK_DEPENDS.pango?=	pango>=1.2.5nb1
+BUILDLINK_PKGSRCDIR.pango?=	../../devel/pango
 
-.include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/zlib/buildlink3.mk"
-.include "../../fonts/Xft2/buildlink3.mk"
-.include "../../graphics/freetype2/buildlink3.mk"
-
+.  include "../../devel/glib2/buildlink3.mk"
+.  include "../../fonts/Xft2/buildlink3.mk"
 .endif # PANGO_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
