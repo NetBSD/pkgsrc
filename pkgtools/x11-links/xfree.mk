6ea649d8a172b2351d648d434bb9fbfc4368f653
jlam <jlam>
2004-02-17 16:50:26 +0000

    Update to x11-links-0.23: catch up with Xft2/buildlink3.mk changes.

--- pkgtools/x11-links/xfree.mk
+++ pkgtools/x11-links/xfree.mk
@@ -1,11 +1,11 @@
-# $NetBSD: xfree.mk,v 1.6 2004/02/17 08:26:25 jlam Exp $
+# $NetBSD: xfree.mk,v 1.7 2004/02/17 16:50:26 jlam Exp $
 
 FILES_LIST=	${FILESDIR}/xfree
 
 X11_MODULES=	MesaLib glu
 X11_MODULES+=	freetype2
 X11_MODULES+=	xpm
-X11_MODULES+=	Xrender expat fontconfig render
+X11_MODULES+=	Xft2 Xrender expat fontconfig render
 X11_MODULES+=	Xrandr randrext
 X11_MODULES+=	xcursor
 
@@ -16,6 +16,7 @@ X11_MODULES+=	xcursor
 BUILDLINK_CHECK_BUILTIN.${_module_}=	YES
 .endfor
 
+.include "../../fonts/Xft2/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
