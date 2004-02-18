e1720521d0a3d72d0677c724d4ea47c2f900ba11
minskim <minskim@pkgsrc.org>
2004-02-18 16:04:50 +0000

    Fix a version-specific string in comments.

--- math/py-Numeric/buildlink3.mk
+++ math/py-Numeric/buildlink3.mk
@@ -1,6 +1,6 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/18 16:02:01 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/18 16:04:50 minskim Exp $
 #
-# This Makefile fragment is included by packages that use py23pth-Numeric.
+# This Makefile fragment is included by packages that use py-Numeric.
 #
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
