f7df9736c09affaa28e3700f037bf60c86065aa0
jlam <jlam@pkgsrc.org>
2004-02-19 06:41:21 +0000

    Clean up this buildlink3.mk file and check for USE_ESOUND before including the esound/buildlink3.mk file.  Fixes PR 24485.

--- audio/libmikmod/buildlink3.mk
+++ audio/libmikmod/buildlink3.mk
@@ -1,24 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/11 08:36:17 xtraeme Exp $
-#
-# This Makefile fragment is included by packages that use libmikmod.
-#
-# This file was created automatically using createbuildlink-3.1.
-#
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/19 06:41:21 jlam Exp $
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBMIKMOD_BUILDLINK3_MK:=	${LIBMIKMOD_BUILDLINK3_MK}+
+
+.include "../../mk/bsd.prefs.mk"
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	libmikmod
 .endif
 
 .if !empty(LIBMIKMOD_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			libmikmod
-BUILDLINK_DEPENDS.libmikmod+=		libmikmod>=3.1.9
-BUILDLINK_PKGSRCDIR.libmikmod?=		../../audio/libmikmod
+BUILDLINK_PACKAGES+=		libmikmod
+BUILDLINK_DEPENDS.libmikmod+=	libmikmod>=3.1.9
+BUILDLINK_PKGSRCDIR.libmikmod?=	../../audio/libmikmod
 
-.include "../../audio/esound/buildlink3.mk"
-
+.  if defined(USE_ESOUND) && !empty(USE_ESOUND:M[Yy][Ee][Ss])
+.    include "../../audio/esound/buildlink3.mk"
+.  endif
 .endif # LIBMIKMOD_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
