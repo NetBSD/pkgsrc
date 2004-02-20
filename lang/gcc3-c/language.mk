84c758dfdea79e7b1e34fe85a026128815551152
jlam <jlam@pkgsrc.org>
2004-02-20 06:52:43 +0000

    Use stricter dependency patterns so that we only match ones with the same distribution version number.

--- lang/gcc3-c/language.mk
+++ lang/gcc3-c/language.mk
@@ -1,6 +1,6 @@
-# $NetBSD: language.mk,v 1.5 2004/02/13 15:13:01 jlam Exp $
+# $NetBSD: language.mk,v 1.6 2004/02/20 06:52:43 jlam Exp $
 
-BUILDLINK_DEPENDS.gcc3c+=	gcc3${GCC3_PKGMODIF}-c>=${GCC_VERSION}
+BUILDLINK_DEPENDS.gcc3c+=	gcc3${GCC3_PKGMODIF}-c-${GCC_VERSION}{,nb*}
 BUILDLINK_DEPMETHOD.gcc3c+=	full
 
 .include "../gcc3-c/Makefile.common"
