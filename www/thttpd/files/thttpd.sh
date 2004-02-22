4fba534809054076880d28fcda9dcebbbcca4b27
jlam <jlam@pkgsrc.org>
2004-02-22 18:34:54 +0000

    bl3ify and general touch-up

--- www/thttpd/files/thttpd.sh
+++ www/thttpd/files/thttpd.sh
@@ -1,12 +1,11 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: thttpd.sh,v 1.9 2002/09/20 02:02:02 grant Exp $
+# $NetBSD: thttpd.sh,v 1.10 2004/02/22 18:34:54 jlam Exp $
 #
 # PROVIDE: thttpd
 # REQUIRE: DAEMON
 
-if [ -f /etc/rc.subr ]
-then
+if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
@@ -16,8 +15,7 @@ command="@PREFIX@/sbin/${name}"
 command_args="-C @PKG_SYSCONFDIR@/${name}.conf"
 required_files="@PKG_SYSCONFDIR@/${name}.conf"
 
-if [ -f /etc/rc.subr ]
-then
+if [ -f /etc/rc.subr ]; then
 	load_rc_config $name
 	run_rc_command "$1"
 else
