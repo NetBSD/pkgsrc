6c597eb8d855d67d9b13e35521e2013946a752c7
kristerw <kristerw@pkgsrc.org>
2004-02-22 04:51:25 +0000

    Previous change broke build on NetBSD because it tried to "rm -f" a directory instead of just the file that was intended.

--- lang/gcc3-c/language.mk
+++ lang/gcc3-c/language.mk
@@ -1,4 +1,4 @@
-# $NetBSD: language.mk,v 1.7 2004/02/21 08:38:45 seb Exp $
+# $NetBSD: language.mk,v 1.8 2004/02/22 04:51:25 kristerw Exp $
 
 BUILDLINK_DEPENDS.gcc3c+=	gcc3${GCC3_PKGMODIF}-c-${GCC_VERSION}{,nb*}
 BUILDLINK_DEPMETHOD.gcc3c+=	full
@@ -7,7 +7,7 @@ BUILDLINK_DEPMETHOD.gcc3c+=	full
 
 pre-build:
 	cd ${WRKSRC}/gcc && ${SETENV} ${MAKE_ENV} ${GMAKE} tconfig.h
-	${RM} -f ${WRKSRC}/libiberty ${WRKSRC}/gcc/xgcc
+	${RM} -f ${WRKSRC}/libiberty/libiberty.a ${WRKSRC}/gcc/xgcc
 	${LN} -s ${GCC_PREFIX}/lib/libiberty.a ${WRKSRC}/libiberty
 	${LN} -s ${GCC_PREFIX}/bin/gcc ${WRKSRC}/gcc/xgcc
 
