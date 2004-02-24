ba8bd65c9c59eea89399f94aeeedd7e05b31cd31
jlam <jlam@pkgsrc.org>
2004-02-24 01:18:52 +0000

    Check that the required_files do exist before starting the process.  If start_cmd is defined, then rc.subr skips the required_files check, so do it ourselves within the start_cmd.

--- mail/sqwebmail/files/pcpd.sh
+++ mail/sqwebmail/files/pcpd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: pcpd.sh,v 1.1 2004/02/23 23:41:43 jlam Exp $
+# $NetBSD: pcpd.sh,v 1.2 2004/02/24 01:18:52 jlam Exp $
 #
 # Courier SqWebMail calendaring services daemon
 #
@@ -23,8 +23,20 @@ courier_doit()
 {
 	action=$1
 	case ${action} in
-	start)	@ECHO@ "Starting ${name}." ;;
-	stop)	@ECHO@ "Stopping ${name}." ;;
+	start)
+		for f in $required_files; do
+			if [ ! -r "$f" ]; then
+				@ECHO@ "$0: WARNING: $f is not readable"
+				if [ -z $rc_force ]; then
+					return 1
+				fi
+			fi
+		done
+		@ECHO@ "Starting ${name}."
+		;;
+	stop)
+		@ECHO@ "Stopping ${name}."
+		;;
 	esac
 
 	${command} ${action}
