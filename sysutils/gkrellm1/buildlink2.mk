81984b7dcd88f3308147d784c10a6c5645133405
cube <cube@pkgsrc.org>
2004-02-24 22:42:29 +0000

    Correct paths.

--- sysutils/gkrellm1/buildlink2.mk
+++ sysutils/gkrellm1/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2004/02/24 22:06:56 cube Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2004/02/24 22:42:29 cube Exp $
 #
 
 .if !defined(GKRELLM_BUILDLINK2_MK)
@@ -6,7 +6,7 @@ GKRELLM_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gkrellm
 BUILDLINK_DEPENDS.gkrellm?=		gkrellm>=1.2.13nb3
-BUILDLINK_PKGSRCDIR.gkrellm?=		../../sysutils/gkrellm
+BUILDLINK_PKGSRCDIR.gkrellm?=		../../sysutils/gkrellm1
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gkrellm=gkrellm
 BUILDLINK_PREFIX.gkrellm_DEFAULT=	${LOCALBASE}
