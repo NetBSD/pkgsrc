de090b7e6e560963cac3919b1c7f073aba68a03f
minskim <minskim@pkgsrc.org>
2004-02-25 18:10:49 +0000

    Bump BUILDLINK_DEPENDS due to library major bump.

--- security/libksba/buildlink3.mk
+++ security/libksba/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/10 10:22:03 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/02/25 18:11:06 minskim Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBKSBA_BUILDLINK3_MK:=	${LIBKSBA_BUILDLINK3_MK}+
@@ -9,7 +9,7 @@ BUILDLINK_DEPENDS+=	libksba
 
 .if !empty(LIBKSBA_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=		libksba
-BUILDLINK_DEPENDS.libksba+=	libksba>=0.9.1nb1
+BUILDLINK_DEPENDS.libksba+=	libksba>=0.9.4
 BUILDLINK_PKGSRCDIR.libksba?=	../../security/libksba
 
 .  include "../../security/libgcrypt/buildlink3.mk"
