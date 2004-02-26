48118d35f5ac5f65f5694cfd76205357a5af5cab
minskim <minskim@pkgsrc.org>
2004-02-26 23:14:02 +0000

    This package is not in wip any more.

--- lang/pnetC/buildlink2.mk
+++ lang/pnetC/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2004/01/12 17:18:41 xtraeme Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2004/02/26 23:14:02 minskim Exp $
 #
 # This Makefile fragment is included by packages that use pnetC.
 #
@@ -10,14 +10,14 @@ PNETC_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			pnetC
 BUILDLINK_DEPENDS.pnetC?=		pnetC>=0.6.0
-BUILDLINK_PKGSRCDIR.pnetC?=		../../wip/pnetC
+BUILDLINK_PKGSRCDIR.pnetC?=		../../lang/pnetC
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.pnetC=pnetC
 BUILDLINK_PREFIX.pnetC_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.pnetC+=	lib/cscc/lib/libc64.dll
 BUILDLINK_FILES.pnetC+=	lib/cscc/lib/libm64.dll
 
-.include "../../wip/pnet/buildlink2.mk"
+.include "../../lang/pnet/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	pnetC-buildlink
 
