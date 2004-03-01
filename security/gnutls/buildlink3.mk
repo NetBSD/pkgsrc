d779516574c1c3a23843f967fbfc0f653557727f
jmmv <jmmv@pkgsrc.org>
2004-03-01 15:14:45 +0000

    Update to 1.0.8.  Changes since 1.0.6:
    Version 1.0.8 (28/02/2004)
    - Corrected bug in mutual certificate authentication in SSL 3.0.
    - Several other minor bugfixes.
    
    Version 1.0.7 (25/02/2004)
    - Implemented TLS 1.1 (and also obsoleted the TLS 1.0 CBC protection hack).
    - Some updates in the documentation.

--- security/gnutls/buildlink3.mk
+++ security/gnutls/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/10 00:21:28 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/03/01 15:14:45 jmmv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GNUTLS_BUILDLINK3_MK:=	${GNUTLS_BUILDLINK3_MK}+
@@ -9,7 +9,7 @@ BUILDLINK_DEPENDS+=	gnutls
 
 .if !empty(GNUTLS_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=		gnutls
-BUILDLINK_DEPENDS.gnutls?=	gnutls>=1.0.1
+BUILDLINK_DEPENDS.gnutls?=	gnutls>=1.0.8
 BUILDLINK_PKGSRCDIR.gnutls?=	../../security/gnutls
 
 .  include "../../archivers/liblzo/buildlink3.mk"
