d07881e247a0c0edec1e23af5cb5a5e006b8e7d8
drochner <drochner@pkgsrc.org>
2004-03-02 15:26:02 +0000

    use REPLACE_INTERPRETER instead of our private implementation

--- lang/python/application.mk
+++ lang/python/application.mk
@@ -1,4 +1,4 @@
-# $NetBSD: application.mk,v 1.5 2004/01/14 03:22:47 xtraeme Exp $
+# $NetBSD: application.mk,v 1.6 2004/03/02 15:26:02 drochner Exp $
 
 .include "../../lang/python/pyversion.mk"
 
@@ -6,21 +6,9 @@
 # PYTHON_PATCH_SCRIPTS is a list of Python scripts to be installed,
 #  relative to ${WRKSRC}
 
-_PYTHON_PATCH_SCRIPT_SED=	-e "1s;^\#!.*python[^ ]*;\#!${PYTHONBIN};"
-_PYTHON_PATCH_SCRIPT_SED+=	${PYTHON_PATCH_SCRIPT_SED}
-
 .if defined(PYTHON_PATCH_SCRIPTS)
-python-patch-scripts:
-.for FILE in ${PYTHON_PATCH_SCRIPTS}
-	cd ${WRKSRC}; for file in ${FILE}; do				\
-		${SED} ${_PYTHON_PATCH_SCRIPT_SED}			\
-			$${file} > $${file}.fixed;			\
-		if [ -x $${file} ]; then				\
-			${CHMOD} +x $${file}.fixed;			\
-		fi;							\
-		${MV} -f $${file}.fixed $${file};			\
-	done
-.endfor
-
-post-patch: python-patch-scripts
+REPLACE_INTERPRETER+=	python
+_REPLACE.python.old=	.*python[^ ]*
+_REPLACE.python.new=	${PYTHONBIN}
+_REPLACE_FILES.python=	${PYTHON_PATCH_SCRIPTS}
 .endif
