3ffaa53e6137d6b7f3dee0f50532637f75873d30
drochner <drochner@pkgsrc.org>
2004-03-02 14:56:07 +0000

    update to 4.2.6 changes: -bugfixes -inproved device support (newer Epson Stylus, Canon S200) -European 10x15 cm and US Engineering C paper sizes -CUPS driver: normal and reversed paper order -quality improvements

--- print/gimp-print-lib/buildlink2.mk
+++ print/gimp-print-lib/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.6 2003/07/15 10:05:04 tron Exp $
+# $NetBSD: buildlink2.mk,v 1.7 2004/03/02 14:56:07 drochner Exp $
 
 .if !defined(GIMP_PRINT_BUILDLINK2_MK)
 GIMP_PRINT_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=	gimp-print-lib
-BUILDLINK_DEPENDS.gimp-print-lib?=	gimp-print-lib-4.2.5{,nb*}
+BUILDLINK_DEPENDS.gimp-print-lib?=	gimp-print-lib-4.2.6{,nb*}
 BUILDLINK_PKGSRCDIR.gimp-print-lib?=	../../print/gimp-print-lib
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gimp-print-lib=gimp-print-lib
