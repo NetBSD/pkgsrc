a740a2c0b042f94232a97b6505e97d2d7ecad912
adam <adam@pkgsrc.org>
2004-03-03 11:28:11 +0000

    Changes 5.0.0: - Forced PostScript target in the doc directory to file.
    - Subsetting code extended in order to ensure that components of
      composite characters are also part of the subset.
    
    - Hinting meachanism reworked in order to get rid of some
      rasterizing artifacts.
    
    - Experimental support for stroked fonts (PaintType=1)
    
    - Experimental support for stroking standard ATM fonts with PaintType=0.
    
    - Functions for drawing rectangles.
    
    - Bug Fixes
    
    Changes 5.0.1:
    - Support for Flex was not adapted to the changes from 5.0.0. This led to
      rasterization errors in fonts that use the Flex functionality --> fixed.
    
    - Bug Fixes

--- fonts/t1lib/buildlink3.mk
+++ fonts/t1lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/12 21:40:02 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/03/03 11:28:11 adam Exp $
 #
 # This Makefile fragment is included by packages that use t1lib.
 #
@@ -12,7 +12,7 @@ BUILDLINK_DEPENDS+=	t1lib
 
 .if !empty(T1LIB_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=			t1lib
-BUILDLINK_DEPENDS.t1lib+=		t1lib>=1.3.1
+BUILDLINK_DEPENDS.t1lib+=		t1lib>=5.0.1
 BUILDLINK_PKGSRCDIR.t1lib?=		../../fonts/t1lib
 
 .endif # T1LIB_BUILDLINK3_MK
