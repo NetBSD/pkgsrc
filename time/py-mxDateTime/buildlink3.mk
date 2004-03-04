fac10eb31c99716ef94585926dffa1b33d7f31d0
recht <recht@pkgsrc.org>
2004-03-04 15:02:59 +0000

    D'oh! Fix typo: Only add the dir not the actual header..

--- time/py-mxDateTime/buildlink3.mk
+++ time/py-mxDateTime/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/04 14:50:18 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/03/04 15:02:59 recht Exp $
 #
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
@@ -15,7 +15,7 @@ BUILDLINK_PACKAGES+=			py-mxDateTime
 BUILDLINK_DEPENDS.py-mxDateTime+=		${PYPKGPREFIX}-mxDateTime>=2.0.5
 BUILDLINK_PKGSRCDIR.py-mxDateTime?=		../../time/py-mxDateTime
 
-BUILDLINK_INCDIRS.py-mxDateTime+=	${PYSITELIB}/mx/DateTime/mxDateTime/mxDateTime.h
+BUILDLINK_INCDIRS.py-mxDateTime+=	${PYSITELIB}/mx/DateTime/mxDateTime
 
 .endif # PY_MXDATETIME_BUILDLINK3_MK
 
