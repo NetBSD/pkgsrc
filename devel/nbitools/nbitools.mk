96e676e7c4e3c5472150b9ace6efcc3c91be708e
minskim <minskim@pkgsrc.org>
2004-03-05 21:25:28 +0000

    Not used any more.

--- devel/nbitools/nbitools.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD: nbitools.mk,v 1.2 2004/02/24 22:10:39 minskim Exp $
-.if !defined(NBITOOLS_MK)
-NBITOOLS_MK=	# defined
-
-XMKMF_CMD=	${NBITOOLSDIR}/libexec/itools/xmkmf
-
-.include "../../devel/nbitools/buildlink2.mk"
-
-.endif	# NBITOOLS_MK
