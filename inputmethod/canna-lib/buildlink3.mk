5c72de848341d2d99cbe172a59947412dd8005ce
minskim <minskim@pkgsrc.org>
2004-03-05 21:08:52 +0000

    Match the new buildlink3.mk template.

--- inputmethod/canna-lib/buildlink3.mk
+++ inputmethod/canna-lib/buildlink3.mk
@@ -1,20 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/03/05 19:09:22 minskim Exp $
-#
-# This Makefile fragment is included by packages that use Canna-lib.
-#
+# $NetBSD: buildlink3.mk,v 1.2 2004/03/05 21:08:52 minskim Exp $
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 CANNA_LIB_BUILDLINK3_MK:=	${CANNA_LIB_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	Canna-lib
 .endif
 
-.if !empty(CANNA_LIB_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			Canna-lib
-BUILDLINK_DEPENDS.Canna-lib+=		Canna-lib>=3.6pl3
-BUILDLINK_PKGSRCDIR.Canna-lib?=		../../inputmethod/canna-lib
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NCanna-lib}
+BUILDLINK_PACKAGES+=	Canna-lib
 
-.endif # CANNA_LIB_BUILDLINK3_MK
+.if !empty(CANNA_LIB_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.Canna-lib+=	Canna-lib>=3.6pl4
+BUILDLINK_PKGSRCDIR.Canna-lib?=	../../inputmethod/canna-lib
+
+.endif	# CANNA_LIB_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
