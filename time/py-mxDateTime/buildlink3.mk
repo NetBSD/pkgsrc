48199529ba435e7e636a8d41bb3af45b02b51eb2
jlam <jlam>
2004-03-06 23:46:06 +0000

    Conform to template in revision 1.101 of bsd.buildlink3.mk.

--- time/py-mxDateTime/buildlink3.mk
+++ time/py-mxDateTime/buildlink3.mk
@@ -1,7 +1,6 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/03/04 15:02:59 recht Exp $
-#
+# $NetBSD: buildlink3.mk,v 1.4 2004/03/06 23:46:06 jlam Exp $
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PY_MXDATETIME_BUILDLINK3_MK:=	${PY_MXDATETIME_BUILDLINK3_MK}+
 
 .include "../../lang/python/pyversion.mk"
@@ -10,13 +9,15 @@ PY_MXDATETIME_BUILDLINK3_MK:=	${PY_MXDATETIME_BUILDLINK3_MK}+
 BUILDLINK_DEPENDS+=	py-mxDateTime
 .endif
 
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npy-mxDateTime}
+BUILDLINK_PACKAGES+=	py-mxDateTime
+
 .if !empty(PY_MXDATETIME_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			py-mxDateTime
-BUILDLINK_DEPENDS.py-mxDateTime+=		${PYPKGPREFIX}-mxDateTime>=2.0.5
-BUILDLINK_PKGSRCDIR.py-mxDateTime?=		../../time/py-mxDateTime
+BUILDLINK_DEPENDS.py-mxDateTime+=	${PYPKGPREFIX}-mxDateTime>=2.0.5
+BUILDLINK_PKGSRCDIR.py-mxDateTime?=	../../time/py-mxDateTime
 
 BUILDLINK_INCDIRS.py-mxDateTime+=	${PYSITELIB}/mx/DateTime/mxDateTime
 
-.endif # PY_MXDATETIME_BUILDLINK3_MK
+.endif	# PY_MXDATETIME_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
