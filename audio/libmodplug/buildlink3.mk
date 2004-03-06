89c487a2b41b880f6e431aebf1324dd2e27ccbec
kristerw <kristerw>
2004-03-06 11:58:43 +0000

    Correct a path that still pointed into wip.

--- audio/libmodplug/buildlink3.mk
+++ audio/libmodplug/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/03/05 22:31:42 ben Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/03/06 11:58:43 kristerw Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 LIBMODPLUG_BUILDLINK3_MK:=		${LIBMODPLUG_BUILDLINK3_MK}+
@@ -10,7 +10,7 @@ BUILDLINK_DEPENDS+=			libmodplug
 .if !empty(LIBMODPLUG_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=			libmodplug
 BUILDLINK_DEPENDS.libmodplug+=		libmodplug>=0.7
-BUILDLINK_PKGSRCDIR.libmodplug?=	../../wip/libmodplug
+BUILDLINK_PKGSRCDIR.libmodplug?=	../../audio/libmodplug
 
 .endif # LIBMODPLUG_BUILDLINK3_MK
 
