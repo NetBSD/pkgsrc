430351a890deed96b2cf94cb5e2794a1e7d7b5c3
jlam <jlam@pkgsrc.org>
2004-03-06 23:46:06 +0000

    Conform to template in revision 1.101 of bsd.buildlink3.mk.

--- x11/lablgtk/buildlink3.mk
+++ x11/lablgtk/buildlink3.mk
@@ -1,9 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/03/04 16:13:35 wiz Exp $
-#
-# This Makefile fragment is included by packages that use lablgtk.
-#
-# This file was created automatically using createbuildlink-3.1.
-#
+# $NetBSD: buildlink3.mk,v 1.2 2004/03/06 23:46:06 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LABLGTK_BUILDLINK3_MK:=	${LABLGTK_BUILDLINK3_MK}+
@@ -12,14 +7,16 @@ LABLGTK_BUILDLINK3_MK:=	${LABLGTK_BUILDLINK3_MK}+
 BUILDLINK_DEPENDS+=	lablgtk
 .endif
 
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlablgtk}
+BUILDLINK_PACKAGES+=	lablgtk
+
 .if !empty(LABLGTK_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			lablgtk
-BUILDLINK_DEPENDS.lablgtk+=		lablgtk>=1.2.5nb3
-BUILDLINK_PKGSRCDIR.lablgtk?=		../../x11/lablgtk
+BUILDLINK_DEPENDS.lablgtk+=	lablgtk>=1.2.5nb3
+BUILDLINK_PKGSRCDIR.lablgtk?=	../../x11/lablgtk
 
 .include "../../x11/gtk/buildlink3.mk"
 .include "../../lang/ocaml/buildlink3.mk"
 
-.endif # LABLGTK_BUILDLINK3_MK
+.endif	# LABLGTK_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
