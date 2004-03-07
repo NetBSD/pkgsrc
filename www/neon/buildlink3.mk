a576630f4425a18eba914972aaa2e7ede985a1e2
minskim <minskim>
2004-03-07 22:30:33 +0000

    bl3ify and enable pkgviews installation.

---
+++ www/neon/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/03/07 22:30:33 minskim Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+NEON_BUILDLINK3_MK:=	${NEON_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	neon
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nneon}
+BUILDLINK_PACKAGES+=	neon
+
+.if !empty(NEON_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.neon+=	neon>=0.24.4
+BUILDLINK_PKGSRCDIR.neon?=	../../www/neon
+
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.include "../../textproc/expat/buildlink3.mk"
+
+.endif	# NEON_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
