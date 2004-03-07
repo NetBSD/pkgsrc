ab2146885a488859b06aa322f01befac1f6a6c84
minskim <minskim@pkgsrc.org>
2004-03-07 23:20:55 +0000

    Enable pkgviews installation.

---
+++ devel/swig-build/buildlink3.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/03/07 23:20:55 minskim Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+SWIG_BUILD_BUILDLINK3_MK:=	${SWIG_BUILD_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	swig-build
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nswig-build}
+BUILDLINK_PACKAGES+=	swig-build
+
+.if !empty(SWIG_BUILD_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.swig-build+=	swig-build>=${SWIG_VERSION}
+BUILDLINK_PKGSRCDIR.swig-build?=	../../devel/swig-build
+
+# This package contains no run-time components.
+BUILDLINK_DEPMETHOD.swig-build?=	build
+
+.include "../../devel/swig-build/Makefile.version"
+
+.endif	# SWIG_BUILD_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
