be7762fedf91f77355436c41bc7c1b62acb78cc1
minskim <minskim@pkgsrc.org>
2004-03-07 22:32:05 +0000

    bl3ify.

---
+++ devel/apr/buildlink3.mk
@@ -0,0 +1,30 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/03/07 22:32:05 minskim Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+APR_BUILDLINK3_MK:=	${APR_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	apr
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Napr}
+BUILDLINK_PACKAGES+=	apr
+
+.if !empty(APR_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.apr+=	apr>=0.9.5.2.0.48nb1
+BUILDLINK_PKGSRCDIR.apr?=	../../devel/apr
+
+BUILDLINK_FILES.apr+=	bin/apr-config
+BUILDLINK_FILES.apr+=	bin/apu-config
+BUILDLINK_FILES.apr+=	lib/*.exp
+
+.include "../../mk/bsd.prefs.mk"
+.if ${APR_USE_DB4} == "YES"
+.include "../../databases/db4/buildlink3.mk"
+.endif
+
+.include "../../textproc/expat/buildlink3.mk"
+
+.endif	# APR_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
