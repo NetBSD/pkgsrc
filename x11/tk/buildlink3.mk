c0ffa83031a67dee0fdb317e8a4ea78ba3592bfc
minskim <minskim@pkgsrc.org>
2004-03-08 20:29:43 +0000

    Update tk to 8.4.5.  Too many changes since 8.3.4.  Please see ChangeLog in the distfile.

--- x11/tk/buildlink3.mk
+++ x11/tk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/03/08 19:52:52 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2004/03/08 20:29:43 minskim Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 TK_BUILDLINK3_MK:=	${TK_BUILDLINK3_MK}+
@@ -11,20 +11,20 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ntk}
 BUILDLINK_PACKAGES+=	tk
 
 .if !empty(TK_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.tk+=		tk>=8.3.4
+BUILDLINK_DEPENDS.tk+=		tk>=8.4.5
 BUILDLINK_PKGSRCDIR.tk?=	../../x11/tk
 
 BUILDLINK_FILES.tk=	bin/wish*
 #
-# Make "-ltk" resolve into "-ltk83", so that we don't need to patch so
+# Make "-ltk" resolve into "-ltk84", so that we don't need to patch so
 # many Makefiles.
 #
-BUILDLINK_TRANSFORM+=	l:tk:tk83
+BUILDLINK_TRANSFORM+=	l:tk:tk84
 
 USE_X11=	yes
 TKCONFIG_SH?=	${BUILDLINK_PREFIX.tk}/lib/tkConfig.sh
 
-.include "../../lang/tcl83/buildlink3.mk"
+.include "../../lang/tcl/buildlink3.mk"
 
 .endif	# TK_BUILDLINK3_MK
 
