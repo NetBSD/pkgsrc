82ebd734f7984c74ac819799921250496a3b5b6a
minskim <minskim>
2004-03-08 19:22:26 +0000

    Reimport tcl-8.3.4 into lang/tcl83 before updating Tcl/Tk to 8.4.5, because many packages still need 8.3.  OK'ed by the maintainer (jwise@).

---
+++ lang/tcl83/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/03/08 19:22:26 minskim Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+TCL_BUILDLINK3_MK:=	${TCL_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	tcl
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ntcl}
+BUILDLINK_PACKAGES+=	tcl
+
+.if !empty(TCL_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.tcl+=		tcl>=8.3.4
+BUILDLINK_PKGSRCDIR.tcl?=	../../lang/tcl83
+
+BUILDLINK_FILES.tcl=	bin/tclsh*
+#
+# Make "-ltcl" resolve into "-ltcl83", so that we don't need to patch so
+# many Makefiles.
+#
+BUILDLINK_TRANSFORM+=	l:tcl:tcl83
+
+TCLCONFIG_SH?=		${BUILDLINK_PREFIX.tcl}/lib/tclConfig.sh
+
+.endif	# TCL_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
