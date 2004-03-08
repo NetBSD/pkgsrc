d4f1dc3d43ce56291b3d4bb8af8db3c27695a0f5
kristerw <kristerw@pkgsrc.org>
2004-03-08 00:44:50 +0000

    Correct path to files to install.

--- math/gap/gap.package.mk
+++ math/gap/gap.package.mk
@@ -1,4 +1,4 @@
-# $NetBSD: gap.package.mk,v 1.10 2004/01/01 18:49:22 cjep Exp $
+# $NetBSD: gap.package.mk,v 1.11 2004/03/08 00:44:50 kristerw Exp $
 #
 # Set GAP_PKG_NAME in your Makefile. More may appear here later.
 
@@ -9,6 +9,6 @@ DEPENDS+=       	gap>=4.3.5:../../math/gap
 GAP_DIRECTORY=  	${LOCALBASE}/lib/gap4.3
 GAP_PKG_ROOT=		${GAP_DIRECTORY}/pkg
 GAP_PKG_DIRECTORY=	${GAP_PKG_ROOT}/${GAP_PKG_NAME}
-GAP_PLATFORM=   	${MACHINE_GNU_PLATFORM}-cc
+GAP_PLATFORM=   	${MACHINE_GNU_PLATFORM}-${CC}
 
 PLIST_SUBST=    GAP_PLATFORM=${GAP_PLATFORM}
