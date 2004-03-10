136d79876d7d3557c513fcd32a502b10aec51c91
jlam <jlam@pkgsrc.org>
2004-03-10 18:35:32 +0000

    Catch up with removal of BUILDLINK_{IS,USE}_BUILTIN.

--- pkgtools/x11-links/xfree.mk
+++ pkgtools/x11-links/xfree.mk
@@ -1,20 +1,6 @@
-# $NetBSD: xfree.mk,v 1.7 2004/02/17 16:50:26 jlam Exp $
+# $NetBSD: xfree.mk,v 1.8 2004/03/10 18:35:32 jlam Exp $
 
 FILES_LIST=	${FILESDIR}/xfree
-
-X11_MODULES=	MesaLib glu
-X11_MODULES+=	freetype2
-X11_MODULES+=	xpm
-X11_MODULES+=	Xft2 Xrender expat fontconfig render
-X11_MODULES+=	Xrandr randrext
-X11_MODULES+=	xcursor
-
-# Check if any headers and libraries for ${X11_MODULES} found in
-# ${X11BASE} actually belong to the base XFree86 or not.
-#
-.for _module_ in ${X11_MODULES}
-BUILDLINK_CHECK_BUILTIN.${_module_}=	YES
-.endfor
 
 .include "../../fonts/Xft2/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
@@ -29,8 +15,15 @@ BUILDLINK_CHECK_BUILTIN.${_module_}=	YES
 .include "../../x11/randrext/buildlink3.mk"
 .include "../../x11/xcursor/buildlink3.mk"
 
-.for _module_ in ${X11_MODULES}
-.  if !empty(BUILDLINK_IS_BUILTIN.${_module_}:M[yY][eE][sS])
+# Check if any headers and libraries for ${X11_MODULES} found in
+# ${X11BASE} actually belong to the base XFree86 or not.
+#
+.for _module_ in ${BUILDLINK_PACKAGES}
+CHECK_BUILTIN.${_module_}=	yes
+.  if exists(${BUILDLINK_PKGSRCDIR.${_module_}}/builtin.mk)
+.    include "${BUILDLINK_PKGSRCDIR.${_module_}}/builtin.mk"
+.    if !empty(IS_BUILTIN.${_module_}:M[yY][eE][sS])
 FILES_LIST+=	${FILESDIR}/xfree.${_module_}
+.    endif
 .  endif
 .endfor
