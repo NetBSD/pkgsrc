043e10313f8715329d82de2f5f5f83b6d946c740
minskim <minskim@pkgsrc.org>
2004-03-10 19:29:31 +0000

    Switch to Tcl/Tk 8.4 and bl3ify.  Bump PKGREVISION.

--- x11/py-Tk/buildlink2.mk
+++ x11/py-Tk/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.4 2004/03/08 20:27:17 minskim Exp $
+# $NetBSD: buildlink2.mk,v 1.5 2004/03/10 19:29:31 minskim Exp $
 
 .include "../../lang/python/pyversion.mk"
 
@@ -6,4 +6,4 @@ BUILDLINK_PACKAGES+=		pytk
 BUILDLINK_DEPENDS.pytk?=	${PYPKGPREFIX}-Tk-*
 BUILDLINK_PKGSRCDIR.pytk?=	../../x11/py-Tk
 
-.include "../../x11/tk83/buildlink2.mk"
+.include "../../x11/tk/buildlink2.mk"
