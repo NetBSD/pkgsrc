850e54d3aac39ca406db71018fc5e9df3792e19f
jmmv <jmmv@pkgsrc.org>
2004-03-11 14:01:34 +0000

    Update to 1.0.3:
    libgnomedb 1.0.3, 2004-01-16
    ----------------------------
     - Fixed sorting in grid widget (marius)
     - Fixed selection signal emission in grid widget (rodrigo)
     - Fixed libglade module compilation problems (rodrigo)
     - Fixed compilation problems on Debian (jdassen)
     - Use a grid instead of a list in form widget (jon)
     - Updated translations:
            - ja (aihana)
            - nl (adrighem)
    
    libgnomedb 1.0.2, 2003-11-30
    ----------------------------
     - Avoid creating data sources with duplicating names in GnomeDbDsnConfigDruid
       widget (laurent)
     - Fixes for 64bit platforms (fredreric)
     - Implemented missing gnome_db_combo_set_model (gonzalo)
     - Updated translations:
            - cs (miroslav)
    	- el (pkst)
    	- fi (pauli)
    	- nl (vincent)
    	- pt (duarte)
    	- sr (danilo)
            - sw (christian)
    
    libgnomedb 1.0.1, 2003-10-13
    ----------------------------
     - Fixed problems with gtksourceview compilation (rodrigo)
     - Made labels non-selectable in gray bar widget (álvaro)
     - Fill background color with GTK style color in gray bar (álvaro)
     - Use GTK default text color in grid titles for
       gnome-database-properties (rodrigo)
     - Dont g_free GtkWidget's in gray bar widget finalization (rodrigo)
     - Updated translations:
            - sk (stanislav)

--- databases/libgnomedb/buildlink3.mk
+++ databases/libgnomedb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/05 19:25:09 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/03/11 14:01:34 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGNOMEDB_BUILDLINK3_MK:=	${LIBGNOMEDB_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgnomedb}
 BUILDLINK_PACKAGES+=	libgnomedb
 
 .if !empty(LIBGNOMEDB_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libgnomedb+=		libgnomedb>=1.0.0nb5
+BUILDLINK_DEPENDS.libgnomedb+=		libgnomedb>=1.0.3
 BUILDLINK_PKGSRCDIR.libgnomedb?=	../../databases/libgnomedb
 
 .include "../../databases/libgda/buildlink3.mk"
@@ -20,6 +20,7 @@ BUILDLINK_PKGSRCDIR.libgnomedb?=	../../databases/libgnomedb
 .include "../../devel/libglade2/buildlink3.mk"
 .include "../../devel/libgnomeui/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/gtksourceview/buildlink3.mk"
 
 .endif	# LIBGNOMEDB_BUILDLINK3_MK
 
