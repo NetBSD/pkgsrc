8c6b47ec9c6928e0c8c7d9b39ca863be24cae241
jmmv <jmmv@pkgsrc.org>
2004-03-11 13:17:00 +0000

    Catch up with latest changes WRT BUILDLINK_PACKAGES (AKA, out of date createbuildlink installed...).

--- mail/sylpheed-claws/buildlink3.mk
+++ mail/sylpheed-claws/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/03/11 12:23:19 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/03/11 13:17:00 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use sylpheed-claws.
 #
@@ -12,8 +12,10 @@ SYLPHEED_CLAWS_BUILDLINK3_MK:=	${SYLPHEED_CLAWS_BUILDLINK3_MK}+
 BUILDLINK_DEPENDS+=	sylpheed-claws
 .endif
 
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nsylpheed-claws}
+BUILDLINK_PACKAGES+=	sylpheed-claws
+
 .if !empty(SYLPHEED_CLAWS_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			sylpheed-claws
 BUILDLINK_DEPENDS.sylpheed-claws+=		sylpheed-claws>=0.9.7nb1
 BUILDLINK_PKGSRCDIR.sylpheed-claws?=		../../mail/sylpheed-claws
 
