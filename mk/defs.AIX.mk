530d286b2462fa560cee839e27997a604ff9b3fa
agc <agc@pkgsrc.org>
2004-03-11 22:57:22 +0000

    Get rid of all occurrences of cpp-like tests for ${OPSYS} in bsd.pkg.mk, and use the abstracted means of determining generic shared lib type per operating system.

--- mk/defs.AIX.mk
+++ mk/defs.AIX.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.AIX.mk,v 1.4 2004/01/27 16:11:47 agc Exp $
+# $NetBSD: defs.AIX.mk,v 1.5 2004/03/11 22:57:22 agc Exp $
 #
 # Variable definitions for the AIX operating system.
 
@@ -153,6 +153,7 @@ _OPSYS_HAS_OSSAUDIO=	no	# libossaudio is available
 _OPSYS_PERL_REQD=		# no base version of perl required
 _OPSYS_PTHREAD_AUTO=	no	# -lpthread needed for pthreads
 _OPSYS_RPATH_NAME=	-R	# name of symbol in rpath directive to linker 
+_OPSYS_SHLIB_TYPE=	aixlib	# type of shared lib
 _PATCH_CAN_BACKUP=	yes	# native patch(1) can make backups
 _PATCH_BACKUP_ARG?=	-b -V simple -z 	# switch to patch(1) for backup suffix
 _PREFORMATTED_MAN_DIR=	cat	# directory where catman pages are
--- mk/defs.FreeBSD.mk
+++ mk/defs.FreeBSD.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.FreeBSD.mk,v 1.32 2004/03/09 10:34:31 xtraeme Exp $
+# $NetBSD: defs.FreeBSD.mk,v 1.33 2004/03/11 22:57:22 agc Exp $
 #
 # Variable definitions for the FreeBSD operating system.
 
@@ -140,6 +140,7 @@ _OPSYS_HAS_OSSAUDIO=	yes	# libossaudio is available
 _OPSYS_PERL_REQD=		# no base version of perl required
 _OPSYS_PTHREAD_AUTO=	no	# -lpthread needed for pthreads
 _OPSYS_RPATH_NAME=	-R	# name of symbol in rpath directive to linker 
+_OPSYS_SHLIB_TYPE=	ELF/a.out	# shared lib type
 _PATCH_CAN_BACKUP=	yes	# native patch(1) can make backups
 _PATCH_BACKUP_ARG?=	-V simple -b 	# switch to patch(1) for backup suffix
 _PREFORMATTED_MAN_DIR=	cat	# directory where catman pages are
