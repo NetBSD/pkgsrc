5e0882bb4df69f295c24545f2064321e43e3602e
jlam <jlam@pkgsrc.org>
2004-03-12 21:20:16 +0000

    buildlink3.mk files shouldn't include buildlink2.mk files.

--- x11/gnome-libs/buildlink3.mk
+++ x11/gnome-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/03/05 19:25:42 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/03/12 21:20:16 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_LIBS_BUILDLINK3_MK:=	${GNOME_LIBS_BUILDLINK3_MK}+
@@ -17,14 +17,12 @@ BUILDLINK_DEPENDS.gnome-libs+=		gnome-libs>=1.4.2nb1
 BUILDLINK_PKGSRCDIR.gnome-libs?=	../../x11/gnome-libs
 
 .include "../../audio/esound/buildlink3.mk"
-.if ${OPSYS} == "SunOS"
-.  include "../../databases/db/buildlink2.mk"
-.endif
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../graphics/imlib/buildlink3.mk"
 .include "../../graphics/xpm/buildlink3.mk"
 .include "../../net/ORBit/buildlink3.mk"
 .include "../../x11/gtk/buildlink3.mk"
+.include "../../mk/bdb.buildlink3.mk"
 
 .endif	# GNOME_LIBS_BUILDLINK3_MK
 
