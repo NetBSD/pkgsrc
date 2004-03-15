3f5342c349a8c5d4da57a050164f5f20a29dea24
jlam <jlam@pkgsrc.org>
2004-03-15 16:34:24 +0000

    Remove false comment after latest buildlink3/builtin changes.

--- graphics/Mesa/buildlink3.mk
+++ graphics/Mesa/buildlink3.mk
@@ -1,13 +1,10 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/03/05 19:25:12 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/03/15 16:34:24 jlam Exp $
 
 .if !defined(MESA_BUILDLINK3_MK)
 MESA_BUILDLINK3_MK=	defined
 
 BUILDLINK_PREFIX.Mesa=	${BUILDLINK_PREFIX.MesaLib}
 
-# The ordering is important here as glut/buildlink3.mk sets the version
-# of Mesa required.
-#
 .include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../graphics/glu/buildlink3.mk"
 .include "../../graphics/glut/buildlink3.mk"
