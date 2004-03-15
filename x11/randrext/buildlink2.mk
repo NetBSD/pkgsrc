b69c2be84f15f8c463937b6a689d9878f03fcb32
recht <recht@pkgsrc.org>
2004-03-15 17:30:18 +0000

    Xrandr won't build without the X Extension library which is only present in XFree86>=4.3; ignore it when checking dependencies or buildlinking. Skip also randrext (Xrandr's header files) in this case.
    The patch agains x11-links' builtin.mk is a slightly modified version of
    the patch jlam@ posted in:
    http://mail-index.netbsd.org/tech-pkg/2004/03/15/0001.html
    
    ok'd by jlam@

--- x11/randrext/buildlink2.mk
+++ x11/randrext/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/12/30 23:00:51 cjep Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2004/03/15 17:30:18 recht Exp $
 #
 # This Makefile fragment is included by packages that use randrext.
 #
@@ -8,7 +8,13 @@
 .if !defined(RANDREXT_BUILDLINK2_MK)
 RANDREXT_BUILDLINK2_MK=	# defined
 
+.include "../../mk/bsd.prefs.mk"
+
 BUILDLINK_PACKAGES+=			randrext
+# Xrandr won't build without the X Extension library which is only present
+# in XFree86>=4.3.
+# For this reason we don't need the header files.
+.  if exists(${X11BASE}/include/X11/extensions/extutil.h)
 BUILDLINK_DEPENDS.randrext?=		randrext>=1.0
 BUILDLINK_PKGSRCDIR.randrext?=		../../x11/randrext
 
@@ -16,6 +22,10 @@ EVAL_PREFIX+=	BUILDLINK_PREFIX.randrext=randrext
 BUILDLINK_PREFIX.randrext_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.randrext+=	include/X11/extensions/randr.h
 BUILDLINK_FILES.randrext+=	include/X11/extensions/randrproto.h
+. else
+BUILDLINK_PREFIX.randrext=	${X11BASE}
+BUILDLINK_PREFIX.randrext_DEFAULT=	${X11BASE}
+. endif
 
 BUILDLINK_TARGETS+=	randrext-buildlink
 
