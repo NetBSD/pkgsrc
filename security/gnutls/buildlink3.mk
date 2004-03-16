922c55ddb3984678f862a6023c543a3a1504fe07
jlam <jlam@pkgsrc.org>
2004-03-16 17:58:01 +0000

    BUILDLINK_DEPENDS.<pkg> should be appended to, not set.

--- security/gnutls/buildlink3.mk
+++ security/gnutls/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/03/05 19:25:39 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/03/16 17:58:01 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GNUTLS_BUILDLINK3_MK:=	${GNUTLS_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngnutls}
 BUILDLINK_PACKAGES+=	gnutls
 
 .if !empty(GNUTLS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gnutls?=	gnutls>=1.0.8
+BUILDLINK_DEPENDS.gnutls+=	gnutls>=1.0.8
 BUILDLINK_PKGSRCDIR.gnutls?=	../../security/gnutls
 
 .include "../../archivers/liblzo/buildlink3.mk"
