38bbe23a5401c8b8db5eb59e1b569ad1d68e4002
jlam <jlam@pkgsrc.org>
2004-03-16 18:23:26 +0000

    If the ${PKGBASE} of a package doesn't match the token passed to BUILDLINK_PACKAGES, then set BUILDLINK_PKGBASE.<pkg> explicitly so that we can map from <pkg> to BUILDLINK_PKGBASE.<pkg>.

--- graphics/py-imaging/buildlink3.mk
+++ graphics/py-imaging/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/03/12 02:06:42 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/03/16 18:23:27 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PY_IMAGING_BUILDLINK3_MK:=	${PY_IMAGING_BUILDLINK3_MK}+
@@ -13,6 +13,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npyimaging}
 BUILDLINK_PACKAGES+=	pyimaging
 
 .if !empty(PY_IMAGING_BUILDLINK3_MK:M+)
+BUILDLINK_PKGBASE.pyimaging?=	${PYPKGPREFIX}-imaging
 BUILDLINK_DEPENDS.pyimaging+=	${PYPKGPREFIX}-imaging>=1.1.4
 BUILDLINK_PKGSRCDIR.pyimaging?=	../../graphics/py-imaging
 
