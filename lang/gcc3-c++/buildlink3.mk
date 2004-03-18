855caf377c90319b638ca77036b4f0adf096f2d0
jlam <jlam@pkgsrc.org>
2004-03-18 04:19:39 +0000

    Rename the internal package variable referred to by buildlink[23].  No functional change.

--- lang/gcc3-c++/buildlink3.mk
+++ lang/gcc3-c++/buildlink3.mk
@@ -1,36 +1,36 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/03/16 18:23:27 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2004/03/18 04:19:39 jlam Exp $
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
-GCC3CXX_BUILDLINK3_MK:=	${GCC3CXX_BUILDLINK3_MK}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+GCC3_CXX_BUILDLINK3_MK:=	${GCC3_CXX_BUILDLINK3_MK}+
 
 .include "../../mk/bsd.prefs.mk"
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	gcc3cxx
+BUILDLINK_DEPENDS+=	gcc3-cxx
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngcc3cxx}
-BUILDLINK_PACKAGES+=	gcc3cxx
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngcc3-cxx}
+BUILDLINK_PACKAGES+=	gcc3-cxx
 
-.if !empty(GCC3CXX_BUILDLINK3_MK:M+)
+.if !empty(GCC3_CXX_BUILDLINK3_MK:M+)
 .  if defined(GCC3_INSTALLTO_SUBPREFIX)
 .    if ${GCC3_INSTALLTO_SUBPREFIX} != "gcc3"
 GCC3_PKGMODIF=	_${GCC3_INSTALLTO_SUBPREFIX}
 .    endif
 .  endif
-BUILDLINK_PKGBASE.gcc3cxx?=	gcc3${GCC3_PKGMODIF}-c++
-BUILDLINK_DEPENDS.gcc3cxx+=	gcc3${GCC3_PKGMODIF}-c++>=${_GCC_REQD}
-BUILDLINK_PKGSRCDIR.gcc3cxx?=	../../lang/gcc3-c++
-BUILDLINK_LIBDIRS.gcc3cxx?=	\
-	lib ${_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc3cxx}\///}
+BUILDLINK_PKGBASE.gcc3-cxx?=	gcc3${GCC3_PKGMODIF}-c++
+BUILDLINK_DEPENDS.gcc3-cxx+=	gcc3${GCC3_PKGMODIF}-c++>=${_GCC_REQD}
+BUILDLINK_PKGSRCDIR.gcc3-cxx?=	../../lang/gcc3-c++
+BUILDLINK_LIBDIRS.gcc3-cxx?=	\
+	lib ${_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc3-cxx}\///}
 
 # Packages that link against shared libraries need a full dependency.
 .if defined(USE_GCC_SHLIB)
-BUILDLINK_DEPMETHOD.gcc3cxx+=	full
+BUILDLINK_DEPMETHOD.gcc3-cxx+=	full
 .else
-BUILDLINK_DEPMETHOD.gcc3cxx?=	build
+BUILDLINK_DEPMETHOD.gcc3-cxx?=	build
 .endif
 
-.endif	# GCC3CXX_BUILDLINK3_MK
+.endif	# GCC3_CXX_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
