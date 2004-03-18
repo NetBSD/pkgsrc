5cb37bab543b693fb221ca3e3c67feb0bcbb2399
cube <cube>
2004-03-18 15:28:27 +0000

    Change dependency constraint to 'gkrellm<2.0' so that gkrellm1 plugins don't even try building when gkrellm-2 is installed.

--- sysutils/gkrellm1/buildlink3.mk
+++ sysutils/gkrellm1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/18 09:12:14 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/03/18 15:28:27 cube Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GKRELLM_BUILDLINK3_MK:=	${GKRELLM_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngkrellm}
 BUILDLINK_PACKAGES+=	gkrellm
 
 .if !empty(GKRELLM_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gkrellm+=	gkrellm>=1.2.13nb3
+BUILDLINK_DEPENDS.gkrellm+=	gkrellm<2.0
 BUILDLINK_PKGSRCDIR.gkrellm?=	../../sysutils/gkrellm1
 .endif	# GKRELLM_BUILDLINK3_MK
 
