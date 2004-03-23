00ebf43b6411ff54df0c5263f3a642152e4c1e34
jlam <jlam@pkgsrc.org>
2004-03-23 05:29:38 +0000

    Move inclusion of buildlink3.mk outside of area that's protected from multiple inclusion.  This is needed to ensure that BUILDLINK_PACKAGES is ordered correctly.

--- graphics/Mesa/buildlink3.mk
+++ graphics/Mesa/buildlink3.mk
@@ -1,12 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/03/15 16:34:24 jlam Exp $
-
-.if !defined(MESA_BUILDLINK3_MK)
-MESA_BUILDLINK3_MK=	defined
+# $NetBSD: buildlink3.mk,v 1.5 2004/03/23 05:29:38 jlam Exp $
 
 BUILDLINK_PREFIX.Mesa=	${BUILDLINK_PREFIX.MesaLib}
 
 .include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../graphics/glu/buildlink3.mk"
 .include "../../graphics/glut/buildlink3.mk"
-
-.endif	# MESA_BUILDLINK3_MK
