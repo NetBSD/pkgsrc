cdfe594f9c4e4e5fbee55e5575f8cd9099f85714
jwise <jwise@pkgsrc.org>
2004-03-24 15:24:36 +0000

    Now that we build tcl/tk with thread safety enabled, pull pthread buildlink glue into the tcl buildlink glue.
    Also, update buildlink dependencies appropriately.
    
    Pointed out by wiz@netbsd.org.

--- lang/tcl/buildlink3.mk
+++ lang/tcl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/03/08 19:55:35 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2004/03/24 15:24:36 jwise Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 TCL_BUILDLINK3_MK:=	${TCL_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ntcl}
 BUILDLINK_PACKAGES+=	tcl
 
 .if !empty(TCL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.tcl+=		tcl>=8.4.5
+BUILDLINK_DEPENDS.tcl+=		tcl>=8.4.6nb1
 BUILDLINK_PKGSRCDIR.tcl?=	../../lang/tcl
 
 BUILDLINK_FILES.tcl=	bin/tclsh*
@@ -26,3 +26,5 @@ TCLCONFIG_SH?=		${BUILDLINK_PREFIX.tcl}/lib/tclConfig.sh
 .endif	# TCL_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
+
+.include "../../mk/pthread.buildlink3.mk"
