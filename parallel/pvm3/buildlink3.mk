1d21bb80b5925a7e8855abd26443428f7c765de2
jlam <jlam@pkgsrc.org>
2004-03-25 04:20:49 +0000

    Tell the buildlink3 framework to pass the pvm3/* directories to the compiler and linker since they lie outside the default of ${PREFIX}/include and ${PREFIX}/lib.

--- parallel/pvm3/buildlink3.mk
+++ parallel/pvm3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/05 19:25:39 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/03/25 04:20:49 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PVM_BUILDLINK3_MK:=	${PVM_BUILDLINK3_MK}+
@@ -13,6 +13,11 @@ BUILDLINK_PACKAGES+=	pvm
 .if !empty(PVM_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.pvm+=		pvm>=3.4.3
 BUILDLINK_PKGSRCDIR.pvm?=	../../parallel/pvm3
+
+.  include "${BUILDLINK_PKGSRCDIR.pvm}/arch.mk"
+BUILDLINK_INCDIRS.pvm?=		pvm3/include
+BUILDLINK_LIBDIRS.pvm?=		pvm3/lib/${PVM_ARCH}
+
 .endif	# PVM_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
