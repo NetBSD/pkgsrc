6c3fb39c1723b741f69bbf3a7f376dbd01de94d0
xtraeme <xtraeme@pkgsrc.org>
2004-03-28 19:44:05 +0000

    Initial import of jabberd2 (2.0s2) from pkgsrc-wip.
    Jabber is an open XML protocol for real-time exchange of messages and
    presence notification between any two points on the Internet.  The first
    application of Jabber technology is an asynchronous, extensible instant
    messaging platform, and an IM network that offers functionality similar
    to legacy IM systems such as AIM, ICQ, MSN, and Yahoo.
    
    This is a complete rewrite of the original jabberd.

---
+++ chat/jabberd2/files/sm.sh
@@ -0,0 +1,35 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: sm.sh,v 1.1.1.1 2004/03/28 19:44:07 xtraeme Exp $
+#
+# PROVIDE: sm
+# REQUIRE: DAEMON c2s
+
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
+fi
+
+name="sm"
+rcvar=$name
+command="@PREFIX@/bin/${name}"
+required_files="@PKG_SYSCONFDIR@/${name}.xml"
+extra_commands="reload"
+command_args="2>&1 > /dev/null &"
+sm_user="@JABBERD_USER@"
+pidfile="@JABBERD_PIDDIR@/${name}.pid"
+stop_postcmd="remove_pidfile"
+
+remove_pidfile()
+{
+        if [ -f @JABBERD_PIDDIR@/${name}.pid ]; then
+            rm -f @JABBERD_PIDDIR@/${name}.pid
+        fi
+}
+
+if [ -f /etc/rc.subr ]; then
+	load_rc_config $name
+	run_rc_command "$1"
+else
+	@ECHO@ -n " ${name}"
+	${command} ${sm_flags} ${command_args}
+fi
