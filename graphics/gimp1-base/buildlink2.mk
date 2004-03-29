507803510258187fd2f943c9ea0adc0990599168
xtraeme <xtraeme>
2004-03-29 22:22:54 +0000

    gimp-base -> gimp1-base

--- graphics/gimp1-base/buildlink2.mk
+++ graphics/gimp1-base/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2004/03/29 22:06:48 xtraeme Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2004/03/29 22:24:20 xtraeme Exp $
 #
 # This Makefile fragment is included by packages that use gimp-base.
 #
@@ -10,7 +10,7 @@ GIMP_BASE_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gimp-base
 BUILDLINK_DEPENDS.gimp-base?=		gimp-base>=1.2.5nb2
-BUILDLINK_PKGSRCDIR.gimp-base?=		../../graphics/gimp-base
+BUILDLINK_PKGSRCDIR.gimp-base?=		../../graphics/gimp1-base
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gimp-base=gimp-base
 BUILDLINK_PREFIX.gimp-base_DEFAULT=	${LOCALBASE}
