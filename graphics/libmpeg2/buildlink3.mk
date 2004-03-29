b8a8ccc5aec35b380c662db81c44365ee4578033
xtraeme <xtraeme@pkgsrc.org>
2004-03-29 20:48:16 +0000

    bl3ify

---
+++ graphics/libmpeg2/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/03/29 20:48:16 xtraeme Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBMPEG2_BUILDLINK3_MK:=	${LIBMPEG2_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libmpeg2
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibmpeg2}
+BUILDLINK_PACKAGES+=	libmpeg2
+
+.if !empty(LIBMPEG2_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libmpeg2+=	libmpeg2>=0.4.0
+BUILDLINK_PKGSRCDIR.libmpeg2?=	../../graphics/libmpeg2
+.endif	# LIBMPEG2_BUILDLINK3_MK
+
+.include "../../devel/SDL/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
