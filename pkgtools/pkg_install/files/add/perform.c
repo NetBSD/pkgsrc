e893fbc27c48499be2631ef6b733371b473c980f
tv <tv>
2004-03-29 20:27:46 +0000

    Remove umask call and remove warning on Interix; OK'd by hubertf.

--- pkgtools/pkg_install/files/add/perform.c
+++ pkgtools/pkg_install/files/add/perform.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: perform.c,v 1.17 2004/03/29 02:25:06 tv Exp $	*/
+/*	$NetBSD: perform.c,v 1.18 2004/03/29 20:27:46 tv Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -11,7 +11,7 @@
 #if 0
 static const char *rcsid = "from FreeBSD Id: perform.c,v 1.44 1997/10/13 15:03:46 jkh Exp";
 #else
-__RCSID("$NetBSD: perform.c,v 1.17 2004/03/29 02:25:06 tv Exp $");
+__RCSID("$NetBSD: perform.c,v 1.18 2004/03/29 20:27:46 tv Exp $");
 #endif
 #endif
 
@@ -720,9 +720,10 @@ ignore_replace_depends_check:
 	if (!NoRecord && !Fake) {
 		char    contents[FILENAME_MAX];
 
-		umask(022);
+#ifndef __INTERIX
 		if (getuid() != 0)
 			warnx("not running as root - trying to record install anyway");
+#endif
 		if (!PkgName) {
 			warnx("no package name! can't record package, sorry");
 			errc = 1;
