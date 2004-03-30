79dc1d00e389545867e43d59d5647dc79a91ecdb
ben <ben@pkgsrc.org>
2004-03-30 18:23:46 +0000

    Update tex2rtf to version 2.01.  A changelog is not available, but this version of tex2rtf is updated to work with newer versions of wxwindows, and the prior version is referred to as obsolete.  I am told that some of the patches duplicate work that is done in CVS, so some patches should disappear over time.

---
+++ converters/tex2rtf/files/stringmatch.h
@@ -0,0 +1,5 @@
+#include "wx/object.h"
+#include "wx/list.h"
+#include "wx/filefn.h"
+
+WXDLLEXPORT bool wxStringMatch2(const wxChar *one, const wxChar *two, bool subString = TRUE, bool exact = FALSE);
