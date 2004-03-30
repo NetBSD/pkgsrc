4963912e93175b7fc053953d415310b844b8065d
jlam <jlam@pkgsrc.org>
2004-03-30 16:19:51 +0000

    Always pass the location of the db[34] headers since we install them in a non-standard location.  Also match the new USE_DB185 behaviour where it defaults to "yes".

--- databases/db4/buildlink3.mk
+++ databases/db4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2004/03/22 18:18:26 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2004/03/30 16:20:19 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 DB4_BUILDLINK3_MK:=	${DB4_BUILDLINK3_MK}+
@@ -15,13 +15,14 @@ BUILDLINK_PACKAGES+=	db4
 .if !empty(DB4_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.db4+=		db4>=4.2.52
 BUILDLINK_PKGSRCDIR.db4?=	../../databases/db4
-.  if defined(USE_DB185)
+BUILDLINK_INCDIRS.db4?=		include/db4
+BUILDLINK_TRANSFORM+=		l:db-4:db4
+USE_DB185?=			yes
+.  if !empty(USE_DB185:M[yY][eE][sS])
 #
 # Older db4 packages didn't enable the db-1.85 compatibility API.
 #
 BUILDLINK_DEPENDS.db4+=		db4>=4.2.52nb1
-BUILDLINK_INCDIRS.db4?=		include/db4
-BUILDLINK_TRANSFORM+=		l:db-4:db4
 BUILDLINK_TRANSFORM+=		l:db:db4
 .  endif
 .endif	# DB4_BUILDLINK3_MK
