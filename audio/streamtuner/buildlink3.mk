1f2b549185990cbc2defb9762a2bd83b6192695b
xtraeme <xtraeme@pkgsrc.org>
2004-03-30 22:21:31 +0000

    Update to 0.12.1 and bl3ify.
    0.12.1                                  30 March 2004
    
            * Fixed bookmarks loading
    
    0.12.0                                  30 March 2004
    
            * The bookmarks are now global
            * A preselections handler has been implemented
            * The properties dialog can now edit a stream
            * Some streams can now be deleted
            * HTTP transfers are now compressed if possible (suggested by
              Marcus Rückert)
            * HTTP transfers now follow redirections
            * Socks 5 proxy support has been added
            * The documentation is now in DocBook format
            * A splash screen has been implemented
            * Some icons have been changed
            * Tabs can now be reordered
            * GTK+ interactive search has been fixed
            * Several UI improvements have been performed
            * The API has been enriched
            * The overall performance has been greatly improved
            * Major code cleanups have been performed
            * Several bugs and memory leaks have been fixed
            * streamtuner-theme-convert has been removed
            * Now requires GTK+ version 2.2.2 or superior
            * Now requires libcurl version 7.10.5 or superior
    
    0.11.1                                  07 February 2004
    
            * Fixed a deadlock on Linux and other platforms having
              non-recursive mutexes (reported by Marcus Rückert)
            * Fixed a crlf parsing bug in transfer code
            * A minor bug in tab switching has been fixed
    
    0.11.0                                  31 January 2004
    
            * Reimplemented bookmarks support (unseen since 0.7.3)
            * Multiple stream selection is now possible (a new event has
              been added, ST_HANDLER_EVENT_STREAM_TUNE_IN_MULTIPLE)
            * Categories are now displayed when they are selected
            * Removed the "Stop tune in" feature and the task dialogs
            * The stop button now is now global
            * Removed the category menu and moved its items to the view
              menu
            * The transfer API has been improved:
                    - retrieval of HTTP headers
                    - UNIX, DOS and Mac newline support
                    - cleaner API
            * Introduced the st_action API, obsoleting st_programs
            * Implemented a minimal m3u and pls API
            * Added st_handler_set_description() and
              st_handler_set_home(): a clickable label pointing to the
              currently selected handler's home is now displayed in the
              toolbar
            * GTK_STOCK_REFRESH and GTK_STOCK_STOP now replaces
              ST_STOCK_RELOAD and ST_STOCK_STOP
            * Categories now have a descriptive icon
            * Tab icons can now be hidden
            * Added an "Always refresh categories" option
            * The SHOUTcast plugin can now parse the charset information
              of the Content-Type HTTP header
            * A SHOUTcast playlist writing bug has been fixed
            * Multi-threading is now mandatory

---
+++ audio/streamtuner/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/03/30 22:21:31 xtraeme Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+STREAMTUNER_BUILDLINK3_MK:=	${STREAMTUNER_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	streamtuner
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nstreamtuner}
+BUILDLINK_PACKAGES+=	streamtuner
+
+.if !empty(STREAMTUNER_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.streamtuner+=		streamtuner>=0.12.1
+BUILDLINK_PKGSRCDIR.streamtuner?=	../../audio/streamtuner
+.endif	# STREAMTUNER_BUILDLINK3_MK
+
+.include "../../www/curl/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
