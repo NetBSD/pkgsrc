040966b7fb09f27912197474bbd3656e4efc88a8
jmmv <jmmv@pkgsrc.org>
2004-04-01 18:57:44 +0000

    Update to 0.3.2:
    gnome-speech 0.3.2:
    
    * Drivers now take into account the language specified in getVoices
      and the createSpeaker calls.
    
    gnome-speech 0.3.1:
    
    * Fix some driver strings to fix activation errors.
    
    gnome-speech 0.3.0:
    
    * Updated VoiceInfo structure such that language is a string rather
      than an int.

--- audio/gnome-speech/buildlink3.mk
+++ audio/gnome-speech/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/03/18 09:12:08 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/04/01 18:57:44 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_SPEECH_BUILDLINK3_MK:=	${GNOME_SPEECH_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngnome-speech}
 BUILDLINK_PACKAGES+=	gnome-speech
 
 .if !empty(GNOME_SPEECH_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gnome-speech+=	gnome-speech>=0.2.7
+BUILDLINK_DEPENDS.gnome-speech+=	gnome-speech>=0.3.2
 BUILDLINK_PKGSRCDIR.gnome-speech?=	../../audio/gnome-speech
 .endif	# GNOME_SPEECH_BUILDLINK3_MK
 
