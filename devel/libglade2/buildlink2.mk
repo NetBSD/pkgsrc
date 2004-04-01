56613fa8287442174e67627b0134267fe39ebc10
jmmv <jmmv@pkgsrc.org>
2004-04-01 18:16:46 +0000

    Update to 2.3.6:
    libglade-2.3.6: 10-March-2004
    - Implement support GtkComboBoxEntry, GtkToolItem, GtkToolButton,
      GtkToggleToolButton, GtkRadioToolButton & GtkSeparatorToolItem (Damon
      Chaplin)
    - Fix automake warning (Steve Chaplin)
    - Add libglade version number to the docs (Matthias Clasen)
    - Fix usage of C++ keyword as an argument name (Bas Driessen)
    - Allow additional signal parameter for glademm (Christof Petig)
    - Support tooltips in toolbars (Michael Voigt)
    
    libglade-2.3.2: 15-January-2004
    - Support disambiguating msgids by prefixing them with a context
      string. (Christian Stimming, Matthias Clasen)
    
    libglade-2.3.1:  14-November-2003
    - Register GtkColorButton, GtkComboBox, GtkFileChooser and
      GtkFontButton types (Jonathan Blandford)
    - Implemement support for GtkExpander (Mark McLoughlin)
    - Fix memory leak and incorrect colour map usage (Morten Welinder)
    - Win32 build support (Tor Lillqvist, Masahiro Sakai)
    - Build fixes (James Henstridge, Michael Meeks)

--- devel/libglade2/buildlink2.mk
+++ devel/libglade2/buildlink2.mk
@@ -1,21 +1,16 @@
-# $NetBSD: buildlink2.mk,v 1.9 2004/01/03 18:49:38 reed Exp $
+# $NetBSD: buildlink2.mk,v 1.10 2004/04/01 18:16:46 jmmv Exp $
 
 .if !defined(LIBGLADE2_BUILDLINK2_MK)
 LIBGLADE2_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libglade2
-BUILDLINK_DEPENDS.libglade2?=	libglade2>=2.0.1nb13
+BUILDLINK_DEPENDS.libglade2?=	libglade2>=2.3.6
 BUILDLINK_PKGSRCDIR.libglade2?=	../../devel/libglade2
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.libglade2=libglade2
 BUILDLINK_PREFIX.libglade2_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.libglade2=	include/libglade-2.0/glade/*
 BUILDLINK_FILES.libglade2+=	lib/libglade-2.0.*
-
-BUILDLINK_CPPFLAGS.libglade2= \
-	-I${BUILDLINK_PREFIX.libglade2}/include/libglade-2.0
-
-CPPFLAGS+=	${BUILDLINK_CPPFLAGS.libglade2}
 
 .include "../../textproc/libxml2/buildlink2.mk"
 .include "../../x11/gtk2/buildlink2.mk"
