bc066bdb46952a9b287c493aa3b6ace6ab887feb
jmmv <jmmv@pkgsrc.org>
2004-04-01 19:04:59 +0000

    Update to 1.4.0:
    * View by script as well as by Unicode block.
    * New documentation in the following languages: de es fr it ja ko sv zh_CN
      zh_HK zh_TW
    * English documentation improved and updated.
    * Search modified to look in CJK ideograph definitions as well as character
      names.  Search heuristic also improved in other areas.
    * Bothersome gucharmap input method removed.
    * Many user interface improvements.
    * Much code reorganized.
    * New and updated translations: ar az bn ca cs cy da de el en_CA en_GB es fa
      fi fr ga hi id it ja ko lt mn ms nl no pa pl pt pt_BR sk sq sr sr@Latn sv
      th uk zh_CN

--- fonts/gucharmap/buildlink3.mk
+++ fonts/gucharmap/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/03/26 02:27:40 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2004/04/01 19:04:59 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GUCHARMAP_BUILDLINK3_MK:=	${GUCHARMAP_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngucharmap}
 BUILDLINK_PACKAGES+=	gucharmap
 
 .if !empty(GUCHARMAP_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gucharmap+=	gucharmap>=1.2.0nb2
-BUILDLINK_RECOMMENDED.gucharmap?=	gucharmap>=1.2.0nb8
+BUILDLINK_DEPENDS.gucharmap+=	gucharmap>=1.4.0
 BUILDLINK_PKGSRCDIR.gucharmap?=	../../fonts/gucharmap
 .endif	# GUCHARMAP_BUILDLINK3_MK
 
