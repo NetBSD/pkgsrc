8c3a56999825a891f793cbfc44a20c364acd3bc9
jmmv <jmmv>
2004-04-01 18:36:11 +0000

    Update to 2.6.0.  Changes since previous stable version:
    Version 2.6.0
    =============
    
      Translators
    
    	* Gareth Owen (en_GB)
    
    Version 2.5.92
    ==============
    
      Translations
    
    	* Sayed Jaffer Al-Mosawi (ar)
    	* Peter \"Peshka\" Slavov (bg)
    	* Alexander Winston (en_CA)
    	* Elian Myftiu (sq)
    	* Danilo Segan (sr)
    	* Gurban Mohemmet Tewekgeli (tk)
    	* Maxim Dziumanenko (uk)
    
    Version 2.5.91
    ==============
    
      libgnome-desktop
    
    	* Fix issue where dragging a file from an nfs mounted dir onto
    	  a launcher wouldn't work (Mark)
    	* Make the launcher editor use GtkFileChooser (Brian Skahan)
    
      Misc
    
    	* Don't depend on strptime (Mark)
    	* Check for scrollkeeper in configure (Mark)
    	* Fixup OMF files to validate with latest scrollkeeper (Mark)
    
      Translations
    
    	* Laszlo Dvornik (hu)
    	* Luca Ferretti (it)
    	* Amanpreet Singh Alam (pa)
    	* Duarte Loreto (pt)
    	* Elian Myftiu (sq)
    	* Dinesh Nadarajah (ta)
    	* Paisa Seeluangsawat (th)
    
    Version 2.5.90
    ==============
    
      Misc
    
    	* Allow build against an uninstalled build (Laszlo Peter)
    
    Version 2.5.5
    =============
    
      Misc
    
    	* Use the new applications icon for the Applications menu (Mark)
    
    Version 2.5.4
    =============
    
      About Dialog
    
    	* Set intro text as non-editable (Fernando Herrera)
    	* Plug some leaks (Kjartan Maraas)
    
      libgnome-desktop
    
    	* Include correct i18n header to fix warnings (The Written Word)
    
      Misc
    
    	* Use the common docs build system (Malcolm Tredinnick)
    	* Small configure.in cleanup (Tomasz K³oczko)
    	* Fix underquoted AM_PATH_ESD issue (Alexander Winston)
    	* Don't included generated .desktop files in tarball (Mark)
    	* Distcheck fix (Mark)
    
      Translators
    
    	* Christophe Merlet (RedFox) (fr)
    	* Kjartan Maraas (no)
    	* Miloslav Trmac (cs)
    
    Version 2.5.3
    =============
    
      About Dialog
    
    	* Update the "Contact" link (Alex Duggan)
    
      Misc
    
    	* FDL/GPL/LGPL docs updates (Eric Baudais)
    
      Translators
    
    	* Dmitry G. Mastrukov (ru)
    
    Version 2.5.2
    =============
    
      libgnome-desktop
    
    	* Handle g_filename_from_utf8() failure (Dennis Cranston)
    
      Translators
    
    	* Danilo Segan (sr)
    	* Kjartan Maraas (no)
    	* Kostas Papadimas (el)
    
    Version 2.5.1
    =============
    
      libgnome-desktop
    
    	* Remove unnecessary accessibility code (Padraig)
    	* Use the correct mnemonic widget for the command entry (Padraig)
    
      Misc
    
    	* Add a --enable-deprecations build flag (Mark)
    
      Translators
    
    	Abel Cheung (zh_TW)
    	G Karunakar (hi)
    	Jordi Mallach (ca)
    	Ole Laursen (da)
    	Sanlig Badral (mn)

--- x11/gnome-desktop/buildlink3.mk
+++ x11/gnome-desktop/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/03/26 02:27:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/04/01 18:36:11 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_DESKTOP_BUILDLINK3_MK:=	${GNOME_DESKTOP_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngnome-desktop}
 BUILDLINK_PACKAGES+=	gnome-desktop
 
 .if !empty(GNOME_DESKTOP_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gnome-desktop+=	gnome-desktop>=2.4.1.1nb2
-BUILDLINK_RECOMMENDED.gnome-desktop?=	gnome-desktop>=2.4.1.1nb5
+BUILDLINK_DEPENDS.gnome-desktop+=	gnome-desktop>=2.6.0
 BUILDLINK_PKGSRCDIR.gnome-desktop?=	../../x11/gnome-desktop
 .endif	# GNOME_DESKTOP_BUILDLINK3_MK
 
