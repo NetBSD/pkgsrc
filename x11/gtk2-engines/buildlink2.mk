1d56852b86b8efedcb088be58aa658d4708f2db4
jmmv <jmmv@pkgsrc.org>
2004-04-01 19:02:34 +0000

    Bump PKGREVISION to 6 and fix paths to installed files, due to the update of GTK+ to 2.4.

--- x11/gtk2-engines/buildlink2.mk
+++ x11/gtk2-engines/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2004/01/03 18:49:57 reed Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2004/04/01 19:02:34 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use gtk2-engines.
 #
@@ -9,7 +9,7 @@
 GTK2_ENGINES_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gtk2-engines
-BUILDLINK_DEPENDS.gtk2-engines?=	gtk2-engines>=2.2.0nb4
+BUILDLINK_DEPENDS.gtk2-engines?=	gtk2-engines>=2.2.0nb6
 BUILDLINK_PKGSRCDIR.gtk2-engines?=	../../x11/gtk2-engines
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gtk2-engines=gtk2-engines
