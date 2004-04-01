0763e9c4b68dce58269af8add019b7a0e0a6fc8e
jmmv <jmmv@pkgsrc.org>
2004-04-01 18:29:40 +0000

    Update to 2.6.0:
    libgnomeprint 2.5.3
    	* Some GtkFileChooser adjustments
    	* Fix leak (133415)
    	* Fix printing crash under cups if no ppd file available (132872)
    	* Use g_get_tmp_dir rather than hard coding directory (133926)
    	* Fix header order (required for Darwin) (134806)
    	* Fix PDF locale bug
    	* Accessibility fixes
    
    libgnomeprint 2.5.2
    	* Various fixes
    	* Remove setlocale invocations where they change the
    	  locale (109446)
    	* Fix leak and erroneous model names (131359)
    	* Fix encoding mismatch for cups ppd files
    	* Fix mishandling of font weights (89580)
    	* Fix unconfimed overwriting of files (assuming people use
    	  gnome_print_dialog_run)
    	* Switch to GtkFileChooser
    	* Make the print preview theme-able
    
    libgnomeprint 2.5.1
    	* Handle ttc files
    	* Handle duplex and tumble in postscript files
    	* Improve handling of duplex printing (#128036)
    	* HIGify print dialog (#128614)
    	* Add delayed printing support in cups module
    	* Remove modifying calls to setlocale
    	* Remove markup from translatable text
    	* Offer users the possibility to display several
    	  pages together (#113956).
    	* add source (tray) selector to paper selector (for
    	  cups module)
    
    libgnomeprint 2.5.0.1
    	http://bugzilla.gnome.org/show_bug.cgi?id=126046
    	* make gnumeric work correctly with cups via gnome-print.
    	* handle fonts with spaces in postscript export
    	* Change cups module to load ppd files on demand
    	http://bugzilla.gnome.org/show_bug.cgi?id=115566
    	* Protect against libcups changing the locale
    	http://bugzilla.gnome.org/show_bug.cgi?id=125836
    	* Implement a better fallback than first font.
    	* Replace filename entry field with file selector.
    	* Adjust Preview windo size
    	http://bugzilla.gnome.org/show_bug.cgi?id=124307
    	* Make ctl-q and ctl-w close the preview window
    	* Reduce the usage of setlocale
    
    libgnomeprint 2.5.0
    	http://bugzilla.gnome.org/show_bug.cgi?id=125828
    	* Apply patch (by bland@mail.ru) to improve performance
              exporting large images.
    	http://bugzilla.gnome.org/show_bug.cgi?id=122741
    	* Work around unreliable cupsGetDefault
    	* Fix some leaks
    	* Fix locating the lpr command.
    	http://bugzilla.gnome.org/show_bug.cgi?id=120954
    	* Use Sans Regular rather than Helvetica as fallback
    	http://bugzilla.gnome.org/show_bug.cgi?id=120910
    	* Fix clipping

--- print/libgnomeprint/buildlink3.mk
+++ print/libgnomeprint/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/03/18 09:12:14 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/04/01 18:29:40 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGNOMEPRINT_BUILDLINK3_MK:=	${LIBGNOMEPRINT_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgnomeprint}
 BUILDLINK_PACKAGES+=	libgnomeprint
 
 .if !empty(LIBGNOMEPRINT_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libgnomeprint+=	libgnomeprint>=2.4.2nb1
+BUILDLINK_DEPENDS.libgnomeprint+=	libgnomeprint>=2.6.0
 BUILDLINK_PKGSRCDIR.libgnomeprint?=	../../print/libgnomeprint
 .endif	# LIBGNOMEPRINT_BUILDLINK3_MK
 
