abb9e9cab85c10feeeae676ac26fc1ddd7e2809b
jmmv <jmmv>
2004-04-01 18:16:46 +0000

    Update to 2.3.6:
    libglade-2.3.6: 10-March-2004
    - Implement support GtkComboBoxEntry, GtkToolItem, GtkToolButton,
      GtkToggleToolButton, GtkRadioToolButton & GtkSeparatorToolItem (Damon
      Chaplin)
    - Fix automake warning (Steve Chaplin)
    - Add libglade version number to the docs (Matthias Clasen)
    - Fix usage of C++ keyword as an argument name (Bas Driessen)
    - Allow additional signal parameter for glademm (Christof Petig)
    - Support tooltips in toolbars (Michael Voigt)
    
    libglade-2.3.2: 15-January-2004
    - Support disambiguating msgids by prefixing them with a context
      string. (Christian Stimming, Matthias Clasen)
    
    libglade-2.3.1:  14-November-2003
    - Register GtkColorButton, GtkComboBox, GtkFileChooser and
      GtkFontButton types (Jonathan Blandford)
    - Implemement support for GtkExpander (Mark McLoughlin)
    - Fix memory leak and incorrect colour map usage (Morten Welinder)
    - Win32 build support (Tor Lillqvist, Masahiro Sakai)
    - Build fixes (James Henstridge, Michael Meeks)

--- devel/libglade2/buildlink3.mk
+++ devel/libglade2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/03/18 09:12:10 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/04/01 18:16:46 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGLADE2_BUILDLINK3_MK:=	${LIBGLADE2_BUILDLINK3_MK}+
@@ -11,9 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibglade2}
 BUILDLINK_PACKAGES+=	libglade2
 
 .if !empty(LIBGLADE2_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libglade2+=	libglade2>=2.0.1nb13
+BUILDLINK_DEPENDS.libglade2+=	libglade2>=2.3.6
 BUILDLINK_PKGSRCDIR.libglade2?=	../../devel/libglade2
-BUILDLINK_INCDIRS.libglade2?=	include/libglade-2.0
 .endif	# LIBGLADE2_BUILDLINK3_MK
 
 .include "../../textproc/libxml2/buildlink3.mk"
