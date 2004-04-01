5ca5f0453257b09226d906ac9f857ed36927fd4e
jmmv <jmmv@pkgsrc.org>
2004-04-01 18:23:21 +0000

    Update to 2.6.0.  The list of changes since the previous stable version may be incomplete, but couldn't find anything better:
    libbonoboui 2.5.3
    	* bugs fixed
    		+ toolbar/dock destruction crasher (Michael)
    		+ unused arg cleanup (Fernando Herrera)
    	* featurelets
    		+ BonoboWindow cleanup (Arvind Samptur)
    
    libbonoboui 2.5.2
    	* bugs fixed
    		+ require the right gtk+ (Jason Leach)
    		+ AIX portability (The Written Word)
    		+ svg icon image size (Sergey V. Udaltsov)
    		+ status bar grip problem (Michael)
    		+ control 'activate' signal (Federico, JP)
    		+ memove not strcpy for overlap (JP)
    		+ dock size allocation problem (Michael)
    	* featurelets
    		+ status bar packing improvement (Alexander Larsson)

--- devel/libbonoboui/buildlink3.mk
+++ devel/libbonoboui/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/03/26 02:27:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/04/01 18:23:21 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBBONOBOUI_BUILDLINK3_MK:=	${LIBBONOBOUI_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibbonoboui}
 BUILDLINK_PACKAGES+=	libbonoboui
 
 .if !empty(LIBBONOBOUI_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libbonoboui+=		libbonoboui>=2.4.1nb2
-BUILDLINK_RECOMMENDED.libbonoboui?=	libbonoboui>=2.4.3nb2
+BUILDLINK_DEPENDS.libbonoboui+=		libbonoboui>=2.6.0
 BUILDLINK_PKGSRCDIR.libbonoboui?=	../../devel/libbonoboui
 .endif	# LIBBONOBOUI_BUILDLINK3_MK
 
