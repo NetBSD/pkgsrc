ba818b68e15dbcde31b9b26eb333920835ce4a43
jmmv <jmmv>
2004-04-01 18:32:03 +0000

    Update to 2.1.8.  Sorry, no list of changes available, only the ChangeLog, which is way _too_ long.

--- net/libsoup/buildlink3.mk
+++ net/libsoup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/03/26 02:27:48 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/04/01 18:32:03 jmmv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBSOUP_BUILDLINK3_MK:=	${LIBSOUP_BUILDLINK3_MK}+
@@ -11,13 +11,13 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibsoup}
 BUILDLINK_PACKAGES+=	libsoup
 
 .if !empty(LIBSOUP_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libsoup+=	libsoup>=1.99.26
-BUILDLINK_RECOMMENDED.libsoup?=	libsoup>=1.99.26nb1
+BUILDLINK_DEPENDS.libsoup+=	libsoup>=2.1.8
 BUILDLINK_PKGSRCDIR.libsoup?=	../../net/libsoup
 .endif	# LIBSOUP_BUILDLINK3_MK
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/popt/buildlink3.mk"
-.include "../../security/openssl/buildlink3.mk"
+.include "../../security/gnutls/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
