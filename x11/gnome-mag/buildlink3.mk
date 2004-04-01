550efddc00bb4bee0c80acd8b1376a475dfe0216
jmmv <jmmv@pkgsrc.org>
2004-04-01 18:58:40 +0000

    Update to 0.10.10, the version addressed to GNOME 2.6.0:
    What's new in gnome-mag-0.10.10:
    
    * languages:
    Alexander Winston (en_CA)
    Gareth Owen (en_GB)
    John C Barstow (mi)
    
    What's new in gnome-mag-0.10.9:
    
    * languages:
      MÉtin Æmirov (az)
      Alexander Winston (en_CA)
      Amanpreet Singh Alam (pa)
      Paisa Seeluangsawat (th)
      Gurban Mohemmet Tewekgeli (tk)
    
    What's new in gnome-mag-0.10.8:
    
    * fix for builds with non-standard X directories. [bug 130787, from
      Fernando Herrera]
    
    * smooth-scrolling is now the default.
    
    * magnifier toplevel window is now TYPE_POPUP as it
      needs to be override-redirect and always on top.
    
    * fixes to benchmarking code reports (Brian Cameron).
    
    * cache GC's used in magnifier, to reduce X traffic (Brian Cameron).
    
    * makefile cleanup: (Tomasz K³oczko)
    
    * languages:
      punjabi  Guntupalli Karunakar
      thai	   Paisa Seeluangsawat
      croation Robert Sedak
      arabic   Arafat Medini

--- x11/gnome-mag/buildlink3.mk
+++ x11/gnome-mag/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/03/18 09:12:16 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2004/04/01 18:58:40 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_MAG_BUILDLINK3_MK:=	${GNOME_MAG_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngnome-mag}
 BUILDLINK_PACKAGES+=	gnome-mag
 
 .if !empty(GNOME_MAG_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gnome-mag+=	gnome-mag>=0.10.4nb1
+BUILDLINK_DEPENDS.gnome-mag+=	gnome-mag>=0.10.10
 BUILDLINK_PKGSRCDIR.gnome-mag?=	../../x11/gnome-mag
 .endif	# GNOME_MAG_BUILDLINK3_MK
 
