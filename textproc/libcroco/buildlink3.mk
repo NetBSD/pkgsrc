410416617b8b5ca639792529e2c4d0c76b8662e8
jmmv <jmmv@pkgsrc.org>
2004-04-01 18:26:22 +0000

    Update to 0.5.0:
    - better regression suite framework [Dodji Seketeli]
    - support of the 'color' property gathering [Benjamin Dauvergne]
    - furious memory leaks/corruptions hunting [Dodji Seketeli, Rob BUIS]
    - total support of the !important keyword [Dodji Seketeli, Rob BUIS]
    - selectors evaluation in csslint [Dodji Seketeli, Rob BUIS]
    - fix compilation warnings [David A Knight, Dodji Seketeli]
    - more complete CSS statement serialisation [Dodji Seketeli]
    - better gathering of style information [Dodjij Seketeli]
    - better cascading algorithm conformance [Dodji Seketeli, Rob BUIS]
    - @import rule parsing fix [Dodji Seketeli]
    - support of CSS3 indentifiers [Dodji Seketeli]
    - better w3c DOM integration support [Rob BUIS, Dodji Seketeli]
    - many parsing fixes [Dodji Seketeli]
    - style structure debuging facility [Dodji Seketeli]
    - better fragment parsing support [Dodji Seketeli]
    - Split libcroco and sewfox [Dodji Seketeli]
    - class selector evaluation fix [Rob BUIS]
    - csslint cleanup and bugfixes [Dodji Seketeli]
    - :first-child pseudo class selector support [Dodji Seketeli]
    - pluggable pseudo class selectors evaluators [Dodji Seketeli]

--- textproc/libcroco/buildlink3.mk
+++ textproc/libcroco/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/03/26 02:27:55 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/04/01 18:26:22 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBCROCO_BUILDLINK3_MK:=	${LIBCROCO_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibcroco}
 BUILDLINK_PACKAGES+=	libcroco
 
 .if !empty(LIBCROCO_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libcroco+=	libcroco>=0.4.0
-BUILDLINK_RECOMMENDED.libcroco?=	libcroco>=0.4.0nb3
+BUILDLINK_DEPENDS.libcroco+=	libcroco>=0.5.0
 BUILDLINK_PKGSRCDIR.libcroco?=	../../textproc/libcroco
 .endif	# LIBCROCO_BUILDLINK3_MK
 
