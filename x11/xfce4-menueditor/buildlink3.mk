eae083c88dcfac4ef5d56ade6517433d8da87bb7
martti <martti@pkgsrc.org>
2004-04-02 10:07:26 +0000

    Updated xfce4-menueditor to 1.05
    * filesel dialog for launcher command in edit and add dialog
    * filesel dialog for new menu filename
    * check if all field are filled when adding and creating a new menu
    * no more gint response (might cause prbl at compiling under NetBSD)
    * (begin of) icon support added
    * text in tree formatted
    * patch for my_tree_store_swap_up and my_tree_store_swap_down applied
    * menueditor.c splitted into more files
    * bug in my_tree_store_swap_up and my_tree_store_swap_down leading to
      corrupted menu file fixed.

--- x11/xfce4-menueditor/buildlink3.mk
+++ x11/xfce4-menueditor/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/18 09:12:17 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/04/02 10:07:26 martti Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_MENUEDITOR_BUILDLINK3_MK:=	${XFCE4_MENUEDITOR_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-menueditor}
 BUILDLINK_PACKAGES+=	xfce4-menueditor
 
 .if !empty(XFCE4_MENUEDITOR_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.xfce4-menueditor+=	xfce4-menueditor>=1.0rc3nb2
+BUILDLINK_DEPENDS.xfce4-menueditor+=	xfce4-menueditor>=1.0.5
 BUILDLINK_PKGSRCDIR.xfce4-menueditor?=	../../x11/xfce4-menueditor
 .endif	# XFCE4_MENUEDITOR_BUILDLINK3_MK
 
