cf8820867aae1cf6638a3e4eeb831838c687fe84
recht <recht>
2004-04-02 18:11:57 +0000

    initial import of libsoup-devel 2.1.8 (former libsoup) needed by evolution-data-server
    Libsoup is an HTTP library implementation in C. It was originally part
    of a SOAP (Simple Object Access Protocol) implementation called Soup, but
    the SOAP and non-SOAP parts have now been split into separate packages.
    
    libsoup uses the Glib main loop and is designed to work well with GTK
    applications. This enables GNOME applications to access HTTP servers
    on the network in a completely asynchronous fashion, very similar to
    the Gtk+ programming model (a synchronous operation mode is also
    supported for those who want it).
    
    Features:
      * Completely Asynchronous
      * Connection cache
      * HTTP chunked transfer support
      * HTTP, SOCKS4, and SOCKS5 authenticated proxy support
      * SSL Support using OpenSSL or GnuTLS
      * Client support for Digest, NTLM, and Basic authentication
      * HTTP server
      * Server support for Digest and Basic authentication

---
+++ net/libsoup-devel/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/04/02 18:11:57 recht Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBSOUP_DEVEL_BUILDLINK3_MK:=	${LIBSOUP_DEVEL_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libsoup-devel
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibsoup-devel}
+BUILDLINK_PACKAGES+=	libsoup-devel
+
+.if !empty(LIBSOUP_DEVEL_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libsoup-devel+=	libsoup-devel>=2.1.8
+BUILDLINK_PKGSRCDIR.libsoup-devel?=	../../net/libsoup-devel
+.endif	# LIBSOUP_DEVEL_BUILDLINK3_MK
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/popt/buildlink3.mk"
+.include "../../security/gnutls/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
