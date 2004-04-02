7225374bc1bdba5c35a28dc4533698a1998b1279
recht <recht@pkgsrc.org>
2004-04-02 18:12:37 +0000

    depend on libsoup-devel instead of libsoup

--- mail/evolution-data-server/buildlink3.mk
+++ mail/evolution-data-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/04/01 20:20:23 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/04/02 18:12:37 recht Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 EVOLUTION_DATA_SERVER_BUILDLINK3_MK:=	${EVOLUTION_DATA_SERVER_BUILDLINK3_MK}+
@@ -22,7 +22,7 @@ BUILDLINK_PKGSRCDIR.evolution-data-server?=	../../mail/evolution-data-server
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/libbonobo/buildlink3.mk"
 .include "../../devel/libgnome/buildlink3.mk"
-.include "../../net/libsoup/buildlink3.mk"
+.include "../../net/libsoup-devel/buildlink3.mk"
 .include "../../net/ORBit2/buildlink3.mk"
 .include "../../sysutils/gnome-vfs2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
