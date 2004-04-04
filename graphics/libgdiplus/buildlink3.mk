3be79fc5e047583de68a653d03b3bcdf9abd8411
recht <recht@pkgsrc.org>
2004-04-04 16:54:42 +0000

    update to 0.2
    Bug fixes and code clean-up.

--- graphics/libgdiplus/buildlink3.mk
+++ graphics/libgdiplus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/03/18 09:12:12 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/04/04 16:54:42 recht Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGDIPLUS_BUILDLINK3_MK:=	${LIBGDIPLUS_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgdiplus}
 BUILDLINK_PACKAGES+=	libgdiplus
 
 .if !empty(LIBGDIPLUS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libgdiplus+=		libgdiplus>=0.1
+BUILDLINK_DEPENDS.libgdiplus+=	libgdiplus>=0.2
 BUILDLINK_PKGSRCDIR.libgdiplus?=	../../graphics/libgdiplus
 .endif	# LIBGDIPLUS_BUILDLINK3_MK
 
