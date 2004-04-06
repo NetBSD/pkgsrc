19bc364aaa7a3f557af211fd94a70d0bb1f6b685
jmmv <jmmv@pkgsrc.org>
2004-04-06 09:36:28 +0000

    Update to 0.8.0nb2: - Do not install nor build any plugins that require external libraries.   This keeps this package small, makes it modular and will avoid the need   of "fee-based-commercial-use" licenses while building GNOME. - Split the Makefile and make it easy to create packages for individual   plugins by using Makefile.common. - Use gstreamer/plugins.mk to automatically register installed plugins in   the plugins database. - Fix the regex used to export symbols to something that matches better   the original one. - As a result of the update of gstreamer to 0.8.0nb1 (which requires a   glib2 with the RTLD_GLOBAL fix), plugins work fine now.

--- multimedia/gst-plugins/buildlink3.mk
+++ multimedia/gst-plugins/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/04/01 19:06:38 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/04/06 09:36:28 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GST_PLUGINS_BUILDLINK3_MK:=	${GST_PLUGINS_BUILDLINK3_MK}+
@@ -11,8 +11,14 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngst-plugins}
 BUILDLINK_PACKAGES+=	gst-plugins
 
 .if !empty(GST_PLUGINS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gst-plugins+=		gst-plugins>=0.8.0
+BUILDLINK_DEPENDS.gst-plugins+=		gst-plugins>=0.8.0nb2
 BUILDLINK_PKGSRCDIR.gst-plugins?=	../../multimedia/gst-plugins
 .endif	# GST_PLUGINS_BUILDLINK3_MK
+
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/GConf2/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../multimedia/gstreamer/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
