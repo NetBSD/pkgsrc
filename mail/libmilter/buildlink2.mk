f45299e0352748d8811a73f2605eafd3bae80054
tv <tv>
2004-04-06 13:50:57 +0000

    Make libmilter a build-time dependency only (it's a static library); bump PKGREVISION of its dependencies.

--- mail/libmilter/buildlink2.mk
+++ mail/libmilter/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.9 2004/03/27 22:07:32 tron Exp $
+# $NetBSD: buildlink2.mk,v 1.10 2004/04/06 13:50:57 tv Exp $
 #
 # This Makefile fragment is included by packages that use libmilter.
 #
@@ -9,6 +9,7 @@ LIBMILTER_BUILDLINK2_MK=	# defined
 .include "../../mk/bsd.prefs.mk"
 
 BUILDLINK_DEPENDS.libmilter?=		libmilter>=8.12.9nb1
+BUILDLINK_DEPMETHOD.libmilter?=		build
 BUILDLINK_PKGSRCDIR.libmilter?=		../../mail/libmilter
 BUILDLINK_DEPMETHOD.libmilter?=		build
 
