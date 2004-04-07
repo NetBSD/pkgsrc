338ca84d122333d9cc018384e50ab7283d63719d
salo <salo@pkgsrc.org>
2004-04-07 10:40:01 +0000

    Initial import of libebml-0.6.5: Extensible Binary Meta Language support library.
    EBML, or Extensible Binary Meta-Language, is a simple XML like binary
    language for describing data in structured style. EBML was originally
    designed for use in the Matroska project, but the developers saw that
    EBML was very flexible and extensible.
    
    From pkgsrc-wip, submitted by Sergey Svishchev.

---
+++ devel/libebml/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/04/07 10:40:01 salo Exp $
+#
+# This Makefile fragment is included by packages that use libebml.
+#
+
+BUILDLINK_DEPMETHOD.libebml?=	build
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBEBML_BUILDLINK3_MK:=	${LIBEBML_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libebml
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibebml}
+BUILDLINK_PACKAGES+=	libebml
+
+.if !empty(LIBEBML_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libebml+=	libebml>=0.6.5
+BUILDLINK_PKGSRCDIR.libebml?=	../../devel/libebml
+.endif	# LIBEBML_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
