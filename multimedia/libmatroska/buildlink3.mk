ae7e290fcd9918bb912db6e7aea860162bc0d5ee
salo <salo@pkgsrc.org>
2004-04-07 12:52:16 +0000

    Initial import of libmatroska-0.6.3: Extensible open standard Audio/Video container format library.
    Matroska aims to become THE Standard of Multimedia Container Formats.
    It was derived from a project called MCF, but differentiates from it
    significantly.  It is based on EBML (Extensible Binary Meta Language),
    a binary derivative of XML.
    
    Matroska is designed with the future in mind. It incorporates features
    you would expect from a modern container format, like:
    
    - Streamable over internet (HTTP and RTP)
    - Fast seeking in the file
    - High error recovery
    - Menus (like DVDs have)
    - Chapter entries
    - Selectable subtitle streams
    - Selectable audio streams
    - Modularly extendable
    
    From pkgsrc-wip, submitted by Sergey Svishchev.

---
+++ multimedia/libmatroska/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/04/07 12:52:16 salo Exp $
+#
+# This Makefile fragment is included by packages that use libmatroska.
+#
+
+BUILDLINK_DEPMETHOD.libmatroska?=	build
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBMATROSKA_BUILDLINK3_MK:=	${LIBMATROSKA_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libmatroska
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibmatroska}
+BUILDLINK_PACKAGES+=	libmatroska
+
+.if !empty(LIBMATROSKA_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libmatroska+=	libmatroska>=0.6.3
+BUILDLINK_PKGSRCDIR.libmatroska?=	../../multimedia/libmatroska
+.endif	# LIBMATROSKA_BUILDLINK3_MK
+
+.include "../../devel/libebml/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
