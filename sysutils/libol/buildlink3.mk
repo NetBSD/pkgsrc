b1602b8bee1330cb79e6f3c7e3b9ced6658dca29
salo <salo@pkgsrc.org>
2004-04-07 13:19:45 +0000

    Fix wip reference. (hi minskim!)

--- sysutils/libol/buildlink3.mk
+++ sysutils/libol/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/03/29 00:54:16 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/04/07 13:19:45 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBOL_BUILDLINK3_MK:=	${LIBOL_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libol
 
 .if !empty(LIBOL_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libol+=	libol>=0.3.11
-BUILDLINK_PKGSRCDIR.libol?=	../../wip/libol
+BUILDLINK_PKGSRCDIR.libol?=	../../sysutils/libol
 .endif	# LIBOL_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
