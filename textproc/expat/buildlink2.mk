84276ff0da324670c6d56e64b66ca5d6c68bfb89
reed <reed@pkgsrc.org>
2004-04-08 16:35:21 +0000

    Bump BUILDLINK_DEPENDS.expat from 1.95.4 to 1.95.7. The expat-1.95.6nb1 package has libexpat.so.4.0 and expat-1.95.7 has libexpat.so.5.0.
    I noticed this because I had a fontconfig package built against
    libexpat.so.4 which installed fine on another box
    with expat-1.95.7 (libexpat.so.5). The fontconfig package
    should have required the expat as now available in pkgsrc.
    
    Please note that I have not bumped PKGREVISIONs for all
    the packages depending on this.

--- textproc/expat/buildlink2.mk
+++ textproc/expat/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/10/11 07:10:52 grant Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2004/04/08 16:35:21 reed Exp $
 
 .if !defined(EXPAT_BUILDLINK2_MK)
 EXPAT_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		expat
-BUILDLINK_DEPENDS.expat?=	expat>=1.95.4
+BUILDLINK_DEPENDS.expat?=	expat>=1.95.7
 BUILDLINK_PKGSRCDIR.expat?=	../../textproc/expat
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.expat=expat
