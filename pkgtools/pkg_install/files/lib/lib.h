9ccd31c07abb19fc1e4ca99a4b18cab6cfa5e09d
rh <rh>
2004-04-11 06:56:11 +0000

    Update pkg_install to 20040411, pulling up the changes in the NetBSD source tree:
    Make pkg_add recognise binary packages built with ignored recommendations
    (IGNORE_RECOMMENDED set in the presence of RECOMMENDED pre-requisites).
    This change is fully backward-compatible: binary packages have set the
    corresponding BUILD_INFO since RECOMMENDED was implemented.  Packages
    built before that time will install as before.
    Bump PKGTOOLS_VERSION to 20040411.
    This closes PR pkg/24712 by Thomas Klausner.

--- pkgtools/pkg_install/files/lib/lib.h
+++ pkgtools/pkg_install/files/lib/lib.h
@@ -1,4 +1,4 @@
-/* $NetBSD: lib.h,v 1.10 2004/04/09 18:38:12 tv Exp $ */
+/* $NetBSD: lib.h,v 1.11 2004/04/11 06:56:11 rh Exp $ */
 
 /* from FreeBSD Id: lib.h,v 1.25 1997/10/08 07:48:03 charnier Exp */
 
@@ -198,7 +198,8 @@ typedef enum bi_ent_t {
 	BI_OPSYS,		/*  0 */
 	BI_OS_VERSION,		/*  1 */
 	BI_MACHINE_ARCH,	/*  2 */
-	BI_ENUM_COUNT,		/*  3 */
+	BI_IGNORE_RECOMMENDED,	/*  3 */
+	BI_ENUM_COUNT,		/*  4 */
 }	bi_ent_t;
 
 /* Types */
