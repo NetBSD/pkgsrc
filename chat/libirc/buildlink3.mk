2df343ba32199c2a30e66cb07f0924de281fddfb
snj <snj@pkgsrc.org>
2004-04-11 05:48:49 +0000

    Convert to buildlink3.

---
+++ chat/libirc/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/04/11 05:55:45 snj Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBIRC_BUILDLINK3_MK:=	${LIBIRC_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libirc
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibirc}
+BUILDLINK_PACKAGES+=	libirc
+
+.if !empty(LIBIRC_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libirc+=	libirc>=0.2nb1
+BUILDLINK_PKGSRCDIR.libirc?=	../../chat/libirc
+.endif	# LIBIRC_BUILDLINK3_MK
+
+.include "../../devel/glib/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
