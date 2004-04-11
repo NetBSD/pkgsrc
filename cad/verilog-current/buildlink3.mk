b3ca4d32e7f54e533be80f83994b83c948e6fa9c
snj <snj@pkgsrc.org>
2004-04-11 05:37:09 +0000

    Bah.  Forgot to remove unnecessary includes.

--- cad/verilog-current/buildlink3.mk
+++ cad/verilog-current/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/11 05:36:23 snj Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/04/11 05:37:09 snj Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 VERILOG_CURRENT_BUILDLINK3_MK:=	${VERILOG_CURRENT_BUILDLINK3_MK}+
@@ -14,9 +14,5 @@ BUILDLINK_PACKAGES+=	verilog-current
 BUILDLINK_DEPENDS.verilog-current+=	verilog-current>=20021019
 BUILDLINK_PKGSRCDIR.verilog-current?=	../../cad/verilog-current
 .endif	# VERILOG_CURRENT_BUILDLINK3_MK
-
-.include "../../devel/zlib/buildlink3.mk"
-.include "../../archivers/bzip2/buildlink3.mk"
-.include "../../devel/readline/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
