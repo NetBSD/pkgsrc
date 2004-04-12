4cc788dd528a129e1f6d36fb2aa9c9cd9c506c4a
snj <snj>
2004-04-12 04:48:39 +0000

    Convert to buildlink3.

---
+++ sysutils/medusa/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/04/12 04:48:39 snj Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+MEDUSA_BUILDLINK3_MK:=	${MEDUSA_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	medusa
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmedusa}
+BUILDLINK_PACKAGES+=	medusa
+
+.if !empty(MEDUSA_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.medusa+=	medusa>=0.5.1nb5
+BUILDLINK_PKGSRCDIR.medusa?=	../../sysutils/medusa
+.endif	# MEDUSA_BUILDLINK3_MK
+
+.include "../../sysutils/gnome-vfs/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
