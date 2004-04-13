b5e4331c565337cdf224e186eac1861ea56d7dfd
reed <reed>
2004-04-13 16:22:50 +0000

    Add buildlink3.mk file (which I used for gtkam).

---
+++ graphics/libexif-gtk/buildlink3.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/04/13 16:22:50 reed Exp $
+#
+# This file was created automatically using createbuildlink-3.4.
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBEXIF_GTK_BUILDLINK3_MK:=	${LIBEXIF_GTK_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libexif-gtk
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibexif-gtk}
+BUILDLINK_PACKAGES+=	libexif-gtk
+
+.if !empty(LIBEXIF_GTK_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libexif-gtk+=	libexif-gtk>=0.3.3nb7
+BUILDLINK_PKGSRCDIR.libexif-gtk?=	../../graphics/libexif-gtk
+.endif	# LIBEXIF_GTK_BUILDLINK3_MK
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../graphics/libexif/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
