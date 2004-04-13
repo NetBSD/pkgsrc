2c29b0548dbaa32b6e119571cfeb05c623bd4b5d
heinz <heinz@pkgsrc.org>
2004-04-13 22:04:15 +0000

    Remove redundant assignments of PKG_TOOLS_BIN. PKG_TOOLS_BIN is ${LOCALBASE}/sbin on Linux, Darwin and IRIX.

--- mk/defs.Linux.mk
+++ mk/defs.Linux.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.Linux.mk,v 1.67 2004/04/07 14:26:51 tv Exp $
+# $NetBSD: defs.Linux.mk,v 1.68 2004/04/13 22:04:15 heinz Exp $
 #
 # Variable definitions for the Linux operating system.
 
@@ -194,7 +194,3 @@ _STRIPFLAG_INSTALL?=	-s	# install(1) option to strip
 .endif
 
 LOCALBASE?=		${DESTDIR}/usr/pkg
-.if exists(${LOCALBASE}/sbin/pkg_info)
-PKG_TOOLS_BIN?=		${LOCALBASE}/sbin
-.endif
-PKG_TOOLS_BIN?=		${LOCALBASE}/bin
