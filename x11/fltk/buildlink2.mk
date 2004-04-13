da53d306adab9290f65b41a80a218b237402f2ae
xtraeme <xtraeme@pkgsrc.org>
2004-04-13 09:36:17 +0000

    Update x11/fltk to 1.1.5rc1 and s/USE_X11BASE/USE_X11/.
    Changes:
    
    CHANGES IN FLTK 1.1.5rc1
    
            - Documentation updates (STR #186, STR #245, STR #250,
              STR #277, STR #281, STR #328, STR #338)
            - fl_scroll() did not handle scrolling from off-screen on
              WIN32 (STR #315)
            - Fl_File_Chooser did not allow manual entry of a drive
              letter (STR #339)
            - Fl_Menu now uses the boxtype to redraw the menu
              background (STR #204)
            - Fl_Scroll now shows the background image when a framed
              box type is used and the Fl_Scroll is a direct
              decendent of a window (STR #205)
            - Added a new_directory_tooltip string pointer to allow
              localization of the file chooser's new directory
              button (STR #340)
            - Added Fl_Menu_::find_item() method (STR #316)
            - The Fl_Widget copy operator definitions were not
              conditionally compiled properly (STR #329)
            - FLUID's Layout functionality did not move child
              widgets when laying out group widgets (STR #319)
            - FLUID's Layout->Center In Group functionality did not
              properly handle widgets that were children of a
              Fl_Window widget (STR #318)
            - The Fl_Text_Display destructor did not remove the
              predelete callback associated with the current buffer
              (STR #332)
            - Fixed several bugs in the MacOS X Fl::add_fd()
              handling (STR #333, STR #337)
            - The Fl_Text_Display widget did not display selections
              set by the application (STR #322)
            - FLUID crashed if you did layout with a window widget
              (STR #317)
            - Fl_Scroll::clear() didn't remove the child widget from
              the Fl_Scroll widget (STR #327)
            - Fl_Value_Slider::draw_bg() didn't always apply the
              clipping rectangle (STR #235)
            - fl_filename_relative() returned the wrong string if
              the absolute pathname was equal to the current working
              directory (STR #224)
            - Fl_Help_Dialog didn't correctly restore the scroll
              position when going forward/back in the link history
              if the file changed (STR #218)
    
    And more...

--- x11/fltk/buildlink2.mk
+++ x11/fltk/buildlink2.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink2.mk,v 1.5 2004/02/18 05:07:34 ben Exp $
+# $NetBSD: buildlink2.mk,v 1.6 2004/04/13 09:36:17 xtraeme Exp $
 
 .if !defined(FLTK_BUILDLINK2_MK)
 FLTK_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		fltk
-BUILDLINK_DEPENDS.fltk?=	fltk>=1.0.9
+BUILDLINK_DEPENDS.fltk?=	fltk>=1.1.5rc1
 BUILDLINK_PKGSRCDIR.fltk?=	../../x11/fltk
 
 EVAL_PREFIX+=		BUILDLINK_PREFIX.fltk=fltk
-BUILDLINK_PREFIX.fltk_DEFAULT=	${X11PREFIX}
+BUILDLINK_PREFIX.fltk_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.fltk=	include/FL/*
 BUILDLINK_FILES.fltk+=	include/Fl/*
 BUILDLINK_FILES.fltk+=	lib/libfltk*
