7ce97aaf4d3f68d87c7b0bae8ffd2b4ed187a128
jmmv <jmmv@pkgsrc.org>
2004-04-14 12:07:30 +0000

    Move libmpeg2 from graphics to the multimedia category. This is version 0.4.0.

---
+++ multimedia/libmpeg2/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/04/14 12:07:30 jmmv Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBMPEG2_BUILDLINK3_MK:=	${LIBMPEG2_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libmpeg2
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibmpeg2}
+BUILDLINK_PACKAGES+=	libmpeg2
+
+.if !empty(LIBMPEG2_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libmpeg2+=	libmpeg2>=0.4.0
+BUILDLINK_PKGSRCDIR.libmpeg2?=	../../multimedia/libmpeg2
+.endif	# LIBMPEG2_BUILDLINK3_MK
+
+.include "../../devel/SDL/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
