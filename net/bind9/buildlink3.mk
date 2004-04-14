d64936911ba1073a3abfd5702a0a60c5342ac974
snj <snj@pkgsrc.org>
2004-04-14 20:26:51 +0000

    Convert to buildlink3.

---
+++ net/bind9/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/04/14 20:26:51 snj Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BIND_BUILDLINK3_MK:=	${BIND_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	bind
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nbind}
+BUILDLINK_PACKAGES+=	bind
+
+.if !empty(BIND_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.bind+=	bind>=9.2.2
+BUILDLINK_RECOMMENDED.bind?=	bind>=9.2.3nb2
+BUILDLINK_PKGSRCDIR.bind?=	../../net/bind9
+.endif	# BIND_BUILDLINK3_MK
+
+.include "../../security/openssl/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
