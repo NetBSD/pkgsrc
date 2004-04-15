d4294325d70594d49878f4bdbc5fecd48886c417
tron <tron@pkgsrc.org>
2004-04-15 10:41:14 +0000

    Fix seriously broken buildlink2 script: - incorrect path to the package source directory - library missing from the file list - no protection against multiple inclusion

--- mail/libspf-alt/buildlink2.mk
+++ mail/libspf-alt/buildlink2.mk
@@ -1,12 +1,28 @@
-BUILDLINK_PACKAGES+=		libspf_alt
-BUILDLINK_PKGBASE.libspf_alt=	libspf_alt
-BUILDLINK_DEPENDS.libspf_alt?=	libspf_alt>=0.4.0
-BUILDLINK_PKGSRCDIR.libspf_alt?=../../mail/libspf_alt
+# $NetBSD: buildlink2.mk,v 1.2 2004/04/15 10:41:14 tron Exp $
 
-EVAL_PREFIX+=			BUILDLINK_PREFIX.libspf_alt=libspf_alt
+.if !defined(LIBSPF_ALT_BUILDLINK2_MK)
+LIBSPF_ALT_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=		libspf_alt
+BUILDLINK_DEPENDS.libspf_alt?=	libspf_alt>=0.4.0
+BUILDLINK_PKGSRCDIR.libspf_alt?=	../../mail/libspf-alt
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.libspf_alt=libspf_alt
 BUILDLINK_PREFIX.libspf_alt_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.libspf_alt=	include/spf_alt/*
+BUILDLINK_FILES.libspf_alt+=	include/spf_alt/spf.h
+BUILDLINK_FILES.libspf_alt+=	include/spf_alt/spf_lib_version.h
+BUILDLINK_FILES.libspf_alt+=	include/spf_alt/spf_dns.h
+BUILDLINK_FILES.libspf_alt+=	include/spf_alt/spf_dns_cache.h
+BUILDLINK_FILES.libspf_alt+=	include/spf_alt/spf_dns_null.h
+BUILDLINK_FILES.libspf_alt+=	include/spf_alt/spf_dns_resolv.h
+BUILDLINK_FILES.libspf_alt+=	include/spf_alt/spf_dns_test.h
+BUILDLINK_FILES.libspf_alt+=	include/spf_alt/spf_dns_zone.h
 BUILDLINK_FILES.libspf_alt+=	lib/libspf_alt.*
 
-BUILDLINK_TARGETS+=		libspf_alt-buildlink
-libspf_alt-buildlink:		_BUILDLINK_USE
+.include "../../net/bind9/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	libspf_alt-buildlink
+
+libspf_alt-buildlink: _BUILDLINK_USE
+
+.endif	# LIBSPF_ALT_BUILDLINK2_MK
