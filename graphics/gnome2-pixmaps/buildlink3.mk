5e519096b8cc0f9955655baa8cec7924e004b014
xtraeme <xtraeme@pkgsrc.org>
2004-04-15 10:26:35 +0000

    Revert previous, we need a full dependency.

--- graphics/gnome2-pixmaps/buildlink3.mk
+++ graphics/gnome2-pixmaps/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/04/15 02:36:17 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/04/15 10:26:35 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME2_PIXMAPS_BUILDLINK3_MK:=	${GNOME2_PIXMAPS_BUILDLINK3_MK}+
@@ -13,7 +13,6 @@ BUILDLINK_PACKAGES+=	gnome2-pixmaps
 .if !empty(GNOME2_PIXMAPS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gnome2-pixmaps+=	gnome2-pixmaps>=2.6.0
 BUILDLINK_PKGSRCDIR.gnome2-pixmaps?=	../../graphics/gnome2-pixmaps
-BUILDLINK_DEPMETHOD.gnome2-pixmaps=	build
 .endif	# GNOME2_PIXMAPS_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
