56bb2f1e0a363e38f457bc9ed06c26d6a2389c10
martti <martti@pkgsrc.org>
2004-04-16 16:12:00 +0000

    Updated xfce4 to 4.0.5
    This is a maintenance release, aimed at bug-fixing; no new features
    are being added to the 4.0 branch.
    
    The main purpose of this release is compatibility with the recent GTK+ 2.4.x
    release along with other fixes.

--- x11/libxfce4gui/buildlink3.mk
+++ x11/libxfce4gui/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/18 09:12:17 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/04/16 16:12:00 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBXFCE4GUI_BUILDLINK3_MK:=	${LIBXFCE4GUI_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibxfce4gui}
 BUILDLINK_PACKAGES+=	libxfce4gui
 
 .if !empty(LIBXFCE4GUI_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libxfce4gui+=		libxfce4gui>=4.0.4nb1
+BUILDLINK_DEPENDS.libxfce4gui+=		libxfce4gui>=4.0.5
 BUILDLINK_PKGSRCDIR.libxfce4gui?=	../../x11/libxfce4gui
 .endif	# LIBXFCE4GUI_BUILDLINK3_MK
 
--- x11/libxfce4mcs/buildlink3.mk
+++ x11/libxfce4mcs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/18 09:12:17 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/04/16 16:12:00 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBXFCE4MCS_BUILDLINK3_MK:=	${LIBXFCE4MCS_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibxfce4mcs}
 BUILDLINK_PACKAGES+=	libxfce4mcs
 
 .if !empty(LIBXFCE4MCS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libxfce4mcs+=		libxfce4mcs>=4.0.4nb1
+BUILDLINK_DEPENDS.libxfce4mcs+=		libxfce4mcs>=4.0.5
 BUILDLINK_PKGSRCDIR.libxfce4mcs?=	../../x11/libxfce4mcs
 .endif	# LIBXFCE4MCS_BUILDLINK3_MK
 
