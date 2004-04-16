9dada60fa50976cf8519a91bc1c987cdddf86935
recht <recht@pkgsrc.org>
2004-04-16 14:30:44 +0000

    update to 3.3.1
    changes (among many others):
    
    - New QLocale Class
    
    - The ActiveQt framework, first available in Qt 3.1, has been enhanced in
      lots of ways in Qt 3.3.
    
    - Semi-Transparent Windows
    
    Other Improvements
    - The network module now supports IPv6 in addition to IPv4.
    - New 64-bit platforms are supported: Itanium on Linux (Intel compiler) and
      Windows (MSVC and Intel).
    - QCursor now provides the BusyCursor shape, which combines an hourglass with
      an arrow.
    - Qt's DOM classes are now reentrant when Qt is built with multithread support
      enabled.
    
    See http://www.trolltech.com/products/qt/whatsnew.html for more details.

--- x11/qt3-libs/buildlink3.mk
+++ x11/qt3-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/03/30 18:58:34 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/04/16 14:30:45 recht Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 QT3_LIBS_BUILDLINK3_MK:=	${QT3_LIBS_BUILDLINK3_MK}+
@@ -13,13 +13,14 @@ BUILDLINK_PACKAGES+=	qt3-libs
 .if !empty(QT3_LIBS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.qt3-libs+=	qt3-libs>=3.2.3
 BUILDLINK_PKGSRCDIR.qt3-libs?=	../../x11/qt3-libs
+
+BUILDLINK_INCDIRS.qt3-libs+=	qt3/include
+BUILDLINK_LIBDIRS.qt3-libs+=	qt3/lib
+                                                                                
 .endif	# QT3_LIBS_BUILDLINK3_MK
 
 QTDIR=	${BUILDLINK_PREFIX.qt3-libs}/qt3
-                                                                                
-BUILDLINK_INCDIRS.qt3-libs+=	${QTDIR}/include
-BUILDLINK_LIBDIRS.qt3-libs+=	${QTDIR}/lib
-                                                                                
+
 PTHREAD_OPTS+=	require
 
 .include "../../devel/zlib/buildlink3.mk"
@@ -36,7 +37,7 @@ PTHREAD_OPTS+=	require
 
 CONFIGURE_ENV+=		MOC="${QTDIR}/bin/moc"
 MAKE_ENV+=		MOC="${QTDIR}/bin/moc"
-LDFLAGS+=		-Wl,${RPATH_FLAG}${QTDIR}/lib
+#LDFLAGS+=		-Wl,${RPATH_FLAG}${QTDIR}/lib
 
 .if !defined(BUILD_QT3)
 CONFIGURE_ENV+=		QTDIR="${QTDIR}"
