5d1e10ee1177dd31cd536031ae85c31e079a6ac0
recht <recht@pkgsrc.org>
2004-04-16 14:30:44 +0000

    update to 3.3.1
    changes (among many others):
    
    - New QLocale Class
    
    - The ActiveQt framework, first available in Qt 3.1, has been enhanced in
      lots of ways in Qt 3.3.
    
    - Semi-Transparent Windows
    
    Other Improvements
    - The network module now supports IPv6 in addition to IPv4.
    - New 64-bit platforms are supported: Itanium on Linux (Intel compiler) and
      Windows (MSVC and Intel).
    - QCursor now provides the BusyCursor shape, which combines an hourglass with
      an arrow.
    - Qt's DOM classes are now reentrant when Qt is built with multithread support
      enabled.
    
    See http://www.trolltech.com/products/qt/whatsnew.html for more details.

---
+++ x11/qt3-tools/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/04/16 14:30:45 recht Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+QT3_TOOLS_BUILDLINK3_MK:=	${QT3_TOOLS_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	qt3-tools
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nqt3-tools}
+BUILDLINK_PACKAGES+=	qt3-tools
+
+.if !empty(QT3_TOOLS_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.qt3-tools+=	qt3-tools>=3.1.2nb3
+BUILDLINK_PKGSRCDIR.qt3-tools?=	../../x11/qt3-tools
+.endif	# QT3_TOOLS_BUILDLINK3_MK
+
+.include "../../x11/qt3-libs/buildlink3.mk"
+
+CONFIGURE_ENV+=			UIC="${QTDIR}/bin/uic"
+MAKE_ENV+=			UIC="${QTDIR}/bin/uic"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
