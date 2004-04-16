c6e785e20222aa8d05395ad669609ef9b59bfceb
martti <martti>
2004-04-16 16:12:00 +0000

    Updated xfce4 to 4.0.5
    This is a maintenance release, aimed at bug-fixing; no new features
    are being added to the 4.0 branch.
    
    The main purpose of this release is compatibility with the recent GTK+ 2.4.x
    release along with other fixes.

--- x11/libxfce4util/buildlink3.mk
+++ x11/libxfce4util/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/18 09:12:17 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/04/16 16:12:00 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBXFCE4UTIL_BUILDLINK3_MK:=	${LIBXFCE4UTIL_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibxfce4util}
 BUILDLINK_PACKAGES+=	libxfce4util
 
 .if !empty(LIBXFCE4UTIL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libxfce4util+=	libxfce4util>=4.0.4nb1
+BUILDLINK_DEPENDS.libxfce4util+=	libxfce4util>=4.0.5
 BUILDLINK_PKGSRCDIR.libxfce4util?=	../../x11/libxfce4util
 .endif	# LIBXFCE4UTIL_BUILDLINK3_MK
 
