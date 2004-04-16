f85bc46476b4d1758316eba98bed1292f8298114
martti <martti@pkgsrc.org>
2004-04-16 16:12:00 +0000

    Updated xfce4 to 4.0.5
    This is a maintenance release, aimed at bug-fixing; no new features
    are being added to the 4.0 branch.
    
    The main purpose of this release is compatibility with the recent GTK+ 2.4.x
    release along with other fixes.

--- sysutils/xfce4-fm/buildlink3.mk
+++ sysutils/xfce4-fm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/18 09:12:14 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/04/16 16:12:00 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XFCE4_FM_BUILDLINK3_MK:=	${XFCE4_FM_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-fm}
 BUILDLINK_PACKAGES+=	xfce4-fm
 
 .if !empty(XFCE4_FM_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.xfce4-fm+=	xfce4-fm>=4.0.4nb1
+BUILDLINK_DEPENDS.xfce4-fm+=	xfce4-fm>=4.0.5
 BUILDLINK_PKGSRCDIR.xfce4-fm?=	../../sysutils/xfce4-fm
 .endif	# XFCE4_FM_BUILDLINK3_MK
 
--- x11/xfce4-panel/buildlink3.mk
+++ x11/xfce4-panel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/18 09:12:18 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/04/16 16:12:00 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XFCE4_PANEL_BUILDLINK3_MK:=	${XFCE4_PANEL_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-panel}
 BUILDLINK_PACKAGES+=	xfce4-panel
 
 .if !empty(XFCE4_PANEL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.xfce4-panel+=		xfce4-panel>=4.0.4nb1
+BUILDLINK_DEPENDS.xfce4-panel+=		xfce4-panel>=4.0.5
 BUILDLINK_PKGSRCDIR.xfce4-panel?=	../../x11/xfce4-panel
 .endif	# XFCE4_PANEL_BUILDLINK3_MK
 
