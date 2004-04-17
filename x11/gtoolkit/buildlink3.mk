db52212390b3dc4a0c59af773d4b57531ee0301b
rh <rh>
2004-04-17 00:26:48 +0000

    Make this compile with gcc-3. Adjust PLIST to reflect (not so) new gnustep hierarchy. While at it, convert to buildlink3. Bump PKGREVISION.

---
+++ x11/gtoolkit/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/04/17 00:26:48 rh Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+GTOOLKIT_BUILDLINK3_MK:=	${GTOOLKIT_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gtoolkit
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngtoolkit}
+BUILDLINK_PACKAGES+=	gtoolkit
+
+.if !empty(GTOOLKIT_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.gtoolkit+=	gtoolkit>=0.9.5nb2
+BUILDLINK_PKGSRCDIR.gtoolkit?=	../../x11/gtoolkit
+.endif	# GTOOLKIT_BUILDLINK3_MK
+
+.include "../../x11/gtk/buildlink3.mk"
+.include "../../x11/gnustep-back/buildlink3.mk"
+.include "../../devel/libglade/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
