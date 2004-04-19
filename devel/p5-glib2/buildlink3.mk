5a3a25a1a41d58491955f87f29364e75b3d39c4f
xtraeme <xtraeme>
2004-04-19 17:24:53 +0000

    Initial import of p5-glib2-1.040, from pkgsrc-wip.
    This module provides perl access to GLib and GLib's GObject libraries.
    GLib is a portability and utility library; GObject provides a generic
    type system with inheritance and a powerful signal system.  Together
    these libraries are used as the foundation for many of the libraries
    that make up the Gnome environment, and are used in many unrelated
    projects.

---
+++ devel/p5-glib2/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/04/19 17:24:53 xtraeme Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+P5_GLIB2_BUILDLINK3_MK:=	${P5_GLIB2_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	p5-glib2
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Np5-glib2}
+BUILDLINK_PACKAGES+=	p5-glib2
+
+.if !empty(P5_GLIB2_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.p5-glib2+=	p5-glib2>=1.040
+BUILDLINK_PKGSRCDIR.p5-glib2?=	../../devel/p5-Glib2
+.endif	# P5_GLIB2_BUILDLINK3_MK
+
+.include "../../devel/glib2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
