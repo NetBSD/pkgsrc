19af9c0bc6ca9659c267fad999f96adbc4e90c15
jmmv <jmmv@pkgsrc.org>
2004-04-19 15:01:00 +0000

    Update to 0.5.1: several bugs fixed. Note that they claim this library is API/ABI compatible with the previous one, but the major number version of it has changed...  so a version bump in the buildlink3.mk file is needed.

--- textproc/libcroco/buildlink3.mk
+++ textproc/libcroco/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/04/01 18:26:22 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2004/04/19 15:01:00 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBCROCO_BUILDLINK3_MK:=	${LIBCROCO_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibcroco}
 BUILDLINK_PACKAGES+=	libcroco
 
 .if !empty(LIBCROCO_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libcroco+=	libcroco>=0.5.0
+BUILDLINK_DEPENDS.libcroco+=	libcroco>=0.5.1
 BUILDLINK_PKGSRCDIR.libcroco?=	../../textproc/libcroco
 .endif	# LIBCROCO_BUILDLINK3_MK
 
