8b0da989f1be858eaaa0197c51ad5f7046c1ef9e
jmmv <jmmv@pkgsrc.org>
2004-04-20 16:14:03 +0000

    Handle the lib/libglade and lib/libglade/2.0 directories properly during print-PLIST.

--- devel/libglade2/buildlink3.mk
+++ devel/libglade2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/04/01 18:16:46 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/04/20 16:14:03 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGLADE2_BUILDLINK3_MK:=	${LIBGLADE2_BUILDLINK3_MK}+
@@ -13,6 +13,10 @@ BUILDLINK_PACKAGES+=	libglade2
 .if !empty(LIBGLADE2_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libglade2+=	libglade2>=2.3.6
 BUILDLINK_PKGSRCDIR.libglade2?=	../../devel/libglade2
+
+PRINT_PLIST_AWK+=	/^@dirrm lib\/libglade$$/ { next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/libglade\/2.0$$/ \
+				{ print "@comment in libglade2: " $$0; next; }
 .endif	# LIBGLADE2_BUILDLINK3_MK
 
 .include "../../textproc/libxml2/buildlink3.mk"
