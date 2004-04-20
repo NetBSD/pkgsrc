a92d214d56e26df10c61bcbb477a9431f331b548
xtraeme <xtraeme>
2004-04-20 08:43:33 +0000

    Simplify.

--- devel/libevent/builtin.mk
+++ devel/libevent/builtin.mk
@@ -1,36 +1,10 @@
-# $NetBSD: builtin.mk,v 1.1 2004/04/20 08:32:13 xtraeme Exp $
+# $NetBSD: builtin.mk,v 1.2 2004/04/20 08:43:33 xtraeme Exp $
 
-.if !defined(_LIBEVENT_FOUND)
-_LIBEVENT_FOUND!=							\
-	if [ "`${ECHO} /usr/lib/libevent.*`" = "/usr/lib/libevent.*" ]; then \
-		${ECHO} "no";						\
-	else								\
-		${ECHO} "yes";						\
-	fi
-BUILDLINK_VARS+=	_LIBEVENT_FOUND
-.endif
-
-_EVENT_H=	/usr/include/event.h
-
-.if !defined(IS_BUILTIN.event)
-IS_BUILTIN.event=	no
-.  if exists(${_EVENT_H})
-IS_BUILTIN.event=	yes
+.if !defined(IS_BUILTIN.libevent)
+IS_BUILTIN.libevent=	no
+.  if exists(/usr/include/event.h)
+IS_BUILTIN.libevent=	yes
 .  endif
-.endif	# IS_BUILTIN.event
+.endif	# IS_BUILTIN.libevent
 
-.if !defined(USE_BUILTIN.event)
-USE_BUILTIN.event?=	${IS_BUILTIN.event}
-PREFER.event?=		pkgsrc
-
-.  if !empty(IS_BUILTIN.event:M[nN][oO]) || \
-        (${PREFER.event} == "pkgsrc")
-USE_BUILTIN.event=	no
-.  endif
-.endif	# USE_BUILTIN.event
-
-CHECK_BUILTIN.event?=	no
-.if !empty(CHECK_BUILTIN.event:M[nN][oO])
-_LIBEVENT=		-levent
-BUILDLINK_LDADD.event?=	${_LIBEVENT}
-.endif	# CHECK_BUILTIN.event
+USE_BUILTIN.libevent?=	${IS_BUILTIN.libevent}
