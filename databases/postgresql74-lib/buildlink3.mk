6230268c2e512f4eee36f94492bbed53220e21cb
recht <recht>
2004-04-20 22:37:22 +0000

    Follow a suggestion by Josh Berkus (pgsql core team) and build with --enable-thread-safety. Bump PKGREVISIONs accordingly (meta-pkg, -client, -lib).

--- databases/postgresql74-lib/buildlink3.mk
+++ databases/postgresql74-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/04/19 00:02:33 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/04/20 22:37:22 recht Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 POSTGRESQL_LIB_BUILDLINK3_MK:=	${POSTGRESQL_LIB_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npostgresql-lib}
 BUILDLINK_PACKAGES+=	postgresql-lib
 
 .if !empty(POSTGRESQL_LIB_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.postgresql-lib+=	postgresql-lib>=7.4.2
+BUILDLINK_DEPENDS.postgresql-lib+=	postgresql-lib>=7.4.2nb1
 BUILDLINK_PKGSRCDIR.postgresql-lib?=	../../databases/postgresql74-lib
 BUILDLINK_INCDIRS.postgresql-lib?=	include/postgresql
 .endif	# POSTGRESQL_LIB_BUILDLINK3_MK
