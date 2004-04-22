0126dc0f96b9d94497ff993c927780b762b4fd9e
ben <ben@pkgsrc.org>
2004-04-22 20:02:46 +0000

    Remove XXX comments, this file is verified to work.

--- audio/portaudio/buildlink3.mk
+++ audio/portaudio/buildlink3.mk
@@ -1,9 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/04/22 16:31:20 ben Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.3.
-# XXX After this file as been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink[23].mk files.
+# $NetBSD: buildlink3.mk,v 1.3 2004/04/22 20:02:46 ben Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PORTAUDIO_BUILDLINK3_MK:=	${PORTAUDIO_BUILDLINK3_MK}+
