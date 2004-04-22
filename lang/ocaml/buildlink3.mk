31d9982a1b0d96d03a3ac2bacd7a674c71fadefa
tron <tron@pkgsrc.org>
2004-04-22 09:18:34 +0000

    Fix various issues with this package: - split patches files - always enable threading which has various advantages   - it fixes the build of the Tk library   - it greatly simplifies the package list handling   - it provides threading support under NetBSD 1.6/1.6.x - correct default dependence in "buildlink2.mk" to "build" Bump package revision because of all these changes.

--- lang/ocaml/buildlink3.mk
+++ lang/ocaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/03/05 19:25:36 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2004/04/22 09:18:35 tron Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 OCAML_BUILDLINK3_MK:=	${OCAML_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nocaml}
 BUILDLINK_PACKAGES+=	ocaml
 
 .if !empty(OCAML_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.ocaml+=	ocaml>=3.06
+BUILDLINK_DEPENDS.ocaml+=	ocaml>=3.06nb6
 BUILDLINK_PKGSRCDIR.ocaml?=	../../lang/ocaml
 BUILDLINK_DEPMETHOD.ocaml?=	build
 .endif	# OCAML_BUILDLINK3_MK
