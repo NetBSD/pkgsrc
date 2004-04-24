29fa8cc5b8307a1d5cf2edf081eed0affc972f9b
danw <danw@pkgsrc.org>
2004-04-24 14:45:15 +0000

    Two Darwin fixes: 	- split out the profiled libraries in the PLIST, because they 	  don't get built on Darwin. 	- set INSTALL_UNSTRIPPED on Darwin for both ocaml itself and 	  anything that directly depends on it, because the ocaml compiler 	  generates binaries that don't strip properly.

--- lang/ocaml/buildlink2.mk
+++ lang/ocaml/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2004/04/24 08:06:10 tron Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2004/04/24 14:45:15 danw Exp $
 #
 # This Makefile fragment is included by packages that use ocaml.
 
@@ -22,6 +22,11 @@ BUILDLINK_FILES.ocaml+=	lib/ocaml/threads/*
 BUILDLINK_TARGETS+=	ocaml-buildlink
 
 ocaml-buildlink: _BUILDLINK_USE
+
+.include "../../mk/bsd.prefs.mk"
+.if ${OPSYS} == "Darwin"
+INSTALL_UNSTRIPPED=		yes
+.endif
 
 .include "../../x11/tk/buildlink3.mk"
 
--- lang/ocaml/buildlink3.mk
+++ lang/ocaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/04/24 08:06:10 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2004/04/24 14:45:15 danw Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 OCAML_BUILDLINK3_MK:=	${OCAML_BUILDLINK3_MK}+
@@ -14,6 +14,12 @@ BUILDLINK_DEPMETHOD.ocaml?=	build
 .if !empty(OCAML_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.ocaml+=	ocaml>=3.06nb6
 BUILDLINK_PKGSRCDIR.ocaml?=	../../lang/ocaml
+
+. include "../../mk/bsd.prefs.mk"
+. if ${OPSYS} == "Darwin"
+INSTALL_UNSTRIPPED=		yes
+. endif
+
 .endif	# OCAML_BUILDLINK3_MK
 
 .include "../../x11/tk/buildlink3.mk"
