79f90101fa54b99e79e5b5bded4113dc9afbe19b
tron <tron@pkgsrc.org>
2004-04-24 08:06:10 +0000

    Include buildlink code for "tk" package because this package depends on the "tk" package.

--- lang/ocaml/buildlink2.mk
+++ lang/ocaml/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2004/04/22 09:18:35 tron Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2004/04/24 08:06:10 tron Exp $
 #
 # This Makefile fragment is included by packages that use ocaml.
 
@@ -22,5 +22,7 @@ BUILDLINK_FILES.ocaml+=	lib/ocaml/threads/*
 BUILDLINK_TARGETS+=	ocaml-buildlink
 
 ocaml-buildlink: _BUILDLINK_USE
+
+.include "../../x11/tk/buildlink3.mk"
 
 .endif	# OCAML_BUILDLINK2_MK
