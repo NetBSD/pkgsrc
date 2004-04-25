e03d4eb71b3e73522497fe745b2edbd0d59dd270
reed <reed@pkgsrc.org>
2004-04-25 04:05:10 +0000

    Under NetBSD, some libraries were linked with libiconv, such as /usr/pkg/lib/libgphoto2_port.so.5.1. So include libiconv buildlink3.mk.

--- devel/libgphoto2/buildlink3.mk
+++ devel/libgphoto2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/03/18 09:12:10 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/04/25 04:05:10 reed Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGPHOTO2_BUILDLINK3_MK:=	${LIBGPHOTO2_BUILDLINK3_MK}+
@@ -16,6 +16,7 @@ BUILDLINK_PKGSRCDIR.libgphoto2?=	../../devel/libgphoto2
 .endif	# LIBGPHOTO2_BUILDLINK3_MK
 
 .include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
 .include "../../graphics/libexif/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
