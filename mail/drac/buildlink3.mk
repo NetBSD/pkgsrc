b21f98978e51f364095d4c69324018ab8fd42381
wiz <wiz>
2004-04-25 01:06:29 +0000

    Static library only -> build dependency.

--- mail/drac/buildlink3.mk
+++ mail/drac/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/25 01:05:18 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/04/25 01:06:29 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 DRAC_BUILDLINK3_MK:=	${DRAC_BUILDLINK3_MK}+
@@ -13,6 +13,7 @@ BUILDLINK_PACKAGES+=	drac
 .if !empty(DRAC_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.drac+=	drac>=1.10
 BUILDLINK_PKGSRCDIR.drac?=	../../mail/drac
+BUILDLINK_DEPMETHOD.drac?=	build
 .endif	# DRAC_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
