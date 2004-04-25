307dce5c5a0818e7bda99e389e0464c9fe9071fe
xtraeme <xtraeme@pkgsrc.org>
2004-04-25 00:56:00 +0000

    BUILDLINK_{INC,LIB}DIRS should be relatives to BUILDLINK_PREFIX.<pkg>

--- databases/freetds/buildlink3.mk
+++ databases/freetds/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/25 00:53:15 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/04/25 00:56:00 xtraeme Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 FREETDS_BUILDLINK3_MK:=	${FREETDS_BUILDLINK3_MK}+
@@ -13,8 +13,8 @@ BUILDLINK_PACKAGES+=	freetds
 .if !empty(FREETDS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.freetds+=	freetds>=0.61.2
 BUILDLINK_PKGSRCDIR.freetds?=	../../databases/freetds
-BUILDLINK_INCDIRS.freetds+=	${BUILDLINK_PREFIX.freetds}/freetds/include
-BUILDLINK_LIBDIRS.freetds+=	${BUILDLINK_PREFIX.freetds}/freetds/lib
+BUILDLINK_INCDIRS.freetds+=	freetds/include
+BUILDLINK_LIBDIRS.freetds+=	freetds/lib
 .endif	# FREETDS_BUILDLINK3_MK
 
 .include "../../converters/libiconv/buildlink3.mk"
