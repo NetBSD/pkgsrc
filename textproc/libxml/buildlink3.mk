a0eb9b6a9af64d5a214c9cc6bd91aa5fe0acad69
snj <snj@pkgsrc.org>
2004-04-25 19:55:54 +0000

    Add include/gnome-xml/libxml/*.h to BUILDLINK_FILES, needed to get, e.g., net/soup to build.

--- textproc/libxml/buildlink3.mk
+++ textproc/libxml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/03/18 09:12:15 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/04/25 19:55:54 snj Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBXML_BUILDLINK3_MK:=	${LIBXML_BUILDLINK3_MK}+
@@ -12,6 +12,7 @@ BUILDLINK_PACKAGES+=	libxml
 
 .if !empty(LIBXML_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libxml+=	libxml>=1.8.11
+BUILDLINK_FILES.libxml+=	include/gnome-xml/libxml/*.h
 BUILDLINK_PKGSRCDIR.libxml?=	../../textproc/libxml
 .endif	# LIBXML_BUILDLINK3_MK
 
