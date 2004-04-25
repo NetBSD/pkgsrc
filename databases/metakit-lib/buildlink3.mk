fbb913d6219b2974d8c9d90dbbc91949c30afc08
recht <recht@pkgsrc.org>
2004-04-25 13:43:35 +0000

    Add buildlink3.mk.

---
+++ databases/metakit-lib/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/04/25 13:43:35 recht Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+METAKIT_LIB_BUILDLINK3_MK:=	${METAKIT_LIB_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	metakit-lib
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmetakit-lib}
+BUILDLINK_PACKAGES+=	metakit-lib
+
+.if !empty(METAKIT_LIB_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.metakit-lib+=	metakit-lib>=2.4.8
+BUILDLINK_PKGSRCDIR.metakit-lib?=	../../databases/metakit-lib
+.endif	# METAKIT_LIB_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
