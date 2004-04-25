54d9864bf409661547e1351709d7fbe0688ea58c
wiz <wiz@pkgsrc.org>
2004-04-25 00:19:10 +0000

    Remove XXX.

--- net/hesiod/buildlink3.mk
+++ net/hesiod/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/25 00:18:39 wiz Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.4.
-# XXX After this file as been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink[23].mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.hesiod?=	build
+# $NetBSD: buildlink3.mk,v 1.2 2004/04/25 00:19:10 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 HESIOD_BUILDLINK3_MK:=	${HESIOD_BUILDLINK3_MK}+
