442c3ef1480b445dc4b9d0e623a1664ac589b51f
wiz <wiz@pkgsrc.org>
2004-04-25 11:49:40 +0000

    Set CPPFLAGs like in bl2.

--- misc/rpm/buildlink3.mk
+++ misc/rpm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/25 11:47:08 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/04/25 11:49:40 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 RPM_BUILDLINK3_MK:=	${RPM_BUILDLINK3_MK}+
@@ -13,6 +13,8 @@ BUILDLINK_PACKAGES+=	rpm
 .if !empty(RPM_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.rpm+=	rpm>=2.5.4nb1
 BUILDLINK_PKGSRCDIR.rpm?=	../../misc/rpm
+
+BUILDLINK_CPPFLAGS.rpm= -I${BUILDLINK_PREFIX.rpm}/include/rpm
 .endif	# RPM_BUILDLINK3_MK
 
 .include "../../devel/gettext-lib/buildlink3.mk"
