1ec7ee4eef26cbf1c08b31cdf7922553699fd566
wiz <wiz@pkgsrc.org>
2004-04-25 00:34:21 +0000

    Static library only -> build dependency.

--- net/hesiod/buildlink3.mk
+++ net/hesiod/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/04/25 00:19:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/04/25 00:34:21 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 HESIOD_BUILDLINK3_MK:=	${HESIOD_BUILDLINK3_MK}+
@@ -13,6 +13,7 @@ BUILDLINK_PACKAGES+=	hesiod
 .if !empty(HESIOD_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.hesiod+=	hesiod>=3.0.2
 BUILDLINK_PKGSRCDIR.hesiod?=	../../net/hesiod
+BUILDLINK_DEPMETHOD.hesiod?=	build
 .endif	# HESIOD_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
