093fe775b90a95df001700595cd12d1ba8c1d560
snj <snj>
2004-04-25 06:10:09 +0000

    Convert to buildlink3.

---
+++ devel/libole2/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/04/25 06:10:09 snj Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBOLE2_BUILDLINK3_MK:=	${LIBOLE2_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libole2
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibole2}
+BUILDLINK_PACKAGES+=	libole2
+
+.if !empty(LIBOLE2_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libole2+=	libole2>=0.2.4nb1
+BUILDLINK_PKGSRCDIR.libole2?=	../../devel/libole2
+.endif	# LIBOLE2_BUILDLINK3_MK
+
+.include "../../devel/glib/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
