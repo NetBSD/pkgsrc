7b4f5aad6217007a27603742504ec83cbd9238a1
jlam <jlam@pkgsrc.org>
2004-04-26 05:09:29 +0000

    Add buildlink3.mk and builtin.mk files for devel/cdk that match the behaviour of cdk/buildlink2.mk.

---
+++ devel/cdk/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/04/26 05:09:29 jlam Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+CDK_BUILDLINK3_MK:=	${CDK_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	cdk
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ncdk}
+BUILDLINK_PACKAGES+=	cdk
+
+.if !empty(CDK_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.cdk+=		cdk>=4.9.9nb1
+BUILDLINK_PKGSRCDIR.cdk?=	../../devel/cdk
+.endif	# CDK_BUILDLINK3_MK
+
+.include "../../devel/ncurses/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
