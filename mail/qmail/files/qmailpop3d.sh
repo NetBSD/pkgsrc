4c9c344ba4b2c43845b5341776d8555898e1ffe1
schmonz <schmonz>
2004-04-27 03:05:09 +0000

    Explain why, after setting "command" in the usual way, we set it again in foo_precmd() to a different value. This is an unusual usage, and these comments may help if a future rc.subr isn't as accommodating to such trickery. Suggested by lukem.

--- mail/qmail/files/qmailpop3d.sh
+++ mail/qmail/files/qmailpop3d.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: qmailpop3d.sh,v 1.1 2004/04/24 05:29:36 schmonz Exp $
+# $NetBSD: qmailpop3d.sh,v 1.2 2004/04/27 03:05:09 schmonz Exp $
 #
 
 # PROVIDE: mail
@@ -21,6 +21,9 @@ fi
 
 qmailpop3d_precmd()
 {
+	# tcpserver(1) is akin to inetd(8), but runs one service per process.
+	# We want to signal only the tcpserver process responsible for POP
+	# service. Use argv0(1) to set procname to "qmailpop3d".
 	command="@SETENV@ - @LOCALBASE_NORMAL@/bin/argv0 @LOCALBASE_NORMAL@/bin/tcpserver $name $qmailpop3d_flags 0 110 @QMAILDIR@/bin/qmail-popup `@CAT@ @QMAILDIR@/control/me` @LOCALBASE_NORMAL@/bin/checkpassword @QMAILDIR@/bin/qmail-pop3d Maildir 2>&1 | @QMAILDIR@/bin/splogger pop3d 3"
 	command_args="&"
 	rc_flags=""
