f7086e8abb26734e43c2853a5d4c26ba533a8f74
adam <adam@pkgsrc.org>
2004-04-27 14:16:05 +0000

    buildlink3 fix

--- graphics/cairo/buildlink3.mk
+++ graphics/cairo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/04/04 16:54:22 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2004/04/27 14:16:05 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CAIRO_BUILDLINK3_MK:=	${CAIRO_BUILDLINK3_MK}+
@@ -21,7 +21,9 @@ BUILDLINK_DEPENDS.Xrender+=	Xrender>=0.8
 
 .include "../../fonts/Xft2/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/glitz/buildlink3.mk"
 .include "../../graphics/libpixman/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
 .include "../../x11/Xrender/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
