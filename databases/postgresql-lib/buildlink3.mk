ebb9163fb087a83c56c9f9f30d1ba0ca81d81ba8
snj <snj@pkgsrc.org>
2004-04-27 01:12:51 +0000

    Add include/pgsql/* to BUILDLINK_FILES.  Needed to get, e.g., gtranslator to build.

--- databases/postgresql-lib/buildlink3.mk
+++ databases/postgresql-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/03/26 02:27:37 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2004/04/27 01:12:51 snj Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 POSTGRESQL_LIB_BUILDLINK3_MK:=	${POSTGRESQL_LIB_BUILDLINK3_MK}+
@@ -15,6 +15,7 @@ BUILDLINK_DEPENDS.postgresql-lib+=	postgresql-lib>=7.3.1
 BUILDLINK_RECOMMENDED.postgresql-lib?=	postgresql-lib>=7.3.4nb2
 BUILDLINK_PKGSRCDIR.postgresql-lib?=	../../databases/postgresql-lib
 BUILDLINK_INCDIRS.postgresql-lib?=	include/postgresql
+BUILDLINK_FILES.postgresql-lib+=	include/pgsql/*
 .endif	# POSTGRESQL_LIB_BUILDLINK3_MK
 
 .include "../../security/openssl/buildlink3.mk"
