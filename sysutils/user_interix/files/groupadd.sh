1662cd40f5de72c32f353a51bb56da8c67bddd70
tv <tv@pkgsrc.org>
2004-04-27 01:45:22 +0000

    user(8) emulation scripts for Interix.

---
+++ sysutils/user_interix/files/groupadd.sh
@@ -0,0 +1,30 @@
+#!/bin/ksh
+# $NetBSD: groupadd.sh,v 1.1.1.1 2004/04/27 01:45:22 tv Exp $
+
+export PATH=/bin:/usr/contrib/win32/bin
+
+show_usage () {
+	echo "usage: $0 [-v] group" >&2
+	exit 1
+}
+
+verbose=false
+run_cmd () {
+	if $verbose; then printf '%s\n' "+ $*"; fi
+	"$@"
+}
+
+while getopts 'g:ov' f; do
+	case $f in
+	g)	echo "$0: numeric gid cannot be set manually; ignoring" >&2;;
+	o)	echo "$0: cannot reuse numeric gid of another group; aborting" >&2; exit 2;;
+	v)	verbose=true;;
+	\?)	show_usage;;
+	esac
+done
+shift $(($OPTIND - 1))
+
+if [ $# -ne 1 ]; then show_usage; fi
+$verbose || exec >/dev/null
+
+run_cmd net localgroup $1 /add /comment:"Group added by Interix groupadd command"
