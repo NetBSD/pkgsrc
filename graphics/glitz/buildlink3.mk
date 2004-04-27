db9e2863fb1a39fd199a6f5bb242d9baa01d25dd
adam <adam@pkgsrc.org>
2004-04-27 12:11:43 +0000

    Glitz is a portable 2D graphics library that uses OpenGL to render hardware accelerated graphics.  It supports many of the latest OpenGL features, such as efficient off-screen rendering using pbuffers. Rendering can be carried out by arbitrary OpenGL layers, thanks to glitz's extensible backend system.
    Glitz is designed to match the X Render Extension semantics and provides
    a general way for accelerating this imaging model.  Glitz can be used as
    a stand-alone layer above OpenGL but is also designed to act as a backend for
    cairo, providing it with OpenGL accelerated output.

---
+++ graphics/glitz/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/04/27 12:11:43 adam Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GLITZ_BUILDLINK3_MK:=	${GLITZ_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	glitz
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nglitz}
+BUILDLINK_PACKAGES+=	glitz
+
+.if !empty(GLITZ_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.glitz+=	glitz>=0.1.0
+BUILDLINK_PKGSRCDIR.glitz?=	../../graphics/glitz
+.endif	# GLITZ_BUILDLINK3_MK
+
+.include "../../graphics/MesaLib/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
