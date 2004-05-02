df7f8b6313a00f5c806ab4c2073f814e9075c84a
snj <snj@pkgsrc.org>
2004-05-02 05:35:17 +0000

    Convert to buildlink3.

---
+++ x11/py-qt2-sip/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/05/02 05:35:17 snj Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+PY_QT2_SIP_BUILDLINK3_MK:=	${PY_QT2_SIP_BUILDLINK3_MK}+
+
+.include "../../lang/python/pyversion.mk"
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	pyqt2sip
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npyqt2sip}
+BUILDLINK_PACKAGES+=	pyqt2sip
+
+.if !empty(PY_QT2_SIP_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.pyqt2sip+=	${PYPKGPREFIX}-qt2-sip-[0-9]*
+BUILDLINK_PKGSRCDIR.pyqt2sip?=	../../x11/py-qt2-sip
+.endif	# PY_QT2_SIP_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
