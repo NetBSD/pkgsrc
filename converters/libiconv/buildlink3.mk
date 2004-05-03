9f5830c8941c2f082a0d9ce28592931bf930593d
snj <snj@pkgsrc.org>
2004-05-03 02:22:24 +0000

    Add ${BUILDLINK_LDADD.iconv} to BUILDLINK_LDFLAGS.iconv.

--- converters/libiconv/buildlink3.mk
+++ converters/libiconv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2004/03/16 18:23:27 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2004/05/03 02:22:24 snj Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 ICONV_BUILDLINK3_MK:=	${ICONV_BUILDLINK3_MK}+
@@ -14,6 +14,7 @@ BUILDLINK_PACKAGES+=	iconv
 BUILDLINK_PKGBASE.iconv?=	libiconv
 BUILDLINK_DEPENDS.iconv+=	libiconv>=1.9.1
 BUILDLINK_PKGSRCDIR.iconv?=	../../converters/libiconv
+BUILDLINK_LDFLAGS.iconv=	${BUILDLINK_LDADD.iconv}
 .endif	# ICONV_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
