71f61a72918e7beb586c86d78197eaf219f4539f
snj <snj@pkgsrc.org>
2004-05-04 03:40:50 +0000

    Convert to buildlink3.

---
+++ sysutils/amanda-common/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/05/04 03:40:50 snj Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+AMANDA_COMMON_BUILDLINK3_MK:=	${AMANDA_COMMON_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	amanda-common
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Namanda-common}
+BUILDLINK_PACKAGES+=	amanda-common
+
+.if !empty(AMANDA_COMMON_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.amanda-common+=	amanda-common>=2.4.2p2
+BUILDLINK_PKGSRCDIR.amanda-common?=	../../sysutils/amanda-common
+.endif	# AMANDA_COMMON_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
