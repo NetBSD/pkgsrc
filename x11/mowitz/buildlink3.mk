779e717c4b8abfbd57846ca780847b826a4dbc55
wiz <wiz@pkgsrc.org>
2004-05-04 08:17:23 +0000

    Add (noted missing by kristerw).

---
+++ x11/mowitz/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/05/04 08:17:23 wiz Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+MOWITZ_BUILDLINK3_MK:=	${MOWITZ_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	Mowitz
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NMowitz}
+BUILDLINK_PACKAGES+=	Mowitz
+
+.if !empty(MOWITZ_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.Mowitz+=	Mowitz>=0.2.1
+BUILDLINK_PKGSRCDIR.Mowitz?=	../../x11/mowitz
+.endif	# MOWITZ_BUILDLINK3_MK
+
+.include "../../graphics/xpm/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
