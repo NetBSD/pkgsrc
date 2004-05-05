dd243a2af231a3cd0978adb8680c25289684ae14
xtraeme <xtraeme@pkgsrc.org>
2004-05-05 18:06:03 +0000

    Remove BUILDLINK_FILES_CMD which didn't work, put BUILDLINK_* definitions into the proper block.

--- lang/kaffe/buildlink3.mk
+++ lang/kaffe/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/04/06 18:15:54 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/05/05 18:06:03 xtraeme Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 KAFFE_BUILDLINK3_MK:=	${KAFFE_BUILDLINK3_MK}+
@@ -13,17 +13,11 @@ BUILDLINK_PACKAGES+=	kaffe
 .if !empty(KAFFE_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.kaffe+=	kaffe-[0-9]*
 BUILDLINK_PKGSRCDIR.kaffe?=	../../lang/kaffe
-.endif	# KAFFE_BUILDLINK3_MK
-
-BUILDLINK_FILES_CMD.kaffe=				    \
-	${BUILDLINK_PLIST_CMD.kaffe} |			    \
-	    ${GREP} '\(include.*/\|lib.*/lib[^/]*$$\)' |    \
-	    ${SED} "s,^,$${pkg_prefix},"
-
+EVAL_PREFIX+=			BUILDLINK_JAVA_PREFIX.kaffe=kaffe
 BUILDLINK_JAVA_PREFIX.kaffe=	${BUILDLINK_PREFIX.kaffe}/java/kaffe
-
 BUILDLINK_CPPFLAGS.kaffe=				    \
 	-I${BUILDLINK_JAVA_PREFIX.kaffe}/include	    \
 	-I${BUILDLINK_JAVA_PREFIX.kaffe}/include/kaffe
+.endif  # KAFFE_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
