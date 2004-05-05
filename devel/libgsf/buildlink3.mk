2d45b2215584e8896439087d9e8677e2d33efcd2
jmmv <jmmv@pkgsrc.org>
2004-05-05 14:06:35 +0000

    Update to 1.9.0:
    Dom:
    	* Fix bzip output to flush if last deflate fills buffer
    
    Jody Goldberg:
    	* If the file is not an OLE2 file do not change the filepos.
    	* Handle broken ole files that specify codepage as signed short
    	* Fix reading ole files > 13.6 Meg on sparc/alpha (from Xavier Roche)
    	* Tune validation test for LPSTR properties to handle char width > 1
    	* Make the impl headers for the base interfaces public as requested
    	* Add some directory tree wrappers
    	* Work around a -fstrict-alias warning for SuSE
    	* Move to automake-1.7
    	* Fix gnome-vfs backend to open files with random access
    	* Support merging content from nested nodes for sax parsing
    	* Fix gtk-doc
    
    Joseph Frazee:
    	* patch the spec file.
    
    Morten Welinder:
    	* Properly chain finalizer.
    	* Fix gzip output to flush if last deflate fills buffer
    
    Tor Lillqvist:
    	* packaging tools for win32.

--- devel/libgsf/buildlink3.mk
+++ devel/libgsf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/03/26 02:27:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/05/05 14:06:35 jmmv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBGSF_BUILDLINK3_MK:=	${LIBGSF_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgsf}
 BUILDLINK_PACKAGES+=	libgsf
 
 .if !empty(LIBGSF_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libgsf+=	libgsf>=1.8.2nb3
-BUILDLINK_RECOMMENDED.libgsf?=	libgsf>=1.8.2nb7
+BUILDLINK_DEPENDS.libgsf+=	libgsf>=1.9.0
 BUILDLINK_PKGSRCDIR.libgsf?=	../../devel/libgsf
 .endif	# LIBGSF_BUILDLINK3_MK
 
