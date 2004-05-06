84319615a4f6dfe138141eb4bd2e0a5edd05c17d
jmmv <jmmv@pkgsrc.org>
2004-05-06 11:15:31 +0000

    Reorganize the code a bit to let different providers be built as external packages easily.

--- databases/libgda/buildlink3.mk
+++ databases/libgda/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/03/18 09:12:09 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/05/06 11:15:31 jmmv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBGDA_BUILDLINK3_MK:=	${LIBGDA_BUILDLINK3_MK}+
@@ -13,6 +13,9 @@ BUILDLINK_PACKAGES+=	libgda
 .if !empty(LIBGDA_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libgda+=	libgda>=1.0.3
 BUILDLINK_PKGSRCDIR.libgda?=	../../databases/libgda
+
+PRINT_PLIST_AWK+=	/^@dirrm lib\/libgda\/providers$$/ \
+				{ print "@comment in libgda: " $$0; next; }
 .endif	# LIBGDA_BUILDLINK3_MK
 
 .include "../../devel/glib2/buildlink3.mk"
