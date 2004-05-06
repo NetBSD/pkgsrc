10657dd8718e91d769a977f13fd60ec5b4d3f35a
jmmv <jmmv@pkgsrc.org>
2004-05-06 14:00:27 +0000

    Add some PRINT_PLIST_AWK magic so that packages using this buildlink3.mk file get the right comments in generated PLISTs.  (I'm not aware of packages actually needing this, but will help in catching 'bugs' in the future, i.e, "cannot remove non-empty directory foobar").

--- graphics/gnome-icon-theme/buildlink3.mk
+++ graphics/gnome-icon-theme/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/01 18:29:19 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/05/06 14:00:27 jmmv Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 GNOME_ICON_THEME_BUILDLINK3_MK:=	${GNOME_ICON_THEME_BUILDLINK3_MK}+
@@ -13,9 +13,21 @@ BUILDLINK_PACKAGES+=	gnome-icon-theme
 .if !empty(GNOME_ICON_THEME_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gnome-icon-theme+=	gnome-icon-theme>=1.2.0
 BUILDLINK_PKGSRCDIR.gnome-icon-theme?=	../../graphics/gnome-icon-theme
+
+.  include "../../mk/bsd.prefs.mk"
+
+dirs!=	${GREP} "^@dirrm" ${.CURDIR}/../../graphics/gnome-icon-theme/PLIST | \
+	${CUT} -d ' ' -f 2
+.  for d in ${dirs}
+PRINT_PLIST_AWK+=	/^@dirrm ${d:S/\//\\\//g}$$/ \
+			{ print "@comment in gnome-icon-theme: " $$0; next; }
+.  endfor
+.  undef d
+.  undef dirs
 .endif	# GNOME_ICON_THEME_BUILDLINK3_MK
 
 .include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../graphics/hicolor-icon-theme/buildlink3.mk"
 .include "../../lang/perl5/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
