31cb5750c10b14ab2abcf466bfcf652e78717094
jmmv <jmmv@pkgsrc.org>
2004-05-07 11:13:16 +0000

    Update to 0.11.11:
    - Add APIs for setting font with/without antialiasing, cursor color,
      hilite color, and a forkpty()-alike.  Fix meta-space.  Use glib 2.4's
      child watch API if available.
    - Add a configure switch for setting the default emulation instead of
      hard-coding it to be "xterm".
    - Tweak autowrapping of text to handle cases where the terminal has
      both LP and xn capabilities.
    - Truncate empty lines when copying text to mimic xterm.
    - Internally abstract out matching APIs, though we still use POSIX regex.
    - Try to set UTF8 line editing mode under sufficiently-new Linux.
    - Obey Pango's specified attributes when displaying pre-edit text.
    - Never steal modifier keys which might affect the input method from
      the input methods.
    - Fix python binding so that help() lists the terminal class.

--- x11/vte/buildlink3.mk
+++ x11/vte/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/04/01 18:49:52 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/05/07 11:13:16 jmmv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 VTE_BUILDLINK3_MK:=	${VTE_BUILDLINK3_MK}+
@@ -11,19 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nvte}
 BUILDLINK_PACKAGES+=	vte
 
 .if !empty(VTE_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.vte+=		vte>=0.11.10nb2
+BUILDLINK_DEPENDS.vte+=		vte>=0.11.11
 BUILDLINK_PKGSRCDIR.vte?=	../../x11/vte
-
-BUILDLINK_FILES.vte+=	lib/vte/decset
-BUILDLINK_FILES.vte+=	lib/vte/interpret
-BUILDLINK_FILES.vte+=	lib/vte/iso8859mode
-BUILDLINK_FILES.vte+=	lib/vte/nativeecho
-BUILDLINK_FILES.vte+=	lib/vte/osc
-BUILDLINK_FILES.vte+=	lib/vte/slowcat
-BUILDLINK_FILES.vte+=	lib/vte/utf8echo
-BUILDLINK_FILES.vte+=	lib/vte/utf8mode
-BUILDLINK_FILES.vte+=	lib/vte/vterdb
-BUILDLINK_FILES.vte+=	lib/vte/window
 .endif	# VTE_BUILDLINK3_MK
 
 .include "../../x11/gtk2/buildlink3.mk"
