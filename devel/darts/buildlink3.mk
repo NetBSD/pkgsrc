a32a980e6213d57316f0d80ea63aae56b3223dd0
kristerw <kristerw@pkgsrc.org>
2004-05-08 21:34:43 +0000

    Correct path for BUILDLINK_PKGSRCDIR.darts.

--- devel/darts/buildlink3.mk
+++ devel/darts/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/05/06 13:22:43 taca Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/05/08 21:34:43 kristerw Exp $
 
 BUILDLINK_DEPMETHOD.darts?=	build
 
@@ -14,7 +14,7 @@ BUILDLINK_PACKAGES+=	darts
 
 .if !empty(DARTS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.darts+=	darts>=0.2
-BUILDLINK_PKGSRCDIR.darts?=	../../textproc/darts
+BUILDLINK_PKGSRCDIR.darts?=	../../devel/darts
 .endif	# DARTS_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
