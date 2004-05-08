2749dade88d1714c33166d8131cda9dcc8f503bf
wiz <wiz@pkgsrc.org>
2004-05-08 08:53:54 +0000

    Unused.

--- textproc/py-FourSuite/buildlink2.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: buildlink2.mk,v 1.2 2004/03/29 05:05:45 jlam Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_PACKAGES+=		pyfoursuite
-BUILDLINK_PKGBASE.pyfoursuite?=	${PYPKGPREFIX}-FourSuite
-BUILDLINK_DEPENDS.pyfoursuite?=	${PYPKGPREFIX}-FourSuite-[0-9]*
-BUILDLINK_PKGSRCDIR.pyfoursuite?=	../../textproc/py-FourSuite
