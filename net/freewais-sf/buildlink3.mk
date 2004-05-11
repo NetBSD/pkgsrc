334d583b7f5c22731a151eb3d476589dbd02e9e9
snj <snj@pkgsrc.org>
2004-05-11 04:07:52 +0000

    Convert to buildlink3.

---
+++ net/freewais-sf/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/05/11 04:07:52 snj Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+FREEWAIS_SF_BUILDLINK3_MK:=	${FREEWAIS_SF_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	freewais-sf
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nfreewais-sf}
+BUILDLINK_PACKAGES+=	freewais-sf
+
+.if !empty(FREEWAIS_SF_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.freewais-sf+=	freewais-sf>=2.2.12
+BUILDLINK_PKGSRCDIR.freewais-sf?=	../../net/freewais-sf
+.endif	# FREEWAIS_SF_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
