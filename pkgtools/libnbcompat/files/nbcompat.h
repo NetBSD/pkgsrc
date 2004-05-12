4e0e80c1ba068096156abc1c69649591230c5f06
grant <grant>
2004-05-12 21:50:43 +0000

    don't include poll.h or sys/poll.h, but instead, undefine HAVE_POLL_H and HAVE_SYS_POLL_H if we don't have a native poll() implementation. prevents including of libnbcompat's poll.h as well as the system's poll.h where we should be ignoring the system's poll.h.
    fix tested by Jeff Rizzo on MacOSX 10.3.3.

--- pkgtools/libnbcompat/files/nbcompat.h
+++ pkgtools/libnbcompat/files/nbcompat.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: nbcompat.h,v 1.33 2004/04/20 12:13:05 grant Exp $	*/
+/*	$NetBSD: nbcompat.h,v 1.34 2004/05/12 21:50:43 grant Exp $	*/
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -66,13 +66,9 @@
 #include <termios.h>
 #include <unistd.h>
 
-#if HAVE_POLL
-# if HAVE_POLL_H
-#  include <poll.h>
-# elif HAVE_SYS_POLL_H
-#  include <sys/poll.h>
-# endif
-#else
+#if !HAVE_POLL
+# undef HAVE_POLL_H
+# undef HAVE_SYS_POLL_H
 # include <nbcompat/poll.h>
 #endif
 
