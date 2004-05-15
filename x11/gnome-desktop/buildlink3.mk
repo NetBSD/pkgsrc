ac4d7250e35c39ec73cd64ccdc46fcedac6bb0cd
jmmv <jmmv@pkgsrc.org>
2004-05-15 14:55:46 +0000

    During print-PLIST in packages using this one, print a comment for the share/gnome/vfolders directory, instead of trying to remove it.

--- x11/gnome-desktop/buildlink3.mk
+++ x11/gnome-desktop/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/04/01 18:36:11 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2004/05/15 14:55:46 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_DESKTOP_BUILDLINK3_MK:=	${GNOME_DESKTOP_BUILDLINK3_MK}+
@@ -13,6 +13,10 @@ BUILDLINK_PACKAGES+=	gnome-desktop
 .if !empty(GNOME_DESKTOP_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gnome-desktop+=	gnome-desktop>=2.6.0
 BUILDLINK_PKGSRCDIR.gnome-desktop?=	../../x11/gnome-desktop
+
+PRINT_PLIST_AWK+=	/^@dirrm share\/gnome\/vfolders$$/ \
+				{ print "@comment in gnome-desktop: " $$0; \
+				  next; }
 .endif	# GNOME_DESKTOP_BUILDLINK3_MK
 
 .include "../../devel/gettext-lib/buildlink3.mk"
