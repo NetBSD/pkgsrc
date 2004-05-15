e06430973c2cc298ff2d6e15218aa2e414ac3831
itojun <itojun>
2004-05-15 21:00:57 +0000

    ISC BIND 9.3.0beta3.  changelog is too many to mention here.

---
+++ net/bind9-current/files/lwresd.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# $NetBSD: lwresd.sh,v 1.6 2004/05/15 21:00:58 itojun Exp $
+#
+# PROVIDE: lwresd
+# REQUIRE: SERVERS
+
+. /etc/rc.subr
+
+name="lwresd"
+rcvar=${name}
+command="@PREFIX@/sbin/${name}"
+pidfile="/var/run/${name}.pid"
+
+extra_commands="reload"
+
+load_rc_config ${name}		# check /etc/rc.conf.d/named9
+run_rc_command "$1"
