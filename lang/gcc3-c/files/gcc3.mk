b12a6c7269697ec545e3ca484eac40e8f0c0d626
jschauma <jschauma@pkgsrc.org>
2004-05-20 15:05:46 +0000

    Set the CC variable to .../bin/gcc (not .../bin/cc):  on some systems, there exists a 'cc', leading to buildlink creating a 'cc' wrapper pointing to the original 'cc', which then won't work with the gcc flags, while the 'gcc' wrapper does work (since packages simply call 'cc' (or 'gcc') and rely on buildlink having prepended the path to the right wrapper).

--- lang/gcc3-c/files/gcc3.mk
+++ lang/gcc3-c/files/gcc3.mk
@@ -1,6 +1,6 @@
-# $NetBSD: gcc3.mk,v 1.5 2003/09/16 15:47:21 drochner Exp $
+# $NetBSD: gcc3.mk,v 1.6 2004/05/20 15:05:46 jschauma Exp $
 #
 # make configuration file for @PKGNAME@
 
-CC=		@GCC_PREFIX@/bin/cc
+CC=		@GCC_PREFIX@/bin/gcc
 CPP=		@GCC_PREFIX@/bin/cpp
