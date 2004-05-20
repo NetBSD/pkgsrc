f05a6ca5b8e02901fcbbb48e77104000c054d6dc
danw <danw@pkgsrc.org>
2004-05-20 16:13:05 +0000

    Revert the change to put -liconv into BUILDLINK_LDFLAGS.iconv (which breaks some things on Darwin) and fix audio/musicpd so it doesn't need it (by faking out a configure check that gets confused by the iconv_open -> libiconv_open renaming)

--- converters/libiconv/buildlink3.mk
+++ converters/libiconv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2004/05/17 21:32:34 seb Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2004/05/20 16:13:05 danw Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 ICONV_BUILDLINK3_MK:=	${ICONV_BUILDLINK3_MK}+
@@ -13,7 +13,6 @@ BUILDLINK_PACKAGES+=	iconv
 .if !empty(ICONV_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.iconv+=	libiconv>=1.9.1
 BUILDLINK_PKGSRCDIR.iconv?=	../../converters/libiconv
-BUILDLINK_LDFLAGS.iconv=	${BUILDLINK_LDADD.iconv}
 .endif	# ICONV_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
