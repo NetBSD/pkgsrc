ef5b4917510be7e3d9a4bf3ca5db41c66e05fe9b
kim <kim@pkgsrc.org>
2004-05-23 00:58:25 +0000

    Bump required version, pine 4.60 does not compile with 2002.5nb3

--- mail/imap-uw/buildlink3.mk
+++ mail/imap-uw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/22 21:31:22 ben Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/05/23 00:58:25 kim Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 IMAP_UW_BUILDLINK3_MK:=	${IMAP_UW_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nimap-uw}
 BUILDLINK_PACKAGES+=	imap-uw
 
 .if !empty(IMAP_UW_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.imap-uw+=	imap-uw>=2002.5nb3
+BUILDLINK_DEPENDS.imap-uw+=	imap-uw>=2004
 BUILDLINK_PKGSRCDIR.imap-uw?=	../../mail/imap-uw
 .endif	# IMAP_UW_BUILDLINK3_MK
 
