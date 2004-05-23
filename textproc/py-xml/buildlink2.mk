b767b43c6b22a4092bca98d3586068db4d35c306
recht <recht@pkgsrc.org>
2004-05-23 16:59:05 +0000

    unused

--- textproc/py-xml/buildlink2.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: buildlink2.mk,v 1.2 2004/03/29 05:05:45 jlam Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_PACKAGES+=		pyxml
-BUILDLINK_PKGBASE.pyxml?=	${PYPKGPREFIX}-xml
-BUILDLINK_DEPENDS.pyxml?=	${PYPKGPREFIX}-xml-[0-9]*
-BUILDLINK_PKGSRCDIR.pyxml?=	../../textproc/py-xml
