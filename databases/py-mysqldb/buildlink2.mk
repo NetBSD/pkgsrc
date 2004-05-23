c26e8b0b390d0f0286770c483bef5b4910d35168
recht <recht>
2004-05-23 16:48:45 +0000

    unused

--- databases/py-mysqldb/buildlink2.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: buildlink2.mk,v 1.3 2004/03/29 05:05:33 jlam Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_PACKAGES+=		pymysqldb
-BUILDLINK_PKGBASE.pymysqldb?=	${PYPKGPREFIX}-mysqldb
-BUILDLINK_DEPENDS.pymysqldb?=	${PYPKGPREFIX}-mysqldb-[0-9]*
-BUILDLINK_PKGSRCDIR.pymysqldb?=	../../databases/py-mysqldb
