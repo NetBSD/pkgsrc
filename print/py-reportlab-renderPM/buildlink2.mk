fdc9c99f75b0c0776baef735fd00d1cb8baaf3c7
recht <recht@pkgsrc.org>
2004-05-23 16:59:05 +0000

    unused

--- print/py-reportlab-renderPM/buildlink2.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: buildlink2.mk,v 1.2 2004/03/29 05:05:44 jlam Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_PACKAGES+=		pyrlrenderpm
-BUILDLINK_PKGBASE.pyrlrenderpm?= ${PYPKGPREFIX}-reportlab-renderPM
-BUILDLINK_DEPENDS.pyrlrenderpm?= ${PYPKGPREFIX}-reportlab-renderPM-[0-9]*
-BUILDLINK_PKGSRCDIR.pyrlrenderpm=../../print/py-reportlab-renderPM
--- print/py-reportlab/buildlink2.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: buildlink2.mk,v 1.3 2004/03/29 05:05:43 jlam Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_PACKAGES+=		pyreportlab
-BUILDLINK_PKGBASE.pyreportlab?= ${PYPKGPREFIX}-reportlab
-BUILDLINK_DEPENDS.pyreportlab?= ${PYPKGPREFIX}-reportlab-[0-9]*
-BUILDLINK_PKGSRCDIR.pyreportlab=../../print/py-reportlab
