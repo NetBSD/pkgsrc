d32316195632f2c336c09b044f9caf952eba7b4f
recht <recht@pkgsrc.org>
2004-05-23 11:58:21 +0000

    unused

--- net/py-mimelib/buildlink2.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: buildlink2.mk,v 1.2 2004/03/29 05:05:42 jlam Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_PACKAGES+=		pymimelib
-BUILDLINK_PKGBASE.pymimelib?=	${PYPKGPREFIX}-mimelib
-BUILDLINK_DEPENDS.pymimelib?=	${PYPKGPREFIX}-mimelib-[0-9]*
-BUILDLINK_PKGSRCDIR.pymimelib?=	../../net/py-mimelib
