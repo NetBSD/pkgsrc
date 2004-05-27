ae0037c41d255e8833501b0ea016bb401494dd2e
reed <reed@pkgsrc.org>
2004-05-27 00:51:39 +0000

    The libopencdk.so.8.0 used libgcrypt.so.11. libgcrypt was recently upgraded. So when using binary packages it is possible for new libgcrypt (libgcrypt.so.12.1) to be installed with old opencdk-0.5.4 package.
    So bump PKGREVISION and BUILDLINK_DEPENDS (to force a new package
    to be required).

--- security/opencdk/buildlink3.mk
+++ security/opencdk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/04/02 15:57:58 agc Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/05/27 00:51:39 reed Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 OPENCDK_BUILDLINK3_MK:=	${OPENCDK_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nopencdk}
 BUILDLINK_PACKAGES+=	opencdk
 
 .if !empty(OPENCDK_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.opencdk+=	opencdk>=0.5.4
+BUILDLINK_DEPENDS.opencdk+=	opencdk>=0.5.4nb1
 BUILDLINK_PKGSRCDIR.opencdk?=	../../security/opencdk
 .endif	# OPENCDK_BUILDLINK3_MK
 
