c3488f089b0622b78af59d1bc6366345c633824e
jschauma <jschauma@pkgsrc.org>
2004-05-31 16:47:00 +0000

    Use a cleaner approach for previous: - set RSH on an opsys-specific basis, defaulting to /usr/bin/rsh   (XXX: please confirm/verify this on your OS) - set SSH_RSHPATH to RSH in bsd.pkg.defaults.mk

--- mk/defs.FreeBSD.mk
+++ mk/defs.FreeBSD.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.FreeBSD.mk,v 1.39 2004/05/12 14:10:18 jschauma Exp $
+# $NetBSD: defs.FreeBSD.mk,v 1.40 2004/05/31 16:47:00 jschauma Exp $
 #
 # Variable definitions for the FreeBSD operating system.
 
@@ -64,6 +64,9 @@ PS?=		/bin/ps
 PWD_CMD?=	/bin/pwd	# needs to print physical path
 RM?=		/bin/rm
 RMDIR?=		/bin/rmdir
+# XXX: default from bsd.pkg.defaults.mk.  Verify/corerct for this platform
+# and remove this comment.
+RSH?=		/usr/bin/rsh
 SED?=		/usr/bin/sed
 SETENV?=	/usr/bin/env
 SH?=		/bin/sh
