3ba662f8458b0b4e5ec6348af746104d2fd92bc3
recht <recht>
2004-06-01 16:58:06 +0000

    update to libsoup-devel-2.1.10
    No real NEWS file, so I'm guessing from the ChangeLog that it's
    the usual bugfixing stuff.

--- net/libsoup-devel/buildlink3.mk
+++ net/libsoup-devel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/04/02 18:11:57 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/06/01 16:58:06 recht Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBSOUP_DEVEL_BUILDLINK3_MK:=	${LIBSOUP_DEVEL_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibsoup-devel}
 BUILDLINK_PACKAGES+=	libsoup-devel
 
 .if !empty(LIBSOUP_DEVEL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libsoup-devel+=	libsoup-devel>=2.1.8
+BUILDLINK_DEPENDS.libsoup-devel+=	libsoup-devel>=2.1.10
 BUILDLINK_PKGSRCDIR.libsoup-devel?=	../../net/libsoup-devel
 .endif	# LIBSOUP_DEVEL_BUILDLINK3_MK
 
