b072126a953d48aa02ce373da4e17bdeabcd37dd
recht <recht@pkgsrc.org>
2004-06-02 12:30:55 +0000

    catch up with the Python 2.3.4 update

--- lang/python/srcdist.mk
+++ lang/python/srcdist.mk
@@ -1,16 +1,16 @@
-# $NetBSD: srcdist.mk,v 1.10 2003/12/23 17:24:42 recht Exp $
+# $NetBSD: srcdist.mk,v 1.11 2004/06/02 12:30:55 recht Exp $
 
 .include "../../lang/python/pyversion.mk"
 
 .if ${_PYTHON_VERSION} == "23" || ${_PYTHON_VERSION} == "23pth"
 
-DISTNAME=	Python-2.3.3
+DISTNAME=	Python-2.3.4
 EXTRACT_SUFX=	.tgz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python23/distinfo
 PATCHDIR=	${.CURDIR}/../../lang/python23/patches
-PYSUBDIR=	Python-2.3.3
+PYSUBDIR=	Python-2.3.4
 WRKSRC=		${WRKDIR}/${PYSUBDIR}
-MASTER_SITES=	ftp://ftp.python.org/pub/python/2.3.3/
+MASTER_SITES=	ftp://ftp.python.org/pub/python/2.3.4/
 
 .elif ${_PYTHON_VERSION} == "22" || ${_PYTHON_VERSION} == "22pth"
 
