1c7782891139ff77e25dc542b2ea8890a0818043
minskim <minskim@pkgsrc.org>
2004-06-03 16:28:53 +0000

    Remove dependency on libstroke, which is not used by this package. OK'ed by dmcmahill@, the maintainer.
    Bump PKGREVISION due to the dependency change.

--- cad/libgeda/buildlink3.mk
+++ cad/libgeda/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/03/18 09:12:09 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/06/03 16:28:53 minskim Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBGEDA_BUILDLINK3_MK:=	${LIBGEDA_BUILDLINK3_MK}+
@@ -15,7 +15,6 @@ BUILDLINK_DEPENDS.libgeda+=	libgeda-20040111{,nb[0-9]*}
 BUILDLINK_PKGSRCDIR.libgeda?=	../../cad/libgeda
 .endif	# LIBGEDA_BUILDLINK3_MK
 
-.include "../../devel/libstroke/buildlink3.mk"
 .include "../../graphics/libgdgeda/buildlink3.mk"
 .include "../../lang/guile/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
