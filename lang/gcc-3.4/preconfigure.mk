12ee3338f71aedca1a6adf2aa7c230a12095682f
shannonjr <shannonjr@pkgsrc.org>
2004-06-03 21:27:04 +0000

    Removing package on instructions from Lubomir Sedlacik <salo@xtrmntr.org>

--- lang/gcc-3.4/preconfigure.mk
+++
@@ -1,9 +0,0 @@
-	# Create compiler driver scripts in ${WRKDIR}.
-	(cd ${BUILDLINK_PREFIX.gcc-3.4}/bin && bin_files=`ls *` && \
-	cd ${WRKDIR}/.gcc/bin && \
-	for _target_ in $${bin_files} ; do \
-		${ECHO} '#!${TOOLS_SHELL}' > $${_target_} && \
-		${ECHO} -n "exec ${LOCALBASE}/gcc-3.4.0/bin/$${_target_}" >> $${_target_} && \
-		${ECHO} ' "$$@"' >> $${_target_} && \
-		${CHMOD} +x $${_target_}; \
-	done )
--- lang/gccAda-3.4/preconfigure.mk
+++
@@ -1,9 +0,0 @@
-	# Create compiler driver scripts in ${WRKDIR}.
-	(cd ${BUILDLINK_PREFIX.gccAda-3.4.0}/bin && bin_files=`${FIND} . -type f \( -perm -0100 \)` && \
-	cd ${WRKDIR}/.gcc/bin && \
-	for _target_ in $${bin_files} ; do \
-		${ECHO} '#!${TOOLS_SHELL}' > $${_target_} && \
-		${ECHO} -n "exec ${LOCALBASE}/gccAda-3.4.0/bin/$${_target_}" >> $${_target_} && \
-		${ECHO} ' "$$@"' >> $${_target_} && \
-		${CHMOD} +x $${_target_}; \
-	done )
