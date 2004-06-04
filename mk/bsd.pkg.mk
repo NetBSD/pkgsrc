bf4bdec37c16c2a26deb2668f214554c3f161b68
jschauma <jschauma@pkgsrc.org>
2004-06-04 15:00:14 +0000

    Append ${ABI} to some lib-dirs.  This is a no-op on systems that do not use ABI, but allows IRIX and particularly IRIX64 to find the correct libraries especially when linking against X11 libs.  Tested over several months and multiple bulk-builds.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1466 2004/06/04 13:45:26 xtraeme Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1467 2004/06/04 15:00:14 jschauma Exp $
 #
 # This file is in the public domain.
 #
@@ -440,8 +440,8 @@ SHCOMMENT?=		${ECHO_MSG} >/dev/null '***'
 DISTINFO_FILE?=		${.CURDIR}/distinfo
 
 .if defined(USE_X11)
-X11_LDFLAGS+=		-Wl,${RPATH_FLAG}${X11BASE}/lib
-X11_LDFLAGS+=		-L${X11BASE}/lib
+X11_LDFLAGS+=		-Wl,${RPATH_FLAG}${X11BASE}/lib${ABI}
+X11_LDFLAGS+=		-L${X11BASE}/lib${ABI}
 .endif
 .if !empty(USE_BUILDLINK2:M[nN][oO]) && !empty(USE_BUILDLINK3:M[nN][oO])
 LDFLAGS+=		-Wl,${RPATH_FLAG}${LOCALBASE}/lib
@@ -1081,7 +1081,7 @@ CONFIGURE_ARGS+=	--prefix=${GNU_CONFIGURE_PREFIX}
 HAS_CONFIGURE=		yes
 .  if defined(USE_X11)
 CONFIGURE_ARGS+=	--x-includes=${X11BASE}/include
-CONFIGURE_ARGS+=        --x-libraries=${X11BASE}/lib
+CONFIGURE_ARGS+=        --x-libraries=${X11BASE}/lib${ABI}
 .  endif
 CONFIGURE_HAS_INFODIR?=	yes
 .  if !empty(INFO_FILES) && !empty(CONFIGURE_HAS_INFODIR:M[yY][eE][sS])
