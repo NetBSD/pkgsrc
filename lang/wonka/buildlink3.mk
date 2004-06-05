c67c0108b8ccf19719ffa5eb35af15c7d8c719cd
xtraeme <xtraeme@pkgsrc.org>
2004-06-05 16:33:52 +0000

    Simplify BUILDLINK_JAVA_PREFIX.<jvm>.

--- lang/wonka/buildlink3.mk
+++ lang/wonka/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/05/05 18:04:29 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/06/05 16:35:51 xtraeme Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 WONKA_BUILDLINK3_MK:=	${WONKA_BUILDLINK3_MK}+
@@ -13,14 +13,10 @@ BUILDLINK_PACKAGES+=	wonka
 .if !empty(WONKA_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.wonka+=	wonka-[0-9]*
 BUILDLINK_PKGSRCDIR.wonka?=	../../lang/wonka
-EVAL_PREFIX+=   BUILDLINK_JAVA_PREFIX.wonka=wonka
-BUILDLINK_JAVA_PREFIX.wonka_DEFAULT=					\
-	${BUILDLINK_PREFIX.wonka}/java/wonka
-
+BUILDLINK_JAVA_PREFIX.wonka=	${PREFIX}/java/wonka
 BUILDLINK_CPPFLAGS.wonka=						\
 	-I${BUILDLINK_JAVA_PREFIX.wonka}/include			\
 	-I${BUILDLINK_JAVA_PREFIX.wonka}/include/wonka
-
 .endif	# WONKA_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
