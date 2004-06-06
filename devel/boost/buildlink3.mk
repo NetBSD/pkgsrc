1174972d51d9d79207b338902a6f7b194178be28
tv <tv>
2004-06-06 23:51:36 +0000

    Update devel/boost to 1.31.0.  The most notable change here is a more robust build system, which is actually being used now to build the backing binary libraries necessary to make date_time, regex, and a few other libraries work.
    While here, the thread and python libraries have been split out to their
    own subpackages, devel/boost-thread and devel/boost-python, so that the
    main boost package need not rely on the presence of either to provide
    basic functionality.

--- devel/boost/buildlink3.mk
+++ devel/boost/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/06 23:46:06 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/06/06 23:51:36 tv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 BOOST_BUILDLINK3_MK:=	${BOOST_BUILDLINK3_MK}+
@@ -11,8 +11,11 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nboost}
 BUILDLINK_PACKAGES+=	boost
 
 .if !empty(BOOST_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.boost+=	boost>=1.30.2
+BUILDLINK_DEPENDS.boost?=	boost-1.31.* # ABI requires exactly this pattern
 BUILDLINK_PKGSRCDIR.boost?=	../../devel/boost
+BUILDLINK_CPPFLAGS.boost+=	-I${BUILDLINK_PREFIX.boost}/include/boost-1_31
 .endif	# BOOST_BUILDLINK3_MK
+
+.include "../../devel/boost/toolset.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
