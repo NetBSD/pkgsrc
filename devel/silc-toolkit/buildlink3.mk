100720b6e02a4768c0d4cc494e14819ca2984118
salo <salo@pkgsrc.org>
2004-06-06 12:23:17 +0000

    Initial import of silc-toolkit-0.9.12: Secure Internet Live Conferencing (SILC) protocol Toolkit.
    SILC (Secure Internet Live Conferencing) is a protocol which provides
    secure conferencing services in the Internet over insecure channel.
    
    SILC superficially resembles IRC, although they are very different
    internally. SILC is much more than just about `encrypting the traffic'.
    That is easy enough to do with IRC and SSL hybrids, but even then the
    entire network cannot be secured, only part of it.
    
    SILC provides security services, such as sending private messages entirely
    secure; noone can see the message except you and the real receiver of the
    message. SILC also provides same functionality for channels; noone except
    those clients joined to the channel may see the messages destined to the
    channel. Communication between client and server is also secured with
    session keys and all commands, authentication data (such as passwords etc.)
    and other traffic is entirely secured. The entire network, and all parts of
    it, is secured.
    
    SILC has secure key exchange protocol that is used to create the session keys
    for each connection. SILC also provides strong authentication based on either
    passwords or public key authentication. All authentication data is always
    encrypted in the SILC network. Each connection has their own session keys,
    all channels have channel specific keys, and all private messages can be
    secured with private message specific keys.

---
+++ devel/silc-toolkit/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/06/06 12:23:17 salo Exp $
+#
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+SILC_TOOLKIT_BUILDLINK3_MK:=	${SILC_TOOLKIT_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	silc-toolkit
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nsilc-toolkit}
+BUILDLINK_PACKAGES+=	silc-toolkit
+
+.if !empty(SILC_TOOLKIT_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.silc-toolkit+=	silc-toolkit>=0.9.12
+BUILDLINK_PKGSRCDIR.silc-toolkit?=	../../devel/silc-toolkit
+.endif	# SILC_TOOLKIT_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
