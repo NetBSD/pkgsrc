ea2c4272213274910c4792688dfffa635fbe6929
grant <grant>
2004-06-12 04:01:45 +0000

    swap order of depends so that perl>=5.8.3 is preferred over p5-Test-Harness.
    the old way:
    
      {p5-Test-Harness-[0-9]*,perl{,-thread}>=5.8.3}:../../devel/p5-Test-Harness
    
    is normally ok because if either is installed the dependency is
    satisfied. however, in a bulk build, every package which uses this
    DEPENDS is automatically failed because p5-Test-Harness is preferred
    but it conflicts with perl>=5.8.3 (they both install bin/prove and
    prove.1).
    
    this way, perl is preferred and the dependency should be satisfied
    correctly in bulk builds.

--- devel/p5-Test-Harness/depends.mk
+++ devel/p5-Test-Harness/depends.mk
@@ -1,5 +1,5 @@
-# $NetBSD: depends.mk,v 1.1 2004/05/08 04:05:58 grant Exp $
+# $NetBSD: depends.mk,v 1.2 2004/06/12 04:01:45 grant Exp $
 
 # perl58>=5.8.3 includes Test::Harness, so allow it to satisfy this
 # dependency.
-DEPENDS+=	{p5-Test-Harness-[0-9]*,perl{,-thread}>=5.8.3}:../../devel/p5-Test-Harness
+DEPENDS+=	{perl{,-thread}>=5.8.3,p5-Test-Harness-[0-9]*}:../../devel/p5-Test-Harness
