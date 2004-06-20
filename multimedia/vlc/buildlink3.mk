7c9c2f1d0aab72cd14cbf14e480607a1f9afc6df
xtraeme <xtraeme@pkgsrc.org>
2004-06-20 18:43:26 +0000

    Update multimedia/vlc to 0.7.2.
    Changes between 0.7.1 and 0.7.2:
    --------------------------------
    
    Core support:
     * Bookmarks feature for easier seeking/access inside medias.
     * Support for video output embedded in interfaces.
     * Improved HTTP daemon.
     * Saved playlists now remember VLC-specific options.
    
    Codecs:
     * New Continuous Media Markup Language (CMML) codec.
       (http://www.annodex.net/overview.html)
     * New H.261 video decoder using openmash.
     * H264 encoder, demuxer and packetizer.
     * Packetizer interfaces between demux and codec when needed
       (allows using ffmpeg plugin to decode MPEG streams and better aac decoding).
     * Support for Theora alpha3 (both decoding and encoding).
    
    Input:
     * --start-time <sec> and --stop-time <sec> to start and stop playing a
       file at the specified amount of seconds. Only works with a few
       fileformats (avi, mov, mkv, mp4 )
     * Improved directory access module.
     * New "file-cat" option to play truncated movies.
     * Better handling of meta info (title, author, description, etc...).
     * New options to pass meta info to the input.
     * It is now possible to stream programs from a DVB-S/C/T stream
       (satellite, cable, or digital terestrial television)
    
    Demux:
     * Annodex (http://www.annodex.net) support.
     * mmsh streaming fixes.
     * Fixed infinite loop in the AVI demux on broken/incomplete files.
    
    Subtitles:
     * Subviewer and subviewer v2 subtitles support.
     * Ability to choose autodetected subtitles path.
     * Subtitles delay can be changed in real time with hotkeys.
    
    Stream output:
     * Improved session announcement system.
     * Minimize threads usage by default.
     * Added faster than realtime stream output (limited by CPU) for file output.
     * Improved MOV/MP4 muxer.
     * Improved MPEG TS muxer.
     * Improved transrater.
     * Meta info options used by the muxers.
     * New configuration system.
     * Better audio channels downmixing when transcoding.
    
    VideoLAN manager:
     * New videolan (media) manager (vlm): a little manager designed to launch
       and manage multiple streams from within one instance of VLC.
     * Supports live streams and VoD.
     * Supports scheduling.
     * Telnet interface for vlm.
     * HTTP interface for vlm.
    
    Interfaces:
     * Skins II (Windows and Linux only)
        - Ability to embed video output.
        - Support for bitmap fonts.
        - Lots of improvements.
     * wxWindows (default Windows and Linux interface)
        - New design and set of icons.
        - Ability to embed video output.
        - Support for hotkeys.
        - Support for bookmarks.
     * Mac OS X
        - Support for 'groups' and playlist item properties.
        - Better hotkeys handling.
    
    Mac OS X port:
     * OpenGL video output is now the default when available.
     * Added FAAC encoder (mp4a).
     * Audio output fix to work with multiple streams on a HAL device.
     * Possible fix for conflict with CodeTek VirtualDesktop (untested).
    
    Win32 port:
     * DirectShow input plugin should work with more devices.
     * Disable monitor power down when watching movies.
     * Improved Windows installer.
    
    Linux port:
     * PowerPC fixes.
    
    Misc:
     * Improvements to the Goom visualisation plugin.
     * Roku HD1000 audio output

--- multimedia/vlc/buildlink3.mk
+++ multimedia/vlc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/04/14 12:11:30 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/06/20 18:43:26 xtraeme Exp $
 #
 # This Makefile fragment is included by packages that use vlc.
 #
@@ -16,24 +16,30 @@ BUILDLINK_DEPENDS+=	vlc
 BUILDLINK_PACKAGES+=		vlc
 BUILDLINK_DEPENDS.vlc?=		vlc>=0.7.1
 BUILDLINK_PKGSRCDIR.vlc?=	../../multimedia/vlc
+.endif # VLC_BUILDLINK3_MK
 
+.include "../../multimedia/libdvdcss/buildlink3.mk"
 .include "../../audio/flac/buildlink3.mk"
+.include "../../audio/lame/buildlink3.mk"
 .include "../../audio/liba52/buildlink3.mk"
+.include "../../audio/libcddb/buildlink3.mk"
+.include "../../audio/libid3tag/buildlink3.mk"
 .include "../../audio/libmad/buildlink3.mk"
 .include "../../audio/libvorbis/buildlink3.mk"
-.include "../../audio/speex/buildlink3.mk"
 .include "../../converters/fribidi/buildlink3.mk"
 .include "../../devel/SDL/buildlink3.mk"
-.include "../../graphics/freetype2/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
-.include "../../misc/libdvdcss/buildlink3.mk"
-.include "../../misc/libdvdplay/buildlink3.mk"
-.include "../../misc/libdvdread/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../misc/libcdio/buildlink3.mk"
+.include "../../multimedia/ffmpeg/buildlink3.mk"
+.include "../../multimedia/libdvdplay/buildlink3.mk"
+.include "../../multimedia/libdvbpsi/buildlink3.mk"
+.include "../../multimedia/libdvdread/buildlink3.mk"
+.include "../../multimedia/libmatroska/buildlink3.mk"
 .include "../../multimedia/libmpeg2/buildlink3.mk"
 .include "../../multimedia/libogg/buildlink3.mk"
 .include "../../net/openslp/buildlink3.mk"
+.include "../../sysutils/vcdimager-devel/buildlink3.mk"
 .include "../../x11/wxGTK/buildlink3.mk"
-
-.endif # VLC_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
