8cefe37694db718e3b3b52f4d9db15417925022c
xtraeme <xtraeme@pkgsrc.org>
2004-06-20 21:46:36 +0000

    As discussed in tech-pkg, remove X11/Xpm support in graphics/gd. While I'm here, s/-lpthread/${PTHREAD_LDFLAGS}/.
    Bump PKGREVISION.

--- graphics/gd/buildlink3.mk
+++ graphics/gd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2004/03/18 09:12:11 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2004/06/20 21:46:36 xtraeme Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GD_BUILDLINK3_MK:=	${GD_BUILDLINK3_MK}+
@@ -11,17 +11,14 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngd}
 BUILDLINK_PACKAGES+=	gd
 
 .if !empty(GD_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gd+=		gd>=2.0.15
+BUILDLINK_DEPENDS.gd+=		gd>=2.0.15nb1
 BUILDLINK_PKGSRCDIR.gd?=	../../graphics/gd
 .endif	# GD_BUILDLINK3_MK
-
-USE_X11=	yes
 
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../graphics/jpeg/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
-.include "../../graphics/xpm/buildlink3.mk"
 
 .include "../../mk/pthread.buildlink3.mk"
 
