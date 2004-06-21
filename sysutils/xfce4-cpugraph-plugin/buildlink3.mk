1653e6a305632e10607cdc346ad79adea1a990ea
martti <martti@pkgsrc.org>
2004-06-21 06:06:11 +0000

    CPU load plugin for XFce4.

---
+++ sysutils/xfce4-cpugraph-plugin/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/06/21 06:06:11 martti Exp $
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+XFCE4_CPUGRAPH_PLUGIN_BUILDLINK3_MK:=	${XFCE4_CPUGRAPH_PLUGIN_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	xfce4-cpugraph-plugin
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-cpugraph-plugin}
+BUILDLINK_PACKAGES+=	xfce4-cpugraph-plugin
+
+.if !empty(XFCE4_CPUGRAPH_PLUGIN_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.xfce4-cpugraph-plugin+=	xfce4-cpugraph-plugin>=0.2.0
+BUILDLINK_PKGSRCDIR.xfce4-cpugraph-plugin?=	../../sysutils/xfce4-cpugraph-plugin
+.endif	# XFCE4_CPUGRAPH_PLUGIN_BUILDLINK3_MK
+
+.include "../../x11/xfce4-panel/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
