72321bbe0e8715ffd8c5b6164b0eab5870258d43
martti <martti@pkgsrc.org>
2004-06-21 06:08:34 +0000

    Keyboard plugin for XFce4.

---
+++ sysutils/xfce4-xkb-plugin/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/06/21 06:08:34 martti Exp $
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+XFCE4_XKB_PLUGIN_BUILDLINK3_MK:=	${XFCE4_XKB_PLUGIN_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	xfce4-xkb-plugin
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-xkb-plugin}
+BUILDLINK_PACKAGES+=	xfce4-xkb-plugin
+
+.if !empty(XFCE4_XKB_PLUGIN_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.xfce4-xkb-plugin+=	xfce4-xkb-plugin>=0.3.1
+BUILDLINK_PKGSRCDIR.xfce4-xkb-plugin?=	../../sysutils/xfce4-xkb-plugin
+.endif	# XFCE4_XKB_PLUGIN_BUILDLINK3_MK
+
+.include "../../x11/xfce4-panel/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
