b170a46b54b02cb846b74d44e3b64f766497b7a5
drochner <drochner@pkgsrc.org>
2004-06-22 18:38:46 +0000

    add a non-threaded libtcl.so, for embedding into non-threaded applications

---
+++ lang/libtcl-nothread/buildlink3.mk
@@ -0,0 +1,33 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/06/22 18:38:46 drochner Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+TCLNT_BUILDLINK3_MK:=	${TCLNT_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	tclnt
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ntclnt}
+BUILDLINK_PACKAGES+=	tclnt
+
+.if !empty(TCLNT_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.tclnt+=	libtcl-nothread>=8.4.6
+BUILDLINK_PKGSRCDIR.tclnt?=	../../lang/libtcl-nothread
+
+BUILDLINK_FILES.tcl=	bin/tclsh*
+#
+# Make "-ltcl" and "-ltcl8.4" resolve into "-ltcl84", so that we don't
+# need to patch so many Makefiles.
+#
+BUILDLINK_TRANSFORM+=	l:tcl:tcl84-nothread
+BUILDLINK_TRANSFORM+=	l:tcl8.4:tcl84-nothread
+
+TCLCONFIG_SH?=		${BUILDLINK_PREFIX.tcl}/lib/tclConfig-nothread.sh
+BUILD_ENV+=		_TCL_NOTHREAD=yes
+
+.endif	# TCLNT_BUILDLINK3_MK
+
+_TCL_NOTHREAD=		yes
+.include "../../lang/tcl/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
