2b75758ac582f37378a090ff570ee8004a45ed5f
kristerw <kristerw@pkgsrc.org>
2004-06-26 17:37:14 +0000

    Correct path to multimedia/smpeg.

--- multimedia/smpeg/buildlink3.mk
+++ multimedia/smpeg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/06/26 09:16:10 grant Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/06/26 17:37:14 kristerw Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 SPMEG_BUILDLINK3_MK:=	${SPMEG_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	smpeg
 
 .if !empty(SPMEG_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.smpeg+=	smpeg>=0.4.4nb3
-BUILDLINK_PKGSRCDIR.smpeg?=	../../devel/smpeg
+BUILDLINK_PKGSRCDIR.smpeg?=	../../multimedia/smpeg
 .endif	# SMPEG_BUILDLINK3_MK
 
 .include "../../devel/SDL/buildlink3.mk"
