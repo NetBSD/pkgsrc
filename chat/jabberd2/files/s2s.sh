1399f729f690323716b407c1992946f50ddac9ff
abs <abs@pkgsrc.org>
2004-06-26 11:21:46 +0000

    Update jabberd2 to jabberd-2.0s2nb1 Since the various jabber daemons need to write their pid files as the jabberd user, ensure a jabberd owned /var/run/jabberd is created by rc.d files

--- chat/jabberd2/files/s2s.sh
+++ chat/jabberd2/files/s2s.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: s2s.sh,v 1.1.1.1 2004/03/28 19:44:07 xtraeme Exp $
+# $NetBSD: s2s.sh,v 1.2 2004/06/26 11:21:46 abs Exp $
 #
 # PROVIDE: s2s
 # REQUIRE: DAEMON c2s
@@ -18,6 +18,13 @@ command_args="2>&1 > /dev/null &"
 s2s_user="@JABBERD_USER@"
 pidfile="@JABBERD_PIDDIR@/${name}.pid"
 stop_postcmd="remove_pidfile"
+start_precmd="ensure_piddir"
+
+ensure_piddir()
+{
+	mkdir -p @JABBERD_PIDDIR@
+	chown @JABBERD_USER@ @JABBERD_PIDDIR@
+}
 
 remove_pidfile()
 {
