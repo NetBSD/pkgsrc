a11fbb61d67a15715c8167e46787f001178190bb
recht <recht>
2004-06-26 20:18:50 +0000

    update to icu-3.0
    major changes:
    
    ICU 3.0 includes the latest bug fixes, locale/charset updates, and
    performance/build/porting enhancements.
    - Collation
      Collation data is in a separate data tree, allowing for easier
      modularization and maintenance.
      getFunctionalEquivalent API allows for better caching and UI support.
    - Unicode 4.0.1
      ICU is updated to the latest version of Unicode standard, which had
      significant property changes.
    - CLDR 1.1
      Updates to CLDR 1.1, with many updates to locale data, and special
      emphasis on collation data.
    - Formatting
      As an aid to migration of traditional C (stdio) and C++ (iostream)
      formatting, the POSIX-like input/output library, icuio, is officially
      supported.
      Significant digits now supported in DecimalFormat, for general use and
      %g support.
    - RFC822 time zone format support in DateFormat for compatibility.
    - Currency formatting/parsing improvements
      Allows parsing multiple currencies with one formatter, without knowing the
      currency in advance. Much cleaner design allowing extensibility to other
      measurement units in the future.
    - Regular expressions (C)
      The regular expressions framework now features a C API, instead of just C++.
    - Locales
      Locale canonicalization spec defined and implemented. Provides
      interoperability with POSIX and .NET locale IDs, more RFC 3066 support.
    - Layout engine
      Layout engine now supports using different canonically-equivalent Unicode
      forms of the same text: e.g. a + ´ or á. This is especially important for
      non-Latin scripts.
    - Build Environment
      ICU can now build its data library much faster on most platforms.
    
    For a complete list see:
    http://oss.software.ibm.com/cvs/icu/~checkout~/icu/readme.html?tag=release-3-0

--- textproc/icu/buildlink3.mk
+++ textproc/icu/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/04/04 16:58:16 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2004/06/26 20:18:50 recht Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 ICU_BUILDLINK3_MK:=	${ICU_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nicu}
 BUILDLINK_PACKAGES+=	icu
 
 .if !empty(ICU_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.icu+=	icu>=2.8
+BUILDLINK_DEPENDS.icu+=	icu>=3.0
 BUILDLINK_PKGSRCDIR.icu?=	../../textproc/icu
 .endif	# ICU_BUILDLINK3_MK
 
