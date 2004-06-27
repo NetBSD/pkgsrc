a90d2e5119e16470255aa30d223019537881b657
jmmv <jmmv@pkgsrc.org>
2004-06-27 17:27:12 +0000

    Update to 0.31.  While here, make this package also install the xml-i18n-tools utility to obsolete the standalone xml-i18n-tools package.
    Changes since 0.30:
    * Usage of [no-xml] option in AC_PROG_INTLTOOL to avoid requiring
      XML::Parser Perl module. (Rodney Dawes)
    * Support for C#. (Rodney)
    * Support for running intltool-update without autoconf, or with
      po/Makevars file (as used in gettext). (Danilo Segan)
    * New XML merging (compatible with pre-0.27.2 intltool) -- bug
      #130802 -- entity decoding and encoding is not done anymore for
      XML. (Danilo)
    * Multiple-output XML merging now produces simpler files, and sets
      "xml:lang" only when translation is used -- bug #46650. (Danilo)
    * Support for passing comments to translators from XML and GConf
      schema files. (Danilo)
    * Many bug fixes (Rodney, Danilo, Callum McKenzie)

--- textproc/intltool/buildlink3.mk
+++ textproc/intltool/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/03/18 09:12:15 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/06/27 17:27:12 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 INTLTOOL_BUILDLINK3_MK:=	${INTLTOOL_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nintltool}
 BUILDLINK_PACKAGES+=	intltool
 
 .if !empty(INTLTOOL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.intltool+=	intltool>=0.30
+BUILDLINK_DEPENDS.intltool+=	intltool>=0.31
 BUILDLINK_PKGSRCDIR.intltool?=	../../textproc/intltool
 BUILDLINK_DEPMETHOD.intltool?=	build
 .endif	# INTLTOOL_BUILDLINK3_MK
