6cdeadf66c02dc4532b26e296f5659e7d346ed4b
shannonjr <shannonjr>
2004-06-29 18:01:24 +0000

    This is the gcc 3.4 compiler suite.

---
+++ lang/gcc34/buildlink3.mk
@@ -0,0 +1,42 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/06/29 18:01:24 shannonjr Exp $
+
+BUILDLINK_DEPTH:=       ${BUILDLINK_DEPTH}+
+GCC34_BUILDLINK3_MK:=  ${GCC34_BUILDLINK3_MK}+
+BUILDLINK_PREFIX.gcc34:=${LOCALBASE}/gcc34
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=     gcc34
+.endif
+
+BUILDLINK_PACKAGES:=    ${BUILDLINK_PACKAGES:Ngcc34}
+BUILDLINK_PACKAGES+=    gcc34
+
+.if !empty(GCC34_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.gcc34+=	gcc34>=3.4.1
+BUILDLINK_PKGSRCDIR.gcc34?=	../../lang/gcc34
+_GNAT1!=${BUILDLINK_PREFIX.gcc34}/bin/gcc -print-prog-name=gnat1
+.  if exists(${_GNAT1})
+BUILDLINK_ENV+=	ADAC=${BUILDLINK_PREFIX.gcc34}/bin/gcc
+.  endif
+_GCC_ARCHDIR!=	${DIRNAME} `${BUILDLINK_PREFIX.gcc34}/bin/gcc --print-libgcc-file-name`
+.  if empty(_GCC_ARCHDIR:M*not_found*)
+BUILDLINK_LIBDIRS.gcc34+=	lib ${_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc34}\///}/
+.    if exists(${_GNAT1})
+BUILDLINK_LIBDIRS.gcc34+=	${_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc34}\///}/adalib
+.    endif
+BUILDLINK_INCDIRS.gcc34+=	include ${_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc34}\///}/include
+.  endif
+.endif  # GCC34_BUILDLINK3_MK
+
+BUILDLINK_FILES_CMD.gcc34=	\
+	(cd  ${BUILDLINK_PREFIX.gcc34} &&	\
+	${FIND} bin libexec lib \( -type file -o -type link \) -print)
+BUILDLINK_TRANSFORM.gcc34=	-e s:\buildlink:buildlink/gcc34:
+
+# Packages that link against shared libraries need a full dependency.
+BUILDLINK_DEPMETHOD.gcc34?=	full
+
+.include "../../mk/pthread.buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
