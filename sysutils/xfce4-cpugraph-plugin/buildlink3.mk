4ce537ae1f9a1e8048756ced38dc1adf822bcbf3
martti <martti>
2004-06-30 12:33:53 +0000

    Updated xfce4-cpugraph-plugin to 0.2.2
    * Bug fixes

--- sysutils/xfce4-cpugraph-plugin/buildlink3.mk
+++ sysutils/xfce4-cpugraph-plugin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/06/21 06:06:11 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/06/30 12:33:53 martti Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_CPUGRAPH_PLUGIN_BUILDLINK3_MK:=	${XFCE4_CPUGRAPH_PLUGIN_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-cpugraph-plugin}
 BUILDLINK_PACKAGES+=	xfce4-cpugraph-plugin
 
 .if !empty(XFCE4_CPUGRAPH_PLUGIN_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.xfce4-cpugraph-plugin+=	xfce4-cpugraph-plugin>=0.2.0
+BUILDLINK_DEPENDS.xfce4-cpugraph-plugin+=	xfce4-cpugraph-plugin>=0.2.2
 BUILDLINK_PKGSRCDIR.xfce4-cpugraph-plugin?=	../../sysutils/xfce4-cpugraph-plugin
 .endif	# XFCE4_CPUGRAPH_PLUGIN_BUILDLINK3_MK
 
