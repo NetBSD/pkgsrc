0c1eb7608899837d84c22bd962753b029546bddb
shannonjr <shannonjr@pkgsrc.org>
2004-06-30 16:18:03 +0000

    Update to 2.15, the latest release of GNU binutils.

--- devel/binutils/buildlink3.mk
+++ devel/binutils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2004/03/10 17:57:14 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2004/06/30 16:18:03 shannonjr Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 BINUTILS_BUILDLINK3_MK:=	${BINUTILS_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nbinutils}
 BUILDLINK_PACKAGES+=	binutils
 
 .if !empty(BINUTILS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.binutils+=		binutils>=2.14.0
+BUILDLINK_DEPENDS.binutils+=		binutils>=2.15.0
 BUILDLINK_PKGSRCDIR.binutils?=		../../devel/binutils
 BUILDLINK_DEPMETHOD.binutils?=		build
 .endif	# BINUTILS_BUILDLINK3_MK
