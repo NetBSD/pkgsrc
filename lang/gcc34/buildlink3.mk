05540213a296c818c0c3654294d94946b4025174
shannonjr <shannonjr>
2004-07-01 08:18:07 +0000

    Modified buildlink3.mk to check for the existance of ${BUILDLINK_PREFIX.gcc34}/bin/gcc before calling it.

--- lang/gcc34/buildlink3.mk
+++ lang/gcc34/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/06/29 18:01:24 shannonjr Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/07/01 08:18:07 shannonjr Exp $
 
 BUILDLINK_DEPTH:=       ${BUILDLINK_DEPTH}+
 GCC34_BUILDLINK3_MK:=  ${GCC34_BUILDLINK3_MK}+
@@ -14,17 +14,19 @@ BUILDLINK_PACKAGES+=    gcc34
 .if !empty(GCC34_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gcc34+=	gcc34>=3.4.1
 BUILDLINK_PKGSRCDIR.gcc34?=	../../lang/gcc34
+.  if exists(${BUILDLINK_PREFIX.gcc34}/bin/gcc)
 _GNAT1!=${BUILDLINK_PREFIX.gcc34}/bin/gcc -print-prog-name=gnat1
-.  if exists(${_GNAT1})
-BUILDLINK_ENV+=	ADAC=${BUILDLINK_PREFIX.gcc34}/bin/gcc
-.  endif
-_GCC_ARCHDIR!=	${DIRNAME} `${BUILDLINK_PREFIX.gcc34}/bin/gcc --print-libgcc-file-name`
-.  if empty(_GCC_ARCHDIR:M*not_found*)
-BUILDLINK_LIBDIRS.gcc34+=	lib ${_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc34}\///}/
 .    if exists(${_GNAT1})
-BUILDLINK_LIBDIRS.gcc34+=	${_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc34}\///}/adalib
+BUILDLINK_ENV+=	ADAC=${BUILDLINK_PREFIX.gcc34}/bin/gcc
 .    endif
+_GCC_ARCHDIR!=	${DIRNAME} `${BUILDLINK_PREFIX.gcc34}/bin/gcc --print-libgcc-file-name`
+.    if empty(_GCC_ARCHDIR:M*not_found*)
+BUILDLINK_LIBDIRS.gcc34+=	lib ${_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc34}\///}/
+.      if exists(${_GNAT1})
+BUILDLINK_LIBDIRS.gcc34+=	${_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc34}\///}/adalib
+.      endif
 BUILDLINK_INCDIRS.gcc34+=	include ${_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc34}\///}/include
+.    endif
 .  endif
 .endif  # GCC34_BUILDLINK3_MK
 
