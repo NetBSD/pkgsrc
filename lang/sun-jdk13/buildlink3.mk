5cb58103d84c9004ea1e6f8b186c70f98df22bcb
wiz <wiz@pkgsrc.org>
2004-07-02 12:51:57 +0000

    Add appropriate CPPFLAGS and PASSTHRU_DIRS variables so that packages depending on this one can find the header files. Noted broken by snj.

--- lang/sun-jdk13/buildlink3.mk
+++ lang/sun-jdk13/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/05/05 17:22:51 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/07/02 12:51:57 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SUN_JDK13_BUILDLINK3_MK:=	${SUN_JDK13_BUILDLINK3_MK}+
@@ -15,6 +15,9 @@ BUILDLINK_PACKAGES+=	sun-jdk13
 BUILDLINK_DEPENDS.sun-jdk13+=	sun-jdk13-[0-9]*
 BUILDLINK_PKGSRCDIR.sun-jdk13?=	../../lang/sun-jdk13
 BUILDLINK_DEPMETHOD.sun-jdk13?= build
+
+BUILDLINK_PASSTHRU_DIRS+=	${BUILDLINK_JAVA_PREFIX.sun-jre13}
+
 BUILDLINK_CPPFLAGS.sun-jdk13=						\
 	-I${BUILDLINK_JAVA_PREFIX.sun-jre13}/include			\
 	-I${BUILDLINK_JAVA_PREFIX.sun-jre13}/include/linux
