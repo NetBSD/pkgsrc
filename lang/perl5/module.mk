b1db96e288665ef1f6c262d1b96fdd909fca29be
wiz <wiz@pkgsrc.org>
2004-07-03 14:13:06 +0000

    Do not unset USE_GCC3, just complain if building with GCC on AIX. Okayed by grant.

--- lang/perl5/module.mk
+++ lang/perl5/module.mk
@@ -1,4 +1,4 @@
-# $NetBSD: module.mk,v 1.35 2004/06/09 17:53:04 xtraeme Exp $
+# $NetBSD: module.mk,v 1.36 2004/07/03 14:13:06 wiz Exp $
 #
 # This Makefile fragment is intended to be included by packages that build
 # and install perl5 modules.
@@ -51,11 +51,12 @@ PERL5_CONFIGURE_DIRS?=	${CONFIGURE_DIRS}
 #
 MAKE_PARAMS+=		INSTALLDIRS=site
 
+.include "../../mk/compiler.mk"
+
 .if ${OPSYS} == "AIX"
-# Perl doesn't like building with gcc on AIX
-CC=/usr/bin/cc
-MAKE_PARAMS+=  CC="${CC}"
-.undef USE_GCC3
+.if !empty(CC_VERSION:Mgcc*)
+BROKEN=		Perl does not like building with gcc on AIX, please use a different compiler
+.endif
 .endif
 
 MAKE_ENV+=	LC_ALL=C
