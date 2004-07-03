0783036b3e06e4872116917076f33fee7137c41a
grant <grant@pkgsrc.org>
2004-07-03 11:25:49 +0000

    gcc 3.3.4 is in pkgsrc now.

--- mk/compiler/gcc.mk
+++ mk/compiler/gcc.mk
@@ -1,4 +1,4 @@
-# $NetBSD: gcc.mk,v 1.67 2004/05/17 05:33:08 jlam Exp $
+# $NetBSD: gcc.mk,v 1.68 2004/07/03 11:25:49 grant Exp $
 
 .if !defined(COMPILER_GCC_MK)
 COMPILER_GCC_MK=	defined
@@ -12,7 +12,7 @@ GCC_REQD+=	2.8.0
 # _GCC_DIST_VERSION is the highest version of GCC installed by the pkgsrc
 # without the PKGREVISIONs.
 #
-_GCC_DIST_VERSION=	3.3.3
+_GCC_DIST_VERSION=	3.3.4
 
 # _GCC2_PATTERNS matches N s.t. N <= 2.95.3.
 _GCC2_PATTERNS=	[0-1].* 2.[0-9] 2.[0-9].* 2.[1-8][0-9] 2.[1-8][0-9].*	\
