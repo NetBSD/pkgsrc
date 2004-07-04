8a290dc4c40b5e6a43b71250b335b23fb6c5a446
mrg <mrg@pkgsrc.org>
2004-07-04 10:25:42 +0000

    this has been sitting in my tree for over 6 months... update a minor revision to 0.2.2.  from the web page:
    This release is only to fix a few bugs with sound in swf_play and a
    few problems with the GdkPixbuf plugin.

--- graphics/swfdec/buildlink3.mk
+++ graphics/swfdec/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/03/18 09:12:12 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/07/04 10:25:42 mrg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 SWFDEC_BUILDLINK3_MK:=	${SWFDEC_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nswfdec}
 BUILDLINK_PACKAGES+=	swfdec
 
 .if !empty(SWFDEC_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.swfdec+=	swfdec>=0.2.1nb7
+BUILDLINK_DEPENDS.swfdec+=	swfdec>=0.2.2
 BUILDLINK_PKGSRCDIR.swfdec?=	../../graphics/swfdec
 .endif	# SWFDEC_BUILDLINK3_MK
 
