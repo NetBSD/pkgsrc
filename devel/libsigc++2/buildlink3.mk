56f5f79cf8c416aa7973d7d1c327da3a00b06f73
drochner <drochner@pkgsrc.org>
2004-07-04 13:47:54 +0000

    Import version 2 of the Type-safe callback system for C++ programs. This can well coexist with the previous generations (1.0, 1.2).

---
+++ devel/libsigc++2/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/07/04 13:47:54 drochner Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBSIGC++_BUILDLINK3_MK:=	${LIBSIGC++_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libsigcxx
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibsigcxx}
+BUILDLINK_PACKAGES+=	libsigcxx
+
+.if !empty(LIBSIGC++_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libsigcxx+=	libsigc++2>=2.0.3
+BUILDLINK_PKGSRCDIR.libsigcxx?=	../../devel/libsigc++2
+.endif	# LIBSIGC++_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
