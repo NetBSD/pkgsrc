39e2d2961ea40611fb9327d9ce431cfb6f6770f3
tv <tv@pkgsrc.org>
2004-07-04 02:55:42 +0000

    Rather than using .error, set BROKEN if an unknown compiler is encountered.

--- devel/boost/toolset.mk
+++ devel/boost/toolset.mk
@@ -1,4 +1,4 @@
-# $NetBSD: toolset.mk,v 1.1 2004/06/06 23:51:36 tv Exp $
+# $NetBSD: toolset.mk,v 1.2 2004/07/04 02:55:42 tv Exp $
 # Translation from PKGSRC_COMPILER to BOOST_TOOLSET.
 
 .include "../../mk/compiler.mk"
@@ -10,5 +10,5 @@ BOOST_TOOLSET=		mipspro
 .elif !empty(PKGSRC_COMPILER:Msunpro)
 BOOST_TOOLSET=		sunpro
 .else
-.error unknown BOOST_TOOLSET
+BROKEN+=		"unknown compiler ${PKGSRC_COMPILER} for Boost"
 .endif
