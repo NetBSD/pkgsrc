19eb3924592617a25af19d15d368142dac9892c3
drochner <drochner@pkgsrc.org>
2004-07-06 20:44:05 +0000

    import libgnomeuimm20-2.0.0 which is a component of the gtkmm-2.0/2.2 family of C++ bindings for gnome libraries fortunately, these families can coexist

---
+++ devel/libgnomeuimm20/buildlink3.mk
@@ -0,0 +1,34 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/07/06 20:44:05 drochner Exp $
+# XXX
+# XXX This file was created automatically using createbuildlink-3.4.
+# XXX After this file as been verified as correct, the comment lines
+# XXX beginning with "XXX" should be removed.  Please do not commit
+# XXX unverified buildlink[23].mk files.
+# XXX
+# XXX Packages that only install static libraries or headers should
+# XXX include the following line:
+# XXX
+# XXX	BUILDLINK_DEPMETHOD.libgnomeuimm20?=	build
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBGNOMEUIMM20_BUILDLINK3_MK:=	${LIBGNOMEUIMM20_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libgnomeuimm20
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgnomeuimm20}
+BUILDLINK_PACKAGES+=	libgnomeuimm20
+
+.if !empty(LIBGNOMEUIMM20_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libgnomeuimm20+=	libgnomeuimm20>=2.0.0
+BUILDLINK_PKGSRCDIR.libgnomeuimm20?=	../../devel/libgnomeuimm20
+.endif	# LIBGNOMEUIMM20_BUILDLINK3_MK
+
+.include "../../devel/libgnomeui/buildlink3.mk"
+.include "../../devel/libgnomemm20/buildlink3.mk"
+.include "../../graphics/libgnomecanvasmm20/buildlink3.mk"
+.include "../../devel/gconfmm/buildlink3.mk"
+.include "../../devel/libglademm20/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
