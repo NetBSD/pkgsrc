90ee4e90da030b6c0c245e138c6b31c9105c6b6b
wiz <wiz@pkgsrc.org>
2004-07-07 08:45:39 +0000

    Set BUILDLINK_LIBDIRS.db?, as mail/exim expects.

--- databases/db3/buildlink3.mk
+++ databases/db3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2004/05/13 20:23:21 kristerw Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2004/07/07 08:45:39 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 DB3_BUILDLINK3_MK:=	${DB3_BUILDLINK3_MK}+
@@ -16,6 +16,7 @@ BUILDLINK_PACKAGES+=	db3
 BUILDLINK_DEPENDS.db3+=		db3>=2.9.2
 BUILDLINK_PKGSRCDIR.db3?=	../../databases/db3
 BUILDLINK_INCDIRS.db3?=		include/db3
+BUILDLINK_LIBDIRS.db3?=		lib
 BUILDLINK_TRANSFORM+=		l:db-3:db3
 USE_DB185?=			yes
 .  if !empty(USE_DB185:M[yY][eE][sS])
