c0cd984d8544ae7484cdc4e943a5ebbdc5294a4a
recht <recht@pkgsrc.org>
2004-07-13 14:45:33 +0000

    Follow devel/boehm-gc's lead and also correctly define ELFSIZE for NetBSD here. Bump BUILDLINK_DEPENDS to 1.0nb1 for the ELFSIZE fix.

--- lang/mono/buildlink3.mk
+++ lang/mono/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2004/07/03 23:02:50 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2004/07/13 14:45:33 recht Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 MONO_BUILDLINK3_MK:=	${MONO_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmono}
 BUILDLINK_PACKAGES+=	mono
 
 .if !empty(MONO_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.mono+=	mono>=1.0
+BUILDLINK_DEPENDS.mono+=	mono>=1.0nb1
 BUILDLINK_PKGSRCDIR.mono?=	../../lang/mono
 .endif	# MONO_BUILDLINK3_MK
 
