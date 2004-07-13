6c3025b340bac097612938809bbc4f444f4b95e3
kristerw <kristerw@pkgsrc.org>
2004-07-13 22:01:03 +0000

    Work around brokenness in the bulk build.
    The problem is that perl58>=5.8.3 includes Test::Harness, so we allow
    it to satisfy this dependency.  The default perl in pkgsrc is 5.8.4,
    so no package will depend on devel/p5-Test-Harness (unless the user
    has an old perl installation, or overrides the default).  But the way
    the bulk build tracks dependencies makes it add dependencies on
    devel/p5-Test-Harness anyway...
    
    Work around this for now, by noting that if there is no PERL5 installed,
    and the user has not overridden the default, then we do not need
    to add anything to the DEPENDS at all (since perl 5.8.4 will be
    used from the normal USE_PERL5 mechanism).

--- devel/p5-Test-Harness/depends.mk
+++ devel/p5-Test-Harness/depends.mk
@@ -1,5 +1,20 @@
-# $NetBSD: depends.mk,v 1.2 2004/06/12 04:01:45 grant Exp $
+# $NetBSD: depends.mk,v 1.3 2004/07/13 22:01:03 kristerw Exp $
 
-# perl58>=5.8.3 includes Test::Harness, so allow it to satisfy this
+.include "../../mk/bsd.prefs.mk"
+
+# perl58>=5.8.3 includes Test::Harness, so we allow it to satisfy this
 # dependency.
+#
+# This does however cause problems for the bulk-builds; it adds all
+# packages in the "path"-part of the DEPENDS to the list of required
+# packages, and thus refuses to build the packages depending on
+# this package when it fails...
+#
+# Fix this for now by noting that if there is no PERL5 installed,
+# and PERL5_PKGSRCDIR has not been set, then a sufficiently new
+# perl will be installed, so the DEPENDS line is not needed...
+# This hack should be removed when the bulk-build dependency-
+# tracking has been fixed.
+.if exists(${PERL5}) || defined(${PERL5_PKGSRCDIR})
 DEPENDS+=	{perl{,-thread}>=5.8.3,p5-Test-Harness-[0-9]*}:../../devel/p5-Test-Harness
+.endif
