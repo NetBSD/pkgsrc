2387ef894c869ffd75718cbf421634466f1bdebc
agc <agc>
2004-07-14 11:17:18 +0000

    Pullup ticket 56 to the pkgsrc-2004Q2 branch, requested by Martti Kuparinen.
    Update xfce4 to version 4.0.6 - a bugfix release. Also add various plugins:
    
    	xfce4-weather-plugin
    	xfce4-cpugraph-plugin
    	xfce4-xkb-plugin
    	xfce4-taskbar-plugin
    	xfce4-windowlist-plugin
    
    	Module Name:    pkgsrc
    	Committed By:   martti
    	Date:           Mon Jul 12 08:25:11 UTC 2004
    
    	Modified Files:
    		pkgsrc/audio/xfce4-mixer: PLIST buildlink3.mk distinfo
    		pkgsrc/graphics/xfce4-iconbox: PLIST buildlink3.mk distinfo
    		pkgsrc/meta-pkgs/xfce4: Makefile.common buildlink3.mk
    		pkgsrc/print/xfce4-print: PLIST buildlink3.mk distinfo
    		pkgsrc/sysutils/xfce4-fm: PLIST buildlink3.mk distinfo
    		pkgsrc/sysutils/xfce4-fm-icons: buildlink3.mk distinfo
    		pkgsrc/sysutils/xfce4-systray: buildlink3.mk distinfo
    		pkgsrc/wm/xfce4-wm: PLIST buildlink3.mk distinfo
    		pkgsrc/wm/xfce4-wm-themes: buildlink3.mk distinfo
    		pkgsrc/x11/libxfce4gui: PLIST buildlink3.mk distinfo
    		pkgsrc/x11/libxfce4mcs: buildlink3.mk distinfo
    		pkgsrc/x11/libxfce4util: buildlink3.mk distinfo
    		pkgsrc/x11/xfce4-desktop: PLIST buildlink3.mk distinfo
    		pkgsrc/x11/xfce4-gtk2-engines: Makefile buildlink3.mk distinfo
    		pkgsrc/x11/xfce4-mcs-manager: PLIST buildlink3.mk distinfo
    		pkgsrc/x11/xfce4-mcs-plugins: PLIST buildlink3.mk distinfo
    		pkgsrc/x11/xfce4-panel: PLIST buildlink3.mk distinfo
    		pkgsrc/x11/xfce4-themes: buildlink3.mk distinfo
    		pkgsrc/x11/xfce4-toys: PLIST buildlink3.mk distinfo
    		pkgsrc/x11/xfce4-trigger-launcher: PLIST buildlink3.mk distinfo
    		pkgsrc/x11/xfce4-utils: PLIST buildlink3.mk distinfo
    
    	Log Message:
    	Updated XFce to 4.0.6
    
    	As usual, this is a maintenance release, aimed at bug-fixing;
    	no new features are being added to the 4.0 branch.
    
    	The main purpose of this release is compatibility with recent GTK+
    	releases along with other fixes.

--- x11/xfce4-mcs-manager/buildlink3.mk
+++ x11/xfce4-mcs-manager/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/04/16 16:12:00 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.3.2.1 2004/07/14 11:17:20 agc Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_MCS_MANAGER_BUILDLINK3_MK:=	${XFCE4_MCS_MANAGER_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-mcs-manager}
 BUILDLINK_PACKAGES+=	xfce4-mcs-manager
 
 .if !empty(XFCE4_MCS_MANAGER_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.xfce4-mcs-manager+=	xfce4-mcs-manager>=4.0.5
+BUILDLINK_DEPENDS.xfce4-mcs-manager+=	xfce4-mcs-manager>=4.0.6
 BUILDLINK_PKGSRCDIR.xfce4-mcs-manager?=	../../x11/xfce4-mcs-manager
 .endif	# XFCE4_MCS_MANAGER_BUILDLINK3_MK
 
--- x11/xfce4-trigger-launcher/buildlink3.mk
+++ x11/xfce4-trigger-launcher/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/04/16 16:12:01 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.3.2.1 2004/07/14 11:17:20 agc Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_TRIGGER_LAUNCHER_BUILDLINK3_MK:=	${XFCE4_TRIGGER_LAUNCHER_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-trigger-launcher}
 BUILDLINK_PACKAGES+=	xfce4-trigger-launcher
 
 .if !empty(XFCE4_TRIGGER_LAUNCHER_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.xfce4-trigger-launcher+=	xfce4-trigger-launcher>=4.0.5
+BUILDLINK_DEPENDS.xfce4-trigger-launcher+=	xfce4-trigger-launcher>=4.0.6
 BUILDLINK_PKGSRCDIR.xfce4-trigger-launcher?=	../../x11/xfce4-trigger-launcher
 .endif	# XFCE4_TRIGGER_LAUNCHER_BUILDLINK3_MK
 
