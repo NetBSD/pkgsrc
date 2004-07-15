c9213142867712d939bee5b1a82828bc53e4c540
jlam <jlam@pkgsrc.org>
2004-07-15 08:29:05 +0000

    Print warnings to stderr, not stdout.  This matches rc.subr behaviour.

--- mail/courier-imap/files/courierpops.sh
+++ mail/courier-imap/files/courierpops.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: courierpops.sh,v 1.9 2004/07/14 20:07:16 jlam Exp $
+# $NetBSD: courierpops.sh,v 1.10 2004/07/15 08:29:05 jlam Exp $
 #
 # Courier POP3/SSL services daemon
 #
@@ -43,7 +43,7 @@ courier_doit()
 		done
 		for f in $required_files; do
 			if [ ! -r "$f" ]; then
-				@ECHO@ "$0: WARNING: $f is not readable"
+				@ECHO@ 1>&2 "$0: WARNING: $f is not readable"
 				if [ -z $rc_force ]; then
 					return 1
 				fi
