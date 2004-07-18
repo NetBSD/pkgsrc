3a5d054eb9ace178455a79f1b5248416c1084343
markd <markd@pkgsrc.org>
2004-07-18 12:59:02 +0000

    Redo original bind version test in way that doesnt result in a make warning about null output from shell.  We don't want to get a version number for bind versions older that 9 as that complicates the later tests.

--- net/bind9/builtin.mk
+++ net/bind9/builtin.mk
@@ -1,11 +1,11 @@
-# $NetBSD: builtin.mk,v 1.2 2004/07/18 11:22:13 tron Exp $
+# $NetBSD: builtin.mk,v 1.3 2004/07/18 12:59:02 markd Exp $
 
 .if !defined(IS_BUILTIN.bind)
 IS_BUILTIN.bind=        no
 .  if exists(/usr/sbin/named)
-_BIND_VERSION!=/usr/sbin/named -v | ${HEAD} -1 | ${CUT} -d ' ' -f 2
+_BIND_VERSION!=${ECHO} 'vers ' && /usr/sbin/named -v | ${SED} -n 's/^BIND //p'
 .  endif
-.  if defined(_BIND_VERSION) && !empty(_BIND_VERSION)
+.  if defined(_BIND_VERSION) && !empty(_BIND_VERSION:Nvers)
 IS_BUILTIN.bind=        yes
 BUILTIN_PKG.bind=       bind-${_BIND_VERSION}
 BUILDLINK_VARS+=        BUILTIN_PKG.bind
