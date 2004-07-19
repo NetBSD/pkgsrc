829511c8db3fd72dc4ee14ab8d624497f8aee2ba
jlam <jlam>
2004-07-19 20:00:15 +0000

    Stop the proliferation of <PKG>_USE_<FOO> yes/no variables in the mail/postfix package -- create a new option "POSTFIX_OPTIONS" that is a list of optional add-ons that will be built into Postfix.  Currently, it accepts any of "inet6 ldap mysql mysql4 prce sasl tls".  Move the existing POSTFIX_USE_* variables into bsd.pkg.obsolete.mk.

--- mk/bsd.pkg.obsolete.mk
+++ mk/bsd.pkg.obsolete.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.pkg.obsolete.mk,v 1.12 2004/06/14 03:16:43 uebayasi Exp $
+# $NetBSD: bsd.pkg.obsolete.mk,v 1.13 2004/07/19 20:00:15 jlam Exp $
 #
 # This file holds make(1) logic to allow obsolete or deprecated variables
 # still to be used.  These may eventually disappear over time as the contents
@@ -34,10 +34,6 @@ PKG_SYSCONFDIR.ssh?=			${SSH_CONF_DIR}
 PKG_SYSCONFDIR.vtun?=			${VTUN_SYSCONFDIR}
 .endif
 
-.if defined(POSTFIX_USE_SASL_AUTH)
-USE_SASL=				YES
-.endif
-
 .if defined(SAMBA_WITH_CUPS)
 USE_CUPS=				YES
 .endif
@@ -52,4 +48,43 @@ ELK_GUI=	xaw
 
 .if defined(PRIV_CONF_DIR)
 PKG_SYSCONFDIR.priv?=	${PRIV_CONF_DIR}
+.endif
+
+.if defined(POSTFIX_USE_INET6) || defined(POSTFIX_USE_TLS) || \
+    defined(POSTFIX_USE_PCRE) || defined(POSTFIX_USE_MYSQL) || \
+    defined(POSTFIX_USE_PGSQL) || defined(POSTFIX_USE_VERP) || \
+    defined(POSTFIX_USE_SASL_AUTH) || defined(USE_SASL) || \
+    defined(USE_SASL2) || defined(USE_OPENLDAP)
+.  if !defined(POSTFIX_OPTIONS)
+.    if defined(POSTFIX_USE_INET6) && !empty(POSTFIX_USE_INET6:M[yY][eE][sS])
+POSTFIX_OPTIONS+=	inet6
+.    endif
+.    if defined(POSTFIX_USE_TLS) && !empty(POSTFIX_USE_TLS:M[yY][eE][sS])
+POSTFIX_OPTIONS+=	tls
+.    endif
+.    if defined(POSTFIX_USE_PCRE) && !empty(POSTFIX_USE_PCRE:M[yY][eE][sS])
+POSTFIX_OPTIONS+=	pcre 
+.    endif
+.    if defined(POSTFIX_USE_MYSQL) && !empty(POSTFIX_USE_MYSQL:M[yY][eE][sS])
+POSTFIX_OPTIONS+=	mysql
+.    endif
+.    if defined(POSTFIX_USE_PGSQL) && !empty(POSTFIX_USE_PGSQL:M[yY][eE][sS])
+POSTFIX_OPTIONS+=	pgsql
+.    endif
+.    if defined(POSTFIX_USE_VERP) && !empty(POSTFIX_USE_VERP:M[yY][eE][sS])
+POSTFIX_OPTIONS+=	verp
+.    endif
+.    if defined(USE_OPENLDAP) && !empty(USE_OPENLDAP:M[yY][eE][sS])
+POSTFIX_OPTIONS+=	ldap
+.    endif
+.    if defined(POSTFIX_USE_SASL_AUTH) && defined(POSTFIX_USE_SASL_AUTH)
+USE_SASL=		YES
+.    endif
+.    if defined(USE_SASL) && !empty(USE_SASL:M[yY][eE][sS])
+POSTFIX_OPTIONS+=	sasl
+.    endif
+.    if defined(USE_SASL2) && !empty(USE_SASL2:M[yY][eE][sS])
+POSTFIX_OPTIONS+=	sasl
+.    endif
+.  endif
 .endif
