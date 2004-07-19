2af4f1d311823a81b94563e40e7bc43f1138f834
markd <markd@pkgsrc.org>
2004-07-19 04:08:13 +0000

    Do package version handling properly.

--- net/bind9/builtin.mk
+++ net/bind9/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.4 2004/07/19 04:07:16 markd Exp $
+# $NetBSD: builtin.mk,v 1.5 2004/07/19 04:08:13 markd Exp $
 
 .if !defined(IS_BUILTIN.bind)
 IS_BUILTIN.bind=	no
@@ -7,10 +7,24 @@ _BIND_VERSION!=${ECHO} 'vers ' && /usr/sbin/named -v | ${SED} -n 's/^BIND //p'
 .  endif
 .  if defined(_BIND_VERSION) && !empty(_BIND_VERSION:Nvers)
 IS_BUILTIN.bind=	yes
-BUILTIN_PKG.bind=	bind-${_BIND_VERSION}
+BUILTIN_PKG.bind=	bind-${_BIND_VERSION:Nvers}
 BUILDLINK_VARS+=	BUILTIN_PKG.bind
 .  endif
 .endif  # IS_BUILTIN.bind
 BUILDLINK_VARS+=	IS_BUILTIN.bind
 
+.if !defined(USE_BUILTIN.bind)
 USE_BUILTIN.bind?=	${IS_BUILTIN.bind}
+.  if defined(BUILTIN_PKG.bind)
+.    for _depend_ in ${BUILDLINK_DEPENDS.bind}
+.      if !empty(USE_BUILTIN.bind:M[yY][eE][sS])
+USE_BUILTIN.bind!=							\
+	if ${PKG_ADMIN} pmatch '${_depend_}' ${BUILTIN_PKG.bind}; then	\
+		${ECHO} "yes";						\
+	else								\
+		${ECHO} "no";						\
+	fi
+.      endif
+.    endfor
+.  endif
+.endif	# USE_BUILTIN.bind
