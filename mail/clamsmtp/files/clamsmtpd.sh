2239fad927cbea3bfecf88549e1467ae1d94f1db
xtraeme <xtraeme@pkgsrc.org>
2004-07-21 03:54:24 +0000

    Initial import of clamsmtp-0.3.
    ClamSMTP is an SMTP filter that allows you to check for viruses using
    the ClamAV anti-virus software. It accepts SMTP connections and
    forwards the SMTP commands and responses to another SMTP server. The
    'DATA' email body is intercepted and scanned before forwarding.
    
    ClamSMTP aims to be lightweight, reliable, and simple rather than have
    a myriad of options. It's written in C without major dependencies.
    If you need more options then you could use something big like AMaViS
    which is written in PERL and can do almost anything.

---
+++ mail/clamsmtp/files/clamsmtpd.sh
@@ -0,0 +1,24 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: clamsmtpd.sh,v 1.1.1.1 2004/07/21 03:54:25 xtraeme Exp $
+#
+# PROVIDE: clamsmtpd
+# REQUIRE: LOGIN clamd
+# BEFORE: mail
+# KEYWORD: shutdown
+
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
+fi
+
+name="clamsmtpd"
+rcvar=$name
+command="@PREFIX@/sbin/${name}"
+pidfile="/tmp/${name}.pid"
+clamsmtpd_user="@CLAMAV_USER@"
+clamav_conf="@PKG_SYSCONFDIR@/clamav.conf"
+socket=$(@AWK@ '/^#/ {next}; /LocalSocket/ {print $2}' ${clamav_conf})
+command_args="-c ${socket} -p ${pidfile} 127.0.0.1:10026"
+
+load_rc_config $name
+run_rc_command "$1"
