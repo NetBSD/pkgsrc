c2d060094757e4c893006ed6a0560d4f370d1fdc
adam <adam@pkgsrc.org>
2004-07-22 14:59:53 +0000

    Changes 2.0.12: * Fixed a bug in ./configure which would sometimes assume that GTK is not   installed whereas it actually is * Fixed a race condition in nessus-adduser for users who do not configure   their TMPDIR variable (thanks to Cyrille Barthelemy) * Fixed a bug in nessus-update-plugins which would not update the plugins   properly on all systems (thanks to Keith Butler) * Fixed the installer to compile Nessus with GTK support if gtk-config OR   pkg-config is installed.

--- security/libnasl/buildlink3.mk
+++ security/libnasl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/05/03 05:52:55 snj Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/07/22 15:00:20 adam Exp $
 # XXX	BUILDLINK_DEPMETHOD.libnasl?=	build
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
@@ -12,9 +12,11 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibnasl}
 BUILDLINK_PACKAGES+=	libnasl
 
 .if !empty(LIBNASL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libnasl+=	libnasl>=2.0.9
-BUILDLINK_RECOMMENDED.libnasl?=	libnasl>=2.0.9nb1
+BUILDLINK_DEPENDS.libnasl+=	libnasl>=2.0.12
+BUILDLINK_RECOMMENDED.libnasl?=	libnasl>=2.0.12
 BUILDLINK_PKGSRCDIR.libnasl?=	../../security/libnasl
 .endif	# LIBNASL_BUILDLINK3_MK
+
+.include "../../security/nessus-libraries/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
--- security/nessus-core/buildlink3.mk
+++ security/nessus-core/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/05/03 05:52:55 snj Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/07/22 15:00:50 adam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 NESSUS_CORE_BUILDLINK3_MK:=	${NESSUS_CORE_BUILDLINK3_MK}+
@@ -11,13 +11,12 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nnessus-core}
 BUILDLINK_PACKAGES+=	nessus-core
 
 .if !empty(NESSUS_CORE_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.nessus-core+=	nessus-core>=2.0.9
-BUILDLINK_RECOMMENDED.nessus-core?=	nessus-core>=2.0.9nb1
+BUILDLINK_DEPENDS.nessus-core+=	nessus-core>=2.0.12
+BUILDLINK_RECOMMENDED.nessus-core?=	nessus-core>=2.0.12
 BUILDLINK_PKGSRCDIR.nessus-core?=	../../security/nessus-core
 .endif	# NESSUS_CORE_BUILDLINK3_MK
 
 .include "../../security/libnasl/buildlink3.mk"
-.include "../../security/nessus-libraries/buildlink3.mk"
-.include "../../x11/gtk/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
