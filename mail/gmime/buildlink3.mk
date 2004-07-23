1ab7b79a062dd9e4478d52475605a4caf1c1898c
recht <recht@pkgsrc.org>
2004-07-23 08:58:47 +0000

    correct path to pkg directory

--- mail/gmime/buildlink3.mk
+++ mail/gmime/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/07/04 23:10:50 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/07/23 08:58:47 recht Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GMIME_BUILDLINK3_MK:=	${GMIME_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	gmime
 
 .if !empty(GMIME_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gmime+=	gmime>=2.1.6
-BUILDLINK_PKGSRCDIR.gmime?=	../../mail/gmime.work
+BUILDLINK_PKGSRCDIR.gmime?=	../../mail/gmime
 .endif	# GMIME_BUILDLINK3_MK
 
 .include "../../devel/glib2/buildlink3.mk"
