6b7577445bbe5239eb0a485708fbc921bb053942
jlam <jlam>
2004-07-24 17:24:29 +0000

    Need to bump the version in buildlink3.mk since the shlib major was bumped.

--- databases/openldap/buildlink3.mk
+++ databases/openldap/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2004/03/26 02:27:36 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2004/07/24 17:24:29 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 OPENLDAP_BUILDLINK3_MK:=	${OPENLDAP_BUILDLINK3_MK}+
@@ -13,9 +13,9 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nopenldap}
 BUILDLINK_PACKAGES+=	openldap
 
 .if !empty(OPENLDAP_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.openldap+=	openldap>=2.1.22
-BUILDLINK_RECOMMENDED.openldap?=	openldap>=2.1.25nb3
-BUILDLINK_PKGSRCDIR.openldap?=	../../databases/openldap
+BUILDLINK_DEPENDS.openldap+=		openldap>=2.1.22
+BUILDLINK_RECOMMENDED.openldap?=	openldap>=2.2.13
+BUILDLINK_PKGSRCDIR.openldap?=		../../databases/openldap
 .endif	# OPENLDAP_BUILDLINK3_MK
 
 .if defined(USE_SASL2) && (${USE_SASL2} == "YES")
