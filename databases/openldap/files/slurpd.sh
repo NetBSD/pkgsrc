3aad886cb1179b9b931ff65a810a8e5f363e51d2
jlam <jlam@pkgsrc.org>
2004-07-24 03:32:24 +0000

    Update databases/openldap to version 2.2.13, the latest stable release. Major changes from version 2.1.30 include:
      * "LDAP Sync"-based lightweight replication
      * Proxy Cache Support
      * Hierarchical Backend
      * NS-SLAPI Support
      * Backend Layering
      * Access Control extensions including dynamic group support
      * LDAPv3 extensions:
        - ACID extensions
        - Cancel Operation
        - Content Synchronization Operation
        - DIT Content Rules
        - Duplicate Entry Extension
        - Simple Paged Results Extension
        - Proxy Authorization Extension

--- databases/openldap/files/slurpd.sh
+++ databases/openldap/files/slurpd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: slurpd.sh,v 1.1 2004/02/14 11:31:50 jlam Exp $
+# $NetBSD: slurpd.sh,v 1.2 2004/07/24 03:32:24 jlam Exp $
 #
 # OpenLDAP LDAP database replication daemon
 #
@@ -14,12 +14,11 @@ fi
 name="slurpd"
 rcvar=${name}
 command="@PREFIX@/libexec/${name}"
-required_files="@PKG_SYSCONFDIR@/slapd.conf"
+required_files="@OPENLDAP_ETCDIR@/slapd.conf"
 required_vars="slapd"
 
 if [ -f /etc/rc.subr ]; then
 	load_rc_config $name
-	eval [ -z "\$${rcvar}" ] && eval ${rcvar}=NO
 	run_rc_command "$1"
 else
 	@ECHO@ -n " ${name}"
