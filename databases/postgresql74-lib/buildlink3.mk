2bd79e7e157558826e8a34e60a6b03ddd1a9fdcb
recht <recht@pkgsrc.org>
2004-07-24 22:44:12 +0000

    change pkgname to postgresql74* (from postgresql*)

--- databases/postgresql74-lib/buildlink3.mk
+++ databases/postgresql74-lib/buildlink3.mk
@@ -1,20 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/06/27 16:38:32 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/07/24 22:44:12 recht Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-POSTGRESQL_LIB_BUILDLINK3_MK:=	${POSTGRESQL_LIB_BUILDLINK3_MK}+
+POSTGRESQL74_LIB_BUILDLINK3_MK:=	${POSTGRESQL74_LIB_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	postgresql-lib
+BUILDLINK_DEPENDS+=	postgresql74-lib
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npostgresql-lib}
-BUILDLINK_PACKAGES+=	postgresql-lib
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npostgresql74-lib}
+BUILDLINK_PACKAGES+=	postgresql74-lib
 
-.if !empty(POSTGRESQL_LIB_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.postgresql-lib+=	postgresql-lib>=7.4.3
-BUILDLINK_PKGSRCDIR.postgresql-lib?=	../../databases/postgresql74-lib
-BUILDLINK_INCDIRS.postgresql-lib?=	include/postgresql
-.endif	# POSTGRESQL_LIB_BUILDLINK3_MK
+.if !empty(POSTGRESQL74_LIB_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.postgresql74-lib+=	postgresql74-lib>=7.4.3
+BUILDLINK_PKGSRCDIR.postgresql74-lib?=	../../databases/postgresql74-lib
+BUILDLINK_INCDIRS.postgresql74-lib?=	include/postgresql
+.endif	# POSTGRESQL74_LIB_BUILDLINK3_MK
 
 .include "../../security/openssl/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
