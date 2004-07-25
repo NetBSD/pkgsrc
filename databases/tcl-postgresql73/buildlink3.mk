6b71696ccda72dbd80c5b60ebad00c13825de035
kristerw <kristerw@pkgsrc.org>
2004-07-25 23:09:25 +0000

    Correct path to ../../databases/tcl-postgresql73

--- databases/tcl-postgresql73/buildlink3.mk
+++ databases/tcl-postgresql73/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/07/24 22:25:01 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/07/25 23:09:25 kristerw Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 TCL_POSTGRESQL_BUILDLINK3_MK:=	${TCL_POSTGRESQL_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_PACKAGES+=	tcl-postgresql
 .if !empty(TCL_POSTGRESQL_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.tcl-postgresql+=	tcl-postgresql>=7.3.1
 BUILDLINK_RECOMMENDED.tcl-postgresql?=	tcl-postgresql>=7.3.4nb3
-BUILDLINK_PKGSRCDIR.tcl-postgresql?=	../../databases/tcl-postgresql
+BUILDLINK_PKGSRCDIR.tcl-postgresql?=	../../databases/tcl-postgresql73
 .endif	# TCL_POSTGRESQL_BUILDLINK3_MK
 
 .include "../../lang/tcl/buildlink3.mk"
