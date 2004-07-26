5d3ec65f6703a555a445d696b55b787354b110f5
agc <agc>
2004-07-26 16:30:37 +0000

    Pullup ticket 71 to the pkgsrc-2004Q2 branch, requested by Grant Beattie
    Solaris fix for libpcap
    
    	Module Name:    pkgsrc
    	Committed By:   grant
    	Date:           Sun Jun 27 12:04:45 UTC 2004
    
    	Modified Files:
    		pkgsrc/net/libpcap: buildlink3.mk
    
    	Log Message:
    	on Solaris, binaries that use libpcap.a always need -lnsl and -lsocket
    	for the socket functions.

--- net/libpcap/buildlink3.mk
+++ net/libpcap/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2004/03/10 17:57:14 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.12.4.1 2004/07/26 16:30:37 agc Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBPCAP_BUILDLINK3_MK:=	${LIBPCAP_BUILDLINK3_MK}+
@@ -13,6 +13,16 @@ BUILDLINK_PACKAGES+=	libpcap
 .if !empty(LIBPCAP_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libpcap+=	libpcap>=0.7.2
 BUILDLINK_PKGSRCDIR.libpcap?=	../../net/libpcap
+
+.  include "../../mk/bsd.prefs.mk"
+
+# on Solaris, we always need libnsl and libsocket because libpcap.a
+# references symbols in them.
+.  if ${OPSYS} == "SunOS"
+BUILDLINK_LDADD.libpcap+=	-lnsl -lsocket
+LDFLAGS+=			${BUILDLINK_LDADD.libpcap}
+.  endif
+
 .endif	# LIBPCAP_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
