902f9113ecfa480d13d0ec70adf9cc2281be6514
recht <recht>
2004-07-27 19:29:45 +0000

    Fix PKGNAME after PostgreSQL 73 changes.

--- databases/tcl-postgresql73/buildlink3.mk
+++ databases/tcl-postgresql73/buildlink3.mk
@@ -1,20 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/07/25 23:09:25 kristerw Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/07/27 19:29:45 recht Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-TCL_POSTGRESQL_BUILDLINK3_MK:=	${TCL_POSTGRESQL_BUILDLINK3_MK}+
+TCL_POSTGRESQL73_BUILDLINK3_MK:=	${TCL_POSTGRESQL73_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	tcl-postgresql
+BUILDLINK_DEPENDS+=	tcl-postgresql73
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ntcl-postgresql}
-BUILDLINK_PACKAGES+=	tcl-postgresql
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ntcl-postgresql73}
+BUILDLINK_PACKAGES+=	tcl-postgresql73
 
-.if !empty(TCL_POSTGRESQL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.tcl-postgresql+=	tcl-postgresql>=7.3.1
-BUILDLINK_RECOMMENDED.tcl-postgresql?=	tcl-postgresql>=7.3.4nb3
-BUILDLINK_PKGSRCDIR.tcl-postgresql?=	../../databases/tcl-postgresql73
-.endif	# TCL_POSTGRESQL_BUILDLINK3_MK
+.if !empty(TCL_POSTGRESQL73_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.tcl-postgresql73+=	tcl-postgresql73>=7.3.6
+BUILDLINK_PKGSRCDIR.tcl-postgresql73?=	../../databases/tcl-postgresql73
+.endif	# TCL_POSTGRESQL73_BUILDLINK3_MK
 
 .include "../../lang/tcl/buildlink3.mk"
 
