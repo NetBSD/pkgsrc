5216e9177760cc81880d4f29c9c9194e376329f9
shannonjr <shannonjr@pkgsrc.org>
2004-07-28 21:40:21 +0000

    Applied several changes from lang/gcc34: 1) Test and mkdir if necessary to correct problem reported by    Matthias Drochner: "For me (using ALT_GCC), the directory .work/.gcc was    not present at pre-configure time, which made some "cd" fail." 2) Makefile and pkglint cleanup as recommended by Lubomir Sedlacik

--- lang/gcc34-ada/preconfigure.mk
+++ lang/gcc34-ada/preconfigure.mk
@@ -1,4 +1,5 @@
 	# Create compiler driver scripts in ${WRKDIR}.
+	(test -d ${WRKDIR}/.gcc/bin || ${MKDIR} ${WRKDIR}/.gcc/bin)
 	(cd ${BUILDLINK_PREFIX.gcc34-ada}/bin && bin_files=`${FIND} . -type f \( -perm -0100 \)` && \
 	cd ${WRKDIR}/.gcc/bin && \
 	for _target_ in $${bin_files} ; do \
