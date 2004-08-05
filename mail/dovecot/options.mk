1243a989e949fdbe6e8204f4133005dea1cb57ad
jlam <jlam@pkgsrc.org>
2004-08-05 02:22:19 +0000

    Document each of the options.

--- mail/dovecot/options.mk
+++ mail/dovecot/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2004/08/04 17:12:46 salo Exp $
+# $NetBSD: options.mk,v 1.3 2004/08/05 02:22:19 jlam Exp $
 
 # Global and legacy options
 .if defined(DOVECOT_USE_GNUTLS) || defined(DOVECOT_USE_MYSQL) || \
@@ -30,6 +30,9 @@ PKG_OPTIONS_VAR=	PKG_OPTIONS.dovecot
 PKG_SUPPORTED_OPTIONS=	gnutls inet6 ldap mysql pgsql sasl
 .include "../../mk/bsd.options.mk"
 
+###
+### Build with GNU TLS or OpenSSL as the underlying crypto library.
+###
 .if !empty(PKG_OPTIONS:Mgnutls)
 CONFIGURE_ARGS+=	--enable-ssl=gnutls
 .  include "../../security/gnutls/buildlink3.mk"
@@ -38,26 +41,41 @@ CONFIGURE_ARGS+=        --enable-ssl=openssl
 .  include "../../security/openssl/buildlink3.mk"
 .endif
 
+###
+### MySQL support.
+###
 .if !empty(PKG_OPTIONS:Mmysql)
 CONFIGURE_ARGS+=	--with-mysql
 .  include "../../databases/mysql4-client/buildlink3.mk"
 .endif
 
+###
+### PostgreSQL support.
+###
 .if !empty(PKG_OPTIONS:Mpgsql)
 CONFIGURE_ARGS+=	--with-pgsql
 CPPFLAGS+=		-I${BUILDLINK_DIR}/include/pgsql
 .  include "../../mk/pgsql.buildlink3.mk"
 .endif
 
+###
+### IPv6 support.
+###
 .if !empty(PKG_OPTIONS:Minet6)
 CONFIGURE_ARGS+=	--enable-ipv6
 .endif
 
+###
+### LDAP directory support.
+###
 .if !empty(PKG_OPTIONS:Mldap)
 CONFIGURE_ARGS+=	--with-ldap
 .  include "../../databases/openldap/buildlink3.mk"
 .endif
 
+###
+### IMAP-AUTH via SASL.
+###
 .if !empty(PKG_OPTIONS:Msasl)
 CONFIGURE_ARGS+=	--with-cyrus-sasl2
 .  include "../../security/cyrus-sasl2/buildlink3.mk"
