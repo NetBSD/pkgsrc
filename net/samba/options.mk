cbf750a434c562456ee964768455d92fd0d17de8
jlam <jlam@pkgsrc.org>
2004-08-05 03:04:33 +0000

    Rename Makefile.options to options.mk in the packages that I maintain. This follows the example of the mail/dovecot package, as suggested by <schmonz>.

--- net/samba/options.mk
+++ net/samba/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: Makefile.options,v 1.1 2004/07/30 21:05:41 jlam Exp $
+# $NetBSD: options.mk,v 1.1 2004/08/05 03:04:34 jlam Exp $
 
 # Global and legacy options
 .if defined(USE_CUPS) || defined(USE_PAM) || defined(USE_OPENLDAP) || \
