28b5a92008af592442341bc4b5c9b142755f9678
jlam <jlam@pkgsrc.org>
2004-08-06 16:55:06 +0000

    Update pkgtools/libnbcompat to 20040805.  Changes from version 20040512 include:
      (1) Add a statvfs() implementation based on statfs(), and remove the
          statfs() NetBSD-compatibility implementation.  statvfs() is used
          in src/usr.sbin/pkg_install on the HEAD branch.
    
      (2) Add a fnmatch() implementation, needed by IRIX.  fnmatch() is used
          in src/usr.sbin/pkg_install on the HEAD branch.
    
      (3) Remove fseeko(), inet_ntop(), inet_pton(), sl_*() strptime(), and
          timegm() as they're not used by any of the bootstrap packages that
          require libnbcompat (pax, mtree, pkg_install).  All that's left is
          what's necessary to bootstrap those three packages.

--- pkgtools/libnbcompat/buildlink3.mk
+++ pkgtools/libnbcompat/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/03/05 19:25:39 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2004/08/06 16:55:09 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBNBCOMPAT_BUILDLINK3_MK:=	${LIBNBCOMPAT_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibnbcompat}
 BUILDLINK_PACKAGES+=	libnbcompat
 
 .if !empty(LIBNBCOMPAT_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libnbcompat+=		libnbcompat>=20030916
+BUILDLINK_DEPENDS.libnbcompat+=		libnbcompat>=20040805
 BUILDLINK_PKGSRCDIR.libnbcompat?=	../../pkgtools/libnbcompat
 BUILDLINK_DEPMETHOD.libnbcompat?=	build
 BUILDLINK_LDADD.libnbcompat=		-lnbcompat
