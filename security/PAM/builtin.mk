894b7efd68304aa22e0cc42cff3f902a6bad1a9f
reed <reed@pkgsrc.org>
2004-08-06 03:14:22 +0000

    In the rare case when LOCALBASE is /usr, don't let this think that /usr/include/security/pam_appl.h means that PAM is builtin. (This is so a dependency can be registered correctly.)

--- security/PAM/builtin.mk
+++ security/PAM/builtin.mk
@@ -1,8 +1,8 @@
-# $NetBSD: builtin.mk,v 1.2 2004/03/29 05:43:35 jlam Exp $
+# $NetBSD: builtin.mk,v 1.3 2004/08/06 03:14:22 reed Exp $
 
 .if !defined(IS_BUILTIN.pam)
 IS_BUILTIN.pam=	no
-.  if exists(/usr/include/security/pam_appl.h)
+.  if exists(/usr/include/security/pam_appl.h) && (${LOCALBASE} != "/usr")
 IS_BUILTIN.pam=	yes
 .  endif
 .endif	# IS_BUILTIN.pam
