c4b3ae0830edb150c4e7b151632ddbcc4686dde6
jlam <jlam@pkgsrc.org>
2004-08-06 15:41:46 +0000

    Accidentally reversed meaning of test.

--- security/PAM/builtin.mk
+++ security/PAM/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.4 2004/08/06 15:37:20 jlam Exp $
+# $NetBSD: builtin.mk,v 1.5 2004/08/06 15:41:46 jlam Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
@@ -7,7 +7,7 @@ _SECURITY_PAM_APPL_H=	/usr/include/security/pam_appl.h
 .if !defined(IS_BUILTIN.pam)
 IS_BUILTIN.pam=	no
 .  if exists(${_SECURITY_PAM_APPL_H}) && \
-      !empty(_SECURITY_PAM_APPL_H:M${LOCALBASE}/*)
+      empty(_SECURITY_PAM_APPL_H:M${LOCALBASE}/*)
 IS_BUILTIN.pam=	yes
 .  endif
 .endif	# IS_BUILTIN.pam
