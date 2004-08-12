92451480e97649767c36d779211e864e1e94fde2
jlam <jlam>
2004-08-12 10:09:49 +0000

    First cut at supporting the built-in PAM on MacOS X.  It's not the same as Linux-PAM, but it's close enough for the purposes of compiling programs.

--- security/PAM/builtin.mk
+++ security/PAM/builtin.mk
@@ -1,6 +1,7 @@
-# $NetBSD: builtin.mk,v 1.6 2004/08/06 15:43:09 jlam Exp $
+# $NetBSD: builtin.mk,v 1.7 2004/08/12 10:09:49 jlam Exp $
 
 .include "../../mk/bsd.prefs.mk"
+_PAM_PAM_APPL_H=	/usr/include/pam/pam_appl.h
 
 _SECURITY_PAM_APPL_H=	/usr/include/security/pam_appl.h
 
@@ -12,4 +13,22 @@ IS_BUILTIN.pam=	yes
 .  endif
 .endif	# IS_BUILTIN.pam
 
+.if !defined(USE_BUILTIN.pam)
 USE_BUILTIN.pam?=	${IS_BUILTIN.pam}
+.  if empty(_PAM_PAM_APPL_H:M${LOCALBASE}/*) && exists(${_PAM_PAM_APPL_H})
+#
+# Treat MacOS X's PAM implementation as Linux-PAM-compatible.
+#
+USE_BUILTIN.pam=	yes
+.  endif
+.endif
+
+CHECK_BUILTIN.pam?=	no
+.if !empty(CHECK_BUILTIN.pam:M[nN][oO])
+
+.if !empty(USE_BUILTIN.pam:M[yY][eE][sS])
+BUILDLINK_TRANSFORM.pam+=	-e "s|/include/pam/|/include/security/|"
+BUILDLINK_FILES.pam+=		include/pam/*.h
+.endif
+
+.endif	# CHECK_BUILTIN.pam
