7ae855db80cca036ae165cbe4bbb1a8608d3ff8e
minskim <minskim@pkgsrc.org>
2004-08-17 15:34:23 +0000

    This package is not in pkgsrc-wip any more.

--- x11/py-Xlib/buildlink3.mk
+++ x11/py-Xlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/08/17 15:30:43 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/08/17 15:34:23 minskim Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PY23PTH_XLIB_BUILDLINK3_MK:=	${PY23PTH_XLIB_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	py23pth-Xlib
 
 .if !empty(PY23PTH_XLIB_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.py23pth-Xlib+=	py23pth-Xlib>=0.12
-BUILDLINK_PKGSRCDIR.py23pth-Xlib?=	../../wip/py-Xlib
+BUILDLINK_PKGSRCDIR.py23pth-Xlib?=	../../x11/py-Xlib
 .endif	# PY23PTH_XLIB_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
