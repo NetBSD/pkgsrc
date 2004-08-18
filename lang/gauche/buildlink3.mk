f683645e67632d7481865c250b628ef31fabc9e1
minoura <minoura@pkgsrc.org>
2004-08-18 13:52:38 +0000

    Update for recent dependencies/version.

--- lang/gauche/buildlink3.mk
+++ lang/gauche/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/03/29 09:34:26 kei Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.4.
-# XXX After this file as been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink[23].mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.Gauche?=	build
+# $NetBSD: buildlink3.mk,v 1.2 2004/08/18 13:52:38 minoura Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GAUCHE_BUILDLINK3_MK:=	${GAUCHE_BUILDLINK3_MK}+
@@ -21,11 +11,12 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NGauche}
 BUILDLINK_PACKAGES+=	Gauche
 
 .if !empty(GAUCHE_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.Gauche+=	Gauche>=0.7.4.2
+BUILDLINK_DEPENDS.Gauche+=	Gauche>=0.8.1
 BUILDLINK_PKGSRCDIR.Gauche?=	../../lang/gauche
 .endif	# GAUCHE_BUILDLINK3_MK
 
 .include "../../devel/boehm-gc/buildlink3.mk"
+.include "../../devel/dlcompat/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
