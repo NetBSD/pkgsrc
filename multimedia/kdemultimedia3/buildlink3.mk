149e5c8b07d823557fa52906208d84d31c1ace3c
markd <markd@pkgsrc.org>
2004-08-21 14:44:47 +0000

    Update to KDE 3.3
    Changes:
    * aRts
      o JACK output plugin Zack Rusin, Matthias Kretz
    * aKode
      o akodelib: New multithreaded audio (de/en)coding library Allan
        Sandfeld Jensen
      o akodelib: Plugins for MPEG-audio, Ogg/Vorbis, FLAC, Ogg/FLAC and
        Musepack decoding Allan Sandfeld Jensen
      o akodelib: Support for external encoding and output plugins Allan
        Sandfeld Jensen
      o akodelib_artsplugin: aRts-plugin that uses the akode-decoding
        plugins Allan Sandfeld Jensen
      o akodelib_artsplugin: Support streaming aRts API, to enable
        streaming playback Allan Sandfeld Jensen
    * KRec
      o Update the documentation. Arnold Krille
    * JuK
      o Improved track announcement popup Michael Pyne
      o Add to K3B support for songs Michael Pyne
      o Improved DCOP support Michael Pyne
      o Drag and drop retagging of songs Michael Pyne
      o Quickly play songs from the Search Bar Michael Pyne
      o Album random play Michael Pyne
    * KFilePlugins
      o New plugin for C64 PSID music files Rolf Magnus

--- multimedia/kdemultimedia3/buildlink3.mk
+++ multimedia/kdemultimedia3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/20 13:13:42 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/08/21 14:44:47 markd Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 KDEMULTIMEDIA_BUILDLINK3_MK:=	${KDEMULTIMEDIA_BUILDLINK3_MK}+
@@ -15,9 +15,11 @@ BUILDLINK_DEPENDS.kdemultimedia+=	kdemultimedia>=3.2.2
 BUILDLINK_PKGSRCDIR.kdemultimedia?=	../../multimedia/kdemultimedia3
 .endif	# KDEMULTIMEDIA_BUILDLINK3_MK
 
-.include "../../audio/musicbrainz/buildlink3.mk"
-.include "../../audio/taglib/buildlink3.mk"
 .include "../../audio/arts/buildlink3.mk"
+.include "../../audio/lame/buildlink3.mk"
+.include "../../audio/musicbrainz/buildlink3.mk"
+.include "../../audio/tunepimp/buildlink3.mk"
+.include "../../audio/taglib/buildlink3.mk"
 .include "../../multimedia/xine-lib/buildlink3.mk"
 .include "../../x11/kdebase3/buildlink3.mk"
 .include "../../x11/kdelibs3/buildlink3.mk"
