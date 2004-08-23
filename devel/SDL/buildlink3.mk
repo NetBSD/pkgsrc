dedf2f80804a365c8ae9cebffe96f5e91e941ae2
hubertf <hubertf@pkgsrc.org>
2004-08-23 23:57:20 +0000

    Don't pull in Mesa on IRIX - patch by Staffan Thomén (duck@IRC)

--- devel/SDL/buildlink3.mk
+++ devel/SDL/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/03/23 04:37:38 snj Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2004/08/23 23:57:20 hubertf Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 SDL_BUILDLINK3_MK:=	${SDL_BUILDLINK3_MK}+
@@ -18,7 +18,11 @@ BUILDLINK_PKGSRCDIR.SDL?=	../../devel/SDL
 USE_X11=	yes
 PTHREAD_OPTS+=	require
 
+.include "../../mk/bsd.prefs.mk"
+
+.if ${OPSYS} != "IRIX"
 .include "../../graphics/Mesa/buildlink3.mk"
+.endif
 .include "../../graphics/aalib-x11/buildlink3.mk"
 
 .include "../../mk/pthread.buildlink3.mk"
