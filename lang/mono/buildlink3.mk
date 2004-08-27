5f2edfa8a2c416fb4ce8cb5f6853928958d38b87
recht <recht>
2004-08-27 22:02:47 +0000

    update to mono-1.0.1
    Mono 1.0.1 is a maintenance release release for the 1.0 series of the
    Mono runtime and thus only contains bug-fixes -- most notably for amd64.
    
    While at it apply some changes to the package:
    - Remove the MONO_GC_TYPE Makefile knob -- with 1.x the included
      libgc is the way to go.
    - Also remove MONO_USE_NPTL knob and let configure do the right thing.

--- lang/mono/buildlink3.mk
+++ lang/mono/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2004/07/13 14:45:33 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2004/08/27 22:02:47 recht Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 MONO_BUILDLINK3_MK:=	${MONO_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmono}
 BUILDLINK_PACKAGES+=	mono
 
 .if !empty(MONO_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.mono+=	mono>=1.0nb1
+BUILDLINK_DEPENDS.mono+=	mono>=1.0.1
 BUILDLINK_PKGSRCDIR.mono?=	../../lang/mono
 .endif	# MONO_BUILDLINK3_MK
 
