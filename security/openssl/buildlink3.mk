e2a87e4dfb5a9641b2788fb2c903f3e9eaa3a644
jlam <jlam@pkgsrc.org>
2004-08-28 20:38:18 +0000

    Back out previous... unintended commit.

--- security/openssl/buildlink3.mk
+++ security/openssl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2004/08/28 06:05:32 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2004/08/28 20:38:18 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 OPENSSL_BUILDLINK3_MK:=	${OPENSSL_BUILDLINK3_MK}+
@@ -13,13 +13,13 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nopenssl}
 BUILDLINK_PACKAGES+=	openssl
 
 .if !empty(OPENSSL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.openssl+=	openssl>=0.9.6
+BUILDLINK_DEPENDS.openssl+=	openssl>=0.9.6m
 BUILDLINK_PKGSRCDIR.openssl?=	../../security/openssl
 
 # Ensure that -lcrypt comes before -lcrypto when linking so that the
 # system crypt() routine is used.
 #
-WRAPPER_REORDER_CMDS+=	reorder:l:crypt:crypto
+BUILDLINK_TRANSFORM+=	reorder:l:crypt:crypto
 
 SSLBASE=	${BUILDLINK_PREFIX.openssl}
 BUILD_DEFS+=	SSLBASE
