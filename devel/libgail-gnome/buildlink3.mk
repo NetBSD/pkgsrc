2fcffbf8a21b99439d2d3978efd0f54a3f12b5eb
jmmv <jmmv@pkgsrc.org>
2004-08-31 18:10:25 +0000

    Initial addition.

---
+++ devel/libgail-gnome/buildlink3.mk
@@ -0,0 +1,26 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/08/31 18:10:25 jmmv Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBGAIL_GNOME_BUILDLINK3_MK:=	${LIBGAIL_GNOME_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libgail-gnome
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgail-gnome}
+BUILDLINK_PACKAGES+=	libgail-gnome
+
+.if !empty(LIBGAIL_GNOME_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libgail-gnome+=	libgail-gnome>=1.0.4
+BUILDLINK_PKGSRCDIR.libgail-gnome?=	../../devel/libgail-gnome
+.endif	# LIBGAIL_GNOME_BUILDLINK3_MK
+
+.include "../../devel/at-spi/buildlink3.mk"
+.include "../../devel/atk/buildlink3.mk"
+.include "../../devel/libbonobo/buildlink3.mk"
+.include "../../devel/libbonoboui/buildlink3.mk"
+.include "../../devel/libgnomeui/buildlink3.mk"
+.include "../../x11/gnome-panel/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
