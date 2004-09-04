492bae4489a3123840acd71e010ad42d73c8b82b
jmmv <jmmv@pkgsrc.org>
2004-09-04 14:28:50 +0000

    Update to 0.17:
    Changes in 0.17:
    ----------------
    * Compile fixes (Sjoerd Simons)
    * Depends on GLib 2.4
    * Fixed memory leak
    * Added support to work tunnel, using another JID than the connect server.
    * Support for keep alive packages
    
    Changes in 0.16:
    ----------------
    * Support for giving the context to which an LmConnection should run in
      (should solve issues when wanting to run several LmConnections in various
       threads)
    * HTTP proxy support
    * Memory leak fix
    * Distribute the win32-config.h file needed to build on Windows.

--- chat/loudmouth/buildlink3.mk
+++ chat/loudmouth/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/03/18 09:12:09 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/09/04 14:28:50 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LOUDMOUTH_BUILDLINK3_MK:=	${LOUDMOUTH_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nloudmouth}
 BUILDLINK_PACKAGES+=	loudmouth
 
 .if !empty(LOUDMOUTH_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.loudmouth+=	loudmouth>=0.15.1nb3
+BUILDLINK_DEPENDS.loudmouth+=	loudmouth>=0.17
 BUILDLINK_PKGSRCDIR.loudmouth?=	../../chat/loudmouth
 .endif	# LOUDMOUTH_BUILDLINK3_MK
 
