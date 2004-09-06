7ca24b66ffaff8450623fe03cf78289aadc8e7d4
danw <danw@pkgsrc.org>
2004-09-06 20:38:36 +0000

    make this use libtool rather than assuming linux-like shlib build rules. bump PKGREVISION for soname change

--- devel/cfg+/buildlink3.mk
+++ devel/cfg+/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/08/27 13:05:58 drochner Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.5.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.libcfg?=	build
+# $NetBSD: buildlink3.mk,v 1.2 2004/09/06 20:38:36 danw Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBCFG_BUILDLINK3_MK:=	${LIBCFG_BUILDLINK3_MK}+
@@ -21,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibcfg}
 BUILDLINK_PACKAGES+=	libcfg
 
 .if !empty(LIBCFG_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libcfg+=	libcfg+>=0.6.2
+BUILDLINK_DEPENDS.libcfg+=	libcfg+>=0.6.2nb1
 BUILDLINK_PKGSRCDIR.libcfg?=	../../devel/cfg+
 .endif	# LIBCFG_BUILDLINK3_MK
 
