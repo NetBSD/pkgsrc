4fb9f04ff3729920f2c65e8b81e07b443d31aadc
jmmv <jmmv@pkgsrc.org>
2004-09-08 08:37:38 +0000

    Fix build by solving a namespace collision introduced by libnbcompat. I'm doing it the easy way because jlam@ will look at this problem closely in a future.

--- textproc/nbsed/files/defs.h
+++ textproc/nbsed/files/defs.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: defs.h,v 1.4 2004/06/13 13:04:02 grant Exp $	*/
+/*	$NetBSD: defs.h,v 1.5 2004/09/08 08:37:38 jmmv Exp $	*/
 
 /*-
  * Copyright (c) 1992, 1993
@@ -32,7 +32,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)defs.h	8.1 (Berkeley) 6/6/93
- *	$NetBSD: defs.h,v 1.4 2004/06/13 13:04:02 grant Exp $
+ *	$NetBSD: defs.h,v 1.5 2004/09/08 08:37:38 jmmv Exp $
  */
 
 /*-
@@ -70,7 +70,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)defs.h	8.1 (Berkeley) 6/6/93
- *	$NetBSD: defs.h,v 1.4 2004/06/13 13:04:02 grant Exp $
+ *	$NetBSD: defs.h,v 1.5 2004/09/08 08:37:38 jmmv Exp $
  */
 
 /*
@@ -127,6 +127,10 @@ struct s_command {
 	u_int nonsel:1;				/* True if ! */
 	u_int inrange:1;			/* True if in range */
 };
+
+/* XXX This constant is defined by utmpx.h, although we don't need it.
+ * Undefine it to solve a namespace collision until it's better fixed. */
+#undef EMPTY
 
 /*
  * Types of command arguments recognised by the parser
