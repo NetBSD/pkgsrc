60a4758e513e7ecfb1bcc39e634f25aee84cfba4
jlam <jlam>
2004-09-15 04:11:11 +0000

    Include buildlink3.mk files for packages needed to satisfy library dependencies.  This fixes link failures when the Heimdal dependency is satisfied by the package rather than the builtin Heimdal.  Pointed out by Mark Davies in private email.
    I've intentionally left out including readline/buildlink3.mk.  Although
    it is used by libsl.* and libss.*, those libraries are not actually
    critical or used by other packages that depend on Heimdal for Kerberos
    functionality.

--- security/heimdal/buildlink3.mk
+++ security/heimdal/buildlink3.mk
@@ -1,7 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.17 2004/09/14 14:41:34 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2004/09/15 04:11:11 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 HEIMDAL_BUILDLINK3_MK:=	${HEIMDAL_BUILDLINK3_MK}+
+
+.include "../../mk/bsd.prefs.mk"
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	heimdal
@@ -16,5 +18,14 @@ BUILDLINK_RECOMMENDED.heimdal?=	heimdal>=0.6.3
 BUILDLINK_PKGSRCDIR.heimdal?=	../../security/heimdal
 BUILDLINK_INCDIRS.heimdal?=	include/krb5
 .endif	# HEIMDAL_BUILDLINK3_MK
+
+PKG_OPTIONS.heimdal?=	${PKG_DEFAULT_OPTIONS}
+
+.include "../../security/openssl/buildlink3.mk"
+.if !empty(PKG_OPTIONS.heimdal:Mdb4)
+.  include "../../databases/db4/buildlink3.mk"
+.else
+.  include "../../mk/bdb.buildlink3.mk"
+.endif
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
