0287cb1a6a6d916e73b4a21f81779ddbde39ea80
minskim <minskim>
2004-09-16 15:09:01 +0000

    Incorporate security fixes of X.Org X11R6.8.1.
    Bump PKGREVISION.

--- graphics/xpm/buildlink3.mk
+++ graphics/xpm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2004/03/10 17:57:14 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2004/09/16 15:09:01 minskim Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 XPM_BUILDLINK3_MK:=	${XPM_BUILDLINK3_MK}+
@@ -12,6 +12,7 @@ BUILDLINK_PACKAGES+=	xpm
 
 .if !empty(XPM_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.xpm+=		xpm>=3.4k
+BUILDLINK_RECOMMENDED.xpm?=	xpm>=3.4knb2
 BUILDLINK_PKGSRCDIR.xpm?=	../../graphics/xpm
 
 USE_X11=	yes
