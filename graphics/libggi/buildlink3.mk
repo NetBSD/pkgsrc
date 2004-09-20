82fee817ae61632995f65acdb541b49962d85e8d
wiz <wiz>
2004-09-20 22:39:03 +0000

    Update to 2.0.6: New in 2.0.6: 2004-09-14: * build system:   - Prepend with GII include path when --with-uninst-gii is specified via configure   - disable vgagl helper when not building the svga target * X-target:   - Bugfix: Ensure aligned memory access. Fixes crash on NetBSD/sparc64   - Bugfix: Perform correct clipping in ggiFillscreen(3). * display-memory: Bugfix: Free directbuffer completely. * display-monotext: Bugfix: ggiCheckMode() returns actual error code than always success. * documentation update:   - add manual page for display-mansync   - do not document not-existing GGIFLAG_TIDYBUF flag. It is a new feature in devel tree.
    New in 2.0.5: 2004-05-29:
    * X-target:
      - Fix over-allocation of win y dimension.
      - Forward expose events to the application when -nobuffer is used.
      - Fix "strange colored pixels when re-exposing backbuffer"-Bug.
      - Focus pulling fix. Key focus should no longer get lost,
        when the mouse is NOT over the window. Note: Don't forget to
        update libgii's input-xwin and input-x before as libggi now
        passes some additional information to libgii.
    * build system: It checks for buildable demos. Fixes compiling breakage on some platforms.
    * other build system updates
    * libtool update. Fixes Debian bug #208288.
    * documentation updates
    
    Hi Christoph!

--- graphics/libggi/buildlink3.mk
+++ graphics/libggi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/16 17:39:32 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/09/20 22:39:03 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBGGI_BUILDLINK3_MK:=	${LIBGGI_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibggi}
 BUILDLINK_PACKAGES+=	libggi
 
 .if !empty(LIBGGI_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libggi+=	libggi>=2.0.4
+BUILDLINK_DEPENDS.libggi+=	libggi>=2.0.6
 BUILDLINK_PKGSRCDIR.libggi?=	../../graphics/libggi
 .endif	# LIBGGI_BUILDLINK3_MK
 
