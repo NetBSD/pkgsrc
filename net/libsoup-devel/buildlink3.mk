ce30b3c5d34f5c8f7ba07a16c2749dff4acff39b
jmmv <jmmv@pkgsrc.org>
2004-09-21 16:51:37 +0000

    Update to 2.2.0.  This version corresponds to GNOME 2.8.0.
    Changes in libsoup from the 2.0 series (1.99.x versions) to 2.2:
    
    	* Most of the libsoup datatypes are now GObjects. (SoupUri
    	  is currently an exception to this.)
    
    		* SoupMessage now emits signals at various stages of
    	          processing. (Eg, "wrote_body", "got_headers".) (You
    	          can also still use soup_message_add_*handler().)
    
    	* SoupContexts are gone; soup_message_new() now takes a URI
    	  string.
    
    	* All formerly global state is now maintained by the
              SoupSession object. (This includes the connection pool,
              proxy server, cached authentication information, SSL
              certificates, etc.)
    
    		* You can create a SoupSessionAsync (for 2.0-like
    		  behavior) or SoupSessionSync (for blocking,
    		  synchronous usage).
    
    		* You can add SoupMessageFilter objects to a session
                      to have certain processing automatically performed
                      on every message sent via that session. (Eg, setting
                      up handlers.)
    
    		* NTLM authentication is no longer supported by
    	          default. You must enable it by setting the
    		  SOUP_SESSION_USE_NTLM flag on the session.
    
    		* The preferred method of handling authentication is
    	          now via the "authenticate" and "reauthenticate"
    	          signals on SoupSession. (The old style, of encoding
    	          the user and password information into the url is
    	          also still supported.)
    
    	* The SOUP_ERROR_* values are now SOUP_STATUS_* (so that we
              don't have "SOUP_ERROR_OK" and the like).
    
    		* SOUP_MESSAGE_IS_ERROR() is gone, since some cases
    		  want to include 3xx responses and some don't.
    
    		* SOUP_ERROR_CANT_AUTHENTICATE and
                      SOUP_ERROR_CANT_AUTHENTICATE_PROXY are now gone,
                      since they didn't carry any information that
                      SOUP_STATUS_UNAUTHORIZED and
                      SOUP_STATUS_PROXY_UNAUTHORIZED don't.
    
    		* DNS errors now show up as the new status code
    		  SOUP_STATUS_CANT_RESOLVE rather than being mixed in
    		  with SOUP_ERROR_CANT_CONNECT.
    
    	* Minimal SOAP support has been added back, via
              SoupSoapMessage/SoupSoapResponse
    
    	* The HTTP I/O state machine was completely rewritten, fixing
    	  numerous crashes, leaks, and protocol errors.
    
    	* SoupUri now conforms to RFC 2396. Mostly.
    
    	* Various test programs have been added under tests/
    
    	* Removed:
    
    		* Support for OpenSSL (which was horribly buggy) and
    		  Mozilla NSS (which was never finished). We only
    		  support GNUTLS for SSL now.
    
    		* SOCKS support
    
    		* CGI support in SoupServer

--- net/libsoup-devel/buildlink3.mk
+++ net/libsoup-devel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/06/01 16:58:06 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/09/21 16:51:37 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBSOUP_DEVEL_BUILDLINK3_MK:=	${LIBSOUP_DEVEL_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibsoup-devel}
 BUILDLINK_PACKAGES+=	libsoup-devel
 
 .if !empty(LIBSOUP_DEVEL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libsoup-devel+=	libsoup-devel>=2.1.10
+BUILDLINK_DEPENDS.libsoup-devel+=	libsoup-devel>=2.2.0
 BUILDLINK_PKGSRCDIR.libsoup-devel?=	../../net/libsoup-devel
 .endif	# LIBSOUP_DEVEL_BUILDLINK3_MK
 
