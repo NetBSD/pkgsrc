caf3611d6e3e5c4684d3e1d0ffc1f6a54efbd266
jmmv <jmmv@pkgsrc.org>
2004-09-21 16:46:14 +0000

    Update to 2.8.0.  This version corresponds to GNOME 2.8.0.
    libgnome 2.8.0
    --------------
    
    What's new since 2.7.2
    
    * Bug fixes:
    	- Various fixes (Kjartan Maraas)
    	- Avoid duplication of locale alias tables by using the ones
    	  in bonobo-activation (Alexander Larsson)
    
    * Translations:
            - New Norwegian Bookmal translation (Kjartan Maraas)
    	- Various updated translations (ar, az, cs, bg, bs, ca, cs,
    					cy, de, el, es, eu, en_gb, fi,
    					gu, hu, ja, ko, mn, nb, ne,
    					nl, nn, no, pa, pl, pt, ro,
    					ru, sk, sq, sr, sv, tr, uk,
    					zh_CH)
    
    libgnome 2.7.2
    --------------
    
    What's new since 2.7.1
    
    * Bug fixes:
    	- Allow language binding instantiation of GnomeProgram
    
    * Translations:
    	- Various updated translations (ne)
    
    libgnome 2.7.0
    --------------
    
    What's new since 2.6.1
    
    * Bug fixes:
    	- Various fixes (Chris Lahey, Christian Persche,
    			 Steve Chaplin, Toshio Kuratomi,
    			 Julio Merino, Kjartan Maraas)
    
    * Features:
    	- Update to automake 1.7 (James Henstridge)
    
    * Translations:
    	- Various updated translations (en_CA, hu, nl)

--- devel/libgnome/buildlink3.mk
+++ devel/libgnome/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/04/01 18:22:04 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2004/09/21 16:46:14 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGNOME_BUILDLINK3_MK:=	${LIBGNOME_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgnome}
 BUILDLINK_PACKAGES+=	libgnome
 
 .if !empty(LIBGNOME_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libgnome+=	libgnome>=2.6.0
+BUILDLINK_DEPENDS.libgnome+=	libgnome>=2.8.0
 BUILDLINK_PKGSRCDIR.libgnome?=	../../devel/libgnome
 .endif	# LIBGNOME_BUILDLINK3_MK
 
