138fc26573041ed05648b7c35ebc3c24cdf1a4ee
jmmv <jmmv@pkgsrc.org>
2004-09-21 16:59:47 +0000

    Update to 1.1.0.  This version corresponds to GNOME 2.8.0. While here, move the documentation to share/doc, out of share/doc/html.
    * Use Pango for printing.
    * New languages specs for Ruby, LUA, Perl's POD, VB.NET, Haskell and
      Texinfo.
    * Bug fixes and updated translations.

--- x11/gtksourceview/buildlink3.mk
+++ x11/gtksourceview/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/04/01 18:35:00 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/09/21 16:59:47 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GTKSOURCEVIEW_BUILDLINK3_MK:=	${GTKSOURCEVIEW_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngtksourceview}
 BUILDLINK_PACKAGES+=	gtksourceview
 
 .if !empty(GTKSOURCEVIEW_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gtksourceview+=	gtksourceview>=1.0.0
+BUILDLINK_DEPENDS.gtksourceview+=	gtksourceview>=1.1.0
 BUILDLINK_PKGSRCDIR.gtksourceview?=	../../x11/gtksourceview
 .endif	# GTKSOURCEVIEW_BUILDLINK3_MK
 
