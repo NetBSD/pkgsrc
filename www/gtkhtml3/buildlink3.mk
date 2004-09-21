9cb9d47e83a6c6b853eb977dd1eff6979b50ff2d
jmmv <jmmv@pkgsrc.org>
2004-09-21 16:52:41 +0000

    Update to 3.2.1.  This version corresponds to GNOME 2.8.0.
    gtkhtml-3.2.1 "Gone stable" 2004-09-10
    ------------------------------------------------
    
    New in this release
    
       * Updated translations:
    	el (Nikos Charonitakis)
    	cs (Miloslav Trmac)
    	it (Luca Ferretti)
    	ta (Jayaradha)
    	ms (Hasbullah Bin Pit)
    	sq (Laurent Dhima)
    
    gtkhtml-3.2.0 "Going stable" 2004-08-26
    ------------------------------------------------
    
    New in this release
    
       * make sure backstore pixmap has a colormap (Robert McQueen)
    
    New in 3.1.20
    
       * optionally use new gtk file chooser
       * fixed table layout calculation
       * fixed embedded widgets width
       * do not use title from MOZILLA_URL in text mode
       * fixed table cell cursor
       * a11y fixes (Eric Zhao)
       * fixed style setting in editor
    
    New in 3.1.19
    
       * fixed crash in embedded widgets code
       * auto* files fixes (release string vs. api version)
       * better size request/allocation handling for child widgets
       * memory leak fix
    
    New in 3.1.18
    
       * save TT tag again
    
    New in 3.1.17
    
       * fixed links editing
       * fixed problems with gnome print and &nbsp;'s
       * fixed crash when pasting html with embedded widgets
       * display link attributes in plain text
       * fixed cursor movement
       * set iframe's base url before emitting iframe_created signal
       * disable focus forwarding in editor (Eric Zhao)
       * fixed MOZILLA_URL pasting again
       * fixed leaking pango glyphs (Owen Taylor)
       * removed line breaks around puntuation in links
       * caret is now consistent with focus (Eric Zhao)
       * more compiler warnings fixes (Owen Taylor)
    
    New in 3.1.16
    
       * compiler warnings fixes
       * font settings fixes
       * fixed pasting MOZILLA_URL crash
       * added missing KP_Home, KP_End bindings
    
    New in 3.1.15
    
       * fixed printing
       * fixed page color editing
       * fixed table editing
       * fixed local image url's
       * optimalizations
       * colorset fixes
       * fixed crash related to displaying spell errors
       * a11y fixes (Eric Zhao)
       * search dialog fixes (Pratik V. Parikh)
       * line breaking fixes in links
       * make Wrap Lines menu item usable again
       * fixed line breaking
       * use GIT
       * HIGifications
    
    New in 3.1.5
    
       * improved IM preedit string display
       * removed gal dependency
       * better smiley substitution in the composer (bounty by Jan Arne Petersen)
    
    New in 3.1.5
    
       * better font sizing
       * updated translations.
    
    New in 3.1.4
    
       * new and improved html parser handles broken html much better.
       * better pango integration.
       * updated translations.
       * better ascii mode rendering of lists.
       * improved exposure handling.
       * improved animation rendering.
       * versioned .server files for parallel installs with version 3.0.x.

--- www/gtkhtml3/buildlink3.mk
+++ www/gtkhtml3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/03/26 02:27:57 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/09/21 16:52:41 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GTKHTML3_BUILDLINK3_MK:=	${GTKHTML3_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngtkhtml3}
 BUILDLINK_PACKAGES+=	gtkhtml3
 
 .if !empty(GTKHTML3_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gtkhtml3+=	gtkhtml3>=3.0.9nb5
-BUILDLINK_RECOMMENDED.gtkhtml3?=	gtkhtml3>=3.0.9nb6
+BUILDLINK_DEPENDS.gtkhtml3+=	gtkhtml3>=3.2.1
 BUILDLINK_PKGSRCDIR.gtkhtml3?=	../../www/gtkhtml3
 .endif	# GTKHTML3_BUILDLINK3_MK
 
