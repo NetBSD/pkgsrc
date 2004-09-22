d1ff4bf5c743f9c8376e7e189469fd39184a327d
jlam <jlam@pkgsrc.org>
2004-09-22 07:38:52 +0000

    Remove libimapuw.* symlinks, which wouldn't work on a.out platforms anyway.  Also convert to use LIBTOOLIZE_PLIST while we're here.  Bump the PKGREVISION.

--- mail/imap-uw/buildlink3.mk
+++ mail/imap-uw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/05/23 00:58:25 kim Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/09/22 07:38:52 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 IMAP_UW_BUILDLINK3_MK:=	${IMAP_UW_BUILDLINK3_MK}+
@@ -13,6 +13,11 @@ BUILDLINK_PACKAGES+=	imap-uw
 .if !empty(IMAP_UW_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.imap-uw+=	imap-uw>=2004
 BUILDLINK_PKGSRCDIR.imap-uw?=	../../mail/imap-uw
+#
+# Make "-limapuw" resolve into "-lc-client".
+#
+BUILDLINK_TRANSFORM+=		l:imapuw:c-client
+
 .endif	# IMAP_UW_BUILDLINK3_MK
 
 .include "../../security/openssl/buildlink3.mk"
