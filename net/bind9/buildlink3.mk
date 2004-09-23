97a9d370519e9401562756ba65d4cedf6f4d5562
jlam <jlam>
2004-09-23 04:33:21 +0000

    Favor using BUILDLINK_INCDIRS.<pkg> instead of BUILDLINK_CPPFLAGS.<pkg> to add directories to the header search path.  Also, use BUILDLINK_LDADD.<pkg>, which currently doesn't do anything yet, but will some time soon.

--- net/bind9/buildlink3.mk
+++ net/bind9/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/09/22 20:48:44 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/09/23 04:33:21 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 BIND_BUILDLINK3_MK:=	${BIND_BUILDLINK3_MK}+
@@ -15,8 +15,9 @@ BUILDLINK_DEPENDS.bind+=	bind>=9.2.2
 BUILDLINK_RECOMMENDED.bind?=	bind>=9.2.3nb4
 BUILDLINK_PKGSRCDIR.bind?=	../../net/bind9
 
-BUILDLINK_LDFLAGS.bind=		-lbind
-BUILDLINK_TRANSFORM.bind=	-e "s|include/bind/|include/|g"
+BUILDLINK_INCDIRS.bind?=	include/bind
+BUILDLINK_LDADD.bind?=		-lbind
+BUILDLINK_LDFLAGS.bind?=	${BUILDLINK_LDADD.bind}
 .endif	# BIND_BUILDLINK3_MK
 
 .include "../../security/openssl/buildlink3.mk"
