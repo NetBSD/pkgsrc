7136777f41bba450936a03b04c51e96a728bc176
rh <rh>
2004-09-24 01:56:53 +0000

    Update gnustep-base to 1.10.0. Bump PKGREVISION and BUILDLINK_RECOMMENDED for dependent packages due to library major revision bump.
    Noteworthy changes in gnustep-base-1.10.0:
    ==========================================
    
    Note the interface version of the library has changed so that apps,
    tools and libraries the use the base library need to be recompiled to
    use it.
    
       * A large majority of the library classes have been documented
         thanks to the efforts of Andrew Ruder
    
       * URL classes support persistant connections
    
       * Mac OSX XML compatibility fixes.

--- x11/gtoolkit/buildlink3.mk
+++ x11/gtoolkit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/04/17 00:28:14 rh Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/09/24 01:56:54 rh Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GTOOLKIT_BUILDLINK3_MK:=	${GTOOLKIT_BUILDLINK3_MK}+
@@ -12,6 +12,7 @@ BUILDLINK_PACKAGES+=	gtoolkit
 
 .if !empty(GTOOLKIT_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gtoolkit+=	gtoolkit>=0.9.5nb5
+BUILDLINK_RECOMMENDED.gtoolkit+=gtoolkit>=0.9.5nb6
 BUILDLINK_PKGSRCDIR.gtoolkit?=	../../x11/gtoolkit
 .endif	# GTOOLKIT_BUILDLINK3_MK
 
