b186f0c4bd460d62806662897548f8e9a52d526e
rh <rh>
2004-09-24 01:56:53 +0000

    Update gnustep-base to 1.10.0. Bump PKGREVISION and BUILDLINK_RECOMMENDED for dependent packages due to library major revision bump.
    Noteworthy changes in gnustep-base-1.10.0:
    ==========================================
    
    Note the interface version of the library has changed so that apps,
    tools and libraries the use the base library need to be recompiled to
    use it.
    
       * A large majority of the library classes have been documented
         thanks to the efforts of Andrew Ruder
    
       * URL classes support persistant connections
    
       * Mac OSX XML compatibility fixes.

--- graphics/GeometryKit/buildlink3.mk
+++ graphics/GeometryKit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/05/05 03:36:24 snj Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/09/24 01:56:54 rh Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GEOMETRYKIT_BUILDLINK3_MK:=	${GEOMETRYKIT_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	GeometryKit
 
 .if !empty(GEOMETRYKIT_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.GeometryKit+=	GeometryKit>=0.3.1r2nb3
-BUILDLINK_RECOMMENDED.GeometryKit?=	GeometryKit>=0.3.1r2nb5
+BUILDLINK_RECOMMENDED.GeometryKit?=	GeometryKit>=0.3.1r2nb6
 BUILDLINK_PKGSRCDIR.GeometryKit?=	../../graphics/GeometryKit
 .endif	# GEOMETRYKIT_BUILDLINK3_MK
 
