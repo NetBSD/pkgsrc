0e03eff09192bf211b0a86000ab2999850bc04a2
rh <rh@pkgsrc.org>
2004-09-24 01:58:53 +0000

    Update gnustep-ssl to 1.10.0.  No major changes, this just syncs the library changes for gnustep-base 1.10.0.

--- security/gnustep-ssl/buildlink3.mk
+++ security/gnustep-ssl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/04/11 00:08:11 rh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/09/24 01:58:53 rh Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNUSTEP_SSL_BUILDLINK3_MK:=	${GNUSTEP_SSL_BUILDLINK3_MK}+
@@ -12,6 +12,7 @@ BUILDLINK_PACKAGES+=	gnustep-ssl
 
 .if !empty(GNUSTEP_SSL_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gnustep-ssl+=		gnustep-ssl>=1.9.1
+BUILDLINK_RECOMMENDED.gnustep-ssl+=		gnustep-ssl>=1.10.0
 BUILDLINK_PKGSRCDIR.gnustep-ssl?=	../../security/gnustep-ssl
 .endif	# GNUSTEP_SSL_BUILDLINK3_MK
 
