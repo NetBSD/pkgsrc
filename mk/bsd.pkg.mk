4bee2600f956ad964d682ae8855942e15424eba5
tv <tv@pkgsrc.org>
2004-10-04 19:48:09 +0000

    Fix completely hosed test for g++ when CXX=g++.  The old test worked fine. Revert until the hosage is tracked down and eliminated at the source.
    (Bump to libtool-1.5.10nb1, and make this the required version.)

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1506 2004/10/03 04:24:20 hira Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1507 2004/10/04 19:48:09 tv Exp $
 #
 # This file is in the public domain.
 #
@@ -390,7 +390,7 @@ CONFIGURE_ENV+=		install_sh=${INSTALL:Q}
 .if defined(_OPSYS_LIBTOOL_REQD)
 LIBTOOL_REQD=		${_OPSYS_LIBTOOL_REQD}
 .else
-LIBTOOL_REQD?=		1.5.10
+LIBTOOL_REQD?=		1.5.10nb1
 .endif
 #
 # PKG_LIBTOOL is the path to the libtool script installed by libtool-base.
