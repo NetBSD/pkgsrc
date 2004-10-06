6640c0f5689f342a086afe529c9fbca4b5090b55
jlam <jlam@pkgsrc.org>
2004-10-06 20:51:47 +0000

    Reorganize some of the files under pkgsrc/mk:
        (1) defs.${OPSYS}.mk --> platform/${OPSYS}.mk.
    
    The "platform" subdirectory is where all of the ${OPSYS}-specific
    infrastructure logic should reside.
    
        (2) bsd.pkg.defaults.mk --> defaults/mk.conf
            bsd.pkg.obsolete.mk --> defaults/obsolete.mk
    
    Renaming bsd.pkg.defaults.mk to defaults/mk.conf is to mimic the way
    that NetBSD has /etc/rc.conf as well as /etc/defaults/rc.conf, where
    the latter is a full list of user-settable variables, and the two
    files share the same name to reinforce the fact /etc/defaults/rc.conf
    can be directly copied in place as /etc/rc.conf.  This is the same
    relationship shared by defaults/mk.conf and /etc/mk.conf.

--- mk/platform/BSDOS.mk
+++ mk/platform/BSDOS.mk
@@ -1,6 +1,6 @@
-# $NetBSD: defs.AIX.mk,v 1.12 2004/08/27 06:29:09 jlam Exp $
+# $NetBSD: BSDOS.mk,v 1.1 2004/10/06 20:51:47 jlam Exp $
 #
-# Variable definitions for the AIX operating system.
+# Variable definitions for the BSD/OS operating system.
 
 AWK?=		/usr/bin/awk
 BASENAME?=	/usr/bin/basename
@@ -10,64 +10,57 @@ CHOWN?=		/usr/sbin/chown
 CHGRP?=		/usr/bin/chgrp
 CMP?=		/usr/bin/cmp
 CP?=		/bin/cp
-CPP?=       ${LOCALBASE}/bin/cpp
+#CPP=		/usr/bin/cpp
+# BSD/OS doesn't have c++ command
+.if !exists(${CXX})
+CXX=		/usr/bin/g++
+.endif
 CUT?=		/usr/bin/cut
 DATE?=		/bin/date
 DC?=		/usr/bin/dc
 DIRNAME?=	/usr/bin/dirname
 ECHO?=		echo				# Shell builtin
 ECHO_N?=	${ECHO} -n
-.if exists(${LOCALBASE}/bin/egrep)
-EGREP?=		${LOCALBASE}/bin/egrep
-.else
-EGREP?=     /usr/bin/grep -E
-.endif
+EGREP?=		/bin/egrep
 EXPR?=		/bin/expr
 FALSE?=		false				# Shell builtin
-FETCH_CMD?= ${LOCALBASE}/bin/ftp
-.if exists(${LOCALBASE}/bin/fgrep)
-FGREP?=		${LOCALBASE}/bin/fgrep
-.else
-FGREP?=     /usr/bin/grep -F
-.endif
+FGREP?=		/bin/fgrep
 FILE_CMD?=	/usr/bin/file
 FIND?=		/usr/bin/find
+.if exists(${LOCALBASE}/bin/gmake)
 GMAKE?=		${LOCALBASE}/bin/gmake
-.if exists(${LOCALBASE}/bin/grep)
-GREP?=		${LOCALBASE}/bin/grep
 .else
-GREP?=		/usr/bin/grep
+GMAKE?=		/usr/contrib/bin/gmake
 .endif
-.if exists(/bin/tar)
+GREP?=		/bin/grep
+.if exists(${LOCALBASE}/bin/gtar)
+GTAR?=		${LOCALBASE}/bin/gtar
+.else
 GTAR?=		/bin/tar
-.else
-GTAR?=		/usr/bin/tar
 .endif
-GUNZIP_CMD?=	${LOCALBASE}/bin/gunzip -f
-GZCAT?=		${LOCALBASE}/bin/zcat
+GUNZIP_CMD?=	/usr/contrib/bin/gunzip -f
+GZCAT?=		/usr/contrib/bin/gzcat
 GZIP?=		-9
-GZIP_CMD?=	${LOCALBASE}/bin/gzip -nf ${GZIP}
+GZIP_CMD?=	/usr/contrib/bin/gzip -nf ${GZIP}
 HEAD?=		/usr/bin/head
 HOSTNAME_CMD?=	/bin/hostname
 ID?=		/usr/bin/id
-#.if ${INSTALL} == "install"
-INSTALL=   ${LOCALBASE}/bin/install-sh
-#.endif
 LDCONFIG?=	/sbin/ldconfig
 LN?=		/bin/ln
 LS?=		/bin/ls
 M4?=		/usr/bin/m4
-MAIL_CMD?=	/usr/bin/mailx
+MAIL_CMD?=	/usr/bin/mail
 MKDIR?=		/bin/mkdir -p
-MTREE?=		${LOCALBASE}/sbin/mtree
+.if exists(${LOCALBASE}/sbin/mtree)
+MTREE?=	${LOCALBASE}/sbin/mtree
+.else
+MTREE?=		/usr/sbin/mtree
+.endif
 MV?=		/bin/mv
 NICE?=		/usr/bin/nice
-PATCH?=		${LOCALBASE}/bin/gpatch
-.if exists(${LOCALBASE}/bin/pax)
-PAX?=       ${LOCALBASE}/bin/pax
-.else
-PAX?=		/bin/pax
-.endif
+PATCH?=		/usr/contrib/bin/patch
+# native /bin/pax doesn't support -O
+PAX?=		${LOCALBASE}/bin/pax
 PERL5?=		${LOCALBASE}/bin/perl
 PKGLOCALEDIR?=	share
 PS?=		/bin/ps
@@ -77,23 +70,17 @@ RMDIR?=		/bin/rmdir
 # XXX: default from bsd.pkg.defaults.mk.  Verify/corerct for this platform
 # and remove this comment.
 RSH?=		/usr/bin/rsh
-.if exists(${LOCALBASE}/bin/nbsed)
-SED?=		${LOCALBASE}/bin/nbsed
-.else
 SED?=		/usr/bin/sed
-.endif
 SETENV?=	/usr/bin/env
-SH?=		/bin/sh
-SHLOCK=		/usr/bin/shlock
+# native sh and ksh both have problems under BSD/OS 4.3.1
+# /usr/contrib/bin/bash is a symlink to /bin/bash
+SH?=		/bin/bash
+SHLOCK=		${LOCALBASE}/bin/shlock
 SORT?=		/usr/bin/sort
-# AIX strip fails too easily.  Use a wrapper script instead
-.if exists(${LOCALBASE}/bin/strip)
-STRIP?=     ${LOCALBASE}/bin/strip
-.endif
 SU?=		/usr/bin/su
 TAIL?=		/usr/bin/tail
-.if exists(${LOCALBASE}/bin/tar)
-TAR?=		${LOCALBASE}/bin/tar
+.if exists(/bin/tar)
+TAR?=		/bin/tar
 .else
 TAR?=		/usr/bin/tar
 .endif
@@ -103,36 +90,33 @@ TOUCH?=		/usr/bin/touch
 TR?=		/usr/bin/tr
 TRUE?=		true				# Shell builtin
 TSORT?=		/usr/bin/tsort
-TYPE?=		type				# Shell builtin
+# sh doesn't have built-in type
+TYPE?=		which
 WC?=		/usr/bin/wc
 XARGS?=		/usr/bin/xargs
 
-.if exists(/usr/sbin/user)
-USERADD?=	/usr/sbin/useradd
-GROUPADD?=	/usr/sbin/groupadd
-.else
-USERADD?=	${LOCALBASE}/sbin/useradd
-GROUPADD?=	${LOCALBASE}/sbin/groupadd
-_USER_DEPENDS=	user>=20000313:../../sysutils/user
-DEPENDS+=	${USE_USERADD:D${_USER_DEPENDS}}
-DEPENDS+=	${USE_GROUPADD:D${_USER_DEPENDS}}
-.endif
+USERADD?=	/usr/sbin/adduser
+GROUPADD?=	/usr/sbin/addgroup
 
 CPP_PRECOMP_FLAGS?=	# unset
 DEF_UMASK?=		0022
+.if ${OBJECT_FMT} == "ELF"
 EXPORT_SYMBOLS_LDFLAGS?=-Wl,-E	# add symbols to the dynamic symbol table
+.else
+EXPORT_SYMBOLS_LDFLAGS?=-Wl,--export-dynamic
+.endif
 MOTIF_TYPE_DEFAULT?=	openmotif	# default 2.0 compatible libs type
 NOLOGIN?=		/sbin/nologin
 PKG_TOOLS_BIN?=		${LOCALBASE}/sbin
 ROOT_CMD?=		${SU} - root -c
 ROOT_USER?=		root
-ROOT_GROUP?=	system
+ROOT_GROUP?=	wheel
 ULIMIT_CMD_datasize?=	ulimit -d `ulimit -H -d`
 ULIMIT_CMD_stacksize?=	ulimit -s `ulimit -H -s`
 ULIMIT_CMD_memorysize?=	ulimit -m `ulimit -H -m`
 
 # imake installs manpages in weird places
-# these values from /usr/X11R6/lib/X11/config/NetBSD.cf
+# XXX: assume NetBSD defaults until somebody determines correct values
 IMAKE_MAN_SOURCE_PATH=	man/cat
 IMAKE_MAN_SUFFIX=	1
 IMAKE_LIBMAN_SUFFIX=	3
@@ -142,22 +126,23 @@ IMAKE_LIBMAN_DIR=	${IMAKE_MAN_SOURCE_PATH}3
 IMAKE_FILEMAN_DIR=	${IMAKE_MAN_SOURCE_PATH}5
 IMAKE_MANNEWSUFFIX=	0
 
-_DO_SHLIB_CHECKS=	no	# fixup PLIST for shared libs/run ldconfig
-                 	  	# doesn't work on AIX b/c no ldd.
+_DO_SHLIB_CHECKS=	yes	# fixup PLIST for shared libs/run ldconfig
 _IMAKE_MAKE=		${MAKE}	# program which gets invoked by imake
 .if exists(/usr/include/netinet6)
 _OPSYS_HAS_INET6=	yes	# IPv6 is standard
 .else
 _OPSYS_HAS_INET6=	no	# IPv6 is not standard
 .endif
+# XXX need to check this for Java
 _OPSYS_HAS_JAVA=	no	# Java is not standard
 _OPSYS_HAS_MANZ=	yes	# MANZ controls gzipping of man pages
-_OPSYS_HAS_OSSAUDIO=	no	# libossaudio is available
+# XXX need to check this for libossaudio
+_OPSYS_HAS_OSSAUDIO=	yes	# libossaudio is available
 _OPSYS_PERL_REQD=		# no base version of perl required
 _OPSYS_PTHREAD_AUTO=	no	# -lpthread needed for pthreads
-_OPSYS_SHLIB_TYPE=	aixlib	# type of shared lib
+_OPSYS_SHLIB_TYPE=	ELF/a.out	# shared lib type
 _PATCH_CAN_BACKUP=	yes	# native patch(1) can make backups
-_PATCH_BACKUP_ARG?=	-b -V simple -z 	# switch to patch(1) for backup suffix
+_PATCH_BACKUP_ARG?=	-V simple -b -z	# switch to patch(1) for backup suffix
 _PREFORMATTED_MAN_DIR=	cat	# directory where catman pages are
 _USE_GNU_GETTEXT=	no	# Don't use GNU gettext
 _USE_RPATH=		yes	# add rpath to LDFLAGS
@@ -167,20 +152,15 @@ _USE_RPATH=		yes	# add rpath to LDFLAGS
 _OPSYS_WHOLE_ARCHIVE_FLAG=	-Wl,--whole-archive
 _OPSYS_NO_WHOLE_ARCHIVE_FLAG=	-Wl,--no-whole-archive
 
-# for programs which use dlopen()
-# not necessary since 1.6 (shared libs are linked against libgcc_pic)
-.if !empty(OS_VERSION:M1.5*)
-LINK_ALL_LIBGCC_HACK=	-Wl,--whole-archive -lgcc -Wl,--no-whole-archive
-.endif
-
 .if !defined(DEBUG_FLAGS)
 _STRIPFLAG_CC?=		-s	# cc(1) option to strip
 _STRIPFLAG_INSTALL?=	-s	# install(1) option to strip
 .endif
 
-DEFAULT_SERIAL_DEVICE?=	/dev/tty0
-SERIAL_DEVICES?=	/dev/tty0 \
-			/dev/tty
+DEFAULT_SERIAL_DEVICE?=	/dev/null
+SERIAL_DEVICES?=	/dev/null
+
+PKG_CREATE_USERGROUP=	NO # until it works
 
 # check for maximum command line length and set it in configure's environment,
 # to avoid a test required by the libtool script that takes forever.
--- mk/platform/SunOS.mk
+++ mk/platform/SunOS.mk
@@ -1,6 +1,6 @@
-# $NetBSD: defs.UnixWare.mk,v 1.8 2004/09/21 15:01:40 jlam Exp $
+# $NetBSD: SunOS.mk,v 1.1 2004/10/06 20:51:47 jlam Exp $
 #
-# Variable definitions for the UnixWare 7 operating system.
+# Variable definitions for the SunOS/Solaris operating system.
 
 AWK?=		/usr/bin/nawk
 BASENAME?=	/usr/bin/basename
@@ -8,22 +8,22 @@ CAT?=		/usr/bin/cat
 CHMOD?=		/usr/bin/chmod
 CHOWN?=		/usr/bin/chown
 CHGRP?=		/usr/bin/chgrp
-CMP?=		/usr/bin/cmp
+CMP?=		cmp
 CP?=		/usr/bin/cp
 CUT?=		/usr/bin/cut
-DATE?=		/usr/bin/date
+DATE?=		/usr/xpg4/bin/date
 DC?=		/usr/bin/dc
 DIRNAME?=	/usr/bin/dirname
 ECHO?=		/usr/ucb/echo
 ECHO_N?=	${ECHO} -n
-EGREP?=		/usr/bin/grep -E
-EXPR?=		/usr/bin/expr
+EGREP?=		/usr/xpg4/bin/grep -E
+EXPR?=		/usr/xpg4/bin/expr
 FALSE?=		/usr/bin/false
-FGREP?=		/usr/bin/grep -F
+FGREP?=		/usr/xpg4/bin/fgrep
 FILE_CMD?=	/usr/bin/file
 FIND?=		/usr/bin/find
 GMAKE?=		${LOCALBASE}/bin/gmake
-GREP?=		/usr/bin/grep
+GREP?=		/usr/xpg4/bin/grep
 .if exists(${LOCALBASE}/bin/gtar)
 GTAR?=		${LOCALBASE}/bin/gtar
 .else
@@ -34,31 +34,35 @@ GUNZIP_CMD?=	${LOCALBASE}/bin/gunzip -f
 GZCAT?=		${LOCALBASE}/bin/zcat
 GZIP?=		-9
 GZIP_CMD?=	${LOCALBASE}/bin/gzip -nf ${GZIP}
-.elif exists(/usr/local/bin/gzip)
-GUNZIP_CMD?=	/usr/local/bin/gzip -df
-GZCAT?=		/usr/local/bin/gzip -cd
+.elif exists(/usr/bin/gzip)
+GUNZIP_CMD?=	/usr/bin/gzip -df
+GZCAT?=		/usr/bin/gzip -cd
 GZIP?=		-9
-GZIP_CMD?=	/usr/local/bin/gzip -nf ${GZIP}
+GZIP_CMD?=	/usr/bin/gzip -nf ${GZIP}
 .endif
 HEAD?=		/usr/bin/head
-HOSTNAME_CMD?=	/usr/bin/hostname
-ID?=		/usr/bin/id
-IMAKE?=		# XXX only available in the SDK? what about xmkmf?
+HOSTNAME_CMD?=	/bin/hostname
+ID?=		/usr/xpg4/bin/id
+IMAKE?=		${X11BASE}/bin/imake ${IMAKEOPTS}
 LDCONFIG?=	/usr/bin/true
 LN?=		/usr/bin/ln
 LS?=		/usr/bin/ls
 M4?=		/usr/ccs/bin/m4
 MAIL_CMD?=	/usr/bin/mailx
-MKDIR?=		${LOCALBASE}/sbin/mkdir-sh -p
+MKDIR?=		/usr/bin/mkdir -p
 MTREE?=		${LOCALBASE}/sbin/mtree
 MV?=		/usr/bin/mv
-NICE?=		/usr/bin/nice
+NICE?=		/usr/xpg4/bin/nice
+.if exists(/usr/bin/gpatch)
+PATCH?=		/usr/bin/gpatch
+.else
 PATCH?=		${LOCALBASE}/bin/${GNU_PROGRAM_PREFIX}patch
+.endif
 PAX?=		${LOCALBASE}/bin/pax
 PERL5?=		${LOCALBASE}/bin/perl
 PKGLOCALEDIR?=	lib
-PS?=		/usr/bin/ps
-PWD_CMD?=	/usr/bin/pwd	# needs to print physical path
+PS?=		/bin/ps
+PWD_CMD?=	/bin/pwd	# needs to print physical path
 RM?=		/usr/bin/rm
 RMDIR?=		/usr/bin/rmdir
 # XXX: default from bsd.pkg.defaults.mk.  Verify/corerct for this platform
@@ -67,17 +71,17 @@ RSH?=		/usr/bin/rsh
 .if exists(${LOCALBASE}/bin/nbsed)
 SED?=		${LOCALBASE}/bin/nbsed
 .else
-SED?=		/usr/bin/sed
+SED?=		/usr/xpg4/bin/sed
 .endif
 SETENV?=	/usr/bin/env
-SH?=		/usr/bin/ksh
+SH?=		/bin/ksh
 SHLOCK=		${LOCALBASE}/bin/shlock
 SORT?=		/usr/bin/sort
 SU?=		/usr/bin/su
-TAIL?=		/usr/bin/tail
+TAIL?=		/usr/xpg4/bin/tail
 TAR?=		${LOCALBASE}/bin/tar
 TEE?=		/usr/bin/tee
-TEST?=		test				# Shell builtin
+TEST?=		test
 TOUCH?=		/usr/bin/touch
 TR?=		/usr/bin/tr
 TRUE?=		/usr/bin/true
@@ -114,7 +118,7 @@ IMAKE_MANNEWSUFFIX=	${IMAKE_MAN_SUFFIX}
 
 _DO_SHLIB_CHECKS=	yes		# fixup PLIST for shared libs
 _IMAKE_MAKE=	/usr/ccs/bin/make	# program which gets invoked by imake
-.if exists(/usr/include/netinet/in6.h)
+.if exists(/usr/include/netinet/ip6.h)
 _OPSYS_HAS_INET6=	yes		# IPv6 is standard
 .else
 _OPSYS_HAS_INET6=	no		# IPv6 is not standard
@@ -129,33 +133,60 @@ _PATCH_CAN_BACKUP=	yes		# native patch(1) can make backups
 _PATCH_BACKUP_ARG?= 	-b -V simple -z	# switch to patch(1) for backup suffix
 _PREFORMATTED_MAN_DIR=	man		# directory where catman pages are
 _USE_GNU_GETTEXT=	yes		# Use GNU gettext
-#
-# The native linker for UnixWare doesn't really support an option to pass
-# rpath directives, but pretend it does anyway since the wrapper scripts
-# will correctly convert it into the the proper LD_RUN_PATH variable.
-#
 _USE_RPATH=		yes		# add rpath to LDFLAGS
 
 # flags passed to the linker to extract all symbols from static archives.
 # this is the standard Solaris linker, /usr/ccs/bin/ld. The use of GNU
 # ld is not currently supported.
-# XXX is this needed for UnixWare?
-_OPSYS_WHOLE_ARCHIVE_FLAG=
-_OPSYS_NO_WHOLE_ARCHIVE_FLAG=
+_OPSYS_WHOLE_ARCHIVE_FLAG=	-z allextract
+_OPSYS_NO_WHOLE_ARCHIVE_FLAG=	-z defaultextract
 
-# UnixWare has /usr/include/iconv.h, but it's not GNU iconv, so mark it
+# Solaris has /usr/include/iconv.h, but it's not GNU iconv, so mark it
 # incompatible.
-_INCOMPAT_ICONV=	UnixWare-*-*
+_INCOMPAT_ICONV=	SunOS-*-*
 
-# XXX Where are those variables used?
 .if !defined(DEBUG_FLAGS)
-_STRIPFLAG_CC?=			# cc(1) option to strip
-_STRIPFLAG_INSTALL?=		# install(1) option to strip
+_STRIPFLAG_CC?=		-s	# cc(1) option to strip
+_STRIPFLAG_INSTALL?=	-s	# install(1) option to strip
+.endif
+
+# Migration aid for old /usr/local LOCALBASE
+.if !defined(LOCALBASE) && exists(${DESTDIR}/usr/local/libexec/cgi-bin) && \
+	!exists(${DESTDIR}/usr/pkg/libexec/cgi-bin)
+.BEGIN:
+	@echo "On Solaris and /usr/local/libexec/cgi-bin found:"
+	@echo "- If you have an existing pkgsrc installation & wish to continue"
+	@echo "  using /usr/local, append LOCALBASE=/usr/local to /etc/mk.conf."
+	@echo "- Otherwise set LOCALBASE=/usr/pkg in your environment for the"
+	@echo "  first package install."
+	@false
+.endif
+
+.if !exists(${SED}) || ${SED} == "/usr/xpg4/bin/sed"
+.BEGIN:
+	@echo ""
+	@echo "==========================================================================="
+	@echo ""
+	@echo "ERROR: pkgsrc now requires a more functional sed(1) than Solaris provides."
+	@echo "you can satisfy this requirement by running:"
+	@echo ""
+	@echo "    cd ${_PKGSRCDIR}/textproc/nbsed; ${MAKE} install"
+	@echo ""
+	@echo "this will install ${LOCALBASE}/bin/nbsed and eliminate this message."
+	@echo "(nbsed is already installed by recent bootstrap-pkgsrc)."
+	@echo ""
+	@echo "==========================================================================="
+	@echo ""
+	@false
 .endif
 
 LOCALBASE?=		${DESTDIR}/usr/pkg
 
+.if exists(${LOCALBASE}/sbin/pkg_info)
 PKG_TOOLS_BIN?=		${LOCALBASE}/sbin
+.else
+PKG_TOOLS_BIN?=		${LOCALBASE}/bin
+.endif
 
 # check for maximum command line length and set it in configure's environment,
 # to avoid a test required by the libtool script that takes forever.
