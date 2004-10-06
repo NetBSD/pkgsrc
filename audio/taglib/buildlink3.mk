289e75dbac7b095ca88ae82d9d5e9cfb222b088d
chris <chris@pkgsrc.org>
2004-10-06 00:09:02 +0000

    Update taglib to 1.3.  Main changes from 1.2 to 1.3 are: Added support for APE tags Added support for MPC files Added support for APE tags in MP3 files Added support for compressed ID3v2 frames Added support for uniuque file identifier frames in ID3v2 Added checks for several potential divide by zero (SIGFPE) cases Fixed support for custom text frames
    Fixes PR 27061 (basically applied the patch in PR, but added the change to
    buildlink3.mk and removed LIBTOOLIZE_PLIST option from Makefile.)

--- audio/taglib/buildlink3.mk
+++ audio/taglib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/10/03 00:13:09 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2004/10/06 00:09:02 chris Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 TAGLIB_BUILDLINK3_MK:=	${TAGLIB_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	taglib
 
 .if !empty(TAGLIB_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.taglib+=	taglib>=1.0
-BUILDLINK_RECOMMENDED.taglib+=	taglib>=1.2nb1
+BUILDLINK_RECOMMENDED.taglib+=	taglib>=1.3
 BUILDLINK_PKGSRCDIR.taglib?=	../../audio/taglib
 .endif	# TAGLIB_BUILDLINK3_MK
 
