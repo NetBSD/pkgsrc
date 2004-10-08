ac5edb93378442c4e41e8138866ea3f37a8fed03
adam <adam@pkgsrc.org>
2004-10-08 13:19:26 +0000

    Changes: 0.9.6  20040430  Compilation fixes for Solaris                  Compilation fixes for ImageMagick unavailability
    0.9.5  20040406  Bug fixes
                     Compilation with gcc 2.95.x should work again
    
    0.9.4  20040316  Mostly bug fixes
                     Minimum version required for AbiWord 2.1.1 and later
    
    0.9.3  20040226  Minimum version required for new AbiWord 2.1 plugin
                     Fixed a few minor bugs
                     Added psiconv_unicode_from_list and psiconv_unicode_strstr
                     Much better error and progress reporting in generators
    
    0.9.2.1 20040223 Quick fix for the library number mistake in 0.9.2
    
    0.9.2  20040223  Many bugfixes, among them some critical ones:
                     * Package now builds without psiconv.conf files installed
                     UTF8 Psion support might now actually work
                     -c argument introduced for psiconv program
    
    0.9.1  20040209  Added the psiconv-config program.
    		 Fixed the installation of @includedir@/psiconv/general.h
                     Added the forgotted psiconv_config_free function.
    		 Removed some small memory leaks.
    
    0.9.0  20040204  New major library version number breaks source compatibility:
    		 * All strings and characters are now stored in UCS-2
                     * New config structure.
                       - Call psiconv_config_default and psiconv_config_read at the
                         start of your program
                       - Almost all functions have a new psiconv_config parameter
                       - All former global variables are now in this structure
                     * Several minor changes in the functions defined in
                       parse_routines.h and generate_routines.h
                     * All generate_* routines have now a lev parameter except
                       generate_file.
                     * psiconv_sketch_section data representation changes
                     * New verbosity level PSICONV_VERB_ERROR
                     Use config files /etc/psiconv.conf and ~/.psiconv.conf
                     Support automake 1.6, 1.7 and 1.8
                     Support autoconf-2.50 and up
                     Support ImageMagick 5.4.x and 5.5.x (API changed once again...)
                     Support Unicode output in psiconv program (Keita Kawabe)
                     Don't use features not present in plain sh (Keita Kawabe)
                     Clean up some automake-related stuff.
    		 Format documentation is now installed too.
                     Debian build support
                     Embedded objects in Word are now properly parsed and generated
                     Sketch file generation is added (stand-alone and as object)
                     MBM and Clipart file generation is added
                     Image files other than 2-bits greyscale are parsed and
                       generated (experimental)
                     Errors and warnings sanitized
                     Rewrite of the psiconv program:
                     * Outputs in UTF8, UCS2, ASCII or Psion encodings
    		 * XHTML target generates strict XHTML using CSS
                     * HTML4 target generates traditional HTML without CSS
                     * IMAGE target remains the same
                     * LATEX and RTF targets are not supported anymore

--- converters/psiconv/buildlink3.mk
+++ converters/psiconv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:13:17 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/10/08 13:19:26 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PSICONV_BUILDLINK3_MK:=	${PSICONV_BUILDLINK3_MK}+
@@ -15,7 +15,5 @@ BUILDLINK_DEPENDS.psiconv+=	psiconv>=0.8.3nb1
 BUILDLINK_RECOMMENDED.psiconv+=	psiconv>=0.8.3nb2
 BUILDLINK_PKGSRCDIR.psiconv?=	../../converters/psiconv
 .endif	# PSICONV_BUILDLINK3_MK
-
-.include "../../graphics/ImageMagick/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
