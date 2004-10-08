1750bbdea6ecb9f7be8fdaa554953d1db2e46c42
xtraeme <xtraeme@pkgsrc.org>
2004-10-08 04:44:52 +0000

    Check for the .pc files presence...

--- x11/compositeext/builtin.mk
+++ x11/compositeext/builtin.mk
@@ -1,11 +1,13 @@
-# $NetBSD: builtin.mk,v 1.2 2004/10/08 04:02:35 xtraeme Exp $
+# $NetBSD: builtin.mk,v 1.3 2004/10/08 04:44:52 xtraeme Exp $
 
-_COMPOSITE_H=	${X11BASE}/include/X11/extensions/composite.h
+_COMPOSITE_H=		${X11BASE}/include/X11/extensions/composite.h
 _COMPOSITE_PROTO_H=	${X11BASE}/include/X11/extensions/compositeproto.h
+_COMPOSITEEXT_PC=	${X11BASE}/lib/X11/pkgconfig/compositeext.pc
 
 .if !defined(IS_BUILTIN.compositeext)
 IS_BUILTIN.compositeext=     no
-.  if exists(${_COMPOSITE_H}) && exists(${_COMPOSITE_PROTO_H})
+.  if exists(${_COMPOSITE_H}) && exists(${_COMPOSITE_PROTO_H}) && \
+      exists(${_COMPOSITEEXT_PC})
 IS_BUILTIN.compositeext=     yes
 .  endif
 .endif  # IS_BUILTIN.compositeext
