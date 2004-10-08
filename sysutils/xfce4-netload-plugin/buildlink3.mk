e89964ea5d7b090269c4e72c8ca93318d40bcb0a
martti <martti@pkgsrc.org>
2004-10-08 06:21:52 +0000

    Updated xfce4-netload-plugin to 0.2.3
    * Bug fixes

--- sysutils/xfce4-netload-plugin/buildlink3.mk
+++ sysutils/xfce4-netload-plugin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/10/03 00:18:17 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/10/08 06:21:52 martti Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_NETLOAD_PLUGIN_BUILDLINK3_MK:=	${XFCE4_NETLOAD_PLUGIN_BUILDLINK3_MK}+
@@ -11,8 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-netload-plugin}
 BUILDLINK_PACKAGES+=	xfce4-netload-plugin
 
 .if !empty(XFCE4_NETLOAD_PLUGIN_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.xfce4-netload-plugin+=	xfce4-netload-plugin>=0.2.2nb4
-BUILDLINK_RECOMMENDED.xfce4-netload-plugin+=	xfce4-netload-plugin>=0.2.2nb5
+BUILDLINK_DEPENDS.xfce4-netload-plugin+=	xfce4-netload-plugin>=0.2.3
+BUILDLINK_RECOMMENDED.xfce4-netload-plugin+=	xfce4-netload-plugin>=0.2.3
 BUILDLINK_PKGSRCDIR.xfce4-netload-plugin?=	../../sysutils/xfce4-netload-plugin
 .endif	# XFCE4_NETLOAD_PLUGIN_BUILDLINK3_MK
 
