8342268de747f779fa48061f6741e01faf0df0eb
xtraeme <xtraeme@pkgsrc.org>
2004-10-08 05:24:46 +0000

    Check for pkgconfig files (.pc) presence.

--- x11/Xcomposite/builtin.mk
+++ x11/Xcomposite/builtin.mk
@@ -1,10 +1,11 @@
-# $NetBSD: builtin.mk,v 1.2 2004/10/08 04:01:49 xtraeme Exp $
+# $NetBSD: builtin.mk,v 1.3 2004/10/08 05:24:46 xtraeme Exp $
 
 _XCOMPOSITE_H=	${X11BASE}/include/X11/extensions/xcomposite.h
+_XCOMPOSITE_PC=	${X11BASE}/lib/X11/pkgconfig/xcomposite.pc
 
 .if !defined(IS_BUILTIN.Xcomposite)
 IS_BUILTIN.Xcomposite=     no
-.  if exists(${_XCOMPOSITE_H})
+.  if exists(${_XCOMPOSITE_H}) && exists(${_XCOMPOSITE_PC})
 IS_BUILTIN.Xcomposite=     yes
 .  endif
 .endif  # IS_BUILTIN.Xcomposite
--- x11/Xfixes/builtin.mk
+++ x11/Xfixes/builtin.mk
@@ -1,10 +1,11 @@
-# $NetBSD: builtin.mk,v 1.1 2004/09/21 17:42:21 xtraeme Exp $
+# $NetBSD: builtin.mk,v 1.2 2004/10/08 05:24:46 xtraeme Exp $
 
 _H_XFIXES=		${X11BASE}/include/X11/extensions/Xfixes.h
+_FIXESEXT_PC=		${X11BASE}/lib/X11/pkgconfig/xfixes.pc
 
 .if !defined(IS_BUILTIN.Xfixes)
 IS_BUILTIN.Xfixes=     no
-.  if exists(${_H_XFIXES})
+.  if exists(${_H_XFIXES}) && exists(${_FIXESEXT_PC})
 IS_BUILTIN.Xfixes=     yes
 .  endif
 .endif  # IS_BUILTIN.Xfixes
