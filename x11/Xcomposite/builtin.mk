ff2c5a96cc840620748dbb94b82ae9136e4ac7b6
xtraeme <xtraeme>
2004-10-08 04:01:49 +0000

    typo

--- x11/Xcomposite/builtin.mk
+++ x11/Xcomposite/builtin.mk
@@ -1,10 +1,10 @@
-# $NetBSD: builtin.mk,v 1.1 2004/10/08 03:59:36 xtraeme Exp $
+# $NetBSD: builtin.mk,v 1.2 2004/10/08 04:01:49 xtraeme Exp $
 
-_COMPOSITE_H=	${X11BASE}/include/X11/extensions/xcomposite.h
+_XCOMPOSITE_H=	${X11BASE}/include/X11/extensions/xcomposite.h
 
 .if !defined(IS_BUILTIN.Xcomposite)
 IS_BUILTIN.Xcomposite=     no
-.  if exists(${_COMPOSITE_H})
+.  if exists(${_XCOMPOSITE_H})
 IS_BUILTIN.Xcomposite=     yes
 .  endif
 .endif  # IS_BUILTIN.Xcomposite
