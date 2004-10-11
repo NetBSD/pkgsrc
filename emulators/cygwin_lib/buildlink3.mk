57258cca0d96fc1f3632ea9e1ef3d6604eebf785
tv <tv>
2004-10-11 20:08:11 +0000

    cygwin_lib, a pkgsrc-izing of the barebones Cygwin DLL.  This provides a framework for some Win32 gateway services for Interix.

---
+++ emulators/cygwin_lib/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/10/11 20:08:11 tv Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+CYGWIN_LIB_BUILDLINK3_MK:=	${CYGWIN_LIB_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	cygwin_lib
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ncygwin_lib}
+BUILDLINK_PACKAGES+=	cygwin_lib
+
+.if !empty(CYGWIN_LIB_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.cygwin_lib+=		cygwin_lib>=1.5.11.1
+BUILDLINK_PKGSRCDIR.cygwin_lib?=	../../emulators/cygwin_lib
+.endif	# CYGWIN_LIB_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
