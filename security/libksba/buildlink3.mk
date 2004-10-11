d8c994c604e035d67252f955f3cc9d2289c30b3f
shannonjr <shannonjr@pkgsrc.org>
2004-10-11 09:46:44 +0000

    1) Taking over maintainance of package on agreement with previous    maintainer Klaus Klein. 2) Update to version 0.9.7 to satisfy version requirements for, soon to    be committed, gnupg2 (1.9.10) that provides SMIME support. Libksba is a library to make the tasks of working with X.509 certificates, CMS data and related objects more easy. It a highlevel interface to the implemented protocols and presents the data in a consistent way.

--- security/libksba/buildlink3.mk
+++ security/libksba/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/10/03 00:18:09 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2004/10/11 09:46:44 shannonjr Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBKSBA_BUILDLINK3_MK:=	${LIBKSBA_BUILDLINK3_MK}+
@@ -11,8 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibksba}
 BUILDLINK_PACKAGES+=	libksba
 
 .if !empty(LIBKSBA_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libksba+=	libksba>=0.9.4
-BUILDLINK_RECOMMENDED.libksba+=	libksba>=0.9.4nb1
+BUILDLINK_DEPENDS.libksba+=	libksba>=0.9.7
+BUILDLINK_RECOMMENDED.libksba+=	libksba>=0.9.7
 BUILDLINK_PKGSRCDIR.libksba?=	../../security/libksba
 .endif	# LIBKSBA_BUILDLINK3_MK
 
