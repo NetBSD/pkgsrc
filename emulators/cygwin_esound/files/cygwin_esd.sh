59495732ebc739c7af89ba2fe3b6fac566f43ced
tv <tv>
2004-10-11 21:02:53 +0000

    Don't set "procname" (developer's holdover).

--- emulators/cygwin_esound/files/cygwin_esd.sh
+++ emulators/cygwin_esound/files/cygwin_esd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: cygwin_esd.sh,v 1.1.1.1 2004/10/11 20:14:35 tv Exp $
+# $NetBSD: cygwin_esd.sh,v 1.2 2004/10/11 21:02:53 tv Exp $
 #
 # PROVIDE: cygwin_esd
 # REQUIRE: DAEMON
@@ -8,7 +8,6 @@
 name="cygwin_esd"
 command="@PREFIX@/emul/cygwin/bin/esd.exe"
 command_args="-tcp"
-procname="esd.exe"
 
 if [ ! -d /etc/rc.d ]
 then
