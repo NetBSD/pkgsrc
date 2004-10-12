a2febe816acb9d0f9b5d50c6088889066fc86fa0
jmmv <jmmv@pkgsrc.org>
2004-10-12 08:15:35 +0000

    Recreate the VARBASE/run/dbus directory before starting the daemon, in case it does not exist; this directory is removed at boot time by another script. Also, don't bother to create it with an OWN_DIRS_PERMS line, because the rc.d script will do it for us. Bump PKGREVISION to 1.

--- sysutils/dbus/files/dbus.sh
+++ sysutils/dbus/files/dbus.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: dbus.sh,v 1.1.1.1 2004/10/11 17:45:27 jmmv Exp $
+# $NetBSD: dbus.sh,v 1.2 2004/10/12 08:15:35 jmmv Exp $
 #
 # PROVIDE: dbus
 # REQUIRE: DAEMON
@@ -12,10 +12,18 @@ name="dbus"
 rcvar=$name
 command="@PREFIX@/bin/dbus-daemon-1"
 command_args="--system"
-required_dirs="@VARBASE@/run/dbus"
 pidfile="@VARBASE@/run/dbus/pid"
+start_precmd=dbus_prestart
 dbus_user=messagebus
 dbus_group=messagebus
+
+dbus_prestart() {
+	if @TEST@ ! -d "@VARBASE@/run/dbus"; then
+		@MKDIR@ "@VARBASE@/run/dbus"
+		@CHMOD@ 0755 "@VARBASE@/run/dbus"
+		@CHOWN@ ${dbus_user}:${dbus_group} "@VARBASE@/run/dbus"
+	fi
+}
 
 load_rc_config $name
 run_rc_command "$1"
