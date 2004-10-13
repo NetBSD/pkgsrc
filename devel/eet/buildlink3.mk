6a3d535d5a3e3e5aba1bcac83267c7b8f212fcf2
minskim <minskim@pkgsrc.org>
2004-10-13 08:57:55 +0000

    Import eet from pkgsrc-wip.  Packaged by Peter Bex and modified by me.
    EET is a tiny library designed to write an arbitrary set of chunks of
    data to a file and optionally compress each chunk (very much like a
    zip file) and allow fast random-access reading of the file later on.
    It does not do zip as a zip itself has more complexity than is needed,
    and it was much simpler to implement this once here.
    
    EET is extremely fast, small and simple.  EET files can be very small
    and highly compressed, making them very optimal for just sending
    across the Internet without having to archive, compress or decompress
    and install them.  They allow for lightning-fast random-access reads
    once created, making them perfect for storing data that is written
    once (or rarely) and read many times, but the program does not want to
    have to read it all in at once.
    
    It also can encode and decode data structures in memory, as well as
    image data for saving to EET files or sending across the network to
    other machines, or just writing to arbitrary files on the system.  All
    data is encoded in a platform independent way and can be written and
    read by any architecture.

---
+++ devel/eet/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/10/13 08:57:55 minskim Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+EET_BUILDLINK3_MK:=	${EET_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	eet
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Neet}
+BUILDLINK_PACKAGES+=	eet
+
+.if !empty(EET_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.eet+=	eet>=0.9.9
+BUILDLINK_PKGSRCDIR.eet?=	../../wip/eet
+.endif	# EET_BUILDLINK3_MK
+
+.include "../../devel/libtool/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
