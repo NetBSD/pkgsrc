a9bc326932d7142e5ff7ede5ffd5f30b49d8df82
tv <tv@pkgsrc.org>
2004-10-15 12:14:07 +0000

    Update linkage to libltdl, now in its own package.

--- net/gift/buildlink3.mk
+++ net/gift/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:17:51 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/10/15 12:14:07 tv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GIFT_BUILDLINK3_MK:=	${GIFT_BUILDLINK3_MK}+
@@ -16,7 +16,7 @@ BUILDLINK_RECOMMENDED.gift+=	gift>=0.11.6nb2
 BUILDLINK_PKGSRCDIR.gift?=	../../net/gift
 .endif	# GIFT_BUILDLINK3_MK
 
-.include "../../devel/libtool/buildlink3.mk"
+.include "../../devel/libltdl/buildlink3.mk"
 .include "../../audio/libvorbis/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
