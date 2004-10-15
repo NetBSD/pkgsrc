0cfe13a2294ea1cf46b16c61ddb505a9e26d1ea2
tv <tv@pkgsrc.org>
2004-10-15 14:50:05 +0000

    Update linkage to libltdl (via guile{,14}), which is now its own package.
    Unfortunately, guile{,14}/buildlink3.mk directly includes it, and I don't
    know which dependencies actually need libltdl, so it was a recursive bump.
    Hopefully this recursive inclusion can be ripped out of
    guile{,14}/buildlink3.mk at some point and bubble down to dependencies that
    actually use libltdl, avoiding this headache in the future....

--- math/libmatheval/buildlink3.mk
+++ math/libmatheval/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:12:56 tv Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.4.
-# XXX After this file as been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink[23].mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.libmatheval?=	build
+# $NetBSD: buildlink3.mk,v 1.3 2004/10/15 14:50:07 tv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBMATHEVAL_BUILDLINK3_MK:=	${LIBMATHEVAL_BUILDLINK3_MK}+
