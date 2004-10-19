44b2dfcbbe435e0c862b64073084ee533c53598e
grant <grant@pkgsrc.org>
2004-10-19 10:36:54 +0000

    xvidcore is i386 only, so use it if we are on i386.

--- multimedia/xvidcore/buildlink3.mk
+++ multimedia/xvidcore/buildlink3.mk
@@ -1,7 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/04/05 18:41:14 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/10/19 10:36:54 grant Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XVIDCORE_BUILDLINK3_MK:=	${XVIDCORE_BUILDLINK3_MK}+
+
+.include "../../mk/bsd.prefs.mk"
+
+# xvidcore is ONLY_FOR_PLATFORM -*-*-i386
+.if ${MACHINE_ARCH} == "i386"
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	xvidcore
@@ -14,5 +19,7 @@ BUILDLINK_PACKAGES+=	xvidcore
 BUILDLINK_DEPENDS.xvidcore+=	xvidcore>=1.0.0
 BUILDLINK_PKGSRCDIR.xvidcore?=	../../multimedia/xvidcore
 .endif	# XVIDCORE_BUILDLINK3_MK
+
+.endif	# MACHINE_ARCH == i386
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
