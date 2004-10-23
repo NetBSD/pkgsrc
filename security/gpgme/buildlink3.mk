81bf9560c3cb3aeb48debe66adb36143fccac337
shannonjr <shannonjr@pkgsrc.org>
2004-10-23 15:00:48 +0000

    1) Updated to gpgme-1.0.1. This is a bug fix release. 2) Added optional support for OpenPGP/MIME and SMIME via gnupg-devel    as described here: http://www.gnupg.org/aegypten2/

--- security/gpgme/buildlink3.mk
+++ security/gpgme/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/10/03 00:18:09 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2004/10/23 15:00:48 shannonjr Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GPGME_BUILDLINK3_MK:=	${GPGME_BUILDLINK3_MK}+
@@ -12,10 +12,11 @@ BUILDLINK_PACKAGES+=	gpgme
 
 .if !empty(GPGME_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gpgme+=	gpgme>=0.9.0
-BUILDLINK_RECOMMENDED.gpgme+=	gpgme>=0.9.0nb1
+BUILDLINK_RECOMMENDED.gpgme+=	gpgme>=1.0.1
 BUILDLINK_PKGSRCDIR.gpgme?=	../../security/gpgme
 .endif	# GPGME_BUILDLINK3_MK
 
 .include "../../security/libgpg-error/buildlink3.mk"
+.include "../../devel/librfuncs/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
