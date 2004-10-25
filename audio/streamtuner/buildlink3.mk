bb9dfa6e828217e19bdaa168f23321b3b66479ee
xtraeme <xtraeme@pkgsrc.org>
2004-10-25 09:05:27 +0000

    Update audio/streamtuner to 0.99.
    0.99                                    23 October 2004
    
            * The plugins have been included in the core distribution
            * The plugins can now be disabled from the preferences
            * The paned position and column widths were not saved anymore:
              fixed
            * The Local plugin now depends on TagLib (instead of libid3tag
              and libvorbis)
            * The Xiph plugin has been fixed, it now lists all the streams
              (but depends on libxml2)
            * STREAMTUNER_LOCAL_ROOT has been deprecated: the music folder
              can now be set from the preferences
            * The session is now always saved upon exit
            * Tab labels now have their own popup menu, containing
              tab-specific actions
            * The german translations have been updated (submitted by
              Martin Stromberger)
            * Minor fixes and cleanups

--- audio/streamtuner/buildlink3.mk
+++ audio/streamtuner/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:13:09 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/10/25 09:05:27 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 STREAMTUNER_BUILDLINK3_MK:=	${STREAMTUNER_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nstreamtuner}
 BUILDLINK_PACKAGES+=	streamtuner
 
 .if !empty(STREAMTUNER_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.streamtuner+=		streamtuner>=0.12.1
-BUILDLINK_RECOMMENDED.streamtuner+=	streamtuner>=0.12.1nb1
+BUILDLINK_DEPENDS.streamtuner+=		streamtuner>=0.99
 BUILDLINK_PKGSRCDIR.streamtuner?=	../../audio/streamtuner
 .endif	# STREAMTUNER_BUILDLINK3_MK
 
