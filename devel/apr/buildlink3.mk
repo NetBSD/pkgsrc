51de8c3b22867daad6ec86e771eafd42af8edbab
xtraeme <xtraeme@pkgsrc.org>
2004-10-26 04:57:12 +0000

    Replace the .elif statement for APR_USE_OPENLDAP with a new .if block to fix the problem reported in PR pkg/27442 by Rasputin.

--- devel/apr/buildlink3.mk
+++ devel/apr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/10/03 00:13:24 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2004/10/26 04:57:12 xtraeme Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 APR_BUILDLINK3_MK:=	${APR_BUILDLINK3_MK}+
@@ -24,7 +24,8 @@ BUILDLINK_FILES.apr+=	lib/*.exp
 
 .if !empty(APR_USE_DB4:M[yY][eE][sS])
 .  include "../../databases/db4/buildlink3.mk"
-.elif !empty(APR_USE_OPENLDAP:M[Yy][Ee][Ss])
+.endif
+.if !empty(APR_USE_OPENLDAP:M[Yy][Ee][Ss])
 .  include "../../databases/openldap/buildlink3.mk"
 .endif
 
