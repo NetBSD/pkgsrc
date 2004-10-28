0546d3958d0d12dc004e664aa78f6a1362da7c2a
wiz <wiz@pkgsrc.org>
2004-10-28 13:32:20 +0000

    Remove trailing whitespace.

--- mail/dspam/options.mk
+++ mail/dspam/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2004/10/22 05:43:29 xtraeme Exp $
+# $NetBSD: options.mk,v 1.5 2004/10/28 13:32:20 wiz Exp $
 
 .if defined(DSPAM_DELIVERY_AGENT) && !empty(DSPAM_DELIVERY_AGENT:Mcustom)
 DSPAM_DELIVERY_AGENT:=	${DSPAM_DELIVERY_AGENT_ARGS}
@@ -99,7 +99,7 @@ CONFIGURE_ARGS+=	--enable-preferences-extension
 ### Tells DSPAM to create virtual user ids.  Use this if your users are
 ### not system users.
 ###
-.if !empty(PKG_OPTIONS:Mvirtualusers) 
+.if !empty(PKG_OPTIONS:Mvirtualusers)
 CONFIGURE_ARGS+=	--enable-virtual-users
 .endif
 
