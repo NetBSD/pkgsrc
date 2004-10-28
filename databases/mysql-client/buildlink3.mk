cbefa7f38c93e16be46e48673a852e99f42ae971
xtraeme <xtraeme@pkgsrc.org>
2004-10-28 01:08:15 +0000

    Update mysql-{client,server} to 4.0.21 (previously mysql4-*).

--- databases/mysql-client/buildlink3.mk
+++ databases/mysql-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2004/10/03 00:13:21 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2004/10/28 01:08:15 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MYSQL_CLIENT_BUILDLINK3_MK:=	${MYSQL_CLIENT_BUILDLINK3_MK}+
@@ -11,12 +11,13 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmysql-client}
 BUILDLINK_PACKAGES+=	mysql-client
 
 .if !empty(MYSQL_CLIENT_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.mysql-client+=	mysql-client>=3.23.58
-BUILDLINK_RECOMMENDED.mysql-client+=	mysql-client>=3.23.58nb1
-BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql-client
+BUILDLINK_DEPENDS.mysql-client+=	mysql-client>=4.0.18nb1
+BUILDLINK_RECOMMENDED.mysql-client?=	mysql-client>=4.0.20nb1
+BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql4-client
 BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
 .endif	# MYSQL_CLIENT_BUILDLINK3_MK
 
+.include "../../security/openssl/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
