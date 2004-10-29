42f62186fe38e783e6b811f20b6efbdd841313de
jdolecek <jdolecek>
2004-10-29 20:31:54 +0000

    Import PHP 5.0.2, based on the pkgsrc-wip PHP5 package.
    Some of the key features of PHP 5 include:
    * The Zend Engine II with a new object model and dozens of new features. 
    * XML support has been completely redone in PHP 5, all extensions
      are now focused around the excellent libxml2 library
      (http://www.xmlsoft.org/).
    * A new SimpleXML extension for easily accessing and manipulating
      XML as PHP objects. It can also interface with the DOM extension
      and vice-versa.
    * A brand new built-in SOAP extension for interoperability with Web Services. 
    * A new MySQL extension named MySQLi for developers using MySQL
      4.1 and later. This new extension includes an object-oriented
      interface in addition to a traditional interface; as well as support
      for many of MySQL's new features, such as prepared statements.
    * SQLite has been bundled with PHP. For more information on SQLite,
      please visit their website. 
    * Streams have been greatly improved, including the ability to access
      low-level socket operations on streams.

---
+++ lang/php5/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/10/29 20:31:54 jdolecek Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+PHP_BUILDLINK3_MK:=	${PHP_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	php
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nphp}
+BUILDLINK_PACKAGES+=	php
+
+.if !empty(PHP_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.php+=	php-5.0.*
+BUILDLINK_PKGSRCDIR.php?=	../../wip/php5
+.endif	# PHP_BUILDLINK3_MK
+
+.include "../../textproc/libxml2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
