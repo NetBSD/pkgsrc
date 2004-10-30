511d2ddf03a26d32a18d74a00a41ead221498c5e
minskim <minskim>
2004-10-30 00:59:01 +0000

    Bump BUILDLINK_DEPENDS.libexif in libexif/buildlink3.mk and PKGREVISIONs of packages including it, because the recent update of libexif changed the major version number of libexif.  Noted by dieter and Jeremy C. Reed on tech-pkg@.

--- graphics/libexif/buildlink3.mk
+++ graphics/libexif/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/10/03 00:14:55 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/10/30 00:59:02 minskim Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBEXIF_BUILDLINK3_MK:=	${LIBEXIF_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibexif}
 BUILDLINK_PACKAGES+=	libexif
 
 .if !empty(LIBEXIF_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libexif+=	libexif>=0.5.12
-BUILDLINK_RECOMMENDED.libexif+=	libexif>=0.5.12nb2
+BUILDLINK_DEPENDS.libexif+=	libexif>=0.6.11
 BUILDLINK_PKGSRCDIR.libexif?=	../../graphics/libexif
 .endif	# LIBEXIF_BUILDLINK3_MK
 
