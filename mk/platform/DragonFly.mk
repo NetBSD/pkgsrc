7ee43a0aa56a1eccbfe47645ff065c5cf87124b9
wiz <wiz@pkgsrc.org>
2004-10-31 22:05:02 +0000

    Add DragonFlyBSD support, provided by Todd Willey on tech-pkg.

---
+++ mk/platform/DragonFly.mk
@@ -0,0 +1,186 @@
+# $NetBSD: DragonFly.mk,v 1.1 2004/10/31 22:05:02 wiz Exp $
+#
+# Variable definitions for the DragonFly operating system.
+
+_IGNORE_USE_GNU_TOOLS=patch
+
+AWK?=		/usr/bin/awk
+BASENAME?=	/usr/bin/basename
+BRANDELF?=	/usr/bin/brandelf		# used by linux compat layer
+CAT?=		/bin/cat
+CHMOD?=		/bin/chmod
+CHOWN?=		/usr/sbin/chown
+CHGRP?=		/usr/bin/chgrp
+CMP?=		/usr/bin/cmp
+CP?=		/bin/cp
+CUT?=		/usr/bin/cut
+DATE?=		/bin/date
+DC?=		/usr/bin/dc
+DIRNAME?=	/usr/bin/dirname
+ECHO?=		echo				# Shell builtin
+ECHO_N?=	${ECHO} -n
+EGREP?=		/usr/bin/egrep
+EXPR?=		/bin/expr
+FALSE?=		/usr/bin/false
+FGREP?=		/usr/bin/fgrep
+FILE_CMD?=	/usr/bin/file
+FIND?=		/usr/bin/find
+GMAKE?=		${LOCALBASE}/bin/gmake
+GREP?=		/usr/bin/grep
+.if exists(${LOCALBASE}/bin/tar)
+GTAR?=		${LOCALBASE}/bin/tar
+.else
+GTAR?=		/usr/bin/tar
+.endif
+GUNZIP_CMD?=	/usr/bin/gunzip -f
+GZCAT?=		/usr/bin/gzcat
+GZIP?=		-9
+GZIP_CMD?=	/usr/bin/gzip -nf ${GZIP}
+HEAD?=		/usr/bin/head
+HOSTNAME_CMD?=	/bin/hostname
+ID?=		/usr/bin/id
+IMAKE?=		${X11BASE}/bin/imake ${IMAKEOPTS}
+IMAKEOPTS+=	-DBuildHtmlManPages=NO
+LDCONFIG?=	/sbin/ldconfig
+LN?=		/bin/ln
+LS?=		/bin/ls
+M4?=		/usr/bin/m4
+MAIL_CMD?=	/usr/bin/mail
+MKDIR?=		/bin/mkdir -p
+.if exists(${LOCALBASE}/sbin/mtree)
+MTREE?=		${LOCALBASE}/sbin/mtree
+.else
+MTREE?=		/usr/sbin/mtree
+.endif
+MV?=		/bin/mv
+NICE?=		/usr/bin/nice
+PATCH?=		/usr/bin/patch
+.if exists(${LOCALBASE}/bin/pax)
+PAX?=		${LOCALBASE}/bin/pax
+.else
+PAX?=		/bin/pax
+.endif
+PERL5?=		${LOCALBASE}/bin/perl
+PKGLOCALEDIR?=	share
+PS?=		/bin/ps
+PWD_CMD?=	/bin/pwd	# needs to print physical path
+RM?=		/bin/rm
+RMDIR?=		/bin/rmdir
+RSH?=		/usr/bin/rsh
+SED?=		/usr/bin/sed
+SETENV?=	/usr/bin/env
+SH?=		/bin/sh
+SHLOCK=		/usr/bin/shlock
+SORT?=		/usr/bin/sort
+SU?=		/usr/bin/su
+TAIL?=		/usr/bin/tail
+.if exists(${LOCALBASE}/bin/tar)
+TAR?=		${LOCALBASE}/bin/tar
+.else
+TAR?=		/usr/bin/tar
+.endif
+TEE?=		/usr/bin/tee
+TEST?=		/bin/test
+TOUCH?=		/usr/bin/touch
+TR?=		/usr/bin/tr
+TRUE?=		/usr/bin/true
+TSORT?=		/usr/bin/tsort
+TYPE?=		type				# Shell builtin
+WC?=		/usr/bin/wc
+XARGS?=		/usr/bin/xargs
+
+.if exists(/usr/sbin/user)
+USERADD?=	/usr/sbin/useradd
+GROUPADD?=	/usr/sbin/groupadd
+.else
+USERADD?=	${LOCALBASE}/sbin/useradd
+GROUPADD?=	${LOCALBASE}/sbin/groupadd
+_USER_DEPENDS=	user>=20000313:../../sysutils/user
+DEPENDS+=	${USE_USERADD:D${_USER_DEPENDS}}
+DEPENDS+=	${USE_GROUPADD:D${_USER_DEPENDS}}
+.endif
+
+CPP_PRECOMP_FLAGS?=	# unset
+DEF_UMASK?=		0022
+.if ${OBJECT_FMT} == "ELF"
+EXPORT_SYMBOLS_LDFLAGS?=-Wl,-E	# add symbols to the dynamic symbol table
+.else
+EXPORT_SYMBOLS_LDFLAGS?=-Wl,--export-dynamic
+.endif
+MOTIF_TYPE_DEFAULT?=	openmotif	# default 2.0 compatible libs type
+NOLOGIN?=		/sbin/nologin
+PKG_TOOLS_BIN?=		${LOCALBASE}/sbin
+ROOT_CMD?=		${SU} - root -c
+ROOT_USER?=		root
+ROOT_GROUP?=	wheel
+ULIMIT_CMD_datasize?=	ulimit -d `ulimit -H -d`
+ULIMIT_CMD_stacksize?=	ulimit -s `ulimit -H -s`
+ULIMIT_CMD_memorysize?=	ulimit -m `ulimit -H -m`
+
+# imake installs manpages in weird places
+IMAKE_MAN_SOURCE_PATH=	man/man
+IMAKE_MAN_SUFFIX=	1
+IMAKE_LIBMAN_SUFFIX=	3
+IMAKE_KERNMAN_SUFFIX=	4
+IMAKE_FILEMAN_SUFFIX=	5
+IMAKE_MISCMAN_SUFFIX=	7
+IMAKE_MAN_DIR=		${IMAKE_MAN_SOURCE_PATH}1
+IMAKE_LIBMAN_DIR=	${IMAKE_MAN_SOURCE_PATH}3
+IMAKE_KERNMAN_DIR=	${IMAKE_MAN_SOURCE_PATH}4
+IMAKE_FILEMAN_DIR=	${IMAKE_MAN_SOURCE_PATH}5
+IMAKE_MISCMAN_DIR=	${IMAKE_MAN_SOURCE_PATH}7
+IMAKE_MANNEWSUFFIX=	${IMAKE_MAN_SUFFIX}
+
+_DO_SHLIB_CHECKS=	yes	# fixup PLIST for shared libs/run ldconfig
+_IMAKE_MAKE=		${MAKE}	# program which gets invoked by imake
+.if exists(/usr/include/netinet6)
+_OPSYS_HAS_INET6=	yes	# IPv6 is standard
+.else
+_OPSYS_HAS_INET6=	no	# IPv6 is not standard
+.endif
+_OPSYS_HAS_JAVA=	no	# Java is not standard
+_OPSYS_HAS_MANZ=	yes	# MANZ controls gzipping of man pages
+_OPSYS_HAS_OSSAUDIO=	yes	# libossaudio is available
+_OPSYS_PERL_REQD=		# no base version of perl required
+_OPSYS_PTHREAD_AUTO=	no	# -lpthread needed for pthreads
+_OPSYS_SHLIB_TYPE=	ELF/a.out	# shared lib type
+_PATCH_CAN_BACKUP=	yes	# native patch(1) can make backups
+_PATCH_BACKUP_ARG?=	-V simple -b 	# switch to patch(1) for backup suffix
+_PREFORMATTED_MAN_DIR=	cat	# directory where catman pages are
+_USE_GNU_GETTEXT=	no	# Don't use GNU gettext
+_USE_RPATH=		yes	# add rpath to LDFLAGS
+
+# flags passed to the linker to extract all symbols from static archives.
+# this is GNU ld.
+_OPSYS_WHOLE_ARCHIVE_FLAG=	-Wl,--whole-archive
+_OPSYS_NO_WHOLE_ARCHIVE_FLAG=	-Wl,--no-whole-archive
+
+.if (!defined(INSTALL_UNSTRIPPED) || empty(INSTALL_UNSTRIPPED:M[yY][eE][sS])) && !defined(DEBUG_FLAGS)
+_STRIPFLAG_CC?=		-s	# cc(1) option to strip
+_STRIPFLAG_INSTALL?=	-s	# install(1) option to strip
+.endif
+
+DEFAULT_SERIAL_DEVICE?=	/dev/cuaa0
+SERIAL_DEVICES?=	/dev/cuaa0
+
+# check for kqueue(2) support
+.if exists(/usr/include/sys/event.h)
+PKG_HAVE_KQUEUE=	# defined
+.endif
+
+# check for maximum command line length and set it in configure's environment,
+# to avoid a test required by the libtool script that takes forever.
+.if defined(GNU_CONFIGURE) && defined(USE_LIBTOOL)
+_OPSYS_MAX_CMDLEN!=	/sbin/sysctl -n kern.argmax
+CONFIGURE_ENV+=		lt_cv_sys_max_cmd_len=${_OPSYS_MAX_CMDLEN}
+.endif
+
+# If games are to be installed setgid, then SETGIDGAME is set to 'yes'
+# (it defaults to 'no' as per bsd.pkg.defaults.mk).
+# Set the group and mode to meaningful values in that case (defaults to
+# BINOWN, BINGRP and BINMODE as per bsd.pkg.defaults.mk).
+.if !(empty(SETGIDGAME:M[yY][eE][sS]))
+GAMEOWN=		games
+GAMEGRP=		games
+GAMEMODE=		2555
+.endif
