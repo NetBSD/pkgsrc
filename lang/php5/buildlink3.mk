7280e6d63bfe646a61b9d6d9ff427ff0b0f4078a
jdolecek <jdolecek@pkgsrc.org>
2004-11-01 19:52:58 +0000

    default to full dependance on php for anything including this buildlink

--- lang/php5/buildlink3.mk
+++ lang/php5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/30 05:46:21 grant Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/11/01 19:52:58 jdolecek Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PHP_BUILDLINK3_MK:=	${PHP_BUILDLINK3_MK}+
@@ -13,6 +13,9 @@ BUILDLINK_PACKAGES+=	php
 .if !empty(PHP_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.php+=	php-5.0.*
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php5
+
+# Use full dependency for extension and pear modules
+BUILDLINK_DEPMETHOD.php?=	full
 .endif	# PHP_BUILDLINK3_MK
 
 .include "../../textproc/libxml2/buildlink3.mk"
