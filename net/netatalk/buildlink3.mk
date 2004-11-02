a2189f00f373f3a2509d8e96d44f1fd0a9673afd
markd <markd@pkgsrc.org>
2004-11-02 11:26:28 +0000

    Update netatalk to 2.0.1
    Changes:
         * Netatalk's file server, afpd, now speaking AFP 3.1 allows long
           filenames, UTF-8 names, large file support and full MacOS X
           compatibility
         * The print server task, papd, can directly interact with CUPS,
           automagically sharing all CUPS queues
         * Kerberos V support, allowing true "Single Sign On"
         * Whole rework of the CNID subsystem, providing reliable and
           persistant storage of file and directory IDs
         * Huge improvements regarding product documentation making
           Netatalk's features accessible more easily
         * Tons of bugs fixed compared to all previous versions

--- net/netatalk/buildlink3.mk
+++ net/netatalk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/10/03 00:17:56 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/11/02 11:26:28 markd Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 NETATALK_BUILDLINK3_MK:=	${NETATALK_BUILDLINK3_MK}+
@@ -7,15 +7,13 @@ NETATALK_BUILDLINK3_MK:=	${NETATALK_BUILDLINK3_MK}+
 BUILDLINK_DEPENDS+=	netatalk
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGESNnetatalk}
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nnetatalk}
 BUILDLINK_PACKAGES+=	netatalk
 
 .if !empty(NETATALK_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.netatalk+=	netatalk>=1.6.2
-BUILDLINK_RECOMMENDED.netatalk+=	netatalk>=1.6.2nb1
-BUILDLINK_PKGSRCDIR.netatalk?=	../../net/netatalk
+BUILDLINK_DEPENDS.netatalk+=	netatalk>=2.0.1
+BUILDLINK_PKGSRCDIR.netatalk?=	../../mcs/netatalk
+BUILDLINK_DEPMETHOD.netatalk?=	build
 .endif	# NETATALK_BUILDLINK3_MK
-
-.include "../../devel/rx/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
