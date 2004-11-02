46d5b72e9cccc9cbb007497e47ae33a2500f4aa3
gavan <gavan@pkgsrc.org>
2004-11-02 20:44:26 +0000

    librfuncs is not needed on Solaris. Ideally this would be detected by infrastructure such as a builtin.mk file.

--- security/gpgme/buildlink3.mk
+++ security/gpgme/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/10/23 15:00:48 shannonjr Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2004/11/02 20:44:26 gavan Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GPGME_BUILDLINK3_MK:=	${GPGME_BUILDLINK3_MK}+
@@ -17,6 +17,8 @@ BUILDLINK_PKGSRCDIR.gpgme?=	../../security/gpgme
 .endif	# GPGME_BUILDLINK3_MK
 
 .include "../../security/libgpg-error/buildlink3.mk"
+.if ${OPSYS} != "SunOS"
 .include "../../devel/librfuncs/buildlink3.mk"
+.endif
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
