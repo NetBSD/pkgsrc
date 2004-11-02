643fe23161500acb68f50b3b379e5d6d841abaf1
kristerw <kristerw@pkgsrc.org>
2004-11-02 21:48:53 +0000

    Include mk/bsd.prefs.mk so that OPSYS is defined.

--- security/gpgme/buildlink3.mk
+++ security/gpgme/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/11/02 20:44:26 gavan Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2004/11/02 21:48:53 kristerw Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GPGME_BUILDLINK3_MK:=	${GPGME_BUILDLINK3_MK}+
@@ -17,6 +17,7 @@ BUILDLINK_PKGSRCDIR.gpgme?=	../../security/gpgme
 .endif	# GPGME_BUILDLINK3_MK
 
 .include "../../security/libgpg-error/buildlink3.mk"
+.include "../../mk/bsd.prefs.mk"
 .if ${OPSYS} != "SunOS"
 .include "../../devel/librfuncs/buildlink3.mk"
 .endif
