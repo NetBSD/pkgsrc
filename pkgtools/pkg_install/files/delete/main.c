3baf0d5792c6ce374960c74330f7bfe1b7f5b31d
wiz <wiz@pkgsrc.org>
2004-11-03 14:03:53 +0000

    Sync with basesrc.

--- pkgtools/pkg_install/files/delete/main.c
+++ pkgtools/pkg_install/files/delete/main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main.c,v 1.14 2004/11/02 00:10:15 erh Exp $	*/
+/*	$NetBSD: main.c,v 1.15 2004/11/03 14:03:53 wiz Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -11,7 +11,7 @@
 #if 0
 static char *rcsid = "from FreeBSD Id: main.c,v 1.11 1997/10/08 07:46:48 charnier Exp";
 #else
-__RCSID("$NetBSD: main.c,v 1.14 2004/11/02 00:10:15 erh Exp $");
+__RCSID("$NetBSD: main.c,v 1.15 2004/11/03 14:03:53 wiz Exp $");
 #endif
 #endif
 
@@ -42,7 +42,7 @@ __RCSID("$NetBSD: main.c,v 1.14 2004/11/02 00:10:15 erh Exp $");
 #include "lib.h"
 #include "delete.h"
 
-static char Options[] = "DFK:NORVdfhnp:rv";
+static char Options[] = "DdFfhK:NnOp:RrVv";
 
 char   *Prefix = NULL;
 char   *ProgramPath = NULL;
@@ -58,7 +58,7 @@ lpkg_head_t pkgs;
 static void
 usage(void)
 {
-	fprintf(stderr, "usage: pkg_delete [-vVDdnFfOrR] [-p prefix] pkg-name ...\n");
+	fprintf(stderr, "usage: pkg_delete [-DdFfNnORrVv] [-K pkg_dbdir] [-p prefix] pkg-name ...\n");
 	exit(1);
 }
 
@@ -95,14 +95,14 @@ main(int argc, char **argv)
 			_pkgdb_setPKGDB_DIR(optarg);
 			break;
 
-		case 'n':
-			Fake = TRUE;
-			Verbose = TRUE;
-			break;
-
 		case 'N':
 			NoDeleteFiles = TRUE;
 			NoDeInstall = TRUE;
+			break;
+
+		case 'n':
+			Fake = TRUE;
+			Verbose = TRUE;
 			break;
 
 		case 'O':
