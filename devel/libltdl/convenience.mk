de60b1402eacab10fc9711ebde76b09c870c040c
tv <tv@pkgsrc.org>
2004-11-04 04:27:35 +0000

    Also override libltdl.la (the non-convenience version).

--- devel/libltdl/convenience.mk
+++ devel/libltdl/convenience.mk
@@ -1,6 +1,8 @@
-# $NetBSD: convenience.mk,v 1.1 2004/10/17 15:34:27 tv Exp $
+# $NetBSD: convenience.mk,v 1.2 2004/11/04 04:27:35 tv Exp $
+#
 # Override "libltdlc.la" -- the libltdl "convenience" library embedded into
-# programs that ship with libltdl.
+# programs that ship with libltdl.  Also override packages attempting to
+# build and install the full "libltdl.la".
 
 post-patch: fix-libltdlc
 fix-libltdlc:
@@ -8,6 +10,7 @@ fix-libltdlc:
 	${ECHO} 'all install:' >${WRKSRC}/libltdl/Makefile
 	${RM} -f ${WRKSRC}/libltdl/Makefile.am ${WRKSRC}/libltdl/configure*
 	${LN} -fs ${BUILDLINK_DIR}/include/ltdl.h ${WRKSRC}/libltdl/ltdl.h
+	${LN} -fs ${BUILDLINK_DIR}/lib/libltdl.la ${WRKSRC}/libltdl/libltdl.la
 	${LN} -fs ${BUILDLINK_DIR}/lib/libltdl.la ${WRKSRC}/libltdl/libltdlc.la
 
 .include "../../devel/libltdl/buildlink3.mk"
