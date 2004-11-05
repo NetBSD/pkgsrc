3434763a64e12c92e15bc8708633d9f1a1c2ef44
xtraeme <xtraeme@pkgsrc.org>
2004-11-05 16:19:58 +0000

    typo

--- sysutils/swapd/files/swapd.sh
+++ sysutils/swapd/files/swapd.sh
@@ -1,9 +1,9 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: swapd.sh,v 1.1.1.1 2004/10/30 05:06:15 xtraeme Exp $
+# $NetBSD: swapd.sh,v 1.2 2004/11/05 16:19:58 xtraeme Exp $
 #
 # PROVIDE: swapd
-# REQUIRE: DAEMON LOGIN swap
+# REQUIRE: DAEMON LOGIN
 
 
 name="swapd"
