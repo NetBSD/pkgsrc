f30e5d33c170b321fd16910c6f9ef31cc0a492c6
wiz <wiz@pkgsrc.org>
2004-11-05 20:56:28 +0000

    Fix path.

--- audio/libvisual/buildlink3.mk
+++ audio/libvisual/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/11/05 20:55:48 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/11/05 20:56:28 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBVISUAL_BUILDLINK3_MK:=	${LIBVISUAL_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libvisual
 
 .if !empty(LIBVISUAL_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libvisual+=	libvisual>=0.1.7
-BUILDLINK_PKGSRCDIR.libvisual?=	../../zzz/libvisual
+BUILDLINK_PKGSRCDIR.libvisual?=	../../audio/libvisual
 .endif	# LIBVISUAL_BUILDLINK3_MK
 
 .include "../../graphics/MesaLib/buildlink3.mk"
