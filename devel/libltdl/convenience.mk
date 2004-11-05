ce746a29f2742e3d62babe1a76ee907f470d496e
tv <tv@pkgsrc.org>
2004-11-05 17:31:28 +0000

    Add do-nothing "clean" target to fake libltdl Makefile.

--- devel/libltdl/convenience.mk
+++ devel/libltdl/convenience.mk
@@ -1,4 +1,4 @@
-# $NetBSD: convenience.mk,v 1.2 2004/11/04 04:27:35 tv Exp $
+# $NetBSD: convenience.mk,v 1.3 2004/11/05 17:31:28 tv Exp $
 #
 # Override "libltdlc.la" -- the libltdl "convenience" library embedded into
 # programs that ship with libltdl.  Also override packages attempting to
@@ -6,8 +6,8 @@
 
 post-patch: fix-libltdlc
 fix-libltdlc:
-	${ECHO} 'all install:' >${WRKSRC}/libltdl/Makefile.in
-	${ECHO} 'all install:' >${WRKSRC}/libltdl/Makefile
+	${ECHO} 'all install clean:' >${WRKSRC}/libltdl/Makefile.in
+	${ECHO} 'all install clean:' >${WRKSRC}/libltdl/Makefile
 	${RM} -f ${WRKSRC}/libltdl/Makefile.am ${WRKSRC}/libltdl/configure*
 	${LN} -fs ${BUILDLINK_DIR}/include/ltdl.h ${WRKSRC}/libltdl/ltdl.h
 	${LN} -fs ${BUILDLINK_DIR}/lib/libltdl.la ${WRKSRC}/libltdl/libltdl.la
