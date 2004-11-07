e8ad134de8c4feea590f01082fd95bcb251fa963
shannonjr <shannonjr@pkgsrc.org>
2004-11-07 10:44:13 +0000

    Re: pkg/27884: Use of librfuncs breaks gpgme Updated to 1.0.4 to fix a bug in getenv_r() where ENOENT was not returned if the string name was not found in the environment.

--- devel/librfuncs/buildlink3.mk
+++ devel/librfuncs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/30 12:30:22 shannonjr Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/11/07 10:44:13 shannonjr Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBRFUNCS_BUILDLINK3_MK:=	${LIBRFUNCS_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibrfuncs}
 BUILDLINK_PACKAGES+=	librfuncs
 
 .if !empty(LIBRFUNCS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.librfuncs+=	librfuncs>=1.0
+BUILDLINK_DEPENDS.librfuncs+=	librfuncs>=1.0.4
 BUILDLINK_PKGSRCDIR.librfuncs?=	../../devel/librfuncs
 .endif	# LIBRFUNCS_BUILDLINK3_MK
 
