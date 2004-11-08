e1d859896ec39e64dd5df57c0b203fab381c1f58
xtraeme <xtraeme@pkgsrc.org>
2004-11-08 10:07:02 +0000

    Erm, cannot use a shared options.mk file for xmame/xmess...

---
+++ emulators/xmess/options.mk
@@ -0,0 +1,25 @@
+# $NetBSD: options.mk,v 1.1 2004/11/08 10:07:02 xtraeme Exp $
+#
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.xmess
+PKG_SUPPORTED_OPTIONS=	sdl opengl esound
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Msdl)
+MAME_DISP_METHOD?=	SDL
+.include "../../devel/SDL/buildlink3.mk"
+.elif !empty(PKG_OPTIONS:Mopengl)
+MAME_DISP_METHOD?=	xgl
+.include "../../graphics/Mesa/buildlink3.mk"
+.else
+MAME_DISP_METHOD?=	x11
+.endif
+
+.if !empty(PKG_OPTIONS:Msdl) && !empty(PKG_OPTIONS:Mopengl)
+PKG_SKIP_REASON+=	"Only one display method is allowed."
+.endif
+
+.if !empty(PKG_OPTIONS:Mesound)
+MAKE_FLAGS+=		SOUND_ESOUND=1
+.inclUde "../../audio/esound/buildlink3.mk"
+.endif
