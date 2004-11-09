787fa2bd9b24d51735f27926c7ae3c290838fe96
manu <manu@pkgsrc.org>
2004-11-09 22:40:32 +0000

    Add buildlink to libradius

---
+++ net/libradius/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/11/09 22:40:32 manu Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBRADIUS_LINUX_BUILDLINK3_MK:=	${LIBRADIUS_LINUX_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libradius-linux
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibradius-linux}
+BUILDLINK_PACKAGES+=	libradius-linux
+
+.if !empty(LIBRADIUS_LINUX_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libradius-linux+=	libradius-linux>=20040827
+BUILDLINK_PKGSRCDIR.libradius-linux?=	../../net/libradius
+.endif	# LIBRADIUS_LINUX_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
