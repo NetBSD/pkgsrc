671755b5f9c4bfbe2ec7a47e08fc81f886340334
xtraeme <xtraeme@pkgsrc.org>
2004-11-10 09:10:46 +0000

    This package now is called "libradius" not "libradius-linux". Ok'ed manu@

--- net/libradius/buildlink3.mk
+++ net/libradius/buildlink3.mk
@@ -1,19 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/11/10 08:48:36 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/11/10 09:10:46 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBRADIUS_LINUX_BUILDLINK3_MK:=	${LIBRADIUS_LINUX_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	libradius-linux
+BUILDLINK_DEPENDS+=	libradius
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibradius-linux}
-BUILDLINK_PACKAGES+=	libradius-linux
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibradius}
+BUILDLINK_PACKAGES+=	libradius
 
 .if !empty(LIBRADIUS_LINUX_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libradius-linux+=	libradius-linux>=20040827
-BUILDLINK_PKGSRCDIR.libradius-linux?=	../../net/libradius
-BUILDLINK_DEPMETHOD.libradius-linux?=	build
+BUILDLINK_DEPENDS.libradius+=	libradius>=20040827
+BUILDLINK_PKGSRCDIR.libradius?=	../../net/libradius
+BUILDLINK_DEPMETHOD.libradius?=	build
 .endif	# LIBRADIUS_LINUX_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
