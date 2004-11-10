7c4b47e4065df670eb9b73f177595838c6f2b664
xtraeme <xtraeme@pkgsrc.org>
2004-11-10 08:48:36 +0000

    This package only install one static lib and headers, so make this use BUILDLINK_DEPMETHOD.<pkg>?= build.

--- net/libradius/buildlink3.mk
+++ net/libradius/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/11/09 22:40:32 manu Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/11/10 08:48:36 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBRADIUS_LINUX_BUILDLINK3_MK:=	${LIBRADIUS_LINUX_BUILDLINK3_MK}+
@@ -13,6 +13,7 @@ BUILDLINK_PACKAGES+=	libradius-linux
 .if !empty(LIBRADIUS_LINUX_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libradius-linux+=	libradius-linux>=20040827
 BUILDLINK_PKGSRCDIR.libradius-linux?=	../../net/libradius
+BUILDLINK_DEPMETHOD.libradius-linux?=	build
 .endif	# LIBRADIUS_LINUX_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
