2a7f023e16dd61bbe4576c5e602f993f23d99d4b
jlam <jlam>
2004-11-14 07:23:07 +0000

    Add a mini-framework to centralise the handling of "hacks" for getting particular packages to compile correctly.  bsd.hacks.mk auto-includes the hacks.mk file in the current directory.  An example hacks file:
    -------------8<-------------8<-------------8<-------------8<-------------
    .if !defined(FOO_HACKS_MK)
    FOO_HACKS_MK=	defined
    
    ### [Sun Nov 14 02:08:31 EST 2004 : jlam]
    ### arm GCC 2.95.x optimisation bug tickled when compiling
    ### lib/funkyfile.c.  Require a more recent version of GCC as a
    ### workaround and patch lib/funkyfile.c (patch-ae).
    ###
    .if !empty(MACHINE_ARCH:Marm*)
    PKG_HACKS+=	optimisation
    GCC_REQD+=	3.0
    .endif
    
    .endif  # FOO_HACKS_MK
    -------------8<-------------8<-------------8<-------------8<-------------
    
    The comment which heads each individual hack should timestamp when the
    hack was added and describe the bug that requires the hack.  By
    separating out hacks to a separate file, we should be able to simplify
    package Makefiles so that they are more readable.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1536 2004/11/12 20:25:41 sketch Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1537 2004/11/14 07:23:07 jlam Exp $
 #
 # This file is in the public domain.
 #
@@ -78,6 +78,9 @@ build-defs-message: ${WRKDIR}
 .if defined(OBJECT_FMT)
 MAKE_ENV+=	OBJECT_FMT="${OBJECT_FMT}"
 .endif
+
+# Include any hacks necessary for the package to build properly.
+.include "../../mk/bsd.hacks.mk"
 
 # Allow variables to be set on a per-OS basis
 OPSYSVARS+=	CFLAGS CXXFLAGS CPPFLAGS LDFLAGS LIBS
