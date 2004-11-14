ecdd661d53edf3944965ec03e7477ad6e49ec7cb
ben <ben@pkgsrc.org>
2004-11-14 15:38:42 +0000

    As it was, libtool failed to link to libgcrypt on NetBSD 1.6.x because buildlink did not bring in PTH.  For example, this made opencdk fail to build.  Fix this by adding mk/pthread.buildlink3.mk to security/gcrypt/buildlink3.mk

--- security/libgcrypt/buildlink3.mk
+++ security/libgcrypt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/10/23 08:57:33 shannonjr Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2004/11/14 15:38:42 ben Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGCRYPT_BUILDLINK3_MK:=	${LIBGCRYPT_BUILDLINK3_MK}+
@@ -17,5 +17,6 @@ BUILDLINK_PKGSRCDIR.libgcrypt?=	../../security/libgcrypt
 .endif	# LIBGCRYPT_BUILDLINK3_MK
 
 .include "../../security/libgpg-error/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
