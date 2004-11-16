339a63bfcb57e05ecc0759d0c9a6d05ebf4b2df1
rh <rh@pkgsrc.org>
2004-11-16 06:13:15 +0000

    Add dependency on freetype2/Xft2. Bump PKGREVISION. This closes PR pkg/28114 by Francois Briere, thanks!

--- x11/gnustep-back/buildlink3.mk
+++ x11/gnustep-back/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/09/24 01:56:54 rh Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/11/16 06:13:15 rh Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNUSTEP_BACK_BUILDLINK3_MK:=	${GNUSTEP_BACK_BUILDLINK3_MK}+
@@ -16,6 +16,8 @@ BUILDLINK_RECOMMENDED.gnustep-back+=	gnustep-back>=0.9.3nb1
 BUILDLINK_PKGSRCDIR.gnustep-back?=	../../x11/gnustep-back
 .endif	# GNUSTEP_BACK_BUILDLINK3_MK
 
+.include "../../fonts/Xft2/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
 .include "../../graphics/libart2/buildlink3.mk"
 .include "../../x11/gnustep-gui/buildlink3.mk"
 
