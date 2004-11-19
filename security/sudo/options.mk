af47f009c3b3f31f731a2491ccf9aae7e81ce048
jlam <jlam@pkgsrc.org>
2004-11-19 23:18:01 +0000

    Honor PKG_SYSCONFDIR by looking for the LDAP configuration file in ${PKG_SYSCONFDIR}/ldap.conf (by default, /usr/pkg/etc/ldap.conf) instead of hard-coding /etc/ldap.conf.  Bump the PKGREVISION.

--- security/sudo/options.mk
+++ security/sudo/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2004/11/18 23:02:01 jlam Exp $
+# $NetBSD: options.mk,v 1.3 2004/11/19 23:18:01 jlam Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sudo
@@ -16,6 +16,7 @@ CONFIGURE_ARGS+=	--without-kerb5
 .if !empty(PKG_OPTIONS:Mldap)
 .  include "../../databases/openldap/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-ldap=${BUILDLINK_PREFIX.openldap}
+CONFIGURE_ARGS+=	--with-ldap-conf-file=${PKG_SYSCONFDIR}/ldap.conf
 .endif
 
 .if !empty(PKG_OPTIONS:MPAM)
