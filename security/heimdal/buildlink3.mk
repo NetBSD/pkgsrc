25821c4cedbeccd17d29917f7326b4c58a873b17
jlam <jlam@pkgsrc.org>
2004-11-19 23:16:02 +0000

    Correctly detect the old DES API in the OpenSSL in NetBSD's base install.  This prevents Heimdal from building and installing its own DES library and headers.  Bump the PKGREVISION.

--- security/heimdal/buildlink3.mk
+++ security/heimdal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2004/10/03 00:18:09 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2004/11/19 23:16:02 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 HEIMDAL_BUILDLINK3_MK:=	${HEIMDAL_BUILDLINK3_MK}+
@@ -14,7 +14,7 @@ BUILDLINK_PACKAGES+=	heimdal
 
 .if !empty(HEIMDAL_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.heimdal+=	heimdal>=0.4e
-BUILDLINK_RECOMMENDED.heimdal+=	heimdal>=0.6.3nb1
+BUILDLINK_RECOMMENDED.heimdal+=	heimdal>=0.6.3nb2
 BUILDLINK_PKGSRCDIR.heimdal?=	../../security/heimdal
 BUILDLINK_INCDIRS.heimdal?=	include/krb5
 .endif	# HEIMDAL_BUILDLINK3_MK
