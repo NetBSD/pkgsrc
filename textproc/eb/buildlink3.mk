486656aa03a3527bef189ae19e84ae338ba1ced1
uebayasi <uebayasi>
2004-11-20 16:00:35 +0000

    Update eb to 4.1.1.
    Changes since 3.3.4 are:
    
    * Accept remote access.
    * Adopt BSD license.
    * Include only Japanese HTML documents (English documents are removed!).
    * Accept Japanese encodings other than EUC-JP.
    * Improve EB_BookList API.
    * Many bug fixes.
    
    While here, enable pkgviews installation.

--- textproc/eb/buildlink3.mk
+++ textproc/eb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:18:19 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/11/20 16:00:35 uebayasi Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 EB_BUILDLINK3_MK:=	${EB_BUILDLINK3_MK}+
@@ -10,8 +10,13 @@ BUILDLINK_DEPENDS+=	eb
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Neb}
 BUILDLINK_PACKAGES+=	eb
 
+# XXX We need something generic.
+.include "../../mk/bsd.prefs.mk"
+PKG_SYSCONFDIR.eb!=	${PKG_INFO} -B eb | \
+			sed -ne '/^PKG_SYSCONFDIR/ { s/^PKG_SYSCONFDIR=//; p; }'
+
 .if !empty(EB_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.eb+=	eb>=3.3.2nb1
+BUILDLINK_DEPENDS.eb+=		eb>=3.3.2nb1
 BUILDLINK_RECOMMENDED.eb+=	eb>=3.3.4nb1
 BUILDLINK_PKGSRCDIR.eb?=	../../textproc/eb
 .endif	# EB_BUILDLINK3_MK
