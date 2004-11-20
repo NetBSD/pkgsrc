641d783df53c89653e469807976b121aafa3c8ee
jmmv <jmmv@pkgsrc.org>
2004-11-20 22:07:49 +0000

    Backport security fixes (in the nanohttp and the nanoftp modules) from libxml2 (several buffer overflows).  Bump PKGREVISION to 3.

--- textproc/libxml/buildlink3.mk
+++ textproc/libxml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/10/03 00:18:20 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2004/11/20 22:07:49 jmmv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBXML_BUILDLINK3_MK:=	${LIBXML_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libxml
 
 .if !empty(LIBXML_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libxml+=	libxml>=1.8.11
-BUILDLINK_RECOMMENDED.libxml+=	libxml>=1.8.17nb2
+BUILDLINK_RECOMMENDED.libxml+=	libxml>=1.8.17nb3
 BUILDLINK_FILES.libxml+=	include/gnome-xml/libxml/*.h
 BUILDLINK_PKGSRCDIR.libxml?=	../../textproc/libxml
 .endif	# LIBXML_BUILDLINK3_MK
