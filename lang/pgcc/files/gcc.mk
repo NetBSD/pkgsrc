f5c9ff7159a151e05ddead96d1cf30bf9362581c
wiz <wiz@pkgsrc.org>
2004-11-25 15:10:08 +0000

    Remove pgcc -- obsoleted by the release of gcc3.

--- lang/pgcc/files/gcc.mk
+++
@@ -1,7 +0,0 @@
-# $NetBSD: gcc.mk,v 1.2 2003/07/21 12:48:46 seb Exp $
-#
-# make configuration file for @PKGNAME_NOREV@
-
-CC=	@GCC_PREFIX@/bin/cc
-CPP=	@GCC_PREFIX@/bin/cpp
-CXX=	@GCC_PREFIX@/bin/c++
