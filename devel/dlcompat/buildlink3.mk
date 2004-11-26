e4e9824158b2645f0f913785eacbee24b7a97e1a
jlam <jlam>
2004-11-26 09:39:30 +0000

    dlopen.buildlink3.mk will automatically include dlcompat/buildlink3.mk only on Darwin, so remove OPSYS check in dlcompat/buildlink3.mk and make packages include dlopen.buildlink3.mk instead.

--- devel/dlcompat/buildlink3.mk
+++ devel/dlcompat/buildlink3.mk
@@ -1,6 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/07/18 09:35:08 schmonz Exp $
-
-.if ${OPSYS} == "Darwin"
+# $NetBSD: buildlink3.mk,v 1.7 2004/11/26 09:39:30 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 DLCOMPAT_BUILDLINK3_MK:=	${DLCOMPAT_BUILDLINK3_MK}+
@@ -18,5 +16,3 @@ BUILDLINK_PKGSRCDIR.dlcompat?=	../../devel/dlcompat
 .endif  # DLCOMPAT_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
-
-.endif	# OPSYS
