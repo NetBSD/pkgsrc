d7be9187a4ef8dd5c6fbe44f0467018ee9a8e3a5
grant <grant@pkgsrc.org>
2004-11-26 10:17:40 +0000

    add "reload" which exim supports.

--- mail/exim/files/exim.sh
+++ mail/exim/files/exim.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-#	$NetBSD: exim.sh,v 1.6 2003/11/25 10:21:58 abs Exp $
+#	$NetBSD: exim.sh,v 1.7 2004/11/26 10:17:40 grant Exp $
 #
 # PROVIDE: mail
 # REQUIRE: LOGIN
@@ -13,6 +13,7 @@ rcvar=$name
 command="@PREFIX@/sbin/${name}"
 pidfile="/var/run/${name}.pid"
 required_files="@PKG_SYSCONFDIR@/configure"
+extra_commands="reload"
 
 load_rc_config $name
 run_rc_command "$1"
