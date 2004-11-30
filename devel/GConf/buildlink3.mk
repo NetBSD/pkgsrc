ffcff07ffe9fd9f71612e471a38f8f53a73abc7e
wiz <wiz@pkgsrc.org>
2004-11-30 18:25:56 +0000

    Disable db3 backend in GConf1:
      (1) The Berkeley DB storage backend requires DB-3.x, and used an
          experimental feature of DB-3.x.
    
      (2) There is a pretty severe bug that "prevents serious use of the
          Berkeley DB backend", according to the documentation included
          with the GConf sources.
    
      (3) GConf2, the successor to GConf, doesn't build with the Berkeley
          DB storage backend either.
    
    Suggested by jlam.
    
    Bump PKGREVISION.
    Remove db3 bl3 inclusion from bl3 file, and bump BUILDLINK_DEPENDS
    for it.
    
    This fixes the gnucash build problems noted in PR 28423.

--- devel/GConf/buildlink3.mk
+++ devel/GConf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/10/03 00:13:24 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/11/30 18:25:56 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GCONF_BUILDLINK3_MK:=	${GCONF_BUILDLINK3_MK}+
@@ -11,12 +11,10 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NGConf}
 BUILDLINK_PACKAGES+=	GConf
 
 .if !empty(GCONF_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.GConf+=	GConf>=1.0.9nb5
-BUILDLINK_RECOMMENDED.GConf+=	GConf>=1.0.9nb6
+BUILDLINK_DEPENDS.GConf+=	GConf>=1.0.9nb7
 BUILDLINK_PKGSRCDIR.GConf?=	../../devel/GConf
 .endif	# GCONF_BUILDLINK3_MK
 
-.include "../../databases/db3/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/oaf/buildlink3.mk"
 .include "../../devel/popt/buildlink3.mk"
