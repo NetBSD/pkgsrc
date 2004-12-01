05c9a194be97b5c3afbc56d99dd612498e28f4dc
peter <peter@pkgsrc.org>
2004-12-01 00:20:20 +0000

    Remove part of the builtin version detection that doesn't work properly. It can't be fixed easily because the pflkm package uses different versioning (dates instead of 3.5, 3.6, etc). So just disable it for now until I've found a better way to handle this.
    Approved by wiz.

--- security/pflkm/builtin.mk
+++ security/pflkm/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.1.1.1 2004/11/05 15:05:30 peter Exp $
+# $NetBSD: builtin.mk,v 1.2 2004/12/01 00:20:20 peter Exp $
 
 _PF_VERSION=	3.6	# pkg default
 _PF_PFVAR_H=	/usr/include/net/pfvar.h
@@ -19,23 +19,7 @@ BUILTIN_PKG.pflkm=	3.5
 
 _PF_VERSION=		${BUILTIN_PKG.pflkm}
 
-.endif	# exists(${_PF_PFVAR_H})
+.  endif	# exists(${_PF_PFVAR_H})
+.endif	# IS_BUILTIN.pflkm
 
-.if !defined(USE_BUILTIN.pflkm)
 USE_BUILTIN.pflkm?=	${IS_BUILTIN.pflkm}
-
-.  if defined(BUILTIN_PKG.pflkm)
-USE_BUILTIN.pflkm=	yes
-.    for _depend_ in ${BUILDLINK_DEPENDS.pflkm}
-.      if !empty(USE_BUILTIN.pflkm:M[yY][eE][sS])
-USE_BUILTIN.pflkm!=	\
-	if ${PKG_ADMIN} pmatch '${_depend_}' ${BUILTIN_PKG.pflkm}; then \
-		${ECHO} "yes";						\
-	else								\
-		${ECHO} "no";						\
-	fi
-.      endif
-.    endfor
-.  endif
-.endif	# USE_BUILTIN.pflkm
-.endif  # IS_BUILTIN.pflkm
