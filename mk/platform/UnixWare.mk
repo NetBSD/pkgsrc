913b2647ad85ebeee270c0a63884d9035c5853fc
wiz <wiz@pkgsrc.org>
2004-12-01 09:52:59 +0000

    "the the" -> "the"

--- mk/platform/UnixWare.mk
+++ mk/platform/UnixWare.mk
@@ -1,4 +1,4 @@
-# $NetBSD: UnixWare.mk,v 1.4 2004/11/16 18:04:00 tv Exp $
+# $NetBSD: UnixWare.mk,v 1.5 2004/12/01 09:53:01 wiz Exp $
 #
 # Variable definitions for the UnixWare 7 operating system.
 
@@ -132,7 +132,7 @@ _USE_GNU_GETTEXT=	yes		# Use GNU gettext
 #
 # The native linker for UnixWare doesn't really support an option to pass
 # rpath directives, but pretend it does anyway since the wrapper scripts
-# will correctly convert it into the the proper LD_RUN_PATH variable.
+# will correctly convert it into the proper LD_RUN_PATH variable.
 #
 _USE_RPATH=		yes		# add rpath to LDFLAGS
 
