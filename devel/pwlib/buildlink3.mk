4432518be74d4eefc873d62a8a4fafdea8e54aef
wiz <wiz>
2004-12-01 13:40:54 +0000

    Bump PKGREVISION for openssl dependency, and depend on version with openssl dependency.

--- devel/pwlib/buildlink3.mk
+++ devel/pwlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/12/01 13:11:46 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2004/12/01 13:40:54 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PWLIB_BUILDLINK3_MK:=	${PWLIB_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npwlib}
 BUILDLINK_PACKAGES+=	pwlib
 
 .if !empty(PWLIB_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.pwlib+=	pwlib>=1.8.3
-BUILDLINK_RECOMMENDED.pwlib+=	pwlib>=1.8.3
+BUILDLINK_DEPENDS.pwlib+=	pwlib>=1.8.3nb1
 BUILDLINK_PKGSRCDIR.pwlib?=	../../devel/pwlib
 .endif	# PWLIB_BUILDLINK3_MK
 
