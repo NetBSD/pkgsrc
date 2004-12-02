5bb685f2025ad5443acf838f74fa742a134cb9d2
dillo <dillo@pkgsrc.org>
2004-12-02 21:05:44 +0000

    Initial import of gob2 2.0.11 (package provided by Greg Troxel via pkgsrc-wip):
      GObject Builder (for glib2) is a simple preprocessor for easily
    creating GObject objects.  It does not parse any C code and ignores
    any C errors.  It is in spirit similar to things like lex or yacc.  In
    some ways it also resembles java.  But it is really just a simple
    preprocessor for creating GObjects for use in C or C++ and it is not a
    programming language.

---
+++ devel/gob2/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/12/02 21:05:44 dillo Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GOB2_BUILDLINK3_MK:=	${GOB2_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gob2
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngob2}
+BUILDLINK_PACKAGES+=	gob2
+
+.if !empty(GOB2_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.gob2+=	gob2>=2.0.10
+BUILDLINK_PKGSRCDIR.gob2?=	../../wip/gob2
+.endif	# GOB2_BUILDLINK3_MK
+
+#.include "../../devel/glib2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
