0331a68ef4bf59abddb4edc1910723f0a1838a83
grant <grant@pkgsrc.org>
2004-12-02 10:10:47 +0000

    move >=7.4.3 to RECOMMENDED and allow for both postgresql74- and postgresql- pkgnames in DEPENDS so installations from earlier can still be used if you IGNORE_RECOMMENDED.

--- databases/postgresql74-lib/buildlink3.mk
+++ databases/postgresql74-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/07/24 22:44:12 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/12/02 10:10:47 grant Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 POSTGRESQL74_LIB_BUILDLINK3_MK:=	${POSTGRESQL74_LIB_BUILDLINK3_MK}+
@@ -11,7 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npostgresql74-lib}
 BUILDLINK_PACKAGES+=	postgresql74-lib
 
 .if !empty(POSTGRESQL74_LIB_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.postgresql74-lib+=	postgresql74-lib>=7.4.3
+BUILDLINK_RECOMMENDED.postgresql74-lib+=	postgresql74-lib>=7.4.3
+BUILDLINK_DEPENDS.postgresql74-lib+=		postgresql{74,}-lib>=7.4.0
 BUILDLINK_PKGSRCDIR.postgresql74-lib?=	../../databases/postgresql74-lib
 BUILDLINK_INCDIRS.postgresql74-lib?=	include/postgresql
 .endif	# POSTGRESQL74_LIB_BUILDLINK3_MK
