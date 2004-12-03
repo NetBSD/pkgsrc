b8a1db76184e2c76d4c57d1b66dab9880215c680
wiz <wiz@pkgsrc.org>
2004-12-03 17:06:58 +0000

    Update to 1.9.3, from Greg Troxel via pkgsrc-wip.
    Changes since 1.3.4:
    
    Version 1.9.3
    
      - Comes with a compatibility layer that allows GnuCash to be built
        with it.
    
      - Reverts the enhanced generics support, which was introduced in
        1.9.2, since that currently breaks guile-gnome
    
      - Bugfixes
        + g-wrap-2.0-guile.pc is now generated correctly, even if the
          internal libffi is used.
        + Types used by constants are now considered when calculating the
          set of referenced types in a wrapset. This bug led to missing code
          and compile errors when a type defined in another wrapset was used
          by only by a constant.
        + On platforms which lack alloca(), the version shipped with
          G-Wrap should now build correctly.
    
    Version 1.9.2
    
      - Now comes with libffi included, for systems which lack an libffi
        installation
    
      - SRFI module checks now work correctly, even if you do an in-tree build
    
    Version 1.9.1
    
     - API change: destruct-value-cg is now named destroy-value-cg
    
     - Bugfixes
       + A bug in the latent generic code has been fixed
       + The error message for WCP type errors has been fixed
    
    Version 1.9.0
    
     - Complete rewrite; too many changes to list, see the manual for the
       new GOOPS-based API and the guile-gnome project for an extended
       example.
    
      - New features:
        + Output arguments
        + Default arguments
    
      - G-Wrap should be target-language agnostic, like it was in its
        early days.

--- devel/g-wrap/buildlink3.mk
+++ devel/g-wrap/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/12/03 14:31:29 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/12/03 17:06:58 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 G_WRAP_BUILDLINK3_MK:=	${G_WRAP_BUILDLINK3_MK}+
@@ -11,13 +11,10 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ng-wrap}
 BUILDLINK_PACKAGES+=	g-wrap
 
 .if !empty(G_WRAP_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.g-wrap+=	g-wrap>=1.3.4nb3
-BUILDLINK_RECOMMENDED.g-wrap+=	g-wrap>=1.3.4nb4
+BUILDLINK_DEPENDS.g-wrap+=	g-wrap>=1.9.3
 BUILDLINK_PKGSRCDIR.g-wrap?=	../../devel/g-wrap
 .endif	# G_WRAP_BUILDLINK3_MK
 
-.include "../../devel/glib/buildlink3.mk"
-.include "../../lang/guile14/buildlink3.mk"
-.include "../../devel/guile14-gtk/buildlink3.mk"
+.include "../../lang/guile/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
