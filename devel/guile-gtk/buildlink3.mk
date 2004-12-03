49ffdff97ff3b0be13c7a628f19647c57d40b456
wiz <wiz@pkgsrc.org>
2004-12-03 15:28:33 +0000

    Update to 0.40.91 snapshot, by Greg Troxel via pkgsrc-wip.
    NEWS since guile-gtk-1.2-0.31
    
    * Guile-gtk-1.2 is now part of the GNU Project.
    
    * Drag'n'Drop support - see examples/test-dnd.scm
    
    * GdkPixbuf bindings. Formerly these bindings were part of
      guile-gnome, unfortunately they were far from compelete,
      moreover they were not working at all. Now there are full
      GdkPixbuf bindings in the (gtk-1.2 gdk-pixbuf) module.
    
    * LibGlade bindings (formerly part of gnome-guile). Imported from
      gnome-guile, completed and improved. Now it is possible to
      autoconnect scheme functions and build a guile-gtk (glade)
      application in three commands - see examples/test-glade.scm and the
      little bit longer examples/run-glade.scm.
    
      You can find libGlade bindings in the (gtk-1.2 glade) module.
    
    * GtkGLArea bindings (formerly part of guileGL). Imported from
      guileGL, completed and improved.
    
      (gtk-1.2 gdk-gl) module provides bindings for Gdk part of GtkGLArea
      library (GdkGLContext, GdkGLPixmap etc), (gtk-1.2 gtk-gl-area) has
      bindings for GtkGLArea functions.
    
    * configure script has options --with-gdk-pixbuf, --with-glade and
      --with-gtkgl. Just in case you would like to turn off these new
      nifty features.
    
    * build-guile-gtk-1.2 supports functions with arbitrary number of
      required arguments.
    
    * GtkPixmap bindings closely reflects C API, helper function
      gtk-pixmap-new-from-file implements old behaviour.
      GtkPixmap bindings are complete.
    
    * Lots of additional bindings, especially for Gdk.
    
    NEWS in guile-gtk-1.2-0.31
    
    * Bug fixes.
    
    NEWS in guile-gtk-1.2-0.30
    
    * Support for Gnome and Gtk+-2.0 has been removed.
    
      The modules have been renamed from (gtk ...) to (gtk-1.2 ...).  They
      are still available under their old names, but the new ones are
      preferred.
    
      Likewise, build-guile-gtk has been renamed to build-guile-gtk-1.2,
      with the old name still available but deprecated.
    
      Building new programs with "build-guile-gtk main" is no longer
      supported; only shared libraries are.  Consequently, the guile-gtk
      program is no longer provided.
    
    NEWS in 0.20
    
    * New `cname' option for field specifiers.  You can now specify what
      name to use on the C side for field accessors.  For example, to
      access the allocation.x subfield of a GtkWidget structure, you can use
    
        (fields
          (int allocation-x (cname allocation.x)))
    
      The value of the option can be either a symbol or a string.
    
    * New cstring type added to deal with const char *str declarations.
    
    * Guile-gtk will now start the usual `top-repl' of Guile.  The
      processing of Gtk events will happen in a separate handler thread.
      When your Guile doesn't support threading, you get the old event
      driven repl.
    
    * `gdk-event-button-state' and `gdk-event-key-state' have been
      replaced by `gdk-event-state'.  The new function will return a
      GtkModifierType which is a list of symbol such as
    
        (shift-mask control-mask)
    
    * Automatic *.h->*defs translator
    
    * Threads support
    
    * Experimental GTK+ 2.0 support
    
    * RPM spec file corrected
    
    * Lots of additions and corrections to the *.defs files
    
    * Various bug fixes

--- devel/guile-gtk/buildlink3.mk
+++ devel/guile-gtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:13:28 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/12/03 15:28:33 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GUILE_GTK_BUILDLINK3_MK:=	${GUILE_GTK_BUILDLINK3_MK}+
@@ -11,12 +11,11 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nguile-gtk}
 BUILDLINK_PACKAGES+=	guile-gtk
 
 .if !empty(GUILE_GTK_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.guile-gtk+=	guile-gtk>=0.19nb4
-BUILDLINK_RECOMMENDED.guile-gtk+=	guile-gtk>=0.19nb5
+BUILDLINK_DEPENDS.guile-gtk+=	guile-gtk>=0.40
 BUILDLINK_PKGSRCDIR.guile-gtk?=	../../devel/guile-gtk
 .endif	# GUILE_GTK_BUILDLINK3_MK
 
-.include "../../lang/guile14/buildlink3.mk"
+.include "../../lang/guile/buildlink3.mk"
 .include "../../x11/gtk/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
