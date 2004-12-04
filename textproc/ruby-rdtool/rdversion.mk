595cab13f8d23c5a1ef429a3c6b43679946fe699
taca <taca@pkgsrc.org>
2004-12-04 16:07:01 +0000

    Change method to create RUBY_RD_LIST as rubyversion.mk's change, avoiding use of external command, sed(1).

--- textproc/ruby-rdtool/rdversion.mk
+++ textproc/ruby-rdtool/rdversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rdversion.mk,v 1.3 2004/12/04 15:04:53 taca Exp $
+# $NetBSD: rdversion.mk,v 1.4 2004/12/04 16:07:01 taca Exp $
 #
 
 .ifndef _RD_VERSION_MK
@@ -11,6 +11,5 @@ RUBY_RD_VERSION =	0.6.14
 RUBY_RD_REQD=		0.6.14
 
 # create string for dependency list
-RUBY_RD_LIST!=	${ECHO} ${RUBY_VERSION_LIST:S/^/ruby/g:S/$/-rdtool/} | \
-			${SED} -e 's| |,|g'
+RUBY_RD_LIST=	${RUBY_VERSION_LIST:C/([1-9][0-9]*)/ruby&-rdtool/g}
 .endif
