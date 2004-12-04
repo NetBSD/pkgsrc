2ce8b2ed1648e2ceb1506b68954f09c4f0e4dc7c
taca <taca@pkgsrc.org>
2004-12-04 15:04:53 +0000

    Prevent using :ts variable modifire to work on NetBSD 1.6 again. Thanks to Krister Walfridsson noted by private mail.

--- textproc/ruby-rdtool/rdversion.mk
+++ textproc/ruby-rdtool/rdversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rdversion.mk,v 1.2 2004/12/01 15:32:39 taca Exp $
+# $NetBSD: rdversion.mk,v 1.3 2004/12/04 15:04:53 taca Exp $
 #
 
 .ifndef _RD_VERSION_MK
@@ -10,10 +10,7 @@ RUBY_RD=		${LOCALBASE}/bin/rd2
 RUBY_RD_VERSION =	0.6.14
 RUBY_RD_REQD=		0.6.14
 
-# create depends list
-.for f in ${RUBY_VERSION_LIST}
-_RD_NAME+=	ruby${f}-rdtool
-.endfor
-RUBY_RD_LIST=	${_RD_NAME:ts,}
-
+# create string for dependency list
+RUBY_RD_LIST!=	${ECHO} ${RUBY_VERSION_LIST:S/^/ruby/g:S/$/-rdtool/} | \
+			${SED} -e 's| |,|g'
 .endif
