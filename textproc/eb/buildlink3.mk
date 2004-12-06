7d5cf7b13432e89fda6e0f7e91d7ffbfdaa7dfc5
uebayasi <uebayasi@pkgsrc.org>
2004-12-06 09:09:48 +0000

    * Run commands carefully in make(1)'s ``VAR!= cmd'' syntax.  Problem   reported by Krister Walfridsson. * Set INSTALLATION_DIRS so that pkgviews installation succeeds.

--- textproc/eb/buildlink3.mk
+++ textproc/eb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/11/20 16:00:35 uebayasi Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/12/06 09:09:48 uebayasi Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 EB_BUILDLINK3_MK:=	${EB_BUILDLINK3_MK}+
@@ -10,13 +10,21 @@ BUILDLINK_DEPENDS+=	eb
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Neb}
 BUILDLINK_PACKAGES+=	eb
 
+_EB_REQD=		3.3.2nb1
+
 # XXX We need something generic.
 .include "../../mk/bsd.prefs.mk"
-PKG_SYSCONFDIR.eb!=	${PKG_INFO} -B eb | \
-			sed -ne '/^PKG_SYSCONFDIR/ { s/^PKG_SYSCONFDIR=//; p; }'
+PKG_SYSCONFDIR.eb!=						\
+	if ${PKG_ADMIN} pmatch 'eb>=${_EB_REQD}' 2>/dev/null; then \
+		${PKG_INFO} -B eb 2>/dev/null |			\
+		    ${SED} -ne '/^PKG_SYSCONFDIR/ {		\
+			s/^PKG_SYSCONFDIR=//; p; }';		\
+	else							\
+		${ECHO};					\
+	fi
 
 .if !empty(EB_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.eb+=		eb>=3.3.2nb1
+BUILDLINK_DEPENDS.eb+=		eb>=${_EB_REQD}
 BUILDLINK_RECOMMENDED.eb+=	eb>=3.3.4nb1
 BUILDLINK_PKGSRCDIR.eb?=	../../textproc/eb
 .endif	# EB_BUILDLINK3_MK
