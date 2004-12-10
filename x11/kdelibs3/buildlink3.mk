34e9f422a8936df10d01bc42e551920a608ad30f
markd <markd@pkgsrc.org>
2004-12-10 06:02:57 +0000

    Update to kde 3.3.2
    Changes:
    * kdecore: recognize known names for the MacRoman charset
    * khtml: only disallow fixed positioning on body, not relative/absolute
    * khtml: added Javascript-support for HTMLDocument.compatMode
    * khtml: fixed textarea manipulation by DOM
    * khtml: paged media support (page-break-before and -after)
    * khtml: correctly handle text-transform on first-letter
    * khtml: fixed display: block on pseudo-elements
    * khtml: return Undefined instead of Null for out-of-range indices
    * khtml: fixed stopPropagation when called from the target node itself
    * khtml: fixed htmlCompat mode for documents served as text/html
    * khtml: fixed display: compact
    * khtml: support z-order for text areas and list boxes
    * khtml: don't set assume <select> got items when we calculate a height
      for items
    * khtml: changed default horizontal margins for H1-H6 from auto to 0
    * khtml: escape otherwise unencodable characters in form submits
    * khtml: merged min,max-height fixes
    * khtml: merged handling of event handlers
    * khtml: implemented CSS 2.1 compliant parsing of background-position
    * khtml: fixed m_value vs ATTR_VALUE problem which didn't allow sending
      mail on gmail
    * khtml: make use of the suggested filename for images
    * khtml: tons of bidi fixes
    * khtml: enable strict CSS parsing also for transitional doctypes
    * khtml: ignore height element for input elements that are not image
    * khtml: form widget fixes for plastik (and others)
    * khtml: make sure we are layouted before scrolling if we are still
      loading
    * khtml: add compensated font scale from Todd Fahrner's "Toward a
      standard font size interval system"
    * khtml: avoid triggering full repaints of the view before the first
      layout is done
    * khtml: implement CSS3 property box-sizing to match MacIE, Opera and
      Mozilla
    * khtml: fix globeandmail.com famlily of crashes
    * khtml: floats and flowing around floats improvements
    * khtml: fix textarea scrolling positions
    * khtml: tons of fixes for tables
    * khtml: remove hidden widgets from the painting
    * khtml: avoid crashes on XML documents
    * khtml: ignore relative positioning on table sections
    * khtml: fix static position calculation for positioned elements
    * khtml: support insertAdjacentHTML
    * khtml: fix crash in sites manipulating frames
    * khtml: async kwallet support
    * khtml: fix selectorText() when no namespace is specified
    * khtml: update the layout before fetching the computed values
    * khtml: color 'http://foo' links as visited if 'http://foo/' is in
      history
    * khtml: corrected ref-counting error leading to segfault/invalid
      memory usage
    * khtml: fixed some memory corruptions with garbage HTML
    * khtml: account for scrollbar height when sizing variable
      overflow:scroll/auto boxes

--- x11/kdelibs3/buildlink3.mk
+++ x11/kdelibs3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/10/03 00:18:36 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/12/10 06:02:57 markd Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 KDELIBS_BUILDLINK3_MK:=	${KDELIBS_BUILDLINK3_MK}+
@@ -16,6 +16,8 @@ BUILDLINK_PACKAGES+=	kdelibs
 BUILDLINK_DEPENDS.kdelibs+=	kdelibs>=3.2.0
 BUILDLINK_RECOMMENDED.kdelibs?=	kdelibs>=3.3.0nb2
 BUILDLINK_PKGSRCDIR.kdelibs?=	../../x11/kdelibs3
+
+.include "../../x11/kdelibs3/dirs.mk"
 .endif	# KDELIBS_BUILDLINK3_MK
 
 .if defined(USE_CUPS) && (${USE_CUPS} == "YES")
---
+++ x11/kdelibs3/dirs.mk
@@ -0,0 +1,136 @@
+# $NetBSD: dirs.mk,v 1.1 2004/12/10 06:02:57 markd Exp $
+#
+
+.if !defined(DIRS_KDE3_MK)
+DIRS_XDG_MK=	#defined
+
+KDE3_DIRS=	include/dom
+KDE3_DIRS+=	include/kabc
+KDE3_DIRS+=	include/kate
+KDE3_DIRS+=	lib/kconf_update_bin
+KDE3_DIRS+=	lib/kde3
+KDE3_DIRS+=	lib/kde3/plugins
+KDE3_DIRS+=	lib/kde3/plugins/designer
+KDE3_DIRS+=	lib/kde3/plugins/styles
+KDE3_DIRS+=	share/applications/kde
+KDE3_DIRS+=	share/desktop-directories
+KDE3_DIRS+=	share/doc/kde
+KDE3_DIRS+=	share/doc/kde/HTML
+KDE3_DIRS+=	share/doc/kde/HTML/en
+KDE3_DIRS+=	share/doc/kde/HTML/en/common
+KDE3_DIRS+=	share/doc/kde/HTML/en/kcontrol
+KDE3_DIRS+=	share/doc/kde/HTML/en/kinfocenter
+
+_LANGS= cs da de en_GB es et eu fi fr he hr hu it ja nl pt pt_BR \
+	ro ru sk sl sr sv tr uk zh_CN zh_TW
+.for dir in ${_LANGS}
+KDE3_DIRS+=	share/doc/kde/HTML/${dir}
+.endfor
+
+KDE3_DIRS+=	share/kde
+KDE3_DIRS+=	share/kde/applnk
+KDE3_DIRS+=	share/kde/applnk/.hidden
+KDE3_DIRS+=	share/kde/applnk/Applications
+KDE3_DIRS+=	share/kde/applnk/Development
+KDE3_DIRS+=	share/kde/applnk/Editors
+KDE3_DIRS+=	share/kde/applnk/Graphics
+KDE3_DIRS+=	share/kde/applnk/Internet
+KDE3_DIRS+=	share/kde/applnk/Settings
+KDE3_DIRS+=	share/kde/applnk/Settingsmenu
+KDE3_DIRS+=	share/kde/applnk/System
+KDE3_DIRS+=	share/kde/applnk/System/ScreenSavers
+KDE3_DIRS+=	share/kde/applnk/Toys
+KDE3_DIRS+=	share/kde/applnk/Utilities
+KDE3_DIRS+=	share/kde/apps
+KDE3_DIRS+=	share/kde/apps/kabc
+KDE3_DIRS+=	share/kde/apps/kabc/formats
+KDE3_DIRS+=	share/kde/apps/kaddressbook
+KDE3_DIRS+=	share/kde/apps/kate
+KDE3_DIRS+=	share/kde/apps/kate/plugins
+KDE3_DIRS+=	share/kde/apps/kate/scripts
+KDE3_DIRS+=	share/kde/apps/katepart
+KDE3_DIRS+=	share/kde/apps/katepart/syntax
+KDE3_DIRS+=	share/kde/apps/kcm_componentchooser
+KDE3_DIRS+=	share/kde/apps/kconf_update
+KDE3_DIRS+=	share/kde/apps/kcontrol
+KDE3_DIRS+=	share/kde/apps/kcontrol/pics
+KDE3_DIRS+=	share/kde/apps/kdeprint
+KDE3_DIRS+=	share/kde/apps/kdesktop
+KDE3_DIRS+=	share/kde/apps/kdesktop/programs
+KDE3_DIRS+=	share/kde/apps/khangman
+KDE3_DIRS+=	share/kde/apps/khangman/data
+KDE3_DIRS+=	share/kde/apps/khtml
+KDE3_DIRS+=	share/kde/apps/khtml/kpartplugins
+KDE3_DIRS+=	share/kde/apps/kicker
+KDE3_DIRS+=	share/kde/apps/kicker/applets
+KDE3_DIRS+=	share/kde/apps/kicker/extensions
+KDE3_DIRS+=	share/kde/apps/klettres
+KDE3_DIRS+=	share/kde/apps/konqsidebartng
+KDE3_DIRS+=	share/kde/apps/konqsidebartng/virtual_folders
+KDE3_DIRS+=	share/kde/apps/konqsidebartng/virtual_folders/services
+KDE3_DIRS+=	share/kde/apps/konqueror
+KDE3_DIRS+=	share/kde/apps/konqueror/dirtree
+KDE3_DIRS+=	share/kde/apps/konqueror/dirtree/remote
+KDE3_DIRS+=	share/kde/apps/konqueror/kpartplugins
+KDE3_DIRS+=	share/kde/apps/konqueror/servicemenus
+KDE3_DIRS+=	share/kde/apps/kstyle
+KDE3_DIRS+=	share/kde/apps/kstyle/themes
+KDE3_DIRS+=	share/kde/apps/ktuberling
+KDE3_DIRS+=	share/kde/apps/ktuberling/sounds
+KDE3_DIRS+=	share/kde/apps/kwin
+KDE3_DIRS+=	share/kde/apps/profiles
+KDE3_DIRS+=	share/kde/autostart
+KDE3_DIRS+=	share/kde/config
+KDE3_DIRS+=	share/kde/config/magic
+KDE3_DIRS+=	share/kde/config.kcfg
+
+_ICONCOLORS=     crystalsvg hicolor locolor
+_ICONSIZES=      16x16 22x22 32x32 48x48 64x64 128x128
+_ICONDIRS=       actions actions/kde apps devices filesystems mimetypes
+
+KDE3_DIRS+=	share/kde/icons
+.for color in ${_ICONCOLORS}
+KDE3_DIRS+=	share/kde/icons/${color}
+.  for size in ${_ICONSIZES}
+KDE3_DIRS+=	share/kde/icons/${color}/${size}
+.    for dir in ${_ICONDIRS}
+KDE3_DIRS+=	share/kde/icons/${color}/${size}/${dir}
+.    endfor
+.  endfor
+.endfor
+KDE3_DIRS+=	share/kde/icons/crystalsvg/scalable
+.for dir in ${_ICONDIRS}
+KDE3_DIRS+=	share/kde/icons/crystalsvg/scalable/${dir}
+.endfor
+
+KDE3_DIRS+=	share/kde/mimelnk
+KDE3_DIRS+=	share/kde/mimelnk/all
+KDE3_DIRS+=	share/kde/mimelnk/application
+KDE3_DIRS+=	share/kde/mimelnk/audio
+KDE3_DIRS+=	share/kde/mimelnk/fonts
+KDE3_DIRS+=	share/kde/mimelnk/image
+KDE3_DIRS+=	share/kde/mimelnk/inode
+KDE3_DIRS+=	share/kde/mimelnk/interface
+KDE3_DIRS+=	share/kde/mimelnk/kdedevice
+KDE3_DIRS+=	share/kde/mimelnk/message
+KDE3_DIRS+=	share/kde/mimelnk/model
+KDE3_DIRS+=	share/kde/mimelnk/multipart
+KDE3_DIRS+=	share/kde/mimelnk/print
+KDE3_DIRS+=	share/kde/mimelnk/text
+KDE3_DIRS+=	share/kde/mimelnk/video
+KDE3_DIRS+=	share/kde/services
+KDE3_DIRS+=	share/kde/services/kaddressbook
+KDE3_DIRS+=	share/kde/services/kconfiguredialog
+KDE3_DIRS+=	share/kde/services/kded
+KDE3_DIRS+=	share/kde/services/kresources
+KDE3_DIRS+=	share/kde/services/kresources/kabc
+KDE3_DIRS+=	share/kde/servicetypes
+KDE3_DIRS+=	share/kde/sounds
+KDE3_DIRS+=	share/kde/wallpapers
+
+.for dir in ${KDE3_DIRS}
+PRINT_PLIST_AWK+=       /^@dirrm ${dir:S|/|\\/|g}$$/ \
+                                { print "@comment in kde: " $$0; next; }
+.endfor
+
+.endif		# !defined(DIRS_KDE3_MK)
