7cdd25bb32d6229d6af3b611480661e054898abd
adrianp <adrianp@pkgsrc.org>
2004-12-13 07:04:20 +0000

    bl3'ify this package. This is needed by the recent update to p5-libapreq2 2.4.3.  Also closes PR# 27592.

---
+++ www/ap2-perl/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/12/13 07:04:20 adrianp Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+AP2_PERL_BUILDLINK3_MK:=	${AP2_PERL_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	ap2-perl
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nap2-perl}
+BUILDLINK_PACKAGES+=	ap2-perl
+
+.if !empty(AP2_PERL_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.ap2-perl+=	ap2-perl>=1.99.17
+BUILDLINK_PKGSRCDIR.ap2-perl?=	../../www/ap2-perl
+.endif	# AP2_PERL_BUILDLINK3_MK
+
+.include "../../www/apache2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
