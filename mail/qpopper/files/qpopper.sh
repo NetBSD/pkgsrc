ec0b2e3ead3a8e36c2ced24aae349717a02c3300
grant <grant@pkgsrc.org>
2004-12-19 01:33:36 +0000

    qpopper supports reloading config files with SIGHUP, so enable the "reload" command.

--- mail/qpopper/files/qpopper.sh
+++ mail/qpopper/files/qpopper.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: qpopper.sh,v 1.2 2004/12/18 03:54:54 grant Exp $
+# $NetBSD: qpopper.sh,v 1.3 2004/12/19 01:33:36 grant Exp $
 #
 
 # PROVIDE: qpopper
@@ -13,6 +13,7 @@ name=qpopper
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
 pidfile="/var/run/${name}.pid"
+extra_commands="reload"
 
 load_rc_config $name
 run_rc_command "$1"
