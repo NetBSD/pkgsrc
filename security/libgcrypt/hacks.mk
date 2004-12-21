5112bf0639e3b529e3e604dc4226bf59835d8efd
snj <snj>
2004-12-21 19:10:13 +0000

    Pullup ticket 172 - requested by grant beattie build fix for libgcrypt
    Module Name:    pkgsrc
    Committed By:   grant
    Date:           Tue Dec 21 08:57:48 UTC 2004
    
    Modified Files:
            pkgsrc/security/libgcrypt: hacks.mk
    
    Log Message:
    fix socklen_t hack by falling through to ${TRUE} if ${GREP} fails
    (pattern not matched). ugh, bash.
    
    fixes build on Linux. reported by minskim@

--- security/libgcrypt/hacks.mk
+++ security/libgcrypt/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2004/12/18 23:54:58 grant Exp $
+# $NetBSD: hacks.mk,v 1.1.2.1 2004/12/21 19:10:13 snj Exp $
 
 # config.h #defines socklen_t if it is not defined, but libgcrypt does
 # not propogate this to the installed gcrypt.h, so packages using it
@@ -6,7 +6,7 @@
 #
 # hack around this by using the same #define (if present) in gcrypt.h.
 post-configure:
-	socklen=`${GREP} '^#define socklen_t' ${WRKSRC}/config.h`; \
+	socklen=`${GREP} '^#define socklen_t' ${WRKSRC}/config.h || ${TRUE}`; \
 		${MV} ${WRKSRC}/src/gcrypt.h ${WRKSRC}/src/gcrypt.h.old; \
 		${SED} -e "s,^/\* socklen_t \*/,$$socklen," \
 			${WRKSRC}/src/gcrypt.h.old > ${WRKSRC}/src/gcrypt.h
