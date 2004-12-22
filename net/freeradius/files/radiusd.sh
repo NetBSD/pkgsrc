5afd6fa5b126b825a1903cf10d87d43b57a5e177
salo <salo>
2004-12-22 10:49:10 +0000

    Pullup ticket 175 - requested by Adrian Portelli usability fixes for freeradius
            Module Name:    pkgsrc
            Committed By:   adrianp
            Date:           Tue Dec 21 22:16:01 UTC 2004
    
            Modified Files:
                    pkgsrc/net/freeradius: Makefile
                    pkgsrc/net/freeradius/files: radiusd.sh
    
            Log Message:
            - Fix freeradius not starting because of missing directory cleaned
              out at reboot.
            - Issue spotted and patch sent to me from kbrand (at) dplanet.ch,
              thanks.
            - Bumped PKGREVISION
    ---
            Module Name:    pkgsrc
            Committed By:   adrianp
            Date:           Wed Dec 22 10:07:21 UTC 2004
    
            Modified Files:
                    pkgsrc/net/freeradius: Makefile
                    pkgsrc/net/freeradius/files: radiusd.sh
    
            Log Message:
            - Portability fix, use @ROOT_USER@:@ROOT_GROUP@ as opposed to
              hardcoding root:wheel in the startup script.
            - Again thanks to kbrand (at) dplanet.ch for the suggestion.

--- net/freeradius/files/radiusd.sh
+++ net/freeradius/files/radiusd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: radiusd.sh,v 1.2 2004/09/23 19:51:22 adrianp Exp $
+# $NetBSD: radiusd.sh,v 1.2.2.1 2004/12/22 10:49:10 salo Exp $
 #
 # PROVIDE: radiusd
 # REQUIRE: network
@@ -14,6 +14,16 @@ fi
 name="radiusd"
 command="@PREFIX@/sbin/radiusd"
 pidfile="@VARBASE@/run/radiusd/radiusd.pid"
+start_precmd="radiusd_precmd"
+
+radiusd_precmd()
+{
+	if [ ! -d @VARBASE@/run/radiusd ]; then
+		@MKDIR@ @VARBASE@/run/radiusd
+		@CHMOD@ 0750 @VARBASE@/run/radiusd
+		@CHOWN@ @ROOT_USER@:@ROOT_GROUP@ @VARBASE@/run/radiusd
+	fi
+}
 
 if [ -f /etc/rc.subr ]
 then
