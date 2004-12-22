8459b08aa5d813cc7d7103ebbefd0f9684cfd823
reed <reed@pkgsrc.org>
2004-12-22 03:57:15 +0000

    patch-ag and patch-ao already had security fixes for CESA-2004-006. But now these are improved in response to  iDEFENSE Security Advisory 12.21.04  www.idefense.com/application/poi/display?id=173&type=vulnerabilities  libtiff STRIPOFFSETS Integer Overflow Vulnerability  December 21, 2004
    This fix (in two files) was from that advisory and also seen
    in tiff 3.7.1.
    
    PKGREVISION is bumped to 6 and BUILDLINK_RECOMMENDED is adjusted for
    this possible security issue. Other packages depending on this are not
    bumped.

--- graphics/tiff/buildlink3.mk
+++ graphics/tiff/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2004/10/03 00:14:58 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2004/12/22 03:57:15 reed Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 TIFF_BUILDLINK3_MK:=	${TIFF_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	tiff
 
 .if !empty(TIFF_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.tiff+=	tiff>=3.6.1
-BUILDLINK_RECOMMENDED.tiff+=	tiff>=3.6.1nb3
+BUILDLINK_RECOMMENDED.tiff+=	tiff>=3.6.1nb6
 BUILDLINK_PKGSRCDIR.tiff?=	../../graphics/tiff
 .endif	# TIFF_BUILDLINK3_MK
 
