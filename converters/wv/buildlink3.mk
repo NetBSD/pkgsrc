81bd36a571abc052f6452a6c75d383516fceafd3
reed <reed@pkgsrc.org>
2004-12-24 00:14:45 +0000

    Bump PKGREVISION and bump BUILDLINK_RECOMMENDED.wv.
    I saw on one of my boxes:
    
    libwv-1.0.so.2:
            libxml2.so.8 => not found
    
    This was caused because I have wv-1.0.2nb1 which depends on
    libxml2>=2.6.2.
    
    libxml2 was later upgraded, but since we don't have recursive dependencies
    (which is fine), the old and now missing libxml2 library was still good
    enough for wv. (I saw this because I updated libxml2 in place without removing
    packages that depend on it

--- converters/wv/buildlink3.mk
+++ converters/wv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/10/03 00:13:17 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/12/24 00:14:45 reed Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 WV_BUILDLINK3_MK:=	${WV_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	wv
 
 .if !empty(WV_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.wv+=		wv>=1.0.0
-BUILDLINK_RECOMMENDED.wv+=	wv>=1.0.2nb1
+BUILDLINK_RECOMMENDED.wv+=	wv>=1.0.2nb2
 BUILDLINK_PKGSRCDIR.wv?=	../../converters/wv
 .endif	# WV_BUILDLINK3_MK
 
