2f0e9f2df55177d19e89716ba450265a87531ff7
tv <tv@pkgsrc.org>
2004-12-24 23:44:30 +0000

    Add LIBLTDL_CONVENIENCE_SUBDIR variable to allow packages to indicate that bundled libltdl is not in the subdir "libltdl".

--- devel/libltdl/convenience.mk
+++ devel/libltdl/convenience.mk
@@ -1,19 +1,20 @@
-# $NetBSD: convenience.mk,v 1.4 2004/11/15 12:19:40 sketch Exp $
+# $NetBSD: convenience.mk,v 1.5 2004/12/24 23:44:30 tv Exp $
 #
 # Override "libltdlc.la" -- the libltdl "convenience" library embedded into
 # programs that ship with libltdl.  Also override packages attempting to
 # build and install the full "libltdl.la".
 
+LIBLTDL_CONVENIENCE_SUBDIR?=	libltdl
+
 post-patch: fix-libltdlc
 fix-libltdlc:
-	${ECHO} 'all install clean:' >${WRKSRC}/libltdl/Makefile.in
-	${ECHO} 'all install clean:' >${WRKSRC}/libltdl/Makefile
-	${RM} -f ${WRKSRC}/libltdl/Makefile.am ${WRKSRC}/libltdl/configure*
-	${RM} -f ${WRKSRC}/libltdl/ltdl.h
-	${LN} -s ${BUILDLINK_DIR}/include/ltdl.h ${WRKSRC}/libltdl/ltdl.h
-	${RM} -f ${WRKSRC}/libltdl/libltdl.la
-	${LN} -s ${BUILDLINK_DIR}/lib/libltdl.la ${WRKSRC}/libltdl/libltdl.la
-	${RM} -f ${WRKSRC}/libltdl/libltdlc.la
-	${LN} -s ${BUILDLINK_DIR}/lib/libltdl.la ${WRKSRC}/libltdl/libltdlc.la
+	@${ECHO} 'Fixing libltdl convenience library linkage.'
+	@cd ${WRKSRC}/${LIBLTDL_CONVENIENCE_SUBDIR} && \
+		${ECHO} 'all install clean:' >Makefile.in && \
+		${ECHO} 'all install clean:' >Makefile && \
+		${RM} -f Makefile.am configure* ltdl.h *.la && \
+		${LN} -s ${BUILDLINK_DIR}/include/ltdl.h ltdl.h && \
+		${LN} -s ${BUILDLINK_DIR}/lib/libltdl.la libltdl.la && \
+		${LN} -s ${BUILDLINK_DIR}/lib/libltdl.la libltdlc.la
 
 .include "../../devel/libltdl/buildlink3.mk"
