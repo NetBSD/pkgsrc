d5c5f2676bdba3ae1abb1d95154a9c9387730297
taya <taya@pkgsrc.org>
2004-12-25 15:39:42 +0000

    Update mozilla, mozilla-gtk2, and mozilla-stable to 1.7.5
    Changes from release notes
    - NPRuntime support. NPRuntime is an extension to the Netscape Plugin API
      that was developed in cooperation with Apple, Opera, and a group of plugin
      vendors. http://www.mozilla.org/projects/plugins/npruntime.html
    
    - Added undetectable document.all support, and support for exposing elements
      by their ID in the global scope for greater IE compatibility when viewing
      pages that don't request standards compliant behaviour.
      https://bugzilla.mozilla.org/show_bug.cgi?id=248549 and
      https://bugzilla.mozilla.org/show_bug.cgi?id=256932.

--- www/mozilla-gtk2/buildlink3.mk
+++ www/mozilla-gtk2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/08/09 14:19:20 taya Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/12/25 15:39:42 taya Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MOZILLA_GTK2_BUILDLINK3_MK:=	${MOZILLA_GTK2_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmozilla-gtk2}
 BUILDLINK_PACKAGES+=	mozilla-gtk2
 
 .if !empty(MOZILLA_GTK2_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.mozilla-gtk2+=	mozilla-gtk2>=1.7.2
+BUILDLINK_DEPENDS.mozilla-gtk2+=	mozilla-gtk2>=1.7.5
 BUILDLINK_PKGSRCDIR.mozilla-gtk2?=	../../www/mozilla-gtk2
 .endif	# MOZILLA_GTK2_BUILDLINK3_MK
 
