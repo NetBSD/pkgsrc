475fe222fc696af6fd3e0eb943f807d7b803ead5
jlam <jlam@pkgsrc.org>
2004-12-26 23:15:48 +0000

    It's "modules/by-module".

--- mk/bsd.sites.mk
+++ mk/bsd.sites.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.sites.mk,v 1.29 2004/12/25 03:43:33 jlam Exp $
+# $NetBSD: bsd.sites.mk,v 1.30 2004/12/26 23:15:48 jlam Exp $
 #
 # Default MASTER_SITES
 
@@ -49,12 +49,12 @@ MASTER_SITE_GNUSTEP+=   \
 	http://ftp.easynet.nl/mirror/GNUstep/
 
 MASTER_SITE_PERL_CPAN+= \
-	ftp://cpan.pair.com/module/by-module/ \
-	ftp://mirrors.cloud9.net/mirrors/CPAN/module/by-module/ \
-	ftp://ftp.uvsq.fr/pub/perl/CPAN/module/by-module/ \
-	ftp://ftp.gmd.de/mirrors/CPAN/module/by-module/ \
-	ftp://ftp.tuwien.ac.at/pub/CPAN/module/by-module/ \
-	ftp://cpan.perl.org/CPAN/module/by-module/
+	ftp://cpan.pair.com/modules/by-module/ \
+	ftp://mirrors.cloud9.net/mirrors/CPAN/modules/by-module/ \
+	ftp://ftp.uvsq.fr/pub/perl/CPAN/modules/by-module/ \
+	ftp://ftp.gmd.de/mirrors/CPAN/modules/by-module/ \
+	ftp://ftp.tuwien.ac.at/pub/CPAN/modules/by-module/ \
+	ftp://cpan.perl.org/CPAN/modules/by-module/
 
 MASTER_SITE_R_CRAN+=    \
 	http://cran.r-project.org/src/ \
