b1bda19b522575deba953cd06a68445ea94dc884
xtraeme <xtraeme@pkgsrc.org>
2004-12-28 04:44:56 +0000

    If the crypto option has not been enabled, don't depend on libgcrypt, make it conditionally.

--- textproc/libxslt/buildlink3.mk
+++ textproc/libxslt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/10/03 00:18:20 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2004/12/28 04:44:56 xtraeme Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBXSLT_BUILDLINK3_MK:=	${LIBXSLT_BUILDLINK3_MK}+
@@ -16,8 +16,13 @@ BUILDLINK_RECOMMENDED.libxslt+=	libxslt>=1.1.10nb1
 BUILDLINK_PKGSRCDIR.libxslt?=	../../textproc/libxslt
 .endif	# LIBXSLT_BUILDLINK3_MK
 
+PKG_OPTIONS.libxslt?=       ${PKG_DEFAULT_OPTIONS}
+
+.if !empty(PKG_OPTIONS.libxslt:Mcrypto)
+.  include "../../security/libgcrypt/buildlink3.mk"
+.endif
+
 .include "../../lang/perl5/buildlink3.mk"
-.include "../../security/libgcrypt/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
