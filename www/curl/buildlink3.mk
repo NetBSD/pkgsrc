9ea1f979e989c3e426e754a6dbca3daafda177dd
wiz <wiz@pkgsrc.org>
2005-01-03 15:58:52 +0000

    Add dependency on libidn, and DEPEND on latest curl version.

--- www/curl/buildlink3.mk
+++ www/curl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/10/03 00:18:25 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2005/01/03 15:58:52 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CURL_BUILDLINK3_MK:=	${CURL_BUILDLINK3_MK}+
@@ -11,11 +11,11 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ncurl}
 BUILDLINK_PACKAGES+=	curl
 
 .if !empty(CURL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.curl+=	curl>=7.9.1
-BUILDLINK_RECOMMENDED.curl?=	curl>=7.12.1nb1
+BUILDLINK_DEPENDS.curl+=	curl>=7.12.3
 BUILDLINK_PKGSRCDIR.curl?=	../../www/curl
 .endif	# CURL_BUILDLINK3_MK
 
+.include "../../devel/libidn/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
 
