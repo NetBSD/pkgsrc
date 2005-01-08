43c62558038dd11d4a8ea2d00a02ee9f34d6b56c
schmonz <schmonz>
2005-01-08 06:44:41 +0000

    pkgsrc changes:
    * Remove unnecessary patches to strip /usr/local from -L and -I paths.
    * Makefile style nits.
    * Specify dependency on howl's mDNSResponder in the rc.d script.
    
    Ride the version and PKGREVISION from mjl's commit an hour ago.

--- audio/daapd/files/daapd.sh
+++ audio/daapd/files/daapd.sh
@@ -1,27 +1,18 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: daapd.sh,v 1.2 2005/01/06 14:43:09 mjl Exp $
+# $NetBSD: daapd.sh,v 1.3 2005/01/08 06:44:41 schmonz Exp $
 #
 
 # PROVIDE: daapd
-# REQUIRE: DAEMON LOGIN
+# REQUIRE: DAEMON LOGIN howl
 
 . /etc/rc.subr
 
 name="daapd"
-help_name=$name
 rcvar=$name
 command="@PREFIX@/bin/${name}"
 required_files="@PKG_SYSCONFDIR@/daapd.conf"
 command_args="-c $required_files 2>&1 > /dev/null &"
-
-#if ps auxww | grep -v grep | grep -q mDNSResponder ; then
-#  echo "Runs: mDNSResponder"
-#  :
-#else
-#  echo "Starting mDNSResponder"
-#  /usr/pkg/bin/mDNSResponder
-#fi
 
 load_rc_config $name
 run_rc_command "$1"
