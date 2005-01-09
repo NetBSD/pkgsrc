99a3689b80a708717d2188ecb65a8ce38b25c136
seb <seb@pkgsrc.org>
2005-01-09 10:33:36 +0000

    Initial addition of libkexif version 0.2.1 into the NetBSD Packages Collection.
    Libkexif is a library for manipulating EXIF information embedded in
    images.  It currently supports viewing of all EXIF information via
    libexif. It also supports the modification of a few attributes in a safe
    way that preserves all other EXIF information in the file.

---
+++ graphics/libkexif/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/01/09 10:33:37 seb Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBKEXIF_BUILDLINK3_MK:=	${LIBKEXIF_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libkexif
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibkexif}
+BUILDLINK_PACKAGES+=	libkexif
+
+.if !empty(LIBKEXIF_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libkexif+=	libkexif>=0.2.1
+BUILDLINK_PKGSRCDIR.libkexif?=	../../graphics/libkexif
+.endif	# LIBKEXIF_BUILDLINK3_MK
+
+.include "../../graphics/libexif/buildlink3.mk"
+.include "../../x11/kdelibs3/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
