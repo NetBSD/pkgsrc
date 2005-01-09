d247e4f6d91d09a80e5f20c26d66e20545b0d7d9
seb <seb@pkgsrc.org>
2005-01-09 11:14:09 +0000

    Update to version 0.7.1.
    Key new features since last packaged version (0.6.2):
    - Reliable and fast database (sqlite) backend for saving metadata
    - Tagging support for photos.
    - Tags are grouped together as virtual folders shown similar to albums.
    - Extensive drag and drop support for tagging and moving/copying photos
    - Enhanced camera interface with support for automatic photo rotation and
      renaming of photos while downloading.
    - EXIF support with optional oriented display of thumbnails and photos using
      camera provided information
    - Customizable thumbnails for albums and tags
    - Support for nested albums
    - Tooltips providing detailed photo information
    - Themeing support for digiKam
    - KIPI support for enhanced plugin support. KIPI is an initiative between
      various KDE image management applications to provide a common architecture
      for implementing image based plugins.
    - New fast image viewer and editor which uses its own plugin architecture to
      provide various additional functionalities in addition to the usual
      gamma/contrast/brightness adjustments, rotation, resize functions.
      Some of the plugins supplied with digiKam are:
    	Histogram Viewer
    	Red Eye correction
    	Black & White and Sepia conversion
    	Blurring and sharpening
    	RGB color correction
    	Hue/Saturation/Lightness correction
    	Normalize and Equalize
    - Improved thumbnail loading speed.
    - Unified Image properties dialog - with separate pages for file information,
      EXIF and Histogram
    - Threaded histogram calculation for better responsiveness
    - Navigation of albums (based on navigation history) using forward and back
      button
    - Auto album creation using date based album names while downloading photos
      from the camera.
    - Load and display image formats which imlib2 doesnt support, but kde does.
    - Generate thumbnails for raw images using Dave Coffins dcraw tool.
    - New standalone photo viewer named showFoto for kde using digiKams image
      viewer canvas.
    - New commandline option "--detect-camera" to detect connected cameras and to
      launch the camera window on startup.
    - New Aspect Ratio Constrained Crop plugin
    - Free Rotation tool is now a plugin and has guides to help with rotation.

--- graphics/digikam/buildlink3.mk
+++ graphics/digikam/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/12/28 23:18:16 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2005/01/09 11:14:09 seb Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 DIGIKAM_BUILDLINK3_MK:=	${DIGIKAM_BUILDLINK3_MK}+
@@ -11,14 +11,16 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ndigikam}
 BUILDLINK_PACKAGES+=	digikam
 
 .if !empty(DIGIKAM_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.digikam+=	digikam>=0.6.2
-BUILDLINK_RECOMMENDED.digikam+=	digikam>=0.6.2nb5
+BUILDLINK_DEPENDS.digikam+=	digikam>=0.7.1
 BUILDLINK_PKGSRCDIR.digikam?=	../../graphics/digikam
 .endif	# DIGIKAM_BUILDLINK3_MK
 
+.include "../../databases/gdbm/buildlink3.mk"
 .include "../../devel/libgphoto2/buildlink3.mk"
-.include "../../graphics/imlib/buildlink3.mk"
-.include "../../graphics/libexif/buildlink3.mk"
+.include "../../graphics/imlib2/buildlink3.mk"
+.include "../../graphics/libkexif/buildlink3.mk"
+.include "../../graphics/libkipi/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
 .include "../../x11/kdelibs3/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
