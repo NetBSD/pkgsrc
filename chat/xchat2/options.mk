49e633b61222e5510b588b70a39a1d632284202d
adrianp <adrianp@pkgsrc.org>
2005-01-10 20:59:43 +0000

    Remove inet6 from PKG_DEFAULT_OPTIONS. When USE_INET6 is defined it will set inet6. Move to calling all such flags 'inet6'.

--- chat/xchat2/options.mk
+++ chat/xchat2/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.2 2004/11/17 19:56:49 xtraeme Exp $
+# $NetBSD: options.mk,v 1.3 2005/01/10 20:59:43 adrianp Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xchat2
 PKG_SUPPORTED_OPTIONS=	inet6 ssl socks
 
 .if !defined(PKG_OPTIONS.xchat2)
-PKG_DEFAULT_OPTIONS+=	inet6 ssl
+PKG_DEFAULT_OPTIONS+=	ssl
 .endif
 
 .include "../../mk/bsd.options.mk"
