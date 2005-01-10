e1d9bb731fae51bace062c96f1d26d60250c13ca
adrianp <adrianp@pkgsrc.org>
2005-01-10 20:59:43 +0000

    Remove inet6 from PKG_DEFAULT_OPTIONS. When USE_INET6 is defined it will set inet6. Move to calling all such flags 'inet6'.

--- mail/qpopper/options.mk
+++ mail/qpopper/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.1 2004/12/18 03:59:30 grant Exp $
+# $NetBSD: options.mk,v 1.2 2005/01/10 20:59:43 adrianp Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.qpopper
 PKG_SUPPORTED_OPTIONS=	inet6 ssl
 
 .if !defined(PKG_OPTIONS.qpopper)
-PKG_DEFAULT_OPTIONS+=	inet6 ssl
+PKG_DEFAULT_OPTIONS+=	ssl
 .endif
 
 .include "../../mk/bsd.options.mk"
