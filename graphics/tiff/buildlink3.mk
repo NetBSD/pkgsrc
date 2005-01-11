927e41664e33f0c75515d28f255dcc13eda30218
jlam <jlam@pkgsrc.org>
2005-01-11 05:29:55 +0000

    Create a separate libtiffcxx.so library for use by C++ applications. It's meant to be used directly in place of libtiff.so, i.e. instead of linking against -ltiff, you link against -ltiffcxx.  It also leaves libtiff.so as a plain C-linkage library again, as it was in tiff-3.6.x, and avoids needing to patch tiff-using packages to link using the C++ compiler.  Bump the PKGREVISION to 1.

--- graphics/tiff/buildlink3.mk
+++ graphics/tiff/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2004/12/28 23:10:10 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2005/01/11 05:29:55 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 TIFF_BUILDLINK3_MK:=	${TIFF_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	tiff
 
 .if !empty(TIFF_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.tiff+=	tiff>=3.6.1
-BUILDLINK_RECOMMENDED.tiff+=	tiff>=3.7.0
+BUILDLINK_RECOMMENDED.tiff+=	tiff>=3.7.1nb1
 BUILDLINK_PKGSRCDIR.tiff?=	../../graphics/tiff
 .endif	# TIFF_BUILDLINK3_MK
 
