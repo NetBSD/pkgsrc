ef126c7f4fef042372d682eff32b2e7ebbc2563c
salo <salo@pkgsrc.org>
2005-01-11 17:13:43 +0000

    Pullup ticket 214 - requested by Johnny C. Lam security and build fixes for cups
            Module Name:	pkgsrc
            Committed By:	minskim
            Date:		Wed Dec 29 15:31:24 UTC 2004
    
            Modified Files:
            	pkgsrc/print/cups: Makefile
    
            Log Message:
            Use VARBASE.
    ---
            Module Name:	pkgsrc
            Committed By:	jmmv
            Date:		Tue Jan  4 14:48:22 UTC 2005
    
            Modified Files:
            	pkgsrc/print/cups: buildlink3.mk
    
            Log Message:
            OpenSSL is needed here because 'cups-config --libs' lists -lssl
            as a dependency (so we need it in the buildlink directory to build
            other packages).  Should fix build of libgnomeprint shown in
            minskim@'s latest Linux bulk build.
    ---
            Module Name:	pkgsrc
            Committed By:	jlam
            Date:		Thu Jan  6 07:26:39 UTC 2005
    
            Modified Files:
            	pkgsrc/print/cups: Makefile PLIST distinfo
            	pkgsrc/print/cups/files: cupsd.sh
            Added Files:
            	pkgsrc/print/cups/patches: patch-at
    
            Log Message:
            Update print/cups to 1.1.23.  Changes from version 1.1.22 include:
    
            - The scheduler's is_path_absolute() code could cause a DoS
              (STR #1042)
            - The scheduler's device loading code used the wrong size limits
              for the make/model and info parameters (STR #1035)
            - The PNG loading code did not use a "long unsigned integer"
              format specifier for the width and height (STR #1032)
            - The web interface only showed the first 4 or 8 characters of
              "{variable-name}" for undefined template variables (STR #1031)
            - The hpgltops filter did not handle a common PCL command to enter
              HP-GL/2 mode (STR #1037)
            - The scheduler no longer sends the page-set option when printing
              banner pages (STR #995)
            - The hpgltops filter contained two buffer overflows that could
              potentially allow remote access to the "lp" account (STR #1024)
            - The lppasswd command did not protect against file descriptor or
              ulimit attacks (STR #1023)
            - The "lpc status" command used the wrong resource path when
              querying the list of printers and jobs, causing unnecessary
              authentication requests (STR #1018)
            - The httpWait() function did not handle signal interruptions
              (STR #1020)
            - The USB backend used the wrong size status variable when
              checking the printer status (STR #1017)
            - The scheduler did not delete classes from other classes or
              implicit classes, which could cause a crash (STR #1015)
            - The IPP backend now logs the remote print job ID at log level
              NOTICE instead of INFO (so it shows up in the error_log file...)
    ---
            Module Name:	pkgsrc
            Committed By:	jlam
            Date:		Thu Jan  6 18:22:29 UTC 2005
    
            Modified Files:
            	pkgsrc/print/cups: distinfo
            	pkgsrc/print/cups/patches: patch-at
    
            Log Message:
            Don't create the directories during installation... let the pkgsrc
            INSTALL script handle it instead to ensure proper ownership.
    ---
            Module Name:	pkgsrc
            Committed By:	salo
            Date:		Tue Jan 11 00:09:21 UTC 2005
    
            Modified Files:
            	pkgsrc/print/cups: buildlink3.mk
    
            Log Message:
            Bump BUILDLINK_RECOMMENDED after latest security update.

--- print/cups/buildlink3.mk
+++ print/cups/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2004/10/03 00:18:04 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.9.2.1 2005/01/11 17:13:43 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CUPS_BUILDLINK3_MK:=	${CUPS_BUILDLINK3_MK}+
@@ -12,11 +12,12 @@ BUILDLINK_PACKAGES+=	cups
 
 .if !empty(CUPS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.cups+=	cups>=1.1.19nb3
-BUILDLINK_RECOMMENDED.cups+=	cups>=1.1.20nb3
+BUILDLINK_RECOMMENDED.cups+=	cups>=1.1.23
 BUILDLINK_PKGSRCDIR.cups?=	../../print/cups
 .endif	# CUPS_BUILDLINK3_MK
 
 .include "../../graphics/png/buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
