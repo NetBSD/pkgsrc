011455d41af6070303b265ea84330d3b65a167eb
jlam <jlam@pkgsrc.org>
2005-01-12 15:43:39 +0000

    Nuke PKG_FC definition here which broke the semantics of PKG_FC as defined by the wrapper framework.  This commit is part of the USE_FORTRAN-removal commit.

--- lang/f2c/buildlink3.mk
+++ lang/f2c/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:15:00 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/01/12 15:43:39 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 F2C_BUILDLINK3_MK:=	${F2C_BUILDLINK3_MK}+
@@ -11,12 +11,10 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nf2c}
 BUILDLINK_PACKAGES+=	f2c
 
 .if !empty(F2C_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.f2c+=	f2c>=20001205nb3
+BUILDLINK_DEPENDS.f2c+=		f2c>=20001205nb3
 BUILDLINK_RECOMMENDED.f2c+=	f2c>=20001205nb7
 BUILDLINK_PKGSRCDIR.f2c?=	../../lang/f2c
 .endif	# F2C_BUILDLINK3_MK
-
-PKG_FC=                 ${BUILDLINK_PREFIX.f2c}/bin/f2c-f77
 
 # The f2c-f77 script takes some environment variables to override hardcoded
 # values for the compiler and C preprocessor.
