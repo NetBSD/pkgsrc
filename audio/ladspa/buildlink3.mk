02e394369759e4807231e595a471b4e5b6a5df5f
xtraeme <xtraeme@pkgsrc.org>
2005-01-12 13:57:49 +0000

    Initial import of ladspa-1.12 from pkgsrc-wip.
    Many audio synthesis and recording packages are in use or in
    development. These work in many different ways. LADSPA
    provides a standard way for `plugin' audio processors to be used with
    a wide range of these packages.
    
    For instance, this allows a developer to make a reverb program and
    bundle it into a LADSPA `plugin library.' Ordinary users can then use
    this reverb within any LADSPA-friendly audio application.

---
+++ audio/ladspa/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/01/12 13:57:51 xtraeme Exp $
+#
+# This Makefile fragment is included by packages that use ladspa.
+#
+# This file was created automatically using createbuildlink-3.1.
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LADSPA_BUILDLINK3_MK:=	${LADSPA_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	ladspa
+.endif
+
+.if !empty(LADSPA_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			ladspa
+BUILDLINK_DEPENDS.ladspa+=		ladspa>=1.12
+BUILDLINK_PKGSRCDIR.ladspa?=		../../wip/ladspa
+
+.endif # LADSPA_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
