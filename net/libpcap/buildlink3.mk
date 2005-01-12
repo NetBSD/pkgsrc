e55b577b39370b62cda16ac606ac6be506a280de
xtraeme <xtraeme@pkgsrc.org>
2005-01-12 15:44:12 +0000

    Lower dependency requirement to 0.5.0, required for some pkgs on NetBSD with old libpcap.

--- net/libpcap/buildlink3.mk
+++ net/libpcap/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2004/09/28 08:24:42 mrg Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2005/01/12 15:44:12 xtraeme Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBPCAP_BUILDLINK3_MK:=	${LIBPCAP_BUILDLINK3_MK}+
@@ -14,7 +14,7 @@ BUILDLINK_PACKAGES+=	libpcap
 
 .if !empty(LIBPCAP_BUILDLINK3_MK:M+)
 # XXX?
-BUILDLINK_DEPENDS.libpcap+=	libpcap>=0.7.2
+BUILDLINK_DEPENDS.libpcap+=	libpcap>=0.5.0
 BUILDLINK_PKGSRCDIR.libpcap?=	../../net/libpcap
 
 # on Solaris, we always need libnsl and libsocket because libpcap.a
