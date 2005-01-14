bff1eecc9779dce80a073bfe6ca8980e457b7227
jlam <jlam@pkgsrc.org>
2005-01-14 07:54:20 +0000

    Modify linux-pam and solaris-pam builtin.mk files to be more general in their tests for built-in versions of the PAM implementations.  The MacOS X case now collapses nicely into the linux-pam case.  Allow pam.buildlink3.mk to use solaris-pam as an accepted PAM implementation.

--- mk/solaris-pam.builtin.mk
+++ mk/solaris-pam.builtin.mk
@@ -1,18 +1,27 @@
-# $NetBSD: solaris-pam.builtin.mk,v 1.1 2005/01/14 06:29:55 jlam Exp $
+# $NetBSD: solaris-pam.builtin.mk,v 1.2 2005/01/14 07:54:20 jlam Exp $
 
-_SECURITY_PAM_APPL_H=	/usr/include/security/pam_appl.h
+_SOLARIS_PAM_APPL_HEADERS=	/usr/include/security/pam_appl.h
+_SOLARIS_PAM_IDENT=		Copyright.*Sun Microsystems
 
 .if !defined(IS_BUILTIN.solaris-pam)
-IS_BUILTIN.solaris-pam=	no
-.  if empty(_SECURITY_PAM_APPL_H:M${LOCALBASE}/*) && \
-      exists(${_SECURITY_PAM_APPL_H})
+IS_BUILTIN.solaris-pam=		no
+.  for _inc_ in ${_SOLARIS_PAM_APPL_HEADERS}
+.    if !empty(IS_BUILTIN.solaris-pam:M[nN][oO]) && exists(${_inc_})
 IS_BUILTIN.solaris-pam!=	\
-	if ${GREP} -q "Copyright.*Sun Microsystems" ${_SECURITY_PAM_APPL_H}; then \
-		${ECHO} "yes";						\
-	else								\
+	case ${_inc_} in						\
+	${LOCALBASE}/*)							\
 		${ECHO} "no";						\
-	fi
-.  endif
+		;;							\
+	*)								\
+		if ${GREP} -q "${_SOLARIS_PAM_IDENT}" ${_inc_}; then	\
+			${ECHO} "yes";					\
+		else							\
+			${ECHO} "no";					\
+		fi;							\
+		;;							\
+	esac
+.    endif
+.  endfor
 BUILDLINK_VARS+=	IS_BUILTIN.solaris-pam
 .endif	# IS_BUILTIN.solaris-pam
 
