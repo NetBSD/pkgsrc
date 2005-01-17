2f9899a3d52da9b4f257eadc0f11dba703014c4e
martti <martti@pkgsrc.org>
2005-01-17 12:56:13 +0000

    Updated XFce to 4.2.0
    Too many changes to list here, please read the Changelog from www.xfce.org
    (http://www.xfce.org/release_notes/4.2.0_changelog.html)

--- print/xfce4-print/buildlink3.mk
+++ print/xfce4-print/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/07/12 08:25:11 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2005/01/17 12:56:13 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XFCE4_PRINT_BUILDLINK3_MK:=	${XFCE4_PRINT_BUILDLINK3_MK}+
@@ -11,10 +11,11 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-print}
 BUILDLINK_PACKAGES+=	xfce4-print
 
 .if !empty(XFCE4_PRINT_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.xfce4-print+=	xfce4-print>=4.0.6
+BUILDLINK_DEPENDS.xfce4-print+=	xfce4-print>=4.2.0
 BUILDLINK_PKGSRCDIR.xfce4-print?=	../../print/xfce4-print
 .endif	# XFCE4_PRINT_BUILDLINK3_MK
 
+.include "../../print/a2ps/buildlink3.mk"
 .include "../../x11/xfce4-mcs-plugins/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 
