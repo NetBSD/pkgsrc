c742ac88e8ed0f327ad9d729e28ad1b7f164cf89
martti <martti@pkgsrc.org>
2005-01-17 12:52:48 +0000

    Session manager for XFce4.

---
+++ x11/xfce4-session/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/01/17 12:52:48 martti Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+XFCE4_SESSION_BUILDLINK3_MK:=	${XFCE4_SESSION_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	xfce4-session
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-session}
+BUILDLINK_PACKAGES+=	xfce4-session
+
+.if !empty(XFCE4_SESSION_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.xfce4-session+=	xfce4-session>=4.2.0
+BUILDLINK_PKGSRCDIR.xfce4-session?=	../../x11/xfce4-session
+.endif	# XFCE4_SESSION_BUILDLINK3_MK
+
+.include "../../x11/xfce4-mcs-plugins/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
