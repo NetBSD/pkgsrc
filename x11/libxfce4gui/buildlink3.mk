0d122ce26de2e5682265f7bc3fefe05808326c05
martti <martti>
2005-01-17 12:56:13 +0000

    Updated XFce to 4.2.0
    Too many changes to list here, please read the Changelog from www.xfce.org
    (http://www.xfce.org/release_notes/4.2.0_changelog.html)

--- x11/libxfce4gui/buildlink3.mk
+++ x11/libxfce4gui/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/10/03 00:18:36 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2005/01/17 12:56:13 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBXFCE4GUI_BUILDLINK3_MK:=	${LIBXFCE4GUI_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibxfce4gui}
 BUILDLINK_PACKAGES+=	libxfce4gui
 
 .if !empty(LIBXFCE4GUI_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libxfce4gui+=		libxfce4gui>=4.0.6
-BUILDLINK_RECOMMENDED.libxfce4gui+=	libxfce4gui>=4.0.6nb1
+BUILDLINK_DEPENDS.libxfce4gui+=		libxfce4gui>=4.2.0
 BUILDLINK_PKGSRCDIR.libxfce4gui?=	../../x11/libxfce4gui
 .endif	# LIBXFCE4GUI_BUILDLINK3_MK
 
