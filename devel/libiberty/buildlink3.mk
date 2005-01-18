0513607334528f4df0a1e9c7db7c8d460e955143
tv <tv@pkgsrc.org>
2005-01-18 21:58:55 +0000

    nb1: Install ansidecl.h as libiberty_decl.h (to avoid collision).  Done as patches rather than altering custom tarball so as to keep the tarball as close to FSF's source as possible.
    Add application.mk convenience include to add to CPPFLAGS and LIBS enough
    glue to use libiberty in a gcc-based app compile.

--- devel/libiberty/buildlink3.mk
+++ devel/libiberty/buildlink3.mk
@@ -1,4 +1,5 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/06/02 11:28:38 shannonjr Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/01/18 21:58:55 tv Exp $
+
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBIBERTY_BUILDLINK3_MK:=	${LIBIBERTY_BUILDLINK3_MK}+
 
@@ -10,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibiberty}
 BUILDLINK_PACKAGES+=	libiberty
 
 .if !empty(LIBIBERTY_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libiberty+=	libiberty>=1.0
+BUILDLINK_DEPENDS.libiberty+=	libiberty>=1.0nb1
 BUILDLINK_PKGSRCDIR.libiberty?=	../../devel/libiberty
 .endif	# LIBIBERTY_BUILDLINK3_MK
 BUILDLINK_DEPMETHOD.libiberty?= build
