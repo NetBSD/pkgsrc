029ab64fe26c98e6a85e224a40b05309798d5793
xtraeme <xtraeme@pkgsrc.org>
2005-01-19 15:39:44 +0000

    Add another changeable option via rc.conf "gld_waitmysql_seconds", with a default value of "5" seconds to wait for mysql. This is necessary because the gld process needs mysqld available when it's loaded, and sometimes when booting mysqld, it needs more than 2 seconds to be available.

--- mail/gld/files/gld.sh
+++ mail/gld/files/gld.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: gld.sh,v 1.3 2004/11/29 16:17:22 xtraeme Exp $
+# $NetBSD: gld.sh,v 1.4 2005/01/19 15:39:44 xtraeme Exp $
 #
 
 # PROVIDE: gld
@@ -14,10 +14,21 @@ rcvar=$name
 command="@PREFIX@/bin/${name}"
 required_files="@PKG_SYSCONFDIR@/$name.conf"
 pidfile="@VARBASE@/run/${name}.pid"
+extra_commands="gld_waitmysql_seconds"
+
+: ${gld_waitmysql_seconds:=5}
+start_precmd="waitmysql $gld_waitmysql_seconds"
 
 if [ "$1" != "stop" -o "$1" != "status" ]; then
 	echo $(check_process $command) > $pidfile
 fi
+
+waitmysql()
+{
+	_sec=$1
+	echo "$name: waiting for MySQL ${_sec} seconds..."
+	sleep ${_sec}
+}
 
 load_rc_config $name
 run_rc_command "$1"
