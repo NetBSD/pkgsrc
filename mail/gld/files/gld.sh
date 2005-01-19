4d530890358ec0dca2c55ef667de1f515706b3ce
xtraeme <xtraeme@pkgsrc.org>
2005-01-19 15:48:41 +0000

    This needs mysqld in REQUIRE as well.

--- mail/gld/files/gld.sh
+++ mail/gld/files/gld.sh
@@ -1,11 +1,11 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: gld.sh,v 1.4 2005/01/19 15:39:44 xtraeme Exp $
+# $NetBSD: gld.sh,v 1.5 2005/01/19 15:48:41 xtraeme Exp $
 #
 
 # PROVIDE: gld
 # BEFORE: mail
-# REQUIRE: DAEMON LOGIN
+# REQUIRE: DAEMON LOGIN mysqld
 
 . /etc/rc.subr
 
