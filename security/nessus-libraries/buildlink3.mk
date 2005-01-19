cc6120958bfdc1427b9c089f44cafe4856641a36
frueauf <frueauf@pkgsrc.org>
2005-01-19 18:45:33 +0000

    really depend on nessus-libraries>=2.2.2a since >=2.2.0 does not work on alpha as seen in pr pkg/28969. libnasl 2.2.2a needs nessus-libraries 2.2.2a to compile properly.

--- security/nessus-libraries/buildlink3.mk
+++ security/nessus-libraries/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/11/02 00:01:56 adrianp Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2005/01/19 18:45:33 frueauf Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 NESSUS_LIBRARIES_BUILDLINK3_MK:=	${NESSUS_LIBRARIES_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nnessus-libraries}
 BUILDLINK_PACKAGES+=	nessus-libraries
 
 .if !empty(NESSUS_LIBRARIES_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.nessus-libraries+=	nessus-libraries>=2.2.0
+BUILDLINK_DEPENDS.nessus-libraries+=	nessus-libraries>=2.2.2a
 BUILDLINK_PKGSRCDIR.nessus-libraries?=	../../security/nessus-libraries
 .endif	# NESSUS_LIBRARIES_BUILDLINK3_MK
 
