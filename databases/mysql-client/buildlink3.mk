8878f20636241f62d7fe0a9e5fe8f383ca919052
xtraeme <xtraeme@pkgsrc.org>
2005-01-20 14:17:14 +0000

    Update mysql-{client,server} to 4.0.23a (bugfix release).
    See http://dev.mysql.com/doc/mysql/en/News-4.0.23.html for the full
    list of changes.
    
    This update applies the fix for the mysqlaccess script symlink
    vulnerability, bump BUILDLINK_RECOMMENDED.

--- databases/mysql-client/buildlink3.mk
+++ databases/mysql-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2004/10/28 01:45:19 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2005/01/20 14:17:15 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MYSQL_CLIENT_BUILDLINK3_MK:=	${MYSQL_CLIENT_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	mysql-client
 
 .if !empty(MYSQL_CLIENT_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.mysql-client+=	mysql-client>=4.0.18nb1
-BUILDLINK_RECOMMENDED.mysql-client?=	mysql-client>=4.0.20nb1
+BUILDLINK_RECOMMENDED.mysql-client?=	mysql-client>=4.0.23a
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql-client
 BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
 .endif	# MYSQL_CLIENT_BUILDLINK3_MK
