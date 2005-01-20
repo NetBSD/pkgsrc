de16ef651aa79c8fbcdf625a226f4d5f96fb2901
xtraeme <xtraeme@pkgsrc.org>
2005-01-20 13:37:48 +0000

    Apply patch from MySQL BitKeeper repository to fix the symlink vulnerability in the mysqlaccess script.
    Bump PKGREVISION and BUILDLINK_RECOMMENDED.

--- databases/mysql4-client/buildlink3.mk
+++ databases/mysql4-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2004/11/16 21:48:38 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2005/01/20 13:37:48 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MYSQL_CLIENT_BUILDLINK3_MK:=	${MYSQL_CLIENT_BUILDLINK3_MK}+
@@ -12,6 +12,7 @@ BUILDLINK_PACKAGES+=	mysql-client
 
 .if !empty(MYSQL_CLIENT_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.mysql-client+=	mysql-client>=4.1.7
+BUILDLINK_RECOMMENDED.mysql-client+=	mysql-client>=4.1.9nb1
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql4-client
 BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
 .endif	# MYSQL_CLIENT_BUILDLINK3_MK
