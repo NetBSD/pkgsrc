280a64c5ed717cc6eed321deed25a375a5939065
xtraeme <xtraeme@pkgsrc.org>
2005-01-20 14:15:04 +0000

    Apply patches from FreeBSD/ports to fix vulnerabilities in the mysqlaccess and mysqlhotcopy scripts.
    Bump PKGREVISION and BUILDLINK_RECOMMENDED.

--- databases/mysql3-client/buildlink3.mk
+++ databases/mysql3-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/28 01:17:33 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/01/20 14:15:04 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MYSQL_CLIENT_BUILDLINK3_MK:=	${MYSQL_CLIENT_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	mysql-client
 
 .if !empty(MYSQL_CLIENT_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.mysql-client+=	mysql-client>=3.23.58
-BUILDLINK_RECOMMENDED.mysql-client+=	mysql-client>=3.23.58nb1
+BUILDLINK_RECOMMENDED.mysql-client+=	mysql-client>=3.23.58nb3
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql3-client
 BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
 .endif	# MYSQL_CLIENT_BUILDLINK3_MK
