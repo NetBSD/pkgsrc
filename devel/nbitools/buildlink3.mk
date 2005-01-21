ce764021534ee981bf25d9bf6e993fb91b7d7ce0
salo <salo>
2005-01-21 01:17:59 +0000

    Pullup ticket 232 - requested by Min Sik Kim build fix for nbitools
    Revisions pulled up:
    - pkgsrc/devel/nbitools/buildlink3.mk 1.3
    
            Module Name:    pkgsrc
            Committed By:   minskim
            Date:           Tue Jan 18 17:05:21 UTC 2005
    
            Modified Files:
                   pkgsrc/devel/nbitools: buildlink3.mk
    
            Log Message:
            Let packages that require nbitools use GNU make on Linux, because
            imake in nbitools generates Makefile that needs GNU make on Linux.

--- devel/nbitools/buildlink3.mk
+++ devel/nbitools/buildlink3.mk
@@ -1,7 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/05 21:08:52 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2.8.1 2005/01/21 01:17:59 salo Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 NBITOOLS_BUILDLINK3_MK:=	${NBITOOLS_BUILDLINK3_MK}+
+
+.include "../../mk/bsd.prefs.mk"
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	nbitools
@@ -18,6 +20,10 @@ BUILDLINK_DEPMETHOD.nbitools?=	build
 BUILDLINK_FILES.nbitools+=	lib/itools/X11/config/*
 
 XMKMF_CMD=	${BUILDLINK_PREFIX.nbitools}/libexec/itools/xmkmf
+
+.if ${OPSYS} == "Linux"
+USE_GNU_TOOLS+=	make
+.endif
 
 .endif	# NBITOOLS_BUILDLINK3_MK
 
