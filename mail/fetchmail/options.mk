d5971888d088e923b0a016052f0ce49e8fd943a2
frueauf <frueauf@pkgsrc.org>
2005-01-21 15:46:39 +0000

    Rework kerberos V support: 	- use kerberos instead of kerberos5 as PKG_SUPPORTED_OPTIONS 	  to keep compliance with other kerberos aware packages in pkgsrc 	- use the krb5 buildlink environment Introduce support for gssapi which was also requested in pr pkg/26170 with the according PKG_SUPPORTED_OPTIONS. gssapi will imply kerberos5.

--- mail/fetchmail/options.mk
+++ mail/fetchmail/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.6 2005/01/20 12:59:11 frueauf Exp $
+# $NetBSD: options.mk,v 1.7 2005/01/21 15:46:39 frueauf Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fetchmail
-PKG_SUPPORTED_OPTIONS=	inet6 kerberos4 kerberos5 ssl
+PKG_SUPPORTED_OPTIONS=	inet6 kerberos4 kerberos gssapi ssl
 
 .if !defined(PKG_OPTIONS.fetchmail)
 PKG_DEFAULT_OPTIONS+=	ssl
@@ -17,10 +17,21 @@ CONFIGURE_ARGS+=	--enable-inet6
 .endif
 
 ###
-### KPOP (POP3 + Kerberos 4) support.
+### Authentication via GSSAPI (currently only over Kerberos V) support.
+###
+.if !empty(PKG_OPTIONS:Mgssapi)
+PKG_OPTIONS+=		kerberos
+CONFIGURE_ARGS+=	--with-gssapi=${KRB5BASE}
+.else
+CONFIGURE_ARGS+=	--with-gssapi=no
+.endif
+
+###
+### KPOP (POP3 + Kerberos IV) support.
 ###
 .if !empty(PKG_OPTIONS:Mkerberos4)
 CRYPTO+=		uses Kerberos encryption code
+PKG_USE_KERBEROS=	# defined
 CONFIGURE_ARGS+=	--with-kerberos=yes
 .  if empty(MACHINE_PLATFORM:MNetBSD-1.[0-4]*-i386)
 REPLACE_KERBEROS_LIBS=	yes
@@ -30,11 +41,12 @@ CONFIGURE_ARGS+=	--with-kerberos=no
 .endif
 
 ###
-### Kerberos 5 / GSSAPI support.
+### Kerberos V support.
 ###
-.if !empty(PKG_OPTIONS:Mkerberos5)
-CRYPTO+=                uses Kerberos encryption code
-CONFIGURE_ARGS+=        --with-kerberos5=yes
+.if !empty(PKG_OPTIONS:Mkerberos)
+.  include "../../mk/krb5.buildlink3.mk"
+PKG_USE_KERBEROS=	# defined
+CONFIGURE_ARGS+=        --with-kerberos5=${KRB5BASE}
 .else
 CONFIGURE_ARGS+=        --with-kerberos5=no
 .endif
