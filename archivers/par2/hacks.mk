1f74c7f892118b9921ca7fd965b57a349a5a22f6
tv <tv@pkgsrc.org>
2005-01-21 21:48:32 +0000

    Move GCC_REQD-for-Interix check into a hacks.mk; suggested by jlam.

---
+++ archivers/par2/hacks.mk
@@ -0,0 +1,14 @@
+# $NetBSD: hacks.mk,v 1.1 2005/01/21 21:48:32 tv Exp $
+
+.ifndef PAR2_HACKS_MK
+PAR2_HACKS_MK=		# defined
+
+### [Fri Jan 21 21:44:32 UTC 2005 : tv]
+### Interix system-supplied g++ 3.3 iostream has no "long long" support
+###
+.if ${OPSYS} == "Interix"
+PKG_HACKS+=		g++-iostream-longlong
+GCC_REQD+=		3.3.4
+.endif
+
+.endif
