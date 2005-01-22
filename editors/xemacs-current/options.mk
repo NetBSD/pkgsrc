d5f9aa3bfd54ab91248ee7e58ab6e96f03aecd85
magick <magick@pkgsrc.org>
2005-01-22 13:03:06 +0000

    Make this package build again:
    Remove esound-support from options.mk until i find the reason why
    libossaudio is not used.
    Fix canna-support with a patch from Thomas Klausner. Thanks.

--- editors/xemacs-current/options.mk
+++ editors/xemacs-current/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2004/12/28 15:45:12 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2005/01/22 13:03:06 magick Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xemacs
 PKG_SUPPORTED_OPTIONS=	esound ldap xface canna
@@ -6,29 +6,29 @@ PKG_SUPPORTED_OPTIONS=	esound ldap xface canna
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mldap)
-CONFIGURE_ARGS=	--with-ldap
+CONFIGURE_ARGS+=	--with-ldap
 .  include "../../databases/openldap/buildlink3.mk"
 .else
-CONFIGURE_ARGS=	--without-ldap
+CONFIGURE_ARGS+=	--without-ldap
 .endif
 
 .if !empty(PKG_OPTIONS:Mxface)
-CONFIGURE_ARGS=	--with-xface
+CONFIGURE_ARGS+=	--with-xface
 .  include "../../mail/faces/buildlink3.mk"
 .else
-CONFIGURE_ARGS=	--without-xface
+CONFIGURE_ARGS+=	--without-xface
 .endif
 
 .if !empty(PKG_OPTIONS:Mcanna)
 .  include "../../inputmethod/canna-lib/buildlink3.mk"
-CONFIGURE_ARGS=	--with-canna
+CONFIGURE_ARGS+=	--with-canna
 .else
-CONFIGURE_ARGS=	--without-canna
+CONFIGURE_ARGS+=	--without-canna
 .endif
 
-.if !empty(PKG_OPTIONS:Mesound)
-.  include "../../audio/esound/buildlink3.mk"
-.else
+#.if !empty(PKG_OPTIONS:Mesound)
+#.  include "../../audio/esound/buildlink3.mk"
+#.else
 CONFIGURE_ARGS+=	--with-sound=none
-.endif
+#.endif
 
