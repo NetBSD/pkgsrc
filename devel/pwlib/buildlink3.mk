ad822971ce10d20634591328be5f204feb6da7e5
kristerw <kristerw@pkgsrc.org>
2005-01-22 21:38:51 +0000

    Include pthread.buildlink3.mk.

--- devel/pwlib/buildlink3.mk
+++ devel/pwlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/12/01 13:40:54 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2005/01/22 21:38:51 kristerw Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PWLIB_BUILDLINK3_MK:=	${PWLIB_BUILDLINK3_MK}+
@@ -16,5 +16,6 @@ BUILDLINK_PKGSRCDIR.pwlib?=	../../devel/pwlib
 .endif	# PWLIB_BUILDLINK3_MK
 
 .include "../../security/openssl/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
