5d62af15e0b3e5c57015c2ea820675ecf960b9fe
recht <recht>
2005-01-23 22:57:19 +0000

    Intial import of Python 2.3.4 / without thread support (what used to be the python23 package).
    Python is an interpreted, interactive, object-oriented
    programming language that combines remarkable power with
    very clear syntax. For an introduction to programming in
    Python you are referred to the Python Tutorial. The
    Python Library Reference documents built-in and standard
    types, constants, functions and modules. Finally, the
    Python Reference Manual describes the syntax and semantics
    of the core language in (perhaps too) much detail.
    
    Python's basic power can be extended with your own modules
    written in C or C++. On most systems such modules may be
    dynamically loaded. Python is also adaptable as an exten-
    sion language for existing applications. See the internal
    documentation for hints.
    
    This package has been compiled without support for threads.

---
+++ lang/python23-nth/buildlink3.mk
@@ -0,0 +1,27 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/01/23 22:57:19 recht Exp $
+
+BUILDLINK_DENTH:=		${BUILDLINK_DENTH}+
+PYTHON23_NTH_BUILDLINK3_MK:=	${PYTHON23_NTH_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DENTH:M+)
+BUILDLINK_DEPENDS+=	python23-nth
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npython23-nth}
+BUILDLINK_PACKAGES+=	python23-nth
+
+.if !empty(PYTHON23_NTH_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.python23-nth+=	python23-nth>=2.3.4
+BUILDLINK_PKGSRCDIR.python23-nth?=	../../lang/python23-nth
+
+.if defined(BUILDLINK_DEPMETHOD.python)
+BUILDLINK_DEPMETHOD.python23nth?=	${BUILDLINK_DEPMETHOD.python}
+.endif
+
+BUILDLINK_INCDIRS.python23-nth+=	include/python2n3
+BUILDLINK_LIBDIRS.python23-nth+=	lib/python2n3/config
+BUILDLINK_TRANSFORM+=			l:python:python2n3
+
+.endif	# PYTHON23_NTH_BUILDLINK3_MK
+
+BUILDLINK_DENTH:=     ${BUILDLINK_DENTH:S/+$//}
