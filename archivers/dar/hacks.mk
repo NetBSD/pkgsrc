7389e71a4001738ed58f514206ac758c82156bc3
tv <tv@pkgsrc.org>
2005-01-24 20:48:35 +0000

    Give the hack a PKG_HACKS name.

--- archivers/dar/hacks.mk
+++ archivers/dar/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2005/01/24 16:58:06 tv Exp $
+# $NetBSD: hacks.mk,v 1.2 2005/01/24 20:48:35 tv Exp $
 
 .ifndef DAR_HACKS_MK
 DAR_HACKS_MK=		# defined
@@ -7,6 +7,7 @@ DAR_HACKS_MK=		# defined
 ### Interix has no <inttypes.h>/<stdint.h>
 ###
 .if ${OPSYS} == "Interix"
+PKG_HACKS+=		standard-headers
 CONFIGURE_ARGS+=	--enable-os-bits=32
 .endif
 
