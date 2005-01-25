ca852c1c655a5fc91f8fd4d285d4dfde4e53b3be
tv <tv@pkgsrc.org>
2005-01-25 18:22:24 +0000

    Add another Interix hack -- ut_xtime is the obsolescent name for utmpx.ut_tv.

--- shells/zsh3/hacks.mk
+++ shells/zsh3/hacks.mk
@@ -1,13 +1,16 @@
-# $NetBSD: hacks.mk,v 1.1 2005/01/24 22:27:48 tv Exp $
+# $NetBSD: hacks.mk,v 1.2 2005/01/25 18:22:24 tv Exp $
 
 .ifndef ZSH3_HACKS_MK
 ZSH3_HACKS_MK=		# defined
 
 ### [Mon Jan 24 22:27:33 UTC 2005 : tv]
-### For some reason, the GETPGRP_VOID test is not working correctly
+### for some reason, the GETPGRP_VOID test is not working correctly;
+### also, ut_xtime (the obsolescent utmpx field) is changed
 ###
 .if ${OPSYS} == "Interix"
+PKG_HACKS+=		compat
 CONFIGURE_ENV+=		ac_cv_func_getpgrp_void=yes
+CPPFLAGS+=		-Dut_xtime=ut_tv.tv_sec
 .endif
 
 .endif
