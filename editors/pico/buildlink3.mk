b047a090e5e86d0ceadb3344bccdac60ae6aed55
tv <tv@pkgsrc.org>
2005-01-31 19:09:16 +0000

    Fix botched update:  The shlib major of libpico was supposed to be bumped. Bump BUILDLINK_DEPENDS of pico, and the PKGREVISION of both pico and pine.
    To help avoid this problem in the future, add a comment to pico/Makefile,
    indicating where the shlib version is maintained and when it should be bumped.

--- editors/pico/buildlink3.mk
+++ editors/pico/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2005/01/30 17:15:11 kim Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2005/01/31 19:09:16 tv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PICO_BUILDLINK3_MK:=	${PICO_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npico}
 BUILDLINK_PACKAGES+=	pico
 
 .if !empty(PICO_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.pico+=	pico>=4.9
+BUILDLINK_DEPENDS.pico+=	pico>=4.9nb1
 BUILDLINK_PKGSRCDIR.pico?=	../../editors/pico
 .endif	# PICO_BUILDLINK3_MK
 
