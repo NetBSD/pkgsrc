93dd763f12c4bb84826355fc50bb31b9dd15c408
jschauma <jschauma@pkgsrc.org>
2005-02-01 21:51:12 +0000

    Initial import of devel/nss from pkgsrc-wip, provided by matthewluckie:
    Network Security Services (NSS) is a set of libraries designed to support
    cross-platform development of security-enabled server applications.
    Applications built with NSS can support SSL v2 and v3, TLS, PKCS #5, PKCS #7,
    PKCS #11, PKCS #12, S/MIME, X.509 v3 certificates, and other security
    standards.

---
+++ devel/nss/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/02/01 21:51:12 jschauma Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+NSS_BUILDLINK3_MK:=	${NSS_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	nss
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nnss}
+BUILDLINK_PACKAGES+=	nss
+
+.if !empty(NSS_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.nss+=		nss>=3.9.2
+BUILDLINK_RECOMMENDED.nss+=	nss>=3.9.2
+BUILDLINK_PKGSRCDIR.nss?=	../../devel/nss
+.endif  # NSS_BUILDLINK3_MK
+
+.include "../../devel/nspr/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
