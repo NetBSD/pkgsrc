92762b96b0a303d69f8a2395b735320c81b6121c
jlam <jlam@pkgsrc.org>
2005-02-04 01:19:03 +0000

    Pull in documentation/whitespace fixes and improvements from the HEAD revision of src/usr.sbin/pkg_install as of 20050204.  I ran:
    	cd pkgsrc/pkg_install/files
    	./pkg_install2nbcompat .
    
    Then I ran "cvs diff" and looked for portability-related changes that
    were removed as a result of the import and resurrected them from the
    repository.

--- pkgtools/pkg_install/files/info/perform.c
+++ pkgtools/pkg_install/files/info/perform.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: perform.c,v 1.18 2004/12/29 12:16:56 agc Exp $	*/
+/*	$NetBSD: perform.c,v 1.19 2005/02/04 01:19:04 jlam Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -11,7 +11,7 @@
 #if 0
 static const char *rcsid = "from FreeBSD Id: perform.c,v 1.23 1997/10/13 15:03:53 jkh Exp";
 #else
-__RCSID("$NetBSD: perform.c,v 1.18 2004/12/29 12:16:56 agc Exp $");
+__RCSID("$NetBSD: perform.c,v 1.19 2005/02/04 01:19:04 jlam Exp $");
 #endif
 #endif
 
@@ -180,7 +180,7 @@ pkg_do(char *pkg)
 
 			/* No match */
 			warnx("can't find package `%s' installed%s!", pkg,
-			      usedot ? " or in a file" : "");
+				  usedot ? " or in a file" : "");
 			return 1;
 		}
 		if (chdir(log_dir) == FAIL) {
