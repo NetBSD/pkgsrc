e0bceaae1c3245a05926a020eebb69ac17172396
taca <taca>
2005-02-06 13:21:45 +0000

    Fix pathname of ".include"; wip => databases.

--- databases/postgresql80-lib/buildlink3.mk
+++ databases/postgresql80-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/02/06 12:32:06 jdolecek Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/02/06 13:21:45 taca Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 POSTGRESQL80_LIB_BUILDLINK3_MK:=	${POSTGRESQL80_LIB_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	postgresql80-lib
 
 .if !empty(POSTGRESQL80_LIB_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.postgresql80-lib+=	postgresql80-lib>=8.0.0beta4
-BUILDLINK_PKGSRCDIR.postgresql80-lib?=	../../wip/postgresql80-lib
+BUILDLINK_PKGSRCDIR.postgresql80-lib?=	../../databases/postgresql80-lib
 
 BUILDLINK_INCDIRS.postgresql80-lib+=	pgsql80/include
 BUILDLINK_LIBDIRS.postgresql80-lib+=	pgsql80/lib
