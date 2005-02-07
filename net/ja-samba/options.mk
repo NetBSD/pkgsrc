c9be61de36a114b7f062f8fb1a9331d31088fc6c
jlam <jlam@pkgsrc.org>
2005-02-07 11:35:41 +0000

    Instantly deprecate USE_PAM from pkgsrc as its value is being set from within NetBSD-current's bsd.own.mk, which conflicts with its usage in pkgsrc.  The package that use USE_PAM have been converted to use the bsd.options.mk framework.  This should fix PR pkg/29257.

---
+++ net/ja-samba/options.mk
@@ -0,0 +1,38 @@
+# $NetBSD: options.mk,v 1.1 2005/02/07 11:35:45 jlam Exp $
+
+# Global and legacy options
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.ja-samba
+PKG_SUPPORTED_OPTIONS=	PAM cups ldap
+.include "../../mk/bsd.options.mk"
+
+###
+### Support PAM authentication.
+###
+.if !empty(PKG_OPTIONS:MPAM)
+.  include "../../mk/pam.buildlink3.mk"
+CONFIGURE_ARGS+=	--with-pam
+.endif
+
+###
+### Native CUPS support for printing
+###
+.if !empty(PKG_OPTIONS:Mcups)
+.  include "../../print/cups/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-cups
+.endif
+
+###
+### Support Samba-2.x LDAP password and account databases.
+###
+.if !empty(PKG_OPTIONS:Mldap-compat)
+.  include "../../databases/openldap/buildlink3.mk"
+BUILD_DEFS+=		USE_OPENLDAP
+CONFIGURE_ARGS+=	--with-ldapsam
+PLIST_SUBST+=		SAMBA_LDAP=""
+.else   
+PLIST_SUBST+=		SAMBA_LDAP="@comment "
+MESSAGE_SRC=		${.CURDIR}/MESSAGE.common ${.CURDIR}/MESSAGE.smbpasswd
+SMBPASSWD_FILE=		/dev/null ${SAMBA_PRIVATE}/smbpasswd		\
+			${ROOT_USER} ${ROOT_GROUP} 0600
+.endif
