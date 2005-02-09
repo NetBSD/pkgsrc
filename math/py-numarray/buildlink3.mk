d09dfe35121dfa94f7a0a32042d55403b8c96319
markd <markd>
2005-02-09 12:16:02 +0000

    Update py-numarray to version 1.1.1
    Support Python 2.4
    
    1.1.1 changes:
    numarray-1.1.1 is a bugfix release to numarray-1.1. Notable bugs
    fixed include memory leaks in matrixmultiply and comparison ufuncs.
    
    
    1.1 changes:
    I. ENHANCEMENTS
    
    CharArray eval() sped up
    Document memmap.py (memory mapping)
    Unsigned int type support limited
    Add kroenecker product
    
    II. BUGS FIXED / CLOSED
    
    max.reduce of byteswapped array
    numeric compatibility byteoffset
    matrixmultiply (a,b) leaves b transposed
    random_array.randint exceeds boundaries
    buffer not aligned on 8 byte boundary  (Windows-98 broken)
    Object Array repr for >1000 elements
    Invalid sequences errors
    Segfault in array element deletion
    Incorrect handling of overlapping assignments in Numarray
    Weirdness with 'new' method
    searchsorted bug and fix
    randint bug fix patch
    a.is_c_array() mixed int/bool results
    argsort of string arrays
    
    III. CAUTIONS
    
    1. This release is binary incompatible with numarray-1.0.  Writers of
    C-extensions which directly reference the byteoffset field of the
    PyArrayObject should be aware that the data pointer is now the sum of
    byteoffset and the buffer base pointer.  All C extensions which use
    the numarray C-API must be recompiled.  This incompatibility was an
    unfortunate consequence of the fix for "numeric compatibility
    byteoffset".

--- math/py-numarray/buildlink3.mk
+++ math/py-numarray/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/22 15:14:59 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/02/09 12:16:02 markd Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 PY_NUMARRAY_BUILDLINK3_MK:=	${PY_NUMARRAY_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npynumarray}
 BUILDLINK_PACKAGES+=	pynumarray
 
 .if !empty(PY_NUMARRAY_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.pynumarray+=	${PYPKGPREFIX}-numarray>=0.9
+BUILDLINK_DEPENDS.pynumarray+=	${PYPKGPREFIX}-numarray>=1.1.1
 BUILDLINK_PKGSRCDIR.pynumarray?=	../../math/py-numarray
 .endif	# PY_NUMARRAY_BUILDLINK3_MK
 
