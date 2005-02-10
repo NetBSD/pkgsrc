fbcc961f58284c6ce68732a1f6ca690fd7d74aca
grant <grant@pkgsrc.org>
2005-02-10 23:51:18 +0000

    pull in binary pkg handling changes from src:
    rework pkg_info(8) operating on binary packages, deprecating the `-.'
    argument as it is no longer required (but is retained as a no-op for
    backward compatibility).
    
    the behaviour is now what is expected and intended:
    
            - when the pkg argument is path (absolute or relative) to a
              binary pkg, pkg_info operates on it.
            - when no pkg argument is given, or the argument is not a
              binary pkg path, pkg_info operates on the installed packages.
    
    `pkg_info foo-1.0.tgz', `pkg_info /path/to/foo-1.0.tgz', etc. now work
    correctly when foo-1.0.tgz is in the cwd.
    
    bump PKGTOOLS_VERSION to 20050210.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.40 2005/02/04 09:10:13 jlam Exp $	*/
+/*	$NetBSD: version.h,v 1.41 2005/02/10 23:51:18 grant Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20050204"
+#define PKGTOOLS_VERSION "20050210"
 
 #endif /* _INST_LIB_VERSION_H_ */
