f6ee6f16723c49aeef76fa87b0e84627208ca402
salo <salo@pkgsrc.org>
2005-02-17 10:52:46 +0000

    Update to version 0.7.5
    Changes:
    
    - fix the segment family UID
    - no more TrackAngle and new TrackTranslate elements
    - rename ChapterLink* to ChapterTranslate*
    - add KaxChapterLinkEditionUID
    - fix the KaxSegmentFamily flags
    - added KaxSegmentFamily, KaxChapterLink, KaxChapterLinkCodec and
      KaxChapterLinkID
    - Move all of the (re-)activated Matroska v1 elements out of "#if == 2"
      blocks.
    - support for KaxMaxBlockAdditionID and fix default values
    - BlockAdditions is in Matroska 1 now
    - added the button track type
    - Use malloc() and free() instead of new[] and delete[] where the lib might
      deal with pointers that have been allocated by the app, or where the app
      has allocated the memory. Reason: new[]ed memory cannot be realloc()ed.
    - remove support for the gap flag in the Block
    - add almost automatic handling of SilentTracks
    - added KaxTrackFlagForced
    - added KaxTrackAngle and various fixes
    - TrackOverlay is now multiple
    - added KaxClusterSilentTracks and KaxClusterSilentTrackNumber

--- multimedia/libmatroska/buildlink3.mk
+++ multimedia/libmatroska/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/10/14 17:04:35 salo Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2005/02/17 10:52:46 salo Exp $
 #
 # This Makefile fragment is included by packages that use libmatroska.
 #
@@ -16,7 +16,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibmatroska}
 BUILDLINK_PACKAGES+=	libmatroska
 
 .if !empty(LIBMATROSKA_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libmatroska+=	libmatroska>=0.7.4
+BUILDLINK_DEPENDS.libmatroska+=	libmatroska>=0.7.5
 BUILDLINK_PKGSRCDIR.libmatroska?=	../../multimedia/libmatroska
 .endif	# LIBMATROSKA_BUILDLINK3_MK
 
