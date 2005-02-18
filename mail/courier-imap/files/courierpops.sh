28479f43b093e823b5d040965c4165c558d1949d
jlam <jlam@pkgsrc.org>
2005-02-18 22:12:46 +0000

    Update mail/courier-imap to courier-imap-4.0.1.  Changes from version 3.0.5 include:
        * Implemented IMAP and POP3 proxy aggregator.  With a proxy
          aggregator, the mail accounts are split between multiple,
          independent servers, with an IMAP/POP3 server running on each
          individual server.  A separate, proxy server sits in front and
          accepts ordinary IMAP and POP3 connections.  It reads the login
          ID, determines which server the account is located on, connects
          to the server, and logs in.  Then, for the lifetime on the login
          session the front-end server takes itself out of the loop, and
          forwards all session traffic between the IMAP/POP3 client, and
          the back-end server.
    
        * Use courier-authlib for user authentication.
    
        * IPv6 support.
    
        * Minor bug fixes.
    
    Please note when updating that the locations of the userdb files has
    changed to be within the courier-authlib config directory, e.g.
    /usr/pkg/etc/authlib/userdb*.

--- mail/courier-imap/files/courierpops.sh
+++ mail/courier-imap/files/courierpops.sh
@@ -1,21 +1,20 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: courierpops.sh,v 1.10 2004/07/15 08:29:05 jlam Exp $
+# $NetBSD: courierpops.sh,v 1.11 2005/02/18 22:12:46 jlam Exp $
 #
 # Courier POP3/SSL services daemon
 #
 # PROVIDE: courierpops
 # REQUIRE: authdaemond
+# KEYWORD: shutdown
 
-if [ -f /etc/rc.subr ]; then
-	. /etc/rc.subr
-fi
+. /etc/rc.subr
 
 name="courierpops"
 rcvar=${name}
-command="@PREFIX@/libexec/courier/couriertcpd"
-ctl_command="@PREFIX@/libexec/courier/pop3d-ssl.rc"
-pidfile="/var/run/pop3d-ssl.pid"
+command="@PREFIX@/sbin/couriertcpd"
+ctl_command="@PREFIX@/sbin/pop3d-ssl"
+pidfile="@VARBASE@/run/pop3d-ssl.pid"
 required_files="@PKG_SYSCONFDIR@/pop3d @PKG_SYSCONFDIR@/pop3d-ssl"
 required_files="${required_files} @SSLCERTS@/pop3d.pem"
 required_vars="authdaemond"
@@ -59,10 +58,5 @@ courier_doit()
 	${ctl_command} ${action}
 }
 
-if [ -f /etc/rc.subr ]; then
-	load_rc_config $name
-	run_rc_command "$1"
-else
-	@ECHO@ -n " ${name}"
-	exec ${ctl_command} start
-fi
+load_rc_config $name
+run_rc_command "$1"
