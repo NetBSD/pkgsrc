e9f2a6c5775ae77a04c432da23b1065c92861d80
jlam <jlam@pkgsrc.org>
2005-02-18 22:12:50 +0000

    Update mail/courier-maildir to 0.48.2.  Changes from version 0.45.6 include minor bugfixes, and fixing a major typo where the maildiracl and maildirkw utilities were actually just copies of the maildirmake utility.

--- mail/courier-maildir/files/sharedindexupdate.sh
+++ mail/courier-maildir/files/sharedindexupdate.sh
@@ -1,24 +1,23 @@
 #!@SH@
 #
-# $NetBSD: sharedindexupdate.sh,v 1.1.1.1 2004/07/14 20:05:50 jlam Exp $
+# $NetBSD: sharedindexupdate.sh,v 1.2 2005/02/18 22:12:50 jlam Exp $
 #
 # This script updates the index of shared folders on the system and is
 # only needed if Courier-IMAP or SqWebMail used to serve mail and shared
 # folders are used.  If so, then this script should be run regularly as a
 # system process to ensure the shared folder index is kept up-to-date.
 #
-# The courier-auth package must be separately installed to provide the
+# The courier-authlib package must be separately installed to provide the
 # "authenumerate" program used in this script.
 
 sysconfdir="@PKG_SYSCONFDIR@"
 sbindir="@PREFIX@/sbin"
-datadir="@DATADIR@"
 
 @RM@ -rf $sysconfdir/shared.tmp
 @MKDIR@ $sysconfdir/shared.tmp || exit 1
 
 @TEST@ -x $sbindir/authenumerate || exit 1
 $sbindir/authenumerate > $sysconfdir/shared.tmp/.tmplist || exit 1
-$datadir/sharedindexsplit $sysconfdir/shared.tmp 1 < $sysconfdir/shared.tmp/.tmplist || exit 1
+$sysconfdir/sharedindexsplit $sysconfdir/shared.tmp 1 < $sysconfdir/shared.tmp/.tmplist || exit 1
 @RM@ -f $sysconfdir/shared.tmp/.tmplist
-$datadir/sharedindexinstall
+$sysconfdir/sharedindexinstall
