adb42ca4ab89d189fa256c419c6bfac67271be68
wiz <wiz@pkgsrc.org>
2005-02-19 22:46:44 +0000

    Disable gnutls support -- this version of libsoup uses an obsolete interface to gnutls. Bump PKGREVISION and depend on it.

--- net/libsoup/buildlink3.mk
+++ net/libsoup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/10/03 00:17:54 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2005/02/19 22:46:44 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBSOUP_BUILDLINK3_MK:=	${LIBSOUP_BUILDLINK3_MK}+
@@ -11,14 +11,12 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibsoup}
 BUILDLINK_PACKAGES+=	libsoup
 
 .if !empty(LIBSOUP_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libsoup+=	libsoup>=1.99.28
-BUILDLINK_RECOMMENDED.libsoup+=	libsoup>=1.99.28nb2
+BUILDLINK_DEPENDS.libsoup+=	libsoup>=1.99.28nb3
 BUILDLINK_PKGSRCDIR.libsoup?=	../../net/libsoup
 .endif	# LIBSOUP_BUILDLINK3_MK
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/popt/buildlink3.mk"
-.include "../../security/gnutls/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
