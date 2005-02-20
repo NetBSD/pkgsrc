31ec6c76927edb2b691db06bbcfc3d15d55a15c2
cube <cube>
2005-02-20 21:50:57 +0000

    o Add a way to pass special definitions to the compilation, to match kernel   environment, and document it in DESCR by lack of a better place. o Fix a reference to local in (yet unused) bl3.mk file.
    Both issues reported by Hubert Feyrer.

--- net/netbsd-tap/options.mk
+++ net/netbsd-tap/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2005/01/20 18:02:39 cube Exp $
+# $NetBSD: options.mk,v 1.2 2005/02/20 21:50:57 cube Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.netbsd-tap
 PKG_SUPPORTED_OPTIONS=	bpf
@@ -9,3 +9,7 @@ PKG_DEFAULT_OPTIONS?=	bpf
 .if !empty(PKG_OPTIONS:Mbpf)
 BUILD_ENV+=	USE_BPF=YES
 .endif
+
+TAP_KERNEL_ENV?=	# empty
+BUILD_DEFS+=	TAP_KERNEL_ENV
+MAKE_ENV+=	TAP_KERNEL_ENV=${TAP_KERNEL_ENV:Q}
