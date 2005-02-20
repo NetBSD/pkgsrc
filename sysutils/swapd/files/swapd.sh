c3a3998dcdbff4c19e30ef3eaf5d083fc0b13d7e
xtraeme <xtraeme@pkgsrc.org>
2005-02-20 13:56:09 +0000

    Update to 1.0.3
    Made the free/used memory polling time configurable through the
    "delay" option.
    
    pkgsrc changes:
    
    modified the rc.d script to work on systems without the NetBSD's rc.d
    system.

--- sysutils/swapd/files/swapd.sh
+++ sysutils/swapd/files/swapd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: swapd.sh,v 1.3 2004/11/29 16:19:19 xtraeme Exp $
+# $NetBSD: swapd.sh,v 1.4 2005/02/20 13:56:09 xtraeme Exp $
 #
 # PROVIDE: swapd
 # REQUIRE: DAEMON LOGIN
@@ -12,10 +12,45 @@ required_files="@PKG_SYSCONFDIR@/${name}.conf"
 pidfile="@VARBASE@/run/${name}.pid"
 sig_stop="KILL"
 
-. /etc/rc.subr
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
+fi
 
-load_rc_config $name
-run_rc_command "$1"
+if [ -f /etc/rc.subr ]; then
+	load_rc_config $name
+	run_rc_command "$1"
+else
+	case ${1:-start} in
+	start)
+		if [ -x ${command} ]; then
+			echo "Starting ${command}."
+			eval ${command} ${distccd_flags} ${command_args}
+		fi
+		;;
+	stop)
+		if [ -f ${pidfile} ]; then
+			pid=`/bin/head -1 ${pidfile}`
+			echo "Stopping ${name}."
+			kill -${sig_stop} ${pid}
+		else
+			echo "${name} not running?"
+		fi
+		;;
+	restart)
+		( $0 stop )
+		sleep 1
+		$0 start
+		;;
+	status)
+		if [ -f ${pidfile} ]; then
+			pid=`/bin/head -1 ${pidfile}`
+			echo "${name} is running as pid ${pid}."
+		else
+			echo "${name} is not running."
+		fi
+		;;
+	esac
+fi
 
 if [ "$1" != "stop" -o "$1" != "status" ]; then
 	echo $(check_process $command) > $pidfile
