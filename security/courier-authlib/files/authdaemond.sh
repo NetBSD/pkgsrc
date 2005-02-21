f43f0141a2038847c88af06fef555fe60ed0f648
jlam <jlam@pkgsrc.org>
2005-02-21 17:08:39 +0000

    Fix the authdaemond rc.d script to invoke the correct script to start the authentication daemon.  Fix provided by Inseo Park in private mail. Also, honor VARBASE.  Bump the PKGREVISION to 1.

--- security/courier-authlib/files/authdaemond.sh
+++ security/courier-authlib/files/authdaemond.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: authdaemond.sh,v 1.1.1.1 2005/02/10 03:21:31 jlam Exp $
+# $NetBSD: authdaemond.sh,v 1.2 2005/02/21 17:08:39 jlam Exp $
 #
 # Courier user authentication daemon
 #
@@ -12,7 +12,7 @@
 name="authdaemond"
 rcvar=${name}
 command="@PREFIX@/sbin/courierlogger"
-ctl_command="@PREFIX@/@AUTHLIBEXECDIR@/authdaemond"
+ctl_command="@PREFIX@/sbin/authdaemond"
 pidfile="@AUTHDAEMONVAR@/pid"
 required_files="@PKG_SYSCONFDIR@/authdaemonrc"
 
