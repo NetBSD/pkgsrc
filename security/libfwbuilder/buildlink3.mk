081cf2bccfbbf58b2cf78ec4e76aeb073f612da1
bad <bad@pkgsrc.org>
2005-02-24 10:59:56 +0000

    Replace libfwbuilder with version 2.0.5 as the previous version wasn't useful.
    Firewall Builder is a multi-platform firewall configuration and
    management tool.  It consists of a GUI and a set of policy compilers for
    various firewall platforms.  Firewall Builder uses an object-oriented
    approach, it helps administrators maintain a database of network
    objects and allows policy editing using simple drag-and-drop
    operations.  Firewall Builder currently supports
    
    	iptables,
    	IP Filter,
    	ipfw,
    	OpenBSD PF, and
    	Cisco PIX
    
    libfwbuilder provides the back-end functionality in a library.

--- security/libfwbuilder/buildlink3.mk
+++ security/libfwbuilder/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/07 16:27:13 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/02/24 10:59:56 bad Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBFWBUILDER_BUILDLINK3_MK:=	${LIBFWBUILDER_BUILDLINK3_MK}+
@@ -11,14 +11,15 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibfwbuilder}
 BUILDLINK_PACKAGES+=	libfwbuilder
 
 .if !empty(LIBFWBUILDER_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libfwbuilder+=	libfwbuilder>=1.0.2
-BUILDLINK_RECOMMENDED.libfwbuilder+=	libfwbuilder>=1.0.2nb2
+BUILDLINK_DEPENDS.libfwbuilder+=	libfwbuilder-2.0.5
 BUILDLINK_PKGSRCDIR.libfwbuilder?=	../../security/libfwbuilder
 .endif	# LIBFWBUILDER_BUILDLINK3_MK
 
-.include "../../devel/glib/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
 .include "../../net/net-snmp/buildlink3.mk"
 .include "../../textproc/libxml/buildlink3.mk"
 .include "../../textproc/libxslt/buildlink3.mk"
+.include "../../x11/qt3-libs/buildlink3.mk"
+.include "../../x11/qt3-tools/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
