c0aa5fc27e102abfcff7e2063b84d94e8b484335
kim <kim@pkgsrc.org>
2005-02-28 02:21:20 +0000

    Upgrade to libshout 2.1, from Juan RP (xtraeme@) in private mail. Bug fixes, no API changes.

--- audio/libshout/buildlink3.mk
+++ audio/libshout/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/10/03 00:13:07 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2005/02/28 02:21:20 kim Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBSHOUT_BUILDLINK3_MK:=	${LIBSHOUT_BUILDLINK3_MK}+
@@ -12,11 +12,12 @@ BUILDLINK_PACKAGES+=	libshout
 
 .if !empty(LIBSHOUT_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libshout+=	libshout>=2.0
-BUILDLINK_RECOMMENDED.libshout+=	libshout>=2.0nb1
+BUILDLINK_RECOMMENDED.libshout+=libshout>=2.0nb1
 BUILDLINK_PKGSRCDIR.libshout?=	../../audio/libshout
 .endif	# LIBSHOUT_BUILDLINK3_MK
 
 .include "../../audio/libvorbis/buildlink3.mk"
+.include "../../multimedia/libtheora/buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
