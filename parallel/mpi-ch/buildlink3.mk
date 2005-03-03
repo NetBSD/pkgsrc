5bcba1878d902fb177149a6aade6f933ef25fe6f
garbled <garbled@pkgsrc.org>
2005-03-03 12:01:30 +0000

    update mpich to 1.2.6, with most work provided by Phil Nelson in PR pkg/27273 with some minor PLIST and etc fixes by me.

--- parallel/mpi-ch/buildlink3.mk
+++ parallel/mpi-ch/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/05 19:25:39 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/03/03 12:01:30 garbled Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 MPICH_BUILDLINK3_MK:=	${MPICH_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmpich}
 BUILDLINK_PACKAGES+=	mpich
 
 .if !empty(MPICH_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.mpich+=	mpich>=1.2.5.2
+BUILDLINK_DEPENDS.mpich+=	mpich>=1.2.6
 BUILDLINK_PKGSRCDIR.mpich?=	../../parallel/mpi-ch
 .endif	# MPICH_BUILDLINK3_MK
 
