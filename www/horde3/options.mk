3dd9d4a68c6b23f9e1321e68c3e9d0a2f27adc4d
adrianp <adrianp@pkgsrc.org>
2005-03-04 22:35:22 +0000

    - Update horde to 3.0.3 - Whitespace police - PLIST sorting and fixing - Fix postgresql support to now extend to 8.0 as noted in PR# 29590 by    jaapb (at) kerguelen.org, thanks for the PR.
    > Major changes compared to the Horde version 3.0.3-RC1 are:
    >     * Added support for clearing user preferences to LDAP driver.
    >     * Minor bug fixes and improvements.
    >
    > Major changes compared to the Horde version 3.0.2 are:
    >     * Improved layout of wrapping menus in Gecko and KHTML based browsers.
    >     * Fixed some session handler issues.
    >     * Fixed caching in the Version Control library, used by Chora.
    >     * Fixed updating of IMAP folder trees when (un)subscribing IMAP folders.
    >     * Improved performance of DataTree library, used by IMP's message history.
    >     * Changed log level of logins and logouts.
    >     * Updated German and Japanese translations.
    >     * Minor bug and layout fixes.
    >

--- www/horde3/options.mk
+++ www/horde3/options.mk
@@ -1,20 +1,12 @@
-# $NetBSD: options.mk,v 1.1.1.1 2005/01/16 21:10:16 adrianp Exp $
+# $NetBSD: options.mk,v 1.2 2005/03/04 22:35:22 adrianp Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.horde
-PKG_SUPPORTED_OPTIONS=	ldap postgresql73 postgresql74 mysql
+PKG_SUPPORTED_OPTIONS=	ldap postgresql mysql
 
 .if !defined(PKG_OPTIONS.horde)
 PKG_DEFAULT_OPTIONS+=	mysql
 .endif
 .include "../../mk/bsd.options.mk"
-
-###
-### Can't support both versions of PostgreSQL
-###
-.if !empty(PKG_OPTIONS:Mpostgresql73) && !empty(PKG_OPTIONS:Mpostgresql74)
-PKG_FAIL_REASON+=	"PostgreSQL 7.3 and 7.4 cannot both be compiled in." \
-			"Please change ${PKG_OPTIONS_VAR} to one or the other."
-.endif
 
 ###
 ### Use OpenLDAP for storing user details
@@ -25,18 +17,10 @@ DEPENDS+=	php-ldap>=4.3.3:../../databases/php-ldap
 .endif
 
 ###
-### Use PostgreSQL v7.3 for storing user details
+### Use PostgreSQL for storing user details
 ###
-.if !empty(PKG_OPTIONS:Mpostgresql73)
-.	include "../../databases/postgresql73-lib/buildlink3.mk"
-DEPENDS+=	php-pgsql>=4.3.3:../../databases/php-pgsql
-.endif
-
-###
-### Use PostgreSQL v7.4 for storing user details
-###
-.if !empty(PKG_OPTIONS:Mpostgresql74)
-.	include "../../databases/postgresql74-lib/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mpostgresql)
+.	include "../../mk/pgsql.buildlink3.mk"
 DEPENDS+=	php-pgsql>=4.3.3:../../databases/php-pgsql
 .endif
 
