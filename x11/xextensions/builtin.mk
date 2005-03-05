eca9545c0c6f497eec6ec486a63b72e190ef0e57
wiz <wiz@pkgsrc.org>
2005-03-05 18:16:21 +0000

    Instead of disabling the randrext and Xrandr packages on systems which have no extutil.h header file, just make them depend on the xextensions package which provides it. Bump PKGREVISION for Xrandr and randrext. Add builtin.mk file for xextensions.
    jlam says ok.

---
+++ x11/xextensions/builtin.mk
@@ -0,0 +1,46 @@
+# $NetBSD: builtin.mk,v 1.1 2005/03/05 18:16:21 wiz Exp $
+
+_X11_EXTENSIONS_EXTUTIL_H=	${X11BASE}/include/X11/extensions/extutil.h
+
+.if !defined(IS_BUILTIN.xextensions)
+IS_BUILTIN.xextensions=	no
+.  if exists(${_X11_EXTENSIONS_EXTUTIL_H})
+IS_BUILTIN.xextensions=	yes
+# hardcode version for now, since there currently is only one, really
+BUILTIN_PKG.xextensions=	xextensions-1.0.1
+BUILDLINK_VARS+=	BUILTIN_PKG.xextensions
+.  endif
+BUILDLINK_VARS+=	IS_BUILTIN.xextensions
+.endif	# IS_BUILTIN.xextensions
+
+.if !defined(USE_BUILTIN.xextensions)
+USE_BUILTIN.xextensions?=	${IS_BUILTIN.xextensions}
+
+.  if defined(BUILTIN_PKG.xextensions)
+USE_BUILTIN.xextensions=	yes
+.    for _depend_ in ${BUILDLINK_DEPENDS.xextensions}
+.      if !empty(USE_BUILTIN.xextensions:M[yY][eE][sS])
+USE_BUILTIN.xextensions!=		\
+	if ${PKG_ADMIN} pmatch '${_depend_}' ${BUILTIN_PKG.xextensions}; then \
+		${ECHO} "yes";						\
+	else								\
+		${ECHO} "no";						\
+	fi
+.      endif
+.    endfor
+.  endif
+.endif	# USE_BUILTIN.xextensions
+
+CHECK_BUILTIN.xextensions?=	no
+.if !empty(CHECK_BUILTIN.xextensions:M[nN][oO])
+
+.if !empty(USE_BUILTIN.xextensions:M[nN][oO])
+BUILDLINK_DEPENDS.xextensions+=	xextensions>=1.0
+.endif
+
+.if !empty(USE_BUILTIN.xextensions:M[yY][eE][sS])
+BUILDLINK_PREFIX.xextensions=	${X11BASE}
+USE_X11=			yes
+.endif
+
+.endif	# CHECK_BUILTIN.xextensions
