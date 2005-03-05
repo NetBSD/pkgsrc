a437583ff45ebd2f353c6132f0962417f931e00e
wiz <wiz@pkgsrc.org>
2005-03-05 11:26:48 +0000

    Dependencies should look like: foo-[0-9]* (instead of foo-*). Fix some.

--- inputmethod/skk/options.mk
+++ inputmethod/skk/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2005/03/05 11:19:10 uebayasi Exp $
+# $NetBSD: options.mk,v 1.2 2005/03/05 11:26:48 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.skk
 PKG_SUPPORTED_OPTIONS=	lookup
@@ -6,7 +6,7 @@ PKG_SUPPORTED_OPTIONS=	lookup
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mlookup)
-BUILD_DEPENDS+=	${PKGNAME_PREFIX}lookup-*:../../misc/lookup
+BUILD_DEPENDS+=	${PKGNAME_PREFIX}lookup-[0-9]*:../../misc/lookup
 PLIST_SUBST+=	FOR_LOOKUP=""
 .else
 PLIST_SUBST+=	FOR_LOOKUP="@comment "
