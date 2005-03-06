bab1e78c17ef32cb319dfee49242ad4eecaaf161
adrianp <adrianp@pkgsrc.org>
2005-03-06 11:10:29 +0000

    - Update libol to 0.3.15 - Requested in PR# 29610 by joel (at) carnat.net, thanks for the PR!
    > 2005-02-04  Balazs Scheidler  <bazsi@bzorp.balabit>
    >
    >         * configure.in: bumped verison number to 0.3.15
    >
    > 2005-02-01  Balazs Scheidler  <bazsi@bzorp.balabit>
    >
    >         * src/werror.c (msg_vformat): fixed signed/unsigned comparison wrt
    >         the length variable, a stack overflow (not buffer overflow) can be
    >         caused by a very long format result (werror("%2147483648i", 1)),
    >         credits go to Thief_hu for reporting it; it is believed not to be
    >         exploitable as werror format strings do not contain dynamic values
    >
    > 2004-08-05  Balazs Scheidler  <bazsi@bzorp.balabit>
    >
    >         * configure.in: bumped version number to 0.3.14, cleaned up to
    >         work with autoconf2.50 and automake 1.7
    >
    >         exploitable as werror format strings do not contain dynamic values
    >
    > 2004-08-05  Balazs Scheidler  <bazsi@bzorp.balabit>
    >
    >         * configure.in: bumped version number to 0.3.14, cleaned up to
    >         work with autoconf2.50 and automake 1.7
    >
    >         * src/objtypes.h: removed colliding macro names to avoid warnings
    >         on BSD derived systems
    >
    > 2004-07-04  Balazs Scheidler  <bazsi@bzorp.balabit>
    >
    >         * src/io.c (get_inaddr): use in_addr_t for the private definition
    >         of INADDR_NONE if one is not found.
    >
    > 2004-05-06  Balazs Scheidler  <bazsi@bzorp.balabit>
    >
    >         * added libol.spec.in patch by jpo
    >
    > 2004-05-04  Balazs Scheidler  <bazsi@bzorp.balabit>
    >
    >         * configure.in: use CFLAGS from environment if set

--- sysutils/libol/buildlink3.mk
+++ sysutils/libol/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/10/03 00:18:16 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2005/03/06 11:10:29 adrianp Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBOL_BUILDLINK3_MK:=	${LIBOL_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibol}
 BUILDLINK_PACKAGES+=	libol
 
 .if !empty(LIBOL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libol+=	libol>=0.3.11
-BUILDLINK_RECOMMENDED.libol+=	libol>=0.3.13nb1
+BUILDLINK_DEPENDS.libol+=	libol>=0.3.15
 BUILDLINK_PKGSRCDIR.libol?=	../../sysutils/libol
 .endif	# LIBOL_BUILDLINK3_MK
 
