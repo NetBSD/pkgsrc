b635276311fff15fdc141b88fb2bd7070966d7e9
salo <salo@pkgsrc.org>
2005-03-09 19:16:39 +0000

    Pullup ticket 339 - requested by Shin'ichiro TAYA security fix for firefox
    Patch supplied by submitter, equals to:
    
       Module Name:		pkgsrc
       Committed By:	taya
       Date:		Sun Feb 27 13:20:43 UTC 2005
    
       Log Message:
       Update firefox to 1.0.1.
    
       Changes from release notes:
    
       * Improved stability
       * International Domain Names are now displayed as punycode.
         (To show International Domain Names in Unicode, set the
         "network.IDN_show_punycode" preference to false.)
       * Several security fixes.
       MFSA 2005-29  Internationalized Domain Name (IDN) homograph spoofing
       MFSA 2005-28 Unsafe /tmp/plugtmp directory exploitable to erase user's files
       MFSA 2005-27 Plugins can be used to load privileged content
       MFSA 2005-26 Cross-site scripting by dropping javascript: link on tab
       MFSA 2005-25 Image drag and drop executable spoofing
       MFSA 2005-24 HTTP auth prompt tab spoofing
       MFSA 2005-23 Download dialog source spoofing
       MFSA 2005-22 Download dialog spoofing using Content-Disposition header
       MFSA 2005-21 Overwrite arbitrary files downloading .lnk twice
       MFSA 2005-20 XSLT can include stylesheets from arbitrary hosts
       MFSA 2005-19 Autocomplete data leak
       MFSA 2005-18 Memory overwrite in string library
       MFSA 2005-17 Install source spoofing with user:pass@host
       MFSA 2005-16 Spoofing download and security dialogs with overlapping windows
       MFSA 2005-15 Heap overflow possible in UTF8 to Unicode conversion
       MFSA 2005-14 SSL "secure site" indicator spoofing
       MFSA 2005-13 Window Injection Spoofing

--- www/firefox-gtk2/buildlink3.mk
+++ www/firefox-gtk2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/11/17 02:34:53 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.1.2.1 2005/03/09 19:16:39 salo Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 FIREFOX_GTK2_BUILDLINK3_MK:=	${FIREFOX_GTK2_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nfirefox-gtk2}
 BUILDLINK_PACKAGES+=	firefox-gtk2
 
 .if !empty(FIREFOX_GTK2_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.firefox-gtk2+=	firefox-gtk2>=1.0nb1
+BUILDLINK_DEPENDS.firefox-gtk2+=	firefox-gtk2>=1.0.1
 BUILDLINK_PKGSRCDIR.firefox-gtk2?=	../../www/firefox-gtk2
 .endif	# FIREFOX_GTK2_BUILDLINK3_MK
 
--- www/firefox/buildlink3.mk
+++ www/firefox/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/11/17 02:34:53 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.1.2.1 2005/03/09 19:16:39 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 FIREFOX_BUILDLINK3_MK:=	${FIREFOX_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nfirefox}
 BUILDLINK_PACKAGES+=	firefox
 
 .if !empty(FIREFOX_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.firefox+=	firefox>=1.0nb1
+BUILDLINK_DEPENDS.firefox+=	firefox>=1.0.1
 BUILDLINK_PKGSRCDIR.firefox?=	../../www/firefox
 .endif	# FIREFOX_BUILDLINK3_MK
 
