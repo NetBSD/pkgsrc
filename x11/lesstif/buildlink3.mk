abcbb52185582176640dbea8979d63e8c952f99a
snj <snj@pkgsrc.org>
2005-03-11 06:55:42 +0000

    Pullup ticket 352 - requested by Lubomir Sedlacik security fix for lesstif
    Revisions pulled up:
    - pkgsrc/x11/lesstif/Makefile		1.77,1.78
    - pkgsrc/x11/lesstif/buildlink3.mk	1.4
    - pkgsrc/x11/lesstif/distinfo		1.11,1.13
    - pkgsrc/x11/lesstif/PLIST		1.9
    - pkgsrc/x11/lesstif/patches/patch-ab	1.16
    - pkgsrc/x11/lesstif/patches/patch-ac	1.17
    
    
        Module Name:    pkgsrc
        Committed By:   adam
        Date:           Wed Jan  5 11:16:38 UTC 2005
    
        Modified Files:
                pkgsrc/x11/lesstif: Makefile distinfo
    
        Log Message:
        Changes 0.94.0:
        * XPM security fixes
        * memory leak fixes
        * other fixes
    
        ----
    
        Module Name:    pkgsrc
        Committed By:   wiz
        Date:           Thu Mar 10 16:07:16 UTC 2005
    
        Modified Files:
                pkgsrc/x11/lesstif: PLIST
    
        Log Message:
        Sort.
    
        ----
    
        Module Name:    pkgsrc
        Committed By:   wiz
        Date:           Thu Mar 10 16:07:27 UTC 2005
    
        Modified Files:
                pkgsrc/x11/lesstif: Makefile distinfo
        Added Files:
                pkgsrc/x11/lesstif/patches: patch-ab patch-ac
    
        Log Message:
        Add patch to fix CAN-2005-0605. Bump PKGREVISION.
    
        ----
    
        Module Name:    pkgsrc
        Committed By:   salo
        Date:           Fri Mar 11 00:34:19 UTC 2005
    
        Modified Files:
                pkgsrc/x11/lesstif: buildlink3.mk
    
        Log Message:
        Bump BUILDLINK_RECOMMENDED for security update. (hi wiz!)

--- x11/lesstif/buildlink3.mk
+++ x11/lesstif/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/10/03 00:18:36 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3.2.1 2005/03/11 06:55:42 snj Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LESSTIF_BUILDLINK3_MK:=	${LESSTIF_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	lesstif
 
 .if !empty(LESSTIF_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.lesstif+=	lesstif>=0.91.4
-BUILDLINK_RECOMMENDED.lesstif+=	lesstif>=0.93.96nb1
+BUILDLINK_RECOMMENDED.lesstif+=	lesstif>=0.94.0nb1
 BUILDLINK_PKGSRCDIR.lesstif?=	../../x11/lesstif
 .endif	# LESSTIF_BUILDLINK3_MK
 
