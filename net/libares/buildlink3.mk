2ddff048ce3690f775d448f1be1c9cff0c049e57
tv <tv@pkgsrc.org>
2005-03-12 05:42:48 +0000

    Make build on Interix.
    Part of the fix adds <sys/time.h> to ares.h to make it get fd_set on more
    platforms, so bump PKGREVISION.

--- net/libares/buildlink3.mk
+++ net/libares/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/04/25 00:34:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/03/12 05:42:48 tv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBARES_BUILDLINK3_MK:=	${LIBARES_BUILDLINK3_MK}+
@@ -10,10 +10,19 @@ BUILDLINK_DEPENDS+=	libares
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibares}
 BUILDLINK_PACKAGES+=	libares
 
+.include "../../mk/bsd.prefs.mk"
+
 .if !empty(LIBARES_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libares+=	libares>=1.1.1
 BUILDLINK_PKGSRCDIR.libares?=	../../net/libares
 BUILDLINK_DEPMETHOD.libares?=	build
 .endif	# LIBARES_BUILDLINK3_MK
+
+.if ${OPSYS} == "Interix"
+BUILDLINK_PASSTHRU_DIRS+=	/usr/local/include/bind /usr/local/lib/bind
+BUILDLINK_CPPFLAGS.libares+=	-I/usr/local/include/bind
+BUILDLINK_LDFLAGS.libares+=	-L/usr/local/lib/bind
+BUILDLINK_LIBS.libares+=	-lbind -ldb
+.endif
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
