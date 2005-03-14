c98e3240fb6f88535f4cf0631a43b57575ba6ecc
tv <tv>
2005-03-14 19:48:52 +0000

    Use BUILDLINK_PREFIX.groff, not a local _GROFFPATH.

--- textproc/groff/builtin.mk
+++ textproc/groff/builtin.mk
@@ -1,15 +1,13 @@
-# $NetBSD: builtin.mk,v 1.2 2004/07/09 12:30:17 drochner Exp $
+# $NetBSD: builtin.mk,v 1.3 2005/03/14 19:48:52 tv Exp $
 
 .if !defined(IS_BUILTIN.groff)
 .  if exists(/usr/bin/groff)
 IS_BUILTIN.groff=	yes
-_GROFFPATH=		/usr/bin
 .  else
 IS_BUILTIN.groff=	no
-_GROFFPATH=		${LOCALBASE}/bin
 .  endif
 .endif  # IS_BUILTIN.groff
 
 USE_BUILTIN.groff?=	${IS_BUILTIN.groff}
-GROFF?=			${_GROFFPATH}/groff
-TBL?=			${_GROFFPATH}/tbl
+GROFF?=			${BUILDLINK_PREFIX.groff}/bin/groff
+TBL?=			${BUILDLINK_PREFIX.groff}/bin/tbl
