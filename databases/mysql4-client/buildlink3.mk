42a6500467d55c2e6b4d4490b87077e945b255e6
xtraeme <xtraeme@pkgsrc.org>
2005-03-15 15:09:14 +0000

    Update to 4.1.10a.
    - Fix potential security vulnerabilities in the creation of temporary
      table file names and the handling of User Defined Functions (UDFs).
    
    More info: http://www.k-otik.com/english/advisories/2005/0252
    
    Increased BUILDLINK_RECOMMENDED to 4.1.10a.

--- databases/mysql4-client/buildlink3.mk
+++ databases/mysql4-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2005/01/20 13:37:48 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2005/03/15 15:09:14 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MYSQL_CLIENT_BUILDLINK3_MK:=	${MYSQL_CLIENT_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	mysql-client
 
 .if !empty(MYSQL_CLIENT_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.mysql-client+=	mysql-client>=4.1.7
-BUILDLINK_RECOMMENDED.mysql-client+=	mysql-client>=4.1.9nb1
+BUILDLINK_RECOMMENDED.mysql-client+=	mysql-client>=4.1.10a
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql4-client
 BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
 .endif	# MYSQL_CLIENT_BUILDLINK3_MK
