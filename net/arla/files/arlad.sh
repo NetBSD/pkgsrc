9f1c532d9b71873afe95eb95d0550d6c8fbe395a
wennmach <wennmach@pkgsrc.org>
2005-03-15 15:58:52 +0000

    Update arla to 0.38. The update is necessary to support NetBSD-2.0. The update during the pkgsrc freeze has been approved by <wiz>.
    Two patches have been provided by <christos>.
    
    Tested on NetBSD-1.6.1/i386 and NetBSD-2.0/i386.
    
    Closes PR pkg/20906.

--- net/arla/files/arlad.sh
+++ net/arla/files/arlad.sh
@@ -1,25 +1,18 @@
-#!/bin/sh
+#!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: arlad.sh,v 1.4 2002/09/25 00:06:34 jlam Exp $
+# $NetBSD: arlad.sh,v 1.5 2005/03/15 15:58:52 wennmach Exp $
 #
 # PROVIDE: arlad
 # REQUIRE: beforemountlkm
+# KEYWORD: shutdown
 
-if [ -f /etc/rc.subr ]; then
-	. /etc/rc.subr
-fi
+. /etc/rc.subr
 
 name="arlad"
 rcvar=$name
-command="@PREFIX@/libexec/${name}"
-command_args="-z /dev/xfs0"
-required_files="/dev/xfs0"
+command="/usr/pkg/libexec/${name}"
+command_args="-z /dev/nnpfs0"
+required_files="/dev/nnpfs0"
 
-if [ -f /etc/rc.subr -a -d /etc/rc.d -a -f /etc/rc.d/DAEMON ]; then
-	load_rc_config $name
-	eval [ -z "\$${rcvar}" ] && eval ${rcvar}=NO
-	run_rc_command "$1"
-else
-	echo -n ' ${name}'
-	${command} ${arlad_flags} ${command_args}
-fi
+load_rc_config $name
+run_rc_command "$1"
