aebb385838254bff2661f0ec2c79183343521297
jmmv <jmmv@pkgsrc.org>
2005-03-16 21:14:46 +0000

    Include dbus's buildlink3.mk file (needed at build-time, specially because of the dbus.pc file).

--- sysutils/dbus-glib/buildlink3.mk
+++ sysutils/dbus-glib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/10/11 17:46:41 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/03/16 21:14:46 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 DBUS_GLIB_BUILDLINK3_MK:=	${DBUS_GLIB_BUILDLINK3_MK}+
@@ -16,6 +16,7 @@ BUILDLINK_PKGSRCDIR.dbus-glib?=	../../sysutils/dbus-glib
 .endif	# DBUS_GLIB_BUILDLINK3_MK
 
 .include "../../devel/glib2/buildlink3.mk"
+.include "../../sysutils/dbus/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
