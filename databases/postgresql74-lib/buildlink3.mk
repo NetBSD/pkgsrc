48932065a89bcb8fe78e50ed7f970e2ab6a40a6f
jlam <jlam@pkgsrc.org>
2005-03-18 19:57:31 +0000

    PostgreSQL doesn't provide *.la files that include the library dependencies needed when linking PostgreSQL clients with -lpq.  Define a BUILDLINK_LDADD.${PGSQL_TYPE} variable that lists the full set libraries that would be needed to link with -lpq.

--- databases/postgresql74-lib/buildlink3.mk
+++ databases/postgresql74-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/12/02 10:41:33 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2005/03/18 19:57:31 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 POSTGRESQL74_LIB_BUILDLINK3_MK:=	${POSTGRESQL74_LIB_BUILDLINK3_MK}+
@@ -15,6 +15,9 @@ BUILDLINK_DEPENDS.postgresql74-lib+=		postgresql{74,}-lib>=7.4.0
 BUILDLINK_RECOMMENDED.postgresql74-lib+=	postgresql74-lib>=7.4.3
 BUILDLINK_PKGSRCDIR.postgresql74-lib?=	../../databases/postgresql74-lib
 BUILDLINK_INCDIRS.postgresql74-lib?=	include/postgresql
+
+# This variable contains the libraries need to link most clients.
+BUILDLINK_LDADD.postgresql74-lib=	-lpq ${BUILDLINK_LDADD.gettext}
 .endif	# POSTGRESQL74_LIB_BUILDLINK3_MK
 
 .include "../../security/openssl/buildlink3.mk"
--- databases/postgresql80-lib/buildlink3.mk
+++ databases/postgresql80-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2005/02/06 13:21:45 taca Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/03/18 19:57:31 jlam Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 POSTGRESQL80_LIB_BUILDLINK3_MK:=	${POSTGRESQL80_LIB_BUILDLINK3_MK}+
@@ -17,6 +17,8 @@ BUILDLINK_PKGSRCDIR.postgresql80-lib?=	../../databases/postgresql80-lib
 BUILDLINK_INCDIRS.postgresql80-lib+=	pgsql80/include
 BUILDLINK_LIBDIRS.postgresql80-lib+=	pgsql80/lib
 
+# This variable contains the libraries need to link most clients.
+BUILDLINK_LDADD.postgresql80-lib=	-lpq ${BUILDLINK_LDADD.gettext}
 .endif	# POSTGRESQL80_LIB_BUILDLINK3_MK
 
 .include "../../security/openssl/buildlink3.mk"
