025c70d9c3a4194d2321344486e5a36fddaeaceb
tv <tv@pkgsrc.org>
2005-03-18 18:16:34 +0000

    Fix a gleaming illustration of why using YES/NO variables everywhere possible, just for the sake of doing so, is not a good thing to do:
    The platform files define _STRIPFLAG_* to determine whether to strip things.
    But since this is included in bsd.prefs.mk, ".if ..." checks cannot take
    things set in the Makefile into account.  So convert INSTALL_UNSTRIPPED=YES
    to a defined/undefined variable check in bsd.pkg.mk, and use the :D:U idiom
    in the _STRIPFLAG_* variables.
    
    This should fix PR pkg/28772 and PR pkg/29031.

--- mk/platform/OpenBSD.mk
+++ mk/platform/OpenBSD.mk
@@ -1,4 +1,4 @@
-# $NetBSD: OpenBSD.mk,v 1.11 2005/01/28 21:05:59 jlam Exp $
+# $NetBSD: OpenBSD.mk,v 1.12 2005/03/18 18:16:35 tv Exp $
 #
 # Variable definitions for the OpenBSD operating system.
 
@@ -138,10 +138,8 @@ _USE_RPATH=		yes	# add rpath to LDFLAGS
 _OPSYS_WHOLE_ARCHIVE_FLAG=	-Wl,--whole-archive
 _OPSYS_NO_WHOLE_ARCHIVE_FLAG=	-Wl,--no-whole-archive
 
-.if (!defined(INSTALL_UNSTRIPPED) || empty(INSTALL_UNSTRIPPED:M[yY][eE][sS])) && !defined(DEBUG_FLAGS)
-_STRIPFLAG_CC?=		-s	# cc(1) option to strip
-_STRIPFLAG_INSTALL?=	-s	# install(1) option to strip
-.endif
+_STRIPFLAG_CC?=		${_INSTALL_UNSTRIPPED:D:U-s}	# cc(1) option to strip
+_STRIPFLAG_INSTALL?=	${_INSTALL_UNSTRIPPED:D:U-s}	# install(1) option to strip
 
 .if (${MACHINE_ARCH} == alpha)
 DEFAULT_SERIAL_DEVICE?=	/dev/ttyC0
