44372f8a901093cb45c7c9e4de533b92c37ca221
jmmv <jmmv@pkgsrc.org>
2005-03-20 17:52:20 +0000

    Update to 0.56, latest stable version.  Also add some options to customize the build.  Closes PR pkg/28646 by pancake@phreaker.net.
    Changes:
    - Solve ERROR_ vs LOG_ conflicts
    - Add cache code into HSML (sepharad)
    - Remove silly code in Date Format
    - Hsml code fragmented into language modules: c, perl, brainfuck and python
      modules

---
+++ www/screws/options.mk
@@ -0,0 +1,28 @@
+# $NetBSD: options.mk,v 1.1 2005/03/20 17:52:20 jmmv Exp $
+#
+
+PKG_DEFAULT_OPTIONS+=	inet6 lua ssl
+PKG_OPTIONS_VAR=	PKG_OPTIONS.screws
+PKG_SUPPORTED_OPTIONS=	inet6 lua ssl
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Minet6)
+MAKE_ENV+=	IPV6=1
+.else
+MAKE_ENV+=	IPV6=0
+.endif
+
+.if !empty(PKG_OPTIONS:Mlua)
+.include "../../lang/lua/buildlink3.mk"
+MAKE_ENV+=	INCORE_LUA=1
+.else
+MAKE_ENV+=	INCORE_LUA=0
+.endif
+
+.if !empty(PKG_OPTIONS:Mssl)
+.include "../../security/openssl/buildlink3.mk"
+MAKE_ENV+=	SSL=1
+.else
+MAKE_ENV+=	SSL=0
+.endif
