26ea49f78cdc2db87978466111dc7960517aafcf
jmmv <jmmv@pkgsrc.org>
2005-03-22 16:03:37 +0000

    Update to 2.10.0:
    Mars 7, 2005: Overview of Changes from 2.9.92 to 2.10.0
    =======================================================
    * Updated i18n.
    
    February 28, 2005: Overview of Changes from 2.9.90 to 2.9.92
    ============================================================
    * *BSD updated by marcus@freebsd.org (Joe Marcus Clarke).
    * Removed a lot of dead files :)
    * Don't build&install libgtop_daemon when it's never used.
    
    February 6, 2004: Overview of Changes from 2.9.90 to 2.9.91
    ==========================================================
    * Fixed typo in stub.
    * Updated i18n.
    
    January 24, 2004: Overview of Changes from 2.9.4 to 2.9.90
    ==========================================================
    * Updated Linux support.
    
    January 10, 2004: Overview of Changes from 2.9.3 to 2.9.4
    =========================================================
    * New glibtop_proc_open_files nick@reloco.com.ar (Nicolás Lichtmaier).
    * Updated AIX support (Christophe Belle).
    
    December 20, 2004: Overview of Changes from 2.9.0 to 2.9.3
    ==========================================================
    * Removed libgtop_names.
    * Fixed Argument passing between library and server. #139809.
    * *BSD updated by marcus@freebsd.org (Joe Marcus Clarke).
    * glibtop_get_mountlist(False) now ignores 'unknown' fs.

--- sysutils/libgtop2/buildlink3.mk
+++ sysutils/libgtop2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/09/21 16:49:46 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/03/22 16:03:37 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGTOP2_BUILDLINK3_MK:=	${LIBGTOP2_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgtop2}
 BUILDLINK_PACKAGES+=	libgtop2
 
 .if !empty(LIBGTOP2_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libgtop2+=	libgtop2>=2.8.0
+BUILDLINK_DEPENDS.libgtop2+=	libgtop2>=2.10.0
 BUILDLINK_PKGSRCDIR.libgtop2?=	../../sysutils/libgtop2
 .endif	# LIBGTOP2_BUILDLINK3_MK
 
