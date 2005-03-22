36201a6e431d3c25b7a0fce5b4a8cedb35e88d16
jmmv <jmmv@pkgsrc.org>
2005-03-22 16:09:27 +0000

    Update gnome2-pixmaps and gnome-desktop to 2.10.0:
    ==============
    Version 2.10.0
    ==============
    
      Translators
    
    	* Gabor Kelemen (hu)
    	* Luca Ferretti (it)
    	* Žygimantas Beručka (lt)
    	* Dan Damian (ro)
    	* Leonid Kanter (ru)
    	* Данило Шеган (sr)
    	* Canonical Ltd (xh)
    	* Abel Cheung (zh_TW)
    
    ==============
    Version 2.9.92
    ==============
    
      Misc
    
    	* Remove icons that are in gnome-icon-theme
    
      Translators
    
    	* Vladimir "Kaladan" Petkov (bg)
    	* Martin Willemoes Hansen (da)
    	* Christophe Merlet (RedFox) (fr)
    	* Ankit Patel (gu)
    	* Changwoo Ryu (ko)
    	* Kjartan Maraas (nb)
    	* Kjartan Maraas (no)
    	* Gnome PL Team (pl)
    	* Evandro Fernandes Giovanini (pt_BR)
    	* Paisa Seeluangsawat (th)
    	* Baris Cicek (tr)
    	* Maxim Dziumanenko (uk)
    	* Canonical Ltd (xh)
    
    ==============
    Version 2.9.91
    ==============
    
      libgnome-desktop
    
    	* New gnome_desktop_item_set_launch_time() API (Elijah Newren, Mark)
    
      Translators
    
    	* Jordi Mallach (ca)
    	* Nikos Charonitakis (el)
    	* David Lodge (en_GB)
    	* Ivar Smolin (et)
    	* Pauli Virtanen (fi)
    	* Gil Osher (he)
    	* Žygimantas Beručka (lt)
    	* Duarte Loreto (pt)
    	* Elian Myftiu (sq)
    
    ================
    Version 2.9.90.1
    ================
    
      Misc
    
    	* update LT_VERSION
    	* update GNOME version
    
    ==============
    Version 2.9.90
    ==============
    
      Translators
    
    	* Vladimir "Kaladan" Petkov (bg)
    	* Telsa Gwynne (cy)
    	* Frank Arnold (de)
    	* Francisco Javier F. Serrador (es)
    	* Priit Laes (et)
    	* Takeshi AIHANA (ja)
    	* Tino Meinen (nl)
    	* Åsmund Skjæveland (nn)
    	* Marcel Telka (sk)
    	* Elian Myftiu (sq)
    	* Christian Rose (sv)
    
    =============
    Version 2.9.4
    =============
    
      Misc
    
    	* Add .desktop file for gnome-about (Vincent Untz)
    	* Fix compiler warning (Kjartan)
    	* distcheck fixing (Mark)
    
      libgnome-desktop
    
    	* Don't append invalid URIs to command line when launching (Vijaykumar Patwari)
    
      Translators
    
    	* Miloslav Trmac (cs)
    	* Adam Weinberger (en_CA)
    	* Žygimantas Beručka (lt)
    	* Kjartan Maraas (nb)
    	* Kjartan Maraas (no)
    	* Christian Rose (sv)
    
    =============
    Version 2.9.3
    =============
    
      Translators
    
    	* Zuza Software Foundation (nso)
    	* Zuza Software Foundation (zu)
    
    =============
    Version 2.9.2
    =============
    
      Misc
    
    	* move some icons in gnome-applets & gnome-icon-theme (David Madeley, Mark)
    	* move data files for the menus to gnome-menus (Mark)
    	* don't use some deprecated functions (Vincent)
    
      Translators
    
    	* Meelad Zakaria (fa)
    	* Zuza Software Foundation (nso)

--- graphics/gnome2-pixmaps/buildlink3.mk
+++ graphics/gnome2-pixmaps/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/09/21 16:54:06 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2005/03/22 16:09:27 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME2_PIXMAPS_BUILDLINK3_MK:=	${GNOME2_PIXMAPS_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngnome2-pixmaps}
 BUILDLINK_PACKAGES+=	gnome2-pixmaps
 
 .if !empty(GNOME2_PIXMAPS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gnome2-pixmaps+=	gnome2-pixmaps>=2.8.0
+BUILDLINK_DEPENDS.gnome2-pixmaps+=	gnome2-pixmaps>=2.10.0
 BUILDLINK_PKGSRCDIR.gnome2-pixmaps?=	../../graphics/gnome2-pixmaps
 .endif	# GNOME2_PIXMAPS_BUILDLINK3_MK
 
