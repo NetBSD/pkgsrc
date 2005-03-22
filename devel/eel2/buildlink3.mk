72a0690de207393f51f0b3774630af77da83346e
jmmv <jmmv@pkgsrc.org>
2005-03-22 16:12:47 +0000

    Update to 2.10.0:
    Major changes in 2.10.0 were:
    * translation updates
    
    Major changes in 2.9.92 were:
    * Application chooser handles case with no existing applications better
    * Use automake 1.7
    * Fix tests on 64bit systems
    * translation updates
    
    Major changes in 2.9.91 were:
    * fix crashes in open with dialog
    * translation updates
    
    Major changes in 2.9.90 were:
    * Show existing applications in open with dialog
    * Show icon in application chooser
    
    Major changes in 2.9.2 were:
    * Not using deprecated mime apis

--- devel/eel2/buildlink3.mk
+++ devel/eel2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2004/10/03 00:13:26 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2005/03/22 16:12:47 jmmv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 EEL2_BUILDLINK3_MK:=	${EEL2_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Neel2}
 BUILDLINK_PACKAGES+=	eel2
 
 .if !empty(EEL2_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.eel2+=	eel2>=2.8.0
-BUILDLINK_RECOMMENDED.eel2+=	eel2>=2.8.0nb1
+BUILDLINK_DEPENDS.eel2+=	eel2>=2.10.0
 BUILDLINK_PKGSRCDIR.eel2?=	../../devel/eel2
 .endif	# EEL2_BUILDLINK3_MK
 
@@ -25,8 +24,10 @@ BUILDLINK_PKGSRCDIR.eel2?=	../../devel/eel2
 .include "../../devel/libgnomeui/buildlink3.mk"
 .include "../../devel/popt/buildlink3.mk"
 .include "../../graphics/libart2/buildlink3.mk"
+.include "../../sysutils/gnome-menus/buildlink3.mk"
 .include "../../sysutils/gnome-vfs2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
+.include "../../x11/gnome-desktop/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
