f66cb820064fae298cb9576ba2607d90c2090ecf
wiz <wiz@pkgsrc.org>
2005-03-22 17:50:55 +0000

    Update to 1.4.1:
    Noteworthy changes in version 1.4.1 (2005-03-15)
    ------------------------------------------------
    
        * New --rfc2440-text option which controls how text is handled in
          signatures.  This is in response to some problems seen with
          certain PGP/MIME mail clients and GnuPG version 1.4.0.  More
          details about this are available at
          <http://lists.gnupg.org/pipermail/gnupg-users/2005-January/024408.html>.
    
        * New "import-unusable-sigs" and "export-unusable-sigs" tags for
          --import-options and --export-options.  These are off by
          default, and cause GnuPG to not import or export key signatures
          that are not usable (e.g. expired signatures).
    
        * New experimental HTTP, HTTPS, FTP, and FTPS keyserver helper
          that uses the cURL library <http://curl.haxx.se> to retrieve
          keys.  This is disabled by default, but may be enabled with the
          configure option --with-libcurl.  Without this option, the
          existing HTTP code is used for HTTP, and HTTPS, FTP, and FTPS
          are not supported.
    
          [enabled with the "curl" option for the package]
    
        * When running a --card-status or --card-edit and a public key is
          available, missing secret key stubs will be created on the fly.
          Details of the key are listed too.
    
        * The implicit packet dumping in double verbose mode is now sent
          to stderr and not to stdout.
    
        * Added countermeasures against the Mister/Zuccherato CFB attack
          <http://eprint.iacr.org/2005/033>.
    
        * Add new --edit-key command "bkuptocard" to allow restoring a
          card key from a backup.
    
        * The "fetch" command of --card-edit now retrieves the key using
          the default keyserver if no URL has been stored on the card.
    
        * New configure option --enable-noexecstack.
    
    Also, gpgkeys_mailto is not installed any longer, dropping the
    dependency on perl.

--- security/gnupg/options.mk
+++ security/gnupg/options.mk
@@ -1,26 +1,7 @@
-# $NetBSD: options.mk,v 1.1 2004/12/25 02:54:49 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2005/03/22 17:50:55 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnupg
-PKG_SUPPORTED_OPTIONS=	i586-optimized idea ldap m68060-optimized
-
-.include "../../mk/bsd.prefs.mk"
-
-# legacy options -- remove after 2005Q1
-.if defined(USE_I586) && !empty(USE_I586:M[yY][eE][sS])
-PKG_DEFAULT_OPTIONS+=	i586-optimized
-.endif
-
-.if defined(M68060) && !empty(M68060:M[yY][eE][sS])
-PKG_DEFAULT_OPTIONS+=	m68060-optimized
-.endif
-
-.if defined(USE_IDEA) && !empty(USE_IDEA:M[yY][eE][sS])
-PKG_DEFAULT_OPTIONS+=	idea
-.endif
-
-.if defined(USE_OPENLDAP) && !empty(USE_OPENLDAP:M[yY][eE][sS])
-PKG_DEFAULT_OPTIONS+=	ldap
-.endif
+PKG_SUPPORTED_OPTIONS=	i586-optimized curl idea ldap m68060-optimized
 
 .include "../../mk/bsd.options.mk"
 
@@ -30,10 +11,12 @@ ONLY_FOR_PLATFORM=	*-*-i386
 MACHINE_GNU_ARCH=	i586
 .endif
 
-.if !empty(PKG_OPTIONS:Mm68060-optimized)
-# be more efficient on M68060 machines
-CONFIGURE_ENV+=                M68060=${M68060}
-CFLAGS+=                       -m68060
+.if !empty(PKG_OPTIONS:Mcurl)
+.include "../../www/curl/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-libcurl
+PLIST_SUBST+=	CURL="" NOCURL="@comment "
+.else
+PLIST_SUBST+=	CURL="@comment " NOCURL=""
 .endif
 
 .if !empty(PKG_OPTIONS:Midea)
@@ -53,4 +36,10 @@ PLIST_SUBST+=	OPENLDAP=""
 .else
 CONFIGURE_ARGS+=--disable-ldap
 PLIST_SUBST+=	OPENLDAP="@comment "
+.endif
+
+.if !empty(PKG_OPTIONS:Mm68060-optimized)
+# be more efficient on M68060 machines
+CONFIGURE_ENV+=                M68060=${M68060}
+CFLAGS+=                       -m68060
 .endif
