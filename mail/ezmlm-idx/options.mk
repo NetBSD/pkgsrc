adc502f43f7ba7470edeec3ea08a4ec04f77acb7
schmonz <schmonz@pkgsrc.org>
2005-03-23 00:51:09 +0000

    Take advantage of mk/djbware.mk.

--- mail/ezmlm-idx/options.mk
+++ mail/ezmlm-idx/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2004/12/21 03:42:25 schmonz Exp $
+# $NetBSD: options.mk,v 1.6 2005/03/23 00:51:09 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ezmlm-idx
 PKG_SUPPORTED_OPTIONS=	mysql pgsql
@@ -20,9 +20,14 @@ PKG_FAIL_REASON+=       "MySQL and PostgreSQL cannot both be compiled in." \
 .  include "../../mk/mysql.buildlink3.mk"
 .  if !target(post-configure)
 post-configure:
-	${ECHO} -I${BUILDLINK_PREFIX.mysql-client}/include/mysql > ${WRKSRC}/sub_mysql/conf-sqlcc
-	${ECHO} -L${BUILDLINK_PREFIX.mysql-client}/lib/mysql -Wl,-R${BUILDLINK_PREFIX.mysql-client}/lib/mysql -lmysqlclient > ${WRKSRC}/sub_mysql/conf-sqlld
-	${ECHO} mysql > ${WRKSRC}/conf-sub
+	cd ${WRKSRC};							\
+	${ECHO} -I${BUILDLINK_PREFIX.mysql-client}/include/mysql	\
+		> sub_mysql/conf-sqlcc;					\
+	${ECHO} -L${BUILDLINK_PREFIX.mysql-client}/lib/mysql 		\
+		-Wl,-R${BUILDLINK_PREFIX.mysql-client}/lib/mysql	\
+		-lmysqlclient						\
+		> sub_mysql/conf-sqlld;					\
+	${ECHO} mysql > conf-sub
 .  else
 PKG_FAIL_REASON+=	"mysql: can't define post-configure target"
 .  endif
@@ -35,9 +40,14 @@ PKG_FAIL_REASON+=	"mysql: can't define post-configure target"
 .  include "../../mk/pgsql.buildlink3.mk"
 .  if !target(post-configure)
 post-configure:
-	${ECHO} -I${PGSQL_PREFIX}/include > ${WRKSRC}/sub_pgsql/conf-sqlcc
-	${ECHO} -L${PGSQL_PREFIX}/lib -Wl,-R${PGSQL_PREFIX}/lib -lpq > ${WRKSRC}/sub_pgsql/conf-sqlld
-	${ECHO} pgsql > ${WRKSRC}/conf-sub
+	cd ${WRKSRC};							\
+	${ECHO} -I${PGSQL_PREFIX}/include				\
+		> sub_pgsql/conf-sqlcc;					\
+	${ECHO} -L${PGSQL_PREFIX}/lib					\
+		-Wl,-R${PGSQL_PREFIX}/lib				\
+		-lpq							\
+		> sub_pgsql/conf-sqlld;					\
+	${ECHO} pgsql > conf-sub
 .  else
 PKG_FAIL_REASON+=	"pgsql: can't define post-configure target"
 .  endif
