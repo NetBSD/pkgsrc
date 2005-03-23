ca9588c2e4bfec35e4dd689318be6f13d77d7df6
markd <markd@pkgsrc.org>
2005-03-23 22:28:09 +0000

    Update to KDE 3.4
    Highlights at a glance
    
    * Text-to-speech system with support built into Konqueror, Kate, KPDF
      and the standalone application KSayIt
    * Support for text to speech synthesis is integrated with the desktop
    * Completely redesigned, more flexible trash system
    * Kicker with improved look and feel
    * KPDF now enables you to select, copy & paste text and images from
      PDFs, along with many other improvements
    * Kontact supports now various groupware servers, including eGroupware,
      GroupWise, Kolab, OpenGroupware.org and SLOX
    * Kopete supports Novell Groupwise and Lotus Sametime and gets
      integrated into Kontact
    * DBUS/HAL support allows to keep dynamic device icons in media:/ and
      on the desktop in sync with the state of all devices
    * KHTML has improved standard support and now close to full support for
      CSS 2.1 and the CSS 3 Selectors module
    * Better synchronization between 2 PCs
    * A new high contrast style and a complete monochrome icon set
    * An icon effect to paint all icons in two chosen colors, converting
      third party application icons into high contrast monochrome icons
    * Akregator allows you to read news from your favourite RSS-enabled
      websites in one application
    * Juk has now an album cover management via Google Image Search
    * KMail now stores passwords securely with KWallet
    * SVG files can now be used as wallpapers
    * KHTML plug-ins are now configurable, so the user can selectively
      disable ones that are not used. This does not include Netscape-style
      plug-ins. Netscape plug-in in CPU usage can be manually lowered, and
      plug-ins are more stable.
    * more than 6,500 bugs have been fixed
    * more than 1,700 wishes have been fullfilled
    * more than 80,000 contributions with several million lines of code and
      documentation added or changed

--- games/kdegames3/buildlink3.mk
+++ games/kdegames3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:14:45 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/03/23 22:28:09 markd Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 KDEGAMES_BUILDLINK3_MK:=	${KDEGAMES_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	kdegames
 
 .if !empty(KDEGAMES_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.kdegames+=	kdegames>=3.2.0
-BUILDLINK_RECOMMENDED.kdegames?=	kdegames>=3.3.0nb1
+BUILDLINK_RECOMMENDED.kdegames?=	kdegames>=3.4.0
 BUILDLINK_PKGSRCDIR.kdegames?=	../../games/kdegames3
 .endif	# KDEGAMES_BUILDLINK3_MK
 
