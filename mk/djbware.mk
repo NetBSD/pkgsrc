f3b97e71a8e6bf33dae575f6a8ff6e6ec6fc2d36
schmonz <schmonz@pkgsrc.org>
2005-03-23 01:45:34 +0000

    Replace inline definitions of errno with "#include <errno.h>" to fix compilation on systems with glibc 2.3.1 or newer.

--- mk/djbware.mk
+++ mk/djbware.mk
@@ -1,4 +1,4 @@
-# $NetBSD: djbware.mk,v 1.1 2005/03/23 00:50:06 schmonz Exp $
+# $NetBSD: djbware.mk,v 1.2 2005/03/23 01:45:34 schmonz Exp $
 #
 # Makefile fragment for packages with djb-style build machinery
 #
@@ -6,16 +6,18 @@
 # * a boilerplate RESTRICTED clause
 # * typical defaults for BUILD_TARGET and INSTALL_TARGET
 # * typical values for conf-* files
+# * replace inline definitions of errno with "#include <errno.h>"
 #
 # TODO:
 # * set RESTRICTED automatically iff patches or other mods are applied
-# * on recent glibc, subst.mk inline errno defn into #include <errno.h>
+# * do the <errno.h> dance only where needed (glibc>=2.3)
 #
 
 .if !defined(DJBWARE_MK)
 DJBWARE_MK=		# defined
 
 DJB_RESTRICTED?=	YES
+DJB_ERRNO_HACK?=	YES
 DJB_BUILD_TARGETS?=	# empty
 DJB_INSTALL_TARGETS?=	# empty
 DJB_CONFIG_DIR?=	${WRKSRC}
@@ -23,6 +25,8 @@ DJB_CONFIG_PREFIX?=	${PREFIX}
 DJB_CONFIG_HOME?=	conf-home
 DJB_CONFIG_CMDS?=	${DO_NADA}
 BGWARE_INSTALLER?=	NO
+
+BUILD_DEFS+=		DJB_ERRNO_HACK
 
 .if !empty(DJB_RESTRICTED:M[yY][eE][sS])
 RESTRICTED=		"modified source and binaries may not be distributed"
@@ -32,6 +36,14 @@ NO_BIN_ON_FTP=		${RESTRICTED}
 
 BUILD_TARGET?=		it ${DJB_BUILD_TARGETS}
 INSTALL_TARGET?=	setup check ${DJB_INSTALL_TARGETS}
+
+.if !empty(DJB_ERRNO_HACK:M[yY][eE][sS])
+SUBST_CLASSES+=		djbware
+SUBST_STAGE.djbware=	do-configure
+SUBST_FILES.djbware=	error.h
+SUBST_SED.djbware=	-e 's|^extern\ int\ errno\;|\#include \<errno.h\>|'
+SUBST_MESSAGE.djbware=	"Correcting definition of errno."
+.endif
 
 .if !target(do-configure)
 do-configure:
