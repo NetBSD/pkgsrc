71130ebe8718dc27f99b74020276ff20c8adb650
markd <markd@pkgsrc.org>
2005-03-23 21:44:37 +0000

    Update to KDE 3.4
    Highlights at a glance
    
    * Text-to-speech system with support built into Konqueror, Kate, KPDF
      and the standalone application KSayIt
    * Support for text to speech synthesis is integrated with the desktop
    * Completely redesigned, more flexible trash system
    * Kicker with improved look and feel
    * KPDF now enables you to select, copy & paste text and images from
      PDFs, along with many other improvements
    * Kontact supports now various groupware servers, including eGroupware,
      GroupWise, Kolab, OpenGroupware.org and SLOX
    * Kopete supports Novell Groupwise and Lotus Sametime and gets
      integrated into Kontact
    * DBUS/HAL support allows to keep dynamic device icons in media:/ and
      on the desktop in sync with the state of all devices
    * KHTML has improved standard support and now close to full support for
      CSS 2.1 and the CSS 3 Selectors module
    * Better synchronization between 2 PCs
    * A new high contrast style and a complete monochrome icon set
    * An icon effect to paint all icons in two chosen colors, converting
      third party application icons into high contrast monochrome icons
    * Akregator allows you to read news from your favourite RSS-enabled
      websites in one application
    * Juk has now an album cover management via Google Image Search
    * KMail now stores passwords securely with KWallet
    * SVG files can now be used as wallpapers
    * KHTML plug-ins are now configurable, so the user can selectively
      disable ones that are not used. This does not include Netscape-style
      plug-ins. Netscape plug-in in CPU usage can be manually lowered, and
      plug-ins are more stable.
    * more than 6,500 bugs have been fixed
    * more than 1,700 wishes have been fullfilled
    * more than 80,000 contributions with several million lines of code and
      documentation added or changed

--- x11/kdebase3/buildlink3.mk
+++ x11/kdebase3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/11/08 12:50:18 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2005/03/23 21:44:37 markd Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 KDEBASE_BUILDLINK3_MK:=	${KDEBASE_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	kdebase
 
 .if !empty(KDEBASE_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.kdebase+=	kdebase>=3.2.0
-BUILDLINK_RECOMMENDED.kdebase?=	kdebase>=3.3.0nb1
+BUILDLINK_RECOMMENDED.kdebase?=	kdebase>=3.4.0
 BUILDLINK_PKGSRCDIR.kdebase?=	../../x11/kdebase3
 .endif	# KDEBASE_BUILDLINK3_MK
 
@@ -21,6 +21,7 @@ BUILDLINK_DEPENDS.Xrandr+=      Xrandr>=1.0
 .include "../../databases/openldap/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/openexr/buildlink3.mk"
 .include "../../x11/Xrandr/buildlink3.mk"
 .include "../../x11/kdelibs3/buildlink3.mk"
 .include "../../x11/xcursor/buildlink3.mk"
