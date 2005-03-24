a9e9e4efa10f3e61dad19908b71b4e8622af694b
jlam <jlam>
2005-03-24 22:42:58 +0000

    Don't check for USE_BUILDLINK3 anymore in these files.  These are included by other package Makefiles, and with the deprecation of USE_BUILDLINK3 support in the infrastructure files, these had the potential to break existing packages.

--- sysutils/desktop-file-utils/desktopdb.mk
+++ sysutils/desktop-file-utils/desktopdb.mk
@@ -1,4 +1,4 @@
-# $NetBSD: desktopdb.mk,v 1.1.1.1 2004/09/21 17:03:54 jmmv Exp $
+# $NetBSD: desktopdb.mk,v 1.2 2005/03/24 22:42:58 jlam Exp $
 #
 # This Makefile fragment is intended to be included by packages that install
 # desktop entries.  It takes care of registering them in the global database.
@@ -7,7 +7,7 @@
 .if !defined(DESKTOP_FILE_UTILS_MIMEDB_MK)
 DESKTOP_FILE_UTILS_MIMEDB_MK=	# defined
 
-. include "../../mk/bsd.prefs.mk"
+.include "../../mk/bsd.prefs.mk"
 
 USE_PKGINSTALL=		YES
 INSTALL_EXTRA_TMPL+=	${.CURDIR}/../../sysutils/desktop-file-utils/files/install.tmpl
@@ -18,8 +18,6 @@ FILES_SUBST+=		UPDATE_DESKTOPDB="${BUILDLINK_PREFIX.desktop-file-utils}/bin/upda
 
 PRINT_PLIST_AWK+=	/^share\/applications\/mimeinfo.cache$$/ { next }
 
-.if !defined(NO_BUILDLINK) && !empty(USE_BUILDLINK3:M[yY][eE][sS])
-.  include "../../sysutils/desktop-file-utils/buildlink3.mk"
-.endif
+.include "../../sysutils/desktop-file-utils/buildlink3.mk"
 
 .endif	# DESKTOP_FILE_UTILS_MIMEDB_MK
