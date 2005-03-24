9ad65365974aab09876ee7dcf0a9cfeb8d26b607
markd <markd>
2005-03-24 05:08:41 +0000

    Make cyrus-sasl2 dependency optional but on by default.  Default might be switched if common authentication cases are handled without it.

--- misc/kdepim3/buildlink3.mk
+++ misc/kdepim3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2005/03/23 22:07:07 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2005/03/24 05:08:41 markd Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 KDEPIM_BUILDLINK3_MK:=	${KDEPIM_BUILDLINK3_MK}+
@@ -16,9 +16,14 @@ BUILDLINK_RECOMMENDED.kdepim+=	kdepim>=3.4.0
 BUILDLINK_PKGSRCDIR.kdepim?=	../../misc/kdepim3
 .endif	# KDEPIM_BUILDLINK3_MK
 
+PKG_OPTIONS.kdepim?=	${PKG_DEFAULT_OPTIONS}
+
 .include "../../comms/libmal/buildlink3.mk"
 .include "../../comms/pilot-link-libs/buildlink3.mk"
 .include "../../security/gpgme/buildlink3.mk"
+.if !empty(PKG_OPTIONS.kdepim:Msasl2)
+.  include "../../security/cyrus-sasl2/buildlink3.mk"
+.endif
 .include "../../x11/kdebase3/buildlink3.mk"
 .include "../../x11/kdelibs3/buildlink3.mk"
 
