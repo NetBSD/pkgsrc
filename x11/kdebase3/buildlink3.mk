69f60456c1a608bc48070af2985c0afec104c1af
markd <markd@pkgsrc.org>
2005-03-24 04:47:58 +0000

    Add missing explicit dependency on cyrus-sasl2.  Make it optional but on by default.  The default might be changed later if the common authentication cases are covered without it.

--- x11/kdebase3/buildlink3.mk
+++ x11/kdebase3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2005/03/23 21:44:37 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2005/03/24 04:47:58 markd Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 KDEBASE_BUILDLINK3_MK:=	${KDEBASE_BUILDLINK3_MK}+
@@ -16,12 +16,20 @@ BUILDLINK_RECOMMENDED.kdebase?=	kdebase>=3.4.0
 BUILDLINK_PKGSRCDIR.kdebase?=	../../x11/kdebase3
 .endif	# KDEBASE_BUILDLINK3_MK
 
+PKG_OPTIONS.kdebase?=   ${PKG_DEFAULT_OPTIONS}
+
 BUILDLINK_DEPENDS.Xrandr+=      Xrandr>=1.0
 
 .include "../../databases/openldap/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../graphics/openexr/buildlink3.mk"
+.if !empty(PKG_OPTIONS.kdebase:Msamba)
+.  include "../../net/samba/buildlink3.mk"
+.endif
+.if !empty(PKG_OPTIONS.kdebase:Msasl2)
+.  include "../../security/cyrus-sasl2/buildlink3.mk"
+.endif
 .include "../../x11/Xrandr/buildlink3.mk"
 .include "../../x11/kdelibs3/buildlink3.mk"
 .include "../../x11/xcursor/buildlink3.mk"
