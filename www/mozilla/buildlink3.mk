8bb0cd80ae46f4304ce356cd59022ec8d87d03d1
snj <snj>
2005-03-27 05:32:19 +0000

    Pullup ticket 392 - requested by Shin'ichiro TAYA security fix for mozilla and mozilla-gtk2
    Revisions pulled up:
    - pkgsrc/www/mozilla/Makefile		1.142
    - pkgsrc/www/mozilla/PLIST		1.16
    - pkgsrc/www/mozilla/buildlink3.mk	1.8, 1.9
    - pkgsrc/www/mozilla/distinfo		1.73
    - pkgsrc/www/mozilla-gtk2/Makefile	1.17
    - pkgsrc/www/mozilla-gtk2/PLIST		1.6
    - pkgsrc/www/mozilla-gtk2/buildlink3.mk	1.6, 1.7
    
    
       Module Name:    pkgsrc
       Committed By:   taya
       Date:           Thu Mar 24 14:08:29 UTC 2005
    
       Modified Files:
               pkgsrc/www/mozilla: Makefile PLIST buildlink3.mk distinfo
               pkgsrc/www/mozilla-gtk2: Makefile PLIST buildlink3.mk
    
       Log Message:
       Update mozilla & mozilla-gtk2 to 1.7.6
    
       This is a security fix release.
       Fixed bugs are follows.
    
       MFSA 2005-32  Drag and drop loading of privileged XUL
       MFSA 2005-30 GIF heap overflow parsing Netscape extension 2
       MFSA 2005-29 Internationalized Domain Name (IDN) homograph spoofing
       MFSA 2005-28 Unsafe /tmp/plugtmp directory exploitable to erase user's files
       MFSA 2005-27 Plugins can be used to load privileged content
       MFSA 2005-26 Cross-site scripting by dropping javascript: link on tab
       MFSA 2005-25 Image drag and drop executable spoofing
       MFSA 2005-24 HTTP auth prompt tab spoofing
       MFSA 2005-23 Download dialog source spoofing
       MFSA 2005-21 Overwrite arbitrary files downloading .lnk twice
       MFSA 2005-20 XSLT can include stylesheets from arbitrary hosts
       MFSA 2005-18 Memory overwrite in string library
       MFSA 2005-17 Install source spoofing with user:pass@host
       MFSA 2005-16 Spoofing download and security dialogs with overlapping windows
       MFSA 2005-15 Heap overflow possible in UTF8 to Unicode conversion
       MFSA 2005-14 SSL "secure site" indicator spoofing
       MFSA 2005-13 Window Injection Spoofing
    
       see changelog for detail.
       http://www.mozilla.org/releases/mozilla1.7.6/changelog.html
    ---
       Module Name:		pkgsrc
       Committed By:	taya
       Date:		Sat Mar 26 13:49:31 UTC 2005
    
       Modified Files:
            pkgsrc/www/mozilla: buildlink3.mk
            pkgsrc/www/mozilla-gtk2: buildlink3.mk
    
       Log Message:
       ABI did not change, add BUILDLINK_RECOMMENDED instead of updating
       BUILDLINK_DEPENDS.

--- www/mozilla/buildlink3.mk
+++ www/mozilla/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/12/25 15:39:42 taya Exp $
+# $NetBSD: buildlink3.mk,v 1.7.2.1 2005/03/27 05:32:19 snj Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 MOZILLA_BUILDLINK3_MK:=	${MOZILLA_BUILDLINK3_MK}+
@@ -12,6 +12,7 @@ BUILDLINK_PACKAGES+=	mozilla
 
 .if !empty(MOZILLA_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.mozilla+=	mozilla>=1.7.5
+BUILDLINK_RECOMMENDED.mozilla+=	mozilla>=1.7.6
 BUILDLINK_PKGSRCDIR.mozilla?=	../../www/mozilla
 .endif	# MOZILLA_BUILDLINK3_MK
 
