e60f447c3e5f69fb23caad66664d62956f7734de
recht <recht@pkgsrc.org>
2005-03-27 10:29:20 +0000

    update to libgdiplus-1.1.5 (part of the mono 1.1.5 release)
    changes:
    - bugfixes, more complete .Net API support
    
    changes to the package:
    - don't depend on lang/mono any longer
    - use the included cairo/libpixman

--- graphics/libgdiplus/buildlink3.mk
+++ graphics/libgdiplus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2004/12/28 23:18:17 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2005/03/27 10:29:20 recht Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGDIPLUS_BUILDLINK3_MK:=	${LIBGDIPLUS_BUILDLINK3_MK}+
@@ -11,16 +11,17 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgdiplus}
 BUILDLINK_PACKAGES+=	libgdiplus
 
 .if !empty(LIBGDIPLUS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libgdiplus+=	libgdiplus>=1.0.1
-BUILDLINK_RECOMMENDED.libgdiplus+=	libgdiplus>=1.0.1nb2
+BUILDLINK_DEPENDS.libgdiplus+=	libgdiplus>=1.1.5
 BUILDLINK_PKGSRCDIR.libgdiplus?=	../../graphics/libgdiplus
 .endif	# LIBGDIPLUS_BUILDLINK3_MK
 
-.include "../../graphics/cairo/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../fonts/Xft2/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
 .include "../../graphics/jpeg/buildlink3.mk"
 .include "../../graphics/libungif/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
-.include "../../lang/mono/buildlink3.mk"
 .include "../../x11/Xrender/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
