c563ca9c1dc83b571fe7f3c2fd905afcb0dab823
jlam <jlam@pkgsrc.org>
2005-03-28 08:57:58 +0000

    Remove Cyrus SASL 1.5.x support from mail/mutt.  Users that require SASL support should install mail/mutt-devel instead.

--- mail/mutt/options.mk
+++ mail/mutt/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.5 2005/01/01 22:05:26 grant Exp $
+# $NetBSD: options.mk,v 1.6 2005/03/28 08:57:58 jlam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mutt
-PKG_SUPPORTED_OPTIONS=	slang ncurses ssl sasl1 buffy-size
+PKG_SUPPORTED_OPTIONS=	slang ncurses ssl buffy-size
 
 .if !defined(PKG_OPTIONS.mutt)
 PKG_DEFAULT_OPTIONS+=	ssl
@@ -21,14 +21,6 @@ USE_NCURSES=		yes
 .  endif
 .  include "../../devel/ncurses/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-curses=${BUILDLINK_PREFIX.ncurses}
-.endif
-
-###
-### SASLv1
-###
-.if !empty(PKG_OPTIONS:Msasl1)
-.  include "../../security/cyrus-sasl/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-sasl=${BUILDLINK_PREFIX.cyrus-sasl}
 .endif
 
 ###
