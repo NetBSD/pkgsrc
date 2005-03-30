be01bda00183a15c0c18386c8bd49ef66125dde3
adam <adam@pkgsrc.org>
2005-03-30 18:09:47 +0000

    Fix buildlink3.mk to include archivers/szip

--- devel/hdf5/buildlink3.mk
+++ devel/hdf5/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:13:28 tv Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.4.
-# XXX After this file as been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink[23].mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.hdf5?=	build
+# $NetBSD: buildlink3.mk,v 1.3 2005/03/30 18:09:47 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 HDF5_BUILDLINK3_MK:=	${HDF5_BUILDLINK3_MK}+
@@ -26,6 +16,7 @@ BUILDLINK_RECOMMENDED.hdf5+=	hdf5>=1.6.2nb1
 BUILDLINK_PKGSRCDIR.hdf5?=	../../devel/hdf5
 .endif	# HDF5_BUILDLINK3_MK
 
+.include "../../archivers/szip/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
