48e5ce90408fb5a4f56061bcda05a93f9233b0fb
martti <martti@pkgsrc.org>
2005-03-30 07:04:05 +0000

    Fix xfce4 build problems
    - make libxfce4gui depend on startup-notification
    - rebuild the buildlink3.mk files to catch the new dependencies

--- sysutils/xfce4-netload-plugin/buildlink3.mk
+++ sysutils/xfce4-netload-plugin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/10/08 06:21:52 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2005/03/30 07:04:05 martti Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_NETLOAD_PLUGIN_BUILDLINK3_MK:=	${XFCE4_NETLOAD_PLUGIN_BUILDLINK3_MK}+
@@ -12,7 +12,6 @@ BUILDLINK_PACKAGES+=	xfce4-netload-plugin
 
 .if !empty(XFCE4_NETLOAD_PLUGIN_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.xfce4-netload-plugin+=	xfce4-netload-plugin>=0.2.3
-BUILDLINK_RECOMMENDED.xfce4-netload-plugin+=	xfce4-netload-plugin>=0.2.3
 BUILDLINK_PKGSRCDIR.xfce4-netload-plugin?=	../../sysutils/xfce4-netload-plugin
 .endif	# XFCE4_NETLOAD_PLUGIN_BUILDLINK3_MK
 
--- sysutils/xfce4-xkb-plugin/buildlink3.mk
+++ sysutils/xfce4-xkb-plugin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/07/07 11:27:44 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/03/30 07:04:06 martti Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_XKB_PLUGIN_BUILDLINK3_MK:=	${XFCE4_XKB_PLUGIN_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-xkb-plugin}
 BUILDLINK_PACKAGES+=	xfce4-xkb-plugin
 
 .if !empty(XFCE4_XKB_PLUGIN_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.xfce4-xkb-plugin+=	xfce4-xkb-plugin>=0.3.2
+BUILDLINK_DEPENDS.xfce4-xkb-plugin+=	xfce4-xkb-plugin>=0.3.2nb1
 BUILDLINK_PKGSRCDIR.xfce4-xkb-plugin?=	../../sysutils/xfce4-xkb-plugin
 .endif	# XFCE4_XKB_PLUGIN_BUILDLINK3_MK
 
