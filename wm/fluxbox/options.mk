6da6e8e46c0cd784d5820f4dd095bc19d698b12c
jmcneill <jmcneill@pkgsrc.org>
2005-03-30 11:59:10 +0000

    GNOME and KDE support in fluxbox don't depend on the GNOME and KDE libraries. Remove the dependencies for libgnome and kdelibs.

--- wm/fluxbox/options.mk
+++ wm/fluxbox/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2005/03/24 15:00:40 jmcneill Exp $
+# $NetBSD: options.mk,v 1.2 2005/03/30 11:59:10 jmcneill Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fluxbox
 PKG_SUPPORTED_OPTIONS=	gnome kde imlib2 xft
@@ -7,14 +7,12 @@ PKG_SUPPORTED_OPTIONS=	gnome kde imlib2 xft
 
 .if !empty(PKG_OPTIONS:Mgnome)
 CONFIGURE_ARGS+=        --enable-gnome
-.include "../../devel/libgnome/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-gnome
 .endif
 
 .if !empty(PKG_OPTIONS:Mkde)
 CONFIGURE_ARGS+=        --enable-kde
-.include "../../x11/kdelibs3/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-kde
 .endif
