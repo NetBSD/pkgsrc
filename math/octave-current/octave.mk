9f021de029ab74d23df867802f37a2ef64d29ff1
adam <adam@pkgsrc.org>
2005-04-01 10:15:19 +0000

    Changes 2.9.1: * use glpk * improvements

--- math/octave-current/octave.mk
+++ math/octave-current/octave.mk
@@ -1,5 +1,4 @@
-# $NetBSD: octave.mk,v 1.2 2004/11/25 12:59:21 adam Exp $
-#
+# $NetBSD: octave.mk,v 1.3 2005/04/01 10:15:19 adam Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
@@ -7,8 +6,7 @@ OCTAVE_CONFIG=	${LOCALBASE}/bin/octave-config
 
 # OCTAVE_VARS is a list of variables to define in the make environment.
 # This may be overridden in an octave add-on package Makefile.  
-#
-OCTAVE_VARS?=	LOCALVERFCNFILEDIR LOCALVEROCTFILEDIR  LOCALVERARCHLIBDIR VERSION
+OCTAVE_VARS?=	LOCALVERFCNFILEDIR LOCALVEROCTFILEDIR LOCALVERARCHLIBDIR VERSION
 
 # Set OCT_<var> to the value of <var> as returned by octave-config.
 .if !empty(PHASES_AFTER_EXTRACT:M${PKG_PHASE})
