bbb95517ee1a6c4b8907d5b11ae33d64c418e97e
adam <adam@pkgsrc.org>
2005-04-01 10:15:46 +0000

    Changes 2.1.69: * Bug fixes

---
+++ math/octave/octave.mk
@@ -0,0 +1,21 @@
+# $NetBSD: octave.mk,v 1.1 2005/04/01 10:15:47 adam Exp $
+
+.include "../../mk/bsd.prefs.mk"
+
+OCTAVE_CONFIG=	${LOCALBASE}/bin/octave-config
+
+# OCTAVE_VARS is a list of variables to define in the make environment.
+# This may be overridden in an octave add-on package Makefile.  
+OCTAVE_VARS?=	LOCALVERFCNFILEDIR LOCALVEROCTFILEDIR LOCALVERARCHLIBDIR VERSION
+
+# Set OCT_<var> to the value of <var> as returned by octave-config.
+.if !empty(PHASES_AFTER_EXTRACT:M${PKG_PHASE})
+.  if exists(${OCTAVE_CONFIG})  
+.    for _var_ in ${OCTAVE_VARS}
+.      if !defined(OCT_${_var_})
+OCT_${_var_}!=  ${OCTAVE_CONFIG} -p ${_var_} 
+.      endif 
+.    endfor
+.    undef _var_
+.  endif 
+.endif 
