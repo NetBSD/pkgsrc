8bcd80ae7fe4483b2530e19fdb2501b7d1f8f8d5
salo <salo@pkgsrc.org>
2005-04-01 10:51:50 +0000

    Security fix for CAN-2005-0891:
    "David Costanzo has reported a vulnerability in GTK+, which can be
     exploited by malicious people to crash certain applications on
     a user's system.
    
     The vulnerability is caused due to a double free error in the BMP
     loader.  This can be exploited to crash an application linked against
     GTK+ when a specially crafted BMP image is processed."
    
    Bump PKGREVISION.  Patch from Fedora.

--- x11/gtk2/buildlink3.mk
+++ x11/gtk2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2004/12/28 23:18:21 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2005/04/01 10:51:50 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GTK2_BUILDLINK3_MK:=	${GTK2_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	gtk2
 
 .if !empty(GTK2_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gtk2+=	gtk2+>=2.4.0
-BUILDLINK_RECOMMENDED.gtk2+=	gtk2+>=2.6.0nb1
+BUILDLINK_RECOMMENDED.gtk2+=	gtk2+>=2.6.4nb1
 BUILDLINK_PKGSRCDIR.gtk2?=	../../x11/gtk2
 
 PRINT_PLIST_AWK+=	/^@dirrm lib\/gtk-2.0$$/ { next; }
