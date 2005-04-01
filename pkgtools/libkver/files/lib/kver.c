cc5905cd576eca4810e1b613fa8b55e9ded370e6
snj <snj@pkgsrc.org>
2005-04-01 05:30:34 +0000

    Pullup ticket 403 - requested by Lubomir Sedlacik libkver update
    Revisions pulled up:
    - pkgsrc/pkgtools/libkver/Makefile		1.13
    - pkgsrc/pkgtools/libkver/files/lib/kver.c	1.7
    
    
        Module Name:    pkgsrc
        Committed By:   seb
        Date:           Wed Mar 30 06:41:01 UTC 2005
    
        Modified Files:
                pkgsrc/pkgtools/libkver: Makefile
                pkgsrc/pkgtools/libkver/files/lib: kver.c
    
        Log Message:
        Update to version 0.6.
    
        Support NetBSD release like X.Y.Z_LABEL eg 1.6.2_STABLE.

--- pkgtools/libkver/files/lib/kver.c
+++ pkgtools/libkver/files/lib/kver.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kver.c,v 1.6 2005/01/14 17:15:21 cube Exp $	*/
+/*	$NetBSD: kver.c,v 1.6.2.1 2005/04/01 05:30:34 snj Exp $	*/
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -89,10 +89,10 @@ str2osrevision(char *s)
 			n = (n * 10) + (*s - '0');
 		}
 		r += (n * 100);
-		if (*s != 0)
-			return KVER_INVALID_OSRELEASE;
-		return r;
 	}
+	/* nothing more, return */
+	if (*s == 0)
+		return r;
 	/* or optional underscore followed by release status */
 	if (*s == '_')
 		/* ignore the trailing label */
