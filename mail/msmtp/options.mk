34eeab38406c5c1157672eed15c30c70bdb5834c
adam <adam@pkgsrc.org>
2005-04-04 08:45:24 +0000

    Changes 1.4.0: - This is the new stable release of msmtp.   BEWARE: When upgrading from 1.2.4, note that   - Authentication is not enabled automatically anymore! Insert the command     "auth on" into account definitions that need it.   - Some command line options have changed! - Summary of new features since 1.2.4:   - Sendmail compatible command line options, including -t, -N, and -R   - New long options to configure almost everything on the command line   - Optional account selection with -f/--from   - Support for a system wide configuration file   - Optional automatic construction of envelope from addresses   - More flexible account definitions   - Configurable connection timeouts   - Improved log file logging   - Syslog logging   - Improved TLS/SSL support   - Support for LMTP   - Support for the EXTERNAL and GSSAPI authentication methods   - Support for .netrc and password prompting   - Rewritten documentation, available in various formats

--- mail/msmtp/options.mk
+++ mail/msmtp/options.mk
@@ -1,38 +1,29 @@
-# $NetBSD: options.mk,v 1.4 2005/01/10 20:59:43 adrianp Exp $
+# $NetBSD: options.mk,v 1.5 2005/04/04 08:45:24 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.msmtp
-PKG_SUPPORTED_OPTIONS=	gnutls inet6 ssl gsasl nossl
-
-.if !defined(PKG_OPTIONS.msmtp)
-PKG_DEFAULT_OPTIONS+=	ssl
-.endif
+PKG_DEFAULT_OPTIONS=	ssl
+PKG_SUPPORTED_OPTIONS=	gnutls gsasl inet6 ssl
 
 .include "../../mk/bsd.options.mk"
 
-###
-### GNUtls support.
-###
+# GNUtls support
 .if !empty(PKG_OPTIONS:Mgnutls)
 .include "../../security/gnutls/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-ssl=gnutls
 .endif
 
-###
-### OpenSSL support.
-###
-.if !empty(PKG_OPTIONS:Mssl)
-.include "../../security/openssl/buildlink3.mk"
-.endif
-
-###
-### GNUsasl support.
-###
+# GNUsasl support
 .if !empty(PKG_OPTIONS:Mgsasl)
 .include "../../security/gsasl/buildlink3.mk"
 .endif
 
-###
-### No SSL support.
-###
-.if !empty(PKG_OPTIONS:Mnossl)
+# OpenSSL support
+.if !empty(PKG_OPTIONS:Mssl)
+.include "../../security/openssl/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-ssl=openssl
+.endif
+
+# Disable any SSL support
+.if empty(PKG_OPTIONS:Mgnutls) && empty(PKG_OPTIONS:Mssl)
 CONFIGURE_ARGS+=	--disable-ssl
 .endif
