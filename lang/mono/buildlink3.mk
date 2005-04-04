cdd0dabf64ec8e8d0dd770c8dc4c0b4e88878cc7
recht <recht>
2005-04-04 07:22:34 +0000

    update to mono-1.1.6 This is a micro-release and only contains some bug fixes:
    * Packaging Fixes: Mono 1.1.5 RPM packages were compiled with an option that
      would allow users to downgrade their kernel, but the feature had not been
      tested.
      One simple fix was to compile from source code, but we decided that redoing
      the packages was the major burden of a release, and we could include a few
      other fixes.
    * Apple G3 Support: The G3 was not previously supported, this version adds
      support for it.
    * KEvent on BSD: A crashing bug on BSD-based systems (OSX included) were
      fixed.
    * Cookies: A couple of small bugs were found on the Cookie handling with
      certain web sites, this has now been fixed.
    * AMD compilation: Makes sure that libraries are installed in lib, not lib64.
    * Compilation: Compilation on some older systems has been fixed.
    * SQL Server reset: The reset operation is not supported on all SQL servers.

--- lang/mono/buildlink3.mk
+++ lang/mono/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2005/03/27 10:25:51 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2005/04/04 07:22:34 recht Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 MONO_BUILDLINK3_MK:=	${MONO_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmono}
 BUILDLINK_PACKAGES+=	mono
 
 .if !empty(MONO_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.mono+=	mono>=1.1.5
+BUILDLINK_DEPENDS.mono+=	mono>=1.1.6
 BUILDLINK_PKGSRCDIR.mono?=	../../lang/mono
 .endif	# MONO_BUILDLINK3_MK
 
