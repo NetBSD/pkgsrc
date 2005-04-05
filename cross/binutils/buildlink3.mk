c60b4c7d78c385616bbea60fc058519c454fc470
wiz <wiz@pkgsrc.org>
2005-04-05 11:04:20 +0000

    Add buildlink3 file.

---
+++ cross/binutils/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2005/04/05 11:04:20 wiz Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+CROSS_BINUTILS_BUILDLINK3_MK:=	${CROSS_BINUTILS_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	cross-binutils
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ncross-binutils}
+BUILDLINK_PACKAGES+=	cross-binutils
+
+.if !empty(CROSS_BINUTILS_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.cross-binutils+=	cross-binutils>=2.15.0.0nb3
+BUILDLINK_PKGSRCDIR.cross-binutils?=	../../cross/binutils
+.endif	# CROSS_BINUTILS_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
