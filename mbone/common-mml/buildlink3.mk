0e6aeaef35e9544ae1e81c4de51c95d8684cba7f
wiz <wiz@pkgsrc.org>
2005-04-05 10:34:00 +0000

    Add buildlink3 file.

---
+++ mbone/common-mml/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2005/04/05 10:34:00 wiz Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+COMMON_MML_BUILDLINK3_MK:=	${COMMON_MML_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	common-mml
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ncommon-mml}
+BUILDLINK_PACKAGES+=	common-mml
+
+.if !empty(COMMON_MML_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.common-mml+=	common-mml>=1.2.0nb1
+BUILDLINK_PKGSRCDIR.common-mml?=	../../mbone/common-mml
+.endif	# COMMON_MML_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
