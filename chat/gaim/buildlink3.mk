ce0d71e17b8fa913399567a210eb162b138b72ba
snj <snj@pkgsrc.org>
2005-04-06 03:51:05 +0000

    Pullup ticket 427 - requested by Lubomir Sedlacik security fix for gaim
    Revisions pulled up:
    - pkgsrc/chat/gaim/Makefile		1.82, 1.83
    - pkgsrc/chat/gaim/PLIST		1.38
    - pkgsrc/chat/gaim/distinfo		1.62, 1.63
    - pkgsrc/chat/gaim/buildlink3.mk	1.4
    
    
        Module Name:    pkgsrc
        Committed By:   jmmv
        Date:           Tue Mar 22 09:47:47 UTC 2005
    
        Modified Files:
                pkgsrc/chat/gaim: Makefile distinfo
        Added Files:
                pkgsrc/chat/gaim/patches: patch-ab
    
        Log Message:
        Update to 1.2.0.
        Patch provided by Matthew Luckie (maintainer) in PR pkg/29752.
    
        * Yahoo file receiving and buddy icon receiving work again.
        * Limit animated buddy icon frame rates to 10 frames per second
          (Nathan Conrad)
        * Fix a bug where portions of your account configuration would fail to be
          read correctly if you set a proxy user name or password containing invalid
          XML characters such as < and > (Bastien Durel)
        * Yahoo! privacy improvements (Bleeter)
    ----
        Module Name:    pkgsrc
        Committed By:   salo
        Date:           Tue Apr  5 09:50:06 UTC 2005
    
        Modified Files:
                pkgsrc/chat/gaim: Makefile PLIST distinfo
        Removed Files:
                pkgsrc/chat/gaim/patches: patch-ab
    
        Log Message:
        Update to version 1.2.1
    
        Security fixes for three DoS vulnerabilities:
    
         Remote DoS on receiving malformed HTML
         Remote DoS on receiving certain messages over IRC
         Jabber remote crash
    
        Changes:
        - URL escaping now works with UTF-8 text. This may break some old
          log files.
        - Revert to XOR auth for ICQ as the md5 is not fully functional
        - Fix bug with going away while in a jabber chat
        - MSN bug fixes (Felipe Contreras)
        - Escape things properly in IRC
    
        From MAINTAINER via PR pkg/29888
    ----
        Module Name:    pkgsrc
        Committed By:   salo
        Date:           Tue Apr  5 09:53:17 UTC 2005
    
        Modified Files:
                pkgsrc/chat/gaim: buildlink3.mk
    
        Log Message:
        Bump BUILDLINK_RECOMMENDED.

--- chat/gaim/buildlink3.mk
+++ chat/gaim/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2005/03/16 17:46:55 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.3.2.1 2005/04/06 03:51:06 snj Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GAIM_BUILDLINK3_MK:=	${GAIM_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	gaim
 
 .if !empty(GAIM_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gaim+=	gaim>=1.1.2
-BUILDLINK_RECOMMENDED.gaim+=	gaim>=1.1.2
+BUILDLINK_RECOMMENDED.gaim+=	gaim>=1.2.1
 BUILDLINK_PKGSRCDIR.gaim?=	../../chat/gaim
 
 PRINT_PLIST_AWK+=	/^@dirrm lib\/gaim$$/ \
