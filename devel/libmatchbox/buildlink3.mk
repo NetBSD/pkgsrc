91f6b7ae2a1f3ea8a66e6a4b334ffe4d788b3f0c
ben <ben@pkgsrc.org>
2005-04-07 13:43:15 +0000

    Update libmatchbox to 1.6.  Many bug fixes since 1.4. Now using GPL 2.1 instead of 2.

--- devel/libmatchbox/buildlink3.mk
+++ devel/libmatchbox/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:13:30 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/04/07 13:43:15 ben Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBMATCHBOX_BUILDLINK3_MK:=	${LIBMATCHBOX_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libmatchbox
 
 .if !empty(LIBMATCHBOX_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libmatchbox+=	libmatchbox>=1.4
-BUILDLINK_RECOMMENDED.libmatchbox+=	libmatchbox>=1.4nb1
+BUILDLINK_RECOMMENDED.libmatchbox+=	libmatchbox>=1.6
 BUILDLINK_PKGSRCDIR.libmatchbox?=	../../devel/libmatchbox
 .endif	# LIBMATCHBOX_BUILDLINK3_MK
 
