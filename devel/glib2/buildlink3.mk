07c586af2884e885b8d0351c65f04846d8bd8480
reed <reed@pkgsrc.org>
2005-04-07 02:35:39 +0000

    Add BUILDLINK_RECOMMENDED.glib2+=  glib2>=2.6.1
    Noted on tech-pkg today that older packages had libgobject-2.0.so.400
    and libglib-2.0.so.400 and newer packages provided
    libgobject-2.0.so.0.600.3 and libglib-2.0.so.0.
    
    The libtool changes on Sept. 22, 2004 made this install different
    library naming. The next update was in 2.6.1 (on January 9, 2005).
    So setting BUILDLINK_RECOMMENDED.glib2 to that version.
    
    Apparently, 2.6 is binary-compatible with any 2.x version.
    
    Hopefully, this will be good enough to force anyone with glib2 packages
    from before Sept. 22, 2004 to update.

--- devel/glib2/buildlink3.mk
+++ devel/glib2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/04/01 18:13:31 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2005/04/07 02:35:39 reed Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GLIB2_BUILDLINK3_MK:=	${GLIB2_BUILDLINK3_MK}+
@@ -12,6 +12,7 @@ BUILDLINK_PACKAGES+=	glib2
 
 .if !empty(GLIB2_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.glib2+=	glib2>=2.4.0
+BUILDLINK_RECOMMENDED.glib2+=	glib2>=2.6.1
 BUILDLINK_PKGSRCDIR.glib2?=	../../devel/glib2
 .endif	# GLIB2_BUILDLINK3_MK
 
