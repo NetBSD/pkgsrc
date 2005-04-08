8742170448f92eccc0aa207a5dc71e973024c7be
salo <salo@pkgsrc.org>
2005-04-08 15:12:48 +0000

    Update to version 1.0.2
    Changes:
    - convert to options.mk
    
    1.0.2:
    ======
    Due to the new SILC Server release with support to the UTF-8 nicknames and
    channel names new version of the SILC Client is released as it seems the
    older ones do not function very well with the new server.  This version
    includes support to the UTF-8 encoded nicknames and channel names, however,
    certain problems may still exists, as this is a slight premature release.
    
    - Added support for UTF-8 encoded nicknames and channel names.
    - Command queue implemented for better UTF-8 support.
    - --with-silc-includes and --with-silc-libs added gto configure to use
      pre-installed SILC Toolkit.

---
+++ chat/silc-client/options.mk
@@ -0,0 +1,26 @@
+# $NetBSD: options.mk,v 1.1 2005/04/08 15:12:48 salo Exp $
+#
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.silc-client
+PKG_SUPPORTED_OPTIONS=	debug inet6 perl
+PKG_DEFAULT_OPTIONS=	inet6 perl
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug
+.endif
+
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--enable-ipv6
+.endif
+
+.if !empty(PKG_OPTIONS:Mperl)
+CONFIGURE_ARGS+=	--with-perl-lib=${PREFIX}/lib/${PKGBASE}/perl5
+PERL5_REQD+=		5.6.1nb10
+.include "../../lang/perl5/buildlink3.mk"
+DEPENDS+=		p5-File-MMagic>=1.20:../../misc/p5-File-MMagic
+PLIST_SRC+=		${PKGDIR}/PLIST.perl
+PLIST_SRC+=		${WRKDIR}/PLIST.perl
+.else
+CONFIGURE_ARGS+=	--with-perl=no
+.endif
