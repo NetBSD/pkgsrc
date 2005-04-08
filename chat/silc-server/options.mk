7d04a887d619b239c50856ff96a59d8e09388d54
salo <salo@pkgsrc.org>
2005-04-08 15:09:43 +0000

    Update to version 0.9.19p1
    Changes:
    - convert to options.mk
    
    0.9.19p1:
    =========
    A little update with this 0.9.19p1.  After such a major release problems
    were expected and the p1 fixes some crashes.  Upgrade strongly
    recommended.
    
    0.9.19:
    =======
    And after a long break new SILC Server is out.  This version finalizes the
    SILC protocol version 1.2 development and introduces UTF-8 nicknames, channel
    names, usernames and host names.  It is now possible to create practically any
    kind of nicknames and channel names.  Practically all letters, numbers and
    punctuation marks are supported.  Special characters, control characters and
    various odd symbol characters however are not allowed.  Several minor and
    major bugs has been fixed as well. Upgrading is strongly recommended.  Old
    clients that does not yet support UTF-8 encoded nicknames and channel names
    are still able to connect and function normally as long as they do not need
    to handle odd UTF-8 encoded names.
    
    - Added support for UTF-8 encoded identifier strings, such as nicknames
      and channel names.
    - Fixed founder mode handling on JOIN on normal/backup on empty
      channels.
    - Fixed WATCH command handling on backup router.
    - Fixed WATCH command announcing.  The WATCH and SILC Gaim buddy list
      should work better now.
    - Simplified INVITE and BAN string handling in server.  Announcing
      INVITE and BAN strings should work more reliably now.
    - Fixed several bugs from the backup and resuming protocol.
    - Fixed, hopefully, all the wrong server statistics numbers.
    - Fixed CLOSE command to handle backup routers correctly.
    - Fixed various detaching and resuming bugs.
    - Fixed announcing to not announce unregistered (ghost) clients.
    - Fixed reconnect_keep_trying and QoS settings in server config files.
    - Several other bugfixes were made.

---
+++ chat/silc-server/options.mk
@@ -0,0 +1,15 @@
+# $NetBSD: options.mk,v 1.1 2005/04/08 15:09:43 salo Exp $
+#
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.silc-server
+PKG_SUPPORTED_OPTIONS=	debug inet6
+PKG_DEFAULT_OPTIONS=	inet6
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug
+.endif
+
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--enable-ipv6
+.endif
