5923432800510d09fcf817dffbb42704a9342b26
schmonz <schmonz@pkgsrc.org>
2005-04-15 05:17:01 +0000

    Update to 20050415. Changes:
    * Add "foo_log" booleans to enable or disable logging (enabled by default).
    * Always run loggers as the "qmaill" user. This makes it simpler
        to define alternate loggers.

--- mail/qmail-run/files/qmailpop3d.sh
+++ mail/qmail-run/files/qmailpop3d.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: qmailpop3d.sh,v 1.5 2005/03/30 20:06:20 schmonz Exp $
+# $NetBSD: qmailpop3d.sh,v 1.6 2005/04/15 05:17:02 schmonz Exp $
 #
 # @PKGNAME@ script to control qmail-pop3d (POP3 server for Maildirs).
 #
@@ -11,7 +11,7 @@ name="qmailpop3d"
 
 # User-settable rc.conf variables and their default values:
 : ${qmailpop3d_postenv:=""}
-: ${qmailpop3d_tcpflags:="-v -R -H -l 0"}
+: ${qmailpop3d_tcpflags:="-vRHl0"}
 : ${qmailpop3d_tcphost:="0"}
 : ${qmailpop3d_tcpport:="110"}
 : ${qmailpop3d_datalimit:="2000000"}
@@ -19,7 +19,9 @@ name="qmailpop3d"
 : ${qmailpop3d_prepop3d:=""}
 : ${qmailpop3d_checkpassword:="@LOCALBASE@/bin/checkpassword"}
 : ${qmailpop3d_maildirname:="Maildir"}
-: ${qmailpop3d_logcmd:="@LOCALBASE@/bin/setuidgid qmaill logger -t nb${name} -p mail.info"}
+: ${qmailpop3d_log:="YES"}
+: ${qmailpop3d_logcmd:="logger -t nb${name} -p mail.info"}
+: ${qmailpop3d_nologcmd:="@LOCALBASE@/bin/multilog -*"}
 
 if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
@@ -43,7 +45,10 @@ qmailpop3d_precmd()
 	# tcpserver(1) is akin to inetd(8), but runs one service per process.
 	# We want to signal only the tcpserver process responsible for POP
 	# service. Use argv0(1) to set procname to "qmailpop3d".
-	command="@SETENV@ - ${qmailpop3d_postenv} @LOCALBASE@/bin/softlimit -m ${qmailpop3d_datalimit} ${qmailpop3d_pretcpserver} @LOCALBASE@/bin/argv0 @LOCALBASE@/bin/tcpserver ${name} ${qmailpop3d_tcpflags} -x @PKG_SYSCONFDIR@/tcp.pop3.cdb -c `@HEAD@ -1 @PKG_SYSCONFDIR@/control/concurrencypop3` ${qmailpop3d_tcphost} ${qmailpop3d_tcpport} @LOCALBASE@/bin/qmail-popup `@HEAD@ -1 @PKG_SYSCONFDIR@/control/me` ${qmailpop3d_checkpassword} ${qmailpop3d_prepop3d} @LOCALBASE@/bin/qmail-pop3d ${qmailpop3d_maildirname} 2>&1 | ${qmailpop3d_logcmd}"
+	if [ -f /etc/rc.subr ]; then
+		checkyesno qmailpop3d_log || qmailpop3d_logcmd=${qmailpop3d_nologcmd}
+	fi
+	command="@SETENV@ - ${qmailpop3d_postenv} @LOCALBASE@/bin/softlimit -m ${qmailpop3d_datalimit} ${qmailpop3d_pretcpserver} @LOCALBASE@/bin/argv0 @LOCALBASE@/bin/tcpserver ${name} ${qmailpop3d_tcpflags} -x @PKG_SYSCONFDIR@/tcp.pop3.cdb -c `@HEAD@ -1 @PKG_SYSCONFDIR@/control/concurrencypop3` ${qmailpop3d_tcphost} ${qmailpop3d_tcpport} @LOCALBASE@/bin/qmail-popup `@HEAD@ -1 @PKG_SYSCONFDIR@/control/me` ${qmailpop3d_checkpassword} ${qmailpop3d_prepop3d} @LOCALBASE@/bin/qmail-pop3d ${qmailpop3d_maildirname} 2>&1 | @LOCALBASE@/bin/setuidgid qmaill ${qmailpop3d_logcmd}"
 	command_args="&"
 	rc_flags=""
 }
