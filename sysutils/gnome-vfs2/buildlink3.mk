cb62da3135d7c3c7555623c7224db61f71e6eebf
rh <rh@pkgsrc.org>
2005-04-15 01:59:29 +0000

    Set BUILDLINK_RECOMMENDED to 2.10.0 or higher due to CDDB vulnerability: http://secunia.com/advisories/14877/

--- sysutils/gnome-vfs2/buildlink3.mk
+++ sysutils/gnome-vfs2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2005/03/18 19:32:21 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2005/04/15 01:59:29 rh Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_VFS2_BUILDLINK3_MK:=	${GNOME_VFS2_BUILDLINK3_MK}+
@@ -12,6 +12,7 @@ BUILDLINK_PACKAGES+=	gnome-vfs2
 
 .if !empty(GNOME_VFS2_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gnome-vfs2+=		gnome-vfs2>=2.8.0
+BUILDLINK_RECOMMENDED.gnome-vfs2+=	gnome-vfs2>=2.10.0
 BUILDLINK_PKGSRCDIR.gnome-vfs2?=	../../sysutils/gnome-vfs2
 
 PRINT_PLIST_AWK+=	/^@dirrm lib\/gnome-vfs-2.0\/modules/ \
