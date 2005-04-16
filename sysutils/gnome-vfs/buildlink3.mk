5b11009787fc3eb6252e27cad157073977e590f2
snj <snj@pkgsrc.org>
2005-04-16 19:32:17 +0000

    Pullup ticket 448 - requested by Lubomir Sedlacik security fix for gnome-vfs
    Revisions pulled up:
    - pkgsrc/sysutils/gnome-vfs/Makefile		1.47
    - pkgsrc/sysutils/gnome-vfs/buildlink3.mk	1.8
    - pkgsrc/sysutils/gnome-vfs/distinfo		1.16
    - pkgsrc/sysutils/gnome-vfs/patches/patch-aa	1.7
    
    
        Module Name:    pkgsrc
        Committed By:   rh
        Date:           Fri Apr 15 02:09:27 UTC 2005
    
        Modified Files:
                pkgsrc/sysutils/gnome-vfs: Makefile buildlink3.mk distinfo
        Added Files:
                pkgsrc/sysutils/gnome-vfs/patches: patch-aa
    
        Log Message:
        Patch remote code execution security hole pointed out in advisory at
        http://secunia.com/advisories/14877/
        Bump PKGREVISION
        Set BUILDLINK_RECOMMENDED to gnome-vfs>=1.0.5nb8

--- sysutils/gnome-vfs/buildlink3.mk
+++ sysutils/gnome-vfs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/11/30 18:26:40 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7.4.1 2005/04/16 19:32:17 snj Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_VFS_BUILDLINK3_MK:=	${GNOME_VFS_BUILDLINK3_MK}+
@@ -11,7 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngnome-vfs}
 BUILDLINK_PACKAGES+=	gnome-vfs
 
 .if !empty(GNOME_VFS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gnome-vfs+=	gnome-vfs>=1.0.5nb7
+BUILDLINK_DEPENDS.gnome-vfs+=		gnome-vfs>=1.0.5nb7
+BUILDLINK_RECOMMENDED.gnome-vfs+=	gnome-vfs>=1.0.5nb8
 BUILDLINK_PKGSRCDIR.gnome-vfs?=	../../sysutils/gnome-vfs
 .endif	# GNOME_VFS_BUILDLINK3_MK
 
