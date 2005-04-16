6c5768a6a802148c1da45a8d703f9aa5717ece70
snj <snj@pkgsrc.org>
2005-04-16 19:23:22 +0000

    Pullup ticket 447 - requested by Lubomir Sedlacik security fix for libcdaudio
    Revisions pulled up:
    - pkgsrc/audio/libcdaudio/Makefile		1.25
    - pkgsrc/audio/libcdaudio/buildlink3.mk		1.3
    - pkgsrc/audio/libcdaudio/distinfo		1.11
    - pkgsrc/audio/libcdaudio/patches/patch-ac	1.1
    
    
        Module Name:    pkgsrc
        Committed By:   rh
        Date:           Fri Apr 15 01:50:57 UTC 2005
    
        Modified Files:
                pkgsrc/audio/libcdaudio: Makefile buildlink3.mk distinfo
        Added Files:
                pkgsrc/audio/libcdaudio/patches: patch-ac
    
        Log Message:
        Fix vulnerability pointed out in http://secunia.com/advisories/14877/
        Bump PKGREVISION
        Update BUILDLINK_RECOMMENDED

--- audio/libcdaudio/buildlink3.mk
+++ audio/libcdaudio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:13:07 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.2.4.1 2005/04/16 19:23:22 snj Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBCDAUDIO_BUILDLINK3_MK:=	${LIBCDAUDIO_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libcdaudio
 
 .if !empty(LIBCDAUDIO_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libcdaudio+=		libcdaudio>=0.99.4nb1
-BUILDLINK_RECOMMENDED.libcdaudio+=	libcdaudio>=0.99.10nb1
+BUILDLINK_RECOMMENDED.libcdaudio+=	libcdaudio>=0.99.12nb1
 BUILDLINK_PKGSRCDIR.libcdaudio?=	../../audio/libcdaudio
 .endif	# LIBCDAUDIO_BUILDLINK3_MK
 
