0057068f8031f0f695d013d1bfed1c48fadb1f01
adam <adam>
2005-04-21 08:53:11 +0000

    Changes 1.6rc1: * Fixed: memory leaks, crashes * Now works with a proxy * Enhanced scripts * Bug fixes

--- chat/ekg/buildlink3.mk
+++ chat/ekg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/10/03 00:13:14 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2005/04/21 08:53:11 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 EKG_BUILDLINK3_MK:=	${EKG_BUILDLINK3_MK}+
@@ -11,14 +11,16 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nekg}
 BUILDLINK_PACKAGES+=	ekg
 
 .if !empty(EKG_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.ekg+=		ekg>=1.1
-BUILDLINK_RECOMMENDED.ekg?=	ekg>=1.5nb1
+BUILDLINK_DEPENDS.ekg+=		ekg>=1.6
+BUILDLINK_RECOMMENDED.ekg?=	ekg>=1.6
 BUILDLINK_PKGSRCDIR.ekg?=	../../chat/ekg
 .endif	# EKG_BUILDLINK3_MK
 
 .include "../../devel/ncurses/buildlink3.mk"
+.include "../../devel/pkgconfig/buildlink3.mk"
 .include "../../devel/readline/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
