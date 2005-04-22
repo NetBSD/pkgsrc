dfd7ca6531b20cc67d4807b427dba6f800a3e126
jlam <jlam@pkgsrc.org>
2005-04-22 04:19:26 +0000

    MAKE_PROGRAM should already be a full path, so don't bother with the check.

--- mk/tools/make.mk
+++ mk/tools/make.mk
@@ -1,4 +1,4 @@
-# $NetBSD: make.mk,v 1.1 2005/04/15 00:00:21 jlam Exp $
+# $NetBSD: make.mk,v 1.2 2005/04/22 04:19:26 jlam Exp $
 
 # Always provide a symlink from ${TOOLS_DIR}/bin/make to the "make"
 # used to build the package.  This lets a bare "make" invoke the
@@ -6,6 +6,4 @@
 #
 TOOLS_SYMLINK+=		make
 TOOLS_CMD.make=		${TOOLS_DIR}/bin/make
-.if !empty(MAKE_PROGRAM:M/*)
 TOOLS_REAL_CMD.make=	${MAKE_PROGRAM}
-.endif
