0c235b964fec68498b7f9b23a26c1a0c1cb3e420
shannonjr <shannonjr@pkgsrc.org>
2005-04-23 12:25:05 +0000

    Update from 0.9.10 to 0.9.11. This is part of a multi-package update for S/MIME capability involving gnupg-devel and dirmngr also. The changes to this pkg are bug fixes.

--- security/libksba/buildlink3.mk
+++ security/libksba/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/10/11 09:46:44 shannonjr Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2005/04/23 12:25:05 shannonjr Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBKSBA_BUILDLINK3_MK:=	${LIBKSBA_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libksba
 
 .if !empty(LIBKSBA_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libksba+=	libksba>=0.9.7
-BUILDLINK_RECOMMENDED.libksba+=	libksba>=0.9.7
+BUILDLINK_RECOMMENDED.libksba+=	libksba>=0.9.11
 BUILDLINK_PKGSRCDIR.libksba?=	../../security/libksba
 .endif	# LIBKSBA_BUILDLINK3_MK
 
