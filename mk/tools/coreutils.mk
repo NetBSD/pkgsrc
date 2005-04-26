7d32d543003f6937be5e2319ffb4ea6770c77e3c
jlam <jlam@pkgsrc.org>
2005-04-26 23:02:48 +0000

    Let USE_TOOLS+=coreutils cause BASENAME, CAT, CHGRP, etc. to be overridden to point to the pkgsrc versions of those tools.

--- mk/tools/coreutils.mk
+++ mk/tools/coreutils.mk
@@ -1,4 +1,4 @@
-# $NetBSD: coreutils.mk,v 1.2 2005/04/26 15:32:05 jlam Exp $
+# $NetBSD: coreutils.mk,v 1.3 2005/04/26 23:02:48 jlam Exp $
 #
 # This Makefile fragment unconditionally replaces the system-supplied
 # "core" utilities with the ones from the GNU coreutils package when
@@ -25,11 +25,52 @@ _TOOLS_COREUTILS+=	[ basename cat chgrp chmod chown cp cut date	\
 			ls mkdir mv nice pwd rm rmdir sort tail tee	\
 			test touch tr true tsort wc
 
+_TOOLS_VARNAME.basename=	BASENAME
+_TOOLS_VARNAME.cat=		CAT
+_TOOLS_VARNAME.chgrp=		CHGRP
+_TOOLS_VARNAME.chmod=		CHMOD
+_TOOLS_VARNAME.chown=		CHOWN
+_TOOLS_VARNAME.cp=		CP
+_TOOLS_VARNAME.cut=		CUT
+_TOOLS_VARNAME.date=		DATE
+_TOOLS_VARNAME.dirname=		DIRNAME
+_TOOLS_VARNAME.echo=		ECHO
+_TOOLS_VARNAME.env=		SETENV
+_TOOLS_VARNAME.expr=		EXPR
+_TOOLS_VARNAME.false=		FALSE
+_TOOLS_VARNAME.head=		HEAD
+_TOOLS_VARNAME.hostname=	HOSTNAME
+_TOOLS_VARNAME.id=		ID
+_TOOLS_VARNAME.ln=		LN
+_TOOLS_VARNAME.ls=		LS
+_TOOLS_VARNAME.mkdir=		MKDIR
+_TOOLS_VARNAME.mv=		MV
+_TOOLS_VARNAME.nice=		NICE
+_TOOLS_VARNAME.pwd=		PWD
+_TOOLS_VARNAME.rm=		RM
+_TOOLS_VARNAME.rmdir=		RMDIR
+_TOOLS_VARNAME.sort=		SORT
+_TOOLS_VARNAME.tail=		TAIL
+_TOOLS_VARNAME.tee=		TEE
+_TOOLS_VARNAME.test=		TEST
+_TOOLS_VARNAME.touch=		TOUCH
+_TOOLS_VARNAME.tr=		TR
+_TOOLS_VARNAME.true=		TRUE
+_TOOLS_VARNAME.tsort=		TSORT
+_TOOLS_VARNAME.wc=		WC
+
 # Create symlinks for each of the coreutils under ${TOOLS_DIR}.
 .    for _t_ in ${_TOOLS_COREUTILS}
-TOOLS_SYMLINK+=		${_t_}
-TOOLS_REAL_CMD.${_t_}=	${LOCALBASE}/bin/${GNU_PROGRAM_PREFIX}${_t_}
+TOOLS_SYMLINK+=			${_t_}
+TOOLS_REAL_CMD.${_t_}=		${LOCALBASE}/bin/${GNU_PROGRAM_PREFIX}${_t_}
+.      if defined(_TOOLS_VARNAME.${_t_}) && exists(${TOOLS_REAL_CMD.${_t_}})
+${_TOOLS_VARNAME.${_t_}}=	${TOOLS_REAL_CMD.${_t_}}
+.      endif
 .    endfor
 .    undef _t_
+#
+# Fix up a few definitions, e.g. ${MKDIR} needs to be "mkdir -p".
+#
+${_TOOLS_VARNAME.mkdir}:=	${${_TOOLS_VARNAME.mkdir}} -p
 .  endif
 .endif
