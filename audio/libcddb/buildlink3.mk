73d80c2caa9a4b744b5c6433339592b3fe5f9776
kristerw <kristerw@pkgsrc.org>
2005-04-29 19:46:52 +0000

    Include converters/libiconv/buildlink3.mk

--- audio/libcddb/buildlink3.mk
+++ audio/libcddb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/10/03 00:13:07 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2005/04/29 19:46:52 kristerw Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBCDDB_BUILDLINK3_MK:=	${LIBCDDB_BUILDLINK3_MK}+
@@ -16,6 +16,7 @@ BUILDLINK_RECOMMENDED.libcddb+=	libcddb>=0.9.5nb1
 BUILDLINK_PKGSRCDIR.libcddb?=	../../audio/libcddb
 .endif	# LIBCDDB_BUILDLINK3_MK
 
+.include "../../converters/libiconv/buildlink3.mk"
 .include "../../misc/libcdio/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
