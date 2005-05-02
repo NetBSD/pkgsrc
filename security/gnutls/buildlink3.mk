a5aa20f67af93ecaf0109d5ee67340ff6c2d1b8f
salo <salo@pkgsrc.org>
2005-05-02 19:48:37 +0000

    Bump BUILDLINK_RECOMMENDED after latest security update. (hi wiz!)

--- security/gnutls/buildlink3.mk
+++ security/gnutls/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/10/03 00:18:08 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2005/05/02 19:48:37 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GNUTLS_BUILDLINK3_MK:=	${GNUTLS_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	gnutls
 
 .if !empty(GNUTLS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gnutls+=	gnutls>=1.0.13
-BUILDLINK_RECOMMENDED.gnutls+=	gnutls>=1.0.20nb2
+BUILDLINK_RECOMMENDED.gnutls+=	gnutls>=1.2.3
 BUILDLINK_PKGSRCDIR.gnutls?=	../../security/gnutls
 .endif	# GNUTLS_BUILDLINK3_MK
 
