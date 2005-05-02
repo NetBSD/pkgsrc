622c4344c01e85ddb27d8fc4087162e523bc4c93
jlam <jlam@pkgsrc.org>
2005-05-02 04:33:53 +0000

    PERL5 shouldn't be recursively defined.

--- mk/tools/perl.mk
+++ mk/tools/perl.mk
@@ -1,4 +1,4 @@
-# $NetBSD: perl.mk,v 1.3 2005/04/28 03:01:11 jlam Exp $
+# $NetBSD: perl.mk,v 1.4 2005/05/02 04:33:53 jlam Exp $
 
 # Create a symlink from ${TOOLS_DIR}/bin/perl to ${PERL5} when USE_PERL5
 # is defined.  This ensures that when "perl" is invoked, the pkgsrc perl
@@ -12,7 +12,7 @@ MAKEFLAGS+=		TOOLS_IGNORE.perl=
 TOOLS_DEPMETHOD.perl?=	BUILD_DEPENDS
 TOOLS_DEPENDS.perl?=	${BUILDLINK_DEPENDS.perl}:${BUILDLINK_PKGSRCDIR.perl}
 TOOLS_SYMLINK+=		perl
-TOOLS_REAL_CMD.perl=	${PERL5}
+TOOLS_REAL_CMD.perl=	${LOCALBASE}/bin/perl
 .    if exists(${TOOLS_REAL_CMD.perl})
 PERL5=			${TOOLS_REAL_CMD.perl}
 .    endif
