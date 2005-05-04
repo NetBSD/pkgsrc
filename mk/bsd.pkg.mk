91092c57895d2b87d14d5f6701952bdd03c18a73
jlam <jlam@pkgsrc.org>
2005-05-04 06:42:43 +0000

    Teach the new tools framework about ${STRIP}, which is used in some packages to strip installed executables.  If INSTALL_UNSTRIPPED == "yes", then we create a "strip" wrapper in ${TOOLS_DIR} that just calls ${TRUE} by considering ${TRUE} the system-supplied strip command.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1626 2005/05/04 04:23:34 jlam Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1627 2005/05/04 06:42:43 jlam Exp $
 #
 # This file is in the public domain.
 #
@@ -943,8 +943,8 @@ USE_TOOLS+=	[ awk basename cat chgrp chmod chown cmp cp cut date	\
 		dirname echo egrep env expr false fgrep file find grep	\
 		gtar gunzip gzcat gzip head hostname id install		\
 		ldconfig ln ls m4 mkdir mtree mv nice pax pwd rm rmdir	\
-		sed sh shlock sort tail tee test touch tr true tsort wc	\
-		xargs
+		sed sh shlock sort strip tail tee test touch tr true	\
+		tsort wc xargs
 
 # We need a mail command to send mail to ${PKGSRC_MESSAGE_RECIPIENTS}.
 .if !empty(PKGSRC_MESSAGE_RECIPIENTS)
