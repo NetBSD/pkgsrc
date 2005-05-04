44a9ec78db3e776ce842b2f2cae63614ead3025d
jlam <jlam@pkgsrc.org>
2005-05-04 06:42:43 +0000

    Teach the new tools framework about ${STRIP}, which is used in some packages to strip installed executables.  If INSTALL_UNSTRIPPED == "yes", then we create a "strip" wrapper in ${TOOLS_DIR} that just calls ${TRUE} by considering ${TRUE} the system-supplied strip command.

--- mk/tools/tools.AIX.mk
+++ mk/tools/tools.AIX.mk
@@ -1,4 +1,4 @@
-# $NetBSD: tools.AIX.mk,v 1.5 2005/05/03 16:30:34 jlam Exp $
+# $NetBSD: tools.AIX.mk,v 1.6 2005/05/04 06:42:43 jlam Exp $
 #
 # System-supplied tools for the AIX operating system.
 
@@ -41,6 +41,7 @@ TOOLS_PLATFORM.sed?=		/usr/bin/sed
 TOOLS_PLATFORM.sh?=		/bin/sh
 TOOLS_PLATFORM.shlock?=		/usr/bin/shlock
 TOOLS_PLATFORM.sort?=		/usr/bin/sort
+TOOLS_PLATFORM.strip?=		${TOOLS_PLATFORM.true}
 TOOLS_PLATFORM.tail?=		/usr/bin/tail
 TOOLS_PLATFORM.tee?=		/usr/bin/tee
 TOOLS_PLATFORM.test?=		test			# shell builtin
