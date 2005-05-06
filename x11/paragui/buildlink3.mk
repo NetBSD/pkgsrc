f343258b5586fa8da3747c0f5e946b34a46c6b37
xtraeme <xtraeme@pkgsrc.org>
2005-05-06 01:24:57 +0000

    Update to 1.1.8, lots of changes! please review the Changelog file.

--- x11/paragui/buildlink3.mk
+++ x11/paragui/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/12/28 23:18:18 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2005/05/06 01:24:57 xtraeme Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PARAGUI_BUILDLINK3_MK:=	${PARAGUI_BUILDLINK3_MK}+
@@ -11,13 +11,14 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nparagui}
 BUILDLINK_PACKAGES+=	paragui
 
 .if !empty(PARAGUI_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.paragui+=	paragui>=1.0.4nb3
-BUILDLINK_RECOMMENDED.paragui+=	paragui>=1.0.4nb6
+BUILDLINK_DEPENDS.paragui+=	paragui>=1.1.8
 BUILDLINK_PKGSRCDIR.paragui?=	../../x11/paragui
 .endif	# PARAGUI_BUILDLINK3_MK
 
 .include "../../devel/SDL/buildlink3.mk"
+.include "../../devel/libsigc++/buildlink3.mk"
 .include "../../devel/physfs/buildlink3.mk"
+.include "../../devel/pkgconfig/buildlink3.mk"
 .include "../../graphics/SDL_image/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
