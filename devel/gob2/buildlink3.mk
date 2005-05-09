af6d5e549b06c1dddf45c7535b8bfa151ce0650f
rillig <rillig@pkgsrc.org>
2005-05-09 23:46:04 +0000

    Replaced wip category with devel. Approved by xtraeme.

--- devel/gob2/buildlink3.mk
+++ devel/gob2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/12/02 21:05:44 dillo Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/05/09 23:46:04 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GOB2_BUILDLINK3_MK:=	${GOB2_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	gob2
 
 .if !empty(GOB2_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gob2+=	gob2>=2.0.10
-BUILDLINK_PKGSRCDIR.gob2?=	../../wip/gob2
+BUILDLINK_PKGSRCDIR.gob2?=	../../devel/gob2
 .endif	# GOB2_BUILDLINK3_MK
 
 #.include "../../devel/glib2/buildlink3.mk"
