ef1665c54fda9fd94ea460903a4a8c9ba4b3e90a
jlam <jlam@pkgsrc.org>
2005-05-10 19:56:51 +0000

    Remove conditional expression from tools.*.mk files.  Strip handling has moved to its own file, and we only need to define TOOLS_PLATFORM.strip if it actually exists on the system.

--- mk/tools/tools.BSDOS.mk
+++ mk/tools/tools.BSDOS.mk
@@ -1,4 +1,4 @@
-# $NetBSD: tools.BSDOS.mk,v 1.8 2005/05/04 06:42:43 jlam Exp $
+# $NetBSD: tools.BSDOS.mk,v 1.9 2005/05/10 19:56:51 jlam Exp $
 #
 # System-supplied tools for the BSD/OS operating system.
 
@@ -47,8 +47,7 @@ TOOLS_PLATFORM.rmdir?=		/bin/rmdir
 TOOLS_PLATFORM.sed?=		/usr/bin/sed
 TOOLS_PLATFORM.sh?=		/bin/bash
 TOOLS_PLATFORM.sort?=		/usr/bin/sort
-TOOLS_PLATFORM.strip?=		\
-	${_INSTALL_UNSTRIPPED:D${TOOLS_PLATFORM.true}:U/usr/bin/strip}
+TOOLS_PLATFORM.strip?=		/usr/bin/strip
 TOOLS_PLATFORM.tail?=		/usr/bin/tail
 TOOLS_PLATFORM.tee?=		/usr/bin/tee
 TOOLS_PLATFORM.test?=		test			# shell builtin
