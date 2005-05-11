3d6d7c6d66580357ac9b78d21a1ea6207cfcded0
peter <peter@pkgsrc.org>
2005-05-11 22:01:28 +0000

    Update to 20050511. Changes:
    Updated to OpenBSD 3.7 pf:
    * Support limiting TCP connections by establishment rate, automatically
      adding flooding IP addresses to tables and flushing states
      (max-src-conn-rate, overload <table>, flush global).
    * Improved functionality of tags (tag and tagged for translation rules,
      tagging of all packets matching state entries).
    * Improved diagnostics (error messages and additional counters from pfctl -si).
    * New keyword set skip on to skip filtering on arbitrary interfaces,
      like loopback.
    * Several bugfixes improving stability.
    
    ALTQ is now also supported by using the option 'altq', see the homepage
    for information about how to apply the kernel patch.
    
    Approved by: Thomas Klausner <wiz@NetBSD.org>

--- security/pflkm/builtin.mk
+++ security/pflkm/builtin.mk
@@ -1,6 +1,6 @@
-# $NetBSD: builtin.mk,v 1.2 2004/12/01 00:20:20 peter Exp $
+# $NetBSD: builtin.mk,v 1.3 2005/05/11 22:01:28 peter Exp $
 
-_PF_VERSION=	3.6	# pkg default
+_PF_VERSION=	3.7	# pkg default
 _PF_PFVAR_H=	/usr/include/net/pfvar.h
 
 .if !defined(IS_BUILTIN.pflkm)
@@ -8,10 +8,14 @@ IS_BUILTIN.pflkm=	no
 .  if exists(${_PF_PFVAR_H})
 IS_BUILTIN.pflkm=	yes
 
+# OpenBSD 3.7: pf_threshold added
+_PF_3_7!=	${GREP} -c pf_threshold ${_PF_PFVAR_H} || ${TRUE}
 # OpenBSD 3.6: pf_cksum_fixup added
 _PF_3_6!=	${GREP} -c pf_cksum_fixup ${_PF_PFVAR_H} || ${TRUE}
 
-.if ${_PF_3_6} == "1"
+.if ${_PF_3_7} == "1"
+BUILTIN_PKG.pflkm=	3.7
+.elif ${_PF_3_6} == "1"
 BUILTIN_PKG.pflkm=	3.6
 .else
 BUILTIN_PKG.pflkm=	3.5
