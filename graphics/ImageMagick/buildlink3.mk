ba3e338073a314bb2c489177f9403975529c54cb
salo <salo@pkgsrc.org>
2005-05-11 13:22:15 +0000

    Bump BUILDLINK_RECOMMENDED after latest update with security fixes.

--- graphics/ImageMagick/buildlink3.mk
+++ graphics/ImageMagick/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/12/28 23:18:20 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2005/05/11 13:22:15 salo Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 IMAGEMAGICK_BUILDLINK3_MK:=	${IMAGEMAGICK_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	ImageMagick
 
 .if !empty(IMAGEMAGICK_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.ImageMagick+=		ImageMagick>=5.5.7.11nb1
-BUILDLINK_RECOMMENDED.ImageMagick+=	ImageMagick>=6.1.6.0nb1
+BUILDLINK_RECOMMENDED.ImageMagick+=	ImageMagick>=6.2.2.0
 BUILDLINK_PKGSRCDIR.ImageMagick?=	../../graphics/ImageMagick
 .endif	# IMAGEMAGICK_BUILDLINK3_MK
 
