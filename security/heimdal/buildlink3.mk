5a51ec7a26a5d24ab7b54de176895e7317393d88
jlam <jlam>
2005-05-11 22:03:52 +0000

    I mixed up MAKE_FLAGS with MAKEFLAGS.  The latter is what we actually use to pass make flags to bmake.

--- security/heimdal/buildlink3.mk
+++ security/heimdal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2005/05/09 05:14:08 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2005/05/11 22:03:52 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 HEIMDAL_BUILDLINK3_MK:=	${HEIMDAL_BUILDLINK3_MK}+
@@ -22,8 +22,8 @@ BUILDLINK_INCDIRS.heimdal?=	include/krb5
 .if !defined(PKG_BUILD_OPTIONS.heimdal)
 PKG_BUILD_OPTIONS.heimdal!=						\
 	cd ${BUILDLINK_PKGSRCDIR.heimdal} &&				\
-	${MAKE} show-var ${MAKE_FLAGS} VARNAME=PKG_OPTIONS
-MAKE_FLAGS+=	PKG_BUILD_OPTIONS.heimdal=${PKG_BUILD_OPTIONS.heimdal:Q}
+	${MAKE} show-var ${MAKEFLAGS} VARNAME=PKG_OPTIONS
+MAKEFLAGS+=	PKG_BUILD_OPTIONS.heimdal=${PKG_BUILD_OPTIONS.heimdal:Q}
 .endif
 MAKE_VARS+=	PKG_BUILD_OPTIONS.heimdal
 
