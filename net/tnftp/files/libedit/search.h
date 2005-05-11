af58dc0c001dd0cb387e34545fbe5a05798d4962
lukem <lukem@pkgsrc.org>
2005-05-11 09:17:39 +0000

    Update to tnftp 20070806. Notable changes:  - Implement '-s srcaddr' to set the local IP address for all connections.  - Support '-q quittime' when waiting for server replies.  - Use IEC 60027-2 "KiB", "MiB" (etc) instead of "KB", "MB", ...  - Portability fixes, including for FreeBSD, Mac OS X, and Solaris.

--- net/tnftp/files/libedit/search.h
+++ net/tnftp/files/libedit/search.h
@@ -1,4 +1,4 @@
-/*	NetBSD: search.h,v 1.2 2005/05/11 01:17:39 lukem Exp	*/
+/*	$NetBSD: search.h,v 1.1.1.3 2005/05/11 09:17:39 lukem Exp $	*/
 /*	from	NetBSD: search.h,v 1.8 2003/10/18 23:27:36 christos Exp	*/
 
 /*-
