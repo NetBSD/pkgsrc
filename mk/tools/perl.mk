1ee9760a813257fe577beddaebf936b2d44a0e2c
jlam <jlam@pkgsrc.org>
2005-05-12 21:03:46 +0000

    Teach the new tools framework more about perl.  We include perl5/vars.mk in mk/tools/perl.mk since many packages expect to be able to use the variables defined in vars.mk, but those variables can only be defined if PERL5 is correctly defined, and that is only true after it is set here.

--- mk/tools/perl.mk
+++ mk/tools/perl.mk
@@ -1,4 +1,4 @@
-# $NetBSD: perl.mk,v 1.7 2005/05/11 19:42:17 jlam Exp $
+# $NetBSD: perl.mk,v 1.8 2005/05/12 21:03:46 jlam Exp $
 
 # Create a symlink from ${TOOLS_DIR}/bin/perl to ${PERL5} when USE_PERL5
 # is defined.  This ensures that when "perl" is invoked, the pkgsrc perl
@@ -12,13 +12,22 @@ MAKEFLAGS+=		TOOLS_IGNORE.perl=
 TOOLS_DEPMETHOD.perl?=	BUILD_DEPENDS
 TOOLS_DEPENDS.perl?=	${BUILDLINK_DEPENDS.perl}:${BUILDLINK_PKGSRCDIR.perl}
 TOOLS_CREATE+=		perl
-EVAL_PREFIX+=		TOOLS_PREFIX.perl=perl
+FIND_PREFIX:=		TOOLS_PREFIX.perl=perl
+.    include "../../mk/find-prefix.mk"
 TOOLS_REAL_CMD.perl=	${TOOLS_PREFIX.perl}/bin/perl
 PERL5=			${TOOLS_REAL_CMD.perl}
+CONFIGURE_ENV+=		PERL=${PERL5:Q}
 .  endif
 .  if defined(TOOLS_DEPMETHOD.perl) && defined(TOOLS_DEPENDS.perl)
 .    if empty(${TOOLS_DEPMETHOD.perl}:M${TOOLS_DEPENDS.perl})
 ${TOOLS_DEPMETHOD.perl}+=	${TOOLS_DEPENDS.perl}
 .    endif
 .  endif
+
+# Define PERL5_* variables that locate the site directories for ${PERL5}.
+# These variables depend on PERL5 being properly defined and existing
+# on the filesystem.
+#
+.  include "../../lang/perl5/vars.mk"
+
 .endif
