d661269b3e03a8d5df862a001ed9b8cd923ba067
salo <salo@pkgsrc.org>
2005-05-12 10:32:18 +0000

    Update to version 1.3.0
    Changes:
    - Fixes for two remotely exploitable crash bugs.  See
      http://gaim.sourceforge.net/security/ for more information.
    - Removed parts of the font selection dialog that were not respected
    - Fix being invited to a multi user chat on MSN
    - Multiple SILC accounts should work now (Pekka Riikonen)
    - Fix times on jabber chat backlogs
    - Fix gevolution plugin to compile with e-d-s 1.0 or 1.2
    - Fix gevolution plugin to remember buddy name when someone added you
      and you then add them
    - Formatting in jabber chats works
    - Fix to prevent MSN disconnecting if you change status while
    - connecting
    - Change to correctly handle adding jabber buddies on ejabberd servers
    
    Mostly from MAINTAINER via PR pkg/30204

--- chat/gaim/buildlink3.mk
+++ chat/gaim/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2005/04/05 09:53:17 salo Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2005/05/12 10:32:18 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GAIM_BUILDLINK3_MK:=	${GAIM_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	gaim
 
 .if !empty(GAIM_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gaim+=	gaim>=1.1.2
-BUILDLINK_RECOMMENDED.gaim+=	gaim>=1.2.1
+BUILDLINK_RECOMMENDED.gaim+=	gaim>=1.3.0
 BUILDLINK_PKGSRCDIR.gaim?=	../../chat/gaim
 
 PRINT_PLIST_AWK+=	/^@dirrm lib\/gaim$$/ \
