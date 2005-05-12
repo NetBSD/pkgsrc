8afcf6dd31405bc263516a9cb10d903d049b68fb
salo <salo@pkgsrc.org>
2005-05-12 12:53:21 +0000

    Security fix:
    "A vulnerability in libTIFF was found, it can be potentially exploited by
     malicious people to compromise a vulnerable system."
    
    http://secunia.com/advisories/15320/
    http://bugzilla.remotesensing.org/show_bug.cgi?id=843
    
    Bump PKGREVISION, patch from libtiff cvs repository.

--- graphics/tiff/buildlink3.mk
+++ graphics/tiff/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2005/01/11 05:29:55 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2005/05/12 12:53:21 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 TIFF_BUILDLINK3_MK:=	${TIFF_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	tiff
 
 .if !empty(TIFF_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.tiff+=	tiff>=3.6.1
-BUILDLINK_RECOMMENDED.tiff+=	tiff>=3.7.1nb1
+BUILDLINK_RECOMMENDED.tiff+=	tiff>=3.7.2nb1
 BUILDLINK_PKGSRCDIR.tiff?=	../../graphics/tiff
 .endif	# TIFF_BUILDLINK3_MK
 
