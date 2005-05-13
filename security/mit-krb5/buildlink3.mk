f1eb092518442bb40528368feb1bbc9f0bcd04c7
salo <salo>
2005-05-13 10:08:52 +0000

    Pullup ticket 444 - requested by Johnny C. Lam security update for mit-krb5
    Revisions pulled up:
    - pkgsrc/security/mit-krb5/Makefile		1.17-1.18, 1.20
    - pkgsrc/security/mit-krb5/PLIST		1.6-1.8
    - pkgsrc/security/mit-krb5/buildlink3.mk	1.4
    - pkgsrc/security/mit-krb5/distinfo		1.9-1.10
    - pkgsrc/security/mit-krb5/files/kadmind.sh	1.2
    - pkgsrc/security/mit-krb5/files/kdc.sh		1.2
    - pkgsrc/security/mit-krb5/patches/patch-aa	1.2
    - pkgsrc/security/mit-krb5/patches/patch-ab	1.2
    - pkgsrc/security/mit-krb5/patches/patch-ac	1.2
    - pkgsrc/security/mit-krb5/patches/patch-ad	1.2
    - pkgsrc/security/mit-krb5/patches/patch-ae	1.2
    - pkgsrc/security/mit-krb5/patches/patch-af	1.3
    - pkgsrc/security/mit-krb5/patches/patch-ag	1.3
    - pkgsrc/security/mit-krb5/patches/patch-ai	removed
    - pkgsrc/security/mit-krb5/patches/patch-aj	1.2
    - pkgsrc/security/mit-krb5/patches/patch-ak	1.1
    - pkgsrc/security/mit-krb5/patches/patch-al	1.1
    
       Module Name:		pkgsrc
       Committed By:	jlam
       Date:		Sun Apr 10 07:15:25 UTC 2005
    
       Modified Files:
       	pkgsrc/security/mit-krb5: Makefile PLIST distinfo
       	pkgsrc/security/mit-krb5/files: kadmind.sh kdc.sh
       	pkgsrc/security/mit-krb5/patches: patch-aa patch-ab patch-ac patch-ad
       	    patch-ae patch-af patch-ag patch-aj
       Added Files:
       	pkgsrc/security/mit-krb5/patches: patch-ak
       Removed Files:
       	pkgsrc/security/mit-krb5/patches: patch-ai
    
       Log Message:
       Updated security/mit-krb5 to krb5-1.4.
       Changes from version 1.3.6 include:
    
       * Merged Athena telnetd changes for creating a new option for requiring
           encryption.
       * Add implementation of the RPCSEC_GSS authentication flavor to the RPC
           library.
       * The kadmind4 backwards-compatibility admin server and the v5passwdd
           backwards-compatibility password-changing server have been removed.
       * Thread safety for krb5 libraries.
       * Yarrow code now uses AES.
       * Merged Athena changes to allow ftpd to require encrypted passwords.
       * Incorporate gss_krb5_set_allowable_enctypes() and
           gss_krb5_export_lucid_sec_context(), which are needed for NFSv4.
       * Fix heap buffer overflow in password history mechanism.
           [MITKRB5-SA-2004-004]
    ---
       Module Name:		pkgsrc
       Committed By:	jlam
       Date:		Sun Apr 10 07:45:31 UTC 2005
    
       Modified Files:
       	pkgsrc/security/mit-krb5: PLIST
    
       Log Message:
       Remove the examples directory on deinstallation.
    ---
       Module Name:		pkgsrc
       Committed By:	jlam
       Date:		Sun Apr 10 07:46:51 UTC 2005
    
       Modified Files:
       	pkgsrc/security/mit-krb5: Makefile distinfo
       Added Files:
       	pkgsrc/security/mit-krb5/patches: patch-al
    
       Log Message:
       Patch from http://web.mit.edu/kerberos/advisories/2005-001-patch_1.4.txt
       which fixes MITKRB5-SA-2005-001 (CAN-2005-0468 & CAN-2005-0469) relating
       to buffer overflows in the telnet client.  Bump PKGREVISION to 1.
    ---
       Module Name:		pkgsrc
       Committed By:	jlam
       Date:		Mon Apr 11 22:44:54 UTC 2005
    
       Modified Files:
       	pkgsrc/security/mit-krb5: PLIST
    
       Log Message:
       The FTP daemon is always named "kftpd" regardless of whether prefix-cmds
       is a PKG_OPTION.
    ---
       Module Name:		pkgsrc
       Committed By:	jlam
       Date:		Thu Apr 14 23:07:55 UTC 2005
    
       Modified Files:
       	pkgsrc/security/mit-krb5: Makefile
    
       Log Message:
       Remove unused section... MIT krb5 apparently now detects NetBSD's utmpx
       implementation correctly on NetBSD>=2.0.
    ---
       Module Name:		pkgsrc
       Committed By:	salo
       Date:		Sat Apr 16 14:32:53 UTC 2005
    
       Modified Files:
       	pkgsrc/security/mit-krb5: buildlink3.mk
    
       Log Message:
       Bump BUILDLINK_RECOMMENDED for latest security fix. (hi jlam!)

--- security/mit-krb5/buildlink3.mk
+++ security/mit-krb5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/10/03 00:18:10 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3.4.1 2005/05/13 10:08:52 salo Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MIT_KRB5_BUILDLINK3_MK:=	${MIT_KRB5_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	mit-krb5
 
 .if !empty(MIT_KRB5_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.mit-krb5+=		mit-krb5>=1.3.1
-BUILDLINK_RECOMMENDED.mit-krb5+=	mit-krb5>=1.3.4nb3
+BUILDLINK_RECOMMENDED.mit-krb5+=	mit-krb5>=1.4nb1
 BUILDLINK_PKGSRCDIR.mit-krb5?=		../../security/mit-krb5
 .endif	# MIT_KRB5_BUILDLINK3_MK
 
