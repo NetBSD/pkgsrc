2f8a0ccbb33985efb894aea271b5ae9bbf6371fd
salo <salo@pkgsrc.org>
2005-05-13 11:57:59 +0000

    Security fix:
    "Matthias Clasen has reported a vulnerability in libexif, which can be
    exploited by malicious people to cause a DoS (Denial of Service).
    
    The vulnerability is caused due to an infinite recursion in the
    "exif_data_load_data_content()" function and can be exploited to
    cause a stack overflow when parsing a specially crafted image.
    
    Successful exploitation may crash an application linked against the
    vulnerable library."
    
    Bump PKGREVISION.  Patch from:
    http://sourceforge.net/tracker/index.php?func=detail&aid=1196787&group_id=12272&atid=112272

--- graphics/libexif/buildlink3.mk
+++ graphics/libexif/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2005/03/10 22:21:56 salo Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2005/05/13 11:57:59 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBEXIF_BUILDLINK3_MK:=	${LIBEXIF_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libexif
 
 .if !empty(LIBEXIF_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libexif+=	libexif>=0.6.11
-BUILDLINK_RECOMMENDED.libexif+=	libexif>=0.6.11nb1
+BUILDLINK_RECOMMENDED.libexif+=	libexif>=0.6.12nb1
 BUILDLINK_PKGSRCDIR.libexif?=	../../graphics/libexif
 .endif	# LIBEXIF_BUILDLINK3_MK
 
