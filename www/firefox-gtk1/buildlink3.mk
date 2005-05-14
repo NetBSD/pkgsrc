f481ec64c07090e09b97fd7f2696940121f7fe42
taya <taya@pkgsrc.org>
2005-05-14 15:27:10 +0000

    Update firefox & firefox-gtk1 to 1.0.4.
    This is a security fix release.
    Fixed vulnerabilities are follows:
    
    MFSA 2005-44  Privilege escalation via non-DOM property overrides
    MFSA 2005-43 "Wrapped" javascript: urls bypass security checks
    MFSA 2005-42 Code execution via javascript: IconURL

--- www/firefox-gtk1/buildlink3.mk
+++ www/firefox-gtk1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2005/03/25 08:23:46 salo Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2005/05/14 15:27:10 taya Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 FIREFOX_GTK2_BUILDLINK3_MK:=	${FIREFOX_GTK2_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	firefox-gtk1
 
 .if !empty(FIREFOX_GTK2_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.firefox-gtk1+=	firefox-gtk1>=1.0
-BUILDLINK_RECOMMENDED.firefox-gtk1+=	firefox-gtk1>=1.0.2
+BUILDLINK_RECOMMENDED.firefox-gtk1+=	firefox-gtk1>=1.0.4
 BUILDLINK_PKGSRCDIR.firefox-gtk1?=	../../www/firefox-gtk1
 .endif	# FIREFOX_GTK2_BUILDLINK3_MK
 
