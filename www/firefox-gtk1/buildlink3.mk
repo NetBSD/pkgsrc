28c33edee7c282d45ceed978c4db889858299af2
salo <salo>
2005-05-16 15:07:13 +0000

    Pullup ticket 504 - requested by Shin'ichiro TAYA security update for firefox
    Revisions pulled up:
    - pkgsrc/www/firefox/Makefile			1.15
    - pkgsrc/www/firefox/Makefile-firefox.common	1.14, 1.16-1.18
    - pkgsrc/www/firefox/PLIST			1.13-1.15
    - pkgsrc/www/firefox/buildlink3.mk		1.5
    - pkgsrc/www/firefox/distinfo			1.29
    - pkgsrc/www/firefox-gtk1/PLIST			1.3-1.4
    - pkgsrc/www/firefox-gtk1/buildlink3.mk		1.4
    
       Module Name:		pkgsrc
       Committed By:	taya
       Date:		Wed Apr 13 13:34:26 UTC 2005
    
       Modified Files:
       	pkgsrc/www/firefox: Makefile-firefox.common PLIST
       	pkgsrc/www/firefox-gtk1: PLIST
    
       Log Message:
       change extensions list as same as Linux official build.
       bump PKGREVISION.
       fix PR pkg/29595
    ---
       Module Name:		pkgsrc
       Committed By:	wiz
       Date:		Fri Apr 15 12:42:27 UTC 2005
    
       Modified Files:
       	pkgsrc/www/firefox: PLIST
    
       Log Message:
       Add two @exec ${MKDIR} lines for empty directories which have @dirrm
       lines, to fix binary packages.
    ---
       Module Name:		pkgsrc
       Committed By:	wiz
       Date:		Fri Apr 15 12:44:30 UTC 2005
    
       Modified Files:
       	pkgsrc/www/firefox-gtk1: PLIST
    
       Log Message:
       Add an @exec ${MKDIR} line for an empty directory which has a @dirrm
       line, to fix binary packages.
    ---
       Module Name:		pkgsrc
       Committed By:	taya
       Date:		Sun Apr 24 14:00:12 UTC 2005
    
       Modified Files:
       	pkgsrc/www/firefox: Makefile-firefox.common
    
       Log Message:
       concatinate extensions with separator ',' and set to MOZILLA_EXTENSIONS,
       instead of using ':ts' modifier.
       becase make of NetBSD-1.6.x doesn't have it.
       suggested by Jeremy C. Reed.
    ---
       Module Name:		pkgsrc
       Committed By:	reed
       Date:		Mon Apr 25 19:26:10 UTC 2005
    
       Modified Files:
       	pkgsrc/www/firefox: Makefile-firefox.common
    
       Log Message:
       Make sure build is without gssapi support. (Okayed by maintainer,
       taya ... well really he said "I don't object your idea.")
    
       This fixes a build bug when heimdal is detected but not buildlinked.
       It is a known mozilla bug:
        https://bugzilla.mozilla.org/show_bug.cgi?id=245467
    
       I didn't put this in the mozilla/Makefile.common, because didn't test
       that yet.
    
       This issue probably only happens when using /usr as the LOCALBASE,
       which is not really supported and maybe I am the only one to hit this
       with pkgsrc.
    
       Maybe later someone can consider adding a build option for GSSAPI,
       but I don't know anything about it in regards to a web browser myself.
    ---
       Module Name:		pkgsrc
       Committed By:	taya
       Date:		Sat May 14 15:27:10 UTC 2005
    
       Modified Files:
       	pkgsrc/www/firefox: Makefile Makefile-firefox.common PLIST
       	    buildlink3.mk distinfo
       	pkgsrc/www/firefox-gtk1: buildlink3.mk
    
       Log Message:
       Update firefox & firefox-gtk1 to 1.0.4.
    
       This is a security fix release.
       Fixed vulnerabilities are follows:
    
       MFSA 2005-44  Privilege escalation via non-DOM property overrides
       MFSA 2005-43 "Wrapped" javascript: urls bypass security checks
       MFSA 2005-42 Code execution via javascript: IconURL

--- www/firefox-gtk1/buildlink3.mk
+++ www/firefox-gtk1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1.2.1 2005/03/25 08:29:00 salo Exp $
+# $NetBSD: buildlink3.mk,v 1.1.1.1.2.2 2005/05/16 15:07:13 salo Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 FIREFOX_GTK2_BUILDLINK3_MK:=	${FIREFOX_GTK2_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	firefox-gtk1
 
 .if !empty(FIREFOX_GTK2_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.firefox-gtk1+=	firefox-gtk1>=1.0
-BUILDLINK_RECOMMENDED.firefox-gtk1+=	firefox-gtk1>=1.0.2
+BUILDLINK_RECOMMENDED.firefox-gtk1+=	firefox-gtk1>=1.0.4
 BUILDLINK_PKGSRCDIR.firefox-gtk1?=	../../www/firefox-gtk1
 .endif	# FIREFOX_GTK2_BUILDLINK3_MK
 
