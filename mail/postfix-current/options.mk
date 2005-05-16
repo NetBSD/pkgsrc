cf8005a4510e788439df07455522ae6a130c3a2b
jlam <jlam@pkgsrc.org>
2005-05-16 01:32:22 +0000

    Note that tar is required by this package.

--- mail/postfix-current/options.mk
+++ mail/postfix-current/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.8 2005/03/28 09:39:57 jlam Exp $
+# $NetBSD: options.mk,v 1.9 2005/05/16 01:32:23 jlam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.postfix
 #PKG_SUPPORTED_OPTIONS=	inet6 ldap mysql mysql4 pcre pgsql sasl tls
@@ -30,6 +30,7 @@ PKG_SUPPORTED_OPTIONS=	ldap mysql mysql4 pcre pgsql sasl
 # SITES_${TLS_PATCH}=	http://mirrors.loonybin.net/postfix_tls/	\
 # 			ftp://mirrors.loonybin.net/pub/postfix_tls/	\
 # 			ftp://ftp.aet.tu-cottbus.de/pub/postfix_tls/
+# PKGSRC_USE_TOOLS+=	tar
 # PATCH_DIST_CAT.${TLS_PATCH}=	${TAR} -zxOf ${TLS_PATCH} "*/pfixtls.diff"
 # PATCH_DIST_STRIP.${TLS_PATCH}=	-p1
 # .  endif
