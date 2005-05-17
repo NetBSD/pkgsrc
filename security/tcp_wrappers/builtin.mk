e64d57926bcda04d752b6ae238fcb112c8e68ccc
jschauma <jschauma@pkgsrc.org>
2005-05-17 18:04:49 +0000

    When checking for the library, accept either libwrap.so or libwrap.a since apparently some systems only have .a (pointed out by adrianp@).

--- security/tcp_wrappers/builtin.mk
+++ security/tcp_wrappers/builtin.mk
@@ -1,8 +1,9 @@
-# $NetBSD: builtin.mk,v 1.3 2005/05/10 02:41:15 jschauma Exp $
+# $NetBSD: builtin.mk,v 1.4 2005/05/17 18:04:49 jschauma Exp $
 
 .if !defined(IS_BUILTIN.tcp_wrappers)
 IS_BUILTIN.tcp_wrappers=	no
-.  if exists(/usr/include/tcpd.h) && exists(/usr/lib${ABI}/libwrap.so)
+.  if exists(/usr/include/tcpd.h) && (exists(/usr/lib${ABI}/libwrap.so) ||
+					exists(/usr/lib${ABI}/libwrap.a))
 IS_BUILTIN.tcp_wrappers=	yes
 .  endif
 .endif	# IS_BUILTIN.tcp_wrappers
