d1f46bcdeecb108bfa7682865e1b96e838a61d40
gavan <gavan@pkgsrc.org>
2005-05-17 21:53:20 +0000

    Add missing line continuation

--- security/tcp_wrappers/builtin.mk
+++ security/tcp_wrappers/builtin.mk
@@ -1,8 +1,8 @@
-# $NetBSD: builtin.mk,v 1.4 2005/05/17 18:04:49 jschauma Exp $
+# $NetBSD: builtin.mk,v 1.5 2005/05/17 21:53:20 gavan Exp $
 
 .if !defined(IS_BUILTIN.tcp_wrappers)
 IS_BUILTIN.tcp_wrappers=	no
-.  if exists(/usr/include/tcpd.h) && (exists(/usr/lib${ABI}/libwrap.so) ||
+.  if exists(/usr/include/tcpd.h) && (exists(/usr/lib${ABI}/libwrap.so) || \
 					exists(/usr/lib${ABI}/libwrap.a))
 IS_BUILTIN.tcp_wrappers=	yes
 .  endif
