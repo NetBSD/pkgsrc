912880b1683804976eda1e4e93dbf21a663a246d
reed <reed@pkgsrc.org>
2005-05-18 22:35:04 +0000

    Remove Linux work-around for bug noted in PR #25402 from arts and koffice build.
    And do in meta-pkgs/kde3/kde3.mk as suggested by maintainer, markd.
    
    It helps with builds on Linux for kdegraphics3, libkipi, gwenview,
    kdelibs3, kdebase3, arts, and many other KDE-based packages on Linux.
    (It was not needed for kile, but will be used.)
    
    Thank you, Torsten Harenberg for testing many packages such as
    kdenetwork, kdegames, kdepim, kdemultimedia.
    
    I still don't know what the correct fix is for PR #25402.

--- meta-pkgs/kde3/kde3.mk
+++ meta-pkgs/kde3/kde3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: kde3.mk,v 1.7 2005/04/27 02:09:05 markd Exp $
+# $NetBSD: kde3.mk,v 1.8 2005/05/18 22:35:04 reed Exp $
 #
 # This Makefile fragment is included by packages that use the KDE3
 # configure-and-build process.
@@ -41,6 +41,11 @@ USE_X11=		YES
 USE_PKGLOCALEDIR=	YES
 
 .include "../../mk/bsd.prefs.mk"
+
+.if ${OPSYS} == "Linux"
+# work-around bug noted in PR #25402
+BUILDLINK_TRANSFORM+=	rm:-Wl,--no-undefined
+.endif
 
 .if ${OPSYS} == "SunOS"
 GCC_REQD+=		3.3
