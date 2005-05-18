bf8476b9c2c787dd550a51e1d144aa7aae396c38
adrianp <adrianp@pkgsrc.org>
2005-05-18 19:10:01 +0000

    - Add an option ${APACHE_SUEXEC_LOGFILE} so the user can specify   where they would like the suexec logfile to go.  Ok'ed tron@ - Bump pkgrevision

--- www/apache2/options.mk
+++ www/apache2/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2004/12/22 21:46:25 jlam Exp $
+# $NetBSD: options.mk,v 1.3 2005/05/18 19:10:01 adrianp Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.apache
 PKG_SUPPORTED_OPTIONS=	suexec
@@ -8,12 +8,13 @@ PKG_SUPPORTED_OPTIONS=	suexec
 .if !empty(PKG_OPTIONS:Msuexec)
 APACHE_SUEXEC_DOCROOT?=	${PREFIX}/share/httpd/htdocs
 APACHE_SUEXEC_PATH=	/bin:/usr/bin:${PREFIX}/bin:/usr/local/bin
+APACHE_SUEXEC_LOGFILE?=	${VARBASE}/log/httpd/suexec.log
 APACHE_SUEXEC_CONFIGURE_ARGS+=						\
 	--with-suexec-bin=${PREFIX}/sbin/suexec				\
 	--with-suexec-caller=${APACHE_USER}				\
 	--with-suexec-safepath='${APACHE_SUEXEC_PATH}'			\
 	--with-suexec-docroot=${APACHE_SUEXEC_DOCROOT}			\
-	--with-suexec-logfile=${VARBASE}/log/httpd/suexec.log
+	--with-suexec-logfile=${APACHE_SUEXEC_LOGFILE}
 
 APACHE_MODULES+=        suexec
 CONFIGURE_ARGS+=        ${APACHE_SUEXEC_CONFIGURE_ARGS:M--with-suexec-*}
