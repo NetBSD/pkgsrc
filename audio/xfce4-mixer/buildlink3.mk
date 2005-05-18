40788eb411d92e23ddef30e330bc2f93fbf5773b
martti <martti>
2005-05-18 06:10:30 +0000

    Updated xfce4 to 4.2.2
    General:
    
    * new and updated translations: bn_IN, ca, cs, el, el_GR, eo, eu, fi, hu, ja,
      ko, mr, pl, ro, vi,
    * new french documentation.
    
    gtk-xfce-engine:
    
    * fix various colors issues with ExoIconBar, prelight colors for treeview,
      Adobe® Acrobattm 7, progress bar, combo box, gtk scales (#848).
    
    libxfcegui4:
    
    * check ~/.local/share/pixmaps/ as a fallback (#731).
    
    xfcalendar:
    
    * fix compiling with --enable-debug=full (#843),
    * remove C99 variable declaration and C++ comments.
    
    xfce4-mixer:
    
    * fix a bug with ALSA.
    
    xfce4-session:
    
    * fix a crash with the MCS plugin.
    
    xfce-utils:
    
    * taskbar: Add a command option "--monitor=x" to allow specifying which
      monitor to use in Xinerama mode.
    
    xfdesktop:
    
    * fix translation problems with menu panel plugin,
    * fix bug where binding 'xfdesktop -menu' only works sporadically (#441),
    * fix unresolved symbol problem with menu module.
    
    xffm:
    
    * fix property dialog crahsing on FreeBSD (#945),
    * fix library version info not set on the properties module (#882, #249),
    * fix fstab parsing (#524),
    * fix compilation issues with ansi compiler and gcc4 (#862).
    
    xfprint:
    
    * make bsdlpr plugin compile,
    * change CUPS detection code,
    * set correct ld flags (#871),
    * add support for printcap aliases (#821),
    * fix MCS plugin.
    
    xfwm4:
    
    * fix mouse buttons being left ungrabbed when focus was transfered to
      an unmanaged window,
    * document hidden option "workspace_scroll" (#948),
    * various focus issues (#759, #869, #899, #905, #924, #931),
    * fix pixmap depth not being properly set (#895),
    * refresh frame when "skip taskbar" state changes (#898),
    * allow seemless windows to be hidden from kbd shortcuts,
    * code cleanup.

--- audio/xfce4-mixer/buildlink3.mk
+++ audio/xfce4-mixer/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2005/03/30 07:04:05 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2005/05/18 06:10:30 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XFCE4_MIXER_BUILDLINK3_MK:=	${XFCE4_MIXER_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-mixer}
 BUILDLINK_PACKAGES+=	xfce4-mixer
 
 .if !empty(XFCE4_MIXER_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.xfce4-mixer+=	xfce4-mixer>=4.2.1
+BUILDLINK_DEPENDS.xfce4-mixer+=	xfce4-mixer>=4.2.2
 BUILDLINK_PKGSRCDIR.xfce4-mixer?=	../../audio/xfce4-mixer
 .endif	# XFCE4_MIXER_BUILDLINK3_MK
 
--- print/xfce4-print/buildlink3.mk
+++ print/xfce4-print/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2005/03/18 07:59:09 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2005/05/18 06:10:30 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XFCE4_PRINT_BUILDLINK3_MK:=	${XFCE4_PRINT_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-print}
 BUILDLINK_PACKAGES+=	xfce4-print
 
 .if !empty(XFCE4_PRINT_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.xfce4-print+=	xfce4-print>=4.2.1
+BUILDLINK_DEPENDS.xfce4-print+=	xfce4-print>=4.2.2
 BUILDLINK_PKGSRCDIR.xfce4-print?=	../../print/xfce4-print
 .endif	# XFCE4_PRINT_BUILDLINK3_MK
 
