9a9824e63d1bd2972bab2b770e4bf8ae7efeff32
jlam <jlam>
2005-05-19 15:18:13 +0000

    PERL5_PKGSRCDIR is no more.  Explicitly use "lang/perl58" for the location of the perl package to install for bulk builds.

--- mk/bulk/bsd.bulk-pkg.mk
+++ mk/bulk/bsd.bulk-pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.bulk-pkg.mk,v 1.80 2005/05/06 00:52:30 rillig Exp $
+#	$NetBSD: bsd.bulk-pkg.mk,v 1.81 2005/05/19 15:18:13 jlam Exp $
 
 #
 # Copyright (c) 1999, 2000 Hubert Feyrer <hubertf@NetBSD.org>
@@ -179,7 +179,7 @@ bulk-cache:
 .else
 	@${ECHO_MSG} "BULK> Extracting database for SPECIFIC_PKGS subset of pkgsrc"
 	@${ECHO_MSG} "      along with their dependencies"
-.for __tmp__ in ${SUBDIR} ${BULK_PREREQ} ${PERL5_PKGSRCDIR:C|.*/([^/]*/[^/]*)$|\1|} pkgtools/pkglint
+.for __tmp__ in ${SUBDIR} ${BULK_PREREQ} lang/perl58 pkgtools/pkglint
 	cd ${PKGSRCDIR}/${__tmp__} && ${SETENV} BMAKE=${MAKE} AWK=${AWK} EXPR=${EXPR} ${SH} ../../mk/scripts/mkdatabase -a -f ${BULK_DBFILE}
 .endfor
 	@${ECHO_MSG} "BULK> Extracting dependency tree file"
