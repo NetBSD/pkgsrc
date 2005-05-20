0fd4d2b4f566fc38dd3107f25db43ad29f55567c
rillig <rillig>
2005-05-20 22:40:36 +0000

    Made sure the :sh operator does not result in an empty string. Rewrote the variable substitution for the find(1) command to be more readable.

--- mk/wrapper/bsd.wrapper.mk
+++ mk/wrapper/bsd.wrapper.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.wrapper.mk,v 1.31 2005/05/20 21:36:05 jlam Exp $
+# $NetBSD: bsd.wrapper.mk,v 1.32 2005/05/20 22:40:36 rillig Exp $
 #
 # Copyright (c) 2004 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -696,10 +696,14 @@ _UNWRAP_PATTERNS+=	*-config
 _UNWRAP_PATTERNS+=	*Conf.sh
 _UNWRAP_PATTERNS+=	*.pc
 _UNWRAP_PATTERNS_FIND_cmd=	\
-	cd ${WRKSRC} && ${FIND} . \( ${_UNWRAP_PATTERNS:S/$/!/:S/^/-o -name !/:S/!/"/g:S/-o//1} \) -print | ${SED} -e 's|^\./||' | ${SORT} -u
+	cd ${WRKSRC} && \
+	${ECHO} "__dummy-entry__" && \
+	${FIND} . \( ${_UNWRAP_PATTERNS:C/.*/-o -name "&"/g:S/-o//1} \) -print \
+	| ${SED} -e 's|^\./||' \
+	| ${SORT} -u
 UNWRAP_FILES?=		# empty
 _UNWRAP_FILES=		${UNWRAP_FILES}					\
-			${_UNWRAP_PATTERNS_FIND_cmd:sh}
+			${_UNWRAP_PATTERNS_FIND_cmd:sh:N__dummy-entry__}
 _UNWRAP_SED?=		# empty
 
 SUBST_CLASSES+=		unwrap
