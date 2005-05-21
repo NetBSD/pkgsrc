ebe0a3fa827c403ca49e51c6b479fabfb1d77254
jlam <jlam>
2005-05-21 04:53:17 +0000

    Grammar 101 -- sentences should end in a period.

--- mk/tools/perl.mk
+++ mk/tools/perl.mk
@@ -1,10 +1,10 @@
-# $NetBSD: perl.mk,v 1.15 2005/05/21 04:46:52 jlam Exp $
+# $NetBSD: perl.mk,v 1.16 2005/05/21 04:53:17 jlam Exp $
 #
 # Copyright (c) 2005 The NetBSD Foundation, Inc.
 # All rights reserved.
 #
 # This code is derived from software contributed to The NetBSD Foundation
-# by Johnny C. Lam
+# by Johnny C. Lam.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
