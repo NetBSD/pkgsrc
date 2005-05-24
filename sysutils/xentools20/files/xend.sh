db8de4458a9afd7d23e18de11b035f7e381a0109
xtraeme <xtraeme@pkgsrc.org>
2005-05-24 16:02:08 +0000

    Update to 2.0.6 (latest stable distribution).
    Lots of changes, notably now it uses Linux-2.6.11.x, a NetBSD
    bug was fixed.
    
    pkgsrc changes:
    
    * Honor PKG_SYSCONFDIR and SUPPORT_FILES for configuration files.
    * xend.sh: no more python warning messages when the script is used
      on non NetBSD/Xen systems.

--- sysutils/xentools20/files/xend.sh
+++ sysutils/xentools20/files/xend.sh
@@ -1,12 +1,11 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: xend.sh,v 1.2 2005/04/01 19:06:12 bouyer Exp $
+# $NetBSD: xend.sh,v 1.3 2005/05/24 16:02:08 xtraeme Exp $
 #
 # PROVIDE: xend
 # REQUIRE: disks network
 
-if [ -f /etc/rc.subr ]
-then
+if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
@@ -14,7 +13,16 @@ name="xend"
 rcvar=$name
 command="@PREFIX@/sbin/xend"
 command_interpreter="@RCD_INTERPRETER@"
-start_cmd="echo starting ${name}. && ${command} start"
+start_cmd="@ECHO@ Starting ${name}. && ${command} start"
+start_precmd="test_kern_privcmd"
+
+test_kern_privcmd()
+{
+	if [ ! -f /kern/xen/privcmd ]; then
+		@ECHO@ "${name}: Cannot find /kern/xen/privcmd!"
+		exit 1
+	fi
+}
 
 load_rc_config $name
 run_rc_command "$1"
