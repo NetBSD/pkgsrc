54b962f2b105ba8f58a845f1b6bf735feeb26069
xtraeme <xtraeme>
2005-05-24 15:30:39 +0000

    Cannot use mysql and pgsql options together, use one of them.

--- mail/gld/options.mk
+++ mail/gld/options.mk
@@ -1,10 +1,14 @@
-# $NetBSD: options.mk,v 1.1 2005/04/14 22:22:56 xtraeme Exp $
+# $NetBSD: options.mk,v 1.2 2005/05/24 15:30:39 xtraeme Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gld
 PKG_SUPPORTED_OPTIONS=	mysql pgsql
 PKG_DEFAULT_OPTIONS+=	mysql
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mmysql) && !empty(PKG_OPTIONS:Mpgsql)
+PKG_FAIL_REASON+=	"Choose one option: mysql or pgsql."
+.endif
 
 .if !empty(PKG_OPTIONS:Mmysql)
 .  include "../../mk/mysql.buildlink3.mk"
