04a3acbd97e8d66bb34d9c5a421afc3994a9a14a
dmcmahill <dmcmahill@pkgsrc.org>
2005-05-28 02:50:45 +0000

    - pull in the changes from usr.sbin which prevent an over flow   of a 32 bit signed number when pkg_add-ing a binary package   file larger than 512 Mb.
    - Bump PKGTOOLS_VERSION (NOT!!! PKGTOOLS_REQD) to 20050527

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.42 2005/03/20 15:55:54 hubertf Exp $	*/
+/*	$NetBSD: version.h,v 1.43 2005/05/28 02:50:46 dmcmahill Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20050318"
+#define PKGTOOLS_VERSION "20050527"
 
 #endif /* _INST_LIB_VERSION_H_ */
