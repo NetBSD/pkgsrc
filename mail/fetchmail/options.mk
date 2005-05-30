4e72ffeaa0dc796674c558cfd8fff7586407a11d
frueauf <frueauf@pkgsrc.org>
2005-05-30 18:06:37 +0000

    Correct handling if gssapi and kerberos is defined. Proposal by Dieter Baron, tested by Jukka Salmi.

--- mail/fetchmail/options.mk
+++ mail/fetchmail/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.10 2005/05/30 14:02:17 frueauf Exp $
+# $NetBSD: options.mk,v 1.11 2005/05/30 18:06:37 frueauf Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fetchmail
 PKG_SUPPORTED_OPTIONS=	inet6 kerberos4 kerberos gssapi ssl
@@ -17,8 +17,7 @@ CONFIGURE_ARGS+=	--enable-inet6
 ###
 ### Authentication via GSSAPI (currently only over Kerberos V) support.
 ###
-.if !empty(PKG_OPTIONS:Mgssapi) && empty(PKG_OPTIONS:Mkerberos)
-PKG_OPTIONS+=		kerberos
+.if !empty(PKG_OPTIONS:Mgssapi)
 CONFIGURE_ARGS+=	--with-gssapi=${KRB5BASE}
 .else
 CONFIGURE_ARGS+=	--with-gssapi=no
@@ -40,7 +39,7 @@ CONFIGURE_ARGS+=	--with-kerberos=no
 ###
 ### Kerberos V support.
 ###
-.if !empty(PKG_OPTIONS:Mkerberos)
+.if !empty(PKG_OPTIONS:Mkerberos) || !empty(PKG_OPTIONS:Mgssapi)
 .  include "../../mk/krb5.buildlink3.mk"
 PKG_USE_KERBEROS=	# defined
 CONFIGURE_ARGS+=        --with-kerberos5=${KRB5BASE}
