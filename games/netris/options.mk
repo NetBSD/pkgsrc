6f4f62420eb6a53e5090d2867a562546aed756d3
wiz <wiz@pkgsrc.org>
2005-05-31 22:02:58 +0000

    Whitespace consistency.

--- games/netris/options.mk
+++ games/netris/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2005/05/31 21:46:06 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2005/05/31 22:02:58 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.netris
 PKG_SUPPORTED_OPTIONS=	inet6
@@ -6,7 +6,7 @@ PKG_SUPPORTED_OPTIONS=	inet6
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGS+=        --enable-ipv6
+CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
 .endif
