0cd6298be55ec67212eafda5d2e33f5e2bf94f44
wiz <wiz@pkgsrc.org>
2005-05-31 22:04:06 +0000

    Fix conversion.

--- games/xtris/options.mk
+++ games/xtris/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2005/05/31 21:47:34 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2005/05/31 22:04:06 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xtris
 PKG_SUPPORTED_OPTIONS=	inet6
@@ -6,7 +6,5 @@ PKG_SUPPORTED_OPTIONS=	inet6
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGS+=        --enable-ipv6
-.else
-CONFIGURE_ARGS+=	--disable-ipv6
+CFLAGS+=	-DINET6
 .endif
