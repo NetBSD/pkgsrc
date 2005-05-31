a9699800a126ab66b7b795ab28a960c7d5f7068f
wiz <wiz@pkgsrc.org>
2005-05-31 22:12:18 +0000

    Use x11/control-center (instead of x11/controlcenter).

--- www/gtkhtml/buildlink3.mk
+++ www/gtkhtml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/11/30 18:26:40 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2005/05/31 22:12:18 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GTKHTML_BUILDLINK3_MK:=	${GTKHTML_BUILDLINK3_MK}+
@@ -20,6 +20,6 @@ BUILDLINK_PKGSRCDIR.gtkhtml?=	../../www/gtkhtml
 .include "../../print/gnome-print/buildlink3.mk"
 .include "../../www/glibwww/buildlink3.mk"
 .include "../../www/libghttp/buildlink3.mk"
-.include "../../x11/controlcenter/buildlink3.mk"
+.include "../../x11/control-center/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
