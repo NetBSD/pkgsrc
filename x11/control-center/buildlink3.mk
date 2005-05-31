a90a1270979a502ef6f687dc8646c4d760e16593
wiz <wiz@pkgsrc.org>
2005-05-31 22:09:35 +0000

    Re-import control-center under its proper PKGNAME.

---
+++ x11/control-center/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/05/31 22:09:35 wiz Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+CONTROL_CENTER_BUILDLINK3_MK:=	${CONTROL_CENTER_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	control-center
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ncontrol-center}
+BUILDLINK_PACKAGES+=	control-center
+
+.if !empty(CONTROL_CENTER_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.control-center+=	control-center>=1.4.0.4nb9
+BUILDLINK_RECOMMENDED.control-center+=	control-center>=1.4.0.4nb10
+BUILDLINK_PKGSRCDIR.control-center?=	../../x11/control-center
+.endif	# CONTROL_CENTER_BUILDLINK3_MK
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/popt/buildlink3.mk"
+.include "../../graphics/gdk-pixbuf/buildlink3.mk"
+.include "../../sysutils/gnome-vfs/buildlink3.mk"
+.include "../../x11/gnome-libs/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
