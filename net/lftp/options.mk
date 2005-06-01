a71eb7fa9b9df8b171c4a75452e502eb889a506c
dillo <dillo@pkgsrc.org>
2005-06-01 14:05:03 +0000

    set PKG_SUGGESTED_OPTIONS, not PKG_DEFAULT_OPTIONS

--- net/lftp/options.mk
+++ net/lftp/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.1 2005/05/30 16:01:40 salo Exp $
+# $NetBSD: options.mk,v 1.2 2005/06/01 14:05:03 dillo Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.lftp
 PKG_SUPPORTED_OPTIONS=	gnutls ssl
-PKG_DEFAULT_OPTIONS=	gnutls
+PKG_SUPPORTED_OPTIONS=	gnutls
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgnutls) && !empty(PKG_OPTIONS:Mssl)
