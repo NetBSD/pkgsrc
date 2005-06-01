7debfabe11a6c449f363e04cb58b8174e722e4b5
jlam <jlam@pkgsrc.org>
2005-06-01 18:33:02 +0000

    Use IMAKE_MAKE instead of MAKE to process the makefile generated by imake.

--- x11/Xcomposite/builtin.mk
+++ x11/Xcomposite/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.5 2005/06/01 18:03:27 jlam Exp $
+# $NetBSD: builtin.mk,v 1.6 2005/06/01 18:33:02 jlam Exp $
 
 BUILTIN_PKG:=	Xcomposite
 
@@ -22,7 +22,7 @@ IS_BUILTIN.Xcomposite!=							\
 	${IMAKE} -DUseInstalled -I${X11BASE}/lib/X11/config		\
 		-f ${BUILDLINK_PKGSRCDIR.Xcomposite}/builtin-imake.mk	\
 		-s - |							\
-	${MAKE} -f - builtin-test
+	${IMAKE_MAKE} -f - builtin-test
 .    endif
 .  endif
 .endif
--- x11/xcursor/builtin.mk
+++ x11/xcursor/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.5 2005/06/01 18:03:32 jlam Exp $
+# $NetBSD: builtin.mk,v 1.6 2005/06/01 18:33:02 jlam Exp $
 
 BUILTIN_PKG:=	xcursor
 
@@ -21,7 +21,7 @@ IS_BUILTIN.xcursor!=							\
 	${IMAKE} -DUseInstalled -I${X11BASE}/lib/X11/config		\
 		-f ${BUILDLINK_PKGSRCDIR.xcursor}/builtin-imake.mk	\
 		-s - |							\
-	${MAKE} -f - builtin-test
+	${IMAKE_MAKE} -f - builtin-test
 .    endif
 .  endif
 .endif
