31c4c3d4417e8fcce06498c2d2f2784c094539ea
markd <markd>
2005-06-03 22:58:54 +0000

    Now uses some semaphore functions so depend on pthread-sem on old NetBSD systems to provide them.

--- multimedia/kdemultimedia3/buildlink3.mk
+++ multimedia/kdemultimedia3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2005/03/23 21:59:47 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2005/06/03 22:58:54 markd Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 KDEMULTIMEDIA_BUILDLINK3_MK:=	${KDEMULTIMEDIA_BUILDLINK3_MK}+
@@ -21,6 +21,9 @@ BUILDLINK_PKGSRCDIR.kdemultimedia?=	../../multimedia/kdemultimedia3
 .include "../../audio/musicbrainz/buildlink3.mk"
 .include "../../audio/tunepimp/buildlink3.mk"
 .include "../../audio/taglib/buildlink3.mk"
+.if defined(PTHREAD_TYPE) && ${PTHREAD_TYPE} == "pth"
+.include "../../devel/pthread-sem/buildlink3.mk"
+.endif
 .include "../../multimedia/xine-lib/buildlink3.mk"
 .include "../../x11/kdebase3/buildlink3.mk"
 .include "../../x11/kdelibs3/buildlink3.mk"
