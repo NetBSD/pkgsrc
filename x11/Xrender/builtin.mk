3d7747eacc5527d9a5d859ed22057439102e5849
jlam <jlam@pkgsrc.org>
2005-06-03 19:12:49 +0000

    Coalesce the common makefile code in the builtin.mk file that inspect the X11 distribution using imake into mk/buildlink3/imake-check.mk. imake-check.mk calls out to a helper shell script mk/buildlink3/imake-check that generates the required Imakefiles and runs imake.  Remove the now extraneous builtin-imake.mk files as the builtin.mk files can now contain the name of the imake symbol to check.

--- x11/Xrender/builtin.mk
+++ x11/Xrender/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.10 2005/06/03 17:02:36 jlam Exp $
+# $NetBSD: builtin.mk,v 1.11 2005/06/03 19:12:50 jlam Exp $
 
 BUILTIN_PKG:=	Xrender
 
@@ -14,18 +14,9 @@ BUILTIN_FIND_FILES.H_XRENDER=	${X11BASE}/include/X11/extensions/Xrender.h
 .if !defined(IS_BUILTIN.Xrender)
 IS_BUILTIN.Xrender=	no
 .  if exists(${H_XRENDER})
-PKGSRC_USE_TOOLS+=	imake			# XXX
-IMAKE?=			${X11BASE}/bin/imake	# XXX
-_BUILTIN_IMAKE_CMD=	${IMAKE:C/^/_asdf_/1:M_asdf_*:S/^_asdf_//}
-.    if exists(${_BUILTIN_IMAKE_CMD})
-IS_BUILTIN.Xrender!=							\
-	dir=`cd ${BUILDLINK_PKGSRCDIR.Xrender} && ${PWD_CMD}`;		\
-	cd ${TMPDIR:U/tmp:Q} && 					\
-	${IMAKE} -DUseInstalled -I${X11BASE}/lib/X11/config		\
-		-f $$dir/builtin-imake.mk -C builtin-imake.$$$$.c	\
-		-s - |							\
-	${IMAKE_MAKE} -f - builtin-test
-.    endif
+BUILTIN_IMAKE_CHECK:=	Xrender:BuildRenderLibrary
+.    include "../../mk/buildlink3/imake-check.mk"
+IS_BUILTIN.Xrender=	${BUILTIN_IMAKE_CHECK.Xrender}
 .  endif
 .endif
 MAKEVARS+=	IS_BUILTIN.Xrender
