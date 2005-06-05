6b173e992e801c30a74a1044a38a2f220a494113
tv <tv@pkgsrc.org>
2005-06-05 18:05:54 +0000

    Force libtool-base-1.5.18nb3 on DragonFly.

--- mk/platform/DragonFly.mk
+++ mk/platform/DragonFly.mk
@@ -1,4 +1,4 @@
-# $NetBSD: DragonFly.mk,v 1.16 2005/06/01 18:14:23 jlam Exp $
+# $NetBSD: DragonFly.mk,v 1.17 2005/06/05 18:05:54 tv Exp $
 #
 # Variable definitions for the DragonFly operating system.
 
@@ -52,6 +52,7 @@ _OPSYS_HAS_INET6=	no	# IPv6 is not standard
 _OPSYS_HAS_JAVA=	no	# Java is not standard
 _OPSYS_HAS_MANZ=	yes	# MANZ controls gzipping of man pages
 _OPSYS_HAS_OSSAUDIO=	yes	# libossaudio is available
+_OPSYS_LIBTOOL_REQD=	1.5.18nb3 # for -pthread fix
 _OPSYS_PERL_REQD=		# no base version of perl required
 _OPSYS_PTHREAD_AUTO=	no	# -lpthread needed for pthreads
 _OPSYS_SHLIB_TYPE=	ELF/a.out	# shared lib type
