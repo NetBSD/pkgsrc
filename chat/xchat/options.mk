ee471723c74fdf0935e3a3a309a45dd8e36c4b0e
tron <tron@pkgsrc.org>
2005-06-06 11:35:39 +0000

    Replace the outdated "xchat" package (version 1.8.11) with the "xchat2" package (version 2.4.3).

--- chat/xchat/options.mk
+++ chat/xchat/options.mk
@@ -1,21 +1,32 @@
-# $NetBSD: options.mk,v 1.1 2005/06/02 21:48:42 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2005/06/06 11:35:39 tron Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.xchat
-PKG_SUPPORTED_OPTIONS=	inet6 socks5 ssl
+PKG_OPTIONS_VAR=	PKG_OPTIONS.xchat2
+PKG_SUPPORTED_OPTIONS=	inet6 ssl socks5
 PKG_SUGGESTED_OPTIONS=	ssl
-PKG_OPTIONS_LEGACY_VARS+=	XCHAT_USE_SSL:ssl
+PKG_OPTIONS_LEGACY_OPTS+=	socks:socks5
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGS+=	--enable-ipv6
-.endif
-
+###
+### Use OpenSSL libraries for connecting to ircs servers
+###
 .if !empty(PKG_OPTIONS:Mssl)
-.  include "../../security/openssl/buildlink3.mk"
+.	include "../../security/openssl/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-openssl=${SSLBASE}
+.else
+CONFIGURE_ARGS+=	--enable-openssl=no
 .endif
 
+###
+### Support for connecting thru SOCKS servers
+###
 .if !empty(PKG_OPTIONS:Msocks5)
 CONFIGURE_ARGS+=	--enable-socks
+.endif
+
+###
+### IPv6 support.
+###
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--enable-ipv6
 .endif
