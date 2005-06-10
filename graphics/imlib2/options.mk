e4be7e4aa1f62958dcc5c00c35aeb5526455d116
wiz <wiz@pkgsrc.org>
2005-06-10 18:14:23 +0000

    Wrap complete options part in ifdef i386, since there is only one option and that only on i386. Fixes bulk build problem.

--- graphics/imlib2/options.mk
+++ graphics/imlib2/options.mk
@@ -1,15 +1,14 @@
-# $NetBSD: options.mk,v 1.1 2005/05/31 21:36:36 wiz Exp $
-
-PKG_OPTIONS_VAR=	PKG_OPTIONS.imlib2
+# $NetBSD: options.mk,v 1.2 2005/06/10 18:14:23 wiz Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
 .if ${MACHINE_ARCH} == "i386"
+PKG_OPTIONS_VAR=	PKG_OPTIONS.imlib2
 PKG_SUPPORTED_OPTIONS=	mmx
-.endif
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mmmx)
 CONFIGURE_ARGS+=	--enable-mmx
+.endif
 .endif
