33a4d9397c0200aebebfb6402f5c25261bd79132
jlam <jlam@pkgsrc.org>
2005-06-14 07:25:24 +0000

    Require xpkgwedge>=1.13 when used as a build dependency so that the correct make(1) program is invoked by pkgxmkmf.

--- mk/bsd.pkg.use.mk
+++ mk/bsd.pkg.use.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.use.mk,v 1.17 2005/06/14 02:09:38 jlam Exp $
+#	$NetBSD: bsd.pkg.use.mk,v 1.18 2005/06/14 07:25:24 jlam Exp $
 #
 # Turn USE_* macros into proper depedency logic.  Included near the top of
 # bsd.pkg.mk, after bsd.prefs.mk.
@@ -125,5 +125,5 @@ RMAN?=			${X11BASE}/bin/rman
 ### USE_XPKGWEDGE
 
 .if defined(USE_X11BASE) && !empty(USE_XPKGWEDGE:M[yY][eE][sS])
-BUILD_DEPENDS+=		xpkgwedge>=${_XPKGWEDGE_REQD:U1.5}:../../pkgtools/xpkgwedge
+BUILD_DEPENDS+=		xpkgwedge>=${_XPKGWEDGE_REQD:U1.13}:../../pkgtools/xpkgwedge
 .endif
