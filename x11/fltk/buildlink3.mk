6ce94e3a034d409fb6f1b5c8e874a57164c2df24
kristerw <kristerw@pkgsrc.org>
2005-06-16 23:49:40 +0000

    Pthreads are needed to link to these libraries. Add mk/pthread.buildlink3.mk.

--- x11/fltk/buildlink3.mk
+++ x11/fltk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2005/06/15 02:24:40 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2005/06/16 23:49:40 kristerw Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 FLTK_BUILDLINK3_MK:=	${FLTK_BUILDLINK3_MK}+
@@ -19,6 +19,7 @@ BUILDLINK_FILES.fltk+=		include/Fl/*
 
 .include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../graphics/glu/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
 .include "../../mk/x11.buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
