a7db187995c683f6ef44b67e408c5a15f9ede0e9
jmmv <jmmv>
2005-06-18 14:32:38 +0000

    Enable sonames under DragonFly, FreeBSD and NetBSD.  The default build infrastructure only uses them under Linux and OpenBSD (eww, hardcoded logic based on OS names).
    Aside making installations more consistent across systems, this lets
    Boost work correctly on the systems where sonames were previously used.
    Otherwise, they are unable to find the correct libraries at runtime and
    we get PLIST errors (more files installed than expected).  The problem
    exposes itself when building software that needs Boost (e.g. monotone).
    
    This also means that we can't rename the installed libraries any more as
    we were doing until now, because programs linked against them will be
    looking for their respective sonames.  Therefore, keep the default names
    produced by a --layout=system build.
    
    Bump PKGREVISION of boost, boost-libs and boost-python to 1.

--- devel/boost-libs/buildlink3.mk
+++ devel/boost-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2005/02/28 01:44:17 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/06/18 14:32:38 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 BOOST_LIBS_BUILDLINK3_MK:=	${BOOST_LIBS_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	boost-libs
 
 .if !empty(BOOST_LIBS_BUILDLINK3_MK:M+)
 # Use a dependency pattern that guarantees the proper ABI.
-BUILDLINK_DEPENDS.boost-libs+=		boost-libs-1.32.*
+BUILDLINK_DEPENDS.boost-libs+=		boost-libs-1.32.*nb*
 BUILDLINK_PKGSRCDIR.boost-libs?=	../../devel/boost-libs
 .endif	# BOOST_LIBS_BUILDLINK3_MK
 
