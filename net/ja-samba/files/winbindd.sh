a2146eb83a1dd2912057857b22ad4f616c9492e2
taca <taca>
2005-06-19 18:38:40 +0000

    Update ja-samba package to 2.2.12.0.9.1 (2.2.12-ja-1.0beta1).
    - Fix security problem up fixed by 2.2.12 and recent problem reported
      by CAN-2004-1154.
    
    - pkgsrc changes:
    
    	* Overhaul pkgsrc structure (as net/samba).
    	* Adapt PKG_OPTIONS frame work.
    	* Add support for winbind (not tested).
    	* Add SAMBA_USER/SAMBA_GROUP for default user.
    	* move codepages directory from share/samba to lib/samba.

--- net/ja-samba/files/winbindd.sh
+++ net/ja-samba/files/winbindd.sh
@@ -1,26 +1,25 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: smbd.sh,v 1.3 2005/06/19 18:38:40 taca Exp $
+# $NetBSD: winbindd.sh,v 1.1 2005/06/19 18:38:40 taca Exp $
 #
-# PROVIDE: smbd
-# REQUIRE: DAEMON
+# PROVIDE: winbindd
+# REQUIRE: nmbd
 
 if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
-name="smbd"
+name="winbindd"
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
+required_vars="nmbd"
 required_files="@SAMBA_ETCDIR@/smb.conf"
 extra_commands="reload"
-command_args="-D"		# _must_ start as daemon from rc.d;
-				# add more flags through ${${name}_flags}
 
 if [ -f /etc/rc.subr ]; then
 	load_rc_config $name
 	run_rc_command "$1"
 else
 	@ECHO@ -n " ${name}"
-	${command} ${smbd_flags} ${command_args}
+	${command} ${winbindd_flags}
 fi
