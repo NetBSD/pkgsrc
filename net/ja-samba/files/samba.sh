3c667279c696a1097f3a4060c2be0b68024f3818
taca <taca@pkgsrc.org>
2005-06-19 18:38:40 +0000

    Update ja-samba package to 2.2.12.0.9.1 (2.2.12-ja-1.0beta1).
    - Fix security problem up fixed by 2.2.12 and recent problem reported
      by CAN-2004-1154.
    
    - pkgsrc changes:
    
    	* Overhaul pkgsrc structure (as net/samba).
    	* Adapt PKG_OPTIONS frame work.
    	* Add support for winbind (not tested).
    	* Add SAMBA_USER/SAMBA_GROUP for default user.
    	* move codepages directory from share/samba to lib/samba.

--- net/ja-samba/files/samba.sh
+++ net/ja-samba/files/samba.sh
@@ -1,11 +1,16 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: samba.sh,v 1.2 2002/09/20 02:01:58 grant Exp $
+# $NetBSD: samba.sh,v 1.3 2005/06/19 18:38:40 taca Exp $
 #
 # KEYWORD: nostart
+#
+#	This script is intended for the end-user.
+#	It is not started by /etc/rc.
+#	It manipulates various samba services (smbd, nmbd, winbindd),
+#	which are controlled via their appropriate rc.conf(5) settings.
+#
 
-if [ -f /etc/rc.subr ]
-then
+if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
@@ -37,7 +42,7 @@ reverse_commands()
 	done
 }
 
-COMMAND_LIST="nmbd smbd"
+COMMAND_LIST="nmbd smbd winbindd"
 
 name="samba"
 start_cmd="forward_commands"
@@ -46,11 +51,10 @@ reload_cmd="forward_commands"
 status_cmd="forward_commands"
 extra_commands="reload status"
 
-if [ -f /etc/rc.subr ]
-then
+if [ -f /etc/rc.subr ]; then
 	run_rc_command "$1"
 else
-	@ECHO@ -n ' ${name}'
+	@ECHO@ -n " ${name}"
 	_arg="$1"
 	${start_cmd}
 fi
