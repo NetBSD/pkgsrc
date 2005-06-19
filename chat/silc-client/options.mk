ebc310a31a03d388cd8ee4364c9d00b6ea7e1895
salo <salo@pkgsrc.org>
2005-06-19 01:46:35 +0000

    "inet6" option support is determined in bsd.prefs.mk, don't add it to PKG_SUGGESTED_OPTIONS.

--- chat/silc-client/options.mk
+++ chat/silc-client/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.2 2005/05/31 10:01:36 dillo Exp $
+# $NetBSD: options.mk,v 1.3 2005/06/19 01:46:35 salo Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.silc-client
 PKG_SUPPORTED_OPTIONS=	debug inet6 perl
-PKG_SUGGESTED_OPTIONS=	inet6 perl
+PKG_SUGGESTED_OPTIONS=	perl
 
 .include "../../mk/bsd.options.mk"
 
