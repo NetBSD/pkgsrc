886cab8ed4ff5e5ab590a28aacf69521fd9e5868
markd <markd@pkgsrc.org>
2005-06-22 13:14:50 +0000

    Fix finding perl in the Configure for the new tools infrastructure. Also change dependencies from build to run as needed while in use.
    oked during "deep freeze" by Alistair Crooks.

--- net/argus/options.mk
+++ net/argus/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2005/06/08 14:48:13 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2005/06/22 13:14:50 markd Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.argus
 PKG_SUPPORTED_OPTIONS=	inet6
@@ -6,6 +6,6 @@ PKG_SUPPORTED_OPTIONS=	inet6
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Minet6)
-BUILD_DEPENDS+= fping6-[0-9]*:../../net/fping6
-BUILD_DEPENDS+= p5-Socket6>=0.12:../../net/p5-Socket6
+DEPENDS+= fping6-[0-9]*:../../net/fping6
+DEPENDS+= p5-Socket6>=0.12:../../net/p5-Socket6
 .endif
