e8bbfe08871c90ce29a6c5c19a3d032c6a4fd39f
jlam <jlam@pkgsrc.org>
2005-06-23 04:02:48 +0000

    * Libtoolize graphics/OpenRM so that we correctly build shared libraries   on all platforms supported by libtool.
    * Remove the unnecessary dependencies on glut and perl, and only use
      perl as a build dependency.
    
    * Use REPLACE_PERL instead of hand-crafted substitution code.
    
    Bump the PKGREVISION for graphics/OpenRM to 2.  Due to the changed
    dependency graph, bump the PKGREVISION for packages that use the OpenRM
    libraries.  In this case, games/genecys-client is the only one, so
    bump its PKGREVISION to 2.

--- graphics/OpenRM/buildlink3.mk
+++ graphics/OpenRM/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/03/18 09:12:11 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2005/06/23 04:02:48 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 OPENRM_BUILDLINK3_MK:=	${OPENRM_BUILDLINK3_MK}+
@@ -15,9 +15,10 @@ BUILDLINK_DEPENDS.OpenRM+=	OpenRM>=1.5.1
 BUILDLINK_PKGSRCDIR.OpenRM?=	../../graphics/OpenRM
 .endif	# OPENRM_BUILDLINK3_MK
 
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
 .include "../../graphics/jpeg/buildlink3.mk"
-.include "../../graphics/Mesa/buildlink3.mk"
-
 .include "../../mk/pthread.buildlink3.mk"
+.include "../../mk/x11.buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
