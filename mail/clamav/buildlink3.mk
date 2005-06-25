abe5ae37973ce01d8a3530cf7823f0374c380a7f
salo <salo@pkgsrc.org>
2005-06-25 17:39:01 +0000

    Bump BUILDLINK_RECOMMENDED, the fix in 0.86.1 prevents a DoS vulnerability.

--- mail/clamav/buildlink3.mk
+++ mail/clamav/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2005/06/24 05:59:09 grant Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2005/06/25 17:39:01 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CLAMAV_BUILDLINK3_MK:=	${CLAMAV_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	clamav
 
 .if !empty(CLAMAV_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.clamav+=	clamav>=0.60nb1
-BUILDLINK_RECOMMENDED.clamav+=	clamav>=0.85
+BUILDLINK_RECOMMENDED.clamav+=	clamav>=0.86.1
 BUILDLINK_PKGSRCDIR.clamav?=	../../mail/clamav
 .endif	# CLAMAV_BUILDLINK3_MK
 
