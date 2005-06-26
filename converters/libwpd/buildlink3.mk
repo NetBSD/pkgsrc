f8c2fb3efba9a8524a84cacb6ab9f6f7a8e9f57c
markd <markd@pkgsrc.org>
2005-06-26 22:08:08 +0000

    Initial import of libwpd version 0.8.1. From Jeremy Reed in pkgsrc-wip, updated to 0.8.1 by me.
    libwpd is a library for reading and writing WordPerfect(tm)
    documents.
    
    This package also includes utilities for converting to HTML
    and plain text.

---
+++ converters/libwpd/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/06/26 22:08:08 markd Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBWPD_BUILDLINK3_MK:=	${LIBWPD_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libwpd
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibwpd}
+BUILDLINK_PACKAGES+=	libwpd
+
+.if !empty(LIBWPD_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libwpd+=	libwpd>=0.8.1
+BUILDLINK_PKGSRCDIR.libwpd?=	../../converters/libwpd
+.endif	# LIBWPD_BUILDLINK3_MK
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/libgsf/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
