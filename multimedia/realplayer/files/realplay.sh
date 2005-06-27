cefeb4042d39c9c2f6bbc535dcb6c1d6b94ef236
wiz <wiz@pkgsrc.org>
2005-06-27 10:41:03 +0000

    Remove realplayer: vulnerable package, old branch, no new release will come out. Use RealPlayerGold instead.

--- multimedia/realplayer/files/realplay.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# $NetBSD: realplay.sh,v 1.1.1.1 2004/01/26 13:09:58 jmmv Exp $
-#
-REALPLAYER_HOME=@REALPLAYER_HOME@ ; export REALPLAYER_HOME
-LD_LIBRARY_PATH=${REALPLAYER_HOME}:${LD_LIBRARY_PATH} ; export LD_LIBRARY_PATH
-exec ${REALPLAYER_HOME}/realplay "$@"
