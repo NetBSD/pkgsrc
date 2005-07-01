30587f1f2b20fb096c9b13f07bb771baba12ecbe
tron <tron@pkgsrc.org>
2005-07-01 15:32:51 +0000

    Disable optimizations for GCC <3.4. This fixes PR pkg/30647 by Ian Spray.

--- databases/db4/hacks.mk
+++ databases/db4/hacks.mk
@@ -1,16 +1,16 @@
-# $NetBSD: hacks.mk,v 1.2 2004/12/27 04:27:49 jlam Exp $
+# $NetBSD: hacks.mk,v 1.3 2005/07/01 15:32:51 tron Exp $
 
 ### [Sun Mar 14 19:32:40 UTC 2004 : jlam]
-### Pre-3.3 GCC has an optimization bug on powerpc that's tickled by
-### the db4 source code, so remove optimization flags in that case.
-### This fixes PR 23737 by John Klos.
+### GCC 3.3.x and older versions have an optimization bug on powerpc that's
+### tickled by the db4 source code, so remove optimization flags in that
+### case. This fixes PR pkg/30647 by Ian Spray.
 ###
 .if ${MACHINE_ARCH} == "powerpc"
 .  include "../../mk/compiler.mk"
 .  if !empty(CC_VERSION:Mgcc*)
 .    if !defined(_GCC_IS_TOO_OLD)
 _GCC_IS_TOO_OLD!=	\
-	if ${PKG_ADMIN} pmatch 'gcc<3.3' ${CC_VERSION}; then		\
+	if ${PKG_ADMIN} pmatch 'gcc<3.4' ${CC_VERSION}; then		\
 		${ECHO} "yes";						\
 	else								\
 		${ECHO} "no";						\
