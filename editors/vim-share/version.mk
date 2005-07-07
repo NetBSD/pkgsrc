f89115bb11e078d428dc37c9eeb489227725c012
martti <martti>
2005-07-07 08:21:36 +0000

    Updated vim to 6.3.079
      1745  6.3.075  syntax highlighting wrong after unloading another buffer
      3546  6.3.076  crash when using Cscope interface with very long result
      1853  6.3.079  crash after executing a command in the command-line window

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.1 2005/05/30 14:18:14 martti Exp $
+# $NetBSD: version.mk,v 1.2 2005/07/07 08:21:36 martti Exp $
 
 VIM_VERSION=		6.3
-VIM_PATCHLEVEL=		074
+VIM_PATCHLEVEL=		079
 VIM_SUBDIR=		vim63
