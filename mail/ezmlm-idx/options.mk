a6821beb094e0055068a77a304245798d6174075
schmonz <schmonz@pkgsrc.org>
2005-07-14 03:49:40 +0000

    Simplify, simplify. Take advantage of DJB_CONFIG_CMDS, CPPFLAGS, LDFLAGS.

--- mail/ezmlm-idx/options.mk
+++ mail/ezmlm-idx/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2005/07/05 06:04:17 schmonz Exp $
+# $NetBSD: options.mk,v 1.8 2005/07/14 03:49:40 schmonz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.ezmlm-idx
 PKG_OPTIONS_OPTIONAL_GROUPS=	database
@@ -11,19 +11,10 @@ PKG_OPTIONS_GROUP.database=	mysql pgsql
 ###
 .if !empty(PKG_OPTIONS:Mmysql)
 .  include "../../mk/mysql.buildlink3.mk"
-.  if !target(post-configure)
-post-configure:
-	cd ${WRKSRC};							\
-	${ECHO} -I${BUILDLINK_PREFIX.mysql-client}/include/mysql	\
-		> sub_mysql/conf-sqlcc;					\
-	${ECHO} -L${BUILDLINK_PREFIX.mysql-client}/lib/mysql 		\
-		-Wl,-R${BUILDLINK_PREFIX.mysql-client}/lib/mysql	\
-		-lmysqlclient						\
-		> sub_mysql/conf-sqlld;					\
+DJB_CONFIG_CMDS=							\
+	${ECHO} ${CPPFLAGS} > sub_mysql/conf-sqlcc;			\
+	${ECHO} ${LDFLAGS} -lmysqlclient > sub_mysql/conf-sqlld;	\
 	${ECHO} mysql > conf-sub
-.  else
-PKG_FAIL_REASON+=	"ezmlm-idx: can't define post-configure mysql target"
-.  endif
 .endif
 
 ###
@@ -31,17 +22,8 @@ PKG_FAIL_REASON+=	"ezmlm-idx: can't define post-configure mysql target"
 ###
 .if !empty(PKG_OPTIONS:Mpgsql)
 .  include "../../mk/pgsql.buildlink3.mk"
-.  if !target(post-configure)
-post-configure:
-	cd ${WRKSRC};							\
-	${ECHO} -I${PGSQL_PREFIX}/include				\
-		> sub_pgsql/conf-sqlcc;					\
-	${ECHO} -L${PGSQL_PREFIX}/lib					\
-		-Wl,-R${PGSQL_PREFIX}/lib				\
-		-lpq							\
-		> sub_pgsql/conf-sqlld;					\
+DJB_CONFIG_CMDS=							\
+	${ECHO} ${CPPFLAGS} > sub_pgsql/conf-sqlcc;			\
+	${ECHO} ${LDFLAGS} -lpq > sub_pgsql/conf-sqlld;			\
 	${ECHO} pgsql > conf-sub
-.  else
-PKG_FAIL_REASON+=	"ezmlm-idx: can't define post-configure pgsql target"
-.  endif
 .endif
