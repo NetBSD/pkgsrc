b2e3e5a71622d22cd354a72462584ab4f50b40a4
snj <snj>
2005-07-14 22:20:33 +0000

    Pullup ticket 604 - requested by Adrian Portelli security update for net-snmp
    Revisions pulled up:
    - pkgsrc/net/net-snmp/Makefile		1.31
    - pkgsrc/net/net-snmp/distinfo		1.16
    - pkgsrc/net/net-snmp/buildlink3.mk	1.6
    
        Module Name:    pkgsrc
        Committed By:   adam
        Date:           Fri Jul  8 07:49:27 UTC 2005
    
        Modified Files:
                pkgsrc/net/net-snmp: Makefile distinfo
    
        Log Message:
        Changes 5.2.1.2:
        * Security fix: denial of serivce when stream sockets are used
    ----
        Module Name:    pkgsrc
        Committed By:   adrianp
        Date:           Thu Jul 14 20:49:49 UTC 2005
    
        Modified Files:
                pkgsrc/net/net-snmp: buildlink3.mk
    
        Log Message:
        Bump BUILDLINK_RECOMMENDED to 5.2.1.2 for recent security issue.

--- net/net-snmp/buildlink3.mk
+++ net/net-snmp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2005/05/27 13:47:39 salo Exp $
+# $NetBSD: buildlink3.mk,v 1.5.2.1 2005/07/14 22:20:33 snj Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 NET_SNMP_BUILDLINK3_MK:=	${NET_SNMP_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	net-snmp
 
 .if !empty(NET_SNMP_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.net-snmp+=	net-snmp>=5.0.9nb3
-BUILDLINK_RECOMMENDED.net-snmp+=	net-snmp>=5.2.1nb1
+BUILDLINK_RECOMMENDED.net-snmp+=	net-snmp>=5.2.1.2
 BUILDLINK_PKGSRCDIR.net-snmp?=	../../net/net-snmp
 .endif	# NET_SNMP_BUILDLINK3_MK
 
