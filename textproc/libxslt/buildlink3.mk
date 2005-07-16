83147ffa6886cc46382e68a213e4e42ae33953e8
jlam <jlam@pkgsrc.org>
2005-07-16 19:10:37 +0000

    Remove some unnecessarily strong dependencies on perl that resulted from including perl5/buildlink3.mk.  These packages just need the Perl interpreter, and can just add "perl" to USE_TOOLS instead.

--- textproc/libxslt/buildlink3.mk
+++ textproc/libxslt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2004/12/30 16:42:22 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2005/07/16 19:10:40 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBXSLT_BUILDLINK3_MK:=	${LIBXSLT_BUILDLINK3_MK}+
@@ -16,7 +16,6 @@ BUILDLINK_RECOMMENDED.libxslt+=	libxslt>=1.1.10nb1
 BUILDLINK_PKGSRCDIR.libxslt?=	../../textproc/libxslt
 .endif	# LIBXSLT_BUILDLINK3_MK
 
-.include "../../lang/perl5/buildlink3.mk"
 .include "../../security/libgcrypt/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 
