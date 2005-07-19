d12cd892bb23e33220f430b09b8d8bc71c4dc20d
dillo <dillo>
2005-07-19 10:38:19 +0000

    also test unsupported and unused legacy options

--- regress/pkg-options/files/legacy-opt.mk
+++ regress/pkg-options/files/legacy-opt.mk
@@ -1,4 +1,4 @@
-# $NetBSD: legacy-opt.mk,v 1.2 2005/05/31 13:33:39 dillo Exp $
+# $NetBSD: legacy-opt.mk,v 1.3 2005/07/19 10:38:19 dillo Exp $
 #
 # This file tests PKG_OPTIONS_LEGACY_OPTS.
 #
@@ -6,8 +6,10 @@
 MAKECONF=			/dev/null
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.foo
-PKG_SUPPORTED_OPTIONS=		new
+PKG_SUPPORTED_OPTIONS=		new new2
 PKG_OPTIONS_LEGACY_OPTS+=	old:new
+PKG_OPTIONS_LEGACY_OPTS+=	old2:new2
+PKG_OPTIONS_LEGACY_OPTS+=	old3:new3
 PKG_OPTIONS.foo=		old
 
 .include "../../mk/bsd.options.mk"
