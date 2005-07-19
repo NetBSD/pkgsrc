10abf663aa9880b9f48ed1d99d51448bba05f3f4
schmonz <schmonz@pkgsrc.org>
2005-07-19 19:41:19 +0000

    Distinguish package-specific options as such, supporting the old names via PKG_OPTIONS_LEGACY_OPTS until after the next branch.

--- mail/ezmlm/options.mk
+++ mail/ezmlm/options.mk
@@ -1,18 +1,16 @@
-# $NetBSD: options.mk,v 1.2 2005/05/31 10:01:37 dillo Exp $
+# $NetBSD: options.mk,v 1.3 2005/07/19 19:41:19 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ezmlm
-PKG_SUPPORTED_OPTIONS=	althash
+PKG_SUPPORTED_OPTIONS=	ezmlm-althash
+PKG_OPTIONS_LEGACY_OPTS+=	althash:ezmlm-althash
 
 .if ${OPSYS} == "Darwin"
-PKG_SUGGESTED_OPTIONS=	althash
+PKG_SUGGESTED_OPTIONS=	ezmlm-althash
 .endif
 
 .include "../../mk/bsd.options.mk"
 
-###
-### alternate subscriber hashing scheme for case-insensitive filesystems
-###
-.if !empty(PKG_OPTIONS:Malthash)
+.if !empty(PKG_OPTIONS:Mezmlm-althash)
 post-patch:
 	@cd ${WRKSRC} && ${PATCH} ${PATCH_ARGS} < ${FILESDIR}/patch-althash
 .endif
