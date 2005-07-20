1ea1f8d4c460bd4676f9f2f8c45e24c5d6d19d26
drochner <drochner@pkgsrc.org>
2005-07-20 19:39:42 +0000

    import pygame-1.6, a collection of Python modules for games

---
+++ devel/py-game/buildlink3.mk
@@ -0,0 +1,27 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/07/20 19:39:42 drochner Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+PY_GAME_BUILDLINK3_MK:=	${PY_GAME_BUILDLINK3_MK}+
+
+.include "../../lang/python/pyversion.mk"
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	${PYPKGPREFIX}-game
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:N${PYPKGPREFIX}-game}
+BUILDLINK_PACKAGES+=	${PYPKGPREFIX}-game
+
+.if !empty(PY_GAME_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.${PYPKGPREFIX}-game+=	${PYPKGPREFIX}-game>=1.6
+BUILDLINK_PKGSRCDIR.${PYPKGPREFIX}-game?=	../../devel/py-game
+
+.include "../../audio/SDL_mixer/buildlink3.mk"
+.include "../../devel/SDL_ttf/buildlink3.mk"
+.include "../../graphics/SDL_image/buildlink3.mk"
+.include "../../multimedia/smpeg/buildlink3.mk"
+.include "../../math/py-Numeric/buildlink3.mk"
+
+.endif	# PY_GAME_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
