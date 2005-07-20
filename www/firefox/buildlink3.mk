25a9c808eecd7c13998045ecfe7441838df59012
taya <taya>
2005-07-20 23:33:30 +0000

    update firefox & firefox-gtk1 to 1.0.6
    Firefox 1.0.6 is a stability update. We recommend that users upgrade
    to this latest version.
    
    Here's what's new in Firefox 1.0.6:
    
    	* Restore API compatibility for extensions and web applications
    	that did not work in Firefox 1.0.5.

--- www/firefox/buildlink3.mk
+++ www/firefox/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2005/05/14 15:27:10 taya Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2005/07/20 23:33:30 taya Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 FIREFOX_BUILDLINK3_MK:=	${FIREFOX_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	firefox
 
 .if !empty(FIREFOX_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.firefox+=	firefox>=1.0
-BUILDLINK_RECOMMENDED.firefox+=	firefox>=1.0.4
+BUILDLINK_RECOMMENDED.firefox+=	firefox>=1.0.6
 BUILDLINK_PKGSRCDIR.firefox?=	../../www/firefox
 .endif	# FIREFOX_BUILDLINK3_MK
 
