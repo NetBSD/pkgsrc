9787c3354c532efdefdde9e678fe6234acd51825
snj <snj@pkgsrc.org>
2005-07-21 02:49:04 +0000

    Pullup ticket 613 - requested by Shin'ichiro TAYA security update for firefox and firefox-gtk1
    Revisions pulled up:
    - pkgsrc/www/firefox/Makefile-firefox.common	1.19, 1.20
    - pkgsrc/www/firefox/PLIST			1.16
    - pkgsrc/www/firefox/distinfo			1.34, 1.35
    - pkgsrc/www/firefox-gtk1/PLIST			1.5
    - pkgsrc/www/firefox/buildlink3.mk		1.6
    - pkgsrc/www/firefox-gtk1/buildlink3.mk		1.5
    
        Module Name:  pkgsrc
        Committed By: taya
        Date:         Thu Jul 14 16:38:42 UTC 2005
    
        Modified Files:
              pkgsrc/www/firefox: Makefile-firefox.common PLIST distinfo
              pkgsrc/www/firefox-gtk1: PLIST
    
        Log Message:
        Update firefox & firefox-gtk1 to 1.0.5.
    
        Firefox 1.0.5 is a security update.
        Fixed vulnerabilities are:
    
        2005-56  Code execution through shared function objects
        MFSA 2005-55 XHTML node spoofing
        MFSA 2005-54 Javascript prompt origin spoofing
        MFSA 2005-53 Standalone applications can run arbitrary code through the
                     browser
        MFSA 2005-52 Same origin violation: frame calling top.focus()
        MFSA 2005-51 The return of frame-injection spoofing
        MFSA 2005-50 Possibly exploitable crash in InstallVersion.compareTo()
        MFSA 2005-49 Script injection from Firefox sidebar panel using data:
        MFSA 2005-48 Same-origin violation with InstallTrigger callback
        MFSA 2005-47 Code execution via "Set as Wallpaper"
        MFSA 2005-46 XBL scripts ran even when Javascript disabled
        MFSA 2005-45 Content-generated event vulnerabilities
    ----
        Module Name:  pkgsrc
        Committed By: taya
        Date:         Wed Jul 20 23:33:30 UTC 2005
    
        Modified Files:
             pkgsrc/www/firefox: Makefile-firefox.common buildlink3.mk distinfo
             pkgsrc/www/firefox-gtk1: buildlink3.mk
    
        Log Message:
        update firefox & firefox-gtk1 to 1.0.6
    
        Firefox 1.0.6 is a stability update. We recommend that users upgrade
        to this latest version.
    
        Here's what's new in Firefox 1.0.6:
    
             * Restore API compatibility for extensions and web applications
             that did not work in Firefox 1.0.5.

--- www/firefox-gtk1/buildlink3.mk
+++ www/firefox-gtk1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2005/05/14 15:27:10 taya Exp $
+# $NetBSD: buildlink3.mk,v 1.4.2.1 2005/07/21 02:49:04 snj Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 FIREFOX_GTK2_BUILDLINK3_MK:=	${FIREFOX_GTK2_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	firefox-gtk1
 
 .if !empty(FIREFOX_GTK2_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.firefox-gtk1+=	firefox-gtk1>=1.0
-BUILDLINK_RECOMMENDED.firefox-gtk1+=	firefox-gtk1>=1.0.4
+BUILDLINK_RECOMMENDED.firefox-gtk1+=	firefox-gtk1>=1.0.6
 BUILDLINK_PKGSRCDIR.firefox-gtk1?=	../../www/firefox-gtk1
 .endif	# FIREFOX_GTK2_BUILDLINK3_MK
 
--- www/firefox/buildlink3.mk
+++ www/firefox/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2005/05/14 15:27:10 taya Exp $
+# $NetBSD: buildlink3.mk,v 1.5.2.1 2005/07/21 02:49:04 snj Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 FIREFOX_BUILDLINK3_MK:=	${FIREFOX_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	firefox
 
 .if !empty(FIREFOX_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.firefox+=	firefox>=1.0
-BUILDLINK_RECOMMENDED.firefox+=	firefox>=1.0.4
+BUILDLINK_RECOMMENDED.firefox+=	firefox>=1.0.6
 BUILDLINK_PKGSRCDIR.firefox?=	../../www/firefox
 .endif	# FIREFOX_BUILDLINK3_MK
 
