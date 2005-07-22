d0e8eb195dd2a9822cc8648469a995b5a7ea7622
adam <adam@pkgsrc.org>
2005-07-22 21:45:07 +0000

    Changes 2.10.0: * Increased version for GNOME 2.10 release version.
    Changes 2.8.0:
    * Increased version for GNOME 2.8 release version.
    
    Changes 2.7.2:
    * Now builds against latest gnome-vfs 2.7 API.
    
    Changes 2.7.1:
    This is the first version for GNOME 2.7/2.8 with additional API:
    * Added Address.
    * Added ResolveHandle.
    * Added DnsSd functions.
    * Drive: Added get_hal_udi().
    * Volume: Added get_hal_udi().

---
+++ devel/gnome-vfsmm/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/07/22 21:45:07 adam Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+GNOME_VFSMM_BUILDLINK3_MK:=	${GNOME_VFSMM_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gnome-vfsmm
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngnome-vfsmm}
+BUILDLINK_PACKAGES+=	gnome-vfsmm
+
+.if !empty(GNOME_VFSMM_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.gnome-vfsmm+=	gnome-vfsmm>=2.10.0
+BUILDLINK_PKGSRCDIR.gnome-vfsmm?=	../../sysutils/gnome-vfsmm
+.endif	# GNOME_VFSMM_BUILDLINK3_MK
+
+.include "../../devel/glibmm/buildlink3.mk"
+.include "../../sysutils/gnome-vfs2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
