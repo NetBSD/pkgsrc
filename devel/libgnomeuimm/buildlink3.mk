a9ccf234aa4db26243f96e335eabefbe35d673e1
adam <adam@pkgsrc.org>
2005-07-22 22:05:31 +0000

    Changes 2.10.0: * Increased version for GNOME 2.10 release version.
    Changes 2.8.0:
    * Increased version for GNOME 2.8 release version.
    
    Changes 2.7.2:
    * IconList: Added select_all().
    * DruidPageStandard: Added contents-background,
      contents-background-gdk, and contents-background-set properties.

---
+++ devel/libgnomeuimm/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/07/22 22:05:31 adam Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBGNOMEUIMM_BUILDLINK3_MK:=	${LIBGNOMEUIMM_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libgnomeuimm
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgnomeuimm}
+BUILDLINK_PACKAGES+=	libgnomeuimm
+
+.if !empty(LIBGNOMEUIMM_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libgnomeuimm+=	libgnomeuimm>=2.10.0
+BUILDLINK_PKGSRCDIR.libgnomeuimm?=	../../devel/libgnomeuimm
+.endif	# LIBGNOMEUIMM_BUILDLINK3_MK
+
+.include "../../devel/gconfmm/buildlink3.mk"
+.include "../../devel/libglademm/buildlink3.mk"
+.include "../../devel/libgnomemm/buildlink3.mk"
+.include "../../devel/libgnomeui/buildlink3.mk"
+.include "../../graphics/libgnomecanvasmm/buildlink3.mk"
+.include "../../sysutils/gnome-vfsmm/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
