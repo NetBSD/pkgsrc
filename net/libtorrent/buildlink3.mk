e0ceb37b7761b65861ca8f68aa620aa7176fd352
wiz <wiz@pkgsrc.org>
2005-07-23 11:37:08 +0000

    Fix path to libtorrent directory.

--- net/libtorrent/buildlink3.mk
+++ net/libtorrent/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/07/22 22:24:39 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/07/23 11:37:08 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBTORRENT_BUILDLINK3_MK:=	${LIBTORRENT_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libtorrent
 
 .if !empty(LIBTORRENT_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libtorrent+=	libtorrent>=0.7.0
-BUILDLINK_PKGSRCDIR.libtorrent?=	../../wip/libtorrent
+BUILDLINK_PKGSRCDIR.libtorrent?=	../../net/libtorrent
 .endif	# LIBTORRENT_BUILDLINK3_MK
 
 .include "../../devel/libsigc++2/buildlink3.mk"
