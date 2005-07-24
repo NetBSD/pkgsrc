1ed83bb8c2475a371007549d4d848e0ed5208713
wiz <wiz@pkgsrc.org>
2005-07-24 00:55:43 +0000

    openmotif needs x11 headers and libraries, so include mk/x11.buildlink3.mk.

--- x11/openmotif/buildlink3.mk
+++ x11/openmotif/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/11 18:21:33 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/07/24 00:55:43 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 OPENMOTIF_BUILDLINK3_MK:=	${OPENMOTIF_BUILDLINK3_MK}+
@@ -14,5 +14,7 @@ BUILDLINK_PACKAGES+=	openmotif
 BUILDLINK_DEPENDS.openmotif+=	openmotif>=2.1.30
 BUILDLINK_PKGSRCDIR.openmotif?=	../../x11/openmotif
 .endif	# OPENMOTIF_BUILDLINK3_MK
+
+.include "../../mk/x11.buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
