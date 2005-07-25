526271c20caec6bd8b24e214bb0d2c52257b8b59
wiz <wiz@pkgsrc.org>
2005-07-25 10:59:44 +0000

    Update for fox-1.4.x (library name changed).

--- x11/fox/buildlink3.mk
+++ x11/fox/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/12/28 23:18:16 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2005/07/25 10:59:44 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 FOX_BUILDLINK3_MK:=	${FOX_BUILDLINK3_MK}+
@@ -11,10 +11,9 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nfox}
 BUILDLINK_PACKAGES+=	fox
 
 .if !empty(FOX_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.fox+=		fox>=1.0.43nb2
-BUILDLINK_RECOMMENDED.fox+=	fox>=1.0.43nb6
+BUILDLINK_DEPENDS.fox+=		fox>=1.4.16
 BUILDLINK_PKGSRCDIR.fox?=	../../x11/fox
-BUILDLINK_INCDIRS.fox?=		include/fox
+BUILDLINK_INCDIRS.fox?=		include/fox-1.4
 .endif	# FOX_BUILDLINK3_MK
 
 .include "../../graphics/jpeg/buildlink3.mk"
