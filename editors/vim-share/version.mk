1c1da1629ab2e78b58bd2535d617b7abc8c79cb3
martti <martti>
2005-07-27 06:29:55 +0000

    Updated vim to 6.3.085
      1729  6.3.081  glob() may execute shell command unexpectedly
      2102  6.3.082  (after 6.3.081) more generic solution to avoid shell commands
      5053  6.3.083  VMS: add function keys to vt320 termcap entry
      3408  6.3.085  crash in syntax highlighting

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.2 2005/07/07 08:21:36 martti Exp $
+# $NetBSD: version.mk,v 1.3 2005/07/27 06:29:55 martti Exp $
 
 VIM_VERSION=		6.3
-VIM_PATCHLEVEL=		079
+VIM_PATCHLEVEL=		085
 VIM_SUBDIR=		vim63
