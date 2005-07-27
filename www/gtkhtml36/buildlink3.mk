62c85f0d9c203198c5dc7b047ec64caf74ecd5ae
drochner <drochner>
2005-07-27 17:26:52 +0000

    Makefile uses libsoup-devel (which is not so -devel anymore...), so should buildlink3.

--- www/gtkhtml36/buildlink3.mk
+++ www/gtkhtml36/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/03/22 16:07:33 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/07/27 17:26:52 drochner Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GTKHTML36_BUILDLINK3_MK:=	${GTKHTML36_BUILDLINK3_MK}+
@@ -18,6 +18,6 @@ BUILDLINK_PKGSRCDIR.gtkhtml36?=	../../www/gtkhtml36
 .include "../../devel/gail/buildlink3.mk"
 .include "../../devel/gal2/buildlink3.mk"
 .include "../../devel/libbonobo/buildlink3.mk"
-.include "../../net/libsoup/buildlink3.mk"
+.include "../../net/libsoup-devel/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
