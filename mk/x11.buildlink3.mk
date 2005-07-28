f505c16d88ca9dc1f478d37574e715218ec75834
jlam <jlam@pkgsrc.org>
2005-07-28 16:20:52 +0000

    Add an inclusion guard for parts of x11.buildlink3.mk that we don't want to be seen over and over again each time that file is included. This stops us from appending the same bits over and over again to CONFIGURE_ARGS and to X11_LDFLAGS.

--- mk/x11.buildlink3.mk
+++ mk/x11.buildlink3.mk
@@ -1,22 +1,26 @@
-# $NetBSD: x11.buildlink3.mk,v 1.1 2005/06/01 18:03:06 jlam Exp $
+# $NetBSD: x11.buildlink3.mk,v 1.2 2005/07/28 16:20:52 jlam Exp $
 #
 # This Makefile fragment is meant to be included by packages that
 # require an X11 distribution.  x11.buildlink3.mk will include the
 # buildlink3.mk file from the appropriate X11 distribution.
 #
 
+X11_BUILDLINK3_MK:=	${X11_BUILDLINK3_MK}+
+
 .include "../../mk/bsd.prefs.mk"
 
+.if !empty(X11_BUILDLINK3_MK:M+)
 USE_X11=	yes
 
-.include "../../mk/x11.version.mk"
+.  include "../../mk/x11.version.mk"
 
-.if defined(GNU_CONFIGURE)
+.  if defined(GNU_CONFIGURE)
 CONFIGURE_ARGS+=	--x-includes=${X11BASE:Q}/include
 CONFIGURE_ARGS+=	--x-libraries=${X11BASE:Q}/lib${LIBABISUFFIX:Q}
-.endif
+.  endif
 
 X11_LDFLAGS+=	${COMPILER_RPATH_FLAG}${X11BASE}/lib${LIBABISUFFIX}
 X11_LDFLAGS+=	-L${X11BASE}/lib${LIBABISUFFIX}
+.endif	# X11_BUILDLINK3_MK
 
 .sinclude "${X11_PKGSRCDIR.${X11_TYPE}}/buildlink3.mk"
