6ae97284ebf14cce6e59e0abeae646c6758fa509
wiz <wiz@pkgsrc.org>
2005-07-28 14:58:36 +0000

    Use ldap option instead of openldap (consistency). Add PKG_OPTIONS_LEGACY_OPTS line for openldap.

--- mail/balsa2/options.mk
+++ mail/balsa2/options.mk
@@ -1,12 +1,14 @@
-# $NetBSD: options.mk,v 1.2 2004/11/17 19:56:49 xtraeme Exp $
+# $NetBSD: options.mk,v 1.3 2005/07/28 14:58:36 wiz Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.balsa2
-PKG_SUPPORTED_OPTIONS=	openldap ssl
+PKG_SUPPORTED_OPTIONS=	ldap ssl
+# remove after 2005Q3
+PKG_OPTIONS_LEGACY_OPTS+= openldap:ldap
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mopenldap)
+.if !empty(PKG_OPTIONS:Mldap)
 CONFIGURE_ARGS+=        --with-ldap
 .  include "../../databases/openldap/buildlink3.mk"
 .endif
