2b7a180be2cfff9cb17aa2e2956e1ae447e5f70c
grant <grant>
2005-07-29 01:15:16 +0000

    fix comment to match reality

--- multimedia/mjpegtools/hacks.mk
+++ multimedia/mjpegtools/hacks.mk
@@ -1,11 +1,11 @@
-#	$NetBSD: hacks.mk,v 1.1 2005/06/15 22:42:38 hubertf Exp $
+#	$NetBSD: hacks.mk,v 1.2 2005/07/29 01:15:16 grant Exp $
 
 .if !defined(MJPEGTOOLS_HACKS_MK)
 MJPEGTOOLS_HACKS_MK= defined
 
 ### Hack to work around MacOS X 4.x (and probably other)'s gcc 4.x 
 ### which does not understand -fmove-all-movables. Remove that option
-### if gcc-4 is _not_ used.
+### if gcc-4 is used.
 ###
 .include "../../mk/compiler.mk"
 
