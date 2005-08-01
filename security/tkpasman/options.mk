5de06439adda0e487f2f2630a88b244d756968d1
wiz <wiz@pkgsrc.org>
2005-08-01 11:45:19 +0000

    Convert to options framework.

---
+++ security/tkpasman/options.mk
@@ -0,0 +1,18 @@
+# $NetBSD: options.mk,v 1.1 2005/08/01 11:45:19 wiz Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.tkpasman
+PKG_SUPPORTED_OPTIONS=		ssl
+# remove after pkgsrc-2005Q3
+PKG_OPTIONS_LEGACY_VARS+=	TKPASMAN_USE_SSL:ssl
+
+.include "../../mk/bsd.options.mk"
+
+# SSL support
+.if !empty(PKG_OPTIONS:Mssl)
+.  include "../../security/openssl/buildlink3.mk"
+CONFIG_SUBST+=		USE_OPENSSL="true"
+CONFIG_SUBST+=		OPENSSL="${BUILDLINK_PREFIX.openssl}/bin/openssl"
+.else
+CONFIG_SUBST+=		USE_OPENSSL="false"
+CONFIG_SUBST+=		OPENSSL=""
+.endif
