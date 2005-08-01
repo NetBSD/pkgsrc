ff151cad11b4baa5fb67ab29369dab687a40c7aa
rillig <rillig@pkgsrc.org>
2005-08-01 01:02:05 +0000

    Simplified the testcase.

--- regress/make-quoting/files/bug2.mk
+++ regress/make-quoting/files/bug2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bug2.mk,v 1.1 2005/08/01 00:40:30 rillig Exp $
+# $NetBSD: bug2.mk,v 1.2 2005/08/01 01:02:05 rillig Exp $
 #
 # This file demonstrates a subtle inconsistency that only occurs when an
 # undefined variable is used in another, which is then quoted and copied
@@ -9,14 +9,10 @@
 # this variable references the undefined variable
 UNDEF_REF=	${UNDEFINED}
 
-# here the reference is quoted
-VARS+=		${UNDEF_REF:Q}
-
 # and the current value is assigned to another variable
-VARS_VALUE:=	${VARS}
+UNDEF_VALUE:=	${UNDEF_REF:Q}
 
 all:
 	@echo UNDEFINED=${UNDEFINED:Q}""
 	@echo UNDEF_REF=${UNDEF_REF:Q}""
-	@echo VARS=${VARS:Q}""
-	@echo VARS_VALUE=${VARS_VALUE:Q}""
+	@echo UNDEF_VALUE=${UNDEF_VALUE:Q}""
