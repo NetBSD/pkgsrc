af2540f6741ce313a652d436eb890fc0207cc965
rillig <rillig@pkgsrc.org>
2005-08-01 00:37:21 +0000

    I found the reason for the misbehaviour. This will result in a new testcase (bug2) for regress/make-quoting.

--- mk/x11.buildlink3.mk
+++ mk/x11.buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: x11.buildlink3.mk,v 1.3 2005/07/31 23:54:04 rillig Exp $
+# $NetBSD: x11.buildlink3.mk,v 1.4 2005/08/01 00:37:21 rillig Exp $
 #
 # This Makefile fragment is meant to be included by packages that
 # require an X11 distribution.  x11.buildlink3.mk will include the
@@ -15,11 +15,8 @@ USE_X11=	yes
 .  include "../../mk/x11.version.mk"
 
 .  if defined(GNU_CONFIGURE)
-# XXX when X11BASE is quoted using :Q here, audio/bmp does not build
-# XXX because the $ disappears, leaving {DESTDIR}/usr/X11R6/include.
-# XXX Can someone please explain this?
-CONFIGURE_ARGS+=	--x-includes=${X11BASE}/include
-CONFIGURE_ARGS+=	--x-libraries=${X11BASE}/lib${LIBABISUFFIX:Q}
+CONFIGURE_ARGS+=	--x-includes=${X11BASE:Q}/include
+CONFIGURE_ARGS+=	--x-libraries=${X11BASE:Q}/lib${LIBABISUFFIX:Q}
 .  endif
 
 X11_LDFLAGS+=	${COMPILER_RPATH_FLAG}${X11BASE}/lib${LIBABISUFFIX}
