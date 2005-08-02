964437fd7577158d84780d306001056bf0bee377
seb <seb>
2005-08-02 09:26:06 +0000

    Prevent paths of the build directories from being included in all amanda packages. Set runtime dependencies on GNU tar and gzip for all amanda packages.
    Bump PKGREVISIONs to 1 for amanda-client and amanda-plot, to 2 for
    amanda-common and amanda-server.

--- sysutils/amanda-common/buildlink3.mk
+++ sysutils/amanda-common/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/11/12 12:13:45 mjl Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2005/08/02 09:26:06 seb Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 AMANDA_COMMON_BUILDLINK3_MK:=	${AMANDA_COMMON_BUILDLINK3_MK}+
@@ -11,8 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Namanda-common}
 BUILDLINK_PACKAGES+=	amanda-common
 
 .if !empty(AMANDA_COMMON_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.amanda-common+=	amanda-common>=2.4.4p4
-BUILDLINK_RECOMMENDED.amanda-common+=	amanda-common>=2.4.4p4
+BUILDLINK_DEPENDS.amanda-common+=	amanda-common>=2.4.4p4nb2
+BUILDLINK_RECOMMENDED.amanda-common+=	amanda-common>=2.4.4p4nb2
 BUILDLINK_PKGSRCDIR.amanda-common?=	../../sysutils/amanda-common
 .endif	# AMANDA_COMMON_BUILDLINK3_MK
 
