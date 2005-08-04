6ab2757c188518c9e4c649bee93b9c3e3e993196
taca <taca>
2005-08-04 05:56:39 +0000

    Fix BUILDLINK_PKGSRCDIR.qalculate.

--- math/qalculate/buildlink3.mk
+++ math/qalculate/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2005/08/03 13:40:38 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/08/04 05:56:39 taca Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 QALCULATE_BUILDLINK3_MK:=	${QALCULATE_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	qalculate
 
 .if !empty(QALCULATE_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.qalculate+=	qalculate>=0.8.1.1
-BUILDLINK_PKGSRCDIR.qalculate?=	../../adam/qalculate
+BUILDLINK_PKGSRCDIR.qalculate?=	../../math/qalculate
 .endif	# QALCULATE_BUILDLINK3_MK
 
 .include "../../devel/glib2/buildlink3.mk"
