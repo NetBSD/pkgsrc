9912ef910787d9a3da817425240408390e19d638
salo <salo@pkgsrc.org>
2005-08-05 13:07:32 +0000

    backout previous, it's nb2, not rc3.. *sigh*

--- chat/ekg/buildlink3.mk
+++ chat/ekg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2005/08/05 13:04:26 salo Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2005/08/05 13:07:32 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 EKG_BUILDLINK3_MK:=	${EKG_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	ekg
 
 .if !empty(EKG_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.ekg+=		ekg>=1.6
-BUILDLINK_RECOMMENDED.ekg?=	ekg>=1.6nb3
+BUILDLINK_RECOMMENDED.ekg?=	ekg>=1.6nb2
 BUILDLINK_PKGSRCDIR.ekg?=	../../chat/ekg
 .endif	# EKG_BUILDLINK3_MK
 
