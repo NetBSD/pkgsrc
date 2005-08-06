8e4a5cd27f7601501aabe62937aafd782ca5f13e
salo <salo>
2005-08-06 22:17:34 +0000

    Pullup ticket 662 - requested by Adrian Portelli update and security fixes for proftpd
    Revisions pulled up:
    - pkgsrc/net/proftpd/Makefile		1.22, 1.23, 1.24
    - pkgsrc/net/proftpd/PLIST		1.9
    - pkgsrc/net/proftpd/distinfo		1.10, 1.11, 1.12, 1.13
    - pkgsrc/net/proftpd/options.mk		1.1
    - pkgsrc/net/proftpd/files/proftpd.sh	1.2
    - pkgsrc/net/proftpd/patches/patch-ab	1.1
    - pkgsrc/net/proftpd/patches/patch-ac	1.1, 1.2
    - pkgsrc/net/proftpd/patches/patch-ad	1.1
    - pkgsrc/net/proftpd/patches/patch-ae	1.1
    
       Module Name:		pkgsrc
       Committed By:	salo
       Date:		Wed Jun 22 22:43:37 UTC 2005
    
       Modified Files:
       	pkgsrc/net/proftpd: Makefile PLIST distinfo
       	pkgsrc/net/proftpd/files: proftpd.sh
       Added Files:
       	pkgsrc/net/proftpd: options.mk
       	pkgsrc/net/proftpd/patches: patch-ab
    
       Log Message:
       PKGREVISION++
       - split PKG_OPTIONS to options.mk file
       - fix issue with state directory in (typically) /var/run,
         noted by Justin Newcomer on tech-pkg@
       - delint
    ---
       Module Name:		pkgsrc
       Committed By:	christos
       Date:		Thu Jun 23 04:49:27 UTC 2005
    
       Modified Files:
       	pkgsrc/net/proftpd: Makefile distinfo
       Added Files:
       	pkgsrc/net/proftpd/patches: patch-ac
    
       Log Message:
       Make it log in wtmpx properly.
       XXX: someone should fix the array has type char issues.
    ---
       Module Name:		pkgsrc
       Committed By:	christos
       Date:		Sun Jul 10 19:57:07 UTC 2005
    
       Modified Files:
       	pkgsrc/net/proftpd: distinfo
       	pkgsrc/net/proftpd/patches: patch-ac
    
       Log Message:
       Simplify (and correct) the previous ut_ss filling code.
    ---
       Module Name:		pkgsrc
       Committed By:	adrianp
       Date:		Sat Aug  6 11:51:18 UTC 2005
    
       Modified Files:
       	pkgsrc/net/proftpd: Makefile
       Added Files:
       	pkgsrc/net/proftpd/patches: patch-ad patch-ae
    
       Log Message:
       Patches to address recent security issue via Gentoo:
       	http://secunia.com/advisories/16181/
    ---
       Module Name:		pkgsrc
       Committed By:	adrianp
       Date:		Sat Aug  6 12:29:25 UTC 2005
    
       Modified Files:
       	pkgsrc/net/proftpd: distinfo
    
       Log Message:
       Add missing patch from last update - ride the previous bump

--- net/proftpd/files/proftpd.sh
+++ net/proftpd/files/proftpd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: proftpd.sh,v 1.1 2003/03/22 10:15:27 jmmv Exp $
+# $NetBSD: proftpd.sh,v 1.1.14.1 2005/08/06 22:17:34 salo Exp $
 #
 
 # PROVIDE: proftpd
@@ -12,7 +12,7 @@
 name="proftpd"
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
-pidfile="/var/run/${name}.pid"
+pidfile="@VARBASE@/run/${name}.pid"
 required_files="@PKG_SYSCONFDIR@/proftpd.conf"
 
 load_rc_config $name
