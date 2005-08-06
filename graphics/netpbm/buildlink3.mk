278886f3223b80a21a1ec91229aa80118cab5d50
salo <salo>
2005-08-06 14:07:07 +0000

    Update to version 10.28
    - security fix for SA16184, patch from Debian
    
    Changes:
    - ppmtompeg: fix insecure temp file creation
    - jpeg2000: don't do unsafe temporary files
    - add cameratopam (Dave Coffin's dcraw)
    - various other bugfixes

--- graphics/netpbm/buildlink3.mk
+++ graphics/netpbm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/12/28 23:18:17 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2005/08/06 14:07:07 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 NETPBM_BUILDLINK3_MK:=	${NETPBM_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	netpbm
 
 .if !empty(NETPBM_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.netpbm+=	netpbm>=10.11.6
-BUILDLINK_RECOMMENDED.netpbm+=	netpbm>=10.18.17nb1
+BUILDLINK_RECOMMENDED.netpbm+=	netpbm>=10.28
 BUILDLINK_PKGSRCDIR.netpbm?=	../../graphics/netpbm
 .endif	# NETPBM_BUILDLINK3_MK
 
