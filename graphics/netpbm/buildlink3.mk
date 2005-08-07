acc9e866ee0b0b658cdbe4484cda6cb38ac264ea
snj <snj@pkgsrc.org>
2005-08-07 05:03:33 +0000

    Pullup ticket 666 - requested by Lubomir Sedlacik security update for netpbm
    Revisions pulled up:
    - pkgsrc/graphics/netpbm/Makefile		1.116, 1.119, 1.120
    - pkgsrc/graphics/netpbm/distinfo		1.36, 1.38, 1.39
    - pkgsrc/graphics/netpbm/patches/patch-ad	removed
    - pkgsrc/graphics/netpbm/patches/patch-af	1.9
    - pkgsrc/graphics/netpbm/patches/patch-ag	1.8
    - pkgsrc/graphics/netpbm/buildlink3.mk		1.6
    
        Module Name:    pkgsrc
        Committed By:   adam
        Date:           Thu Jun 30 14:55:13 UTC 2005
    
        Modified Files:
                pkgsrc/graphics/netpbm: Makefile distinfo
        Removed Files:
                pkgsrc/graphics/netpbm/patches: patch-ad
    
        Log Message:
        Changes 10.26.11:
        * Fixed memory problem in pamcomp
        Changes 10.26.10:
        * Fixed: jpeg2ktopam, rle_open: ppmtompeg
    ----
        Module Name:    pkgsrc
        Committed By:   adam
        Date:           Tue Jul 26 14:34:53 UTC 2005
    
        Modified Files:
                pkgsrc/graphics/netpbm: Makefile distinfo
    
        Log Message:
        Changes 10.26.12:
        * ppmtompeg: fix uname() rc test (sometimes > 0 is success)
        * Fix bytesToWordInt()
    ----
        Module Name:    pkgsrc
        Committed By:   salo
        Date:           Sat Aug  6 14:07:07 UTC 2005
    
        Modified Files:
                pkgsrc/graphics/netpbm: Makefile buildlink3.mk distinfo
        Added Files:
                pkgsrc/graphics/netpbm/patches: patch-af patch-ag
    
        Log Message:
        Update to version 10.28
    
        - security fix for SA16184, patch from Debian
    
        Changes:
        - ppmtompeg: fix insecure temp file creation
        - jpeg2000: don't do unsafe temporary files
        - add cameratopam (Dave Coffin's dcraw)
        - various other bugfixes

--- graphics/netpbm/buildlink3.mk
+++ graphics/netpbm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/12/28 23:18:17 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.5.4.1 2005/08/07 05:03:33 snj Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 NETPBM_BUILDLINK3_MK:=	${NETPBM_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	netpbm
 
 .if !empty(NETPBM_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.netpbm+=	netpbm>=10.11.6
-BUILDLINK_RECOMMENDED.netpbm+=	netpbm>=10.18.17nb1
+BUILDLINK_RECOMMENDED.netpbm+=	netpbm>=10.28
 BUILDLINK_PKGSRCDIR.netpbm?=	../../graphics/netpbm
 .endif	# NETPBM_BUILDLINK3_MK
 
