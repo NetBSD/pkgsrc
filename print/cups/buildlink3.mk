2c4a4f82a5c00a130b0123fa4d270670e5e2a38f
snj <snj>
2005-08-11 07:08:36 +0000

    Pullup ticket 673 - requested by Lubomir Sedlacik security fix for cups
    Revisions pulled up:
    - pkgsrc/print/cups/Makefile		1.95
    - pkgsrc/print/cups/buildlink3.mk	1.13
    - pkgsrc/print/cups/distinfo		1.32-1.33
    - pkgsrc/print/cups/patches/patch-aw	1.3-1.4
    
        Module Name:    pkgsrc
        Committed By:   salo
        Date:           Wed Aug 10 23:37:08 UTC 2005
    
        Modified Files:
                pkgsrc/print/cups: Makefile buildlink3.mk distinfo
        Added Files:
                pkgsrc/print/cups/patches: patch-aw
    
        Log Message:
        Security fix for CAN-2005-2097.
    
        "A vulnerability has been reported in CUPS, which can be exploited by
        malic ious people to cause a DoS (Denial of Service) on a vulnerable
        system.
    
        When processing a PDF file, bounds checking was not correctly performed on
        some fields. This could cause the pdftops filter (running as user "lp") to
        crash."
    
        http://secunia.com/advisories/16380/
        http://rhn.redhat.com/errata/RHSA-2005-706.html
    
        Patch from RedHat.
    ----
        Module Name:    pkgsrc
        Committed By:   salo
        Date:           Thu Aug 11 00:03:18 UTC 2005
    
        Modified Files:
                pkgsrc/print/cups: distinfo
                pkgsrc/print/cups/patches: patch-aw
    
        Log Message:
        Remove erroneous (literally) include.

--- print/cups/buildlink3.mk
+++ print/cups/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2005/01/11 00:09:21 salo Exp $
+# $NetBSD: buildlink3.mk,v 1.12.4.1 2005/08/11 07:08:36 snj Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CUPS_BUILDLINK3_MK:=	${CUPS_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	cups
 
 .if !empty(CUPS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.cups+=	cups>=1.1.19nb3
-BUILDLINK_RECOMMENDED.cups+=	cups>=1.1.23
+BUILDLINK_RECOMMENDED.cups+=	cups>=1.1.23nb3
 BUILDLINK_PKGSRCDIR.cups?=	../../print/cups
 .endif	# CUPS_BUILDLINK3_MK
 
