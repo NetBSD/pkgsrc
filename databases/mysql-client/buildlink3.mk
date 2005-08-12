1539691b833eb8cdd8981e65997c6447e7880ac3
salo <salo>
2005-08-12 16:23:16 +0000

    Pullup ticket 677 - requested by Juan Romero Pardines security update for mysql
    Revisions pulled up:
    - pkgsrc/databases/mysql-client/Makefile.common		1.47
    - pkgsrc/databases/mysql-client/buildlink3.mk		1.13
    - pkgsrc/databases/mysql-client/distinfo		1.19
    - pkgsrc/databases/mysql-client/patches/patch-au	removed
    - pkgsrc/databases/mysql-server/distinfo		1.31
    - pkgsrc/databases/mysql-server/patches/patch-au	removed
    
       Module Name:		pkgsrc
       Committed By:	schmonz
       Date:		Thu Jul 14 03:35:41 UTC 2005
    
       Modified Files:
       	pkgsrc/databases/mysql-client: buildlink3.mk
    
       Log Message:
       Define BUILDLINK_INCDIRS.mysql-client to ensure that the MySQL
       include dir is automatically available in ${CPPFLAGS}, following
       the example of postgres74-lib.
    ---
       Module Name:		pkgsrc
       Committed By:	xtraeme
       Date:		Fri Aug 12 13:03:05 UTC 2005
    
       Modified Files:
       	pkgsrc/databases/mysql-client: Makefile.common
       distinfo pkgsrc/databases/mysql-server: distinfo
       Removed Files:
       	pkgsrc/databases/mysql-client/patches: patch-au
       	pkgsrc/databases/mysql-server/patches: patch-au
    
       Log Message:
       Update mysql-{client,server} to 4.0.25, which should fix
       the following vulnerabilities:
    
       http://www.appsecinc.com/resources/alerts/mysql/2005-001.html
       http://www.appsecinc.com/resources/alerts/mysql/2005-002.html
    
       Please see changelog: http://dev.mysql.com/doc/mysql/en/news-4-0-25.html

--- databases/mysql-client/buildlink3.mk
+++ databases/mysql-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2005/03/15 15:43:28 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.12.4.1 2005/08/12 16:23:16 salo Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MYSQL_CLIENT_BUILDLINK3_MK:=	${MYSQL_CLIENT_BUILDLINK3_MK}+
@@ -15,6 +15,7 @@ BUILDLINK_DEPENDS.mysql-client+=	mysql-client>=4.0.18nb1
 BUILDLINK_RECOMMENDED.mysql-client?=	mysql-client>=4.0.24
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql-client
 BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
+BUILDLINK_INCDIRS.mysql-client?=	include/mysql
 .endif	# MYSQL_CLIENT_BUILDLINK3_MK
 
 .include "../../security/openssl/buildlink3.mk"
