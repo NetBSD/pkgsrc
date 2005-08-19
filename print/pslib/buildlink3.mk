1f3b689946ffcfd62f2fc322c0e0d6e8da410795
drochner <drochner>
2005-08-19 16:05:03 +0000

    import pslib-0.2.6, a tool to create postscript from programs

---
+++ print/pslib/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/08/19 16:05:03 drochner Exp $
+# XXX
+# XXX This file was created automatically using createbuildlink-3.9.
+# XXX After this file has been verified as correct, the comment lines
+# XXX beginning with "XXX" should be removed.  Please do not commit
+# XXX unverified buildlink3.mk files.
+# XXX
+# XXX Packages that only install static libraries or headers should
+# XXX include the following line:
+# XXX
+# XXX	BUILDLINK_DEPMETHOD.pslib?=	build
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+PSLIB_BUILDLINK3_MK:=	${PSLIB_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	pslib
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npslib}
+BUILDLINK_PACKAGES+=	pslib
+
+.if !empty(PSLIB_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.pslib+=	pslib>=0.2.6
+BUILDLINK_PKGSRCDIR.pslib?=	../../print/pslib
+.endif	# PSLIB_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
