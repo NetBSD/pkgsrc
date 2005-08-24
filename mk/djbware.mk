e9e964af052c875416537c8a864ecb63a9fd552d
schmonz <schmonz@pkgsrc.org>
2005-08-24 04:47:10 +0000

    Introduce DJB_MAKE_TARGETS boolean, on by default, off in the following packages because they don't use "it" and "setup check" as BUILD_TARGET and INSTALL_TARGET, respectively:
    devel/bglibs
    mail/mailfront
    mail/qmail-qfilter
    sysutils/memtester
    sysutils/qlogtools
    
    Since there's only one consumer of BGWARE_INSTALLER, and future
    bgware will use bg-installer from bglibs, move the do-install
    routine directly into qlogtools.

--- mk/djbware.mk
+++ mk/djbware.mk
@@ -1,4 +1,4 @@
-# $NetBSD: djbware.mk,v 1.8 2005/08/19 13:47:26 schmonz Exp $
+# $NetBSD: djbware.mk,v 1.9 2005/08/24 04:47:10 schmonz Exp $
 #
 # Makefile fragment for packages with djb-style build machinery
 #
@@ -12,7 +12,6 @@
 # * centralize handling of third-party manpages
 # * centralize MASTER_SITES and SITES_foo
 # * centralize compiler hack for arm{,32}
-# * phase out BGWARE_INSTALLER (instead, use default INSTALL_TARGET)
 # * common install script for compatibility with default conf-home pathnames
 # * PKG_OPTIONS (default):
 #     djbware-errno-hack (off)
@@ -28,12 +27,12 @@ DJBWARE_MK=		# defined
 
 DJB_RESTRICTED?=	YES
 DJB_ERRNO_HACK?=	YES
+DJB_MAKE_TARGETS?=	YES
 DJB_BUILD_TARGETS?=	# empty
 DJB_INSTALL_TARGETS?=	# empty
 DJB_CONFIG_PREFIX?=	${PREFIX}
 DJB_CONFIG_HOME?=	conf-home
 DJB_CONFIG_CMDS?=	${DO_NADA}
-BGWARE_INSTALLER?=	NO
 
 .if !empty(DJB_RESTRICTED:M[yY][eE][sS])
 RESTRICTED=		"modified source and binaries may not be distributed"
@@ -41,8 +40,10 @@ NO_BIN_ON_CDROM=	${RESTRICTED}
 NO_BIN_ON_FTP=		${RESTRICTED}
 .endif
 
+.if !empty(DJB_MAKE_TARGETS:M[yY][eE][sS])
 BUILD_TARGET?=		it ${DJB_BUILD_TARGETS}
 INSTALL_TARGET?=	setup check ${DJB_INSTALL_TARGETS}
+.endif
 
 .if !defined(NO_CONFIGURE) && !target(do-configure)
 do-configure:
@@ -65,15 +66,6 @@ do-configure:
 	[ -f conf-bglibs ] && \
 		${ECHO} ${LOCALBASE}/lib/bglibs > conf-bglibs;		\
 	${DJB_CONFIG_CMDS}
-.endif
-
-.if !empty(BGWARE_INSTALLER:M[yY][eE][sS])
-. if !target(do-install)
-do-install:
-	${_PKG_SILENT}${_PKG_DEBUG}cd ${WRKSRC};			\
-	./installer;							\
-	./instcheck
-. endif
 .endif
 
 PKG_SUPPORTED_OPTIONS+=	djbware-errno-hack
