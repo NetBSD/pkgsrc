c81b8a6c63ddd687ebf7e3a5a92ee4e876311280
schmonz <schmonz@pkgsrc.org>
2005-08-24 04:49:31 +0000

    Update to 0.75. From the changelog:
    - Added an "autoconvert" mode to cvm-vmailmgr, which converts encrypted
      passwords to plain-text on successful authentication if
      $VMAILMGR_AUTOCONVERT is set.

--- security/cvm/buildlink3.mk
+++ security/cvm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/07/20 03:28:41 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/08/24 04:49:31 schmonz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CVM_BUILDLINK3_MK:=	${CVM_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ncvm}
 BUILDLINK_PACKAGES+=	cvm
 
 .if !empty(CVM_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.cvm+=		cvm>=0.71
+BUILDLINK_DEPENDS.cvm+=		cvm>=0.75
 BUILDLINK_PKGSRCDIR.cvm?=	../../security/cvm
 BUILDLINK_DEPMETHOD.cvm?=	build
 .endif	# CVM_BUILDLINK3_MK
