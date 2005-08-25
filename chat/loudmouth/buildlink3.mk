bd7b4dbcf337d7680b8edb28e62d439379aab36b
jmmv <jmmv@pkgsrc.org>
2005-08-25 20:16:15 +0000

    Update to 1.0:
    Changes in 1.0:
    ---------------
    * Fix crash when connection goes down using SSL, LM-3 (Mikael Hallendal)
    * Fixed some GCC 4.0 warnings                         (Mikael Hallendal)
    * Some API fixups for consistency in LmSLL            (Mikael Hallendal)
    * Don't try to setup SSL if it's not supported        (Martyn Russel)
    * Documentation fixes                                 (Mikael Hallendal)
    * State fixes when using blocking API, LM-7           (Mikael Hallendal)
    * Check that connection is open before using it, LM-6 (Mikael Hallendal)
    * Fix infinite loop in blocking open if failed, LM-8  (Mikael Hallendal)
    
    Changes in 0.90:
    ----------------
    * Two crashers for ssl read fixed (Micke, Daniel Lavalliere)
    * Fixed a couple of leaks when freeing the connection (Daniel Lavalliere)
    * A couple of thread related issues solved (Micke, Daniel Lavalliere)
    * Work with latest GnuTLS (foser)
    * Improved the example tools some (Micke)

--- chat/loudmouth/buildlink3.mk
+++ chat/loudmouth/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/10/03 00:13:15 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2005/08/25 20:16:15 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LOUDMOUTH_BUILDLINK3_MK:=	${LOUDMOUTH_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nloudmouth}
 BUILDLINK_PACKAGES+=	loudmouth
 
 .if !empty(LOUDMOUTH_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.loudmouth+=	loudmouth>=0.17
-BUILDLINK_RECOMMENDED.loudmouth+=	loudmouth>=0.17nb1
+BUILDLINK_DEPENDS.loudmouth+=	loudmouth>=1.0
 BUILDLINK_PKGSRCDIR.loudmouth?=	../../chat/loudmouth
 .endif	# LOUDMOUTH_BUILDLINK3_MK
 
