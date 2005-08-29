f81ef4596b2744b680e5595e53c225aafc0da41f
tv <tv@pkgsrc.org>
2005-08-29 13:12:14 +0000

    Only one SSL scheme can be used.  Default to (and prefer) gnutls, as requested by the package author.  Fall back to openssl only if ssl is set and -gnutls is specified in OPTIONS.

--- net/lftp/options.mk
+++ net/lftp/options.mk
@@ -1,22 +1,23 @@
-# $NetBSD: options.mk,v 1.3 2005/06/01 14:06:50 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2005/08/29 13:12:14 tv Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.lftp
 PKG_SUPPORTED_OPTIONS=	gnutls ssl
 PKG_SUGGESTED_OPTIONS=	gnutls
+
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mgnutls) && !empty(PKG_OPTIONS:Mssl)
-PKG_FAIL_REASON+=	"Options \"gnutls\" and \"ssl\" are mutually exclusive."
-.endif
-
+###
+### SSL support
+###
 .if !empty(PKG_OPTIONS:Mgnutls)
 CONFIGURE_ARGS+=	--without-openssl
-.include "../../security/gnutls/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mssl)
+.  include "../../security/gnutls/buildlink3.mk"
+.elif !empty(PKG_OPTIONS:Mssl)
 CONFIGURE_ARGS+=	--without-gnutls
 CONFIGURE_ARGS+=	--with-openssl=${SSLBASE}
-.include "../../security/openssl/buildlink3.mk"
+.  include "../../security/openssl/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-gnutls
+CONFIGURE_ARGS+=	--without-openssl
 .endif
