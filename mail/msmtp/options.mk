a15d968b996d2feb57d29200630fbcf577d7decd
tv <tv@pkgsrc.org>
2005-08-29 13:09:58 +0000

    Only one SSL scheme can be used.  Default to (and prefer) OpenSSL; allow fallback to gnutls of ssl is not set in OPTIONS.

--- mail/msmtp/options.mk
+++ mail/msmtp/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2005/05/31 10:01:38 dillo Exp $
+# $NetBSD: options.mk,v 1.7 2005/08/29 13:09:58 tv Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.msmtp
 PKG_SUPPORTED_OPTIONS=	gnutls gsasl inet6 ssl
@@ -6,24 +6,22 @@ PKG_SUGGESTED_OPTIONS=	ssl
 
 .include "../../mk/bsd.options.mk"
 
-# GNUtls support
-.if !empty(PKG_OPTIONS:Mgnutls)
-.include "../../security/gnutls/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-ssl=gnutls
-.endif
-
-# GNUsasl support
-.if !empty(PKG_OPTIONS:Mgsasl)
-.include "../../security/gsasl/buildlink3.mk"
-.endif
-
-# OpenSSL support
+###
+### SSL support
+###
 .if !empty(PKG_OPTIONS:Mssl)
-.include "../../security/openssl/buildlink3.mk"
+.  include "../../security/openssl/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-ssl=openssl
+.elif !empty(PKG_OPTIONS:Mgnutls)
+.  include "../../security/gnutls/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-ssl=gnutls
+.else
+CONFIGURE_ARGS+=	--disable-ssl
 .endif
 
-# Disable any SSL support
-.if empty(PKG_OPTIONS:Mgnutls) && empty(PKG_OPTIONS:Mssl)
-CONFIGURE_ARGS+=	--disable-ssl
+###
+### GNUsasl support
+###
+.if !empty(PKG_OPTIONS:Mgsasl)
+.  include "../../security/gsasl/buildlink3.mk"
 .endif
