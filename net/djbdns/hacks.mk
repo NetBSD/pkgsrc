d87a8ce30104a219ccf43aff1478f131d90ecdb6
schmonz <schmonz>
2005-09-02 01:40:20 +0000

    Give these hacks a meaningful name ("arm-gcc2-optimizer").

--- net/djbdns/hacks.mk
+++ net/djbdns/hacks.mk
@@ -1,9 +1,9 @@
-# $NetBSD: hacks.mk,v 1.1 2005/08/24 04:36:14 schmonz Exp $
+# $NetBSD: hacks.mk,v 1.2 2005/09/02 01:40:20 schmonz Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
 # Avoid gcc optimizer bug on arm platforms
 .if ${MACHINE_ARCH} == "arm" || ${MACHINE_ARCH} == "arm32"
-PKG_HACKS+=	djbware-XXX-foo
+PKG_HACKS+=	arm-gcc2-optimizer
 GCC_REQD+=	3.0
 .endif
