2b4380789b2255a38a6181d121ad532009347f1e
adam <adam@pkgsrc.org>
2005-09-05 07:34:05 +0000

    buildlink3.mk matches Makefile now

--- security/gnutls/buildlink3.mk
+++ security/gnutls/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2005/05/02 19:48:37 salo Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2005/09/05 07:34:05 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GNUTLS_BUILDLINK3_MK:=	${GNUTLS_BUILDLINK3_MK}+
@@ -16,8 +16,9 @@ BUILDLINK_RECOMMENDED.gnutls+=	gnutls>=1.2.3
 BUILDLINK_PKGSRCDIR.gnutls?=	../../security/gnutls
 .endif	# GNUTLS_BUILDLINK3_MK
 
-.include "../../archivers/liblzo/buildlink3.mk"
+.include "../../archivers/lzo/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/libcfg+/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../security/libgcrypt/buildlink3.mk"
 .include "../../security/libtasn1/buildlink3.mk"
