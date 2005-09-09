aa2f6e2eae772bf5553b3d17f6e57147b8ce5207
schmonz <schmonz@pkgsrc.org>
2005-09-09 05:49:00 +0000

    Allow versions of PHP newer than 5.0.4nb1 to satisfy BUILDLINK_RECOMMENDED. Fixes build of ap-php and PHP modules.

--- lang/php5/buildlink3.mk
+++ lang/php5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2005/09/03 14:41:05 adrianp Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2005/09/09 05:49:00 schmonz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PHP_BUILDLINK3_MK:=	${PHP_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	php
 
 .if !empty(PHP_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.php+=	php-5.0.*
-BUILDLINK_RECOMMENDED.php+=	php-5.0.4nb1
+BUILDLINK_RECOMMENDED.php+=	php>=5.0.4nb1
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php5
 .endif	# PHP_BUILDLINK3_MK
 
