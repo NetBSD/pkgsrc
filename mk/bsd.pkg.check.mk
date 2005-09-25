50b86bc2e5af2ea79041d34ce9dbcb5f033240dd
kristerw <kristerw@pkgsrc.org>
2005-09-25 00:34:12 +0000

    Some of the names listed in CHECK_FILES_SKIP are for directories, and the last commit prevents them from matching the filenames within the directories.  Add wildcards so that they work as before.

--- mk/bsd.pkg.check.mk
+++ mk/bsd.pkg.check.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.pkg.check.mk,v 1.16 2005/09/25 00:02:27 kristerw Exp $
+# $NetBSD: bsd.pkg.check.mk,v 1.17 2005/09/25 00:34:12 kristerw Exp $
 #
 # This Makefile fragment is included by bsd.pkg.mk and defines the
 # relevant variables and targets for the various install-time "check"
@@ -58,13 +58,13 @@ USE_TOOLS+=		awk cat cmp diff echo find grep rm sed test	\
 CHECK_FILES_SKIP+=	${PREFIX}/${INFO_DIR}/dir
 .endif
 CHECK_FILES_SKIP+=	${PERL5_INSTALLARCHLIB}/perllocal.pod
-CHECK_FILES_SKIP+=	${PREFIX}/emul/linux/proc
-CHECK_FILES_SKIP+=	${PKG_DBDIR}.refcount
+CHECK_FILES_SKIP+=	${PREFIX}/emul/linux/proc.*
+CHECK_FILES_SKIP+=	${PKG_DBDIR}.refcount.*
 .for d in ${MAKE_DIRS} ${OWN_DIRS}
-CHECK_FILES_SKIP+=	${d:C/^([^\/])/${PREFIX}\/\1/}
+CHECK_FILES_SKIP+=	${d:C/^([^\/])/${PREFIX}\/\1/}.*
 .endfor
 .for d o g m in ${MAKE_DIRS_PERMS} ${OWN_DIRS_PERMS}
-CHECK_FILES_SKIP+=	${d:C/^([^\/])/${PREFIX}\/\1/}
+CHECK_FILES_SKIP+=	${d:C/^([^\/])/${PREFIX}\/\1/}.*
 .endfor
 _CHECK_FILES_SKIP_FILTER=	${GREP} -vx ${CHECK_FILES_SKIP:@f@-e ${f:Q}@}
 
