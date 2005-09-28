eeb28d15b5e0d48868d8e0d681ac1fd980eabfd2
rillig <rillig@pkgsrc.org>
2005-09-28 12:04:29 +0000

    Added RCS Id tags. Removed trailing empty line from patch-sg.

--- www/mozilla/gecko-depends.mk
+++ www/mozilla/gecko-depends.mk
@@ -1,3 +1,5 @@
+# $NetBSD: gecko-depends.mk,v 1.2 2005/09/28 12:04:29 rillig Exp $
+
 .if !empty(PKG_OPTIONS:Mfirefox)
 # firefox didn't have a pkgconfig file before 1.0.4
 BUILDLINK_DEPENDS.firefox+= firefox>=1.0.4
