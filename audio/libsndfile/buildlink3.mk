3b347872f8d00f1f9f684aceb3496afc7c091e84
adam <adam@pkgsrc.org>
2005-09-30 13:26:02 +0000

    Changes 1.0.12: * Add support for FLAC and Apple's Core Audio Format (CAF). * Add virtual I/O interface (still needs docs). * Cygwin and other Win32 fixes. * Minor bug fixes and cleanups.

--- audio/libsndfile/buildlink3.mk
+++ audio/libsndfile/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/10/03 00:13:07 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2005/09/30 13:26:02 adam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBSNDFILE_BUILDLINK3_MK:=	${LIBSNDFILE_BUILDLINK3_MK}+
@@ -15,5 +15,7 @@ BUILDLINK_DEPENDS.libsndfile+=		libsndfile>=1.0.0
 BUILDLINK_RECOMMENDED.libsndfile+=	libsndfile>=1.0.10nb1
 BUILDLINK_PKGSRCDIR.libsndfile?=	../../audio/libsndfile
 .endif	# LIBSNDFILE_BUILDLINK3_MK
+
+.include "../../audio/flac/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
