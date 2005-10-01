36867cb8c40094433811420bb0e1489ee8ea77ce
rh <rh@pkgsrc.org>
2005-10-01 04:18:47 +0000

    Initial import of gnustep-objc-lf2-2.95.3.88, an Objective-C base library required by libFoundation.

---
+++ devel/gnustep-objc-lf2/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/10/01 04:18:47 rh Exp $
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+GNUSTEP_OBJC_LF2_BUILDLINK3_MK:=	${GNUSTEP_OBJC_LF2_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gnustep-objc-lf2
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngnustep-objc-lf2}
+BUILDLINK_PACKAGES+=	gnustep-objc-lf2
+
+.if !empty(GNUSTEP_OBJC_LF2_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.gnustep-objc-lf2+=	gnustep-objc-lf2>=2.95.3.88
+BUILDLINK_PKGSRCDIR.gnustep-objc-lf2?=	../../devel/gnustep-objc-lf2
+BUILDLINK_TRANSFORM.gnustep-objc-lf2?=	-e "s|Headers/objc_lf2/|Headers/objc/|g"
+BUILDLINK_TRANSFORM+=	l:objc:objc_lf2
+.endif	# GNUSTEP_OBJC_LF2_BUILDLINK3_MK
+
+.include "../../devel/gnustep-make/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
