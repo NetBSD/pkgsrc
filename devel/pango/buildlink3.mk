1cb32b90cf96664110e3eaef87f307205678c2a0
jmmv <jmmv@pkgsrc.org>
2005-10-02 12:16:58 +0000

    Update to 1.10.0:
    overview of changes between 1.9.1 and 1.10.0
    ============================================
    * Update script tables, bidi tables, and Arabic joining tables for Unicode 4.1
      [Roozbeh Pournader, Behdad Esfahbod]
    * Misc bug and build fixes [Sebastien Bacher, Hans Breuer, Behdad,
      J. Ali Harlow, Tor Lillqvist, Keith Packard, Vittorio Palmisano, Manish Singh]
    
    overview of changes between 1.9.0 and 1.9.1
    ==========================================
    * Support setting of font options and DPI on PangoContext for
      the Cairo backend.
    * Adapt to recent Cairo API changes
    * Win32 build improvements [Tor Lillqvist]
    * Misc bug fixes [Hans Breuer, Thomas Fitzsimmons, Kazuki IWAMOTO]
    
    Overview of changes between 1.8.x and 1.9.x
    ===========================================
    * Add Cairo support; PangoCairoFontmap is an interface with
      implementations for Fontconfig fonts and Win32 fonts
    * Extend PangoFcFontmap to allow more customization by subclasses
    * Handle NULL language better for pango_context_get_metrics()
    * misc optimization [Paolo Borelli, Behdad Esfahbod]
    * Misc bug and build fixes [Hans Breuer, Damien Carbery,
      Keith Packard, Manish Singh]
    * Doc fixes [Tim Janik]

--- devel/pango/buildlink3.mk
+++ devel/pango/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/10/03 00:13:32 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2005/10/02 12:16:58 jmmv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PANGO_BUILDLINK3_MK:=	${PANGO_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	pango
 
 .if !empty(PANGO_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.pango+=	pango>=1.6.0
-BUILDLINK_RECOMMENDED.pango+=	pango>=1.6.0nb1
+BUILDLINK_RECOMMENDED.pango+=	pango>=1.10.0
 BUILDLINK_PKGSRCDIR.pango?=	../../devel/pango
 .endif	# PANGO_BUILDLINK3_MK
 
@@ -20,6 +20,7 @@ BUILDLINK_PKGSRCDIR.pango?=	../../devel/pango
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../fonts/Xft2/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../x11/Xrender/buildlink3.mk"
 
