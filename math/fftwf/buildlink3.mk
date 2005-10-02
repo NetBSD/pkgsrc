23a13ca7266b03a2e260496c6afaf27830469b69
wulf <wulf@pkgsrc.org>
2005-10-02 12:06:42 +0000

    New package fftwf-3.0.1: A single precision library version of math/fftw

---
+++ math/fftwf/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/10/02 12:06:42 wulf Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+FFTWF_BUILDLINK3_MK:=	${FFTWF_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	fftwf
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nfftwf}
+BUILDLINK_PACKAGES+=	fftwf
+
+.if !empty(FFTWF_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.fftwf+=	fftwf>=3.0
+BUILDLINK_RECOMMENDED.fftwf+=	fftwf>=3.0.1
+BUILDLINK_PKGSRCDIR.fftwf?=	../../math/fftwf
+.endif	# FFTWF_BUILDLINK3_MK
+
+.include "../../math/fftw/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
