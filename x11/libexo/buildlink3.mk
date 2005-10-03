f24e5f2fe9ce3bf5e9fb98335995224c551b0fd5
minskim <minskim>
2005-10-03 05:10:03 +0000

    Remove irrelevant comments and fix the package path.

--- x11/libexo/buildlink3.mk
+++ x11/libexo/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/10/03 04:51:33 minskim Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.9.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.exo?=	build
+# $NetBSD: buildlink3.mk,v 1.2 2005/10/03 05:10:03 minskim Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 EXO_BUILDLINK3_MK:=	${EXO_BUILDLINK3_MK}+
@@ -21,18 +11,12 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nexo}
 BUILDLINK_PACKAGES+=	exo
 
 .if !empty(EXO_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.exo+=	exo>=0.3.0
-BUILDLINK_PKGSRCDIR.exo?=	../../wip-kr/libexo
+BUILDLINK_DEPENDS.exo+=		exo>=0.3.0
+BUILDLINK_PKGSRCDIR.exo?=	../../x11/libexo
 .endif	# EXO_BUILDLINK3_MK
 
-# XXX
-# XXX Uncomment and keep only the buildlink3 lines below which are directly
-# XXX needed for dependencies to compile, link, and run.  If this package
-# XXX provides a wrappered API or otherwise does not expose the APIs of the
-# XXX buildlink3 lines below to dependencies, remove them.
-# XXX
-#.include "../../x11/gtk2/buildlink3.mk"
-#.include "../../x11/libxfce4gui/buildlink3.mk"
-#.include "../../x11/libxfce4util/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/libxfce4gui/buildlink3.mk"
+.include "../../x11/libxfce4util/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
