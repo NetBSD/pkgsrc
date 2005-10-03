f5cfb67a44078841db6c499fb7c8984ac97585ef
xtraeme <xtraeme@pkgsrc.org>
2005-10-03 14:24:29 +0000

    Apply patch from David A. Holland in PR pkg/31435 to honor rc.conf values for mysql_user, mysql_group and mysql_datadir options.
    Bump PKGREVISION.

--- databases/mysql4-server/files/mysqld.sh
+++ databases/mysql4-server/files/mysqld.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: mysqld.sh,v 1.2 2004/07/21 17:45:13 jlam Exp $
+# $NetBSD: mysqld.sh,v 1.3 2005/10/03 14:24:30 xtraeme Exp $
 #
 # PROVIDE: mysqld
 # REQUIRE: DAEMON LOGIN mountall
@@ -24,9 +24,9 @@ name="mysqld"
 rcvar=${name}
 command="@PREFIX@/bin/mysqld_safe"
 procname="@PREFIX@/libexec/${name}"
-mysqld_user="@MYSQL_USER@"
-mysqld_group="@MYSQL_GROUP@"
-mysqld_datadir="@MYSQL_DATADIR@"
+: ${mysqld_user:=@MYSQL_USER@}
+: ${mysqld_group:=@MYSQL_GROUP@}
+: ${mysqld_datadir=@MYSQL_DATADIR@}
 pidfile="${mysqld_datadir}/`@HOSTNAME_CMD@`.pid"
 
 extra_commands="initdb"
