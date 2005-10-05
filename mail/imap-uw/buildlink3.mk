2a02c627feea955a16b439fa3d265eff491998e1
salo <salo@pkgsrc.org>
2005-10-05 15:49:44 +0000

    Security fix for SA17062:
    "A vulnerability in UW-imapd can be exploited by malicious users to
     cause a DoS (Denial of Service) or compromise a vulnerable system.
    
     The vulnerability is caused due to a boundary error in the
     "mail_valid_net_parse_work()" function when copying the user supplied
     mailbox name to a stack buffer. This can be exploited to cause a
     stack-based buffer overflow via a specially crafted mailbox name that
     contains an single opening double-quote character, without the
     corresponding closing double-quote.
    
     Successful exploitation allows arbitrary code execution, but requires
     valid credentials on the IMAP server."
    
    http://secunia.com/advisories/17062/
    www.idefense.com/application/poi/display?id=313&type=vulnerabilities
    http://cve.mitre.org/cgi-bin/cvename.cgi?name=CAN-2005-2933
    
    Patch from 2004g.

--- mail/imap-uw/buildlink3.mk
+++ mail/imap-uw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/10/03 00:12:53 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2005/10/05 15:49:44 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 IMAP_UW_BUILDLINK3_MK:=	${IMAP_UW_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	imap-uw
 
 .if !empty(IMAP_UW_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.imap-uw+=	imap-uw>=2004
-BUILDLINK_RECOMMENDED.imap-uw+=	imap-uw>=2004anb2
+BUILDLINK_RECOMMENDED.imap-uw+=	imap-uw>=2004enb1
 BUILDLINK_PKGSRCDIR.imap-uw?=	../../mail/imap-uw
 #
 # Make "-limapuw" resolve into "-lc-client".
