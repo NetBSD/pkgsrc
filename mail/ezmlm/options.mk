07daddfb161a2893654ccb77c100eb58269f7456
schmonz <schmonz@pkgsrc.org>
2005-10-05 05:46:54 +0000

    Remove legacy DJB_ERRNO_HACK (replaced with djbware-errno-hack option).

--- mail/ezmlm/options.mk
+++ mail/ezmlm/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.5 2005/10/05 04:28:00 schmonz Exp $
+# $NetBSD: options.mk,v 1.6 2005/10/05 05:46:54 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ezmlm
 PKG_SUPPORTED_OPTIONS+=	ezmlm-althash
 
 .if ${OPSYS} == "Darwin"
-PKG_SUGGESTED_OPTIONS=	ezmlm-althash
+PKG_SUGGESTED_OPTIONS+=	ezmlm-althash
 .endif
 
 .include "../../mk/bsd.options.mk"
