5a9760a07400e84d18f0ff6f324ca220a59b0dc2
abs <abs@pkgsrc.org>
2005-10-07 10:28:34 +0000

    update exim to exim-4.52nb5 - if exim-build-eximon: also work with xorg and fix PLIST

--- mail/exim/options.mk
+++ mail/exim/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2005/10/03 20:20:18 abs Exp $
+# $NetBSD: options.mk,v 1.4 2005/10/07 10:28:34 abs Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.exim
 PKG_SUPPORTED_OPTIONS=	exim-build-eximon exim-content-scan exim-lookup-dnsdb
@@ -8,10 +8,13 @@ PKG_SUGGESTED_OPTIONS=	exim-content-scan exim-lookup-dsearch exim-old-demime
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_SRC=${PKGDIR}/PLIST
+
 .if !empty(PKG_OPTIONS:Mexim-build-eximon)
 LOCAL_MAKEFILE_OPTIONS+=EXIM_MONITOR=eximon.bin
+LOCAL_MAKEFILE_OPTIONS+=X11=${X11BASE}
 PLIST_SRC+=${PKGDIR}/PLIST.eximon
-USE_X11BASE=yes
+.include "../../mk/x11.buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mexim-content-scan)
