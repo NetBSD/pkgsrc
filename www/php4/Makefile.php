f64c8478810235750342f155ac39ec91601a1260
jdolecek <jdolecek@pkgsrc.org>
2005-10-08 20:34:26 +0000

    openssl is included unconditionally

--- www/php4/Makefile.php
+++ www/php4/Makefile.php
@@ -1,4 +1,4 @@
-# $NetBSD: Makefile.php,v 1.24 2005/10/08 16:49:35 jdolecek Exp $
+# $NetBSD: Makefile.php,v 1.25 2005/10/08 20:34:26 jdolecek Exp $
 
 .include "../../www/php4/Makefile.common"
 
@@ -21,6 +21,9 @@ CONFIGURE_ARGS+=	--with-regex=system
 CONFIGURE_ARGS+=	--enable-memory-limit
 CONFIGURE_ARGS+=	--enable-track-vars
 
+CONFIGURE_ARGS+=	--with-openssl
+.include "../../security/openssl/buildlink3.mk"
+
 # Support for linking some PHP4 extensions statically into the php CGI and
 # into the apache mod_php.so DSO.
 #
@@ -39,12 +42,5 @@ CONFIGURE_ENV+=		ac_cv_lib_pam_pam_start=no
 CONFIGURE_ENV+=		EXTENSION_DIR="${PREFIX}/${PHP_EXTENSION_DIR}"
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.${PKGNAME:C/-[^-]*$//}
-PKG_SUPPORTED_OPTIONS+=	ssl
-PKG_SUGGESTED_OPTIONS+=	ssl
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Mssl)
-.  include "../../security/openssl/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-openssl
-.endif
