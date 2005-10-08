e0e78ff7d00af97ddfc50fae1702dd1b779b0e85
jmmv <jmmv@pkgsrc.org>
2005-10-08 12:30:38 +0000

    For packages using libao, add a default dependency on a plugin (either oss or sun), based on the current OS name.  This is only used if no other libao plugin is installed, and is binary package friendly.
    Addresses PR pkg/30920 by Chuck Cranor.

--- audio/libao/buildlink3.mk
+++ audio/libao/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2005/05/20 19:20:55 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2005/10/08 12:30:38 jmmv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBAO_BUILDLINK3_MK:=	${LIBAO_BUILDLINK3_MK}+
@@ -14,6 +14,21 @@ BUILDLINK_PACKAGES+=	libao
 BUILDLINK_DEPENDS.libao+=	libao>=0.8.4
 BUILDLINK_RECOMMENDED.libao+=	libao>=0.8.4nb1
 BUILDLINK_PKGSRCDIR.libao?=	../../audio/libao
+
+.  if !defined(_LIBAO_BUILDING_PLUGIN)
+.    include "../../mk/bsd.prefs.mk"
+.    if ${OPSYS} != "Darwin" && ${OPSYS} != "Interix" && ${OPSYS} != "SunOS"
+_LIBAO_DEFAULT_PLUGIN=		oss
+.    elif ${OPSYS} == "NetBSD" || ${OPSYS} == "SunOS"
+_LIBAO_DEFAULT_PLUGIN=		sun
+.    else
+_LIBAO_DEFAULT_PLUGIN=
+.    endif
+
+.    if !empty(_LIBAO_DEFAULT_PLUGIN)
+DEPENDS+=	libao-[a-z]*-[0-9]*:../../audio/libao-${_LIBAO_DEFAULT_PLUGIN}
+.    endif
+.  endif
 .endif	# LIBAO_BUILDLINK3_MK
 
 PRINT_PLIST_AWK+=	/^@dirrm lib\/ao\/plugins-2$$/ \
