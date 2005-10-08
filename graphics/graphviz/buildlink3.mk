a3d15e23188f26d2eb447edf80dbaf9b7d53a8cf
adam <adam>
2005-10-08 08:11:10 +0000

    Changes 2.6: 	- experimentally moved source code management from CVS to GIT 	- added iterator functions to script bindings 	- more C-API tuning 	- add "-c" switch to dot to explicitly generate plugin "config" file 		instead of generating it as a side-effect of "dot -V" 	- better support for binary relocation. 	- plugin versioning and version checking 	- clean up of header files 	- provide statically linked "dot_static" (not incl. in rpms) 	- additional "event" support for GUIs (e.g. "DotEdit" graphviz-cairo) 	- add some information about plugins to "dot -v" output. 	- lefty/dotty fixes 	- fix bugs

--- graphics/graphviz/buildlink3.mk
+++ graphics/graphviz/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2005/04/28 14:58:06 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2005/10/08 08:11:10 adam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GRAPHVIZ_BUILDLINK3_MK:=	${GRAPHVIZ_BUILDLINK3_MK}+
@@ -16,11 +16,14 @@ BUILDLINK_RECOMMENDED.graphviz+=	graphviz>=1.12nb1
 BUILDLINK_PKGSRCDIR.graphviz?=	../../graphics/graphviz
 .endif	# GRAPHVIZ_BUILDLINK3_MK
 
+.include "../../devel/libltdl/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
+.include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/gd/buildlink3.mk"
 .include "../../graphics/jpeg/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
-.include "../../graphics/freetype2/buildlink3.mk"
-.include "../../x11/tk/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
+.include "../../x11/tk/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
