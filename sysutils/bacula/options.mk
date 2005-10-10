be17736ede78612a153c50b448ff46a3f27b15ae
wiz <wiz>
2005-10-10 15:27:45 +0000

    Add catalog-mysql option, from Geert Hendrickx on tech-pkg.

--- sysutils/bacula/options.mk
+++ sysutils/bacula/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2005/10/07 11:33:28 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2005/10/10 15:27:45 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.bacula
 PKG_OPTIONS_REQUIRED_GROUPS=	database
-PKG_OPTIONS_GROUP.database=	catalog-sqlite catalog-pgsql
+PKG_OPTIONS_GROUP.database=	catalog-sqlite catalog-pgsql catalog-mysql
 PKG_SUGGESTED_OPTIONS=		catalog-sqlite
 
 .include "../../mk/bsd.options.mk"
