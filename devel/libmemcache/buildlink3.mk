8cf87748e54d33f81f4595f5a3c879eecb7597f9
joerg <joerg@pkgsrc.org>
2005-10-11 15:31:20 +0000

    Initial import of libmemcache-1.4.0b9 from pkgsrc-wip. The pkgsrc-wip maintainer asked me to take maintainership due to lack of time.
    From DESCR:
    libmemcache is a C API for memcached, which is a high-performance,
    distributed memory object caching system, generic in nature, but
    intended for use in speeding up dynamic web applications by
    alleviating database load.

---
+++ devel/libmemcache/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/10/11 15:31:20 joerg Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBMEMCACHE_BUILDLINK3_MK:=	${LIBMEMCACHE_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libmemcache
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibmemcache}
+BUILDLINK_PACKAGES+=	libmemcache
+
+.if !empty(LIBMEMCACHE_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libmemcache+=	libmemcache>=1.4.0beta5
+BUILDLINK_PKGSRCDIR.libmemcache?=	../../devel/libmemcache
+.endif	# LIBMEMCACHE_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
