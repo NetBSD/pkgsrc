d4ab24d54408f9457070a3c69eebb25a37a6acd5
jmmv <jmmv@pkgsrc.org>
2005-10-12 21:49:39 +0000

    Update to 2.12.0.  Thanks go to the FreeBSD GNOME developers for all the patches I borrowed from them (patch-b*).
    September 4, 2005: Overview of changes from 2.11.92 to 2.12.0
    =============================================================
    * Updated translations.
    
    August 22, 2005: Overview of changes from 2.11.91 to 2.11.92
    ============================================================
    * Linux:
      - fixed for 2.6 without /sys
      - glibtop_get_cpu() : Do not add irq, softirq and iowait to idle.
    
    August 9, 2005: Overview of changes from 2.11.90 to 2.11.91
    ===========================================================
    * All:
      - cleaned some headers.
      - removed static examples.
    * Linux:
      - fixed some issues with 2.6 without /sys
    
    July 24, 2005: Overview of changes from 2.11.2 to 2.11.90
    =========================================================
    * Linux:
      - fixed glibtop_uptime .flags.
    
    July &3, 2005: Overview of changes from 2.11.1 to 2.11.2
    ========================================================
    * Linux:
      - little improvements for 2.6.
    
    July 3, 2005: Overview of changes from 2.11.0 to 2.11.1
    =======================================================
    * Linux:
      - glibtop_get_swap : added page(in|out) for linux 2.6.
    
    June 8, 2005: Overview of changes from 2.10.x to 2.11.0
    =======================================================
    * Replaced a lot of macros by functions
    * Got rid of dozens of useless files.
    * libgtop 2.11.x are and will stay API and ABI compatible with 2.10.x, i swear.

--- sysutils/libgtop2/buildlink3.mk
+++ sysutils/libgtop2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2005/03/22 16:03:37 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2005/10/12 21:49:39 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGTOP2_BUILDLINK3_MK:=	${LIBGTOP2_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgtop2}
 BUILDLINK_PACKAGES+=	libgtop2
 
 .if !empty(LIBGTOP2_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libgtop2+=	libgtop2>=2.10.0
+BUILDLINK_DEPENDS.libgtop2+=	libgtop2>=2.12.0
 BUILDLINK_PKGSRCDIR.libgtop2?=	../../sysutils/libgtop2
 .endif	# LIBGTOP2_BUILDLINK3_MK
 
