c03be61a2c647286a7e97df35bb41993ec4afdad
jmmv <jmmv@pkgsrc.org>
2005-10-12 21:47:45 +0000

    Bump dependency version, make this a build only dependency and fix an indentation problem.

--- textproc/gnome-doc-utils/buildlink3.mk
+++ textproc/gnome-doc-utils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/03/22 16:24:11 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/10/12 21:47:45 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_DOC_UTILS_BUILDLINK3_MK:=	${GNOME_DOC_UTILS_BUILDLINK3_MK}+
@@ -11,8 +11,9 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngnome-doc-utils}
 BUILDLINK_PACKAGES+=	gnome-doc-utils
 
 .if !empty(GNOME_DOC_UTILS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gnome-doc-utils+=	gnome-doc-utils>=0.1.3
+BUILDLINK_DEPMETHOD.gnome-doc-utils+=	build
+BUILDLINK_DEPENDS.gnome-doc-utils+=	gnome-doc-utils>=0.4.2
 BUILDLINK_PKGSRCDIR.gnome-doc-utils?=	../../textproc/gnome-doc-utils
 .endif	# GNOME_DOC_UTILS_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
