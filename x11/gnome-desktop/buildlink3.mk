f04a5272c737a34200fbeb69a0e08fb578b13d38
jmmv <jmmv>
2005-10-12 21:52:01 +0000

    Update to 2.12.1:
    ===============
    Version 2.12.1
    ===============
    
      Misc
    
    	* disable scrollkeeper for distcheck (Vincent)
    
      Translators
    
    	* Mahay Alam Khan (bn)
    	* Ivar Smolin (et)
    	* Iñaki Larrañaga (eu)
    	* Ignacio Casal Quinteiro (gl)
    	* Gil Osher (he)
    	* Baris Cicek (tr)
    	* Clytie Siddall (vi)
    
    ===============
    Version 2.12.0
    ===============
    
      Translators
    
    	* Vladimir "Kaladan" Petkov (bg)
    	* Jordi Mallach (ca)
    	* Gareth Bowker (cy)
    	* Hendrik Richter (de)
    	* Francisco Javier F. Serrador (es)
    	* Christophe Merlet (RedFox) (fr)
    	* Gabor Kelemen (hu)
    	* Norayr Chilingaryan (hy)
    	* Takeshi AIHANA (ja)
    	* Young-Ho Cha (ko)
    	* Erdal Ronahi (ku)
    	* Žygimantas Beručka (lt)
    	* Leonid Kanter (ru)
    	* Elian Myftiu (sq)
    	* Данило Шеган (sr)
    
    ===============
    Version 2.11.92
    ===============
    
      Docs Translations
    
    	* Tommi Vainikainen (fi)
    
      Translators
    
    	* Ankit Patel (gu)
    	* Gabor Kelemen (hu)
    	* Mohammad DAMT (id)
    	* Kjartan Maraas (nb, no)
    	* Gnome PL Team (pl)
    	* Evandro Fernandes Giovanini (pt_BR)
    	* Duarte Loreto (pt)
    	* Dan Damian (ro)
    	* Marcel Telka (sk)
    	* Prajasakti Localisation Team (te)
    	* Gheyret T.Kenji (ug)
    	* Maxim Dziumanenko (uk)
    	* Wang Jian (zh_CN)
    	* Abel Cheung (zh_TW)
    
    ===============
    Version 2.11.90
    ===============
    
      Misc
    
    	* Use gnome-doc-utils for building the help docs (Vincent Untz)
    	* Use GtkIconTheme for gnome_icon_theme_(find|get)_icon (Christian Persch, Mark)
    	* Use trademarked GNOME logo in gnome-about (Luca Cavalli)
    
      Translators
    
    	* Miloslav Trmac (cs)
    	* Adam Weinberger (en_CA)
    	* Francisco Javier F. Serrador (es)
    	* Ivar Smolin (et)
    	* Ilkka Tuohela (fi)
    	* Kjartan Maraas (nb, no)
    	* Tino Meinen (nl)
    	* Paisa Seeluangsawat (th)
    	* Clytie Siddall (vi)
    
    ==============
    Version 2.11.5
    ==============
    
      Misc
    
    	* Fix leak (Kjartan Maraas)
    
      Translators
    
    	* Данило Шеган (sr)
    
    ==============
    Version 2.11.4
    ==============
    
      Misc
    
    	* Fix leak (Kjartan Maraas)
    
      Translators
    
    	* Martin Willemoes Hansen (da)
    	* "Francisco Javier F. Serrador" (es)
    	* Marcel Telka (sk)
    	* Abel Cheung (zh_TW)
    
    ==============
    Version 2.11.3
    ==============
    
      Translators
    
    	* Ales Nyakhaychyk (be)
    	* Adam Weinberger (en_CA)
    	* Francisco Javier F. Serrador (es)
    	* Ivar Smolin (et)
    	* Iñaki Larrañaga (eu)
    	* Lilit Azizbekyan (hy)
    	* Takeshi AIHANA (ja)
    	* Kjartan Maraas (nb)
    	* Kjartan Maraas (no)
    	* Elian Myftiu (sq)
    	* Paisa Seeluangsawat (th)
    
    ==============
    Version 2.11.1
    ==============
    
      Misc
    
    	* Add gnome_desktop_item_new_from_basename() API (Dennis Cranston)
    	* Fix i18n inialization in GnomeDesktopItem (Frederic Crozat)
    
      Translators
    
    	* Vladimir \"Kaladan\" Petkov (bg)
    	* Miloslav Trmac (cs)
    	* Iñaki Larrañaga (eu)
    	* Ankit Patel (gu)
    	* Tino Meinen (nl)

--- x11/gnome-desktop/buildlink3.mk
+++ x11/gnome-desktop/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2005/08/21 11:26:47 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2005/10/12 21:52:01 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_DESKTOP_BUILDLINK3_MK:=	${GNOME_DESKTOP_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngnome-desktop}
 BUILDLINK_PACKAGES+=	gnome-desktop
 
 .if !empty(GNOME_DESKTOP_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gnome-desktop+=	gnome-desktop>=2.10.0
+BUILDLINK_DEPENDS.gnome-desktop+=	gnome-desktop>=2.12.1
 BUILDLINK_PKGSRCDIR.gnome-desktop?=	../../x11/gnome-desktop
 .endif	# GNOME_DESKTOP_BUILDLINK3_MK
 
