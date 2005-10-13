5361a3ba332fb3b1e9997185197463e2b415ac00
wiz <wiz>
2005-10-13 20:41:51 +0000

    libtoolize. Now installs a shared library too. Bump PKGREVISION.

--- devel/libmimedir/buildlink3.mk
+++ devel/libmimedir/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/10/13 15:15:28 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/10/13 20:41:51 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBMIMEDIR_BUILDLINK3_MK:=	${LIBMIMEDIR_BUILDLINK3_MK}+
@@ -11,9 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibmimedir}
 BUILDLINK_PACKAGES+=	libmimedir
 
 .if !empty(LIBMIMEDIR_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libmimedir+=	libmimedir>=0.4
+BUILDLINK_DEPENDS.libmimedir+=	libmimedir>=0.4nb1
 BUILDLINK_PKGSRCDIR.libmimedir?=	../../devel/libmimedir
-BUILDLINK_DEPMETHOD.libmimedir?=	build
 .endif	# LIBMIMEDIR_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
