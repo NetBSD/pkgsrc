fd4bcd09b404ae3d3adb32e7f07efe77fd9be15c
jmmv <jmmv>
2005-10-14 21:54:57 +0000

    Initial import of iso-codes, version 0.48:
    iso-codes provides the list of the country, language and currency names in
    one place, rather than repeated in many third-party programs.  This was
    originally done to centralize this data in Debian systems.
    
    The aim of iso-codes is to create a single "gettext" domain "iso-639" which
    contains the translations of language names, and one "iso-3166" listing the
    translations of country names.

---
+++ textproc/iso-codes/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/10/14 21:54:57 jmmv Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+ISO_CODES_BUILDLINK3_MK:=	${ISO_CODES_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	iso-codes
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Niso-codes}
+BUILDLINK_PACKAGES+=	iso-codes
+
+.if !empty(ISO_CODES_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.iso-codes+=	iso-codes>=0.48
+BUILDLINK_PKGSRCDIR.iso-codes?=	../../textproc/iso-codes
+.endif	# ISO_CODES_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
