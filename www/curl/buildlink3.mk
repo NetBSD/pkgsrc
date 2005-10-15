1c99ad01e8b39cde897f8545a026382bc832d67a
reed <reed>
2005-10-15 15:37:16 +0000

    Update to version 7.15.0.
    This is a security issue.
    
    http://curl.haxx.se/mail/lib-2005-10/0061.html
    
    Also update BUILDLINK_RECOMMENDED.curl.

--- www/curl/buildlink3.mk
+++ www/curl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2005/02/25 01:05:51 salo Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2005/10/15 15:37:16 reed Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CURL_BUILDLINK3_MK:=	${CURL_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	curl
 
 .if !empty(CURL_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.curl+=	curl>=7.12.3
-BUILDLINK_RECOMMENDED.curl+=	curl>=7.13.0nb1
+BUILDLINK_RECOMMENDED.curl+=	curl>=7.15
 BUILDLINK_PKGSRCDIR.curl?=	../../www/curl
 .endif	# CURL_BUILDLINK3_MK
 
