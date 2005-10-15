e2fc11009803c85bc5134d8518355c696485451f
reed <reed@pkgsrc.org>
2005-10-15 15:39:51 +0000

    Change BUILDLINK_RECOMMENDED.curl from 7.15 to real 7.15.0.

--- www/curl/buildlink3.mk
+++ www/curl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2005/10/15 15:37:16 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2005/10/15 15:39:51 reed Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CURL_BUILDLINK3_MK:=	${CURL_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	curl
 
 .if !empty(CURL_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.curl+=	curl>=7.12.3
-BUILDLINK_RECOMMENDED.curl+=	curl>=7.15
+BUILDLINK_RECOMMENDED.curl+=	curl>=7.15.0
 BUILDLINK_PKGSRCDIR.curl?=	../../www/curl
 .endif	# CURL_BUILDLINK3_MK
 
