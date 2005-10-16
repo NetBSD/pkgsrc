e9b3037fb2612abe13a47b37a97c20446277373d
taca <taca@pkgsrc.org>
2005-10-16 15:09:46 +0000

    Set USE_RUBY_DIST_SUBDIR to yes if it isn't defined yet.

--- lang/ruby/modules.mk
+++ lang/ruby/modules.mk
@@ -1,7 +1,12 @@
-# $NetBSD: modules.mk,v 1.10 2005/09/11 15:54:22 taca Exp $
+# $NetBSD: modules.mk,v 1.11 2005/10/16 15:09:46 taca Exp $
 
 .if !defined(_RUBY_MODULE_MK)
 _RUBY_MODULE_MK=	# defined
+
+#
+# default DIST_SUBDIR of ruby based packages
+#
+USE_RUBY_DIST_SUBDIR?=	yes
 
 .include "../../lang/ruby/rubyversion.mk"
 
