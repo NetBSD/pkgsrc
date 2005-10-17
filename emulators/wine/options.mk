315a5bdcc6e49d3f4ab01b79be0c145fb46482bb
adam <adam@pkgsrc.org>
2005-10-17 14:09:12 +0000

    Added options to use arts and sane

--- emulators/wine/options.mk
+++ emulators/wine/options.mk
@@ -1,10 +1,18 @@
-# $NetBSD: options.mk,v 1.1 2005/05/31 21:42:04 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2005/10/17 14:09:12 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.wine
-PKG_SUPPORTED_OPTIONS=	cups
+PKG_SUPPORTED_OPTIONS=	arts cups sane
 
 .include "../../mk/bsd.options.mk"
 
+.if !empty(PKG_OPTIONS:Marts)
+.include "../../audio/arts/buildlink3.mk"
+.endif
+
 .if !empty(PKG_OPTIONS:Mcups)
 .include "../../print/cups/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Msane)
+.include "../../graphics/sane-backends/buildlink3.mk"
 .endif
