8bd24a185700235b76f8c893ad36b12ce6b046e2
grant <grant@pkgsrc.org>
2005-10-18 04:32:01 +0000

    tcp_wrappers needs libnsl, libsocket for linking into an executable on Solaris.

--- security/tcp_wrappers/buildlink3.mk
+++ security/tcp_wrappers/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/10/03 00:18:13 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2005/10/18 04:32:01 grant Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 TCP_WRAPPERS_BUILDLINK3_MK:=	${TCP_WRAPPERS_BUILDLINK3_MK}+
@@ -15,5 +15,13 @@ BUILDLINK_DEPENDS.tcp_wrappers+=	tcp_wrappers>=7.6.1nb1
 BUILDLINK_RECOMMENDED.tcp_wrappers+=	tcp_wrappers>=7.6.1nb3
 BUILDLINK_PKGSRCDIR.tcp_wrappers?=	../../security/tcp_wrappers
 .endif	# TCP_WRAPPERS_BUILDLINK3_MK
+
+# need libnsl and libsocket to link into an executable on Solaris.
+.include "../../mk/bsd.prefs.mk"
+
+.if ${OPSYS} == "SunOS"
+BUILDLINK_LDADD.tcp_wrappers+=		-lnsl -lsocket
+BUILDLINK_LDFLAGS.tcp_wrappers?=	${BUILDLINK_LDADD.tcp_wrappers}
+.endif
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
