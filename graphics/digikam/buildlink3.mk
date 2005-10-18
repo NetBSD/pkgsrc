d18b313e655dc81240e2a5b2a89dcb6afa9ee8f3
seb <seb>
2005-10-18 09:44:09 +0000

    Update to version 0.7.4
    Note that DigiKam's documentation is now in the upcoming new package
    digikam-doc.
    
    Changes since last packaged version (0.7.2):
    
    version 0.7.4
    Updated translations
    
    version 0.7.3
    + Showfoto : add image properties dialog like in digiKam, add RMB menu, Move
    to trash/Delete File option, add setup dialog, add new option to toogle
    thumbbar in fullscreen mode, DigikamImagePlugins support, load/unload plugins
    dynamicly in according with setup, add statusbar, add splashscreen support,
    add resize image option, add Print capability, add Key shortcuts and toolbar
    configuration settings, add Edit/copy action, Undo/Redo actions, add showfoto
    icon, GUI improvements.
    
    + Image filters levels and curves adjustments : add option to see over-exposed
    pixels in preview effect (like with Acdsee(tm) tools).
    
    + Image histogram : color guide tool.
    
    + Image filters levels and curves adjustments : mouse tracking picker color
    like in Gimp and Photoshop(tm).
    
    + Patch from Alfons Hoogervorst about histogram visible while viewing photo in
    digiKam image editor and showfoto using blending.
    
    + digiKam Splashscreen from Andreas Fischer.
    
    + showfoto Splashscreen from Joern Ahrens.
    
    + Added a history based undo/redo function. The undo/redo icons in the toolbar
    are popup-menus, which describes the actions on the image.
    
    + Add a new button to the album properties which calculates
    the average date of the images in the album based on the EXIF
    headers or the modification date of the images if the exif
    headers are not available. It sets the date property of the album
    to the average date.
    
    + New digiKam image editor plugin for black and white conversion :
              - Since digikam include colors channel mixer, using this one for
              BW conversion. Give best results than old implementation. BW
              conversion are based on gimpguru.org tutorial
              (http://www.gimpguru.org/Tutorials/Color2BW).
              - This plugin has a dialog with preview effect.
              - B&W filters available are :  Neutral B&W, B&W with Green
              filter(NEW), B&W with Orange filter(NEW), B&W with Red
              filter(NEW), B&W with Yellow filter(NEW), Sepia,
              Brown, Cold, Selenium, Platinum.
    
    Bugs Fix : 103618 104005 103458 103515 105007 103147 105303 107155 103219
    103445 103282 103243 102195 102322 100991 100987 100472 98706

--- graphics/digikam/buildlink3.mk
+++ graphics/digikam/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2005/03/22 09:45:28 seb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2005/10/18 09:44:09 seb Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 DIGIKAM_BUILDLINK3_MK:=	${DIGIKAM_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ndigikam}
 BUILDLINK_PACKAGES+=	digikam
 
 .if !empty(DIGIKAM_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.digikam+=	digikam>=0.7.2
+BUILDLINK_DEPENDS.digikam+=	digikam>=0.7.4
 BUILDLINK_PKGSRCDIR.digikam?=	../../graphics/digikam
 .endif	# DIGIKAM_BUILDLINK3_MK
 
