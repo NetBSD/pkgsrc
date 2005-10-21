2f6c8386bfdfd4b15f8a40912c9cd37d4773b503
drochner <drochner@pkgsrc.org>
2005-10-21 17:31:55 +0000

    import the current development version of GIMP (2.3.4) for those who want to play with the new selection tool, color management or whatever

---
+++ graphics/gimp24/buildlink3.mk
@@ -0,0 +1,35 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/10/21 17:31:58 drochner Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GIMP_BUILDLINK3_MK:=	${GIMP_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gimp
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngimp}
+BUILDLINK_PACKAGES+=	gimp
+
+.if !empty(GIMP_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.gimp+=	gimp>=2.0.1nb1
+BUILDLINK_RECOMMENDED.gimp+=	gimp>=2.0.6nb1
+BUILDLINK_PKGSRCDIR.gimp?=	../../graphics/gimp
+.endif	# GIMP_BUILDLINK3_MK
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../graphics/aalib/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/lcms/buildlink3.mk"
+.include "../../graphics/libart2/buildlink3.mk"
+.include "../../graphics/libexif/buildlink3.mk"
+.include "../../graphics/librsvg2/buildlink3.mk"
+.include "../../graphics/mng/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+.include "../../graphics/libwmf/buildlink3.mk"
+.include "../../print/gimp-print-lib/buildlink3.mk"
+.include "../../www/libgtkhtml/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
