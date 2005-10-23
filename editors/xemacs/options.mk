dd1b115a22ff20213541031c158c0eae184170bf
rillig <rillig@pkgsrc.org>
2005-10-23 14:16:46 +0000

    Append things to CONFIGURE_ARGS instead of overwriting them. Found by pkglint.

--- editors/xemacs/options.mk
+++ editors/xemacs/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2005/03/03 16:16:03 uebayasi Exp $
+# $NetBSD: options.mk,v 1.5 2005/10/23 14:16:46 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xemacs
 PKG_SUPPORTED_OPTIONS=	ldap xface canna
@@ -6,24 +6,24 @@ PKG_SUPPORTED_OPTIONS=	ldap xface canna
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mldap)
-CONFIGURE_ARGS=	--with-ldap
+CONFIGURE_ARGS+=	--with-ldap
 .  include "../../databases/openldap/buildlink3.mk"
 .else
-CONFIGURE_ARGS=	--without-ldap
+CONFIGURE_ARGS+=	--without-ldap
 .endif
 
 .if !empty(PKG_OPTIONS:Mxface)
-CONFIGURE_ARGS=	--with-xface
+CONFIGURE_ARGS+=	--with-xface
 .  include "../../mail/faces/buildlink3.mk"
 .else
-CONFIGURE_ARGS=	--without-xface
+CONFIGURE_ARGS+=	--without-xface
 .endif
 
 .if !empty(PKG_OPTIONS:Mcanna)
 .  include "../../inputmethod/canna-lib/buildlink3.mk"
-CONFIGURE_ARGS=	--with-canna
+CONFIGURE_ARGS+=	--with-canna
 PLIST_SUBST+=	FOR_MULE=''
 .else
-CONFIGURE_ARGS=	--without-canna
+CONFIGURE_ARGS+=	--without-canna
 PLIST_SUBST+=	FOR_MULE='@comment '
 .endif
