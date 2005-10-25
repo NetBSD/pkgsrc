df5f8233d5abe60592eff0e7dc6a4c5dd9e949fd
scottr <scottr>
2005-10-25 17:52:38 +0000

    Add SOCKS4/SOCKS5 support.

--- mail/fetchmail/options.mk
+++ mail/fetchmail/options.mk
@@ -1,7 +1,9 @@
-# $NetBSD: options.mk,v 1.12 2005/09/28 21:55:32 rillig Exp $
+# $NetBSD: options.mk,v 1.13 2005/10/25 17:52:38 scottr Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fetchmail
 PKG_SUPPORTED_OPTIONS=	inet6 kerberos4 kerberos gssapi ssl
+PKG_OPTIONS_OPTIONAL_GROUPS=	socks
+PKG_OPTIONS_GROUP.socks=	socks4 socks5
 
 PKG_SUGGESTED_OPTIONS= ssl
 
@@ -55,4 +57,16 @@ CONFIGURE_ARGS+=        --with-kerberos5=no
 CONFIGURE_ARGS+=	--with-ssl=${SSLBASE}
 .else
 CONFIGURE_ARGS+=	--without-ssl
+.endif
+
+###
+### Include SOCKS firewall support
+###
+.if !empty(PKG_OPTIONS:Msocks4)
+.include "../../net/socks4/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-socks=${BUILDLINK_PREFIX.socks4}
+.endif
+.if !empty(PKG_OPTIONS:Msocks5)
+.include "../../net/socks5/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-socks5=${BUILDLINK_PREFIX.socks5}
 .endif
