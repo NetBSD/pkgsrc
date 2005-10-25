96a0725bb55f43cce943e3082e24bb4c6a3715f7
perry <perry@pkgsrc.org>
2005-10-25 17:17:42 +0000

    Fix so this does not depend on tcl83/tk83. Also update depends to 2.4z

--- x11/blt/buildlink3.mk
+++ x11/blt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/03/18 09:12:16 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2005/10/25 17:17:42 perry Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 BLT_BUILDLINK3_MK:=	${BLT_BUILDLINK3_MK}+
@@ -11,11 +11,11 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nblt}
 BUILDLINK_PACKAGES+=	blt
 
 .if !empty(BLT_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.blt+=		blt>=2.4o
+BUILDLINK_DEPENDS.blt+=		blt>=2.4z
 BUILDLINK_PKGSRCDIR.blt?=	../../x11/blt
 .endif	# BLT_BUILDLINK3_MK
 
-.include "../../lang/tcl83/buildlink3.mk"
-.include "../../x11/tk83/buildlink3.mk"
+.include "../../lang/tcl/buildlink3.mk"
+.include "../../x11/tk/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
