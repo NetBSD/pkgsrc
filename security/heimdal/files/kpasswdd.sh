a91aefa0a8c8659a8a1cfea8403a2603ebd3a593
jlam <jlam@pkgsrc.org>
2005-10-26 15:12:45 +0000

    Update security/heimdal to 0.7.1 (approved by lha).  We drop support for the "db4" option and just rely on the appropriate BDB_* settings via bdb.buildlink3.mk.  Also, we tweak the builtin.mk file so use krb5-config, if it's available, to check the version of the built-in heimdal.  Patches patch-ab, patch-ae and patch-af have been sent back upstream and will be incorporated into future Heimdal releases.
    Changes between version 0.6.5 and version 0.7.1 include:
    
     * Support for KCM, a process based credential cache
     * Support CCAPI credential cache
     * SPNEGO support
     * AES (and the gssapi conterpart, CFX) support
     * Adding new and improve old documentation
     * Bug fixes

--- security/heimdal/files/kpasswdd.sh
+++ security/heimdal/files/kpasswdd.sh
@@ -1,18 +1,20 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: kdc.sh,v 1.3 2005/10/26 15:12:45 jlam Exp $
+# $NetBSD: kpasswdd.sh,v 1.1 2005/10/26 15:12:45 jlam Exp $
 #
-# PROVIDE: kdc
+# PROVIDE: kpasswdd
 # REQUIRE: NETWORKING
 # BEFORE:  SERVERS
 
 . /etc/rc.subr
 
-name="kdc"
+name="kpasswdd"
 rcvar=$name
 command="@PREFIX@/libexec/${name}"
-command_args="--detach"
+command_args="& sleep 2"
 required_files="@PKG_SYSCONFDIR@/krb5.conf"
+required_vars="kdc"
 
 load_rc_config $name
+load_rc_config_var kdc kdc
 run_rc_command "$1"
