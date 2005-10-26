a2ead28d227b3570a0169271a7c03519b4d99a96
minskim <minskim@pkgsrc.org>
2005-10-26 04:22:38 +0000

    Update tcl-itcl to 3.2.1, a bug fix release. Also switch to Tcl/Tk 8.4.

--- lang/tcl-itcl/buildlink3.mk
+++ lang/tcl-itcl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:15:03 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/10/26 04:22:38 minskim Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 TCL_ITCL_BUILDLINK3_MK:=	${TCL_ITCL_BUILDLINK3_MK}+
@@ -11,12 +11,11 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ntcl-itcl}
 BUILDLINK_PACKAGES+=	tcl-itcl
 
 .if !empty(TCL_ITCL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.tcl-itcl+=	tcl-itcl>=3.2
-BUILDLINK_RECOMMENDED.tcl-itcl+=	tcl-itcl>=3.2nb2
+BUILDLINK_DEPENDS.tcl-itcl+=	tcl-itcl>=3.2.1
 BUILDLINK_PKGSRCDIR.tcl-itcl?=	../../lang/tcl-itcl
 .endif	# TCL_ITCL_BUILDLINK3_MK
 
-.include "../../lang/tcl83/buildlink3.mk"
-.include "../../x11/tk83/buildlink3.mk"
+.include "../../lang/tcl/buildlink3.mk"
+.include "../../x11/tk/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
