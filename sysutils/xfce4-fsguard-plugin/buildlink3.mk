a940c371292be40b725d1a698fff207f99a7ed42
martti <martti@pkgsrc.org>
2005-10-28 06:42:49 +0000

    Disk space plugin for XFce4.

---
+++ sysutils/xfce4-fsguard-plugin/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/10/28 06:42:49 martti Exp $
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+XFCE4_FSGUARD_PLUGIN_BUILDLINK3_MK:=	${XFCE4_FSGUARD_PLUGIN_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	xfce4-fsguard-plugin
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-fsguard-plugin}
+BUILDLINK_PACKAGES+=	xfce4-fsguard-plugin
+
+.if !empty(XFCE4_FSGUARD_PLUGIN_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.xfce4-fsguard-plugin+=	xfce4-fsguard-plugin>=0.2.1
+BUILDLINK_PKGSRCDIR.xfce4-fsguard-plugin?=	../../sysutils/xfce4-fsguard-plugin
+.endif	# XFCE4_FSGUARD_PLUGIN_BUILDLINK3_MK
+
+.include "../../x11/xfce4-panel/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
