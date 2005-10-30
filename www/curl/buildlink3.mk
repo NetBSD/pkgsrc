a5f8d2bf9055298810dfb8088451ddf709fc55a8
salo <salo>
2005-10-30 13:33:19 +0000

    Pullup ticket 862 - requested by Min Sik Kim security update for curl
    Revisions pulled up:
    - pkgsrc/www/curl/Makefile		1.57, 1.58
    - pkgsrc/www/curl/buildlink3.mk		1.9, 1.10
    - pkgsrc/www/curl/distinfo		1.39, 1.40
    - pkgsrc/www/curl/patches/patch-ab	1.40
    
       Module Name:		pkgsrc
       Committed By:	reed
       Date:		Sat Oct 15 15:37:16 UTC 2005
    
       Modified Files:
       	pkgsrc/www/curl: Makefile buildlink3.mk distinfo
    
       Log Message:
       Update to version 7.15.0.
    
       This is a security issue.
    
       http://curl.haxx.se/mail/lib-2005-10/0061.html
    
       Also update BUILDLINK_RECOMMENDED.curl.
    ---
       Module Name:		pkgsrc
       Committed By:	reed
       Date:		Sat Oct 15 15:39:51 UTC 2005
    
       Modified Files:
       	pkgsrc/www/curl: buildlink3.mk
    
       Log Message:
       Change BUILDLINK_RECOMMENDED.curl from 7.15
       to real 7.15.0.
    ---
       Module Name:		pkgsrc
       Committed By:	minskim
       Date:		Thu Oct 20 16:25:15 UTC 2005
    
       Modified Files:
       	pkgsrc/www/curl: Makefile distinfo
       Added Files:
       	pkgsrc/www/curl/patches: patch-ab
    
       Log Message:
       Make "curl-config --vernum" work again.  It was broken in 7.15.0.
       Bump PKGREVISION.

--- www/curl/buildlink3.mk
+++ www/curl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2005/02/25 01:05:51 salo Exp $
+# $NetBSD: buildlink3.mk,v 1.8.6.1 2005/10/30 13:33:19 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CURL_BUILDLINK3_MK:=	${CURL_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	curl
 
 .if !empty(CURL_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.curl+=	curl>=7.12.3
-BUILDLINK_RECOMMENDED.curl+=	curl>=7.13.0nb1
+BUILDLINK_RECOMMENDED.curl+=	curl>=7.15.0
 BUILDLINK_PKGSRCDIR.curl?=	../../www/curl
 .endif	# CURL_BUILDLINK3_MK
 
