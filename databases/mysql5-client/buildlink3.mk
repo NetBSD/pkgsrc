9c0043862a26de94db2939cf70ce2b6ac5d74475
xtraeme <xtraeme@pkgsrc.org>
2005-10-31 09:21:40 +0000

    Initial import of mysql5-client-5.0.15.
    MySQL is a SQL (Structured Query Language) database server.  SQL is the
    most popular database language in the world.  MySQL is a client-server
    implementation that consists of a server daemon `mysqld' and many
    different client programs/libraries.
    
    The main goals of MySQL are speed and robustness.
    
    The base upon which MySQL is built is a set of routines that have been
    used in a highly demanding production environment for many years.  While
    MySQL is still in development it already offers a rich and highly useful
    function set.
    
    The official way to pronounce 'MySQL' is 'My Ess Que Ell' (Not MY-SEQUEL).
    
    This package contains the MySQL client programs and libraries.

---
+++ databases/mysql5-client/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/10/31 09:21:40 xtraeme Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+MYSQL_CLIENT_BUILDLINK3_MK:=	${MYSQL_CLIENT_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	mysql-client
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmysql-client}
+BUILDLINK_PACKAGES+=	mysql-client
+
+.if !empty(MYSQL_CLIENT_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.mysql-client+=	mysql-client>=5.0.15
+BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql5-client
+BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
+BUILDLINK_INCDIRS.mysql-client?=	include/mysql
+.endif	# MYSQL_CLIENT_BUILDLINK3_MK
+
+.include "../../security/openssl/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
