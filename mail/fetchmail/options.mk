83ff3d721d0637c4eaf12dfc054e3ba7333353ac
salo <salo@pkgsrc.org>
2005-11-01 21:51:26 +0000

    Pullup ticket 877 - requested by Adrian Portelli security fix for fetchmailconf, portability fixes for fetchmail
    Revisions pulled up:
    - pkgsrc/mail/fetchmail/Makefile		1.154
    - pkgsrc/mail/fetchmail/distinfo		1.32, 1.33
    - pkgsrc/mail/fetchmail/options.mk		1.12, 1.13
    - pkgsrc/mail/fetchmail/patches/patch-ah	1.1
    - pkgsrc/mail/fetchmail/patches/patch-ai	1.1
    - pkgsrc/mail/fetchmail/patches/patch-aj	1.1
    - pkgsrc/mail/fetchmail/patches/patch-ak	1.1
    - pkgsrc/mail/fetchmailconf/Makefile		1.65
    
       Module Name:		pkgsrc
       Committed By:	rillig
       Date:		Wed Sep 28 21:55:32 UTC 2005
    
       Modified Files:
       	pkgsrc/mail/fetchmail: options.mk
    
       Log Message:
       Replaced "# defined" with "yes" in Makefile variables like GNU_CONFIGURE,
       NO_BUILD, USE_LIBTOOL.
    ---
       Module Name:		pkgsrc
       Committed By:	scottr
       Date:		Tue Oct 25 17:52:38 UTC 2005
    
       Modified Files:
       	pkgsrc/mail/fetchmail: options.mk
    
       Log Message:
       Add SOCKS4/SOCKS5 support.
    ---
       Module Name:		pkgsrc
       Committed By:	tonio
       Date:		Fri Oct 21 20:56:50 UTC 2005
    
       Modified Files:
       	pkgsrc/mail/fetchmail: Makefile distinfo
       Added Files:
       	pkgsrc/mail/fetchmail/patches: patch-ah patch-ai patch-aj
    
       Log Message:
       Fix mail/fetchmail under darwin (PR 28543).
       The added patches add a prefix "fm_" to lock related finctions, to avoid
       name clash with darwin lock functions. Link with -lresolv under darwin.
       (thanks scole_at_sdf.lonestar.org for the patches)
       Bump PKGREVISION
    ---
       Module Name:		pkgsrc
       Committed By:	adrianp
       Date:		Tue Nov  1 19:16:52 UTC 2005
    
       Modified Files:
       	pkgsrc/mail/fetchmail: distinfo
       Added Files:
       	pkgsrc/mail/fetchmail/patches: patch-ak
    
       Log Message:
       Add patch-ak for a fetchmailconf security issue.  This patch does
       not impact the fetchmail package so no version bump is required.
    ---
       Module Name:		pkgsrc
       Committed By:	adrianp
       Date:		Tue Nov  1 19:17:41 UTC 2005
    
       Modified Files:
       	pkgsrc/mail/fetchmailconf: Makefile
    
       Log Message:
       Bump to nb3 for security patch

--- mail/fetchmail/options.mk
+++ mail/fetchmail/options.mk
@@ -1,7 +1,9 @@
-# $NetBSD: options.mk,v 1.11 2005/05/30 18:06:37 frueauf Exp $
+# $NetBSD: options.mk,v 1.11.4.1 2005/11/01 21:51:26 salo Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fetchmail
 PKG_SUPPORTED_OPTIONS=	inet6 kerberos4 kerberos gssapi ssl
+PKG_OPTIONS_OPTIONAL_GROUPS=	socks
+PKG_OPTIONS_GROUP.socks=	socks4 socks5
 
 PKG_SUGGESTED_OPTIONS= ssl
 
@@ -27,7 +29,7 @@ CONFIGURE_ARGS+=	--with-gssapi=no
 ### KPOP (POP3 + Kerberos IV) support.
 ###
 .if !empty(PKG_OPTIONS:Mkerberos4)
-PKG_USE_KERBEROS=	# defined
+PKG_USE_KERBEROS=	yes
 CONFIGURE_ARGS+=	--with-kerberos=yes
 .  if empty(MACHINE_PLATFORM:MNetBSD-1.[0-4]*-i386)
 REPLACE_KERBEROS_LIBS=	yes
@@ -41,7 +43,7 @@ CONFIGURE_ARGS+=	--with-kerberos=no
 ###
 .if !empty(PKG_OPTIONS:Mkerberos) || !empty(PKG_OPTIONS:Mgssapi)
 .  include "../../mk/krb5.buildlink3.mk"
-PKG_USE_KERBEROS=	# defined
+PKG_USE_KERBEROS=	yes
 CONFIGURE_ARGS+=        --with-kerberos5=${KRB5BASE}
 .else
 CONFIGURE_ARGS+=        --with-kerberos5=no
@@ -55,4 +57,16 @@ CONFIGURE_ARGS+=        --with-kerberos5=no
 CONFIGURE_ARGS+=	--with-ssl=${SSLBASE}
 .else
 CONFIGURE_ARGS+=	--without-ssl
+.endif
+
+###
+### Include SOCKS firewall support
+###
+.if !empty(PKG_OPTIONS:Msocks4)
+.include "../../net/socks4/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-socks=${BUILDLINK_PREFIX.socks4}
+.endif
+.if !empty(PKG_OPTIONS:Msocks5)
+.include "../../net/socks5/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-socks5=${BUILDLINK_PREFIX.socks5}
 .endif
