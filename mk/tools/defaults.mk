5fcf9851336d2a4a5036d21c6717d71dd8e4ada4
jlam <jlam>
2005-11-04 20:02:01 +0000

    Teach the tools framework about "sleep" which can be provided by the sysutils/coreutils package.

--- mk/tools/defaults.mk
+++ mk/tools/defaults.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defaults.mk,v 1.39 2005/11/04 19:28:33 jlam Exp $
+# $NetBSD: defaults.mk,v 1.40 2005/11/04 20:02:01 jlam Exp $
 #
 # Copyright (c) 2005 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -114,6 +114,7 @@ _TOOLS_VARNAME.rmdir=		RMDIR
 _TOOLS_VARNAME.sed=		SED
 _TOOLS_VARNAME.sh=		SH
 _TOOLS_VARNAME.shlock=		SHLOCK
+_TOOLS_VARNAME.sleep=		SLEEP
 _TOOLS_VARNAME.sort=		SORT
 _TOOLS_VARNAME.tail=		TAIL
 _TOOLS_VARNAME.tar=		TAR
