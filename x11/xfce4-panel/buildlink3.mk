b3f095baa143f9f07abfa2fa2c664fc9739dbe06
martti <martti>
2005-11-09 06:42:58 +0000

    Updated xfce4 to 4.2.3.1
    * Lots of bug fixes
    * Translation updates
    
    Official ChangeLog is available at:
    
    http://www.xfce.org/release_notes/4.2.3.1_changelog.html

--- x11/xfce4-panel/buildlink3.mk
+++ x11/xfce4-panel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2005/05/18 06:10:31 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2005/11/09 06:42:59 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XFCE4_PANEL_BUILDLINK3_MK:=	${XFCE4_PANEL_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-panel}
 BUILDLINK_PACKAGES+=	xfce4-panel
 
 .if !empty(XFCE4_PANEL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.xfce4-panel+=	xfce4-panel>=4.2.2
+BUILDLINK_DEPENDS.xfce4-panel+=	xfce4-panel>=4.2.3
 BUILDLINK_PKGSRCDIR.xfce4-panel?=	../../x11/xfce4-panel
 .endif	# XFCE4_PANEL_BUILDLINK3_MK
 
