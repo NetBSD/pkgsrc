32ae03b60b88e048f58d21fac0e3a524b68ea215
heinz <heinz>
2005-11-13 22:48:32 +0000

    Updated to version 3.1.0.
    Pkgsrc changes:
      - p5-Storable is no longer a necessary.
      - Let DragonFlyBSD also use the rc.d script (patch-ad).
      - Sa-update needs p5-libwww (for LWP::UserAgent, HTTP::Date),
        p5-Archive-Tar and p5-IO-Zlib.
      - Many of the plugins are available as pkgsrc packages (p5-Mail-SPF-Query,
        p5-IP-Country, p5-Net-Ident, ...) but are not required.
      - Renamed some options to follow the naming conventions described in the
        pkgsrc guide.
      - Removed patch-ax again; it is already incorporated in 3.1.0.
    
      - Reworked DESCR to use less than 25 lines.
      - Removed SPAMASSASSIN_VERSION for clarity of DISTNAME and PKGNAME.
      - Prepended variables internal to the package with an underscore.
      - Rearranged MAKE_PARAMS alphabetically.
      - Simplified some internal variables (concatenation instead of
        substitution: _EGDIR, _DOCDIR,...)
      - Loop variables use all lower-case now.
      - Added a rule to lower score for mail from pkgsrc-bugs in netbsd_lists.cf.
      - The test t/spf.t (fails for SPF_HELO_*) has a know problem (SA Bug 4685).
    
    Relevant changes since version 3.0.4:
    =====================================
    - Apache preforking algorithm adopted; number of spamd child processes is now
      scaled, according to demand.  This provides better VM behaviour when not
      under peak load.
    
    - Inclusion of sa-update script which will allow for updates of rules and
      scores in between code releases.
    
    - added PostgreSQL, MySQL 4.1+, and local SDBM file Bayes storage modules. SQL
      storage is now recommended for Bayes, instead of DB_File. NDBM_File support
      has been dropped due to a major bug in that module.
    
    - detect legitimate SMTP AUTH submission, to avoid false positives on
      Dynablock-style rules.
    
    - new Advance Fee Fraud (419 scam) rules.
    
    - removed use of the Storable module, due to several reported hangs on SMP
      Linux machines.
    
    - Converted several rule/engine components into Plugins such as:
      AccessDB, AWL, Pyzor, Razor2, DCC, Bayes AutoLearn Determination, etc.
    
    - new plugins: DomainKeys (off by default), MIMEHeader: a new plugin to perform
      tests against header in internal MIME structure, ReplaceTags: plugin by Felix
      Bauer to support fuzzy text matching, WhiteListSubject: plugin added to
      support user whitelists by Subject header.
    
    - TextCat language guesser moved to a plugin.  (This means "ok_languages"
      is no longer part of the core engine by default.)
    
    - Razor: disable Razor2 support by default per our policy, since the
      service is not free for non-personal use.  It's trivial to reenable.
    
    - DCC: disable DCC for similar reasons, due to new license terms.
    
    - Net::DNS bug: high load caused answer packets to be mixed up and delivered as
      answers to the wrong request, causing false positives.  worked around.
    
    - DNSBL lookups and other DNS operations are now more efficient, by using a
      custom single-socket event-based model instead of Net::DNS.
    
    - add support for accreditation services, including Habeas v2.
    
    - better URI parsing -- many evasion tricks now caught.
    
    - URIBL lookups are prioritized based on the location in the message
      the URI was found.
    
    - mass-check now supports reusing realtime DNSBL hit results, and sample-based
      Bayes autolearning emulation, to reduce complexity.
    
    - sa-learn, spamassassin and mass-check now have optional progress bars.
    
    - modify header ordering for DomainKeys compatibility, by placing markup
      headers at the top of the message instead at the bottom of the list.
    
    - spamd/spamc now support remote Bayes training, and reporting spam.
    
    - spamc now supports reading its flags from a configuration file using the -F
      switch, contributed by John Madden.
    
    - added SPF-based whitelisting.
    
    - Polish rules contributed by Radoslaw Stachowiak.
    
    - many rule changes and additions.

--- mail/spamassassin/options.mk
+++ mail/spamassassin/options.mk
@@ -1,10 +1,99 @@
-# $NetBSD: options.mk,v 1.3 2005/11/04 17:20:09 rillig Exp $
+# $NetBSD: options.mk,v 1.4 2005/11/13 22:48:32 heinz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.spamassassin
-PKG_SUPPORTED_OPTIONS=	ssl net-tests perl-warnings perl-taint-checks \
-			awl-sql-tests bayes-sql-tests
-PKG_SUGGESTED_OPTIONS=	perl-taint-checks perl-warnings ssl
+PKG_SUPPORTED_OPTIONS=	\
+			spamassassin-perl-warnings \
+			spamassassin-taint-checks \
+			spamassassin-test-awl-sql \
+			spamassassin-test-bayes-sql \
+			spamassassin-test-net \
+			spamassassin-test-prefork \
+			ssl
+PKG_OPTIONS_LEGACY_OPTS+= \
+			awl-sql-tests:spamassassin-test-awl-sql \
+			bayes-sql-tests:spamassassin-test-bayes-sql \
+			net-tests:spamassassin-test-net \
+			perl-taint-checks:spamassassin-taint-checks \
+			perl-warnings:spamassassin-perl-warnings
+
+#
+# Default options
+#
+PKG_SUGGESTED_OPTIONS=	spamassassin-taint-checks \
+			spamassassin-perl-warnings \
+			ssl
 .include "../../mk/bsd.options.mk"
+
+#
+# change t/config according to all the options
+#
+SUBST_CLASSES+=		satests
+SUBST_STAGE.satests=	post-configure
+SUBST_FILES.satests=	t/config
+SUBST_SED.satests=	\
+	-e s!awl_sql_tests=n!awl_sql_tests=${_AWL_SQL_TEST}! \
+	-e s!bayes_sql_tests=n!bayes_sql_tests=${_BAYES_SQL_TEST}! \
+	-e s!run_net_tests=n!run_net_tests=${_NET_TEST}! \
+	-e s!prefork_stress_test=n!prefork_stress_test=${_PREFORK_TEST}!
+
+#
+# Enable tests of the SQL storage module for the automatic whitelist
+# (AWL). This option enables interactive questions about the database
+# configuration during 'configure' phase.
+#
+.if !empty(PKG_OPTIONS:Mspamassassin-test-awl-sql)
+_AWL_SQL_TEST=y
+.else
+_AWL_SQL_TEST=n
+.endif
+
+#
+# Enable tests of the SQL storage module for Bayes information.
+# This option enables interactive questions about the database
+# configuration during 'configure' phase.
+#
+.if !empty(PKG_OPTIONS:Mspamassassin-test-bayes-sql)
+_BAYES_SQL_TEST=y
+.else
+_BAYES_SQL_TEST=n
+.endif
+
+#
+# Enable Internet based tests during 'make test' (Razor, Pyzor, etc.)
+#
+.if !empty(PKG_OPTIONS:Mspamassassin-test-net)
+_NET_TEST=y
+.else
+_NET_TEST=n
+.endif
+
+#
+# Enable Perl warnings in spamd and spamassassin
+#
+.if !empty(PKG_OPTIONS:Mspamassassin-perl-warnings)
+MAKE_PARAMS+=		PERL_WARN=yes
+.else
+MAKE_PARAMS+=		PERL_WARN=no
+.endif
+
+#
+# Enable Perl taint checks in spamd and spamassassin to improve security
+#
+.if !empty(PKG_OPTIONS:Mspamassassin-taint-checks)
+MAKE_PARAMS+=		PERL_TAINT=yes
+.else
+MAKE_PARAMS+=		PERL_TAINT=no
+.endif
+
+#
+# Enable spamd prefork stress test (will kill any processes containing the
+# string 'spam child' in their process title).
+#
+.if !empty(PKG_OPTIONS:Mspamassassin-test-prefork)
+_PREFORK_TEST=y
+.else
+_PREFORK_TEST=n
+.endif
 
 #
 # Build spamc and spamd with support for connections over SSL
@@ -17,55 +106,4 @@ MAKE_PARAMS+=		ENABLE_SSL=yes
 .else
 CONFIGURE_ARGS+=	--disable-ssl
 MAKE_PARAMS+=		ENABLE_SSL=no
-.endif
-
-#
-# Enable Internet based tests during 'make test' (Razor, Pyzor, etc.)
-#
-.if !empty(PKG_OPTIONS:Mnet-tests)
-MAKE_PARAMS+=		RUN_NET_TESTS=yes
-.else
-MAKE_PARAMS+=		RUN_NET_TESTS=no
-.endif
-
-#
-# Enable Perl warnings in spamd and spamassassin
-#
-.if !empty(PKG_OPTIONS:Mperl-warnings)
-MAKE_PARAMS+=		PERL_WARN=yes
-.else
-MAKE_PARAMS+=		PERL_WARN=no
-.endif
-
-#
-# Enable Perl taint checks in spamd and spamassassin to improve security
-#
-.if !empty(PKG_OPTIONS:Mperl-taint-checks)
-MAKE_PARAMS+=		PERL_TAINT=yes
-.else
-MAKE_PARAMS+=		PERL_TAINT=no
-.endif
-
-#
-# Enable tests of the SQL storage module for the automatic whitelist
-# (AWL). This option enables interactive questions about the database
-# configuration during 'configure' phase.
-#
-.if !empty(PKG_OPTIONS:Mawl-sql-tests)
-MAKE_PARAMS+=		RUN_AWL_SQL_TESTS=yes
-INTERACTIVE_STAGE+=	configure
-.else
-MAKE_PARAMS+=		RUN_AWL_SQL_TESTS=no
-.endif
-
-#
-# Enable tests of the SQL storage module for Bayes information.
-# This option enables interactive questions about the database
-# configuration during 'configure' phase.
-#
-.if !empty(PKG_OPTIONS:Mbayes-sql-tests)
-MAKE_PARAMS+=		RUN_BAYES_SQL_TESTS=yes
-INTERACTIVE_STAGE+=	configure
-.else
-MAKE_PARAMS+=		RUN_BAYES_SQL_TESTS=no
 .endif
