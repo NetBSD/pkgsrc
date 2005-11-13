6f528580bfd808c7e34d35731c91ea9d6689feb8
adrianp <adrianp@pkgsrc.org>
2005-11-13 22:58:05 +0000

    Fix commented description for ziplinks

--- chat/ircd-hybrid/options.mk
+++ chat/ircd-hybrid/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2005/11/13 22:40:00 adrianp Exp $
+# $NetBSD: options.mk,v 1.2 2005/11/13 22:58:05 adrianp Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ircd-hybrid
 PKG_SUPPORTED_OPTIONS=	efnet ssl halfops small-net ziplinks
@@ -42,7 +42,7 @@ CONFIGURE_ARGS+=	--enable-small-net
 .endif
 
 ###
-### Enable OpenSSL support
+### Compress data before transmitting it between servers
 ###
 .if !empty(PKG_OPTIONS:Mziplinks)
 .	include "../../devel/zlib/buildlink3.mk"
