fc81d8a1c2e0e585a6046e9de0714e7c6389ab7a
tv <tv@pkgsrc.org>
2005-11-13 01:17:40 +0000

    Remove navigator3.  It's no longer useful, and shouldn't be used by anyone at this point.  (Mentioned on tech-pkg with no objections.)

--- www/navigator3/files/netscape.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export XKEYSYMDB ; XKEYSYMDB=@X11BASE@/lib/X11/XKeysymDB
-export XNLSPATH  ; XNLSPATH=@PREFIX@/share/netscape/nls
-export XAPPLRESDIR ; XAPPLRESDIR=@PREFIX@/lib/X11/app-defaults
-export CLASSPATH ; CLASSPATH=@PREFIX@/share/netscape/java/java_301
-exec @EXECDIR@/netscape3.bin "$@"
