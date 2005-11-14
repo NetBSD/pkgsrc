a015d1831c2fd80a5fd62e593af3d10a17c586ee
joerg <joerg>
2005-11-14 22:53:09 +0000

    When linking against libpython, the dependencies of libpython might be needed as well. This is especially the case when lib/pythonXconfig/Makefile is used as done e.g. www/ap2-python.

--- lang/python24/buildlink3.mk
+++ lang/python24/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/12/05 23:27:45 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/11/14 22:53:09 joerg Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PYTHON24_BUILDLINK3_MK:=	${PYTHON24_BUILDLINK3_MK}+
@@ -25,3 +25,7 @@ BUILDLINK_TRANSFORM+=		l:python:python2.4
 .endif	# PYTHON24_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+
+.include "../../mk/dlopen.buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
