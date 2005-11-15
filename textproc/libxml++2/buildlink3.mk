95406a456b0a94d007864ce659ceb901a26bd8e2
wiz <wiz>
2005-11-15 18:54:08 +0000

    Fix path to itself.

--- textproc/libxml++2/buildlink3.mk
+++ textproc/libxml++2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/11/12 12:35:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/11/15 18:54:08 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBXML++2_BUILDLINK3_MK:=	${LIBXML++2_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libxml++2
 
 .if !empty(LIBXML++2_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libxml++2+=	libxml++2>=2.10.0
-BUILDLINK_PKGSRCDIR.libxml++2?=	../../wip/libxml++2
+BUILDLINK_PKGSRCDIR.libxml++2?=	../../textproc/libxml++2
 .endif	# LIBXML++2_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
