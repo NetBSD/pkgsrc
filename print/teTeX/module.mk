0e782ae69503b48cd0017aa5fe3504662738a40a
minskim <minskim@pkgsrc.org>
2005-11-17 00:42:14 +0000

    Replace TEXFONTMAPS with TEX_FONTMAPS for consistency with other TEX_* variables in mk/tex.buildlink3.mk and better readability.

--- print/teTeX/module.mk
+++ print/teTeX/module.mk
@@ -1,4 +1,4 @@
-# $NetBSD: module.mk,v 1.2 2005/11/15 07:13:19 minskim Exp $
+# $NetBSD: module.mk,v 1.3 2005/11/17 00:42:14 minskim Exp $
 #
 # This Makefile fragment is intended to be included by packages that
 # install TeX packages.  It takes care of rebuilding the ls-R database
@@ -9,20 +9,20 @@
 #    TEXMFDIRS - A list of TEXMF directories that need to update ls-R.
 #                Default: ${PKG_LOCALTEXMFPREFIX}
 #
-#    TEXFONTMAPS - A list of font map files
+#    TEX_FONTMAPS - A list of font map files
 #
 
 .if !defined(TEX_PACKAGE_MK)
 TEX_PACKAGE_MK=		# defined
 
 TEXMFDIRS?=		${PKG_LOCALTEXMFPREFIX}
-TEXFONTMAPS?=		# empty
+TEX_FONTMAPS?=		# empty
 
 USE_PKGINSTALL=		yes
 FILES_SUBST+=		MKTEXLSR="${LOCALBASE}/bin/mktexlsr"
 FILES_SUBST+=		TEXMFDIRS="${TEXMFDIRS}"
 FILES_SUBST+=		UPDMAP_SYS="${LOCALBASE}/bin/updmap-sys"
-FILES_SUBST+=		TEXFONTMAPS="${TEXFONTMAPS}"
+FILES_SUBST+=		TEX_FONTMAPS="${TEX_FONTMAPS}"
 INSTALL_EXTRA_TMPL+=	../../print/teTeX/files/texmf.tmpl
 DEINSTALL_EXTRA_TMPL+=	../../print/teTeX/files/texmf.tmpl
 
