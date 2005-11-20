71845fbd0a22b9d0959050b303b0e43a9818a6a2
minskim <minskim>
2005-11-20 19:14:50 +0000

    - Install a sample config file into share/examples, rather than into   the texmf tree. - Correct paths to this package.
    No PKGREVISION bump since this package didn't build due to the wrong
    path to map.mk.

--- print/dvipdfmx/map.mk
+++ print/dvipdfmx/map.mk
@@ -1,4 +1,4 @@
-# $NetBSD: map.mk,v 1.1 2005/11/20 18:51:54 minskim Exp $
+# $NetBSD: map.mk,v 1.2 2005/11/20 19:14:50 minskim Exp $
 # This Makefile fragment is intended to be included by packages that
 # install font maps used by dvipdfmx.  It takes care of adding and
 # removing font map entires in dvipdfmx.cfg.
@@ -15,14 +15,14 @@ DVIPDFMX_MAP_MK=	# defined
 DVIPDFMX_FONTMAPS?=	# empty
 
 .if empty(DISTNAME:Mdvipdfmx-[0-9]*)
-DEPENDS+=		dvipdfmx>=0.0.0.20050627:../../wip-kr/dvipdfmx
+DEPENDS+=		dvipdfmx>=0.0.0.20050627:../../print/dvipdfmx
 .endif
 
 USE_PKGINSTALL=		yes
 FILES_SUBST+=		DVIPDFMX_FONTMAPS="${DVIPDFMX_FONTMAPS}"
 FILES_SUBST+=		DVIPDFMX_CONFIG_DIR="${PKG_LOCALTEXMFPREFIX}/dvipdfm/config"
-INSTALL_EXTRA_TMPL+=	../../wip-kr/dvipdfmx/files/map.tmpl
-DEINSTALL_EXTRA_TMPL+=	../../wip-kr/dvipdfmx/files/map.tmpl
+INSTALL_EXTRA_TMPL+=	../../print/dvipdfmx/files/map.tmpl
+DEINSTALL_EXTRA_TMPL+=	../../print/dvipdfmx/files/map.tmpl
 
 PRINT_PLIST_AWK+=	/^${PKG_LOCALTEXMFPREFIX:S|${PREFIX}/||:S|/|\\/|g}\/dvidpfm\/config\/dvipdfmx.cfg$$/ \
 			{ next; }
