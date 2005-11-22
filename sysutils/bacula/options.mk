51ece4bd779c9e40653d04a4695f42f6286c88d2
tv <tv@pkgsrc.org>
2005-11-22 19:48:32 +0000

    Add sqlite3 database option (catalog-sqlite3).  Unfortunately it's a completely separate option as SQLite 3.x is not backwards compatible with SQLite 1 and 2.

--- sysutils/bacula/options.mk
+++ sysutils/bacula/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.5 2005/11/22 19:33:06 tv Exp $
+# $NetBSD: options.mk,v 1.6 2005/11/22 19:48:32 tv Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.bacula
 PKG_OPTIONS_REQUIRED_GROUPS=	database
-PKG_OPTIONS_GROUP.database=	catalog-sqlite catalog-pgsql catalog-mysql
+PKG_OPTIONS_GROUP.database=	catalog-sqlite catalog-sqlite3 catalog-pgsql catalog-mysql
 PKG_SUGGESTED_OPTIONS=		catalog-sqlite
 
 .include "../../mk/bsd.options.mk"
@@ -11,6 +11,10 @@ PKG_SUGGESTED_OPTIONS=		catalog-sqlite
 .  include "../../databases/sqlite/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-sqlite=${BUILDLINK_PREFIX.sqlite}
 BACULA_DB=		sqlite
+.elif !empty(PKG_OPTIONS:Mcatalog-sqlite3)
+.  include "../../databases/sqlite3/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-sqlite3=${BUILDLINK_PREFIX.sqlite3}
+BACULA_DB=		sqlite3
 .elif !empty(PKG_OPTIONS:Mcatalog-pgsql)
 .  include "../../mk/pgsql.buildlink3.mk"
 CONFIGURE_ARGS+=	--with-postgresql=${PGSQL_PREFIX}
