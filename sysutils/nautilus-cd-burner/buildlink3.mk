e1ec97731fe8003eb151c6d2790024c1e87f16eb
wiz <wiz>
2005-11-25 21:23:14 +0000

    Bump PKGREVISION for libgsf dependencies, and BUILDLINK_DEPENDS because of the libgsf shlib major bump and associated BUILDLINK_DEPENDS bump.

--- sysutils/nautilus-cd-burner/buildlink3.mk
+++ sysutils/nautilus-cd-burner/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2005/03/22 16:33:55 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/11/25 21:23:15 wiz Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 NAUTILUS_CD_BURNER_BUILDLINK3_MK:=	${NAUTILUS_CD_BURNER_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nnautilus-cd-burner}
 BUILDLINK_PACKAGES+=	nautilus-cd-burner
 
 .if !empty(NAUTILUS_CD_BURNER_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.nautilus-cd-burner+=		nautilus-cd-burner>=2.10.0
+BUILDLINK_DEPENDS.nautilus-cd-burner+=		nautilus-cd-burner>=2.12.1nb1
 BUILDLINK_PKGSRCDIR.nautilus-cd-burner?=	../../sysutils/nautilus-cd-burner
 .endif	# NAUTILUS_CD_BURNER_BUILDLINK3_MK
 
