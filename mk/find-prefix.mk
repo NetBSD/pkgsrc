ade93e447214e79aa8dd05fa8630c1e4518ea7ff
jlam <jlam@pkgsrc.org>
2005-11-28 05:54:17 +0000

    Extend find-prefix.mk so that we can specify a package wildcard instead of just ${PKGBASE}, e.g. M4DIR=m4>=1.0 will set M4DIR to the installation prefix of m4 if it matches "m4>=1.0", otherwise it will set M4DIR to ${M4DIR_DEFAULT}.

--- mk/find-prefix.mk
+++ mk/find-prefix.mk
@@ -1,12 +1,12 @@
-# $NetBSD: find-prefix.mk,v 1.3 2005/05/13 23:37:54 jlam Exp $
+# $NetBSD: find-prefix.mk,v 1.4 2005/11/28 05:54:17 jlam Exp $
 #
 # This is a "subroutine" that can be included to find the installation
 # prefix of a package.
 #
-# The input variable is FIND_PREFIX, which is a list of VARNAME=<package>
+# The input variable is FIND_PREFIX, which is a list of VARNAME=<pattern>
 # pairs, where "VARNAME" is the variable that will be set to the
-# installation prefix for the package, and <package> is the ${PKGNAME}
-# for the package.
+# installation prefix for the package, and <pattern> is a package
+# wildcard pattern used to match the installed package (see pkg_info(8)).
 #
 # An example use is:
 #
@@ -19,7 +19,7 @@
 .  if !defined(${_def_:C/=.*$//})
 ${_def_:C/=.*$//}_DEFAULT?=	${LOCALBASE}
 _${_def_:C/=.*$//}_cmd=		\
-	${PKG_INFO} -qp ${_def_:C/^.*=//} 2>/dev/null |			\
+	${PKG_INFO} -qp ${_def_:C/^[^=]*=//:Q} 2>/dev/null |		\
 	{ read cmd arg;							\
 	  case "$$arg" in						\
 	  "")	${ECHO} ${${_def_:C/=.*$//}_DEFAULT:Q} ;;		\
