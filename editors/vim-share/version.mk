e602d798564e02161c61c4455ab5c4313b121748
martti <martti@pkgsrc.org>
2005-12-01 14:04:46 +0000

    Updated editors/vim to 6.4.3
      7802  6.4.002  Unix: may change owner of wrong file in rare cases
      1463  6.4.003  (after 6.4.002) build problem on non-Unix system

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.4 2005/10/17 06:49:34 martti Exp $
+# $NetBSD: version.mk,v 1.5 2005/12/01 14:04:46 martti Exp $
 
 VIM_VERSION=		6.4
-VIM_PATCHLEVEL=		0
+VIM_PATCHLEVEL=		3
 VIM_SUBDIR=		vim64
