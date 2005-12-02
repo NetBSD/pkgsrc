157adb0cb7e77cc4e4e2f24ac249bc9da43c169f
sjg <sjg@pkgsrc.org>
2005-12-02 00:02:59 +0000

    Import bmake-20051105

--- devel/bmake/files/suff.c
+++ devel/bmake/files/suff.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: suff.c,v 1.1 2005/10/31 21:34:25 reed Exp $	*/
+/*	$NetBSD: suff.c,v 1.1.1.1 2005/12/02 00:03:00 sjg Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: suff.c,v 1.1 2005/10/31 21:34:25 reed Exp $";
+static char rcsid[] = "$NetBSD: suff.c,v 1.1.1.1 2005/12/02 00:03:00 sjg Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)suff.c	8.4 (Berkeley) 3/21/94";
 #else
-__RCSID("$NetBSD: suff.c,v 1.1 2005/10/31 21:34:25 reed Exp $");
+__RCSID("$NetBSD: suff.c,v 1.1.1.1 2005/12/02 00:03:00 sjg Exp $");
 #endif
 #endif /* not lint */
 #endif
--- devel/bmake/files/targ.c
+++ devel/bmake/files/targ.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: targ.c,v 1.1 2005/10/31 21:34:25 reed Exp $	*/
+/*	$NetBSD: targ.c,v 1.1.1.1 2005/12/02 00:03:00 sjg Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: targ.c,v 1.1 2005/10/31 21:34:25 reed Exp $";
+static char rcsid[] = "$NetBSD: targ.c,v 1.1.1.1 2005/12/02 00:03:00 sjg Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)targ.c	8.2 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: targ.c,v 1.1 2005/10/31 21:34:25 reed Exp $");
+__RCSID("$NetBSD: targ.c,v 1.1.1.1 2005/12/02 00:03:00 sjg Exp $");
 #endif
 #endif /* not lint */
 #endif
