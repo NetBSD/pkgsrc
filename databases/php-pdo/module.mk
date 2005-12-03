5441b1c596763a2ff996928a56daea3059629cab
jdolecek <jdolecek@pkgsrc.org>
2005-12-03 17:24:28 +0000

    Update the PHP PDO to latest available PECL versions (1.0/1.0.1/1.0.2). Changes are bugfixes mostly.
    Also rework php-pdo/module.mk somewhat, so that the base php-pdo package
    can use it too.

--- databases/php-pdo/module.mk
+++ databases/php-pdo/module.mk
@@ -1,15 +1,19 @@
-# $NetBSD: module.mk,v 1.4 2005/10/10 14:29:05 tv Exp $
+# $NetBSD: module.mk,v 1.5 2005/12/03 17:24:28 jdolecek Exp $
 
 # Items common for all PDO drivers
 
+.if !empty(PDO_DRIVER)
+_PDO_SEP=		_
+.else
+_PDO_SEP=		# empty
+.endif
+
 PDO_DRIVER_TR=		${ECHO} ${PDO_DRIVER:Q} | ${TR} a-z A-Z
-MODNAME=		PDO_${PDO_DRIVER_TR:sh}
-PKGMODNAME=		pdo_${PDO_DRIVER}
-PKGNAME=		php-${PKGMODNAME}-${PHP_BASE_VERS}.${PECL_VERSION}
-PDO_VERSION?=		1.0RC1
+MODNAME=		PDO${_PDO_SEP}${PDO_DRIVER_TR:sh}
+PKGMODNAME=		pdo${_PDO_SEP}${PDO_DRIVER}
 PECL_VERSION?=		${PDO_VERSION}
+PKGNAME=		php-${PKGMODNAME}-${PHP_BASE_VERS}.${PECL_VERSION}
+
 CATEGORIES+=		databases
 MAINTAINER=		jdolecek@NetBSD.org
 COMMENT=		PHP PDO extension for ${PDO_COMMENT} databases
-
-.include "../../databases/php-pdo/buildlink3.mk"
