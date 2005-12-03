c8a4465951a07e0afaefe082b2cdbae9583e344e
jdolecek <jdolecek>
2005-12-03 13:16:36 +0000

    use ${PATH_ARGS} in do-patch target, and use _PKG_SILENT/_PKG_DEBUG to reduce output for normal runs

--- lang/php/ext.mk
+++ lang/php/ext.mk
@@ -1,4 +1,4 @@
-# $NetBSD: ext.mk,v 1.10 2005/10/04 16:50:50 tv Exp $
+# $NetBSD: ext.mk,v 1.11 2005/12/03 13:16:36 jdolecek Exp $
 #
 # PHP extension package framework, for both PECL and bundled PHP extensions.
 #
@@ -80,9 +80,10 @@ do-module-install:
 .if defined(USE_PHP_EXT_PATCHES)
 PATCHDIR=		${.CURDIR}/${PHPPKGSRCDIR}/patches
 do-patch:
-	cd ${WRKSRC}; \
+	${_PKG_SILENT}${_PKG_DEBUG}
+	cd ${WRKSRC};			\
 	for p in `${EGREP} -l '^\+\+\+ ext/${MODNAME}/' ${PATCHDIR}/patch-*`;do\
-		${SED} -e 's,^+++ ext/${MODNAME}/,+++ ,' $$p | ${PATCH}; \
+		${SED} -e 's,^+++ ext/${MODNAME}/,+++ ,' $$p | ${PATCH} ${PATCH_ARGS}; \
 	done
 .endif
 
