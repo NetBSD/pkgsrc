6c83588fec151ff713173c51e7b5b3f51db8f21b
taca <taca@pkgsrc.org>
2005-12-05 14:12:29 +0000

    - Fix hangup when communicating with smbpasswd(8). - Quiet pkglint; changing make macro in double quotation into :Q modifier.
    Bump PKGREVISION.

--- mail/poppassd/options.mk
+++ mail/poppassd/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2005/06/13 16:57:02 taca Exp $
+# $NetBSD: options.mk,v 1.2 2005/12/05 14:12:29 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.poppassd
 PKG_SUPPORTED_OPTIONS=	inet6
@@ -6,7 +6,7 @@ PKG_SUPPORTED_OPTIONS=	inet6
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ENV+=		O_DEFS="${O_DEFS}" CFLAGS="-DINET6"
+CONFIGURE_ENV+=		O_DEFS=${O_DEFS:Q} CFLAGS="-DINET6"
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
 .endif
