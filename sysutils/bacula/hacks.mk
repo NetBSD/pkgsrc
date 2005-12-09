20a7586d9b10714cba570bc4cd39a41c2f92c1c0
tv <tv>
2005-12-09 19:34:36 +0000

    whitespace nit: use nesting spaces rather than doublespacing .-directives

--- sysutils/bacula/hacks.mk
+++ sysutils/bacula/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.2 2005/12/05 20:50:59 rillig Exp $
+# $NetBSD: hacks.mk,v 1.3 2005/12/09 19:34:36 tv Exp $
 
 .if !defined(BACULA_HACKS_MK)
 BACULA_HACKS_MK=	#defined
@@ -7,14 +7,10 @@ BACULA_HACKS_MK=	#defined
 ### with optimizations (-O2) turned on (causes segfaults).
 ###
 .if ${MACHINE_ARCH} == "amd64"
-
-.include "../../mk/compiler.mk"
-
-.if !empty(CC_VERSION:Mgcc-4.0*)
+.  include "../../mk/compiler.mk"
+.    if !empty(CC_VERSION:Mgcc-4.0*)
 PKG_HACKS+=		dontoptimze
 BUILDLINK_TRANSFORM+=	rm:-O[0-9]*
-.endif
-
-.endif
-
+.    endif
+.  endif
 .endif
