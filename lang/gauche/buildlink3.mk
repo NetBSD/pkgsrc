fb2ca8bcb0c34b9fe83983d79087dcb929a80545
uebayasi <uebayasi@pkgsrc.org>
2005-12-09 15:38:54 +0000

    Update gauche to 0.8.6.  Patch provided by Kenji Hisazumi.
    From this release we don't use external Boehm GC library because
    gauche needs GC to be built with special compilation options.
    
    New features in 0.8.6:
    
        * New modules:
    
    	+ dbi: Database independent access layer, providing unified
    	  access to various relational databases. You need separate
    	  "driver" packages to access the actual RDBMS. There are a
    	  few driver packages available at
    	  http://www.kahua.org/cgi-bin/kahua.fcgi/kahua-web/show/dev/DBI/.
    	    Note: If you have been using the separate dbi module, make
    	    sure you remove it before using the new dbi and dbd
    	    modules. You can find the old dbi.scm under somewhere like
    	    /usr/local/share/gauche/site/lib (the actualy directory
    	    depends on the configuration when you've installed the dbi
    	    module).
    	+ util.relation: A framework to work with relations (as defined
    	  by Codd). The result of database access via dbi is represened as
    	  a relation.
    	+ text.sql: SQL parser/constructor. Full features are not
    	  implemented yet, but used in dbi module for prepared queries.
    
        * New SRFIs:
    
    	+ SRFI-40 (Library of streams) as util.stream.
    	+ SRFI-43 (vector library) as srfi-43.
    	+ SRFI-45 (Primitives for Expressing Iterative Lazy
    	    Algorithms) : built-in.
    
        * New built-in proceduers global-variable-bound? and
          glboal-variable-ref. The former supersedes symbol-bound? (
          symbol-bound? is now deprecated and will go away in the future
          releases. Code that uses symbol-bound? should change it to
          global-variable-bound?. ). The latter removes some need of using
          eval just to peek the value of the global variable.
    
        * New regexp procedures: regexp-replace*, regexp-replace-all*,
          regexp-case-fold?.
    
        * Stack overflow handling is largely improved. You can see better
          performance if your script frequently oveflows the stack.
    
    0.8.5 was a maintainance release.
    
    0.8.4:
    
    Gauche 0.8.4:
    
    The compiler and VM have been rewritten. Now Gauche runs faster with
    less memory (as fast as 1.9x, or cosumes 0.7x memory, in best cases of
    our tests. But your mileage may vary.) The compiler now does simple
    closure optimization, so the typical loop-by-local-closure style code
    will get the advantage. On the other hand, you won't see much gain in
    OO-heavy or library-heavy programs.
    
    Other changes:
    
        * New features:
    
    	  + srfi-42 (Eager comprehension) is supported.
    	  + srfi-55 (require-extension) is supported.
    	  + A simple sampling profiler is implemented to help tuning
    	    programs. Check out "Profiling and tuning" section of the
    	    reference manual. The profiler may not be available on all
    	    platforms.
    	  + We provide an experimenal Windows/MinGW binary package for
    	    the convenience. See download page.

--- lang/gauche/buildlink3.mk
+++ lang/gauche/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/11/26 09:39:30 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2005/12/09 15:38:54 uebayasi Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GAUCHE_BUILDLINK3_MK:=	${GAUCHE_BUILDLINK3_MK}+
@@ -15,7 +15,6 @@ BUILDLINK_DEPENDS.Gauche+=	Gauche>=0.8.1
 BUILDLINK_PKGSRCDIR.Gauche?=	../../lang/gauche
 .endif	# GAUCHE_BUILDLINK3_MK
 
-.include "../../devel/boehm-gc/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../mk/dlopen.buildlink3.mk"
 
