44daf032b7994bb1d8893ab800f4a02bfc2240a0
salo <salo@pkgsrc.org>
2005-12-11 17:58:49 +0000

    Update to version 0.8.0
    Changes:
    - support for reading extra features of SimpleBlock
    - bugfixes

--- multimedia/libmatroska/buildlink3.mk
+++ multimedia/libmatroska/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2005/05/30 16:31:40 salo Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2005/12/11 17:58:49 salo Exp $
 #
 # This Makefile fragment is included by packages that use libmatroska.
 #
@@ -16,7 +16,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibmatroska}
 BUILDLINK_PACKAGES+=	libmatroska
 
 .if !empty(LIBMATROSKA_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libmatroska+=	libmatroska>=0.7.7
+BUILDLINK_DEPENDS.libmatroska+=	libmatroska>=0.8.0
 BUILDLINK_PKGSRCDIR.libmatroska?=	../../multimedia/libmatroska
 .endif	# LIBMATROSKA_BUILDLINK3_MK
 
