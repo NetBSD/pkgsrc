93955f7de37a7cf796186dcbaab4067b0735de17
taca <taca@pkgsrc.org>
2005-12-11 13:34:31 +0000

    Remove ruby16-* packages support.

--- textproc/ruby-rdtool/buildlink3.mk
+++ textproc/ruby-rdtool/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/12/16 13:20:37 taca Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2005/12/11 13:34:31 taca Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 
@@ -14,17 +14,10 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:N${RUBY_PKGPREFIX}-rdtool}
 BUILDLINK_PACKAGES+=	${RUBY_PKGPREFIX}-rdtool
 
 .if !empty(RUBY_RDTOOL_BUILDLINK3_MK:M+)
-.if ${RUBY_VER} == "16"
-BUILDLINK_FILES.ruby16-rdtool+=		bin/rd2
-BUILDLINK_DEPMETHOD.ruby16-rdtool?=	build
-BUILDLINK_DEPENDS.ruby16-rdtool+=	{${RUBY_RD_LIST}}>=0.6.14
-BUILDLINK_PKGSRCDIR.ruby16-rdtool?=	../../textproc/ruby-rdtool
-.elif ${RUBY_VER} == "18"
 BUILDLINK_FILES.ruby18-rdtool+=		bin/rd2
 BUILDLINK_DEPMETHOD.ruby18-rdtool?=	build
 BUILDLINK_DEPENDS.ruby18-rdtool+=	{${RUBY_RD_LIST}}>=0.6.14
 BUILDLINK_PKGSRCDIR.ruby18-rdtool?=	../../textproc/ruby-rdtool
-.endif
 .endif
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
