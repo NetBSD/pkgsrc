96e41dc9691835b00bbaf650f6036240f9bba4c4
rillig <rillig>
2005-12-18 21:47:23 +0000

    Fixed CFLAGS quoting.

--- audio/libsndfile/options.mk
+++ audio/libsndfile/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2005/05/31 16:13:18 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2005/12/18 21:47:23 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libsndfile
 PKG_SUPPORTED_OPTIONS=	oss
@@ -7,6 +7,6 @@ PKG_SUPPORTED_OPTIONS=	oss
 
 .if !empty(PKG_OPTIONS:Moss)
 .include "../../mk/ossaudio.buildlink3.mk"
-OSS_DEFS=	-DDEV_DSP=\\\"${DEVOSSAUDIO}\\\" -DUSE_OSS=1
+OSS_DEFS=	-DDEV_DSP=\"${DEVOSSAUDIO:Q}\" -DUSE_OSS=1
 MAKE_ENV+=	AM_CFLAGS=${OSS_DEFS:Q}
 .endif
