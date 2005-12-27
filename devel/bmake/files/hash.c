eaa296f2b2ae0b6ee32fe3db4ababd89f10e7f57
agc <agc>
2005-12-27 09:25:50 +0000

    Add the CHANGES-pkgsrc-2005Q4 file to track branch changes.
    pkgsrc-2005Q4 branch created at Tue Dec 27 09:10:03 GMT 2005

--- devel/bmake/files/hash.c
+++ devel/bmake/files/hash.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: hash.c,v 1.1.1.1 2005/12/02 00:03:00 sjg Exp $	*/
+/*	$NetBSD: hash.c,v 1.1 2005/10/31 21:34:24 reed Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -70,14 +70,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: hash.c,v 1.1.1.1 2005/12/02 00:03:00 sjg Exp $";
+static char rcsid[] = "$NetBSD: hash.c,v 1.1 2005/10/31 21:34:24 reed Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)hash.c	8.1 (Berkeley) 6/6/93";
 #else
-__RCSID("$NetBSD: hash.c,v 1.1.1.1 2005/12/02 00:03:00 sjg Exp $");
+__RCSID("$NetBSD: hash.c,v 1.1 2005/10/31 21:34:24 reed Exp $");
 #endif
 #endif /* not lint */
 #endif
