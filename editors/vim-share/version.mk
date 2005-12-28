485d235e4fece7424d84a8a8e862402283fd390d
seb <seb@pkgsrc.org>
2005-12-28 21:58:02 +0000

    Pullup ticket 971 - requested by Martti Kuparinen sync editors/vim-share, hence editors/vim-* except editors/vim-kde, with HEAD
    Revisions pulled up:
    - pkgsrc/editors/vim-share/Makefile.common                    1.52
    - pkgsrc/editors/vim-share/distinfo                           1.43
    - pkgsrc/editors/vim-share/version.mk                         1.6
    
       Module Name:	pkgsrc
       Committed By:	martti
       Date:		Tue Dec 27 10:17:29 UTC 2005
    
       Modified Files:
       	pkgsrc/editors/vim-share: Makefile.common distinfo version.mk
    
       Log Message:
       Updated vim to 6.4.6
    
          1581  6.4.004  "go" doesn't work correctly when 'virtualedit' is set
          1577  6.4.005  truncating message may write before buffer
          2320  6.4.006  ":argedit", "argdel" and ":argadd" used count argument

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.5 2005/12/01 14:04:46 martti Exp $
+# $NetBSD: version.mk,v 1.5.2.1 2005/12/28 21:58:02 seb Exp $
 
 VIM_VERSION=		6.4
-VIM_PATCHLEVEL=		3
+VIM_PATCHLEVEL=		6
 VIM_SUBDIR=		vim64
