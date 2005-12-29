f0aa85ac53670635c71ed535af5b801c6385608f
jlam <jlam@pkgsrc.org>
2005-12-29 03:44:38 +0000

    Re-implement the pkgsrc fonts-handling by integrating it into the pkginstall framework:
      * Rewrite the mk/install/fonts script fragment as a scriptlet that's
        generated by the +INSTALL script during package installation.
    
      * Rename the FONTS_<TYPE>_DIRS variables to FONTS_DIRS.<type> to
        be more consistent with how "families" of variables are currently
        named in pkgsrc.
    
      * Rewrite mk/fonts.mk so that it's implemented in terms of the new
        functionality in the pkginstall framework.  This file will be
        obsoleted in the near future after packages have been transitioned
        to use the new functionality in the pkginstall framework.  Currently,
        packages will continue to work with no changes.
    
    Integrating the fonts-handling into the pkginstall framework has the
    benefit of generating a +FONTS scriptlet that may be run independently
    of the +INSTALL scripts to (idempotently) update the fonts databases
    and fix package installation errors.

--- mk/fonts.mk
+++ mk/fonts.mk
@@ -1,61 +1,20 @@
-# $NetBSD: fonts.mk,v 1.8 2005/12/28 17:54:20 joerg Exp $
+# $NetBSD: fonts.mk,v 1.9 2005/12/29 03:44:38 jlam Exp $
 #
-# This Makefile fragment is intended to be included by packages that install
-# fonts (most of them in the fonts category).  It takes care of updating the
-# fonts.dir files at install/deinstall time.
-#
-# The following variables need to be defined by packages using fonts.mk:
-#
-# FONTS_<TYPE>_DIRS - Whitespaced list of directories where the font database
-#                     is updated. If empty, nothing is done for this TYPE.
-#
-# Supported TYPEs: TTF, TYPE1, X11.
+# XXX This file's use is deprecated.  This is here temporarily to help
+# XXX packages transition to using the new fonts capability of the
+# XXX pkginstall framework.
 #
 
 .if !defined(FONTS_MK)
 FONTS_MK=		# defined
 
-# TrueType fonts
-FONTS_TTF_DIRS?=
-# Type1 fonts
-FONTS_TYPE1_DIRS?=
-# Generic X fonts (PCF, SNF, BDF)
-FONTS_X11_DIRS?=
-
-.if !empty(FONTS_TTF_DIRS) || !empty(FONTS_TYPE1_DIRS) || !empty(FONTS_X11_DIRS)
+# Support the old FONTS_<TYPE>_DIRS variables for a while until we can
+# nuke them. 
+#
+FONTS_DIRS.ttf+=	${FONTS_TTF_DIRS}
+FONTS_DIRS.type1+=	${FONTS_TYPE1_DIRS}
+FONTS_DIRS.x11+=	${FONTS_X11_DIRS}
 
 USE_PKGINSTALL=		YES
-HEADER_EXTRA_TMPL+=	${.CURDIR}/../../mk/install/fonts
-
-.if !empty(FONTS_TTF_DIRS)
-EVAL_PREFIX+=			TTMKFDIR_PREFIX=ttmkfdir
-TTMKFDIR_PREFIX_DEFAULT=	${LOCALBASE}
-FILES_SUBST+=		FONTS_TTF="YES"
-FILES_SUBST+=		FONTS_TTF_DIRS=${FONTS_TTF_DIRS:Q}
-FILES_SUBST+=		TTMKFDIR="${TTMKFDIR_PREFIX}/bin/ttmkfdir"
-DEPENDS+=		ttmkfdir2>=20021109:../../fonts/ttmkfdir2
-# also need to run mkfontdir there
-FONTS_X11_DIRS+=	${FONTS_TTF_DIRS}
-.endif
-
-.if !empty(FONTS_TYPE1_DIRS)
-EVAL_PREFIX+=			TYPE1INST_PREFIX=type1inst
-TYPE1INST_PREFIX_DEFAULT=	${LOCALBASE}
-FILES_SUBST+=		FONTS_TYPE1="YES"
-FILES_SUBST+=		FONTS_TYPE1_DIRS=${FONTS_TYPE1_DIRS:Q}
-FILES_SUBST+=		TYPE1INST="${TYPE1INST_PREFIX}/bin/type1inst"
-DEPENDS+=		type1inst>=0.6.1:../../fonts/type1inst
-# also need to run mkfontdir there
-FONTS_X11_DIRS+=	${FONTS_TYPE1_DIRS}
-.endif
-
-.if !empty(FONTS_X11_DIRS)
-FILES_SUBST+=		FONTS_X11="YES"
-FILES_SUBST+=		FONTS_X11_DIRS=${FONTS_X11_DIRS:Q}
-FILES_SUBST+=		MKFONTDIR="${X11BASE}/bin/mkfontdir"
-USE_TOOLS+=		mkfontdir
-.endif
-
-.endif
 
 .endif	# FONTS_MK
