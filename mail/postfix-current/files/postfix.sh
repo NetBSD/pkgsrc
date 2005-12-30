a5d50ba3f369e6d2d9a6adaaaeb42317900ae2fb
seb <seb>
2005-12-30 13:58:14 +0000

    Pullup ticket 982 - requested by Martti Kuparinen improve rc script usability of mail/postfix and mail/postfix-current
    Revisions pulled up:
    - pkgsrc/mail/postfix/Makefile                                1.172
    - pkgsrc/mail/postfix-current/Makefile                        1.52
    - pkgsrc/mail/postfix-current/files/postfix.sh                1.4
    - pkgsrc/mail/postfix/files/postfix.sh                        1.5
    
       Module Name:	pkgsrc
       Committed By:	martti
       Date:		Fri Dec 30 06:29:41 UTC 2005
    
       Modified Files:
       	pkgsrc/mail/postfix: Makefile
       	pkgsrc/mail/postfix-current: Makefile
       	pkgsrc/mail/postfix-current/files: postfix.sh
       	pkgsrc/mail/postfix/files: postfix.sh
    
       Log Message:
       Make sure ${spooletcdir} exists. Bump PKGREVISION as this affects
       the binary package.

--- mail/postfix-current/files/postfix.sh
+++ mail/postfix-current/files/postfix.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: postfix.sh,v 1.3 2004/07/31 05:53:44 jlam Exp $
+# $NetBSD: postfix.sh,v 1.3.12.1 2005/12/30 13:58:14 seb Exp $
 #
 
 # PROVIDE: mail
@@ -30,6 +30,7 @@ postfix_precmd()
 	# is used).
 	#
 
+	[ ! -d ${spooletcdir} ] && mkdir -p ${spooletcdir}
 	for f in localtime resolv.conf services; do
 		if [ -f /etc/$f ]; then
 			cmp -s /etc/$f ${spooletcdir}/$f || \
