f8e51f0c0a91f79cf1f414e11563f38c6db28efc
ben <ben@pkgsrc.org>
2005-12-31 16:28:40 +0000

    > When building on Interix, add Xext to X_EXTRA_LIBS.  The xditview > program requires Xaw, which requires the Xlib shape routines, which on > Interix are in Xext.  http://www.faqs.org/faqs/x-faq/part6/section-27.html
    Move fix from Makefile to options.mk, so it is only used when the x11
    option is enabled.

--- textproc/groff/options.mk
+++ textproc/groff/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2005/12/09 20:40:56 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2005/12/31 16:28:40 ben Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.groff
 PKG_SUPPORTED_OPTIONS=	x11
@@ -12,6 +12,10 @@ CONFIGURE_ARGS+=	--with-appresdir=${PREFIX}/lib/X11/app-defaults
 PLIST_SRC=		PLIST.x11 PLIST
 .include "../../mk/xaw.buildlink3.mk"
 .include "../../mk/x11.buildlink3.mk"
+.include "../../mk/bsd.prefs.mk"
+.if ${OPSYS} == "Interix"
+CONFIGURE_ENV+=         X_EXTRA_LIBS=-lXext
+.endif
 .else
 CONFIGURE_ARGS+=	--without-x
 .endif
