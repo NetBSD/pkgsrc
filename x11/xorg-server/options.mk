311aa026b0b3c1da67ddca3016188dfcf7ee9091
wiz <wiz@pkgsrc.org>
2006-01-01 18:53:03 +0000

    Remove stuff that was marked for removal after 2005Q4.

--- x11/xorg-server/options.mk
+++ x11/xorg-server/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2005/12/02 17:51:50 wiz Exp $
+# $NetBSD: options.mk,v 1.6 2006/01/01 18:53:03 wiz Exp $
 
 _COMMONCARDDRIVERS=	xorg-server-apm xorg-server-ark xorg-server-ati \
 			xorg-server-chips xorg-server-cirrus \
@@ -35,11 +35,6 @@ _OSCARDDRIVERS+=	${_LINUXCARDDRIVERS}
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xorg-server
 PKG_SUPPORTED_OPTIONS=	${_COMMONCARDDRIVERS} ${_OSCARDDRIVERS}
 PKG_SUGGESTED_OPTIONS=	${PKG_SUPPORTED_OPTIONS}
-
-# remove legacy handling after 2005Q4
-.for _drv in ${_ALLCARDDRIVERS}
-PKG_OPTIONS_LEGACY_OPTS+=	${_drv:S/xorg-server-//}:${_drv}
-.endfor
 
 .include "../../mk/bsd.options.mk"
 
