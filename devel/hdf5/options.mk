b4ed2de012ae122841e648dc8f349b4dd4e27c0e
recht <recht@pkgsrc.org>
2006-01-04 06:05:44 +0000

    Addd optional support for thread safety and C++ with the options: cxx threads

--- devel/hdf5/options.mk
+++ devel/hdf5/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1 2005/05/19 15:39:04 jwise Exp $
+# $NetBSD: options.mk,v 1.2 2006/01/04 06:05:44 recht Exp $
 
 PKG_OPTIONS_VAR=PKG_OPTIONS.hdf5
-PKG_SUPPORTED_OPTIONS=	szip
+PKG_SUPPORTED_OPTIONS=	szip cxx threads
 PKG_SUGGESTED_OPTIONS=	szip
 
 .include "../../mk/bsd.options.mk"
@@ -9,4 +9,17 @@ PKG_SUGGESTED_OPTIONS=	szip
 .if !empty(PKG_OPTIONS:Mszip)
 CONFIGURE_ARGS+=	--with-szlib
 .include "../../archivers/szip/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mcxx)
+USE_LANGUAGES+=		c++
+CONFIGURE_ARGS+=	--enable-cxx
+PLIST_SUBST+=		CXX=
+.else
+PLIST_SUBST+=		CXX="@comment "
+.endif
+
+.if !empty(PKG_OPTIONS:Mthreads)
+CONFIGURE_ARGS+=	--enable-threadsafe
+.include "../../mk/pthread.buildlink3.mk"
 .endif
