33d7178d4a2d70e885be8cefe8104396cc6acce6
joerg <joerg>
2006-01-05 23:16:01 +0000

    Adjust comment about SUBST_FILES to match reality: absolute file names work as well.

--- mk/bulk/bsd.bulk-pkg.mk
+++ mk/bulk/bsd.bulk-pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.bulk-pkg.mk,v 1.108 2005/11/28 20:08:37 rillig Exp $
+#	$NetBSD: bsd.bulk-pkg.mk,v 1.109 2006/01/05 23:16:01 joerg Exp $
 
 #
 # Copyright (c) 1999, 2000 Hubert Feyrer <hubertf@NetBSD.org>
@@ -181,6 +181,7 @@ _ESCPKGPATH=	${PKGPATH:C@\/@\\/@g:C@\+@\\+@g:C@\.@\\.@g:Q}
 
 # The directory where package-specific log files are saved.
 _BULK_PKGLOGDIR=	${BULKFILESDIR}/${PKGPATH}
+_INTERACTIVE_COOKIE_DIR= ${BULK_PKGLOGDIR}
 
 # Package-specific log files.
 _BROKENFILE=		${_BULK_PKGLOGDIR}/${BROKENFILE}
