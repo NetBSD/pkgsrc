a479a1684ecdc1ab528f0267b6bb047a346c6008
tonio <tonio>
2006-01-08 14:00:11 +0000

    Make teTeX3 the default TEX_DEFAULT bump PKGREVISION where necessary
    Move PKG_TEXMFPREFIX and PKG_LOCALTEXMFPREFIX definitions to
    teTeX?/buidlink3.mk, so that packages may include
    print/teTeX1-bin/buildlink3.mk directly (however, using teTeX/module.mk do not
    allow that)

--- print/teTeX-bin/buildlink3.mk
+++ print/teTeX-bin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2005/11/14 21:57:00 tonio Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2006/01/08 14:00:12 tonio Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 TETEX_BIN_BUILDLINK3_MK:=	${TETEX_BIN_BUILDLINK3_MK}+
@@ -17,5 +17,7 @@ BUILDLINK_PKGSRCDIR.teTeX-bin?=	../../print/teTeX-bin
 
 TEX=tex
 PDFTEX=pdftex
+PKG_TEXMFPREFIX=	${PREFIX}/share/texmf
+PKG_LOCALTEXMFPREFIX=	${PREFIX}/share/texmf
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
