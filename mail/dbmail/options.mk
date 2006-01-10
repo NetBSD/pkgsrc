1a87ec64d529df1a04ae320788b1b2ecec3bf180
ghen <ghen@pkgsrc.org>
2006-01-10 12:39:03 +0000

    Use mk/mysql.buildlink3.mk instead of databases/mysqlX-client/bl3.mk, same for pgsql.bl3.mk.  Bump PKGREVISION.  Ok by wiz.

--- mail/dbmail/options.mk
+++ mail/dbmail/options.mk
@@ -1,24 +1,17 @@
-# $NetBSD: options.mk,v 1.2 2005/05/31 10:01:37 dillo Exp $
+# $NetBSD: options.mk,v 1.3 2006/01/10 12:39:04 ghen Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dbmail
-PKG_SUPPORTED_OPTIONS=	mysql pgsql
+PKG_OPTIONS_REQUIRED_GROUPS=	sql
+PKG_OPTIONS_GROUP.sql=	mysql pgsql
 PKG_SUGGESTED_OPTIONS=	mysql
 
 .include "../../mk/bsd.options.mk"
 
 ###
-### We can't compile support for both MySQL and PostgreSQL.
-###
-.if !empty(PKG_OPTIONS:Mmysql) && !empty(PKG_OPTIONS:Mpgsql)
-PKG_FAIL_REASON+=       "MySQL and PostgreSQL cannot both be compiled in." \
-			"Please change ${PKG_OPTIONS_VAR} to one or the other."
-.endif
-
-###
 ### MySQL database support
 ###
 .if !empty(PKG_OPTIONS:Mmysql)
-.include "../../databases/mysql4-client/buildlink3.mk"
+.include "../../mk/mysql.buildlink3.mk"
 CONFIGURE_ARGS+=	--with-mysql
 SQLDB=			mysql
 .endif
@@ -27,8 +20,7 @@ SQLDB=			mysql
 ### PostgreSQL database support
 ###
 .if !empty(PKG_OPTIONS:Mpgsql)
-DEPENDS+=	postgresql74-client-[0-9]*:../../databases/postgresql74-client
-.include "../../databases/postgresql74-lib/buildlink3.mk"
+.include "../../mk/pgsql.buildlink3.mk"
 CONFIGURE_ARGS+=	--with-pgsql
 SQLDB=			pgsql
 .endif
