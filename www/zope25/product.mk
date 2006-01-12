6a04038277d5d3438540aacef22a1efa2b351a76
rillig <rillig@pkgsrc.org>
2006-01-12 23:50:14 +0000

    Added the target zope-std-copyproduct-long-nogarbage that works like zope-std-copyproduct-long, except that it doesn't copy CVS files and *.orig.

--- www/zope25/product.mk
+++ www/zope25/product.mk
@@ -1,4 +1,4 @@
-# $NetBSD: product.mk,v 1.7 2005/05/24 14:26:26 rillig Exp $
+# $NetBSD: product.mk,v 1.8 2006/01/12 23:50:14 rillig Exp $
 
 DIST_SUBDIR=	zope
 BUILDLINK_DEPENDS.zope?=	zope-2.*
@@ -15,6 +15,15 @@ PRODNAMES?=	${PKGNAME:C/^zope25-(.*)-.*/\1/}
 ZOPE_COPYPRODUCT?= zope-std-copyproduct-short
 
 PRODSUBDIR=	lib/python/Products/${PRODNAME}
+
+.PHONY: zope-std-copyproduct-long-nogarbage
+zope-std-copyproduct-long-nogarbage:
+	cd ${WRKSRC:Q} && ${PAX} -rw		\
+		-s ',.*/CVS/.*,,'		\
+		-s ',.*/CVS$$,,'		\
+		-s ',.*/\.cvsignore$$,,'	\
+		-s ',.*\.orig$$,,'		\
+		lib ${ZOPEDIR}
 
 zope-std-copyproduct-long:
 	(cd ${WRKSRC}; ${PAX} -r -w lib ${ZOPEDIR})
