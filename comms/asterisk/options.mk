4936a95fcb1f97c325344de3f57670b4eaad02e9
riz <riz>
2006-01-13 20:32:38 +0000

    Update asterisk to version 1.2.1.  Many, many bugfixes, and some new features, including support for DUNDi.  (http://www.dundi.com/ for more information)
    The initial framework and porting of this package upgrade was done by
    Martin J. Laubach, with lots of feature/PLIST fixes by me.  DragonFly
    support added by Joerg Sonnenberger.

--- comms/asterisk/options.mk
+++ comms/asterisk/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2005/04/08 03:10:52 riz Exp $
+# $NetBSD: options.mk,v 1.2 2006/01/13 20:32:38 riz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.asterisk
 PKG_SUPPORTED_OPTIONS=	zaptel gtk
@@ -7,9 +7,9 @@ PKG_SUPPORTED_OPTIONS=	zaptel gtk
 .if !empty(PKG_OPTIONS:Mzaptel)
 # zaptel only supported under NetBSD at the moment
 .  include "../../comms/zaptel-netbsd/buildlink3.mk"
-MAKE_FLAGS+=	ASTERISK_USE_ZAPTEL=1
 PLIST_SUBST+=		ZAPTEL=
 .else
+MAKE_FLAGS+=	WITHOUT_ZAPTEL=1
 PLIST_SUBST+=		ZAPTEL="@comment "
 .endif
 
