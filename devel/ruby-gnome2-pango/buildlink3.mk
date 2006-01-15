c0a00157d548622087e53882f9c109d524819401
minskim <minskim@pkgsrc.org>
2006-01-15 16:20:12 +0000

    Do not set BUILDLINK_DEPMETHOD of ruby-gnome2-pango to "build", because its files are required at run time by other packages depending on it.  Patch from dieter Roelants in PR pkg/32518.
    Bump PKGREVISION of ruby-gnome2-gtk due to dependency change.

--- devel/ruby-gnome2-pango/buildlink3.mk
+++ devel/ruby-gnome2-pango/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/01/08 03:26:02 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2006/01/15 16:20:12 minskim Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 RUBY_GNOME2_PANGO_BUILDLINK3_MK:=	${RUBY_GNOME2_PANGO_BUILDLINK3_MK}+
@@ -13,7 +13,6 @@ BUILDLINK_PACKAGES+=	ruby-gnome2-pango
 .if !empty(RUBY_GNOME2_PANGO_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.ruby-gnome2-pango+=	ruby-gnome2-pango>=0.14.1
 BUILDLINK_PKGSRCDIR.ruby-gnome2-pango?=	../../devel/ruby-gnome2-pango
-BUILDLINK_DEPMETHOD.ruby-gnome2-pango?=	build
 .endif	# RUBY_GNOME2_PANGO_BUILDLINK3_MK
 
 
