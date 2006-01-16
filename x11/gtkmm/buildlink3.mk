d085fa75656c48f8caa9b5ce764b8e9e13192da8
adam <adam@pkgsrc.org>
2006-01-16 14:24:21 +0000

    Changes 2.8.2:
    * Gdk::Window::set_cursor():
      Avoid leaking an extra Cursor instance.
    * Build:
      - Correctly find gmmproc on 64-bit systems.
      - Optionally don't build the documentation, for
      people in a hurry.
      - Windows: Check for mkifofs().
      - For reduced-resources devices, optionally allow deprecated
        API to be left out of the library.
    * Documentation:
      - Generated Since and Deprecated lists of API.
      - Improved Expander documentation.
      - Corrections to DrawArea chapter in book.

--- x11/gtkmm/buildlink3.mk
+++ x11/gtkmm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2005/07/22 21:21:47 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2006/01/16 14:24:21 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GTKMM_BUILDLINK3_MK:=	${GTKMM_BUILDLINK3_MK}+
@@ -18,4 +18,4 @@ BUILDLINK_PKGSRCDIR.gtkmm?=	../../x11/gtkmm
 .include "../../devel/glibmm/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
