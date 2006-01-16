b693811fead84388d5818d8f6c59e4fbfb5124e1
adam <adam@pkgsrc.org>
2006-01-16 10:08:04 +0000

    Changes 2.8.3: * filename_display_name(): Correct the declaration to fix   the linker error. * Build: For reduced-resources devices, optionally allow deprecated   API to be left out of the library. * Documentation:   - Show Since and Deprecated lists of API.
    Changes 2.8.2:
    * Solaris build fix: Correct the detection of make.

--- devel/glibmm/buildlink3.mk
+++ devel/glibmm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/07/22 21:12:20 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2006/01/16 10:08:04 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GLIBMM_BUILDLINK3_MK:=	${GLIBMM_BUILDLINK3_MK}+
@@ -18,4 +18,4 @@ BUILDLINK_PKGSRCDIR.glibmm?=	../../devel/glibmm
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/libsigc++2/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
