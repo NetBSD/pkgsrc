242def50680f318f43cc95c2712cf7d34b63d0a9
jmmv <jmmv@pkgsrc.org>
2006-01-18 12:44:00 +0000

    Rename gstreamer, gst-plugins and gst-plugins-* to gstreamer0.8, gst-plugins0.8 and gst-plugins0.8-* respectively.  This is to leave room for gstreamer 0.10 (which will be added with versioned names too, to avoid annoying renames in the future).
    While here, do some changes:
    - Make gstreamer0.8 use the alternatives system to provide the unversioned
      gst-* utilities.
    - Rename all internal variables to have 0.8 in them to make it clear which
      package they belong to.

---
+++ audio/gst-plugins0.8-esound/buildlink3.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink3.mk,v 1.1 2006/01/18 12:44:01 jmmv Exp $
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+GST_PLUGINS0.8_ESOUND_BUILDLINK3_MK:=	${GST_PLUGINS0.8_ESOUND_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gst-plugins0.8-esound
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngst-plugins0.8-esound}
+BUILDLINK_PACKAGES+=	gst-plugins0.8-esound
+
+.if !empty(GST_PLUGINS0.8_ESOUND_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.gst-plugins0.8-esound+=	gst-plugins0.8-esound>=0.8.11
+BUILDLINK_PKGSRCDIR.gst-plugins0.8-esound?=	../../audio/gst-plugins0.8-esound
+.endif	# GST_PLUGINS0.8_ESOUND_BUILDLINK3_MK
+
+.include "../../audio/esound/buildlink3.mk"
+.include "../../multimedia/gst-plugins0.8/buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
