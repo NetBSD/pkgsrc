30ffc6c34a02db5a8fab7847e41b8c33fff0e6b7
jlam <jlam@pkgsrc.org>
2006-01-18 18:03:08 +0000

    Modify the show-depends-pkgpaths implementation so that we don't assume package directories specified in DEPENDS lines begin with "../..".  The new standalone script pkgsrc/mk/scripts/pkg_path will canonicalize them into package paths relative to ${PKGSRCDIR}.

--- mk/bsd.utils.mk
+++ mk/bsd.utils.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.utils.mk,v 1.2 2006/01/18 03:58:19 jlam Exp $
+# $NetBSD: bsd.utils.mk,v 1.3 2006/01/18 18:03:08 jlam Exp $
 #
 # This Makefile fragment is included by bsd.pkg.mk and defines utility
 # and otherwise miscellaneous variables and targets.
@@ -10,20 +10,24 @@
 #
 DEPENDS_TYPE?=  all
 .if !empty(DEPENDS_TYPE:Mbuild) || !empty(DEPENDS_TYPE:Mall)
-_ALL_DEPENDS_PKGPATHS+=	\
-	${BUILD_DEPENDS:C/^[^:]*://:S/^..\/..\///:S/${PKGSRCDIR}\///}
+_ALL_DEPENDS_PKGSRCDIRS+=	${BUILD_DEPENDS:C/^[^:]*://}
 .endif
 .if !empty(DEPENDS_TYPE:Minstall) || !empty(DEPENDS_TYPE:Mpackage) || \
     !empty(DEPENDS_TYPE:Mall)
-_ALL_DEPENDS_PKGPATHS+=	\
-	${DEPENDS:C/^[^:]*://:S/^..\/..\///:S/${PKGSRCDIR}\///}
+_ALL_DEPENDS_PKGSRCDIRS+=	${DEPENDS:C/^[^:]*://}
 .endif
+
+# _PKG_PATHS_CMD canonicalizes package paths so that they're relative to
+# ${PKGSRCDIR} and also verifies that they exist within pkgsrc.
+#
+_PKG_PATHS_CMD=								\
+	${SETENV} ECHO=${TOOLS_ECHO:Q} PKGSRCDIR=${PKGSRCDIR:Q}		\
+		PWD_CMD=${TOOLS_PWD_CMD:Q} TEST=${TOOLS_TEST:Q}		\
+	${SH} ${.CURDIR}/../../mk/scripts/pkg_path
 
 .PHONY: show-depends-pkgpaths
 show-depends-pkgpaths:
-.for _deppath_ in ${_ALL_DEPENDS_PKGPATHS:O:u}
-	@${ECHO} ${_deppath_}
-.endfor
+	@${_PKG_PATHS_CMD} ${_ALL_DEPENDS_PKGSRCDIRS:O:u}
 
 # _DEPENDS_WALK_CMD holds the command (sans arguments) to walk the
 # dependency graph for a package.
