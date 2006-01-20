144a0dcabc2125d5bbb1c282f433fcd87f77652e
jmmv <jmmv@pkgsrc.org>
2006-01-20 13:28:22 +0000

    Update to 0.4.4:
    Changes in 0.4.4:
    -----------------
    * Added missing GPL and LGPL texts              (Shaun McCance)
    * Added another fix for line numbering, #317270 (Brent Smith)
    * Updated translations:
      bn      (Mahay Alam Khan)
      et      (Ivar Smolin)
      (33/47 languages with 100% translation)
    
    Changes in 0.4.3:
    -----------------
    * Fix for loops on old bourne shells, #317308     (Shaun McCance)
    * Fix paths when $(srcdir) is absolute            (Shaun McCance)
    * Fix DESTDIR support #318007                     (Frederic Crozat)
    * Updated translations:
      it      (Alessio Frusciante)
      (32/46 languages with 100% translation)

--- textproc/gnome-doc-utils/buildlink3.mk
+++ textproc/gnome-doc-utils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2005/10/12 21:47:45 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2006/01/20 13:28:22 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_DOC_UTILS_BUILDLINK3_MK:=	${GNOME_DOC_UTILS_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	gnome-doc-utils
 
 .if !empty(GNOME_DOC_UTILS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPMETHOD.gnome-doc-utils+=	build
-BUILDLINK_DEPENDS.gnome-doc-utils+=	gnome-doc-utils>=0.4.2
+BUILDLINK_DEPENDS.gnome-doc-utils+=	gnome-doc-utils>=0.4.4
 BUILDLINK_PKGSRCDIR.gnome-doc-utils?=	../../textproc/gnome-doc-utils
 .endif	# GNOME_DOC_UTILS_BUILDLINK3_MK
 
