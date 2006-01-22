b21265e9f2b9a1a16ce9509b07123a6d84e0c668
schwarz <schwarz@pkgsrc.org>
2006-01-22 21:36:31 +0000

    added hack for handling of inttypes.h on IRIX 5.3

--- graphics/netpbm/hacks.mk
+++ graphics/netpbm/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.2 2005/12/05 20:50:21 rillig Exp $
+# $NetBSD: hacks.mk,v 1.3 2006/01/22 21:36:31 schwarz Exp $
 
 .if !defined(NETPBM_HACKS_MK)
 NETPBM_HACKS_MK=	# defined
@@ -21,6 +21,17 @@ GCC_REQD+=		3.0
 .if ${OPSYS} == "Interix"
 PKG_HACKS+=		standard-headers
 MAKE_FLAGS+=		INTTYPES_H='<sys/types.h>'
+.endif
+
+### [Fri Dec 30 21:00:59 CET 2005 : schwarz]
+### make sys/types.h not conflict with inttypes.h
+### (issue is specific to IRIX 5.3)
+###
+.if ${LOWER_OPSYS} == "irix5.3"
+PKG_HACKS+=		sys_types_h-inttypes_h-conflict
+post-wrapper:
+		${MKDIR} ${BUILDLINK_DIR}/include/sys && ${GREP} -v '^typedef.*[^u_]int[12368]*_t;' /usr/include/sys/types.h > ${BUILDLINK_DIR}/include/sys/types.h
+# should match int8_t, int16_t, and int32_t (only)
 .endif
 
 .endif
