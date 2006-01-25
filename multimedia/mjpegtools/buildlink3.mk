e337d62d370e847d8eaedbab6daadf855ef73dfc
xtraeme <xtraeme>
2006-01-25 08:09:12 +0000

    Update mjpegtools/mplex to 1.8.0.
    Some changes:
    
    * Modularised input handling for MPEG compression tools.
    * movtar read/write support
    * MJPEG playback in software, and experimental MJPEG hardware
       overlay playback (see mjpeg/)
    * mpeg2enc _much_ improved!

--- multimedia/mjpegtools/buildlink3.mk
+++ multimedia/mjpegtools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/01/24 07:32:27 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2006/01/25 08:09:12 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MJPEGTOOLS_BUILDLINK3_MK:=	${MJPEGTOOLS_BUILDLINK3_MK}+
@@ -11,14 +11,25 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmjpegtools}
 BUILDLINK_PACKAGES+=	mjpegtools
 
 .if !empty(MJPEGTOOLS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.mjpegtools+=		mjpegtools>=1.6.1.90nb3
-BUILDLINK_RECOMMENDED.mjpegtools+=	mjpegtools>=1.6.2nb8
+BUILDLINK_DEPENDS.mjpegtools+=		mjpegtools>=1.6.8
 BUILDLINK_PKGSRCDIR.mjpegtools?=	../../multimedia/mjpegtools
 .endif	# MJPEGTOOLS_BUILDLINK3_MK
 
 .include "../../audio/lame/buildlink3.mk"
 .include "../../devel/SDL/buildlink3.mk"
-.include "../../graphics/ImageMagick/buildlink3.mk"
 .include "../../graphics/jpeg/buildlink3.mk"
+
+.if !defined(PKG_BUILD_OPTIONS.mjpegtools)
+PKG_BUILD_OPTIONS.mjpegtools!=			\
+	cd ${BUILDLINK_PKGSRCDIR.apr} &&	\
+	${MAKE} show-var ${MAKEFLAGS} VARNAME=PKG_OPTIONS
+MAKEFLAGS+=	PKG_BUILD_OPTIONS.mjpegtools=	\
+			${PKG_BUILD_OPTIONS.mjpegtools:Q}
+.endif
+MAKEVARS+=      PKG_BUILD_OPTIONS.mjpegtools
+
+.if !empty(PKG_BUILD_OPTIONS.mjpegtools:Mdv)
+.  include "../../multimedia/libdv/buildlink3.mk"
+.endif
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
