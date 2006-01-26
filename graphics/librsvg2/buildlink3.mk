d1956ec20707729ebcd71a3c7225560f04073119
adam <adam@pkgsrc.org>
2006-01-26 23:06:16 +0000

    Buildlink fix

--- graphics/librsvg2/buildlink3.mk
+++ graphics/librsvg2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2006/01/24 07:32:16 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2006/01/26 23:06:17 adam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBRSVG2_BUILDLINK3_MK:=	${LIBRSVG2_BUILDLINK3_MK}+
@@ -11,14 +11,13 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibrsvg2}
 BUILDLINK_PACKAGES+=	librsvg2
 
 .if !empty(LIBRSVG2_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.librsvg2?=	librsvg2>=2.12.6nb1
-BUILDLINK_RECOMMENDED.librsvg2?=	librsvg2>=2.12.7nb1
+BUILDLINK_DEPENDS.librsvg2+=	librsvg2>=2.12.6nb1
+BUILDLINK_RECOMMENDED.librsvg2+=	librsvg2>=2.12.7nb1
 BUILDLINK_PKGSRCDIR.librsvg2?=	../../graphics/librsvg2
 .endif	# LIBRSVG2_BUILDLINK3_MK
 
-.include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/libgsf/buildlink3.mk"
+.include "../../print/libgnomeprintui/buildlink3.mk"
 .include "../../textproc/libcroco/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
