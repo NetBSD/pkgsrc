a8893e25b6c70c44cf2a03b36c26b7ce58fd7fd9
adam <adam@pkgsrc.org>
2006-01-26 20:38:18 +0000

    Changes 6.2.6.0: * Bug fixes

--- graphics/ImageMagick/options.mk
+++ graphics/ImageMagick/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2005/06/01 18:02:53 jlam Exp $
+# $NetBSD: options.mk,v 1.4 2006/01/26 20:38:18 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ImageMagick
 PKG_SUPPORTED_OPTIONS=	x11
@@ -7,7 +7,7 @@ PKG_SUGGESTED_OPTIONS=	x11
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mx11)
-BUILDLINK_DEPENDS.jasper+= jasper>=1.701.0
+BUILDLINK_DEPENDS.jasper+=	jasper>=1.701.0
 DEPENDS+=		mpeg2codec-1.2:../../graphics/mpeg2codec
 
 .include "../../graphics/jasper/buildlink3.mk"
