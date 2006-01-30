f2349dbd3628ad16f67080ba43e944412c35f930
xtraeme <xtraeme@pkgsrc.org>
2006-01-30 08:44:25 +0000

    * Add missing files now that mplex has been re-added. * Add a patch that should fix the build on amd64. * Fixed pkglint warnings.
    Bump PKGREVISION.

--- multimedia/mjpegtools/hacks.mk
+++ multimedia/mjpegtools/hacks.mk
@@ -1,9 +1,9 @@
-#	$NetBSD: hacks.mk,v 1.2 2005/07/29 01:15:16 grant Exp $
+#	$NetBSD: hacks.mk,v 1.3 2006/01/30 08:44:25 xtraeme Exp $
 
 .if !defined(MJPEGTOOLS_HACKS_MK)
 MJPEGTOOLS_HACKS_MK= defined
 
-### Hack to work around MacOS X 4.x (and probably other)'s gcc 4.x 
+### Hack to work around MacOS X 4.x (and probably other)'s gcc 4.x
 ### which does not understand -fmove-all-movables. Remove that option
 ### if gcc-4 is used.
 ###
