47a74387eb08d880fd9d34fb315fc5375ee38cae
wiz <wiz@pkgsrc.org>
2006-01-30 18:07:06 +0000

    Fix tcpwrappers option. Since it is on by default, bump PKGREVISION. From Hironaga KOJIMA in private mail.

--- net/vsftpd/options.mk
+++ net/vsftpd/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2006/01/13 18:12:46 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2006/01/30 18:07:06 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.vsftpd
 PKG_SUPPORTED_OPTIONS=	inet6 ssl tcpwrappers
@@ -15,7 +15,7 @@ BROKEN=		Needs ipv6 option enabled.
 .if !empty(PKG_OPTIONS:Mssl)
 .include "../../security/openssl/buildlink3.mk"
 SUBST_CLASSES+=		ssl
-SUBST_FILES.ssl=	builddefs.h tunables.c vsftpd.conf.5
+SUBST_FILES.ssl=	builddefs.h
 SUBST_SED.ssl+=		-e 's,undef VSF_BUILD_SSL,define VSF_BUILD_SSL,g'
 SUBST_SED.ssl+=		-e 's,/usr/share/ssl/certs/vsftpd.pem,${SSLCERTS}/vsftpd.pem,g'
 SUBST_STAGE.ssl=	pre-configure
@@ -23,8 +23,8 @@ SUBST_STAGE.ssl=	pre-configure
 
 .if !empty(PKG_OPTIONS:Mtcpwrappers)
 .include "../../security/tcp_wrappers/buildlink3.mk"
-SUBST_CLASSES+=		tcpwrappers
-SUBST_FILES.ssl=	builddefs.h
-SUBST_SED.ssl+=		-e 's,undef VSF_BUILD_TCPWRAPPERS,define VSF_BUILD_TCPWRAPPERS,g'
-SUBST_STAGE.ssl=	pre-configure
+SUBST_CLASSES+=			tcpwrappers
+SUBST_FILES.tcpwrappers=	builddefs.h
+SUBST_SED.tcpwrappers+=		-e 's,undef VSF_BUILD_TCPWRAPPERS,define VSF_BUILD_TCPWRAPPERS,g'
+SUBST_STAGE.tcpwrappers=	pre-configure
 .endif
