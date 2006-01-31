1bf85a33a7f0af352174332dac11865971217df5
tv <tv>
2006-01-31 20:19:02 +0000

    Backport a change from 1.5.22 to 1.5.18 that fixes some issues with temp dir creation when using relink-based finalization.

--- games/scummvm-fotaq/files/fotaq.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec scummvm -p@FOTAQDIR@ -f queen
--- regress/buildlink-transform/tests/libdir-pkgdir-slashdot.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: libdir-pkgdir-slashdot.mk,v 1.1.1.1 2004/08/27 21:34:58 jlam Exp $
-
-.PHONY: libdir-pkgdir-slashdot
-do-regress: libdir-pkgdir-slashdot
-libdir-pkgdir-slashdot:
-	@test="-L${LOCALBASE}/lib/.";					\
-	expected="-L${BUILDLINK_DIR}/lib";				\
-	${TEST_RESULT}
