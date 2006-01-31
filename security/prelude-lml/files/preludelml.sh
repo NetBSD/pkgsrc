4862b7793192516ff7c2e85a97c05847ae4ea4a5
shannonjr <shannonjr@pkgsrc.org>
2006-01-31 10:46:31 +0000

    Update to 0.9.2. Changes: - Get rid of the 1024 characters per line limitation (defined as per   the syslog RFC), since LML is not limited to parsing input from syslog   anymore. - Handle events in Clamav logging format as well as syslog. - Abstracted Squid chain regex to allow parsing of data directly   from Squid log files. - Introduced support for openhostapd. - Began expanding rulesets with additional_data and vendor-specific   classification data. - Various ruleset updates and bug fixes.
    Prelude-LML is a signature based log analyzer monitoring logfile and
    received syslog messages for suspicious activity. It handle events
    generated by a large set of components, including but not limited to:
    BigIP, Grsecurity, Honeyd, ipchains, Netfilter, ipfw, Nokia ipso,
    Nagios, Norton Antivirus Corporate Edition, NTsyslog, PAM, Portsentry,
    Postfix, Proftpd, ssh, etc.

--- security/prelude-lml/files/preludelml.sh
+++ security/prelude-lml/files/preludelml.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: preludelml.sh,v 1.1.1.1 2006/01/29 15:56:42 shannonjr Exp $
+# $NetBSD: preludelml.sh,v 1.2 2006/01/31 10:46:31 shannonjr Exp $
 #
 
 # PROVIDE: preludelml
@@ -11,8 +11,15 @@ $_rc_subr_loaded . /etc/rc.subr
 name="preludelml"
 rcvar=${name}
 required_files="@PKG_SYSCONFDIR@/prelude-lml/prelude-lml.conf"
-start_cmd="@PREFIX@/sbin/run-prelude-lml -d"
+start_precmd="preludelml_precommand"
+start_cmd="@PREFIX@/sbin/run-prelude-lml -d --pidfile @PRELUDE_LML_PID_DIR@/prelude-lml.pid"
 pidfile="@PRELUDE_LML_PID_DIR@/prelude-lml.pid"
+
+preludelml_precommand()
+{
+	/bin/mkdir -p @PRELUDE_LML_PID_DIR@
+	/usr/sbin/chown @PRELUDE_USER@:@PRELUDE_GROUP@ @PRELUDE_LML_PID_DIR@
+}
 
 load_rc_config $name
 run_rc_command "$1"
