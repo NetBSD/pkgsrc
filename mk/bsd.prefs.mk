2207d04891312024606bf48dd0a5adbf6af6c204
joerg <joerg>
2006-02-02 13:46:37 +0000

    Don't include define lower opsys version for the host name, e.g. use i386-pc-dragonflybsd instead of i386-pc-dragonflybsd1. Saves a few forks and execs and makes DragonFly more homogenous with NetBSD.

--- mk/bsd.prefs.mk
+++ mk/bsd.prefs.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.prefs.mk,v 1.218 2006/01/12 14:57:19 joerg Exp $
+# $NetBSD: bsd.prefs.mk,v 1.219 2006/02/02 13:46:37 joerg Exp $
 #
 # Make file, included to get the site preferences, if any.  Should
 # only be included by package Makefiles before any .if defined()
@@ -125,7 +125,6 @@ LOWER_OPSYS?=		dragonfly
 LOWER_ARCH!=		${UNAME} -p
 MACHINE_ARCH=		${LOWER_ARCH}
 MAKEFLAGS+=		LOWER_ARCH=${LOWER_ARCH:Q}
-LOWER_OPSYS_VERSUFFIX!=	echo ${LOWER_OS_VERSION} | ${CUT} -c -1
 LOWER_VENDOR?=		pc
 
 .elif ${OPSYS} == "FreeBSD"
