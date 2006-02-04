ff1aa4638a6380aea356cc3cac1b5fcc0a38d024
joerg <joerg@pkgsrc.org>
2006-02-04 18:03:57 +0000

    Add a b3.mk for the sake of other packages.

---
+++ devel/gettext-asprintf/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2006/02/04 18:03:57 joerg Exp $
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+GETTEXT_ASPRINTF_BUILDLINK3_MK:=	${GETTEXT_ASPRINTF_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gettext-asprintf
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngettext-asprintf}
+BUILDLINK_PACKAGES+=	gettext-asprintf
+
+.if !empty(GETTEXT_ASPRINTF_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.gettext-asprintf+=	gettext-asprintf>=0.14.5
+BUILDLINK_PKGSRCDIR.gettext-asprintf?=	../../devel/gettext-asprintf
+.endif	# GETTEXT_ASPRINTF_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
