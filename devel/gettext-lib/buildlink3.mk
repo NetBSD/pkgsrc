4fb31e1174b8781f54cab6259d4e12fc219c2a9d
joerg <joerg@pkgsrc.org>
2006-02-05 22:45:53 +0000

    Update gettext to 0.14.5. Many bugfixes and improvements, including support for relocable programs, Qt support, separation of the PO processing functions into a separate library and more.
    Reorganise the gettext infrastructure by splitting of the
    tools into devel/gettext-tools, which will be used by the tools
    framework. The remaining devel/gettext package contains
    gettextize and autopoint aka the infrastructure to embbed gettext
    into a package.
    
    Due to the ABI and API changes, a recursive revision bump will
    follow.
    
    Take blaim by receiving the maintainer hat.
    
    With input from jlam@, reed@ and wiz@.

--- devel/gettext-lib/buildlink3.mk
+++ devel/gettext-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2005/06/01 18:02:43 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2006/02/05 22:45:54 joerg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GETTEXT_BUILDLINK3_MK:=	${GETTEXT_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngettext}
 BUILDLINK_PACKAGES+=	gettext
 
 .if !empty(GETTEXT_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gettext+=	gettext-lib>=0.10.35nb1
-BUILDLINK_RECOMMENDED.gettext+=	gettext-lib>=0.11.5nb4
+BUILDLINK_DEPENDS.gettext+=	gettext-lib>=0.14.5
 BUILDLINK_PKGSRCDIR.gettext?=	../../devel/gettext-lib
 .endif	# GETTEXT_BUILDLINK3_MK
 
