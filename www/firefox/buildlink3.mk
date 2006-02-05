7d45c949e0eae31cd978da3596534386fa5a01e7
ghen <ghen@pkgsrc.org>
2006-02-05 21:03:33 +0000

    Bump BUILDLINK_RECOMMENDED to 1.5.0.1.

--- www/firefox/buildlink3.mk
+++ www/firefox/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/01/24 07:32:47 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2006/02/05 21:03:33 ghen Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 FIREFOX_BUILDLINK3_MK:=	${FIREFOX_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	firefox
 
 .if !empty(FIREFOX_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.firefox+=	firefox>=1.5
-BUILDLINK_RECOMMENDED.firefox+=	firefox>=1.5nb3
+BUILDLINK_RECOMMENDED.firefox+=	firefox>=1.5.0.1
 BUILDLINK_PKGSRCDIR.firefox?=	../../www/firefox
 .endif	# FIREFOX_BUILDLINK3_MK
 
