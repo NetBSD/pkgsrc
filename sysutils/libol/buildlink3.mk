c31c102145209b1cafa509ea2aa5b41fa6aaabe0
adrianp <adrianp@pkgsrc.org>
2006-02-06 18:31:23 +0000

    Update to 0.3.17 > 2005-11-23  Balazs Scheidler  <bazsi@bzorp.balabit> > >         * configure.in: bumped version number to 0.3.17 > >         * src/io.c (do_write): added workaround for broken destination >         sockets > >         * src/pkt_buffer.c (do_flush_stream): make sure data is not lost >         if A_WRITE returns failure by pushing it back to the buffer > > 2005-05-03  Balazs Scheidler  <bazsi@bzorp.balabit> > >         * configure.in: bumped version number to 0.3.16 > >         * src/io.c (io_iter): fixed possible nonblocking_fd leaks as >         unlinking an fd might have dropped some valid structures > >         * src/werror.c: fixed werror_hexdump and related functions, >         still #if 0ed but useful for tracking down gc related problems

--- sysutils/libol/buildlink3.mk
+++ sysutils/libol/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2005/03/06 11:10:29 adrianp Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/02/06 18:31:23 adrianp Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBOL_BUILDLINK3_MK:=	${LIBOL_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibol}
 BUILDLINK_PACKAGES+=	libol
 
 .if !empty(LIBOL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libol+=	libol>=0.3.15
+BUILDLINK_DEPENDS.libol+=	libol>=0.3.17
 BUILDLINK_PKGSRCDIR.libol?=	../../sysutils/libol
 .endif	# LIBOL_BUILDLINK3_MK
 
