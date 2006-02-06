6ad25ed8d9f23828392f245dab56e1cb4907a9ec
ghen <ghen>
2006-02-06 00:25:06 +0000

    Reindent + fix some pkglint warnings.

--- www/mozilla/buildlink3.mk
+++ www/mozilla/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.16 2006/02/05 23:11:25 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2006/02/06 00:25:06 ghen Exp $
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
-MOZILLA_BUILDLINK3_MK:=	${MOZILLA_BUILDLINK3_MK}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+MOZILLA_BUILDLINK3_MK:=		${MOZILLA_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	mozilla
+BUILDLINK_DEPENDS+=		mozilla
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmozilla}
-BUILDLINK_PACKAGES+=	mozilla
+BUILDLINK_PACKAGES:=		${BUILDLINK_PACKAGES:Nmozilla}
+BUILDLINK_PACKAGES+=		mozilla
 
 .if !empty(MOZILLA_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.mozilla+=	mozilla>=1.7.5
@@ -23,4 +23,4 @@ BUILDLINK_PKGSRCDIR.mozilla?=	../../www/mozilla
 .include "../../net/ORBit/buildlink3.mk"
 .include "../../x11/gtk/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
