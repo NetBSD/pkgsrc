33fb36ed4cbd5bee394fca83dc9f491da84719c9
ghen <ghen@pkgsrc.org>
2006-02-06 00:25:06 +0000

    Reindent + fix some pkglint warnings.

--- www/mozilla-gtk2/buildlink3.mk
+++ www/mozilla-gtk2/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.14 2006/02/05 23:11:26 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2006/02/06 00:25:06 ghen Exp $
 
-BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-MOZILLA_GTK2_BUILDLINK3_MK:=	${MOZILLA_GTK2_BUILDLINK3_MK}+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+MOZILLA_GTK2_BUILDLINK3_MK:=		${MOZILLA_GTK2_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	mozilla-gtk2
+BUILDLINK_DEPENDS+=			mozilla-gtk2
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmozilla-gtk2}
-BUILDLINK_PACKAGES+=	mozilla-gtk2
+BUILDLINK_PACKAGES:=			${BUILDLINK_PACKAGES:Nmozilla-gtk2}
+BUILDLINK_PACKAGES+=			mozilla-gtk2
 
 .if !empty(MOZILLA_GTK2_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.mozilla-gtk2+=	mozilla-gtk2>=1.7.5
@@ -22,4 +22,4 @@ BUILDLINK_PKGSRCDIR.mozilla-gtk2?=	../../www/mozilla-gtk2
 .include "../../net/libIDL/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
