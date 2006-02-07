17959c07dd36555e54f12c3f5e1506e2b9adbf91
drochner <drochner@pkgsrc.org>
2006-02-07 21:27:28 +0000

    update to 2.8.4 changes: -Split out gobject wrappers -Bug fixes

--- x11/py-gtk2/buildlink3.mk
+++ x11/py-gtk2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2006/02/05 23:11:41 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2006/02/07 21:27:28 drochner Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PY_GTK2_BUILDLINK3_MK:=	${PY_GTK2_BUILDLINK3_MK}+
@@ -13,11 +13,11 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npygtk2}
 BUILDLINK_PACKAGES+=	pygtk2
 
 .if !empty(PY_GTK2_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.pygtk2+=	${PYPKGPREFIX}-gtk2>=2.4.0
-BUILDLINK_RECOMMENDED.pygtk2+=	${PYPKGPREFIX}-gtk2>=2.8.2nb2
+BUILDLINK_DEPENDS.pygtk2+=	${PYPKGPREFIX}-gtk2>=2.8.4
 BUILDLINK_PKGSRCDIR.pygtk2?=	../../x11/py-gtk2
 .endif	# PY_GTK2_BUILDLINK3_MK
 
+.include "../../devel/py-gobject/buildlink3.mk"
 .include "../../devel/libglade2/buildlink3.mk"
 .include "../../math/py-Numeric/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
