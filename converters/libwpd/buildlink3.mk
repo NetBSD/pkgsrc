d6c898cc6f7f53648bbd93f43ea111f27b4aa1ea
adam <adam@pkgsrc.org>
2006-02-08 09:42:53 +0000

    Changes 0.8.4: - Fix compilation with gcc >=  4.1.0 - Initial support for tables in WP5.x format - Fix compilation on MacOSX - Conversion of font name, size and colour for WP3.x format - Collapse the LL and HL Listeners into one classe each - Initial support of tables in WP3.x format including the cell background   colour - Conversion of sections with multiple columns in WP3.x format - Conversion of foot/endnotes in WP3.x format - Adding an assignment operator to WPXString class as well as trying   to make it much more a std::string wrapper

--- converters/libwpd/buildlink3.mk
+++ converters/libwpd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/02/05 23:08:29 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2006/02/08 09:42:53 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBWPD_BUILDLINK3_MK:=	${LIBWPD_BUILDLINK3_MK}+
@@ -11,12 +11,11 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibwpd}
 BUILDLINK_PACKAGES+=	libwpd
 
 .if !empty(LIBWPD_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libwpd?=	libwpd>=0.8.1nb1
-BUILDLINK_RECOMMENDED.libwpd?=	libwpd>=0.8.1nb4
+BUILDLINK_DEPENDS.libwpd+=	libwpd>=0.8.1nb1
+BUILDLINK_RECOMMENDED.libwpd?=	libwpd>=0.8.4
 BUILDLINK_PKGSRCDIR.libwpd?=	../../converters/libwpd
 .endif	# LIBWPD_BUILDLINK3_MK
 
-.include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/libgsf/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
