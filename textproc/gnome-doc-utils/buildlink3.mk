d700ba1a17b54b98bd525c7490c4429cce5f4f65
jmmv <jmmv@pkgsrc.org>
2006-02-10 08:42:44 +0000

    Only allow python 2.3 or 2.4, as older versions cause problems at runtime WRT Unicode.  Found by jnemeth@.  Bump PKGREVISION to 3.

--- textproc/gnome-doc-utils/buildlink3.mk
+++ textproc/gnome-doc-utils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/02/05 23:11:02 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2006/02/10 08:42:44 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_DOC_UTILS_BUILDLINK3_MK:=	${GNOME_DOC_UTILS_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_PACKAGES+=	gnome-doc-utils
 .if !empty(GNOME_DOC_UTILS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPMETHOD.gnome-doc-utils+=	build
 BUILDLINK_DEPENDS.gnome-doc-utils+=	gnome-doc-utils>=0.4.4
-BUILDLINK_RECOMMENDED.gnome-doc-utils?=	gnome-doc-utils>=0.4.4nb2
+BUILDLINK_RECOMMENDED.gnome-doc-utils?=	gnome-doc-utils>=0.4.4nb3
 BUILDLINK_PKGSRCDIR.gnome-doc-utils?=	../../textproc/gnome-doc-utils
 .endif	# GNOME_DOC_UTILS_BUILDLINK3_MK
 
