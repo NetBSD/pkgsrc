1817f04910f4ceb6c352abbd7961558876759514
dmcmahill <dmcmahill@pkgsrc.org>
2006-02-11 02:40:49 +0000

    Repair the version number for the dependency.  Hi Joerg!

--- cad/libgeda/buildlink3.mk
+++ cad/libgeda/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2006/02/05 23:08:22 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2006/02/11 02:40:49 dmcmahill Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBGEDA_BUILDLINK3_MK:=	${LIBGEDA_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgeda}
 BUILDLINK_PACKAGES+=	libgeda
 
 .if !empty(LIBGEDA_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libgeda+=	libgeda-20060123
+BUILDLINK_DEPENDS.libgeda+=	libgeda-20060123{,nb[0-9]*}
 BUILDLINK_RECOMMENDED.libgeda?=	libgeda>=20060123nb1
 BUILDLINK_PKGSRCDIR.libgeda?=	../../cad/libgeda
 .endif	# LIBGEDA_BUILDLINK3_MK
