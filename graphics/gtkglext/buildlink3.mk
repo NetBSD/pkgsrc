834cfd486b25a2edb653c49181355eadfa31a480
adam <adam@pkgsrc.org>
2006-02-13 20:56:15 +0000

    Changes 1.2.0: * This is the first stable release of GtkGLExt 1.0 since maintainership changed   to Tim Shead, tshead@k-3d.com

--- graphics/gtkglext/buildlink3.mk
+++ graphics/gtkglext/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2006/02/05 23:09:33 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2006/02/13 20:56:15 adam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GTKGLEXT_BUILDLINK3_MK:=	${GTKGLEXT_BUILDLINK3_MK}+
@@ -16,8 +16,7 @@ BUILDLINK_RECOMMENDED.gtkglext+=	gtkglext>=1.0.6nb3
 BUILDLINK_PKGSRCDIR.gtkglext?=	../../graphics/gtkglext
 .endif	# GTKGLEXT_BUILDLINK3_MK
 
-.include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../graphics/glu/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
