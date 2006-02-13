bdd84d6a862b361e1953d4641717564f845e1b7d
rillig <rillig@pkgsrc.org>
2006-02-13 15:23:03 +0000

    Converted the package to the options framework. The only supported option is "gtk1" (disabled by default). This makes the pornview-gtk2 package obsolete. Bumped PKGREVISION.

--- graphics/pornview/options.mk
+++ graphics/pornview/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1.1.1 2006/02/11 13:06:43 rillig Exp $
+# $NetBSD: options.mk,v 1.2 2006/02/13 15:23:03 rillig Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.pornview
-PKG_SUPPORTED_OPTIONS=		exif
+PKG_SUPPORTED_OPTIONS=		exif gtk1
 PKG_OPTIONS_OPTIONAL_GROUPS=	movie
 PKG_OPTIONS_GROUP.movie=	mplayer xine
 PKG_SUGGESTED_OPTIONS=		exif
@@ -12,6 +12,17 @@ PKG_SUGGESTED_OPTIONS=		exif
 CONFIGURE_ARGS+=	--enable-exif
 .else
 CONFIGURE_ARGS+=	--disable-exif
+.endif
+
+.if !empty(PKG_OPTIONS:Mgtk1)
+CONFIGURE_ARGS+=	--without-gtk2
+.  include "../../graphics/gdk-pixbuf/buildlink3.mk"
+.  include "../../x11/gtk/buildlink3.mk"
+.else
+CONFIURE_ARGS+=	--with-gtk2
+USE_TOOLS+=		pkg-config
+.  include "../../devel/glib2/buildlink3.mk"
+.  include "../../x11/gtk2/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mmplayer)
