4223da088a370aee0de719f4667d7915e95425fe
rillig <rillig@pkgsrc.org>
2006-02-15 22:02:56 +0000

    Converted to the SUBST framework. Fixed all pkglint warnings.

--- lang/tcl-itcl-current/buildlink3.mk
+++ lang/tcl-itcl-current/buildlink3.mk
@@ -1,6 +1,6 @@
-# $NetBSD: buildlink3.mk,v 1.3 2006/02/05 23:09:52 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2006/02/15 22:02:56 rillig Exp $
 
-BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 TCL_ITCL_CURRENT_BUILDLINK3_MK:=	${TCL_ITCL_CURRENT_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
@@ -11,16 +11,16 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ntcl-itcl-current}
 BUILDLINK_PACKAGES+=	tcl-itcl-current
 
 .if !empty(TCL_ITCL_CURRENT_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.tcl-itcl-current+=	tcl-itcl-current>=20040920
+BUILDLINK_DEPENDS.tcl-itcl-current+=		tcl-itcl-current>=20040920
 BUILDLINK_RECOMMENDED.tcl-itcl-current?=	tcl-itcl-current>=20040920nb4
-BUILDLINK_PKGSRCDIR.tcl-itcl-current?=	../../lang/tcl-itcl-current
+BUILDLINK_PKGSRCDIR.tcl-itcl-current?=		../../lang/tcl-itcl-current
 
-BUILDLINK_TRANSFORM+=   l:itcl:itcl33
-BUILDLINK_TRANSFORM+=   l:itk:itk33
+BUILDLINK_TRANSFORM+=	l:itcl:itcl33
+BUILDLINK_TRANSFORM+=	l:itk:itk33
 
 .endif	# TCL_ITCL_CURRENT_BUILDLINK3_MK
 
 .include "../../lang/tcl/buildlink3.mk"
 .include "../../x11/tk/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
