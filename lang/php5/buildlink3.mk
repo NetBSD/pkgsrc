0e7b98e72d89c6f13cbab1d76914c37aed753dc5
adam <adam@pkgsrc.org>
2006-02-17 09:48:01 +0000

    Install bin/pear with correct file permissions

--- lang/php5/buildlink3.mk
+++ lang/php5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2006/02/06 06:39:59 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2006/02/17 09:48:01 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PHP_BUILDLINK3_MK:=	${PHP_BUILDLINK3_MK}+
@@ -11,11 +11,11 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nphp}
 BUILDLINK_PACKAGES+=	php
 
 .if !empty(PHP_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.php+=	php>=5.1.2
+BUILDLINK_DEPENDS.php+=		php>=5.1.2
 BUILDLINK_RECOMMENDED.php+=	php>=5.1.2
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php5
 .endif	# PHP_BUILDLINK3_MK
 
 .include "../../textproc/libxml2/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
