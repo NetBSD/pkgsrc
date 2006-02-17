39234d4ac2d0677a8da9cb16b92c2809220be3cf
martti <martti@pkgsrc.org>
2006-02-17 07:04:25 +0000

    Fixed warnings found by pkglint -Wall.

--- mail/squirrelmail/buildlink3.mk
+++ mail/squirrelmail/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/02/05 23:09:58 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2006/02/17 07:04:25 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SQUIRRELMAIL_BUILDLINK3_MK:=	${SQUIRRELMAIL_BUILDLINK3_MK}+
@@ -16,4 +16,4 @@ BUILDLINK_RECOMMENDED.squirrelmail?=	squirrelmail>=1.4.5nb5
 BUILDLINK_PKGSRCDIR.squirrelmail?=	../../mail/squirrelmail
 .endif	# SQUIRRELMAIL_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
--- mail/squirrelmail/plugin.mk
+++ mail/squirrelmail/plugin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: plugin.mk,v 1.2 2005/12/05 20:50:35 rillig Exp $
+# $NetBSD: plugin.mk,v 1.3 2006/02/17 07:04:25 martti Exp $
 
 DISTNAME?=		${PLUGIN_NAME}-${PLUGIN_VERSION}
 PKGNAME=		sm-${PLUGIN_NAME}-${PLUGIN_VERSION}
@@ -18,7 +18,7 @@ INSTALLATION_DIRS+=	${PLUGIN_DIR}
 PLIST_SUBST+=		PLUGIN_DIR=${PLUGIN_DIR:Q}
 
 do-install:
-	cd ${WRKSRC} && \
+	cd ${WRKSRC:Q} && \
 		${FIND} . -type f ! -name '*.orig' 2>/dev/null | \
 		${PAX} -rw ${FULL_PLUGIN_DIR}
 
