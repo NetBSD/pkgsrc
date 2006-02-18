202bfbc702b390143c101ebfbaa313fc563934a2
rillig <rillig@pkgsrc.org>
2006-02-18 11:39:46 +0000

    - Guess the data type of *PATH variables to be Pathname, of *PATHS to be   List of Pathname.

--- pkgtools/pkglint/files/pkglint.pl
+++ pkgtools/pkglint/files/pkglint.pl
@@ -1,5 +1,5 @@
 #! @PERL@
-# $NetBSD: pkglint.pl,v 1.522 2006/02/17 18:35:34 rillig Exp $
+# $NetBSD: pkglint.pl,v 1.523 2006/02/18 11:39:46 rillig Exp $
 #
 
 # pkglint - static analyzer and checker for pkgsrc packages
@@ -3194,6 +3194,8 @@ sub checkline_mk_vartype($$$$$) {
 				: ($varname =~ qr"DIR$") ? "Pathname"
 				: ($varname =~ qr"FILES$") ? "List of Pathmask"
 				: ($varname =~ qr"FILE$") ? "Pathname"
+				: ($varname =~ qr"PATH$") ? "Pathname"
+				: ($varname =~ qr"PATHS$") ? "List of Pathname"
 				: ($varname =~ qr"_USER$") ? "UserGroupName"
 				: ($varname =~ qr"_GROUP$") ? "UserGroupName"
 				: ($varname =~ qr"_ENV$") ? "List+ of ShellWord"
