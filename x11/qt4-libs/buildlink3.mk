9b562b6977ce960874d2b1d29a19ca9e4fa2eef3
adam <adam@pkgsrc.org>
2006-02-20 12:07:12 +0000

    Qt(TM) is a GUI software toolkit. Qt simplifies the task of writing and maintaining GUI (graphical user interface) applications.
    Qt is written in C++ and is fully object-oriented. It has everything you need
    to create professional GUI applications. And it enables you to create them
    quickly.
    
    Qt is a multi-platform toolkit. When developing software with Qt, you can run
    it on the X Window System (Unix/X11) or Microsoft Windows NT and Windows 95/98.
    Simply recompile your source code on the platform you want.
    
    Qt cuts down the complexity in implementing large and complex systems. Its
    ingenious signal-slot technology enables true component programming.

---
+++ x11/qt4-libs/buildlink3.mk
@@ -0,0 +1,44 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/02/20 12:08:11 adam Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+QT4_LIBS_BUILDLINK3_MK:=	${QT4_LIBS_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	qt4-libs
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nqt4-libs}
+BUILDLINK_PACKAGES+=	qt4-libs
+
+.if !empty(QT4_LIBS_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.qt4-libs+=	qt4-libs>=4.1.0
+BUILDLINK_PKGSRCDIR.qt4-libs?=	../../x11/qt4-libs
+
+BUILDLINK_INCDIRS.qt4-libs+=	qt4/include
+BUILDLINK_LIBDIRS.qt4-libs+=	qt4/lib
+.endif	# QT4_LIBS_BUILDLINK3_MK
+
+QTDIR=	${BUILDLINK_PREFIX.qt4-libs}/qt4
+BUILDLINK_PASSTHRU_DIRS+= ${QTDIR}
+
+PTHREAD_OPTS+=	require
+
+.include "../../fonts/Xft2/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../graphics/mng/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../x11/Xrandr/buildlink3.mk"
+.include "../../x11/xcursor/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+
+.if !defined(BUILD_QT4)
+CONFIGURE_ENV+=		QTDIR=${QTDIR:Q}
+MAKE_ENV+=		QTDIR=${QTDIR:Q}
+.endif
+
+CONFIGURE_ENV+=		MOC="${QTDIR}/bin/moc"
+MAKE_ENV+=		MOC="${QTDIR}/bin/moc"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
---
+++ x11/qt4-tools/buildlink3.mk
@@ -0,0 +1,33 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/02/20 12:09:01 adam Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+QT4_TOOLS_BUILDLINK3_MK:=	${QT4_TOOLS_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	qt4-tools
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nqt4-tools}
+BUILDLINK_PACKAGES+=	qt4-tools
+
+.if !empty(QT4_TOOLS_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.qt4-tools+=	qt4-tools>=4.1.0
+BUILDLINK_PKGSRCDIR.qt4-tools?=	../../x11/qt4-tools
+BUILDLINK_DEPMETHOD.qt4-tools?=	build
+.endif	# QT4_TOOLS_BUILDLINK3_MK
+
+.include "../../x11/qt4-libs/buildlink3.mk"
+
+CONFIGURE_ENV+=		UIC="${QTDIR}/bin/uic"
+MAKE_ENV+=		UIC="${QTDIR}/bin/uic"
+CONFIGURE_ENV+=		QMAKE="${QTDIR}/bin/qmake"
+MAKE_ENV+=		QMAKE="${QTDIR}/bin/qmake"
+
+# the way the spec files are currently instealled via pkgsrc, they
+# are all identical anyway, so just pick one and point to it.
+# Without specifying QMAKESPEC, qmake can't figure it out on some
+# platforms (solaris for example)
+CONFIGURE_ENV+=		QMAKESPEC=${QTDIR}/mkspecs/netbsd-g++
+MAKE_ENV+=		QMAKESPEC=${QTDIR}/mkspecs/netbsd-g++
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
