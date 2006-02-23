e40779864228fd9bfa2bb22b1b4d54bfcb6dd5e4
jlam <jlam@pkgsrc.org>
2006-02-23 23:17:30 +0000

    Tell the buildlink3 framework where the installed shared libraries are so that the path isn't automatically stripped out by the wrappers.

--- x11/xorg-libs/buildlink3.mk
+++ x11/xorg-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2006/02/05 23:11:48 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2006/02/23 23:17:30 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XORG_LIBS_BUILDLINK3_MK:=	${XORG_LIBS_BUILDLINK3_MK}+
@@ -19,8 +19,9 @@ BUILDLINK_PACKAGES:=	xorg-libs ${BUILDLINK_PACKAGES}
 .if !empty(XORG_LIBS_BUILDLINK3_MK:M+)
 
 BUILDLINK_DEPENDS.xorg-libs+=		xorg-libs>=6.9.0
-BUILDLINK_RECOMMENDED.xorg-libs?=		xorg-libs>=6.9.0nb2
+BUILDLINK_RECOMMENDED.xorg-libs?=	xorg-libs>=6.9.0nb2
 BUILDLINK_PKGSRCDIR.xorg-libs?=		../../x11/xorg-libs
+BUILDLINK_LIBDIRS.xorg-libs?=		${X11ROOT_PREFIX}/lib
 BUILDLINK_TRANSFORM.xorg-libs+=		-e "s|/${X11ROOT_PREFIX}/|/|"
 
 .endif  # XORG_LIBS_BUILDLINK3_MK
