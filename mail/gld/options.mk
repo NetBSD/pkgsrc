c8502cc31810551bbb15d7bd01b11dfa86d0adba
wiz <wiz@pkgsrc.org>
2006-03-02 20:49:39 +0000

    Fix build both for mysql and pgsql backends. Addresses second part of PR 32969 by Greg Oster.

--- mail/gld/options.mk
+++ mail/gld/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2006/03/02 20:41:30 wiz Exp $
+# $NetBSD: options.mk,v 1.5 2006/03/02 20:49:39 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.gld
 PKG_OPTIONS_REQUIRED_GROUPS=	database
@@ -10,9 +10,13 @@ PKG_SUGGESTED_OPTIONS=		mysql
 .if !empty(PKG_OPTIONS:Mmysql)
 .  include "../../mk/mysql.buildlink3.mk"
 CONFIGURE_ARGS+=	--with-mysql=${BUILDLINK_PREFIX.mysql-client}
+LIBS+=			-L${BUILDLINK_PREFIX.mysql-client}/lib \
+			-Wl,-R${BUILDLINK_PREFIX.mysql-client}/lib -lmysqlclient
 .endif
 
 .if !empty(PKG_OPTIONS:Mpgsql)
 .  include "../../mk/pgsql.buildlink3.mk"
 CONFIGURE_ARGS+=	--with-pgsql=${BUILDLINK_PREFIX.postgresql-libs}
+LIBS+=			-L${BUILDLINK_PREFIX.mysql-client}/lib \
+			-Wl,-R${BUILDLINK_PREFIX.mysql-client}/lib -lpq
 .endif
