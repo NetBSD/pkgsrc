6433375214f2f4cd2a1ce0f275b8640a41481124
wiz <wiz@pkgsrc.org>
2006-03-02 20:54:08 +0000

    Make MESSAGE mysql-specific, since it is. Final part of PR 32969 by Greg Oster.

--- mail/gld/options.mk
+++ mail/gld/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2006/03/02 20:49:39 wiz Exp $
+# $NetBSD: options.mk,v 1.6 2006/03/02 20:54:08 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.gld
 PKG_OPTIONS_REQUIRED_GROUPS=	database
@@ -11,12 +11,13 @@ PKG_SUGGESTED_OPTIONS=		mysql
 .  include "../../mk/mysql.buildlink3.mk"
 CONFIGURE_ARGS+=	--with-mysql=${BUILDLINK_PREFIX.mysql-client}
 LIBS+=			-L${BUILDLINK_PREFIX.mysql-client}/lib \
-			-Wl,-R${BUILDLINK_PREFIX.mysql-client}/lib -lmysqlclient
+	${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.mysql-client}/lib -lmysqlclient
+MESSAGE_SRC+=		MESSAGE.mysql
 .endif
 
 .if !empty(PKG_OPTIONS:Mpgsql)
 .  include "../../mk/pgsql.buildlink3.mk"
 CONFIGURE_ARGS+=	--with-pgsql=${BUILDLINK_PREFIX.postgresql-libs}
 LIBS+=			-L${BUILDLINK_PREFIX.mysql-client}/lib \
-			-Wl,-R${BUILDLINK_PREFIX.mysql-client}/lib -lpq
+	${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.mysql-client}/lib -lpq
 .endif
