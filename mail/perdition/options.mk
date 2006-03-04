1cf093c7dcde7f01795fcdbf611a5d2d85882ff9
tv <tv@pkgsrc.org>
2006-03-04 16:06:29 +0000

    Make "pam" an option as it is with many other pkgs; it is now off by default.  This correctly fixes the PLIST problem with pam, and makes the package more consistent with the rest of pkgsrc.

--- mail/perdition/options.mk
+++ mail/perdition/options.mk
@@ -1,10 +1,22 @@
-# $NetBSD: options.mk,v 1.1 2006/01/31 21:07:30 tv Exp $
+# $NetBSD: options.mk,v 1.2 2006/03/04 16:06:29 tv Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.perdition
-PKG_SUPPORTED_OPTIONS=	ssl
+PKG_SUPPORTED_OPTIONS=	ssl pam
 PKG_SUGGESTED_OPTIONS=	ssl
 
 .include "../../mk/bsd.options.mk"
+
+###
+### Build with PAM support.
+###
+.if !empty(PKG_OPTIONS:Mpam)
+.  include "../../mk/pam.buildlink3.mk"
+PLIST_SUBST+=		PERDITION_HAVE_PAM=
+.else
+# prevent recognition of PAM in the base system
+CONFIGURE_ENV+=		ac_cv_header_security_pam_appl_h=no
+PLIST_SUBST+=		PERDITION_HAVE_PAM="@comment "
+.endif
 
 ###
 ### Build with OpenSSL as the underlying crypto library.
