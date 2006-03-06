0407dc4d77138f42920ea2b0f311679c99d47fc7
wiz <wiz>
2006-03-06 00:18:10 +0000

    Belatedly bump PKGREVISION for all libtasn1 dependencies, since libtasn1 had a shlib major bump. Also update dependencies in bl3.mk files.
    Addresses PR 32998 by Robert Elz.

--- devel/libgsf/buildlink3.mk
+++ devel/libgsf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2006/02/05 23:08:45 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2006/03/06 00:18:11 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBGSF_BUILDLINK3_MK:=	${LIBGSF_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libgsf
 
 .if !empty(LIBGSF_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libgsf+=	libgsf>=1.13.3
-BUILDLINK_RECOMMENDED.libgsf?=	libgsf>=1.13.3nb3
+BUILDLINK_RECOMMENDED.libgsf?=	libgsf>=1.14.0
 BUILDLINK_PKGSRCDIR.libgsf?=	../../devel/libgsf
 .endif	# LIBGSF_BUILDLINK3_MK
 
