0a3c815742f95893500eb344e2f0d667f85e275e
wiz <wiz>
2006-03-06 00:18:10 +0000

    Belatedly bump PKGREVISION for all libtasn1 dependencies, since libtasn1 had a shlib major bump. Also update dependencies in bl3.mk files.
    Addresses PR 32998 by Robert Elz.

--- security/libprelude/buildlink3.mk
+++ security/libprelude/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/02/05 23:10:44 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2006/03/06 00:18:10 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBPRELUDE_BUILDLINK3_MK:=	${LIBPRELUDE_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libprelude
 
 .if !empty(LIBPRELUDE_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libprelude+=	libprelude>=0.9.0
-BUILDLINK_RECOMMENDED.libprelude?=	libprelude>=0.9.4nb1
+BUILDLINK_RECOMMENDED.libprelude?=	libprelude>=0.9.6.1
 BUILDLINK_PKGSRCDIR.libprelude?=	../../security/libprelude
 .endif	# LIBPRELUDE_BUILDLINK3_MK
 
