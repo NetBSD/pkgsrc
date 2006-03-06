574ad96877558d7282130dc62094bc0f3705913b
jlam <jlam>
2006-03-06 05:25:45 +0000

    Teach the tools framework how to supply a pkgsrc version of install-info if a native one isn't available.  We ensure that the "install-info" tool in the tools directory is a no-op since the real info file registration is handled by the INSTALL/DEINSTALL script in pkgsrc/mk/pkginstall/install-info.

--- mk/texinfo.mk
+++ mk/texinfo.mk
@@ -1,4 +1,4 @@
-# $NetBSD: texinfo.mk,v 1.32 2006/03/05 16:27:29 jlam Exp $
+# $NetBSD: texinfo.mk,v 1.33 2006/03/06 05:25:45 jlam Exp $
 
 #
 # This Makefile fragment is included by bsd.pkg.mk when INFO_FILES is
@@ -17,25 +17,7 @@ TEXINFO_MK=	# defined
 # files and the Info dir file.
 INFO_DIR?=	info
 
-# Does the system have the install-info command?
-# Any version will fit (really?).
-_INSTALL_INFO=
-.  for _i_ in /usr/bin/install-info /sbin/install-info
-.    if exists(${_i_}) && (${LOCALBASE} != ${_i_:C|/[^/]+/install-info$||})
-_INSTALL_INFO=			${_i_}
-.    endif
-.  endfor
-
-# If no install-info was found provide one with the pkg_install-info package.
-# And set INSTALL_INFO to the install-info command it provides.
-.  if empty(_INSTALL_INFO)
-_PKG_INSTALL_INFO_PREFIX_DEFAULT=	${LOCALBASE}
-DEPENDS+=	pkg_install-info-[0-9]*:../../pkgtools/pkg_install-info
-EVAL_PREFIX+=	_PKG_INSTALL_INFO_PREFIX=pkg_install-info
-INSTALL_INFO=	${_PKG_INSTALL_INFO_PREFIX}/bin/pkg_install-info
-.  else
-INSTALL_INFO=	${_INSTALL_INFO}
-.  endif
+USE_TOOLS+=	install-info:run
 
 # Generate INSTALL/DEINSTALL scripts code for handling install-info.
 INSTALL_EXTRA_TMPL+=	${.CURDIR}/../../mk/install/install-info
