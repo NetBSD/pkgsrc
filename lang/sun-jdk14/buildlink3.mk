bf503b9232cd4204bd716d7cb05f09f9845fd5a7
wiz <wiz>
2006-03-07 04:36:21 +0000

    Bump BUILDLINK_RECOMMENDED for security fix.

--- lang/sun-jdk14/buildlink3.mk
+++ lang/sun-jdk14/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/03/02 21:37:31 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/03/07 04:39:19 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SUN_JDK14_BUILDLINK3_MK:=	${SUN_JDK14_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_PACKAGES+=	sun-jdk14
 .if !empty(SUN_JDK14_BUILDLINK3_MK:M+)
 
 BUILDLINK_DEPENDS.sun-jdk14+=	sun-jdk14-[0-9]*
-BUILDLINK_RECOMMENDED.sun-jdk14?=	sun-jdk14>=2.9nb1
+BUILDLINK_RECOMMENDED.sun-jdk14?=	sun-jdk14>=2.11
 BUILDLINK_PKGSRCDIR.sun-jdk14?=	../../lang/sun-jdk14
 BUILDLINK_DEPMETHOD.sun-jdk14?= build
 
