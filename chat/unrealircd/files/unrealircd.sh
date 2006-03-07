6693b5dc22fb8ab185d67aa520199d4dde3aa660
adrianp <adrianp@pkgsrc.org>
2006-03-07 17:23:51 +0000

    pkglintification Add rehash option to rc.d script Remove dud mirror site Sort PLIST Bump pkgrevision

--- chat/unrealircd/files/unrealircd.sh
+++ chat/unrealircd/files/unrealircd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: unrealircd.sh,v 1.1.1.1 2005/11/29 19:17:03 adrianp Exp $
+# $NetBSD: unrealircd.sh,v 1.2 2006/03/07 17:23:51 adrianp Exp $
 #
 # PROVIDE: unrealircd
 # REQUIRE: DAEMON
@@ -16,8 +16,16 @@ name="unrealircd"
 rcvar=${name}
 command="@PREFIX@/sbin/ircd"
 required_files="@UIRCD_HOME@/unrealircd.conf"
+pidfile="@UIRCD_HOME@/ircd.pid"
 unrealircd_user="@UIRCD_USER@"
 unrealircd_group="@UIRCD_GROUP@"
+extra_commands="rehash"
+rehash_cmd="unrealircd_rehash"
+
+unrealircd_rehash()
+{
+	kill -1 `@CAT@ $pidfile`
+}
 
 load_rc_config $name
 run_rc_command "$1"
