c84fb4999c9f9fddd6e09866b2067e30f1e9a418
wiz <wiz@pkgsrc.org>
2006-03-08 06:33:31 +0000

    Update to 0.43. Update to 0.42 provided in PR 32760 by George Michaelson.
    0.43:
    SSL bugfixes.
    
    0.42:
    SMTP addition.
    bugfixes.
    
    2005-12-02 - libetpan-0.41
    
        * release 0.41 - Transfixion
            * passphrase is implemented for S/MIME and PGP.
            * avoid symbols conflict between GnuTLS and OpenSSL.
    
    2005-10-07 - libetpan-0.40 - hoa
    
        * release 0.40 - Kashmir
    
    	* SSL initialization is optional.
    
    2005-09-16 - libetpan-0.39 - hoa
    
        * release 0.39 - Deliverance
    
    	* compatibility with GnuTLS
    	* SASL for higher level
    
    Add cyrus-sasl dependency.
    Bump RECOMMENDED for shlib major bump.

--- mail/libetpan/buildlink3.mk
+++ mail/libetpan/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/01/06 15:10:05 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2006/03/08 06:33:31 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBETPAN_BUILDLINK3_MK:=	${LIBETPAN_BUILDLINK3_MK}+
@@ -12,11 +12,13 @@ BUILDLINK_PACKAGES+=	libetpan
 
 .if !empty(LIBETPAN_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libetpan+=	libetpan>=0.38nb1
+BUILDLINK_RECOMMENDED.libetpan?=	libetpan>=0.43
 BUILDLINK_PKGSRCDIR.libetpan?=	../../mail/libetpan
 .endif	# LIBETPAN_BUILDLINK3_MK
 
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../databases/db4/buildlink3.mk"
+.include "../../security/cyrus-sasl2/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 
