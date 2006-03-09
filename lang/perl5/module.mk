598dfc557a931edeae77ab61fcbd4d689c2b204e
itohy <itohy>
2006-03-09 11:10:47 +0000

    Make perl5 modules build with pthread library from pkgsrc.

--- lang/perl5/module.mk
+++ lang/perl5/module.mk
@@ -1,4 +1,4 @@
-# $NetBSD: module.mk,v 1.50 2005/10/19 04:40:23 jlam Exp $
+# $NetBSD: module.mk,v 1.51 2006/03/09 11:10:47 itohy Exp $
 #
 # This Makefile fragment is intended to be included by packages that build
 # and install perl5 modules.
@@ -176,5 +176,7 @@ LDFLAGS+=	${PERL5_LDFLAGS}
 .endif
 
 .include "../../lang/perl5/packlist.mk"
+
+.include "../../mk/pthread.buildlink3.mk"
 
 .endif	# _PERL5_MODULE_MK
