6892a49f9da1d690159440f4d07e46218544fd7a
jlam <jlam@pkgsrc.org>
2006-03-09 20:23:13 +0000

    Reimplement OSS audio handling in pkgsrc.  Add two new files:
    	oss.buildlink3.mk
    	oss.builtin.mk
    
    that work like modern buildlink3.mk and builtin.mk files do.  We no
    longer support audio/oss (which will be removed shortly).  Mark
    ossaudio.buildlink3.mk for deprecation after all references to it
    have been replaced with "oss.buildlink3.mk" in package Makefiles.

---
+++ mk/oss.buildlink3.mk
@@ -0,0 +1,29 @@
+# $NetBSD: oss.buildlink3.mk,v 1.1 2006/03/09 20:23:13 jlam Exp $
+#
+# This Makefile fragment is included by packages that require an Open Sound
+# System (OSS) implementation.
+#
+OSS_BUILDLINK3_MK:=	${OSS_BUILDLINK3_MK}+
+
+BUILDLINK_PACKAGES:=		${BUILDLINK_PACKAGES:Noss}
+BUILDLINK_PACKAGES+=		oss
+BUILDLINK_BUILTIN_MK.oss=	../../mk/oss.builtin.mk
+
+.if !empty(OSS_BUILDLINK3_MK:M+)
+LIBOSSAUDIO?=		${BUILDLINK_LDADD.oss}
+.  if (${OPSYS} == "Linux") && exists(/dev/dsp)
+DEVOSSAUDIO?=		/dev/dsp
+DEVOSSSOUND?=		/dev/dsp
+.  else
+DEVOSSAUDIO?=		/dev/audio
+DEVOSSSOUND?=		/dev/sound
+.  endif
+
+CONFIGURE_ENV+=		LIBOSSAUDIO=${LIBOSSAUDIO:Q}
+CONFIGURE_ENV+=		DEVOSSAUDIO=${DEVOSSAUDIO:Q}
+CONFIGURE_ENV+=		DEVOSSSOUND=${DEVOSSSOUND:Q}
+
+MAKE_ENV+=		LIBOSSAUDIO=${LIBOSSAUDIO:Q}
+MAKE_ENV+=		DEVOSSAUDIO=${DEVOSSAUDIO:Q}
+MAKE_ENV+=		DEVOSSSOUND=${DEVOSSSOUND:Q}
+.endif	# OSS_BUILDLINK3_MK
