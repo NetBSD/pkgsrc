fc668436af9c5fb1bed7347467a9a55458c3c2a2
reed <reed@pkgsrc.org>
2006-03-09 01:19:16 +0000

    Remove the support for installing the "glib" wrapper files. A new package, print/poppler-glib, now provides this. And this means X11 (and GTK2) is no longer needed for libpoppler.
    Bump PKGREVISION.
    
    In buildlink3.mk file, remove the BUILDLINK_RECOMMENDED.poppler
    and increase the BUILDLINK_DEPENDS.poppler to this latest version
    so packages don't accidently attempt to depend on old poppler
    and poppler-glib at same time.
    Also include the fontconfig buildlink3.mk at joerg's request.

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/02/05 23:10:38 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/03/09 01:19:16 reed Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 POPPLER_BUILDLINK3_MK:=	${POPPLER_BUILDLINK3_MK}+
@@ -11,9 +11,10 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npoppler}
 BUILDLINK_PACKAGES+=	poppler
 
 .if !empty(POPPLER_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.poppler+=	poppler>=0.4.1
-BUILDLINK_RECOMMENDED.poppler+=	poppler>=0.4.4nb2
+BUILDLINK_DEPENDS.poppler+=	poppler>=0.4.5nb1
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 .endif	# POPPLER_BUILDLINK3_MK
+
+.include "../../fonts/fontconfig/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
