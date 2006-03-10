e21d944d0215a204c83260df9fe5b5d753374dc4
martti <martti@pkgsrc.org>
2006-03-10 08:25:05 +0000

    Updated sysutils/xfce4-xkb-plugin to 0.3.3
    * bug fixes
    * translation updates

--- sysutils/xfce4-xkb-plugin/buildlink3.mk
+++ sysutils/xfce4-xkb-plugin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/02/17 06:49:25 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2006/03/10 08:25:05 martti Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_XKB_PLUGIN_BUILDLINK3_MK:=	${XFCE4_XKB_PLUGIN_BUILDLINK3_MK}+
@@ -11,8 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-xkb-plugin}
 BUILDLINK_PACKAGES+=	xfce4-xkb-plugin
 
 .if !empty(XFCE4_XKB_PLUGIN_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.xfce4-xkb-plugin+=	xfce4-xkb-plugin>=0.3.2nb1
-BUILDLINK_RECOMMENDED.xfce4-xkb-plugin?=	xfce4-xkb-plugin>=0.3.2nb3
+BUILDLINK_DEPENDS.xfce4-xkb-plugin+=	xfce4-xkb-plugin>=0.3.3
+BUILDLINK_RECOMMENDED.xfce4-xkb-plugin?=	xfce4-xkb-plugin>=0.3.3
 BUILDLINK_PKGSRCDIR.xfce4-xkb-plugin?=	../../sysutils/xfce4-xkb-plugin
 .endif	# XFCE4_XKB_PLUGIN_BUILDLINK3_MK
 
