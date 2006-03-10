78580fde65800cfaa6c0178040785742637efda5
rillig <rillig@pkgsrc.org>
2006-03-10 22:32:21 +0000

    Fixed pkglint warnings.

--- audio/wsoundserver/buildlink3.mk
+++ audio/wsoundserver/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/02/05 23:08:15 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2006/03/10 22:32:21 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 WSOUNDSERVER_BUILDLINK3_MK:=	${WSOUNDSERVER_BUILDLINK3_MK}+
@@ -21,4 +21,4 @@ BUILDLINK_PKGSRCDIR.wsoundserver?=	../../audio/wsoundserver
 .include "../../devel/libproplist/buildlink3.mk"
 .include "../../wm/windowmaker/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
