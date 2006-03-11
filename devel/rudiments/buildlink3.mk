349f6d7fb8197c20641f4cef9e377e540fe6ce5d
rillig <rillig@pkgsrc.org>
2006-03-11 02:41:51 +0000

    Updated rudiments to 0.29.
    Changes since 0.28.2:
    added check for whether SSL_read/write takes void * or char * parameter
    added _XOPEN_SOURCE to filedescriptorincludes.h for sys/socket for irix
    added collapse argument to charstring::split
    added configure test for -Wall
    switched lots of:
    		char	a[charstring::length(b)];
    	to:
    		char	*a=new char[charstring::length(b)];
    		...
    		delete[] a;
    	to improve compiler compatibility
    removed #ifdef __GNUC__ around static variable definitions, I'll add
    	#ifndefs for compilers that don't allow them (like SCO's) if
    	necessary
    added -D__EXTENSIONS__ to CPPFLAGS
    added charstring::escape/unescape
    defaulted zeropadding to 1 rather than 0, I'm not sure if it's a bug
    	in glibc or not, but printf("%0*lld\n",0,(int64_t)0); prints
    	nothing rather than 0, but printf("%0*ld\n",0,(int32_t)0);
    	prints 0.  Using 1 instead of 0 for the number of 0's works
    	in all cases.
    solaris's strchr/strrchr return const char *,
    	so I made charstring::findFirst/findLast return const char *
    use snprintf's rather than sprintf's now
    uses fchmod/fchown rather than chmod/chown now
    made envelope classes out of all classes
    added a configure test for ftok that takes a char * argument
    switched a bunch of AC_TRY_LINK's to AC_TRY_COMPILE's in configure
    	script
    configure script does less work looking for getpwnam_r and cousins now
    if pthread libs can't be found, configure also tries just plain -pthread
    added #ifdefs for individual POSIX_FADV_* and MADV_* macros
    in configure script, for cygwin, tests for w32api files and functions
    	are omitted
    configure tests for strtoll/strtoull now
    configure tests for caddr_t types for all mmap-related functions
    	individually now
    unsupported memorymap methods return false now rather than not existing
    added -pthread when compiling as well as linking
    fixed a time-remaining-related bug in snooze class
    fixed a bug where the date/time string buffer could easily be overrun

--- devel/rudiments/buildlink3.mk
+++ devel/rudiments/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2006/02/05 23:08:53 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2006/03/11 02:41:51 rillig Exp $
 #
 # This Makefile fragment is included by packages that use rudiments.
 #
 # This file was created automatically using createbuildlink-3.1.
 #
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 RUDIMENTS_BUILDLINK3_MK:=	${RUDIMENTS_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
@@ -20,4 +20,4 @@ BUILDLINK_PKGSRCDIR.rudiments?=		../../devel/rudiments
 
 .endif # RUDIMENTS_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
