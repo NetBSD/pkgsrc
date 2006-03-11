9d2cd53d2f7e57e636861c66d9aa880027cb371f
schwarz <schwarz@pkgsrc.org>
2006-03-11 17:41:59 +0000

    added a workaround for a bug with Apple's gcc-4.0.0

--- graphics/netpbm/hacks.mk
+++ graphics/netpbm/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.3 2006/01/22 21:36:31 schwarz Exp $
+# $NetBSD: hacks.mk,v 1.4 2006/03/11 17:41:59 schwarz Exp $
 
 .if !defined(NETPBM_HACKS_MK)
 NETPBM_HACKS_MK=	# defined
@@ -32,6 +32,23 @@ PKG_HACKS+=		sys_types_h-inttypes_h-conflict
 post-wrapper:
 		${MKDIR} ${BUILDLINK_DIR}/include/sys && ${GREP} -v '^typedef.*[^u_]int[12368]*_t;' /usr/include/sys/types.h > ${BUILDLINK_DIR}/include/sys/types.h
 # should match int8_t, int16_t, and int32_t (only)
+.endif
+
+### [ Sun Mar  5 20:19:52 CET 2006 : schwarz ]
+### work around bug in Apple's gcc-4.0.0
+### that shows up when compiling frame.c
+###
+.if ${OPSYS} == "Darwin" && ${PKGSRC_COMPILER} == "gcc"
+_GCC_BUILD!=	${CC} --version|${GREP} '^powerpc-apple-darwin8-gcc-4\.0\.0.*build\ 4061'
+. if !empty(_GCC_BUILD)
+PKG_HACKS+=		apple-gcc-4-bug
+BUILDLINK_TRANSFORM+=	rm:-finline-floor # unknown option to gcc-3.3
+BUILDLINK_TRANSFORM+=	rm:-mpowerpc64 # prevent internal compiler error
+#					 with gcc-3.3 when compiling exif.c
+post-wrapper:
+		${SED} -e s,/gcc,/gcc-3.3, ${WRKDIR}/.gcc/bin/gcc > ${WRKDIR}/.gcc/bin/gcc-3.3 && ${MV} -f ${WRKDIR}/.gcc/bin/gcc-3.3 ${WRKDIR}/.gcc/bin/gcc && ${CHMOD} a+rx ${WRKDIR}/.gcc/bin/gcc
+# use gcc-3.3 instead of gcc-4.0.0
+. endif
 .endif
 
 .endif
