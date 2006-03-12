77d75ede23fb411f7f77176355261879bddb12e7
rillig <rillig>
2006-03-12 14:38:25 +0000

    Fixed pkglint warnings.

--- mail/Pantomime/buildlink3.mk
+++ mail/Pantomime/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2006/02/05 23:09:53 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2006/03/12 14:38:25 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PANTOMIME_BUILDLINK3_MK:=	${PANTOMIME_BUILDLINK3_MK}+
@@ -17,10 +17,10 @@ BUILDLINK_PKGSRCDIR.Pantomime?=	../../mail/Pantomime
 
 .include "../../devel/gnustep-make/gnustep.mk"
 
-BUILDLINK_INCDIRS.Pantomime+=	${GNUSTEP_LOCAL_ROOT}/${GNUSTEP_IDIR}/Pantomime
+BUILDLINK_INCDIRS.Pantomime+=	${GNUSTEP_LOCAL_ROOT:Q}/${GNUSTEP_IDIR:Q}/Pantomime
 
 .endif	# PANTOMIME_BUILDLINK3_MK
 
 .include "../../devel/gnustep-base/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
