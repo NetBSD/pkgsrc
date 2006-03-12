0eb85b075a6197d38d214403ff78b1c011b76fc5
rillig <rillig@pkgsrc.org>
2006-03-12 16:33:32 +0000

    Fixed pkglint warnings. Changed the directory of the HTML documentation to share/doc/cln/html. Bumped PKGREVISION.

--- math/cln/buildlink3.mk
+++ math/cln/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/02/05 23:10:01 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/03/12 16:33:32 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CLN_BUILDLINK3_MK:=	${CLN_BUILDLINK3_MK}+
@@ -16,4 +16,4 @@ BUILDLINK_RECOMMENDED.cln+=	cln>=1.1.9nb1
 BUILDLINK_PKGSRCDIR.cln?=	../../math/cln
 .endif	# CLN_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
