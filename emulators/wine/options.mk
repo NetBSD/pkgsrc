016e2678e5ae2ee9ac8a485e12cf0e57af6b1ce4
adam <adam>
2006-03-13 08:31:23 +0000

    OpenGL is now optional, enabled by default

--- emulators/wine/options.mk
+++ emulators/wine/options.mk
@@ -1,7 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2005/10/17 14:09:12 adam Exp $
+# $NetBSD: options.mk,v 1.3 2006/03/13 08:31:23 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.wine
-PKG_SUPPORTED_OPTIONS=	arts cups sane
+PKG_SUPPORTED_OPTIONS=	arts cups opengl sane
+PKG_SUGGESTED_OPTIONS=	opengl
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,6 +12,15 @@ PKG_SUPPORTED_OPTIONS=	arts cups sane
 
 .if !empty(PKG_OPTIONS:Mcups)
 .include "../../print/cups/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mopengl)
+PLIST_SUBST=	WINE_OPENGL=""
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../graphics/glut/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-opengl
+PLIST_SUBST=	WINE_OPENGL="@comment "
 .endif
 
 .if !empty(PKG_OPTIONS:Msane)
