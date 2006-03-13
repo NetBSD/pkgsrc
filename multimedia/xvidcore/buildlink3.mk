83f7d177df57505d7654c3315ee8344313014979
tonio <tonio@pkgsrc.org>
2006-03-13 17:32:39 +0000

    Remove the i386 only: stay cohenrent with Makefile, and also it works fine on macppc

--- multimedia/xvidcore/buildlink3.mk
+++ multimedia/xvidcore/buildlink3.mk
@@ -1,12 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/02/05 23:10:20 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2006/03/13 17:32:39 tonio Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XVIDCORE_BUILDLINK3_MK:=	${XVIDCORE_BUILDLINK3_MK}+
 
 .include "../../mk/bsd.prefs.mk"
-
-# xvidcore is ONLY_FOR_PLATFORM -*-*-i386
-.if ${MACHINE_ARCH} == "i386"
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	xvidcore
@@ -20,7 +17,5 @@ BUILDLINK_DEPENDS.xvidcore+=	xvidcore>=1.0.0
 BUILDLINK_RECOMMENDED.xvidcore?=	xvidcore>=1.1.0nb1
 BUILDLINK_PKGSRCDIR.xvidcore?=	../../multimedia/xvidcore
 .endif	# XVIDCORE_BUILDLINK3_MK
-
-.endif	# MACHINE_ARCH == i386
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
