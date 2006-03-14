9112c47201d0e89217f980ba46c1421d911dc5de
jlam <jlam@pkgsrc.org>
2006-03-14 01:14:26 +0000

    Modify the pkginstall framework so that it manages all aspects of INSTALL/DEINSTALL script creation within pkgsrc.
    If an INSTALL or DEINSTALL script is found in the package directory,
    it is automatically used as a template for the pkginstall-generated
    scripts.  If instead, they should be used simply as the full scripts,
    then the package Makefile should set INSTALL_SRC or DEINSTALL_SRC
    explicitly, e.g.:
    
    	INSTALL_SRC=	${PKGDIR}/INSTALL
    	DEINSTALL_SRC=	# emtpy
    
    As part of the restructuring of the pkginstall framework internals,
    we now *always* generate temporary INSTALL or DEINSTALL scripts.  By
    comparing these temporary scripts with minimal INSTALL/DEINSTALL
    scripts formed from only the base templates, we determine whether or
    not the INSTALL/DEINSTALL scripts are actually needed by the package
    (see the generate-install-scripts target in bsd.pkginstall.mk).
    
    In addition, more variables in the framework have been made private.
    The *_EXTRA_TMPL variables have been renamed to *_TEMPLATE, which are
    more sensible names given the very few exported variables in this
    framework.  The only public variables relating to the templates are:
    
    	INSTALL_SRC		INSTALL_TEMPLATE
    	DEINSTALL_SRC		DEINSTALL_TEMPLATE
    				HEADER_TEMPLATE
    
    The packages in pkgsrc have been modified to reflect the changes in
    the pkginstall framework.

--- multimedia/gstreamer0.8/plugins.mk
+++ multimedia/gstreamer0.8/plugins.mk
@@ -1,4 +1,4 @@
-# $NetBSD: plugins.mk,v 1.1 2006/01/18 12:44:04 jmmv Exp $
+# $NetBSD: plugins.mk,v 1.2 2006/03/14 01:14:31 jlam Exp $
 #
 # This Makefile fragment is intended to be included by packages that install
 # GStreamer modules.  It takes care of rebuilding the corresponding databases
@@ -15,8 +15,8 @@ GST0.8_REGISTER=	${BUILDLINK_PREFIX.gstreamer0.8}/bin/gst-register-0.8
 FILES_SUBST+=		GST0.8_DBDIR=${GST0.8_DBDIR:Q}
 FILES_SUBST+=		GST0.8_COMPPREP=${GST0.8_COMPPREP:Q}
 FILES_SUBST+=		GST0.8_REGISTER=${GST0.8_REGISTER:Q}
-INSTALL_EXTRA_TMPL+=	../../multimedia/gstreamer0.8/files/plugins.tmpl
-DEINSTALL_EXTRA_TMPL+=	../../multimedia/gstreamer0.8/files/plugins.tmpl
+INSTALL_TEMPLATE+=	../../multimedia/gstreamer0.8/files/plugins.tmpl
+DEINSTALL_TEMPLATE+=	../../multimedia/gstreamer0.8/files/plugins.tmpl
 
 PRINT_PLIST_AWK+=	/^libdata\/gstreamer-0.8/ { next; }
 PRINT_PLIST_AWK+=	/^@dirrm libdata\/gstreamer-0.8/ { next; }
