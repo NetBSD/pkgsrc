f5e04caa466a7249a5e45ed016a5c3194157972a
jlam <jlam@pkgsrc.org>
2006-03-20 05:30:33 +0000

    Use EMACS_MODULES instead of EMACS_USE_LEIM and the conditional dependency on xemacs-packages.  In the "lookup" option dependency, correct the package dependency name in the xemacs case by fixing a typo.  Bump the PKGREVISION to 4.

--- inputmethod/skk/options.mk
+++ inputmethod/skk/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2005/03/05 11:26:48 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2006/03/20 05:30:33 jlam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.skk
 PKG_SUPPORTED_OPTIONS=	lookup
@@ -6,7 +6,7 @@ PKG_SUPPORTED_OPTIONS=	lookup
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mlookup)
-BUILD_DEPENDS+=	${PKGNAME_PREFIX}lookup-[0-9]*:../../misc/lookup
+BUILD_DEPENDS+=	${EMACS_PKGNAME_PREFIX}lookup-[0-9]*:../../misc/lookup
 PLIST_SUBST+=	FOR_LOOKUP=""
 .else
 PLIST_SUBST+=	FOR_LOOKUP="@comment "
