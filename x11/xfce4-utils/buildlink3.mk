8b92c86fd06f63d774630aa146b2511109d77f08
martti <martti@pkgsrc.org>
2006-03-20 06:29:09 +0000

    Updated x11/xfce4-utils to 4.2.3nb3
    Fix the 'id -u' issue on Solaris (pkg/33027). The remaining changes (like
    sshagent startup) should go via xfce.org.

--- x11/xfce4-utils/buildlink3.mk
+++ x11/xfce4-utils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2006/02/17 06:49:26 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2006/03/20 06:29:09 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XFCE4_UTILS_BUILDLINK3_MK:=	${XFCE4_UTILS_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	xfce4-utils
 
 .if !empty(XFCE4_UTILS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.xfce4-utils+=	xfce4-utils>=4.2.3
-BUILDLINK_RECOMMENDED.xfce4-utils?=	xfce4-utils>=4.2.3nb2
+BUILDLINK_RECOMMENDED.xfce4-utils?=	xfce4-utils>=4.2.3nb3
 BUILDLINK_PKGSRCDIR.xfce4-utils?=	../../x11/xfce4-utils
 .endif	# XFCE4_UTILS_BUILDLINK3_MK
 
