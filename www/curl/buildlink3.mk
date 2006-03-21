58a377891c46e965fdcd6633d350e37e855ca35a
recht <recht@pkgsrc.org>
2006-03-21 21:49:47 +0000

    update to curl 7.15.3
    Fixes a TFTP packet buffer overflow vulnerability.
    See http://curl.haxx.se/docs/adv_20060320.html for details.
    
    Changes:
    - added docs for --ftp-method and CURLOPT_FTP_FILEMETHOD
    
    Bugfixes:
    - TFTP Packet Buffer Overflow Vulnerability
    - properly detecting problems with sending the FTP command USER
    - wrong error message shown when certificate verification failed
    - multi-part formpost with multi interface crash
    - the CURLFTPSSL_CONTROL setting for CURLOPT_FTP_SSL is acknowledged
    - "SSL: couldn't set callback" is now treated as a less serious problem
    - Interix build fix
    - fixed curl "hang" when out of file handles at start
    - prevent FTP uploads to URLs with trailing slash

--- www/curl/buildlink3.mk
+++ www/curl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/02/05 23:11:20 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2006/03/21 21:49:47 recht Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CURL_BUILDLINK3_MK:=	${CURL_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	curl
 
 .if !empty(CURL_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.curl+=	curl>=7.12.3
-BUILDLINK_RECOMMENDED.curl+=	curl>=7.15.1nb1
+BUILDLINK_RECOMMENDED.curl+=	curl>=7.15.3
 BUILDLINK_PKGSRCDIR.curl?=	../../www/curl
 .endif	# CURL_BUILDLINK3_MK
 
