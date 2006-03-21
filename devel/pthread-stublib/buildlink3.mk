4a7261902a99efa1fffaf6dec69ad4bb3d4c67dc
drochner <drochner>
2006-03-21 19:23:08 +0000

    Add a library to catch calls to pthread_mutex* and *specific* functions where the base system doesn't provide stubs for non-threaded programs (which is just NetBSD<=2 for now). This is absolutely primitive and just sufficient to make MesaLib usable with nonthreaded programs.

---
+++ devel/pthread-stublib/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/03/21 19:23:08 drochner Exp $
+# XXX
+# XXX This file was created automatically using createbuildlink-3.10.
+# XXX After this file has been verified as correct, the comment lines
+# XXX beginning with "XXX" should be removed.  Please do not commit
+# XXX unverified buildlink3.mk files.
+# XXX
+# XXX Packages that only install static libraries or headers should
+# XXX include the following line:
+# XXX
+# XXX	BUILDLINK_DEPMETHOD.pthread-stublib?=	build
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+PTHREAD_STUBLIB_BUILDLINK3_MK:=	${PTHREAD_STUBLIB_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	pthread-stublib
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npthread-stublib}
+BUILDLINK_PACKAGES+=	pthread-stublib
+
+.if !empty(PTHREAD_STUBLIB_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.pthread-stublib+=	pthread-stublib>=1.0
+BUILDLINK_PKGSRCDIR.pthread-stublib?=	../../devel/pthread-stublib
+.endif	# PTHREAD_STUBLIB_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
