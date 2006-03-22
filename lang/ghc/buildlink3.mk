b4b44fa4a3c9cf81fdfc32c925cc7a693638887a
jlam <jlam@pkgsrc.org>
2006-03-22 15:12:07 +0000

    We include gmp/buildlink3.mk outside of the BUILDLINK_DEPTH guards so that "gmp" is registered as a direct dependency for any package that includes ghc/buildlink3.mk to get ghc as a build dependency.  This is needed since software built by ghc requires routines from the "gmp" shared library.  This fixes PR pkg/33100.
    Remove the workaround in devel/darcs and x11/wxhaskell.

--- lang/ghc/buildlink3.mk
+++ lang/ghc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/02/05 23:09:45 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2006/03/22 15:12:07 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GHC_BUILDLINK3_MK:=	${GHC_BUILDLINK3_MK}+
@@ -18,7 +18,13 @@ BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc
 BUILDLINK_DEPMETHOD.ghc?=	build
 .endif	# GHC_BUILDLINK3_MK
 
-.include "../../devel/gmp/buildlink3.mk"
 .include "../../devel/readline/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+
+# We include gmp/buildlink3.mk here so that "gmp" is registered as a
+# direct dependency for any package that includes this buildlink3.mk
+# to get ghc as a build dependency.  This is needed since software
+# built by ghc requires routines from the "gmp" shared library.
+#
+.include "../../devel/gmp/buildlink3.mk"
