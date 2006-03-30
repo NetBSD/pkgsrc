9a1246bf6e398ba49117e82fdd9efc7ae0765b63
jmmv <jmmv@pkgsrc.org>
2006-03-30 21:09:48 +0000

    Update to 2.14.0:
    Mars 13, 2006: Overview of changes in 2.14.0
    ============================================
    * Updated translations.
    
    February 27, 2006: Overview of changes in 2.13.92
    =================================================
    * Fixed proc_state documentation.
    * Updated mountlist ignore list.
    
    January 23, 2006: Overview of changes in 2.13.3
    ===============================================
    * linux:
    	- fixe get_procmap for linux < 2.6.14.
    
    January 2, 2006: Overview of changes in 2.13.2
    ==============================================
    * Fixed libtool versioning (James Henstridge)
    
    December 15, 2005: Overview of changes in 2.13.1
    ================================================
    * linux:
        - fixed proclist
        - implemented server->real_ncpu
    
    December 13, 2005: Overview of changes in 2.13.0
    ================================================
    * Requires automake-1.9 (James Henstridge)
    * Supports up to 32 CPU
    * Supports Linux smaps
    * linux: fixed SMP detection
    
    This release is not ABI compatible with previous releases. Sorry.

--- sysutils/libgtop2/buildlink3.mk
+++ sysutils/libgtop2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2006/02/05 23:10:55 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2006/03/30 21:09:48 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGTOP2_BUILDLINK3_MK:=	${LIBGTOP2_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgtop2}
 BUILDLINK_PACKAGES+=	libgtop2
 
 .if !empty(LIBGTOP2_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libgtop2+=	libgtop2>=2.12.0
-BUILDLINK_RECOMMENDED.libgtop2?=	libgtop2>=2.12.0nb3
+BUILDLINK_DEPENDS.libgtop2+=	libgtop2>=2.14.0
 BUILDLINK_PKGSRCDIR.libgtop2?=	../../sysutils/libgtop2
 .endif	# LIBGTOP2_BUILDLINK3_MK
 
