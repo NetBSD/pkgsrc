62340629a4395cb70441dbb375abca265d4e7bc9
jmmv <jmmv@pkgsrc.org>
2006-03-30 21:11:51 +0000

    Update to 2.13.15:
     Features
    
            * Add "include NoDisplay" flag (Mark McLoughlin)
    
     Fixes
    
            * Fix issue where menu wouldn't fully reload after lots of
              file change events (Mark McLoughlin, Frederic Crozat)
            * Remove some unused code (Kjartan Maraas)
            * Fix incorrect escaping of C format string (The Written Word)
    
      Translators
    
           * Rostislav Raykov (bg)
           * Mahay Alam Khan (bn)
           * Jordi Mallach (ca)
           * Miloslav Trmac (cs)
           * Adam Weinberger (en_CA)
           * Francisco Javier F. Serrador (es)
           * Priit Laes (et)
           * Iñaki Larrañaga (eu)
           * Ilkka Tuohela (fi)
           * Christophe Merlet (RedFox) (fr)
           * Ignacio Casal Quinteiro (gl)
           * Ankit Patel (gu)
           * Gabor Kelemen (hu)
           * Francesco Marletta (it)
           * Erdal Ronahî (ku)
           * Timur Jamakeev (ky)
           * Kjartan Maraas (nb, no)
           * Tino Meinen (nl)
           * Marcel Telka (sk)
           * Christian Rose (sv)
           * Y.Kiran Chandra (te)
           * Theppitak Karoonboonyanan (th)
           * Abduxukur Abdurixit (ug)
           * Clytie Siddall (vi)
           * Woodman Tuen (zh_HK, zh_TW)

--- sysutils/gnome-menus/buildlink3.mk
+++ sysutils/gnome-menus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/02/05 23:10:50 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2006/03/30 21:11:51 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_MENUS_BUILDLINK3_MK:=	${GNOME_MENUS_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngnome-menus}
 BUILDLINK_PACKAGES+=	gnome-menus
 
 .if !empty(GNOME_MENUS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gnome-menus+=		gnome-menus>=2.12.0
-BUILDLINK_RECOMMENDED.gnome-menus?=	gnome-menus>=2.12.0nb3
+BUILDLINK_DEPENDS.gnome-menus+=		gnome-menus>=2.13.5
 BUILDLINK_PKGSRCDIR.gnome-menus?=	../../sysutils/gnome-menus
 .endif	# GNOME_MENUS_BUILDLINK3_MK
 
