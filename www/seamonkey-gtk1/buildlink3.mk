419fb823af07d0f18bf8df315def03bb9cad0561
ghen <ghen@pkgsrc.org>
2006-03-30 19:16:24 +0000

    Import Mozilla Seamonkey 1.0 from pkgsrc-wip (gtk1 version).  Seamonkey is the community-driven continuation of the Mozilla Suite, which is no longer maintained by Mozilla.

---
+++ www/seamonkey-gtk1/buildlink3.mk
@@ -0,0 +1,26 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/03/30 19:16:32 ghen Exp $
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+SEAMONKEY_GTK1_BUILDLINK3_MK:=		${SEAMONKEY_GTK1_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=			seamonkey-gtk1
+.endif
+
+BUILDLINK_PACKAGES:=			${BUILDLINK_PACKAGES:Nseamonkey-gtk1}
+BUILDLINK_PACKAGES+=			seamonkey-gtk1
+
+.if !empty(SEAMONKEY_GTK1_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.seamonkey-gtk1+=	seamonkey-gtk1>=1.0
+BUILDLINK_RECOMMENDED.seamonkey-gtk1+=	seamonkey-gtk1>=1.0
+BUILDLINK_PKGSRCDIR.seamonkey-gtk1?=	../../wip/seamonkey-gtk1
+.endif	# SEAMONKEY_GTK1_BUILDLINK3_MK
+
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/gdk-pixbuf/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../net/ORBit/buildlink3.mk"
+.include "../../x11/gtk/buildlink3.mk"
+
+BUILDLINK_DEPTH:=    			 ${BUILDLINK_DEPTH:S/+$//}
