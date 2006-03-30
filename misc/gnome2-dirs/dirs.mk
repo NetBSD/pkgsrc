6c8388da3d42f58b4b8325ead04d77e72fc1dab3
jmmv <jmmv@pkgsrc.org>
2006-03-30 21:14:32 +0000

    Update gnome-dirs, gnome1-dirs and gnome2-dirs to 1.6: - Add the share/gnome-default-applications.

--- misc/gnome2-dirs/dirs.mk
+++ misc/gnome2-dirs/dirs.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dirs.mk,v 1.4 2005/12/05 20:50:39 rillig Exp $
+# $NetBSD: dirs.mk,v 1.5 2006/03/30 21:14:32 jmmv Exp $
 #
 # This file is intended to be included by mk/dirs.mk, not directly by packages.
 #
@@ -15,6 +15,7 @@ GNOME2_DIRS+=		share/control-center-2.0/capplets
 GNOME2_DIRS+=		share/control-center-2.0/icons
 GNOME2_DIRS+=		share/gnome-2.0
 GNOME2_DIRS+=		share/gnome-2.0/ui
+GNOME2_DIRS+=		share/gnome-default-applications
 GNOME2_DIRS+=		share/gnome/wm-properties
 
 .if defined(_USE_GNOME2_DIRS) && !empty(_USE_GNOME2_DIRS)
