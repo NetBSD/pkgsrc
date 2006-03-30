461c824af757dfce73c8b77f87c27124e5b94100
jmmv <jmmv>
2006-03-30 20:47:44 +0000

    Update to 0.9:
    2006-01-12  Alexander Larsson  <alexl@redhat.com>
    
    	* Makefile (VERSION):
    	Bump version to 0.9
    
    2005-12-23  Luca Ferretti <elle.uca@libero.it>
    
    	* index.theme:
    	Sort values for Directories keys (fix gnome bug 315841) and add missing
    	entries. Now all installed directories should have an entry in this
    	file.
    
    2005-04-08  Alexander Larsson  <alexl@redhat.com>
    
    	* Makefile.in (ICON_CONTEXTS):
    	Add emblem subdir
    
    	* configure.ac:
    	Bump version to 0.8
    
    	* index.theme:
    	Add emblem subdir
    
    2005-02-04  Alexander Larsson  <alexl@redhat.com>
    
    	* configure.ac:
    	Bump version to 0.7
    
    	* index.theme (Size):
    	Fix typos in last version
    
    2005-02-02  Alexander Larsson  <alexl@redhat.com>
    
    	* configure.ac:
    	Bump version to 0.6
    
    	* Makefile.in (ICON_SIZES):
    	Add 24x24 dirs
    
    	* index.theme (Directories):
    	Add 24x24 version of action, apps, devices, filesystem & mimetypes

--- graphics/hicolor-icon-theme/buildlink3.mk
+++ graphics/hicolor-icon-theme/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2006/02/05 23:09:33 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2006/03/30 20:47:44 jmmv Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 HICOLOR_ICON_THEME_BUILDLINK3_MK:=	${HICOLOR_ICON_THEME_BUILDLINK3_MK}+
@@ -24,6 +24,28 @@ PRINT_PLIST_AWK+=	/^@dirrm ${d:S/\//\\\//g}$$/ \
 			{ print "@comment in hicolor-icon-theme: " $$0; next; }
 .  endfor
 .  undef dirs
+
+.if !defined(HICOLOR_ICON_THEME_DEPEND_ONLY)
+.include  "../../x11/gtk2/buildlink3.mk"
+FILES_SUBST+=		GTK_UPDATE_ICON_CACHE="${BUILDLINK_PREFIX.gtk2}/bin/gtk-update-icon-cache"
+FILES_SUBST+=		ICON_THEME_DIR="${BUILDLINK_PREFIX.hicolor-icon-theme}/share/icons/hicolor"
+INSTALL_TEMPLATE+=	../../graphics/hicolor-icon-theme/files/icon-cache.tmpl
+DEINSTALL_TEMPLATE+=	../../graphics/hicolor-icon-theme/files/icon-cache.tmpl
+PRINT_PLIST_AWK+=	/^share\/icons\/hicolor\/icon-theme.cache$$/ { next; }
+
+BUILDLINK_TARGETS+=	guic-buildlink-fake
+_GUIC_FAKE=		${BUILDLINK_DIR}/bin/gtk-update-icon-cache
+
+.PHONY: guic-buildlink-fake
+guic-buildlink-fake:
+	${_PKG_SILENT}${_PKG_DEBUG}					\
+	if [ ! -f ${_GUIC_FAKE} ]; then					\
+		${ECHO_BUILDLINK_MSG} "Creating ${_GUIC_FAKE}";	\
+		${MKDIR} ${_GUIC_FAKE:H};				\
+		${ECHO} "#!${SH}" > ${_GUIC_FAKE};			\
+		${CHMOD} +x ${_GUIC_FAKE};				\
+	fi
+.endif	# HICOLOR_ICON_THEME_DEPEND_ONLY
 .endif	# HICOLOR_ICON_THEME_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
