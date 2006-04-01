3186ed52c8b6d28f226865b9971e42b563a74719
jmmv <jmmv@pkgsrc.org>
2006-04-01 21:39:54 +0000

    Update xdg-dirs and xdg-x11-dirs to 1.3: - Register the share/pixmaps/backgrounds directory.

--- misc/xdg-x11-dirs/dirs.mk
+++ misc/xdg-x11-dirs/dirs.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dirs.mk,v 1.5 2005/12/05 20:50:41 rillig Exp $
+# $NetBSD: dirs.mk,v 1.6 2006/04/01 21:39:55 jmmv Exp $
 #
 # This file is intended to be included by mk/dirs.mk, not directly by packages.
 #
@@ -13,6 +13,7 @@ XDG_X11_DIRS+=		share/icons
 XDG_X11_DIRS+=		share/images
 XDG_X11_DIRS+=		share/mime-info
 XDG_X11_DIRS+=		share/pixmaps
+XDG_X11_DIRS+=		share/pixmaps/backgrounds
 XDG_X11_DIRS+=		share/sounds
 XDG_X11_DIRS+=		share/themes
 
