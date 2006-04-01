412c157a0393d491bcc7fe543e57f2e14902ce78
grant <grant@pkgsrc.org>
2006-04-01 23:27:49 +0000

    require libtool >=1.5.22nb3 for /bin/sh fix.

--- mk/platform/SunOS.mk
+++ mk/platform/SunOS.mk
@@ -1,4 +1,4 @@
-# $NetBSD: SunOS.mk,v 1.22 2006/03/18 21:40:45 jlam Exp $
+# $NetBSD: SunOS.mk,v 1.23 2006/04/01 23:27:49 grant Exp $
 #
 # Variable definitions for the SunOS/Solaris operating system.
 
@@ -57,7 +57,8 @@ _OPSYS_HAS_INET6=	no		# IPv6 is not standard
 _OPSYS_HAS_JAVA=	no		# Java is not standard
 _OPSYS_HAS_MANZ=	no		# no MANZ for gzipping of man pages
 _OPSYS_HAS_OSSAUDIO=	no		# libossaudio is available
-_OPSYS_PERL_REQD=		# no base version of perl required
+_OPSYS_LIBTOOL_REQD=	1.5.22nb3
+_OPSYS_PERL_REQD=			# no base version of perl required
 _OPSYS_PTHREAD_AUTO=	no		# -lpthread needed for pthreads
 _OPSYS_SHLIB_TYPE=	ELF		# shared lib type
 _PATCH_CAN_BACKUP=	yes		# native patch(1) can make backups
