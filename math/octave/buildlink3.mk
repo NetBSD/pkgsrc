0196ce0ee5f48c1ca34e2c665db243252ac593fb
adam <adam@pkgsrc.org>
2006-04-06 09:10:28 +0000

    Changes 2.1.73: * Bug fixes

--- math/octave/buildlink3.mk
+++ math/octave/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/04/06 06:22:21 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2006/04/06 09:10:28 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 OCTAVE_BUILDLINK3_MK:=	${OCTAVE_BUILDLINK3_MK}+
@@ -29,4 +29,4 @@ BUILDLINK_PKGSRCDIR.octave?=	../../math/octave
 .include "../../math/fftw/buildlink3.mk"
 .include "../../math/lapack/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
