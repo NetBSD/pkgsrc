fa6717d27dd34351b2cf6f329b94fcbf12a4418c
joerg <joerg@pkgsrc.org>
2006-04-10 14:58:48 +0000

    Sync in libdfui-4.2 from pkgsrc-wip.

--- devel/libdfui/buildlink3.mk
+++ devel/libdfui/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/04/06 06:21:50 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2006/04/10 14:58:48 joerg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBDFUI_BUILDLINK3_MK:=	${LIBDFUI_BUILDLINK3_MK}+
@@ -11,10 +11,10 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibdfui}
 BUILDLINK_PACKAGES+=	libdfui
 
 .if !empty(LIBDFUI_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.libdfui+=	libdfui>=3.0
+BUILDLINK_API_DEPENDS.libdfui+=	libdfui>=4.1
 BUILDLINK_PKGSRCDIR.libdfui?=	../../devel/libdfui
 .endif	# LIBDFUI_BUILDLINK3_MK
 
-.include "../../devel/libaura/buildlink3.mk"
+.include "../../wip/libaura/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
