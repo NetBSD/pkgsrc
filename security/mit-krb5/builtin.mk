77a0a1527d6d1363f71969e954dbe45ec07be3d7
jlam <jlam@pkgsrc.org>
2006-04-10 18:05:16 +0000

    At least look for "Massachusetts Institute of Technology" as a crude check for MIT Kerberos 5 when inspecting /usr/include/krb5.h.  Also, bring this file more in line with heimdal/builtin.mk.

--- security/mit-krb5/builtin.mk
+++ security/mit-krb5/builtin.mk
@@ -1,44 +1,39 @@
-# $NetBSD: builtin.mk,v 1.1 2006/04/10 15:04:44 tron Exp $
+# $NetBSD: builtin.mk,v 1.2 2006/04/10 18:05:16 jlam Exp $
 
 BUILTIN_PKG:=	mit-krb5
 
-BUILTIN_FIND_FILES_VAR:=	KRB5_CONFIG
-BUILTIN_FIND_FILES.KRB5_CONFIG=	/usr/bin/krb5-config
+BUILTIN_FIND_FILES_VAR:=		H_MIT_KRB5 SH_KRB5_CONFIG
+BUILTIN_FIND_FILES.H_MIT_KRB5=		/usr/include/krb5.h
+BUILTIN_FIND_GREP.H_MIT_KRB5=		Massachusetts Institute of Technology
+BUILTIN_FIND_FILES.SH_KRB5_CONFIG=	/usr/bin/krb5-config
+BUILTIN_FIND_GREP.SH_KRB5_CONFIG=	^[ 	]*--version)
 
 .include "../../mk/buildlink3/bsd.builtin.mk"
-
-###
-### Figure out the version of MIT Kerberos V installed on the system.
-###
-
-.if !defined(BUILTIN_VERSION.mit-krb5) && \
-    empty(KRB5_CONFIG:M__nonexistent__) && \
-    empty(KRB5_CONFIG:M${LOCALBASE}/*)
-BUILTIN_VERSION.mit-krb5!=	${KRB5_CONFIG} --version | \
-				${SED} -e 's/.*release //' -e 's/-.*//'
-.endif
-MAKEVARS+=	BUILTIN_VERSION.mit-krb5
 
 ###
 ### Determine if there is a built-in implementation of the package and
 ### set IS_BUILTIN.<pkg> appropriately ("yes" or "no").
 ###
 .if !defined(IS_BUILTIN.mit-krb5)
-IS_BUILTIN.mit-krb5=        no
-.  if defined(BUILTIN_VERSION.mit-krb5)
-IS_BUILTIN.mit-krb5=        yes
+IS_BUILTIN.mit-krb5=	no
+.  if empty(H_MIT_KRB5:M__nonexistent__) && empty(H_MIT_KRB5:M${LOCALBASE}/*)
+IS_BUILTIN.mit-krb5=	yes
 .  endif
 .endif
-MAKEVARS+=      IS_BUILTIN.mit-krb5
+MAKEVARS+=	IS_BUILTIN.mit-krb5
 
 ###
 ### If there is a built-in implementation, then set BUILTIN_PKG.<pkg> to
 ### a package name to represent the built-in package.
 ###
 .if !defined(BUILTIN_PKG.mit-krb5) && \
-    !empty(IS_BUILTIN.mit-krb5:M[yY][eE][sS]) && \
-    defined(BUILTIN_VERSION.mit-krb5)
-BUILTIN_PKG.mit-krb5=       mit-krb5-${BUILTIN_VERSION.mit-krb5}
+    !empty(IS_BUILTIN.mit-krb5:M[yY][eE][sS])
+.  if empty(SH_KRB5_CONFIG:M__nonexistent__)
+BUILTIN_VERSION.mit-krb5!=	${SH_KRB5_CONFIG} --version | \
+				${SED} -e 's/.*release //' -e 's/-.*//'
+.  endif
+BUILTIN_VERSION.mit-krb5?=	1.4.0
+BUILTIN_PKG.mit-krb5=		mit-krb5-${BUILTIN_VERSION.mit-krb5}
 .endif
 MAKEVARS+=      BUILTIN_PKG.mit-krb5
 
@@ -57,7 +52,7 @@ USE_BUILTIN.mit-krb5=	yes
 .      for _dep_ in ${BUILDLINK_API_DEPENDS.mit-krb5}
 .        if !empty(USE_BUILTIN.mit-krb5:M[yY][eE][sS])
 USE_BUILTIN.mit-krb5!=							\
-	if ${PKG_ADMIN} pmatch ${_dep_:Q} ${BUILTIN_PKG.mit-krb5:Q}; then	\
+	if ${PKG_ADMIN} pmatch ${_dep_:Q} ${BUILTIN_PKG.mit-krb5:Q}; then \
 		${ECHO} "yes";						\
 	else								\
 		${ECHO} "no";						\
