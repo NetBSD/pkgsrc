2e6fe823fa694a1355e5ceacbec57802084d6db4
minskim <minskim@pkgsrc.org>
2006-04-11 18:39:41 +0000

    Update common-mml to 1.2.16.  This version was packaged by David Price, and modified by me.
    Major changes since 1.2.0:
      - Add support for SAP and SDP.

--- mbone/common-mml/buildlink3.mk
+++ mbone/common-mml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/04/06 06:22:22 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2006/04/11 18:39:41 minskim Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 COMMON_MML_BUILDLINK3_MK:=	${COMMON_MML_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ncommon-mml}
 BUILDLINK_PACKAGES+=	common-mml
 
 .if !empty(COMMON_MML_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.common-mml+=	common-mml>=1.2.0nb1
+BUILDLINK_API_DEPENDS.common-mml+=	common-mml>=1.2.14
 BUILDLINK_PKGSRCDIR.common-mml?=	../../mbone/common-mml
 .endif	# COMMON_MML_BUILDLINK3_MK
 
