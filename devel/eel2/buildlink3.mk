d2c53c7b9a9c9d3c7818bea5dd88866ccd5346ca
drochner <drochner@pkgsrc.org>
2006-04-12 09:46:26 +0000

    update to 2.14.1 changes: * Translation updates * Build fixes * Drop popt dependency * Show all desktop items in application selection * GnomeGoals: Use po/LINGUAS

--- devel/eel2/buildlink3.mk
+++ devel/eel2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2006/04/06 06:21:48 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2006/04/12 09:46:26 drochner Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 EEL2_BUILDLINK3_MK:=	${EEL2_BUILDLINK3_MK}+
@@ -23,7 +23,6 @@ BUILDLINK_PKGSRCDIR.eel2?=	../../devel/eel2
 .include "../../devel/libglade2/buildlink3.mk"
 .include "../../devel/libgnome/buildlink3.mk"
 .include "../../devel/libgnomeui/buildlink3.mk"
-.include "../../devel/popt/buildlink3.mk"
 .include "../../graphics/libart2/buildlink3.mk"
 .include "../../sysutils/gnome-menus/buildlink3.mk"
 .include "../../sysutils/gnome-vfs2/buildlink3.mk"
