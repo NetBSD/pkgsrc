540cc20bcbbe25ad81de182dd063e81366e52047
cube <cube@pkgsrc.org>
2006-04-14 13:47:29 +0000

    PHP4/5 security changes...  They're not critical issues;  secunia classes them between "not critical" and "less critical".
    Fix CVE-2006-0996, CVE-2006-1494, CVE-2006-1608, CVE-2006-1490.
    
    See:
        http://secunia.com/advisories/19383/
        http://secunia.com/advisories/19599/
    
    Patches were extracted from CVS.  I had to translate the one for
    CVE-2006-1608 on php4 because it has not made its way to the php4.4 branch
    (I don't know why;  I can confirm it fixes the issue).
    
    While here, add PATCHDIR to the list of variables php5's Makefile.php
    defines.  That way, ap-php gets patched too...

--- lang/php5/Makefile.php
+++ lang/php5/Makefile.php
@@ -1,9 +1,10 @@
-# $NetBSD: Makefile.php,v 1.17 2005/12/05 23:55:10 rillig Exp $
+# $NetBSD: Makefile.php,v 1.18 2006/04/14 13:47:29 cube Exp $
 #
 
 .include "../../lang/php5/Makefile.common"
 
 DISTINFO_FILE=	${.CURDIR}/../../lang/php5/distinfo
+PATCHDIR=	${.CURDIR}/../../lang/php5/patches
 
 BUILD_DEFS+=		USE_INET6
 
