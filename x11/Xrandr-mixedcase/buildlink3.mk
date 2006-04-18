ad56ca92e66f0bd3a878c526e9d06cd5ff43b0a4
reed <reed>
2006-04-18 19:23:46 +0000

    Change to use new mainatined *proto headers instead of unmaintained *ext packages. In some cases, the headers are identical.
    I tested on DragonFly using X11_TYPE=xorg and on NetBSD
    using native XFree86 (and more).

--- x11/Xrandr-mixedcase/buildlink3.mk
+++ x11/Xrandr-mixedcase/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2006/04/06 06:22:56 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2006/04/18 19:23:47 reed Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 XRANDR_BUILDLINK3_MK:=	${XRANDR_BUILDLINK3_MK}+
@@ -16,7 +16,7 @@ BUILDLINK_ABI_DEPENDS.Xrandr+=	Xrandr>=1.0.2nb3
 BUILDLINK_PKGSRCDIR.Xrandr?=	../../x11/Xrandr
 .endif	# XRANDR_BUILDLINK3_MK
 
-.include "../../x11/randrext/buildlink3.mk"
+.include "../../x11/randrproto/buildlink3.mk"
 .include "../../x11/Xrender/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
