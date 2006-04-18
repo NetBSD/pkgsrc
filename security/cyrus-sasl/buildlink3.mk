bb27a9a3c1124b6c14d30b726c7f7e24cd320b39
jlam <jlam@pkgsrc.org>
2006-04-18 17:41:30 +0000

    Updated security/cyrus-sasl to 2.1.21.  Changes from version 2.1.20 include:
    * lib/dlopen.c: log the reason for opendir() failure
      when loading plugin.
    * lib/common.c: honor log level setting
    * sample/sample-client.c, sample/sample-server.c: Fixed several
      64 bit portability warnings.
    * utils/testsuite.c: Fixed several 64 bit portability warnings.
    * utils/saslpasswd.c: Fixed typo in an auxprop name.
    * include/saslplug.h, lib/common.c, lib/saslint.h,
      lib/server.c: Added sasl_server_plugin_info().
    * lib/common.c: initialize path in case caller didn't.

--- security/cyrus-sasl/buildlink3.mk
+++ security/cyrus-sasl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/04/13 21:32:40 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2006/04/18 17:41:30 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 CYRUS_SASL_BUILDLINK3_MK:=	${CYRUS_SASL_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ncyrus-sasl}
 BUILDLINK_PACKAGES+=	cyrus-sasl
 
 .if !empty(CYRUS_SASL_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.cyrus-sasl+=		cyrus-sasl>=2.1.12
+BUILDLINK_API_DEPENDS.cyrus-sasl+=	cyrus-sasl>=2.1.12
 BUILDLINK_ABI_DEPENDS.cyrus-sasl?=	cyrus-sasl>=2.1.20nb3
 BUILDLINK_PKGSRCDIR.cyrus-sasl?=	../../security/cyrus-sasl
 .endif	# CYRUS_SASL_BUILDLINK3_MK
