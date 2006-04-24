f724bacbb304d8a47b64e29344dde8c02e52252b
veego <veego@pkgsrc.org>
2006-04-24 16:52:58 +0000

    Update "mozilla" packages to version 1.7.13. Changes since 1.7.12: - stability fixes - security fixes  - MFSA 2006-27 Table Rebuilding Code Execution Vulnerability  - MFSA 2006-25 Privilege escalation through Print Preview  - MFSA 2006-24 Privilege escalation using crypto.generateCRMFRequest  - MFSA 2006-23 File stealing by changing input type  - MFSA 2006-22 CSS Letter-Spacing Heap Overflow Vulnerability  - MFSA 2006-21 JavaScript execution in mail when forwarding in-line  - MFSA 2006-19 Cross-site scripting using .valueOf.call()  - MFSA 2006-18 Mozilla Firefox Tag Order Vulnerability  - MFSA 2006-17 cross-site scripting through window.controllers  - MFSA 2006-16 Accessing XBL compilation scope via valueOf.call()  - MFSA 2006-15 Privilege escalation using a JavaScript function's cloned                 parent  - MFSA 2006-14 Privilege escalation via XBL.method.eval  - MFSA 2006-13 Downloading executables with "Save Image As..."  - MFSA 2006-12 Secure-site spoof (requires security warning dialog)  - MFSA 2006-11 Crashes with evidence of memory corruption (rv:1.8)  - MFSA 2006-10 JavaScript garbage-collection hazard audit  - MFSA 2006-09 Cross-site JavaScript injection using event handlers  - MFSA 2006-05 Localstore.rdf XML injection through XULDocument.persist()  - MFSA 2006-03 Long document title causes startup denial of Service  - MFSA 2006-01 JavaScript garbage-collection hazards - additional patch to fix pr#33333 Shin'ichiro TAYA told me that i can do this update.

--- www/mozilla-gtk2/buildlink3.mk
+++ www/mozilla-gtk2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2006/04/17 13:45:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2006/04/24 16:52:58 veego Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 MOZILLA_GTK2_BUILDLINK3_MK:=		${MOZILLA_GTK2_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=			mozilla-gtk2
 
 .if !empty(MOZILLA_GTK2_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.mozilla-gtk2+=	mozilla-gtk2>=1.7.5
-BUILDLINK_ABI_DEPENDS.mozilla-gtk2+=	mozilla-gtk2>=1.7.12nb4
+BUILDLINK_ABI_DEPENDS.mozilla-gtk2+=	mozilla-gtk2>=1.7.13
 BUILDLINK_PKGSRCDIR.mozilla-gtk2?=	../../www/mozilla-gtk2
 .endif	# MOZILLA_GTK2_BUILDLINK3_MK
 
