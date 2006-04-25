170327d8ea920cc3df8cbfed8a10fe84d08b14c7
salo <salo@pkgsrc.org>
2006-04-25 12:45:57 +0000

    Update to version 2.2.7
    Approved by <frueauf>
    
    Changes:
    - make it work as binary packages,
    - remove useless MESSAGE files,
    - add nmap.nasl plugin, not included by default upstream,
    - make the installation a bit more sane and easier to configure.
    
    2.2.7:
    ======
    
    Nessus 2.2.7 contains several fixes for bugs which have been found
    during the 3.x developement process and have been backported to this
    branch. It also slightly extends the NASL language by adding support for
    arrays of arrays. We will use this feature in some key plugins (SMB in
    particular) within 6 months, so you should definitely upgrade to 2.2.7
    or 3.0.x.
    
    nessus-libraries:
    - Fixed a NULL pointer dereferencement in the BPF server (this mostly
      affects OpenBSD and FreeBSD < 5)
    - The 'service' functions now only deal with the services file provided
    - with Nessus (instead of using a mix of /etc/services and others)
    
    libnasl:
    - Fixed off-by-one bugs in insstr() and str_replace() which would
      sometimes prevent these two functions from properly dealing with the
      last character of a string
    - Fixed tcp_ping() which was too aggressive and may therefore sometimes
      miss a live host
    - Fixed a bug in send() which would not properly validate the value of the
      'length' variable
    - Now handle arrays of arrays
    - Fixed open_priv_sock_tcp() which would report a successful connection
      when timing out
    
    nessusd:
    - Properly install the file 'nessus-services' in $prefix/var/nessus/
    - Bigger buffer when receiving preferences from the client (to avoid a
      possible truncation of the plugin list in the future)
    - Fixed a bug in the preferences parser which would cause nessusd to die
      on startup when processing a malformed preference file
    
    nessus client:
    - Fixed an unlikely but potential segmentation fault when viewing the
      report in the GUI
    - Erase the credentials from memory after having used them (thanks to
      Sumiut Siddhart for noticing this)
    
    plugins:
    - Fixed several bugs in find_services.c which would not properly set the
      key Transport/SSL or which may read some data beyond its buffer
    - Fixed a bad #if/#endif clause in nessus_tcp_scanner.c which prevented it
      from recomputing the RTT, hence negatively impacting the performance
    - nmap.nasl has been removed from the main distribution (to use nmap from
      within Nessus read http://www.nessus.org/documentation/?doc=nmap-usage)

--- security/nessus-core/buildlink3.mk
+++ security/nessus-core/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2006/04/17 13:46:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2006/04/25 12:45:57 salo Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 NESSUS_CORE_BUILDLINK3_MK:=	${NESSUS_CORE_BUILDLINK3_MK}+
@@ -11,8 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nnessus-core}
 BUILDLINK_PACKAGES+=	nessus-core
 
 .if !empty(NESSUS_CORE_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.nessus-core+=		nessus-core>=2.2.3
-BUILDLINK_ABI_DEPENDS.nessus-core?=	nessus-core>=2.2.6nb2
+BUILDLINK_API_DEPENDS.nessus-core+=	nessus-core>=2.2.7
+BUILDLINK_ABI_DEPENDS.nessus-core?=	nessus-core>=2.2.7
 BUILDLINK_PKGSRCDIR.nessus-core?=	../../security/nessus-core
 .endif	# NESSUS_CORE_BUILDLINK3_MK
 
