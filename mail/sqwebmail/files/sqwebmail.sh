bbe903e447b767714372e97025915cfbd2dad84a
jlam <jlam@pkgsrc.org>
2006-04-28 18:16:25 +0000

    Update mail/sqwebmail to 5.1.0.  Changes from version 5.0.7 include:
      + Add an INSTALL script that detects the presence of the old
        sqwebmail state directory and that informs the admin to move it
        to the new location.
    
      + Install some more of the HTML documentation in the location expected
        by courier-mta.
    
      * Complete re-implementation of the LDAP addressbook.
    
      * Increase the maximum size of the CGI environment to avoid certain
        classes of browser/website problems.

--- mail/sqwebmail/files/sqwebmail.sh
+++ mail/sqwebmail/files/sqwebmail.sh
@@ -1,11 +1,12 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: sqwebmail.sh,v 1.3 2005/02/18 22:12:56 jlam Exp $
+# $NetBSD: sqwebmail.sh,v 1.4 2006/04/28 18:16:25 jlam Exp $
 #
 # Courier SqWebMail services daemon
 #
 # PROVIDE: sqwebmail
 # REQUIRE: authdaemond
+# KEYWORD: shutdown
 
 . /etc/rc.subr
 
@@ -15,7 +16,6 @@ command="@PREFIX@/sbin/courierlogger"
 ctl_command="@PREFIX@/sbin/sqwebmaild"
 pidfile="@VARBASE@/run/sqwebmaild.pid"
 required_files="@PKG_SYSCONFDIR@/calendarmode @PKG_SYSCONFDIR@/sqwebmaild"
-required_vars="authdaemond"
 
 start_cmd="courier_doit start"
 stop_cmd="courier_doit stop"
@@ -23,37 +23,22 @@ stop_cmd="courier_doit stop"
 courier_doit()
 {
 	action=$1
-	case ${action} in
+	case $action in
 	start)
-		for _f in $required_vars; do
-			eval _value=\$${_f}
-			case $_value in
-			[Yy][Ee][Ss]|[Tt][Rr][Uu][Ee]|[Oo][Nn]|1)
-				;;
-			*)
-				@ECHO@ 1>&2 "$0: WARNING: \$${_f} is not set"
-				if [ -z $rc_force ]; then
-					return 1
-				fi
-				;;
-			esac
-		done
 		for f in $required_files; do
 			if [ ! -r "$f" ]; then
 				@ECHO@ 1>&2 "$0: WARNING: $f is not readable"
-				if [ -z $rc_force ]; then
-					return 1
-				fi
+				return 1
 			fi
 		done
 		@ECHO@ "Starting ${name}."
+		${ctl_command} $action
 		;;
 	stop)
 		@ECHO@ "Stopping ${name}."
+		${ctl_command} $action
 		;;
 	esac
-
-	${ctl_command} ${action}
 }
 
 load_rc_config $name
