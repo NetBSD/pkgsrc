49e0dc765c537819a7d90d45d9f34dae5eb17793
rillig <rillig@pkgsrc.org>
2006-05-01 18:02:11 +0000

    Fixed pkglint warnings.

--- audio/ladspa/buildlink3.mk
+++ audio/ladspa/buildlink3.mk
@@ -1,9 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/04/12 10:27:00 rillig Exp $
-#
-# This Makefile fragment is included by packages that use ladspa.
-#
-# This file was created automatically using createbuildlink-3.1.
-#
+# $NetBSD: buildlink3.mk,v 1.6 2006/05/01 18:02:11 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LADSPA_BUILDLINK3_MK:=	${LADSPA_BUILDLINK3_MK}+
@@ -12,12 +7,14 @@ LADSPA_BUILDLINK3_MK:=	${LADSPA_BUILDLINK3_MK}+
 BUILDLINK_DEPENDS+=	ladspa
 .endif
 
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nladspa}
+BUILDLINK_PACKAGES+=	ladspa
+
 .if !empty(LADSPA_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=			ladspa
 BUILDLINK_API_DEPENDS.ladspa+=		ladspa>=1.12
 BUILDLINK_ABI_DEPENDS.ladspa?=		ladspa>=1.12nb1
 BUILDLINK_PKGSRCDIR.ladspa?=		../../audio/ladspa
-
 .endif # LADSPA_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
