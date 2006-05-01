5db33b9e22873610a73e99b0fab7e5a4fc9d78c4
rillig <rillig@pkgsrc.org>
2006-05-01 17:17:52 +0000

    Removed preliminary comments.

--- archivers/ucl/buildlink3.mk
+++ archivers/ucl/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/04/06 06:21:33 reed Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.10.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.ucl?=	build
+# $NetBSD: buildlink3.mk,v 1.3 2006/05/01 17:17:52 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 UCL_BUILDLINK3_MK:=	${UCL_BUILDLINK3_MK}+
