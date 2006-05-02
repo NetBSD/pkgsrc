3cf7c7f5c9a416f7d669a8eaecdbffa4b3a4dbe0
joerg <joerg@pkgsrc.org>
2006-05-02 09:08:32 +0000

    Include libart in b3.mk, it is needed for linking. Reset maintainer on his request.

--- databases/rrdtool/buildlink3.mk
+++ databases/rrdtool/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/04/17 13:46:05 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2006/05/02 09:08:32 joerg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 RRDTOOL_BUILDLINK3_MK:=	${RRDTOOL_BUILDLINK3_MK}+
@@ -19,6 +19,7 @@ BUILDLINK_PKGSRCDIR.rrdtool?=	../../databases/rrdtool
 .include "../../graphics/freetype-lib/buildlink3.mk"
 .include "../../graphics/gd/buildlink3.mk"
 .include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/libart2/buildlink3.mk"
 .include "../../www/cgilib/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
