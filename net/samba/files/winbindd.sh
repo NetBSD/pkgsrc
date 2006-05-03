bffdca3f46091ebe8d2656eaf91ca467cd5a7180
jlam <jlam@pkgsrc.org>
2006-05-03 23:23:06 +0000

    Samba developers say that winbindd does not require nmbd to be running. Bump PKGREVISION to 1.

--- net/samba/files/winbindd.sh
+++ net/samba/files/winbindd.sh
@@ -1,33 +1,18 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: winbindd.sh,v 1.3 2005/12/21 04:17:53 jlam Exp $
+# $NetBSD: winbindd.sh,v 1.4 2006/05/03 23:23:06 jlam Exp $
 #
 # PROVIDE: winbindd
-# REQUIRE: nmbd
+# REQUIRE: DAEMON
 
 . /etc/rc.subr
 
 name="winbindd"
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
-required_vars="nmbd"
 required_files="@SAMBA_ETCDIR@/smb.conf"
 extra_commands="reload"
 command_args="-B"
 
-# load_rc_config_var() from /etc/rc.subr on the netbsd-3 branch, for
-# the benefit of platforms with older versions of /etc/rc.subr.
-#
-load_rc_config_var()
-{
-	eval $(eval '(
-		load_rc_config '$1' >/dev/null;
-		if [ -n "${'$2'}" -o "${'$2'-UNSET}" != "UNSET" ]; then
-			echo '$2'=\'\''${'$2'}\'\'';
-		fi
-	)' )
-}
-
 load_rc_config $name
-load_rc_config_var nmbd nmbd
 run_rc_command "$1"
