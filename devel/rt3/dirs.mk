e91a3a6b1670648b47b22f7aabd2b09f5b5bbc5b
cube <cube@pkgsrc.org>
2006-05-06 07:14:45 +0000

    Fix RT_I18N_DIR, which unbreaks devel/RTFM.  No bump required because only RTFM uses that value.

--- devel/rt3/dirs.mk
+++ devel/rt3/dirs.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dirs.mk,v 1.1 2006/05/01 09:38:08 cube Exp $
+# $NetBSD: dirs.mk,v 1.2 2006/05/06 07:14:45 cube Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
@@ -12,4 +12,4 @@ RT_SHARE_DIR=	${LOCALBASE}/share/rt3
 RT_VAR_DIR=	${VARBASE}/spool/rt3
 
 RT_HTML_DIR=	${RT_SHARE_DIR}/html
-RT_I18N_DIR=	${RT_LIB_DIR}/I18N
+RT_I18N_DIR=	${RT_LIB_DIR}/RT/I18N
