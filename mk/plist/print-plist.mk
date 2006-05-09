a7c206b3683406f9646559cf2c3ed411d0163b4c
minskim <minskim@pkgsrc.org>
2006-05-09 21:37:33 +0000

    Apply PRINT_PLIST_AWK and _PLIST_PLIST_AWK_SUBST in the same order for both files and directories.

--- mk/plist/print-plist.mk
+++ mk/plist/print-plist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: print-plist.mk,v 1.6 2006/04/17 06:12:46 jlam Exp $
+# $NetBSD: print-plist.mk,v 1.7 2006/05/09 21:37:33 minskim Exp $
 
 ###
 ### Automatic PLIST generation
@@ -126,6 +126,7 @@ print-PLIST:
 	 | ${AWK} '							\
 		{ sub("${PREFIX}/\\./", ""); }				\
 		${_PRINT_PLIST_AWK_IGNORE} { next; } 			\
+		${PRINT_PLIST_AWK}					\
 		${_PRINT_PLIST_AWK_SUBST}				\
 		/^@/ { print $$0; next }				\
 		/.*\/lib[^\/]+\.so\.[0-9]+\.[0-9]+\.[0-9]+$$/ { 	\
@@ -146,7 +147,6 @@ print-PLIST:
 			if ('$$genlinks') print $$0;			\
 			next;						\
 		}							\
-		${PRINT_PLIST_AWK}					\
 		{ print $$0; }'
 	${_PKG_SILENT}${_PKG_DEBUG}\
 	for i in `${_PRINT_PLIST_DIRS_CMD}				\
