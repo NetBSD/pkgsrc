6f7fab5bff6c3df7ef373e77fde3498fb47c85f7
adam <adam>
2006-05-13 19:15:51 +0000

    Changes 2.10.2: * Added --enable-api-properties, --enable-api-vfuncs, and --enable-api-exceptions.   When using these, the API, and any API generated by gmmproc, will be   changed. This allows users of embedded platforms to reduce the code size of *mm   libraries and *mm-using applications, at the small cost of losing some rarely-used   API. Alternatives exist for that API, as show in the examples.

--- devel/glibmm/buildlink3.mk
+++ devel/glibmm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/04/06 06:21:49 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/05/13 19:15:51 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GLIBMM_BUILDLINK3_MK:=	${GLIBMM_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nglibmm}
 BUILDLINK_PACKAGES+=	glibmm
 
 .if !empty(GLIBMM_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.glibmm+=	glibmm>=2.6.1
-BUILDLINK_ABI_DEPENDS.glibmm?=	glibmm>=2.8.3nb1
+BUILDLINK_ABI_DEPENDS.glibmm+=	glibmm>=2.8.3nb1
 BUILDLINK_PKGSRCDIR.glibmm?=	../../devel/glibmm
 .endif	# GLIBMM_BUILDLINK3_MK
 
