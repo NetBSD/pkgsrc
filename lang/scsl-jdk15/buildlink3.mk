40d4d734f2b8c1f693837b95871baac59655e40a
tv <tv@pkgsrc.org>
2006-05-15 19:51:53 +0000

    Fix buildlink DEPENDS versioning (these originally came from the sun-* incarnations).

--- lang/scsl-jdk15/buildlink3.mk
+++ lang/scsl-jdk15/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2006/05/15 19:44:07 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2006/05/15 19:51:53 tv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SCSL_JDK15_BUILDLINK3_MK:=	${SCSL_JDK15_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_PACKAGES+=	scsl-jdk15
 .if !empty(SCSL_JDK15_BUILDLINK3_MK:M+)
 
 BUILDLINK_API_DEPENDS.scsl-jdk15+=	scsl-jdk15-[0-9]*
-BUILDLINK_ABI_DEPENDS.scsl-jdk15?=	scsl-jdk15>=5.0.6nb1
+BUILDLINK_ABI_DEPENDS.scsl-jdk15?=	scsl-jdk15>=1.5.0
 BUILDLINK_PKGSRCDIR.scsl-jdk15?=	../../lang/scsl-jdk15
 BUILDLINK_DEPMETHOD.scsl-jdk15?= build
 
@@ -21,7 +21,7 @@ BUILDLINK_PASSTHRU_DIRS+=	${BUILDLINK_JAVA_PREFIX.scsl-jre15}
 
 BUILDLINK_CPPFLAGS.scsl-jdk15=						\
 	-I${BUILDLINK_JAVA_PREFIX.scsl-jre15}/include			\
-	-I${BUILDLINK_JAVA_PREFIX.scsl-jre15}/include/linux
+	-I${BUILDLINK_JAVA_PREFIX.scsl-jre15}/include/${LOWER_OPSYS}
 
 .include "../../lang/scsl-jre15/buildlink3.mk"
 
