ed973d66a1e71b1da59d659e45e73e96a5337799
tv <tv@pkgsrc.org>
2006-05-15 19:51:53 +0000

    Fix buildlink DEPENDS versioning (these originally came from the sun-* incarnations).

--- lang/scsl-jre15/buildlink3.mk
+++ lang/scsl-jre15/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2006/05/15 19:44:07 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2006/05/15 19:51:53 tv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SCSL_JRE15_BUILDLINK3_MK:=	${SCSL_JRE15_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	scsl-jre15
 
 .if !empty(SCSL_JRE15_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.scsl-jre15+=		scsl-jre15-[0-9]*
-BUILDLINK_ABI_DEPENDS.scsl-jre15?=		scsl-jre15>=5.0.6nb2
+BUILDLINK_ABI_DEPENDS.scsl-jre15?=		scsl-jre15>=1.5.0
 BUILDLINK_PKGSRCDIR.scsl-jre15?=		../../lang/scsl-jre15
 BUILDLINK_JAVA_PREFIX.scsl-jre15=	${PREFIX}/java/scsl-1.5
 .endif	# SCSL_JRE15_BUILDLINK3_MK
