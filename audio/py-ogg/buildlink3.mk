ff0d2cb3de94c53d21791224d0bccfa1c6cdf19a
rillig <rillig>
2006-05-19 16:14:14 +0000

    pyversion.mk only needs to be included once.

--- audio/py-ogg/buildlink3.mk
+++ audio/py-ogg/buildlink3.mk
@@ -1,9 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/04/12 10:27:02 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2006/05/19 16:14:14 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PY_OGG_BUILDLINK3_MK:=	${PY_OGG_BUILDLINK3_MK}+
-
-.include "../../lang/python/pyversion.mk"
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	pyogg
@@ -13,6 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npyogg}
 BUILDLINK_PACKAGES+=	pyogg
 
 .if !empty(PY_OGG_BUILDLINK3_MK:M+)
+.include "../../lang/python/pyversion.mk"
 BUILDLINK_API_DEPENDS.pyogg+=	${PYPKGPREFIX}-ogg>=1.3
 BUILDLINK_ABI_DEPENDS.pyogg?=	${PYPKGPREFIX}-ogg>=1.3nb2
 BUILDLINK_PKGSRCDIR.pyogg?=	../../audio/py-ogg
