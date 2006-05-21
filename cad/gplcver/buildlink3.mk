e2f978039e50c75aee009713c6ce7cb138324129
rillig <rillig@pkgsrc.org>
2006-05-21 11:44:39 +0000

    Removed default comment.

--- cad/gplcver/buildlink3.mk
+++ cad/gplcver/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/04/06 06:21:38 reed Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.10.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.gplcver?=	build
+# $NetBSD: buildlink3.mk,v 1.3 2006/05/21 11:44:39 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GPLCVER_BUILDLINK3_MK:=	${GPLCVER_BUILDLINK3_MK}+
