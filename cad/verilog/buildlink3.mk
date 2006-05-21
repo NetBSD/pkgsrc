4612187786675b1ca21a042d7703587fc96bc61b
rillig <rillig@pkgsrc.org>
2006-05-21 08:00:49 +0000

    Fixed pkglint warnings. Since bison and lex are not used when building, they don't need to be defined in USE_TOOLS.

--- cad/verilog/buildlink3.mk
+++ cad/verilog/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/04/06 06:21:38 reed Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.10.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.verilog?=	build
+# $NetBSD: buildlink3.mk,v 1.3 2006/05/21 08:00:50 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 VERILOG_BUILDLINK3_MK:=	${VERILOG_BUILDLINK3_MK}+
@@ -25,15 +15,9 @@ BUILDLINK_API_DEPENDS.verilog+=	verilog>=0.8.1nb1
 BUILDLINK_PKGSRCDIR.verilog?=	../../cad/verilog
 .endif	# VERILOG_BUILDLINK3_MK
 
-# XXX
-# XXX Uncomment and keep only the buildlink3 lines below which are directly
-# XXX needed for dependencies to compile, link, and run.  If this package
-# XXX provides a wrappered API or otherwise does not expose the APIs of the
-# XXX buildlink3 lines below to dependencies, remove them.
-# XXX
-#.include "../../devel/zlib/buildlink3.mk"
-#.include "../../archivers/bzip2/buildlink3.mk"
-#.include "../../devel/gperf/buildlink3.mk"
-#.include "../../devel/readline/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../devel/gperf/buildlink3.mk"
+.include "../../devel/readline/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
