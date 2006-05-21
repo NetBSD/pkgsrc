766c861a2c9fc8aae15faa5a6d9383971b4b3bd6
rillig <rillig@pkgsrc.org>
2006-05-21 16:34:19 +0000

    Fixed pkglint warnings.

--- devel/swig/buildlink3.mk
+++ devel/swig/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/04/13 13:32:53 wulf Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2006/05/21 16:34:19 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 SWIG_BUILDLINK3_MK:=	${SWIG_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	swig
 
 .if !empty(SWIG_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.swig+=	swig>=1.3.24
-BUILDLINK_ABI_DEPENDS.swig?=	swig>=1.3.29
+BUILDLINK_ABI_DEPENDS.swig+=	swig>=1.3.29
 BUILDLINK_PKGSRCDIR.swig?=	../../devel/swig
 # only contains a static library
 BUILDLINK_DEPMETHOD.swig?=	build
