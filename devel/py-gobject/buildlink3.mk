8879ba2de5cae5e7aa877b4402cd31039585e424
joerg <joerg@pkgsrc.org>
2006-05-21 18:44:46 +0000

    Split off the header and pc file into a separate package, shared by the various Python version dependent packages. Bump revision. Force new API version to avoid problems.

--- devel/py-gobject/buildlink3.mk
+++ devel/py-gobject/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2006/04/12 10:27:13 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2006/05/21 18:44:46 joerg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PY_GOBJECT_BUILDLINK3_MK:=	${PY_GOBJECT_BUILDLINK3_MK}+
@@ -13,10 +13,11 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npygobject}
 BUILDLINK_PACKAGES+=	pygobject
 
 .if !empty(PY_GOBJECT_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.pygobject+=	${PYPKGPREFIX}-gobject>=2.9.1
+BUILDLINK_API_DEPENDS.pygobject+=	${PYPKGPREFIX}-gobject>=2.10.1nb1
 BUILDLINK_PKGSRCDIR.pygobject?=	../../devel/py-gobject
 .endif	# PY_GOBJECT_BUILDLINK3_MK
 
+.include "../../devel/py-gobject-shared/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
