91754485dd770bda24805e715c801361802d55a6
adam <adam>
2006-05-23 10:17:20 +0000

    Changes 4.10:
    Cutting planes of two new classes were implemented: mixed cover
    cuts and clique cuts. On API level this feature can be enabled
    by setting control parameter LPX_K_USECUTS passed to the routine
    lpx_intopt. In glpsol this feature is available through the
    command-line options --cover and --clique. For more details see
    the reference manual.
    
    Now the routines lpx_read_mps and lpx_read_freemps support LI
    bound type. It is similar to LO, however, indicates the column
    as of integer kind.

--- math/glpk/buildlink3.mk
+++ math/glpk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/04/12 10:27:25 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/05/23 10:17:20 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GLPK_BUILDLINK3_MK:=	${GLPK_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	glpk
 
 .if !empty(GLPK_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.glpk+=	glpk>=4.8nb1
-BUILDLINK_ABI_DEPENDS.glpk?=	glpk>=4.9nb1
+BUILDLINK_ABI_DEPENDS.glpk+=	glpk>=4.9nb1
 BUILDLINK_PKGSRCDIR.glpk?=	../../math/glpk
 .endif	# GLPK_BUILDLINK3_MK
 
