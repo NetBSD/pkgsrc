f2f569dc8844cdc3e0a6de4c6f1fa0d95805dc61
rillig <rillig>
2006-05-23 11:17:33 +0000

    Fixed the automatic changes done by revbump. It had replaced ${PYPKGPREFIX} with py24. While here, fixed pkglint warnings.

--- graphics/py-cairo/buildlink3.mk
+++ graphics/py-cairo/buildlink3.mk
@@ -1,9 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/04/17 13:46:15 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2006/05/23 11:17:33 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PY_CAIRO_BUILDLINK3_MK:=	${PY_CAIRO_BUILDLINK3_MK}+
-
-.include "../../lang/python/pyversion.mk"
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	pycairo
@@ -13,8 +11,9 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npycairo}
 BUILDLINK_PACKAGES+=	pycairo
 
 .if !empty(PY_CAIRO_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.pycairo+=	${PYPKGPREFIX}-cairo>=1.0.2
-BUILDLINK_ABI_DEPENDS.pycairo?=	py24-cairo>=1.0.2nb1
+.  include "../../lang/python/pyversion.mk"
+BUILDLINK_API_DEPENDS.pycairo+=	${PYPKGPREFIX}-cairo>=1.0.2
+BUILDLINK_ABI_DEPENDS.pycairo+=	${PYPKGPREFIX}-cairo>=1.0.2nb1
 BUILDLINK_PKGSRCDIR.pycairo?=	../../graphics/py-cairo
 .endif	# PY_CAIRO_BUILDLINK3_MK
 
