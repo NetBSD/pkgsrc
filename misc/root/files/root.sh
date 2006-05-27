6a251ce4c6d3f6239166c08acd3a39764f15aa79
drochner <drochner@pkgsrc.org>
2006-05-27 12:38:27 +0000

    update to 5.10 There were 3 major revisions inbetween, so I honestly can't tell what changed. (would be good to libtoolize this one day, but for now I've done only minimal changes to the freebsd5 configuration)

--- misc/root/files/root.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# $NetBSD: root.sh,v 1.1.1.1 1999/11/17 16:17:03 drochner Exp $
-
-ROOTSYS=XXXPREFIXXXX/root
-export ROOTSYS
-PATH=XXXPREFIXXXX/root/bin:${PATH}
-exec XXXPREFIXXXX/root/bin/`basename $0` $*
