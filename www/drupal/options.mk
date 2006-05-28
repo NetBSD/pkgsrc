f48c70d74d94639176ff737134d84d8d801b20e5
adrianp <adrianp@pkgsrc.org>
2006-05-28 14:46:47 +0000

    Update to the 4.7.x branch of drupal For a full list of changes see: http://drupal.org/drupal-4.7.0 In short: - Updated Documentation for All Modules - Auto-complete Fields(AJAX) - Added Mass Comment Operations - Easier to Make Menu Items - RSS Feed Settings - Better Search Index - New Forms API

--- www/drupal/options.mk
+++ www/drupal/options.mk
@@ -1,11 +1,12 @@
-# $NetBSD: options.mk,v 1.1.1.1 2006/01/05 00:02:43 adrianp Exp $
+# $NetBSD: options.mk,v 1.2 2006/05/28 14:46:47 adrianp Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.drupal
 
 PKG_OPTIONS_REQUIRED_GROUPS=	db
 PKG_OPTIONS_GROUP.db=		mysql pgsql
 
-PKG_SUGGESTED_OPTIONS=	mysql
+PKG_SUPPORTED_OPTIONS=	drupal-xmlservices
+PKG_SUGGESTED_OPTIONS=	mysql drupal-xmlservices
 
 .include "../../mk/bsd.options.mk"
 
@@ -21,4 +22,16 @@ DEPENDS+=	php-pgsql-[0-9]*:../../databases/php-pgsql
 ###
 .	include "../../mk/mysql.buildlink3.mk"
 DEPENDS+=	php-mysql>=4.3.3:../../databases/php-mysql
+.endif
+
+###
+### Enable XML-based services such as the Blogger API, Jabber and RSS
+### syndication.
+###
+.if !empty(PKG_OPTIONS:Mdrupal-xmlservices)
+.	if ${PKG_PHP_VERSION} == "4"
+DEPENDS+=	php-domxml>=4.3.3:../../textproc/php4-domxml
+.	elif ${PKG_PHP_VERSION} == "5"
+DEPENDS+=	php-dom-[0-9]*:../../textproc/php5-dom
+.	endif
 .endif
