3ac4151678b2c36f22b750f4645d543fd7bc1b71
joerg <joerg@pkgsrc.org>
2006-05-28 15:50:05 +0000

    Triggers ICE with Propolice on DragonFly: disable.

--- graphics/Coin/hacks.mk
+++ graphics/Coin/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2006/03/19 20:06:05 minskim Exp $
+# $NetBSD: hacks.mk,v 1.2 2006/05/28 15:50:05 joerg Exp $
 
 .if !defined(COIN_HACKS_MK)
 COIN_HACKS_MK=	defined
@@ -11,6 +11,21 @@ COIN_HACKS_MK=	defined
 .if ${OPSYS} == "NetBSD"
 PKG_HACKS+=		optimization
 BUILDLINK_TRANSFORM+=   rm:-O[0-9]*
+.endif
+
+.if ${OPSYS} == "DragonFly" && ${PKGSRC_COMPILER} == "gcc"
+#
+# Workaround an ICE in the stack-smashing protection in GCC 3.4.x.
+#
+.if !defined(HAS_PROPOLICE)
+HAS_PROPOLICE!=	( ${CC} -v 2>&1 | ${GREP} 'propolice' ) 2>/dev/null || echo no
+MAKEVARS+=	HAS_PROPOLICE
+.endif
+
+.if ${HAS_PROPOLICE} != "no"
+CFLAGS+=	-fno-stack-protector
+CXXFLAGS+=	-fno-stack-protector
+.endif
 .endif
 
 .endif	# COIN_HACKS_MK
