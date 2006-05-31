3a81afccccb3767f9ad3a163e0478ddf8736324e
rillig <rillig@pkgsrc.org>
2006-05-31 10:09:24 +0000

    It suffices to load bsd.prefs.mk once.

--- databases/db/buildlink3.mk
+++ databases/db/buildlink3.mk
@@ -1,9 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.21 2006/04/12 10:27:04 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2006/05/31 10:09:24 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 DB_BUILDLINK3_MK:=	${DB_BUILDLINK3_MK}+
-
-.include "../../mk/bsd.prefs.mk"
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	db2
@@ -19,6 +17,7 @@ BUILDLINK_PKGSRCDIR.db2?=	../../databases/db
 BUILDLINK_INCDIRS.db2?=		include/db2
 BUILDLINK_LDADD.db2=		-ldb2
 
+.  include "../../mk/bsd.prefs.mk"
 .  if defined(USE_DB185) && !empty(USE_DB185:M[yY][eE][sS])
 BUILDLINK_LIBS.db2=		${BUILDLINK_LDADD.db2}
 BUILDLINK_TRANSFORM+=		l:db:db2
