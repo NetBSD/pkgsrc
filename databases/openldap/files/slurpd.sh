c2ebb198db115d7221a4b817ee7db50adb03d17d
ghen <ghen>
2006-05-31 18:17:49 +0000

    Split the databases/openldap package in -client and -server component packages (and add a -doc component), which I'll import in a minute.  This package is now nothing but a meta-package, depending on those three.  Take over MAINTAINERship (ok with joerg).  Add an inet6 option to -client and -server (PR pkg/33524). Bump PKGREVISION.

--- databases/openldap/files/slurpd.sh
+++ databases/openldap/files/slurpd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: slurpd.sh,v 1.3 2005/10/26 15:08:13 jlam Exp $
+# $NetBSD: slurpd.sh,v 1.4 2006/05/31 18:17:49 ghen Exp $
 #
 # OpenLDAP LDAP database replication daemon
 #
@@ -14,6 +14,7 @@ rcvar=${name}
 command="@PREFIX@/libexec/${name}"
 required_files="@OPENLDAP_ETCDIR@/slapd.conf"
 required_vars="slapd"
+slurpd_user="@SLURPD_USER@"
 
 load_rc_config $name
 run_rc_command "$1"
