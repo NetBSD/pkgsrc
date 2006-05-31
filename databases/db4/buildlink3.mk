38437277d81fc4ef315eaa9ec01e59947cef4dcb
rillig <rillig>
2006-05-31 10:13:23 +0000

    Load the files bsd.prefs.mk and compiler.mk only once.

--- databases/db4/buildlink3.mk
+++ databases/db4/buildlink3.mk
@@ -1,9 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.26 2006/04/06 06:21:40 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2006/05/31 10:13:23 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 DB4_BUILDLINK3_MK:=	${DB4_BUILDLINK3_MK}+
-
-.include "../../mk/bsd.prefs.mk"
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	db4
@@ -19,6 +17,7 @@ BUILDLINK_INCDIRS.db4?=		include/db4
 BUILDLINK_LDADD.db4=		-ldb4
 BUILDLINK_TRANSFORM+=		l:db-4:db4
 
+.  include "../../mk/bsd.prefs.mk"
 .  if defined(USE_DB185) && !empty(USE_DB185:M[yY][eE][sS])
 #
 # Older db4 packages didn't enable the db-1.85 compatibility API.
@@ -27,12 +26,12 @@ BUILDLINK_API_DEPENDS.db4+=		db4>=4.2.52nb1
 BUILDLINK_LIBS.db4=		${BUILDLINK_LDADD.db4}
 BUILDLINK_TRANSFORM+=		l:db:db4
 .  endif
-.endif	# DB4_BUILDLINK3_MK
 
-.include "../../mk/compiler.mk"
-.if empty(PKGSRC_COMPILER:Mgcc)
+.  include "../../mk/compiler.mk"
+.  if empty(PKGSRC_COMPILER:Mgcc)
 PTHREAD_OPTS+=	native
-.  include "../../mk/pthread.buildlink3.mk"
-.endif
+.    include "../../mk/pthread.buildlink3.mk"
+.  endif
+.endif	# DB4_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
