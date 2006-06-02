9d2fa554c4a3f64b36e9c5e8f78a89ac9dc88ba2
joerg <joerg@pkgsrc.org>
2006-06-02 18:27:54 +0000

    Rename all PHP 4 packages to php4-*, all PHP 5 packages to php5-*, all PEAR packages to php?-pear-* and all Apache packages to ap13-* or ap2-* respectively. Add new variables to simplify the Makefile handling. Add CONFLICTS on the old names. Reset revisions of bumped packages. ap-php will now depend on the default Apache and PHP version. All programs using it have an implicit option of the Apache version as well.
    OK from jlam@ and adrianp@.

--- www/ap-perl/buildlink3.mk
+++ www/ap-perl/buildlink3.mk
@@ -1,18 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/04/12 10:27:38 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2006/06/02 18:27:59 joerg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 AP_PERL_BUILDLINK3_MK:=	${AP_PERL_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	ap-perl
+BUILDLINK_DEPENDS+=	ap13-perl
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nap-perl}
-BUILDLINK_PACKAGES+=	ap-perl
+BUILDLINK_PACKAGES+=	ap13-perl
 
 .if !empty(AP_PERL_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.ap-perl+=	ap-perl>=1.29nb3
-BUILDLINK_ABI_DEPENDS.ap-perl?=	ap-perl>=1.29nb5
+BUILDLINK_API_DEPENDS.ap-perl+=	ap13-perl>=1.29nb3
+BUILDLINK_ABI_DEPENDS.ap-perl?=	ap13-perl>=1.29nb5
 BUILDLINK_PKGSRCDIR.ap-perl?=	../../www/ap-perl
 
 BUILDLINK_FILES.ap-perl+=	lib/httpd/mod_perl.*
