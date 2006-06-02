9ba0a3e5d9e971cbb9f495e39132f009caa2d798
tron <tron@pkgsrc.org>
2006-06-02 08:39:00 +0000

    Set "USE_NCURSES" here because program using "libcdk" will otherwise fail to link properly.

--- devel/cdk/buildlink3.mk
+++ devel/cdk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/04/12 10:27:07 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/06/02 08:39:00 tron Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CDK_BUILDLINK3_MK:=	${CDK_BUILDLINK3_MK}+
@@ -15,6 +15,8 @@ BUILDLINK_API_DEPENDS.cdk+=		cdk>=4.9.9nb1
 BUILDLINK_ABI_DEPENDS.cdk+=	cdk>=4.9.9nb2
 BUILDLINK_PKGSRCDIR.cdk?=	../../devel/cdk
 .endif	# CDK_BUILDLINK3_MK
+
+USE_NCURSES=	yes
 
 .include "../../devel/ncurses/buildlink3.mk"
 
