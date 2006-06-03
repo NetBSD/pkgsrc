da00355ef9f0ccdb29bf7fc00bdd973c5a3e99ef
jlam <jlam@pkgsrc.org>
2006-06-03 23:11:42 +0000

    First pass at implementing support for package system flavors other than pkgsrc's current one.  This is an important lead-up to any project that redesigns the pkg_* tools in that it doesn't tie us to past design (mis)choices.  This commit mostly deals with rearranging code, although there was a considerable amount of rewriting done in cases where I thought the code was somewhat messy and was difficult to understand.
    The design I chose for supporting multiple package system flavors is
    that the various depends, install, package, etc.  modules would define
    default targets and variables that may be overridden in files from
    pkgsrc/mk/flavor/${PKG_FLAVOR}.  The default targets would do the
    sensible thing of doing nothing, and pkgsrc infrastructure would rely
    on the appropriate things to be defined in pkgsrc/mk/flavor to do the
    real work.  The pkgsrc/mk/flavor directory contains subdirectories
    corresponding to each package system flavor that we support.  Currently,
    I only have "pkg" which represents the current pkgsrc-native package
    flavor.  I've separated out most of the code where we make assumptions
    about the package system flavor, mostly either because we directly
    use the pkg_* tools, or we make assumptions about the package meta-data
    directory, or we directly manipulate the package meta-data files, and
    placed it into pkgsrc/mk/flavor/pkg.
    
    There are several new modules that have been refactored out of bsd.pkg.mk
    as part of these changes: check, depends, install, package, and update.
    Each of these modules has been slimmed down by rewriting them to avoid
    some recursive make calls.  I've also religiously documented which
    targets are "public" and which are "private" so that users won't rely
    on reaching into pkgsrc innards to call a private target.
    
    The "depends" module is a complete overhaul of the way that we handle
    dependencies.  There is now a separate "depends" phase that occurs
    before the "extract" phase where dependencies are installed.  This
    differs from the old way where dependencies were installed just before
    extraction occurred.  The reduce-depends.mk file is now replaced by
    a script that is invoked only once during the depends phase and is
    used to generate a cookie file that holds the full set of reduced
    dependencies.  It is now possible to type "make depends" in a package
    directory and all missing dependencies will be installed.
    
    Future work on this project include:
    
        * Resolve the workflow design in anticipation of future work on
          staged installations where "package" conceptually happens before
          "install".
    
        * Rewrite the buildlink3 framework to not assume the use of the
          pkgsrc pkg_* tools.
    
        * Rewrite the pkginstall framework to provide a standard pkg_*
          tool to perform the actions, and allowing a purely declarative
          file per package to describe what actions need to be taken at
          install or deinstall time.
    
        * Implement support for the SVR4 package flavor.  This will be
          proof that the appropriate abstractions are in place to allow
          using a completely different set of package management tools.

---
+++ mk/flavor/pkg/views.mk
@@ -0,0 +1,91 @@
+# $NetBSD: views.mk,v 1.1 2006/06/03 23:11:42 jlam Exp $
+
+# By default, all packages attempt to link into the views.
+.if ${PKG_INSTALLATION_TYPE} == "pkgviews"
+BUILD_VIEWS?=	yes
+.endif
+
+# DEFAULT_VIEW.${PKGBASE}	default view for ${PKGBASE}
+# VIEWBASE			base location of files at run-time
+#
+DEFAULT_VIEW.${PKGBASE}?=	${DEFAULT_VIEW}
+.if ${PKG_INSTALLATION_TYPE} == "pkgviews"
+.  if empty(DEFAULT_VIEW.${PKGBASE}:M"")
+VIEWBASE=	${LOCALBASE}/${DEFAULT_VIEW.${PKGBASE}}
+.  else
+VIEWBASE=	${LOCALBASE}
+.  endif
+.elif ${PKG_INSTALLATION_TYPE} == "overwrite"
+VIEWBASE=	${PREFIX}
+.endif
+MAKE_ENV+=	VIEWBASE=${VIEWBASE}
+
+PKGVIEWS+=	${DEFAULT_VIEW.${PKGBASE}}
+
+.if ${PKG_INSTALLATION_TYPE} == "pkgviews"
+privileged-install-hook: build-views
+.endif
+
+######################################################################
+### build-views, su-build-views (PRIVATE)
+######################################################################
+### build-views adds the package to each view in PKGVIEWS.
+###
+.PHONY: build-views su-build-views
+build-views: su-target
+	@${ECHO_MSG} "${_PKGSRC_IN}> Building views for ${PKGNAME}"
+
+su-build-views:
+	${_PKG_SILENT}${_PKG_DEBUG}					\
+	${MKDIR} ${LOCALBASE};						\
+	for v in "" ${PKGVIEWS}; do					\
+		${TEST} -n "$$v" || continue;				\
+		case "$$v" in						\
+		"")	dbdir=${PKG_DBDIR}; viewname=standard ;;	\
+		*)	dbdir=${LOCALBASE}/$$v/.dbdir; viewname=$$v ;;	\
+		esac;							\
+		${ECHO} "=> Performing package view clash check for ${PKGNAME} in $$viewname view"; \
+		pkg=`${PKG_INFO_CMD} -K $$dbdir -e ${PKGBASE} || ${TRUE}`; \
+		case "$$pkg" in						\
+		"")	;;						\
+		*)	${ECHO} "*** ${PKGBASE} exists in $$viewname view - package $$pkg ***"; \
+			${ECHO} "*** Not hoisting ${PKGNAME} into $$viewname view"; \
+			continue;					\
+			;;						\
+		esac;							\
+		${ECHO} "=> Performing package view overwrite check for ${PKGNAME} in $$viewname view"; \
+		dups=`${SETENV} PLIST_IGNORE_FILES=${_PLIST_IGNORE_FILES:Q} ${PKG_VIEW} --view=$$v check ${PKGNAME} || ${TRUE}`; \
+		case "$$dups" in					\
+		"")	;;						\
+		*)	${ECHO} "***********************************************************"; \
+			${ECHO} "**** The following symbolic links will be overwritten *****"; \
+			for f in $$dups; do				\
+				${LS} -l ${LOCALBASE}/$$v/$$f;		\
+			done;						\
+			${ECHO} "***********************************************************"; \
+			;;						\
+		esac;							\
+		${ECHO} "=> Linking package into $$viewname view";	\
+		${SETENV} PLIST_IGNORE_FILES=${_PLIST_IGNORE_FILES:Q} ${PKG_VIEW} --view=$$v add ${PKGNAME}; \
+	done
+
+######################################################################
+### remove-views, su-remove-views (PRIVATE)
+######################################################################
+### remove-views removes the package from each view in PKGVIEWS.
+###
+.PHONY: remove-views su-remove-views
+remove-views: su-target
+	@${ECHO_MSG} "${_PKGSRC_IN}> Removing ${PKGNAME} from views"
+
+su-remove-views:
+	${_PKG_SILENT}${_PKG_DEBUG}					\
+	for v in "" ${PKGVIEWS}; do					\
+		${TEST} -n "$$v" || continue;				\
+		case "$$v" in						\
+		"")	dbdir=${PKG_DBDIR}; viewname=standard ;;	\
+		*)	dbdir=${LOCALBASE}/$$v/.dbdir; viewname=$$v ;;	\
+		esac;							\
+		${ECHO} "=> Removing package from $$viewname view";	\
+		${SETENV} PLIST_IGNORE_FILES=${_PLIST_IGNORE_FILES:Q} ${PKG_VIEW} --view=$$v delete ${PKGNAME}; \
+	done
