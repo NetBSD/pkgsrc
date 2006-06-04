52b46935ecae942434f4f7c2f82082fc5bed4307
joerg <joerg@pkgsrc.org>
2006-06-04 20:29:40 +0000

    Suffixes should match everywhere.

--- www/ap-perl/buildlink3.mk
+++ www/ap-perl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2006/06/02 18:27:59 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2006/06/04 20:29:40 joerg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 AP_PERL_BUILDLINK3_MK:=	${AP_PERL_BUILDLINK3_MK}+
@@ -11,14 +11,14 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nap-perl}
 BUILDLINK_PACKAGES+=	ap13-perl
 
 .if !empty(AP_PERL_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.ap-perl+=	ap13-perl>=1.29nb3
-BUILDLINK_ABI_DEPENDS.ap-perl?=	ap13-perl>=1.29nb5
-BUILDLINK_PKGSRCDIR.ap-perl?=	../../www/ap-perl
+BUILDLINK_API_DEPENDS.ap13-perl+=	ap13-perl>=1.29nb3
+BUILDLINK_ABI_DEPENDS.ap13-perl?=	ap13-perl>=1.29nb5
+BUILDLINK_PKGSRCDIR.ap13-perl?=	../../www/ap-perl
 
-BUILDLINK_FILES.ap-perl+=	lib/httpd/mod_perl.*
-BUILDLINK_FILES.ap-perl+=	\
+BUILDLINK_FILES.ap13-perl+=	lib/httpd/mod_perl.*
+BUILDLINK_FILES.ap13-perl+=	\
 	${PERL5_SUB_INSTALLVENDORARCH}/auto/Apache/include/*
-BUILDLINK_FILES.ap-perl+=	\
+BUILDLINK_FILES.ap13-perl+=	\
 	${PERL5_SUB_INSTALLVENDORARCH}/auto/Apache/include/modules/perl/*
 .endif	# AP_PERL_BUILDLINK3_MK
 
