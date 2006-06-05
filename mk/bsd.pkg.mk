a41bc8906945ca4bf533637f81f9aaa0506c258c
jlam <jlam@pkgsrc.org>
2006-06-05 01:42:58 +0000

    Backout previous and fix in another way -- use "set -e" to force the shell to exit if any untested command fails.  This is based on Roland Illig's recommendation from his pkglint talk at pkgsrcCon/Paris.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1834 2006/06/04 20:10:12 tv Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1835 2006/06/05 01:42:58 jlam Exp $
 #
 # This file is in the public domain.
 #
@@ -1693,7 +1693,7 @@ real-test: test-message pre-test do-test post-test test-cookie
 #
 .PHONY: su-target
 su-target: .USE
-	${_PKG_SILENT}${_PKG_DEBUG}					\
+	${_PKG_SILENT}${_PKG_DEBUG}set -e;				\
 	case ${PRE_CMD.su-${.TARGET}:Q}"" in				\
 	"")	;;							\
 	*)	${PRE_CMD.su-${.TARGET}} ;;				\
@@ -1706,8 +1706,8 @@ su-target: .USE
 		*) ${ECHO} "*** WARNING *** Running: "${PRE_ROOT_CMD:Q} ;; \
 		esac;							\
 		${PRE_ROOT_CMD};					\
-		${ECHO_MSG} "${_PKGSRC_IN}> Becoming \`\`${ROOT_USER}'' to make su-${.TARGET} (`${ECHO} ${SU_CMD} | ${AWK} '{ print $$1 }'`)" && \
-		${SU_CMD} "cd ${.CURDIR}; ${SETENV} PATH='$${PATH}:${SU_CMD_PATH_APPEND}' ${MAKE} ${MAKEFLAGS} PKG_DEBUG_LEVEL=${PKG_DEBUG_LEVEL} su-${.TARGET} ${MAKEFLAGS.su-${.TARGET}}" && \
+		${ECHO_MSG} "${_PKGSRC_IN}> Becoming \`\`${ROOT_USER}'' to make su-${.TARGET} (`${ECHO} ${SU_CMD} | ${AWK} '{ print $$1 }'`)"; \
+		${SU_CMD} "cd ${.CURDIR}; ${SETENV} PATH='$${PATH}:${SU_CMD_PATH_APPEND}' ${MAKE} ${MAKEFLAGS} PKG_DEBUG_LEVEL=${PKG_DEBUG_LEVEL} su-${.TARGET} ${MAKEFLAGS.su-${.TARGET}}"; \
 		${ECHO_MSG} "${_PKGSRC_IN}> Dropping \`\`${ROOT_USER}'' privileges."; \
 	fi
 
