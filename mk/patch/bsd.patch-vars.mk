ed32d966c9a714e12792fd0df8b251e85a843c4c
jlam <jlam@pkgsrc.org>
2006-06-06 04:48:19 +0000

    Refactor "patch" code into correspondingly named subdirectory of pkgsrc/mk.  Also get rid of the recursive make for the "patch" target. This basically merges the "patch" phase into the "tools" phase.
    There should eventually be a standalone script that can be used to
    verify checksums listed in distinfo that should be used instead of
    the roll-your-own code in the do-pkgsrc-patch target.

---
+++ mk/patch/bsd.patch-vars.mk
@@ -0,0 +1,44 @@
+# $NetBSD: bsd.patch-vars.mk,v 1.1 2006/06/06 04:48:19 jlam Exp $
+#
+# This Makefile fragment is included to bsd.prefs.mk and defines some
+# variables which must be defined earlier than where bsd.patch.mk is
+# included.
+#
+# The following variables may be set in a package Makefile:
+#
+#    PATCHFILES is a list of distribution patches relative to
+#	${_DISTDIR} that are applied first to the package.
+#
+#    PATCHDIR is the location of the pkgsrc patches for the package.
+#	This defaults to the "patches" subdirectory of the package
+#	directory.
+#
+# The following variables may be set by the user:
+#
+#    LOCALPATCHES is the location of local patches that are maintained
+#	in a directory tree reflecting the same hierarchy as the pkgsrc
+#	tree, e.g., local patches for www/apache would be found in
+#	${LOCALPATCHES}/www/apache.  These patches are applied after
+#	the patches in ${PATCHDIR}.
+#
+
+PATCHDIR?=	${.CURDIR}/patches
+
+.if (defined(PATCHFILES) && !empty(PATCHFILES)) || \
+    (defined(PATCHDIR) && exists(${PATCHDIR})) || \
+    (defined(LOCALPATCHES) && exists(${LOCALPATCHES}/${PKGPATH}))
+USE_TOOLS+=	patch
+.endif
+
+# These tools are used to output the contents of the distribution patches
+# to stdout.
+#
+.if defined(PATCHFILES)
+USE_TOOLS+=	cat
+.  if !empty(PATCHFILES:M*.Z) || !empty(PATCHFILES:M*.gz)
+USE_TOOLS+=	gzcat
+.  endif
+.  if !empty(PATCHFILES:M*.bz2)
+USE_TOOLS+=	bzcat
+.  endif
+.endif
