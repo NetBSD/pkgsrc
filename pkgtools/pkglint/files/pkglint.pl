12bf720ca3b123fb1aeaa9093977702279bad57e
rillig <rillig@pkgsrc.org>
2006-06-06 11:41:40 +0000

    Completed the recent change by Stoned, who overlooked a second instance of bsd.sites.mk in a comment.

--- pkgtools/pkglint/files/pkglint.pl
+++ pkgtools/pkglint/files/pkglint.pl
@@ -1,5 +1,5 @@
 #! @PERL@
-# $NetBSD: pkglint.pl,v 1.607 2006/06/06 11:39:25 rillig Exp $
+# $NetBSD: pkglint.pl,v 1.608 2006/06/06 11:41:40 rillig Exp $
 #
 
 # pkglint - static analyzer and checker for pkgsrc packages
@@ -1985,7 +1985,7 @@ sub load_dist_sites() {
 		}
 	}
 
-	# Explicitly allowed, although not defined in mk/bsd.sites.mk.
+	# Explicitly allowed, although not defined in mk/fetch/sites.mk.
 	$names->{"MASTER_SITE_SUSE_UPD"} = true;
 	$names->{"MASTER_SITE_LOCAL"} = true;
 
