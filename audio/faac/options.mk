4655911b97541960577e0981eee7acc83378b4c3
hira <hira@pkgsrc.org>
2006-06-07 21:33:21 +0000

    Use `mpeg4ip' option instead of `faad' option for encoding to AAC format.  No PKGREVISION bump because it's disabled by default.

--- audio/faac/options.mk
+++ audio/faac/options.mk
@@ -1,11 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2005/12/11 11:09:06 hira Exp $
+# $NetBSD: options.mk,v 1.2 2006/06/07 21:33:21 hira Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.faac
-PKG_SUPPORTED_OPTIONS=	faad
+PKG_SUPPORTED_OPTIONS=	mpeg4ip
 PKG_SUGGESTED_OPTIONS=
+PKG_OPTIONS_LEGACY_OPTS+=faad:mpeg4ip
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mfaad)
-.  include "../../audio/faad2/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mmpeg4ip)
+.  include "../../multimedia/mpeg4ip/buildlink3.mk"
 .endif
