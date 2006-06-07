3177559ea2a266aa1001926ce0dacef5f903815b
jlam <jlam>
2006-06-07 17:04:01 +0000

    Note the check-clean target, which removes files related to the check-* targets.

--- mk/check/bsd.check.mk
+++ mk/check/bsd.check.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.check.mk,v 1.1 2006/06/03 23:11:42 jlam Exp $
+# $NetBSD: bsd.check.mk,v 1.2 2006/06/07 17:04:01 jlam Exp $
 #
 # This Makefile fragment is included by bsd.pkg.mk and provides all
 # variables and targets related to build and install checks.
@@ -14,3 +14,13 @@
 .include "${PKGSRCDIR}/mk/check/check-shlibs.mk"
 .include "${PKGSRCDIR}/mk/check/check-vulnerable.mk"
 .include "${PKGSRCDIR}/mk/check/check-wrkref.mk"
+
+######################################################################
+### check-clean (PRIVATE)
+######################################################################
+### check-clean is a helper target onto which one can hook all of the
+### targets that clean up the output of "check" targets.
+###
+.PHONY: check-clean
+check-clean:
+	@${DO_NADA}
