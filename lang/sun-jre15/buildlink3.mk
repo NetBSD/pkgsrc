785b283709c270c869a90fb6225aac30bf2544f5
rillig <rillig@pkgsrc.org>
2006-06-08 21:21:21 +0000

    Fixed some pkglint warnings.

--- lang/sun-jre15/buildlink3.mk
+++ lang/sun-jre15/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/04/12 10:27:22 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2006/06/08 21:21:21 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SUN_JRE15_BUILDLINK3_MK:=	${SUN_JRE15_BUILDLINK3_MK}+
@@ -15,8 +15,8 @@ BUILDLINK_API_DEPENDS.sun-jre15+=		sun-jre15-[0-9]*
 BUILDLINK_ABI_DEPENDS.sun-jre15?=		sun-jre15>=5.0.6nb2
 BUILDLINK_PKGSRCDIR.sun-jre15?=		../../lang/sun-jre15
 BUILDLINK_JAVA_PREFIX.sun-jre15=	${PREFIX}/java/sun-1.5
-.endif	# SUN_JRE15_BUILDLINK3_MK
 
 UNLIMIT_RESOURCES+=	datasize	# Must be at least 131204
+.endif	# SUN_JRE15_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
