59548db9f28aced55e12ce0a9e48381cd04ecf8c
hira <hira@pkgsrc.org>
2006-06-08 09:09:21 +0000

    Use `mpeg4ip' option instead of `faad' option for reading AAC files. No PKGREVISION bump because it's disabled by default.

--- audio/gtkpod/options.mk
+++ audio/gtkpod/options.mk
@@ -1,11 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2005/11/15 16:12:33 hira Exp $
+# $NetBSD: options.mk,v 1.2 2006/06/08 09:09:21 hira Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.gtkpod
-PKG_SUPPORTED_OPTIONS=	faad
+PKG_OPTIONS_VAR=		PKG_OPTIONS.gtkpod
+PKG_SUPPORTED_OPTIONS=		mpeg4ip
 PKG_SUGGESTED_OPTIONS=
+PKG_OPTIONS_LEGACY_OPTS+=	faad:mpeg4ip
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mfaad)
-.  include "../../audio/faad2/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mmpeg4ip)
+.  include "../../multimedia/mpeg4ip/buildlink3.mk"
 .endif
