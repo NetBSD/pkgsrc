1997485f031ceca00ae8051911deb51149b811a6
joerg <joerg@pkgsrc.org>
2006-06-08 12:18:27 +0000

    Fix impossible dependencies due to the reset of the revision of ap13-perl.

--- www/ap-perl/buildlink3.mk
+++ www/ap-perl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2006/06/04 20:29:40 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2006/06/08 12:18:27 joerg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 AP_PERL_BUILDLINK3_MK:=	${AP_PERL_BUILDLINK3_MK}+
@@ -11,8 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nap-perl}
 BUILDLINK_PACKAGES+=	ap13-perl
 
 .if !empty(AP_PERL_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.ap13-perl+=	ap13-perl>=1.29nb3
-BUILDLINK_ABI_DEPENDS.ap13-perl?=	ap13-perl>=1.29nb5
+BUILDLINK_API_DEPENDS.ap13-perl+=	ap13-perl>=1.29
+BUILDLINK_ABI_DEPENDS.ap13-perl?=	ap13-perl>=1.29
 BUILDLINK_PKGSRCDIR.ap13-perl?=	../../www/ap-perl
 
 BUILDLINK_FILES.ap13-perl+=	lib/httpd/mod_perl.*
