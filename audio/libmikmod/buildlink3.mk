f706a2d82d0e82f23f948e503cc4477629920d3e
rillig <rillig>
2006-06-08 14:44:21 +0000

    Fixed pkglint warnings.

--- audio/libmikmod/buildlink3.mk
+++ audio/libmikmod/buildlink3.mk
@@ -1,35 +1,34 @@
-# $NetBSD: buildlink3.mk,v 1.10 2006/04/12 10:27:01 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2006/06/08 14:44:21 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBMIKMOD_BUILDLINK3_MK:=	${LIBMIKMOD_BUILDLINK3_MK}+
 
-.include "../../mk/bsd.prefs.mk"
-
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	libmikmod
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibmikmod}
 BUILDLINK_PACKAGES+=	libmikmod
 
-.if !empty(LIBMIKMOD_BUILDLINK3_MK:M+)
+.if ${LIBMIKMOD_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.libmikmod+=	libmikmod>=3.1.9
 BUILDLINK_ABI_DEPENDS.libmikmod+=	libmikmod>=3.1.11.1nb2
 BUILDLINK_PKGSRCDIR.libmikmod?=	../../audio/libmikmod
-.endif	# LIBMIKMOD_BUILDLINK3_MK
 
-.if !defined(PKG_BUILD_OPTIONS.libmikmod)
+.  include "../../mk/bsd.prefs.mk"
+.  if !defined(PKG_BUILD_OPTIONS.libmikmod)
 PKG_BUILD_OPTIONS.libmikmod!=	cd ${BUILDLINK_PKGSRCDIR.libmikmod} && \
 			${MAKE} show-var ${MAKEFLAGS} VARNAME=PKG_OPTIONS
 MAKEFLAGS+=	PKG_BUILD_OPTIONS.libmikmod=${PKG_BUILD_OPTIONS.libmikmod:Q}
-.endif
+.  endif
 MAKEVARS+=	PKG_BUILD_OPTIONS.libmikmod
 
 # On some platforms, libmikmod dynamically loads esound,
 # so there is no library dependency
-.if !empty(PKG_BUILD_OPTIONS.libmikmod:Mesound) && \
+.  if !empty(PKG_BUILD_OPTIONS.libmikmod:Mesound) && \
   empty(OPSYS:MNetBSD) && empty(OPSYS:MLinux)
-.  include "../../audio/esound/buildlink3.mk"
-.endif
+.    include "../../audio/esound/buildlink3.mk"
+.  endif
+.endif	# LIBMIKMOD_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
