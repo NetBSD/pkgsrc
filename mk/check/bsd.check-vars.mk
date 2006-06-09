9508da689f831f58ea08e37f0bf5bb7a672e57b0
jlam <jlam@pkgsrc.org>
2006-06-09 16:12:08 +0000

    Add a new variable CHECK_FILES_SUPPORTED which is a package-settable variable to show whether the package supports running the check-files target.
    Set CHECK_FILES_SUPPORTED to "no" in pkgtools/pkg_install in the case
    where the PREFIX does not match ${LOCALBASE} it's likely the tools are
    being installed in some place that's completely outside pkgsrc control,
    and check-files fails horribly in that case.

--- mk/check/bsd.check-vars.mk
+++ mk/check/bsd.check-vars.mk
@@ -1,10 +1,11 @@
-# $NetBSD: bsd.check-vars.mk,v 1.2 2006/06/06 15:28:52 jlam Exp $
+# $NetBSD: bsd.check-vars.mk,v 1.3 2006/06/09 16:12:08 jlam Exp $
 #
 # This Makefile fragment is included separately by bsd.pkg.mk and
 # defines some variables which must be defined earlier than where
 # bsd.check.mk is included.
 #
 
+CHECK_FILES_SUPPORTED?=		yes
 CHECK_SHLIBS_SUPPORTED?=	yes
 
 USE_TOOLS+=	awk cat cmp diff echo find grep rm sed test touch true
