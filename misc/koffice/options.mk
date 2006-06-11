4df5e84bad58c1810473ff59ca0b0118db926d0f
markd <markd@pkgsrc.org>
2006-06-11 13:50:19 +0000

    Make msword and word perfect import support optional but on by default. Patch from YOMURA Masanori, cleaned up by me. No PKGREVISON bump as no change to default.

--- misc/koffice/options.mk
+++ misc/koffice/options.mk
@@ -1,7 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2006/04/12 12:37:35 markd Exp $
+# $NetBSD: options.mk,v 1.3 2006/06/11 13:50:19 markd Exp $
 
 PKG_OPTIONS_VAR=        PKG_OPTIONS.koffice
-PKG_SUPPORTED_OPTIONS=  mysql pgsql
+PKG_SUPPORTED_OPTIONS=  mysql pgsql koffice-wv2 koffice-libwpd
+PKG_SUGGESTED_OPTIONS=	koffice-wv2 koffice-libwpd
 
 .include "../../mk/bsd.options.mk"
 
@@ -24,4 +25,19 @@ PLIST_SUBST+=		HAVE_PGSQL=
 .else
 CONFIGURE_ARGS+=	--disable-pgsql
 PLIST_SUBST+=		HAVE_PGSQL="@comment "
+.endif
+
+.if !empty(PKG_OPTIONS:Mkoffice-wv2)
+.include "../../converters/wv2/buildlink3.mk"
+PLIST_SUBST+=		HAVE_WV2=
+.else
+CONFIGURE_ARGS+=	--without-libwv2
+PLIST_SUBST+=		HAVE_WV2="@comment "
+.endif
+
+.if !empty(PKG_OPTIONS:Mkoffice-libwpd)
+.include "../../converters/libwpd/buildlink3.mk"
+PLIST_SUBST+=		HAVE_WPD=
+.else
+PLIST_SUBST+=		HAVE_WPD="@comment "
 .endif
