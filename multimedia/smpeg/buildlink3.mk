5c1eb6342d58698b0f3dc7869d642cc65b773b90
wiz <wiz>
2006-06-12 16:28:05 +0000

    Bump PKGREVISION and BUILDLINK_ABI_DEPENDS (where applicable) for SDL shlib changes.

--- multimedia/smpeg/buildlink3.mk
+++ multimedia/smpeg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2006/04/06 06:22:27 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2006/06/12 16:28:12 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 SPMEG_BUILDLINK3_MK:=	${SPMEG_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	smpeg
 
 .if !empty(SPMEG_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.smpeg+=	smpeg>=0.4.4nb3
-BUILDLINK_ABI_DEPENDS.smpeg+=	smpeg>=0.4.4nb12
+BUILDLINK_ABI_DEPENDS.smpeg+=	smpeg>=0.4.4nb13
 BUILDLINK_PKGSRCDIR.smpeg?=	../../multimedia/smpeg
 .endif	# SMPEG_BUILDLINK3_MK
 
