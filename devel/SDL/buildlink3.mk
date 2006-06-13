2cc316115ab90a871c5adf08f8a1905a13980053
tron <tron@pkgsrc.org>
2006-06-13 15:02:58 +0000

    Include buildlink3 files for "arts", "esound" and "nas" if support for these package has been compiled into the "SDL" package. Based on a patch provided by Thomas Klausner in private e-mail.

--- devel/SDL/buildlink3.mk
+++ devel/SDL/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2006/06/12 16:20:44 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2006/06/13 15:02:58 tron Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 SDL_BUILDLINK3_MK:=	${SDL_BUILDLINK3_MK}+
@@ -27,5 +27,25 @@ PTHREAD_OPTS+=	require
 .include "../../graphics/aalib/buildlink3.mk"
 
 .include "../../mk/pthread.buildlink3.mk"
+
+.if !defined(PKG_BUILD_OPTIONS.SDL)
+PKG_BUILD_OPTIONS.SDL!= \
+	cd ${BUILDLINK_PKGSRCDIR.SDL} && \
+	${MAKE} show-var ${MAKEFLAGS} VARNAME=PKG_OPTIONS
+MAKEFLAGS+=	PKG_BUILD_OPTIONS.SDL=${PKG_BUILD_OPTIONS.SDL:Q}
+.endif
+MAKEVARS+=	PKG_BUILD_OPTIONS.SDL
+
+.if !empty(PKG_BUILD_OPTIONS.SDL:Marts)
+.  include "../../audio/arts/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.SDL:Mesd)
+.  include "../../audio/esound/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.SDL:Mnas)
+.  include "../../audio/nas/buildlink3.mk"
+.endif
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
