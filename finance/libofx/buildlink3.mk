c1fb616fabb498f43c225e24617bc30f307021d9
rillig <rillig@pkgsrc.org>
2006-06-14 10:26:42 +0000

    Fixed pkglint warnings.

--- finance/libofx/buildlink3.mk
+++ finance/libofx/buildlink3.mk
@@ -1,7 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.3 2006/04/12 10:27:15 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2006/06/14 10:26:42 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
-LIBOFX_BUILDLINK3_MK:=	${OPENSP_BUILDLINK3_MK}+
+LIBOFX_BUILDLINK3_MK:=	${LIBOFX_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	libofx
@@ -13,9 +13,8 @@ BUILDLINK_PACKAGES+=	libofx
 .if !empty(LIBOFX_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.libofx+=	libofx>=0.8.0
 BUILDLINK_PKGSRCDIR.libofx?=	../../finance/libofx
+BUILDLINK_INCDIRS.libofx+=	include/libofx
 .endif	# LIBOFX_BUILDLINK3_MK
-
-BUILDLINK_INCDIRS.libofx+=    include/libofx
 
 .include "../../textproc/opensp/buildlink3.mk"
 .include "../../www/curl/buildlink3.mk"
