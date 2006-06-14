9793c5c6844f59b80d42ccff4ba881d494657f8c
adam <adam@pkgsrc.org>
2006-06-14 07:27:57 +0000

    Changes 2.14.0: * Increased version number for GNOME 2.14

--- sysutils/gnome-vfsmm/buildlink3.mk
+++ sysutils/gnome-vfsmm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/04/12 10:27:35 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2006/06/14 07:27:57 adam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_VFSMM_BUILDLINK3_MK:=	${GNOME_VFSMM_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	gnome-vfsmm
 
 .if !empty(GNOME_VFSMM_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.gnome-vfsmm+=	gnome-vfsmm>=2.10.0
-BUILDLINK_ABI_DEPENDS.gnome-vfsmm?=	gnome-vfsmm>=2.12.0nb3
+BUILDLINK_ABI_DEPENDS.gnome-vfsmm+=	gnome-vfsmm>=2.12.0nb3
 BUILDLINK_PKGSRCDIR.gnome-vfsmm?=	../../sysutils/gnome-vfsmm
 .endif	# GNOME_VFSMM_BUILDLINK3_MK
 
