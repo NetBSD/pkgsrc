ed65565c103ec3f04541ed9ff8f2b4f00ad1964f
adam <adam>
2006-06-15 09:45:05 +0000

    Make aalib an option, enabled by default

--- devel/SDL/options.mk
+++ devel/SDL/options.mk
@@ -1,10 +1,15 @@
-# $NetBSD: options.mk,v 1.1 2006/06/12 16:20:44 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2006/06/15 09:45:05 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.SDL
-PKG_SUPPORTED_OPTIONS=	arts esound nas
-PKG_SUGGESTED_OPTIONS=	arts esound nas
+PKG_SUPPORTED_OPTIONS=	aalib arts esound nas
+PKG_SUGGESTED_OPTIONS=	aalib arts esound nas
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Maalib)
+CONFIGURE_ARGS+=	--enable-video-aalib
+.include "../../graphics/aalib/buildlink3.mk"
+.endif
 
 .if !empty(PKG_OPTIONS:Marts)
 CONFIGURE_ARGS+=	--enable-arts
