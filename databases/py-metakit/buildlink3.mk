7647cf08ac92cbe15b6387b061ce5693bf21652a
rillig <rillig@pkgsrc.org>
2006-06-16 10:58:23 +0000

    Fixed pkglint warnings.

--- databases/py-metakit/buildlink3.mk
+++ databases/py-metakit/buildlink3.mk
@@ -1,9 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/04/12 10:27:05 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/06/16 10:58:23 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PY_METAKIT_BUILDLINK3_MK:=	${PY_METAKIT_BUILDLINK3_MK}+
-
-.include "../../lang/python/pyversion.mk"
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	pymetakit
@@ -13,8 +11,9 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npymetakit}
 BUILDLINK_PACKAGES+=	pymetakit
 
 .if !empty(PY_METAKIT_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.pymetakit+=	${PYPKGPREFIX}-metakit-*
-BUILDLINK_ABI_DEPENDS.pymetakit?=	${PYPKGPREFIX}-metakit>=2.4.9.3nb2
+.include "../../lang/python/pyversion.mk"
+BUILDLINK_API_DEPENDS.pymetakit+=	${PYPKGPREFIX}-metakit-[0-9]*
+BUILDLINK_ABI_DEPENDS.pymetakit+=	${PYPKGPREFIX}-metakit>=2.4.9.3nb2
 BUILDLINK_PKGSRCDIR.pymetakit?=	../../databases/py-metakit
 .endif	# PY_METAKIT_BUILDLINK3_MK
 
