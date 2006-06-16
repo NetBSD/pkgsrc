c99f32ebb0f6cf416ad64c18ffb3ca0b1d24ab1f
rillig <rillig@pkgsrc.org>
2006-06-16 09:12:35 +0000

    Fixed pkglint warnings, including some copy&paste errors.

--- databases/tcl-postgresql74/buildlink3.mk
+++ databases/tcl-postgresql74/buildlink3.mk
@@ -1,20 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/04/12 10:27:05 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/06/16 09:12:35 rillig Exp $
 
-BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-TCL_POSTGRESQL73_BUILDLINK3_MK:=	${TCL_POSTGRESQL73_BUILDLINK3_MK}+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+TCL_POSTGRESQL74_BUILDLINK3_MK:=	${TCL_POSTGRESQL74_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	tcl-postgresql73
+BUILDLINK_DEPENDS+=	tcl-postgresql74
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ntcl-postgresql73}
-BUILDLINK_PACKAGES+=	tcl-postgresql73
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ntcl-postgresql74}
+BUILDLINK_PACKAGES+=	tcl-postgresql74
 
-.if !empty(TCL_POSTGRESQL73_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.tcl-postgresql73+=	tcl-postgresql73>=7.3.6
-BUILDLINK_ABI_DEPENDS.tcl-postgresql73?=	tcl-postgresql74>=7.4.8nb1
-BUILDLINK_PKGSRCDIR.tcl-postgresql73?=	../../databases/tcl-postgresql73
-.endif	# TCL_POSTGRESQL73_BUILDLINK3_MK
+.if !empty(TCL_POSTGRESQL74_BUILDLINK3_MK:M+)
+BUILDLINK_API_DEPENDS.tcl-postgresql74+=	tcl-postgresql74>=7.3.6
+BUILDLINK_ABI_DEPENDS.tcl-postgresql74+=	tcl-postgresql74>=7.4.8nb1
+BUILDLINK_PKGSRCDIR.tcl-postgresql74?=	../../databases/tcl-postgresql74
+.endif	# TCL_POSTGRESQL74_BUILDLINK3_MK
 
 .include "../../lang/tcl/buildlink3.mk"
 
