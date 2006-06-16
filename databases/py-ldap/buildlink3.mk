9c8533ec5a5f2f8a2b4199b865c8e2193a9d2e9a
rillig <rillig@pkgsrc.org>
2006-06-16 10:58:23 +0000

    Fixed pkglint warnings.

--- databases/py-ldap/buildlink3.mk
+++ databases/py-ldap/buildlink3.mk
@@ -1,9 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/05/14 21:09:20 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2006/06/16 10:58:23 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PY_LDAP_BUILDLINK3_MK:=	${PY_LDAP_BUILDLINK3_MK}+
-
-.include "../../lang/python/pyversion.mk"
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	pyldap
@@ -13,8 +11,9 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npyldap}
 BUILDLINK_PACKAGES+=	pyldap
 
 .if !empty(PY_LDAP_BUILDLINK3_MK:M+)
+.include "../../lang/python/pyversion.mk"
 BUILDLINK_API_DEPENDS.pyldap+=	${PYPKGPREFIX}-ldap>=2.2.0
-BUILDLINK_ABI_DEPENDS.pyldap?=	${PYPKGPREFIX}-ldap>=2.2.0
+BUILDLINK_ABI_DEPENDS.pyldap+=	${PYPKGPREFIX}-ldap>=2.2.0
 BUILDLINK_PKGSRCDIR.pyldap?=	../../databases/py-ldap
 .endif	# PY_LDAP_BUILDLINK3_MK
 
