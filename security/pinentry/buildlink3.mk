1510037deed0e7546f207dfd26c60ae63aaeda77
rillig <rillig@pkgsrc.org>
2006-06-17 13:46:34 +0000

    Fixed the set of installed files when the "qt" or "gtk" options are selected. Fixed pkglint warnings. Bumped PKGREVISION.

--- security/pinentry/buildlink3.mk
+++ security/pinentry/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/04/12 10:27:34 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/06/17 13:46:34 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PINENTRY_BUILDLINK3_MK:=	${PINENTRY_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	pinentry
 
 .if !empty(PINENTRY_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.pinentry+=	pinentry>=0.7.1
-BUILDLINK_ABI_DEPENDS.pinentry?=	pinentry>=0.7.1nb3
+BUILDLINK_ABI_DEPENDS.pinentry+=	pinentry>=0.7.1nb3
 BUILDLINK_PKGSRCDIR.pinentry?=	../../security/pinentry
 .endif	# PINENTRY_BUILDLINK3_MK
 
