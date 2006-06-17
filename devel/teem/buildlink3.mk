3cd51c99f8cf6fd8a983388794ecd00a7b52ea2d
rillig <rillig@pkgsrc.org>
2006-06-17 21:05:00 +0000

    Fixed pkglint warnings. Removed unnecessary inclusion of bsd.prefs.mk.

--- devel/teem/buildlink3.mk
+++ devel/teem/buildlink3.mk
@@ -1,9 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/04/12 10:27:14 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/06/17 21:05:00 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 TEEM_BUILDLINK3_MK:=	${TEEM_BUILDLINK3_MK}+
-
-.include "../../mk/bsd.prefs.mk"
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	teem
@@ -14,7 +12,7 @@ BUILDLINK_PACKAGES+=	teem
 
 .if !empty(TEEM_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.teem+=		teem>=1.8.0
-BUILDLINK_ABI_DEPENDS.teem?=		teem>=1.8.0nb1
+BUILDLINK_ABI_DEPENDS.teem+=		teem>=1.8.0nb1
 BUILDLINK_PKGSRCDIR.teem?=		../../devel/teem
 
 BUILDLINK_FILES.teem+=	include/teem/*
@@ -23,23 +21,23 @@ BUILDLINK_FILES.teem+=	include/teemEndian.h include/teemPng.h
 BUILDLINK_FILES.teem+=	include/teemQnanhibit.h
 
 BUILDLINK_FILES.teem+=	lib/libair.*
-BUILDLINK_FILES.teem+=  lib/libalan.*
-BUILDLINK_FILES.teem+=  lib/libbane.*
-BUILDLINK_FILES.teem+=  lib/libbiff.*
-BUILDLINK_FILES.teem+=  lib/libcoil.*
-BUILDLINK_FILES.teem+=  lib/libdye.*
-BUILDLINK_FILES.teem+=  lib/libecho.*
-BUILDLINK_FILES.teem+=  lib/libell.*
-BUILDLINK_FILES.teem+=  lib/libgage.*
-BUILDLINK_FILES.teem+=  lib/libhest.*
-BUILDLINK_FILES.teem+=  lib/libhoover.*
-BUILDLINK_FILES.teem+=  lib/liblimn.*
-BUILDLINK_FILES.teem+=  lib/libmite.*
-BUILDLINK_FILES.teem+=  lib/libmoss.*
-BUILDLINK_FILES.teem+=  lib/libnrrd.*
-BUILDLINK_FILES.teem+=  lib/libteem.*
-BUILDLINK_FILES.teem+=  lib/libten.*
-BUILDLINK_FILES.teem+=  lib/libunrrdu.*
+BUILDLINK_FILES.teem+=	lib/libalan.*
+BUILDLINK_FILES.teem+=	lib/libbane.*
+BUILDLINK_FILES.teem+=	lib/libbiff.*
+BUILDLINK_FILES.teem+=	lib/libcoil.*
+BUILDLINK_FILES.teem+=	lib/libdye.*
+BUILDLINK_FILES.teem+=	lib/libecho.*
+BUILDLINK_FILES.teem+=	lib/libell.*
+BUILDLINK_FILES.teem+=	lib/libgage.*
+BUILDLINK_FILES.teem+=	lib/libhest.*
+BUILDLINK_FILES.teem+=	lib/libhoover.*
+BUILDLINK_FILES.teem+=	lib/liblimn.*
+BUILDLINK_FILES.teem+=	lib/libmite.*
+BUILDLINK_FILES.teem+=	lib/libmoss.*
+BUILDLINK_FILES.teem+=	lib/libnrrd.*
+BUILDLINK_FILES.teem+=	lib/libteem.*
+BUILDLINK_FILES.teem+=	lib/libten.*
+BUILDLINK_FILES.teem+=	lib/libunrrdu.*
 
 .endif	# TEEM_BUILDLINK3_MK
 
