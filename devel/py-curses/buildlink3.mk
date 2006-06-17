852f7be636fe031270c4ffb541bd0ad69739908f
rillig <rillig@pkgsrc.org>
2006-06-17 17:31:08 +0000

    Fixed pkglint warnings.

--- devel/py-curses/buildlink3.mk
+++ devel/py-curses/buildlink3.mk
@@ -1,9 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/04/12 10:27:13 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/06/17 17:31:08 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PY_CURSES_BUILDLINK3_MK:=	${PY_CURSES_BUILDLINK3_MK}+
-
-.include "../../lang/python/pyversion.mk"
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	pycurses
@@ -13,8 +11,9 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npycurses}
 BUILDLINK_PACKAGES+=	pycurses
 
 .if !empty(PY_CURSES_BUILDLINK3_MK:M+)
+.  include "../../lang/python/pyversion.mk"
 BUILDLINK_API_DEPENDS.pycurses+=	${PYPKGPREFIX}-curses-[0-9]*
-BUILDLINK_ABI_DEPENDS.pycurses?=	${PYPKGPREFIX}-curses>=0nb4
+BUILDLINK_ABI_DEPENDS.pycurses+=	${PYPKGPREFIX}-curses>=0nb4
 BUILDLINK_PKGSRCDIR.pycurses?=	../../devel/py-curses
 .endif	# PY_CURSES_BUILDLINK3_MK
 
