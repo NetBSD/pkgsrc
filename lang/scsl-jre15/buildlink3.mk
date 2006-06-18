7c75131bebe0f85f4fe59402f17c6e52202955a5
rillig <rillig@pkgsrc.org>
2006-06-18 08:45:56 +0000

    Fixed many pkglint warnings.

--- lang/scsl-jre15/buildlink3.mk
+++ lang/scsl-jre15/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/05/15 19:51:53 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2006/06/18 08:45:56 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SCSL_JRE15_BUILDLINK3_MK:=	${SCSL_JRE15_BUILDLINK3_MK}+
@@ -12,11 +12,11 @@ BUILDLINK_PACKAGES+=	scsl-jre15
 
 .if !empty(SCSL_JRE15_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.scsl-jre15+=		scsl-jre15-[0-9]*
-BUILDLINK_ABI_DEPENDS.scsl-jre15?=		scsl-jre15>=1.5.0
+BUILDLINK_ABI_DEPENDS.scsl-jre15+=		scsl-jre15>=1.5.0
 BUILDLINK_PKGSRCDIR.scsl-jre15?=		../../lang/scsl-jre15
 BUILDLINK_JAVA_PREFIX.scsl-jre15=	${PREFIX}/java/scsl-1.5
-.endif	# SCSL_JRE15_BUILDLINK3_MK
 
 UNLIMIT_RESOURCES+=	datasize	# Must be at least 131204
+.endif	# SCSL_JRE15_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
