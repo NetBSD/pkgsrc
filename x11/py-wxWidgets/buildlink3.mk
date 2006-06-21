37ed8a3d53f061a7049d4e39b3e924d8e83eccf2
joerg <joerg>
2006-06-21 21:19:08 +0000

    Fix expanded PYPREFIX. Since nothing could really work with the old version, don't bump revisions recursively.

--- x11/py-wxWidgets/buildlink3.mk
+++ x11/py-wxWidgets/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/04/17 13:46:04 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2006/06/21 21:19:08 joerg Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 PY_WXWIDGETS_BUILDLINK3_MK:=	${PY_WXWIDGETS_BUILDLINK3_MK}+
@@ -14,7 +14,7 @@ BUILDLINK_PACKAGES+=	${PYPKGPREFIX}-wxWidgets
 
 .if !empty(PY_WXWIDGETS_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.${PYPKGPREFIX}-wxWidgets+=	${PYPKGPREFIX}-wxWidgets>=2.6.1.0
-BUILDLINK_ABI_DEPENDS.${PYPKGPREFIX}-wxWidgets?=	py24-wxWidgets>=2.6.1.0nb4
+BUILDLINK_ABI_DEPENDS.${PYPKGPREFIX}-wxWidgets?=	${PYPKGPREFIX}-wxWidgets>=2.6.1.0nb4
 BUILDLINK_PKGSRCDIR.${PYPKGPREFIX}-wxWidgets?=	../../x11/py-wxWidgets
 
 .include "../../x11/wxGTK/buildlink3.mk"
