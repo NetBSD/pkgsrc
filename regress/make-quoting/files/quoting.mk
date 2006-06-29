68aa231d3aaadf5c7af2e7eeba021f36fb369434
rillig <rillig@pkgsrc.org>
2006-06-29 13:35:18 +0000

    Added a testcase for a bug in the :Q operator in bmake.

--- regress/make-quoting/files/quoting.mk
+++ regress/make-quoting/files/quoting.mk
@@ -1,7 +1,10 @@
-# $NetBSD: quoting.mk,v 1.1.1.1 2005/05/15 21:10:16 rillig Exp $
+# $NetBSD: quoting.mk,v 1.2 2006/06/29 13:35:18 rillig Exp $
 #
 # This file demonstrates various techniques for quoting variables when
 # passing them to the shell.
+#
+# The third line revealed a bug in the NetBSD 3.0 shell, which discards
+# the last argument under some circumstances when it is empty.
 #
 
 EVIL_STRING?=	"   "
