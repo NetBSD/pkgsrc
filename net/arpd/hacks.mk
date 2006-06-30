3e9c76a290f84b98bc49ba1f33ecea15a603c726
tron <tron@pkgsrc.org>
2006-06-30 15:33:39 +0000

    Just turn off "nonnull" warning instead of all warnings.

--- net/arpd/hacks.mk
+++ net/arpd/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2006/06/30 15:13:51 tron Exp $
+# $NetBSD: hacks.mk,v 1.2 2006/06/30 15:33:39 tron Exp $
 
 # GCC 4.1.x complains about a NULL argument to memcpy() because it gets
 # confused by the definition of the ar_tha() macro under NetBSD.
@@ -6,5 +6,9 @@
 
 .if ${OPSYS} == "NetBSD" && !empty(CC_VERSION:Mgcc-4.1.*)
 PKG_HACKS+=		gcc41-null-warning
-BUILDLINK_TRANSFORM+=	rm:-Wall
+
+SUBST_CLASSES+=         gcc41
+SUBST_STAGE.gcc41=	post-configure
+SUBST_FILES.gcc41=	Makefile
+SUBST_SED.gcc41=	-e "s/-Wall/-Wall -Wno-nonnull/"
 .endif
