804c4cf958984989fd68e822a5c01512bf35e987
rillig <rillig@pkgsrc.org>
2006-07-02 09:38:42 +0000

    Added a proper interface documentation. Instead of a .for loop, the run-time for loop is used, which allows for white-space in CHECK_INTERPRETER_SKIP.

--- mk/check/check-interpreter.mk
+++ mk/check/check-interpreter.mk
@@ -1,17 +1,32 @@
-# $NetBSD: check-interpreter.mk,v 1.7 2006/06/16 12:03:38 rillig Exp $
+# $NetBSD: check-interpreter.mk,v 1.8 2006/07/02 09:38:42 rillig Exp $
 
-CHECK_INTERPRETER?=	no
-
-###########################################################################
-# CHECK_INTERPRETER_SKIP is a list of shell globs.  Installed files that
-# match these globs are skipped when running the check-interpreter target.
+# This file checks that after installation, all files of the package
+# that start with a "#!" line will find their interpreter. Files that
+# have a "#!" line with a non-existent interpreter will generate an
+# error message if they are executable, and a warning message otherwise.
 #
+# The following variables may be set by the pkgsrc user in mk.conf:
+#
+# CHECK_INTERPRETER: YesNo (default: no)
+#	Whether this check should be enabled or not.
+#
+# The following variables may be set by a package:
+#
+# CHECK_INTERPRETER_SKIP: List of PathMask (default: empty)
+#	The list of files that are skipped when running the check.
+#	Additionally, all files in share/examples and share/doc are
+#	skipped as well.
+#
+
+CHECK_INTERPRETER?=		no
 CHECK_INTERPRETER_SKIP?=	# empty
 
+_CHECK_INTERP_SKIP=		share/doc
+_CHECK_INTERP_SKIP+=		share/examples
+_CHECK_INTERP_SKIP+=		${CHECK_INTERPRETER_SKIP}
+
 _CHECK_INTERP_SKIP_FILTER=	case $$file in
-.for _pattern_ in ${CHECK_INTERPRETER_SKIP}
-_CHECK_INTERP_SKIP_FILTER+=	${PREFIX}/${_pattern_}|${_pattern_}) continue ;;
-.endfor
+_CHECK_INTERP_SKIP_FILTER+=	${_CHECK_INTERP_SKIP:@.pattern.@${PREFIX}/${.pattern.}|${.pattern.}) continue ;;@}
 _CHECK_INTERP_SKIP_FILTER+=	*) ;;
 _CHECK_INTERP_SKIP_FILTER+=	esac
 
