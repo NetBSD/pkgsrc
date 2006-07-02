a742ed8dde54ec09487115ff654c6236f5e9a6ce
martti <martti@pkgsrc.org>
2006-07-02 17:05:14 +0000

    Updated editors/vim-share to 7.0.35
      1649  7.0.018  VMS: plugins are not loaded on startup
      1425  7.0.019  crash for "VjA789" and repeating
      1470  7.0.020  GUI: crash when using 'mousefocus'
      4240  7.0.021  crash when using "\\[" and "\\]" in 'errorformat
      5471  7.0.022  Ruby: buffer.append() could append to the wrong buffer
      5351  7.0.023  crash after Insert mode completion without matches
      1619  7.0.024  it is possible to set arbitrary v: variables
      2606  7.0.025  crash when deleting an item from a:000
      1800  7.0.026  Unix: when using libcall() and old error may be shown
      1494  7.0.029  cursor position may be wrong when using getchar()
      1664  7.0.030  the ":compiler" command can't be used in a FileChangedRO event
      1808  7.0.031  after deleting a buffer its Select mode mappings remain
      2431  7.0.033  pasting after autoindent removes the indent
      2042  7.0.034  repeating completion was wrong after typing text or using BS
      5905  7.0.035  repeating Insert mode completion doesn't work properly

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.9 2006/05/21 13:32:27 martti Exp $
+# $NetBSD: version.mk,v 1.10 2006/07/02 17:05:15 martti Exp $
 
 VIM_VERSION=		7.0
-VIM_PATCHLEVEL=		17
+VIM_PATCHLEVEL=		35
 VIM_SUBDIR=		vim70
