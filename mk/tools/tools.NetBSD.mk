bb74d3bb13a575c8daf48ec498241f861b6a695c
jlam <jlam@pkgsrc.org>
2006-07-05 04:32:10 +0000

    Teach the tools framework about the following new tools:
    	itools, intltool, diff3, sdiff, msgmerge
    
    * Adding USE_TOOLS+=itools to a package Makefile will cause the
      tool-directory versions of imake, makedepend, mkdirhier and xmkmf
      to point to the ones from the devel/nbitools package.
    
      This change will remove the need for nbitools/buildlink3.mk, which
      currently does a bit of hackery to force the "right" imake tools to
      be used by packages that need it.
    
    * Adding USE_TOOLS+=intltool to a package Makefile will cause the
      local versions of intltool-* inside ${WRKSRC} to be replaced by
      copies from the textproc/intltool package.  If "intltool" is not
      specified as a tool, then we create "broken" intltool-* tools in
      the tools directory to help highlight hidden dependencies on the
      intltool package.
    
      In addition, modify the tools framework so that if "perl" is not
      specified as a tool, then we create a "broken" perl tool in the
      tools directory for the same reason as for "intltool".
    
      These two changes together will remove the need for
      intltools/buildlink3.mk and should also catch all cases where the
      sources' intltools may have been silently used because perl was
      found on the system.
    
    * Adding USE_TOOLS+=diff3, USE_TOOLS+=sdiff, or USE_TOOLS+=msgmerge
      to a package Makefile will cause the corresponding tool to be pulled
      into the tools directory.
    
      These are convenience tools to help simplify dependencies for some
      packages.

--- mk/tools/tools.NetBSD.mk
+++ mk/tools/tools.NetBSD.mk
@@ -1,4 +1,4 @@
-# $NetBSD: tools.NetBSD.mk,v 1.32 2006/06/14 16:57:07 jlam Exp $
+# $NetBSD: tools.NetBSD.mk,v 1.33 2006/07/05 04:32:10 jlam Exp $
 #
 # System-supplied tools for the NetBSD operating system.
 
@@ -19,6 +19,7 @@ TOOLS_PLATFORM.csh?=		/bin/csh
 TOOLS_PLATFORM.cut?=		/usr/bin/cut
 TOOLS_PLATFORM.date?=		/bin/date
 TOOLS_PLATFORM.diff?=		/usr/bin/diff
+TOOLS_PLATFORM.diff3?=		/usr/bin/diff3
 TOOLS_PLATFORM.dirname?=	/usr/bin/dirname
 TOOLS_PLATFORM.echo?=		echo			# shell builtin
 TOOLS_PLATFORM.egrep?=		/usr/bin/egrep
@@ -74,6 +75,7 @@ TOOLS_PLATFORM.printf?=		/usr/bin/printf
 TOOLS_PLATFORM.pwd?=		/bin/pwd
 TOOLS_PLATFORM.rm?=		/bin/rm
 TOOLS_PLATFORM.rmdir?=		/bin/rmdir
+TOOLS_PLATFORM.sdiff?=		/usr/bin/sdiff
 TOOLS_PLATFORM.sed?=		/usr/bin/sed
 TOOLS_PLATFORM.sh?=		/bin/sh
 TOOLS_PLATFORM.shlock?=		/usr/bin/shlock
