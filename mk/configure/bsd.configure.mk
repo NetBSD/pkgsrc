2806bdc9ce811cee29fb5f0b4b64229181e0456c
jlam <jlam>
2006-07-05 06:09:15 +0000

    Refactor configure, build, test and wrapper phases out of bsd.pkg.mk and into their own directories.  Also do some cleanups with build/_build and pkginstall -- we get rid of _build and simply run pkginstall as part of the "build" target.
    Introduce a new mechanism to handle varying directory depths under
    ${WRKSRC} in which we find files to override, e.g. configure, config.*,
    libtool, etc.  OVERRIDE_DIRDEPTH is a package-settable variable that
    specifies how far under ${WRKSRC} the various targets should look,
    and it defaults to "2".  We preserve the
    meaning of the various *_OVERRIDE variables, so if they are defined,
    then their values supersede the OVERRIDE_DIRDEPTH mechanism.
    
    devel/tla will need to specially set OVERRIDE_DIRDEPTH to 3 (see log
    for revision 1.1857 for bsd.pkg.mk -- to be done in a separate commit.

---
+++ mk/configure/bsd.configure.mk
@@ -0,0 +1,39 @@
+# $NetBSD: bsd.configure.mk,v 1.1 2006/07/05 06:09:15 jlam Exp $
+#
+# This Makefile fragment is included by bsd.pkg.mk and provides all
+# variables and targets related to configuring packages for building.
+#
+# The following are the "public" targets provided by this module:
+#
+#    configure
+#
+# The following targets may be overridden in a package Makefile:
+#
+#    pre-configure, do-configure, post-configure
+#
+
+_CONFIGURE_COOKIE=	${WRKDIR}/.configure_done
+
+######################################################################
+### configure (PUBLIC)
+######################################################################
+### configure is a public target to configure the software for building.
+###
+.PHONY: configure
+.if defined(NO_CONFIGURE)
+.  if !target(configure)
+configure: patch configure-cookie
+.  endif
+.else
+.  include "${PKGSRCDIR}/mk/configure/configure.mk"
+.endif
+
+######################################################################
+### configure-cookie (PRIVATE)
+######################################################################
+### configure-cookie creates the "configure" cookie file.
+###
+.PHONY: configure-cookie
+configure-cookie:
+	${_PKG_SILENT}${_PKG_DEBUG}${MKDIR} ${_CONFIGURE_COOKIE:H}
+	${_PKG_SILENT}${_PKG_DEBUG}${ECHO} ${PKGNAME} >> ${_CONFIGURE_COOKIE}
