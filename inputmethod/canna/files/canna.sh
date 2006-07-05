468f00a444d5c92bbe1e5d6eff0791bd2e2ddb32
jlam <jlam>
2006-07-05 04:48:28 +0000

    * Convert to USE_TOOLS+=itools instead of including nbitools/buildlink3.mk.
    * Fully convert to the pkginstall framework by using it to manage
      directories required at install time.
    
    * Rewrite the canna rc.d script to ensure that all needed directories
      are present before the daemon is started.
    
    * Miscellaneous touch ups to the DESCR and MESSAGE files.
    
    * Remove the need to explicitly set PREFIX.  These packages now install
      into ${LOCALBASE} and use symlinks and file copies to put the files
      into the ${IMDICTDIR}.
    
    Bump the PKGREVISIONs for the canna* packages.

--- inputmethod/canna/files/canna.sh
+++ inputmethod/canna/files/canna.sh
@@ -1,25 +1,42 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: canna.sh,v 1.1 2003/07/29 19:42:53 jmmv Exp $
+# $NetBSD: canna.sh,v 1.2 2006/07/05 04:48:28 jlam Exp $
 #
 # PROVIDE: canna
 # REQUIRE: DAEMON
 
-if [ -f /etc/rc.subr ]
-then
-	. /etc/rc.subr
-fi
+. /etc/rc.subr
 
 name="canna"
 rcvar=$name
 command="@PREFIX@/sbin/cannaserver"
+start_precmd="canna_prestart"
 stop_cmd="@PREFIX@/sbin/cannakill"
 
-if [ -f /etc/rc.subr -a -d /etc/rc.d -a -f /etc/rc.d/DAEMON ]
-then
-	load_rc_config $name
-	run_rc_command "$1"
-else
-	@ECHO@ -n ' ${name}'
-	${command} ${canna_flags} ${command_args}
-fi
+mkdir_perms() {
+	dir="$1"; user="$2"; group="$3"; mode="$4"
+	@TEST@ -d $dir || @MKDIR@ $dir
+	@CHOWN@ $user $dir
+	@CHGRP@ $group $dir
+	@CHMOD@ $mode $dir
+}
+
+canna_prestart()
+{
+	mkdir_perms @CANNA_SPOOL@ @CANNA_USER@ @CANNA_GROUP@ 0755
+	mkdir_perms @IMDICTDIR@/canna @CANNA_USER@ @CANNA_GROUP@ 0775
+	mkdir_perms @IMDICTDIR@/canna/canna @CANNA_USER@ @CANNA_GROUP@ 0775
+
+	srcdictdir="@PREFIX@/libdata/canna"
+	dictdir="@IMDICTDIR@/canna/canna"
+	if @TEST@ -d $srcdictdir; then
+		( cd $srcdictdir; for f in *; do
+			@TEST@ "$f" != "*" || continue
+			@RM@ -f $dictdir/$f
+			@LN@ -fs $srcdictdir/$f $dictdir/$f
+		done )
+	fi
+}
+
+load_rc_config $name
+run_rc_command "$1"
