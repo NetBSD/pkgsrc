ca847a0a70775f84733fc4c1b62b856fd36e3c50
jlam <jlam@pkgsrc.org>
2006-07-05 06:09:15 +0000

    Refactor configure, build, test and wrapper phases out of bsd.pkg.mk and into their own directories.  Also do some cleanups with build/_build and pkginstall -- we get rid of _build and simply run pkginstall as part of the "build" target.
    Introduce a new mechanism to handle varying directory depths under
    ${WRKSRC} in which we find files to override, e.g. configure, config.*,
    libtool, etc.  OVERRIDE_DIRDEPTH is a package-settable variable that
    specifies how far under ${WRKSRC} the various targets should look,
    and it defaults to "2".  We preserve the
    meaning of the various *_OVERRIDE variables, so if they are defined,
    then their values supersede the OVERRIDE_DIRDEPTH mechanism.
    
    devel/tla will need to specially set OVERRIDE_DIRDEPTH to 3 (see log
    for revision 1.1857 for bsd.pkg.mk -- to be done in a separate commit.

---
+++ mk/build/bsd.build-vars.mk
@@ -0,0 +1,47 @@
+# $NetBSD: bsd.build-vars.mk,v 1.1 2006/07/05 06:09:15 jlam Exp $
+#
+# BUILD_DIRS is the list of directories in which to perform the build
+#	process.  If the directories are relative paths, then they
+#	are assumed to be relative to ${WRKSRC}.
+#
+# MAKE_PROGRAM is the path to the make executable that is run to
+#	process the source makefiles.  This is always overridden by
+#	the tools framework in pkgsrc/mk/tools/make.mk, but we provide
+#	a default here for documentation purposes.
+#
+# MAKE_ENV is the shell environment that is exported to the make
+#	process.
+#
+# MAKE_FLAGS is a list of arguments that is pass to the make process.
+#
+# MAKEFILE is the path to the makefile that is processed by the make
+#	executable.  If the path is relative, then it is assumed to
+#	be relative to each directory listed in BUILD_DIRS.
+#
+BUILD_DIRS?=	${CONFIGURE_DIRS}
+MAKE_PROGRAM?=	${MAKE}
+MAKE_ENV?=	# empty
+MAKE_FLAGS?=	# empty
+MAKEFILE?=	Makefile
+
+MAKE_ENV+=	${ALL_ENV}
+MAKE_ENV+=	${NO_EXPORT_CPP:D:UCPP=${CPP:Q}}
+MAKE_ENV+=	LINK_ALL_LIBGCC_HACK=${LINK_ALL_LIBGCC_HACK:Q}
+MAKE_ENV+=	LOCALBASE=${LOCALBASE:Q}
+MAKE_ENV+=	NO_WHOLE_ARCHIVE_FLAG=${NO_WHOLE_ARCHIVE_FLAG:Q}
+MAKE_ENV+=	WHOLE_ARCHIVE_FLAG=${WHOLE_ARCHIVE_FLAG:Q}
+MAKE_ENV+=	X11BASE=${X11BASE:Q}
+MAKE_ENV+=	X11PREFIX=${X11PREFIX:Q}
+MAKE_ENV+=	PKGMANDIR=${PKGMANDIR:Q}
+
+# Provide a consistent environment for packages using (Net)BSD-style
+# Makefiles.
+#
+MAKE_ENV+=	MAKECONF=${PKGMAKECONF:U/dev/null}
+MAKE_ENV+=	OBJECT_FMT=${OBJECT_FMT:Q}
+MAKE_ENV+=	${USETOOLS:DUSETOOLS=${USETOOLS:Q}}
+
+# Add these bits to the environment used when invoking the recursive make
+# processes for build-related phases.
+#
+BUILD_ENV+=	PATH=${PATH:Q}
---
+++ mk/configure/gnu-configure.mk
@@ -0,0 +1,100 @@
+# $NetBSD: gnu-configure.mk,v 1.1 2006/07/05 06:09:15 jlam Exp $
+
+HAS_CONFIGURE=			defined
+OVERRIDE_GNU_CONFIG_SCRIPTS=	defined
+
+CONFIG_SHELL?=	${SH}
+CONFIGURE_ENV+=	CONFIG_SHELL=${CONFIG_SHELL:Q}
+CONFIGURE_ENV+=	LIBS=${LIBS:M*:Q}
+CONFIGURE_ENV+=	install_sh=${INSTALL:Q}
+CONFIGURE_ENV+=	ac_given_INSTALL=${INSTALL:Q}\ -c\ -o\ ${BINOWN}\ -g\ ${BINGRP}
+
+.if (defined(USE_LIBTOOL) || !empty(PKGPATH:Mdevel/libtool-base)) && \
+    defined(_OPSYS_MAX_CMDLEN_CMD)
+CONFIGURE_ENV+=	lt_cv_sys_max_cmd_len=${_OPSYS_MAX_CMDLEN_CMD:sh}
+.endif
+
+GNU_CONFIGURE_PREFIX?=	${PREFIX}
+CONFIGURE_ARGS+=	--prefix=${GNU_CONFIGURE_PREFIX:Q}
+
+USE_GNU_CONFIGURE_HOST?=	yes
+.if !empty(USE_GNU_CONFIGURE_HOST:M[yY][eE][sS])
+CONFIGURE_ARGS+=	--host=${MACHINE_GNU_PLATFORM:Q}
+.endif
+
+# PKGINFODIR is the subdirectory of ${PREFIX} into which the info
+# files are installed unless the software was configured with an
+# installation prefix other than ${PREFIX}.
+#
+CONFIGURE_HAS_INFODIR?=	yes
+.if ${GNU_CONFIGURE_PREFIX} == ${PREFIX}
+GNU_CONFIGURE_INFODIR?=	${GNU_CONFIGURE_PREFIX}/${PKGINFODIR}
+.else
+GNU_CONFIGURE_INFODIR?=	${GNU_CONFIGURE_PREFIX}/info
+.endif
+.if defined(INFO_FILES) && !empty(CONFIGURE_HAS_INFODIR:M[yY][eE][sS])
+CONFIGURE_ARGS+=	--infodir=${GNU_CONFIGURE_INFODIR:Q}
+.endif
+
+# PKGMANDIR is the subdirectory of ${PREFIX} into which the man and
+# catman pages are installed unless the software was configured with
+# an installation prefix other than ${PREFIX}.
+#
+CONFIGURE_HAS_MANDIR?=	 yes
+.if ${GNU_CONFIGURE_PREFIX} == ${PREFIX}
+GNU_CONFIGURE_MANDIR?=	${GNU_CONFIGURE_PREFIX}/${PKGMANDIR}
+.else
+GNU_CONFIGURE_MANDIR?=	${GNU_CONFIGURE_PREFIX}/man
+.endif
+.if !empty(CONFIGURE_HAS_MANDIR:M[yY][eE][sS])
+CONFIGURE_ARGS+=	--mandir=${GNU_CONFIGURE_MANDIR:Q}
+.endif
+
+######################################################################
+### configure-scripts-override (PRIVATE)
+######################################################################
+### configure-scripts-override modifies the GNU configure scripts in
+### ${WRKSRC} so that the generated config.status scripts never do
+### anything on "--recheck".  This is important in pkgsrc because we
+### only ever want to run the configure checks during the configure
+### phase, and "recheck" is often run during the build and install
+### phases.
+###
+do-configure-pre-hook: configure-scripts-override
+
+_SCRIPT.configure-scripts-override=					\
+	${AWK} '/ *-recheck *\| *--recheck.*\)/ {			\
+			print;						\
+			print "	: Avoid regenerating within pkgsrc";	\
+			print "	exit 0";				\
+			next;						\
+		}							\
+		{ print }' $$file > $$file.override;			\
+	${CHMOD} +x $$file.override;					\
+	${MV} -f $$file.override $$file
+
+OVERRIDE_DIRDEPTH.configure?=	${OVERRIDE_DIRDEPTH}
+
+.PHONY: configure-scripts-override
+configure-scripts-override:
+	@${STEP_MSG} "Modifying GNU configure scripts to avoid --recheck"
+.if defined(CONFIGURE_SCRIPTS_OVERRIDE) && !empty(CONFIGURE_SCRIPTS_OVERRIDE)
+	@echo HERE
+	${_PKG_SILENT}${_PKG_DEBUG}set -e;				\
+	cd ${WRKSRC};							\
+	for file in ${CONFIGURE_SCRIPTS_OVERRIDE}; do			\
+		${TEST} -f "$$file" || continue;			\
+		${_SCRIPT.${.TARGET}};					\
+	done
+.else
+	${_PKG_SILENT}${_PKG_DEBUG}set -e;				\
+	cd ${WRKSRC};							\
+	depth=0; pattern=${CONFIGURE_SCRIPT:T};				\
+	while ${TEST} $$depth -le ${OVERRIDE_DIRDEPTH.configure}; do	\
+		for file in $$pattern; do				\
+			${TEST} -f "$$file" || continue;		\
+			${_SCRIPT.${.TARGET}};				\
+		done;							\
+		depth=`${EXPR} $$depth + 1`; pattern="*/$$pattern";	\
+	done
+.endif
