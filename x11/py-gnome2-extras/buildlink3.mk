34872e7b60decb9f870a6a425076771e53425458
rillig <rillig@pkgsrc.org>
2006-07-06 16:27:28 +0000

    Fixed pkglint warnings.

--- x11/py-gnome2-extras/buildlink3.mk
+++ x11/py-gnome2-extras/buildlink3.mk
@@ -1,22 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.9 2006/04/17 13:46:12 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2006/07/06 16:27:28 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-PY_GNOME2EX_BUILDLINK3_MK:=	${PY_GNOME2EX_BUILDLINK3_MK}+
+PYGNOME2EX_BUILDLINK3_MK:=	${PYGNOME2EX_BUILDLINK3_MK}+
 
-.include "../../lang/python/pyversion.mk"
-
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	pygnome2ex
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npygnome2ex}
 BUILDLINK_PACKAGES+=	pygnome2ex
 
-.if !empty(PY_GNOME2EX_BUILDLINK3_MK:M+)
+.if ${PYGNOME2EX_BUILDLINK3_MK} == "+"
+.include "../../lang/python/pyversion.mk"
 BUILDLINK_API_DEPENDS.pygnome2ex+=	${PYPKGPREFIX}-gnome2-extras>=2.12.0nb3
-BUILDLINK_ABI_DEPENDS.pygnome2ex?=	py24-gnome2-extras>=2.12.0nb7
+BUILDLINK_ABI_DEPENDS.pygnome2ex+=	${PYPKGPREFIX}-gnome2-extras>=2.12.0nb7
 BUILDLINK_PKGSRCDIR.pygnome2ex?=	../../x11/py-gnome2-extras
-.endif	# PY_GNOME2EX_BUILDLINK3_MK
+.endif	# PYGNOME2EX_BUILDLINK3_MK
 
 .include "../../x11/py-gnome2/buildlink3.mk"
 
