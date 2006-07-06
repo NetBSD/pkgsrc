5fbd7534bcf944ebf55e0a750aef901e9d894a4e
dillo <dillo@pkgsrc.org>
2006-07-06 08:37:27 +0000

    - Convert gnome option to use gnome2 libraries, via official patch.   Manually patch Makefile.in and configure to avoid dependency on autotools. - Explicitly disable esd when not requested, it is found and used   with gnome enabled otherwise.

--- emulators/vice/options.mk
+++ emulators/vice/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2006/02/17 19:26:20 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2006/07/06 08:37:27 dillo Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.vice
 PKG_SUPPORTED_OPTIONS=	esound ffmpeg lame gnome
@@ -7,6 +7,8 @@ PKG_SUPPORTED_OPTIONS=	esound ffmpeg lame gnome
 
 .if !empty(PKG_OPTIONS:Mesound)
 .include "../../audio/esound/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-esd
 .endif
 
 .if !empty(PKG_OPTIONS:Mffmpeg)
@@ -19,5 +21,8 @@ CONFIGURE_ARGS+=	--enable-ffmpeg
 
 .if !empty(PKG_OPTIONS:Mgnome)
 CONFIGURE_ARGS+=	--enable-gnomeui
-.include "../../x11/gnome-libs/buildlink3.mk"
+PATCHFILES=		vice-1.19-gnome2.diff.gz
+PATCH_SITES=		http://www.viceteam.org/online/
+PATCH_DIST_STRIP=	-p1
+.include "../../devel/libgnomeui/buildlink3.mk"
 .endif
