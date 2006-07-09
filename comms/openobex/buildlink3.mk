5f1b3920f0be6a289b0aee7353804728ef7f4464
xtraeme <xtraeme>
2006-07-09 09:27:52 +0000

    Update to 1.3:
    ver 1.3:
            Add support for setting the creation-ID.
            Updated constants to match IrOBEX 1.3.
            Replace netbuf implementation with databuffer.
            Fix segmentation fault when receiving invalid OBEX packets.
    
    ver 1.2:
            Add OBEX_EV_REQCHECK support.
            Add support for suspend after sending a header.
            Add support for empty headers for buggy OBEX servers.
            Fix memory leak in obex_object_send().
    
    ver 1.1:
            Fix list of exported functions.
            Fix duplicate string from basename() result.
            Fix wrong order of sanity checks.
            Fix memory leak in send_stream() function.
            ISO C99 says that inttypes.h includes stdint.h header.
            Add proper client side ABORT support.
            Add support for OBEX_SuspendRequest() and OBEX_ResumeRequest().
            Add USB transport support.
            Increase the allowed maximum MTU to 64kB-1.
            Disconnect when an ABORT fails.
            Make it possible to include headers from C++ source code.
            Advertise OBEX Protocol Version 1.0.
    
    pkgsrc:
    
    	* I've added patches to detect properly NetBSD bluetooth support
    	  and I have sent it to the openobex folks, they will be integrated
    	  soon.
    	* Take maintainership for now.
    	* Bump BUILDLINK_API_DEPENDS because the API has changed.
    
    *** PLEASE DO NOT TOUCH OBEXFTP, I'M UPDATING IT RIGHT NOW. THANKS ***

--- comms/openobex/buildlink3.mk
+++ comms/openobex/buildlink3.mk
@@ -1,20 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/07/08 23:10:38 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2006/07/09 09:27:52 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 OPENOBEX_BUILDLINK3_MK:=	${OPENOBEX_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	openobex
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nopenobex}
 BUILDLINK_PACKAGES+=	openobex
-BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}openobex
 
-.if !empty(OPENOBEX_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.openobex+=	openobex>=1.0.1
-BUILDLINK_ABI_DEPENDS.openobex+=	openobex>=1.0.1nb2
+.if ${OPENOBEX_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.openobex+=	openobex>=1.3
 BUILDLINK_PKGSRCDIR.openobex?=	../../comms/openobex
 .endif	# OPENOBEX_BUILDLINK3_MK
+
+.include "../../devel/libusb/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
