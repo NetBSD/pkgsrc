eb27d05acd6cdd303925959c632ab5de09456404
jlam <jlam@pkgsrc.org>
2006-07-10 22:17:58 +0000

    Back out revision 1.172 of mk/tools/replace.mk -- we never want to allow IMAKE to be set by anything other than the tools framework. Modify the IRIX files so that the native imake is listed as a built-in tool in the case where X11_TYPE is "native".  Also, move the include of tools/default.mk a bit lower in bsd.prefs.mk so that tools.${OPSYS}.mk files can use the value of X11_TYPE.  This should properly set and point IMAKE to the right binary on IRIX without destroying the configuration for platforms where IMAKE was not explicitly set, i.e. every non-IRIX platform.

--- mk/tools/tools.IRIX.mk
+++ mk/tools/tools.IRIX.mk
@@ -1,4 +1,4 @@
-# $NetBSD: tools.IRIX.mk,v 1.15 2005/11/04 20:02:01 jlam Exp $
+# $NetBSD: tools.IRIX.mk,v 1.16 2006/07/10 22:17:58 jlam Exp $
 #
 # System-supplied tools for the IRIX operating system.
 
@@ -37,6 +37,9 @@ TOOLS_PLATFORM.gzip?=		/usr/sbin/gzip -nf ${GZIP}
 TOOLS_PLATFORM.head?=		/usr/bsd/head
 TOOLS_PLATFORM.hostname?=	/usr/bsd/hostname
 TOOLS_PLATFORM.id?=		/usr/bin/id
+.if defined(X11_TYPE) && !empty(X11_TYPE:Mnative) && exists(/usr/bin/X11/imake)
+TOOLS_PLATFORM.imake?=		/usr/bin/X11/imake
+.endif
 TOOLS_PLATFORM.ln?=		/sbin/ln
 TOOLS_PLATFORM.ls?=		/sbin/ls
 TOOLS_PLATFORM.m4?=		/sbin/m4
