b12ec50a7a1a4240d219f00ac2dfa155eadc9729
wiz <wiz>
2006-07-12 13:07:19 +0000

    Reimport libtunepimp as libtunepimp0.4, in preparation for the libtunepimp update, for packages which still need the 0.4 API.

---
+++ audio/libtunepimp0.4/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/07/12 13:07:19 wiz Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBTUNEPIMP_BUILDLINK3_MK:=	${LIBTUNEPIMP_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	libtunepimp
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibtunepimp}
+BUILDLINK_PACKAGES+=	libtunepimp
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libtunepimp
+
+.if ${LIBTUNEPIMP_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.libtunepimp+=	libtunepimp>=0.4.2
+BUILDLINK_PKGSRCDIR.libtunepimp?=	../../audio/libtunepimp0.4
+.endif	# LIBTUNEPIMP_BUILDLINK3_MK
+
+.include "../../audio/musicbrainz/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
