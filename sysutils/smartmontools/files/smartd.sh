c868866b9e8d4f4050de29d69766162f31f279df
wiz <wiz@pkgsrc.org>
2006-07-17 17:01:32 +0000

    Update to 5.36, provided by Sergey Svishchev in PR 33958:
    Date 2006-04-12
    Summary: smartmontools release 5.36 (STABLE)
    -----------------------------------------------------------
    This is a stable smartmontools release.  The 5.34 version
    described just below was never officially released because
    Bruce Allen decided to wait until Linux support for
    accessing SATA devices through libata was in the official
    kernel.org sources.  Changes include:
    
    - Win 2000/XP:ability to cancel drive self-tests
    - Additions to the table of known drives
    - FreeBSD support for 3ware char device interface and
      multiple 3ware cards
    - Various cygwin improvements for running as service
    - Works 'out of the box' with Linux SATA libata
    - smartd option added to list test schedules
    - smartctl option added to list part of drive database
    - various improvements for SCSI disks and logs
    
    
    Date 2005-04-19
    Summary: smartmontools release 5.34 (STABLE)
    -----------------------------------------------------------
    This is a stable smartmontools release.  It includes:
    - OS/2 and eComStation support
    All Platforms:
     - Printing of drive family info
     - SCSI disks: output size of grown defect list
     - Added info about drive family to 'smartctl -i' output.
     - Added option ',q' for smartd '-n' directive to suppress
       'skipping checks' message which may spin up laptop disk.
     - Added message if smartd '-n' check power mode spins up disk.
    Cygwin and Windows:
     - Added info about Windows version and service pack to banner line.
     - Added support for smartd '-n' directive for Win2000/XP.
     - Added support for READ_LOG for WinNT4 also.
     - Fixed bug that prevents display of empty logs on Win2000/XP
     - Fixed use of cached smart enabled state in 'smartctl -i' output.
    Windows:
     - Fixed bug that prevents running smartd as service on WinNT4.
    
    
    Date 2004-9-5
    Summary: smartmontools release 5.33 (UNSTABLE/EXPERIMENTAL)
    -----------------------------------------------------------
    This is an unstable/experimental release of smartmontools.  It includes
     - support for Darwin/Mac OSX
     - support for OpenBSD
     - support for 3ware ATA RAID controllers under FreeBSD
     - support for 3ware 9500 series SATA RAID controllers under
       Linux.  Use /dev/twa[0-15] devices to access these.
     - support for 3ware character device interfaces /dev/twe[0-15]
       under Linux.  This allows (for example) Selective Self-tests.
     - support for Marvell chip-set based SATA controllers under Linux.
     - smartd mail now works also under Windows (using "Blat" mailer).
     - smartd can now be run as a Windows service.
    Please report sucess/failure with these items to the
    smartmontools-support mailing list.

--- sysutils/smartmontools/files/smartd.sh
+++ sysutils/smartmontools/files/smartd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: smartd.sh,v 1.3 2004/10/20 07:25:39 xtraeme Exp $
+# $NetBSD: smartd.sh,v 1.4 2006/07/17 17:01:32 wiz Exp $
 #
 # PROVIDE: smartd
 # REQUIRE: DAEMON
@@ -11,8 +11,11 @@ name="smartd"
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
 pidfile="@VARBASE@/run/${name}.pid"
-smartd_flags="-p $pidfile"
-extra_commands="reload"
+required_files="@PKG_SYSCONFDIR@/${name}.conf"
+command_args="-p $pidfile"
+extra_commands="reload check"
+check_precmd='[ "$rc_pid" ]'
+check_cmd='kill -USR1 $rc_pid'
 
 load_rc_config $name
 run_rc_command "$1"
