152f5ba10f3ecfd0489c657051d3c241a134adc5
rillig <rillig@pkgsrc.org>
2006-07-18 20:37:31 +0000

    Oops. Debugging messages should only appear when -D<anything> is set on the command line.

--- pkgtools/pkglint/files/pkglint.pl
+++ pkgtools/pkglint/files/pkglint.pl
@@ -1,5 +1,5 @@
 #! @PERL@
-# $NetBSD: pkglint.pl,v 1.655 2006/07/18 18:14:33 rillig Exp $
+# $NetBSD: pkglint.pl,v 1.656 2006/07/18 20:37:31 rillig Exp $
 #
 
 # pkglint - static analyzer and checker for pkgsrc packages
@@ -2699,7 +2699,7 @@ sub get_filetype($$) {
 		return "unknown";
 	}
 
-	$line->log_debug("Don't know the file type of ${fname}.");
+	$opt_debug_misc and $line->log_debug("Don't know the file type of ${fname}.");
 
 	return "unknown";
 }
