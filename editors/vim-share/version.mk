795f0fb3f2320d06be68be5bfa4ba414f26c52b6
martti <martti>
2006-07-18 10:27:16 +0000

    * Make sure PATCHDIR is always set so that the patches are really used.   This fixes the vim-motif build.
    * Update to 7.0.39
    
      3990  7.0.036  can't compile with small features and syntax HL
      1880  7.0.037  crash when resizing Vim window when a line doesn't fit
      3236  7.0.038  complete() can be used from expr. mapping after inserting text
      3240  7.0.039  third argument for inputdialog() doesn't work in the console

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.10 2006/07/02 17:05:15 martti Exp $
+# $NetBSD: version.mk,v 1.11 2006/07/18 10:27:16 martti Exp $
 
 VIM_VERSION=		7.0
-VIM_PATCHLEVEL=		35
+VIM_PATCHLEVEL=		39
 VIM_SUBDIR=		vim70
