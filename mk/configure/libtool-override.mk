3901a92d052bc4c9d09a25bf4d9a032b8ed63993
jlam <jlam@pkgsrc.org>
2006-07-19 19:14:37 +0000

    Drop support for LTCONFIG_OVERRIDE.  For quite a long time, pkgsrc had actually been ignoring LTCONFIG_OVERRIDE anyway and just using the default LIBTOOL_OVERRIDE to replace libtool scripts in packages. This just formalizes the fact that LTCONFIG_OVERRIDE is not used meaningfully by pkgsrc.

--- mk/configure/libtool-override.mk
+++ mk/configure/libtool-override.mk
@@ -1,50 +1,18 @@
-# $NetBSD: libtool-override.mk,v 1.4 2006/07/07 15:25:05 jlam Exp $
+# $NetBSD: libtool-override.mk,v 1.5 2006/07/19 19:14:39 jlam Exp $
 
 ######################################################################
-### {ltconfig,libtool,shlibtool}-override (PRIVATE)
+### {libtool,shlibtool}-override (PRIVATE)
 ######################################################################
-### {ltconfig,libtool,shlibtool}-override replace any existing ltconfig
-### and libtool under ${WRKSRC} with the versions installed by pkgsrc.
+### {libtool,shlibtool}-override replace any existing libtool under
+### ${WRKSRC} with the version installed by pkgsrc.
 ###
-.if defined(LTCONFIG_OVERRIDE)
-do-configure-pre-hook: ltconfig-override
-.endif
 do-configure-post-hook: libtool-override
 .if defined(SHLIBTOOL_OVERRIDE)
 do-configure-post-hook: shlibtool-override
 .endif
 
-OVERRIDE_DIRDEPTH.ltconfig?=	${OVERRIDE_DIRDEPTH}
 OVERRIDE_DIRDEPTH.libtool?=	${OVERRIDE_DIRDEPTH}
 OVERRIDE_DIRDEPTH.shlibtool?=	${OVERRIDE_DIRDEPTH}
-
-_SCRIPT.ltconfig-override=						\
-	${RM} -f $$file;						\
-	${ECHO} "${RM} -f libtool; ${LN} -s ${_LIBTOOL} libtool" > $$file; \
-	${CHMOD} +x $$file
-
-.PHONY: ltconfig-override
-ltconfig-override:
-	@${STEP_MSG} "Modifying ltconfig scripts to use pkgsrc libtool"
-.if defined(LTCONFIG_OVERRIDE) && !empty(LTCONFIG_OVERRIDE)
-	${_PKG_SILENT}${_PKG_DEBUG}set -e;				\
-	cd ${WRKSRC};							\
-	for file in ${LTCONFIG_OVERRIDE}; do				\
-		${TEST} -f "$$file" || continue;			\
-		${_SCRIPT.${.TARGET}};					\
-	done
-.else
-	${_PKG_SILENT}${_PKG_DEBUG}set -e;				\
-	cd ${WRKSRC};							\
-	depth=0; pattern=ltconfig;					\
-	while ${TEST} $$depth -le ${OVERRIDE_DIRDEPTH.ltconfig}; do	\
-		for file in $$pattern; do				\
-			${TEST} -f "$$file" || continue;		\
-			${_SCRIPT.${.TARGET}};				\
-		done;							\
-		depth=`${EXPR} $$depth + 1`; pattern="*/$$pattern";	\
-	done
-.endif
 
 _OVERRIDE_PATH.libtool=		${_LIBTOOL}
 _OVERRIDE_PATH.shlibtool=	${_SHLIBTOOL}
