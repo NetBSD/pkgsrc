a047553289e78c6e23be60779115b249ffff8eff
jlam <jlam@pkgsrc.org>
2006-07-20 20:02:23 +0000

    Remove USE_GNU_GETTEXT from pkgsrc.  Packages generally only needed it for plurals support, but that is already handled correctly (FSVO "correctly") by the pkgsrc/mk/tools/msgfmt.sh script.
    Also remove _USE_GNU_GETTEXT definitions from pkgsrc/mk/platform/*.mk
    files as that value has been unused by pkgsrc for quite some time
    (going back several branches).

--- mk/platform/DragonFly.mk
+++ mk/platform/DragonFly.mk
@@ -1,4 +1,4 @@
-# $NetBSD: DragonFly.mk,v 1.27 2006/04/12 21:01:15 joerg Exp $
+# $NetBSD: DragonFly.mk,v 1.28 2006/07/20 20:02:23 jlam Exp $
 #
 # Variable definitions for the DragonFly operating system.
 
@@ -64,7 +64,6 @@ _OPSYS_PTHREAD_AUTO=	no	# -lpthread needed for pthreads
 _OPSYS_SHLIB_TYPE=	ELF	# shared lib type
 _PATCH_CAN_BACKUP=	yes	# native patch(1) can make backups
 _PATCH_BACKUP_ARG?=	-V simple -z 	# switch to patch(1) for backup suffix
-_USE_GNU_GETTEXT=	no	# Don't use GNU gettext
 _USE_RPATH=		yes	# add rpath to LDFLAGS
 
 # flags passed to the linker to extract all symbols from static archives.
