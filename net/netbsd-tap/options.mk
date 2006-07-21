f2623b1d191f91e5cfafbc7ecdc2c92cac20ea94
jlam <jlam@pkgsrc.org>
2006-07-21 14:27:56 +0000

    "BUILD_ENV" was misnamed.  It's actually the environment that is passed to all internal recursive make processes.  As such, rename it to "PKGSRC_MAKE_ENV".
    XXX Note, some of the usage of this variable in package Makefiles seems
    XXX incorrect.  They probably want "MAKE_ENV", which is the environment
    XXX passed to the make process when running "make" within ${WRKSRC}.

--- net/netbsd-tap/options.mk
+++ net/netbsd-tap/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2005/05/31 10:01:39 dillo Exp $
+# $NetBSD: options.mk,v 1.4 2006/07/21 14:27:56 jlam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.netbsd-tap
 PKG_SUPPORTED_OPTIONS=	bpf
@@ -7,7 +7,7 @@ PKG_SUGGESTED_OPTIONS=	bpf
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mbpf)
-BUILD_ENV+=	USE_BPF=YES
+PKGSRC_MAKE_ENV+=	USE_BPF=YES
 .endif
 
 TAP_KERNEL_ENV?=	# empty
