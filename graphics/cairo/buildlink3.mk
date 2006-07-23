5ea0cb8a3a7311bde87542308d9426bbe8544d85
minskim <minskim@pkgsrc.org>
2006-07-23 04:07:34 +0000

    Support the x11 option to enable/disable xlib and xlib-xrender backends.

--- graphics/cairo/buildlink3.mk
+++ graphics/cairo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2006/07/23 02:55:59 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2006/07/23 04:07:34 minskim Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CAIRO_BUILDLINK3_MK:=	${CAIRO_BUILDLINK3_MK}+
@@ -19,10 +19,23 @@ BUILDLINK_PKGSRCDIR.cairo?=	../../graphics/cairo
 BUILDLINK_API_DEPENDS.Xrender+=	Xrender>=0.8
 .endif	# CAIRO_BUILDLINK3_MK
 
+.include "../../mk/bsd.prefs.mk"
+
+.if !defined(PKG_BUILD_OPTIONS.cairo)
+PKG_BUILD_OPTIONS.cairo!= \
+	cd ${BUILDLINK_PKGSRCDIR.cairo} && \
+	${MAKE} show-var ${MAKEFLAGS} VARNAME=PKG_OPTIONS
+MAKEFLAGS+=	PKG_BUILD_OPTIONS.cairo=${PKG_BUILD_OPTIONS.cairo:Q}
+.endif
+MAKEVARS+=	PKG_BUILD_OPTIONS.cairo
+
+.if !empty(PKG_BUILD_OPTIONS.cairo:Mx11)
+.include "../../x11/Xrender/buildlink3.mk"
+.endif
+
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../graphics/glitz/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
-.include "../../x11/Xrender/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
