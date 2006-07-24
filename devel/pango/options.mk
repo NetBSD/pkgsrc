fb48fd96dbafacae730ae25fc7738f631e925f9e
minskim <minskim@pkgsrc.org>
2006-07-24 23:20:50 +0000

    Build the atsui module only when the x11 option is disabled, because it causes the xlib backend of cairo to crash.  Bump PKGREVISION.

--- devel/pango/options.mk
+++ devel/pango/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2006/07/23 04:59:34 minskim Exp $
+# $NetBSD: options.mk,v 1.2 2006/07/24 23:20:50 minskim Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pango
 PKG_SUPPORTED_OPTIONS=	x11
@@ -10,6 +10,10 @@ PKG_SUGGESTED_OPTIONS=	x11
 ### X11 support
 ###
 .if !empty(PKG_OPTIONS:Mx11)
+# Force not to build the atsui module, because it causes the xlib backend
+# of cairo to crash.
+CONFIGURE_ENV+=	ac_cv_lib_cairo_cairo_atsui_font_face_create_for_atsu_font_id=no
+PLIST_SUBST+=		CARBON="@comment "
 PLIST_SUBST+=		X11=""
 BUILDLINK_API_DEPENDS.Xrender+=	Xrender>=0.8
 .include "../../fonts/Xft2/buildlink3.mk"
@@ -18,4 +22,9 @@ BUILDLINK_API_DEPENDS.Xrender+=	Xrender>=0.8
 .else
 CONFIGURE_ARGS+=	--without-x
 PLIST_SUBST+=		X11="@comment "
+.if exists(/System/Library/Frameworks/Carbon.framework)
+PLIST_SUBST+=		CARBON=
+.else
+PLIST_SUBST+=		CARBON="@comment "
+.endif
 .endif
