25f07f49405eeb2dbf10982143b2903f748e32a7
wiz <wiz@pkgsrc.org>
2006-07-24 16:49:12 +0000

    Add groff-docs option, enabled by default.
    When turning it off, you can get rid of the netpbm, psutils,
    and ghostscript dependencies. Might help in bootstrapping some
    platforms. Based on a patch from schmonz@.
    
    While here, improve PLIST by trying to remove an additional directory
    and bump PKGREVISION for that.

--- textproc/groff/options.mk
+++ textproc/groff/options.mk
@@ -1,15 +1,22 @@
-# $NetBSD: options.mk,v 1.2 2005/12/31 16:28:40 ben Exp $
+# $NetBSD: options.mk,v 1.3 2006/07/24 16:49:12 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.groff
-PKG_SUPPORTED_OPTIONS=	x11
-PKG_SUGGESTED_OPTIONS=	x11
+PKG_SUPPORTED_OPTIONS=	groff-docs x11
+PKG_SUGGESTED_OPTIONS=	groff-docs x11
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgroff-docs)
+DEPENDS+=		netpbm>=10.0:../../graphics/netpbm
+DEPENDS+=		psutils>=1.17:../../print/psutils
+USE_TOOLS+=		gs:run
+PLIST_SRC+=		PLIST.docs
+.endif
 
 .if !empty(PKG_OPTIONS:Mx11)
 CONFIGURE_ARGS+=	--with-x
 CONFIGURE_ARGS+=	--with-appresdir=${PREFIX}/lib/X11/app-defaults
-PLIST_SRC=		PLIST.x11 PLIST
+PLIST_SRC+=		PLIST.x11
 .include "../../mk/xaw.buildlink3.mk"
 .include "../../mk/x11.buildlink3.mk"
 .include "../../mk/bsd.prefs.mk"
