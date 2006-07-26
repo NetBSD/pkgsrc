c008f090387409c6b5c8f513a7bba341767863b3
jlam <jlam@pkgsrc.org>
2006-07-26 20:00:27 +0000

    * Split the MESSAGE file into two parts, one of which only shows up   if "ssl" is a package option.
    * Stop the abuse of BUILD_TARGET and use MAKE_FLAGS instead.  Also,
      use OPSYSVARS to simplify the specification of the correct BUILD_TARGET
      for each platform.
    
    * Make use of the EXTRASPECIALS variable used by imap makefiles to pass
      special MAKE_FLAGS settings through to all recursive make processes.
      This gets rid of some MAKE_ENV statements.
    
    * Split off the special alpha-codegen hack into a hacks.mk file.
    
    * Do man page fixups at post-build time, not post-extract time.  This
      leaves the files pristine for possible patching.
    
    * Add back the special handling if IMAP_UW_MAILSPOOLHOME is defined.
      It was accidentally removed in patch-am when the whoson modifications
      were added.  Move the modifications to the configure phase instead
      of post-patch so that the modifications aren't accidentally picked
      up by mkpatches.
    
    * Instead of listing each Makefile that needs the sed modification
      s/c-client.a/libc-client.la/ and modifying them at post-extract
      time, simply create patches for them.
    
    * Instead of listing each header file to be installed, just derive
      the list from the PLIST.
    
    * Make the libtoolification a bit more transparent by patching libtool
      references directly into the imap makefiles.
    
    * Drop the -limapuw -> -lc-client buildlink transform that was only
      needed for much older versions of the imap-uw package, and stop
      installing libimapuw.*.  All dependents of imap-uw already correctly
      use -lc-client.
    
    * Fix the handling of the kerberos package option so that we can use
      the pkgsrc Kerberos 5 packages instead of only using the native
      ones.
    
    * Properly document the options.mk file.
    
    Bump the PKGREVISION for the libimapuw.* changes and for the
    IMAP_UW_MAILSPOOLHOME fixes.  The rest of the changes are all
    pkgsrc-related and don't really affect the binary package.

--- mail/imap-uw/options.mk
+++ mail/imap-uw/options.mk
@@ -1,44 +1,74 @@
-# $NetBSD: options.mk,v 1.2 2006/01/01 18:53:03 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2006/07/26 20:00:27 jlam Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
-# Kerberos is explicitly not enabled on NetBSD 1.x due to interoperability
-# issues with later versions of OpenSSL (e.g. 0.9.7)
-.if empty(MACHINE_PLATFORM:MNetBSD-1.*)
-PKG_SUPPORTED_OPTIONS+=		kerberos
-.endif
-
 PKG_OPTIONS_VAR=	PKG_OPTIONS.imap-uw
-PKG_SUPPORTED_OPTIONS+=	inet6 ssl imapuw-cleartextpwd imapuw-whoson
-PKG_SUGGESTED_OPTIONS+=	ssl imapuw-cleartextpwd
+PKG_SUPPORTED_OPTIONS+=	imapuw-cleartextpwd imapuw-whoson inet6 ssl
+PKG_SUGGESTED_OPTIONS+=	imapuw-cleartextpwd ssl
+
+# On NetBSD 1.x, using the native Kerberos 5 implementation causes
+# interoperability problems with OpenSSL 0.9.7 and above.
+#
+.if !empty(MACHINE_PLATFORM:MNetBSD-1.*)
+CHECK_BUILTIN.heimdal:=	yes
+.  include "../../security/heimdal/builtin.mk"
+CHECK_BUILTIN.heimdal:=	no
+.  if !empty(USE_BUILTIN.heimdal:M[nN][oO])
+PKG_SUPPORTED_OPTIONS+=	kerberos
+.  endif
+.else
+PKG_SUPPORTED_OPTIONS+=	kerberos
+.endif
 
 .include "../../mk/bsd.options.mk"
 
-.if empty(PKG_OPTIONS:Minet6)
-MAKE_ENV+=	IP=4
+###
+### Support both IPv6 and IPv4 connections.
+###
+.if !empty(PKG_OPTIONS:Minet6)
+MAKE_FLAGS+=	IP=6
 .else
-MAKE_ENV+=	IP=6
+MAKE_FLAGS+=	IP=4
 .endif
 
+###
+### Support GSSAPI authentication via Kerberos 5.
+###
 .if !empty(PKG_OPTIONS:Mkerberos)
-PKG_USE_KERBEROS=	yes
-
-BUILD_TARGET+=	EXTRAAUTHENTICATORS="gss"
+.  include "../../mk/krb5.buildlink3.mk"
+MAKE_ENV+=	KRB5_TYPE=${KRB5_TYPE}
+MAKE_FLAGS+=	EXTRAAUTHENTICATORS=gss
+EXTRASPECIALS+=	GSSDIR=${KRB5BASE}
+CFLAGS.heimdal=	-DHEIMDAL_KRB5
+CFLAGS+=	${CFLAGS.${KRB5_TYPE}}
 .endif
 
-.if !empty(PKG_OPTIONS:Mssl)
-.include "../../security/openssl/buildlink3.mk"
-.endif
-
+###
+### Support using WHOSON for authentication.
+###
 .if !empty(PKG_OPTIONS:Mimapuw-whoson)
+.  include "../../net/whoson/buildlink3.mk"
 LDFLAGS+=	-lwhoson
 CFLAGS+=	-DUSE_WHOSON
-.include "../../net/whoson/buildlink3.mk"
 .endif
 
-.if empty(PKG_OPTIONS:Mimapuw-cleartextpwd)
-BUILD_TARGET+=	SSLTYPE="nopwd"
-.elif !empty(PKG_OPTIONS:Mssl)
-BUILD_TARGET+=	SPECIALAUTHENTICATORS="ssl"
-BUILD_TARGET+=	SSLTYPE="unix"
+###
+### Support SSL/TLS connections.
+###
+.if !empty(PKG_OPTIONS:Mssl)
+.  include "../../security/openssl/buildlink3.mk"
+.  if !empty(PKG_OPTIONS:Mimapuw-cleartextpwd)
+MAKE_FLAGS+=	SSLTYPE=unix	# plaintext auth
+.  else
+MAKE_FLAGS+=	SSLTYPE=nopwd	# plaintext auth only over SSL/TLS
+.  endif
+MESSAGE_SRC+=	${PKGDIR}/MESSAGE.ssl
+MESSAGE_SUBST+=	SSLCERTS=${SSLCERTS:Q}
+MESSAGE_SUBST+=	SSLKEYS=${SSLKEYS:Q}
+
+EXTRASPECIALS+=	SSLDIR=${SSLBASE:Q}
+EXTRASPECIALS+=	SSLCERTS=${SSLCERTS:Q}
+EXTRASPECIALS+=	SSLKEYS=${SSLKEYS:Q}
+.else
+MAKE_FLAGS+=	SSLTYPE=none	# no SSL/TLS
 .endif
