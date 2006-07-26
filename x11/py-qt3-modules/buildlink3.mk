628bb4c7b10d102720aaad2b09cb0626f91327cd
wiz <wiz@pkgsrc.org>
2006-07-26 17:56:25 +0000

    Fix BUILDLINK_ABI_DEPENDS to use PYPKGPREFIX.

--- x11/py-qt3-modules/buildlink3.mk
+++ x11/py-qt3-modules/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/07/08 23:11:15 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2006/07/26 17:56:25 wiz Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 PYQT3_MOD_BUILDLINK3_MK:=	${PYQT3_MOD_BUILDLINK3_MK}+
@@ -15,7 +15,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}pyqt3-mod
 
 .if !empty(PYQT3_MOD_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.pyqt3-mod+=	${PYPKGPREFIX}-qt3-modules>=3.11
-BUILDLINK_ABI_DEPENDS.pyqt3-mod?=	py24-qt3-modules>=3.15.1nb3
+BUILDLINK_ABI_DEPENDS.pyqt3-mod?=	${PYPKGPREFIX}-qt3-modules>=3.15.1nb3
 BUILDLINK_PKGSRCDIR.pyqt3-mod?=	../../x11/py-qt3-modules
 
 BUILDLINK_LIBDIRS.pyqt3-mod+=	${PYSITELIB}
