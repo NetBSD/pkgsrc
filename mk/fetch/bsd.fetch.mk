28ebc1cbc49cf2d6c094bd61de667fe90bf59f48
jlam <jlam@pkgsrc.org>
2006-07-27 17:28:38 +0000

    Note that "fetch-list" is a public target.

--- mk/fetch/bsd.fetch.mk
+++ mk/fetch/bsd.fetch.mk
@@ -1,11 +1,11 @@
-# $NetBSD: bsd.fetch.mk,v 1.3 2006/07/18 22:41:06 jlam Exp $
+# $NetBSD: bsd.fetch.mk,v 1.4 2006/07/27 17:28:38 jlam Exp $
 #
 # This Makefile fragment is included by bsd.pkg.mk and defines the
 # relevant variables and targets for the "fetch" step.
 #
 # The following are the "public" targets provided by this module:
 #
-#    fetch
+#    fetch, fetch-list
 #
 # The following targets may be overridden in a package Makefile:
 #
