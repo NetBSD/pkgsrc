0f045ba158f7603ecf97ca673bf3a884b48bf046
salo <salo>
2006-07-30 19:21:26 +0000

    Pullup ticket 1761 - requested by wiz security update for apache
    Revisions pulled up:
    - pkgsrc/www/apache/Makefile			1.186, 1.187
    - pkgsrc/www/apache/buildlink3.mk		1.16
    - pkgsrc/www/apache/distinfo			1.52
    - pkgsrc/www/apache/module.mk			1.11
    - pkgsrc/www/apache/patches/patch-ap		removed
    
       Module Name:		pkgsrc
       Committed By:	rillig
       Date:		Sun Jul  2 10:43:19 UTC 2006
    
       Modified Files:
       	pkgsrc/www/apache: Makefile buildlink3.mk module.mk
    
       Log Message:
       Fixed some easy pkglint warnings.
    ---
       Module Name:		pkgsrc
       Committed By:	wiz
       Date:		Wed Jul 19 22:45:14 UTC 2006
    
       Modified Files:
       	pkgsrc/www/apache: Makefile distinfo
       Removed Files:
       	pkgsrc/www/apache/patches: patch-ap
    
       Log Message:
       Update to 1.3.36:
    
       Changes with Apache 1.3.36
    
         *) Reverted SVN rev #396294 due to unwanted regression.
            The new feature introduced in 1.3.35 (Allow usage of the
            "Include" configuration directive within previously "Include"d
            files) has been removed in the meantime.
            (http://svn.apache.org/viewcvs?rev=396294&viewàev)
    
       Changes with Apache 1.3.35
    
         *) SECURITY: CVE-2005-3352 (cve.mitre.org)
            mod_imap: Escape untrusted referer header before outputting in HTML
            to avoid potential cross-site scripting.  Change also made to
            ap_escape_html so we escape quotes.  Reported by JPCERT.
            [Mark Cox]
    
         *) core: Allow usage of the "Include" configuration directive within
            previously "Include"d files. [Colm MacCarthaigh]
    
         *) HTML-escape the Expect error message.  Not classed as security as
            an attacker has no way to influence the Expect header a victim will
            send to a target site.  Reported by Thiago Zaninotti [Mark Cox]
    
         *) mod_cgi: Remove block on OPTIONS method so that scripts can
            respond to OPTIONS directly rather than via server default.
            [Roy Fielding] PR 15242

--- www/apache/buildlink3.mk
+++ www/apache/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2006/06/04 12:21:24 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.15.2.1 2006/07/30 19:21:26 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 APACHE_BUILDLINK3_MK:=	${APACHE_BUILDLINK3_MK}+
@@ -15,18 +15,18 @@ BUILDLINK_API_DEPENDS.apache+=	apache-1.3.*
 BUILDLINK_ABI_DEPENDS.apache?=	apache>=1.3.34nb5
 BUILDLINK_PKGSRCDIR.apache?=	../../www/apache
 
-.if defined(APACHE_MODULE)
+.  if defined(APACHE_MODULE)
 BUILDLINK_DEPMETHOD.apache+=	full
-.else
+.  else
 BUILDLINK_DEPMETHOD.apache?=	build
-.endif
+.  endif
 
 USE_TOOLS+=	perl	# for "apxs"
 APXS?=		${BUILDLINK_PREFIX.apache}/sbin/apxs
 
-.if defined(GNU_CONFIGURE)
+.  if defined(GNU_CONFIGURE)
 CONFIGURE_ARGS+=	--with-apxs=${APXS:Q}
-.endif
+.  endif
 
 # required because httpd/os.h includes <dlfcn.h> when it is found by
 # Apache's configure script.
