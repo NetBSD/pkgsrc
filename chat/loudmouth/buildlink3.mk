374cad7bd75c0a9797a6b79c8028ed2399d8fb7d
adam <adam@pkgsrc.org>
2006-07-31 09:11:46 +0000

    Changes 1.0.4: * Fixed issue where no disconnect signal was emitted when connection closed   during opening * Plugged a memory leak in LmConnection * Fixed warnings due to uninitialized value * Fixed a reentrancy-related issue with LmConnection

--- chat/loudmouth/buildlink3.mk
+++ chat/loudmouth/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2006/07/08 23:10:38 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2006/07/31 09:11:46 adam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LOUDMOUTH_BUILDLINK3_MK:=	${LOUDMOUTH_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}loudmouth
 
 .if !empty(LOUDMOUTH_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.loudmouth+=	loudmouth>=1.0
-BUILDLINK_ABI_DEPENDS.loudmouth?=	loudmouth>=1.0.1nb2
+BUILDLINK_ABI_DEPENDS.loudmouth+=	loudmouth>=1.0.1nb2
 BUILDLINK_PKGSRCDIR.loudmouth?=	../../chat/loudmouth
 .endif	# LOUDMOUTH_BUILDLINK3_MK
 
