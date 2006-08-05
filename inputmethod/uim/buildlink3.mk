8cb2d7b6e7e064b615fd05c5e068d9d27217200b
wiz <wiz@pkgsrc.org>
2006-08-05 18:49:29 +0000

    Update to 1.2.0, bump BUILDLINK_ABI_DEPENDS for shlib major bump. (There are no dependencies in pkgsrc.)
    Overview of changes from 1.2.0-beta to 1.2.0
    ==
    * Fixes
      o Minor
        - Prime
          - Fix initialization of the context upon IM switch (Konosuke).
    
        - uim-pref-qt
          - Handle Meta modifier.
        - uim-toolbar-qt
          - Fix to shrink toolbar when any of the buttons are removed.
    
        - uim.el
          - Fix displaying candidates in input prediction (Konosuke).
          - Fix displaying candidates with appendix on uim-prime (Konosuke).
    
    * Others
      - Bump libuim version.
    
    Overview of changes from 1.2.0-alpha to 1.2.0-beta
    ==
    * Fixes
      o Minor
        - Anthy
        - Canna
        - Mana
          - Commit appropriate space letter in halfwidth_alnum input mode.
    
        - Qt immodule
          - Fix getting default IM in some environments.
    
    Overview of changes from 1.1.x to 1.2.0-alpha
    ==
    * New features
      - Anthy
        - Input prediction support.
        - Half-width and full-width alphanumeric conversion with a specific
          segment.
        - Add half-width alphanumeric input mode.
        - Change definition of some input-mode transition keys.
    
      - Canna
      - Mana
        - Hiragana, katakana, half-width katakana, half- and full-width
          alphanumeric conversion with a specific segment.
        - Add half-width alphanumeric input mode.
        - Change definition of some input-mode transition keys.
    
      - uim-dict-gtk
        - Support Canna private dictionary.
    
      - Gtk+ immodule
      - Qt immodule
        - Support locale dependent Compose file and ~/.XCompose
    
    * Fixes
      - Anthy, Canan, Mana
        - Don't insert full-width space on half-width katakana mode (bug #502).
    
      - uim-xim
        - Support ja (EUC) locale in Solaris 10 (bug #7558, KIHARA Hideto).
    
      - general
        - Handle several input mode keys in Japanese keyboard.

--- inputmethod/uim/buildlink3.mk
+++ inputmethod/uim/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/07/08 23:10:54 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2006/08/05 18:49:29 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 UIM_BUILDLINK3_MK:=	${UIM_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}uim
 
 .if !empty(UIM_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.uim+=		uim>=0.4.6
-BUILDLINK_ABI_DEPENDS.uim?=	uim>=1.0.1nb2
+BUILDLINK_ABI_DEPENDS.uim?=	uim>=1.2.0
 BUILDLINK_PKGSRCDIR.uim?=	../../inputmethod/uim
 .endif  # UIM_BUILDLINK3_MK
 
