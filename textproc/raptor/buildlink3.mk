84b04abbceda7708a701d87aaad6d03834c33b8f
wiz <wiz@pkgsrc.org>
2006-08-08 15:19:15 +0000

    regen.

--- textproc/raptor/buildlink3.mk
+++ textproc/raptor/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.7 2006/07/08 23:11:11 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2006/08/08 15:21:39 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 RAPTOR_BUILDLINK3_MK:=	${RAPTOR_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	raptor
 .endif
 
@@ -11,9 +11,10 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nraptor}
 BUILDLINK_PACKAGES+=	raptor
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}raptor
 
-.if !empty(RAPTOR_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.raptor?=		raptor>=1.0.0
-BUILDLINK_ABI_DEPENDS.raptor+=		raptor>=1.0.0nb6
+.if ${RAPTOR_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.raptor?=	raptor>=1.0.0
+BUILDLINK_ABI_DEPENDS.raptor+=	raptor>=1.0.0nb6
+BUILDLINK_PKGSRCDIR.raptor?=	../../textproc/raptor
 .endif	# RAPTOR_BUILDLINK3_MK
 
 .include "../../textproc/libxml2/buildlink3.mk"
