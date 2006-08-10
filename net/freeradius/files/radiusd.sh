3ff2d5fbcd582979e3433fd42462fc4efeb66536
adrianp <adrianp@pkgsrc.org>
2006-08-10 10:55:51 +0000

    Default to running radiusd as a non-root user bump to nb2

--- net/freeradius/files/radiusd.sh
+++ net/freeradius/files/radiusd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: radiusd.sh,v 1.5 2005/02/09 09:14:26 seb Exp $
+# $NetBSD: radiusd.sh,v 1.6 2006/08/10 10:55:51 adrianp Exp $
 #
 # PROVIDE: radiusd
 # REQUIRE: network
@@ -22,7 +22,7 @@ radiusd_precmd()
 	if [ ! -d @VARBASE@/run/radiusd ]; then
 		@MKDIR@ @VARBASE@/run/radiusd
 		@CHMOD@ 0750 @VARBASE@/run/radiusd
-		@CHOWN@ @ROOT_USER@:@ROOT_GROUP@ @VARBASE@/run/radiusd
+		@CHOWN@ @RADIUS_USER@:@RADIUS_GROUP@ @VARBASE@/run/radiusd
 	fi
 }
 
