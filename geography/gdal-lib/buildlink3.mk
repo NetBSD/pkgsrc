cb5fddbb672cb156034d9c7e7aa919d83f4ed8fa
wiz <wiz@pkgsrc.org>
2006-08-10 00:38:25 +0000

    Correct dependency handling; from Brook Milligan in private mail.

--- geography/gdal-lib/buildlink3.mk
+++ geography/gdal-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/07/08 23:10:49 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/08/10 00:38:25 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GDAL_LIB_BUILDLINK3_MK:=	${GDAL_LIB_BUILDLINK3_MK}+
@@ -15,5 +15,21 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gdal-lib
 BUILDLINK_API_DEPENDS.gdal-lib+=	gdal-lib>=1.3.1
 BUILDLINK_PKGSRCDIR.gdal-lib?=	../../geography/gdal-lib
 .endif	# GDAL_LIB_BUILDLINK3_MK
+
+.if !defined(PKG_BUILD_OPTIONS.gdal-lib)
+PKG_BUILD_OPTIONS.gdal-lib!=                                             \
+	cd ${BUILDLINK_PKGSRCDIR.gdal-lib} &&                            \
+	${MAKE} show-var ${MAKEFLAGS} VARNAME=PKG_OPTIONS
+MAKEFLAGS+=	PKG_BUILD_OPTIONS.gdal-lib=${PKG_BUILD_OPTIONS.gdal-lib:Q}
+.endif
+MAKEVARS+=	PKG_BUILD_OPTIONS.gdal-lib
+
+.if !empty(PKG_BUILD_OPTIONS.gdal-lib:Mpgsql)
+.include "../../mk/pgsql.buildlink3.mk"
+.endif
+
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../geography/geos/buildlink3.mk"
+.include "../../textproc/xerces-c/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
