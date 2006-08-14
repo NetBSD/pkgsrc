4c679bf3a549a79b5ac34af2031724c457086fcd
tron <tron@pkgsrc.org>
2006-08-14 15:57:40 +0000

    Update "squirrelmail" and "squirrelmail-locales" package to version 1.4.8. Changes since version 1.4.7: - A security fix for CVE-2006-4019 - A collection of bugfixes

--- mail/squirrelmail/buildlink3.mk
+++ mail/squirrelmail/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2006/07/13 07:59:34 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2006/08/14 15:57:40 tron Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SQUIRRELMAIL_BUILDLINK3_MK:=	${SQUIRRELMAIL_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	squirrelmail
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}squirrelmail
 
 .if ${SQUIRRELMAIL_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.squirrelmail+=	squirrelmail>=1.4.7
+BUILDLINK_API_DEPENDS.squirrelmail+=	squirrelmail>=1.4.8
 BUILDLINK_PKGSRCDIR.squirrelmail?=	../../mail/squirrelmail
 .endif	# SQUIRRELMAIL_BUILDLINK3_MK
 
