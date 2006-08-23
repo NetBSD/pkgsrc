e243daeba104ba1fbfdcb2aa82f251bbf5bd649e
jmmv <jmmv@pkgsrc.org>
2006-08-23 09:45:16 +0000

    Update to 0.71:
    D-Bus GLib Bindings 0.71 (24 July 2006)
    ==
    - Correctly installs a few missing headers
    - Build was cleaned up a bit
    
    D-Bus GLib Bindings 0.70 (17 July 2006)
    ==
    - First release after bindings split
    - dbus-binding-tool heeds org.freedesktop.DBus.GLib.ClientCSymbol C symbol
      name annotations when generating glib client bindings
    - DBusGProxy can now be inherited from
    - Support added for generating bindings to arrays that are represented as
      GPtrArrays rather than GArrays (ie size-variable things, such as strings,
      objects, structs, etc).
    - Modification of the existing specialised types to have N type parameters
      (rather than the current 1 or 2 for arrays and dictionaries respectively).
      You can then use this to get a glib type to represent any arbitrary
      D-Bus struct type using dbus_g_type_get_struct. The only implementation of
      these types is with GValueArrays as before, but it's now possible to store
      these in arrays, emit them in signals, etc.
    - New methodbus_g_connection_open provides a way to open connections to an
      arbitrary address
    - Various bugs and memory leaks fixed

--- sysutils/dbus-glib/buildlink3.mk
+++ sysutils/dbus-glib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2006/07/08 23:11:08 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2006/08/23 09:45:16 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 DBUS_GLIB_BUILDLINK3_MK:=	${DBUS_GLIB_BUILDLINK3_MK}+
@@ -13,6 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}dbus-glib
 
 .if !empty(DBUS_GLIB_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.dbus-glib+=	dbus-glib>=0.61
+BUILDLINK_ABI_DEPENDS.dbus-glib+=	dbus-glib>=0.71
 BUILDLINK_PKGSRCDIR.dbus-glib?=	../../sysutils/dbus-glib
 .endif	# DBUS_GLIB_BUILDLINK3_MK
 
