92d18f5568e9dcf6b4443e7a755c11ff9124a14c
jmmv <jmmv@pkgsrc.org>
2006-08-28 08:28:57 +0000

    Update to 0.92:
    - Add support for LOCAL_CREDS socket credentials so that we can connect to
      the system daemon under NetBSD.  It was useless before...
    
    Changes since 0.91:
    
    - Proper thread locking added to pending calls
    - Threading semantics changed from init early to init before the second thread
      is started
    - Correctly error out when an application tries to acquire or release the
      org.freedesktop.DBus name instead of sending false result codes
    - kqueue directory watching code can now be used to monitor config file changes
      on FreeBSD
    - --with-dbus-daemondir configure switch added so the daemon can be installed
      separate from the user binaries
    - Makefiles fixed for cygwin
    - Various fixes for the ongoing Windows port
    - Fixed docs and comments to use the D-Bus spelling instead of D-BUS
    - Many memleaks and bugs fixed

--- sysutils/dbus/files/dbus.sh
+++ sysutils/dbus/files/dbus.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: dbus.sh,v 1.3 2006/03/30 20:57:08 jmmv Exp $
+# $NetBSD: dbus.sh,v 1.4 2006/08/28 08:28:57 jmmv Exp $
 #
 # PROVIDE: dbus
 # REQUIRE: DAEMON
@@ -14,14 +14,12 @@ command="@PREFIX@/bin/dbus-daemon"
 command_args="--system"
 pidfile="@VARBASE@/run/dbus/pid"
 start_precmd=dbus_prestart
-dbus_user=messagebus
-dbus_group=messagebus
 
 dbus_prestart() {
 	if @TEST@ ! -d "@VARBASE@/run/dbus"; then
 		@MKDIR@ "@VARBASE@/run/dbus"
 		@CHMOD@ 0755 "@VARBASE@/run/dbus"
-		@CHOWN@ ${dbus_user}:${dbus_group} "@VARBASE@/run/dbus"
+		@CHOWN@ messagebus:messagebus "@VARBASE@/run/dbus"
 	fi
 }
 
