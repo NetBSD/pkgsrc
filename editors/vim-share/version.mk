63f0d21bd860d4d43464fa22189e871d654d6d18
martti <martti>
2006-08-31 18:44:36 +0000

    Updated editors/vim-share to 7.0.76
      1534  7.0.043  "%!" at start of 'statusline' didn't work
      5277  7.0.044  Perl: changing a line in a non-current buffer may not work
      2318  7.0.046  the matchparen plugin didn't handle parens in single quotes
      2219  7.0.047  the exit status of the configure script can be wrong
      2173  7.0.048  the gzip plugin can't handle file names that have a paren
      2221  7.0.049  some Tcl scripts are not recognized
      1437  7.0.050  can't properly close a buffer through the NetBeans interface
      6097  7.0.051  (after 7.0.44) compile and/or run problem with Perl interface
      2237  7.0.052  the Vim server may expose more functionality than expected
      1379  7.0.053  shortening multi-byte directory name may fail
      2464  7.0.054  using an empty menu name may cause a crash
      2204  7.0.055  ":startins" in CmdwinEnter autocmd doesn't work immediately
      1484  7.0.056  can't use "#!" in Vim scripts to make them executable
      1423  7.0.058  gbk and gb18030 encodings are not recognized
      2110  7.0.059  Perl interface doesn't compile with ActiveState Perl 5.8.8
     18082  7.0.060  code for switching buffers is repeated in several places
      2249  7.0.061  crash in Vim command completion when nothing to complete
      4075  7.0.062  Mac: crash when using popup menu with spell error
      1295  7.0.063  tiny chance for a memory leak
      1818  7.0.064  compiler warnings for unused variables
      1407  7.0.066  tab pages line not redrawn after removing the popup menu
      3038  7.0.067  undo problems when using input method
      6123  7.0.068  'ignorecase' is not used properly for Insert mode completion
      1457  7.0.069  crash when setting 'guitablabel' to "%!expand(\%)"
     71560  7.0.070  compiler warnings for shadowed and uninitialized variables
      1550  7.0.071  using empty search pattern may cause a crash
      2791  7.0.072  can't specify anything to do when starting the GUI fails
      1597  7.0.073  CR in Insert mode completion may select original text
      1495  7.0.075  winsaveview() could store wrong desired cursor column
      2635  7.0.076  (after 7.0.010) could not get spell files from non-ftp server

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.12 2006/07/25 06:20:54 martti Exp $
+# $NetBSD: version.mk,v 1.13 2006/08/31 18:44:36 martti Exp $
 
 VIM_VERSION=		7.0
-VIM_PATCHLEVEL=		42
+VIM_PATCHLEVEL=		76
 VIM_SUBDIR=		vim70
