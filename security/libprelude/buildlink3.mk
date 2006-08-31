0eab4d0e6aae9f74913c5b2ceb23663f4ac854e2
rillig <rillig@pkgsrc.org>
2006-08-31 13:11:23 +0000

    Fixed a few pkglint warnings.

--- security/libprelude/buildlink3.mk
+++ security/libprelude/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/07/08 23:11:07 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2006/08/31 13:11:23 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBPRELUDE_BUILDLINK3_MK:=	${LIBPRELUDE_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libprelude
 
 .if !empty(LIBPRELUDE_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.libprelude+=	libprelude>=0.9.0
-BUILDLINK_ABI_DEPENDS.libprelude?=	libprelude>=0.9.7
+BUILDLINK_ABI_DEPENDS.libprelude+=	libprelude>=0.9.7
 BUILDLINK_PKGSRCDIR.libprelude?=	../../security/libprelude
 .endif	# LIBPRELUDE_BUILDLINK3_MK
 
