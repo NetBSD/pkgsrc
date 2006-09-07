8b8eb3c41493e24ef3920a6cf90de7ae21c8732a
wennmach <wennmach@pkgsrc.org>
2006-09-07 08:46:20 +0000

    Update kth-krb4 to 1.2.2 (the current release).
    New in 1.2.2:
    
        * cross-realm disabled in the server
    
    Addresses PR pkg/31431 by Zafer Aydogan.
    
    While there, make package pkglint-clean.

--- security/kth-krb4/buildlink3.mk
+++ security/kth-krb4/buildlink3.mk
@@ -1,5 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2006/07/08 23:11:06 jlam Exp $
-# XXX	BUILDLINK_DEPMETHOD.kth-krb4?=	build
+# $NetBSD: buildlink3.mk,v 1.8 2006/09/07 08:46:20 wennmach Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 KTH_KRB4_BUILDLINK3_MK:=	${KTH_KRB4_BUILDLINK3_MK}+
@@ -14,9 +13,9 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}kth-krb4
 
 .if !empty(KTH_KRB4_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.kth-krb4+=	kth-krb4>=1.1.1
-BUILDLINK_ABI_DEPENDS.kth-krb4+=kth-krb4>=1.2.1nb5
+BUILDLINK_ABI_DEPENDS.kth-krb4+=	kth-krb4>=1.2.1nb5
 BUILDLINK_ABI_DEPENDS.kth-krb4?=	kth-krb4>=1.2.1nb6
-BUILDLINK_PKGSRCDIR.kth-krb4?=	../../security/kth-krb4
+BUILDLINK_PKGSRCDIR.kth-krb4?=		../../security/kth-krb4
 .endif	# KTH_KRB4_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
