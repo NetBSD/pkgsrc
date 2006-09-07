0f4311bd66d97677bda7694e17e87f1316f20f6a
martti <martti>
2006-09-07 06:57:39 +0000

    Updated xfce4-artwork to 0.1 (changes unknown).

--- graphics/xfce4-artwork/buildlink3.mk
+++ graphics/xfce4-artwork/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/07/08 23:10:53 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2006/09/07 06:57:39 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XFCE4_ARTWORK_BUILDLINK3_MK:=	${XFCE4_ARTWORK_BUILDLINK3_MK}+
@@ -12,8 +12,7 @@ BUILDLINK_PACKAGES+=	xfce4-artwork
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-artwork
 
 .if !empty(XFCE4_ARTWORK_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.xfce4-artwork+=	xfce4-artwork>=0.0.4nb5
-BUILDLINK_ABI_DEPENDS.xfce4-artwork?=	xfce4-artwork>=0.0.4nb8
+BUILDLINK_API_DEPENDS.xfce4-artwork+=	xfce4-artwork>=0.1
 BUILDLINK_PKGSRCDIR.xfce4-artwork?=	../../graphics/xfce4-artwork
 .endif	# XFCE4_ARTWORK_BUILDLINK3_MK
 
