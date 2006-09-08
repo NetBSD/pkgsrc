f2ba59aed83c094605f3092decebcc7c815fea16
jmmv <jmmv@pkgsrc.org>
2006-09-08 16:27:34 +0000

    Update to 1.4: add the share/dtds directory, initially used by the forthcoming icon-naming-utils package.

--- misc/xdg-x11-dirs/dirs.mk
+++ misc/xdg-x11-dirs/dirs.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dirs.mk,v 1.6 2006/04/01 21:39:55 jmmv Exp $
+# $NetBSD: dirs.mk,v 1.7 2006/09/08 16:27:34 jmmv Exp $
 #
 # This file is intended to be included by mk/dirs.mk, not directly by packages.
 #
@@ -9,6 +9,7 @@ DIRS_XDG_X11_MK=	# defined
 XDG_X11_DIRS=		share/application-registry
 XDG_X11_DIRS+=		share/applications
 XDG_X11_DIRS+=		share/desktop-directories
+XDG_X11_DIRS+=		share/dtds
 XDG_X11_DIRS+=		share/icons
 XDG_X11_DIRS+=		share/images
 XDG_X11_DIRS+=		share/mime-info
