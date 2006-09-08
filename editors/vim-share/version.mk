a67c970bb9336dd26f9b55acfc4a7814c17a056c
martti <martti@pkgsrc.org>
2006-09-08 06:18:28 +0000

    Updated editors/vim-share to 7.0.91
      2799  7.0.077  crash when unletting a v: variable
      1968  7.0.078  there are two error messages called E46
     51669  7.0.079  vimtutor doesn't work for Russian when 'encoding' is utf-8
      1759  7.0.080  compile error when CFLAGS contains a backslash
      2043  7.0.081  command line completion doesn't work for ":!/path"
      2850  7.0.082  crash when using List or Dict argument and waiting for input
      3001  7.0.083  mouse doesn't work for all entries with inputlist()
      5968  7.0.084  garbage collection may free Lists and Dicts still in use
      1512  7.0.085  running tests changes the viminfo file
      2379  7.0.086  getqflist() uses number zero for empty pattern and text
      7382  7.0.087  'acd' doesn't work for ":file fname" and ":saveas fname"
      3066  7.0.088  generated prototypes have "extern" when build with Perl
      2445  7.0.089  "ga" gave wrong results for non-Unicode multi-byte encoding
      2486  7.0.090  need to type Esc twice in console confirm() dialog
      1698  7.0.091  winrestview() may leave cursor in the wrong position

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.13 2006/08/31 18:44:36 martti Exp $
+# $NetBSD: version.mk,v 1.14 2006/09/08 06:18:29 martti Exp $
 
 VIM_VERSION=		7.0
-VIM_PATCHLEVEL=		76
+VIM_PATCHLEVEL=		91
 VIM_SUBDIR=		vim70
