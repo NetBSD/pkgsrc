bf533821c30657ccf16d219395ba83d5fa5da57e
obache <obache@pkgsrc.org>
2006-09-09 02:39:11 +0000

    Usage of variable MAKEFILE in pkgsrc has been changed. So no longer needs this hack.

--- devel/bmake/files/main.c
+++ devel/bmake/files/main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main.c,v 1.1.1.1 2005/12/02 00:03:00 sjg Exp $	*/
+/*	$NetBSD: main.c,v 1.2 2006/09/09 02:39:11 obache Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,7 +69,7 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: main.c,v 1.1.1.1 2005/12/02 00:03:00 sjg Exp $";
+static char rcsid[] = "$NetBSD: main.c,v 1.2 2006/09/09 02:39:11 obache Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
@@ -81,7 +81,7 @@ __COPYRIGHT("@(#) Copyright (c) 1988, 1989, 1990, 1993\n\
 #if 0
 static char sccsid[] = "@(#)main.c	8.3 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: main.c,v 1.1.1.1 2005/12/02 00:03:00 sjg Exp $");
+__RCSID("$NetBSD: main.c,v 1.2 2006/09/09 02:39:11 obache Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -1136,16 +1136,7 @@ ReadMakefile(ClientData p, ClientData q __unused)
 		Parse_File("(stdin)", stdin);
 		Var_Set("MAKEFILE", "", VAR_GLOBAL, 0);
 	} else {
-#ifdef __INTERIX
-		/*
-		 * XXX Hack from tv:
-		 * This system has broken filesystem support - can't
-		 * always distinguish b/w [Mm]akefile.
-		 */
-		setMAKEFILE = FALSE;
-#else
 		setMAKEFILE = strcmp(fname, ".depend");
-#endif
 
 		/* if we've chdir'd, rebuild the path name */
 		if (strcmp(curdir, objdir) && *fname != '/') {
