2c96e5d34e48430065a885bac893bd90f3ff84a8
dmcmahill <dmcmahill@pkgsrc.org>
2006-09-10 19:47:54 +0000

    Update to 20060906 snapshot.  Fixes a crashing bug in the gschem autosave code.

--- cad/libgeda/buildlink3.mk
+++ cad/libgeda/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2006/09/01 12:00:43 dmcmahill Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2006/09/10 19:47:54 dmcmahill Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBGEDA_BUILDLINK3_MK:=	${LIBGEDA_BUILDLINK3_MK}+
@@ -12,8 +12,8 @@ BUILDLINK_PACKAGES+=	libgeda
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libgeda
 
 .if !empty(LIBGEDA_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.libgeda+=	libgeda-20060824{,nb[0-9]*}
-BUILDLINK_ABI_DEPENDS.libgeda?=	libgeda>=20060824
+BUILDLINK_API_DEPENDS.libgeda+=	libgeda-20060906{,nb[0-9]*}
+BUILDLINK_ABI_DEPENDS.libgeda?=	libgeda>=20060906
 BUILDLINK_PKGSRCDIR.libgeda?=	../../cad/libgeda
 .endif	# LIBGEDA_BUILDLINK3_MK
 
