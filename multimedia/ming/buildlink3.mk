11e4638cfb670d462f20c8d4e4a820704f8efefb
obache <obache@pkgsrc.org>
2006-09-13 15:53:51 +0000

    Update ming to 0.3.0. ChangeLog is too large, so omit it.

--- multimedia/ming/buildlink3.mk
+++ multimedia/ming/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/07/08 23:11:02 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2006/09/13 15:53:51 obache Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 MING_BUILDLINK3_MK:=	${MING_BUILDLINK3_MK}+
@@ -12,8 +12,12 @@ BUILDLINK_PACKAGES+=	ming
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}ming
 
 .if !empty(MING_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.ming+=	ming>=0.2
+BUILDLINK_API_DEPENDS.ming+=	ming>=0.3.0
 BUILDLINK_PKGSRCDIR.ming?=	../../multimedia/ming
 .endif	# MING_BUILDLINK3_MK
+
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../graphics/libungif/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
