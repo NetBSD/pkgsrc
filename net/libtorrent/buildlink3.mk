e4e0d2d890d357df5ecd7874abe792c7ec8fe175
tron <tron@pkgsrc.org>
2006-09-13 21:04:19 +0000

    Update "libtorrent" package to version 0.10.1 and "rtorrent" package to version 0.6.1. Changes since version 0.8.5 respectively 0.4.5 are: - improved user interface (apparently also fixing redraw problems   under DragonFly BSD) - better diskspace handling - show client names and versions in the peer list - improved tracker communication - lots of bug fixes
    Based on patches provided by Jan 'Rumko' Kosir in private e-mail.

--- net/libtorrent/buildlink3.mk
+++ net/libtorrent/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2006/07/08 23:11:03 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2006/09/13 21:04:19 tron Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBTORRENT_BUILDLINK3_MK:=	${LIBTORRENT_BUILDLINK3_MK}+
@@ -12,8 +12,8 @@ BUILDLINK_PACKAGES+=	libtorrent
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libtorrent
 
 .if !empty(LIBTORRENT_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.libtorrent+=		libtorrent>=0.8.0
-BUILDLINK_ABI_DEPENDS.libtorrent+=	libtorrent>=0.8.5nb1
+BUILDLINK_API_DEPENDS.libtorrent+=	libtorrent>=0.10.0
+BUILDLINK_ABI_DEPENDS.libtorrent+=	libtorrent>=0.10.0
 BUILDLINK_PKGSRCDIR.libtorrent?=	../../net/libtorrent
 .endif	# LIBTORRENT_BUILDLINK3_MK
 
