b7cf15651584dd0dea886e1899ea66bc3cced0df
jmmv <jmmv@pkgsrc.org>
2006-09-15 15:05:52 +0000

    Update to 0.8.0:
    Changes in 0.8.0:
    -----------------
    * Use an XSLT namespace alias for XInclude output
    * Updated translations:
      bn      (Jamil Ahmed)
      bn_IN   (Runa Bhattacharjee)
      de      (Hendrik Richter)
      el      (Kostas Papadimas)
      es      (Francisco Javier F. Serrador)
      eu      (Inaki Larranaga)
      hi      (Rajesh Ranjan)
      hu      (Gabor Kelemen)
      id      (Ahmad Riza H Nst)
      ja      (Satoru SATOH)
      lt      (Gintautas Miliauskas)
      mg      (Thierry Randrianiriana)
      mr      (Rahul Bhalrao)
      or      (Subhransu Behera)
      pt      (Duarte Loreto)
      pt_BR   (Leonardo Ferreira Fontenelle)
      ru      (Leonid Kanter)
      sl      (Matic Žgur)
      ug      (Gheyret T.Kenji)
      uk      (Maxim Dziumanenko)
      zh_CN   (Funda Wang)
      (38/61 languages with 100% translation)
    
    Changes in 0.7.2:
    -----------------
    * Updated translations:
      bn_IN   (Runa Bhattacharjee)
      es      (Francisco Javier F. Serrador)
      fr      (Benoît Dejean)
      id      (Ahmad Riza H Nst)
      ko      (Changwoo Ryu
      lv      (Raivis Dejus)
      ml      (Ani Peter)
      mn      (Sanlig Badral)
      nl      (Tino Meinen)
      ta      (I. Felix)
      zh_HK   (Chao-Hsiung Liao)
      zh_TW   (Chao-Hsiung Liao)
    
    Changes in 0.7.1:
    -----------------
    
    * build: Added support for the LINGUAS environment variable  (Shaun McCance)
    * build: Removed the code to copy gnome-doc-utils.make in  (Shaun McCance)
    * build: Some fixes for HTML generation  (Shaun McCance)
    * build: Added the long-missing template document (Sven Herzberg)
    * xml2po: Treat year as utf-8 as well  (Danilo Šegan)
    * xml2po: Check if "translator-credits" is translated  (Danilo Šegan)
    * xml2po: Updated test cases  (Danilo Šegan)
    * xml2po: Added Ubuntu mode supporting &language;  (Danilo Šegan)
    * xml2po: Mark non-translated passages with @lang; #172459  (Danilo Šegan)
    * xml2po: Added entity-non-expanding serialization  (Danilo Šegan)
    * xml2po: Fixed attribute handling; #317110  (Danilo Šegan)
    * xml2po: Improved fix for #336765  (Danilo Šegan)
    * xml2po: Don't remove blank nodes followed by entities; #317502  (Danilo Šegan)
    * xml2po: Integrate DTD when keeping entities; #336765  (Danilo Šegan)
    * xml2po: Ignore blank nodes; #172784  (Danilo Šegan)
    * xslt: Fixed plurals for fr, wa, nso; bug #338641  (Shaun McCance)
    * xslt: Don't put content before attributes  (Shaun McCance)
    * Updated translations:
      bg      (Alexander Shopov)
      ca      (Josep Puigdemont Casamajó)
      cs      (Lukas Novotny)
      cy      (Rhys Jones)
      dz      (Pema Geyleg)
      es      (Francisco Javier F. Serrador)
      et      (Ivar Smolin)
      fi      (Ilkka Tuohela)
      gl      (Ignacio Casal Quinteiro)
      gu      (Ankit Patel)
      ka      (Vladimer Sichinava)
      nb      (Kjartan Maraas)
      no      (Kjartan Maraas)
      ro      (Dan Damian)
      sv      (Daniel Nylander)
      th      (Theppitak Karoonboonyanan)
      vi      (Clytie Siddall)
      zh_HK   (Chao-Hsiung Liao)
      zh_TW   (Chao-Hsiung Liao)

--- textproc/gnome-doc-utils/buildlink3.mk
+++ textproc/gnome-doc-utils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/07/08 23:11:10 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2006/09/15 15:05:52 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_DOC_UTILS_BUILDLINK3_MK:=	${GNOME_DOC_UTILS_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gnome-doc-utils
 
 .if !empty(GNOME_DOC_UTILS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPMETHOD.gnome-doc-utils+=	build
-BUILDLINK_API_DEPENDS.gnome-doc-utils+=	gnome-doc-utils>=0.6.0
+BUILDLINK_API_DEPENDS.gnome-doc-utils+=	gnome-doc-utils>=0.8.0
 BUILDLINK_PKGSRCDIR.gnome-doc-utils?=	../../textproc/gnome-doc-utils
 .endif	# GNOME_DOC_UTILS_BUILDLINK3_MK
 
