9cc2a4b298290858ddf3d29f72cbe5a466c89416
jmmv <jmmv@pkgsrc.org>
2006-09-15 15:12:07 +0000

    Update to 1.8.0:
    Evolution-Data-Server 1.8.0 2006-09-04
    --------------------------------------
    Evolution -Data-Server 1.8 Stable Release for GNOME 2.16.0.
    
    Evolution-Data-Server 1.7.92, 2006-08-21
    ------------------------------------------
    
    Updated Translations
    
    Hendrik Richter (de), Alexander Shopov (bg), Rahul Bhalerao (mr),
    Satoru SATOH (ja), Kjartan Maraas (nb), Artur Flinta (pl),
    Žygimantas Beručka (lt), Daniel Nylander (sv), Runa Bhattacharjee (bn_IN),
    Gabor Kelemen (hu), Leonid Kanter (ru), Matic Žgur (sl),
    Clytie Siddall (vi), Funda Wang (zh_CN), Changwoo Ryu (ko),
    Vladimer Sichinava (ka), Xavier Conde Rueda (ca), Maxim Dziumanenko (uk),
    Guntupalli Karunakar (dz), Theppitak Karoonboonyanan (th),
    Priit Laes (et).
    
    Bug Fixes:
    
    Ricardo Markiewicz, Chenthill, Harish, Kjartan Maraas,
    j@bitron.ch
    
    Evolution-Data-Server 1.7.91, 2006-08-07
    ------------------------------------------
    
    Updated Translations
    
    Francisco Javier F. Serrador (es), Inaki Larranaga (eu), Ilkka Tuohela (fi),
    Christophe Merlet (fr), Ahmad Riza H Nst (id), Kjartan Maraas	 (nb),
    Vincent van Adrighem (nl), Jovan Naumovski (mk), Theppitak Karoonboonyanan (th)
    
    New in EDS 1.7.91:
    - Add filters to Groupwise SOAP calls so that each component fetches items of
      its own type and none other. (Harish)
    - Memory fixes in Addressbook (Hans Peters Jansson)
    
    Bug Fixes:
    Matthew Barnes, Chenthill Palanisamy, Devashish Sharma, Srinivasa Ragavan,
    Tor Lillqvist, Harish Krishnaswamy, Ed Catmur, Ross Burton,
    Wouter Bolsterlee
    
    Evolution-Data-Server 1.7.90.1, 2006-07-25
    ------------------------------------------
    Fix a dist bug in tarball.
    
    Evolution-Data-Server 1.7.90, 2006-07-24
    ----------------------------------------
    Updated Translations contributed by:
    
    Funda Wangi (zh_CN), Christophe Merlet (fr),
    Jonathan Ernst (fr), Changwoo Ryu (ko),
    Kostas Papadimas (el), Dzongkhalinux team, DIT (dz),
    Ilkka Tuohela (fi), Ankit Patel (gu), Daniel Nylander (sv),
    Priit Laes (et), Francisco Javier F. Serrador (es),
    Raivis Dejus (lv).
    
    New in EDS 1.7.90 :
    Support for GW Reminder Notes (Chenthill)
    Split cache for less memory, better performance (Harish)
    Support for security-classifications in GW send options (Sankar)
    Support for default alarms in GW (Harish)
    Support for Gadu Gadu IM in Contact Entries
    (Artur Flinta, Fryderyk Dziarmagowski)
    Memory reduction and Performance improvements in Contacts (Ross Burton)
    
    Bug fixes :
    Andre Klapper, Shreyas Srinivasan, Sushma Rai, Vandana Shenoy,
    Harish Krishnaswamy, Devashish Sharma, Hiroyuki Ikezoe, Srinivasa Ragavan.
    Luca Ferretti
    
    Evolution-Data-Server 1.7.4, 2006-07-10
    ---------------------------------------
    Updated Translations contributed by:
    
    Inaki Larranaga, Ilkka Tuohela,
    Vincent van Adrighem, Runa Bhattacharjee,
    Changwoo Ryu, I.Felix, Rajesh Ranjan,
    Dzongkhalinux, Yair Hershkovitz, Kjartan Maraas,
    Chao-Hsiung Liao, Ignacio Casal Quienteiro,
    Theppitak Karoonboonyan, Francisco Javier F. Serrador,
    Ankit Patel, Hendrik Richter.
    
    Bug fixes:
    Harish Krishnaswamy, Jeffrey Stedfast, Sankar P,
    Tor Lillqvist, Andre Klapper, ,Parthasarathi,
    Srinivasa Ragavan, Chenthill, Wang Xin, Ross Burton,
    Devashish Sharma, Boby Wang, Simon Zheng, Hiroyuki
    Ikezoe, Frederic Peters.
    
    
    Evolution-Data-Server 1.7.3, 2006-06-12
    ---------------------------------------
    Updated Translations contributed by:
    
    Kjartan Maraas, Francisco Javier F. Serrador,
    Gabor Kelemen, Ignacio Casal Quinteiro,
    Ankit Patel, Pema Geyleg, Inaki Larranaga,
    Clytie Siddall, Emil Hessman.
    
    Bug fixes and other changes :
    Chris Heath , Tor Lillqvist, Srinivasa Ragavan,
    Jeffrey Stedfast, Parthasarathi Susarla,
    Sankar P, Harish Krishnaswamy, Ross Burton, Wang Xin
    Simon Zheng, Devashish Sharma, Hiroyuki Ikezoe,
    Frederic Peters.
    
    Evolution-Data-Server 1.7.1, 2006-04-24
    ----------------------------------------
    
    First release of 1.7 development series.
    
    What is new ?
    -------------
    
    * Capability to link dynamically to libdb.
    * Integration with gnome-keyring.

--- mail/evolution-data-server/buildlink3.mk
+++ mail/evolution-data-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2006/07/08 23:10:57 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2006/09/15 15:12:07 jmmv Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 EVOLUTION_DATA_SERVER_BUILDLINK3_MK:=	${EVOLUTION_DATA_SERVER_BUILDLINK3_MK}+
@@ -12,8 +12,7 @@ BUILDLINK_PACKAGES+=	evolution-data-server
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}evolution-data-server
 
 .if !empty(EVOLUTION_DATA_SERVER_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.evolution-data-server+=	evolution-data-server>=1.4.2.1nb1
-BUILDLINK_ABI_DEPENDS.evolution-data-server?=	evolution-data-server>=1.4.2.1nb3
+BUILDLINK_API_DEPENDS.evolution-data-server+=	evolution-data-server>=1.8.0
 BUILDLINK_PKGSRCDIR.evolution-data-server?=	../../mail/evolution-data-server
 
 PRINT_PLIST_AWK+=/^@dirrm lib\/evolution-data-server-1.2\/extensions$$/ \
