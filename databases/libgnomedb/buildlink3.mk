4e5abd1fdc1260a3507e680d21b95f2e67faffa3
dsainty <dsainty>
2006-09-22 15:08:09 +0000

    Pull in security/openssl/buildlink3.mk.
    libgnomedb_entry_builtin_plugins.so.4.0.0 links with libssl, and if
    PREFER.openssl=pkgsrc then it should link with Pkgsrc libssl.  Without
    including security/openssl, it links with /usr/lib/libssl.so instead.

--- databases/libgnomedb/buildlink3.mk
+++ databases/libgnomedb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2006/09/16 12:20:34 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2006/09/22 15:11:40 dsainty Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGNOMEDB_BUILDLINK3_MK:=	${LIBGNOMEDB_BUILDLINK3_MK}+
@@ -22,6 +22,7 @@ BUILDLINK_PKGSRCDIR.libgnomedb?=	../../databases/libgnomedb
 .include "../../devel/libbonoboui/buildlink3.mk"
 .include "../../devel/libglade2/buildlink3.mk"
 .include "../../devel/libgnomeui/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .include "../../x11/gtksourceview/buildlink3.mk"
 
