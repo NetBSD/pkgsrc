02fb81502792ae82395ed78ee833d97009759b01
adam <adam@pkgsrc.org>
2006-09-24 16:48:27 +0000

    Changes 2.6.3: * Build fixes for Updated for Visual Studio 2005.

--- devel/libglademm/buildlink3.mk
+++ devel/libglademm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/07/08 23:10:44 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2006/09/24 16:48:27 adam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGLADEMM_BUILDLINK3_MK:=	${LIBGLADEMM_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libglademm
 
 .if !empty(LIBGLADEMM_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.libglademm+=	libglademm>=2.6.0
-BUILDLINK_ABI_DEPENDS.libglademm?=	libglademm>=2.6.2nb1
+BUILDLINK_ABI_DEPENDS.libglademm+=	libglademm>=2.6.2nb1
 BUILDLINK_PKGSRCDIR.libglademm?=	../../devel/libglademm
 .endif	# LIBGLADEMM_BUILDLINK3_MK
 
