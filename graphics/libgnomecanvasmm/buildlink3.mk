76621c342e633e587c9bd98d231af0bab2c70d35
adam <adam>
2006-09-24 16:49:19 +0000

    Changes 2.16.0: * Increased version number for GNOME 2.16

--- graphics/libgnomecanvasmm/buildlink3.mk
+++ graphics/libgnomecanvasmm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/07/08 23:10:52 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2006/09/24 16:49:19 adam Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 LIBGNOMECANVASMM_BUILDLINK3_MK:=	${LIBGNOMECANVASMM_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libgnomecanvasmm
 
 .if !empty(LIBGNOMECANVASMM_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.libgnomecanvasmm+=	libgnomecanvasmm>=2.10.0
-BUILDLINK_ABI_DEPENDS.libgnomecanvasmm?=	libgnomecanvasmm>=2.14.0nb1
+BUILDLINK_ABI_DEPENDS.libgnomecanvasmm+=	libgnomecanvasmm>=2.14.0nb1
 BUILDLINK_PKGSRCDIR.libgnomecanvasmm?=	../../graphics/libgnomecanvasmm
 .endif	# LIBGNOMECANVASMM_BUILDLINK3_MK
 
