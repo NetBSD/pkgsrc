809a0b2bbbc41e9bf155b514cb96059d8ec99bed
salo <salo@pkgsrc.org>
2006-09-24 16:46:27 +0000

    Remove Mozilla Suite and friends. No longer maintained upstream, replaced by SeaMonkey.

--- www/mozilla/gecko-depends.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD: gecko-depends.mk,v 1.3 2006/04/06 06:22:55 reed Exp $
-
-.if !empty(PKG_OPTIONS:Mfirefox)
-# firefox didn't have a pkgconfig file before 1.0.4
-BUILDLINK_API_DEPENDS.firefox+= firefox>=1.0.4
-.include "../../www/firefox/buildlink3.mk"
-.else
-.include "../../www/mozilla-gtk2/buildlink3.mk"
-.endif
