a1c2508b94035aeba051992aedd11e3d1acb07c6
salo <salo@pkgsrc.org>
2006-09-24 16:26:54 +0000

    Switch to "seamonkey" backend from "mozilla" as an alternative to "firefox".
    Our package is too old to make use of it, add BROKEN for now.

--- www/kazehakase/options.mk
+++ www/kazehakase/options.mk
@@ -1,11 +1,11 @@
-# $NetBSD: options.mk,v 1.1 2006/01/04 23:59:40 reed Exp $
+# $NetBSD: options.mk,v 1.2 2006/09/24 16:26:54 salo Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kazehakase
 PKG_SUPPORTED_OPTIONS=	gnutls
 PKG_SUGGESTED_OPTIONS+=	gnutls
 
-.include "../../www/mozilla/gecko-options.mk"
+.include "../../www/seamonkey/gecko-options.mk"
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgnutls)
@@ -13,4 +13,10 @@ PKG_SUGGESTED_OPTIONS+=	gnutls
 CONFIGURE_ARGS+=	--enable-ssl
 .else
 CONFIGURE_ARGS+=	--disable-ssl
+.endif
+
+# Remove after upgrade to 0.3.4 or higher!
+#
+.if !empty(PKG_OPTIONS:Mseamonkey)
+BROKEN=		seamonkey backend is currently not supported
 .endif
