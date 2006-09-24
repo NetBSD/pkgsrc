3bd1d0afc033d6824e388033fe9c9d4ac4661e7d
salo <salo@pkgsrc.org>
2006-09-24 16:24:34 +0000

    Switch to "seamonkey" backend from "mozilla" as an alternative to "firefox", split option part to a separate file.

---
+++ devel/devhelp/options.mk
@@ -0,0 +1,6 @@
+# $NetBSD: options.mk,v 1.1 2006/09/24 16:24:34 salo Exp $
+
+PKG_OPTIONS_VAR=        PKG_OPTIONS.devhelp
+
+.include "../../www/seamonkey/gecko-options.mk"
+.include "../../mk/bsd.options.mk"
