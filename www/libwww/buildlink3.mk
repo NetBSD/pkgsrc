3b81c6c4a701b24ffb53d4ef072146a415029f36
joerg <joerg@pkgsrc.org>
2006-09-24 17:40:39 +0000

    When linking against libwww built with socks4 or socks5 option, the corresponding buildlinks have to be present as well. Get the option libwww was built with to decide whether to do that. Inspired by and fixing PR 28412.

--- www/libwww/buildlink3.mk
+++ www/libwww/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2006/07/08 23:11:12 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2006/09/24 17:40:39 joerg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBWWW_BUILDLINK3_MK:=	${LIBWWW_BUILDLINK3_MK}+
@@ -16,6 +16,24 @@ BUILDLINK_API_DEPENDS.libwww+=	libwww>=5.3.2nb2
 BUILDLINK_ABI_DEPENDS.libwww+=	libwww>=5.4.0nb5
 BUILDLINK_PKGSRCDIR.libwww?=	../../www/libwww
 .endif	# LIBWWW_BUILDLINK3_MK
+
+.include "../../mk/bsd.prefs.mk"
+
+.if !defined(PKG_BUILD_OPTIONS.libwww)
+PKG_BUILD_OPTIONS.libwww!= \
+	cd ${BUILDLINK_PKGSRCDIR.libwww} && \
+	${MAKE} show-var ${MAKEFLAGS} VARNAME=PKG_OPTIONS
+MAKEFLAGS+=	PKG_BUILD_OPTIONS.libwww=${PKG_BUILD_OPTIONS.libwww:Q}
+.endif
+MAKEVARS+=	PKG_BUILD_OPTIONS.libwww
+
+.if !empty(PKG_BUILD_OPTIONS.libwww:Msocks4)
+.include "../../net/socks4/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.libwww:Msocks5)
+.include "../../net/socks5/buildlink3.mk"
+.endif
 
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
