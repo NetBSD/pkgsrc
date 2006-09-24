0c6a4ba0c1e8925d2bf8a512e75237660f60f7da
salo <salo@pkgsrc.org>
2006-09-24 16:21:33 +0000

    Install pkg-config files so the gecko engine can be used by other applications, bump PKGREVISION.
    Sync files with www/mozilla and allow seamonkey to satisfy gecko
    dependency.

---
+++ www/seamonkey/gecko-options.mk
@@ -0,0 +1,5 @@
+# $NetBSD: gecko-options.mk,v 1.1 2006/09/24 16:21:33 salo Exp $
+
+PKG_OPTIONS_REQUIRED_GROUPS+=	gecko
+PKG_OPTIONS_GROUP.gecko=	firefox seamonkey
+PKG_SUGGESTED_OPTIONS+=		firefox
--- www/seamonkey/options.mk
+++ www/seamonkey/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2006/03/30 19:15:47 ghen Exp $
+# $NetBSD: options.mk,v 1.2 2006/09/24 16:21:33 salo Exp $
 
 PKG_OPTIONS_VAR		= PKG_OPTIONS.gecko
 PKG_SUPPORTED_OPTIONS	= debug
@@ -9,4 +9,19 @@ PKG_SUPPORTED_OPTIONS	= debug
 # so it is correct
 .if empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--disable-debug
+.endif
+
+.if ( ${MOZILLA_BIN} == "firefox-bin" || ${MOZILLA_BIN} == "thunderbird-bin" )
+PKG_SUPPORTED_OPTIONS  += official-mozilla-branding
+.endif
+
+# Enable Official mozilla.org Branding for Firefox or Thunderbird.
+# Note that you cannot distribute builds with Official Branding
+# without permission of the Mozilla Foundation.
+# See http://www.mozilla.org/foundation/trademarks/
+.if !empty(PKG_OPTIONS:Mofficial-mozilla-branding)
+CONFIGURE_ARGS+=	--enable-official-branding
+RESTRICTED=		Cannot redistribute builds with Official Branding at this moment, we need permission of The Mozilla Foundation for this.
+NO_BIN_ON_CDROM=	${RESTRICTED}
+NO_BIN_ON_FTP=		${RESTRICTED}
 .endif
