961f39bcbcaea8bbbe95e460ad7f3175cbd31352
joerg <joerg@pkgsrc.org>
2006-10-09 12:50:38 +0000

    distutils allows installation in a separate tree by passing --root. Do so when using destdir.

--- lang/python/extension.mk
+++ lang/python/extension.mk
@@ -1,4 +1,4 @@
-# $NetBSD: extension.mk,v 1.14 2006/01/24 07:25:35 recht Exp $
+# $NetBSD: extension.mk,v 1.15 2006/10/09 12:50:38 joerg Exp $
 
 # derive a python version from the package name if possible
 # optionally handled quoted package names
@@ -15,11 +15,16 @@ PKG_FAIL_REASON+=	"${PKGNAME} needs dynamic loading"
 .endif
 
 .if defined(PYDISTUTILSPKG)
+.include "../../mk/bsd.prefs.mk"
+
 PYSETUP?=		setup.py
 PYSETUPBUILDARGS?=	#empty
 PYSETUPINSTALLARGS?=	#empty
 PYSETUPOPTARGS?=	-c -O1
 _PYSETUPINSTALLARGS=	${PYSETUPINSTALLARGS} ${PYSETUPOPTARGS}
+.if ${_USE_DESTDIR} != "no"
+_PYSETUPINSTALLARGS+=	--root=${DESTDIR:Q}
+.endif
 PY_PATCHPLIST?=		yes
 PYSETUPSUBDIR?=		#empty
 
