8a331971e66b3da6673d3ed4a7a5027f642db6df
martti <martti>
2006-10-09 11:53:30 +0000

    Added optional ruby support (from Matthias-Christian Ott in a private mail)
    Updated to 7.0.121
    
      2952  7.0.092  (after 7.0.082) list of internal function args is obsolete
      2320  7.0.093  matchparen plugin can't handle some values of 'matchpairs'
      2394  7.0.094  fileinfo message is given too often after ":buf N"
     52575  7.0.095  Greek tutor is not available in utf-8
      2561  7.0.096  taglist() does not use path of tags file
      2617  7.0.097  tab pages line was not updated for ":tabclose N"
      2196  7.0.098  redirection didn't work inside cmdline completion
      2189  7.0.099  GUI: scrolling causes problems for the popup menu
      2086  7.0.100  "zug" may report the wrong filename
      2732  7.0.101  "zw" mostly doesn't work; "zg" may create wrong directory
      1634  7.0.102  redraw problem in cmdline when using SCIM
      1617  7.0.103  (after 7.0.101) compiler warning for uninitialized variable
      2522  7.0.104  CursorHold[I] may trigger when it is not wanted
      1513  7.0.105  during incremental search the ruler is not updated
      5102  7.0.106  spell popup menu may have side effects
      1805  7.0.107  tab pages line was not redrawn when using 'incsearch'
      1923  7.0.109  Lisp indenting gets confused by an escaped quote in a string
      2557  7.0.110  Amiga: Compilation problems when not using libnix
     10411  7.0.111  gzip plugin can't handle file names with special characters
      2687  7.0.112  Python interface doesn't compile with Python 2.5
      1878  7.0.113  crash when using CTRL-L in Insert mode completion
      1964  7.0.114  get extra undo point when using CTRL-C to abort Insert mode
      2339  7.0.115  missing some matches when Insert mode completion ignores case
      1508  7.0.116  MS-Windows: :version reported 32 bits for 64 bits systems
      5028  7.0.117  problem when mixing keepend and extend in syntax highlighting
      1529  7.0.118  printf() doesn't allow zero padding for strings
      1412  7.0.119  CursorHold event is not triggered when leaving Insert mode
      1780  7.0.120  crash when using getreg('=') in the = register
      1549  7.0.121  GUI: dragging last status line doesn't work with text tabline

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.14 2006/09/08 06:18:29 martti Exp $
+# $NetBSD: version.mk,v 1.15 2006/10/09 11:53:30 martti Exp $
 
 VIM_VERSION=		7.0
-VIM_PATCHLEVEL=		91
+VIM_PATCHLEVEL=		121
 VIM_SUBDIR=		vim70
