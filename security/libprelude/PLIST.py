6277ab3a8ed29db917ad4a200c7605715e6d41e8
shannonjr <shannonjr@pkgsrc.org>
2006-10-11 14:46:10 +0000

    Update to 0.9.11. Changes: - Hook class comparison function. Accept NULL, equal, not equal operator. - Introduce better error checking in the idmef-class API, which is now   considered public and might be used by external application. Rename   error code to reflect the API. - Change to the way IDMEF listed element are handled. Specifying negative   number as the position of the element from the low level API now allow   to position the element at the specified (reversed) index. Using the   high level API a negative index permit to address a list of element   backward (replace an element). - Build fixes for SWIG > 1.3.27. - Modify idmef_value_match() so that it always unroll listed value   (do it for both val1 and val2. Remove assertion, and let   idmef_value_type_compare() return an error code in case there is an issue. - Handle path using IDMEF_LIST_APPEND or IDMEF_LIST_PREPEND as   path using an undefined list index on idmef_path_get() call. - Make criteria parser accept (*) list index. - Implement comparison function for all IDMEF object.

--- security/libprelude/PLIST.py
+++ security/libprelude/PLIST.py
@@ -1,4 +1,5 @@
-@comment $NetBSD: PLIST.py,v 1.2 2006/08/31 13:04:54 rillig Exp $
+@comment $NetBSD: PLIST.py,v 1.3 2006/10/11 14:46:10 shannonjr Exp $
 ${PYSITELIB}/_prelude.so
 ${PYSITELIB}/prelude.py
 ${PYSITELIB}/prelude.pyc
+${PYSITELIB}/prelude.py
