0489a859007d9d7319f46b433dc2af6db0d6dba2
rillig <rillig@pkgsrc.org>
2006-10-12 17:57:05 +0000

    Portability checks, version two.
    The actual check has moved into a shell file to allow for nice-looking
    code. Instead of only the configure scripts, it scans all files whose
    first line matches "#!*/bin/sh". Therefore the check is run no matter if
    HAS_CONFIGURE is set or not.
    
    Added a warning (not an error) for every use of $RANDOM that is not
    combined with $$, the process ID. $RANDOM is only implemented by bash
    and some versions of the ksh.

--- mk/configure/check-portability.mk
+++ mk/configure/check-portability.mk
@@ -1,4 +1,4 @@
-# $NetBSD: check-portability.mk,v 1.4 2006/10/05 10:52:40 rillig Exp $
+# $NetBSD: check-portability.mk,v 1.5 2006/10/12 17:57:05 rillig Exp $
 #
 # This file contains some checks that are applied to the configure
 # scripts to check for certain constructs that are known to cause
@@ -33,29 +33,7 @@ do-configure-pre-hook: _configure-check-for-test
 .endif
 .PHONY: _configure-check-for-test
 _configure-check-for-test:
-	@${STEP_MSG} "Checking for \"test ... == ...\" in configure scripts"
-.for d in ${CONFIGURE_DIRS}
-	${_PKG_SILENT}${_PKG_DEBUG}set -e;				\
-	cd ${WRKSRC}; cd ${d};						\
-	case `sed '1q' < ${CONFIGURE_SCRIPT}` in			\
-	"#!"*"/bin/sh")							\
-		found=no;						\
-		while read line; do					\
-			set args $$line; shift;				\
-			while [ $$# -ge 3 ]; do				\
-				if [ ":$$1" = ":test" ] && [ ":$$3" = ":==" ]; then \
-					found=yes;			\
-					${ERROR_MSG} "[configure.mk] $$line"; \
-				elif [ ":$$1" = ":#" ]; then		\
-					break;				\
-				fi;					\
-				shift;					\
-			done;						\
-			if [ "$$found" = "yes" ]; then			\
-				${ERROR_MSG} "[configure.mk] Found test ... == ... in configure script."; \
-				exit 1;					\
-			fi;						\
-		done < ${CONFIGURE_SCRIPT};				\
-		;;							\
-	esac
-.endfor
+	@${STEP_MSG} "Checking for portability problems in extracted files"
+	${_PKG_SILENT}${_PKG_DEBUG}					\
+	cd ${WRKSRC}							\
+	&& sh ${PKGSRCDIR}/mk/configure/check-portability.sh
