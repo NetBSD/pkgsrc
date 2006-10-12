b7969d7cdd22efb4d843c0e2ad097c72b5940f1b
martti <martti>
2006-10-12 09:24:19 +0000

    Regenerate to fix some pkglint warnings.

--- sysutils/xfce4-appfinder/buildlink3.mk
+++ sysutils/xfce4-appfinder/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.12 2006/07/08 23:11:09 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2006/10/12 09:24:19 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XFCE4_APPFINDER_BUILDLINK3_MK:=	${XFCE4_APPFINDER_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	xfce4-appfinder
 .endif
 
@@ -11,9 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-appfinder}
 BUILDLINK_PACKAGES+=	xfce4-appfinder
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-appfinder
 
-.if !empty(XFCE4_APPFINDER_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.xfce4-appfinder+=	xfce4-appfinder>=4.2.3
-BUILDLINK_ABI_DEPENDS.xfce4-appfinder?=	xfce4-appfinder>=4.2.3nb3
+.if ${XFCE4_APPFINDER_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.xfce4-appfinder+=	xfce4-appfinder>=4.2.3nb3
 BUILDLINK_PKGSRCDIR.xfce4-appfinder?=	../../sysutils/xfce4-appfinder
 .endif	# XFCE4_APPFINDER_BUILDLINK3_MK
 
