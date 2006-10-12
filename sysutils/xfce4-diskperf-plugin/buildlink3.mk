804862e3c1d0795c1f0ab3c84d3348356dd4d959
martti <martti@pkgsrc.org>
2006-10-12 09:24:19 +0000

    Regenerate to fix some pkglint warnings.

--- sysutils/xfce4-diskperf-plugin/buildlink3.mk
+++ sysutils/xfce4-diskperf-plugin/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.12 2006/07/08 23:11:09 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2006/10/12 09:24:19 martti Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_DISKPERF_PLUGIN_BUILDLINK3_MK:=	${XFCE4_DISKPERF_PLUGIN_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	xfce4-diskperf-plugin
 .endif
 
@@ -11,9 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-diskperf-plugin}
 BUILDLINK_PACKAGES+=	xfce4-diskperf-plugin
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-diskperf-plugin
 
-.if !empty(XFCE4_DISKPERF_PLUGIN_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.xfce4-diskperf-plugin+=	xfce4-diskperf-plugin>=1.5
-BUILDLINK_ABI_DEPENDS.xfce4-diskperf-plugin?=	xfce4-diskperf-plugin>=1.5nb3
+.if ${XFCE4_DISKPERF_PLUGIN_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.xfce4-diskperf-plugin+=	xfce4-diskperf-plugin>=1.5nb3
 BUILDLINK_PKGSRCDIR.xfce4-diskperf-plugin?=	../../sysutils/xfce4-diskperf-plugin
 .endif	# XFCE4_DISKPERF_PLUGIN_BUILDLINK3_MK
 
