e21103e89a8d71bbe8a58f5934abf916250e35ff
salo <salo@pkgsrc.org>
2006-10-17 16:18:08 +0000

    Pullup ticket 1857 - requested by dmcmahill portability fixes for libwww
    Revisions pulled up:
    - pkgsrc/www/libwww/Makefile			1.69, 1.70, 1.71
    - pkgsrc/www/libwww/distinfo			1.22
    - pkgsrc/www/libwww/options.mk			1.2
    - pkgsrc/www/libwww/patches/patch-aq		1.1
    
       Module Name:		pkgsrc
       Committed By:	rillig
       Date:		Wed Oct  4 19:45:21 UTC 2006
    
       Modified Files:
       	pkgsrc/www/libwww: distinfo
       Added Files:
       	pkgsrc/www/libwww/patches: patch-aq
    
       Log Message:
       Added a patch for the broken "test ==".
    ---
       Module Name:		pkgsrc
       Committed By:	rillig
       Date:		Wed Oct  4 19:45:58 UTC 2006
    
       Modified Files:
       	pkgsrc/www/libwww: Makefile options.mk
    
       Log Message:
       Fixed some pkglint warnings.
    ---
       Module Name:		pkgsrc
       Committed By:	rillig
       Date:		Thu Oct  5 10:05:41 UTC 2006
    
       Modified Files:
       	pkgsrc/www/libwww: Makefile
    
       Log Message:
       Overwrite the configure script in the post-extract phase, so that it can
       be patched. Noted by Andreas Gustafsson in private mail.
    ---
       Module Name:		pkgsrc
       Committed By:	dmcmahill
       Date:		Tue Oct 10 00:24:54 UTC 2006
    
       Modified Files:
       	pkgsrc/www/libwww: Makefile
    
       Log Message:
       USE_TOOLS+áutomake14
    
       This package contained several Makefile.am patches but then proceeded to
       ignore all of them by touching all of the Makefile.in's.  Unfortunately
       those patches were there for a reason!
    
       Correctly packages on solaris now.

--- www/libwww/options.mk
+++ www/libwww/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2005/12/02 17:05:04 wiz Exp $
+# $NetBSD: options.mk,v 1.1.8.1 2006/10/17 16:18:08 salo Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libwww
 PKG_OPTIONS_OPTIONAL_GROUPS+=	socks
@@ -7,11 +7,11 @@ PKG_OPTIONS_GROUP.socks=	socks4 socks5
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Msocks4)
-CONFIGURE_ARGS+= 	--with-socks4=${BUILDLINK_PREFIX.socks4}/lib/libsocks4.a
+CONFIGURE_ARGS+=	--with-socks4=${BUILDLINK_PREFIX.socks4}/lib/libsocks4.a
 CONFIGURE_ARGS+=	--with-socks5=no
 .include "../../net/socks4/buildlink3.mk"
 .elif !empty(PKG_OPTIONS:Msocks5)
 CONFIGURE_ARGS+=	--with-socks4=no
-CONFIGURE_ARGS+= 	--with-socks5=${BUILDLINK_prefix.socks5}/lib/libsocks5.a
+CONFIGURE_ARGS+=	--with-socks5=${BUILDLINK_PREFIX.socks5}/lib/libsocks5.a
 .include "../../net/socks5/buildlink3.mk"
 .endif
