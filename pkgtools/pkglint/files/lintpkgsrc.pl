b3625e41bdeea2be05320c56ab3d90e3951b4a47
abs <abs@pkgsrc.org>
2006-10-23 09:19:31 +0000

    Update to 4.72: check for .tbz as well as .tgz

--- pkgtools/pkglint/files/lintpkgsrc.pl
+++ pkgtools/pkglint/files/lintpkgsrc.pl
@@ -1,6 +1,6 @@
 #! @PERL@
 
-# $NetBSD: lintpkgsrc.pl,v 1.112 2006/10/02 19:35:43 abs Exp $
+# $NetBSD: lintpkgsrc.pl,v 1.113 2006/10/23 09:19:32 abs Exp $
 
 # Written by David Brownlee <abs@netbsd.org>.
 #
@@ -290,7 +290,7 @@ sub check_prebuilt_packages() {
         $File::Find::prune = 1;
 
     }
-    elsif (/(.+)-(\d.*)\.tgz$/) {
+    elsif (/(.+)-(\d.*)\.t[bg]z$/) {
         my ( $pkg, $ver ) = ( $1, $2 );
 
         # XXX: hack for python and ruby prefix support
