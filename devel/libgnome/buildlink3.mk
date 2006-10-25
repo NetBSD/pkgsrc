d116b0bddf351d6cb6f7339b3bdbeedc2bb8c323
dmcmahill <dmcmahill>
2006-10-25 01:40:30 +0000

    add missing bdb.buildlink3.mk

--- devel/libgnome/buildlink3.mk
+++ devel/libgnome/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2006/09/16 12:20:32 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2006/10/25 01:40:30 dmcmahill Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGNOME_BUILDLINK3_MK:=	${LIBGNOME_BUILDLINK3_MK}+
@@ -26,5 +26,6 @@ BUILDLINK_PKGSRCDIR.libgnome?=	../../devel/libgnome
 .include "../../sysutils/gnome-vfs2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../textproc/libxslt/buildlink3.mk"
+.include "../../mk/bdb.buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
