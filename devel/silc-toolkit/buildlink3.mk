ce9803a13bdd357968a0fc815850d8fa69a026b9
dmcmahill <dmcmahill@pkgsrc.org>
2006-10-25 01:40:36 +0000

    add missing devel/libgetopt/buildlink3.mk

--- devel/silc-toolkit/buildlink3.mk
+++ devel/silc-toolkit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/07/08 23:10:48 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2006/10/25 01:40:36 dmcmahill Exp $
 #
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
@@ -17,5 +17,7 @@ BUILDLINK_API_DEPENDS.silc-toolkit+=	silc-toolkit>=0.9.12
 BUILDLINK_ABI_DEPENDS.silc-toolkit+=	silc-toolkit>=0.9.12nb1
 BUILDLINK_PKGSRCDIR.silc-toolkit?=	../../devel/silc-toolkit
 .endif	# SILC_TOOLKIT_BUILDLINK3_MK
+
+.include "../../devel/libgetopt/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
