fa17d8152ce67422d05df029fda53a5857643c0b
rillig <rillig@pkgsrc.org>
2006-10-26 17:08:08 +0000

    Replaced a .sinclude with .include to catch errors when X11_TYPE is not one of "native" or "xorg". Failing with an error message in the right file is better than failing somewhere else with a completely obscure error message.

--- mk/x11.builtin.mk
+++ mk/x11.builtin.mk
@@ -1,8 +1,8 @@
-# $NetBSD: x11.builtin.mk,v 1.1 2005/06/01 18:03:06 jlam Exp $
+# $NetBSD: x11.builtin.mk,v 1.2 2006/10/26 17:08:08 rillig Exp $
 #
 # x11.builtin.mk will include the builtin.mk file from the appropriate
 # X11 distribution.
 #
 
 .include "../../mk/x11.version.mk"
-.sinclude "${X11_PKGSRCDIR.${X11_TYPE}}/builtin.mk"
+.include "${X11_PKGSRCDIR.${X11_TYPE}}/builtin.mk"
