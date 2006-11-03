37747a353bfe5a7832b1f4e9426ad0bf2420874a
joerg <joerg@pkgsrc.org>
2006-11-03 17:31:12 +0000

    Regen b3.mk.

--- x11/libICE/buildlink3.mk
+++ x11/libICE/buildlink3.mk
@@ -1,22 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/11/03 17:27:30 joerg Exp $
-#
-# This Makefile fragment is included by packages that use libICE.
-#
-# This file was created automatically using createbuildlink-3.0.
-#
+# $NetBSD: buildlink3.mk,v 1.2 2006/11/03 17:31:12 joerg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBICE_BUILDLINK3_MK:=	${LIBICE_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	libICE
 .endif
 
-.if !empty(LIBICE_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			libICE
-BUILDLINK_API_DEPENDS.libICE+=		libICE>=0.99.1
-BUILDLINK_PKGSRCDIR.libICE?=		../../x11/libICE
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NlibICE}
+BUILDLINK_PACKAGES+=	libICE
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libICE
 
-.endif # LIBICE_BUILDLINK3_MK
+.if ${LIBICE_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.libICE+=	libICE>=0.99.1
+BUILDLINK_PKGSRCDIR.libICE?=	../../x11/libICE
+.endif	# LIBICE_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
