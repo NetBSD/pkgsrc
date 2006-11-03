f0dcf6cab703d0c999419d71bffaf39bf395461a
joerg <joerg@pkgsrc.org>
2006-11-03 21:18:51 +0000

    Import libXrender-0.9.2 from pkgsrc-wip: This package provides the X Render Library from the modular X.org X11 project.

---
+++ x11/libXrender/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/11/03 21:18:51 joerg Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBXRENDER_BUILDLINK3_MK:=	${LIBXRENDER_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	libXrender
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NlibXrender}
+BUILDLINK_PACKAGES+=	libXrender
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libXrender
+
+.if ${LIBXRENDER_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.libXrender+=	libXrender>=0.9.2
+BUILDLINK_PKGSRCDIR.libXrender?=	../../x11/libXrender
+.endif	# LIBXRENDER_BUILDLINK3_MK
+
+.include "../../x11/renderproto/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
