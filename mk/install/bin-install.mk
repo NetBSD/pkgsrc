de78f558a96771a025ac8a9d3b01fa9012219f20
joerg <joerg@pkgsrc.org>
2006-11-03 08:01:04 +0000

    Add package-install. For non-DESTDIR builds, package and package-install are identical. For DESTDIR builds, the package is not installed to PREFIX as part of the build, so package-install does exactly that after package is done. Change bin-install to call package-install.

--- mk/install/bin-install.mk
+++ mk/install/bin-install.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bin-install.mk,v 1.5 2006/10/03 11:07:05 rillig Exp $
+# $NetBSD: bin-install.mk,v 1.6 2006/11/03 08:01:04 joerg Exp $
 #
 
 # This file provides the following targets:
@@ -71,6 +71,6 @@ do-bin-install-from-source:
 		: "Nothing to do";					\
 	else								\
 		${STEP_MSG} "No binary package found for ${PKGNAME}; installing from source."; \
-		${RECURSIVE_MAKE} ${MAKEFLAGS} DEPENDS_TARGET=${DEPENDS_TARGET:Q} package \
+		${RECURSIVE_MAKE} ${MAKEFLAGS} DEPENDS_TARGET=${DEPENDS_TARGET:Q} package-install \
 		&& ${RECURSIVE_MAKE} ${MAKEFLAGS} clean;		\
 	fi
