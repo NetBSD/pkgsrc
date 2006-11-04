cce1b94f445225dc950ac50ef4aaf3dd14d8780b
salo <salo>
2006-11-04 22:23:14 +0000

    Pullup ticket 1873 - requested by adrianp security update for bugzilla
    Revisions pulled up:
    - pkgsrc/devel/bugzilla/MESSAGE				1.3
    - pkgsrc/devel/bugzilla/Makefile			1.20, 1.21
    - pkgsrc/devel/bugzilla/PLIST				1.10, 1.11
    - pkgsrc/devel/bugzilla/distinfo			1.11, 1.12
    - pkgsrc/devel/bugzilla/options.mk			1.2
    - pkgsrc/devel/bugzilla/files/localconfig		1.3
    
       Module Name:		pkgsrc
       Committed By:	adrianp
       Date:		Sun Oct 15 12:21:14 UTC 2006
    
       Modified Files:
       	pkgsrc/devel/bugzilla: MESSAGE Makefile PLIST distinfo options.mk
       	pkgsrc/devel/bugzilla/files: localconfig
    
       Log Message:
       Update to 2.22
    
       New features include:
       * Complete PostgreSQL Support
       * Parameters In Sections
       * One Codebase, Multiple Databases
       * UTF-8 for New Installations
       * Admins Can Impersonate Users
       * Bug Import and Moving Improvements
       * Adding Individual Bugs to Saved Searches
       * Attach URLs
       * Optional "Strict Isolation" for Groups
       * "editcomponents" Change
       * "shutdownhtml" Change
       * Miscellaneous Improvements
    
       For further details see:
       	http://www.bugzilla.org/releases/2.22/new-features.html
       	http://www.bugzilla.org/releases/2.22/release-notes.html
    ---
       Module Name:		pkgsrc
       Committed By:	adrianp
       Date:		Sun Oct 15 12:36:05 UTC 2006
    
       Modified Files:
       	pkgsrc/devel/bugzilla: Makefile PLIST distinfo
    
       Log Message:
       Update to 2.22.1
       + When sending mail, Bugzilla could throw the error "Insecure dependency in
       exec while running with -T switch" (bug 340538).
    
       + Using the public webdot server (for dependency graphs) should work
       again (bug 351243).
    
       + The "I'm added to or removed from this capacity" email preference
       wasn't working for new bugs (bug 349852).
    
       + The original release of 2.22 incorrectly said it required Template-Toolkit
       version 2.08. In actual fact, Bugzilla requires version 2.10 (bug 351478).
    
       + votes.cgi would crash if your bug was the one confirming a bug (bug 351300).
    
       + checksetup.pl now correctly reports if your Template::Plugin::GD module
       is missing. If missing, it could lead to charts and graphs not working
       (bug 345389).
    
       + The "Keyword" field on buglist.cgi was not sorted alphabetically, so
       it wasn't very useful for sorting (bug 342828).
    
       + Sendmail will no longer complain about there being a newline in the
       email address, when Bugzilla sends mail (bug 331365).
    
       + contrib/bzdbcopy.pl would try to insert an invalid value into the
       database, unnecessarily (bug 335572).
    
       + Deleting a bug now correctly deletes its attachments from the database
       (bug 339667).

--- devel/bugzilla/options.mk
+++ devel/bugzilla/options.mk
@@ -1,9 +1,38 @@
-# $NetBSD: options.mk,v 1.1.1.1 2005/01/02 13:39:55 adrianp Exp $
+# $NetBSD: options.mk,v 1.1.1.1.14.1 2006/11/04 22:23:14 salo Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.bugzilla
-PKG_SUPPORTED_OPTIONS=	ldap charts patchviewer xmlimportbugs
+
+PKG_OPTIONS_REQUIRED_GROUPS=	db
+PKG_OPTIONS_GROUP.db=		mysql pgsql
+
+PKG_OPTIONS_LEGACY_OPTS+=	patchviewer:bugzilla-patchviewer
+PKG_OPTIONS_LEGACY_OPTS+=	xmlimportbugs:bugzilla-xmlimportbugs
+PKG_OPTIONS_LEGACY_OPTS+=	charts:bugzilla-charts
+
+PKG_SUPPORTED_OPTIONS=	ldap bugzilla-charts bugzilla-patchviewer mysql pgsql
+PKG_SUPPORTED_OPTIONS+=	bugzilla-xmlimportbugs bugzilla-imagemagick
+PKG_SUGGESTED_OPTIONS=	mysql
 
 .include "../../mk/bsd.options.mk"
+
+###
+### Use mysql or postgresql backend
+###
+.if !empty(PKG_OPTIONS:Mmysql)
+DEPENDS+=	p5-DBD-mysql>=2.9003:../../databases/p5-DBD-mysql
+DBDRIVER=	mysql
+.elif !empty(PKG_OPTIONS:Mpgsql)
+DEPENDS+=	p5-DBD-postgresql>=1.45:../../databases/p5-DBD-postgresql
+DBDRIVER=	pgsql
+.endif
+
+###
+### If you want to convert BMP image attachments to PNG to conserve
+### disk space.
+###
+.if !empty(PKG_OPTIONS:Mbugzilla-imagemagick)
+DEPENDS+=	p5-PerlMagick-[0-9]*:../../graphics/p5-PerlMagick
+.endif
 
 ###
 ### Use OpenLDAP for authentication
@@ -15,7 +44,7 @@ DEPENDS+=	p5-perl-ldap-[0-9]*:../../databases/p5-perl-ldap
 ###
 ### Generation of charts
 ###
-.if !empty(PKG_OPTIONS:Mcharts)
+.if !empty(PKG_OPTIONS:Mbugzilla-charts)
 DEPENDS+=	gd>=1.20:../../graphics/gd
 DEPENDS+=	p5-Chart>=1.0:../../graphics/p5-Chart
 DEPENDS+=	p5-GDTextUtil-[0-9]*:../../graphics/p5-GDTextUtil
@@ -25,7 +54,7 @@ DEPENDS+=	p5-GDGraph-[0-9]*:../../graphics/p5-GDGraph
 ###
 ### Patchviewer support
 ###
-.if !empty(PKG_OPTIONS:Mpatchviewer)
+.if !empty(PKG_OPTIONS:Mbugzilla-patchviewer)
 DEPENDS+=	p5-PatchReader>=0.9.4:../../devel/p5-PatchReader
 DEPENDS+=	patchutils-[0-9]*:../../devel/patchutils
 .endif
@@ -33,6 +62,6 @@ DEPENDS+=	patchutils-[0-9]*:../../devel/patchutils
 ###
 ### Add support for bulk import/export of bugs in XML format
 ###
-.if !empty(PKG_OPTIONS:Mxmlimportbugs)
-DEPENDS+=	p5-XML-Parser-[0-9]*:../../textproc/p5-XML-Parser
+.if !empty(PKG_OPTIONS:Mbugzilla-xmlimportbugs)
+DEPENDS+=	p5-XML-Twig-[0-9]*:../../textproc/p5-XML-Twig
 .endif
