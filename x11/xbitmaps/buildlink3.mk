838be487fe296a5ba54710dca20538063c7fb1e2
joerg <joerg@pkgsrc.org>
2006-11-06 10:29:33 +0000

    Redirect to mk/x11.b3.mk for !modular case.

--- x11/xbitmaps/buildlink3.mk
+++ x11/xbitmaps/buildlink3.mk
@@ -1,4 +1,10 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/11/03 19:41:40 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2006/11/06 10:29:33 joerg Exp $
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${X11_TYPE} != "modular"
+.include "../../mk/x11.buildlink3.mk"
+.else
 
 BUILDLINK_DEPMETHOD.xbitmaps?=	build
 
@@ -19,3 +25,5 @@ BUILDLINK_PKGSRCDIR.xbitmaps?=	../../x11/xbitmaps
 .endif	# XBITMAPS_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
+
+.endif
