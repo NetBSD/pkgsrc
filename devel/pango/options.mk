52b09b2dc11854c6942bc055a8db52d1df7e7492
joerg <joerg>
2006-11-06 11:24:26 +0000

    Switch to modular buildlinks.

--- devel/pango/options.mk
+++ devel/pango/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2006/07/24 23:20:50 minskim Exp $
+# $NetBSD: options.mk,v 1.3 2006/11/06 11:24:26 joerg Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pango
 PKG_SUPPORTED_OPTIONS=	x11
@@ -15,10 +15,11 @@ PKG_SUGGESTED_OPTIONS=	x11
 CONFIGURE_ENV+=	ac_cv_lib_cairo_cairo_atsui_font_face_create_for_atsu_font_id=no
 PLIST_SUBST+=		CARBON="@comment "
 PLIST_SUBST+=		X11=""
-BUILDLINK_API_DEPENDS.Xrender+=	Xrender>=0.8
-.include "../../fonts/Xft2/buildlink3.mk"
-.include "../../x11/Xrender/buildlink3.mk"
-.include "../../mk/x11.buildlink3.mk"
+.include "../../x11/libXft/buildlink3.mk"
+.include "../../x11/libXrender/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+BUILDLINK_DEPMETHOD.libXt?=	build # only for configure
+.include "../../x11/libXt/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-x
 PLIST_SUBST+=		X11="@comment "
