af44755e0773950ab8770a4cee0fa87c8fbbdb63
joerg <joerg@pkgsrc.org>
2006-11-06 11:28:02 +0000

    Switch to modular buildlinks.

--- graphics/glitz/options.mk
+++ graphics/glitz/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2006/07/23 03:42:43 minskim Exp $
+# $NetBSD: options.mk,v 1.2 2006/11/06 11:28:02 joerg Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.glitz
 PKG_SUPPORTED_OPTIONS=	x11
@@ -13,7 +13,9 @@ PKG_SUGGESTED_OPTIONS=	x11
 LIBS.Darwin+=	-lXext
 PLIST_SUBST+=	X11=""
 .include "../../graphics/MesaLib/buildlink3.mk"
-.include "../../mk/x11.buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+BUILDLINK_DEPMETHOD.libXt?=	build # only for configure
+.include "../../x11/libXt/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-glx
 PLIST_SUBST+=	X11="@comment "
