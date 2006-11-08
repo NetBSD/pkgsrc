0a9f38d517b22c333a0073ef443bb3835bc2ebca
joerg <joerg@pkgsrc.org>
2006-11-08 13:17:37 +0000

    Regen.

--- x11/libxkbfile/buildlink3.mk
+++ x11/libxkbfile/buildlink3.mk
@@ -1,16 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/11/07 17:22:27 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2006/11/08 13:17:37 joerg Exp $
+#
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBXKBFILE_BUILDLINK3_MK:=	${LIBXKBFILE_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	libxkbfile
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibxkbfile}
 BUILDLINK_PACKAGES+=	libxkbfile
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libxkbfile
 
-.if !empty(LIBXKBFILE_BUILDLINK3_MK:M+)
+.if ${LIBXKBFILE_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.libxkbfile+=	libxkbfile>=0.99.1
 BUILDLINK_PKGSRCDIR.libxkbfile?=	../../x11/libxkbfile
 .endif	# LIBXKBFILE_BUILDLINK3_MK
@@ -18,4 +20,4 @@ BUILDLINK_PKGSRCDIR.libxkbfile?=	../../x11/libxkbfile
 .include "../../x11/kbproto/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
