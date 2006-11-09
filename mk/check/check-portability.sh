bcae005180c58cef099c9e35d9b706d45cce809d
rillig <rillig@pkgsrc.org>
2006-11-09 14:36:18 +0000

    Rewrote check-portability in awk instead of shell, since the shell has a huge performance problem: When reading files, it calls read(2) for every single byte. awk instead reads a whole line at a time. For the lang/php5 package, the execution time changed from (7.8 real 4.5 user 3.1 sys) to (1.6 real 1.5 user 0.4 sys).

--- mk/check/check-portability.sh
+++ mk/check/check-portability.sh
@@ -1,4 +1,4 @@
-# $NetBSD: check-portability.sh,v 1.2 2006/11/09 10:52:21 rillig Exp $
+# $NetBSD: check-portability.sh,v 1.3 2006/11/09 14:36:18 rillig Exp $
 #
 # This program checks the extracted files for portability issues that
 # are likely to result in false assumptions by the package.
@@ -17,60 +17,22 @@ found_test_eqeq=no
 
 # usage: check_shell <fname>
 check_shell() {
-	# See the end of the loop for the redirection.
-	while read line; do
-
-		# Note: This code does not find _all_ instances of
-		# unportable code. If a single line contains an unsafe and
-		# a safe usage of $RANDOM, it will pass the test.
-
-		# Strip comments.
-		# Note: this has the side-effect that the # in $# is also
-		# regarded as a comment.
-		line="${line%%#*}"
-
-		# Check for $RANDOM, which is specific to ksh and bash.
-		case "$line" in
-		*"\$\$-\$RANDOM"* \
-		| *"\$RANDOM-\$\$"* \
-		| *"\$RANDOM"[A-Z_]*)
-			# When $RANDOM is prefixed by the process ID, it
-			# doesn't matter too much if $RANDOM is empty.
-			# This code is often found in GNU configure scripts.
-			;;
-
-		*\$RANDOM*)
-			found_random=yes
-			cs_warning_heading "Found \$RANDOM:"
-			cs_warning_msg "$fname: $line"
-			;;
-		esac
-
-		#
-		# Split the line into words and check them.
-		#
-		set args $line; shift
-		while [ $# -ge 3 ]; do
-			case "$1" in
-			"test" | "[")
-				if [ "==" = "$3" ]; then
-					found_test_eqeq=yes
-					cs_error_heading "Found test ... == ...:"
-					cs_error_msg "$fname: $line"
-				fi
-				;;
-			esac
-			shift
-		done
-
-	done < "$1"
+	env \
+		CK_FNAME="$1" \
+		CK_PROGNAME="check-portability.awk" \
+		awk	-f "$PKGSRCDIR/mk/check/check-subr.awk" \
+			-f "$PKGSRCDIR/mk/check/check-portability.awk" \
+		< "$1" \
+	|| cs_exitcode=1
 }
 
 find * -type f -print 2>/dev/null \
 | {
 	while read fname; do
 
-		eval "case \"\$fname\" in $SKIP_FILTER *.orig) continue;; esac"
+		skip=no
+		eval "case \"\$fname\" in $SKIP_FILTER *.orig) skip=yes;; esac"
+		[ $skip = no ] || continue
 
 		read firstline < "$fname" || continue
 		case "$firstline" in
@@ -79,33 +41,6 @@ find * -type f -print 2>/dev/null \
 			;;
 		esac
 	done
-
-	if [ $found_random = yes ]; then
-		cs_explain <<EOF
-The variable \$RANDOM is not required for a POSIX-conforming shell, and
-many implementations of /bin/sh do not support it. It should therefore
-not be used in shell programs that are meant to be portable across a
-large number of POSIX-like systems.
-EOF
-	fi
-
-	if [ $found_test_eqeq = yes ]; then
-		cs_explain <<EOF
-The "test" command, as well as the "[" command, are not required to know
-the "==" operator. Only a few implementations like bash and some
-versions of ksh support it.
-
-When you run "test foo == foo" on a platform that does not support the
-"==" operator, the result will be "false" instead of "true". This can
-lead to unexpected behavior.
-
-There are two ways to fix this error message. If the file that contains
-the "test ==" is needed for building the package, you should create a
-patch for it, replacing the "==" operator with "=". If the file is not
-needed, add its name to the CHECK_PORTABILITY_SKIP variable in the
-package Makefile.
-EOF
-	fi
 
 	cs_exit
 }
