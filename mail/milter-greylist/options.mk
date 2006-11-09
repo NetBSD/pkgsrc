f709ac7504e6dc270e5e3c65bbc157ad0d3ce142
ghen <ghen>
2006-11-09 13:15:10 +0000

    Fix typo.

--- mail/milter-greylist/options.mk
+++ mail/milter-greylist/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2006/10/12 13:14:43 tron Exp $
+# $NetBSD: options.mk,v 1.2 2006/11/09 13:15:10 ghen Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.milter-greylist
 PKG_SUPPORTED_OPTIONS=		dnsrbl drac spf
@@ -7,7 +7,7 @@ PKG_SUGGESTED_OPTIONS=		dnsrbl spf
 .include "../../mk/bsd.options.mk"
 
 ###
-### RNS Realtime Black List
+### DNS Realtime Black List
 ###
 .if empty(PKG_OPTIONS:Mdnsrbl)
 CONFIGURE_ARGS+=	--disable-dnsrbl
