20b909b37a6bd803cef6826435ddb0580cb61bf6
rillig <rillig@pkgsrc.org>
2006-11-09 10:31:54 +0000

    Added another sentence that explains how to disable this check for individual files.

--- mk/check/check-headers.sh
+++ mk/check/check-headers.sh
@@ -1,4 +1,4 @@
-# $NetBSD: check-headers.sh,v 1.3 2006/11/09 10:26:50 rillig Exp $
+# $NetBSD: check-headers.sh,v 1.4 2006/11/09 10:31:54 rillig Exp $
 #
 # This program checks the header files for possible problems.
 #
@@ -46,26 +46,29 @@ find * -type f -print 2>/dev/null \
 
 	if [ $found_unresolved_variable = yes ]; then
 		cat 1>&2 <<EOF
-
 ===========================================================================
-The above macros probably contain references to shell variables.
+The above macros may contain references to shell variables.
 
 The cause of this problem is usually that in a configure.ac or
 configure.in file, there is some code like
 
-    FOO_DIR=\"\${bindir}\"
-    AC_DEFINE_UNQUOTED(FOO_DIR, \"\$FOO_DIR\", [Directory where foo files go])
+    FOO_DIR="\${bindir}"
+    AC_DEFINE_UNQUOTED(FOO_DIR, "\$FOO_DIR", [Directory where foo files go])
 
 You can fix this by telling the original package author not to use
 AC_DEFINE_UNQUOTED for directories. Instead, {he,she} should do
 something like this:
 
     # in configure.ac:
-    foodir=\"\${bindir}\"
+    foodir="\${bindir}"
     AC_SUBST(FOO_DIR)
 
     # in the Makefile.am files (can be more than one):
-    AM_CPPFLAGS= -DFOO_DIR=\\\"@FOO_DIR@\\\"
+    AM_CPPFLAGS= -DFOO_DIR=\\"@FOO_DIR@\\"
+
+If this check is wrong and the package really wants to have "\${" in the
+macros, append the above filenames to the CHECK_HEADERS_SKIP variable in
+the package Makefile.
 ===========================================================================
 EOF
 
