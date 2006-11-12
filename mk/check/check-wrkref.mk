91230d2d7cd1a281df7589ede467194068ebf6e5
rillig <rillig@pkgsrc.org>
2006-11-12 00:13:26 +0000

    Moved the check for whether check-wrkref should be run from install/install.mk to check/check-wrkref.mk.
    Renamed the check-wrkref target to _check-wrkref.

--- mk/check/check-wrkref.mk
+++ mk/check/check-wrkref.mk
@@ -1,4 +1,4 @@
-# $NetBSD: check-wrkref.mk,v 1.9 2006/11/12 00:09:50 rillig Exp $
+# $NetBSD: check-wrkref.mk,v 1.10 2006/11/12 00:13:26 rillig Exp $
 #
 # This file checks that the installed files don't contain any strings
 # that point to the directory where the package had been built, to make
@@ -42,7 +42,11 @@ PKG_FAIL_REASON+=	"[check-wrkref.mk] CHECK_WRKREF must be one of { no tools work
 .endif
 _CHECK_WRKREF_DIR=	${_CHECK_WRKREF_DIR.${CHECK_WRKREF}}
 
-check-wrkref: error-check .PHONY
+.if empty(CHECK_WRKREF:M[nN][oO])
+privileged-install-hook: _check-wrkref
+.endif
+
+_check-wrkref: error-check .PHONY
 	@${STEP_MSG} "Checking for work-directory references in ${PKGNAME}"
 	${RUN} rm -f ${ERROR_DIR}/${.TARGET}
 	${_PKG_SILENT}${_PKG_DEBUG}					\
