d80f2c9c00e13e9f03046b60485dab3a10eb785f
wulf <wulf>
2006-11-13 16:59:22 +0000

    Updated gnuradio-core to 3.0.2: GNU Radio Core has seen the addition of many new features and bug fixes of which some are listed below:
    	* bugfix in multiple stream support
    	* addition of AGC to GMSK code
    	* new accessor for threshold
    	* new bits_per_baud parameter to handle QPSK
    	* new function to enable realtime scheduling
    	* new set_sample_rate method
    	* new set_n method
    	* new differential decoder
    	* new general purpose block for searching for access code
    
    For more details see gnuradio-core/ChangeLog file

--- ham/gnuradio-core/buildlink3.mk
+++ ham/gnuradio-core/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/07/08 23:10:53 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2006/11/13 16:59:22 wulf Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNURADIO_CORE_BUILDLINK3_MK:=	${GNURADIO_CORE_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	gnuradio-core
 .endif
 
@@ -11,17 +11,18 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngnuradio-core}
 BUILDLINK_PACKAGES+=	gnuradio-core
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gnuradio-core
 
-.if !empty(GNURADIO_CORE_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.gnuradio-core+=	gnuradio-core>=2.8
+.if ${GNURADIO_CORE_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.gnuradio-core+=	gnuradio-core>=3.0.2
 BUILDLINK_PKGSRCDIR.gnuradio-core?=	../../ham/gnuradio-core
+BUILDLINK_CPPFLAGS.gnuradio-core+=    -I${BUILDLINK_PREFIX.gnuradio-core}/include/gnuradio
 .endif	# GNURADIO_CORE_BUILDLINK3_MK
 
 .include "../../devel/boost-headers/buildlink3.mk"
 .include "../../devel/cppunit/buildlink3.mk"
 .include "../../devel/swig/buildlink3.mk"
-.include "../../lang/python/pyversion.mk"
-.include "../../lang/python/extension.mk"
 .include "../../lang/python/application.mk"
+.include "../../lang/python/extension.mk"
+.include "../../lang/python/pyversion.mk"
 .include "../../math/fftwf/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
