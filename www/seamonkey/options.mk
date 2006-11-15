8e9344d340c0c6e4b67afa901adf784449218362
martin <martin@pkgsrc.org>
2006-11-15 15:28:21 +0000

    Explicitly set --enable-debug if we have the "debug" option. Otherwise at least the firefox2 configure script will default to non-debug mode.

--- www/seamonkey/options.mk
+++ www/seamonkey/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2006/10/03 12:09:14 ghen Exp $
+# $NetBSD: options.mk,v 1.4 2006/11/15 15:28:21 martin Exp $
 
 PKG_OPTIONS_VAR		= PKG_OPTIONS.gecko
 PKG_SUPPORTED_OPTIONS	= debug
@@ -13,6 +13,8 @@ PKG_SUPPORTED_OPTIONS  += official-mozilla-branding
 # so it is correct
 .if empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--disable-debug
+.else
+CONFIGURE_ARGS+=	--enable-debug
 .endif
 
 # Enable Official mozilla.org Branding for Firefox or Thunderbird.
