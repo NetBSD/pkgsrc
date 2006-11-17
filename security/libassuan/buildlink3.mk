7d5738da93a2e0c9645b6c6bc6d8b580f46658c6
shannonjr <shannonjr@pkgsrc.org>
2006-11-17 12:34:47 +0000

    Update to 1.0.0.
    There are no actual code changes since the last release, only some
    cleanups and a complete manual.

--- security/libassuan/buildlink3.mk
+++ security/libassuan/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/07/08 23:11:06 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2006/11/17 12:34:47 shannonjr Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBASSUAN_BUILDLINK3_MK:=	${LIBASSUAN_BUILDLINK3_MK}+
@@ -12,8 +12,8 @@ BUILDLINK_PACKAGES+=	libassuan
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libassuan
 
 .if !empty(LIBASSUAN_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.libassuan+=	libassuan>=0.6.7
-BUILDLINK_ABI_DEPENDS.libassuan?=	libassuan>=0.6.10nb2
+BUILDLINK_API_DEPENDS.libassuan+=	libassuan>=1.0.0
+BUILDLINK_ABI_DEPENDS.libassuan?=	libassuan>=1.0.0
 BUILDLINK_PKGSRCDIR.libassuan?=	../../security/libassuan
 .endif	# LIBASSUAN_BUILDLINK3_MK
 
