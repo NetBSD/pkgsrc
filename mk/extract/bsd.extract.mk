3a7dfd86a94d6beb30e53f2908c8e8deadd0a389
rillig <rillig@pkgsrc.org>
2006-11-20 00:03:22 +0000

    Added better documentation for pre-extract, do-extract and post-extract. They are found by "make help" now.

--- mk/extract/bsd.extract.mk
+++ mk/extract/bsd.extract.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.extract.mk,v 1.1 2006/06/06 03:05:48 jlam Exp $
+# $NetBSD: bsd.extract.mk,v 1.2 2006/11/20 00:03:22 rillig Exp $
 #
 # This Makefile fragment is included to bsd.pkg.mk and defines the
 # relevant variables and targets for the "extract" phase.
@@ -9,7 +9,25 @@
 #
 # The following targets may be overridden in a package Makefile:
 #
-#    pre-extract, do-extract, post-extract
+# pre-extract:
+#	Can be used to prepare the distfiles for extraction.
+#	For binary-only packages, this can mean to make the distfile
+#	executable.
+#
+#	Default action: Nothing.
+#
+# do-extract:
+#	This target does the "real" work of extracting the distfiles.
+#
+#	Default action: Extracts the files listed in EXTRACT_ONLY into
+#	WRKDIR.
+#
+# post-extract:
+#	Can be used to remove unneeded files that have been extracted.
+#	Often, additional files from FILESDIR are copied to WRKSRC here,
+#	too.
+#
+#	Default action: Nothing.
 #
 
 .include "${PKGSRCDIR}/mk/extract/extract.mk"
