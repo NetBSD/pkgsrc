6f6ef3c4056bff38f265777eccf8a2cac8109ed6
reed <reed@pkgsrc.org>
2006-11-25 00:09:19 +0000

    Include the xproto buildlink.
    I forgot in last commit. The xau.pc.in has "Requires: xproto".

--- x11/libXau/buildlink3.mk
+++ x11/libXau/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/11/25 00:02:08 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2006/11/25 00:09:19 reed Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBXAU_BUILDLINK3_MK:=	${LIBXAU_BUILDLINK3_MK}+
@@ -15,5 +15,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libXau
 BUILDLINK_API_DEPENDS.libXau+=	libXau>=1.0
 BUILDLINK_PKGSRCDIR.libXau?=	../../x11/libXau
 .endif	# LIBXAU_BUILDLINK3_MK
+
+.include "../../x11/xproto/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
