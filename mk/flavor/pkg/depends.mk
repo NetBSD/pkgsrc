add126d14444b7567d5227cd3b17b41d17aa15ac
rillig <rillig@pkgsrc.org>
2006-11-26 12:21:13 +0000

    Oops, forgot to remove a line continuation.

--- mk/flavor/pkg/depends.mk
+++ mk/flavor/pkg/depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: depends.mk,v 1.15 2006/11/26 11:51:30 rillig Exp $
+# $NetBSD: depends.mk,v 1.16 2006/11/26 12:21:13 rillig Exp $
 
 _DEPENDS_FILE=		${WRKDIR}/.depends
 _REDUCE_DEPENDS_CMD=	${SETENV} CAT=${CAT:Q}				\
@@ -52,7 +52,7 @@ ${_DEPENDS_FILE}:
 		${TEST} -n "$$dir" || exit 1;				\
 		${ECHO} "build	$$pattern	$$dir";			\
 	done >> ${.TARGET}
-	${RUN} ${_REDUCE_DEPENDS_CMD} ${DEPENDS:Q} > ${.TARGET}.tmp	\
+	${RUN} ${_REDUCE_DEPENDS_CMD} ${DEPENDS:Q} > ${.TARGET}.tmp
 	${RUN}								\
 	exec 0< ${.TARGET}.tmp;						\
 	while read dep; do						\
