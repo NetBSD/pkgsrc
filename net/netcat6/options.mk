900e17d866f069f934da36b61f961201f01c8ace
tv <tv@pkgsrc.org>
2006-11-28 16:28:55 +0000

    You *can't* disable IPv6 in netcat6; require it via PKG_OPTIONS instead.

--- net/netcat6/options.mk
+++ net/netcat6/options.mk
@@ -1,10 +1,7 @@
-# $NetBSD: options.mk,v 1.1.1.1 2005/12/28 15:49:41 ghen Exp $
+# $NetBSD: options.mk,v 1.2 2006/11/28 16:28:55 tv Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.netcat6
-PKG_SUPPORTED_OPTIONS=	inet6
+PKG_OPTIONS_VAR=		PKG_OPTIONS.netcat6
+PKG_OPTIONS_REQUIRED_GROUPS=	inet6 # require inet6 capability
+PKG_OPTIONS_GROUP.inet6=	inet6
 
 .include "../../mk/bsd.options.mk"
-
-.if empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGS+=	--disable-ipv6
-.endif
