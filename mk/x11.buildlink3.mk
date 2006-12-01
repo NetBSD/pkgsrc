19f594fed7ed99acf7a31ead114345c8922ee543
joerg <joerg@pkgsrc.org>
2006-12-01 13:05:53 +0000

    Proper include guards to unbreak nautilus. This wasn't such a big problem before, but the move to Xorg increased the including of this file a lot.

--- mk/x11.buildlink3.mk
+++ mk/x11.buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: x11.buildlink3.mk,v 1.6 2006/11/17 09:50:54 rillig Exp $
+# $NetBSD: x11.buildlink3.mk,v 1.7 2006/12/01 13:05:53 joerg Exp $
 #
 # This Makefile fragment is meant to be included by packages that
 # require an X11 distribution.  x11.buildlink3.mk will include the
@@ -7,7 +7,9 @@
 
 X11_BUILDLINK3_MK:=	${X11_BUILDLINK3_MK}+
 
-.if ${X11_BUILDLINK3_MK} == "+"
+.if !defined(_X11_BUILDLINK3_MK)
+_X11_BUILDLINK3_MK=	1
+
 USE_X11=	yes
 
 .  include "../../mk/bsd.prefs.mk"
@@ -32,7 +34,7 @@ CONFIGURE_ARGS+=	--x-libraries=${X11BASE:Q}/lib${LIBABISUFFIX:Q}
 
 X11_LDFLAGS+=	${COMPILER_RPATH_FLAG}${X11BASE}/lib${LIBABISUFFIX}
 X11_LDFLAGS+=	-L${X11BASE}/lib${LIBABISUFFIX}
-.endif	# X11_BUILDLINK3_MK
+.endif	# _X11_BUILDLINK3_MK
 
 .include "${X11_PKGSRCDIR.${X11_TYPE}}/buildlink3.mk"
 
