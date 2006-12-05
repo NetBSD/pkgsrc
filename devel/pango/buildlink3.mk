778159bc2fd364461c9d37ae1f8ee750624e0731
minskim <minskim@pkgsrc.org>
2006-12-05 06:45:00 +0000

    Do not include buildlink3.mk of packages not exposed by this package.

--- devel/pango/buildlink3.mk
+++ devel/pango/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2006/11/06 11:24:26 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2006/12/05 06:45:00 minskim Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PANGO_BUILDLINK3_MK:=	${PANGO_BUILDLINK3_MK}+
@@ -29,11 +29,9 @@ MAKEVARS+=	PKG_BUILD_OPTIONS.pango
 
 .if !empty(PKG_BUILD_OPTIONS.pango:Mx11)
 .include "../../x11/libXft/buildlink3.mk"
-.include "../../x11/libXrender/buildlink3.mk"
 .endif
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/zlib/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../graphics/cairo/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
