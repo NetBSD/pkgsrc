b7cdf7c58e862c612f9baf24620b32d963f865ea
tron <tron@pkgsrc.org>
2006-12-05 17:43:06 +0000

    - Fix broken shared library generation under Mac OS X. - Fix permission problems in "include/nspr". Bump package revision because of these changes.

--- devel/nspr/buildlink3.mk
+++ devel/nspr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2006/07/08 23:10:47 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2006/12/05 17:43:06 tron Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 NSPR_BUILDLINK3_MK:=	${NSPR_BUILDLINK3_MK}+
@@ -13,7 +13,11 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}nspr
 
 .if !empty(NSPR_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.4.1nb1
+.if ${OPSYS} == "Darwin"
+BUILDLINK_ABI_DEPENDS.nspr+=	nspr>=4.6.1nb3
+.else
 BUILDLINK_ABI_DEPENDS.nspr+=	nspr>=4.6.1nb1
+.endif
 BUILDLINK_PKGSRCDIR.nspr?=	../../devel/nspr
 
 BUILDLINK_FILES.nspr+=		lib/nspr/*
