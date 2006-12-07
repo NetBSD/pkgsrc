0613ea25418eec0e190906b43f9d3d622ddc1fba
markd <markd@pkgsrc.org>
2006-12-07 10:07:10 +0000

    Slight tidyup of the KDE handling.  No functional change.

--- multimedia/gnash/options.mk
+++ multimedia/gnash/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2006/12/06 00:13:05 xtraeme Exp $
+# $NetBSD: options.mk,v 1.2 2006/12/07 10:07:10 markd Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnash
@@ -9,8 +9,7 @@ PKG_SUPPORTED_OPTIONS=	klash
 .if !empty(PKG_OPTIONS:Mklash)
 PLIST_SRC+=		${PKGDIR}/PLIST.klash
 .include "../../x11/kdebase3/buildlink3.mk"
-.include "../../x11/qt3-libs/buildlink3.mk"
-.include "../../x11/qt3-tools/buildlink3.mk"
+.include "../../meta-pkgs/kde3/kde3.mk"
 CONFIGURE_ARGS+=	\
 	--with-plugindir=${PREFIX}/share/gnash/plugins
 CONFIGURE_ARGS+=	--enable-klash
