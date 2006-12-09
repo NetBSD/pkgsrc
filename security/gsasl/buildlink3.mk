4f3baa032da793cff3e615f11a45045fad662b62
obache <obache>
2006-12-09 14:06:13 +0000

    Update gsasl to 0.2.15, based on patch provided by PR 33638.
    * Version 0.2.15 (released 2006-08-22)
    
    ** Changed libgsasl shared library version.
    The shared library version was not incremented correctly in the last
    release, even though new APIs were added.
    
    * Version 0.2.14 (released 2006-08-19)
    
    ** New section "Requirements" in the manual, lists the external components.
    Suggested by James Mansion.
    
    ** Update of gnulib files.
    
    * Version 0.2.13 (released 2006-06-14)
    
    ** Update of gnulib files.
    Further improves portability to MinGW.
    
    ** Various improvements in the manuals.
    
    ** The tests are run under valgrind, if it is installed.
    Use --disable-valgrind-tests to unconditionally disable this.  It is
    disabled by default for cross compiles.
    
    ** Various minor fixes.
    
    * Version 0.2.12 (released 2006-03-08)
    
    ** Update of gnulib files.
    Improves portability to Mingw32.
    
    * Version 0.2.11 (released 2006-02-07)
    
    ** Ported to Windows by cross-compiling using Mingw32.
    Using Debian's mingw32 compiler, you can build it for Windows by invoking
    `./configure --host=i586-mingw32msvc --disable-gssapi'.
    
    ** Update of gnulib files.
    
    * Version 0.2.10 (released 2005-10-23)
    
    ** Work around bug in GnuTLS that made the command line tool exit after
    ** failing to write a zero length message to the peer.
    
    ** Don't use GnuTLS if gnutls_certificate_verify_peers2 isn't present.
    
    ** Update of gnulib files.
    
    * Version 0.2.9 (released 2005-10-07)
    
    ** Update of gnulib files.
    
    * Version 0.2.8 (released 2005-09-08)
    
    ** The gsasl tool now support STARTTLS for IMAP and SMTP using GnuTLS.
    
    ** The --client and --server parameters for the gsasl tool now work properly.
    
    ** The --client and --server stdin/stdout modes now use the readline library.
    
    ** Fixed build problems in getpass on uClibc and Mingw32 platforms.
    
    ** Kinyarwanda translation added.
    
    * Version 0.2.7 (released 2005-08-25)
    
    ** Fix build problems when cross-compiling to uClibc and Mingw32 platforms.
    
    ** Detecting and using the readline library has been improved.
    
    * Version 0.2.6 (released 2005-08-08)
    
    ** The gsasl tool now try to connect to all addresses for a server name.
    
    ** The help-gsasl@gnu.org mailing list is now mentioned in documentation.
    
    ** The license template in files were updated with the new FSF address.
    
    ** Update of gnulib files.

--- security/gsasl/buildlink3.mk
+++ security/gsasl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/07/08 23:11:06 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2006/12/09 14:06:13 obache Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GSASL_BUILDLINK3_MK:=	${GSASL_BUILDLINK3_MK}+
@@ -12,15 +12,14 @@ BUILDLINK_PACKAGES+=	gsasl
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gsasl
 
 .if !empty(GSASL_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.gsasl+=	gsasl>=0.2.3
-BUILDLINK_ABI_DEPENDS.gsasl?=	gsasl>=0.2.5nb1
+BUILDLINK_API_DEPENDS.gsasl+=	gsasl>=0.2.15
 BUILDLINK_PKGSRCDIR.gsasl?=	../../security/gsasl
 .endif	# GSASL_BUILDLINK3_MK
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/libidn/buildlink3.mk"
 .include "../../devel/libntlm/buildlink3.mk"
-.include "../../security/gss/buildlink3.mk"
 .include "../../security/libgcrypt/buildlink3.mk"
+.include "../../mk/krb5.buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
