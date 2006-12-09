9056acbc33adab9e19160bccc178fa27372435e8
adam <adam@pkgsrc.org>
2006-12-09 15:34:13 +0000

    Changes 8.2.0: * Query language enhancements including "INSERT/UPDATE/DELETE   RETURNING", multirow VALUES lists, and optional target-table alias   in "UPDATE"/"DELETE" * Index creation without blocking concurrent   "INSERT"/"UPDATE"/"DELETE" operations * Many query optimization improvements, including support for   reordering outer joins * Improved sorting performance with lower memory usage * More efficient locking with better concurrency * More efficient vacuuming * Easier administration of warm standby servers * New FILLFACTOR support for tables and indexes * Monitoring, logging, and performance tuning additions * More control over creating and dropping objects * Table inheritance relationships can be defined for and removed from   pre-existing tables * "COPY TO" can copy the output of an arbitrary "SELECT" statement * Array improvements, including nulls in arrays * Aggregate-function improvements, including multiple-input   aggregates and SQL:2003 statistical functions * Many "contrib/" improvements

---
+++ databases/postgresql82/files/dynloader-ltdl.h
@@ -0,0 +1,17 @@
+/* $NetBSD: dynloader-ltdl.h,v 1.1.1.1 2006/12/09 15:34:13 adam Exp $ */
+
+/*
+ * dynamic loader based on libltdl
+ */
+#ifndef PORT_PROTOS_H
+#define PORT_PROTOS_H
+
+#include <ltdl.h>
+#include "utils/dynamic_loader.h"
+
+#define pg_dlopen(a)		((void *)lt_dlopen(a))
+#define pg_dlsym(a,b)		lt_dlsym((lt_dlhandle)(a), (b))
+#define pg_dlclose(a)		lt_dlclose((lt_dlhandle)(a))
+#define pg_dlerror		lt_dlerror
+
+#endif   /* PORT_PROTOS_H */
