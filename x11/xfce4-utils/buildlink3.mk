fc1aec41b4dd5c2f526e5bf4695c98c8eb25dfeb
martti <martti@pkgsrc.org>
2006-12-12 06:19:33 +0000

    Use correct int size to avoid crash in sparc64. Patch from Thierry Supplisson.
    PKGREVISION++

--- x11/xfce4-utils/buildlink3.mk
+++ x11/xfce4-utils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2006/10/12 09:24:21 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2006/12/12 06:19:33 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XFCE4_UTILS_BUILDLINK3_MK:=	${XFCE4_UTILS_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	xfce4-utils
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-utils
 
 .if ${XFCE4_UTILS_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.xfce4-utils+=	xfce4-utils>=4.2.3nb5
+BUILDLINK_API_DEPENDS.xfce4-utils+=	xfce4-utils>=4.2.3nb6
 BUILDLINK_PKGSRCDIR.xfce4-utils?=	../../x11/xfce4-utils
 .endif	# XFCE4_UTILS_BUILDLINK3_MK
 
