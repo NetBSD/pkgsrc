e0823e516d692f6e3ac2ec04cb498079fc4b57e6
martin <martin@pkgsrc.org>
2006-12-15 16:01:16 +0000

    Do not force useage of "rxvt" as $TERM by default, but make it an option. Also add a hint to the runtime override. Fixes PR pkg/34605.

--- x11/rxvt-unicode/options.mk
+++ x11/rxvt-unicode/options.mk
@@ -1,33 +1,36 @@
 #
-# $Id: options.mk,v 1.1 2006/12/15 15:01:30 martin Exp $
+# $Id: options.mk,v 1.2 2006/12/15 16:01:16 martin Exp $
 #
 
 PKG_OPTIONS_VAR=        PKG_OPTIONS.rxvt-unicode
-PKG_SUPPORTED_OPTIONS=  perl unicode3 xft2
+PKG_SUPPORTED_OPTIONS=  perl unicode3 xft2 rxvt-term
 PKG_SUGGESTED_OPTIONS=  perl unicode3 xft2
 
 .include "../../mk/bsd.options.mk"
 
+.if !empty(PKG_OPTIONS:Mrxvt-term)
+CONFIGURE_ARGS+=	--with-term=rxvt
+.endif
+
 .if !empty(PKG_OPTIONS:Mperl)
-CONFIGURE_ARGS+=        --enable-perl
-PLIST_SUBST+=           PERL=
+CONFIGURE_ARGS+=	--enable-perl
+PLIST_SUBST+=		PERL=
 .include "../../lang/perl5/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=        --disable-perl
-PLIST_SUBST+=           PERL='@comment '
+CONFIGURE_ARGS+=	--disable-perl
+PLIST_SUBST+=		PERL='@comment '
 .endif
 
 # use 21 instead of 16 bits to represent unicode characters
 .if !empty(PKG_OPTIONS:Municode3)
-CONFIGURE_ARGS+=        --enable-unicode3
+CONFIGURE_ARGS+=	--enable-unicode3
 .else
-CONFIGURE_ARGS+=        --disable-unicode3
+CONFIGURE_ARGS+=	--disable-unicode3
 .endif
 
 .if !empty(PKG_OPTIONS:Mxft2)
-CONFIGURE_ARGS+=        --enable-xft
+CONFIGURE_ARGS+=	--enable-xft
 .include "../../fonts/Xft2/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=        --disable-xft
+CONFIGURE_ARGS+=	--disable-xft
 .endif
-
