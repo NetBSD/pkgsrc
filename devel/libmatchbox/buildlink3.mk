fa022216dcc30d105a30e345177ddc45b4777d4b
joerg <joerg@pkgsrc.org>
2006-12-15 20:32:52 +0000

    Mechanically replace all includes of buildlink3.mk of the following packages with the modular Xorg equivalent. Those are falling back to the old location by default, so this commmit doesn't change dependencies.
    graphics/xpm ==> x11/libXpm
    fonts/Xft2 ==> x11/libXft
    x11/Xfixes ==> x11/libXfixes
    x11/xcursor ==> x11/libXcursor
    x11/Xrender ==> x11/libXrender
    x11/Xrandr ==> libXrandr

--- devel/libmatchbox/buildlink3.mk
+++ devel/libmatchbox/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/07/08 23:10:45 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2006/12/15 20:32:54 joerg Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBMATCHBOX_BUILDLINK3_MK:=	${LIBMATCHBOX_BUILDLINK3_MK}+
@@ -17,8 +17,8 @@ BUILDLINK_ABI_DEPENDS.libmatchbox+=	libmatchbox>=1.7nb3
 BUILDLINK_PKGSRCDIR.libmatchbox?=	../../devel/libmatchbox
 .endif	# LIBMATCHBOX_BUILDLINK3_MK
 
-.include "../../fonts/Xft2/buildlink3.mk"
-.include "../../graphics/xpm/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
+.include "../../x11/libXft/buildlink3.mk"
+.include "../../x11/libXpm/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
