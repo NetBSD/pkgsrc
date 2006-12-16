f6106e7c675b06d5dba79b766fb2e48ec59c43c5
joerg <joerg>
2006-12-16 01:04:43 +0000

    For reasons beyond my compressions, (b)make handles
    .if foo || bar
    BAZ=	no
    .else
    BAZ=	yes
    .endif
    
    different from
    
    .if foo
    BAZ=	no
    .elif bar
    BAZ=	no
    .else
    BAZ=	yes
    .endif
    
    in some situations exposed in this builtin.mk. Workaround this for now.

--- x11/compositeproto/builtin.mk
+++ x11/compositeproto/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.3 2006/12/15 20:43:00 joerg Exp $
+# $NetBSD: builtin.mk,v 1.4 2006/12/16 01:04:43 joerg Exp $
 
 BUILTIN_PKG:=	compositeproto
 
@@ -14,7 +14,9 @@ BUILTIN_FIND_FILES.H_COMPOSITEPROTO=	\
 ### Determine if there is a built-in implementation of the package and
 ### set IS_BUILTIN.<pkg> appropriately ("yes" or "no").
 ###
-.if !defined(IS_BUILTIN.compositeproto) || ${X11BASE} == ${LOCALBASE}
+.if ${X11BASE} == ${LOCALBASE}
+IS_BUILTIN.compositeproto=	no
+.elif !defined(IS_BUILTIN.compositeproto)
 IS_BUILTIN.compositeproto=	no
 #
 # Here, we skip checking whether the files are under ${LOCALBASE} since
--- x11/renderproto/builtin.mk
+++ x11/renderproto/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.5 2006/12/15 20:43:00 joerg Exp $
+# $NetBSD: builtin.mk,v 1.6 2006/12/16 01:04:43 joerg Exp $
 
 BUILTIN_PKG:=	renderproto
 
@@ -11,7 +11,9 @@ BUILTIN_FIND_FILES.H_RENDER=	${X11BASE}/include/X11/extensions/render.h
 ### Determine if there is a built-in implementation of the package and
 ### set IS_BUILTIN.<pkg> appropriately ("yes" or "no").
 ###
-.if !defined(IS_BUILTIN.renderproto) || ${X11BASE} == ${LOCALBASE}
+.if ${X11BASE} == ${LOCALBASE}
+IS_BUILTIN.renderproto=	no
+.elif !defined(IS_BUILTIN.xproto)
 IS_BUILTIN.renderproto=	no
 #
 # Here, we skip checking whether the files are under ${LOCALBASE} since
