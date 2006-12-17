07c2ede26e3b6a9dabd16363f52d703267ae0111
joerg <joerg@pkgsrc.org>
2006-12-17 21:31:09 +0000

    Non-modular redirect + buildlink xf86vidmodeproto to make this more useful.

--- x11/libXxf86vm/buildlink3.mk
+++ x11/libXxf86vm/buildlink3.mk
@@ -1,4 +1,10 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/11/14 15:06:45 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2006/12/17 21:31:09 joerg Exp $
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${X11_TYPE} != "modular"
+.include "../../mk/x11.buildlink3.mk"
+.else
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBXXF86VM_BUILDLINK3_MK:=	${LIBXXF86VM_BUILDLINK3_MK}+
@@ -18,5 +24,8 @@ BUILDLINK_PKGSRCDIR.libXxf86vm?=	../../x11/libXxf86vm
 
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/xf86vidmodeproto/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
+
+.endif
