2d4c17d2994de4aee5bbf23004296bb21303a0e9
joerg <joerg@pkgsrc.org>
2006-12-17 22:38:14 +0000

    Remove libXi include, if packages need it, they have to include it directly anyway.

--- x11/trapproto/buildlink3.mk
+++ x11/trapproto/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/11/03 20:02:34 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2006/12/17 22:38:14 joerg Exp $
 
 BUILDLINK_DEPMETHOD.trapproto?=	build
 
@@ -19,7 +19,6 @@ BUILDLINK_PKGSRCDIR.trapproto?=	../../x11/trapproto
 .endif	# TRAPPROTO_BUILDLINK3_MK
 
 .include "../../x11/libX11/buildlink3.mk"
-.include "../../x11/libXi/buildlink3.mk"
 .include "../../x11/xproto/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
