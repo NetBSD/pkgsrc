97a670c408f4fcb86e7ad4ab987ce26d8f418c20
wiz <wiz@pkgsrc.org>
2006-12-23 10:18:39 +0000

    Add more options. Only include openssl/bl3.mk if ssl option is chosen (no change for default options). From Blair Sadewitz in private mail.

--- www/elinks/options.mk
+++ www/elinks/options.mk
@@ -1,8 +1,9 @@
-# $NetBSD: options.mk,v 1.3 2006/12/16 11:23:45 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2006/12/23 10:18:39 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.elinks
-PKG_SUPPORTED_OPTIONS+=	bittorrent nntp spidermonkey fsp finger gopher
-PKG_SUPPORTED_OPTIONS+=	inet6 x11
+PKG_SUPPORTED_OPTIONS+=	bittorrent nntp spidermonkey finger gopher
+PKG_SUPPORTED_OPTIONS+=	inet6 x11 elinks-fastmem elinks-exmode expat
+PKG_SUPPORTED_OPTIONS+= elinks-html-highlight elinks-root-exec ssl
 PKG_OPTIONS_GROUP.tls=	gnutls ssl
 PKG_OPTIONS_REQUIRED_GROUPS=	tls
 PKG_SUGGESTED_OPTIONS=	ssl
@@ -16,8 +17,10 @@ CONFIGURE_ARGS+= --disable-ipv6
 .endif
 
 .if !empty(PKG_OPTIONS:Mx11)
-CONFIGURE_ARGS+=	--with-x
+
 .include "../../mk/x11.buildlink3.mk"
+
+CONFIGURE_ARGS+=	--with-x
 .else
 CONFIGURE_ARGS+=	--without-x
 .endif
@@ -35,30 +38,38 @@ CONFIGURE_ARGS+=	--disable-nntp
 .endif
 
 .if !empty(PKG_OPTIONS:Mspidermonkey)
+
+.include "../../lang/spidermonkey/buildlink3.mk"
+
 CONFIGURE_ARGS+=	--with-spidermonkey
 CONFIGURE_ARGS+=	--enable-sm-scripting
-.include "../../lang/spidermonkey/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-spidermonkey
 CONFIGURE_ARGS+=	--disable-sm-scripting
 .endif
 
 .if !empty (PKG_OPTIONS:Mssl)
-CONFIGURE_ARGS+=	--with-openssl
+
 .include "../../security/openssl/buildlink3.mk"
-CONFIGURE_ARGS+=	--without-openssl
+
+CONFIGURE_ARGS+=	--with-openssl=${BUILDLINK_PREFIX.openssl}
+
 .elif !empty(PKG_OPTIONS:Mgnutls)
+
+.include "../../security/gnutls/buildlink3.mk"
+
 CONFIGURE_ARGS+= --with-gnutls-includes=${BUILDLINK_PREFIX.gnutls}/include
 CONFIGURE_ARGS+= --with-gnutls-libs=${BUILDLINK_PREFIX.gnutls}/lib
 CONFIGURE_ARGS+= --without-openssl
-.include "../../security/gnutls/buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mfsp)
-CONFIGURE_ARGS+=	--enable-fsp
-.else
-CONFIGURE_ARGS+=	--disable-fsp
-.endif
+# Requires fsplib, which is not currently in pkgsrc.
+#
+#.if !empty(PKG_OPTIONS:Mfsp)
+#CONFIGURE_ARGS+=	--enable-fsp
+#.else
+#CONFIGURE_ARGS+=	--disable-fsp
+#.endif
 
 .if !empty(PKG_OPTIONS:Mfinger)
 CONFIGURE_ARGS+=	--enable-finger
@@ -70,4 +81,37 @@ CONFIGURE_ARGS+=	--disable-finger
 CONFIGURE_ARGS+=	--enable-gopher
 .else
 CONFIGURE_ARGS+=	--disable-gopher
+.endif
+
+.if !empty(PKG_OPTIONS:Mexpat)
+
+.include "../../textproc/expat/buildlink3.mk"
+
+CONFIGURE_ARGS+=	--enable-xbel
+.else
+CONFIGURE_ARGS+=	--disable-xbel
+.endif
+
+.if !empty(PKG_OPTIONS:Melinks-fastmem)
+CONFIGURE_ARGS+=	--enable-fastmem
+.else
+CONFIGURE_ARGS+=	--disable-fastmem
+.endif
+
+.if !empty(PKG_OPTIONS:Melinks-html-highlight)
+CONFIGURE_ARGS+=	--enable-html-highlight
+.else
+CONFIGURE_ARGS+=	--disable-html-hightlight
+.endif
+
+.if !empty(PKG_OPTIONS:Melinks-exmode)
+CONFIGURE_ARGS+=	--enable-exmode
+.else
+CONFIGURE_ARGS+=	--disable-exmode
+.endif
+
+.if !empty(PKG_OPTIONS:Melinks-root-exec)
+CONFIGURE_ARGS+=	--disable-no-root
+.else
+CONFIGURE_ARGS+=	--enable-no-root
 .endif
