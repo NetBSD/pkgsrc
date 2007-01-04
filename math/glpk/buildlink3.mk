82a7416f6f76707fb27b081048c20b594d4516f1
adam <adam>
2007-01-04 22:38:49 +0000

    Changes 4.13:         A tentative implementation of the "exact" simplex method based         on bignum (rational) arithmetic was included in the package.
            On API level this new feature is available through the routine
            lpx_exact, which is similar to the routine lpx_simplex.
    
            In the solver glpsol this feature is available through two new
            command-line options: --exact and --xcheck. If the '--exact'
            option is specified, glpsol solves LP instance using the exact
            simplex method; in case of MIP it is used to obtain optimal
            solution of LP relaxation. If the --xcheck option is specified,
            LP instance (or LP relaxation) is solved using the standard
            (floating-point) simplex method, however, then glpsol calls the
            exact simplex routine to make sure that the final LP basis is
            exactly optimal, and if it is not, to perform some additional
            simplex iterations in exact arithmetic.
    
    Changes 4.12:
            A tentative implementation of some simplex method routines
            based on exact (bignum) arithmetic was included in the package.
            Currently these routines provide computing LU-factorization of
            the basis matrix and computing components of basic solution.
    
            These routines were used to implement a routine, which checks
            primal and dual feasibility of basic solution exactly, i.e. in
            rational numbers, without round-off errors. In glpsol this
            feature is available through the command-line option --xcheck.
    
            GLPK has its own low-level routines implementing operations on
            integer and rational numbers that makes it independent on other
            software packages. However, to attain a much better performance
            it is highly recommended to install (before configuring GLPK)
            the GNU Multiple Precision Arithmetic Library (GMP). Using GMP
            makes computations 100-200 times faster.

--- math/glpk/buildlink3.mk
+++ math/glpk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2006/07/08 23:10:59 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2007/01/04 22:38:49 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GLPK_BUILDLINK3_MK:=	${GLPK_BUILDLINK3_MK}+
@@ -16,5 +16,7 @@ BUILDLINK_API_DEPENDS.glpk+=	glpk>=4.8nb1
 BUILDLINK_ABI_DEPENDS.glpk+=	glpk>=4.9nb1
 BUILDLINK_PKGSRCDIR.glpk?=	../../math/glpk
 .endif	# GLPK_BUILDLINK3_MK
+
+.include "../../devel/gmp/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
