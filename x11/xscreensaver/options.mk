727099611b82003fc4916ed92d3743c298c2dd72
rillig <rillig>
2007-01-06 19:53:40 +0000

    Fixed PKGMANDIR.

--- x11/xscreensaver/options.mk
+++ x11/xscreensaver/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2006/07/18 18:40:36 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2007/01/06 19:53:40 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xscreensaver
 PKG_SUPPORTED_OPTIONS=	pam xscreensaver-webcollage
@@ -14,7 +14,7 @@ CONFIGURE_ARGS+=	--with-pam
 PLIST_SRC=	PLIST.webcollage PLIST
 .else
 post-install:
-	${RM} ${PREFIX}/libexec/xscreensaver/config/webcollage.xml
-	${RM} ${PREFIX}/libexec/xscreensaver/webcollage
-	${RM} ${PREFIX}/man/man6/webcollage.6
+	rm ${PREFIX}/libexec/xscreensaver/config/webcollage.xml
+	rm ${PREFIX}/libexec/xscreensaver/webcollage
+	rm ${PREFIX}/${PKGMANDIR}/man6/webcollage.6
 .endif
