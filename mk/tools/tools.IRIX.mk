a5b5de5434a16f710c5cb2242a6cdb2962967b59
rillig <rillig@pkgsrc.org>
2007-01-08 06:54:34 +0000

    Added patch to the built-in IRIX tools. Without that definition, it is impossible to do any work, since almost all packages have patches and devel/patch has a broken configure script, which makes the build fail.

--- mk/tools/tools.IRIX.mk
+++ mk/tools/tools.IRIX.mk
@@ -1,4 +1,4 @@
-# $NetBSD: tools.IRIX.mk,v 1.17 2006/10/17 06:06:14 rillig Exp $
+# $NetBSD: tools.IRIX.mk,v 1.18 2007/01/08 06:54:34 rillig Exp $
 #
 # System-supplied tools for the IRIX operating system.
 
@@ -51,6 +51,7 @@ TOOLS_PLATFORM.mail?=		/usr/bin/mail
 TOOLS_PLATFORM.mkdir?=		/sbin/mkdir -p
 TOOLS_PLATFORM.mv?=		/sbin/mv
 TOOLS_PLATFORM.nice?=		/sbin/nice
+TOOLS_PLATFORM.patch?=		/usr/sbin/patch
 .if exists(/usr/bin/printf)
 TOOLS_PLATFORM.printf?=		/usr/bin/printf
 .endif
