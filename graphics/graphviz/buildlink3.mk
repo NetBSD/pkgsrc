26da53043ff524b5241c6bd38e13392550aae7b6
adam <adam@pkgsrc.org>
2007-01-08 11:29:47 +0000

    Changes 2.12: - Bug fix release for 2.10 - The gd plugin for font handlers was not being used at all if the build   did not use fontconfig, e.g., on Windows. In addition, the code had   dropped the name mapping to Windows font names. - PostScript output had an extraneous '%' character on the first line,   which would cause printing to fail. - Text handling, during both sizing and layout, incorrectly handled   empty lines such as label="\nabc". - HTML-like tables had been changed to use too much vertical space,   to possibly use the wrong font in calculating the height of a line,   and to use the wrong offset when moving the baseline from one line to   the next.

--- graphics/graphviz/buildlink3.mk
+++ graphics/graphviz/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.12 2006/07/16 15:02:43 seb Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2007/01/08 11:29:47 adam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GRAPHVIZ_BUILDLINK3_MK:=	${GRAPHVIZ_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	graphviz
 .endif
 
@@ -11,19 +11,16 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngraphviz}
 BUILDLINK_PACKAGES+=	graphviz
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}graphviz
 
-.if !empty(GRAPHVIZ_BUILDLINK3_MK:M+)
+.if ${GRAPHVIZ_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.graphviz+=	graphviz>=1.12
 BUILDLINK_ABI_DEPENDS.graphviz+=	graphviz>=2.6nb5
 BUILDLINK_PKGSRCDIR.graphviz?=	../../graphics/graphviz
 .endif	# GRAPHVIZ_BUILDLINK3_MK
 
-.include "../../devel/libltdl/buildlink3.mk"
-.include "../../devel/zlib/buildlink3.mk"
-.include "../../fonts/fontconfig/buildlink3.mk"
-.include "../../graphics/freetype2/buildlink3.mk"
-# The configure script requires gd>=2.0.34, which was never released.
-# Uncomment the following when graphics/gd is updated to 2.0.34
-#.include "../../graphics/gd/buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
+.include "../../devel/swig/buildlink3.mk"
+.include "../../lang/lua/buildlink3.mk"
 .include "../../graphics/jpeg/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
