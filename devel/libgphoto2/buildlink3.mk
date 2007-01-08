d3186b323291384b325d87aeec42d743f0535076
joerg <joerg@pkgsrc.org>
2007-01-08 16:17:47 +0000

    libusb is recursively needed on !SunOS.

--- devel/libgphoto2/buildlink3.mk
+++ devel/libgphoto2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2007/01/02 18:32:47 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2007/01/08 16:17:47 joerg Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGPHOTO2_BUILDLINK3_MK:=	${LIBGPHOTO2_BUILDLINK3_MK}+
@@ -16,6 +16,11 @@ BUILDLINK_API_DEPENDS.libgphoto2+=		libgphoto2>=2.1.2
 BUILDLINK_ABI_DEPENDS.libgphoto2+=	libgphoto2>=2.1.6nb1
 BUILDLINK_PKGSRCDIR.libgphoto2?=	../../devel/libgphoto2
 .endif	# LIBGPHOTO2_BUILDLINK3_MK
+
+.include "../../mk/bsd.fast.prefs.mk"
+.if ${OPSYS} != "SunOS"
+.include "../../devel/libusb/buildlink3.mk"
+.endif
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
