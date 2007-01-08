83e4f5a5bcbe671b06175481c70695e7dd3205c2
drochner <drochner>
2007-01-08 21:45:10 +0000

    don't install suid on NetBSD per default; now there are 3 ways: -no PAM (default): let configure figure out whether shadow passwords  are used, in that case xscreensaver will be installed suid to enable  checking -"pam" option enabled: assume that PAM is able to check the password  w/o root privs; require the new pam-pwauth_suid on NetBSD for that -"pam" option enabled; the user decides not to use pam-pwauth_suid  but sets xscreensaver suid instead (on NetBSD, or if the PAM  implementation needs it) add a MESSAGE discussing the latter 2 options bump PKGREVISION

--- x11/xscreensaver/options.mk
+++ x11/xscreensaver/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2007/01/06 19:53:40 rillig Exp $
+# $NetBSD: options.mk,v 1.4 2007/01/08 21:45:10 drochner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xscreensaver
 PKG_SUPPORTED_OPTIONS=	pam xscreensaver-webcollage
@@ -8,12 +8,22 @@ PKG_SUPPORTED_OPTIONS=	pam xscreensaver-webcollage
 .if !empty(PKG_OPTIONS:Mpam)
 .  include "../../mk/pam.buildlink3.mk"
 CONFIGURE_ARGS+=	--with-pam
+CONFIGURE_ARGS+=	--without-shadow
+.if ${OPSYS} == "NetBSD"
+# needed to read shadow passwords
+DEPENDS+=	pam-pwauth_suid-*:../../security/pam-pwauth_suid
+.endif
+.else
+CONFIGURE_ARGS+=	--without-pam
+# configure should figure out
+#CONFIGURE_ARGS+=	--without-shadow
 .endif
 
 .if !empty(PKG_OPTIONS:Mxscreensaver-webcollage)
 PLIST_SRC=	PLIST.webcollage PLIST
 .else
-post-install:
+post-install: delwebcollage
+delwebcollage:
 	rm ${PREFIX}/libexec/xscreensaver/config/webcollage.xml
 	rm ${PREFIX}/libexec/xscreensaver/webcollage
 	rm ${PREFIX}/${PKGMANDIR}/man6/webcollage.6
