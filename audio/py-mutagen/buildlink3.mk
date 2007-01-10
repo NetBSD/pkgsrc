a7b8abc6789ec1f741c3eeea5713d335d38b0d76
drochner <drochner@pkgsrc.org>
2007-01-10 10:21:25 +0000

    add py-mutagen-1.9, a Python module to handle audio metadata (This is just the Python extension part. If the command line utils are needed, a seperate pkg should be added.)

---
+++ audio/py-mutagen/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/01/10 10:21:25 drochner Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+PY_MUTAGEN_BUILDLINK3_MK:=	${PY_MUTAGEN_BUILDLINK3_MK}+
+
+.include "../../lang/python/pyversion.mk"
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	py-mutagen
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npy-mutagen}
+BUILDLINK_PACKAGES+=	py-mutagen
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}py-mutagen
+
+.if ${PY_MUTAGEN_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.py-mutagen+=	${PYPKGPREFIX}-mutagen>=1.9
+BUILDLINK_PKGSRCDIR.py-mutagen?=	../../audio/py-mutagen
+.endif	# PY_MUTAGEN_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
