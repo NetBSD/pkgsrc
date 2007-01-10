8663457f9961a0089182a3c2ed788da8b72a95f5
abs <abs@pkgsrc.org>
2007-01-10 10:00:47 +0000

    fix build with modular X11

--- graphics/ImageMagick/options.mk
+++ graphics/ImageMagick/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2006/05/22 11:30:19 adam Exp $
+# $NetBSD: options.mk,v 1.8 2007/01/10 10:00:47 abs Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ImageMagick
 PKG_SUPPORTED_OPTIONS=	x11
@@ -11,7 +11,7 @@ BUILDLINK_API_DEPENDS.jasper+=	jasper>=1.701.0
 DEPENDS+=	mpeg2codec>=1.2:../../graphics/mpeg2codec
 .include "../../graphics/jasper/buildlink3.mk"
 .include "../../graphics/libwmf/buildlink3.mk"
-.include "../../mk/x11.buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-x
 .endif
