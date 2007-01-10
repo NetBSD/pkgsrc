aec226bf03911ae8ff84c49a28fb039e513e34c5
adam <adam@pkgsrc.org>
2007-01-10 09:09:49 +0000

    Changes 0.9.29: - More work on the new Direct3D state management. - Debugger support for Mac OS. - Many OLE fixes and improvements. - Audio input support on Mac OS. - Lots of bug fixes.

--- emulators/wine/buildlink3.mk
+++ emulators/wine/buildlink3.mk
@@ -1,26 +1,26 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/07/10 21:12:03 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2007/01/10 09:09:49 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 WINE_BUILDLINK3_MK:=	${WINE_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	wine
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nwine}
 BUILDLINK_PACKAGES+=	wine
-
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}wine
 
-.if !empty(WINE_BUILDLINK3_MK:M+)
+.if ${WINE_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.wine+=	wine>=0.9.6nb1
 BUILDLINK_PKGSRCDIR.wine?=	../../emulators/wine
 .endif	# WINE_BUILDLINK3_MK
 
-.include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
+.if !empty(PKG_BUILD_OPTIONS.wine:Mopengl)
 .include "../../graphics/glu/buildlink3.mk"
 .include "../../graphics/glut/buildlink3.mk"
+.endif
 .include "../../graphics/jpeg/buildlink3.mk"
 .include "../../graphics/libungif/buildlink3.mk"
 
