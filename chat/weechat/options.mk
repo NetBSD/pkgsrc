a251fcf2179b85b7469ef874c99bd4c4b11c43fa
tonio <tonio@pkgsrc.org>
2007-01-15 20:25:01 +0000

    Update chat/weechat to 0.2.3
    Version 0.2.3 (2007-01-10):
       * fixed display bugs with nicklist at top/bottom when look_nicklist_separator
         is OFF (bug #18737)
       * fixed iconv problem, causing truncated words when using iso locale
       * fixed topic scroll when topic has multi-bytes chars
       * fixed compilation problem with iconv under FreeBSD
       * fixed bugs with charset: now decodes/encodes nicks and channels in IRC
         messages (bug #18716)
    
    Version 0.2.2 (2007-01-06):
       * fixed bug with status bar (missing refresh) when closing a buffer
       * fixed bug with use of first buffer for a channel if not connected
         to server (now allowed only for a server buffer)
       * fixed refresh bug with private buffer title
       * fixed bug with nick completion in command args (now uses option
         look_nick_completion_ignore)
       * fixed display bug with color for first line on screen (bug #17719)
       * added anti-flood option (irc_anti_flood) (task #5442)
       * fixed bug with "set_config" function in plugins API (bug #18448)
       * plugins: "add_message_handler" now accepts "*" for all IRC messages
       * added keys (F9/F10) to scroll topic (task #6030)
       * added auto completion with channels and filenames (task #5423)
       * fixed memleak in keyboard input
       * fixed refresh bug when changing config options if window is splited
       * added space between chat and nicklist when position is "right" (bug #17852)
       * added option "look_nicklist_separator" (task #5437)
       * fixed bug with DCC SEND when filename begins with '~'
       * added "irc_send_unknown_commands" option to send unknown commands to
         IRC server (OFF by default) (task #5947)
       * /charset command and charset conversions now made by "charset" plugin
       * fixed display bug in status bar, wrong length when using UTF-8
       * fixed bug with ignore: now any IRC command is allowed
       * fixed crash with Ctrl-T (transpose) and one char on line (bug #18153)
       * added filename completion (task #5425)
       * added "modifier" in plugins API
       * improved /plugin command
       * fixed bug on ignore with "mode" IRC command (bug #18058)
       * fixed crash when loading ruby script if file does not exist, with
         Ruby >= 1.9 only (bug #18064)
       * added date in plugin function get_buffer_data()
       * fixed some portability bugs (patch #5271)
       * fixed iconv detection for BSD (patch #5456)
       * fixed typo in configure.in (bash specific test) (patch #5450)
       * mode changes with /op, /deop, /voice, /devoice, /halfop, /dehalfop are
         now sent in one mode command to server (task #5968)
       * added more values for config boolean values: y/true/t/1 and n/false/f/0
       * fixed bug with /alias and arguments (like $1), now text after
         argument(s) is used (bug #17944)
       * fixed minor display bug with special chars on some arch like PPC

--- chat/weechat/options.mk
+++ chat/weechat/options.mk
@@ -1,9 +1,18 @@
-# $NetBSD: options.mk,v 1.1 2006/10/02 21:02:03 tonio Exp $
+# $NetBSD: options.mk,v 1.2 2007/01/15 20:25:01 tonio Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.weechat
-PKG_SUPPORTED_OPTIONS=	python lua
+PKG_SUPPORTED_OPTIONS=	charset python lua
+PKG_SUGGESTED_OPTIONS=	charset
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mcharset)
+CONFIGURE_ARGS+=	--enable-charset
+PLIST_SUBST+= WITHCHARSET=""
+.else
+CONFIGURE_ARGS+=	--disable-charset
+PLIST_SUBST+= WITHCHARSET="@comment "
+.endif
 
 .if !empty(PKG_OPTIONS:Mpython)
 .include "../../lang/python/extension.mk"
@@ -23,7 +32,8 @@ CONFIGURE_ARGS+=	--disable-lua
 PLIST_SUBST+= WITHLUA="@comment "
 .endif
 
-.if !empty(PKG_OPTIONS:Mpython) || !empty(PKG_OPTIONS:Mlua)
+.if !empty(PKG_OPTIONS:Mpython) || !empty(PKG_OPTIONS:Mlua) \
+    || !empty(PKG_OPTIONS:Mcharset)
 PLIST_SUBST+= WITHPLUGIN=""
 .else
 PLIST_SUBST+= WITHPLUGIN="@comment "
