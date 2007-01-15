b417ec03d00d7180a40a84b0d844947fba6b2e8b
joerg <joerg@pkgsrc.org>
2007-01-15 19:26:33 +0000

    Modular Xorg support.

--- x11/libxklavier/buildlink3.mk
+++ x11/libxklavier/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2006/09/15 15:11:52 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2007/01/15 19:26:33 joerg Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBXKLAVIER_BUILDLINK3_MK:=	${LIBXKLAVIER_BUILDLINK3_MK}+
@@ -17,5 +17,6 @@ BUILDLINK_PKGSRCDIR.libxklavier?=	../../x11/libxklavier
 .endif	# LIBXKLAVIER_BUILDLINK3_MK
 
 .include "../../textproc/libxml2/buildlink3.mk"
+.include "../../x11/libxkbfile/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
