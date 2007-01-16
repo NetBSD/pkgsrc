e78b35931699dcc3e87de07439a9553229215fb7
joerg <joerg@pkgsrc.org>
2007-01-16 20:19:13 +0000

    For libtool, libSM and libX11 are recursively needed.

--- x11/libxfce4mcs/buildlink3.mk
+++ x11/libxfce4mcs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2006/10/12 09:24:20 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2007/01/16 20:19:13 joerg Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBXFCE4MCS_BUILDLINK3_MK:=	${LIBXFCE4MCS_BUILDLINK3_MK}+
@@ -16,7 +16,9 @@ BUILDLINK_API_DEPENDS.libxfce4mcs+=	libxfce4mcs>=4.2.3nb2
 BUILDLINK_PKGSRCDIR.libxfce4mcs?=	../../x11/libxfce4mcs
 .endif	# LIBXFCE4MCS_BUILDLINK3_MK
 
-.include "../../x11/libxfce4util/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libxfce4util/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
