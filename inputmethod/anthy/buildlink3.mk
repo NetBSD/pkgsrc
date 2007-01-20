70cd8a74bca6ce5d118fece1c36b4c4b551e9034
wiz <wiz@pkgsrc.org>
2007-01-20 17:11:52 +0000

    Update to 7900, based on patches provided by Yamashiro, Jun in PR 34651. Additionally, split option handling into options.mk and properly handle the configuration file.
    Changelog in anthy(7100 to 7900):
    * Fix Japanese convert algorithm
    * Update main Japanese Kana-Kanji dictionary
    
    Changelog in pkgsrc:
    * Fix option for additional Kana-Kanji dictionary.
    * Update Okinawa dicrectory(optional).

--- inputmethod/anthy/buildlink3.mk
+++ inputmethod/anthy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/07/08 23:10:53 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2007/01/20 17:11:52 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 ANTHY_BUILDLINK3_MK:=	${ANTHY_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}anthy
 
 .if !empty(ANTHY_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.anthy+=	anthy>=6300
-BUILDLINK_ABI_DEPENDS.anthy?=	anthy>=7100bnb1
+BUILDLINK_ABI_DEPENDS.anthy?=	anthy>=7900
 BUILDLINK_PKGSRCDIR.anthy?=	../../inputmethod/anthy
 .endif  # ANTHY_BUILDLINK3_MK
 
