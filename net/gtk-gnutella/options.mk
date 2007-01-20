39883136640c4a80eb770a1e11de07ce10e96dd9
cbiere <cbiere>
2007-01-20 14:50:03 +0000

    * Updated to 0.96.3.  * Added GUI option "headless".  * Removed dbus dependency. It's just a gimmick, not worth it.  * Added patch-ab to avoid picking up stuff from /usr/local for example.

--- net/gtk-gnutella/options.mk
+++ net/gtk-gnutella/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.1 2006/05/12 20:43:54 adam Exp $
+# $NetBSD: options.mk,v 1.2 2007/01/20 14:50:03 cbiere Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gtk-gnutella
 PKG_SUPPORTED_OPTIONS=		# empty
 PKG_OPTIONS_OPTIONAL_GROUPS=	gui
-PKG_OPTIONS_GROUP.gui=		gtk1 gtk2
+PKG_OPTIONS_GROUP.gui=		gtk1 gtk2 headless
 PKG_SUGGESTED_OPTIONS=		gtk2
 
 .include "../../mk/bsd.options.mk"
@@ -16,4 +16,9 @@ CONFIGURE_ARGS+=	-D gtkversion=1
 .if !empty(PKG_OPTIONS:Mgtk2)
 CONFIGURE_ARGS+=	-D gtkversion=2
 .  include "../../x11/gtk2/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mheadless)
+CONFIGURE_ARGS+=	-D d_headless
+.  include "../../devel/glib2/buildlink3.mk"
 .endif
