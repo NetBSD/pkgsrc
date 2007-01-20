cbf7beb46b53035f04ecc22179b5d79eb7f421b3
wiz <wiz@pkgsrc.org>
2007-01-20 16:56:43 +0000

    Update to 1.3.1, provided by Yamashiro, Jun in PR 35315.
    Overview of changes from 1.3.0 to 1.3.1
    ==
    * Fixes
      - SKK
        - Fix specific key handling in wide-latin direct input mode
    
      - uim-pref-qt
        - Fix compilation on non-immodule version of Qt3
    
    * Others
      - uim-toolbar-{gtk,gtk-systray,applet}
        - Avoid potential misbehavior in property activation by popup menu
      - uim_panelapplet
        - Improve layout upon resize of the KDE panel
      - uim-candwin-qt
        - Handle disconnect from uim-xim sanely
    
    Overview of changes from 1.3.0-beta to 1.3.0
    ==
    * Fixed
      - XIM bridge
        - Fix compilation on pre-X11R6.7 environment.
    
    Overview of changes from 1.3.0-alpha to 1.3.0-beta
    ==
    * Fixed
      - libuim
        - Minor fix for text acquisition and injection
        - Add caps-lock, scroll-lock, and num-lock keysyms
    
      - Tcode
      - Try-code
        - Update compose table
    
      - uim-toolbar-qt
        - Suppress runtime warning when icon files are missing
    
      - GTK+ bridge
      - Qt bridge
      - XIM bridge
        - Support caps-lock, scroll-lock, and num-lock keysyms
        - Fix page shift behavior
    
    Overview of changes from 1.2.x to 1.3.0-alpha
    ==
    * New features
      - libuim
        - Obsolete experimental surrounding text API
        - Add text acquisition API and support it in GTK+ and Qt bridges
        - Add text injection API (uim_input_string())
        - Support focus-in and focus-out handler in IM context
        - More keysyms support
        - See doc/COMPATIBILITY for further details
    
      - Anthy
        - Kana keyboard support (experimental)
      - Latin IM
        - Support deadkeys with preedit enabled
      - Try-Code IM
        - New Japanese IM based on T-Code
    
      - uim_panelapplet
        - New toolbar applet for KDE panel
      - uim-toolbar-gtk
        - Double click to shrink the window (Masatake Yamato)
      - uim-toolbar-qt
        - Double click to shrink the window
    
      - uim-pref-gtk
      - uim-pref-qt
        - Support more keysyms in key editor
    
      - GTK+ bridge
      - Qt bridge
      - XIM bridge
        - More keysyms support
    
    * Fixes
      - Prime
        - Fix key handling in word registration mode (bug #8984)
      - Tcode
        - Update compose table
      - Generic (Tcode, Hangul, PY)
        - Fix key handling
    
      - uim-toolbar-gtk
        - More icons (Shun-ichi TAHARA)
      - uim-toolbar-qt
        - More icons
        - Use smaller toolbar icons
        - Fix in movement with mouse pointer
    
      - GTK+ bridge
        - Use workaround for gtk_im_context_reset()
      - Qt bridge
        - Fix for preedit relocation with Japanese IM
      - XIM bridge
        - Fix for java applications with incorrect font setting
        - Workaround for Flash Player 9 plugin
    
    * Others
      - Input method icons
        - Replace with inputmethod-icons 1.1.1
      - uim-module-manager
        - Set newly registering module names to the head of IM list
      - uim-input-pad-ja
        - Reorginize pad buttons (Shun-ichi TAHARA)

--- inputmethod/uim/options.mk
+++ inputmethod/uim/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.9 2006/03/09 18:04:32 wiz Exp $
+# $NetBSD: options.mk,v 1.10 2007/01/20 16:56:43 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.uim
 PKG_SUPPORTED_OPTIONS=	anthy canna eb gtk qt
@@ -6,17 +6,29 @@ PKG_SUGGESTED_OPTIONS=	anthy canna gtk
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_SUBST+=		HELPERDATA="@comment "
+PLIST_SUBST+=		UIM_DICT_GTK="@comment "
+
 .if !empty(PKG_OPTIONS:Manthy)
-.include "../../inputmethod/anthy/buildlink3.mk"
-PLIST_SRC+=		PLIST.anthy
+.  include "../../inputmethod/anthy/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-anthy --enable-dict
+PLIST_SUBST+=		ANTHY=
+.  if !empty(PKG_OPTIONS:Mgtk)
+PLIST_SUBST+=		HELPERDATA=
+PLIST_SUBST+=		UIM_DICT_GTK=
+.  endif
 .else
 CONFIGURE_ARGS+=	--without-anthy
+PLIST_SUBST+=		ANTHY="@comment "
 .endif
 
 .if !empty(PKG_OPTIONS:Mcanna)
-.include "../../inputmethod/canna-lib/buildlink3.mk"
+.  include "../../inputmethod/canna-lib/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-canna
-PLIST_SRC+=		PLIST.canna
+PLIST_SUBST+=		CANNA=
+.else
+CONFIGURE_ARGS+=	--without-canna
+PLIST_SUBST+=		CANNA="@comment "
 .endif
 
 .if !empty(PKG_OPTIONS:Meb)
@@ -27,18 +39,21 @@ CONFIGURE_ARGS+=	--without-eb
 
 .if !empty(PKG_OPTIONS:Mgtk)
 .include "../../x11/gtk2/modules.mk"
-PLIST_SRC+=		PLIST.gtk
+PLIST_SUBST+=		GTK=
 .else
 CONFIGURE_ARGS+=	--without-gtk2
+PLIST_SUBST+=		GTK="@comment "
 .endif
 
 .if !empty(PKG_OPTIONS:Mqt)
-.include "../../x11/qt3-libs/buildlink3.mk"
-.include "../../x11/qt3-tools/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-qt
+.  include "../../x11/kdelibs3/buildlink3.mk"
+.  include "../../x11/qt3-libs/buildlink3.mk"
+BUILD_DEPENDS+=		qt3-tools-3.*:../../x11/qt3-tools
+CONFIGURE_ARGS+=	--with-qt CXXFLAGS=-lc
 # Not worked this option.  need immodule patch for Qt3
-CONFIGURE_ARGS+=	--with-qt-immodule
-PLIST_SRC+=		PLIST.qt
+#CONFIGURE_ARGS+=	--with-qt-immodule
+PLIST_SUBST+=		HELPERDATA=
+PLIST_SUBST+=		QT=
+.else
+PLIST_SUBST+=		QT="@comment "
 .endif
-
-PLIST_SRC+=		PLIST.common
