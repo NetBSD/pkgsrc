1b44e2a47d995132fe0d503288d79f194a72307b
martti <martti@pkgsrc.org>
2007-01-23 11:53:46 +0000

    Xfce 4.2.4 released
    A new bug fix release of Xfce 4.2 is available. This release is supposed to
    be the last release for the 4.2 branch. It includes several fixes ported from
    the current developpment branch. This release should not be confused with the
    upcoming Xfce 4.4 release [1], it's a bug fix release of the previous stable
    branch.
    
    [1] Xfce 4.4.0 is already in pkgsrc-wip.

--- graphics/xfce4-icon-theme/buildlink3.mk
+++ graphics/xfce4-icon-theme/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/10/12 09:24:19 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2007/01/23 11:53:46 martti Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_ICON_THEME_BUILDLINK3_MK:=	${XFCE4_ICON_THEME_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	xfce4-icon-theme
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-icon-theme
 
 .if ${XFCE4_ICON_THEME_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.xfce4-icon-theme+=	xfce4-icon-theme>=4.2.3nb1
+BUILDLINK_API_DEPENDS.xfce4-icon-theme+=	xfce4-icon-theme>=4.2.4
 BUILDLINK_PKGSRCDIR.xfce4-icon-theme?=	../../graphics/xfce4-icon-theme
 .endif	# XFCE4_ICON_THEME_BUILDLINK3_MK
 
