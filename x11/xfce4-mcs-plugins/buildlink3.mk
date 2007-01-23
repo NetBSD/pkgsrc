9feb37ab5328544c7be1617044f7eb622421183e
martti <martti>
2007-01-23 11:53:46 +0000

    Xfce 4.2.4 released
    A new bug fix release of Xfce 4.2 is available. This release is supposed to
    be the last release for the 4.2 branch. It includes several fixes ported from
    the current developpment branch. This release should not be confused with the
    upcoming Xfce 4.4 release [1], it's a bug fix release of the previous stable
    branch.
    
    [1] Xfce 4.4.0 is already in pkgsrc-wip.

--- x11/xfce4-mcs-plugins/buildlink3.mk
+++ x11/xfce4-mcs-plugins/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2006/10/12 09:24:21 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2007/01/23 11:53:47 martti Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_MCS_PLUGINS_BUILDLINK3_MK:=	${XFCE4_MCS_PLUGINS_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	xfce4-mcs-plugins
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-mcs-plugins
 
 .if ${XFCE4_MCS_PLUGINS_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.xfce4-mcs-plugins+=	xfce4-mcs-plugins>=4.2.3nb4
+BUILDLINK_API_DEPENDS.xfce4-mcs-plugins+=	xfce4-mcs-plugins>=4.2.4
 BUILDLINK_PKGSRCDIR.xfce4-mcs-plugins?=	../../x11/xfce4-mcs-plugins
 .endif	# XFCE4_MCS_PLUGINS_BUILDLINK3_MK
 
