5c19caa1f00fddc082673496fad7eed98899608f
joerg <joerg>
2007-01-23 19:47:53 +0000

    Update to libX11-1.1.1:
    Changes include:
    - fix use-after-free
    - fix dependency on inputproto
    - XListFontsWithInfo
    - avoid calling callbacks to applications with holding the display lock
    - fix leaks in XKB
    - fix compose entries in the NLS lists
    - fix display locking in XKB
    - fix double open of compose file
    - fallback to unix domain or TCP connections, when the local connection
      failed
    
    Note that XCB is currently not active as I don't think it is currently
    useful or stable enough.

--- x11/libX11/buildlink3.mk
+++ x11/libX11/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2007/01/02 11:35:13 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2007/01/23 19:47:53 joerg Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -18,14 +18,14 @@ BUILDLINK_PACKAGES+=	libX11
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libX11
 
 .if ${LIBX11_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.libX11+=	libX11>=0.99
+BUILDLINK_API_DEPENDS.libX11+=	libX11>=1.1
+BUILDLINK_ABI_DEPENDS.libX11+=	libX11>=0.99
 BUILDLINK_PKGSRCDIR.libX11?=	../../x11/libX11
 
 X11_LDFLAGS+=	${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.libX11}/lib
 X11_LDFLAGS+=	-L${BUILDLINK_PREFIX.libX11}/lib
 .endif	# LIBX11_BUILDLINK3_MK
 
-.include "../../x11/inputproto/buildlink3.mk"
 .include "../../x11/kbproto/buildlink3.mk"
 .include "../../x11/libXau/buildlink3.mk"
 .include "../../x11/libXdmcp/buildlink3.mk"
