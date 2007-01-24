9fea36bddaca199970a76e29ca1813fc21101281
martti <martti>
2007-01-24 05:22:01 +0000

    Fixed PLIST when using quota and mysql options.

--- net/proftpd/options.mk
+++ net/proftpd/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.6 2006/05/31 18:22:24 ghen Exp $
+# $NetBSD: options.mk,v 1.7 2007/01/24 05:22:01 martti Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.proftpd
-PKG_SUPPORTED_OPTIONS=	pam wrap quota ldap proftpd-readme
+PKG_SUPPORTED_OPTIONS=	pam wrap quota ldap proftpd-readme tls
 PKG_OPTIONS_OPTIONAL_GROUPS+=	sql
 PKG_OPTIONS_GROUP.sql=		mysql pgsql
 
@@ -19,8 +19,14 @@ MODULES=	# empty
 MODULES:=	${MODULES}:mod_wrap
 .endif
 
+.if !empty(PKG_OPTIONS:Mtls)
+MODULES:=	${MODULES}:mod_tls
+.include "../../security/openssl/buildlink3.mk"
+.endif
+
 .if !empty(PKG_OPTIONS:Mquota)
 MODULES:=	${MODULES}:mod_quotatab:mod_quotatab_file
+PLIST_SRC+=	${PKGDIR}/PLIST.mod_quota
 .endif
 
 .if !empty(PKG_OPTIONS:Mquota) && !empty(PKG_OPTIONS:Mmysql)
@@ -42,6 +48,7 @@ MODULES:=	${MODULES}:mod_ldap
 
 .if !empty(PKG_OPTIONS:Mmysql)
 MODULES:=	${MODULES}:mod_sql:mod_sql_mysql
+PLIST_SRC+=	${PKGDIR}/PLIST.mod_sql
 .include "../../mk/mysql.buildlink3.mk"
 .endif
 
