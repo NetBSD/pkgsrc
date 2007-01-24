b5d3f3c3387c4d7ca26a276e224bfde9b631d734
rillig <rillig@pkgsrc.org>
2007-01-24 01:37:20 +0000

    Quick fix: The main code of the bulk build cannot be run in the shell's "set -e" mode, since it would fill up the temporary directory. Reverted that change.

--- mk/bulk/bsd.bulk-pkg.mk
+++ mk/bulk/bsd.bulk-pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.bulk-pkg.mk,v 1.133 2007/01/22 12:24:20 rillig Exp $
+#	$NetBSD: bsd.bulk-pkg.mk,v 1.134 2007/01/24 01:37:20 rillig Exp $
 
 #
 # Copyright (c) 1999, 2000 Hubert Feyrer <hubertf@NetBSD.org>
@@ -356,7 +356,7 @@ bulk-package:
 	  ${ECHO} "###"; \
 	  ${ECHO} ""; \
 	} | ${TEE} -a ${_BUILDLOG}
-	${RUN} \
+	${RUN} set +e; \
 	uptodate=`${RECURSIVE_MAKE} ${MAKEFLAGS} bulk-check-uptodate REF=${PKGFILE}` ; \
 	if [ $$uptodate = 1 ]; then \
 		{ ${BULK_MSG} "Nothing to be done for ${PKGNAME}." ; \
