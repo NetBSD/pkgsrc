28d6c370299a9c02b75f5e69b0038f523615c863
rillig <rillig>
2007-01-24 09:59:02 +0000

    Made "make help" find this.

--- lang/python/application.mk
+++ lang/python/application.mk
@@ -1,10 +1,14 @@
-# $NetBSD: application.mk,v 1.7 2006/03/11 10:07:50 ghen Exp $
+# $NetBSD: application.mk,v 1.8 2007/01/24 09:59:02 rillig Exp $
 
 .include "../../lang/python/pyversion.mk"
 
 # replace shell magic by python version specific path
+#
 # PYTHON_PATCH_SCRIPTS is a list of Python scripts to be installed,
 #  relative to ${WRKSRC}
+#
+# Keywords: python
+#
 
 .if defined(PYTHON_PATCH_SCRIPTS)
 REPLACE_INTERPRETER+=	python
