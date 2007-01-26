e64f135d375459af9c4c360d9968fe3e6bbb49cc
markd <markd>
2007-01-26 01:26:46 +0000

    Update pilot-link-libs, pilot-link, p5-pilot-link to 0.12.1
    pilot-link v0.12.1
    There isn't much "new" in this release, just some rapid-fire bug fixes
    reported from users and a lot of code has been cleaned up in the tree
    to remove unnecessary includes.
    * Code cleanup in libpisock, libpisync, src and include
    * Fixed the installation faux pas with udev rules, oops!
    
    pilot-link v0.12.0
    pilot-link 0.12.0 "Trois ans" ("Three Years") is now official.
    There have been so many changes in this release and I can't remember
    them all, but here's the short list:
    * Many of the userland binaries now have new names, to avoid some
      potential namespace collisions (dlpsh is now pilot-dlpsh for example,
      memos is now pilot-memos). If you use scripts which call these
      applications, you may have to make minor changes to accomodate the
      new naming scheme.
    * Documentation is now in DocBook XML, and manpages are dynamically
      generated from that source. Other docs are much more detailed, and
      there are quite a few README files that have been added to help.
    * Direct libusb support! This is one of the most monumental features in
      0.12.0, and should help those who have trouble using the kernel's
      visor driver in Linux with their device. Now you can get a healthy
      300% to 600% speedup in sync times, depending on your Palm's
      processor speed. See doc/README.libusb for details. Thanks go to
      Zephania Hull for working out how to get native libusb working with
      pilot-link.
    * Native support for Mac OS X. You can now just download the tarball
      and build it directly on Mac OS X machines, and it will just run.
      Thanks go to Florent Pillet for his tireless work in getting this all
      working efficiently and for squashing countless bugs in the libraries
      along the way.
    * So many other fixes, additions, features and new things to count.

--- comms/pilot-link-libs/buildlink3.mk
+++ comms/pilot-link-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/07/08 23:10:38 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2007/01/26 01:26:46 markd Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PILOT_LINK_LIBS_BUILDLINK3_MK:=	${PILOT_LINK_LIBS_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}pilot-link-libs
 
 .if !empty(PILOT_LINK_LIBS_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.pilot-link-libs+=	pilot-link-libs>=0.11.7nb1
-BUILDLINK_ABI_DEPENDS.pilot-link-libs+=	pilot-link-libs>=0.11.7nb2
+BUILDLINK_ABI_DEPENDS.pilot-link-libs+=	pilot-link-libs>=0.12.1
 BUILDLINK_PKGSRCDIR.pilot-link-libs?=	../../comms/pilot-link-libs
 .endif	# PILOT_LINK_LIBS_BUILDLINK3_MK
 
