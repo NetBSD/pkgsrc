ceac56944d3f90d4224150db7444bc7e8c85057b
cbiere <cbiere@pkgsrc.org>
2007-01-31 21:33:31 +0000

    Update to 1.2.4.

--- audio/libmusepack/buildlink3.mk
+++ audio/libmusepack/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/07/08 23:10:36 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2007/01/31 21:33:31 cbiere Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBMUSEPACK_BUILDLINK3_MK:=	${LIBMUSEPACK_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libmusepack
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libmusepack
 
 .if !empty(LIBMUSEPACK_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.libmusepack+=		libmusepack>=1.0.3
+BUILDLINK_API_DEPENDS.libmusepack+=		libmusepack>=1.2.4
 BUILDLINK_PKGSRCDIR.libmusepack?=	../../audio/libmusepack
 .endif	# LIBMUSEPACK_BUILDLINK3_MK
 
