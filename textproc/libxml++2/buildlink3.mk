68e9624a98c285e636368fcbe89cb7e9b0190002
drochner <drochner>
2007-02-02 09:23:44 +0000

    include bl3 files for libxml2 and glibmm

--- textproc/libxml++2/buildlink3.mk
+++ textproc/libxml++2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/09/24 16:52:08 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2007/02/02 09:23:44 drochner Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBXML++2_BUILDLINK3_MK:=	${LIBXML++2_BUILDLINK3_MK}+
@@ -16,5 +16,8 @@ BUILDLINK_API_DEPENDS.libxml++2+=	libxml++2>=2.10.0
 BUILDLINK_ABI_DEPENDS.libxml++2+=	libxml++2>=2.10.0nb1
 BUILDLINK_PKGSRCDIR.libxml++2?=	../../textproc/libxml++2
 .endif	# LIBXML++2_BUILDLINK3_MK
+
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../devel/glibmm/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
