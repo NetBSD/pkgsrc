e428c5632cbde10fae77ab157f7029b16719d8fc
joerg <joerg@pkgsrc.org>
2007-02-02 17:58:02 +0000

    Adjust for modular Xorg based on the includes as x11 is currently not buildable due to the incomplete implementation of an abstract class.

--- games/scummvm/options.mk
+++ games/scummvm/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2006/03/09 21:04:44 jlam Exp $
+# $NetBSD: options.mk,v 1.6 2007/02/02 17:58:02 joerg Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scummvm
 PKG_SUPPORTED_OPTIONS=	sdl x11
@@ -14,5 +14,6 @@ CONFIGURE_ARGS+=	--backend=sdl
 .if !empty(PKG_OPTIONS:Mx11)
 CONFIGURE_ARGS+=	--backend=x11
 .include "../../mk/oss.buildlink3.mk"
-.include "../../mk/x11.buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/xextproto/buildlink3.mk"
 .endif
