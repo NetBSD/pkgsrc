1317812eebdc6c8cfee076c2bb4741e02619a236
joerg <joerg>
2007-02-04 15:26:34 +0000

    Modular Xorg support.

--- graphics/libggi/buildlink3.mk
+++ graphics/libggi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2006/07/08 23:10:52 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2007/02/04 15:26:34 joerg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBGGI_BUILDLINK3_MK:=	${LIBGGI_BUILDLINK3_MK}+
@@ -17,7 +17,15 @@ BUILDLINK_ABI_DEPENDS.libggi?=	libggi>=2.1.1nb1
 BUILDLINK_PKGSRCDIR.libggi?=	../../graphics/libggi
 .endif	# LIBGGI_BUILDLINK3_MK
 
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${X11_TYPE} == "modular"
+.include "../../x11/libXxf86vm/buildlink3.mk"
+.endif
+
 .include "../../devel/ncurses/buildlink3.mk"
 .include "../../graphics/libgii/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/xextproto/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
