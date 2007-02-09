0f8b3f78a7a5028957cbbcb21ed606e2132ea263
drochner <drochner@pkgsrc.org>
2007-02-09 20:32:38 +0000

    build against libmp4v2 instead of the full mpeg4ip and bump PKGREVISION where the default build is affected

--- audio/daapd/options.mk
+++ audio/daapd/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2007/01/30 07:05:58 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2007/02/09 20:32:38 drochner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.daapd
 PKG_SUPPORTED_OPTIONS=	mpeg4ip
@@ -6,7 +6,7 @@ PKG_SUPPORTED_OPTIONS=	mpeg4ip
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mmpeg4ip)
-.include "../../multimedia/mpeg4ip/buildlink3.mk"
+.include "../../multimedia/libmp4v2/buildlink3.mk"
 MAKE_ENV+= WITH_FAAD=1
 .else
 MAKE_ENV+= WITH_FAAD=0
