f6a8043bf55ad177022fb9d7be1269c73c8638d0
rillig <rillig@pkgsrc.org>
2007-02-10 08:59:07 +0000

    Replaced the deprecated PKG_SKIP_REASON with PKG_FAIL_REASON.

--- mk/compiler/gcc.mk
+++ mk/compiler/gcc.mk
@@ -1,4 +1,4 @@
-# $NetBSD: gcc.mk,v 1.89 2007/01/08 19:29:45 gavan Exp $
+# $NetBSD: gcc.mk,v 1.90 2007/02/10 08:59:07 rillig Exp $
 #
 # This is the compiler definition for the GNU Compiler Collection.
 #
@@ -274,7 +274,7 @@ _NEED_NEWER_GCC!=	\
 .endif
 .if !empty(_USE_PKGSRC_GCC:M[yY][eE][sS]) && \
     !empty(_NEED_NEWER_GCC:M[yY][eE][sS])
-PKG_SKIP_REASON=	"Unable to satisfy dependency: ${_GCC_DEPENDS}"
+PKG_FAIL_REASON=	"Unable to satisfy dependency: ${_GCC_DEPENDS}"
 .endif
 
 # GNU ld option used to set the rpath
