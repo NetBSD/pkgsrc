2b261a3ee291b1dee8a2f547c7effd01324b0397
joerg <joerg>
2007-02-15 22:53:09 +0000

    Modular Xorg support.

--- print/ghostscript6/options.mk
+++ print/ghostscript6/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2006/12/27 17:39:14 joerg Exp $
+# $NetBSD: options.mk,v 1.2 2007/02/15 23:01:43 joerg Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ghostscript
 PKG_SUPPORTED_OPTIONS=	x11
@@ -10,7 +10,8 @@ PKG_SUGGESTED_OPTIONS=	x11
 XLIBS=			Xt SM ICE Xext X11
 GS_DISPLAY_DEVICE=	x11.dev
 MAKEFRAGS+=		${FILESDIR}/devs.x11
-.include "../../mk/x11.buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
 .else
 XLIBS?=			# empty
 GS_DISPLAY_DEVICE?=	# empty
