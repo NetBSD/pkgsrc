c076f840bdcc8b8a6793be40be8c486db1379e37
joerg <joerg@pkgsrc.org>
2007-02-19 18:22:58 +0000

    Improve pthread test and handle modular Xorg.

--- net/kismet/options.mk
+++ net/kismet/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2006/08/03 15:09:49 salo Exp $
+# $NetBSD: options.mk,v 1.2 2007/02/19 18:22:58 joerg Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kismet
@@ -13,5 +13,5 @@ DEPENDS+=		wget>=1.9.1:../../net/wget
 .include "../../devel/gmp/buildlink3.mk"
 .include "../../graphics/ImageMagick/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
-.include "../../mk/x11.buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
 .endif
