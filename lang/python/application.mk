00f7cd7e0cffae37ecaee5eb7fe718cf445ca470
rillig <rillig@pkgsrc.org>
2007-02-20 11:30:43 +0000

    Added REPLACE_PYTHON as an alias to PYTHON_PATCH_SCRIPTS, to have more consistent names. (See REPLACE_PERL, REPLACE_AWK, REPLACE_SH, ...)

--- lang/python/application.mk
+++ lang/python/application.mk
@@ -1,18 +1,23 @@
-# $NetBSD: application.mk,v 1.8 2007/01/24 09:59:02 rillig Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-# replace shell magic by python version specific path
+# $NetBSD: application.mk,v 1.9 2007/02/20 11:30:43 rillig Exp $
 #
-# PYTHON_PATCH_SCRIPTS is a list of Python scripts to be installed,
-#  relative to ${WRKSRC}
+# Replace the #! interpreter for Python scripts.
+#
+# Package-settable variables:
+#
+# REPLACE_PYTHON
+#	A list of Python scripts to be installed, relative to ${WRKSRC}.
+#
+# PYTHON_PATCH_SCRIPTS
+#	The same as REPLACE_PYTHON.
 #
 # Keywords: python
 #
 
-.if defined(PYTHON_PATCH_SCRIPTS)
+.include "../../lang/python/pyversion.mk"
+
+.if defined(PYTHON_PATCH_SCRIPTS) || defined(REPLACE_PYTHON)
 REPLACE_INTERPRETER+=	python
 REPLACE.python.old=	.*python[^ ]*
 REPLACE.python.new=	${PYTHONBIN}
-REPLACE_FILES.python=	${PYTHON_PATCH_SCRIPTS}
+REPLACE_FILES.python=	${PYTHON_PATCH_SCRIPTS} ${REPLACE_PYTHON}
 .endif
