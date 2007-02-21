3dde1baf1c9f9e1af20d3e2c3262b338ab6f30d0
rillig <rillig@pkgsrc.org>
2007-02-21 01:38:53 +0000

    Added the category "filesystems".

--- pkgtools/pkglint/files/pkglint.pl
+++ pkgtools/pkglint/files/pkglint.pl
@@ -1,5 +1,5 @@
 #! @PERL@
-# $NetBSD: pkglint.pl,v 1.699 2007/02/20 10:14:55 rillig Exp $
+# $NetBSD: pkglint.pl,v 1.700 2007/02/21 01:38:53 rillig Exp $
 #
 
 # pkglint - static analyzer and checker for pkgsrc packages
@@ -4786,7 +4786,7 @@ sub checkline_mk_vartype_basic($$$$$$$$) {
 			cad chat chinese comms converters cross crosspkgtools
 			databases devel
 			editors emulators
-			finance fonts
+			filesystems finance fonts
 			games geography gnome gnustep graphics
 			ham
 			inputmethod
