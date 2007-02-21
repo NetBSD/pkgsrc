db7df3668c72aff457efdb29dae38339cff87ace
minskim <minskim@pkgsrc.org>
2007-02-21 21:06:12 +0000

    Import librxspencer from pkgsrc-wip.  Packaged by Aleksey Cheusov. This is Henry Spencer's regular expression library, with some modifications.

---
+++ devel/librxspencer/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/02/21 21:06:12 minskim Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBRXSPENCER_BUILDLINK3_MK:=	${LIBRXSPENCER_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	librxspencer
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibrxspencer}
+BUILDLINK_PACKAGES+=	librxspencer
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}librxspencer
+
+.if !empty(LIBRXSPENCER_BUILDLINK3_MK:M+)
+BUILDLINK_API_DEPENDS.librxspencer+=	librxspencer>=3.8.3
+BUILDLINK_PKGSRCDIR.librxspencer?=	../../devel/librxspencer
+BUILDLINK_INCDIRS.librxspencer+=	include/rxspencer
+.endif	# LIBRXSPENCER_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
