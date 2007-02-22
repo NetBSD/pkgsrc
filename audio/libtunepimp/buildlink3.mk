a31b2a7c96924ca9206c8269faf094176fe15f49
drochner <drochner@pkgsrc.org>
2007-02-22 16:51:32 +0000

    remove direct dependency on fftw -- this is pulled in by libofa

--- audio/libtunepimp/buildlink3.mk
+++ audio/libtunepimp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2007/02/15 08:29:29 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2007/02/22 16:51:32 drochner Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBTUNEPIMP_BUILDLINK3_MK:=	${LIBTUNEPIMP_BUILDLINK3_MK}+
@@ -19,7 +19,6 @@ BUILDLINK_PKGSRCDIR.libtunepimp?=	../../audio/libtunepimp
 
 .include "../../audio/musicbrainz/buildlink3.mk"
 .include "../../audio/libofa/buildlink3.mk"
-.include "../../math/fftw/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
 .include "../../www/curl/buildlink3.mk"
 
