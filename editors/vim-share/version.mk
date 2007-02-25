139dcdce67a2d15f6ed4c411133222de5d8737ab
martti <martti@pkgsrc.org>
2007-02-25 12:20:20 +0000

    Updated editors/vim-share to 7.0.201
      2751  7.0.196  mouse pointer position wrong after ":vert ball"
      1717  7.0.197  (extra) Win32: can't compile with EXITFREE
      7454  7.0.198  (extra) Win32: compiler warnings, don't generate gvim.exe.mnf
      1969  7.0.199  cursor in wrong col when completing+formatting multi-byte
      2792  7.0.200  memory leaks when out of memory
      2511  7.0.201  error message from ":diffput" may be wrong

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.24 2007/02/15 06:30:48 martti Exp $
+# $NetBSD: version.mk,v 1.25 2007/02/25 12:20:21 martti Exp $
 
 VIM_VERSION=		7.0
-VIM_PATCHLEVEL=		195
+VIM_PATCHLEVEL=		201
 VIM_SUBDIR=		vim70
