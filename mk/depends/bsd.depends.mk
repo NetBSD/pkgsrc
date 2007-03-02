553be2ab842005122bde05eb12180eb7e950d105
wiz <wiz@pkgsrc.org>
2007-03-02 05:58:34 +0000

    Remove support for obsolete and unused NO_DEPENDS.

--- mk/depends/bsd.depends.mk
+++ mk/depends/bsd.depends.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.depends.mk,v 1.9 2006/07/13 14:02:34 jlam Exp $
+# $NetBSD: bsd.depends.mk,v 1.10 2007/03/02 05:58:34 wiz Exp $
 #
 # This Makefile fragment is included by bsd.pkg.mk and provides all
 # variables and targets related to dependencies.
@@ -33,17 +33,7 @@ DEPENDS_TARGET=		reinstall
 ### depends is a public target to install missing dependencies for
 ### the package.
 ###
-.PHONY: depends
-.if !defined(NO_DEPENDS)
-.  include "${PKGSRCDIR}/mk/depends/depends.mk"
-.elif !target(depends)
-.  if exists(${_COOKIE.depends})
-depends:
-	@${DO_NADA}
-.  else
-depends: depends-cookie
-.  endif
-.endif
+.include "${PKGSRCDIR}/mk/depends/depends.mk"
 
 ######################################################################
 ### bootstrap-depends (PUBLIC, OVERRIDE)
