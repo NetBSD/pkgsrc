685b486fe66295622742c954d33f6650bb445249
martti <martti>
2007-03-06 14:06:48 +0000

    Updated editors/vim-share to 7.0.207
      9403  7.0.202  compiler and runtime problems on Tandem systems
      5579  7.0.203  0x80 and CSI need to be escaped when executing a register
      1892  7.0.204  cscope: parsing of lines for listing doesn't work properly
      1573  7.0.205  (after 7.0.203) can't compile tiny version
      2099  7.0.206  (after 7.0.058) can't edit all gb18030 files properly
      3000  7.0.207  (after 7.0.203) 0x80 and CSI escaped too often when recording

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.25 2007/02/25 12:20:21 martti Exp $
+# $NetBSD: version.mk,v 1.26 2007/03/06 14:06:49 martti Exp $
 
 VIM_VERSION=		7.0
-VIM_PATCHLEVEL=		201
+VIM_PATCHLEVEL=		207
 VIM_SUBDIR=		vim70
