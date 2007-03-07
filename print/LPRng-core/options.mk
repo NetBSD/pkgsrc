9620c88921e02c955ed6e7a126d39c1f718af40d
rillig <rillig@pkgsrc.org>
2007-03-07 11:04:40 +0000

    Added a kerberos option. Fixes PR 12292.

--- print/LPRng-core/options.mk
+++ print/LPRng-core/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.2 2005/10/05 13:29:49 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2007/03/07 11:04:40 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.LPRng-core
-PKG_SUPPORTED_OPTIONS=	lprng-priv-ports lprng-suid
+PKG_SUPPORTED_OPTIONS=	kerberos lprng-priv-ports lprng-suid
 PKG_SUGGESTED_OPTIONS=	lprng-suid
 
 .include "../../mk/bsd.options.mk"
@@ -11,4 +11,9 @@ CONFIGURE_ARGS+=	--enable-priv_ports
 .endif
 .if empty(PKG_OPTIONS:Mlprng-suid)
 CONFIGURE_ARGS+=	--disable-setuid
+.endif
+.if !empty(PKG_OPTIONS:Mkerberos)
+CONFIGURE_ARGS+=	--enable-kerberos
+KRB5_ACCEPTED=		mit-krb5
+.  include "../../mk/krb5.buildlink3.mk"
 .endif
