5ec53687636f908722e9f89f351f19da2c5782ed
wiz <wiz@pkgsrc.org>
2007-03-12 06:42:33 +0000

    Depend on latest cmake, for shlib fixes.

--- devel/cmake/buildlink3.mk
+++ devel/cmake/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2007/02/23 11:48:18 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2007/03/12 06:42:33 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CMAKE_BUILDLINK3_MK:=	${CMAKE_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	cmake
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}cmake
 
 .if ${CMAKE_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.cmake+=	cmake>=2.4.6
+BUILDLINK_API_DEPENDS.cmake+=	cmake>=2.4.6nb3
 BUILDLINK_PKGSRCDIR.cmake?=	../../devel/cmake
 BUILDLINK_DEPMETHOD.cmake?=	build
 BUILDLINK_FILES.cmake+=		share/cmake-*/include/*
