23d949b1e9a8dc1eccf308b5b57df476de2a8c02
wiz <wiz@pkgsrc.org>
2007-03-21 12:04:58 +0000

    Update to 0.8. Bump ABI depends for new dbus dependency.
    Changes in version 0.8 are:
    * Translations
    
    Changes in version 0.7.92 are:
    * Fix build by including sys/types.h
    * In gnome_keyring_free() don't crash on NULL parameter.
    
    Changes in version 0.7.91 are:
    * Add method for library to discover daemon via DBus. Adds soft
      DBus dependency.
    * Fixes for building on kFreeBSD.
    
    Changes in version 0.7.3 are:
    * Fix endless loop when creating a keyring and a file by that name
      already exists.
    * Fix crasher when deleting session keyring.
    * Fix crasher when doing find operation with NULL attribute string.
    * Sync files to disk after writing to keyring.
    
    Changes in version 0.7.2 are:
    * Don't have multiple password dialogs presented for the same
      keyring
    
    Changes in version 0.7.1 are:
    * Added GNOME_KEYRING_ITEM_APPLICATION_SECRET which allows an item
      to be for a single application only with strict access controls.
    * New function gnome_keyring_item_get_info_full(_sync) which allow
      retrieval of item meta data without the secret, thus not incurring
      an ACL prompt.
    * Translation updates

--- security/gnome-keyring/buildlink3.mk
+++ security/gnome-keyring/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2006/07/08 23:11:06 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2007/03/21 12:04:58 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_KEYRING_BUILDLINK3_MK:=	${GNOME_KEYRING_BUILDLINK3_MK}+
@@ -13,11 +13,12 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gnome-keyring
 
 .if !empty(GNOME_KEYRING_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.gnome-keyring+=	gnome-keyring>=0.4.0
-BUILDLINK_ABI_DEPENDS.gnome-keyring+=	gnome-keyring>=0.4.9nb1
+BUILDLINK_ABI_DEPENDS.gnome-keyring?=	gnome-keyring>=0.8
 BUILDLINK_PKGSRCDIR.gnome-keyring?=	../../security/gnome-keyring
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
+.include "../../sysutils/dbus/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 
 .endif	# GNOME_KEYRING_BUILDLINK3_MK
