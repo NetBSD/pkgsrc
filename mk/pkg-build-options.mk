07b996ab3ff3d4bedcd7bf26f26612ac9676bf0b
rillig <rillig>
2007-04-10 03:45:36 +0000

    Fixed typo in the comment.

--- mk/pkg-build-options.mk
+++ mk/pkg-build-options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: pkg-build-options.mk,v 1.1 2007/03/18 11:09:32 rillig Exp $
+# $NetBSD: pkg-build-options.mk,v 1.2 2007/04/10 03:45:36 rillig Exp $
 #
 # This procedure determines the PKG_OPTIONS that have been in effect
-# when the package ${pkg} has been built. When the package is not yet
-# installed, the current PKG_OPTIONS are queried.
+# when the package ${pkgbase} has been built. When the package is not
+# yet installed, the current PKG_OPTIONS are queried.
 #
 # Parameters:
 #	pkgbase
