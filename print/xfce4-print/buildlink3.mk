cc3a1b98dbb5f28f9899b166be27a16e4831791a
martti <martti@pkgsrc.org>
2007-04-12 09:55:08 +0000

    Updated to Xfce 4.4.1
    See http://www.xfce.org/documentation/changelogs/4.4.1 for details.

--- print/xfce4-print/buildlink3.mk
+++ print/xfce4-print/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2007/01/23 11:53:46 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2007/04/12 09:55:12 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XFCE4_PRINT_BUILDLINK3_MK:=	${XFCE4_PRINT_BUILDLINK3_MK}+
@@ -12,13 +12,14 @@ BUILDLINK_PACKAGES+=	xfce4-print
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-print
 
 .if ${XFCE4_PRINT_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.xfce4-print+=	xfce4-print>=4.2.4
+BUILDLINK_API_DEPENDS.xfce4-print+=	xfce4-print>=4.4.1
 BUILDLINK_PKGSRCDIR.xfce4-print?=	../../print/xfce4-print
 .endif	# XFCE4_PRINT_BUILDLINK3_MK
 
 .include "../../graphics/hicolor-icon-theme/buildlink3.mk"
 .include "../../print/a2ps/buildlink3.mk"
 .include "../../x11/xfce4-mcs-plugins/buildlink3.mk"
+.include "../../devel/xfce4-dev-tools/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 
 .if !defined(PKG_BUILD_OPTIONS.xfce4-print)
--- sysutils/xfce4-systemload-plugin/buildlink3.mk
+++ sysutils/xfce4-systemload-plugin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2006/10/12 09:24:20 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2007/04/12 09:55:17 martti Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_SYSTEMLOAD_PLUGIN_BUILDLINK3_MK:=	${XFCE4_SYSTEMLOAD_PLUGIN_BUILDLINK3_MK}+
@@ -12,11 +12,12 @@ BUILDLINK_PACKAGES+=	xfce4-systemload-plugin
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-systemload-plugin
 
 .if ${XFCE4_SYSTEMLOAD_PLUGIN_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.xfce4-systemload-plugin+=	xfce4-systemload-plugin>=0.3.6nb4
+BUILDLINK_API_DEPENDS.xfce4-systemload-plugin+=	xfce4-systemload-plugin>=0.4.2
 BUILDLINK_PKGSRCDIR.xfce4-systemload-plugin?=	../../sysutils/xfce4-systemload-plugin
 .endif	# XFCE4_SYSTEMLOAD_PLUGIN_BUILDLINK3_MK
 
 .include "../../x11/xfce4-panel/buildlink3.mk"
+.include "../../devel/xfce4-dev-tools/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
