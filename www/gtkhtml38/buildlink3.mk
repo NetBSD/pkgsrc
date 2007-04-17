3fca58ca4c18ef239e3ac64dec34720e83e664f4
wiz <wiz>
2007-04-17 06:29:40 +0000

    Downgrade to latest release from 3.8 branch, 3.12.3nb1. Newer versions are in gtkhtml314.

--- www/gtkhtml38/buildlink3.mk
+++ www/gtkhtml38/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2007/04/11 22:45:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2007/04/17 06:29:41 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GTKHTML38_BUILDLINK3_MK:=	${GTKHTML38_BUILDLINK3_MK}+
@@ -12,8 +12,9 @@ BUILDLINK_PACKAGES+=	gtkhtml38
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gtkhtml38
 
 .if !empty(GTKHTML38_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.gtkhtml38+=	gtkhtml38>=3.14.0
-BUILDLINK_PKGSRCDIR.gtkhtml38?=		../../www/gtkhtml38
+BUILDLINK_API_DEPENDS.gtkhtml38+=	gtkhtml38>=3.8.0
+BUILDLINK_ABI_DEPENDS.gtkhtml38?=	gtkhtml38>=3.12.3nb1
+BUILDLINK_PKGSRCDIR.gtkhtml38?=	../../www/gtkhtml38
 .endif	# GTKHTML38_BUILDLINK3_MK
 
 # XXX: gail is not required by the .pc file but appears as a library
