b24dc4d7933a673d35d86d1a0aa6994bdd608896
drochner <drochner@pkgsrc.org>
2007-04-17 11:26:11 +0000

    version suffix 38->314

--- www/gtkhtml314/buildlink3.mk
+++ www/gtkhtml314/buildlink3.mk
@@ -1,20 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/04/17 06:24:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2007/04/17 11:26:11 drochner Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-GTKHTML38_BUILDLINK3_MK:=	${GTKHTML38_BUILDLINK3_MK}+
+GTKHTML314_BUILDLINK3_MK:=	${GTKHTML314_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	gtkhtml38
+BUILDLINK_DEPENDS+=	gtkhtml314
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngtkhtml38}
-BUILDLINK_PACKAGES+=	gtkhtml38
-BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gtkhtml38
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngtkhtml314}
+BUILDLINK_PACKAGES+=	gtkhtml314
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gtkhtml314
 
-.if !empty(GTKHTML38_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.gtkhtml38+=	gtkhtml38>=3.14.0
-BUILDLINK_PKGSRCDIR.gtkhtml38?=		../../www/gtkhtml38
-.endif	# GTKHTML38_BUILDLINK3_MK
+.if !empty(GTKHTML314_BUILDLINK3_MK:M+)
+BUILDLINK_API_DEPENDS.gtkhtml314+=	gtkhtml314>=3.14.0
+BUILDLINK_PKGSRCDIR.gtkhtml314?=	../../www/gtkhtml314
+.endif	# GTKHTML314_BUILDLINK3_MK
 
 # XXX: gail is not required by the .pc file but appears as a library
 # '-lgailutil' in the .la file...  I'm not sure about putting the dependency
