540ec0744937780e4c6bd81f33694248e99c0dfe
tnn <tnn@pkgsrc.org>
2007-04-17 10:48:29 +0000

    Save a fork() by not invoking the 'tr' utility. XXX: the OS_VERSION stuff could use a similar cleanup, replacing tr A-Z a-z occurences with the make :tl modifier.

--- mk/bsd.prefs.mk
+++ mk/bsd.prefs.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.prefs.mk,v 1.247 2007/04/14 14:17:49 tnn Exp $
+# $NetBSD: bsd.prefs.mk,v 1.248 2007/04/17 10:48:29 tnn Exp $
 #
 # Make file, included to get the site preferences, if any.  Should
 # only be included by package Makefiles before any .if defined()
@@ -56,7 +56,7 @@ CUT=echo Unknown
 .endif
 
 .if !defined(OPSYS)
-OPSYS!=			${UNAME} -s | tr -d /-
+OPSYS=			${:!${UNAME} -s!:S/-//g:S/\///g}
 MAKEFLAGS+=		OPSYS=${OPSYS:Q}
 .endif
 
