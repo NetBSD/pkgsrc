bd2de1ff8d19b7704b9d409b038041dd5d63897a
tnn <tnn@pkgsrc.org>
2007-04-24 19:35:46 +0000

    Add pkgtools/posix_headers version 0.4: This package attempts to resolve deficiencies in system headers. These deficiencies are commonly found on older operating system releases. When use of posix_headers has been enabled on a platform, this package will become a compile-time dependency for every package. It will provide customized header files that take preference over some system provided ones (that may not even exist). This happens transparently through use of buildlink3.

---
+++ pkgtools/posix_headers/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/04/24 19:35:46 tnn Exp $
+
+BUILD_DEPENDS+=		posix_headers>=0.4:../../pkgtools/posix_headers
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+POSIX_HEADERS_BUILDLINK3_MK:=	${POSIX_HEADERS_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+#BUILDLINK_DEPENDS+=	# intentionally left blank.
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nposix_headers}
+BUILDLINK_PACKAGES+=	posix_headers
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}posix_headers
+
+.if !empty(POSIX_HEADERS_BUILDLINK3_MK:M+)
+BUILDLINK_API_DEPENDS.posix_headers+=	posix_headers>=0.1bogus
+BUILDLINK_ABI_DEPENDS.posix_headers+=	posix_headers>=0.1bogus
+BUILDLINK_PKGSRCDIR.posix_headers?=	../../pkgtools/posix_headers
+.endif	# POSIX_HEADERS_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
