78a39735f763ff10ac9ef18f595286a6219e77b0
tnn <tnn>
2007-04-25 16:38:37 +0000

    Update to libevent-1.3b. 1.3b:     * Fixes when using signals in multi-threaded applications from Scott Lamb.     * Other small bug fixes. 1.3a:     * Small bug and performances fixes to evhttp layer. 1.3:     * DNS Server from Adam Langley and Nick Mathewson     * Chunked transfer encoding for HTTP/1.1 from Dug Song.     * DNS DDoS fix; bug report from Jon Oberheide.     * Various small improvements.

--- devel/libevent/buildlink3.mk
+++ devel/libevent/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2007/02/11 05:21:56 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2007/04/25 16:38:37 tnn Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBEVENT_BUILDLINK3_MK:=	${LIBEVENT_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libevent
 
 .if !empty(LIBEVENT_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.libevent+=libevent>=0.6
-BUILDLINK_ABI_DEPENDS.libevent+=libevent-1.2a* # exact match -- see Makefile
+BUILDLINK_ABI_DEPENDS.libevent+=libevent-1.3b* # exact match -- see Makefile
 BUILDLINK_PKGSRCDIR.libevent?=	../../devel/libevent
 .endif	# LIBEVENT_BUILDLINK3_MK
 
