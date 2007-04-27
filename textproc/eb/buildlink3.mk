99d14ad9edc633a0836342373d2a933ba5c8f772
uebayasi <uebayasi@pkgsrc.org>
2007-04-27 04:08:19 +0000

    Simplify PKG_SYSCONFDIR handling by assuming all packages have a consistent PKG_SYSCONFDIR.

--- textproc/eb/buildlink3.mk
+++ textproc/eb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2006/12/12 21:52:37 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2007/04/27 04:08:19 uebayasi Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 EB_BUILDLINK3_MK:=	${EB_BUILDLINK3_MK}+
@@ -11,25 +11,10 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Neb}
 BUILDLINK_PACKAGES+=	eb
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}eb
 
-_EB_REQD=		3.3.2nb1
-
-# This variable should be directly passed to shell commands.  (E.g., a
-# configure argument.)
-#
-# XXX We need something generic.
-.include "../../mk/bsd.fast.prefs.mk"
-PKG_SYSCONFDIR.eb=$$(						\
-	if ${PKG_ADMIN} pmatch 'eb>=${_EB_REQD}'		\
-	    $$( ${PKG_INFO} -e eb ) 2>/dev/null; then		\
-		${PKG_INFO} -B eb 2>/dev/null |			\
-		    ${SED} -ne '/^PKG_SYSCONFDIR/		\
-			{ s/^PKG_SYSCONFDIR=//; p; }';		\
-	else ${ECHO}; fi )
-
 .if !empty(EB_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.eb+=		eb>=${_EB_REQD}
-BUILDLINK_ABI_DEPENDS.eb+=	eb>=4.1.3nb1
-BUILDLINK_PKGSRCDIR.eb?=	../../textproc/eb
+BUILDLINK_API_DEPENDS.eb+=		eb>=3.3.2nb1
+BUILDLINK_ABI_DEPENDS.eb+=		eb>=4.1.3nb1
+BUILDLINK_PKGSRCDIR.eb?=		../../textproc/eb
 .endif	# EB_BUILDLINK3_MK
 
 .include "../../devel/gettext-lib/buildlink3.mk"
