0157dbae6627e549e18ade1f0273c66bcf17480e
tnn <tnn>
2007-04-28 23:33:36 +0000

    Disable inlined setenv. This needs more work.

--- pkgtools/posix_headers/files/stdlib.h
+++ pkgtools/posix_headers/files/stdlib.h
@@ -1,4 +1,4 @@
-/* $NetBSD: stdlib.h,v 1.2 2007/04/28 21:25:06 tnn Exp $ */
+/* $NetBSD: stdlib.h,v 1.3 2007/04/28 23:33:36 tnn Exp $ */
 #ifndef _PKGSRC_STDLIB_H_
 #define _PKGSRC_STDLIB_H_
 #include "@REAL_HEADER@"
@@ -7,9 +7,14 @@
 #include <inttypes.h>
 #define strtoll __strtoll
 #define strtoull __strtoull
+/* provide a setenv(3) implementation */
+#if 0
+/*
+ * XXX: disabled because some GNU configure scripts don't pick this up,
+ * they look for the symbol in the c library. A better solution is needed.
+ */
 #include <string.h>
 #include <stdio.h>
-/* provide a setenv(3) implementation */
 static __inline int setenv(const char *name, const char *value, int overwrite);
 static __inline int setenv(const char *name, const char *value, int overwrite) {
 	char *c;
@@ -22,5 +27,6 @@ static __inline int setenv(const char *name, const char *value, int overwrite) {
 	free(c);
 	return(ret);
 }
+#endif /* 0 */
 #endif /* __hpux */
 #endif /* _PKGSRC_STDLIB_H_ */
