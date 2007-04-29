1279e3d53fe20deefa6dc4bac845ce03cafaebc5
tnn <tnn>
2007-04-29 19:52:44 +0000

    Use BUILDLINK_DEPMETHOD instead of BUILD_DEPENDS hack.

--- pkgtools/posix_headers/buildlink3.mk
+++ pkgtools/posix_headers/buildlink3.mk
@@ -1,12 +1,10 @@
-# $NetBSD: buildlink3.mk,v 1.3 2007/04/28 21:03:56 tnn Exp $
-
-BUILD_DEPENDS+=		posix_headers>=0.4nb5:../../pkgtools/posix_headers
+# $NetBSD: buildlink3.mk,v 1.4 2007/04/29 19:52:44 tnn Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 POSIX_HEADERS_BUILDLINK3_MK:=	${POSIX_HEADERS_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-#BUILDLINK_DEPENDS+=	# intentionally left blank.
+BUILDLINK_DEPENDS+=	posix_headers
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nposix_headers}
@@ -14,9 +12,10 @@ BUILDLINK_PACKAGES+=	posix_headers
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}posix_headers
 
 .if !empty(POSIX_HEADERS_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.posix_headers+=	posix_headers>=0.1bogus
-BUILDLINK_ABI_DEPENDS.posix_headers+=	posix_headers>=0.1bogus
+BUILDLINK_API_DEPENDS.posix_headers+=	posix_headers>=0.4nb5
+BUILDLINK_ABI_DEPENDS.posix_headers+=	posix_headers>=0.4nb5
 BUILDLINK_PKGSRCDIR.posix_headers?=	../../pkgtools/posix_headers
+BUILDLINK_DEPMETHOD.posix_headers?=	build
 .endif	# POSIX_HEADERS_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
