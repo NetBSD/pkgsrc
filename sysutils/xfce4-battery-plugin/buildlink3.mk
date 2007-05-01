90ed378fe4b5c0d9f701c0287be2a92f46a0ff60
martti <martti@pkgsrc.org>
2007-05-01 17:41:59 +0000

    Updated sysutils/xfce4-battery-plugin to 0.5.0nb1
     - Don't change behavior of remaining time on Linux and FreeBSD.
     - Don't print remaining percentage as `100%' if there are no
       batteries.

--- sysutils/xfce4-battery-plugin/buildlink3.mk
+++ sysutils/xfce4-battery-plugin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2007/04/27 11:09:26 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2007/05/01 17:42:00 martti Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_BATTERY_PLUGIN_BUILDLINK3_MK:=	${XFCE4_BATTERY_PLUGIN_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	xfce4-battery-plugin
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-battery-plugin
 
 .if ${XFCE4_BATTERY_PLUGIN_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.xfce4-battery-plugin+=	xfce4-battery-plugin>=0.5.0
+BUILDLINK_API_DEPENDS.xfce4-battery-plugin+=	xfce4-battery-plugin>=0.5.0nb1
 BUILDLINK_PKGSRCDIR.xfce4-battery-plugin?=	../../sysutils/xfce4-battery-plugin
 .endif	# XFCE4_BATTERY_PLUGIN_BUILDLINK3_MK
 
