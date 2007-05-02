983af0820e9ac5c85df359454bd5edadffed4eea
martti <martti@pkgsrc.org>
2007-05-02 09:29:18 +0000

    Updated editors/vim-share to 7.0.239
      1777  7.0.238  crash when using ":match" pattern that runs into maxmempattern
      3886  7.0.239  session file has a problem with local directories

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.29 2007/05/01 17:42:29 martti Exp $
+# $NetBSD: version.mk,v 1.30 2007/05/02 09:29:18 martti Exp $
 
 VIM_VERSION=		7.0
-VIM_PATCHLEVEL=		237
+VIM_PATCHLEVEL=		239
 VIM_SUBDIR=		vim70
