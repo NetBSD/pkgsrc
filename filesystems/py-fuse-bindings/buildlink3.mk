437ef3a87eb6fe54bbe1bdcd8aa88ab2ebe164e5
agc <agc>
2007-05-04 19:51:52 +0000

    Initial import of python bindings (version 0.2pre1) for fuse and refuse into the Packages Collection.
    	Python bindings for the FUSE and refuse interfaces.
    
    	FUSE allows file sytsem operations to take place at user level, rather
    	than in the kernel.  Refuse is a BSD-licensed re-implementation of
    	fuse, running on top of the puffs functionality.  This package
    	provides a python language interface to the FUSE interface.
    
    	To achieve backwards compatibility with previous versions of the fuse
    	bindings for python, set the environment variable
    
    		FUSE_PYTHON_COMPAT=0.1

---
+++ filesystems/py-fuse-bindings/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/05/04 19:51:52 agc Exp $
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+PY24_FUSE_BINDINGS_BUILDLINK3_MK:=	${PY24_FUSE_BINDINGS_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	py24-fuse-bindings
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npy24-fuse-bindings}
+BUILDLINK_PACKAGES+=	py24-fuse-bindings
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}py24-fuse-bindings
+
+.if ${PY24_FUSE_BINDINGS_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.py24-fuse-bindings+=	py24-fuse-bindings>=0.2pre1
+BUILDLINK_PKGSRCDIR.py24-fuse-bindings?=	../../filesystems/py-fuse-bindings
+.endif	# PY24_FUSE_BINDINGS_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
