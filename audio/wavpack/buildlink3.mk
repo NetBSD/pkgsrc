d1d5cc8e376ef6ce1d48efe28727d391ca802224
tnn <tnn@pkgsrc.org>
2007-05-09 23:43:05 +0000

    Update to 4.41.0:
    fixed: corrupt floating-point audio on big-endian machines
    fixed: limit RIFF data buffering to 16 MB to prevent out-of-memory crash
    improved: attempt to mute errors when decoding corrupt legacy WavPack files
    improved: overall performance enhancements of 10% to 30% (depending on mode)
    added: MMX intrinsics for 24-bit (and higher) stereo encoding (thanks to
    	Joachim Henke)
    added: --skip and --until commands to unpack specified range of audio data

--- audio/wavpack/buildlink3.mk
+++ audio/wavpack/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/02/01 16:52:28 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2007/05/09 23:43:05 tnn Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 WAVPACK_BUILDLINK3_MK:=	${WAVPACK_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	wavpack
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}wavpack
 
 .if ${WAVPACK_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.wavpack+=	wavpack>=4.40.0
+BUILDLINK_API_DEPENDS.wavpack+=	wavpack>=4.41.0
 BUILDLINK_PKGSRCDIR.wavpack?=	../../audio/wavpack
 .endif	# WAVPACK_BUILDLINK3_MK
 
