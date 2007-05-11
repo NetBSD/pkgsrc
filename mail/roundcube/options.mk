1b746356e52b7aa235267fb2e593cd617b0abf38
schmonz <schmonz@pkgsrc.org>
2007-05-11 11:51:05 +0000

    Update to 20070508 snapshot. From the changelog:
    2007/05/01 (thomasb)
    ----------
    - Updated German, Euskara, Hungarian, Romanian and Spanish translation
    - Added Hindi and Kurdish localization
    
    2007/04/28 (thomasb)
    ----------
    - LDAP access is back in address book (closes #1484087)
    - Added search function for contacts
    - New Template parsing and output encoding
    - Fixed bugs #1484119 and #1483978
    
    2007/04/08 (thomasb)
    ----------
    - Fixed message moving procedure (closes #1484308)
    - Fixed display of multiple attachments (closes #1466563)
    - Fixed check for new messages (closes #1484310)
    - List attachments without filename
    
    2007/03/27 (thomasb)
    ----------
    - New session authentication: Change sessid cookie when login, authentication with sessauth cookie is now configurable.
      Should close bugs #1483951 and #1484299
    
    2007/03/23 (thomasb)
    ----------
    - Correctly translate mailbox names (closes #1484276)
    - Quote e-mail address links (closes #1484300)
    
    2007/03/21 (thomasb)
    ----------
    - Updated PEAR::Mail_mime package
    - Added Persian localization
    - Updated Catalan and Brazilian Portuguese translations
    - Updated INSTALL manual with a note about database passwords
    - Accept single quotes for HTML attributes when modifying message body (thanks Jason)
    - Sanitize input for new users/identities (thanks Colin Alston)
    
    2007/03/19 (thomasb)
    ----------
    - Don't download HTML message parts
    - Convert HTML parts to plaintext if 'prefer_html' is off
    - Correctly parse message/rfc822 parts (closes #1484045)
    - Code cleanup
    
    2007/03/18 (thomasb)
    ----------
    - Also use user_id for unique key in messages table (closes #1484074)
    - Hide contacts drop down on blur (closes #1484203)
    - Make entries in contacts drop down clickable
    - Turn off browser autocompletion on login page
    - Quote <? in text/html message parts
    - Hide border around radio buttons
    - Replaced old JS function calls.
    
    2007/03/13 (thomasb)
    ----------
    - Applied patch for attachment download by crichardson (closes #1484198)
    - Fixed bug in Postgres DB handling (closes #1484068)
    - Fixed bug of invalid calls to fetchRow() in rcube_db.inc (closes #1484280)
    - Fixed array_merge bug (closes #1484281)
    - Fixed flag for deletion in list view (closes #1484264)
    - Finally support semicolons as recipient separator (closes ##1484251)
    - Fixed message headers (subject) encoding
    
    pkgsrc changes:
    * Make iconv and mbstring dependencies PKG_OPTIONS ("iconv" and "multibyte")
    * Write logs and tempfiles under ${VARBASE}, not ${PREFIX}
    * Cosmetic and pkglint-appeasing tweaks

--- mail/roundcube/options.mk
+++ mail/roundcube/options.mk
@@ -1,14 +1,16 @@
-# $NetBSD: options.mk,v 1.1.1.1 2007/04/08 17:04:08 adrianp Exp $
+# $NetBSD: options.mk,v 1.2 2007/05/11 11:51:05 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.roundcube
 
 PKG_OPTIONS_REQUIRED_GROUPS=	db
 PKG_OPTIONS_GROUP.db=		mysql pgsql sqlite
 
-PKG_SUPPORTED_OPTIONS=	ldap
+PKG_SUPPORTED_OPTIONS=	iconv ldap multibyte
 PKG_SUGGESTED_OPTIONS=	mysql
 
 .include "../../mk/bsd.options.mk"
+
+.include "../../lang/php/phpversion.mk"
 
 ###
 ### Use mysql backend
@@ -36,8 +38,22 @@ DEPENDS+=	${PHP_PKG_PREFIX}-sqlite-[0-9]*:../../databases/php-sqlite
 .endif
 
 ###
+### Use iconv
+###
+.if !empty(PKG_OPTIONS:Miconv)
+DEPENDS+=	${PHP_PKG_PREFIX}-iconv>=4.3.1:../../converters/php-iconv
+.endif
+
+###
 ### Use OpenLDAP for storing data
 ###
 .if !empty(PKG_OPTIONS:Mldap)
 DEPENDS+=	${PHP_PKG_PREFIX}-ldap>=4.3.1:../../databases/php-ldap
+.endif
+
+###
+### Use mbstring
+###
+.if !empty(PKG_OPTIONS:Mmultibyte)
+DEPENDS+=	${PHP_PKG_PREFIX}-mbstring>=4.3.1:../../misc/php-mbstring
 .endif
