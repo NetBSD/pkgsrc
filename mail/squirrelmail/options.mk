54c795bbd6355a4e7ef9dd598465bf50f91a7444
obache <obache@pkgsrc.org>
2007-05-23 12:17:12 +0000

    Define PATCH files related variables strictly. works well even if define both options or one of the options.

--- mail/squirrelmail/options.mk
+++ mail/squirrelmail/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2007/05/22 11:13:27 taca Exp $
+# $NetBSD: options.mk,v 1.3 2007/05/23 12:17:12 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.squirrelmail
 PKG_SUPPORTED_OPTIONS=	squirrelmail-japanese squirrelmail-lite
@@ -6,13 +6,16 @@ PKG_SUPPORTED_OPTIONS=	squirrelmail-japanese squirrelmail-lite
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Msquirrelmail-japanese)
-PATCH_SITES=		http://www.yamaai-tech.com/~masato/Download/
-PATCHFILES=		squirrelmail-1.4.10a-ja-20070511-patch.gz
-PATCH_DIST_STRIP=	-p1
+PATCHFILES+=		squirrelmail-1.4.10a-ja-20070511-patch.gz
+SITES.squirrelmail-1.4.10a-ja-20070511-patch.gz= \
+			http://www.yamaai-tech.com/~masato/Download/
+PATCH_DIST_STRIP.squirrelmail-1.4.10a-ja-20070511-patch.gz=	-p1
 
 .endif
 
 .if !empty(PKG_OPTIONS:Msquirrelmail-lite)
-PATCH_SITES+=		ftp://ftp.back-street.net/pub/squirrelmail/
 PATCHFILES+=		squirrelmail-1.4.10a-lite-20070522-patch
+SITES.squirrelmail-1.4.10a-lite-20070522-patch=	\
+			ftp://ftp.back-street.net/pub/squirrelmail/
+PATCH_DIST_STRIP.squirrelmail-1.4.10a-lite-20070522-patch=	-p1
 .endif
