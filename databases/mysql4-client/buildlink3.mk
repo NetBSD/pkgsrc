1dfc3a208da98897890f7500bbf7dcbf180268ed
martti <martti@pkgsrc.org>
2007-05-23 08:11:48 +0000

    Updated MySQL to 4.1.22
    Lots of bug fixes, see http://dev.mysql.com/doc/refman/4.1/en/news-4-1-22.html

--- databases/mysql4-client/buildlink3.mk
+++ databases/mysql4-client/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.17 2006/07/08 23:10:40 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2007/05/23 08:11:48 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MYSQL_CLIENT_BUILDLINK3_MK:=	${MYSQL_CLIENT_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	mysql-client
 .endif
 
@@ -11,14 +11,14 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmysql-client}
 BUILDLINK_PACKAGES+=	mysql-client
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}mysql-client
 
-.if !empty(MYSQL_CLIENT_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.mysql-client+=	mysql-client>=4.1.7
-BUILDLINK_ABI_DEPENDS.mysql-client+=	mysql-client>=4.1.15nb1
+.if ${MYSQL_CLIENT_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.mysql-client+=	mysql-client>=4.1.22
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql4-client
 BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
 BUILDLINK_INCDIRS.mysql-client?=	include/mysql
 .endif	# MYSQL_CLIENT_BUILDLINK3_MK
 
+.include "../../devel/readline/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 
