73ed3af9310050401554051a5d0d2156bfc728bc
obache <obache@pkgsrc.org>
2007-05-26 16:38:21 +0000

    Need devel/glib2, graphics/cairo and x11/gtk2's buildlink3 files, for poppler-glib.pc.

--- print/poppler-glib/buildlink3.mk
+++ print/poppler-glib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/07/08 23:11:05 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2007/05/26 16:38:21 obache Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 POPPLER_GLIB_BUILDLINK3_MK:=	${POPPLER_GLIB_BUILDLINK3_MK}+
@@ -16,6 +16,9 @@ BUILDLINK_API_DEPENDS.poppler-glib+=	poppler-glib>=0.5.1
 BUILDLINK_PKGSRCDIR.poppler-glib?=	../../print/poppler-glib
 .endif	# POPPLER_GLIB_BUILDLINK3_MK
 
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
 .include "../../print/poppler/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
