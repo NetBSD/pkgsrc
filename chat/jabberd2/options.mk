e4a0fbe788791bdecb8a9fbcf098b23a085c8c2e
xtraeme <xtraeme@pkgsrc.org>
2007-05-26 11:50:12 +0000

    Update to 2.1.6 (last stable version).
    A lot of changes were made since previous update, like SASL support,
    SQLite support, etc. Please see the following URL for a full list
    of changes:
    
    http://jabberd2.xiaoka.com/wiki/Releases

--- chat/jabberd2/options.mk
+++ chat/jabberd2/options.mk
@@ -1,38 +1,87 @@
-# $NetBSD: options.mk,v 1.12 2007/02/22 19:26:12 wiz Exp $
+# $NetBSD: options.mk,v 1.13 2007/05/26 11:50:12 xtraeme Exp $
 #
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.jabberd2
-PKG_SUPPORTED_OPTIONS=	db ldap pam
-PKG_OPTIONS_OPTIONAL_GROUPS=	database
-PKG_OPTIONS_GROUP.database=	mysql pgsql
-PKG_SUGGESTED_OPTIONS=	mysql
+PKG_OPTIONS_VAR=		PKG_OPTIONS.jabberd2
+PKG_OPTIONS_REQUIRED_GROUPS=	auth storage sasl
+# Authentication backend
+PKG_OPTIONS_GROUP.auth=		auth-mysql auth-pgsql auth-sqlite
+PKG_OPTIONS_GROUP.auth+=	auth-db auth-ldap auth-pam
+# Storage backend
+PKG_OPTIONS_GROUP.storage=	storage-mysql storage-pgsql
+PKG_OPTIONS_GROUP.storage+=	storage-sqlite storage-db
+# SASL implementation
+PKG_OPTIONS_GROUP.sasl=		cyrus-sasl gnusasl
+PKG_SUGGESTED_OPTIONS=		auth-sqlite storage-sqlite gnusasl
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mdb)
-BUILD_DEFS+=		JABBERD_DBDIR
+.if !empty(PKG_OPTIONS:Mauth-db) || !empty(PKG_OPTIONS:Mstorage-db)
+BUILD_DEFS+=		VARBASE JABBERD_DBDIR
 JABBERD_DBDIR?=		${VARBASE}/db/jabberd
+PLIST_SUBST+=		DB_OPT=
 CONFIGURE_ARGS+=	--enable-db
 .  include "../../databases/db4/buildlink3.mk"
+.else
+PLIST_SUBST+=		DB_OPT='@comment '
+CONFIGURE_ARGS+=	--disable-db
 .endif
 
-.if !empty(PKG_OPTIONS:Mmysql)
+.if !empty(PKG_OPTIONS:Mauth-mysql) || !empty(PKG_OPTIONS:Mstorage-mysql)
+PLIST_SUBST+=		MYSQL_OPT=
 CONFIGURE_ARGS+=	--enable-mysql
 CPPFLAGS+=		-I${BUILDLINK_PREFIX.mysql-client}/include/mysql
 .  include "../../mk/mysql.buildlink3.mk"
+.else
+PLIST_SUBST+=		MYSQL_OPT='@comment '
+CONFIGURE_ARGS+=	--disable-mysql
 .endif
 
-.if !empty(PKG_OPTIONS:Mpgsql)
+.if !empty(PKG_OPTIONS:Mauth-pgsql) || !empty(PKG_OPTIONS:Mstorage-pgsql)
+PLIST_SUBST+=		PGSQL_OPT=
 CONFIGURE_ARGS+=	--enable-pgsql
 .  include "../../mk/pgsql.buildlink3.mk"
+.else
+PLIST_SUBST+=		PGSQL_OPT='@comment '
+CONFIGURE_ARGS+=	--disable-pgsql
 .endif
 
-.if !empty(PKG_OPTIONS:Mldap)
+.if !empty(PKG_OPTIONS:Mauth-sqlite) || !empty(PKG_OPTIONS:Mstorage-sqlite)
+PLIST_SUBST+=		SQLITE_OPT=
+CONFIGURE_ARGS+=	--enable-sqlite
+.  include "../../databases/sqlite3/buildlink3.mk"
+.else
+PLIST_SUBST+=		SQLITE_OPT='@comment '
+CONFIGURE_ARGS+=	--disable-sqlite
+.endif
+
+.if !empty(PKG_OPTIONS:Mauth-ldap)
+PLIST_SUBST+=		LDAP_OPT=
 CONFIGURE_ARGS+=	--enable-ldap
 .  include "../../databases/openldap-client/buildlink3.mk"
+.else
+PLIST_SUBST+=		LDAP_OPT='@comment '
+CONFIGURE_ARGS+=	--disable-ldap
 .endif
 
-.if !empty(PKG_OPTIONS:Mpam)
+.if !empty(PKG_OPTIONS:Mcyrus-sasl)
+CONFIGURE_ARGS+=	--enable-cyrus
+.  include "../../security/cyrus-sasl/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-cyrus
+.endif
+
+.if !empty(PKG_OPTIONS:Mgnusasl)
+CONFIGURE_ARGS+=	--enable-gsasl
+.  include "../../security/gsasl/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-gsasl
+.endif
+
+.if !empty(PKG_OPTIONS:Mauth-pam)
+PLIST_SUBST+=		PAM_OPT=
 CONFIGURE_ARGS+=	--enable-pam
 .  include "../../mk/pam.buildlink3.mk"
+.else
+PLIST_SUBST+=		PAM_OPT='@comment '
+CONFIGURE_ARGS+=	--disable-pam
 .endif
