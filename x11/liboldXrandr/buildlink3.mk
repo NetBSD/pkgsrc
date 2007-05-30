0e48d5ba4808bf3fa2fe086dfb491adff150756d
schmonz <schmonz@pkgsrc.org>
2007-05-30 01:22:12 +0000

    Move Xrandr to liboldXrandr (name suggested by joerg) so it can be checked out on a case-insensitive filesystem without interfering with lowercase xrandr. PKGNAME stays the same.
    Moved Xrandr to Xrandr-mixedcase in the repository [schmonz 2007-06-07]

---
+++ x11/liboldXrandr/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1 2007/05/30 01:22:12 schmonz Exp $
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+XRANDR_BUILDLINK3_MK:=	${XRANDR_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	Xrandr
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NXrandr}
+BUILDLINK_PACKAGES+=	Xrandr
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}Xrandr
+
+.if !empty(XRANDR_BUILDLINK3_MK:M+)
+BUILDLINK_API_DEPENDS.Xrandr+=	Xrandr>=0.99
+BUILDLINK_ABI_DEPENDS.Xrandr+=	Xrandr>=1.0.2nb3
+BUILDLINK_PKGSRCDIR.Xrandr?=	../../x11/Xrandr
+.endif	# XRANDR_BUILDLINK3_MK
+
+.include "../../x11/libXrender/buildlink3.mk"
+.include "../../x11/randrproto/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
