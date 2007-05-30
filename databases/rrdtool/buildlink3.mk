46b4ce8080e2a6e7187e82c23df8f55bd5a154b3
martti <martti@pkgsrc.org>
2007-05-30 07:27:46 +0000

    Regenerated.

--- databases/rrdtool/buildlink3.mk
+++ databases/rrdtool/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2006/07/21 16:17:55 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2007/05/30 07:27:46 martti Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 RRDTOOL_BUILDLINK3_MK:=	${RRDTOOL_BUILDLINK3_MK}+
@@ -12,14 +12,12 @@ BUILDLINK_PACKAGES+=	rrdtool
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}rrdtool
 
 .if ${RRDTOOL_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.rrdtool+=	rrdtool>=1.2.9
-BUILDLINK_ABI_DEPENDS.rrdtool+=	rrdtool>=1.2.12nb3
+BUILDLINK_API_DEPENDS.rrdtool+=	rrdtool>=1.2.23
 BUILDLINK_PKGSRCDIR.rrdtool?=	../../databases/rrdtool
 .endif	# RRDTOOL_BUILDLINK3_MK
 
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .include "../../graphics/libart2/buildlink3.mk"
-.include "../../www/cgilib/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
