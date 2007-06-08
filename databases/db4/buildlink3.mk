d03a6fb7e5cb32ca710354bf51442ee39a96c1f5
wiz <wiz@pkgsrc.org>
2007-06-08 12:23:50 +0000

    Update to 4.5.20.2:
    Multi-Version Concurrency Control (MVCC)
    improves performance of highly concurrent, mixed read/write systems
    by giving each user their own snapshot of the database and managing
    concurrent changes by many users
    
    Non-stop Upgrades
    enables a replicated Oracle Berkeley DB system to be upgraded
    without downtime. For customers that must maintain 99.999 percent
    system uptime, this allows them to upgrade the Oracle Berkeley DB
    database without taking down the entire system
    
    Replication Framework
    provides a pre-built and supported set of functions for quickly
    building replicated or highly available systems, simplifying and
    accelerating their development effort

--- databases/db4/buildlink3.mk
+++ databases/db4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.30 2006/12/12 21:52:35 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2007/06/08 12:23:51 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 DB4_BUILDLINK3_MK:=	${DB4_BUILDLINK3_MK}+
@@ -12,21 +12,11 @@ BUILDLINK_PACKAGES+=	db4
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}db4
 
 .if !empty(DB4_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.db4+=		db4>=4.4.16
+BUILDLINK_API_DEPENDS.db4+=	db4>=4.5.20
 BUILDLINK_PKGSRCDIR.db4?=	../../databases/db4
 BUILDLINK_INCDIRS.db4?=		include/db4
 BUILDLINK_LDADD.db4=		-ldb4
 BUILDLINK_TRANSFORM+=		l:db-4:db4
-
-.  include "../../mk/bsd.fast.prefs.mk"
-.  if defined(USE_DB185) && !empty(USE_DB185:M[yY][eE][sS])
-#
-# Older db4 packages didn't enable the db-1.85 compatibility API.
-#
-BUILDLINK_API_DEPENDS.db4+=		db4>=4.2.52nb1
-BUILDLINK_LIBS.db4=		${BUILDLINK_LDADD.db4}
-BUILDLINK_TRANSFORM+=		l:db:db4
-.  endif
 
 .  include "../../mk/compiler.mk"
 .  if empty(PKGSRC_COMPILER:Mgcc)
