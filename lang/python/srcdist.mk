8ebe653aaf876f844878e2a34d993f3ddc422de1
tron <tron@pkgsrc.org>
2007-06-08 18:50:05 +0000

    Correct version number for Python 2.4.x.

--- lang/python/srcdist.mk
+++ lang/python/srcdist.mk
@@ -1,14 +1,14 @@
-# $NetBSD: srcdist.mk,v 1.18 2006/04/12 20:21:48 drochner Exp $
+# $NetBSD: srcdist.mk,v 1.19 2007/06/08 18:50:05 tron Exp $
 
 .include "../../lang/python/pyversion.mk"
 
 .if ${_PYTHON_VERSION} == "24"
 
-DISTNAME=	Python-2.4.3
+DISTNAME=	Python-2.4.4
 EXTRACT_SUFX=	.tar.bz2
 DISTINFO_FILE=	${.CURDIR}/../../lang/python24/distinfo
 PATCHDIR=	${.CURDIR}/../../lang/python24/patches
-PYSUBDIR=	Python-2.4.3
+PYSUBDIR=	Python-2.4.4
 WRKSRC=		${WRKDIR}/${PYSUBDIR}
 MASTER_SITES=	ftp://ftp.python.org/pub/python/2.4/
 
