89c85ebe1e73633d8c16076bc8de273f567e10d8
dmcmahill <dmcmahill@pkgsrc.org>
2007-06-12 03:02:33 +0000

    Update to guile-1.8.1.  Note that the 1.6.x series of guile is still available in lang/guile16.  Many, many changes since the last 1.6.x release including bug fixes, build fixes, and many feature additions.  See the NEWS file for the full (> 1,000 lines) list of changes.

--- lang/guile/buildlink3.mk
+++ lang/guile/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/07/08 23:10:55 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2007/06/12 03:02:34 dmcmahill Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GUILE_BUILDLINK3_MK:=	${GUILE_BUILDLINK3_MK}+
@@ -12,11 +12,12 @@ BUILDLINK_PACKAGES+=	guile
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}guile
 
 .if !empty(GUILE_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.guile+=	guile>=1.6.3nb1
-BUILDLINK_ABI_DEPENDS.guile+=	guile>=1.6.7nb3
+BUILDLINK_API_DEPENDS.guile+=	guile>=1.8.1
+BUILDLINK_ABI_DEPENDS.guile+=	guile>=1.8.1
 BUILDLINK_PKGSRCDIR.guile?=	../../lang/guile
 .endif	# GUILE_BUILDLINK3_MK
 
+.include "../../devel/gmp/buildlink3.mk"
 .include "../../devel/libltdl/buildlink3.mk"
 .include "../../devel/ncurses/buildlink3.mk"
 .include "../../devel/readline/buildlink3.mk"
