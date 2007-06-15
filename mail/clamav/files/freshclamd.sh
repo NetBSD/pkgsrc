2fec7af1c93bf12a1190e14784bc17e7234b9a5d
wiz <wiz@pkgsrc.org>
2007-06-15 22:31:39 +0000

    Improvements for freshclamd script from Sergey Svishchev: For some reason, the script creates the pid file itself, instead of using '-p' option to freshclamd.  sig_stop=KILL seems unnecessary, too.
    Bump PKGREVISION.

--- mail/clamav/files/freshclamd.sh
+++ mail/clamav/files/freshclamd.sh
@@ -1,26 +1,20 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: freshclamd.sh,v 1.3 2006/06/20 20:24:45 jnemeth Exp $
+# $NetBSD: freshclamd.sh,v 1.4 2007/06/15 22:31:39 wiz Exp $
 #
 # PROVIDE: freshclamd
 # REQUIRE: DAEMON LOGIN clamd
-
 
 name="freshclamd"
 rcvar=$name
 command="@PREFIX@/bin/freshclam"
 required_files="@PKG_SYSCONFDIR@/freshclam.conf"
-pidfile="@VARBASE@/run/${name}.pid"
-sig_stop="KILL"
+pidfile="@VIRUSDBDIR@/${name}.pid"
 freshclamd_user="@CLAMAV_USER@"
 freshclamd_flags=${freshclamd_flags:=-c 2}
-command_args="-d"
+command_args="-d -p $pidfile"
 
 . /etc/rc.subr
 
 load_rc_config $name
 run_rc_command "$1"
-
-if [ "$1" != "stop" ]; then
-	echo $(check_process $command) > $pidfile
-fi
