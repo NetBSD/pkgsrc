050fb1306b149b1eac0b44f15baa42345317d434
wiz <wiz@pkgsrc.org>
2007-06-17 22:06:33 +0000

    Fix path to pidfile. Noted by Peter Avalos. Bump PKGREVISION.

--- mail/clamav/files/freshclamd.sh
+++ mail/clamav/files/freshclamd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: freshclamd.sh,v 1.4 2007/06/15 22:31:39 wiz Exp $
+# $NetBSD: freshclamd.sh,v 1.5 2007/06/17 22:06:33 wiz Exp $
 #
 # PROVIDE: freshclamd
 # REQUIRE: DAEMON LOGIN clamd
@@ -9,7 +9,7 @@ name="freshclamd"
 rcvar=$name
 command="@PREFIX@/bin/freshclam"
 required_files="@PKG_SYSCONFDIR@/freshclam.conf"
-pidfile="@VIRUSDBDIR@/${name}.pid"
+pidfile="@CLAMAV_DBDIR@/${name}.pid"
 freshclamd_user="@CLAMAV_USER@"
 freshclamd_flags=${freshclamd_flags:=-c 2}
 command_args="-d -p $pidfile"
