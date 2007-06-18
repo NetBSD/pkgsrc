e298bbf31451e6e533ea2a90af2e2ae8ecd6a0b7
rillig <rillig@pkgsrc.org>
2007-06-18 07:33:34 +0000

    Changed the plain "mkdir" into ${MKDIR}, like in all the other cases. Apparently, the tools directory isn't in the PATH at the point where this code is executed.
    Fixes PR 35487.

--- mk/wrapper/bsd.wrapper.mk
+++ mk/wrapper/bsd.wrapper.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.wrapper.mk,v 1.69 2007/06/06 13:20:34 rillig Exp $
+# $NetBSD: bsd.wrapper.mk,v 1.70 2007/06/18 07:33:34 rillig Exp $
 #
 # Copyright (c) 2005 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -590,7 +590,7 @@ ${_WRAP_BUILDCMD.${_wrappee_}}: ${WRAPPER_SRCDIR}/buildcmd
 
 .  if !target(${_WRAP_CACHE.${_wrappee_}})
 ${_WRAP_CACHE.${_wrappee_}}:
-	${RUN} mkdir ${.TARGET:H}
+	${RUN} ${MKDIR} ${.TARGET:H}
 	${RUN} echo "cachehit=no" > ${.TARGET}
 .  endif
 
