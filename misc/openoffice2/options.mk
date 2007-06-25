1a1beac15298a831917a878c01351cc6c8fd390b
salo <salo>
2007-06-25 15:37:45 +0000

    Pullup ticket 2117 - requested by lkundrak security update for openoffice2
    Updated via patch from hira, the MAINTAINER.
    
       Module Name:		pkgsrc
       Committed By:	hira
       Date:		Fri Jun 22 03:52:21 UTC 2007
    
       Modified Files:
       	pkgsrc/misc/openoffice2: Makefile distinfo
    
       Log Message:
       Update to 2.2.1 during freeze because of security fixes (CVE-2007-0245
       and CVE-2007-2754).
    
       Changes from 2.2.0:
        * freetypettg: fix CVE-2007-2754 for the internal freetype copy
        * autotext02: A showstopper fix for OOo2.2.1
        * l10n77316fix: contains fix for i77316
        * notepaint: Fix for invisible text in Calc notes while editing
        * macosxpbuildrepair_OOF680:
          This cws fixes broken Mac OS X 10.3 build, and does contain :
            - a fix for a build breaker in slideshow ( fixed by thb ) #i74325#
            - gcc-3.3 parser fixes #i75990#
            - a complete fix in sal for security.c ( with sb help ) #i76159#
            - a fix for broken linking ( because of extra symbols, due to bad
              filtering ) #i72922#
            - a fix for main.applescript ( build borken on Panther ) #i75972#
          All issue targeted 2.2.1 ( supposing the changes will be integrated
          in SRC680 too)
        * ause078_OOF680: quick and small to get back correct dependencies
        * os95_OOF680,plthes: add license-fixed polish thesaurus
        * olenoserver: A fix for a regression.
        * hro15: Unicode command line support fix for Windows only.
        * custompropsfix: Small bug fix in Word import for import of document
                          variables
        * dba221b: fix issue 73722, in its incarnation as issue 76434, for 2.2.1.
        * swvalgrind: Fix for i76133
        * calc221: Calc fixes for OOo2.2.1
        * fix75967: fix issue 75967
        * c03v8,c07v012,native86,nativebroffice: New Product BrOffice.org
        * impress120: Bugfix workspace for OOo 2.2.1
       	sj->wg: the performance test hasn|t been finished yet, but I think
               it should be no problem to hand over this issue without the test.
        * larsbehr01: Bugfix for i66661: Slideshow bug fixing for 2.2.1
        * dba221a: ongoing DBA-related bugfixing towards OOo 2.2.1
        * sch17: Fixes for sch
        * tbe29: OOo 2.2.1 accessibility bug fixes
        * impress119: Bug fixes for OOo 2.2.1.
        * printhelpfix: Bugfix for 134037.
        * autotext01: A showstopper cws related to autotext bug.
        * native79: Preparing OOo 2.2.1 and SO 8 Update 7
        * vcl73_OOF680: 2.2.1 issues
        * cmcfixes32_OOF680: minor customized build fixes
        * jl57_OOF680: Contains a patch for building with an older gcc compiler.
        * fsfixes06_OOF680: Fix for a bug in WordPerfect typedetection that caused
                            us to crash on unsupported documents instead of refusing
                            them with grace
        * jl62,oasisrng02: Fixes of violations of the RelaxNG schema in ODF.
        * aw050: OOo 2.2.1 BugFixing
        * swqbugfix01: Issues in Writer regarding saving of documents

--- misc/openoffice2/options.mk
+++ misc/openoffice2/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.7 2007/03/30 13:34:53 hira Exp $
+# $NetBSD: options.mk,v 1.7.2.1 2007/06/25 15:37:46 salo Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.openoffice2
-PKG_SUPPORTED_OPTIONS=		cups gnome gtk2 # kde
-PKG_OPTIONS_REQUIRED_GROUPS=	browser lang
+PKG_SUPPORTED_OPTIONS=		cups gnome gtk2 kde
+PKG_OPTIONS_REQUIRED_GROUPS=	browser
 PKG_OPTIONS_GROUP.browser=	firefox # seamonkey firefox-gtk1 seamonkey-gtk1
 OO_SUPPORTED_LANGUAGES=		en-US af as-IN be-BY bg br bs ca cs cy da de \
 				el en-GB en-ZA eo es et fa fi fr ga gu-IN he \
@@ -12,7 +12,7 @@ OO_SUPPORTED_LANGUAGES=		en-US af as-IN be-BY bg br bs ca cs cy da de \
 				ta-IN te-IN tg th ti-ER tn tr ts uk ur-IN ve \
 				vi xh zh-CN zh-TW zu
 .for l in ${OO_SUPPORTED_LANGUAGES}
-PKG_OPTIONS_GROUP.lang+=	lang-${l}
+PKG_SUPPORTED_OPTIONS+=		lang-${l}
 .endfor
 PKG_SUGGESTED_OPTIONS=		gtk2 lang-en-US firefox
 PKG_OPTIONS_LEGACY_OPTS+=	gnome-vfs:gnome
@@ -20,61 +20,64 @@ PKG_OPTIONS_LEGACY_OPTS+=	gnome-vfs:gnome
 .include "../../mk/bsd.options.mk"
 .include "../../mk/bsd.prefs.mk"
 
-OPENOFFICE_LANGUAGE=		${PKG_OPTIONS:Mlang-*:S/^lang-//1}
+.for l in ${PKG_OPTIONS:Mlang-*}
+OO_LANGS+=	${l:S/^lang-//1}
+.endfor
+OO_LANGS?=	en-US
 
 .if !empty(PKG_OPTIONS:Mfirefox)
-.include "../../www/firefox/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-system-mozilla --with-firefox
+.include "../../www/firefox/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mfirefox-gtk1)
-.include "../../www/firefox-gtk1/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-system-mozilla --with-firefox
+.include "../../www/firefox-gtk1/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mseamonkey)
-.include "../../www/seamonkey/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-system-mozilla
+.include "../../www/seamonkey/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mseamonkey-gtk1)
-.include "../../www/seamonkey-gtk1/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-system-mozilla
+.include "../../www/seamonkey-gtk1/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mcups)
-.include "../../print/cups/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-cups
+.include "../../print/cups/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-cups
 .endif
 
 .if !empty(PKG_OPTIONS:Mgnome)
+CONFIGURE_ARGS+=	--enable-gnome-vfs --enable-evolution2
 .include "../../devel/GConf2/buildlink3.mk"
 .include "../../devel/libbonobo/buildlink3.mk"
 .include "../../sysutils/gnome-vfs2/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-gnome-vfs --enable-evolution2
 .else
 CONFIGURE_ARGS+=	--disable-gnome-vfs --disable-evolution2
 .endif
 
 .if !empty(PKG_OPTIONS:Mgtk2)
-.include "../../x11/gtk2/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-gtk
 # *.pc files are needed with NetBSD using native XFree86.
-.if ${OPSYS} == "NetBSD" && defined(X11_TYPE) && !empty(X11_TYPE:Mnative)
+.  if ${OPSYS} == "NetBSD" && defined(X11_TYPE) && !empty(X11_TYPE:Mnative)
 USE_BUILTIN.Xfixes=	no
 USE_BUILTIN.Xrandr=	no
 USE_BUILTIN.Xrender=	no
-.endif
+.  endif
+.include "../../x11/gtk2/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-gtk
 .endif
 
-# XXX: Not yet.
 .if !empty(PKG_OPTIONS:Mkde)
-.include "../../x11/kdelibs3/buildlink3.mk"
+CONFIGURE_ENV+=		KDEDIR=${BUILDLINK_PREFIX.kdelibs:Q}
 CONFIGURE_ARGS+=	--enable-kde --enable-kdeab
+.include "../../x11/kdelibs3/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-kde --disable-kdeab
 .endif
