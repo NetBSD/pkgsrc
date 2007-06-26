f24be610fc2587b0efc636605742fa8eee75be6e
joerg <joerg@pkgsrc.org>
2007-06-26 14:47:55 +0000

    API depends of multimedia/totem/buildlink3.mk allows both totem and totem-xine, ABI depends only the former. This creates conflicts when pkg_add picks the latter to fulfill the API depends. Allow both packages for the ABI depends as well and bump the revisions of the two affected packages.

--- multimedia/totem/buildlink3.mk
+++ multimedia/totem/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2007/06/05 05:37:05 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2007/06/26 14:47:55 joerg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 TOTEM_BUILDLINK3_MK:=	${TOTEM_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}totem
 
 .if !empty(TOTEM_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.totem+=	{totem,totem-xine}>=1.4.0
-BUILDLINK_ABI_DEPENDS.totem?=	totem>=2.18.1nb1
+BUILDLINK_ABI_DEPENDS.totem?=	{totem,totem-xine}>=2.18.1nb1
 BUILDLINK_PKGSRCDIR.totem?=	../../multimedia/totem
 .endif	# TOTEM_BUILDLINK3_MK
 
