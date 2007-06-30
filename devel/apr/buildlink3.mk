7796dae6c24cf4ba3097ea580437f08edce56821
joerg <joerg@pkgsrc.org>
2007-06-30 15:04:55 +0000

    Make live easier for Apache 2.2 modules and provide apr-config and apu-config in .buildlink/bin.

--- devel/apr/buildlink3.mk
+++ devel/apr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.30 2007/05/29 22:39:17 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2007/06/30 15:04:55 joerg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 APR_BUILDLINK3_MK:=	${APR_BUILDLINK3_MK}+
@@ -18,6 +18,11 @@ BUILDLINK_INCDIRS.apr?=		include/apr-1
 
 BUILDLINK_FILES.apr+=	bin/apr-1-config
 BUILDLINK_FILES.apr+=	lib/apr.exp
+
+${BUILDLINK_DIR}/bin/apr-config: buildlink-directories
+	${MKDIR} ${BUILDLINK_DIR}/bin && ${LN} -fs apr-1-config ${BUILDLINK_DIR}/bin/apr-config
+
+buildlink-apr-cookie: ${BUILDLINK_DIR}/bin/apr-config
 
 .endif	# APR_BUILDLINK3_MK
 
