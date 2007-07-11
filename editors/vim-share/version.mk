76fe1ab7247564943b22aaf928a6065b18f6bde1
martti <martti@pkgsrc.org>
2007-07-11 12:20:53 +0000

    Updated editors/vim-share to 7.1.28
      1650  7.1.019  ":python" doesn't mention the command is not implemented
      3229  7.1.020  reading uninitialized memory when using a dialog
      2875  7.1.021  (after 7.1.015) MzScheme interface doesn't build on Win32
      1842  7.1.022  when setting 'keymap' twice b:keymap_name variable isn't set
      1718  7.1.023  "dw" in a line with one character deletes the line
      1335  7.1.024  using a pointer that may have become invalid
      2292  7.1.025  search('pat', 'bc) doesn't find word under cursor at BOL
      1710  7.1.026  "[p" doesn't work in Visual mode
      3345  7.1.027  on Sun OS reading from a process substitutions doesn't work
      2239  7.1.028  ":sort" does nothing special with empty search pattern

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.33 2007/07/02 07:47:59 martti Exp $
+# $NetBSD: version.mk,v 1.34 2007/07/11 12:20:54 martti Exp $
 
 VIM_VERSION=		7.1
-VIM_PATCHLEVEL=		18
+VIM_PATCHLEVEL=		28
 VIM_SUBDIR=		vim71
