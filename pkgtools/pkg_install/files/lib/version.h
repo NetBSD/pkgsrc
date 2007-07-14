e067a8f9e2cfde7c3036c6526f402d66b8e5bb13
joerg <joerg>
2007-07-14 22:57:15 +0000

    Merge 1.75 of lib/ftpio.c from NetBSD:
    When fixing vsftpd, other FTP server were broken as they return
    different error codes for unmatching glob patterns. Basically ignore the
    error code and just try the other pattern. chdir errors are still
    considered fatal.
    
    Bump version to 20070715.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.66 2007/07/14 20:19:48 adrianp Exp $	*/
+/*	$NetBSD: version.h,v 1.67 2007/07/14 22:57:15 joerg Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20070714"
+#define PKGTOOLS_VERSION "20070715"
 
 #endif /* _INST_LIB_VERSION_H_ */
