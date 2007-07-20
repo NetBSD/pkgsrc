9ee531f0a081c0c832a2431e6814c1dd8703124d
joerg <joerg@pkgsrc.org>
2007-07-20 22:22:52 +0000

    Split pattern related functions from lib/str.c off into lib/opattern.c. Rename pmatch to pkg_match and add new function pkg_order that implements the order logic used by pkg_add internally.
    Change the interface of findmatchingname's callback to take the pattern
    used for the matching as first argument. The new pkg_order function
    takes it and it doesn't hurt in other cases. Adjust callers and
    corresponding callbacks accordingly.
    
    Remove an unused matchfn typedef in dewey.c.
    
    Bump to 20070720. OK jlam@

--- pkgtools/pkg_install/files/lib/dewey.c
+++ pkgtools/pkg_install/files/lib/dewey.c
@@ -1,4 +1,4 @@
-/* $NetBSD: dewey.c,v 1.5 2006/04/04 06:30:10 wiz Exp $ */
+/* $NetBSD: dewey.c,v 1.6 2007/07/20 22:22:53 joerg Exp $ */
 
 /*
  * Copyright © 2002 Alistair G. Crooks.  All rights reserved.
@@ -44,7 +44,6 @@
 #include "dewey.h"
 
 #define PKG_PATTERN_MAX 1024
-typedef int (*matchfn) (const char *, void *);
 
 /* do not modify these values, or things will NOT work */
 enum {
