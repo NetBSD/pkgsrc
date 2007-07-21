7b3974b643658b00b8e53209f0c1dca09be55244
salo <salo>
2007-07-21 01:19:08 +0000

    Pullup ticket 2142 - requested by joerg remove PKG_TOOLS_PREFIX handling from bootstrap tools
    Revisions pulled up:
    - pkgsrc/pkgtools/mtree/Makefile				1.22
    - pkgsrc/pkgtools/mtree/Makefile.common				1.3
    - pkgsrc/pkgtools/pax/Makefile					1.15
    - pkgsrc/pkgtools/pkg_install/Makefile				1.131
    - pkgsrc/pkgtools/pkg_install/files/lib/version.h		1.65
    - pkgsrc/pkgtools/tnftp/Makefile				1.3
    
       Module Name:		pkgsrc
       Committed By:	joerg
       Date:		Tue Jul 10 15:19:18 UTC 2007
    
       Modified Files:
       	pkgsrc/pkgtools/mtree: Makefile Makefile.common
       	pkgsrc/pkgtools/pax: Makefile
       	pkgsrc/pkgtools/pkg_install: Makefile
       	pkgsrc/pkgtools/pkg_install/files/lib: version.h
       	pkgsrc/pkgtools/tnftp: Makefile
    
       Log Message:
       Remove PKG_TOOLS_PREFIX handling for mtree, pax, pkg_install and
       tnftp. Modifying files and possibly removing them from the base system
       is doing more harm than use. Bump revision or version of each
       package.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.64 2007/04/16 12:55:35 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.64.2.1 2007/07/21 01:19:08 salo Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20070416"
+#define PKGTOOLS_VERSION "20070710"
 
 #endif /* _INST_LIB_VERSION_H_ */
