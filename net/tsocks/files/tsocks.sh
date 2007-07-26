70371272b3e7ab0406b2b0fe409638c536acbb36
rillig <rillig@pkgsrc.org>
2007-07-26 12:27:18 +0000

    Changed tsocks.sh so that it matches the man page. PKGREVISION++

--- net/tsocks/files/tsocks.sh
+++ net/tsocks/files/tsocks.sh
@@ -1,9 +1,58 @@
-#!@SH@
+#! @SH@
+
+libtsocks="@PREFIX@/lib/libtsocks.so"
+
+usage() {
+	echo "usage:"
+	echo "  $0 [program [arguments...]]"
+	echo "  . $0 on"
+	echo "  . $0 off"
+}
+
+tsocks_on() {
+	case $LD_PRELOAD in
+	*"$libtsocks"*)
+		;;
+	"")	LD_PRELOAD="$libtsocks"
+		;;
+	*)	LD_PRELOAD="$LD_PRELOAD $libtsocks"
+		;;
+	esac
+	export LD_PRELOAD
+}
+
+tsocks_off() {
+	LD_PRELOAD=`echo $LD_PRELOAD | sed 's,$libtsocks[ :]?,,'`
+	if [ "$LD_PRELOAD" ]; then
+		export LD_PRELOAD
+	else
+		unset LD_PRELOAD
+	fi
+}
 
 if [ $# -eq 0 ]; then
-	echo "usage: `basename $0` command" 1>&2
-	exit 1
+	set -- ${SHELL:-/bin/sh}
 fi
 
-LD_PRELOAD="@PREFIX@/lib/libtsocks.so"; export LD_PRELOAD
-exec $*
+case "$1" in
+on)
+	tsocks_on
+	;;
+off)
+	tsocks_off
+	;;
+
+show | sh)
+	echo "LD_PRELOAD=\"$LD_PRELOAD\""
+	;;
+-h | -?)
+	usage
+	;;
+-*)	usage 1>&2
+	exit 1
+	;;
+*)
+	tsocks_on
+	exec "$@"
+	;;
+esac
