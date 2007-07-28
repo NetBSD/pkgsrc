ef3a0c262b3285b3ac5c8b19a8dd9ae00124fc15
gdt <gdt@pkgsrc.org>
2007-07-28 12:32:17 +0000

    Fix stray wip references.

--- chat/libpurple/buildlink3.mk
+++ chat/libpurple/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/07/28 12:10:43 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2007/07/28 12:32:17 gdt Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBPURPLE_BUILDLINK3_MK:=	${LIBPURPLE_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libpurple
 
 .if ${LIBPURPLE_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.libpurple+=	libpurple>=0.0.1
-BUILDLINK_PKGSRCDIR.libpurple?=	../../wip/libpurple
+BUILDLINK_PKGSRCDIR.libpurple?=	../../chat/libpurple
 .endif	# LIBPURPLE_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
