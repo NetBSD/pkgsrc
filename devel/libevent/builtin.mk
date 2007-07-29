91530ef5a7c180009e9e5091e976c9c1ed1c43c0
obache <obache>
2007-07-29 06:23:04 +0000

    Detect built-in libevent version. Need for net/spreadload (PR 36348).

--- devel/libevent/builtin.mk
+++ devel/libevent/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.5 2006/04/06 06:21:50 reed Exp $
+# $NetBSD: builtin.mk,v 1.6 2007/07/29 06:23:04 obache Exp $
 
 BUILTIN_PKG:=	libevent
 
@@ -18,6 +18,72 @@ IS_BUILTIN.libevent=	yes
 .  endif
 .endif	# IS_BUILTIN.libevent
 MAKEVARS+=	IS_BUILTIN.libevent
+
+###
+### If there is a built-in implementation, then set BUILTIN_PKG.<pkg> to
+### a package name to represent the built-in package.
+###
+.if !defined(BUILTIN_PKG.libevent) && \
+    !empty(IS_BUILTIN.libevent:M[yY][eE][sS]) && \
+    empty(H_LIBEVENT:M__nonexistent__)
+# libevent>=1.3: bufferevent_base_set added
+_BLTN_EVENT_13!=	\
+	${GREP} -c bufferevent_base_set ${H_LIBEVENT} || ${TRUE}
+# libevent>=1.2: evtag_*, event_base_free added
+_BLTN_EVENT_12!=	\
+	${GREP} -c evtag_ ${H_LIBEVENT} || ${TRUE}
+# libevent>=1.1b: evbuffer_add_vprintf added
+_BLTN_EVENT_11b!=	\
+	${GREP} -c evbuffer_add_vprintf ${H_LIBEVENT} || ${TRUE}
+# libevent>=1.0d: evbuffer_readline added
+_BLTN_EVENT_10d!=	\
+	${GREP} -c event_base_loopexit ${H_LIBEVENT} || ${TRUE}
+# libevent>=1.0c: event_set_log_callback, event_base_loopexit and
+#                 event_get_{version,method} added
+_BLTN_EVENT_10c!=	\
+	${GREP} -c event_base_loopexit ${H_LIBEVENT} || ${TRUE}
+# libevent>=1.0: event_base_* and event_priority_* added
+_BLTN_EVENT_10!=	\
+	${GREP} -c event_base_ ${H_LIBEVENT} || ${TRUE}
+# libevent>=0.9: evbuffer_{expand,remove} added
+_BLTN_EVENT_09!=	\
+	${GREP} -c evbuffer_expand ${H_LIBEVENT} || ${TRUE}
+# libevent>=0.8: buffering functions added
+_BLTN_EVENT_08!=	\
+	${GREP} -c evbuffer ${H_LIBEVENT} || ${TRUE}
+# libevent>=0.7b: WIN32 support added
+_BLTN_EVENT_07b!=	\
+	${GREP} -c WIN32 ${H_LIBEVENT} || ${TRUE}
+# libevent>=0.6: evtimer_* added
+_BLTN_EVENT_06!=	\
+	${GREP} -c evtimer_ ${H_LIBEVENT} || ${TRUE}
+
+.  if ${_BLTN_EVENT_13} == "1"
+BUILTIN_VERSION.libevent=	1.3
+.  elif ${_BLTN_EVENT_12} == "1"
+BUILTIN_VERSION.libevent=	1.2
+.  elif ${_BLTN_EVENT_11b} == "1"
+BUILTIN_VERSION.libevent=	1.1b
+.  elif ${_BLTN_EVENT_10d} == "1"
+BUILTIN_VERSION.libevent=	1.0d
+.  elif ${_BLTN_EVENT_10c} == "1"
+BUILTIN_VERSION.libevent=	1.0c
+.  elif ${_BLTN_EVENT_10} == "1"
+BUILTIN_VERSION.libevent=	1.0
+.  elif ${_BLTN_EVENT_09} == "1"
+BUILTIN_VERSION.libevent=	0.9
+.  elif ${_BLTN_EVENT_08} == "1"
+BUILTIN_VERSION.libevent=	0.8
+.  elif ${_BLTN_EVENT_07b} == "1"
+BUILTIN_VERSION.libevent=	0.7b
+.  elif ${_BLTN_EVENT_06} == "1"
+BUILTIN_VERSION.libevent=	0.6
+.  else
+BUILTIN_VERSION.libevent=	0.5
+.  endif
+BUILTIN_PKG.libevent=	libevent-${BUILTIN_VERSION.libevent}
+.endif
+MAKEVARS+=	BUILTIN_PKG.libevent
 
 ###
 ### Determine whether we should use the built-in implementation if it
