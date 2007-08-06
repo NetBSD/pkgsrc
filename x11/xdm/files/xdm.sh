0f54c1adc4b60bbdf56c2994c7b0d12049f1c5cb
tron <tron@pkgsrc.org>
2007-08-06 16:43:40 +0000

    Fix bugs in "rc.d" script, bump package revision.

--- x11/xdm/files/xdm.sh
+++ x11/xdm/files/xdm.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: xdm.sh,v 1.1.1.1 2007/08/06 14:56:33 tron Exp $
+# $NetBSD: xdm.sh,v 1.2 2007/08/06 16:43:40 tron Exp $
 #
 
 # PROVIDE: xdm
@@ -12,7 +12,8 @@ $_rc_subr_loaded . /etc/rc.subr
 name="xdm"
 rcvar=$name
 xdm_config="/etc/X11/${name}/${name}-config"
-command="@PREFIX@/bin/${name} -config ${xdm_config}"
+command="@PREFIX@/bin/${name}"
+command_args="-config ${xdm_config}"
 pidfile="/var/run/${name}.pid"
 required_files="${xdm_config}"
 extra_commands="reload"
