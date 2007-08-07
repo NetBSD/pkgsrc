7ad5482183ec8d2f0c555c9f4d671fcdf5393161
lukem <lukem@pkgsrc.org>
2007-08-07 04:55:09 +0000

    No support for SOCKS4 in tnftp 20070806.

--- net/tnftp/options.mk
+++ net/tnftp/options.mk
@@ -1,16 +1,13 @@
-# $NetBSD: options.mk,v 1.1 2005/12/02 17:03:06 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2007/08/07 04:55:09 lukem Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tnftp
 PKG_SUPPORTED_OPTIONS=		inet6
 PKG_OPTIONS_OPTIONAL_GROUPS+=	socks
-PKG_OPTIONS_GROUP.socks=	socks4 socks5
+PKG_OPTIONS_GROUP.socks=	socks5
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Msocks4)
-CONFIGURE_ARGS+=	--with-socks4
-.include "../../net/socks4/buildlink3.mk"
-.elif !empty(PKG_OPTIONS:Msocks5)
+.if !empty(PKG_OPTIONS:Msocks5)
 CONFIGURE_ARGS+=	--with-socks5
 .include "../../net/socks5/buildlink3.mk"
 .endif
