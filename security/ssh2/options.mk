de33c8f09702309732217d84d7e4835a7a89017c
tnn <tnn@pkgsrc.org>
2007-08-09 20:15:38 +0000

    Enable modular X.org support, PR pkg/36760.

--- security/ssh2/options.mk
+++ security/ssh2/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2006/02/04 00:33:17 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2007/08/09 20:15:38 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ssh2
 PKG_SUPPORTED_OPTIONS=	x11
@@ -9,7 +9,13 @@ PKG_SUGGESTED_OPTIONS=	x11
 .if !empty(PKG_OPTIONS:Mx11)
 CONFIGURE_ARGS+=	--with-x
 PLIST_SUBST+=		X11_SUPPORT=
-.include "../../mk/x11.buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+BUILDLINK_DEPMETHOD.libXt?=	build
+.include "../../x11/libXt/buildlink3.mk"
+.if ${X11_TYPE} == "modular"
+DEPENDS+=		xauth>=1.0.2:../../x11/xauth
+CONFIGURE_ENV+=		ac_cv_path_XAUTH_PATH=${PREFIX:Q}/bin/xauth
+.endif
 .else
 CONFIGURE_ARGS+=	--without-x
 PLIST_SUBST+=		X11_SUPPORT='@comment '
