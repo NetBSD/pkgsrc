8133520391ca8e064b3f6cd42cbf8a2a1a11ab93
tnn <tnn>
2007-08-09 20:28:10 +0000

    for modular, also needs libSM.

--- security/ssh2/options.mk
+++ security/ssh2/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2007/08/09 20:15:38 tnn Exp $
+# $NetBSD: options.mk,v 1.3 2007/08/09 20:28:10 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ssh2
 PKG_SUPPORTED_OPTIONS=	x11
@@ -10,6 +10,7 @@ PKG_SUGGESTED_OPTIONS=	x11
 CONFIGURE_ARGS+=	--with-x
 PLIST_SUBST+=		X11_SUPPORT=
 .include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
 BUILDLINK_DEPMETHOD.libXt?=	build
 .include "../../x11/libXt/buildlink3.mk"
 .if ${X11_TYPE} == "modular"
