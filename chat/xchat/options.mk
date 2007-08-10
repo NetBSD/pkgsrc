ab66c0d6f62f9b2a827de8fded46d92d9c94b0d7
tron <tron@pkgsrc.org>
2007-08-10 19:14:06 +0000

    Apply extensive patch contributed by Blair Sadewitz in private e-mail: 1.) Add new options:     GNOME support, Spell checking, Socks 5 supports and fast Tinting     via MIT-SHM 2.) Use "desktop-file-utils" package to register the "xchat.desktop" file.     This is useful for GNOME and KDE users.
    Bump package revision because of these changes.

--- chat/xchat/options.mk
+++ chat/xchat/options.mk
@@ -1,10 +1,12 @@
-# $NetBSD: options.mk,v 1.8 2005/12/05 23:55:03 rillig Exp $
+# $NetBSD: options.mk,v 1.9 2007/08/10 19:14:06 tron Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xchat
-PKG_SUPPORTED_OPTIONS=	inet6 ssl socks5
+PKG_SUPPORTED_OPTIONS=	inet6 ssl socks5 mitshm gnome
+PKG_OPTIONS_GROUP.spell=	libsexy gtkspell
+PKG_OPTIONS_OPTIONAL_GROUPS=	spell
 PKG_SUGGESTED_OPTIONS=	ssl
 
 .include "../../mk/bsd.options.mk"
@@ -31,4 +33,45 @@ CONFIGURE_ARGS+=	--enable-socks
 ###
 .if !empty(PKG_OPTIONS:Minet6)
 CONFIGURE_ARGS+=	--enable-ipv6
+.endif
+
+###
+### MIT-SHM extension support (for "fast tinting")
+###
+.if !empty(PKG_OPTIONS:Mmitshm)
+CONFIGURE_ARGS+=	--enable-shm
+.else
+CONFIGURE_ARGS+=	--disable-shm
+.endif
+
+###
+### GNOME support
+###
+.if !empty(PKG_OPTIONS:Mgnome)
+CONFIGURE_ARGS+=	--enable-dbus
+PLIST_SUBST+=		GNOME=""
+SUBST_CLASSES+=		gconf
+SUBST_STAGE.gconf+=	pre-configure
+SUBST_MESSAGE.gconf+=	Disabling schema registration/installation.
+SUBST_SED.gconf+=	-e 's,DO_GCONF_TRUE,DO_GCONF_FALSE,g'
+SUBST_FILES.gconf+=	src/common/dbus/Makefile.in
+USE_DIRS+=		gnome2-1.5
+.	include "../../devel/GConf2/schemas.mk"
+.	include "../../sysutils/dbus-glib/buildlink3.mk"
+.else
+# We set this to ${FALSE} to avoid detection.
+CONFIGURE_ARGS+=	--disable-dbus
+PLIST_SUBST=		GNOME="@comment "
+USE_DIRS+=		xdg-1.1
+.endif
+
+###
+### Support for spell checking
+###
+.if !empty(PKG_OPTIONS:Mgtkspell)
+CONFIGURE_ARGS+=	--enable-spell=gtkspell
+.	include "../../textproc/gtkspell/buildlink3.mk"
+.elif !empty(PKG_OPTIONS:Mlibsexy)
+CONFIGURE_ARGS+=	--enable-spell=libsexy
+.	include "../../devel/libsexy/buildlink3.mk"
 .endif
