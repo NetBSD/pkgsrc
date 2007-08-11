8d21b4cd2e2c98ca1d040bcdd8501c2c093e8230
schmonz <schmonz@pkgsrc.org>
2007-08-11 14:41:36 +0000

    Make pthreads support optional. Bump PKGREVISION.

--- security/stunnel/options.mk
+++ security/stunnel/options.mk
@@ -1,7 +1,10 @@
-# $NetBSD: options.mk,v 1.2 2005/07/04 10:29:43 salo Exp $
+# $NetBSD: options.mk,v 1.3 2007/08/11 14:41:36 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.stunnel
-PKG_SUPPORTED_OPTIONS=	inet6
+PKG_SUPPORTED_OPTIONS=	inet6 pthread
+.if defined(PTHREAD_TYPE) && ${PTHREAD_TYPE} != "none"
+PKG_SUGGESTED_OPTIONS+=	pthread
+.endif
 
 .include "../../mk/bsd.options.mk"
 
@@ -12,4 +15,15 @@ PKG_SUPPORTED_OPTIONS=	inet6
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
+.endif
+
+###
+### Support pthreads
+###
+.if !empty(PKG_OPTIONS:Mpthread)
+CONFIGURE_ARGS+=	--with-threads=pthread
+CONFIGURE_ENV+=		CPPFLAGS="${CPPFLAGS} ${PTHREAD_CFLAGS}" \
+			LDFLAGS="${LDFLAGS} ${PTHREAD_LIBS}"
+.else
+CONFIGURE_ARGS+=	--with-threads=fork
 .endif
