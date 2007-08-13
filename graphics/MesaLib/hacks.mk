ace73d156f109b90a7313b0bc6999e6ecd4d091b
seb <seb@pkgsrc.org>
2007-08-13 11:18:28 +0000

    As discussed on tech-pkg@ revert for now the last updates of the Mesa packages.
    Downgraded graphics/MesaLib to 6.4.2nb3
    Downgraded graphics/glu to 6.4.2
    Downgraded graphics/glut to 6.4.2
    Downgraded graphics/glx-utils to 6.4.2
    Downgraded graphics/MesaDemos to 6.4.2
    Downgraded graphics/Mesa to 6.4.2
    
    Approved byg agc@.

--- graphics/MesaLib/hacks.mk
+++ graphics/MesaLib/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.2 2007/08/12 15:14:48 adam Exp $
+# $NetBSD: hacks.mk,v 1.3 2007/08/13 11:18:43 seb Exp $
 
 .if !defined(MESALIBS_HACKS_MK)
 MESALIBS_HACKS_MK=	# defined
@@ -10,7 +10,7 @@ MESALIBS_HACKS_MK=	# defined
 .if ${LOWER_OPSYS} == "irix5.3"
 PKG_HACKS+=		sys_types_h-inttypes_h-conflict
 post-wrapper:
-		mkdir ${BUILDLINK_DIR}/include/sys && grep -v '^typedef.*[^u_]int[12368]*_t;' /usr/include/sys/types.h > ${BUILDLINK_DIR}/include/sys/types.h
+		${MKDIR} ${BUILDLINK_DIR}/include/sys && ${GREP} -v '^typedef.*[^u_]int[12368]*_t;' /usr/include/sys/types.h > ${BUILDLINK_DIR}/include/sys/types.h
 # should match int8_t, int16_t, and int32_t (only)
 .endif
 
