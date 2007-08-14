e8929966ef33d4592d2dae369262fc720dd17682
joerg <joerg>
2007-08-14 23:58:24 +0000

    Fix a number of bugs in the DESTDIR support: - check-perms.mk was not DESTDIR aware, prefix files before passing it   to the directory extraction - PKG_FILELIST_CMD was calling pkg_info, which is fine for normal   installation, but fails of course for DESTDIR. Just drop the @ lines   from ${_DEPENDS_PLIST} and use that. - To make he former work, ensure that _flavor-generate-metadata is part   of _INSTALL_ALL_TARGETS. It was normally a dependency of   _flavor-register, but that is skipped for DESTDIR. - Remove ${_DEPENDS_PLIST} when running install-clean.

--- mk/flavor/pkg/flavor.mk
+++ mk/flavor/pkg/flavor.mk
@@ -1,4 +1,4 @@
-# $NetBSD: flavor.mk,v 1.1 2006/06/03 23:11:42 jlam Exp $
+# $NetBSD: flavor.mk,v 1.2 2007/08/14 23:58:24 joerg Exp $
 #
 # This Makefile fragment provides variable and target overrides that are
 # specific to the pkgsrc native package format.
@@ -7,7 +7,13 @@
 # PKG_FILELIST_CMD outputs the list of files owned by ${PKGNAME} as
 # registered on the system.
 #
+# For DESTDIR support, just use _DEPENDS_PLIST instead.
+#
+.if ${_USE_DESTDIR} == "no"
 PKG_FILELIST_CMD=	${PKG_INFO} -qL ${PKGNAME:Q}
+.else
+PKG_FILELIST_CMD=	${SED} -e "/^@/d" -e "s|^|${PREFIX}/|" ${_DEPENDS_PLIST}
+.endif
 
 .include "${PKGSRCDIR}/mk/flavor/pkg/depends.mk"
 .include "${PKGSRCDIR}/mk/flavor/pkg/check.mk"
