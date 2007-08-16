f8b11b4d294ee945d3fcf42d006c7c72b47dfbbc
joerg <joerg>
2007-08-16 23:12:20 +0000

    Add DESTDIR and modular Xorg support.

--- net/mtr/options.mk
+++ net/mtr/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2006/04/03 18:38:58 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2007/08/16 23:12:21 joerg Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mtr
 PKG_SUPPORTED_OPTIONS=	gtk
@@ -7,7 +7,6 @@ PKG_SUPPORTED_OPTIONS=	gtk
 
 .if !empty(PKG_OPTIONS:Mgtk)
 .include "../../x11/gtk/buildlink3.mk"
-.include "../../mk/x11.buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-gtk
 .endif
