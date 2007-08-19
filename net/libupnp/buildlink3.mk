41e6159ddc84160d42902c33d6c7addb6c63642b
obache <obache@pkgsrc.org>
2007-08-19 01:36:22 +0000

    Update libupnp to 1.6.0.  Merge from wip/libupnp. No functionally changes since RC1.

--- net/libupnp/buildlink3.mk
+++ net/libupnp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2007/06/19 01:40:13 rh Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2007/08/19 01:36:22 obache Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBUPNP_BUILDLINK3_MK:=	${LIBUPNP_BUILDLINK3_MK}+
@@ -12,12 +12,8 @@ BUILDLINK_PACKAGES+=	libupnp
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libupnp
 
 .if ${LIBUPNP_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.libupnp+=	libupnp>1.4.6
+BUILDLINK_API_DEPENDS.libupnp+=	libupnp>=1.4.6
 BUILDLINK_PKGSRCDIR.libupnp?=	../../net/libupnp
-
-PRINT_PLIST_AWK+=	/^@dirrm include\/upnp$$/ { print "@comment in net/libupnp: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/include\/upnp$$/ { next; }
-
 .endif	# LIBUPNP_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
