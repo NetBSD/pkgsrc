e21f48b50698d87ef49b4277028e7c26bd3532e3
obache <obache@pkgsrc.org>
2007-08-19 05:42:35 +0000

    PAM capable Darwin need should use build target "oxp" instead of "osx", reported in PR 36767. Also fixed namespace issue reported in PR 35592.
    Bump PKGREVISION.

--- mail/imap-uw/buildlink3.mk
+++ mail/imap-uw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2007/08/05 04:51:34 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2007/08/19 05:42:35 obache Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 IMAP_UW_BUILDLINK3_MK:=	${IMAP_UW_BUILDLINK3_MK}+
@@ -11,10 +11,15 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nimap-uw}
 BUILDLINK_PACKAGES+=	imap-uw
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}imap-uw
 
+.include "../../mk/bsd.fast.prefs.mk"
+
 .if !empty(IMAP_UW_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.imap-uw+=	imap-uw>=2004
 BUILDLINK_ABI_DEPENDS.imap-uw+=	imap-uw>=2006j2nb1
 BUILDLINK_PKGSRCDIR.imap-uw?=	../../mail/imap-uw
+. if ${OPSYS} == "Darwin"
+BUILDLINK_LDFLAGS.imap-uw+=	-flat_namespace
+. endif
 .endif	# IMAP_UW_BUILDLINK3_MK
 
 .include "../../security/openssl/buildlink3.mk"
