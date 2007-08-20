07f7b415923bc308d0019241b1e298dac7b03e0b
abs <abs@pkgsrc.org>
2007-08-20 11:34:05 +0000

    Update devel/cpuflags to 1.11:   optimize_gcc.mk:     - Add cases for textproc/aspell and mail/procmail from Yakovetsky Vladimir     - Shuffle some sections around and update comments     - Retire old USE_GCC3 test

--- devel/cpuflags/files/optimize_gcc.mk
+++ devel/cpuflags/files/optimize_gcc.mk
@@ -1,4 +1,4 @@
-# $Id: optimize_gcc.mk,v 1.32 2007/08/13 10:31:06 abs Exp $
+# $Id: optimize_gcc.mk,v 1.33 2007/08/20 11:34:05 abs Exp $
 
 # This file is 'experimental' - which is doublespeak for unspeakably
 # ugly, and quite broken by design.
@@ -8,39 +8,47 @@
 # compile some code, and even generate curdled binaries. It is completely
 # unsupported. Any questions should be directed to <abs@absd.org>.
 
-# -O3 would give -finline-functions and -frename-registers
-# As of gcc3-3.3nb4 -frename-registers still causes problems with xdm
-
 .ifdef BSD_PKG_MK			# Try to catch various package opts
 
-# This is a horrible mess, but how else to adjust per package?
+# -----------------------------------------------------------------------------
+# Per package exclusions
 
-COPT_FLAGS=-finline-functions -fomit-frame-pointer -ffast-math 
-
-# v1.0, NetBSD i386/2.0
+# firefox-1: v1.0, NetBSD i386/2.0
 PKG_EXCLUDE_OMIT_FRAME_POINTER+=www/firefox www/firefox-gtk1
 PKG_EXCLUDE_OMIT_FRAME_POINTER+=www/firefox15 www/firefox15-gtk1
+PKG_EXCLUDE_FAST_MATH+=www/firefox www/firefox-gtk1
+PKG_EXCLUDE_FAST_MATH+=www/firefox15 www/firefox15-gtk1
 
-PKG_EXCLUDE_OMIT_FRAME_POINTER+=lang/lua
-
-# 1.0.5 gcc-4.1.2 NetBSD 4.99.3 i386
+# seamonkey-1.0.5: gcc-4.1.2 NetBSD 4.99.3 i386
 PKG_EXCLUDE_OMIT_FRAME_POINTER+=www/seamonkey www/seamonkey-gtk1
 
 PKG_EXCLUDE_OMIT_FRAME_POINTER+=mail/thunderbird mail/thunderbird-gtk1
 PKG_EXCLUDE_OMIT_FRAME_POINTER+=mail/thunderbird15 mail/thunderbird15-gtk1
+
+PKG_EXCLUDE_OMIT_FRAME_POINTER+=lang/lua
 PKG_EXCLUDE_OMIT_FRAME_POINTER+=lang/ruby18-base        # NetBSD i386/3.0
+
+# perl5: '49/49!=1 in test'
+PKG_EXCLUDE_FAST_MATH+=lang/perl5
 
 PKG_EXCLUDE_INLINE_FUNCTIONS+=emulators/qemu net/userppp multimedia/vlc
 
-# binutils-2.17, Linux 2.6.21.5 i686, gcc-4.1.3
+# binutils-2.17: Linux 2.6.21.5 i686, gcc-4.1.3
 PKG_EXCLUDE_INLINE_FUNCTIONS+=cross/binutils devel/binutils
 
-# v1.0, NetBSD i386/2.0
-PKG_EXCLUDE_FAST_MATH+=www/firefox www/firefox-gtk1
-PKG_EXCLUDE_FAST_MATH+=www/firefox15 www/firefox15-gtk1
+# procmail-3.22: debian_linux-2.6.21.5/gcc-4.1.3, gcc-4.1.2 NetBSD 4.0_BETA2
+PKG_EXCLUDE_INLINE_FUNCTIONS+=mail/procmail
 
-PKG_EXCLUDE_FAST_MATH+=x11/qt3-libs    	# gcc3.3.3 NetBSD i386/3.0 breaks kde3
-PKG_EXCLUDE_FAST_MATH+=lang/perl5      	# perl5 '49/49!=1'
+# aspell-0.60.5: gcc-4.1.2 NetBSD 4.0_BETA2 i386
+PKG_EXCLUDE_INLINE_FUNCTIONS+=textproc/aspell
+
+# gcc3.3.3 NetBSD i386/3.0 breaks kde3
+PKG_EXCLUDE_FAST_MATH+=x11/qt3-libs
+
+# -----------------------------------------------------------------------------
+# Assign default flags, then remove values based on settings above
+#
+COPT_FLAGS=-finline-functions -fomit-frame-pointer -ffast-math 
 
 .if !empty(PKG_EXCLUDE_OMIT_FRAME_POINTER:M${PKGPATH})
 COPT_FLAGS:=    ${COPT_FLAGS:S/-fomit-frame-pointer//}
@@ -58,7 +66,7 @@ CFLAGS+=${COPT_FLAGS}
 CXXFLAGS+=${COPT_FLAGS}
 MAKE_FLAGS+=CCOPTIONS="${COPT_FLAGS}"	# Override CCOPTIONS for imake
 
-.else					# Assume in base system, only COPTS
+.else	# (BSD_PKG_MK) Assume in NetBSD base system, only COPTS
 
 .if ${USETOOLS:Uyes} == "no"
 COPT_FLAGS+=-fomit-frame-pointer
@@ -68,10 +76,6 @@ COPTS=${DEFCOPTS} ${COPT_FLAGS}
 .else
 COPTS+=${COPT_FLAGS}
 .endif
-.endif
-
-.if defined(USE_GCC3)
-DEFWARNINGS=no
 .endif
 
 .endif
