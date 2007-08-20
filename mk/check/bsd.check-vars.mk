588079390c2dba17105192d3fa9804f8a18259cd
joerg <joerg@pkgsrc.org>
2007-08-20 11:04:02 +0000

    Add a cross-compile and DESTDIR safe alternative to ldd for check-shlibs. This allows more stricter detection (e.g. it checks whether the package links against a build dependency). It also checks whether the rpath contains wrkdir references (like qt3-libs). It depends on readelf and is only available for ELF systems for now. To test it, add USE_CHECK_SHLIBS_ELF to mk.conf.
    Supported-by: Google SoC 2007

--- mk/check/bsd.check-vars.mk
+++ mk/check/bsd.check-vars.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.check-vars.mk,v 1.3 2006/06/09 16:12:08 jlam Exp $
+# $NetBSD: bsd.check-vars.mk,v 1.4 2007/08/20 11:04:02 joerg Exp $
 #
 # This Makefile fragment is included separately by bsd.pkg.mk and
 # defines some variables which must be defined earlier than where
@@ -7,5 +7,10 @@
 
 CHECK_FILES_SUPPORTED?=		yes
 CHECK_SHLIBS_SUPPORTED?=	yes
+USE_CHECK_SHLIBS_ELF?=		no
 
 USE_TOOLS+=	awk cat cmp diff echo find grep rm sed test touch true
+
+.if !empty(USE_CHECK_SHLIBS_ELF:M[yY][eE][sS])
+USE_TOOLS+=	readelf
+.endif
