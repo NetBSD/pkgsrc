7e49ccffe2d0936c5ca14067c6eec540416b6f9d
martti <martti@pkgsrc.org>
2007-08-20 06:24:32 +0000

    Updated editors/vim-share to 7.1.83
      2757  7.1.069  GTK GUI: confirm() dialog has a default button when not wanted
      1560  7.1.070  (extra) Win32 GUI: same confirm() default button problem
      5534  7.1.071  (after 7.1.040) add a basic test for regexp patterns
      5705  7.1.072  (extra, after 7.1.041 and 7.1.071) extra part of 7.1.071
      3595  7.1.073  (after 7.1.062) crash when 'preserveindent' is set
      5259  7.1.074  crash when using string() on a recursively nested List
      1686  7.1.075  ":let v:statusmsg" reads memory already freed
      2376  7.1.076  a couple more strcpy() with overlapping arguments
      2678  7.1.078  dropping file name on gvim containing CSI byte doesn't work
      2922  7.1.079  "@" character in 'isfname' doesn't pick up umlauts for latin1
      2960  7.1.080  (extra) Compiler warnings for gvimex.cpp
      3253  7.1.081  completion doesn't work after ":!cat <foo"
      3464  7.1.082  matchparen plugin doesn't update after window split
      3448  7.1.083  (after 7.1.081) completion doesn't work with wildcards

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.35 2007/08/13 07:04:52 martti Exp $
+# $NetBSD: version.mk,v 1.36 2007/08/20 06:24:32 martti Exp $
 
 VIM_VERSION=		7.1
-VIM_PATCHLEVEL=		68
+VIM_PATCHLEVEL=		83
 VIM_SUBDIR=		vim71
