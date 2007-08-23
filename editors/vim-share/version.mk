64348caa58222e11bb2910b2b2386954ef61b54b
martti <martti>
2007-08-23 09:30:41 +0000

    Updated editors/vim-share to 7.1.87
      1671  7.1.084  netbeans doesn't get fileOpened events when using -nb twice
      2496  7.1.085  after ":split fold.c" folds in one window disappear
      3561  7.1.086  crash when using specific Python syntax highlighting
      4165  7.1.087  cscope: reading past command end; writing past buffer end

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.36 2007/08/20 06:24:32 martti Exp $
+# $NetBSD: version.mk,v 1.37 2007/08/23 09:30:42 martti Exp $
 
 VIM_VERSION=		7.1
-VIM_PATCHLEVEL=		83
+VIM_PATCHLEVEL=		87
 VIM_SUBDIR=		vim71
