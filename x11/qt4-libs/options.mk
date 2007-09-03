e880ee22c80af3b1e7923182fafe20b583a44866
drochner <drochner@pkgsrc.org>
2007-09-03 09:35:15 +0000

    -make sqlite support an extra pkg instead of an option -work around iconv(3) incompatibilities: while NetBSD's iconv() is  probed as GNU iconv (due to its signature), it doesn't support empty  strings as encoding (interpreted as "current locale's encoding" by  GNU iconv). Enable some code which uses nl_langinfo(CODESET) to make  it work as expected. -update to 4.3.1  changes:  -bugfixes  -Updated the German translation to provide complete coverage of Qt

--- x11/qt4-libs/options.mk
+++ x11/qt4-libs/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.2 2006/05/31 19:18:07 adam Exp $
+# $NetBSD: options.mk,v 1.3 2007/09/03 09:35:16 drochner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.qt4
-PKG_SUPPORTED_OPTIONS=	cups debug sqlite
+PKG_SUPPORTED_OPTIONS=	cups debug
 
 .include "../../mk/bsd.options.mk"
 
@@ -17,11 +17,4 @@ CC+=			-ggdb
 CXX+=			-ggdb
 CONFIGURE_ARGS+=	--debug
 INSTALL_UNSTRIPPED=	yes
-.endif
-
-.if !empty(PKG_OPTIONS:Msqlite)
-.  include "../../databases/sqlite3/buildlink3.mk"
-CONFIGURE_ARGS+=	-qt-sql-sqlite
-.else
-CONFIGURE_ARGS+=	-no-sql-sqlite
 .endif
