66a0cbeee0cb5207c9ec118464436edc06364063
joerg <joerg@pkgsrc.org>
2007-09-14 08:12:29 +0000

    Don't use u_char when uint8_t are meant. Don't mess with __attribute__, conditionally use it in the only place it is used. This should fix problems on QNX reported by Sean Boudreau.

--- pkgtools/digest/files/sha1.h
+++ pkgtools/digest/files/sha1.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: sha1.h,v 1.5 2007/08/02 13:54:34 joerg Exp $	*/
+/*	$NetBSD: sha1.h,v 1.6 2007/09/14 08:12:29 joerg Exp $	*/
 
 /*
  * SHA-1 in C
@@ -20,17 +20,17 @@
 typedef struct {
 	uint32_t state[5];
 	uint32_t count[2];  
-	u_char buffer[64];
+	uint8_t buffer[64];
 } SHA1_CTX;
   
-void	SHA1Transform __P((uint32_t state[5], const u_char buffer[64]));
+void	SHA1Transform __P((uint32_t state[5], const uint8_t buffer[64]));
 void	SHA1Init __P((SHA1_CTX *context));
-void	SHA1Update __P((SHA1_CTX *context, const u_char *data, u_int len));
-void	SHA1Final __P((u_char digest[20], SHA1_CTX *context));
+void	SHA1Update __P((SHA1_CTX *context, const uint8_t *data, u_int len));
+void	SHA1Final __P((uint8_t digest[20], SHA1_CTX *context));
 #ifndef _KERNEL
 char	*SHA1End __P((SHA1_CTX *, char *));
 char	*SHA1File __P((char *, char *));
-char	*SHA1Data __P((const u_char *, size_t, char *));
+char	*SHA1Data __P((const uint8_t *, size_t, char *));
 #endif /* _KERNEL */
 
 #endif /* _SYS_SHA1_H_ */
