0f29df6ebd41119e402b62dce65d393a656d4029
wiz <wiz@pkgsrc.org>
2007-09-19 23:53:47 +0000

    Rename gnome2-control-center to control-center, to match upstream name. ok jmmv, drochner

--- x11/control-center/buildlink3.mk
+++ x11/control-center/buildlink3.mk
@@ -1,21 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.22 2007/09/19 23:13:37 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2007/09/19 23:53:49 wiz Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
-GNOME2_CONTROL_CENTER_BUILDLINK3_MK:=	${GNOME2_CONTROL_CENTER_BUILDLINK3_MK}+
+CONTROL_CENTER_BUILDLINK3_MK:=	${CONTROL_CENTER_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	gnome2-control-center
+BUILDLINK_DEPENDS+=	control-center
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngnome2-control-center}
-BUILDLINK_PACKAGES+=	gnome2-control-center
-BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gnome2-control-center
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ncontrol-center}
+BUILDLINK_PACKAGES+=	control-center
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}control-center
 
-.if !empty(GNOME2_CONTROL_CENTER_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.gnome2-control-center+=	gnome2-control-center>=2.12.1nb1
-BUILDLINK_ABI_DEPENDS.gnome2-control-center?=	gnome2-control-center>=2.18.1nb1
-BUILDLINK_PKGSRCDIR.gnome2-control-center?=	../../x11/gnome2-control-center
-.endif	# GNOME2_CONTROL_CENTER_BUILDLINK3_MK
+.if !empty(CONTROL_CENTER_BUILDLINK3_MK:M+)
+BUILDLINK_API_DEPENDS.control-center+=	control-center>=2.12.1nb1
+BUILDLINK_ABI_DEPENDS.control-center?=	control-center>=2.18.1nb1
+BUILDLINK_PKGSRCDIR.control-center?=	../../x11/control-center
+.endif	# CONTROL_CENTER_BUILDLINK3_MK
 
 .include "../../audio/esound/buildlink3.mk"
 .include "../../devel/GConf2/buildlink3.mk"
@@ -32,6 +32,5 @@ BUILDLINK_PKGSRCDIR.gnome2-control-center?=	../../x11/gnome2-control-center
 .include "../../x11/gnome-desktop/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .include "../../x11/libXft/buildlink3.mk"
-.include "../../x11/startup-notification/buildlink3.mk"
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
