770be7e86b14bf1b3618116b5787fa68c9234e34
wiz <wiz@pkgsrc.org>
2007-09-19 23:13:12 +0000

    Use gnome-vfs instead of gnome-vfs2.

--- devel/libgnome/buildlink3.mk
+++ devel/libgnome/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2007/06/05 05:37:05 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2007/09/19 23:13:16 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGNOME_BUILDLINK3_MK:=	${LIBGNOME_BUILDLINK3_MK}+
@@ -23,7 +23,7 @@ BUILDLINK_PKGSRCDIR.libgnome?=	../../devel/libgnome
 .include "../../devel/libbonobo/buildlink3.mk"
 .include "../../devel/popt/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
-.include "../../sysutils/gnome-vfs2/buildlink3.mk"
+.include "../../sysutils/gnome-vfs/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../textproc/libxslt/buildlink3.mk"
 .include "../../mk/bdb.buildlink3.mk"
--- sysutils/gnome-menus/buildlink3.mk
+++ sysutils/gnome-menus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2007/06/05 05:37:01 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2007/09/19 23:13:28 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_MENUS_BUILDLINK3_MK:=	${GNOME_MENUS_BUILDLINK3_MK}+
@@ -18,6 +18,6 @@ BUILDLINK_PKGSRCDIR.gnome-menus?=	../../sysutils/gnome-menus
 .endif	# GNOME_MENUS_BUILDLINK3_MK
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../sysutils/gnome-vfs2/buildlink3.mk"
+.include "../../sysutils/gnome-vfs/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
