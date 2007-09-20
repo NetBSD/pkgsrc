f132c77764216a5f3b9fd2f56f67e37ae497bf85
rillig <rillig@pkgsrc.org>
2007-09-20 07:59:11 +0000

    Fixed pkglint warnings and notes.

--- x11/py-qt3-modules/buildlink3.mk
+++ x11/py-qt3-modules/buildlink3.mk
@@ -1,9 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.9 2006/07/26 17:56:25 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2007/09/20 07:59:11 rillig Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 PYQT3_MOD_BUILDLINK3_MK:=	${PYQT3_MOD_BUILDLINK3_MK}+
-
-.include "../../lang/python/pyversion.mk"
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	pyqt3-mod
@@ -14,8 +12,9 @@ BUILDLINK_PACKAGES+=	pyqt3-mod
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}pyqt3-mod
 
 .if !empty(PYQT3_MOD_BUILDLINK3_MK:M+)
+.include "../../lang/python/pyversion.mk"
 BUILDLINK_API_DEPENDS.pyqt3-mod+=	${PYPKGPREFIX}-qt3-modules>=3.11
-BUILDLINK_ABI_DEPENDS.pyqt3-mod?=	${PYPKGPREFIX}-qt3-modules>=3.15.1nb3
+BUILDLINK_ABI_DEPENDS.pyqt3-mod+=	${PYPKGPREFIX}-qt3-modules>=3.15.1nb3
 BUILDLINK_PKGSRCDIR.pyqt3-mod?=	../../x11/py-qt3-modules
 
 BUILDLINK_LIBDIRS.pyqt3-mod+=	${PYSITELIB}
