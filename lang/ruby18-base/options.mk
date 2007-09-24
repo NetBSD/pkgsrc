ca23b7ea74c77ee2901c3ebaeb6e785f25201c89
taca <taca>
2007-09-24 21:58:27 +0000

    Update Ruby to 1.8.6-p110.
    Approved by wiz@.
    
    This is bug fix release of Ruby 1.8.6.  Especially it fixes thread/eval
    function problem on Mac OS X.  It also contains an openssl extention's
    portablity problem which was bad patch by pkgsrc.
    
    For more detail, please refer CHANGES file.

--- lang/ruby18-base/options.mk
+++ lang/ruby18-base/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2007/09/09 14:17:13 taca Exp $
+# $NetBSD: options.mk,v 1.4 2007/09/24 21:58:28 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ruby
 PKG_SUPPORTED_OPTIONS=	ruby-build-ri-db
@@ -8,5 +8,6 @@ PKG_SUGGESTED_OPTIONS=	ruby-build-ri-db
 
 .if !empty(PKG_OPTIONS:Mruby-build-ri-db)
 CONFIGURE_ARGS+=	--enable-install-doc
+PRIVILEGED_STAGES+=    clean
 RUBY_DYNAMIC_DIRS+=	${RUBY_SYSRIDIR}
 .endif
