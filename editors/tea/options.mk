7e222e4299737ca51e749f442ceca3f8c8e99fa3
hira <hira>
2007-09-25 10:16:08 +0000

    GConf2 -> GConf. gnome-vfs2 -> gnome-vfs.

--- editors/tea/options.mk
+++ editors/tea/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2007/04/14 04:49:08 reed Exp $
+# $NetBSD: options.mk,v 1.2 2007/09/25 10:23:22 hira Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tea
 PKG_SUPPORTED_OPTIONS=	aspell gtksourceview # enchant antiword
@@ -13,8 +13,8 @@ PKG_SUGGESTED_OPTIONS=	# none enabled by default
 # --enable-enchant disabled aspell so choose one or the other
 
 .if !empty(PKG_OPTIONS:Mgtksourceview)
-.include "../../devel/GConf2/buildlink3.mk"
-.include "../../sysutils/gnome-vfs2/buildlink3.mk"
+.include "../../devel/GConf/buildlink3.mk"
+.include "../../sysutils/gnome-vfs/buildlink3.mk"
 .include "../../x11/gtksourceview/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--enable-legacy
