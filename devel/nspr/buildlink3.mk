5032cb225d6e370423ee6716c8cfdfd24d62d389
bjs <bjs>
2007-09-25 04:18:25 +0000

    Update to version 4.6.7 (bugfix release).  See new patches for more information about them.  While here, fix pkglint warnings.
    ok'd by joerg@

--- devel/nspr/buildlink3.mk
+++ devel/nspr/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/12/05 17:43:06 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2007/09/25 04:18:25 bjs Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 NSPR_BUILDLINK3_MK:=	${NSPR_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	nspr
 .endif
 
@@ -11,21 +11,24 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nnspr}
 BUILDLINK_PACKAGES+=	nspr
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}nspr
 
-.if !empty(NSPR_BUILDLINK3_MK:M+)
+.if ${NSPR_BUILDLINK3_MK} == "+"
+
+.  include "../../mk/bsd.fast.prefs.mk"
+
 BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.4.1nb1
-.if ${OPSYS} == "Darwin"
-BUILDLINK_ABI_DEPENDS.nspr+=	nspr>=4.6.1nb3
-.else
-BUILDLINK_ABI_DEPENDS.nspr+=	nspr>=4.6.1nb1
-.endif
+.  if ${OPSYS} == "Darwin"
+BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.6.1nb3
+.  else
+BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.6.1nb1
+.  endif
+
 BUILDLINK_PKGSRCDIR.nspr?=	../../devel/nspr
+BUILDLINK_FILES.nspr+=          lib/nspr/*
+BUILDLINK_FILES.nspr+=          include/nspr/*
 
-BUILDLINK_FILES.nspr+=		lib/nspr/*
-BUILDLINK_FILES.nspr+=		include/nspr/*
-
-BUILDLINK_INCDIRS.nspr+=	include/nspr
-BUILDLINK_LIBDIRS.nspr+=	lib/nspr
-BUILDLINK_RPATHDIRS.nspr+=	lib/nspr
+BUILDLINK_INCDIRS.nspr+=        include/nspr
+BUILDLINK_LIBDIRS.nspr+=        lib/nspr
+BUILDLINK_RPATHDIRS.nspr+=      lib/nspr
 
 .endif	# NSPR_BUILDLINK3_MK
 
