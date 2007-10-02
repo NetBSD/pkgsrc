543d35474b9d84e5f48d4a2f019842247c7dc1a0
martti <martti@pkgsrc.org>
2007-10-02 05:28:19 +0000

    Updated mail/squirrelmail to 1.4.11
    Version 1.4.11 - 29 September 2007
    ----------------------------------
      - Minimum PHP requirement raised from 4.0.6 to 4.1.0.
        SquirrelMail has been broken for a while with 4.0.x without anyone
        noticing, this move merely reflects reality.
      - Fix broken set_url_var function in functions/html.php (#1729814).
      - Fix config.pl not detecting auth support correctly (#1727033).
      - Fix display of X-Priority in message view.
      - Work around mailers sending broken Date headers with no space after the
        first comma.
      - Let POP3 class properly cope with lines starting with a '.'.
      - Some HTML validation cleanups.
      - Invalid year in sent_subfolders plugin (#1607380).
      - Always treat Content-Type case-insensitively (#1732092).
      - Fix typo: html/plain should be text/html.
      - Fix en/decode header swith in MDN (#1694687).
      - Fix compatibility with Windows path in administrator plugin (#1740469).
      - Fix disabling password encryption in mail_fetch (#1738001).
      - Fix busy loop and notice when two literals in IMAP fetch (#1739433).
      - Backported code for site wide SMTP authentication (#1531889).
      - Fixed issue with compose session not being cleaned after message is
        saved or sent.
      - Added ability to detect HTTP_X_FORWARDED_PROTO in get_location(),
        thanks to Daniel Watts
      - Fix test for signout.php in the logged in check in is_logged_in() so it
        cannot be circumvented by manipulating the URL. External plugins might
        rely on this function guaranteeing that the user is logged in.
      - Use attachment_dir only at the point where we're actually
        reading from / writing to the files, do not carry it around
        in the object. This makes us safer in the event the object
        is somehow exposed to the outside world.
      - Better support mailboxes named 'None' (#1598890).
      - Sort readdir() output in conf.pl (#1755886).
      - Fix message cache in printer friendly, thanks Tomas Kuliavas.
      - Made the webmail_top hook work again for plugins that want to change
        the URI of the "right" frame; plugins have to change the value of the
        global variable $right_frame_url
      - Fix issue in darkness theme with extra closing bracket.
      - No longer store all message composition sessions in the PHP session,
        since it was not made use of and in rare cases, made sessions too big.
      - Composition restoration functionality now correctly restores attachments.
      - Added smtp_auth hook.
      - Change default Selection List Style to Indented.
      - Added "preselected" query argument to mailbox list.
      - Added mailbox_display_buttons hook.
      - Removed "Include CCs when Forwarding Messages", which had no functionality
        whatsoever.
      - Make the Message Details plugin actually show the correct entity when
        viewing details of attached messages.

--- mail/squirrelmail/buildlink3.mk
+++ mail/squirrelmail/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2007/05/10 10:22:40 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2007/10/02 05:28:19 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SQUIRRELMAIL_BUILDLINK3_MK:=	${SQUIRRELMAIL_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	squirrelmail
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}squirrelmail
 
 .if ${SQUIRRELMAIL_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.squirrelmail+=	squirrelmail>=1.4.10a
+BUILDLINK_API_DEPENDS.squirrelmail+=	squirrelmail>=1.4.11
 BUILDLINK_PKGSRCDIR.squirrelmail?=	../../mail/squirrelmail
 .endif	# SQUIRRELMAIL_BUILDLINK3_MK
 
