26aa6e4a0182686d7c2fabc79cb74b8622327494
heinz <heinz@pkgsrc.org>
2007-10-02 16:20:05 +0000

    Added NetBSD 3 to the list of platforms which need pthreads linked into the application if it uses dlopen(). This change was triggered by the longstanding bug that mod_perl (due to perl itself) was built with threads but apache was not, thus leading to incompatibility between apache and mod_perl. Whether NetBSD 4 needs to be added as well is still an open question.

--- mk/dlopen.builtin.mk
+++ mk/dlopen.builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dlopen.builtin.mk,v 1.14 2005/06/26 13:11:22 minskim Exp $
+# $NetBSD: dlopen.builtin.mk,v 1.15 2007/10/02 16:20:05 heinz Exp $
 
 BUILTIN_PKG:=	dl
 
@@ -47,7 +47,8 @@ _BLNK_DLOPEN_REQUIRE_PTHREAD_PLATFORMS=					\
 	NetBSD-2.[0-9]_*-*						\
 	NetBSD-2.[0-9]-* NetBSD-2.[0-9].[0-9]*-*			\
 	NetBSD-2.[0-8][0-9]*-* NetBSD-2.9[0-8]*-*			\
-	NetBSD-2.99.[0-9]-* NetBSD-2.99.10-*
+	NetBSD-2.99.[0-9]-* NetBSD-2.99.10-*				\
+	NetBSD-3.[0-9]-* NetBSD-3.[0-9].[0-9]*-*
 
 .if !defined(_BLNK_DLOPEN_REQUIRE_PTHREADS)
 _BLNK_DLOPEN_REQUIRE_PTHREADS?=	no
