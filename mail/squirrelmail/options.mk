da68d7fd7589790ac8cf17f3d8cff22411616d96
taca <taca@pkgsrc.org>
2007-10-03 10:11:23 +0000

    Update optional patch to support squirrelmail-1.4.11.
    Since this commmit fixes broken options, I don't bump PKGREVISION now.

--- mail/squirrelmail/options.mk
+++ mail/squirrelmail/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2007/05/23 12:17:12 obache Exp $
+# $NetBSD: options.mk,v 1.4 2007/10/03 10:11:23 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.squirrelmail
 PKG_SUPPORTED_OPTIONS=	squirrelmail-japanese squirrelmail-lite
@@ -6,16 +6,15 @@ PKG_SUPPORTED_OPTIONS=	squirrelmail-japanese squirrelmail-lite
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Msquirrelmail-japanese)
-PATCHFILES+=		squirrelmail-1.4.10a-ja-20070511-patch.gz
-SITES.squirrelmail-1.4.10a-ja-20070511-patch.gz= \
+PATCHFILES+=		squirrelmail-1.4.11-ja-20071002-patch.gz
+SITES.squirrelmail-1.4.11-ja-20071002-patch.gz= \
 			http://www.yamaai-tech.com/~masato/Download/
-PATCH_DIST_STRIP.squirrelmail-1.4.10a-ja-20070511-patch.gz=	-p1
+PATCH_DIST_STRIP.squirrelmail-1.4.11-ja-20071002-patch.gz=	-p1
 
 .endif
 
 .if !empty(PKG_OPTIONS:Msquirrelmail-lite)
-PATCHFILES+=		squirrelmail-1.4.10a-lite-20070522-patch
-SITES.squirrelmail-1.4.10a-lite-20070522-patch=	\
+PATCHFILES+=		squirrelmail-1.4.11-lite-20071003-patch.bz2
+SITES.squirrelmail-1.4.11-lite-20071003-patch= \
 			ftp://ftp.back-street.net/pub/squirrelmail/
-PATCH_DIST_STRIP.squirrelmail-1.4.10a-lite-20070522-patch=	-p1
 .endif
