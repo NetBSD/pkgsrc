f111365c6f76a5df102568a9af00fef84f199934
taca <taca@pkgsrc.org>
2007-10-09 08:51:30 +0000

    Correct SITE.* macro for the squirrelmail-lite's patch file.

--- mail/squirrelmail/options.mk
+++ mail/squirrelmail/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2007/10/03 10:11:23 taca Exp $
+# $NetBSD: options.mk,v 1.5 2007/10/09 08:51:30 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.squirrelmail
 PKG_SUPPORTED_OPTIONS=	squirrelmail-japanese squirrelmail-lite
@@ -15,6 +15,6 @@ PATCH_DIST_STRIP.squirrelmail-1.4.11-ja-20071002-patch.gz=	-p1
 
 .if !empty(PKG_OPTIONS:Msquirrelmail-lite)
 PATCHFILES+=		squirrelmail-1.4.11-lite-20071003-patch.bz2
-SITES.squirrelmail-1.4.11-lite-20071003-patch= \
+SITES.squirrelmail-1.4.11-lite-20071003-patch.bz2= \
 			ftp://ftp.back-street.net/pub/squirrelmail/
 .endif
