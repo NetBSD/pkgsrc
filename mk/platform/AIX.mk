a4c2c0e8c57fa5eda43a221cb479cdea5fdd7cb0
rillig <rillig@pkgsrc.org>
2007-10-09 20:39:39 +0000

    AIX has nothing to do with NetBSD-1.5.

--- mk/platform/AIX.mk
+++ mk/platform/AIX.mk
@@ -1,4 +1,4 @@
-# $NetBSD: AIX.mk,v 1.26 2007/07/02 14:03:34 joerg Exp $
+# $NetBSD: AIX.mk,v 1.27 2007/10/09 20:39:39 rillig Exp $
 #
 # Variable definitions for the AIX operating system.
 
@@ -62,12 +62,6 @@ _USE_RPATH=		yes	# add rpath to LDFLAGS
 # this is GNU ld.
 _OPSYS_WHOLE_ARCHIVE_FLAG=	-Wl,--whole-archive
 _OPSYS_NO_WHOLE_ARCHIVE_FLAG=	-Wl,--no-whole-archive
-
-# for programs which use dlopen()
-# not necessary since 1.6 (shared libs are linked against libgcc_pic)
-.if !empty(OS_VERSION:M1.5*)
-LINK_ALL_LIBGCC_HACK=	-Wl,--whole-archive -lgcc -Wl,--no-whole-archive
-.endif
 
 _STRIPFLAG_CC?=		${_INSTALL_UNSTRIPPED:D:U-s}	# cc(1) option to strip
 _STRIPFLAG_INSTALL?=	${_INSTALL_UNSTRIPPED:D:U-s}	# install(1) option to strip
