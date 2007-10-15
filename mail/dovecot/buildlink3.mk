64bd3471594d2f4fca15b5b2c66db695141c5181
ghen <ghen@pkgsrc.org>
2007-10-15 12:46:09 +0000

    Fix references to OPENLDAP (where this bl3 files was originally copied from).

--- mail/dovecot/buildlink3.mk
+++ mail/dovecot/buildlink3.mk
@@ -1,7 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.1 2007/08/27 10:38:51 ghen Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2007/10/15 12:46:09 ghen Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
-OPENLDAP_BUILDLINK3_MK:=		${OPENLDAP_BUILDLINK3_MK}+
+DOVECOT_BUILDLINK3_MK:=			${DOVECOT_BUILDLINK3_MK}+
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -13,7 +13,7 @@ BUILDLINK_PACKAGES:=			${BUILDLINK_PACKAGES:Ndovecot}
 BUILDLINK_PACKAGES+=			dovecot
 BUILDLINK_ORDER:=			${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}dovecot
 
-.if !empty(OPENLDAP_BUILDLINK3_MK:M+)
+.if !empty(DOVECOT_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.dovecot+=		dovecot>=1.0.3nb1
 BUILDLINK_PKGSRCDIR.dovecot?=		../../mail/dovecot
 .endif
