78c22164fc43067d81b07d26af3638a081467e0a
tnn <tnn>
2007-10-16 23:48:58 +0000

    Fix abusers of LOWER_OPSYS to check OPSYS or MACHINE_PLATFORM instead.

--- graphics/libwmf/hacks.mk
+++ graphics/libwmf/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2006/01/29 21:58:31 schwarz Exp $
+# $NetBSD: hacks.mk,v 1.2 2007/10/16 23:48:59 tnn Exp $
 
 .if !defined(LIBEXIF_HACKS_MK)
 LIBEXIF_HACKS_MK=	# defined
@@ -7,7 +7,7 @@ LIBEXIF_HACKS_MK=	# defined
 ### make sys/types.h not conflict with inttypes.h
 ### (issue is specific to IRIX 5.3)
 ###
-.if ${LOWER_OPSYS} == "irix5.3"
+.if !empty(MACHINE_PLATFORM:MIRIX-5.3-*)
 PKG_HACKS+=		sys_types_h-inttypes_h-conflict
 post-wrapper:
 		${MKDIR} ${BUILDLINK_DIR}/include/sys && ${GREP} -v '^typedef.*[^u_]int[12368]*_t;' /usr/include/sys/types.h > ${BUILDLINK_DIR}/include/sys/types.h
