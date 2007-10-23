0acf48904b4f181d4a386f66c91567fbd1fbcc4c
smb <smb@pkgsrc.org>
2007-10-23 20:16:50 +0000

    Update claws-mail to 3.0.2: * Bug fixes:         o bug 1320, 'Memory leak caused by gtk_container_get_children'         o bug 1321, 'access before allocation when scanning for uris'         o bug 1324, 'Corrupted IMAP cache'         o bug 1327, 'segfault when replying to a non-existent news                      message'         o bug 1329, 'error in quicksearch.c'         o bug 1334, 'user defined mail incorporation script not run'         o bug 1336, 'Segfault if empty space before search-string'         o bug 1337, 'Crash on removing uncached emails'         o Prevent unquoted mime boundaries, by always using a /         o Remove unnecessary error when cancelling export to mbox.         o MAEMO: make all Folder Properties pages visible         o MAEMO: fix overlapping buttons in Edit Accounts page
    Also update the version dependencies of all plug-ins.  The reality
    of the numbering for standard plug-ins was different than had been
    assumed, and the actual published dependencies were more complex tha
    was captured by the previous structure.

--- mail/claws-mail/plugins.mk
+++ mail/claws-mail/plugins.mk
@@ -1,15 +1,13 @@
-# $NetBSD: plugins.mk,v 1.1 2007/08/03 02:34:41 smb Exp $
+# $NetBSD: plugins.mk,v 1.2 2007/10/23 20:16:51 smb Exp $
 #
 
 .include "../../mail/claws-mail/Makefile.common"
 
-PKGNAME=	claws-mail-${PLUGIN_NAME:S/_/-/}-${CLAWS_VERSION}
+PKGNAME=	claws-mail-${PLUGIN_NAME:S/_/-/}-${PLUG_VER}
 
 CONFIGURE_DIRS=	.
 BUILD_DIRS=	src/plugins/${PLUGIN_NAME}
 INSTALL_DIRS=	src/plugins/${PLUGIN_NAME}
-
-DEPENDS+=       claws-mail>=${CLAWS_VERSION}:../../mail/claws-mail
 
 LIBTOOL_OVERRIDE=	../../../libtool
 
