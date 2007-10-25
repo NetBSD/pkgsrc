d92a3175c8ad379a6b36f1d1ab13b2efc8b958e4
ghen <ghen@pkgsrc.org>
2007-10-25 16:00:05 +0000

    Update to 1.4d (previous 1.4.2 was 1.4b).  Take MAINTAINERship.
    2006-11-16  Brian Burton  <brian@burton-computer.com>
    
    * Released as 1.4d
    
    * configure.ac: Added ability to selectively disable image
      processing using --without-gif, --without-jpeg, and/or
      --without-png.
    
    * src/spamprobe/spamprobe.cc (set_headers): Added ability to
      selectively ignore individual headers using -H-headername.
    
    * src/includes/Ptr,Ref,Array.h: Restored missing <cassert> include.
    
    * src/parser/PngParser.cc (tokenizeImage): added basic tokens from
      PNG images.
    
    2006-11-16  Brian Burton  <brian@localhost.localdomain>
    
    * src/parser/PngParser.cc (PngParser): Stub for PNG parsing using
      libpng.
    
    * src/parser/JpegParser.cc (tokenizeMarker): Preliminary
      implementation of jpeg parsing using jpeglib.
    
    * configure.ac: Auto detect of either libungig or libgif depending
      on which one is available.
    
    2007-01-04  Brian Burton  <brian@burton-computer.com>
    
    * Released as 1.4c
    
    * spamprobe.1: Modified man page to remove unnecessary informaton
      and make it more conformant with man page conventions.
    
    * src/spamprobe/spamprobe.cc (process_extended_options): added
      ignore-body option.
    
    * src/parser/HeaderPrefixList.cc (HeaderPrefixList::addHeaderPrefix):
      Forced header prefixes and names to lower case instead of
      relying on an assert to enforce the restriction.
    
    * src/database/FrequencyDBImpl_hash.cc (hash::FrequencyDBImpl_hash):
      Disabled experimental hash database auto-cleaning.
    
    * src/includes/Ref.h: Removed cassert include.
    
    * src/spamprobe/spamprobe.cc (process_extended_options): Added
      whitelist option to allow use of SP as a bayesian white list in
      conjunction with other filters.

--- mail/spamprobe/options.mk
+++ mail/spamprobe/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.4 2007/02/22 19:26:44 wiz Exp $
+# $NetBSD: options.mk,v 1.5 2007/10/25 16:00:05 ghen Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.spamprobe
-PKG_SUPPORTED_OPTIONS=		bdb gif
-PKG_SUGGESTED_OPTIONS=		gif
+PKG_SUPPORTED_OPTIONS=		bdb gif png jpeg
+PKG_SUGGESTED_OPTIONS=		gif png jpeg
 
 .include "../../mk/bsd.options.mk"
 
@@ -14,4 +14,12 @@ CONFIGURE_ARGS+=	--with-db=${BDBBASE:Q}
 
 .if !empty(PKG_OPTIONS:Mgif)
 .include "../../graphics/libungif/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mpng)
+.include "../../graphics/png/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mjpg)
+.include "../../graphics/jpeg/buildlink3.mk"
 .endif
