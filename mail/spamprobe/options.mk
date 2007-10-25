28a35ac2fb09e158098547349293ae6a30ecb634
ghen <ghen@pkgsrc.org>
2007-10-25 21:45:42 +0000

    Fix typo, spotted by Chris Ross.

--- mail/spamprobe/options.mk
+++ mail/spamprobe/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2007/10/25 16:00:05 ghen Exp $
+# $NetBSD: options.mk,v 1.6 2007/10/25 21:45:42 ghen Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.spamprobe
 PKG_SUPPORTED_OPTIONS=		bdb gif png jpeg
@@ -20,6 +20,6 @@ CONFIGURE_ARGS+=	--with-db=${BDBBASE:Q}
 .include "../../graphics/png/buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mjpg)
+.if !empty(PKG_OPTIONS:Mjpeg)
 .include "../../graphics/jpeg/buildlink3.mk"
 .endif
