30678da7c57768f183ada490c4ccd408ff356b13
rillig <rillig>
2007-10-25 18:27:05 +0000

    simpler includes.

--- mk/plist/bsd.plist.mk
+++ mk/plist/bsd.plist.mk
@@ -1,7 +1,7 @@
-# $NetBSD: bsd.plist.mk,v 1.1 2006/01/12 23:43:57 jlam Exp $
+# $NetBSD: bsd.plist.mk,v 1.2 2007/10/25 18:27:05 rillig Exp $
 #
 # This Makefile fragment is included by bsd.pkg.mk and provides all
 # PLIST-related variables and targets.
 
-.include "../../mk/plist/plist.mk"
-.include "../../mk/plist/print-plist.mk"
+.include "plist.mk"
+.include "print-plist.mk"
