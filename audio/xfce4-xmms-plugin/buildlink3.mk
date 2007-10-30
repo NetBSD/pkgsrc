a7a6f940fb30ec3b8407404b01c6c7cdee0fc273
martti <martti@pkgsrc.org>
2007-10-30 08:15:18 +0000

    No not include options.mk

--- audio/xfce4-xmms-plugin/buildlink3.mk
+++ audio/xfce4-xmms-plugin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2007/04/12 09:55:08 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2007/10/30 08:15:18 martti Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_XMMS_PLUGIN_BUILDLINK3_MK:=	${XFCE4_XMMS_PLUGIN_BUILDLINK3_MK}+
@@ -15,8 +15,6 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-xmms-plugin
 BUILDLINK_API_DEPENDS.xfce4-xmms-plugin+=	xfce4-xmms-plugin>=0.5.1
 BUILDLINK_PKGSRCDIR.xfce4-xmms-plugin?=	../../audio/xfce4-xmms-plugin
 .endif	# XFCE4_XMMS_PLUGIN_BUILDLINK3_MK
-
-.include "options.mk"
 
 .include "../../x11/xfce4-panel/buildlink3.mk"
 .include "../../devel/xfce4-dev-tools/buildlink3.mk"
