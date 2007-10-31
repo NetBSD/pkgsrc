e3dd69ca5026b7433e9c3159319effb19db33b39
drochner <drochner@pkgsrc.org>
2007-10-31 20:26:21 +0000

    Switch to use the scrollkeeper compatible wrappers which come with the new "rarian" document manager. rarian (as configured currently) can read .omf files directly, so the wrappers are basically no-ops. Keep the "rarian" dependency anyway to avoid major reorganisations in the future.

--- textproc/scrollkeeper/omf.mk
+++ textproc/scrollkeeper/omf.mk
@@ -1,4 +1,4 @@
-# $NetBSD: omf.mk,v 1.12 2006/06/15 22:13:59 jlam Exp $
+# $NetBSD: omf.mk,v 1.13 2007/10/31 20:26:21 drochner Exp $
 #
 # This Makefile fragment is intended to be included by packages that install
 # OMF files.  It takes care of registering them in scrollkeeper's global
@@ -16,11 +16,11 @@ SCROLLKEEPER_OMF_MK=	# defined
 .include "../../mk/bsd.prefs.mk"
 
 # scrollkeeper's data directory.
-SCROLLKEEPER_DATADIR=	${BUILDLINK_PREFIX.scrollkeeper}/libdata/scrollkeeper
+SCROLLKEEPER_DATADIR=	${BUILDLINK_PREFIX.rarian}/libdata/scrollkeeper
 
 # scrollkeeper binary programs.
-SCROLLKEEPER_REBUILDDB=	${BUILDLINK_PREFIX.scrollkeeper}/bin/scrollkeeper-rebuilddb
-SCROLLKEEPER_UPDATEDB=	${BUILDLINK_PREFIX.scrollkeeper}/bin/scrollkeeper-update
+SCROLLKEEPER_REBUILDDB=	${BUILDLINK_PREFIX.rarian}/bin/scrollkeeper-rebuilddb
+SCROLLKEEPER_UPDATEDB=	${BUILDLINK_PREFIX.rarian}/bin/scrollkeeper-update
 
 INSTALL_TEMPLATES+=	${.CURDIR}/../../textproc/scrollkeeper/files/install.tmpl
 DEINSTALL_TEMPLATES+=	${.CURDIR}/../../textproc/scrollkeeper/files/install.tmpl
@@ -29,12 +29,10 @@ FILES_SUBST+=		SCROLLKEEPER_DATADIR=${SCROLLKEEPER_DATADIR:Q}
 FILES_SUBST+=		SCROLLKEEPER_REBUILDDB=${SCROLLKEEPER_REBUILDDB:Q}
 FILES_SUBST+=		SCROLLKEEPER_UPDATEDB=${SCROLLKEEPER_UPDATEDB:Q}
 
-PRINT_PLIST_AWK+=	/^libdata\/scrollkeeper/ { next; }
-PRINT_PLIST_AWK+=	/^@dirrm libdata\/scrollkeeper/ { next; }
 PRINT_PLIST_AWK+=	/^@dirrm share\/omf$$/ \
 				{ print "@comment in scrollkeeper: " $$0; \
 				  next; }
 
-.include "../../textproc/scrollkeeper/buildlink3.mk"
+.include "../../textproc/rarian/buildlink3.mk"
 
 .endif	# SCROLLKEEPER_OMF_MK
