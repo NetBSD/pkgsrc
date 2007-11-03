7de040776ed9d96dcbf994a1de761f4fc8d08f69
drochner <drochner@pkgsrc.org>
2007-11-03 16:27:40 +0000

    update to 2.20.3 This switches to the new gnome-2.20 branch.

--- editors/gedit/buildlink3.mk
+++ editors/gedit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2007/09/21 13:03:40 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2007/11/03 16:27:40 drochner Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GEDIT_BUILDLINK3_MK:=	${GEDIT_BUILDLINK3_MK}+
@@ -20,6 +20,6 @@ BUILDLINK_PKGSRCDIR.gedit?=	../../editors/gedit
 .include "../../devel/libglade/buildlink3.mk"
 .include "../../devel/libgnomeui/buildlink3.mk"
 .include "../../print/libgnomeprintui/buildlink3.mk"
-.include "../../x11/gtksourceview/buildlink3.mk"
+.include "../../x11/gtksourceview2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
