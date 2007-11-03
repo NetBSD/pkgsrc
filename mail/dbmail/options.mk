335fc285f31b12c2aaf43f0c7f5215291b9299c6
obache <obache>
2007-11-03 11:39:49 +0000

    Merge with wip/dbmail.
    * Honor PKG_SYSCONFDIR and VARBASE.
    * Added sqlite and ldap option.
    * Fixes PLIST when db driver != mysql.
    * Install more documentation.
    * Install initial create tables sql script files.
    * Rename timsieved's rc script to similar name with other scripts.
    
    Bump PKGREVISION.

--- mail/dbmail/options.mk
+++ mail/dbmail/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.5 2007/10/30 22:52:44 shannonjr Exp $
+# $NetBSD: options.mk,v 1.6 2007/11/03 11:39:50 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dbmail
-PKG_SUPPORTED_OPTIONS=	sieve
+PKG_SUPPORTED_OPTIONS=	ldap sieve
 PKG_OPTIONS_REQUIRED_GROUPS=	sql
-PKG_OPTIONS_GROUP.sql=	mysql pgsql
-PKG_SUGGESTED_OPTIONS=	mysql
+PKG_OPTIONS_GROUP.sql=	mysql pgsql sqlite
+PKG_SUGGESTED_OPTIONS=	mysql sieve
 
 .include "../../mk/bsd.options.mk"
 
@@ -13,7 +13,11 @@ PKG_SUGGESTED_OPTIONS=	mysql
 ###
 .if !empty(PKG_OPTIONS:Mmysql)
 .include "../../mk/mysql.buildlink3.mk"
-SQLDB=			mysql
+CONFIGURE_ARGS+=	--with-mysql
+PLIST_SUBST+=		MYSQL=""
+INSTALLATION_DIRS+=	${DATADIR:Q}/sql/mysql
+.else
+PLIST_SUBST+=		MYSQL="@comment "
 .endif
 
 ###
@@ -21,16 +25,58 @@ SQLDB=			mysql
 ###
 .if !empty(PKG_OPTIONS:Mpgsql)
 .include "../../mk/pgsql.buildlink3.mk"
-SQLDB=			pgsql
+CONFIGURE_ARGS+=	--with-pgsql
+PLIST_SUBST+=		PGSQL=""
+INSTALLATION_DIRS+=	${DATADIR:Q}/sql/pgsql
+.else
+PLIST_SUBST+=		PGSQL="@comment "
 .endif
 
 ###
-### libsieve support
+### SQLite database support
 ###
-PLIST_SRC=      ${.CURDIR}/PLIST
-.if !empty(PKG_OPTIONS:Msieve)
-CONFIGURE_ARGS+=	--with-sieve=${BUILDLINK_PREFIX.libsieve}
-PLIST_SRC+=		${.CURDIR}/PLIST.sieve
-.include "../../mail/libsieve/buildlink3.mk"
-.include "../../mail/gmime/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Msqlite)
+.include "../../databases/sqlite3/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-sqlite
+PLIST_SUBST+=		SQLITE=""
+INSTALLATION_DIRS+=	${DATADIR:Q}/sql/sqlite
+.else
+PLIST_SUBST+=		SQLITE="@comment "	
 .endif
+
+###
+### Sieve support
+###
+.if !empty(PKG_OPTIONS:Msieve)
+.include "../../mail/libsieve/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-sieve=${BUILDLINK_PREFIX.libsieve:Q}
+PLIST_SUBST+=		SIEVE=""
+FILES_SUBST+=		TIMSIEVED="dbmailtimsieved"
+RCD_SCRIPTS+=		dbmailtimsieved
+.else
+PLIST_SUBST+=		SIEVE="@comment "
+FILES_SUBST+=		TIMSIEVED=""
+.endif
+
+###
+### LDAP support
+###
+.if !empty(PKG_OPTIONS:Mldap)
+.include "../../databases/openldap-client/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-ldap=${BUILDLINK_PREFIX.openldap-client:Q}
+PLIST_SUBST+=		LDAP=""
+.else
+PLIST_SUBST+=		LDAP="@comment "
+.endif
+
+###
+### Migration scripts
+###
+post-install: dbmail-install-scripts
+
+dbmail-install-scripts:
+.for d in ${PKG_OPTIONS_GROUP.sql}
+.  if !empty(PKG_OPTIONS:M${d})
+	${INSTALL_DATA} ${WRKSRC:Q}/sql/$d/* ${DATADIR:Q}/sql/$d
+.  endif
+.endfor
