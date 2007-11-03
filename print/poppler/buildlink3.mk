9077e037fe57eb4042e647fa36f94d12a631ddcc
drochner <drochner@pkgsrc.org>
2007-11-03 16:10:38 +0000

    update to 0.6.1 This switches to the new 0.6 branch which is not source compatible to 0.5.x. Most notable changes: -Merge xpdf 3.02 changes -Support for Sound objects -Support for Opening/Closing page actions -Support for page duration -Improve PS Tokenizer performance -Beginning of Interactive Form support -xpdfrc is no longer used for anything -Fix security issue MOAB-06-01-2007 -Lots of bugs fixed

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2006/07/08 23:11:05 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2007/11/03 16:10:38 drochner Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 POPPLER_BUILDLINK3_MK:=	${POPPLER_BUILDLINK3_MK}+
@@ -17,5 +17,6 @@ BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 .endif	# POPPLER_BUILDLINK3_MK
 
 .include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
