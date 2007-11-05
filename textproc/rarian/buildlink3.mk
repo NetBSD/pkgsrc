8d1d989bb3c639bd8be4ba7608fea94a34375334
jmmv <jmmv>
2007-11-05 17:06:33 +0000

    Mimic scrollkeeper's buildlink3.mk and create fake wrappers for those tools that we don't want packages to execute directly.

--- textproc/rarian/buildlink3.mk
+++ textproc/rarian/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/10/11 18:43:48 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2007/11/05 17:06:33 jmmv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 RARIAN_BUILDLINK3_MK:=	${RARIAN_BUILDLINK3_MK}+
@@ -14,6 +14,28 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}rarian
 .if ${RARIAN_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.rarian+=	rarian>=0.6.0
 BUILDLINK_PKGSRCDIR.rarian?=	../../textproc/rarian
+.endif	# RARIAN_BUILDLINK3_MK
+
+.if !empty(RARIAN_BUILDLINK3_MK:M+)
+BUILDLINK_TARGETS+=	rarian-buildlink-fake
+
+rarian-buildlink-fake:
+	${_PKG_SILENT}${_PKG_DEBUG}					\
+	for fake in scrollkeeper-update	scrollkeeper-rebuilddb		\
+	    rarian-sk-update rarian-sk-rebuild;				\
+	do								\
+		if [ ! -f ${BUILDLINK_DIR}/bin/$${fake} ]; then		\
+			${ECHO_BUILDLINK_MSG}				\
+			    "Creating ${BUILDLINK_DIR}/bin/$${fake}";	\
+			${MKDIR} ${BUILDLINK_DIR}/bin;			\
+			${ECHO} "#!/bin/sh"				\
+			    > ${BUILDLINK_DIR}/bin/$${fake};		\
+			${ECHO} "echo '[pkgsrc] Doing nothing for"	\
+			    "$${fake}' \$$*"				\
+			    >>${BUILDLINK_DIR}/bin/$${fake};		\
+			${CHMOD} +x ${BUILDLINK_DIR}/bin/$${fake};	\
+		fi;							\
+	done
 .endif	# RARIAN_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
