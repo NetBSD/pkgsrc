676634e016a3a5ccdf3b9d32b7b5803dc4c4afc0
joerg <joerg@pkgsrc.org>
2007-11-08 19:39:42 +0000

    Bump version or revision of all packages that have a runtime dependency on gzip.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.84 2007/11/03 13:54:14 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.85 2007/11/08 19:39:42 joerg Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20071103"
+#define PKGTOOLS_VERSION "20071108"
 
 #endif /* _INST_LIB_VERSION_H_ */
