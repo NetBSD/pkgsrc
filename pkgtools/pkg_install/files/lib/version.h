8210aa84138c31d0bfeff8589b126dfa0eb55ccb
joerg <joerg@pkgsrc.org>
2007-11-08 23:24:56 +0000

    Don't use the build time version to find packages, we want to pick up the newest version. Bump version to 20071109.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.85 2007/11/08 19:39:42 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.86 2007/11/08 23:24:56 joerg Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20071108"
+#define PKGTOOLS_VERSION "20071109"
 
 #endif /* _INST_LIB_VERSION_H_ */
