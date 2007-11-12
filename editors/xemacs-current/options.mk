902dc58806a89aef6b1eea9b587ae2ed0e790a02
uebayasi <uebayasi>
2007-11-12 01:48:47 +0000

    Support XEmacs 21.5.x without X too.

--- editors/xemacs-current/options.mk
+++ editors/xemacs-current/options.mk
@@ -1,9 +1,18 @@
-# $NetBSD: options.mk,v 1.8 2006/09/04 09:20:51 martin Exp $
+# $NetBSD: options.mk,v 1.9 2007/11/12 01:48:48 uebayasi Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xemacs
-PKG_SUPPORTED_OPTIONS=	esound ldap xface canna debug
+PKG_SUPPORTED_OPTIONS=	esound ldap xface canna debug x11
+PKG_OPTIONS_OPTIONAL_GROUPS=	toolkit
+PKG_OPTIONS_GROUP.toolkit=	gtk motif xaw
+PKG_SUGGESTED_OPTIONS=	x11 xaw
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgtk) || !empty(PKG_OPTIONS:Mmotif) || !empty(PKG_OPTIONS:Mxaw)
+.  if empty(PKG_OPTIONS:Mx11)
+PKG_OPTIONS+=		x11
+.  endif
+.endif
 
 .if !empty(PKG_OPTIONS:Mldap)
 CONFIGURE_ARGS+=	--with-ldap
@@ -36,3 +45,29 @@ CONFIGURE_ARGS+=	--with-sound=none
 CONFIGURE_ARGS+=	--enable-debug=yes --with-debug
 .endif
 
+.if !empty(PKG_OPTIONS:Mx11)
+.  include "../../graphics/jpeg/buildlink3.mk"
+.  include "../../graphics/png/buildlink3.mk"
+.  include "../../graphics/tiff/buildlink3.mk"
+.  include "../../x11/xbitmaps/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-x11
+CONFIGURE_ARGS+=	--without-jpeg
+CONFIGURE_ARGS+=	--without-png
+CONFIGURE_ARGS+=	--without-tiff
+CONFIGURE_ARGS+=	--without-xpm
+.endif
+
+.if !empty(PKG_OPTIONS:Mgtk)
+.  include "../../x11/gtk/buildlink3.mk"
+WITH_WIDGETS=	--with-gtk
+.endif
+
+.if !empty(PKG_OPTIONS:Mmotif)
+.  include "../../mk/motif.buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mxaw)
+.  include "../../mk/xaw.buildlink3.mk"
+WITH_WIDGETS=	--with-widgets=lucid
+.endif
