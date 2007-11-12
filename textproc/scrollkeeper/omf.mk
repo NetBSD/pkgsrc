5580a5e180f50ad057ee58873661262cbc98746c
drochner <drochner>
2007-11-12 20:41:18 +0000

    move the install/deinstall templates from scrollkeeper to rarian to reflect where the work is done (call the backwards-compatible rarian version "install-scrollkeeper" to leave room for a future non-compatibility version)

--- textproc/scrollkeeper/omf.mk
+++ textproc/scrollkeeper/omf.mk
@@ -1,4 +1,4 @@
-# $NetBSD: omf.mk,v 1.13 2007/10/31 20:26:21 drochner Exp $
+# $NetBSD: omf.mk,v 1.14 2007/11/12 20:41:18 drochner Exp $
 #
 # This Makefile fragment is intended to be included by packages that install
 # OMF files.  It takes care of registering them in scrollkeeper's global
@@ -22,15 +22,17 @@ SCROLLKEEPER_DATADIR=	${BUILDLINK_PREFIX.rarian}/libdata/scrollkeeper
 SCROLLKEEPER_REBUILDDB=	${BUILDLINK_PREFIX.rarian}/bin/scrollkeeper-rebuilddb
 SCROLLKEEPER_UPDATEDB=	${BUILDLINK_PREFIX.rarian}/bin/scrollkeeper-update
 
-INSTALL_TEMPLATES+=	${.CURDIR}/../../textproc/scrollkeeper/files/install.tmpl
-DEINSTALL_TEMPLATES+=	${.CURDIR}/../../textproc/scrollkeeper/files/install.tmpl
+INSTALL_TEMPLATES+= \
+ ${.CURDIR}/../../textproc/rarian/files/install-scrollkeeper.tmpl
+DEINSTALL_TEMPLATES+= \
+ ${.CURDIR}/../../textproc/rarian/files/install-scrollkeeper.tmpl
 
 FILES_SUBST+=		SCROLLKEEPER_DATADIR=${SCROLLKEEPER_DATADIR:Q}
 FILES_SUBST+=		SCROLLKEEPER_REBUILDDB=${SCROLLKEEPER_REBUILDDB:Q}
 FILES_SUBST+=		SCROLLKEEPER_UPDATEDB=${SCROLLKEEPER_UPDATEDB:Q}
 
 PRINT_PLIST_AWK+=	/^@dirrm share\/omf$$/ \
-				{ print "@comment in scrollkeeper: " $$0; \
+				{ print "@comment in rarian: " $$0; \
 				  next; }
 
 .include "../../textproc/rarian/buildlink3.mk"
