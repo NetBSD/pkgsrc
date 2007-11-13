3e2dc464a55eb057f56c1ff75607f0b4a9a2f6b4
jlam <jlam>
2007-11-13 18:02:15 +0000

    Import cone-0.73 as pkgsrc/mail/cone:
    Cone is a text-based mail client.  Cone seamlessly handles multiple
    POP3, IMAP accounts, and local mail folders.  Cone is also a simple
    newsreader.  Cone's interface is foolproof enough to be used by
    inexperienced users, but it also offers advanced features for power
    users.  Cone also serves as a platform for development of a new
    experimental network mail access protocol, SMAP, that offers additional
    functionality not available with IMAP or POP3.

---
+++ mail/cone/options.mk
@@ -0,0 +1,22 @@
+PKG_OPTIONS_VAR=		PKG_OPTIONS.cone
+PKG_SUPPORTED_OPTIONS=		aspell ldap
+PKG_SUGGESTED_OPTIONS=		aspell ldap
+
+.include "../../mk/bsd.options.mk"
+
+# Package-specific option-handling
+
+###
+### LDAP addressbook support
+###
+.if !empty(PKG_OPTIONS:Mldap)
+.  include "../../databases/openldap-client/buildlink3.mk"
+.endif
+
+###
+### Aspell spell-check support
+###
+.if !empty(PKG_OPTIONS:Maspell)
+.  include "../../textproc/aspell/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-spellcheck=aspell
+.endif
