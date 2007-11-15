781e4c8c351e6a18dc861e2acf46d43b719e1867
rillig <rillig@pkgsrc.org>
2007-11-15 10:39:18 +0000

    Updated sqlite to 3.5.2. The main reason to update was that on Solaris, the file lemon.c uses the same identifiers (B_TRUE, B_FALSE) as the <sys/types.h> header, and therefore fails to build.
    Changes since 3.4.2:
    
       2007 Nov 05 (3.5.2)
    
         * Dropped support for the SQLITE_OMIT_MEMORY_ALLOCATION compile-time
           option.
         * Always open files using FILE_FLAG_RANDOM_ACCESS under windows.
         * The 3rd parameter of the built-in SUBSTR() function is now
           optional.
         * Bug fix: do not invoke the authorizer when reparsing the schema
           after a schema change.
         * Added the experimental malloc-free memory allocator in mem3.c.
         * Virtual machine stores 64-bit integer and floating point constants
           in binary instead of text for a performance boost.
         * Fix a race condition in test_async.c.
         * Added the ".timer" command to the CLI
    
       2007 Oct 04 (3.5.1)
    
         * Nota Bene: We are not using terms "alpha" or "beta" on this release
           because the code is stable and because if we use those terms,
           nobody will upgrade. However, we still reserve the right to make
           incompatible changes to the new VFS interface in future releases.
         * Fix a bug in the handling of SQLITE_FULL errors that could lead to
           database corruption. [11]Ticket #2686.
         * The test_async.c drive now does full file locking and works
           correctly when used simultaneously by multiple processes on the
           same database.
         * The CLI ignores whitespace (including comments) at the end of lines
         * Make sure the query optimizer checks dependences on all terms of a
           compound SELECT statement. [12]Ticket #2640.
         * Add demonstration code showing how to build a VFS for a raw mass
           storage without a filesystem.
         * Added an output buffer size parameter to the xGetTempname() method
           of the VFS layer.
         * Sticky SQLITE_FULL or SQLITE_IOERR errors in the pager are reset
           when a new transaction is started.
    
       2007 Sep 04 (3.5.0) alpha
    
         * Redesign the OS interface layer. See [13]34to35.html for details.
           *** Potentially incompatible change ***
         * The [14]sqlite3_release_memory(), [15]sqlite3_soft_heap_limit(),
           and [16]sqlite3_enable_shared_cache() interfaces now work cross all
           threads in the process, not just the single thread in which they
           are invoked. *** Potentially incompatible change ***
         * Added the [17]sqlite3_open_v2() interface.
         * Reimplemented the memory allocation subsystem and made it
           replacable at compile-time.
         * Created a new mutex subsystem and made it replacable at
           compile-time.
         * The same database connection may now be used simultaneously by
           separate threads.

--- databases/sqlite3/buildlink3.mk
+++ databases/sqlite3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/07/08 23:10:40 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2007/11/15 10:39:18 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 SQLITE3_BUILDLINK3_MK:=	${SQLITE3_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}sqlite3
 
 .if !empty(SQLITE3_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.sqlite3+=	sqlite3>=3.0.8
-BUILDLINK_ABI_DEPENDS.sqlite3?=	sqlite3>=3.2.7nb1
+BUILDLINK_ABI_DEPENDS.sqlite3+=	sqlite3>=3.2.7nb1
 BUILDLINK_PKGSRCDIR.sqlite3?=	../../databases/sqlite3
 .endif	# SQLITE3_BUILDLINK3_MK
 
