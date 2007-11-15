fafd035bab88686c3ade7bad940765d47aa119e2
sborrill <sborrill@pkgsrc.org>
2007-11-15 12:57:21 +0000

    The startup order of the jabberd components is important; alphabetical order isn't good enough!
    Ensure that c2s starts after router (which it needs to connect to). This
    saves it having to retry the connection.
    
    Bump PKGREVISION

--- chat/jabberd2/files/jabberd.sh
+++ chat/jabberd2/files/jabberd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: jabberd.sh,v 1.1.1.1 2004/03/28 19:44:07 xtraeme Exp $
+# $NetBSD: jabberd.sh,v 1.2 2007/11/15 12:57:22 sborrill Exp $
 #
 # KEYWORD: nostart
 
@@ -36,7 +36,7 @@ reverse_commands()
 	done
 }
 
-COMMAND_LIST="c2s resolver router s2s sm"
+COMMAND_LIST="resolver router c2s s2s sm"
 
 name="jabberd"
 start_cmd="forward_commands"
