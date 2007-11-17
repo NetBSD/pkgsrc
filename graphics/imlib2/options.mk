bad2dc259bfce4b6fe0f0c6104f2eea23430419f
obache <obache>
2007-11-17 01:40:50 +0000

    Add x11 option (default: enabled).
    No functionally change, but bump PKGREVISION due to detect x11 option.

--- graphics/imlib2/options.mk
+++ graphics/imlib2/options.mk
@@ -1,9 +1,10 @@
-# $NetBSD: options.mk,v 1.6 2007/06/13 14:01:11 obache Exp $
+# $NetBSD: options.mk,v 1.7 2007/11/17 01:40:50 obache Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.imlib2
-PKG_SUPPORTED_OPTIONS=	#empty
+PKG_SUPPORTED_OPTIONS=	x11
+PKG_SUGGESTED_OPTIONS=	x11
 .if ${MACHINE_ARCH} == "i386"
 PKG_SUPPORTED_OPTIONS+=	mmx
 .elif ${MACHINE_ARCH} == "x86_64"
@@ -20,3 +21,14 @@ CONFIGURE_ARGS+=	--enable-mmx
 .if !empty(PKG_OPTIONS:Mimlib2-amd64)
 CONFIGURE_ARGS+=	--enable-amd64
 .endif
+
+.if !empty(PKG_OPTIONS:Mx11)
+.include "../../x11/xextproto/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+PLIST_SUBST+=		X11=""
+.else
+CONFIGURE_ARGS+=	--without-x
+PLIST_SUBST+=		X11="@comment "
+.endif
+
