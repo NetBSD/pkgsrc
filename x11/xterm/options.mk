8af38d8a1f1ec52327efa8dcf638bc758a92601b
bjs <bjs@pkgsrc.org>
2007-11-23 14:23:34 +0000

    Force ac_cv_lib_pcreposix_pcreposix_regcomp=yes, bump rev. This function does not exist AFAIK in our pcre libraries, and the configure check was failing.
    A segment of nm output reads:
    
    /usr/pkg/lib/libpcreposix.so:00000a06 T regcomp
    /usr/pkg/lib/libpcreposix.so:00000abf T regerror
    /usr/pkg/lib/libpcreposix.so:000007a8 T regexec
    /usr/pkg/lib/libpcreposix.so:00000780 T regfree
    
    These symbols will override those in libc:
    
    
    /usr/lib/libc.so:00031be8 T _regcomp
    /usr/lib/libc.so:0002fbf8 T _regerror
    /usr/lib/libc.so:0002e9c2 T _regexec
    /usr/lib/libc.so:0006336c T _regfree
    /usr/lib/libc.so:00031be8 W regcomp
    /usr/lib/libc.so:0002fbf8 W regerror
    /usr/lib/libc.so:0002e9c2 W regexec
    /usr/lib/libc.so:0006336c W regfree

--- x11/xterm/options.mk
+++ x11/xterm/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2007/10/02 15:04:53 aymeric Exp $
+# $NetBSD: options.mk,v 1.7 2007/11/23 14:23:34 bjs Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xterm
 PKG_SUPPORTED_OPTIONS=	pcre xterm-luit freetype xterm-toolbar
@@ -30,7 +30,14 @@ CONFIGURE_ARGS+= --enable-freetype
 CONFIGURE_ARGS+= --enable-toolbar
 .endif
 
+###
+### XXX The configure test checks for pcreposix_regcomp in libpcreposix.
+###	However, there is no such function [insofar as I can tell].
+###	Moreover, libc's regex(3) functions are weak-aliased to those
+###     within libc's namespace, so we should have no problems.
+###
 .if !empty(PKG_OPTIONS:Mpcre)
 CONFIGURE_ARGS+= --with-pcre
+CONFIGURE_ENV+=  ac_cv_lib_pcreposix_pcreposix_regcomp=yes
 .  include "../../devel/pcre/buildlink3.mk"
 .endif
