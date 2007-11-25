d4136923fccf56d78d317b50df5ede41043d5c6f
wiz <wiz>
2007-11-25 12:44:07 +0000

    Make curses support completely optional, in response to PR 37173 by David A. Holland.

--- news/tin/options.mk
+++ news/tin/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.8 2007/09/19 05:55:05 wiz Exp $
+# $NetBSD: options.mk,v 1.9 2007/11/25 12:44:07 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.tin
 PKG_SUPPORTED_OPTIONS=		icu inet6 tin-use-inn-spool
-PKG_OPTIONS_REQUIRED_GROUPS=	display
+PKG_OPTIONS_OPTIONAL_GROUPS=	display
 PKG_OPTIONS_GROUP.display=	curses ncurses ncursesw
 PKG_SUGGESTED_OPTIONS=		curses
 # untested
