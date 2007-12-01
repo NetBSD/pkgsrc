877488e218cf952fac1d11493179fde7fa416dd6
rillig <rillig>
2007-12-01 04:17:50 +0000

    Updated crypto++ to 5.5.2.
    Changes since 5.2.1:
    5.2.2 - added SHA-224
          - put SHA-256, SHA-384, SHA-512, RSASSA-PSS into DLL
    
    5.2.3 - fixed issues with FIPS algorithm test vectors
          - put RSASSA-ISO into DLL
    
    5.3 - ported to MSVC 2005 with support for x86-64
        - added defense against AES timing attacks, and more AES test vectors
        - changed StaticAlgorithmName() of Rijndael to "AES", CTR to "CTR"
    
    5.4 - added Salsa20
        - updated Whirlpool to version 3.0
        - ported to GCC 4.1, Sun C++ 5.8, and Borland C++Builder 2006
    
    5.5 - added VMAC and Sosemanuk (with x86-64 and SSE2 assembly)
        - improved speed of integer arithmetic, AES, SHA-512, Tiger, Salsa20,
          Whirlpool, and PANAMA cipher using assembly (x86-64, MMX, SSE2)
        - optimized Camellia and added defense against timing attacks
        - updated benchmarks code to show cycles per byte and to time key/IV setup
        - started using OpenMP for increased multi-core speed
        - enabled GCC optimization flags by default in GNUmakefile
        - added blinding and computational error checking for RW signing
        - changed RandomPool, X917RNG, GetNextIV, DSA/NR/ECDSA/ECNR to reduce
          the risk of reusing random numbers and IVs after virtual machine state
          rollback
        - changed default FIPS mode RNG from AutoSeededX917RNG<DES_EDE3> to
          AutoSeededX917RNG<AES>
        - fixed PANAMA cipher interface to accept 256-bit key and 256-bit IV
        - moved MD2, MD4, MD5, PanamaHash, ARC4, WAKE_CFB into the namespace "Weak"
        - removed HAVAL, MD5-MAC, XMAC
    
    5.5.1 - fixed VMAC validation failure on 32-bit big-endian machines
    
    5.5.2 - ported x64 assembly language code for AES, Salsa20, Sosemanuk, and Panama
            to MSVC 2005 (using MASM since MSVC doesn't support inline assembly on x64)
          - fixed Salsa20 initialization crash on non-SSE2 machines
          - fixed Whirlpool crash on Pentium 2 machines
          - fixed possible branch prediction analysis (BPA) vulnerability in
            MontgomeryReduce(), which may affect security of RSA, RW, LUC
          - fixed link error with MSVC 2003 when using "debug DLL" form of runtime library
          - fixed crash in SSE2_Add on P4 machines when compiled with
            MSVC 6.0 SP5 with Processor Pack
          - added support for newly released compilers: MSVC 2008, GCC 4.2, Sun CC 5.9,
            Intel C++ Compiler 10.0, and Borland C++Builder 2007

--- security/crypto++/buildlink3.mk
+++ security/crypto++/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2006/07/08 23:11:06 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2007/12/01 04:17:50 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 CRYPTOPP_BUILDLINK3_MK:=	${CRYPTOPP_BUILDLINK3_MK}+
@@ -15,7 +15,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}cryptopp
 BUILDLINK_API_DEPENDS.cryptopp+=	cryptopp>=5.1
 BUILDLINK_DEPMETHOD.cryptopp?=	build
 BUILDLINK_ABI_DEPENDS.cryptopp?=	cryptopp>=5.2.1nb1
-BUILDLINK_PKGSRCDIR.cryptopp?=	../../security/cryptopp
+BUILDLINK_PKGSRCDIR.cryptopp?=	../../security/crypto++
 .endif	# CRYPTOPP_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
