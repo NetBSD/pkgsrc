0f6795e4861da1139aa7eb2276e5573908912ce1
tnn <tnn@pkgsrc.org>
2007-12-02 04:17:54 +0000

    Update the pidgin IM suite to version 2.3.0.
    version 2.3.0 (11/24/2007):
    	http://developer.pidgin.im/query?status=closed&milestone=2.3.0
    Some highlights from ChangeLog:
    
    Pidgin:
    * Connection errors are now reported in mini-dialogs inside the buddy
      list, rather than as buttons in the buddy list and with dialog
      boxes.  If several accounts are disabled when you sign on elsewhere,
      you can now re-enable them all with a single click.
    * If you alias a buddy to an alias that is already present within
      a particular group, we now offer to merge the buddies into the
      same contact.
    * A music emblem is now displayed in the buddy list for a buddy if we
      know she is listening to some soothing music.
    * Added a 'Move to' menu in buddy list context menu for moving buddies
      to other groups as an alternative to dragging.
    * It is now possible to mark a chat on your buddy list as "Persistent"
      so you do not leave the chat when the window or tab is closed.
    * The auto-join option for chats is now listed in the "Add Chat"
      dialog along with the new persistence option.
    * Closing an IM no longer immediately closes your conversation.  It
      will now remain active for a short time so that if the conversation
      resumes, the history will be retained.  A preference has been added
      to toggle this behavior.
    * The "Smiley" menu has been moved to the top-level of the toolbar.
    * Pidgin's display is now saved with the command line for session
      restoration.  (David Mohr)
    * ICQ Birthday notifications are shown as buddy list emblems.
    * Plugin actions are now available from the docklet context menu
      in addition to the Tool menu of the buddy list.
    * The manual page has been heavily rewritten to bring it in line
      with current functionality.
    
    Finch:
    * It's possible to bind key-strokes to specific menuitems in the windows.
      Read the 'Menus' section in the man-page for details.
    * 'transpose-chars' operation for the entry boxes. The default key-binding
      is ctrl+t.
    * 'yank' operation for the entry boxes. The default binding is ctrl+y.

--- chat/pidgin/buildlink3.mk
+++ chat/pidgin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2007/11/03 15:59:18 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2007/12/02 04:17:55 tnn Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PIDGIN_BUILDLINK3_MK:=	${PIDGIN_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	pidgin
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}pidgin
 
 .if ${PIDGIN_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.pidgin+=	pidgin>=2.2.2
+BUILDLINK_API_DEPENDS.pidgin+=	pidgin>=2.3.0
 BUILDLINK_PKGSRCDIR.pidgin?=	../../chat/pidgin
 .endif	# PIDGIN_BUILDLINK3_MK
 
