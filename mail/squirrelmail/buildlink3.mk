d8f2db13e75b003e640c9c29cc83d14470eccc90
ghen <ghen@pkgsrc.org>
2007-12-05 07:31:12 +0000

    Pullup ticket 2238 - requested by martti security update for squirrelmail
    - pkgsrc/mail/squirrelmail/Makefile			1.95
    - pkgsrc/mail/squirrelmail/PLIST			1.24
    - pkgsrc/mail/squirrelmail/buildlink3.mk		1.17
    - pkgsrc/mail/squirrelmail/distinfo			1.43
    - pkgsrc/mail/squirrelmail/patches/patch-aa		1.14
    
       Module Name:	pkgsrc
       Committed By:	martti
       Date:		Wed Dec  5 07:11:29 UTC 2007
    
       Modified Files:
    	   pkgsrc/mail/squirrelmail: Makefile PLIST buildlink3.mk distinfo
    	   pkgsrc/mail/squirrelmail/patches: patch-aa
    
       Log Message:
       Updated mail/squirrelmail to 1.4.12
    
       NOTE: includes a critical bug fix in the attachment handling
    
          - Enabled user selection of address format when adding from address
            book during message composition.
          - Fixed issue with adding attachments in PHP 4.x environments (#1805471).
          - Backport size setting on "newmail" popup window.
          - Added a "short_open_tag" configuration test.
          - Undefined notice in error message box when no default folder prefix is set.
          - Undefined index error when downloading.  Possibly caused by using tabs and
            opening multiple mailboxes.
          - PAGE_NAME might not be defined in all plugins, which might cause a
            "not defined" error on session timeouts.
          - Fixed outgoing messages to allow addresses such as "0@..." or "000@...",
            etc. (#1818398).
          - Fixed issue with in-reply-to and reference headers not being retained on
            reply (#1810659).
          - Revived logout_error hook (#1800015).
          - Allow custom session handlers to work correctly (and be defined at the
            application level with SquirrelMail).
          - Fix off-by-one in bodystructure parsing triggered by servers sending
            a body location part (e.g. Sun Java System Messaging Server). Thanks
            John Callahan (#1808382).
          - Invalid initialization of To: header (#1772893).
          - Includes cleanup in include/validate.php.
          - Cleanup in multiple files to remove unneeded includes.
          - Added sort by size (#812233 and #159997, plus multiple list requests).
            Patch provided by Christopher E. Brown.
          - Fix bug in sitewide SMTP settings still using authenticated user, rather
            than configured settings (#1835942).
          - Fixed mailto: functionality.
          - Added mailto: link handling when viewing messages.
          - Handle PHP's insistence on setting the value to 'deleted' for destroyed
            sessions

--- mail/squirrelmail/buildlink3.mk
+++ mail/squirrelmail/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2007/10/02 05:28:19 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.16.2.1 2007/12/05 07:31:13 ghen Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SQUIRRELMAIL_BUILDLINK3_MK:=	${SQUIRRELMAIL_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	squirrelmail
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}squirrelmail
 
 .if ${SQUIRRELMAIL_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.squirrelmail+=	squirrelmail>=1.4.11
+BUILDLINK_API_DEPENDS.squirrelmail+=	squirrelmail>=1.4.12
 BUILDLINK_PKGSRCDIR.squirrelmail?=	../../mail/squirrelmail
 .endif	# SQUIRRELMAIL_BUILDLINK3_MK
 
