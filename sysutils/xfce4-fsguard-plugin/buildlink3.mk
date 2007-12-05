b723485ba4633d8c24bdbd637c4e3bd5d5d91335
martti <martti@pkgsrc.org>
2007-12-05 07:49:44 +0000

    Updated sysutils/xfce4-fsguard-plugin to 0.4.0
    Bug fixes.

--- sysutils/xfce4-fsguard-plugin/buildlink3.mk
+++ sysutils/xfce4-fsguard-plugin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2007/04/12 09:55:16 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2007/12/05 07:49:44 martti Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_FSGUARD_PLUGIN_BUILDLINK3_MK:=	${XFCE4_FSGUARD_PLUGIN_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	xfce4-fsguard-plugin
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-fsguard-plugin
 
 .if ${XFCE4_FSGUARD_PLUGIN_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.xfce4-fsguard-plugin+=	xfce4-fsguard-plugin>=0.3.0
+BUILDLINK_API_DEPENDS.xfce4-fsguard-plugin+=	xfce4-fsguard-plugin>=0.4.0
 BUILDLINK_PKGSRCDIR.xfce4-fsguard-plugin?=	../../sysutils/xfce4-fsguard-plugin
 .endif	# XFCE4_FSGUARD_PLUGIN_BUILDLINK3_MK
 
