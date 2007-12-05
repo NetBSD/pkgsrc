ce0bc59d8b13e93cd605dd33af2fb9191c487625
martti <martti@pkgsrc.org>
2007-12-05 07:51:18 +0000

    Updated x11/xfce4-places-plugin to 1.0.0
    Bug fixes.

--- x11/xfce4-places-plugin/buildlink3.mk
+++ x11/xfce4-places-plugin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/04/27 10:48:06 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2007/12/05 07:51:18 martti Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_PLACES_PLUGIN_BUILDLINK3_MK:=	${XFCE4_PLACES_PLUGIN_BUILDLINK3_MK}+
@@ -12,10 +12,11 @@ BUILDLINK_PACKAGES+=	xfce4-places-plugin
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-places-plugin
 
 .if ${XFCE4_PLACES_PLUGIN_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.xfce4-places-plugin+=	xfce4-places-plugin>=0.1.0
+BUILDLINK_API_DEPENDS.xfce4-places-plugin+=	xfce4-places-plugin>=1.0.0
 BUILDLINK_PKGSRCDIR.xfce4-places-plugin?=	../../x11/xfce4-places-plugin
 .endif	# XFCE4_PLACES_PLUGIN_BUILDLINK3_MK
 
+.include "../../sysutils/xfce4-thunar/buildlink3.mk"
 .include "../../x11/xfce4-panel/buildlink3.mk"
 .include "../../devel/xfce4-dev-tools/buildlink3.mk"
 .include "../../x11/libSM/buildlink3.mk"
