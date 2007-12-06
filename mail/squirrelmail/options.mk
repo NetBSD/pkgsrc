a5a040427528f5d52e53955e8c0918467ed25ba4
ghen <ghen>
2007-12-06 10:36:33 +0000

    Pullup ticket 2243 - requested by taca bugfix update for squirrelmail (squirrelmail-japanese option)
    - pkgsrc/mail/squirrelmail/distinfo			1.44
    - pkgsrc/mail/squirrelmail/options.mk			1.6
    
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Wed Dec  5 11:25:57 UTC 2007
    
       Modified Files:
    	   pkgsrc/mail/squirrelmail: distinfo options.mk
    
       Log Message:
       Catch up squirrelmail-japanese patch to 1.4.12-ja-20071205.

--- mail/squirrelmail/options.mk
+++ mail/squirrelmail/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2007/10/09 08:51:30 taca Exp $
+# $NetBSD: options.mk,v 1.5.2.1 2007/12/06 10:36:33 ghen Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.squirrelmail
 PKG_SUPPORTED_OPTIONS=	squirrelmail-japanese squirrelmail-lite
@@ -6,10 +6,10 @@ PKG_SUPPORTED_OPTIONS=	squirrelmail-japanese squirrelmail-lite
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Msquirrelmail-japanese)
-PATCHFILES+=		squirrelmail-1.4.11-ja-20071002-patch.gz
-SITES.squirrelmail-1.4.11-ja-20071002-patch.gz= \
+PATCHFILES+=		squirrelmail-1.4.12-ja-20071205-patch.gz
+SITES.squirrelmail-1.4.12-ja-20071205-patch.gz= \
 			http://www.yamaai-tech.com/~masato/Download/
-PATCH_DIST_STRIP.squirrelmail-1.4.11-ja-20071002-patch.gz=	-p1
+PATCH_DIST_STRIP.squirrelmail-1.4.12-ja-20071205-patch.gz=	-p1
 
 .endif
 
@@ -17,4 +17,6 @@ PATCH_DIST_STRIP.squirrelmail-1.4.11-ja-20071002-patch.gz=	-p1
 PATCHFILES+=		squirrelmail-1.4.11-lite-20071003-patch.bz2
 SITES.squirrelmail-1.4.11-lite-20071003-patch.bz2= \
 			ftp://ftp.back-street.net/pub/squirrelmail/
+
+DEPENDS+=	${PHP_PKG_PREFIX}-mbstring>=4.3.3:../../misc/php-mbstring
 .endif
