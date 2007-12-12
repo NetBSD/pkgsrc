096c42f3247002f66839d946772652c180f515c1
markd <markd>
2007-12-12 05:14:15 +0000

    Add support for kde4 (currently in wip) so that packages in wip can use "USE_DIRS+= kde4-1.0"

--- mk/dirs.mk
+++ mk/dirs.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dirs.mk,v 1.12 2007/08/23 09:49:18 joerg Exp $
+# $NetBSD: dirs.mk,v 1.13 2007/12/12 05:14:15 markd Exp $
 #
 
 .if !defined(DIRS_MK)
@@ -7,6 +7,7 @@ DIRS_MK=		# defined
 _USE_GNOME1_DIRS=	# empty
 _USE_GNOME2_DIRS=	# empty
 _USE_KDE3_DIRS=		# empty
+_USE_KDE4_DIRS=		# empty
 _USE_TEXMF_DIRS=	# empty
 _USE_THEME_DIRS=	# empty
 _USE_XDG_DIRS=		# empty
@@ -23,6 +24,8 @@ _USE_GNOME1_DIRS:=	${ver}
 _USE_GNOME2_DIRS:=	${ver}
 .  elif ${pkg} == "kde3" && ${_USE_KDE3_DIRS} < ${ver}
 _USE_KDE3_DIRS:=	${ver}
+.  elif ${pkg} == "kde4" && ${_USE_KDE4_DIRS} < ${ver}
+_USE_KDE4_DIRS:=	${ver}
 .  elif ${pkg} == "texmf" && ${_USE_TEXMF_DIRS} < ${ver}
 _USE_TEXMF_DIRS:=	${ver}
 .  elif ${pkg} == "theme" && ${_USE_THEME_DIRS} < ${ver}
@@ -49,6 +52,10 @@ _USE_XDG_X11_DIRS:=	${ver}
 
 .if !empty(_USE_KDE3_DIRS)
 .  include "../../misc/kde3-dirs/dirs.mk"
+.endif
+
+.if !empty(_USE_KDE4_DIRS)
+.  include "../../wip/kde4-dirs/dirs.mk"
 .endif
 
 .if !empty(_USE_TEXMF_DIRS)
