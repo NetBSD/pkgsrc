443f04ba2f168540f4733b7ab5b3ebae68efe4eb
adrianp <adrianp@pkgsrc.org>
2007-12-12 20:02:08 +0000

    This package provides the ISC Dynamic Host Configuration Protocol (DHCP) server and DHCP relay agent.
    It includes an DHCP (and BOOTP) server daemon used to distribute network
    information, an Object Management API command shell for querying and
    modifying DHCP server attributes (while it is running), and an agent
    for relaying DHCP and BOOTP requests between subnets.
    
    This package contains the Base component.
    
    Imported from pkgsrc-wip.  Inital packaging by reed@

---
+++ net/isc-dhcp/files/site.h
@@ -0,0 +1,6 @@
+#define _PATH_DHCPD_CONF "@@SYSCONFDIR@@/dhcpd.conf"
+#define _PATH_DHCPD_PID "@@VARBASE@@/run/isc-dhcp/isc-dhcpd.pid"
+#define _PATH_DHCPD_DB "@@VARBASE@@/db/isc-dhcp/dhcpd.leases"
+#define _PATH_DHCLIENT_DB "@@VARBASE@@/db/isc-dhcp/dhclient.leases"
+#define _PATH_DHCLIENT_SCRIPT "@@PREFIX@@/sbin/dhclient-script"
+#define DHCPD_LOG_FACILITY LOG_LOCAL@@DHCP_SYSLOG_LEVEL@@
