74d46196b98e6df0aa65779a1fc11c2d5df35834
rillig <rillig@pkgsrc.org>
2007-12-16 21:45:30 +0000

    Fixed trailing and intermediate whitespace, according to the pkglint notes.

--- audio/cmus/options.mk
+++ audio/cmus/options.mk
@@ -1,34 +1,36 @@
+# $NetBSD: options.mk,v 1.2 2007/12/16 21:45:30 rillig Exp $
+
 PKG_OPTIONS_VAR=				PKG_OPTIONS.cmus
 PKG_SUPPORTED_OPTIONS=			flac mad vorbis arts ao mpcdec #faad alsa
 PKG_OPTIONS_OPTIONAL_GROUPS=	mod
 PKG_OPTIONS_GROUP.mod=			modplug mikmod
 PKG_SUGGESTED_OPTIONS=			flac mad ao vorbis modplug
-  
+
 .include "../../mk/bsd.prefs.mk"
-    
+
 .include "../../mk/bsd.options.mk"
-                                      
+
 # Package-specific option-handling
 
 # Backends:
 #
-  
-# AO support                      
-#                                  
-.if !empty(PKG_OPTIONS:Mao)  
+
+# AO support
+#
+.if !empty(PKG_OPTIONS:Mao)
 .  include "../../audio/libao/buildlink3.mk"
-CONFIGURE_ARGS+=  CONFIG_AO=y
+CONFIGURE_ARGS+=	CONFIG_AO=y
 .else
-CONFIGURE_ARGS+=  CONFIG_AO=n
-.endif                               
+CONFIGURE_ARGS+=	CONFIG_AO=n
+.endif
 
 # ARTS support
 #
 .if !empty(PKG_OPTIONS:Marts)
 .  include "../../audio/arts/buildlink3.mk"
-CONFIGURE_ARGS+=  CONFIG_ARTS=y
+CONFIGURE_ARGS+=	CONFIG_ARTS=y
 .else
-CONFIGURE_ARGS+=  CONFIG_ARTS=n
+CONFIGURE_ARGS+=	CONFIG_ARTS=n
 .endif
 
 # ALSA support
@@ -36,7 +38,7 @@ CONFIGURE_ARGS+=  CONFIG_ARTS=n
 # UNTESTED
 #.if !empty(PKG_OPTIONS:Malsa)
 #ONLY_FOR_PLATFORM=  Linux-*-* # Alsa is Linux only
-#CONFIGURE_ARGS+=  CONFIG_ALSA=y
+#CONFIGURE_ARGS+=	CONFIG_ALSA=y
 #.endif
 
 # Codecs:
@@ -45,37 +47,37 @@ CONFIGURE_ARGS+=  CONFIG_ARTS=n
 # MAD support
 #
 .if !empty(PKG_OPTIONS:Mmad)
-.include "../../audio/libmad/buildlink3.mk" 
-CONFIGURE_ARGS+=  CONFIG_MAD=y
+.include "../../audio/libmad/buildlink3.mk"
+CONFIGURE_ARGS+=	CONFIG_MAD=y
 .else
-CONFIGURE_ARGS+=  CONFIG_MAD=n
+CONFIGURE_ARGS+=	CONFIG_MAD=n
 .endif
 
 # VORBIS support
 #
 .if !empty(PKG_OPTIONS:Mvorbis)
-.include "../../audio/libvorbis/buildlink3.mk"                            
-CONFIGURE_ARGS+=  CONFIG_VORBIS=y
+.include "../../audio/libvorbis/buildlink3.mk"
+CONFIGURE_ARGS+=	CONFIG_VORBIS=y
 .else
-CONFIGURE_ARGS+=  CONFIG_VORBIS=n
+CONFIGURE_ARGS+=	CONFIG_VORBIS=n
 .endif
 
 # FLAC support
 #
 .if !empty(PKG_OPTIONS:Mflac)
-.include "../../audio/flac/buildlink3.mk"                            
-CONFIGURE_ARGS+=  CONFIG_FLAC=y
+.include "../../audio/flac/buildlink3.mk"
+CONFIGURE_ARGS+=	CONFIG_FLAC=y
 .else
-CONFIGURE_ARGS+=  CONFIG_FLAC=n
+CONFIGURE_ARGS+=	CONFIG_FLAC=n
 .endif
 
 # MPCDEC support
 #
 .if !empty(PKG_OPTIONS:Mmpcdec)
-.include "../../audio/libmpcdec/buildlink3.mk"                           
-CONFIGURE_ARGS+=  CONFIG_MPC=y
+.include "../../audio/libmpcdec/buildlink3.mk"
+CONFIGURE_ARGS+=	CONFIG_MPC=y
 .else
-CONFIGURE_ARGS+=  CONFIG_MPC=n
+CONFIGURE_ARGS+=	CONFIG_MPC=n
 .endif
 
 ###
@@ -83,13 +85,13 @@ CONFIGURE_ARGS+=  CONFIG_MPC=n
 ###
 .if !empty(PKG_OPTIONS:Mmikmod)
 .include "../../audio/libmikmod/buildlink3.mk"
-CONFIGURE_ARGS+=  CONFIG_MODPLUG=n
-CONFIGURE_ARGS+=  CONFIG_MIKMOD=y
+CONFIGURE_ARGS+=	CONFIG_MODPLUG=n
+CONFIGURE_ARGS+=	CONFIG_MIKMOD=y
 .endif
 .if !empty(PKG_OPTIONS:Mmodplug)
 .include "../../audio/libmodplug/buildlink3.mk"
-CONFIGURE_ARGS+=  CONFIG_MODPLUG=y
-CONFIGURE_ARGS+=  CONFIG_MIKMOD=n
+CONFIGURE_ARGS+=	CONFIG_MODPLUG=y
+CONFIGURE_ARGS+=	CONFIG_MIKMOD=n
 .endif
 
 
@@ -98,10 +100,10 @@ CONFIGURE_ARGS+=  CONFIG_MIKMOD=n
 # XXX: faad2 in pkgsrc is heavily outdated as of 2007Q3, cmus needs newer.
 #
 #.if !empty(PKG_OPTIONS:Mfaad)
-#.include "../../audio/faad2/buildlink3.mk" 
-#CONFIGURE_ARGS+=  CONFIG_AAC=y
-#CONFIGURE_ARGS+=  CONFIG_MP4=y
+#.include "../../audio/faad2/buildlink3.mk"
+#CONFIGURE_ARGS+=	CONFIG_AAC=y
+#CONFIGURE_ARGS+=	CONFIG_MP4=y
 #.else
-#CONFIGURE_ARGS+=  CONFIG_AAC=n
-#CONFIGURE_ARGS+=  CONFIG_MP4=n
+#CONFIGURE_ARGS+=	CONFIG_AAC=n
+#CONFIGURE_ARGS+=	CONFIG_MP4=n
 #.endif
