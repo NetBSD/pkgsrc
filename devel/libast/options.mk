6258ccb172da4828f4d7402d886ac45185f9ffd4
obache <obache@pkgsrc.org>
2007-12-18 14:25:21 +0000

    Add default empty PKG_SUPPORTED_OPTIONS definition. Should fix PR 37563.

--- devel/libast/options.mk
+++ devel/libast/options.mk
@@ -1,11 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2005/05/31 21:35:13 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2007/12/18 14:25:21 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libast
+PKG_SUPPORTED_OPTIONS=	# empty
 
 .include "../../mk/bsd.prefs.mk"
 
 .if ${MACHINE_ARCH} == "i386"
-PKG_SUPPORTED_OPTIONS=	mmx
+PKG_SUPPORTED_OPTIONS+=	mmx
 .endif
 
 .include "../../mk/bsd.options.mk"
