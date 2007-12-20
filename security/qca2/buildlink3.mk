cb7ddd3ae9c75802cdb2899d7b7d169db0f29473
jdolecek <jdolecek>
2007-12-20 20:17:47 +0000

    re-import security/qca version 2.0.0 as security/qca2 in preparation for having both qca 1.x and qca 2.x in tree

---
+++ security/qca2/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/12/20 20:17:47 jdolecek Exp $
+#
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+QCA_BUILDLINK3_MK:=	${QCA_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	qca
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nqca}
+BUILDLINK_PACKAGES+=	qca
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}qca
+
+.if !empty(QCA_BUILDLINK3_MK:M+)
+BUILDLINK_API_DEPENDS.qca+=	qca>=2.0.0
+BUILDLINK_ABI_DEPENDS.qca?=	qca>=2.0.0
+BUILDLINK_PKGSRCDIR.qca?=	../../security/qca
+.endif	# QCA_BUILDLINK3_MK
+
+.include "../../x11/qt4-libs/buildlink3.mk"
+.include "../../x11/qt4-tools/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
