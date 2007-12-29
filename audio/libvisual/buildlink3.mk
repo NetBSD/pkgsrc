5d7052d1158d5a54220fb4c65363ed6076a4b252
joerg <joerg@pkgsrc.org>
2007-12-29 16:11:38 +0000

    Recursively include gettext-lib for the sake of libtool.

--- audio/libvisual/buildlink3.mk
+++ audio/libvisual/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2006/07/08 23:10:37 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2007/12/29 16:11:38 joerg Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBVISUAL_BUILDLINK3_MK:=	${LIBVISUAL_BUILDLINK3_MK}+
@@ -15,5 +15,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libvisual
 BUILDLINK_API_DEPENDS.libvisual+=	libvisual>=0.4.0
 BUILDLINK_PKGSRCDIR.libvisual?=		../../audio/libvisual
 .endif	# LIBVISUAL_BUILDLINK3_MK
+
+.include "../../devel/gettext-lib/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
