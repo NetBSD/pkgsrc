f95c2d1255ea5fd01a242dcaf5a13126e38bd6dc
rillig <rillig@pkgsrc.org>
2008-01-03 17:08:52 +0000

    Fixed a pkglint warning, which revealed a naming conflict. Fixed that too.

--- net/libradius/buildlink3.mk
+++ net/libradius/buildlink3.mk
@@ -1,11 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.9 2006/12/12 21:52:37 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2008/01/03 17:08:52 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-LIBRADIUS_LINUX_BUILDLINK3_MK:=	${LIBRADIUS_LINUX_BUILDLINK3_MK}+
+LIBRADIUS_BUILDLINK3_MK:=	${LIBRADIUS_BUILDLINK3_MK}+
 
-.include "../../mk/bsd.fast.prefs.mk"
-
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	libradius
 .endif
 
@@ -13,15 +11,16 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibradius}
 BUILDLINK_PACKAGES+=	libradius
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libradius
 
-.if !empty(LIBRADIUS_LINUX_BUILDLINK3_MK:M+)
+.if ${LIBRADIUS_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.libradius+=	libradius>=20040827
 BUILDLINK_PKGSRCDIR.libradius?=	../../net/libradius
 BUILDLINK_DEPMETHOD.libradius?=	build
 
+.  include "../../mk/bsd.fast.prefs.mk"
 .  if ${OPSYS} == "FreeBSD"
 BUILDLINK_LDADD.libradius+=	-lmd
 BUILDLINK_LDFLAGS.libradius?=	${BUILDLINK_LDADD.libradius}
 .  endif
-.endif	# LIBRADIUS_LINUX_BUILDLINK3_MK
+.endif	# LIBRADIUS_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
