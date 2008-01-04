640fda11c58f10e423aded7820553168c6563f6f
rillig <rillig@pkgsrc.org>
2008-01-04 01:25:51 +0000

    Fixed pkglint warnings.

--- x11/neXtaw/buildlink3.mk
+++ x11/neXtaw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2007/12/24 18:57:49 bjs Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2008/01/04 01:25:51 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 NEXTAW_BUILDLINK3_MK:=	${NEXTAW_BUILDLINK3_MK}+
@@ -18,25 +18,24 @@ BUILDLINK_PKGSRCDIR.neXtaw?=	../../x11/neXtaw
 
 .include "../../mk/bsd.fast.prefs.mk"
 
-.if ${X11_TYPE} == "modular"
+.  if ${X11_TYPE} == "modular"
 .PHONY: buildlink-neXtaw-inc-hack
 buildlink-neXtaw-cookie: buildlink-neXtaw-inc-hack
- 
+
 buildlink-neXtaw-inc-hack: buildlink-directories
 	[ ! -h ${BUILDLINK_DIR}/include/X11/Xaw ] && \
 		${MKDIR} ${BUILDLINK_DIR}/include/X11 && \
 		${LN} -s neXtaw ${BUILDLINK_DIR}/include/X11/Xaw
-.endif
-.endif	# NEXTAW_BUILDLINK3_MK
-
-.include "../../mk/bsd.fast.prefs.mk"
-
-.include "../../x11/libXext/buildlink3.mk"
-.include "../../x11/libXmu/buildlink3.mk"
-.include "../../x11/libXt/buildlink3.mk"
+.  endif
 
 LIBXAW?=	-L${BUILDLINK_PREFIX.neXtaw}/lib			\
 		${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.neXtaw}/lib	\
 		-lneXtaw
+
+.endif	# NEXTAW_BUILDLINK3_MK
+
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXmu/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
