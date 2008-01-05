1a05c014569b8736f8d024f944312749911d6b61
rillig <rillig@pkgsrc.org>
2008-01-05 20:45:59 +0000

    Fixed naming mismatches reported by pkglint.

--- x11/py-gnome2/buildlink3.mk
+++ x11/py-gnome2/buildlink3.mk
@@ -1,7 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.20 2006/09/16 06:40:03 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2008/01/05 20:45:59 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-PY_GNOME2_BUILDLINK3_MK:=	${PY_GNOME2_BUILDLINK3_MK}+
+PYGNOME2_BUILDLINK3_MK:=	${PYGNOME2_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	pygnome2
@@ -11,13 +11,13 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npygnome2}
 BUILDLINK_PACKAGES+=	pygnome2
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}pygnome2
 
-.if !empty(PY_GNOME2_BUILDLINK3_MK:M+)
+.if !empty(PYGNOME2_BUILDLINK3_MK:M+)
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.pygnome2+=	${PYPKGPREFIX}-gnome2>=2.10.0
 BUILDLINK_ABI_DEPENDS.pygnome2+=	${PYPKGPREFIX}-gnome2>=2.12.4
 BUILDLINK_PKGSRCDIR.pygnome2?=	../../x11/py-gnome2
-.endif	# PY_GNOME2_BUILDLINK3_MK
+.endif	# PYGNOME2_BUILDLINK3_MK
 
 .include "../../net/py-ORBit/buildlink3.mk"
 .include "../../x11/py-gtk2/buildlink3.mk"
--- x11/py-gtk2/buildlink3.mk
+++ x11/py-gtk2/buildlink3.mk
@@ -1,7 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.21 2007/09/21 13:04:44 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2008/01/05 20:45:59 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
-PY_GTK2_BUILDLINK3_MK:=	${PY_GTK2_BUILDLINK3_MK}+
+PYGTK2_BUILDLINK3_MK:=	${PYGTK2_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	pygtk2
@@ -11,13 +11,13 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npygtk2}
 BUILDLINK_PACKAGES+=	pygtk2
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}pygtk2
 
-.if !empty(PY_GTK2_BUILDLINK3_MK:M+)
+.if !empty(PYGTK2_BUILDLINK3_MK:M+)
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.pygtk2+=	${PYPKGPREFIX}-gtk2>=2.8.4
 BUILDLINK_ABI_DEPENDS.pygtk2+=	${PYPKGPREFIX}-gtk2>=2.8.5nb1
 BUILDLINK_PKGSRCDIR.pygtk2?=	../../x11/py-gtk2
-.endif	# PY_GTK2_BUILDLINK3_MK
+.endif	# PYGTK2_BUILDLINK3_MK
 
 .include "../../devel/py-gobject/buildlink3.mk"
 .include "../../devel/libglade/buildlink3.mk"
