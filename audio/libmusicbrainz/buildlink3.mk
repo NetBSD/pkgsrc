6e2f027af5b092b8a2bb5eaef1f2e747ebe45d8b
rillig <rillig@pkgsrc.org>
2008-01-05 20:56:56 +0000

    Fixed category name in BUILDLINK_PKGSRCDIR.
    (hi wiz!)

--- audio/libmusicbrainz/buildlink3.mk
+++ audio/libmusicbrainz/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/06/08 07:02:15 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/01/05 20:56:56 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBMUSICBRAINZ_BUILDLINK3_MK:=	${LIBMUSICBRAINZ_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libmusicbrainz
 
 .if ${LIBMUSICBRAINZ_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.libmusicbrainz+=	libmusicbrainz>=3.0.0
-BUILDLINK_PKGSRCDIR.libmusicbrainz?=	../../zzz/libmusicbrainz
+BUILDLINK_PKGSRCDIR.libmusicbrainz?=	../../audio/libmusicbrainz
 .endif	# LIBMUSICBRAINZ_BUILDLINK3_MK
 
 .include "../../www/neon/buildlink3.mk"
