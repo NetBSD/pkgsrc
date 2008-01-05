c97d667ce1bf783d2dd920d5ce0b11e51e6c451d
rillig <rillig@pkgsrc.org>
2008-01-05 20:57:37 +0000

    Fixed name mismatch.

--- audio/py-ogg/buildlink3.mk
+++ audio/py-ogg/buildlink3.mk
@@ -1,7 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.14 2006/07/08 23:10:37 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2008/01/05 20:57:37 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
-PY_OGG_BUILDLINK3_MK:=	${PY_OGG_BUILDLINK3_MK}+
+PYOGG_BUILDLINK3_MK:=	${PYOGG_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	pyogg
@@ -11,12 +11,12 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npyogg}
 BUILDLINK_PACKAGES+=	pyogg
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}pyogg
 
-.if !empty(PY_OGG_BUILDLINK3_MK:M+)
+.if !empty(PYOGG_BUILDLINK3_MK:M+)
 .include "../../lang/python/pyversion.mk"
 BUILDLINK_API_DEPENDS.pyogg+=	${PYPKGPREFIX}-ogg>=1.3
 BUILDLINK_ABI_DEPENDS.pyogg?=	${PYPKGPREFIX}-ogg>=1.3nb2
 BUILDLINK_PKGSRCDIR.pyogg?=	../../audio/py-ogg
-.endif	# PY_OGG_BUILDLINK3_MK
+.endif	# PYOGG_BUILDLINK3_MK
 
 .include "../../multimedia/libogg/buildlink3.mk"
 
