aec761c402356ca6ce41c5869e3e14e17e627a33
rillig <rillig@pkgsrc.org>
2008-01-06 00:08:30 +0000

    pkgsrc doesn't have "xorg" anymore.

--- x11/xproto/buildlink3.mk
+++ x11/xproto/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2006/12/12 21:52:38 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2008/01/06 00:08:30 rillig Exp $
 #
 # This Makefile fragment is included by packages that use xproto.
 #
 
 .include "../../mk/bsd.fast.prefs.mk"
 
-.if ${X11_TYPE} == "xorg"
+.if ${X11_TYPE} != "modular"
 .include "../../mk/x11.buildlink3.mk"
 .else
 
