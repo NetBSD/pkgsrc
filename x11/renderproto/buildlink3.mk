79c300463f33cdb8980719bdcc14fb96b0f58d49
tron <tron@pkgsrc.org>
2008-01-07 13:59:35 +0000

    Unbreak build of a lot of X11 packages on systems which use native X11 (e.g. NetBSD with bundled XFree86). Checking whether "X11_TYPE" is "xorg" is obsolete and *not* equivalent to checking whether we are not using modular X.org.

--- x11/renderproto/buildlink3.mk
+++ x11/renderproto/buildlink3.mk
@@ -1,10 +1,6 @@
-# $NetBSD: buildlink3.mk,v 1.7 2008/01/06 16:57:43 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2008/01/07 13:59:36 tron Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
-
-.if ${X11_TYPE} != "modular"
-.include "../../mk/x11.buildlink3.mk"
-.else
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 RENDERPROTO_BUILDLINK3_MK:=	${RENDERPROTO_BUILDLINK3_MK}+
@@ -24,5 +20,3 @@ BUILDLINK_DEPMETHOD.renderproto?=	build
 .endif	# RENDERPROTO_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
-
-.endif
