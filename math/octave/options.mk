0abb1ee75c4e73f2ac825d453084557c9a495a72
adam <adam@pkgsrc.org>
2008-01-11 01:23:43 +0000

    Changes 3.0.0: * Compatibility with Matlab graphics is much better now.  We now   have some graphics features that work like Matlab's Handle   Graphics (tm): * The way Octave handles search paths has changed. * Previous versions of Octave had a number of built-in variables to   control warnings (for example, warn_divide_by_zero).  These   variables have been replaced by warning identifiers that are used   with the warning function to control the state of warnings. * All built-in variables have been converted to functions. * For compatibility with Matlab, the output order of Octave's   "system" function has changed. * For compatibility with Matlab, the output of Octave's fsolve   function has been changed. * For compatibility with Matlab, normcdf, norminv, normpdf, and   normrnd have been modified to compute distributions using the   standard deviation instead of the variance. * For compatibility with Matlab, gamcdf, gaminv, gampdf, gamrnd,   expcdf, expinv, exppdf and exprnd have been modified to compute   the distributions using the standard scale factor rather than   one over the scale factor.

--- math/octave/options.mk
+++ math/octave/options.mk
@@ -1,13 +1,31 @@
-# $NetBSD: options.mk,v 1.2 2005/05/31 10:01:38 dillo Exp $
+# $NetBSD: options.mk,v 1.3 2008/01/11 01:23:43 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.octave
-PKG_SUPPORTED_OPTIONS=	hdf5
-PKG_SUGGESTED_OPTIONS=	hdf5
+PKG_SUPPORTED_OPTIONS=	glpk hdf5 pcre qhull
+PKG_SUGGESTED_OPTIONS=	glpk hdf5 pcre qhull
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mglpk)
+.include "../../math/glpk/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-glpk
+.endif
 
 .if !empty(PKG_OPTIONS:Mhdf5)
 .include "../../devel/hdf5/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-hdf5
+.endif
+
+.if !empty(PKG_OPTIONS:Mpcre)
+.include "../../devel/pcre/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-pcre
+.endif
+
+.if !empty(PKG_OPTIONS:Mqhull)
+.include "../../math/qhull/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-qhull
 .endif
