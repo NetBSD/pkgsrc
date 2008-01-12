54a9e8b02ddc06323222b7d4f5cc4bd2d0333725
adam <adam>
2008-01-12 11:36:28 +0000

    db4 update related revision bump

--- devel/subversion-base/buildlink3.mk
+++ devel/subversion-base/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2007/06/08 12:25:03 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2008/01/12 11:36:29 adam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SUBVERSION_BASE_BUILDLINK3_MK:=	${SUBVERSION_BASE_BUILDLINK3_MK}+
@@ -16,7 +16,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}subversion-base
 
 .if !empty(SUBVERSION_BASE_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.subversion-base+=	subversion-base>=1.0.0
-BUILDLINK_ABI_DEPENDS.subversion-base?=	subversion-base>=1.4.3nb2
+BUILDLINK_ABI_DEPENDS.subversion-base?=	subversion-base>=1.4.6nb2
 BUILDLINK_PKGSRCDIR.subversion-base?=	../../devel/subversion-base
 
 BUILDLINK_FILES.subversion-base+=	bin/svn-config
