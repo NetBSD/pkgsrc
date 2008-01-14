d60f2641ce22238f7adcc17a77c9d2511c037ed4
wiz <wiz@pkgsrc.org>
2008-01-14 23:17:27 +0000

    Update to 0.1.6:
    0.1.6
    ------------------------------------------------------------------------
    - API Changed : Kexiv2 destructor is now virtual.
                    Krazy Code Checker fix (http://www.englishbreakfastnetwork.org/krazy)
    
    Bugs fixed from B.K.O (http://bugs.kde.org):
    001 ==> 137750: color mode2 nikon d70s not recognized as adobe rgb
    002 ==> 149267: digiKam crashes after finding gif,avi,txt and so on.
    003 ==> 148182: Iptc.Application2.Keywords appends always the 0-byte.

--- graphics/libkexiv2/buildlink3.mk
+++ graphics/libkexiv2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2007/11/30 21:55:03 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2008/01/14 23:17:27 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBKEXIV2_BUILDLINK3_MK:=	${LIBKEXIV2_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libkexiv2
 
 .if ${LIBKEXIV2_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.libkexiv2+=	libkexiv2>=0.1.1
-BUILDLINK_ABI_DEPENDS.libkexiv2+=	libkexiv2>=0.1.5nb2
+BUILDLINK_ABI_DEPENDS.libkexiv2+=	libkexiv2>=0.1.6
 BUILDLINK_PKGSRCDIR.libkexiv2?=	../../graphics/libkexiv2
 .endif	# LIBKEXIV2_BUILDLINK3_MK
 
