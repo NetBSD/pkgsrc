3b14a17953bfe8c7819f4fa84a4b099c0cc8cebd
rillig <rillig@pkgsrc.org>
2008-01-27 16:10:25 +0000

    Set PKG_SUPPORTED_OPTIONS to an empty list if we are not on i386. Fixes PR 37883.

--- x11/eterm/options.mk
+++ x11/eterm/options.mk
@@ -1,11 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2005/06/01 19:44:07 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2008/01/27 16:10:25 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.eterm
 
 .include "../../mk/bsd.prefs.mk"
 
+PKG_SUPPORTED_OPTIONS=	# none
 .if ${MACHINE_ARCH} == "i386"
-PKG_SUPPORTED_OPTIONS=	mmx
+PKG_SUPPORTED_OPTIONS+=	mmx
 .endif
 
 .include "../../mk/bsd.options.mk"
