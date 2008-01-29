c568f0c7449d65440f5e850e9a72a5690c08269c
gdt <gdt@pkgsrc.org>
2008-01-29 19:04:28 +0000

    Explain why options are defined but not used in options.mk (because it's included in many packages).

--- devel/subversion-base/options.mk
+++ devel/subversion-base/options.mk
@@ -1,7 +1,12 @@
-# $NetBSD: options.mk,v 1.2 2007/01/25 18:15:49 epg Exp $
+# $NetBSD: options.mk,v 1.3 2008/01/29 19:04:28 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.subversion
 PKG_SUPPORTED_OPTIONS=	apache22 apr1 serf
+
+# Note that this file is included as part of several packages.
+# Therefore this file only defines options, but does not include the
+# typical fragments to change behavior based on options, leaving those
+# to the individual Makefiles.
 
 .include "../../mk/bsd.options.mk"
 
