31b899b36812fda5e8184caec7117b1ec89f1dcc
rillig <rillig>
2008-01-31 13:08:12 +0000

    Fixed pkglint warnings.

--- x11/Xaw-Xpm/buildlink3.mk
+++ x11/Xaw-Xpm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2007/12/24 18:57:49 bjs Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2008/01/31 13:08:12 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 XAW_XPM_BUILDLINK3_MK:=	${XAW_XPM_BUILDLINK3_MK}+
@@ -18,23 +18,23 @@ BUILDLINK_PKGSRCDIR.Xaw-Xpm?=	../../x11/Xaw-Xpm
 
 .include "../../mk/bsd.fast.prefs.mk"
 
-.if ${X11_TYPE} == "modular"
-.PHONY: buildlink-Xaw_Xpm-inc-hack
-buildlink-Xaw-Xpm-cookie: buildlink-Xaw-Xpm-inc-hack
+.  if ${X11_TYPE} == "modular"
+buildlink-Xaw-Xpm-cookie: .PHONY buildlink-Xaw-Xpm-inc-hack
 
-buildlink-Xaw-Xpm-inc-hack: buildlink-directories
+buildlink-Xaw-Xpm-inc-hack: .PHONY buildlink-directories
 	[ ! -h ${BUILDLINK_DIR}/include/X11/Xaw ] && \
 		${MKDIR} ${BUILDLINK_DIR}/include/X11 && \
 		${LN} -s Xaw3d ${BUILDLINK_DIR}/include/X11/Xaw
-.endif
-.endif	# XAW_XPM_BUILDLINK3_MK
-
-.include "../../x11/libXpm/buildlink3.mk"
+.  endif
 
 LIBXAW?=	-L${BUILDLINK_PREFIX.Xaw-Xpm}/lib			\
 		${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.Xaw-Xpm}/lib	\
 		-L${BUILDLINK_PREFIX.libXpm}/lib			\
 		${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.libXpm}/lib	\
 		-lXaw3d -lXpm
+
+.endif	# XAW_XPM_BUILDLINK3_MK
+
+.include "../../x11/libXpm/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
