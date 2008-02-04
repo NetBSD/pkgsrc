926ef98693b85e47cb108283164683e64036a19e
ghen <ghen@pkgsrc.org>
2008-02-04 15:29:28 +0000

    Add an option for LDAP authentication as explained on http://www.postgresql.org/docs/8.3/static/auth-methods.html#AUTH-LDAP

--- databases/postgresql82/options.mk
+++ databases/postgresql82/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2006/12/09 15:34:13 adam Exp $
+# $NetBSD: options.mk,v 1.2 2008/02/04 15:29:28 ghen Exp $
 
 PKG_SUPPORTED_OPTIONS+=		# empty
 
@@ -10,4 +10,12 @@ PKG_SUPPORTED_OPTIONS+=		# empty
 .if !empty(PKG_OPTIONS:Mpam)
 .  include "../../mk/pam.buildlink3.mk"
 CONFIGURE_ARGS+=	--with-pam
+.endif
+
+###
+### LDAP authentication for the PostgreSQL backend.
+###
+.if !empty(PKG_OPTIONS:Mldap)
+.  include "../../databases/openldap-client/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-ldap
 .endif
