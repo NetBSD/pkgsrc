a3f348d091823d1172a88d74e630177f5528c168
tnn <tnn>
2008-02-05 22:03:57 +0000

    revert a local change that wasn't supposed to be committed.

--- sysutils/dbus/hacks.mk
+++ sysutils/dbus/hacks.mk
@@ -1,5 +1,4 @@
-# $NetBSD: hacks.mk,v 1.2 2008/02/05 22:03:02 tnn Exp $
+# $NetBSD: hacks.mk,v 1.3 2008/02/05 22:03:57 tnn Exp $
 
 # gcc 2.95 does not understand -Wfloat-equal
 BUILDLINK_TRANSFORM+=	rm:-Wfloat-equal
-#XXX_DISABLED=yes
