e08e39667b3795905e2c06ac83280a17a2fc4739
joerg <joerg@pkgsrc.org>
2008-02-07 23:40:52 +0000

    pkg_install-20080208: Explicitly check in show_var that the buffer is non-NULL. This can stops pkg_info -Q foo from segfaulting for explicitly installed packages. Reported by Stoned Elipot.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.90 2008/02/04 14:03:10 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.91 2008/02/07 23:40:52 joerg Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20080204"
+#define PKGTOOLS_VERSION "20080208"
 
 #endif /* _INST_LIB_VERSION_H_ */
