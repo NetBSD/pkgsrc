3612e9b35a7de39d3a1632ae54a61b202cc55141
joerg <joerg@pkgsrc.org>
2008-02-07 16:24:01 +0000

    EAUTH and ENEEDAUTH might not exist, so use them conditionally. To flag HTTP authentication errors, fallback to EPERM.

--- net/libfetch/files/http.c
+++ net/libfetch/files/http.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: http.c,v 1.2 2008/02/07 16:14:44 joerg Exp $	*/
+/*	$NetBSD: http.c,v 1.3 2008/02/07 16:24:01 joerg Exp $	*/
 /*-
  * Copyright (c) 2000-2004 Dag-Erling Coïdan Smørgrav
  * All rights reserved.
@@ -712,7 +712,11 @@ http_connect(struct url *URL, struct url *purl, const char *flags)
 	    fetch_ssl(conn, verbose) == -1) {
 		fetch_close(conn);
 		/* grrr */
+#ifdef EAUTH
 		errno = EAUTH;
+#else
+		errno = EPERM;
+#endif
 		fetch_syserr();
 		return (NULL);
 	}
