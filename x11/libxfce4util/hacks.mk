849d228b925b8e6729b6252bc8b0a6110c7ca174
tnn <tnn@pkgsrc.org>
2008-02-08 10:25:26 +0000

    Fix PKGSRC_COMPILER check so that it works with compiler chaining.

--- x11/libxfce4util/hacks.mk
+++ x11/libxfce4util/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2006/02/18 20:43:00 schwarz Exp $
+# $NetBSD: hacks.mk,v 1.2 2008/02/08 10:25:26 tnn Exp $
 
 .if !defined(LIBXFCE4UTIL_HACKS_MK)
 LIBXFCE4UTIL_HACKS_MK=	# defined
@@ -6,7 +6,7 @@ LIBXFCE4UTIL_HACKS_MK=	# defined
 ### [Sat Feb  4 09:07:25 CET 2006: schwarz]
 ### SGI ido compiler does not handle (...) in defines
 ###
-.if ${PKGSRC_COMPILER} == "ido"
+.if !empty(PKGSRC_COMPILER:Mido)
 PKG_HACKS+=		no-dots-in-defines
 SUBST_CLASSES+=		dots
 SUBST_STAGE.dots=	post-patch
