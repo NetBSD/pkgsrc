6259f524f0ec8fe745b864e4baa57679be5879cc
tnn <tnn@pkgsrc.org>
2008-02-10 11:44:48 +0000

    Enable check-fakehome.mk.

--- mk/check/bsd.check.mk
+++ mk/check/bsd.check.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.check.mk,v 1.6 2007/10/13 11:04:17 dsl Exp $
+# $NetBSD: bsd.check.mk,v 1.7 2008/02/10 11:44:48 tnn Exp $
 #
 # This Makefile fragment is included by bsd.pkg.mk and provides all
 # variables and targets related to build and install checks.
@@ -33,6 +33,7 @@
 .include "check-shlibs.mk"
 .include "check-vulnerable.mk"
 .include "check-wrkref.mk"
+.include "check-fakehome.mk"
 
 pre-configure-checks-hook \
 pre-build-checks-hook: .PHONY
