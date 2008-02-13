d1542015dd5ffd7c0955ea087f42f11a31f6ce4c
adrianp <adrianp@pkgsrc.org>
2008-02-13 22:03:58 +0000

    ISC's Dynamic Host Configuration Protocol Distribution provides a freely redistributable reference implementation of all aspects of DHCP, through a suite of DHCP tools:
    * A DHCP server
    * A DHCP client
    * A DHCP relay agent
    
    These tools all use a modular API which is designed to be sufficiently
    general that it can easily be made to work on POSIX-compliant
    operating systems and also non-POSIX systems like Windows NT and
    MacOS.
    
    The DHCP server, client and relay agent are provided both as
    reference implementations of the protocol and as working, fully-featured
    sample implementations. Both the client and the server provide
    functionality that, while not strictly required by the protocol,
    is very useful in practice. The DHCP server also makes allowances
    for non-compliant clients which one might still like to support.
    
    This package contains the Base component.

---
+++ net/isc-dhcp4/files/isc_dhcpd.sh
@@ -0,0 +1,36 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: isc_dhcpd.sh,v 1.1.1.1 2008/02/13 22:03:58 adrianp Exp $
+#
+
+# PROVIDE: dhcpd
+# REQUIRE: DAEMON
+# BEFORE:  LOGIN
+
+if [ -f /etc/rc.subr ]; then
+        . /etc/rc.subr
+fi
+
+name="dhcpd"
+rcvar="isc_${name}"
+command="@PREFIX@/sbin/${name}"
+pidfile="@VARBASE@/run/isc-dhcp/isc-${name}.pid"
+required_files="@PKG_SYSCONFDIR@/${name}.conf"
+start_precmd="isc_dhcpd_precmd"
+
+isc_dhcpd_precmd()
+{
+	if [ ! -d @VARBASE@/run/isc-dhcp ]; then
+		@MKDIR@ @VARBASE@/run/isc-dhcp
+		@CHMOD@ 0770 @VARBASE@/run/isc-dhcp
+	fi
+
+	if [ ! -f @DHCP_HOME@/dhcpd.leases ]; then
+		@MKDIR@ @DHCP_HOME@
+		@TOUCH@ @DHCP_HOME@/dhcpd.leases
+		@CHMOD@ 0640 @DHCP_HOME@/dhcpd.leases
+	fi
+}
+
+load_rc_config $rcvar
+run_rc_command "$1"
