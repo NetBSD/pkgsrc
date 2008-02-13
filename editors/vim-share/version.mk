1b4d03212f42e64d796136e7d628ba3baefb35d8
martti <martti@pkgsrc.org>
2008-02-13 12:46:26 +0000

    Updated to 7.1.248 as it was release just after my 7.1.247 upgrade.
      3691  7.1.248  can't set the '" mark; can't know if setpos() was successful

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.46 2008/02/13 12:06:30 martti Exp $
+# $NetBSD: version.mk,v 1.47 2008/02/13 12:46:26 martti Exp $
 
 VIM_VERSION=		7.1
-VIM_PATCHLEVEL=		247
+VIM_PATCHLEVEL=		248
 VIM_SUBDIR=		vim71
