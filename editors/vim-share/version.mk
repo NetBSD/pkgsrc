0307941b2fb438a7019663d8fc12b28adf255328
martti <martti@pkgsrc.org>
2008-02-15 06:36:10 +0000

    Updated editors/vim-share to 7.1.252
    * Set "Modified by ${MAINTAINER}" to meet the license requirements
    * Changes since 7.1.248
    
      2144  7.1.249  after "U" the cursor can be past end of line
      1684  7.1.250  error for ":setglobal fenc=anything" when 'modifiable' is off
      3134  7.1.251  accessing freed memory when spell checking enabled
      1399  7.1.252  (after 7.1.243) test 39 fails when locale uses utf-8 encoding

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.47 2008/02/13 12:46:26 martti Exp $
+# $NetBSD: version.mk,v 1.48 2008/02/15 06:36:11 martti Exp $
 
 VIM_VERSION=		7.1
-VIM_PATCHLEVEL=		248
+VIM_PATCHLEVEL=		252
 VIM_SUBDIR=		vim71
