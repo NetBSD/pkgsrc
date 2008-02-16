f06db0dd6ef2bdbfbd1fe0372370232518134236
obache <obache@pkgsrc.org>
2008-02-16 15:57:06 +0000

    DB-1.85 compatability was enabled long long ago. At least, all db4>=4.6.21 have it.
    Add same trick as db2/db3.
    Fixed link problem that expect db-1.85 compat library is -ldb, like a PR 37647.

--- databases/db4/buildlink3.mk
+++ databases/db4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.32 2008/01/10 21:55:52 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.33 2008/02/16 15:57:06 obache Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 DB4_BUILDLINK3_MK:=	${DB4_BUILDLINK3_MK}+
@@ -17,6 +17,12 @@ BUILDLINK_PKGSRCDIR.db4?=	../../databases/db4
 BUILDLINK_INCDIRS.db4?=		include/db4
 BUILDLINK_LDADD.db4=		-ldb4
 BUILDLINK_TRANSFORM+=		l:db-4:db4
+
+.  include "../../mk/bsd.fast.prefs.mk"
+.  if defined(USE_DB185) && !empty(USE_DB185:M[yY][eE][sS])
+BUILDLINK_LIBS.db4=		${BUILDLINK_LDADD.db4}
+BUILDLINK_TRANSFORM+=		l:db:db4
+.  endif
 
 .  include "../../mk/compiler.mk"
 .  if empty(PKGSRC_COMPILER:Mgcc)
