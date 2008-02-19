6e8021ba1a9975758ce0ead5807a1fc4468cdc17
taca <taca>
2008-02-19 14:12:46 +0000

    Update plone3 package to 3.0.6.
    Change log
    
    * Various tests were enhanced to increase our test coverage
    * Implement unlocking for content which does not use portal_factory
      and for LinguaPlone translations.
    * Add a method to cleanup persistent schemas from content objects
      which were created by the 'update schema' feature from older
      Archetypes releases. This is available through the ZMI.
    * Correct removing of all roles from a group. This fixes This fixes 6994.
    * Correct generation of session cookies for long userids. This fixes
      problems with OpenID2 accounts.
    * Correct handling of unicode arguments for
      acl_users.enumerateUsers. This fixes zope-pas bug 189627.
    * Kupu updates:
        o Correct full screen mode. This fixes 7473.
        o Correct intenal link insertion for IE. This fixes 7494.
        o Correct stripping out of anchor to top of current page. This fixes 7680.
        o The 'Home' link nows goes to the content root instead of the
          Plone root. This fixes 7713.
        o 'Link using UIDs' broke indexing of richt text fields with
          non-ASCII characters. This fixes 7728.
        o Update the flags and languages list. This fixes 7441.
        o  Revert internal change in language selector code in the
           plone.app.i18n release from Plone 3.0.5 in the language
           selector widget which broke LinguaPlone.
        o Fix lock timeout which was set by default to 12 minutes, it is
          now set to maxtimeout (71582788 minutes). This fixes 7358.
        o Fix TypeError when an anonymous user locks content. This fixes 7246.
    
    Updated packages
    
        * archetypes.kss 1.2.6
        * plone.app.i18n 1.0.3
        * plone.app.controlpanel 1.0.4
        * plone.app.linkintegrity 1.0.5
        * plone.app.vocabularies 1.0.3
        * plone.locking 1.0.5
        * plone.session 1.2
        * Archetypes 1.5.6
        * CMF 2.1.1
        * CMFPlone 3.0.6
        * PloneLanguageTool 2.0.2
        * PlonePAS 3.2
        * PloneTranslations 3.0.11
        * PluggableAuthService 1.5.3
        * kupu 1.4.8

--- www/plone/ploneversion.mk
+++ www/plone/ploneversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: ploneversion.mk,v 1.2 2008/01/09 22:12:23 taca Exp $
+# $NetBSD: ploneversion.mk,v 1.3 2008/02/19 14:12:46 taca Exp $
 #
 
 .if !defined(_PLONEVERSION_MK)
@@ -41,7 +41,7 @@ PLONE_VERSION_ACCEPTED?=	${PLONE_VERSION_SUPPORTED}
 
 # current Plone version
 PLONE25_VERSION=		2.5.5
-PLONE3_VERSION=			3.0.5
+PLONE3_VERSION=			3.0.6
 
 BUILDLINK_API_DEPENDS.plone25?=	plone25>=${PLONE25_VERSION}
 BUILDLINK_API_DEPENDS.plone3?=	plone30>=${PLONE3_VERSION}
