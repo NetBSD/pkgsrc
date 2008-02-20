259be48a7ff561cdaa020180de08b6edb98b20a5
xtraeme <xtraeme>
2008-02-20 11:28:19 +0000

    Downgrade to 1.8.5, suggested by drochner@.

--- x11/gtksourceview/buildlink3.mk
+++ x11/gtksourceview/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.14 2008/01/18 05:09:56 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2008/02/20 11:28:19 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GTKSOURCEVIEW_BUILDLINK3_MK:=	${GTKSOURCEVIEW_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	gtksourceview
 .endif
 
@@ -11,14 +11,14 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngtksourceview}
 BUILDLINK_PACKAGES+=	gtksourceview
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gtksourceview
 
-.if !empty(GTKSOURCEVIEW_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.gtksourceview+=	gtksourceview>=1.2.0
-BUILDLINK_ABI_DEPENDS.gtksourceview?=	gtksourceview>=1.8.5nb2
+.if ${GTKSOURCEVIEW_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.gtksourceview+=	gtksourceview>=1.8.5
 BUILDLINK_PKGSRCDIR.gtksourceview?=	../../x11/gtksourceview
 .endif	# GTKSOURCEVIEW_BUILDLINK3_MK
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
 .include "../../print/libgnomeprint/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
