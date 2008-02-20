c795f707e82836cf49ffd928f855b94f528f60f3
xtraeme <xtraeme@pkgsrc.org>
2008-02-20 09:56:44 +0000

    Update to 1.7:
    Frames are cached when reading the first frame after a seek. This allows
    faster reverse playback.

--- multimedia/libmpeg3/buildlink3.mk
+++ multimedia/libmpeg3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/07/08 23:11:02 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2008/02/20 09:56:44 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBMPEG3_BUILDLINK3_MK:=	${LIBMPEG3_BUILDLINK3_MK}+
@@ -14,7 +14,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libmpeg3
 .if !empty(LIBMPEG3_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.libmpeg3+=	libmpeg3>=1.3nb4
 BUILDLINK_ABI_DEPENDS.libmpeg3?=	libmpeg3>=1.3nb6
-BUILDLINK_PKGSRCDIR.libmpeg3?=	../../multimedia/libmpeg3
+BUILDLINK_PKGSRCDIR.libmpeg3?=		../../multimedia/libmpeg3
 .endif	# LIBMPEG3_BUILDLINK3_MK
 
 .include "../../mk/pthread.buildlink3.mk"
