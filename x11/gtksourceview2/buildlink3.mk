101a84ecbaadc0606b60f88d93685a2f36b5449f
xtraeme <xtraeme@pkgsrc.org>
2008-02-20 11:21:54 +0000

    Import gtksourceview 2.0.2, API incompatible with 1.x to avoid incompatibilities in packages, noted by drochner@.

--- x11/gtksourceview2/buildlink3.mk
+++ x11/gtksourceview2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/11/03 17:24:41 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.1.1.2 2008/02/20 11:21:54 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GTKSOURCEVIEW2_BUILDLINK3_MK:=	${GTKSOURCEVIEW2_BUILDLINK3_MK}+
@@ -12,13 +12,14 @@ BUILDLINK_PACKAGES+=	gtksourceview2
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gtksourceview2
 
 .if ${GTKSOURCEVIEW2_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.gtksourceview2+=	gtksourceview2>=2.0.0
-BUILDLINK_PKGSRCDIR.gtksourceview2?=	../../x11/gtksourceview2
+BUILDLINK_API_DEPENDS.gtksourceview2+=	gtksourceview2>=2.0.2
+BUILDLINK_PKGSRCDIR.gtksourceview2?=	../../x11/gtksourceview
 .endif	# GTKSOURCEVIEW2_BUILDLINK3_MK
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/pango/buildlink3.mk"
+.include "../../print/libgnomeprint/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 
