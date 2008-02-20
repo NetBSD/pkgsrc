83547923409f21c5f72c8537f746eba2e13b0b0a
markd <markd@pkgsrc.org>
2008-02-20 09:43:11 +0000

    update required ABI version to 1.5.8nb1 to ensure correct package metadata available for dependent packages to fix PR 37931

--- audio/arts/buildlink3.mk
+++ audio/arts/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2008/01/21 00:43:49 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2008/02/20 09:43:11 markd Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 ARTS_BUILDLINK3_MK:=	${ARTS_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}arts
 
 .if !empty(ARTS_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.arts+=	arts>=1.1.4nb1
-BUILDLINK_ABI_DEPENDS.arts+=	arts>=1.5.1nb1
+BUILDLINK_ABI_DEPENDS.arts+=	arts>=1.5.8nb1
 BUILDLINK_PKGSRCDIR.arts?=	../../audio/arts
 
 PRINT_PLIST_AWK+=	/^@dirrm include\/arts$$/ \
