1e67be4a803f84a684c59087f01900dd5e3670ab
jlam <jlam@pkgsrc.org>
2008-02-20 22:31:06 +0000

    Update mail/cone to version 0.74.  Changes from version 0.73 include:
    + Add full DESTDIR support.
    
    + New "gnutls" option to select between using GNU TLS and OpenSSL.
      Default to "ssl".
    
    + New "wide-curses" option to select between curses and wide-curses
      displays.  Default to "curses".
    
    * Add/fix GNU TLS support in cone.

--- mail/cone/options.mk
+++ mail/cone/options.mk
@@ -1,8 +1,11 @@
-# $NetBSD: options.mk,v 1.4 2008/02/20 13:18:24 wiz Exp $
+# $NetBSD: options.mk,v 1.5 2008/02/20 22:31:06 jlam Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.cone
 PKG_SUPPORTED_OPTIONS=		aspell ldap
-PKG_SUGGESTED_OPTIONS=		aspell ldap
+PKG_OPTIONS_REQUIRED_GROUPS=	display tls
+PKG_OPTIONS_GROUP.display=	curses wide-curses
+PKG_OPTIONS_GROUP.tls=		gnutls ssl
+PKG_SUGGESTED_OPTIONS=		aspell curses ldap ssl
 
 .include "../../mk/bsd.options.mk"
 
@@ -17,10 +20,11 @@ CONFIGURE_ARGS+=	--with-spellcheck=aspell
 .endif
 
 ###
-### LDAP addressbook support
+### Curses display
 ###
-.if !empty(PKG_OPTIONS:Mldap)
-.  include "../../databases/openldap-client/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mcurses)
+USE_NCURSES=	yes	# termattrs()
+.  include "../../devel/ncurses/buildlink3.mk"
 .endif
 
 ###
@@ -28,4 +32,34 @@ CONFIGURE_ARGS+=	--with-spellcheck=aspell
 ###
 .if !empty(PKG_OPTIONS:Mfam)
 .  include "../../mk/fam.buildlink3.mk"
+.endif
+
+###
+### GNU TLS support
+###
+.if !empty(PKG_OPTIONS:Mgnutls)
+.  include "../../security/gnutls/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-gnutls
+.endif
+
+###
+### LDAP addressbook support
+###
+.if !empty(PKG_OPTIONS:Mldap)
+.  include "../../databases/openldap-client/buildlink3.mk"
+.endif
+
+###
+### OpenSSL TLS support
+###
+.if !empty(PKG_OPTIONS:Mssl)
+.  include "../../security/openssl/buildlink3.mk"
+.endif
+
+###
+### Wide curses display
+###
+.if !empty(PKG_OPTIONS:Mwide-curses)
+USE_NCURSESW=	yes	# termattrs()
+.  include "../../devel/ncursesw/buildlink3.mk"
 .endif
