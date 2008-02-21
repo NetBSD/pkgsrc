5316fb734796d24692013bef3772859844849c71
jlam <jlam@pkgsrc.org>
2008-02-21 21:17:45 +0000

    + Rename the "ncursesw" option to "wide-curses".  This should be supported   under NetBSD-current without actually needing ncursesw.
    + Only make "wide-curses" an available option on platforms that have the
      necessary wide-character support.  It turns out NetBSD-3.x is not one
      of them.
    
    + Use a less patchy way to turn -ldb into -ldb3 by letting the wrappers
      do it.
    
    + Add full DESTDIR support.
    
    Bump the PKGREVISION to 6.

--- editors/nvi/options.mk
+++ editors/nvi/options.mk
@@ -1,14 +1,33 @@
-# $NetBSD: options.mk,v 1.1 2007/09/05 23:05:13 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2008/02/21 21:17:45 jlam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nvi
-PKG_SUPPORTED_OPTIONS=	ncursesw
+PKG_SUPPORTED_OPTIONS=	# empty
+
+.include "../../mk/bsd.prefs.mk"
+
+NVI_NON_WIDE_PLATFORMS=	NetBSD-[0-3].*-*	# missing swprintf()
+.for _pattern_ in ${NVI_NON_WIDE_PLATFORMS}
+.  if !empty(MACHINE_PLATFORM:M${_pattern_})
+NVI_NON_WIDE_PLATFORM=	yes
+.  endif
+.endfor
+
+# Only support the ``wide-curses'' option on platforms that have the
+# necessary wide character support.
+#
+.if !defined(NVI_NON_WIDE_PLATFORM)
+PKG_SUPPORTED_OPTIONS+=	wide-curses
+PKG_LEGACY_OPTS+=	ncursesw:wide-curses
+.endif
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mncursesw)
+###
+### Wide curses support; otherwise, default to using narrow curses.
+###
+.if !empty(PKG_OPTIONS:Mwide-curses)
 .  include "../../devel/ncursesw/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-widechar
-#CONFIGURE_ARGS+=	--with-curses=${BUILDLINK_PREFIX.ncursesw}
 .else
 .  include "../../mk/curses.buildlink3.mk"
 .endif
