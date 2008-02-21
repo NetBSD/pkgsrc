cc127f27e1375f1defd7aa0877254bd7c5e24a25
jlam <jlam>
2008-02-21 22:47:28 +0000

    + Rename the "ncursesw" option to "wide-curses" and get rid of the   "ncurses" option.  "wide-curses" now just toggles whether we use   wide or narrow curses, which is a much simpler knob for users.
    Bump the PKGREVISION to 5.

--- www/snownews/options.mk
+++ www/snownews/options.mk
@@ -1,17 +1,19 @@
-# $NetBSD: options.mk,v 1.1 2007/09/13 23:24:04 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2008/02/21 22:47:28 jlam Exp $
 
-PKG_OPTIONS_VAR=		PKG_OPTIONS.snownews
-PKG_OPTIONS_REQUIRED_GROUPS=	display
-PKG_OPTIONS_GROUP.display=	ncurses ncursesw
-PKG_SUGGESTED_OPTIONS=		ncurses
+PKG_OPTIONS_VAR=	PKG_OPTIONS.snownews
+PKG_SUPPORTED_OPTIONS=	wide-curses
+PKG_LEGACY_OPTS+=	ncursesw:wide-curses
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mncurses)
-.  include "../../devel/ncurses/buildlink3.mk"
-.endif
+###
+### Wide curses support; otherwise, default to using narrow curses.
+###
+USE_NCURSES=		yes	# WA_BOLD, WA_REVERSE, KEY_RESIZE
 
-.if !empty(PKG_OPTIONS:Mncursesw)
+.if !empty(PKG_OPTIONS:Mwide-curses)
 .  include "../../devel/ncursesw/buildlink3.mk"
 CONFIGURE_ARGS+=	--charset=UTF-8
+.else
+.  include "../../devel/ncurses/buildlink3.mk"
 .endif
