5a0e7e27ce1d80538cd72b053055215269c54274
jlam <jlam@pkgsrc.org>
2008-02-21 21:40:44 +0000

    + Rename the "ncursesw" option to "wide-curses".  This should be supported   under NetBSD-current without actually needing ncursesw.
    + It's not necessary to use ncurses for narrow curses support.  The native
      BSD curses on NetBSD is enough.
    
    + Add full DESTDIR support.
    
    Bump the PKGREVISION to 1.

--- editors/vile/options.mk
+++ editors/vile/options.mk
@@ -1,13 +1,17 @@
-# $NetBSD: options.mk,v 1.1 2007/10/14 04:53:35 obache Exp $
+# $NetBSD: options.mk,v 1.2 2008/02/21 21:40:44 jlam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.vile
-PKG_SUPPORTED_OPTIONS=	ncursesw
+PKG_SUPPORTED_OPTIONS=	wide-curses
+PKG_LEGACY_OPTS+=	ncursesw:wide-curses
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mncursesw)
+###
+### Wide curses support; otherwise, default to using narrow curses.
+###
+.if !empty(PKG_OPTIONS:Mwide-curses)
 .  include "../../devel/ncursesw/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-screen=ncursesw
 .else
-.  include "../../devel/ncurses/buildlink3.mk"
+.  include "../../mk/curses.buildlink3.mk"
 .endif
