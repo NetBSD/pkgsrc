f510888d3a36c2226835148bbcb5a91c355c24e4
jlam <jlam@pkgsrc.org>
2008-02-22 17:04:34 +0000

    + Rename the "ncursesw" option to "wide-curses" and get rid of the   "ncurses" option.  "wide-curses" now just toggles whether we use   wide or narrow curses, which is a much simpler knob for users.
    Bump the PKGREVISION to 2.

--- textproc/hunspell/options.mk
+++ textproc/hunspell/options.mk
@@ -1,17 +1,16 @@
-# $NetBSD: options.mk,v 1.1.1.1 2007/09/11 17:40:29 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2008/02/22 17:04:34 jlam Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.hunspell
-PKG_OPTIONS_REQUIRED_GROUPS=	display
-PKG_OPTIONS_GROUP.display=	ncurses ncursesw
-PKG_SUGGESTED_OPTIONS=		ncurses
+PKG_SUPPORTED_OPTIONS=		wide-curses
+PKG_SUGGESTED_OPTIONS=		# empty
+PKG_LEGACY_OPTS+=		ncursesw:wide-curses
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mncurses)
-USE_NCURSES=		yes
-.  include "../../devel/ncurses/buildlink3.mk"
-.endif
+USE_NCURSES=	yes
 
-.if !empty(PKG_OPTIONS:Mncursesw)
+.if !empty(PKG_OPTIONS:Mwide-curses)
 .  include "../../devel/ncursesw/buildlink3.mk"
+.else
+.  include "../../devel/ncurses/buildlink3.mk"
 .endif
