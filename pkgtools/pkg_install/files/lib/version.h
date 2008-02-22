e118896b9818e7eeeaa6c58df0bc73e2e4534cd5
joerg <joerg>
2008-02-22 15:53:52 +0000

    pkg_install-20080222: Free libarchive's side of the package before closing the file descriptor. This stops leaking up to 1MB / package when using bzip2 and addresses PR 38082. Check that at least +COMMENTS, +CONTENTS and +DESC can be extracted, otherwise skip the entry. This stops pkg_info -X from dumping core on non-package files.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.92 2008/02/19 15:16:24 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.93 2008/02/22 15:53:52 joerg Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20080219"
+#define PKGTOOLS_VERSION "20080222"
 
 #endif /* _INST_LIB_VERSION_H_ */
