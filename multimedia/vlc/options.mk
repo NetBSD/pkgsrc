eadf8d083e81ab8aa7a2dbfd84e215809039f5d1
sborrill <sborrill@pkgsrc.org>
2008-02-22 15:37:28 +0000

    Make faad and flac options (enabled by default)

--- multimedia/vlc/options.mk
+++ multimedia/vlc/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2007/11/03 17:40:29 sborrill Exp $
+# $NetBSD: options.mk,v 1.3 2008/02/22 15:37:28 sborrill Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.vlc
-PKG_SUPPORTED_OPTIONS=		skins wxwidgets
-PKG_SUGGESTED_OPTIONS=		wxwidgets
+PKG_SUPPORTED_OPTIONS=		skins wxwidgets faad flac
+PKG_SUGGESTED_OPTIONS=		wxwidgets faad flac
 
 .include "../../mk/bsd.options.mk"
 
@@ -21,4 +21,18 @@ PLIST_SUBST+=		COND_WXWIDGETS=
 .else
 CONFIGURE_ARGS+=	--disable-wxwidgets
 PLIST_SUBST+=		COND_WXWIDGETS="@comment "
+.endif
+
+.if !empty(PKG_OPTIONS:Mfaad)
+CONFIGURE_ARGS+=	--enable-faad
+.include "../../audio/faad2/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-faad
+.endif
+
+.if !empty(PKG_OPTIONS:Mflac)
+CONFIGURE_ARGS+=	--enable-flac
+.include "../../audio/flac/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-flac
 .endif
