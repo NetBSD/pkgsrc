50f2434a6652883196a78642ca374cf407b6c981
joerg <joerg@pkgsrc.org>
2008-02-22 21:58:16 +0000

    pkg_install-20080223: Redo the check for incomplete packages by annotating which meta data field are required and count which were found during reading the binary package. The original approach didn't work as loads from pkgdb are reduced to the minimal set.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.93 2008/02/22 15:53:52 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.94 2008/02/22 21:58:16 joerg Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -33,6 +33,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20080222"
+#define PKGTOOLS_VERSION "20080223"
 
 #endif /* _INST_LIB_VERSION_H_ */
