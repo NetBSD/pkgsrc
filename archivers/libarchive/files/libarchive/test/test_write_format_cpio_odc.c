492930b6e1fdd0c53cc0577e853e148b4bfad737
joerg <joerg>
2008-02-26 13:09:20 +0000

    Import libarchive-2.4.13. This brings a number of bugfixes, support for more ZIP archives and a more complete cpio frontend.

--- archivers/libarchive/files/libarchive/test/test_write_format_cpio_odc.c
+++ archivers/libarchive/files/libarchive/test/test_write_format_cpio_odc.c
@@ -23,7 +23,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "test.h"
-__FBSDID("$FreeBSD$");
+__FBSDID("$FreeBSD: src/lib/libarchive/test/test_write_format_cpio_odc.c,v 1.1 2008/01/01 22:28:04 kientzle Exp $");
 
 
 static int
