31c098b469230d56eb38cefd32b98f67809aec4b
jlam <jlam@pkgsrc.org>
2008-02-27 18:16:37 +0000

    + Teach the readline buildlink3.mk and builtin.mk files to determine   the correct terminal library that must be linked along with -lreadline.   On NetBSD, this is -ltermcap.  On Linux, this should be either -ltinfo   or -lcurses.  On Solaris, this should be -lcurses.
    + Add a "termlib" dependency to the devel/readline package that allows
      for linking against -lcurses.  Bump the PKGREVISON to 1.

--- devel/readline/buildlink3.mk
+++ devel/readline/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.28 2007/01/17 03:11:18 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.29 2008/02/27 18:16:37 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 READLINE_BUILDLINK3_MK:=	${READLINE_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	readline
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}readline
 
 .if !empty(READLINE_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.readline+=		readline>=2.2
+BUILDLINK_API_DEPENDS.readline+=	readline>=2.2
 BUILDLINK_ABI_DEPENDS.readline+=	readline>=5.0
 BUILDLINK_PKGSRCDIR.readline?=		../../devel/readline
 
@@ -23,22 +23,26 @@ BUILDLINK_FNAME_TRANSFORM.readline+= \
 	-e "s|include/history\.h|include/readline/history.h|g"		\
 	-e "s|include/readline\.h|include/readline/readline.h|g"
 
-# Many GNU configure scripts don't check for the correct termcap library
+# Many GNU configure scripts don't check for the correct terminal library
 # when testing for -lreadline.  If BROKEN_READLINE_DETECTION is set to
 # "yes", then automatically add the right one.
 #
 .  include "../../mk/bsd.fast.prefs.mk"
 BROKEN_READLINE_DETECTION?=	no
 .  if !empty(BROKEN_READLINE_DETECTION:M[yY][eE][sS])
-BUILDLINK_RL_TERMLIB.Linux=	curses
-BUILDLINK_RL_TERMLIB.*=		termcap
-.    if defined(BUILDLINK_RL_TERMLIB.${OPSYS})
-BUILDLINK_RL_TERMLIB?=		${BUILDLINK_RL_TERMLIB.${OPSYS}}
-.    else
-BUILDLINK_RL_TERMLIB?=		${BUILDLINK_RL_TERMLIB.*}
-.    endif
-BUILDLINK_TRANSFORM+=		l:readline:readline:${BUILDLINK_RL_TERMLIB}
+BUILDLINK_TRANSFORM+=		l:readline:readline:${BUILDLINK_LIBNAME.termlib}
 .  endif
 .endif	# READLINE_BUILDLINK3_MK
+
+CHECK_BUILTIN.readline:=	yes
+.include "../../devel/readline/builtin.mk"
+CHECK_BUILTIN.readline:=	no
+
+# A built-in readline is always going to use a built-in termlib.
+.if !empty(USE_BUILTIN.readline:M[yY][eE][sS])
+USE_BUILTIN.termlib=	yes
+.endif
+
+.include "../../mk/termlib.buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
