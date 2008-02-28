07352d4a6c55976aa4b34df05f116ff8a6ff6e2d
jlam <jlam>
2008-02-28 08:14:41 +0000

    Update security/heimdal to version 1.1.  Changes from version 0.7.2 include:
     * Read-only PKCS11 provider built-in to hx509.
     * Better compatibilty with Windows 2008 Server pre-releases and Vista.
     * Add RFC3526 modp group14 as default.
     * Handle [kdc] database = { } entries without realm = stanzas.
     * Add gss_pseudo_random() for mechglue and krb5.
     * Make session key for the krbtgt be selected by the best encryption
       type of the client.
     * Better interoperability with other PK-INIT implementations.
     * Alias support for inital ticket requests.
     * Make ASN.1 library less paranoid to with regard to NUL in string to
       make it inter-operate with MIT Kerberos again.
     * PK-INIT support.
     * HDB extensions support, used by PK-INIT.
     * New ASN.1 compiler.
     * GSS-API mechglue from FreeBSD.
     * Updated SPNEGO to support RFC4178.
     * Support for Cryptosystem Negotiation Extension (RFC 4537).
     * A new X.509 library (hx509) and related crypto functions.
     * A new ntlm library (heimntlm) and related crypto functions.
     * KDC will return the "response too big" error to force TCP retries
       for large (default 1400 bytes) UDP replies.  This is common for
       PK-INIT requests.
     * Libkafs defaults to use 2b tokens.
     * krb5_kuserok() also checks ~/.k5login.d directory for acl files.
     * Fix memory leaks.
     * Bugs fixes

--- security/heimdal/buildlink3.mk
+++ security/heimdal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.33 2008/01/18 05:09:37 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2008/02/28 08:14:41 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 HEIMDAL_BUILDLINK3_MK:=	${HEIMDAL_BUILDLINK3_MK}+
@@ -15,7 +15,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}heimdal
 
 .if !empty(HEIMDAL_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.heimdal+=	heimdal>=0.4e
-BUILDLINK_ABI_DEPENDS.heimdal+=	heimdal>=0.7.2nb4
+BUILDLINK_ABI_DEPENDS.heimdal+=	heimdal>=1.1
 BUILDLINK_PKGSRCDIR.heimdal?=	../../security/heimdal
 BUILDLINK_INCDIRS.heimdal?=	include/krb5
 .endif	# HEIMDAL_BUILDLINK3_MK
