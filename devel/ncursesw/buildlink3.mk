1edde7369f9ea875c7704cfb1a546e93f55f0977
jlam <jlam@pkgsrc.org>
2008-02-29 19:23:07 +0000

    Provide value for BUILDLINK_LDADD.ncursesw that can be used by packages that need to know how to link against ncursesw.

--- devel/ncursesw/buildlink3.mk
+++ devel/ncursesw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2008/02/20 17:17:46 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2008/02/29 19:23:07 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 NCURSESW_BUILDLINK3_MK:=${NCURSESW_BUILDLINK3_MK}+
@@ -15,6 +15,9 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}ncursesw
 BUILDLINK_API_DEPENDS.ncursesw+=	ncursesw>=5.5
 BUILDLINK_ABI_DEPENDS.ncursesw+=	ncursesw>=5.5
 BUILDLINK_PKGSRCDIR.ncursesw?=		../../devel/ncursesw
+
+BUILDLINK_LIBNAME.ncursesw=	ncursesw
+BUILDLINK_LDADD.ncursesw=	${BUILDLINK_LIBNAME.ncursesw:S/^/-l/:S/^-l$//}
 .endif	# NCURSESW_BUILDLINK3_MK
 
 .include "../../devel/ncurses/buildlink3.mk"
