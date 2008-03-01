d372be03e093259351355c01bb27cb388f24ebbd
joerg <joerg@pkgsrc.org>
2008-03-01 14:14:20 +0000

    When CUPS is compiled with Kerberos support, buildlink that as well.

--- print/cups/buildlink3.mk
+++ print/cups/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2008/01/18 05:08:53 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2008/03/01 14:14:20 joerg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CUPS_BUILDLINK3_MK:=	${CUPS_BUILDLINK3_MK}+
@@ -16,6 +16,13 @@ BUILDLINK_API_DEPENDS.cups+=	cups>=1.1.19nb3
 BUILDLINK_ABI_DEPENDS.cups+=	cups>=1.2.12nb3
 BUILDLINK_PKGSRCDIR.cups?=	../../print/cups
 .endif	# CUPS_BUILDLINK3_MK
+
+pkgbase := cups
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.cups:Mkerberos)
+.include "../../mk/krb5.buildlink3.mk"
+.endif
 
 .include "../../graphics/png/buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
