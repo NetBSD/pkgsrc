c17df2a891d74d54e31492eafe71623f4eee4546
jlam <jlam@pkgsrc.org>
2008-03-04 06:45:33 +0000

    + Conditionally add GAMES_USER and GAMES_GROUP to the platform *.mk   files.  These variables are currently usable if ${SETGIDGAME} == yes.   These variables should be used when describing ownership of files   and directories to the pkginstall framework, e.g.
    	SPECIAL_PERMS=	bin/foogame ${GAMES_USER} ${GAMES_GROUP} 2555
    
    + Rename SETGID_GAME_PERMS to SETGID_GAMES_PERMS because the default
      group name is "games".
    
    + Define SETGID_GAMES_PERMS in terms of GAMES_USER and GAMES_GROUP so
      that these names are protected from the normal flow of unprivileged.mk.
      This fixes the +INSTALL scripts in "user-destdir" packages to
      correctly refer to the games:games instead of the user:group of the
      user that built the packages.

--- mk/platform/BSDOS.mk
+++ mk/platform/BSDOS.mk
@@ -1,4 +1,4 @@
-# $NetBSD: BSDOS.mk,v 1.23 2007/10/19 13:41:34 rillig Exp $
+# $NetBSD: BSDOS.mk,v 1.24 2008/03/04 06:45:34 jlam Exp $
 #
 # Variable definitions for the BSD/OS operating system.
 
@@ -89,8 +89,10 @@ PKG_CREATE_USERGROUP=	NO # until it works
 # BINOWN, BINGRP and BINMODE as per defaults/mk.conf).
 # FIXME: Adjust to work on this system and enable the lines below.
 #.if !(empty(SETGIDGAME:M[yY][eE][sS]))
-#GAMEOWN=		games
-#GAMEGRP=		games
+#GAMES_USER=		games
+#GAMES_GROUP=		games
+#GAMEOWN=		${GAMES_USER}
+#GAMEGRP=		${GAMES_GROUP}
 #GAMEMODE=		2555
 #GAMEDIRMODE=		0775
 #.endif
