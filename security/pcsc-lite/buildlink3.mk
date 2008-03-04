75f6fbaec6b62f25652fcdddf667aecc8e574948
shannonjr <shannonjr>
2008-03-04 11:29:08 +0000

    The purpose of PC/SC Lite is to provide a Windows(R) SCard interface in a very small form factor for communicating to smartcards and readers.
    The PC/SC Lite library is used to connect to the PC/SC daemon from a
    client application and provide access to the desired reader.

---
+++ security/pcsc-lite/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/03/04 11:29:08 shannonjr Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+PCSC_LITE_BUILDLINK3_MK:=	${PCSC_LITE_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	pcsc-lite
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npcsc-lite}
+BUILDLINK_PACKAGES+=	pcsc-lite
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}pcsc-lite
+
+.if ${PCSC_LITE_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.pcsc-lite+=	pcsc-lite>=1.4.99
+BUILDLINK_PKGSRCDIR.pcsc-lite?=	../../local/pcsc-lite
+.endif	# PCSC_LITE_BUILDLINK3_MK
+
+.include "../../devel/libusb/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
