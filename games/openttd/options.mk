b924061b117e80c66eb7cfbfe65cd8cfc5ba3de5
tnn <tnn@pkgsrc.org>
2008-03-04 04:05:30 +0000

    Add "ttd-data" option (default off) which installs TTD datafiles along with the package and makes the package restricted.

---
+++ games/openttd/options.mk
@@ -0,0 +1,32 @@
+# $NetBSD: options.mk,v 1.1 2008/03/04 04:05:30 tnn Exp $
+
+.include "../../mk/bsd.prefs.mk"
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.openttd
+PKG_SUPPORTED_OPTIONS=	ttd-data
+PKG_SUGGESTED_OPTIONS=	# empty
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mttd-data)
+PLIST_SUBST+=		TTD_DATA=""
+LICENSE=		commercial-software
+RESTRICTED=		commercial-software
+NO_BIN_ON_CDROM=	${RESTRICTED}
+NO_BIN_ON_FTP=		${RESTRICTED}
+NO_SRC_ON_CDROM=	${RESTRICTED}
+NO_SRC_ON_FTP=		${RESTRICTED}
+
+DATAFILES=	sample.cat trg1r.grf trgcr.grf trghr.grf trgir.grf trgtr.grf
+GMFILES=	00 01 02 03 04 05 06 07 08 09 10
+GMFILES+=	11 12 13 14 15 16 17 18 19 20 21
+GMFILES:=	${GMFILES:C/^/gm_tt/g:C/$/.gm/g}
+DISTFILES=	${DISTNAME}${EXTRACT_SUFX} \
+		  ${DATAFILES:C/^/ttd\//g} ${GMFILES:C/^/ttd\//g}
+
+post-extract:
+	${RUN} for f in ${DATAFILES} ${GMFILES}; do \
+	  cp ${DISTDIR}/ttd/$$f ${WRKDIR}; done
+.else
+PLIST_SUBST+=	TTD_DATA="@comment "
+.endif
