bd89951b8295d703a8f08a4896d025a84c723f87
jlam <jlam@pkgsrc.org>
2008-03-07 22:00:43 +0000

    Drop the inet6 option and mark this package as simply "IPV6_READY" because it doesn't care about the IP family.

--- www/bozohttpd/options.mk
+++ www/bozohttpd/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1 2008/03/03 15:53:03 jlam Exp $
+# $NetBSD: options.mk,v 1.2 2008/03/07 22:00:43 jlam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.bozohttpd
-PKG_SUPPORTED_OPTIONS=	bozohttpd-do-htpasswd inet6
+PKG_SUPPORTED_OPTIONS=	bozohttpd-do-htpasswd
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,11 +10,4 @@ PKG_SUPPORTED_OPTIONS=	bozohttpd-do-htpasswd inet6
 ###
 .if !empty(PKG_OPTIONS:Mbozohttpd-do-htpasswd)
 CPPFLAGS+=	-DDO_HTPASSWD
-.endif
-
-###
-### IPv6 support.
-###
-.if !empty(PKG_OPTIONS:Minet6)
-CPPFLAGS+=	-DINET6
 .endif
