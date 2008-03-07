dd2005e6588a1d399b628e8731da7c460fd0f885
wiz <wiz@pkgsrc.org>
2008-03-07 07:07:01 +0000

    Accept totem-xine again (fix problem introduced by bl3bump). Addresses PR 38189 by charlie.

--- multimedia/totem/buildlink3.mk
+++ multimedia/totem/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2008/03/06 14:53:52 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2008/03/07 07:07:01 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 TOTEM_BUILDLINK3_MK:=	${TOTEM_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}totem
 
 .if !empty(TOTEM_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.totem+=	{totem,totem-xine}>=1.4.0
-BUILDLINK_ABI_DEPENDS.totem?=	totem>=2.20.3nb2
+BUILDLINK_ABI_DEPENDS.totem+=	{totem,totem-xine}>=2.20.3nb2
 BUILDLINK_PKGSRCDIR.totem?=	../../multimedia/totem
 .endif	# TOTEM_BUILDLINK3_MK
 
