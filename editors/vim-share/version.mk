eac964ad0570112c203a06d81c572a582374c343
martti <martti>
2008-03-11 10:55:02 +0000

    VIM 7.1.270
      1751  7.1.253  ":sort" doesn't work in a one line file
      1894  7.1.254  Tests 49 and 55 fail when the locale is French
      1514  7.1.255  Vim doesn't support utf-32
     50921  7.1.256  findfile() also returns directories
      3326  7.1.257  configure can't always find the Tcl header files
      3360  7.1.258  crash when doing "d/\n/e" and 'virtualedit' is "all"
      1944  7.1.259  cursor in wrong place with 'rl', "utf-8" and illegal byte
      1805  7.1.260  cursor position wrong after ^@ wrapping halfway if using utf-8
      2255  7.1.261  for a 2 byte BOM UCS-2 is used, which doesn't work for UTF-16
      3438  7.1.262  can't get the process ID of Vim
      2442  7.1.263  filetype with dot doesn't work for indent plugins
      6295  7.1.264  crash when C-indenting
      1310  7.1.265  hang when completing file name and space in 'isfname'
      2510  7.1.266  version string returned by terminal may be used as typed input
      1957  7.1.267  when changing folds cursor may be positioned in a wrong place
      1576  7.1.268  always shows "+" at end of screen line with 'cursurline'
      6183  7.1.269  matchparen plugin has an arbitrary line number limit
      2861  7.1.270  ":?foo?" matches in current line since patch 7.1.025

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.48 2008/02/15 06:36:11 martti Exp $
+# $NetBSD: version.mk,v 1.49 2008/03/11 10:55:03 martti Exp $
 
 VIM_VERSION=		7.1
-VIM_PATCHLEVEL=		252
+VIM_PATCHLEVEL=		270
 VIM_SUBDIR=		vim71
