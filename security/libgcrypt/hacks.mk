2831cff1871d5d929797cf8afaa07d722a9ed607
seb <seb@pkgsrc.org>
2008-03-16 20:14:38 +0000

    Add a hack for GCC 3.* failing to compile asm() call in cipher/rijndael.c:do_padlock(): seen with GCC 3.3.3 on NetBSD.

--- security/libgcrypt/hacks.mk
+++ security/libgcrypt/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.2 2004/12/21 08:57:48 grant Exp $
+# $NetBSD: hacks.mk,v 1.3 2008/03/16 20:14:38 seb Exp $
 
 # config.h #defines socklen_t if it is not defined, but libgcrypt does
 # not propogate this to the installed gcrypt.h, so packages using it
@@ -10,3 +10,10 @@ post-configure:
 		${MV} ${WRKSRC}/src/gcrypt.h ${WRKSRC}/src/gcrypt.h.old; \
 		${SED} -e "s,^/\* socklen_t \*/,$$socklen," \
 			${WRKSRC}/src/gcrypt.h.old > ${WRKSRC}/src/gcrypt.h
+
+# GCC 3.x (at least 3.3.3 on NetBSD) fails to compile asm() call in
+# cipher/rijndael.c:do_padlock()
+.include "../../mk/compiler.mk"
+.if !empty(CC_VERSION:Mgcc-3.*)
+CFLAGS+=	 -fomit-frame-pointer
+.endif
