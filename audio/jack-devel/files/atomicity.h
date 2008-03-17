fa8084995acf6b08a2d66992f605b026a80706fc
bjs <bjs@pkgsrc.org>
2008-03-17 02:32:43 +0000

    Oops, add ${FILESDIR} and its contents.

---
+++ audio/jack-devel/files/atomicity.h
@@ -0,0 +1,26 @@
+// Low-level functions for atomic operations: IA64 version  -*- C++ -*-
+
+/* $NetBSD: atomicity.h,v 1.1 2008/03/17 02:32:43 bjs Exp $ */
+
+#ifndef _NETBSD_ATOMICITY_H
+#define _NETBSD_ATOMICITY_H       1
+
+#include <sys/atomic.h>
+
+typedef unsigned int  _Atomic_word;
+
+static inline _Atomic_word 
+__attribute__ ((__unused__))
+__exchange_and_add(volatile _Atomic_word* __mem, int __val)
+{
+  return atomic_add_int_nv(__mem, __val);
+}
+
+static inline void
+__attribute__ ((__unused__))
+__atomic_add(volatile _Atomic_word* __mem, int __val)
+{
+  atomic_add_int(__mem, __val);
+}
+
+#endif /* atomicity.h */
