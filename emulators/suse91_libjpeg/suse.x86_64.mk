0981996a5e9616eca53da672f04d15f562866ac1
wiz <wiz@pkgsrc.org>
2008-03-25 13:45:36 +0000

    Remove suse-9.1 emulation packages. They are long outdated and not maintained upstream.
    Update infrastructure for their removal.
    
    Removal was announced on pkgsrc-users on March 13.

--- emulators/suse91_libjpeg/suse.x86_64.mk
+++
@@ -1,4 +0,0 @@
-# $NetBSD: suse.x86_64.mk,v 1.1 2007/07/29 05:19:15 jlam Exp $
-
-# SuSE x86_64 RPMs
-DISTFILSE=	libjpeg-6.2.0-320.${SUSE_ARCH}.rpm
--- emulators/suse91_locale/suse.i386.mk
+++
@@ -1,4 +0,0 @@
-# $NetBSD: suse.i386.mk,v 1.1 2007/07/29 05:19:19 jlam Exp $
-
-# SuSE i386 RPMs
-DISTFILES=	glibc-locale-2.3.3-98.${SUSE_ARCH}.rpm
