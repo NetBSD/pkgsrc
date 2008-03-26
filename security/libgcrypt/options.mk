1b2c998d0f31c8d3814584e211582da6dc69ee31
tron <tron@pkgsrc.org>
2008-03-26 20:09:43 +0000

    Add a new option "via-padlock" which enables support for the VIA PadLock Security Engine: - The new option is turned on by default. - The new option is only available on i386 systems except Mac OS X (which   doesn't work on VIA CPU). - The new option isn't available on system which uses GCC 3.x because it   causes build failures. This fixes PR pkg/38197.
    Approved by Dieter Baron.

--- security/libgcrypt/options.mk
+++ security/libgcrypt/options.mk
@@ -1,12 +1,24 @@
-# $NetBSD: options.mk,v 1.1 2007/08/29 23:11:38 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2008/03/26 20:09:43 tron Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libgcrypt
 PKG_SUPPORTED_OPTIONS=	idea
 
+.include "../../mk/bsd.prefs.mk"
+
+.if ${MACHINE_ARCH} == "i386" && ${OPSYS} != "Darwin"
+.  include "../../mk/compiler.mk"
+# GCC 3.x (at least 3.3.3 on NetBSD) fails to compile asm() call in
+# cipher/rijndael.c:do_padlock()
+.  if empty(CC_VERSION:Mgcc-3.*)
+PKG_SUPPORTED_OPTIONS+=	via-padlock
+PKG_SUGGESTED_OPTIONS+=	via-padlock
+.  endif
+.endif
+
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Midea)
-# use of IDEA as crypto function
+# Use of IDEA as crypto function.
 LICENSE=	idea-license
 RESTRICTED=     Commercial distribution is claimed to require a license.
 NO_SRC_ON_CDROM=        ${RESTRICTED}
@@ -21,8 +33,13 @@ SUBST_STAGE.idea=	post-patch
 SUBST_FILES.idea=	cipher/idea.c
 SUBST_SED.idea=		-e 's,^.*SIZEOF_UNSIGNED_LONG.*$$,,'
 
-USE_TOOLS+=		automake
+USE_TOOLS+=		autoreconf
 
 pre-configure:
 	cd ${WRKSRC} && autoreconf -i -v
+.endif
+
+.if empty(PKG_OPTIONS:Mvia-padlock)
+# Disable VIA Padlock support.
+CONFIGURE_ARGS+=        --disable-padlock-support
 .endif
