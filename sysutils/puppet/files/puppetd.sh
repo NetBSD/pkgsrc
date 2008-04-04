e6f20e935ace0a9295d564adcadea71e0b5465b5
jlam <jlam>
2008-04-04 15:20:48 +0000

    Update puppet to version 0.24.4.  Changes from verison 0.22.4 include:
    + Install as a Ruby gem.
    
    - Better caching of external lookups.
    
    - Many bug fixes.
    
    - External node commands can specify an environment and Puppet will now
      use it.
    
    - Always duplicating resource defaults in the parser, so that stacked
      metaparameter values do not result in all resources that receive a
      given default also getting those stacked values.
    
    - Puppet's parser no longer changes the order in which statements are
      evaluated, which means that case statements can now set variables that
      are used by other variables.
    
    - Clients should now fail to install files whose checksums do not match the
      checksum from the server.
    
    - Autoloading now searches the plugins directory in each module, in
      addition to the lib directory.
    
    - Virtual defined types are no longer evaluated.
    
      NOTE: This introduces a behaviour change, in that you previously could
      realize a resource within a virtual defined resource, and now you must
      realize the entire defined resource, rather than just the contained
      resource.
    
    - Added builtin support for Nagios types using Naginator to parse and
      generate the files.
    
    - Modifying the behaviour of the certdnsnames setting.  It now defaults
      to an empty string, and will only be used if it is set to something
      else.
    
    - External node support now requires that you set the 'node_terminus'
      setting to 'exec'.
    
    - You now must specify an environment and you are required to specify
      the valid environments for your site.
    
    - The --use-nodes and --no-nodes options are now obsolete.  Puppet
      automatically detects when nodes are defined, and if they are defined it
      will require that a node be found, else it will not look for a node nor
      will it fail if it fails to find one.
    
    - Added support for managing interfaces.
    
    - Puppet clients now have http proxy support.
    
    - Adding module autoloading.
    
    - Significantly reworked external node support, in a way that's NOT
      backward-compatible:
    
        Only ONE node source can be used -- you can use LDAP, code, or
        an external node program, but not more than one.
    
        LDAP node support has two changes:  First, the "ldapattrs" attribute is
        now used for setting the attributes to retrieve from the server (in
        addition to required attriutes), and second, all retrieved attributes
        are set as variables in the top scope.  This means you can set attributes
        on your LDAP nodes and they will automatically appear as variables
        in your configurations.
    
        External node support has been completely rewritten.  These programs must
        now generate a YAML dump of a hash, with "classes" and "parameters" keys.
        The classes should be an array, and the parameters should be a hash.  The
        external node program has no support for parent nodes -- the script must
        handle that on its own.
    
    - Replaced the obsolete RRD ruby library with the maintained RubyRRDtool
      library
    
    - Significantly reworking configuration parsing.  Executables all now
      look for 'puppet.conf', although they will parse the old-style
      configuration files if they are present, although they throw a
      deprecation warning.  Also, file parameters (owner, mode, group) are now
      set on the same line as the parameter, in brackets.

--- sysutils/puppet/files/puppetd.sh
+++ sysutils/puppet/files/puppetd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: puppetd.sh,v 1.1.1.1 2008/03/13 14:17:05 tonnerre Exp $
+# $NetBSD: puppetd.sh,v 1.2 2008/04/04 15:20:48 jlam Exp $
 #
 # PROVIDE: puppetd
 # REQUIRE: DAEMON
@@ -9,7 +9,7 @@
 # Add the following lines to /etc/rc.conf to enable puppetd:
 #
 # puppetd="YES"
-# puppetd_confdir="@PREFIX@/etc/puppet"
+# puppetd_confdir="@PKG_SYSCONFDIR@"
 # puppetd_pid="/var/run/${name}.pid"
 # puppetd_flags="--confdir $puppetd_confdir --rundir /var/run"
 
@@ -20,8 +20,8 @@ fi
 name="puppetd"
 rcvar=$name
 command="@PREFIX@/bin/${name}"
-command_interpreter="@PREFIX@/bin/ruby18"
-: ${puppetd_confdir="@PREFIX@/etc/puppet"}
+command_interpreter="@RUBY@"
+: ${puppetd_confdir="@PKG_SYSCONFDIR@"}
 : ${puppetd_pid="/var/run/${name}.pid"}
 : ${puppetd_flags="--confdir $puppetd_confdir --rundir /var/run"}
 
