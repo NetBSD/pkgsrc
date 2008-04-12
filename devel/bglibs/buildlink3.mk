d972618e58ef565d0a7928b3d608b5d23a0bd2d2
schmonz <schmonz@pkgsrc.org>
2008-04-12 20:56:25 +0000

    Bump default BUILDLINK_API_DEPENDS to 1.104 (the latest).

--- devel/bglibs/buildlink3.mk
+++ devel/bglibs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/12/28 03:20:32 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2008/04/12 20:56:25 schmonz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 BGLIBS_BUILDLINK3_MK:=	${BGLIBS_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	bglibs
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}bglibs
 
 .if !empty(BGLIBS_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.bglibs+=	bglibs>=1.101
+BUILDLINK_API_DEPENDS.bglibs+=	bglibs>=1.104
 BUILDLINK_PKGSRCDIR.bglibs?=	../../devel/bglibs
 .endif	# BGLIBS_BUILDLINK3_MK
 
