c86e5daea8bbf9928eeeecb3f94fe5ac08f32a9e
wiz <wiz@pkgsrc.org>
2008-04-13 21:43:39 +0000

    Update to 1.0.19.
    Remove sane-backends-hp3900 option, support is now enabled by default.
    
    New with 1.0.19, released 2008-02-10:
    
    * New backends: cardscan (Corex Cardscan 800c), epjitsu (Epson-based Fujitsu),
      epson2 (various Epson scanners), hp3900 (HP ScanJet 3970 and more),
      hp5590 (HP ScanJet 5590 and more), hpljm1005 (HP LaserJet M1005 and more),
      hs2p (Ricoh IS400 series)
    * Updated backends: abaton, agfafocus, apple, artec, artec_eplus48u, as6e,
      avision, canon, coolscan, coolscan2, dc25, dell1600n_net, dll, epson,
      fujitsu, genesys, gt68xx, hp3500, ibm, lexmark, microtek, microtek2, mustek,
      nec, net, pie, pint, pixma, plustek, plustek_pp, ricoh, s9036, sm3600,
      sm3840, snapscan, sp15c, st400, tamarack, teco2, test, u12, umax, umax1220u,
      umax_pp
    * Scanimage detects more chipsets.
    * Internal scsi, usb, tcp and udp code updates.
    * Basic HAL .fdi creation support
    * Build system updates.
    * Updated translations.
    * Documentation updates.
    * Bugfixes.

--- graphics/sane-backends/options.mk
+++ graphics/sane-backends/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.6 2008/04/12 22:43:02 jlam Exp $
+# $NetBSD: options.mk,v 1.7 2008/04/13 21:43:39 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.sane-backends
-PKG_SUPPORTED_OPTIONS=		inet6 sane-backends-hp3900
+PKG_SUPPORTED_OPTIONS=		inet6
 PKG_SUGGESTED_OPTIONS=		#defined
 
 .include "../../mk/bsd.options.mk"
@@ -12,19 +12,4 @@ PKG_SUGGESTED_OPTIONS=		#defined
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
-.endif
-
-
-###
-### hp3900 support
-###
-PLIST_VARS+=	hp3900
-
-.if !empty(PKG_OPTIONS:Mhp3900)
-PLIST.hp3900=	yes
-DISTFILES+=	hp3900-sane1017_src_0.5.tar.gz
-SITES.hp3900-sane1017_src_0.5.tar.gz=${MASTER_SITE_SOURCEFORGE:=hp3900-series/}
-post-patch:
-	cd ${WRKSRC} && \
-	${PATCH} -p1 <${WRKDIR}/hp3900-sane1017_src_0.5/hp3900-sane1017_0.5.diff
 .endif
