26d81c84bc5feae12bd74829c8f457db3096ab6f
drochner <drochner@pkgsrc.org>
2008-04-15 17:45:38 +0000

    update to 2.22.1 This switches to the gnome-2.22 release branch.

--- mail/evolution-data-server/buildlink3.mk
+++ mail/evolution-data-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2008/03/06 14:53:51 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2008/04/15 17:45:38 drochner Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 EVOLUTION_DATA_SERVER_BUILDLINK3_MK:=	${EVOLUTION_DATA_SERVER_BUILDLINK3_MK}+
@@ -27,5 +27,6 @@ PRINT_PLIST_AWK+=/^@dirrm lib\/evolution-data-server-1.2$$/ \
 .include "../../devel/libbonobo/buildlink3.mk"
 .include "../../devel/libgnome/buildlink3.mk"
 .include "../../devel/nss/buildlink3.mk"
+.include "../../databases/db4/buildlink3.mk"
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
