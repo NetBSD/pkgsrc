4958f2d070fc92a753a905a046a4776568a3a8b2
joerg <joerg@pkgsrc.org>
2008-04-15 16:49:56 +0000

    bootstrap-mk-files-20080415: Don't bother with real dependency tracking, just touch .depend.

--- pkgtools/bootstrap-mk-files/files/bsd.dep.mk
+++ pkgtools/bootstrap-mk-files/files/bsd.dep.mk
@@ -1,9 +1,7 @@
-#	$NetBSD: bsd.dep.mk,v 1.1.1.1 2006/07/14 23:13:00 jlam Exp $
+#	$NetBSD: bsd.dep.mk,v 1.2 2008/04/15 16:49:56 joerg Exp $
 
 .PHONY:		cleandepend
 cleandir: cleandepend
-
-MKDEP?=		mkdep
 
 # some of the rules involve .h sources, so remove them from mkdep line
 realdepend: beforedepend
@@ -12,35 +10,7 @@ realdepend: .depend
 .NOPATH: .depend
 .depend: ${SRCS} ${DPSRCS}
 	@rm -f .depend
-	@files="${.ALLSRC:M*.s} ${.ALLSRC:M*.S}"; \
-	if [ "$$files" != " " ]; then \
-	  echo ${MKDEP} -a ${MKDEPFLAGS} \
-	    ${AFLAGS:M-[ID]*:Q} ${CPPFLAGS:Q} -traditional-cpp ${AINC:Q} \
-	    $$files; \
-	  ${MKDEP} -a ${MKDEPFLAGS} \
-	    ${AFLAGS:M-[ID]*} ${CPPFLAGS} -traditional-cpp ${AINC} $$files; \
-	fi
-	@files="${.ALLSRC:M*.c}"; \
-	if [ "$$files" != "" ]; then \
-	  echo ${MKDEP} -a ${MKDEPFLAGS} \
-	    ${CFLAGS:M-[ID]*:Q} ${CPPFLAGS:Q} $$files; \
-	  ${MKDEP} -a ${MKDEPFLAGS} \
-	    ${CFLAGS:M-[ID]*} ${CPPFLAGS} $$files; \
-	fi
-	@files="${.ALLSRC:M*.m}"; \
-	if [ "$$files" != "" ]; then \
-	  echo ${MKDEP} -a ${MKDEPFLAGS} \
-	    ${OBJCFLAGS:M-[ID]*:Q} ${CPPFLAGS:Q} $$files; \
-	  ${MKDEP} -a ${MKDEPFLAGS} \
-	    ${OBJCFLAGS:M-[ID]*} ${CPPFLAGS} $$files; \
-	fi
-	@files="${.ALLSRC:M*.cc} ${.ALLSRC:M*.C} ${.ALLSRC:M*.cxx}"; \
-	if [ "$$files" != "  " ]; then \
-	  echo ${MKDEP} -a ${MKDEPFLAGS} \
-	    ${CXXFLAGS:M-[ID]*:Q} ${CPPFLAGS:Q} $$files; \
-	  ${MKDEP} -a ${MKDEPFLAGS} \
-	    ${CXXFLAGS:M-[ID]*} ${CPPFLAGS} $$files; \
-	fi
+	@touch .depend
 cleandepend:
 	rm -f .depend ${.CURDIR}/tags ${CLEANDEPEND}
 .else
