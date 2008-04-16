1a5441c7218db25340484247e2a3a3b1e47d2bf0
bjs <bjs@pkgsrc.org>
2008-04-16 00:09:39 +0000

    Update to version 0.13.1+2.  This update includes myriad bug fixes and feature enhancements--way too many for this commit message. While here, deprecate aria2-metalink option. Aria2 now supports building with libexpat, so we do that by default now.
    Some of the more significant changes:
    -------------------------------------
    
    * Fixed compile error without gnutls/libgcrypt/libgpg-error and openSSL
      installed.  Fixed compilation on: x84-64 platform, Mac OSX, g++-4.3,
      and without c-ares/ares.
    
    * Added Simplified Chinese translation. Also updated the following
      translations: Polish, Dutch, German, Thai, Indonesian,
      Norwegian Nynorsk, Hungarian, Finnish, Hebrew.
    
    * Changed inactive connection timeout to 120.
      To accept more mulually interested peers, disconnect peer when there
      is no interest between us after certain time passed.
      Lengthened DNS timeout to 30. I think old value '10' will be a
      little bit short when some DNS servers are offline and several
      DNS servers are tried. It should be configured by
      command-line option.
    
    * Fixed the bug that prevents aria2 from stopping other than by pressing
      Ctrl-C in BitTorrent download. It is reproducible using --seed-time
      option. aria2 doesn't stop even after --seed-time is satisfied and
      continues to output blank lines in the console.
    
    * Added Message Stream Encryption(MSE) support.
      Currently, aria2 accepts incoming connections with Obfuscation Header
      and legacy BitTorrent Header and establishes connections with
      Obfuscation Header first and if failed then retry with legacy
      BitTorrent header. If plain text and ARC4 is provided, aria2 always
      choose ARC4. The new option to change the default behavior is planned.
      For tracker extension, "supportcrypto=1" is added statically.
    
    * Added the ability to load nodes from torrent file. These nodes are
      added to the routing table when downloading that torrent.
    
    * Added the ability to stop aria2 itself when given time has passed
      from start. Use --stop option to specify time in minutes.
      When 0 is given, this feature is disabled.
      before calling RequestGroupMan::fillRequestGroupFromReserver().
      Without this modification, the result list shows "ERR" when aria2 is
      stopped by --stop option. It should be "INPR".
      Bootstrap through node added by port message.
      Currently bootstrap is executed if the number of buckets in routing
      table is 1.
    
    * Added DHT functionality, compatible with mainline.
      DHT is disabled by default. To enable it, give --enable-dht to aria2c.
      You may need to specify entry point to DHT network using
      --dht-entry-point. DHT uses UDP port to listen incoming message.
      Use --dht-listen-port to specify port number. Make sure that your
      firewall configuration can pass through UDP traffic to the port.
      The routing table is saved in $HOME/.aria2/dht.dat.  Request peers though
      DHT only when download hasn't finished.

--- net/aria2/options.mk
+++ net/aria2/options.mk
@@ -1,51 +1,35 @@
-# $NetBSD: options.mk,v 1.4 2007/11/23 07:46:35 bjs Exp $
+# $NetBSD: options.mk,v 1.5 2008/04/16 00:09:39 bjs Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.aria2
 
-PKG_OPTIONS_GROUP.tls=	gnutls ssl
+PKG_OPTIONS_GROUP.ssl=		gnutls ssl
 PKG_OPTIONS_GROUP.asyncns=	ares cares
-
-PKG_OPTIONS_REQUIRED_GROUPS=	tls asyncns
-
-PKG_SUPPORTED_OPTIONS+=		aria2-metalink
-PKG_SUGGESTED_OPTIONS+=		ssl cares aria2-metalink
+PKG_OPTIONS_REQUIRED_GROUPS=	ssl asyncns
+PKG_SUGGESTED_OPTIONS=		ssl cares
+###
+### XXX Remove this in the future [2008-04-15 bjs]
+###
+.if !empty(PKG_OPTIONS.aria2:U:Maria2-metalink) || \
+	!empty(PKG_DEFAULT_OPTIONS:U:Maria2-metalink)
+PKG_OPTIONS_DEPRECATED_WARNINGS+=\
+	"NOTE:	The aria2-metalink option is deprecated; this package"\
+"	now supports the metalink protocol by default."
+.endif
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mssl)
-CONFIGURE_ARGS+=		--with-openssl
-CONFIGURE_ARGS+=		--without-gnutls
-
-.include "../../security/openssl/buildlink3.mk"
-
+CONFIGURE_ARGS+=	--with-openssl
+CONFIGURE_ARGS+=	--without-gnutls
+.  include "../../security/openssl/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=		--with-gnutls
-CONFIGURE_ARGS+=		--without-openssl
-
-.include "../../security/gnutls/buildlink3.mk"
-
+CONFIGURE_ARGS+=	--with-gnutls
+CONFIGURE_ARGS+=	--without-openssl
+.  include "../../security/gnutls/buildlink3.mk"
 .endif
-
 .if !empty(PKG_OPTIONS:Mcares)
 CONFIGURE_ARGS+=	--with-libcares-prefix=${BUILDLINK_PREFIX.libcares:Q}
-
-.include "../../net/libcares/buildlink3.mk"
-
+.  include "../../net/libcares/buildlink3.mk"
 .elif !empty(PKG_OPTIONS:Mares)
 CONFIGURE_ARGS+=	--with-libares-prefix=${BUILDLINK_PREFIX.libares:Q}
-
-.endif
-
-
-.if !empty(PKG_OPTIONS:Maria2-metalink)
-CONFIGURE_ARGS+=	--with-libxml2
-CONFIGURE_ARGS+=	--enable-metalink
-CONFIGURE_ARGS+=	--with-xml-prefix=${BULDLINK_PREFIX.libxml2:Q}
-
-.include "../../textproc/libxml2/buildlink3.mk"
-
-.else
-CONFIGURE_ARGS+=	--without-libxml2
-CONFIGURE_ARGS+=	--disable-metalink
-
 .endif
