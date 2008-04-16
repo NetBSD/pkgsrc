189a8ce2d44dc67165796222abdc9268d0f29131
sborrill <sborrill@pkgsrc.org>
2008-04-16 14:55:29 +0000

    Fix required_files line in rc.d script now that we are using PKG_SYSCONFSUBDIR. Bump PKGREVISION

--- www/dansguardian/files/dansguardian.sh
+++ www/dansguardian/files/dansguardian.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: dansguardian.sh,v 1.1.1.1 2008/02/22 17:13:29 sborrill Exp $
+# $NetBSD: dansguardian.sh,v 1.2 2008/04/16 14:55:29 sborrill Exp $
 #
 # PROVIDE: dansguardian
 # REQUIRE: DAEMON LOGIN
@@ -12,7 +12,7 @@ name="dansguardian"
 rcvar=$name
 command="/usr/pkg/sbin/${name}"
 pidfile="/var/run/${name}.pid"
-required_files="@PKG_SYSCONFDIR@/${name}/${name}.conf"
+required_files="@PKG_SYSCONFDIR@/${name}.conf"
 extra_commands="reload"
 
 load_rc_config $name
