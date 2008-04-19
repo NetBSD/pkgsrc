857e4830cde1c7ee98c226486db7405c5339a656
kefren <kefren@pkgsrc.org>
2008-04-19 18:42:10 +0000

    Update to mono-1.9
    Between the release of 1.2.6 and 1.9 more than 400 bugs were fixed.

--- lang/mono/buildlink3.mk
+++ lang/mono/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2008/03/21 14:52:15 kefren Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2008/04/19 18:42:10 kefren Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 MONO_BUILDLINK3_MK:=	${MONO_BUILDLINK3_MK}+
@@ -12,11 +12,9 @@ BUILDLINK_PACKAGES+=	mono
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}mono
 
 .if ${MONO_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.mono+=	mono>=1.2.6
+BUILDLINK_API_DEPENDS.mono+=	mono>=1.9
 BUILDLINK_PKGSRCDIR.mono?=	../../lang/mono
 .endif	# MONO_BUILDLINK3_MK
-
-ALL_ENV+=	MONO_SHARED_DIR=${WRKDIR:Q}
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../textproc/icu/buildlink3.mk"
