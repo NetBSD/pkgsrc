fdb0586822446e69f5065a6d07a247383b6e2e05
wiz <wiz@pkgsrc.org>
2008-04-22 21:23:41 +0000

    Update to 1.0.2. Add speex to DEPENDS. Update buildlink3.mk and bump ABI.
    04172008 - Look for Speex includes in new directory. Converted all text
               encoding from ISO-8859-1 to UTF-8. Fixed "make dist" script for
               dealing with Subversion instead of CVS. Added Speex to the README.
               Upped version to 1.0.2.
    04112008 - Check if Speex header has bogus data (CVE-2008-1686).
    08062007 - Updated my email address.
    07152007 - Minor correction in Timidity resampling code (Thanks, Sam!).
    07062007 - Fixed uninitialized buffer in mpglib. (Thanks, Phil!).
    10292006 - Fixed bogus memory dereference when SMPEG fails init (thanks, Chris!)
    10272006 - FLAC 1.1.3 breaks their API _again_, so we try to do the right
               thing at build time. (Thanks, Josh!).
    05122006 - Patched to get mpglib compiling again (thanks, Sam!).
    12172005 - Fixed gcc4 whining in playsound_simple.c.
    12062005 - Trimmed a bunch of junk out of the build system, and now it works
               on Mac OS X again.
    10122005 - Check for libmodplug headers in two possible places (thanks, Tyler!)
    10012005 - Added playsound_simple.c.
    05302005 - Backport from devtree: Fixed automake nonsense.
    11122004 - Backport from devtree: fix .voc decoder crash on file open.
    05082004 - Fixed "bootstrap" to work with MacOSX.
    05072004 - Backed out some commits, converted repository to Subversion, and
               branched off to a 1.1.0 development tree. Changed MikMod URL...old
               one is now a porn site.  :(
    10252003 - VOC decoder was broken. Now it isn't.
    10142003 - Build system fix: acinclude.m4 had some word wrapping badness.

--- audio/SDL_sound/buildlink3.mk
+++ audio/SDL_sound/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/07/08 23:10:35 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2008/04/22 21:23:41 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SDL_SOUND_BUILDLINK3_MK:=	${SDL_SOUND_BUILDLINK3_MK}+
@@ -13,13 +13,14 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}SDL_sound
 
 .if !empty(SDL_SOUND_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.SDL_sound+=	SDL_sound>=1.0.1
-BUILDLINK_ABI_DEPENDS.SDL_sound+=	SDL_sound>=1.0.1nb5
+BUILDLINK_ABI_DEPENDS.SDL_sound+=	SDL_sound>=1.0.2
 BUILDLINK_PKGSRCDIR.SDL_sound?=	../../audio/SDL_sound
 .endif	# SDL_SOUND_BUILDLINK3_MK
 
+.include "../../audio/flac/buildlink3.mk"
 .include "../../audio/libmikmod/buildlink3.mk"
 .include "../../audio/libvorbis/buildlink3.mk"
+.include "../../audio/speex/buildlink3.mk"
 .include "../../devel/SDL/buildlink3.mk"
-.include "../../multimedia/smpeg/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
