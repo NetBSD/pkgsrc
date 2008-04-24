53b4bb28fb698a42efaa6202f26cec797a8d8e94
bjs <bjs@pkgsrc.org>
2008-04-24 07:40:23 +0000

    Welcome to Mesa-7.0.3.
    Changes from 6.4.2 are (obviously) far too numerous to mention!  This
    package brings with it support for the Direct Rendering Infrastructure
    (DRI) on NetBSD-current; the kernel DRM was committed approximately
    one year ago.  Patches for NetBSD 4.0 are available in the mailing list
    archives (I think), but are _not supported_.
    
    Developers and other interested parties are encouraged to read the TODO
    file, as well as my remarks in options.mk.  There is still a significant
    amount of work to be done.  Users of NetBSD/i386 (-current) shouldn't
    have much trouble getting mainstream cards (Intel i915 and Radeon 9600-
    R430 are by far the most tested) to work.
    
    I have taken great pains to ensure that this works properly, but inevitably
    there will be problems--especially on heretofore untested (read: non-x86)
    hardware.  Your questions, comments, problem reports, and of course
    bug fixes are welcome.
    
    NOTE: There are currently problems (on NetBSD, at least) linking some
    executables, e.g. compiz, due to unresolved symbols for libpthread
    functions.  This is due to libX11's thread stubs, which should not
    be enabled on NetBSD >= 2.0; see the TODO For more information
    and the URL of an interim patch.

--- graphics/MesaLib/buildlink3.mk
+++ graphics/MesaLib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.43 2007/12/06 04:31:23 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.44 2008/04/24 07:40:24 bjs Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 MESALIB_BUILDLINK3_MK:=	${MESALIB_BUILDLINK3_MK}+
@@ -14,11 +14,8 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}MesaLib
 .if !empty(MESALIB_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.MesaLib+=	MesaLib>=3.4.2
 BUILDLINK_ABI_DEPENDS.MesaLib+=	MesaLib>=6.4.1nb1
+BUILDLINK_ABI_DEPENDS.MesaLib+= MesaLib>=7.0.3
 BUILDLINK_PKGSRCDIR.MesaLib?=	../../graphics/MesaLib
-
-.if !defined(BUILDING_MESA)
-BUILDLINK_CPPFLAGS.MesaLib=	-DGLX_GLXEXT_LEGACY
-.endif
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -27,7 +24,12 @@ BUILDLINK_CPPFLAGS.MesaLib=	-DGLX_GLXEXT_LEGACY
 BUILDLINK_LDFLAGS.MesaLib+=	-Wl,-dylib_file -Wl,/System/Library/Frameworks/OpenGL.framework/Versions/A/Libraries/libGL.dylib:/System/Library/Frameworks/OpenGL.framework/Versions/A/Libraries/libGL.dylib
 .endif
 
-BUILDLINK_TRANSFORM+=		l:MesaGL:GL
+pkgbase:= MesaLib
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.MesaLib:Mdri)
+.  include "../../graphics/MesaLib/dri.mk"
+.endif
 
 .endif	# MESALIB_BUILDLINK3_MK
 
--- graphics/glut/buildlink3.mk
+++ graphics/glut/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2007/01/17 16:16:14 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2008/04/24 07:40:24 bjs Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GLUT_BUILDLINK3_MK:=	${GLUT_BUILDLINK3_MK}+
@@ -14,6 +14,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}glut
 .if !empty(GLUT_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.glut+=	glut>=3.4.2
 BUILDLINK_ABI_DEPENDS.glut+=	glut>=6.4.1nb1
+BUILDLINK_ABI_DEPENDS.glut+=	glut>=7.0.3
 BUILDLINK_PKGSRCDIR.glut?=	../../graphics/glut
 .endif	# GLUT_BUILDLINK3_MK
 
