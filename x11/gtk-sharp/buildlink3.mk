a9ea00619358d53acfaf86b1cbf3c236c99934cb
wiz <wiz@pkgsrc.org>
2008-04-27 09:21:27 +0000

    Bump BUILDLINK_API_DEPENDS for gtk-sharp (name changes) and bump PKGREVISION of dependencies.

--- x11/gtk-sharp/buildlink3.mk
+++ x11/gtk-sharp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/03/17 22:50:28 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/04/27 09:21:27 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GTK_SHARP_BUILDLINK3_MK:=	${GTK_SHARP_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	gtk-sharp
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gtk-sharp
 
 .if ${GTK_SHARP_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.gtk-sharp+=	gtk-sharp>=2.10.3
+BUILDLINK_API_DEPENDS.gtk-sharp+=	gtk-sharp>=2.12.1
 BUILDLINK_PKGSRCDIR.gtk-sharp?=		../../x11/gtk-sharp
 .endif	# GTK_SHARP_BUILDLINK3_MK
 
