592b6a048b1a7a7c8c85f82fbb4f5c033d754052
wiz <wiz@pkgsrc.org>
2008-04-27 06:44:52 +0000

    Update to 0.8.2, based on pkgsrc-wip package by Paulo Matias.
    Additional changes:
    kde option compiles now, use standard installation method for
    Firefox plugin.
    
    2008-02-21  Sandro Santilli <strk@keybit.net>
    
    Fifth alpha release of Gnash (0.8.2).
    Improvements since 0.8.1 release are:
    
        - Improved timeline redesign.
        - Action execution order fixes.
        - Keyboard handling improved [1].
        - New classes implemented: System.capabilities (partial),
          SharedObject, LocalConnection.
        - New opcodes implemented: ActionImplements, Try/Throw.
        - Movieclip.beginGradientFill completes drawing API support.
        - MovieClip._lockroot support added.
        - Implement GET/POST for MovieClip.loadVariables and
          MovieClip.loadMovie
        - Textfield support improved.
        - Security: configurable sandbox restricts filesystem access by movies.
        - Better support for SWF8.
        - Streaming OGG-contained multimedia (e.g. vorbis/theora or vorbis/dirac),
          and other free formats.
        - OGL and Cairo renderers much improved.
        - Improvements of the Framebuffer GUI (hide text messages, support Linux
          events system, built-in touchscreen calibration, simple keyboard support,
          better architecture support).
        - SOLdumper utility for reading SOL files ('flash cookies').
        - DumpShm utility finds and dumps Local Connection memory segments.
        - Fullscreen display possible.
        - Extensions for LIRC and DBus.
        - High quality, cross-platform psuedo-randomness using boost random.
        - NPAPI (Mozilla) plugin: keyboard events work, navigation and javascript
          from within movies.
        - Set and save preferences from the GUI (GTK).
        - Improved stability and robustness with malformed SWFs.
        - Many other bugfixes.
    
    2007-08-29  Sandro Santilli <strk@keybit.net>
    
    Fourth alpha release of Gnash (0.8.1).
    Improvements since 0.8.0 release are:
    
    	- Garbage Collector
    	- Support for device fonts
    	- Support for dynamic text fields
    	- Better key handling
    	- Support for FFMPEG SVN
    	- Support for on-demand install of needed Gstreamer codec plugins.
    	- Debugger off by default (improves performance and memory use)
    	- AGG renderer by default
    	- Support for KDE/AGG combination
    	- Layout change : we'll now build <gui>-gnash and 'gnash' is a shell
    	  script with gui selection capabilities (-G)
    	- More robust parser
    	- Better support for flash player detection
    	- Fix for 'debugLog' directive in gnashrc

--- multimedia/gnash/options.mk
+++ multimedia/gnash/options.mk
@@ -1,20 +1,99 @@
-# $NetBSD: options.mk,v 1.4 2007/06/12 08:51:14 ghen Exp $
+# $NetBSD: options.mk,v 1.5 2008/04/27 06:44:52 wiz Exp $
 #
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.gnash
-PKG_SUPPORTED_OPTIONS=	klash
+#
+# see http://www.gnu.org/software/gnash/manual/gnashref.html#codedeps
+#
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.gnash
+PKG_SUPPORTED_OPTIONS=		gtk kde agg cairo mitshm opengl
+PKG_OPTIONS_OPTIONAL_GROUPS=    gnash-media
+PKG_OPTIONS_GROUP.gnash-media=  ffmpeg gstreamer
+PKG_SUGGESTED_OPTIONS+=         agg gstreamer gtk mitshm
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mklash)
-PLIST_SRC+=		${PKGDIR}/PLIST.klash
+###
+### Select GUIs.
+###
+.if !empty(PKG_OPTIONS:Mgtk)
+GNASH_GUIS+=		gtk
+PLIST_SRC+=		${PKGDIR}/PLIST.gtk
+CONFIGURE_ARGS+=	--with-npapi-plugindir=${PREFIX}/lib/firefox/plugins
+INSTALL_TARGET+=	install-plugin
+.include "../../x11/gtk2/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mkde)
+GNASH_GUIS+=		kde
+PLIST_SRC+=		${PKGDIR}/PLIST.kde
+PLIST_SUBST+=		KDE="kde/"
 .include "../../x11/kdebase3/buildlink3.mk"
 .include "../../meta-pkgs/kde3/kde3.mk"
-CONFIGURE_ARGS+=	--datadir="${PREFIX}/share"
-CONFIGURE_ARGS+=	\
-	--with-plugindir=${PREFIX}/share/gnash/plugins
-CONFIGURE_ARGS+=	--enable-klash
 .else
-CONFIGURE_ARGS+=	--disable-klash
-CONFIGURE_ARGS+=	--disable-plugin
+PLIST_SUBST+=		KDE=""
+.endif
+
+CONFIGURE_ARGS+=	--enable-gui=${GNASH_GUIS:tW:S/ /,/}
+
+###
+### Select renderers.
+###
+.if !empty(PKG_OPTIONS:Magg)
+GNASH_RENDERS+=		agg
+.include "../../graphics/agg/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mcairo)
+GNASH_RENDERS+=		cairo
+.include "../../graphics/cairo/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mopengl)
+GNASH_RENDERS+=		ogl
+.include "../../x11/glproto/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mgtk)
+.include "../../graphics/gtkglext/buildlink3.mk"
+.endif
+.endif
+
+CONFIGURE_ARGS+=	--enable-renderer=${GNASH_RENDERS:tW:S/ /,/}
+
+###
+### Select a media handler
+###
+.if !empty(PKG_OPTIONS:Mffmpeg)
+CONFIGURE_ARGS+=	--enable-media=ffmpeg
+.include "../../audio/SDL_mixer/buildlink3.mk"
+.include "../../devel/SDL/buildlink3.mk"
+.include "../../multimedia/ffmpeg-devel/buildlink3.mk"
+# is ffmpeg-devel buildlink broken? we need all that:
+.include "../../audio/faac/buildlink3.mk"
+.include "../../audio/lame/buildlink3.mk"
+.include "../../multimedia/libtheora/buildlink3.mk"
+.include "../../multimedia/libogg/buildlink3.mk"
+.include "../../audio/libvorbis/buildlink3.mk"
+.include "../../multimedia/x264-devel/buildlink3.mk"
+# --------------------------------------------------
+.endif
+
+.if !empty(PKG_OPTIONS:Mgstreamer)
+CONFIGURE_ARGS+=	--enable-media=gst --enable-gstreamer
+.include "../../multimedia/gstreamer0.10/buildlink3.mk"
+# see http://bjacques.org/gst-plugins
+DEPENDS+= gst-plugins0.10-base-[0-9]*:../../multimedia/gst-plugins0.10-base
+DEPENDS+= gst-plugins0.10-oss-[0-9]*:../../audio/gst-plugins0.10-oss
+DEPENDS+= gst-fluendo-mp3-0.10.[0-9]*:../../audio/gst-plugins0.10-fluendo-mp3
+DEPENDS+= gst-ffmpeg-0.10.[0-9]*:../../multimedia/gst-plugins0.10-ffmpeg
+DEPENDS+= gst-plugins0.10-gnomevfs-[0-9]*:../../sysutils/gst-plugins0.10-gnomevfs
+DEPENDS+= gst-plugins0.10-x11-[0-9]*:../../x11/gst-plugins0.10-x11
+.endif
+
+###
+### MIT-SHM Support.
+###
+.if !empty(PKG_OPTIONS:Mmitshm)
+CONFIGURE_ARGS+=	--enable-mit-shm
+.else
+CONFIGURE_ARGS+=	--disable-mit-shm
 .endif
