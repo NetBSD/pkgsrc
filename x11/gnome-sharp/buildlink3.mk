6309c31397f6f1c7363300d80b67caa8259f6a57
wiz <wiz>
2008-04-27 23:03:09 +0000

    Update to 2.20.1.
    Parts were split off into a separate package which I'll commit shortly,
    gnome-desktop-sharp.
    
    Changes:
    * bootstrap-2.20: bump version.
    * bootstrap-generic: substitute the GTK_SHARP_REQUIRED_VERSION.
    * bootstrap-2.20: check for gtk 2.12.0 and gtk-sharp 2.12.1.
    * configure.in.in: separate the gtk+ and gtk-sharp version variables.
    * gnome/Program.custom: use GLib.Object.Harden to avoid some
    reflection.
    * bootstrap-2.20: bump gtk-sharp requirement to 2.12.1.
    * gnome/Program.custom: revert QuitPrepare hook and instead do an
    obnoxious hack to disable the toggle_ref mechanism for GnomeProgram.
    Uses reflection since I didn't want to expose this hack in the public
    GLib.Object API.
    * gnome/Program.custom: hook into Gtk.Application.QuitPrepare to
    dispose our managed resources prior to invoking gtk_main_quit.  Avoids
    a crash on exit() in toggle_ref usage after managed releases have been
    released.
    * bootstrap-2.20: bump version and tag.
    * gnome/gnome-api-2.20.raw: reparse
    * sources/gnome-sharp-2.20-sources.xml: hide private gailcanvas hdrs.
    hide gnome-print-gdi.h.
    * bootstrap-2.20: bump svn version.
    * gnome/Gnome.metadata: hide new Global class which was hiding
    stable API.  [Fixes #356649]
    * configure.in.in: integrate build service patch from Wade.
    * bootstrap-2.20: update to version 2.19.90.
    * * Move to 2.20 API.
    * configure.in.in: move vte, rsvg, and gtkhtml to gnome-desktop-sharp.
    * Makefile.am: ditto
    * sample/Makefile.am: ditto
    * Makefile.include: some refinement of the variable mangling.
    * configure.in.in: expand new pc.in.
    * gconf/GConf/gconf-sharp-2.0.pc.in: remove gnome-sharp Requires and
    peditors.dll from the Libs: var.
    * gconf/GConf.PropertyEditors/Makefile.am: install new pc.
    * gconf/GConf.PropertyEditors/gconf-sharp-peditors-2.0.pc.in: expose
    new pc file since this is conditional and the gconf-sharp.dll build
    is not.
    * gconf/GConf/Makefile.am: Copy AssemblyInfo.cs to the current dir so
    that csc can find it.  (csc doesn't understand ../..)
    * gconf/GConf/Makefile.am:
    * gconf/GConf.PropertyEditors/Makefile.am:
    use top_builddir instead of .. to avoid win32 build issues.
    * gconf/GConf/Makefile.am: remove GNOME condition.  It's only needed
    on the peditor build.
    * gnome/CanvasItem.custom: marshal GType as IntPtr.
    [Fixes comment #8 of 327058]
    * gconf/GConf/Value.cs: Deal with zero length arrays gracefully.
    Thanks to Sebastian Dröge <slomo@circular-chaos.org> for a
    patch this one was derived from.  [Fixes #325516]
    * AssembyInfo.cs.in : add [IgnoreClassInitializers]
    * vte/Vte.metadata : rule from Peter Johanson to mark a string
    const.
    * gnome/CanvasPathDef.custom : adapt to generated pinvoke sig change.
    * configure.in.in : add blurb about the gnome-sharp.dll requirements
    since it seems to be a FAQ for people trying to build from sources.
    * bootstrap-generic : use automake --foreign to keep automake-1.10 from
    blowing up on the gnu make function usage.  grrr.
    * sample/gconf/Makefile.am : use install-data-hook to make 1.10 happy.
    * gconf/tools/gconfsharp2-schemagen.in:
    Revert unportable 'relocatable' wrapper using which (doesn't work with symlinks)
    * gconf/GConf/Value.cs: Allow the storage of empty lists in
    gconf-sharp.

--- x11/gnome-sharp/buildlink3.mk
+++ x11/gnome-sharp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2008/03/17 22:54:02 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2008/04/27 23:03:09 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_SHARP_BUILDLINK3_MK:=	${GNOME_SHARP_BUILDLINK3_MK}+
@@ -14,22 +14,17 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gnome-sharp
 .if ${GNOME_SHARP_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.gnome-sharp+=	gnome-sharp>=2.16.1
 BUILDLINK_ABI_DEPENDS.gnome-sharp?=	gnome-sharp>=2.16.1nb1
-BUILDLINK_PKGSRCDIR.gnome-sharp?=	../../x11/gnome-sharp
+BUILDLINK_PKGSRCDIR.gnome-sharp?=	../../zzz/gnome-sharp
 .endif	# GNOME_SHARP_BUILDLINK3_MK
 
-.include "../../devel/libglade/buildlink3.mk"
 .include "../../devel/libgnomeui/buildlink3.mk"
 .include "../../graphics/libart/buildlink3.mk"
-.include "../../graphics/librsvg/buildlink3.mk"
 .include "../../lang/mono/buildlink3.mk"
 .include "../../lang/monodoc/buildlink3.mk"
 .include "../../print/libgnomeprintui/buildlink3.mk"
 .include "../../sysutils/gnome-vfs/buildlink3.mk"
-.include "../../textproc/libxml2/buildlink3.mk"
-.include "../../www/gtkhtml38/buildlink3.mk"
 .include "../../x11/gnome-panel/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .include "../../x11/gtk-sharp/buildlink3.mk"
-.include "../../x11/vte/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
