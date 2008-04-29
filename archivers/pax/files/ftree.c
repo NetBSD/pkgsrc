15a93bcc7f8633674bf86f5ad48d6c7972d3958b
martin <martin>
2008-04-29 05:46:08 +0000

    Drop clauses 3 and 4 from TNF licenses

--- archivers/pax/files/ftree.c
+++ archivers/pax/files/ftree.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: ftree.c,v 1.11 2008/02/07 22:27:53 joerg Exp $	*/
+/*	$NetBSD: ftree.c,v 1.12 2008/04/29 05:46:09 martin Exp $	*/
 
 /*-
  * Copyright (c) 1992 Keith Muller.
@@ -48,13 +48,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
@@ -84,7 +77,7 @@
 #if 0
 static char sccsid[] = "@(#)ftree.c	8.2 (Berkeley) 4/18/94";
 #else
-__RCSID("$NetBSD: ftree.c,v 1.11 2008/02/07 22:27:53 joerg Exp $");
+__RCSID("$NetBSD: ftree.c,v 1.12 2008/04/29 05:46:09 martin Exp $");
 #endif
 #endif /* not lint */
 
--- net/tnftp/files/libnetbsd/sl_init.c
+++ net/tnftp/files/libnetbsd/sl_init.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: sl_init.c,v 1.4 2007/08/07 02:07:00 lukem Exp $	*/
+/*	$NetBSD: sl_init.c,v 1.5 2008/04/29 05:46:09 martin Exp $	*/
 /*	from	NetBSD: stringlist.c,v 1.10 2000/01/25 16:24:40 enami Exp	*/
 
 /*-
@@ -16,13 +16,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
