bba937d36f97a9fe2dad29af396358cefc212541
bjs <bjs@pkgsrc.org>
2008-05-06 15:43:03 +0000

    Use pkg-build-options.mk to include optional dependencies in buildlink3.mk.  I am currently fixing x11/nucleo, and without this pkg-config gets very upset.

--- multimedia/ffmpeg-devel/buildlink3.mk
+++ multimedia/ffmpeg-devel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2008/02/20 10:12:04 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2008/05/06 15:43:03 bjs Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 FFMPEG_DEVEL_BUILDLINK3_MK:=	${FFMPEG_DEVEL_BUILDLINK3_MK}+
@@ -14,7 +14,45 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}ffmpeg-devel
 .if ${FFMPEG_DEVEL_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.ffmpeg-devel+=	ffmpeg-devel>=20071004
 BUILDLINK_PKGSRCDIR.ffmpeg-devel?=	../../multimedia/ffmpeg-devel
+
+pkgbase := ffmpeg-devel
+.  include "../../mk/pkg-build-options.mk"
+
 .endif	# FFMPEG_DEVEL_BUILDLINK3_MK
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg-devel:Msdl)
+.  include "../../devel/SDL/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg-devel:Mtheora)
+.  include "../../multimedia/libtheora/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg-devel:Mxvid)
+.  include "../../multimedia/xvidcore/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg-devel:Mx264)
+.  include "../../multimedia/x264-devel/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg-devel:Mlame)
+.  include "../../audio/lame/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg-devel:Mvorbis)
+.  include "../../audio/libvorbis/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg-devel:Mfaad)
+.  include "../../audio/faad2/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg-devel:Mfaac)
+.  include "../../audio/faac/buildlink3.mk"
+.endif
 
 .include "../../devel/libgetopt/buildlink3.mk"
 
