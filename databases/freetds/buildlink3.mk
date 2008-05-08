9cc6be723e33b3bf0bf3b40cd2aa2dbd4282a0bf
jlam <jlam@pkgsrc.org>
2008-05-08 20:14:10 +0000

    Restore old BUILDLINK_API_DEPENDS value so we don't need to revbump packages.

--- databases/freetds/buildlink3.mk
+++ databases/freetds/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2008/05/08 13:28:39 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2008/05/08 20:14:10 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 FREETDS_BUILDLINK3_MK:=	${FREETDS_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}freetds
 
 .if !empty(FREETDS_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.freetds+=	freetds>=0.82
-BUILDLINK_ABI_DEPENDS.freetds?=	freetds>=0.82
+BUILDLINK_ABI_DEPENDS.freetds?=	freetds>=0.63nb4
 BUILDLINK_PKGSRCDIR.freetds?=	../../databases/freetds
 .endif	# FREETDS_BUILDLINK3_MK
 
