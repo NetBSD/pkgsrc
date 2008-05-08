95cb9201211695a545313f859f82bb87d5aad0ba
jlam <jlam@pkgsrc.org>
2008-05-08 13:28:39 +0000

    Update freetds to version 0.82.  Changes from version 0.63 include:
    Pkgsrc changes:
      + Support user-destdir installation.
      + Document the way that pkgsrc handles the freetds documentation.
      + Use GENERATE_PLIST instead of creating a .PLIST.doc.
      + Remove patch-ac and patch-ad due to reports of success compiling
        FreeTDS on Mac OS X without changes on the freetds@ mailing list.
    
    Package changes:
      - libtds.* has been removed.  Its objects are now linked directly
        into the other client libraries.
      - support TLSv1 sessions to MSSQL servers
      - fixed timeout handling
      - added protocol version discovery
      - reduced network bandwidth use on Linux and -BSD
      - fixed conversion NUMERIC->NUMERIC changing precision/scale
      - added named instance support (mssql2k)
      - many other code improvements and bug fixes
      - improved pool server
      - ODBC:
        - cursors (mssql)
        - fixed database setting
        - fixed SQLGetData result
        - fixed compute handling in ODBC
        - paramset support

--- databases/freetds/options.mk
+++ databases/freetds/options.mk
@@ -1,8 +1,9 @@
-# $NetBSD: options.mk,v 1.2 2008/04/12 22:42:59 jlam Exp $
+# $NetBSD: options.mk,v 1.3 2008/05/08 13:28:39 jlam Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.freetds
-PKG_OPTIONS_OPTIONAL_GROUPS+=	odbc
+PKG_OPTIONS_OPTIONAL_GROUPS+=	odbc tls
 PKG_OPTIONS_GROUP.odbc=		iodbc #unixodbc
+PKG_OPTIONS_GROUP.tls=		gnutls openssl
 
 .include "../../mk/bsd.options.mk"
 
@@ -31,4 +32,24 @@ PLIST.odbc=		yes
 
 .if empty(PKG_OPTIONS:Miodbc) && empty(PKG_OPTIONS:Munixodbc)
 CONFIGURE_ARGS+=	--disable-odbc
+.endif
+
+###
+### Use GNU TLS for TLSv1 encrypted session to MSSQL servers.
+###
+.if !empty(PKG_OPTIONS:Mgnutls)
+.include "../../security/gnutls/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-gnutls
+.else
+CONFIGURE_ARGS+=	--without-gnutls
+.endif
+
+###
+### Use OpenSSL for TLSv1 encrypted session to MSSQL servers.
+###
+.if !empty(PKG_OPTIONS:Mopenssl)
+.include "../../security/openssl/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-openssl=${BUILDLINK_PREFIX.openssl:Q}
+.else
+CONFIGURE_ARGS+=	--without-openssl
 .endif
