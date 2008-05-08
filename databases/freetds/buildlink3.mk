546f30a0b3cba66738aa15525c7292776305dd43
jlam <jlam@pkgsrc.org>
2008-05-08 13:28:39 +0000

    Update freetds to version 0.82.  Changes from version 0.63 include:
    Pkgsrc changes:
      + Support user-destdir installation.
      + Document the way that pkgsrc handles the freetds documentation.
      + Use GENERATE_PLIST instead of creating a .PLIST.doc.
      + Remove patch-ac and patch-ad due to reports of success compiling
        FreeTDS on Mac OS X without changes on the freetds@ mailing list.
    
    Package changes:
      - libtds.* has been removed.  Its objects are now linked directly
        into the other client libraries.
      - support TLSv1 sessions to MSSQL servers
      - fixed timeout handling
      - added protocol version discovery
      - reduced network bandwidth use on Linux and -BSD
      - fixed conversion NUMERIC->NUMERIC changing precision/scale
      - added named instance support (mssql2k)
      - many other code improvements and bug fixes
      - improved pool server
      - ODBC:
        - cursors (mssql)
        - fixed database setting
        - fixed SQLGetData result
        - fixed compute handling in ODBC
        - paramset support

--- databases/freetds/buildlink3.mk
+++ databases/freetds/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2006/07/08 23:10:39 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2008/05/08 13:28:39 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 FREETDS_BUILDLINK3_MK:=	${FREETDS_BUILDLINK3_MK}+
@@ -12,11 +12,22 @@ BUILDLINK_PACKAGES+=	freetds
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}freetds
 
 .if !empty(FREETDS_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.freetds+=	freetds>=0.63nb2
-BUILDLINK_ABI_DEPENDS.freetds?=	freetds>=0.63nb4
+BUILDLINK_API_DEPENDS.freetds+=	freetds>=0.82
+BUILDLINK_ABI_DEPENDS.freetds?=	freetds>=0.82
 BUILDLINK_PKGSRCDIR.freetds?=	../../databases/freetds
 .endif	# FREETDS_BUILDLINK3_MK
 
 .include "../../converters/libiconv/buildlink3.mk"
+
+pkgbase := freetds
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.freetds:Mgnutls)
+.  include "../../security/gnutls/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.freetds:Mopenssl)
+.  include "../../security/openssl/buildlink3.mk"
+.endif
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
