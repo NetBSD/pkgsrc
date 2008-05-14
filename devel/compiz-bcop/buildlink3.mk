341a0f25d5ace615990706ad504fe31c98c14083
drochner <drochner@pkgsrc.org>
2008-05-14 16:40:20 +0000

    -make this a build dependency by default, if is used to generate source  code during the build (if at all - currently it is unused unless  someone patches xml metadata in compiz plugin sources) -it needs bash and GNU getopt to work correctly

--- devel/compiz-bcop/buildlink3.mk
+++ devel/compiz-bcop/buildlink3.mk
@@ -1,6 +1,8 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/05/12 12:40:32 jmcneill Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/05/14 16:40:20 drochner Exp $
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BUILDLINK_DEPMETHOD.compiz-bcop?=	build
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 COMPIZ_BCOP_BUILDLINK3_MK:=	${COMPIZ_BCOP_BUILDLINK3_MK}+
 
 .if ${BUILDLINK_DEPTH} == "+"
@@ -16,6 +18,4 @@ BUILDLINK_API_DEPENDS.compiz-bcop+=	compiz-bcop>=0.6.0
 BUILDLINK_PKGSRCDIR.compiz-bcop?=	../../devel/compiz-bcop
 .endif	# COMPIZ_BCOP_BUILDLINK3_MK
 
-.include "../../wm/compiz/buildlink3.mk"
-
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
