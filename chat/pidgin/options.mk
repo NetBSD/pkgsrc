252bd84764e1b6c11f2fa8d771007c065cf7aec4
tnn <tnn>
2008-05-17 23:45:47 +0000

    Update to pidgin-2.4.2.
    * The typing notification in the conversation history can be disabled or
      customized (font, color etc.) in .gtkrc-2.0.
    * Added a plugin (not installed by default) which adds a Send button back to
      the conversation window. People without physical keyboards have a hard time
      with the lack of the button.
    * Clicking on the buddyicon in the conversation window toggles the size of the
      icon between small and large.
    * The settings of a chat (e.g. Handle in an XMPP chat, or Exchange in an AIM
      chat) can be edited from its context menu in the buddy list.
    * Add a "Present conversation window" preference to the Message Notification
      plugin; the "Raise conversation window" option does not unminimize windows
      or draw attention to them when they are on other workspaces--the "Present"
      option should.
    * Add a preference to set Escape as the keyboard shortcut for closing the
      conversation window.
    * Add an option in the context menu to disable smileys in the selected text
      in the conversation history/log viewer. This should help people who
      regularly paste code in conversations.
    * Add a preference to choose the minimum size of the text input area in lines.
    * Moved the "Local alias" field in the Modify Account dialog to be below the
      "User Options" heading on the "Basic" tab.
    * Number of room occupants is now shown in chat tooltips where possible

--- chat/pidgin/options.mk
+++ chat/pidgin/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2008/04/29 12:35:10 jmmv Exp $
+# $NetBSD: options.mk,v 1.4 2008/05/17 23:45:47 tnn Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.pidgin
 PKG_SUPPORTED_OPTIONS+=		dbus debug gstreamer gtkspell
@@ -13,24 +13,20 @@ CONFIGURE_ARGS+=	--enable-dbus
 PLIST.dbus=		yes
 .  include "../../sysutils/dbus/buildlink3.mk"
 .  include "../../sysutils/dbus-glib/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-dbus
 .endif
 
 .if !empty(PKG_OPTIONS:Mgtkspell)
+CONFIGURE_ARGS+=	--enable-gtkspell
 .  include "../../textproc/gtkspell/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-gtkspell
 .endif
 
 .if !empty(PKG_OPTIONS:Mgstreamer)
+CONFIGURE_ARGS+=	--enable-gstreamer
 .  include "../../multimedia/gstreamer0.10/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-gstreamer
 .endif
 
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+= --enable-debug
-.else
-CONFIGURE_ARGS+= --disable-debug
 .endif
