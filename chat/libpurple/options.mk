8de5eb7bf925e5d03a2be04ae7aacd812bc72a9f
tnn <tnn>
2008-05-17 23:26:06 +0000

    Aggressively disable options in Makefile.common and selectively enable them in options.mk, through overrides.

--- chat/libpurple/options.mk
+++ chat/libpurple/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2008/04/12 22:42:58 jlam Exp $
+# $NetBSD: options.mk,v 1.8 2008/05/17 23:26:06 tnn Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.libpurple
 PKG_SUPPORTED_OPTIONS+=		gnutls perl tcl debug dbus sasl
@@ -9,13 +9,13 @@ PKG_SUGGESTED_OPTIONS+=		gnutls
 PLIST_VARS+=		dbus
 
 .if !empty(PKG_OPTIONS:Mgnutls)
-CONFIGURE_ARGS+=	--disable-nss
+CONFIGURE_ARGS+=	--enable-gnutls
 CONFIGURE_ARGS+= --with-gnutls-includes=${BUILDLINK_PREFIX.gnutls}/include
 CONFIGURE_ARGS+= --with-gnutls-libs=${BUILDLINK_PREFIX.gnutls}/lib
 
 .  include "../../security/gnutls/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=	--disable-gnutls
+CONFIGURE_ARGS+=	--enable-nss
 CONFIGURE_ARGS+= --with-nspr-includes=${BUILDLINK_PREFIX.nspr}/include/nspr
 CONFIGURE_ARGS+= --with-nspr-libs=${BUILDLINK_PREFIX.nspr}/lib/nspr
 CONFIGURE_ARGS+= --with-nss-includes=${BUILDLINK_PREFIX.nss}/include/nss/nss
@@ -25,17 +25,14 @@ CONFIGURE_ARGS+= --with-nss-libs=${BUILDLINK_PREFIX.nss}/lib/nss
 .endif
 
 .if !empty(PKG_OPTIONS:Mperl)
+CONFIGURE_ARGS+=	--enable-perl
 USE_TOOLS+=		perl:run
 .  include "../../lang/perl5/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-perl
 .endif
 
 .if !empty(PKG_OPTIONS:Mtcl)
 CONFIGURE_ARGS+=	--with-tclconfig=${BUILDLINK_PREFIX.tcl}/lib
 .  include "../../lang/tcl/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-tcl
 .endif
 
 .if !empty(PKG_OPTIONS:Mdbus)
@@ -50,19 +47,13 @@ REPLACE_PYTHON+=	libpurple/purple-url-handler
 .  include "../../sysutils/dbus/buildlink3.mk"
 .  include "../../sysutils/dbus-glib/buildlink3.mk"
 .  include "../../lang/python/application.mk"
-.else
-CONFIGURE_ARGS+=	--disable-dbus
 .endif
 
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-debug
-.else
-CONFIGURE_ARGS+=	--disable-debug
 .endif
 
 .if !empty(PKG_OPTIONS:Msasl)
 CONFIGURE_ARGS+=	--enable-cyrus-sasl
 .  include "../../security/cyrus-sasl/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-cyrus-sasl
 .endif
