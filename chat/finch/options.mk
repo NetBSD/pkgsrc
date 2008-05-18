5737418a9ab48fc768b08cc4932e783c7e60aad8
tnn <tnn@pkgsrc.org>
2008-05-18 00:06:46 +0000

    Update to finch-2.4.2.
    * New default binding ctrl+x to open context menus.
    * Menu triggers and other bindings will no longer conflict.
    * Middle click pastes the internal clipboard (when mouse support is enabled).

--- chat/finch/options.mk
+++ chat/finch/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2007/08/04 14:01:01 tnn Exp $
+# $NetBSD: options.mk,v 1.3 2008/05/18 00:06:46 tnn Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.finch
 PKG_SUPPORTED_OPTIONS+=		gstreamer debug
@@ -8,12 +8,8 @@ PKG_SUGGESTED_OPTIONS+=
 
 .if !empty(PKG_OPTIONS:Mgstreamer)
 .  include "../../multimedia/gstreamer0.10/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-gstreamer
 .endif
 
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-debug
-.else
-CONFIGURE_ARGS+=	--disable-debug
 .endif
