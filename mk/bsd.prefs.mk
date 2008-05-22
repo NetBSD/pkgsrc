83290ab7333152fa5cd843c3063d6ac13c3be953
tnn <tnn@pkgsrc.org>
2008-05-22 15:48:13 +0000

    Daniel Horecki pointed out on irc that all USE_INET6 references have been purged from pkgsrc-wip, so remove the support code for that.

--- mk/bsd.prefs.mk
+++ mk/bsd.prefs.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.prefs.mk,v 1.281 2008/04/16 12:28:48 tnn Exp $
+# $NetBSD: bsd.prefs.mk,v 1.282 2008/05/22 15:48:13 tnn Exp $
 #
 # This file includes the mk.conf file, which contains the user settings.
 #
@@ -461,11 +461,6 @@ IPV6_READY=		YES
 .else
 IPV6_READY=		NO
 .endif
-
-# XXX
-# XXX Retain the following until USE_INET6 has been purged from pkgsrc-wip.
-# XXX
-USE_INET6?=		${IPV6_READY}
 
 LOCALBASE?=		/usr/pkg
 X11_TYPE?=		native
