10b162a93e2aa25a9fdf4960490ec694f0db97fa
joerg <joerg>
2008-05-23 15:57:04 +0000

    Merge license changes from the HEAD.

--- pkgtools/pkg_install/files/admin/check.c
+++ pkgtools/pkg_install/files/admin/check.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: check.c,v 1.2.4.1 2008/05/23 15:51:22 joerg Exp $	*/
+/*	$NetBSD: check.c,v 1.2.4.2 2008/05/23 15:57:04 joerg Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -8,7 +8,7 @@
 #include <sys/cdefs.h>
 #endif
 #ifndef lint
-__RCSID("$NetBSD: check.c,v 1.2.4.1 2008/05/23 15:51:22 joerg Exp $");
+__RCSID("$NetBSD: check.c,v 1.2.4.2 2008/05/23 15:57:04 joerg Exp $");
 #endif
 
 /*-
@@ -26,13 +26,6 @@ __RCSID("$NetBSD: check.c,v 1.2.4.1 2008/05/23 15:51:22 joerg Exp $");
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
