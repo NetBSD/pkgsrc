94e23cdce317bec3c1d7f9526c68a6344341147e
tnn <tnn@pkgsrc.org>
2008-05-24 03:35:38 +0000

    Import compat_headers-0.1 as pkgtools/compat_headers. This package attempts to resolve deficiencies in system headers by providing missing types, definitions and prototypes in shadow headers. New in this version: - Rewrote all of the tests to use autoconf. - Implemented tests and support for more standard integer types.

---
+++ pkgtools/compat_headers/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/05/24 03:35:38 tnn Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+COMPAT_HEADERS_BUILDLINK3_MK:=	${COMPAT_HEADERS_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	compat_headers
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ncompat_headers}
+BUILDLINK_PACKAGES+=	compat_headers
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}compat_headers
+
+.if !empty(COMPAT_HEADERS_BUILDLINK3_MK:M+)
+BUILDLINK_API_DEPENDS.compat_headers+=	compat_headers>=0.1
+BUILDLINK_ABI_DEPENDS.compat_headers+=	compat_headers>=0.1
+BUILDLINK_PKGSRCDIR.compat_headers?=	../../pkgtools/compat_headers
+BUILDLINK_DEPMETHOD.compat_headers?=	build
+.endif	# COMPAT_HEADERS_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
