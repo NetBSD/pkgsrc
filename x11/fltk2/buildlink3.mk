82410ab9b1df2d7fc3436af656f93492db4f7ccc
joerg <joerg>
2008-05-25 15:29:19 +0000

    Import build rules for fltk2. This is currently static only.

---
+++ x11/fltk2/buildlink3.mk
@@ -0,0 +1,30 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/05/25 15:29:19 joerg Exp $
+
+BUILDLINK_DEPMETHOD.fltk2?=	build
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+FLTK2_BUILDLINK3_MK:=	${FLTK2_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	fltk2
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nfltk2}
+BUILDLINK_PACKAGES+=	fltk2
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}fltk2
+
+.if ${FLTK2_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.fltk2+=	fltk2>=2.0pre6129
+BUILDLINK_PKGSRCDIR.fltk2?=	../../x11/fltk2
+.endif	# FLTK2_BUILDLINK3_MK
+
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXft/buildlink3.mk"
+.include "../../x11/libXi/buildlink3.mk"
+.include "../../x11/libXinerama/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
