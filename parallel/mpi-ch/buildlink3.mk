30e20dfa08a4c46f23da1d3dccf8afcc055fae93
tnn <tnn>
2008-05-29 20:27:47 +0000

    prune wip reference

--- parallel/mpi-ch/buildlink3.mk
+++ parallel/mpi-ch/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2008/05/29 20:25:53 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2008/05/29 20:27:47 tnn Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 MPICH2_BUILDLINK3_MK:=	${MPICH2_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}mpich2
 
 .if !empty(MPICH2_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.mpich2+=	mpich2>=1.0.7
-BUILDLINK_PKGSRCDIR.mpich2?=	../../wip/mpi-ch
+BUILDLINK_PKGSRCDIR.mpich2?=	../../parallel/mpi-ch
 BUILDLINK_DEPMETHOD.mpich2?=	build
 .endif	# MPICH2_BUILDLINK3_MK
 
