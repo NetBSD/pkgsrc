f5b441cc24906f5cf48ae1635383cb014b287f0c
wiz <wiz@pkgsrc.org>
2008-05-30 12:28:42 +0000

    Update to 2.0.13:
    version 2.0.13 (2008-05-20):
    	* Additional translations (see po/ChangeLog).
    	* Enchant support (support for pspell/aspell dropped) (Dom Lachowicz)
    
    version 2.0.12 (2008-05-09):
    	* Make debug, quiet and build_suggestion_menu static in gtkspell.c
    	  to prevent exporting them.
    	* One additional translation (see po/ChangeLog).
    	* Move spell-checking library from Libs to Libs.private in
    	  gtkspell-2.0.pc
    	* Update autogen.sh to use intltool
    	* Fix warnings with -Wstrict-prototypes by Richard Laager
    
    Bump API for dependency changes.

--- textproc/gtkspell/buildlink3.mk
+++ textproc/gtkspell/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/07/08 23:11:10 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2008/05/30 12:28:42 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GTKSPELL_BUILDLINK3_MK:=	${GTKSPELL_BUILDLINK3_MK}+
@@ -12,12 +12,11 @@ BUILDLINK_PACKAGES+=	gtkspell
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gtkspell
 
 .if !empty(GTKSPELL_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.gtkspell+=	gtkspell>=2.0.2nb6
-BUILDLINK_ABI_DEPENDS.gtkspell+=	gtkspell>=2.0.11nb3
+BUILDLINK_API_DEPENDS.gtkspell+=	gtkspell>=2.0.13
 BUILDLINK_PKGSRCDIR.gtkspell?=	../../textproc/gtkspell
 .endif	# GTKSPELL_BUILDLINK3_MK
 
-.include "../../textproc/aspell/buildlink3.mk"
+.include "../../textproc/enchant/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
