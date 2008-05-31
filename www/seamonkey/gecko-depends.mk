6a10767fe0c670fc5aec996f97b48a9e2f723690
tnn <tnn@pkgsrc.org>
2008-05-31 23:02:40 +0000

    Tie down the firefox API dependency for the firefox option to <= 2.99. "gtkmozembed" is deprecated and not shipped with firefox 3.0+ Some packages that need firefox (like www/epiphany?) can be configured to use libxul instead. For those packages that support it, it might be a good idea to add a firefox3 option when we get closer to firefox 3 launch day.

--- www/seamonkey/gecko-depends.mk
+++ www/seamonkey/gecko-depends.mk
@@ -1,8 +1,9 @@
-# $NetBSD: gecko-depends.mk,v 1.1 2006/09/24 16:21:33 salo Exp $
+# $NetBSD: gecko-depends.mk,v 1.2 2008/05/31 23:02:40 tnn Exp $
 
 .if !empty(PKG_OPTIONS:Mfirefox)
 # needs .pc files
 BUILDLINK_API_DEPENDS.firefox+=		firefox>=1.5nb2
+BUILDLINK_API_DEPENDS.firefox+=		firefox<=2.99
 .include "../../www/firefox/buildlink3.mk"
 .else
 # needs .pc files
