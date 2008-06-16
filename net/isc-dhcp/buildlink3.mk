624c90a92f032382e0106f474d46837cf247859b
joerg <joerg@pkgsrc.org>
2008-06-16 14:52:58 +0000

    Make sure that DHCP 3 packages only pull in v3, not v4. Bump revisions.

--- net/isc-dhcp/buildlink3.mk
+++ net/isc-dhcp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2007/12/13 02:54:49 taca Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2008/06/16 14:52:58 joerg Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 ISC_DHCP_BASE_BUILDLINK3_MK:=	${ISC_DHCP_BASE_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	isc-dhcp-base
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}isc-dhcp-base
 
 .if ${ISC_DHCP_BASE_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.isc-dhcp-base+=	isc-dhcp-base>=3.1.0
+BUILDLINK_API_DEPENDS.isc-dhcp-base+=	isc-dhcp-base>=3.1.0<4
 BUILDLINK_PKGSRCDIR.isc-dhcp-base?=	../../net/isc-dhcp
 .endif	# ISC_DHCP_BASE_BUILDLINK3_MK
 
