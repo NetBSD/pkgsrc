9a1aedb83ef00a40335a72f691a71438e53551af
abs <abs@pkgsrc.org>
2008-06-17 00:00:30 +0000

    Upgrade wm/jwm to 2.0.1, plus add fribidi and jpeg options (default on) # JWM v2.0 <> 20070624
        * New look.
        * Background support on a per-desktop basis.
        * Veritcal/horizontal maximization support.
        * Added the ability to move windows with the pager.
        * Many bug fixes.
    
    # JWM v2.0.1 <> 20070704
    
        * Fix a file descriptor leak when JPEG is enabled.
        * Give swallow windows time to map when restarting.

--- wm/jwm/options.mk
+++ wm/jwm/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2007/02/22 19:27:18 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2008/06/17 00:00:30 abs Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.jwm
-PKG_SUPPORTED_OPTIONS=	debug png
-PKG_SUGGESTED_OPTIONS=	png
+PKG_SUPPORTED_OPTIONS=	debug fribidi jpeg png
+PKG_SUGGESTED_OPTIONS=	fribidi jpeg png
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,10 +10,16 @@ PKG_SUGGESTED_OPTIONS=	png
 CONFIGURE_ARGS+=	--enable-debug
 .endif
 
+.if !empty(PKG_OPTIONS:Mfribidi)
+.include "../../converters/fribidi/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mjpeg)
+.include "../../graphics/jpeg/buildlink3.mk"
+.endif
+
 .if !empty(PKG_OPTIONS:Mpng)
 .include "../../graphics/png/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-png
 .endif
-
-# TODO: fribidi
