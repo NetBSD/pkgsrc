0da059767e54004b3bee5d1f84afbbcfc35bca80
bjs <bjs@pkgsrc.org>
2008-06-19 01:56:44 +0000

    - Fix support for NetBSD's atomic ops. - Fix up some inconsistent handing of CONFIGURE_ARGS (mostly related   to the debug option). - Add test target (commented out, see Makefile). - Add some patches from freedesktop.org git (see comments atop new patches). - Remove trailing comma from an enum in tools/dbus-monitor.c.
    Passes all tests on NetBSD/amd64 4.99.65, save for spawn test, which seems
    to hang (?).  I'm also running GNOME now and everything seems to be in order.

--- sysutils/dbus/options.mk
+++ sysutils/dbus/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2008/06/13 11:28:35 drochner Exp $
+# $NetBSD: options.mk,v 1.2 2008/06/19 01:56:44 bjs Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dbus
 PKG_SUPPORTED_OPTIONS+=	debug kqueue nox11
@@ -14,6 +14,8 @@ PKG_SUGGESTED_OPTIONS+=	kqueue
 
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS.enable+=	asserts tests verbose-mode
+.else
+CONFIGURE_ARGS.disable+= asserts tests verbose-mode
 .endif
 
 .if !empty(PKG_OPTIONS:Mkqueue)
