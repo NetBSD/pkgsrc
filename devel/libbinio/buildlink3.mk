b04bcf5b903bb4f4e4750be6ee0fa29c53255312
joerg <joerg@pkgsrc.org>
2008-06-19 16:30:45 +0000

    Don't set build time dependency for a library that is very likely used via dynamic linkage. Fixes run time linkage of adplug. Bump revision of the latter.

--- devel/libbinio/buildlink3.mk
+++ devel/libbinio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/09/10 15:50:11 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/06/19 16:30:45 joerg Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBBINIO_BUILDLINK3_MK:=	${LIBBINIO_BUILDLINK3_MK}+
@@ -14,7 +14,6 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libbinio
 .if ${LIBBINIO_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.libbinio+=	libbinio>=1.4
 BUILDLINK_PKGSRCDIR.libbinio?=		../../devel/libbinio
-BUILDLINK_DEPMETHOD.libbinio?=		build
 .endif	# LIBBINIO_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
