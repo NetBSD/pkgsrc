8b69ebd6ed04b2101e0cb98dc2bcf530c4a7d48d
taca <taca@pkgsrc.org>
2008-06-22 03:31:52 +0000

    Fix BUILDLINK_API_DEPENDS.ruby18-rcairo.  Therei is no ruby-rcairo package version 1.6.0 or later but 1.2.0 and later.

--- graphics/ruby-rcairo/buildlink3.mk
+++ graphics/ruby-rcairo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/07/23 16:57:25 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/06/22 03:31:52 taca Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 
@@ -13,7 +13,7 @@ BUILDLINK_PACKAGES+=	${RUBY_PKGPREFIX}-rcairo
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}${RUBY_PKGPREFIX}-rcairo
 
 .if ${RUBY_RCAIRO_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.ruby18-rcairo+=	${RUBY_PKGPREFIX}-rcairo>=1.2.0
+BUILDLINK_API_DEPENDS.ruby18-rcairo+=	${RUBY_PKGPREFIX}-rcairo>=1.6.0
 BUILDLINK_PKGSRCDIR.ruby18-rcairo?=	../../graphics/ruby-rcairo
 .endif	# RUBY_RCAIRO_BUILDLINK3_MK
 
