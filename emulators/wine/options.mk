32a73bc5cc291e149abbb822506235a405fbc50b
dholland <dholland@pkgsrc.org>
2008-06-22 23:38:18 +0000

    Update emulators/wine to 1.0, as requested in PR pkg/38985.
    Add missing dep on libxml2.
    
    Also merge some stuff from wip/wine:
      - more options (esound, ldap, dbus, ssl, x11)
      - split PLIST to accomodate extra files on Linux and FreeBSD (PR pkg/36320)
    
    This also fixes the broken build against current openldap. (PR pkg/38025)

--- emulators/wine/options.mk
+++ emulators/wine/options.mk
@@ -1,18 +1,64 @@
-# $NetBSD: options.mk,v 1.8 2008/04/12 22:43:00 jlam Exp $
+# $NetBSD: options.mk,v 1.9 2008/06/22 23:38:18 dholland Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.wine
-PKG_SUPPORTED_OPTIONS=	cups opengl sane
-PKG_SUGGESTED_OPTIONS=	opengl
+PKG_SUPPORTED_OPTIONS=	cups opengl sane esound ldap dbus ssl x11
+PKG_SUGGESTED_OPTIONS=	opengl esound ldap dbus ssl x11
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		opengl
+PLIST_VARS+=		opengl x11
+
+.if !empty(PKG_OPTIONS:Mesound)
+.include "../../audio/esound/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mldap)
+.include "../../databases/openldap-client/buildlink3.mk"
+.else
+CONFIGURE_ARGS+= --without-ldap
+.endif
+
+.if !empty(PKG_OPTIONS:Mdbus)
+.include "../../sysutils/dbus/buildlink3.mk"
+.else
+CONFIGURE_ARGS+= --without-dbus
+.endif
+
+.if !empty(PKG_OPTIONS:Mssl)
+.include "../../security/openssl/buildlink3.mk"
+.else
+CONFIGURE_ARGS+= --without-openssl
+.endif
 
 .if !empty(PKG_OPTIONS:Mcups)
 .include "../../print/cups/buildlink3.mk"
+.else
+CONFIGURE_ARGS+= --without-cups
 .endif
 
-.if !empty(PKG_OPTIONS:Mopengl)
+.if !empty(PKG_OPTIONS:Mx11)
+PLIST.x11=		yes
+.include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/lcms/buildlink3.mk"
+.include "../../graphics/libungif/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../x11/libICE/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXcursor/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXi/buildlink3.mk"
+.include "../../x11/libXinerama/buildlink3.mk"
+.include "../../x11/libXrandr/buildlink3.mk"
+.include "../../x11/libXrender/buildlink3.mk"
+.include "../../x11/libXxf86vm/buildlink3.mk"
+.else
+CONFIGURE_ARGS+= --without-x11
+.endif
+
+.if !empty(PKG_OPTIONS:Mopengl) && !empty(PKG_OPTIONS:Mx11)
 PLIST.opengl=		yes
 .include "../../graphics/glu/buildlink3.mk"
 .include "../../graphics/glut/buildlink3.mk"
