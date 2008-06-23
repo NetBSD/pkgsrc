2a9820f8c134a9ae069a9be7d423cfb07fafdb4c
spz <spz@pkgsrc.org>
2008-06-23 09:10:53 +0000

    - bump inn version to 2.4.4 - major change of directory structure - two new options (uucp and perl now both optional) - change of maintainer

--- news/inn/files/innd.sh
+++ news/inn/files/innd.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
-# $NetBSD: innd.sh,v 1.13 2002/09/29 10:39:36 jlam Exp $
+# $NetBSD: innd.sh,v 1.14 2008/06/23 09:10:53 spz Exp $
 #
 # PROVIDE: inn
 # REQUIRE: DAEMON
 # KEYWORD: shutdown
 
-if [ -x @PREFIX@/inn/bin/rc.news -a -s @INN_DATA_DIR@/db/active ]
+if [ -x @INN_PATHBIN@/rc.news -a -s @INN_DATA_DIR@/db/active ]
 then
 	if [ ! -f @PREFIX@/etc/nntp/server ]
 	then
@@ -25,18 +25,18 @@ then
 	if [ $# -eq 0 ]
 	then
 		echo -n ' innd'
-		su news -c "@PREFIX@/inn/bin/rc.news start" >/dev/null
+		su news -c "@INN_PATHBIN@/rc.news start" >/dev/null
 		exit 0
 	fi
 
 	case "$1" in
 	start )
 		echo "Starting INN."
-		su news -c "@PREFIX@/inn/bin/rc.news $1" >/dev/null
+		su news -c "@INN_PATHBIN@/rc.news $1" >/dev/null
 		exit 0
 		;;
 	stop )
-		su news -c "@PREFIX@/inn/bin/rc.news $1"
+		su news -c "@INN_PATHBIN@/rc.news $1"
 		exit 0
 		;;
 	restart )
