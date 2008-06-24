112778c66adb10f9125d6c30ce6aac0a6cfc2a46
wiz <wiz@pkgsrc.org>
2008-06-24 13:03:29 +0000

    Fix _two_ bugs in obviously untested previous commit.

--- mk/bsd.pkg.readme.mk
+++ mk/bsd.pkg.readme.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.pkg.readme.mk,v 1.21 2008/06/23 01:38:54 abs Exp $
+# $NetBSD: bsd.pkg.readme.mk,v 1.22 2008/06/24 13:03:29 wiz Exp $
 #
 # This Makefile fragment is included by bsd.pkg.mk and encapsulates the
 # code to produce README.html files in each package directory.
@@ -327,10 +327,10 @@ print-summary-data:
 	else								\
 		${ECHO} "notfor ${PKGPATH} not ${NOT_FOR_PLATFORM}";	\
 	fi;
-	if [ -n "${OWNER}" ]; then
-		@${ECHO} "owner ${PKGPATH} ${OWNER}";			\
+	if [ -n "${OWNER}" ]; then					\
+		${ECHO} "owner ${PKGPATH} ${OWNER}";			\
 	else								\
-		@${ECHO} "maintainer ${PKGPATH} ${MAINTAINER}";		\
+		${ECHO} "maintainer ${PKGPATH} ${MAINTAINER}";		\
 	fi
 	@${ECHO} "categories ${PKGPATH} ${CATEGORIES}"
 	@if [ -f ${DESCR_SRC} ]; then					\
