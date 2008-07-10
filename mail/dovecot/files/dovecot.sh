7ab34c321727765da28c42bfd851cd85c72a154a
jnemeth <jnemeth>
2008-07-10 00:54:30 +0000

    bump default file descriptor limit as it is too low on NetBSD

--- mail/dovecot/files/dovecot.sh
+++ mail/dovecot/files/dovecot.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: dovecot.sh,v 1.3 2008/06/22 17:41:23 ghen Exp $
+# $NetBSD: dovecot.sh,v 1.4 2008/07/10 00:54:30 jnemeth Exp $
 #
 
 # PROVIDE: dovecot
@@ -13,6 +13,20 @@ rcvar=$name
 command="@PREFIX@/sbin/${name}"
 required_files="@PKG_SYSCONFDIR@/$name.conf"
 extra_commands="reload"
+
+dovecot_fdlimit=${dovecot_fdlimit-"768"}
+
+# A default limit of 64 (at least on NetBSD) may be too low for many people
+SOFT_FDLIMIT=`ulimit -S -n`
+HARD_FDLIMIT=`ulimit -H -n`
+
+if [ ${dovecot_fdlimit} -gt ${SOFT_FDLIMIT} ]; then
+  if [ ${dovecot_fdlimit} -le ${HARD_FDLIMIT} ]; then
+    ulimit -S -n ${dovecot_fdlimit}
+  else
+    ulimit -S -n ${HARD_FDLIMIT}
+  fi
+fi
 
 load_rc_config $name
 run_rc_command "$1"
