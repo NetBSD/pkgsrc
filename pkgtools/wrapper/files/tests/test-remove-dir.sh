9d730eec5e45f1be6c10d1dbf19d6f2fad88f707
schmonz <schmonz>
2008-07-12 04:49:17 +0000

    Initial import of my GSoC project to replace mk/wrapper with faster, more maintainable C code. No code yet, just a pair of tests from regress/buildlink-transform ported to ATF.

---
+++ pkgtools/wrapper/files/tests/test-remove-dir.sh
@@ -0,0 +1,13 @@
+atf_test_case removedir
+removedir_head() {
+    atf_set 'descr' 'Checks that undesirable link/include dirs are removed'
+}
+removedir_body() {
+    input='-L/opt/schily/lib -I/opt/schily/include'
+    echo > expout
+    atf_check "echowrapper ${input}" 0 expout ignore
+}
+
+atf_init_test_cases() {
+    atf_add_test_case removedir
+}
