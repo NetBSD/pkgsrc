3f1fdb0eca6a4f983a14c063c1eee1d8750799c1
schmonz <schmonz>
2008-07-13 18:36:31 +0000

    Format more consistently with other tests.

--- pkgtools/wrapper/files/tests/test-remove_dir.sh
+++ pkgtools/wrapper/files/tests/test-remove_dir.sh
@@ -1,16 +1,16 @@
-# $NetBSD: test-remove_dir.sh,v 1.1.2.1 2008/07/12 06:57:49 schmonz Exp $
+# $NetBSD: test-remove_dir.sh,v 1.1.2.2 2008/07/13 18:36:31 schmonz Exp $
 #
 
-atf_test_case removedir
-removedir_head() {
+atf_test_case remove_dir
+remove_dir_head() {
     atf_set 'descr' 'Checks that undesirable link/include dirs are removed'
 }
-removedir_body() {
-    input='-L/opt/schily/lib -I/opt/schily/include'
+remove_dir_body() {
+    input="-L/opt/schily/lib -I/opt/schily/include"
     echo > expout
     atf_check 'echowrapper ${input}' 0 expout ignore
 }
 
 atf_init_test_cases() {
-    atf_add_test_case removedir
+    atf_add_test_case remove_dir
 }
