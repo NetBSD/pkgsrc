b471d747c27bd0ef80f66e633566d59f7bcb0e90
smb <smb@pkgsrc.org>
2008-07-13 22:32:05 +0000

    Finish update to 3.5.0

--- mail/claws-mail-pgpcore/buildlink3.mk
+++ mail/claws-mail-pgpcore/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2008/07/04 17:02:50 smb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/07/13 22:32:05 smb Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 CLAWS_MAIL_PGPCORE_BUILDLINK3_MK:=	${CLAWS_MAIL_PGPCORE_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	claws-mail-pgpcore
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}claws-mail-pgpcore
 
 .if ${CLAWS_MAIL_PGPCORE_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.claws-mail-pgpcore+=	claws-mail-pgpcore>=3.4.0
+BUILDLINK_API_DEPENDS.claws-mail-pgpcore+=	claws-mail-pgpcore>=3.5.0
 BUILDLINK_PKGSRCDIR.claws-mail-pgpcore?=	../../mail/claws-mail-pgpcore
 .endif	# CLAWS_MAIL_PGPCORE_BUILDLINK3_MK
 
