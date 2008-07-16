dc1ba1768a00454d0d9985145e60842f6434f844
wiz <wiz@pkgsrc.org>
2008-07-16 07:40:14 +0000

    Replace with one generated from scratch -- the old one had too many errors.

--- x11/xlt/buildlink3.mk
+++ x11/xlt/buildlink3.mk
@@ -1,30 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.1 2008/07/15 19:45:40 christos Exp $
-
-.include "../../mk/bsd.fast.prefs.mk"
-
-.if ${X11_TYPE} != "modular"
-.include "../../mk/x11.buildlink3.mk"
-.else
+# $NetBSD: buildlink3.mk,v 1.2 2008/07/16 07:40:14 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
-LIBXLT_BUILDLINK3_MK:=	${LIBXLT_BUILDLINK3_MK}+
+XLT_BUILDLINK3_MK:=	${XLT_BUILDLINK3_MK}+
 
 .if ${BUILDLINK_DEPTH} == "+"
-BUILDLINK_DEPENDS+=	libXlt
+BUILDLINK_DEPENDS+=	xlt
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NlibXv}
-BUILDLINK_PACKAGES+=	libXv
-BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libXv
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxlt}
+BUILDLINK_PACKAGES+=	xlt
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xlt
 
-.if ${LIBXLT_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.libXlt+=	libXlt>=1.0.1
-BUILDLINK_PKGSRCDIR.libXv?=	../../x11/libXlt
-.endif	# LIBXLT_BUILDLINK3_MK
+.if ${XLT_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.xlt+=	xlt>=13.0.13
+BUILDLINK_PKGSRCDIR.xlt?=	../../x11/xlt
+.endif	# XLT_BUILDLINK3_MK
 
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/libXext/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
-
-.endif
