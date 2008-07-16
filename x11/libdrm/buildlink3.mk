7657825b67d5e073e0c4c463cb99bd626e769d6a
bjs <bjs@pkgsrc.org>
2008-07-16 06:41:17 +0000

    Update to libdrm-2.3.1.  There is no up-to-date ChangeLog in the package whatsoever, so I cannot include a list here (see GIT history if you're curious).
    NOTE: This version of libdrm no longer includes the TTM interface.
          If any [xorg/mesa] packages are updated to versions explicitly requiring
          libdrm-2.3.1, then BUILDLINK_API_DEPENDS for this package will
          need to be changed.  Also, the i915tex dri driver found in
          graphics/MesaLib will not build with this version--but nor was it
          supported on any platform except Linux.  I have remove i915tex
          from the MesaLib driver build; this should be enough to avoid problems.

--- x11/libdrm/buildlink3.mk
+++ x11/libdrm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2007/01/14 10:57:47 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2008/07/16 06:41:17 bjs Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBDRM_BUILDLINK3_MK:=	${LIBDRM_BUILDLINK3_MK}+
@@ -13,6 +13,14 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libdrm
 
 .if ${LIBDRM_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.libdrm+=	libdrm>=2.3.0
+###
+### XXX Explicitly required by (at least) Mesa 7.1, which is nearing release.
+###	Upon updating pkgsrc Mesa to 7.1 and/orr xorg-server to a version which
+###	requires libdrm-2.3.1, BUILDLINK_API_DEPENDS should be increased
+###	and a recursive dependency bump performed.
+###	[bjs 20080714]
+###
+#BUILDLINK_API_DEPENDS.libdrm+=	libdrm>=2.3.1	
 BUILDLINK_PKGSRCDIR.libdrm?=	../../x11/libdrm
 .endif	# LIBDRM_BUILDLINK3_MK
 
