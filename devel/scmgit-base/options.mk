a1fc8f3a88f5ce93edadd82f1d57e175e27ef438
minskim <minskim@pkgsrc.org>
2008-08-01 11:30:20 +0000

    Enable the GUI application for Mac OS X when the scmgit-gui option is given.
    While here, add clean to PRIVILEGED_STATES; otherwise, an unprivileged
    user cannot remove the Mac OS X GUI application with "make clean".

--- devel/scmgit-base/options.mk
+++ devel/scmgit-base/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2008/04/21 16:34:45 xtraeme Exp $
+# $NetBSD: options.mk,v 1.2 2008/08/01 11:30:20 minskim Exp $
 #
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scmgit
 PKG_SUPPORTED_OPTIONS=	scmgit-gui
@@ -7,11 +7,16 @@ PKG_SUGGESTED_OPTIONS=	scmgit-gui
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=		gui
+PLIST_VARS+=		macosx
 
 .if !empty(PKG_OPTIONS:Mscmgit-gui)
 PLIST.gui=		yes
 USE_TOOLS+=		wish:run
 CONFIGURE_ARGS+=	--with-tcltk=${WISH:Q}
+.  if ${OPSYS} == "Darwin" && exists(/System/Library/Frameworks/Tk.framework)
+PLIST.macosx=		yes
+PRIVILEGED_STAGES+=	clean
+.  endif
 .else
 CONFIGURE_ARGS+=	--without-tcltk
 .endif
