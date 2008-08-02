5896629496901ae81b5c2484e5bed9a82cb6ae56
obache <obache@pkgsrc.org>
2008-08-02 05:39:20 +0000

    Suppress gpgme-config detection when gpgme option is off. Fixes build failure with installed gpgme but disabled gpgme option reported by PR 39267.

--- chat/centerim/options.mk
+++ chat/centerim/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2007/08/30 00:47:01 jnemeth Exp $
+# $NetBSD: options.mk,v 1.2 2008/08/02 05:39:20 obache Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.centerim
@@ -12,5 +12,6 @@ PKG_SUGGESTED_OPTIONS=	gpgme
 #CONFIGURE_ARGS+=	--with-gpgme=${BUILDLINK_PREFIX.gpgme:Q}/bin/gpgme-config
 .  include "../../security/gpgme/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=	--without-gpgme
+CONFIGURE_ARGS+=	--without-gpgme-prefix
+CONFIGURE_ENV+=		ac_cv_path_GPGME_CONFIG=
 .endif
