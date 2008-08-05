8e4ef4ab56bf97925a78356063c04411f0313d2e
joerg <joerg@pkgsrc.org>
2008-08-05 20:42:54 +0000

    Bump to pkg_install-20080805 for today's chdir related changes.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.102.2.11 2008/07/30 15:38:37 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.102.2.12 2008/08/05 20:42:54 joerg Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20080730"
+#define PKGTOOLS_VERSION "20080805"
 
 #endif /* _INST_LIB_VERSION_H_ */
