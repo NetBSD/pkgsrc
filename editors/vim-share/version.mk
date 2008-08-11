646b846b7c2217c229622017a8db7840946427cf
martti <martti@pkgsrc.org>
2008-08-11 12:14:10 +0000

    Updated VIM to 7.2
    This is a minor release of Vim.  In the 15 months since Vim 7.1 many
    bugs were fixed and runtime files have been added and updated.  There
    are also security improvements, therefore it's a good idea to upgrade.
    
    The main new feature since 7.1 is floating point support.  You can use
    it in the Vim script language to add up a list of amounts, for example.
    
    Once you have installed Vim 7.2 you can find details about the changes
    since Vim 7.1 with ":help version-7.2".

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.54 2008/06/23 05:07:56 martti Exp $
+# $NetBSD: version.mk,v 1.55 2008/08/11 12:14:10 martti Exp $
 
-VIM_VERSION=		7.1
-VIM_PATCHLEVEL=		330
-VIM_SUBDIR=		vim71
+VIM_VERSION=		7.2
+VIM_PATCHLEVEL=		0
+VIM_SUBDIR=		vim72
