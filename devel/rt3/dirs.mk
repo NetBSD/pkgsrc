cb0b8848b6595474c6d51ac79520d3103c4ca708
tonnerre <tonnerre>
2008-08-23 23:58:29 +0000

    Update rt3 to version 3.8.1 (from 3.6.6). Changes are:  * New visual style (web2).  * Rich text mails.  * Email signatures and encryption.  * User settings for:    - Ticket history ordering.    - Timezones.    - Date and time format.    - Username format.    - Default queue.    - Size of message text boxes.  * Charts of ticket relationships.  * Breeze through upgrades with new upgrade tools.  * Subscribe to iCalendar feeds of ticket due dates.  * Bookmark frequently-used tickets.  * Turn off mail from RT when you go on vacation.  * Get your mail from RT as a daily or weekly batch.  * Delete historical or spam tickets with RT::Shredder (only as a superuser).  * Set up more configurable business rules with new Scrip Conditions and    Actions.  * Forward tickets to third-parties from within RT.  * Enable and Disable RT extensions with the new Plugins system.  * Automatically log out inactive users with rt-clean-sessions.  * Run faster with less memory, thanks to numerous  performance improvements    and bug fixes.  * Fixed a potential HTML injection attck via user's properties.  * Better support for installation on Solaris and FreeBSD (non-GNU make).  * Updates to documentation and scripts for upgrading from MySQL 4.0  * Updated upgrade documentation for the new Queue Tag and bookmarks features.  * Multiple bugs in iCal support fixed.  * Backwards compatibility fixes for extensions developed against 3.6  * Added support for external links in tabs and targets.  * Addition of a new callback before ticket creation so you can implement    custom validation or stop creation for another reason.  * Missing documentation to external authentication configuration variable    in bin/rt and make it possible to set it via ENV.  * Merged method in RT::Ticket.

--- devel/rt3/dirs.mk
+++ devel/rt3/dirs.mk
@@ -1,14 +1,14 @@
-# $NetBSD: dirs.mk,v 1.2 2006/05/06 07:14:45 cube Exp $
+# $NetBSD: dirs.mk,v 1.3 2008/08/23 23:58:29 tonnerre Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
 RT_LOCAL_DIR?=		${VARBASE}/rt3
 BUILD_DEFS+=		RT_LOCAL_DIR
 
-RT_DOC_DIR=	${LOCALBASE}/share/doc/rt3
-RT_EXAMPLE_DIR= ${LOCALBASE}/share/examples/rt3
-RT_LIB_DIR=	${LOCALBASE}/lib/rt3
-RT_SHARE_DIR=	${LOCALBASE}/share/rt3
+RT_DOC_DIR=	${PREFIX}/share/doc/rt3
+RT_EXAMPLE_DIR= ${PREFIX}/share/examples/rt3
+RT_LIB_DIR=	${PREFIX}/lib/rt3
+RT_SHARE_DIR=	${PREFIX}/share/rt3
 RT_VAR_DIR=	${VARBASE}/spool/rt3
 
 RT_HTML_DIR=	${RT_SHARE_DIR}/html
