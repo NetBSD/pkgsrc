d093ebfaa06467aaa735bd476585b9399dd72cb8
taca <taca@pkgsrc.org>
2008-08-27 15:38:43 +0000

    Update PLONE3_VERSION to 3.1.5.1.

--- www/plone/ploneversion.mk
+++ www/plone/ploneversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: ploneversion.mk,v 1.8 2008/08/27 15:38:11 taca Exp $
+# $NetBSD: ploneversion.mk,v 1.9 2008/08/27 15:38:43 taca Exp $
 #
 
 .if !defined(_PLONEVERSION_MK)
@@ -41,7 +41,7 @@ PLONE_VERSION_ACCEPTED?=	${PLONE_VERSION_SUPPORTED}
 
 # current Plone version
 PLONE25_VERSION=		2.5.5
-PLONE3_VERSION=			3.1.4
+PLONE3_VERSION=			3.1.5.1
 
 BUILDLINK_API_DEPENDS.plone25?=	plone25>=${PLONE25_VERSION}
 BUILDLINK_API_DEPENDS.plone3?=	plone30>=${PLONE3_VERSION}
