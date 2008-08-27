8134bb8edd2c0bc301b5afbcfc5c4a3933034d95
epg <epg@pkgsrc.org>
2008-08-27 00:28:57 +0000

    Import built-in sqlite support for Python 2.5.  The python25 package contains the .py parts of this, but does not include the .so linked against sqlite3 (similar to the py-expat situation).

---
+++ databases/py-sqlite3/files/setup.py
@@ -0,0 +1,33 @@
+# $NetBSD
+
+from distutils.core import setup, Extension
+
+sqlite_prefix = '@SQLITE_PREFIX@'
+sqlite_incdir = sqlite_prefix + '/include'
+sqlite_libdir = sqlite_prefix + '/lib'
+
+sqlite_defines = [('MODULE_NAME', '"sqlite3"')]
+
+sqlite_srcs = ['_sqlite/cache.c',
+               '_sqlite/connection.c',
+               '_sqlite/cursor.c',
+               '_sqlite/microprotocols.c',
+               '_sqlite/module.c',
+               '_sqlite/prepare_protocol.c',
+               '_sqlite/row.c',
+               '_sqlite/statement.c',
+               '_sqlite/util.c', ]
+sqlite_srcs = ['Modules/' + x for x in sqlite_srcs]
+
+setup(
+    ext_modules = [
+        Extension('_sqlite3',
+                  sqlite_srcs,
+                  define_macros=sqlite_defines,
+                  include_dirs=["Modules/_sqlite",
+                                sqlite_incdir],
+                  library_dirs=[sqlite_libdir],
+                  runtime_library_dirs=[sqlite_libdir],
+                  libraries=["sqlite3"]),
+        ]
+)
