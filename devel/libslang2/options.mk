22c4e0340a565f1693df36d096bfa9aa16ec5dce
obache <obache@pkgsrc.org>
2008-08-28 07:39:49 +0000

    Try to fix uncompleted PLIST handling.

--- devel/libslang2/options.mk
+++ devel/libslang2/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2008/08/09 21:22:11 bjs Exp $
+# $NetBSD: options.mk,v 1.2 2008/08/28 07:39:49 obache Exp $
 #
 PKG_OPTIONS_VAR=		PKG_OPTIONS.libslang2
 PKG_SUPPORTED_OPTIONS=		png oniguruma pcre
@@ -6,12 +6,10 @@ PKG_SUPPORTED_OPTIONS=		png oniguruma pcre
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS=	${PKG_SUPPORTED_OPTIONS}
-.for var in ${PLIST_VARS}
-PLIST.${var}=	yes
-.endfor
 
 .if !empty(PKG_OPTIONS:Mpng)
 CONFIGURE_ARGS+=	--with-png=${BUILDLINK_PREFIX.png}
+PLIST.png=		yes
 .  include "../../graphics/png/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-png
@@ -19,6 +17,7 @@ CONFIGURE_ARGS+=	--without-png
 
 .if !empty(PKG_OPTIONS:Mpcre)
 CONFIGURE_ARGS+=	--with-pcre=${BUILDLINK_PREFIX.pcre}
+PLIST.pcre=		yes
 .  include "../../devel/pcre/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-pcre
@@ -26,6 +25,7 @@ CONFIGURE_ARGS+=	--without-pcre
 
 .if !empty(PKG_OPTIONS:Moniguruma)
 CONFIGURE_ARGS+=	--with-onig=${BUILDLINK_PREFIX.oniguruma}
+PLIST.oniguruma=	yes
 .  include "../../textproc/oniguruma/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-onig
