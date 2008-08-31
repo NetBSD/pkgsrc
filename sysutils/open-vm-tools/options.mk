112cb41bb349f0ad2f659ff9b5d2967ded3f72de
scottr <scottr@pkgsrc.org>
2008-08-31 06:36:47 +0000

    Update to version 2008.08.08-109361. This release provides the following enhancements:
     - Simple command-line tools to interface with VMware host
     - Guest OS scripts that can be triggered by the VMware host
     - Startup script
    
    When compiled with X11 support (the default), this release also provides:
    
     - GTK+-based GUI to configure time sync, device connections, and scripts
     - Helper application to interface with the xf86-video-vmware driver,
       enabling dynamic screen resize

---
+++ sysutils/open-vm-tools/options.mk
@@ -0,0 +1,18 @@
+# $NetBSD: options.mk,v 1.1 2008/08/31 06:36:47 scottr Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.open-vm-tools
+PKG_SUPPORTED_OPTIONS=	x11
+PKG_SUGGESTED_OPTIONS=	x11
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mx11)
+PLIST_SRC+=	PLIST.x11
+.  include "../../x11/gtk2/buildlink3.mk"
+.  include "../../x11/libXinerama/buildlink3.mk"
+.  include "../../x11/libXrandr/buildlink3.mk"
+.  include "../../x11/libXrender/buildlink3.mk"
+.  include "../../x11/libXtst/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=		--without-x
+.endif
