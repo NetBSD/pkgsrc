7f643f41ee36d9a6561dff70b2539c9aab27f85d
obache <obache>
2008-09-03 09:45:59 +0000

    Bump suse100_glx to 4. Update xorg-x11-Mesa from 6.8.2-100 to 6.8.2-100.13.
    This update fixes the following issues: X Font Server build_range()
    Integer Overflow Vulnerability [IDEF2708], X Font Server
    swap_char2b() Heap Overflow Vulnerability [IDEF2709], Composite
    extension buffer overflow.

--- emulators/suse100_glx/suse.x86_64.mk
+++ emulators/suse100_glx/suse.x86_64.mk
@@ -1,9 +1,9 @@
-# $NetBSD: suse.x86_64.mk,v 1.1 2007/07/29 05:18:47 jlam Exp $
+# $NetBSD: suse.x86_64.mk,v 1.2 2008/09/03 09:45:59 obache Exp $
 
 # SuSE x86_64 RPMs; the -devel version is necessary for libGL.so et.al.
 # symlinks only.
 #
-DISTFILES+=	xorg-x11-Mesa-6.8.2-100.${SUSE_ARCH}.rpm
-DISTFILES+=	xorg-x11-Mesa-devel-6.8.2-100.${SUSE_ARCH}.rpm
-DISTFILES+=	xorg-x11-Mesa-32bit-6.8.2-100.${SUSE_ARCH}.rpm
-DISTFILES+=	xorg-x11-Mesa-devel-32bit-6.8.2-100.${SUSE_ARCH}.rpm
+DISTFILES+=	xorg-x11-Mesa-6.8.2-100.13.${SUSE_ARCH}.rpm
+DISTFILES+=	xorg-x11-Mesa-devel-6.8.2-100.13.${SUSE_ARCH}.rpm
+DISTFILES+=	xorg-x11-Mesa-32bit-6.8.2-100.13.${SUSE_ARCH}.rpm
+DISTFILES+=	xorg-x11-Mesa-devel-32bit-6.8.2-100.13.${SUSE_ARCH}.rpm
