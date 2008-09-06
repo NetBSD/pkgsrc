b706a607f3f5ffbd531e43868c811ef7c942093e
wiz <wiz@pkgsrc.org>
2008-09-06 21:39:25 +0000

    libevent update incurs ABI bump.

--- devel/libevent/buildlink3.mk
+++ devel/libevent/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2008/04/23 05:53:00 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2008/09/06 21:39:25 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBEVENT_BUILDLINK3_MK:=	${LIBEVENT_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libevent
 
 .if !empty(LIBEVENT_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.libevent+=libevent>=0.6
-BUILDLINK_ABI_DEPENDS.libevent+=libevent-1.4.3* # exact match -- see Makefile
+BUILDLINK_ABI_DEPENDS.libevent+=libevent-1.4.5* # exact match -- see Makefile
 BUILDLINK_PKGSRCDIR.libevent?=	../../devel/libevent
 .endif	# LIBEVENT_BUILDLINK3_MK
 
