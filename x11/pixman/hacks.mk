6d3f41ecce5f16eff19f92c94f94664317c18c99
bjs <bjs@pkgsrc.org>
2008-09-06 20:09:15 +0000

    Update to version 0.11.10, a bug-fix release.  In addition, add a 'gen-symfile' target for regenerating the libtool export symfile.  This should be run upon updating the package. While here, silence needless autotools warnings and update sse2 conditional in hacks.mk.
    Changes since 0.11.8:
    
                  Add depth 30 formats to pixman_format_supported_source.
    
                  [sse2] Change pixman-sse to pass __mm128i args as
                  pointers, so as not to
    
                  Use CopyAreasse2, plus a compatibility fix
    
                  Use hidden attribute for private functions when
                  compiling with Sun Studio
    
                  Minor portability fixes
                  Post-release version-bump
                  Update RELEASING and release targets in Makefile.am
                  Fix typo in sse2 configure logic
                  TODO
                  Don't require GCC 4.2 on x86-64
                  Bug 16921. MMX and SSE2 intrinsics not enabled when
                  compiling with Intel'
                  Remove use of MMX extensions.
                  Be consistent in naming SSE2 related things SSE2
                  A few other renamings of SSE->SSE2
                  Rename pixman-sse.h pixman-sse2.h
                  Make sure pixman-combine{32,64}.h are disted
                  Use error instead of #error in a couple of other places
                  Check for __sun || __sun in pixman.h. Update TODO
                  Bump release
                  Update RELEASING
    
                  Add sys/inttypes.h include for AIX

--- x11/pixman/hacks.mk
+++ x11/pixman/hacks.mk
@@ -1,12 +1,15 @@
-# $NetBSD: hacks.mk,v 1.1 2008/07/18 03:34:10 bjs Exp $
+# $NetBSD: hacks.mk,v 1.2 2008/09/06 20:09:15 bjs Exp $
 #
 .if !defined(PIXMAN_HACKS_MK)
 PIXMAN_HACKS_MK=	# empty
+.  include "../../mk/bsd.fast.prefs.mk"
 .  include "../../mk/compiler.mk"
 ###
-### XXX SSE2 intrinsics require gcc-4.2+ to build.
+### XXX SSE2 intrinsics require gcc-4.2+ to build unless
+###	${MACHINE_ARCH} == "x86_64".
 ###
-.  if !empty(PKGSRC_COMPILER:Msunpro) || empty(CC_VERSION:Mgcc-4.2*)
+.  if !empty(PKGSRC_COMPILER:Msunpro) || \
+   (empty(MACHINE_ARCH:Mx86_64) && empty(CC_VERSION:Mgcc-[4-9].[2-9]*))
 CONFIGURE_ARGS+=	--disable-sse2
 .  endif
 .endif
