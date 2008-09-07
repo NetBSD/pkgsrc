33451ce28591da831e57afa9fb548399c68652d6
ahoka <ahoka>
2008-09-07 23:39:37 +0000

    Track FreeBSD's SVN snapshots of ffmpeg as there are no more stable releases produced by the ffmpeg team.
    Add v4l2 support for NetBSD.

--- multimedia/ffmpeg/buildlink3.mk
+++ multimedia/ffmpeg/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.9 2006/07/08 23:11:01 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2008/09/07 23:39:37 ahoka Exp $
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 FFMPEG_BUILDLINK3_MK:=	${FFMPEG_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	ffmpeg
 .endif
 
@@ -11,10 +11,41 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nffmpeg}
 BUILDLINK_PACKAGES+=	ffmpeg
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}ffmpeg
 
-.if !empty(FFMPEG_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.ffmpeg+=	ffmpeg>=0.4.9pre1
-BUILDLINK_ABI_DEPENDS.ffmpeg?=	ffmpeg>=0.4.9pre1
+.if ${FFMPEG_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.ffmpeg+=	ffmpeg>=20080727
 BUILDLINK_PKGSRCDIR.ffmpeg?=	../../multimedia/ffmpeg
+
+pkgbase := ffmpeg
+.  include "../../mk/pkg-build-options.mk"
+
 .endif	# FFMPEG_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg:Msdl)
+.  include "../../devel/SDL/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg:Mtheora)
+.  include "../../multimedia/libtheora/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg:Mxvid)
+.  include "../../multimedia/xvidcore/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg:Mx264)
+.  include "../../multimedia/x264-devel/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg:Mfaad)
+.  include "../../audio/faad2/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg:Mfaac)
+.  include "../../audio/faac/buildlink3.mk"
+.endif
+
+.include "../../devel/libgetopt/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
