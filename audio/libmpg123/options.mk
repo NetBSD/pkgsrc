e92cecd7ac4309af07a47c3188d114dc3a67238c
bjs <bjs@pkgsrc.org>
2008-09-07 00:56:15 +0000

    Import libmpg123-1.5.1.  This package provides the mpg123 project's core decoder library, which the new mpg123 package will depend on. Other software packages are beginning to write plug-ins for this library, and so installing it seperately is worthwhile.
    NOTE: I have encountered segfaults when attempting to use the jack
    module, which is threaded, with mpg123 unless I linked mpg123 with
    libpthread.  This may or may not apply to the library, though my
    intuition is that it does not, as obviously the executable is
    what calls lt_dlopen().  Thus, for now the Makefile and b3.mk of the
    executable will pull in dlopen.buildlink3.mk, setting
    DLOPEN_REQUIRE_PTHREADS=yes as necessary (last time I checked on
    NetBSD 4.99, it was).

---
+++ audio/libmpg123/options.mk
@@ -0,0 +1,38 @@
+# $NetBSD: options.mk,v 1.1.1.1 2008/09/07 00:56:15 bjs Exp $
+#
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.libmp123
+PKG_SUPPORTED_OPTIONS=	inet6 debug
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${MACHINE_ARCH} == "powerpc" || ${MACHINE_ARCH} == "i386"
+PKG_SUPPORTED_OPTIONS+=	simd
+.elif !empty(MACHINE_ARCH:Marm*)
+PKG_SUPPORTED_OPTIONS+=	mpg123-arm-fixed64
+.endif
+
+PKG_SUGGESTED_OPTIONS=	${PKG_SUPPORTED_OPTIONS}
+
+.include "../../mk/bsd.options.mk"
+
+SIMD_CONFIGURE_ARGS.powerpc=	altvec
+SIMD_CONFIGURE_ARGS.i386=	x86_dither
+
+.if empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--with-ipv6=no
+.endif
+
+.if !empty(PKG_OPTIONS:Msimd)
+CONFIGURE_ARGS+=	--with-cpu=${SIMD_CONFIGURE_ARGS.${MACHINE_ARCH}}
+.elif !empty(PKG_OPTIONS:Mmpg123-arm-fixed64)
+CONFIGURE_ARGS+=	--with-cpu=generic_nofpu
+.else
+CONFIGURE_ARGS+=	--with-cpu=generic_fpu
+.endif
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug=yes
+.else
+CONFIGURE_ARGS+=	--enable-debug=no
+.endif
