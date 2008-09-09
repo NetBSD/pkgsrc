1e8116f15f5fecbf945c37b5001a1440b5e685fe
tron <tron@pkgsrc.org>
2008-09-09 22:23:32 +0000

    Re-add Berkeley DB 4.6.21 as "db46" package. This package can coexist with the "db4" package without problems. This is the first half of the fix for PR pkg/39500.

---
+++ databases/db46/hacks.mk
@@ -0,0 +1,25 @@
+# $NetBSD: hacks.mk,v 1.1 2008/09/09 22:23:32 tron Exp $
+
+### [Sun Mar 14 19:32:40 UTC 2004 : jlam]
+### GCC 3.3.x and older versions have an optimization bug on powerpc that's
+### tickled by the db4 source code, so remove optimization flags in that
+### case. This fixes PR pkg/30647 by Ian Spray.
+###
+.if ${MACHINE_ARCH} == "powerpc"
+.  include "../../mk/compiler.mk"
+.  if !empty(CC_VERSION:Mgcc*)
+.    if !defined(_GCC_IS_TOO_OLD)
+_GCC_IS_TOO_OLD!=	\
+	if ${PKG_ADMIN} pmatch 'gcc<3.4' ${CC_VERSION}; then		\
+		${ECHO} "yes";						\
+	else								\
+		${ECHO} "no";						\
+	fi
+MAKEFLAGS+=	_GCC_IS_TOO_OLD=${_GCC_IS_TOO_OLD:Q}
+.    endif
+.    if !empty(_GCC_IS_TOO_OLD:M[yY][eE][sS])
+PKG_HACKS+=		powerpc-codegen
+BUILDLINK_TRANSFORM+=	rm:-O[0-9]*
+.    endif
+.  endif
+.endif
