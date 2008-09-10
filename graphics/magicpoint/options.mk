42ac7e470103fedfdc9aa10a0b84093dcc97a854
taca <taca@pkgsrc.org>
2008-09-10 13:32:47 +0000

    Update magicpoint to 1.13a.
    * Fix build error with xft2 option and graphics/freetype-lib package is
      installed.  Shoud be fix PR pkg/32656.
    * Refering PR pkg/38552 adpot some of patches.
    * Move PKG_OPTIONS related item to options.mk.
    * More pkgsrc clean up.
    * Try to reduce compile warnings.
    
    
    What's new in 1.13a?
    
        * m17n library support enhancement
        * MacOS support enhancement
        * add new environment variable MGPRC for the path of the .mgprc file.
        * add "psfont" directive to specify the font used by mgp2ps
        * add -g option for mgp2ps to allow users to adjust line distanc
        * add -zoomonclk option for newimage
        * allow to handle gif format images by default
        * many bug fixes
    
          Dedication
    
          Magicpoint 1.13a is dedicated to the memory of Dr. Jun-ichiro
          "itojun" Hagino, who was known for his enormous contribution to
          the Internet and the Open Source Software community. He was the
          developer of Magicpoint from its infancy. Magicpoint could not
          exist without his devoted work.
    
    What's new in 1.12a?
    
        * add %tsystem directive which might be more stable than %xsystem
        * add -rotate option for newimage
        * allow to use non-ASCI filename in slides (use -U to enable this feature)
        * many bug fixes

---
+++ graphics/magicpoint/options.mk
@@ -0,0 +1,48 @@
+# $NetBSD: options.mk,v 1.1 2008/09/10 13:32:47 taca Exp $
+
+.include "../../mk/bsd.prefs.mk"
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.magicpoint
+PKG_SUPPORTED_OPTIONS=	freetype vflib xft2
+PKG_SUGGESTED_OPTIONS=	xft2
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mfreetype)
+CONFIGURE_ARGS+=	--enable-freetype
+MGP_TFONT=	% %
+MGP_VFONT=	# empty
+.include "../../graphics/freetype-lib/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-freetype
+.endif
+
+.if !empty(PKG_OPTIONS:Mvflib)
+CONFIGURE_ARGS+=	--enable-vflib
+MGP_TFONT=	# empty
+MGP_VFONT=	% %
+.include "../../print/ja-vflib-lib/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-vflib
+.endif
+
+.if !empty(PKG_OPTIONS:Mxft2)
+CONFIGURE_ARGS+=	--enable-xft2
+MGP_XFONT=	# empty
+MGP_TFONT=	% %
+MGP_VFONT=	% %
+
+.include "../../x11/libXft/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-xft2
+.endif
+
+.include "../../graphics/imlib/buildlink3.mk"
+
+
+#
+# fall back defaults
+#
+MGP_VFONT?=	% %
+MGP_TFONT?=	% %
+MGP_XFONT?=	% %
