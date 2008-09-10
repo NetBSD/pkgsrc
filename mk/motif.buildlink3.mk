b8a8834077a73409b3d67e8c4c49f9e64173be55
joerg <joerg@pkgsrc.org>
2008-09-10 16:01:52 +0000

    Add MOTIF_TYPE and MOTIF_BASE to BUILD_DEFS.

--- mk/motif.buildlink3.mk
+++ mk/motif.buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: motif.buildlink3.mk,v 1.11 2006/11/06 11:29:38 joerg Exp $
+# $NetBSD: motif.buildlink3.mk,v 1.12 2008/09/10 16:01:52 joerg Exp $
 #
 # Package-settable variables:
 #
@@ -20,6 +20,8 @@
 MOTIF_BUILDLINK3_MK=	# defined
 
 .include "../../mk/bsd.prefs.mk"
+
+BUILD_DEFS+=			MOTIF_TYPE MOTIF_BASE
 
 #
 # If /usr/dt is a valid Motif-2.0 installation, then use it.  Otherwise,
