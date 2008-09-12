77a8ed0a963a8eb63ea40f59260a08d82dbac95a
sborrill <sborrill@pkgsrc.org>
2008-09-12 16:00:56 +0000

    Make ssl support an option (that defaults to on)

--- net/rdesktop/options.mk
+++ net/rdesktop/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2008/06/08 16:29:28 tron Exp $
+# $NetBSD: options.mk,v 1.2 2008/09/12 16:00:56 sborrill Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.rdesktop
-PKG_SUPPORTED_OPTIONS+=	inet6
-PKG_SUGGESTED_OPTIONS+=	inet6
+PKG_SUPPORTED_OPTIONS+=	inet6 ssl
+PKG_SUGGESTED_OPTIONS+=	inet6 ssl
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,4 +11,9 @@ PKG_SUGGESTED_OPTIONS+=	inet6
 ###
 .if !empty(PKG_OPTIONS:Minet6)
 CONFIGURE_ARGE+=	--with-ipv6
+.endif
+
+.if !empty(PKG_OPTIONS:Mssl)
+CONFIGURE_ARGS+=	--with-openssl=${SSLBASE}
+.include "../../security/openssl/buildlink3.mk"
 .endif
