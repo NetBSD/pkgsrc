11ecbb2806bc9450b74c9a933ea0aa4eebd0c8d2
taca <taca>
2008-09-12 14:59:51 +0000

    Restructuring Squid package:
    * Now www/squid directory is common directory for squid packages.
    * package options clean up and all options are described.
    * LDAP helper support. (PR pkg/39386)
    * DESTDIR support.
    * Move some MESSAGE to documation directory.
    
    Next, I'll import squid26, squid27 and squid30 packages.

--- www/squid/options.mk
+++ www/squid/options.mk
@@ -1,43 +1,71 @@
-# $NetBSD: options.mk,v 1.13 2007/08/02 15:45:10 taca Exp $
+# $NetBSD: options.mk,v 1.14 2008/09/12 14:59:52 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.squid
-PKG_SUPPORTED_OPTIONS=	aufs carp coss icmp pam-helper snmp ssl unlinkd
+PKG_SUPPORTED_OPTIONS=	snmp ssl \
+		squid-backend-aufs squid-backend-null \
+		squid-backend-ufs squid-carp squid-unlinkd \
+		squid-ldap-helper squid-pam-helper
+
+PKG_OPTIONS_LEGACY_OPTS=	aufs:squid-backend-aufs \
+	coss:squid-backend-coss diskd:squid-backend-diskd \
+	null:squid-backend-null ufs:squid-backend-ufs \
+	linux-netfilter:squid-netfilter ipf-transparent:squid-ipf \
+	pf-transparent:squid-pf unlinkd:squid-unlinkd \
+	arp-acl:squid-arp-acl pam-helper:squid-pam-helper carp:squid-carp
+
+PLIST_VARS+=	coss diskd snmp unlinkd
+PLIST_VARS+=	ba_LDAP ba_MSNT ba_NCSA ba_PAM ba_YP ba_getpwnam
+PLIST_VARS+=	da_ldap da_password
+PLIST_VARS+=	na_SMB na_fakeauth
+PLIST_VARS+=	eacl_ip_user eacl_ldap_group eacl_unix_group
+
 #
 # most of options are enabled by default except aufs.  aufs backend isn't
 # tested well.
-PKG_SUGGESTED_OPTIONS=	carp icmp pam-helper snmp ssl unlinkd
+PKG_SUGGESTED_OPTIONS=	squid-carp snmp ssl squid-pam-helper squid-unlinkd
+
+.include "../../mk/bsd.prefs.mk"
+
+#
+# Squid 3.0's COSS support is not stable now.
+#
+.if !empty(PKGNAME:Msquid-2.[0-9]*.[0-9]*)
+PKG_SUPPORTED_OPTIONS+=	squid-backend-coss 
+PKG_SUGGESTED_OPTIONS+=	squid-backend-coss
+.endif
 
 .if !empty(OPSYS:MLinux)
-PKG_SUPPORTED_OPTIONS+=	linux-netfilter
+PKG_SUPPORTED_OPTIONS+=	squid-netfilter
 .endif
 
 # some release of NetBSD lacks <ipl.h> header of IP Filter 4.x
 .if ${OPSYS} != "NetBSD" || ${OS_VERSION:M[01].*} || exists(/usr/include/netinet/ipl.h)
-PKG_SUPPORTED_OPTIONS+=	ipf-transparent
+PKG_SUPPORTED_OPTIONS+=	squid-ipf
 .endif
 
 .if ${OPSYS} == "FreeBSD" || ${OPSYS} == "NetBSD" || ${OPSYS} == "OpenBSD" || ${OPSYS} == "DragonFly"
-PKG_SUPPORTED_OPTIONS+=	pf-transparent
+PKG_SUPPORTED_OPTIONS+=	squid-pf
 .endif
 
 .if (${OPSYS} == "FreeBSD" || ${OPSYS} == "NetBSD") && \
-	!empty(PKG_SUPPORTED_OPTIONS:Mipf-transparent)
-PKG_SUGGESTED_OPTIONS+=	ipf-transparent
+	!empty(PKG_SUPPORTED_OPTIONS:Msquid-ipf)
+PKG_SUGGESTED_OPTIONS+=	squid-ipf
 .endif
 
 .if ${OPSYS} == "OpenBSD"
-PKG_SUGGESTED_OPTIONS+=	pf-transparent
+PKG_SUGGESTED_OPTIONS+=	squid-pf
 .endif
 
 # Darwin dosen't support System V IPC support.
 .if empty(OPSYS:MDarwin)
-PKG_SUPPORTED_OPTIONS+=	diskd
-PKG_SUGGESTED_OPTIONS+=	diskd
+PKG_SUPPORTED_OPTIONS+=	squid-backend-diskd
+PKG_SUGGESTED_OPTIONS+=	squid-backend-diskd
+PLIST.diskd=		yes
 .endif
 
-# limited platform support arp-acl
+# limited platform support squid-arp-acl
 .if !empty(OPSYS:MFreeBSD) || !empty(OPSYS:MNetBSD) || !empty(OPSYS:MOpenBSD) || !empty(OPSYS:MLinux) || !empty(OPSYS:MSunOS)
-PKG_SUPPORTED_OPTIONS+=	arp-acl
+PKG_SUPPORTED_OPTIONS+=	squid-arp-acl
 .endif
 
 .include "../../mk/bsd.options.mk"
@@ -62,26 +90,29 @@ CONFIGURE_ARGS+=	--enable-pf-transparent
 CONFIGURE_ARGS+=	--enable-ipf-transparent
 .endif
 
-.if !empty(PKG_OPTIONS:Marp-acl)
+.if !empty(PKG_OPTIONS:Msquid-arp-acl)
 CONFIGURE_ARGS+=	--enable-arp-acl
 .endif
 
-.if !empty(PKG_OPTIONS:Mcarp)
+.if !empty(PKG_OPTIONS:Msquid-carp)
 CONFIGURE_ARGS+=	--enable-carp
 .endif
 
-.if !empty(PKG_OPTIONS:Micmp)
-CONFIGURE_ARGS+=	--enable-icmp
-INSTALL_TARGET+=	install install-pinger
+.if !empty(PKG_OPTIONS:Msquid-ldap-helper)
+.include "../../databases/openldap-client/buildlink3.mk"
+SQUID_BASIC_AUTH_HELPERS+=	LDAP
+SQUID_DIGEST_AUTH_HELPERS+=	ldap
+SQUID_EXTERNAL_ACL_HELPERS+=	ldap_group
 .endif
 
-.if !empty(PKG_OPTIONS:Mpam-helper)
+.if !empty(PKG_OPTIONS:Msquid-pam-helper)
 .  include "../../mk/pam.buildlink3.mk"
 SQUID_BASIC_AUTH_HELPERS+=	PAM
 .endif
 
 .if !empty(PKG_OPTIONS:Msnmp)
 CONFIGURE_ARGS+=	--enable-snmp
+PLIST.snmp=		yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mssl)
@@ -89,39 +120,57 @@ CONFIGURE_ARGS+=	--enable-ssl --with-openssl=${SSLBASE:Q}
 .include "../../security/openssl/buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Maufs)
+.if !empty(PKG_OPTIONS:Msquid-backend-aufs)
 SQUID_BACKENDS+=	aufs
 PTHREAD_AUTO_VARS=	yes
 .include "../../mk/pthread.buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mcoss)
+.if !empty(PKG_OPTIONS:Msquid-backend-coss)
+.if empty(PKG_OPTIONS:Msquid-backend-aufs)
+SQUID_BACKENDS+=	aufs
+PTHREAD_AUTO_VARS=	yes
+.include "../../mk/pthread.buildlink3.mk"
+.endif
 SQUID_BACKENDS+=	coss
-.if empty(PKG_OPTIONS:Maufs)
-PKG_FAIL_REASON+=	"The option needs aufs option, too."
-.endif
+PLIST.coss=		yes
 .endif
 
-.if !empty(PKG_OPTIONS:Mdiskd)
+.if !empty(PKG_OPTIONS:Msquid-backend-diskd)
 SQUID_BACKENDS+=	diskd
+PLIST.diskd=		yes
 .endif
 
-.if empty(PKG_OPTIONS:Munlinkd)
+.if empty(PKG_OPTIONS:Msquid-unlinkd)
 CONFIGURE_ARGS+=	--disable-unlinkd
+.else
+PLIST.unlinkd=		yes
 .endif
 
 .if !empty(SQUID_BASIC_AUTH_HELPERS)
 CONFIGURE_ARGS+= --enable-basic-auth-helpers=${SQUID_BASIC_AUTH_HELPERS:Q}
+.for i in ${SQUID_BASIC_AUTH_HELPERS}
+PLIST.ba_${i}=		yes
+.endfor
 .endif
 
 .if !empty(SQUID_DIGEST_AUTH_HELPERS)
 CONFIGURE_ARGS+= --enable-digest-auth-helpers=${SQUID_DIGEST_AUTH_HELPERS:Q}
+.for i in ${SQUID_DIGEST_AUTH_HELPERS}
+PLIST.da_${i}=		yes
+.endfor
 .endif
 
 .if !empty(SQUID_NTLM_AUTH_HELPERS)
 CONFIGURE_ARGS+= --enable-ntlm-auth-helpers=${SQUID_NTLM_AUTH_HELPERS:Q}
+.for i in ${SQUID_NTLM_AUTH_HELPERS}
+PLIST.na_${i}=		yes
+.endfor
 .endif
 
 .if !empty(SQUID_EXTERNAL_ACL_HELPERS)
 CONFIGURE_ARGS+= --enable-external-acl-helpers=${SQUID_EXTERNAL_ACL_HELPERS:Q}
+.for i in ${SQUID_EXTERNAL_ACL_HELPERS}
+PLIST.eacl_${i}=	yes
+.endfor
 .endif
