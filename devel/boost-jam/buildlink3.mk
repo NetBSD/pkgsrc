8fa2284ca5f198210d353fbbec876c1800a7f029
jmmv <jmmv@pkgsrc.org>
2008-09-13 16:14:13 +0000

    Update boost to 1.36.0.  Way too many changes since 1.34 to be listed here.  Based on patches sent by Brook Milligan through private mail with some minimal changes by me to fix boost-python and builds on Mac OS X.  Tested on NetBSD/amd64 current and Tiger.

--- devel/boost-jam/buildlink3.mk
+++ devel/boost-jam/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2008/01/04 19:58:40 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2008/09/13 16:14:13 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 BOOST_JAM_BUILDLINK3_MK:=	${BOOST_JAM_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	boost-jam
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}boost-jam
 
 .if !empty(BOOST_JAM_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.boost-jam+=		boost-jam-1.34.*
+BUILDLINK_API_DEPENDS.boost-jam+=		boost-jam-1.36.*
 BUILDLINK_DEPMETHOD.boost-jam?=	build
 BUILDLINK_PKGSRCDIR.boost-jam?=	../../devel/boost-jam
 .endif	# BOOST_JAM_BUILDLINK3_MK
