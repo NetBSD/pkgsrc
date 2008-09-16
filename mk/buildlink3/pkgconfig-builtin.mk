e8897a0ab9ea7db4abf450aa755084cb932b5f23
cube <cube@pkgsrc.org>
2008-09-16 03:03:30 +0000

    pkgconfig-builtin.mk is a generic builtin.mk file testing the presence of a given .pc file and if the package is indeed built-in, construct a version number from it so that it's suitable for BUILDINK_API_DEPENDS tests.

---
+++ mk/buildlink3/pkgconfig-builtin.mk
@@ -0,0 +1,53 @@
+# $NetBSD: pkgconfig-builtin.mk,v 1.1.2.1 2008/09/16 03:03:30 cube Exp $
+
+# This file is used to factor out a common pattern in builtin.mk files backed
+# up by the existence of a pkgconfig file.
+#
+# Caller has to define BUILTIN_PKG and PKGCONFIG_FILE.<BUILTIN_PKG>.
+
+BUILTIN_FIND_FILES_VAR:=			FIND_FILES_${BUILTIN_PKG}
+BUILTIN_FIND_FILES.FIND_FILES_${BUILTIN_PKG}=	${PKGCONFIG_FILE.${BUILTIN_PKG}}
+
+.include "../../mk/buildlink3/bsd.builtin.mk"
+
+.if ${X11BASE} == ${LOCALBASE}
+IS_BUILTIN.${BUILTIN_PKG}=	no
+.elif !defined(IS_BUILTIN.${BUILTIN_PKG})
+IS_BUILTIN.${BUILTIN_PKG}=	no
+.  if empty(FIND_FILES_${BUILTIN_PKG}:M__nonexistent__)
+IS_BUILTIN.${BUILTIN_PKG}=	yes
+.  endif
+.endif
+MAKEVARS:=	${MAKEVARS} IS_BUILTIN.${BUILTIN_PKG}
+
+.if !defined(BUILTIN_PKG.${BUILTIN_PKG}) && \
+    !empty(IS_BUILTIN.${BUILTIN_PKG}:M[yY][eE][sS]) && \
+    empty(FIND_FILES_${BUILTIN_PKG}:M__nonexistent__)
+BUILTIN_VERSION.${BUILTIN_PKG}!= \
+	${SED} -n -e 's/Version: //p' ${FIND_FILES_${BUILTIN_PKG}}
+BUILTIN_PKG.${BUILTIN_PKG}= ${BUILTIN_PKG}-${BUILTIN_VERSION.${BUILTIN_PKG}}
+.endif
+MAKEVARS:=      ${MAKEVARS} BUILTIN_PKG.${BUILTIN_PKG}
+
+.if !defined(USE_BUILTIN.${BUILTIN_PKG})
+.  if ${PREFER_PKGSRC.${BUILTIN_PKG}} == "pkgsrc"
+USE_BUILTIN.${BUILTIN_PKG}=	no
+.  else
+USE_BUILTIN.${BUILTIN_PKG}=	${IS_BUILTIN.${BUILTIN_PKG}}
+.    if defined(BUILTIN_PKG.${BUILTIN_PKG}) && \
+	!empty(IS_BUILTIN.${BUILTIN_PKG}:M[Yy][Ee][Ss])
+USE_BUILTIN.${BUILTIN_PKG}=	yes
+.      for _dep_ in ${BUILDLINK_API_DEPENDS.${BUILTIN_PKG}}
+.        if !empty(USE_BUILTIN.${BUILTIN_PKG}:M[Yy][Ee][Ss])
+USE_BUILTIN.${BUILTIN_PKG}!= \
+	if ${PKG_ADMIN} pmatch ${_dep_:Q} ${BUILTIN_PKG.${BUILTIN_PKG}}; then \
+		${ECHO} yes; \
+	else \
+		${ECHO} no; \
+	fi
+.        endif
+.      endfor
+.    endif
+.  endif
+.endif
+MAKEVARS:=	${MAKEVARS} USE_BUILTIN.${BUILTIN_PKG}
