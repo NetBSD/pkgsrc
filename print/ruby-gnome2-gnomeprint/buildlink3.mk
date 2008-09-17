a3b5ecd4fa3ec9c1310f5d1614014df0559bf607
obache <obache@pkgsrc.org>
2008-09-17 00:21:48 +0000

    Update ruby-gnome2 to 0.17.0 release.
    pkgsrc changes:
       * Add some additional modules:
        	devel/ruby-gnome2-bonobo
        	devel/ruby-gnome2-bonoboui
        	devel/ruby-gnome2-gconf
        	devel/ruby-gnome2-libglade
        	graphics/ruby-gnome2-gtkglext
        	multimedia/ruby-gnome2-gstreamer
        	print/ruby-gnome2-gnomeprintui
        	sysutils/ruby-gnome2-gnomevfs
        	www/ruby-gnome2-gtkhtml2
        	www/ruby-gnome2-gtkmozembed
        	x11/ruby-gnome2-gtksourceview
        	x11/ruby-gnome2-vte
       * Fixed detection of rcairo. It had been broken since switched to gem.
         patch-a{c,h,i,j,k,l.m}
         (It Already in upstream trunk)
       * Add patches for allow to load gem rcairo.
         patch-a{n,o,p}
       * Fixed overwritten glib module when installing modules require generated
         header of glib module.
         Modify to install the header as part of glib module and use bl3.mk.
       * clean up dependency
       * Add test target for modules having unit test.
       * Modified patch-a{a,b,d,f,g} to acceptable by upstream.
    
    Ruby-GNOME2-0.17.0 (2008-09-07)
    
      Release notes:
      * This Release supports Ruby 1.8.7 and has many bug fixes and
        new bound functions.
    
      Main changes:
      * Ruby/GLib:
        - [#2060606] Crash when removing ruby applet from gnome panel with 0.7rc1
          [Kouhei Sutou]
    
      * Ruby/GTK:
        - [#2025651] FTBFS with gtk+ 2.13.5 [Cesare Tirabassi, Kouhei Sutou]
    
        - [#2043970] Segfaults on 64-bit Linux [Neil Roberts, Kouhei Sutou]
    
      * Ruby/Pango:
        - [#2043970] Pango::FontDescription#weight= seg fault [Kouhei Sutou]
    
      * Many other changes, GC bugs, Segfault fixes. See corresponding ChangeLog
        for detailed information on changes and contributors.
    
        Special thanks for your contributions to: (list in no particular order)
        - Neil Roberts
        - Cesare Tirabassi
        - Arnaud Cornet

---
+++ print/ruby-gnome2-gnomeprint/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2008/09/17 00:21:49 obache Exp $
+
+BUILDLINK_DEPTH:=				${BUILDLINK_DEPTH}+
+RUBY_GNOME2_GNOMEPRINT_BUILDLINK3_MK:=	${RUBY_GNOME2_GNOMEPRINT_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	ruby-gnome2-gnomeprint
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nruby-gnome2-gnomeprint}
+BUILDLINK_PACKAGES+=	ruby-gnome2-gnomeprint
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}ruby-gnome2-gnomeprint
+
+.if ${RUBY_GNOME2_GNOMEPRINT_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.ruby-gnome2-gnomeprint+=	${RUBY_PKGPREFIX}-gnome2-gnomeprint>=0.17.0
+BUILDLINK_PKGSRCDIR.ruby-gnome2-gnomeprint?=	../../print/ruby-gnome2-gnomeprint
+.endif	# RUBY_GNOME2_GNOMEPRINT_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=				${BUILDLINK_DEPTH:S/+$//}
