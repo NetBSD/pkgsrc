dca8a0ad14484c57c41827630efae82df704a7d3
taca <taca@pkgsrc.org>
2008-09-22 15:31:10 +0000

    Update drupal6-translations to 20080922.
    * sort each PLIST file.
    * Update fi translation to 6.x-1.0.
    * Add pl, sv and uk-ua translation.

--- www/drupal6-translations/options.mk
+++ www/drupal6-translations/options.mk
@@ -1,27 +1,30 @@
-# $NetBSD: options.mk,v 1.1 2008/09/01 15:16:25 taca Exp $
+# $NetBSD: options.mk,v 1.2 2008/09/22 15:31:10 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.drupal-translations
 
 # Broken directory structure: eo vi
-# development version only: ar fr nb nn pt sv th tr
-DL_SUPPORTED_LANGUAGES=	cs de el es fi gu he hu ja lt nl pt-br ro sk \
-			zh-cn zh-tw
+# development version only: ar fr nb nn pt th tr
+DL_SUPPORTED_LANGUAGES=	cs da de el es fi gu he hu ja lt nl pl pt-br ro \
+			sk sv uk-ua zh-cn zh-tw
 
 DL_TRANSLATIONS.cs=	cs-6.x-1.8.tar.gz
 DL_TRANSLATIONS.da=	da-6.x-1.0.tar.gz
 DL_TRANSLATIONS.de=	de-6.x-1.0-rc1.tar.gz
 DL_TRANSLATIONS.el=	el-6.x-1.0-beta1.tar.gz
 DL_TRANSLATIONS.es=	es-6.x-1.0.tar.gz
-DL_TRANSLATIONS.fi=	fi-6.x-1.0-rc.tar.gz
+DL_TRANSLATIONS.fi=	fi-6.x-1.0.tar.gz
 DL_TRANSLATIONS.gu=	gu-6.x-1.0.tar.gz
 DL_TRANSLATIONS.he=	he-6.x-1.0.tar.gz
 DL_TRANSLATIONS.hu=	hu-6.x-1.0.tar.gz
 DL_TRANSLATIONS.ja=	ja-6.x-1.2.tar.gz
 DL_TRANSLATIONS.lt=	lt-6.x-1.2.tar.gz
 DL_TRANSLATIONS.nl=	nl-6.x-1.1.tar.gz
+DL_TRANSLATIONS.pl=	pl-6.x-0.3.tar.gz
 DL_TRANSLATIONS.pt-br=	pt-br-6.x-1.0-rc5.tar.gz
 DL_TRANSLATIONS.ro=	ro-6.x-1.1.tar.gz
 DL_TRANSLATIONS.sk=	sk-6.x-1.0-beta5.tar.gz
+DL_TRANSLATIONS.sv=	sv-6.x-1.0.tar.gz
+DL_TRANSLATIONS.uk-ua=	uk-ua-6.x-1.2-beta2.tar.gz
 DL_TRANSLATIONS.zh-cn=	zh-hans-6.x-1.0.tar.gz
 DL_TRANSLATIONS.zh-tw=	zh-hant-6.x-1.1.tar.gz
 
