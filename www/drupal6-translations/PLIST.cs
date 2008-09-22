c0c17dcb967f8d714216be387fd62f60187c8fc5
taca <taca@pkgsrc.org>
2008-09-22 15:31:10 +0000

    Update drupal6-translations to 20080922.
    * sort each PLIST file.
    * Update fi translation to 6.x-1.0.
    * Add pl, sv and uk-ua translation.

--- www/drupal6-translations/PLIST.cs
+++ www/drupal6-translations/PLIST.cs
@@ -1,10 +1,6 @@
-@comment $NetBSD: PLIST.cs,v 1.1.1.1 2008/08/17 05:35:05 taca Exp $
+@comment $NetBSD: PLIST.cs,v 1.2 2008/09/22 15:31:10 taca Exp $
 ${DRUPAL_BASE}/LICENSE.cs.txt
 ${DRUPAL_BASE}/STATUS.cs.txt
-${DRUPAL_BASE}/modules/system/translations/general.cs.po
-${DRUPAL_BASE}/modules/system/translations/includes.cs.po
-${DRUPAL_BASE}/profiles/default/translations/cs.po
-${DRUPAL_BASE}/modules/system/translations/misc.cs.po
 ${DRUPAL_BASE}/modules/aggregator/translations/modules-aggregator.cs.po
 ${DRUPAL_BASE}/modules/block/translations/modules-block.cs.po
 ${DRUPAL_BASE}/modules/blog/translations/modules-blog.cs.po
@@ -29,6 +25,9 @@ ${DRUPAL_BASE}/modules/profile/translations/modules-profile.cs.po
 ${DRUPAL_BASE}/modules/search/translations/modules-search.cs.po
 ${DRUPAL_BASE}/modules/statistics/translations/modules-statistics.cs.po
 ${DRUPAL_BASE}/modules/syslog/translations/modules-syslog.cs.po
+${DRUPAL_BASE}/modules/system/translations/general.cs.po
+${DRUPAL_BASE}/modules/system/translations/includes.cs.po
+${DRUPAL_BASE}/modules/system/translations/misc.cs.po
 ${DRUPAL_BASE}/modules/system/translations/modules-system.cs.po
 ${DRUPAL_BASE}/modules/taxonomy/translations/modules-taxonomy.cs.po
 ${DRUPAL_BASE}/modules/throttle/translations/modules-throttle.cs.po
@@ -38,6 +37,7 @@ ${DRUPAL_BASE}/modules/trigger/translations/modules-trigger.cs.po
 ${DRUPAL_BASE}/modules/update/translations/modules-update.cs.po
 ${DRUPAL_BASE}/modules/upload/translations/modules-upload.cs.po
 ${DRUPAL_BASE}/modules/user/translations/modules-user.cs.po
+${DRUPAL_BASE}/profiles/default/translations/cs.po
 ${DRUPAL_BASE}/themes/chameleon/translations/themes-chameleon.cs.po
 ${DRUPAL_BASE}/themes/garland/translations/themes-garland.cs.po
 ${DRUPAL_BASE}/themes/pushbutton/translations/themes-pushbutton.cs.po
