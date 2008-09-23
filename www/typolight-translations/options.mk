12d9c4011223ea616024b0233a012e1fd8540253
taca <taca@pkgsrc.org>
2008-09-23 06:52:11 +0000

    Update typolight-translations to 20080922.
    * Sort each PLIST file.
    * Fix missing files in PLIST.sv.
    * Update translations to support TYPOlight 2.6.1.

--- www/typolight-translations/options.mk
+++ www/typolight-translations/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2008/09/15 09:59:08 taca Exp $
+# $NetBSD: options.mk,v 1.2 2008/09/23 06:52:12 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.typolight-translations
 
@@ -6,20 +6,20 @@ TL_SUPPORTED_LANGUAGES=	af bg ca da es fa fr is it ja lt lv nl no \
 			pl pt ro ru sk sq sr sv uk
 
 TL_TRANSLATIONS.af=	TYPOlight-Afrikaans-v1.zip
-TL_TRANSLATIONS.bg=	TYPOlight-Bulgarian-v8.zip
+TL_TRANSLATIONS.bg=	TYPOlight-Bulgarian-v11.zip
 TL_TRANSLATIONS.ca=	TYPOlight-Catalan-v3.zip
 TL_TRANSLATIONS.da=	TYPOlight-Danish-v5.zip
 TL_TRANSLATIONS.es=	TYPOlight-Spanish-v10.zip
 TL_TRANSLATIONS.fa=	TYPOlight-Persian-v2.zip
-TL_TRANSLATIONS.fr=	TYPOlight-French-v30.zip
+TL_TRANSLATIONS.fr=	TYPOlight-French-v32.zip
 TL_TRANSLATIONS.is=	TYPOlight-Icelandic-v2.zip
 TL_TRANSLATIONS.it=	TYPOlight-Italian-v6.zip
-TL_TRANSLATIONS.ja=	TYPOlight-Japanese-v8.zip
+TL_TRANSLATIONS.ja=	TYPOlight-Japanese-v9.zip
 TL_TRANSLATIONS.lt=	TYPOlight-Lithuanian-v2.zip
-TL_TRANSLATIONS.lv=	TYPOlight-Latvian-v13.zip
-TL_TRANSLATIONS.nl=	TYPOlight-Dutch-v23.zip
+TL_TRANSLATIONS.lv=	TYPOlight-Latvian-v14.zip
+TL_TRANSLATIONS.nl=	TYPOlight-Dutch-v24.zip
 TL_TRANSLATIONS.no=	TYPOlight-Norwegian-v8.zip
-TL_TRANSLATIONS.pl=	TYPOlight-Polish-v17.zip
+TL_TRANSLATIONS.pl=	TYPOlight-Polish-v19.zip
 TL_TRANSLATIONS.pt=	TYPOlight-Portuguese-v2.zip
 TL_TRANSLATIONS.ro=	TYPOlight-Romanian-v4.zip
 TL_TRANSLATIONS.ru=	TYPOlight-Russian-v16.zip
