eabb2fe3d768fab84c5440ad861689e9b3d15956
minskim <minskim>
2008-09-24 19:32:00 +0000

    This version of bdb-xml requires db<4.7.  Depend on db46 instead of db4. This fixes the build error shown on Joerg's bulk builds on NetBSD-current.
    Bump PKGREVISION.

--- databases/bdb-xml/buildlink3.mk
+++ databases/bdb-xml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2008/09/06 20:54:31 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2008/09/24 19:32:00 minskim Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 BDB_XML_BUILDLINK3_MK:=	${BDB_XML_BUILDLINK3_MK}+
@@ -17,7 +17,7 @@ BUILDLINK_ABI_DEPENDS.bdb-xml?=	bdb-xml>=1.2.1nb8
 BUILDLINK_PKGSRCDIR.bdb-xml?=	../../databases/bdb-xml
 .endif	# BDB_XML_BUILDLINK3_MK
 
-.include "../../databases/db4/buildlink3.mk"
+.include "../../databases/db46/buildlink3.mk"
 .include "../../textproc/libpathan/buildlink3.mk"
 .include "../../textproc/xerces-c/buildlink3.mk"
 
