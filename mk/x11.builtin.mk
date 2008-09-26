43f2e9712facb8f5a85ccd6c2064fda34edac62f
cube <cube@pkgsrc.org>
2008-09-26 19:58:13 +0000

    Add a check for CHECK_BUILTIN.<BUILTIN_PKG>, set BUILDLINK_PREFIX in we're using the built-in version and include x11.buildlink3.mk.  This is the way x11.builtin.mk is currently used by all its consumer.  This will factor a bit of code out of the existing builtin.mk files and will make it a lot easier to use by other builtim.mk files.

--- mk/x11.builtin.mk
+++ mk/x11.builtin.mk
@@ -1,8 +1,19 @@
-# $NetBSD: x11.builtin.mk,v 1.2 2006/10/26 17:08:08 rillig Exp $
+# $NetBSD: x11.builtin.mk,v 1.2.20.1 2008/09/26 19:58:13 cube Exp $
 #
 # x11.builtin.mk will include the builtin.mk file from the appropriate
 # X11 distribution.
 #
+# BUILTIN_PKG and USE_BUILTIN.<BUILTIN_PKG> must be defined by the caller
 
-.include "../../mk/x11.version.mk"
-.include "${X11_PKGSRCDIR.${X11_TYPE}}/builtin.mk"
+CHECK_BUILTIN.${BUILTIN_PKG}?=	no
+.if !empty(CHECK_BUILTIN.${BUILTIN_PKG}:M[Nn][Oo])
+.  if !empty(USE_BUILTIN.${BUILTIN_PKG}:M[Yy][Ee][Ss])
+
+BUILDLINK_PREFIX.${BUILTIN_PKG}=	${X11BASE}
+
+.    include "../../mk/x11.buildlink3.mk"
+.    include "../../mk/x11.version.mk"
+.    include "${X11_PKGSRCDIR.${X11_TYPE}}/builtin.mk"
+
+.  endif
+.endif
