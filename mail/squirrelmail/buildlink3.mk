5b07cfba3d2f44c67d57b7f7fccc6c7ac8ff5a6f
martti <martti@pkgsrc.org>
2008-09-28 17:32:26 +0000

    Updated mail/squirrelmail to 1.4.16
    The SquirrelMail team is happy to announce the release 1.4.16. The most
    notable change is that cookies are now sent with the secure attribute set for
    HTTPS-connections, meaning that they cannot leak to an HTTP-connection on the
    same SquirrelMail installation. For details see the included ReleaseNotes. We
    advise users that offer their SquirrelMail both over HTTP and HTTPS to
    upgrade.

--- mail/squirrelmail/buildlink3.mk
+++ mail/squirrelmail/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2008/05/27 12:40:37 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2008/09/28 17:32:26 martti Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SQUIRRELMAIL_BUILDLINK3_MK:=	${SQUIRRELMAIL_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	squirrelmail
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}squirrelmail
 
 .if ${SQUIRRELMAIL_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.squirrelmail+=	squirrelmail>=1.4.15
+BUILDLINK_API_DEPENDS.squirrelmail+=	squirrelmail>=1.4.16
 BUILDLINK_PKGSRCDIR.squirrelmail?=	../../mail/squirrelmail
 .endif	# SQUIRRELMAIL_BUILDLINK3_MK
 
