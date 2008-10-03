2ce9edf1ae51e4ade62552fe1c55032961912a80
cube <cube@pkgsrc.org>
2008-10-03 02:02:47 +0000

    libXres provides libXRes.so, and evidently not the library of the same name as the package.

--- x11/libXres/builtin.mk
+++ x11/libXres/builtin.mk
@@ -1,8 +1,8 @@
-# $NetBSD: builtin.mk,v 1.1.2.2 2008/09/26 21:15:19 cube Exp $
+# $NetBSD: builtin.mk,v 1.1.2.3 2008/10/03 02:02:47 cube Exp $
 
 BUILTIN_PKG:=	libXres
 PKGCONFIG_FILE.libXres=	${X11BASE}/lib/pkgconfig/xres.pc \
-			${X11BASE}/lib/libXres.so
+			${X11BASE}/lib/libXRes.so
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
