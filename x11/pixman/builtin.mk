5097b167435436732328b15fb176275edf826680
tron <tron@pkgsrc.org>
2008-10-06 11:22:09 +0000

    Work arond the broken libtool archive "/usr/X11/lib/libpixman-1.la" under Mac OS X Leopard. This fixes build problems in e.g. the "cairo" package.

--- x11/pixman/builtin.mk
+++ x11/pixman/builtin.mk
@@ -1,7 +1,14 @@
-# $NetBSD: builtin.mk,v 1.4 2008/10/05 21:36:34 cube Exp $
+# $NetBSD: builtin.mk,v 1.5 2008/10/06 11:22:09 tron Exp $
 
 BUILTIN_PKG:=	pixman
 PKGCONFIG_FILE.pixman=	${X11BASE}/lib/pkgconfig/pixman-1.pc
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
+
+# Work around broken libtool archive "/usr/X11/lib/libpixman-1.la" under
+# Mac OS 10.5.4 or newer which references a non-existing version of the
+# PNG shared library.
+.if !empty(MACHINE_PLATFORM:MDarwin-9.*-*)
+BUILDLINK_TRANSFORM+=	rename:-lpixman-1.0.10.0:-lpixman-1
+.endif
