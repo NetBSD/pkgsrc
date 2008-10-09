2a2a6f5df7b3c9eedb2e86072a3ad041ef3010eb
tron <tron@pkgsrc.org>
2008-10-09 11:50:35 +0000

    Pullup ticket #2549 - requested by lukem tnftpd: security update
    Revisions pulled up:
    - net/tnftpd/Makefile		1.18
    - net/tnftpd/PLIST		1.3
    - net/tnftpd/distinfo		1.10
    - net/tnftpd/options.mk		1.2
    - net/tnftpd/patches/patch-aa	delete
    ---
    Module Name:	pkgsrc
    Committed By:	lukem
    Date:		Thu Oct  9 04:14:29 UTC 2008
    
    Modified Files:
    	pkgsrc/net/tnftpd: Makefile PLIST distinfo options.mk
    Removed Files:
    	pkgsrc/net/tnftpd/patches: patch-aa
    
    Log Message:
    Update to tnftpd 20081009.  Notable changes since 20061217:
    
    * Don't split large commands into multiple commands; just fail on them.
      This prevents cross-site request forgery (CSRF)-like attacks,
      when a web browser is used to access an ftp server.
    * Enhance -C to support an optional @host ('-C user[@host]'):
      checks whether user as connecting from host would be granted
      access by ftpusers(5).
    * Support IPv6 in the host directive of ftpusers(5).
    * Implement -n to disable hostname lookups.
    
    * Disable SOCKS support; I don't have the ability to test it,
      and the autoconf checks were very out of date.
    * Add configure --with-pam to enable PAM authentication support.
      Defaults to checking for PAM.
    * Add configure --with-skey to enable S/Key authentication support.
      Incompatible with --with-pam, defaults to no.
    * Fix pathnames in the installed manual pages to contain
      the appropriate $(prefix) substitution.
    * Use fcntl(3) locking instead of flock(3) or lockf(3).
    * Various other portability improvements.

--- net/tnftpd/options.mk
+++ net/tnftpd/options.mk
@@ -1,22 +1,17 @@
-# $NetBSD: options.mk,v 1.1 2005/12/02 17:03:54 wiz Exp $
+# $NetBSD: options.mk,v 1.1.28.1 2008/10/09 11:50:35 tron Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tnftpd
-PKG_SUPPORTED_OPTIONS=		inet6
-PKG_OPTIONS_OPTIONAL_GROUPS+=	socks
-PKG_OPTIONS_GROUP.socks=	socks4 socks5
+PKG_SUPPORTED_OPTIONS=	inet6 pam
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Msocks4)
-CONFIGURE_ARGS+=	--with-socks4
-.include "../../net/socks4/buildlink3.mk"
-.elif !empty(PKG_OPTIONS:Msocks5)
-CONFIGURE_ARGS+=	--with-socks5
-.include "../../net/socks5/buildlink3.mk"
-.endif
 
 .if !empty(PKG_OPTIONS:Minet6)
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
+.endif
+
+.if !empty(PKG_OPTIONS:Mpam)
+.include "../../mk/pam.buildlink3.mk"
+CONFIGURE_ARGS+=	--with-pam
 .endif
