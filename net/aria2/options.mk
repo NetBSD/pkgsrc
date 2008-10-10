751d6a32d5c4639c974c973cb3e0f49ee0e86878
bjs <bjs>
2008-10-10 01:56:38 +0000

    Update to aria2-0.16.0.  Lots of bug fixes, improved help text, plus firefox3 cookie support.  This option requires sqlite3, though the firefox3 option also enables this functionality.

--- net/aria2/options.mk
+++ net/aria2/options.mk
@@ -1,10 +1,12 @@
-# $NetBSD: options.mk,v 1.5 2008/04/16 00:09:39 bjs Exp $
+# $NetBSD: options.mk,v 1.6 2008/10/10 01:56:38 bjs Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.aria2
 
 PKG_OPTIONS_GROUP.ssl=		gnutls ssl
 PKG_OPTIONS_GROUP.asyncns=	ares cares
 PKG_OPTIONS_REQUIRED_GROUPS=	ssl asyncns
+
+PKG_SUPPORTED_OPTIONS=		sqlite firefox3
 PKG_SUGGESTED_OPTIONS=		ssl cares
 ###
 ### XXX Remove this in the future [2008-04-15 bjs]
@@ -17,19 +19,36 @@ PKG_OPTIONS_DEPRECATED_WARNINGS+=\
 .endif
 
 .include "../../mk/bsd.options.mk"
-
+###
+###	SSL/TLS implementation
+###
 .if !empty(PKG_OPTIONS:Mssl)
 CONFIGURE_ARGS+=	--with-openssl
-CONFIGURE_ARGS+=	--without-gnutls
+CONFIGURE_ARGS+=	--with-gnutls=no
 .  include "../../security/openssl/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--with-gnutls
-CONFIGURE_ARGS+=	--without-openssl
+CONFIGURE_ARGS+=	--with-openssl=no
 .  include "../../security/gnutls/buildlink3.mk"
 .endif
+###
+###	asynchronous resolver library.
+###
 .if !empty(PKG_OPTIONS:Mcares)
+CONFIGURE_ARGS+=	--with-libares=no
 CONFIGURE_ARGS+=	--with-libcares-prefix=${BUILDLINK_PREFIX.libcares:Q}
 .  include "../../net/libcares/buildlink3.mk"
 .elif !empty(PKG_OPTIONS:Mares)
+CONFIGURE_ARGS+=	--with-libcares=no
 CONFIGURE_ARGS+=	--with-libares-prefix=${BUILDLINK_PREFIX.libares:Q}
+.endif
+###
+###	firefox3 cookie	support via sqlite3
+###
+.if !empty(PKG_OPTIONS:Mfirefox3) || !empty(PKG_OPTIONS:Msqlite)
+CONFIGURE_ARGS+=	--with-sqlite3
+CONFIGURE_ARGS+=	--with-sqlite3-prefix=${BUILDLINK_PREFIX.sqlite:Q}
+.  include "../../databases/sqlite3/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--with-sqlite3=no
 .endif
