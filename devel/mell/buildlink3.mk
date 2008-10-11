458f7c95ef10ef9b23325574bf60b19acac09739
uebayasi <uebayasi@pkgsrc.org>
2008-10-11 09:31:54 +0000

    Move mk/emacs.mk to editors/emacs/modules.mk.
    Don't call pkg_info to get the installed Emacs version; always use the
    version matching EMACS_TYPE set by users.  Be DEPENDS to it.  This should
    address pkg/37146 by Aleksey Cheusov.
    
    While here convert some emacs lisp packages to user-destdir.

--- devel/mell/buildlink3.mk
+++ devel/mell/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/03/02 12:26:08 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/10/11 09:31:55 uebayasi Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 MELL_BUILDLINK3_MK:=	${MELL_BUILDLINK3_MK}+
 
-.include "../../mk/emacs.mk"
+.include "../../editors/emacs/modules.mk"
 
 .if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	mell
