bd9a9448b8a4fc7ca48d982387339ac50620b995
adrianp <adrianp@pkgsrc.org>
2008-10-12 12:50:17 +0000

    Update from 2.1.4->2.5.7 Use ./configure as one is now supplied libmxl2 is no longer optional but curl is Rename doc/eg dirs from ap-security to ap-modsecurity
    * Allow for disabling request body limit checks in phase:1
    * Now log XML parsing/validation warnings and errors to be in the debug log
      at levels 3 and 4, respectivly.
    * Transformation caching has been deprecated, and is now off by default. We
      now advise against using transformation caching in production.
    * Improve request body processing error messages.
    
    Any many more . . . see CHANGES for all the details

--- security/ap-modsecurity2/options.mk
+++ security/ap-modsecurity2/options.mk
@@ -1,9 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2007/03/18 10:35:13 adrianp Exp $
+# $NetBSD: options.mk,v 1.3 2008/10/12 12:50:17 adrianp Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ap-modsecurity2
 
-PKG_SUPPORTED_OPTIONS=	xml
-PKG_SUGGESTED_OPTIONS=	xml
+PKG_SUPPORTED_OPTIONS=	curl
 
 .include "../../mk/bsd.options.mk"
 
@@ -12,5 +11,18 @@ PKG_SUGGESTED_OPTIONS=	xml
 ###
 .if !empty(PKG_OPTIONS:Mxml)
 .  include "../../textproc/libxml2/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-libxml
 MESSAGE_SRC+=		${WRKDIR}/.MESSAGE_SRC.xml
+.else
+CONFIGURE_ARGS+=	--without-libxml
+.endif
+
+###
+### Bring in support for curl
+###
+.if !empty(PKG_OPTIONS:Mcurl)
+.  include "../../www/curl/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-curl=${PREFIX}/bin/curl
+.else
+CONFIGURE_ARGS+=	--without-curl
 .endif
