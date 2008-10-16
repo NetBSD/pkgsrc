1c8d8b9dcbc9268022521b205c4a28a5dcc80f9a
adam <adam@pkgsrc.org>
2008-10-16 21:34:07 +0000

    Changes 2.14.1: * pangomm is a separate package now * compatible with gtk+-2.14.x * bug fixes and improvements

--- x11/gtkmm/buildlink3.mk
+++ x11/gtkmm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2006/09/24 16:44:19 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2008/10/16 21:34:08 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GTKMM_BUILDLINK3_MK:=	${GTKMM_BUILDLINK3_MK}+
@@ -13,12 +13,11 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gtkmm
 
 .if !empty(GTKMM_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.gtkmm+=	gtkmm>=2.6.3
-BUILDLINK_ABI_DEPENDS.gtkmm+=	gtkmm>=2.10.1
+BUILDLINK_ABI_DEPENDS.gtkmm+=	gtkmm>=2.14.1
 BUILDLINK_PKGSRCDIR.gtkmm?=	../../x11/gtkmm
 .endif	# GTKMM_BUILDLINK3_MK
 
-.include "../../devel/glibmm/buildlink3.mk"
-.include "../../graphics/cairomm/buildlink3.mk"
+.include "../../devel/pangomm/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
