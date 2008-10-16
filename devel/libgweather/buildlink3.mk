d893579d186ff96fbfe99e590182fc49e3e30822
drochner <drochner@pkgsrc.org>
2008-10-16 13:36:46 +0000

    update to 2.24.0 This switches to the gnome-2.24 release branch.

--- devel/libgweather/buildlink3.mk
+++ devel/libgweather/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/04/11 19:01:24 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/10/16 13:36:46 drochner Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGWEATHER_BUILDLINK3_MK:=	${LIBGWEATHER_BUILDLINK3_MK}+
@@ -16,6 +16,7 @@ BUILDLINK_API_DEPENDS.libgweather+=	libgweather>=2.22.0
 BUILDLINK_PKGSRCDIR.libgweather?=	../../devel/libgweather
 .endif	# LIBGWEATHER_BUILDLINK3_MK
 
+.include "../../net/libsoup24/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .include "../../sysutils/gnome-vfs/buildlink3.mk"
 
