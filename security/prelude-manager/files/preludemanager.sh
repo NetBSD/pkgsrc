10133f0b206d23749bf7d4b6e4f2c5e99871d6eb
shannonjr <shannonjr@pkgsrc.org>
2008-10-18 13:31:12 +0000

    Prelude-manager now can send alerts via E-mail. Make start depend on mail.

--- security/prelude-manager/files/preludemanager.sh
+++ security/prelude-manager/files/preludemanager.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-# $NetBSD: preludemanager.sh,v 1.2 2006/01/31 17:54:10 shannonjr Exp $
+# $NetBSD: preludemanager.sh,v 1.3 2008/10/18 13:31:12 shannonjr Exp $
 #
 
 # PROVIDE: preludemanager
-# REQUIRE: mysqld LOGIN
+# REQUIRE: mysqld mail LOGIN
 
 $_rc_subr_loaded . /etc/rc.subr
 
