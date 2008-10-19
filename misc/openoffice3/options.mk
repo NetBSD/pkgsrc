11c82a449dd561588b2f6d3ece0fed08293154a4
hira <hira@pkgsrc.org>
2008-10-19 05:01:21 +0000

    Don't set PLIST.gtk2 to `no'.

--- misc/openoffice3/options.mk
+++ misc/openoffice3/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2008/10/18 01:05:08 hira Exp $
+# $NetBSD: options.mk,v 1.4 2008/10/19 05:01:21 hira Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.openoffice3
 PKG_SUPPORTED_OPTIONS=		cups gnome gtk2 java kde ooo-external-libwpd
@@ -74,7 +74,6 @@ PLIST.gtk2=		yes
 CONFIGURE_ARGS+=	--enable-gtk
 .include "../../x11/gtk2/buildlink3.mk"
 .else
-PLIST.gtk2=		no
 CONFIGURE_ARGS+=	--disable-gtk
 .endif
 
