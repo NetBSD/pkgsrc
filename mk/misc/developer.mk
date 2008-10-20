d71446336ed353a9584d5a67e73a0574237bd803
rillig <rillig@pkgsrc.org>
2008-10-20 10:19:18 +0000

    ... and added a few more keywords.

--- mk/misc/developer.mk
+++ mk/misc/developer.mk
@@ -1,4 +1,4 @@
-# $NetBSD: developer.mk,v 1.13 2008/10/20 10:17:40 rillig Exp $
+# $NetBSD: developer.mk,v 1.14 2008/10/20 10:19:18 rillig Exp $
 #
 # Public targets for developers:
 #
@@ -39,6 +39,9 @@
 # commit-changes-entry:
 # cce:
 #	Like changes-entry, plus the CHANGES file is committed.
+#
+# Keywords: commit update add rename changes
+#
 
 CTYPE?=			Updated
 NETBSD_LOGIN_NAME?=	${_NETBSD_LOGIN_NAME_cmd:sh}
