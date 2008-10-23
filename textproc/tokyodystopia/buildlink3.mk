078a96f0adc527f27a4e21d93152244b570307e6
obache <obache@pkgsrc.org>
2008-10-23 05:10:53 +0000

    Fixes a typo.

--- textproc/tokyodystopia/buildlink3.mk
+++ textproc/tokyodystopia/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/09/04 12:21:20 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/10/23 05:10:53 obache Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 TOKYODYSTOPIA_BUILDLINK3_MK:=	${TOKYODYSTOPIA_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}tokyodystopia
 
 .if ${TOKYODYSTOPIA_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.tokyodystopia+=	tokyodystopia>=0.9.5
-BUILDLINK_PKGSRCDIR.tokyodystopia?=	../../textproc/tokyodystpia
+BUILDLINK_PKGSRCDIR.tokyodystopia?=	../../textproc/tokyodystopia
 .endif	# TOKYODYSTOPIA_BUILDLINK3_MK
 
 .include "../../databases/tokyocabinet/buildlink3.mk"
