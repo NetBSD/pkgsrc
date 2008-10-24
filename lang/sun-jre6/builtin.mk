c62c3bc1ca938a9bd0a77129ed67114c1502f32b
adrianp <adrianp@pkgsrc.org>
2008-10-24 21:35:14 +0000

    Add support for builtin Java 1.6 on Darwin 9.*

---
+++ lang/sun-jre6/builtin.mk
@@ -0,0 +1,74 @@
+# $NetBSD: builtin.mk,v 1.1 2008/10/24 21:35:14 adrianp Exp $
+
+BUILTIN_PKG:=	sun-jre6
+
+BUILTIN_FIND_FILES_VAR:=		JAVAVM6
+BUILTIN_FIND_FILES.JAVAVM6=	\
+	/System/Library/Frameworks/JavaVM.framework/Versions/1.6
+
+.include "../../mk/buildlink3/bsd.builtin.mk"
+
+###
+### Determine if there is a built-in implementation of the package and
+### set IS_BUILTIN.<pkg> appropriately ("yes" or "no").
+###
+.if !defined(IS_BUILTIN.sun-jre6)
+.  if empty(JAVAVM6:M__nonexistent__)
+IS_BUILTIN.sun-jre6=	yes
+.  else
+IS_BUILTIN.sun-jre6=	no
+.  endif
+.endif
+MAKEVARS+=	IS_BUILTIN.sun-jre6
+
+###
+### If there is a built-in implementation, then set BUILTIN_PKG.<pkg> to
+### a package name to represent the built-in package.
+###
+.if !defined(BUILTIN_PKG.sun-jre6) && \
+    !empty(IS_BUILTIN.sun-jre6:M[yY][eE][sS]) && \
+    empty(JAVAVM6:M__nonexistent__)
+BUILTIN_VERSION.sun-jre6!=						\
+	${BASENAME} /System/Library/Frameworks/JavaVM.framework/Versions/1.6.*
+BUILTIN_PKG.sun-jre6=	sun-jre6-${BUILTIN_VERSION.sun-jre6}
+.endif
+MAKEVARS+=	BUILTIN_PKG.sun-jre6
+
+###
+### Determine whether we should use the built-in implementation if it
+### exists, and set USE_BUILTIN.<pkg> appropriate ("yes" or "no").
+###
+.if !defined(USE_BUILTIN.sun-jre6)
+.  if ${PREFER.sun-jre6} == "pkgsrc"
+USE_BUILTIN.sun-jre6=	no
+.  else
+USE_BUILTIN.sun-jre6=	${IS_BUILTIN.sun-jre6}
+.    if defined(BUILTIN_PKG.sun-jre6) && \
+        !empty(IS_BUILTIN.sun-jre6:M[yY][eE][sS])
+USE_BUILTIN.sun-jre6=	yes
+.      for _dep_ in ${BUILDLINK_API_DEPENDS.sun-jre6}
+.        if !empty(USE_BUILTIN.sun-jre6:M[yY][eE][sS])
+USE_BUILTIN.sun-jre6!=						\
+	if ${PKG_ADMIN} pmatch ${_dep_:Q} ${BUILTIN_PKG.sun-jre6:Q}; then \
+		${ECHO} yes;						\
+	else								\
+		${ECHO} no;						\
+	fi
+.        endif
+.      endfor
+.    endif
+.  endif  # PREFER.sun-jre6
+.endif
+MAKEVARS+=	USE_BUILTIN.sun-jre6
+
+###
+### The section below only applies if we are not including this file
+### solely to determine whether a built-in implementation exists.
+###
+CHECK_BUILTIN.sun-jre6?=	no
+.if !empty(CHECK_BUILTIN.sun-jre6:M[nN][oO])
+.  if !empty(USE_BUILTIN.sun-jre6:M[yY][eE][sS])
+PKG_JAVA_HOME= \
+	/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
+.  endif
+.endif
