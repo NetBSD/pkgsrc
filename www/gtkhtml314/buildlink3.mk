9d27e23663ee9b0fd6d022b21cd01e9d498611dd
wiz <wiz@pkgsrc.org>
2008-10-24 20:31:54 +0000

    Include textproc/enchant/buildlink3.mk here as well.

--- www/gtkhtml314/buildlink3.mk
+++ www/gtkhtml314/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2008/10/24 19:27:18 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2008/10/24 20:31:54 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GTKHTML314_BUILDLINK3_MK:=	${GTKHTML314_BUILDLINK3_MK}+
@@ -18,5 +18,6 @@ BUILDLINK_PKGSRCDIR.gtkhtml314?=	../../www/gtkhtml314
 
 .include "../../devel/libglade/buildlink3.mk"
 .include "../../devel/libgnomeui/buildlink3.mk"
+.include "../../textproc/enchant/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
