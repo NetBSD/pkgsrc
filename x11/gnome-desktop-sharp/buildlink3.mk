c29358feec53d56425d62398a34d84c23fd95287
wiz <wiz@pkgsrc.org>
2008-10-24 20:59:54 +0000

    Update to 2.24.0:
    2008-08-27  Mike Kestner  <mkestner@novell.com>
    
    	* gnomedesktop/gnomedesktop-sharp.dll.config.in: fix so version.
    	* gnomepanel/gnome-panel-sharp.dll.config.in: add missing dllmap.
    	Fixes for 420142 and 420145 from Goetz Waschk.
    
    2008-08-20  Mike Kestner  <mkestner@novell.com>
    
    	* configure.in: update gtk-sharp requirement to 2.12.1 since the
    	gnomepanel glue generation doesn't occur using 2.12.0.  This breaks
    	the build since it's expecting a generated.c to be present.
    	[Fixes #418291]
    
    2008-08-06  Mike Kestner  <mkestner@novell.com>
    
    	* *: Update to GNOME 2.23.5 API.  Add gnome-print-sharp.dll
    	and gnome-panel-sharp.dll assemblies, along with .pcs to detect
    	their availability.

--- x11/gnome-desktop-sharp/buildlink3.mk
+++ x11/gnome-desktop-sharp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2008/09/06 20:54:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2008/10/24 20:59:54 wiz Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 GNOME_DESKTOP_SHARP_BUILDLINK3_MK:=	${GNOME_DESKTOP_SHARP_BUILDLINK3_MK}+
@@ -12,20 +12,19 @@ BUILDLINK_PACKAGES+=	gnome-desktop-sharp
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gnome-desktop-sharp
 
 .if ${GNOME_DESKTOP_SHARP_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.gnome-desktop-sharp+=	gnome-desktop-sharp>=2.20.1
-BUILDLINK_ABI_DEPENDS.gnome-desktop-sharp?=	gnome-desktop-sharp>=2.20.1nb1
+BUILDLINK_API_DEPENDS.gnome-desktop-sharp+=	gnome-desktop-sharp>=2.24.0
 BUILDLINK_PKGSRCDIR.gnome-desktop-sharp?=	../../x11/gnome-desktop-sharp
 .endif	# GNOME_DESKTOP_SHARP_BUILDLINK3_MK
 
-.include "../../lang/mono/buildlink3.mk"
-.include "../../lang/monodoc/buildlink3.mk"
 .include "../../devel/libwnck/buildlink3.mk"
 .include "../../graphics/librsvg/buildlink3.mk"
+.include "../../print/libgnomeprint/buildlink3.mk"
+.include "../../print/libgnomeprintui/buildlink3.mk"
 .include "../../www/gtkhtml314/buildlink3.mk"
-.include "../../x11/vte/buildlink3.mk"
-.include "../../x11/gnome-desktop/buildlink3.mk"
+.include "../../x11/gnome-panel/buildlink3.mk"
 .include "../../x11/gnome-sharp/buildlink3.mk"
 .include "../../x11/gtk-sharp/buildlink3.mk"
 .include "../../x11/gtksourceview2/buildlink3.mk"
+.include "../../x11/vte/buildlink3.mk"
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
