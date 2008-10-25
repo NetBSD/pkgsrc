0d128642ad23fe0076582f71e12e195498eba4f3
adrianp <adrianp>
2008-10-25 18:11:16 +0000

    Add an opengrok-source option to build the war locally instead of installing  the one that comes with the tarball.

--- devel/opengrok/options.mk
+++ devel/opengrok/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1.1.1 2007/04/08 16:59:41 adrianp Exp $
+# $NetBSD: options.mk,v 1.2 2008/10/25 18:11:16 adrianp Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.opengrok
 
-PKG_SUPPORTED_OPTIONS=	svn
+PKG_SUPPORTED_OPTIONS=	svn opengrok-build
 
 .include "../../mk/bsd.options.mk"
 
@@ -12,4 +12,18 @@ PKG_SUPPORTED_OPTIONS=	svn
 .if !empty(PKG_OPTIONS:Msvn)
 .  include "../../devel/subversion-base/buildlink3.mk"
 BUILDLINK_API_DEPENDS.subversion-base+= subversion-base>=1.3.0
+.endif
+
+###
+### Rebuild OpenGrok
+###
+.if !empty(PKG_OPTIONS:Mopengrok-build)
+DEPENDS+=	jflex-[0-9]*:../../devel/jflex
+DEPENDS+=	apache-ant-[0-9]*:../../devel/apache-ant
+USE_JAVA=	yes
+
+OPENGROK_SOURCES=	${PKGNAME_NOREV}-src${EXTRACT_SUFX}
+DISTFILES+=		${OPENGROK_SOURCES}
+EXTRACT_ONLY+=		${OPENGROK_SOURCES}
+WRKSRC=			${WRKDIR}/${PKGNAME_NOREV}-src
 .endif
