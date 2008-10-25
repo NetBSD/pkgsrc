f544319abd4abe6733845cdd99681e59ed004ee5
wiz <wiz>
2008-10-25 11:15:16 +0000

    Update to 2.24.1:
    2.24.1:
    Bug fixes and translation updates.
    
    ====================================================
    Epiphany 2.24.0.1
    ====================================================
    
    In this release, the address entry has improved logic which should
    result in faster autocompletion lookups.
    
    Bug fixes:
    
    * Crash when showing download notification bubble (bug #536768)
    * Window title copied the status bar (bug #524587)
    * Fix RDF import (bug #523414)
    * Proxy password prompt missing with gecko 1.9 (bug #539417)
    * Password manager doesn't remove passwords with gecko 1.9 (bug #539418)
    * Printing scales incorrectly (bug #541168)
    * do not activate the smart bookmark entry after middle-clicking paste
      text into it. (bug #378165)
    * Address entry fixes:
     - substring suggestions (bug #151932)
     - unicode support (bug #343906)
     - diacritics in topic keywords (bug #328162)
     - completion on history items titles (bug #534218)
    
    Enhancements:
    
    * Update lock icon to a tango style one (bug #547936)
    * Documentation updates (bug #552436, #552555, #534744)
    * Enable complete-download sound with libcanberra
    * revert special handling of double click in the address entry (bug
    #426349)
    
    Contributors to this release:
    Diego Escalante Urrelo, Sebastian Keller, Josselin Mouette, Mike Hommey,
    Paul Drain, Cosimo Cecchi, Bruce Cowan, Lucas Lommer, Colin Walters,
    Loïc Minier, Vincent Untz, Christian Persch, Reinout van Schouwen
    
    Translators:
    Jorge Gonzalez (es), Kjartan Maraas (nb), Khaled Hosny (ar), Ivar Smolin
    (et), Sweta Kothari, Reinout van Schouwen (nl), Daniel Nylander (sv),
    Yair Hershkovitz (he), icq, Lucas Lommer (cz), Duarte Loreto (pt), Gil
    Forcada (ca), Takeshi AIHANA (ja), sprasad, Theppitak Karoonboonyanan
    (th), ituohela (fi), Robert-André Mauchin (fr), grakic (sr@latin), Inaki
    Larranaga Murgoitio (eu), pgeyleg (dz), Hendrik Richter (de), Nguyễn
    Thái Ngọc Duy (vi), Funda Wang (zh_CN), Philip Withnall (en_GB), Claude
    Peroz (fr), mateju, rranjan (hi), apravi, Sankarshan Mukhopadhyay,
    Gintautas Miliauskas (lt), Baris Ciçek (tr), Gabor Kelemen (hu), ifelix,
    sandeeps, kelemeng, cwryu (kr), Alexander Shopov (bg), tvainika (fi),
    Nickolay V. Shmyrev (ru), Mugurel Tudor (ro), Ask H. Larsen (dk)

--- www/epiphany/options.mk
+++ www/epiphany/options.mk
@@ -1,17 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2006/09/24 16:28:20 salo Exp $
+# $NetBSD: options.mk,v 1.2 2008/10/25 11:15:16 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.epiphany
+PKG_SUPPORTED_OPTIONS=	enchant # avahi
+PKG_SUGGESTED_OPTIONS=	enchant
 
-.include "../../www/seamonkey/gecko-options.mk"
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mfirefox)
-CPPFLAGS+=	-I${BUILDLINK_PREFIX.firefox}/include/firefox/find
-CFLAGS+=	-I${BUILDLINK_PREFIX.firefox}/include/firefox/find
-CPPFLAGS+=	-I${BUILDLINK_PREFIX.firefox}/include/firefox/necko
-CFLAGS+=	-I${BUILDLINK_PREFIX.firefox}/include/firefox/necko
-CPPFLAGS+=	-I${BUILDLINK_PREFIX.firefox}/include/firefox/pref
-CFLAGS+=	-I${BUILDLINK_PREFIX.firefox}/include/firefox/pref
-.else
-BROKEN=		seamonkey backend is currently not supported
+.if !empty(PKG_OPTIONS:Menchant)
+CONFIGURE_ARGS+=	--enable-spell-checker
+.include "../../textproc/enchant/buildlink3.mk"
 .endif
