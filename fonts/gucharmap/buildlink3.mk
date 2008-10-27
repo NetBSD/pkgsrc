5d2ae66c65993d107b38efda31a7a4feafff23a4
wiz <wiz>
2008-10-27 14:37:30 +0000

    Update to 2.24.1. API changed since 2.22, but only two packages in pkgsrc depend on it directly, and one already supports the new API, so I'm just updating the package and bumping the API depends.
    gucharmap 2.24.1
    
    - Updated translations.
    
    gucharmap 2.24.0
    
    - Updated translations.
    
    gucharmap 2.23.3
    
    - API changed; the pkg-config name is now "gucharmap-2"
    - support for Unicode 5.1
    - gtk-doc API docs
    - python bindings
    - many bug fixes

--- fonts/gucharmap/buildlink3.mk
+++ fonts/gucharmap/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2008/03/06 14:53:50 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2008/10/27 14:37:30 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GUCHARMAP_BUILDLINK3_MK:=	${GUCHARMAP_BUILDLINK3_MK}+
@@ -12,8 +12,7 @@ BUILDLINK_PACKAGES+=	gucharmap
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gucharmap
 
 .if !empty(GUCHARMAP_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.gucharmap+=	gucharmap>=1.4.0
-BUILDLINK_ABI_DEPENDS.gucharmap+=	gucharmap>=1.10.2nb1
+BUILDLINK_API_DEPENDS.gucharmap+=	gucharmap>=2.24.1
 BUILDLINK_PKGSRCDIR.gucharmap?=	../../fonts/gucharmap
 .endif	# GUCHARMAP_BUILDLINK3_MK
 
