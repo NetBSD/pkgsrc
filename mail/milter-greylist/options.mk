ae081e3a6ada5f39564d0533c1146477ab5cb139
tron <tron@pkgsrc.org>
2008-10-27 19:40:11 +0000

    Use "libspf2" if SPF support is enabled (the default) because "libspf_alt" is unmaintained and leaks memory. Bump package revision because of this change.
    Problem pointed out by Fredrik Pettai in private e-mail.

--- mail/milter-greylist/options.mk
+++ mail/milter-greylist/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2007/12/14 18:57:54 tron Exp $
+# $NetBSD: options.mk,v 1.5 2008/10/27 19:40:11 tron Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.milter-greylist
 PKG_SUPPORTED_OPTIONS=		dnsrbl drac spf
@@ -27,8 +27,10 @@ CONFIGURE_ARGS+=	--enable-drac --with-drac-db=${DRACD_DB}
 ###
 ### Sender Policy Framework
 ###
-.if !empty(PKG_OPTIONS:Mspf)
-.include "../../mail/libspf-alt/buildlink3.mk"
+.if empty(PKG_OPTIONS:Mspf)
+CONFIGURE_ARGS+=	--disable-libspf2
+.else
+.include "../../mail/libspf2/buildlink3.mk"
 
-CONFIGURE_ARGS+=	--with-libspf_alt=${PREFIX:Q}
+CONFIGURE_ARGS+=	--with-libspf2=${PREFIX:Q}
 .endif
