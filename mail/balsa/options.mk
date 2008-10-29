77654115cd9b3ecd092c9fbdbe47604962cc376b
wiz <wiz>
2008-10-29 00:18:51 +0000

    Update to 2.3.26, and enable many more features by default.
    Changes:
    * Balsa-2.3.26 release. Release date 2008-09-07
    
    - composing format=flowed messages with spaces pasted in works better.
    - allow to explicitly specify a GPG key id for an identity.
    
    * Balsa-2.3.25 release. Release date 2008-06-14
    
    - GMail compatibility.
    - notification icon in GNOME panel is optional now.
    - wrapping of flowed text improved.
    - lots of various bug fixes.
    
    * Balsa-2.3.24 release. Release date 2008-05-31
    
    - Support for accessing files over gnome-vfs (smb, ssh mounts, etc.)
    - maildir tree scanning fixes.
    - Rudimentary support for sending text/html messages.
    - UIDPLUS IMAP extension support.
    
    * Balsa-2.3.23 release. Release date 2008-03-20
    
    - Suppport for IMAP MULTIAPPEND extension for faster uploading of
      message batches to and from IMAP servers.
    - improved scanning of mailbox trees.
    - GtkSourceView2 support.
    
    * Balsa-2.3.22 release. Release date 2007-12-23
    
    - new address edition widget in the compose window.
    - compose window displays languages available for spell checking.
    - more new icons.
    
    * Balsa-2.3.21 release. Release date 2007-12-08
    
    - migration from GnomeUI to GtkUIManager
    - Rubrica address book support.
    - bugs fixed: GNOME: 481842, 488432, 496316; Redhat: 414361.
    
    * Balsa-2.3.20 release. Release date 2007-09-07
    
    - fix buffer overflow (#474366).
    - avoid sending messages twice under certain conditions.
    
    * Balsa-2.3.19 release. Release date 2007-08-25
    
    - more build fixes.
    
    * Balsa-2.3.18 release. Release date 2007-08-22
    
    - reflow improvements.
    - Build problems against steaming-hot GNOME fixed.
    - Make BINARY and ESEARCH IMAP extensions usage more robust.
    
    * Balsa-2.3.17 release. Release date 2007-07-04
    
    - check validity of server APOP challenge.
    - add ability to store VCARD signatures.
    - use ESEARCH IMAP extension if available.
    - message wrapping fixes.
    - and an usual bunch of bugfixes.
    
    * Balsa-2.3.16 release. Release date 2007-05-27
    
    - fix message threading regression.
    - reorganize "Identity" dialog.
    - code cleanup.
    
    * Balsa-2.3.15 release. Release date 2007-05-01
    
    - Use GtkPrint infrastructure if available.
    - handle LDAP referrals.
    - nicer citation marks.
    - initial libnotify support.
    - IMAP BINARY support.
    
    * Balsa-2.3.14 release. Release date 2006-12-12
    
    - deferred population of the mailbox index.
    - client-side IMAP sorting.
    - be compatible with recent gtk2 releases.
    - mailbox tabs reorderable with d&d.
    - enhanced mailto: URL handling.
    
    * Balsa-2.3.13 release. Release date 2006-06-26
    
    - many small fixes
    - nicer icons.
    
    * Balsa-2.3.12 release. Release date 2006-03-04
    
    - do not reference freed memory in mbox driver.
    
    * Balsa-2.3.11 release. Release date 2006-02-28
    
    - assure messages are properly signed after identity change.
    - Ctrl-N goes over unread messages in all mailboxes.
    - Redone Preference Manager.
    - support better slow LDAP servers.
    - use better real estate when displaying message counts.
    - fixed bugs: 330085, 330093, 332980.

--- mail/balsa/options.mk
+++ mail/balsa/options.mk
@@ -1,14 +1,25 @@
-# $NetBSD: options.mk,v 1.1 2007/09/20 20:50:50 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2008/10/29 00:18:51 wiz Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.balsa
-PKG_SUPPORTED_OPTIONS=	ldap ssl
+PKG_SUPPORTED_OPTIONS=	compface ldap sqlite ssl
+PKG_SUGGESTED_OPTIONS=	compface ldap sqlite ssl
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mcompface)
+CONFIGURE_ARGS+=	--with-compface
+.  include "../../graphics/compface/buildlink3.mk"
+.endif
 
 .if !empty(PKG_OPTIONS:Mldap)
 CONFIGURE_ARGS+=	--with-ldap
 .  include "../../databases/openldap-client/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Msqlite)
+CONFIGURE_ARGS+=	--with-sqlite
+.  include "../../databases/sqlite3/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mssl)
