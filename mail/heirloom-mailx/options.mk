0ecf2aba58e28fb57b95b1ee8a904e0cd4b6ef3c
obache <obache@pkgsrc.org>
2008-10-30 13:01:00 +0000

    Import heirloom-mailx-12.4 as mail/heirloom-mailx. Based on PR 38262, some portability improvement and ipv6 support optionify.
    mailx (Heirloom Mailx; formerly nail) is a mail user agent derived from
    Berkeley Mail 8.1. It is intended to provide the functionality of the
    POSIX.2 mailx command with built-in support for MIME messages. This means
    it can handle international character sets as well as attachments. In
    recent system environments, nail is Unicode/UTF-8 capable. It further
    contains some minor enhancements like the ability to set a "From:" Address.

---
+++ mail/heirloom-mailx/options.mk
@@ -0,0 +1,16 @@
+# $NetBSD: options.mk,v 1.1.1.1 2008/10/30 13:01:00 obache Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.heirloom-mailx
+PKG_SUPPORTED_OPTIONS=	inet6
+
+.include "../../mk/bsd.prefs.mk"
+
+.if !empty(IPV6_READY:M[Yy][Ee][Ss])
+PKG_SUGGESTED_OPTIONS+=	inet6
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Minet6)
+MAKE_ENV+=		IPv6=-DHAVE_IPv6_FUNCS
+.endif
