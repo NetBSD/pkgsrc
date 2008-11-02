8403b121f6ca17f6f488b749f0579b1380093a13
bjs <bjs@pkgsrc.org>
2008-11-02 01:55:53 +0000

    Enable support for libmagic (sysutils/file).

--- net/gift/buildlink3.mk
+++ net/gift/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2006/07/08 23:11:03 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2008/11/02 01:55:53 bjs Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GIFT_BUILDLINK3_MK:=	${GIFT_BUILDLINK3_MK}+
@@ -19,5 +19,6 @@ BUILDLINK_PKGSRCDIR.gift?=	../../net/gift
 
 .include "../../devel/libltdl/buildlink3.mk"
 .include "../../audio/libvorbis/buildlink3.mk"
+.include "../../sysutils/file/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
