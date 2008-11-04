cfa2fbfd92d9fd3b51a4b7938530680cf840d008
markd <markd@pkgsrc.org>
2008-11-04 10:07:38 +0000

    Import qimageblitz 0.0.4
    Interm graphical effect and filter library for KDE4.0

---
+++ graphics/qimageblitz/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/11/04 10:07:38 markd Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+QIMAGEBLITZ_BUILDLINK3_MK:=	${QIMAGEBLITZ_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	qimageblitz
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nqimageblitz}
+BUILDLINK_PACKAGES+=	qimageblitz
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}qimageblitz
+
+.if ${QIMAGEBLITZ_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.qimageblitz+=	qimageblitz>=0.0.4
+BUILDLINK_PKGSRCDIR.qimageblitz?=	../../graphics/qimageblitz
+.endif	# QIMAGEBLITZ_BUILDLINK3_MK
+
+.include "../../x11/qt4-libs/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
