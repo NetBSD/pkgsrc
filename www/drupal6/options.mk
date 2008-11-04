3c56cb7c0a2b0baca0fd00a8ef922083aeae7298
tron <tron>
2008-11-04 22:16:02 +0000

    Pullup ticket #2571 - requested by adrianp drupal6: security update
    www/drupal6/Makefile				1.7-1.9
    www/drupal6/PLIST				1.2
    www/drupal6/distinfo				1.5-1.6
    www/drupal6/options.mk				1.2
    ---
    Module Name:	pkgsrc
    Committed By:	adrianp
    Date:		Sun Oct 12 00:34:40 UTC 2008
    
    Modified Files:
    	pkgsrc/www/drupal6: Makefile distinfo
    
    Log Message:
    This release fixes security vulnerabilities. Sites are urged to upgrade immediately after reading the security announcement:
    
        * SA-2008-060 - Drupal core - Multiple vulnerabilities
    
    In addition to this security vulnerability, the following bugs have been fixed since the 6.4 release:
    
    * - Patch 246143 by bjaspan, Damien Tournoud: make sure updates are run in numeric order, not in definition order.
    * - Patch 221230 by Heine: convert requirement error on update to requirement warning.
    * - Patch 252430 by quicksketch: allow base theme prefix in preprocessor function names to correct expected behavior.
    * - Patch 245322 by mfb: fixed breadcrumb behavior.
    * - Patch 287949 by Freso, Damien Tournoud: keep language icons in consistent order across nodes.
    * - Patch 265899 by mfb: uri_brief mail token did not support https URLs.
    * - Patch 272952 by NancyDru and chx: fixed documentation issue.
    * - Patch 170310 by mfb, JohnAlbin: avoid SSL cookie getting over-written by non-SSL cookie.
    * - Patch 243063 by GoofyX: fixed typo in context-sensitve help.
    * - Patch 295152 by dww, Damien Tournoud, et al: fixed version comparison.
    * - Patch 278759 by douggreen, fletchgqc: improved code comment.
    * - Patch 276018 by mfb: extend the lifetime of temporary files.
    * - Patch 228576 by sun: too ambiguous stylesheet in dblog.css when form_altering the watchdog table.
    * - Patch 285309 by pwolanin: menu_name in hook_menu is ignored on updates.
    * - Patch 261859 by rse, Damien Tournoud: make the trigger module work on PostgreSQL.
    * - Patch 305436 by Damien Tournoud, lelutin: fixed unclosed <li> tag in the context-sensitive help.
    
    Any many more.  See http://drupal.org/node/318701 for all the details
    ---
    Module Name:	pkgsrc
    Committed By:	adrianp
    Date:		Thu Oct 23 21:37:23 UTC 2008
    
    Modified Files:
    	pkgsrc/www/drupal6: Makefile distinfo
    
    Log Message:
    Update to 6.6
    
    The sixth maintenance and security release of the Drupal 6 series. Only
    fixes for security vulnerabilities and other bugs have been committed. New
    features are only being added to the forthcoming Drupal 7.0 release.
    
    This release fixes security vulnerabilities. Sites are urged to upgrade
    immediately after reading the security announcement:
    
    * SA-2008-067 - Drupal core - Multiple vulnerabilities
    
    In addition to this security vulnerability, the following bugs have been
    fixed since the 6.5 release:
    
    - Patch #315656 by Damien Tournoud: fixed bug in drupal_lookup_path('wipe').
    #318102 by Dave Reid: hook_exit() was not invoked for some cached requests.
    #277206 by Damien Tournoud, lilou, fp: untranslatable string in the installer
    - Patch #324080 by winterheart: missing </td>-tag.
    
    See http://drupal.org/node/324832 for all the details
    ---
    Module Name:	pkgsrc
    Committed By:	adrianp
    Date:		Fri Oct 24 18:52:32 UTC 2008
    
    Modified Files:
    	pkgsrc/www/drupal6: Makefile PLIST options.mk
    
    Log Message:
    Fix PLIST issues when using binary packages - spotted by martti@
    After some feedback from Roy Marples set up the package so it's easier
     to get drupal to run under other web servers than apache.  As the
     default web server, apache will remain.  Users can disable it using
     the options.mk framework.
    Rename APACHE_* variables to WWW_* and set some sane defaults.

--- www/drupal6/options.mk
+++ www/drupal6/options.mk
@@ -1,12 +1,12 @@
-# $NetBSD: options.mk,v 1.1.1.1 2008/04/06 10:15:27 adrianp Exp $
+# $NetBSD: options.mk,v 1.1.1.1.8.1 2008/11/04 22:16:02 tron Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.drupal
 
 PKG_OPTIONS_REQUIRED_GROUPS=	db
 PKG_OPTIONS_GROUP.db=		mysql pgsql
 
-PKG_SUPPORTED_OPTIONS=	drupal-xmlservices
-PKG_SUGGESTED_OPTIONS=	mysql drupal-xmlservices
+PKG_SUPPORTED_OPTIONS=	drupal-xmlservices apache
+PKG_SUGGESTED_OPTIONS=	mysql drupal-xmlservices apache
 
 .include "../../mk/bsd.options.mk"
 
@@ -34,4 +34,17 @@ DEPENDS+=	${PHP_PKG_PREFIX}-domxml>=4.3.3:../../textproc/php4-domxml
 .	elif ${PKG_PHP_VERSION} == "5"
 DEPENDS+=	${PHP_PKG_PREFIX}-dom-[0-9]*:../../textproc/php5-dom
 .	endif
+.endif
+
+###
+### Drupal can run on a number of web servers, we support apache by default
+###
+.if !empty(PKG_OPTIONS:Mapache)
+DEPENDS+=	${APACHE_PKG_PREFIX}-${PHP_PKG_PREFIX}>=4.3.5:../../www/ap-php
+.	include "../../mk/apachever.mk"
+WWW_USER?=	${APACHE_USER}
+WWW_GROUP?=	${APACHE_GROUP}
+.else
+WWW_USER?=	${ROOT_USER}
+WWW_GROUP?=	${ROOT_GROUP}
 .endif
