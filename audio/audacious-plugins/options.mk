06d38d3d798b6c5211551a60a01b2b8003919681
wiz <wiz@pkgsrc.org>
2008-11-10 11:08:27 +0000

    Use PLIST_VARS framework.

--- audio/audacious-plugins/options.mk
+++ audio/audacious-plugins/options.mk
@@ -1,58 +1,58 @@
-# $NetBSD: options.mk,v 1.2 2008/11/03 06:52:59 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2008/11/10 11:08:27 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.audacious-plugins
 PKG_SUPPORTED_OPTIONS=	arts esound inet6 jack midi # pulse sid
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=	sid
 #.if !empty(PKG_OPTIONS:Msid)
 #.  include "../../wip/resid-builder/buildlink3.mk"
-#PLIST_SUBST+=		SID=""
+#PLIST.sid=	yes
 #.else
 #CONFIGURE_ARGS+=	--disable-sid
-#PLIST_SUBST+=		SID="@comment "
 #.endif
 
+PLIST_VARS+=	arts
 .if !empty(PKG_OPTIONS:Marts)
 .  include "../../audio/arts/buildlink3.mk"
-PLIST_SUBST+=		ARTS=""
+PLIST.arts=	yes
 .else
 CONFIGURE_ARGS+=	--disable-arts
-PLIST_SUBST+=		ARTS="@comment "
 .endif
 
+PLIST_VARS+=	esd
 .if !empty(PKG_OPTIONS:Mesound)
 .  include "../../audio/esound/buildlink3.mk"
-PLIST_SUBST+=		ESD=""
+PLIST.esd=	yes
 .else
 CONFIGURE_ARGS+=	--disable-esd
-PLIST_SUBST+=		ESD="@comment "
 .endif
 
 .if !empty(PKG_OPTIONS:Minet6)
 CONFIGURE_ARGS+=	--enable-ipv6
 .endif
 
+PLIST_VARS+=	jack
 .if !empty(PKG_OPTIONS:Mjack)
 .  include "../../audio/jack/buildlink3.mk"
-PLIST_SUBST+=		JACK=""
+PLIST.jack=	yes
 .else
 CONFIGURE_ARGS+=	--disable-jack
-PLIST_SUBST+=		JACK="@comment "
 .endif
 
+PLIST_VARS+=	midi
 .if !empty(PKG_OPTIONS:Mmidi)
 BUILD_DEPENDS+=		timidity>=0:../../audio/timidity
-PLIST_SUBST+=		MIDI=""
+PLIST.midi=	yes
 .else
 CONFIGURE_ARGS+=	--disable-timidity
-PLIST_SUBST+=		MIDI="@comment "
 .endif
 
+PLIST_VARS+=	pulse
 #.if !empty(PKG_OPTIONS:Mpulse)
 #.  include "../../wip/pulseaudio/buildlink3.mk"
-#PLIST_SUBST+=		PULSE=""
+#PLIST.pulse=	yes
 #.else
 #CONFIGURE_ARGS+=	--disable-pulse
-#PLIST_SUBST+=		PULSE="@comment "
 #.endif
