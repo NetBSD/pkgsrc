db3f41ba13336a7628d159510152ce71775c0083
chuck <chuck@pkgsrc.org>
2008-11-13 17:36:54 +0000

    ghostscript has hand-rolled shared lib handling in src/unix-dll.mak that does not do the right thing on Darwin (where shared libs are created with different linking flags and use dylib rather than so as an extension).
    you need to manually reconfigure unix-dll.mak to make it work, so
    do it in hacks.mk.
    
    XXX: this still leaves the issue of libgs.so (et al.) being hardwired
    in PLIST, instead of using libgs.dylib (et al.).   i'm not sure of
    the best way to fix that.   at least it compiles now....

--- print/ghostscript/hacks.mk
+++ print/ghostscript/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2007/01/09 21:03:02 tv Exp $
+# $NetBSD: hacks.mk,v 1.2 2008/11/13 17:36:54 chuck Exp $
 
 ### [Tue Jan  9 15:57:05 EST 2007 : tv]
 ### Interix has u_int64_t, but not uint64_t
@@ -8,4 +8,15 @@
 .if ${OPSYS} == "Interix"
 PKG_HACKS+=		interix-uint64_t
 CPPFLAGS.Interix+=	-Duint64_t=u_int64_t
+.endif
+
+### [ Thu Nov 13 12:30:45 EST 2008 : chuck]
+### gs has hand-rolled shared lib handling that doesn't do the right
+### thing on Darwin by default, you need to reconfigure unix-dll.mak
+### to make it work.
+.if ${OPSYS} == "Darwin"
+post-patch:
+	${SED} -e 's/^#Darwin#//' ${WRKSRC}/src/unix-dll.mak > \
+		${WRKSRC}/src/unix-darwin.mak
+	${MV} ${WRKSRC}/src/unix-darwin.mak ${WRKSRC}/src/unix-dll.mak
 .endif
