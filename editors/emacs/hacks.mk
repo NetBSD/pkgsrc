3e952fd72cb5385bbdf7518f4eec84700d234e7a
tron <tron>
2008-11-14 16:05:15 +0000

    The work around for PR pkg/39778 is also necessary under NetBSD 5.0_BETA and newer.

--- editors/emacs/hacks.mk
+++ editors/emacs/hacks.mk
@@ -1,9 +1,17 @@
-# $NetBSD: hacks.mk,v 1.1 2008/10/30 03:57:06 minskim Exp $
+# $NetBSD: hacks.mk,v 1.2 2008/11/14 16:05:15 tron Exp $
+
+.if !defined(EMACS_HACKS_MK)
+EMACS_HACKS_MK=	# defined
+
 
 ###
-### Workaround for PR 39778
+### Workaround for PR pkg/39778
 ###
-.if !empty(MACHINE_PLATFORM:MNetBSD-4.99.*-x86_64)
+.  include "../../mk/bsd.fast.prefs.mk"
+.  if !empty(MACHINE_PLATFORM:MNetBSD-4.99.*-x86_64) || \
+      !empty(MACHINE_PLATFORM:MNetBSD-[5-9].*-x86_64)
 pre-build:
 	${TOUCH} ${WRKSRC}/leim/quail/tsang-b5.el
+.  endif
+
 .endif
