76e9835466984d9781a0827b0b71755e3bb3c24c
adam <adam>
2008-11-21 17:40:29 +0000

    Wine is a program which allows running Microsoft Windows programs (including DOS, Windows 3.x and Win32 executables) on Unix.  It consists of a program loader which loads and executes an Microsoft Windows binary, and a library that implements Windows API calls using their Unix or X11 equivalents.  The library may also be used for porting Win32 code into native Unix executables.

---
+++ emulators/wine-devel/buildlink3.mk
@@ -0,0 +1,30 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/11/21 17:40:31 adam Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+WINE_BUILDLINK3_MK:=	${WINE_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	wine
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nwine}
+BUILDLINK_PACKAGES+=	wine
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}wine
+
+.if ${WINE_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.wine+=	wine>=0.9.6nb1
+BUILDLINK_PKGSRCDIR.wine?=	../../emulators/wine
+.endif	# WINE_BUILDLINK3_MK
+
+pkgbase:= wine
+.include "../../mk/pkg-build-options.mk"
+
+.include "../../graphics/freetype2/buildlink3.mk"
+.if !empty(PKG_BUILD_OPTIONS.wine:Mopengl)
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../graphics/glut/buildlink3.mk"
+.endif
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/libungif/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
