45cdf09671274e512a1090b8bdab7e75985fd717
wiz <wiz@pkgsrc.org>
2008-11-22 17:35:15 +0000

    Separate options handling into options.mk.

---
+++ x11/gdm/options.mk
@@ -0,0 +1,35 @@
+# $NetBSD: options.mk,v 1.1 2008/11/22 17:35:15 wiz Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.gdm
+PKG_SUPPORTED_OPTIONS=	inet6 pam consolekit
+PKG_SUGGESTED_OPTIONS=	consolekit
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--enable-ipv6
+.else
+CONFIGURE_ARGS+=	--disable-ipv6
+.endif
+
+.if !empty(PKG_OPTIONS:Mconsolekit)
+CONFIGURE_ARGS+=	--with-console-kit=yes
+.include "../../sysutils/consolekit/buildlink3.mk"
+.include "../../sysutils/dbus/buildlink3.mk"
+.include "../../sysutils/dbus-glib/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--with-console-kit=no
+.endif
+
+PLIST_VARS+=		pam
+.if !empty(PKG_OPTIONS:Mpam)
+.  include "../../mk/pam.buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-authentication-scheme=pam
+.  if ${PAM_TYPE} == "linux-pam"
+PLIST.pam=		yes
+.  endif
+.elif exists(/etc/shadow)
+CONFIGURE_ARGS+=	--enable-authentication-scheme=shadow
+.else
+CONFIGURE_ARGS+=	--enable-authentication-scheme=crypt
+.endif
