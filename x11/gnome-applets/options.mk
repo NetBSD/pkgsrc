5af6959b7826431f5a605915dfd18f47a7ca613f
jmcneill <jmcneill@pkgsrc.org>
2008-11-24 15:02:18 +0000

    enable hal and policykit, bump PKGREVISION

--- x11/gnome-applets/options.mk
+++ x11/gnome-applets/options.mk
@@ -1,7 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2008/10/27 04:23:01 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2008/11/24 15:02:18 jmcneill Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnome-applets
-PKG_SUPPORTED_OPTIONS=	inet6
+PKG_SUPPORTED_OPTIONS=	inet6 hal
+PKG_SUGGESTED_OPTIONS+=	hal
 
 .include "../../mk/bsd.options.mk"
 
@@ -9,4 +10,13 @@ PKG_SUPPORTED_OPTIONS=	inet6
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
+.endif
+
+.if !empty(PKG_OPTIONS:Mhal)
+.include "../../sysutils/hal/buildlink3.mk"
+.include "../../security/policykit/buildlink3.mk"
+.include "../../security/policykit-gnome/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-polkit
+CONFIGURE_ARGS+=	--without-hal
 .endif
