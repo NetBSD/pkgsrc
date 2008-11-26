cc0a12b284920952ca6f7427faf9681c2f703817
drochner <drochner@pkgsrc.org>
2008-11-26 12:19:56 +0000

    add gimp-2.6.3 first cut on a pkg for gimp-2.6 (might replace gimp-2.4 after some testing)

---
+++ graphics/gimp26/buildlink3.mk
@@ -0,0 +1,48 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/11/26 12:19:58 drochner Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GIMP_BUILDLINK3_MK:=	${GIMP_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gimp
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngimp}
+BUILDLINK_PACKAGES+=	gimp
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gimp
+
+.if !empty(GIMP_BUILDLINK3_MK:M+)
+BUILDLINK_API_DEPENDS.gimp+=	gimp>=2.4.0
+BUILDLINK_ABI_DEPENDS.gimp+=	gimp>=2.4.4
+BUILDLINK_PKGSRCDIR.gimp?=	../../graphics/gimp
+.endif	# GIMP_BUILDLINK3_MK
+
+pkgbase := gimp
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.gimp:Maalib)
+.include "../../graphics/aalib/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.gimp:Mgnome)
+.include "../../www/libgtkhtml/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.gimp:Mmng)
+.include "../../graphics/mng/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.gimp:Msvg)
+.include "../../graphics/librsvg/buildlink3.mk"
+.endif
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../graphics/lcms/buildlink3.mk"
+.include "../../graphics/libart/buildlink3.mk"
+.include "../../graphics/libexif/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/libwmf/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
