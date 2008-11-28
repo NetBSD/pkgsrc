e8305e106a0300b73c74dcf4aca619acc861c3e9
jmcneill <jmcneill@pkgsrc.org>
2008-11-28 14:29:31 +0000

    Add hal support, bump pkgrevision

--- graphics/cheese/options.mk
+++ graphics/cheese/options.mk
@@ -1,14 +1,14 @@
-# $NetBSD: options.mk,v 1.1.1.1 2008/09/07 22:43:00 jmcneill Exp $
+# $NetBSD: options.mk,v 1.2 2008/11/28 14:29:31 jmcneill Exp $
 #
 PKG_OPTIONS_VAR=	PKG_OPTIONS.cheese
 PKG_SUPPORTED_OPTIONS=	hal
+PKG_SUGGESTED_OPTIONS=	hal
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mhal)
 CONFIGURE_ARGS+=	--enable-hal
-# XXX FIXME
-#.  include "../../sysutils/hal/buildlink3.mk"
+.include "../../sysutils/hal/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--enable-hal=no
 .endif
