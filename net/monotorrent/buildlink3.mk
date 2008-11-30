b59fb97b8ebd02e5b4bcb1f553362ae2cfae9030
kefren <kefren@pkgsrc.org>
2008-11-30 11:26:35 +0000

    add buildlink file required by net/monsoon

---
+++ net/monotorrent/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2008/11/30 11:26:35 kefren Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+MONOTORRENT_BUILDLINK3_MK:=	${MONOTORRENT_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	monotorrent
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmonotorrent}
+BUILDLINK_PACKAGES+=	monotorrent
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}monotorrent
+
+.if ${MONOTORRENT_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.monotorrent+=	monotorrent>=0.62
+BUILDLINK_PKGSRCDIR.monotorrent?=	../../net/monotorrent
+.endif	# MONOTORRENT_BUILDLINK3_MK
+
+.include "../../lang/mono/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
