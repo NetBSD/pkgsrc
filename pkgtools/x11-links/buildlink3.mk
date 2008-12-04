23abb33176382c15d0748bb90fd3cefab86c4e64
cube <cube@pkgsrc.org>
2008-12-04 04:17:21 +0000

    Bump version after libXtst builtinification.  Sorry guys.

--- pkgtools/x11-links/buildlink3.mk
+++ pkgtools/x11-links/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2008/11/11 16:19:23 cube Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2008/12/04 04:17:21 cube Exp $
 #
 # Don't include this file manually!  It will be included as necessary
 # by bsd.buildlink3.mk.
@@ -18,7 +18,7 @@ BUILDLINK_PACKAGES:=	x11-links ${BUILDLINK_PACKAGES}
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}x11-links
 
 .if !empty(X11_LINKS_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.41
+BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.42
 BUILDLINK_PKGSRCDIR.x11-links?=	../../pkgtools/x11-links
 BUILDLINK_DEPMETHOD.x11-links?=	build
 
