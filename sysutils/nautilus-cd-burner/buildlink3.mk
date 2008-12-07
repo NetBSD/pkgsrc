1cb533d75952c651525c399a3503a3502c6bceb7
wiz <wiz>
2008-12-07 15:16:53 +0000

    Fix includes. Bump version.

--- sysutils/nautilus-cd-burner/buildlink3.mk
+++ sysutils/nautilus-cd-burner/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.16 2008/03/06 14:53:54 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2008/12/07 15:16:53 wiz Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 NAUTILUS_CD_BURNER_BUILDLINK3_MK:=	${NAUTILUS_CD_BURNER_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	nautilus-cd-burner
 .endif
 
@@ -11,12 +11,13 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nnautilus-cd-burner}
 BUILDLINK_PACKAGES+=	nautilus-cd-burner
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}nautilus-cd-burner
 
-.if !empty(NAUTILUS_CD_BURNER_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.nautilus-cd-burner+=		nautilus-cd-burner>=2.12.1nb1
-BUILDLINK_ABI_DEPENDS.nautilus-cd-burner?=	nautilus-cd-burner>=2.14.3nb9
+.if ${NAUTILUS_CD_BURNER_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.nautilus-cd-burner+=	nautilus-cd-burner>=2.24.0
 BUILDLINK_PKGSRCDIR.nautilus-cd-burner?=	../../sysutils/nautilus-cd-burner
 .endif	# NAUTILUS_CD_BURNER_BUILDLINK3_MK
 
-.include "../../sysutils/nautilus/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../sysutils/hal/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
