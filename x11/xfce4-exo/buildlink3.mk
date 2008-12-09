b825b057a76fa266d01d29ce4598112fe82148c0
ahoka <ahoka@pkgsrc.org>
2008-12-09 10:33:36 +0000

    Enable HAL support.

--- x11/xfce4-exo/buildlink3.mk
+++ x11/xfce4-exo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2007/12/07 19:43:27 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2008/12/09 10:33:36 ahoka Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XFCE4_EXO_BUILDLINK3_MK:=	${XFCE4_EXO_BUILDLINK3_MK}+
@@ -15,6 +15,13 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-exo
 BUILDLINK_API_DEPENDS.xfce4-exo+=	xfce4-exo>=0.3.4
 BUILDLINK_PKGSRCDIR.xfce4-exo?=	../../x11/xfce4-exo
 .endif	# XFCE4_EXO_BUILDLINK3_MK
+
+pkgbase := xfce4-exo
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.xfce4-exo:Mhal)
+.include "../../sysutils/hal/buildlink3.mk"
+.endif
 
 .include "../../graphics/hicolor-icon-theme/buildlink3.mk"
 .include "../../x11/xfce4-mcs-plugins/buildlink3.mk"
