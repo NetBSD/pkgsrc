1aed07f30c79c693bfa2428247bb3e4f9195bec6
reinoud <reinoud>
2008-12-11 18:25:17 +0000

    Fix typo

---
+++ sysutils/hal/files/hald-netbsd/devinfo_optical.h
@@ -0,0 +1,20 @@
+/***************************************************************************
+ *
+ * devinfo_optical.h : definition for optical (MMC compliant) media
+ *
+ * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
+ * Use is subject to license terms.
+ *
+ * Licensed under the Academic Free License version 2.1
+ *
+ **************************************************************************/
+
+#ifndef DEVINFO_OPTICAL_H
+#define DEVINFO_OPTICAL_H
+
+#include "devinfo.h"
+
+extern DevinfoDevHandler devinfo_optical_handler;
+
+#endif /* DEVINFO_OPTICAL_H */
+
