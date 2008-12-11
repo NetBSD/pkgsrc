33221daf3fbe418cb222184f437a415a33400be5
taca <taca@pkgsrc.org>
2008-12-11 16:26:49 +0000

    Update drupal6-translations pacakge to 20081207.
    * Add Itarian(it) translation.
    * Update Spanish(es) translation
    * Add last modified date of each translation in comment.

--- www/drupal6-translations/options.mk
+++ www/drupal6-translations/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2008/10/12 13:05:01 taca Exp $
+# $NetBSD: options.mk,v 1.6 2008/12/11 16:26:49 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.drupal-translations
 
@@ -8,30 +8,38 @@ PKG_OPTIONS_VAR=	PKG_OPTIONS.drupal-translations
 # Broken directory structure: eo vi
 # Development version only: ar fr nn pt th tr
 # Empty, LICENSE.it.txt only: it
-DL_SUPPORTED_LANGUAGES=	cs da de el es fi gu he hu ja lt nb nl pl pt-br ro \
+DL_SUPPORTED_LANGUAGES=	cs da de el es fi gu he hu it ja lt nb nl pl pt-br ro \
 			sk sv uk-ua zh-cn zh-tw
 
-DL_TRANSLATIONS.cs=	cs-6.x-1.9.tar.gz
-DL_TRANSLATIONS.da=	da-6.x-1.0.tar.gz
-DL_TRANSLATIONS.de=	de-6.x-1.0-rc1.tar.gz
-DL_TRANSLATIONS.el=	el-6.x-1.0-beta1.tar.gz
-DL_TRANSLATIONS.es=	es-6.x-1.0.tar.gz
-DL_TRANSLATIONS.fi=	fi-6.x-1.0.tar.gz
-DL_TRANSLATIONS.gu=	gu-6.x-1.0.tar.gz
-DL_TRANSLATIONS.he=	he-6.x-1.0.tar.gz
-DL_TRANSLATIONS.hu=	hu-6.x-1.0.tar.gz
-DL_TRANSLATIONS.ja=	ja-6.x-1.2.tar.gz
-DL_TRANSLATIONS.lt=	lt-6.x-1.2.tar.gz
-DL_TRANSLATIONS.nb=	nb-6.x-1.0.tar.gz
-DL_TRANSLATIONS.nl=	nl-6.x-1.1.tar.gz
-DL_TRANSLATIONS.pl=	pl-6.x-0.3.tar.gz
-DL_TRANSLATIONS.pt-br=	pt-br-6.x-1.0-rc5.tar.gz
-DL_TRANSLATIONS.ro=	ro-6.x-1.1.tar.gz
-DL_TRANSLATIONS.sk=	sk-6.x-1.0.tar.gz
-DL_TRANSLATIONS.sv=	sv-6.x-1.1.tar.gz
-DL_TRANSLATIONS.uk-ua=	uk-ua-6.x-1.2-beta3.tar.gz
-DL_TRANSLATIONS.zh-cn=	zh-hans-6.x-1.0.tar.gz
-DL_TRANSLATIONS.zh-tw=	zh-hant-6.x-1.1.tar.gz
+DL_TRANSLATIONS.cs=	cs-6.x-1.9.tar.gz		# 2008/09/29
+DL_TRANSLATIONS.da=	da-6.x-1.0.tar.gz		# 2008/08/25
+DL_TRANSLATIONS.de=	de-6.x-1.0-rc1.tar.gz		# 2008/08/16
+DL_TRANSLATIONS.el=	el-6.x-1.0-beta1.tar.gz		# 2008/04/05
+#DL_TRANSLATIONS.eo=	eo-6.x-1.0.tar.gz		# 2008/05/31
+DL_TRANSLATIONS.es=	es-6.x-1.2.tar.gz		# 2008/11/17
+DL_TRANSLATIONS.fi=	fi-6.x-1.0.tar.gz		# 2008/09/10
+#DL_TRANSLATIONS.fr=	fr-6.x-1.x-dev.tar.gz		# 2008/08/29
+DL_TRANSLATIONS.gu=	gu-6.x-1.0.tar.gz		# 2008/06/11
+DL_TRANSLATIONS.he=	he-6.x-1.0.tar.gz		# 2008/03/14
+DL_TRANSLATIONS.hu=	hu-6.x-1.0.tar.gz		# 2008/02/15
+DL_TRANSLATIONS.it=	it-6.x-1.4.tar.gz		# 2008/12/07
+DL_TRANSLATIONS.ja=	ja-6.x-1.2.tar.gz		# 2008/04/12
+DL_TRANSLATIONS.lt=	lt-6.x-1.2.tar.gz		# 2008/02/26
+DL_TRANSLATIONS.nb=	nb-6.x-1.0.tar.gz		# 2008/10/10
+DL_TRANSLATIONS.nl=	nl-6.x-1.1.tar.gz		# 2008/07/19
+#DL_TRANSLATIONS.nl=	nn-6.x-1.x-dev.tar.gz		# 2008/05/13
+DL_TRANSLATIONS.pl=	pl-6.x-0.3.tar.gz		# 2008/09/08
+DL_TRANSLATIONS.pt-br=	pt-br-6.x-1.0-rc5.tar.gz	# 2008/08/28
+#DL_TRANSLATIONS.pt-br=	pt-pt-6.x-1.x-dev.tar.gz	# 2008/11/30
+DL_TRANSLATIONS.ro=	ro-6.x-1.1.tar.gz		# 2008/02/21
+DL_TRANSLATIONS.sk=	sk-6.x-1.0.tar.gz		# 2008/10/02
+DL_TRANSLATIONS.sv=	sv-6.x-1.1.tar.gz		# 2008/09/25
+#DL_TRANSLATIONS.th=	th-6.x-1.x-dev.tar.gz		# 2008/05/22
+#DL_TRANSLATIONS.tr=	tr-6.x-1.x-dev.tar.gz		# 2008/05/29
+DL_TRANSLATIONS.uk-ua=	uk-ua-6.x-1.2-beta3.tar.gz	# 2008/10/03
+#DL_TRANSLATIONS.vi=	sv-6.x-1.x-dev.tar.gz		# 2008/03/26
+DL_TRANSLATIONS.zh-cn=	zh-hans-6.x-1.0.tar.gz		# 2008/06/01
+DL_TRANSLATIONS.zh-tw=	zh-hant-6.x-1.1.tar.gz		# 2008/05/14
 
 .for l in ${DL_SUPPORTED_LANGUAGES}
 PKG_SUPPORTED_OPTIONS+=	lang-${l}
