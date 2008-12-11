12eaaeb2694b84682ed0d356bae1dc63539e0dbc
taca <taca>
2008-12-11 02:30:22 +0000

    Update typolight-translations package to 20081209.
    * Update Bulgarian translation which catch up to TYPOlight 2.6.2.
    * Fix PLIST for Romanian (PLIST.ro) which should be updated by previous commit.
    * Update French and Latvian translations.

--- www/typolight-translations/options.mk
+++ www/typolight-translations/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.8 2008/12/09 07:28:16 taca Exp $
+# $NetBSD: options.mk,v 1.9 2008/12/11 02:30:22 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.typolight-translations
 
@@ -6,17 +6,17 @@ TL_SUPPORTED_LANGUAGES=	af bg ca da es fa fr is it ja lt lv nl no \
 			pl pt ro ru sk sq sr sv th tr uk zh
 
 TL_TRANSLATIONS.af=	TYPOlight-Afrikaans-v1.zip	# 2008/01/15
-TL_TRANSLATIONS.bg=	TYPOlight-Bulgarian-v11.zip	# 2008/09/21
+TL_TRANSLATIONS.bg=	TYPOlight-Bulgarian-v12.zip	# 2008/12/09
 TL_TRANSLATIONS.ca=	TYPOlight-Catalan-v4.zip	# 2008/09/24
 TL_TRANSLATIONS.da=	TYPOlight-Danish-v5.zip		# 2008/04/28
 TL_TRANSLATIONS.es=	TYPOlight-Spanish-v10.zip	# 2008/07/15
 TL_TRANSLATIONS.fa=	TYPOlight-Persian-v2.zip	# 2008/06/05
-TL_TRANSLATIONS.fr=	TYPOlight-French-v33.zip	# 2008/11/01
+TL_TRANSLATIONS.fr=	TYPOlight-French-v34.zip	# 2008/12/08
 TL_TRANSLATIONS.is=	TYPOlight-Icelandic-v2.zip	# 2008/01/31
 TL_TRANSLATIONS.it=	TYPOlight-Italian-v8.zip	# 2008/11/02
 TL_TRANSLATIONS.ja=	TYPOlight-Japanese-v10.zip	# 2008/11/06
 TL_TRANSLATIONS.lt=	TYPOlight-Lithuanian-v2.zip	# 2008/04/25
-TL_TRANSLATIONS.lv=	TYPOlight-Latvian-v15.zip	# 2008/11/04
+TL_TRANSLATIONS.lv=	TYPOlight-Latvian-v16.zip	# 2008/12/09
 TL_TRANSLATIONS.nl=	TYPOlight-Dutch-v25.zip		# 2008/11/04
 TL_TRANSLATIONS.no=	TYPOlight-Norwegian-v8.zip	# 2008/09/02
 TL_TRANSLATIONS.pl=	TYPOlight-Polish-v19.zip	# 2008/09/21
