c20cd612434a9e3f3acb2886a80b94383bb8af28
taca <taca@pkgsrc.org>
2008-12-12 12:45:41 +0000

    Update typolight-translations pacakge to 20081212.
    Add Czech language translation.
    Update Japanese translation and it reflects fix of Online translator.

--- www/typolight-translations/options.mk
+++ www/typolight-translations/options.mk
@@ -1,20 +1,21 @@
-# $NetBSD: options.mk,v 1.9 2008/12/11 02:30:22 taca Exp $
+# $NetBSD: options.mk,v 1.10 2008/12/12 12:45:41 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.typolight-translations
 
-TL_SUPPORTED_LANGUAGES=	af bg ca da es fa fr is it ja lt lv nl no \
+TL_SUPPORTED_LANGUAGES=	af bg ca cs da es fa fr is it ja lt lv nl no \
 			pl pt ro ru sk sq sr sv th tr uk zh
 
 TL_TRANSLATIONS.af=	TYPOlight-Afrikaans-v1.zip	# 2008/01/15
 TL_TRANSLATIONS.bg=	TYPOlight-Bulgarian-v12.zip	# 2008/12/09
 TL_TRANSLATIONS.ca=	TYPOlight-Catalan-v4.zip	# 2008/09/24
+TL_TRANSLATIONS.cs=	TYPOlight-Czech-v2.zip		# 2008/12/11
 TL_TRANSLATIONS.da=	TYPOlight-Danish-v5.zip		# 2008/04/28
 TL_TRANSLATIONS.es=	TYPOlight-Spanish-v10.zip	# 2008/07/15
 TL_TRANSLATIONS.fa=	TYPOlight-Persian-v2.zip	# 2008/06/05
 TL_TRANSLATIONS.fr=	TYPOlight-French-v34.zip	# 2008/12/08
 TL_TRANSLATIONS.is=	TYPOlight-Icelandic-v2.zip	# 2008/01/31
 TL_TRANSLATIONS.it=	TYPOlight-Italian-v8.zip	# 2008/11/02
-TL_TRANSLATIONS.ja=	TYPOlight-Japanese-v10.zip	# 2008/11/06
+TL_TRANSLATIONS.ja=	TYPOlight-Japanese-v12.zip	# 2008/12/12
 TL_TRANSLATIONS.lt=	TYPOlight-Lithuanian-v2.zip	# 2008/04/25
 TL_TRANSLATIONS.lv=	TYPOlight-Latvian-v16.zip	# 2008/12/09
 TL_TRANSLATIONS.nl=	TYPOlight-Dutch-v25.zip		# 2008/11/04
