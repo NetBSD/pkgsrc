3a623d22e81a1064a16ae3397a429b9c9baaecf8
obache <obache@pkgsrc.org>
2008-12-13 08:52:13 +0000

    * For w3m-img, imagelib is not optional, but required. * Add support of gtk2 image lib. * Allow to enable imagelib support for www/w3m.

--- www/w3m/options.mk
+++ www/w3m/options.mk
@@ -1,15 +1,17 @@
-# $NetBSD: options.mk,v 1.8 2008/12/13 04:21:28 obache Exp $
+# $NetBSD: options.mk,v 1.9 2008/12/13 08:52:13 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.w3m
 PKG_SUPPORTED_OPTIONS=	inet6 w3m-lynx-key w3m-m17n w3m-unicode
 PKG_SUGGESTED_OPTIONS=	w3m-m17n
 
 .if ${_W3M_USE_IMAGE} == "YES"
-PKG_OPTIONS_OPTIONAL_GROUPS=	imagelib
-PKG_OPTIONS_GROUP.imagelib=	w3m-image-gdk-pixbuf w3m-image-imlib	\
-				w3m-image-imlib2
+PKG_OPTIONS_REQUIRED_GROUPS+=	imagelib
 PKG_SUGGESTED_OPTIONS+=		w3m-image-gdk-pixbuf
+.else
+PKG_OPTIONS_OPTIONAL_GROUPS+=	imagelib
 .endif
+PKG_OPTIONS_GROUP.imagelib=	w3m-image-gdk-pixbuf w3m-image-imlib	\
+				w3m-image-imlib2 w3m-image-gtk2
 
 # Memo about W3m configuration:
 #
@@ -54,7 +56,7 @@ CONFIGURE_ARGS+=	--enable-keymap=w3m
 .endif
 
 PLIST_VARS+=		image
-.if ${_W3M_USE_IMAGE} == "YES"
+.if !empty(PKG_OPTIONS:Mw3m-image-*)
 .  for _imagelib in ${PKG_OPTIONS_GROUP.imagelib}
 .    if !empty(PKG_OPTIONS:M${_imagelib})
 _W3M_IMAGELIB=		${_imagelib:C|^w3m-image-||}
@@ -65,7 +67,12 @@ CONFIGURE_ARGS+=	--enable-image=x11 --with-imagelib=${_W3M_IMAGELIB:Q}
 PLIST.image=		yes
 BUILDLINK_DEPMETHOD.libXt?=	build
 .  include "../../x11/libXt/buildlink3.mk"
-.  include "../../graphics/${_W3M_IMAGELIB}/buildlink3.mk"
+.  if empty(_W3M_IMAGELIB:Mgtk2)
+.    include "../../graphics/${_W3M_IMAGELIB}/buildlink3.mk"
+.  else
+USE_TOOLS+=	pkg-config
+.    include "../../x11/${_W3M_IMAGELIB}/buildlink3.mk"
+.  endif
 .else
 CONFIGURE_ARGS+=	--disable-image
 .endif
