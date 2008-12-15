897d704c86e241f3105396edb132ad9d540ad661
roy <roy@pkgsrc.org>
2008-12-15 15:55:42 +0000

    An implementation of the Proxy ARP Daemon specified in RFC1027.

--- net/parpd/files/parpd.sh
+++ net/parpd/files/parpd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: parpd.sh,v 1.1.1.1 2008/10/09 00:24:28 joerg Exp $
+# $NetBSD: parpd.sh,v 1.1.1.2 2008/12/15 15:55:42 roy Exp $
 #
 
 # PROVIDE: parpd
@@ -11,7 +11,7 @@ $_rc_subr_loaded . /etc/rc.subr
 name="parpd"
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
-required_files="@PKG_SYSCONFDIR@/${name}.conf"
+required_files="@PREFIX@/etc/${name}.conf"
 
 load_rc_config $name
 run_rc_command "$1"
