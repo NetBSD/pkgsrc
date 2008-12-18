0946ff49c727318b7629cd5d0eae706a6752e6d7
bjs <bjs@pkgsrc.org>
2008-12-18 16:19:07 +0000

    Update to 20081218 snapshot.  There is no NEWS file or list of changes; please see the repository for more information.  I'm doing this update our version is over a year old and some software is starting to require a newer version.

--- multimedia/x264-devel/buildlink3.mk
+++ multimedia/x264-devel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2008/03/10 11:59:26 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2008/12/18 16:19:07 bjs Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 X264_DEVEL_BUILDLINK3_MK:=	${X264_DEVEL_BUILDLINK3_MK}+
@@ -12,8 +12,22 @@ BUILDLINK_PACKAGES+=	x264-devel
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}x264-devel
 
 .if !empty(X264_DEVEL_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.x264-devel+=	x264-devel>=20060127
+###
+### NOTE: When changing BUILDLINK_A[BP]I_DEPENDS.x264-devel, please record
+###	  the value of X264_BUILD in x264.h along with the appropriate
+###	  dependency specification.  Perhaps we should incorporate this
+###	  into the version number and/or pkg-config file?
+###
+###	  Prior specification: x264-devel>=20060127 # X264_BUILD 44
+###
+BUILDLINK_API_DEPENDS.x264-devel+=	x264-devel>=20081212 # X264_BUILD 65
 BUILDLINK_PKGSRCDIR.x264-devel?=	../../multimedia/x264-devel
+pkgbase:= x264-devel
+.  include "../../mk/pkg-build-options.mk"
 .endif	# X264_DEVEL_BUILDLINK3_MK
+
+.if !empty(PKG_BUILD_OPTIONS.x264-devel:Mthreads)
+.  include "../../mk/pthread.buildlink3.mk"
+.endif
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
