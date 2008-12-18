7fdedf73be48119f3780c0c9d12edab138187376
hira <hira@pkgsrc.org>
2008-12-18 16:43:28 +0000

    Bump BUILDLINK_API_DEPENDS.

--- graphics/xfce4-icon-theme/buildlink3.mk
+++ graphics/xfce4-icon-theme/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2007/12/07 19:43:21 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2008/12/18 16:43:28 hira Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_ICON_THEME_BUILDLINK3_MK:=	${XFCE4_ICON_THEME_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	xfce4-icon-theme
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-icon-theme
 
 .if ${XFCE4_ICON_THEME_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.xfce4-icon-theme+=	xfce4-icon-theme>=4.4.2
+BUILDLINK_API_DEPENDS.xfce4-icon-theme+=	xfce4-icon-theme>=4.4.3
 BUILDLINK_PKGSRCDIR.xfce4-icon-theme?=	../../graphics/xfce4-icon-theme
 .endif	# XFCE4_ICON_THEME_BUILDLINK3_MK
 
