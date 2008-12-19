4a47d70aec136168235b04de7a00cfa5dc3e7715
joerg <joerg>
2008-12-19 12:49:54 +0000

    Make patch appliable.

--- www/ap-auth-ntlm/patches/patch-ntlmssp.inc.c
+++ www/ap-auth-ntlm/patches/patch-ntlmssp.inc.c
@@ -1,8 +1,6 @@
-$NetBSD: patch-ntlmssp.inc.c,v 1.1.1.1 2008/10/17 19:23:25 sborrill Exp $
+$NetBSD: patch-ntlmssp.inc.c,v 1.2 2008/12/19 12:49:54 joerg Exp $
 --- ntlmssp.inc.c.orig	2004-02-19 15:24:08.000000000 +0000
 +++ ntlmssp.inc.c	2008-10-17 17:53:34.000000000 +0100
-@@ -1,3 +1,4 @@
+@@ -1,1 +1,2 @@
 +#include "httpd/httpd.h"
  /*
-  * $Id: patch-ntlmssp.inc.c,v 1.1.1.1 2008/10/17 19:23:25 sborrill Exp $
-  *
