6e77b5c9ce368b68d35d4bc022922470fffea34f
reed <reed@pkgsrc.org>
2008-12-20 20:52:27 +0000

    Make the gnome build an option for evince. Defaults to enabled. So -gnome will build the GTK version.
    Make should also do the --disable-dbus, but will think about that later.

--- print/evince/options.mk
+++ print/evince/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2008/10/25 00:11:13 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2008/12/20 20:52:27 reed Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.evince
-PKG_SUPPORTED_OPTIONS=	dvi djvu
-PKG_SUGGESTED_OPTIONS=	# none
+PKG_SUPPORTED_OPTIONS=	dvi djvu gnome
+PKG_SUGGESTED_OPTIONS=	gnome
 
 .include "../../mk/bsd.options.mk"
 
@@ -21,3 +21,13 @@ PLIST.djvu=		yes
 GCONF_SCHEMAS+=	evince-thumbnailer-djvu.schemas
 .include "../../graphics/djvulibre-lib/buildlink3.mk"
 .endif
+
+.if !empty(PKG_OPTIONS:Mgnome)
+COMMENT+=		for the GNOME Desktop
+BUILDLINK_API_DEPENDS.libgnomeui+=	libgnomeui>=2.14.0
+.include "../../devel/libgnomeui/buildlink3.mk"
+.else
+COMMENT+=		(GTK version)
+CONFIGURE_ARGS+=	--without-libgnome --disable-nautilus
+.endif
+
