aa413c5e38ba87c357857bebde758650d395d426
martti <martti>
2008-12-20 20:47:02 +0000

    Updated editors/vim-share to 7.2.69
      8484  7.2.050  compiler warnings for not using return value of fwrite()
     15179  7.2.051  can't avoid 'wig' and 'suffixes' for glob() and globpath()
      2611  7.2.052  synIDattr() doesn't support "sp" for special color
      1754  7.2.053  crash when using WorkShop command ":ws foo"
      2006  7.2.054  compilation warnings for fprintf format
     34319  7.2.055  various compiler warnings with strict checking
      1635  7.2.056  (after 7.2.050) tests 58 and 59 fail
      3210  7.2.057  (after 7.2.056) trying to put size_t in int variable
      2338  7.2.058  can't add a feature name in the :version output
      1847  7.2.059  diff is not always displayed properly
     34772  7.2.060  spell checking doesn't work well for compound words
      1886  7.2.061  creating funcref requires loading the autoload script first
      1657  7.2.062  "[Scratch]" is not translated
      3558  7.2.063  warning for NULL argument of Perl_sys_init3()
      1942  7.2.064  repeating "~" on a Visual block doesn't always update screen
      5149  7.2.065  GTK GUI: cursor disappears doing ":vsp" when maximized
      2759  7.2.066  not easy to check if 'encoding' is a multi-byte encoding
      1683  7.2.067  can't load sesison extra file when it contains special chars
      2598  7.2.068  error when Emacs tags file line is too long
      1726  7.2.069  (after 7.2.060) compiler warning for putting size_t in int

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.61 2008/11/21 08:00:31 martti Exp $
+# $NetBSD: version.mk,v 1.62 2008/12/20 20:47:02 martti Exp $
 
 VIM_VERSION=		7.2
-VIM_PATCHLEVEL=		49
+VIM_PATCHLEVEL=		69
 VIM_SUBDIR=		vim72
