6e9974d9bbed31c452868df12696fcb6f6cf1784
obache <obache@pkgsrc.org>
2008-12-21 12:09:42 +0000

    Negative option name "nox11" should not be used, use "-x11" instead.

--- sysutils/dbus/options.mk
+++ sysutils/dbus/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2008/06/19 01:56:44 bjs Exp $
+# $NetBSD: options.mk,v 1.3 2008/12/21 12:09:42 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dbus
-PKG_SUPPORTED_OPTIONS+=	debug kqueue nox11
-PKG_SUGGESTED_OPTIONS=	# empty
+PKG_SUPPORTED_OPTIONS+=	debug kqueue x11
+PKG_SUGGESTED_OPTIONS=	x11
 
 .if (${OPSYS} == "NetBSD"  ||	\
      ${OPSYS} == "FreeBSD" ||	\
@@ -24,11 +24,11 @@ CONFIGURE_ARGS.enable+= 	kqueue
 CONFIGURE_ARGS.disable+=	kqueue
 .endif
 
-.if !empty(PKG_OPTIONS:Mnox11)
-CONFIGURE_ARGS.without=	x
-.else
+.if !empty(PKG_OPTIONS:Mx11)
 CONFIGURE_ARGS.with+=	x
 .  include "../../x11/libX11/buildlink3.mk"
 BUILDLINK_DEPMETHOD.libXt=	build
 .  include "../../x11/libXt/buildlink3.mk"
+.else
+CONFIGURE_ARGS.without=	x
 .endif
