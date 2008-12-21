32cdd39cee538d40d62d32613e344736299797f1
wiz <wiz@pkgsrc.org>
2008-12-21 11:37:15 +0000

    Add avahi option, on by default, for zeroconf bookmarks. Bump PKGREVISION.

--- www/epiphany/options.mk
+++ www/epiphany/options.mk
@@ -1,13 +1,20 @@
-# $NetBSD: options.mk,v 1.3 2008/12/12 01:27:24 jmcneill Exp $
+# $NetBSD: options.mk,v 1.4 2008/12/21 11:37:15 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.epiphany
-PKG_SUPPORTED_OPTIONS=		enchant # avahi
+PKG_SUPPORTED_OPTIONS=		avahi enchant
 PKG_OPTIONS_OPTIONAL_GROUPS=	engine
 PKG_OPTIONS_GROUP.engine=	firefox webkit
-PKG_SUGGESTED_OPTIONS=		enchant firefox
+PKG_SUGGESTED_OPTIONS=		avahi enchant firefox
 PLIST_VARS=			gecko
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mavahi)
+CONFIGURE_ARGS+=	--enable-zeroconf
+.include "../../net/avahi/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-zeroconf
+.endif
 
 .if !empty(PKG_OPTIONS:Menchant)
 CONFIGURE_ARGS+=	--enable-spell-checker
