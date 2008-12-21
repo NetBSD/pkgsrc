7e8bbea95d5b9580c1501ae9515f5752311d5ca7
wiz <wiz@pkgsrc.org>
2008-12-21 11:00:37 +0000

    Add avahi option, and enable it by default. Bump PKGREVISION.

--- net/vino/options.mk
+++ net/vino/options.mk
@@ -1,9 +1,17 @@
-# $NetBSD: options.mk,v 1.1 2007/08/14 09:30:56 obache Exp $
+# $NetBSD: options.mk,v 1.2 2008/12/21 11:00:37 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.vino
-PKG_SUPPORTED_OPTIONS+=	inet6
+PKG_SUPPORTED_OPTIONS+=	avahi inet6
+PKG_SUGGESTED_OPTIONS+=	avahi
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mavahi)
+CONFIGURE_ARGS+=	--enable-avahi
+.include "../../net/avahi/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-avahi
+.endif
 
 .if !empty(PKG_OPTIONS:Minet6)
 CONFIGURE_ARGS+=	--enable-ipv6
