f65c9203f1ee8e9e2b96e7fc272644e0a73d8121
jmcneill <jmcneill@pkgsrc.org>
2008-12-22 02:48:56 +0000

    Bump BUILDLINK_API_DEPENDS

--- devel/libatomic_ops/buildlink3.mk
+++ devel/libatomic_ops/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2008/10/25 00:36:10 bjs Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/12/22 02:48:56 jmcneill Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBATOMIC_OPS_BUILDLINK3_MK:=	${LIBATOMIC_OPS_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libatomic_ops
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libatomic_ops
 
 .if ${LIBATOMIC_OPS_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.libatomic_ops+=	libatomic_ops>=20081024
+BUILDLINK_API_DEPENDS.libatomic_ops+=	libatomic_ops>=20081024nb1
 BUILDLINK_PKGSRCDIR.libatomic_ops?=	../../devel/libatomic_ops
 .endif	# LIBATOMIC_OPS_BUILDLINK3_MK
 
