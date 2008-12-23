46c7dea9d66c8e11eaca0d85b015bc9b16f4a37b
abs <abs@pkgsrc.org>
2008-12-23 15:39:07 +0000

    Only derive PYTHON_VERSION_REQD from PKGNAME_REQD or PKGNAME_OLD if they match py([0-9][0-9])-.*. Fixes breakage when building gnome which then tries to build hamster-applet and chokes due to PYPKGPREFIX ending up undefined.

--- lang/python/extension.mk
+++ lang/python/extension.mk
@@ -1,10 +1,10 @@
-# $NetBSD: extension.mk,v 1.20 2008/10/06 14:41:50 joerg Exp $
+# $NetBSD: extension.mk,v 1.21 2008/12/23 15:39:07 abs Exp $
 
 # derive a python version from the package name if possible
 # optionally handled quoted package names
-.if defined(PKGNAME_REQD)
+.if defined(PKGNAME_REQD) && !empty(${PKGNAME_REQD:Mpy([0-9][0-9])-.*})
 PYTHON_VERSION_REQD?= ${PKGNAME_REQD:C/(^.*-|^)py([0-9][0-9])-.*/\2/}
-.elif defined(PKGNAME_OLD)
+.elif defined(PKGNAME_OLD) && !empty(${PKGNAME_OLD:Mpy([0-9][0-9])-.*})
 PYTHON_VERSION_REQD?= ${PKGNAME_OLD:C/(^.*-|^)py([0-9][0-9])-.*/\2/}
 .endif
 
