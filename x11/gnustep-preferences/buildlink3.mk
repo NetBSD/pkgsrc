86f5097e3a84d88382727990f5e6167308f687c4
adam <adam@pkgsrc.org>
2008-12-28 07:56:38 +0000

    Fix building with the latest GNUstep; PR #40211

--- x11/gnustep-preferences/buildlink3.mk
+++ x11/gnustep-preferences/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2008/01/18 05:09:55 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2008/12/28 07:56:38 adam Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 GNUSTEP_PREFERENCES_BUILDLINK3_MK:=	${GNUSTEP_PREFERENCES_BUILDLINK3_MK}+
@@ -13,10 +13,10 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gnustep-preferences
 
 .if !empty(GNUSTEP_PREFERENCES_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.gnustep-preferences+=	gnustep-preferences>=1.2.0
-BUILDLINK_ABI_DEPENDS.gnustep-preferences?=	gnustep-preferences>=1.2.0nb16
+BUILDLINK_ABI_DEPENDS.gnustep-preferences+=	gnustep-preferences>=1.2.0nb16
 BUILDLINK_PKGSRCDIR.gnustep-preferences?=	../../x11/gnustep-preferences
 .endif	# GNUSTEP_PREFERENCES_BUILDLINK3_MK
 
-#.include "../../x11/gnustep-back/buildlink3.mk"
+.include "../../x11/gnustep-back/buildlink3.mk"
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
