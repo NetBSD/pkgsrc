cd2190897b05ecdf10b6176445365a6800109b30
adrianp <adrianp@pkgsrc.org>
2009-01-04 00:56:32 +0000

    Update to 4.1.0
    ISC DHCP 4.1.x will have several new DHCPv6 features that were not in DHCP 4.0.x. These new features include:
    
    * Support for the rapid-commit option on the client side
    * Prefix Delegation support
    * IA_TA address support
    * A basic DHCPv6 relay agent
    * basic DHCPv6 Leasequery support

--- net/isc-dhcrelay4/files/isc_dhcrelay.sh
+++ net/isc-dhcrelay4/files/isc_dhcrelay.sh
@@ -1,10 +1,10 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: isc_dhclient.sh,v 1.1.1.1 2008/02/13 22:03:58 adrianp Exp $
+# $NetBSD: isc_dhcrelay.sh,v 1.1 2009/01/04 01:00:09 adrianp Exp $
 #
 
-# PROVIDE: dhclient
-# REQUIRE: network mountcritlocal
+# PROVIDE: dhcrelay
+# REQUIRE: DAEMON
 # BEFORE:  NETWORKING
 #
 #	Note that there no syslog logging of dhclient messages at boot because
@@ -15,13 +15,13 @@ if [ -f /etc/rc.subr ]; then
         . /etc/rc.subr
 fi
 
-name="dhclient"
-rcvar="isc_${name}"
-command="@PREFIX@/sbin/${name}"
-pidfile="@VARBASE@/run/isc-dhcp/isc-${name}.pid"
-start_precmd="isc_dhclient_precmd"
+name="isc_dhcrelay"
+rcvar="${name}"
+command="@PREFIX@/sbin/dhcrelay"
+pidfile="@VARBASE@/run/isc-dhcp/isc-dhcrelay.pid"
+start_precmd="isc_dhcrelay_precmd"
 
-isc_dhclient_precmd()
+isc_dhcrelay_precmd()
 {
 	if [ ! -d @VARBASE@/run/isc-dhcp ]; then
 		@MKDIR@ @VARBASE@/run/isc-dhcp
