456cb55be8c3d66f983c35a64c60fa8c8d19f4ec
martin <martin@pkgsrc.org>
2009-01-05 09:58:36 +0000

    Fix a few more LP64 bit bugs (may be in dead code, but who knows) and fix the xptc call stubs for sparc64 so that tree views with a javascript data source (like history and about:config) now work. All changes already reported upstream.

--- www/firefox3/files/xptcstubs_sparc64_netbsd.cpp
+++ www/firefox3/files/xptcstubs_sparc64_netbsd.cpp
@@ -53,6 +53,8 @@ PrepareAndDispatch(nsXPTCStubBase* self, PRUint64 methodIndex, PRUint64* args)
     else
         dispatchParams = paramBuffer;
     NS_ASSERTION(dispatchParams,"no place for params");
+    if (!dispatchParams)
+        return NS_ERROR_OUT_OF_MEMORY;
 
     PRUint64* ap = args;
     for(i = 0; i < paramCount; i++, ap++)
@@ -69,19 +71,19 @@ PrepareAndDispatch(nsXPTCStubBase* self, PRUint64 methodIndex, PRUint64* args)
         // else
         switch(type)
         {
-        case nsXPTType::T_I8     : dp->val.i8  = *((PRInt32*)  ap);      break;
-        case nsXPTType::T_I16    : dp->val.i16 = *((PRInt32*) ap);       break;
-        case nsXPTType::T_I32    : dp->val.i32 = *((PRInt32*) ap);       break;
-        case nsXPTType::T_DOUBLE : dp->val.d   = *((double*) ap);        break;
-        case nsXPTType::T_U64    : dp->val.u64 = *((PRUint64*) ap);      break;
+        case nsXPTType::T_BOOL   : dp->val.b   = *((PRInt64*)  ap);      break;
+        case nsXPTType::T_CHAR   : dp->val.c   = *((PRUint64*) ap);      break;
+        case nsXPTType::T_WCHAR  : dp->val.wc  = *((PRInt64*) ap);       break;
+        case nsXPTType::T_I8     : dp->val.i8  = *((PRInt64*)  ap);      break;
+        case nsXPTType::T_I16    : dp->val.i16 = *((PRInt64*) ap);       break;
+        case nsXPTType::T_I32    : dp->val.i32 = *((PRInt64*) ap);       break;
         case nsXPTType::T_I64    : dp->val.i64 = *((PRInt64*) ap);       break;
-        case nsXPTType::T_U8     : dp->val.u8  = *((PRUint32*) ap);      break;
-        case nsXPTType::T_U16    : dp->val.u16 = *((PRUint32*)ap);       break;
-        case nsXPTType::T_U32    : dp->val.u32 = *((PRUint32*)ap);       break;
-        case nsXPTType::T_FLOAT  : dp->val.f   = *((float*)   ap);       break;
-        case nsXPTType::T_BOOL   : dp->val.b   = *((PRBool*)  ap);       break;
-        case nsXPTType::T_CHAR   : dp->val.c   = *((PRUint32*) ap);      break;
-        case nsXPTType::T_WCHAR  : dp->val.wc  = *((PRInt32*) ap);       break;
+        case nsXPTType::T_U8     : dp->val.u8  = *((PRUint64*) ap);      break;
+        case nsXPTType::T_U16    : dp->val.u16 = *((PRUint64*)ap);       break;
+        case nsXPTType::T_U32    : dp->val.u32 = *((PRUint64*)ap);       break;
+        case nsXPTType::T_U64    : dp->val.u64 = *((PRUint64*) ap);      break;
+        case nsXPTType::T_FLOAT  : dp->val.f   = ((float*)   ap)[1];     break;
+        case nsXPTType::T_DOUBLE : dp->val.d   = *((double*) ap);        break;
         default:
             NS_ASSERTION(0, "bad type");
             break;
