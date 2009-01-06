bc451f039247409538df53475cfd025f20010440
obache <obache@pkgsrc.org>
2009-01-06 12:08:01 +0000

    Update msmtp to 1.4.17. Based on PR 40278 by Leonardo Taccari.
    Version 1.4.17:
    - Remove most W32-specific code from net.c and use the appropriate gnulib
      modules instead.
    - Gnulib upate to 2008-12-24.
    - Unified handling of the Gnome and MacOS keyrings. Both are disabled by default
      and must be enabled using --with-*-keyring options.
    - Support for SYSCONFDIR/netrc (as a fallback for ~/.netrc) was added by Jim
      Pryor. Thanks!
    - Support for the GNOME Keyring was added by Satoru SATOH. Thanks a lot!
    - Added a vim syntax file for msmtprc files to scripts/vim. The file was
      written by Simon Ruderich. Thanks!
    - Updated the msmtpq/msmtpQ scripts.

--- mail/msmtp/options.mk
+++ mail/msmtp/options.mk
@@ -1,11 +1,11 @@
-# $NetBSD: options.mk,v 1.9 2007/09/08 08:05:23 obache Exp $
+# $NetBSD: options.mk,v 1.10 2009/01/06 12:08:01 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.msmtp
 
 PKG_OPTIONS_OPTIONAL_GROUPS=	ssl
 PKG_OPTIONS_GROUP.ssl=	gnutls ssl
 
-PKG_SUPPORTED_OPTIONS=	gsasl idn inet6
+PKG_SUPPORTED_OPTIONS=	gnome-keyring gsasl idn inet6
 PKG_SUGGESTED_OPTIONS=	ssl
 
 .include "../../mk/bsd.options.mk"
@@ -36,4 +36,13 @@ CONFIGURE_ARGS+=	--disable-ssl
 .if !empty(PKG_OPTIONS:Midn)
 .  include "../../devel/libidn/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-libidn
+.endif
+
+###
+### GNOME keyring support
+###
+.if !empty(PKG_OPTIONS:Mgnome-keyring)
+.  include "../../security/gnome-keyring/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-gnome-keyring
+USE_TOOLS+=		pkg-config
 .endif
