2b9dcc35d04e27dd5e9553ddacee33de8d4242b7
kefren <kefren@pkgsrc.org>
2009-01-06 08:54:21 +0000

    Initial import of sublib 0.9, a C# library for subtitles editing and conversion

---
+++ textproc/sublib/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/01/06 08:54:21 kefren Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+SUBLIB_BUILDLINK3_MK:=	${SUBLIB_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	sublib
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nsublib}
+BUILDLINK_PACKAGES+=	sublib
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}sublib
+
+.if ${SUBLIB_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.sublib+=	sublib>=0.9
+BUILDLINK_PKGSRCDIR.sublib?=	../../textproc/sublib
+.endif	# SUBLIB_BUILDLINK3_MK
+
+.include "../../lang/mono/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
