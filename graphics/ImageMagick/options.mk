668c5450ac6fa1f8492c4c2e9ae29b07ae04fc80
adam <adam@pkgsrc.org>
2009-01-07 08:58:07 +0000

    Changes 6.4.8-3: * Reverse argument and option in Throw*InvalidArgumentException()   macro for proper exception message. * Recognize negative geometries for -shear and -annotate (.e.g. 45x-30). * Fix Y shear angle for -shear option.
    Changes 6.4.8-2:
    * Use GetImageArtifact() to retrieve -attentuate option value for +noise.
    * Support reading FITS test files.
    * Replace input.ttf with the public domain Tuffy.ttf font.
    * MagickNewImage() no longer faults when parameters are incorrect (reference
    * Allocate strings returned by XGetResourceInstance().
    * The -append gravity is relative to the container.
    
    Changes 6.4.8-1:
    * Reference cache rather than acquiring when cloning an image.
    
    Changes 6.4.8-0:
    * Cross-compilation failure using MinGW on Linux for ImageMagick trunk
    * Add -interword-spacing option to convert.
    * Initialize the black channel when testing the CMY colorspace.
    * remove calls to exit() in MagickWand.
    * Validate -affine arguments.
    * Accept -attentuate as an option for convert.
    * Properly create RGBA image if colormapped image has transparency.
    * Support X11 geometry like this: +-4+-4;  wierd but X11 supports it.

--- graphics/ImageMagick/options.mk
+++ graphics/ImageMagick/options.mk
@@ -1,17 +1,43 @@
-# $NetBSD: options.mk,v 1.8 2007/01/10 10:00:47 abs Exp $
+# $NetBSD: options.mk,v 1.9 2009/01/07 08:58:07 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ImageMagick
-PKG_SUPPORTED_OPTIONS=	x11
-PKG_SUGGESTED_OPTIONS=	x11
+PKG_SUPPORTED_OPTIONS=	x11 jasper djvu ghostscript wmf
+PKG_SUGGESTED_OPTIONS=	x11 jasper
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mx11)
-BUILDLINK_API_DEPENDS.jasper+=	jasper>=1.701.0
-DEPENDS+=	mpeg2codec>=1.2:../../graphics/mpeg2codec
-.include "../../graphics/jasper/buildlink3.mk"
-.include "../../graphics/libwmf/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/xextproto/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-x
+.endif
+
+.if !empty(PKG_OPTIONS:Mjasper)
+BUILDLINK_API_DEPENDS.jasper+=	jasper>=1.701.0
+.include "../../graphics/jasper/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-jp2
+.else
+CONFIGURE_ARGS+=	--without-jp2
+.endif
+
+.if !empty(PKG_OPTIONS:Mdjvu)
+.include "../../graphics/djvulibre-lib/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-djvu
+.else
+CONFIGURE_ARGS+=	--without-djvu
+.endif
+
+.if !empty(PKG_OPTIONS:Mghostscript)
+.include "../../print/ghostscript/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-gslib
+.else
+CONFIGURE_ARGS+=	--without-gslib
+.endif
+
+.if !empty(PKG_OPTIONS:Mwmf)
+.include "../../graphics/libwmf/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-wmf
+.else
+CONFIGURE_ARGS+=	--without-wmf
 .endif
