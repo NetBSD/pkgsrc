efcb21c3fb00bd3e4e3b10cff57538dc6deb1291
wiz <wiz@pkgsrc.org>
2009-01-07 21:33:37 +0000

    Update to 0.9.16:
    2008-07-05 Rasqal Version 0.9.16 Released
    
       WARNING: ABI AND API CHANGED in this release. Rasqal 0.9.16 is
       incompatible with 0.9.15.
    
       Added a rasqal_world object used for all constructor functions
       Removed deprecated functions and macros
       Fixed some memory leaks and made some low-memory resiliance fixes
       Query result sets can be read/written from sparql XML results format
       Improved error syntax error reporting
    
       See the Raptor 0.9.16 Release Notes for the full details of the
       changes.

--- textproc/rasqal/buildlink3.mk
+++ textproc/rasqal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/11/04 10:35:22 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/01/07 21:33:37 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 RASQAL_BUILDLINK3_MK:=	${RASQAL_BUILDLINK3_MK}+
@@ -12,8 +12,7 @@ BUILDLINK_PACKAGES+=	rasqal
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}rasqal
 
 .if ${RASQAL_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.rasqal+=	rasqal>=0.9.14
-BUILDLINK_ABI_DEPENDS.rasqal?=	rasqal>=0.9.14nb1
+BUILDLINK_API_DEPENDS.rasqal+=	rasqal>=0.9.16
 BUILDLINK_PKGSRCDIR.rasqal?=	../../textproc/rasqal
 .endif	# RASQAL_BUILDLINK3_MK
 
