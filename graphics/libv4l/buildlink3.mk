1b98bf78e9deaf6ff2a8728d09e27deaca9cdeeb
jmcneill <jmcneill>
2009-01-10 21:04:22 +0000

    Add libv4l1 compatibility support, bump PKGREVISION.

--- graphics/libv4l/buildlink3.mk
+++ graphics/libv4l/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/09/08 19:07:41 jmcneill Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/01/10 21:04:23 jmcneill Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBV4L_BUILDLINK3_MK:=		${LIBV4L_BUILDLINK3_MK}+
@@ -12,8 +12,8 @@ BUILDLINK_PACKAGES+=	libv4l
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libv4l
 
 .if !empty(LIBV4L_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.libv4l+=		libv4l>=0.4.3
-BUILDLINK_ABI_DEPENDS.libv4l+=		libv4l>=0.4.3
+BUILDLINK_API_DEPENDS.libv4l+=		libv4l>=0.4.3nb2
+BUILDLINK_ABI_DEPENDS.libv4l+=		libv4l>=0.4.3nb2
 BUILDLINK_PKGSRCDIR.libv4l?=		../../graphics/libv4l
 .endif	# LIBV4L_BUILDLINK3_MK
 
