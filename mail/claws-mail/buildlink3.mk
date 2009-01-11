acd36f63efac6d2b80b497442f4bd790c5c5b98b
smb <smb>
2009-01-11 03:11:37 +0000

    Updated claws-mail and all of its plug-ins to 3.7.0
    * 3.7.0
    -------
    
    * Expanding/Collapsing of threads is now approximately 95% faster.
    
    * Cache reading is now approximately 10% faster.
    
    * The shortcut key settings of the main window and the message list
      context menus are now connected.
    
    * The preferences on the 'Other' page have now been moved to
      Other/Miscellaneous. The parent pages are now unselectable and the
      first page is automatically selected on opening the preferences.
    
    * In the Compose window, on the Others tab, the 'Save message to'
      entry now has a drop-down list of the previous save locations.
    
    *  In the Compose window, the Attachments Properties window is now
       confirmed and closed with the Return key.
    
    * When using an external editor to compose messages, the message is
      automatically saved to Drafts when the external editor is closed.
    
    * A hidden option has been added, 'primary_paste_unselects' which
      causes the primary buffer to be cleared and the insertion point to
      be repositioned when the middle mouse button is used for pasting
      text. It is turned off by default.
    
    * In the Actions window, the Escape key now cancels the action
      editing.
    
    * Offline SSL certificate verification has been added.
    
    * Privacy plugins: The decryption failure messages are now shown
      in the NoticeView just above the message text rather than in a
      popup window.
    
    * Privacy plugins: when listing the UIDs during a signature check,
      show the UID validity
    
    * tools/popfile-link.sh
      Support for reusing existing POPFile session ID has been added.
    
    * tools/kdeservicemenu
      Support for kde4 has been added.
    
    * win32: better integration has been implemented by using the
      standard file associations.

--- mail/claws-mail/buildlink3.mk
+++ mail/claws-mail/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2008/10/15 16:22:02 smb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/01/11 03:11:37 smb Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 CLAWS_MAIL_BUILDLINK3_MK:=	${CLAWS_MAIL_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	claws-mail
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}claws-mail
 
 .if ${CLAWS_MAIL_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.claws-mail+= claws-mail>=3.6.1
+BUILDLINK_API_DEPENDS.claws-mail+= claws-mail>=3.7.0
 BUILDLINK_PKGSRCDIR.claws-mail?=	../../mail/claws-mail
 .endif	# CLAWS_MAIL_BUILDLINK3_MK
 
