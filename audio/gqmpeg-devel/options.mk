3e1f3f8c1b5b01cc0da200a41022db75c87c4c10
shattered <shattered@pkgsrc.org>
2009-01-12 21:12:40 +0000

    gqmpeg options (gqmpeg-russian and gqmpeg-japanese) are mutually exclusive. Place them into a group, make it optional.
    OK by wiz@.

--- audio/gqmpeg-devel/options.mk
+++ audio/gqmpeg-devel/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2005/10/05 13:29:49 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2009/01/12 21:12:40 shattered Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gqmpeg
-PKG_SUPPORTED_OPTIONS=	gqmpeg-japanese gqmpeg-russian
-PKG_SUGGESTED_OPTIONS=	gqmpeg-japanese gqmpeg-russian
+PKG_OPTIONS_OPTIONAL_GROUPS=	recode
+PKG_OPTIONS_GROUP.recode=	gqmpeg-japanese gqmpeg-russian
 
 .include "../../mk/bsd.options.mk"
 
