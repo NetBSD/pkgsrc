6400efab3e63744dd222603f215a6568db67b3ef
wiz <wiz@pkgsrc.org>
2009-01-13 10:55:37 +0000

    Add an ignore pattern.

--- sysutils/dbus/buildlink3.mk
+++ sysutils/dbus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2006/08/23 09:44:27 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2009/01/13 10:55:37 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 DBUS_BUILDLINK3_MK:=	${DBUS_BUILDLINK3_MK}+
@@ -17,6 +17,7 @@ BUILDLINK_PKGSRCDIR.dbus?=	../../sysutils/dbus
 
 PRINT_PLIST_AWK+=	/^@dirrm include\/dbus-1.0\/dbus$$/ \
 				{ print "@comment in dbus: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@exec ..MKDIR. %D\/share\/dbus-1\/services/ { next; }
 PRINT_PLIST_AWK+=	/^@dirrm share\/dbus-1\/services/ \
 				{ print "@comment in dbus: " $$0; next }
 .endif	# DBUS_BUILDLINK3_MK
