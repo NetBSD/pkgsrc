361227c6f779538177b9aa64340dbb1dc37600b2
wiz <wiz@pkgsrc.org>
2009-01-13 10:18:56 +0000

    Update to 0.10.3:
    Release 0.10.3
    
    	core:
    	 * Fix a crash on documents with malformed outline. Bug #19024
    	 * Fix leak on AnnotScreen destructor. Bug #19095
    	 * Fix wrong PS generation when a large image is in Patterns. Bug #18908
    	 * Remove BaseFile.h it was never used. Bug #19298
    	 * Improve document saving
    	 * Fix PS generation of PDF with malformed font Length2 definition
    	 * Fix a leak while parsing annotations
    	 * Fix rendering of some checkboxes
    
    	Qt4:
    	 * Fix positioning of Form rects on PDF with cropbox
    	 * Fix positioning of Annotation rects on PDF with cropbox. Bug #18558.
    	 * Small documentation improvements
    	 * Make Document::fonts() work when called more than once. Bug #19405
    
    	build system:
    	 * CMake: look harder for openjpeg
    	 * CMake: update the poppler core headers installation
    	 * Autotools: do not install Function.cc as it's not a header
    
    	Qt:
    	 * Fix deserialization of links right coordinate

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2009/01/06 22:58:49 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2009/01/13 10:18:56 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 POPPLER_BUILDLINK3_MK:=	${POPPLER_BUILDLINK3_MK}+
@@ -15,6 +15,10 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}poppler
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
 BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.10.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
+
+PRINT_PLIST_AWK+=	/^@exec ..MKDIR. %D\/include\/poppler$$/ { next; }
+PRINT_PLIST_AWK+=	/^@dirrm include\/poppler$$/ \
+				{ print "@comment in poppler: " $$0; next; }
 .endif	# POPPLER_BUILDLINK3_MK
 
 .include "../../fonts/fontconfig/buildlink3.mk"
