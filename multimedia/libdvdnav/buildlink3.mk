ee7246e0c4b80238abc3c2939fd24c85eb712b94
jmcneill <jmcneill@pkgsrc.org>
2009-01-14 22:30:46 +0000

    Update to libdvdnav version 4.1.3. libdvdnav is now maintained by the mplayer project, and the next release after 0.1.10 (the one we're coming from) was 4.1.1.
    libdvdnav (4.1.3)
      * an embarassing amount of fixes regarding potential memory and resource leaks
        (patches contributed by Erik Hovland)
      * added dvdread-config (dvdnav-config's younger brother)
      * added pkg-config support
      * split dvdread to a separate tree; now you need to check it out
        and install it before building libdvdnav.
        In order to configure libdvdnav We need the executable dvdread-config
        somewhere in the PATH or explicitly specified to configure[2] with
        --with-dvdread-config=~/bin/dvdread-config
    
    libdvdnav (4.1.2)
      * multiple build system fixes
      * added dvdnav_describe_title_chapters(title) to get title and chapters
        duration
    
    libdvdnav (4.1.1)
      * added dvdnav_audio_stream_channels() to return number of channels
      * fixed dvdnav_time_search() in multi-angle dvds (but it still needs
        improvements)
      * added dvdnav_audio_stream_format() to identify the codec used
        in audio streams
      * starting DVD playback at specific title/part positions with
        dvdnav_{title,part}_play() works again
      * removed wrong SPU stream change event filter
        (fixes unwanted subtitles in the trailer of "Girl, interrupted", RC2)
      * fixed error "Expected NAV packet but none found." occuring sometimes
        on resume from menu

--- multimedia/libdvdnav/buildlink3.mk
+++ multimedia/libdvdnav/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/07/08 23:11:01 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2009/01/14 22:30:46 jmcneill Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBDVDNAV_BUILDLINK3_MK:=	${LIBDVDNAV_BUILDLINK3_MK}+
@@ -12,8 +12,8 @@ BUILDLINK_PACKAGES+=	libdvdnav
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libdvdnav
 
 .if !empty(LIBDVDNAV_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.libdvdnav+=	libdvdnav>=0.1.9
-BUILDLINK_ABI_DEPENDS.libdvdnav+=	libdvdnav>=0.1.9nb1
+BUILDLINK_API_DEPENDS.libdvdnav+=	libdvdnav>=4.1.3
+BUILDLINK_ABI_DEPENDS.libdvdnav+=	libdvdnav>=4.1.3
 BUILDLINK_PKGSRCDIR.libdvdnav?=	../../multimedia/libdvdnav
 .endif	# LIBDVDNAV_BUILDLINK3_MK
 
