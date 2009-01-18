b01a829329b05c8d91dbb377e2f281315c8ead83
obache <obache@pkgsrc.org>
2009-01-18 12:46:39 +0000

    Import pfstools-1.7.0 as graphics/pfstools. Based on PR 39437 by Thomas Zander.
    A set of command line tools for reading, writing, manipulating
    and viewing high-dynamic range (HDR) images and video frames.

---
+++ graphics/pfstools/options.mk
@@ -0,0 +1,72 @@
+# $NetBSD: options.mk,v 1.1.1.1 2009/01/18 12:46:39 obache Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.pfstools
+PKG_SUPPORTED_OPTIONS=	gdal imagemagick openexr octave qt opengl
+
+PLIST_VARS+=	gdal im exr octave qt gl
+
+.include "../../mk/bsd.prefs.mk"
+
+.if ${OPSYS} == "Darwin"
+PKG_SUGGESTED_OPTIONS+=	opengl
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgdal)
+.include "../../geography/gdal-lib/buildlink3.mk"
+PLIST.gdal=	yes
+CONFIGURE_ARGS+=	--enable-gdal
+.else
+CONFIGURE_ARGS+=	--disable-gdal
+.endif
+
+.if !empty(PKG_OPTIONS:Mimagemagick)
+.include "../../graphics/ImageMagick/buildlink3.mk"
+PLIST.im=	yes
+CONFIGURE_ARGS+=	--enable-imagemagick
+.else
+CONFIGURE_ARGS+=	--disable-imagemagick
+.endif
+
+.if !empty(PKG_OPTIONS:Mopenexr)
+.include "../../graphics/openexr/buildlink3.mk"
+PLIST.exr=	yes
+CONFIGURE_ARGS+=	--enable-openexr
+.else
+CONFIGURE_ARGS+=	--disable-openexr
+.endif
+
+.if !empty(PKG_OPTIONS:Moctave)
+.include "../../math/octave/buildlink3.mk"
+REPLACE_OCTAVE+=	src/octave/pfsoctavelum src/octave/pfsoctavergb
+REPLACE_OCTAVE+=	src/octave/pfsstat
+.include "../../math/octave/octave.mk"
+PLIST.octave=	yes
+PLIST_SUBST+=	OCT_LOCALVEROCTFILEDIR=${OCT_LOCALVEROCTFILEDIR:S/${BUILDLINK_PREFIX.octave}\///}
+PLIST_SUBST+=	OCT_LOCALVERFCNFILEDIR=${OCT_LOCALVERFCNFILEDIR:S/${BUILDLINK_PREFIX.octave}\///}
+USE_TOOLS+=	gmake
+USE_LANGUAGES+=	fortran
+CONFIGURE_ARGS+=	--enable-octave
+.else
+CONFIGURE_ARGS+=	--disable-octave
+.endif
+
+.if !empty(PKG_OPTIONS:Mqt)
+.include "../../x11/qt3-libs/buildlink3.mk"
+PLIST.qt=	yes
+USE_TOOLS+=	gmake
+CONFIGURE_ARGS+=	--enable-qt
+.else
+CONFIGURE_ARGS+=	--disable-qt
+.endif
+
+.if !empty(PKG_OPTIONS:Mopengl)
+.  if ${OPSYS} != "Darwin"
+.include "../../graphics/glut/buildlink3.mk"
+.  endif
+CONFIGURE_ARGS+=	--enable-opengl
+PLIST.gl=	yes
+.else
+CONFIGURE_ARGS+=	--disable-opengl
+.endif
