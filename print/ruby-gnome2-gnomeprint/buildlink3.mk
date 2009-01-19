02c8a3de53af3a6334db380a61b12a69f773bf95
obache <obache>
2009-01-19 04:12:55 +0000

    include rubyversion.mk to resolve RUBY_PKGPREFIX.

--- print/ruby-gnome2-gnomeprint/buildlink3.mk
+++ print/ruby-gnome2-gnomeprint/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2008/09/17 00:21:49 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/01/19 04:12:55 obache Exp $
 
 BUILDLINK_DEPTH:=				${BUILDLINK_DEPTH}+
 RUBY_GNOME2_GNOMEPRINT_BUILDLINK3_MK:=	${RUBY_GNOME2_GNOMEPRINT_BUILDLINK3_MK}+
@@ -12,6 +12,7 @@ BUILDLINK_PACKAGES+=	ruby-gnome2-gnomeprint
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}ruby-gnome2-gnomeprint
 
 .if ${RUBY_GNOME2_GNOMEPRINT_BUILDLINK3_MK} == "+"
+.include "../../lang/ruby/rubyversion.mk"
 BUILDLINK_API_DEPENDS.ruby-gnome2-gnomeprint+=	${RUBY_PKGPREFIX}-gnome2-gnomeprint>=0.17.0
 BUILDLINK_PKGSRCDIR.ruby-gnome2-gnomeprint?=	../../print/ruby-gnome2-gnomeprint
 .endif	# RUBY_GNOME2_GNOMEPRINT_BUILDLINK3_MK
