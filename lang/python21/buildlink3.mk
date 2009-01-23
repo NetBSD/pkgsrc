b0508438ab5b8fc30aa4f23f8033cc7c4e450243
dsainty <dsainty@pkgsrc.org>
2009-01-23 03:05:53 +0000

    Any package that determines libraries to link via:
      $PYTHON -c "from distutils import sysconfig; print sysconfig.get_config_var('SHLIBS');"
    
    ... where bdb.buildlink.mk has been used and it satisfied the requirement from
    Pkgsrc (E.g. via databases/db4) would fail to build because the required -ldb4
    library was not itself buildlinked.
    
    To rectify this, pull in bdb.buildlink.mk in python??/buildlink3.mk under the
    same conditions as it is pulled in in the package's own makefile.
    
    No revision bump required, this almost certainly only affects packages and
    environments that simply wouldn't build at all prior to the fix.
    
    Fixes the build of py-ORBit on Linux (Python 2.4 or 2.5), and PR39377.

--- lang/python21/buildlink3.mk
+++ lang/python21/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2006/07/08 23:10:55 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2009/01/23 03:05:53 dsainty Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PYTHON21_BUILDLINK3_MK:=	${PYTHON21_BUILDLINK3_MK}+
@@ -26,6 +26,11 @@ BUILDLINK_TRANSFORM+=		l:python:python2.1
 .endif	# PYTHON21_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
+
+.include "../../mk/bsd.fast.prefs.mk"
+.if ${OPSYS} != "SunOS" && ${OPSYS} != "IRIX"
+.  include "../../mk/bdb.buildlink3.mk"
+.endif
 
 .include "../../mk/dlopen.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
