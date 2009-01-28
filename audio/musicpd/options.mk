03a84846038bdd9f690aa173a161bcb7091ea3ce
drochner <drochner@pkgsrc.org>
2009-01-28 18:06:21 +0000

    -kill the "iconv" option which doesn't serve any purpose anymore -remove libmikmod from default options - it is buggy and possibly a  security risk (it is disabled in "configure" per default too) -remove shout option - this didn't work anyway (can be fixed, but  I haven't used shout so far) -enable ipv6 option - the comment on options.mk stating that IPv6  disables IPv4 doesn't seem to ve valid anymore -fix some configure args which changed spelling over time -fix a crash in case libmikmod is enabled and used bump PKGREVISION

--- audio/musicpd/options.mk
+++ audio/musicpd/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.8 2009/01/11 20:57:20 ahoka Exp $
+# $NetBSD: options.mk,v 1.9 2009/01/28 18:06:21 drochner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.musicpd
-PKG_SUPPORTED_OPTIONS=	aac audiofile flac iconv id3 libmikmod musepack ogg shout
-PKG_SUGGESTED_OPTIONS=	aac audiofile flac iconv id3 libmikmod musepack ogg shout
+PKG_SUPPORTED_OPTIONS=	aac audiofile flac id3 libmikmod musepack ogg inet6
+PKG_SUGGESTED_OPTIONS=	aac audiofile flac id3 musepack ogg
 
 .include "../../mk/bsd.options.mk"
 
@@ -15,38 +15,30 @@ CONFIGURE_ARGS+=	--disable-aac
 
 .if !empty(PKG_OPTIONS:Maudiofile)
 .  include "../../audio/libaudiofile/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-audiofile=${BUILDLINK_PREFIX.audiofile}
-CONFIGURE_ENV+=		ac_cv_lib_iconv_iconv_open=yes
+CONFIGURE_ARGS+=	--enable-audiofile
 .else
 CONFIGURE_ARGS+=	--disable-audiofile
 .endif
 
 .if !empty(PKG_OPTIONS:Mflac)
 .  include "../../audio/flac/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-flac=${BUILDLINK_PREFIX.flac}
+CONFIGURE_ARGS+=	--enable-flac
 .else
 CONFIGURE_ARGS+=	--disable-flac
 .endif
 
-.if !empty(PKG_OPTIONS:Miconv)
-.  include "../../converters/libiconv/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-iconv=${BUILDLINK_PREFIX.iconv}
-.else
-CONFIGURE_ARGS+=	--disable-iconv
-.endif
-
 .if !empty(PKG_OPTIONS:Mid3)
 .  include "../../audio/libid3tag/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-id3tag=${BUILDLINK_PREFIX.libid3tag}
+CONFIGURE_ARGS+=	--enable-id3
 .else
 CONFIGURE_ARGS+=	--disable-id3
 .endif
 
 .if !empty(PKG_OPTIONS:Mlibmikmod)
 .  include "../../audio/libmikmod/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-libmikmod=${BUILDLINK_PREFIX.libmikmod}
+CONFIGURE_ARGS+=	--enable-mod
 .else
-CONFIGURE_ARGS+=	--disable-libmikmod
+CONFIGURE_ARGS+=	--disable-mod
 .endif
 
 .if !empty(PKG_OPTIONS:Mmusepack)
@@ -59,21 +51,13 @@ CONFIGURE_ARGS+=	--disable-mpc
 
 .if !empty(PKG_OPTIONS:Mogg)
 .  include "../../audio/libvorbis/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-ogg=${BUILDLINK_PREFIX.libvorbis}
+CONFIGURE_ARGS+=	--enable-oggvorbis
 .else
-CONFIGURE_ARGS+=	--disable-ogg
+CONFIGURE_ARGS+=	--disable-oggvorbis
 .endif
 
-.if !empty(PKG_OPTIONS:Mshout)
-.  include "../../audio/libshout/buildlink3.mk"
-CONFIGURE_ARGS+=        --enable-shout
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--enable-ipv6
 .else
-CONFIGURE_ARGS+=        --enable-shout
-.endif
-
-# when IPv6 support is enabled, mpd doesn't listen on an IPv4 address.
-#.if !empty(PKG_OPTIONS:Minet6)
-#CONFIGURE_ARGS+=	--enable-ipv6
-#.else
 CONFIGURE_ARGS+=	--disable-ipv6
-#.endif
+.endif
