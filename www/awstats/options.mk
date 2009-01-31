95626ac7dbdd037c2c437c5e7a29eb62bd046d37
tron <tron>
2009-01-31 21:28:41 +0000

    Pullup ticket #2674 - requested by taca awstats: security update
    Revisions pulled up:
    - www/awstats/Makefile				1.40
    - www/awstats/PLIST				1.14
    - www/awstats/distinfo				1.23
    - www/awstats/options.mk			1.2
    - www/awstats/patches/patch-aa			1.9
    - www/awstats/patches/patch-ab			1.6
    - www/awstats/patches/patch-ac			delete
    ---
    Module Name:	pkgsrc
    Committed By:	adam
    Date:		Thu Jan 15 12:12:04 UTC 2009
    
    Modified Files:
    	pkgsrc/www/awstats: Makefile PLIST distinfo options.mk
    	pkgsrc/www/awstats/patches: patch-aa patch-ab
    
    Log Message:
    Changes 6.9:
    * With postfix that support DSN (Delivery Status Notifications) we exclude
      some lines to avoid counting mails twice in maillogconvert.pl script.
    * Logresolvemerge.pl support FreeRADIUS logs or anything else using
      (the fixed length!) ctime format timestamp.
    * Add option stoponfirsteof in logresolvemerge tool.
    * Add patch to support host_proxy tag in LogFormat (for Apache LogFormat
      containing %{X-Forwarded-For}i)
    * Renamed Add to favourites on "Hit on favicon".
    * Increase robots, search engines database (Added Google Chrome browser,
      better Vista, WII, detection, ...)
    * Update languages files.
    * Added a lot of patch from sourceforge.
    * Bug fixes.
    ---
    Module Name:	pkgsrc
    Committed By:	adam
    Date:		Thu Jan 15 12:12:20 UTC 2009
    
    Removed Files:
    	pkgsrc/www/awstats/patches: patch-ac
    
    Log Message:
    Changes 6.9:
    * With postfix that support DSN (Delivery Status Notifications) we exclude
      some lines to avoid counting mails twice in maillogconvert.pl script.
    * Logresolvemerge.pl support FreeRADIUS logs or anything else using
      (the fixed length!) ctime format timestamp.
    * Add option stoponfirsteof in logresolvemerge tool.
    * Add patch to support host_proxy tag in LogFormat (for Apache LogFormat
      containing %{X-Forwarded-For}i)
    * Renamed Add to favourites on "Hit on favicon".
    * Increase robots, search engines database (Added Google Chrome browser,
      better Vista, WII, detection, ...)
    * Update languages files.
    * Added a lot of patch from sourceforge.
    * Bug fixes.

--- www/awstats/options.mk
+++ www/awstats/options.mk
@@ -1,14 +1,14 @@
-# $NetBSD: options.mk,v 1.1 2008/01/03 12:37:06 adrianp Exp $
+# $NetBSD: options.mk,v 1.1.12.1 2009/01/31 21:28:41 tron Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.awstats
 
-PKG_SUPPORTED_OPTIONS=	awstats-geoipfree
+PKG_SUPPORTED_OPTIONS=	geoip
 
 .include "../../mk/bsd.options.mk"
 
 ###
 ### Use the Geo IP Free module
 ###
-.if !empty(PKG_OPTIONS:Mawstats-geoipfree)
+.if !empty(PKG_OPTIONS:Mgeoip)
 DEPENDS+=	p5-Geo-IPfree-[0-9]*:../../net/p5-Geo-IPfree
 .endif
