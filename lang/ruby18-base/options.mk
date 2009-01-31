ec452cf9a2125c79e5360f8efb2a7f48a1b3b91f
taca <taca>
2009-01-31 16:07:23 +0000

    Move ri data generation from install phase to build phase.
    No other functional change.

--- lang/ruby18-base/options.mk
+++ lang/ruby18-base/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2008/02/19 03:56:08 taca Exp $
+# $NetBSD: options.mk,v 1.6 2009/01/31 16:07:23 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ruby
 PKG_SUPPORTED_OPTIONS=	ruby-build-ri-db
@@ -7,6 +7,7 @@ PKG_SUGGESTED_OPTIONS=	ruby-build-ri-db
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mruby-build-ri-db)
+BUILD_TARGET=	all rdoc
 CONFIGURE_ARGS+=	--enable-install-doc
 PRIVILEGED_STAGES+=    clean
 RUBY_DYNAMIC_DIRS+=	${RUBY_SYSRIDIR}
