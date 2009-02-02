bb89e5883052dc7fde7ee6cf6d871cf6bcd8db3c
joerg <joerg>
2009-02-02 12:34:59 +0000

    Merge pkg_install-20090201 from pkg_install-renovation branch.
    - DB support is always included from libnbcompat if needed
    - pkg_view and linkfarm are not installed any more; they are not moved
    into the attic yet, so they can easily be installed as separte package
    - common configuration file to customise the behavior of various
    components; this supersedes the old audit-packages.conf
    - support for PKSC7 signatures (using X509 certs) and GPG signatures for
    packages in a secure way. See pkg_admin(8) for how to create them and
    pkg_install.conf(5) for the options to use them
    - audit-packages and download-vulnerability-list are wrapper scripts
      around pkg_admin. They try to mimic the classic options if used sanely.
      "pkg_admin audit" is now an order of magnitude faster than before
    - pkg_add uses libarchive and libfetch instead of external ftp and tar:
      - progress bar is currently missing for downloads
      - "pkg_add -" is no longer supported
      - no adhoc check for conficts between dependencies and already
        installed packages
      - "pkg_add -s" has been replaced with an option in pkg_install.conf,
        verification of plain detached GPG signatures is no longer supported
      - optional check for vulnerabilities before adding a package
      - if /var and /usr/pkg are on different fileystems it is twice as fast
        now
      - conflicts due to overlapping plists are checked before installation
      - pkg_add no longer plays with the process limits
    - pkg_add and pkg_delete have a new destdir option; scripts have to
      either be modified to use PKG_DESTDIR or should be disabled
    - pkg_add -u for now can't be used to update to the exact same version
    - internal "rm -rf" and "mkdir_p" code
    - all memory allocation failures are not explicitly fatal
    - if a file is not removed due to a failed checksum, still remove the
      entry from pkgdb

--- pkgtools/pkg_install/files/info/main.c
+++ pkgtools/pkg_install/files/info/main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main.c,v 1.22 2007/11/05 09:39:38 joerg Exp $	*/
+/*	$NetBSD: main.c,v 1.23 2009/02/02 12:35:01 joerg Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -7,13 +7,7 @@
 #if HAVE_SYS_CDEFS_H
 #include <sys/cdefs.h>
 #endif
-#ifndef lint
-#if 0
-static char *rcsid = "from FreeBSD Id: main.c,v 1.14 1997/10/08 07:47:26 charnier Exp";
-#else
-__RCSID("$NetBSD: main.c,v 1.22 2007/11/05 09:39:38 joerg Exp $");
-#endif
-#endif
+__RCSID("$NetBSD: main.c,v 1.23 2009/02/02 12:35:01 joerg Exp $");
 
 /*
  *
@@ -271,11 +265,9 @@ main(int argc, char **argv)
 
 			s = pkgdb_retrieve(CheckPkg);
 
-			if (s) {
-				CheckPkg = strdup(s);
-			} else {
+			if (s == NULL)
 				errx(EXIT_FAILURE, "No matching pkg for %s.", CheckPkg);
-			}
+			CheckPkg = xstrdup(s);
 
 			pkgdb_close();
 		}
