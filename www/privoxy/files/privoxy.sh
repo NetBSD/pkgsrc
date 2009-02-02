7071c605b43024459f7169b178fa71ccd2859af3
jnemeth <jnemeth@pkgsrc.org>
2009-02-02 20:00:40 +0000

    PR/40532 - Cem Kayali -- group permissions too broad Just fixing security issue, will leave pkg update for MAINTAINER for now.

--- www/privoxy/files/privoxy.sh
+++ www/privoxy/files/privoxy.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: privoxy.sh,v 1.3 2003/07/12 15:11:39 salo Exp $
+# $NetBSD: privoxy.sh,v 1.4 2009/02/02 20:00:40 jnemeth Exp $
 #
 
 # PROVIDE: privoxy
@@ -14,8 +14,9 @@ command="/usr/pkg/sbin/${name}"
 pidfile="/var/run/${name}.pid"
 pconfig="@PKG_SYSCONFDIR@/config"
 puser="@PRIVOXY_USER@"
+pgroup="@PRIVOXY_GROUP@"
 required_files="$pconfig"
-command_args="--pidfile ${pidfile} --user ${puser} ${pconfig} 2>/dev/null"
+command_args="--pidfile ${pidfile} --user ${puser}.${pgroup} ${pconfig} 2>/dev/null"
 
 load_rc_config $name
 run_rc_command "$1"
