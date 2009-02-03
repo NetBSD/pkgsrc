47526a0411d74a9eaa0f4fbdf575040cd55a835d
adam <adam@pkgsrc.org>
2009-02-03 06:21:40 +0000

    PLIST fix; svg now is an option

--- graphics/gegl/buildlink3.mk
+++ graphics/gegl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/10/16 21:53:57 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/02/03 06:21:40 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GEGL_BUILDLINK3_MK:=	${GEGL_BUILDLINK3_MK}+
@@ -16,9 +16,15 @@ BUILDLINK_API_DEPENDS.gegl+=	gegl>=0.0.20
 BUILDLINK_PKGSRCDIR.gegl?=	../../graphics/gegl
 .endif	# GEGL_BUILDLINK3_MK
 
+pkgbase := gegl
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.gegl:Msvg)
+.include "../../graphics/librsvg/buildlink3.mk"
+.endif
+
 .include "../../graphics/babl/buildlink3.mk"
 .include "../../graphics/jpeg/buildlink3.mk"
-.include "../../graphics/librsvg/buildlink3.mk"
 .include "../../graphics/openexr/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
