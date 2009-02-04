f9e431ab2a215ec706c139e77f646e3a9624d541
drochner <drochner@pkgsrc.org>
2009-02-04 21:20:39 +0000

    give up supplementary group memberships on uid/gid switch, fixes unexpected privileges reported in PR pkg/40532 by Cem Kayali, the issue is being discussed with upstream, thanks to Cem for detailed reports, also back out explicit passing of PRIVOXY_GROUP to the program -- while it does not hurt it is redundant because PRIVOXY_GROUP is already the primary group of PRIVOXY_USER

--- www/privoxy/files/privoxy.sh
+++ www/privoxy/files/privoxy.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: privoxy.sh,v 1.4 2009/02/02 20:00:40 jnemeth Exp $
+# $NetBSD: privoxy.sh,v 1.5 2009/02/04 21:20:39 drochner Exp $
 #
 
 # PROVIDE: privoxy
@@ -14,9 +14,8 @@ command="/usr/pkg/sbin/${name}"
 pidfile="/var/run/${name}.pid"
 pconfig="@PKG_SYSCONFDIR@/config"
 puser="@PRIVOXY_USER@"
-pgroup="@PRIVOXY_GROUP@"
 required_files="$pconfig"
-command_args="--pidfile ${pidfile} --user ${puser}.${pgroup} ${pconfig} 2>/dev/null"
+command_args="--pidfile ${pidfile} --user ${puser} ${pconfig} 2>/dev/null"
 
 load_rc_config $name
 run_rc_command "$1"
