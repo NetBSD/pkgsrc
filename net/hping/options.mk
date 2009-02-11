34f140022174f4cfde918ab248d57d76c2253d04
obache <obache@pkgsrc.org>
2009-02-11 08:06:17 +0000

    * detection of endian in bytesex.h is not enough, use result of byteorder.h. * replace hard coded user and mode for hping-suid option with SPECIAL_PERMS. * replace remaining of hard coded "man" with ${PKGMANDIR}. * add DESTDIR support. * merge patch-a{e,g}, both for same file. * patch to replace __sun__ with __sun for portability, pkglint(1) said. * also regen other patches with mkpatches(1).

--- net/hping/options.mk
+++ net/hping/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2006/03/30 13:51:28 salo Exp $
+# $NetBSD: options.mk,v 1.2 2009/02/11 08:06:17 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.hping
 PKG_SUPPORTED_OPTIONS=	hping-suid
@@ -6,6 +6,5 @@ PKG_SUPPORTED_OPTIONS=	hping-suid
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mhping-suid)
-BINOWN=			root
-BINMODE=		4511
+SPECIAL_PERMS+=	sbin/hping2 ${SETUID_ROOT_PERMS}
 .endif
