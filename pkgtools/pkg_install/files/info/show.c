8c9efc58c98731aaa91c290bd9fa645a6e30da84
joerg <joerg@pkgsrc.org>
2009-02-11 23:51:30 +0000

    Don't segfault if running pkg_info on a binary package without +BUILD_INFO (or installed without +INSTALLED_INFO). Second half of PR 30276.

--- pkgtools/pkg_install/files/info/show.c
+++ pkgtools/pkg_install/files/info/show.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: show.c,v 1.23 2009/02/02 12:35:01 joerg Exp $	*/
+/*	$NetBSD: show.c,v 1.24 2009/02/11 23:51:30 joerg Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -7,7 +7,7 @@
 #if HAVE_SYS_CDEFS_H
 #include <sys/cdefs.h>
 #endif
-__RCSID("$NetBSD: show.c,v 1.23 2009/02/02 12:35:01 joerg Exp $");
+__RCSID("$NetBSD: show.c,v 1.24 2009/02/11 23:51:30 joerg Exp $");
 
 /*
  * FreeBSD install - a package for the installation and maintainance
@@ -366,7 +366,10 @@ show_summary(struct pkg_meta *meta, package_t *plist, const char *binpkgfile)
 	print_string_as_var("COMMENT", meta->meta_comment);
 	print_string_as_var("SIZE_PKG", meta->meta_size_pkg);
 
-	var_copy_list(meta->meta_build_info, bi_vars);
+	if (meta->meta_build_info)
+		var_copy_list(meta->meta_build_info, bi_vars);
+	else
+		warnx("Build information missing");
 
 	if (binpkgfile != NULL && stat(binpkgfile, &st) == 0) {
 	    const char *base;
