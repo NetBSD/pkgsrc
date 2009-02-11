4efb7580b9583eb8cf19dc13e76ac7e855c86fd4
joerg <joerg@pkgsrc.org>
2009-02-11 14:34:36 +0000

    Use pkg_admin for audit processing if pkg_install >= 20090201 is installed.

--- mk/flavor/pkg/check.mk
+++ mk/flavor/pkg/check.mk
@@ -1,4 +1,4 @@
-# $NetBSD: check.mk,v 1.9 2008/04/07 13:16:26 joerg Exp $
+# $NetBSD: check.mk,v 1.10 2009/02/11 14:34:36 joerg Exp $
 #
 
 # _flavor-check-vulnerable:
@@ -7,7 +7,7 @@
 #
 _flavor-check-vulnerable: .PHONY
 	${_PKG_SILENT}${_PKG_DEBUG}					\
-	_PKGVULNDIR=`${AUDIT_PACKAGES} ${AUDIT_PACKAGES_FLAGS} -Q PKGVULNDIR`; \
+	_PKGVULNDIR=`${_EXTRACT_PKGVULNDIR}`; \
 	vulnfile=$$_PKGVULNDIR/pkg-vulnerabilities;		\
 	if ${TEST} ! -f "$$vulnfile"; then 			\
 		${PHASE_MSG} "Skipping vulnerability checks.";	\
@@ -16,7 +16,7 @@ _flavor-check-vulnerable: .PHONY
 		exit 0;						\
 	fi;							\
 	${PHASE_MSG} "Checking for vulnerabilities in ${PKGNAME}"; \
-	${AUDIT_PACKAGES} ${AUDIT_PACKAGES_FLAGS} -n ${PKGNAME};	\
+	${AUDIT_PACKAGES} ${_AUDIT_PACKAGES_CMD} ${AUDIT_PACKAGES_FLAGS} ${PKGNAME};	\
 	if ${TEST} "$$?" -ne 0; then				\
 		${ERROR_MSG} "Define ALLOW_VULNERABLE_PACKAGES in mk.conf or IGNORE_URLS in audit-packages.conf(5) if this package is absolutely essential."; \
 		${FALSE};					\
