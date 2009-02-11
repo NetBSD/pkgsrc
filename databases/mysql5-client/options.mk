ce94a8dcf2e04a869861cd6ecdb40f2f3ad61fe3
adrianp <adrianp>
2009-02-11 00:16:41 +0000

    Fix for ssl support on OpenSolaris

--- databases/mysql5-client/options.mk
+++ databases/mysql5-client/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2007/07/09 22:34:27 adrianp Exp $
+# $NetBSD: options.mk,v 1.2 2009/02/11 00:16:41 adrianp Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mysql5-client
 
@@ -12,7 +12,18 @@ PKG_SUGGESTED_OPTIONS=	ssl
 ###
 .if !empty(PKG_OPTIONS:Mssl)
 .	include "../../security/openssl/buildlink3.mk"
+. if ${OPSYS} == "SunOS"
+CHECK_BUILTIN.openssl:=yes
+.  include "../../security/openssl/builtin.mk"
+CHECK_BUILTIN.openssl:=no
+.  if ${USE_BUILTIN.openssl} == "yes"
+CONFIGURE_ARGS+=	--with-openssl=yes
+CONFIGURE_ARGS+=	--with-openssl-libs=/lib
+CONFIGURE_ARGS+=	--with-openssl-includes=/usr/include
+.  endif
+. else
 CONFIGURE_ARGS+=	--with-openssl=${BUILDLINK_PREFIX.openssl}
+. endif
 .else
 CONFIGURE_ARGS+=	--without-openssl
 .endif
