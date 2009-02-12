db54efee8d777a50ec1e61db776dbd99aa969747
drochner <drochner@pkgsrc.org>
2009-02-12 22:52:49 +0000

    move the libltdl where it belongs

--- audio/libmp3splt/buildlink3.mk
+++ audio/libmp3splt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2008/10/05 22:47:23 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/02/12 22:52:49 drochner Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBMP3SPLT_BUILDLINK3_MK:=	${LIBMP3SPLT_BUILDLINK3_MK}+
@@ -25,5 +25,6 @@ BUILDLINK_DEPMETHOD.libvorbis?= build
 .include "../../audio/libvorbis/buildlink3.mk"
 
 .include "../../multimedia/libogg/buildlink3.mk"
+.include "../../devel/libltdl/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
