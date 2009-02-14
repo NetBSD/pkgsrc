b35502ed7380bb67dbf72862dffcae84d93a10ad
joerg <joerg@pkgsrc.org>
2009-02-14 17:08:05 +0000

    Tag as pkg_install-20090214 in preparation for updating NetBSD current.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.111 2009/02/13 01:31:42 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.112 2009/02/14 17:08:05 joerg Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20090213"
+#define PKGTOOLS_VERSION "20090214"
 
 #endif /* _INST_LIB_VERSION_H_ */
