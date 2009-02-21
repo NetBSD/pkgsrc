1780d36a515a238337b8aa2bd19f8809b7b6df82
wiz <wiz@pkgsrc.org>
2009-02-21 13:20:45 +0000

    Update to 1.0.18:
    Version 1.0.18 (2009-02-07)
      * Add Ogg/Vorbis support (thanks to John ffitch).
      * Remove captive FLAC library.
      * Many new features and bug fixes.
      * Generate Win32 and Win64 pre-compiled binaries.
    
    Add ogg and vorbis dependencies to bl3.mk and increase ABI depends.

--- audio/libsndfile/buildlink3.mk
+++ audio/libsndfile/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2007/01/07 12:25:52 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2009/02/21 13:20:45 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBSNDFILE_BUILDLINK3_MK:=	${LIBSNDFILE_BUILDLINK3_MK}+
@@ -12,11 +12,13 @@ BUILDLINK_PACKAGES+=	libsndfile
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libsndfile
 
 .if !empty(LIBSNDFILE_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.libsndfile+=		libsndfile>=1.0.0
-BUILDLINK_ABI_DEPENDS.libsndfile+=	libsndfile>=1.0.17nb1
+BUILDLINK_API_DEPENDS.libsndfile+=	libsndfile>=1.0.0
+BUILDLINK_ABI_DEPENDS.libsndfile+=	libsndfile>=1.0.18
 BUILDLINK_PKGSRCDIR.libsndfile?=	../../audio/libsndfile
 .endif	# LIBSNDFILE_BUILDLINK3_MK
 
+.include "../../audio/libvorbis/buildlink3.mk"
 .include "../../audio/flac/buildlink3.mk"
+.include "../../multimedia/libogg/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
