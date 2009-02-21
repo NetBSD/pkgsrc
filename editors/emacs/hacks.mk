0e218ffd28b59425ef9cad119ffce5a3e5689425
tron <tron>
2009-02-21 23:18:30 +0000

    Pullup ticket #2705 - requested by he emacs: build fix
    Revisions pulled up:
    - editors/emacs/hacks.mk		1.3
    ---
    Module Name:	pkgsrc
    Committed By:	markd
    Date:		Wed Jan 28 11:36:34 UTC 2009
    
    Modified Files:
    	pkgsrc/editors/emacs: hacks.mk
    
    Log Message:
    Seems the hack is needed for 4.0/x86_64 as well.  See Manuel Bouyer's
    bulk builds.

--- editors/emacs/hacks.mk
+++ editors/emacs/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.2 2008/11/14 16:05:15 tron Exp $
+# $NetBSD: hacks.mk,v 1.2.2.1 2009/02/21 23:18:30 tron Exp $
 
 .if !defined(EMACS_HACKS_MK)
 EMACS_HACKS_MK=	# defined
@@ -8,8 +8,7 @@ EMACS_HACKS_MK=	# defined
 ### Workaround for PR pkg/39778
 ###
 .  include "../../mk/bsd.fast.prefs.mk"
-.  if !empty(MACHINE_PLATFORM:MNetBSD-4.99.*-x86_64) || \
-      !empty(MACHINE_PLATFORM:MNetBSD-[5-9].*-x86_64)
+.  if !empty(MACHINE_PLATFORM:MNetBSD-[4-9].*-x86_64)
 pre-build:
 	${TOUCH} ${WRKSRC}/leim/quail/tsang-b5.el
 .  endif
