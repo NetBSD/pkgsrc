2c6cb6cd63fe16253b9be5a1a398275ec112cb5a
adrianp <adrianp@pkgsrc.org>
2009-02-23 09:22:16 +0000

    Since rc1 the default location of the pidfiles have changed so update the rc.d scripts accordingly. Problem found by John Klos

--- net/bind96/files/lwresd.sh
+++ net/bind96/files/lwresd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: lwresd.sh,v 1.1.1.1 2009/01/04 00:21:36 adrianp Exp $
+# $NetBSD: lwresd.sh,v 1.2 2009/02/23 09:22:16 adrianp Exp $
 #
 # PROVIDE: lwresd
 # REQUIRE: SERVERS
@@ -10,9 +10,16 @@
 name="lwresd"
 rcvar=${name}
 command="@PREFIX@/sbin/${name}"
-pidfile="/var/run/${name}.pid"
-
+pidfile="@VARBASE@/run/lwresd/${name}.pid"
 extra_commands="reload"
+start_precmd="lwresd_precmd"
+
+lwresd_precmd()
+{
+	if [ ! -d @VARBASE@/run/lwresd ]; then
+		@MKDIR@ @VARBASE@/run/lwresd
+	fi
+}
 
 load_rc_config ${name}		# check /etc/rc.conf.d/named9
 run_rc_command "$1"
