04dad03ac1c3110a1a4980f4b9b33ef671548845
wiz <wiz@pkgsrc.org>
2009-02-23 11:00:01 +0000

    Filter another directory for print-PLIST.

--- sysutils/dbus/buildlink3.mk
+++ sysutils/dbus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/01/13 10:55:37 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2009/02/23 11:00:01 wiz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 DBUS_BUILDLINK3_MK:=	${DBUS_BUILDLINK3_MK}+
@@ -19,6 +19,8 @@ PRINT_PLIST_AWK+=	/^@dirrm include\/dbus-1.0\/dbus$$/ \
 				{ print "@comment in dbus: " $$0; next; }
 PRINT_PLIST_AWK+=	/^@exec ..MKDIR. %D\/share\/dbus-1\/services/ { next; }
 PRINT_PLIST_AWK+=	/^@dirrm share\/dbus-1\/services/ \
+				{ print "@comment in dbus: " $$0; next }
+PRINT_PLIST_AWK+=	/^@dirrm share\/dbus-1/ \
 				{ print "@comment in dbus: " $$0; next }
 .endif	# DBUS_BUILDLINK3_MK
 
