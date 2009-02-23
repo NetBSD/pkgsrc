3745eeee8928195bc6294245367bc6766f2c7887
cube <cube@pkgsrc.org>
2009-02-23 23:19:22 +0000

    Remove net/netbsd-tap.  NetBSD 2.0 is just too old;  later versions of NetBSD have tap(4) in the kernel.

--- net/netbsd-tap/files/tap_postinstall.sh
+++
@@ -1,8 +0,0 @@
-#!@SH@
-
-rm -f /dev/tap /dev/tap0 /dev/tap1 /dev/tap2 /dev/tap3
-mknod /dev/tap c $3 0xfffff
-mknod /dev/tap0 c $3 0
-mknod /dev/tap1 c $3 1
-mknod /dev/tap2 c $3 2
-mknod /dev/tap3 c $3 3
