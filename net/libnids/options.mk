3c876a5b8addc7a4b1447901c23b6ff58eae8b0a
cube <cube>
2009-02-24 00:44:26 +0000

    Silence some more pkglint warnings.

--- net/libnids/options.mk
+++ net/libnids/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2009/02/24 00:42:29 cube Exp $
+# $NetBSD: options.mk,v 1.6 2009/02/24 00:44:26 cube Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.libnids
 PKG_OPTIONS_REQUIRED_GROUPS=	libnet
@@ -12,7 +12,7 @@ PKG_SUGGESTED_OPTIONS=		libnet10
 ###
 .if !empty(PKG_OPTIONS:Mlibnet10)
 .	include "../../devel/libnet10/buildlink3.mk"
-BUILDLINK_DEPMETHOD.libnet10=	build
+BUILDLINK_DEPMETHOD.libnet10+=	build
 SUBST_SED.conf=	-e "s|libnet-config|libnet10-config|g"
 .endif
 
@@ -21,6 +21,6 @@ SUBST_SED.conf=	-e "s|libnet-config|libnet10-config|g"
 ###
 .if !empty(PKG_OPTIONS:Mlibnet11)
 .	include "../../devel/libnet11/buildlink3.mk"
-BUILDLINK_DEPMETHOD.libnet11=	build
+BUILDLINK_DEPMETHOD.libnet11+=	build
 SUBST_SED.conf=	-e "s|libnet-config|libnet11-config|g"
 .endif
