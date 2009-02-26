282a65aad13a75f256f12d9cd641962f27f2351c
sno <sno@pkgsrc.org>
2009-02-26 19:45:14 +0000

    pkgsrc changes:
    - update to 2.20.3
    
    Upstream changes:
    
    October 7, 2008
            - Release 2.20.1
            - Security fix for stack overflow problem in the graph parser reported by IBM
    June 25, 2008
            - Release 2.20.2
            - Fix bug in HTML-like labels
    June 23, 2008
            - Release 2.20.1
            - Fix bug in ccomps related to conversion to cgraph
    June 20, 2008
            - Release 2.20.0
            - Preparing for Release 2.20
            - Fixed bugs: 1315, 1317, 1324, 1336, 1343, 1364
            - Add new "folder" shape for nodes.
            - Migration of gvpr tools to libcgraph.
            - New output format -Teps  (encapsulated postscript)
            - Various NetBSD and SuSE fixes incorporated
            - ./configure now provides a summary
            - RPM specfile updates for fedora-10 (no more string comparisons)
            - Add MacOS support (Glen Low)
    March 10, 2008
            - Release 2.18
            - Fixed bugs: 1249, 1255, 1256, 1268, 1276, 1289, 1295, 1300,
                    Fedora BZ#247376,
            - in -Tps use a new number formatter that suppresses trailing 0.
            - support tcl/tk-8.5
            - support gcc-4.3
            - support for node usershapes/images in svg format (thanks Alex Poylisher)
            - install: perl, php, python, ruby, tcl, bindings in language-specified directories
            - add arrowhead scaling with edge penwidth
            - add "folder" node shape (thanks Pander)
            - many windows and mac fixes (thanks Glen)
            - add "smyna" large graph view (thanks Arif) (not yet included in binary distros)

--- graphics/graphviz/options.mk
+++ graphics/graphviz/options.mk
@@ -1,14 +1,23 @@
-# $NetBSD: options.mk,v 1.4 2008/11/15 01:28:41 epg Exp $
+# $NetBSD: options.mk,v 1.5 2009/02/26 19:45:14 sno Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.graphviz
-PKG_SUPPORTED_OPTIONS=	guile lua ocaml swig tcl gtk
-PKG_SUGGESTED_OPTIONS=	lua swig tcl
+PKG_SUPPORTED_OPTIONS=	x11 pangocairo guile lua ocaml swig tcl gtk gd
+PKG_SUGGESTED_OPTIONS=	x11 pangocairo lua swig tcl
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		guile gtk lua ocaml tcl
+PLIST_VARS+=		guile gtk lua ocaml tcl pangocairo x11 gd
 
-.if !empty(PKG_OPTIONS:Mguile)
+.if !empty(PKG_OPTIONS:Mx11)
+.include "../../x11/libXp/buildlink3.mk"
+.include "../../mk/xaw.buildlink3.mk"
+CONFIGURE_ENV+=		X11PREFIX=${X11PREFIX} X11BASE=${X11BASE}
+PLIST.x11=		yes
+.else
+CONFIGURE_ARGS+=	--without-x
+.endif
+
+.if !empty(PKG_OPTIONS:Mx11) && !empty(PKG_OPTIONS:Mguile)
 .include "../../lang/guile/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-guile
 PLIST.guile=		yes
@@ -45,10 +54,26 @@ PLIST.tcl=		yes
 CONFIGURE_ARGS+=	--disable-tcl
 .endif
 
-.if !empty(PKG_OPTIONS:Mgtk)
+.if !empty(PKG_OPTIONS:Mx11) && !empty(PKG_OPTIONS:Mgtk)
 .include "../../x11/gtk2/buildlink3.mk"
 PLIST.gtk=		yes
 .else
 CONFIGURE_ARGS+=	--without-gdk-pixbuf
 CONFIGURE_ARGS+=	--without-gtk
+CONFIGURE_ARGS+=	--without-gnomeui
+.endif
+
+.if !empty(PKG_OPTIONS:Mgd)
+.include "../../graphics/gd/buildlink3.mk"
+PLIST.gd=		yes
+.else
+CONFIGURE_ARGS+=	--without-libgd
+.endif
+
+.if !empty(PKG_OPTIONS:Mx11) && !empty(PKG_OPTIONS:Mpangocairo)
+.include "../../devel/pango/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
+PLIST.pangocairo=		yes
+.else
+CONFIGURE_ARGS+=	--without-pangocairo
 .endif
