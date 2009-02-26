42d533291b6adbe82506fed2dd88146c71eab256
sno <sno@pkgsrc.org>
2009-02-26 19:45:14 +0000

    pkgsrc changes:
    - update to 2.20.3
    
    Upstream changes:
    
    October 7, 2008
            - Release 2.20.1
            - Security fix for stack overflow problem in the graph parser reported by IBM
    June 25, 2008
            - Release 2.20.2
            - Fix bug in HTML-like labels
    June 23, 2008
            - Release 2.20.1
            - Fix bug in ccomps related to conversion to cgraph
    June 20, 2008
            - Release 2.20.0
            - Preparing for Release 2.20
            - Fixed bugs: 1315, 1317, 1324, 1336, 1343, 1364
            - Add new "folder" shape for nodes.
            - Migration of gvpr tools to libcgraph.
            - New output format -Teps  (encapsulated postscript)
            - Various NetBSD and SuSE fixes incorporated
            - ./configure now provides a summary
            - RPM specfile updates for fedora-10 (no more string comparisons)
            - Add MacOS support (Glen Low)
    March 10, 2008
            - Release 2.18
            - Fixed bugs: 1249, 1255, 1256, 1268, 1276, 1289, 1295, 1300,
                    Fedora BZ#247376,
            - in -Tps use a new number formatter that suppresses trailing 0.
            - support tcl/tk-8.5
            - support gcc-4.3
            - support for node usershapes/images in svg format (thanks Alex Poylisher)
            - install: perl, php, python, ruby, tcl, bindings in language-specified directories
            - add arrowhead scaling with edge penwidth
            - add "folder" node shape (thanks Pander)
            - many windows and mac fixes (thanks Glen)
            - add "smyna" large graph view (thanks Arif) (not yet included in binary distros)

--- graphics/graphviz/buildlink3.mk
+++ graphics/graphviz/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2008/02/06 18:56:52 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2009/02/26 19:45:14 sno Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GRAPHVIZ_BUILDLINK3_MK:=	${GRAPHVIZ_BUILDLINK3_MK}+
@@ -18,7 +18,7 @@ BUILDLINK_PKGSRCDIR.graphviz?=	../../graphics/graphviz
 .endif	# GRAPHVIZ_BUILDLINK3_MK
 
 .include "../../converters/libiconv/buildlink3.mk"
-.include "../../devel/pango/buildlink3.mk"
+#.include "../../devel/pango/buildlink3.mk"
 .include "../../graphics/jpeg/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
