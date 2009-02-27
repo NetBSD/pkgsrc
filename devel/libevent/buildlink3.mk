d9471c7839c9529430a119ad3feacd1d16bd60fa
wiz <wiz>
2009-02-27 22:50:43 +0000

    Update to 1.4.9, bump ABI depends as usual for this package.
    Changes in 1.4.9-stable:
     o event_add would not return error for some backends; from Dean
     McNamee
     o Clear the timer cache on entering the event loop; reported by
     Victor Chang
     o Only bind the socket on connect when a local address has been
     provided; reported by Alejo Sanchez
     o Allow setting of local port for evhttp connections to support
     millions of connections from a single system; from Richard Jones.
     o Clear the timer cache when leaving the event loop; reported by
     Robin Haberkorn
     o Fix a typo in setting the global event base; reported by lance.
     o Fix a memory leak when reading multi-line headers
     o Fix a memory leak by not running explicit close detection for
     server connections

--- devel/libevent/buildlink3.mk
+++ devel/libevent/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2008/10/16 21:51:47 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2009/02/27 22:50:43 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBEVENT_BUILDLINK3_MK:=	${LIBEVENT_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libevent
 
 .if !empty(LIBEVENT_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.libevent+=libevent>=0.6
-BUILDLINK_ABI_DEPENDS.libevent+=libevent-1.4.8* # exact match -- see Makefile
+BUILDLINK_ABI_DEPENDS.libevent+=libevent-1.4.9* # exact match -- see Makefile
 BUILDLINK_PKGSRCDIR.libevent?=	../../devel/libevent
 .endif	# LIBEVENT_BUILDLINK3_MK
 
