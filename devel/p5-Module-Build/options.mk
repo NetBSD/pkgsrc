bcdcd7c04b221d146adeeb5d7350ce3a662b8513
sno <sno>
2009-03-01 21:47:22 +0000

    pkgsrc changes:
    - update to module version 0.32
    - introduce options to choose recommented dependencies
    - make previous dependencies and other useful default
    
    Upstream changes:
    
    0.32 - Wed Feb 25 17:40:02 PST 2009
    
     No changes since 0.31_04.
    
    0.31_04 - Fri Feb 20 11:04:59 PST 2009
    
     Other
     - Bumped Test::Harness prereq to 3.16 for latest PERL5LIB fixes (solves
       test failures when installing Module::Build using CPANPLUS::Dist::Build)
       [David Golden]
    
    0.31_03 - Sun Feb  8 14:54:01 PST 2009
    
     Enhancements
     - added a "prereq_data" action that prints a Perl data structure of
       all prerequisites; can be loaded by external tools using eval()
       [David Golden]
    
     Bug-fixes
     - 'fakeinstall' action warns and skips without ExtUtils::Install 1.32+
       [David Golden, reported by Zefram]
     - allows Module::Build version mismatch when installing self; works around
       limitations in CPANPLUS::Dist::Build [David Golden]
    
    0.31_02 - Tue Jan 27 09:16:43 PST 2009
    
     Other
     - tests now use File::Temp (added to build_requires); appears to fix
       Win32 testing heisenbug on directory removal during high system loads
     - use_tap_harness.t will skip unless a release version of TAP::Harness
       is installed
     - improved diagnostics to ensure_blib() tests in t/lib/MBTest.pm
    
     Compat
     - passthrough Makefile.PL will now play nice with cpantesters' on
       exit(0) (RT#32018) [Eric Wilhelm]
    
     Bug Fixes
     - fix for doubling-up of --prefix (RT#19951)

---
+++ devel/p5-Module-Build/options.mk
@@ -0,0 +1,36 @@
+# $NetBSD: options.mk,v 1.1 2009/03/01 21:47:22 sno Exp $
+
+PKG_OPTIONS_VAR=        PKG_OPTIONS.p5-Module-Build
+
+PKG_SUPPORTED_OPTIONS=  archive-tar module-signature pod-readme
+PKG_SUPPORTED_OPTIONS+=	extutils-parsexs extutils-cbuilder extutils-install
+PKG_SUPPORTED_OPTIONS+=	yaml version
+PKG_SUGGESTED_OPTIONS=  archive-tar extutils-cbuilder extutils-parsexs
+PKG_SUGGESTED_OPTIONS+=	pod-readme yaml version
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Marchive-tar)
+DEPENDS+=	{perl>=5.10,p5-Archive-Tar>=1.08}:../../archivers/p5-Archive-Tar
+.endif
+.if !empty(PKG_OPTIONS:Myaml)
+BUILD_DEPENDS+=	p5-YAML>=0.50:../../textproc/p5-YAML
+.endif
+.if !empty(PKG_OPTIONS:Mmodule-signature)
+DEPENDS+=	p5-Module-Signature>=0.55:../../security/p5-Module-Signature
+.endif
+.if !empty(PKG_OPTIONS:Mextutils-parsexs)
+DEPENDS+=	{perl>=5.10,p5-ExtUtils-ParseXS>=1.02}:../../devel/p5-ExtUtils-ParseXS
+.endif
+.if !empty(PKG_OPTIONS:Mextutils-cbuilder)
+DEPENDS+=	p5-ExtUtils-CBuilder>=0.15:../../devel/p5-ExtUtils-CBuilder
+.endif
+.if !empty(PKG_OPTIONS:Mextutils-install)
+DEPENDS+=	p5-ExtUtils-Install>=0.30:../../devel/p5-ExtUtils-Install
+.endif
+.if !empty(PKG_OPTIONS:Mpod-readme)
+DEPENDS+=	p5-Pod-Readme>=0.04:../../textproc/p5-Pod-Readme
+.endif
+.if !empty(PKG_OPTIONS:Mversion)
+DEPENDS+=	p5-version>=0.74:../../devel/p5-version
+.endif
