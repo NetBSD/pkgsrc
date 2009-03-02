9e033dff1ddc24ae23e16469aa17a999460ed7c0
manu <manu@pkgsrc.org>
2009-03-02 21:08:34 +0000

    Missing library for dependencies

--- security/xmlsec1/buildlink3.mk
+++ security/xmlsec1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2009/03/02 16:44:29 manu Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/03/02 21:08:34 manu Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 XMLSEC1_BUILDLINK3_MK:=	${XMLSEC1_BUILDLINK3_MK}+
@@ -16,6 +16,7 @@ BUILDLINK_API_DEPENDS.xmlsec1+=	xmlsec1>=1.2.6nb6
 BUILDLINK_PKGSRCDIR.xmlsec1?=	../../security/xmlsec1
 BUILDLINK_INCDIRS.xmlsec1+=	include/xmlsec1
 BUILDLINK_CPPFLAGS.xmlsec1+=	-DXMLSEC_CRYPTO_OPENSSL
+BUILDLINK_LIBS.xmlsec1+=	-lxmlsec1-openssl
 .endif	# XMLSEC1_BUILDLINK3_MK
 
 .include "../../security/openssl/buildlink3.mk"
