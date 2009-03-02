4f2896e127a542c42a2986c3325d62ab19767cd8
joerg <joerg>
2009-03-02 14:59:14 +0000

    pkg_install-20090301: Plug a number of file descriptor leaks.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.115 2009/02/28 16:03:56 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.116 2009/03/02 14:59:14 joerg Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20090228"
+#define PKGTOOLS_VERSION "20090301"
 
 #endif /* _INST_LIB_VERSION_H_ */
