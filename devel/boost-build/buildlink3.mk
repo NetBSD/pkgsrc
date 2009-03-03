ac1ade435fae20b0013456d6301edcca000b61ff
jmmv <jmmv@pkgsrc.org>
2009-03-03 08:57:57 +0000

    Update boost to 1.38.0.  Patches from Brook Milligan in private mail.
    Lots of changes to list here.  As usual, some new libraries have been
    added and there have been improvements all around.  Of special interest
    is that we can now erase most of our local patches because they have
    been imported upstream.

--- devel/boost-build/buildlink3.mk
+++ devel/boost-build/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2008/09/13 16:14:13 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2009/03/03 08:57:57 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 BOOST_BUILD_BUILDLINK3_MK:=	${BOOST_BUILD_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	boost-build
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}boost-build
 
 .if !empty(BOOST_BUILD_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.boost-build+=		boost-build-1.36.*
+BUILDLINK_API_DEPENDS.boost-build+=		boost-build-1.38.*
 BUILDLINK_DEPMETHOD.boost-build?=	build
 BUILDLINK_PKGSRCDIR.boost-build?=	../../devel/boost-build
 .endif	# BOOST_BUILD_BUILDLINK3_MK
