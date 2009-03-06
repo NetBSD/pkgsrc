2b62e294f3baf8f5c145a300d7efbce38e187506
wiz <wiz>
2009-03-06 14:28:44 +0000

    Add some patterns for PLIST handling.

--- x11/gtk-sharp/buildlink3.mk
+++ x11/gtk-sharp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2008/04/27 09:21:27 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/03/06 14:28:44 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GTK_SHARP_BUILDLINK3_MK:=	${GTK_SHARP_BUILDLINK3_MK}+
@@ -15,5 +15,12 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gtk-sharp
 BUILDLINK_API_DEPENDS.gtk-sharp+=	gtk-sharp>=2.12.1
 BUILDLINK_PKGSRCDIR.gtk-sharp?=		../../x11/gtk-sharp
 .endif	# GTK_SHARP_BUILDLINK3_MK
+
+PRINT_PLIST_AWK+=	/^@dirrm share\/gapi-2.0$$/ \
+				{ print "@comment in gtk-sharp: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/mono\/gtk-sharp-2.0$$/ \
+				{ print "@comment in gtk-sharp: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/gtk-sharp-2.0$$/ \
+				{ print "@comment in gtk-sharp: " $$0; next; }
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
