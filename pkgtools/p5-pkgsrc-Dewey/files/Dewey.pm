be5647dfbc67ed65a93a4690352b3c5ca4b2d788
wiz <wiz@pkgsrc.org>
2009-03-07 01:44:50 +0000

    Fix documentation -- dewey_cmp and dewey_match texts were swapped. Noted and patch provided by Mihai Chelaru.
    Bump version to 1.1.

--- pkgtools/p5-pkgsrc-Dewey/files/Dewey.pm
+++ pkgtools/p5-pkgsrc-Dewey/files/Dewey.pm
@@ -10,7 +10,7 @@ our @ISA = qw(Exporter);
 
 our @EXPORT = qw(dewey_cmp dewey_match);
 
-our $VERSION = '1.0';
+our $VERSION = '1.1';
 
 require XSLoader;
 XSLoader::load('pkgsrc::Dewey', $VERSION);
@@ -40,16 +40,16 @@ numbers.
 It provides two functions:
 C<dewey_cmp()> and C<dewey_match()>.
 
-C<dewey_cmp(pattern, package)>
+C<dewey_cmp(lhs, op, rhs)>
+compares two pkgsrc Dewey version numbers. Both C<lhs> and
+C<rhs> should be version numbers (not containing any package names).
+If the operator C<op> is invalid, C<dewey_cmp> returns -1.
+Otherwise it returns 1 if the comparison is true and 0 if not.
+
+C<dewey_match(pattern, package)>
 compares the C<pattern> against the C<package>, and returns 1 if the
 C<package> matches the C<pattern> and 0 if not. Both the C<pattern>
 and the C<package> should contain a package name.
-
-C<dewey_match(lhs, op, rhs)>
-compares two pkgsrc Dewey version numbers. Both C<lhs> and
-C<rhs> should be version numbers (not containing any package names).
-If the operator C<op> is invalid, C<dewey_match> returns -1.
-Otherwise it returns 1 if the comparison is true and 0 if not.
 
 =head2 EXPORT
 
