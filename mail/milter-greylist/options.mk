bd673999139fd64c29f498f31e66610ef3248a1e
tron <tron@pkgsrc.org>
2009-03-08 15:54:26 +0000

    Add two new options "postfix-milter" and "sendmail-milter". One of the two must be used to determine whether this package is built for use with Sendmail (the default) or Postfix. Use an appropriate user (either "smmsp" or "postfix") in the two cases. This avoid permission problems between Postfix and milter-greylist.
    Bump package revision because of these changes.

--- mail/milter-greylist/options.mk
+++ mail/milter-greylist/options.mk
@@ -1,8 +1,10 @@
-# $NetBSD: options.mk,v 1.5 2008/10/27 19:40:11 tron Exp $
+# $NetBSD: options.mk,v 1.6 2009/03/08 15:54:26 tron Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.milter-greylist
+PKG_OPTIONS_REQUIRED_GROUPS=	mta
+PKG_OPTIONS_GROUP.mta=		postfix-milter sendmail-milter
 PKG_SUPPORTED_OPTIONS=		dnsrbl drac spf
-PKG_SUGGESTED_OPTIONS=		dnsrbl spf
+PKG_SUGGESTED_OPTIONS=		dnsrbl sendmail-milter spf
 
 .include "../../mk/bsd.options.mk"
 
@@ -22,6 +24,34 @@ CONFIGURE_ARGS+=	--disable-drac
 DRACD_DB?=		/etc/mail/dracd.db
 
 CONFIGURE_ARGS+=	--enable-drac --with-drac-db=${DRACD_DB}
+.endif
+
+###
+### Postfix support
+###
+.if !empty(PKG_OPTIONS:Mpostfix-milter)
+CONFIGURE_ARGS+=	--enable-postfix
+
+POSTFIX_QUEUE_DIR?=	${VARBASE}/spool/postfix
+BUILD_DEFS+=		VARBASE
+
+PKG_GROUPS?=		postfix
+PKG_USERS?=		postfix:postfix
+PKG_GECOS.postfix=	Postfix User
+PKG_HOME.postfix=	${POSTFIX_QUEUE_DIR}
+
+MILTER_USER=		postfix
+.endif
+
+###
+### Sendmail support
+###
+.if !empty(PKG_OPTIONS:Msendmail-milter)
+PKG_GROUPS=		smmsp
+PKG_USERS=		smmsp:smmsp
+PKG_GECOS.smmsp=	Sendmail Message Submission Program
+
+MILTER_USER=		smmsp
 .endif
 
 ###
