970aa917f414e7ef9ef364570dce7a65394b60fd
joerg <joerg@pkgsrc.org>
2009-03-09 19:58:45 +0000

    pkg_install-20090309: Fix a double free. Reported by seb.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.118 2009/03/08 14:50:37 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.119 2009/03/09 19:58:45 joerg Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20090307"
+#define PKGTOOLS_VERSION "20090309"
 
 #endif /* _INST_LIB_VERSION_H_ */
