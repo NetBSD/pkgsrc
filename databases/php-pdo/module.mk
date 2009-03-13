6bab605f085d77efb4fd8112e6312185c451fb59
sborrill <sborrill@pkgsrc.org>
2009-03-13 10:57:20 +0000

    The php_pdo* packages have not kept step with PHP changes. The PDO components are now built into PHP rather than maintained separately in PECL. The old PECL versions are unmaintained and haven't been altered for 3 years. This is confusing as the package versions report themselves to be 5.2.9.1.0.2, etc. but they are in fact based on the 5.1.x code-base.
    Therefore, we need to switch to building the PDO modules like any other
    PHP module.
    
    A side-effect of this is that the PECL version number will get stripped
    off the end meaning that the new version numbers are just 5.2.9 instead
    of 5.2.9.1.0.2 (for example). Unfortunately, 5.2.9 is considered to be
    older than 5.2.9.1.0.2 meaning that it will appear as though a downgrade
    has taken place. Therefore, while we are still using PHP 5.2.9, add .99 on
    the end (i.e. 5.2.9.99) so that the package appears newer. When PHP 5.2.10
    is released this can be removed (the module.mk file explicitly cgecks for
    5.2.9 before adding the .99 suffix so that it will not alter 5.2.10 if the
    hack is forgotten. However, for the record, when 5.2.10 is released,
    php-pdo/module.mk can be removed along with the references to it in each
    php-pdo*/Makefile

--- databases/php-pdo/module.mk
+++ databases/php-pdo/module.mk
@@ -1,28 +1,7 @@
-# $NetBSD: module.mk,v 1.7 2006/06/02 18:27:55 joerg Exp $
+# $NetBSD: module.mk,v 1.8 2009/03/13 10:57:20 sborrill Exp $
 
-# Items common for all PDO drivers
-
-.if !empty(PDO_DRIVER)
-_PDO_SEP=		_
-.else
-_PDO_SEP=		# empty
+# This file is now only needed to patch up PKGNAME until PHP 5.2.10 or later is
+# released. 
+.if "${PHP_BASE_VERS}" == "5.2.9"
+PKGNAME=		${PHP_PKG_PREFIX}-${MODNAME}-${PHP_BASE_VERS}.99
 .endif
-
-PDO_DRIVER_TR=		${ECHO} ${PDO_DRIVER:Q} | ${TR} a-z A-Z
-MODNAME=		PDO${_PDO_SEP}${PDO_DRIVER_TR:sh}
-PKGMODNAME=		pdo${_PDO_SEP}${PDO_DRIVER}
-PECL_VERSION?=		${PDO_VERSION}
-PKGNAME=		${PHP_PKG_PREFIX}-${PKGMODNAME}-${PHP_BASE_VERS}.${PECL_VERSION}
-
-CONFLICTS=		php-pdo${_PDO_SEP}${PDO_DRIVER}-[0-9]*
-
-CATEGORIES+=		databases
-MAINTAINER=		jdolecek@NetBSD.org
-COMMENT=		PHP PDO extension for ${PDO_COMMENT} databases
-
-# needed due to (broken?) PDO_* configure checks
-CONFIGURE_ENV+=		PHP_PDO_SHARED="1"
-
-PHP_VERSIONS_ACCEPTED=	5
-
-.include "../../lang/php/phpversion.mk"
