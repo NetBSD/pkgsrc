1e2485b6503f75224b91a53eaad81628c994f31b
abs <abs@pkgsrc.org>
2009-03-14 12:06:49 +0000

    make opengl support optional, and default to off on very slow archs (m68k,m68000,vax). bump PKGREVISION

---
+++ x11/fltk2/options.mk
@@ -0,0 +1,20 @@
+# $NetBSD: options.mk,v 1.1 2009/03/14 12:06:49 abs Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.fltk2
+PKG_SUPPORTED_OPTIONS=	opengl
+PKG_SUGGESTED_OPTIONS=
+
+# Default
+.if ${MACHINE_ARCH} != "m68k" && && ${MACHINE_ARCH} != "m68000" && \
+	${MACHINE_ARCH} != "vax"
+PKG_SUGGESTED_OPTIONS+=	opengl
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgl)
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-gl
+.endif
