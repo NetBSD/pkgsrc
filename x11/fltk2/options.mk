32d912947b05c5a9034903f794122e7a303f37f2
abs <abs@pkgsrc.org>
2009-03-14 12:10:21 +0000

    fix typo

--- x11/fltk2/options.mk
+++ x11/fltk2/options.mk
@@ -1,11 +1,11 @@
-# $NetBSD: options.mk,v 1.1 2009/03/14 12:06:49 abs Exp $
+# $NetBSD: options.mk,v 1.2 2009/03/14 12:10:21 abs Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fltk2
 PKG_SUPPORTED_OPTIONS=	opengl
 PKG_SUGGESTED_OPTIONS=
 
 # Default
-.if ${MACHINE_ARCH} != "m68k" && && ${MACHINE_ARCH} != "m68000" && \
+.if ${MACHINE_ARCH} != "m68k" && ${MACHINE_ARCH} != "m68000" && \
 	${MACHINE_ARCH} != "vax"
 PKG_SUGGESTED_OPTIONS+=	opengl
 .endif
