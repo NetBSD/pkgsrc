be6978e88c3f196da76f989fa2acfbce4f352bd6
tron <tron@pkgsrc.org>
2009-03-15 19:21:22 +0000

    Pullup ticket #2721 - requested by adrianp php5: security update
    Revisions pulled up:
    - lang/php5/MESSAGE.suhosin			1.1 via patch
    - lang/php5/Makefile				1.71 via patch
    - lang/php5/Makefile.common			1.35
    - lang/php5/Makefile.php			1.33-1.34
    - lang/php5/PLIST				1.21
    - lang/php5/distinfo				1.61-1.62
    - lang/php5/patches/patch-an			patch
    - lang/php5/patches/patch-ar			patch
    - lang/php5/patches/patch-as			delete
    ---
    Module Name:	pkgsrc
    Committed By:	adrianp
    Date:		Mon Mar  2 22:52:17 UTC 2009
    
    Modified Files:
    	pkgsrc/lang/php5: Makefile Makefile.common Makefile.php PLIST distinfo
    Removed Files:
    	pkgsrc/lang/php5/patches: patch-as
    
    Log Message:
    The PHP development team would like to announce the immediate availability of PHP 5.2.9. This release focuses on improving the stability of the PHP 5.2.x branch with over 50 bug fixes, several of which are security related. All users of PHP are encouraged to upgrade to this release.
    
    Security Enhancements and Fixes in PHP 5.2.9:
    
        * Fixed security issue in imagerotate(), background colour isn't validated correctly with a non truecolour image. Reported by Hamid Ebadi, APA Laboratory (Fixes CVE-2008-5498). (Scott)
        * Fixed a crash on extract in zip when files or directories entry names contain a relative path. (Pierre)
        * Fixed explode() behavior with empty string to respect negative limit. (Shire)
        * Fixed a segfault when malformed string is passed to json_decode(). (Scott)
    
    Key enhancements in PHP 5.2.9 include:
    
        * Added optional sorting type flag parameter to array_unique(). Default is SORT_REGULAR. (Andrei)
        * Fixed bug #45996 (libxml2 2.7 causes breakage with character data in xml_parse()). (Rob)
        * A number of fixes in the mbstring extension (Moriyoshi)
        * Fixed bug #44336 (Improve pcre UTF-8 string matching performance). (frode at coretrek dot com, Nuno)
        * Fixed bug #46699 (xml_parse crash when parser is namespace aware). (Rob)
        * Fixed bug #46748 (Segfault when an SSL error has more than one error). (Scott)
        * Fixed bug #46889 (Memory leak in strtotime()). (Derick)
        * Fixed bug #47049 (SoapClient::__soapCall causes a segmentation fault). (Dmitry)
        * Fixed bug #47165 (Possible memory corruption when passing return value by reference). (Dmitry)
        * Fixed bug #47282 (FILTER_VALIDATE_EMAIL is marking valid email addresses as invalid). (Ilia)
        * Fixed bug #47422 (modulus operator returns incorrect results on 64 bit linux). (Matt)
        * Over 50 bug fixes.
    ---
    Module Name:	pkgsrc
    Committed By:	adrianp
    Date:		Thu Mar  5 23:22:24 UTC 2009
    
    Modified Files:
    	pkgsrc/lang/php5: Makefile.php distinfo
    
    Log Message:
    Add back suhosin patch as a new one for 5.2.9 is out

--- lang/php5/Makefile.php
+++ lang/php5/Makefile.php
@@ -1,4 +1,4 @@
-# $NetBSD: Makefile.php,v 1.29 2008/08/17 18:36:50 adrianp Exp $
+# $NetBSD: Makefile.php,v 1.29.6.1 2009/03/15 19:21:22 tron Exp $
 #
 
 .include "../../lang/php5/Makefile.common"
@@ -10,6 +10,7 @@ USE_LIBTOOL=		YES
 USE_LANGUAGES=		c c++
 GNU_CONFIGURE=		YES
 BUILD_DEFS+=		VARBASE
+PLIST_VARS+=		suhosin
 
 CONFIGURE_ENV+=		EXTENSION_DIR="${PREFIX}/${PHP_EXTENSION_DIR}"
 
@@ -41,7 +42,7 @@ CONFIGURE_ARGS+=	--with-libxml-dir=${PREFIX:Q}
 # Note: This expression is the same as ${PKGBASE}, but the latter is
 # not defined yet, so we cannot use it here.
 PKG_OPTIONS_VAR=	PKG_OPTIONS.${PKGNAME:C/-[0-9].*//}
-PKG_SUPPORTED_OPTIONS+=	inet6 ssl maintainer-zts
+PKG_SUPPORTED_OPTIONS+=	inet6 ssl maintainer-zts suhosin
 PKG_SUGGESTED_OPTIONS+=	ssl
 
 SUBST_CLASSES+=		ini
@@ -53,6 +54,23 @@ SUBST_MESSAGE.ini=	Fixing default ini files.
 
 .include "../../mk/bsd.options.mk"
 
+.if !empty(PKG_OPTIONS:Msuhosin)
+SUHOSIN_PHPVER=		5.2.9
+. if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS}
+PKG_FAIL_REASON+=	"The suhosin patch is currently not available for"
+PKG_FAIL_REASON+=	"this version of PHP.  You may have to wait until"
+PKG_FAIL_REASON+=	"an updated patch is released or temporarily"
+PKG_FAIL_REASON+=	"build this package without the suhosin option."
+. else
+PATCH_SITES=		http://download.suhosin.org/
+PATCHFILES+=		suhosin-patch-${SUHOSIN_PHPVER}-0.9.7.patch.gz
+PATCH_DIST_STRIP=	-p1
+PLIST.suhosin=		yes
+MESSAGE_SRC=		${.CURDIR}/../../lang/php5/MESSAGE
+MESSAGE_SRC+=		${.CURDIR}/../../lang/php5/MESSAGE.suhosin
+. endif
+.endif
+
 .if !empty(PKG_OPTIONS:Minet6)
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
@@ -61,7 +79,12 @@ CONFIGURE_ARGS+=	--disable-ipv6
 
 .if !empty(PKG_OPTIONS:Mssl)
 .  include "../../security/openssl/buildlink3.mk"
+.  if ${OPSYS} == "SunOS"
+CONFIGURE_ARGS+=	--with-openssl=yes
+LIBS.SunOS+=		-lcrypto
+.  else
 CONFIGURE_ARGS+=	--with-openssl=${BUILDLINK_PREFIX.openssl}
+.  endif
 .else
 CONFIGURE_ARGS+=	--without-openssl
 .endif
