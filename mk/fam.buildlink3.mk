5e463a1c238a0769e4366dfaf67931b0080dc1a5
jmcneill <jmcneill>
2009-03-15 21:47:44 +0000

    Switch to gamin as the default FAM implementation.

--- mk/fam.buildlink3.mk
+++ mk/fam.buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: fam.buildlink3.mk,v 1.5 2006/11/04 22:18:58 rillig Exp $
+# $NetBSD: fam.buildlink3.mk,v 1.6 2009/03/15 21:47:44 jmcneill Exp $
 #
 # This Makefile fragment is meant to be included by packages that
 # require a FAM implementation.  fam.buildlink3.mk will:
@@ -6,7 +6,7 @@
 #	* set FAMBASE to the base directory of the FAM files;
 #	* set FAM_TYPE to the FAM implementation used.
 #
-# There are two variables that can be used to tweak the selection of
+# There are three variables that can be used to tweak the selection of
 # the FAM implementation:
 #
 # FAM_DEFAULT is a user-settable variable whose value is the default
@@ -14,13 +14,16 @@
 #
 # FAM_ACCEPTED is a package-settable list of FAM implementations that
 #	may be used by the package.
+#
+# FAM_SERVER is a package-settable flag whose value determines whether
+#	or not the server and library are both required.
 
 MK_FAM_BUILDLINK3_MK:=	${MK_FAM_BUILDLINK3_MK}+
 
 .include "../../mk/bsd.prefs.mk"
 
 .if !empty(MK_FAM_BUILDLINK3_MK:M+)
-FAM_DEFAULT?=	fam
+FAM_DEFAULT?=	gamin
 FAM_ACCEPTED?=	${_FAM_PKGS}
 
 # This is an exhaustive list of all of the FAM implementations that may
@@ -44,11 +47,16 @@ BUILD_DEFS+=		FAM_DEFAULT
 BUILD_DEFS_EFFECTS+=	FAMBASE FAM_TYPE
 .endif	# MK_FAM_BUILDLINK3_MK
 
+FAM_SERVER?=	yes
+
 .if ${FAM_TYPE} == "none"
 PKG_FAIL_REASON=	\
 	"${_FAM_TYPE} is not an acceptable FAM type for ${PKGNAME}."
 .elif ${FAM_TYPE} == "fam"
 .  include "../../sysutils/fam/buildlink3.mk"
 .elif ${FAM_TYPE} == "gamin"
-.  include "../../sysutils/gamin/buildlink3.mk"
+.  include "../../sysutils/libgamin/buildlink3.mk"
+.  if !empty(FAM_SERVER:M[Yy][Ee][Ss])
+DEPENDS+=	gamin>=0.1.10nb1:../../sysutils/gamin
+.  endif
 .endif
