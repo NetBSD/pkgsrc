b69e79a40336ba114f0fc8b16d81ee54f93137ce
jmcneill <jmcneill@pkgsrc.org>
2009-03-16 13:02:18 +0000

    Needs sqlite3

--- net/libsoup24/buildlink3.mk
+++ net/libsoup24/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/16 12:54:57 jmcneill Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/03/16 13:02:18 jmcneill Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBSOUP24_BUILDLINK3_MK:=	${LIBSOUP24_BUILDLINK3_MK}+
@@ -17,6 +17,7 @@ BUILDLINK_ABI_DEPENDS.libsoup24?=	libsoup24>=2.4.0
 BUILDLINK_PKGSRCDIR.libsoup24?=		../../net/libsoup24
 .endif	# LIBSOUP24_BUILDLINK3_MK
 
+.include "../../databases/sqlite3/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../security/gnutls/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
