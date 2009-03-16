3860069aae4e81072795f4b1a1ab9e2620725a2f
joerg <joerg@pkgsrc.org>
2009-03-16 23:17:14 +0000

    Add glob to USE_FEATURES only once.

--- textproc/libxml2/buildlink3.mk
+++ textproc/libxml2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2007/09/08 21:58:01 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2009/03/16 23:17:14 joerg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBXML2_BUILDLINK3_MK:=	${LIBXML2_BUILDLINK3_MK}+
@@ -17,9 +17,9 @@ BUILDLINK_ABI_DEPENDS.libxml2+=	libxml2>=2.6.23nb1
 BUILDLINK_PKGSRCDIR.libxml2?=	../../textproc/libxml2
 
 BUILDLINK_FILES.libxml2+=	bin/xml2-config
-.endif	# LIBXML2_BUILDLINK3_MK
 
 USE_FEATURES+=		glob
+.endif	# LIBXML2_BUILDLINK3_MK
 
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
