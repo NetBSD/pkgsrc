7d12dc3dc0fc3235cc4ef0e7e080601347c52269
drochner <drochner>
2009-03-17 16:28:36 +0000

    propagate dependencies on libX11/libSM, fixes build breakage in dependent pkgs

--- x11/startup-notification/buildlink3.mk
+++ x11/startup-notification/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/07/08 23:11:16 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2009/03/17 16:28:36 drochner Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 STARTUP_NOTIFICATION_BUILDLINK3_MK:=	${STARTUP_NOTIFICATION_BUILDLINK3_MK}+
@@ -16,5 +16,8 @@ BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5
 BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.8nb1
 BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification
 .endif	# STARTUP_NOTIFICATION_BUILDLINK3_MK
+
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
