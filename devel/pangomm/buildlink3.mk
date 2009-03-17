05fee4d80d4c49dafe84bd63a9bc0caacd9b7980
jmcneill <jmcneill@pkgsrc.org>
2009-03-17 22:10:15 +0000

    Update pangomm to 2.24.0.
    2.24.0:
    (Skipped version numbers since 2.14.1, to match pango 1.24
    (apart from the first digit, which we can't decrease.)
    
    * Layout: Added show_in_cairo_context().
      (Louis-Francis Ratté-Boulianne) Bug #560232.
    * Build: Allow pangomm to build outside the source tree. Bug #570322
      (Theppitak Karoonboonyanan)
    * License Header consistency. (Deng Xiyue)

--- devel/pangomm/buildlink3.mk
+++ devel/pangomm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/10/16 18:01:30 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/03/17 22:10:15 jmcneill Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PANGOMM_BUILDLINK3_MK:=	${PANGOMM_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	pangomm
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}pangomm
 
 .if ${PANGOMM_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.pangomm+=	pangomm>=2.14.0
+BUILDLINK_API_DEPENDS.pangomm+=	pangomm>=2.24.0
 BUILDLINK_PKGSRCDIR.pangomm?=	../../devel/pangomm
 .endif	# PANGOMM_BUILDLINK3_MK
 
