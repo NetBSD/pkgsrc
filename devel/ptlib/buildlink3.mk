c89fe362639edcc1ed7ef83cf4e5544e883d6a72
jmcneill <jmcneill@pkgsrc.org>
2009-03-18 11:57:15 +0000

    Update ptlib to 2.6.1. Lots of changes, no changelog in sight.
    Main features on this new major release branch are:
    
        * Instant Messaging. Using several different ?standards?: RFC4975, MSRP, T.140, SIP-IM etc etc.
        * Much improved support for SIP event handling (SUBSCRIBE/NOTIFY/PUBLISH) using factories so more event packages can be added easily.
        * Support for SIP ?dialog? event package.
        * Support for Shared Line Appearance (aka Multiple Line Appearance, Bridged Line Appearance or Busy Lamp Field)
        * Significant improvements in IPv6 support (thanks Yuri Kiryanov!)
        * Major enhancements to the video rate controller.
        * Removal of the old H.263 ffmpeg plug in and enhancement of the H.263-1998 plug it to take its place.
        * H.224 support (H.323 only).
        * SBC audio codec plug in.
        * G.722 audio codec plug in.
        * CELT audio codec plug in.
        * SWIG support for use in other languages, Java first.
        * Much improved multi-platform sample GUI client, OpenPhone. Now standard test environment replacing simpleOPAL.

--- devel/ptlib/buildlink3.mk
+++ devel/ptlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/01/30 12:46:23 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/03/18 11:57:15 jmcneill Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PTLIB_BUILDLINK3_MK:=	${PTLIB_BUILDLINK3_MK}+
@@ -12,8 +12,8 @@ BUILDLINK_PACKAGES+=	ptlib
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}ptlib
 
 .if !empty(PTLIB_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.ptlib+=	ptlib>=2.4.2
-BUILDLINK_ABI_DEPENDS.ptlib?=	ptlib>=2.4.2
+BUILDLINK_API_DEPENDS.ptlib+=	ptlib>=2.6.1
+BUILDLINK_ABI_DEPENDS.ptlib?=	ptlib>=2.6.1
 BUILDLINK_PKGSRCDIR.ptlib?=	../../devel/ptlib
 .endif	# PTLIB_BUILDLINK3_MK
 
