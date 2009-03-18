adb99953401fbb47da4673857ec2e68ad6599082
roy <roy>
2009-03-18 21:58:26 +0000

    We need pkg-config to build against DBus.

--- net/dnsmasq/options.mk
+++ net/dnsmasq/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2008/12/19 07:53:43 roy Exp $
+# $NetBSD: options.mk,v 1.2 2009/03/18 21:58:26 roy Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dnsmasq
 
@@ -7,6 +7,7 @@ PKG_SUPPORTED_OPTIONS=	dbus inet6
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdbus)
+USE_TOOLS+=	pkg-config
 CFLAGS+=	-DHAVE_DBUS
 .include "../../sysutils/dbus/buildlink3.mk"
 .endif
