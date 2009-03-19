acf2860e01d70b2f535899f0efff0a74f8216b05
cube <cube@pkgsrc.org>
2009-03-19 16:20:52 +0000

    Don't use + in variable names.  It's asking for troubles.

--- x11/gtk+extra/buildlink3.mk
+++ x11/gtk+extra/buildlink3.mk
@@ -1,7 +1,7 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/07/08 23:11:14 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2009/03/19 16:20:52 cube Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-GTK+EXTRA_BUILDLINK3_MK:=	${GTK+EXTRA_BUILDLINK3_MK}+
+GTK_EXTRA_BUILDLINK3_MK:=	${GTK+EXTRA_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	gtk+extra
@@ -11,11 +11,11 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngtk+extra}
 BUILDLINK_PACKAGES+=	gtk+extra
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gtk+extra
 
-.if !empty(GTK+EXTRA_BUILDLINK3_MK:M+)
+.if !empty(GTK_EXTRA_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.gtk+extra+=	gtk+extra>=0.99.17nb2
 BUILDLINK_ABI_DEPENDS.gtk+extra?=	gtk+extra>=0.99.17nb5
 BUILDLINK_PKGSRCDIR.gtk+extra?=	../../x11/gtk+extra
-.endif	# GTK+EXTRA_BUILDLINK3_MK
+.endif	# GTK_EXTRA_BUILDLINK3_MK
 
 .include "../../x11/gtk/buildlink3.mk"
 
