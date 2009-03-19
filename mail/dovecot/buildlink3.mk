536a720b00b876efd4d369011685fc2936fd2ed4
joerg <joerg@pkgsrc.org>
2009-03-19 15:50:27 +0000

    Add comment about include guard.

--- mail/dovecot/buildlink3.mk
+++ mail/dovecot/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2008/06/22 17:41:23 ghen Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/03/19 15:50:27 joerg Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 DOVECOT_BUILDLINK3_MK:=			${DOVECOT_BUILDLINK3_MK}+
@@ -16,7 +16,7 @@ BUILDLINK_ORDER:=			${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}dovecot
 .if !empty(DOVECOT_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.dovecot+=		dovecot>=1.1beta16
 BUILDLINK_PKGSRCDIR.dovecot?=		../../mail/dovecot
-.endif
+.endif # DOVECOT_BUILDLINK3_MK
 
 pkgbase := dovecot
 .include "../../mk/pkg-build-options.mk"
