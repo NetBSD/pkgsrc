fb4440096ddd46cfe165f505b87fdf9591ddf1e3
cube <cube@pkgsrc.org>
2009-03-19 17:05:51 +0000

    Decorate .endif with the name of the variable the .if tested.

--- devel/boost-python/buildlink3.mk
+++ devel/boost-python/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2009/03/03 08:57:58 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2009/03/19 17:05:51 cube Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 BOOST_PYTHON_BUILDLINK3_MK:=	${BOOST_PYTHON_BUILDLINK3_MK}+
@@ -16,7 +16,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}boost-python
 BUILDLINK_API_DEPENDS.boost-python+=	boost-python-1.38.*
 BUILDLINK_ABI_DEPENDS.boost-python?=	boost-python>=1.38.*
 BUILDLINK_PKGSRCDIR.boost-python?=	../../devel/boost-python
-.endif
+.endif	# BOOST_PYTHON_BUILDLINK3_MK
 
 .include "../../devel/boost-headers/buildlink3.mk"
 
