adb24efaeba8829e384676b5f5c7a7970bf9de53
joerg <joerg@pkgsrc.org>
2009-03-20 16:52:40 +0000

    Skip vulnerability check for packages that set NO_PKGTOOLS_REQD_CHECK. Set AUDIT_PACKAGES and friends correctly though. Fixes bootstrap on platforms with pdksh.

--- mk/flavor/pkg/check.mk
+++ mk/flavor/pkg/check.mk
@@ -1,10 +1,14 @@
-# $NetBSD: check.mk,v 1.11 2009/03/17 22:13:36 rillig Exp $
+# $NetBSD: check.mk,v 1.12 2009/03/20 16:52:40 joerg Exp $
 #
 
 # _flavor-check-vulnerable:
 #	Checks for known vulnerabilities in the package if a vulnerability
 #	file exists.
 #
+.if defined(NO_PKGTOOLS_REQD_CHECK)
+_flavor-check-vulnerable: .PHONY
+	${RUN}${DO_NADA}
+.else
 _flavor-check-vulnerable: .PHONY
 	${RUN}\
 	_PKGVULNDIR=`${_EXTRACT_PKGVULNDIR}`; \
@@ -18,3 +22,4 @@ _flavor-check-vulnerable: .PHONY
 	${PHASE_MSG} "Checking for vulnerabilities in ${PKGNAME}"; \
 	${AUDIT_PACKAGES} ${_AUDIT_PACKAGES_CMD} ${AUDIT_PACKAGES_FLAGS} ${PKGNAME} \
 	|| ${FAIL_MSG} "Define ALLOW_VULNERABLE_PACKAGES in mk.conf or IGNORE_URLS in audit-packages.conf(5) if this package is absolutely essential."
+.endif
