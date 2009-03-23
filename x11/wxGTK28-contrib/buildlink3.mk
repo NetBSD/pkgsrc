00a994a05e3f1547995f0e0890af16ffa1832f0e
joerg <joerg@pkgsrc.org>
2009-03-23 00:38:55 +0000

    Update to wxGTK-2.8.10: - bugfixes
    For pkgsrc:
    - fix installation of wxrc
    - build both Unicode and non-Unicode version, they are not API and ABI
      compatible and different programs either

--- x11/wxGTK28-contrib/buildlink3.mk
+++ x11/wxGTK28-contrib/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:52 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/03/23 00:38:56 joerg Exp $
 
 BUILDLINK_TREE+=	wxGTK28-contrib
 
 .if !defined(WXGTK28_CONTRIB_BUILDLINK3_MK)
 WXGTK28_CONTRIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.wxGTK28-contrib+=	wxGTK28-contrib>=2.8.8
+BUILDLINK_API_DEPENDS.wxGTK28-contrib+=	wxGTK28-contrib>=2.8.10
 BUILDLINK_PKGSRCDIR.wxGTK28-contrib?=	../../x11/wxGTK28-contrib
 
 .include "../../x11/wxGTK28/buildlink3.mk"
