ffa22693d1c0f5806e066ecf1c8d9826043b19ba
he <he@pkgsrc.org>
2009-03-25 23:01:48 +0000

    Update from version 0.4.11nb1 to 1.0.0.
    Pkgsrc changes:
     o New MASTER_SITES
     o Take maintainership (apologies if I'm stepping on someone's toes)
     o The package now has built-in knowledge if the CPU arch has working
       jit support (currently i386, powerpc and arm, of which the two first
       have been tested), so there's no need to direct the jit support from
       the main Makefile
     o Update PLIST
     o Added patches to port to NetBSD/powerpc (reported upstream)
     o Added patch to remove a GNUism from one of the shell scripts
       (test using == in place of =)
    
    
    Upstream changes:
     o Too many to mention; there have been some 20-odd number of
       intervening releases since the 0.4.11 release of 2007-Apr-17,
       according to
       http://docs.parrot.org/parrot/latest/html/docs/parrothist.pod.html
       which contains the release history.
    
    
    OK'ed by wiz@.

--- lang/parrot/buildlink3.mk
+++ lang/parrot/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:24:51 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2009/03/25 23:01:48 he Exp $
 
 BUILDLINK_TREE+=	parrot
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	parrot
 PARROT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.parrot+=		parrot>=0.4.0
-BUILDLINK_ABI_DEPENDS.parrot?=		parrot>=0.4.1nb1
+BUILDLINK_ABI_DEPENDS.parrot?=		parrot>=1.0.0
 BUILDLINK_PKGSRCDIR.parrot?=		../../lang/parrot
 
 .include "../../textproc/icu/buildlink3.mk"
