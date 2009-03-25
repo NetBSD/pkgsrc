34239d0519768a287d5bd59b489764e0aae7a0b7
he <he@pkgsrc.org>
2009-03-25 22:37:12 +0000

    Bump PKGREVISION following the update of textproc/icu, and the corresponding change of ABI.
    OK'ed by wiz@ (as was the textproc/icu update).

--- x11/gnome-desktop-sharp/buildlink3.mk
+++ x11/gnome-desktop-sharp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:25:41 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2009/03/25 22:37:15 he Exp $
 
 BUILDLINK_TREE+=	gnome-desktop-sharp
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	gnome-desktop-sharp
 GNOME_DESKTOP_SHARP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gnome-desktop-sharp+=	gnome-desktop-sharp>=2.24.0
+BUILDLINK_ABI_DEPENDS.gnome-desktop-sharp?=	gnome-desktop-sharp>=2.26.0nb1
 BUILDLINK_PKGSRCDIR.gnome-desktop-sharp?=	../../x11/gnome-desktop-sharp
 
 .include "../../devel/libwnck/buildlink3.mk"
