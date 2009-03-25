e4c7a559ee0c58f8232551220615f61626d8e758
he <he@pkgsrc.org>
2009-03-25 22:30:19 +0000

    Update from version 3.6nb2 to 4.0.1.
    Pkgsrc changes:
     o New MASTER_SITE
     o Adjust PLIST
     o Remove no-longer-needed patches, since corresponding changes
       have been adopted upstream
     o BUILDLINK_ABI_DEPENDS bumped to >=4.0, since a new shared library
       version is installed
     o Fixes security vulnerability, ref. below.
    
    Dependent pkgsrc packages will have their revisions bumped shortly
    due to the (possibly/probably) changed ABI.
    
    
    Upstream changes:
    4.0.1:
      ICU4C 4.0.1 is a maintenance release of ICU4J 4.0. The primary
      changes of this release were:
    
        * Updated time zone data to 2008i
        * Technical preview of string search implementation using
          Boyer-Moore algorithm (#6286). For detail information, please
          see the tech note here.
        * #5691 Conversion: consistent illegal sequences
        * #6435 Bad @stable ICU4.0 tags
        * #6597 TestDisplayNamesMeta failure
        * #6670 Test failure in format/TimeZoneTest/TestShortZoneIDs
    
    4.0:
      Major changes in ICU 4.0 include the following:
    
        * Common Changes
    	  o Unicode 5.1 (#5696)
    	  o Locale Data: ICU uses and supports data from Common
    	    Locale Data Repository (CLDR) 1.6 , which includes many
    	    improvements in quality and quantity of data.
    	  o add/removeLikelySubtags (#6124)
    	  o Charset converter file size improvement (#5987)
    	  o Date Interval Formatting (#6157) Note: Calendar type
    	    supported by this feature is Gregorian only in this
    	    release.
    	  o Improved Plural support
        * ICU4C Specific Changes
    	    Additional Calendars
    		+ Chinese (#4081)
    		+ Coptic/Ethiopic (#4571)
        * ICU4J Specific Changes
    	  o Charset
    		+ Graduated from Technology Preview status
    		+ ICU2022 Converter (#5791)
    		+ HZ Converter (#6128)
    		+ SCSU/BOCU-1 Converter (#2147)
    		+ Charset Converter Callback (#6144)
    	  o Thai Dictionary break iterator (#5385)
    	  o JDK TimeZone support (#5975)
    	  o Locale Service Provider (#5976)
    	  o More convenient formatting of year+month, day+month,
    	    and other combinations (#6304)
    	  o Simple Duration Formatting (#6303)
        * ICU4C Security Fixes
          ICU4C 4.0 resolves the vulnerabilities CVE-2007-4770 and
          CVE-2007-4771 which were found in earlier versions of ICU.
          The standard ICU tests verify that these have been corrected,
          however, the updated versions of the previous tests may be
          run by applying the following patch to ICU 4.0: r24324. As
          well, ICU4C and ICU4J 4.0 resolve the issue underlying
          CVE-2008-1036.

--- textproc/icu/buildlink3.mk
+++ textproc/icu/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2009/03/20 19:25:29 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2009/03/25 22:30:19 he Exp $
 
 BUILDLINK_TREE+=	icu
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	icu
 ICU_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.icu+=	icu>=3.4
-BUILDLINK_ABI_DEPENDS.icu?=	icu>=3.4nb1
+BUILDLINK_ABI_DEPENDS.icu?=	icu>=4.0
 BUILDLINK_PKGSRCDIR.icu?=	../../textproc/icu
 .endif # ICU_BUILDLINK3_MK
 
