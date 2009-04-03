11122f85a0139c84821e79247759eca0f4015e12
drochner <drochner>
2009-04-03 12:28:26 +0000

    update for new PKGNAME

--- multimedia/py-gstreamer0.10/buildlink3.mk
+++ multimedia/py-gstreamer0.10/buildlink3.mk
@@ -1,11 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:05 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/04/03 12:28:26 drochner Exp $
 
 BUILDLINK_TREE+=	gst-python
 
 .if !defined(GST_PYTHON_BUILDLINK3_MK)
 GST_PYTHON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gst-python+=	gst-python>=0.10.5
+.include "../../lang/python/pyversion.mk"
+
+BUILDLINK_API_DEPENDS.gst-python+=	${PYPKGPREFIX}-gstreamer0.10>=0.10.5
 BUILDLINK_PKGSRCDIR.gst-python?=	../../multimedia/py-gstreamer0.10
 .endif # GST_PYTHON_BUILDLINK3_MK
 
