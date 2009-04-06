b3efec9199df5d75d63a164b7cf1a932a632a337
markd <markd@pkgsrc.org>
2009-04-06 12:54:06 +0000

    Update neon to 0.28.3
    Remove comment about checking subversion for neon > 0.27 as 0.28.3 is
    in fact the prefered version for the current subversion.
    
    Several years of bug fixes.

--- www/neon/buildlink3.mk
+++ www/neon/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2009/03/20 19:25:37 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2009/04/06 12:54:06 markd Exp $
 
 BUILDLINK_TREE+=	neon
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	neon
 NEON_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.neon+=	neon>=0.24.4
-BUILDLINK_ABI_DEPENDS.neon+=	neon>=0.26.3nb2
+BUILDLINK_ABI_DEPENDS.neon+=	neon>=0.28.3
 BUILDLINK_PKGSRCDIR.neon?=	../../www/neon
 
 .include "../../devel/zlib/buildlink3.mk"
