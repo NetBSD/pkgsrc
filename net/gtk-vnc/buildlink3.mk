5102e74ad97af70a65b334aaf2e140db22df3f3b
wiz <wiz@pkgsrc.org>
2009-04-06 10:17:32 +0000

    Initial import of gtk-vnc-0.3.8nb1 from pkgsrc-wip: gtk-vnc is a VNC viewer widget for GTK. It is built using coroutines allowing it to be completely asynchronous while remaining single threaded. It provides a core C library, and bindings for Python (PyGTK).

---
+++ net/gtk-vnc/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/04/06 10:17:32 wiz Exp $
+
+BUILDLINK_TREE+=	gtk-vnc
+
+.if !defined(GTK_VNC_BUILDLINK3_MK)
+GTK_VNC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gtk-vnc+=	gtk-vnc>=0.3.7
+BUILDLINK_PKGSRCDIR.gtk-vnc?=	../../wip/gtk-vnc
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../graphics/gtkglext/buildlink3.mk"
+.include "../../security/gnutls/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif # GTK_VNC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gtk-vnc
