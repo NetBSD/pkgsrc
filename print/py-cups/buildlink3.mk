b2464ea098725a1e633bade3d85f8d3022d3f895
drochner <drochner@pkgsrc.org>
2009-04-07 11:10:00 +0000

    include "pyversion" as all these python extensions do

--- print/py-cups/buildlink3.mk
+++ print/py-cups/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:16 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/04/07 11:10:00 drochner Exp $
 
 BUILDLINK_TREE+=	py-cups
 
 .if !defined(PY_CUPS_BUILDLINK3_MK)
 PY_CUPS_BUILDLINK3_MK:=
+
+.include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.py-cups+=	${PYPKGPREFIX}-cups>=1.9.44
 BUILDLINK_PKGSRCDIR.py-cups?=	../../print/py-cups
