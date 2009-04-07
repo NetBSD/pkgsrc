f37cc581542bfd46689f217ad2d6260a1e1f65a3
dsainty <dsainty@pkgsrc.org>
2009-04-07 03:14:07 +0000

    Pull in lang/python/extension.mk when buildlinking.
    Specifically, this fixes the build of dependent packages (like net/libsoup24)
    under Linux, which attempt (and fail) to link in -ldb4 as requested by
    ${PREFIX}/lib/libproxy.la.

--- www/libproxy/buildlink3.mk
+++ www/libproxy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:37 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/04/07 03:14:07 dsainty Exp $
 
 BUILDLINK_TREE+=	libproxy
 
@@ -7,6 +7,9 @@ LIBPROXY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libproxy+=	libproxy>=0.2.3
 BUILDLINK_PKGSRCDIR.libproxy?=	../../www/libproxy
+
+.include "../../lang/python/extension.mk"
+
 .endif # LIBPROXY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libproxy
