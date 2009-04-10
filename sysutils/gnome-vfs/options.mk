570c708f828a380ca28eda89092032630c5bd6bf
asau <asau@pkgsrc.org>
2009-04-10 22:27:16 +0000

    Make FAM support optional (enabled by default). Per request in PR pkg/41025

--- sysutils/gnome-vfs/options.mk
+++ sysutils/gnome-vfs/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.1 2008/12/01 14:41:52 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2009/04/10 22:27:16 asau Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnome-vfs
 PKG_SUPPORTED_OPTIONS=	gssapi hal inet6
-PKG_SUGGESTED_OPTIONS=	hal
+PKG_SUGGESTED_OPTIONS=	fam hal
 
 .include "../../mk/bsd.prefs.mk"
 
@@ -13,6 +13,13 @@ PKG_SUGGESTED_OPTIONS+= gssapi
 .endif
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mfam)
+.include "../../mk/fam.buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-fam
+.else
+CONFIGURE_ARGS+=	--disable-fam
+.endif
 
 .if !empty(PKG_OPTIONS:Mgssapi)
 .include "../../mk/krb5.buildlink3.mk"
