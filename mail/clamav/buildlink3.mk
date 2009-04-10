0831905bc04394f46983a58c45a37c900b77cbbf
tron <tron@pkgsrc.org>
2009-04-10 08:29:37 +0000

    Pullup ticket #2730 - requested by martti clamav: bug fix update
    Revisions pulled up:
    - mail/clamav/Makefile			1.91
    - mail/clamav/buildlink3.mk		1.21
    - mail/clamav/distinfo			1.58
    - mail/clamav/patches/patch-ad		1.18
    - mail/clamav/patches/patch-af		1.9
    ---
    Module Name:	pkgsrc
    Committed By:	martti
    Date:		Thu Apr  9 08:00:10 UTC 2009
    
    Modified Files:
    	pkgsrc/mail/clamav: Makefile distinfo
    	pkgsrc/mail/clamav/patches: patch-ad patch-af
    
    Log Message:
    Updated mail/clamav to 0.95.1
    
    Lots of bug fixes, see http://svn.clamav.net/svn/clamav-devel/trunk/ChangeLog
    for details.
    ---
    Module Name:	pkgsrc
    Committed By:	martti
    Date:		Thu Apr  9 08:03:44 UTC 2009
    
    Modified Files:
    	pkgsrc/mail/clamav: buildlink3.mk
    
    Log Message:
    Regenerated.

--- mail/clamav/buildlink3.mk
+++ mail/clamav/buildlink3.mk
@@ -1,17 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.20 2009/03/25 09:21:20 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.20.2.1 2009/04/10 08:29:37 tron Exp $
 
 BUILDLINK_TREE+=	clamav
 
 .if !defined(CLAMAV_BUILDLINK3_MK)
 CLAMAV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clamav+=	clamav>=0.94.2
+BUILDLINK_API_DEPENDS.clamav+=	clamav>=0.95.1
 BUILDLINK_PKGSRCDIR.clamav?=	../../mail/clamav
 
 .include "../../archivers/bzip2/buildlink3.mk"
 .include "../../devel/libltdl/buildlink3.mk"
+.include "../../devel/ncurses/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../devel/gmp/buildlink3.mk"
-.endif # CLAMAV_BUILDLINK3_MK
+.endif	# CLAMAV_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-clamav
