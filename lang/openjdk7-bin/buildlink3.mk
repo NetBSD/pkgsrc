7e6166d407ad839535d2b54647c2b2e938919473
abs <abs>
2009-04-10 09:09:37 +0000

    Add a buildlink file for the java vm framework

---
+++ lang/openjdk7-bin/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2009/04/10 09:09:37 abs Exp $
+
+BUILDLINK_TREE+=	openjdk7-bin
+
+.if !defined(OPENJDK7_BIN_BUILDLINK3_MK)
+OPENJDK7_BIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.openjdk7-bin+=	openjdk7-bin-[0-9]*
+BUILDLINK_ABI_DEPENDS.openjdk7-bin?=	openjdk7-bin>=6.0.4
+BUILDLINK_PKGSRCDIR.openjdk7-bin?=	../../lang/openjdk7-bin
+BUILDLINK_JAVA_PREFIX.openjdk7-bin=	${PREFIX}/java/openjdk7-bin
+
+UNLIMIT_RESOURCES+=	datasize	# Must be at least 131204
+.endif # OPENJDK7_BIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-openjdk7-bin
