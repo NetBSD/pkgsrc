f1d9f87de3eb2a587043db6841f644016ddf947e
spz <spz>
2009-04-11 17:38:48 +0000

    Pullup ticket 2732 - requested by joerg Bug fix
    Revisions pulled up:
    - pkgsrc/pkgtools/pkg_install/files/add/perform.c	1.85
    - pkgsrc/pkgtools/pkg_install/files/lib/version.h	1.121
    
       Module Name:	pkgsrc
       Committed By:	joerg
       Date:		Mon Apr  6 14:34:15 UTC 2009
    
       Modified Files:
       	pkgsrc/pkgtools/pkg_install/files/add: perform.c
       	pkgsrc/pkgtools/pkg_install/files/lib: version.h
    
       Log Message:
       pkg_install-20090406:
       Remove premature return that broke dependency tracking for pkg_add -u.
       Fixes PR 41143.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.84 -r1.85 pkgsrc/pkgtools/pkg_install/files/add/perform.c
       cvs rdiff -u -r1.120 -r1.121 pkgsrc/pkgtools/pkg_install/files/lib/version.h

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.120 2009/03/26 14:32:39 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.120.2.1 2009/04/11 17:38:49 spz Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION "20090326"
+#define PKGTOOLS_VERSION "20090406"
 
 #endif /* _INST_LIB_VERSION_H_ */
