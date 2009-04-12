ce36fc50c160d91725488f64b6759297df5dfdb8
adam <adam@pkgsrc.org>
2009-04-12 20:46:20 +0000

    Changes 3.0.5: * Fixed a serious file reading bug.
    Changes 3.0.4:
    * Bug-fixing release.

--- math/octave/octave.mk
+++ math/octave/octave.mk
@@ -1,4 +1,4 @@
-# $NetBSD: octave.mk,v 1.7 2009/01/18 02:43:38 obache Exp $
+# $NetBSD: octave.mk,v 1.8 2009/04/12 20:46:20 adam Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
@@ -27,4 +27,3 @@ REPLACE.octave.old=	.*octave
 REPLACE.octave.new=	${LOCALBASE}/bin/octave
 REPLACE_FILES.octave=	${REPLACE_OCTAVE}
 .endif # defined(REPLACE_OCTAVE)
-
