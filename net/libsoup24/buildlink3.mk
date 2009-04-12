e820b7e76350b975bf5bf1ee152174c25b74bc1b
wiz <wiz@pkgsrc.org>
2009-04-12 16:40:00 +0000

    Bump ABI depends because this version of the buildlink3.mk file pulls in more dependencies.

--- net/libsoup24/buildlink3.mk
+++ net/libsoup24/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:25:10 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2009/04/12 16:40:00 wiz Exp $
 
 BUILDLINK_TREE+=	libsoup24
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libsoup24
 LIBSOUP24_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libsoup24+=	libsoup24>=2.4.0
-BUILDLINK_ABI_DEPENDS.libsoup24?=	libsoup24>=2.4.0
+BUILDLINK_ABI_DEPENDS.libsoup24?=	libsoup24>=2.26.0
 BUILDLINK_PKGSRCDIR.libsoup24?=		../../net/libsoup24
 
 .include "../../databases/sqlite3/buildlink3.mk"
