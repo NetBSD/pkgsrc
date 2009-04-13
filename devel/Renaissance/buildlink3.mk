1bba0c5d89789064b1b4dfdd9eef374edcb3a62c
rh <rh@pkgsrc.org>
2009-04-13 12:33:43 +0000

    Update Renaissance to 0.9.0.  Changes are mostly bugfixes and a more hier(7) compliant fhs-style filesystem layout.

--- devel/Renaissance/buildlink3.mk
+++ devel/Renaissance/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:24:08 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2009/04/13 12:33:43 rh Exp $
 
 BUILDLINK_TREE+=	Renaissance
 
 .if !defined(RENAISSANCE_BUILDLINK3_MK)
 RENAISSANCE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.Renaissance+=		Renaissance>=0.7.0
-BUILDLINK_ABI_DEPENDS.Renaissance+=	Renaissance>=0.8.0nb7
+BUILDLINK_API_DEPENDS.Renaissance+=	Renaissance>=0.9.0
+BUILDLINK_ABI_DEPENDS.Renaissance+=	Renaissance>=0.9.0
 BUILDLINK_PKGSRCDIR.Renaissance?=	../../devel/Renaissance
 
 .include "../../x11/gnustep-back/buildlink3.mk"
