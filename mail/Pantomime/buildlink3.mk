22ba5424b03756a11df4b20a20722dbb904fe4b3
rh <rh@pkgsrc.org>
2009-04-13 12:40:40 +0000

    Update Pantomime to 1.2.0pre3.  Changes include some structural overhaul, including a brand new mailbox caching code and other performance improvements.  This also makes Pantomime use the fhs filesystem layout to make it more hier(7) compliant.

--- mail/Pantomime/buildlink3.mk
+++ mail/Pantomime/buildlink3.mk
@@ -1,17 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:24:53 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2009/04/13 12:40:40 rh Exp $
 
 BUILDLINK_TREE+=	Pantomime
 
 .if !defined(PANTOMIME_BUILDLINK3_MK)
 PANTOMIME_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.Pantomime+=	Pantomime>=1.1.2
-BUILDLINK_ABI_DEPENDS.Pantomime+=	Pantomime>=1.1.2nb5
+BUILDLINK_API_DEPENDS.Pantomime+=	Pantomime>1.1.9
+BUILDLINK_ABI_DEPENDS.Pantomime+=	Pantomime>1.1.9
 BUILDLINK_PKGSRCDIR.Pantomime?=	../../mail/Pantomime
 
 .include "../../devel/gnustep-make/gnustep.mk"
 
-BUILDLINK_INCDIRS.Pantomime+=	${GNUSTEP_LOCAL_ROOT:Q}/${GNUSTEP_IDIR:Q}/Pantomime
+BUILDLINK_INCDIRS.Pantomime+=	lib/GNUstep/Frameworks/Pantomime.framework/Headers
+BUILDLINK_FILES.Pantomime+= 	include/Pantomime/*.h
 
 .include "../../devel/gnustep-base/buildlink3.mk"
 .endif # PANTOMIME_BUILDLINK3_MK
