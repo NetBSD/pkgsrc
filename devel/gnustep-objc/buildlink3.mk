59ee2686860b78010b9ff8af5216fa1ee4831901
rh <rh@pkgsrc.org>
2009-04-13 12:12:44 +0000

    Update gnustep-objc to 1.6.0.  Changes are mainly bugfixes.

--- devel/gnustep-objc/buildlink3.mk
+++ devel/gnustep-objc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:24:14 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2009/04/13 12:12:44 rh Exp $
 
 BUILDLINK_TREE+=	gnustep-objc
 
@@ -8,6 +8,11 @@ GNUSTEP_OBJC_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gnustep-objc+=	gnustep-objc>=1.3.0
 BUILDLINK_ABI_DEPENDS.gnustep-objc?=	gnustep-objc>=1.3.0nb1
 BUILDLINK_PKGSRCDIR.gnustep-objc?=	../../devel/gnustep-objc
+
+BUILDLINK_LDFLAGS.gnustep-objc+=	-lobjc -lpthread
+
+PRINT_PLIST_AWK+=	/^@dirrm include\/objc$$/ { print "@comment in devel/gnustep-objc: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/include\/objc$$/ { next; }
 
 .include "../../devel/gnustep-make/buildlink3.mk"
 .endif # GNUSTEP_OBJC_BUILDLINK3_MK
