4d8d9c94babaf28094ed93b53822d14a3eac0f39
rh <rh@pkgsrc.org>
2009-04-13 12:21:37 +0000

    Update gnustep-back to 0.16.0.  This makes the backend more hier(7) compliant.  Changes also include gui rendering improvements and other bug fixes.

--- x11/gnustep-back/buildlink3.mk
+++ x11/gnustep-back/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2009/03/20 19:25:41 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2009/04/13 12:21:37 rh Exp $
 
 BUILDLINK_TREE+=	gnustep-back
 
@@ -9,10 +9,20 @@ BUILDLINK_API_DEPENDS.gnustep-back+=	gnustep-back>=0.9.2
 BUILDLINK_ABI_DEPENDS.gnustep-back+=	gnustep-back>=0.12.0nb1
 BUILDLINK_PKGSRCDIR.gnustep-back?=	../../x11/gnustep-back
 
-.include "../../graphics/cairo/buildlink3.mk"
-.include "../../graphics/libart/buildlink3.mk"
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Bundles\/libgnustep-back-016.bundle$$/ { print "@comment in x11/gnustep-back: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Bundles\/libgnustep-back-016.bundle\/Resources$$/ { print "@comment in x11/gnustep-back: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Bundles\/libgnustep-back-016.bundle\/Resources\/English.lproj$$/ { print "@comment in x11/gnustep-back: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Bundles\/libgnustep-back-016.bundle\/Resources\/Swedish.lproj$$/ { print "@comment in x11/gnustep-back: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Fonts$$/ { print "@comment in x11/gnustep-back: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Fonts\/Helvetica.nfont$$/ { print "@comment in x11/gnustep-back: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Bundles\/libgnustep-back-016.bundle$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Bundles\/libgnustep-back-016.bundle\/Resources$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Bundles\/libgnustep-back-016.bundle\/Resources\/English.lproj$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Bundles\/libgnustep-back-016.bundle\/Resources\/Swedish.lproj$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Fonts$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Fonts\/Helvetica.nfont$$/ { next; }
+
 .include "../../x11/gnustep-gui/buildlink3.mk"
-.include "../../x11/libXft/buildlink3.mk"
 .endif # GNUSTEP_BACK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnustep-back
