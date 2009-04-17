b8c2e24a617db0bd3c4c814358608e5d8fe66afd
rh <rh@pkgsrc.org>
2009-04-17 23:10:30 +0000

    Make this build within the new gnustep world order.  Bump PKGREVISION.

--- devel/gnustep-objc-lf2/buildlink3.mk
+++ devel/gnustep-objc-lf2/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:24:15 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2009/04/17 23:10:30 rh Exp $
 
 BUILDLINK_TREE+=	gnustep-objc-lf2
 
 .if !defined(GNUSTEP_OBJC_LF2_BUILDLINK3_MK)
 GNUSTEP_OBJC_LF2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnustep-objc-lf2+=	gnustep-objc-lf2>=2.95.3.88
-BUILDLINK_ABI_DEPENDS.gnustep-objc-lf2?=	gnustep-objc-lf2>=2.95.3.88nb1
+BUILDLINK_API_DEPENDS.gnustep-objc-lf2+=	gnustep-objc-lf2>=2.95.3.88nb3
+BUILDLINK_ABI_DEPENDS.gnustep-objc-lf2?=	gnustep-objc-lf2>=2.95.3.88nb3
 BUILDLINK_PKGSRCDIR.gnustep-objc-lf2?=	../../devel/gnustep-objc-lf2
-BUILDLINK_FNAME_TRANSFORM.gnustep-objc-lf2?=	-e "s|Headers/objc_lf2/|Headers/objc/|g"
+BUILDLINK_FNAME_TRANSFORM.gnustep-objc-lf2?=	-e "s|include/objc_lf2/|include/objc/|g"
 BUILDLINK_TRANSFORM+=	l:objc:objc_lf2
 
 .include "../../devel/gnustep-make/buildlink3.mk"
