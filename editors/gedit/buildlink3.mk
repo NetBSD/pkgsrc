18303ac42830a5c2948bb07157803c7d26467ee9
drochner <drochner@pkgsrc.org>
2009-04-17 18:40:42 +0000

    update to 2.26.1 changes: -bugfixes -translation updates

--- editors/gedit/buildlink3.mk
+++ editors/gedit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2009/03/20 19:24:31 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2009/04/17 18:40:42 drochner Exp $
 
 BUILDLINK_TREE+=	gedit
 
@@ -9,8 +9,6 @@ BUILDLINK_API_DEPENDS.gedit+=	gedit>=2.12.1nb4
 BUILDLINK_ABI_DEPENDS.gedit?=	gedit>=2.20.4nb2
 BUILDLINK_PKGSRCDIR.gedit?=	../../editors/gedit
 
-.include "../../devel/libglade/buildlink3.mk"
-.include "../../devel/libgnomeui/buildlink3.mk"
 .include "../../x11/gtksourceview2/buildlink3.mk"
 .endif # GEDIT_BUILDLINK3_MK
 
