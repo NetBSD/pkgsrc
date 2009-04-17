488fa8f5f7cf8fb44cec23367687f8ecd2ddbefe
rh <rh>
2009-04-17 22:01:19 +0000

    Make this build with the new gnustep world order. Bump PKGREVISION.

--- devel/edcommon/buildlink3.mk
+++ devel/edcommon/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:24:12 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2009/04/17 22:01:19 rh Exp $
 
 BUILDLINK_TREE+=	edcommon
 
 .if !defined(EDCOMMON_BUILDLINK3_MK)
 EDCOMMON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.edcommon+=	edcommon>=32
-BUILDLINK_ABI_DEPENDS.edcommon?=	edcommon>=32nb2
+BUILDLINK_API_DEPENDS.edcommon+=	edcommon>=32nb6
+BUILDLINK_ABI_DEPENDS.edcommon?=	edcommon>=32nb6
 BUILDLINK_PKGSRCDIR.edcommon?=	../../devel/edcommon
 .endif # EDCOMMON_BUILDLINK3_MK
 
