c9d78edb105a69eca5b01f1517663bece7e059a6
rh <rh@pkgsrc.org>
2009-04-18 02:19:23 +0000

    Update libFoundation to 1.1.7.168.  This makes libFoundation play nice with the new gnustep-make directory structure.  Other notable changes include:  * bugfixes and stability improvements  * Support for some new Cocoa classes, like NSURL  * Improved compatibility with gstep-base  * more and fixed timezone files  * support for read-only strings in UTF-8 and UTF-16  * some fixes for warnings thrown in new gcc (gcc3+) compilers

--- devel/libFoundation/buildlink3.mk
+++ devel/libFoundation/buildlink3.mk
@@ -1,56 +1,55 @@
-# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:24:16 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2009/04/18 02:19:23 rh Exp $
 
 BUILDLINK_TREE+=	libFoundation
 
 .if !defined(LIBFOUNDATION_BUILDLINK3_MK)
 LIBFOUNDATION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libFoundation+=	libFoundation>=1.0.72.107nb1
-BUILDLINK_ABI_DEPENDS.libFoundation?=	libFoundation>=1.0.72.107nb2
+BUILDLINK_API_DEPENDS.libFoundation+=	libFoundation>=1.1.7.168
 BUILDLINK_PKGSRCDIR.libFoundation?=	../../devel/libFoundation
 
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Libraries$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Libraries\/Resources$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Libraries\/Resources\/libFoundation$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/CharacterSets$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/Defaults$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo\/Africa$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo\/Asia$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo\/Australia$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo\/Canada$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo\/Europe$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo\/SystemV$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo\/US$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Library\/Headers\/libFoundation$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Library\/Headers\/libFoundation\/${GNUSTEP_HOST_CPU}$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Library\/Headers\/libFoundation\/${GNUSTEP_HOST_CPU}\/${LOWER_OPSYS}$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Library\/Headers\/libFoundation\/${GNUSTEP_HOST_CPU}\/${LOWER_OPSYS}\/GNU$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Library\/Headers\/libFoundation\/Foundation$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Library\/Headers\/libFoundation\/Foundation\/exceptions$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Library\/Headers\/libFoundation\/extensions$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/GNUstep\/System\/Library\/Headers\/libFoundation\/extensions\/exceptions$$/ { print "@comment in devel/libFoundation: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Libraries$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Libraries\/Resources$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Libraries\/Resources\/libFoundation$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/CharacterSets$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/Defaults$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo\/Africa$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo\/Asia$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo\/Australia$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo\/Canada$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo\/Europe$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo\/SystemV$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Libraries\/Resources\/libFoundation\/TimeZoneInfo\/US$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Library\/Headers\/libFoundation$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Library\/Headers\/libFoundation\/${GNUSTEP_HOST_CPU}$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Library\/Headers\/libFoundation\/${GNUSTEP_HOST_CPU}\/${LOWER_OPSYS}$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Library\/Headers\/libFoundation\/${GNUSTEP_HOST_CPU}\/${LOWER_OPSYS}\/GNU$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Library\/Headers\/libFoundation\/Foundation$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Library\/Headers\/libFoundation\/Foundation\/exceptions$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Library\/Headers\/libFoundation\/extensions$$/ { next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/share\/GNUstep\/System\/Library\/Headers\/libFoundation\/extensions\/exceptions$$/ { next; }
+PRINT_PLIST_AWK+=	/^@dirrm include\/libFoundation$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm include\/libFoundation\/${MACHINE_ARCH}$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm include\/libFoundation\/${MACHINE_ARCH}\/${LOWER_OPSYS}$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm include\/libFoundation\/${MACHINE_ARCH}\/${LOWER_OPSYS}\/GNU$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm include\/libFoundation\/Foundation$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm include\/libFoundation\/Foundation\/exceptions$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm include\/libFoundation\/extensions$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm include\/libFoundation\/extensions\/exceptions$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Libraries\/${PKGNAME}$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/CharacterSets$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/Defaults$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo\/Africa$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo\/Asia$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo\/Australia$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo\/Canada$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo\/Europe$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo\/SystemV$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo\/US$$/ { print "@comment in devel/libFoundation: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/include\/libFoundation$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/include\/libFoundation\/${MACHINE_ARCH}$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/include\/libFoundation\/${MACHINE_ARCH}\/${LOWER_OPSYS}$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/include\/libFoundation\/${MACHINE_ARCH}\/${LOWER_OPSYS}\/GNU$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/include\/libFoundation\/Foundation$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/include\/libFoundation\/Foundation\/exceptions$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/include\/libFoundation\/extensions$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/include\/libFoundation\/extensions\/exceptions$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Libraries\/${PKGNAME}$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/CharacterSets$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/Defaults$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo\/Africa$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo\/Asia$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo\/Australia$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo\/Canada$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo\/Europe$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo\/SystemV$$/ { next; }
+PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/lib\/GNUstep\/Libraries\/${PKGNAME}\/Resources\/libFoundation\/TimeZoneInfo\/US$$/ { next; }
 
 FOUNDATION_LIB?=	fd
 ADDITIONAL_INCLUDE_DIRS+=-I${GNUSTEP_SYSTEM_ROOT}/Library/Headers/libFoundation/${GNUSTEP_HOST_CPU}/${LOWER_OPSYS}/GNU
@@ -61,7 +60,8 @@ MAKE_ENV+=		FOUNDATION_LIB=${FOUNDATION_LIB:Q}
 MAKE_ENV+=		ADDITIONAL_INCLUDE_DIRS=${ADDITIONAL_INCLUDE_DIRS:Q}
 .endif
 
+.include "../../devel/gnustep-make/buildlink3.mk"
 .include "../../devel/gnustep-objc-lf2/buildlink3.mk"
-.endif # LIBFOUNDATION_BUILDLINK3_MK
+.endif	# LIBFOUNDATION_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libFoundation
---
+++ devel/libFoundation/common.mk
@@ -0,0 +1,31 @@
+# $NetBSD: common.mk,v 1.1 2009/04/18 02:19:23 rh Exp $
+#
+PKG_DESTDIR_SUPPORT=	destdir
+
+USE_LANGUAGES=		objc
+
+WRKSRC=			${WRKDIR}/libfoundation
+
+GNU_CONFIGURE=		yes
+MAKE_JOBS_SAFE=		no
+MAKE_ENV+=		ADDITIONAL_OBJCFLAGS="-I${WRKSRC} -I."
+MAKE_ENV+=		FOUNDATION_LIB="fd"
+MAKE_ENV+=		INSTALL_DATA=${INSTALL_DATA:Q}
+
+LIBF_HOST_OS=	${GNUSTEP_HOST:C/.*-//}
+
+FIX_GNUSTEP_INSTALLATION_DIR=	yes
+
+.include "../../devel/gnustep-make/gnustep.mk"
+
+PLIST_SUBST+=	GNUSTEP_HOST_CPU=${GNUSTEP_HOST_CPU:Q}
+PLIST_SUBST+=	LIBF_HOST_OS=${LIBF_HOST_OS}
+
+pre-configure:
+	${MKDIR} ${WRKSRC}/config/${MACHINE_GNU_ARCH}
+	[ -e ${WRKSRC}/config/${MACHINE_GNU_ARCH}/${GNUSTEP_HOST_OS}.h ] || \
+	${LN} -s linux.h ${WRKSRC}/config/${MACHINE_GNU_ARCH}/${GNUSTEP_HOST_OS}.h
+.if ${GNUSTEP_HOST_OS} != ${LIBF_HOST_OS}
+	[ -e ${WRKSRC}/config/${MACHINE_GNU_ARCH}/${LIBF_HOST_OS}.h ] || \
+	${LN} -s linux.h ${WRKSRC}/config/${MACHINE_GNU_ARCH}/${LIBF_HOST_OS}.h
+.endif
