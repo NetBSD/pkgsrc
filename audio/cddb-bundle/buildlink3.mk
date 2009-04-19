ce50d1908c13843c4643ae72040ddca254b72078
rh <rh@pkgsrc.org>
2009-04-19 08:19:37 +0000

    Make this play nice with the new gnustep directory hierarchy.  While at it, add full DESTDIR support.  Bump PKGREVISION.

--- audio/cddb-bundle/buildlink3.mk
+++ audio/cddb-bundle/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:23:52 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2009/04/19 08:19:37 rh Exp $
 
 BUILDLINK_TREE+=	cddb-bundle
 
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.cddb-bundle+=	cddb-bundle>=0.2
 BUILDLINK_ABI_DEPENDS.cddb-bundle?=	cddb-bundle>=0.2nb1
 BUILDLINK_PKGSRCDIR.cddb-bundle?=	../../audio/cddb-bundle
 
-.include "../../devel/gnustep-make/buildlink3.mk"
 .endif # CDDB_BUNDLE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-cddb-bundle
