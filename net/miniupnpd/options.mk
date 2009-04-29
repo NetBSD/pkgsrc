02e8149226f7aaf8cc43838e98837b92ddfe5700
sborrill <sborrill@pkgsrc.org>
2009-04-29 12:34:42 +0000

    NetBSD GENERIC has IPFilter enabled and pf disabled by default. Therefore, on NetBSD at least, we shouldn't default to pf. Bump PKGREVISION

--- net/miniupnpd/options.mk
+++ net/miniupnpd/options.mk
@@ -1,13 +1,17 @@
-# $NetBSD: options.mk,v 1.2 2009/01/06 23:39:53 jmcneill Exp $
+# $NetBSD: options.mk,v 1.3 2009/04/29 12:34:42 sborrill Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.miniupnpd
 PKG_OPTIONS_REQUIRED_GROUPS=	miniupnpd-fw
 PKG_OPTIONS_GROUP.miniupnpd-fw=	pf ipfilter
 
-.if exists(/usr/include/net/pfvar.h)
-PKG_SUGGESTED_OPTIONS+=		pf
-.else
+.if ${OPSYS} == "NetBSD"
 PKG_SUGGESTED_OPTIONS+=		ipfilter
+.else
+.  if exists(/usr/include/net/pfvar.h)
+PKG_SUGGESTED_OPTIONS+=		pf
+.  else
+PKG_SUGGESTED_OPTIONS+=		ipfilter
+.  endif
 .endif
 
 .include "../../mk/bsd.options.mk"
