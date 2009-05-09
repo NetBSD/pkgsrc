98a8fe4144748ff7fe2644f9d3e4a1e22578763a
joerg <joerg@pkgsrc.org>
2009-05-09 20:15:35 +0000

    Add some defines not defined by the nbcompat version of paths.h as fallback.

--- devel/nbpatch/files/pathnames.h
+++ devel/nbpatch/files/pathnames.h
@@ -1,7 +1,7 @@
 /*
  * $OpenBSD: pathnames.h,v 1.1 2003/07/29 20:10:17 millert Exp $
  * $DragonFly: src/usr.bin/patch/pathnames.h,v 1.2 2008/08/11 00:04:12 joerg Exp $
- * $NetBSD: pathnames.h,v 1.2 2009/05/09 20:09:33 joerg Exp $
+ * $NetBSD: pathnames.h,v 1.3 2009/05/09 20:15:35 joerg Exp $
  */
 
 /*
@@ -11,4 +11,14 @@
 
 #include <nbcompat/paths.h>
 
+#ifndef _PATH_DEVNULL
+#define	_PATH_DEVNULL		"/dev/null"
+#endif
+
+#ifndef _PATH_ED
 #define	_PATH_ED		"/bin/ed"
+#endif
+
+#ifndef _PATH_TTY
+#define	_PATH_TTY		"/dev/tty"
+#endif
