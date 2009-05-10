3308c50c97aedf0c1c2dd2f78684da1ed3546136
hasso <hasso@pkgsrc.org>
2009-05-10 11:24:26 +0000

    Update to 2.8.0. It is interface-compatible with Xerces-C++ 2.7.0 and contains a large number of bug fixes as well as improvements to the build system. For the compete list of upstream changes see:
    http://xerces.apache.org/xerces-c/releases_archive.html#Release280

--- textproc/xerces-c/buildlink3.mk
+++ textproc/xerces-c/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:25:33 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2009/05/10 11:24:26 hasso Exp $
 
 BUILDLINK_TREE+=	xerces-c
 
 .if !defined(XERCES_C_BUILDLINK3_MK)
 XERCES_C_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xerces-c+=	xerces-c>=2.7.0
-BUILDLINK_ABI_DEPENDS.xerces-c?=	xerces-c>=2.7.0
+BUILDLINK_API_DEPENDS.xerces-c+=	xerces-c>=2.8.0
+BUILDLINK_ABI_DEPENDS.xerces-c?=	xerces-c>=2.8.0
 BUILDLINK_PKGSRCDIR.xerces-c?=	../../textproc/xerces-c
 
 .include "../../converters/libiconv/buildlink3.mk"
