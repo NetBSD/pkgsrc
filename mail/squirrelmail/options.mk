5cde5a210cdec2b26ab6af8adec0758ed4efb625
martti <martti@pkgsrc.org>
2009-05-14 06:54:38 +0000

    Updated mail/squirrelmail to 1.4.18
    The SquirrelMail Team is pleased to announce the release of
    SquirrelMail version 1.4.18.  The most notable changes for this
    version are several security fixes, including a couple XSS exploits, a
    session fixation issue, and an obscure but dangerous server-side code
    execution hole.  However, this version also includes three new
    languages and more than a few enhancements to things such as the
    filters plugin, the address book system and other things under the
    hood.  For more complete details, see the ReleaseNotes and ChangeLog
    files included in this release (they have moved to the doc/
    directory).  We advise all users of SquirrelMail software to upgrade.

--- mail/squirrelmail/options.mk
+++ mail/squirrelmail/options.mk
@@ -1,21 +1,20 @@
-# $NetBSD: options.mk,v 1.9 2008/11/19 14:47:51 taca Exp $
+# $NetBSD: options.mk,v 1.10 2009/05/14 06:54:39 martti Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.squirrelmail
 PKG_SUPPORTED_OPTIONS=	squirrelmail-japanese squirrelmail-lite
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Msquirrelmail-japanese)
-PATCHFILES+=		squirrelmail-1.4.16-ja-20081013-patch.gz
-SITES.squirrelmail-1.4.16-ja-20081013-patch.gz= \
-			http://www.yamaai-tech.com/~masato/Download/
-PATCH_DIST_STRIP.squirrelmail-1.4.16-ja-20081013-patch.gz=	-p1
-
-.endif
+#.if !empty(PKG_OPTIONS:Msquirrelmail-japanese)
+#PATCHFILES+=		squirrelmail-1.4.16-ja-20081013-patch.gz
+#SITES.squirrelmail-1.4.16-ja-20081013-patch.gz= \
+#			http://www.yamaai-tech.com/~masato/Download/
+#PATCH_DIST_STRIP.squirrelmail-1.4.16-ja-20081013-patch.gz=	-p1
+#.endif
 
 .if !empty(PKG_OPTIONS:Msquirrelmail-lite)
-PATCHFILES+=		squirrelmail-1.4.11-lite-20071003-patch.bz2
-SITES.squirrelmail-1.4.11-lite-20071003-patch.bz2= \
+PATCHFILES+=		squirrelmail-1.4.18-lite-20090512-patch.bz2
+SITES.squirrelmail-1.4.18-lite-20090512-patch.bz2= \
 			ftp://ftp.back-street.net/pub/squirrelmail/
 
 DEPENDS+=	${PHP_PKG_PREFIX}-mbstring>=4.3.3:../../misc/php-mbstring
