4f597bc07da55c401c420c0a793d0d80691e6b53
obache <obache>
2009-05-15 08:52:42 +0000

    Update libdatrie to 0.2.2.
    0.2.2 (2009-04-29)
    =====
    - Support building with linkers without symbol versioning supports,
      including Mac, Cygwin, MinGW.
    - Support other iconv implementations than that's in glibc, for Mac and MinGW.
    
    0.2.1 (2009-04-05)
    =====
    - Fix errors in documentation
    - Symbol versioning to ease upgrade across SONAME
    - Minor cleanups
    
    0.2.0 (2009-03-24)
    =====
    - New APIs for performance: trie_state_copy(), trie_state_is_single()
    - Clean-ups
    
    0.1.99.2 (2008-12-15)
    ========
    - More robust handling of alphabet ranges
    - Allow co-existence with libdatrie0
    
    0.1.99.1 (2008-12-12)
    ========
    - Breaking ABI with libdatrie0
    - More capacity with 32-bit node index
    - Adjusted API for non-file trie usage
    - All data in one file, no more *.br and *.tl split
    - Drop SBTrie wrapper; all features are merged into Trie
    - Domain characters are now Unicode

--- devel/libdatrie/buildlink3.mk
+++ devel/libdatrie/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:17 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/05/15 08:52:42 obache Exp $
 
 BUILDLINK_TREE+=	libdatrie
 
 .if !defined(LIBDATRIE_BUILDLINK3_MK)
 LIBDATRIE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libdatrie+=	libdatrie>=0.1.3
+BUILDLINK_API_DEPENDS.libdatrie+=	libdatrie>=0.2.0
 BUILDLINK_PKGSRCDIR.libdatrie?=	../../devel/libdatrie
 .endif # LIBDATRIE_BUILDLINK3_MK
 
