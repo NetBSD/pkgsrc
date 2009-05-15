d9683dfee305c02e65cf96a4b1e9450457725a87
obache <obache>
2009-05-15 08:55:35 +0000

    Update libthai to 0.1.11.
    LibThai 0.1.11 (6 April 2009):
    * Diminished link flags in pkg-config info, for less dependency for clients.
    * More words in word break dictionary.
    
    LibThai 0.1.10 (30 March 2009):
    * More dictionary capacity with libdatrie 0.2.
    * Dictionary clean-up on unload.
    * Some performance fine-tuning.
    * More words in word break dictionary.

--- devel/libthai/buildlink3.mk
+++ devel/libthai/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:23 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/05/15 08:55:35 obache Exp $
 
 BUILDLINK_TREE+=	libthai
 
 .if !defined(LIBTHAI_BUILDLINK3_MK)
 LIBTHAI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libthai+=	libthai>=0.1.9
+BUILDLINK_API_DEPENDS.libthai+=	libthai>=0.1.10
 BUILDLINK_PKGSRCDIR.libthai?=	../../devel/libthai
 
 .include "../../devel/libdatrie/buildlink3.mk"
