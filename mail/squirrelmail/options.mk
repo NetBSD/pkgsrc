8fe1604141c71fb3ce49691357395f91810eedaf
taca <taca@pkgsrc.org>
2009-05-15 13:01:00 +0000

    Enable squirrelmail-japanese option with Japanese patch: squirrelmail-1.4.16-ja-20081013-patch.
    Bump PKGREVISION.

--- mail/squirrelmail/options.mk
+++ mail/squirrelmail/options.mk
@@ -1,16 +1,16 @@
-# $NetBSD: options.mk,v 1.10 2009/05/14 06:54:39 martti Exp $
+# $NetBSD: options.mk,v 1.11 2009/05/15 13:01:00 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.squirrelmail
 PKG_SUPPORTED_OPTIONS=	squirrelmail-japanese squirrelmail-lite
 
 .include "../../mk/bsd.options.mk"
 
-#.if !empty(PKG_OPTIONS:Msquirrelmail-japanese)
-#PATCHFILES+=		squirrelmail-1.4.16-ja-20081013-patch.gz
-#SITES.squirrelmail-1.4.16-ja-20081013-patch.gz= \
-#			http://www.yamaai-tech.com/~masato/Download/
-#PATCH_DIST_STRIP.squirrelmail-1.4.16-ja-20081013-patch.gz=	-p1
-#.endif
+.if !empty(PKG_OPTIONS:Msquirrelmail-japanese)
+PATCHFILES+=		squirrelmail-1.4.18-ja-20090515-patch.gz
+SITES.squirrelmail-1.4.18-ja-20090515-patch.gz= \
+			http://www.yamaai-tech.com/~masato/Download/
+PATCH_DIST_STRIP.squirrelmail-1.4.18-ja-20090515-patch.gz=	-p1
+.endif
 
 .if !empty(PKG_OPTIONS:Msquirrelmail-lite)
 PATCHFILES+=		squirrelmail-1.4.18-lite-20090512-patch.bz2
