302f8829632e320da6e3530013b3d70aa5e7ea8f
obache <obache@pkgsrc.org>
2009-05-16 04:18:13 +0000

    Convert options.mk to standard format/usage.

--- devel/libstatgrab/options.mk
+++ devel/libstatgrab/options.mk
@@ -1,19 +1,14 @@
-# $NetBSD: options.mk,v 1.2 2008/04/09 16:21:48 jlam Exp $
+# $NetBSD: options.mk,v 1.3 2009/05/16 04:18:13 obache Exp $
 
-PKG_OPTIONS_VAR=	libstatgrab
+PKG_OPTIONS_VAR=	PKG_OPTIONS.libstatgrab
 PKG_SUPPORTED_OPTIONS=	saidar statgrab
-PKG_DEFAULT_OPTIONS=	saidar statgrab
+PKG_SUGGESTED_OPTIONS=	saidar statgrab
+PKG_OPTIONS_LEGACY_VARS+=	WITHOUT_SAIDAR:-saidar
+PKG_OPTIONS_LEGACY_VARS+=	WITHOUT_STATGRAB:-statgrab
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=		saidar statgrab
-
-.if defined(WITHOUT_SAIDAR)
-PKG_OPTIONS_DEPRECATED_WARNINGS+="Deprecated variable WITHOUT_SAIDAR used; use \`\`-saidar'' instead."
-.endif
-.if defined(WITHOUT_STATGRAB)
-PKG_OPTIONS_DEPRECATED_WARNINGS+="Deprecated variable WITHOUT_STATGRAB used; use \`\`-statgrab'' instead."
-.endif
 
 .if !empty(PKG_OPTIONS:Mstatgrab)
 PLIST.statgrab=		yes
