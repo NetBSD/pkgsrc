64841d9facc027d6e4fc14354daf28e84e315bb6
tron <tron>
2009-05-17 14:33:22 +0000

    Pullup ticket #2772 - requested by taca squirrelmail: Re-add Japanese language option
    Revisions pulled up:
    - mail/squirrelmail/Makefile		1.104-1.105
    - mail/squirrelmail/distinfo		1.52
    - mail/squirrelmail/options.mk		1.11
    ---
    Module Name:	pkgsrc
    Committed By:	taca
    Date:		Thu May 14 14:24:50 UTC 2009
    
    Modified Files:
    	pkgsrc/mail/squirrelmail: Makefile
    
    Log Message:
    Since iso_2022_jp.php isn't included in squirrelmail distribution,
    remove extra pre-configure processing.
    ---
    Module Name:	pkgsrc
    Committed By:	taca
    Date:		Fri May 15 13:01:00 UTC 2009
    
    Modified Files:
    	pkgsrc/mail/squirrelmail: Makefile distinfo options.mk
    
    Log Message:
    Enable squirrelmail-japanese option with Japanese patch:
    squirrelmail-1.4.16-ja-20081013-patch.
    
    Bump PKGREVISION.

--- mail/squirrelmail/options.mk
+++ mail/squirrelmail/options.mk
@@ -1,16 +1,16 @@
-# $NetBSD: options.mk,v 1.9.4.1 2009/05/14 09:08:59 tron Exp $
+# $NetBSD: options.mk,v 1.9.4.2 2009/05/17 14:33:22 tron Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.squirrelmail
 PKG_SUPPORTED_OPTIONS=	squirrelmail-japanese squirrelmail-lite
 
 .include "../../mk/bsd.options.mk"
 
-#.if !empty(PKG_OPTIONS:Msquirrelmail-japanese)
-#PATCHFILES+=		squirrelmail-1.4.16-ja-20081013-patch.gz
-#SITES.squirrelmail-1.4.16-ja-20081013-patch.gz= \
-#			http://www.yamaai-tech.com/~masato/Download/
-#PATCH_DIST_STRIP.squirrelmail-1.4.16-ja-20081013-patch.gz=	-p1
-#.endif
+.if !empty(PKG_OPTIONS:Msquirrelmail-japanese)
+PATCHFILES+=		squirrelmail-1.4.18-ja-20090515-patch.gz
+SITES.squirrelmail-1.4.18-ja-20090515-patch.gz= \
+			http://www.yamaai-tech.com/~masato/Download/
+PATCH_DIST_STRIP.squirrelmail-1.4.18-ja-20090515-patch.gz=	-p1
+.endif
 
 .if !empty(PKG_OPTIONS:Msquirrelmail-lite)
 PATCHFILES+=		squirrelmail-1.4.18-lite-20090512-patch.bz2
