77ab1e4c3ff0098df3c81d45e70a5cae3f42564b
sno <sno@pkgsrc.org>
2009-05-17 12:55:52 +0000

    PkgSrc changes:   - Updating package graphics/graphviz to 2.22.2   - Setting license to cpl-1.0   - Rework optional dependencies handling (handle dependencies     of dependencies)
    Upstream changes:
    March 13, 2009
            - Release 2.22.2
                    - fix for buffer overflow (present in 2.22.0 and 2.22.1)
            - Fixed bugs:
                    1596, 1602
    
    March 9, 2009
            - Release 2.22.1
                    - build fixes for Visual Studio and for FreeBSD
    March 3, 2009
            - Release 2.22.0
            - Core:
                    - libgvc api changed, version bumped.  Affects third party
                      applications using libgvc.
                    - plugin api changed, version bumped.  Affects third party
                      plugins for graphviz.
                    - 90% conversion to cgraph has been done, but not enabled yet,
                      (and yes, its true what they say about the last 10% )
                    - drop libagraph from distribution  (use libcgraph)
                    - layout code completely converted to floating point.
                    - new "dot -P" option for generating a graph of available
                      plugins.
                    - registered MIME type:  text/vnd.graphviz for .gv files
                    - rename files from .dot to .gv to avoid conflict with
                      Word templates.  .dot still supported, but deprecated.
                    - new command: mm2gv   (matrix-market graph file conversion)
                    - rename commands:      dot2gxl -> gv2gxl
                                            gxl2dot -> gxl2gv
            - Plugins:
                    - new rsvg plugin for support of node shapes in SVG format
                    - new gs plugin for support of node shapes in PS format
                    - new lasi plugin for support of UTF-8 characters in PS output
                      (the above thee plugins are Linux only, at the moment)
                    - new quartz plugin (MacOSx only)
                    - new gdiplus plugin (Windows only)
                    - new -Tvml support in core plugin (thanks Steve Roush)
                    - new -Ttk support in core plugin (also used by Tcldot and
                      gv_tcl language bindings.)
                    - disabled old style codegens completely
            - Linux:
                    - new Ubuntu8 builds
                    - new Fedora 10 and 11 builds
            - MacOSx:
                    - Universal binary for Leopard: i386, x86_64, ppc, ppc64
                    - Should not conflict with parallel install of MacPorts
                      version of graphviz
                    - Improved GUI
            - Windows:
                    - VisualC project files now available, in addition to the GNU
                      Makefiles that are used the mingw builds.
            - Language Bindings:
                    - fixed problem with writing dot, xdot, plain, canon to
                      memory or to Tcl_Channels
                    - renamed man pages to Debian style:  gv.3tcl, gv.3perl, etc
            - Fixed bugs:
                    827, 1365, 1366, 1367, 1368, 1374, 1375, 1376, 1378, 1380, 1382,
                    1383, 1385, 1386, 1388, 1390, 1391, 1392, 1394, 1395, 1397,
                    1398, 1399, 1405, 1407, 1410, 1412, 1414, 1415, 1416, 1421,
                    1424, 1425, 1427, 1429, 1431, 1433, 1435, 1436, 1437, 1438,
                    1440, 1441, 1444, 1446, 1451, 1452, 1453, 1456, 1457, 1459,
                    1460, 1461, 1462, 1463, 1464, 1465, 1466, 1470, 1474, 1475,
                    1476, 1477, 1478, 1484, 1485, 1489, 1490, 1492, 1493, 1495,
                    1496, 1499, 1500, 1501, 1502, 1503, 1505, 1509, 1513, 1521,
                    1523, 1525, 1530, 1531, 1532, 1533, 1535, 1536, 1539, 1540,
                    1542, 1543, 1546, 1547, 1551, 1553, 1554, 1561, 1565, 1566,
                    1568, 1569, 1570, 1571, 1573, 1577, 1578, 1579, 1580, 1581,
                    1582, 1584, 1586,

--- graphics/graphviz/options.mk
+++ graphics/graphviz/options.mk
@@ -1,29 +1,71 @@
-# $NetBSD: options.mk,v 1.5 2009/02/26 19:45:14 sno Exp $
+# $NetBSD: options.mk,v 1.6 2009/05/17 12:55:52 sno Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.graphviz
-PKG_SUPPORTED_OPTIONS=	x11 pangocairo guile lua ocaml swig tcl gtk gd
-PKG_SUGGESTED_OPTIONS=	x11 pangocairo lua swig tcl
+PKG_SUPPORTED_OPTIONS=	gd x11 pangocairo gtk rsvg swig guile lua ocaml tcl perl
+PKG_SUGGESTED_OPTIONS=	gd x11 pangocairo gtk rsvg swig lua tcl perl
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		guile gtk lua ocaml tcl pangocairo x11 gd
+PLIST_VARS+=		gd x11 pangocairo rsvg gtk guile lua ocaml tcl perl
 
 .if !empty(PKG_OPTIONS:Mx11)
 .include "../../x11/libXp/buildlink3.mk"
 .include "../../mk/xaw.buildlink3.mk"
 CONFIGURE_ENV+=		X11PREFIX=${X11PREFIX} X11BASE=${X11BASE}
 PLIST.x11=		yes
+
+.if !empty(PKG_OPTIONS:Mpangocairo)
+.include "../../devel/pango/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
+PLIST.pangocairo=	yes
+
+.if !empty(PKG_OPTIONS:Mgtk)
+.include "../../x11/gtk2/buildlink3.mk"
+PLIST.gtk=		yes
 .else
-CONFIGURE_ARGS+=	--without-x
+CONFIGURE_ARGS+=	--without-gdk-pixbuf
+CONFIGURE_ARGS+=	--without-gtk
+CONFIGURE_ARGS+=	--without-gnomeui
 .endif
 
-.if !empty(PKG_OPTIONS:Mx11) && !empty(PKG_OPTIONS:Mguile)
-.include "../../lang/guile/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-guile
-PLIST.guile=		yes
+.if !empty(PKG_OPTIONS:Mrsvg)
+.include "../../graphics/librsvg/buildlink3.mk"
+PLIST.rsvg=		yes
 .else
-CONFIGURE_ARGS+=	--disable-guile
+CONFIGURE_ARGS+=	--without-rsvg
 .endif
+
+.else
+CONFIGURE_ARGS+=	--without-pangocairo
+
+CONFIGURE_ARGS+=	--without-gdk-pixbuf
+CONFIGURE_ARGS+=	--without-gtk
+CONFIGURE_ARGS+=	--without-gnomeui
+
+CONFIGURE_ARGS+=	--without-rsvg
+.endif
+
+.else
+CONFIGURE_ARGS+=	--without-x
+
+CONFIGURE_ARGS+=	--without-pangocairo
+
+CONFIGURE_ARGS+=	--without-gdk-pixbuf
+CONFIGURE_ARGS+=	--without-gtk
+CONFIGURE_ARGS+=	--without-gnomeui
+
+CONFIGURE_ARGS+=	--without-rsvg
+.endif
+
+.if !empty(PKG_OPTIONS:Mgd)
+.include "../../graphics/gd/buildlink3.mk"
+PLIST.gd=		yes
+.else
+CONFIGURE_ARGS+=	--without-libgd
+.endif
+
+.if !empty(PKG_OPTIONS:Mswig)
+.include "../../devel/swig/buildlink3.mk"
 
 .if !empty(PKG_OPTIONS:Mlua)
 .include "../../lang/lua/buildlink3.mk"
@@ -39,12 +81,6 @@ PLIST.ocaml=		yes
 CONFIGURE_ARGS+=	--disable-ocaml
 .endif
 
-.if !empty(PKG_OPTIONS:Mswig)
-.include "../../devel/swig/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-swig
-.endif
-
 .if !empty(PKG_OPTIONS:Mtcl)
 .include "../../x11/tk/buildlink3.mk"
 CONFIGURE_ENV+=		TCLCONFIG=${TCLCONFIG_SH:Q}
@@ -54,26 +90,28 @@ PLIST.tcl=		yes
 CONFIGURE_ARGS+=	--disable-tcl
 .endif
 
-.if !empty(PKG_OPTIONS:Mx11) && !empty(PKG_OPTIONS:Mgtk)
-.include "../../x11/gtk2/buildlink3.mk"
-PLIST.gtk=		yes
+.if !empty(PKG_OPTIONS:Mguile)
+.include "../../lang/guile/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-guile
+PLIST.guile=		yes
 .else
-CONFIGURE_ARGS+=	--without-gdk-pixbuf
-CONFIGURE_ARGS+=	--without-gtk
-CONFIGURE_ARGS+=	--without-gnomeui
+CONFIGURE_ARGS+=	--disable-guile
 .endif
 
-.if !empty(PKG_OPTIONS:Mgd)
-.include "../../graphics/gd/buildlink3.mk"
-PLIST.gd=		yes
+.if !empty(PKG_OPTIONS:Mperl)
+.include "../../lang/perl5/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-perl
+PLIST.perl=		yes
+USE_TOOLS+=perl
 .else
-CONFIGURE_ARGS+=	--without-libgd
+CONFIGURE_ARGS+=	--disable-perl
 .endif
 
-.if !empty(PKG_OPTIONS:Mx11) && !empty(PKG_OPTIONS:Mpangocairo)
-.include "../../devel/pango/buildlink3.mk"
-.include "../../graphics/cairo/buildlink3.mk"
-PLIST.pangocairo=		yes
 .else
-CONFIGURE_ARGS+=	--without-pangocairo
+CONFIGURE_ARGS+=	--disable-swig
+CONFIGURE_ARGS+=	--disable-tcl
+CONFIGURE_ARGS+=	--disable-ocaml
+CONFIGURE_ARGS+=	--disable-lua
+CONFIGURE_ARGS+=	--disable-guile
+CONFIGURE_ARGS+=	--disable-perl
 .endif
