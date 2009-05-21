8f597895f312e496ff5336e573d139cec4b60709
sno <sno@pkgsrc.org>
2009-05-21 19:24:44 +0000

    Importing package p5-pango 1.220 as dependency for scheduled update of p5-gtk2.
    Perl bindings to the pango layout and rendering library
    (http://www.pango.org/).

---
+++ devel/p5-pango/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/05/21 19:24:44 sno Exp $
+
+BUILDLINK_TREE+=	p5-pango
+
+.if !defined(P5_PANGO_BUILDLINK3_MK)
+P5_PANGO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.p5-pango+=	p5-pango>=1.200
+BUILDLINK_PKGSRCDIR.p5-pango?=		../../devel/p5-pango
+
+.include "../../devel/pango/buildlink3.mk"
+.endif # P5_PANGO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-p5-pango
