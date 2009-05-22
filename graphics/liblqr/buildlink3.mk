2283cfd5ac61d6c873323877a5c7fab554e41f7e
tnn <tnn@pkgsrc.org>
2009-05-22 20:33:23 +0000

    bump the BUILDLINK_API_DEPENDS

--- graphics/liblqr/buildlink3.mk
+++ graphics/liblqr/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:42 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/05/22 20:33:23 tnn Exp $
 
 BUILDLINK_TREE+=	liblqr
 
 .if !defined(LIBLQR_BUILDLINK3_MK)
 LIBLQR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liblqr+=	liblqr>=0.1.0.2
+BUILDLINK_API_DEPENDS.liblqr+=	liblqr>=0.4.1
 BUILDLINK_PKGSRCDIR.liblqr?=	../../graphics/liblqr
 
 .include "../../devel/glib2/buildlink3.mk"
