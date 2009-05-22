bcad8021385fada8938318d68d72ecf08bb04a4d
sno <sno@pkgsrc.org>
2009-05-22 05:58:22 +0000

    add missing include of p5-pango

--- x11/p5-gtk2/buildlink3.mk
+++ x11/p5-gtk2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:48 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/05/22 05:58:22 sno Exp $
 
 BUILDLINK_TREE+=	p5-gtk2
 
@@ -10,6 +10,7 @@ BUILDLINK_PKGSRCDIR.p5-gtk2?=	../../x11/p5-gtk2
 
 .include "../../devel/p5-glib2/buildlink3.mk"
 .include "../../graphics/p5-cairo/buildlink3.mk"
+.include "../../devel/p5-pango/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .endif # P5_GTK2_BUILDLINK3_MK
 
