c1e245c2ad6ccd35a1c7966ea04e43c5bab4daf3
wiz <wiz@pkgsrc.org>
2009-05-26 21:59:57 +0000

    Allow overriding build target for do-build, using PYSETUPBUILDTARGET.

--- lang/python/extension.mk
+++ lang/python/extension.mk
@@ -1,4 +1,4 @@
-# $NetBSD: extension.mk,v 1.24 2009/03/08 17:05:10 joerg Exp $
+# $NetBSD: extension.mk,v 1.25 2009/05/26 21:59:58 wiz Exp $
 
 .include "../../lang/python/pyversion.mk"
 
@@ -6,6 +6,7 @@
 .include "../../mk/bsd.prefs.mk"
 
 PYSETUP?=		setup.py
+PYSETUPBUILDTARGET?=	build
 PYSETUPBUILDARGS?=	#empty
 PYSETUPINSTALLARGS?=	#empty
 PYSETUPOPTARGS?=	-c -O1
@@ -18,7 +19,7 @@ PYSETUPSUBDIR?=		#empty
 
 do-build:
 	(cd ${WRKSRC}/${PYSETUPSUBDIR} && ${SETENV} ${MAKE_ENV} ${PYTHONBIN} \
-	 ${PYSETUP} build ${PYSETUPBUILDARGS})
+	 ${PYSETUP} ${PYSETUPBUILDTARGET} ${PYSETUPBUILDARGS})
 
 do-install:
 	(cd ${WRKSRC}/${PYSETUPSUBDIR} && ${SETENV} ${INSTALL_ENV} ${MAKE_ENV} \
