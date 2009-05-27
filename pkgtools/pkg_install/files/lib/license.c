7e93d54fa22040d4c67b87dcd8c1658e165c1bd0
joerg <joerg@pkgsrc.org>
2009-05-27 21:56:11 +0000

    Sync license list with mk/license.mk.

--- pkgtools/pkg_install/files/lib/license.c
+++ pkgtools/pkg_install/files/lib/license.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: license.c,v 1.2 2009/05/18 10:01:37 wiz Exp $	*/
+/*	$NetBSD: license.c,v 1.3 2009/05/27 21:56:11 joerg Exp $	*/
 
 /*-
  * Copyright (c) 2009 Joerg Sonnenberger <joerg@NetBSD.org>.
@@ -51,7 +51,7 @@ const char *default_acceptable_licenses =
     "gnu-gpl-v2 gnu-lgpl-v2 gnu-lgpl-v2.1 "
     "gnu-gpl-v3 gnu-lgpl-v3 "
     "original-bsd modified-bsd "
-    "x11 "
+    "x11 mit "
     "apache-1.1 apache-2.0 "
     "artistic artistic-2.0 "
     "cddl-1.0 "
