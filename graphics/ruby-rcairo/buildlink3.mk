31d3d9ec50b0c9deb7ce251859c99efb59d305b4
obache <obache@pkgsrc.org>
2009-05-27 05:47:44 +0000

    Just use ruby- for future ruby19...

--- graphics/ruby-rcairo/buildlink3.mk
+++ graphics/ruby-rcairo/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:24:45 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/05/27 05:47:44 obache Exp $
 
-BUILDLINK_TREE+=	${RUBY_PKGPREFIX}-rcairo
+BUILDLINK_TREE+=	ruby-rcairo
 
 .if !defined(RUBY_RCAIRO_BUILDLINK3_MK)
 RUBY_RCAIRO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby18-rcairo+=	${RUBY_PKGPREFIX}-rcairo>=1.6.0
-BUILDLINK_PKGSRCDIR.ruby18-rcairo?=	../../graphics/ruby-rcairo
+BUILDLINK_API_DEPENDS.ruby-rcairo+=	${RUBY_PKGPREFIX}-rcairo>=1.6.0
+BUILDLINK_PKGSRCDIR.ruby-rcairo?=	../../graphics/ruby-rcairo
 
 .include "../../graphics/cairo/buildlink3.mk"
 .endif # RUBY_RCAIRO_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-${RUBY_PKGPREFIX}-rcairo
+BUILDLINK_TREE+=	-ruby-rcairo
