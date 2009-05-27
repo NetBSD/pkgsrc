09b67a17f4fa7cf5b3a1fec1f7db8d1d020775e9
obache <obache@pkgsrc.org>
2009-05-27 05:38:52 +0000

    Update py-ldap to 2.3.8.
    ----------------------------------------------------------------
    Released 2.3.8 2009-04-30
    
    Changes since 2.3.7:
    
    Lib/
    * ldap.schema.models: More fault-tolerant parsing of SYNTAX in
      AttributeTypeDescription
    * ldap.schema.tokenizer.split_tokens():
      More tolerant parsing of items separated only with a DOLLAR without
      surrounding white-spaces (because WSP is declared as zero or more spaces
      in RFC 4512)
    
    ----------------------------------------------------------------
    Released 2.3.7 2009-04-09
    
    Changes since 2.3.6:
    
    Lib/
    * urllib.quote() is now used in LDAPUrlExtension.unparse() to quote
      all special URL characters in extension values
    
    Modules/
    * Fixed ldapcontrol.c not to raise ldap.ENCODING_ERROR in
      function encode_rfc2696() on 64-bit systems
    * Fixed seg fault if error code in a LDAP response was outside
      the known error codes and could not be mapped to a specific
      exception class (thanks to Sean)
    * errors.c: LDAP_ERROR_MAX set to LDAP_PROXIED_AUTHORIZATION_DENIED
      if available in OpenLDAP header
    * new exception class ldap.PROXIED_AUTHORIZATION_DENIED
      if available in OpenLDAP header
    * Fixed functions.c not to raise ldap.ENCODING_ERROR in
      function l_ldap_str2dn() on 64-bit systems (see SF#2725356)
    
    ----------------------------------------------------------------
    Released 2.3.6 2009-02-22
    
    Changes since 2.3.5:
    
    Lib/
    * Importing ldap.str2dn() which directly imported _ldap.str2dn()
      is prohibited now (see SF#2181141)
    
    Modules/
    * get_option(): Added support for reading more SASL options.
      (OPT_X_SASL_MECH, OPT_X_SASL_REALM, OPT_X_SASL_AUTHCID and
      OPT_X_SASL_AUTHZID)
    * Added some explicit type casts to fix issues while building
      with SunStudio
    * Fixed compiling issue with GCC 4.4
      (see SF#2555793, thanks to Matej and Martin)
    
    Doc/
    * Clarified not to use ldap_get_dn() directly
    * Fixed description of ldap.SASL_AVAIL and ldap.TLS_AVAIL
      (see SF#2555804, thanks to Matej and Martin)
    
    ----------------------------------------------------------------
    Released 2.3.5 2008-07-06
    
    Changes since 2.3.4:
    
    Lib/
    * Fixed methods ldap.cidict.__contains__() and
      ldap.schema.models.Entry.__contains__()
    * FWIW method LDAPObject.cancel_s() returns a result now
    * Fixed ldap.schema.models.NameForm: Class attribute oc is now
      of type string, not tuple to be compliant with RFC 4512
    ----------------------------------------------------------------
    Released 2.3.4 2008-03-29
    
    Changes since 2.3.3:
    
    Modules/
    * Fixed seg fault when calling LDAPObject.get_option()
      (see SF#1926507, thanks to Matej)
    
    ----------------------------------------------------------------
    Released 2.3.3 2008-03-26
    
    Changes since 2.3.2:
    
    Fixed backward-compability when building with OpenLDAP 2.3.x libs.
    
    ----------------------------------------------------------------
    Released 2.3.2 2008-03-26
    
    Changes since 2.3.1:
    
    Lib/
    * ldap.dn.escape_dn_chars() now really adheres to
      RFC 4514 section 2.4 by escaping null characters and a
      space occurring at the beginning of the string
    * New method ldap.cidict.cidict.__contains__()
    * ldap.dn.explode_dn() and ldap.dn.explode_rdn()
      have a new optional key-word argument flags which is
      passed to ldap.dn.str2dn().
    
    Modules/
    * Removed unused OPT_PRIVATE_EXTENSION_BASE from constants.c
    
    Doc/
    * Various additions, updates, polishing (thanks to James).

--- databases/py-ldap/buildlink3.mk
+++ databases/py-ldap/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:24:07 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2009/05/27 05:38:52 obache Exp $
 
-BUILDLINK_TREE+=	pyldap
+BUILDLINK_TREE+=	py-ldap
 
 .if !defined(PY_LDAP_BUILDLINK3_MK)
 PY_LDAP_BUILDLINK3_MK:=
 
 .include "../../lang/python/pyversion.mk"
 
-BUILDLINK_API_DEPENDS.pyldap+=	${PYPKGPREFIX}-ldap>=2.2.0
-BUILDLINK_ABI_DEPENDS.pyldap+=	${PYPKGPREFIX}-ldap>=2.2.0
-BUILDLINK_PKGSRCDIR.pyldap?=	../../databases/py-ldap
+BUILDLINK_API_DEPENDS.py-ldap+=	${PYPKGPREFIX}-ldap>=2.2.0
+BUILDLINK_ABI_DEPENDS.py-ldap+=	${PYPKGPREFIX}-ldap>=2.2.0
+BUILDLINK_PKGSRCDIR.py-ldap?=	../../databases/py-ldap
 .endif # PY_LDAP_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-pyldap
+BUILDLINK_TREE+=	-py-ldap
