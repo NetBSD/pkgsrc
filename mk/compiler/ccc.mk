c0184c7970ed510488eb80c1f4926f600fb5fce2
joerg <joerg@pkgsrc.org>
2009-06-02 22:32:49 +0000

    Learn from the AIX issues with hard links to symbolic links and always create the compiler aliases with ln -fs.

--- mk/compiler/ccc.mk
+++ mk/compiler/ccc.mk
@@ -1,4 +1,4 @@
-# $NetBSD: ccc.mk,v 1.18 2009/05/30 18:16:26 joerg Exp $
+# $NetBSD: ccc.mk,v 1.19 2009/06/02 22:32:49 joerg Exp $
 #
 # This is the compiler definition for the Compaq C Compilers.
 #
@@ -83,7 +83,7 @@ ${_CCC_${_var_}}:
 .    for _alias_ in ${_ALIASES.${_var_}:S/^/${.TARGET:H}\//}
 	${RUN}					\
 	if [ ! -x "${_alias_}" ]; then					\
-		${LN} -f ${.TARGET} ${_alias_};				\
+		${LN} -f -s ${.TARGET} ${_alias_};			\
 	fi
 .    endfor
 .  endif
--- mk/compiler/hp.mk
+++ mk/compiler/hp.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hp.mk,v 1.6 2009/05/30 18:16:26 joerg Exp $
+# $NetBSD: hp.mk,v 1.7 2009/06/02 22:32:49 joerg Exp $
 #
 # This is the compiler definition for the HP-UX C/aC++ compilers.
 #
@@ -85,7 +85,7 @@ ${_HP_${_var_}}:
 .    for _alias_ in ${_ALIASES.${_var_}:S/^/${.TARGET:H}\//}
 	${RUN}					\
 	if [ ! -x "${_alias_}" ]; then					\
-		${LN} -f ${.TARGET} ${_alias_};				\
+		${LN} -f -s ${.TARGET} ${_alias_};			\
 	fi
 .    endfor
 .  endif
