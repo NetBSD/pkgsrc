6f948c628c8bacf5a458989ee8744714dbdcf171
wiz <wiz@pkgsrc.org>
2009-06-03 10:20:59 +0000

    Add missing file to PLIST.
    pdftops only seems to be a link to an existing pdftops, so add
    an option to use the ones from gs, poppler-utils, or xpdf.
    Default to using the one from poppler-utils (adds dependency on that).
    
    Bump PKGREVISION.

--- print/cups/options.mk
+++ print/cups/options.mk
@@ -1,9 +1,27 @@
-# $NetBSD: options.mk,v 1.2 2008/04/12 22:43:09 jlam Exp $
+# $NetBSD: options.mk,v 1.3 2009/06/03 10:20:59 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.cups
+PKG_OPTIONS_OPTIONAL_GROUPS=	pdftops
+PKG_OPTIONS_GROUP.pdftops=	gs poppler xpdf
 PKG_SUPPORTED_OPTIONS=	dnssd kerberos pam slp
-PKG_SUGGESTED_OPTIONS=	dnssd kerberos slp
+PKG_SUGGESTED_OPTIONS=	dnssd kerberos poppler slp
+
 .include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		pdftops
+
+.if !empty(PKG_OPTIONS:Mdnssd)
+.include "../../net/mDNSResponder/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mgs)
+PLIST.pdftops=	yes
+DEPENDS+=	ghostscript-[0-9]*:../../print/ghostscript
+.endif
+
+.if !empty(PKG_OPTIONS:Mkerberos)
+.include "../../mk/krb5.buildlink3.mk"
+.endif
 
 PLIST_VARS+=		pam
 .if !empty(PKG_OPTIONS:Mpam)
@@ -17,6 +35,11 @@ CONFIGURE_ARGS+=	--disable-pam
 MESSAGE_SRC=		${.CURDIR}/MESSAGE
 .endif
 
+.if !empty(PKG_OPTIONS:Mpoppler)
+PLIST.pdftops=	yes
+DEPENDS+=	poppler-utils-[0-9]*:../../print/poppler-utils
+.endif
+
 .if !empty(PKG_OPTIONS:Mslp)
 .include "../../net/openslp/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-slp
@@ -24,10 +47,7 @@ CONFIGURE_ARGS+=	--enable-slp
 CONFIGURE_ARGS+=	--disable-slp
 .endif
 
-.if !empty(PKG_OPTIONS:Mkerberos)
-.include "../../mk/krb5.buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mdnssd)
-.include "../../net/mDNSResponder/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mxpdf)
+PLIST.pdftops=	yes
+DEPENDS+=	xpdf-[0-9]*:../../print/xpdf
 .endif
