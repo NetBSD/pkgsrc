4527649fa720d46a750f8184765015269e29ff16
hasso <hasso@pkgsrc.org>
2009-06-04 07:54:26 +0000

    Make it build on DragonFly.

--- net/miniupnpd/options.mk
+++ net/miniupnpd/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2009/04/29 12:34:42 sborrill Exp $
+# $NetBSD: options.mk,v 1.4 2009/06/04 07:54:26 hasso Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.miniupnpd
 PKG_OPTIONS_REQUIRED_GROUPS=	miniupnpd-fw
@@ -7,7 +7,7 @@ PKG_OPTIONS_GROUP.miniupnpd-fw=	pf ipfilter
 .if ${OPSYS} == "NetBSD"
 PKG_SUGGESTED_OPTIONS+=		ipfilter
 .else
-.  if exists(/usr/include/net/pfvar.h)
+.  if exists(/usr/include/net/pfvar.h) || exists(/usr/include/net/pf/pfvar.h)
 PKG_SUGGESTED_OPTIONS+=		pf
 .  else
 PKG_SUGGESTED_OPTIONS+=		ipfilter
