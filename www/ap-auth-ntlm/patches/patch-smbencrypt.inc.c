bc31858f124c61f3fe9930b51d7c0341a2d3a970
hasso <hasso@pkgsrc.org>
2009-06-05 08:13:48 +0000

    Make it build on DragonFly.

--- www/ap-auth-ntlm/patches/patch-smbencrypt.inc.c
+++ www/ap-auth-ntlm/patches/patch-smbencrypt.inc.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-smbencrypt.inc.c,v 1.1.1.1 2008/10/17 19:23:25 sborrill Exp $
+$NetBSD: patch-smbencrypt.inc.c,v 1.2 2009/06/05 08:13:48 hasso Exp $
 --- smbval/smbencrypt.inc.c.orig	2003-06-05 20:21:26.000000000 +0100
 +++ smbval/smbencrypt.inc.c	2008-10-17 17:47:37.000000000 +0100
 @@ -19,7 +19,9 @@
  #include <arpa/inet.h>
  #include <dirent.h>
  #include <string.h>
-+#ifndef __NetBSD__
++#if !defined(__NetBSD__) && !defined(__DragonFly__)
  #include <sys/vfs.h>
 +#endif
  #include <netinet/in.h>
