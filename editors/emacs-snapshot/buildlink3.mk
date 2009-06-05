2cd1653143056bab46806188499cb844daf150f1
minskim <minskim>
2009-06-05 21:18:02 +0000

    Add emacs23 as a valid EMACS_TYPE.
    This addresses the issue reported in PR 40023.

---
+++ editors/emacs-snapshot/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2009/06/05 21:18:02 minskim Exp $
+
+BUILDLINK_TREE+=	emacs
+
+.if !defined(EMACS_BUILDLINK3_MK)
+EMACS_BUILDLINK3_MK:=
+
+.include "../../editors/emacs/modules.mk"
+BUILDLINK_API_DEPENDS.emacs+=	${_EMACS_REQD}
+BUILDLINK_PKGSRCDIR.emacs?=	${_EMACS_PKGDIR}
+
+BUILDLINK_CONTENTS_FILTER.emacs=	${EGREP} '.*\.el$$|.*\.elc$$'
+.endif # EMACS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-emacs
