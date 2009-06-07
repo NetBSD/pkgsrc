012d71f00fe57a3756e2971f13320bc83506d983
joerg <joerg@pkgsrc.org>
2009-06-07 21:12:11 +0000

    Update evas to the most recent e17 snapshot. No detailed changes known.

--- graphics/evas-software-x11/buildlink3.mk
+++ graphics/evas-software-x11/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:38 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/06/07 21:12:12 joerg Exp $
 
 BUILDLINK_TREE+=	evas-software-x11
 
 .if !defined(EVAS_SOFTWARE_X11_BUILDLINK3_MK)
 EVAS_SOFTWARE_X11_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.evas-software-x11+=	evas-software-x11>=0.9.9.050
+BUILDLINK_API_DEPENDS.evas-software-x11+=	evas-software-x11>=0.9.9.060
 BUILDLINK_PKGSRCDIR.evas-software-x11?=	../../graphics/evas-software-x11
 
 .include "../../x11/libX11/buildlink3.mk"
