3593ca2c4bc567ee6504d2ee2eafae6d003b2da4
joerg <joerg@pkgsrc.org>
2009-06-07 21:12:11 +0000

    Update evas to the most recent e17 snapshot. No detailed changes known.

--- graphics/evas-buffer/buildlink3.mk
+++ graphics/evas-buffer/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:38 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/06/07 21:12:11 joerg Exp $
 
 BUILDLINK_TREE+=	evas-buffer
 
 .if !defined(EVAS_BUFFER_BUILDLINK3_MK)
 EVAS_BUFFER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.evas-buffer+=	evas-buffer>=0.9.9.050
+BUILDLINK_API_DEPENDS.evas-buffer+=	evas-buffer>=0.9.9.060
 BUILDLINK_PKGSRCDIR.evas-buffer?=	../../graphics/evas-buffer
 .endif # EVAS_BUFFER_BUILDLINK3_MK
 
