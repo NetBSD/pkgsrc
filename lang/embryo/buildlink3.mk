a87144372171ce0a8f7b7a66861dd88d908a8b86
joerg <joerg@pkgsrc.org>
2009-06-07 19:54:02 +0000

    Bump required version for consistency in the E17 snapshot.

--- lang/embryo/buildlink3.mk
+++ lang/embryo/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:48 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/06/07 19:54:02 joerg Exp $
 
 BUILDLINK_TREE+=	embryo
 
 .if !defined(EMBRYO_BUILDLINK3_MK)
 EMBRYO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.embryo+=	embryo>=0.9.5.050
+BUILDLINK_API_DEPENDS.embryo+=	embryo>=0.9.5.060
 BUILDLINK_PKGSRCDIR.embryo?=	../../lang/embryo
 .endif # EMBRYO_BUILDLINK3_MK
 
