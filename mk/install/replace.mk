f1e25d0130d29ea2f163307d3ffdc66521d6e887
joerg <joerg@pkgsrc.org>
2009-06-09 08:40:27 +0000

    Add support for using USE_DESTDIR=yes and replace together. Based on the patch send to tech-pkg in 2008, but less intrusive.

--- mk/install/replace.mk
+++ mk/install/replace.mk
@@ -1,4 +1,4 @@
-# $NetBSD: replace.mk,v 1.10 2007/03/09 03:28:58 rillig Exp $
+# $NetBSD: replace.mk,v 1.11 2009/06/09 08:40:28 joerg Exp $
 #
 # Public targets:
 #
@@ -14,12 +14,19 @@
 # Private targets that must be defined by the package system flavor:
 #
 # _flavor-replace:
-#	Updates a package in-place on the system.
+#	Updates a package in-place on the system (USE_DESTDIR=no).
+#
+# _flavor-destdir-replace:
+#	Updates a package in-place on the system (USE_DESTDIR=yes).
 #
 # _flavor-undo-replace:
 #	Undoes a previous "make _flavor-replace".
 
+.if ${_USE_DESTDIR} == "no"
 _REPLACE_TARGETS+=	${_PKGSRC_BUILD_TARGETS}
+.else
+_REPLACE_TARGETS+=	package
+.endif
 _REPLACE_TARGETS+=	replace-message
 _REPLACE_TARGETS+=	unprivileged-install-hook
 
@@ -38,7 +45,11 @@ replace-message: .PHONY
 	@${PHASE_MSG} "Replacing for ${PKGNAME}"
 	@${WARNING_MSG} "experimental target - DATA LOSS MAY OCCUR."
 
+.if ${_USE_DESTDIR} == "no"
 su-replace: .PHONY _flavor-replace
+.else
+su-replace: .PHONY _flavor-destdir-replace
+.endif
 MAKEFLAGS.su-replace=	_UPDATE_RUNNING=yes
 
 #
