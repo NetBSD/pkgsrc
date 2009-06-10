25f837da6cd4053d5c87dda754ae8f633edaa961
minskim <minskim>
2009-06-10 06:40:11 +0000

    Update eet to 1.2.0.
    Changes:
    	* Make use of eina.
    	* Fix string in list and hash.
    	* Fix array in eet_data.
    	* Add crypto support to eet with OpenSSL.
    	* Add GNUtls support to eet.
    	* Make password callback work with GNUtls.
    	* Add a function to retrieve raw signature.
    	* Fix a corrupted pointer use in eet_cipher.c
    	* Add some missing __UNUSED__ flags.
    	* Fix problem reported by llvm
    	* Add sha1 retrieval for an Eet_File.
    	* Force fsync() after data is written to file, solve ext4 issues.
    	* Disable fsync. Edit the code if you need it.
    	* Make eet_data thread safe.
    	* Fix eet pkg-config dependencies.
    	* Fix double init of gcry.
    	* Release eet 1.2.0

--- devel/eet/buildlink3.mk
+++ devel/eet/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:24:12 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2009/06/10 06:40:11 minskim Exp $
 
 BUILDLINK_TREE+=	eet
 
@@ -8,6 +8,7 @@ EET_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.eet+=	eet>=1.0.1
 BUILDLINK_PKGSRCDIR.eet?=	../../devel/eet
 
+.include "../../devel/eina/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../graphics/jpeg/buildlink3.mk"
 .endif # EET_BUILDLINK3_MK
