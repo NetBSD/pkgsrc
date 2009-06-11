b53d4f58ec643b1f3989e71c4d26b9fbffaee56b
wiz <wiz@pkgsrc.org>
2009-06-11 08:09:28 +0000

    Update to 0.20.5:
    0.20.5
    =======
    - Followup release to undo unintended .soname bump

--- x11/vte/buildlink3.mk
+++ x11/vte/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2009/06/04 11:30:15 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2009/06/11 08:09:28 wiz Exp $
 
 BUILDLINK_TREE+=	vte
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	vte
 VTE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.vte+=	vte>=0.14.0
-BUILDLINK_ABI_DEPENDS.vte?=	vte>=0.20.4
+BUILDLINK_ABI_DEPENDS.vte?=	vte>=0.20.5
 BUILDLINK_PKGSRCDIR.vte?=	../../x11/vte
 
 .include "../../devel/glib2/buildlink3.mk"
