2848bb494aeece460acfd4905c13592b2423e417
wiz <wiz@pkgsrc.org>
2009-06-11 08:10:25 +0000

    Bump PKGREVISION for vte-0.20.5.

--- x11/gnome-desktop-sharp/buildlink3.mk
+++ x11/gnome-desktop-sharp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/03/25 22:37:15 he Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/06/11 08:10:25 wiz Exp $
 
 BUILDLINK_TREE+=	gnome-desktop-sharp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gnome-desktop-sharp
 GNOME_DESKTOP_SHARP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gnome-desktop-sharp+=	gnome-desktop-sharp>=2.24.0
-BUILDLINK_ABI_DEPENDS.gnome-desktop-sharp?=	gnome-desktop-sharp>=2.26.0nb1
+BUILDLINK_ABI_DEPENDS.gnome-desktop-sharp?=	gnome-desktop-sharp>=2.26.0nb5
 BUILDLINK_PKGSRCDIR.gnome-desktop-sharp?=	../../x11/gnome-desktop-sharp
 
 .include "../../devel/libwnck/buildlink3.mk"
