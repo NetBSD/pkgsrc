658d6f64dadf9dadddd7ddb66b00714da02e84a8
ahoka <ahoka@pkgsrc.org>
2009-06-12 17:24:53 +0000

    Bump for ffmpeg update.

--- x11/nucleo/buildlink3.mk
+++ x11/nucleo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/03/20 19:25:48 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/06/12 17:24:54 ahoka Exp $
 
 BUILDLINK_TREE+=	nucleo
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	nucleo
 NUCLEO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.nucleo+=	nucleo>=0.6
-BUILDLINK_ABI_DEPENDS.nucleo?=	nucleo>=0.7.1nb2
+BUILDLINK_ABI_DEPENDS.nucleo?=	nucleo>=0.7.1nb4
 BUILDLINK_PKGSRCDIR.nucleo?=	../../x11/nucleo
 pkgbase:=nucleo
 .  include "../../mk/pkg-build-options.mk"
