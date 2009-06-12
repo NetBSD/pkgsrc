b5af3066a1ab9494666d3b0e6c500206c69edc26
jnemeth <jnemeth@pkgsrc.org>
2009-06-12 09:04:55 +0000

    Add Asterisk 1.6.0.10.  At the moment, this version doesn't have any hardware support, so it can't replace comms/asterisk.  However, apparently there is demand for this version, so wiz@ suggested it be imported here into comms/asterisk16.  The latest version is 1.6.1.1, but I won't have time to update all the patches before the freeze.  I'll update to that version sometime after the freeze when I get a chance.

---
+++ comms/asterisk16/options.mk
@@ -0,0 +1,71 @@
+# $NetBSD: options.mk,v 1.1.1.1 2009/06/12 09:04:56 jnemeth Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.asterisk
+PKG_SUPPORTED_OPTIONS=		zaptel x11 unixodbc ilbc
+PKG_OPTIONS_LEGACY_OPTS+=	gtk:x11
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		zaptel x11 unixodbc ilbc
+
+# Asterisk now uses DAHDI, not zaptel; not implemented yet...
+#.if !empty(PKG_OPTIONS:Mzaptel)
+## zaptel only supported under NetBSD at the moment
+#.  include "../../comms/zaptel-netbsd/buildlink3.mk"
+#PLIST.zaptel=		yes
+#.else
+#MAKE_FLAGS+=		WITHOUT_ZAPTEL=1
+#.endif
+
+# gtkconsole depends on GTK 1.x
+.if !empty(PKG_OPTIONS:Mx11)
+.  include "../../x11/gtk/buildlink3.mk"
+.  include "../../devel/SDL/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-sdl
+CONFIGURE_ARGS+=	--with-gtk
+PLIST.x11=		yes
+.else
+CONFIGURE_ARGS+=	--without-sdl
+CONFIGURE_ARGS+=	--without-gtk
+.endif
+
+.if !empty(PKG_OPTIONS:Munixodbc)
+.  include "../../databases/unixodbc/buildlink3.mk"
+.  include "../../devel/libltdl/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-ltdl
+CONFIGURE_ARGS+=	--with-odbc
+PLIST.unixodbc=		yes
+.else
+CONFIGURE_ARGS+=	--without-ltdl
+CONFIGURE_ARGS+=	--without-odbc
+.endif
+
+.if !empty(PKG_OPTIONS:Milbc)
+DISTFILES=		${DEFAULT_DISTFILES}
+DISTFILES+=		rfc3951.txt
+SITES.rfc3951.txt=	http://www.ietf.org/rfc/
+DISTFILES+=		extract-cfile.awk
+SITES.extract-cfile.awk=	http://www.ilbcfreeware.org/documentation/
+USE_TOOLS+=		awk
+PLIST.ilbc=		yes
+.endif
+
+post-configure:
+.if !empty(PKG_OPTIONS:Mx11)
+	${ECHO} "MENUSELECT_PBX=-pbx_gtkconsole" >> ${WRKSRC}/pkgsrc.makeopts
+.endif
+.if !empty(PKG_OPTIONS:Munixodbc)
+	${ECHO} "MENUSELECT_OPTS_app_voicemail=ODBC_STORAGE" >> ${WRKSRC}/pkgsrc.makeopts
+.endif
+.if !empty(PKG_OPTIONS:Milbc)
+	${ECHO} "MENUSELECT_CODECS=-codec_ilbc" >> ${WRKSRC}/pkgsrc.makeopts
+.endif
+# this is a hack to work around a bug in menuselect
+	cd ${WRKSRC} && make menuselect.makeopts
+
+post-extract:
+.if !empty(PKG_OPTIONS:Milbc)
+	cp ${DISTDIR}/${DIST_SUBDIR}/rfc3951.txt ${WRKSRC}/codecs/ilbc
+	cp ${DISTDIR}/${DIST_SUBDIR}/extract-cfile.awk ${WRKSRC}/codecs/ilbc
+	cd ${WRKSRC}/codecs/ilbc && ${AWK} -f extract-cfile.awk < rfc3951.txt
+.endif
