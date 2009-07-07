2ab271a1e3c09cebff8bdfa7ef0262e874b785cf
drochner <drochner@pkgsrc.org>
2009-07-07 17:49:00 +0000

    update to 3.0.2 sorry, didn't find a changelog pkgsrc change: pull in libdiscid, makes CD lookup work (eg in picard)

--- audio/libmusicbrainz/buildlink3.mk
+++ audio/libmusicbrainz/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/04/16 21:42:41 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/07/07 17:49:00 drochner Exp $
 
 BUILDLINK_TREE+=	libmusicbrainz
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.libmusicbrainz+=	libmusicbrainz>=3.0.0
 BUILDLINK_ABI_DEPENDS.libmusicbrainz?=	libmusicbrainz>=3.0.0nb2
 BUILDLINK_PKGSRCDIR.libmusicbrainz?=	../../audio/libmusicbrainz
 
+.include "../../audio/libdiscid/buildlink3.mk"
 .include "../../www/neon/buildlink3.mk"
 .endif # LIBMUSICBRAINZ_BUILDLINK3_MK
 
