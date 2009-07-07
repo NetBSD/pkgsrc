5f9743fbe36f62d881fae58de5f8934b3fa41b50
obache <obache@pkgsrc.org>
2009-07-07 10:44:18 +0000

    Added minimum buildlink found in lib/libpulse.la (not all libpulse*.la). Fixes PR#41625.

--- audio/pulseaudio/buildlink3.mk
+++ audio/pulseaudio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:23:58 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/07/07 10:44:18 obache Exp $
 
 BUILDLINK_TREE+=	pulseaudio
 
@@ -8,7 +8,17 @@ PULSEAUDIO_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.pulseaudio+=	pulseaudio>=0.9.13
 BUILDLINK_PKGSRCDIR.pulseaudio?=	../../audio/pulseaudio
 
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} == "NetBSD" && !exists(/usr/include/sys/atomic.h)
+.include "../../devel/libatomic_ops/buildlink3.mk"
+.endif
+
+.include "../../databases/gdbm/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/libICE/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
 .endif # PULSEAUDIO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-pulseaudio
