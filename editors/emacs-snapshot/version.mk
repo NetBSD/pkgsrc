06c1d6e3b06c5350248ec1ce60338f7d4dcbb57a
minskim <minskim@pkgsrc.org>
2009-07-08 13:32:39 +0000

    Update emacs-snapshot to 23.0.95.20090703.
    Changes:
    * configure.in: Restore netbsd on mips, mipsel, mipseb.
    * make-dist
      (nextstep/Cocoa/Emacs.base/Contents/Resources/preferences.nib)
      (nextstep/GNUstep/Emacs.base/Resources/preferences.gorm):
      No longer make links.
    * make-dist (tempdir): Don't create directories preferences.gorm
      and preferences.nib, they are no longer required.
    * configure.in: Delete mac-fix-env target, which has been
      removed (Bug#3531).

--- editors/emacs-snapshot/version.mk
+++ editors/emacs-snapshot/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.2 2009/06/06 05:06:12 minskim Exp $
+# $NetBSD: version.mk,v 1.3 2009/07/08 13:32:39 minskim Exp $
 
 _EMACS_FLAVOR=	emacs
 _EMACS_REQD=	emacs>=23
@@ -7,4 +7,4 @@ _EMACS_PKGDEP.leim=
 
 _EMACS_VERSION_MAJOR=	23
 _EMACS_VERSION_MINOR=	0
-_EMACS_VERSION_MICRO=	94
+_EMACS_VERSION_MICRO=	95
