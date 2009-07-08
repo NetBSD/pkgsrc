809cf78ebb23dc9389c3c9a5167a3909567527a3
joerg <joerg@pkgsrc.org>
2009-07-08 13:55:58 +0000

    Provide PY_COMPILE_ALL and PY_COMPILE_O_ALL to compile all Python sources in a directory (tree) in pversion.mk. Adjust the various places that called it locally.

--- lang/python/pyversion.mk
+++ lang/python/pyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: pyversion.mk,v 1.72 2009/07/02 09:37:12 joerg Exp $
+# $NetBSD: pyversion.mk,v 1.73 2009/07/08 13:55:59 joerg Exp $
 
 # This file determines which Python version is used as a dependency for
 # a package.
@@ -156,6 +156,10 @@ BUILDLINK_DEPMETHOD.python?=	build
 .endif
 
 PYTHONBIN=	${LOCALBASE}/bin/python${PYVERSSUFFIX}
+PY_COMPILE_ALL= \
+	${PYTHONBIN} ${PREFIX}/lib/python${PYVERSSUFFIX}/compileall.py -q
+PY_COMPILE_O_ALL= \
+	${PYTHONBIN} -O ${PREFIX}/lib/python${PYVERSSUFFIX}/compileall.py -q
 
 .if exists(${PYTHONBIN})
 PYINC!=	${PYTHONBIN} -c "import distutils.sysconfig; \
--- www/zope/zopeversion.mk
+++ www/zope/zopeversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: zopeversion.mk,v 1.11 2009/06/14 22:58:11 joerg Exp $
+# $NetBSD: zopeversion.mk,v 1.12 2009/07/08 13:56:00 joerg Exp $
 #
 
 .if !defined(_ZOPEVERSION_MK)
@@ -206,7 +206,5 @@ PRINT_PLIST_AWK+=	/^${ZOPE_BASE:S|/|\\/|g}/ \
 
 #
 ZOPE_PY_COMPILE= ${PYTHONBIN} ${ZOPE_DIR}/bin/compilezpy.py
-PY_COMPILE_ALL= \
-	${PYTHONBIN} ${PREFIX}/lib/python${PYVERSSUFFIX}/compileall.py -q
 
 .endif
