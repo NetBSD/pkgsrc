c8a7d69055e4f8e4c3d94583ab1df1c0fe1e1c80
minskim <minskim@pkgsrc.org>
2009-07-08 13:27:52 +0000

    Uncompress .xz using xzcat.

--- mk/extract/bsd.extract-vars.mk
+++ mk/extract/bsd.extract-vars.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.extract-vars.mk,v 1.11 2008/11/15 20:25:34 minskim Exp $
+# $NetBSD: bsd.extract-vars.mk,v 1.12 2009/07/08 13:27:52 minskim Exp $
 #
 # This Makefile fragment is included separately by bsd.pkg.mk and
 # defines some variables which must be defined earlier than where
@@ -55,6 +55,9 @@ USE_TOOLS+=	bzcat
 .endif
 .if !empty(EXTRACT_ONLY:M*.lzma)
 USE_TOOLS+=	lzcat
+.endif
+.if !empty(EXTRACT_ONLY:M*.xz)
+USE_TOOLS+=	xzcat
 .endif
 .if !empty(EXTRACT_ONLY:M*.zip)
 USE_TOOLS+=	unzip
