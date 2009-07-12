1cd1568b2142ecf30e9aead74c4513a75ec4ae7d
minskim <minskim@pkgsrc.org>
2009-07-12 11:56:54 +0000

    Update emacs-snapshot to 23.0.96.20090710.
    Nothing notable in ChangeLog.  The minor version number was incremented.

--- editors/emacs-snapshot/version.mk
+++ editors/emacs-snapshot/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.3 2009/07/08 13:32:39 minskim Exp $
+# $NetBSD: version.mk,v 1.4 2009/07/12 11:56:54 minskim Exp $
 
 _EMACS_FLAVOR=	emacs
 _EMACS_REQD=	emacs>=23
@@ -7,4 +7,4 @@ _EMACS_PKGDEP.leim=
 
 _EMACS_VERSION_MAJOR=	23
 _EMACS_VERSION_MINOR=	0
-_EMACS_VERSION_MICRO=	95
+_EMACS_VERSION_MICRO=	96
