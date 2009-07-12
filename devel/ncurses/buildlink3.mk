dafd104f230f155a6705dff8f258ba64efff0238
wiz <wiz@pkgsrc.org>
2009-07-12 17:41:25 +0000

    Fix typos in comment.

--- devel/ncurses/buildlink3.mk
+++ devel/ncurses/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.34 2009/07/12 17:23:13 ahoka Exp $
+# $NetBSD: buildlink3.mk,v 1.35 2009/07/12 17:41:25 wiz Exp $
 
 BUILDLINK_TREE+=	ncurses
 
@@ -14,7 +14,7 @@ BUILDLINK_LDADD.ncurses?=	${BUILDLINK_LIBNAME.ncurses:S/^/-l/:S/^-l$//}
 
 # Many packages expect the ncurses headers and libraries to be usable as
 # <curses.h> and -lcurses and they often only look in include/ rather than
-# some properly written autoconf macros wich also try to puck up
+# some properly written autoconf macros which also try to pick up
 # ncurses/ncurses.h.
 #
 BUILDLINK_TARGETS+=		buildlink-ncurses-curses-h buildlink-ncurses-ncurses-h
