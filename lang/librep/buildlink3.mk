ae92c10d549628e45cf09f9687246ddc6c65f86c
tnn <tnn@pkgsrc.org>
2009-07-14 10:01:28 +0000

    Update to librep-0.90.0. Changes: * Added UTF-8 Support! [Wang Diancheng] * Remove scheme and unscheme modules * Going on with code-cleanup

--- lang/librep/buildlink3.mk
+++ lang/librep/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/03/20 19:24:50 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2009/07/14 10:01:28 tnn Exp $
 
 BUILDLINK_TREE+=	librep
 
 .if !defined(LIBREP_BUILDLINK3_MK)
 LIBREP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.librep+=	librep>=0.17
-BUILDLINK_ABI_DEPENDS.librep+=	librep>=0.17nb2
+BUILDLINK_API_DEPENDS.librep+=	librep>=0.90.0
 BUILDLINK_PKGSRCDIR.librep?=	../../lang/librep
 MAKE_FLAGS+=			rep_LIBTOOL=${LIBTOOL:Q}
 
