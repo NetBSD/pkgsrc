7e1d430d439ae30a0cde07cba292f8647b7c31e4
tonio <tonio@pkgsrc.org>
2009-07-15 22:14:18 +0000

    Add rc.d file for bitlbee, so that it can be used in deamon mode Use gnutls instead of openssl, as google talk does not work with openssl
    This fixes PR pkg/35345

---
+++ chat/bitlbee/files/bitlbee.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# PROVIDE: bitlbee
+# REQUIRE: DAEMON
+
+. /etc/rc.subr
+
+name="bitlbee"
+rcvar=$name
+command="@PREFIX@/sbin/bitlbee"
+pidfile="/var/run/${name}.pid"
+command_args="-D"	# run as a daemon
+
+if [ -f /etc/rc.subr ]; then
+	load_rc_config $name
+	run_rc_command "$1"
+else
+	echo -n "${name}"
+	${command} ${bitlbee_flags} ${command_args}
+fi
