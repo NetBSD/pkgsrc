04219641db7dd815dd058bf4867fe6f4ca165901
kefren <kefren@pkgsrc.org>
2009-07-15 11:37:16 +0000

    Update to 2.4.2.1 This version is the first version to integrate Microsoft's open source ASP.NET MVC stack. More than 140 bug fixed. Check http://www.mono-project.com/Release_Notes_Mono_2.4.2 for more changes.

--- lang/mono/buildlink3.mk
+++ lang/mono/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.33 2009/06/14 22:58:02 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2009/07/15 11:37:16 kefren Exp $
 
 BUILDLINK_TREE+=	mono
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.mono+=	mono>=2.2
 BUILDLINK_ABI_DEPENDS.mono?=	mono>=2.2nb4
 BUILDLINK_PKGSRCDIR.mono?=	../../lang/mono
 ALL_ENV+=			MONO_SHARED_DIR=${WRKDIR:Q}
+BUILDLINK_CONTENTS_FILTER.mono+=	${EGREP} '(^include/|^lib/)'
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../textproc/icu/buildlink3.mk"
