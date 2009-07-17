20927adaf8232573a08cbdf6b6ff074c2d44b1ef
sno <sno@pkgsrc.org>
2009-07-17 06:44:46 +0000

    adjust PERL5_LICENSE - enable both valid licenses

--- lang/perl5/license.mk
+++ lang/perl5/license.mk
@@ -1,4 +1,4 @@
-# $NetBSD: license.mk,v 1.1 2009/06/11 10:35:07 sno Exp $
+# $NetBSD: license.mk,v 1.2 2009/07/17 06:44:46 sno Exp $
 #
 # This Makefile fragment is intended to be included by packages that build
 # and install perl5 modules and by the perl5 package.
@@ -9,4 +9,4 @@
 #			Allowed values: gnu-gpl-v2 and artistic until
 #			pkgsrc can handle dual licenses correctly.
 #
-PERL5_LICENSE?=		gnu-gpl-v2 # OR artistic
+PERL5_LICENSE?=		gnu-gpl-v2 OR artistic
