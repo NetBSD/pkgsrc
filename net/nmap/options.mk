be22734f1c08627ab6b947f5da5bbcd74d237995
adrianp <adrianp@pkgsrc.org>
2009-07-20 19:40:08 +0000

    Update to 5.00 Fix for PR#41506 Fix missing @dirrm entries from PLIST*
    Before we go into the detailed changes, here are the top 5 improvements in Nmap 5:
    1. The new Ncat tool aims to be your Swiss Army Knife for data transfer, redirection, and debugging. We released a whole users' guide detailing security testing and network administration tasks made easy with Ncat.
    2. The addition of the Ndiff scan comparison tool completes Nmap's growth into a whole suite of applications which work together to serve network administrators and security practitioners. Ndiff makes it easy to automatically scan your network daily and report on any changes (systems coming up or going down or changes to the software services they are running). The other two tools now packaged with Nmap itself are Ncat and the much improved Zenmap GUI and results viewer.
    3. Nmap performance has improved dramatically. We spent last summer scanning much of the Internet and merging that data with internal enterprise scan logs to determine the most commonly open ports. This allows Nmap to scan fewer ports by default while finding more open ports. We also added a fixed-rate scan engine so you can bypass Nmap's congestion control algorithms and scan at exactly the rate (packets per second) you specify.
    4. We released Nmap Network Scanning, the official Nmap guide to network discovery and security scanning. From explaining port scanning basics for novices to detailing low-level packet crafting methods used by advanced hackers, this book suits all levels of security and networking professionals. A 42-page reference guide documents every Nmap feature and option, while the rest of the book demonstrates how to apply those features to quickly solve real-world tasks. More than half the book is available in the free online edition.
    5. The Nmap Scripting Engine (NSE) is one of Nmap's most powerful and flexible features. It allows users to write (and share) simple scripts to automate a wide variety of networking tasks. Those scripts are then executed in parallel with the speed and efficiency you expect from Nmap. All existing scripts have been improved, and 32 new ones added. New scripts include a whole bunch of MSRPC/NetBIOS attacks, queries, and vulnerability probes; open proxy detection; whois and AS number lookup queries; brute force attack scripts against the SNMP and POP3 protocols; and many more. All NSE scripts and modules are described in the new NSE documentation portal.
    
    Details are here: http://nmap.org/changelog.html

--- net/nmap/options.mk
+++ net/nmap/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2009/01/15 20:28:31 shattered Exp $
+# $NetBSD: options.mk,v 1.3 2009/07/20 19:40:08 adrianp Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nmap
 
-PKG_SUPPORTED_OPTIONS=	zenmap inet6
+PKG_SUPPORTED_OPTIONS=	zenmap inet6 ndiff
 
 .include "../../mk/bsd.options.mk"
 
@@ -26,6 +26,15 @@ PY_PATCHPLIST=			yes
 .include "../../x11/py-gtk2/buildlink3.mk"
 .include "../../databases/py-sqlite2/buildlink3.mk"
 .include "../../textproc/py-xml/buildlink3.mk"
+.include "../../sysutils/desktop-file-utils/desktopdb.mk"
 .else
 CONFIGURE_ARGS+=	--without-zenmap
+.endif
+
+.if !empty(PKG_OPTIONS:Mndiff)
+CONFIGURE_ARGS+=		--with-ndiff
+PLIST_SRC+=			${PKGDIR}/PLIST.ndiff
+PYTHON_VERSIONS_ACCEPTED=	25 24
+.else
+CONFIGURE_ARGS+=		--without-ndiff
 .endif
