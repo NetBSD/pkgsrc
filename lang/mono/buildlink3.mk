bfae4c0b3447828d7a2800da0a4a046ace6dd971
tron <tron@pkgsrc.org>
2009-07-21 10:50:58 +0000

    Pullup ticket #2826 - requested by kefren mono: security update
    Revisions pulled up:
    - lang/mono/Makefile			1.89-1.90
    - lang/mono/PLIST.common		1.6
    - lang/mono/buildlink3.mk		1.34
    - lang/mono/distinfo			1.52-1.53
    - lang/mono/patches/patch-dj		1.1
    ---
    Module Name:	pkgsrc
    Committed By:	kefren
    Date:		Wed Jul 15 11:37:16 UTC 2009
    
    Modified Files:
    	pkgsrc/lang/mono: Makefile PLIST.common buildlink3.mk distinfo
    
    Log Message:
    Update to 2.4.2.1
    This version is the first version to integrate Microsoft's open source
    ASP.NET MVC stack. More than 140 bug fixed. Check
    http://www.mono-project.com/Release_Notes_Mono_2.4.2 for more changes.
    ---
    Module Name:	pkgsrc
    Committed By:	hasso
    Date:		Fri Jul 17 02:53:52 UTC 2009
    
    Modified Files:
    	pkgsrc/lang/mono: distinfo
    Added Files:
    	pkgsrc/lang/mono/patches: patch-dj
    
    Log Message:
    Make it build again on DragonFly.
    ---
    Module Name:	pkgsrc
    Committed By:	kefren
    Date:		Sun Jul 19 07:53:53 UTC 2009
    
    Modified Files:
    	pkgsrc/lang/mono: Makefile distinfo
    
    Log Message:
    Update to 2.4.2.2 - Contains fixes for CVE-2009-0217

--- lang/mono/buildlink3.mk
+++ lang/mono/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.33 2009/06/14 22:58:02 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.33.2.1 2009/07/21 10:50:58 tron Exp $
 
 BUILDLINK_TREE+=	mono
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.mono+=	mono>=2.2
 BUILDLINK_ABI_DEPENDS.mono?=	mono>=2.2nb4
 BUILDLINK_PKGSRCDIR.mono?=	../../lang/mono
 ALL_ENV+=			MONO_SHARED_DIR=${WRKDIR:Q}
+BUILDLINK_CONTENTS_FILTER.mono+=	${EGREP} '(^include/|^lib/)'
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../textproc/icu/buildlink3.mk"
