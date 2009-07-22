96327489c21325ac521c224664976fe0eb8087e8
manu <manu@pkgsrc.org>
2009-07-22 15:59:50 +0000

    Add exop patch from Pierangelo Masarati and myself.
    This enables the folllowing extended operations:
    - passwd
    - whoami
    - refresh
    And the following controls:
    - manageDSAit
    - paged_results
    - ppolicy
    - noop
    - manageDIT
    - permissive_modify

--- databases/php-ldap/options.mk
+++ databases/php-ldap/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1 2009/05/29 21:23:58 manu Exp $
+# $NetBSD: options.mk,v 1.2 2009/07/22 15:59:50 manu Exp $
 
 PKG_OPTIONS_VAR=        PKG_OPTIONS.php-ldap
-PKG_SUPPORTED_OPTIONS=  sasl
+PKG_SUPPORTED_OPTIONS=  sasl exop
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,4 +11,10 @@ USE_BUILTIN.openldap-client=no
 .  include "../../databases/openldap-client/buildlink3.mk"
 .  include "../../security/cyrus-sasl/buildlink3.mk"
 CONFIGURE_ARGS+= --with-ldap-sasl=${PREFIX}
+.endif
+
+.if !empty(PKG_OPTIONS:Mexop)
+EXOP_PATCH=	${FILESDIR}/ldap-ctrl-exop.patch
+post-patch:
+	${PATCH} -d ${WRKSRC} -p2 --forward --quiet  < ${EXOP_PATCH}
 .endif
