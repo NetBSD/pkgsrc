2275da7184ba633744eab2ca5897a9db1eec9c3c
wiz <wiz>
2009-07-22 19:38:34 +0000

    Fix typo in comment.

--- graphics/graphviz/options.mk
+++ graphics/graphviz/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.7 2009/07/22 12:35:29 gdt Exp $
+# $NetBSD: options.mk,v 1.8 2009/07/22 19:38:34 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.graphviz
 PKG_SUPPORTED_OPTIONS=	gd x11 pangocairo gtk rsvg swig guile lua ocaml tcl perl
 PKG_SUGGESTED_OPTIONS=	gd x11 pangocairo gtk rsvg swig lua tcl perl
 # Explanation of consquence of options, to help those trying to slim down:
-#   swig: build-time only, needed for any of the lanaguages.  Silently
+#   swig: build-time only, needed for any of the languages.  Silently
 #     forces off language options.
 #   ocaml lua tcl perl: extension language support
 #   x11: Omits all linking with x11.  Silently forces off pangocairo, gtk
