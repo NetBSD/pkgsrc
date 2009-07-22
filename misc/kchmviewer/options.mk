221191426bc16231f76b30b70c757ffd8702f9b6
wiz <wiz@pkgsrc.org>
2009-07-22 13:10:22 +0000

    Update to 4.1. Set LICENSE. Fix a pkglint warning.     - Version changed to 4.1     - Updated Dutch translation (thanks to Sander Pientka)     - Updated Turkish translation (thanks to Ozan Caglayan)     - Default toolbar button order changed to standard -/+     - Automatic encoding detection for KHTMLPart/WebKit is now disabled by default.     - Fixed incorrect index string encoding for non-English strings.     - Fixed text size auto-increase each time the file is loaded for WebKit browser.     - License changed from GPLv2 to GPLv3.

--- misc/kchmviewer/options.mk
+++ misc/kchmviewer/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2009/01/28 06:35:46 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2009/07/22 13:10:22 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kchmviewer
 PKG_SUPPORTED_OPTIONS=	kde4
@@ -14,11 +14,13 @@ BUILD_DEPENDS+=		automoc4-[0-9]*:../../devel/automoc4
 .include "../../wip/kde4/kde4.mk"
 PLIST.kde4=		yes
 .else
+
+INSTALLATION_DIRS=	bin
+
 do-build:
 	cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} ${QTDIR}/bin/qmake
 	cd ${WRKSRC} && ${SETENV} ${MAKE_ENV} ${MAKE}
 
 do-install:
-	${INSTALL_PROGRAM_DIR} ${DESTDIR}${PREFIX}/bin
 	${INSTALL_PROGRAM} ${WRKSRC}/bin/kchmviewer ${DESTDIR}${PREFIX}/bin
 .endif
