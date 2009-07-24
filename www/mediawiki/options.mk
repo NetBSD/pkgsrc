ea5c062b88471fee49aec28c58ffacde89661d07
abs <abs@pkgsrc.org>
2009-07-24 11:44:51 +0000

    fix borked options file

--- www/mediawiki/options.mk
+++ www/mediawiki/options.mk
@@ -1,6 +1,6 @@
-# $NetBSD: options.mk,v 1.1 2009/07/23 17:34:09 abs Exp $
+# $NetBSD: options.mk,v 1.2 2009/07/24 11:44:51 abs Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.bugzilla
+PKG_OPTIONS_VAR=	PKG_OPTIONS.mediawiki
 
 PKG_OPTIONS_REQUIRED_GROUPS=	db
 PKG_OPTIONS_GROUP.db=		mysql pgsql
@@ -16,5 +16,5 @@ PKG_SUGGESTED_OPTIONS=	mysql
 .if !empty(PKG_OPTIONS:Mmysql)
 DEPENDS+=	${PHP_PKG_PREFIX}-mysql>=4.3.10:../../databases/php-mysql
 .elif !empty(PKG_OPTIONS:Mpgsql)
-DEPENDS+=	${PHP_PKG_PREFIX}-pgsql>=8:../../databases/php-pgsql
+DEPENDS+=	${PHP_PKG_PREFIX}-pgsql>=5:../../databases/php-pgsql
 .endif
