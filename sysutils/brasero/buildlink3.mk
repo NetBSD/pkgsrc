423be74e07a7edaa95d6081ceec33e836864ab6f
drochner <drochner>
2009-07-26 18:25:01 +0000

    update brasero to 2.26.3 changes: minor bugfixes, translation updates
    pkgsrc changes:
    -in scsi io code, remove the code which builds a raw device path
     from a block device - use the raw_device hal property instead
    -split out the nautilus plugin, allows to use brasero (and its
     clients, eg sound-juicer) with less gnome desktop overhead
    -support playlist parsing as an option -- unfortunately this
     pulls in evolution-data-server indirectly which is pretty large
     for that limited gain of functionality (on per default, as in
     other pkgs able to use the evolution database)
    -cleanup

--- sysutils/brasero/buildlink3.mk
+++ sysutils/brasero/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:23 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/07/26 18:25:01 drochner Exp $
 
 BUILDLINK_TREE+=	brasero
 
@@ -7,6 +7,11 @@ BRASERO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.brasero+=	brasero>=2.26.0
 BUILDLINK_PKGSRCDIR.brasero?=	../../sysutils/brasero
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../sysutils/hal/buildlink3.mk"
+
 .endif # BRASERO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-brasero
