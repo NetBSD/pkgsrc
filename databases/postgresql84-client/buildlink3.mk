f294a0946135f7e1383883ddbaca88429ffd51b3
adam <adam@pkgsrc.org>
2009-07-29 06:26:17 +0000

    Many of the changes in PostgreSQL 8.4 are new or improved administration and monitoring tools and commands. Each user has their own favorite features which will make day-to-day work with PostgreSQL easier and more productive for them. Among the most popular enhancements are:
    * Parallel Database Restore, speeding up recovery from backup up to 8 times
    * Per-Column Permissions, allowing more granular control of sensitive data
    * Per-database Collation Support, making PostgreSQL more useful in
      multi-lingual environments
    * In-place Upgrades through pg_migrator (beta), enabling upgrades from 8.3 to
      8.4 without extensive downtime
    * New Query Monitoring Tools, giving administrators more insight into query
      activity
    * Greatly Reduced VACUUM Overhead through the Visibility Map
    * New Monitoring Tools for current queries, query load and deadlocks
    
    Version 8.4 also makes data analysis easier through the advanced ANSI SQL2003
    features of windowing functions, common table expressions and recursive queries.
    Enhancements to stored procedures, such as default parameters and variadic
    parameters, make database server programming simpler and more compact.
    Of course, there are also performance improvements included in this version.

---
+++ databases/postgresql84-client/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/07/29 06:27:56 adam Exp $
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+BUILDLINK_TREE+=	postgresql84-client
+
+.if !defined(POSTGRESQL84_CLIENT_BUILDLINK3_MK)
+POSTGRESQL84_CLIENT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.postgresql84-client+=	postgresql84-client>=8.4.0
+BUILDLINK_ABI_DEPENDS.postgresql84-client?=	postgresql84-client>=8.4.0
+BUILDLINK_PKGSRCDIR.postgresql84-client?=	../../databases/postgresql84-client
+
+.  if defined(PG_SUBPREFIX) && !empty(PG_SUBPREFIX)
+BUILDLINK_INCDIRS.postgresql84-client?=	${PG_SUBPREFIX}/include
+BUILDLINK_LIBDIRS.postgresql84-client?=	${PG_SUBPREFIX}/lib
+.  endif
+
+# This variable contains the libraries need to link most clients.
+BUILDLINK_LDADD.postgresql84-client=	-lpq ${BUILDLINK_LDADD.gettext}
+
+BUILDLINK_FILES.postgresql84-client+=	bin/pg_config
+
+.include "../../security/openssl/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
+.endif # POSTGRESQL84_CLIENT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-postgresql84-client
