57ea5b298d5bc4d4407ad5e322788532af05b7df
drochner <drochner>
2009-08-02 11:24:06 +0000

    add gssdp-0.6.4, a service discovery library for for UPnP

---
+++ net/gssdp/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/08/02 11:24:06 drochner Exp $
+
+BUILDLINK_TREE+=	gssdp
+
+.if !defined(GSSDP_BUILDLINK3_MK)
+GSSDP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gssdp+=	gssdp>=0.6.4
+BUILDLINK_PKGSRCDIR.gssdp?=	../../net/gssdp
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../net/libsoup24/buildlink3.mk"
+.endif	# GSSDP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gssdp
