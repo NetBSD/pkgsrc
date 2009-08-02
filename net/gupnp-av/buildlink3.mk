253880d7250762b9a70442ac273891411b5162a7
drochner <drochner@pkgsrc.org>
2009-08-02 11:37:20 +0000

    add gupnp-av-0.4.1, helpers for audio/video applications using GUPnP

---
+++ net/gupnp-av/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/08/02 11:37:20 drochner Exp $
+
+BUILDLINK_TREE+=	gupnp-av
+
+.if !defined(GUPNP_AV_BUILDLINK3_MK)
+GUPNP_AV_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gupnp-av+=	gupnp-av>=0.3.1
+BUILDLINK_PKGSRCDIR.gupnp-av?=	../../net/gupnp-av
+
+.include "../../net/gupnp/buildlink3.mk"
+.endif	# GUPNP_AV_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gupnp-av
