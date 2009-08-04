46ba29a5c1ba37f24fa0a71d94c4517e482ddf3d
martti <martti@pkgsrc.org>
2009-08-04 05:53:03 +0000

    Updated editors/vim-share to 7.2.245
      2052  7.2.219  (extra) Photon GUI is outdated
      2958  7.2.220  (after 7.2.215) BufEnter "cd" autocommand causes problems
      7103  7.2.221  X cut_buffer0 text may be used in the wrong encoding
      1816  7.2.222  ":mksession" doesn't work properly with 'acd' set
      5132  7.2.223  a script run with ":silent" cannot give any messages
      2542  7.2.224  crash when using 'completefunc'
      2874  7.2.225  when using ":normal" a saved character may be executed
      7470  7.2.226  ml_get error after deleting the last line
      1574  7.2.227  when using ":cd" in a script there is no way to track this
     14946  7.2.228  cscope is limited to 8 connections
      1595  7.2.229  warning for shadowed variable
      2442  7.2.230  a few old lint-style ARGUSED comments
      1473  7.2.231  warning for unreacheable code in Perl interface
      2704  7.2.232  cannot debug problems with being in a wrong directory
      2901  7.2.233  extra part of 7.2.232
      3831  7.2.234  it is not possible to ignore file names without a suffix
      2696  7.2.235  Using CTRL-O z= in Insert mode has a delay before redrawing
      2809  7.2.236  Mac: Compiling with Ruby doesn't always work
      1965  7.2.237  crash on exit when window icon not set
      3941  7.2.238  leaking memory when setting term to "builtin_dumb"
      4151  7.2.239  using :diffpatch twice may cause a crash
      2078  7.2.240  crash when using GUI find/replace dialog repeatedly
      5174  7.2.241  problems with using ":bufdo" and "doautoall" or ":vimgrep"
      2505  7.2.242  setting 'lazyredraw' causes the cursor column to be recomputed
      1918  7.2.243  memory leak when using :vimgrep and resizing
      4757  7.2.244  insufficient info for a conversion error from utf-8 to latin1
      5093  7.2.245  wrong conversion when writing Unicode encoded files

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.67 2009/06/25 05:34:54 martti Exp $
+# $NetBSD: version.mk,v 1.68 2009/08/04 05:53:03 martti Exp $
 
 VIM_VERSION=		7.2
-VIM_PATCHLEVEL=		218
+VIM_PATCHLEVEL=		245
 VIM_SUBDIR=		vim72
