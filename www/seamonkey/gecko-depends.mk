3ddedca4f9bab81337322ca2ee70f047960853ac
tnn <tnn@pkgsrc.org>
2009-08-05 15:04:40 +0000

    mark as deprecated

--- www/seamonkey/gecko-depends.mk
+++ www/seamonkey/gecko-depends.mk
@@ -1,12 +1,3 @@
-# $NetBSD: gecko-depends.mk,v 1.2 2008/05/31 23:02:40 tnn Exp $
+# $NetBSD: gecko-depends.mk,v 1.3 2009/08/05 15:04:40 tnn Exp $
 
-.if !empty(PKG_OPTIONS:Mfirefox)
-# needs .pc files
-BUILDLINK_API_DEPENDS.firefox+=		firefox>=1.5nb2
-BUILDLINK_API_DEPENDS.firefox+=		firefox<=2.99
-.include "../../www/firefox/buildlink3.mk"
-.else
-# needs .pc files
-BUILDLINK_API_DEPENDS.seamonkey+=	seamonkey>=1.0.5nb1
-.include "../../www/seamonkey/buildlink3.mk"
-.endif
+BROKEN=	"gecko-depends.mk" is deprecated. Use xulrunner instead.
