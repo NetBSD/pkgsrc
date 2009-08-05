7f9e94cd2436ae0117bc4e863b199074fd609ee6
drochner <drochner@pkgsrc.org>
2009-08-05 17:32:54 +0000

    the .pc file requires ndesk-dbus

--- sysutils/ndesk-dbus-glib/buildlink3.mk
+++ sysutils/ndesk-dbus-glib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/25 22:37:14 he Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/08/05 17:32:54 drochner Exp $
 
 BUILDLINK_TREE+=	ndesk-dbus-glib
 
@@ -10,6 +10,7 @@ BUILDLINK_ABI_DEPENDS.ndesk-dbus-glib?=	ndesk-dbus-glib>=0.4.1nb1
 BUILDLINK_PKGSRCDIR.ndesk-dbus-glib?=	../../sysutils/ndesk-dbus-glib
 
 .include "../../lang/mono/buildlink3.mk"
+.include "../../sysutils/ndesk-dbus/buildlink3.mk"
 .endif # NDESK_DBUS_GLIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ndesk-dbus-glib
