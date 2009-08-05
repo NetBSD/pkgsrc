7d6ef249e39da2f8ba73f7d3d04db59adc3e0c3c
minskim <minskim@pkgsrc.org>
2009-08-05 10:30:26 +0000

    Import emacs-22.3nb2 in editors/emacs as editors/emacs22; editors/emacs will be updated to 23.1.
    Proposed by obache@, and OK'ed by markd@ and drochner@.

---
+++ editors/emacs22/version.mk
@@ -0,0 +1,9 @@
+# $NetBSD: version.mk,v 1.1.1.1 2009/08/05 10:30:29 minskim Exp $
+
+_EMACS_FLAVOR=	emacs
+_EMACS_REQD=	emacs>=22
+_EMACS_PKGDEP.base=
+_EMACS_PKGDEP.leim=
+
+_EMACS_VERSION_MAJOR=	22
+_EMACS_VERSION_MINOR=	3
