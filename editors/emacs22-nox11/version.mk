5ddbc8c33d701a173f9a0f199b88d43b37363eeb
minskim <minskim@pkgsrc.org>
2009-08-05 10:32:54 +0000

    Import emacs-nox11-22.3nb2 in editors/emacs-nox11 as editors/emacs22-nox11.

---
+++ editors/emacs22-nox11/version.mk
@@ -0,0 +1,9 @@
+# $NetBSD: version.mk,v 1.1.1.1 2009/08/05 10:32:54 minskim Exp $
+
+_EMACS_FLAVOR=	emacs
+_EMACS_REQD=	emacs-nox11>=22<23
+_EMACS_PKGDEP.base=
+_EMACS_PKGDEP.leim=
+
+_EMACS_VERSION_MAJOR=	22
+_EMACS_VERSION_MINOR=	3
