8f5556b34cded3920402b7cf83add0bd7eacaf91
tnn <tnn>
2009-08-05 15:04:40 +0000

    mark as deprecated

--- www/seamonkey/gecko-options.mk
+++ www/seamonkey/gecko-options.mk
@@ -1,5 +1,3 @@
-# $NetBSD: gecko-options.mk,v 1.1 2006/09/24 16:21:33 salo Exp $
+# $NetBSD: gecko-options.mk,v 1.2 2009/08/05 15:04:40 tnn Exp $
 
-PKG_OPTIONS_REQUIRED_GROUPS+=	gecko
-PKG_OPTIONS_GROUP.gecko=	firefox seamonkey
-PKG_SUGGESTED_OPTIONS+=		firefox
+BROKEN= "gecko-options.mk" is deprecated. Use xulrunner instead.
