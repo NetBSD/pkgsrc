0cafb42fda553e13520e96e15e473a3634ca5a6b
tnn <tnn@pkgsrc.org>
2009-08-06 12:37:06 +0000

    drop bogus reference to gecko-options.mk (this package uses webkit)

--- devel/devhelp/options.mk
+++
@@ -1,6 +0,0 @@
-# $NetBSD: options.mk,v 1.2 2007/02/22 19:26:21 wiz Exp $
-
-PKG_OPTIONS_VAR=	PKG_OPTIONS.devhelp
-
-.include "../../www/seamonkey/gecko-options.mk"
-.include "../../mk/bsd.options.mk"
