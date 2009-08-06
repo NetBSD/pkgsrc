fe9ef69b7686e4bcb0a72c28567182c39053751c
joerg <joerg>
2009-08-06 23:21:31 +0000

    Update to py-wxWidgets-2.8.10.1. The biggest change is the switch to wxGTK 2.8 and the Unicode version as backend.

--- x11/py-wxWidgets/buildlink3.mk
+++ x11/py-wxWidgets/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2009/03/20 19:25:49 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2009/08/06 23:21:31 joerg Exp $
 
 BUILDLINK_TREE+=	${PYPKGPREFIX}-wxWidgets
 
@@ -11,7 +11,7 @@ BUILDLINK_API_DEPENDS.${PYPKGPREFIX}-wxWidgets+=	${PYPKGPREFIX}-wxWidgets>=2.6.1
 BUILDLINK_ABI_DEPENDS.${PYPKGPREFIX}-wxWidgets?=	${PYPKGPREFIX}-wxWidgets>=2.6.1.0nb4
 BUILDLINK_PKGSRCDIR.${PYPKGPREFIX}-wxWidgets?=	../../x11/py-wxWidgets
 
-.include "../../x11/wxGTK26/buildlink3.mk"
+.include "../../x11/wxGTK28/buildlink3.mk"
 .endif # PY_WXWIDGETS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-${PYPKGPREFIX}-wxWidgets
