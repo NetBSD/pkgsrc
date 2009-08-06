495da267d2429ca27c7f26ddcc104dd0404d7616
tnn <tnn@pkgsrc.org>
2009-08-06 07:37:33 +0000

    fix a busted .pc file. Bump PKGREVISION again.

--- devel/xulrunner/buildlink3.mk
+++ devel/xulrunner/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/08/05 06:10:31 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/08/06 07:37:33 tnn Exp $
 
 BUILDLINK_TREE+=	xulrunner
 
 .if !defined(XULRUNNER_BUILDLINK3_MK)
 XULRUNNER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=1.9.1nb1
+BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=1.9.1nb4
 BUILDLINK_PKGSRCDIR.xulrunner?=	../../devel/xulrunner
 
 #.include "../../net/libIDL/buildlink3.mk"
