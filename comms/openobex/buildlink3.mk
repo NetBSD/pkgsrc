165bc1a85e122c0564302b965ed06069621634a7
plunky <plunky@pkgsrc.org>
2009-08-07 12:18:33 +0000

    update to openobex 1.5,
    Bluetooth detection on BSD is much improved so the patches are no longer
    required. They got it wrong though, so one patch added (I also fed that
    upstream)
    
    while here,
    	include LICENSE=gnu-lgpl-v2
    	update HOMEPAGE=http://www.openobex.org/

--- comms/openobex/buildlink3.mk
+++ comms/openobex/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:24:03 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2009/08/07 12:18:33 plunky Exp $
 
 BUILDLINK_TREE+=	openobex
 
 .if !defined(OPENOBEX_BUILDLINK3_MK)
 OPENOBEX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.openobex+=	openobex>=1.3
+BUILDLINK_API_DEPENDS.openobex+=	openobex>=1.5
 BUILDLINK_PKGSRCDIR.openobex?=	../../comms/openobex
 
 .include "../../devel/libusb/buildlink3.mk"
