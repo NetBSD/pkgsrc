a3d73f24f8b4368194943c06ad2f329043cdf3dc
minskim <minskim@pkgsrc.org>
2009-08-09 12:01:18 +0000

    Update mew to 6.2.  This fixes PR 40808 and PR 41853.
    Major changes:
        * Configuration in ".mew.el", especially of mew-config-alist is
          simplified. See the config2 node in info.
        * Virtual mode is categorized to Selection and Thread. You can create
          Selection by keyword, dialog, sorting, and so on. You can make
          Thread even of Selection as well as of Summary.
        * "S" is now virtual sort creating Selection. Physical sort is
          assigned to "M-s".
        * Punycode is supported. Mew now can display internationalized domain
          names.
        * "Z" collects a folder list according to the current world.
          No numeric argument is necessary any more.
        * "_" toggles a long line: normal, long, wrapped.
        * You can use gmail with IMAP.

--- mail/mew/buildlink3.mk
+++ mail/mew/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:56 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/08/09 12:01:18 minskim Exp $
 #
 
 BUILDLINK_TREE+=	mew
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	mew
 .if !defined(MEW_BUILDLINK3_MK)
 MEW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mew+=	${EMACS_PKGNAME_PREFIX}mew>=5
+BUILDLINK_API_DEPENDS.mew+=	${EMACS_PKGNAME_PREFIX}mew>=6
 BUILDLINK_PKGSRCDIR.mew?=	../../mail/mew
 
 BUILDLINK_CONTENTS_FILTER.mew=	${EGREP} '.*\.el$$|.*\.elc$$'
