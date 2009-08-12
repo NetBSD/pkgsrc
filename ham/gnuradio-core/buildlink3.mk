26432dfeee9845d1390dc852ea85aae5988d09ce
markd <markd>
2009-08-12 12:20:04 +0000

    Update to gnuradio 3.2
    many new features and bugfixes.

--- ham/gnuradio-core/buildlink3.mk
+++ ham/gnuradio-core/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2009/03/20 19:24:46 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2009/08/12 12:21:39 markd Exp $
 
 BUILDLINK_TREE+=	gnuradio-core
 
@@ -10,12 +10,14 @@ BUILDLINK_PKGSRCDIR.gnuradio-core?=	../../ham/gnuradio-core
 BUILDLINK_CPPFLAGS.gnuradio-core+=    -I${BUILDLINK_PREFIX.gnuradio-core}/include/gnuradio
 
 .include "../../devel/boost-headers/buildlink3.mk"
+.include "../../devel/boost-libs/buildlink3.mk"
 .include "../../devel/cppunit/buildlink3.mk"
 .include "../../devel/swig/buildlink3.mk"
 .include "../../lang/python/application.mk"
 .include "../../lang/python/extension.mk"
 .include "../../lang/python/pyversion.mk"
 .include "../../math/fftwf/buildlink3.mk"
+.include "../../math/gsl/buildlink3.mk"
 .endif # GNURADIO_CORE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnuradio-core
