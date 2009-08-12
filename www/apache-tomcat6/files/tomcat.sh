d6dda42a6e9583a6d613417061f46a612fd09057
adrianp <adrianp@pkgsrc.org>
2009-08-12 21:51:49 +0000

    Add tomcat_stop()

--- www/apache-tomcat6/files/tomcat.sh
+++ www/apache-tomcat6/files/tomcat.sh
@@ -1,6 +1,6 @@
 #! @RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: tomcat.sh,v 1.2 2009/08/12 20:07:57 adrianp Exp $
+# $NetBSD: tomcat.sh,v 1.3 2009/08/12 21:51:49 adrianp Exp $
 #
 # PROVIDE: tomcat
 # REQUIRE: DAEMON
@@ -63,6 +63,14 @@ tomcat_start()
 	cd $CATALINA_HOME/logs
 	@SU@ ${tomcat_user} -c "${command} start"
 }
+
+tomcat_stop()
+{
+	rc_pid=$(check_pidfile $pidfile "java")
+	@SU@ ${tomcat_user} -c "${command} stop"
+	wait_for_pids $rc_pid
+}
+
 
 tomcat_run()
 {
