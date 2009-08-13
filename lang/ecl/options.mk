53a6b1a0be74c09f8f91ce94ba9ab1107ffae3b3
asau <asau@pkgsrc.org>
2009-08-13 18:17:18 +0000

    Update to ECL 9.8.3.
    Changes since previous packaged version (8.12.0) include half year
    of very active development:
    
    - The dynamic FFI is now implemented using libffi.
    
    - Support of UNICODE.
    
    - Multithreading (provided Boehm-GC built with threads support).
    
    - Command line flags to control the different memory limits:
      --heap-size, --lisp-stack, --frame-stack and --c-stack.
    
    - The stack size is now measured in bytes, not in lisp words.
    
    - The out of memory error (ext:storage-exhausted) can now be recovered.
      By default a correctable error is signaled and the user is given the
      chance to increase the heap size.
    
    - Better compiler, optimizer, debugger.
    
    - Improved handling of compiler errors.
    
    - New and more easily extensible implementation of streams.
    
    - *STANDARD-INPUT*, *{STANDARD,ERROR,TRACE}-OUTPUT* are no longer synonyms to
      *TERMINAL-IO* but directly the input or output streams associated to stdin,
      stdout and stderr.
    
    - Improved numerics.
    
    - Read-only readtables.
    
    - Numerous bug fixes.

---
+++ lang/ecl/options.mk
@@ -0,0 +1,24 @@
+# $NetBSD: options.mk,v 1.1 2009/08/13 18:17:18 asau Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.ecl
+PKG_SUPPORTED_OPTIONS+=		threads unicode
+PKG_SUGGESTED_OPTIONS+=		# empty
+# Unicode support proved to break Axioms.
+# Threads are off, since threaded ECL build crashes on NetBSD and FreeBSD.
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_SRC=	PLIST	# default value
+
+.if !empty(PKG_OPTIONS:Mthreads)
+CONFIGURE_ARGS+=	--enable-threads --enable-debug
+CONFIGURE_ENV+=		THREAD_CFLAGS=${PTHREAD_CFLAGS:Q}
+CONFIGURE_ENV+=		THREAD_LDLAGS=${BUILDLINK_LDLAGS.pthread:Q}
+CONFIGURE_ENV+=		THREAD_LIBS=${BUILDLINK_LIBS.pthread:Q}
+.include "../../mk/pthread.buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Municode)
+CONFIGURE_ARGS+=	--enable-unicode
+PLIST_SRC+=		PLIST.unicode
+.endif
