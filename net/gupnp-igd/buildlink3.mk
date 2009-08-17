cf566516f1094762bd85c1cd14b37b475aa26de5
wiz <wiz@pkgsrc.org>
2009-08-17 21:07:40 +0000

    Initial import of gupnp-igd-0.1.3: This is a library to handle UPnP IGD port mapping.

---
+++ net/gupnp-igd/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/08/17 21:07:40 wiz Exp $
+
+BUILDLINK_TREE+=	gupnp-igd
+
+.if !defined(GUPNP_IGD_BUILDLINK3_MK)
+GUPNP_IGD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gupnp-igd+=	gupnp-igd>=0.1.3
+BUILDLINK_PKGSRCDIR.gupnp-igd?=	../../net/gupnp-igd
+
+.include "../../net/gupnp/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.endif	# GUPNP_IGD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gupnp-igd
