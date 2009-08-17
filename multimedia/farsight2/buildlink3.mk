7d2a2db3f7d09e57c7cb398f3068de45ebf4e661
wiz <wiz@pkgsrc.org>
2009-08-17 21:13:03 +0000

    Initial import of farsight2-0.0.14:
    The Farsight project is an effort to create a framework to deal
    with all known audio/video conferencing protocols. On one side it
    offers a generic API that makes it possible to write plugins for
    different streaming protocols, on the other side it offers an API
    for clients to use those plugins.
    
    The main target clients for Farsight are Instant Messaging
    applications. These applications should be able to use Farsight
    for all their Audio/Video conferencing needs without having to
    worry about any of the lower level streaming and NAT traversal
    issues.
    
    Farsight forms an integral part of the Telepathy framework. It is
    used by Empathy through the Telepathy-Farsight library. It can also
    be easily used on embedded platforms by using Stream-Engine. The
    Telepathy-Farsight library binds it to the Connection Managers via
    D-Bus and the Telepathy Media Stream Spec and is used for all their
    streaming requirements.

---
+++ multimedia/farsight2/buildlink3.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/08/17 21:13:03 wiz Exp $
+
+BUILDLINK_TREE+=	farsight2
+
+.if !defined(FARSIGHT2_BUILDLINK3_MK)
+FARSIGHT2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.farsight2+=	farsight2>=0.0.14
+BUILDLINK_PKGSRCDIR.farsight2?=	../../multimedia/farsight2
+
+# unsure which are needed exactly
+#.include "../../devel/py-gobject/buildlink3.mk"
+#.include "../../devel/glib2/buildlink3.mk"
+#.include "../../multimedia/gst-plugins0.10-bad/buildlink3.mk"
+.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
+#.include "../../multimedia/gst-plugins0.10-good/buildlink3.mk"
+.include "../../multimedia/gstreamer0.10/buildlink3.mk"
+#.include "../../multimedia/py-gstreamer0.10/buildlink3.mk"
+#.include "../../net/gupnp-igd/buildlink3.mk"
+#.include "../../net/libnice/buildlink3.mk"
+#.include "../../x11/py-gtk2/buildlink3.mk"
+.endif	# FARSIGHT2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-farsight2
