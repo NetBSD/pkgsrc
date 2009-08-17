2da67a25817f20be0e5eba4d875fb20565a080b3
wiz <wiz@pkgsrc.org>
2009-08-17 21:10:28 +0000

    Initial import of libnice-0.0.9:
    GLib ICE library
    
     - only implements ICE Lite
     - no support for RTCP
     - no support for IPv6

---
+++ net/libnice/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/08/17 21:10:28 wiz Exp $
+
+BUILDLINK_TREE+=	libnice
+
+.if !defined(LIBNICE_BUILDLINK3_MK)
+LIBNICE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libnice+=	libnice>=0.0.9
+BUILDLINK_PKGSRCDIR.libnice?=	../../net/libnice
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../net/gupnp-igd/buildlink3.mk"
+.endif	# LIBNICE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libnice
