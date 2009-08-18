68e1ab8cec30f70a02fda07fd97a83a983dafc46
sno <sno@pkgsrc.org>
2009-08-18 14:10:53 +0000

    Updating package for devel/libev from 3.7nb1 to 3.8 - bumping API/ABI-depends, because upstream change says nothing about   ABI/API incompatible change
    Upstream changes:
    3.8  Sun Aug  9 14:30:45 CEST 2009
    	- incompatible change: do not necessarily reset signal handler
              to SIG_DFL when a sighandler is stopped.
            - ev_default_destroy did not properly free or zero some members,
              potentially causing crashes and memory corruption on repated
              ev_default_destroy/ev_default_loop calls.
    	- take advantage of signalfd on GNU/Linux systems.
    	- document that the signal mask might be in an unspecified
              state when using libev's signal handling.
            - take advantage of some GNU/Linux calls to set cloexec/nonblock
              on fd creation, to avoid race conditions.

--- devel/libev/buildlink3.mk
+++ devel/libev/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/08/08 19:40:37 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/08/18 14:10:53 sno Exp $
 
 BUILDLINK_TREE+=	libev
 
 .if !defined(LIBEV_BUILDLINK3_MK)
 LIBEV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libev+=libev>=3.7
-BUILDLINK_ABI_DEPENDS.libev+=libev>=3.7
+BUILDLINK_API_DEPENDS.libev+=libev>=3.8
+BUILDLINK_ABI_DEPENDS.libev+=libev>=3.8
 BUILDLINK_PKGSRCDIR.libev?=	../../devel/libev
 .endif # LIBEV_BUILDLINK3_MK
 
