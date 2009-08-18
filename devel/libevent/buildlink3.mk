2aa8497cb59d3799b092795d6c64921303acdea1
drochner <drochner@pkgsrc.org>
2009-08-18 16:53:14 +0000

    That's all not true... SONAMEs didn't change since at least 1.4.3. Relax ABI dependency and remove an outdated comment.

--- devel/libevent/buildlink3.mk
+++ devel/libevent/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2009/08/16 15:35:35 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2009/08/18 16:53:14 drochner Exp $
 
 BUILDLINK_TREE+=	libevent
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libevent
 LIBEVENT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libevent+=libevent>=0.6
-BUILDLINK_ABI_DEPENDS.libevent+=libevent-1.4.12* # exact match -- see Makefile
+BUILDLINK_ABI_DEPENDS.libevent+=libevent>=1.4.3
 BUILDLINK_PKGSRCDIR.libevent?=	../../devel/libevent
 .endif # LIBEVENT_BUILDLINK3_MK
 
