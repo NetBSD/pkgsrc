fc998478692ebc1943eab794df5bd2d735508be4
sno <sno@pkgsrc.org>
2009-08-26 16:50:46 +0000

    Updating package graphics/jpeg from version 6bnb4 to 7
    pkgsrc changes:
      - update MASTER_SITES
      - bump ApI_DEPENDS in buildlink3.mk
    
    Upstream changes:
    - The new features added with version 6bnb4 are now added upstream (except
      exifautotran and jpegexiforient)
    
    Version 7  27-Jun-2009
    ----------------------
    
    New scaled DCTs implemented.
    djpeg now supports scalings N/8 with all N from 1 to 16.
    cjpeg now supports scalings 8/N with all N from 1 to 16.
    Scaled DCTs with size larger than 8 are now also used for resolving the
    common 2x2 chroma subsampling case without additional spatial resampling.
    Separate spatial resampling for those kind of files is now only necessary
    for N>8 scaling cases.
    Furthermore, separate scaled DCT functions are provided for direct resolving
    of the common asymmetric subsampling cases (2x1 and 1x2) without additional
    spatial resampling.
    
    cjpeg -quality option has been extended for support of separate quality
    settings for luminance and chrominance (or in general, for every provided
    quantization table slot).
    New API function jpeg_default_qtables() and q_scale_factor array in library.
    
    Added -nosmooth option to cjpeg, complementary to djpeg.
    New variable "do_fancy_downsampling" in library, complement to fancy
    upsampling.  Fancy upsampling now uses direct DCT scaling with sizes
    larger than 8.  The old method is not reversible and has been removed.
    
    Support arithmetic entropy encoding and decoding.
    Added files jaricom.c, jcarith.c, jdarith.c.
    
    Straighten the file structure:
    Removed files jidctred.c, jcphuff.c, jchuff.h, jdphuff.c, jdhuff.h.
    
    jpegtran has a new "lossless" cropping feature.
    
    Implement -perfect option in jpegtran, new API function
    jtransform_perfect_transform() in transupp. (DP 204_perfect.dpatch)
    
    Better error messages for jpegtran fopen failure.
    (DP 203_jpegtran_errmsg.dpatch)
    
    Fix byte order issue with 16bit PPM/PGM files in rdppm.c/wrppm.c:
    according to Netpbm, the de facto standard implementation of the PNM formats,
    the most significant byte is first. (DP 203_rdppm.dpatch)
    
    Add -raw option to rdjpgcom not to mangle the output.
    (DP 205_rdjpgcom_raw.dpatch)
    
    Make rdjpgcom locale aware. (DP 201_rdjpgcom_locale.dpatch)
    
    Add extern "C" to jpeglib.h.
    This avoids the need to put extern "C" { ... } around #include "jpeglib.h"
    in your C++ application.  Defining the symbol DONT_USE_EXTERN_C in the
    configuration prevents this. (DP 202_jpeglib.h_c++.dpatch)

--- graphics/jpeg/buildlink3.mk
+++ graphics/jpeg/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:24:41 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2009/08/26 16:50:46 sno Exp $
 
 BUILDLINK_TREE+=	jpeg
 
 .if !defined(JPEG_BUILDLINK3_MK)
 JPEG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.jpeg+=	jpeg>=6b
-BUILDLINK_ABI_DEPENDS.jpeg+=	jpeg>=6bnb2
+BUILDLINK_API_DEPENDS.jpeg+=	jpeg>=7
+#BUILDLINK_ABI_DEPENDS.jpeg+=	jpeg>=7
 BUILDLINK_PKGSRCDIR.jpeg?=	../../graphics/jpeg
 .endif # JPEG_BUILDLINK3_MK
 
