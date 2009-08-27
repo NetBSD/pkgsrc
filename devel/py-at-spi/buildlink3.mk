e82b7aee1e0f2e59a2a76f823e955c174e002110
wiz <wiz>
2009-08-27 18:53:36 +0000

    Fix fallout from recursive jpeg shlib bump. The man page says that you should check the diff before committing, especially for the case that went wrong here (PYPKGPREFIX -> py25). Hi sno!

--- devel/py-at-spi/buildlink3.mk
+++ devel/py-at-spi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/08/26 19:57:07 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2009/08/27 18:53:36 wiz Exp $
 
 BUILDLINK_TREE+=	py-at-spi
 
@@ -8,7 +8,7 @@ PY_AT_SPI_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.py-at-spi+=	${PYPKGPREFIX}-at-spi>=1.20.1
-BUILDLINK_ABI_DEPENDS.py-at-spi?=		py25-at-spi>=1.26.0nb1
+BUILDLINK_ABI_DEPENDS.py-at-spi?=		${PYPKGPREFIX}-at-spi>=1.26.0nb1
 BUILDLINK_PKGSRCDIR.py-at-spi?=		../../devel/py-at-spi
 
 .include "../../devel/at-spi/buildlink3.mk"
