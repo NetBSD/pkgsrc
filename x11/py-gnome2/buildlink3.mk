947512f650ce42b64a528d411d23aacc2c5c4a65
wiz <wiz>
2009-08-27 18:53:36 +0000

    Fix fallout from recursive jpeg shlib bump. The man page says that you should check the diff before committing, especially for the case that went wrong here (PYPKGPREFIX -> py25). Hi sno!

--- x11/py-gnome2/buildlink3.mk
+++ x11/py-gnome2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.24 2009/08/26 19:57:05 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2009/08/27 18:53:38 wiz Exp $
 
 BUILDLINK_TREE+=	pygnome2
 
@@ -8,7 +8,7 @@ PYGNOME2_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.pygnome2+=	${PYPKGPREFIX}-gnome2>=2.10.0
-BUILDLINK_ABI_DEPENDS.pygnome2+=	py25-gnome2>=2.26.1nb1
+BUILDLINK_ABI_DEPENDS.pygnome2+=	${PYPKGPREFIX}-gnome2>=2.26.1nb1
 BUILDLINK_PKGSRCDIR.pygnome2?=	../../x11/py-gnome2
 
 .include "../../net/py-ORBit/buildlink3.mk"
