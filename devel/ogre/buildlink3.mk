87edea982c193568464608a57330dbb9f66470c8
hasso <hasso@pkgsrc.org>
2009-09-01 16:53:40 +0000

    Update to 1.6.3. Not pasting the upstream changelog here, because it's a massive (4 years of development).

--- devel/ogre/buildlink3.mk
+++ devel/ogre/buildlink3.mk
@@ -1,18 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.15 2009/08/26 19:56:44 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2009/09/01 16:53:40 hasso Exp $
 
 BUILDLINK_TREE+=	ogre
 
 .if !defined(OGRE_BUILDLINK3_MK)
 OGRE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ogre+=	ogre>=0.12.1nb1
-BUILDLINK_ABI_DEPENDS.ogre+=	ogre>=0.12.1nb9
+BUILDLINK_API_DEPENDS.ogre+=	ogre>=1.6.3
+BUILDLINK_ABI_DEPENDS.ogre+=	ogre>=1.6.3
 BUILDLINK_PKGSRCDIR.ogre?=	../../devel/ogre
 
-.include "../../devel/SDL/buildlink3.mk"
+.include "../../archivers/zziplib/buildlink3.mk"
 .include "../../devel/devIL/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../x11/libXaw/buildlink3.mk"
 .endif # OGRE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ogre
