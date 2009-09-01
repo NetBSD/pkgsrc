55b2230f9ecbf74f60d591d2b6184fc6d5564dd8
joerg <joerg@pkgsrc.org>
2009-09-01 17:48:14 +0000

    Don't abuse WRAPPER_DIR, libgnutls-config belongs into BUILDLINK_DIR.

--- security/gnutls/libgnutls-config.mk
+++ security/gnutls/libgnutls-config.mk
@@ -1,18 +1,18 @@
-# $NetBSD: libgnutls-config.mk,v 1.1 2009/07/02 18:53:46 tnn Exp $
+# $NetBSD: libgnutls-config.mk,v 1.2 2009/09/01 17:48:14 joerg Exp $
 
 # Makefile intended to be included by packages that need "libgnutls-config"
 # during build time.
 
 USE_TOOLS+=	pkg-config
 
-pre-configure: ${WRAPPER_DIR}/bin/libgnutls-config
+pre-configure: hack-libgnutls-config
 
-${WRAPPER_DIR}/bin/libgnutls-config:
+hack-libgnutls-config:
 	${PRINTF} "#! ${SH}\\n\
 	  case \$$1 in\\n\
 	  --cflags|--libs) pkg-config \$$1 gnutls;;\\n\
 	  --version) pkg-config --modversion gnutls;;\\n\
 	  *) exit 1;;\\n\
 	  esac\\n\
-	" > ${.TARGET}
-	${CHMOD} +x ${.TARGET}
+	" > ${BUILDLINK_DIR}/bin/libgnutls-config
+	${CHMOD} +x ${BUILDLINK_DIR}/bin/libgnutls-config
