e29e788d7f474d2c9c0a6c66f0a607d4c520412f
hasso <hasso>
2009-09-01 16:44:26 +0000

    Update to 1.7.8. Not pasting the upstream changelog here, because it's a massive (6 years of development).

--- devel/devIL/buildlink3.mk
+++ devel/devIL/buildlink3.mk
@@ -1,15 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.16 2009/08/26 19:56:43 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2009/09/01 16:44:26 hasso Exp $
 
 BUILDLINK_TREE+=	devIL
 
 .if !defined(DEVIL_BUILDLINK3_MK)
 DEVIL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.devIL+=	devIL>=1.6.6nb1
-BUILDLINK_ABI_DEPENDS.devIL+=	devIL>=1.6.6nb10
+BUILDLINK_API_DEPENDS.devIL+=	devIL>=1.7.8
+BUILDLINK_ABI_DEPENDS.devIL+=	devIL>=1.7.8
 BUILDLINK_PKGSRCDIR.devIL?=	../../devel/devIL
 
 .include "../../devel/SDL/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../graphics/jasper/buildlink3.mk"
 .include "../../graphics/jpeg/buildlink3.mk"
 .include "../../graphics/libungif/buildlink3.mk"
 .include "../../graphics/mng/buildlink3.mk"
