94b9a43f3fdeee934eb31f7444defe3d0ce3e541
abs <abs>
2009-09-06 21:06:44 +0000

    PR 41978 from Joachim Kuebart
    Update:
        chat/finch to 2.6.1
        chat/libpurple to 2.6.1
        chat/pidgin to 2.6.1
        chat/pidgin-sametime to 2.6.1
        chat/pidgin-silc to 2.6.1
    
    major changes:
     o addition of farsight support for voice/video chats (untested, new option on by default)
     o addition of dependency on devel/libidn
     o addition of gstreamer option for libpurple (on by default)

--- chat/pidgin/options.mk
+++ chat/pidgin/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.6 2008/12/21 14:08:39 ahoka Exp $
+# $NetBSD: options.mk,v 1.7 2009/09/06 21:06:45 abs Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.pidgin
-PKG_SUPPORTED_OPTIONS+=		dbus debug gstreamer gtkspell
-PKG_SUGGESTED_OPTIONS+=		gtkspell dbus gstreamer
+PKG_SUPPORTED_OPTIONS+=		dbus debug farsight gstreamer gtkspell
+PKG_SUGGESTED_OPTIONS+=		gtkspell dbus farsight gstreamer
 
 .include "../../mk/bsd.options.mk"
 
@@ -13,6 +13,11 @@ CONFIGURE_ARGS+=	--enable-dbus
 PLIST.dbus=		yes
 .  include "../../sysutils/dbus/buildlink3.mk"
 .  include "../../sysutils/dbus-glib/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mfarsight)
+CONFIGURE_ARGS+=	--enable-farsight
+.  include "../../multimedia/farsight2/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mgtkspell)
