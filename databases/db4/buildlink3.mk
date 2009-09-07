0e111fab3cc2278c95c1adb061e5c8eee78ba03f
drochner <drochner@pkgsrc.org>
2009-09-07 18:37:12 +0000

    don't set BUILDLINK_LDADD/BUILDLINK_LIBS -- this ends up in the LIBS environment variable which makes (at least if GNU configure is used the common way) that everything in the pkg gets linked against db4, whether needed or not, which causes hidden dependencies and strange errors

--- databases/db4/buildlink3.mk
+++ databases/db4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.35 2009/03/20 19:24:04 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2009/09/07 18:37:12 drochner Exp $
 
 BUILDLINK_TREE+=	db4
 
@@ -9,12 +9,12 @@ BUILDLINK_API_DEPENDS.db4+=	db4>=4.7.25.1
 BUILDLINK_ABI_DEPENDS.db4?=	db4>=4.7.25.1
 BUILDLINK_PKGSRCDIR.db4?=	../../databases/db4
 BUILDLINK_INCDIRS.db4?=		include/db4
-BUILDLINK_LDADD.db4=		-ldb4
+#BUILDLINK_LDADD.db4=		-ldb4
 BUILDLINK_TRANSFORM+=		l:db-4:db4
 
 .  include "../../mk/bsd.fast.prefs.mk"
 .  if defined(USE_DB185) && !empty(USE_DB185:M[yY][eE][sS])
-BUILDLINK_LIBS.db4=		${BUILDLINK_LDADD.db4}
+#BUILDLINK_LIBS.db4=		${BUILDLINK_LDADD.db4}
 BUILDLINK_TRANSFORM+=		l:db:db4
 .  endif
 
