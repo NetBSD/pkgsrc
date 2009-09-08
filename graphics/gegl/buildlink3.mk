61bbb56fb6123e8beb09c250cdec4d1314f85a90
drochner <drochner@pkgsrc.org>
2009-09-08 20:30:25 +0000

    trim list of propagated dependencies

--- graphics/gegl/buildlink3.mk
+++ graphics/gegl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/08/26 19:56:43 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2009/09/08 20:30:25 drochner Exp $
 
 BUILDLINK_TREE+=	gegl
 
@@ -17,10 +17,7 @@ pkgbase := gegl
 .endif
 
 .include "../../graphics/babl/buildlink3.mk"
-.include "../../graphics/jpeg/buildlink3.mk"
-.include "../../graphics/openexr/buildlink3.mk"
-.include "../../graphics/png/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
 .endif # GEGL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gegl
