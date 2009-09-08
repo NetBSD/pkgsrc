ab2612aa7afab7e5cbc304674958b0dc07741ad5
wiz <wiz@pkgsrc.org>
2009-09-08 09:49:31 +0000

    Needs to include speex here as well.

--- devel/opal/buildlink3.mk
+++ devel/opal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:24:25 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2009/09/08 09:49:31 wiz Exp $
 
 BUILDLINK_TREE+=	opal
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.opal+=	opal>=3.6.1
 BUILDLINK_ABI_DEPENDS.opal?=	opal>=3.6.1
 BUILDLINK_PKGSRCDIR.opal?=	../../devel/opal
 
+.include "../../audio/speex/buildlink3.mk"
 .include "../../devel/ptlib/buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 .endif # OPAL_BUILDLINK3_MK
