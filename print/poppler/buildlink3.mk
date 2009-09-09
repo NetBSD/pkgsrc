a80bc4c0daef4b3fc629cd940dd1dded9fbeced0
drochner <drochner@pkgsrc.org>
2009-09-09 19:57:05 +0000

    update to 0.12.0 This is a major update, switches to the 0.12 release branch. Most notable change is that the color management stuff which we had as patches in pkgsrc is official now. Otherwise - many fixes and enhancements.
    (while this is a .0 release I've been tracking the 0.11.x snapshots
    and release candidate for a while so I don't expect big problems)

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2009/08/26 19:56:42 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2009/09/09 19:57:05 drochner Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.10.7nb2
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.11.2
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 pkgbase := poppler
