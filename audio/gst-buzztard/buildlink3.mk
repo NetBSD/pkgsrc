c5d992fe9c08d85fb22c9dac4cc7e45759f8d528
wiz <wiz@pkgsrc.org>
2009-09-10 20:19:18 +0000

    Fix paths.

--- audio/gst-buzztard/buildlink3.mk
+++ audio/gst-buzztard/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/09/10 20:16:45 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/09/10 20:19:18 wiz Exp $
 
 BUILDLINK_TREE+=	gst-buzztard
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gst-buzztard
 GST_BUZZTARD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gst-buzztard+=	gst-buzztard>=0.5.0
-BUILDLINK_PKGSRCDIR.gst-buzztard?=	../../nih/gst-buzztard
+BUILDLINK_PKGSRCDIR.gst-buzztard?=	../../audio/gst-buzztard
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
