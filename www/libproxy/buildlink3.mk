d04163e27a5886f19fd1bf643fa29f0e2a539e52
dsainty <dsainty@pkgsrc.org>
2009-09-10 02:03:11 +0000

    Roll back version 1.3 (pulling in lang/python/extension.mk).  With the recent -ldb4 fixes it's unlikely this there's any need or value in pulling in extension.mk any more.

--- www/libproxy/buildlink3.mk
+++ www/libproxy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/04/07 03:14:07 dsainty Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/09/10 02:03:11 dsainty Exp $
 
 BUILDLINK_TREE+=	libproxy
 
@@ -7,9 +7,6 @@ LIBPROXY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libproxy+=	libproxy>=0.2.3
 BUILDLINK_PKGSRCDIR.libproxy?=	../../www/libproxy
-
-.include "../../lang/python/extension.mk"
-
 .endif # LIBPROXY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libproxy
