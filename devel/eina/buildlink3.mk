af55b91c44e22256a205092c95fe5d987b77b79f
joerg <joerg@pkgsrc.org>
2009-09-11 20:20:18 +0000

    eina-0.0.2.062: Update to 2009/07/29 snapshot of e17. No detailed changes.

--- devel/eina/buildlink3.mk
+++ devel/eina/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/05/19 17:26:28 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/09/11 20:20:18 joerg Exp $
 
 BUILDLINK_TREE+=	eina
 
 .if !defined(EINA_BUILDLINK3_MK)
 EINA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.eina+=	eina>=0.0.2.060
+BUILDLINK_API_DEPENDS.eina+=	eina>=0.0.2.062
 BUILDLINK_PKGSRCDIR.eina?=	../../devel/eina
 .endif	# EINA_BUILDLINK3_MK
 
