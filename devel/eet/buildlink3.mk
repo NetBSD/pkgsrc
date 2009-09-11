f8ae701b6595961aa6387eb722528526202579bd
joerg <joerg@pkgsrc.org>
2009-09-11 21:48:01 +0000

    Expose OpenSSL so that the pkgconfig file works.

--- devel/eet/buildlink3.mk
+++ devel/eet/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/08/26 19:56:40 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2009/09/11 21:48:01 joerg Exp $
 
 BUILDLINK_TREE+=	eet
 
@@ -12,6 +12,7 @@ BUILDLINK_PKGSRCDIR.eet?=	../../devel/eet
 .include "../../devel/eina/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../graphics/jpeg/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
 .endif # EET_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-eet
