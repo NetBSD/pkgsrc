57e2c183e61f54c6b67f2b7c772b65cf32d254ce
tnn <tnn@pkgsrc.org>
2009-09-13 19:10:10 +0000

    export gnome dependencies in bl3.mk when gnome option is on

--- devel/xulrunner/buildlink3.mk
+++ devel/xulrunner/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/08/26 19:56:54 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2009/09/13 19:10:10 tnn Exp $
 
 BUILDLINK_TREE+=	xulrunner
 
@@ -9,9 +9,12 @@ BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=1.9.1nb4
 BUILDLINK_ABI_DEPENDS.xulrunner?=	xulrunner>=1.9.1.2nb3
 BUILDLINK_PKGSRCDIR.xulrunner?=	../../devel/xulrunner
 
-#.include "../../net/libIDL/buildlink3.mk"
-#.include "../../x11/gtk2/buildlink3.mk"
-#.include "../../x11/libXt/buildlink3.mk"
+pkgbase := xulrunner
+.include "../../mk/pkg-build-options.mk"
+.if !empty(PKG_BUILD_OPTIONS.xulrunner:Mgnome)
+. include "../../devel/libgnomeui/buildlink3.mk"
+. include "../../sysutils/gnome-vfs/buildlink3.mk"
+.endif
 .endif # XULRUNNER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xulrunner
