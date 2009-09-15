9960cda7886ee1c2ed82d177239d6900cbf92934
spz <spz@pkgsrc.org>
2009-09-15 20:03:47 +0000

    take wip/pkcs11-helper as generated by Daniel 'morr' Horecki <shinden@@linux.pl>
    add options, DESTDIR, LICENSE and other small updates to pkgsrc-current

---
+++ security/pkcs11-helper/buildlink3.mk
@@ -0,0 +1,27 @@
+# $NetBSD: buildlink3.mk,v 1.1 2009/09/15 20:03:47 spz Exp $
+
+BUILDLINK_TREE+=	pkcs11-helper
+
+.if !defined(PKCS11_HELPER_BUILDLINK3_MK)
+PKCS11_HELPER_BUILDLINK3_MK:=
+
+BUILDLINK_DEPMETHOD.pkcs11-helper?=	build
+BUILDLINK_API_DEPENDS.pkcs11-helper+=	pkcs11-helper>=1.06
+BUILDLINK_PKGSRCDIR.pkcs11-helper?=	../../security/pkcs11-helper
+
+pkgbase := pkcs11-helper
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.pkcs11-helper:Mopenssl)
+.  include "../../security/openssl/buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.pkcs11-helper:Mgnutls)
+.  include "../../security/gnutls/buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.pkcs11-helper:Mnss)
+.  include "../../devel/nss/buildlink3.mk"
+.endif
+
+.endif # PKCS11_HELPER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-pkcs11-helper
---
+++ security/pkcs11-helper/options.mk
@@ -0,0 +1,30 @@
+# $NetBSD: options.mk,v 1.1 2009/09/15 20:03:47 spz Exp $
+
+PKG_OPTIONS_VAR=        PKG_OPTIONS.pkcs11-helper
+PKG_SUPPORTED_OPTIONS=  openssl gnutls nss
+PKG_SUGGESTED_OPTIONS=	openssl
+
+.include "../../mk/bsd.options.mk"
+
+# crypto engines to use
+
+.if !empty(PKG_OPTIONS:Mopenssl)
+.include "../../security/openssl/buildlink3.mk"
+CONFIGURE_ARGS+=--enable-crypto-engine-openssl
+.else
+CONFIGURE_ARGS+=--disable-crypto-engine-openssl
+.endif
+
+.if !empty(PKG_OPTIONS:Mgnutls)
+.include "../../security/gnutls/buildlink3.mk"
+CONFIGURE_ARGS+=--enable-crypto-engine-gnutls
+.else
+CONFIGURE_ARGS+=--disable-crypto-engine-gnutls
+.endif
+
+.if !empty(PKG_OPTIONS:Mnss)
+.include "../../devel/nss/buildlink3.mk"
+CONFIGURE_ARGS+=--enable-crypto-engine-nss
+.else
+CONFIGURE_ARGS+=--disable-crypto-engine-nss
+.endif
