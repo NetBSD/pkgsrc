dbfcf5af37be3c27ecf5566460d3fcfcb3426eeb
reed <reed@pkgsrc.org>
2009-09-16 01:15:32 +0000

    Upgrading Xfce to 4.6.1.
    This is for PR #40984. This is based on work from
    dieter roelants. Thank you Dieter.
    Also I had done some work independently on this.
    Hopefully, I am getting this all committed at one time.
    
    The changelogs are at
    http://www.xfce.org/documentation/changelogs/4.6.1
    http://www.xfce.org/documentation/changelogs/4.6.0
    
    Also don't need the xfce4-dev-tools for building. (It is only
    needed when building from SVN source.)

--- x11/xfce4-desktop/buildlink3.mk
+++ x11/xfce4-desktop/buildlink3.mk
@@ -1,21 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.25 2009/08/26 19:57:03 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2009/09/16 01:15:35 reed Exp $
 
 BUILDLINK_TREE+=	xfce4-desktop
 
 .if !defined(XFCE4_DESKTOP_BUILDLINK3_MK)
 XFCE4_DESKTOP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-desktop+=	xfce4-desktop>=4.4.3nb1
-BUILDLINK_ABI_DEPENDS.xfce4-desktop?=	xfce4-desktop>=4.4.3nb2
+BUILDLINK_API_DEPENDS.xfce4-desktop+=	xfce4-desktop>=4.6.0
 BUILDLINK_PKGSRCDIR.xfce4-desktop?=	../../x11/xfce4-desktop
 
-.include "../../graphics/hicolor-icon-theme/buildlink3.mk"
-.include "../../sysutils/dbus-glib/buildlink3.mk"
-.include "../../sysutils/xfce4-thunar/buildlink3.mk"
-.include "../../textproc/libxml2/buildlink3.mk"
-.include "../../x11/xfce4-panel/buildlink3.mk"
-.include "../../devel/xfce4-dev-tools/buildlink3.mk"
-.include "../../devel/glib2/buildlink3.mk"
 .endif # XFCE4_DESKTOP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xfce4-desktop
