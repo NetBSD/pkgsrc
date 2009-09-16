aab221b8a2ffe57848ed2c6cbd15e44fb5195fd7
reed <reed@pkgsrc.org>
2009-09-16 01:15:32 +0000

    Upgrading Xfce to 4.6.1.
    This is for PR #40984. This is based on work from
    dieter roelants. Thank you Dieter.
    Also I had done some work independently on this.
    Hopefully, I am getting this all committed at one time.
    
    The changelogs are at
    http://www.xfce.org/documentation/changelogs/4.6.1
    http://www.xfce.org/documentation/changelogs/4.6.0
    
    Also don't need the xfce4-dev-tools for building. (It is only
    needed when building from SVN source.)

--- wm/xfce4-wm/buildlink3.mk
+++ wm/xfce4-wm/buildlink3.mk
@@ -1,20 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.26 2009/08/26 19:57:02 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2009/09/16 01:15:34 reed Exp $
 
 BUILDLINK_TREE+=	xfce4-wm
 
 .if !defined(XFCE4_WM_BUILDLINK3_MK)
 XFCE4_WM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-wm+=	xfce4-wm>=4.4.3nb1
-BUILDLINK_ABI_DEPENDS.xfce4-wm?=	xfce4-wm>=4.4.3nb2
+BUILDLINK_API_DEPENDS.xfce4-wm+=	xfce4-wm>=4.6.0
 BUILDLINK_PKGSRCDIR.xfce4-wm?=	../../wm/xfce4-wm
 
-.include "../../graphics/hicolor-icon-theme/buildlink3.mk"
-.include "../../x11/libXpm/buildlink3.mk"
-.include "../../x11/xfce4-mcs-plugins/buildlink3.mk"
-.include "../../x11/startup-notification/buildlink3.mk"
-.include "../../devel/xfce4-dev-tools/buildlink3.mk"
-.include "../../devel/glib2/buildlink3.mk"
 .endif # XFCE4_WM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xfce4-wm
--- x11/xfce4-terminal/buildlink3.mk
+++ x11/xfce4-terminal/buildlink3.mk
@@ -1,19 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/08/26 19:57:03 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2009/09/16 01:15:36 reed Exp $
 
 BUILDLINK_TREE+=	xfce4-terminal
 
 .if !defined(XFCE4_TERMINAL_BUILDLINK3_MK)
 XFCE4_TERMINAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-terminal+=	xfce4-terminal>=0.2.8.3nb1
-BUILDLINK_ABI_DEPENDS.xfce4-terminal?=	xfce4-terminal>=0.2.8.3nb4
+BUILDLINK_API_DEPENDS.xfce4-terminal+=	xfce4-terminal>=0.2.10
 BUILDLINK_PKGSRCDIR.xfce4-terminal?=	../../x11/xfce4-terminal
 
-.include "../../graphics/hicolor-icon-theme/buildlink3.mk"
-.include "../../x11/xfce4-exo/buildlink3.mk"
-.include "../../x11/vte/buildlink3.mk"
-.include "../../devel/xfce4-dev-tools/buildlink3.mk"
-.include "../../devel/glib2/buildlink3.mk"
 .endif # XFCE4_TERMINAL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xfce4-terminal
