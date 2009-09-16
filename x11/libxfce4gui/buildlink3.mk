6a1ae5e3c60551eab5e38831e943bd5740d23cc1
reed <reed>
2009-09-16 01:15:32 +0000

    Upgrading Xfce to 4.6.1.
    This is for PR #40984. This is based on work from
    dieter roelants. Thank you Dieter.
    Also I had done some work independently on this.
    Hopefully, I am getting this all committed at one time.
    
    The changelogs are at
    http://www.xfce.org/documentation/changelogs/4.6.1
    http://www.xfce.org/documentation/changelogs/4.6.0
    
    Also don't need the xfce4-dev-tools for building. (It is only
    needed when building from SVN source.)

--- x11/libxfce4gui/buildlink3.mk
+++ x11/libxfce4gui/buildlink3.mk
@@ -1,21 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.26 2009/08/26 19:56:58 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2009/09/16 01:15:34 reed Exp $
 
 BUILDLINK_TREE+=	libxfce4gui
 
 .if !defined(LIBXFCE4GUI_BUILDLINK3_MK)
 LIBXFCE4GUI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxfce4gui+=	libxfce4gui>=4.4.3nb1
-BUILDLINK_ABI_DEPENDS.libxfce4gui?=	libxfce4gui>=4.4.3nb2
+BUILDLINK_API_DEPENDS.libxfce4gui+=	libxfce4gui>=4.6.0
 BUILDLINK_PKGSRCDIR.libxfce4gui?=	../../x11/libxfce4gui
 
-.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/xfconf/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .include "../../x11/libxfce4util/buildlink3.mk"
 .include "../../x11/startup-notification/buildlink3.mk"
-.include "../../graphics/hicolor-icon-theme/buildlink3.mk"
-.include "../../devel/xfce4-dev-tools/buildlink3.mk"
-.include "../../devel/glib2/buildlink3.mk"
 .endif # LIBXFCE4GUI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libxfce4gui
--- x11/xfce4-utils/buildlink3.mk
+++ x11/xfce4-utils/buildlink3.mk
@@ -1,18 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.27 2009/08/26 19:57:03 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2009/09/16 01:15:36 reed Exp $
 
 BUILDLINK_TREE+=	xfce4-utils
 
 .if !defined(XFCE4_UTILS_BUILDLINK3_MK)
 XFCE4_UTILS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-utils+=	xfce4-utils>=4.4.3nb1
-BUILDLINK_ABI_DEPENDS.xfce4-utils?=	xfce4-utils>=4.4.3nb2
+BUILDLINK_API_DEPENDS.xfce4-utils+=	xfce4-utils>=4.6.0
 BUILDLINK_PKGSRCDIR.xfce4-utils?=	../../x11/xfce4-utils
 
-.include "../../graphics/hicolor-icon-theme/buildlink3.mk"
-.include "../../x11/xfce4-mcs-plugins/buildlink3.mk"
-.include "../../devel/xfce4-dev-tools/buildlink3.mk"
-.include "../../devel/glib2/buildlink3.mk"
 .endif # XFCE4_UTILS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xfce4-utils
