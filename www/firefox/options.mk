2199d50b03742c3c566ab7ea6d4f060a65ce17cd
tnn <tnn@pkgsrc.org>
2009-09-16 19:06:18 +0000

    Build firefox against external runtime components from devel/xulrunner. Bump PKGREVISION.

--- www/firefox/options.mk
+++ www/firefox/options.mk
@@ -1,50 +1,16 @@
-# $NetBSD: options.mk,v 1.6 2009/08/30 01:14:50 markd Exp $
+# $NetBSD: options.mk,v 1.7 2009/09/16 19:06:18 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.firefox
-PKG_SUPPORTED_OPTIONS=	debug official-mozilla-branding mozilla-jemalloc gnome
+PKG_SUPPORTED_OPTIONS=	debug official-mozilla-branding
 
-PLIST_VARS+=		branding gnome
-
-.if ${OPSYS} == "Linux" || ${OPSYS} == "SunOS"
-PKG_SUGGESTED_OPTIONS+=	mozilla-jemalloc
-.endif
-
-.if !empty(MACHINE_ARCH:Mi386) || !empty(MACHINE_ARCH:Msparc) || \
-	!empty(MACHINE_ARCH:Marm)
-PKG_SUPPORTED_OPTIONS+= mozilla-jit
-PKG_SUGGESTED_OPTIONS+= mozilla-jit
-.endif
+PLIST_VARS+=		branding
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Mgnome)
-.include "../../devel/libgnome/buildlink3.mk"
-.include "../../sysutils/gnome-vfs/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-gnomevfs --enable-dbus
-PLIST.gnome=		yes
-.else
-CONFIGURE_ARGS+=	--disable-gnomevfs --disable-dbus
-.endif
-
-.if !empty(PKG_OPTIONS:Mmozilla-jemalloc)
-CONFIGURE_ARGS+=	--enable-jemalloc
-. if ${OPSYS} == "SunOS"
-PLIST.jemalloc=		yes
-. endif
-.else
-CONFIGURE_ARGS+=	--disable-jemalloc
-.endif
 
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-debug
 .else
 CONFIGURE_ARGS+=	--disable-debug
-.endif
-
-.if !empty(PKG_OPTIONS:Mmozilla-jit)
-CONFIGURE_ARGS+=	--enable-jit
-.else
-CONFIGURE_ARGS+=	--disable-jit
 .endif
 
 .if !empty(PKG_OPTIONS:Mofficial-mozilla-branding)
