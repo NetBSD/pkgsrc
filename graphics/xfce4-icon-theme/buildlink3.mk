381733f2969470face57649b98e3ab7a0fac5d0a
reed <reed@pkgsrc.org>
2009-09-16 01:15:32 +0000

    Upgrading Xfce to 4.6.1.
    This is for PR #40984. This is based on work from
    dieter roelants. Thank you Dieter.
    Also I had done some work independently on this.
    Hopefully, I am getting this all committed at one time.
    
    The changelogs are at
    http://www.xfce.org/documentation/changelogs/4.6.1
    http://www.xfce.org/documentation/changelogs/4.6.0
    
    Also don't need the xfce4-dev-tools for building. (It is only
    needed when building from SVN source.)

--- graphics/xfce4-icon-theme/buildlink3.mk
+++ graphics/xfce4-icon-theme/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2009/08/26 19:56:58 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2009/09/16 01:15:33 reed Exp $
 
 BUILDLINK_TREE+=	xfce4-icon-theme
 
@@ -6,11 +6,9 @@ BUILDLINK_TREE+=	xfce4-icon-theme
 XFCE4_ICON_THEME_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xfce4-icon-theme+=	xfce4-icon-theme>=4.4.3
-BUILDLINK_ABI_DEPENDS.xfce4-icon-theme?=	xfce4-icon-theme>=4.4.3nb1
 BUILDLINK_PKGSRCDIR.xfce4-icon-theme?=	../../graphics/xfce4-icon-theme
 
 .include "../../graphics/hicolor-icon-theme/buildlink3.mk"
-.include "../../devel/xfce4-dev-tools/buildlink3.mk"
 .include "../../x11/libSM/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
--- x11/xfce4-panel/buildlink3.mk
+++ x11/xfce4-panel/buildlink3.mk
@@ -1,20 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.24 2009/08/26 19:57:02 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2009/09/16 01:15:35 reed Exp $
 
 BUILDLINK_TREE+=	xfce4-panel
 
 .if !defined(XFCE4_PANEL_BUILDLINK3_MK)
 XFCE4_PANEL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-panel+=	xfce4-panel>=4.4.3nb1
-BUILDLINK_ABI_DEPENDS.xfce4-panel?=	xfce4-panel>=4.4.3nb2
+BUILDLINK_API_DEPENDS.xfce4-panel+=	xfce4-panel>=4.6.0
 BUILDLINK_PKGSRCDIR.xfce4-panel?=	../../x11/xfce4-panel
 
-.include "../../graphics/hicolor-icon-theme/buildlink3.mk"
-.include "../../textproc/libxml2/buildlink3.mk"
-.include "../../x11/xfce4-mcs-plugins/buildlink3.mk"
-.include "../../x11/startup-notification/buildlink3.mk"
-.include "../../devel/xfce4-dev-tools/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/libxfce4gui/buildlink3.mk"
+.include "../../x11/libxfce4util/buildlink3.mk"
 .endif # XFCE4_PANEL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xfce4-panel
