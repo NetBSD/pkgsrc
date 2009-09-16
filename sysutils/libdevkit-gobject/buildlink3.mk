bb82a8d3515d29394869e37ac0acb5b3b44fdd58
wiz <wiz@pkgsrc.org>
2009-09-16 19:03:10 +0000

    Initial import of libdevkit-gobject-003, part of DeviceKit. Based on work by jmcneill.
    DeviceKit is an abstraction for enumerating devices and listening
    to device events. Any application on the system can access the
    org.freedesktop.DeviceKit service via the system message bus. On
    GNU/Linux, DeviceKit can be considered a simple D-Bus frontend to
    udev(7).
    
    This package contains devkit and the devkit-gobject library.

---
+++ sysutils/libdevkit-gobject/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/09/16 19:03:10 wiz Exp $
+
+BUILDLINK_TREE+=	libdevkit-gobject
+
+.if !defined(LIBDEVKIT_GOBJECT_BUILDLINK3_MK)
+LIBDEVKIT_GOBJECT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libdevkit-gobject+=	libdevkit-gobject>=003
+BUILDLINK_PKGSRCDIR.libdevkit-gobject?=	../../sysutils/libdevkit-gobject
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../sysutils/dbus-glib/buildlink3.mk"
+.endif	# LIBDEVKIT_GOBJECT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libdevkit-gobject
