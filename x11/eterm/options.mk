15056aa359a74b5ef4d6bed793dc697567e776d9
obache <obache@pkgsrc.org>
2009-09-21 07:29:30 +0000

    Update Eterm to 0.9.5. Changes in pkgsrc:  * remove patch-ac.      It is noted for CVE-2008-1142. but the CVE is for rxvt.      CVE-2008-1692 is for Eterm and fixed by this release.  * add xim option and enabled by default  * enable mult-charset and auto-encoding  * replace bg images with currently distributed one.
    Changes in upsteram:
    Wed Oct 18 13:35:18 2006                        Michael Jennings (mej)
    
    Fixed a typo and some logic errors in libscream located by Mike
    Frysinger <vapier@gentoo.org>.
    ----------------------------------------------------------------------
    Mon Oct 30 16:11:47 2006                        Michael Jennings (mej)
    
    Patch from Jason McCarver <slam@parasite.cc> to support -S/--sticky
    option for "sticky" (i.e., present on all desktops) startup.
    ----------------------------------------------------------------------
    Tue Jul  3 20:48:46 2007                        Michael Jennings (mej)
    
    Remove some cruft from kEsetroot.
    ----------------------------------------------------------------------
    Wed May 14 12:42:51 2008                        Michael Jennings (mej)
    
    Patch from Kim Woelders <kim@woelders.dk>:
    
        There is a race problem with Eterm during startup related to the
        shell LINES/COLUMNS env vars.
    
        If the WM changes the window size (e.g. due to saved settings)
        before mapping the window, sometimes the shell will set LINES and
        COLUMNS according to the old/incorrect size and sometimes to the
        new/correct size, depending on wheter the call to tt_winsize() at
        command.c line 2322 (by the shell child process) or the
        tt_resize() (by the Eterm process) due to the ConfigureNotify
        caused by the resize (or WM ICCCM ConfigureNotify) operation
        happens first.
    
    The call in question was added by Azundris for Escreen.  So far
    Escreen seems to be behaving properly with this patch applied, but all
    my Eterm windows (Escreen and otherwise) are pre-sized with -g anyway.
    So I'm going to keep my eye on it for awhile.  In case of trouble,
    change the "#if 0" to "#ifdef ESCREEN" to revert to previous behavior
    when in Escreen mode.  Normal operation should not require the call in
    question.
    ----------------------------------------------------------------------
    Wed May 14 14:54:16 2008                        Michael Jennings (mej)
    
    Modified patch from hsim@gmx.li to allow setting of the "Urgent" hint
    on beep.
    ----------------------------------------------------------------------
    Wed May 14 15:26:13 2008                        Michael Jennings (mej)
    
    Patch from Emmanuel Anne <emmanuel.anne@gmail.com> to fix cut/paste
    with KDE applications.
    ----------------------------------------------------------------------
    Wed May 14 16:09:04 2008                        Michael Jennings (mej)
    
    (Correct) fix for CVE-2008-1692.  Eterm no longer defaults to using
    ":0" for $DISPLAY due to the possibility that an attacker can create a
    fake X server on a shared system, intercept the Eterm X connection,
    and send fake keystrokes to the victim's Eterm to execute arbitrary
    commands as that user.
    
    The previous fix, while it did indeed correct the vulnerability, broke
    the --display option.  The original fix from Bernhard Link was more
    correct, albeit not quite on target.

--- x11/eterm/options.mk
+++ x11/eterm/options.mk
@@ -1,10 +1,11 @@
-# $NetBSD: options.mk,v 1.2 2008/01/27 16:10:25 rillig Exp $
+# $NetBSD: options.mk,v 1.3 2009/09/21 07:29:30 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.eterm
+PKG_SUPPORTED_OPTIONS=	xim
+PKG_SUGGESTED_OPTIONS=	xim
 
 .include "../../mk/bsd.prefs.mk"
 
-PKG_SUPPORTED_OPTIONS=	# none
 .if ${MACHINE_ARCH} == "i386"
 PKG_SUPPORTED_OPTIONS+=	mmx
 .endif
@@ -13,4 +14,8 @@ PKG_SUPPORTED_OPTIONS+=	mmx
 
 .if !empty(PKG_OPTIONS:Mmmx)
 CONFIGURE_ARGS+=	--enable-mmx
+.endif
+
+.if !empty(PKG_OPTIONS:Mxim)
+CONFIGURE_ARGS+=	--enable-xim
 .endif
