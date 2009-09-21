16a4aef204a2c4464d8028d8df8468c6d3358be1
martti <martti@pkgsrc.org>
2009-09-21 17:35:55 +0000

    Updated editors/vim-share to 7.2.262
      1870  7.2.255  (after 7.2.242) cursor column may be wrong after :set
      1783  7.2.256  GTK font dialog doesn't have a default when 'guifont' not set
      2092  7.2.257  GTK 2.17: lots of assertion error messages
      1989  7.2.258  v:beval_col and b:beval_text are wrong in UTF-8 text
      5393  7.2.259  exists() doesn't work properly for an empty aucmd group
      5431  7.2.260  (extra part of 7.2.259)
      2515  7.2.261  E38 may appear when deleting folded lines
      5652  7.2.262  string passed to user cmd custom completion can be too long

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.69 2009/09/12 15:34:38 martti Exp $
+# $NetBSD: version.mk,v 1.70 2009/09/21 17:35:55 martti Exp $
 
 VIM_VERSION=		7.2
-VIM_PATCHLEVEL=		254
+VIM_PATCHLEVEL=		262
 VIM_SUBDIR=		vim72
