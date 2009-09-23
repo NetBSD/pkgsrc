62b5eba0829bf25855a45905c4ce27c34d33060d
jmmv <jmmv@pkgsrc.org>
2009-09-23 16:03:25 +0000

    Update to 1.8.7.  Too many changes to list here.
    This update is quite delicate and I'm sure it'll break somewhere.  So far
    I've only been able to test it in NetBSD/amd64 and Mac OS X Leopard.
    
    I'm bumping the dependency version in buildlink3.mk because the only package
    using this seems to be Monotone, and I'll updating it right away.

--- security/botan/buildlink3.mk
+++ security/botan/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2009/03/20 19:25:16 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2009/09/23 16:03:25 jmmv Exp $
 
 BUILDLINK_TREE+=	botan
 
 .if !defined(BOTAN_BUILDLINK3_MK)
 BOTAN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.botan+=	botan>=1.4.11
+BUILDLINK_API_DEPENDS.botan+=	botan>=1.8.7
 BUILDLINK_PKGSRCDIR.botan?=	../../security/botan
 .endif # BOTAN_BUILDLINK3_MK
 
