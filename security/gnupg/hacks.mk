12dd4471939ec813e57f82c088e812ffce85e615
tnn <tnn@pkgsrc.org>
2009-09-28 20:15:08 +0000

    - AIX fixes from Louis Guillaume - kill a trailing whitespace

--- security/gnupg/hacks.mk
+++ security/gnupg/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.4 2007/02/22 19:27:07 wiz Exp $
+# $NetBSD: hacks.mk,v 1.5 2009/09/28 20:15:08 tnn Exp $
 
 .if !defined(GNUPG_HACKS_MK)
 GNUPG_HACKS_MK=	# defined
@@ -9,6 +9,14 @@ GNUPG_HACKS_MK=	# defined
 .if ${MACHINE_ARCH} == "sparc"
 PKG_HACKS+=		mcpu
 BUILDLINK_TRANSFORM+=	rm:-mcpu=.*
+.endif
+
+# Turn off optimization in CFLAGS for AIX. Otherwise
+# decryption fails with, "Rijndael-128 test encryption failed."
+# at runtime.
+.if ${OPSYS} == "AIX"
+PKG_HACKS+=	aix-codegen
+CFLAGS:=	-O0 ${CFLAGS:C/-O[0-9]?//g}
 .endif
 
 .endif
