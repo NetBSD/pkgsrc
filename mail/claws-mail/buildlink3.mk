9a7a9cf1b7cf3f80278644ee874a35c47ad061fe
drochner <drochner@pkgsrc.org>
2009-09-30 08:51:50 +0000

    propagate "enchant" dependency, fixes build of some "extra" (ie not bundled) plugins

--- mail/claws-mail/buildlink3.mk
+++ mail/claws-mail/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/03/20 19:24:54 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/09/30 08:51:50 drochner Exp $
 
 BUILDLINK_TREE+=	claws-mail
 
@@ -7,6 +7,8 @@ CLAWS_MAIL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.claws-mail+= claws-mail>=3.7.0
 BUILDLINK_PKGSRCDIR.claws-mail?=	../../mail/claws-mail
+
+.include "../../textproc/enchant/buildlink3.mk"
 .endif # CLAWS_MAIL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-claws-mail
