1a5fe64202597f1b1d18e72e08a7426d0eedefd8
hasso <hasso@pkgsrc.org>
2009-10-02 09:59:40 +0000

    Add perl bindings for LibPreludeDB libraries. Approved by agc@.

---
+++ security/libpreludedb-perl/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/10/02 09:59:40 hasso Exp $
+
+BUILDLINK_TREE+=	libpreludedb-perl
+
+.if !defined(LIBPRELUDEDB_PERL_BUILDLINK3_MK)
+LIBPRELUDEDB_PERL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libpreludedb-perl+=	libpreludedb-perl>=0.9.15.3
+BUILDLINK_PKGSRCDIR.libpreludedb-perl?=	../../security/libpreludedb-perl
+
+.endif	# LIBPRELUDEDB_PERL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libpreludedb-perl
