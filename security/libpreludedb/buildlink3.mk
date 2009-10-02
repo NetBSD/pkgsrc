904454dcdcfff6fe5aac00fd6973e965d4c5ff4e
hasso <hasso@pkgsrc.org>
2009-10-02 09:50:38 +0000

    Split up the libpreludedb. Approved by agc@.

--- security/libpreludedb/buildlink3.mk
+++ security/libpreludedb/buildlink3.mk
@@ -1,16 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.12 2009/03/20 19:25:19 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2009/10/02 09:50:38 hasso Exp $
 
 BUILDLINK_TREE+=	libpreludedb
 
 .if !defined(LIBPRELUDEDB_BUILDLINK3_MK)
 LIBPRELUDEDB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libpreludedb+=	libpreludedb>=0.9.3
-BUILDLINK_ABI_DEPENDS.libpreludedb?=	libpreludedb>=0.9.13nb2
+BUILDLINK_API_DEPENDS.libpreludedb+=	libpreludedb>=0.9.15.3
 BUILDLINK_PKGSRCDIR.libpreludedb?=	../../security/libpreludedb
 
 .include "../../security/libprelude/buildlink3.mk"
-.include "../../lang/perl5/buildlink3.mk"
-.endif # LIBPRELUDEDB_BUILDLINK3_MK
+.include "../../devel/libltdl/buildlink3.mk"
+.endif	# LIBPRELUDEDB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libpreludedb
