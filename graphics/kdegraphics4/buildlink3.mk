19dac79f75cec13b5b9205704ef70f10e7d7f176
wiz <wiz@pkgsrc.org>
2009-10-03 07:09:22 +0000

    Remove wip artefacts.

--- graphics/kdegraphics4/buildlink3.mk
+++ graphics/kdegraphics4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/10/02 20:11:00 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/10/03 07:09:22 wiz Exp $
 
 BUILDLINK_TREE+=	kdegraphics4
 
@@ -10,7 +10,7 @@ BUILDLINK_ABI_DEPENDS.kdegraphics4?=	kdegraphics4>=4.1.3nb1
 BUILDLINK_PKGSRCDIR.kdegraphics4?=	../../graphics/kdegraphics4
 
 .include "../../graphics/tiff/buildlink3.mk"
-.include "../../wip/kdelibs4/buildlink3.mk"
+.include "../../x11/kdelibs4/buildlink3.mk"
 .endif # KDEGRAPHICS4_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kdegraphics4
