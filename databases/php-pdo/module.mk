139613203264f57d8afa167d73e81d677309e426
taca <taca>
2009-10-07 05:01:51 +0000

    Remove workaround code which isn't used any more.

--- databases/php-pdo/module.mk
+++
@@ -1,7 +0,0 @@
-# $NetBSD: module.mk,v 1.8 2009/03/13 10:57:20 sborrill Exp $
-
-# This file is now only needed to patch up PKGNAME until PHP 5.2.10 or later is
-# released. 
-.if "${PHP_BASE_VERS}" == "5.2.9"
-PKGNAME=		${PHP_PKG_PREFIX}-${MODNAME}-${PHP_BASE_VERS}.99
-.endif
