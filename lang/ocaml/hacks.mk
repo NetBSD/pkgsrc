f9ec0479ecd3f36b364e085d5ce55b9d6085c36a
dmcmahill <dmcmahill@pkgsrc.org>
2009-10-08 01:24:51 +0000

    gcc44 fix

---
+++ lang/ocaml/hacks.mk
@@ -0,0 +1,20 @@
+# $NetBSD: hacks.mk,v 1.1 2009/10/08 01:24:51 dmcmahill Exp $
+
+.if !defined(OCAML_HACKS_MK)
+OCAML_HACKS_MK=   defined
+
+.include "../../mk/compiler.mk"
+
+### [ Wed Oct  7 9:16:42 GMT 2009 : dmcmahill ]
+### On NetBSD/i386, gcc optimisation, at least for version 4.4.1 (lang/gcc44),
+### produces a binary which segfaults.  The binary is used during part of the
+### build process.
+###
+.if !empty(MACHINE_PLATFORM:MNetBSD-*-i386)
+.  if !empty(CC_VERSION:Mgcc-4.4.*)
+PKG_HACKS+=             optimisation
+BUILDLINK_TRANSFORM+=   rename:-O[0-9]*:-O
+.  endif
+.endif
+
+.endif  # OCAML_HACKS_MK
