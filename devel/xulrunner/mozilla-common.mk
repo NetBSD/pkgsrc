195c3b3ccbef3228a8375dc45e67a0a7b91594cb
tnn <tnn@pkgsrc.org>
2009-10-10 20:01:31 +0000

    add common Makefile fragment for mozilla packages based on gecko 1.9.1, such as xulrunner-1.9.1, firefox-3.5, thunderbird-3.0 and seamonkey-2.0. Nothing in the tree uses this file yet. Having it here now makes for one less pullup later.

---
+++ devel/xulrunner/mozilla-common.mk
@@ -0,0 +1,66 @@
+# $NetBSD: mozilla-common.mk,v 1.1 2009/10/10 20:01:31 tnn Exp $
+#
+# common Makefile fragment for mozilla packages based on gecko 1.9.1.
+
+GNU_CONFIGURE=		yes
+USE_TOOLS+=		pkg-config perl gmake autoconf213
+USE_LANGUAGES+=		c c++
+
+BUILD_DEPENDS+=		zip>=2.3:../../archivers/zip
+
+PKG_DESTDIR_SUPPORT=	user-destdir
+CHECK_PORTABILITY_SKIP+=${MOZILLA_DIR}security/nss/tests/libpkix/libpkix.sh
+PRIVILEGED_STAGES+=	clean
+
+CONFIGURE_ARGS+=	--disable-tests --disable-pedantic
+CONFIGURE_ARGS+=	--enable-crypto
+CONFIGURE_ARGS+=	--enable-optimize=-O2 --with-pthreads
+CONFIGURE_ARGS+=	--disable-javaxpcom
+CONFIGURE_ARGS+=	--enable-default-toolkit=cairo-gtk2
+CONFIGURE_ARGS+=	--enable-svg --enable-mathml
+CONFIGURE_ARGS+=	--enable-system-cairo
+CONFIGURE_ARGS+=	--with-system-jpeg
+CONFIGURE_ARGS+=	--with-system-zlib --with-system-bz2
+CONFIGURE_ARGS+=	--enable-system-sqlite
+CONFIGURE_ARGS+=	--disable-crashreporter
+
+SUBST_CLASSES+=			fix-paths
+SUBST_STAGE.fix-paths=		pre-configure
+SUBST_MESSAGE.fix-paths=	Fixing absolute paths.
+SUBST_FILES.fix-paths=		${MOZILLA_DIR}xpcom/build/nsXPCOMPrivate.h
+SUBST_FILES.fix-paths+=		${MOZILLA_DIR}xulrunner/app/nsRegisterGREUnix.cpp
+SUBST_FILES.fix-paths+=		${MOZILLA_DIR}xulrunner/installer/Makefile.in
+SUBST_FILES.fix-paths+=		${MOZILLA_DIR}extensions/java/xpcom/interfaces/org/mozilla/xpcom/Mozilla.java
+SUBST_FILES.fix-paths+=		${MOZILLA_DIR}xpcom/io/nsAppFileLocationProvider.cpp
+SUBST_SED.fix-paths=		-e 's,/etc/gre.d,${PREFIX}/lib/xulrunner/gre.d,g'
+SUBST_SED.fix-paths+=		-e 's,/usr/lib/mozilla/plugins,${PREFIX}/lib/netscape/plugins,g'
+
+CONFIG_GUESS_OVERRIDE+=		${MOZILLA_DIR}build/autoconf/config.guess
+CONFIG_GUESS_OVERRIDE+=		${MOZILLA_DIR}js/src/build/autoconf/config.guess
+CONFIG_GUESS_OVERRIDE+=		${MOZILLA_DIR}nsprpub/build/autoconf/config.guess
+CONFIG_SUB_OVERRIDE+=		${MOZILLA_DIR}build/autoconf/config.sub
+CONFIG_SUB_OVERRIDE+=		${MOZILLA_DIR}js/src/build/autoconf/config.sub
+CONFIG_SUB_OVERRIDE+=		${MOZILLA_DIR}nsprpub/build/autoconf/config.sub
+
+PYTHON_FOR_BUILD_ONLY=		yes
+.include "../../lang/python/application.mk"
+CONFIGURE_ENV+=		PYTHON=${PYTHONBIN:Q}
+
+# Makefiles sometimes call "rm -f" without more arguments. Kludge around ...
+.PHONY: create-rm-wrapper
+pre-configure: create-rm-wrapper
+create-rm-wrapper:
+	printf '#!/bin/sh\n[ "$$*" = "-f" ] && exit 0\nexec /bin/rm $$@\n' > \
+	  ${WRAPPER_DIR}/bin/rm
+	chmod +x ${WRAPPER_DIR}/bin/rm
+
+.if ${OPSYS} == "Linux"
+.include "../../audio/alsa-lib/buildlink3.mk"
+.endif
+.include "../../archivers/bzip2/buildlink3.mk"
+BUILDLINK_API_DEPENDS.sqlite3+=	sqlite3>=3.6.10
+.include "../../databases/sqlite3/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../net/libIDL/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
