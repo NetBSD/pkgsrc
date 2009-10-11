6aae20d414785392a1978a514eca29eac2701c4c
plunky <plunky>
2009-10-11 10:22:51 +0000

    fix a problem with the rc script where the command would not start at boot time. This is because the & is within the command_args string. use a start_cmd instead.

--- mail/nullmailer/files/nullmailer.sh
+++ mail/nullmailer/files/nullmailer.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: nullmailer.sh,v 1.2 2005/08/20 02:20:45 schmonz Exp $
+# $NetBSD: nullmailer.sh,v 1.3 2009/10/11 10:22:51 plunky Exp $
 #
 # PROVIDE: mail
 # REQUIRE: LOGIN
@@ -15,10 +15,16 @@ required_files="@PKG_SYSCONFDIR@/nullmailer/remotes"
 required_files="${required_files} @VARBASE@/spool/nullmailer/trigger"
 required_dirs="@VARBASE@/spool/nullmailer/queue @VARBASE@/spool/nullmailer/tmp"
 command="@PREFIX@/libexec/nullmailer/nullmailer-send"
-command_args="2>&1 | logger -t nullmailer -p mail.info &"
+start_cmd="nullmailer_start"
 
 nullmailer_user="@NULLMAILER_USER@"
 nullmailer_group="@NULLMAILER_GROUP@"
+
+nullmailer_start()
+{
+
+	${command} 2>&1 | logger -t ${name} -p mail.info &
+}
 
 if [ -f /etc/rc.subr ]; then
 	load_rc_config $name
