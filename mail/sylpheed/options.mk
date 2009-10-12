11117638f54ae6c6f73759d84f3c63f2875124b3
ahoka <ahoka@pkgsrc.org>
2009-10-12 19:47:40 +0000

    Have spellchecker by default Take maintainership from Chris (I have asked him in private mail) Remove PKG_REVPATH, I think we gave enough time.

--- mail/sylpheed/options.mk
+++ mail/sylpheed/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.5 2008/06/29 11:38:32 tnn Exp $
+# $NetBSD: options.mk,v 1.6 2009/10/12 19:47:40 ahoka Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sylpheed
 PKG_SUPPORTED_OPTIONS=	jpilot ldap inet6 gpgme gtkspell
-PKG_SUGGESTED_OPTIONS=	inet6 gpgme
+PKG_SUGGESTED_OPTIONS=	inet6 gpgme gtkspell
 
 .include "../../mk/bsd.options.mk"
 
