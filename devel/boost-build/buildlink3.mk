d34ad4ac951749c220f2f752d588875078b2b5d2
adam <adam>
2009-10-14 06:40:07 +0000

    Changes 1.40.0: * Build System improvements. * Updated Libraries: Accumulators, Asio, Circular Buffer, Foreach, Function,   Fusion, Hash, Interprocess, Intrusive, MPL, Program.Options, Proto, Random,   Serialization, Unordered, Xpressive.
    Changes 1.39.0:
    * New Libraries: Signals2.
    * Updated Libraries: Asio, Flyweight, Foreach, Hash, Interprocess, Intrusive,
      Program.Options, Proto, PtrContainer, Range, Unordered, Xpressive.
    * Updated Tools: Boostbook, Quickbook.

--- devel/boost-build/buildlink3.mk
+++ devel/boost-build/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:24:10 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2009/10/14 06:40:07 adam Exp $
 
 BUILDLINK_TREE+=	boost-build
 
 .if !defined(BOOST_BUILD_BUILDLINK3_MK)
 BOOST_BUILD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-build+=		boost-build-1.38.*
+BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.40.*
 BUILDLINK_DEPMETHOD.boost-build?=	build
 BUILDLINK_PKGSRCDIR.boost-build?=	../../devel/boost-build
 .endif # BOOST_BUILD_BUILDLINK3_MK
