5375192651b3317585bcf93289408e721543af22
adam <adam@pkgsrc.org>
2009-10-14 06:40:07 +0000

    Changes 1.40.0: * Build System improvements. * Updated Libraries: Accumulators, Asio, Circular Buffer, Foreach, Function,   Fusion, Hash, Interprocess, Intrusive, MPL, Program.Options, Proto, Random,   Serialization, Unordered, Xpressive.
    Changes 1.39.0:
    * New Libraries: Signals2.
    * Updated Libraries: Asio, Flyweight, Foreach, Hash, Interprocess, Intrusive,
      Program.Options, Proto, PtrContainer, Range, Unordered, Xpressive.
    * Updated Tools: Boostbook, Quickbook.

--- devel/boost-python/buildlink3.mk
+++ devel/boost-python/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2009/03/20 19:24:10 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2009/10/14 06:40:08 adam Exp $
 
 BUILDLINK_TREE+=	boost-python
 
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	boost-python
 BOOST_PYTHON_BUILDLINK3_MK:=
 
 # Use a dependency pattern that guarantees the proper ABI.
-BUILDLINK_API_DEPENDS.boost-python+=	boost-python-1.38.*
-BUILDLINK_ABI_DEPENDS.boost-python?=	boost-python>=1.38.*
+BUILDLINK_API_DEPENDS.boost-python+=	boost-python-1.40.*
+BUILDLINK_ABI_DEPENDS.boost-python?=	boost-python>=1.40.*
 BUILDLINK_PKGSRCDIR.boost-python?=	../../devel/boost-python
 
 .include "../../devel/boost-headers/buildlink3.mk"
