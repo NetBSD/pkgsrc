10547f539a26207defa11ee712224b1b9c5882f0
wiz <wiz@pkgsrc.org>
2009-10-18 22:29:10 +0000

    libtool archive references gnome-keyring, so include its buildlink3.mk here.

--- net/libsoup24/buildlink3.mk
+++ net/libsoup24/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/04/12 16:40:00 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/10/18 22:29:10 wiz Exp $
 
 BUILDLINK_TREE+=	libsoup24
 
@@ -11,6 +11,7 @@ BUILDLINK_PKGSRCDIR.libsoup24?=		../../net/libsoup24
 
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
+.include "../../security/gnome-keyring/buildlink3.mk"
 .include "../../security/gnutls/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../www/libproxy/buildlink3.mk"
