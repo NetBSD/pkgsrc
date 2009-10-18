815a8925157ba295f35d4d8d59cdf35ed170ce4c
tnn <tnn@pkgsrc.org>
2009-10-18 10:21:01 +0000

    Add initial support for DragonFly/i386 2.4. (only build tested)

--- lang/openjdk7/bootstrap.mk
+++ lang/openjdk7/bootstrap.mk
@@ -1,6 +1,7 @@
-# $NetBSD: bootstrap.mk,v 1.1.1.1 2009/10/14 11:26:15 tnn Exp $
+# $NetBSD: bootstrap.mk,v 1.2 2009/10/18 10:21:01 tnn Exp $
 
 ONLY_FOR_PLATFORM=	NetBSD-[456].*-i386 NetBSD-[456].*-x86_64
+ONLY_FOR_PLATFORM+=	DragonFly-2.4.*-i386
 
 BOOT.nb4-i386=		bootstrap-jdk-7-bin-netbsd-4-i386-20091012.tar.bz2
 # not yet
@@ -9,6 +10,7 @@ BOOT.nb5-i386=		bootstrap-jdk-7-bin-netbsd-5-i386-20090926.tar.bz2
 BOOT.nb5-amd64=		bootstrap-jdk-7-bin-netbsd-5-amd64-20090925.tar.bz2
 BOOT.nb6-i386=		bootstrap-jdk-7-bin-netbsd-6-i386-20091014.tar.bz2
 BOOT.nb6-amd64=		bootstrap-jdk-7-bin-netbsd-6-amd64-20091013.tar.bz2
+BOOT.df24-i386=		bootstrap-jdk-7-bin-dragonfly-2_4-i386-20091018.tar.bz2
 
 .if !empty(MACHINE_PLATFORM:MNetBSD-4.[0-8]*-i386) || make(distinfo)
 DISTFILES+=		${BOOT.nb4-i386}
@@ -40,6 +42,11 @@ EXTRACT_ONLY+=		${BOOT.nb6-i386}
     !empty(MACHINE_PLATFORM:MNetBSD-5.99.*-x86_64)
 DISTFILES+=		${BOOT.nb6-amd64}
 EXTRACT_ONLY+=		${BOOT.nb6-amd64}
+.endif
+
+.if !empty(MACHINE_PLATFORM:MDragonFly-2.4.*-i386) || make(distinfo)
+DISTFILES+=		${BOOT.df24-i386}
+EXTRACT_ONLY+=		${BOOT.df24-i386}
 .endif
 
 ALT_BOOTDIR=		${WRKDIR}/bootstrap
