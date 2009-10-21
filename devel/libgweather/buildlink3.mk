ee6356063e7c5b636ef06ef1f7434b95ef4abeb7
drochner <drochner@pkgsrc.org>
2009-10-21 15:53:23 +0000

    update to 2.28.0 This switches to the gnome-2.28 release branch.

--- devel/libgweather/buildlink3.mk
+++ devel/libgweather/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/08/26 19:56:57 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2009/10/21 15:53:23 drochner Exp $
 
 BUILDLINK_TREE+=	libgweather
 
@@ -11,7 +11,6 @@ BUILDLINK_PKGSRCDIR.libgweather?=	../../devel/libgweather
 
 .include "../../net/libsoup24/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
-.include "../../sysutils/gnome-vfs/buildlink3.mk"
 .endif # LIBGWEATHER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libgweather
