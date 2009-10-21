deb903091bd33d921316d6f533e9f618bda976b6
drochner <drochner@pkgsrc.org>
2009-10-21 16:12:03 +0000

    update to 2.28.1 This switches to the gnome-2.28 release branch.

--- multimedia/totem-pl-parser/buildlink3.mk
+++ multimedia/totem-pl-parser/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/08/26 19:56:53 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/10/21 16:12:03 drochner Exp $
 
 BUILDLINK_TREE+=	totem-pl-parser
 
@@ -13,6 +13,7 @@ BUILDLINK_PKGSRCDIR.totem-pl-parser?=	../../multimedia/totem-pl-parser
 .include "../../x11/gtk2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../mail/evolution-data-server/buildlink3.mk"
+.include "../../mail/gmime24/buildlink3.mk"
 .endif # TOTEM_PL_PARSER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-totem-pl-parser
