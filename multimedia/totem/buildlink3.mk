b3bbf61e2548fa50a647c1c52c23d1d49b27b464
drochner <drochner@pkgsrc.org>
2009-10-21 18:56:15 +0000

    update to 2.28.1 This switches to the gnome-2.28 release branch.

--- multimedia/totem/buildlink3.mk
+++ multimedia/totem/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/08/26 19:57:00 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2009/10/21 18:56:15 drochner Exp $
 
 BUILDLINK_TREE+=	totem
 
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.totem+=	{totem,totem-xine}>=1.4.0
 BUILDLINK_ABI_DEPENDS.totem+=	totem>=2.26.3nb1
 BUILDLINK_PKGSRCDIR.totem?=	../../multimedia/totem
 
-.include "../../sysutils/gnome-vfs/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .endif # TOTEM_BUILDLINK3_MK
