20512f16d16ed3b0f736e2a665dcc9651c196b3f
drochner <drochner@pkgsrc.org>
2009-10-22 17:48:39 +0000

    update to 2.28.1 This switches to the gnome-2.28 release branch.

--- multimedia/gnome-media/buildlink3.mk
+++ multimedia/gnome-media/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2009/08/26 19:56:46 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2009/10/22 17:48:39 drochner Exp $
 
 BUILDLINK_TREE+=	gnome-media
 
@@ -11,8 +11,6 @@ BUILDLINK_PKGSRCDIR.gnome-media?=	../../multimedia/gnome-media
 
 .include "../../devel/GConf-ui/buildlink3.mk"
 .include "../../devel/libglade/buildlink3.mk"
-.include "../../devel/libgnome/buildlink3.mk"
-.include "../../devel/libgnomeui/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .endif # GNOME_MEDIA_BUILDLINK3_MK
 
