e33ed81120e252bd6a72eee1b4243f71ed311da9
drochner <drochner@pkgsrc.org>
2009-10-22 17:50:53 +0000

    update to 3.28.1 This switches to the gnome-2.28 release branch.

--- www/gtkhtml314/buildlink3.mk
+++ www/gtkhtml314/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2009/08/26 19:56:46 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2009/10/22 17:50:53 drochner Exp $
 
 BUILDLINK_TREE+=	gtkhtml314
 
@@ -9,8 +9,7 @@ BUILDLINK_API_DEPENDS.gtkhtml314+=	gtkhtml314>=3.24.0
 BUILDLINK_ABI_DEPENDS.gtkhtml314?=	gtkhtml314>=3.26.3nb1
 BUILDLINK_PKGSRCDIR.gtkhtml314?=	../../www/gtkhtml314
 
-.include "../../devel/libglade/buildlink3.mk"
-.include "../../devel/libgnomeui/buildlink3.mk"
+.include "../../devel/GConf/buildlink3.mk"
 .include "../../textproc/enchant/buildlink3.mk"
 .include "../../textproc/iso-codes/buildlink3.mk"
 .endif # GTKHTML314_BUILDLINK3_MK
