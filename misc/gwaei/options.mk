16d5af124e67573ae89200864ccb8f9eaa9dbbca
wiz <wiz@pkgsrc.org>
2009-10-22 22:27:23 +0000

    Update to 1.0.1:
    Version 1.0.1
    
    Another release. The most important fix in this bugfix release is
    where the romanji->hiragana conversion engine would convert "ge"
    as "ke". Most othe bug fixes have to do with building the program
    on different archintectures. If you have the program and it is
    working fine for you, you may want to wait until gWaei 1.1 releases
    in a week or so.
    
    June 14, 2009
    
    Version 1.0
    
    Yay! It finally happened. This will be the stable version for a
    while. I'm a bit sad that I had to drop support for Debian 5.0
    (Lenny) though. Fortunately, it will be compatible with the next
    version. The next stable version of gWaei will be 1.1. If you want
    to see something fixed for it, please submit a bug report! gWaei
    2.0 will have new features, a reworked interface, and hopefully be
    all around better and more polished.
    
    May 27, 2009
    
    Version 1.0rc1
    
    There were some small tweaks to the progam's code to get it to
    compile under Windows correctly. There was also a small memory leak
    when opening the help menu. This will be the last revision before
    1.0!
    
    May 16, 2009
    
    Version 1.0b3
    
    Small changes. Radicals have been reordered and some holes filled.
    The number separators for the radicals have been made red to make
    them more visible. RPM building has been slightly reworked. Libgnome
    is no longer a dependancy.
    
    April 25, 2009
    
    Version 1.0b1
    
    Most important thing fixed in this release is a minor buffer overflow
    that caused the program to crash on Ubuntu 9. I have made some
    headway on MS Windows support, but still am not 100% there yet.
    The last important change is that when gnome is turned off in the
    configuration for the build, GConf is no longer a forced dependancy.
    
    Onward-ho to 1.0!
    
    March 29, 2009
    
    Version 0.15.4
    
    This release brings some small changes and fixes. I have to thank
    all of the contributors who helped. I have never had so many people
    helping by submitting bugs, translations, and fixes. Thank you all!
    
    The major notables this time are a reworked Radical Search Tool
    window and a fix so that Mac OS X users should be bale to compile
    the program.
    
    I want to eventually release a 1.0 version of gWaei so that people
    have something stable to use. In order to do this, I want to squash
    the last of the large bugs and make the program usable on MS Windows
    in the basic sense. If anyone wants to help, email me or contact
    me through my blog.

---
+++ misc/gwaei/options.mk
@@ -0,0 +1,28 @@
+# $NetBSD: options.mk,v 1.1 2009/10/22 22:27:23 wiz Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.gwaei
+PKG_SUPPORTED_OPTIONS=	gnome
+PKG_SUGGESTED_OPTIONS=	gnome
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgnome)
+GCONF_SCHEMAS=		gwaei.schemas
+PLIST_SRC+=		PLIST.gnome
+
+post-install:
+	${INSTALL_DATA} ${WRKSRC}/src/schemas/gwaei.schemas \
+		${DESTDIR}${PREFIX}/share/gconf/schemas/
+
+BUILDLINK_API_DEPENDS.GConf+=	GConf>=2.22.0
+.include "../../devel/GConf/schemas.mk"
+BUILDLINK_API_DEPENDS.libgnome+=	libgnome>=2.22.0
+.include "../../devel/libgnome/buildlink3.mk"
+BUILDLINK_API_DEPENDS.libsexy+=		libsexy>=0.1.11
+.include "../../devel/libsexy/buildlink3.mk"
+.include "../../graphics/hicolor-icon-theme/buildlink3.mk"
+.include "../../sysutils/desktop-file-utils/desktopdb.mk"
+.include "../../mk/omf-scrollkeeper.mk"
+.else
+CONFIGURE_ARGS+=	--without-gnome
+.endif
