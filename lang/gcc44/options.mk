27abb173ac14ef91ffea8b532f07f34d054a8e53
tnn <tnn@pkgsrc.org>
2009-10-24 20:16:02 +0000

    remove unused var

--- lang/gcc44/options.mk
+++ lang/gcc44/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2009/10/24 20:15:05 tnn Exp $
+# $NetBSD: options.mk,v 1.2 2009/10/24 20:16:02 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gcc44
 PKG_SUPPORTED_OPTIONS=	nls
@@ -9,13 +9,10 @@ PKG_SUGGESTED_OPTIONS+=	nls
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		nls
-
 ###
 ### Native Language Support
 ###
 .if !empty(PKG_OPTIONS:Mnls)
-PLIST.nls=		yes
 CONFIGURE_ARGS+=	--enable-nls
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
