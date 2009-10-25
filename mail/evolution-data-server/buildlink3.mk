d032df77032127cd7c5f7fde21f8c2ab1d9a32be
wiz <wiz>
2009-10-25 13:23:49 +0000

    libecal uses libedataserver uses GConf, ORBit2, libxml2, so provide these header files for dependencies as well.

--- mail/evolution-data-server/buildlink3.mk
+++ mail/evolution-data-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.30 2009/10/22 15:49:45 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2009/10/25 13:23:49 wiz Exp $
 
 BUILDLINK_TREE+=	evolution-data-server
 
@@ -10,11 +10,14 @@ BUILDLINK_ABI_DEPENDS.evolution-data-server?=	evolution-data-server>=2.22.3nb1
 BUILDLINK_PKGSRCDIR.evolution-data-server?=	../../mail/evolution-data-server
 
 .include "../../databases/sqlite3/buildlink3.mk"
+.include "../../devel/GConf/buildlink3.mk"
 .include "../../devel/libbonobo/buildlink3.mk"
 .include "../../devel/nss/buildlink3.mk"
 .include "../../databases/db4/buildlink3.mk"
 .include "../../net/libsoup24/buildlink3.mk"
+.include "../../net/ORBit2/buildlink3.mk"
 .include "../../time/libical/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
 .endif # EVOLUTION_DATA_SERVER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-evolution-data-server
