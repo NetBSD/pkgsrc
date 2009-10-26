99a896435b4e9ba54515809314e640626c79a693
obache <obache@pkgsrc.org>
2009-10-26 08:36:30 +0000

    switch to use clutter08. Bump PKGREVISION.

--- graphics/clutter-cairo/buildlink3.mk
+++ graphics/clutter-cairo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/08/26 19:57:06 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/10/26 08:36:30 obache Exp $
 #
 
 BUILDLINK_TREE+=	clutter-cairo
@@ -7,11 +7,11 @@ BUILDLINK_TREE+=	clutter-cairo
 CLUTTER_CAIRO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.clutter-cairo+=	clutter-cairo>=0.8.0
-BUILDLINK_ABI_DEPENDS.clutter-cairo?=	clutter-cairo>=0.8.2nb1
+BUILDLINK_ABI_DEPENDS.clutter-cairo+=	clutter-cairo>=0.8.2nb2
 BUILDLINK_PKGSRCDIR.clutter-cairo?=	../../graphics/clutter-cairo
 
 .include "../../graphics/cairo/buildlink3.mk"
-.include "../../graphics/clutter/buildlink3.mk"
+.include "../../graphics/clutter08/buildlink3.mk"
 .endif # CLUTTER_CAIRO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-clutter-cairo
