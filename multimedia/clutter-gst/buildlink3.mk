d0c0d676a75b058aa2b3c48cb2e5c5ea274d7324
obache <obache@pkgsrc.org>
2009-10-26 08:52:43 +0000

    Update clutter-gst to 0.10.0.
    clutter-gst 0.10.0 (29/07/2009)
    ===============================================================================
    
      * list of changes since 0.9.0
    
        o Requires stable Clutter 1.0.0

--- multimedia/clutter-gst/buildlink3.mk
+++ multimedia/clutter-gst/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/08/26 19:57:06 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/10/26 08:52:43 obache Exp $
 #
 
 BUILDLINK_TREE+=	clutter-gst
@@ -6,8 +6,7 @@ BUILDLINK_TREE+=	clutter-gst
 .if !defined(CLUTTER_GST_BUILDLINK3_MK)
 CLUTTER_GST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clutter-gst+=	clutter-gst>=0.8.0
-BUILDLINK_ABI_DEPENDS.clutter-gst?=	clutter-gst>=0.8.0nb1
+BUILDLINK_API_DEPENDS.clutter-gst+=	clutter-gst>=0.10.0
 BUILDLINK_PKGSRCDIR.clutter-gst?=	../../multimedia/clutter-gst
 
 .include "../../graphics/clutter/buildlink3.mk"
