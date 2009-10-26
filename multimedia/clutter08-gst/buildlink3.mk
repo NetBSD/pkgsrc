ae1e2c62fc7d79cab41ac105731de141f323d084
obache <obache@pkgsrc.org>
2009-10-26 08:35:18 +0000

    Import clutter08-gst-0.8.0 as multimedia/clutter08-gst.
    clutter-gst for clutter-0.8 branch.

---
+++ multimedia/clutter08-gst/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/10/26 08:35:18 obache Exp $
+#
+
+BUILDLINK_TREE+=	clutter08-gst
+
+.if !defined(CLUTTER08_GST_BUILDLINK3_MK)
+CLUTTER08_GST_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.clutter08-gst+=	clutter08-gst>=0.8.0
+BUILDLINK_PKGSRCDIR.clutter08-gst?=	../../multimedia/clutter08-gst
+
+.include "../../graphics/clutter08/buildlink3.mk"
+.include "../../multimedia/gstreamer0.10/buildlink3.mk"
+.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
+.endif # CLUTTER08_GST_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-clutter08-gst
