051cb0d119cdb2d559f5dd1aef3e9086b80e219d
obache <obache@pkgsrc.org>
2009-10-26 08:30:16 +0000

    Import clutter08-0.8.8 as graphics/clutter08.
    clutter-0.8 branch.

---
+++ graphics/clutter08/buildlink3.mk
@@ -0,0 +1,30 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/10/26 08:30:16 obache Exp $
+#
+
+BUILDLINK_TREE+=	clutter08
+
+.if !defined(CLUTTER08_BUILDLINK3_MK)
+CLUTTER08_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.clutter08+=	clutter08>=0.8.0
+BUILDLINK_PKGSRCDIR.clutter08?=	../../graphics/clutter08
+
+pkgbase := clutter08
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.clutter08:Mx11)
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXdamage/buildlink3.mk"
+.include "../../x11/libXfixes/buildlink3.mk"
+.include "../../x11/libXcomposite/buildlink3.mk"
+.include "../../x11/libXi/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
+.include "../../textproc/json-glib/buildlink3.mk"
+.endif # CLUTTER08_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-clutter08
