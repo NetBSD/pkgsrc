11229551c26293f8be2e877259999ea85075d3b3
obache <obache@pkgsrc.org>
2009-10-26 08:51:01 +0000

    Update clutter-gtk to 0.10.2.
    Clutter-GTK 0.10  29/07/2009
    ============================
    
    Alexander Larsson (1):
          Make GtkClutterEmbed derive from GtkContainer
    
    Chris Lord (1):
          [util] Add error arguments to texture functions
    
    Emmanuele Bassi (14):
          [zoomable] Add Zoomable interface
          [zoomable] Fix Introspection data generation
          [viewport] Implement Zoomable
          Update the Viewport example
          [git ignore] Ignore backup and m4 files
          Post-release bump to 0.9.3
          Bump up the required Clutter version
          [examples] Add a Notebook case
          [viewport] Use the ::apply_transform vfunc
          Merge branch 'zoomable'
          [build] Use symbolic values for clutter-gtk version
          [build] Depend on Clutter 1.0.0
          [docs] Update the API Reference
          [docs] Update the Since tags

--- graphics/clutter-gtk/buildlink3.mk
+++ graphics/clutter-gtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/08/26 19:56:57 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/10/26 08:51:01 obache Exp $
 #
 
 BUILDLINK_TREE+=	clutter-gtk
@@ -6,8 +6,7 @@ BUILDLINK_TREE+=	clutter-gtk
 .if !defined(CLUTTER_GTK_BUILDLINK3_MK)
 CLUTTER_GTK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clutter-gtk+=	clutter-gtk>=0.8.0
-BUILDLINK_ABI_DEPENDS.clutter-gtk?=	clutter-gtk>=0.8.3nb1
+BUILDLINK_API_DEPENDS.clutter-gtk+=	clutter-gtk>=0.10.0
 BUILDLINK_PKGSRCDIR.clutter-gtk?=	../../graphics/clutter-gtk
 
 .include "../../x11/gtk2/buildlink3.mk"
