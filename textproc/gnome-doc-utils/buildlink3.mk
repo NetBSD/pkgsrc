c230f7e57fb05fb71edfd90d51627f43fb5eb4db
drochner <drochner@pkgsrc.org>
2009-10-27 14:50:07 +0000

    update to 0.18.0 This switches to the gnome-2.28 release branch.
    pkgsrc changes:
    -depend in docbook-xml-4.5 and scrollkeeper-dtd because these are used
     by some gnome pkgs (should fix PR pkg/42233 by Robert Elz)
    -prepare to call xsltproc with the -nonet option
     (commented out for now because it makes xsltproc fail even if no network
     accesses are necessary, needs investigation)

--- textproc/gnome-doc-utils/buildlink3.mk
+++ textproc/gnome-doc-utils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/03/20 19:25:29 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2009/10/27 14:50:07 drochner Exp $
 
 BUILDLINK_TREE+=	gnome-doc-utils
 
@@ -8,6 +8,9 @@ GNOME_DOC_UTILS_BUILDLINK3_MK:=
 BUILDLINK_DEPMETHOD.gnome-doc-utils+=	build
 BUILDLINK_API_DEPENDS.gnome-doc-utils+=	gnome-doc-utils>=0.8.0
 BUILDLINK_PKGSRCDIR.gnome-doc-utils?=	../../textproc/gnome-doc-utils
+
+# doesn't work yet
+#.include "../../textproc/libxslt/xsltproc-nonet.mk"
 .endif # GNOME_DOC_UTILS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnome-doc-utils
