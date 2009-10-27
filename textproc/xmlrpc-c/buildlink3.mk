86c7505aa4133e523fce8be30833fc0e6ec9d703
drochner <drochner@pkgsrc.org>
2009-10-27 18:50:38 +0000

    update to 1.06.38 this switches to the "super stable" branch which is the only one where tarballs are distributed of, too many changes to list here, the most notable recent one is that the recent expat DOS vulnerability was fixed, as were some portability issues which caused problems on NetBSD

--- textproc/xmlrpc-c/buildlink3.mk
+++ textproc/xmlrpc-c/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:25:34 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/10/27 18:50:39 drochner Exp $
 
 BUILDLINK_TREE+=	xmlrpc-c
 
 .if !defined(XMLRPC_C_BUILDLINK3_MK)
 XMLRPC_C_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xmlrpc-c+=	xmlrpc-c>=1.09.00
-BUILDLINK_ABI_DEPENDS.xmlrpc-c?=	xmlrpc-c>=1.09.00nb3
+BUILDLINK_API_DEPENDS.xmlrpc-c+=	xmlrpc-c>=1.06.32
+BUILDLINK_ABI_DEPENDS.xmlrpc-c?=	xmlrpc-c>=1.06.32
 BUILDLINK_PKGSRCDIR.xmlrpc-c?=	../../textproc/xmlrpc-c
 
 .include "../../www/libwww/buildlink3.mk"
