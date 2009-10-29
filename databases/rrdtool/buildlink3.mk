64d5a1bd383c4baeff49eccc4b04a8798de69ecd
martti <martti@pkgsrc.org>
2009-10-29 12:43:49 +0000

    Updated databases/rrdtool to 1.3.9
    * bug fixes

--- databases/rrdtool/buildlink3.mk
+++ databases/rrdtool/buildlink3.mk
@@ -1,22 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.22 2009/03/20 19:24:07 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2009/10/29 12:43:49 martti Exp $
 
 BUILDLINK_TREE+=	rrdtool
 
 .if !defined(RRDTOOL_BUILDLINK3_MK)
 RRDTOOL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rrdtool+=	rrdtool>=1.3.5
+BUILDLINK_API_DEPENDS.rrdtool+=	rrdtool>=1.3.9
 BUILDLINK_PKGSRCDIR.rrdtool?=	../../databases/rrdtool
 
-.include "../../graphics/freetype2/buildlink3.mk"
-.include "../../graphics/png/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../devel/pango/buildlink3.mk"
 .include "../../graphics/cairo/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../x11/pixman/buildlink3.mk"
-.endif # RRDTOOL_BUILDLINK3_MK
+.endif	# RRDTOOL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-rrdtool
