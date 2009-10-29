4787f1c4fe36e37f8d05e18ee6058f0f389f099f
drochner <drochner@pkgsrc.org>
2009-10-29 17:39:11 +0000

    rename the pkg so that it doesn't share the namespace with the former version anymore, to avoid problems with binary pkgs

--- textproc/xmlrpc-c/buildlink3.mk
+++ textproc/xmlrpc-c/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/10/27 18:50:39 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2009/10/29 17:39:11 drochner Exp $
 
 BUILDLINK_TREE+=	xmlrpc-c
 
 .if !defined(XMLRPC_C_BUILDLINK3_MK)
 XMLRPC_C_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xmlrpc-c+=	xmlrpc-c>=1.06.32
-BUILDLINK_ABI_DEPENDS.xmlrpc-c?=	xmlrpc-c>=1.06.32
+BUILDLINK_API_DEPENDS.xmlrpc-c+=	xmlrpc-c-ss>=1.06.32
+BUILDLINK_ABI_DEPENDS.xmlrpc-c?=	xmlrpc-c-ss>=1.06.32
 BUILDLINK_PKGSRCDIR.xmlrpc-c?=	../../textproc/xmlrpc-c
 
 .include "../../www/libwww/buildlink3.mk"
