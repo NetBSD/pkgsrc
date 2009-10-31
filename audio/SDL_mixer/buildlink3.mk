a1db892afbaa6ad9e99159e02f91ae18a643037f
wiz <wiz@pkgsrc.org>
2009-10-31 00:16:40 +0000

    Update to 1.2.9. Set LICENSE. Remove dynamically loaded libraries from buildlink3.mk.
    1.2.9:
    Ryan Gordon - Sun Oct 18 11:42:31 PDT 2009
     * Updated native MIDI support on Mac OS X for 10.6
    Ryan Gordon - Sun Oct 11 05:29:55 2009 UTC
     * Reset channel volumes after a fade out interrupts a fade in.
    Ryan Gordon - Sun Oct 11 02:59:12 2009 UTC
     * Fixed crash race condition with position audio functions
    Ryan Gordon - Sat Oct 10 17:05:45 2009 UTC
     * Fixed stereo panning in 8-bit mode
    Sam Lantinga - Sat Oct 10 11:07:15 2009 UTC
     * Added /usr/share/timidity to the default timidity.cfg locations
    Sam Lantinga - Sat Oct  3 13:33:36 PDT 2009
     * MOD support uses libmikmod and is dynamically loaded by default
     * A patched version of libmikmod is included in libmikmod-3.1.12.zip
     * The libmikmod patches fix security issues CVE-2007-6720 and CVE-2009-0179.
    Sam Lantinga - Sat Oct  3 02:49:41 PDT 2009
     * Added TIMIDITY_CFG environment variable to fully locate timidity.cfg
    Sam Lantinga - Fri Oct  2 07:15:35 PDT 2009
     * Implemented seamless looping for music playback
    Forrest Voight - 2009-06-13 20:31:38 PDT
     * ID3 files are now recognized as MP3 format
    Steven Noonan - 2008-05-13 13:31:36 PDT
     * Fixed native MIDI crash on 64-bit Windows
    Ryan Gordon - Fri Jun 5 16:07:08 2009 UTC
     * Added decoder enumeration API:
    	Mix_GetNumChunkDecoders(), Mix_GetChunkDecoder(),
    	Mix_GetNumMusicDecoders(), Mix_GetMusicDecoder()
    Austen Dicken - Tue Feb 26 23:28:27 PST 2008
     * Added support for FLAC audio both as chunks and streaming
    Tilman Sauerbeck - Tue Feb 26 03:44:47 PST 2008
     * Added support for streaming WAV files with Mix_LoadMUS_RW()
    Ryan Gordon - Mon Feb 4 17:10:08 UTC 2008
     * Fixed crash caused by not resetting position_channels

--- audio/SDL_mixer/buildlink3.mk
+++ audio/SDL_mixer/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2009/03/20 19:23:51 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2009/10/31 00:16:40 wiz Exp $
 
 BUILDLINK_TREE+=	SDL_mixer
 
@@ -10,9 +10,7 @@ BUILDLINK_ABI_DEPENDS.SDL_mixer+=	SDL_mixer>=1.2.6nb5
 BUILDLINK_PKGSRCDIR.SDL_mixer?=	../../audio/SDL_mixer
 BUILDLINK_INCDIRS.SDL_mixer?=	include/SDL
 
-.include "../../audio/libvorbis/buildlink3.mk"
-.include "../../multimedia/libogg/buildlink3.mk"
-.include "../../multimedia/smpeg/buildlink3.mk"
+.include "../../devel/SDL/buildlink3.mk"
 .endif # SDL_MIXER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-SDL_mixer
