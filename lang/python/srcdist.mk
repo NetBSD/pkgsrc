abacec77913eae94723a6962d8c314c2aa4f7176
wiz <wiz@pkgsrc.org>
2009-11-03 02:21:05 +0000

    Update for Python26-2.6.4

--- lang/python/srcdist.mk
+++ lang/python/srcdist.mk
@@ -1,14 +1,14 @@
-# $NetBSD: srcdist.mk,v 1.29 2009/10/21 06:36:24 wiz Exp $
+# $NetBSD: srcdist.mk,v 1.30 2009/11/03 02:21:05 wiz Exp $
 
 .include "../../lang/python/pyversion.mk"
 
 .if ${_PYTHON_VERSION} == "26"
 
-DISTNAME=	Python-2.6.3
+DISTNAME=	Python-2.6.4
 EXTRACT_SUFX=	.tar.bz2
 DISTINFO_FILE=	${.CURDIR}/../../lang/python26/distinfo
 PATCHDIR=	${.CURDIR}/../../lang/python26/patches
-PYSUBDIR=	Python-2.6.3
+PYSUBDIR=	Python-2.6.4
 WRKSRC=		${WRKDIR}/${PYSUBDIR}
 MASTER_SITES=	ftp://ftp.python.org/pub/python/2.6/
 
