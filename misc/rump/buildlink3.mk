ad0d15a079e2eb51ea05428a843c74654dfc3d9f
stacktic <stacktic>
2009-11-05 08:36:00 +0000

    Updated misc/rump to version 20091103 - Should fix building on Linux 64bits (reported by Alexander Nasonov) - Fixed the build of dependencies

--- misc/rump/buildlink3.mk
+++ misc/rump/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2009/10/30 18:25:23 stacktic Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/11/05 08:36:00 stacktic Exp $
 
 BUILDLINK_TREE+=	rump
 
 .if !defined(RUMP_BUILDLINK3_MK)
 RUMP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rump+=	rump>=20091030
+BUILDLINK_API_DEPENDS.rump+=	rump>=20091103
 BUILDLINK_PKGSRCDIR.rump?=	../../misc/rump
 .endif	# RUMP_BUILDLINK3_MK
 
