6cc7c35222f794e1289c4156a5121e0f5c35c29b
tnn <tnn@pkgsrc.org>
2009-11-10 16:44:06 +0000

    - update to nspr-4.8.2.5 - take maintainership - let this package automatically track the stable gecko branch - clean out some legacy patches
    This is the first part of PR pkg/42277.

--- devel/nspr/buildlink3.mk
+++ devel/nspr/buildlink3.mk
@@ -1,26 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:24:25 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2009/11/10 16:44:06 tnn Exp $
 
 BUILDLINK_TREE+=	nspr
 
 .if !defined(NSPR_BUILDLINK3_MK)
 NSPR_BUILDLINK3_MK:=
 
-.  include "../../mk/bsd.fast.prefs.mk"
-
-BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.4.1nb1
-.  if ${OPSYS} == "Darwin"
-BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.6.1nb3
-.  else
-BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.6.1nb1
-.  endif
-
+BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.8.2.5
 BUILDLINK_PKGSRCDIR.nspr?=	../../devel/nspr
+
 BUILDLINK_FILES.nspr+=          lib/nspr/*
 BUILDLINK_FILES.nspr+=          include/nspr/*
 
 BUILDLINK_INCDIRS.nspr+=        include/nspr
 BUILDLINK_LIBDIRS.nspr+=        lib/nspr
 BUILDLINK_RPATHDIRS.nspr+=      lib/nspr
-.endif # NSPR_BUILDLINK3_MK
+.endif	# NSPR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-nspr
