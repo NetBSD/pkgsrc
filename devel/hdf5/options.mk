e4d82db08df4a65c8c3df7a71b1d1a17f5eac1c9
drochner <drochner@pkgsrc.org>
2009-11-15 15:50:50 +0000

    update to 1.6.10 (yes I know there is the 1.8 branch, but I don't want to experiment right now) changes: many fixes, some C++ API improvements pkgsrc change: DESTDIR support

--- devel/hdf5/options.mk
+++ devel/hdf5/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2008/04/12 22:42:59 jlam Exp $
+# $NetBSD: options.mk,v 1.5 2009/11/15 15:50:50 drochner Exp $
 
 PKG_OPTIONS_VAR=PKG_OPTIONS.hdf5
 PKG_SUPPORTED_OPTIONS=	szip cxx threads
@@ -21,5 +21,6 @@ PLIST.cxx=		yes
 
 .if !empty(PKG_OPTIONS:Mthreads)
 CONFIGURE_ARGS+=	--enable-threadsafe
+# XXX libraries should not be linked against -lpthread
 .include "../../mk/pthread.buildlink3.mk"
 .endif
