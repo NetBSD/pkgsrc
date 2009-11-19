6e15810f5a305ff1c2f829c55aedc439cc5692d3
reed <reed@pkgsrc.org>
2009-11-19 01:19:23 +0000

    From PR: pkg/42344: update for devel/libmaa [patch]
    update devel/libmaa to 1.2.0.
    
    Major changes in upstream:
    
      For better conformance with POSIX/SUS xmalloc, xrealloc and xcalloc
      functions take 'size_t' args, not 'unsigned int'.
      Due to change in API a major shared library number is bumped from 1 to 2
    
      New trivial test for log.c
    
      fix for sltest.c: on OpenBSD intptr_t is defined in stdint.h
    
      Makefile.in: GNU make is not required anymore, bsd make is enough

--- devel/libmaa/buildlink3.mk
+++ devel/libmaa/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:20 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/11/19 01:19:23 reed Exp $
 
 BUILDLINK_TREE+=	libmaa
 
 .if !defined(LIBMAA_BUILDLINK3_MK)
 LIBMAA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libmaa+=	libmaa>=0.990
+BUILDLINK_API_DEPENDS.libmaa+=	libmaa>=1.2.0
 BUILDLINK_PKGSRCDIR.libmaa?=	../../devel/libmaa
 .endif # LIBMAA_BUILDLINK3_MK
 
