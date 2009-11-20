6c5e2ddd3905b35bbc16f1a299f4c934c86894d4
rillig <rillig@pkgsrc.org>
2009-11-20 12:02:33 +0000

    The GNU tools usually provide replacements for their counterparts whose name doesn't start with a "g". Fixes PR 40261.

--- pkgtools/pkglint/files/pkglint.pl
+++ pkgtools/pkglint/files/pkglint.pl
@@ -1,5 +1,5 @@
 #! @PERL@
-# $NetBSD: pkglint.pl,v 1.820 2009/09/10 21:22:56 joerg Exp $
+# $NetBSD: pkglint.pl,v 1.821 2009/11/20 12:02:33 rillig Exp $
 #
 
 # pkglint - static analyzer and checker for pkgsrc packages
@@ -4473,7 +4473,7 @@ sub checkline_mk_shelltext($$) {
 "package via pkg_add.");
 
 			} elsif (exists(get_tool_names()->{$shellword})) {
-				if (!exists($mkctx_tools->{$shellword})) {
+				if (!exists($mkctx_tools->{$shellword}) && !exists($mkctx_tools->{"g$shellword"})) {
 					$line->log_warning("The \"${shellword}\" tool is used but not added to USE_TOOLS.");
 				}
 
