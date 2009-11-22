05963c2cc5a5465a7b5a84611c1f0f853a6c72ad
asau <asau@pkgsrc.org>
2009-11-22 12:52:53 +0000

    Update f2c to 2009-04-11 snapshot. Changes are mostly bug fixes. Perform conservative update: recreate libF77 and libI77 providing binary compatibility. Prefer files to patches creating respective files. Override build system completely, we effectively did that anyway.

---
+++ lang/f2c/files/libi77.mk
@@ -0,0 +1,34 @@
+LIB=	I77
+SHLIB_MAJOR=	0
+SHLIB_MINOR=	0
+
+.include "bsd.init.mk"
+.PATH:	$(LIBF2CSRCDIR)
+
+SRCS =	backspac.c close.c dfe.c dolio.c due.c endfile.c err.c \
+	fmt.c fmtlib.c ftell_.c iio.c ilnw.c inquire.c lread.c lwrite.c \
+	open.c rdfmt.c rewind.c rsfe.c rsli.c rsne.c sfe.c sue.c typesize.c \
+	uio.c util.c wref.c wrtfmt.c wsfe.c wsle.c wsne.c xwsne.c
+SRCS+=	i77vers.c	# previously known as "Version.c"
+# Previously there was "backspace.c" instead of "backspac.c".
+
+DPSRCS=		f2c.h sysdep1.h
+
+CPPFLAGS=	-I.	# for generated include files
+
+f2c.h:	f2c.h0;		cp $> $@
+sysdep1.h: sysdep1.h0;	cp $> $@
+
+CLEANFILES=	$(DPSRCS)
+
+CPPFLAGS+=	-DNO_My_ctype
+CPPFLAGS+=	-DSkip_f2c_Undefs
+
+i77vers.lo: i77vers.c
+	$(LIBTOOL) --mode=compile $(CC) $(CFLAGS) -c $(.ALLSRC:M*.c)
+
+.if defined(MKLTARCHIVE) && !empty(MKLTARCHIVE:M[Yy][Ee][Ss])
+.include "../libtool.mk"
+.else
+.include "bsd.lib.mk"
+.endif
