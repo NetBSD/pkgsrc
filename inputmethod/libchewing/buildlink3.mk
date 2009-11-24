fd3e714ba1ee545736225a11900ab0a75f53deb8
obache <obache@pkgsrc.org>
2009-11-24 11:31:04 +0000

    Update libchewing to 0.3.2.
    What's New in libchewing 0.3.2
    ---------------------------------------------------------
    * Add gcc visibility support to hide private APIs.
    * APIs and Headers refactoring.
    * Doxygen-friendly API documentation.
    * New binary data manipulation approach to dramatically reduce storage
      usage and loading time via memory mapping. (enabled by default)
    * New word segmentation method based on mmseg.
    * Improved auto-learn new phrases method.
    * Improved test suite and added simple benchmark program.
    * Option to indicate the behavior for phrase choice to be rearward
      or not.
    * New APIs to enumerate supported keyboard types.
    * Repair improper memory access.
    * API united in Win32 and UNIX world.
    * Initial Pythin binding support.
    * Merged with libchewing-data project. (r342)
    * Canonical debugging messages.
    * Successful stories:
      . OpenBSD porting
      . ibus-chewing : chewing engine for ibus input method framework.
      . UIM-chewing leveraging libchewing 0.3.x development branch.
    
    What's New in libchewing 0.3.1
    ----------------------------------------------------------
    * New keyboard layout DACHEN_CP26.
    * Add key '`' to open symbol table.
    * Erase candidate and reset its length when 'Esc' key is handled.
    * Closes candidate window when user pressed 'Up' key.
    * Doing select at the end of edit buffer works on the
      last phrase (interval).
    * Improved reminding message text.
    * Sync with libchewingdata project.
    * A new HanYu-PinYin mapping table
    * More robust behavior when IM crashes.
    * Allow '#' comment and blank line in tsi.src.
    * Fix chewing switch between Full/Half mode and cursor moves one space in
      gedit.
    * Fix when list available phrases to choice, user's own phrases are
      not included.
    * Release PgUp/PgDn key event to running applications.
    * Fix portability issues on FreeBSD.
    * Fix incorrect initialization.
    * Fix pinyin key order, put "shun" before "shuo".
    * Fix various potenital boffer overflow bugs.
    * Fix various memory leaks.
    * chewing_set_PinYinMethod to support multiple PinYin methods.
    * chewing_handle_CtrlOption was removed.
    * New structure SymbolEntry.
    * Rename selectAreaLen to candPerPage.
    * Added bEasySymbolInput.
    
    What's New in libchewing 0.3.0
    ----------------------------------------------------------
    * This is a new development branch differing a lot from
      0.2.x series, and breaks API/ABI for new features.
    * UTF-8 based language engine core for common Unicode
      environment.
    * Porting layer for POSIX/Unix-like and Win32 to reduce the
      redundant porting process.
    * New API design for better consistency.
    * Merge the efforts from libchewing-data subproject, which
      means various phrases have been reviewed and corrected.
    * Zuin fixes and symbol improvements.
    * Support key 'j' / 'k' for paging in selection mode.
    * Fix some internal memory management bugs and leaks.
    * Improved internal test suite.
    * New binary-form of user hash data to speed up loading and
      solve hash data corruption.
    * Better calculating of internal tree and phone constants.
    * Revised tsi.src for richer phrases and avoiding crashes.
    * Merge phone and phrase from CNS11643.
    * Reduce the amount for the expose for internal symbols.
    * Improved Han-Yu PinYin to use table-lookup implementation.
    * Experimental frequency-evaluation. (re-compute chewing
      lifetime)
    * Implement the choice mechanism for pair symbols.
    * Improved Tab handling to allow the switching over different
      frequency orders of candidated phrases.
    * New auxiliary module for simpler API uses.
    * Experimental memory-mapping based binary data handling to
      speed up data loading.
    * Successful stories:
      . Sun Solaris 10 U2 and 11 will provide built-in Chewing
        IM support.
      . OXIM input method suite from OpenDesktop/OSSI provides
        Chewing support as its default input method.
      . New Win32-Chewing subproject to provide efficient input
        mthod solution on Microsoft Windows platforms.

--- inputmethod/libchewing/buildlink3.mk
+++ inputmethod/libchewing/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2009/03/20 19:24:46 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2009/11/24 11:31:04 obache Exp $
 
 BUILDLINK_TREE+=	libchewing
 
 .if !defined(LIBCHEWING_BUILDLINK3_MK)
 LIBCHEWING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libchewing+=		libchewing>=0.2.7
-BUILDLINK_ABI_DEPENDS.libchewing+=	libchewing>=0.2.7
+BUILDLINK_API_DEPENDS.libchewing+=		libchewing>=0.3.2
+BUILDLINK_ABI_DEPENDS.libchewing+=	libchewing>=0.3.2
 BUILDLINK_PKGSRCDIR.libchewing?=	../../inputmethod/libchewing
 .endif # LIBCHEWING_BUILDLINK3_MK
 
