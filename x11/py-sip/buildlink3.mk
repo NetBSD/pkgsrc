921ed40fc5a7fbe72a502721c7b4a93d6a6cf633
drochner <drochner@pkgsrc.org>
2009-11-24 17:17:35 +0000

    sip-4.9 changed the API major, so a py-qt built against an older one will not be compatible anymore (noticed by Dennis den Brok, see mail to tech-pkg) bump the API requirement being here, update to 4.9.3 which brings another little bugfix

--- x11/py-sip/buildlink3.mk
+++ x11/py-sip/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:25:49 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/11/24 17:17:35 drochner Exp $
 
 BUILDLINK_TREE+=	py-sip
 
@@ -7,7 +7,7 @@ PY_SIP_BUILDLINK3_MK:=
 
 .include "../../lang/python/pyversion.mk"
 
-BUILDLINK_API_DEPENDS.py-sip+=	${PYPKGPREFIX}-sip>=4.7.7
+BUILDLINK_API_DEPENDS.py-sip+=	${PYPKGPREFIX}-sip>=4.9.1
 BUILDLINK_PKGSRCDIR.py-sip?=	../../x11/py-sip
 .endif # PY_SIP_BUILDLINK3_MK
 
