7e2863401c2951b3bf3c84dee888576fdb70de35
adam <adam@pkgsrc.org>
2009-11-24 09:50:33 +0000

    Changes 1.14.0: * New Libraries   Property Tree: A tree data structure especially suited to storing   configuration data, from Marcin Kalicinski and Sebastian Redl. * Updated Libraries * Build System   A bug preventing "fat" 32-bit + 64-bit builds on OSX has been fixed.

--- devel/boost-jam/buildlink3.mk
+++ devel/boost-jam/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2009/10/14 06:40:08 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2009/11/24 09:50:33 adam Exp $
 
 BUILDLINK_TREE+=	boost-jam
 
 .if !defined(BOOST_JAM_BUILDLINK3_MK)
 BOOST_JAM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.40.*
+BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.41.*
 BUILDLINK_DEPMETHOD.boost-jam?=		build
 BUILDLINK_PKGSRCDIR.boost-jam?=		../../devel/boost-jam
 .endif # BOOST_JAM_BUILDLINK3_MK
