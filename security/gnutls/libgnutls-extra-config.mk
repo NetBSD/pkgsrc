4cbfb4a358500883cbe1830e4a33eb7cff5a50a9
joerg <joerg@pkgsrc.org>
2009-11-26 14:18:26 +0000

    Craete libgnutls-extra-config in .buildlink/bin, not the WRAPPER_DIR.

--- security/gnutls/libgnutls-extra-config.mk
+++ security/gnutls/libgnutls-extra-config.mk
@@ -1,18 +1,18 @@
-# $NetBSD: libgnutls-extra-config.mk,v 1.1 2009/07/03 11:00:05 drochner Exp $
+# $NetBSD: libgnutls-extra-config.mk,v 1.2 2009/11/26 14:18:26 joerg Exp $
 
 # Makefile intended to be included by packages that need
 # "libgnutls-extra-config" during build time.
 
 USE_TOOLS+=	pkg-config
 
-pre-configure: ${WRAPPER_DIR}/bin/libgnutls-extra-config
+pre-configure: hack-libgnutls-extra-config
 
-${WRAPPER_DIR}/bin/libgnutls-extra-config:
+hack-libgnutls-extra-config:
 	${PRINTF} "#! ${SH}\\n\
 	  case \$$1 in\\n\
 	  --cflags|--libs) pkg-config \$$1 gnutls-extra;;\\n\
 	  --version) pkg-config --modversion gnutls-extra;;\\n\
 	  *) exit 1;;\\n\
 	  esac\\n\
-	" > ${.TARGET}
-	${CHMOD} +x ${.TARGET}
+	" > ${BUILDLINK_DIR}/bin/libgnutls-extra-config
+	${CHMOD} +x ${BUILDLINK_DIR}/bin/libgnutls-extra-config
