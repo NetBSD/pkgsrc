af5cff0947b01324d771dc23eb6200f03cabd4a9
tnn <tnn@pkgsrc.org>
2009-11-29 03:06:42 +0000

    update to thunderbird-3.0rc1. The 2.x version is still available in mail/thunderbird2.
    Major changes:
    - New Mail Account Setup Wizard
    - Redesigned Mail Toolbar
    - Tabbed Email Messages
    - Smart Folders
    - New Message Summary View
    - Column Headings
    - Message Archive
    - Activity Manager
    - New Add-ons Manager
    - Improved Address Book
    - Improved Gmail Integration
    
    Full release notes:
    http://www.mozillamessaging.com/en-US/thunderbird/3.0rc1/releasenotes/

---
+++ mail/thunderbird/options.mk
@@ -0,0 +1,55 @@
+# $NetBSD: options.mk,v 1.1 2009/11/29 03:06:42 tnn Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.thunderbird
+PKG_SUPPORTED_OPTIONS=	debug mozilla-jemalloc gnome official-mozilla-branding
+
+PLIST_VARS+=		branding debug gnome
+
+.if ${OPSYS} == "Linux" || ${OPSYS} == "SunOS"
+PKG_SUGGESTED_OPTIONS+=	mozilla-jemalloc
+.endif
+
+.if !empty(MACHINE_ARCH:Mi386) || !empty(MACHINE_ARCH:Msparc) || \
+	!empty(MACHINE_ARCH:Marm)
+PKG_SUPPORTED_OPTIONS+=	mozilla-jit
+PKG_SUGGESTED_OPTIONS+=	mozilla-jit
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgnome)
+.include "../../devel/libgnomeui/buildlink3.mk"
+.include "../../sysutils/gnome-vfs/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-gnomevfs --enable-dbus --enable-gnomeui
+PLIST.gnome=		yes
+.else
+CONFIGURE_ARGS+=	--disable-gnomevfs --disable-dbus --disable-gnomeui
+.endif
+
+.if !empty(PKG_OPTIONS:Mmozilla-jemalloc)
+CONFIGURE_ARGS+=	--enable-jemalloc
+.else
+CONFIGURE_ARGS+=	--disable-jemalloc
+.endif
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug
+PLIST.debug=		yes
+.else
+CONFIGURE_ARGS+=	--disable-debug
+.endif
+
+.if !empty(PKG_OPTIONS:Mmozilla-jit)
+CONFIGURE_ARGS+=	--enable-jit
+.else
+CONFIGURE_ARGS+=	--disable-jit
+.endif
+
+.if !empty(PKG_OPTIONS:Mofficial-mozilla-branding)
+CONFIGURE_ARGS+=	--enable-official-branding
+PLIST.branding=		yes
+LICENSE=		mozilla-trademark-license
+RESTRICTED=		Trademark holder prohibits distribution of modified versions.
+NO_BIN_ON_CDROM=	${RESTRICTED}
+NO_BIN_ON_FTP=		${RESTRICTED}
+.endif
