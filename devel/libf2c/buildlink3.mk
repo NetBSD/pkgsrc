52b19ea8bfb178b5c43bc828cf51b416d652dd61
asau <asau@pkgsrc.org>
2009-12-02 22:32:46 +0000

    Import libf2c-20090201 as devel/libf2c.
    libf2c is Fortran support library distributed by Netlib with f2c
    (Fortran 77 to C translator).
    libf2c combines libF77 and libI77 in one library.

---
+++ devel/libf2c/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/12/02 22:32:46 asau Exp $
+
+BUILDLINK_TREE+=	libf2c
+
+.if !defined(LIBF2C_BUILDLINK3_MK)
+LIBF2C_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libf2c+=	libf2c>=20090201
+BUILDLINK_PKGSRCDIR.libf2c?=	../../devel/libf2c
+.endif	# LIBF2C_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libf2c
---
+++ devel/libf2c/files/bsd.init.mk
@@ -0,0 +1,9 @@
+# Simulate "bsd.init.mk" because pkgsrc doesn't provide it for some reason:
+.if !target(__initialized__)
+__initialized__:
+.if exists(${.CURDIR}/../Makefile.inc)
+.include "${.CURDIR}/../Makefile.inc"
+.endif
+.include <bsd.own.mk>
+.MAIN:		all
+.endif
