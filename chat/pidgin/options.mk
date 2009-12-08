15709c62b1b81b82e4e1df800d4c5fde9ef93fcf
wiz <wiz@pkgsrc.org>
2009-12-08 12:45:42 +0000

    Update to 2.6.2, based on patches provided by Joachim Kuebart in PR 42005. Additional changes: Fix farsight handling in libpurple. Set LICENSE.
    2.6.2 (09/05/2009):
    	Mark: Woo boy it's been a busy two weeks.  There was a lot of new code
    	in 2.6.0, and with new code comes new bugs.  The cadre of relentless
    	developers responsible for Pidgin have been hard at work, and I believe
    	they have fixed all the major bugs that cropped up.  My thanks to all
    	those names listed as Current Developers in Pidgin's 'About' window.
    
    	Elliott: Well now, just as Mark said, there was a lot of new stuff that
    	probably came up with tons of bugs.  So I can't say I wrote anything
    	super-awesome, but I definitely fixed quite a few of those itty-bitty
    	why-didn't-this-work-this-way sort of bugs.

--- chat/pidgin/options.mk
+++ chat/pidgin/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2009/09/06 21:06:45 abs Exp $
+# $NetBSD: options.mk,v 1.8 2009/12/08 12:45:42 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.pidgin
 PKG_SUPPORTED_OPTIONS+=		dbus debug farsight gstreamer gtkspell
@@ -6,13 +6,19 @@ PKG_SUGGESTED_OPTIONS+=		gtkspell dbus farsight gstreamer
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		dbus
+PLIST_VARS+=		dbus vv
 
 .if !empty(PKG_OPTIONS:Mdbus)
 CONFIGURE_ARGS+=	--enable-dbus
 PLIST.dbus=		yes
 .  include "../../sysutils/dbus/buildlink3.mk"
 .  include "../../sysutils/dbus-glib/buildlink3.mk"
+.endif
+
+# voice/video support requires both farsight and gstreamer
+.if !empty(PKG_OPTIONS:Mfarsight) && !empty(PKG_OPTIONS:Mgstreamer)
+CONFIGURE_ARGS+=	--enable-vv
+PLIST.vv=		yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mfarsight)
