3ecec1d7b26f7efdc6d979fe73cc80f8f8b41a32
drochner <drochner@pkgsrc.org>
2009-12-09 10:22:04 +0000

    update to 0.97 changes: -High quality cross-platform print and pdf output by use of cairo -Rotated text, but only with the new "Standard - Ouline" object -Object properties dialog finally does handle multiple objects -Improved text edit mode for consistent handling of the Delete key -A new integrated user interface mode with diagrams in tabs -Almost 200 bugs and feature requests resolved
    pkgsrc note: the printing problem mentioned in TODO is resolved
     (some outdated nonsense in options.mk disabled use of "cairo")

--- graphics/dia/options.mk
+++ graphics/dia/options.mk
@@ -1,9 +1,8 @@
-# $NetBSD: options.mk,v 1.7 2009/07/22 09:01:31 wiz Exp $
+# $NetBSD: options.mk,v 1.8 2009/12/09 10:22:04 drochner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dia
-#PKG_SUPPORTED_OPTIONS=	gnome cairo
-PKG_SUPPORTED_OPTIONS=	gnome
-PKG_SUGGESTED_OPTIONS=	gnome
+PKG_SUPPORTED_OPTIONS=	gnome cairo
+PKG_SUGGESTED_OPTIONS=	gnome cairo
 
 .include "../../mk/bsd.options.mk"
 
@@ -20,7 +19,6 @@ CONFIGURE_ARGS+=	--disable-gnome
 PLIST_SUBST+=		DIA_HELPDIR=share/dia/help
 .endif
 
-# Disabled, freedesktop is down.
 .if !empty(PKG_OPTIONS:Mcairo)
 CONFIGURE_ARGS+=	--with-cairo
 .  include "../../graphics/cairo/buildlink3.mk"
