7b12f9f0999df0290fb3d8e88794fd4fc6ee1fe2
obache <obache>
2009-12-09 12:27:27 +0000

    Some clean up realted to net/libnet{10,11}. * buildlink bin/libnet{10,11}-config to bin/libnet-config for comvenience   (they ware renamed in pkgsrc to avoid conflict) * remove -lnet from BUILDLINK_CPPFLAGS.libnet11.   linker flags should not be in CPPFLAGS, and it berak likage with libtool   as reported in PR 37300. * libnet11 install just a static library, so set defaut DEPMETHOD = build
    Bump PKGREVISION for libnet11 related packages (net/isic will be updated later).

--- net/libnids/options.mk
+++ net/libnids/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2009/05/06 21:54:54 adrianp Exp $
+# $NetBSD: options.mk,v 1.8 2009/12/09 12:27:28 obache Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.libnids
 
@@ -17,7 +17,6 @@ PKG_SUGGESTED_OPTIONS=		libnet10 glib
 .if !empty(PKG_OPTIONS:Mlibnet10)
 .	include "../../devel/libnet10/buildlink3.mk"
 BUILDLINK_DEPMETHOD.libnet10+=	build
-SUBST_SED.conf=	-e "s|libnet-config|libnet10-config|g"
 .endif
 
 ###
@@ -26,7 +25,6 @@ SUBST_SED.conf=	-e "s|libnet-config|libnet10-config|g"
 .if !empty(PKG_OPTIONS:Mlibnet11)
 .	include "../../devel/libnet11/buildlink3.mk"
 BUILDLINK_DEPMETHOD.libnet11+=	build
-SUBST_SED.conf=	-e "s|libnet-config|libnet11-config|g"
 .endif
 
 ###
