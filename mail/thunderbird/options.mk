b813791830d082e0b92a40a27b4a008f183452d7
tnn <tnn@pkgsrc.org>
2009-12-11 15:00:00 +0000

    explicitly --disable-official-branding. Looks like the default changed but the trademark policy didn't.

--- mail/thunderbird/options.mk
+++ mail/thunderbird/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2009/11/29 03:06:42 tnn Exp $
+# $NetBSD: options.mk,v 1.2 2009/12/11 15:00:00 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.thunderbird
 PKG_SUPPORTED_OPTIONS=	debug mozilla-jemalloc gnome official-mozilla-branding
@@ -52,4 +52,6 @@ LICENSE=		mozilla-trademark-license
 RESTRICTED=		Trademark holder prohibits distribution of modified versions.
 NO_BIN_ON_CDROM=	${RESTRICTED}
 NO_BIN_ON_FTP=		${RESTRICTED}
+.else
+CONFIGURE_ARGS+=	--disable-official-branding
 .endif
