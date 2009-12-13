45227085d999b9043a59047da04f6b06f027cd0c
obache <obache@pkgsrc.org>
2009-12-13 09:34:33 +0000

    buildlink avahi and fam conditionally as PKG_BUILD_OPTION.

--- sysutils/gnome-vfs/buildlink3.mk
+++ sysutils/gnome-vfs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2009/06/14 22:58:09 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2009/12/13 09:34:33 obache Exp $
 
 BUILDLINK_TREE+=	gnome-vfs
 
@@ -14,7 +14,18 @@ BUILDLINK_PKGSRCDIR.gnome-vfs?=		../../sysutils/gnome-vfs
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../security/gnutls/buildlink3.mk"
 .include "../../sysutils/dbus-glib/buildlink3.mk"
+
+pkgbase := gnome-vfs
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.gnome-vfs:Mavahi)
+.include "../../net/avahi/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.gnome-vfs:Mfam)
 .include "../../mk/fam.buildlink3.mk"
+.endif
+
 .endif # GNOME_VFS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnome-vfs
