942bbd9435c8718fc4c231837526f90c3fd6ef4d
gdt <gdt@pkgsrc.org>
2009-12-14 23:36:05 +0000

    ABI incompatible change in 3.2.0, although perhaps only due to shlib version matching release version.

--- geography/geos/buildlink3.mk
+++ geography/geos/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:24:35 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2009/12/14 23:36:05 gdt Exp $
 
 BUILDLINK_TREE+=	geos
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	geos
 GEOS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.geos+=	geos>=2.2.3
-BUILDLINK_ABI_DEPENDS.geos?=	geos>=3.0.0
+BUILDLINK_ABI_DEPENDS.geos?=	geos>=3.2.0
 BUILDLINK_PKGSRCDIR.geos?=	../../geography/geos
 .endif # GEOS_BUILDLINK3_MK
 
