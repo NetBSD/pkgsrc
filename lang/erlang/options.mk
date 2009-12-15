791a491847b491da34ccf45b608e28c5f0cbb52c
asau <asau@pkgsrc.org>
2009-12-15 12:07:57 +0000

    Update to Erlang/OTP R13B03.
    SunOS, Java and ODBC support by Filip Hajny
    and some tests by Matthias Kretschmer.
    
    
    Changes in R13B03
    -----------------
    
    There are mostly error corrections, but also some new functionality.
    Some highlights in the R13B03 release are:
     - Native Implemented Functions (NIFs) still experimental but very useful.
     - The documentation is built in a new way using xsltproc and Apache FOP.
       The layout is changed both in HTML and PDF versions. This is the first
       step, more changes and improvements will come in the following releases.
    
    
    Changes between R13A and R13B02 are unknown.
    
    
    Changes in R13A
    ---------------
    
    1.1 Erlang Language and Run-time System
    
    Multi-core and SMP performance improvements
    
    There SMP performance is significantly improved:
     - multiple, scheduler specific run queues;
     - further optimized message passing;
     - CPU-bound scheduler threads.
    
    Unicode support as described in EEP10
    
    New BIF's: atom_to_binary/2, binary_to_atom/2, binary_to_existing_atom/2.
    
    Independent Erlang clusters on the same host
    
    
    1.2 New Applications
    
    Reltool, release management tool.
    WxErlang
    
    
    1.3 New features in Existing Applications
    
    Common_test
     - SSH and SFTP support
     - test case groups
    
    Dialyzer
     - opaque type declarations;
     - UTF segments handling
    
    SSL
     - pure Erlang implementation (except crypto routines)
    
    STDLIB
     - scanner has been augmented to return white-space, comments
     and exact location of tokens.

--- lang/erlang/options.mk
+++ lang/erlang/options.mk
@@ -1,7 +1,9 @@
-# $NetBSD: options.mk,v 1.2 2006/05/23 15:09:52 tonio Exp $
+# $NetBSD: options.mk,v 1.3 2009/12/15 12:07:57 asau Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.erlang
-PKG_SUPPORTED_OPTIONS=	java
+PKG_SUPPORTED_OPTIONS=	java erlang-hipe
+PKG_OPTIONS_OPTIONAL_GROUPS=	odbc
+PKG_OPTIONS_GROUP.odbc=		iodbc unixodbc
 
 .include "../../mk/bsd.options.mk"
 
@@ -12,4 +14,31 @@ USE_JAVA2=		yes
 PLIST_SRC+=		PLIST.java
 .else
 CONFIGURE_ENV+=	JAVAC=none
+.endif
+
+.if !empty(PKG_OPTIONS:Merlang-hipe)
+###
+### XXX Need to check which platforms will enable this by default, if any.
+###	Should it be an option?
+###
+PLIST_SUBST+=		SMP="@comment "
+CONFIGURE_ARGS+=	--enable-hipe
+.else
+PLIST_SUBST+=		SMP=""
+CONFIGURE_ARGS+=	--disable-hipe
+.endif
+
+###
+### Provide iodbc/unixodbc option support
+###
+.if !empty(PKG_OPTIONS:Miodbc)
+.  include "../../databases/iodbc/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-odbc=${BUILDLINK_PREFIX.iodbc}
+PLIST_SRC+=		PLIST.odbc
+.endif
+
+.if !empty(PKG_OPTIONS:Munixodbc)
+.  include "../../databases/unixodbc/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-odbc=${BUILDLINK_PREFIX.unixodbc}
+PLIST_SRC+=		PLIST.odbc
 .endif
