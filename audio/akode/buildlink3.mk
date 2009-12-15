c2fc8b298f05c242bf2d16e55d47cc3b6981afc1
joerg <joerg@pkgsrc.org>
2009-12-15 21:30:51 +0000

    Recursive bump for libltdl

--- audio/akode/buildlink3.mk
+++ audio/akode/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:23:52 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/12/15 21:30:51 joerg Exp $
 
 BUILDLINK_TREE+=	akode
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	akode
 AKODE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.akode+=	akode>=2.0.2
+BUILDLINK_ABI_DEPENDS.akode+=	akode>=2.0.2nb2
 BUILDLINK_PKGSRCDIR.akode?=	../../audio/akode
 
 .include "../../devel/libltdl/buildlink3.mk"
