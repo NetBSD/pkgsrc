c57c9005ffe8276bcd60f5255b6b9d85a1ae4e57
joerg <joerg@pkgsrc.org>
2009-12-15 21:54:17 +0000

    Recursive bump for libltdl

--- security/openct/buildlink3.mk
+++ security/openct/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:21 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/12/15 22:10:39 joerg Exp $
 
 BUILDLINK_TREE+=	openct
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	openct
 OPENCT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.openct+=	openct>=0.6.15
+BUILDLINK_ABI_DEPENDS.openct+=	openct>=0.6.18nb2
 BUILDLINK_PKGSRCDIR.openct?=	../../security/openct
 pkgbase := openct
 .include "../../mk/pkg-build-options.mk"
