ca76c6ca33f351210946f52c9b71f138ff88e3b4
tnn <tnn@pkgsrc.org>
2009-12-16 22:28:32 +0000

    Compilation failed on Sun Studio 12. Disable mmx/sse2. Please don't re-enable this without extensive tests, it seems fragile.

--- x11/pixman/hacks.mk
+++ x11/pixman/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.3 2009/04/23 08:30:53 sno Exp $
+# $NetBSD: hacks.mk,v 1.4 2009/12/16 22:28:32 tnn Exp $
 #
 .if !defined(PIXMAN_HACKS_MK)
 PIXMAN_HACKS_MK=	# empty
@@ -8,9 +8,12 @@ PIXMAN_HACKS_MK=	# empty
 ### XXX SSE2 intrinsics require gcc-4.2+ to build unless
 ###	${MACHINE_ARCH} == "x86_64".
 ###
-.  if empty(PKGSRC_COMPILER:Msunpro) && \
-   (empty(MACHINE_ARCH:Mx86_64) && \
+.if (empty(MACHINE_ARCH:Mx86_64) && \
      (empty(CC_VERSION:Mgcc-4.[2-9]*) || empty(CC_VERSION:Mgcc-[5-9].*) || empty(CC_VERSION:Mgcc-[1-9][0-9]*)))
 CONFIGURE_ARGS+=	--disable-sse2
 .  endif
+.endif
+
+.if !empty(PKGSRC_COMPILER:Msunpro)
+CONFIGURE_ARGS+=	--disable-sse2 --disable-mmx
 .endif
