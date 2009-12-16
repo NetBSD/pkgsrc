ba00dc057e39e70e0d2dee0b06d4001765af5b23
tnn <tnn@pkgsrc.org>
2009-12-16 08:18:32 +0000

    Update to firefox-3.5.6. Security and bugfix release.
    While here, switch NetBSD build from sunaudio to OSS emulation.
    This greatly improves HTML5 video playback.
    (Yes, we ought to fix the busted sunaudio support or PKG_OPTIONalize this.
     Perhaps another day.)
    
    Advisories relating to this release:
    MFSA 2009-71 GeckoActiveXObject exception messages can be used to
                 enumerate installed COM objects
    MFSA 2009-70 Privilege escalation via chrome window.opener
    MFSA 2009-69 Location bar spoofing vulnerabilities
    MFSA 2009-68 NTLM reflection vulnerability
    MFSA 2009-67 Integer overflow, crash in libtheora video library
    MFSA 2009-66 Memory safety fixes in liboggplay media library
    MFSA 2009-65 Crashes with evidence of memory corruption (rv:1.9.1.6/ 1.9.0.16)

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,11 +1,11 @@
-# $NetBSD: dist.mk,v 1.2 2009/11/23 14:16:22 tnn Exp $
+# $NetBSD: dist.mk,v 1.3 2009/12/16 08:18:32 tnn Exp $
 #
 # used by devel/nspr/Makefile
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	3.5.5
+FIREFOX_VER=	3.5.6
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
