360368320984e114972b9d91588f8f3d6ddb4e45
wiz <wiz@pkgsrc.org>
2009-12-16 09:29:06 +0000

    Fix path to itself.

--- lang/erlang/buildlink3.mk
+++ lang/erlang/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2009/12/15 12:07:57 asau Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2009/12/16 09:29:06 wiz Exp $
 
 BUILDLINK_TREE+=	erlang
 
@@ -7,7 +7,7 @@ ERLANG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.erlang+=	erlang>=10.1
 BUILDLINK_ABI_DEPENDS.erlang?=	erlang>=10.1.9nb1
-BUILDLINK_PKGSRCDIR.erlang?=	../../wip/erlang
+BUILDLINK_PKGSRCDIR.erlang?=	../../lang/erlang
 
 pkgbase := erlang
 .include "../../mk/pkg-build-options.mk"
