72eeb2fb6f9457dd6e6c47fe9e271d78a2d0ffbb
drochner <drochner@pkgsrc.org>
2009-12-18 18:54:24 +0000

    put back the (non-default) "idea" option which got lost a while ago

--- security/libgcrypt/options.mk
+++ security/libgcrypt/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.4 2009/01/31 08:53:14 adam Exp $
+# $NetBSD: options.mk,v 1.5 2009/12/18 18:54:24 drochner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libgcrypt
-PKG_SUPPORTED_OPTIONS=
+PKG_SUPPORTED_OPTIONS=	idea
 
 .include "../../mk/bsd.prefs.mk"
 
@@ -20,4 +20,22 @@ PKG_SUGGESTED_OPTIONS+=	via-padlock
 .if empty(PKG_OPTIONS:Mvia-padlock)
 # Disable VIA Padlock support.
 CONFIGURE_ARGS+=	--disable-padlock-support
+.endif
+
+.if !empty(PKG_OPTIONS:Midea)
+# use of IDEA as crypto function
+LICENSE=	idea-license
+RESTRICTED=	Commercial distribution is claimed to require a license.
+NO_SRC_ON_CDROM=	${RESTRICTED}
+NO_BIN_ON_CDROM=	${RESTRICTED}
+
+DISTFILES+=	gcrypt.tar.bz2
+SITES.gcrypt.tar.bz2=	http://www.kfwebs.com/
+CFLAGS+=	-DUSE_IDEA
+MAKE_ENV+=	IDEAOBJECT=idea.lo
+USE_TOOLS+=	patch
+post-patch:	ideapatch
+ideapatch:
+	cd ${WRKDIR} && ${PATCH} <${FILESDIR}/idea-patch && \
+	${CP} idea.c ${WRKSRC}/cipher
 .endif
