c50aee9ab0b6cc69ee0fdd3e08121ff64f976f34
joerg <joerg@pkgsrc.org>
2009-12-18 11:01:16 +0000

    Fix dependency.

--- www/clearsilver/buildlink3.mk
+++ www/clearsilver/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:25:36 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2009/12/18 11:01:16 joerg Exp $
 
 BUILDLINK_DEPMETHOD.clearsilver?=	build
 
@@ -8,7 +8,7 @@ BUILDLINK_TREE+=	clearsilver-base
 CLEARSILVER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.clearsilver+=		clearsilver-base>=0.9.0
-BUILDLINK_ABI_DEPENDS.clearsilver+=	clearsilver>=0.10.1
+BUILDLINK_ABI_DEPENDS.clearsilver+=	clearsilver-base>=0.10.1
 BUILDLINK_PKGSRCDIR.clearsilver?=	../../www/clearsilver-base
 .endif # CLEARSILVER_BUILDLINK3_MK
 
