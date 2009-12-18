b680d69065a2f9c9c9cc4838f2ee5f6e0137d5f1
joerg <joerg@pkgsrc.org>
2009-12-18 12:05:15 +0000

    Fix some more issues with the b3.mk file.

--- www/clearsilver/buildlink3.mk
+++ www/clearsilver/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.9 2009/12/18 11:01:16 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2009/12/18 12:05:15 joerg Exp $
 
 BUILDLINK_DEPMETHOD.clearsilver?=	build
 
-BUILDLINK_TREE+=	clearsilver-base
+BUILDLINK_TREE+=	clearsilver
 
 .if !defined(CLEARSILVER_BUILDLINK3_MK)
 CLEARSILVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clearsilver+=		clearsilver-base>=0.9.0
+BUILDLINK_API_DEPENDS.clearsilver+=	clearsilver-base>=0.9.0
 BUILDLINK_ABI_DEPENDS.clearsilver+=	clearsilver-base>=0.10.1
 BUILDLINK_PKGSRCDIR.clearsilver?=	../../www/clearsilver-base
 .endif # CLEARSILVER_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-clearsilver-base
+BUILDLINK_TREE+=	-clearsilver
