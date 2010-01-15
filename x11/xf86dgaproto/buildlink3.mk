729781d67a406e18b995025f46eb77ac95deda08
tnn <tnn@pkgsrc.org>
2010-01-15 18:14:50 +0000

    Update to xf86dgaproto-2.1. No protocol changes, but some headers files are moving to libXxf86dga.

--- x11/xf86dgaproto/buildlink3.mk
+++ x11/xf86dgaproto/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:25:52 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2010/01/15 18:14:50 tnn Exp $
 
 BUILDLINK_DEPMETHOD.xf86dgaproto?=	build
 
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	xf86dgaproto
 .if !defined(XF86DGAPROTO_BUILDLINK3_MK)
 XF86DGAPROTO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xf86dgaproto+=	xf86dgaproto>=2.0.3
+BUILDLINK_API_DEPENDS.xf86dgaproto+=	xf86dgaproto>=2.1
 BUILDLINK_PKGSRCDIR.xf86dgaproto?=	../../x11/xf86dgaproto
 
 .include "../../x11/xproto/buildlink3.mk"
