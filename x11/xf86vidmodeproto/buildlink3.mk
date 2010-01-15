627274d29455636b163890aca0c32c6b3d570ca5
tnn <tnn@pkgsrc.org>
2010-01-15 18:20:47 +0000

    Update to xf86vidmodeproto-2.3. No protocol changes, but some headers are moving to libXxf86vm.

--- x11/xf86vidmodeproto/buildlink3.mk
+++ x11/xf86vidmodeproto/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:53 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2010/01/15 18:20:47 tnn Exp $
 
 BUILDLINK_DEPMETHOD.xf86vidmodeproto?=	build
 
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	xf86vidmodeproto
 .if !defined(XF86VIDMODEPROTO_BUILDLINK3_MK)
 XF86VIDMODEPROTO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xf86vidmodeproto+=	xf86vidmodeproto>=2.2.1
+BUILDLINK_API_DEPENDS.xf86vidmodeproto+=	xf86vidmodeproto>=2.3
 BUILDLINK_PKGSRCDIR.xf86vidmodeproto?=	../../x11/xf86vidmodeproto
 
 .include "../../x11/xproto/buildlink3.mk"
