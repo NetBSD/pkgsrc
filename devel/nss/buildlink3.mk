084ee429b4686cf4c6534aac1d5a408017203c7d
tnn <tnn@pkgsrc.org>
2010-01-16 14:41:25 +0000

    - update to 3.12.4.5 - reach over to xulrunner, track the stable gecko release - use external sqlite3 - cleanup - take maintainership
    This is the second part of PR pkg/42277.

--- devel/nss/buildlink3.mk
+++ devel/nss/buildlink3.mk
@@ -1,20 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:24:25 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2010/01/16 14:41:25 tnn Exp $
 
 BUILDLINK_TREE+=	nss
 
 .if !defined(NSS_BUILDLINK3_MK)
 NSS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nss+=		nss>=3.9.2nb1
-BUILDLINK_ABI_DEPENDS.nss+=	nss>=3.9.2nb4
+BUILDLINK_API_DEPENDS.nss+=	nss>=3.12.4.5
 BUILDLINK_PKGSRCDIR.nss?=	../../devel/nss
-
-BUILDLINK_FILES.nss+=		lib/nss/*
-BUILDLINK_FILES.nss+=		include/nss/*
-
-BUILDLINK_INCDIRS.nss+=		include/nss
-BUILDLINK_LIBDIRS.nss+=		lib/nss
-BUILDLINK_RPATHDIRS.nss+=	lib/nss
 
 .include "../../devel/nspr/buildlink3.mk"
 .endif # NSS_BUILDLINK3_MK
