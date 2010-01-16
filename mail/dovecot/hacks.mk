61eff7ff9ee6b7cb83a3c5e0b9d61284be504e67
wiz <wiz>
2010-01-16 17:57:36 +0000

    Remove workaround for compiler bug in gcc2 on sparc64.

--- mail/dovecot/hacks.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD: hacks.mk,v 1.3 2008/06/22 17:41:23 ghen Exp $
-
-.include "../../mk/compiler.mk"
-
-# Avoid an ICE in gcc2 on sparc64
-.if !empty(CC_VERSION:Mgcc-*)
-PKG_HACKS+=		sparc64-gcc2-ice
-CONFIGURE_ENV+=		F77=${FALSE:Q}
-.endif
