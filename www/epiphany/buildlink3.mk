e1436a7461bf143627a36e36a22fa6849b480c29
tnn <tnn>
2010-01-16 14:46:27 +0000

    Update to epiphany-2.28.2. gecko support has been removed upstream, so switch to webkit rendering.

--- www/epiphany/buildlink3.mk
+++ www/epiphany/buildlink3.mk
@@ -1,8 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.30 2009/08/26 19:56:45 sno Exp $
-#
-# This Makefile fragment is included by packages that use epiphany.
-#
-# This file was created automatically using createbuildlink-3.1.
+# $NetBSD: buildlink3.mk,v 1.31 2010/01/16 14:46:27 tnn Exp $
 #
 
 BUILDLINK_TREE+=	epiphany
@@ -10,8 +6,7 @@ BUILDLINK_TREE+=	epiphany
 .if !defined(EPIPHANY_BUILDLINK3_MK)
 EPIPHANY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.epiphany+=		epiphany>=1.8.2
-BUILDLINK_ABI_DEPENDS.epiphany?=		epiphany>=2.26.3nb2
+BUILDLINK_API_DEPENDS.epiphany+=		epiphany>=2.28
 BUILDLINK_PKGSRCDIR.epiphany?=		../../www/epiphany
 
 .include "../../mk/bsd.fast.prefs.mk"
@@ -20,7 +15,8 @@ BUILDLINK_PKGSRCDIR.epiphany?=		../../www/epiphany
 .include "../../devel/libbonobo/buildlink3.mk"
 .include "../../devel/libglade/buildlink3.mk"
 .include "../../devel/libgnomeui/buildlink3.mk"
-.include "../../devel/xulrunner/buildlink3.mk"
+.include "../../devel/nss/buildlink3.mk"
+.include "../../www/webkit-gtk/buildlink3.mk"
 .include "../../net/ORBit2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
