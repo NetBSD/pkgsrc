c4f4128a64d0e0e2ffde3b4654376d816599ce2a
minskim <minskim@pkgsrc.org>
2010-01-16 03:52:40 +0000

    Update dvipdfmx to 20090708.
    Major changes:
        - Add -E option that enables DVIPDFM emulation mode in DVIPDFMx.
        - Don't try to convert included PDF files whose version number
          is higher than output file version. (If run as dvipdfm,
          conversion still occurs.)
        - Added dvipdfm compatibility mode. This mode is triggered
          if dvipdfmx is called as "dvipdfm", and likewise extractbb
          as "ebb". (The latter mode was already present.)
        - Made predefined paper sizes match values given by libpaper.

--- print/dvipdfmx/map.mk
+++ print/dvipdfmx/map.mk
@@ -1,4 +1,4 @@
-# $NetBSD: map.mk,v 1.8 2009/01/14 06:35:22 minskim Exp $
+# $NetBSD: map.mk,v 1.9 2010/01/16 03:52:41 minskim Exp $
 # This Makefile fragment is intended to be included by packages that
 # install font maps used by dvipdfmx.  It takes care of adding and
 # removing font map entires in dvipdfmx.cfg.
@@ -14,12 +14,12 @@ DVIPDFMX_MAP_MK=	# defined
 
 DVIPDFMX_FONTMAPS?=	# empty
 
-.if empty(DISTNAME:Mdvipdfmx-[0-9]*)
+.if empty(PKGNAME:Mdvipdfmx-[0-9]*)
 DEPENDS+=		dvipdfmx>=0.0.0.20070518:../../print/dvipdfmx
 .endif
 
 FILES_SUBST+=		DVIPDFMX_FONTMAPS=${DVIPDFMX_FONTMAPS:Q}
-FILES_SUBST+=		DVIPDFMX_CONFIG_DIR="${LOCALBASE}/share/texmf-dist/dvipdfm"
+FILES_SUBST+=		DVIPDFMX_CONFIG_DIR="${LOCALBASE}/share/texmf/dvipdfmx"
 INSTALL_TEMPLATES+=	../../print/dvipdfmx/files/map.tmpl
 DEINSTALL_TEMPLATES+=	../../print/dvipdfmx/files/map.tmpl
 
