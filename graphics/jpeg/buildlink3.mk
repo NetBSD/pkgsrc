c13132904514d380a4af2fc1602e058bbf5d7780
tnn <tnn@pkgsrc.org>
2010-01-17 18:41:54 +0000

    library major was bumped again. (sigh)

--- graphics/jpeg/buildlink3.mk
+++ graphics/jpeg/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.12 2009/08/26 19:56:41 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2010/01/17 18:41:54 tnn Exp $
 
 BUILDLINK_TREE+=	jpeg
 
 .if !defined(JPEG_BUILDLINK3_MK)
 JPEG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.jpeg+=	jpeg>=7
-#BUILDLINK_ABI_DEPENDS.jpeg+=	jpeg>=7
-BUILDLINK_ABI_DEPENDS.jpeg?=	jpeg>=7
+BUILDLINK_API_DEPENDS.jpeg+=	jpeg>=8
+#BUILDLINK_ABI_DEPENDS.jpeg+=	jpeg>=8
+BUILDLINK_ABI_DEPENDS.jpeg?=	jpeg>=8
 BUILDLINK_PKGSRCDIR.jpeg?=	../../graphics/jpeg
 .endif # JPEG_BUILDLINK3_MK
 
