13e60476428350a03efe92ca0debe7515b3f45d1
wiz <wiz>
2010-01-20 13:44:02 +0000

    Update to 0.7.1:
    0.7.1
    =====
    
    - Don't leak target regex.
    - Make GSSDPClient ignore Point to Point interfaces.
    - Use SO_REUSEPORT if present. Darwin and some BSDs don't have SO_REUSEADDR, but
      SO_REUSEPORT.
    - If we can't create a request socket don't try to create a multicast socket.
    - Have specific GError code for interfaces without an IP address.
    - Actually remove gssdp_client_new_full().
    
    Bugs fixed:
    
    1898 - GSSDPClient keeps autoselecting my VPN
    1810 - Not possible to run multiple ssdp clients on darwin
    1800 - leak of a gregex in gssdp-resource-browser
    1796 - gssdp_client_new_full is declared in header but not implemented
    
    All contributors:
    
    Olivier Crête <olivier.crete@collabora.co.uk>
    Ross Burton <ross@linux.intel.com>
    Iain Holmes <iain@linux.intel.com>
    Mattias Wadman <mattias.wadman@gmail.com>
    Zeeshan Ali (Khattak) <zeeshanak@gnome.org>
    
    0.7
    ===
    
    - Join the multicast group on the specified interface.
    - Put the message src timeout in the specified GMainContext.
    - "host-ip" property moved from GUPnPContext to parent GSSDPClient.
    - Use inet_aton instead of inet_addr and handle the error from it.
    - gssdp-device-sniffer ported to GtkBuilder.
    - Version-independent target match. This makes client forward and backward
      compatible with resources that implement different version of the specs than
      the client.
    - Version-independent M-SEARCH response. This has the same effect as above but
      only the other way around.
    - GSSDPClient now binds to interface rather than IP.
    - No need to find the default route.
    - Add "active" property to GSSDPClient.
    - Fix pkg-config paths.
    - Add basic .gitignore.
    - Other minor/internal changes.
    
    Bugs fixed:
    
    1621 - new gssdp timeout not added to the specific maincontext
    1570 - gupnp doesn't set the pkgconfig lib dir correctly in 64 bit env
    
    Dependencies dropped:
    
    - libglade
    
    All contributors:
    
    Zeeshan Ali (Khattak) <zeeshanak@gnome.org>
    Ross Burton <ross@linux.intel.com>
    Jens Georg <mail@jensge.org>
    Olivier Crête <olivier.crete@collabora.co.uk>
    
    WARNING: This release is API and ABI incompatible with previous releases.

--- net/gssdp/buildlink3.mk
+++ net/gssdp/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/08/02 11:24:06 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/01/20 13:44:02 wiz Exp $
 
 BUILDLINK_TREE+=	gssdp
 
 .if !defined(GSSDP_BUILDLINK3_MK)
 GSSDP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gssdp+=	gssdp>=0.6.4
+BUILDLINK_API_DEPENDS.gssdp+=	gssdp>=0.7.1
 BUILDLINK_PKGSRCDIR.gssdp?=	../../net/gssdp
 
 .include "../../devel/glib2/buildlink3.mk"
