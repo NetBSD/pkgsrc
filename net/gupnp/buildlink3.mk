2c93f35a7ae12a377f27f10d198a9ca91c6838a6
wiz <wiz>
2010-01-20 14:04:52 +0000

    Bump PKGREVISION for gupnp/gssdp API changes.

--- net/gupnp/buildlink3.mk
+++ net/gupnp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/01/20 13:44:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2010/01/20 14:04:53 wiz Exp $
 
 BUILDLINK_TREE+=	gupnp
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	gupnp
 GUPNP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gupnp+=	gupnp>=0.13.2
+BUILDLINK_ABI_DEPENDS.gupnp?=	gupnp>=0.13.2
 BUILDLINK_PKGSRCDIR.gupnp?=	../../net/gupnp
 
 .include "../../devel/glib2/buildlink3.mk"
