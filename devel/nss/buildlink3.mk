568ccb20b22c55c0424538d13389268fd8dddf29
tnn <tnn@pkgsrc.org>
2010-01-20 17:24:57 +0000

    Put back BUILDLINK_LIBDIR & BUILDLINK_RPATHDIR for now; although I don't like it. Needed to build mail/evolution-exchange due to bl3's .la mangling.

--- devel/nss/buildlink3.mk
+++ devel/nss/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2010/01/16 14:41:25 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2010/01/20 17:24:57 tnn Exp $
 
 BUILDLINK_TREE+=	nss
 
@@ -7,6 +7,9 @@ NSS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.nss+=	nss>=3.12.4.5
 BUILDLINK_PKGSRCDIR.nss?=	../../devel/nss
+
+BUILDLINK_LIBDIRS.nss+=		lib/nss
+BUILDLINK_RPATHDIRS.nss+=	lib/nss
 
 .include "../../devel/nspr/buildlink3.mk"
 .endif # NSS_BUILDLINK3_MK
