21bc2c0400b2881fc8113cf6f7f8b33ce4f30d6e
drochner <drochner@pkgsrc.org>
2010-01-21 17:05:59 +0000

    make the "sasl" extension an option (default on to match previous behaviour, although I don't see any use in the pkgsrc ecosystem), and propagate it properly

--- net/gtk-vnc/buildlink3.mk
+++ net/gtk-vnc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/10/26 11:02:21 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2010/01/21 17:05:59 drochner Exp $
 
 BUILDLINK_TREE+=	gtk-vnc
 
@@ -8,6 +8,12 @@ GTK_VNC_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gtk-vnc+=	gtk-vnc>=0.3.7
 BUILDLINK_ABI_DEPENDS.gtk-vnc?=	gtk-vnc>=0.3.8nb2
 BUILDLINK_PKGSRCDIR.gtk-vnc?=	../../net/gtk-vnc
+
+pkgbase := gtk-vnc
+.include "../../mk/pkg-build-options.mk"
+.if !empty(PKG_BUILD_OPTIONS.gtk-vnc:Mvnc-sasl)
+.include "../../security/cyrus-sasl/buildlink3.mk"
+.endif
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
