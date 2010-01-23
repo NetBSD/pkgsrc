fe22375e31e56f0875bc05d68be09cfd5bbe279f
joerg <joerg@pkgsrc.org>
2010-01-23 16:32:11 +0000

    Update to nginx-0.8.32. Way too many changes to list after two years of development. Based on wip/nginx.

--- www/nginx/options.mk
+++ www/nginx/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1.1.1 2007/12/11 09:12:26 ghen Exp $
+# $NetBSD: options.mk,v 1.2 2010/01/23 16:32:11 joerg Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nginx
-PKG_SUPPORTED_OPTIONS=	ssl pcre
+PKG_SUPPORTED_OPTIONS=	ssl pcre dav flv sub gtools mail-proxy memcache realip
 PKG_SUGGESTED_OPTIONS=	ssl pcre
 
 .include "../../mk/bsd.options.mk"
@@ -9,6 +9,7 @@ PKG_SUGGESTED_OPTIONS=	ssl pcre
 .if !empty(PKG_OPTIONS:Mssl)
 .include "../../security/openssl/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-mail_ssl_module
+CONFIGURE_ARGS+=	--with-http_ssl_module
 .endif
 
 .if !empty(PKG_OPTIONS:Mpcre)
@@ -17,3 +18,32 @@ CONFIGURE_ARGS+=	--with-mail_ssl_module
 CONFIGURE_ARGS+=	--without-pcre
 CONFIGURE_ARGS+=	--without-http_rewrite_module
 .endif
+
+.if !empty(PKG_OPTIONS:Mdav)
+CONFIGURE_ARGS+=	--with-http_dav_module
+.endif
+
+.if !empty(PKG_OPTIONS:Mflv)
+CONFIGURE_ARGS+=	--with-http_flv_module
+.endif
+
+.if !empty(PKG_OPTIONS:Msub)
+CONFIGURE_ARGS+=	--with-http_sub_module
+.endif
+
+.if !empty(PKG_OPTIONS:Mgtools)
+CONFIGURE_ARGS+=	--with-google_perftools_module
+.endif
+
+.if !empty(PKG_OPTIONS:Mmail-proxy)
+CONFIGURE_ARGS+=	--with-mail
+.endif
+
+.if empty(PKG_OPTIONS:Mmemcache)
+CONFIGURE_ARGS+=	--without-http_memcached_module
+.endif
+
+.if !empty(PKG_OPTIONS:Mrealip)
+CONFIGURE_ARGS+=       --with-http_realip_module
+.endif
+
