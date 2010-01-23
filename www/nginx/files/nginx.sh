5a89a5e1dfb6488e62ff00976988cc9732c6436a
joerg <joerg@pkgsrc.org>
2010-01-23 16:32:11 +0000

    Update to nginx-0.8.32. Way too many changes to list after two years of development. Based on wip/nginx.

--- www/nginx/files/nginx.sh
+++ www/nginx/files/nginx.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: nginx.sh,v 1.1.1.1 2007/12/11 09:12:26 ghen Exp $
+# $NetBSD: nginx.sh,v 1.2 2010/01/23 16:32:11 joerg Exp $
 #
 # PROVIDE: nginx
 # REQUIRE: DAEMON
@@ -12,6 +12,8 @@ rcvar=${name}
 command="@PREFIX@/sbin/${name}"
 required_files="@PKG_SYSCONFDIR@/${name}.conf"
 pidfile="@VARBASE@/run/${name}.pid"
+start_precmd="ulimit -n 2048"
+extra_commands="reload"
 
 load_rc_config $name
 run_rc_command "$1"
