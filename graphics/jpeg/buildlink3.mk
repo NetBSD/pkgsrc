2035d4fe77d6ea74dbdcb753c169c6293486e367
tnn <tnn@pkgsrc.org>
2010-01-25 08:45:34 +0000

    bump dependency again because linking was broken in a two day window.

--- graphics/jpeg/buildlink3.mk
+++ graphics/jpeg/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.13 2010/01/17 18:41:54 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2010/01/25 08:45:34 tnn Exp $
 
 BUILDLINK_TREE+=	jpeg
 
 .if !defined(JPEG_BUILDLINK3_MK)
 JPEG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.jpeg+=	jpeg>=8
-#BUILDLINK_ABI_DEPENDS.jpeg+=	jpeg>=8
-BUILDLINK_ABI_DEPENDS.jpeg?=	jpeg>=8
+BUILDLINK_API_DEPENDS.jpeg+=	jpeg>=8nb1
+#BUILDLINK_ABI_DEPENDS.jpeg+=	jpeg>=8nb1
+BUILDLINK_ABI_DEPENDS.jpeg?=	jpeg>=8nb1
 BUILDLINK_PKGSRCDIR.jpeg?=	../../graphics/jpeg
 .endif # JPEG_BUILDLINK3_MK
 
