8430daedd0909aff466e5c85de1365b51c22808d
tron <tron@pkgsrc.org>
2010-01-26 16:22:59 +0000

    Update "firefox" package to 3.5.7. Changes since version 3.5.6: - Fixed a common stability issue. - Fixed a problem with how updates were being presented to users.
    Approved by Tobias Nygren.

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.4 2010/01/19 10:07:49 tnn Exp $
+# $NetBSD: dist.mk,v 1.5 2010/01/26 16:22:59 tron Exp $
 #
 # used by devel/nspr/Makefile
 # used by devel/nss/Makefile
@@ -6,7 +6,7 @@
 # used by www/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	3.5.6
+FIREFOX_VER=	3.5.7
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
