c3e2c489965ef062693dc1e9090a5eb8557de2bd
ghen <ghen>
2010-01-26 13:31:50 +0000

    When using the ncursesw option, set USE_NCURSES to force the real ncursesw, NetBSD's built-in curses is not sufficient.  See devel/ncursesw/builtin.mk.

--- mail/mutt-devel/options.mk
+++ mail/mutt-devel/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.36 2009/07/16 20:23:41 tonio Exp $
+# $NetBSD: options.mk,v 1.37 2010/01/26 13:31:50 ghen Exp $
 
 # Global and legacy options
 
@@ -50,6 +50,7 @@ CONFIGURE_ARGS+=	--with-sasl=${BUILDLINK_PREFIX.cyrus-sasl}
 ### ncursesw
 ###
 .if !empty(PKG_OPTIONS:Mncursesw)
+USE_NCURSES=		yes
 .  include "../../devel/ncursesw/buildlink3.mk"
 .else
 SUBST_CLASSES+=		curse
