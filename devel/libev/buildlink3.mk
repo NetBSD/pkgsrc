69208aa004fa82242afcc5b0d2df63dc6932a723
tnn <tnn>
2010-01-29 15:05:00 +0000

    avoid conflicts with libevent

--- devel/libev/buildlink3.mk
+++ devel/libev/buildlink3.mk
@@ -1,13 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/08/18 14:10:53 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2010/01/29 15:05:00 tnn Exp $
 
 BUILDLINK_TREE+=	libev
 
 .if !defined(LIBEV_BUILDLINK3_MK)
 LIBEV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libev+=libev>=3.8
-BUILDLINK_ABI_DEPENDS.libev+=libev>=3.8
+BUILDLINK_API_DEPENDS.libev+=libev>=3.9nb1
+BUILDLINK_ABI_DEPENDS.libev+=libev>=3.9nb1
 BUILDLINK_PKGSRCDIR.libev?=	../../devel/libev
+
+BUILDLINK_LIBDIRS.libev+=	lib/ev
+BUILDLINK_RPATHDIRS.libev+=	lib/ev
+BUILDLINK_INCDIRS.libev+=	include/ev
+
 .endif # LIBEV_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libev
