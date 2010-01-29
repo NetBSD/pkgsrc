86b077a0540d0bffc024038f470a2902d8f2f3df
minskim <minskim@pkgsrc.org>
2010-01-29 04:10:14 +0000

    Set INSTALL_TEMPLATES and DEINSTALL_TEMPLATES only when TEX_TEXMF_DIRS is not empty.

--- print/kpathsea/texmf.mk
+++ print/kpathsea/texmf.mk
@@ -1,4 +1,4 @@
-# $NetBSD: texmf.mk,v 1.2 2009/01/11 09:11:07 minskim Exp $
+# $NetBSD: texmf.mk,v 1.3 2010/01/29 04:10:14 minskim Exp $
 #
 # This Makefile fragment is intended to be included by packages that
 # install TeX packages.  It rebuilds the ls-R databases at
@@ -21,8 +21,10 @@ TEX_TEXMF_DIRS?=	${PREFIX}/share/texmf-dist
 EVAL_PREFIX+=		KPATHSEA_PREFIX=kpathsea
 FILES_SUBST+=		MKTEXLSR=${KPATHSEA_PREFIX:Q}/bin/mktexlsr
 FILES_SUBST+=		TEXMF_DIRS=${TEX_TEXMF_DIRS:Q}
+.if !empty(TEX_TEXMF_DIRS)
 INSTALL_TEMPLATES+=	../../print/kpathsea/files/texmf-install.tmpl
 DEINSTALL_TEMPLATES+=	../../print/kpathsea/files/texmf-deinstall.tmpl
+.endif
 
 .  if !empty(TEX_CONFIG_NAMES)
 FILES_SUBST+=		CONFIG_NAMES=${TEX_CONFIG_NAMES:Q}
