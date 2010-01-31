c7671627c47c6f7ba2afa2bd2f93c0ff60eb59a2
tron <tron>
2010-01-31 14:04:37 +0000

    Pullup ticket #2982 - requested by tnn devel/libev: avoid conflict with "libevent" package devel/libevent: avoid conflict with "libev" package devel/p5-EV: update to match "libev" update
    Revisions pulled up:
    - devel/libev/Makefile				1.6 via patch
    - devel/libev/PLIST				1.2
    - devel/libev/buildlink3.mk			1.3 via patch
    - devel/libevent/Makefile			1.35 via patch
    - devel/p5-EV/Makefile				1.3
    ---
    Module Name:	pkgsrc
    Committed By:	tnn
    Date:		Fri Jan 29 15:05:00 UTC 2010
    
    Modified Files:
    	pkgsrc/devel/libev: Makefile PLIST buildlink3.mk
    
    Log Message:
    avoid conflicts with libevent
    ---
    Module Name:	pkgsrc
    Committed By:	tnn
    Date:		Fri Jan 29 15:07:12 UTC 2010
    
    Modified Files:
    	pkgsrc/devel/p5-EV: Makefile
    
    Log Message:
    bump revision due to libev/bl3.mk bump
    ---
    Module Name:	pkgsrc
    Committed By:	tnn
    Date:		Fri Jan 29 15:13:00 UTC 2010
    
    Modified Files:
    	pkgsrc/devel/libevent: Makefile
    
    Log Message:
    adjust CONFLICTSCVS: ----------------------------------------------------------------------

--- devel/libev/buildlink3.mk
+++ devel/libev/buildlink3.mk
@@ -1,13 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/08/18 14:10:53 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.2.4.1 2010/01/31 14:04:37 tron Exp $
 
 BUILDLINK_TREE+=	libev
 
 .if !defined(LIBEV_BUILDLINK3_MK)
 LIBEV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libev+=libev>=3.8
-BUILDLINK_ABI_DEPENDS.libev+=libev>=3.8
+BUILDLINK_API_DEPENDS.libev+=libev>=3.8nb1
+BUILDLINK_ABI_DEPENDS.libev+=libev>=3.8nb1
 BUILDLINK_PKGSRCDIR.libev?=	../../devel/libev
+
+BUILDLINK_LIBDIRS.libev+=	lib/ev
+BUILDLINK_RPATHDIRS.libev+=	lib/ev
+BUILDLINK_INCDIRS.libev+=	include/ev
+
 .endif # LIBEV_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libev
