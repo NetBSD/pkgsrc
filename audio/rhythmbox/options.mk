f3906ecfe9ec03151d1cb6b3cf9a2b3e1025e9a1
drochner <drochner@pkgsrc.org>
2010-02-03 12:45:15 +0000

    make "daap" support an option which if off per default -- it depends on "avahi" bump PKGREVISION

--- audio/rhythmbox/options.mk
+++ audio/rhythmbox/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.1 2009/05/26 20:26:58 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2010/02/03 12:45:15 drochner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.rhythmbox
-PKG_SUPPORTED_OPTIONS=	hal
+PKG_SUPPORTED_OPTIONS=	hal daap
 PKG_SUGGESTED_OPTIONS=	hal
-PLIST_VARS+=		hal
+PLIST_VARS+=		hal daap
 
 .include "../../mk/bsd.options.mk"
 
@@ -18,4 +18,11 @@ PLIST.hal=		yes
 .include "../../sysutils/hal/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-hal
+.endif
+
+.if !empty(PKG_OPTIONS:Mdaap)
+.include "../../net/avahi/buildlink3.mk"
+PLIST.daap=		yes
+.else
+CONFIGURE_ARGS+=	--disable-daap
 .endif
