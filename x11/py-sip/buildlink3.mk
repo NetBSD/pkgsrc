a96be7fb1817b6ecc8f6caaf570747d2d87f67aa
drochner <drochner@pkgsrc.org>
2010-02-04 18:34:43 +0000

    update to 4.10 changes: added some methods and options
    pkgsrc note: this changes the API revision compiled into
    the runtime library, so updates of py-qt* must be enforced

--- x11/py-sip/buildlink3.mk
+++ x11/py-sip/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/11/24 17:17:35 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2010/02/04 18:34:43 drochner Exp $
 
 BUILDLINK_TREE+=	py-sip
 
@@ -7,7 +7,7 @@ PY_SIP_BUILDLINK3_MK:=
 
 .include "../../lang/python/pyversion.mk"
 
-BUILDLINK_API_DEPENDS.py-sip+=	${PYPKGPREFIX}-sip>=4.9.1
+BUILDLINK_API_DEPENDS.py-sip+=	${PYPKGPREFIX}-sip>=4.10
 BUILDLINK_PKGSRCDIR.py-sip?=	../../x11/py-sip
 .endif # PY_SIP_BUILDLINK3_MK
 
