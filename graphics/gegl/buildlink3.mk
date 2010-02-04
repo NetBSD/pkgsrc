14983b978629c72d0817b63ea24a7eb083bb35dd
adam <adam@pkgsrc.org>
2010-02-04 12:28:43 +0000

    Fix building with babl-0.1.2

--- graphics/gegl/buildlink3.mk
+++ graphics/gegl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/09/08 20:30:25 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2010/02/04 12:28:43 adam Exp $
 
 BUILDLINK_TREE+=	gegl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gegl
 GEGL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gegl+=	gegl>=0.0.20
-BUILDLINK_ABI_DEPENDS.gegl?=	gegl>=0.1.0nb1
+BUILDLINK_ABI_DEPENDS.gegl+=	gegl>=0.1.0nb3
 BUILDLINK_PKGSRCDIR.gegl?=	../../graphics/gegl
 
 pkgbase := gegl
@@ -17,7 +17,7 @@ pkgbase := gegl
 .endif
 
 .include "../../graphics/babl/buildlink3.mk"
-.include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
 .endif # GEGL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gegl
