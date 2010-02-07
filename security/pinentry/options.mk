73661a5ed9a27ba077137ccdf31d2efe3ef4fc3d
drochner <drochner@pkgsrc.org>
2010-02-07 15:36:58 +0000

    put the pinentry variants which depend on a toolkit into seperate pkgs rather than having their build controlled by options (no change in the default case)

--- security/pinentry/options.mk
+++ security/pinentry/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2008/04/12 22:43:12 jlam Exp $
+# $NetBSD: options.mk,v 1.6 2010/02/07 15:36:58 drochner Exp $
 #
 
 # XXX This usage of bsd.options.mk is incorrect.  The package should
@@ -11,33 +11,20 @@ PKG_OPTIONS_VAR=	PKG_OPTIONS.pinentry
 PKG_SUPPORTED_OPTIONS=	gtk gtk2 qt
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		${PKG_SUPPORTED_OPTIONS}
-
-.if !empty(PKG_OPTIONS:Mgtk)
-CONFIGURE_ARGS+=	--enable-pinentry-gtk
-PLIST.gtk=		yes
-.  include "../../x11/gtk/buildlink3.mk"
-.else
 CONFIGURE_ARGS+=	--disable-pinentry-gtk
+.if !empty(PKG_OPTIONS:Mgtk)
+DEPENDS+=		pinentry-gtk-[0-9]*:../../security/pinentry-gtk
+PINENTRY_DEFAULT=	gtk
 .endif
 
-.if !empty(PKG_OPTIONS:Mgtk2)
-CONFIGURE_ARGS+=	--enable-pinentry-gtk2
-PLIST.gtk2=		yes
-.  include "../../x11/gtk2/buildlink3.mk"
-.else
 CONFIGURE_ARGS+=	--disable-pinentry-gtk2
+.if !empty(PKG_OPTIONS:Mgtk2)
+DEPENDS+=		pinentry-gtk2-[0-9]*:../../security/pinentry-gtk2
+PINENTRY_DEFAULT=	gtk2
 .endif
 
-.if !empty(PKG_OPTIONS:Mqt)
-USE_LANGUAGES+=		c c++
-USE_TOOLS+=		gmake
-CONFIGURE_ARGS+=	--enable-pinentry-qt
-CONFIGURE_ARGS+=	--with-qt-dir=${QTDIR:Q}
-CONFIGURE_ARGS+=	--with-qt-includes=${BUILDLINK_PREFIX.qt3-libs}/qt3/include
-CONFIGURE_ARGS+=	--with-qt-libraries=${BUILDLINK_PREFIX.qt3-libs}/qt3/lib
-PLIST.qt=		yes
-.  include "../../x11/qt3-libs/buildlink3.mk"
-.else
 CONFIGURE_ARGS+=	--disable-pinentry-qt
+.if !empty(PKG_OPTIONS:Mqt)
+DEPENDS+=		pinentry-qt-[0-9]*:../../security/pinentry-qt
+PINENTRY_DEFAULT=	qt
 .endif
