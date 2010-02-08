99d74ba1d11bff39d15d2f6b594b8e480e7348d0
taca <taca@pkgsrc.org>
2010-02-08 15:44:48 +0000

    Update typolight28-translation package to 20100207.
    * Update Polish language files, supporting really 2.8.RC2.
    * Add missing language files which should be added: Bosnian, Croatian,
      Finnish, Hungarian, Slovenian and Tagalog.
    * Drop executable permission from translation files.

--- www/typolight28-translations/PLIST.pl
+++ www/typolight28-translations/PLIST.pl
@@ -1,4 +1,4 @@
-@comment $NetBSD: PLIST.pl,v 1.1.1.1 2009/12/02 16:10:55 taca Exp $
+@comment $NetBSD: PLIST.pl,v 1.2 2010/02/08 15:44:48 taca Exp $
 ${TL_WEBDIR}/system/modules/backend/languages/pl/.htaccess
 ${TL_WEBDIR}/system/modules/backend/languages/pl/countries.php
 ${TL_WEBDIR}/system/modules/backend/languages/pl/default.php
@@ -10,6 +10,7 @@ ${TL_WEBDIR}/system/modules/backend/languages/pl/tl_content.php
 ${TL_WEBDIR}/system/modules/backend/languages/pl/tl_files.php
 ${TL_WEBDIR}/system/modules/backend/languages/pl/tl_form.php
 ${TL_WEBDIR}/system/modules/backend/languages/pl/tl_form_field.php
+${TL_WEBDIR}/system/modules/backend/languages/pl/tl_install.php
 ${TL_WEBDIR}/system/modules/backend/languages/pl/tl_layout.php
 ${TL_WEBDIR}/system/modules/backend/languages/pl/tl_log.php
 ${TL_WEBDIR}/system/modules/backend/languages/pl/tl_maintenance.php
