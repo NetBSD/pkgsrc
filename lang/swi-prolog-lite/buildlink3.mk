e41a50d52d5b99375cd51c85b307d17cf0e0e5b3
asau <asau@pkgsrc.org>
2010-02-10 21:56:40 +0000

    Update to SWI Prolog 5.9.7. Lots of changes since 5.6.17.

--- lang/swi-prolog-lite/buildlink3.mk
+++ lang/swi-prolog-lite/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/05/20 00:58:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2010/02/10 21:56:41 asau Exp $
 
 BUILDLINK_TREE+=	swi-prolog-lite
 
@@ -6,11 +6,11 @@ BUILDLINK_TREE+=	swi-prolog-lite
 SWI_PROLOG_LITE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.swi-prolog-lite+=	swi-prolog-lite>=5.2.9
-BUILDLINK_ABI_DEPENDS.swi-prolog-lite?=	swi-prolog-lite>=5.6.17nb4
 BUILDLINK_PKGSRCDIR.swi-prolog-lite?=	../../lang/swi-prolog-lite
 
 .include "../../devel/ncurses/buildlink3.mk"
 .include "../../devel/readline/buildlink3.mk"
-.endif # SWI_PROLOG_LITE_BUILDLINK3_MK
+#.include "../../devel/gmp/buildlink3.mk" ?
+.endif	# SWI_PROLOG_LITE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-swi-prolog-lite
