10995907dd07cb62fd56ba6a626a49e7421713a4
joerg <joerg>
2010-02-11 13:37:44 +0000

    Add an option for the oracle backend.

--- www/py-django/options.mk
+++ www/py-django/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.7 2010/02/10 19:30:09 joerg Exp $
+# $NetBSD: options.mk,v 1.8 2010/02/11 13:37:45 joerg Exp $
 
 PKG_OPTIONS_VAR =	PKG_OPTIONS.django
-PKG_SUPPORTED_OPTIONS=	mysql pgsql sqlite
+PKG_SUPPORTED_OPTIONS=	mysql oracle pgsql sqlite
 PKG_SUGGESTED_OPTIONS=	pgsql
 
 .include "../../mk/bsd.options.mk"
@@ -11,6 +11,11 @@ PLIST_VARS+=	${PKG_SUPPORTED_OPTIONS}
 .if !empty(PKG_OPTIONS:Mmysql)
 DEPENDS+=	${PYPKGPREFIX}-mysqldb-[0-9]*:../../databases/py-mysqldb
 PLIST.mysql=	yes
+.endif
+
+.if !empty(PKG_OPTIONS:Moracle)
+DEPENDS+=	${PYPKGPREFIX}-cx_Oracle-[0-9]*:../../databases/py-cx_Oracle
+PLIST.oracle=	yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mpgsql)
