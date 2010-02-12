5471fed8589f05a668ce6faa8c2977d288800fd5
adam <adam@pkgsrc.org>
2010-02-12 10:31:04 +0000

    Changes 1.42.0: * New Libraries: Uuid. * Updated Libraries: Asio, Circular Buffer, Fusion, Graph, Integer, Iostreams,   Program.Options, PropertyMap, Proto, Regex, Spirit, Unordered, Xpressive.

--- devel/boost-build/buildlink3.mk
+++ devel/boost-build/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/11/24 09:53:53 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2010/02/12 10:31:04 adam Exp $
 
 BUILDLINK_TREE+=	boost-build
 
 .if !defined(BOOST_BUILD_BUILDLINK3_MK)
 BOOST_BUILD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.41.*
+BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.42.*
 BUILDLINK_DEPMETHOD.boost-build?=	build
 BUILDLINK_PKGSRCDIR.boost-build?=	../../devel/boost-build
 .endif # BOOST_BUILD_BUILDLINK3_MK
