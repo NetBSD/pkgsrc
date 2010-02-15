d923cfab780a86d01121fab37ff327011cec4dc2
wiz <wiz@pkgsrc.org>
2010-02-15 15:57:48 +0000

    Initial import of qtscriptgenerator-0.1.0nb3 from wip, packaged by myself.
    Qt Script Generator is a tool that generates Qt bindings for Qt
    Script. With the generated bindings you get access to substantial
    portions of the Qt API from within Qt Script.

---
+++ devel/qtscriptgenerator/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/02/15 15:57:49 wiz Exp $
+
+BUILDLINK_TREE+=	qtscriptgenerator
+
+.if !defined(QTSCRIPTGENERATOR_BUILDLINK3_MK)
+QTSCRIPTGENERATOR_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qtscriptgenerator+=	qtscriptgenerator>=0.1.0
+BUILDLINK_ABI_DEPENDS.qtscriptgenerator?=	qtscriptgenerator>=0.1.0nb2
+BUILDLINK_PKGSRCDIR.qtscriptgenerator?=	../../devel/qtscriptgenerator
+
+.include "../../x11/qt4-libs/buildlink3.mk"
+.endif	# QTSCRIPTGENERATOR_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qtscriptgenerator
