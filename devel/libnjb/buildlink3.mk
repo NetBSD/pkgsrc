ea529bbd1a81111d6b0dfdbdbe513c85c09d279c
wiz <wiz@pkgsrc.org>
2010-02-15 16:51:13 +0000

    Remove wip reference.

--- devel/libnjb/buildlink3.mk
+++ devel/libnjb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/02/15 16:38:15 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/02/15 16:51:13 wiz Exp $
 
 BUILDLINK_TREE+=	libnjb
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libnjb
 LIBNJB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libnjb+=	libnjb>=2.2.6
-BUILDLINK_PKGSRCDIR.libnjb?=	../../wip/libnjb
+BUILDLINK_PKGSRCDIR.libnjb?=	../../devel/libnjb
 
 .include "../../devel/libusb/buildlink3.mk"
 .endif # LIBNJB_BUILDLINK3_MK
