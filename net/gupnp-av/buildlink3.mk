77c85524c97e05f40662513933ecdcfa1fcbde89
wiz <wiz@pkgsrc.org>
2010-02-16 15:29:50 +0000

    Update to 0.5.4:
    0.5.4
    =====
    
    Changes since 0.5.2:
    
    - Gracefully handle empty or no 'res' node.
    - Function to get the list of descriptors from DIDL-Lite objects.
    - More complete comparison for LPCM mime types.
    - Fix incorrect type conversion of DLNA flags.
    - DLNA flags should not default to a specific DLNA version.
    - New APIs to deal with contributor-related properties in DIDL-Lite objects.
    - Remove redundant construction methods.
    - Add forgotten header to the meta-header file.
    - Add and fix gobject-introspection annotations.
    - Lots of documentation fixes.
    - Many minor non-functional fixes/improvements.
    
    Bug fixes in this release:
    
    1935 - Incorrect conversion while parsing primary DLNA Flags
    1934 - DLNA Flags should not default to a specific DLNA version.
    1933 - Content type matching fails when additional parameters exist in LPCM
           mime-type
    1814 - Add "artists" property to GUPnPDIDLLiteObject
    
    All contributors to this release:
    
    Zeeshan Ali (Khattak) <zeeshanak@gnome.org>
    Yakup Akbay <yakbay@ubicom.com>
    Zachary Goldberg <zach@zachgoldberg.com>
    
    0.5.2
    =====
    
    Changes since 0.5.1:
    
    - Add a missing NULL check.
    - Fix a potential leak of xmlDoc.
    - Register a (g)type for GUPnPSearchCriteriaOp for better gtk-doc and
      vala-gen-introspect support.
    - Fix docs for GUPnPSearchCriteriaParser::expression.
    - Fix parsing of SearchCriteria strings: Closing parenthesis doesn't imply end
      of SearchCriteria expression.
    
    All contributors to this release:
    
    Zeeshan Ali (Khattak) <zeeshanak@gnome.org>
    
    0.5.1
    =====
    
    A micro release to fix a regression in DLNA profile guessing code in the
    previous (0.5) release.
    
    0.5
    ===
    
    Changes in this release:
    
    - New completely object-oriented, much simpler and consistent API:
      * GUPnPDIDLLiteResource & GUPnPDIDLLiteObject are now first-class GObjects.
      * Convert the helper methods to read props and their attributes from DIDL-Lite
        xml node into new first-class objects with writable properties:
        - GUPnPDIDLLiteObject
        - GUPnPDIDLLiteItem
        - GUPnPDIDLLiteContainer
      * GUPnPDIDLLiteObject provides a convenient method to get the compatible
        resource given a SinkProtocolInfo string.
      * A new class for dealing with protocolInfo fields: GUPnPProtocolInfo.
      * A new class for dealing with DIDL-Lite descriptors: GUPnPDIDLLiteDescriptor.
      * GUPnPDIDLLiteWriter now provides a much simpler API that is consistent with
        rest of the GUPnP AV API.
      * GUPnPDIDLLiteWriter now handles filtering of DIDL-Lite XML.
      * Correct possible values of GUPnPDLNAFlags.
      * Replace GUPnPDIDLLiteParserObjectCallback by following signals:
        - object-available
        - container-available
        - item-available
    - New Error domain for protocol related errors.
    - Workaround for broken printf() implementations.
    - Bitrate is in bytes/second and not bits/second.
    - Enable DLL on windows.
    
    Dependencies changed:
    
    - gupnp >= 0.13
    
    Bug fixes in this release:
    
    1729 - bitrate checks in gupnp-dlna.c look wrong.
    1579 - Minor fixes to enable dynamic library on windows
    
    All contributors to this release:
    
    Zeeshan Ali (Khattak) <zeeshanak@gnome.org>
    Sven Neumann <s.neumann@raumfeld.com>
    Jens Georg <mail@jensge.org>
    Ross Burton <ross@openedhand.com>
    
    WARNING: This release is API and ABI incompatible with previous releases.

--- net/gupnp-av/buildlink3.mk
+++ net/gupnp-av/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/01/20 14:04:53 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2010/02/16 15:29:50 wiz Exp $
 
 BUILDLINK_TREE+=	gupnp-av
 
 .if !defined(GUPNP_AV_BUILDLINK3_MK)
 GUPNP_AV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gupnp-av+=	gupnp-av>=0.3.1
-BUILDLINK_ABI_DEPENDS.gupnp-av?=	gupnp-av>=0.4.1nb1
+BUILDLINK_API_DEPENDS.gupnp-av+=	gupnp-av>=0.5.4
 BUILDLINK_PKGSRCDIR.gupnp-av?=	../../net/gupnp-av
 
 .include "../../net/gupnp/buildlink3.mk"
