489005d76de9ce82d52b4b12ee73a758fb642763
wiz <wiz@pkgsrc.org>
2010-02-16 13:35:40 +0000

    PKGREVISION bump for libxklavier shlib update.

--- x11/libgnomekbd/buildlink3.mk
+++ x11/libgnomekbd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:25:46 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2010/02/16 13:35:40 wiz Exp $
 
 BUILDLINK_TREE+=	libgnomekbd
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	libgnomekbd
 LIBGNOMEKBD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libgnomekbd+=	libgnomekbd>=2.18.0
+BUILDLINK_ABI_DEPENDS.libgnomekbd?=	libgnomekbd>=2.28.2nb1
 BUILDLINK_PKGSRCDIR.libgnomekbd?=	../../x11/libgnomekbd
 
 .include "../../x11/libxklavier/buildlink3.mk"
