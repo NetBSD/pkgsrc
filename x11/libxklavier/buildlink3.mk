3220b70e3e4a9b4440056fb7fc47b82a4c346f33
wiz <wiz@pkgsrc.org>
2010-02-16 14:54:41 +0000

    Revert to 4.0, on request by drochner, for gnome.

--- x11/libxklavier/buildlink3.mk
+++ x11/libxklavier/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2010/02/16 13:35:14 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2010/02/16 14:54:41 wiz Exp $
 
 BUILDLINK_TREE+=	libxklavier
 
@@ -6,7 +6,6 @@ BUILDLINK_TREE+=	libxklavier
 LIBXKLAVIER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libxklavier+=	libxklavier>=3.0
-BUILDLINK_ABI_DEPENDS.libxklavier?=	libxklavier>=5.0
 BUILDLINK_PKGSRCDIR.libxklavier?=	../../x11/libxklavier
 
 .include "../../textproc/libxml2/buildlink3.mk"
