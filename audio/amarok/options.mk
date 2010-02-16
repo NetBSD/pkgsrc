a730722c98e1e8ee119fc1feb0d07dd292c8b5f9
wiz <wiz@pkgsrc.org>
2010-02-16 09:01:46 +0000

    Update to 2.2.2nb3 from wip, packaged by myself.
    This is the KDE4 version of amarok, lots of changes. Try it out!

--- audio/amarok/options.mk
+++ audio/amarok/options.mk
@@ -1,30 +1,44 @@
-# $NetBSD: options.mk,v 1.6 2008/09/22 05:35:20 wiz Exp $
+# $NetBSD: options.mk,v 1.7 2010/02/16 09:01:46 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.amarok
-PKG_SUPPORTED_OPTIONS=	libgpod libmtp mysql pgsql
+PKG_SUPPORTED_OPTIONS=	libgpod libmp4v2 libmtp libvisual mp3tunes
+PKG_SUGGESTED_OPTIONS=	libgpod libmp4v2 libmtp libvisual mp3tunes
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	ipod libmtp
-
 .if !empty(PKG_OPTIONS:Mlibgpod)
-PLIST.ipod=		yes
-CONFIGURE_ARGS+=	--enable-libgpod
-BUILDLINK_API_DEPENDS.libgpod+=	libgpod>=0.4.2
-.  include "../../audio/libgpod/buildlink3.mk"
+BUILDLINK_API_DEPENDS.libgpod+=	libgpod>=0.6.0nb1
+.include "../../audio/libgpod/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif
+
+# libifp is currently not supported
+#.if !empty(PKG_OPTIONS:Mlibifp)
+#.include "../../audio/libifp/buildlink3.mk"
+#.endif
+
+.if !empty(PKG_OPTIONS:Mlibmp4v2)
+.include "../../multimedia/libmp4v2/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mlibmtp)
-PLIST.libmtp=		yes
-.  include "../../devel/libmtp/buildlink3.mk"
+.include "../../devel/libmtp/buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mmysql)
-CONFIGURE_ARGS+=	--enable-mysql
-.  include "../../mk/mysql.buildlink3.mk"
+# libnjb is currently not supported
+#.if !empty(PKG_OPTIONS:Mlibnjb)
+#.include "../../devel/libnjb/buildlink3.mk"
+#.endif
+
+.if !empty(PKG_OPTIONS:Mlibvisual)
+.include "../../audio/libvisual/buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mpgsql)
-CONFIGURE_ARGS+=	--enable-postgresql
-.  include "../../mk/pgsql.buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mmp3tunes)
+.include "../../chat/loudmouth/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
+# XXX: make sure qt4-libs have glib enabled
 .endif
