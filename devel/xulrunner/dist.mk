75e062f34f3f7dc499d092aa9b6d82d41930d474
spz <spz>
2010-02-17 22:24:32 +0000

    Pullup ticket 3009 - requested by tnn security and bug fixes
    Revisions pulled up:
    - pkgsrc/devel/nspr/Makefile		by patch to equiv of 1.34
    - pkgsrc/devel/xulrunner/dist.mk	by patch to equiv of 1.7
    - pkgsrc/devel/xulrunner/distinfo	by patch to equiv of 1.24
    - pkgsrc/www/firefox/Makefile		by patch to equiv of 1.70
    
       --------------------------------------------------------------------
       The attached diff against 2009Q4 updates firefox to 3.5.8.
       This is a security and bugfix update.

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,11 +1,11 @@
-# $NetBSD: dist.mk,v 1.3 2009/12/16 08:18:32 tnn Exp $
+# $NetBSD: dist.mk,v 1.3.2.1 2010/02/17 22:24:32 spz Exp $
 #
 # used by devel/nspr/Makefile
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	3.5.6
+FIREFOX_VER=	3.5.8
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
