9f10abba62fccac89eddeb78ef6ac287c79aa9a6
taca <taca@pkgsrc.org>
2010-02-18 15:46:10 +0000

    Update mysql5-client and mysql5-server package to version 5.0.90. This release many bug fixes and DoS security problem (CVE-2009-4484).
    Plese refer these URL in detail.
    
    http://dev.mysql.com/doc/refman/5.0/en/news-5-0-89.html
    http://dev.mysql.com/doc/refman/5.0/en/news-5-0-90.html
    
    There some minor pkgsrc change to prevent compile time warnings.

--- databases/mysql5-client/buildlink3.mk
+++ databases/mysql5-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2010/01/17 12:02:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2010/02/18 15:46:10 taca Exp $
 
 BUILDLINK_TREE+=	mysql-client
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	mysql-client
 MYSQL_CLIENT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mysql-client+=	mysql-client>=5.0.15
-BUILDLINK_ABI_DEPENDS.mysql-client+=	mysql-client>=5.0.88
+BUILDLINK_ABI_DEPENDS.mysql-client+=	mysql-client>=5.0.90
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql5-client
 BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
 BUILDLINK_INCDIRS.mysql-client?=	include/mysql
