14ed9cf988c049e5c01c1a19d8a5c6f5d250a199
schmonz <schmonz>
2010-02-19 08:33:28 +0000

    Make x11 support optional, default off. Bump PKGREVISION.

--- math/pari/options.mk
+++ math/pari/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1 2010/02/06 10:37:06 sno Exp $
+# $NetBSD: options.mk,v 1.2 2010/02/19 08:33:28 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pari
-PKG_SUPPORTED_OPTIONS=	gmp
+PKG_SUPPORTED_OPTIONS=	gmp x11
 PKG_SUGGESTED_OPTIONS=	gmp
 
 .include "../../mk/bsd.options.mk"
@@ -16,4 +16,17 @@ CONFIGURE_ARGS+=	--with-gmp=${PREFIX:Q}
 .include "../../devel/gmp/buildlink3.mk"
 .else
 PLIST.nogmp=		yes
+.endif
+
+.if !empty(PKG_OPTIONS:Mx11)
+CONFIGURE_ARGS+=	--graphic=X11
+CONFIGURE_ARGS+=	--with-fltk=${PREFIX:Q}
+CONFIGURE_ENV+=		X11BASE=${X11BASE:Q}
+CONFIGURE_ENV+=		Xincroot=${X11BASE}/include
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/fltk/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--graphic=none
+# don't let the configure script find an installed fltk
+CONFIGURE_ARGS+=	--with-fltk={BUILDLINK_DIR:Q}/nonexistent
 .endif
