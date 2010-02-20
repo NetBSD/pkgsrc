ba5680c342c084befa546d58d85c1c9d6107c2b7
joerg <joerg>
2010-02-20 03:48:25 +0000

    Import libarchive 2.8.0: - Infrastructure:   - Allow command line tools as fallback for missing compression     libraries. If compiled without gzip for example, gunzip will     be used automatically.   - Improved support for a number of platforms like high-resolution     timestamps and Extended Attributes on various Unix systems   - New convience interface for creating archives based on disk content,     complement of the archive_write_disk interface. - Frontends:   - bsdcpio ready for public consumption   - hand-written date parser replaces the yacc code - Filter system:   - Simplified read filter chains   - Option support for filters   - LZMA, XZ, uudecode handled - Format support:   - Write support for mtree files based on file system or archive     content   - Basic read support for Joliet   - Write support for zip files   - Write support for shar archives, both text-only and binary-safe

--- archivers/libarchive/files/doc/update.sh
+++ archivers/libarchive/files/doc/update.sh
@@ -5,43 +5,103 @@
 # the mdoc man pages stored in each project.
 #
 
-# Remove existing manpages from the doc tree
-chmod -R +w man text
-rm -f man/*.[135]
-rm -f text/*.[135]
+# Collect list of man pages, relative to my subdirs
+cd man
+MANPAGES=`for d in libarchive tar cpio;do ls ../../$d/*.[135];done | grep -v '\.so\.'`
+cd ..
 
 # Build Makefile in 'man' directory
 cd man
+rm -f *.[135]
 echo > Makefile
 echo "default: all" >>Makefile
 echo >>Makefile
 all="all:"
-for d in libarchive tar cpio; do
-    for f in ../../$d/*.[135]; do
-	echo >> Makefile
-	echo `basename $f`: ../mdoc2man.awk $f >> Makefile
-	echo "	awk -f ../mdoc2man.awk < $f > `basename $f`" >> Makefile
-        all="$all `basename $f`"
-    done
+for f in $MANPAGES; do
+    outname="`basename $f`"
+    echo >> Makefile
+    echo $outname: ../mdoc2man.awk $f >> Makefile
+    echo "	awk -f ../mdoc2man.awk < $f > $outname" >> Makefile
+    all="$all $outname"
 done
 echo $all >>Makefile
 cd ..
 
 # Rebuild Makefile in 'text' directory
 cd text
+rm -f *.txt
 echo > Makefile
-echo "$all" >>Makefile
+echo "default: all" >>Makefile
 echo >>Makefile
-for d in libarchive tar cpio; do
-    for f in ../../$d/*.[135]; do
-	echo >> Makefile
-	echo `basename $f`: $f >> Makefile
-	echo "	nroff -mdoc $f | col -b > `basename $f`" >> Makefile
-    done
+all="all:"
+for f in $MANPAGES; do
+    outname="`basename $f`.txt"
+    echo >> Makefile
+    echo $outname: $f >> Makefile
+    echo "	nroff -mdoc $f | col -b > $outname" >> Makefile
+    all="$all $outname"
 done
+echo $all >>Makefile
 cd ..
 
-# Convert all of the manpages to -man format.
+# Rebuild Makefile in 'pdf' directory
+cd pdf
+rm -f *.pdf
+echo > Makefile
+echo "default: all" >>Makefile
+echo >>Makefile
+all="all:"
+for f in $MANPAGES; do
+    outname="`basename $f`.pdf"
+    echo >> Makefile
+    echo $outname: $f >> Makefile
+    echo "	groff -mdoc -T ps $f | ps2pdf - - > $outname" >> Makefile
+    all="$all $outname"
+done
+echo $all >>Makefile
+cd ..
+
+# Build Makefile in 'html' directory
+cd html
+rm -f *.html
+echo > Makefile
+echo "default: all" >>Makefile
+echo >>Makefile
+all="all:"
+for f in $MANPAGES; do
+    outname="`basename $f`.html"
+    echo >> Makefile
+    echo $outname: ../mdoc2man.awk $f >> Makefile
+    echo "	groff -mdoc -T html $f > $outname" >> Makefile
+    all="$all $outname"
+done
+echo $all >>Makefile
+cd ..
+
+# Build Makefile in 'wiki' directory
+cd wiki
+rm -f *.wiki
+echo > Makefile
+echo "default: all" >>Makefile
+echo >>Makefile
+all="all:"
+for f in $MANPAGES; do
+    outname="`basename $f | awk '{ac=split($0,a,"[_.-]");o="ManPage";for(w=0;w<=ac;++w){o=o toupper(substr(a[w],1,1)) substr(a[w],2)};print o}'`.wiki"
+    echo >> Makefile
+    echo $outname: ../mdoc2wiki.awk $f >> Makefile
+    echo "	awk -f ../mdoc2wiki.awk < $f > $outname" >> Makefile
+    all="$all $outname"
+done
+echo $all >>Makefile
+cd ..
+
+# Convert all of the manpages to -man format
 (cd man && make)
 # Format all of the manpages to text
 (cd text && make)
+# Format all of the manpages to PDF
+(cd pdf && make)
+# Format all of the manpages to HTML
+(cd html && make)
+# Format all of the manpages to Google Wiki syntax
+(cd wiki && make)
