2d78d895074004434ed8c58ab4fb311ab11d0497
drochner <drochner>
2010-02-25 11:30:03 +0000

    add the qt4 version of the pinentry widget, from Sverre Froyen per PM

--- security/pinentry/options.mk
+++ security/pinentry/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2010/02/08 11:43:23 drochner Exp $
+# $NetBSD: options.mk,v 1.8 2010/02/25 11:30:03 drochner Exp $
 #
 
 # XXX This usage of bsd.options.mk is incorrect.  The package should
@@ -8,7 +8,7 @@
 #   pinentry-qt
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pinentry
-PKG_SUPPORTED_OPTIONS=	gtk gtk2 qt
+PKG_SUPPORTED_OPTIONS=	gtk gtk2 qt qt4
 .include "../../mk/bsd.options.mk"
 
 CONFIGURE_ARGS+=	--disable-pinentry-gtk
@@ -27,4 +27,10 @@ CONFIGURE_ARGS+=	--disable-pinentry-qt
 .if !empty(PKG_OPTIONS:Mqt)
 DEPENDS+=		pinentry-qt-[0-9]*:../../security/pinentry-qt
 PINENTRY_DEFAULT=	qt
+.endif
+
+CONFIGURE_ARGS+=	--disable-pinentry-qt4
+.if !empty(PKG_OPTIONS:Mqt4)
+DEPENDS+=		pinentry-qt4-[0-9]*:../../security/pinentry-qt4
+PINENTRY_DEFAULT=	qt4
 .endif
