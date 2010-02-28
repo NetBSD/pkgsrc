2aeec6f8ad74f22b436c5f62a0f3548328e59d0a
tonio <tonio@pkgsrc.org>
2010-02-28 20:49:21 +0000

    Add a gnome option, enabled by default. Fix PR pkg/42854, applying a similar patch.
    Remove the dependency on gtk-doc, it wasn't used.

--- net/libsoup24/buildlink3.mk
+++ net/libsoup24/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2010/02/25 17:24:34 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2010/02/28 20:49:21 tonio Exp $
 
 BUILDLINK_TREE+=	libsoup24
 
@@ -12,11 +12,18 @@ BUILDLINK_PKGSRCDIR.libsoup24?=		../../net/libsoup24
 
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../security/gnome-keyring/buildlink3.mk"
 .include "../../security/gnutls/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../www/libproxy/buildlink3.mk"
+
+pkgbase := libsoup24
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.libsoup24:Mgnome)
+.include "../../security/gnome-keyring/buildlink3.mk"
 .include "../../devel/GConf/buildlink3.mk"
+.endif
+
 .endif # LIBSOUP24_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libsoup24
