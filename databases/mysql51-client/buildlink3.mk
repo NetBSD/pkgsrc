b9625f1c42d33e594ec1cdc3aecc9b691b09cf7a
taca <taca@pkgsrc.org>
2010-03-04 16:27:18 +0000

    Importing mysql51-client 5.1.44, client part of MySQL 5.1.44.
    For release MySQL 5.1 specific information please refer:
    
    	http://dev.mysql.com/doc/refman/5.1/en/mysql-nutshell.html
    
    
    MySQL is a SQL (Structured Query Language) database server.  SQL is the
    most popular database language in the world.  MySQL is a client-server
    implementation that consists of a server daemon `mysqld' and many
    different client programs/libraries.
    
    The main goals of MySQL are speed and robustness.
    
    The base upon which MySQL is built is a set of routines that have been
    used in a highly demanding production environment for many years.  While
    MySQL is still in development it already offers a rich and highly useful
    function set.
    
    The official way to pronounce 'MySQL' is 'My Ess Que Ell' (Not MY-SEQUEL).
    
    This package contains the MySQL client programs and libraries.

---
+++ databases/mysql51-client/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/03/04 16:27:18 taca Exp $
+
+BUILDLINK_TREE+=	mysql-client
+
+.if !defined(MYSQL_CLIENT_BUILDLINK3_MK)
+MYSQL_CLIENT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.mysql-client+=	mysql-client>=5.1.44
+BUILDLINK_ABI_DEPENDS.mysql-client+=	mysql-client>=5.1.44
+BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql51-client
+BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
+BUILDLINK_INCDIRS.mysql-client?=	include/mysql
+
+.include "../../security/openssl/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.endif # MYSQL_CLIENT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-mysql-client
---
+++ databases/mysql51-client/options.mk
@@ -0,0 +1,28 @@
+# $NetBSD: options.mk,v 1.1.1.1 2010/03/04 16:27:18 taca Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.mysql5
+
+PKG_SUPPORTED_OPTIONS+=	embedded-server ssl
+#PKG_SUGGESTED_OPTIONS+=	embedded-server ssl
+PKG_SUGGESTED_OPTIONS+=	ssl
+
+.include "../../mk/bsd.options.mk"
+
+###
+### Enable OpenSSL support
+###
+.if !empty(PKG_OPTIONS:Mssl)
+.	include "../../security/openssl/buildlink3.mk"
+. if ${OPSYS} == "SunOS"
+CHECK_BUILTIN.openssl:=yes
+.  include "../../security/openssl/builtin.mk"
+CHECK_BUILTIN.openssl:=no
+.  if ${USE_BUILTIN.openssl} == "yes"
+CONFIGURE_ARGS+=	--with-ssl=yes
+.  endif
+. else
+CONFIGURE_ARGS+=	--with-ssl=yes
+. endif
+.else
+CONFIGURE_ARGS+=	--without-ssl
+.endif
