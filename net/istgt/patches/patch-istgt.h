25e49360d89a6dae44ef644a72d2c373110c9914
sborrill <sborrill@pkgsrc.org>
2010-03-07 10:51:02 +0000

    Fix default path to auth file. Observe PREFIX in istgtcontrol when looking for config file. Bump PKGREVISION

--- net/istgt/patches/patch-istgt.h
+++ net/istgt/patches/patch-istgt.h
@@ -1,8 +1,14 @@
-$NetBSD: patch-istgt.h,v 1.1.1.1 2010/02/02 16:09:51 sborrill Exp $
+$NetBSD: patch-istgt.h,v 1.2 2010/03/07 10:51:02 sborrill Exp $
 
---- src/istgt.h.orig	2010-02-02 13:49:40.000000000 +0000
-+++ src/istgt.h	2010-02-02 13:51:11.000000000 +0000
-@@ -55,7 +55,11 @@
+--- src/istgt.h.orig	2010-01-25 12:55:45.000000000 +0000
++++ src/istgt.h	2010-02-28 18:51:50.000000000 +0000
+@@ -50,12 +50,16 @@
+ 
+ #define DEFAULT_CONFIG "/usr/local/etc/istgt/istgt.conf"
+ #define DEFAULT_PIDFILE "/var/run/istgt.pid"
+-#define DEFAULT_AUTHFILE "/usr/local/etc/istgt/auths"
++#define DEFAULT_AUTHFILE "/usr/local/etc/istgt/auth.conf"
+ #define DEFAULT_MEDIAFILE "/usr/local/etc/istgt/media.conf"
  #define DEFAULT_LIVEFILE "/usr/local/etc/istgt/istgt.live"
  #define DEFAULT_MEDIADIRECTORY "/var/istgt"
  //#define DEFAULT_NODEBASE "iqn.2007-09.jp.ne.peach.istgt"
