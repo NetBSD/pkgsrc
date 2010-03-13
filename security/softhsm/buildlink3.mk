b1ee9cf8927075631ac1fcb92e0c67cacbea4106
pettai <pettai>
2010-03-13 17:51:26 +0000

    Fixed wip errors

--- security/softhsm/buildlink3.mk
+++ security/softhsm/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/03/13 17:47:56 pettai Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/03/13 17:51:26 pettai Exp $
 
 BUILDLINK_TREE+=	softhsm
 
 .if !defined(SOFTHSM_BUILDLINK3_MK)
 SOFTHSM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.softhsm+=	softhsm>=1.1.1
-BUILDLINK_PKGSRCDIR.softhsm?=	../../wip/softhsm
+BUILDLINK_API_DEPENDS.softhsm+=	softhsm>=1.1.3
+BUILDLINK_PKGSRCDIR.softhsm?=	../../security/softhsm
 
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../security/botan/buildlink3.mk"
