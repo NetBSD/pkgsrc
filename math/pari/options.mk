c1fa9c9941e94a8a04d3603cdfdb30ca82c14221
schmonz <schmonz@pkgsrc.org>
2010-03-13 22:16:34 +0000

    Add a "doc" option (on by default) to control whether the documentation is rendered and installed. Disabling the option is helpful e.g. for web servers supporting OpenID logins, where X and TeX aren't present. Bump PKGREVISION.

--- math/pari/options.mk
+++ math/pari/options.mk
@@ -1,24 +1,33 @@
-# $NetBSD: options.mk,v 1.3 2010/02/19 18:16:42 schmonz Exp $
+# $NetBSD: options.mk,v 1.4 2010/03/13 22:16:34 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pari
-PKG_SUPPORTED_OPTIONS=	gmp x11
-PKG_SUGGESTED_OPTIONS=	gmp x11
+PKG_SUPPORTED_OPTIONS=	doc gmp x11
+PKG_SUGGESTED_OPTIONS=	doc gmp x11
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		nogmp gmp
+PLIST_VARS+=		doc
+.if !empty(PKG_OPTIONS:Mdoc)
+PLIST.doc=		yes
+BUILD_TARGET+=		doc
+INSTALL_TARGET+=	install-doc
+BUILD_DEPENDS+=		dvipsk-[0-9]*:../../print/dvipsk
+TEX_ACCEPTED=		teTeX3
+TEX_DEPMETHOD=		build
+.include "../../mk/tex.buildlink3.mk"
+.endif
 
+PLIST_VARS+=		nogmp gmp
 .if !empty(PKG_OPTIONS:Mgmp)
 PLIST.gmp=		yes
-
 CONFIGURE_ARGS+=	--with-gmp=${PREFIX:Q}
-
 .include "../../devel/gmp/buildlink3.mk"
 .else
 PLIST.nogmp=		yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mx11)
+USE_TOOLS+=		imake
 CONFIGURE_ARGS+=	--graphic=X11
 CONFIGURE_ARGS+=	--with-fltk=${PREFIX:Q}
 CONFIGURE_ENV+=		X11BASE=${X11BASE:Q}
