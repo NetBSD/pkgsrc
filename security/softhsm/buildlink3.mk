30ae9b24fa2de62ed27fc413b84ff887ccfc6bd2
pettai <pettai@pkgsrc.org>
2010-03-13 17:47:56 +0000

    SoftHSM is an implementation of a cryptographic store accessible through a PKCS#11 interface. You can use it to explore PKCS#11 without having a Hardware Security Module. It is being developed as a part of the OpenDNSSEC project. SoftHSM uses Botan for its cryptographic operations.

---
+++ security/softhsm/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/03/13 17:47:56 pettai Exp $
+
+BUILDLINK_TREE+=	softhsm
+
+.if !defined(SOFTHSM_BUILDLINK3_MK)
+SOFTHSM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.softhsm+=	softhsm>=1.1.1
+BUILDLINK_PKGSRCDIR.softhsm?=	../../wip/softhsm
+
+.include "../../databases/sqlite3/buildlink3.mk"
+.include "../../security/botan/buildlink3.mk"
+.endif	# SOFTHSM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-softhsm
