d25781cb8e5e5d7b450d37ed120d2e0087c7b2db
wiz <wiz>
2010-03-14 16:32:15 +0000

    Fix path to proj after its reimport.

--- databases/postgresql-postgis/buildlink3.mk
+++ databases/postgresql-postgis/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/01/21 12:53:19 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/03/14 16:32:15 wiz Exp $
 
 BUILDLINK_TREE+=	postgresql-postgis
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.postgresql-postgis+=	postgresql${PGSQL_VERSION}-postgis>=1
 BUILDLINK_PKGSRCDIR.postgresql-postgis?=	../../databases/postgresql-postgis
 
 .include "../../geography/geos/buildlink3.mk"
-.include "../../misc/proj/buildlink3.mk"
+.include "../../geography/proj/buildlink3.mk"
 .include "../../mk/pgsql.buildlink3.mk"
 .endif	# POSTGRESQL_POSTGIS_BUILDLINK3_MK
 
