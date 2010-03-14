c2f16d9c08ccb4ad883358304040b5ab4c53491d
wiz <wiz@pkgsrc.org>
2010-03-14 16:32:15 +0000

    Fix path to proj after its reimport.

--- geography/gdal-lib/buildlink3.mk
+++ geography/gdal-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2010/01/26 08:26:11 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2010/03/14 16:32:15 wiz Exp $
 
 BUILDLINK_TREE+=	gdal-lib
 
@@ -22,11 +22,11 @@ pkgbase := gdal-lib
 
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../geography/geos/buildlink3.mk"
+.include "../../geography/proj/buildlink3.mk"
 .include "../../graphics/jasper/buildlink3.mk"
 .include "../../graphics/libungif/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
-.include "../../misc/proj/buildlink3.mk"
 .include "../../textproc/xerces-c/buildlink3.mk"
 .include "../../www/curl/buildlink3.mk"
 .endif # GDAL_LIB_BUILDLINK3_MK
