964db1d0b12ba745b1dbb179c3898d38ee9c5b8a
taca <taca@pkgsrc.org>
2010-03-15 16:34:20 +0000

    Update typolight28-translations package to 20100312.
    Czech, Danish, Italian, Russian and Slovenian language files are updated.

--- www/typolight28-translations/options.mk
+++ www/typolight28-translations/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.12 2010/03/03 15:41:48 taca Exp $
+# $NetBSD: options.mk,v 1.13 2010/03/15 16:34:20 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.typolight-translations
 
@@ -9,8 +9,8 @@ TL_TRANSLATIONS.af=	TYPOlight-Afrikaans-v1.zip	# 2008/01/15
 TL_TRANSLATIONS.bg=	TYPOlight-Bulgarian-v18.zip	# 2010/02/18
 TL_TRANSLATIONS.bs=	TYPOlight-Bosnian-v9.zip	# 2010/01/17
 TL_TRANSLATIONS.ca=	TYPOlight-Catalan-v4.zip	# 2008/09/24
-TL_TRANSLATIONS.cs=	TYPOlight-Czech-v10.zip		# 2010/03/02
-TL_TRANSLATIONS.da=	TYPOlight-Danish-v8.zip		# 2009/11/27
+TL_TRANSLATIONS.cs=	TYPOlight-Czech-v11.zip		# 2010/03/11
+TL_TRANSLATIONS.da=	TYPOlight-Danish-v9.zip		# 2010/03/22
 TL_TRANSLATIONS.es=	TYPOlight-Spanish-v12.zip	# 2009/07/01
 TL_TRANSLATIONS.fa=	TYPOlight-Persian-v2.zip	# 2008/06/05
 TL_TRANSLATIONS.fi=	TYPOlight-Finnish-v1.zip	# 2009/11/03
@@ -19,7 +19,7 @@ TL_TRANSLATIONS.hr=	TYPOlight-Croatian-v4.zip	# 2010/03/01
 TL_TRANSLATIONS.hu=	TYPOlight-Hungarian-v3.zip	# 2010/01/17
 TL_TRANSLATIONS.id=	TYPOlight-Indonesian-v1.zip	# 2009/01/13
 TL_TRANSLATIONS.is=	TYPOlight-Icelandic-v5.zip	# 2010/02/10
-TL_TRANSLATIONS.it=	TYPOlight-Italian-v21.zip	# 2010/03/01
+TL_TRANSLATIONS.it=	TYPOlight-Italian-v22.zip	# 2010/03/11
 TL_TRANSLATIONS.ja=	TYPOlight-Japanese-v27.zip	# 2010/03/01
 TL_TRANSLATIONS.lt=	TYPOlight-Lithuanian-v3.zip	# 2009/12/10
 TL_TRANSLATIONS.lv=	TYPOlight-Latvian-v23.zip	# 2010/02/04
@@ -28,9 +28,9 @@ TL_TRANSLATIONS.no=	TYPOlight-Norwegian-v11.zip	# 2009/11/16
 TL_TRANSLATIONS.pl=	TYPOlight-Polish-v27.zip	# 2010/02/19
 TL_TRANSLATIONS.pt=	TYPOlight-Portuguese-v7.zip	# 2009/11/01
 TL_TRANSLATIONS.ro=	TYPOlight-Romanian-v8.zip	# 2009/03/17
-TL_TRANSLATIONS.ru=	TYPOlight-Russian-v26.zip	# 2010/03/01
+TL_TRANSLATIONS.ru=	TYPOlight-Russian-v27.zip	# 2010/03/04
 TL_TRANSLATIONS.sk=	TYPOlight-Slovak-v8.zip		# 2009/08/15
-TL_TRANSLATIONS.sl=	TYPOlight-Slovenian-v5.zip	# 2009/06/20
+TL_TRANSLATIONS.sl=	TYPOlight-Slovenian-v6.zip	# 2010/03/12
 TL_TRANSLATIONS.sq=	TYPOlight-Albanian-v4.zip	# 2008/12/14
 TL_TRANSLATIONS.sr=	TYPOlight-Serbian-v6.zip	# 2009/06/09
 TL_TRANSLATIONS.sv=	TYPOlight-Swedish-v43.zip	# 2010/03/01
