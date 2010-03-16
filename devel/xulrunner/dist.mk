0445a343ccf0efb328195784548fd9e4882c1775
tnn <tnn>
2010-03-16 15:57:02 +0000

    Update to firefox-3.6.2. .2 is not formally released yet, but is release tagged in the scm and I want to get this update in before we freeze the tree.
    "Firefox 3.6 is built on Mozilla's Gecko 1.9.2 web rendering platform,
    which has been under development since early 2009 and contains many
    improvements for web developers, add-on developers, and users."
    
    - Improved JavaScript performance, overall browser responsiveness,
      and startup time.
    - The ability for web developers to indicate that scripts should run
      asynchronously to speed up page load times.
    - Continued support for downloadable web fonts using the new WOFF font format.
    - Support for new CSS attributes such as gradients, background sizing,
      and pointer events.
    - Support for new DOM and HTML5 specifications including the Drag & Drop API
      and the File API, which allow for more interactive web pages.

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.7 2010/02/17 18:32:18 tnn Exp $
+# $NetBSD: dist.mk,v 1.8 2010/03/16 15:57:02 tnn Exp $
 #
 # used by devel/nspr/Makefile
 # used by devel/nss/Makefile
@@ -6,9 +6,9 @@
 # used by www/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	3.5${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	1.9.1
-MOZ_BRANCH_MINOR=	.8
+FIREFOX_VER=	3.6${MOZ_BRANCH_MINOR}
+MOZ_BRANCH=	1.9.2
+MOZ_BRANCH_MINOR=	.2
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
@@ -16,3 +16,8 @@ DISTINFO_FILE=	${.CURDIR}/../../devel/xulrunner/distinfo
 PATCHDIR=	${.CURDIR}/../../devel/xulrunner/patches
 
 WRKSRC=		${WRKDIR}/mozilla-${MOZ_BRANCH}
+
+# snapshot overrides
+DISTNAME=	f14062c981ba
+MASTER_SITES=	http://hg.mozilla.org/releases/mozilla-1.9.2/archive/
+WRKSRC=		${WRKDIR}/mozilla-1-9-2-f14062c981ba
