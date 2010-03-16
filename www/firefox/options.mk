735cb2e08545191e68e6ae1327398e6b83ba029c
tnn <tnn@pkgsrc.org>
2010-03-16 15:57:02 +0000

    Update to firefox-3.6.2. .2 is not formally released yet, but is release tagged in the scm and I want to get this update in before we freeze the tree.
    "Firefox 3.6 is built on Mozilla's Gecko 1.9.2 web rendering platform,
    which has been under development since early 2009 and contains many
    improvements for web developers, add-on developers, and users."
    
    - Improved JavaScript performance, overall browser responsiveness,
      and startup time.
    - The ability for web developers to indicate that scripts should run
      asynchronously to speed up page load times.
    - Continued support for downloadable web fonts using the new WOFF font format.
    - Support for new CSS attributes such as gradients, background sizing,
      and pointer events.
    - Support for new DOM and HTML5 specifications including the Drag & Drop API
      and the File API, which allow for more interactive web pages.

--- www/firefox/options.mk
+++ www/firefox/options.mk
@@ -1,15 +1,12 @@
-# $NetBSD: options.mk,v 1.8 2010/02/17 18:32:18 tnn Exp $
+# $NetBSD: options.mk,v 1.9 2010/03/16 15:57:03 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.firefox
 PKG_SUPPORTED_OPTIONS=	official-mozilla-branding
-
-PLIST_VARS+=		branding
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mofficial-mozilla-branding)
 CONFIGURE_ARGS+=	--enable-official-branding
-PLIST.branding=		yes
 LICENSE=		mozilla-trademark-license
 RESTRICTED=		Trademark holder prohibits distribution of modified versions.
 NO_BIN_ON_CDROM=	${RESTRICTED}
