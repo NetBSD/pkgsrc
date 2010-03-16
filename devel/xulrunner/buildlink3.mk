2140f8f35c7573554036389fdcbf902ef0aba1dc
tnn <tnn@pkgsrc.org>
2010-03-16 15:57:02 +0000

    Update to firefox-3.6.2. .2 is not formally released yet, but is release tagged in the scm and I want to get this update in before we freeze the tree.
    "Firefox 3.6 is built on Mozilla's Gecko 1.9.2 web rendering platform,
    which has been under development since early 2009 and contains many
    improvements for web developers, add-on developers, and users."
    
    - Improved JavaScript performance, overall browser responsiveness,
      and startup time.
    - The ability for web developers to indicate that scripts should run
      asynchronously to speed up page load times.
    - Continued support for downloadable web fonts using the new WOFF font format.
    - Support for new CSS attributes such as gradients, background sizing,
      and pointer events.
    - Support for new DOM and HTML5 specifications including the Drag & Drop API
      and the File API, which allow for more interactive web pages.

--- devel/xulrunner/buildlink3.mk
+++ devel/xulrunner/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.6 2010/01/18 09:58:47 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2010/03/16 15:57:02 tnn Exp $
 
 BUILDLINK_TREE+=	xulrunner
 
 .if !defined(XULRUNNER_BUILDLINK3_MK)
 XULRUNNER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=1.9.1nb4
-BUILDLINK_ABI_DEPENDS.xulrunner?=	xulrunner>=1.9.1.6
-BUILDLINK_PKGSRCDIR.xulrunner?=	../../devel/xulrunner
+BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=1.9.2.2
+BUILDLINK_ABI_DEPENDS.xulrunner?=	xulrunner>=1.9.2.2
+BUILDLINK_PKGSRCDIR.xulrunner?=		../../devel/xulrunner
 
 pkgbase := xulrunner
 .include "../../mk/pkg-build-options.mk"
