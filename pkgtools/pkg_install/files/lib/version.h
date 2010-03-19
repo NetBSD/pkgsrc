b4534959f02875729528433fa411ab6c2c00b2da
wiz <wiz@pkgsrc.org>
2010-03-19 12:49:53 +0000

    Add man pages for audit-packages and download-vulnerability-list which point to pkg_admin and notes that the scripts are obsolete.
    Bump version to 20100319.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.151 2010/02/20 04:40:03 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.152 2010/03/19 12:49:53 wiz Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION 20100220
+#define PKGTOOLS_VERSION 20100319
 
 #endif /* _INST_LIB_VERSION_H_ */
