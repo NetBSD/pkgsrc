393ae5425e94c40ff12b69cbe7fe64e110cd4f1c
wiz <wiz@pkgsrc.org>
2010-03-20 12:43:29 +0000

    Remove evolution-data-server dependency from totem-pl-parser buildlink3.mk, depend on version that removed dependency and bump PKGREVISION for dependencies.

--- multimedia/totem-pl-parser/buildlink3.mk
+++ multimedia/totem-pl-parser/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2009/10/21 16:12:03 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2010/03/20 12:43:29 wiz Exp $
 
 BUILDLINK_TREE+=	totem-pl-parser
 
@@ -6,13 +6,12 @@ BUILDLINK_TREE+=	totem-pl-parser
 TOTEM_PL_PARSER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.totem-pl-parser+=	totem-pl-parser>=2.22.1
-BUILDLINK_ABI_DEPENDS.totem-pl-parser?=	totem-pl-parser>=2.26.2nb1
+BUILDLINK_ABI_DEPENDS.totem-pl-parser?=	totem-pl-parser>=2.28.2nb1
 BUILDLINK_PKGSRCDIR.totem-pl-parser?=	../../multimedia/totem-pl-parser
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
-.include "../../mail/evolution-data-server/buildlink3.mk"
 .include "../../mail/gmime24/buildlink3.mk"
 .endif # TOTEM_PL_PARSER_BUILDLINK3_MK
 
