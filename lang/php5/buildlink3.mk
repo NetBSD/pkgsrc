ecb78d9fd915a7fb57e60efbbd04918a780ffa5c
joerg <joerg@pkgsrc.org>
2010-03-20 15:25:50 +0000

    Limit packages to PHP80xa before 5.3

--- lang/php5/buildlink3.mk
+++ lang/php5/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.14 2009/03/20 19:24:51 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2010/03/20 15:25:50 joerg Exp $
 
 BUILDLINK_TREE+=	php
 
 .if !defined(PHP_BUILDLINK3_MK)
 PHP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.php+=		php>=5.1.2
+BUILDLINK_API_DEPENDS.php+=		php>=5.1.2<5.3
 BUILDLINK_ABI_DEPENDS.php+=	php>=5.1.2
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php5
 
