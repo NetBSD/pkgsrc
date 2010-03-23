e13332e0de70aa3a7488e8d52f7e83b3a411d040
reed <reed@pkgsrc.org>
2010-03-23 20:42:55 +0000

    Fix build by increasing BUILDLINK_API_DEPENDS.pangomm. Error was:
    checking for GDKMM... configure: error: Package requirements (giomm-2.4 >= 2.22 pangomm-1.4 >= 2.26 gtk+-2.0 >= 2.18) were not met:
    
    Requested 'pangomm-1.4 >= 2.26' but version of Pangomm is 2.24.0
    
    (2.24.0 doesn't provide needed pangommconfig.h.)
    
    While here remove now-irrelevant comment from buildlink3.mk.

--- x11/gtkmm/buildlink3.mk
+++ x11/gtkmm/buildlink3.mk
@@ -1,11 +1,10 @@
-# $NetBSD: buildlink3.mk,v 1.18 2009/10/22 08:22:02 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2010/03/23 20:42:55 reed Exp $
 
 BUILDLINK_TREE+=	gtkmm
 
 .if !defined(GTKMM_BUILDLINK3_MK)
 GTKMM_BUILDLINK3_MK:=
 
-# increase API_DEPENDS as a hack because now depends on pangomm
 BUILDLINK_API_DEPENDS.gtkmm+=	gtkmm>=2.18.2
 BUILDLINK_ABI_DEPENDS.gtkmm?=	gtkmm>=2.18.2
 BUILDLINK_PKGSRCDIR.gtkmm?=	../../x11/gtkmm
