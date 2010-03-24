4f77736783db76ee89fcf02f34e554d38b78707d
asau <asau>
2010-03-24 21:54:15 +0000

    Recursive revision bump for GMP update, 2nd part.

--- devel/g-wrap/buildlink3.mk
+++ devel/g-wrap/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2009/12/15 21:40:00 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2010/03/24 21:54:16 asau Exp $
 
 BUILDLINK_TREE+=	g-wrap
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	g-wrap
 G_WRAP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.g-wrap+=	g-wrap>=1.9.5
-BUILDLINK_ABI_DEPENDS.g-wrap?=	g-wrap>=1.9.12nb2
+BUILDLINK_ABI_DEPENDS.g-wrap?=	g-wrap>=1.9.12nb3
 BUILDLINK_PKGSRCDIR.g-wrap?=	../../devel/g-wrap
 
 .include "../../devel/libffi/buildlink3.mk"
