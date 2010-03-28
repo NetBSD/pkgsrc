ffef2196d71abbcdb12536f82a0d95782ba1f03a
drochner <drochner@pkgsrc.org>
2010-03-28 12:45:44 +0000

    propagate dependencies on the libs we link against, fixes build issue with graphics/h5utils (did show up after archivers/szip is not used by hdf5 anymore)

--- devel/hdf/buildlink3.mk
+++ devel/hdf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:24:15 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2010/03/28 12:45:44 drochner Exp $
 
 BUILDLINK_TREE+=	hdf
 
@@ -9,5 +9,9 @@ BUILDLINK_API_DEPENDS.hdf+=	hdf>=4.1r5
 BUILDLINK_ABI_DEPENDS.hdf?=	hdf>=4.2r1nb1
 BUILDLINK_PKGSRCDIR.hdf?=	../../devel/hdf
 .endif # HDF_BUILDLINK3_MK
+
+.include "../../archivers/szip/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
 
 BUILDLINK_TREE+=	-hdf
