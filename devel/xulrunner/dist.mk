16883a4bb25174405fdf42f3df76ff1cd867cb10
tnn <tnn@pkgsrc.org>
2010-04-02 20:28:23 +0000

    firefox-3.6.3 fixes a use-after-free bug which could be exploited to run arbitrary code.

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.9 2010/03/23 12:43:38 tnn Exp $
+# $NetBSD: dist.mk,v 1.10 2010/04/02 20:28:24 tnn Exp $
 #
 # used by devel/nspr/Makefile
 # used by devel/nss/Makefile
@@ -8,7 +8,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	3.6${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	1.9.2
-MOZ_BRANCH_MINOR=	.2
+MOZ_BRANCH_MINOR=	.3
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
