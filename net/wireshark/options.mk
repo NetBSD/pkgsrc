001370ed8dfe0ef6f20b4cce723252769b24bbcc
adam <adam@pkgsrc.org>
2010-04-08 10:03:31 +0000

    Changed option name from 'x11' to 'gtk2', as GTK2 can be built without X11 support (e.g. on Mac OS X). Tested - works on Mac OS X without X11.

--- net/wireshark/options.mk
+++ net/wireshark/options.mk
@@ -1,17 +1,17 @@
-# $NetBSD: options.mk,v 1.2 2008/04/12 22:43:09 jlam Exp $
+# $NetBSD: options.mk,v 1.3 2010/04/08 10:03:31 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.wireshark
-PKG_SUPPORTED_OPTIONS=	x11
-PKG_SUGGESTED_OPTIONS=	x11
+PKG_SUPPORTED_OPTIONS=	gtk2
+PKG_SUGGESTED_OPTIONS=	gtk2
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		x11
+PLIST_VARS+=		gtk2
 
-.if !empty(PKG_OPTIONS:Mx11)
+.if !empty(PKG_OPTIONS:Mgtk2)
 CONFIGURE_ARGS+=	--enable-gtk2
 CONFIGURE_ARGS+=	--with-gtk-prefix=${BUILDLINK_PREFIX.gtk2}
-PLIST.x11=		yes
+PLIST.gtk2=		yes
 .include "../../x11/gtk2/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-wireshark
