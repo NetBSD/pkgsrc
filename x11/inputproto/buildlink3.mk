9825f9979fef368128eec422cd32eb7b8347255f
tnn <tnn@pkgsrc.org>
2010-04-09 08:21:48 +0000

    Update to inputproto-2.0.
    "The first stable release of the X Input Extension Version 2.0 protocol
    specification and header files."
    
    XI2 provides a number of enhancements over version 1.5, including:
    - use of XGE and GenericEvents. GenericEvents are of flexible length with a
      minimum length of 32 bytes.
    - explicit device hierarchy of master and slave devices. See Section 4.
    - use of multiple independent master devices (Multi-Poiner X or MPX).
    - the ability for devices to change capabilities at runtime.
    - raw device events

--- x11/inputproto/buildlink3.mk
+++ x11/inputproto/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:25:42 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2010/04/09 08:21:48 tnn Exp $
 
 BUILDLINK_TREE+=	inputproto
 
 .if !defined(INPUTPROTO_BUILDLINK3_MK)
 INPUTPROTO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.inputproto+=	inputproto>=1.4
+BUILDLINK_API_DEPENDS.inputproto+=	inputproto>=2.0
 BUILDLINK_PKGSRCDIR.inputproto?=	../../x11/inputproto
 BUILDLINK_DEPMETHOD.inputproto?=	build
 .endif # INPUTPROTO_BUILDLINK3_MK
