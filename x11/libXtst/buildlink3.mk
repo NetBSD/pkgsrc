2c961fa658017b017dc9f6db87c7b5f27d66eccd
tnn <tnn>
2010-04-09 08:41:23 +0000

    Update to libXtst-1.1.0.
    1.1.0:
    Just two changes, increasing the requires for libXext to what it really is
    and the usual xorg-macros change.
    
    1.0.99.2:
    Xlib headers from the recordproto module have moved to here.
    The new requirement is thus recordproto 1.13.99.2
    No other interesting changes.
    
    1.0.99.1:
    The first libXtst release for nearly two years and boy, aren't we glad it's
    here. 1.0.99.1 contains various build system fixes and a new header file
    that previouly resided in the xextproto module.
    This version requires xextproto 7.0.99.3 or later to avoid conflicts with
    header files.

--- x11/libXtst/buildlink3.mk
+++ x11/libXtst/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:25:46 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2010/04/09 08:41:23 tnn Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -7,12 +7,13 @@ BUILDLINK_TREE+=	libXtst
 .if !defined(LIBXTST_BUILDLINK3_MK)
 LIBXTST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libXtst+=	libXtst>=1.0.1
+BUILDLINK_API_DEPENDS.libXtst+=	libXtst>=1.1.0
 BUILDLINK_PKGSRCDIR.libXtst?=	../../x11/libXtst
 
 .include "../../x11/recordproto/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXi/buildlink3.mk"
 .endif # LIBXTST_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libXtst
