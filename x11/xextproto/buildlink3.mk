ee34f143e6adc66399af94156a445db17a84c660
tnn <tnn@pkgsrc.org>
2010-04-10 21:09:20 +0000

    revert bl3.mk changes since there were problems with native X11.

--- x11/xextproto/buildlink3.mk
+++ x11/xextproto/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2010/04/09 08:33:38 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2010/04/10 21:09:21 tnn Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -9,7 +9,7 @@ BUILDLINK_TREE+=	xextproto
 .if !defined(XEXTPROTO_BUILDLINK3_MK)
 XEXTPROTO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xextproto+=	xextproto>=7.1.1
+BUILDLINK_API_DEPENDS.xextproto+=	xextproto>=7.0
 BUILDLINK_PKGSRCDIR.xextproto?=	../../x11/xextproto
 
 .include "../../x11/inputproto/buildlink3.mk"
