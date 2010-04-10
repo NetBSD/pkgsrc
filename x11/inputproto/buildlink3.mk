260161ae598aef42846ff31740113bd437f546e4
tnn <tnn>
2010-04-10 21:09:20 +0000

    revert bl3.mk changes since there were problems with native X11.

--- x11/inputproto/buildlink3.mk
+++ x11/inputproto/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2010/04/09 08:21:48 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2010/04/10 21:09:20 tnn Exp $
 
 BUILDLINK_TREE+=	inputproto
 
 .if !defined(INPUTPROTO_BUILDLINK3_MK)
 INPUTPROTO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.inputproto+=	inputproto>=2.0
+BUILDLINK_API_DEPENDS.inputproto+=	inputproto>=1.4
 BUILDLINK_PKGSRCDIR.inputproto?=	../../x11/inputproto
 BUILDLINK_DEPMETHOD.inputproto?=	build
 .endif # INPUTPROTO_BUILDLINK3_MK
