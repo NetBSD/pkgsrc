be29beb327155dedd7d48da5138de07287f117fc
seb <seb@pkgsrc.org>
2010-04-10 14:19:38 +0000

    Initial import of p5-B-Hooks-OP-Annotation version 0.43 in the NetBSD Packages Collection.
    The Perl 5 module B::Hooks::OP::Annotation provides a way for XS
    code that hijacks OP op_ppaddr functions to delegate to (or restore)
    the previous functions, whether assigned by perl or by another
    module. Typically this should be used in conjunction with
    B::Hooks::OP::Check.

---
+++ devel/p5-B-Hooks-OP-Annotation/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/04/10 14:19:38 seb Exp $
+
+BUILDLINK_TREE+=	p5-B-Hooks-OP-Annotation
+
+.if !defined(P5_B_HOOKS_OP_ANNOTATION_BUILDLINK3_MK)
+P5_B_HOOKS_OP_ANNOTATION_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.p5-B-Hooks-OP-Annotation+=	p5-B-Hooks-OP-Annotation>=0.43
+BUILDLINK_PKGSRCDIR.p5-B-Hooks-OP-Annotation?=	../../devel/p5-B-Hooks-OP-Annotation
+BUILDLINK_INCDIRS.p5-B-Hooks-OP-Annotation?=	${PERL5_SUB_INSTALLVENDORARCH}/B/Hooks/OP/Annotation
+BUILDLINK_CONTENTS_FILTER.p5-B-Hooks-OP-Annotation?=	${GREP} '${PERL5_SUB_INSTALLVENDORARCH}/B/Hooks/OP/Annotation'
+
+.include "../../lang/perl5/buildlink3.mk"
+.endif # P5_B_HOOKS_OP_ANNOTATION_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-p5-B-Hooks-OP-Annotation
