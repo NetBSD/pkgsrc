20c31ec7be9ffd307d6409fb169f31a9ce7ba452
sborrill <sborrill>
2010-04-15 09:36:50 +0000

    winbindd does not have a -B flag. As far as I can see, it never did have (It was added to this rc.d script as part of the upgrade to 3.0.20b, but winbindd in 3.0.20b does not have a -B flag).

--- net/samba/files/winbindd.sh
+++ net/samba/files/winbindd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: winbindd.sh,v 1.4 2006/05/03 23:23:06 jlam Exp $
+# $NetBSD: winbindd.sh,v 1.5 2010/04/15 09:36:50 sborrill Exp $
 #
 # PROVIDE: winbindd
 # REQUIRE: DAEMON
@@ -12,7 +12,6 @@ rcvar=$name
 command="@PREFIX@/sbin/${name}"
 required_files="@SAMBA_ETCDIR@/smb.conf"
 extra_commands="reload"
-command_args="-B"
 
 load_rc_config $name
 run_rc_command "$1"
