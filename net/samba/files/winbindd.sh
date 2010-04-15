4f23f9e17ef10a112f8f75f5e911660631e21402
tron <tron@pkgsrc.org>
2010-04-15 18:07:23 +0000

    Pullup ticket #3078 - requested by sborrill: net/samba: build fix and bug fix net/samba33: build fix and bug fix
    Revisions pulled up:
    - net/samba/Makefile				1.199-1.200
    - net/samba/files/winbindd.sh			1.5
    - net/samba33/Makefile				1.8-1.9
    ---
    Module Name:    pkgsrc
    Committed By:   sborrill
    Date:           Wed Apr 14 14:51:01 UTC 2010
    
    Modified Files:
             pkgsrc/net/samba: Makefile
             pkgsrc/net/samba33: Makefile
    
    Log Message:
    Add openssl bl3.mk file so that we play nicely with PREFER_PKGSRC+=openssl
    ---
    Module Name:    pkgsrc
    Committed By:   sborrill
    Date:           Thu Apr 15 09:36:50 UTC 2010
    
    Modified Files:
             pkgsrc/net/samba/files: winbindd.sh
    
    Log Message:
    winbindd does not have a -B flag. As far as I can see, it never did have
    (It was added to this rc.d script as part of the upgrade to 3.0.20b, but
    winbindd in 3.0.20b does not have a -B flag).
    ---
    Module Name:    pkgsrc
    Committed By:   sborrill
    Date:           Thu Apr 15 09:38:09 UTC 2010
    
    Modified Files:
             pkgsrc/net/samba: Makefile
             pkgsrc/net/samba33: Makefile
    
    Log Message:
    Bump PKGREVISION due to removing -B flag from winbindd rc.d script

--- net/samba/files/winbindd.sh
+++ net/samba/files/winbindd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: winbindd.sh,v 1.4 2006/05/03 23:23:06 jlam Exp $
+# $NetBSD: winbindd.sh,v 1.4.36.1 2010/04/15 18:07:23 tron Exp $
 #
 # PROVIDE: winbindd
 # REQUIRE: DAEMON
@@ -12,7 +12,6 @@ rcvar=$name
 command="@PREFIX@/sbin/${name}"
 required_files="@SAMBA_ETCDIR@/smb.conf"
 extra_commands="reload"
-command_args="-B"
 
 load_rc_config $name
 run_rc_command "$1"
