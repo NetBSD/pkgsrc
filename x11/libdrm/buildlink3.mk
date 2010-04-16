6549de9138670747bce60796ec6539b2e1ea28f9
tnn <tnn@pkgsrc.org>
2010-04-16 13:43:36 +0000

    Update to libdrm-2.4.20. Many changes since 2.4.13, I won't pretend to understand them all. This update is a prerequisite for eventually moving forward with MesaLib and xorg-server updates.

--- x11/libdrm/buildlink3.mk
+++ x11/libdrm/buildlink3.mk
@@ -1,19 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:25:46 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2010/04/16 13:43:36 tnn Exp $
 
 BUILDLINK_TREE+=	libdrm
 
 .if !defined(LIBDRM_BUILDLINK3_MK)
 LIBDRM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libdrm+=	libdrm>=2.3.0
-###
-### XXX Explicitly required by (at least) Mesa 7.1, which is nearing release.
-###	Upon updating pkgsrc Mesa to 7.1 and/orr xorg-server to a version which
-###	requires libdrm-2.3.1, BUILDLINK_API_DEPENDS should be increased
-###	and a recursive dependency bump performed.
-###	[bjs 20080714]
-###
-#BUILDLINK_API_DEPENDS.libdrm+=	libdrm>=2.3.1
+BUILDLINK_API_DEPENDS.libdrm+=	libdrm>=2.4.20
 BUILDLINK_PKGSRCDIR.libdrm?=	../../x11/libdrm
 .endif # LIBDRM_BUILDLINK3_MK
 
