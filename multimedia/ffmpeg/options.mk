9a665730d93f746072fe7d9a59b44f039d5d3492
reed <reed@pkgsrc.org>
2010-04-16 15:49:12 +0000

    Add opencore-arm option. Not enabled by default for ffmpeg.
    It worked for me to play a "Adaptive Multi-Rate Codec (GSM telephony)" file
    using ffplay.

--- multimedia/ffmpeg/options.mk
+++ multimedia/ffmpeg/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.14 2009/08/05 11:22:17 ahoka Exp $
+# $NetBSD: options.mk,v 1.15 2010/04/16 15:49:12 reed Exp $
 
 # Global and legacy options
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ffmpeg
-PKG_SUPPORTED_OPTIONS=	theora xvid faad faac x264
+PKG_SUPPORTED_OPTIONS=	theora xvid faad faac x264 opencore-amr
 PKG_SUGGESTED_OPTIONS=	theora xvid x264
 #PKG_OPTIONS_OPTIONAL_GROUPS=	aac-decoder
 #PKG_OPTIONS_GROUP.aac-decoder=	faad faac
@@ -30,6 +30,26 @@ NO_BIN_ON_FTP=		${RESTRICTED}
 CONFIGURE_ARGS+=	--enable-libfaac \
 			--enable-nonfree
 .include "../../audio/faac/buildlink3.mk"
+.endif
+
+###
+### opencore-amr option
+###
+
+.if !empty(PKG_OPTIONS:Mopencore-amr)
+CONFIGURE_ARGS+=  --enable-libopencore-amrnb --enable-libopencore-amrwb
+
+# "The OpenCORE external libraries are under the Apache License
+# 2.0. That license is incompatible with the LGPL v2.1 and the GPL
+# v2, but not with version 3 of those licenses. So to combine the
+# OpenCORE libraries with FFmpeg, the license version needs to be
+# upgraded by passing --enable-version3 to configure."
+CONFIGURE_ARGS+=	--enable-version3
+# TODO: LICENSE
+
+.include "../../audio/opencore-amr/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=  --disable-libopencore-amrnb --disable-libopencore-amrwb
 .endif
 
 ###
