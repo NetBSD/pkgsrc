c9c50a7ee9c12a5ebfa16357910ae4548a24b79d
matthias <matthias@pkgsrc.org>
2010-04-27 14:24:14 +0000

    add option x11 to make it possible to build without x11.

--- geography/mapserver/options.mk
+++ geography/mapserver/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2010/01/27 08:12:19 adam Exp $
+# $NetBSD: options.mk,v 1.3 2010/04/27 14:24:14 matthias Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mapserver
-PKG_SUPPORTED_OPTIONS=	fastcgi pgsql mysql
-PKG_SUGGESTED_OPTIONS=	# empty
+PKG_SUPPORTED_OPTIONS=	fastcgi pgsql mysql x11
+PKG_SUGGESTED_OPTIONS=	x11
 
 .include "../../mk/bsd.options.mk"
 
@@ -28,4 +28,13 @@ CONFIGURE_ARGS+=	--with-postgis
 .if !empty(PKG_OPTIONS:Mmysql)
 .include "../../databases/mysql5-client/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-mygis
+.endif
+
+#
+# x11 (xpm) support
+#
+.if !empty(PKG_OPTIONS:Mx11)
+.include "../../x11/libXpm/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-xpm
 .endif
