eb0243bf775e8d9281b03f8efe23f065a370cbf3
wiz <wiz>
2010-04-30 10:17:46 +0000

    Initial import of gtkmm-utils-0.4.1:
    Utility functions, classes and widgets written on top of gtkmm and
    glibmm.
    
     - logging framework
     - option parsing, date and string utilities
     - tile widgets
     - dialog helpers

---
+++ x11/gtkmm-utils/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/04/30 10:17:46 wiz Exp $
+
+BUILDLINK_TREE+=	gtkmm-utils
+
+.if !defined(GTKMM_UTILS_BUILDLINK3_MK)
+GTKMM_UTILS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gtkmm-utils+=	gtkmm-utils>=0.4.1
+BUILDLINK_PKGSRCDIR.gtkmm-utils?=	../../nih/gtkmm-utils
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/glibmm/buildlink3.mk"
+.include "../../devel/libsigc++/buildlink3.mk"
+.include "../../x11/gtkmm/buildlink3.mk"
+.endif # GTKMM_UTILS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gtkmm-utils
