dfed39b5fe66c02de6a11b0055e550a19bd7b644
tonnerre <tonnerre>
2010-05-01 15:09:44 +0000

    Import libcassandra version 20100501.
    libcassandra is a C++ client API for Apache Cassandra. The standard
    front-end (in the sense of "language binding") for writing C++ programs
    that use Cassandra.

---
+++ databases/libcassandra/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/05/01 15:09:44 tonnerre Exp $
+
+BUILDLINK_TREE+=	libcassandra
+
+.if !defined(LIBCASSANDRA_BUILDLINK3_MK)
+LIBCASSANDRA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libcassandra+=	libcassandra>=20100501
+BUILDLINK_ABI_DEPENDS.libcassandra?=	libcassandra>=20100501
+BUILDLINK_PKGSRCDIR.libcassandra?=	../../databases/libcassandra
+.endif # LIBCASSANDRA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libcassandra
