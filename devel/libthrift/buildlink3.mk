77ca14dede2041f9d229e9ef5a0005a2168dbbd0
tonnerre <tonnerre>
2010-05-01 02:49:10 +0000

    Import libthrift version 0.2.0.
    Thrift is a software framework for scalable cross-language services
    development. It combines a software stack with a code generation engine to
    build services that work efficiently and seamlessly between C++, Java,
    Python, PHP, Ruby, Erlang, Perl, Haskell, C#, Cocoa, Smalltalk, and OCaml.

---
+++ devel/libthrift/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/05/01 02:49:10 tonnerre Exp $
+
+BUILDLINK_TREE+=	libthrift
+
+.if !defined(LIBTHRIFT_BUILDLINK3_MK)
+LIBTHRIFT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libthrift+=	libthrift>=0.2.0
+BUILDLINK_ABI_DEPENDS.libthrift+=	libthrift>=0.2.0
+BUILDLINK_PKGSRCDIR.libthrift?=		../../devel/libthrift
+
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../devel/libevent/buildlink3.mk"
+
+.endif # LIBTHRIFT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libthrift
