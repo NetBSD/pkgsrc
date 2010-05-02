d22c835f948abfbf41e9c65e72d53b925d728c84
wiz <wiz@pkgsrc.org>
2010-05-02 20:26:13 +0000

    Revert v1.3.
    It broke expat detection/usage on NetBSD with native X.
    See thread "intltool not building (expat problem)" on tech-pkg,
    started on May 1.

--- mk/buildlink3/find-files.mk
+++ mk/buildlink3/find-files.mk
@@ -1,4 +1,4 @@
-# $NetBSD: find-files.mk,v 1.3 2010/04/28 08:02:18 obache Exp $
+# $NetBSD: find-files.mk,v 1.4 2010/05/02 20:26:13 wiz Exp $
 #
 # Copyright (c) 2005 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -83,30 +83,17 @@ USE_TOOLS+=	grep
 .  if !defined(${_var_})
 ${_var_}=	__nonexistent__
 .    for _file_ in ${BUILTIN_FIND_FILES.${_var_}}
-.      if !empty(${_var_}:M__nonexistent__)
-_real_file_=	__noexistent__
-.       if exists(${_file_})
-_real_file_=	${_file_}
-.       endif
-.       if !empty(_real_file_:M__noexistent__) && defined(BEINCLUDES) && !empty(BEINCLUDES)
-.        for _try_file_ in ${BEINCLUDES:S/;/ /g:=${_file_:S/\/usr\/include\//\//g}}
-.          if !empty(_real_file_:M__noexistent__) && exists(${_try_file_})
-_real_file_=	${_try_file_}
-.          endif
-.        endfor
-.       endif
-.       if empty(_real_file_:M__noexistent__)
+.      if !empty(${_var_}:M__nonexistent__) && exists(${_file_})
 .        if !defined(BUILTIN_FIND_GREP.${_var_})
-${_var_}=	${_real_file_}
+${_var_}=	${_file_}
 .        else
 ${_var_}!=								\
-	if ${GREP} -q ${BUILTIN_FIND_GREP.${_var_}:Q} ${_real_file_:Q}; then	\
-		${ECHO} ${_real_file_:Q};				\
+	if ${GREP} -q ${BUILTIN_FIND_GREP.${_var_}:Q} ${_file_:Q}; then	\
+		${ECHO} ${_file_:Q};					\
 	else								\
 		${ECHO} __nonexistent__;				\
 	fi
 .        endif
-.       endif
 .      endif
 .    endfor
 .  endif
