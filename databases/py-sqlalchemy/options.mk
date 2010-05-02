519e9d6c5e21b8d7d72536c2313e1e38f7751e03
tonnerre <tonnerre@pkgsrc.org>
2010-05-02 14:13:09 +0000

    Add MSSQL as an option to SQLAlchemy. It merely adds a dependency for people who actively want it, so no PKGREVISION bump. Addresses PR pkg/43180.

--- databases/py-sqlalchemy/options.mk
+++ databases/py-sqlalchemy/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1.1.1 2008/09/04 20:42:28 tonnerre Exp $
+# $NetBSD: options.mk,v 1.2 2010/05/02 14:13:09 tonnerre Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-sqlalchemy
-PKG_SUPPORTED_OPTIONS=	mysql sqlite pgsql
+PKG_SUPPORTED_OPTIONS=	mysql sqlite pgsql mssql
 
 .include "../../mk/bsd.options.mk"
 
@@ -15,4 +15,8 @@ DEPENDS+=	${PYPKGPREFIX}-sqlite2-[0-9]*:../../databases/py-sqlite2
 
 .if !empty(PKG_OPTIONS:Mpgsql)
 DEPENDS+=	${PYPKGPREFIX}-psycopg2-[0-9]*:../../databases/py-psycopg2
+.endif
+
+.if !empty(PKG_OPTIONS:Mmssql)
+DEPENDS+=	${PYPKGPREFIX}-mssql-[0-9]*:../../databases/py-mssql
 .endif
