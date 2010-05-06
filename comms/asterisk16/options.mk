263b2ce4e9b8916de1970891dddf0ba98dd049fc
jnemeth <jnemeth@pkgsrc.org>
2010-05-06 20:10:17 +0000

    Add a webvmail option which installs the vmail.cgi script accessing voicemail using a browser.

--- comms/asterisk16/options.mk
+++ comms/asterisk16/options.mk
@@ -1,12 +1,12 @@
-# $NetBSD: options.mk,v 1.4 2010/03/01 07:06:48 jnemeth Exp $
+# $NetBSD: options.mk,v 1.5 2010/05/06 20:10:17 jnemeth Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.asterisk
-PKG_SUPPORTED_OPTIONS=		zaptel x11 unixodbc ilbc
+PKG_SUPPORTED_OPTIONS=		zaptel x11 unixodbc ilbc webvmail
 PKG_OPTIONS_LEGACY_OPTS+=	gtk:x11
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		zaptel x11 unixodbc ilbc
+PLIST_VARS+=		zaptel x11 unixodbc ilbc webvmail
 
 # Asterisk now uses DAHDI, not zaptel; not implemented yet...
 #.if !empty(PKG_OPTIONS:Mzaptel)
@@ -74,4 +74,16 @@ post-extract:
 	cp ${DISTDIR}/${DIST_SUBDIR}/rfc3951.txt ${WRKSRC}/codecs/ilbc
 	cp ${DISTDIR}/${DIST_SUBDIR}/extract-cfile.awk ${WRKSRC}/codecs/ilbc
 	cd ${WRKSRC}/codecs/ilbc && ${AWK} -f extract-cfile.awk < rfc3951.txt
+.endif
+
+.if !empty(PKG_OPTIONS:Mwebvmail)
+SUBST_CLASSES+=		webvmail
+SUBST_STAGE.webvmail=	post-patch
+SUBST_FILES.webvmail=	contrib/scripts/vmail.cgi
+SUBST_SED.webvmail+=	-e 's|@ASTETCDIR@|${ASTETCDIR}|'
+SUBST_SED.webvmail+=	-e "s|@ASTSPOOLDIR@|${ASTSPOOLDIR}|"
+INSTALLATION_DIRS+=	${PREFIX}/libexec/cgi-bin ${PREFIX}/share/httpd/htdocs
+SPECIAL_PERMS+=		${PREFIX}/libexec/cgi-bin/vmail ${ASTERISK_USER} ${ASTERISK_GROUP} 04555
+INSTALL_TARGET+=	webvmail
+PLIST.webvmail=		yes
 .endif
