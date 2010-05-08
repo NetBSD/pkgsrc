5e8d6a0f448dab67ed04be2fb41bb71429421f22
drochner <drochner>
2010-05-08 15:32:13 +0000

    add qt4-qscintilla-2.4.3, a programmer's editor component for qt4, from Helge Muehlmeier per pkgsrc-wip

---
+++ x11/qt4-qscintilla/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/05/08 15:32:13 drochner Exp $
+
+BUILDLINK_TREE+=	qt4-qscintilla
+
+.if !defined(QT4_QSCINTILLA_BUILDLINK3_MK)
+QT4_QSCINTILLA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt4-qscintilla+=	qt4-qscintilla>=2.4.3
+BUILDLINK_PKGSRCDIR.qt4-qscintilla?=	../../x11/qt4-qscintilla
+
+.include "../../x11/qt4-libs/buildlink3.mk"
+.endif	# QT4_QSCINTILLA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt4-qscintilla
