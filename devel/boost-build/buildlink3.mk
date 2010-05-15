0449a85c93a23aa1b757445abafa4f299032bc85
adam <adam@pkgsrc.org>
2010-05-15 05:42:23 +0000

    Changes 1.43.0: New Libraries: * Functional/factory: Function objects for dynamic and by-value construction. * Functional/forward: Function object adapters to address the forwarding  problem. Major Updates: * Range: Boost.Range has undergone extensive updates that it include all of the   features from the recently reviewed Boost.RangeEx. * Range-based version of the full STL iterator based algorithms. * Range adaptors which can be combined with range-based algorithms for   unpresedented expressiveness and efficiency. * New functions: irange, istream_range, join, combine. Updated Libraries Build System: * When cross-compiling to GNU targets, static libraries are properly created.   Previously, using such libraries resulted in Archive has no index error. * The naming of DLL and import libraries with gcc compiler on Windows, both   mingw and cygwin, now matches default conventions of those environments.   Previously, the naming scheme was the same as for other Windows compilers. Updated Tools: * Quickbook: Unicode escape characters. * Support the UTF-8 byte order mark. * Disallow [ in simple markup. Fixes some errors with mismatched punctuation. * Add a command line flag (-D) to define macros at the command line. * Improved post-processor output. * Check that [section] and [endsect] tags are balanced in templates.

--- devel/boost-build/buildlink3.mk
+++ devel/boost-build/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.14 2010/02/12 10:31:04 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2010/05/15 05:42:23 adam Exp $
 
 BUILDLINK_TREE+=	boost-build
 
 .if !defined(BOOST_BUILD_BUILDLINK3_MK)
 BOOST_BUILD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.42.*
+BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.43.*
 BUILDLINK_DEPMETHOD.boost-build?=	build
 BUILDLINK_PKGSRCDIR.boost-build?=	../../devel/boost-build
 .endif # BOOST_BUILD_BUILDLINK3_MK
