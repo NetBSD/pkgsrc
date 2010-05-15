a42538d75b9379ba0e0dbea998f3f8ba5713feeb
tron <tron@pkgsrc.org>
2010-05-15 20:27:28 +0000

    Add a new option "lua" to enable support for Lua scripts and turn it on by default. Bump package revisions as the binary package changed.
    Suggested by Jonathan Kollasch in private e-mail.

--- net/wireshark/options.mk
+++ net/wireshark/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.3 2010/04/08 10:03:31 adam Exp $
+# $NetBSD: options.mk,v 1.4 2010/05/15 20:27:28 tron Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.wireshark
-PKG_SUPPORTED_OPTIONS=	gtk2
-PKG_SUGGESTED_OPTIONS=	gtk2
+PKG_SUPPORTED_OPTIONS=	gtk2 lua
+PKG_SUGGESTED_OPTIONS=	gtk2 lua
 
 .include "../../mk/bsd.options.mk"
 
@@ -15,4 +15,15 @@ PLIST.gtk2=		yes
 .include "../../x11/gtk2/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-wireshark
+.endif
+
+PLIST_VARS+=		lua
+
+.if empty(PKG_OPTIONS:Mlua)
+CONFIGURE_ARGS+=	--with-lua=no
+.else
+.include "../../lang/lua/buildlink3.mk"
+
+CONFIGURE_ARGS+=	--with-lua=${BUILDLINK_PREFIX.lua}
+PLIST.lua=		yes
 .endif
