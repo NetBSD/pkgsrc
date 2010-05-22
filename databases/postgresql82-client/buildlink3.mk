8a76ce525e8b474247ea6e99860ed9bdeac49ae8
tron <tron@pkgsrc.org>
2010-05-22 11:22:53 +0000

    Pullup ticket #3182 - requested by adam databases/postgresql82: security update databases/postgresql82-adminpack: security update databases/postgresql82-client: security update databases/postgresql82-plperl: security update databases/postgresql82-plpython: security update databases/postgresql82-pltcl: security update databases/postgresql82-server: security update databases/postgresql82-tsearch2: security update
    Revisions pulled up:
    - databases/postgresql82-client/Makefile	1.15
    - databases/postgresql82-client/PLIST		1.20
    - databases/postgresql82-client/buildlink3.mk	1.7
    - databases/postgresql82-server/PLIST		1.13
    - databases/postgresql82/Makefile.common	1.21
    - databases/postgresql82/distinfo		1.20
    - databases/postgresql82/patches/patch-ad	1.4
    ---
    Module Name:	pkgsrc
    Committed By:	adam
    Date:		Thu May 20 12:36:15 UTC 2010
    
    Modified Files:
    	pkgsrc/databases/postgresql82: Makefile.common distinfo
    	pkgsrc/databases/postgresql82-client: Makefile PLIST buildlink3.mk
    	pkgsrc/databases/postgresql82-server: PLIST
    Added Files:
    	pkgsrc/databases/postgresql82/patches: patch-ad
    
    Log Message:
    Changes 8.2.17:
    * Enforce restrictions in plperl using an opmask applied to the whole
      interpreter, instead of using "Safe.pm"
    * Prevent PL/Tcl from executing untrustworthy code from pltcl_modules
    * Fix possible crash if a cache reset message is received during
      rebuild of a relcache entry
    * Do not allow an unprivileged user to reset superuser-only parameter
      settings
    * Avoid possible crash during backend shutdown if shutdown occurs
      when a CONTEXT addition would be made to log entries
    * Update pl/perl's "ppport.h" for modern Perl versions
    * Fix assorted memory leaks in pl/python
    * Prevent infinite recursion in psql when expanding a variable that
      refers to itself
    * Fix psql's \copy to not add spaces around a dot within \copy
      (select ...)
    * Ensure that "contrib/pgstattuple" functions respond to cancel
      interrupts promptly
    * Make server startup deal properly with the case that shmget()
      returns EINVAL for an existing shared memory segment
    * Avoid possible crashes in syslogger process on Windows
    * Deal more robustly with incomplete time zone information in the
      Windows registry
    * Update the set of known Windows time zone names
    * Update time zone data files to tzdata release 2010j for DST law
      changes in Argentina, Australian Antarctic, Bangladesh, Mexico,
      Morocco, Pakistan, Palestine, Russia, Syria, Tunisia; also
      historical corrections for Taiwan.
      Also, add PKST (Pakistan Summer Time) to the default set of
      timezone abbreviations.

--- databases/postgresql82-client/buildlink3.mk
+++ databases/postgresql82-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2010/01/17 12:02:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6.2.1 2010/05/22 11:22:54 tron Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -11,10 +11,10 @@ BUILDLINK_API_DEPENDS.postgresql82-client+=	postgresql82-client>=8.2.0
 BUILDLINK_ABI_DEPENDS.postgresql82-client?=	postgresql82-client>=8.2.15nb1
 BUILDLINK_PKGSRCDIR.postgresql82-client?=	../../databases/postgresql82-client
 
-.  if defined(PG_SUBPREFIX) && !empty(PG_SUBPREFIX)
+.if defined(PG_SUBPREFIX) && !empty(PG_SUBPREFIX)
 BUILDLINK_INCDIRS.postgresql82-client?=	${PG_SUBPREFIX}/include
 BUILDLINK_LIBDIRS.postgresql82-client?=	${PG_SUBPREFIX}/lib
-.  endif
+.endif
 
 # This variable contains the libraries need to link most clients.
 BUILDLINK_LDADD.postgresql82-client=	-lpq ${BUILDLINK_LDADD.gettext}
