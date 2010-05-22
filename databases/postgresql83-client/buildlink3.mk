b90c7dfc9a8572dc22f81ca9f5f71bd06c4e5f0b
tron <tron@pkgsrc.org>
2010-05-22 10:58:01 +0000

    Pullup ticket #3182 - requested by adam databases/postgresql83: security update databases/postgresql83-adminpack: security update databases/postgresql83-client: security update databases/postgresql83-plperl: security update databases/postgresql83-plpython: security update databases/postgresql83-pltcl: security update databases/postgresql83-server: security update
    Revisions pulled up:
    - databases/postgresql83-client/Makefile	1.15
    - databases/postgresql83-client/buildlink3.mk	1.4
    - databases/postgresql83-server/PLIST		1.11
    - databases/postgresql83/Makefile.common	1.12
    - databases/postgresql83/distinfo		1.12
    - databases/postgresql83/patches/patch-ad	1.4
    ---
    Module Name:    pkgsrc
    Committed By:   adam
    Date:           Thu May 20 12:36:28 UTC 2010
    
    Modified Files:
             pkgsrc/databases/postgresql83: Makefile.common distinfo
             pkgsrc/databases/postgresql83-client: Makefile buildlink3.mk
             pkgsrc/databases/postgresql83-server: PLIST
    Added Files:
             pkgsrc/databases/postgresql83/patches: patch-ad
    
    Log Message:
    Changes 8.3.11:
    * Enforce restrictions in plperl using an opmask applied to the whole
       interpreter, instead of using "Safe.pm"
    * Prevent PL/Tcl from executing untrustworthy code from pltcl_modules
    * Fix possible crash if a cache reset message is received during
       rebuild of a relcache entry
    * Apply per-function GUC settings while running the language
       validator for the function
    * Do not allow an unprivileged user to reset superuser-only parameter
       settings
    * Avoid possible crash during backend shutdown if shutdown occurs
       when a CONTEXT addition would be made to log entries
    * Ensure the archiver process responds to changes in archive_command
       as soon as possible
    * Update pl/perl's "ppport.h" for modern Perl versions
    * Fix assorted memory leaks in pl/python
    * Prevent infinite recursion in psql when expanding a variable that
       refers to itself
    * Fix psql's \copy to not add spaces around a dot within \copy
       (select ...)
    * Fix unnecessary "GIN indexes do not support whole-index scans"
       errors for unsatisfiable queries using "contrib/intarray" operators
    * Ensure that "contrib/pgstattuple" functions respond to cancel
       interrupts promptly
    * Make server startup deal properly with the case that shmget()
       returns EINVAL for an existing shared memory segment
    * Avoid possible crashes in syslogger process on Windows
    * Deal more robustly with incomplete time zone information in the
       Windows registry
    * Update the set of known Windows time zone names
    * Update time zone data files to tzdata release 2010j for DST law
       changes in Argentina, Australian Antarctic, Bangladesh, Mexico,
       Morocco, Pakistan, Palestine, Russia, Syria, Tunisia; also
       historical corrections for Taiwan.
       Also, add PKST (Pakistan Summer Time) to the default set of
       timezone abbreviations.

--- databases/postgresql83-client/buildlink3.mk
+++ databases/postgresql83-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2010/01/17 12:02:11 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3.2.1 2010/05/22 10:58:02 tron Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -11,10 +11,10 @@ BUILDLINK_API_DEPENDS.postgresql83-client+=	postgresql83-client>=8.3.0
 BUILDLINK_ABI_DEPENDS.postgresql83-client?=	postgresql83-client>=8.3.9nb1
 BUILDLINK_PKGSRCDIR.postgresql83-client?=	../../databases/postgresql83-client
 
-.  if defined(PG_SUBPREFIX) && !empty(PG_SUBPREFIX)
+.if defined(PG_SUBPREFIX) && !empty(PG_SUBPREFIX)
 BUILDLINK_INCDIRS.postgresql83-client?=	${PG_SUBPREFIX}/include
 BUILDLINK_LIBDIRS.postgresql83-client?=	${PG_SUBPREFIX}/lib
-.  endif
+.endif
 
 # This variable contains the libraries need to link most clients.
 BUILDLINK_LDADD.postgresql83-client=	-lpq ${BUILDLINK_LDADD.gettext}
