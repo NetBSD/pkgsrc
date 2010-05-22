38ae4f26f65f73318483137b95ab3d85ba356514
tron <tron>
2010-05-22 10:38:54 +0000

    Pullup ticket #3182 - requested by adam databases/postgresql84-client: security update databases/postgresql84-plperl: security update databases/postgresql84-plpython: security update databases/postgresql84-pltcl: security update databases/postgresql84-server: security update databases/postgresql84: security update
    Revisions pulled up:
    - databases/postgresql84-client/Makefile	1.8
    - databases/postgresql84-client/PLIST		1.6
    - databases/postgresql84-client/buildlink3.mk	1.3
    - databases/postgresql84-plperl/PLIST		1.3
    - databases/postgresql84-plpython/PLIST		1.3
    - databases/postgresql84-pltcl/PLIST		1.3
    - databases/postgresql84-server/PLIST		1.5
    - databases/postgresql84/Makefile.common	1.6
    - databases/postgresql84/distinfo		1.5
    - databases/postgresql84/patches/patch-ad	1.3
    ---
    Module Name:    pkgsrc
    Committed By:   adam
    Date:           Thu May 20 12:36:39 UTC 2010
    
    Modified Files:
             pkgsrc/databases/postgresql84: Makefile.common distinfo
             pkgsrc/databases/postgresql84-client: Makefile PLIST
    buildlink3.mk
             pkgsrc/databases/postgresql84-plperl: PLIST
             pkgsrc/databases/postgresql84-plpython: PLIST
             pkgsrc/databases/postgresql84-pltcl: PLIST
             pkgsrc/databases/postgresql84-server: PLIST
    Added Files:
             pkgsrc/databases/postgresql84/patches: patch-ad
    
    Log Message:
    Changes 8.4.4:
    * Enforce restrictions in plperl using an opmask applied to the whole
       interpreter, instead of using "Safe.pm"
    * Prevent PL/Tcl from executing untrustworthy code from pltcl_modules
    * Fix data corruption during WAL replay of ALTER ... SET TABLESPACE
    * Fix possible crash if a cache reset message is received during
       rebuild of a relcache entry
    * Apply per-function GUC settings while running the language
       validator for the function
    * Do constraint exclusion for inherited "UPDATE" and "DELETE" target
       tables when constraint_exclusion = partition
    * Do not allow an unprivileged user to reset superuser-only parameter
       settings
    * Avoid possible crash during backend shutdown if shutdown occurs
       when a CONTEXT addition would be made to log entries
    * Fix erroneous handling of %r parameter in recovery_end_command
    * Ensure the archiver process responds to changes in archive_command
       as soon as possible
    * Fix pl/pgsql's CASE statement to not fail when the case expression
       is a query that returns no rows
    * Update pl/perl's "ppport.h" for modern Perl versions
    * Fix assorted memory leaks in pl/python
    * Handle empty-string connect parameters properly in ecpg
    * Prevent infinite recursion in psql when expanding a variable that
       refers to itself
    * Fix psql's \copy to not add spaces around a dot within \copy
       (select ...)
    * Avoid formatting failure in psql when running in a locale context
       that doesn't match the client_encoding
    * Fix unnecessary "GIN indexes do not support whole-index scans"
       errors for unsatisfiable queries using "contrib/intarray" operators
    * Ensure that "contrib/pgstattuple" functions respond to cancel
       interrupts promptly
    * Make server startup deal properly with the case that shmget()
       returns EINVAL for an existing shared memory segment
    * Avoid possible crashes in syslogger process on Windows
    * Deal more robustly with incomplete time zone information in the
       Windows registry
    * Update the set of known Windows time zone names
    * Update time zone data files to tzdata release 2010j for DST law
       changes in Argentina, Australian Antarctic, Bangladesh, Mexico,
       Morocco, Pakistan, Palestine, Russia, Syria, Tunisia; also
       historical corrections for Taiwan.
       Also, add PKST (Pakistan Summer Time) to the default set of
       timezone abbreviations.

--- databases/postgresql84-client/buildlink3.mk
+++ databases/postgresql84-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/01/17 12:02:11 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2.2.1 2010/05/22 10:38:54 tron Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -11,10 +11,10 @@ BUILDLINK_API_DEPENDS.postgresql84-client+=	postgresql84-client>=8.4.0
 BUILDLINK_ABI_DEPENDS.postgresql84-client?=	postgresql84-client>=8.4.2nb1
 BUILDLINK_PKGSRCDIR.postgresql84-client?=	../../databases/postgresql84-client
 
-.  if defined(PG_SUBPREFIX) && !empty(PG_SUBPREFIX)
+.if defined(PG_SUBPREFIX) && !empty(PG_SUBPREFIX)
 BUILDLINK_INCDIRS.postgresql84-client?=	${PG_SUBPREFIX}/include
 BUILDLINK_LIBDIRS.postgresql84-client?=	${PG_SUBPREFIX}/lib
-.  endif
+.endif
 
 # This variable contains the libraries need to link most clients.
 BUILDLINK_LDADD.postgresql84-client=	-lpq ${BUILDLINK_LDADD.gettext}
