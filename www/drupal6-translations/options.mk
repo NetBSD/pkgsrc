b9d42c0f5ffc57eadf66ade41d1be3c96e3e6414
taca <taca@pkgsrc.org>
2010-05-26 17:18:56 +0000

    Update drupal6-translations-20100527
    Added languages:
    
    	Arabic			ar-6.x-1.2
    	Estonian		et-6.x-2.0
    	Persian			fa-6.x-2.0-beta1
    	Hindi			hi-6.x-2.0
    	Marathi			mr-6.x-1.0-beta1
    	Malay			ms-6.x-1.0-beta3
    
    Updated	languages:
    
    	Danish			da-6.x-1.3
    	Spanish			es-6.x-1.4
    	Finish			fi-6.x-1.1
    	Japanese		ja-6.x-1.6
    	Norwegian Bokmaal	nb-6.x-2.0
    	Dutch			nl-6.x-1.6
    	Portuguese Brazil	pt-br-6.x-1.4
    	Slovak			sk-6.x-1.3
    	Ukrainian		uk-6.x-1.6

--- www/drupal6-translations/options.mk
+++ www/drupal6-translations/options.mk
@@ -1,51 +1,50 @@
-# $NetBSD: options.mk,v 1.9 2009/09/29 13:47:03 taca Exp $
+# $NetBSD: options.mk,v 1.10 2010/05/26 17:18:56 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.drupal-translations
 
 # Select official release or beta versions which contains correct
 # directory structure.
 #
-# Broken directory structure: eo hi vi
-# Development version only: ar fr nn pt th tr
-# Empty, LICENSE.it.txt only: it
-DL_SUPPORTED_LANGUAGES=	be ca cs da de el es fi fr gu he hr hu id it ja \
-			ko lt nb nl pl pt-br pt-pt ro ru sk sv tr uk uk-ua \
-			zh-cn zh-tw
+DL_SUPPORTED_LANGUAGES=	ar be ca cs da de el es et fa fi fr gu he hi hr hu \
+			id it ja ko lt mr ms nb nl pl pt-br pt-pt ro ru sk \
+			sv tr uk uk-ua zh-cn zh-tw
 
+DL_TRANSLATIONS.ar=	ar-6.x-1.2.tar.gz		# 2010/02/20
 DL_TRANSLATIONS.be=	be-6.x-1.0.tar.gz		# 2009/05/28
 DL_TRANSLATIONS.ca=	ca-6.x-1.7.tar.gz		# 2009/07/26
 DL_TRANSLATIONS.cs=	cs-6.x-1.9.tar.gz		# 2008/09/29
-DL_TRANSLATIONS.da=	da-6.x-1.1.tar.gz		# 2009/05/20
+DL_TRANSLATIONS.da=	da-6.x-1.3.tar.gz		# 2010/05/04
 DL_TRANSLATIONS.de=	de-6.x-1.0.tar.gz		# 2009/09/14
 DL_TRANSLATIONS.el=	el-6.x-1.0-beta4.tar.gz		# 2009/07/26
-#DL_TRANSLATIONS.eo=	eo-6.x-1.0.tar.gz		# 2008/05/31
-DL_TRANSLATIONS.es=	es-6.x-1.3.tar.gz		# 2009/02/14
-DL_TRANSLATIONS.fi=	fi-6.x-1.0.tar.gz		# 2008/09/10
+DL_TRANSLATIONS.es=	es-6.x-1.4.tar.gz		# 2009/10/07
+DL_TRANSLATIONS.et=	et-6.x-2.0.tar.gz		# 2010/04/09
+DL_TRANSLATIONS.fa=	fa-6.x-2.0-beta1.tar.gz		# 2009/11/03
+DL_TRANSLATIONS.fi=	fi-6.x-1.1.tar.gz		# 2009/10/09
 DL_TRANSLATIONS.fr=	fr-6.x-1.0.tar.gz		# 2009/01/13
 DL_TRANSLATIONS.gu=	gu-6.x-1.0.tar.gz		# 2008/06/11
 DL_TRANSLATIONS.he=	he-6.x-1.0.tar.gz		# 2008/03/14
+DL_TRANSLATIONS.hi=	hi-6.x-2.0.tar.gz		# 2010/01/09
 DL_TRANSLATIONS.hr=	hr-6.x-1.0-beta3.tar.gz		# 2009/08/19
 DL_TRANSLATIONS.hu=	hu-6.x-1.1.tar.gz		# 2009/09/17
 DL_TRANSLATIONS.id=	id-6.x-1.16.tar.gz		# 2009/06/21
 DL_TRANSLATIONS.it=	it-6.x-1.6.tar.gz		# 2009/08/08
-DL_TRANSLATIONS.ja=	ja-6.x-1.4.tar.gz		# 2009/07/22
+DL_TRANSLATIONS.ja=	ja-6.x-1.6.tar.gz		# 2010/03/04
 DL_TRANSLATIONS.ko=	ko-6.x-1.2.tar.gz		# 2008/12/12
 DL_TRANSLATIONS.lt=	lt-6.x-1.2.tar.gz		# 2008/02/26
-DL_TRANSLATIONS.nb=	nb-6.x-1.0.tar.gz		# 2008/10/10
-DL_TRANSLATIONS.nl=	nl-6.x-1.5.tar.gz		# 2009/07/28
-#DL_TRANSLATIONS.nn=	nn-6.x-1.x-dev.tar.gz		# 2008/05/13
+DL_TRANSLATIONS.mr=	mr-6.x-1.0-beta1.tar.gz		# 2010/01/09
+DL_TRANSLATIONS.ms=	ms-6.x-1.0-beta3.tar.gz		# 2010/02/08
+DL_TRANSLATIONS.nb=	nb-6.x-2.0.tar.gz		# 2009/02/28
+DL_TRANSLATIONS.nl=	nl-6.x-1.6.tar.gz		# 2010/03/25
 DL_TRANSLATIONS.pl=	pl-6.x-0.3.tar.gz		# 2008/09/08
-DL_TRANSLATIONS.pt-br=	pt-br-6.x-1.3.tar.gz		# 2008/02/11
+DL_TRANSLATIONS.pt-br=	pt-br-6.x-1.4.tar.gz		# 2009/12/11
 DL_TRANSLATIONS.pt-pt=	pt-pt-6.x-1.1.tar.gz		# 2009/09/26
 DL_TRANSLATIONS.ro=	ro-6.x-1.1.tar.gz		# 2008/02/21
 DL_TRANSLATIONS.ru=	ru-6.x-1.3.tar.gz		# 2009/05/04
-DL_TRANSLATIONS.sk=	sk-6.x-1.1.tar.gz		# 2009/01/04
+DL_TRANSLATIONS.sk=	sk-6.x-1.3.tar.gz		# 2010/03/19
 DL_TRANSLATIONS.sv=	sv-6.x-1.3.tar.gz		# 2009/08/06
-#DL_TRANSLATIONS.th=	th-6.x-1.x-dev.tar.gz		# 2008/05/22
 DL_TRANSLATIONS.tr=	tr-6.x-1.0-rc2.tar.gz		# 2009/04/08
 DL_TRANSLATIONS.uk-ua=	uk-ua-6.x-1.2-beta3.tar.gz	# 2008/10/03
-DL_TRANSLATIONS.uk=	uk-6.x-1.5.tar.gz		# 2009/09/19
-#DL_TRANSLATIONS.vi=	sv-6.x-1.x-dev.tar.gz		# 2008/03/26
+DL_TRANSLATIONS.uk=	uk-6.x-1.6.tar.gz		# 2009/10/07
 DL_TRANSLATIONS.zh-cn=	zh-hans-6.x-1.0.tar.gz		# 2008/06/01
 DL_TRANSLATIONS.zh-tw=	zh-hant-6.x-1.5.tar.gz		# 2009/08/20
 
