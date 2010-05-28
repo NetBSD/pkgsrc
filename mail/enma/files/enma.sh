a48e5959673d1f7c2847bc4d8087b371a450fb7a
obache <obache@pkgsrc.org>
2010-05-28 07:26:48 +0000

    Create required directory in pre-start stage, /var/run may be cleared at boot.

--- mail/enma/files/enma.sh
+++ mail/enma/files/enma.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: enma.sh,v 1.2 2009/04/09 02:25:37 obache Exp $
+# $NetBSD: enma.sh,v 1.3 2010/05/28 07:26:48 obache Exp $
 #
 # PROVIDE: enma
 # REQUIRE: NETWORK
@@ -14,6 +14,18 @@ command="@PREFIX@/libexec/enma"
 pidfile="@VARBASE@/run/enma/${name}.pid"
 required_files="@PKG_SYSCONFDIR@/enma.conf"
 command_args="-c @PKG_SYSCONFDIR@/enma.conf"
+
+start_precmd="${name}_prestart"
+
+enma_prestart()
+{
+	if [ ! -d @VARBASE@/run/enma ]; then
+		@MKDIR@ @VARBASE@/run/enma
+		@CHOWN@ @ENMA_USER@ @VARBASE@/run/enma
+		@CHGRP@ @ENMA_GROUP@ @VARBASE@/run/enma
+		@CHMOD@ 0700 @VARBASE@/run/enma
+	fi
+}
 
 load_rc_config ${name}
 run_rc_command "$1"
