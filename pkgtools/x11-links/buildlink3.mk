107397b3c1becd5d2cf828681da5ece94da91cd4
wiz <wiz@pkgsrc.org>
2010-05-31 21:20:02 +0000

    Apply patch from Bernd Ernesti in PR 43392, updating the package for recent changes in NetBSD-current's xsrc.
    Bump version.

--- pkgtools/x11-links/buildlink3.mk
+++ pkgtools/x11-links/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2010/04/30 03:37:44 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2010/05/31 21:20:02 wiz Exp $
 #
 # Don't include this file manually!  It will be included as necessary
 # by bsd.buildlink3.mk.
@@ -12,7 +12,7 @@ BUILDLINK_TREE+=	x11-links
 .if !defined(X11_LINKS_BUILDLINK3_MK)
 X11_LINKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.48
+BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.49
 BUILDLINK_PKGSRCDIR.x11-links?=	../../pkgtools/x11-links
 BUILDLINK_DEPMETHOD.x11-links?=	build
 
