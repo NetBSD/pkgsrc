0437eb7c6668928440a4ceed8d29ec4a242210f9
drochner <drochner@pkgsrc.org>
2010-06-01 10:51:04 +0000

    add eggdbus-0.6, a DBus binding library needed for gnome-2.30

---
+++ sysutils/eggdbus/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/06/01 10:51:04 drochner Exp $
+
+BUILDLINK_TREE+=	eggdbus
+
+.if !defined(EGGDBUS_BUILDLINK3_MK)
+EGGDBUS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.eggdbus+=	eggdbus>=0.6
+BUILDLINK_PKGSRCDIR.eggdbus?=	../../sysutils/eggdbus
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../sysutils/dbus/buildlink3.mk"
+.include "../../sysutils/dbus-glib/buildlink3.mk"
+.endif	# EGGDBUS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-eggdbus
