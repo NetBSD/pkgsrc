fcfc5be350f3ec98c121459d3a3cc71349b30db1
minskim <minskim@pkgsrc.org>
2010-06-01 21:44:54 +0000

    Update emacs to 23.2.
    Patches were provided by Makoto Fujiwara in PR 43335.
    
    Excerpt from the announcement:
    
      In addition to a large number of bugfixes, Emacs 23.2 includes
      several new packages, such as the CEDET suite of development tools
      (including Semantic, a set of libraries and utilities for parsing
      source code, and EDE, a package for managing code projects), and a
      new mode for editing Javascript.  The default mail composition mode
      is now Message mode, which provides features such as MIME handling.
      Many other part of Emacs have also been improved.  For a more
      complete list of changes, see the file etc/NEWS.

--- editors/emacs/version.mk
+++ editors/emacs/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.3 2009/08/05 22:04:50 minskim Exp $
+# $NetBSD: version.mk,v 1.4 2010/06/01 21:44:54 minskim Exp $
 
 _EMACS_FLAVOR=	emacs
 _EMACS_REQD=	emacs>=23
 
 _EMACS_VERSION_MAJOR=	23
-_EMACS_VERSION_MINOR=	1
+_EMACS_VERSION_MINOR=	2
