3c6dc29aa560e86c4bb686dc81daa251749f1fba
markd <markd@pkgsrc.org>
2010-06-01 21:55:32 +0000

    Add a pulseaudio option and patch so that cmake doesn't try to build pulseaudio plugin unless option selected.  Option off by default so no change to default package build. Fixes build if you happen to have pulseaudio package installed when building.

--- lang/squeak-vm/options.mk
+++ lang/squeak-vm/options.mk
@@ -1,14 +1,19 @@
-# $NetBSD: options.mk,v 1.1.1.1 2010/02/11 00:58:19 asau Exp $
+# $NetBSD: options.mk,v 1.2 2010/06/01 21:55:32 markd Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.squeak-vm
-PKG_SUPPORTED_OPTIONS=		nas
+PKG_SUPPORTED_OPTIONS=		nas pulseaudio
 PKG_SUGGESTED_OPTIONS=		nas
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS=	nas
+PLIST_VARS=	nas pulse
 
 .if !empty(PKG_OPTIONS:Mnas)
 PLIST.nas=	yes
 .include "../../audio/nas/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+PLIST.pulse=	yes
+.include "../../audio/pulseaudio/buildlink3.mk"
 .endif
