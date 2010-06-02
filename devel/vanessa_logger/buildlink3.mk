5d97ce7a90dd6eedb139fe2aaf088e68fd8cfd2e
adam <adam@pkgsrc.org>
2010-06-02 12:42:14 +0000

    Changes 0.0.8: * Bump changelog date so it is after the release of standards version 3.8.3 * Don't include full LGPL text in Debian changelog * Update to standards version 3.8.3 * LT_CONFIG_LTDL_DIR appears to be incompatible with libtool 2 * move dev documentation should go in dev package * Use dh_prep as dh_clean -k is deprecated * Versioned build dependency on debhelper 7 * syntax error in changelog * Use debhelper compatibility level 7 * new package contains an updated config.{sub,guess} and thus closes #532527 * Update standards version to 3.8.2 * Resolve various libtoolize warnings * libvanessa-socket-dev: Remove dependancy on libc6-dev * more...

--- devel/vanessa_logger/buildlink3.mk
+++ devel/vanessa_logger/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:24:30 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2010/06/02 12:42:14 adam Exp $
 #
 # This file was created automatically using createbuildlink-3.5.
 
@@ -7,8 +7,8 @@ BUILDLINK_TREE+=	vanessa_logger
 .if !defined(VANESSA_LOGGER_BUILDLINK3_MK)
 VANESSA_LOGGER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vanessa_logger+=	vanessa_logger>=0.0.7
-BUILDLINK_ABI_DEPENDS.vanessa_logger+=	vanessa_logger>=0.0.7nb1
+BUILDLINK_API_DEPENDS.vanessa_logger+=	vanessa_logger>=0.0.8
+BUILDLINK_ABI_DEPENDS.vanessa_logger+=	vanessa_logger>=0.0.8
 BUILDLINK_PKGSRCDIR.vanessa_logger?=	../../devel/vanessa_logger
 .endif # VANESSA_LOGGER_BUILDLINK3_MK
 
