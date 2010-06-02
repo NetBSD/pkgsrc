a790da8d6336a439f186690f165706279832e58e
adam <adam@pkgsrc.org>
2010-06-02 12:06:21 +0000

    Changes 4.8.30: * The log file format changed in 11gR2. * Replication Manager sites can specify one or more possible client-to-client   peers. * Added resource management feature in all Berkeley DB APIs to automatically   manage cursor and database handles by closing them when they are not   required, if they are not yet closed. * Added a SQL interface to the Berkeley DB library. The interface is based on -   and a drop-in-replacement for - the SQLite API. It can be accessed via a   command line utility, a C API, or existing APIs built for SQLite. * Added hash databases support to the DB->compact interface. * Renamed the "db_sql" utility to "db_sql_codegen". This utility is not built   by default. To build this utility, enter --enable-sql_codegen as an argument   to configure. * Added transactional support in db_sql_codegen utility. Specify TRANSACTIONAL   or NONTRANSACTIONAL in hint comments in SQL statement, db_sql_codegen   enable/disable transaction in generated code accordingly. * Added the feature read-your-writes consistency that allows client application   to check, or wait for a specific transaction to be replicated from the master   before reading database. * Added DB log verification feature, accessible via the API and a new utility.   This feature can help debugging and analysis. * Added support for applications to assign master/client role explicitly at any   time. Replication Manager can now be configured not to initiate elections. * more...

--- databases/db4/buildlink3.mk
+++ databases/db4/buildlink3.mk
@@ -1,28 +1,25 @@
-# $NetBSD: buildlink3.mk,v 1.37 2009/09/11 21:26:18 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.38 2010/06/02 12:06:21 adam Exp $
 
 BUILDLINK_TREE+=	db4
 
 .if !defined(DB4_BUILDLINK3_MK)
 DB4_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.db4+=	db4>=4.7.25.1
-BUILDLINK_ABI_DEPENDS.db4?=	db4>=4.7.25.1
+BUILDLINK_API_DEPENDS.db4+=	db4>=4.8.30
+BUILDLINK_ABI_DEPENDS.db4?=	db4>=4.8.30
 BUILDLINK_PKGSRCDIR.db4?=	../../databases/db4
 BUILDLINK_INCDIRS.db4?=		include/db4
 BUILDLINK_LDADD.db4=		-ldb4
 BUILDLINK_TRANSFORM+=		l:db-4:db4
 
-.  include "../../mk/bsd.fast.prefs.mk"
-.  if defined(USE_DB185) && !empty(USE_DB185:M[yY][eE][sS])
+.include "../../mk/bsd.fast.prefs.mk"
+.if defined(USE_DB185) && !empty(USE_DB185:M[yY][eE][sS])
 #BUILDLINK_LIBS.db4=		${BUILDLINK_LDADD.db4}
 BUILDLINK_TRANSFORM+=		l:db:db4
-.  endif
+.endif
 
-.  include "../../mk/compiler.mk"
-.  if empty(PKGSRC_COMPILER:Mgcc)
 PTHREAD_OPTS+=	native
-.    include "../../mk/pthread.buildlink3.mk"
-.  endif
+.include "../../mk/pthread.buildlink3.mk"
 .endif # DB4_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-db4
