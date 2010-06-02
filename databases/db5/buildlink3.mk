a3c3130afc543d61187d2e4dfc3f04b53f610e97
adam <adam@pkgsrc.org>
2010-06-02 12:08:48 +0000

    Berkeley DB is an embeddable database system that supports keyed access to data. The software is distributed in source code form, and developers can compile and link the source code into a single library for inclusion directly in their applications.
    Developers may choose to store data in any of several different storage
    structures to satisfy the requirements of a particular application. In
    database terminology, these storage structures and the code that operates on
    them are called access methods. The library includes support for the
    following access methods:
    
       * B+tree: Stores keys in sorted order, using either a programmer-supplied
         ordering function or a default function that does lexicographical
         ordering of keys. Applications may perform equality or range searches.
       * Hashing: Stores records in a hash table for fast searches based on
         strict equality. Extended Linear Hashing modifies the hash function
         used by the table as new records are inserted, in order to keep buckets
         underfull in the steady state.
       * Fixed and Variable-Length Records: Stores fixed- or variable-length
         records in sequential order. Record numbers may be immutable or
         mutable, i.e., permitting new records to be inserted between existing
         records or requiring that new records be added only at the end of the
         database.

---
+++ databases/db5/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/06/02 12:08:48 adam Exp $
+
+BUILDLINK_TREE+=	db5
+
+.if !defined(DB5_BUILDLINK3_MK)
+DB5_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.db5+=	db5>=5.0.21
+BUILDLINK_ABI_DEPENDS.db5+=	db5>=5.0.21
+BUILDLINK_PKGSRCDIR.db5?=	../../databases/db5
+BUILDLINK_INCDIRS.db5?=		include/db5
+BUILDLINK_LDADD.db5=		-ldb5
+BUILDLINK_TRANSFORM+=		l:db-5:db5
+
+.include "../../mk/bsd.fast.prefs.mk"
+.if defined(USE_DB185) && !empty(USE_DB185:M[yY][eE][sS])
+#BUILDLINK_LIBS.db5=		${BUILDLINK_LDADD.db5}
+BUILDLINK_TRANSFORM+=		l:db:db5
+.endif
+
+PTHREAD_OPTS+=	native
+.include "../../mk/pthread.buildlink3.mk"
+.endif # DB5_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-db5
