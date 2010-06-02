1256fd012477ce825d699830a910178989f1282c
adam <adam@pkgsrc.org>
2010-06-02 12:22:59 +0000

    Changes 5.0.0: * Support for Berkeley DB 5.0. * Drop support for Python 3.0. * Now you can use TMPDIR env variable to override default   test directory ("/tmp"). * Versioning of C API. If you use the code from C, please   check the bsddb_api->api_version number against   PYBSDDB_API_VERSION macro. * In C code, the bsddb_api->dbsequence_type component is always available,   even if the Berkeley DB version used doesn't support sequences. In that   case, the component will be NULL. * In C code, "DBSequenceObject_Check()" macro always exists, even if the   Berkeley DB version used doesn't suport sequences. In that case, the test   macro always returns "false". * For a long time, the API has been accesible via C using "_bsddb.api" or   "_pybsddb.api". If you are using Python >=2.7, you acquire access to that   API via the new Capsule protocol (see "bsddb.h").  If you use the C API and   upgrade to Python 2.7 and up, you must update the access code (see   "bsddb.h"). The Capsule protocol is not supported in Python 3.0, but   pybsddb 5.0.x doesn't support Python 3.0 anymore. * Capsule support was buggy. The string passed in to PyCapsule_New() must   outlive the capsule. * Solve an "Overflow" warning in the testsuite running under python 2.3. * When doing a complete full-matrix test, any warning will be considered   an error.

--- databases/py-bsddb3/buildlink3.mk
+++ databases/py-bsddb3/buildlink3.mk
@@ -1,18 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.15 2009/03/20 19:24:06 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2010/06/02 12:22:59 adam Exp $
 
 BUILDLINK_TREE+=	py-bsddb3
 
 .if !defined(PY_BSDDB3_BUILDLINK3_MK)
 PY_BSDDB3_BUILDLINK3_MK:=
 
-.  include "../../mk/pthread.buildlink3.mk"
-.  include "../../lang/python/pyversion.mk"
+.include "../../mk/pthread.buildlink3.mk"
+.include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.py-bsddb3+=	${PYPKGPREFIX}-bsddb3>=4.7.2
 BUILDLINK_ABI_DEPENDS.py-bsddb3+=	${PYPKGPREFIX}-bsddb3>=4.7.2
-BUILDLINK_PKGSRCDIR.py-bsddb3?=	../../databases/py-bsddb3
+BUILDLINK_PKGSRCDIR.py-bsddb3?=		../../databases/py-bsddb3
 
-.include "../../databases/db4/buildlink3.mk"
+BDB_ACCEPTED?=		db4 db5
+.include "../../mk/bdb.buildlink3.mk"
 .endif # PY_BSDDB3_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-py-bsddb3
