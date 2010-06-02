99ec37edf1dc65326aa1e4e828a5382edb9ad00e
adam <adam@pkgsrc.org>
2010-06-02 12:50:03 +0000

    Changes 1.18: See http://hg.vergenet.net/perdition/perdition/

--- mail/perdition/buildlink3.mk
+++ mail/perdition/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:24:56 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2010/06/02 12:50:03 adam Exp $
 
 BUILDLINK_TREE+=	perdition
 
@@ -6,13 +6,13 @@ BUILDLINK_TREE+=	perdition
 PERDITION_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.perdition+=	perdition>=1.17
-BUILDLINK_ABI_DEPENDS.perdition?=	perdition>=1.17nb1
+BUILDLINK_ABI_DEPENDS.perdition+=	perdition>=1.17nb1
 BUILDLINK_PKGSRCDIR.perdition?=	../../mail/perdition
 
 # The following are included in Makefile.common for "configure"'s sake.
 #.include "../../devel/popt/buildlink3.mk"
-#.include "../../devel/vanessa_adt/buildlink3.mk"
-#.include "../../devel/vanessa_logger/buildlink3.mk"
+.include "../../devel/vanessa_adt/buildlink3.mk"
+.include "../../devel/vanessa_logger/buildlink3.mk"
 #.include "../../devel/vanessa_socket/buildlink3.mk"
 .endif # PERDITION_BUILDLINK3_MK
 
