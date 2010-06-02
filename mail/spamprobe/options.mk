6f060bdbcc6f85533a3f31031a80f1cbb3fa7cf6
adam <adam>
2010-06-02 13:14:47 +0000

    Fix building with db5; revision bump for db4 update

--- mail/spamprobe/options.mk
+++ mail/spamprobe/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2007/10/25 21:45:42 ghen Exp $
+# $NetBSD: options.mk,v 1.7 2010/06/02 13:17:22 adam Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.spamprobe
 PKG_SUPPORTED_OPTIONS=		bdb gif png jpeg
@@ -7,9 +7,10 @@ PKG_SUGGESTED_OPTIONS=		gif png jpeg
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mbdb)
-BDB_ACCEPTED=		db4 # db3 db2 (not tested)
+BDB_ACCEPTED=		db4 db5 # db3 db2 (not tested)
+BUILDLINK_TRANSFORM+=	l:db:${BDB_TYPE}
 .include "../../mk/bdb.buildlink3.mk"
-CONFIGURE_ARGS+=	--with-db=${BDBBASE:Q}
+CONFIGURE_ARGS+=	--with-db=${BDBBASE}
 .endif
 
 .if !empty(PKG_OPTIONS:Mgif)
