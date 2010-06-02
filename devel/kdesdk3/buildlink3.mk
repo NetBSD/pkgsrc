a2258cf7bfd816fe02b762a7bd055d7f2f8a97ef
adam <adam@pkgsrc.org>
2010-06-02 12:26:13 +0000

    Fix building with db5; revision bump for db4 update

--- devel/kdesdk3/buildlink3.mk
+++ devel/kdesdk3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.24 2010/01/18 09:58:45 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2010/06/02 12:26:13 adam Exp $
 
 BUILDLINK_TREE+=	kdesdk
 
@@ -9,10 +9,11 @@ BUILDLINK_API_DEPENDS.kdesdk+=	kdesdk>=3.5.0nb1
 BUILDLINK_ABI_DEPENDS.kdesdk?=	kdesdk>=3.5.10nb6
 BUILDLINK_PKGSRCDIR.kdesdk?=	../../devel/kdesdk3
 
-.include "../../databases/db4/buildlink3.mk"
+BDB_ACCEPTED?=		db4 db5
 .include "../../devel/libltdl/buildlink3.mk"
 .include "../../x11/kdelibs3/buildlink3.mk"
 .include "../../x11/kdebase3/buildlink3.mk"
+.include "../../mk/bdb.buildlink3.mk"
 .endif # KDESDK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kdesdk
