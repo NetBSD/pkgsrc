a9d2ab4c74ed9ef7f393a75b19b3ec934629e5ac
adam <adam@pkgsrc.org>
2010-06-02 12:43:37 +0000

    Changes 0.0.10: * Bug-fixes.

--- devel/vanessa_socket/buildlink3.mk
+++ devel/vanessa_socket/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:24:31 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2010/06/02 12:43:37 adam Exp $
 #
 # This file was created automatically using createbuildlink-3.5.
 
@@ -7,8 +7,8 @@ BUILDLINK_TREE+=	vanessa_socket
 .if !defined(VANESSA_SOCKET_BUILDLINK3_MK)
 VANESSA_SOCKET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vanessa_socket+=	vanessa_socket>=0.0.7nb2
-BUILDLINK_ABI_DEPENDS.vanessa_socket?=	vanessa_socket>=0.0.7nb3
+BUILDLINK_API_DEPENDS.vanessa_socket+=	vanessa_socket>=0.0.10
+BUILDLINK_ABI_DEPENDS.vanessa_socket+=	vanessa_socket>=0.0.10
 BUILDLINK_PKGSRCDIR.vanessa_socket?=	../../devel/vanessa_socket
 
 .include "../../devel/popt/buildlink3.mk"
