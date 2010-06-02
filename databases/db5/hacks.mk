9001eeee29e8241b938c7cf1dc4bf1707c29f2b0
adam <adam@pkgsrc.org>
2010-06-02 12:08:48 +0000

    Berkeley DB is an embeddable database system that supports keyed access to data. The software is distributed in source code form, and developers can compile and link the source code into a single library for inclusion directly in their applications.
    Developers may choose to store data in any of several different storage
    structures to satisfy the requirements of a particular application. In
    database terminology, these storage structures and the code that operates on
    them are called access methods. The library includes support for the
    following access methods:
    
       * B+tree: Stores keys in sorted order, using either a programmer-supplied
         ordering function or a default function that does lexicographical
         ordering of keys. Applications may perform equality or range searches.
       * Hashing: Stores records in a hash table for fast searches based on
         strict equality. Extended Linear Hashing modifies the hash function
         used by the table as new records are inserted, in order to keep buckets
         underfull in the steady state.
       * Fixed and Variable-Length Records: Stores fixed- or variable-length
         records in sequential order. Record numbers may be immutable or
         mutable, i.e., permitting new records to be inserted between existing
         records or requiring that new records be added only at the end of the
         database.

---
+++ databases/db5/hacks.mk
@@ -0,0 +1,25 @@
+# $NetBSD: hacks.mk,v 1.1.1.1 2010/06/02 12:08:53 adam Exp $
+
+### [Sun Mar 14 19:32:40 UTC 2004 : jlam]
+### GCC 3.3.x and older versions have an optimization bug on powerpc that's
+### tickled by the db5 source code, so remove optimization flags in that
+### case. This fixes PR pkg/30647 by Ian Spray.
+###
+.if ${MACHINE_ARCH} == "powerpc"
+.  include "../../mk/compiler.mk"
+.  if !empty(CC_VERSION:Mgcc*)
+.    if !defined(_GCC_IS_TOO_OLD)
+_GCC_IS_TOO_OLD!=	\
+	if ${PKG_ADMIN} pmatch 'gcc<3.4' ${CC_VERSION}; then		\
+		${ECHO} "yes";						\
+	else								\
+		${ECHO} "no";						\
+	fi
+MAKEFLAGS+=	_GCC_IS_TOO_OLD=${_GCC_IS_TOO_OLD:Q}
+.    endif
+.    if !empty(_GCC_IS_TOO_OLD:M[yY][eE][sS])
+PKG_HACKS+=		powerpc-codegen
+BUILDLINK_TRANSFORM+=	rm:-O[0-9]*
+.    endif
+.  endif
+.endif
