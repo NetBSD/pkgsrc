c97d1d21b6beac110cac730675a1765d1cfecb4f
adam <adam@pkgsrc.org>
2010-06-02 12:53:49 +0000

    Fix building with db5; revision bump for db4 update

--- mail/evolution-data-server/buildlink3.mk
+++ mail/evolution-data-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.31 2009/10/25 13:23:49 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2010/06/02 13:03:11 adam Exp $
 
 BUILDLINK_TREE+=	evolution-data-server
 
@@ -6,18 +6,19 @@ BUILDLINK_TREE+=	evolution-data-server
 EVOLUTION_DATA_SERVER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.evolution-data-server+=	evolution-data-server>=1.8.0
-BUILDLINK_ABI_DEPENDS.evolution-data-server?=	evolution-data-server>=2.22.3nb1
+BUILDLINK_ABI_DEPENDS.evolution-data-server+=	evolution-data-server>=2.22.3nb1
 BUILDLINK_PKGSRCDIR.evolution-data-server?=	../../mail/evolution-data-server
 
+BDB_ACCEPTED?=		db4 db5
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../devel/GConf/buildlink3.mk"
 .include "../../devel/libbonobo/buildlink3.mk"
 .include "../../devel/nss/buildlink3.mk"
-.include "../../databases/db4/buildlink3.mk"
 .include "../../net/libsoup24/buildlink3.mk"
 .include "../../net/ORBit2/buildlink3.mk"
 .include "../../time/libical/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
+.include "../../mk/bdb.buildlink3.mk"
 .endif # EVOLUTION_DATA_SERVER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-evolution-data-server
