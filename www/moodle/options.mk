d205acf5a72703a444f5f5d854d0af9bb7f3ac19
wiz <wiz@pkgsrc.org>
2010-06-04 09:20:00 +0000

    Initial import of moodle-1.9.8 from wip, packaged by Wen Heping.
    Moodle is a course management system (CMS) - a free, Open Source software
    package designed using sound pedagogical principles, to help educators
    create effective online learning communities. You can use it on any
    computer you have handy (including webhosts), yet it can scale from a
    single-teacher site to a 40,000-student University.

---
+++ www/moodle/options.mk
@@ -0,0 +1,18 @@
+# $NetBSD: options.mk,v 1.1.1.1 2010/06/04 09:20:02 wiz Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.moodle
+
+PKG_OPTIONS_REQUIRED_GROUPS=	db
+PKG_OPTIONS_GROUP.db=		mysql pgsql
+PKG_SUGGESTED_OPTIONS=		mysql
+
+.include "../../mk/bsd.options.mk"
+
+###
+### Use mysql or postgresql backend
+###
+.if !empty(PKG_OPTIONS:Mmysql)
+DEPENDS+=	${PHP_PKG_PREFIX}-mysql>=4.3.10:../../databases/php-mysql
+.elif !empty(PKG_OPTIONS:Mpgsql)
+DEPENDS+=	${PHP_PKG_PREFIX}-pgsql>=5:../../databases/php-pgsql
+.endif
