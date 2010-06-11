c11c74a93609b977c97f08dd969e770d421ac287
drochner <drochner@pkgsrc.org>
2010-06-11 13:45:50 +0000

    update to 2.30.1 This switches to the gnome-2.30 release branch
    pkgsrc note: temporarily add a dependency on libgnome-keyring which
    was split out of the old gnome-keyring pkg, so that client pkgs
    get the same as before

--- security/gnome-keyring/buildlink3.mk
+++ security/gnome-keyring/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/08/26 19:56:58 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2010/06/11 13:45:50 drochner Exp $
 
 BUILDLINK_TREE+=	gnome-keyring
 
@@ -8,6 +8,9 @@ GNOME_KEYRING_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gnome-keyring+=	gnome-keyring>=0.4.0
 BUILDLINK_ABI_DEPENDS.gnome-keyring?=	gnome-keyring>=2.26.3nb1
 BUILDLINK_PKGSRCDIR.gnome-keyring?=	../../security/gnome-keyring
+
+# XXX see Makefile
+.include "../../security/libgnome-keyring/buildlink3.mk"
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
