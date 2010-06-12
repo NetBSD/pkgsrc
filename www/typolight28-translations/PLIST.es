fbbb49deabbabf437249236095ad527917b6d673
tron <tron@pkgsrc.org>
2010-06-12 15:59:49 +0000

    Pullup ticket #3142 - requested by taca www/typolight28-translations: build fix
    Revisions pulled up:
    - www/typolight28-translations/Makefile		1.19-1.28
    - www/typolight28-translations/PLIST.es		1.2
    - www/typolight28-translations/PLIST.fa		1.2
    - www/typolight28-translations/PLIST.hu		1.2
    - www/typolight28-translations/PLIST.lt		1.3
    - www/typolight28-translations/PLIST.sr		1.2
    - www/typolight28-translations/PLIST.tr		1.2
    - www/typolight28-translations/PLIST.uk		1.2
    - www/typolight28-translations/distinfo		1.19-1.28
    - www/typolight28-translations/options.mk	1.19-1.28
    ---
    Module Name:	pkgsrc
    Committed By:	taca
    Date:		Tue Apr 13 15:58:56 UTC 2010
    
    Modified Files:
    	pkgsrc/www/typolight28-translations: Makefile distinfo options.mk
    
    Log Message:
    Update typolight28-translations package to 20100413.
    
    Update Czech and Japanese translation files are updated to support
    TYPOlight 2.8.2.
    ---
    Module Name:	pkgsrc
    Committed By:	taca
    Date:		Fri Apr 16 15:37:50 UTC 2010
    
    Modified Files:
    	pkgsrc/www/typolight28-translations: Makefile PLIST.lt distinfo
    	    options.mk
    
    Log Message:
    Update typolight28-translations pacakge to 20100415.
    
    Update Czech, French, Italian, Lithuanian and Swedish language files.
    ---
    Module Name:	pkgsrc
    Committed By:	taca
    Date:		Tue Apr 20 23:20:38 UTC 2010
    
    Modified Files:
    	pkgsrc/www/typolight28-translations: Makefile distinfo options.mk
    
    Log Message:
    Update typolight28-translations package to 20100420.
    
    Update Latvian and Russian language files.
    ---
    Module Name:	pkgsrc
    Committed By:	taca
    Date:		Tue May  4 15:18:50 UTC 2010
    
    Modified Files:
    	pkgsrc/www/typolight28-translations: Makefile distinfo options.mk
    
    Log Message:
    Update typolight28-translations package to 20100504.
    
    Update Japanese and Russian langage files.
    ---
    Module Name:	pkgsrc
    Committed By:	taca
    Date:		Sun May 16 09:41:57 UTC 2010
    
    Modified Files:
    	pkgsrc/www/typolight28-translations: Makefile PLIST.fa PLIST.hu
    	    PLIST.sr PLIST.tr PLIST.uk distinfo options.mk
    
    Log Message:
    Update typolight28-translations package to 20100514.
    
    Update Croatian, Hungarian, Persian,  Russian, Serbian, Turkish
    and Ukrainian language files.
    ---
    Module Name:	pkgsrc
    Committed By:	taca
    Date:		Mon May 17 15:41:16 UTC 2010
    
    Modified Files:
    	pkgsrc/www/typolight28-translations: Makefile distinfo options.mk
    
    Log Message:
    Update typolight28-translations pacakge to 20100516.
    
    Update Danish language files.
    ---
    Module Name:	pkgsrc
    Committed By:	taca
    Date:		Wed May 26 16:18:42 UTC 2010
    
    Modified Files:
    	pkgsrc/www/typolight28-translations: Makefile distinfo options.mk
    
    Log Message:
    Update typolight28-translations package to 20100523.
    
    Update Czech language files.
    ---
    Module Name:	pkgsrc
    Committed By:	taca
    Date:		Wed Jun  2 13:37:34 UTC 2010
    
    Modified Files:
    	pkgsrc/www/typolight28-translations: Makefile distinfo options.mk
    
    Log Message:
    Update typolight28-translations package to 20100527.
    
    Update Dutch language files.
    ---
    Module Name:	pkgsrc
    Committed By:	taca
    Date:		Sat Jun  5 15:19:37 UTC 2010
    
    Modified Files:
    	pkgsrc/www/typolight28-translations: Makefile distinfo options.mk
    
    Log Message:
    Update typolight28-translations package to 20100603.
    
    Update Czech language files.
    ---
    Module Name:	pkgsrc
    Committed By:	taca
    Date:		Mon Jun  7 11:30:07 UTC 2010
    
    Modified Files:
    	pkgsrc/www/typolight28-translations: Makefile PLIST.es distinfo
    	    options.mk
    
    Log Message:
    Update typolight28-translations package to 20100606.
    
    * Update Spanish, Swedish and Turkish language files.
    * Update HOMEPAGE and MASTER_SITES since www.TYPOlight.org migrated to
      www.contao.org.

--- www/typolight28-translations/PLIST.es
+++ www/typolight28-translations/PLIST.es
@@ -1,4 +1,4 @@
-@comment $NetBSD: PLIST.es,v 1.1.1.1 2009/12/02 16:10:54 taca Exp $
+@comment $NetBSD: PLIST.es,v 1.1.1.1.4.1 2010/06/12 15:59:49 tron Exp $
 ${TL_WEBDIR}/system/modules/backend/languages/es/.htaccess
 ${TL_WEBDIR}/system/modules/backend/languages/es/countries.php
 ${TL_WEBDIR}/system/modules/backend/languages/es/default.php
@@ -10,6 +10,7 @@ ${TL_WEBDIR}/system/modules/backend/languages/es/tl_content.php
 ${TL_WEBDIR}/system/modules/backend/languages/es/tl_files.php
 ${TL_WEBDIR}/system/modules/backend/languages/es/tl_form.php
 ${TL_WEBDIR}/system/modules/backend/languages/es/tl_form_field.php
+${TL_WEBDIR}/system/modules/backend/languages/es/tl_install.php
 ${TL_WEBDIR}/system/modules/backend/languages/es/tl_layout.php
 ${TL_WEBDIR}/system/modules/backend/languages/es/tl_log.php
 ${TL_WEBDIR}/system/modules/backend/languages/es/tl_maintenance.php
--- www/typolight28-translations/options.mk
+++ www/typolight28-translations/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.18 2010/04/06 22:29:59 taca Exp $
+# $NetBSD: options.mk,v 1.18.2.1 2010/06/12 15:59:49 tron Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.typolight-translations
 
@@ -9,36 +9,36 @@ TL_TRANSLATIONS.af=	TYPOlight-Afrikaans-v1.zip	# 2008/01/15
 TL_TRANSLATIONS.bg=	TYPOlight-Bulgarian-v18.zip	# 2010/02/18
 TL_TRANSLATIONS.bs=	TYPOlight-Bosnian-v10.zip	# 2010/03/18
 TL_TRANSLATIONS.ca=	TYPOlight-Catalan-v4.zip	# 2008/09/24
-TL_TRANSLATIONS.cs=	TYPOlight-Czech-v12.zip		# 2010/04/04
-TL_TRANSLATIONS.da=	TYPOlight-Danish-v9.zip		# 2010/03/22
-TL_TRANSLATIONS.es=	TYPOlight-Spanish-v12.zip	# 2009/07/01
-TL_TRANSLATIONS.fa=	TYPOlight-Persian-v2.zip	# 2008/06/05
+TL_TRANSLATIONS.cs=	TYPOlight-Czech-v19.zip		# 2010/06/03
+TL_TRANSLATIONS.da=	TYPOlight-Danish-v10.zip	# 2010/05/16
+TL_TRANSLATIONS.es=	TYPOlight-Spanish-v13.zip	# 2010/06/06
+TL_TRANSLATIONS.fa=	TYPOlight-Persian-v4.zip	# 2010/05/12
 TL_TRANSLATIONS.fi=	TYPOlight-Finnish-v1.zip	# 2009/11/03
-TL_TRANSLATIONS.fr=	TYPOlight-French-v52.zip	# 2010/03/01
+TL_TRANSLATIONS.fr=	TYPOlight-French-v53.zip	# 2010/04/14
 TL_TRANSLATIONS.gl=	TYPOlight-Galician-v3.zip	# 2010/03/26
-TL_TRANSLATIONS.hr=	TYPOlight-Croatian-v4.zip	# 2010/03/01
-TL_TRANSLATIONS.hu=	TYPOlight-Hungarian-v3.zip	# 2010/01/17
+TL_TRANSLATIONS.hr=	TYPOlight-Croatian-v5.zip	# 2010/05/07
+TL_TRANSLATIONS.hu=	TYPOlight-Hungarian-v4.zip	# 2010/05/04
 TL_TRANSLATIONS.id=	TYPOlight-Indonesian-v1.zip	# 2009/01/13
 TL_TRANSLATIONS.is=	TYPOlight-Icelandic-v5.zip	# 2010/02/10
-TL_TRANSLATIONS.it=	TYPOlight-Italian-v22.zip	# 2010/03/11
-TL_TRANSLATIONS.ja=	TYPOlight-Japanese-v27.zip	# 2010/03/01
-TL_TRANSLATIONS.lt=	TYPOlight-Lithuanian-v3.zip	# 2009/12/10
-TL_TRANSLATIONS.lv=	TYPOlight-Latvian-v23.zip	# 2010/02/04
-TL_TRANSLATIONS.nl=	TYPOlight-Dutch-v40.zip		# 2010/04/06
+TL_TRANSLATIONS.it=	TYPOlight-Italian-v23.zip	# 2010/04/15
+TL_TRANSLATIONS.ja=	TYPOlight-Japanese-v29.zip	# 2010/05/04
+TL_TRANSLATIONS.lt=	TYPOlight-Lithuanian-v4.zip	# 2010/04/14
+TL_TRANSLATIONS.lv=	TYPOlight-Latvian-v24.zip	# 2010/04/20
+TL_TRANSLATIONS.nl=	TYPOlight-Dutch-v41.zip		# 2010/05/27
 TL_TRANSLATIONS.no=	TYPOlight-Norwegian-v11.zip	# 2009/11/16
 TL_TRANSLATIONS.pl=	TYPOlight-Polish-v27.zip	# 2010/02/19
 TL_TRANSLATIONS.pt=	TYPOlight-Portuguese-v7.zip	# 2009/11/01
 TL_TRANSLATIONS.ro=	TYPOlight-Romanian-v8.zip	# 2009/03/17
-TL_TRANSLATIONS.ru=	TYPOlight-Russian-v27.zip	# 2010/03/04
+TL_TRANSLATIONS.ru=	TYPOlight-Russian-v32.zip	# 2010/05/10
 TL_TRANSLATIONS.sk=	TYPOlight-Slovak-v8.zip		# 2009/08/15
 TL_TRANSLATIONS.sl=	TYPOlight-Slovenian-v6.zip	# 2010/03/12
 TL_TRANSLATIONS.sq=	TYPOlight-Albanian-v4.zip	# 2008/12/14
-TL_TRANSLATIONS.sr=	TYPOlight-Serbian-v6.zip	# 2009/06/09
-TL_TRANSLATIONS.sv=	TYPOlight-Swedish-v45.zip	# 2010/04/05
+TL_TRANSLATIONS.sr=	TYPOlight-Serbian-v7.zip	# 2010/05/10
+TL_TRANSLATIONS.sv=	TYPOlight-Swedish-v48.zip	# 2010/06/06
 TL_TRANSLATIONS.th=	TYPOlight-Thai-v2.zip		# 2008/11/23
 TL_TRANSLATIONS.tl=	TYPOlight-Tagalog-v1.zip	# 2010/01/29
-TL_TRANSLATIONS.tr=	TYPOlight-Turkish-v5.zip	# 2009/12/28
-TL_TRANSLATIONS.uk=	TYPOlight-Ukrainian-v12.zip	# 2009/11/02
+TL_TRANSLATIONS.tr=	TYPOlight-Turkish-v10.zip	# 2010/06/06
+TL_TRANSLATIONS.uk=	TYPOlight-Ukrainian-v13.zip	# 2010/15/14
 TL_TRANSLATIONS.zh=	TYPOlight-Chinese-v20.zip	# 2010/01/29
 
 .for l in ${TL_SUPPORTED_LANGUAGES}
@@ -55,6 +55,6 @@ TL_LANGUAGES+=	${l:S/^lang-//1}
 .for l in ${TL_LANGUAGES}
 TL_DISTFILES+=	${TL_TRANSLATIONS.${l}}
 SITES.${TL_TRANSLATIONS.${l}}= \
-	http://www.typolight.org/download.html?iso=${l}&file=tl_files/languages/${l}/
+	http://www.contao.org/download.html?iso=${l}&file=tl_files/languages/${l}/
 PLIST_SRC+=	PLIST.${l}
 .endfor
