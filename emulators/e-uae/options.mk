c3631868059dc1cdf06f88a48b635da35fa98ca7
wiz <wiz@pkgsrc.org>
2010-06-14 06:30:24 +0000

    gtk option needs pkg-config, add it to USE_TOOLS.

--- emulators/e-uae/options.mk
+++ emulators/e-uae/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2007/08/16 22:23:46 joerg Exp $
+# $NetBSD: options.mk,v 1.3 2010/06/14 06:30:24 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.e-uae
 PKG_SUPPORTED_OPTIONS=	gtk sdl x11
@@ -7,6 +7,7 @@ PKG_SUGGESTED_OPTIONS=	gtk sdl
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgtk)
+USE_TOOLS+=	pkg-config
 .include "../../x11/gtk2/buildlink3.mk"
 .endif
 
