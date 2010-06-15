36aecfc21ec07ca59a62ac147310e30a0eff8341
pettai <pettai>
2010-06-15 23:10:44 +0000

    courier-authlib-0.63.0:
    * authldapescape.c: Factor out LDAP string escape function.
    * authldap.schema: Various fixes
    * authldap.ldif (olcObjectClasses): Create LDIF format schema from
      authldap.schema
    * authoption.c (auth_getoptionenvint): For account options that
      are parsed to an int, an option value that begins with t, T, y, or Y
      is evaluated as 1, other alphabetic values as 0; so that 'true'
      or 'yes' get evaluated as 1.
    
    (See the Changelog for the previous releases)
    
    Based on patch(es) from PR pkg/42989 by Brian Candler

--- security/courier-authlib/buildlink3.mk
+++ security/courier-authlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:25:16 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2010/06/15 23:10:44 pettai Exp $
 
 BUILDLINK_TREE+=	courier-authlib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	courier-authlib
 COURIER_AUTHLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.courier-authlib+=	courier-authlib>=0.53
-BUILDLINK_ABI_DEPENDS.courier-authlib?=	courier-authlib>=0.58nb2
+BUILDLINK_ABI_DEPENDS.courier-authlib?=	courier-authlib>=0.63.0
 BUILDLINK_PKGSRCDIR.courier-authlib?=	../../security/courier-authlib
 .endif # COURIER_AUTHLIB_BUILDLINK3_MK
 
