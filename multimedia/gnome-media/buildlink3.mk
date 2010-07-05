6c023339569855a4f7fdeda378e6af127437fbcc
dsainty <dsainty@pkgsrc.org>
2010-07-05 00:00:18 +0000

    The .la for gnome-media may explicitly link in gstreamer-0.10, so buildlink it across so that other packages don't fail at link time.
    Fixes x11/py-gnome2-desktop build on (at least) ancient SuSE systems.

--- multimedia/gnome-media/buildlink3.mk
+++ multimedia/gnome-media/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2010/06/13 22:45:04 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2010/07/05 00:00:18 dsainty Exp $
 
 BUILDLINK_TREE+=	gnome-media
 
@@ -11,6 +11,7 @@ BUILDLINK_PKGSRCDIR.gnome-media?=	../../multimedia/gnome-media
 
 .include "../../devel/GConf-ui/buildlink3.mk"
 .include "../../devel/libglade/buildlink3.mk"
+.include "../../multimedia/gstreamer0.10/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .endif # GNOME_MEDIA_BUILDLINK3_MK
 
