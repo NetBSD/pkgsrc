477e6aefa22ec85b4d5fc22869e280d1ae60c252
drochner <drochner@pkgsrc.org>
2010-07-06 15:23:59 +0000

    update to 2.30.2 This switches to the gnome-2.30 release branch.

--- sysutils/brasero/buildlink3.mk
+++ sysutils/brasero/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2010/06/13 22:45:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2010/07/06 15:23:59 drochner Exp $
 
 BUILDLINK_TREE+=	brasero
 
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.brasero+=	brasero>=2.26.0
 BUILDLINK_ABI_DEPENDS.brasero?=	brasero>=2.28.3nb3
 BUILDLINK_PKGSRCDIR.brasero?=	../../sysutils/brasero
 
-.include "../../devel/glib2/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 
 .endif # BRASERO_BUILDLINK3_MK
