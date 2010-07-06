0bf7ebd52cb1f580b7918ed44cd8355a31d69ed4
obache <obache@pkgsrc.org>
2010-07-06 07:33:43 +0000

    Update msmtp to 1.4.21. Based on PR#43566, with additional clean up, exactly enable/disable options.
    Version 1.4.21:
    - No significant changes.
    
    Version 1.4.20:
    - Added support for authentication mechanism SCRAM-SHA-1 via GNU SASL.
    - The new command tls_fingerprint allows one to trust one particular TLS
      certificate, in case tls_trust_file cannot be used for some reason.
    - The new script msmtp-gnome-tool.py manages Gnome Keyring passwords for msmtp.
    
    Version 1.4.19:
    - When using OpenSSL, msmtp now correctly handles NUL characters in the Common
      Name and Subject Alternative Name fields of certificates. This fixes a
      security problem. Note that msmtp is not affected by this problem if GnuTLS is
      used.

--- mail/msmtp/options.mk
+++ mail/msmtp/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.10 2009/01/06 12:08:01 obache Exp $
+# $NetBSD: options.mk,v 1.11 2010/07/06 07:33:43 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.msmtp
 
@@ -16,9 +16,11 @@ PKG_SUGGESTED_OPTIONS=	ssl
 .if !empty(PKG_OPTIONS:Mssl)
 .  include "../../security/openssl/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-ssl=openssl
+CONFIGURE_ARGS+=	--with-libssl-prefix=${BUILDLINK_PREFIX.openssl}
 .elif !empty(PKG_OPTIONS:Mgnutls)
 .  include "../../security/gnutls/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-ssl=gnutls
+CONFIGURE_ARGS+=	--with-libgnutls-prefix=${BUILDLINK_PREFIX.gnutls}
 .else
 CONFIGURE_ARGS+=	--disable-ssl
 .endif
@@ -28,6 +30,10 @@ CONFIGURE_ARGS+=	--disable-ssl
 ###
 .if !empty(PKG_OPTIONS:Mgsasl)
 .  include "../../security/gsasl/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-libgsasl
+CONFIGURE_ARGS+=	--with-libgsasl-prefix=${BUILDLINK_PREFIX.gsasl}
+.else
+CONFIGURE_ARGS+=	--without-libgsasl
 .endif
 
 ###
@@ -36,6 +42,9 @@ CONFIGURE_ARGS+=	--disable-ssl
 .if !empty(PKG_OPTIONS:Midn)
 .  include "../../devel/libidn/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-libidn
+CONFIGURE_ARGS+=	--with-libidn-prefix=${BUILDLINK_PREFIX.libidn}
+.else
+CONFIGURE_ARGS+=	--without-libidn
 .endif
 
 ###
@@ -45,4 +54,6 @@ CONFIGURE_ARGS+=	--with-libidn
 .  include "../../security/gnome-keyring/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-gnome-keyring
 USE_TOOLS+=		pkg-config
+.else
+CONFIGURE_ARGS+=	--without-gnome-keyring
 .endif
