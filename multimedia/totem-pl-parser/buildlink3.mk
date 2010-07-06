af4f2be93470ddf4c6e13098e734d04e45f1b015
drochner <drochner@pkgsrc.org>
2010-07-06 15:22:23 +0000

    update to 2.30.1 This switches to the gnome-2.30 release branch.
    (shlib major change, bl3 dependency bumped)

--- multimedia/totem-pl-parser/buildlink3.mk
+++ multimedia/totem-pl-parser/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2010/06/13 22:45:07 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2010/07/06 15:22:23 drochner Exp $
 
 BUILDLINK_TREE+=	totem-pl-parser
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	totem-pl-parser
 TOTEM_PL_PARSER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.totem-pl-parser+=	totem-pl-parser>=2.22.1
-BUILDLINK_ABI_DEPENDS.totem-pl-parser?=	totem-pl-parser>=2.28.3nb1
+BUILDLINK_ABI_DEPENDS.totem-pl-parser?=	totem-pl-parser>=2.30.1
 BUILDLINK_PKGSRCDIR.totem-pl-parser?=	../../multimedia/totem-pl-parser
 
 .include "../../devel/glib2/buildlink3.mk"
