cd12fd240b9d982b7b025591127f28be8d9b18a0
obache <obache@pkgsrc.org>
2010-07-11 02:53:24 +0000

    Fixes memory corruption, PR#43586.
    Bump PKGREVISION.

--- inputmethod/ja-freewnn-lib/buildlink3.mk
+++ inputmethod/ja-freewnn-lib/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:24:46 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2010/07/11 02:53:24 obache Exp $
 
 BUILDLINK_TREE+=	ja-FreeWnn-lib
 
 .if !defined(JA_FREEWNN_LIB_BUILDLINK3_MK)
 JA_FREEWNN_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ja-FreeWnn-lib+=	ja-FreeWnn-lib>=1.10nb3
+BUILDLINK_API_DEPENDS.ja-FreeWnn-lib+=	ja-FreeWnn-lib>=1.10nb4
 BUILDLINK_PKGSRCDIR.ja-FreeWnn-lib?=	../../inputmethod/ja-freewnn-lib
 BUILDLINK_DEPMETHOD.ja-freewnn?=	build
 .endif # JA_FREEWNN_LIB_BUILDLINK3_MK
