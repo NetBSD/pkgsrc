9c2c27aec409fb15700cedb75562580c343439d8
drochner <drochner@pkgsrc.org>
2010-07-12 15:47:36 +0000

    propagate some more dependencies explicitely for sanity

--- sysutils/brasero/buildlink3.mk
+++ sysutils/brasero/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2010/07/06 15:23:59 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2010/07/12 15:47:36 drochner Exp $
 
 BUILDLINK_TREE+=	brasero
 
@@ -10,6 +10,9 @@ BUILDLINK_ABI_DEPENDS.brasero?=	brasero>=2.28.3nb3
 BUILDLINK_PKGSRCDIR.brasero?=	../../sysutils/brasero
 
 .include "../../x11/gtk2/buildlink3.mk"
+.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
+.include "../../audio/libcanberra/buildlink3.mk"
+.include "../../multimedia/totem-pl-parser/buildlink3.mk"
 
 .endif # BRASERO_BUILDLINK3_MK
 
