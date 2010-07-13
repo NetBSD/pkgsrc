e59610f9c12912eee30ac5f917e0378f438bcca7
drochner <drochner@pkgsrc.org>
2010-07-13 19:52:30 +0000

    update to 2.30.2 This switches to the gnome-2.30 release branch.

--- chat/empathy/buildlink3.mk
+++ chat/empathy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2010/06/13 22:43:56 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2010/07/13 19:52:30 drochner Exp $
 
 BUILDLINK_TREE+=	empathy
 
@@ -9,8 +9,6 @@ BUILDLINK_API_DEPENDS.empathy+=	empathy>=2.24.1nb1
 BUILDLINK_ABI_DEPENDS.empathy?=	empathy>=2.26.2nb6
 BUILDLINK_PKGSRCDIR.empathy?=	../../chat/empathy
 
-.include "../../chat/telepathy-mission-control/buildlink3.mk"
-.include "../../devel/libglade/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../mail/evolution-data-server/buildlink3.mk"
 .include "../../sysutils/dbus/buildlink3.mk"
