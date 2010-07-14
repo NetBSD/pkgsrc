44f3927ac72b59d6f855d24225a4f7a9cb15547d
drochner <drochner>
2010-07-14 16:13:20 +0000

    update to 5.0 changes: -bugfixes -incompatible API change: introducing counters for start/stop  listen operations

--- x11/libxklavier/buildlink3.mk
+++ x11/libxklavier/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2010/02/16 14:54:41 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2010/07/14 16:13:20 drochner Exp $
 
 BUILDLINK_TREE+=	libxklavier
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	libxklavier
 LIBXKLAVIER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libxklavier+=	libxklavier>=3.0
+BUILDLINK_ABI_DEPENDS.libxklavier+=	libxklavier>=5.0
 BUILDLINK_PKGSRCDIR.libxklavier?=	../../x11/libxklavier
 
 .include "../../textproc/libxml2/buildlink3.mk"
