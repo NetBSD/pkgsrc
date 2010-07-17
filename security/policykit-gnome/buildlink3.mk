e60b7f3e8fae81126480c2c0ed18e1148e7de3f7
wiz <wiz@pkgsrc.org>
2010-07-17 12:17:51 +0000

    Bump depends for libpng bump. PR 43564 by David H. Gutteridge.

--- security/policykit-gnome/buildlink3.mk
+++ security/policykit-gnome/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:22 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2010/07/17 12:19:35 wiz Exp $
 
 BUILDLINK_TREE+=	policykit-gnome
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	policykit-gnome
 POLICYKIT_GNOME_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.policykit-gnome+=	policykit-gnome>=0.9.2
+BUILDLINK_ABI_DEPENDS.policykit-gnome+=	policykit-gnome>=0.9.2nb2
 BUILDLINK_PKGSRCDIR.policykit-gnome?=	../../security/policykit-gnome
 
 .include "../../security/policykit/buildlink3.mk"
