8bac1de65820ca80e4e6e1d43ca510541adb6c13
obache <obache@pkgsrc.org>
2010-07-19 12:37:27 +0000

    Revert unwanted bump.

--- x11/gnome-control-center/buildlink3.mk
+++ x11/gnome-control-center/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2010/07/19 12:05:33 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2010/07/19 12:37:28 obache Exp $
 
 BUILDLINK_TREE+=	gnome-control-center
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gnome-control-center
 GNOME_CONTROL_CENTER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gnome-control-center+=	gnome-control-center>=2.20.0.1
-BUILDLINK_ABI_DEPENDS.gnome-control-center?=	gnome-control-center>=2.30.1nb1
+BUILDLINK_ABI_DEPENDS.gnome-control-center?=	gnome-control-center>=2.28.1nb4
 BUILDLINK_PKGSRCDIR.gnome-control-center?=	../../x11/gnome-control-center
 
 .include "../../sysutils/gnome-menus/buildlink3.mk"
