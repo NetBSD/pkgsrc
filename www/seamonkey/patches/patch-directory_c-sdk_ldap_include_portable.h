0710dfeb4e6d781e39effd9e202807b831471f4f
tron <tron>
2010-07-22 14:31:30 +0000

    Pullup ticket #3178 - requested by tnn www/seamonkey: security update and build fix
    Revisions pulled up:
    - www/seamonkey/Makefile			1.38
    - www/seamonkey/distinfo			1.51-1.54
    - www/seamonkey/patches/patch-directory_c-sdk_configure.in 1.1-1.2
    - www/seamonkey/patches/patch-directory_c-sdk_ldap_include_portable.h 1.1-1.3
    - www/seamonkey/patches/patch-directory_c-sdk_ldap_libraries_libldap_Makefile.in 1.1-1.2
    - www/seamonkey/patches/patch-mp		1.2
    ---
    Module Name:	pkgsrc
    Committed By:	tnn
    Date:		Mon Jul 12 11:57:00 UTC 2010
    
    Modified Files:
    	pkgsrc/www/seamonkey: distinfo
    Added Files:
    	pkgsrc/www/seamonkey/patches: patch-directory_c-sdk_configure.in
    	    patch-directory_c-sdk_ldap_include_portable.h
    	    patch-directory_c-sdk_ldap_libraries_libldap_Makefile.in
    
    Log Message:
    Add some DragonFlyBSD fixes, contributed by Francois Tigeot.
    ---
    Module Name:	pkgsrc
    Committed By:	tnn
    Date:		Mon Jul 12 13:57:29 UTC 2010
    
    Modified Files:
    	pkgsrc/www/seamonkey: distinfo
    	pkgsrc/www/seamonkey/patches: patch-directory_c-sdk_configure.in
    	    patch-directory_c-sdk_ldap_include_portable.h
    	    patch-directory_c-sdk_ldap_libraries_libldap_Makefile.in
    
    Log Message:
    Add upstream bug ref.
    ---
    Module Name:	pkgsrc
    Committed By:	tnn
    Date:		Wed Jul 21 11:48:06 UTC 2010
    
    Modified Files:
    	pkgsrc/www/seamonkey: distinfo
    	pkgsrc/www/seamonkey/patches:
    	    patch-directory_c-sdk_ldap_include_portable.h
    
    Log Message:
    Fix undefined reference to re_comp/re_exec on dragonfly.
    Reported by Francois Tigeot.
    ---
    Module Name:	pkgsrc
    Committed By:	tnn
    Date:		Wed Jul 21 18:25:06 UTC 2010
    
    Modified Files:
    	pkgsrc/www/seamonkey: Makefile distinfo
    	pkgsrc/www/seamonkey/patches: patch-mp
    
    Log Message:
    Update to seamonkey-2.0.6
    
    MFSA 2010-47 Cross-origin data leakage from script filename in error messages
    MFSA 2010-46 Cross-domain data theft using CSS
    MFSA 2010-45 Multiple location bar spoofing vulnerabilities
    MFSA 2010-42 Cross-origin data disclosure via Web Workers and importScripts
    MFSA 2010-41 Remote code execution using malformed PNG image
    MFSA 2010-40 nsTreeSelection dangling pointer remote code execution
    MFSA 2010-39 nsCSSValue::Array index integer overflow
    MFSA 2010-37 Plugin parameter EnsureCachedAttrParamArrays remote code execution
    MFSA 2010-36 Use-after-free error in NodeIterator
    MFSA 2010-35 DOM attribute cloning remote code execution vulnerability
    MFSA 2010-34 Miscellaneous memory safety hazards (rv:1.9.2.7/ 1.9.1.11)

---
+++ www/seamonkey/patches/patch-directory_c-sdk_ldap_include_portable.h
@@ -0,0 +1,38 @@
+$NetBSD: patch-directory_c-sdk_ldap_include_portable.h,v 1.3.2.2 2010/07/22 14:31:30 tron Exp $
+
+Add DragonFly support.
+https://bugzilla.mozilla.org/show_bug.cgi?id=577298
+
+--- directory/c-sdk/ldap/include/portable.h.orig	2006-10-03 20:43:40.000000000 +0000
++++ directory/c-sdk/ldap/include/portable.h
+@@ -122,8 +122,11 @@
+  * some systems don't have the BSD re_comp and re_exec routines
+  */
+ #ifndef NEED_BSDREGEX
+-#if ( defined( SYSV ) || defined( NETBSD ) || defined( freebsd ) || defined( linux ) || defined( DARWIN )) && !defined(sgi)
++#if ( defined( SYSV ) || defined( NETBSD ) || defined( freebsd ) || defined(DRAGONFLY) || defined( linux ) || defined( DARWIN )) && !defined(sgi)
+ #define NEED_BSDREGEX
++/* there are conflicting prototypes in unistd.h on DragonFly */
++#define re_comp ldap_compat_re_comp
++#define re_exec ldap_compat_re_exec
+ #endif
+ #endif
+ 
+@@ -295,7 +298,7 @@ typedef char GETHOSTBYNAME_buf_t [NSLDAP
+ #elif defined(HPUX10)
+ #define GETHOSTBYNAME_BUF_T struct hostent_data
+ #define GETHOSTBYNAME( n, r, b, l, e )	nsldapi_compat_gethostbyname_r( n, r, (char *)&b, l, e )
+-#elif defined(LINUX)
++#elif defined(LINUX) || defined(DRAGONFLY)
+ typedef char GETHOSTBYNAME_buf_t [NSLDAPI_NETDB_BUF_SIZE];
+ #define GETHOSTBYNAME_BUF_T GETHOSTBYNAME_buf_t
+ #define GETHOSTBYNAME( n, r, b, l, rp, e )  gethostbyname_r( n, r, b, l, rp, e )
+@@ -317,7 +320,7 @@ typedef char GETHOSTBYNAME_buf_t [NSLDAP
+ 	|| defined(OSF1V4) || defined(AIX) || defined(UnixWare) \
+         || defined(hpux) || defined(HPUX11) || defined(NETBSD) \
+         || defined(IRIX6) || defined(FREEBSD) || defined(VMS) \
+-        || defined(NTO) || defined(OPENBSD)
++        || defined(NTO) || defined(OPENBSD) || defined(DRAGONFLY)
+ #define NSLDAPI_CTIME( c, b, l )        ctime_r( c, b )
+ #elif defined( OSF1V3 )
+ #define NSLDAPI_CTIME( c, b, l )	(ctime_r( c, b, l ) ? NULL : b)
