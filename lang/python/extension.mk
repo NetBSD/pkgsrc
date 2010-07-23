a3d52accd25c9ea74ae81249414dba52691c6de5
gdt <gdt@pkgsrc.org>
2010-07-23 22:56:40 +0000

    Explain what extension.mk is for, vs egg.mk, and clarify the notion of egg info files in non-egg python distributions.
    Add a comment that PY_NO_EGG appears wrong.  Obviously there is a
    reason it was added, but we need to figure out why and if it's still
    true.

--- lang/python/extension.mk
+++ lang/python/extension.mk
@@ -1,6 +1,15 @@
-# $NetBSD: extension.mk,v 1.26 2010/07/21 11:29:10 wiz Exp $
+# $NetBSD: extension.mk,v 1.27 2010/07/23 22:56:40 gdt Exp $
 
 .include "../../lang/python/pyversion.mk"
+
+# Packages that are a non-egg distutils extension should set
+# PYDISTUTILSPKG=YES and include this mk file.
+
+# This mk fragment is included to handle packages that create
+# extensions to python, which by definition are those that place files
+# in ${PYSITELIB}.  Extensions can be implemented via setuptools as
+# eggs (see egg.mk), via distutils (confusing, with an egg-info file,
+# even though they are not eggs), or via more ad hocs methods.
 
 .if defined(PYDISTUTILSPKG)
 .include "../../mk/bsd.prefs.mk"
@@ -27,6 +36,10 @@ do-install:
 	 ${PYTHONBIN} ${PYSETUP} ${PYSETUPARGS} "install" ${_PYSETUPINSTALLARGS})
 .endif
 
+# XXX This suppress the installation of the egg-info file (and
+# therefore its inclusion in the package).  Python practice is be to
+# use these files to let 'require' verify that python distributions
+# are present.
 PY_NO_EGG?=		yes
 .if !empty(PY_NO_EGG:M[yY][eE][sS])
 # see python25/patches/patch-av
