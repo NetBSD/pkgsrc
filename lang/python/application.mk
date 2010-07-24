b41a0ae257b171ceeb2b40cb826b48f78f8139d2
gdt <gdt@pkgsrc.org>
2010-07-24 18:12:11 +0000

    Clarify when to use this fragment, and that it's ok to use it and egg.mk in the same package.

--- lang/python/application.mk
+++ lang/python/application.mk
@@ -1,6 +1,11 @@
-# $NetBSD: application.mk,v 1.9 2007/02/20 11:30:43 rillig Exp $
+# $NetBSD: application.mk,v 1.10 2010/07/24 18:12:11 gdt Exp $
 #
 # Replace the #! interpreter for Python scripts.
+#
+# This mk fragment should be included in all python packages that
+# install python scripts, or at least those that don't use setuptools
+# or some other mechanism to set the real path.  Specifically, it is
+# reasonable to include both egg.mk and application.mk.
 #
 # Package-settable variables:
 #
