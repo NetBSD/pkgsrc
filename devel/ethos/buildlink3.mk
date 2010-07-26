fe4c347f1d9f3a277fa0eb4f1e72bedc3d2076d3
drochner <drochner@pkgsrc.org>
2010-07-26 11:23:56 +0000

    add ethos-0.2.2, a plugin framework for glib2 based applications

---
+++ devel/ethos/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/07/26 11:23:56 drochner Exp $
+
+BUILDLINK_TREE+=	ethos
+
+.if !defined(ETHOS_BUILDLINK3_MK)
+ETHOS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ethos+=	ethos>=0.2.2
+BUILDLINK_PKGSRCDIR.ethos?=	../../devel/ethos
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# ETHOS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ethos
