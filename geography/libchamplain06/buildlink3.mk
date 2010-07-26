ad7230346c8ca58f3de86b5e6404c3991eab20bf
drochner <drochner@pkgsrc.org>
2010-07-26 11:07:18 +0000

    add libchamplain06-0.6.1, a GTK map widget (the development version, can be installed in parallel to 0.4.x)

---
+++ geography/libchamplain06/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/07/26 11:07:18 drochner Exp $
+
+BUILDLINK_TREE+=	libchamplain06
+
+.if !defined(LIBCHAMPLAIN06_BUILDLINK3_MK)
+LIBCHAMPLAIN06_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libchamplain06+=	libchamplain06>=0.6.1
+BUILDLINK_PKGSRCDIR.libchamplain06?=	../../geography/libchamplain06
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../graphics/clutter-gtk/buildlink3.mk"
+.include "../../databases/sqlite3/buildlink3.mk"
+.include "../../net/libsoup24/buildlink3.mk"
+.endif	# LIBCHAMPLAIN06_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libchamplain06
