d516d1d101b41d55a603cffabffb29e363b83cd3
drochner <drochner@pkgsrc.org>
2010-07-26 11:13:43 +0000

    add one

---
+++ graphics/eog/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2010/07/26 11:13:43 drochner Exp $
+
+BUILDLINK_TREE+=	eog
+
+.if !defined(EOG_BUILDLINK3_MK)
+EOG_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.eog+=	eog>=2.30.2
+BUILDLINK_PKGSRCDIR.eog?=	../../graphics/eog
+
+.include "../../devel/GConf/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# EOG_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-eog
