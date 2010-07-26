25811d2e7c8772f9d89a796334e82c0f0d8284ef
drochner <drochner>
2010-07-26 11:05:26 +0000

    add libchamplain04-0.4.6, a GTK map widget (the current stable version)

---
+++ geography/libchamplain04/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/07/26 11:05:26 drochner Exp $
+
+BUILDLINK_TREE+=	libchamplain
+
+.if !defined(LIBCHAMPLAIN_BUILDLINK3_MK)
+LIBCHAMPLAIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libchamplain+=	libchamplain04>=0.4.6
+BUILDLINK_PKGSRCDIR.libchamplain?=	../../geography/libchamplain04
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../graphics/clutter-gtk/buildlink3.mk"
+.include "../../databases/sqlite3/buildlink3.mk"
+.include "../../net/libsoup24/buildlink3.mk"
+.endif	# LIBCHAMPLAIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libchamplain
