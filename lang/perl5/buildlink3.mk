06da3467ecbe1d3a5808ae703f428449c04c2616
seb <seb@pkgsrc.org>
2010-08-19 20:47:09 +0000

    Update perl from version 5.10.1nb2 to version 5.12.1.
    Pkgsrc changes:
    - Adjust some patches
    - Remove patches integrated upstream
    - Use Perl's "Policy.sh" framework (along the way remove some patches)
    - Link in an enterprise friendly way on HPUX
    - in buildlink3.mk bump minimum required pkgsrc Perl version to
    5.12.0
    - placate some pkglint warnings
    
    Upstream changes:
    - From version 5.12.1 announce
    
    This release contains minor bug fixes and updates of several core
    modules, as well as minor documentation updates. It should be fully
    backward compatible with Perl 5.12.0.
    
    - From version 5.12.0 announce:
    
    Perl 5.12.0 represents approximately two years of development since
    version 5.10.0 and contains over 750,000 lines of changes across over
    3,000 files from over 200 authors and committers.
    
    [...]
    
    Notable changes in Perl 5.12 include:
    
    * Perl now conforms much more closely to the Unicode standard.
      Additionally, this release includes an upgrade to version
      5.2 of the standard.
    
    * New experimental APIs allow developers to extend Perl with
      "pluggable" keywords and syntax.
    
    * Perl now has a better sense of time and will be able to keep
      accurate time well past the "Y2038" barrier.
    
    * New syntax allows developers to specify package version numbers
      directly in "package" statements
    
    * Perl now warns the user about the use of deprecated features
      by default.
    
    - For a complete list of changes see included perl5120delta(1) and
    perl5121delta(1) or http://perldoc.perl.org/perl5120delta.html and
    http://perldoc.perl.org/perl5121delta.html

--- lang/perl5/buildlink3.mk
+++ lang/perl5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.45 2009/07/17 06:43:27 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.46 2010/08/19 20:47:09 seb Exp $
 
 BUILDLINK_TREE+=	perl
 
@@ -6,13 +6,13 @@ BUILDLINK_TREE+=	perl
 PERL5_BUILDLINK3_MK:=
 
 .include "../../mk/bsd.fast.prefs.mk"
-PERL5_REQD+=			5.10.0
+PERL5_REQD+=			5.12.0
 .for _perl5_ver_ in ${PERL5_REQD}
 BUILDLINK_API_DEPENDS.perl+=	perl>=${_perl5_ver_}
 .endfor
-# Prevent p5-* etc. packages built for 5.10.0 to be used
+# Prevent p5-* etc. packages built for 5.12.0 to be used
 # with the next incompatible version of perl:
-BUILDLINK_API_DEPENDS.perl+=    perl<5.12.0
+BUILDLINK_API_DEPENDS.perl+=	perl<5.14.0
 
 BUILDLINK_PKGSRCDIR.perl?=	../../lang/perl5
 
@@ -24,21 +24,19 @@ pkgbase := perl
 .include "../../mk/pkg-build-options.mk"
 
 PERL5_OPTIONS?=		# empty
-.  if !empty(PERL5_OPTIONS:Mthreads)
+.if !empty(PERL5_OPTIONS:Mthreads)
 INSTALL_TEMPLATES+=	${.CURDIR}/../../lang/perl5/files/install_threads.tmpl
-.  endif
+.endif
 
-.  if ${PKG_INSTALLATION_TYPE} == "overwrite"
+.if ${PKG_INSTALLATION_TYPE} == "overwrite"
 #
 # Perl keeps headers and odd libraries in an odd path not caught by the
 # default BUILDLINK_FILES_CMD, so name them to be symlinked into
 # ${BUILDLINK_DIR}.
 #
 .include "../../lang/perl5/vars.mk"
-BUILDLINK_FILES.perl=							\
-	${PERL5_SUB_INSTALLARCHLIB}/CORE/*				\
-	${PERL5_SUB_INSTALLARCHLIB}/auto/DynaLoader/DynaLoader.a
-.  endif
+BUILDLINK_FILES.perl=	${PERL5_SUB_INSTALLARCHLIB}/CORE/*
+.endif
 .endif # PERL5_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-perl
