4d5bc8e81cd11fed459e3e54376a6b1efd692b2f
asau <asau@pkgsrc.org>
2010-08-21 17:09:09 +0000

    Adjust buildlink3.mk after update.

--- lang/librep/buildlink3.mk
+++ lang/librep/buildlink3.mk
@@ -1,17 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.15 2010/03/24 21:54:16 asau Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2010/08/21 17:09:09 asau Exp $
 
 BUILDLINK_TREE+=	librep
 
 .if !defined(LIBREP_BUILDLINK3_MK)
 LIBREP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.librep+=	librep>=0.90.0
-BUILDLINK_ABI_DEPENDS.librep?=	librep>=0.90.0nb1
+BUILDLINK_API_DEPENDS.librep+=	librep>=0.90.5
 BUILDLINK_PKGSRCDIR.librep?=	../../lang/librep
 MAKE_FLAGS+=			rep_LIBTOOL=${LIBTOOL:Q}
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/gmp/buildlink3.mk"
+.include "../../devel/libffi/buildlink3.mk"
 .endif # LIBREP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-librep
