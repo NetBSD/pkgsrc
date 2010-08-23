0c1bf65f2037c7e958bf172beaabfbe48a3cba79
taca <taca@pkgsrc.org>
2010-08-23 14:41:48 +0000

    Update contao29-translations package to 20100823.
    Update Danish, French, Croatian, Hungarian, Japanese and Turkish
    language files and add Finnish language files.

--- www/contao29-translations/options.mk
+++ www/contao29-translations/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.5 2010/08/10 15:43:55 taca Exp $
+# $NetBSD: options.mk,v 1.6 2010/08/23 14:41:48 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.contao-translations
 
-# Not yet ready for Contao 2.9: af ca fi id no pt sk sq sr th tl zh
+# Not yet ready for Contao 2.9: af ca id no pt sk sq sr th tl zh
 #
-CT_SUPPORTED_LANGUAGES=	bg bs cs da es fa fr gl gn hr hu is it ja \
+CT_SUPPORTED_LANGUAGES=	bg bs cs da es fa fi fr gl gn hr hu is it ja \
 			lt lv nl pl ro ru sl sv tr uk
 
 CT_TRANSLATIONS.af=	TYPOlight-Afrikaans-v1.zip	# 2008/01/15
@@ -12,19 +12,19 @@ CT_TRANSLATIONS.bg=	TYPOlight-Bulgarian-v18.zip	# 2010/02/18
 CT_TRANSLATIONS.bs=	TYPOlight-Bosnian-v10.zip	# 2010/03/18
 CT_TRANSLATIONS.ca=	TYPOlight-Catalan-v4.zip	# 2008/09/24
 CT_TRANSLATIONS.cs=	TYPOlight-Czech-v24.zip		# 2010/07/08
-CT_TRANSLATIONS.da=	TYPOlight-Danish-v11.zip	# 2010/07/26
+CT_TRANSLATIONS.da=	TYPOlight-Danish-v12.zip	# 2010/08/16
 CT_TRANSLATIONS.es=	TYPOlight-Spanish-v14.zip	# 2010/06/10
 CT_TRANSLATIONS.fa=	TYPOlight-Persian-v5.zip	# 2010/07/06
-CT_TRANSLATIONS.fi=	TYPOlight-Finnish-v1.zip	# 2009/11/03
-CT_TRANSLATIONS.fr=	TYPOlight-French-v57.zip	# 2010/07/19
+CT_TRANSLATIONS.fi=	TYPOlight-Finnish-v2.zip	# 2010/08/22
+CT_TRANSLATIONS.fr=	TYPOlight-French-v59.zip	# 2010/08/13
 CT_TRANSLATIONS.gl=	TYPOlight-Galician-v3.zip	# 2010/03/26
 CT_TRANSLATIONS.gn=	TYPOlight-Guarani-v2.zip	# 2010/08/09
-CT_TRANSLATIONS.hr=	TYPOlight-Croatian-v9.zip	# 2010/07/20
-CT_TRANSLATIONS.hu=	TYPOlight-Hungarian-v6.zip	# 2010/08/09
+CT_TRANSLATIONS.hr=	TYPOlight-Croatian-v11.zip	# 2010/08/17
+CT_TRANSLATIONS.hu=	TYPOlight-Hungarian-v7.zip	# 2010/08/12
 CT_TRANSLATIONS.id=	TYPOlight-Indonesian-v1.zip	# 2009/01/13
 CT_TRANSLATIONS.is=	TYPOlight-Icelandic-v5.zip	# 2010/02/10
 CT_TRANSLATIONS.it=	TYPOlight-Italian-v25.zip	# 2010/08/09
-CT_TRANSLATIONS.ja=	TYPOlight-Japanese-v33.zip	# 2010/07/19
+CT_TRANSLATIONS.ja=	TYPOlight-Japanese-v35.zip	# 2010/08/12
 CT_TRANSLATIONS.lt=	TYPOlight-Lithuanian-v4.zip	# 2010/04/14
 CT_TRANSLATIONS.lv=	TYPOlight-Latvian-v25.zip	# 2010/07/15
 CT_TRANSLATIONS.nl=	TYPOlight-Dutch-v47.zip		# 2010/08/09
@@ -40,7 +40,7 @@ CT_TRANSLATIONS.sr=	TYPOlight-Serbian-v7.zip	# 2010/05/10
 CT_TRANSLATIONS.sv=	TYPOlight-Swedish-v53.zip	# 2010/08/09
 CT_TRANSLATIONS.th=	TYPOlight-Thai-v2.zip		# 2008/11/23
 CT_TRANSLATIONS.tl=	TYPOlight-Tagalog-v1.zip	# 2010/01/29
-CT_TRANSLATIONS.tr=	TYPOlight-Turkish-v11.zip	# 2010/06/14
+CT_TRANSLATIONS.tr=	TYPOlight-Turkish-v12.zip	# 2010/08/23
 CT_TRANSLATIONS.uk=	TYPOlight-Ukrainian-v14.zip	# 2010/07/02
 CT_TRANSLATIONS.zh=	TYPOlight-Chinese-v20.zip	# 2010/01/29
 
