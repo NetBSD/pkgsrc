0442ecc269a077afcefd51f5916dc9871fed4680
adam <adam>
2010-08-26 17:40:58 +0000

    Changes 1.44.0: New Libraries * Meta State Machine: High-performance expressive UML2 finite state machines,   from Christophe Henry * Polygon: Booleans/clipping, resizing/offsetting and more for planar polygons   with integral coordinates, from Lucanus Simonson. Updated Libraries * Accumulators * Asio * Config * Filesystem * Foreach * Hash * Iostreams * Math * MPL * Multi-index Containers * Proto * Regex * Thread * TR1 * Type Traits * uBLAS * Utility * Uuid * Xpressive

--- devel/boost-jam/buildlink3.mk
+++ devel/boost-jam/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2010/05/15 05:42:23 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2010/08/26 17:40:58 adam Exp $
 
 BUILDLINK_TREE+=	boost-jam
 
 .if !defined(BOOST_JAM_BUILDLINK3_MK)
 BOOST_JAM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.43.*
+BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.44.*
 BUILDLINK_DEPMETHOD.boost-jam?=		build
 BUILDLINK_PKGSRCDIR.boost-jam?=		../../devel/boost-jam
 .endif # BOOST_JAM_BUILDLINK3_MK
