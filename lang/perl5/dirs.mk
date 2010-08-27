9c7eaa3e6e5cf604993b5cd25e337b7e51c91e9e
sno <sno>
2010-08-27 05:27:37 +0000

    Updating lang/perl5 from 5.12.1 to 5.12.1nb1
    pkgsrc changes:
    - preparing for databases/p5-gdbm (using Makefile.common)
    - supporting external access to perl install dirs (dirs.mk)
    - fixing some errors/warnings from pkglint
    - adding conflict to packages of CPAN modules which are in Perl5 code
      on the one hand and are broken with perl-5.10+
    
    Bumping PKGREVISION (dependencies changed, rebuild highly recommended).
    Recursive rebuild is not required.

---
+++ lang/perl5/dirs.mk
@@ -0,0 +1,39 @@
+# $NetBSD: dirs.mk,v 1.1 2010/08/27 05:27:37 sno Exp $
+#
+# Provide typical required Perl directories
+
+.if !defined(_PERL5_DIRS_MK)
+_PERL5_DIRS_MK=	# defined
+
+.include "../../mk/bsd.prefs.mk"
+
+PERL5_PREFIX=		${PREFIX}
+PERL5_VENDORPREFIX=	${PERL5_PREFIX}
+PERL5_SITEPREFIX?=	${PERL5_PREFIX}
+BUILD_DEFS+=		PERL5_SITEPREFIX
+
+PERL5=			${PERL5_PREFIX}/bin/perl
+PERL5_PERLBASE=		${PERL5_PREFIX}/lib/perl5
+PERL5_VENDORBASE=	${PERL5_VENDORPREFIX}/lib/perl5/vendor_perl
+#
+# The unusual check for empty(PERL5_SITEPREFIX) is because ${PREFIX} isn't
+# defined until bsd.pkg.mk is included, so when we inspect its value at
+# this point, it's empty.
+#
+.if empty(PERL5_SITEPREFIX) || \
+    !empty(PERL5_SITEPREFIX:M${LOCALBASE}) || \
+    !empty(PERL5_SITEPREFIX:M${LOCALBASE}/*)
+PERL5_SITEBASE=		${PERL5_SITEPREFIX}/lib/perl5/site_perl
+.else
+PERL5_SITEBASE=		${PERL5_SITEPREFIX}
+.endif
+
+PERL5_PRIVLIB=		${PERL5_PERLBASE}/${PERL5_API_VERS}
+PERL5_VENDORLIB=	${PERL5_VENDORBASE}/${PERL5_API_VERS}
+.if !empty(PERL5_SITEBASE:M*lib/perl5/site_perl)
+PERL5_SITELIB=		${PERL5_SITEBASE}/${PERL5_API_VERS}
+.else
+PERL5_SITELIB=		${PERL5_SITEBASE}/lib/perl5/site_perl/${PERL5_API_VERS}
+.endif
+
+.endif
