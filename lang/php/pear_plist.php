fca356e728ee8423aa52ac3952372614aaaac99e
obache <obache@pkgsrc.org>
2010-08-29 06:08:07 +0000

    Improve pear package handling * support packages from other than default channels * replace deprecated function usage in pear_plist.php * generate filelist from installed registry, to support "install as" files.
    tested with all pear pacakgs in pkgsrc.

--- lang/php/pear_plist.php
+++ lang/php/pear_plist.php
@@ -1,58 +1,32 @@
 <?php
-# $NetBSD: pear_plist.php,v 1.7 2009/06/14 22:58:02 joerg Exp $
+# $NetBSD: pear_plist.php,v 1.8 2010/08/29 06:08:07 obache Exp $
 # Parses package XML file and outputs appropriate PLIST
 
+include_once "PEAR/Registry.php";
+include_once "PEAR/PackageFile.php";
+
+$PREFIX = getenv('PREFIX');
 $PEAR_LIB = getenv('PEAR_LIB');
 $WRKSRC = getenv('WRKSRC');
-$PEAR_DIRRM_BASEDIR = getenv('PEAR_DIRRM_BASEDIR');
-$PEAR_DIRRM_EXCLUDE = getenv('PEAR_DIRRM_EXCLUDE');
-$dirrm = array();
+if(! $DESTDIR = getenv('DESTDIR')) $DESTDIR='';
 
-include_once "PEAR/Common.php";
-$obj = &new PEAR_Common;
-$info = $obj->infoFromAny("$WRKSRC/package.xml");
+$config = &PEAR_Config::singleton();
+$package = &new PEAR_PackageFile($config);
+$info = $package->fromAnyFile("$WRKSRC/package.xml", PEAR_VALIDATE_INSTALLING);
 
-if (!empty($info['attribs']) && $info['attribs']['version'] == '2.0')
-	$pkg = $info['name'];
-else
-	$pkg = $info['package'];
+$pkg = $info->getName();
+$channel = $info->getChannel();
 
+$registry = &new PEAR_Registry($DESTDIR.$PREFIX."/".$PEAR_LIB);
+$flist = $registry->packageInfo($pkg, 'filelist', $channel);
+
+$regfile = $PEAR_LIB.'/.registry/.channel.'.$channel.'/'.strtolower($pkg).'.reg';
+if (!file_exists($DESTDIR.$PREFIX.'/'.$regfile)) {
+	$regfile = $PEAR_LIB.'/.registry/'.strtolower($pkg).'.reg';
+}
+echo "$regfile\n";
 # output list of package files, in same order as specified in package
-echo "$PEAR_LIB/.registry/".strtolower($pkg).".reg\n";
-foreach($info['filelist'] as $f => $v) {
-	switch($v['role']) {
-	case 'test':
-	case 'doc':
-	case 'data':
-		$prefix = "$v[role]/$pkg/";
-		$dirrm["$v[role]/$pkg"] = true;
-		break;
-
-	case 'php':
-	default:
-		if (!empty($v['baseinstalldir']) && $v['baseinstalldir'] != '/') {
-			$prefix = $v['baseinstalldir'] . '/';
-
-			# sometimes the baseinstalldir begins with a slash,
-			# which make the PLIST output to have two instead of
-			# one.  We fix this here.
-			if ($prefix[0] == '/')
-				$prefix = substr($prefix, 1);
-
-			if ($PEAR_DIRRM_BASEDIR)
-				$dirrm[$v['baseinstalldir']] = true;
-		} else
-			$prefix = '';
-		break;
-	}
-
-	# replace backslashes with forward slashes in the path name, for
-	# pear packages written by non-UNIX oriented authors.
-	$f = str_replace('\\', '/', $f);
-
-	echo "{$PEAR_LIB}/{$prefix}{$f}\n";
-
-	while(($f = dirname($f)) && $f != '.')
-		$dirrm["{$prefix}{$f}"] = true;
+foreach($flist as $f) {
+	echo str_replace($PREFIX.'/','', $f['installed_as'])."\n";
 }
 ?>
