fb2c7b61178472451433cace35bcbdfa1209e8da
drochner <drochner>
2010-08-30 17:19:49 +0000

    propagate libsoup dependency

--- multimedia/totem-pl-parser/buildlink3.mk
+++ multimedia/totem-pl-parser/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2010/07/06 15:22:23 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2010/08/30 17:19:49 drochner Exp $
 
 BUILDLINK_TREE+=	totem-pl-parser
 
@@ -13,6 +13,7 @@ BUILDLINK_PKGSRCDIR.totem-pl-parser?=	../../multimedia/totem-pl-parser
 .include "../../x11/gtk2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../mail/gmime24/buildlink3.mk"
+.include "../../net/libsoup24/buildlink3.mk"
 .endif # TOTEM_PL_PARSER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-totem-pl-parser
