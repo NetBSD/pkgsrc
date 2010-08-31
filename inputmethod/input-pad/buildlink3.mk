84ce20ee862b1b58b1100feca6c5802226f2e299
obache <obache@pkgsrc.org>
2010-08-31 08:25:20 +0000

    Import input-pad-0.1.1 as inputmethod/input-pad.
    The input pad is a tool to send a character on button to text applications.

---
+++ inputmethod/input-pad/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/08/31 08:25:20 obache Exp $
+#
+
+BUILDLINK_TREE+=	input-pad
+
+.if !defined(INPUT_PAD_BUILDLINK3_MK)
+INPUT_PAD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.input-pad+=	input-pad>=0.1.1
+BUILDLINK_PKGSRCDIR.input-pad?=	../../inputmethod/input-pad
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libxkbfile/buildlink3.mk"
+.include "../../x11/libxklavier/buildlink3.mk"
+.endif	# INPUT_PAD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-input-pad
