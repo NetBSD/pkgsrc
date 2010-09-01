d3b27d88df8c779301847e88af49583a90ba9e31
wiz <wiz@pkgsrc.org>
2010-09-01 20:41:45 +0000

    Initial import of libvdpau-0.4, packaged by Aleksey Cheusov for wip.
    The Video Decode and Presentation API for Unix (VDPAU) provides a complete
    solution for decoding, post-processing, compositing, and displaying
    compressed or uncompressed video streams. These video streams may be
    combined (composited) with bitmap content, to implement OSDs and other
    application user interfaces.
    
    This VDPAU API allows video programs to offload portions of the video
    decoding process and video post-processing to the GPU video-hardware.

---
+++ multimedia/libvdpau/buildlink3.mk
@@ -0,0 +1,26 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/09/01 20:41:45 wiz Exp $
+
+#
+# For linking use LDFLAGS.vdpau variable
+#
+
+# Check whether VDPAU is available on this platform
+.include "../../wip/libvdpau/available.mk"
+.if ${VDPAU_AVAILABLE} == "yes"
+
+BUILDLINK_TREE+=	libvdpau
+
+.  if !defined(LIBVDPAU_BUILDLINK3_MK)
+LIBVDPAU_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libvdpau+=	libvdpau>=0.3
+BUILDLINK_PKGSRCDIR.libvdpau?=	../../wip/libvdpau
+
+.include "../../x11/libX11/buildlink3.mk"
+.  endif	# LIBVDPAU_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libvdpau
+
+LDFLAGS.vdpau=		-lvdpau
+
+.endif # Linux, FreeBSD, SunOS...
