9267642d9a6de728510d669b8e634e8460846d30
drochner <drochner@pkgsrc.org>
2010-09-02 09:18:10 +0000

    update to 4.11 changes: added the %UnitPostIncludeCode directive /KeepReference/ will now accept a key to track when the same object  is passed to more than one method operator() and __call__() can now accept keyword arguments added support for Python v3.2
    pkgsrc note: runtime API changed, client pkgs need to be rebuilt

--- x11/py-sip/buildlink3.mk
+++ x11/py-sip/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2010/02/04 18:34:43 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2010/09/02 09:18:10 drochner Exp $
 
 BUILDLINK_TREE+=	py-sip
 
@@ -7,7 +7,7 @@ PY_SIP_BUILDLINK3_MK:=
 
 .include "../../lang/python/pyversion.mk"
 
-BUILDLINK_API_DEPENDS.py-sip+=	${PYPKGPREFIX}-sip>=4.10
+BUILDLINK_API_DEPENDS.py-sip+=	${PYPKGPREFIX}-sip>=4.11
 BUILDLINK_PKGSRCDIR.py-sip?=	../../x11/py-sip
 .endif # PY_SIP_BUILDLINK3_MK
 
