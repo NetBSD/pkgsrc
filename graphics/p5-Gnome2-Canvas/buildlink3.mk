266291a34c3f62dc8c980da6700d3e2e22ad4999
wiz <wiz@pkgsrc.org>
2010-09-03 22:38:19 +0000

    Initial import of p5-Gnome2-Canvas-1.002:
    The Gnome2::Canvas module allows a perl developer to use the
    GnomeCanvas widget with Gtk2-Perl.
    
    Like the Gtk2 module on which it depends, Gnome2::Canvas follows
    the C API of libgnomecanvas-2.0 as closely as possible while still
    being perlish.  Thus, the C API reference remains the canonical
    documentation.

---
+++ graphics/p5-Gnome2-Canvas/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/09/03 22:38:19 wiz Exp $
+
+BUILDLINK_TREE+=	p5-Gnome2-Canvas
+
+.if !defined(P5_GNOME2_CANVAS_BUILDLINK3_MK)
+P5_GNOME2_CANVAS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.p5-Gnome2-Canvas+=	p5-Gnome2-Canvas>=1.002
+BUILDLINK_PKGSRCDIR.p5-Gnome2-Canvas?=		../../graphics/p5-Gnome2-Canvas
+
+.include "../../graphics/libgnomecanvas/buildlink3.mk"
+.include "../../x11/p5-gtk2/buildlink3.mk"
+.endif # P5_GNOME2_CANVAS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-p5-Gnome2-Canvas
