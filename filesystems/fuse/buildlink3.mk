1f2ab39faf6af014a4bff769fd6e5ac35dce262c
wiz <wiz>
2010-09-06 10:30:43 +0000

    On NetBSD, pull in perfuse.

--- filesystems/fuse/buildlink3.mk
+++ filesystems/fuse/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:24:33 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2010/09/06 10:30:43 wiz Exp $
 
 BUILDLINK_TREE+=	fuse
 
@@ -7,6 +7,12 @@ FUSEFS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.fuse+=	fuse>=2.5.2
 BUILDLINK_PKGSRCDIR.fuse?=	../../filesystems/fuse
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} == "NetBSD"
+.include "../../filesystems/perfuse/buildlink3.mk"
+.endif
 .endif # FUSEFS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-fuse
