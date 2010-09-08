c77fa7d73f3ea10510cc3b6271b11648e2d879d2
drochner <drochner@pkgsrc.org>
2010-09-08 11:53:04 +0000

    back out update to the API incompatible 2.4 branch -- there are still users of the 2.0 API, and mail/gmome24 has been there before

--- mail/gmime/buildlink3.mk
+++ mail/gmime/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.13 2010/09/07 19:04:15 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2010/09/08 11:53:04 drochner Exp $
 
 BUILDLINK_TREE+=	gmime
 
 .if !defined(GMIME_BUILDLINK3_MK)
 GMIME_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gmime+=	gmime>=2.4.0
+BUILDLINK_API_DEPENDS.gmime+=	gmime>=2.1.7
 BUILDLINK_PKGSRCDIR.gmime?=	../../mail/gmime
 
 .include "../../devel/glib2/buildlink3.mk"
