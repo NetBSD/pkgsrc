a26b834954b272458310413be1ceb711aa3d3ebb
drochner <drochner@pkgsrc.org>
2010-09-08 16:45:40 +0000

    update to 0.98.11 changes: -minor fixes -translation updates

--- multimedia/dvdrip/options.mk
+++ multimedia/dvdrip/options.mk
@@ -1,11 +1,11 @@
-# $NetBSD: options.mk,v 1.1 2007/08/23 17:10:39 abs Exp $
+# $NetBSD: options.mk,v 1.2 2010/09/08 16:45:40 drochner Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dvdrip
 .if !empty(MACHINE_ARCH:Mi386)
 PKG_SUPPORTED_OPTIONS=	rar
 
-PKG_SUGGESTED_OPTIONS=	rar
+#PKG_SUGGESTED_OPTIONS=	rar
 .endif
 
 .include "../../mk/bsd.options.mk"
