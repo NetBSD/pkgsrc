6a15ee56b6629bc7c81aec1e8665829b7a9847ca
taca <taca@pkgsrc.org>
2010-09-10 03:29:00 +0000

    Update ruby18-base to 1.8.7.302 (Ruby 1.8.7 patchlevel 302).
    Since many changes from previous release, please refer
    http://www.ruby-lang.org/en/news/2010/08/16/ruby-1-8-7-p302-is-released/.
    
    Note: Since all security updates are already in previous package,
    This update dosen't include any securify fix.

--- lang/ruby18-base/options.mk
+++ lang/ruby18-base/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2009/09/22 13:32:35 taca Exp $
+# $NetBSD: options.mk,v 1.8 2010/09/10 03:29:00 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ruby
 PKG_SUPPORTED_OPTIONS=	readline ruby-build-ri-db
@@ -9,7 +9,6 @@ PKG_SUGGESTED_OPTIONS=	readline ruby-build-ri-db
 .if !empty(PKG_OPTIONS:Mruby-build-ri-db)
 BUILD_TARGET=	all rdoc
 CONFIGURE_ARGS+=	--enable-install-doc
-PRIVILEGED_STAGES+=    clean
 RUBY_DYNAMIC_DIRS+=	${RUBY_SYSRIDIR}
 # Use huge memory.
 UNLIMIT_RESOURCES+=	datasize
