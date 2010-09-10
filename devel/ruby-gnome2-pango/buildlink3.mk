f57ed7abab7b113c58a5a10a963f8a8276a2881c
taca <taca@pkgsrc.org>
2010-09-10 05:47:23 +0000

    Allow depending "ruby19-" package.

--- devel/ruby-gnome2-pango/buildlink3.mk
+++ devel/ruby-gnome2-pango/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2010/06/13 22:44:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2010/09/10 05:47:23 taca Exp $
 
 BUILDLINK_TREE+=	ruby-gnome2-pango
 
 .if !defined(RUBY_GNOME2_PANGO_BUILDLINK3_MK)
 RUBY_GNOME2_PANGO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ruby-gnome2-pango+=	${RUBY_PKGPREFIX}-gnome2-pango>=0.16.0
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-pango?=	ruby18-gnome2-pango>=0.19.4nb1
+BUILDLINK_API_DEPENDS.ruby-gnome2-pango+= ${RUBY_PKGPREFIX}-gnome2-pango>=0.16.0
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-pango?= ${RUBY_PKGPREFIX}-gnome2-pango>=0.19.4nb1
 BUILDLINK_PKGSRCDIR.ruby-gnome2-pango?=	../../devel/ruby-gnome2-pango
 
 .include "../../devel/ruby-gnome2-glib/buildlink3.mk"
