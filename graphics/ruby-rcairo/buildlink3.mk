586afde5b1d99f0fd164f82b523c10c7a52223e7
taca <taca@pkgsrc.org>
2010-09-10 06:55:02 +0000

    Update graphics/ruby-rcairo to 1.8.5.
    * Allow graphics/ruby-rcairo/buildlink3.mk to specify "ruby19-" package.
    
    win32 changes only and no change for pkgsrc.

--- graphics/ruby-rcairo/buildlink3.mk
+++ graphics/ruby-rcairo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2010/06/13 22:44:44 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2010/09/10 06:55:02 taca Exp $
 
 BUILDLINK_TREE+=	ruby-rcairo
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ruby-rcairo
 RUBY_RCAIRO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-rcairo+=	${RUBY_PKGPREFIX}-rcairo>=1.6.0
-BUILDLINK_ABI_DEPENDS.ruby-rcairo?=	ruby18-rcairo>=1.8.1nb2
+BUILDLINK_ABI_DEPENDS.ruby-rcairo?=	${RUBY_PKGPREFIX}-rcairo>=1.8.1nb2
 BUILDLINK_PKGSRCDIR.ruby-rcairo?=	../../graphics/ruby-rcairo
 
 .include "../../graphics/cairo/buildlink3.mk"
