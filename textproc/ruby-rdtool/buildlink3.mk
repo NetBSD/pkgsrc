98f007ed0bed44bef72ae49c17fa907380a41099
taca <taca@pkgsrc.org>
2010-09-10 08:35:16 +0000

    * Use lang/ruby/gem.mk instead of misc/rubygems/rubygem.mk. * Add LICENSE. * Tweak CONFLICTS pattern. * Make buildlink3.mk support ruby18 and ruby19.

--- textproc/ruby-rdtool/buildlink3.mk
+++ textproc/ruby-rdtool/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:25:32 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2010/09/10 08:35:16 taca Exp $
 
 BUILDLINK_TREE+=	${RUBY_PKGPREFIX}-rdtool
 
@@ -10,12 +10,13 @@ RUBY_RD_VERSION =	0.6.18
 RUBY_RD_REQD=		0.6.14
 
 # create string for dependency list
-RUBY_RD_LIST=	${RUBY_VERSION_LIST:C/([1-9][0-9]*)/ruby&-rdtool/g}
+#_RUBY_RD_LIST:=	${RUBY_VERSION_LIST:C/([1-9][0-9]*)/ruby&-rdtool/g:ts,}
 
-BUILDLINK_FILES.ruby18-rdtool+=		bin/rd2
-BUILDLINK_DEPMETHOD.ruby18-rdtool?=	build
-BUILDLINK_API_DEPENDS.ruby18-rdtool+=	{${RUBY_RD_LIST}}>=0.6.14
-BUILDLINK_PKGSRCDIR.ruby18-rdtool?=	../../textproc/ruby-rdtool
+BUILDLINK_FILES.${RUBY_PKGPREFIX}-rdtool+=	bin/rd2
+BUILDLINK_DEPMETHOD.${RUBY_PKGPREFIX}-rdtool?=	build
+BUILDLINK_API_DEPENDS.${RUBY_PKGPREFIX}-rdtool+=${RUBY_PKGPREFIX}-rdtool>=0.6.14
+BUILDLINK_PKGSRCDIR.${RUBY_PKGPREFIX}-rdtool?=	../../textproc/ruby-rdtool
+
 .endif # RUBY_RDTOOL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-${RUBY_PKGPREFIX}-rdtool
