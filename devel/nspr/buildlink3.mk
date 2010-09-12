9efd60400497901e805c349919a4dfb00a114b3a
tnn <tnn@pkgsrc.org>
2010-09-12 09:11:49 +0000

    Bump minimum dependency (PR pkg/43867)

--- devel/nspr/buildlink3.mk
+++ devel/nspr/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.13 2010/01/20 17:24:57 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2010/09/12 09:11:49 tnn Exp $
 
 BUILDLINK_TREE+=	nspr
 
 .if !defined(NSPR_BUILDLINK3_MK)
 NSPR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.8.2.5
+BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.8.6
 BUILDLINK_PKGSRCDIR.nspr?=	../../devel/nspr
 
 BUILDLINK_FILES.nspr+=          lib/nspr/*
