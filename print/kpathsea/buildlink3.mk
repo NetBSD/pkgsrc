341341e73a3b8f907b26d2dd1cc977afbd958146
minskim <minskim>
2010-09-13 04:06:12 +0000

    Update kpathsea to 6.0.0.
    Changes:
    - Added texhash.
    - Changed some function names.

--- print/kpathsea/buildlink3.mk
+++ print/kpathsea/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/06/09 21:29:17 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2010/09/13 04:06:12 minskim Exp $
 
 BUILDLINK_TREE+=	kpathsea
 
 .if !defined(KPATHSEA_BUILDLINK3_MK)
 KPATHSEA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kpathsea+=	kpathsea>=5.0.0
+BUILDLINK_API_DEPENDS.kpathsea+=	kpathsea>=6.0.0
 BUILDLINK_PKGSRCDIR.kpathsea?=	../../print/kpathsea
 BUILDLINK_FILES.kpathsea+=	bin/kpsewhich
 .endif # KPATHSEA_BUILDLINK3_MK
