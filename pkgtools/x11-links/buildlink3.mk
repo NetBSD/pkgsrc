14103ad89a1be27d85649be5e1530d6929810afa
tron <tron@pkgsrc.org>
2010-09-14 08:55:42 +0000

    Update "x11-links" package to version 0.55: - Add "pthread-stubs.pc" which is required for "libxcb" under Mac OS X.

--- pkgtools/x11-links/buildlink3.mk
+++ pkgtools/x11-links/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.31 2010/09/11 20:26:11 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2010/09/14 08:55:42 tron Exp $
 #
 # Don't include this file manually!  It will be included as necessary
 # by bsd.buildlink3.mk.
@@ -12,7 +12,7 @@ BUILDLINK_TREE+=	x11-links
 .if !defined(X11_LINKS_BUILDLINK3_MK)
 X11_LINKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.54
+BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.55
 BUILDLINK_PKGSRCDIR.x11-links?=	../../pkgtools/x11-links
 BUILDLINK_DEPMETHOD.x11-links?=	build
 
