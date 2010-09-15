322f80649d38cab8b825269bb0a6956441009eab
drochner <drochner@pkgsrc.org>
2010-09-15 18:00:17 +0000

    do missing dependency bump for glitz removal

--- x11/libgnomekbd/buildlink3.mk
+++ x11/libgnomekbd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2010/07/19 12:05:33 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2010/09/15 18:00:17 drochner Exp $
 
 BUILDLINK_TREE+=	libgnomekbd
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libgnomekbd
 LIBGNOMEKBD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libgnomekbd+=	libgnomekbd>=2.18.0
-BUILDLINK_ABI_DEPENDS.libgnomekbd?=	libgnomekbd>=2.30.2
+BUILDLINK_ABI_DEPENDS.libgnomekbd?=	libgnomekbd>=2.30.2nb1
 BUILDLINK_PKGSRCDIR.libgnomekbd?=	../../x11/libgnomekbd
 
 .include "../../x11/libxklavier/buildlink3.mk"
