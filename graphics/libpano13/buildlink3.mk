ba762e169b4e4a5e8ebb91f20e3a456ff9d99a18
adam <adam@pkgsrc.org>
2010-09-15 10:13:26 +0000

    Changes 2.9.17: * Bug-fix release.

--- graphics/libpano13/buildlink3.mk
+++ graphics/libpano13/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2010/06/13 22:44:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2010/09/15 10:13:26 adam Exp $
 
 BUILDLINK_TREE+=	libpano13
 
 .if !defined(LIBPANO13_BUILDLINK3_MK)
 LIBPANO13_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libpano13+=	libpano13>=2.9.14
-BUILDLINK_ABI_DEPENDS.libpano13?=		libpano13>=2.9.14nb3
+BUILDLINK_API_DEPENDS.libpano13+=	libpano13>=2.9.17
+BUILDLINK_ABI_DEPENDS.libpano13+=	libpano13>=2.9.17
 BUILDLINK_PKGSRCDIR.libpano13?=		../../graphics/libpano13
 
 .include "../../graphics/jpeg/buildlink3.mk"
