02039d24080ad92c7b877ae0479b7deac36bde1e
obache <obache@pkgsrc.org>
2010-09-17 07:11:41 +0000

    Share distfile related information for each python variants with srcdist.mk to make update simplicity.

--- lang/python/srcdist.mk
+++ lang/python/srcdist.mk
@@ -1,38 +1,11 @@
-# $NetBSD: srcdist.mk,v 1.33 2010/09/04 15:31:29 tron Exp $
+# $NetBSD: srcdist.mk,v 1.34 2010/09/17 07:11:41 obache Exp $
 
 .include "../../lang/python/pyversion.mk"
 
-.if ${_PYTHON_VERSION} == "26"
+.sinclude "${.CURDIR}/${PYPKGSRCDIR}/dist.mk"
 
-DISTNAME=	Python-2.6.6
-EXTRACT_SUFX=	.tar.bz2
-DISTINFO_FILE=	${.CURDIR}/../../lang/python26/distinfo
-PATCHDIR=	${.CURDIR}/../../lang/python26/patches
-PYSUBDIR=	Python-2.6.6
+PYSUBDIR=	${DISTNAME}
 WRKSRC=		${WRKDIR}/${PYSUBDIR}
-MASTER_SITES=	ftp://ftp.python.org/pub/python/2.6/
-
-.elif ${_PYTHON_VERSION} == "25"
-
-DISTNAME=	Python-2.5.5
-EXTRACT_SUFX=	.tar.bz2
-DISTINFO_FILE=	${.CURDIR}/../../lang/python25/distinfo
-PATCHDIR=	${.CURDIR}/../../lang/python25/patches
-PYSUBDIR=	Python-2.5.5
-WRKSRC=		${WRKDIR}/${PYSUBDIR}
-MASTER_SITES=	ftp://ftp.python.org/pub/python/2.5/
-
-.elif ${_PYTHON_VERSION} == "24"
-
-DISTNAME=	Python-2.4.5
-EXTRACT_SUFX=	.tar.bz2
-DISTINFO_FILE=	${.CURDIR}/../../lang/python24/distinfo
-PATCHDIR=	${.CURDIR}/../../lang/python24/patches
-PYSUBDIR=	Python-2.4.5
-WRKSRC=		${WRKDIR}/${PYSUBDIR}
-MASTER_SITES=	ftp://ftp.python.org/pub/python/2.4/
-
-.endif
 
 .if defined(PYDISTUTILSPKG)
 # This is used for standard modules shipped with Python but build as
