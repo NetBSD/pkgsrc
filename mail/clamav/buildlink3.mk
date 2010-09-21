3891d9fc7c58e8e46ef58d5903e1b0ae421688ce
adam <adam>
2010-09-21 07:47:25 +0000

    Changes 0.96.3: This release fixes problems with the PDF parser and the internal bzip2 library. A complete list of changes is available in the Changelog file.

--- mail/clamav/buildlink3.mk
+++ mail/clamav/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2010/03/24 21:54:16 asau Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2010/09/21 07:47:25 adam Exp $
 
 BUILDLINK_TREE+=	clamav
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	clamav
 CLAMAV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.clamav+=	clamav>=0.95.3nb1
-BUILDLINK_ABI_DEPENDS.clamav?=	clamav>=0.95.3nb2
+BUILDLINK_ABI_DEPENDS.clamav+=	clamav>=0.95.3nb2
 BUILDLINK_PKGSRCDIR.clamav?=	../../mail/clamav
 
 .include "../../archivers/bzip2/buildlink3.mk"
