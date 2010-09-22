62ec87cf828e853dc9fbf1dfe54d248d92701620
obache <obache@pkgsrc.org>
2010-09-22 09:33:41 +0000

    Update mlterm to 3.0.1. Based on PR#43822 by YAMASHIRO, Jun. Additional clean up by me.
    pkgsrc changes:
    * set LICENSE=modified-bsd (with uim option, gnu-lgpl-v2 additionally).
    * add m17nlib option.
    
    ver 3.0.1
    * Improvements for performance of changing 256 colors by
      "\x1b]4;N;rgb:RR/GG/BB\x07" sequence. (Thanks to Konosuke Watanabe san)
    * Support "ESC[9Nm" and "ESC[10Nm" sequences.
    * Add --button3/button3_behavior option. (If "menu1" is specified, pressing
      button3 shows conf_menu_path_1 program (mlterm-menu) like gnome-terminal.)
    * Add "Copy" & "Paste" menus to mlterm-menu.
    * Improvements for compatibility with xterm about RGB of system colors.
    * Add 'col_size_of_width_a' to configuration protocol. (doc/en/PROTOCOL)
    * Merge SF patches:
      #2988801, #2987646 (Thanks to Ahmed El-Mahmoudy)
    * Other bug fixes:
      #2818015(partly),#2818018 (Thanks to Thomas Wolff)
    
    ver 3.0.0
    * Support 256 colors. (Thanks to Shinichiro Hamaji san)
    * Character convertion tables are separated from libmkf library.
    * Support win32(Windows 2000-XP) native GUI.
      ('--with-gui=win32' in mingw, msys and cygwin)
    * Support font and color configuration protocol.
      (doc/en/PROTOCOL.font,PROTOCOL.color)
    * Change 'aafont' format as follows. (Elements in parentheses are optional.)
      [Family]-[Encoding](:[Percentage])
      => [Family]( [Weight] [Slant] [Size]-[Encoding]:[Percentage])
    * Add 'DEFAULT' key which specifies fonts used by default
      for font configuration files.
    * Add --maxptys option.
    * Add 'mlclient', 'mlclientx', 'static_backscroll_mode' and 'type_engine' to
      configuration protocol. (doc/en/PROTOCOL)
    * Add 'mlclientx' command.
    * Merge SF patches -
      #1574764 (Thanks to Christian Biere)
      #        (Thanks to Frederic Heulin)
      #2879800 (Thanks to Kouichi Kusanagi san)
    * Other bug fixes:
      #1999220 (Thanks to Christian Biere)
    
    ver 2.9.4
    * Arabic translation of mlconfig [Khaled Hosny]
    * Selected region can be expanded by right-clicking [minami]
      (Thanks to ssakane)
    * New config key string "title" to allow change pty title [minami]
    * Set DISPLAY correctly for ptys opened by -P option [nokada] (SF Bug #1534313)
    * Fixed compile errors with gcc 4.3 [mfabian]
    * Fixed a potentiol off-by-one bug [mfabian] (Thanks to Dominique Leuenberger)
    * Fixed compile errors on biarch systems [Kenshi Muto] (Debian Bug #396532)
    * Plugged file-desctiptor leaks [Christian Biere] (SF Patch #1576044)
    * Made "restore cursor" work properly with alternate screens [minami]
      (SF Bug#1472143)
    * Fixed broken handling of Atom [Kusanagi Kouichi]
    * Other bug fixes:
     - Debian BUG #329194 [David Kolovratnik]
     - SF Bug #1568596 [nokada]
     - SF Patch #1574241 [Christian Biere]

--- x11/mlterm/options.mk
+++ x11/mlterm/options.mk
@@ -1,16 +1,24 @@
-# $NetBSD: options.mk,v 1.3 2008/04/12 22:43:14 jlam Exp $
+# $NetBSD: options.mk,v 1.4 2010/09/22 09:33:41 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mlterm
-PKG_SUPPORTED_OPTIONS=	uim xft2
+PKG_SUPPORTED_OPTIONS=	m17nlib uim xft2
 PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		uim
+PLIST_VARS+=		m17nlib uim
+
+.if !empty(PKG_OPTIONS:Mm17nlib)
+.include "../../devel/m17n-lib/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-m17nlib
+PLIST.m17nlib=		yes
+.endif
+
 .if !empty(PKG_OPTIONS:Muim)
 .include "../../inputmethod/uim/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-uim
 PLIST.uim=		yes
+LICENSE+=		AND gnu-lgpl-v2
 .endif
 
 .if !empty(PKG_OPTIONS:Mxft2)
