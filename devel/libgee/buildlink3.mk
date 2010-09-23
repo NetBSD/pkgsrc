b8768b495f0339fec39d2a4d8804922b62c78a70
obache <obache@pkgsrc.org>
2010-09-23 08:26:09 +0000

    Update libgee to 0.5.3.
    pkgsrc changes:
    * set LICENSE=gnu-lgpl-v2.1
    * drop dependency on vala, not used.
    * add TEST_TARGET.
    
    NEWS from upstream:
    libgee 0.5.3
    ============
    
     * Fix memory leaks in TimSort and LinkedList.
     * Bug fixes in TreeMap and TreeSet.
    
    libgee 0.5.2
    ============
    
     * Build system enhancements to conform Gnome's style.
     * Bug fixes in TreeSet, TreeMap and Collection.to_array.
     * Deprecated Map methods marked with [Deprecated].
    
    libgee 0.5.1
    ============
    
     * Bug fixes in HashMultiSet, TimSort, TreeMap, TreeMultiSet, and TreeSet.
     * Fix build with Vala 0.8.0 and later.
     * Fix build on Windows.
     * Fix subprocess handling in test infrastructure.
    
    libgee 0.5.0
    ============
    
     * API changes
       * Introduce Functions hash, equal and compare function factory
       * Introduce Comparable<G> interface
       * Implementations provides sane defaults when constructed without functions
       * ReadOnly* made internal in favor of a `read_only_view` properties
       * Iterator<G> is now mutable and resettable
       * Introduction of BidirIterator<G> bidirectional iterators
       * List<G>.list_iterator () returns a specialized ListIterator<G>
       * Various API contracts clarifications and enhancements
       * Introduce List<G>.sort () with TimSort implementation
       * Complete Map API rework
       * Introduce Queue and Deque interfaces
       * Introduce MultiSet and MultiMap interfaces (Ali Sabil)
       * Introduce SortedSet interface (Maciej Pietchotka)
    
     * New implementations
       * HashMultiSet and HashMultiMap (Ali Sabil)
       * TreeMultiSet and TreeMultiMap.
       * PriorityQueue
       * LinkedList now implements Deque
       * TreeSet now implements SortedSet (Maciej Pietchotka)
    
     * Infrastructure
       * New test framework and complete tests refactoring (Julien Peters)
       * New doc/ documentation directory (--enable-doc)
       * Test coverage analysis using lcov (--enable-coverage)
       * Basic benchmark framework and sort benchmark (--enable-benchmark)
       * Use silent rules by default for users of automake 1.11
    
     * Code Quality
       * Many bug fixes
       * Many optimizations
       * Lots of additional documentation
       * Better overall encapsulation, dangerous public setters have been removed
       * Better test coverage (more than 90%)
    
    libgee 0.4.0
    ============
    
     * Bug fixes.
    
    libgee 0.3.0
    ============
    
     * Introduce AbstractCollection, AbstractList and AbstractMap base classes.
     * Add new properties and methods to Collection, List and Map. (Tomaž Vajngerl)
     * Add LinkedList implementation. (Mark Lee)
     * Add TreeSet and TreeMap implementations. (Maciej Piechotka)
     * Iterable.element_type is now a property.
     * Map.remove now takes an optional parameter to retrieve the removed value.
     * Bump library version info to reflect API/ABI breakage
    
    libgee 0.2.0
    ============
    
     * Generate GIR file.
    
    libgee 0.1.6
    ============
    
     * Fix build for Vala 0.7.
     * Bug fixes.

--- devel/libgee/buildlink3.mk
+++ devel/libgee/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:18 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2010/09/23 08:26:09 obache Exp $
 
 BUILDLINK_TREE+=	libgee
 
 .if !defined(LIBGEE_BUILDLINK3_MK)
 LIBGEE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgee+=	libgee>=0.1.5
+BUILDLINK_API_DEPENDS.libgee+=	libgee>=0.5.3
 BUILDLINK_PKGSRCDIR.libgee?=	../../devel/libgee
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../lang/vala/buildlink3.mk"
 .endif # LIBGEE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libgee
