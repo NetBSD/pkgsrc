e59332d06c50b4edb70abcb0a6460631302b727f
tron <tron@pkgsrc.org>
2010-09-25 10:30:52 +0000

    Pullup ticket #3230 - requested by adam mail/clamav: security update
    Revisions pulled up:
    - mail/clamav/Makefile				1.104-1.105
    - mail/clamav/buildlink3.mk			1.24
    - mail/clamav/distinfo				1.65-1.67
    - mail/clamav/patches/patch-ac			1.9
    ---
    Module Name:	pkgsrc
    Committed By:	adam
    Date:		Thu Aug 26 05:49:30 UTC 2010
    
    Modified Files:
    	pkgsrc/mail/clamav: Makefile distinfo
    	pkgsrc/mail/clamav/patches: patch-ac
    
    Log Message:
    Changes 0.96.2:
    * contrib: add safe_clamd from Luca
    * freshclam: uses private symbol which changed proto, change name to prevent
      crash
    * libclamav: fix callback_sigload
    * clamdscan: fix parsing of virus names in extended mode and --stream
    * libclamav/c++/detect.cpp: Mac OS X can run 64-bit apps on 32-bit kernel
    * libclamav/others.h: bump f-level
    * sigtool/sigtool.c: fix handling of --datadir
    * libclamav/matcher-ac.c: improve offset handling
    * libclamav/7z/Archive/7z/7zDecode.c: shut up a warning
    * libclamav/autoit.c: properly resume from empty files regression introduced
      in ac867aad
    * libclamav/elf.c: fix zero mem alloc warning
    * win32: fix libclamav's triple and fix GetVersion
    * libclamav/bytecode.c: save lsig counts/offsets
    * libclamav/{bytecode,matcher}.c: matchicon API
    * libclamav/pe_icons.c: BE fixes
    * more...
    ---
    Module Name:	pkgsrc
    Committed By:	adam
    Date:		Thu Aug 26 06:02:07 UTC 2010
    
    Modified Files:
    	pkgsrc/mail/clamav: distinfo
    
    Log Message:
    patch-ac has been changed
    ---
    Module Name:	pkgsrc
    Committed By:	adam
    Date:		Tue Sep 21 07:47:26 UTC 2010
    
    Modified Files:
    	pkgsrc/mail/clamav: Makefile buildlink3.mk distinfo
    
    Log Message:
    Changes 0.96.3:
    This release fixes problems with the PDF parser and the internal bzip2 library.
    A complete list of changes is available in the Changelog file.

--- mail/clamav/buildlink3.mk
+++ mail/clamav/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2010/03/24 21:54:16 asau Exp $
+# $NetBSD: buildlink3.mk,v 1.23.4.1 2010/09/25 10:30:52 tron Exp $
 
 BUILDLINK_TREE+=	clamav
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	clamav
 CLAMAV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.clamav+=	clamav>=0.95.3nb1
-BUILDLINK_ABI_DEPENDS.clamav?=	clamav>=0.95.3nb2
+BUILDLINK_ABI_DEPENDS.clamav+=	clamav>=0.95.3nb2
 BUILDLINK_PKGSRCDIR.clamav?=	../../mail/clamav
 
 .include "../../archivers/bzip2/buildlink3.mk"
