8d937d7b9480f945506896004ec6c4caea261864
obache <obache@pkgsrc.org>
2010-09-27 11:15:22 +0000

    No need a patch for configure, so no need to autoconf.

--- multimedia/fuppes/options.mk
+++ multimedia/fuppes/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2009/11/28 19:31:29 kefren Exp $
+# $NetBSD: options.mk,v 1.2 2010/09/27 11:15:23 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fuppes
 PKG_SUPPORTED_OPTIONS=	mysql
@@ -9,7 +9,7 @@ PLIST_VARS+=		${PKG_SUPPORTED_OPTIONS}
 
 .if !empty(PKG_OPTIONS:Mmysql)
 PLIST.mysql=		yes
-CONFIGURE_ARGS+=	--enable-mysql
-
 .include "../../mk/mysql.buildlink3.mk"
+.else
+CONFIGURE_ENV+=		ac_cv_path_MYSQL_CONFIG=no
 .endif
