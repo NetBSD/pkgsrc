fec9799a2128cc2a018cedd0e1cfb60232ea3659
taca <taca@pkgsrc.org>
2010-09-28 12:24:25 +0000

    Don't hardcode "ruby18-" but use "${RUBY_PKGPREFIX}-".
    Fix build problem of ruby-gnome2 with ruby19.
    (Although textproc/ruby-eruby has another problem with ruby19.)

--- x11/ruby-gnome2-gtk/buildlink3.mk
+++ x11/ruby-gnome2-gtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2010/09/14 11:01:00 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2010/09/28 12:24:26 taca Exp $
 #
 
 BUILDLINK_TREE+=	ruby-gnome2-gtk
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	ruby-gnome2-gtk
 RUBY_GNOME2_GTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-gtk+=	${RUBY_PKGPREFIX}-gnome2-gtk>=0.17.0
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk?=	ruby18-gnome2-gtk>=0.19.4nb2
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk?=	${RUBY_PKGPREFIX}-gnome2-gtk>=0.19.4nb2
 BUILDLINK_PKGSRCDIR.ruby-gnome2-gtk?=	../../x11/ruby-gnome2-gtk
 
 .include "../../devel/ruby-gnome2-glib/buildlink3.mk"
