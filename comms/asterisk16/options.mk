c39327cf4d6a8ce5abc507bcb29cbb5700a9995b
jnemeth <jnemeth@pkgsrc.org>
2010-10-06 22:39:41 +0000

    DISTFILES is now initialized in Makefile, don't re-initialize it here.

--- comms/asterisk16/options.mk
+++ comms/asterisk16/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.8 2010/09/23 23:30:38 jnemeth Exp $
+# $NetBSD: options.mk,v 1.9 2010/10/06 22:39:41 jnemeth Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.asterisk
 PKG_SUPPORTED_OPTIONS=		zaptel x11 unixodbc ilbc webvmail ldap
@@ -42,7 +42,6 @@ CONFIGURE_ARGS+=	--without-odbc
 .endif
 
 .if !empty(PKG_OPTIONS:Milbc)
-DISTFILES=		${DEFAULT_DISTFILES}
 DISTFILES+=		rfc3951.txt
 SITES.rfc3951.txt=	http://www.ietf.org/rfc/
 DISTFILES+=		extract-cfile.awk
