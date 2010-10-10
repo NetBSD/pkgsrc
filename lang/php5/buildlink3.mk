53eae8bf586da30eda7f6c282979dc34f6bd7df9
obache <obache@pkgsrc.org>
2010-10-10 12:08:17 +0000

    ABI is also restricted to <5.3

--- lang/php5/buildlink3.mk
+++ lang/php5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2010/03/20 15:25:50 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2010/10/10 12:08:17 obache Exp $
 
 BUILDLINK_TREE+=	php
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	php
 PHP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.php+=		php>=5.1.2<5.3
-BUILDLINK_ABI_DEPENDS.php+=	php>=5.1.2
+BUILDLINK_ABI_DEPENDS.php+=	php>=5.1.2<5.3
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php5
 
 .include "../../textproc/libxml2/buildlink3.mk"
