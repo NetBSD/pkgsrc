1b929265b093ea9cfdf4ee774f70bb1dd3e50d5c
kefren <kefren@pkgsrc.org>
2010-10-16 21:19:47 +0000

    Add buildlink file that will be used for various add-ons

---
+++ devel/monodevelop/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2010/10/16 21:19:47 kefren Exp $
+
+BUILDLINK_TREE+=	monodevelop
+
+.if !defined(MONODEVELOP_BUILDLINK3_MK)
+MONODEVELOP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.monodevelop+=	monodevelop>=2.4
+BUILDLINK_PKGSRCDIR.monodevelop?=	../../devel/monodevelop
+BUILDLINK_CONTENTS_FILTER.monodevelop+=	${EGREP} '^lib/'
+
+.include "../../databases/shared-mime-info/buildlink3.mk"
+.include "../../devel/mono-addins/buildlink3.mk"
+.include "../../graphics/libgdiplus/buildlink3.mk"
+.include "../../lang/mono/buildlink3.mk"
+.include "../../x11/gtksourceview-sharp2/buildlink3.mk"
+.include "../../x11/gtk-sharp/buildlink3.mk"
+.include "../../x11/gnome-sharp/buildlink3.mk"
+.endif	# MONODEVELOP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-monodevelop
