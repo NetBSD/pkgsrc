febd7d8be42c7ebf713dd33536e647c0cb2305c3
abs <abs@pkgsrc.org>
2010-10-19 16:34:18 +0000

    Add a buildkink3.mk - for consumers of libsox.so

---
+++ audio/sox/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.3 2010/10/19 16:34:18 abs Exp $
+
+BUILDLINK_TREE+=	sox
+
+.if !defined(SOX_BUILDLINK3_MK)
+SOX_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.sox+=	sox>=14.3.1
+BUILDLINK_ABI_DEPENDS.sox+=	sox>=14.3.1
+BUILDLINK_PKGSRCDIR.sox?=	../../audio/sox
+.endif # SOX_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-sox
