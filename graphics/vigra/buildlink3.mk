7cc2c97a09ef610535efeb37f00aa38637a8ab33
adam <adam@pkgsrc.org>
2010-10-19 09:13:10 +0000

    Added buildlink3.mk

---
+++ graphics/vigra/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2010/10/19 09:13:10 adam Exp $
+
+BUILDLINK_TREE+=	vigra
+
+.if !defined(VIGRA_BUILDLINK3_MK)
+VIGRA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.vigra+=	vigra>=1.7.0
+BUILDLINK_PKGSRCDIR.vigra?=	../../graphics/vigra
+
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+.endif	# VIGRA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-vigra
