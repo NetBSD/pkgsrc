18f7cd9aad6c64a8b49045ee47f48bc29061f47c
pettai <pettai>
2010-10-19 23:11:42 +0000

    OpenDKIM is an open source implementation of the DKIM (Domain Keys Identified Mail) sender authentication system proposed by the E-mail Signing Technology Group (ESTG), now standardized by the IETF (RFC4871). It also includes implementations of the Author Domain Signing Practises (ADSP, RFC5617) and Vouch By Reference (VBR, RFC5518) proposed standards.
    The project started from a code fork of version 2.8.3 of the open source
    dkim-milter package developed and maintained by Sendmail, Inc.

---
+++ mail/opendkim/options.mk
@@ -0,0 +1,59 @@
+# $NetBSD: options.mk,v 1.1.1.1 2010/10/19 23:11:42 pettai Exp $
+
+PKG_OPTIONS_VAR=        PKG_OPTIONS.opendkim
+PKG_SUPPORTED_OPTIONS=  filter debug dkim-stats vbr
+PKG_SUGGESTED_OPTIONS=  filter
+
+.include "../../mk/bsd.options.mk"
+
+###
+### Debug support
+###
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug
+.else
+CONFIGURE_ARGS+=	--disable-debug
+.endif
+
+###
+### Install filter (milter) plugin
+###
+.if !empty(PKG_OPTIONS:Mfilter)
+EGDIR=		${PREFIX}/share/examples/opendkim
+RCD_SCRIPTS=	opendkim
+
+CONFIGURE_ARGS+=	--enable-filter
+CONFIGURE_ARGS+=	--with-milter=${PREFIX}
+SUBST_CLASSES+=		conf
+SUBST_STAGE.conf=	pre-configure
+SUBST_FILES.conf=	opendkim/Makefile.in
+SUBST_SED.conf+=	-e 's|@EGDIR@|${EGDIR}|g'
+
+CONF_FILES=		${EGDIR}/opendkim.conf.sample \
+			${PKG_SYSCONFDIR}/opendkim.conf
+
+PLIST_SRC+=		${PKGDIR}/PLIST.filter
+.else
+CONFIGURE_ARGS+=	--disable-filter
+.endif
+
+###
+### Build with VBR support
+###
+.if !empty(PKG_OPTIONS:Mvbr)
+CONFIGURE_ARGS+=	--enable-vbr
+PLIST_SRC+=		${PKGDIR}/PLIST.vbr
+.else
+CONFIGURE_ARGS+=	--disable-vbr
+.endif
+
+###
+### Install stats which outputs opendkim statistics
+###
+.if !empty(PKG_OPTIONS:Mdkim-stats)
+#.include "../../mk/db1.builtin.mk"
+CONFIGURE_ARGS+=	--enable-stats
+PLIST_SRC+=		${PKGDIR}/PLIST.stats
+.else
+CONFIGURE_ARGS+=	--disable-stats
+.endif
