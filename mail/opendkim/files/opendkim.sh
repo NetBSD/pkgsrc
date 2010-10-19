84ce7a19d093a2d263680babdc83d92f3c55d960
pettai <pettai@pkgsrc.org>
2010-10-19 23:11:42 +0000

    OpenDKIM is an open source implementation of the DKIM (Domain Keys Identified Mail) sender authentication system proposed by the E-mail Signing Technology Group (ESTG), now standardized by the IETF (RFC4871). It also includes implementations of the Author Domain Signing Practises (ADSP, RFC5617) and Vouch By Reference (VBR, RFC5518) proposed standards.
    The project started from a code fork of version 2.8.3 of the open source
    dkim-milter package developed and maintained by Sendmail, Inc.

---
+++ mail/opendkim/files/opendkim.sh
@@ -0,0 +1,40 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: opendkim.sh,v 1.1.1.1 2010/10/19 23:11:42 pettai Exp $
+#
+# PROVIDE: opendkim
+# REQUIRE: DAEMON
+# BEFORE: mail
+
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
+fi
+
+name="opendkim"
+rcvar=$name
+command="@PREFIX@/sbin/opendkim"
+pidfile="@VARBASE@/run/${name}.pid"
+command_args="-P ${pidfile} -l -x @PKG_SYSCONFDIR@/opendkim.conf -u @DKIM_USER@:@DKIM_GROUP@"
+required_files="@PKG_SYSCONFDIR@/opendkim.conf"
+start_precmd="dkimfilter_precmd"
+
+dkimfilter_precmd()
+{
+	if [ ! -d @VARBASE@/run/opendkim ]; then
+		@MKDIR@ @VARBASE@/run/opendkim
+		@CHMOD@ 0750 @VARBASE@/run/opendkim
+		@CHOWN@ @DKIM_USER@:@DKIM_GROUP@ @VARBASE@/run/opendkim
+	fi
+
+	if [ -f @VARBASE@/run/opendkim/${name}.sock ]; then
+		@RM@ -f @VARBASE@/run/opendkim/${name}.sock
+	fi
+}
+
+if [ -f /etc/rc.subr ]; then
+        load_rc_config $name
+	run_rc_command "$1"
+else
+	echo -n " ${name}"
+	${command} ${dkimfilter_flags} ${command_args}
+fi
