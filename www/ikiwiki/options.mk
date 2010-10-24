aef6174222d1601ded675372c041128b55836914
schmonz <schmonz@pkgsrc.org>
2010-10-24 15:45:34 +0000

    Update to 3.20101023. From the changelog:
    * Fix typo that broke anonymous git push.
    * Fix web reversion when the srcdir is in a subdir of the git repo.

--- www/ikiwiki/options.mk
+++ www/ikiwiki/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.10 2010/10/20 02:56:13 schmonz Exp $
+# $NetBSD: options.mk,v 1.11 2010/10/24 15:45:34 schmonz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.ikiwiki
 PKG_SUPPORTED_OPTIONS=		cvs ikiwiki-amazon-s3 ikiwiki-search
@@ -58,7 +58,7 @@ post-install:
 		${PREFIX}/share/doc/${PKGBASE}/w3mmode/ikiwiki.setup
 .else
 SUBST_CLASSES+=		w3m
-SUBST_STAGE.w3m=	pre-configure
+SUBST_STAGE.w3m=	post-patch
 SUBST_FILES.w3m=	Makefile.PL
 SUBST_SED.w3m+=		-e 's,^\(.*install .*W3M_CGI_BIN\),\#\1,'
 .endif
