636f8f5a0ce5474a22b83125593a81fa964b23ec
tron <tron>
2010-10-25 08:23:42 +0000

    Pullup ticket #3255 - requested by tnn devel/xulrunner: security update www/firefox: security update
    Revisions pulled up:
    - devel/xulrunner/Makefile			1.42
    - devel/xulrunner/dist.mk			1.15
    - devel/xulrunner/distinfo			1.37
    - devel/xulrunner/patches/patch-be		1.2
    - devel/xulrunner/patches/patch-mc		delete
    - www/firefox/Makefile				1.77
    ---
    Module Name:	pkgsrc
    Committed By:	tnn
    Date:		Thu Oct 21 10:27:21 UTC 2010
    
    Modified Files:
    	pkgsrc/devel/xulrunner: Makefile dist.mk distinfo
    	pkgsrc/devel/xulrunner/patches: patch-be
    	pkgsrc/www/firefox: Makefile
    Removed Files:
    	pkgsrc/devel/xulrunner/patches: patch-mc
    
    Log Message:
    Security & stability update to firefox-3.6.11 (xulrunner-1.9.2.11)
    
    MFSA 2010-72 Insecure Diffie-Hellman key exchange
    MFSA 2010-71 Unsafe library loading vulnerabilities
    MFSA 2010-70 SSL wildcard certificate matching IP addresses
    MFSA 2010-69 Cross-site information disclosure via modal calls
    MFSA 2010-68 XSS in gopher parser when parsing hrefs
    MFSA 2010-67 Dangling pointer vulnerability in LookupGetterOrSetter
    MFSA 2010-66 Use-after-free error in nsBarProp
    MFSA 2010-65 Buffer overflow and memory corruption using document.write
    MFSA 2010-64 Miscellaneous memory safety hazards (rv:1.9.2.11/ 1.9.1.14)

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.14 2010/09/09 09:41:21 tnn Exp $
+# $NetBSD: dist.mk,v 1.14.2.1 2010/10/25 08:23:42 tron Exp $
 #
 # used by devel/nspr/Makefile
 # used by devel/nss/Makefile
@@ -8,7 +8,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	3.6${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	1.9.2
-MOZ_BRANCH_MINOR=	.9
+MOZ_BRANCH_MINOR=	.11
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
