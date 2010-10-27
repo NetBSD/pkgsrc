d4817bfac1e8a0177216f448e099bdd61916a56c
markd <markd>
2010-10-27 05:39:13 +0000

    s/${MV}/${MV} -f/

--- x11/kdebase-workspace4/hacks.mk
+++ x11/kdebase-workspace4/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1.1.1 2009/10/02 19:37:22 markd Exp $
+# $NetBSD: hacks.mk,v 1.2 2010/10/27 05:39:13 markd Exp $
 
 .if !defined(KDEBASE4_HACKS_MK)
 KDEBASE4_HACKS_MK=	# defined
@@ -9,7 +9,7 @@ PKG_HACKS+=	X11_X_h
 post-wrapper:
 	${SED} 's,^#define FamilyInternet6.*,/* undef FamilyInternet6 */,' \
 		${BUILDLINK_DIR}/include/X11/X.h >${BUILDLINK_DIR}/include/X11/X.h.new
-	${MV} ${BUILDLINK_DIR}/include/X11/X.h.new ${BUILDLINK_DIR}/include/X11/X.h
+	${MV} -f ${BUILDLINK_DIR}/include/X11/X.h.new ${BUILDLINK_DIR}/include/X11/X.h
 .endif
 
 .endif
