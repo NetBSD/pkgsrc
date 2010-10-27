2dbe0de7b23927e0c51fa3dcf925bdd73d8e3cc2
gdt <gdt@pkgsrc.org>
2010-10-27 12:26:37 +0000

    Import dcc-1.3.123 as mail/dcc from wip/dcc.
    The Distributed Checksum Clearinghouses or DCC is an anti-spam content filter
    that runs on a variety of operating systems. As of the middle of 2007, it
    involves millions of users, more than six hundred thousand client computer
    systems, and more than 250 servers collecting and counting checksums related to
    more than 300 million mail messages on week days. The counts can be used by
    SMTP servers and mail user agents to detect and reject or filter spam or
    unsolicited bulk mail. DCC servers exchange or "flood" common checksums. The
    checksums include values that are constant across common variations in bulk
    messages, including "personalizations".

---
+++ mail/dcc/files/dccm.sh
@@ -0,0 +1,42 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: dccm.sh,v 1.1.1.1 2010/10/27 12:26:37 gdt Exp $
+#
+#
+# PROVIDE: dccm
+# REQUIRE: DAEMON
+# BEFORE:  mail spamd
+
+$_rc_subr_loaded . /etc/rc.subr
+
+name="dccm"
+rcvar="${name}"
+start_precmd="dcc_precmd"
+command="@PREFIX@/libexec/start-${name}"
+procname="@PREFIX@/libexec/${name}"
+pidfile="@DCC_RUN@/${name}.pid"
+#
+#
+#	start-dcc{d,ifd,m} understands certain flags while
+#	it passes everything behind -a to dcc{d,ifd,m}
+#
+#
+dcc_precmd()
+{
+	if [ -n "${rc_flags}" ]; then
+		rc_flags="-a '${rc_flags}'"
+	fi
+
+	eval dcc_start_flags=\$${name}_start_flags
+	if [ -n "${dcc_start_flags}" ]; then
+		rc_flags="${dcc_start_flags} ${rc_flags}"
+	fi
+
+	if [ -n "${command_args}" ]; then
+		rc_flags="${command_args} ${rc_flags}"
+		unset command_args
+	fi
+}
+
+load_rc_config "${name}"
+run_rc_command "${1}"
