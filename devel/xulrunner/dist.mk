e667a3854a37f89d35eaa94d5533e205de33eb42
tron <tron@pkgsrc.org>
2010-10-28 21:33:59 +0000

    Update the following package: - devel/nspr from 4.8.6.11 to 4.8.6.12 - devel/xulrunner from 1.9.2.11 to 1.9.2.12 - www/firefox from 3.6.11 to 3.6.12
    Security issues fixed since previous versions:
    MFSA 2010-73 Heap buffer overflow mixing document.write and DOM insertion

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.15 2010/10/21 10:27:20 tnn Exp $
+# $NetBSD: dist.mk,v 1.16 2010/10/28 21:33:59 tron Exp $
 #
 # used by devel/nspr/Makefile
 # used by devel/nss/Makefile
@@ -8,7 +8,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	3.6${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	1.9.2
-MOZ_BRANCH_MINOR=	.11
+MOZ_BRANCH_MINOR=	.12
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
