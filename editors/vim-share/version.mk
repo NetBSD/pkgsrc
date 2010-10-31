c5c205354549cbf393e5d589376d378490f93233
wiz <wiz@pkgsrc.org>
2010-10-31 23:50:51 +0000

    Update to latest 7.2 patchlevel, 446. Fixes PR 44018 by Stefan Palm.

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.82 2010/06/07 12:12:22 martti Exp $
+# $NetBSD: version.mk,v 1.83 2010/10/31 23:50:52 wiz Exp $
 
 VIM_VERSION=		7.2
-VIM_PATCHLEVEL=		442
+VIM_PATCHLEVEL=		446
 VIM_SUBDIR=		vim72
