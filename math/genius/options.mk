b3722c983c82363d6c748773e4fc286f8adb9003
drochner <drochner>
2010-11-02 18:02:34 +0000

    update to 1.0.11 changes: * Allow changing variable names for all plotting functions * Add SlopefieldTicks, VectorfieldTicks, LinePlotVariableNames, and   SurfacePlotVariableNames, parameters * Add AskButtons interactive function * Add RungeKuttaFull, EulersMethodFull functions * CHANGE: spelling fix: AuxiliaryUnitMatrix doesn't have two l's * Support for setting legend on LinePlotDrawLine with a "legend" parameter * Allow comparisons (== and !=) with null, treating it as an empty matrix * Uses GIO instead of GnomeVFS * Allow slopefield solutions to leave plot window by a small fudge factor. -bugfixes -documentation / translation updates

--- math/genius/options.mk
+++ math/genius/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2009/09/03 13:15:20 asau Exp $
+# $NetBSD: options.mk,v 1.2 2010/11/02 18:02:34 drochner Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.genius
 PKG_SUPPORTED_OPTIONS=		x11
@@ -11,11 +11,9 @@ PLIST_VARS+=		${PKG_SUPPORTED_OPTIONS}
 .if !empty(PKG_OPTIONS:Mx11)
 PLIST.x11=		yes
 
-.include "../../devel/libglade/buildlink3.mk"
 .include "../../graphics/hicolor-icon-theme/buildlink3.mk"
 .include "../../mk/omf-scrollkeeper.mk"
 .include "../../sysutils/desktop-file-utils/desktopdb.mk"
-.include "../../sysutils/gnome-vfs/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .include "../../x11/gtksourceview2/buildlink3.mk"
 .include "../../x11/vte/buildlink3.mk"
