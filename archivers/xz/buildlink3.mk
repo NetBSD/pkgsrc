4dfbf98989b3ee39c4639e4ba77525b33c16172e
adam <adam@pkgsrc.org>
2010-11-03 14:21:49 +0000

    Changes 5.0.0: * The memory usage limit is now disabled by default. * Added support for XZ_DEFAULTS environment variable. * The compression settings associated with the preset levels   -0 ... -9 have been changed. --extreme was changed a little too. * If a preset level (-0 ... -9) is specified after a custom filter   chain options have been used (e.g. --lzma2), the custom filter   chain will be forgotten. Earlier the preset options were   completely ignored after custom filter chain options had been seen. * xz will create sparse files when decompressing if the uncompressed   data contains long sequences of binary zeros. * Support for "xz --list" was added. Combine with --verbose or   --verbose --verbose (-vv) for detailed output. * I had hoped that liblzma API would have been stable after   4.999.9beta, but there have been a couple of changes in the   advanced features, which don't affect most applications:   - Index handling code was revised. If you were using the old     API, you will get a compiler error (so it's easy to notice).   - A subtle but important change was made to the Block handling     API. lzma_block.version has to be initialized even for     lzma_block_header_decode(). Code that doesn't do it will work     for now, but might break in the future, which makes this API     change easy to miss. * The major soname has been bumped to 5.0.0. liblzma API and ABI   are now stable, so the need to recompile programs linking against   liblzma shouldn't arise soon.

--- archivers/xz/buildlink3.mk
+++ archivers/xz/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2009/12/13 13:26:17 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/11/03 14:21:49 adam Exp $
 
 BUILDLINK_TREE+=	xz
 
 .if !defined(XZ_BUILDLINK3_MK)
 XZ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xz+=	xz>=4.999.9beta
+BUILDLINK_API_DEPENDS.xz+=	xz>=5.0.0
 BUILDLINK_PKGSRCDIR.xz?=	../../archivers/xz
 
 .endif	# XZ_BUILDLINK3_MK
