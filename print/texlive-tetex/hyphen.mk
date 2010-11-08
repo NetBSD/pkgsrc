d5f5baa62ba066f020d5f4886e769b00440874f9
minskim <minskim@pkgsrc.org>
2010-11-08 00:25:51 +0000

    The LuaTeX hyphenation requires recent texlive-tetex and tex-hyphen-base.

--- print/texlive-tetex/hyphen.mk
+++ print/texlive-tetex/hyphen.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hyphen.mk,v 1.2 2010/11/07 17:22:24 minskim Exp $
+# $NetBSD: hyphen.mk,v 1.3 2010/11/08 00:25:51 minskim Exp $
 #
 # This Makefile fragment is intended to be included by packages that
 # install TeX hyphenation pattens.  It updates format files at
@@ -14,8 +14,8 @@
 .if !defined(TEX_HYPHEN_MK)
 TEX_HYPHEN_MK=		# defined
 
-DEPENDS+=		texlive-tetex>=2008:../../print/texlive-tetex
-DEPENDS+=		tex-hyphen-base-[0-9]*:../../print/tex-hyphen-base
+DEPENDS+=		texlive-tetex>=2010:../../print/texlive-tetex
+DEPENDS+=		tex-hyphen-base>=2010nb1:../../print/tex-hyphen-base
 
 EVAL_PREFIX+=		TETEX_PREFIX=texlive-tetex
 FILES_SUBST+=		FMTUTIL_SYS=${TETEX_PREFIX:Q}/bin/fmtutil-sys
