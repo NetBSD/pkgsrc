4ba81e20e9675395042a739fb6eb8833eff0384c
gls <gls@pkgsrc.org>
2010-11-15 18:55:43 +0000

    Import libgexiv2-0.2.1 into the NetBSD Packages collection.
    gexiv2 is a GObject-based wrapper around the Exiv2 library.  It makes
    the basic features of Exiv2 available to GNOME applications.

---
+++ graphics/libgexiv2/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/11/15 18:55:43 gls Exp $
+
+BUILDLINK_TREE+=	libgexiv2
+
+.if !defined(LIBGEXIV2_BUILDLINK3_MK)
+LIBGEXIV2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libgexiv2+=	libgexiv2>=0.2.0
+BUILDLINK_PKGSRCDIR.libgexiv2?=	../../graphics/libgexiv2
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../graphics/exiv2/buildlink3.mk"
+.endif	# LIBGEXIV2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libgexiv2
