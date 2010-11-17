f8d47cb305e43cad58ed7a82fe71bc314e585add
drochner <drochner@pkgsrc.org>
2010-11-17 14:09:55 +0000

    add support for rebuilding giomodule.cache on INSTALL/DEINSTALL, addresses PRs pkg/43578 and pkg/44104 by Robert Elz

---
+++ devel/glib2/modules.mk
@@ -0,0 +1,23 @@
+# $NetBSD: modules.mk,v 1.1 2010/11/17 14:09:55 drochner Exp $
+#
+# This Makefile fragment is intended to be included by packages that install
+# GIO modules.  It takes care of rebuilding the corresponding databases at
+# (de)installation time.
+#
+
+.if !defined(GIO_MODULES_MK)
+GIO_MODULES_MK=	# defined
+
+GIO_QUERYMODULES=	${BUILDLINK_PREFIX.glib2}/bin/gio-querymodules
+
+FILES_SUBST+=		GIO_MODULES_DIR=${GIO_MODULES_DIR:Q}
+FILES_SUBST+=		GIO_QUERYMODULES=${GIO_QUERYMODULES:Q}
+GIO_MODULES_DIR=${BUILDLINK_PREFIX.glib2}/lib/gio/modules
+INSTALL_TEMPLATES+=	../../devel/glib2/files/modules.tmpl
+DEINSTALL_TEMPLATES+=	../../devel/glib2/files/modules.tmpl
+
+TOOLS_NOOP+=	gio-querymodules
+
+.include "../../devel/glib2/buildlink3.mk"
+
+.endif			# GIO_MODULES_MK
