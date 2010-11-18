b62984272110cea2db45a4735cbc322b7668b94a
obache <obache>
2010-11-18 23:59:15 +0000

    Bump ABI_DEPNDS of gtkhtml314 and recursive bump from libgtkhtml shlib name changed.

--- www/gtkhtml314/buildlink3.mk
+++ www/gtkhtml314/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/10/22 17:50:53 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2010/11/18 23:59:15 obache Exp $
 
 BUILDLINK_TREE+=	gtkhtml314
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gtkhtml314
 GTKHTML314_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gtkhtml314+=	gtkhtml314>=3.24.0
-BUILDLINK_ABI_DEPENDS.gtkhtml314?=	gtkhtml314>=3.26.3nb1
+BUILDLINK_ABI_DEPENDS.gtkhtml314?=	gtkhtml314>=3.32.1
 BUILDLINK_PKGSRCDIR.gtkhtml314?=	../../www/gtkhtml314
 
 .include "../../devel/GConf/buildlink3.mk"
