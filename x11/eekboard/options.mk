7789d10e50215902e2acbc9ffc5330a53c60fbcc
obache <obache@pkgsrc.org>
2010-11-22 13:31:05 +0000

    Update eekboard to 0.0.6. bug and compatibility fixes.

--- x11/eekboard/options.mk
+++ x11/eekboard/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2010/08/30 11:47:34 obache Exp $
+# $NetBSD: options.mk,v 1.2 2010/11/22 13:31:05 obache Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.eekboard
@@ -12,7 +12,6 @@ PLIST_VARS+=	clutter
 .if !empty(PKG_OPTIONS:Mclutter)
 PLIST.clutter=	yes
 .include "../../graphics/clutter/buildlink3.mk"
-.include "../../graphics/clutter-gtk/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-clutter=yes
 .else
 CONFIGURE_ARGS+=	--enable-clutter=no
