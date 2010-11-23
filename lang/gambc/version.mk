fa61553d5c95786b508b963fa461bfdc2da0195b
asau <asau>
2010-11-23 21:22:11 +0000

    Update Gambit-C to 4.6.0 With advent of git version control changes are unknown.

--- lang/gambc/version.mk
+++ lang/gambc/version.mk
@@ -1,10 +1,10 @@
-# $NetBSD: version.mk,v 1.2 2008/07/16 07:14:36 bjs Exp $
+# $NetBSD: version.mk,v 1.3 2010/11/23 21:22:11 asau Exp $
 ###
 .if !defined(GAMBC_VERSION_MK)
 GAMBC_VERSION_MK=	# empty
 ###
 ### The package version sets the distribution version.
 ###
-GAMBC_VERSION=		4.2.8
+GAMBC_VERSION=		4.6.0
 GAMBC_DIST_VERSION=	${GAMBC_VERSION:C|^.*|v&|:S|.|_|g}
 .endif
