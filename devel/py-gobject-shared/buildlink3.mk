70fcc1d8336c1fd2d94d89d5d6a2ffefea04f7f7
drochner <drochner@pkgsrc.org>
2010-11-24 18:54:55 +0000

    update to 2.26.0 This switches to the gnome-2.32 release branch.

--- devel/py-gobject-shared/buildlink3.mk
+++ devel/py-gobject-shared/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/03/20 19:24:28 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2010/11/24 18:54:56 drochner Exp $
 
 BUILDLINK_TREE+=	py-gobject-shared
 
@@ -10,6 +10,7 @@ BUILDLINK_PKGSRCDIR.py-gobject-shared?=		../../devel/py-gobject-shared
 BUILDLINK_DEPMETHOD.py-gobject-shared?=		build
 
 .include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/libffi/buildlink3.mk"
 .endif # PY_GOBJECT_SHARED_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-py-gobject-shared
