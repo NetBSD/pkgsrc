60e0d460116d5a7318ab969a2bd2b95291d7dfeb
drochner <drochner>
2010-11-24 18:50:48 +0000

    add telepathy-logger-0.1.6, a logging module for the "telepathy" framework (will be needed by future versions of Gnome's "empathy" IM client)

---
+++ chat/telepathy-logger/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/11/24 18:50:48 drochner Exp $
+
+BUILDLINK_TREE+=	telepathy-logger
+
+.if !defined(TELEPATHY_LOGGER_BUILDLINK3_MK)
+TELEPATHY_LOGGER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.telepathy-logger+=	telepathy-logger>=0.1.4
+BUILDLINK_PKGSRCDIR.telepathy-logger?=	../../chat/telepathy-logger
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/GConf/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../chat/telepathy-glib/buildlink3.mk"
+.include "../../databases/sqlite3/buildlink3.mk"
+.endif	# TELEPATHY_LOGGER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-telepathy-logger
