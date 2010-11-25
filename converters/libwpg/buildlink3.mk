e7d35273faf6076a785057b7735a7b4867acb227
wiz <wiz@pkgsrc.org>
2010-11-25 10:28:34 +0000

    Bump PKGREVISION for libwpd shlib name change.

--- converters/libwpg/buildlink3.mk
+++ converters/libwpg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2010/11/18 10:39:19 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/11/25 10:28:34 wiz Exp $
 
 BUILDLINK_TREE+=	libwpg
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	libwpg
 LIBWPG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libwpg+=	libwpg>=0.1.3
+BUILDLINK_ABI_DEPENDS.libwpg+=	libwpg>=0.1.3nb1
 BUILDLINK_PKGSRCDIR.libwpg?=	../../converters/libwpg
 
 .include "../../converters/libwpd/buildlink3.mk"
