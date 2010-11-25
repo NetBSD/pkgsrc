8c689626ac4681261fa6b4112df1d9cfbcfbe254
wiz <wiz@pkgsrc.org>
2010-11-25 10:27:49 +0000

    Update to 0.9.0. Bump API depends for shlib name change.
    CHANGES:
    0.8.x - 0.9.0
    - Conversion of page-numbering (William)
    - Conversion of embedded images and text boxes in WP1, WP3, WP5 and WP6 documents
      (Fridrich)
    - Conversion of password protected WP1, WP3, WP42 and WP5 documents (Fridrich)
    - Conversion of annotations/comments in WP6 parser (Fridrich)
    - Fix some greek characters so that Text written in WP5.1 with Printer Polyglott
      convert correct.
    - Fix unicode mappings of some older Symbol and Dingbats fonts (Fridrich)
    - Drop the libgsf dependency in favour of pure C++ implementation of the WPXInputStream
      interface (Fridrich, Ariya)
    - Fix the footnote/endnote/comment/annotation/textbox writing in wpd2html converter
    - Improve metadata parsing (David Hislop)

--- converters/libwpd/buildlink3.mk
+++ converters/libwpd/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/03/20 19:24:03 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2010/11/25 10:27:49 wiz Exp $
 
 BUILDLINK_TREE+=	libwpd
 
 .if !defined(LIBWPD_BUILDLINK3_MK)
 LIBWPD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libwpd+=	libwpd>=0.8.1nb1
-BUILDLINK_ABI_DEPENDS.libwpd?=	libwpd>=0.8.9nb4
+BUILDLINK_API_DEPENDS.libwpd+=	libwpd>=0.9.0
 BUILDLINK_PKGSRCDIR.libwpd?=	../../converters/libwpd
 
 .include "../../devel/libgsf/buildlink3.mk"
