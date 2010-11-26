bd02e1fe8ee25db61dcc5f2713331e1fce4b92b0
adam <adam@pkgsrc.org>
2010-11-26 20:46:59 +0000

    Changes 1.45.0: * Fixed a problem on kqueue-based platforms where a deadline_timer   may never fire if the io_service is running in a background thread * Fixed a const-correctness issue that prevented valid uses of   has_service<> from compiling * Fixed MinGW cross-compilation * Removed dependency on deprecated Boost.System functions * Ensured close()/closesocket() failures are correctly propagated * Added a check for errors returned by   InitializeCriticalSectionAndSpinCount * Added support for hardware flow control on QNX * Always use pselect() on HP-UX, if it is available. * Ensured handler arguments are passed as lvalues * Fixed Windows build when thread support is disabled * Fixed a Windows-specific problem where deadline_timer objects with   expiry times set more than 5 minutes in the future may never expire * Fixed the resolver backend on BSD platforms so that an empty service   name resolves to port number 0, as per the documentation * Fixed read operations so that they do not accept buffer sequences of   type const_buffers_1 * Redefined Protocol and id to avoid clashing with Objective-C++ keywords * Fixed a vector reallocation performance issue that can occur when   there are many active deadline_timer objects * Fixed the kqueue backend so that it compiles on NetBSD * Fixed the socket io_control() implementation on 64-bit Mac OS X and   BSD platforms * Fixed a Windows-specific problem where failures from accept() are   incorrectly treated as successes * Deprecated the separate compilation header <boost/asio/impl/src.cpp>   in favour of <boost/asio/impl/src.hpp>

--- devel/boost-libs/buildlink3.mk
+++ devel/boost-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2010/08/26 17:40:58 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2010/11/26 20:47:00 adam Exp $
 
 BUILDLINK_TREE+=	boost-libs
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	boost-libs
 BOOST_LIBS_BUILDLINK3_MK:=
 
 # Use a dependency pattern that guarantees the proper ABI.
-BUILDLINK_API_DEPENDS.boost-libs+=	boost-libs-1.44.*
+BUILDLINK_API_DEPENDS.boost-libs+=	boost-libs-1.45.*
 BUILDLINK_PKGSRCDIR.boost-libs?=	../../devel/boost-libs
 
 .include "../../devel/boost-headers/buildlink3.mk"
