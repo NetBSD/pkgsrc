a6ac08df333b893f6686401caadf93136c22db31
taca <taca>
2010-12-01 13:56:23 +0000

    Revive ruby-build-ri-db option of ruby19-base.  It was no effect since Ruby 1.9.2 imported, maybe.

--- lang/ruby19-base/options.mk
+++ lang/ruby19-base/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2010/09/12 03:50:06 taca Exp $
+# $NetBSD: options.mk,v 1.2 2010/12/01 13:56:23 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ruby
 PKG_SUPPORTED_OPTIONS=	ruby-build-ri-db
@@ -7,10 +7,10 @@ PKG_SUGGESTED_OPTIONS=	ruby-build-ri-db
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mruby-build-ri-db)
-BUILD_TARGET=		all rdoc
-CONFIGURE_ARGS+=	--enable-install-doc
 #PRIVILEGED_STAGES+=    clean
 RUBY_DYNAMIC_DIRS+=	${RUBY_SYSRIDIR}
 # Use huge memory.
 UNLIMIT_RESOURCES+=	datasize
+.else
+CONFIGURE_ARGS+=	--enable-install-doc=no
 .endif
