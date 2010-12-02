375ce3c5420d65b481deaf1d0f0b7bc82232425c
wiz <wiz@pkgsrc.org>
2010-12-02 13:36:56 +0000

    Due to py-cairo 1.8.8<>1.8.10 binary incompatibility, bump dependency pattern in buildlink3.mk and PKGREVISION of dependencies.

--- graphics/py-cairo/buildlink3.mk
+++ graphics/py-cairo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2010/09/14 11:00:45 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2010/12/02 13:36:56 wiz Exp $
 
 BUILDLINK_TREE+=	pycairo
 
@@ -8,7 +8,7 @@ PY_CAIRO_BUILDLINK3_MK:=
 .  include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.pycairo+=	${PYPKGPREFIX}-cairo>=1.0.2
-BUILDLINK_ABI_DEPENDS.pycairo+=	${PYPKGPREFIX}-cairo>=1.8.8nb3
+BUILDLINK_ABI_DEPENDS.pycairo+=	${PYPKGPREFIX}-cairo>=1.8.10
 BUILDLINK_PKGSRCDIR.pycairo?=	../../graphics/py-cairo
 
 .include "../../graphics/cairo/buildlink3.mk"
