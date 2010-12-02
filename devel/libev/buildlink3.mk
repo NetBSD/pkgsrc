48dcf55fe5a8588b8e54c050dea9162112a40477
wiz <wiz@pkgsrc.org>
2010-12-02 15:47:12 +0000

    Update to 4.01. Shlib major bump, so bump bl3.mk.
    4.01 Fri Nov  5 21:51:29 CET 2010
            - automake fucked it up, apparently, --add-missing -f is not quite enough
              to make it update its files, so 4.00 didn't install ev++.h and
              event.h on make install. grrr.
            - ev_loop(count|depth) didn't return anything (Robin Haberkorn).
            - change EV_UNDEF to 0xffffffff to silence some overzealous compilers.
            - use "(libev) " prefix for all libev error messages now.
    
    4.00 Mon Oct 25 12:32:12 CEST 2010
    	- "PORTING FROM LIBEV 3.X TO 4.X" (in ev.pod) is recommended reading.
    	- ev_embed_stop did not correctly stop the watcher (very good
              testcase by Vladimir Timofeev).
            - ev_run will now always update the current loop time - it erroneously
              didn't when idle watchers were active, causing timers not to fire.
            - fix a bug where a timeout of zero caused the timer not to fire
              in the libevent emulation (testcase by Péter Szabó).
    	- applied win32 fixes by Michael Lenaghan (also James Mansion).
    	- replace EV_MINIMAL by EV_FEATURES.
            - prefer EPOLL_CTL_ADD over EPOLL_CTL_MOD in some more cases, as it
              seems the former is *much* faster than the latter.
            - linux kernel version detection (for inotify bug workarounds)
              did not work properly.
            - reduce the number of spurious wake-ups with the ports backend.
            - remove dependency on sys/queue.h on freebsd (patch by Vanilla Hsu).
            - do async init within ev_async_start, not ev_async_set, which avoids
              an API quirk where the set function must be called in the C++ API
              even when there is nothing to set.
            - add (undocumented) EV_ENABLE when adding events with kqueue,
              this might help with OS X, which seems to need it despite documenting
              not to need it (helpfully pointed out by Tilghman Lesher).
            - do not use poll by default on freebsd, it's broken (what isn't
              on freebsd...).
            - allow to embed epoll on kernels >= 2.6.32.
            - configure now prepends -O3, not appends it, so one can still
              override it.
            - ev.pod: greatly expanded the portability section, added a porting
              section, a description of watcher states and made lots of minor fixes.
            - disable poll backend on AIX, the poll header spams the namespace
              and it's not worth working around dead platforms (reported
              and analyzed by Aivars Kalvans).
            - improve header file compatibility of the standalone eventfd code
              in an obscure case.
            - implement EV_AVOID_STDIO option.
            - do not use sscanf to parse linux version number (smaller, faster,
              no sscanf dependency).
            - new EV_CHILD_ENABLE and EV_SIGNAL_ENABLE configurable settings.
            - update libev.m4 HAVE_CLOCK_SYSCALL test for newer glibcs.
            - add section on accept() problems to the manpage.
            - rename EV_TIMEOUT to EV_TIMER.
            - rename ev_loop_count/depth/verify/loop/unloop.
            - remove ev_default_destroy and ev_default_fork.
            - switch to two-digit minor version.
            - work around an apparent gentoo compiler bug.
            - define _DARWIN_UNLIMITED_SELECT. just so.
            - use enum instead of #define for most constants.
            - improve compatibility to older C++ compilers.
            - (experimental) ev_run/ev_default_loop/ev_break/ev_loop_new have now
              default arguments when compiled as C++.
            - enable automake dependency tracking.
            - ev_loop_new no longer leaks memory when loop creation failed.
            - new ev_cleanup watcher type.

--- devel/libev/buildlink3.mk
+++ devel/libev/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2010/01/29 15:05:00 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2010/12/02 15:47:12 wiz Exp $
 
 BUILDLINK_TREE+=	libev
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libev
 LIBEV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libev+=libev>=3.9nb1
-BUILDLINK_ABI_DEPENDS.libev+=libev>=3.9nb1
+BUILDLINK_ABI_DEPENDS.libev+=libev>=4.01
 BUILDLINK_PKGSRCDIR.libev?=	../../devel/libev
 
 BUILDLINK_LIBDIRS.libev+=	lib/ev
