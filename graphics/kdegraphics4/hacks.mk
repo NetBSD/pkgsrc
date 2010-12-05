8aab8e4224c112048ebfb91a68ba7e0bc8f829c3
wiz <wiz@pkgsrc.org>
2010-12-05 23:22:10 +0000

    Remove trailing whitespace.

--- graphics/kdegraphics4/hacks.mk
+++ graphics/kdegraphics4/hacks.mk
@@ -1,8 +1,8 @@
-# $NetBSD: hacks.mk,v 1.1.1.1 2009/10/02 20:11:00 markd Exp $
+# $NetBSD: hacks.mk,v 1.2 2010/12/05 23:22:10 wiz Exp $
 
 #
 # Can't figure out why the kruler build is prefering the below file
-# rather than the one generated in _KDE_build/kruler so for the 
+# rather than the one generated in _KDE_build/kruler so for the
 # time being just get rid of it.
 #
 
