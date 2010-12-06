9a1ccb9a5690e415d93c094693dbbf8ee8fd19e9
markd <markd@pkgsrc.org>
2010-12-06 11:50:35 +0000

    Fix build with recent subversion.

--- devel/kdevelop-base/options.mk
+++ devel/kdevelop-base/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2008/04/12 22:42:59 jlam Exp $
+# $NetBSD: options.mk,v 1.3 2010/12/06 11:50:35 markd Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kdesdk
 PKG_SUPPORTED_OPTIONS=	svn
@@ -9,8 +9,9 @@ PLIST_VARS+=		svn
 
 .if !empty(PKG_OPTIONS:Msvn)
 .include "../../devel/subversion-base/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-apr-config=${BUILDLINK_PREFIX.apr}/bin/apr-config
-CONFIGURE_ARGS+=	--with-apu-config=${BUILDLINK_PREFIX.apr}/bin/apu-config
+CONFIGURE_ARGS+=	--with-subversion
+CONFIGURE_ARGS+=	--with-apr-config=${BUILDLINK_PREFIX.apr}/bin/apr-1-config
+CONFIGURE_ARGS+=	--with-apu-config=${BUILDLINK_PREFIX.apr}/bin/apu-1-config
 CONFIGURE_ARGS+=	--with-svn-include=${BUILDLINK_PREFIX.subversion-base}/include/subversion-1
 CONFIGURE_ARGS+=	--with-svn-lib=${BUILDLINK_PREFIX.subversion-base}/lib
 PLIST.svn=		yes
