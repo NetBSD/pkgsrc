a1dc8a909893a8333c56c590e78bd5f92ad9610b
wiz <wiz@pkgsrc.org>
2010-12-06 14:25:44 +0000

    Update to 2.1.4, provided by Matthew Sporleder in PR 44165. Includes new startup scripts.
    Summary of changes from 2.1.3 to 2.1.4
    
        * A bug in the binary heap layout caused inflated object counts, this has been fixed.
        * Much more comprehensive documentation.
        * A DNS director that uses DNS lookups for choosing which backend to route requests to has been added.
        * The client director now uses the variable client.identity for choosing which backend to send a given request to.
        * String representation of now, making it easier to add Expires headers
        * Portability fixes for Solaris.
        * Various bug fixes.
    
    Summary of changes from 2.1.2 to 2.1.3
    
        * The scalability of critbit, the default hashing method, has been improved.
        * A bug in varnishd would in some cases confuse varnishncsa leading to lost or wrong log lines.
        * Some bugs in the handling of Range requests has been fixed. This only matters if you enable Range support.
        * Add «log» command to VCL which will log to the Varnish log.
    
    Summary of changes from 2.1.1 to 2.1.2
    
        * When adding Range support in 2.1.1 an error was introduced. Garbage was appended to some objects. This affected some load balancers - but clients seemed not to be affected.
    
    Summary of changes from 2.1.0 to 2.1.1
    
        * Experimental support for the Range header, must be enabled with the http_range_support parameter.
        * A bug in workspace rollback prevented ESI from working correctly in all situations. This is now fixed.
        * A race condition and a deadlock in the critbit hasher have been fixed.
        * HEAD requests are no longer converted to GET requests for pass and pipe.
        * Support for completely obliterating objects including all variants («nuke»). See http://varnish-cache.org/changeset/4668 for details
    
    Summary of changes from 2.0.6 to 2.1.0
    
    Varnish 2.1.0 has just been released. It contains lots of changes relative to 2.0.6, the most important ones being:
    
        * Experimental support for persistent cache
        * The regular expression engine is now PCRE
        * Saint mode, where we can serve a cached object if the backend is down or otherwise faulty.
        * A more scalable hashing method called critbit
        * Increased scalability, removing a limitation of maximum 64k connections
        * obj_workspace is removed, this is now scaled automatically
        * Hashing and client IP based directors
    
    Note: The release has some syntaxchanges in VCL, please read the full changelog

---
+++ www/varnish/files/varnishlog.sh
@@ -0,0 +1,14 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# PROVIDE: varnishlog
+# REQUIRE: varnishd
+
+. /etc/rc.subr
+
+name="varnishlog"
+rcvar=$name
+command="@PREFIX@/bin/varnishlog"
+commands_args="-D"
+
+load_rc_config $name
+run_rc_command "$1"
