9d531edcff4fac6d79c391f8ef9c0871b749585a
abs <abs@pkgsrc.org>
2010-12-07 22:23:23 +0000

    support options jce

---
+++ lang/openjdk7/options.mk
@@ -0,0 +1,29 @@
+# $NetBSD: options.mk,v 1.1 2010/12/07 22:23:23 abs Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.openjdk7
+PKG_SUPPORTED_OPTIONS=		sun-jre-jce
+PKG_SUGGESTED_OPTIONS=		#
+
+.include "../../mk/bsd.options.mk"
+
+###
+### Java(TM) Cryptography Extension (JCE)
+###
+.if !empty(PKG_OPTIONS:Msun-jre-jce)
+JCE_DISTFILE=jce_policy-6.zip
+DISTFILES+=	${JCE_DISTFILE}
+EXTRACT_ONLY+=	${JCE_DISTFILE}
+PLIST.jce=	yes
+USE_TOOLS+=	pax
+#
+FETCH_MESSAGE+= "Please download the Java(TM) Cryptography Extension (JCE) Unlimited"
+FETCH_MESSAGE+= "Strength Jurisdiction Policy Files 6 '${JCE_DISTFILE}' from:"
+FETCH_MESSAGE+= "	http://java.sun.com/javase/downloads/"
+FETCH_MESSAGE+= " and place it in:"
+FETCH_MESSAGE+= "       ${DISTDIR}/${DIST_SUBDIR}/${JCE_DISTFILE}"
+FETCH_MESSAGE+= ""
+FETCH_MESSAGE+= " Then resume this build by running '"${MAKE:Q}"' again."
+#
+post-install:
+	cd ${WRKDIR}/jce ; pax -rw -pp . ${DESTDIR}${JAVA_HOME}/jre/lib/security
+.endif
