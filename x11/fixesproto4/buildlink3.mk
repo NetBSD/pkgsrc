6ca241b721bcd3fe772d416c7fd1651f05680c54
abs <abs@pkgsrc.org>
2010-12-08 00:39:31 +0000

    Added x11/fixesproto4 version 4.0 This provides the Fixes extension headers from modular X.org X11 project, specifically for use with Xfixes and native X11.

---
+++ x11/fixesproto4/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/12/08 00:39:31 abs Exp $
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+BUILDLINK_DEPMETHOD.fixesproto?=	build
+
+BUILDLINK_TREE+=	fixesproto4
+
+.if !defined(FIXESPROTO_BUILDLINK3_MK)
+FIXESPROTO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.fixesproto+=	fixesproto>=3.0.0
+BUILDLINK_PKGSRCDIR.fixesproto?=	../../x11/fixesproto4
+
+.include "../../x11/xextproto/buildlink3.mk"
+.include "../../x11/xproto/buildlink3.mk"
+.endif # FIXESPROTO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-fixesproto4
