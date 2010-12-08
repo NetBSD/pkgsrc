b535f480076658f888beb22c906241b625ec62f5
abs <abs@pkgsrc.org>
2010-12-08 08:10:47 +0000

    fix stupid typo in last

--- x11/fixesproto4/buildlink3.mk
+++ x11/fixesproto4/buildlink3.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/12/08 00:39:31 abs Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/12/08 08:10:47 abs Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
 BUILDLINK_DEPMETHOD.fixesproto?=	build
 
-BUILDLINK_TREE+=	fixesproto4
+BUILDLINK_TREE+=	fixesproto
 
 .if !defined(FIXESPROTO_BUILDLINK3_MK)
 FIXESPROTO_BUILDLINK3_MK:=
@@ -16,4 +16,4 @@ BUILDLINK_PKGSRCDIR.fixesproto?=	../../x11/fixesproto4
 .include "../../x11/xproto/buildlink3.mk"
 .endif # FIXESPROTO_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-fixesproto4
+BUILDLINK_TREE+=	-fixesproto
