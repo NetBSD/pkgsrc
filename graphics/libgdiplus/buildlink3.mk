60efecb493f5121fd80ed4c4a4584fcd456d8b1a
kefren <kefren@pkgsrc.org>
2010-12-09 08:31:40 +0000

    remove dependencies used for built-in cairo, update buildlink3.mk in order to reflect the latest changes. Changes suggested by obache@

--- graphics/libgdiplus/buildlink3.mk
+++ graphics/libgdiplus/buildlink3.mk
@@ -1,23 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.28 2010/06/13 22:44:37 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.29 2010/12/09 08:31:40 kefren Exp $
 
 BUILDLINK_TREE+=	libgdiplus
 
 .if !defined(LIBGDIPLUS_BUILDLINK3_MK)
 LIBGDIPLUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgdiplus+=	libgdiplus>=2.0
-BUILDLINK_ABI_DEPENDS.libgdiplus?=	libgdiplus>=2.6.4nb1
+BUILDLINK_API_DEPENDS.libgdiplus+=	libgdiplus>=2.8.1
+BUILDLINK_ABI_DEPENDS.libgdiplus?=	libgdiplus>=2.8.1nb1
 BUILDLINK_PKGSRCDIR.libgdiplus?=	../../graphics/libgdiplus
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../graphics/freetype2/buildlink3.mk"
-.include "../../graphics/glitz/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
 .include "../../graphics/jpeg/buildlink3.mk"
 .include "../../graphics/libungif/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
-.include "../../x11/libXft/buildlink3.mk"
-.include "../../x11/libXrender/buildlink3.mk"
 .endif # LIBGDIPLUS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libgdiplus
