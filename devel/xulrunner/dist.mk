a4e1a305d6f5e1ae226aadebd6766da91d3e5c56
tnn <tnn@pkgsrc.org>
2010-12-11 14:46:29 +0000

    Update to firefox-3.6.13.
    MFSA 2010-84 XSS hazard in multiple character encodings
    MFSA 2010-83 Location bar SSL spoofing using network error page
    MFSA 2010-82 Incomplete fix for CVE-2010-0179
    MFSA 2010-81 Integer overflow vulnerability in NewIdArray
    MFSA 2010-80 Use-after-free error with nsDOMAttribute MutationObserver
    MFSA 2010-79 Java security bypass from LiveConnect loaded via data: URL meta
                 refresh
    MFSA 2010-78 Add support for OTS font sanitizer
    MFSA 2010-77 Crash and remote code execution using HTML tags inside a XUL tree
    MFSA 2010-76 Chrome privilege escalation with window.open and <isindex> element
    MFSA 2010-75 Buffer overflow while line breaking after document.write with
                 long string
    MFSA 2010-74 Miscellaneous memory safety hazards (rv:1.9.2.13/ 1.9.1.16)

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.16 2010/10/28 21:33:59 tron Exp $
+# $NetBSD: dist.mk,v 1.17 2010/12/11 14:46:29 tnn Exp $
 #
 # used by devel/nspr/Makefile
 # used by devel/nss/Makefile
@@ -8,7 +8,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	3.6${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	1.9.2
-MOZ_BRANCH_MINOR=	.12
+MOZ_BRANCH_MINOR=	.13
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
