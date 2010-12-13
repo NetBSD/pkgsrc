ceba711f493377809315e7eef98661e25f0a439e
gls <gls>
2010-12-13 20:32:30 +0000

    Update libraw to 0.12.0
    Complete changelist:
    
    * DCB interpolation and FBDD denoising integrated into main LibRaw.
    * Two addtional demosaic packs:
    o GPL2-licensed: AFD, LMMSE, VCD, Modified AHD and AHD+VCD interpolations.
    Additional median filters from 'Modified DCRAW' package. Foveon support also
    included in this pack.
    o GPL3-licensed: AMaZE interpolation and chromatic aberrations correction from
    RawTherapee
    * LCMS 2.x support
    * New ./configure script, based on GNU autotools.
    * Bugfixes:
    o Fix in green_matching code to support nonstandard Bayer pattern
    o Fixed bug in add_masked_borders_to_bitmap() call for cameras with masked
    border composed of odd number of pixels.
    * New command line options for unprocessed_raw sample: -B - subtract black
    level, -M - add masked pixels to bitmap.

--- graphics/libraw/buildlink3.mk
+++ graphics/libraw/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/11/15 18:59:38 gls Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/12/13 20:32:30 gls Exp $
 
 BUILDLINK_TREE+=	libraw
 
 .if !defined(LIBRAW_BUILDLINK3_MK)
 LIBRAW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libraw+=	libraw>=0.10.0
+BUILDLINK_API_DEPENDS.libraw+=	libraw>=0.12.0
 BUILDLINK_PKGSRCDIR.libraw?=	../../graphics/libraw
 .endif	# LIBRAW_BUILDLINK3_MK
 
