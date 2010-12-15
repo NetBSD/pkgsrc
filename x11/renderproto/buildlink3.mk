71fb3dba9274b930a6189fc89b2acaca7d094e55
abs <abs@pkgsrc.org>
2010-12-15 09:33:13 +0000

    Use renderproto9 for X11_TYPE native and! BUILTIN_X11_VERSION.xorg

--- x11/renderproto/buildlink3.mk
+++ x11/renderproto/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:25:50 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2010/12/15 09:33:13 abs Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -7,8 +7,14 @@ BUILDLINK_TREE+=	renderproto
 .if !defined(RENDERPROTO_BUILDLINK3_MK)
 RENDERPROTO_BUILDLINK3_MK:=
 
+.if ${X11_TYPE} == "native" && !defined(BUILTIN_X11_VERSION.xorg)
+BUILDLINK_API_DEPENDS.renderproto+=	renderproto>=0.9.3nb1
+BUILDLINK_PKGSRCDIR.renderproto?=	../../x11/renderproto9
+.else
 BUILDLINK_API_DEPENDS.renderproto+=	renderproto>=0.9.3nb1
 BUILDLINK_PKGSRCDIR.renderproto?=	../../x11/renderproto
+.endif
+
 BUILDLINK_DEPMETHOD.renderproto?=	build
 .endif # RENDERPROTO_BUILDLINK3_MK
 
