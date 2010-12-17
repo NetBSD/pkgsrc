95a219af4ee78e2724104f3c996483f7c78432cf
obache <obache@pkgsrc.org>
2010-12-17 07:36:08 +0000

    Bump PKGREVISION from icu shlib major bump.

--- www/epiphany/buildlink3.mk
+++ www/epiphany/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.36 2010/11/15 22:58:50 abs Exp $
+# $NetBSD: buildlink3.mk,v 1.37 2010/12/17 07:36:10 obache Exp $
 #
 
 BUILDLINK_TREE+=	epiphany
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	epiphany
 EPIPHANY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.epiphany+=		epiphany>=2.28
-BUILDLINK_ABI_DEPENDS.epiphany?=		epiphany>=2.30.5nb2
+BUILDLINK_ABI_DEPENDS.epiphany?=		epiphany>=2.30.6nb1
 BUILDLINK_PKGSRCDIR.epiphany?=		../../www/epiphany
 
 .include "../../mk/bsd.fast.prefs.mk"
