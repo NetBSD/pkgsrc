0b62fd560e2b95504f1e79a920009b2262f25aac
adam <adam@pkgsrc.org>
2010-12-19 09:53:41 +0000

    Changes 8.4.6: * Force the default wal_sync_method to be fdatasync on Linux * Fix assorted bugs in WAL replay logic for GIN indexes * Fix recovery from base backup when the starting checkpoint WAL   record is not in the same WAL segment as its redo point * Fix persistent slowdown of autovacuum workers when multiple workers   remain active for a long time * Add support for detecting register-stack overrun on IA64 * Add a check for stack overflow in copyObject() * Fix detection of page splits in temporary GiST indexes * Fix error checking during early connection processing * Improve efficiency of window functions * Avoid memory leakage while "ANALYZE"'ing complex index expressions * Ensure an index that uses a whole-row Var still depends on its table * Do not "inline" a SQL function with multiple OUT parameters * Behave correctly if ORDER BY, LIMIT, FOR UPDATE, or WITH is   attached to the VALUES part of INSERT ... VALUES * Fix constant-folding of COALESCE() expressions * Fix postmaster crash when connection acceptance (accept() or one of   the calls made immediately after it) fails, and the postmaster was   compiled with GSSAPI support * Fix missed unlink of temporary files when log_temp_files is active * Add print functionality for InhRelation nodes * Fix incorrect calculation of distance from a point to a horizontal   line segment * Fix incorrect calculation of transaction status in ecpg * Fix PL/pgSQL's handling of "simple" expressions to not fail in   recursion or error-recovery cases * Fix PL/Python's handling of set-returning functions * Fix bug in "contrib/cube"'s GiST picksplit algorithm * Don't emit "identifier will be truncated" notices in   "contrib/dblink" except when creating new connections * Fix potential coredump on missing public key in "contrib/pgcrypto" * Fix memory leak in "contrib/xml2"'s XPath query functions * Update time zone data files to tzdata release 2010o for DST law   changes in Fiji and Samoa; also historical corrections for Hong   Kong.

--- databases/postgresql84-client/buildlink3.mk
+++ databases/postgresql84-client/buildlink3.mk
@@ -1,6 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2010/05/20 12:36:39 adam Exp $
-
-.include "../../mk/bsd.fast.prefs.mk"
+# $NetBSD: buildlink3.mk,v 1.4 2010/12/19 09:53:41 adam Exp $
 
 BUILDLINK_TREE+=	postgresql84-client
 
@@ -8,17 +6,11 @@ BUILDLINK_TREE+=	postgresql84-client
 POSTGRESQL84_CLIENT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.postgresql84-client+=	postgresql84-client>=8.4.0
-BUILDLINK_ABI_DEPENDS.postgresql84-client?=	postgresql84-client>=8.4.2nb1
+BUILDLINK_ABI_DEPENDS.postgresql84-client+=	postgresql84-client>=8.4.2nb1
 BUILDLINK_PKGSRCDIR.postgresql84-client?=	../../databases/postgresql84-client
-
-.if defined(PG_SUBPREFIX) && !empty(PG_SUBPREFIX)
-BUILDLINK_INCDIRS.postgresql84-client?=	${PG_SUBPREFIX}/include
-BUILDLINK_LIBDIRS.postgresql84-client?=	${PG_SUBPREFIX}/lib
-.endif
 
 # This variable contains the libraries need to link most clients.
 BUILDLINK_LDADD.postgresql84-client=	-lpq ${BUILDLINK_LDADD.gettext}
-
 BUILDLINK_FILES.postgresql84-client+=	bin/pg_config
 
 .include "../../security/openssl/buildlink3.mk"
