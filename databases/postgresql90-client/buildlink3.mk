6e71669c9454fb888a46e83e4463dccffceb3694
adam <adam@pkgsrc.org>
2010-12-19 09:53:26 +0000

    Changes 9.0.2: * Force the default wal_sync_method to be fdatasync on Linux * Fix "too many KnownAssignedXids" error during Hot Standby replay * Fix race condition in lock acquisition during Hot Standby * Avoid unnecessary conflicts during Hot Standby * Fix assorted bugs in WAL replay logic for GIN indexes * Fix recovery from base backup when the starting checkpoint WAL   record is not in the same WAL segment as its redo point * Fix corner-case bug when streaming replication is enabled   immediately after creating the master database cluster * Fix persistent slowdown of autovacuum workers when multiple workers   remain active for a long time * Fix long-term memory leak in autovacuum launcher * Avoid failure when trying to report an impending transaction   wraparound condition from outside a transaction * Add support for detecting register-stack overrun on IA64 * Add a check for stack overflow in copyObject() * Fix detection of page splits in temporary GiST indexes * Fix error checking during early connection processing * Improve efficiency of window functions * Avoid memory leakage while "ANALYZE"'ing complex index expressions * Ensure an index that uses a whole-row Var still depends on its table * Add missing support in "DROP OWNED BY" for removing foreign data   wrapper/server privileges belonging to a user * Do not "inline" a SQL function with multiple OUT parameters * Fix crash when inline-ing a set-returning function whose argument   list contains a reference to an inline-able user function * Behave correctly if ORDER BY, LIMIT, FOR UPDATE, or WITH is   attached to the VALUES part of INSERT ... VALUES * Make the OFF keyword unreserved   This prevents problems with using off as a variable name in   PL/pgSQL. That worked before 9.0, but was now broken because   PL/pgSQL now treats all core reserved words as reserved. * Fix constant-folding of COALESCE() expressions

--- databases/postgresql90-client/buildlink3.mk
+++ databases/postgresql90-client/buildlink3.mk
@@ -1,6 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/09/21 07:52:41 adam Exp $
-
-.include "../../mk/bsd.fast.prefs.mk"
+# $NetBSD: buildlink3.mk,v 1.2 2010/12/19 09:53:27 adam Exp $
 
 BUILDLINK_TREE+=	postgresql90-client
 
@@ -8,21 +6,15 @@ BUILDLINK_TREE+=	postgresql90-client
 POSTGRESQL90_CLIENT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.postgresql90-client+=	postgresql90-client>=9.0.0
-BUILDLINK_ABI_DEPENDS.postgresql90-client?=	postgresql90-client>=9.0.0
+BUILDLINK_ABI_DEPENDS.postgresql90-client+=	postgresql90-client>=9.0.0
 BUILDLINK_PKGSRCDIR.postgresql90-client?=	../../databases/postgresql90-client
-
-.if defined(PG_SUBPREFIX) && !empty(PG_SUBPREFIX)
-BUILDLINK_INCDIRS.postgresql90-client?=	${PG_SUBPREFIX}/include
-BUILDLINK_LIBDIRS.postgresql90-client?=	${PG_SUBPREFIX}/lib
-.endif
 
 # This variable contains the libraries need to link most clients.
 BUILDLINK_LDADD.postgresql90-client=	-lpq ${BUILDLINK_LDADD.gettext}
-
 BUILDLINK_FILES.postgresql90-client+=	bin/pg_config
 
-.include "../../security/openssl/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
 .endif # POSTGRESQL90_CLIENT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-postgresql90-client
