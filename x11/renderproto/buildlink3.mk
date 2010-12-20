8c9cacd8aeb4df969ee72fd28844993c1502d840
wiz <wiz>
2010-12-20 06:31:00 +0000

    Revert previous, it broke building cairo on NetBSD-current with builtin X.

--- x11/renderproto/buildlink3.mk
+++ x11/renderproto/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2010/12/15 09:33:13 abs Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2010/12/20 06:31:00 wiz Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -7,13 +7,8 @@ BUILDLINK_TREE+=	renderproto
 .if !defined(RENDERPROTO_BUILDLINK3_MK)
 RENDERPROTO_BUILDLINK3_MK:=
 
-.if ${X11_TYPE} == "native" && !defined(BUILTIN_X11_VERSION.xorg)
-BUILDLINK_API_DEPENDS.renderproto+=	renderproto>=0.9.3nb1
-BUILDLINK_PKGSRCDIR.renderproto?=	../../x11/renderproto9
-.else
 BUILDLINK_API_DEPENDS.renderproto+=	renderproto>=0.9.3nb1
 BUILDLINK_PKGSRCDIR.renderproto?=	../../x11/renderproto
-.endif
 
 BUILDLINK_DEPMETHOD.renderproto?=	build
 .endif # RENDERPROTO_BUILDLINK3_MK
