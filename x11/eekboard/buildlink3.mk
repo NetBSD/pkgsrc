f5736624710e75ab9d5bf5a9e84c8e25cd06e7d4
obache <obache>
2010-12-23 05:47:32 +0000

    * BUILDLINK_ABI_DEPENDS should be "+=". * buildlink with clutter conditionally.

--- x11/eekboard/buildlink3.mk
+++ x11/eekboard/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2010/11/15 22:58:55 abs Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2010/12/23 05:47:32 obache Exp $
 #
 
 BUILDLINK_TREE+=	eekboard
@@ -7,14 +7,20 @@ BUILDLINK_TREE+=	eekboard
 EEKBOARD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.eekboard+=	eekboard>=0.0.4
-BUILDLINK_ABI_DEPENDS.eekboard?=	eekboard>=0.0.5nb2
+BUILDLINK_ABI_DEPENDS.eekboard+=	eekboard>=0.0.5nb2
 BUILDLINK_PKGSRCDIR.eekboard?=	../../x11/eekboard
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+pkgbase := eekboard
+.include "../../mk/pkg-build-options.mk"
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/pango/buildlink3.mk"
 .include "../../graphics/cairo/buildlink3.mk"
+.if !empty(PKG_BUILD_OPTIONS.eekboard:Mclutter)
 .include "../../graphics/clutter/buildlink3.mk"
-.include "../../graphics/clutter-gtk/buildlink3.mk"
+.endif
 .include "../../x11/gtk2/buildlink3.mk"
 .include "../../x11/libxklavier/buildlink3.mk"
 #.include "../../x11/libfakekey/buildlink3.mk"
