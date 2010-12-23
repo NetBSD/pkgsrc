30679f52065ff3e92d9ea3f475e8b20dce56db1a
fhajny <fhajny>
2010-12-23 12:24:00 +0000

    Added BUILDLINK_INCDIRS and BUILDLINK_LDFLAGS support to bl3, to help with building lang/spidermonkey.

--- devel/nspr/buildlink3.mk
+++ devel/nspr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2010/09/12 09:11:49 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2010/12/23 12:24:00 fhajny Exp $
 
 BUILDLINK_TREE+=	nspr
 
@@ -13,6 +13,10 @@ BUILDLINK_FILES.nspr+=          include/nspr/*
 
 BUILDLINK_LIBDIRS.nspr+=	lib/nspr
 BUILDLINK_RPATHDIRS.nspr+=	lib/nspr
+
+BUILDLINK_INCDIRS.nspr+=	include/nspr
+
+BUILDLINK_LDFLAGS.nspr+=	-lnspr4
 
 .endif	# NSPR_BUILDLINK3_MK
 
