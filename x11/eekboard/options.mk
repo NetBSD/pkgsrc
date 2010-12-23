a1f194755d897167de16a47b94092eea13abec76
obache <obache@pkgsrc.org>
2010-12-23 06:04:36 +0000

    Fixes dependency for clutter, clutter-gtk is required.

--- x11/eekboard/options.mk
+++ x11/eekboard/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2010/11/22 13:31:05 obache Exp $
+# $NetBSD: options.mk,v 1.3 2010/12/23 06:04:36 obache Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.eekboard
@@ -12,6 +12,7 @@ PLIST_VARS+=	clutter
 .if !empty(PKG_OPTIONS:Mclutter)
 PLIST.clutter=	yes
 .include "../../graphics/clutter/buildlink3.mk"
+.include "../../graphics/clutter-gtk/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-clutter=yes
 .else
 CONFIGURE_ARGS+=	--enable-clutter=no
