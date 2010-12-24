1ebff1f140bca504b6c47fd5df158fd2fad291db
asau <asau@pkgsrc.org>
2010-12-24 08:07:57 +0000

    Restore PG_SUBPREFIX.

--- databases/postgresql90-client/buildlink3.mk
+++ databases/postgresql90-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/12/19 09:53:27 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2010/12/24 08:10:36 asau Exp $
 
 BUILDLINK_TREE+=	postgresql90-client
 
@@ -9,9 +9,18 @@ BUILDLINK_API_DEPENDS.postgresql90-client+=	postgresql90-client>=9.0.0
 BUILDLINK_ABI_DEPENDS.postgresql90-client+=	postgresql90-client>=9.0.0
 BUILDLINK_PKGSRCDIR.postgresql90-client?=	../../databases/postgresql90-client
 
+.if defined(PG_SUBPREFIX) && !empty(PG_SUBPREFIX)
+BUILDLINK_INCDIRS.postgresql90-client?=	${PG_SUBPREFIX}/include
+BUILDLINK_LIBDIRS.postgresql90-client?=	${PG_SUBPREFIX}/lib
+.endif
+
 # This variable contains the libraries need to link most clients.
 BUILDLINK_LDADD.postgresql90-client=	-lpq ${BUILDLINK_LDADD.gettext}
+.if defined(PG_SUBPREFIX) && !empty(PG_SUBPREFIX)
+BUILDLINK_FILES.postgresql90-client+=	$(PG_SUBPREFIX:/=)/bin/pg_config
+.else
 BUILDLINK_FILES.postgresql90-client+=	bin/pg_config
+.endif
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
