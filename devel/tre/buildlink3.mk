07cfdecc1bc074f7cf2ffb8c8f068bbdfc875573
obache <obache@pkgsrc.org>
2010-12-28 08:40:16 +0000

    buildlink with gettext-lib additionally. libtre.la refer -lintl.

--- devel/tre/buildlink3.mk
+++ devel/tre/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:24:30 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2010/12/28 08:40:16 obache Exp $
 
 BUILDLINK_TREE+=	tre
 
@@ -8,6 +8,8 @@ TRE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.tre+=		tre>=0.7.2
 BUILDLINK_PKGSRCDIR.tre?=		../../devel/tre
 BUILDLINK_CPPFLAGS.tre+=		-I${BUILDLINK_PREFIX.tre}/include/tre
+
+.include "../../devel/gettext-lib/buildlink3.mk"
 .endif # TRE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-tre
