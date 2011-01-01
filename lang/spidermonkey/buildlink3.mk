0712d37ce3a1873d92f71dc6e26afe849346c971
wiz <wiz@pkgsrc.org>
2011-01-01 14:03:42 +0000

    Only include nspr and threads bl3.mk files when threading is enabled; otherwise they are not pulled in by the base package and the buildlink step fails. (Broke avidemux.)

--- lang/spidermonkey/buildlink3.mk
+++ lang/spidermonkey/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/05/20 00:58:20 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2011/01/01 14:03:42 wiz Exp $
 
 BUILDLINK_TREE+=	spidermonkey
 
@@ -16,7 +16,14 @@ BUILDLINK_ABI_DEPENDS.spidermonkey?=	spidermonkey>=1.5nb8
 BUILDLINK_PKGSRCDIR.spidermonkey?=	../../lang/spidermonkey
 
 .include "../../devel/readline/buildlink3.mk"
-.include "../../devel/nspr/buildlink3.mk"
+
+pkgbase := spidermonkey
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.spidermonkey:Mthreads)
+.  include "../../devel/nspr/buildlink3.mk"
+.  include "../../mk/pthread.buildlink3.mk"
+.endif
 .endif # SPIDERMONKEY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-spidermonkey
