c6c2f6b9b99d3ff85edb88847c166b390605d346
dsainty <dsainty@pkgsrc.org>
2011-01-03 08:47:54 +0000

    Since ilmbase 1.0.2 is not reliably usable on some platforms, bump required version to 1.0.2nb1.

--- graphics/ilmbase/buildlink3.mk
+++ graphics/ilmbase/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:40 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/01/03 08:47:54 dsainty Exp $
 
 BUILDLINK_TREE+=	ilmbase
 
 .if !defined(ILMBASE_BUILDLINK3_MK)
 ILMBASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ilmbase+=	ilmbase>=1.0.1
+BUILDLINK_API_DEPENDS.ilmbase+=	ilmbase>=1.0.2nb1
 BUILDLINK_PKGSRCDIR.ilmbase?=	../../graphics/ilmbase
 .endif # ILMBASE_BUILDLINK3_MK
 
