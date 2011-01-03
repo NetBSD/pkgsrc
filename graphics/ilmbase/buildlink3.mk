95fe02e95683549667e50e15e54bec4e5bd8b1c2
wiz <wiz@pkgsrc.org>
2011-01-03 17:58:35 +0000

    Since openexr wants ilmbase>=1.0.2nb1, let openexr demand it instead of changing ilmbase's bl3.mk.

--- graphics/ilmbase/buildlink3.mk
+++ graphics/ilmbase/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/01/03 08:47:54 dsainty Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/01/03 17:58:35 wiz Exp $
 
 BUILDLINK_TREE+=	ilmbase
 
 .if !defined(ILMBASE_BUILDLINK3_MK)
 ILMBASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ilmbase+=	ilmbase>=1.0.2nb1
+BUILDLINK_API_DEPENDS.ilmbase+=	ilmbase>=1.0.1
 BUILDLINK_PKGSRCDIR.ilmbase?=	../../graphics/ilmbase
 .endif # ILMBASE_BUILDLINK3_MK
 
