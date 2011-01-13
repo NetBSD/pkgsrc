da4a44bbddaa24ac6368bc3da61ef44719df3cbd
drochner <drochner@pkgsrc.org>
2011-01-13 16:05:00 +0000

    update to 2.22.0 This switches to the gnome-2.32 release branch. (recursive PKGREV bump needed due to the new atkmm dependency -- we ride on the revbump due to png which covers everything depending on gtk2)

--- x11/gtkmm/buildlink3.mk
+++ x11/gtkmm/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.24 2011/01/13 13:36:24 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2011/01/13 16:05:00 drochner Exp $
 
 BUILDLINK_TREE+=	gtkmm
 
 .if !defined(GTKMM_BUILDLINK3_MK)
 GTKMM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gtkmm+=	gtkmm>=2.20.3
+BUILDLINK_API_DEPENDS.gtkmm+=	gtkmm>=2.22.0
 BUILDLINK_ABI_DEPENDS.gtkmm+=	gtkmm>=2.20.3nb5
 BUILDLINK_PKGSRCDIR.gtkmm?=	../../x11/gtkmm
 
+.include "../../devel/atkmm/buildlink3.mk"
 .include "../../devel/pangomm/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .endif # GTKMM_BUILDLINK3_MK
