cad9cc93a939e2d5582ed246b9479a610850582c
tron <tron@pkgsrc.org>
2011-01-19 17:50:48 +0000

    Add proper builtin support which detects that "libXvMCW" is missing under NetBSD.
    Tested under NetBSD/amd64 5.1 ("libXvMCW" missing) and Mac OS 10.6.6
    ("libXvMCW" present).

--- x11/libXvMC/buildlink3.mk
+++ x11/libXvMC/buildlink3.mk
@@ -1,10 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:25:46 joerg Exp $
-
-.include "../../mk/bsd.fast.prefs.mk"
-
-.if ${X11_TYPE} != "modular"
-.include "../../mk/x11.buildlink3.mk"
-.else
+# $NetBSD: buildlink3.mk,v 1.4 2011/01/19 17:50:48 tron Exp $
 
 BUILDLINK_TREE+=	libXvMC
 
@@ -21,5 +15,3 @@ BUILDLINK_PKGSRCDIR.libXvMC?=	../../x11/libXvMC
 .endif # LIBXVMC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libXvMC
-
-.endif
