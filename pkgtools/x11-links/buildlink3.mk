b9071bc616497877c955ded81055c0d520cf0495
jmcneill <jmcneill@pkgsrc.org>
2011-01-19 03:39:13 +0000

    Update x11-links to 0.62: add libdrm_intel and libdrm_radeon

--- pkgtools/x11-links/buildlink3.mk
+++ pkgtools/x11-links/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.35 2010/11/23 13:24:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2011/01/19 03:39:13 jmcneill Exp $
 #
 # Don't include this file manually!  It will be included as necessary
 # by bsd.buildlink3.mk.
@@ -12,7 +12,7 @@ BUILDLINK_TREE+=	x11-links
 .if !defined(X11_LINKS_BUILDLINK3_MK)
 X11_LINKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.61
+BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.62
 BUILDLINK_PKGSRCDIR.x11-links?=	../../pkgtools/x11-links
 BUILDLINK_DEPMETHOD.x11-links?=	build
 
