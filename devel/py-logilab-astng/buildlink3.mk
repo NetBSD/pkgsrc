a70cd48112bb5d4ce8d5e421e42b302291cd3084
tnn <tnn@pkgsrc.org>
2011-01-23 22:28:17 +0000

    include pyversion.mk for ${PYPKGPREFIX}

--- devel/py-logilab-astng/buildlink3.mk
+++ devel/py-logilab-astng/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/01/11 20:42:34 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/01/23 22:28:17 tnn Exp $
 
 BUILDLINK_TREE+=	py-llab-astng
 
 .if !defined(PY26_LOGILAB_ASTNG_BUILDLINK3_MK)
 PY26_LOGILAB_ASTNG_BUILDLINK3_MK:=
+
+.include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.py-llab-astng+= ${PYPKGPREFIX}-logilab-astng>=0.21.0
 BUILDLINK_PKGSRCDIR.py-llab-astng?=	../../devel/py-logilab-astng
