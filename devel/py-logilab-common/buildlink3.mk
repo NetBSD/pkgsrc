fef6e02a929758e1a1b5956a5dc09d9cb6ee48c3
tnn <tnn@pkgsrc.org>
2011-01-23 22:13:42 +0000

    need pyversion.mk for ${PYPKGPREFIX} here.

--- devel/py-logilab-common/buildlink3.mk
+++ devel/py-logilab-common/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/01/11 20:41:00 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/01/23 22:13:42 tnn Exp $
 
 BUILDLINK_TREE+=	py-llab-common
 
 .if !defined(PY_LOGILAB_COMMON_BUILDLINK3_MK)
 PY_LOGILAB_COMMON_BUILDLINK3_MK:=
+
+.include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.py-llab-common+= ${PYPKGPREFIX}-logilab-common>=0.53.0
 BUILDLINK_PKGSRCDIR.py-llab-common?=	../../devel/py-logilab-common
