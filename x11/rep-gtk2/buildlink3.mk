4b5adaa6c3693f1eab0dbb235dcc110f9ed3b36e
tnn <tnn@pkgsrc.org>
2011-01-23 23:56:53 +0000

    Update to rep-gtk2-0.90.4.
    0.90.4:
    doc Update.
    
    0.90.3:
    small bugfixes [Vincent Untz]
    improved spec [Kim B. Heino]
    SCM_CDR/SCM_CAR >> rep_CDR/rep_CAR [Alexey I. Froloff]

--- x11/rep-gtk2/buildlink3.mk
+++ x11/rep-gtk2/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2009/04/16 18:24:27 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/01/23 23:56:53 tnn Exp $
 
 BUILDLINK_TREE+=	rep-gtk2
 
 .if !defined(REP_GTK2_BUILDLINK3_MK)
 REP_GTK2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rep-gtk2+=	rep-gtk2>=0.18.4
+BUILDLINK_API_DEPENDS.rep-gtk2+=	rep-gtk2>=0.90.4
 BUILDLINK_PKGSRCDIR.rep-gtk2?=	../../x11/rep-gtk2
 
 .endif	# REP_GTK2_BUILDLINK3_MK
