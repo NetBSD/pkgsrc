fcabb9cbea64a97d419915f249151aacea159715
drochner <drochner@pkgsrc.org>
2011-01-25 18:31:56 +0000

    propagate x11 dependency if necessary

--- graphics/libwmf/buildlink3.mk
+++ graphics/libwmf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2011/01/13 13:36:12 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2011/01/25 18:31:56 drochner Exp $
 
 BUILDLINK_TREE+=	libwmf
 
@@ -8,6 +8,12 @@ LIBWMF_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libwmf+=	libwmf>=0.2.8nb2
 BUILDLINK_ABI_DEPENDS.libwmf+=	libwmf>=0.2.8.4nb9
 BUILDLINK_PKGSRCDIR.libwmf?=	../../graphics/libwmf
+
+pkgbase:= libwmf
+.include "../../mk/pkg-build-options.mk"
+.if !empty(PKG_BUILD_OPTIONS.libwmf:Mx11)
+.include "../../x11/libX11/buildlink3.mk"
+.endif
 
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
