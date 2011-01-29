72138e1324856f8dcedc7b37795e6cd28182524c
obache <obache@pkgsrc.org>
2011-01-29 11:53:17 +0000

    Fix automatic BUILDLINK_ABI_DEPENDS pattern update.

--- x11/ruby-gnome2-gtk/buildlink3.mk
+++ x11/ruby-gnome2-gtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2011/01/13 13:36:37 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2011/01/29 11:55:44 obache Exp $
 #
 
 BUILDLINK_TREE+=	ruby-gnome2-gtk
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	ruby-gnome2-gtk
 RUBY_GNOME2_GTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-gtk+=	${RUBY_PKGPREFIX}-gnome2-gtk>=0.17.0
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk?=	ruby18-gnome2-gtk>=0.90.5nb1
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk?=	${RUBY_PKGPREFIX}-gnome2-gtk>=0.90.5nb1
 BUILDLINK_PKGSRCDIR.ruby-gnome2-gtk?=	../../x11/ruby-gnome2-gtk
 
 .include "../../devel/ruby-gnome2-glib/buildlink3.mk"
