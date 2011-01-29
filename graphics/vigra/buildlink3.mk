13bed4b213a311044f85412ab51d50d3e1f438df
adam <adam@pkgsrc.org>
2011-01-29 16:29:03 +0000

    Changes 1.7.1: * Fixed the build process for MacOS X. * Re-activeted vigra-config (script to query VIGRA installation information)   and added VigraConfig.cmake (query VIGRA installation information from within   cmake). * Added CDash support (nightly builds and tests). * Added convexHull(). * Added vigra::Box. * Added vigra::Sampler class to sample given data in various ways. * Added much new functionality to the vigra::RandomForest class (e.g. more   split strategies, variable importance measures, feature selection) * Added readSIF() (reader for the Andor SIF file format). * Added vigra::HDF5File for easier navigation in HDF5 files. * Added recursive approximation of the Gaussian filter   (recursiveGaussianFilterX(), recursiveGaussianFilterY()) * vigranumpy: added Gabor filtering. * Fixed multi-threading bugs at various places. * Minor improvements and bug fixes in the code and documentation.

--- graphics/vigra/buildlink3.mk
+++ graphics/vigra/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/01/13 13:36:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/01/29 16:29:03 adam Exp $
 
 BUILDLINK_TREE+=	vigra
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	vigra
 VIGRA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.vigra+=	vigra>=1.7.0
-BUILDLINK_ABI_DEPENDS.vigra?=	vigra>=1.7.0nb1
+BUILDLINK_ABI_DEPENDS.vigra+=	vigra>=1.7.0nb1
 BUILDLINK_PKGSRCDIR.vigra?=	../../graphics/vigra
 
 .include "../../devel/zlib/buildlink3.mk"
