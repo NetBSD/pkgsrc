f34caa8747e1b904069d87580c2354e1402546b7
rumko <rumko@pkgsrc.org>
2011-01-30 17:58:06 +0000

    lang/php53: add the missing suhosin patch
    5.3.5 was released due to a critical issue and the previous suhosin
    patch still applies. Prior art of this can be seen in OpenBSD's and
    FreeBSD's ports.
    
    ok@ wiz

--- lang/php53/Makefile.php
+++ lang/php53/Makefile.php
@@ -1,4 +1,4 @@
-# $NetBSD: Makefile.php,v 1.3 2010/07/24 22:23:37 tron Exp $
+# $NetBSD: Makefile.php,v 1.4 2011/01/30 17:58:06 rumko Exp $
 # used by lang/php53/Makefile
 # used by www/ap-php/Makefile
 
@@ -59,8 +59,8 @@ PKG_SUGGESTED_OPTIONS+=	inet6 ssl
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Msuhosin)
-SUHOSIN_PHPVER=		5.3.3
-. if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS}
+SUHOSIN_PHPVER=		5.3.4
+. if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS} && (${SUHOSIN_PHPVER} != "5.3.4" || ${PHP_BASE_VERS} != "5.3.5")
 PKG_FAIL_REASON+=	"The suhosin patch is currently not available for"
 PKG_FAIL_REASON+=	"this version of PHP.  You may have to wait until"
 PKG_FAIL_REASON+=	"an updated patch is released or temporarily"
