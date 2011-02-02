57355fdc56f1618f24d254734dba3a6de20d48f9
adam <adam@pkgsrc.org>
2011-02-02 13:18:47 +0000

    Changes 2.7.9:   John: Just a quick release for a security fix here.  Elliott has not   yet had a chance to work on the MSN breakage that's been present in   the last couple releases, but we hope he can do it before 2.7.10!
    Changes 2.7.8:
      Elliott: OK, so I know a few things broke with the last release, and
      it's too bad we had to rush it for that silly certificate thing that
      the MSN people can't configure properly.  I've certainly done a lot of
      small fixes this time, but it's too bad we haven't been able to get the
      transfers with the official client fixed yet.  I promise it'll be in
      the next release (barring any quick security issues).
    
      John: So, it's been about a month since we last released.  Again, we've
      assembled a bugfix release for your enjoyment.  While a few commonly
      reported bugs remain, particularly in MSN, we're working on it for the
      next release.  In the meantime, Merry Christmas and enjoy!
    
    Changes 2.7.7:
      John: Well, this time around, we should finally have the certificate
      issue really and fully fixed for all of you MSN users.  Also, we have
      a few AIM-related fixes in this release, most notably the fix for the
      new "SSL Handshake Failure" message some of you got after upgrading.
      That one was an oversight on our part.  Enjoy the fixes!

--- chat/libpurple/buildlink3.mk
+++ chat/libpurple/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.19 2011/01/13 13:36:45 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2011/02/02 13:18:48 adam Exp $
 
 BUILDLINK_TREE+=	libpurple
 
 .if !defined(LIBPURPLE_BUILDLINK3_MK)
 LIBPURPLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libpurple+=	libpurple>=2.5.0
-BUILDLINK_ABI_DEPENDS.libpurple?=	libpurple>=2.7.3nb3
+BUILDLINK_API_DEPENDS.libpurple+=	libpurple>=2.7.9
+BUILDLINK_ABI_DEPENDS.libpurple+=	libpurple>=2.7.9
 BUILDLINK_PKGSRCDIR.libpurple?=	../../chat/libpurple
 
 pkgbase := libpurple
