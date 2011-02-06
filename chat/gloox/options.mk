362fbb8ab68ef3b129edcd1a64808be68226224f
schnoebe <schnoebe@pkgsrc.org>
2011-02-06 19:46:48 +0000

    Import gloox-1.0 into pkgsrc.
    gloox is a rock-solid, full-featured Jabber/XMPP client library,
    written in C++. It makes writing spec-compliant clients easy and
    allows for hassle-free integration of Jabber/XMPP functionality
    into existing applications.

---
+++ chat/gloox/options.mk
@@ -0,0 +1,31 @@
+# $NetBSD: options.mk,v 1.1.1.1 2011/02/06 19:46:48 schnoebe Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.gloox
+PKG_OPTIONS_REQUIRED_GROUPS=	tls
+# TLS selections
+PKG_OPTIONS_GROUP.tls=		gnutls openssl
+# debugging
+PKG_SUPPORTED_OPTIONS+=		debug
+PKG_SUGGESTED_OPTIONS=		gnutls
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	gnutls openssl
+
+.if !empty(PKG_OPTIONS:Mgnutls)
+PLIST.gnutls=		yes
+CONFIGURE_ARGS+=	--with-gnutls=${BUILDLINK_PREFIX.gnutls}
+.  include "../../security/gnutls/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-gnutls
+.endif
+
+.if !empty(PKG_OPTIONS:Mopenssl)
+PLIST.openssl=		yes
+CONFIGURE_ARGS+=	--with-openssl=${BUILDLINK_PREFIX.openssl}
+.  include "../../security/openssl/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug
+.endif
