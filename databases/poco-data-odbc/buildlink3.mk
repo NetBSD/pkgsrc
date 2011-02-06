3816fe39c2d1dc386c37fef7a1ddb0ea8be8ae98
schnoebe <schnoebe@pkgsrc.org>
2011-02-06 06:08:06 +0000

    Oops, drat.. Forgot  to purge the wip references from the packages before importing.
    wip  references now all purged.

--- databases/poco-data-odbc/buildlink3.mk
+++ databases/poco-data-odbc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/02/06 06:02:52 schnoebe Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/02/06 06:08:07 schnoebe Exp $
 
 BUILDLINK_TREE+=	poco-data-odbc
 
@@ -7,10 +7,10 @@ POCO_DATA_ODBC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poco-data-odbc+=	poco-data-odbc>=1.3.5
 BUILDLINK_ABI_DEPENDS.poco-data-odbc?=	poco-data-odbc>=1.3.5nb1
-BUILDLINK_PKGSRCDIR.poco-data-odbc?=	../../wip/poco-data-odbc
+BUILDLINK_PKGSRCDIR.poco-data-odbc?=	../../databases/poco-data-odbc
 
 .include "../../databases/unixodbc/buildlink3.mk"
-.include "../../wip/poco-data/buildlink3.mk"
+.include "../../databases/poco-data/buildlink3.mk"
 .endif # POCO_DATA_ODBC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-poco-data-odbc
--- devel/poco/buildlink3.mk
+++ devel/poco/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/02/06 05:55:15 schnoebe Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/02/06 06:08:07 schnoebe Exp $
 
 BUILDLINK_TREE+=	poco
 
@@ -7,7 +7,7 @@ POCO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poco+=	poco>=1.3.5
 BUILDLINK_ABI_DEPENDS.poco?=	poco>=1.3.5nb1
-BUILDLINK_PKGSRCDIR.poco?=	../../wip/poco
+BUILDLINK_PKGSRCDIR.poco?=	../../devel/poco
 
 .include "../../security/openssl/buildlink3.mk"
 .endif # POCO_BUILDLINK3_MK
