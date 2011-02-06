d43a98a1a8657603cc8737f128d532dd47648129
schnoebe <schnoebe@pkgsrc.org>
2011-02-06 06:08:06 +0000

    Oops, drat.. Forgot  to purge the wip references from the packages before importing.
    wip  references now all purged.

--- databases/poco-data-mysql/buildlink3.mk
+++ databases/poco-data-mysql/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/02/06 06:02:02 schnoebe Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/02/06 06:08:07 schnoebe Exp $
 
 BUILDLINK_TREE+=	poco-data-mysql
 
@@ -7,10 +7,10 @@ POCO_DATA_MYSQL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poco-data-mysql+=	poco-data-mysql>=1.3.5
 BUILDLINK_ABI_DEPENDS.poco-data-mysql?=	poco-data-mysql>=1.3.5nb1
-BUILDLINK_PKGSRCDIR.poco-data-mysql?=	../../wip/poco-data-mysql
+BUILDLINK_PKGSRCDIR.poco-data-mysql?=	../../databases/poco-data-mysql
 
 .include "../../databases/mysql5-client/buildlink3.mk"
-.include "../../wip/poco-data/buildlink3.mk"
+.include "../../databases/poco-data/buildlink3.mk"
 .endif # POCO_DATA_MYSQL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-poco-data-mysql
