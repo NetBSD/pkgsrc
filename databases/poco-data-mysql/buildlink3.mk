52527cd8ee374356f2b80a9cafa63a980524dd28
schnoebe <schnoebe@pkgsrc.org>
2011-02-06 06:02:02 +0000

    Import the MySQL accessors for the Poco 1.4.0 Data classes.

---
+++ databases/poco-data-mysql/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/02/06 06:02:02 schnoebe Exp $
+
+BUILDLINK_TREE+=	poco-data-mysql
+
+.if !defined(POCO_DATA_MYSQL_BUILDLINK3_MK)
+POCO_DATA_MYSQL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.poco-data-mysql+=	poco-data-mysql>=1.3.5
+BUILDLINK_ABI_DEPENDS.poco-data-mysql?=	poco-data-mysql>=1.3.5nb1
+BUILDLINK_PKGSRCDIR.poco-data-mysql?=	../../wip/poco-data-mysql
+
+.include "../../databases/mysql5-client/buildlink3.mk"
+.include "../../wip/poco-data/buildlink3.mk"
+.endif # POCO_DATA_MYSQL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-poco-data-mysql
