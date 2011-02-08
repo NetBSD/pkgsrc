eb008a17f77b9a43d8f8d79670c6564f8558c12d
adam <adam@pkgsrc.org>
2011-02-08 13:08:06 +0000

    Switch to options.mk and remove plotutils-nox11

--- graphics/plotutils/buildlink3.mk
+++ graphics/plotutils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2011/01/13 13:36:05 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2011/02/08 13:08:06 adam Exp $
 
 BUILDLINK_TREE+=	plotutils
 
@@ -9,11 +9,17 @@ BUILDLINK_API_DEPENDS.plotutils+=	plotutils>=2.4.1nb2
 BUILDLINK_ABI_DEPENDS.plotutils+=	plotutils>=2.6nb2
 BUILDLINK_PKGSRCDIR.plotutils?=	../../graphics/plotutils
 
+pkgbase := plotutils
+.include "../../mk/pkg-build-options.mk"
+
 .include "../../graphics/png/buildlink3.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.plotutils:Mx11)
 .include "../../x11/libXaw/buildlink3.mk"
 .include "../../x11/libXext/buildlink3.mk"
 .include "../../x11/libXp/buildlink3.mk"
 .include "../../x11/libXt/buildlink3.mk"
+.endif
 .endif # PLOTUTILS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-plotutils
