de43d197613c07c17d3cd5c801f37ee2847b14d2
adam <adam@pkgsrc.org>
2011-02-08 13:13:41 +0000

    Changes 4.4.2: * FIX color specification "lc N" * FIX clipping error in filled curves * FIX segfault if "refresh" command follows an interrupted replot
    Changes 4.4.1:
    * NEW Support the "%s" format specifier in strftime()
    * NEW Optional 6th data column in candlesticks style to specific box width
    * NEW If integer arithmetic i*j or i**j would overflow, return value as a real
    * NEW "set style circle radius <default-radius>"
    * FIX autoconfiguration of readline support in OSX
    * FIX font problems in win terminal
    * FIX Protect against runaway recursion by limiting depth of nested functions
    * FIX metapost terminal: Fix inheritance of line thickness by text strings
    * FIX cairo terminals could segfault on certain enhanced text strings
    * FIX pdfcairo output to stdout now works
    * FIX font ",size" in non-enhanced mode postscript terminals
    * FIX Do a better job of estimating the width of UTF-8 encoded strings
    * FIX baseline bug in canvas terminal rotation of enhanced text
    * FIX Allow "with labels" to work for binary input data
    * FIX update user GPVAL variables after plot in table mode
    * CHANGE depth-sorting of pm3d surfaces now applies jointly to all in the plot
    * CHANGE 3D impulses are now always drawn from z=0
    * CHANGE Enable HIDDEN_QUADTREE by default
    * CHANGE Enable "set fit errorvariables" by default
    * CHANGE Regardless of current timefmt, read time from binary files as a binary
    * CHANGE do not limit 3D rotation to 0<rotx<180

--- graphics/gnuplot/options.mk
+++ graphics/gnuplot/options.mk
@@ -1,16 +1,25 @@
-# $NetBSD: options.mk,v 1.1 2010/02/19 14:09:58 asau Exp $
+# $NetBSD: options.mk,v 1.2 2011/02/08 13:13:41 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnuplot
-PKG_SUPPORTED_OPTIONS=	gd pdf
-PKG_SUGGESTED_OPTIONS=	gd pdf
+PKG_SUPPORTED_OPTIONS=	gd pdf x11
+PKG_SUGGESTED_OPTIONS=	gd pdf x11
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mpdf)
-CONFIGURE_ARGS+=        --with-pdf
-.include "../../print/pdflib/buildlink3.mk"
-.endif
+PLIST_VARS+=	x11
 
 .if !empty(PKG_OPTIONS:Mgd)
 .include "../../graphics/gd/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mpdf)
+CONFIGURE_ARGS+=	--with-pdf
+.include "../../print/pdflib-lite/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mx11)
+PLIST.x11=	yes
+.include "../../x11/libXaw/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-x
 .endif
