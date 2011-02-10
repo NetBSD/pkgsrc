807c217558abc68b28b034adb131b76af67f68d4
wiz <wiz@pkgsrc.org>
2011-02-10 09:57:15 +0000

    Stop including tiff/bl3.mk from bl3.mk as well, and add a comment in the Makefile to re-enable it when we switch back to pkgsrc tiff.

--- geography/gdal-lib/buildlink3.mk
+++ geography/gdal-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2011/02/10 03:45:09 brook Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2011/02/10 09:57:15 wiz Exp $
 
 BUILDLINK_TREE+=	gdal-lib
 
@@ -26,7 +26,8 @@ pkgbase := gdal-lib
 .include "../../graphics/jasper/buildlink3.mk"
 .include "../../graphics/libungif/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
-.include "../../graphics/tiff/buildlink3.mk"
+# see Makefile
+#.include "../../graphics/tiff/buildlink3.mk"
 .include "../../textproc/xerces-c/buildlink3.mk"
 .include "../../www/curl/buildlink3.mk"
 .endif # GDAL_LIB_BUILDLINK3_MK
