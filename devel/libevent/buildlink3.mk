822834c9052d40801d9be7455c2585c724f476ea
tnn <tnn@pkgsrc.org>
2011-02-11 21:13:47 +0000

    Update to libevent-2.0.10. Supposedly API backwards-compatible with libevent 1.x. Will need a recursive revbump for shlib name changes, though.
    All regression tests pass except "thread_conditions_simple". Not sure
    why yet, but it's new API so shouldn't break anything existing in pkgsrc
    today.
    
    Changes in libevent 2.0 include cleaner interfaces, buffered I/O improvements,
    improved multithreading and openssl integration.

--- devel/libevent/buildlink3.mk
+++ devel/libevent/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2009/08/18 16:53:14 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2011/02/11 21:13:47 tnn Exp $
 
 BUILDLINK_TREE+=	libevent
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libevent
 LIBEVENT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libevent+=libevent>=0.6
-BUILDLINK_ABI_DEPENDS.libevent+=libevent>=1.4.3
+BUILDLINK_ABI_DEPENDS.libevent+=libevent>=2.0.10
 BUILDLINK_PKGSRCDIR.libevent?=	../../devel/libevent
 .endif # LIBEVENT_BUILDLINK3_MK
 
