2b1c3dfe4b44475aed36d1094064c2213df38560
tnn <tnn@pkgsrc.org>
2011-02-11 21:22:02 +0000

    revbump(1) for devel/libevent update.

--- devel/libthrift/buildlink3.mk
+++ devel/libthrift/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/05/01 02:49:10 tonnerre Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/02/11 21:22:02 tnn Exp $
 
 BUILDLINK_TREE+=	libthrift
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libthrift
 LIBTHRIFT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libthrift+=	libthrift>=0.2.0
-BUILDLINK_ABI_DEPENDS.libthrift+=	libthrift>=0.2.0
+BUILDLINK_ABI_DEPENDS.libthrift+=	libthrift>=0.5.0nb1
 BUILDLINK_PKGSRCDIR.libthrift?=		../../devel/libthrift
 
 .include "../../devel/zlib/buildlink3.mk"
