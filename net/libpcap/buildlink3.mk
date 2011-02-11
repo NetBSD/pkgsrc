a1fbb15ed91f5f914f745688661ac3fe9361e935
adam <adam@pkgsrc.org>
2011-02-11 12:11:26 +0000

    Changes 1.1.1: * Fix build on RHEL5. * Fix shared library build on AIX.
    Changes 1.1.0:
    * Add SocketCAN capture support
    * Add Myricom SNF API support
    * Update Endace DAG and ERF support
    * Add support for shared libraries on Solaris, HP-UX, and AIX
    * Build, install, and un-install shared libraries by default;
      don't build/install shared libraries on platforms we don't support
    * Fix building from a directory other than the source directory
    * Fix compiler warnings and builds on some platforms
    * Update config.guess and config.sub
    * Support monitor mode on mac80211 devices on Linux
    * Fix USB memory-mapped capturing on Linux; it requires a new DLT_
      value
    * On Linux, scan /sys/class/net for devices if we have it; scan
      it, or /proc/net/dev if we don't have /sys/class/net, even if
      we have getifaddrs(), as it'll find interfaces with no
      addresses
    * Add limited support for reading pcap-ng files
    * Fix BPF driver-loading error handling on AIX
    * Support getting the full-length interface description on FreeBSD
    * In the lexical analyzer, free up any addrinfo structure we got back
      from getaddrinfo().
    * Add support for BPF and libdlpi in OpenSolaris (and SXCE)
    * Hyphenate "link-layer" everywhere
    * Add /sys/kernel/debug/usb/usbmon to the list of usbmon locations
    * In pcap_read_linux_mmap(), if there are no frames available, call
      poll() even if we're in non-blocking mode, so we pick up
      errors, and check for the errors in question.
    * Note that poll() works on BPF devices is Snow Leopard
    * If an ENXIO or ENETDOWN is received, it may mean the device has
      gone away.  Deal with it.
    * For BPF, raise the default capture buffer size to from 32k to 512k
    * Support ps_ifdrop on Linux
    * Added a bunch of #ifdef directives to make wpcap.dll (WinPcap) compile
      under cygwin.
    * Changes to Linux mmapped captures.
    * Fix bug where create_ring would fail for particular snaplen and
      buffer size combinations
    * Update pcap-config so that it handles libpcap requiring
      additional libraries
    * Add workaround for threadsafeness on Windows
    * Add missing mapping for DLT_ENC <-> LINKTYPE_ENC
    * DLT: Add DLT_CAN_SOCKETCAN
    * DLT: Add Solaris ipnet
    * Don't check for DLT_IPNET if it's not defined
    * Add link-layer types for Fibre Channel FC-2
    * Add link-layer types for Wireless HART
    * Add link-layer types for AOS
    * Add link-layer types for DECT
    * Autoconf fixes (AIX, HP-UX, OSF/1, Tru64 cleanups)
    * Install headers unconditionally, and include vlan.h/bluetooth.h if
      enabled
    * Autoconf fixes+cleanup

--- net/libpcap/buildlink3.mk
+++ net/libpcap/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2009/03/20 19:25:10 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2011/02/11 12:11:26 adam Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -13,10 +13,10 @@ BUILDLINK_PKGSRCDIR.libpcap?=	../../net/libpcap
 
 # on Solaris, we always need libnsl and libsocket because libpcap.a
 # references symbols in them.
-.  if ${OPSYS} == "SunOS"
+.if ${OPSYS} == "SunOS"
 BUILDLINK_LDADD.libpcap+=	-lnsl -lsocket
 BUILDLINK_LDFLAGS.libpcap?=	${BUILDLINK_LDADD.libpcap}
-.  endif
+.endif
 .endif # LIBPCAP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libpcap
