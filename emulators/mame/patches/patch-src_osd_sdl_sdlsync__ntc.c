ea6d47853624204749a55ab19948c2ace114eec2
abs <abs@pkgsrc.org>
2011-02-11 12:49:33 +0000

    All source files in mame.zip have CR-LF but are marked as binary, so unzip -a does not help. Hence all patches must have CR-LF

--- emulators/mame/patches/patch-src_osd_sdl_sdlsync__ntc.c
+++ emulators/mame/patches/patch-src_osd_sdl_sdlsync__ntc.c
@@ -1,6 +1,4 @@
-$NetBSD: patch-src_osd_sdl_sdlsync__ntc.c,v 1.1.1.1 2011/02/03 22:22:36 wiz Exp $
-
-Sent upstream and accepted.
+$NetBSD: patch-src_osd_sdl_sdlsync__ntc.c,v 1.2 2011/02/11 12:49:33 abs Exp $
 
 --- src/osd/sdl/sdlsync_ntc.c.orig	2010-01-15 20:12:06.000000000 +0000
 +++ src/osd/sdl/sdlsync_ntc.c
