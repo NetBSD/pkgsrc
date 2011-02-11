b7f5ac3e0a22a786ff8edcbb40876919bf149b7a
abs <abs>
2011-02-11 13:16:40 +0000

    add back comments accidently lost in previous commit

--- emulators/mame/patches/patch-src_osd_sdl_sdlsync__ntc.c
+++ emulators/mame/patches/patch-src_osd_sdl_sdlsync__ntc.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_osd_sdl_sdlsync__ntc.c,v 1.2 2011/02/11 12:49:33 abs Exp $
+$NetBSD: patch-src_osd_sdl_sdlsync__ntc.c,v 1.3 2011/02/11 13:16:40 abs Exp $
+
+Sent upstream and accepted.
 
 --- src/osd/sdl/sdlsync_ntc.c.orig	2010-01-15 20:12:06.000000000 +0000
 +++ src/osd/sdl/sdlsync_ntc.c
