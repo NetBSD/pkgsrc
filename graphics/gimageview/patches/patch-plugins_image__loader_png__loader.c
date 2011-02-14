0d0928024bbc9198230a85294a167e85471fa19c
wiz <wiz@pkgsrc.org>
2011-02-14 18:12:24 +0000

    Add upstream bug report URL.

--- graphics/gimageview/patches/patch-plugins_image__loader_png__loader.c
+++ graphics/gimageview/patches/patch-plugins_image__loader_png__loader.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-plugins_image__loader_png__loader.c,v 1.1 2011/02/01 08:50:07 wiz Exp $
+$NetBSD: patch-plugins_image__loader_png__loader.c,v 1.2 2011/02/14 18:12:24 wiz Exp $
 
 Fix build with png-1.5.
+https://sourceforge.net/tracker/?func=detail&aid=3181113&group_id=39083&atid=424297
 
 --- plugins/image_loader/png_loader.c.orig	2004-05-23 14:08:16.000000000 +0000
 +++ plugins/image_loader/png_loader.c
