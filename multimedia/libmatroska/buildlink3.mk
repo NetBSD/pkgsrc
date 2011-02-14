0f340ae755e017dfdaa2d5ecfd4be89759d4eac7
drochner <drochner@pkgsrc.org>
2011-02-14 19:00:45 +0000

    update to 1.1.0 changes: -API extensions and cleanup -bugfixes

--- multimedia/libmatroska/buildlink3.mk
+++ multimedia/libmatroska/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2010/07/08 18:46:46 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2011/02/14 19:00:45 drochner Exp $
 #
 # This Makefile fragment is included by packages that use libmatroska.
 #
@@ -9,7 +9,7 @@ BUILDLINK_TREE+=	libmatroska
 LIBMATROSKA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libmatroska+=	libmatroska>=0.8.0
-BUILDLINK_ABI_DEPENDS.libmatroska?=	libmatroska>=1.0.0
+BUILDLINK_ABI_DEPENDS.libmatroska?=	libmatroska>=1.1.0
 BUILDLINK_PKGSRCDIR.libmatroska?=	../../multimedia/libmatroska
 
 .include "../../devel/libebml/buildlink3.mk"
