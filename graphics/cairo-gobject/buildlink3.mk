434052504e468445255a3126f136ceb5ebb5cf0e
drochner <drochner@pkgsrc.org>
2011-02-15 11:30:47 +0000

    make cairo-gobject usable for clients

---
+++ graphics/cairo-gobject/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2011/02/15 11:30:47 drochner Exp $
+
+BUILDLINK_TREE+=	cairo-gobject
+
+.if !defined(CAIRO_GOBJECT_BUILDLINK3_MK)
+CAIRO_GOBJECT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.cairo-gobject+=	cairo-gobject>=1.10.2
+BUILDLINK_PKGSRCDIR.cairo-gobject?=	../../graphics/cairo-gobject
+
+.include "../../graphics/cairo/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../x11/pixman/buildlink3.mk"
+.endif	# CAIRO_GOBJECT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-cairo-gobject
