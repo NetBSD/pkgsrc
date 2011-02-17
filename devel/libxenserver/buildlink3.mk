58d795411fbd5e6aee7ba89c23f39c49d33547fe
sborrill <sborrill@pkgsrc.org>
2011-02-17 09:54:02 +0000

    SDK for Citrix XenServer 5.6 FP1
    Citrix XenServer includes a XML-RPC based API, providing programmatic access
    to the extensive set of XenServer management features and tools. The
    XenServer API can be called from a remote system as well as local to the
    XenServer host. Remote calls are generally made securely over HTTPS, using
    port 443.

---
+++ devel/libxenserver/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/02/17 09:54:02 sborrill Exp $
+
+BUILDLINK_TREE+=	libxenserver
+
+.if !defined(LIBXENSERVER_BUILDLINK3_MK)
+LIBXENSERVER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libxenserver+=	libxenserver>=5.6.100
+BUILDLINK_ABI_DEPENDS.libxenserver?=	libxenserver>=5.6.100
+BUILDLINK_PKGSRCDIR.libxenserver?=	../../devel/libxenserver
+
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
+.endif	# LIBXENSERVER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libxenserver
