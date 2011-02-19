cfc4ba74830fa9bf886d22f3b9e0535ac496dfba
markd <markd@pkgsrc.org>
2011-02-19 08:54:08 +0000

    Split the gtk parts of imlib into its own package.

---
+++ graphics/imlib-gtk/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/02/19 08:54:08 markd Exp $
+
+BUILDLINK_TREE+=	imlib-gtk
+
+.if !defined(IMLIB_GTK_BUILDLINK3_MK)
+IMLIB_GTK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.imlib-gtk+=	imlib-gtk>=1.9.15
+BUILDLINK_PKGSRCDIR.imlib-gtk?=	../../graphics/imlib-gtk
+
+.include "../../graphics/imlib/buildlink3.mk"
+.include "../../x11/gtk/buildlink3.mk"
+.endif	# IMLIB_GTK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-imlib-gtk
