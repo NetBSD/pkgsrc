2f52c6d55699f4ee398976dfce908ddade6849a9
wiz <wiz@pkgsrc.org>
2011-02-19 15:48:35 +0000

    Added gtkglarea2-2.0.1.

---
+++ x11/gtkglarea2/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.12 2011/02/19 15:48:35 wiz Exp $
+
+BUILDLINK_TREE+=	gtkglarea2
+
+.if !defined(GTKGLAREA2_BUILDLINK3_MK)
+GTKGLAREA2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gtkglarea2+=	gtkglarea2>=2.0.1
+BUILDLINK_PKGSRCDIR.gtkglarea2?=	../../x11/gtkglarea2
+
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# GTKGLAREA2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gtkglarea2
