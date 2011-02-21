7e5bf1d144b517e060cb9a469970211a76e2e2d9
wiz <wiz@pkgsrc.org>
2011-02-21 15:51:44 +0000

    Bump depends and PKGREVISION for libnice shlib major change.

--- chat/libpurple/buildlink3.mk
+++ chat/libpurple/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2011/02/02 13:18:48 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2011/02/21 15:51:44 wiz Exp $
 
 BUILDLINK_TREE+=	libpurple
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libpurple
 LIBPURPLE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libpurple+=	libpurple>=2.7.9
-BUILDLINK_ABI_DEPENDS.libpurple+=	libpurple>=2.7.9
+BUILDLINK_ABI_DEPENDS.libpurple+=	libpurple>=2.7.10nb1
 BUILDLINK_PKGSRCDIR.libpurple?=	../../chat/libpurple
 
 pkgbase := libpurple
