2273e51a5faf19fa3cd69675dc04b9f677b21f2d
wiz <wiz@pkgsrc.org>
2011-02-21 15:51:13 +0000

    Update to 0.1.0.
    libnice 0.1.0 (2011-01-20)
    ==========================
    
    Added nice_candidate_copy to the public API
    Make stun_timer timeouts configurable (Break API and ABI)
    Add compatibility support for MSOC 2007 and MSOC 2007 R2
    Add MS-TURN support for MSOC
    Added and completed TURN RFC 5766 support
    Add a nice_agent_set_port_range API to force a component to use a specific port
    Fix various bugs and memory leaks
    Improve documentation

--- net/libnice/buildlink3.mk
+++ net/libnice/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2010/06/13 22:45:11 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/02/21 15:51:13 wiz Exp $
 
 BUILDLINK_TREE+=	libnice
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libnice
 LIBNICE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libnice+=	libnice>=0.0.9
-BUILDLINK_ABI_DEPENDS.libnice?=	libnice>=0.0.11nb1
+BUILDLINK_ABI_DEPENDS.libnice?=	libnice>=0.1.0
 BUILDLINK_PKGSRCDIR.libnice?=	../../net/libnice
 
 .include "../../devel/glib2/buildlink3.mk"
