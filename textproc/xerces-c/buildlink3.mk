1ce72ee225caca737ae67451ccea6f1c869fbe1f
adam <adam@pkgsrc.org>
2011-02-23 08:38:34 +0000

    buildlink.mk fix

--- textproc/xerces-c/buildlink3.mk
+++ textproc/xerces-c/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2011/02/23 08:31:13 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2011/02/23 08:38:34 adam Exp $
 
 BUILDLINK_TREE+=	xerces-c
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	xerces-c
 XERCES_C_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xerces-c+=	xerces-c>=3.1.1
-BUILDLINK_PKGSRCDIR.xerces-c?=		../../textproc/xerces-c3
+BUILDLINK_PKGSRCDIR.xerces-c?=		../../textproc/xerces-c
 
 .include "../../converters/libiconv/buildlink3.mk"
 .endif	# XERCES_C_BUILDLINK3_MK
