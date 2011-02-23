11614298d6de1d00656dcc3690a09bc596c706cd
adam <adam@pkgsrc.org>
2011-02-23 08:31:13 +0000

    Changes 3.1.1: This version is interface-incompatible with Xerces-C++ 2.8.0 and contains a large number of improvements, bug fixes, and clean-ups, including: * Autotools-based build system for the UNIX/Linux/Mac OS X platforms * Project files for VC++ 9 * Support for the ICU transcoder in VC++ 7.1, 8, and 9 project files * libcurl-based net accessor * Support for XInclude in DOM * Support for both XPath 1 and XPath 2 models in the DOM XPath interface * Support for the XML Schema subset of XPath 1 in DOM * Conformance to the final DOM Level 3 interface specification * Ability to provide custom DOM memory manager as well as tune the global   DOM heap parameters * All public and widely used interfaces as well as a large portion of the   implementation were converted to be 64-bit safe * Various XML Schema fixes including the fix for the large maxOccurs and   minOccurs bug as well as for the changed ##other interpretation * Reviewed and cleaned up diagnostics messages * Optimizations for SAX/SAX2 and DOM parsing as well as XML Schema validation

--- textproc/xerces-c/buildlink3.mk
+++ textproc/xerces-c/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/05/10 11:24:26 hasso Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2011/02/23 08:31:13 adam Exp $
 
 BUILDLINK_TREE+=	xerces-c
 
 .if !defined(XERCES_C_BUILDLINK3_MK)
 XERCES_C_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xerces-c+=	xerces-c>=2.8.0
-BUILDLINK_ABI_DEPENDS.xerces-c?=	xerces-c>=2.8.0
-BUILDLINK_PKGSRCDIR.xerces-c?=	../../textproc/xerces-c
+BUILDLINK_API_DEPENDS.xerces-c+=	xerces-c>=3.1.1
+BUILDLINK_PKGSRCDIR.xerces-c?=		../../textproc/xerces-c3
 
 .include "../../converters/libiconv/buildlink3.mk"
-.endif # XERCES_C_BUILDLINK3_MK
+.endif	# XERCES_C_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xerces-c
