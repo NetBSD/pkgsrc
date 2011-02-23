db9e0a26dea067499f902795f85b969844dad2de
adam <adam@pkgsrc.org>
2011-02-23 09:11:09 +0000

    Changes 2.4.1: Bug * Reload interval staying too low on reload failures. * [HTTP-Redirect binding] [Message encoding] There should be no '%0A' in the   SAMLRequest parameter value

--- security/opensaml/buildlink3.mk
+++ security/opensaml/buildlink3.mk
@@ -1,5 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/03/15 19:40:44 pettai Exp $
-#
+# $NetBSD: buildlink3.mk,v 1.2 2011/02/23 09:11:09 adam Exp $
 
 BUILDLINK_TREE+=	opensaml
 
@@ -7,8 +6,8 @@ BUILDLINK_TREE+=	opensaml
 OPENSAML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.opensaml+=	opensaml>=2.3
-BUILDLINK_ABI_DEPENDS.opensaml?=	opensaml>=2.3
-BUILDLINK_PKGSRCDIR.opensaml?=	../../security/opensaml
+BUILDLINK_ABI_DEPENDS.opensaml+=	opensaml>=2.3
+BUILDLINK_PKGSRCDIR.opensaml?=		../../security/opensaml
 
 .include "../../security/openssl/buildlink3.mk"
 .endif	# OPENSAML_BUILDLINK3_MK
