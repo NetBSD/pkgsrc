6349e6d57074f270ede698f65a67b2259afad97f
adam <adam@pkgsrc.org>
2011-02-23 10:32:29 +0000

    Changes 2.5.16: * Fixed container creation so that it honors page size in XmlContainerConfig * Fix the base-uri of an attribute node when using WholedocContainer storage * Fixed an assertion failure during query preparation with a recursive user   defined function * Fixed an assertion failure when an as-yet unseen URI in used in a query * Fixed a problem where attribute indexes would not properly be updated if   there were no element indexes present. This might have a symptom of   DB_NOTFOUND errors or duplicate index entries for attributes * Changed the algorithm used to create node IDs during partial update to be   more efficient and create shorter node IDs in general * Fixed a problem where deleting the XmlResults object returned by   XmlValue.getAttributes() might cause an exception when the original   XmlResults for the XmlValue object was next accessed * XQuery Update queries will no longer crash when statistics are disabled * Fixed a bug in document level indexing that could result in index entries   being deleted inappropriately when a node was deleted * Fixed fn:doc() to raise an error in all cases if the document does not exist * Fixed a bug occurring when fn:subsequence() and "order by" were used in   certain configurations * Changed XmlResults.asEventWriter(), now only one active XmlEventWriter is   allowed for an XmlResults object

--- databases/bdb-xml/buildlink3.mk
+++ databases/bdb-xml/buildlink3.mk
@@ -1,17 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/03/20 19:24:04 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2011/02/23 10:32:29 adam Exp $
 
 BUILDLINK_TREE+=	bdb-xml
 
 .if !defined(BDB_XML_BUILDLINK3_MK)
 BDB_XML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.bdb-xml+=	bdb-xml>=1.2.1nb3
-BUILDLINK_ABI_DEPENDS.bdb-xml?=	bdb-xml>=1.2.1nb8
-BUILDLINK_PKGSRCDIR.bdb-xml?=	../../databases/bdb-xml
+BUILDLINK_API_DEPENDS.bdb-xml+=		bdb-xml>=2.5.16
+BUILDLINK_ABI_DEPENDS.bdb-xml+=		bdb-xml>=2.5.16
+BUILDLINK_PKGSRCDIR.bdb-xml?=		../../databases/bdb-xml
 
-.include "../../databases/db46/buildlink3.mk"
-.include "../../textproc/libpathan/buildlink3.mk"
+BDB_ACCEPTED=	db4 db5
+.include "../../mk/bdb.buildlink3.mk"
+
+.include "../../devel/zlib/buildlink3.mk"
 .include "../../textproc/xerces-c/buildlink3.mk"
+.include "../../textproc/xqilla/buildlink3.mk"
 .endif # BDB_XML_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-bdb-xml
