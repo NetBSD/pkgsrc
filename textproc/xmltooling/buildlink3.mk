bd5a5a91c785220dd8b07d999701f088192467fa
adam <adam@pkgsrc.org>
2011-02-23 09:09:31 +0000

    Changes 1.4.1: Bug * User-Agent string for AttributeQuery is throwing junk into request * Chaining TrustEngine doesn't clear matching criteria between engines, so   TrustEngine ordering affects the results. New Feature * Implement support for fetching CRLs based on the CRL distribution point   extension

--- textproc/xmltooling/buildlink3.mk
+++ textproc/xmltooling/buildlink3.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/03/15 18:45:36 pettai Exp $
-#
+# $NetBSD: buildlink3.mk,v 1.2 2011/02/23 09:09:31 adam Exp $
 
 BUILDLINK_TREE+=	xmltooling
 
-.if !defined(XMLTOOLING_C_BUILDLINK3_MK)
-XMLTOOLING_C_BUILDLINK3_MK:=
+.if !defined(XMLTOOLING_BUILDLINK3_MK)
+XMLTOOLING_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xmltooling+=	xmltooling>=1.3.3
 BUILDLINK_PKGSRCDIR.xmltooling?=	../../textproc/xmltooling
 
-.endif	# XMLTOOLING_C_BUILDLINK3_MK
+.endif	# XMLTOOLING_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xmltooling
