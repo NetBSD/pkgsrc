a352243f5b8b7b3fa532b788437def18e2fcfbca
adam <adam@pkgsrc.org>
2011-02-24 11:05:34 +0000

    Changes 1.46.0: New Libraries * Icl: Interval Container Library, interval sets and maps and aggregation of   associated values, from Joachim Faulhaber. Updated Libraries * Array:   - Added support for cbegin/cend   - Fixed a problem with the Sun compiler * Asio:   - Fixed a problem on older Linux kernels (where epoll is used without timerfd     support) that prevents timely delivery of deadline_timer handlers, after     the program has been running for some time * Bind:   - make_adaptable now documented * Concept Check:   - fixed warnings with self-assignment * Filesystem:   - Version 3 of the library is now the default.   - IBM vacpp: Workaround for compiler bug affecting iterator_facade   - Verify, clarify, document that <boost/config/user.hpp> can be used to     specify BOOST_FILESYSTEM_VERSIO   - Replaced C-style assert with BOOST_ASSERT.   - Undeprecated unique_path(). Instead, add a note mentioning the workaround    for lack of thread safety and possible change to cwd. unique_path() is just    too convenient to deprecate!   - Cleared several GCC warnings.   - Changed V2 code to use BOOST_THROW_EXCEPTION.   - Windows: Fix status() to report non-symlink reparse point correctly.   - Add symlink_option to recursive_directory_iterator, allowing control over     recursion into directory symlinks. Note that the default is changed to not     recurse into directory symlinks.   - Reference documentation cleanup, including fixing missing and broken links,     and adding missing functions.   - Miscellaneous implementation code cleanup. * Fusion:   - vector copy constructor now copies sequence members in the same order on different platforms * Graph:   - Fixed Graphviz output to work on Visual C++ 7.1.   - Replaced assert with BOOST_ASSERT.   - Changed to Boost.Filesystem v3. More...

--- devel/boost-build/buildlink3.mk
+++ devel/boost-build/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.17 2010/11/26 20:46:59 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2011/02/24 11:05:34 adam Exp $
 
 BUILDLINK_TREE+=	boost-build
 
 .if !defined(BOOST_BUILD_BUILDLINK3_MK)
 BOOST_BUILD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.45.*
+BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.46.*
 BUILDLINK_DEPMETHOD.boost-build?=	build
 BUILDLINK_PKGSRCDIR.boost-build?=	../../devel/boost-build
 .endif # BOOST_BUILD_BUILDLINK3_MK
