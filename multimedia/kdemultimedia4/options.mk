c6da1064af864f4ea7510003252291dd8773ee93
markd <markd@pkgsrc.org>
2011-02-25 20:37:56 +0000

    Add alsa option.

--- multimedia/kdemultimedia4/options.mk
+++ multimedia/kdemultimedia4/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1.1.1 2009/10/02 20:25:35 markd Exp $
+# $NetBSD: options.mk,v 1.2 2011/02/25 20:39:24 markd Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kdemultimedia
-PKG_SUPPORTED_OPTIONS=	pulseaudio
+PKG_SUPPORTED_OPTIONS=	pulseaudio alsa
 PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
@@ -10,4 +10,10 @@ PKG_SUGGESTED_OPTIONS=
 .include "../../audio/pulseaudio/buildlink3.mk"
 .else
 CMAKE_ARGS+=	-DWITH_PulseAudio:BOOL=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Malsa)
+.include "../../audio/alsa-lib/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DWITH_ALSA:BOOL=OFF
 .endif
