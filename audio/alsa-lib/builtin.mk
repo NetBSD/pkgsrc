19d8b0252998e523d26e5d0fbe2cc46a9b4f2d0f
markd <markd@pkgsrc.org>
2011-02-25 20:17:41 +0000

    Add a builtin.mk

---
+++ audio/alsa-lib/builtin.mk
@@ -0,0 +1,23 @@
+# $NetBSD: builtin.mk,v 1.1 2011/02/25 20:17:41 markd Exp $
+
+BUILTIN_PKG:=	alsa-lib
+
+PKGCONFIG_FILE.alsa-lib=	/usr/lib/pkgconfig/alsa.pc
+PKGCONFIG_BASE.alsa-lib=	/usr
+
+.include "../../mk/buildlink3/pkgconfig-builtin.mk"
+
+###
+### The section below only applies if we are not including this file
+### solely to determine whether a built-in implementation exists.
+###
+
+CHECK_BUILTIN.alsa-lib?=      no
+.if !empty(CHECK_BUILTIN.alsa-lib:M[nN][oO])
+
+.  if !empty(USE_BUILTIN.alsa-lib:M[yY][eE][sS])
+BUILDLINK_PREFIX.alsa-lib=    /usr
+BUILDLINK_FILES.alsa-lib+=    lib/pkgconfig/alsa.pc
+.  endif
+
+.endif  # CHECK_BUILTIN.alsa-lib
