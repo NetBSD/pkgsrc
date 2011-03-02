199f7faddb88be4c0ad104873a6cda3d9e29ad52
drochner <drochner@pkgsrc.org>
2011-03-02 10:14:31 +0000

    -disable the "pre-configure" step which turned out to be unnecessary  and could cause problems when building as non-root, from John Marino -put the build of demo programs (which was optionally, not switched  on per default) into its own pkg, to avoid builds in the "install"  phase and conditional PLIST entries -misc cleanup, fix a DESTDIR glitch

--- www/aws/options.mk
+++ www/aws/options.mk
@@ -1,25 +1,25 @@
-# $NetBSD: options.mk,v 1.1.1.1 2011/02/11 16:48:52 drochner Exp $
+# $NetBSD: options.mk,v 1.2 2011/03/02 10:14:31 drochner Exp $
 
 
 # xmlada is built-in (not optional) due to gprbuild dependency
 # zlib is built-in because it's mandatory.  It even comes with AWS.
 # ASIS is not supported right now (custom build, requires part of GNAT)
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.aws
-PKG_SUPPORTED_OPTIONS=	ssl demos ldap ipv6 debug disable-shared-rt relocatable
-PKG_SUGGESTED_OPTIONS=	ssl
+# The "ipv6" option effectively disabled IPv4 support, so it
+# should not be switched on per default even on systems which
+# support IPv6. For that reason, the option name is not "inet6".
 
+PKG_OPTIONS_VAR=	PKG_OPTIONS.aws
+PKG_SUPPORTED_OPTIONS=	ssl ldap ipv6 debug disable-shared-rt relocatable
+PKG_SUGGESTED_OPTIONS=	ssl
 
 .include "../../mk/bsd.prefs.mk"
 .include "../../mk/bsd.options.mk"
-
 
 CONFIGURE_ARGS+=	GCC=gnatgcc
 CONFIGURE_ARGS+=	PROCESSORS=1
 CONFIGURE_ARGS+=	PYTHON=python2.6
 DOTBUILD=		release
-DEMO_DIRS=
-
 
 # The system libgcc is from version 4.1 which doesn't include the symbol
 # _Unwind_GetIPInfo.  If AWS uses the system zlib or if it's configured to use
@@ -35,7 +35,6 @@ DEMO_DIRS=
 CONFIGURE_ARGS+=	ZLIB=false
 .endif
 
-
 ###################
 ##  SSL Support  ##
 ###################
@@ -49,70 +48,26 @@ CONFIGURE_ARGS+= SOCKET=openssl
 .endif
 .endif
 
-
-
 ####################
 ##  LDAP Support  ##
 ####################
 
 .if !empty(PKG_OPTIONS:Mldap)
 CONFIGURE_ARGS+= LDAP=true
-DEPENDS+= openldap>=2.4:/../../databases/openldap
+DEPENDS+= openldap>=2.4:../../databases/openldap
 .endif
-
-
-
-#####################
-##  DEMOS Support  ##
-#####################
-
-.if !empty(PKG_OPTIONS:Mdemos)
-BUILD_DEMOS=	YES
-
-DEMO_DIRS+=	agent
-DEMO_DIRS+=	auth
-DEMO_DIRS+=	com
-DEMO_DIRS+=	dispatch
-DEMO_DIRS+=	hello_world
-DEMO_DIRS+=	hello_wsdl
-DEMO_DIRS+=	hotplug
-DEMO_DIRS+=	interoplab
-DEMO_DIRS+=	jabber_demo
-DEMO_DIRS+=	multiple_sessions
-DEMO_DIRS+=	res_demo
-DEMO_DIRS+=	runme
-DEMO_DIRS+=	soap_demo
-DEMO_DIRS+=	soap_disp
-DEMO_DIRS+=	soap_vs
-DEMO_DIRS+=	split
-DEMO_DIRS+=	test_mail
-DEMO_DIRS+=	text_input
-DEMO_DIRS+=	vh_demo
-DEMO_DIRS+=	web_block
-DEMO_DIRS+=	web_block_ajax
-DEMO_DIRS+=	web_block_ajax_templates
-DEMO_DIRS+=	web_elements
-DEMO_DIRS+=	web_mail
-DEMO_DIRS+=	wps
-DEMO_DIRS+=	ws
-DEMO_DIRS+=	zdemo
-
-.if !empty(PKG_OPTIONS:Mldap)
-DEMO_DIRS+=	test_ldap
-.endif
-.endif
-
-
 
 ####################
 ##  IPv6 Support  ##
 ####################
 
+PLIST_VARS+= ipv6 noipv6
 .if !empty(PKG_OPTIONS:Mipv6)
 CONFIGURE_ARGS+= IPv6=true
+PLIST.ipv6= yes
+.else
+PLIST.noipv6= yes
 .endif
-
-
 
 #####################
 ##  Debug Support  ##
@@ -123,8 +78,6 @@ CONFIGURE_ARGS+= DEBUG=true
 DOTBUILD=debug
 .endif
 
-
-
 ##############################
 ##  Shared Runtime Library  ##
 ##############################
@@ -132,8 +85,6 @@ DOTBUILD=debug
 .if !empty(PKG_OPTIONS:Mdisable-shared-rt)
 CONFIGURE_ARGS+= ENABLE_SHARED=false
 .endif
-
-
 
 ############################
 ##  Default Library Type  ##
