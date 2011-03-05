fe78f6846671a7ac7b857143fa0b26810a95f4f3
brook <brook@pkgsrc.org>
2011-03-05 17:54:35 +0000

    Initial import of the uuid v1.0.2 package from pgfoundry.org.
    The ``uuid'' PostgreSQL package implements creation of Universally
    Unique Identifiers (UUIDs), also referred to as Globally Unique
    Identifiers (GUIDs).  Although PostgreSQL supports storing UUIDs as a
    native datatype, it does not provide any mechanism for creating them.
    This package wraps openssl-supplied md5 and sha1 functions to create
    UUIDs within PostgreSQL.

---
+++ databases/postgresql-uuid/patches/patch-uuid.c
@@ -0,0 +1,32 @@
+$NetBSD: patch-uuid.c,v 1.1.1.1 2011/03/05 17:54:35 brook Exp $
+
+--- uuid.c.orig	2010-03-14 03:04:29.000000000 -0600
++++ uuid.c	2011-02-12 10:51:10.000000000 -0700
+@@ -40,7 +40,7 @@
+ #undef uuid_hash
+ 
+ #include <md5.h>
+-#include <sha.h>
++#include <sha1.h>
+ 
+ PG_MODULE_MAGIC;
+ 
+@@ -158,13 +158,13 @@
+ 
+ 	case 5:  /* namespace-based SHA1 uuids */
+ 	{
+-	    SHA_CTX ctx;
++	    SHA1_CTX ctx;
+ 	    unsigned char buf[20];
+ 
+-	    SHA1_Init(&ctx);
+-	    SHA1_Update(&ctx, ns, 16);
+-	    SHA1_Update(&ctx, (unsigned char *)ptr, len);
+-	    SHA1_Final(buf, &ctx);
++	    SHA1Init(&ctx);
++	    SHA1Update(&ctx, ns, 16);
++	    SHA1Update(&ctx, (unsigned char *)ptr, len);
++	    SHA1Final(buf, &ctx);
+ 
+ 	    sprintf(strbuf,
+ 		    "%02x%02x%02x%02x-"
