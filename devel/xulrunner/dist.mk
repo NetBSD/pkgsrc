dad1ea8048ddc53f3c34e81756bbfaa4efd46e40
schnoebe <schnoebe@pkgsrc.org>
2011-03-06 16:39:37 +0000

    Pullup #3381, requested by tnn
    firefox-3.6.15 update and prerequisite commits.
    
    pkgsrc/www/firefox/Makefile		1.82, 1.83
    pkgsrc/devel/xulrunner/Makefile		1.46, 1.47, 1.48
    pkgsrc/devel/xulrunner/dist.mk		1.18
    pkgsrc/devel/xulrunner/distinfo		1.40
    pkgsrc/devel/xulrunner/patches/patch-mm	1.4
    
       Module Name:	pkgsrc
       Committed By:	wiz
       Date:		Thu Jan 13 13:40:12 UTC 2011
    
       Modified Files:
    	   pkgsrc/www/firefox: Makefile
    
       Log Message:
       png shlib name changed for png>=1.5.0, so bump PKGREVISIONs.
    
       ---
       Module Name:	pkgsrc
       Committed By:	wiz
       Date:		Thu Jan 13 13:53:34 UTC 2011
    
       Modified Files:
    	   pkgsrc/devel/xulrunner: Makefile
    
       Log Message:
       png shlib name changed for png>=1.5.0, so bump PKGREVISIONs.
    
       ---
       Module Name:	pkgsrc
       Committed By:	markd
       Date:		Sun Jan 23 11:18:27 UTC 2011
    
       Modified Files:
    	   pkgsrc/devel/xulrunner: Makefile
    
       Log Message:
       set FREEBL_NO_DEPEND=0 in MAKE_ENV otherwise Linux2.6 defaults to
       FREEBL_NO_DEPEND=1 and files not in PLIST are installed.
       OKed by tnn.
    
       ---
       Module Name:	pkgsrc
       Committed By:	tnn
       Date:		Sat Mar  5 11:57:17 UTC 2011
    
       Modified Files:
    	   pkgsrc/devel/xulrunner: Makefile dist.mk distinfo
    	   pkgsrc/devel/xulrunner/patches: patch-mm
    	   pkgsrc/www/firefox: Makefile
    
       Log Message:
       Stability & security update of firefox to 3.6.15.
    
       MFSA 2011-10 CSRF risk with plugins and 307 redirects
       MFSA 2011-09 Crash caused by corrupted JPEG image
       MFSA 2011-08 ParanoidFragmentSink allows javascript: URLs in chrome documents
       MFSA 2011-07 Memory corruption during text run construction (Windows)
       MFSA 2011-06 Use-after-free error using Web Workers
       MFSA 2011-05 Buffer overflow in JavaScript atom map
       MFSA 2011-04 Buffer overflow in JavaScript upvarMap
       MFSA 2011-03 Use-after-free error in JSON.stringify
       MFSA 2011-02 Recursive eval call causes confirm dialogs to evaluate to true
       MFSA 2011-01 Miscellaneous memory safety hazards (rv:1.9.2.14/ 1.9.1.17)

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.17 2010/12/11 14:46:29 tnn Exp $
+# $NetBSD: dist.mk,v 1.17.2.1 2011/03/06 16:39:38 schnoebe Exp $
 #
 # used by devel/nspr/Makefile
 # used by devel/nss/Makefile
@@ -8,7 +8,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	3.6${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	1.9.2
-MOZ_BRANCH_MINOR=	.13
+MOZ_BRANCH_MINOR=	.15
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
