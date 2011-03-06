7f2e1a6825470630a18a2282068ca011f35d51c2
dholland <dholland@pkgsrc.org>
2011-03-06 03:52:16 +0000

    This exposes libXp, so add libXp to the bl3. From Taylor Campbell in PR 44668. PKGREVISION++.

--- x11/lesstif/buildlink3.mk
+++ x11/lesstif/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2010/01/16 09:15:25 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2011/03/06 03:52:16 dholland Exp $
 
 BUILDLINK_TREE+=	lesstif
 
@@ -10,6 +10,7 @@ BUILDLINK_PKGSRCDIR.lesstif?=	../../x11/lesstif
 
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXp/buildlink3.mk"
 .include "../../x11/libXrender/buildlink3.mk"
 .include "../../x11/libXt/buildlink3.mk"
 .endif # LESSTIF_BUILDLINK3_MK
