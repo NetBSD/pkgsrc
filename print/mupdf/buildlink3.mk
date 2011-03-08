832f50c183f7aac65bea37b663b0dfeda9166c38
drochner <drochner@pkgsrc.org>
2011-03-08 17:09:25 +0000

    update to 0.8 no changelog available, at least some security fixes were done: -fix possible memory corruption due to malformed JPEG images  (was patched in pkgsrc) -fix (at least) 2 cases of integer overflow in array allocation (SA42320)

--- print/mupdf/buildlink3.mk
+++ print/mupdf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/02/18 10:07:32 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/03/08 17:09:25 drochner Exp $
 
 BUILDLINK_TREE+=	mupdf
 
@@ -6,9 +6,8 @@ BUILDLINK_TREE+=	mupdf
 MUPDF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mupdf+=	mupdf>=0.0.937
-BUILDLINK_PKGSRCDIR.mupdf?=	../../nih/mupdf
+BUILDLINK_PKGSRCDIR.mupdf?=	../../print/mupdf
 
-BUILDLINK_DEPMETHOD.mupdf?=	build
 .endif	# MUPDF_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mupdf
