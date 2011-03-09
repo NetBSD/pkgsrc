b1fd86038e4c76a75a1ff0260747cd7666e637a4
drochner <drochner>
2011-03-09 16:26:47 +0000

    libungif/buildlink3.mk -> mk/giflib.buildlink3.mk (uses giflib now per default) bump PKGREV

--- graphics/libgdiplus/buildlink3.mk
+++ graphics/libgdiplus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.31 2011/01/13 13:36:07 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2011/03/09 16:26:48 drochner Exp $
 
 BUILDLINK_TREE+=	libgdiplus
 
@@ -12,7 +12,7 @@ BUILDLINK_PKGSRCDIR.libgdiplus?=	../../graphics/libgdiplus
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../graphics/cairo/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
-.include "../../graphics/libungif/buildlink3.mk"
+.include "../../mk/giflib.buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
 .endif # LIBGDIPLUS_BUILDLINK3_MK
