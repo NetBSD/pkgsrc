561fb770575853d52b4f69d2600afa82b1f5dafa
drochner <drochner@pkgsrc.org>
2011-03-09 17:00:01 +0000

    disallow direct use of {giflib,libungif}/bl3.mk so that mk/giflib.bl3.mk can implement the global policy

--- graphics/giflib/buildlink3.mk
+++ graphics/giflib/buildlink3.mk
@@ -1,4 +1,8 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/01/18 11:57:53 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/03/09 17:00:01 drochner Exp $
+
+.if !defined(MK_GIFLIB_BUILDLINK3_MK)
+PKG_FAIL_REASON= "please use giflib.buildlink3.mk"
+.endif
 
 BUILDLINK_TREE+=	giflib
 
