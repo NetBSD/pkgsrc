0e65562df9b57d2f1aa16ac8d6d6569d6433d367
pettai <pettai@pkgsrc.org>
2011-03-10 23:38:42 +0000

    Version 4.1.3 - 2011-01-17
    fixed a coredump in torrent (introduced in 4.1.2).
    
    Version 4.1.2 - 2010-12-31
    
    new command ln [-s], supported by FTP (SITE [SYM]LINK), FISH, SFTP, and locally via file: schema.
    create remote symlinks in mirror -R if supported by server.
    detect and remove duplicate peers in torrent.
    
    Version 4.1.0 - 2010-11-22
    
    send path components to ftp server separately, unless TVFS is in FEAT reply.
    save pget status at the very beginning of pget execution.
    allow mirror pget continuation (mirror -c --use-pget).
    allow multiple torrent startup at once (e.g. torrent *.torrent).
    pre-allocate disk space in torrent, if posix_fallocate(3) is supported.
    new command prefix `local'. It makes the following command use local session instead of remote.
    added torrent multi-tracker support.
    fixed convert-mozilla-cookies cookie duplication.
    
    Version 4.0.10 - 2010-09-01
    
    use cached file set, don't parse file listing again.
    updated convert-netscape-cookies.
    fixed a problem with zeroed errno in http.
    fixed coredump on mirror --log=file when the file cannot be opened.
    translations updated (de, ru).

--- net/lftp/options.mk
+++ net/lftp/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.10 2009/07/02 18:54:18 tnn Exp $
+# $NetBSD: options.mk,v 1.11 2011/03/10 23:38:42 pettai Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.lftp
@@ -6,7 +6,7 @@ PKG_OPTIONS_VAR=	PKG_OPTIONS.lftp
 PKG_OPTIONS_OPTIONAL_GROUPS=	ssl
 PKG_OPTIONS_GROUP.ssl=	gnutls ssl
 
-PKG_SUPPORTED_OPTIONS=	perl
+PKG_SUPPORTED_OPTIONS=	perl dnssec
 PKG_SUGGESTED_OPTIONS=	ssl
 
 .include "../../mk/bsd.options.mk"
@@ -33,12 +33,20 @@ CONFIGURE_ARGS+=	--without-openssl
 USE_TOOLS+=		perl:run
 DEPENDS+=		p5-Digest-MD5-[0-9]*:../../security/p5-Digest-MD5
 DEPENDS+=		p5-String-CRC32-[0-9]*:../../textproc/p5-String-CRC32
-REPLACE_PERL+=		src/convert-netscape-cookies src/verify-file
+REPLACE_PERL+=		src/convert-mozilla-cookies src/verify-file
 PLIST_SRC+=		${PKGDIR}/PLIST.perl
 
 post-install-perl:
-	${INSTALL_SCRIPT} ${WRKSRC}/src/convert-netscape-cookies \
+	${INSTALL_SCRIPT} ${WRKSRC}/src/convert-mozilla-cookies \
 		${WRKSRC}/src/verify-file ${DESTDIR}${PREFIX}/share/lftp
 .else
 post-install-perl:
+.endif
+
+###
+### DNSSEC support
+###
+.if !empty(PKG_OPTIONS:Mdnssec)
+. include "../../security/dnssec-tools/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-dnssec-local-validation
 .endif
