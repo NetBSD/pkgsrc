3555e00afcfeebabc1f2447681a98f2a136ede5f
obache <obache@pkgsrc.org>
2011-03-15 06:04:03 +0000

    PgSQL support had been dropped from upstream. so options is changed that psycopg2 is the default driver for pgsql support.

--- www/ja-trac/options.mk
+++ www/ja-trac/options.mk
@@ -1,9 +1,10 @@
-# $NetBSD: options.mk,v 1.1 2008/08/01 08:46:55 obache Exp $
+# $NetBSD: options.mk,v 1.2 2011/03/15 06:04:03 obache Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.trac
 PKG_OPTIONS_REQUIRED_GROUPS=	db
-PKG_OPTIONS_GROUP.db=		sqlite pgsql psycopg2 mysql
+PKG_OPTIONS_GROUP.db=		sqlite pgsql mysql
 PKG_SUGGESTED_OPTIONS=		sqlite
+PKG_OPTIONS_LEGACY_OPTS=	psycopg2:pgsql
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,9 +12,6 @@ PKG_SUGGESTED_OPTIONS=		sqlite
 DEPENDS+=	${PYPKGPREFIX}-sqlite2>=2.3.2:../../databases/py-sqlite2
 .endif
 .if !empty(PKG_OPTIONS:Mpgsql)
-DEPENDS+=	${PYPKGPREFIX}-PgSQL>=2.4:../../databases/py-PgSQL
-.endif
-.if !empty(PKG_OPTIONS:Mpsycopg2)
 DEPENDS+=	${PYPKGPREFIX}-psycopg2>=2:../../databases/py-psycopg2
 .endif
 .if !empty(PKG_OPTIONS:Mmysql)
