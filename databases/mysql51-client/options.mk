d2a9e1fe2854f73ec4bd584fb20fa5e821157a92
adam <adam>
2011-03-17 13:26:04 +0000

    Fix building with Clang.

--- databases/mysql51-client/options.mk
+++ databases/mysql51-client/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2010/03/21 14:40:15 taca Exp $
+# $NetBSD: options.mk,v 1.4 2011/03/17 13:26:04 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mysql5
 
@@ -12,16 +12,16 @@ PKG_SUGGESTED_OPTIONS+=	embedded-server mysqlmanager ndb-cluster pstack ssl
 ###
 .if !empty(PKG_OPTIONS:Mssl)
 .	include "../../security/openssl/buildlink3.mk"
-. if ${OPSYS} == "SunOS"
+.  if ${OPSYS} == "SunOS"
 CHECK_BUILTIN.openssl:=yes
-.  include "../../security/openssl/builtin.mk"
+.    include "../../security/openssl/builtin.mk"
 CHECK_BUILTIN.openssl:=no
-.  if ${USE_BUILTIN.openssl} == "yes"
+.    if ${USE_BUILTIN.openssl} == "yes"
+CONFIGURE_ARGS+=	--with-ssl=yes
+.    endif
+.  else
 CONFIGURE_ARGS+=	--with-ssl=yes
 .  endif
-. else
-CONFIGURE_ARGS+=	--with-ssl=yes
-. endif
 .else
 CONFIGURE_ARGS+=	--without-ssl
 .endif
