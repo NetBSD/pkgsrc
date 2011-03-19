c9120d3a0762eeaf889e1ba372318ead26329368
obache <obache>
2011-03-19 13:31:52 +0000

    * additional missing clean up after removal of libxml option. * pass to configure more preferred variables.

--- security/ap-modsecurity2/options.mk
+++ security/ap-modsecurity2/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2008/10/12 12:50:17 adrianp Exp $
+# $NetBSD: options.mk,v 1.4 2011/03/19 13:31:52 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ap-modsecurity2
 
@@ -7,22 +7,11 @@ PKG_SUPPORTED_OPTIONS=	curl
 .include "../../mk/bsd.options.mk"
 
 ###
-### Bring in support for libxml2
-###
-.if !empty(PKG_OPTIONS:Mxml)
-.  include "../../textproc/libxml2/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-libxml
-MESSAGE_SRC+=		${WRKDIR}/.MESSAGE_SRC.xml
-.else
-CONFIGURE_ARGS+=	--without-libxml
-.endif
-
-###
 ### Bring in support for curl
 ###
 .if !empty(PKG_OPTIONS:Mcurl)
 .  include "../../www/curl/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-curl=${PREFIX}/bin/curl
+CONFIGURE_ARGS+=	--with-curl=${BUILDLINK_PREFIX.curl}
 .else
 CONFIGURE_ARGS+=	--without-curl
 .endif
