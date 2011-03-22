6d57eb358f190da0dad40659b4e05960da394a85
obache <obache@pkgsrc.org>
2011-03-22 10:47:11 +0000

    No need to buildlink with cairo, so reset bump ABI from libpng via cairo.

--- devel/gobject-introspection/buildlink3.mk
+++ devel/gobject-introspection/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/01/13 13:36:22 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/03/22 10:47:11 obache Exp $
 
 BUILDLINK_TREE+=	gobject-introspection
 
@@ -6,13 +6,11 @@ BUILDLINK_TREE+=	gobject-introspection
 GOBJECT_INTROSPECTION_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gobject-introspection+=	gobject-introspection>=0.6.14nb1
-BUILDLINK_ABI_DEPENDS.gobject-introspection?=	gobject-introspection>=0.10.0nb1
 BUILDLINK_PKGSRCDIR.gobject-introspection?=	../../devel/gobject-introspection
 
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/libffi/buildlink3.mk"
-.include "../../graphics/cairo/buildlink3.mk"
 .endif	# GOBJECT_INTROSPECTION_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gobject-introspection
