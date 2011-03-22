23aece424ec7c81e44ff5e0eab460ca9838525ce
jmmv <jmmv>
2011-03-22 15:46:08 +0000

    Manually define BUILDLINK_INCDIRS so that lib/libzip/include/zipconf.h can be found and included.  This lets textproc/ebook-tools build.

--- archivers/libzip/buildlink3.mk
+++ archivers/libzip/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2011/03/18 13:49:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2011/03/22 15:46:08 jmmv Exp $
 
 BUILDLINK_TREE+=	libzip
 
@@ -8,6 +8,8 @@ LIBZIP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libzip+=	libzip>=0.7.1
 BUILDLINK_ABI_DEPENDS.libzip+=	libzip>=0.10
 BUILDLINK_PKGSRCDIR.libzip?=	../../archivers/libzip
+
+BUILDLINK_INCDIRS.libzip=	include lib/libzip/include
 
 .include "../../devel/zlib/buildlink3.mk"
 .endif # LIBZIP_BUILDLINK3_MK
