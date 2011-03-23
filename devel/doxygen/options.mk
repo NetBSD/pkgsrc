db7ad1135011d25198f88ab35b5422f4f6bd4d9b
gdt <gdt>
2011-03-23 19:24:35 +0000

    Add latex option, currently defaulting to on. With latex option, depend on latex at both build and runtime. Without latex option, avoid both build- and run-time dependencies.
    (This commit should have no effect unless one adds
    PKG_OPTIONS.doxygen=-latex.)
    
    (Opinions were generally in favor of changing the default to not
    require latex, but it seems best not to do that right before a freeze,
    so I'm leaving it for another day.)

---
+++ devel/doxygen/options.mk
@@ -0,0 +1,39 @@
+# $NetBSD: options.mk,v 1.1 2011/03/23 19:24:35 gdt Exp $
+#
+PKG_OPTIONS_VAR=	PKG_OPTIONS.doxygen
+PKG_SUPPORTED_OPTIONS=	latex
+PKG_SUGGESTED_OPTIONS=	latex
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		latex
+
+.if !empty(PKG_OPTIONS:Mlatex)
+PLIST.latex=		yes
+
+INSTALL_TARGET+=	install_docs
+
+# XXX Clearly separate what is build time vs runtime, and move
+# build-time dependencies into USE_TOOLS framework.
+
+BUILD_DEPENDS+=	tex-epsf-[0-9]*:../../print/tex-epsf
+BUILD_DEPENDS+=	tex-tocloft-[0-9]*:../../print/tex-tocloft
+DEPENDS+=	dvipsk-[0-9]*:../../print/dvipsk
+DEPENDS+=	makeindexk-[0-9]*:../../textproc/makeindexk
+DEPENDS+=	tex-a4wide>=2010nb1:../../print/tex-a4wide
+DEPENDS+=	tex-fancyhdr-[0-9]*:../../print/tex-fancyhdr
+DEPENDS+=	tex-float-[0-9]*:../../print/tex-float
+DEPENDS+=	tex-graphics-[0-9]*:../../print/tex-graphics
+DEPENDS+=	tex-hyperref-[0-9]*:../../print/tex-hyperref
+DEPENDS+=	tex-latex-[0-9]*:../../print/tex-latex
+DEPENDS+=	tex-latex-bin-[0-9]*:../../print/tex-latex-bin
+DEPENDS+=	tex-listings-[0-9]*:../../print/tex-listings
+DEPENDS+=	tex-oberdiek-[0-9]*:../../print/tex-oberdiek
+DEPENDS+=	tex-psnfss>=9.2anb2:../../fonts/tex-psnfss
+DEPENDS+=	tex-pspicture-[0-9]*:../../print/tex-pspicture
+DEPENDS+=	tex-tools-[0-9]*:../../print/tex-tools
+DEPENDS+=	texlive-pdftools-[0-9]*:../../print/texlive-pdftools
+
+.include "../../mk/tex.buildlink3.mk"
+
+.endif
