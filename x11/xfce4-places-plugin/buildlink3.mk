a3fb78a1efe1b7ddbee632385e0fdceaea5d8371
wiz <wiz@pkgsrc.org>
2011-03-24 14:35:16 +0000

    Jens Rehsack noted that the inclusion of jpeg/bl3.mk in xfce4-thunar must be propagated in its bl3.mk file.
    Do that, and depend on that version; recursive PKGREVISION bump
    since a few dependencies might link against jpeg now.

--- x11/xfce4-places-plugin/buildlink3.mk
+++ x11/xfce4-places-plugin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2011/01/13 13:36:44 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2011/03/24 14:35:17 wiz Exp $
 
 BUILDLINK_TREE+=	xfce4-places-plugin
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	xfce4-places-plugin
 XFCE4_PLACES_PLUGIN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xfce4-places-plugin+=	xfce4-places-plugin>=1.0.0
-BUILDLINK_ABI_DEPENDS.xfce4-places-plugin?=	xfce4-places-plugin>=1.0.0nb5
+BUILDLINK_ABI_DEPENDS.xfce4-places-plugin?=	xfce4-places-plugin>=1.0.0nb6
 BUILDLINK_PKGSRCDIR.xfce4-places-plugin?=	../../x11/xfce4-places-plugin
 
 .include "../../sysutils/xfce4-thunar/buildlink3.mk"
