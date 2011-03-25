c09cc939619bccdb8e1695d9ae9bceb87c039240
adam <adam@pkgsrc.org>
2011-03-25 13:58:37 +0000

    Fix building on NetBSD-5 with TLS option; fix for PR#44297

---
+++ net/proftpd/patches/patch-contrib_mod_tls.c
@@ -0,0 +1,13 @@
+$NetBSD: patch-contrib_mod_tls.c,v 1.1 2011/03/25 13:58:37 adam Exp $
+
+--- contrib/mod_tls.c.orig	2011-03-25 13:53:45.000000000 +0000
++++ contrib/mod_tls.c
+@@ -7217,7 +7217,7 @@ static int tls_sess_init(void) {
+   if (opts != NULL)
+     tls_opts = *opts;
+ 
+-#if OPENSSL_VERSION_NUMBER > 0x009080cfL
++#if OPENSSL_VERSION_NUMBER > 0x009080cfL && OPENSSL_VERSION_NUMBER != 0x00909000L
+   /* The OpenSSL team realized that the flag added in 0.9.8l, the
+    * SSL3_FLAGS_ALLOW_UNSAFE_LEGACY_RENEGOTIATION flag, was a bad idea.
+    * So in later versions, it was changed to a context flag,
