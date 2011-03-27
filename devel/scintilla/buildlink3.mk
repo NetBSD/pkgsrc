0fa8c2ae5f949ef57d324877786ac76dfb6e221a
joerg <joerg@pkgsrc.org>
2011-03-27 16:46:22 +0000

    Move assignment into conditional block where it belongs

--- devel/scintilla/buildlink3.mk
+++ devel/scintilla/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/03/11 14:38:22 drochner Exp $
-
-BUILDLINK_DEPMETHOD.scintilla?=	build
+# $NetBSD: buildlink3.mk,v 1.2 2011/03/27 16:46:22 joerg Exp $
 
 BUILDLINK_TREE+=	scintilla
 
 .if !defined(SCINTILLA_BUILDLINK3_MK)
 SCINTILLA_BUILDLINK3_MK:=
+
+BUILDLINK_DEPMETHOD.scintilla?=	build
 
 BUILDLINK_API_DEPENDS.scintilla+=	scintilla>=2.23
 BUILDLINK_PKGSRCDIR.scintilla?=	../../devel/scintilla
