8d4bebbd04aade00727b4852cf98abea7e9edf3a
adam <adam@pkgsrc.org>
2011-03-30 08:58:57 +0000

    Add -std=c++0x to CXXFLAGS; fix for PR#42746

--- devel/boost-jam/bjam.mk
+++ devel/boost-jam/bjam.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bjam.mk,v 1.9 2011/02/24 11:05:35 adam Exp $
+# $NetBSD: bjam.mk,v 1.10 2011/03/30 08:58:57 adam Exp $
 
 .include "../../devel/boost-jam/buildlink3.mk"
 
@@ -14,6 +14,7 @@ BJAM_ARGS+=		--builddir=${WRKSRC}/build
 BJAM_ARGS+=		--layout=system
 BJAM_ARGS+=		--toolset=${BOOST_TOOLSET}
 BJAM_ARGS+=		--disable-long-double
+BJAM_ARGS+=		cxxflags=-std=c++0x
 BJAM_ARGS+=		${BJAM_BUILD}
 
 BJAM_BUILD+=		release
