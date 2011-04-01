c9014a0072555517851e43a9ebbc6d68f15746d6
ahoka <ahoka@pkgsrc.org>
2011-04-01 15:46:04 +0000

    Add avahi as default options, because PA tools require it.

--- audio/pulseaudio/options.mk
+++ audio/pulseaudio/options.mk
@@ -1,7 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2010/01/21 12:12:27 drochner Exp $
+# $NetBSD: options.mk,v 1.3 2011/04/01 15:46:04 ahoka Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pulseaudio
 PKG_SUPPORTED_OPTIONS=	avahi
+PKG_SUGGESTED_OPTIONS=	avahi
 PLIST_VARS+=		${PKG_SUPPORTED_OPTIONS}
 .include "../../mk/bsd.options.mk"
 
