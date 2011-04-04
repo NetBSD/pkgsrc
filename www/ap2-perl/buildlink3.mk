309e89f86f0469557905a0e021fdaf3450a6c161
adam <adam@pkgsrc.org>
2011-04-04 09:24:14 +0000

    Changes 2.0.5: * Prepare modperl for the upcoming perl 5.14 * Add lib/ModPerl/MethodLookup.pm to MANIFEST via lib/ModPerl/Manifest.pm * PerlIOApache_write() now throws an APR::Error object, rather than just   a string error, if modperl_wbucket_write() fails. * Authentication tests fail with LWP 5.815 and later * Concise test won't perform unless StatusTerse is set to ON * Look for a usable apxs in $ENV{PATH} if all other options fail, then prompt   the user for one. * Work around bootstrap warnings when Apache2::BuildConfig has not been   created yet. * Remove Apache::test compatibility (part of mod_perl 1.2.7), that code causes   build issues and is 4 versions out of date. * Make sure perl is built either with multiplicity and ithreads or without both * Support for "install_vendor" and "install_site" make targets * Run tests on bundled pure perl Apache::* modules * Implement a mini-preprocess language for map-files in xs/maps. * Implement APR::Socket::fileno * Export PROXYREQ_RESPONSE, a missing PROXYREQ_* constant * Make sure standard file descriptors are preserved by the perl-script handler * Fix the filter init handler attribute check in   modperl_filter_resolve_init_handler() * Make sure buffer is a valid SV in modperl_filter_read() * Move modperl_response_finish() out of modperl_response_handler_run in   mod_perl.c

--- www/ap2-perl/buildlink3.mk
+++ www/ap2-perl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:25:35 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2011/04/04 09:24:14 adam Exp $
 
 BUILDLINK_TREE+=	ap2-perl
 
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	ap2-perl
 AP2_PERL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ap2-perl+=	${APACHE_PKG_PREFIX}-perl>=2.0.1
-BUILDLINK_ABI_DEPENDS.ap2-perl?=	${APACHE_PKG_PREFIX}-perl>=2.0.3
-BUILDLINK_PKGSRCDIR.ap2-perl?=	../../www/ap2-perl
+BUILDLINK_ABI_DEPENDS.ap2-perl+=	${APACHE_PKG_PREFIX}-perl>=2.0.3
+BUILDLINK_PKGSRCDIR.ap2-perl?=		../../www/ap2-perl
 
 .include "../../mk/apache.mk"
 .endif # AP2_PERL_BUILDLINK3_MK
