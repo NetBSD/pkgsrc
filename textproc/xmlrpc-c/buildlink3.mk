e3766c09ef9eeec96ce0846441e5345f2fd5eef9
adam <adam@pkgsrc.org>
2011-04-06 07:45:47 +0000

    Changes 1.16.34: * Fix compile bug with some compilers on 64 bit.
    Changes 1.16.33:
    * Build: Fix prototype/definition mismatch in libwww.cpp with some compilers
      (e.g. Solaris).
    * Build: Fix linking of xml-rpc-api2cpp with some linkers (e.g. Solaris).
    
    Changes 1.16.32:
    * Windows Abyss server: properly handle wait for connection.
    
    Changes 1.16.31:
    * CGI XML-RPC server accepts (ignores) parameters after "text/xml" in
      Content-type header from client.

--- textproc/xmlrpc-c/buildlink3.mk
+++ textproc/xmlrpc-c/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2010/06/15 09:18:10 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2011/04/06 07:45:47 adam Exp $
 
 BUILDLINK_TREE+=	xmlrpc-c
 
 .if !defined(XMLRPC_C_BUILDLINK3_MK)
 XMLRPC_C_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xmlrpc-c+=	xmlrpc-c-ss>=1.06.32
-BUILDLINK_ABI_DEPENDS.xmlrpc-c+=	xmlrpc-c-ss>=1.06.38nb3
+BUILDLINK_API_DEPENDS.xmlrpc-c+=	xmlrpc-c-ss>=1.16.34
+BUILDLINK_ABI_DEPENDS.xmlrpc-c+=	xmlrpc-c-ss>=1.16.34
 BUILDLINK_PKGSRCDIR.xmlrpc-c?=	../../textproc/xmlrpc-c
 
 .include "../../www/libwww/buildlink3.mk"
