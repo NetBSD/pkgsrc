38d07fb5f4db489dcaed8eef0fe9cdb3362cacae
cegger <cegger@pkgsrc.org>
2011-04-06 09:10:26 +0000

    The Xen virtual machine monitor allows running several virtual machines on a single physical machine.  The xentools41 package contains the tools to create, destroy and control the virtual machines.
    This package contains the tools for Xen 4.1.x
    
    Release notes:
    The Xen team is pleased to announce the release of Xen 4.1.
    
    The result of nearly 12 months of development, new features include:
      * A re-architected and improved XL toolstack replacing XM/XEND
      * Prototype credit2 scheduler designed for latency-sensitive workloads and
        very large systems.
      * CPU Pools for advanced partitioning.
      * Support for large systems (>255 processors)
      * Support for x86 Advanced Vector eXtension (AVX).
      * New Memory Access API enabling integration of 3rd party security
        solutions into Xen virtualized environments.
      * Many IOMMU fixes (both Intel VT-d IOMMU and AMD IOMMU).
      * Many toolstack and buildsystem fixes for Linux and NetBSD hosts.
      * Thirdparty libs: libvirt driver for libxl has been merged to upstream
        libvirt.
      * HVM guest PXE boot enhancements, replacing gPXE with iPXE.
      * Even better stability through our new automated regression tests.
    
    Detailed release notes, including a more extensive feature list:
       http://wiki.xen.org/xenwiki/Xen4.1
    
    To download tarballs:
       http://xen.org/products/xen_source.html
    Or the Mercurial source repository (tag 'RELEASE-4.1.0'):
       http://xenbits.xen.org/xen-unstable.hg
    
    And the announcement on the Xen blog:
       http://blog.xen.org/index.php/2011/03/25/xen-4-1-releases/
    
    Thanks to the many people who have contributed to this release!
    
      Regards,
      The Xen Team

---
+++ sysutils/xentools41/files/xend.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# PROVIDE: xend
+# REQUIRE: xencommons
+
+. /etc/rc.subr
+
+name="xend"
+rcvar=$name
+command="@PREFIX@/sbin/xend"
+start_cmd="echo Starting ${name}. && PATH=${PATH}:@PREFIX@/sbin ${command} start"
+pidfile="/var/run/xend.pid"
+
+load_rc_config $name
+run_rc_command "$1"
