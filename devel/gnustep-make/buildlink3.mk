03bd5019be1bc5dca9e04f213990641e4d459aac
rh <rh@pkgsrc.org>
2011-04-07 01:41:31 +0000

    Update gnustep-make to 2.4.0. Notable changes include:    * Parallel building supported using e.g. make -j 2    * Support source files in subdirectories without using a subproject.    * Support header files in sub-subdirectories.    * Batch-compile Java files (much faster).    * Fixes for building documentation.

--- devel/gnustep-make/buildlink3.mk
+++ devel/gnustep-make/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.17 2009/06/14 22:57:59 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2011/04/07 01:41:31 rh Exp $
 
 BUILDLINK_TREE+=	gnustep-make
 
 .if !defined(GNUSTEP_MAKE_BUILDLINK3_MK)
 GNUSTEP_MAKE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnustep-make+=	gnustep-make>=2.0.8nb2
-BUILDLINK_ABI_DEPENDS.gnustep-make?=	gnustep-make>=2.0.8nb2
+BUILDLINK_API_DEPENDS.gnustep-make+=	gnustep-make>=2.4.0
+BUILDLINK_ABI_DEPENDS.gnustep-make?=	gnustep-make>=2.4.0
 BUILDLINK_PKGSRCDIR.gnustep-make?=	../../devel/gnustep-make
 
 .include "../../devel/gnustep-make/gnustep.mk"
