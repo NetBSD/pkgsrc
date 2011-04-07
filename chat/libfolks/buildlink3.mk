03805cd188fa3b07ba6ff1f69481cf4d5ab4e32e
wiz <wiz@pkgsrc.org>
2011-04-07 12:28:02 +0000

    Make PKGNAME match directory name. Ok drochner@

--- chat/libfolks/buildlink3.mk
+++ chat/libfolks/buildlink3.mk
@@ -1,18 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/04/05 18:13:17 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/04/07 12:28:02 wiz Exp $
 
-BUILDLINK_TREE+=	folks
+BUILDLINK_TREE+=	libfolks
 
-.if !defined(FOLKS_BUILDLINK3_MK)
-FOLKS_BUILDLINK3_MK:=
+.if !defined(LIBFOLKS_BUILDLINK3_MK)
+LIBFOLKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.folks+=	folks>=0.3.6
-BUILDLINK_PKGSRCDIR.folks?=	../../chat/libfolks
+BUILDLINK_API_DEPENDS.libfolks+=	libfolks>=0.3.6
+BUILDLINK_PKGSRCDIR.libfolks?=	../../chat/libfolks
 
 .include "../../devel/glib2/buildlink3.mk"
 #.include "../../devel/gobject-introspection/buildlink3.mk"
 #.include "../../sysutils/dbus-glib/buildlink3.mk"
 .include "../../devel/libgee/buildlink3.mk"
 .include "../../chat/telepathy-glib/buildlink3.mk"
-.endif	# FOLKS_BUILDLINK3_MK
+.endif	# LIBFOLKS_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-folks
+BUILDLINK_TREE+=	-libfolks
