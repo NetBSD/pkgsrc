4b497d21e0c4dbefb8715b021f9bd3fb554970c0
schnoebe <schnoebe@pkgsrc.org>
2011-04-08 16:49:32 +0000

    Correct typo on inet6 CONFIGURE_ARGS.

--- net/rdesktop/options.mk
+++ net/rdesktop/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2008/09/12 16:00:56 sborrill Exp $
+# $NetBSD: options.mk,v 1.3 2011/04/08 16:49:32 schnoebe Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.rdesktop
 PKG_SUPPORTED_OPTIONS+=	inet6 ssl
@@ -10,7 +10,7 @@ PKG_SUGGESTED_OPTIONS+=	inet6 ssl
 ### Support both IPv6 and IPv4 connections.
 ###
 .if !empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGE+=	--with-ipv6
+CONFIGURE_ARGS+=	--with-ipv6
 .endif
 
 .if !empty(PKG_OPTIONS:Mssl)
