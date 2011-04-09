515f8f480a52895f0a037410bd361ec540ff7ccc
tez <tez@pkgsrc.org>
2011-04-09 00:16:18 +0000

    correct openssl dependency (it needs >=0.9.8) correct BUILDLINK_API_DEPENDS.mit-krb5 fix building where libtool chokes on "--version-info : " (at least OS X)

--- security/mit-krb5/buildlink3.mk
+++ security/mit-krb5/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2011/03/22 23:31:04 tez Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2011/04/09 00:16:18 tez Exp $
 
 BUILDLINK_TREE+=	mit-krb5
 
 .if !defined(MIT_KRB5_BUILDLINK3_MK)
 MIT_KRB5_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mit-krb5+=		mit-krb5>=1.3.1
+BUILDLINK_API_DEPENDS.mit-krb5+=		mit-krb5>=1.8
 BUILDLINK_ABI_DEPENDS.mit-krb5+=	mit-krb5>=1.4nb1
 BUILDLINK_PKGSRCDIR.mit-krb5?=		../../security/mit-krb5
 .endif # MIT_KRB5_BUILDLINK3_MK
