cba78e699efd77fe61198c17bf6cd3dd91b4e61b
tron <tron>
2011-04-11 13:31:42 +0000

    Pullup ticket #3405 - requested by drochner devel/xulrunner: security update www/firefox: security update
    Revisions pulled up:
    - devel/xulrunner/dist.mk                                       1.19
    - devel/xulrunner/distinfo                                      1.41
    
    ---
       Module Name:    pkgsrc
       Committed By:   drochner
       Date:           Mon Apr  4 11:05:43 UTC 2011
    
       Modified Files:
               pkgsrc/devel/xulrunner: dist.mk distinfo
    
       Log Message:
       update to 3.6.16
       change: blacklist compromized Comodo certificates

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.18 2011/03/05 11:57:16 tnn Exp $
+# $NetBSD: dist.mk,v 1.18.2.1 2011/04/11 13:31:42 tron Exp $
 #
 # used by devel/nspr/Makefile
 # used by devel/nss/Makefile
@@ -8,7 +8,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	3.6${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	1.9.2
-MOZ_BRANCH_MINOR=	.15
+MOZ_BRANCH_MINOR=	.16
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
