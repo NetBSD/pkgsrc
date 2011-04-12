a021549cbf81ba431d73d823d2e1c98c83a3abc8
adam <adam@pkgsrc.org>
2011-04-12 17:41:28 +0000

    Changes 0.18.1: * msggrep: A '$' anchor in a regular expression now also matches the end of   the string, even if it does not end in a newline. * Dependencies:   The libraries and programs are now linked with libunistring if this library   is already installed. * Installation options:   The configure option --with-cvs is deprecated. The 'autopoint' program will   now use the 'git' program by default to compress its archive. If the   configure option --without-git is specified, 'autopoint' will not rely on   'git', but will instead rely on a locally installed a 3 MB large archive.
    Changes 0.18:
    * Runtime behaviour:
      - On MacOS X and Windows systems, <libintl.h> now extends setlocale() and
        newlocale() so that their determination of the default locale considers
        the choice the user has made in the system control panels.
      - On MacOS X systems, the gettext()/dgettext()/... functions now respect the
        locale of the current thread, if a thread-specific locale has been set.
    * PO file format:
      There is a new field 'Language' in the header entry.  It denotes the language
      code (plus optional country code) for the PO file.  This field can be used
      by automated tools, such as spell checkers.  It is expected to be more
      reliable than looking at the file name or at the 'Language-Team' field in
      the header entry.
      msgmerge, msgcat, msgen have a new option --lang that allows to specify
      this field.  Additionally, msgmerge fills in this new field by looking at
      the 'Language-Team' field (if the --lang option is not given).
    * xgettext and PO file format:
      For messages with plural forms, programmers can inform the translators
      about the range of possible values of the numeric argument, like this:
        /* xgettext: range: 0..15 */
      This information 'range: 0..15' is stored in the PO file as a flag attached
      to the message.  Translators can produce better translations when they know
      that the numeric argument is small.
    * Colorized PO files:
      msgattrib, msgcomm, msgconv, msgen, msgfilter, msggrep, msginit, msgmerge,
      msgunfmt, msguniq, xgettext now have options --color and --style, like msgcat
      has since version 0.17.
    * msgmerge is up to 10 times faster when the PO and POT files are large.
      This speedup was contributed by Ralf Wildenhues.
    * msgcmp has a new option -N/--no-fuzzy-matching, like msgmerge has since
      version 0.12.
    * msgfilter now sets environment variables during the invocation of the
      filter, indicating the msgid and location of the messge being processed.
    * xgettext now can extract plural forms from Qt 4 programs. The recommended
      xgettext command-line options for this case are:
        --qt --keyword=tr:1,1t --keyword=tr:1,2c,2t --keyword=tr:1,1,2c,3t
    * xgettext --language=GCC-source now recognizes also the format strings
      used in the Fortran front-end of the GCC compiler, and marks them as
      'gfc-internal-format'.
    * autopoint can now be used to update several PO directories all together.

--- devel/gettext-asprintf/buildlink3.mk
+++ devel/gettext-asprintf/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/03/20 19:24:13 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2011/04/12 17:41:28 adam Exp $
 
 BUILDLINK_TREE+=	gettext-asprintf
 
 .if !defined(GETTEXT_ASPRINTF_BUILDLINK3_MK)
 GETTEXT_ASPRINTF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gettext-asprintf+=	gettext-asprintf>=0.14.5
+BUILDLINK_API_DEPENDS.gettext-asprintf+=	gettext-asprintf>=0.18
 BUILDLINK_PKGSRCDIR.gettext-asprintf?=	../../devel/gettext-asprintf
 .endif # GETTEXT_ASPRINTF_BUILDLINK3_MK
 
