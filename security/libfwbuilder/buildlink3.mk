4973c01b796b85bb0c078b645769b301832283c1
adam <adam@pkgsrc.org>
2011-04-12 13:41:17 +0000

    Changes 4.1.3: This version comes with the following usability enhancements: * Added checkbox to the Preferences dialog, this checkbox turns off some   tooltips that can be annoying for users who are sufficiently familiar with   the GUI. * Added a tab "Policy Rule" to the "Objects" page of the global preferences   dialog; checkbox in this tab allows the user to choose whether new policy   rules should be created with logging turned on or off. The major bug fixes in this release include: * Fixed installer issue for Windows users that use Putty sessions. Built-in   policy installer can use putty session on Windows when it runs pscp.exe   utility to copy generated script to the firewall * Fixed bug in the generated iptables script that made it to not configure   broadcast address when it added ip addresses to interfaces. * Several bugs that affected cluster configurations were fixed.

--- security/libfwbuilder/buildlink3.mk
+++ security/libfwbuilder/buildlink3.mk
@@ -1,19 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.22 2011/01/13 13:36:16 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2011/04/12 13:41:17 adam Exp $
 
 BUILDLINK_TREE+=	libfwbuilder
 
 .if !defined(LIBFWBUILDER_BUILDLINK3_MK)
 LIBFWBUILDER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libfwbuilder+=	libfwbuilder>=2.0.12
-BUILDLINK_ABI_DEPENDS.libfwbuilder+=	libfwbuilder>=2.0.12nb4
+BUILDLINK_API_DEPENDS.libfwbuilder+=	libfwbuilder>=4.1.3
+BUILDLINK_ABI_DEPENDS.libfwbuilder+=	libfwbuilder>=4.1.3
 BUILDLINK_PKGSRCDIR.libfwbuilder?=	../../security/libfwbuilder
 
-.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
 .include "../../net/net-snmp/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../textproc/libxslt/buildlink3.mk"
-.include "../../x11/qt3-libs/buildlink3.mk"
+.include "../../x11/qt4-libs/buildlink3.mk"
 .endif # LIBFWBUILDER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libfwbuilder
