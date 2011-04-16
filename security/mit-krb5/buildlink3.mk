3b1b49ae4e41efa5af1d138a52ed267e2d11ab83
tron <tron@pkgsrc.org>
2011-04-16 09:43:46 +0000

    Pullup ticket #3415 - requested by tez security/mit-krb5: security patch
    Revisions pulled up:
    - security/mit-krb5/Makefile                                    1.52-1.53
    - security/mit-krb5/buildlink3.mk                               1.11
    - security/mit-krb5/builtin.mk                                  1.7
    - security/mit-krb5/distinfo                                    1.28-1.29
    - security/mit-krb5/patches/patch-ck                            1.1
    
    ---
       Module Name:    pkgsrc
       Committed By:   tez
       Date:           Fri Apr  8 17:30:35 UTC 2011
    
       Modified Files:
               pkgsrc/security/mit-krb5: builtin.mk
    
       Log Message:
       Add builtin support for SunOS per PR#44597
    
    ---
       Module Name:    pkgsrc
       Committed By:   tez
       Date:           Sat Apr  9 00:16:18 UTC 2011
    
       Modified Files:
               pkgsrc/security/mit-krb5: Makefile buildlink3.mk distinfo
       Added Files:
               pkgsrc/security/mit-krb5/patches: patch-ck
    
       Log Message:
       correct openssl dependency (it needs >=0.9.8)
       correct BUILDLINK_API_DEPENDS.mit-krb5
       fix building where libtool chokes on "--version-info : " (at least OS X)
    
    ---
       Module Name:    pkgsrc
       Committed By:   tez
       Date:           Thu Apr 14 19:37:26 UTC 2011
    
       Modified Files:
               pkgsrc/security/mit-krb5: Makefile distinfo
    
       Log Message:
       fix MITKRB5-SA-2011-004 (CVE-2011-0285) DOS in kadmind

--- security/mit-krb5/buildlink3.mk
+++ security/mit-krb5/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2011/03/22 23:31:04 tez Exp $
+# $NetBSD: buildlink3.mk,v 1.10.2.1 2011/04/16 09:43:47 tron Exp $
 
 BUILDLINK_TREE+=	mit-krb5
 
 .if !defined(MIT_KRB5_BUILDLINK3_MK)
 MIT_KRB5_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mit-krb5+=		mit-krb5>=1.3.1
+BUILDLINK_API_DEPENDS.mit-krb5+=		mit-krb5>=1.8
 BUILDLINK_ABI_DEPENDS.mit-krb5+=	mit-krb5>=1.4nb1
 BUILDLINK_PKGSRCDIR.mit-krb5?=		../../security/mit-krb5
 .endif # MIT_KRB5_BUILDLINK3_MK
