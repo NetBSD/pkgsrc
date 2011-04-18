d084bf3f1c4fbdf859fa0c522476b5e13f59e87b
manu <manu@pkgsrc.org>
2011-04-18 16:19:47 +0000

    Update glusterfs to 3.1.4.
    Major new features according to http://www.gluster.org/
    - Elastic Volume Management
    - New Gluster Console Manager
    - Dynamic Storage for the data center and cloud

---
+++ filesystems/glusterfs/files/glusterd.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# $NetBSD: glusterd.sh,v 1.1 2011/04/18 16:19:48 manu Exp $
+#
+
+# PROVIDE: glusterd
+# REQUIRE: rpcbind
+# BEFORE: glusterfsd
+
+$_rc_subr_loaded . /etc/rc.subr
+
+name="glusterd"
+rcvar=$name
+command="@PREFIX@/sbin/${name}"
+pidfile="/var/run/${name}.pid"
+required_files="@PREFIX@/etc/glusterfs/${name}.vol"
+
+load_rc_config $name
+run_rc_command "$1"
