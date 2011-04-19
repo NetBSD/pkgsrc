25523c88be9983b98bdb400ae62915a85782ee2b
tnn <tnn>
2011-04-19 11:16:02 +0000

    Re-import previous version of Firefox, because firefox-4.0 won't work for some users (sparc64 support, for example, is busted and won't appear again until firefox-5.0).

---
+++ devel/xulrunner192/dist.mk
@@ -0,0 +1,16 @@
+# $NetBSD: dist.mk,v 1.1.1.1 2011/04/19 11:16:02 tnn Exp $
+#
+# used by devel/xulrunner192/Makefile
+# used by www/firefox36/Makefile
+
+DISTNAME=	firefox-${FIREFOX_VER}.source
+FIREFOX_VER=	3.6${MOZ_BRANCH_MINOR}
+MOZ_BRANCH=	1.9.2
+MOZ_BRANCH_MINOR=	.16
+MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
+EXTRACT_SUFX=	.tar.bz2
+
+DISTINFO_FILE=	${.CURDIR}/../../devel/xulrunner192/distinfo
+PATCHDIR=	${.CURDIR}/../../devel/xulrunner192/patches
+
+WRKSRC=		${WRKDIR}/mozilla-${MOZ_BRANCH}
