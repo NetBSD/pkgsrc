90aa6fa10c7c2c68ebde1c5a56ec259746cdb9c2
hans <hans>
2011-04-21 21:06:25 +0000

    The pkg name is libaudiofile, so use that in buildlink3.mk.

--- audio/libaudiofile/buildlink3.mk
+++ audio/libaudiofile/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.14 2009/03/20 19:23:54 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2011/04/21 21:06:25 hans Exp $
 
-BUILDLINK_TREE+=	audiofile
+BUILDLINK_TREE+=	libaudiofile
 
 .if !defined(AUDIOFILE_BUILDLINK3_MK)
 AUDIOFILE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.audiofile+=	libaudiofile>=0.2.1
-BUILDLINK_ABI_DEPENDS.audiofile+=	libaudiofile>=0.2.6nb1
-BUILDLINK_PKGSRCDIR.audiofile?=	../../audio/libaudiofile
+BUILDLINK_API_DEPENDS.libaudiofile+=	libaudiofile>=0.2.1
+BUILDLINK_ABI_DEPENDS.libaudiofile+=	libaudiofile>=0.2.6nb1
+BUILDLINK_PKGSRCDIR.libaudiofile?=	../../audio/libaudiofile
 .endif # AUDIOFILE_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-audiofile
+BUILDLINK_TREE+=	-libaudiofile
