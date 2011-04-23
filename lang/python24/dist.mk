0c147b0ca7304db0779098e0784d46cd5c31b44e
obache <obache>
2011-04-23 08:53:53 +0000

    Update python24 to 2.4.6.
    What's New in Python 2.4.6?
    ===========================
    
    *Release date: 19-Dec-2008*
    
    
    
    What's New in Python 2.4.6c1?
    =============================
    
    *Release date: 13-Dec-2008*
    
    Core and builtins
    -----------------
    
    - Issue #4469: Prevent expandtabs() on string and unicode
      objects from causing a segfault when a large width is passed
      on 32-bit platforms. CVE-2008-5031.
    
    - Issue #4317: Fixed a crash in the imageop.rgb2rgb8() function.
    
    - Issue #4230: Fix a crash when a class has a custom __getattr__ and an
      __getattribute__ method that deletes the __getattr__ attribute.
    
    - Apply security patches from Apple. CVE-2008-2315.
    
    - Issue #2620: Overflow checking when allocating or reallocating memory
      was not always being done properly in some python types and extension
      modules.  PyMem_MALLOC, PyMem_REALLOC, PyMem_NEW and PyMem_RESIZE have
      all been updated to perform better checks and places in the code that
      would previously leak memory on the error path when such an allocation
      failed have been fixed.
    
    - Issue #1179: Fix CVE-2007-4965 and CVE-2008-1679, multiple integer
      overflows in the imageop and rgbimgmodule modules.
    
    - Issue #2586: Fix CVE-2008-1721, zlib crash from
      zlib.decompressobj().flush(val) when val is not positive.
    
    - Issues #2588, #2589: Fix potential integer underflow and overflow
      conditions in the PyOS_vsnprintf C API function. CVE-2008-3144.
    
    - Issue #2587: In the C API, PyString_FromStringAndSize() takes a signed size
      parameter but was not verifying that it was greater than zero.  Values
      less than zero will now raise a SystemError and return NULL to indicate a
      bug in the calling C code. CVE-2008-1887.
    
    - Security Issue #2: imageop did not validate arguments correctly and could
      segfault as a result. CVE-2008-4864.
    
    Extension Modules
    -----------------
    
    Library
    -------
    
    Tests
    -----
    
    Build
    -----
    
    Tools/Demos
    -----------
    
    - Tools/faqwiz/move-faqwiz.sh: Fix unsecure use of temporary files.

--- lang/python24/dist.mk
+++ lang/python24/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.1 2010/09/17 07:11:41 obache Exp $
+# $NetBSD: dist.mk,v 1.2 2011/04/23 08:53:53 obache Exp $
 
-PY_DISTVERSION=	2.4.5
+PY_DISTVERSION=	2.4.6
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.bz2
 DISTINFO_FILE=	${.CURDIR}/../../lang/python24/distinfo
