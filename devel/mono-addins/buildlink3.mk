a9eec9c75c8c7b842d3cd226406c0604e17c6dd4
kefren <kefren@pkgsrc.org>
2011-04-25 14:02:30 +0000

    Update to mono-addins 0.6.1, part of mono 2.10

--- devel/mono-addins/buildlink3.mk
+++ devel/mono-addins/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2011/04/22 13:42:31 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2011/04/25 14:02:30 kefren Exp $
 
 BUILDLINK_TREE+=	mono-addins
 
@@ -8,6 +8,7 @@ MONO_ADDINS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.mono-addins+=	mono-addins>=0.3
 BUILDLINK_ABI_DEPENDS.mono-addins?=	mono-addins>=0.5nb1
 BUILDLINK_PKGSRCDIR.mono-addins?=	../../devel/mono-addins
+BUILDLINK_CONTENTS_FILTER.mono-addins+=	${EGREP} '^lib/'
 
 .include "../../lang/mono/buildlink3.mk"
 .include "../../x11/gtk-sharp/buildlink3.mk"
