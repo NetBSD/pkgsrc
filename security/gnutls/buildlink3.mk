4cf2a3d2caa53937222c2f2479165848e0ce6215
adam <adam@pkgsrc.org>
2011-04-26 10:35:29 +0000

    Changes 2.12.3: * libgnutls: Several minor bugfixes. * libgnutls: Restored HMAC-MD5 for compatibility. Although considered weak,   several sites require it for connection. It is enabled for "NORMAL" and   "PERFORMANCE" priority strings. * libgnutls: depend on libdl. * libgnutls: gnutls_transport_set_global_errno() was deprecated. Use your   system's errno fascility or gnutls_transport_set_errno(). * gnutls-cli: Correction with usage of select to check for pending data in   gnutls sessions. It now uses gnutls_record_check_pending(). * tests: More fixes and updates for win32. Patches by LRN. * libgnutls: Several files unnecessarily included <gcrypt.h>; this has been   fixed. ** API and ABI modifications: gnutls_transport_set_global_errno: DEPRECATED
    Changes 2.12.2:
    * libgnutls: Several updates and fixes for win32. Patches by LRN.
    * libgnutls: Several bug and memory leak fixes.
    * srptool: Accepts the -d option to enable debugging.
    * libgnutls: Corrected bug in gnutls_srp_verifier() that prevented the
      allocation of a verifier. Reported by Andrew Wiseman.
    
    Changes 2.12.1:
    * certtool: Generated certificate request with stricter permissions.
    * libgnutls: Bug fixes in opencdk code. Reported by Vitaly Kruglikov.
    * libgnutls: Corrected windows system_errno() function prototype.
    * libgnutls: C++ compatibility fix for compat.h. Reported by Mark Brand.
    * libgnutls: Fix size of gnutls_openpgp_keyid_t by using the
      GNUTLS_OPENPGP_KEYID_SIZE definition. Reported by Andreas Metzler.

--- security/gnutls/buildlink3.mk
+++ security/gnutls/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.24 2011/04/22 13:42:00 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2011/04/26 10:35:29 adam Exp $
 
 BUILDLINK_TREE+=	gnutls
 
 .if !defined(GNUTLS_BUILDLINK3_MK)
 GNUTLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnutls+=	gnutls>=1.2.6
-BUILDLINK_ABI_DEPENDS.gnutls+=	gnutls>=2.10.5nb2
+BUILDLINK_API_DEPENDS.gnutls+=	gnutls>=2.12.3
+BUILDLINK_ABI_DEPENDS.gnutls+=	gnutls>=2.12.3
 BUILDLINK_PKGSRCDIR.gnutls?=	../../security/gnutls
 
 .include "../../archivers/lzo/buildlink3.mk"
