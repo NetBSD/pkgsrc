95de756714e248eecdb1d112843d3153cfd31104
tnn <tnn@pkgsrc.org>
2011-04-26 14:19:39 +0000

    bump version for mozilla 2.0 branch update

--- devel/nss/buildlink3.mk
+++ devel/nss/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.12 2010/01/20 17:24:57 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2011/04/26 14:20:03 tnn Exp $
 
 BUILDLINK_TREE+=	nss
 
 .if !defined(NSS_BUILDLINK3_MK)
 NSS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nss+=	nss>=3.12.4.5
+BUILDLINK_API_DEPENDS.nss+=	nss>=3.12.9.0
 BUILDLINK_PKGSRCDIR.nss?=	../../devel/nss
 
 BUILDLINK_LIBDIRS.nss+=		lib/nss
