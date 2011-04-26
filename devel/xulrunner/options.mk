b17baa0b9c852a4dc64dc31ba96ea2a6c991067c
tnn <tnn@pkgsrc.org>
2011-04-26 14:16:36 +0000

    Update to firefox-4.0.
    Firefox 4 is based on the Gecko 2.0 Web platform. This release features
    JavaScript execution speeds up to six times faster than the previous
    version, new capabilities for Web Developers and Add-on Developers such as
    hardware accelerated graphics and HTML5 technologies, and a completely
    revised user interface.

--- devel/xulrunner/options.mk
+++ devel/xulrunner/options.mk
@@ -1,21 +1,22 @@
-# $NetBSD: options.mk,v 1.8 2010/04/26 12:47:08 tnn Exp $
+# $NetBSD: options.mk,v 1.9 2011/04/26 14:16:36 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xulrunner
-PKG_SUPPORTED_OPTIONS=	debug mozilla-jemalloc gnome
+PKG_SUPPORTED_OPTIONS=	debug mozilla-jemalloc gnome pulseaudio
 
-PLIST_VARS+=	jit gnome debug jemalloc
+PLIST_VARS+=	jit gnome debug
 
 .if ${OPSYS} == "Linux" || ${OPSYS} == "SunOS"
 PKG_SUGGESTED_OPTIONS+=	mozilla-jemalloc
 .endif
 
 .if !empty(MACHINE_ARCH:Mi386) || !empty(MACHINE_ARCH:Msparc) || \
-	!empty(MACHINE_ARCH:Marm)
+	!empty(MACHINE_ARCH:Marm) || !empty(MACHINE_ARCH:Mx86_64)
 PKG_SUPPORTED_OPTIONS+=	mozilla-jit
 PKG_SUGGESTED_OPTIONS+=	mozilla-jit
 NANOJIT_ARCH.i386=	i386
 NANOJIT_ARCH.arm=	ARM
 NANOJIT_ARCH.sparc=	Sparc
+NANOJIT_ARCH.x86_64=	X64
 .endif
 
 .include "../../mk/bsd.options.mk"
@@ -34,7 +35,6 @@ CONFIGURE_ARGS+=	--disable-libnotify
 
 .if !empty(PKG_OPTIONS:Mmozilla-jemalloc)
 CONFIGURE_ARGS+=	--enable-jemalloc
-PLIST.jemalloc=		yes
 .else
 CONFIGURE_ARGS+=	--disable-jemalloc
 .endif
@@ -50,7 +50,12 @@ CONFIGURE_ARGS+=	--disable-debug
 .if !empty(PKG_OPTIONS:Mmozilla-jit)
 PLIST.jit=		yes
 PLIST_SUBST+=		NANOJIT_ARCH=${NANOJIT_ARCH.${MACHINE_ARCH}}
-CONFIGURE_ARGS+=	--enable-jit
+CONFIGURE_ARGS+=	--enable-tracejit
 .else
-CONFIGURE_ARGS+=	--disable-jit
+CONFIGURE_ARGS+=	--disable-tracejit
+.endif
+
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+.include "../../audio/pulseaudio/buildlink3.mk"
+ALL_ENV+=	PKGSRC_PULSEAUDIO=1
 .endif
