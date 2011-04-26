b91b65a6b843da9e97c644bed04340d54a88b01f
tnn <tnn@pkgsrc.org>
2011-04-26 14:19:39 +0000

    bump version for mozilla 2.0 branch update

--- devel/nspr/buildlink3.mk
+++ devel/nspr/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.15 2010/12/23 12:24:00 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2011/04/26 14:19:39 tnn Exp $
 
 BUILDLINK_TREE+=	nspr
 
 .if !defined(NSPR_BUILDLINK3_MK)
 NSPR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.8.6
+BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.8.7
 BUILDLINK_PKGSRCDIR.nspr?=	../../devel/nspr
 
 BUILDLINK_FILES.nspr+=          lib/nspr/*
