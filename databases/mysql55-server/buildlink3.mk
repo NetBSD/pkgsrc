347d0c7a1c1b67169b626a27178bd228cfac145e
adam <adam@pkgsrc.org>
2011-04-27 08:48:59 +0000

    Removed USE_LIBTOOL and BUILDLINK_LIBDIRS

--- databases/mysql55-server/buildlink3.mk
+++ databases/mysql55-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/04/25 21:12:13 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/04/27 08:49:01 adam Exp $
 
 BUILDLINK_TREE+=	mysql-server
 
@@ -7,7 +7,6 @@ MYSQL_SERVER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mysql-server+=	mysql-server>=5.5.11
 BUILDLINK_PKGSRCDIR.mysql-server?=	../../databases/mysql55-server
-BUILDLINK_LIBDIRS.mysql-server?=	lib/mysql
 
 #.include "../../databases/mysql55-client/buildlink3.mk"
 .endif	# MYSQL_SERVER_BUILDLINK3_MK
