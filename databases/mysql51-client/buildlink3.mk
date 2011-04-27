8a9c0e3b5810a419ac45a53937cbf12ba4e9060c
obache <obache@pkgsrc.org>
2011-04-27 06:33:22 +0000

    not compatible with mysql-5.5.

--- databases/mysql51-client/buildlink3.mk
+++ databases/mysql51-client/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/03/04 16:27:18 taca Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/04/27 06:33:22 obache Exp $
 
 BUILDLINK_TREE+=	mysql-client
 
 .if !defined(MYSQL_CLIENT_BUILDLINK3_MK)
 MYSQL_CLIENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mysql-client+=	mysql-client>=5.1.44
-BUILDLINK_ABI_DEPENDS.mysql-client+=	mysql-client>=5.1.44
+BUILDLINK_API_DEPENDS.mysql-client+=	mysql-client>=5.1.44<5.5
+BUILDLINK_ABI_DEPENDS.mysql-client+=	mysql-client>=5.1.44<5.5
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql51-client
 BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
 BUILDLINK_INCDIRS.mysql-client?=	include/mysql
