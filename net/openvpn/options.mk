620b8f615eeb6ad8be567dec0b897bc08672f4c5
adam <adam@pkgsrc.org>
2011-04-28 07:27:24 +0000

    Changes 2.2.0: * Several man-page updates * Several buildsystem fixes * Fixed a bug with GUI icon deletion on upgrade from 2.2-RC or earlier * Change the default --tmp-dir path to a more suitable path * Improve the mysprintf() issue in openvpnserv.c * Fixed bug in port-share that could cause port share process to crash * Fix the --client-cert-not-required feature

--- net/openvpn/options.mk
+++ net/openvpn/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2010/09/05 20:33:48 adam Exp $
+# $NetBSD: options.mk,v 1.5 2011/04/28 07:27:24 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.openvpn
 PKG_SUPPORTED_OPTIONS=	pkcs11 pam
@@ -7,27 +7,25 @@ PKG_SUGGESTED_OPTIONS=
 .include "../../mk/bsd.options.mk"
 
 # include support for certificates on a stick (or card)
-
 .if !empty(PKG_OPTIONS:Mpkcs11)
 .include "../../security/pkcs11-helper/buildlink3.mk"
 .else
-# it would pick it up halfways when installed, and fail building
-CONFIGURE_ARGS+= --disable-pkcs11
+CONFIGURE_ARGS+=	--disable-pkcs11
 .endif
 
-PLIST_VARS+=	pam
+PLIST_VARS+=		pam
 .if !empty(PKG_OPTIONS:Mpam)
-USE_TOOLS+=	gmake
-BUILD_DIRS+=	plugin/auth-pam
-BUILD_TARGET=	# empty
-INSTALL_DIRS=	.
-INSTALL_TARGET=	install
+PLIST.pam=		yes
+USE_TOOLS+=		gmake
+
+BUILD_DIRS+=		plugin/auth-pam
+BUILD_TARGET=		# empty
+INSTALL_DIRS=		.
+INSTALL_TARGET=		install
 INSTALLATION_DIRS+=	lib/openvpn
-PLIST.pam=	yes
 post-install-pam:
-	cd ${WRKSRC}/plugin/auth-pam &&				\
-	${INSTALL_LIB} openvpn-auth-pam.so 			\
-		       ${DESTDIR}${PREFIX}/lib/openvpn || ${TRUE}
+	${INSTALL_LIB} ${WRKSRC}/plugin/auth-pam/openvpn-auth-pam.so \
+		${DESTDIR}${PREFIX}/lib/openvpn || ${TRUE}
 .include "../../mk/pam.buildlink3.mk"
 .else
 post-install-pam:
