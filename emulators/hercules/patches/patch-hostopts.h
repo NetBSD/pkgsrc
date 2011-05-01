73a7ad77585e0eb4d43071a871db22f908a701f7
ryoon <ryoon>
2011-05-01 23:28:57 +0000

    Update to 3.07
    Fix PR pkg/44894
    
    Changelog is to long.
    See CHANGES file in tar ball.

---
+++ emulators/hercules/patches/patch-hostopts.h
@@ -0,0 +1,28 @@
+$NetBSD: patch-hostopts.h,v 1.1 2011/05/01 23:28:59 ryoon Exp $
+
+--- hostopts.h.orig	2010-01-08 22:56:27.000000000 +0000
++++ hostopts.h
+@@ -272,7 +272,7 @@
+ /*-------------------------------------------------------------------*/
+ /* Hard-coded FreeBSD-specific features and options...               */
+ /*-------------------------------------------------------------------*/
+-#elif defined(__FreeBSD__)              /* "FreeBSD" options         */
++#elif defined(__FreeBSD__) || defined(__NetBSD__)/* "BSD" options    */
+ 
+ #define DLL_IMPORT   extern
+ #define DLL_EXPORT
+@@ -295,6 +295,14 @@
+ #undef  OPTION_EXTCURS                  /* Normal cursor handling    */
+ 
+ 
++/* These should become configure checks */
++# if defined(__NetBSD__)
++#  define OPTION_SCSI_TAPE                /* SCSI tape support         */
++# endif
++# if !defined(SOL_TCP)
++#  define SOL_TCP	IPPROTO_TCP
++# endif
++
+ /*-------------------------------------------------------------------*/
+ /* GNU Linux options...                                              */
+ /*-------------------------------------------------------------------*/
