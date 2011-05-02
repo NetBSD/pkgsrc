166849089f08bc8525eb6d47c90394c16440732a
adam <adam@pkgsrc.org>
2011-05-02 21:23:01 +0000

    Changes 1.3.19: * New sound driver architecture for MMDevAPI. * Better support for relative mouse events in DInput. * Debugger support for the ARM platform. * Various improvements in D3DX9. * More MSVC runtime functions. * Various bug fixes.

--- emulators/wine-devel/buildlink3.mk
+++ emulators/wine-devel/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2010/12/23 11:44:30 dsainty Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2011/05/02 21:23:01 adam Exp $
 
 BUILDLINK_TREE+=	wine
 
 .if !defined(WINE_BUILDLINK3_MK)
 WINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.wine+=	wine>=0.9.6nb1
-BUILDLINK_ABI_DEPENDS.wine?=	wine>=1.1.16nb2
+BUILDLINK_API_DEPENDS.wine+=	wine>=1.3.0
 BUILDLINK_PKGSRCDIR.wine?=	../../emulators/wine
 
 pkgbase:= wine
@@ -17,8 +16,8 @@ pkgbase:= wine
 .include "../../graphics/glu/buildlink3.mk"
 .include "../../graphics/glut/buildlink3.mk"
 .endif
+.include "../../mk/giflib.buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
-.include "../../graphics/libungif/buildlink3.mk"
 .endif # WINE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-wine
