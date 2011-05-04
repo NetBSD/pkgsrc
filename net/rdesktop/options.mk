47ec369c194df59f5e9996918f30643cff4ebf44
tron <tron>
2011-05-04 17:21:25 +0000

    Pullup ticket #3421 - requested by obache net/rdesktop: security update
    Revisions pulled up:
    - net/rdesktop/Makefile                                         1.38
    - net/rdesktop/distinfo                                         1.20
    - net/rdesktop/options.mk                                       1.3-1.4
    
    ---
       Module Name:	pkgsrc
       Committed By:	schnoebe
       Date:		Fri Apr  8 16:49:32 UTC 2011
    
       Modified Files:
       	pkgsrc/net/rdesktop: options.mk
    
       Log Message:
       Correct typo on inet6 CONFIGURE_ARGS.
    
    ---
       Module Name:	pkgsrc
       Committed By:	obache
       Date:		Wed May  4 14:47:18 UTC 2011
    
       Modified Files:
       	pkgsrc/net/rdesktop: Makefile distinfo options.mk
    
       Log Message:
       Update rdesktop to 1.7.0.
    
       This fixes some important bugs and is therefore recommended for all users.
    
       Key changes:
    
           * Security: Directory traversal vulnerability with disk redirection (disallow /.. requests)
           * New maintainer: Peter Åstrand <astrand@...>
           * Brush cache support
           * Removed the hardcoded limit of the username length
           * Increased domain name length to 255 chars
           * Improved compatibility with PulseAudio/padsp
           * Cleaned up and documented the return values
           * Keyboard fix: avoid stuck keys in certain cases
           * Support for new pointers
           * License has been changed to GPLv3
           * EWMH fixes for 64-bit machines
           * RandR support: automatically resize session if using relative screen size
           * Improved support for Windows 2008 Session Broker
           * Japanese keyboard map has been improved
           * New keyboard map: fr-bepo
           * Many stability fixes regarding smart card redirection
           * Windows 2008 R2 / 7: Fix sound playback when not using other redirections
           * Windows 2008 R2 / 7: Solve disk redirection read-only issues
           * Windows 2008 R2 / 7: Solve issue with recursive deletion
           * Avoid exit when printing, if lpr command terminates early

--- net/rdesktop/options.mk
+++ net/rdesktop/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2008/09/12 16:00:56 sborrill Exp $
+# $NetBSD: options.mk,v 1.2.24.1 2011/05/04 17:21:25 tron Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.rdesktop
-PKG_SUPPORTED_OPTIONS+=	inet6 ssl
-PKG_SUGGESTED_OPTIONS+=	inet6 ssl
+PKG_SUPPORTED_OPTIONS+=	inet6 ssl xrandr
+PKG_SUGGESTED_OPTIONS+=	inet6 ssl xrandr
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,10 +10,14 @@ PKG_SUGGESTED_OPTIONS+=	inet6 ssl
 ### Support both IPv6 and IPv4 connections.
 ###
 .if !empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGE+=	--with-ipv6
+CONFIGURE_ARGS+=	--with-ipv6
 .endif
 
 .if !empty(PKG_OPTIONS:Mssl)
 CONFIGURE_ARGS+=	--with-openssl=${SSLBASE}
 .include "../../security/openssl/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mxrandr)
+.include "../../x11/libXrandr/buildlink3.mk"
 .endif
