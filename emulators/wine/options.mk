24da9356430adefc36851663fc85f9818db493e3
adam <adam>
2011-05-04 20:31:39 +0000

    Changes 1.2.3: * Translation updates * Various bug fixes

--- emulators/wine/options.mk
+++ emulators/wine/options.mk
@@ -1,21 +1,28 @@
-# $NetBSD: options.mk,v 1.12 2009/04/20 18:55:42 adam Exp $
+# $NetBSD: options.mk,v 1.13 2011/05/04 20:31:39 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.wine
-PKG_SUPPORTED_OPTIONS=	cups opengl sane esound ldap hal ssl x11
-PKG_SUGGESTED_OPTIONS=	opengl esound ldap hal ssl x11
+PKG_SUPPORTED_OPTIONS=	cups dbus esound hal ldap opengl sane ssl x11
+PKG_SUGGESTED_OPTIONS=	opengl ssl x11
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		opengl x11
+PLIST_VARS+=		esd opengl x11
 
-.if !empty(PKG_OPTIONS:Mesound)
-.include "../../audio/esound/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mcups)
+.include "../../print/cups/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-cups
 .endif
 
-.if !empty(PKG_OPTIONS:Mldap)
-.include "../../databases/openldap-client/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mdbus)
+.include "../../sysutils/dbus/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=	--without-ldap
+CONFIGURE_ARGS+=	--without-dbus
+.endif
+
+.if !empty(PKG_OPTIONS:Mesound)
+PLIST.esd=		yes
+.include "../../audio/esound/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mhal)
@@ -24,16 +31,16 @@ CONFIGURE_ARGS+=	--without-ldap
 CONFIGURE_ARGS+=	--without-hal
 .endif
 
+.if !empty(PKG_OPTIONS:Mldap)
+.include "../../databases/openldap-client/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-ldap
+.endif
+
 .if !empty(PKG_OPTIONS:Mssl)
 .include "../../security/openssl/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-openssl
-.endif
-
-.if !empty(PKG_OPTIONS:Mcups)
-.include "../../print/cups/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-cups
 .endif
 
 .if !empty(PKG_OPTIONS:Mx11)
@@ -43,6 +50,7 @@ PLIST.x11=		yes
 .include "../../x11/libICE/buildlink3.mk"
 .include "../../x11/libSM/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXcomposite/buildlink3.mk"
 .include "../../x11/libXcursor/buildlink3.mk"
 .include "../../x11/libXext/buildlink3.mk"
 .include "../../x11/libXi/buildlink3.mk"
@@ -57,10 +65,8 @@ CONFIGURE_ARGS+=	--without-x
 
 .if !empty(PKG_OPTIONS:Mopengl) && !empty(PKG_OPTIONS:Mx11)
 PLIST.opengl=		yes
-.  if ${OPSYS} != "Darwin"
 .include "../../graphics/glu/buildlink3.mk"
 .include "../../graphics/glut/buildlink3.mk"
-.  endif
 .else
 CONFIGURE_ARGS+=	--without-opengl
 .endif
