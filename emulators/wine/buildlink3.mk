6f16e7cb4da7110195cf215d4f42c4a4e6ba6317
adam <adam@pkgsrc.org>
2011-05-04 20:31:39 +0000

    Changes 1.2.3: * Translation updates * Various bug fixes

--- emulators/wine/buildlink3.mk
+++ emulators/wine/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.17 2010/12/23 11:44:30 dsainty Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2011/05/04 20:31:39 adam Exp $
 
 BUILDLINK_TREE+=	wine
 
 .if !defined(WINE_BUILDLINK3_MK)
 WINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.wine+=	wine>=0.9.6nb1
-BUILDLINK_ABI_DEPENDS.wine?=	wine>=1.0.1nb4
+BUILDLINK_API_DEPENDS.wine+=	wine>=1.2.0
+BUILDLINK_ABI_DEPENDS.wine+=	wine>=1.2.0
 BUILDLINK_PKGSRCDIR.wine?=	../../emulators/wine
 
 pkgbase:= wine
@@ -17,8 +17,8 @@ pkgbase:= wine
 .include "../../graphics/glu/buildlink3.mk"
 .include "../../graphics/glut/buildlink3.mk"
 .endif
+.include "../../mk/giflib.buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
-.include "../../graphics/libungif/buildlink3.mk"
 .endif # WINE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-wine
