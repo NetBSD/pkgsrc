36a82c9a6b065b9430cda19c198d150ba3ed4388
obache <obache@pkgsrc.org>
2011-05-05 08:52:43 +0000

    Update libmodplug to 0.8.8.2. While here, set LICENSE=public-domain.
    * 2 April 2011: libmodplug 0.8.8.2
     * Improve compatibility with MSVC 2010
     * Improve PTM playback (Fix byteswapping)
     * Improve S3M support (ignore corrupted data)
     * Improve AMF support (bounds checking)
     * See git checkins for more information
    * 27 May 2010: libmodplug 0.8.8.1
     * Add check to make sure Stereo Separation/MixChannels cannot be 0.
     * Increment soname due to structure change (addition of new config options)
    * 5 April 2010: libmodplug 0.8.8
     * Fix 32 to 32 bit "downsampling" on 64bit platforms.
     * Fix usage of bigendian in include files
     * Fix PSM files on bigendian computers
     * Allow configuration of Stereo Separation and MaxMixChannels
     * Fix possible problem with misaligned data structure when converting to int32
      (load_669)
     (Developer only) update to new automake/autoconf.

--- audio/libmodplug/buildlink3.mk
+++ audio/libmodplug/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:23:55 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2011/05/05 08:52:43 obache Exp $
 
 BUILDLINK_TREE+=	libmodplug
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libmodplug
 LIBMODPLUG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libmodplug+=		libmodplug>=0.7
-BUILDLINK_ABI_DEPENDS.libmodplug+=	libmodplug>=0.7nb1
+BUILDLINK_ABI_DEPENDS.libmodplug+=	libmodplug>=0.8.8.1
 BUILDLINK_PKGSRCDIR.libmodplug?=	../../audio/libmodplug
 .endif # LIBMODPLUG_BUILDLINK3_MK
 
