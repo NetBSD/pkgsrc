72714c183e824b875d1970554cbeda2dca9cdc6c
agc <agc@pkgsrc.org>
2011-05-05 06:53:27 +0000

    Initial import of Jared Mcneill's script to create a memory stick from a NetBSD installation ISO.
    Further information can be found here:
    
    	http://genericzero.wordpress.com/2009/08/01/install-netbsd-from-a-usb-memory-stick-the-easy-way/
    
    	memory stick the easy way
    
    	I got tired of having to jump through hoops to install NetBSD on my
    	Eee PC, so I wrote a simple script to take a NetBSD release ISO and
    	convert it to an image that can be written to a USB memory stick.
    
    	To use the script, simply feed it an ISO and tell it where to write
    	the resulting image:
    
    	$ sh mkmemstick.sh i386cd-5.0.1.iso i386memstick-5.0.1.img
    
    	The resulting image can be written to a memory stick using dd(1):
    
    	$ dd if=i386memstick-5.0.1.img of=/dev/sd0d
    
    	Please note that this script depends on the sysutils/cdrtools package
    	for extracting the contents of the release ISO.
    
    	Hopefully this will be integrated with the build process so these
    	images are available for those who cannot prepare an image due to lack
    	of access to a NetBSD machine.

---
+++ sysutils/mkmemstick/files/mkmemstick.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+#
+# mkmemstick.sh: Create memory stick image from NetBSD installation CDs
+#
+# Copyright (C) 2009 Jared D. McNeill <jmcneill@NetBSD.org>
+
+src="$1"
+img="$2"
+dstbase="/tmp/makeimg$(id -u)"
+dst="${dstbase}/rootfs"
+tmpimg="${dstbase}/netbsd.img"
+disklabel="
+type: unknown
+disk: Memory Stick
+label: 
+flags:
+bytes/sector: 512
+sectors/track: 63
+tracks/cylinder: 16
+sectors/cylinder: 1000
+cylinders: @TCYLINDERS@
+total sectors: @TSECTORS@
+rpm: 3600
+interleave: 1
+trackskew: 0
+cylinderskew: 0
+headswitch: 0
+track-to-track seek: 0
+drivedata: 0 
+
+16 partitions:
+ a: @SECTORS@ 0 4.2BSD 1024 8192 0
+ d: @SECTORS@ 0 unused 0 0
+"
+
+die() {
+	echo "fatal: $0"
+	exit 2
+}
+
+if [ -z "$img" ]; then
+	echo "usage: $0 source.iso memdisk.img"
+	exit 1
+fi
+
+if [ ! -r "$src" ]; then
+	echo "$src not found"
+	exit 1
+fi
+
+if [ -d "$dst" ]; then
+	echo "$dst already exists"
+	exit 1
+fi
+if [ -f "$img" ]; then
+	echo "$img already exists"
+	exit 1
+fi
+
+mkdir -p "$dst" || die "couldn't create directory $dst"
+
+printf " => extracting iso image"
+isoinfo -R -f -i "$src" | while read x; do
+	dstdir="$dst/$(dirname $x)"
+	dstfile="$(basename $x)"
+	if [ -f "$dstdir" ]; then
+		rm "$dstdir"
+	fi
+	mkdir -p "$dstdir"
+	printf .
+	isoinfo -R -x "$x" -i "$src" > "${dstdir}/${dstfile}"
+done
+printf " done.\n"
+
+if [ -f "${dst}/boot.cfg" ]; then
+	echo " => patching bootloader banner"
+	cp "${dst}/boot.cfg" "${dstbase}/boot.cfg" || die "couldn't copy boot.cfg to ${dstbase}"
+	cat "${dstbase}/boot.cfg" | sed 's/installation CD/installation memory stick/' > "${dst}/boot.cfg"
+fi
+echo " => copying /usr/mdec/boot to $dst"
+cp /usr/mdec/boot "$dst" || die "couldn't copy /usr/mdec/boot"
+
+echo " => building filesystem image"
+makefs "$img" "$dst" || die "couldn't build filesystem image"
+
+echo " => installing bootxx_ffsv1"
+/usr/sbin/installboot "$img" /usr/mdec/bootxx_ffsv1 || die "couldn't install bootxx_ffsv1"
+
+echo " => writing disklabel"
+imgsize=$(stat -L -f %z "${img}")
+echo "$disklabel" | \
+	sed "s/@SECTORS@/$(expr $imgsize / 512)/" | \
+	sed "s/@TCYLINDERS@/$(expr $imgsize / 512 / 1000 + 1)/" | \
+	sed "s/@TSECTORS@/$(expr $imgsize / 512 + 1)/" >${dstbase}/disklabel
+disklabel -R -F "${img}" "${dstbase}/disklabel" || die "couldn't write disklabel"
+
+echo " => cleaning up"
+rm -rf "${dstbase}"
+
+echo " => done!"
