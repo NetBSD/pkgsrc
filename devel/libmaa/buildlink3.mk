5ea249f720dc8bc139368d080cb438af5c18a97e
cheusov <cheusov@pkgsrc.org>
2011-05-07 10:06:02 +0000

    Closes PR pkg/44850, oked by wiz@ and reed@ devel/libmaa updated to 1.3.0 recursive bump pkgrevisions of dependent packages due to ABI change

--- devel/libmaa/buildlink3.mk
+++ devel/libmaa/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/11/19 01:19:23 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/05/07 10:06:02 cheusov Exp $
 
 BUILDLINK_TREE+=	libmaa
 
 .if !defined(LIBMAA_BUILDLINK3_MK)
 LIBMAA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libmaa+=	libmaa>=1.2.0
+BUILDLINK_API_DEPENDS.libmaa+=	libmaa>=1.3.0
 BUILDLINK_PKGSRCDIR.libmaa?=	../../devel/libmaa
 .endif # LIBMAA_BUILDLINK3_MK
 
