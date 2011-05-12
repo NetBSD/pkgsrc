6e907bf3210cb10db7efb44cc85475051d2f3ab9
adam <adam@pkgsrc.org>
2011-05-12 11:12:48 +0000

    Changes 1.10.0: * Documentation: Context::arc(): Correct the documentation, as in the C   documentation.
    Changes 1.9.8:
    * Surface:
      - added create() for creating a sub-surface representing a rectangle on a
        target surface
      - PDF versioning API: get_versions(), restrict_to_version(),
        version_to_string()
      - add get_device()
      - add new surface type enum values
    * Device
      - add acquire() and release() functions
      - Add Device::Lock convenience class
    * Region
      - UNSTABLE API CHANGE: Fix do_union(), subtract(), intersect() to throw
        exceptions instead of returning error statuses to be consistent with the
        rest of the cairomm API
      - add xor() functions
      - add API for creating a region from a list of rectangles
      - implement copy()

--- graphics/cairomm/buildlink3.mk
+++ graphics/cairomm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2011/01/13 13:36:23 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2011/05/12 11:12:48 adam Exp $
 
 BUILDLINK_TREE+=	cairomm
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	cairomm
 CAIROMM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cairomm+=	cairomm>=1.2.2
-BUILDLINK_ABI_DEPENDS.cairomm?=	cairomm>=1.8.6nb1
+BUILDLINK_ABI_DEPENDS.cairomm+=	cairomm>=1.8.6nb1
 BUILDLINK_PKGSRCDIR.cairomm?=	../../graphics/cairomm
 
 .include "../../graphics/cairo/buildlink3.mk"
