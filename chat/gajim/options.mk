da5f183324f152272a553063413ec75009cd65e2
obache <obache@pkgsrc.org>
2011-05-14 14:28:07 +0000

    * convert almost python related dependency from buildlink to simple DEPENDS. * require python builtin sqlite3 module, PR#44968. * require python>=25. * drop avahi option from suggested. it require avahi-python, but python option   of net/avahi is disabled by default.
    Bump PKGREVISION.

--- chat/gajim/options.mk
+++ chat/gajim/options.mk
@@ -1,28 +1,28 @@
-# $NetBSD: options.mk,v 1.1.1.1 2010/12/24 00:02:29 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2011/05/14 14:28:07 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gajim
 PKG_SUPPORTED_OPTIONS=	avahi gnome gnome-keyring ssl
-PKG_SUGGESTED_OPTIONS=	avahi ssl
+PKG_SUGGESTED_OPTIONS=	ssl
 
 .include "../../mk/bsd.options.mk"
 
 # ssl
 .if !empty(PKG_OPTIONS:Mssl)
-DEPENDS+=	${PYPKGPREFIX}-OpenSSL-[0-9]*:../../security/py-OpenSSL
-.include "../../security/py-amkCrypto/buildlink3.mk"
+DEPENDS+=	${PYPKGPREFIX}-OpenSSL>=0.9:../../security/py-OpenSSL
+DEPENDS+=	${PYPKGPREFIX}-amkCrypto-[0-9]*:../../security/py-amkCrypto
 .endif
 
 # avahi
 .if !empty(PKG_OPTIONS:Mavahi)
-.include "../../net/avahi/buildlink3.mk"
+DEPENDS+=	avahi-[0-9]*:../../net/avahi
 .endif
 
 # gnome
 .if !empty(PKG_OPTIONS:Mgnome)
-.include "../../x11/py-gnome2/buildlink3.mk"
+DEPENDS+=	${PYPKGPREFIX}-gnome2-[0-9]*:../../x11/py-gnome2
 .endif
 
-# avahi
+# gnome-keyring
 .if !empty(PKG_OPTIONS:Mgnome-keyring)
-.include "../../x11/py-gnome2-desktop/buildlink3.mk"
+DEPENDS+=	${PYPKGPREFIX}-gnome2-desktop-[0-9]*:../../x11/py-gnome2-desktop
 .endif
