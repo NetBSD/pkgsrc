6426eff871985fa932bc828b926441a0e1f4cb5b
obache <obache@pkgsrc.org>
2011-05-18 14:26:26 +0000

    Moudulalize GraphicsMagick, for let PerlMagick to lightweight. Bump PKGREVISION.
    It introduce bump PKGREVISION of p5-GraphicsMagick.
    While here,
     * move distinfo and patches location to Makefile.common.
     * let to exactly linked against installed GraphicsMagick.

--- graphics/GraphicsMagick/buildlink3.mk
+++ graphics/GraphicsMagick/buildlink3.mk
@@ -1,22 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.18 2011/05/17 11:34:04 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2011/05/18 14:26:26 obache Exp $
 
 BUILDLINK_TREE+=	GraphicsMagick
 
 .if !defined(GRAPHICSMAGICK_BUILDLINK3_MK)
 GRAPHICSMAGICK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.GraphicsMagick+=	GraphicsMagick>=1.2
-BUILDLINK_ABI_DEPENDS.GraphicsMagick?=	GraphicsMagick>=1.3.12nb4
+BUILDLINK_API_DEPENDS.GraphicsMagick+=	GraphicsMagick>=1.3.12nb6
 BUILDLINK_PKGSRCDIR.GraphicsMagick?=	../../graphics/GraphicsMagick
-
-.include "../../mk/bsd.fast.prefs.mk"
-
-_GM_PRE_X11_OPTION!= \
-	if ${PKG_INFO} -qe 'GraphicsMagick<=1.3.12nb4'; then		\
-		${ECHO} yes;						\
-	else								\
-		${ECHO} no;						\
-	fi
 
 pkgbase := GraphicsMagick
 .include "../../mk/pkg-build-options.mk"
@@ -24,13 +14,7 @@ pkgbase := GraphicsMagick
 .if !empty(PKG_BUILD_OPTIONS.GraphicsMagick:Mghostscript)
 .include "../../print/ghostscript/buildlink3.mk"
 .endif
-.if !empty(PKG_BUILD_OPTIONS.GraphicsMagick:Mjasper)
-.include "../../graphics/jasper/buildlink3.mk"
-.endif
-.if !empty(PKG_BUILD_OPTIONS.GraphicsMagick:Mwmf)
-.include "../../graphics/libwmf/buildlink3.mk"
-.endif
-.if ${_GM_PRE_X11_OPTION} == "yes" || !empty(PKG_BUILD_OPTIONS.GraphicsMagick:Mx11)
+.if !empty(PKG_BUILD_OPTIONS.GraphicsMagick:Mx11)
 .include "../../x11/libSM/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/libXext/buildlink3.mk"
@@ -41,15 +25,9 @@ pkgbase := GraphicsMagick
 .endif
 
 .include "../../devel/zlib/buildlink3.mk"
+.include "../../devel/libltdl/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
-.include "../../graphics/jbigkit/buildlink3.mk"
-.include "../../mk/jpeg.buildlink3.mk"
 .include "../../graphics/lcms/buildlink3.mk"
-.include "../../graphics/libexif/buildlink3.mk"
-.include "../../graphics/png/buildlink3.mk"
-.include "../../graphics/tiff/buildlink3.mk"
-.include "../../textproc/libxml2/buildlink3.mk"
-.include "../../mk/dlopen.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 .endif # GRAPHICSMAGICK_BUILDLINK3_MK
 
