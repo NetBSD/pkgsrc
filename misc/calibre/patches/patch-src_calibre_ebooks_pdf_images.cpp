6938709083dcaa74aab7451ed3b3d43fd2624005
wiz <wiz@pkgsrc.org>
2011-05-21 15:07:48 +0000

    Update to 0.8.2 provided by Rhialto in PR 44868. Changes since 0.6.x: many too many to list here. Now comes with its own store.

--- misc/calibre/patches/patch-src_calibre_ebooks_pdf_images.cpp
+++ misc/calibre/patches/patch-src_calibre_ebooks_pdf_images.cpp
@@ -1,23 +1,12 @@
-$NetBSD: patch-src_calibre_ebooks_pdf_images.cpp,v 1.1 2011/04/12 21:48:01 wiz Exp $
+$NetBSD: patch-src_calibre_ebooks_pdf_images.cpp,v 1.2 2011/05/21 15:07:48 wiz Exp $
 
-Fix build with png-1.5.
-
---- src/calibre/ebooks/pdf/images.cpp.orig	2009-11-16 19:41:31.000000000 +0000
+--- src/calibre/ebooks/pdf/images.cpp.orig	2011-04-08 17:20:30.000000000 +0000
 +++ src/calibre/ebooks/pdf/images.cpp
-@@ -6,6 +6,7 @@
- #include <math.h>
- #include <iostream>
- #include <wand/MagickWand.h>
-+#include <zlib.h>
+@@ -16,6 +16,7 @@
  
  #include "images.h"
  #include "utils.h"
-@@ -294,7 +295,7 @@ void PNGWriter::write_splash_bitmap(Spla
++#include "zlib.h"
  
- void calibre_png_mem_write(png_structp png_ptr, png_bytep data, png_size_t length) {
-     if (!png_ptr || length < 1) return;
--    vector<char> *buf = static_cast< vector<char>* >(png_ptr->io_ptr);
-+    vector<char> *buf = static_cast< vector<char>* >(png_get_io_ptr(png_ptr));
-     buf->reserve(buf->capacity() + length); 
-     do {
-         buf->push_back(static_cast<char>(*data));
+ #ifdef _WIN32
+ inline double round(double x) { return (x-floor(x))>0.5 ? ceil(x) : floor(x); }
