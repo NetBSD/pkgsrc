59085e6e1666c208b827089ea9189b519251096d
dmcmahill <dmcmahill@pkgsrc.org>
2011-05-25 01:11:29 +0000

    Import idzebra-2.0.47
    Zebra is a high-performance, general-purpose structured text indexing and
    retrieval engine. It reads structured records in a variety of input formats
    (eg. email, XML, MARC) and allows access to them through exact boolean
    search expressions and relevance-ranked free-text queries.
    
    Zebra supports large databases (more than ten gigabytes of data, tens of
    millions of records). It supports incremental, safe database updates on
    live systems. You can access data stored in Zebra using a variety of Index
    Data tools (eg. YAZ and PHP/YAZ) as well as commercial and freeware Z39.50
    clients and toolkits.

---
+++ databases/idzebra/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/05/25 01:11:29 dmcmahill Exp $
+
+BUILDLINK_TREE+=	idzebra
+
+.if !defined(IDZEBRA_BUILDLINK3_MK)
+IDZEBRA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.idzebra+=	idzebra>=2.0.47
+BUILDLINK_PKGSRCDIR.idzebra?=	../../databases/idzebra
+
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../lang/tcl/buildlink3.mk"
+.include "../../net/yaz/buildlink3.mk"
+.include "../../textproc/expat/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../textproc/libxslt/buildlink3.mk"
+.endif	# IDZEBRA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-idzebra
