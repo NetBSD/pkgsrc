12dd2df6774e7f8531e8b4220ed4d2d388d267d2
wiz <wiz@pkgsrc.org>
2011-05-29 11:52:12 +0000

    Update to 1.48. Ok reed except for the cmake dependency :)
    Release notes for 1.48
    
    There are no major new features in 1.48. There are more and better
    checks.
    
    New check: Wrong usage of ! operator in conditions.  Example: if
    (!x == 0) {
    
    New check: Use "throw" without arguments to rethrow exceptions.
    
    New check: Comparison of substring with string literal will
    always/never match because size doesn't match.
    
    New check: Postfix increment of boolean
    
    New check: Clarify condition with parantheses (when there are
    assignment + comparison) Example: if (a = b > 0) {
    
    
    Release notes for 1.47
    
    It is now possible to exclude files and folders from the analysis.
    Use -i on the command line (i=ignore).
    
    Custom rules can now be created using regular expressions. To read
    more about creating custom rules, see
    http://sourceforge.net/projects/cppcheck/files/Articles/
    
    A new XML format is launched. To use this format, the --xml-version=2
    is used. The new xml format is incompatible with the xml format
    used in previous versions. The old XML format will still be used
    unless --xml-version=2 is given, so your existing scripts, tools
    and plugins should still work.
    
    New check: sizeof used on array variable that is a function parameter.
    The expected bahaviour is most likely that the sizeof result is
    the size of the array, but the actual sizeof result is the size of
    the pointer.
    
    New check: catching exception by value. It is better to catch by
    reference for various reasons.
    
    New check: memset calls filling 0 bytes. The given size might be
    wrong.

--- devel/cppcheck/options.mk
+++ devel/cppcheck/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2010/02/03 09:52:32 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2011/05/29 11:52:12 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.cppcheck
 PKG_SUPPORTED_OPTIONS=	qt
@@ -6,43 +6,23 @@ PKG_SUPPORTED_OPTIONS=	qt
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mqt)
-SUBST_CLASSES+=		lrelease
-SUBST_FILES.lrelease=	gui/gui.pro
-SUBST_STAGE.lrelease=	post-extract
-SUBST_SED.lrelease=	-e "s,lrelease gui.pro,${PREFIX}/qt4/bin/lrelease gui.pro,g"
-SUBST_MESSAGE.lrelease=	Fixing path to lrelease.
-
 INSTALLATION_DIRS+=	lib share/cppcheck
 
 PLIST_SRC+=		PLIST PLIST.qt
 
-do-configure:
-	cd ${WRKSRC} && ${PREFIX}/qt4/bin/qmake
-
-.PHONY: link-build
-post-build: link-build
-link-build:
-	${ECHO} '#!/bin/sh' > ${WRKSRC}/cppcheck-gui
-	${ECHO} 'cd ${PREFIX}/share/cppcheck && ${PREFIX}/lib/cppcheck-gui "$$@"' >> ${WRKSRC}/cppcheck-gui
-
-.PHONY: qt-install
-post-install: qt-install
-qt-install:
+do-install:
 	${INSTALL_SCRIPT} ${WRKSRC}/cli/cppcheck ${DESTDIR}${PREFIX}/bin
-	${INSTALL_SCRIPT} ${WRKSRC}/cppcheck-gui ${DESTDIR}${PREFIX}/bin
-	${INSTALL_PROGRAM} ${WRKSRC}/gui/gui \
-		${DESTDIR}${PREFIX}/lib/cppcheck-gui
+	${INSTALL_PROGRAM} ${WRKSRC}/gui/cppcheck-gui \
+		${DESTDIR}${PREFIX}/bin
 	${INSTALL_DATA} ${WRKSRC}/man/cppcheck.1 \
 		${DESTDIR}${PREFIX}/${PKGMANDIR}/man1
-	${INSTALL_DATA} ${WRKSRC}/gui/*.qm ${DESTDIR}${PREFIX}/share/cppcheck
 
 .include "../../x11/qt4-libs/buildlink3.mk"
+BUILDLINK_DEPMETHOD.qt4-tools?= full
 .include "../../x11/qt4-tools/buildlink3.mk"
 .else
-.PHONY: main-install
-post-install: main-install
-main-install:
-	${INSTALL_PROGRAM} ${WRKSRC}/cppcheck ${DESTDIR}${PREFIX}/bin/
+do-install:
+	${INSTALL_PROGRAM} ${WRKSRC}/cli/cppcheck ${DESTDIR}${PREFIX}/bin/
 	${INSTALL_DATA} ${WRKSRC}/man/cppcheck.1 \
 		${DESTDIR}${PREFIX}/${PKGMANDIR}/man1
 .endif
