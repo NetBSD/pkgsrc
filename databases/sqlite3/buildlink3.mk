7d08f40f3e486724ad201a7b8c866839a6768249
adam <adam@pkgsrc.org>
2011-06-01 11:07:24 +0000

    buildlink icu when used as an option

--- databases/sqlite3/buildlink3.mk
+++ databases/sqlite3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:24:08 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2011/06/01 11:07:24 adam Exp $
 
 BUILDLINK_TREE+=	sqlite3
 
@@ -8,6 +8,14 @@ SQLITE3_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.sqlite3+=	sqlite3>=3.0.8
 BUILDLINK_ABI_DEPENDS.sqlite3+=	sqlite3>=3.2.7nb1
 BUILDLINK_PKGSRCDIR.sqlite3?=	../../databases/sqlite3
+
+pkgbase:= sqlite3
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.sqlite3:Micu)
+.include "../../textproc/icu/buildlink3.mk"
+.endif
+
 .endif # SQLITE3_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-sqlite3
