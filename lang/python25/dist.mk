f315a40f34582bca9089dcaa194af8c5e0e9dd0f
obache <obache>
2011-06-04 05:08:19 +0000

    Update python25 to 2.5.6. (contains already fixed issues, #11662 patch-b{a,b}, #8674,#7673 patch-bc)
    What's New in Python 2.5.6?
    ===========================
    
    *Release date: 26-May-2011*
    
    
    What's New in Python 2.5.6c1?
    =============================
    
    *Release date: 17-Apr-2011*
    
    Library
    -------
    
    - Issue #11442: Add a charset parameter to the Content-type in SimpleHTTPServer
      to avoid XSS attacks.
    
    - Issue #11662: Make urllib and urllib2 ignore redirections if the
      scheme is not HTTP, HTTPS or FTP (CVE-2011-1521).
    
    - Issue #8674: Fixed a number of incorrect or undefined-behaviour-inducing
      overflow checks in the audioop module (CVE-2010-1634).
    
    - Issue #7673: Fix security vulnerability (CVE-2010-2089) in the audioop
      module, ensure that the input string length is a multiple of the frame size.

--- lang/python25/dist.mk
+++ lang/python25/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.1 2010/09/17 07:11:41 obache Exp $
+# $NetBSD: dist.mk,v 1.2 2011/06/04 05:08:19 obache Exp $
 
-PY_DISTVERSION=	2.5.5
+PY_DISTVERSION=	2.5.6
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.bz2
 DISTINFO_FILE=	${.CURDIR}/../../lang/python25/distinfo
