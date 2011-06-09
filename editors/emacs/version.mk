339a031bbd92f6f559a188be2aaadbae770c7c1e
drochner <drochner@pkgsrc.org>
2011-06-09 13:47:31 +0000

    update to 23.3 Emacs 23.3 is primarily a bugfix release.  Also included are a number of enhancements to VC and Rmail, and a new indentation library, smie.el. For details, see the file etc/NEWS.

--- editors/emacs/version.mk
+++ editors/emacs/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.6 2011/02/08 13:20:13 hiramatsu Exp $
+# $NetBSD: version.mk,v 1.7 2011/06/09 13:47:31 drochner Exp $
 
 _EMACS_FLAVOR=	emacs
 _EMACS_REQD=	emacs>=23.2<24
 
 _EMACS_VERSION_MAJOR=	23
-_EMACS_VERSION_MINOR=	2
+_EMACS_VERSION_MINOR=	3
