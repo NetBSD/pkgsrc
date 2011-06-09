9bd7c0d58fa8bfc623af4160b36aed96e5bca9bf
schmonz <schmonz@pkgsrc.org>
2011-06-09 00:55:40 +0000

    Update ipv6 patch. Closes PR pkg/44469 by James A. T. Rice. While here, fix indentation broken in previous and quell pkglint.

--- net/djbdns/options.mk
+++ net/djbdns/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.14 2009/05/03 11:28:41 wiz Exp $
+# $NetBSD: options.mk,v 1.15 2011/06/09 00:55:40 schmonz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.djbdns
 PKG_SUPPORTED_OPTIONS+=		inet6 djbdns-cachestats djbdns-ignoreip2
@@ -17,7 +17,7 @@ PKG_SUGGESTED_OPTIONS+=   djbdns-tinydns64
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Minet6)
-IPV6_PATCH=			djbdns-1.05-ipv6.diff.bz2
+IPV6_PATCH=			djbdns-1.05-test23.diff.bz2
 PATCHFILES+=			${IPV6_PATCH}
 SITES.${IPV6_PATCH}=		http://www.fefe.de/dns/
 PATCH_DIST_STRIP.${IPV6_PATCH}=	-p1
