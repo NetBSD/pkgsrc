b7cd8c423823b8536d8d728853f526604c59d50b
schmonz <schmonz@pkgsrc.org>
2011-06-09 01:44:16 +0000

    Instead of a separate PLIST file for inet6, use PLIST_VARS.

--- net/djbdns/options.mk
+++ net/djbdns/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.15 2011/06/09 00:55:40 schmonz Exp $
+# $NetBSD: options.mk,v 1.16 2011/06/09 01:44:16 schmonz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.djbdns
 PKG_SUPPORTED_OPTIONS+=		inet6 djbdns-cachestats djbdns-ignoreip2
@@ -16,12 +16,13 @@ PKG_SUGGESTED_OPTIONS+=   djbdns-tinydns64
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=			inet6
 .if !empty(PKG_OPTIONS:Minet6)
 IPV6_PATCH=			djbdns-1.05-test23.diff.bz2
 PATCHFILES+=			${IPV6_PATCH}
 SITES.${IPV6_PATCH}=		http://www.fefe.de/dns/
 PATCH_DIST_STRIP.${IPV6_PATCH}=	-p1
-PLIST_SRC+=			${PKGDIR}/PLIST.inet6
+PLIST.inet6=			yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mdjbdns-cachestats)
