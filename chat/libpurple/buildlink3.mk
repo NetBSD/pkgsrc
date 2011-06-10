f38363508fd777a3fa11b0752a0dcf632a47c13b
obache <obache@pkgsrc.org>
2011-06-10 09:39:41 +0000

    recursive bump from textproc/icu shlib major bump.

--- chat/libpurple/buildlink3.mk
+++ chat/libpurple/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2011/04/22 13:42:39 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2011/06/10 09:39:47 obache Exp $
 
 BUILDLINK_TREE+=	libpurple
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libpurple
 LIBPURPLE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libpurple+=	libpurple>=2.7.9
-BUILDLINK_ABI_DEPENDS.libpurple+=	libpurple>=2.7.11nb1
+BUILDLINK_ABI_DEPENDS.libpurple+=	libpurple>=2.7.11nb2
 BUILDLINK_PKGSRCDIR.libpurple?=	../../chat/libpurple
 
 pkgbase := libpurple
