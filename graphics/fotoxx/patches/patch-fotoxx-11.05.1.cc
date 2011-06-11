e4118c7df3ca2ba66f8dced6890b744b1c7c3831
ryoon <ryoon@pkgsrc.org>
2011-06-11 18:54:23 +0000

    Update to 11.06.1
    Changelog:
    2011.06.09  v.11.06.1
    Bugfix: A hole was closed that could cause a crash if an area is
    deleted while an edit function is open and active.
    
    2011.06.01  v.11.06
    + Smart Erase and Remove Dust were made slightly more effective.
    + Brightness/Color retouching and Tone Mapping were made 25-40% faster.
    + Retouching brightness/color by "painting" with the mouse (dodge and
      burn) was made much faster (instant response on a fast computer).
    + If at startup the previous image file is no longer present then a
      gallery of the 100 most recent files viewed is shown.
    + New function: Tools > Toolbar Style: set to text, icons, or both.
    + New function: Tools > Edit Translation: edit translations interactively
      as Fotoxx is being used, and the changes are immediately effective.
    + Several other small usability improvements were made.
    + Minor bugfix: paste area edge blending lost 1 pixel around the edge.

--- graphics/fotoxx/patches/patch-fotoxx-11.05.1.cc
+++ graphics/fotoxx/patches/patch-fotoxx-11.05.1.cc
@@ -1,8 +1,8 @@
-$NetBSD: patch-fotoxx-11.05.1.cc,v 1.1 2011/05/11 01:27:03 ryoon Exp $
+$NetBSD: patch-fotoxx-11.05.1.cc,v 1.2 2011/06/11 18:54:23 ryoon Exp $
 
---- fotoxx-11.05.1.cc.orig	2011-05-01 18:44:26.000000000 +0000
-+++ fotoxx-11.05.1.cc
-@@ -484,7 +484,7 @@ int gtkinitfunc(void *data)
+--- fotoxx-11.06.1.cc.orig	2011-06-09 06:29:46.000000000 +0000
++++ fotoxx-11.06.1.cc
+@@ -427,7 +427,7 @@ int gtkinitfunc(void *data)
  
  //  set up current file and directory from command line input or last session parameters        v.11.04
  
@@ -11,7 +11,7 @@ $NetBSD: patch-fotoxx-11.05.1.cc,v 1.1 2011/05/11 01:27:03 ryoon Exp $
     if (ppv) {
        curr_dirk = strdupz(ppv,0,"curr_dirk");
        free(ppv);
-@@ -493,6 +493,15 @@ int gtkinitfunc(void *data)
+@@ -436,6 +436,15 @@ int gtkinitfunc(void *data)
     else curr_dirk = 0;
     
     if (last_file) {                                                        //  from command line or parameters
@@ -27,7 +27,7 @@ $NetBSD: patch-fotoxx-11.05.1.cc,v 1.1 2011/05/11 01:27:03 ryoon Exp $
        ppv = realpath(last_file,0);                                         //  prepend directory if needed
        if (ppv) {
           curr_file = strdupz(ppv,0,"curr_file");
-@@ -503,6 +512,7 @@ int gtkinitfunc(void *data)
+@@ -446,6 +455,7 @@ int gtkinitfunc(void *data)
           zfree(last_file);
           last_file = 0;
        }
@@ -35,7 +35,7 @@ $NetBSD: patch-fotoxx-11.05.1.cc,v 1.1 2011/05/11 01:27:03 ryoon Exp $
     }
  
     if (curr_file) {
-@@ -2007,7 +2017,7 @@ void m_gallery(GtkWidget *, cchar *)
+@@ -1953,7 +1963,7 @@ void m_gallery(GtkWidget *, cchar *)
     if (curr_file)
        image_gallery(0,"paint1",curr_file_posn,m_gallery2,mWin);            //  overlay main window    v.10.9
     else {
