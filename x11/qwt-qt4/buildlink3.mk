aa1dca6f8da8fbf6ae8404781782869e0ec2783c
adam <adam@pkgsrc.org>
2011-06-16 15:21:34 +0000

    Changes 6.0:
    1) Qt3 support dropped
    2) QwtPlot layout/render code ported from int to double
       Exported/printed documents in scalable formats like SVG or PDF
       are 100% scalable now.
    3) Template base classes introduced for curve and curve data
       to be reusable in all plot items displaying series of samples.
    4) New plot items
       - QwtPlotHistogram
       - QwtPlotIntervalCurve ( error bars or displaying the area between 2 curves )
       - QwtPlotSpectroCurve ( mapping the z value to a color )
    5) Raster items
       - QwtMatrixRasterData introduced
       - More accurate rendering
       - Several API changes
       - Thread support for rendering spectrograms
    6) QwtPlot::print moved to QwtPlotRenderer
    7) Other new classes
       QwtColumnSymbol
       QwtDoublePoint3D
       QwtIntervalSymbol
       QwtPlotDirectPainter
       QwtSamplingThread
       QwtSystemClock
    8) QwtPicker and friends reorganized,
       QwtPickerTrackerMachine added for displaying a rubberband for
       mouse moves. Enter/Leave added to events, that are handled
       by the picker machines.
    9) QwtScaleWidget::LayoutFlag added
       Introduced to control the direction of vertical axis titles.
    10)QwtWeedingCurveFitter added
       QwtWeedingCurveFitter is an implementation of the Douglas/Peuker
       algorithm, that can be used to reduce the number of curve points.
       It can be very useful to improve the performance of painting
       curves of many lines ( f.e. by implementing different level of details ).
    11)Legend code update for representing different pixmaps for different
       types of plot items.
    12)Copy operators removed, using pointers instead
    13)QwtPolarPoint from qwtpolar added
    14)QwtThermo
       Optional QwtColorMaps added
    15)Interfaces and code of all sliders/dials cleaned up.
       QApplication::globalStrut(), styled backgrounds ...

--- x11/qwt-qt4/buildlink3.mk
+++ x11/qwt-qt4/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2011/04/22 13:42:36 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2011/06/16 15:21:34 adam Exp $
 
 BUILDLINK_TREE+=	qwt
 
 .if !defined(QWT_BUILDLINK3_MK)
 QWT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qwt+=	qwt>=5.2.0
-BUILDLINK_ABI_DEPENDS.qwt?=	qwt>=5.2.1nb4
+BUILDLINK_API_DEPENDS.qwt+=	qwt>=6.0.0
+BUILDLINK_ABI_DEPENDS.qwt+=	qwt>=6.0.0
 BUILDLINK_PKGSRCDIR.qwt?=	../../x11/qwt-qt4
 
 .include "../../x11/qt4-libs/buildlink3.mk"
