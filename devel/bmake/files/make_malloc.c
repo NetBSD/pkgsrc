b5878f76f8d0734678bd0ca14477e54067f82a8a
bsiegert <bsiegert>
2011-06-18 22:17:55 +0000

    Import bmake-20110606. Many changes, among them:
      - unit-tests/modts now works on MirBSD
      - meta mode
      - ApplyModifiers: when we parse a variable which is not the entire modifier
        string, or not followed by ':', do not consider it as containing modifiers.
      - when long modifiers fail to match, check sysV style.
      - :hash - cheap 32bit hash of value
      - :localtime, :gmtime - use value as format string for strftime.
      - fix for use after free() in CondDoExists().
      - boot-strap (TOOL_DIFF): aparently at least on linux distro
        formats the output of 'type' differently - so eat any "()"
      - correct sysV substitution handling of empty lhs and variable
      - correct exists() check for dir with trailing /
      - correct handling of modifiers for non-existant variables during evaluation
        of conditionals.
      - fix for incorrect .PARSEDIR when .OBJDIR is re-computed after makefiles
        have been read.
      - fix example of :? modifier in man page.
      - sigcompat.c: convert to ansi so we can use higher warning levels.
      - parse.c: SunOS 5.8 at least does not have MAP_FILE
      - use mmap(2) if available, for reading makefiles
      - to ensure unit-tests results match, need to control LC_ALL as well as LANG.
      - if stale dependency is an IMPSRC, search via .PATH
      - machine.sh: like os.sh, allow for uname -p producing useless drivel
      - boot-strap: document configure knobs for meta and filemon.

--- devel/bmake/files/make_malloc.c
+++ devel/bmake/files/make_malloc.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: make_malloc.c,v 1.1.1.1 2009/09/18 20:55:23 joerg Exp $	*/
+/*	$NetBSD: make_malloc.c,v 1.1.1.2 2011/06/18 22:18:03 bsiegert Exp $	*/
 
 /*-
  * Copyright (c) 2009 The NetBSD Foundation, Inc.
@@ -28,7 +28,7 @@
 
 #ifdef MAKE_NATIVE
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: make_malloc.c,v 1.1.1.1 2009/09/18 20:55:23 joerg Exp $");
+__RCSID("$NetBSD: make_malloc.c,v 1.1.1.2 2011/06/18 22:18:03 bsiegert Exp $");
 #endif
 
 #include <stdio.h>
@@ -48,7 +48,7 @@ enomem(void)
 {
 	extern char *progname;
 
-	(void)fprintf(stderr, "%s: %s.\n", progname, strerror(errno));
+	(void)fprintf(stderr, "%s: %s.\n", progname, strerror(ENOMEM));
 	exit(2);
 }
 
