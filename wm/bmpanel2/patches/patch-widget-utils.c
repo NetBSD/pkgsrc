67926f0a1e95e19dbfd9ca2fa52cff03241dcbe4
wiz <wiz@pkgsrc.org>
2011-06-18 15:34:08 +0000

    Add upstream bug report.

--- wm/bmpanel2/patches/patch-widget-utils.c
+++ wm/bmpanel2/patches/patch-widget-utils.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-widget-utils.c,v 1.1.1.1 2011/06/18 15:30:59 wiz Exp $
+$NetBSD: patch-widget-utils.c,v 1.2 2011/06/18 15:34:08 wiz Exp $
 
 alloca.h is deprecated and doesn't exist on many systems.
+http://code.google.com/p/bmpanel2/issues/detail?id=28
 
 --- widget-utils.c.orig	2009-11-01 05:50:26.000000000 +0000
 +++ widget-utils.c
