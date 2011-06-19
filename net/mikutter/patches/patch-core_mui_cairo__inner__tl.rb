65f17e00e9cf679664acb9392366a0f61d3af553
obache <obache@pkgsrc.org>
2011-06-19 04:19:56 +0000

    Update mikutter to 0.0.3.9. While here, let to allow co-existence with ruby-escape package.
    Improvements
    * none
    Fixes
    * impossible to search with Japanese
    * impossible to google search with Japanese
    * fire shortcut key for tweet when typing search string
    * memory leaks
    * devours API with retrying tweet of keyed account that does not authenticat me
    * not play sound on some platforms.

--- net/mikutter/patches/patch-core_mui_cairo__inner__tl.rb
+++ net/mikutter/patches/patch-core_mui_cairo__inner__tl.rb
@@ -1,8 +1,8 @@
-$NetBSD: patch-core_mui_cairo__inner__tl.rb,v 1.1 2011/06/12 12:41:15 obache Exp $
+$NetBSD: patch-core_mui_cairo__inner__tl.rb,v 1.2 2011/06/19 04:19:56 obache Exp $
 
 * use system bseach library instead of included one.
 
---- core/mui/cairo_inner_tl.rb.orig	2011-06-05 13:30:21.000000000 +0000
+--- core/mui/cairo_inner_tl.rb.orig	2011-06-15 17:42:27.000000000 +0000
 +++ core/mui/cairo_inner_tl.rb
 @@ -1,7 +1,7 @@
  # -*- coding: utf-8 -*-
@@ -12,4 +12,4 @@ $NetBSD: patch-core_mui_cairo__inner__tl.rb,v 1.1 2011/06/12 12:41:15 obache Exp
 +miquire :lib, 'bsearch'
  
  class Gtk::TimeLine::InnerTL < Gtk::CRUD
-   attr_accessor :postbox
+   attr_accessor :postbox, :hp
