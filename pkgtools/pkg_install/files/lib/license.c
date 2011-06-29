ca7106c349996bf6e9ebf1d9129789695eb7825a
taca <taca@pkgsrc.org>
2011-06-29 03:16:27 +0000

    Add "php" to default_acceptable_licenses.
    * Why DEFAULT_ACCEPTABLE_LICENSES is hardcoded here?
    * Should I update pkg_install's version?

--- pkgtools/pkg_install/files/lib/license.c
+++ pkgtools/pkg_install/files/lib/license.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: license.c,v 1.20 2011/05/08 10:53:22 ryoon Exp $	*/
+/*	$NetBSD: license.c,v 1.21 2011/06/29 03:16:27 taca Exp $	*/
 
 /*-
  * Copyright (c) 2009 Joerg Sonnenberger <joerg@NetBSD.org>.
@@ -59,7 +59,7 @@ const char *default_acceptable_licenses =
     "cpl-1.0 "
     "open-font-license "
     "mpl-1.0 mpl-1.1 "
-    "png-license "
+    "php png-license "
     "postgresql-license "
     "zpl "
     "python-software-foundation "
