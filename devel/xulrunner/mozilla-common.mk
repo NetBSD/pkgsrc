cefda6552e89cf96af4991089d5ea7743963575a
tnn <tnn@pkgsrc.org>
2011-07-11 21:20:34 +0000

    needs sqlite3 >= 3.7.5 now

--- devel/xulrunner/mozilla-common.mk
+++ devel/xulrunner/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.22 2011/07/11 12:46:14 tnn Exp $
+# $NetBSD: mozilla-common.mk,v 1.23 2011/07/11 21:20:34 tnn Exp $
 #
 # common Makefile fragment for mozilla packages based on gecko 2.0.
 # 
@@ -85,7 +85,7 @@ PREFER.bzip2?=	pkgsrc
 .include "../../audio/alsa-lib/buildlink3.mk"
 .endif
 .include "../../archivers/bzip2/buildlink3.mk"
-BUILDLINK_API_DEPENDS.sqlite3+=	sqlite3>=3.7.4
+BUILDLINK_API_DEPENDS.sqlite3+=	sqlite3>=3.7.5
 CONFIGURE_ENV+=	ac_cv_sqlite_secure_delete=yes	# c.f. patches/patch-al
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../devel/libevent/buildlink3.mk"
