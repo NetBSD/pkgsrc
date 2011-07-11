32ad9e1689f74b060d58900f68a0bb8d86c3d889
tnn <tnn@pkgsrc.org>
2011-07-11 13:17:40 +0000

    Fix PLIST when official branding is disabled.

--- www/firefox/options.mk
+++ www/firefox/options.mk
@@ -1,14 +1,17 @@
-# $NetBSD: options.mk,v 1.9 2010/03/16 15:57:03 tnn Exp $
+# $NetBSD: options.mk,v 1.10 2011/07/11 13:17:40 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.firefox
 PKG_SUPPORTED_OPTIONS=	official-mozilla-branding
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=		nobranding
 .if !empty(PKG_OPTIONS:Mofficial-mozilla-branding)
 CONFIGURE_ARGS+=	--enable-official-branding
 LICENSE=		mozilla-trademark-license
 RESTRICTED=		Trademark holder prohibits distribution of modified versions.
 NO_BIN_ON_CDROM=	${RESTRICTED}
 NO_BIN_ON_FTP=		${RESTRICTED}
+.else
+PLIST.nobranding=	yes
 .endif
