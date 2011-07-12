c604ff7de94ea506d2199821a646de88958ea939
hans <hans@pkgsrc.org>
2011-07-12 18:05:58 +0000

    Use the options framework to select which language frontends to build besides C. Just as before, the default is to build c++, java, fortran and objc. Option java implies option c++.

--- lang/gcc44/options.mk
+++ lang/gcc44/options.mk
@@ -1,11 +1,16 @@
-# $NetBSD: options.mk,v 1.3 2011/04/14 19:38:13 hans Exp $
+# $NetBSD: options.mk,v 1.4 2011/07/12 18:05:58 hans Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gcc44
-PKG_SUPPORTED_OPTIONS=	nls
-PKG_SUGGESTED_OPTIONS=
+PKG_SUPPORTED_OPTIONS=	nls c++ fortran java objc #ada
+PKG_SUGGESTED_OPTIONS=	c++ fortran java objc
 .if ${OPSYS} == "NetBSD"
 PKG_SUGGESTED_OPTIONS+=	nls
 .endif
+
+PKG_OPTIONS_LEGACY_VARS+=	BUILD_CXX:c++
+PKG_OPTIONS_LEGACY_VARS+=	BUILD_FORTRAN:fortran
+PKG_OPTIONS_LEGACY_VARS+=	BUILD_JAVA:java
+PKG_OPTIONS_LEGACY_VARS+=	BUILD_OBJC:objc
 
 .include "../../mk/bsd.options.mk"
 
@@ -20,3 +25,35 @@ USE_TOOLS+=		msgfmt
 .else
 CONFIGURE_ARGS+=	--disable-nls
 .endif
+
+###
+### Optional languages
+### Ada could be added although there is a bootstrapping issue.  See
+### ../gcc34-ada for guidance
+###
+
+LANGS=		c
+
+.if !empty(PKG_OPTIONS:Mjava)
+.  if empty(PKG_OPTIONS:Mc++)
+PKG_OPTIONS+=	c++
+.  endif
+LANGS+=		java
+REPLACE_PYTHON=	libjava/contrib/aot-compile.in
+.endif
+
+.if !empty(PKG_OPTIONS:Mc++)
+LANGS+=		c++
+.endif
+
+.if !empty(PKG_OPTIONS:Mfortran)
+LANGS+=		fortran
+.endif
+
+.if !empty(PKG_OPTIONS:Mobjc)
+LANGS+=		objc
+.endif
+
+#.if !empty(PKG_OPTIONS:Mada)
+#LANGS+=	ada
+#.endif
