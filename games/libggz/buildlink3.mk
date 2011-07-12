d037f28cbd6545c1999fc4b113172b69a9ee2041
abs <abs@pkgsrc.org>
2011-07-12 08:26:18 +0000

    fix typo in last

--- games/libggz/buildlink3.mk
+++ games/libggz/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2011/07/12 08:25:51 abs Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2011/07/12 08:26:18 abs Exp $
 
 BUILDLINK_TREE+=	libggz
 
@@ -10,7 +10,7 @@ BUILDLINK_ABI_DEPENDS.libggz?=	libggz>=0.0.14.1nb1
 BUILDLINK_PKGSRCDIR.libggz?=	../../games/libggz
 
 .include "../../security/gnutls/buildlink3.mk"
-.include "../../security/libgcypt/buildlink3.mk"
+.include "../../security/libgcrypt/buildlink3.mk"
 .endif # LIBGGZ_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libggz
