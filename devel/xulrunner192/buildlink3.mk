57182c07bf648cd7bddae8a8906eeb6dbab988e9
obache <obache@pkgsrc.org>
2011-07-14 11:31:02 +0000

    Set upper bound API_DEPENDS guard, not to allow newer xulrunner.

--- devel/xulrunner192/buildlink3.mk
+++ devel/xulrunner192/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/04/19 11:20:59 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/07/14 11:31:02 obache Exp $
 
 BUILDLINK_TREE+=	xulrunner
 
 .if !defined(XULRUNNER_BUILDLINK3_MK)
 XULRUNNER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=1.9.2.2
+BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=1.9.2.2<2.0
 BUILDLINK_ABI_DEPENDS.xulrunner?=	xulrunner>=1.9.2.13nb1
 BUILDLINK_PKGSRCDIR.xulrunner?=		../../devel/xulrunner192
 
