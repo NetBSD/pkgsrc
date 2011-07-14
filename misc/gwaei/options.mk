5f0f3cadaa7560ea4ab9f1d93993fc631f0c1006
ryoon <ryoon>
2011-07-14 21:11:54 +0000

    Update to 3.0.0
    * Switch gtk2 to gtk3.
    
    Changelog:
    2010-01-10: gWaei 3.0.0
      New version of gwaei made to compile against gtk+-3.0 and match other Gnome 3
    apps.  Most of the code has been rewritten and the advantages won't all appear u
    ntil futuer versions of the program.
    
      -Spellcheck is now don't directly with enchant instead of relying on libsexy
      -The backend is now separated into a libtool library for cleaner code.
      -The dictionary folder is now in ~/.config/gwaei instead of ~/.waei.
      -Manual installation of dictionaries is now easier and more straight forward.
      -Translations are now handled by intltool instead of gettext
      -Much of the code now uses GError for proper error handling and message passin
    g
      -The code now uses GRegex instead of the C regex library included with the OS,
     making the program more cross platform friendly

--- misc/gwaei/options.mk
+++ misc/gwaei/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2011/01/25 11:02:35 ryoon Exp $
+# $NetBSD: options.mk,v 1.5 2011/07/14 21:11:54 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gwaei
 PKG_SUPPORTED_OPTIONS=	gnome
@@ -9,15 +9,8 @@ PKG_SUGGESTED_OPTIONS=	gnome
 .if !empty(PKG_OPTIONS:Mgnome)
 PLIST_SRC+=		PLIST.gnome
 
-BUILDLINK_API_DEPENDS.gtk2+=	gtk2+>=2.14.0
-.include "../../x11/gtk2/buildlink3.mk"
-BUILDLINK_API_DEPENDS.libgnome+=	libgnome>=2.22.0
-.include "../../devel/libgnome/buildlink3.mk"
-BUILDLINK_API_DEPENDS.libsexy+=		libsexy>=0.1.11
-.include "../../devel/libsexy/buildlink3.mk"
-.include "../../graphics/hicolor-icon-theme/buildlink3.mk"
 .include "../../sysutils/desktop-file-utils/desktopdb.mk"
-.include "../../x11/libunique/buildlink3.mk"
+.include "../../x11/libunique3/buildlink3.mk"
 .include "../../mk/omf-scrollkeeper.mk"
 .else
 CONFIGURE_ARGS+=	--without-gnome
