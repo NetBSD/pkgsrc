82b17cdfd7a6e476e45185edd4cd906f0b47e8e0
tron <tron>
2011-07-20 23:24:05 +0000

    Fix build with GCC 4.5's C preprocessor.

--- editors/emacs22/hacks.mk
+++ editors/emacs22/hacks.mk
@@ -1,16 +1,25 @@
-# $NetBSD: hacks.mk,v 1.1.1.1 2009/08/05 10:30:29 minskim Exp $
+# $NetBSD: hacks.mk,v 1.2 2011/07/20 23:24:05 tron Exp $
 
-.if !defined(EMACS_HACKS_MK)
-EMACS_HACKS_MK=	# defined
-
-
+### [Sun Mar 14 19:32:40 UTC 2004 : jlam]
+### GCC 3.3.x and older versions have an optimization bug on powerpc that's
+### tickled by the db4 source code, so remove optimization flags in that
+### case. This fixes PR pkg/30647 by Ian Spray.
 ###
-### Workaround for PR pkg/39778
-###
-.  include "../../mk/bsd.fast.prefs.mk"
-.  if !empty(MACHINE_PLATFORM:MNetBSD-[4-9].*-x86_64)
-pre-build:
-	${TOUCH} ${WRKSRC}/leim/quail/tsang-b5.el
+.if ${MACHINE_ARCH} == "powerpc"
+.  include "../../mk/compiler.mk"
+.  if !empty(CC_VERSION:Mgcc*)
+.    if !defined(_GCC_IS_TOO_OLD)
+_GCC_IS_TOO_OLD!=	\
+	if ${PKG_ADMIN} pmatch 'gcc<3.4' ${CC_VERSION}; then		\
+		${ECHO} "yes";						\
+	else								\
+		${ECHO} "no";						\
+	fi
+MAKEFLAGS+=	_GCC_IS_TOO_OLD=${_GCC_IS_TOO_OLD:Q}
+.    endif
+.    if !empty(_GCC_IS_TOO_OLD:M[yY][eE][sS])
+PKG_HACKS+=		powerpc-codegen
+BUILDLINK_TRANSFORM+=	rm:-O[0-9]*
+.    endif
 .  endif
-
 .endif
