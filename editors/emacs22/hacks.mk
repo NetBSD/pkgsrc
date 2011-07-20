58f1009065e9d7beea3cbd3f785fddcd1ee5be7a
tron <tron@pkgsrc.org>
2011-07-20 23:41:12 +0000

    Build this package without optimization if GCC 4.5.* is used under NetBSD/i386. This stops "temacs" from dumping core.

---
+++ editors/emacs22/hacks.mk
@@ -0,0 +1,19 @@
+# $NetBSD: hacks.mk,v 1.4 2011/07/20 23:41:12 tron Exp $
+
+.if !defined(EMACS_HACKS_MK)
+EMACS_HACKS_MK=	defined
+
+.include "../../mk/compiler.mk"
+
+### [ Wed Jul 20 23:40:49 UTC 2011 : tron ]
+### On NetBSD/i386, gcc optimisation, at least for version 4.5.3 produces a
+### "temacs" binary which segfaults.
+###
+.if !empty(MACHINE_PLATFORM:MNetBSD-*-i386)
+.  if !empty(CC_VERSION:Mgcc-4.5.*)
+PKG_HACKS+=		optimisation
+BUILDLINK_TRANSFORM+=	rename:-O[0-9]*:-O0
+.  endif
+.endif
+
+.endif  # EMACS_HACKS_MK
