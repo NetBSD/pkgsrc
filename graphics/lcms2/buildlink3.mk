6b0570a06bca7b57a84228e50eee6e39d74add0c
jakllsch <jakllsch@pkgsrc.org>
2011-07-25 17:48:55 +0000

    Add Little CMS 2 color correction package.

---
+++ graphics/lcms2/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/25 17:48:55 jakllsch Exp $
+
+BUILDLINK_TREE+=	lcms2
+
+.if !defined(LCMS2_BUILDLINK3_MK)
+LCMS2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.lcms2+=	lcms2>=2.2
+BUILDLINK_PKGSRCDIR.lcms?=	../../jakllsch/lcms2
+
+BUILDLINK_TARGETS+=	buildlink-include-lcms2
+
+.PHONY: buildlink-include-lcms2
+buildlink-include-lcms2:
+.for _h_ in lcms2.h
+	${RUN}								\
+	if [ ! -f ${BUILDLINK_DIR}/include/lcms2/${_h_} ]; then		\
+		${MKDIR} ${BUILDLINK_DIR}/include/lcms2;		\
+		${ECHO_BUILDLINK_MSG} "Linking lcms2/${_h_} to ${_h_} in ${BUILDLINK_DIR}/include"; \
+		${LN} -s ${BUILDLINK_PREFIX.lcms2}/include/${_h_} ${BUILDLINK_DIR}/include/lcms2/${_h_}; \
+	fi
+.endfor
+.endif # LCMS2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-lcms2
