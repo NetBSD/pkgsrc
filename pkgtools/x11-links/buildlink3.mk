b944530db8b477bb506dde45fc5032921f576c7b
wiz <wiz@pkgsrc.org>
2011-08-01 15:09:28 +0000

    Update for recent freetype2 import in NetBSD-current. Welcome to 0.66!

--- pkgtools/x11-links/buildlink3.mk
+++ pkgtools/x11-links/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.39 2011/02/26 10:00:54 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.40 2011/08/01 15:09:28 wiz Exp $
 #
 # Don't include this file manually!  It will be included as necessary
 # by bsd.buildlink3.mk.
@@ -12,7 +12,7 @@ BUILDLINK_TREE+=	x11-links
 .if !defined(X11_LINKS_BUILDLINK3_MK)
 X11_LINKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.65
+BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.66
 BUILDLINK_PKGSRCDIR.x11-links?=	../../pkgtools/x11-links
 BUILDLINK_DEPMETHOD.x11-links?=	build
 
