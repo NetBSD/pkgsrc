e8248761fe4b93d503e2636bb8f51346171ce92d
tron <tron@pkgsrc.org>
2011-08-01 21:42:45 +0000

    The "rtorrent" packges needs at least version 0.12.9 of the "libtorrent" package. As the former is the only user of the later bump both the minium ABI and API version.
    Problem reported by Aleksey Cheusov in private e-mail.

--- net/libtorrent/buildlink3.mk
+++ net/libtorrent/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.17 2010/01/17 12:02:33 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2011/08/01 21:42:45 tron Exp $
 
 BUILDLINK_TREE+=	libtorrent
 
 .if !defined(LIBTORRENT_BUILDLINK3_MK)
 LIBTORRENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libtorrent+=	libtorrent>=0.12.0
-BUILDLINK_ABI_DEPENDS.libtorrent+=	libtorrent>=0.12.6nb1
+BUILDLINK_API_DEPENDS.libtorrent+=	libtorrent>=0.12.9
+BUILDLINK_ABI_DEPENDS.libtorrent+=	libtorrent>=0.12.9
 BUILDLINK_PKGSRCDIR.libtorrent?=	../../net/libtorrent
 
 .include "../../devel/libsigc++/buildlink3.mk"
