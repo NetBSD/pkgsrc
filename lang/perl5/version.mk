1f337496f7da922d948ef12c85072eb535fe7020
adam <adam@pkgsrc.org>
2011-08-05 18:06:14 +0000

    Changes 5.14.1: * Several modules were updated to fix minor bugs. * Tests were fixed. * Documentation was improved.
    Changes 5.14.0:
    * Unicode version 6.0 is now supported.
    * Regular expressions can now be reset to the default modifiers.
    * Four new regular expression modifiers, "/d", "/l" , "/u", and "/a", were
      introduced.
    * The substitution operators now support the modifier "/r" to work
      non-destructively, leaving the original unmodified.
    * Larger octals can now be specified.
    * Array and hash operators can now experimentally operate directly on hard
      references.
    * printf-like functions now understand post-1980 size modifiers.
    * Exception handling was made more reliable and consistent.
    * Other enhancements and many bug fixes were made
    
    PkgSrc:
    * Fixed configuring on Mac OS X 10.7.

--- lang/perl5/version.mk
+++ lang/perl5/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.3 2005/05/27 22:15:53 jlam Exp $
+# $NetBSD: version.mk,v 1.4 2011/08/05 18:06:14 adam Exp $
 #
 # Distill the PERL5_REQD list into a single value that is the highest
 # version of Perl required.
@@ -25,7 +25,7 @@ _PERL5_PKG_SATISFIES_DEP!=	\
 .          endif
 .        endfor
 .        if !empty(_PERL5_PKG_SATISFIES_DEP:M[yY][eE][sS])
-_PERL5_REQD=  ${_version_}
+_PERL5_REQD=	${_version_}
 .        endif
 .      endif
 .    endfor
