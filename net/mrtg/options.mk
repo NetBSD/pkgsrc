a4bda1fe090a36652a3caa43660baa6f91f3accd
tron <tron@pkgsrc.org>
2011-08-06 11:52:16 +0000

    Update "mrtg" package to version 2.17.2. Changes since version 2.16.4: - can not replace a file by renaming another to its name under WIN32   fix in rateup #104 - Fix scaling when noo and noi options are used. fix for  #95 - Stop staticly linking rateup. fix for #97 - Only set LD_RUN_PATH if it is actually necessary. patch from #98 - Net::SNMP only support aes128, des and 3des ... fix for cfgmaker from #99 - Understand desc and descr in ifdesc option of cfgmaker - only set $args{'-maxrepetitions'} if $Net_SNMP_util::MaxRepetitions > 0 - do not load Net_SNMP_util unless snmpv3 is requested ... fix for #75 - be more agressive about updating the confcache. If a key is missing, go   looking for it again and complain. fix for #74 - only apply 32bit counter wrap compensation when the wrap is within 32bit   range. fix for #70 - implement pngdate as TimeStrPos[x]: RU instead of separately. for for #68 - add missing <p> tag arount 'last updated ... ' in html output. fix for #63 - remove rececondition when writing new trafic graph files by writing to a   temp file and moving it later. fix for #51 - make snmpopt_current available in cfgmaker templates. fix for #48 - explicitly import Socket6 routines in SNMP_Session. fix for #45 - default ThreshHyst to 0.1 only if ThreshDir is define. fix for #42 - point out when there is an unknown interface found. fix for #41. - allow for $speed to return 0 and still use SNMPv2 ... some Cisco Gear   seems todo that. Fix for #33 - add dlink support for ifAlias OIDs to cfgmaker from #50 - add WWP (Ciena) support for ifAlias OIDs to cfgmaker from #66 - Updated squid docu #80 - Fix russian translation bug #92 - added rrdcached support   - new keyword:  RRDCached: which overrides anything set in the RRDCACHED_ADDRESS     environment variable and sets MRTG to work in RRDCached mode.  This verifies     that you're using a unix-domain socket and the socket is writeable.   - Check for environment variable RRDCACHED_ADDRESS and use this if no     explicit address was set in the cfg file, making the same config validity     checks.   - Make warning if using rrdcached to the effect that the thresholding     checks will no longer work.   - If in rrdcached-mode, and using RRDTool 1.4, then force use of update     rather than updatev (as rrdcached does not yet support updatev) and     consequently bypass the thresholding checks.   - Changes to validity check error messages  to also report the file     containing the error (as well as the line). - Added support for glob patterns in include - Make daemon reload cfg automatically when it changes - added support for Arista to cfgmaker - fix cfgmaker to work in mixed v1/2/3 snmp environment - do not strigify arguments to conversion function let eval take care of this - untaint inlast and outlast to make  perl taint mode happy - teach cfgmaker about openbsd
    pkgsrc changes:
    - Avoid "Subroutine SNMP_Session::pack_sockaddr_in6 redefined" warnings
      with the Perl 5.14.1. As the patch might break "mrtg" with older version
      of Perl require at least version 5.14.1 of the "perl5" package.

--- net/mrtg/options.mk
+++ net/mrtg/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2010/07/09 08:45:33 adam Exp $
+# $NetBSD: options.mk,v 1.4 2011/08/06 11:52:16 tron Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mrtg
 PKG_SUPPORTED_OPTIONS=	inet6 mrtg-snmpv3
@@ -16,5 +16,5 @@ DEPENDS+=	p5-Net-SNMP-[0-9]*:../../net/p5-Net-SNMP
 ### Support for IPv6 enabled devices
 ###
 .if !empty(PKG_OPTIONS:Minet6)
-DEPENDS+=	p5-IO-Socket-INET6>=2.00:../../net/p5-IO-Socket-INET6
+DEPENDS+=	p5-IO-Socket-INET6>=2.67:../../net/p5-IO-Socket-INET6
 .endif
