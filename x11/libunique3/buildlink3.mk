c33be04a7fa934d9f7cce7de2e1d02e366f7436b
ryoon <ryoon>
2011-08-06 22:23:34 +0000

    Fix typos.

--- x11/libunique3/buildlink3.mk
+++ x11/libunique3/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/08/06 22:21:58 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/08/06 22:23:34 ryoon Exp $
 
-BUILDLINK_TREE+=	libunique
+BUILDLINK_TREE+=	libunique3
 
-.if !defined(LIBUNIQUE_BUILDLINK3_MK)
-LIBUNIQUE_BUILDLINK3_MK:=
+.if !defined(LIBUNIQUE3_BUILDLINK3_MK)
+LIBUNIQUE3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libunique+=	libunique3>=3.0.2
-BUILDLINK_PKGSRCDIR.libunique?=	../../x11/libunique3
+BUILDLINK_API_DEPENDS.libunique3+=	libunique3>=3.0.2
+BUILDLINK_PKGSRCDIR.libunique3?=	../../x11/libunique3
 
-.endif	# LIBUNIQUE_BUILDLINK3_MK
+.endif	# LIBUNIQUE3_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-libunique
+BUILDLINK_TREE+=	-libunique3
