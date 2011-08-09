9be2f1e82fb7061b9dfd2042bbb51fa303e96ddb
manu <manu@pkgsrc.org>
2011-08-09 07:15:42 +0000

    - Use linkat(2) if available. This fixes a EPERM error when using rename(2) on a symlink within a glusterfs volume that uses DHT - Consider gfid changes as nn fatal. Glustefs will fix instead of panic.

--- filesystems/glusterfs/options.mk
+++ filesystems/glusterfs/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2011/07/19 07:54:31 manu Exp $
+# $NetBSD: options.mk,v 1.2 2011/08/09 07:15:42 manu Exp $
 
 PKG_OPTIONS_VAR=        PKG_OPTIONS.glusterfs
 PKG_SUPPORTED_OPTIONS=  georeplication
