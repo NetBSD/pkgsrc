ed36c6b132ee643d77dd53ba0e760a0a5588a6f5
taca <taca@pkgsrc.org>
2011-08-12 15:50:44 +0000

    Update rubygems package to 1.8.7 with adding compatibility to read older gemspec files.
    Changes are too may to write here, please refer History.txt file.

--- misc/rubygems/patches/patch-lib_rubygems_require__paths__builder.rb
+++ misc/rubygems/patches/patch-lib_rubygems_require__paths__builder.rb
@@ -1,6 +1,8 @@
-$NetBSD: patch-lib_rubygems_require__paths__builder.rb,v 1.1 2011/03/23 14:35:05 taca Exp $
+$NetBSD: patch-lib_rubygems_require__paths__builder.rb,v 1.2 2011/08/12 15:50:44 taca Exp $
 
---- lib/rubygems/require_paths_builder.rb.orig	2011-03-04 00:01:24.000000000 +0000
+Compatibility with ruby19-base's gem.
+
+--- lib/rubygems/require_paths_builder.rb.orig	2011-05-18 22:23:53.000000000 +0000
 +++ lib/rubygems/require_paths_builder.rb
 @@ -14,5 +14,5 @@ module Gem::RequirePathsBuilder
        file.puts spec.bindir if spec.bindir
@@ -9,4 +11,4 @@ $NetBSD: patch-lib_rubygems_require__paths__builder.rb,v 1.1 2011/03/23 14:35:05
 -end if Gem::QUICKLOADER_SUCKAGE
 -
 +end # if Gem::QUICKLOADER_SUCKAGE
-+    # it could be enabled after 1.9.2's rubygem updated.
++    # it could be enabled after 1.9.x's rubygem updated.
