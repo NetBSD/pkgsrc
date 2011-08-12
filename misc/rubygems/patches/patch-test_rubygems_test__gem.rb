b8ab946db9c8cce222a097b3ca64142cb1a5ff64
taca <taca@pkgsrc.org>
2011-08-12 15:50:44 +0000

    Update rubygems package to 1.8.7 with adding compatibility to read older gemspec files.
    Changes are too may to write here, please refer History.txt file.

--- misc/rubygems/patches/patch-test_rubygems_test__gem.rb
+++ misc/rubygems/patches/patch-test_rubygems_test__gem.rb
@@ -1,8 +1,10 @@
-$NetBSD: patch-test_rubygems_test__gem.rb,v 1.1 2011/03/23 14:35:05 taca Exp $
+$NetBSD: patch-test_rubygems_test__gem.rb,v 1.2 2011/08/12 15:50:44 taca Exp $
 
---- test/rubygems/test_gem.rb.orig	2011-03-09 03:30:40.000000000 +0000
+Use vendorlibdir.
+
+--- test/rubygems/test_gem.rb.orig	2011-05-26 04:01:53.000000000 +0000
 +++ test/rubygems/test_gem.rb
-@@ -822,13 +822,13 @@ class TestGem < Gem::TestCase
+@@ -806,13 +806,13 @@ class TestGem < Gem::TestCase
      Gem::ConfigMap[:libdir] = orig_libdir
    end
  
