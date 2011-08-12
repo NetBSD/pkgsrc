517df2b7eba10a88d8d24e2c024726c1f1c830b0
taca <taca@pkgsrc.org>
2011-08-12 17:19:26 +0000

    Fix build problem adding paches from http://cvs.pld-linux.org/.

--- textproc/ruby-ferret/patches/patch-ext_r__search.c
+++ textproc/ruby-ferret/patches/patch-ext_r__search.c
@@ -1,8 +1,9 @@
-$NetBSD: patch-ext_r__search.c,v 1.1 2011/06/19 16:01:52 taca Exp $
+$NetBSD: patch-ext_r__search.c,v 1.2 2011/08/12 17:19:27 taca Exp $
 
 * Switch to modern Ruby's API: http://cvs.pld-linux.org/
+* Care for incompatible rb_cvar_set() between ruby18 and ruby19.
 
---- ext/r_search.c.orig	2011-06-10 05:02:24.000000000 +0000
+--- ext/r_search.c.orig	2011-08-08 00:48:52.000000000 +0000
 +++ ext/r_search.c
 @@ -161,8 +161,7 @@ frt_get_td(TopDocs *td, VALUE rsearcher)
      VALUE hit_ary = rb_ary_new2(td->size);
@@ -57,16 +58,20 @@ $NetBSD: patch-ext_r__search.c,v 1.1 2011/06/19 16:01:52 taca Exp $
  		doc_id = FIX2INT(rb_funcall(rhit, id_doc, 0));
  		lzd = sea->get_lazy_doc(sea, doc_id);
  		s = frt_lzd_load_to_json(lzd, &str, s, &len);
-@@ -623,7 +622,7 @@ frt_mtq_set_dmt(VALUE self, VALUE rnum_t
+@@ -623,7 +622,11 @@ frt_mtq_set_dmt(VALUE self, VALUE rnum_t
          rb_raise(rb_eArgError,
                   "%d <= 0. @@max_terms must be > 0", max_terms);
      }
 -    rb_cvar_set(cMultiTermQuery, id_default_max_terms, rnum_terms, Qfalse);
-+    rb_cvar_set(cMultiTermQuery, id_default_max_terms, rnum_terms);
++    rb_cvar_set(cMultiTermQuery, id_default_max_terms, rnum_terms
++#ifdef RUBY18
++		, RTEST(ruby_verbose)
++#endif
++	    );
      return rnum_terms;
  }
  
-@@ -1165,15 +1164,15 @@ frt_phq_add(int argc, VALUE *argv, VALUE
+@@ -1165,15 +1168,15 @@ frt_phq_add(int argc, VALUE *argv, VALUE
              {
                  int i;
                  char *t;
@@ -86,25 +91,33 @@ $NetBSD: patch-ext_r__search.c,v 1.1 2011/06/19 16:01:52 taca Exp $
                      phq_append_multi_term(q, t);
                  }
                  break;
-@@ -1426,7 +1425,7 @@ frt_fq_set_dms(VALUE self, VALUE val)
+@@ -1426,7 +1429,11 @@ frt_fq_set_dms(VALUE self, VALUE val)
                   "%f < 0.0. :min_similarity must be > 0.0", min_sim);
      }
      qp_default_fuzzy_min_sim = (float)min_sim;
 -    rb_cvar_set(cFuzzyQuery, id_default_min_similarity, val, Qfalse);
-+    rb_cvar_set(cFuzzyQuery, id_default_min_similarity, val);
++    rb_cvar_set(cFuzzyQuery, id_default_min_similarity, val
++#ifdef RUBY18
++		, RTEST(ruby_verbose)
++#endif
++	    );
      return val;
  }
  
-@@ -1458,7 +1457,7 @@ frt_fq_set_dpl(VALUE self, VALUE val)
+@@ -1458,7 +1465,11 @@ frt_fq_set_dpl(VALUE self, VALUE val)
                   "%d < 0. :prefix_length must be >= 0", pre_len);
      }
      qp_default_fuzzy_pre_len = pre_len;
 -    rb_cvar_set(cFuzzyQuery, id_default_prefix_length, val, Qfalse);
-+    rb_cvar_set(cFuzzyQuery, id_default_prefix_length, val);
++    rb_cvar_set(cFuzzyQuery, id_default_prefix_length, val
++#ifdef RUBY18
++		, RTEST(ruby_verbose)
++#endif
++	    );
      return val;
  }
  
-@@ -1591,8 +1590,8 @@ frt_spanmtq_init(VALUE self, VALUE rfiel
+@@ -1591,8 +1602,8 @@ frt_spanmtq_init(VALUE self, VALUE rfiel
  {
      Query *q = spanmtq_new(frt_field(rfield));
      int i;
@@ -115,7 +128,7 @@ $NetBSD: patch-ext_r__search.c,v 1.1 2011/06/19 16:01:52 taca Exp $
      }
      Frt_Wrap_Struct(self, NULL, &frt_q_free, q);
      object_add(q, self);
-@@ -1716,8 +1715,8 @@ frt_spannq_init(int argc, VALUE *argv, V
+@@ -1716,8 +1727,8 @@ frt_spannq_init(int argc, VALUE *argv, V
              int i;
              Query *clause;
              Check_Type(v, T_ARRAY);
@@ -126,7 +139,7 @@ $NetBSD: patch-ext_r__search.c,v 1.1 2011/06/19 16:01:52 taca Exp $
                  spannq_add_clause(q, clause);
              }
          }
-@@ -1782,8 +1781,8 @@ frt_spanoq_init(int argc, VALUE *argv, V
+@@ -1782,8 +1793,8 @@ frt_spanoq_init(int argc, VALUE *argv, V
          int i;
          Query *clause;
          Check_Type(rclauses, T_ARRAY);
@@ -137,7 +150,7 @@ $NetBSD: patch-ext_r__search.c,v 1.1 2011/06/19 16:01:52 taca Exp $
              spanoq_add_clause(q, clause);
          }
      }
-@@ -2277,8 +2276,8 @@ frt_sort_init(int argc, VALUE *argv, VAL
+@@ -2277,8 +2288,8 @@ frt_sort_init(int argc, VALUE *argv, VAL
          case 1: 
                  if (TYPE(rfields) == T_ARRAY) {
                      int i;
@@ -148,7 +161,7 @@ $NetBSD: patch-ext_r__search.c,v 1.1 2011/06/19 16:01:52 taca Exp $
                      }
                  } else {
                      frt_sort_add(sort, rfields, reverse);
-@@ -2632,8 +2631,6 @@ frt_sea_search_each(int argc, VALUE *arg
+@@ -2632,8 +2643,6 @@ frt_sea_search_each(int argc, VALUE *arg
  
      rb_scan_args(argc, argv, "11", &rquery, &roptions);
  
@@ -157,7 +170,7 @@ $NetBSD: patch-ext_r__search.c,v 1.1 2011/06/19 16:01:52 taca Exp $
      Data_Get_Struct(rquery, Query, q);
      td = frt_sea_search_internal(q, roptions, sea);
  
-@@ -2648,8 +2645,6 @@ frt_sea_search_each(int argc, VALUE *arg
+@@ -2648,8 +2657,6 @@ frt_sea_search_each(int argc, VALUE *arg
      rtotal_hits = INT2FIX(td->total_hits);
      td_destroy(td);
  
@@ -166,7 +179,7 @@ $NetBSD: patch-ext_r__search.c,v 1.1 2011/06/19 16:01:52 taca Exp $
      return rtotal_hits;
  }
  
-@@ -2753,8 +2748,7 @@ frt_sea_highlight(int argc, VALUE *argv,
+@@ -2753,8 +2760,7 @@ frt_sea_highlight(int argc, VALUE *argv,
          VALUE rexcerpts = rb_ary_new2(size);
  
          for (i = 0; i < size; i++) {
@@ -176,7 +189,7 @@ $NetBSD: patch-ext_r__search.c,v 1.1 2011/06/19 16:01:52 taca Exp $
          }
          ary_destroy(excerpts, &free);
          return rexcerpts;
-@@ -2870,10 +2864,10 @@ frt_ms_init(int argc, VALUE *argv, VALUE
+@@ -2870,10 +2876,10 @@ frt_ms_init(int argc, VALUE *argv, VALUE
          rsearcher = argv[i];
          switch (TYPE(rsearcher)) {
              case T_ARRAY:
@@ -190,16 +203,20 @@ $NetBSD: patch-ext_r__search.c,v 1.1 2011/06/19 16:01:52 taca Exp $
                      Data_Get_Struct(rs, Searcher, s);
                      searchers[top++] = s;
                  }
-@@ -3107,7 +3101,7 @@ Init_MultiTermQuery(void)
+@@ -3107,7 +3113,11 @@ Init_MultiTermQuery(void)
      cMultiTermQuery = rb_define_class_under(mSearch, "MultiTermQuery", cQuery);
      rb_define_alloc_func(cMultiTermQuery, frt_data_alloc);
  
 -    rb_cvar_set(cMultiTermQuery, id_default_max_terms, INT2FIX(512), Qfalse);
-+    rb_cvar_set(cMultiTermQuery, id_default_max_terms, INT2FIX(512));
++    rb_cvar_set(cMultiTermQuery, id_default_max_terms, INT2FIX(512)
++#ifdef RUBY18
++		, RTEST(ruby_verbose)
++#endif
++	    );
      rb_define_singleton_method(cMultiTermQuery, "default_max_terms",
                                 frt_mtq_get_dmt, 0);
      rb_define_singleton_method(cMultiTermQuery, "default_max_terms=",
-@@ -3415,10 +3409,8 @@ Init_FuzzyQuery(void)
+@@ -3415,10 +3425,16 @@ Init_FuzzyQuery(void)
  
      cFuzzyQuery = rb_define_class_under(mSearch, "FuzzyQuery", cQuery);
      rb_define_alloc_func(cFuzzyQuery, frt_data_alloc);
@@ -207,8 +224,16 @@ $NetBSD: patch-ext_r__search.c,v 1.1 2011/06/19 16:01:52 taca Exp $
 -                rb_float_new(0.5), Qfalse);
 -    rb_cvar_set(cFuzzyQuery, id_default_prefix_length,
 -                INT2FIX(0), Qfalse);
-+    rb_cvar_set(cFuzzyQuery, id_default_min_similarity, rb_float_new(0.5));
-+    rb_cvar_set(cFuzzyQuery, id_default_prefix_length, INT2FIX(0));
++    rb_cvar_set(cFuzzyQuery, id_default_min_similarity, rb_float_new(0.5)
++#ifdef RUBY18
++		, RTEST(ruby_verbose)
++#endif
++	    );
++    rb_cvar_set(cFuzzyQuery, id_default_prefix_length, INT2FIX(0)
++#ifdef RUBY18
++		, RTEST(ruby_verbose)
++#endif
++	    );
  
      rb_define_singleton_method(cFuzzyQuery, "default_min_similarity",
                                 frt_fq_get_dms, 0);
