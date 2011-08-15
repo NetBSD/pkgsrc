f7bc3a011d008f8c4f541d9107e3632fde1d8549
schnoebe <schnoebe@pkgsrc.org>
2011-08-15 01:56:33 +0000

    Add an option to select/suppress SNMP support.
    (configure was finding libsnmp.so if net-snmp was installed, but
     net-snmp wasn't buildlink'd in. It would build just fine if net-snmp
     wasn't installed.)

--- graphics/sane-backends/options.mk
+++ graphics/sane-backends/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.7 2008/04/13 21:43:39 wiz Exp $
+# $NetBSD: options.mk,v 1.8 2011/08/15 01:56:33 schnoebe Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.sane-backends
-PKG_SUPPORTED_OPTIONS=		inet6
+PKG_SUPPORTED_OPTIONS=		inet6 snmp
 PKG_SUGGESTED_OPTIONS=		#defined
 
 .include "../../mk/bsd.options.mk"
@@ -12,4 +12,11 @@ PKG_SUGGESTED_OPTIONS=		#defined
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
+.endif
+
+.if !empty(PKG_OPTIONS:Msnmp)
+CONFIGURE_ARGS+=	--with-snmp=yes
+.include "../../net/net-snmp/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--with-snmp=no
 .endif
