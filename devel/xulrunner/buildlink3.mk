c254bb05e75d6c7b33025735b96f75c4ffb017c0
tnn <tnn@pkgsrc.org>
2011-08-18 18:31:09 +0000

    Update to Firefox 6.0.
    Major changes include:
    
    The address bar now highlights the domain of the website you're visiting
    Streamlined the look of the site identity block
    Added support for the latest draft version of WebSockets with a prefixed API
    Added support for EventSource / server-sent events
    Added support for window.matchMedia
    Added Scratchpad, an interactive JavaScript prototyping environment
    Added a new Web Developer menu item and moved development-related items into it
    Improved usability of the Web Console
    Improved the discoverability of Firefox Sync
    Reduced browser startup time when using Panorama
    Fixed several stability issues
    Fixed several security issues

--- devel/xulrunner/buildlink3.mk
+++ devel/xulrunner/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.15 2011/07/21 13:05:51 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2011/08/18 18:31:09 tnn Exp $
 
 BUILDLINK_TREE+=	xulrunner
 
 .if !defined(XULRUNNER_BUILDLINK3_MK)
 XULRUNNER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=5.0
-BUILDLINK_ABI_DEPENDS.xulrunner?=		xulrunner>=5.0nb1
+BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=6.0
+BUILDLINK_ABI_DEPENDS.xulrunner?=		xulrunner>=6.0
 BUILDLINK_PKGSRCDIR.xulrunner?=		../../devel/xulrunner
 
 pkgbase := xulrunner
