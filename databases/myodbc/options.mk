fac12006d9e2dc4de0001e0c60df6c0c00f9b0f5
adam <adam@pkgsrc.org>
2011-08-19 07:59:58 +0000

    Changes 5.1.8: * Documentation in .CHM and .HLP format has been removed from the distribution. * For some procedure and parameter combinations SQLProcedureColumns() did not   work correctly. For example, it could not return records for an existing   procedure with correct parameters supplied. * Further, it returned incorrect data for column 7, TYPE_NAME. For example, it   returned VARCHAR(20) instead of VARCHAR. * The MySQL Connector/ODBC MSI installer did not set the InstallLocation value   in the Microsoft Windows registry. * In bulk upload mode, SQLExecute would return SQL_SUCCESS, even when the   uploaded data contained errors, such as primary key duplication, and foreign   key violation. * SQLDescribeCol and SQLColAttribute could not be called before SQLExecute, if   the query was parameterized and not all parameters were bound. * Note, MSDN states that “For performance reasons, an application should not   call SQLColAttribute/SQLDescribeCol before executing a statement.” However,   it should still be possible to do so if performance reasons are not paramount. * When SQLNumResultCols() was called between SQLPrepare() and SQLExecute() the   driver ran SET @@sql_select_limit=1, which limited the resultset to just one   row. * After installing MySQL Connector/ODBC, the system DSN created could not be   configured or deleted. An error dialog was displayed, showing the error   message “Invalid attribute string”. * In this case the problem was due to the fact that the driver could not parse   the NULL-separated connection string. * When used after a call to SQLTables(), SQLRowCount() did not return the   correct value.

--- databases/myodbc/options.mk
+++ databases/myodbc/options.mk
@@ -1,17 +1,16 @@
-# $NetBSD: options.mk,v 1.4 2008/04/12 22:42:59 jlam Exp $
-#
+# $NetBSD: options.mk,v 1.5 2011/08/19 07:59:58 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.myodbc
 PKG_OPTIONS_REQUIRED_GROUPS=	db
-PKG_OPTIONS_GROUP.db=	myodbc-iodbc myodbc-unixodbc
-PKG_SUPPORTED_OPTIONS=	myodbc-gui
-PKG_SUGGESTED_OPTIONS=	myodbc-iodbc
+PKG_OPTIONS_GROUP.db=	iodbc unixodbc
+PKG_SUPPORTED_OPTIONS=	gui
+PKG_SUGGESTED_OPTIONS=	iodbc
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=		gui
 
-.if !empty(PKG_OPTIONS:Mmyodbc-gui)
+.if !empty(PKG_OPTIONS:Mgui)
 .  include "../../x11/qt3-tools/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-gui
 USE_LANGUAGES=		c c++
@@ -20,13 +19,13 @@ PLIST.gui=		yes
 CONFIGURE_ARGS+=	--disable-gui
 .endif
 
-.if !empty(PKG_OPTIONS:Mmyodbc-iodbc)
+.if !empty(PKG_OPTIONS:Miodbc)
 .  include "../../databases/iodbc/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-iODBC=${BUILDLINK_PREFIX.iodbc}
 CONFIGURE_ARGS+=	--with-odbc-ini=${PKG_SYSCONFDIR.iodbc}/odbc.ini
 .endif
 
-.if !empty(PKG_OPTIONS:Mmyodbc-unixodbc)
+.if !empty(PKG_OPTIONS:Munixodbc)
 .  include "../../databases/unixodbc/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-unixODBC=${BUILDLINK_PREFIX.unixodbc}
 CONFIGURE_ARGS+=	--with-odbc-ini=${PKG_SYSCONFDIR.unixodbc}/odbc.ini
