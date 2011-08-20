c46d4cc8a9800b279853263439a202eb54199011
joerg <joerg@pkgsrc.org>
2011-08-20 15:35:23 +0000

    Fix C99 vs GNU89 inline mess. Annotate that AMD64 grows stack downwards

---
+++ lang/librep/patches/patch-src-repint.h
@@ -0,0 +1,17 @@
+$NetBSD: patch-src-repint.h,v 1.1 2011/08/20 15:35:24 joerg Exp $
+
+--- src/repint.h.orig	2011-08-19 14:37:20.000000000 +0000
++++ src/repint.h
+@@ -243,8 +243,10 @@ typedef struct rep_cons_block_struct {
+    call when the heap needs to grow. */
+ 
+ #if defined __GNUC__ && defined __OPTIMIZE__
+-extern __inline__ repv inline_Fcons (repv x, repv y);
+-extern __inline__ repv
++#if !defined(__GNUC_STDC_INLINE__) || !__GNUC_STDC_INLINE__
++extern
++#endif
++__inline__ repv
+ inline_Fcons (repv x, repv y)
+ {
+     rep_cons *c = rep_cons_freelist;
