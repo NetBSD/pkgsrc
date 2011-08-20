76600218bdade8fc7d20886bed99818461b36e84
tnn <tnn>
2011-08-20 13:05:49 +0000

    Fix build of legacy mozilla packages and rename the PKGBASE to facilitate coinstallation with current packages.

--- devel/xulrunner192/buildlink3.mk
+++ devel/xulrunner192/buildlink3.mk
@@ -1,20 +1,24 @@
-# $NetBSD: buildlink3.mk,v 1.4 2011/07/21 13:05:52 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2011/08/20 13:05:49 tnn Exp $
 
-BUILDLINK_TREE+=	xulrunner
+BUILDLINK_TREE+=	xulrunner192
 
-.if !defined(XULRUNNER_BUILDLINK3_MK)
-XULRUNNER_BUILDLINK3_MK:=
+.if !defined(XULRUNNER192_BUILDLINK3_MK)
+XULRUNNER192_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=1.9.2.2<2.0
-BUILDLINK_ABI_DEPENDS.xulrunner?=	xulrunner>=1.9.2.17nb1
-BUILDLINK_PKGSRCDIR.xulrunner?=		../../devel/xulrunner192
+pre-configure: xulrunner192-preconfigure
+.PHONY: xulrunner192-preconfigure
+xulrunner192-preconfigure:
+	cp ${PREFIX}/lib/xulrunner192/pkgconfig/* ${BUILDLINK_DIR}/lib/pkgconfig
 
-pkgbase := xulrunner
+BUILDLINK_API_DEPENDS.xulrunner192+=	xulrunner192>=1.9.2.17nb2<2.0
+BUILDLINK_PKGSRCDIR.xulrunner192?=	../../devel/xulrunner192
+
+pkgbase := xulrunner192
 .include "../../mk/pkg-build-options.mk"
-.if !empty(PKG_BUILD_OPTIONS.xulrunner:Mgnome)
+.if !empty(PKG_BUILD_OPTIONS.xulrunner192:Mgnome)
 . include "../../devel/libgnomeui/buildlink3.mk"
 . include "../../sysutils/gnome-vfs/buildlink3.mk"
 .endif
-.endif # XULRUNNER_BUILDLINK3_MK
+.endif # XULRUNNER192_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-xulrunner
+BUILDLINK_TREE+=	-xulrunner192
