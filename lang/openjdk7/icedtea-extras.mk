2230fcb7585f5d8eb058f5f598dfe24355cef728
tnn <tnn@pkgsrc.org>
2011-08-21 22:57:54 +0000

    Update openjdk7 package to openjdk7-1.7.0.447.20110811.
    - based on openjdk7 b147 and the bsd-port branch as of 20110811.
    - integrates mozilla rhino 1.7R3 scripting support based on patches
      from icedtea7 snapshot as of 20110819.
    - refresh bootstrap kits
      - tested on NetBSD 5.0/current, i386/amd64 and DragonFlyBSD 2.10 i386.
    
    Upstream changes: mostly bugfixes. Perhaps also noteworthy is that the
    zero assembler port has been integrated which should open up for wider
    platform support in pkgsrc in the future.

--- lang/openjdk7/icedtea-extras.mk
+++ lang/openjdk7/icedtea-extras.mk
@@ -1,23 +1,23 @@
-# $NetBSD: icedtea-extras.mk,v 1.9 2011/03/13 14:18:33 abs Exp $
+# $NetBSD: icedtea-extras.mk,v 1.10 2011/08/21 22:57:54 tnn Exp $
 
 # Needed to extract icedtea
 EXTRACT_USING=			bsdtar
 
 DIST_SUBDIR=			openjdk7
 
-ICEDTEA=			icedtea-8dc6e1ff8ccf
-ICEDTEA_TGZ=			8dc6e1ff8ccf.tar.bz2
-SITES.${ICEDTEA_TGZ}=		http://icedtea.classpath.org/hg/icedtea/archive/
+ICEDTEA=			icedtea7-8ac25d11a9e5
+ICEDTEA_TGZ=			8ac25d11a9e5.tar.bz2
+SITES.${ICEDTEA_TGZ}=		http://icedtea.classpath.org/hg/icedtea7/archive/
 DISTFILES+=			${ICEDTEA_TGZ}
 EXTRACT_ONLY+=			${ICEDTEA_TGZ}
 
-XALAN=				xalan-j_2_7_1
-XALAN_TGZ=			${XALAN}-bin.zip
-SITES.${XALAN_TGZ}=		${MASTER_SITE_APACHE:=xml/xalan-j/}
-DISTFILES+=			${XALAN_TGZ}
-EXTRACT_ONLY+=			${XALAN_TGZ}
+ICEDTEA_WEB=			icedtea-web-61e08e67b176
+ICEDTEA_WEB_TGZ=		61e08e67b176.tar.bz2
+SITES.${ICEDTEA_WEB_TGZ}=	http://icedtea.classpath.org/hg/icedtea-web/archive/
+DISTFILES+=			${ICEDTEA_WEB_TGZ}
+EXTRACT_ONLY+=			${ICEDTEA_WEB_TGZ}
 
-RHINO=				rhino1_7R2
+RHINO=				rhino1_7R3
 RHINO_TGZ=			${RHINO}.zip
 SITES.${RHINO_TGZ}=		${MASTER_SITE_MOZILLA:=js/}
 DISTFILES+=			${RHINO_TGZ}
@@ -32,43 +32,14 @@ EXTRACT_ONLY+=			${ANT_TGZ}
 ANT_BIN=			${WRKDIR}/${ANT}/bin
 PREPEND_PATH+=			${ANT_BIN}
 
-ICEDTEA_CONFIGURE_ARGS=	\
-	  --with-xalan2-jar=${WRKDIR}/${XALAN}/xalan.jar \
-	  --with-xalan2-serializer-jar=${WRKDIR}/${XALAN}/serializer.jar \
-	  --with-xerces2-jar=${WRKDIR}/${XALAN}/xercesImpl.jar \
-	  --with-rhino=${WRKDIR}/${RHINO}/js.jar \
-	  --with-jdk-home=${PREFIX}/java/openjdk7
+JAXP_TGZ=			jaxp145_01.zip
+SITES.${JAXP_TGZ}=		http://icedtea.classpath.org/download/drops/
+DISTFILES+=			${JAXP_TGZ}
 
-.if defined(ICEDTEA_PACKAGE)
-CONFIGURE_ARGS+=	${ICEDTEA_CONFIGURE_ARGS}
+JAXWS_TGZ=			jdk7-jaxws2_2_4-b03-2011_05_27.zip
+SITES.${JAXWS_TGZ}=		http://icedtea.classpath.org/download/drops/
+DISTFILES+=			${JAXWS_TGZ}
 
-USE_LANGUAGES=		c c++
-USE_TOOLS+=	gawk gmake pkg-config autoconf automake
-BUILD_DEPENDS+=	zip-[0-9]*:../../archivers/zip
-CONFIGURE_ENV+=	ac_cv_path_MD5SUM=/usr/bin/true
-CONFIGURE_ENV+=	ac_cv_path_SHA256SUM=/usr/bin/true
-CONFIGURE_ENV+=	ac_cv_path_WGET=/usr/bin/true
-
-pre-configure: icedtea-autoconf
-.PHONY: icedtea-autoconf
-icedtea-autoconf:
-	cd ${WRKDIR}/${ICEDTEA} && ${SETENV} ${CONFIGURE_ENV} autoreconf -if
-
-post-extract: icedtea-post-extract
-.PHONY: icedtea-post-extract
-icedtea-post-extract:
-	chmod +x ${ANT_BIN}/ant
-
-BUILDLINK_DEPMETHOD.cups?=     build
-.include "../../print/cups/buildlink3.mk"
-.include "../../lang/openjdk7/buildlink3.mk"
-.include "../../devel/xulrunner/buildlink3.mk"
-.include "../../mk/giflib.buildlink3.mk"
-BUILDLINK_DEPMETHOD.libXp?=	build
-.include "../../x11/libXp/buildlink3.mk"
-BUILDLINK_DEPMETHOD.libXt?=	build
-.include "../../x11/libXt/buildlink3.mk"
-BUILDLINK_DEPMETHOD.libXtst?=	build
-.include "../../x11/libXtst/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
-.endif
+JAF_TGZ=			jdk7-jaf-2010_08_19.zip
+SITES.${JAF_TGZ}=		http://icedtea.classpath.org/download/drops/
+DISTFILES+=			${JAF_TGZ}
