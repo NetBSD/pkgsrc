365f1f7673afae7e3dafa8b8a4ef8f72b1a1988b
wiz <wiz>
2011-08-21 10:23:42 +0000

    Update to 0.2.19:
    0.2.19  Fri Aug 05 2011  Toni Gundogdu
    
    Fixes: Webscripts
        - ted.lua: attempt to index local 's' (#65)
        - youtube.lua: no match: fmt_url_map (#63)
            - Thanks to Zefram <zefram@fysh.org> for the patch
        - funnyordie.lua: no match: flv url (#64)

--- net/quvi/buildlink3.mk
+++ net/quvi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2011/04/22 13:42:09 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2011/08/21 10:23:42 wiz Exp $
 
 BUILDLINK_TREE+=	quvi
 
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.quvi+=	quvi>=0.2.15
 BUILDLINK_ABI_DEPENDS.quvi?=	quvi>=0.2.15nb1
 BUILDLINK_PKGSRCDIR.quvi?=	../../net/quvi
 
-.include "../../devel/pcre/buildlink3.mk"
 .include "../../www/curl/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../lang/lua/buildlink3.mk"
