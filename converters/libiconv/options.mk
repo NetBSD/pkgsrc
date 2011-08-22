ec01e1fafb0ae47caaf7fa2edbf2bda0a676867a
wiz <wiz@pkgsrc.org>
2011-08-22 14:03:36 +0000

    Update to 1.14; disable cp932 option for now, no patch available yet.
    New in 1.14:
    * The 'iconv' program now produces its output as soon as it can. It no longer
      unnecessarily waits for more input.
    * Updated the GB18030 converter to map 25 characters to code points that have
      been to Unicode since 2000, rather than to code points in the Private Use
      Area.
    * Updated the BIG5-HKSCS converter. The old BIG5-HKSCS converter is renamed to
      BIG5-HKSCS:2004. A new converter BIG5-HKSCS:2008 is added. BIG5-HKSCS is now
      an alias for BIG5-HKSCS:2008.
    * Fixed a bug in the conversion to wchar_t.
    * Fixed a small bug in the CP1258 converter.

--- converters/libiconv/options.mk
+++ converters/libiconv/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2010/11/10 21:00:49 adam Exp $
+# $NetBSD: options.mk,v 1.4 2011/08/22 14:03:36 wiz Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libiconv
@@ -8,6 +8,7 @@ PKG_SUGGESTED_OPTIONS=	libiconv-cp932
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mlibiconv-cp932)
+# http://www2d.biglobe.ne.jp/~msyk/software/libiconv-patch.html
 PATCH_SITES=	http://www2d.biglobe.ne.jp/~msyk/software/libiconv/
 PATCHFILES=	libiconv-1.13-cp932.patch.gz
 PATCH_DIST_STRIP=	-p1
