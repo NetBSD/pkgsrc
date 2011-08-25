2b327481dbacb734f3ec3328b731671011571489
jakllsch <jakllsch@pkgsrc.org>
2011-08-25 13:30:23 +0000

    Add flickcurl-1.21
    Flickcurl is a C library for the Flickr API, handling creating the
    requests, signing, token management, calling the API, marshalling
    request parameters and decoding responses. It uses libcurl to call
    the REST web service and libxml2 to manipulate the XML responses.
    Flickcurl supports all of the API including the functions for
    photo/video uploading, browsing, searching, adding and editing
    comments, groups, notes, photosets, categories, activity, blogs,
    favorites, places, tags, machine tags, institutions, pandas and
    photo/video metadata. It also includes a program flickrdf to turn
    photo metadata, tags, machine tags and places into an RDF triples
    description.

---
+++ net/flickcurl/patches/patch-src_flickcurl__internal.h
@@ -0,0 +1,12 @@
+$NetBSD: patch-src_flickcurl__internal.h,v 1.1.1.1 2011/08/25 13:30:23 jakllsch Exp $
+
+--- src/flickcurl_internal.h.orig	2010-07-24 19:33:34.000000000 +0000
++++ src/flickcurl_internal.h
+@@ -24,7 +24,6 @@
+ #include <libxml/xpath.h>
+ 
+ #include <curl/curl.h>
+-#include <curl/types.h>
+ #include <curl/easy.h>
+ 
+ 
