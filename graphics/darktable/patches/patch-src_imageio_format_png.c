610431f4dea8890d0fd2e827d9a20851c8fd6810
jakllsch <jakllsch@pkgsrc.org>
2011-08-25 14:42:53 +0000

    Update to darktable-0.9.2.
    pkgsrc changes: enable flickr module.
    
    upstream changes:
    
    0.9.1 to 0.9.2:
    not yet available
    
    0.9 to 0.9.1:
     - new rawspeed, dcraw, libraw
     - fixed various segfaults and deadlocks
     - the pipeline is now more real HDR (unbounded color management, no more gamut clipping in between)
     - fixed a nasty bug which could cause complete loss of history for an image
     - darktable-faster now plays nicely with darktablerc (non-gconf)
     - lots of opencl improvements
     - updated translations
     - second part of our GSoC: customizable keyboard shortcuts!

--- graphics/darktable/patches/patch-src_imageio_format_png.c
+++ graphics/darktable/patches/patch-src_imageio_format_png.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_imageio_format_png.c,v 1.1.1.1 2011/07/26 21:37:47 jakllsch Exp $
+$NetBSD: patch-src_imageio_format_png.c,v 1.2 2011/08/25 14:42:53 jakllsch Exp $
 
 Needs a constant from zlib.
 
---- src/imageio/format/png.c.orig	2011-07-02 03:15:32.000000000 +0000
+--- src/imageio/format/png.c.orig	2011-08-24 09:20:29.000000000 +0000
 +++ src/imageio/format/png.c
 @@ -27,6 +27,7 @@
  #include <stdlib.h>
