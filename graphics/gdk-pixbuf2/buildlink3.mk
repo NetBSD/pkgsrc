014e47884a8ae700499f1ee0bf2ff7d5d5fba638
drochner <drochner@pkgsrc.org>
2011-08-30 22:34:13 +0000

    update to 2.24.0 This switches to the latest stable branch.

--- graphics/gdk-pixbuf2/buildlink3.mk
+++ graphics/gdk-pixbuf2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/04/22 13:42:26 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/08/30 22:34:13 drochner Exp $
 
 BUILDLINK_TREE+=	gdk-pixbuf2
 
@@ -10,6 +10,7 @@ BUILDLINK_ABI_DEPENDS.gdk-pixbuf2?=	gdk-pixbuf2>=2.22.1nb2
 BUILDLINK_PKGSRCDIR.gdk-pixbuf2?=	../../graphics/gdk-pixbuf2
 
 .include "../../devel/glib2/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
 .endif	# GDK_PIXBUF2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gdk-pixbuf2
