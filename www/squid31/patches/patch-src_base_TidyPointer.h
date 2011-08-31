fcef8201060bf901a59aff3675962dc2427e8aa7
sbd <sbd>
2011-08-31 09:19:24 +0000

    Pullup ticket #3516 - requested by tron www/squid31 security update
    Revisions pulled up:
    - www/squid31/Makefile                                          1.32-1.33
    - www/squid31/distinfo                                          1.29-1.30
    - www/squid31/patches/patch-src_base_TidyPointer.h              1.1
    - www/squid31/patches/patch-src_ssl_gadgets.cc                  1.1
    
    ---
       Module Name:	pkgsrc
       Committed By:	adam
       Date:		Wed Jul 20 11:44:27 UTC 2011
    
       Modified Files:
       	pkgsrc/www/squid31: Makefile distinfo
       Added Files:
       	pkgsrc/www/squid31/patches: patch-src_base_TidyPointer.h
       	    patch-src_ssl_gadgets.cc
    
       Log Message:
       Changes 3.1.14:
       * Regression Bug 3261: Could not create a DNS socket and exit
    
       Changes 3.1.13:
       * Regression Bug 3239: problems with myip/myport upgrade
       * Bug 3153: hung ICAP RESPMOD transactions
       * Update ssl_crtd to use 'OK' status inline with other helpers
    
    ---
       Module Name:	pkgsrc
       Committed By:	tron
       Date:		Tue Aug 30 23:02:57 UTC 2011
    
       Modified Files:
       	pkgsrc/www/squid31: Makefile distinfo
    
       Log Message:
       Update "squid31" package to version 3.1.15. Changes since version 3.1.14:
       - Regression fix: vhost and defaultsite causing vport to be ignored
       - Regression Bug 3295: broken escaping in rfc1738_do_escape
       - Bug #3232: fails to compile with OpenSSL v1.0.0
       - Bug #3222: cache_peer name is not logging on CONNECT
       - Bug #3131: fd_table[fd].closing() assert from
         ConnStateData::noteMoreBodySpaceAvailable()
       - Bug #3217: "!fd_table[fd].closing()" from
         ServerStateData::noteMoreBodySpaceAvailable
       - Bug #3213: https sites (CONNECT) not open when using NTLM
       - Bug #3114: Memory leak in SSL certificate verify code
       - Bug #3107: ncsa_auth DES silently truncates passwords to 8 bytes
       - Bug #2662: cf_gen failure when cross compiling
       - Bug #2655: passing wrong the username to the url_rewrite_program
       - Bug #2495: ignore whitespace prefix on config lines
       - Bug #2051: 'default' cache_peer option does not match documentation
       - Bug #1842: Optimize order of tests in peerWouldBePinged() and
         peerHTTPOkay()
       - Bug #1791: timestampsSet does not validate Date: if server sends very
         old date
       - Correct parsing of large Gopher indexes
       - Enable negative cacheing on unknown or -1 expiry timestamp
       - Remove hierarchy_stoplist default value
       - Migrate cf_gen tool from C-style to C++
       - ... and several documentation and compiler warning fixes

---
+++ www/squid31/patches/patch-src_base_TidyPointer.h
@@ -0,0 +1,17 @@
+$NetBSD: patch-src_base_TidyPointer.h,v 1.1.2.2 2011/08/31 09:19:24 sbd Exp $
+
+Define NULL if not defined.
+
+--- src/base/TidyPointer.h.orig	2011-07-08 09:42:57.000000000 +0000
++++ src/base/TidyPointer.h
+@@ -5,6 +5,10 @@
+ #ifndef SQUID_BASE_TIDYPOINTER_H
+ #define SQUID_BASE_TIDYPOINTER_H
+ 
++#ifndef NULL
++#define NULL ((void *)0)
++#endif
++
+ /**
+  * A pointer that deletes the object it points to when the pointer's owner or
+  * context is gone. Similar to std::auto_ptr but without confusing assignment
