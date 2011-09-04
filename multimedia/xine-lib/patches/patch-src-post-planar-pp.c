d2883982880bc57cbee0fcb48cac2a14b87b8089
joerg <joerg>
2011-09-04 14:34:52 +0000

    Address computations are not ICE, even if GCC accepts them as such. Fix that and use the same emms constraint as found in other places.

---
+++ multimedia/xine-lib/patches/patch-src-post-planar-pp.c
@@ -0,0 +1,18 @@
+$NetBSD: patch-src-post-planar-pp.c,v 1.1 2011/09/04 14:34:52 joerg Exp $
+
+--- src/post/planar/pp.c.orig	2011-08-30 13:13:55.000000000 +0000
++++ src/post/planar/pp.c
+@@ -56,9 +56,11 @@ typedef struct pp_parameters_s {
+  * description of params struct
+  */
+ START_PARAM_DESCR( pp_parameters_t )
+-PARAM_ITEM( POST_PARAM_TYPE_INT, quality, NULL, 0, PP_QUALITY_MAX, 0,
++PARAM_ITEM( pp_parameters_t,
++	    POST_PARAM_TYPE_INT, quality, NULL, 0, PP_QUALITY_MAX, 0,
+             "postprocessing quality" )
+-PARAM_ITEM( POST_PARAM_TYPE_CHAR, mode, NULL, 0, 0, 0,
++PARAM_ITEM( pp_parameters_t,
++	    POST_PARAM_TYPE_CHAR, mode, NULL, 0, 0, 0,
+             "mode string (overwrites all other options except quality)" )
+ END_PARAM_DESCR( param_descr )
+ 
---
+++ multimedia/xine-lib/patches/patch-src-xine-engine-post.h
@@ -0,0 +1,23 @@
+$NetBSD: patch-src-xine-engine-post.h,v 1.1 2011/09/04 14:34:52 joerg Exp $
+
+--- src/xine-engine/post.h.orig	2010-03-09 22:17:05.000000000 +0000
++++ src/xine-engine/post.h
+@@ -370,14 +370,15 @@ do {                                    
+ 
+ 
+ /* macros to create parameter descriptors */
++#include <stddef.h>
+ 
+ #define START_PARAM_DESCR( param_t ) \
+ static param_t temp_s; \
+ static xine_post_api_parameter_t temp_p[] = {
+ 
+-#define PARAM_ITEM( param_type, var, enumv, min, max, readonly, descr ) \
+-{ param_type, #var, sizeof(temp_s.var), \
+-  (char*)&temp_s.var-(char*)&temp_s, enumv, min, max, readonly, descr },
++#define PARAM_ITEM( param_t, param_type, var, enumv, min, max, readonly, descr ) \
++{ param_type, #var, sizeof(temp_s.var), offsetof(param_t, var), \
++  enumv, min, max, readonly, descr },
+ 
+ #define END_PARAM_DESCR( name ) \
+   { POST_PARAM_TYPE_LAST, NULL, 0, 0, NULL, 0, 0, 1, NULL } \
