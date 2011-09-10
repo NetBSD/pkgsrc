20bbf61b2a92379c64f8f310c9d00d4a20e22d6e
abs <abs@pkgsrc.org>
2011-09-10 16:36:26 +0000

    Add an apache option (defaults on) to allow building without apache

--- www/mediawiki/options.mk
+++ www/mediawiki/options.mk
@@ -1,12 +1,12 @@
-# $NetBSD: options.mk,v 1.2 2009/07/24 11:44:51 abs Exp $
+# $NetBSD: options.mk,v 1.3 2011/09/10 16:36:27 abs Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mediawiki
 
 PKG_OPTIONS_REQUIRED_GROUPS=	db
 PKG_OPTIONS_GROUP.db=		mysql pgsql
 
-PKG_SUPPORTED_OPTIONS+=	mysql pgsql
-PKG_SUGGESTED_OPTIONS=	mysql
+PKG_SUPPORTED_OPTIONS+=	apache mysql pgsql
+PKG_SUGGESTED_OPTIONS=	apache mysql
 
 .include "../../mk/bsd.options.mk"
 
@@ -17,4 +17,8 @@ PKG_SUGGESTED_OPTIONS=	mysql
 DEPENDS+=	${PHP_PKG_PREFIX}-mysql>=4.3.10:../../databases/php-mysql
 .elif !empty(PKG_OPTIONS:Mpgsql)
 DEPENDS+=	${PHP_PKG_PREFIX}-pgsql>=5:../../databases/php-pgsql
+.endif
+
+.if !empty(PKG_OPTIONS:Mapache)
+.include "../../mk/apache.mk"
 .endif
