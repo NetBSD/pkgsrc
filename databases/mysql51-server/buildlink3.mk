8a582b0e71dbcb7762a5997af139333372a8ef36
joerg <joerg@pkgsrc.org>
2011-09-12 10:53:58 +0000

    Add proper upper limit to not result in mix of MySQL 5.1 and 5.5

--- databases/mysql51-server/buildlink3.mk
+++ databases/mysql51-server/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2010/03/17 06:48:57 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/09/12 10:53:58 joerg Exp $
 
 BUILDLINK_TREE+=	mysql-server
 
 .if !defined(MYSQL_SERVER_BUILDLINK3_MK)
 MYSQL_SERVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mysql-server+=	mysql-server>=5.1.44
+BUILDLINK_API_DEPENDS.mysql-server+=	mysql-server>=5.1.44<5.5
 BUILDLINK_PKGSRCDIR.mysql-server?=	../../databases/mysql51-server
 BUILDLINK_LIBDIRS.mysql-server?=	lib/mysql
 
