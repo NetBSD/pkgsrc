81865ff7df4a4fb18d7231c43f7bf5aeb664770a
drochner <drochner@pkgsrc.org>
2011-09-12 17:25:28 +0000

    update to 1.1.11 changes: -security fixes in real and avi demuxers (were patched in pkgsrc) -minor bugfixes

--- multimedia/vlc/patches/patch-modules_audio__output_pulse.c
+++ multimedia/vlc/patches/patch-modules_audio__output_pulse.c
@@ -1,7 +1,7 @@
-$NetBSD: patch-modules_audio__output_pulse.c,v 1.1 2011/07/23 19:48:39 abs Exp $
+$NetBSD: patch-modules_audio__output_pulse.c,v 1.2 2011/09/12 17:25:28 drochner Exp $
 To enable build against pulse-audio 0.9.21 - seems to work
 
---- modules/audio_output/pulse.c.orig	2011-05-30 15:03:50.000000000 +0000
+--- modules/audio_output/pulse.c.orig	2011-06-06 22:53:14.000000000 +0000
 +++ modules/audio_output/pulse.c
 @@ -272,7 +272,7 @@ static int Open(vlc_object_t *obj)
  {
@@ -9,6 +9,6 @@ To enable build against pulse-audio 0.9.21 - seems to work
  
 -#if !defined(PA_CHECK_VERSION) || !PA_CHECK_VERSION(0,9,22)
 +#if !defined(PA_CHECK_VERSION) || !PA_CHECK_VERSION(0,9,20)
-     if( !vlc_xlib_init( p_this ) )
+     if( !vlc_xlib_init( obj ) )
          return VLC_EGENERIC;
  #endif
