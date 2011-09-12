37206349b28f924bbb621e414fe3c408d2822b4e
joerg <joerg>
2011-09-12 11:34:08 +0000

    Restrict dependency patterns to avoid mixing versions.

--- emulators/suse113_linux/emulator.mk
+++ emulators/suse113_linux/emulator.mk
@@ -1,4 +1,4 @@
-# $NetBSD: emulator.mk,v 1.2 2011/02/27 15:54:03 chs Exp $
+# $NetBSD: emulator.mk,v 1.3 2011/09/12 11:34:09 joerg Exp $
 #
 # This file is included by linux-suse.mk in the emulator framework.
 #
@@ -14,9 +14,9 @@
 EMUL_DISTRO=		suse-11.3
 
 .if defined(SUSE_PKG)
-_SUSE_VERSION_REQD=	-${SUSE_VERSION_REQD}{,nb*}
+_SUSE_VERSION_REQD=	-11.3${{,nb*}
 .else
-_SUSE_VERSION_REQD=	>=${SUSE_VERSION_REQD}
+_SUSE_VERSION_REQD=	>=11.3<11.4
 .endif
 
 # DEPENDS_suse-11.3.* maps Linux "modules" to SuSE package dependencies.
