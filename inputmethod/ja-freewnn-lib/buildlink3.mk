29421f1b0cda4666a0d5a8d911557644270eb5e3
mef <mef@pkgsrc.org>
2011-09-13 07:23:16 +0000

    - bump to 1.10 to 1.1.1-a021    See ChangeLog, ChangeLog.en    (too many lines to be listed here)
       Please note minimum security fixes had been updated after
       1.10 was committed, so not really whole thing between 1.10
       and 1.1.1-a021
    
     - And more, a021 to a022 (CVS version) equivalent patches
     - Thus
       PKGNAME=   ja-FreeWnn-{lib,server}-1.11alpha22
     - DESTDIR support  (That's why a022 patches added)
     - package structure adjusted
       from (-lib -dict -server -server-bin) to (-lib -server)
       for minimum modification from DISTFILE to pkgsrc.
     - some common VARIABLES have been moved to ja-freewnn-lib/Makefile.common
    
    ja-freewnn-dict/{DESCR,Makefile,PLIST}
    ja-freewnn-server-bin/{DESCR,Makefile}
      Removed
    
    ja-freewnn-lib/Makefile.common
      More variables moved in
    
    ja-freewnn-lib/patches
      70 files added for a021 -> a022 updates
    
    ja-freewnn-server/PLIST
      - some executable moved sbin to bin
        by following line in Makefile
       +CONFIGURE_ARGS+=       --disable-traditional-layout
    
      - 23 of dictionary files in share/wnn/ja_JP/dic
        moved from ja-freewnn-dict/PLIST to ja-freewnn-server/PLIST
    
    Thanks obache@ for correcting/proofreading for above Changes.

--- inputmethod/ja-freewnn-lib/buildlink3.mk
+++ inputmethod/ja-freewnn-lib/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2010/07/11 02:53:24 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2011/09/13 07:23:16 mef Exp $
 
 BUILDLINK_TREE+=	ja-FreeWnn-lib
 
 .if !defined(JA_FREEWNN_LIB_BUILDLINK3_MK)
 JA_FREEWNN_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ja-FreeWnn-lib+=	ja-FreeWnn-lib>=1.10nb4
+BUILDLINK_API_DEPENDS.ja-FreeWnn-lib+=	ja-FreeWnn-lib>=1.11alpha22
 BUILDLINK_PKGSRCDIR.ja-FreeWnn-lib?=	../../inputmethod/ja-freewnn-lib
-BUILDLINK_DEPMETHOD.ja-freewnn?=	build
 .endif # JA_FREEWNN_LIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ja-FreeWnn-lib
