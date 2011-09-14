a223a96d7d898b8365012f764166bbd9ce56d075
brook <brook>
2011-09-14 17:54:48 +0000

    Replace paths with ${LOCALBASE} instead of hard-coding /usr/pkg into installed files.

--- devel/cmake/buildlink3.mk
+++ devel/cmake/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:24:11 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2011/09/14 17:54:48 brook Exp $
 
 BUILDLINK_TREE+=	cmake
 
 .if !defined(CMAKE_BUILDLINK3_MK)
 CMAKE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cmake+=	cmake>=2.4.6nb3
+BUILDLINK_API_DEPENDS.cmake+=	cmake>=2.8.5nb1
 BUILDLINK_PKGSRCDIR.cmake?=	../../devel/cmake
 BUILDLINK_DEPMETHOD.cmake?=	build
 BUILDLINK_FILES.cmake+=		share/cmake-*/include/*
