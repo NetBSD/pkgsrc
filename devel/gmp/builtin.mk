28b9eb319fbb61d80ebc761091ada022c1169c7c
hans <hans@pkgsrc.org>
2011-09-14 16:10:12 +0000

    Fix detection of builtin gmp on SunOS.

--- devel/gmp/builtin.mk
+++ devel/gmp/builtin.mk
@@ -1,8 +1,8 @@
-# $NetBSD: builtin.mk,v 1.1 2011/07/08 09:40:57 drochner Exp $
+# $NetBSD: builtin.mk,v 1.2 2011/09/14 16:10:12 hans Exp $
 
 BUILTIN_PKG:=	gmp
 
-PKGCONFIG_FILE.gmp=	/usr/include/gmp.h
+PKGCONFIG_FILE.gmp=	/usr/include/gmp.h /usr/include/gmp/gmp.h
 PKGCONFIG_BASE.gmp=	/usr
 
 BUILTIN_VERSION_SCRIPT.gmp= ${AWK} \
@@ -13,3 +13,9 @@ BUILTIN_VERSION_SCRIPT.gmp= ${AWK} \
 	print major "." minor "." patch; else print ""; }'
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
+
+.if !empty(USE_BUILTIN.gmp:M[Yy][Ee][Ss])
+GMP_INCLUDE=		${FIND_FILES_gmp:S/\/gmp.h//}
+CONFIGURE_ARGS+=	--with-gmp-include=${GMP_INCLUDE}
+CPPFLAGS+=		-I${GMP_INCLUDE}
+.endif
