a35240208e1525f488fe07ba8421db1e9051c7bb
hans <hans>
2011-09-14 17:18:29 +0000

    Fix detection of builtin mpfr on SunOS.

--- math/mpfr/builtin.mk
+++ math/mpfr/builtin.mk
@@ -1,8 +1,8 @@
-# $NetBSD: builtin.mk,v 1.2 2011/07/14 18:06:59 drochner Exp $
+# $NetBSD: builtin.mk,v 1.3 2011/09/14 17:18:29 hans Exp $
 
 BUILTIN_PKG:=	mpfr
 
-PKGCONFIG_FILE.mpfr=	/usr/include/mpfr.h
+PKGCONFIG_FILE.mpfr=	/usr/include/mpfr.h /usr/include/mpfr/mpfr.h
 PKGCONFIG_BASE.mpfr=	/usr
 
 BUILTIN_VERSION_SCRIPT.mpfr= ${AWK} \
@@ -11,3 +11,7 @@ BUILTIN_VERSION_SCRIPT.mpfr= ${AWK} \
 	END { gsub("-p",".",v); print v }'
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
+
+.if !empty(USE_BUILTIN.mpfr:M[Yy][Ee][Ss])
+CONFIGURE_ARGS+=	--with-mpfr-include=${FIND_FILES_mpfr:S/\/mpfr.h//}
+.endif
