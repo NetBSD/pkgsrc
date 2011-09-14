8ac1bb8ee034f24e1b5b472c26f1492c4b906fab
hans <hans@pkgsrc.org>
2011-09-14 16:52:19 +0000

    Always add -lresolv on SunOS when linking against this.

--- devel/pth/buildlink3.mk
+++ devel/pth/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:24:27 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2011/09/14 16:52:19 hans Exp $
 
 BUILDLINK_TREE+=	pth
 
@@ -8,6 +8,8 @@ PTH_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.pth+=		pth>=2.0.0
 BUILDLINK_ABI_DEPENDS.pth+=	pth>=2.0.0nb2
 BUILDLINK_PKGSRCDIR.pth?=	../../devel/pth
+
+LIBS.SunOS+=			-lresolv
 .endif # PTH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-pth
