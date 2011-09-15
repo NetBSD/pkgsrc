bfd12bfb1edc31693ca2b3ac46752e60f3f5f362
adam <adam@pkgsrc.org>
2011-09-15 08:27:38 +0000

    Version 9.1 delivers several features which users have been requesting for years, removing roadblocks to deploying new or ported applications on PostgreSQL. These include: * Synchronous Replication: enable high-availability with consistency across   multiple servers * Per-Column Collations: support linguistically-correct sorting per database,   table or column. * Unlogged Tables: greatly improves performance for ephemeral data
    Our community of contributors innovates with cutting-edge features. Version 9.1
    includes several which are new to the database industry, such as:
    * K-Nearest-Neighbor Indexing: index on "distance" for faster location and text
      search queries
    * Serializable Snapshot Isolation: keeps concurrent transactions consistent
      without blocking, using "true serializability"
    * Writeable Common Table Expressions: execute complex multi-stage data updates
      in a single query
    * Security-Enhanced Postgres: deploy military-grade security and Mandatory
      Access Control

---
+++ databases/postgresql91-client/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/09/15 08:28:07 adam Exp $
+
+BUILDLINK_TREE+=	postgresql91-client
+
+.if !defined(POSTGRESQL91_CLIENT_BUILDLINK3_MK)
+POSTGRESQL91_CLIENT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.postgresql91-client+=	postgresql91-client>=9.1.0
+BUILDLINK_ABI_DEPENDS.postgresql91-client+=	postgresql91-client>=9.1.0
+BUILDLINK_PKGSRCDIR.postgresql91-client?=	../../databases/postgresql91-client
+
+# This variable contains the libraries need to link most clients.
+BUILDLINK_LDADD.postgresql91-client=	-lpq ${BUILDLINK_LDADD.gettext}
+BUILDLINK_FILES.postgresql91-client+=	bin/pg_config
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.endif # POSTGRESQL91_CLIENT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-postgresql91-client
