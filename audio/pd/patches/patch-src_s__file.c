d978ce3d05f5f83f7ae47dadd7b7bb377093a6fb
wiz <wiz@pkgsrc.org>
2011-09-15 01:05:04 +0000

    Initial import of pd-0.43.0.
    Pd (aka Pure Data) is a real-time graphical programming environment
    for audio, video, and graphical processing. It is the third major
    branch of the family of patcher programming languages known as Max
    (Max/FTS, ISPW Max, Max/MSP, jMax, etc.) originally developed by
    Miller Puckette and company at IRCAM. The core of Pd is written
    and maintained by Miller Puckette and includes the work of many
    developers, making the whole package very much a community effort.
    
    Pd was created to explore ideas of how to further refine the Max
    paradigm with the core ideas of allowing data to be treated in a
    more open-ended way and opening it up to applications outside of
    audio and MIDI, such as graphics and video.
    
    It is easy to extend Pd by writing object classes ("externals") or
    patches ("abstractions"). The work of many developers is already
    available as part of the standard Pd packages and the Pd developer
    community is growing rapidly. Recent developments include a system
    of abstractions for building performance environments; a library
    of objects for physical modeling; and a library of objects for
    generating and processing video in realtime.

---
+++ audio/pd/patches/patch-src_s__file.c
@@ -0,0 +1,15 @@
+$NetBSD: patch-src_s__file.c,v 1.1.1.1 2011/09/15 01:05:05 wiz Exp $
+
+File handling under NetBSD is the same as under Linux.
+
+--- src/s_file.c.orig	2011-03-19 22:22:27.000000000 +0000
++++ src/s_file.c
+@@ -36,7 +36,7 @@ int sys_defeatrt;
+ t_symbol *sys_flags = &s_;
+ void sys_doflags( void);
+ 
+-#if defined(__linux__) || defined(__CYGWIN__) || defined(__FreeBSD_kernel__) || defined(__GNU__) || defined(ANDROID)
++#if defined(__linux__) || defined(__CYGWIN__) || defined(__FreeBSD_kernel__) || defined(__GNU__) || defined(ANDROID) || defined(__NetBSD__)
+ 
+ static char *sys_prefbuf;
+ static int sys_prefbufsize;
