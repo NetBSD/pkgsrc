d6dec0711240e53e8589ee4f8d3dde2ce78344f9
shattered <shattered@pkgsrc.org>
2011-09-17 11:12:26 +0000

    PR/39143 -- use generic 'perl' option instead of private one.

--- net/oinkmaster/options.mk
+++ net/oinkmaster/options.mk
@@ -1,15 +1,16 @@
-# $NetBSD: options.mk,v 1.1 2008/12/08 13:05:22 adrianp Exp $
+# $NetBSD: options.mk,v 1.2 2011/09/17 11:12:26 shattered Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.oinkmaster
 
-PKG_SUPPORTED_OPTIONS=	oinkmaster-perl-tools
+PKG_SUPPORTED_OPTIONS=	perl
+PKG_OPTIONS_LEGACY_OPTS+=	oinkmaster-perl-tools:perl
 
 .include "../../mk/bsd.options.mk"
 
 ###
 ### Use perl tools for downloading rule files
 ###
-.if !empty(PKG_OPTIONS:Moinkmaster-perl-tools)
+.if !empty(PKG_OPTIONS:Mperl)
 DEPENDS+=	p5-Archive-Tar-[0-9]*:../../archivers/p5-Archive-Tar
 DEPENDS+=	p5-IO-Zlib-[0-9]*:../../devel/p5-IO-Zlib
 DEPENDS+=	p5-libwww-[0-9]*:../../www/p5-libwww
