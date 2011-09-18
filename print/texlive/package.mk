9bfd87d7b3acea414d823fcc2e5c24adbe2f3490
bsiegert <bsiegert@pkgsrc.org>
2011-09-18 19:04:56 +0000

    Before copying files into the destination directory, create it. Some pax implementations do not accept a non-existing destination directory.
    Fixes (some of) the texlive packages on MirBSD.
    
    Reviewed by Jörg Sonnenberger.

--- print/texlive/package.mk
+++ print/texlive/package.mk
@@ -1,4 +1,4 @@
-# $NetBSD: package.mk,v 1.9 2010/09/24 16:04:19 minskim Exp $
+# $NetBSD: package.mk,v 1.10 2011/09/18 19:04:56 bsiegert Exp $
 #
 # This Makefile fragment is intended to be included by packages that build
 # TeX Live packages.
@@ -102,6 +102,7 @@ _texlive-install:
 .for _dir in ${_dirs}
 	if [ -d ${WRKSRC}/${_dir} ]; then \
 		cd ${WRKSRC} && \
+		${MKDIR} ${_topdir} && \
 		pax -rwpm -s ',.*\.orig$$,,' \
 			${_dir} ${_topdir}; \
 	fi
