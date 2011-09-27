b00f193b28d843942b3e2936703284173c1f3c58
adam <adam@pkgsrc.org>
2011-09-27 07:24:46 +0000

    Fix for PR#45403

--- databases/postgresql90-server/files/pgsql.sh
+++ databases/postgresql90-server/files/pgsql.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: pgsql.sh,v 1.2 2010/12/19 09:53:29 adam Exp $
+# $NetBSD: pgsql.sh,v 1.3 2011/09/27 07:24:46 adam Exp $
 #
 # PostgreSQL database rc.d control script
 #
@@ -26,9 +26,9 @@ name="pgsql"
 rcvar=${name}
 command="@PREFIX@/bin/pg_ctl"
 procname="@PREFIX@/bin/postgres"
-pgsql_user="@PGUSER@"
-pgsql_group="@PGGROUP@"
-pgsql_home="@PGHOME@"
+: ${pgsql_user:=@PGUSER@}
+: ${pgsql_group:=@PGGROUP@}
+: ${pgsql_home:=@PGHOME@}
 
 extra_commands="initdb reload"
 initdb_cmd="pgsql_initdb"
