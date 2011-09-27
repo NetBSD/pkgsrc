f69f3f09b7268110186fa273f292a24c14315803
ryoon <ryoon>
2011-09-27 12:43:20 +0000

    Fix PR pkg/45408.
    Treat getine as like as getopt.
    
    Thank you, obache@.

--- devel/scmcvs/patches/patch-lib_getline.h
+++ devel/scmcvs/patches/patch-lib_getline.h
@@ -1,15 +1,17 @@
-$NetBSD: patch-lib_getline.h,v 1.1 2011/09/22 15:16:26 ryoon Exp $
+$NetBSD: patch-lib_getline.h,v 1.2 2011/09/27 12:43:21 ryoon Exp $
 
 * Avoid getline conflict
 
 --- lib/getline.h.orig	2005-04-04 20:46:05.000000000 +0000
 +++ lib/getline.h
-@@ -12,7 +12,7 @@
+@@ -11,8 +11,10 @@
+ 
  #define GETLINE_NO_LIMIT -1
  
++#ifndef HAVE_GETLINE
  int
--  getline __PROTO ((char **_lineptr, size_t *_n, FILE *_stream));
-+  getline_cvs __PROTO ((char **_lineptr, size_t *_n, FILE *_stream));
+   getline __PROTO ((char **_lineptr, size_t *_n, FILE *_stream));
++#endif
  int
    getline_safe __PROTO ((char **_lineptr, size_t *_n, FILE *_stream,
                           int limit));
