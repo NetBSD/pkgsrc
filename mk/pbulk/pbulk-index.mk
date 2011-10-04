82b0a5487e8c6b1820049a8a59be49218a96952b
hans <hans@pkgsrc.org>
2011-10-04 11:41:20 +0000

    Use ${TAR} instead of tar.

--- mk/pbulk/pbulk-index.mk
+++ mk/pbulk/pbulk-index.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: pbulk-index.mk,v 1.12 2011/09/25 19:51:47 joerg Exp $
+#	$NetBSD: pbulk-index.mk,v 1.13 2011/10/04 11:41:20 hans Exp $
 
 # This Makefile fragment is included by bsd.pkg.mk and provides all
 # variables and targets related to the parallel bulk build
@@ -111,4 +111,4 @@ pbulk-index-item:
 .endif
 
 pbulk-save-wrkdir:
-	${RUN} [ ! -d ${WRKDIR} ] || cd ${WRKDIR} && tar cfz ${INTO:Q} .
+	${RUN} [ ! -d ${WRKDIR} ] || cd ${WRKDIR} && ${TAR} cfz ${INTO:Q} .
