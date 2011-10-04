c85b52a608f31ac63a37aec679cae73d435c06a8
minskim <minskim@pkgsrc.org>
2011-10-04 19:30:01 +0000

    Update emacs-snapshot to 24.0.90.20111004.
    Notable changes:
    * buffer.h (struct buffer): Use time_t, not int, for a time stamp.
      This fixes a Y2038 bug on 64-bit hosts.
    * pcmpl-cvs.el (pcmpl-cvs-entries): Update for Emacs 22.1 changes to
      split-string.
    * mouse.el (mouse-drag-track): Set scroll-margin to 0 while
      tracking (Bug#9541).
    * mail/smtpmail.el (smtpmail-via-smtp): Ignore errors that arise
      when sending QUIT
    * emacs-lisp/bytecomp.el (byte-compile-lapcode): Fix overflow check.

--- editors/emacs-snapshot/version.mk
+++ editors/emacs-snapshot/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.7 2010/06/07 22:44:01 minskim Exp $
+# $NetBSD: version.mk,v 1.8 2011/10/04 19:30:02 minskim Exp $
 
 _EMACS_FLAVOR=	emacs
 _EMACS_REQD=	emacs>=23
@@ -7,4 +7,4 @@ _EMACS_PKGDEP.leim=
 
 _EMACS_VERSION_MAJOR=	24
 _EMACS_VERSION_MINOR=	0
-_EMACS_VERSION_MICRO=	50
+_EMACS_VERSION_MICRO=	90
