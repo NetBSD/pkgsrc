e8ef948def201a5f0141cb3c408852ed7b0c22e7
bsiegert <bsiegert>
2011-10-04 18:15:29 +0000

    Import libatomic_ops from wip, copy distfile to ftp.netbsd.org.
    Fixes build on Mac OS X, thus fixing PR pkg/45377.

--- devel/libatomic_ops/buildlink3.mk
+++ devel/libatomic_ops/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:24:16 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/10/04 18:15:29 bsiegert Exp $
 
 BUILDLINK_TREE+=	libatomic_ops
 
 .if !defined(LIBATOMIC_OPS_BUILDLINK3_MK)
 LIBATOMIC_OPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libatomic_ops+=	libatomic_ops>=20081024nb1
-BUILDLINK_PKGSRCDIR.libatomic_ops?=	../../devel/libatomic_ops
+BUILDLINK_API_DEPENDS.libatomic_ops+=	libatomic_ops>=20110807
+BUILDLINK_PKGSRCDIR.libatomic_ops?=	../../wip/libatomic_ops
 .endif # LIBATOMIC_OPS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libatomic_ops
