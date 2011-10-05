0ae434d781b8bbb86303b370b630b03106b71af4
obache <obache@pkgsrc.org>
2011-10-05 02:14:19 +0000

    Take care of rtmp option.

--- www/curl/buildlink3.mk
+++ www/curl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.24 2011/04/22 13:42:07 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2011/10/05 02:14:19 obache Exp $
 
 BUILDLINK_TREE+=	curl
 
@@ -19,6 +19,9 @@ pkgbase:= curl
 .  endif
 .  if !empty(PKG_BUILD_OPTIONS.curl:Mldap)
 .    include "../../databases/openldap-client/buildlink3.mk"
+.  endif
+.  if !empty(PKG_BUILD_OPTIONS.curl:Mrtmp)
+.    include "../../net/rtmpdump/buildlink3.mk"
 .  endif
 
 .include "../../devel/libidn/buildlink3.mk"
