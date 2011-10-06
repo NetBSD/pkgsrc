3002a7213ecb7d323c62425a810050240c59408b
sborrill <sborrill>
2011-10-06 14:19:25 +0000

    Update to netbsd-iscsi-20111006. Allow maximum number of targets in initiator to be set in mk.conf with ISCSI_INITIATOR_MAX_TARGETS
    Changes:
    Provide function in libiscsi to return the compile-time limit on number of
    targets. Use this function in the initiator, to ensure that the same limit
    is used throughout (i.e. stop you attempting to compile the initiator with a
    different limit to the library - the initiator uses the library for most of
    its functionality).
    
    If truncating the number of targets in the initiator, truncate it to the
    correct amount, not half of what it should be.
    
    Obey RFC3720 when determining targets. SendTargets=All MUST be supported
    on a discovery session, and MUST NOT be supported on an operational session.
    Previously, SendTargets=All was used both in the initial discovery session
    and the later session. (from Daisuke Aoyama - author of istgt).

--- devel/netbsd-iscsi-lib/buildlink3.mk
+++ devel/netbsd-iscsi-lib/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/02/22 13:33:22 sborrill Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/10/06 14:19:25 sborrill Exp $
 
 BUILDLINK_TREE+=	netbsd-iscsi-lib
 
 .if !defined(NETBSD_ISCSI_LIB_BUILDLINK3_MK)
 NETBSD_ISCSI_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.netbsd-iscsi-lib+=	netbsd-iscsi-lib>=20110222
-BUILDLINK_ABI_DEPENDS.netbsd-iscsi-lib+=	netbsd-iscsi-lib>=20110222
+BUILDLINK_API_DEPENDS.netbsd-iscsi-lib+=	netbsd-iscsi-lib>=20111006
+BUILDLINK_ABI_DEPENDS.netbsd-iscsi-lib+=	netbsd-iscsi-lib>=20111006
 BUILDLINK_PKGSRCDIR.netbsd-iscsi-lib?=	../../devel/netbsd-iscsi-lib
 .endif # NETBSD_ISCSI_LIB_BUILDLINK3_MK
 
