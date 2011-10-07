40b690e1ae861ac38c949c1e322e8afa543c96d0
ryoon <ryoon@pkgsrc.org>
2011-10-07 13:28:54 +0000

    Update to 3.4.3 based on wip/libreoffice3-bin
    Due to security issue CVE-2011-2713.
    http://www.libreoffice.org/advisories/CVE-2011-2713/

--- misc/libreoffice3-bin/options.mk
+++ misc/libreoffice3-bin/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2011/06/12 09:05:30 ryoon Exp $
+# $NetBSD: options.mk,v 1.2 2011/10/07 13:28:54 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libreoffice-bin
 PKG_SUPPORTED_OPTIONS=	java
