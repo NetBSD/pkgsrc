6b4ff14900f4f2808b9ac8ea2bc50f8497cd6da6
drochner <drochner@pkgsrc.org>
2011-10-07 19:04:46 +0000

    update to 1.3.0 changes: cleanup and minor fixes
    pkgsrc note: according to the ChangeLog, this version is not binary
    compatible to older versions -> ABI_DEPENDS bumped

--- multimedia/libmatroska/buildlink3.mk
+++ multimedia/libmatroska/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2011/03/18 12:42:13 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2011/10/07 19:04:46 drochner Exp $
 
 BUILDLINK_TREE+=	libmatroska
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libmatroska
 LIBMATROSKA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libmatroska+=	libmatroska>=0.8.0
-BUILDLINK_ABI_DEPENDS.libmatroska+=	libmatroska>=1.1.0
+BUILDLINK_ABI_DEPENDS.libmatroska+=	libmatroska>=1.3.0
 BUILDLINK_PKGSRCDIR.libmatroska?=	../../multimedia/libmatroska
 
 .include "../../devel/libebml/buildlink3.mk"
