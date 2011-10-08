88eb376e1a9712848e7a068a02ab2f35523fb65b
tron <tron>
2011-10-08 20:39:43 +0000

    Pullup ticket #3539 - requested by gls pkgsrc/security/p5-Crypt-DSA: security update
    Revisions pulled up:
    - security/p5-Crypt-DSA/Makefile                                1.25
    - security/p5-Crypt-DSA/distinfo                                1.7
    - security/p5-Crypt-DSA/patches/patch-aa                        deleted
    - security/p5-Crypt-DSA/patches/patch-lib_Crypt_DSA_Util.pm     1.1
    
    ---
       Module Name:	pkgsrc
       Committed By:	gls
       Date:		Thu Oct  6 19:22:03 UTC 2011
    
       Modified Files:
       	pkgsrc/security/p5-Crypt-DSA: Makefile distinfo
       Added Files:
       	pkgsrc/security/p5-Crypt-DSA/patches: patch-lib_Crypt_DSA_Util.pm
       Removed Files:
       	pkgsrc/security/p5-Crypt-DSA/patches: patch-aa
    
       Log Message:
       Update security/p5-Crypt-DSA to 1.17
    
       1.17 2011.06.16
       - Upgrade to Module::Install 1.01
       - Added support for OpenSSL 1.0.0 dsaparam format change.
       - Requires perl 5.6 now
       - Fixes for 64-bit support
    
       While here, fix SA46275, with upstream patch from
       https://rt.cpan.org/Public/Bug/Display.html?id=71421

---
+++ security/p5-Crypt-DSA/patches/patch-lib_Crypt_DSA_Util.pm
@@ -0,0 +1,19 @@
+$NetBSD: patch-lib_Crypt_DSA_Util.pm,v 1.1.2.2 2011/10/08 20:39:43 tron Exp $
+
+Fix for SA46275, taken from https://rt.cpan.org/Public/Bug/Display.html?id=71421
+
+--- lib/Crypt/DSA/Util.pm.orig	2011-06-17 01:46:42.000000000 +0000
++++ lib/Crypt/DSA/Util.pm
+@@ -64,11 +64,8 @@ sub makerandom {
+         }
+         close $fh;
+     }
+-    elsif ( require Data::Random ) {
+-        $r .= Data::Random::rand_chars( set=>'numeric' ) for 1..$bytes;
+-    }
+     else {
+-        croak "makerandom requires /dev/random or Data::Random";
++        croak "makerandom requires /dev/random";
+     }
+     my $down = $size - 1;
+     $r = unpack 'H*', pack 'B*', '0' x ( $size % 8 ? 8 - $size % 8 : 0 ) .
