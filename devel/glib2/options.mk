6329d01db7f29d31b30367203d8f64429866c46e
jmmv <jmmv@pkgsrc.org>
2011-10-09 19:40:02 +0000

    Add support for kqueue by using the patches written by Dmitry Matveev during the Google Summer of Code 2011 program.  Congratulations, Dmitry!  :-)
    This comes in the form of a new 'kqueue' build-time option.  This should NOT
    be an option, but it is for two reasons:
    - I consider this still experimental for it to be enabled on everyone's
      machines, and the code is not "official".
    - The patch requires us to run automake/autoconf during the pre-configure
      stage, and I don't want to add these dependencies on the default package
      build.
    When these patches get integrated upstream, we can get rid of the option
    altogether.
    
    Also note that I had to backport some of our custom changes to Makefile.in
    files and some to configure to the original Makefile.am files and
    configure.ac.  This is to prevent our custom patches from being lost during
    the automake/autoconf invocations we now do when 'kqueue' is enabled.
    
    Bump PKGREVISION to 1.

---
+++ devel/glib2/options.mk
@@ -0,0 +1,29 @@
+# $NetBSD: options.mk,v 1.8 2011/10/09 19:40:02 jmmv Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.glib2
+PKG_SUPPORTED_OPTIONS=	kqueue
+PKG_SUGGESTED_OPTIONS=	# empty
+
+# We really should be doing this by default.  However, the kqueue patch is not
+# official, needs some more testing and, due to the way it is written, forces
+# us to run autoconf.  If the code gets integrated upstream, there is no reason
+# for us to provide a 'kqueue' option here and it should just go away.
+#
+#.if exists(/usr/include/sys/event.h)
+#PKG_SUGGESTED_OPTIONS+=	kqueue
+#.endif
+
+.include "../../mk/bsd.prefs.mk"
+.include "../../mk/bsd.options.mk"
+
+.if $(PKG_OPTIONS:Mkqueue)
+PATCH_SITES=		http://dmitrymatveev.co.uk/files/
+PATCHFILES=		glib-gio-kqueue-2.28.8-v0.patch
+PATCH_DIST_STRIP=	-p0
+
+USE_TOOLS+=	automake autoconf
+pre-configure: regen-autotools
+regen-autotools:
+	cd ${WRKSRC} && automake
+	cd ${WRKSRC} && autoconf
+.endif
