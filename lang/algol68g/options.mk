9f5edfdafe309554e3e670f4395c1552da645158
ryoon <ryoon>
2011-10-11 12:43:50 +0000

    Import algol68g-2.3.1 as lang/algol68g from wip/algol68g
    The development of Algol played an important role in establishing
    computer science as an academic discipline. The Algol 68 Genie
    project preserves Algol 68 out of educational as well as
    scientific-historical interest, by making available Algol 68 Genie;
    a recent, well-featured implementation written from scratch. Algol
    68 Genie is a practically full implementation of the language
    defined by the Revised Report. The implementation is a hybrid
    compiler-interpreter; units with considerable interpreter-overhead
    can optionally be compiled.
    
    Thanks for your work, Olaf Seibert.

---
+++ lang/algol68g/options.mk
@@ -0,0 +1,23 @@
+# $NetBSD: options.mk,v 1.1.1.1 2011/10/11 12:43:50 ryoon Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.a68g
+PKG_SUPPORTED_OPTIONS=	plotutils pgsql
+PKG_SUGGESTED_OPTIONS=	plotutils pgsql
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mplotutils)
+.  include "../../graphics/plotutils/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-plotutils
+.else
+CONFIGURE_ARGS+=	--without-plotutils
+.endif
+
+# It seems that when including PostgreSQL support it doesn't actually
+# need to have it installed at build time, just at run time.
+# So maybe this DEPENDS is undesirable.
+.if !empty(PKG_OPTIONS:Mpgsql)
+CONFIGURE_ARGS+=	--with-pgsql
+.else
+CONFIGURE_ARGS+=	--without-pgsql
+.endif
