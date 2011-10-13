7a1a122b5bbec9a14bff1fa45ba88109acf34290
hans <hans@pkgsrc.org>
2011-10-13 10:43:10 +0000

    Fix options nls. No need for msgfmt or libiconv when it is off.

--- lang/gcc46/options.mk
+++ lang/gcc46/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2011/05/15 00:09:56 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2011/10/13 10:43:10 hans Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gcc46
 PKG_SUPPORTED_OPTIONS=	nls
@@ -13,7 +13,9 @@ PKG_SUGGESTED_OPTIONS+=	nls
 ### Native Language Support
 ###
 .if !empty(PKG_OPTIONS:Mnls)
+USE_TOOLS+=		msgfmt
 CONFIGURE_ARGS+=	--enable-nls
+CONFIGURE_ARGS+=	--with-libiconv-prefix=${BUILDLINK_PREFIX.libiconv}
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
 .else
