bcb8452525900fd6f93f2c5dab885f0d8ad4b06f
marino <marino@pkgsrc.org>
2011-10-14 17:22:06 +0000

    devel/gps: Upgrade to version 5.0.1
    Originally this was an attempt to upgrade version 5.0.0 to version
    5.1-RELEASE or even 5.2-DEVELOPMENT, but it turns out that those
    versions require a GNAT Ada compiler based on gcc 4.7, which hasn't
    had its first release yet.  This is mainly due to an change in the
    project management API, but using the 4.7 source files fail to
    compile due to the new SPARK restrictions.  Therefore GPS must
    remain at 5.0.x until such time as GNAT-AUX is based on gcc 4.7.
    
    This is a bug fix release.
    The list of bug fixes is unknown, but it's confirmed the bug on the
    project dialog, library tab has been fixed and thus those patches
    are removed.
    
    The Makefile was updated to allow GPS users to take advantage of
    the numerous Python scripts, the Python console, and the python-GTK
    bindings.  The option is present, but it has been removed from the
    option list because the pkgsrc version of Python cause GPS to core
    dump due to missing symbols in their dynamically-loaded libraries.
    Version 2.6 and version 2.7 were both tested, and fail in different
    ways.  For comparison, the FreeBSD version of GPS builds and operates
    fine with Python 2.7, although at times similar "undefined symbol"
    messages appear it that error log.  Once the issues with Python are
    fixed, this new "python" option in options.mk will be re-enabled.

--- devel/gps/options.mk
+++ devel/gps/options.mk
@@ -1,10 +1,23 @@
-# $NetBSD: options.mk,v 1.1.1.1 2011/02/08 18:32:23 drochner Exp $
+# $NetBSD: options.mk,v 1.2 2011/10/14 17:22:06 marino Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gps
-PKG_SUPPORTED_OPTIONS=	sqlite pgsql
-PKG_SUGGESTED_OPTIONS=
+PKG_SUPPORTED_OPTIONS=	syslog python sqlite 
+PKG_SUGGESTED_OPTIONS=	syslog sqlite
+
+PLIST_VARS+=	pysupport
 
 .include "../../mk/bsd.options.mk"
+
+
+######################
+##  SYSLOG SUPPORT  ##
+######################
+
+.if !empty(PKG_OPTIONS:Msyslog)
+CONFIGURE_ARGS+= --enable-syslog=yes
+.else
+CONFIGURE_ARGS+= --enable-syslog=no
+.endif
 
 
 ######################
@@ -13,7 +26,7 @@ PKG_SUGGESTED_OPTIONS=
 
 .if !empty(PKG_OPTIONS:Msqlite)
 CONFIGURE_ARGS+= --with-sqlite=${PREFIX}
-.include "../../databases/sqlite/buildlink3.mk"
+.include "../../databases/sqlite3/buildlink3.mk"
 .endif
 
 
@@ -25,3 +38,30 @@ CONFIGURE_ARGS+= --with-sqlite=${PREFIX}
 CONFIGURE_ARGS+= --with-postgresql=${PREFIX}
 .include "../../databases/postgresql90-client/buildlink3.mk"
 .endif
+
+
+##############################
+##  PYTHON / PYGTK SUPPORT  ##
+##############################
+
+#
+# Something is wrong with the pkgsrc versions of Python.  Both
+# versions 2.6 and 2.7 will cause a core dump of gps because they
+# contain undefined symbols in their dynamically loaded libraries.
+# Since no version of Python < 3 can work with GPS, this option
+# has been removed from the list.
+#
+
+.if !empty(PKG_OPTIONS:Mpython)
+CONFIGURE_ARGS+= --with-python=${PREFIX}
+CONFIGURE_ARGS+= --enable-shared-python=no
+CONFIGURE_ARGS+= --enable-pygtk
+PLIST.pysupport= yes
+PYTHON_VERSIONS_ACCEPTED= 27 26
+.include "../../lang/python/pyversion.mk"
+.include "../../x11/py-gtk2/buildlink3.mk"
+.else
+CONFIGURE_ARGS+= --without-python
+CONFIGURE_ARGS+= --disable-pygtk
+.endif
+
