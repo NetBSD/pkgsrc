00558c162cd26e74bfc7b0437545a489ff81f56c
adam <adam>
2011-10-15 07:24:30 +0000

    Changes 2.8.6: Kitware is pleased to announce the release of CMake 2.8.6. This release features several notable changes, including a new AUTOMOC target property that helps make it easier to build Qt projects and support for the Intel Fortran compiler in VisualStudio10. Additionally, VisualStudio 9 and 10 generators for Itanium platform have been added, and appear as the “Visual Studio 9 2008 IA64” and “Visual Studio 10 IA64” generators on Windows builds of CMake.
    This release also contains an experimental generator in the Windows build targeting VisualStudio 11. It will remain "Experimental" until VisualStudio 11 itself is finalized. If you have the developer preview of VisualStudio 11, we would appreciate any feedback you have. Please keep in mind that this is very new, and has only been tested enough to be useful to include for the community to test drive and provide feedback on.
    
    Other improvements include the movement of many files from KDE's overrides of CMake module files, which have been accepted into upstream CMake thanks to the hard work of Alex Neundorf and Stephen Kelly. There have been many Xcode generator improvements, including support for iOS projects and the ability to switch between simulator and device builds.

--- devel/cmake/patches/patch-Modules_FindX11.cmake
+++ devel/cmake/patches/patch-Modules_FindX11.cmake
@@ -1,18 +1,18 @@
-$NetBSD: patch-Modules_FindX11.cmake,v 1.1 2011/09/14 17:54:48 brook Exp $
+$NetBSD: patch-Modules_FindX11.cmake,v 1.2 2011/10/15 07:24:31 adam Exp $
 
---- Modules/FindX11.cmake.orig	2011-07-08 12:21:44.000000000 +0000
+--- Modules/FindX11.cmake.orig	2011-10-04 16:09:24.000000000 +0000
 +++ Modules/FindX11.cmake
-@@ -56,7 +56,8 @@ IF (UNIX)
+@@ -60,7 +60,8 @@ IF (UNIX)
    SET(CMAKE_FIND_FRAMEWORK_SAVE ${CMAKE_FIND_FRAMEWORK})
    SET(CMAKE_FIND_FRAMEWORK NEVER)
    SET(X11_INC_SEARCH_PATH
 -    /usr/pkg/xorg/include
 +    @LOCALBASE@/xorg/include
 +    @X11BASE@/include
-     /usr/X11R6/include 
-     /usr/X11R7/include 
+     /usr/X11R6/include
+     /usr/X11R7/include
      /usr/include/X11
-@@ -66,7 +67,8 @@ IF (UNIX)
+@@ -70,7 +71,8 @@ IF (UNIX)
    )
  
    SET(X11_LIB_SEARCH_PATH
@@ -21,4 +21,4 @@ $NetBSD: patch-Modules_FindX11.cmake,v 1.1 2011/09/14 17:54:48 brook Exp $
 +    @X11BASE@/lib
      /usr/X11R6/lib
      /usr/X11R7/lib
-     /usr/openwin/lib 
+     /usr/openwin/lib
