3a2b2f0022b8d3c6d08ed94e4b5f06975c33a387
ryoon <ryoon@pkgsrc.org>
2011-10-16 08:08:29 +0000

    Add missing line to buildlink3.mk file.

--- net/libquvi/buildlink3.mk
+++ net/libquvi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/10/16 07:30:01 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/10/16 08:08:29 ryoon Exp $
 
 BUILDLINK_TREE+=	libquvi
 
@@ -8,6 +8,7 @@ LIBQUVI_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libquvi+=	libquvi>=0.4.0
 BUILDLINK_PKGSRCDIR.libquvi?=	../../net/libquvi
 
+.include "../../lang/lua/buildlink3.mk"
 .include "../../net/libquvi-scripts/buildlink3.mk"
 .endif	# LIBQUVI_BUILDLINK3_MK
 
