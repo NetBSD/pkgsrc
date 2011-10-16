0841de31662a61f3e43eecf448f61cdfba049dfd
adam <adam@pkgsrc.org>
2011-10-16 17:47:49 +0000

    Changes 1.8.0: * Various extensions to Local Minima and Maxima (3D algorithms, on-the-fly   thresholding). * Added vigra::BucketQueue, vigra::MappedBucketQueue. * Refactored and extended 2D watershed algorithms (especially   watershedsRegionGrowing()). * Added the vigra::Quaternion class. * Added Unsupervised Decomposition. * Added mathematical functions (even(), odd(), gamma(), loggamma(), legendre(),   besselJ(), besselY(), linearSequence(), indexSort(), inversePermutation(),   applyPermutation(), checksum()) * Implemented wrapper of the FFTW library to support arbitrary dimensional   Fourier transforms (see fourierTransform() and vigra::FFTWPlan) and FFT-based   convolution (see convolveFFT() and vigra::FFTWConvolvePlan) and refactored   vigra::FFTWComplex. * Added cannyEdgelListThreshold(), cannyEdgelList3x3Threshold(). * Added capability to handle subarrays and anisotropic resolution to   separableConvolveMultiArray() and related Gaussian filters. Added windowRatio   parameters to vigra::Kernel1D::initGaussian() and   vigra::Kernel1D::initGaussianDerivative() * Added vigra::StridedScanOrderIterator and corresponding   vigra::MultiArrayView::begin(). * Extended vigra::MultiArrayView. Added vigra::Shape1 ... vigra::Shape5   convenience typedefs. * Implemented vigra::multi_math (arithmetic and algebraic functions for   multi-dimensional arrays). * Extended the vigra::HDF5File class. * Improved and documented the Timing macros for runtime measurements. * Added support for the OpenEXR image format and multi-page TIFF. Improved   support for the SIF format. * vigranumpy: added axistags and completly re-implemented VigraArray and the   conversion between Python and C++ arrays in terms of axistags. * Minor improvements and bug fixes in the code and documentation.

--- graphics/vigra/buildlink3.mk
+++ graphics/vigra/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2011/01/29 16:29:03 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2011/10/16 17:47:49 adam Exp $
 
 BUILDLINK_TREE+=	vigra
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	vigra
 VIGRA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.vigra+=	vigra>=1.7.0
-BUILDLINK_ABI_DEPENDS.vigra+=	vigra>=1.7.0nb1
+BUILDLINK_ABI_DEPENDS.vigra+=	vigra>=1.8.0
 BUILDLINK_PKGSRCDIR.vigra?=	../../graphics/vigra
 
 .include "../../devel/zlib/buildlink3.mk"
