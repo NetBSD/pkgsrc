052e2af33caab677458b2836c67daaabe05f982a
spz <spz@pkgsrc.org>
2011-10-16 15:19:08 +0000

    fix build issues when building with non-standard PREFIX (found by dholland)
    introduce options
    allow optional building with net-snmp support
    
    pkglint cosmetics

--- net/xymon/patches/patch-build_rrd.sh
+++ net/xymon/patches/patch-build_rrd.sh
@@ -1,4 +1,6 @@
-$NetBSD: patch-build_rrd.sh,v 1.1 2011/10/15 23:07:24 spz Exp $
+$NetBSD: patch-build_rrd.sh,v 1.2 2011/10/16 15:19:09 spz Exp $
+
+make it possible to deal with nonstandard locations of libpng* and libz*
 
 --- build/rrd.sh.orig	2011-03-08 17:20:28.000000000 +0000
 +++ build/rrd.sh
@@ -6,8 +8,8 @@ $NetBSD: patch-build_rrd.sh,v 1.1 2011/10/15 23:07:24 spz Exp $
  	if test "$USERRRDLIB" != ""; then
  		RRDLIB="$USERRRDLIB"
  	fi
-+	if test "$USERPNGLIB" != ""; then
-+		PNGLIB="$USERPNGLIB"
++	if test "$USERPNGCONFIG" != ""; then
++		PNGLIB=`$USERPNGCONFIG --ldflags`
 +	fi
 +	if test "$USERZLIB" != ""; then
 +		ZLIB="$USERZLIB"
