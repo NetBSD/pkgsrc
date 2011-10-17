ba65681012db903c3b5fbbd942dce90b14870e8f
spz <spz@pkgsrc.org>
2011-10-17 20:14:50 +0000

    teach the package options for cases where pthreads would be used, but are broken

---
+++ net/irrd/options.mk
@@ -0,0 +1,15 @@
+PKG_OPTIONS_VAR=        PKG_OPTIONS.irrd
+PKG_SUPPORTED_OPTIONS=  pthreads
+PKG_SUGGESTED_OPTIONS=	pthreads
+
+.include "../../mk/bsd.options.mk"
+
+# include support for pthreads
+.if !empty(PKG_OPTIONS:Mpthreads)
+CONFIGURE_ARGS+=        --enable-thread
+PTHREAD_AUTO_VARS=	yes
+.include "../../mk/pthread.buildlink3.mk"
+.else
+CONFIGURE_ARGS+=        --disable-thread
+.endif
+
