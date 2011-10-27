ebab547044067c6aa0bf2dc07cf68335901239c9
abs <abs@pkgsrc.org>
2011-10-27 11:48:36 +0000

    Handle last change (libflashsupport) correctly - to match adobe-flash-plugin

--- multimedia/adobe-flash-plugin10.1/options.mk
+++ multimedia/adobe-flash-plugin10.1/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2011/10/27 11:46:20 abs Exp $
+# $NetBSD: options.mk,v 1.5 2011/10/27 11:48:36 abs Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.adobe-flash-plugin
@@ -20,6 +20,7 @@ PKG_SUGGESTED_OPTIONS=	nspluginwrapper
 
 .if !empty(PKG_OPTIONS:Mnspluginwrapper)
 DEPENDS+= nspluginwrapper>0:../../www/nspluginwrapper
+DEPENDS+= libflashsupport{,-pulse}>0:../../multimedia/libflashsupport
 INSTALL_TEMPLATES+=	${PKGDIR}/INSTALL.nspluginwrapper
 DEINSTALL_TEMPLATES+=	${PKGDIR}/INSTALL.nspluginwrapper
 .endif
