66a704b8d6ec95edacabd478415093d3214cd1f8
ryoon <ryoon@pkgsrc.org>
2011-10-27 10:32:56 +0000

    Update to 3.5.24
    Changelog:
    NEW IN VERSION 3.5.24
    ---------------------
    - Set ddjvuapi_version to 20.
    - Added ddjvuapi support for changing the white point.

--- graphics/djvulibre-lib/buildlink3.mk
+++ graphics/djvulibre-lib/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:37 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/10/27 10:32:56 ryoon Exp $
 
 BUILDLINK_TREE+=	djvulibre-lib
 
 .if !defined(DJVULIBRE_LIB_BUILDLINK3_MK)
 DJVULIBRE_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.djvulibre-lib+=	djvulibre-lib>=3.5.17
+BUILDLINK_API_DEPENDS.djvulibre-lib+=	djvulibre-lib>=3.5.24
 BUILDLINK_PKGSRCDIR.djvulibre-lib?=	../../graphics/djvulibre-lib
 .endif # DJVULIBRE_LIB_BUILDLINK3_MK
 
