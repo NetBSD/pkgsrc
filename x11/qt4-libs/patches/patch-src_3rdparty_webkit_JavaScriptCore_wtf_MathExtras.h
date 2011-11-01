d1c3c8904f00f8aabfad32abb78ec0fdd38a291c
hans <hans@pkgsrc.org>
2011-11-01 15:52:23 +0000

    Fix the build fix for SunOS, it is not needed with gcc 4.6.

--- x11/qt4-libs/patches/patch-src_3rdparty_webkit_JavaScriptCore_wtf_MathExtras.h
+++ x11/qt4-libs/patches/patch-src_3rdparty_webkit_JavaScriptCore_wtf_MathExtras.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_3rdparty_webkit_JavaScriptCore_wtf_MathExtras.h,v 1.2 2011/09/16 13:19:40 wiz Exp $
+$NetBSD: patch-src_3rdparty_webkit_JavaScriptCore_wtf_MathExtras.h,v 1.3 2011/11/01 15:52:23 hans Exp $
 
 --- src/3rdparty/webkit/JavaScriptCore/wtf/MathExtras.h.orig	2011-08-23 12:02:27.000000000 +0000
 +++ src/3rdparty/webkit/JavaScriptCore/wtf/MathExtras.h
@@ -7,7 +7,7 @@ $NetBSD: patch-src_3rdparty_webkit_JavaScriptCore_wtf_MathExtras.h,v 1.2 2011/09
  inline float grad2rad(float g) { return g * piFloat / 200.0f; }
  
 -#if !COMPILER(MSVC) && !OS(ANDROID) && !OS(SYMBIAN)
-+#if !COMPILER(MSVC) && !OS(ANDROID) && !OS(SYMBIAN) && !OS(SOLARIS)
++#if !COMPILER(MSVC) && !OS(ANDROID) && !OS(SYMBIAN) && !(OS(SOLARIS) && COMPILER(GCC) && (GCC_VERSION < 40600))
  using std::isfinite;
  using std::isinf;
  using std::isnan;
