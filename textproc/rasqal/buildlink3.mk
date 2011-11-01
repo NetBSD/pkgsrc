bdf67f079bcce857343a3208a3b496817778658d
sbd <sbd@pkgsrc.org>
2011-11-01 06:57:57 +0000

    Recursive bump for textproc/rasqal buildlink addition.

--- textproc/rasqal/buildlink3.mk
+++ textproc/rasqal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2011/11/01 06:56:34 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2011/11/01 06:57:57 sbd Exp $
 
 BUILDLINK_TREE+=	rasqal
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	rasqal
 RASQAL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.rasqal+=	rasqal>=0.9.17
-BUILDLINK_ABI_DEPENDS.rasqal?=	rasqal>=0.9.21nb1
+BUILDLINK_ABI_DEPENDS.rasqal?=	rasqal>=0.9.21nb3
 BUILDLINK_PKGSRCDIR.rasqal?=	../../textproc/rasqal
 
 .include "../../devel/gmp/buildlink3.mk"
