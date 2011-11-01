fc9db13d8717c4d5b5b432ae624924bc0f6ed10a
sbd <sbd>
2011-11-01 05:57:41 +0000

    Add missing archivers/bzip2 buildlink
    Bump PKGREVISION

--- graphics/freetype2/buildlink3.mk
+++ graphics/freetype2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.31 2009/07/21 12:00:41 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2011/11/01 05:57:41 sbd Exp $
 
 BUILDLINK_TREE+=	freetype2
 
@@ -6,13 +6,14 @@ BUILDLINK_TREE+=	freetype2
 FREETYPE2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.freetype2+=	freetype2>=2.1.8
-BUILDLINK_ABI_DEPENDS.freetype2+=	freetype2>=2.1.10nb1
+BUILDLINK_ABI_DEPENDS.freetype2+=	freetype2>=2.4.7nb1
 BUILDLINK_PKGSRCDIR.freetype2?=	../../graphics/freetype2
 BUILDLINK_INCDIRS.freetype2?=	include/freetype2
 
 FREETYPE_CONFIG?=	${BUILDLINK_PREFIX.freetype2}/bin/freetype-config
 CONFIGURE_ENV+=		FREETYPE_CONFIG=${FREETYPE_CONFIG:Q}
 
+.include "../../archivers/bzip2/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .endif # FREETYPE2_BUILDLINK3_MK
 
