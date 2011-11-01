37f3b6d4417fe745c6352b67077e30e54401daf3
sbd <sbd>
2011-11-01 23:18:23 +0000

    Revert hard coded "py26" with "${PYPKGPREFIX}".
    "${PYPKGPREFIX}" was accidentally replace with "py26" by blbump when I
    did the recursive bump for graphics/freetype2 and I forgot to change them
    back :(.

--- x11/py-vte/buildlink3.mk
+++ x11/py-vte/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/11/01 06:03:06 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/11/01 23:18:24 sbd Exp $
 
 BUILDLINK_TREE+=	py-vte
 
@@ -8,7 +8,7 @@ PY_VTE_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.py-vte+=	${PYPKGPREFIX}-vte>=0.26.0
-BUILDLINK_ABI_DEPENDS.py-vte?=	py26-vte>=0.28.1nb2
+BUILDLINK_ABI_DEPENDS.py-vte?=	${PYPKGPREFIX}--vte>=0.28.1nb2
 BUILDLINK_PKGSRCDIR.py-vte?=	../../x11/vte
 
 .include "../../x11/vte/buildlink3.mk"
