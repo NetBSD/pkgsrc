57bb46c91dd57804a24008d351c3f91c9329d955
scottr <scottr>
2011-11-01 21:08:55 +0000

    Update viewvc package to 1.1.11.
    Significant changes from version 1.0 include:
    
      * Pluggable, Python-based authorization subsystem
      * Support for path-based authorization, including parsing and honoring
        Subversion authz configuration files
      * Support for per-virtual host and per-root configurations
      * Template improvements

--- www/viewvc/options.mk
+++ www/viewvc/options.mk
@@ -1,11 +1,10 @@
-# $NetBSD: options.mk,v 1.1.1.1 2006/10/21 09:05:44 adrianp Exp $
+# $NetBSD: options.mk,v 1.2 2011/11/01 21:08:55 scottr Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.viewvc
 
-PKG_SUPPORTED_OPTIONS=	cvsgraph viewvc-php
+PKG_SUPPORTED_OPTIONS=	cvsgraph
 
 .include "../../mk/bsd.options.mk"
-.include "../../lang/php/phpversion.mk"
 
 ###
 ### Use cvsgraph for printing pretty little pictures
@@ -15,14 +14,4 @@ DEPENDS+=		cvsgraph-[0-9]*:../../devel/cvsgraph
 CVSGRAPH_OPT=		1
 .else
 CVSGRAPH_OPT=		0
-.endif
-
-###
-### Use php to colorize .php and .inc files
-###
-.if !empty(PKG_OPTIONS:Mviewvc-php)
-.include "${PHPPKGSRCDIR}/buildlink3.mk"
-PHP_OPT=		1
-.else
-PHP_OPT=		0
 .endif
