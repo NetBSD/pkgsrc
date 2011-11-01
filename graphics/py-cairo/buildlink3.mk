cb028af0bef9cc6c9cadbbe423f3edb10d6d6ba1
tron <tron@pkgsrc.org>
2011-11-01 11:48:58 +0000

    Replace hard coded "py26" with "${PYPKGPREFIX}" to make this build with Python 2.7 again.

--- graphics/py-cairo/buildlink3.mk
+++ graphics/py-cairo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2011/11/01 06:01:43 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2011/11/01 11:48:58 tron Exp $
 
 BUILDLINK_TREE+=	pycairo
 
@@ -8,7 +8,7 @@ PY_CAIRO_BUILDLINK3_MK:=
 .  include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.pycairo+=	${PYPKGPREFIX}-cairo>=1.0.2
-BUILDLINK_ABI_DEPENDS.pycairo+=	py26-cairo>=1.8.10nb2
+BUILDLINK_ABI_DEPENDS.pycairo+=	${PYPKGPREFIX}-cairo>=1.8.10nb2
 BUILDLINK_PKGSRCDIR.pycairo?=	../../graphics/py-cairo
 
 .include "../../graphics/cairo/buildlink3.mk"
