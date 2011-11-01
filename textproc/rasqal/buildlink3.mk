4d2878f743012c60ff297571afebbba5bee9f4d1
sbd <sbd@pkgsrc.org>
2011-11-01 06:56:34 +0000

    Add missing devel/gmp and math/mpfr buildlinks
    Bump PKGREVISION

--- textproc/rasqal/buildlink3.mk
+++ textproc/rasqal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2011/04/22 13:42:11 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2011/11/01 06:56:34 sbd Exp $
 
 BUILDLINK_TREE+=	rasqal
 
@@ -9,6 +9,8 @@ BUILDLINK_API_DEPENDS.rasqal+=	rasqal>=0.9.17
 BUILDLINK_ABI_DEPENDS.rasqal?=	rasqal>=0.9.21nb1
 BUILDLINK_PKGSRCDIR.rasqal?=	../../textproc/rasqal
 
+.include "../../devel/gmp/buildlink3.mk"
+.include "../../math/mpfr/buildlink3.mk"
 .include "../../textproc/raptor/buildlink3.mk"
 .include "../../devel/pcre/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
