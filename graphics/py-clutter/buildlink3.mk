a029bf499a38ae6e4fa654d48038918a3d247ff7
sbd <sbd@pkgsrc.org>
2011-11-01 23:18:23 +0000

    Revert hard coded "py26" with "${PYPKGPREFIX}".
    "${PYPKGPREFIX}" was accidentally replace with "py26" by blbump when I
    did the recursive bump for graphics/freetype2 and I forgot to change them
    back :(.

--- graphics/py-clutter/buildlink3.mk
+++ graphics/py-clutter/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2011/11/01 06:01:43 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2011/11/01 23:18:23 sbd Exp $
 #
 
 BUILDLINK_TREE+=	py-clutter
@@ -9,7 +9,7 @@ PY_CLUTTER_BUILDLINK3_MK:=
 .  include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.py-clutter+=	${PYPKGPREFIX}-clutter>=1.0.0
-BUILDLINK_ABI_DEPENDS.py-clutter?=	py26-clutter>=1.0.2nb7
+BUILDLINK_ABI_DEPENDS.py-clutter?=	${PYPKGPREFIX}-clutter>=1.0.2nb7
 BUILDLINK_PKGSRCDIR.py-clutter?=	../../graphics/py-clutter
 
 .include "../../graphics/clutter/buildlink3.mk"
--- sysutils/py-notify/buildlink3.mk
+++ sysutils/py-notify/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2011/11/01 06:02:35 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2011/11/01 23:18:23 sbd Exp $
 
 BUILDLINK_TREE+=	py-notify
 
@@ -8,7 +8,7 @@ PY_NOTIFY_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.py-notify+=	${PYPKGPREFIX}-notify>=0.1.1
-BUILDLINK_ABI_DEPENDS.py-notify?=	py26-notify>=0.1.1nb9
+BUILDLINK_ABI_DEPENDS.py-notify?=	${PYPKGPREFIX}-notify>=0.1.1nb9
 BUILDLINK_PKGSRCDIR.py-notify?=	../../sysutils/py-notify
 
 .include "../../x11/py-gtk2/buildlink3.mk"
