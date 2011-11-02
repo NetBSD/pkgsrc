1e54e7a651a63458385f6222713353fb4bef314f
obache <obache@pkgsrc.org>
2011-11-02 00:16:07 +0000

    Revert hard coded "ruby19" with ${RUBYPKGPREFIX}.
    It was accidenally replaced by recursive bump for graphics/freetype2.

--- devel/ruby-gnome2-pango/buildlink3.mk
+++ devel/ruby-gnome2-pango/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2011/11/01 06:01:04 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2011/11/02 00:16:07 obache Exp $
 
 BUILDLINK_TREE+=	ruby-gnome2-pango
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ruby-gnome2-pango
 RUBY_GNOME2_PANGO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-pango+= ${RUBY_PKGPREFIX}-gnome2-pango>=0.16.0
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-pango?= ruby19-gnome2-pango>=1.0.3nb1
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-pango?= ${RUBY_PKGPREFIX}-gnome2-pango>=1.0.3nb1
 BUILDLINK_PKGSRCDIR.ruby-gnome2-pango?=	../../devel/ruby-gnome2-pango
 
 .include "../../devel/ruby-gnome2-glib/buildlink3.mk"
--- x11/ruby-gnome2-gtk/buildlink3.mk
+++ x11/ruby-gnome2-gtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2011/11/01 06:03:09 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2011/11/02 00:16:08 obache Exp $
 #
 
 BUILDLINK_TREE+=	ruby-gnome2-gtk
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	ruby-gnome2-gtk
 RUBY_GNOME2_GTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-gtk+=	${RUBY_PKGPREFIX}-gnome2-gtk>=0.17.0
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk?=	ruby19-gnome2-gtk>=1.0.3nb1
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk?=	${RUBY_PKGPREFIX}-gnome2-gtk>=1.0.3nb1
 BUILDLINK_PKGSRCDIR.ruby-gnome2-gtk?=	../../x11/ruby-gnome2-gtk
 
 .include "../../devel/ruby-gnome2-glib/buildlink3.mk"
