751155525cf60de9d3caa7a45efc48fb9899c163
tcort <tcort@pkgsrc.org>
2011-11-05 17:10:04 +0000

    Define the tools available on the Minix platform.

---
+++ mk/tools/tools.Minix.mk
@@ -0,0 +1,65 @@
+# $NetBSD: tools.Minix.mk,v 1.1 2011/11/05 17:10:04 tcort Exp $
+#
+# System-supplied tools for the Minix operating system.
+
+#System supplied tools
+TOOLS_PLATFORM.[?=		/bin/[
+TOOLS_PLATFORM.awk?=		/usr/bin/awk
+TOOLS_PLATFORM.basename?=	/usr/bin/basename
+TOOLS_PLATFORM.bzcat?=		/usr/bin/bzcat
+TOOLS_PLATFORM.cat?=		/bin/cat
+TOOLS_PLATFORM.bzip2?=		/usr/bin/bzip2
+TOOLS_PLATFORM.chgrp?=		/usr/bin/chgrp
+TOOLS_PLATFORM.chmod?=		/usr/bin/chmod
+TOOLS_PLATFORM.chown?=		/usr/bin/chown
+TOOLS_PLATFORM.cmp?=		/usr/bin/cmp
+TOOLS_PLATFORM.cp?=		/bin/cp 
+TOOLS_PLATFORM.cut?=		/usr/bin/cut
+TOOLS_PLATFORM.date?=		/bin/date
+TOOLS_PLATFORM.dirname?=	/usr/bin/dirname
+TOOLS_PLATFORM.echo?=		/bin/echo
+TOOLS_PLATFORM.env?=		/usr/bin/env
+TOOLS_PLATFORM.expr?=		/bin/expr
+TOOLS_PLATFORM.false?=		/bin/false
+TOOLS_PLATFORM.file?=		/usr/bin/file
+TOOLS_PLATFORM.find?=		/usr/bin/find
+TOOLS_PLATFORM.flex?=		/usr/bin/flex
+TOOLS_PLATFORM.head?=		/usr/bin/head
+TOOLS_PLATFORM.id?=		/usr/bin/id
+TOOLS_PLATFORM.install?=	/bin/install
+TOOLS_PLATFORM.lex?=		/usr/bin/lex
+TOOLS_PLATFORM.ln?=		/bin/ln
+TOOLS_PLATFORM.ls?=		/bin/ls
+TOOLS_PLATFORM.mail?=		/usr/bin/mail
+TOOLS_PLATFORM.mkdir?=		/bin/mkdir -p
+TOOLS_PLATFORM.mv?=		/bin/mv
+TOOLS_PLATFORM.nice?=		/usr/bin/nice
+TOOLS_PLATFORM.nroff?=		/usr/bin/nroff
+TOOLS_PLATFORM.pwd?=		/bin/pwd
+TOOLS_PLATFORM.rm?=		/bin/rm
+TOOLS_PLATFORM.rmdir?=		/usr/bin/rmdir
+TOOLS_PLATFORM.sed?=		/bin/sed
+TOOLS_PLATFORM.sh?=		/bin/sh
+TOOLS_PLATFORM.sleep?=		/usr/bin/sleep
+TOOLS_PLATFORM.strip?=		/usr/bin/strip
+TOOLS_PLATFORM.tail?=		/usr/bin/tail
+TOOLS_PLATFORM.tar?=		/usr/bin/tar
+TOOLS_PLATFORM.bsdtar?=		/usr/bin/bsdtar
+TOOLS_PLATFORM.fetch?=		/usr/bin/fetch
+TOOLS_PLATFORM.tee?=		/usr/bin/tee
+TOOLS_PLATFORM.test?=		/bin/test
+TOOLS_PLATFORM.tr?=		/usr/bin/tr
+TOOLS_PLATFORM.true?=		/bin/true
+TOOLS_PLATFORM.tsort?=		/usr/bin/tsort
+TOOLS_PLATFORM.wc?=		/usr/bin/wc
+TOOLS_PLATFORM.xargs?=		/usr/bin/xargs
+TOOLS_PLATFORM.yacc?=		/usr/bin/yacc
+TOOLS_PLATFORM.grep?=		/usr/bin/grep
+TOOLS_PLATFORM.egrep?=		/usr/bin/egrep
+TOOLS_PLATFORM.fgrep?=		/usr/bin/fgrep
+TOOLS_PLATFORM.printf?=		/usr/bin/printf
+TOOLS_PLATFORM.touch?=		/usr/bin/touch
+TOOLS_PLATFORM.hostname?=	/usr/bin/hostname
+TOOLS_PLATFORM.patch?=		/usr/bin/patch
+TOOLS_PLATFORM.diff?=		/usr/bin/diff
+TOOLS_PLATFORM.sort?=		/usr/bin/sort
