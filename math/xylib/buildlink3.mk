8371df6e0497820a49683cde5403b6258194b77b
sbd <sbd@pkgsrc.org>
2011-11-05 08:07:12 +0000

    Add missing zlib and bzip2 buildlinks
    Bump PKGREVISION and Update BUILDLINK_ABI_DEPENDS.xylib

--- math/xylib/buildlink3.mk
+++ math/xylib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/12/12 13:58:58 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/11/05 08:07:13 sbd Exp $
 
 BUILDLINK_TREE+=	xylib
 
@@ -6,7 +6,11 @@ BUILDLINK_TREE+=	xylib
 XYLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xylib+=	xylib>=0.4
+BUILDLINK_ABI_DEPENDS.xylib?=	xylib>=0.8nb1
 BUILDLINK_PKGSRCDIR.xylib?=	../../math/xylib
+
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
 
 .endif	# XYLIB_BUILDLINK3_MK
 
