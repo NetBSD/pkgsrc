9bdb102e00e6a3edd80df643c1fad70a21cec5d4
obache <obache>
2011-11-06 07:49:38 +0000

    Update to mikutter to 0.0.4.571.
    switch to new unstable branch.

--- net/mikutter/patches/patch-core_mui_cairo__inner__tl.rb
+++ net/mikutter/patches/patch-core_mui_cairo__inner__tl.rb
@@ -1,8 +1,8 @@
-$NetBSD: patch-core_mui_cairo__inner__tl.rb,v 1.2 2011/06/19 04:19:56 obache Exp $
+$NetBSD: patch-core_mui_cairo__inner__tl.rb,v 1.3 2011/11/06 07:49:38 obache Exp $
 
 * use system bseach library instead of included one.
 
---- core/mui/cairo_inner_tl.rb.orig	2011-06-15 17:42:27.000000000 +0000
+--- core/mui/cairo_inner_tl.rb.orig	2011-09-03 16:58:10.000000000 +0000
 +++ core/mui/cairo_inner_tl.rb
 @@ -1,7 +1,7 @@
  # -*- coding: utf-8 -*-
@@ -10,6 +10,6 @@ $NetBSD: patch-core_mui_cairo__inner__tl.rb,v 1.2 2011/06/19 04:19:56 obache Exp
  miquire :mui, 'timeline'
 -miquire :lib, 'ruby-bsearch-1.5/bsearch'
 +miquire :lib, 'bsearch'
+ miquire :lib, 'uithreadonly'
  
  class Gtk::TimeLine::InnerTL < Gtk::CRUD
-   attr_accessor :postbox, :hp
