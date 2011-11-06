28ddd6929814ec658b8ce9029636451bd019af6d
obache <obache@pkgsrc.org>
2011-11-06 07:49:38 +0000

    Update to mikutter to 0.0.4.571.
    switch to new unstable branch.

--- net/mikutter/patches/patch-core_utils.rb
+++ net/mikutter/patches/patch-core_utils.rb
@@ -1,13 +1,13 @@
-$NetBSD: patch-core_utils.rb,v 1.1 2011/06/19 04:19:56 obache Exp $
+$NetBSD: patch-core_utils.rb,v 1.2 2011/11/06 07:49:38 obache Exp $
 
 * use included escape library instead of system one.
 
---- core/utils.rb.orig	2011-06-12 14:01:26.000000000 +0000
+--- core/utils.rb.orig	2011-10-02 11:08:04.000000000 +0000
 +++ core/utils.rb
-@@ -83,7 +83,7 @@ def file_or_directory_require(rb)
-     require rb end end
- 
- Dir::chdir(File::dirname(__FILE__))
+@@ -35,7 +35,7 @@ if RUBY_VERSION >= '1.9.2'
+     $LOAD_PATH.push(File.expand_path(File.join(Dir.pwd, path)))
+   }
+ end
 -miquire :lib, 'escape'
 +miquire :lib, 'miku/escape'
  miquire :lib, 'lazy'
