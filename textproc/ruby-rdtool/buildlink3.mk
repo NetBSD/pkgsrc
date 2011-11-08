ca88d81f2c6107c93cc36361e967a04ab8ebbf86
taca <taca@pkgsrc.org>
2011-11-08 15:30:44 +0000

    Remove (unused) comment outed RUBY_RD_LIST.

--- textproc/ruby-rdtool/buildlink3.mk
+++ textproc/ruby-rdtool/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2010/09/10 08:35:16 taca Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2011/11/08 15:30:44 taca Exp $
 
 BUILDLINK_TREE+=	${RUBY_PKGPREFIX}-rdtool
 
@@ -8,9 +8,6 @@ RUBY_RDTOOL_BUILDLINK3_MK:=
 RUBY_RD=		rd2
 RUBY_RD_VERSION =	0.6.18
 RUBY_RD_REQD=		0.6.14
-
-# create string for dependency list
-#_RUBY_RD_LIST:=	${RUBY_VERSION_LIST:C/([1-9][0-9]*)/ruby&-rdtool/g:ts,}
 
 BUILDLINK_FILES.${RUBY_PKGPREFIX}-rdtool+=	bin/rd2
 BUILDLINK_DEPMETHOD.${RUBY_PKGPREFIX}-rdtool?=	build
