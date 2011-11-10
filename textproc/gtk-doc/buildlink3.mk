0443724c6c5469af34a8f85db5202926e9544d59
abs <abs@pkgsrc.org>
2011-11-10 09:42:48 +0000

    The latest gtk-doc does not create .devhelp files any more - bump buildlink3 required version to ensure PLISTs match

--- textproc/gtk-doc/buildlink3.mk
+++ textproc/gtk-doc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2011/02/27 15:14:32 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2011/11/10 09:42:48 abs Exp $
 #
 # provided mostly for pkg-config
 
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	gtk-doc
 .if !defined(GTK_DOC_BUILDLINK3_MK)
 GTK_DOC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gtk-doc+=	gtk-doc>=1.4nb1
+BUILDLINK_API_DEPENDS.gtk-doc+=	gtk-doc>=1.18
 BUILDLINK_PKGSRCDIR.gtk-doc?=	../../textproc/gtk-doc
 
 BUILDLINK_DEPMETHOD.gtk-doc?=	build
