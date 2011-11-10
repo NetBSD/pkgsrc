81ec32c1a8aa5eec068d94f659a454c3bab6c9a6
obache <obache@pkgsrc.org>
2011-11-10 10:40:11 +0000

    Update libhangul to 0.1.0 (shlib major bumped). approved by minskim@.
    0.1.0
     * add new API for keycode normalization
     * remove deprecated API
     * add supports for freebsd
     * update some test codes

--- inputmethod/libhangul/buildlink3.mk
+++ inputmethod/libhangul/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:24:47 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/11/10 10:40:11 obache Exp $
 
 BUILDLINK_TREE+=	libhangul
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	libhangul
 LIBHANGUL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libhangul+=	libhangul>=0.0.4
+BUILDLINK_ABI_DEPENDS.libhangul+=	libhangul>=0.1.0
 BUILDLINK_PKGSRCDIR.libhangul?=	../../inputmethod/libhangul
 .endif # LIBHANGUL_BUILDLINK3_MK
 
