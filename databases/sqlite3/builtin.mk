39a9b745631d332bf1a2a15654a012e4192ba961
wiz <wiz@pkgsrc.org>
2011-11-10 17:18:10 +0000

    Fix pkg-config file handling based on solution in alsa-lib. Suggested by reed@

--- databases/sqlite3/builtin.mk
+++ databases/sqlite3/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.1 2011/11/09 01:30:19 jnemeth Exp $
+# $NetBSD: builtin.mk,v 1.2 2011/11/10 17:18:10 wiz Exp $
 
 BUILTIN_PKG:=	sqlite3
 
@@ -6,3 +6,18 @@ PKGCONFIG_FILE.sqlite3=	/usr/lib/pkgconfig/sqlite3.pc
 PKGCONFIG_BASE.sqlite3=	/usr
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
+
+###
+### The section below only applies if we are not including this file
+### solely to determine whether a built-in implementation exists.
+###
+
+CHECK_BUILTIN.sqlite3?=      no
+.if !empty(CHECK_BUILTIN.sqlite3:M[nN][oO])
+
+.  if !empty(USE_BUILTIN.sqlite3:M[yY][eE][sS])
+BUILDLINK_PREFIX.sqlite3=    /usr
+BUILDLINK_FILES.sqlite3+=    lib/pkgconfig/sqlite3.pc
+.  endif
+
+.endif  # CHECK_BUILTIN.sqlite3
