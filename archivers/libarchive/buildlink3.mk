007c37d7ab3c5400cb11d2d4913e09c91aea94c9
sbd <sbd@pkgsrc.org>
2011-11-13 08:11:28 +0000

    Don't include dependence buildlink3 if using the builtin library.
    Patch by obache@

--- archivers/libarchive/buildlink3.mk
+++ archivers/libarchive/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2011/11/01 06:50:28 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2011/11/13 08:11:28 sbd Exp $
 
 BUILDLINK_TREE+=	libarchive
 
@@ -9,9 +9,16 @@ BUILDLINK_API_DEPENDS.libarchive+=	libarchive>=2.2
 BUILDLINK_ABI_DEPENDS.libarchive?=	libarchive>=2.8.4nb1
 BUILDLINK_PKGSRCDIR.libarchive?=	../../archivers/libarchive
 
+CHECK_BUILTIN.libarchive:=     yes
+.include "../../archivers/libarchive/builtin.mk"
+CHECK_BUILTIN.libarchive:=     no
+
+# A built-in libarchive is always using built-in zlib, bzip2, and xz.
+.if empty(USE_BUILTIN.libarchive:M[yY][eE][sS])
 .include "../../archivers/bzip2/buildlink3.mk"
 .include "../../archivers/xz/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
+.endif
 .endif # LIBARCHIVE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libarchive
