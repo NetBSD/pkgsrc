958f1eb5f99590a5a25b81d55d48323fbfa8cf01
sbd <sbd@pkgsrc.org>
2011-11-16 01:33:15 +0000

    Recursive bump for multimedia/ffmpeg buildlink addition.

--- multimedia/vlc/buildlink3.mk
+++ multimedia/vlc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.35 2011/11/01 06:02:15 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2011/11/16 01:33:16 sbd Exp $
 
 BUILDLINK_TREE+=	vlc
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	vlc
 VLC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.vlc?=	vlc>=1.0.0
-BUILDLINK_ABI_DEPENDS.vlc?=	vlc>=1.1.12nb1
+BUILDLINK_ABI_DEPENDS.vlc?=	vlc>=1.1.12nb3
 BUILDLINK_PKGSRCDIR.vlc?=	../../multimedia/vlc
 
 pkgbase:= vlc
