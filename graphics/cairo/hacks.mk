f8cb32fa3dc94fcde055d1e39e892d56d2e6872d
minskim <minskim@pkgsrc.org>
2011-11-16 03:45:03 +0000

    Use a stricter condition to enable the hack.

--- graphics/cairo/hacks.mk
+++ graphics/cairo/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2011/11/16 03:13:28 minskim Exp $
+# $NetBSD: hacks.mk,v 1.2 2011/11/16 03:45:03 minskim Exp $
 
 .if !defined(CAIRO_HACKS_MK)
 CAIRO_HACKS_MK=	defined
@@ -9,7 +9,7 @@ CAIRO_HACKS_MK=	defined
 ###
 .if ${OPSYS} == "Darwin" && !empty(PKGSRC_COMPILER:Mgcc)
 _BAD_GCC_BUILD!=	${CC} --version | (${GREP} -c 'i686-apple-darwin11-llvm-gcc-4\.2\ .*build\ 5658' || ${TRUE})
-.  if ${_BAD_GCC_BUILD} == "1"
+.  if ${_BAD_GCC_BUILD} == "1" && exists(/usr/bin/gcc-4.2)
 PKG_HACKS+=	llvm-link
 CONFIGURE_ENV+=	ac_cv_prog_CC=gcc-4.2
 .  endif
