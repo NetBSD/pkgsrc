c17265a46bb58a914d7e9ef472741b1479d92a46
dholland <dholland>
2011-11-20 01:16:23 +0000

    Apply the proper buildlink prefix for gtk2 instead of hardwiring /usr/pkg.

--- graphics/darktable/patches/patch-cmake_modules_FindGTK2.cmake
+++ graphics/darktable/patches/patch-cmake_modules_FindGTK2.cmake
@@ -1,6 +1,11 @@
-$NetBSD: patch-cmake_modules_FindGTK2.cmake,v 1.2 2011/08/25 14:42:53 jakllsch Exp $
+$NetBSD: patch-cmake_modules_FindGTK2.cmake,v 1.3 2011/11/20 01:16:23 dholland Exp $
 
-Look for GTK where we might find it.  Should honor ${PREFIX} someday.
+Look for GTK in the right place.
+
+Caution: this patch inserts a substitution of PREFIX that is expanded
+from the pkgsrc makefile at pre-configure time. If regenerating this
+patch, make sure to do it in a tree that hasn't been configured or
+built to avoid permanently expanding the substitution.
 
 --- cmake/modules/FindGTK2.cmake.orig	2011-08-24 09:20:29.000000000 +0000
 +++ cmake/modules/FindGTK2.cmake
@@ -8,9 +13,9 @@ Look for GTK where we might find it.  Should honor ${PREFIX} someday.
              /usr/openwin/lib
              /sw/include
              /sw/lib
-+            /usr/pkg/include/glib
-+            /usr/pkg/include
-+            /usr/pkg/lib
++            @PREFIX@/include/glib
++            @PREFIX@/include
++            @PREFIX@/lib
              $ENV{GTKMM_BASEPATH}/include
              $ENV{GTKMM_BASEPATH}/lib
              [HKEY_CURRENT_USER\\SOFTWARE\\gtkmm\\2.4;Path]/include
