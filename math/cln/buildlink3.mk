8b9e55f3c93ee8825b06341a5064405eff6ec2d6
sbd <sbd@pkgsrc.org>
2011-11-20 05:40:27 +0000

    Add missing devel/gmp buildlink.
    Gmp is listed as a prerequisite in the INSTALL file.
    
    Bump PKGREVISION

--- math/cln/buildlink3.mk
+++ math/cln/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2009/09/09 17:29:01 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2011/11/20 05:40:27 sbd Exp $
 
 BUILDLINK_TREE+=	cln
 
@@ -6,8 +6,10 @@ BUILDLINK_TREE+=	cln
 CLN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cln+=		cln>=1.1.6
-BUILDLINK_ABI_DEPENDS.cln+=	cln>=1.3.0
+BUILDLINK_ABI_DEPENDS.cln+=	cln>=1.3.2nb1
 BUILDLINK_PKGSRCDIR.cln?=	../../math/cln
+
+.include "../../devel/gmp/buildlink3.mk"
 .endif # CLN_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-cln
