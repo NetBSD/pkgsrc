60cb660baa30b04543c8e696100c9a749ab28238
joerg <joerg@pkgsrc.org>
2011-11-25 21:25:26 +0000

    Update to Botan 1.8.13:
    * 1.8.13, 2011-07-02
     - A race in Algorithm_Factory that could cause crashes in multithreaded
       code has been fixed.
    
    * 1.8.12, 2011-06-20
     - If EMSA3(Raw) was used for more than one signature, it would produce
       incorrect output.
     - Fix the --enable-debug option to configure.py
     - Improve OS detection on Cygwin
     - Fix compilation under Sun Studio 12 on Solaris
     - Fix a memory leak in the constructors of DataSource_Stream and
       DataSink_Stream which would occur if opening the file failed. PR 144
    
    * 1.8.11, 2010-11-02
     - Fix a number of CRL encoding and decoding bugs
     - When building a debug library under VC++, use the debug runtime
     - Fix compilation under Sun Studio on Linux and Solaris
     - Add several functions for compatability with 1.9
     - In the examples, read most input files as binary
     - The Perl build script has been removed in this release
    
    * 1.8.10, 2010-08-31
     - Switch default PKCS #8 encryption algorithm from 3DES to AES-256
     - Increase default hash iterations from 2048 to 10000 in PBES1 and
    PBES2
     - Use small tables in the first round of AES
     - Add PBKDF typedef and get_pbkdf for better compatability with 1.9
     - Add version of S2K::derive_key taking salt and iteration count
     - Enable the /proc-walking entropy source on NetBSD
     - Fix the doxygen makefile target
    
    * 1.8.9, 2010-06-16
     - Use constant time multiplication in IDEA
     - Avoid possible timing attack against OAEP decoding
     - Add new X509::BER_encode and PKCS8::BER_encode
     - Enable DLL builds under Windows
     - Add Win32 installer support
     - Add support for the Clang compiler
     - Fix problem in semcem.h preventing build under Clang or GCC 3.4
     - Fix bug that prevented creation of DSA groups under 1024 bits
     - Fix crash in GMP_Engine if library is shutdown and reinitialized
     - Work around problem with recent binutils in x86-64 SHA-1
     - The Perl build script is no longer supported and refuses to run by
       default
    
    * 1.8.8, 2009-11-03
     - Alter Skein-512 to match the tweaked 1.2 specification
     - Fix use of inline asm for access to x86 bswap function
     - Allow building the library without AES enabled
     - Add 'powerpc64' alias to ppc64 arch for Gentoo ebuild

--- security/botan/buildlink3.mk
+++ security/botan/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/09/23 16:03:25 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2011/11/25 21:25:26 joerg Exp $
 
 BUILDLINK_TREE+=	botan
 
 .if !defined(BOTAN_BUILDLINK3_MK)
 BOTAN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.botan+=	botan>=1.8.7
+BUILDLINK_API_DEPENDS.botan+=	botan>=1.8.13
 BUILDLINK_PKGSRCDIR.botan?=	../../security/botan
 .endif # BOTAN_BUILDLINK3_MK
 
