179646089d373f17d5a015810f7c77292bf987db
joerg <joerg>
2011-11-25 21:59:57 +0000

    Fix build with modern GCC.

---
+++ ham/fl_logbook/patches/patch-src_qso__db.cpp
@@ -0,0 +1,9 @@
+$NetBSD: patch-src_qso__db.cpp,v 1.1 2011/11/25 21:59:57 joerg Exp $
+
+--- src/qso_db.cpp.orig	2011-11-25 17:52:09.000000000 +0000
++++ src/qso_db.cpp
+@@ -1,3 +1,4 @@
++#include <cstring>
+ #include <stdlib.h>
+ #include <ctype.h>
+ #include "qso_db.h"
