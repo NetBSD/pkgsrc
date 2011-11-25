6e2a83f5716ea0d521150bd502890d327e5f22a8
cheusov <cheusov@pkgsrc.org>
2011-11-25 09:46:02 +0000

    URL to a bug report to upstream was added to the patch.

--- devel/py-subvertpy/patches/patch-subvertpy_wc.c
+++ devel/py-subvertpy/patches/patch-subvertpy_wc.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-subvertpy_wc.c,v 1.1 2011/11/24 22:32:01 cheusov Exp $
+$NetBSD: patch-subvertpy_wc.c,v 1.2 2011/11/25 09:46:02 cheusov Exp $
 
-# PR 45649
+# PR 45649, https://bugs.launchpad.net/subvertpy/+bug/894659
 --- subvertpy/wc.c.orig	2011-10-26 00:23:01.000000000 +0000
 +++ subvertpy/wc.c
 @@ -27,6 +27,7 @@
