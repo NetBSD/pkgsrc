a845dc8cc5b48e27b339170b8ac8c9d21bef2cdf
hans <hans@pkgsrc.org>
2011-11-26 14:58:45 +0000

    Add flex.mk, based on texinfo.mk, to check the version of the platform-provided flex. Packages can set FLEX_REQD to require a minimum flex version.

--- mk/tools/bsd.tools.mk
+++ mk/tools/bsd.tools.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.tools.mk,v 1.50 2009/03/17 22:13:36 rillig Exp $
+# $NetBSD: bsd.tools.mk,v 1.51 2011/11/26 14:58:45 hans Exp $
 #
 # Copyright (c) 2005, 2006 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -151,6 +151,7 @@ post-tools:
 .include "autoconf.mk"
 .include "texinfo.mk"
 .include "gettext.mk"
+.include "flex.mk"
 .include "intltool.mk"
 .include "ldconfig.mk"
 .include "rpcgen.mk"
