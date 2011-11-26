bba5bdec1d68581b3e490dc57a66191539b626e3
joerg <joerg@pkgsrc.org>
2011-11-26 17:21:10 +0000

    Fix build with newer GCC

---
+++ textproc/xapian/patches/patch-common_safeuuid.h
@@ -0,0 +1,12 @@
+$NetBSD: patch-common_safeuuid.h,v 1.1 2011/11/26 17:21:10 joerg Exp $
+
+--- common/safeuuid.h.orig	2011-11-26 02:56:21.000000000 +0000
++++ common/safeuuid.h
+@@ -54,6 +54,7 @@ inline void uuid_unparse_lower(uuid_t uu
+ 
+ // FreeBSD/NetBSD UUID API.
+ # include <uuid.h>
++# include <cstdlib>
+ # include <cstring>
+ # include <exception>
+ 
