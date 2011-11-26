1257fcd22cf7af4eb4b489907663b79b8a754f6a
sbd <sbd>
2011-11-26 06:08:47 +0000

    Add missing devel/zlib buildlink.
    Bump PKGREVISION

--- devel/exempi/buildlink3.mk
+++ devel/exempi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:12 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/11/26 06:08:47 sbd Exp $
 
 BUILDLINK_TREE+=	exempi
 
@@ -6,8 +6,10 @@ BUILDLINK_TREE+=	exempi
 EXEMPI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.exempi+=	exempi>=1.99.9
+BUILDLINK_ABI_DEPENDS.exempi?=	exempi>=2.1.1nb2
 BUILDLINK_PKGSRCDIR.exempi?=	../../devel/exempi
 
+.include "../../devel/zlib/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
 .endif # EXEMPI_BUILDLINK3_MK
 
