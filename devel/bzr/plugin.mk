4219e466c0f3ecadedcdce47ffb816670a9dc2e8
cheusov <cheusov@pkgsrc.org>
2011-11-27 13:31:23 +0000

    Update to 2.4.2 from wip by Oleksander Kozhachuk. Tested on NetBSD current, Linux and Solaris-10.

--- devel/bzr/plugin.mk
+++ devel/bzr/plugin.mk
@@ -1,10 +1,10 @@
-# $NetBSD: plugin.mk,v 1.2 2009/06/14 22:57:59 joerg Exp $
+# $NetBSD: plugin.mk,v 1.3 2011/11/27 13:31:23 cheusov Exp $
 
 BZR_PLUGIN_NAME?=	${PKGBASE:S/^bzr-//}
 BZR_PLUGIN_TEST?=	yes
 BZR_PLUGIN_TEST_DIR?=	${WRKSRC}/build/lib/bzrlib/plugins
 
-DEPENDS+=		bzr>=1.6:../../devel/bzr
+DEPENDS+=		bzr>=2.4:../../devel/bzr
 
 # stolen from graphics/hicolor-icon-theme
 .include "../../mk/bsd.fast.prefs.mk"
