ab45f278aece5c2b4c2bf0cf1d739c17a5e3b664
obache <obache@pkgsrc.org>
2011-11-27 11:26:21 +0000

    Fixes PR pkg/45552 * installing GSettings schemas, so prepare it with framework in glib2. * fixes immodule cache path.
    Bump PKGREVISION.

--- x11/gtk3/modules.mk
+++ x11/gtk3/modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: modules.mk,v 1.2 2011/06/05 12:13:47 obache Exp $
+# $NetBSD: modules.mk,v 1.3 2011/11/27 11:26:21 obache Exp $
 #
 # This Makefile fragment is intended to be included by packages that install
 # GTK3 modules.  It takes care of rebuilding the corresponding databases at
@@ -21,7 +21,7 @@ GTK3_IMMODULES=		YES
 .if defined(GTK3_IMMODULES) && !empty(GTK3_IMMODULES:M[Yy][Ee][Ss])
 FILES_SUBST+=		GTK3_IMMODULES_DB=${GTK3_IMMODULES_DB:Q}
 FILES_SUBST+=		GTK3_QUERY_IMMODULES=${GTK3_QUERY_IMMODULES:Q}
-GTK3_IMMODULES_DB=	${BUILDLINK_PREFIX.gtk3}/libdata/gtk-3.0/gtk.immodules
+GTK3_IMMODULES_DB=	${BUILDLINK_PREFIX.gtk3}/lib/gtk-3.0/3.0.0/immodules.cache
 INSTALL_TEMPLATES+=	../../x11/gtk3/files/immodules.tmpl
 DEINSTALL_TEMPLATES+=	../../x11/gtk3/files/immodules.tmpl
 .endif
