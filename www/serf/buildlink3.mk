5d3f8d28523bfd0b2b4346b005c93755fae21d58
sbd <sbd@pkgsrc.org>
2011-11-27 05:42:50 +0000

    Add missing devel/zlib buildlink.
    Bump PKGREVISION

--- www/serf/buildlink3.mk
+++ www/serf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2010/01/17 12:02:50 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2011/11/27 05:42:50 sbd Exp $
 
 BUILDLINK_TREE+=	serf
 
@@ -6,11 +6,12 @@ BUILDLINK_TREE+=	serf
 SERF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.serf+=	serf>=0.1.0
-BUILDLINK_ABI_DEPENDS.serf?=	serf>=0.3.0nb1
+BUILDLINK_ABI_DEPENDS.serf?=	serf>=0.3.0nb2
 BUILDLINK_PKGSRCDIR.serf?=	../../www/serf
 
 .include "../../devel/apr/buildlink3.mk"
 .include "../../devel/apr-util/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
 .endif # SERF_BUILDLINK3_MK
 
