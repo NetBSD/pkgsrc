17bc30c04029f8433e5aaa6bb3930a804d4f29ff
hans <hans@pkgsrc.org>
2011-11-29 15:42:01 +0000

    Add libuuid dependency stuff from Makefile to buildlink3.mk.

--- net/gupnp/buildlink3.mk
+++ net/gupnp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2011/06/10 09:39:45 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2011/11/29 15:42:01 hans Exp $
 
 BUILDLINK_TREE+=	gupnp
 
@@ -8,6 +8,14 @@ GUPNP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gupnp+=	gupnp>=0.13.2
 BUILDLINK_ABI_DEPENDS.gupnp?=	gupnp>=0.13.3nb3
 BUILDLINK_PKGSRCDIR.gupnp?=	../../net/gupnp
+
+.include "../../mk/bsd.prefs.mk"
+.if !empty(OPSYS:M*BSD) || ${OPSYS} == "DragonFly"
+pre-configure:
+	cp ${FILESDIR}/uuid.pc ${BUILDLINK_DIR}/lib/pkgconfig/
+.else
+.include "../../devel/libuuid/buildlink3.mk"
+.endif
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../net/libsoup24/buildlink3.mk"
