dd720c1e0fb93fc37a4d5deaeb5d5e39489df0d3
is <is@pkgsrc.org>
2011-12-01 16:35:12 +0000

    Unfortunately, fltk13 changes the semantics of the scroll widget in ways not detectable by the compiler... so make a new package. TBD: This still misses cairo support.

---
+++ x11/fltk13/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/12/01 16:35:12 is Exp $
+
+BUILDLINK_TREE+=	fltk
+
+.if !defined(FLTK_BUILDLINK3_MK)
+FLTK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.fltk+=	fltk>=1.3.0
+BUILDLINK_PKGSRCDIR.fltk?=	../../x11/fltk
+BUILDLINK_FILES.fltk+=		include/Fl/*
+
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.endif # FLTK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-fltk
