3ecd4250ad0cb32a283c2be1258317fd339329f1
sbd <sbd@pkgsrc.org>
2011-12-02 07:13:28 +0000

    devel/libuuid is only needed for uuid support on Linux and SunOS.
    Bump PKGREVISION

--- devel/apr/buildlink3.mk
+++ devel/apr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.32 2009/03/20 19:24:09 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.33 2011/12/02 07:13:28 sbd Exp $
 
 BUILDLINK_TREE+=	apr
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	apr
 APR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.apr+=	apr>=1.2.8
+BUILDLINK_ABI_DEPENDS.apr?=	apr>=1.4.5nb3
 BUILDLINK_PKGSRCDIR.apr?=	../../devel/apr
 BUILDLINK_INCDIRS.apr?=		include/apr-1
 
@@ -16,6 +17,11 @@ ${BUILDLINK_DIR}/bin/apr-config: buildlink-directories
 	${MKDIR} ${BUILDLINK_DIR}/bin && ${LN} -fs apr-1-config ${BUILDLINK_DIR}/bin/apr-config
 
 buildlink-apr-cookie: ${BUILDLINK_DIR}/bin/apr-config
+
+.include "../../mk/bsd.prefs.mk"
+.if ${OPSYS} == "Linux" || ${OPSYS} == "SunOS"
+.include "../../devel/libuuid/buildlink3.mk"
+.endif
 .endif # APR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-apr
