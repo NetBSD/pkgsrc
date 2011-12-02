a972212abf10b149eaf9ff6ad785ea3f572ad0af
sbd <sbd@pkgsrc.org>
2011-12-02 07:13:48 +0000

    Recursive bump for devel/apr buildlink addition.

--- www/serf/buildlink3.mk
+++ www/serf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2011/11/27 05:42:50 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2011/12/02 07:13:48 sbd Exp $
 
 BUILDLINK_TREE+=	serf
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	serf
 SERF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.serf+=	serf>=0.1.0
-BUILDLINK_ABI_DEPENDS.serf?=	serf>=0.3.0nb2
+BUILDLINK_ABI_DEPENDS.serf?=	serf>=0.3.0nb3
 BUILDLINK_PKGSRCDIR.serf?=	../../www/serf
 
 .include "../../devel/apr/buildlink3.mk"
