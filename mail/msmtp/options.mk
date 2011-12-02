23b17dcf39bfce31abb21664c91ce5c8e69fc9b9
ryoon <ryoon@pkgsrc.org>
2011-12-02 12:33:53 +0000

    Update to 1.4.26
    * Introduce scripts option to install optional scripts.
    
    Changelog:
    Version 1.4.26:
    - A new version of the msmtpq script fixes serious bugs. To update to the new
      version of the script, you need to remove the old msmtpQ symlink, change
      msmtpQ to msmtpq in your MUA config, and use msmtp-queue for queue management.

--- mail/msmtp/options.mk
+++ mail/msmtp/options.mk
@@ -1,11 +1,11 @@
-# $NetBSD: options.mk,v 1.11 2010/07/06 07:33:43 obache Exp $
+# $NetBSD: options.mk,v 1.12 2011/12/02 12:33:53 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.msmtp
 
 PKG_OPTIONS_OPTIONAL_GROUPS=	ssl
 PKG_OPTIONS_GROUP.ssl=	gnutls ssl
 
-PKG_SUPPORTED_OPTIONS=	gnome-keyring gsasl idn inet6
+PKG_SUPPORTED_OPTIONS=	gnome-keyring gsasl idn inet6 scripts
 PKG_SUGGESTED_OPTIONS=	ssl
 
 .include "../../mk/bsd.options.mk"
@@ -56,4 +56,28 @@ CONFIGURE_ARGS+=	--with-gnome-keyring
 USE_TOOLS+=		pkg-config
 .else
 CONFIGURE_ARGS+=	--without-gnome-keyring
+.endif
+
+###
+### Install additional scripts
+###
+.if !empty(PKG_OPTIONS:Mscripts)
+CHECK_INTERPRETER_SKIP+=	share/msmtp/find_alias/find_alias_for_msmtp.sh \
+				share/msmtp/msmtpq/msmtp-queue \
+				share/msmtp/msmtpq/msmtpq \
+				share/msmtp/msmtp-gnome-tool/msmtp-gnome-tool.py \
+				share/msmtp/msmtpqueue/msmtp-enqueue.sh \
+				share/msmtp/msmtpqueue/msmtp-listqueue.sh \
+				share/msmtp/msmtpqueue/msmtp-runqueue.sh \
+				share/msmtp/set_sendmail/set_sendmail.sh
+PLIST.scripts=		yes
+USE_TOOLS+=		pax
+INSTALLATION_DIRS+=	share/msmtp
+install-msmtp-scripts:
+	cd ${WRKSRC}/scripts && \
+		pax -rw find_alias msmtp-gnome-tool msmtpq msmtpqueue \
+			set_sendmail vim \
+			${DESTDIR}${PREFIX}/share/msmtp
+.else
+install-msmtp-scripts:
 .endif
