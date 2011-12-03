7e5e5866fea80a2c301ba07dcd3416dd14831890
sbd <sbd@pkgsrc.org>
2011-12-03 03:44:38 +0000

    Add devel/libuuid buildlink on Linux and SunOS only.
    Bump PKGREVISION

--- textproc/xapian/buildlink3.mk
+++ textproc/xapian/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:33 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/12/03 03:44:38 sbd Exp $
 
 BUILDLINK_TREE+=	xapian
 
@@ -6,7 +6,16 @@ BUILDLINK_TREE+=	xapian
 XAPIAN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xapian+=	xapian>=1.0.7
+BUILDLINK_ABI_DEPENDS.xapian?=	xapian>=1.2.2nb1
 BUILDLINK_PKGSRCDIR.xapian?=	../../textproc/xapian
+
+.include "../../mk/bsd.prefs.mk"
+
+.if ${OPSYS} == "Linux" || ${OPSYS} == "SunOS"
+.include "../../devel/libuuid/buildlink3.mk"
+.endif
+.include "../../devel/zlib/buildlink3.mk"
+
 .endif # XAPIAN_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xapian
