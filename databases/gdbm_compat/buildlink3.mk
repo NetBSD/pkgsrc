461cb55fdba43724fceb7836ddc08c1d6d8279b9
sbd <sbd@pkgsrc.org>
2011-12-03 08:42:37 +0000

    gdbm_compat should have the same API and ABI depends as gdbm does.

--- databases/gdbm_compat/buildlink3.mk
+++ databases/gdbm_compat/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/10/17 08:44:32 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/12/03 08:42:37 sbd Exp $
 #
 
 BUILDLINK_TREE+=	gdbm_compat
@@ -6,7 +6,8 @@ BUILDLINK_TREE+=	gdbm_compat
 .if !defined(GDBM_COMPAT_BUILDLINK3_MK)
 GDBM_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gdbm_compat+=	gdbm_compat>=1.9.1
+BUILDLINK_API_DEPENDS.gdbm_compat+=	gdbm_compat>=1.8.3
+BUILDLINK_ABI_DEPENDS.gdbm_compat+=	gdbm_compat>=1.9.1
 BUILDLINK_PKGSRCDIR.gdbm_compat?=	../../databases/gdbm_compat
 
 .include "../../databases/gdbm/buildlink3.mk"
