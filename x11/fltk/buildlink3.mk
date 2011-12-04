a062d2c58eda916377098895b9dad72d1eb9c5c8
joerg <joerg@pkgsrc.org>
2011-12-04 00:23:49 +0000

    Merge, the upper limit is critical.

--- x11/fltk/buildlink3.mk
+++ x11/fltk/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.22 2011/12/01 15:46:33 is Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2011/12/04 00:23:49 joerg Exp $
 
 BUILDLINK_TREE+=	fltk
 
 .if !defined(FLTK_BUILDLINK3_MK)
 FLTK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fltk+=	fltk>=1.1.5rc1
-BUILDLINK_API_DEPENDS.fltk+=	fltk<1.3.0
+BUILDLINK_API_DEPENDS.fltk+=	fltk>=1.1.5rc1<1.3
 BUILDLINK_PKGSRCDIR.fltk?=	../../x11/fltk
 BUILDLINK_FILES.fltk+=		include/Fl/*
 
