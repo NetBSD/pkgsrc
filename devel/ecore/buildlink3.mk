1ff3c2a7a973e4ba4cd2034f0429ada4d9bf1754
joerg <joerg@pkgsrc.org>
2011-12-05 17:22:29 +0000

    Import ecore-1.1.0, derived from pkgsrc-wip/ecore:
    Ecore is the core event abstraction layer and X abstraction layer
    that makes doing selections, Xdnd, general X stuff, and event loops,
    timeouts and idle handlers fast, optimized, and convenient.
    It's a separate library so anyone can make use of the work put into Ecore
    to make this job easy for applications.

---
+++ devel/ecore/buildlink3.mk
@@ -0,0 +1,29 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/12/05 17:22:29 joerg Exp $
+
+BUILDLINK_TREE+=	ecore
+
+.if !defined(ECORE_BUILDLINK3_MK)
+ECORE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ecore+=	ecore>=1.1.0
+BUILDLINK_PKGSRCDIR.ecore?=	../../devel/ecore
+
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/eet/buildlink3.mk"
+.include "../../graphics/evas-buffer/buildlink3.mk"
+.include "../../graphics/evas-software-x11/buildlink3.mk"
+.include "../../graphics/evas/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
+.include "../../x11/libXScrnSaver/buildlink3.mk"
+.include "../../x11/libXcomposite/buildlink3.mk"
+.include "../../x11/libXcursor/buildlink3.mk"
+.include "../../x11/libXdamage/buildlink3.mk"
+.include "../../x11/libXinerama/buildlink3.mk"
+.include "../../x11/libXp/buildlink3.mk"
+.include "../../x11/libXrandr/buildlink3.mk"
+.include "../../x11/libXrender/buildlink3.mk"
+.include "../../x11/libXtst/buildlink3.mk"
+.endif # ECORE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ecore
