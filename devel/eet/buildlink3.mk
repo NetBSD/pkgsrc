d16a0872dc5f600ccf02720ecb00ea63fbcf44a8
joerg <joerg>
2011-12-05 17:20:14 +0000

    Update to eet-1.5.0. Includes various performance and memory foot print improvements.

--- devel/eet/buildlink3.mk
+++ devel/eet/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.15 2010/12/23 11:44:26 dsainty Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2011/12/05 17:20:14 joerg Exp $
 
 BUILDLINK_TREE+=	eet
 
 .if !defined(EET_BUILDLINK3_MK)
 EET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.eet+=	eet>=1.0.1
-BUILDLINK_ABI_DEPENDS.eet?=	eet>=1.2.3nb2
+BUILDLINK_API_DEPENDS.eet+=	eet>=1.5.0
 BUILDLINK_PKGSRCDIR.eet?=	../../devel/eet
 
 .include "../../devel/eina/buildlink3.mk"
