cc790b4616f498ace99d8a6f3a3b41dbb82535d1
joerg <joerg@pkgsrc.org>
2011-12-05 17:17:36 +0000

    Update to Embryo 1.1.0. No detailed changes available.

--- lang/embryo/buildlink3.mk
+++ lang/embryo/buildlink3.mk
@@ -1,12 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/06/07 19:54:02 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/12/05 17:17:36 joerg Exp $
 
 BUILDLINK_TREE+=	embryo
 
 .if !defined(EMBRYO_BUILDLINK3_MK)
 EMBRYO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.embryo+=	embryo>=0.9.5.060
+BUILDLINK_API_DEPENDS.embryo+=	embryo>=1.1.0
 BUILDLINK_PKGSRCDIR.embryo?=	../../lang/embryo
+
+.include "../../devel/eina/buildlink3.mk"
 .endif # EMBRYO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-embryo
