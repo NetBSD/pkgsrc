90098ccdfbb8892ea17b46f86a60f84881b2747d
dholland <dholland@pkgsrc.org>
2011-12-05 19:16:57 +0000

    Use motif.bl3.mk instead of direct from openmotif.
    Non-default option, so no revbump.

--- audio/snd/options.mk
+++ audio/snd/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2008/04/28 12:47:44 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2011/12/05 19:16:57 dholland Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.snd
 PKG_SUPPORTED_OPTIONS=	esound guile
@@ -32,7 +32,7 @@ CONFIGURE_ARGS+=	--without-guile
 
 .if !empty(PKG_OPTIONS:Mmotif)
 CONFIGURE_ARGS+=	--with-motif
-.include "../../x11/openmotif/buildlink3.mk"
+.include "../../mk/motif.buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-motif
 .endif
