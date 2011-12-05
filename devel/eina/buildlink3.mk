d8627237fc50de30e996266aa2d3cde680d8002c
joerg <joerg@pkgsrc.org>
2011-12-05 17:14:52 +0000

    Update to eina-1.1.0. Includes 2 years of changes, like better Unicode support, extended hashing, a basic SAX XML parser and much more.

--- devel/eina/buildlink3.mk
+++ devel/eina/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/09/11 20:20:18 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/12/05 17:14:52 joerg Exp $
 
 BUILDLINK_TREE+=	eina
 
 .if !defined(EINA_BUILDLINK3_MK)
 EINA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.eina+=	eina>=0.0.2.062
+BUILDLINK_API_DEPENDS.eina+=	eina>=1.1.0
 BUILDLINK_PKGSRCDIR.eina?=	../../devel/eina
 .endif	# EINA_BUILDLINK3_MK
 
