fbcc9abb4bc6d644b22b077e3aae3b09384f6c60
joerg <joerg@pkgsrc.org>
2011-12-05 17:33:31 +0000

    Update evas to 1.1.0. Includes auto-alignment for text, ICO support, PSD loading, external processors for PDF etc and many bug fixes and performance improvements.

--- graphics/evas-buffer/buildlink3.mk
+++ graphics/evas-buffer/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/09/11 22:31:00 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2011/12/05 17:33:32 joerg Exp $
 
 BUILDLINK_TREE+=	evas-buffer
 
 .if !defined(EVAS_BUFFER_BUILDLINK3_MK)
 EVAS_BUFFER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.evas-buffer+=	evas-buffer>=0.9.9.062
+BUILDLINK_API_DEPENDS.evas-buffer+=	evas-buffer>=1.1.0beta
 BUILDLINK_PKGSRCDIR.evas-buffer?=	../../graphics/evas-buffer
 .endif # EVAS_BUFFER_BUILDLINK3_MK
 
