89a4254b2fb39c9c2a1ee71f06c8ea4f8934f7cf
joerg <joerg@pkgsrc.org>
2011-12-05 17:33:31 +0000

    Update evas to 1.1.0. Includes auto-alignment for text, ICO support, PSD loading, external processors for PDF etc and many bug fixes and performance improvements.

--- graphics/evas-software-x11/buildlink3.mk
+++ graphics/evas-software-x11/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/09/11 22:31:01 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2011/12/05 17:33:33 joerg Exp $
 
 BUILDLINK_TREE+=	evas-software-x11
 
 .if !defined(EVAS_SOFTWARE_X11_BUILDLINK3_MK)
 EVAS_SOFTWARE_X11_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.evas-software-x11+=	evas-software-x11>=0.9.9.062
+BUILDLINK_API_DEPENDS.evas-software-x11+=	evas-software-x11>=1.1.0beta
 BUILDLINK_PKGSRCDIR.evas-software-x11?=	../../graphics/evas-software-x11
 
 .include "../../x11/libX11/buildlink3.mk"
