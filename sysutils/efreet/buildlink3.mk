fe8064535b06fa419a63bdc14c67a1440d85c2f6
joerg <joerg@pkgsrc.org>
2011-12-05 17:38:21 +0000

    Import efreet-1.1.0, derived from pkgsrc-wip/efreet:
    An implementation of several specifications from freedesktop.org intended for
    use in Enlightenment DR17 (e17) and other applications using the Enlightenment
    Foundation Libraries (EFL).
    
    Currently, the following specifications are included:
      o Base Directory
      o Desktop Entry
      o Icon Theme
      o Menu

---
+++ sysutils/efreet/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/12/05 17:38:21 joerg Exp $
+
+BUILDLINK_TREE+=	efreet
+
+.if !defined(EFREET_BUILDLINK3_MK)
+EFREET_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.efreet?=	efreet>=1.1.0
+BUILDLINK_PKGSRCDIR.efreet?=	../../sysutils/efreet
+
+.include        "../../devel/ecore/buildlink3.mk"
+.endif # EFREET_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-efreet
