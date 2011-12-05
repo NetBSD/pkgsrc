f97e5165500ccb2a54698fd251172f0b3ec7e19e
taca <taca@pkgsrc.org>
2011-12-05 11:25:41 +0000

    Specify correct directory.  Noted by PR pkg/45684 and it was already in my work area.

--- x11/fltk13/buildlink3.mk
+++ x11/fltk13/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/12/01 16:35:12 is Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/12/05 11:25:41 taca Exp $
 
 BUILDLINK_TREE+=	fltk
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	fltk
 FLTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.fltk+=	fltk>=1.3.0
-BUILDLINK_PKGSRCDIR.fltk?=	../../x11/fltk
+BUILDLINK_PKGSRCDIR.fltk?=	../../x11/fltk13
 BUILDLINK_FILES.fltk+=		include/Fl/*
 
 .include "../../graphics/MesaLib/buildlink3.mk"
