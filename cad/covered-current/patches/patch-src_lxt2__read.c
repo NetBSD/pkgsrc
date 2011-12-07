7fd58718af9dd460c4613f710e62affded8aba94
joerg <joerg@pkgsrc.org>
2011-12-07 15:13:26 +0000

    Fix assembler syntax for byte swaps

---
+++ cad/covered-current/patches/patch-src_lxt2__read.c
@@ -0,0 +1,15 @@
+$NetBSD: patch-src_lxt2__read.c,v 1.1 2011/12/07 15:15:17 joerg Exp $
+
+--- src/lxt2_read.c.orig	2006-01-27 15:43:58.000000000 +0000
++++ src/lxt2_read.c
+@@ -62,8 +62,8 @@ _LXT2_RD_INLINE static unsigned int lxt2
+ {
+ unsigned short x = *((unsigned short *)((unsigned char *)mm+offset));
+ 
+-  __asm("xchgb %b0,%h0" :
+-        "=q" (x)        :
++  __asm("rorw $8, %w1" :
++        "=r" (x)        :
+         "0" (x));
+ 
+     return (unsigned int) x;
