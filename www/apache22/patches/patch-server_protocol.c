8cfaad65ea08753b7759a20f950741b3b22f9c5a
tron <tron@pkgsrc.org>
2011-12-07 22:58:12 +0000

    Add improved fix for proxy vulnerability reported in CVE-2011-3368. This should also fix CVE-2011-3639 and possibly CVE-2011-4317, both part of SA46987.

--- www/apache22/patches/patch-server_protocol.c
+++ www/apache22/patches/patch-server_protocol.c
@@ -1,13 +1,20 @@
-$NetBSD: patch-server_protocol.c,v 1.1 2011/10/10 10:13:42 taca Exp $
+$NetBSD: patch-server_protocol.c,v 1.2 2011/12/07 22:58:12 tron Exp $
 
 revision 1179239 from http://svn.apache.org/:
 	SECURITY (CVE-2011-3368): Prevent unintended pattern expansion
 	in some reverse proxy configurations by strictly validating
 	the request-URI.
 
---- server/protocol.c.orig	2011-10-10 09:11:16.000000000 +0000
-+++ server/protocol.c
-@@ -640,6 +640,25 @@ static int read_request_line(request_rec
+revision 1179525 from http://svn.apache.org/:
+	SECURITY (CVE-2011-3368): Prevent unintended pattern expansion in some
+	reverse proxy configurations by strictly validating the request-URI:
+	* server/protocol.c (read_request_line): Send a 400 response if the
+	  request-URI does not match the grammar from RFC 2616.  This ensures
+	  the input string for RewriteRule et al really is an absolute path.
+
+--- server/protocol.c.orig	2011-05-07 12:39:29.000000000 +0100
++++ server/protocol.c	2011-12-07 22:48:17.000000000 +0000
+@@ -640,6 +640,44 @@
  
      ap_parse_uri(r, uri);
  
@@ -28,6 +35,25 @@ revision 1179239 from http://svn.apache.org/:
 +	    r->hostname = NULL;
 +	    r->status = HTTP_BAD_REQUEST;
 +	    r->uri = apr_pstrdup(r->pool, uri);
++    }
++
++    /* RFC 2616:
++     *   Request-URI    = "*" | absoluteURI | abs_path | authority
++     *
++     * authority is a special case for CONNECT.  If the request is not
++     * using CONNECT, and the parsed URI does not have scheme, and
++     * it does not begin with '/', and it is not '*', then, fail
++     * and give a 400 response. */
++    if (r->method_number != M_CONNECT 
++        && !r->parsed_uri.scheme 
++        && uri[0] != '/'
++        && !(uri[0] == '*' && uri[1] == '\0')) {
++        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
++                      "invalid request-URI %s", uri);
++        r->args = NULL;
++        r->hostname = NULL;
++        r->status = HTTP_BAD_REQUEST;
++        r->uri = apr_pstrdup(r->pool, uri);
 +    }
 +
      if (ll[0]) {
