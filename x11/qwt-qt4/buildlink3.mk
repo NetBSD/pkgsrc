d54554bb55f94b52de039a11ae43cf41149409d6
drochner <drochner@pkgsrc.org>
2011-12-07 13:36:16 +0000

    roll back to D2011.06.14.22.00.00, i.e. revert to 5.2.1nb4 qwt6 is incompatible, and the update has broken the graphics/scidavis pkg

--- x11/qwt-qt4/buildlink3.mk
+++ x11/qwt-qt4/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2011/11/01 06:03:08 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2011/12/07 13:36:16 drochner Exp $
 
 BUILDLINK_TREE+=	qwt
 
 .if !defined(QWT_BUILDLINK3_MK)
 QWT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qwt+=	qwt>=6.0.0
-BUILDLINK_ABI_DEPENDS.qwt+=	qwt>=6.0.0nb2
+BUILDLINK_API_DEPENDS.qwt+=	qwt>=5.2.0
+BUILDLINK_ABI_DEPENDS.qwt?=	qwt>=5.2.1nb4
 BUILDLINK_PKGSRCDIR.qwt?=	../../x11/qwt-qt4
 
 .include "../../x11/qt4-libs/buildlink3.mk"
