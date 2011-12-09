fbba82aaacc0ee1a158b439107a6a15776642c76
marino <marino@pkgsrc.org>
2011-12-09 01:35:08 +0000

    editors/emacs-snapshot: Fix DragonFly support

--- editors/emacs-snapshot/files/dragonfly.h
+++ editors/emacs-snapshot/files/dragonfly.h
@@ -44,8 +44,8 @@ Boston, MA 02111-1307, USA.  */
 #define GETPGRP_NO_ARG
 
 #ifdef DFLY_CRT_USRLIB
-#define START_FILES pre-crt0.o /usr/lib/crt1.o /usr/lib/crti.o /usr/lib/gcc41/crtbegin.o
-#define LIB_STANDARD -L/usr/lib/gcc41 -lgcc -lc -lgcc /usr/lib/gcc41/crtend.o /usr/lib/crtn.o
+#define ORDINARY_LINK
+#define LIB_STANDARD -lc
 #else
 #define START_FILES pre-crt0.o /usr/lib/gcc41/crt1.o /usr/lib/gcc41/crti.o /usr/lib/gcc41/crtbegin.o
 #define LIB_STANDARD -L/usr/lib/gcc41 -lgcc -lc -lgcc /usr/lib/gcc41/crtend.o /usr/lib/gcc41/crtn.o
