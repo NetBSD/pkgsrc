694eaff4592256d31542abd57efea88c39db7314
tron <tron@pkgsrc.org>
2011-12-12 18:43:14 +0000

    Remove duplicate error check from security patch. No revision bump as there is no functional change.
    Problem pointed out by S.P. Zeidler.

--- www/apache22/patches/patch-server_protocol.c
+++ www/apache22/patches/patch-server_protocol.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-server_protocol.c,v 1.2 2011/12/07 22:58:12 tron Exp $
+$NetBSD: patch-server_protocol.c,v 1.3 2011/12/12 18:43:14 tron Exp $
 
 revision 1179239 from http://svn.apache.org/:
 	SECURITY (CVE-2011-3368): Prevent unintended pattern expansion
@@ -13,8 +13,8 @@ revision 1179525 from http://svn.apache.org/:
 	  the input string for RewriteRule et al really is an absolute path.
 
 --- server/protocol.c.orig	2011-05-07 12:39:29.000000000 +0100
-+++ server/protocol.c	2011-12-07 22:48:17.000000000 +0000
-@@ -640,6 +640,44 @@
++++ server/protocol.c	2011-12-12 18:37:04.000000000 +0000
+@@ -640,6 +640,25 @@
  
      ap_parse_uri(r, uri);
  
@@ -35,25 +35,6 @@ revision 1179525 from http://svn.apache.org/:
 +	    r->hostname = NULL;
 +	    r->status = HTTP_BAD_REQUEST;
 +	    r->uri = apr_pstrdup(r->pool, uri);
-+    }
-+
-+    /* RFC 2616:
-+     *   Request-URI    = "*" | absoluteURI | abs_path | authority
-+     *
-+     * authority is a special case for CONNECT.  If the request is not
-+     * using CONNECT, and the parsed URI does not have scheme, and
-+     * it does not begin with '/', and it is not '*', then, fail
-+     * and give a 400 response. */
-+    if (r->method_number != M_CONNECT 
-+        && !r->parsed_uri.scheme 
-+        && uri[0] != '/'
-+        && !(uri[0] == '*' && uri[1] == '\0')) {
-+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
-+                      "invalid request-URI %s", uri);
-+        r->args = NULL;
-+        r->hostname = NULL;
-+        r->status = HTTP_BAD_REQUEST;
-+        r->uri = apr_pstrdup(r->pool, uri);
 +    }
 +
      if (ll[0]) {
