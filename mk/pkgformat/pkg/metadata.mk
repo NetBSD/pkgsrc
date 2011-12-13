99edd1e880305843a246865592d780c5a9c73976
joerg <joerg>
2011-12-13 16:35:48 +0000

    Be a bit less aggressive in the required version of pkg_install encoded into binary packages.

--- mk/pkgformat/pkg/metadata.mk
+++ mk/pkgformat/pkg/metadata.mk
@@ -1,4 +1,4 @@
-# $NetBSD: metadata.mk,v 1.1 2011/10/15 00:23:09 reed Exp $
+# $NetBSD: metadata.mk,v 1.2 2011/12/13 16:35:48 joerg Exp $
 
 ######################################################################
 ### The targets below are all PRIVATE.
@@ -38,7 +38,7 @@ ${_BUILD_INFO_FILE}: plist
 .if !empty(USE_TOOLS:Mgmake)
 	${RUN}${ECHO} "GMAKE=`${GMAKE} --version | ${GREP} Make`" >> ${.TARGET}.tmp
 .endif
-	${RUN}${ECHO} "PKGTOOLS_VERSION=${PKGTOOLS_VERSION}" >> ${.TARGET}.tmp
+	${RUN}${ECHO} "PKGTOOLS_VERSION=${PKGTOOLS_VERSION_REQD}" >> ${.TARGET}.tmp
 .if defined(HOMEPAGE)
 	${RUN}${ECHO} "HOMEPAGE=${HOMEPAGE}" >> ${.TARGET}.tmp
 .endif
