ec7d039a8ec737cc80f7219cd63317fe0dba6e1b
asau <asau@pkgsrc.org>
2011-12-15 22:59:28 +0000

    Adjust path for buildlink. Use common files from net/samba just like other Samba packages do.

--- net/samba30/buildlink3.mk
+++ net/samba30/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/12/15 22:23:23 asau Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/12/15 22:59:28 asau Exp $
 
 BUILDLINK_TREE+=	samba
 
@@ -7,7 +7,7 @@ SAMBA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.samba+=	samba>=3.0.7
 BUILDLINK_ABI_DEPENDS.samba?=	samba>=3.0.34nb1
-BUILDLINK_PKGSRCDIR.samba?=	../../net/samba
+BUILDLINK_PKGSRCDIR.samba?=	../../net/samba30
 
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/readline/buildlink3.mk"
