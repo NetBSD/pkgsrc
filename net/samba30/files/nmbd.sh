93e76c3aa4da7d05b1d848b1cfc9f777cfa8e9e7
asau <asau>
2011-12-15 22:23:23 +0000

    Reimport Samba 3.0.37 from net/samba as net/samba30 as part of Samba packages rotation.

---
+++ net/samba30/files/nmbd.sh
@@ -0,0 +1,21 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: nmbd.sh,v 1.1.1.1 2011/12/15 22:23:24 asau Exp $
+#
+# PROVIDE: nmbd
+# REQUIRE: DAEMON
+
+. /etc/rc.subr
+
+name="nmbd"
+rcvar=$name
+command="@PREFIX@/sbin/${name}"
+required_files="@SAMBA_ETCDIR@/smb.conf"
+extra_commands="reload"
+command_args="-D"		# _must_ start as daemon from rc.d;
+				# add more flags through ${${name}_flags}
+
+reload_cmd=":"			# avoid dumping debug output on SIGHUP
+
+load_rc_config $name
+run_rc_command "$1"
