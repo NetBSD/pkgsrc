e05f1845ecdc2b3e520134e0ddcb75358d943842
hans <hans@pkgsrc.org>
2011-12-16 16:55:25 +0000

    Fix build on SunOS.

--- mail/fix-mime-charset/patches/patch-src_argp-local.h
+++ mail/fix-mime-charset/patches/patch-src_argp-local.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_argp-local.h,v 1.1 2011/11/24 13:56:02 joerg Exp $
+$NetBSD: patch-src_argp-local.h,v 1.2 2011/12/16 16:55:25 hans Exp $
 
 --- src/argp-local.h.orig	2011-11-24 00:53:41.000000000 +0000
 +++ src/argp-local.h
@@ -10,3 +10,15 @@ $NetBSD: patch-src_argp-local.h,v 1.1 2011/11/24 13:56:02 joerg Exp $
  #include <string.h>
  #include <iostream>
  
+@@ -196,7 +197,11 @@ for(p=_argp->options;p!=_argp_default_op
+    if(!p->name && !p->key)
+      p=_argp_default_options;
+    memset(q,0,sizeof(option));
++#ifndef __sun
+    q->name=p->name;
++#else
++   q->name=(char*)p->name;
++#endif
+    q->has_arg=p->arg==NULL ? no_argument : required_argument;
+    if(p->key>' ' && p->key<='z')
+      {
