c8dbfb12b84163a9474de34b88aacd9fa912ca19
obache <obache@pkgsrc.org>
2011-12-19 06:26:55 +0000

    Update firefox36 to 3.6.24.
    Fixed in Firefox 3.6.24
    MFSA 2011-49 Memory corruption while profiling using Firebug
    MFSA 2011-47 Potential XSS against sites using Shift-JIS
    MFSA 2011-46 loadSubScript unwraps XPCNativeWrapper scope parameter (1.9.2 branch)
    Fixed in Firefox 3.6.23
    MFSA 2011-40 Code installation through holding down Enter
    MFSA 2011-39 Defense against multiple Location headers due to CRLF Injection
    MFSA 2011-38 XSS via plugins and shadowed window.location object
    MFSA 2011-37 Integer underflow when using JavaScript RegExp
    MFSA 2011-36 Miscellaneous memory safety hazards (rv:7.0 / rv:1.9.2.23)

--- devel/xulrunner192/dist.mk
+++ devel/xulrunner192/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.4 2011/09/15 09:49:36 obache Exp $
+# $NetBSD: dist.mk,v 1.5 2011/12/19 06:26:55 obache Exp $
 #
 # used by devel/xulrunner192/Makefile
 # used by www/firefox36/Makefile
@@ -6,7 +6,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	3.6${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	1.9.2
-MOZ_BRANCH_MINOR=	.22
+MOZ_BRANCH_MINOR=	.24
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
