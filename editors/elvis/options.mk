4769243699826f14857a6e7d39b95e4e9899ea52
wiz <wiz@pkgsrc.org>
2011-12-19 15:27:47 +0000

    Fix elvis-x11 build by depending on pkg-config. XXX: This is a big mess. elvis/Makefile is included by other packages, elvis-x11 sets an option...

--- editors/elvis/options.mk
+++ editors/elvis/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2011/10/03 00:52:07 dholland Exp $
+# $NetBSD: options.mk,v 1.3 2011/12/19 15:27:47 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.elvis
 PKG_SUPPORTED_OPTIONS=		x11
@@ -15,6 +15,7 @@ CONFIGURE_ARGS+=		--with-x
 CONFIGURE_ARGS+=		--with-xft
 CONFIGURE_ARGS+=		--x-includes=${X11BASE:Q}/include
 CONFIGURE_ARGS+=		--x-libraries=${X11BASE:Q}/lib
+USE_TOOLS+=			pkg-config
 .else
 CONFIGURE_ARGS+=		--without-x
 .endif
