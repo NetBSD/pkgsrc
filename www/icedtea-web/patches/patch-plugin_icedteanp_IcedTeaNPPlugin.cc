dd6bdd3a699f6484dee8ab486b32ad5c91707b08
asau <asau>
2011-12-20 13:19:00 +0000

    Build fix: pass stricter compiler checks.

---
+++ www/icedtea-web/patches/patch-plugin_icedteanp_IcedTeaNPPlugin.cc
@@ -0,0 +1,13 @@
+$NetBSD: patch-plugin_icedteanp_IcedTeaNPPlugin.cc,v 1.1 2011/12/20 13:19:00 asau Exp $
+
+--- plugin/icedteanp/IcedTeaNPPlugin.cc.orig	2011-08-17 16:01:19.000000000 +0000
++++ plugin/icedteanp/IcedTeaNPPlugin.cc
+@@ -2262,7 +2262,7 @@ NP_Initialize (NPNetscapeFuncs* browserT
+ 
+ // Returns a string describing the MIME type that this plugin
+ // handles.
+-char*
++const char*
+ NP_GetMIMEDescription ()
+ {
+   PLUGIN_DEBUG ("NP_GetMIMEDescription\n");
