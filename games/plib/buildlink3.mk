17a4b83943a3045e4dc3f3dfe3cd5a0d7fca17a0
drochner <drochner@pkgsrc.org>
2011-12-23 13:36:44 +0000

    revbump recursively to force build against fixed plib pkg (which installs static libraries), from David Holland

--- games/plib/buildlink3.mk
+++ games/plib/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:24:34 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2011/12/23 13:36:44 drochner Exp $
 
 BUILDLINK_TREE+=	plib
 
 .if !defined(PLIB_BUILDLINK3_MK)
 PLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.plib+=	plib>=1.6.0
+BUILDLINK_API_DEPENDS.plib+=	plib>=1.8.5nb2
 BUILDLINK_ABI_DEPENDS.plib?=	plib>=1.6.0nb3
 BUILDLINK_PKGSRCDIR.plib?=	../../games/plib
 BUILDLINK_DEPMETHOD.plib?=	build
