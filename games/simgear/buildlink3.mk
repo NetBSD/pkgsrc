53f56b90355f48918557274a2d57531cda374498
drochner <drochner@pkgsrc.org>
2011-12-23 13:36:44 +0000

    revbump recursively to force build against fixed plib pkg (which installs static libraries), from David Holland

--- games/simgear/buildlink3.mk
+++ games/simgear/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.8 2010/12/23 11:44:32 dsainty Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2011/12/23 13:36:45 drochner Exp $
 
 BUILDLINK_TREE+=	simgear
 
 .if !defined(SIMGEAR_BUILDLINK3_MK)
 SIMGEAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.simgear+=	simgear>=1.0.0
+BUILDLINK_API_DEPENDS.simgear+=	simgear>=2.0.0nb6
 BUILDLINK_ABI_DEPENDS.simgear+=	simgear>=1.0.0nb2
 BUILDLINK_PKGSRCDIR.simgear?=	../../games/simgear
 BUILDLINK_DEPMETHOD.simgear?=	build
