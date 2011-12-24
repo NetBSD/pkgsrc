3313ae7df82ebdf114e04e9b955c0e1b9f58c9d7
dholland <dholland@pkgsrc.org>
2011-12-24 04:23:21 +0000

    This needs to expose gnome-panel via bl3 if it's built with the "gnomecanvas" option. Otherwise some packages fail to link because they can't find -lnspr4. PKGREVISION -> 10.
    Because the packages affected by this don't build without it, I don't
    think they need to be revbumped.

--- x11/lablgtk/buildlink3.mk
+++ x11/lablgtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:25:43 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2011/12/24 04:23:21 dholland Exp $
 
 BUILDLINK_TREE+=	lablgtk
 
@@ -8,6 +8,15 @@ LABLGTK_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.lablgtk+=	lablgtk>=2.6.0nb3
 BUILDLINK_ABI_DEPENDS.lablgtk?=	lablgtk>=2.6.0nb5
 BUILDLINK_PKGSRCDIR.lablgtk?=	../../x11/lablgtk
+
+# gnome-panel must be exposed if and only if we built with it
+pkgbase := lablgtk
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.lablgtk:Mgnomecanvas)
+.  include "../../x11/gnome-panel/buildlink3.mk"
+.endif
+
 .endif # LABLGTK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-lablgtk
