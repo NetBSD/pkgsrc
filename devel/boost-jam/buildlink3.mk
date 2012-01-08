974fe0ad657da44863228c0138feadcf5891838e
adam <adam>
2012-01-08 11:35:07 +0000

    Changes 1.48.0: New Libraries * Container: Standard library containers and extensions * Locale: Provide localization and Unicode handling tools for C++   Beilis. * Move: Portable move semantics for C++03 and C++11 compilers
    Details: http://www.boost.org/users/history/version_1_48_0.html

--- devel/boost-jam/buildlink3.mk
+++ devel/boost-jam/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.13 2011/10/11 10:02:08 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2012/01/08 11:35:07 adam Exp $
 
 BUILDLINK_TREE+=	boost-jam
 
 .if !defined(BOOST_JAM_BUILDLINK3_MK)
 BOOST_JAM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.47.*
+BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.48.*
 BUILDLINK_DEPMETHOD.boost-jam?=		build
 BUILDLINK_PKGSRCDIR.boost-jam?=		../../devel/boost-jam
 .endif # BOOST_JAM_BUILDLINK3_MK
