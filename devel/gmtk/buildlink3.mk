c3b623bed3dc5d0e8ac9677bde784fd9d7ab3ad1
obache <obache>
2012-01-08 05:42:08 +0000

    Import gmtk-1.0.5 as devel/gmtk.
    Library of common functions and widgets for gnome-mplayer and gecko-mediaplayer.

---
+++ devel/gmtk/buildlink3.mk
@@ -0,0 +1,27 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/01/08 05:42:08 obache Exp $
+#
+
+BUILDLINK_TREE+=	gmtk
+
+.if !defined(GMTK_BUILDLINK3_MK)
+GMTK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gmtk+=	gmtk>=1.0.5
+BUILDLINK_PKGSRCDIR.gmtk?=	../../devel/gmtk
+
+pkgbase := gmtk
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.gmtk:Mpulseaudio)
+.include "../../audio/pulseaudio/buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.gmtk:Malsa)
+.include "../../audio/alsa-lib/buildlink3.mk"
+.endif
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/GConf/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# GMTK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gmtk
