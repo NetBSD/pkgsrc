1a71deaac52b1e0f64fb91fabad610269a6350db
manu <manu@pkgsrc.org>
2012-01-09 05:25:36 +0000

    Re-enable PAM support, as it works fine provided one does not mix multiple versions of libcrypto in sshd. This can happen if OpenSSH is linked with pkgsrc's OpenSSL and if using nss_ldap, which pulls base-system OpenSSL through kerberos libraries. One needs to disable the krb5 of nss_ldap in order to fix that.

--- security/openssh/options.mk
+++ security/openssh/options.mk
@@ -1,13 +1,9 @@
-# $NetBSD: options.mk,v 1.21 2011/09/01 19:24:01 shattered Exp $
+# $NetBSD: options.mk,v 1.22 2012/01/09 05:25:36 manu Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.openssh
-PKG_SUPPORTED_OPTIONS=	kerberos hpn-patch
-
-.if !empty(OPSYS:MLinux)
-PKG_SUPPORTED_OPTIONS+= pam
-.endif
+PKG_SUPPORTED_OPTIONS=	kerberos hpn-patch pam
 
 .include "../../mk/bsd.options.mk"
 
@@ -26,8 +22,6 @@ PATCH_DIST_STRIP=	-p1
 .endif
 
 .if !empty(PKG_OPTIONS:Mpam)
-# XXX: PAM authentication causes memory faults, and haven't tracked down
-# XXX: why yet.  For the moment, disable PAM authentication for non-Linux.
 .include "../../mk/pam.buildlink3.mk"
 CONFIGURE_ARGS+=	--with-pam
 PLIST_SRC+=		${.CURDIR}/PLIST.pam
