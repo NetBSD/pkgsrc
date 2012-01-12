ed067d0808445394fde4724b7b06d406549eb927
adam <adam@pkgsrc.org>
2012-01-12 22:59:58 +0000

    Changes 4.8.0: Featuring Qt Platform Abstraction, threaded OpenGL support, multithreaded HTTP and optimized file system access.

---
+++ x11/qt4-libs/patches/patch-src_3rdparty_webkit_Source_WebCore_platform_DefaultLocalizationStrategy.cpp
@@ -0,0 +1,15 @@
+$NetBSD: patch-src_3rdparty_webkit_Source_WebCore_platform_DefaultLocalizationStrategy.cpp,v 1.1 2012/01/12 22:59:58 adam Exp $
+
+Fix C++ error.
+
+--- src/3rdparty/webkit/Source/WebCore/platform/DefaultLocalizationStrategy.cpp.orig	2012-01-12 19:51:48.000000000 +0100
++++ src/3rdparty/webkit/Source/WebCore/platform/DefaultLocalizationStrategy.cpp	2012-01-12 20:07:06.000000000 +0100
+@@ -59,7 +59,7 @@
+     return result.get();
+ #elif PLATFORM(QT)
+     va_list arguments;
+-    va_start(arguments, format);
++    va_start(arguments, format.charactersWithNullTermination());
+     QString result;
+     result.vsprintf(format.latin1().data(), arguments);
+     va_end(arguments);
--- x11/qt4-libs/patches/patch-src_network_ssl_qsslsocket__openssl__symbols.cpp
+++ x11/qt4-libs/patches/patch-src_network_ssl_qsslsocket__openssl__symbols.cpp
@@ -1,10 +1,8 @@
-$NetBSD: patch-src_network_ssl_qsslsocket__openssl__symbols.cpp,v 1.1 2011/08/19 14:55:15 obache Exp $
+$NetBSD: patch-src_network_ssl_qsslsocket__openssl__symbols.cpp,v 1.2 2012/01/12 22:59:58 adam Exp $
 
-* fixes build with OpenSSL 0.9.9-dev
-
---- src/network/ssl/qsslsocket_openssl_symbols.cpp.orig	2011-03-30 05:19:09.000000000 +0000
+--- src/network/ssl/qsslsocket_openssl_symbols.cpp.orig	2011-12-08 05:06:02.000000000 +0000
 +++ src/network/ssl/qsslsocket_openssl_symbols.cpp
-@@ -170,7 +170,7 @@ DEFINEFUNC(int, SSL_CTX_check_private_ke
+@@ -171,7 +171,7 @@ DEFINEFUNC(int, SSL_CTX_check_private_ke
  #endif
  DEFINEFUNC4(long, SSL_CTX_ctrl, SSL_CTX *a, a, int b, b, long c, c, void *d, d, return -1, return)
  DEFINEFUNC(void, SSL_CTX_free, SSL_CTX *a, a, return, DUMMYARG)
@@ -13,12 +11,21 @@ $NetBSD: patch-src_network_ssl_qsslsocket__openssl__symbols.cpp,v 1.1 2011/08/19
  DEFINEFUNC(SSL_CTX *, SSL_CTX_new, const SSL_METHOD *a, a, return 0, return)
  #else
  DEFINEFUNC(SSL_CTX *, SSL_CTX_new, SSL_METHOD *a, a, return 0, return)
-@@ -213,7 +213,7 @@ DEFINEFUNC3(void, SSL_set_bio, SSL *a, a
+@@ -213,7 +213,7 @@ DEFINEFUNC(SSL *, SSL_new, SSL_CTX *a, a
+ #if OPENSSL_VERSION_NUMBER >= 0x10000000L
+ DEFINEFUNC4(long, SSL_ctrl, SSL *a, a, int cmd, cmd, long larg, larg, void *parg, parg, return -1, return)
+ #else
+-DEFINEFUNC4(long, SSL_ctrl, SSL *a, a, int cmd, cmd, long larg, larg, const void *parg, parg, return -1, return)
++DEFINEFUNC4(long, SSL_ctrl, SSL *a, a, int cmd, cmd, long larg, larg, void *parg, parg, return -1, return)
+ #endif
+ #endif
+ DEFINEFUNC3(int, SSL_read, SSL *a, a, void *b, b, int c, c, return -1, return)
+@@ -221,7 +221,7 @@ DEFINEFUNC3(void, SSL_set_bio, SSL *a, a
  DEFINEFUNC(void, SSL_set_accept_state, SSL *a, a, return, DUMMYARG)
  DEFINEFUNC(void, SSL_set_connect_state, SSL *a, a, return, DUMMYARG)
  DEFINEFUNC(int, SSL_shutdown, SSL *a, a, return -1, return)
 -#if OPENSSL_VERSION_NUMBER >= 0x10000000L
 +#if OPENSSL_VERSION_NUMBER >= 0x00909000L
+ #ifndef OPENSSL_NO_SSL2
  DEFINEFUNC(const SSL_METHOD *, SSLv2_client_method, DUMMYARG, DUMMYARG, return 0, return)
- DEFINEFUNC(const SSL_METHOD *, SSLv3_client_method, DUMMYARG, DUMMYARG, return 0, return)
- DEFINEFUNC(const SSL_METHOD *, SSLv23_client_method, DUMMYARG, DUMMYARG, return 0, return)
+ #endif
