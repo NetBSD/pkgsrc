85b6705d6c0bd91a7cdc3155c8ce796e2054f5f4
obache <obache>
2012-01-13 10:54:43 +0000

    Recursive bump from audio/libaudiofile, x11/qt4-libs and x11/qt4-tools ABI bump.

--- audio/SDL_sound/buildlink3.mk
+++ audio/SDL_sound/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2009/03/20 19:23:51 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2012/01/13 10:54:44 obache Exp $
 
 BUILDLINK_TREE+=	SDL_sound
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	SDL_sound
 SDL_SOUND_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.SDL_sound+=	SDL_sound>=1.0.3
-BUILDLINK_ABI_DEPENDS.SDL_sound?=	SDL_sound>=1.0.3nb1
+BUILDLINK_ABI_DEPENDS.SDL_sound?=	SDL_sound>=1.0.3nb4
 BUILDLINK_PKGSRCDIR.SDL_sound?=	../../audio/SDL_sound
 
 .include "../../audio/flac/buildlink3.mk"
--- x11/qwt-qt4/buildlink3.mk
+++ x11/qwt-qt4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2011/12/07 13:36:16 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2012/01/13 10:55:51 obache Exp $
 
 BUILDLINK_TREE+=	qwt
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qwt
 QWT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qwt+=	qwt>=5.2.0
-BUILDLINK_ABI_DEPENDS.qwt?=	qwt>=5.2.1nb4
+BUILDLINK_ABI_DEPENDS.qwt?=	qwt>=5.2.2nb1
 BUILDLINK_PKGSRCDIR.qwt?=	../../x11/qwt-qt4
 
 .include "../../x11/qt4-libs/buildlink3.mk"
