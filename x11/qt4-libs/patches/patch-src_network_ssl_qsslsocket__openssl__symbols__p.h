25e97296717847f9a15a846281cc4a58d17e96e9
obache <obache@pkgsrc.org>
2012-01-14 10:23:18 +0000

    Fixes build with openssl<1.0.0. taken from upstream:   http://qt.gitorious.org/qt/qt/commit/4db91cbd6147e40f543342f22c05b7baddc52e5a
    resolve PR#45832.

--- x11/qt4-libs/patches/patch-src_network_ssl_qsslsocket__openssl__symbols__p.h
+++ x11/qt4-libs/patches/patch-src_network_ssl_qsslsocket__openssl__symbols__p.h
@@ -1,8 +1,11 @@
-$NetBSD: patch-src_network_ssl_qsslsocket__openssl__symbols__p.h,v 1.2 2012/01/12 22:59:58 adam Exp $
+$NetBSD: patch-src_network_ssl_qsslsocket__openssl__symbols__p.h,v 1.3 2012/01/14 10:23:18 obache Exp $
 
 * fixes build with OpenSSL 0.9.9-dev
+* fix build with -openssl-linked for OpenSSL 0.9.8*
+  http://qt.gitorious.org/qt/qt/commit/4db91cbd6147e40f543342f22c05b7baddc52e5a
 
---- src/network/ssl/qsslsocket_openssl_symbols_p.h.orig	2011-03-30 05:19:09.000000000 +0000
+
+--- src/network/ssl/qsslsocket_openssl_symbols_p.h.orig	2011-12-08 05:06:02.000000000 +0000
 +++ src/network/ssl/qsslsocket_openssl_symbols_p.h
 @@ -279,7 +279,7 @@ int q_SSL_CTX_check_private_key(SSL_CTX 
  #endif
@@ -13,7 +16,18 @@ $NetBSD: patch-src_network_ssl_qsslsocket__openssl__symbols__p.h,v 1.2 2012/01/1
  SSL_CTX *q_SSL_CTX_new(const SSL_METHOD *a);
  #else
  SSL_CTX *q_SSL_CTX_new(SSL_METHOD *a);
-@@ -329,7 +329,7 @@ void q_SSL_set_bio(SSL *a, BIO *b, BIO *
+@@ -318,18 +318,14 @@ int q_SSL_library_init();
+ void q_SSL_load_error_strings();
+ SSL *q_SSL_new(SSL_CTX *a);
+ #if OPENSSL_VERSION_NUMBER >= 0x0090806fL && !defined(OPENSSL_NO_TLSEXT)
+-#if OPENSSL_VERSION_NUMBER >= 0x10000000L
+ long q_SSL_ctrl(SSL *ssl,int cmd, long larg, void *parg);
+-#else
+-long q_SSL_ctrl(SSL *ssl,int cmd, long larg, const void *parg);
+-#endif
+ #endif
+ int q_SSL_read(SSL *a, void *b, int c);
+ void q_SSL_set_bio(SSL *a, BIO *b, BIO *c);
  void q_SSL_set_accept_state(SSL *a);
  void q_SSL_set_connect_state(SSL *a);
  int q_SSL_shutdown(SSL *a);
