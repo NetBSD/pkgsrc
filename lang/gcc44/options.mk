c44aeab8e78ae8b614c60c9fe2caac5b0b19a260
hans <hans>
2012-01-14 02:09:35 +0000

    Convert to USE_TOOLS=zip.

--- lang/gcc44/options.mk
+++ lang/gcc44/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2011/07/13 13:25:17 hans Exp $
+# $NetBSD: options.mk,v 1.7 2012/01/14 02:09:36 hans Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gcc44
 PKG_SUPPORTED_OPTIONS=	nls gcc-c++ gcc-fortran gcc-java gcc-objc #gcc-ada
@@ -42,8 +42,7 @@ PKG_OPTIONS+=		gcc-c++
 LANGS+=			java
 REPLACE_PYTHON=		libjava/contrib/aot-compile.in
 
-USE_TOOLS+=		unzip
-DEPENDS+=		zip-[0-9]*:../../archivers/zip
+USE_TOOLS+=		unzip zip:run
 CONFIGURE_ARGS+=	--with-system-zlib
 
 # ${WRKSRC}/gcc-4.4.1/libjava/contrib/aotcompile.py.in stores the path to
