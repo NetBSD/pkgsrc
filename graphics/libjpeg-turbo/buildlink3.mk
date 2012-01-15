996c12ea2ccb5f40b9fd89b09f2b3d5b06cb1352
obache <obache@pkgsrc.org>
2012-01-15 10:11:53 +0000

    bump API version to 1.1.0, switched to jpeg8 compatible API/ABI.

--- graphics/libjpeg-turbo/buildlink3.mk
+++ graphics/libjpeg-turbo/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/12/12 11:48:56 dsainty Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/01/15 10:11:53 obache Exp $
 
 BUILDLINK_TREE+=	libjpeg-turbo
 
 .if !defined(LIBJPEG_TURBO_BUILDLINK3_MK)
 LIBJPEG_TURBO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libjpeg-turbo+=	libjpeg-turbo>=1.0.1
+BUILDLINK_API_DEPENDS.libjpeg-turbo+=	libjpeg-turbo>=1.1.0
 BUILDLINK_PKGSRCDIR.libjpeg-turbo?=	../../graphics/libjpeg-turbo
 .endif	# LIBJPEG_TURBO_BUILDLINK3_MK
 
