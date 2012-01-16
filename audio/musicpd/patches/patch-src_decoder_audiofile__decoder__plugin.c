eada1990836335c648f2f8733a1f303345817deb
drochner <drochner@pkgsrc.org>
2012-01-16 20:57:32 +0000

    update to 0.16.6 changes: bugfixes, minor feature improvements

--- audio/musicpd/patches/patch-src_decoder_audiofile__decoder__plugin.c
+++ audio/musicpd/patches/patch-src_decoder_audiofile__decoder__plugin.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-src_decoder_audiofile__decoder__plugin.c,v 1.1 2011/11/30 12:02:25 wiz Exp $
+$NetBSD: patch-src_decoder_audiofile__decoder__plugin.c,v 1.2 2012/01/16 20:57:32 drochner Exp $
 
 Include unistd.h for SEEK_CUR, SEEK_SET.
 
---- src/decoder/audiofile_decoder_plugin.c.orig	2011-03-19 00:41:52.000000000 +0000
+--- src/decoder/audiofile_decoder_plugin.c.orig	2011-12-01 13:44:01.000000000 +0000
 +++ src/decoder/audiofile_decoder_plugin.c
-@@ -25,6 +25,7 @@
- #include <af_vfs.h>
+@@ -26,6 +26,7 @@
  #include <assert.h>
  #include <glib.h>
+ #include <stdio.h>
 +#include <unistd.h>
  
  #undef G_LOG_DOMAIN
