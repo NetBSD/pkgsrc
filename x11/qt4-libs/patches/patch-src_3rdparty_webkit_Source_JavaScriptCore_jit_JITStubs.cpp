116210dd612a2ba2f849c963229e38ce6756cd92
adam <adam@pkgsrc.org>
2012-01-16 09:00:27 +0000

    Fix building on NetBSD/amd64 with GCC.

---
+++ x11/qt4-libs/patches/patch-src_3rdparty_webkit_Source_JavaScriptCore_jit_JITStubs.cpp
@@ -0,0 +1,15 @@
+$NetBSD: patch-src_3rdparty_webkit_Source_JavaScriptCore_jit_JITStubs.cpp,v 1.1 2012/01/16 09:00:27 adam Exp $
+
+Fix building on NetBSD/amd64 with GCC.
+
+--- src/3rdparty/webkit/Source/JavaScriptCore/jit/JITStubs.cpp.orig	2012-01-16 06:31:42.000000000 +0000
++++ src/3rdparty/webkit/Source/JavaScriptCore/jit/JITStubs.cpp
+@@ -79,7 +79,7 @@ namespace JSC {
+ #define THUMB_FUNC_PARAM(name)
+ #endif
+ 
+-#if (OS(LINUX) || OS(FREEBSD)) && CPU(X86_64)
++#if (OS(LINUX) || OS(FREEBSD) || OS(NETBSD)) && CPU(X86_64)
+ #define SYMBOL_STRING_RELOCATION(name) #name "@plt"
+ #elif OS(DARWIN) || (CPU(X86_64) && COMPILER(MINGW) && !GCC_VERSION_AT_LEAST(4, 5, 0))
+ #define SYMBOL_STRING_RELOCATION(name) "_" #name
