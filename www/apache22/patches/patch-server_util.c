27754f863b7813540c349c40e6ef1b725a2796cb
tron <tron>
2012-01-18 19:54:36 +0000

    Pullup ticket #3653 - requested by spz www/apache22: security patch
    Revisions pulled up:
    - www/apache22/Makefile                                         1.77
    - www/apache22/distinfo                                         1.48
    - www/apache22/patches/patch-server_scoreboard.c                1.1
    - www/apache22/patches/patch-server_util.c                      1.2
    
    ---
       Module Name:	pkgsrc
       Committed By:	spz
       Date:		Tue Jan 17 20:48:29 UTC 2012
    
       Modified Files:
       	pkgsrc/www/apache22: Makefile distinfo
       	pkgsrc/www/apache22/patches: patch-server_util.c
       Added Files:
       	pkgsrc/www/apache22/patches: patch-server_scoreboard.c
    
       Log Message:
       add patch for CVE-2012-0031 taken from Revision 1231058 of http://svn.apache.org/
       update patch for http://secunia.com/advisories/45793/

--- www/apache22/patches/patch-server_util.c
+++ www/apache22/patches/patch-server_util.c
@@ -1,11 +1,20 @@
-$NetBSD: patch-server_util.c,v 1.1 2011/11/11 07:58:03 spz Exp $
+$NetBSD: patch-server_util.c,v 1.1.4.1 2012/01/18 19:54:36 tron Exp $
 
 fix for http://secunia.com/advisories/45793/
-snarfed (with adjustment regarding location) from
-http://svn.apache.org/viewvc/httpd/httpd/trunk/server/util.c?r1=1198940&r2=1198939&pathrev=1198940
+http://svn.apache.org/viewvc/httpd/httpd/branches/2.2.x/server/util.c?r1=1124515&r2=1227280
+modulo we have apr 1.3 and can use APR_SIZE_MAX and APR_ENOMEM
 
 --- server/util.c.orig	2011-05-19 02:17:37.000000000 +0000
 +++ server/util.c
+@@ -366,7 +366,7 @@ AP_DECLARE(char *) ap_pregsub(apr_pool_t
+     char *dest, *dst;
+     char c;
+     size_t no;
+-    int len;
++    apr_size_t len;
+ 
+     if (!source)
+         return NULL;
 @@ -391,6 +391,8 @@ AP_DECLARE(char *) ap_pregsub(apr_pool_t
              len++;
          }
