f3be5f68a4e0a8bf8b9366e7d3929eb314ed2771
reed <reed@pkgsrc.org>
2012-01-19 15:26:59 +0000

    Bump API dependency due to eggdbus dependency removed.

--- security/libgnome-keyring/buildlink3.mk
+++ security/libgnome-keyring/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/05/27 14:02:57 hans Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2012/01/19 15:26:59 reed Exp $
 
 BUILDLINK_TREE+=	libgnome-keyring
 
 .if !defined(LIBGNOME_KEYRING_BUILDLINK3_MK)
 LIBGNOME_KEYRING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgnome-keyring+=	libgnome-keyring>=2.30.1
+BUILDLINK_API_DEPENDS.libgnome-keyring+=	libgnome-keyring>=2.32.0
 BUILDLINK_ABI_DEPENDS.libgnome-keyring?=	libgnome-keyring>=2.32.0nb1
 BUILDLINK_PKGSRCDIR.libgnome-keyring?=	../../security/libgnome-keyring
 
