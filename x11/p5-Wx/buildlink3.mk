a9872e98e2116ee59382ef21d44b25756e4242d5
abs <abs>
2012-01-22 19:03:31 +0000

    Updated x11/p5-Wx to 0.9903
    0.9903 Wed Nov 09 08:00:00 BST 2011
        - Remove force of static binding for mingw libgcc & libstdc++
    0.9902 Thu Oct 13 08:00:00 BST 2011
        - 2.9.2 will now build with recent mingw compilers (>= gcc 4.5.2)
        - Fixed MSVC6 build (broken in 0.9702)
        - wrapped wxLogChain::DetachOldLog
        - added wxDF_UNICODETEXT
        - added wxMediaCtrl constants for all events
    
    0.9901 Mon June 06 12:00:00 BST 2011
        - Bumped ExtUtils::XSpp dependency to 0.1602
        - added Wx::Frame::SendSizeEvent
    
    0.99  Thu May 05 08:00:00 BST 2011
        - Release 0.98_01 as 0.99
    
    0.98_01 Thu Apr 21 20:31:30 CEST 2010
        - Added wxProgressDialog::Pulse (wxWidgets 2.8)
        - Mac SetFrontProcess can be switched off for editors
          and tests using env variable. Still switched on by default.
          Added detail to Wx pod
        - Added several wxLog functions & methods + Wx::PLlog::Flush
        - Added wxDateTime::IsValid wxSplitterWindow::UpdateSize
        - fix for Wx::Locale::FindLanguageInfo / GetLanguageInfo
        - wxPli_create_evthandler added for VListBox and VScrolledWindow
        - Added OnDrawSeparator and OnDrawBackground for VListBox
        - added GradientFillConcentric & DrawLabel for wxDC

--- x11/p5-Wx/buildlink3.mk
+++ x11/p5-Wx/buildlink3.mk
@@ -1,23 +1,23 @@
-# $NetBSD: buildlink3.mk,v 1.7 2011/11/01 06:03:04 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2012/01/22 19:03:31 abs Exp $
 
-BUILDLINK_TREE+=        p5-Wx
+BUILDLINK_TREE+=	p5-Wx
 
 .if !defined(P5_WX_BUILDLINK3_MK)
 P5_WX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.p5-Wx+=  p5-Wx>=0.91
-BUILDLINK_ABI_DEPENDS.p5-Wx?=    p5-Wx>=0.98nb6
-BUILDLINK_PKGSRCDIR.p5-Wx?=    ../../x11/p5-Wx
-BUILDLINK_INCDIRS.p5-Wx?=      ${PERL5_SUB_INSTALLVENDORARCH}/auto/Wx
+BUILDLINK_API_DEPENDS.p5-Wx+=	p5-Wx>=0.91
+BUILDLINK_ABI_DEPENDS.p5-Wx?=	p5-Wx>=0.98nb6
+BUILDLINK_PKGSRCDIR.p5-Wx?=	../../x11/p5-Wx
+BUILDLINK_INCDIRS.p5-Wx?=	${PERL5_SUB_INSTALLVENDORARCH}/auto/Wx
 
 # We want all of the arch-dependent DBI files.
-BUILDLINK_CONTENTS_FILTER.p5-Wx?=      ${GREP} '/auto/Wx/'
+BUILDLINK_CONTENTS_FILTER.p5-Wx?=	${GREP} '/auto/Wx/'
 
-BUILD_DEPENDS+=	p5-Alien-wxWidgets>=0.25:../../x11/p5-Alien-wxWidgets
-BUILD_DEPENDS+=	p5-ExtUtils-XSpp>=0.05:../../devel/p5-ExtUtils-XSpp
+BUILD_DEPENDS+= p5-Alien-wxWidgets>=0.25:../../x11/p5-Alien-wxWidgets
+BUILD_DEPENDS+= p5-ExtUtils-XSpp>=0.05:../../devel/p5-ExtUtils-XSpp
 
 .include "../../lang/perl5/buildlink3.mk"
 .include "../../x11/wxGTK28-contrib/buildlink3.mk"
 .endif # P5_WX_BUILDLINK3_MK
 
-BUILDLINK_TREE+=        -p5-Wx
+BUILDLINK_TREE+=	-p5-Wx
