1185e60ba8c274372be0a5d086e8599a505944a0
sbd <sbd@pkgsrc.org>
2012-01-24 09:10:06 +0000

    Use the correct version number for BUILDLINK_ABI_DEPENDS.gdbm

--- databases/gdbm/buildlink3.mk
+++ databases/gdbm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2012/01/24 08:26:23 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2012/01/24 09:10:06 sbd Exp $
 
 BUILDLINK_TREE+=	gdbm
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gdbm
 GDBM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gdbm+=	gdbm>=1.8.3
-BUILDLINK_ABI_DEPENDS.gdbm+=	gdbm>=1.9.1nb3
+BUILDLINK_ABI_DEPENDS.gdbm+=	gdbm>=1.10nb3
 BUILDLINK_PKGSRCDIR.gdbm?=	../../databases/gdbm
 
 .include "../../devel/gettext-lib/buildlink3.mk"
