fcbf624dbae267a6786adf3a177a1de2a2cd774c
sbd <sbd>
2012-01-24 08:26:23 +0000

    Add gettext-lib buildlink to gdbm's buildlink3.mk
    Bump ABI_DEPENDS

--- databases/gdbm/buildlink3.mk
+++ databases/gdbm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2011/08/23 13:06:48 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2012/01/24 08:26:23 sbd Exp $
 
 BUILDLINK_TREE+=	gdbm
 
@@ -6,8 +6,10 @@ BUILDLINK_TREE+=	gdbm
 GDBM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gdbm+=	gdbm>=1.8.3
-BUILDLINK_ABI_DEPENDS.gdbm+=	gdbm>=1.9.1
+BUILDLINK_ABI_DEPENDS.gdbm+=	gdbm>=1.9.1nb3
 BUILDLINK_PKGSRCDIR.gdbm?=	../../databases/gdbm
+
+.include "../../devel/gettext-lib/buildlink3.mk"
 .endif # GDBM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gdbm
