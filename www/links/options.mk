2847ae2aae562300f5fd80a263b33959c582482a
sbd <sbd@pkgsrc.org>
2012-01-27 08:24:28 +0000

    Add links-xz option to www/links and www/links-gnu
    Bump PKGREVISIONs

--- www/links/options.mk
+++ www/links/options.mk
@@ -1,10 +1,16 @@
-# $NetBSD: options.mk,v 1.1 2007/10/16 17:12:23 bjs Exp $
+# $NetBSD: options.mk,v 1.2 2012/01/27 08:24:28 sbd Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.links
-PKG_SUPPORTED_OPTIONS=	bzip2 links-zlib
-PKG_SUGGESTED_OPTIONS=	bzip2 links-zlib
+PKG_SUPPORTED_OPTIONS=	bzip2 links-zlib links-xz
+PKG_SUGGESTED_OPTIONS=	bzip2 links-zlib links-xz
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mlinks-xz)
+.  include "../../archivers/xz/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-lzma
+.endif
 
 .if !empty(PKG_OPTIONS:Mbzip2)
 .  include "../../archivers/bzip2/buildlink3.mk"
