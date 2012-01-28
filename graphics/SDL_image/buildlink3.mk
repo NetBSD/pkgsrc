13eee69034b2d39cbc26d95f33a0d3e28835cc9d
wiz <wiz@pkgsrc.org>
2012-01-28 11:00:01 +0000

    Update to 1.2.12. Add webp support. Update LICENSE (see below).
    While here, remove png/tiff/jpeg includes from buildlink3.mk
    since they are hidden by the library (library not linked against
    them, headers not included in public headers). Recursive bump
    coming next.
    
    1.2.12:
    Sam Lantinga - Thu Jan 19 23:18:09 EST 2012
     * Fixed regression in 1.2.11 loading 8-bit PNG images with libpng
    
    1.2.11:
    Sam Lantinga - Sat Jan 14 17:54:38 EST 2012
     * Fixed loading 8-bit PNG images on Mac OS X
    Sam Lantinga - Sat Dec 31 09:35:40 EST 2011
     * SDL_image is now under the zlib license
    Michael Bonfils - Mon Nov 28 21:46:00 EST 2011
     * Added WEBP image support
    Thomas Klausner - Wed Jan 19 19:31:25 PST 2011
     * Fixed compiling with libpng 1.4
    Sam Lantinga - Mon Jan 10 12:09:57 2011 -0800
     * Added Android.mk to build on the Android platform
    Sam Lantinga - Mon May 10 22:42:53 PDT 2010
     * Fixed loading HAM6 images with stencil mask
    Mark Tucker - Fri, 27 Nov 2009 12:38:21 -0500
     * Fixed bug loading 15 and 16 bit BMP images

--- graphics/SDL_image/buildlink3.mk
+++ graphics/SDL_image/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2011/01/13 13:36:11 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2012/01/28 11:00:01 wiz Exp $
 
 BUILDLINK_TREE+=	SDL_image
 
@@ -6,14 +6,11 @@ BUILDLINK_TREE+=	SDL_image
 SDL_IMAGE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.SDL_image+=	SDL_image>=1.2.3nb1
-BUILDLINK_ABI_DEPENDS.SDL_image+=	SDL_image>=1.2.10nb3
+BUILDLINK_ABI_DEPENDS.SDL_image+=	SDL_image>=1.2.12
 BUILDLINK_PKGSRCDIR.SDL_image?=	../../graphics/SDL_image
 BUILDLINK_INCDIRS.SDL_image?=	include/SDL
 
 .include "../../devel/SDL/buildlink3.mk"
-.include "../../mk/jpeg.buildlink3.mk"
-.include "../../graphics/png/buildlink3.mk"
-.include "../../graphics/tiff/buildlink3.mk"
 .endif # SDL_IMAGE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-SDL_image
