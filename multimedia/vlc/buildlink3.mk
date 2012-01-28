83949e20e139e58cd54a0a407a520c5533470037
wiz <wiz@pkgsrc.org>
2012-01-28 11:00:51 +0000

    Recursive bump for removal of jpeg/tiff/png buildlink3.mk files from SDL_image. If one of these packages needs them, it should include them itself.

--- multimedia/vlc/buildlink3.mk
+++ multimedia/vlc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.36 2011/11/16 01:33:16 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.37 2012/01/28 11:00:51 wiz Exp $
 
 BUILDLINK_TREE+=	vlc
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	vlc
 VLC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.vlc?=	vlc>=1.0.0
-BUILDLINK_ABI_DEPENDS.vlc?=	vlc>=1.1.12nb3
+BUILDLINK_ABI_DEPENDS.vlc?=	vlc>=1.1.13nb2
 BUILDLINK_PKGSRCDIR.vlc?=	../../multimedia/vlc
 
 pkgbase:= vlc
--- x11/paragui/buildlink3.mk
+++ x11/paragui/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2011/11/01 06:03:04 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2012/01/28 11:00:51 wiz Exp $
 
 BUILDLINK_TREE+=	paragui
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	paragui
 PARAGUI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.paragui+=	paragui>=1.0.4nb3
-BUILDLINK_ABI_DEPENDS.paragui+=	paragui>=1.0.4nb18
+BUILDLINK_ABI_DEPENDS.paragui+=	paragui>=1.0.4nb19
 BUILDLINK_PKGSRCDIR.paragui?=	../../x11/paragui
 
 .include "../../devel/SDL/buildlink3.mk"
