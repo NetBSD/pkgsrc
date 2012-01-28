b95fa8855ec839db610b76a2249bd33e58d970b5
wiz <wiz@pkgsrc.org>
2012-01-28 10:33:31 +0000

    Update to 1.2.15:
    General Notes
    
       * Fixed assembly register clobbering in CPU info routines
       * Fixed memory stomp when using stretch blit on large images
       * Fixed pixel corruption with overlapping blits
       * SDL_JOYSTICK_DEVICE can be a colon separated list of joystick devices
       * Disabled MMX blitters since they don't compile on modern compilers
    
    Unix Notes
    
       * Fixed crash in joystick code on newer Linux kernels
       * Fixed channel swizzling for ALSA target with 6-channel output
       * Use the OpenGL GLX_EXT_swap_control extension if available
       * XRandR support is disabled by default because it causes desktop reconfiguring. It can be enabled with the SDL_VIDEO_X11_XRANDR=1
         environment variable, or by applying this patch: http://hg.libsdl.org/SDL/raw-rev/8ec3036098df
    
    Windows Notes
    
       * Fixed SDL_GL_ACCELERATED_VISUAL handling
       * Fixed application state handling with ALT-Tab
       * Fixed occasional crash handling WM_ACTIVATEAPP in Direct X code
       * Fixed UTF-8 decoding of Russian characters
    
    Mac OS X Notes
    
       * Fixed building and running on Mac OS X 10.7 (Lion)

--- devel/SDL/patches/patch-src_video_wscons_SDL__wsconsevents.c
+++ devel/SDL/patches/patch-src_video_wscons_SDL__wsconsevents.c
@@ -1,19 +1,18 @@
-$NetBSD: patch-src_video_wscons_SDL__wsconsevents.c,v 1.3 2012/01/24 15:17:05 wiz Exp $
+$NetBSD: patch-src_video_wscons_SDL__wsconsevents.c,v 1.4 2012/01/28 10:33:31 wiz Exp $
 
 "Port" to NetBSD.
 
---- src/video/wscons/SDL_wsconsevents.c.orig	2009-10-12 23:07:14.000000000 +0000
+--- src/video/wscons/SDL_wsconsevents.c.orig	2012-01-19 06:30:06.000000000 +0000
 +++ src/video/wscons/SDL_wsconsevents.c
 @@ -146,8 +146,11 @@ void WSCONS_InitOSKeymap(_THIS)
    }
  
    switch (private->kbdType) {
 -#ifdef WSKBD_TYPE_ZAURUS
--  case WSKBD_TYPE_ZAURUS:
 +#if defined(__NetBSD__) || !defined(WSKBD_TYPE_ZAURUS)
 +  default:
 +#else
-+  case WSKBD_TYPE_ZAURUS:
+   case WSKBD_TYPE_ZAURUS:
 +#endif
      /* top row */
      keymap[2] = SDLK_1;
