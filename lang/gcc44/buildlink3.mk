e5c237451acf6f850a607b6247d9a1c2c1480356
hans <hans@pkgsrc.org>
2012-02-01 21:17:05 +0000

    Remove old BUILDLINK_FNAME_TRANSFORM, which seems to have been cargo-culted from an older gcc package directly installing into /usr/pkg. Add include/ to buildlinked files.

--- lang/gcc44/buildlink3.mk
+++ lang/gcc44/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2011/07/14 19:54:55 hans Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2012/02/01 21:17:05 hans Exp $
 
 BUILDLINK_TREE+=	gcc44
 
@@ -46,8 +46,7 @@ BUILDLINK_INCDIRS.gcc44+=	${_GCC44_SUBDIR}/include ${gcc44_GCC_ARCHDIR:S/^${BUIL
 
 BUILDLINK_FILES_CMD.gcc44=	\
 	(cd  ${BUILDLINK_PREFIX.gcc44} &&	\
-	${FIND} ${_GCC44_SUBDIR}/bin ${_GCC44_SUBDIR}/libexec ${_GCC44_SUBDIR}/lib \( -type f -o -type l \) -print)
-BUILDLINK_FNAME_TRANSFORM.gcc44=	-e s:buildlink:buildlink/gcc44:
+	${FIND} ${_GCC44_SUBDIR}/bin ${_GCC44_SUBDIR}/include ${_GCC44_SUBDIR}/libexec ${_GCC44_SUBDIR}/lib \( -type f -o -type l \) -print)
 
 # When not using the GNU linker, gcc will always link shared libraries
 # against the shared version of libgcc. Always enable _USE_GCC_SHILB on
