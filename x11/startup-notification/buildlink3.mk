8605b7ab338a34e210936f4a78dedfb1881ecfcd
wiz <wiz@pkgsrc.org>
2012-02-02 11:33:32 +0000

    On NetBSD<5.99 with native X, use startup-notification010 instead, which does not use XCB (which is not provided by native X in older NetBSD releases). From Robert Elz.

--- x11/startup-notification/buildlink3.mk
+++ x11/startup-notification/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/11/08 18:59:56 jakllsch Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2012/02/02 11:33:32 wiz Exp $
 
 BUILDLINK_TREE+=	startup-notification
 
@@ -7,7 +7,21 @@ STARTUP_NOTIFICATION_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5
 BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.8nb1
+
+.include "../../mk/bsd.fast.prefs.mk"
+.if ${X11_TYPE} == "modular"
 BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification
+.else
+.if ${OPSYS} == "NetBSD"
+.if !empty(OS_VERSION:M5.99.*) || !empty(OS_VERSION:M[6789].*)
+BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification
+.else
+BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification010
+.endif
+.else #!NetBSD
+BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification
+.endif
+.endif
 
 .include "../../x11/libSM/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
