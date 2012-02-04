58080bfe926503ac1731b1a303f543b156b5b6c2
adam <adam@pkgsrc.org>
2012-02-04 21:10:43 +0000

    Changes 2.6.12: Bug fixes: * GIMP 2.6.10 segfaults when CTRL-left click on a layer mask * GIMP fails to import a path from SVG * Crash or Gtk-CRITICAL on File->Open * [abrt] gimp-2:2.6.11-1.fc14: py-slice.py:172:slice:TypeError: integer argument   expected, got float * gimp(1) manpage fixes * Sample Colorize does not function non-interactively * file-pdf-load: Don't use deprecated API * file-psp: fix for bogus input data * Fixes for some buffer overflow problems * Guard against crash due to quitting while DND is processed * fails to build with -Werror=format-security * script-fu: make server IPv6 aware

---
+++ graphics/gimp/patches/patch-plug-ins_script-fu_script-fu-server.c
@@ -0,0 +1,18 @@
+$NetBSD: patch-plug-ins_script-fu_script-fu-server.c,v 1.1 2012/02/04 21:10:45 adam Exp $
+
+AI_ADDRCONFIG is not defined on some platforms.
+
+--- plug-ins/script-fu/script-fu-server.c.orig	2012-02-04 18:42:45.000000000 +0000
++++ plug-ins/script-fu/script-fu-server.c
+@@ -446,7 +446,11 @@ server_start (gint         port,
+   const gchar     *progress;
+ 
+   memset (&hints, 0, sizeof (hints));
++#ifdef AI_ADDRCONFIG
+   hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG;
++#else
++  hints.ai_flags = AI_PASSIVE;
++#endif
+   hints.ai_socktype = SOCK_STREAM;
+ 
+   port_s = g_strdup_printf ("%d", port);
