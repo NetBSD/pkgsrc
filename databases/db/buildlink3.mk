d2d1d099a2e022ee78265df7631269225145cc3b
obache <obache@pkgsrc.org>
2012-02-05 12:16:56 +0000

    Don't set BUILDLINK_LIBS for db1, db2 and db3, same as db4.
    It will add BDB libraries to LIBS, and may introduce unwanted linked against BDB
    libraries, whether needed or not.

--- databases/db/buildlink3.mk
+++ databases/db/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2009/03/20 19:24:04 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2012/02/05 12:16:56 obache Exp $
 
 BUILDLINK_TREE+=	db2
 
@@ -13,7 +13,7 @@ BUILDLINK_LDADD.db2=		-ldb2
 
 .  include "../../mk/bsd.fast.prefs.mk"
 .  if defined(USE_DB185) && !empty(USE_DB185:M[yY][eE][sS])
-BUILDLINK_LIBS.db2=		${BUILDLINK_LDADD.db2}
+#BUILDLINK_LIBS.db2=		${BUILDLINK_LDADD.db2}
 BUILDLINK_TRANSFORM+=		l:db:db2
 .  endif
 .endif # DB_BUILDLINK3_MK
