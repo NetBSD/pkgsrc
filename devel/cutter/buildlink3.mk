2b2e81dfe9aa8309061841bd43af572f22f662b6
wiz <wiz@pkgsrc.org>
2012-02-06 12:39:42 +0000

    Revbump for a) tiff update to 4.0 (shlib major change) b) glib2 update 2.30.2 (adds libffi dependency to buildlink3.mk)
    Enjoy.

--- devel/cutter/buildlink3.mk
+++ devel/cutter/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/04/22 13:42:27 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/02/06 12:39:47 wiz Exp $
 #
 
 BUILDLINK_TREE+=	cutter
@@ -8,7 +8,7 @@ CUTTER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cutter+=	cutter>=1.1.3
 BUILDLINK_DEPMETHOD.cutter?=	build
-BUILDLINK_ABI_DEPENDS.cutter?=	cutter>=1.1.7nb1
+BUILDLINK_ABI_DEPENDS.cutter?=	cutter>=1.2.0nb1
 BUILDLINK_PKGSRCDIR.cutter?=	../../devel/cutter
 
 .include "../../devel/glib2/buildlink3.mk"
