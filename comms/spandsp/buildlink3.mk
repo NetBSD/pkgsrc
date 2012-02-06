ef79af981e95438c4aa7cc0509361682c55f6c0e
wiz <wiz@pkgsrc.org>
2012-02-06 12:39:42 +0000

    Revbump for a) tiff update to 4.0 (shlib major change) b) glib2 update 2.30.2 (adds libffi dependency to buildlink3.mk)
    Enjoy.

--- comms/spandsp/buildlink3.mk
+++ comms/spandsp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/02/06 08:32:06 jnemeth Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/02/06 12:39:43 wiz Exp $
 
 BUILDLINK_TREE+=	spandsp
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	spandsp
 SPANDSP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.spandsp+=	spandsp>=0.0.6pre18
+BUILDLINK_ABI_DEPENDS.spandsp?=	spandsp>=0.0.6pre18nb1
 BUILDLINK_PKGSRCDIR.spandsp?=	../../comms/spandsp
 
 .include "../../graphics/tiff/buildlink3.mk"
