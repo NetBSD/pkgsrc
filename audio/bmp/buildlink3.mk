6091efa05df3c9199aee34824ab607eb3bde15cf
wiz <wiz>
2012-02-06 12:39:17 +0000

    Revbump for a) tiff update to 4.0 (shlib major change) b) glib2 update 2.30.2 (adds libffi dependency to buildlink3.mk)
    Enjoy.

--- audio/bmp/buildlink3.mk
+++ audio/bmp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2011/11/01 06:00:35 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2012/02/06 12:39:19 wiz Exp $
 
 BUILDLINK_TREE+=	bmp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	bmp
 BMP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.bmp+=	bmp>=0.9.7rc2nb3
-BUILDLINK_ABI_DEPENDS.bmp?=	bmp>=0.9.7.1nb13
+BUILDLINK_ABI_DEPENDS.bmp?=	bmp>=0.9.7.1nb16
 BUILDLINK_PKGSRCDIR.bmp?=	../../audio/bmp
 
 .include "../../devel/gettext-lib/buildlink3.mk"
