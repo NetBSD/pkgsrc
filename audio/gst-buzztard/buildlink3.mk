7ea3fe584179f9cb9d53babfffecf43524a23389
wiz <wiz@pkgsrc.org>
2012-02-06 12:39:17 +0000

    Revbump for a) tiff update to 4.0 (shlib major change) b) glib2 update 2.30.2 (adds libffi dependency to buildlink3.mk)
    Enjoy.

--- audio/gst-buzztard/buildlink3.mk
+++ audio/gst-buzztard/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/04/22 13:42:15 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2012/02/06 12:39:23 wiz Exp $
 
 BUILDLINK_TREE+=	gst-buzztard
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gst-buzztard
 GST_BUZZTARD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gst-buzztard+=	gst-buzztard>=0.5.0
-BUILDLINK_ABI_DEPENDS.gst-buzztard?=	gst-buzztard>=0.5.0nb1
+BUILDLINK_ABI_DEPENDS.gst-buzztard?=	gst-buzztard>=0.5.0nb3
 BUILDLINK_PKGSRCDIR.gst-buzztard?=	../../audio/gst-buzztard
 
 .include "../../devel/glib2/buildlink3.mk"
