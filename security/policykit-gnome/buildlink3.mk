867976f39145bb163de42b858fe6625e69bb4fb6
wiz <wiz@pkgsrc.org>
2012-02-06 12:41:28 +0000

    Revbump for a) tiff update to 4.0 (shlib major change) b) glib2 update 2.30.2 (adds libffi dependency to buildlink3.mk)
    Enjoy.

--- security/policykit-gnome/buildlink3.mk
+++ security/policykit-gnome/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2011/04/22 13:42:39 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2012/02/06 12:41:28 wiz Exp $
 
 BUILDLINK_TREE+=	policykit-gnome
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	policykit-gnome
 POLICYKIT_GNOME_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.policykit-gnome+=	policykit-gnome>=0.9.2
-BUILDLINK_ABI_DEPENDS.policykit-gnome+=	policykit-gnome>=0.9.2nb7
+BUILDLINK_ABI_DEPENDS.policykit-gnome+=	policykit-gnome>=0.9.2nb10
 BUILDLINK_PKGSRCDIR.policykit-gnome?=	../../security/policykit-gnome
 
 .include "../../security/policykit/buildlink3.mk"
