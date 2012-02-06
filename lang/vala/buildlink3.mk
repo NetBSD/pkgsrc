e9e77c989d6bfd48ac6d5c8af7068f5f11fb18a3
drochner <drochner>
2012-02-06 11:48:32 +0000

    set some variables to point clients to exact ("valac-0.12" rather than just "valac") versions, to prepare for parallel installation of multiple vala compiler versions

--- lang/vala/buildlink3.mk
+++ lang/vala/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2011/04/22 13:42:19 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2012/02/06 11:48:32 drochner Exp $
 
 BUILDLINK_TREE+=	vala
 
@@ -8,6 +8,12 @@ VALA_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.vala+=	vala>=0.10.1
 BUILDLINK_ABI_DEPENDS.vala?=	vala>=0.12.0nb1
 BUILDLINK_PKGSRCDIR.vala?=	../../lang/vala
+
+VALAC=${LOCALBASE}/bin/valac-0.12
+VAPIGEN=${LOCALBASE}/bin/vapigen-0.12
+.if defined(GNU_CONFIGURE)
+CONFIGURE_ENV+=	VALAC=${VALAC} VAPIGEN=${VAPIGEN}
+.endif
 
 .include "../../devel/glib2/buildlink3.mk"
 .endif	# VALA_BUILDLINK3_MK
