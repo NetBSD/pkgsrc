dda11ecf496ff14e17f7a603138cbf11e5c376b3
taca <taca@pkgsrc.org>
2012-02-08 09:52:15 +0000

    Don't hardcode RUBY_PKGPREFIX, too.

--- x11/ruby-gnome2-gtk/buildlink3.mk
+++ x11/ruby-gnome2-gtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2012/02/06 12:42:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2012/02/08 09:52:15 taca Exp $
 #
 
 BUILDLINK_TREE+=	ruby-gnome2-gtk
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	ruby-gnome2-gtk
 RUBY_GNOME2_GTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-gtk+=	${RUBY_PKGPREFIX}-gnome2-gtk>=0.17.0
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk?=	ruby19-gnome2-gtk>=1.1.2nb1
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk?=	${RUBY_PKGPREFIX}-gnome2-gtk>=1.1.2nb1
 BUILDLINK_PKGSRCDIR.ruby-gnome2-gtk?=	../../x11/ruby-gnome2-gtk
 
 .include "../../devel/ruby-gnome2-glib/buildlink3.mk"
