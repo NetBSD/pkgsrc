e6ad9e295d1b3e3ad9c95350b4d553af9d55f7f2
wiz <wiz@pkgsrc.org>
2012-02-12 14:17:10 +0000

    Add two files for support packages. From Tim Larson in PR 45958.

---
+++ games/alephone/scenario.mk
@@ -0,0 +1,5 @@
+# $NetBSD: scenario.mk,v 1.1 2012/02/12 14:17:10 wiz Exp $
+# common Makefile fragment for installing game data playable with Aleph One
+# e.g. games/marathon-* packages
+
+SCENARIO_BASE=	share/AlephOne/Scenarios
