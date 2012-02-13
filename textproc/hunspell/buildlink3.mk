f87f468cd74822cf4d48c22069a7fdb67eb27a76
wiz <wiz>
2012-02-13 14:46:11 +0000

    hunspell shlib name change -> recursive bump

--- textproc/hunspell/buildlink3.mk
+++ textproc/hunspell/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:25:29 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2012/02/13 14:46:11 wiz Exp $
 
 BUILDLINK_TREE+=	hunspell
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	hunspell
 HUNSPELL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hunspell+=	hunspell>=1.2.2
+BUILDLINK_ABI_DEPENDS.hunspell+=	hunspell>=1.3.2
 BUILDLINK_PKGSRCDIR.hunspell?=		../../textproc/hunspell
 .endif # HUNSPELL_BUILDLINK3_MK
 
