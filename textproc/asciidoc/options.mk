bf829f43da503784ab4fe931ea0a7d7b5569f1e3
gls <gls@pkgsrc.org>
2012-02-13 22:38:06 +0000

    - Simplify /usr/bin/env handling. - Add an option to enable source highlighting, dragging in textproc/source-highlight. Off by default.

--- textproc/asciidoc/options.mk
+++ textproc/asciidoc/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1 2010/08/25 02:17:27 gls Exp $
+# $NetBSD: options.mk,v 1.2 2012/02/13 22:38:06 gls Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.asciidoc
-PKG_SUPPORTED_OPTIONS=	pdf epub
+PKG_SUPPORTED_OPTIONS=	pdf epub highlight
 PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
@@ -12,4 +12,7 @@ DEPENDS+=	dblatex-[0-9]*:../../textproc/dblatex
 .if !empty(PKG_OPTIONS:Mepub)
 DEPENDS+=	libxml2-[0-9]*:../../textproc/libxml2
 DEPENDS+=	libxslt-[0-9]*:../../textproc/libxslt
+.endif
+.if !empty(PKG_OPTIONS:Mhighlight)
+DEPENDS+=	source-highlight-[0-9]*:../../textproc/source-highlight
 .endif
