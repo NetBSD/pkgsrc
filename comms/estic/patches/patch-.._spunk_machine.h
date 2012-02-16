e8f567c17e9bd1dee23f0571605c36c24537ab89
hans <hans@pkgsrc.org>
2012-02-16 16:47:57 +0000

    Fix build on SunOS.

--- comms/estic/patches/patch-.._spunk_machine.h
+++ comms/estic/patches/patch-.._spunk_machine.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-.._spunk_machine.h,v 1.1 2011/11/27 19:36:09 joerg Exp $
+$NetBSD: patch-.._spunk_machine.h,v 1.2 2012/02/16 16:47:58 hans Exp $
 
 --- ../spunk/machine.h.orig	2011-11-27 02:08:02.000000000 +0000
 +++ ../spunk/machine.h
@@ -11,3 +11,12 @@ $NetBSD: patch-.._spunk_machine.h,v 1.1 2011/11/27 19:36:09 joerg Exp $
  #    error Byte order not defined!
  #endif
  
+@@ -312,7 +312,7 @@ typedef unsigned        u32;            
+ 
+ #ifdef SOLARIS
+ 
+-#include <sys/endian.h>
++#include <sys/byteorder.h>
+ #ifdef _BIG_ENDIAN
+ #    define CPU_BIG_ENDIAN
+ #else
