022cd2268febfec3ea39298fc92d5198e7bda993
hans <hans@pkgsrc.org>
2012-02-16 18:04:08 +0000

    Remove set but unused variable.

--- converters/libwps/patches/patch-src_lib_WPS8.cpp
+++ converters/libwps/patches/patch-src_lib_WPS8.cpp
@@ -1,8 +1,26 @@
-$NetBSD: patch-src_lib_WPS8.cpp,v 1.1 2011/02/04 20:10:32 adam Exp $
+$NetBSD: patch-src_lib_WPS8.cpp,v 1.2 2012/02/16 18:04:08 hans Exp $
 
 --- src/lib/WPS8.cpp.orig	2011-02-04 20:04:48.000000000 +0000
 +++ src/lib/WPS8.cpp
-@@ -128,7 +128,7 @@ void WPS8Parser::appendUTF16LE(WPXInputS
+@@ -159,7 +159,7 @@ void WPS8Parser::readStreams(WPXInputStr
+ 
+ 	for (unsigned i=0; i < n_streams; i++) {
+ 		uint16_t len;
+-		uint32_t unknown, type = 0;
++		uint32_t type = 0;
+ 
+ 		len = readU16(input);
+ 		if (len > 10) {
+@@ -168,7 +168,7 @@ void WPS8Parser::readStreams(WPXInputStr
+ 		}
+ 
+ 		if (len > 4) {
+-			unknown = readU32(input); // assume == 0x22000000
++			(void) readU32(input); // assume == 0x22000000
+ 			type = readU32(input);
+ 		} else input->seek(len-2,WPX_SEEK_CUR);
+ 
+@@ -251,7 +251,7 @@ void WPS8Parser::appendUTF16LE(WPXInputS
  	uint16_t high_surrogate = 0;
  	bool fail = false;
  	uint16_t readVal;
