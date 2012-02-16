9a9f32d43ade3a746bb9a041a6a585551a872e75
hans <hans@pkgsrc.org>
2012-02-16 17:22:39 +0000

    Buildlink textproc/wbxml2 in buildlink3.mk.

--- comms/libsyncml/buildlink3.mk
+++ comms/libsyncml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:02 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/02/16 17:22:39 hans Exp $
 
 BUILDLINK_TREE+=	libsyncml
 
@@ -7,6 +7,8 @@ LIBSYNCML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libsyncml+=	libsyncml>=0.4.5
 BUILDLINK_PKGSRCDIR.libsyncml?=	../../comms/libsyncml
+
+.include "../../textproc/wbxml2/buildlink3.mk"
 .endif # LIBSYNCML_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libsyncml
