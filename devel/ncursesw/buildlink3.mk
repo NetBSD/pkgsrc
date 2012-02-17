f04e52a37e4a760fee763b81ca33b0cc4257c0f9
obache <obache@pkgsrc.org>
2012-02-17 11:01:01 +0000

    Try to buildlink with ncurses with same conditions as ncursesw was built.
    Fixes buildlink for the case builtin ncurses was not used for building ncursesw.

--- devel/ncursesw/buildlink3.mk
+++ devel/ncursesw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:24:24 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2012/02/17 11:01:01 obache Exp $
 
 BUILDLINK_TREE+=	ncursesw
 
@@ -11,6 +11,18 @@ BUILDLINK_PKGSRCDIR.ncursesw?=		../../devel/ncursesw
 
 BUILDLINK_LIBNAME.ncursesw=	ncursesw
 BUILDLINK_LDADD.ncursesw=	${BUILDLINK_LIBNAME.ncursesw:S/^/-l/:S/^-l$//}
+
+CHECK_BUILTIN.ncursesw:=	yes
+.  include "builtin.mk"
+CHECK_BUILTIN.ncursesw:=	no
+.  if defined(IS_BUILTIN.ncursesw) && !empty(IS_BUILTIN.ncursesw:M[nN][oO])
+PKG_BUILD_DEPENDS.ncurses!=	${PKG_INFO} -qn ncursesw 2> /dev/null \
+	|| { cd ../../devel/ncurses && ${MAKE} ${MAKEFLAGS} show-depends; }
+.    for d in ${PKG_BUILD_DEPENDS.ncurses:Mncurses[-><]*}
+BUILDLINK_API_DEPENDS.ncurses+=	${d}
+.    endfor
+USE_NCURSES=			yes
+.  endif
 
 .include "../../devel/ncurses/buildlink3.mk"
 .endif # NCURSESW_BUILDLINK3_MK
