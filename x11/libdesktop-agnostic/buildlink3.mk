ec14531bfb96e51cabba91f289d28feef359e556
imil <imil@pkgsrc.org>
2012-02-19 14:23:00 +0000

    Initial import of libdesktop-agnostic, version 0.3.92, into the NetBSD Packages Collection.
    This library provides an extensible configuration API, a unified
    virtual file system API, and a desktop item editor (all with
    pluggable backends) for GLib-based projects. It is not tied to any one
    desktop environment, although there are desktop-specific modules.

---
+++ x11/libdesktop-agnostic/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/02/19 14:23:00 imil Exp $
+
+BUILDLINK_TREE+=	libdesktop-agnostic
+
+.if !defined(LIBDESKTOP_AGNOSTIC_BUILDLINK3_MK)
+LIBDESKTOP_AGNOSTIC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libdesktop-agnostic+=	libdesktop-agnostic>=0.3.92
+BUILDLINK_ABI_DEPENDS.libdesktop-agnostic+=	libdesktop-agnostic>=0.3.92
+BUILDLINK_PKGSRCDIR.libdesktop-agnostic?=	../../x11/libdesktop-agnostic
+
+.include "../../lang/vala014/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../devel/GConf/buildlink3.mk"
+.include "../../devel/py-gobject/buildlink3.mk"
+.include "../../devel/gobject-introspection/buildlink3.mk"
+.endif	# LIBDESKTOP_AGNOSTIC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libdesktop-agnostic
