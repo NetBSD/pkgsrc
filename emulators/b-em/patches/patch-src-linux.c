cd058a6d5231e5d233d2f234f41bb82df4d6ec16
reinoud <reinoud@pkgsrc.org>
2012-02-20 15:45:19 +0000

    Add emulators/b-em, a Freeware Acorn BBC Micro Emulator with 2nd processors

---
+++ emulators/b-em/patches/patch-src-linux.c
@@ -0,0 +1,13 @@
+$NetBSD: patch-src-linux.c,v 1.1 2012/02/20 15:45:19 reinoud Exp $
+
+--- src/linux.c.orig	2011-02-12 15:52:50.000000000 +0000
++++ src/linux.c
+@@ -62,6 +62,8 @@ int main(int argc, char *argv[])
+         get_executable_name(exedir,511);
+         p=get_filename(exedir);
+         p[0]=0;
++	/* data was referred relative to the executable; fix: */
++	strcpy(exedir, "@BEMDATADIR@");
+         loadconfig();
+ //        printf("Main\n");
+         initbbc(argc,argv);
---
+++ emulators/b-em/patches/patch-src-win.c
@@ -0,0 +1,12 @@
+$NetBSD: patch-src-win.c,v 1.1 2012/02/20 15:45:19 reinoud Exp $
+
+--- src/win.c.orig	2011-02-12 17:34:10.000000000 +0000
++++ src/win.c
+@@ -346,6 +346,7 @@ int WINAPI WinMain (HINSTANCE hThisInsta
+         get_executable_name(exedir,511);
+         p=get_filename(exedir);
+         p[0]=0;
++	strcpy(exedir, "@BEMDATADIR@");
+ 
+         loadconfig();
+ 
