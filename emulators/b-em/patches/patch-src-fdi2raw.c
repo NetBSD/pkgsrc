a45f91c2e7976a2058b88116aa9426b88390329f
reinoud <reinoud@pkgsrc.org>
2012-02-20 15:45:19 +0000

    Add emulators/b-em, a Freeware Acorn BBC Micro Emulator with 2nd processors

---
+++ emulators/b-em/patches/patch-src-fdi2raw.c
@@ -0,0 +1,40 @@
+$NetBSD: patch-src-fdi2raw.c,v 1.1 2012/02/20 15:45:19 reinoud Exp $
+
+--- src/fdi2raw.c.orig	2010-10-17 11:23:56.000000000 +0000
++++ src/fdi2raw.c
+@@ -1628,7 +1628,7 @@ static void fdi2_decode (FDI *fdi, uint3
+ 				randval = rand();
+ 				if (randval < (RAND_MAX / 2)) {
+ 					if (randval > (RAND_MAX / 4)) {
+-						if (randval <= (3 * RAND_MAX / 8))
++						if (randval <= (3 * (RAND_MAX / 8)))
+ 							randval = (2 * randval) - (RAND_MAX /4);
+ 						else
+ 							randval = (4 * randval) - RAND_MAX;
+@@ -1637,7 +1637,7 @@ static void fdi2_decode (FDI *fdi, uint3
+ 				} else {
+ 					randval -= RAND_MAX / 2;
+ 					if (randval > (RAND_MAX / 4)) {
+-						if (randval <= (3 * RAND_MAX / 8))
++						if (randval <= (3 * (RAND_MAX / 8)))
+ 							randval = (2 * randval) - (RAND_MAX /4);
+ 						else
+ 							randval = (4 * randval) - RAND_MAX;
+@@ -1663,7 +1663,7 @@ static void fdi2_decode (FDI *fdi, uint3
+ 				randval = rand();
+ 				if (randval < (RAND_MAX / 2)) {
+ 					if (randval > (RAND_MAX / 4)) {
+-						if (randval <= (3 * RAND_MAX / 8))
++						if (randval <= (3 * (RAND_MAX / 8)))
+ 							randval = (2 * randval) - (RAND_MAX /4);
+ 						else
+ 							randval = (4 * randval) - RAND_MAX;
+@@ -1672,7 +1672,7 @@ static void fdi2_decode (FDI *fdi, uint3
+ 				} else {
+ 					randval -= RAND_MAX / 2;
+ 					if (randval > (RAND_MAX / 4)) {
+-						if (randval <= (3 * RAND_MAX / 8))
++						if (randval <= (3 * (RAND_MAX / 8)))
+ 							randval = (2 * randval) - (RAND_MAX /4);
+ 						else
+ 							randval = (4 * randval) - RAND_MAX;
