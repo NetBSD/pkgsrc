7926747cbb2d2cbeb76653e8fd6d32ce49f889d9
hans <hans@pkgsrc.org>
2012-02-20 14:34:33 +0000

    Don't just remove WTF::yield, #ifdef it out for SunOS. Fixes build on NetBSD and probably others, too. Spotted by Dieter Roelants.

--- www/webkit-gtk/patches/patch-Source_JavaScriptCore_wtf_Threading.h
+++ www/webkit-gtk/patches/patch-Source_JavaScriptCore_wtf_Threading.h
@@ -1,9 +1,11 @@
 --- Source/JavaScriptCore/wtf/Threading.h.orig	2011-03-21 19:41:47.000000000 +0100
 +++ Source/JavaScriptCore/wtf/Threading.h	2011-09-13 17:47:57.423475599 +0200
-@@ -113,6 +113,5 @@ using WTF::createThread;
+@@ -113,6 +113,8 @@ using WTF::createThread;
  using WTF::currentThread;
  using WTF::detachThread;
  using WTF::waitForThreadCompletion;
--using WTF::yield;
++#if !OS(SOLARIS)
+ using WTF::yield;
++#endif
  
  #endif // Threading_h
