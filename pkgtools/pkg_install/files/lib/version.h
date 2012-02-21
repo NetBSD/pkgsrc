3b43fb570e82d273e828bd8d4342eb874e62358e
wiz <wiz@pkgsrc.org>
2012-02-21 14:54:41 +0000

    Bump version for pkg_add bug fix.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.163 2012/01/28 12:33:05 joerg Exp $	*/
+/*	$NetBSD: version.h,v 1.164 2012/02/21 14:54:41 wiz Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION 20120128
+#define PKGTOOLS_VERSION 20120221
 
 #endif /* _INST_LIB_VERSION_H_ */
