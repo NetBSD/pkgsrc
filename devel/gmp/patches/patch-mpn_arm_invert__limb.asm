8f4fd4e76183e2142040417ecb9dc7574c576767
drochner <drochner@pkgsrc.org>
2012-02-21 13:18:19 +0000

    update to 5.0.4 changes: -bugfixes -New CPUs recognised

--- devel/gmp/patches/patch-mpn_arm_invert__limb.asm
+++ devel/gmp/patches/patch-mpn_arm_invert__limb.asm
@@ -1,11 +1,11 @@
-$NetBSD: patch-mpn_arm_invert__limb.asm,v 1.1 2011/11/16 21:30:07 is Exp $
+$NetBSD: patch-mpn_arm_invert__limb.asm,v 1.2 2012/02/21 13:18:19 drochner Exp $
 
---- mpn/arm/invert_limb.asm.orig	2011-05-08 09:49:29.000000000 +0000
+--- mpn/arm/invert_limb.asm.orig	2012-01-27 08:59:30.000000000 +0000
 +++ mpn/arm/invert_limb.asm
 @@ -42,7 +42,7 @@ L(2):	add	r2, pc, r2
- 	adds	r1, r3, r0
- 	adc	r12, r12, r0
- 	rsb	r0, r12, r2
+ 	adds	r1, r12, r0
+ 	adc	r3, r3, r0
+ 	rsb	r0, r3, r2
 -	bx	lr
 +	mov	pc,lr
  
