c0ed98faeb292b5d914748709ef45d7657c7f59c
obache <obache@pkgsrc.org>
2012-02-22 11:00:06 +0000

    Update firefox36 to 3.6.27.
    pkgsrc changes:
    * use own mozilla-common.mk, some dependency differ from recent Firefox.
    * switch to use system cairo.
    
    Fixed in Firefox 3.6.27
    * MFSA 2012-11 libpng integer overflow
    Fixed in Firefox 3.6.26
    * MFSA 2012-08 Crash with malformed embedded XSLT stylesheets
    * MFSA 2012-07 Potential Memory Corruption When Decoding Ogg Vorbis files
    * MFSA 2012-04 Child nodes from nsDOMAttribute still accessible after removal of nodes
    * MFSA 2012-02 Overly permissive IPv6 literal syntax
    * MFSA 2012-01 Miscellaneous memory safety hazards (rv:10.0/ rv:1.9.2.26)
    Fixed in Firefox 3.6.25
    * MFSA 2011-59 .jar not treated as executable in Firefox 3.6 on Mac

---
+++ devel/xulrunner192/mozilla-common.mk
@@ -0,0 +1,93 @@
+# $NetBSD: mozilla-common.mk,v 1.1 2012/02/22 11:00:07 obache Exp $
+#
+# common Makefile fragment for mozilla packages based on gecko 2.0.
+# 
+# used by devel/xulrunner192/Makefile
+# used by www/firefox36/Makefile
+
+GNU_CONFIGURE=		yes
+USE_TOOLS+=		pkg-config perl gmake autoconf213 unzip zip
+USE_LANGUAGES+=		c99 c++
+UNLIMIT_RESOURCES+=	datasize
+
+PKG_DESTDIR_SUPPORT=	user-destdir
+CHECK_PORTABILITY_SKIP+=${MOZILLA_DIR}security/nss/tests/libpkix/libpkix.sh
+CHECK_PORTABILITY_SKIP+=${MOZILLA_DIR}security/nss/tests/multinit/multinit.sh
+CHECK_INTERPRETER_SKIP+=lib/xulrunner-sdk/sdk/bin/xpt.py
+PRIVILEGED_STAGES+=	clean
+
+CONFIGURE_ARGS+=	--disable-tests --disable-pedantic
+CONFIGURE_ARGS+=	--enable-crypto
+CONFIGURE_ARGS+=	--enable-optimize=-O2 --with-pthreads
+CONFIGURE_ARGS+=	--disable-javaxpcom
+CONFIGURE_ARGS+=	--enable-default-toolkit=cairo-gtk2
+CONFIGURE_ARGS+=	--enable-svg --enable-mathml
+CONFIGURE_ARGS+=	--enable-system-cairo
+CONFIGURE_ARGS+=	--with-system-jpeg
+CONFIGURE_ARGS+=	--with-system-zlib --with-system-bz2
+CONFIGURE_ARGS+=	--with-system-libevent=${BUILDLINK_PREFIX.libevent}
+CONFIGURE_ARGS+=	--enable-system-sqlite
+CONFIGURE_ARGS+=	--disable-crashreporter
+CONFIGURE_ARGS+=	--disable-libnotify
+CONFIGURE_ARGS+=	--disable-necko-wifi
+CONFIGURE_ARGS+=	--enable-chrome-format=flat
+
+SUBST_CLASSES+=			fix-paths
+SUBST_STAGE.fix-paths=		pre-configure
+SUBST_MESSAGE.fix-paths=	Fixing absolute paths.
+SUBST_FILES.fix-paths=		${MOZILLA_DIR}xpcom/build/nsXPCOMPrivate.h
+SUBST_FILES.fix-paths+=		${MOZILLA_DIR}xpcom/io/nsAppFileLocationProvider.cpp
+SUBST_SED.fix-paths+=		-e 's,/usr/lib/mozilla/plugins,${PREFIX}/lib/netscape/plugins,g'
+
+CONFIG_GUESS_OVERRIDE+=		${MOZILLA_DIR}build/autoconf/config.guess
+CONFIG_GUESS_OVERRIDE+=		${MOZILLA_DIR}js/src/build/autoconf/config.guess
+CONFIG_GUESS_OVERRIDE+=		${MOZILLA_DIR}nsprpub/build/autoconf/config.guess
+CONFIG_GUESS_OVERRIDE+=		${MOZILLA_DIR}/js/ctypes/libffi/config.guess
+CONFIG_SUB_OVERRIDE+=		${MOZILLA_DIR}build/autoconf/config.sub
+CONFIG_SUB_OVERRIDE+=		${MOZILLA_DIR}js/src/build/autoconf/config.sub
+CONFIG_SUB_OVERRIDE+=		${MOZILLA_DIR}nsprpub/build/autoconf/config.sub
+CONFIG_SUB_OVERRIDE+=		${MOZILLA_DIR}/js/ctypes/libffi/config.sub
+
+PYTHON_FOR_BUILD_ONLY=		yes
+.include "../../lang/python/application.mk"
+CONFIGURE_ENV+=		PYTHON=${PYTHONBIN:Q}
+
+# Makefiles sometimes call "rm -f" without more arguments. Kludge around ...
+.PHONY: create-rm-wrapper
+pre-configure: create-rm-wrapper
+create-rm-wrapper:
+	printf '#!/bin/sh\n[ "$$*" = "-f" ] && exit 0\nexec /bin/rm $$@\n' > \
+	  ${WRAPPER_DIR}/bin/rm
+	chmod +x ${WRAPPER_DIR}/bin/rm
+
+.include "../../mk/bsd.prefs.mk"
+
+.if ${OPSYS} == "NetBSD"
+# The configure test for __thread succeeds, but later we end up with:
+# dist/bin/libxul.so: undefined reference to `__tls_get_addr'
+CONFIGURE_ENV+=	ac_cv_thread_keyword=no
+.endif
+
+.if ${OPSYS} == "SunOS"
+# native libbz2.so hides BZ2_crc32Table
+PREFER.bzip2?=	pkgsrc
+.endif
+
+.if ${OPSYS} == "Linux"
+.include "../../audio/alsa-lib/buildlink3.mk"
+.endif
+.include "../../archivers/bzip2/buildlink3.mk"
+BUILDLINK_API_DEPENDS.sqlite3+=	sqlite3>=3.7.5
+CONFIGURE_ENV+=	ac_cv_sqlite_secure_delete=yes	# c.f. patches/patch-al
+.include "../../databases/sqlite3/buildlink3.mk"
+BUILDLINK_API_DEPENDS.libevent+=	libevent>=1.1
+.include "../../devel/libevent/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
+BUILDLINK_API_DEPENDS.cairo+=	cairo>=1.8.8
+.include "../../graphics/cairo/buildlink3.mk"
+.include "../../net/libIDL/buildlink3.mk"
+BUILDLINK_API_DEPENDS.gtk2+=	gtk2+>=2.18.3nb1
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
