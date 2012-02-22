283635384558f0e78455c10281bf0869c5393335
wiz <wiz>
2012-02-22 15:27:13 +0000

    Update to 0.62, keeping all pkgsrc patches which have not been fed upstream (hi roy! hi rillig!)
    2011-12-10 PuTTY 0.62 released
    
    PuTTY 0.62 is out, containing only bug fixes from 0.61, in particular a security fix preventing passwords from being accidentally
    retained in memory.
    
    2011-11-27 PuTTY 0.62 pre-release builds available
    
    PuTTY 0.61 had a few noticeable bugs in it (but nothing security-related), so we are planning to make a 0.62 release containing just bug
    fixes. The Wishlist page lists the bugs that will be fixed by the 0.62 release. The Download page now contains pre-release snapshots of
    0.62, which contain those bug fixes and should be otherwise stable. (The usual development snapshots, containing other development since
    0.61, are also still available.)
    
    2011-07-12 PuTTY 0.61 is released
    
    PuTTY 0.61 is out, after over four years (sorry!), with new features, bug fixes, and compatibility updates for Windows 7 and various SSH
    server software.

--- security/putty/patches/patch-windows_window.c
+++ security/putty/patches/patch-windows_window.c
@@ -1,12 +1,10 @@
-$NetBSD: patch-al,v 1.1 2009/02/25 18:08:01 roy Exp $
+$NetBSD: patch-windows_window.c,v 1.1 2012/02/22 15:27:17 wiz Exp $
 
 Make the home/end keys work on BSD servers as well as Linux ones
 
-Index: windows/window.c
-===================================================================
---- windows/window.c	(revision 8466)
-+++ windows/window.c	(working copy)
-@@ -4189,8 +4189,17 @@
+--- windows/window.c.orig	2011-07-16 11:26:19.000000000 +0000
++++ windows/window.c
+@@ -4302,8 +4302,17 @@ static int TranslateKey(UINT message, WP
  		p += sprintf((char *) p, "\x1BO%c", code + 'P' - 11);
  	    return p - output;
  	}
@@ -16,12 +14,12 @@ Index: windows/window.c
 +	if (code == 1 || code == 4) {
 +	    /* Send the correct XTerm or rxvt codes for home/end
 +	     * We used to send ^[1~ and [4~ for Xterm,
-+	     * but those are Linux console */
++             * but those are Linux console */
 +	    const char *he;
 +	    if (cfg.rxvt_homeend)
-+	        he = code == 1 ? "\x1B[7~" : "\x1B[8~";
++		he = code == 1 ? "\x1B[7~" : "\x1B[8~";
 +	    else
-+	        he = code == 1 ? "\x1BOH" : "\x1BOF";
++		he = code == 1 ? "\x1BOH" : "\x1BOF";
 +	    p += sprintf((char *) p, he);
  	    return p - output;
  	}
