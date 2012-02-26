f9f82a485bd40ce8a3318da6884b5f17bc447a28
adam <adam@pkgsrc.org>
2012-02-26 22:16:16 +0000

    Rewritten patch-src_3rdparty_webkit_Source_WebCore_platform_DefaultLocalizationStrategy.cpp, so it only affects compiling with Clang

--- x11/qt4-libs/patches/patch-src_3rdparty_webkit_Source_WebCore_platform_DefaultLocalizationStrategy.cpp
+++ x11/qt4-libs/patches/patch-src_3rdparty_webkit_Source_WebCore_platform_DefaultLocalizationStrategy.cpp
@@ -1,15 +1,18 @@
-$NetBSD: patch-src_3rdparty_webkit_Source_WebCore_platform_DefaultLocalizationStrategy.cpp,v 1.1 2012/01/12 22:59:58 adam Exp $
+$NetBSD: patch-src_3rdparty_webkit_Source_WebCore_platform_DefaultLocalizationStrategy.cpp,v 1.2 2012/02/26 22:16:16 adam Exp $
 
 Fix C++ error.
 
 --- src/3rdparty/webkit/Source/WebCore/platform/DefaultLocalizationStrategy.cpp.orig	2012-01-12 19:51:48.000000000 +0100
 +++ src/3rdparty/webkit/Source/WebCore/platform/DefaultLocalizationStrategy.cpp	2012-01-12 20:07:06.000000000 +0100
-@@ -59,7 +59,7 @@
+@@ -59,7 +59,11 @@
      return result.get();
  #elif PLATFORM(QT)
      va_list arguments;
--    va_start(arguments, format);
++#ifdef __clang__
 +    va_start(arguments, format.charactersWithNullTermination());
++#else
+     va_start(arguments, format);
++#endif
      QString result;
      result.vsprintf(format.latin1().data(), arguments);
      va_end(arguments);
