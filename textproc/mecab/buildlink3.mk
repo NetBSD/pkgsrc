02e7008cf9e44339e35313d749c128ede87b7cc8
obache <obache@pkgsrc.org>
2012-02-27 11:53:06 +0000

    Update mecab to 0.993.
    2012-01-27 MeCab 0.993
    * Fix to works well for MeCab::Tagger::formatNode()
    * To reduce stack size, save almost of local variables(arrays) to heap.
    
    2012-01-14 MeCab 0.992
    * Fix typos in source codes.
    
    2012-01-14 MeCab 0.991
    * Fix parse error with null strings or space strings.
    * Fix failure for creation of user dictionaries.
    
    2011-12-24 MeCab 0.99
    * Add MeCab::Model MeCab::Lattice classes.
      Improve usabilities on multi threads environment. Multiple threads can parse
      with shareing same dictionary.
    * Remove function for automatic dictionary reference share when refereing same
      dictionaly (You can do same works with MeCab::Model).
    * Add function for atomic dictionaly update in parsing. Model::swap()
    * Change Windows version to Unicode binary.
    * Remove experimental online learning, HMM and EM learning codes.
    * Remove MeCab::Node:(begin|end)_node_list members.
    * minor bug fixes.

--- textproc/mecab/buildlink3.mk
+++ textproc/mecab/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:31 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/02/27 11:53:07 obache Exp $
 
 BUILDLINK_TREE+=	mecab
 
 .if !defined(MECAB_BUILDLINK3_MK)
 MECAB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mecab+=	mecab>=0.90
+BUILDLINK_API_DEPENDS.mecab+=	mecab>=0.99
 BUILDLINK_PKGSRCDIR.mecab?=	../../textproc/mecab
 
 .include "../../textproc/mecab-base/buildlink3.mk"
