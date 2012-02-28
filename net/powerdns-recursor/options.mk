5d95606b0536e6bc18e80f5c35b1a21441008a11
roy <roy@pkgsrc.org>
2012-02-28 10:49:27 +0000

    Bump to 3.3, changes include: * Many threading changes for greater perfomance * Can now make TCP/IP queries to remote IPv6 addresses * Domains can be forwarded to IPv6 addresses

--- net/powerdns-recursor/options.mk
+++ net/powerdns-recursor/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2009/12/23 13:00:41 ghen Exp $
+# $NetBSD: options.mk,v 1.2 2012/02/28 10:49:27 roy Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.powerdns-recursor
 PKG_SUPPORTED_OPTIONS=	lua
@@ -13,4 +13,3 @@ MAKE_ENV+=		LUA=1
 MAKE_ENV+=		LUA_LIBS_CONFIG=-llua
 .include "../../lang/lua/buildlink3.mk"
 .endif
-
