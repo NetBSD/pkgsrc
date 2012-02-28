a979f8955bfe446713d50f960f4cd2cc3db8ee93
drochner <drochner@pkgsrc.org>
2012-02-28 18:28:14 +0000

    update to 0.10.36 changes: -new GstAudioDecoder and GstAudioEncoder base classes -new GstAudioInfo audio format description API (which matches the new API  in 0.11) -add support for compressed audio payloading and passhthrough  (think S/PDIF, mp3 over bluetooth) -new tag convenience API to get info about creative commons licenses -new GstTagMux base class -new video API to force key units via events -new video overlay composition API for subtitles -decodebin/playbin2 now plug parsers by default

--- multimedia/gst-plugins0.10-base/buildlink3.mk
+++ multimedia/gst-plugins0.10-base/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2012/02/06 12:41:03 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2012/02/28 18:28:14 drochner Exp $
 
 BUILDLINK_TREE+=	gst-plugins0.10-base
 
@@ -10,6 +10,7 @@ BUILDLINK_ABI_DEPENDS.gst-plugins0.10-base?=	gst-plugins0.10-base>=0.10.35nb1
 BUILDLINK_PKGSRCDIR.gst-plugins0.10-base?=	../../multimedia/gst-plugins0.10-base
 
 .include "../../multimedia/gstreamer0.10/buildlink3.mk"
+.include "../../devel/orc/buildlink3.mk"
 .endif # GST_PLUGINS0.10_BASE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gst-plugins0.10-base
