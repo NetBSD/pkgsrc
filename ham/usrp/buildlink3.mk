b051fc5ec1b0c271d8fb15af1773383bea8d7f6f
hans <hans@pkgsrc.org>
2012-02-29 16:32:19 +0000

    Recursive bump from devel/boost-libs update.

--- ham/usrp/buildlink3.mk
+++ ham/usrp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2012/01/09 02:54:24 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2012/02/29 16:32:25 hans Exp $
 
 BUILDLINK_TREE+=	usrp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	usrp
 USRP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.usrp+=	usrp>=3.0.4
-BUILDLINK_ABI_DEPENDS.usrp?=	usrp>=3.3.0nb2
+BUILDLINK_ABI_DEPENDS.usrp?=	usrp>=3.3.0nb4
 BUILDLINK_PKGSRCDIR.usrp?=	../../ham/usrp
 
 .include "../../ham/gnuradio-core/buildlink3.mk"
--- math/cgal/buildlink3.mk
+++ math/cgal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/02/27 12:46:27 reinoud Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/02/29 16:32:25 hans Exp $
 
 BUILDLINK_TREE+=	cgal
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	cgal
 CGAL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cgal+=	cgal>=3.9
+BUILDLINK_ABI_DEPENDS.cgal?=	cgal>=3.9nb2
 BUILDLINK_PKGSRCDIR.cgal?=	../../math/cgal
 
 .include "../../devel/boost-headers/buildlink3.mk"
