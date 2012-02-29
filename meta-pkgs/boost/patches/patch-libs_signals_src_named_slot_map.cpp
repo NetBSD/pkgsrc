d5b6afb4e6d12bc1c8250830427abc9ca5cd2743
hans <hans@pkgsrc.org>
2012-02-29 16:37:28 +0000

    Add build fix for build with gcc46. From https://svn.boost.org/trac/boost/attachment/ticket/4999/fix4999.patch

---
+++ meta-pkgs/boost/patches/patch-libs_signals_src_named_slot_map.cpp
@@ -0,0 +1,20 @@
+--- libs/signals/src/named_slot_map.cpp.orig	2009-11-08 02:48:37.000000000 +0100
++++ libs/signals/src/named_slot_map.cpp	2011-10-24 16:04:24.749105300 +0200
+@@ -102,7 +102,7 @@ void named_slot_map::disconnect(const st
+       i->first.disconnect();
+       i = next;
+     }
+-    groups.erase(group);
++    groups.erase((const_group_iterator) group);
+   }
+ }
+ 
+@@ -125,7 +125,7 @@ void named_slot_map::remove_disconnected
+     }
+ 
+     // Clear out empty groups
+-    if (empty(g)) groups.erase(g++);
++    if (empty(g)) groups.erase((const_group_iterator) g++);
+     else ++g;
+   }
+ }
