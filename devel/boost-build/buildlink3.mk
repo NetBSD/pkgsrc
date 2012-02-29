0df5c194ce80bd1e5de97da2e7f4d2d6582ed756
hans <hans@pkgsrc.org>
2012-02-29 14:22:23 +0000

    Update boost to 1.49.0
    Changes:
    New Libraries
    * Heap: Priority queue data structures, from Tim Blechmann.
    
    Details: http://www.boost.org/users/history/version_1_49_0.html

--- devel/boost-build/buildlink3.mk
+++ devel/boost-build/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.20 2012/01/08 11:35:07 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2012/02/29 14:22:23 hans Exp $
 
 BUILDLINK_TREE+=	boost-build
 
 .if !defined(BOOST_BUILD_BUILDLINK3_MK)
 BOOST_BUILD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.48.*
+BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.49.*
 BUILDLINK_DEPMETHOD.boost-build?=	build
 BUILDLINK_PKGSRCDIR.boost-build?=	../../devel/boost-build
 .endif # BOOST_BUILD_BUILDLINK3_MK
