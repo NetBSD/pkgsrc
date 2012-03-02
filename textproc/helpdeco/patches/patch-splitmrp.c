7adc4bcec63183f5db81aa00a9f98114f5af4689
hans <hans@pkgsrc.org>
2012-03-02 12:11:01 +0000

    Fix build on SunOS. - use dirent.h instead of sys/dir.h - use FILENAME_MAX instead of NAME_MAX - use uintXX_t instead of u_intXX_t

---
+++ textproc/helpdeco/patches/patch-splitmrp.c
@@ -0,0 +1,15 @@
+--- splitmrb.c.orig	2005-11-17 12:19:38.000000000 +0100
++++ splitmrb.c	2012-03-02 13:05:31.593391213 +0100
+@@ -237,9 +237,9 @@ int GetPackedByte(FILE *f) // RulLen dec
+ #endif
+ typedef struct
+ {
+-  u_int8_t c1,c2,c3;
+-  u_int16_t x,y,w,h;
+-  u_int32_t hash;
++  uint8_t c1,c2,c3;
++  uint16_t x,y,w,h;
++  uint32_t hash;
+ } HOTSPOT
+ #if defined(__GNUC__) && __GNUC__ < 3
+ __attribute__((packed))
