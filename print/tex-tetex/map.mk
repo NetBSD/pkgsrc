a10c1f8c875dc2bddaf0678b7c7d8b8de1e0f8b0
minskim <minskim@pkgsrc.org>
2012-03-04 01:01:43 +0000

    Sync tex-tetex{,-doc} with TeX Live r25424.
    This version supports Japanese font maps for pTeX and pLaTeX.

--- print/tex-tetex/map.mk
+++ print/tex-tetex/map.mk
@@ -1,4 +1,4 @@
-# $NetBSD: map.mk,v 1.1 2011/11/06 08:43:28 minskim Exp $
+# $NetBSD: map.mk,v 1.2 2012/03/04 01:01:43 minskim Exp $
 #
 # This Makefile fragment is intended to be included by packages that
 # install font maps for TeX.  It updates font map entries in updmap.cfg
@@ -10,16 +10,19 @@
 #
 #    TEX_MIXEDMAP_FILES - A list of mixed map files.
 #
+#    TEX_KANJIMAP_FILES - A list of kanji map files.
+#
 
 .if !defined(TEX_MAP_MK)
 TEX_MAP_MK=		# defined
 
-DEPENDS+=		tex-tetex>=3.0.24482:../../print/tex-tetex
+DEPENDS+=		tex-tetex>=3.0.25424:../../print/tex-tetex
 
 EVAL_PREFIX+=		TETEX_PREFIX=tex-tetex
 FILES_SUBST+=		UPDMAP_SYS=${TETEX_PREFIX:Q}/bin/updmap-sys
 FILES_SUBST+=		MAP_FILES=${TEX_MAP_FILES:Q}
 FILES_SUBST+=		MIXEDMAP_FILES=${TEX_MIXEDMAP_FILES:Q}
+FILES_SUBST+=		KANJIMAP_FILES=${TEX_KANJIMAP_FILES:Q}
 INSTALL_TEMPLATES+=	../../print/tex-tetex/files/map-install.tmpl
 DEINSTALL_TEMPLATES+=	../../print/tex-tetex/files/map-deinstall.tmpl
 
