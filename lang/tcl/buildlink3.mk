e18181dbca0e6e4b6b0d487291bbc93cf82e488b
obache <obache>
2012-03-05 13:35:20 +0000

    Use tclsh from pkgsrc for the case lang/tcl is buildlinked.

--- lang/tcl/buildlink3.mk
+++ lang/tcl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2009/06/14 22:58:03 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2012/03/05 13:35:20 obache Exp $
 
 BUILDLINK_TREE+=	tcl
 
@@ -18,6 +18,8 @@ BUILDLINK_TRANSFORM+=	l:tcl:tcl84
 BUILDLINK_TRANSFORM+=	l:tcl8.4:tcl84
 
 TCLCONFIG_SH?=		${BUILDLINK_PREFIX.tcl}/lib/tclConfig.sh
+
+_TOOLS_USE_PKGSRC.tclsh=	yes
 
 .include "../../mk/dlopen.buildlink3.mk"
 .endif # TCL_BUILDLINK3_MK
