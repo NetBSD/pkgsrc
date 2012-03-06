e6caf0090981c47d6609b8ef98cab2c9abb9fdce
minskim <minskim@pkgsrc.org>
2012-03-06 03:01:30 +0000

    Update emacs-snapshot to 24.0.94.20120305.
    Notable changes:
    	* simple.el (count-words): If called from Lisp, return the word
    	count, for symmetry with `count-lines'.  Arglist changed.
    	* faces.el (face-spec-reset-face): For the default face, reset the
    	attributes to default values (Bug#10748).
    	* mail/emacsbug.el (report-emacs-bug-hook): Fix up thinko in
    	previous patch: Check `message-send-mail-function', and not the
    	default function (bug#10897).
    	* cus-start.el: Make x-select-enable-clipboard-manager
    	customizable.
    	* Depessimize the handling of very large macros.

--- editors/emacs-snapshot/version.mk
+++ editors/emacs-snapshot/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.11 2012/02/02 20:21:10 minskim Exp $
+# $NetBSD: version.mk,v 1.12 2012/03/06 03:01:30 minskim Exp $
 
 _EMACS_FLAVOR=	emacs
 _EMACS_REQD=	emacs>=23
@@ -7,4 +7,4 @@ _EMACS_PKGDEP.leim=
 
 _EMACS_VERSION_MAJOR=	24
 _EMACS_VERSION_MINOR=	0
-_EMACS_VERSION_MICRO=	93
+_EMACS_VERSION_MICRO=	94
