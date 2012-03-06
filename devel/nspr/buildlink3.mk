7a6f65818699b74311890b8d37904c5c8f761d95
ryoon <ryoon@pkgsrc.org>
2012-03-06 13:37:32 +0000

    Bump BUILDLINK_API_DEPENDS and BUILDLINK_ABI_DEPENDS.

--- devel/nspr/buildlink3.mk
+++ devel/nspr/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.17 2012/02/13 12:54:43 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2012/03/06 13:37:32 ryoon Exp $
 
 BUILDLINK_TREE+=	nspr
 
 .if !defined(NSPR_BUILDLINK3_MK)
 NSPR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.8.7
+BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.9.2beta1
 BUILDLINK_PKGSRCDIR.nspr?=	../../devel/nspr
 
 BUILDLINK_FILES.nspr+=          lib/nspr/*
--- devel/nss/buildlink3.mk
+++ devel/nss/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.13 2011/04/26 14:20:03 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2012/03/06 13:37:32 ryoon Exp $
 
 BUILDLINK_TREE+=	nss
 
 .if !defined(NSS_BUILDLINK3_MK)
 NSS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nss+=	nss>=3.12.9.0
+BUILDLINK_API_DEPENDS.nss+=	nss>=3.13.1
 BUILDLINK_PKGSRCDIR.nss?=	../../devel/nss
 
 BUILDLINK_LIBDIRS.nss+=		lib/nss
