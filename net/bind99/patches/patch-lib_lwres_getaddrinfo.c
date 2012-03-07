c916a23f110b3c9f2c1b333599e9c033ecfdd27d
taca <taca>
2012-03-07 14:25:00 +0000

    Importing BIND 9.9.0 as pkgsrc/net/bind99.
    Introduction
    
       BIND 9.9.0 is the first production release of BIND 9.9.
    
       This document summarizes changes from BIND 9.8 to BIND 9.9.
       Please see the CHANGES file in the source code release for a
       complete list of all changes.
    
    New Features
    
    * The new "inline-signing" option
    * NXDOMAIN redirection
    * "rndc flushtree <name>" command
    * "rndc sync" command
    * The new "rndc signing" command
    * "auto-dnssec" zones
    * Improves the startup time
    
    And more.

---
+++ net/bind99/patches/patch-lib_lwres_getaddrinfo.c
@@ -0,0 +1,41 @@
+$NetBSD: patch-lib_lwres_getaddrinfo.c,v 1.1.1.1 2012/03/07 14:25:00 taca Exp $
+
+* Add fix for KAME based implementation.
+
+--- lib/lwres/getaddrinfo.c.orig	2008-11-25 23:47:23.000000000 +0000
++++ lib/lwres/getaddrinfo.c
+@@ -142,6 +142,10 @@
+ #include <lwres/netdb.h>
+ #include <lwres/stdlib.h>
+ 
++#ifdef __KAME__
++#include <net/if.h>
++#endif
++
+ #define SA(addr)	((struct sockaddr *)(addr))
+ #define SIN(addr)	((struct sockaddr_in *)(addr))
+ #define SIN6(addr)	((struct sockaddr_in6 *)(addr))
+@@ -366,14 +370,22 @@ lwres_getaddrinfo(const char *hostname, 
+ 			p = strchr(ntmp, '%');
+ 			ep = NULL;
+ 
++#ifdef __KAME__
++			if (p != NULL) {
++				scopeid = if_nametoindex(p + 1);
++				if (scopeid)
++					p = NULL;
++			}
++#endif
+ 			/*
+ 			 * Vendors may want to support non-numeric
+ 			 * scopeid around here.
+ 			 */
+ 
+-			if (p != NULL)
++			if (p != NULL) {
+ 				scopeid = (lwres_uint32_t)strtoul(p + 1,
+ 								  &ep, 10);
++			}
+ 			if (p != NULL && ep != NULL && ep[0] == '\0')
+ 				*p = '\0';
+ 			else {
