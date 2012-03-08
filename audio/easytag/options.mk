59c0f4ca9602373e5fdfe0053e76725f8a47718a
wiz <wiz@pkgsrc.org>
2012-03-08 09:49:25 +0000

    Update to 2.1.7, the new stable version. Changes compared to easytag-current (2.1.6):
    2.1.7 - Jan 14th, 2012 :
    ========================
        * Gaute Amundsen's ConfirmWhenUnsavedFiles.patch
        * Mark Ferry's albumartist patch for ogg and flac
        * Götz Waschk's update for German translation
        * Kip's optimized base64 decoder for album art
        * Updated contributor credits
        * Debianized
        * Updated version to 2.1.7

--- audio/easytag/options.mk
+++ audio/easytag/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.4 2007/11/21 21:47:25 drochner Exp $
+# $NetBSD: options.mk,v 1.5 2012/03/08 09:49:26 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.easytag
-PKG_SUPPORTED_OPTIONS=	flac mpeg4ip ogg
-PKG_SUGGESTED_OPTIONS=	flac mpeg4ip ogg
+PKG_SUPPORTED_OPTIONS=	flac mpeg4ip ogg speex wavpack
+PKG_SUGGESTED_OPTIONS=	flac mpeg4ip ogg speex wavpack
 
 .include "../../mk/bsd.options.mk"
 
@@ -26,4 +26,18 @@ CPPFLAGS+=		-DHAVE_GTK
 CONFIGURE_ARGS+=	--enable-ogg
 .else
 CONFIGURE_ARGS+=	--disable-ogg
+.endif
+
+.if !empty(PKG_OPTIONS:Mspeex)
+.  include "../../audio/speex/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-speex
+.else
+CONFIGURE_ARGS+=	--disable-speex
+.endif
+
+.if !empty(PKG_OPTIONS:Mwavpack)
+.  include "../../audio/wavpack/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-wavpack
+.else
+CONFIGURE_ARGS+=	--disable-wavpack
 .endif
