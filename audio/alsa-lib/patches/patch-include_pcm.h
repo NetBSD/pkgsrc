39fa49272306db67b248bebd00ff5619e05e0423
sbd <sbd>
2012-03-08 04:04:19 +0000

    In the blow commit changing the type of snd_pcm_format_silence* in pcm.h from u_int*_t to uint*_t breaks the build on other OS's (namely Linux), so wrap the change in an "#ifdef __sun" block.
    ---
       Module Name:	pkgsrc
       Committed By:	hans
       Date:		Wed Feb 15 19:43:51 UTC 2012
    
       Modified Files:
       	pkgsrc/audio/alsa-lib: Makefile distinfo
       	pkgsrc/audio/alsa-lib/patches: patch-ac patch-ad patch-ae patch-af
       	    patch-ai patch-aj patch-ak patch-am patch-ap patch-aq patch-ar
       	    patch-at patch-au patch-aw patch-ax patch-ay patch-az patch-ba
       	    patch-bb patch-bf patch-bg patch-bh patch-include_pcm.h
       Added Files:
       	pkgsrc/audio/alsa-lib/patches: patch-src_control_control__hw.c
    
       Log Message:
       Fix build on SunOS.
       Inspired by the patches used for the native package in OI.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.8 -r1.9 pkgsrc/audio/alsa-lib/Makefile
       cvs rdiff -u -r1.9 -r1.10 pkgsrc/audio/alsa-lib/distinfo
       cvs rdiff -u -r1.2 -r1.3 pkgsrc/audio/alsa-lib/patches/patch-ac \
           pkgsrc/audio/alsa-lib/patches/patch-ad \
           pkgsrc/audio/alsa-lib/patches/patch-ae \
           pkgsrc/audio/alsa-lib/patches/patch-af \
           pkgsrc/audio/alsa-lib/patches/patch-ai \
           pkgsrc/audio/alsa-lib/patches/patch-aj \
           pkgsrc/audio/alsa-lib/patches/patch-ak \
           pkgsrc/audio/alsa-lib/patches/patch-am \
           pkgsrc/audio/alsa-lib/patches/patch-ar \
           pkgsrc/audio/alsa-lib/patches/patch-at \
           pkgsrc/audio/alsa-lib/patches/patch-au \
           pkgsrc/audio/alsa-lib/patches/patch-ax \
           pkgsrc/audio/alsa-lib/patches/patch-ay \
           pkgsrc/audio/alsa-lib/patches/patch-az \
           pkgsrc/audio/alsa-lib/patches/patch-ba \
           pkgsrc/audio/alsa-lib/patches/patch-bb \
           pkgsrc/audio/alsa-lib/patches/patch-bf \
           pkgsrc/audio/alsa-lib/patches/patch-bh
       cvs rdiff -u -r1.3 -r1.4 pkgsrc/audio/alsa-lib/patches/patch-ap \
           pkgsrc/audio/alsa-lib/patches/patch-aw
       cvs rdiff -u -r1.1.1.1 -r1.2 pkgsrc/audio/alsa-lib/patches/patch-aq \
           pkgsrc/audio/alsa-lib/patches/patch-bg
       cvs rdiff -u -r1.1 -r1.2 pkgsrc/audio/alsa-lib/patches/patch-include_pcm.h
       cvs rdiff -u -r0 -r1.1 \
           pkgsrc/audio/alsa-lib/patches/patch-src_control_control__hw.c

--- audio/alsa-lib/patches/patch-include_pcm.h
+++ audio/alsa-lib/patches/patch-include_pcm.h
@@ -1,7 +1,7 @@
-$NetBSD: patch-include_pcm.h,v 1.2 2012/02/15 19:43:51 hans Exp $
+$NetBSD: patch-include_pcm.h,v 1.3 2012/03/08 04:04:19 sbd Exp $
 
---- include/pcm.h.orig	2009-12-16 16:18:51.000000000 +0100
-+++ include/pcm.h	2012-02-15 14:10:10.589697413 +0100
+--- include/pcm.h.orig	2009-12-16 15:18:51.000000000 +0000
++++ include/pcm.h
 @@ -456,7 +456,9 @@ int snd_pcm_wait(snd_pcm_t *pcm, int tim
  int snd_pcm_link(snd_pcm_t *pcm1, snd_pcm_t *pcm2);
  int snd_pcm_unlink(snd_pcm_t *pcm);
@@ -12,18 +12,21 @@ $NetBSD: patch-include_pcm.h,v 1.2 2012/02/15 19:43:51 hans Exp $
  
  /*
   * application helpers - these functions are implemented on top
-@@ -920,10 +922,10 @@ int snd_pcm_format_width(snd_pcm_format_
+@@ -920,10 +922,17 @@ int snd_pcm_format_width(snd_pcm_format_
  int snd_pcm_format_physical_width(snd_pcm_format_t format);		/* in bits */
  snd_pcm_format_t snd_pcm_build_linear_format(int width, int pwidth, int unsignd, int big_endian);
  ssize_t snd_pcm_format_size(snd_pcm_format_t format, size_t samples);
--u_int8_t snd_pcm_format_silence(snd_pcm_format_t format);
--u_int16_t snd_pcm_format_silence_16(snd_pcm_format_t format);
--u_int32_t snd_pcm_format_silence_32(snd_pcm_format_t format);
--u_int64_t snd_pcm_format_silence_64(snd_pcm_format_t format);
++#ifdef __sun
 +uint8_t snd_pcm_format_silence(snd_pcm_format_t format);
 +uint16_t snd_pcm_format_silence_16(snd_pcm_format_t format);
 +uint32_t snd_pcm_format_silence_32(snd_pcm_format_t format);
 +uint64_t snd_pcm_format_silence_64(snd_pcm_format_t format);
++#else
+ u_int8_t snd_pcm_format_silence(snd_pcm_format_t format);
+ u_int16_t snd_pcm_format_silence_16(snd_pcm_format_t format);
+ u_int32_t snd_pcm_format_silence_32(snd_pcm_format_t format);
+ u_int64_t snd_pcm_format_silence_64(snd_pcm_format_t format);
++#endif
  int snd_pcm_format_set_silence(snd_pcm_format_t format, void *buf, unsigned int samples);
  
  snd_pcm_sframes_t snd_pcm_bytes_to_frames(snd_pcm_t *pcm, ssize_t bytes);
