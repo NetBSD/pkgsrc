065ed09306571e151457c31df4d41488d5e286f5
wiz <wiz@pkgsrc.org>
2012-03-09 12:33:57 +0000

    Bump PKGREVISION for python default version change to 2.7. py-* not affected, since it built different versions depending on the setting already.

--- chat/pidgin/buildlink3.mk
+++ chat/pidgin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2012/03/06 17:38:54 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2012/03/09 12:34:02 wiz Exp $
 
 BUILDLINK_TREE+=	pidgin
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	pidgin
 PIDGIN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pidgin+=	pidgin>=2.5.1
-BUILDLINK_ABI_DEPENDS.pidgin+=	pidgin>=2.10.0nb4
+BUILDLINK_ABI_DEPENDS.pidgin+=	pidgin>=2.10.0nb5
 BUILDLINK_PKGSRCDIR.pidgin?=	../../chat/pidgin
 
 .include "../../chat/libpurple/buildlink3.mk"
