2c21efccaf7fdf05fd825cabf223c788fd8a8db1
fhajny <fhajny>
2012-03-09 11:48:35 +0000

    Only include the OpenSSL BL3 if mysql-client was built with the 'ssl' option.

--- databases/mysql5-client/buildlink3.mk
+++ databases/mysql5-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2010/09/23 15:22:43 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2012/03/09 11:48:35 fhajny Exp $
 
 BUILDLINK_TREE+=	mysql-client
 
@@ -11,7 +11,13 @@ BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql5-client
 BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
 BUILDLINK_INCDIRS.mysql-client?=	include/mysql
 
-.include "../../security/openssl/buildlink3.mk"
+pkgbase := mysql-client
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.mysql-client:Mssl)
+.  include "../../security/openssl/buildlink3.mk"
+.endif
+
 .include "../../devel/zlib/buildlink3.mk"
 .endif # MYSQL_CLIENT_BUILDLINK3_MK
 
