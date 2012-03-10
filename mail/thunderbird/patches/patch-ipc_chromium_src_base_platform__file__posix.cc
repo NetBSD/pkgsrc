c2d8be41e23f48c16d2301f75e0e507f6c9717e0
ryoon <ryoon@pkgsrc.org>
2012-03-10 11:42:38 +0000

    Update to 10.0.2
    * Add new features.
    * Fix security bugs
    * See http://www.mozilla.org/en-US/thunderbird/10.0/releasenotes/

--- mail/thunderbird/patches/patch-ipc_chromium_src_base_platform__file__posix.cc
+++ mail/thunderbird/patches/patch-ipc_chromium_src_base_platform__file__posix.cc
@@ -1,8 +1,8 @@
-$NetBSD: patch-ipc_chromium_src_base_platform__file__posix.cc,v 1.1 2011/08/19 14:39:10 tnn Exp $
+$NetBSD: patch-ipc_chromium_src_base_platform__file__posix.cc,v 1.2 2012/03/10 11:42:38 ryoon Exp $
 
---- mozilla/ipc/chromium/src/base/platform_file_posix.cc.orig	2011-06-15 21:57:27.000000000 +0000
+--- mozilla/ipc/chromium/src/base/platform_file_posix.cc.orig	2012-02-16 10:25:01.000000000 +0000
 +++ mozilla/ipc/chromium/src/base/platform_file_posix.cc
-@@ -9,6 +9,7 @@
+@@ -10,6 +10,7 @@
  #ifdef ANDROID
  #include <linux/stat.h>
  #endif
