be8ee8dcc82719b1b83ec6808b66fd6fbd2cebd6
wiz <wiz@pkgsrc.org>
2012-03-10 13:31:55 +0000

    Fix patterns so that pbulk does not try to build startup-notification on systems where startup-notification010 is needed instead. Tested by bouyer.

--- x11/startup-notification/buildlink3.mk
+++ x11/startup-notification/buildlink3.mk
@@ -1,12 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.12 2012/02/02 11:33:32 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2012/03/10 13:31:55 wiz Exp $
 
 BUILDLINK_TREE+=	startup-notification
 
 .if !defined(STARTUP_NOTIFICATION_BUILDLINK3_MK)
 STARTUP_NOTIFICATION_BUILDLINK3_MK:=
-
-BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5
-BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.8nb1
 
 .include "../../mk/bsd.fast.prefs.mk"
 .if ${X11_TYPE} == "modular"
@@ -15,8 +12,12 @@ BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification
 .if ${OPSYS} == "NetBSD"
 .if !empty(OS_VERSION:M5.99.*) || !empty(OS_VERSION:M[6789].*)
 BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification
+BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5
+BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.8nb1
 .else
 BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification010
+BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5<0.12
+BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.8nb1<0.12
 .endif
 .else #!NetBSD
 BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification
