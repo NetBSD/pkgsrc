d40663e8044d0c06e6ef894660853058c8d9197f
ryoon <ryoon>
2012-03-10 03:26:04 +0000

    Update to 2.7.2
    * Many new features.
    * Security bugfixes.
    * See http://www.seamonkey-project.org/releases/seamonkey2.7/

--- www/seamonkey/patches/patch-toolkit_toolkit-tiers.mk
+++ www/seamonkey/patches/patch-toolkit_toolkit-tiers.mk
@@ -1,8 +1,8 @@
-$NetBSD: patch-toolkit_toolkit-tiers.mk,v 1.1 2011/07/11 20:46:36 tnn Exp $
+$NetBSD: patch-toolkit_toolkit-tiers.mk,v 1.2 2012/03/10 03:26:06 ryoon Exp $
 
---- mozilla/toolkit/toolkit-tiers.mk.orig	2011-06-15 21:57:56.000000000 +0000
+--- mozilla/toolkit/toolkit-tiers.mk.orig	2012-02-16 14:09:39.000000000 +0000
 +++ mozilla/toolkit/toolkit-tiers.mk
-@@ -90,7 +90,7 @@ ifndef MOZ_NATIVE_BZ2
+@@ -82,7 +82,7 @@ ifndef MOZ_NATIVE_BZ2
  tier_platform_dirs += modules/libbz2
  endif
  tier_platform_dirs += modules/libmar
