f2a76f00a3614770909b5a290448ac9bda23c4ec
ryoon <ryoon@pkgsrc.org>
2012-03-10 03:26:04 +0000

    Update to 2.7.2
    * Many new features.
    * Security bugfixes.
    * See http://www.seamonkey-project.org/releases/seamonkey2.7/

--- www/seamonkey/patches/patch-mozilla_media_libvpx_vpx_config_c.c
+++ www/seamonkey/patches/patch-mozilla_media_libvpx_vpx_config_c.c
@@ -1,18 +1,21 @@
-$NetBSD: patch-mozilla_media_libvpx_vpx_config_c.c,v 1.1 2012/01/07 20:44:32 marino Exp $
+$NetBSD: patch-mozilla_media_libvpx_vpx_config_c.c,v 1.2 2012/03/10 03:26:06 ryoon Exp $
 
---- mozilla/media/libvpx/vpx_config_c.c.orig	2011-11-07 21:08:15.000000000 +0000
+--- mozilla/media/libvpx/vpx_config_c.c.orig	2012-02-16 14:09:23.000000000 +0000
 +++ mozilla/media/libvpx/vpx_config_c.c
-@@ -12,11 +12,11 @@
+@@ -12,12 +12,12 @@
  /* 32 bit MacOS. */
  #include "vpx_config_x86-darwin9-gcc.c"
  
 -#elif defined(__linux__) && defined(__i386__)
-+#elif (defined(__linux__) || defined(__DragonFly__)) && defined(__i386__)
- /* 32 bit Linux. */
+-/* 32 bit Linux. */
++#elif (defined(__linux__) | defined(__DragonFly__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__MirBSD__)) && defined(__i386__)
++/* 32 bit Linux or BSD. */
  #include "vpx_config_x86-linux-gcc.c"
  
 -#elif defined(__linux__) && defined(__x86_64__)
-+#elif (defined(__linux__) || defined(__DragonFly__)) && defined(__x86_64__)
- /* 64 bit Linux. */
+-/* 64 bit Linux. */
++#elif (defined(__linux__) | defined(__DragonFly__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__MirBSD__)) && defined(__x86_64__)
++/* 64 bit Linux or BSD. */
  #include "vpx_config_x86_64-linux-gcc.c"
  
+ #elif defined(__sun) && defined(__i386)
