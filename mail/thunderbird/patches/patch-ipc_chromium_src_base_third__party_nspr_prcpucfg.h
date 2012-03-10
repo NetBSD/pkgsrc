aac29eb3f5bf125bccb0b87de1e89201d1f96f46
ryoon <ryoon>
2012-03-10 11:42:38 +0000

    Update to 10.0.2
    * Add new features.
    * Fix security bugs
    * See http://www.mozilla.org/en-US/thunderbird/10.0/releasenotes/

--- mail/thunderbird/patches/patch-ipc_chromium_src_base_third__party_nspr_prcpucfg.h
+++ mail/thunderbird/patches/patch-ipc_chromium_src_base_third__party_nspr_prcpucfg.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-ipc_chromium_src_base_third__party_nspr_prcpucfg.h,v 1.1 2011/08/19 14:39:10 tnn Exp $
+$NetBSD: patch-ipc_chromium_src_base_third__party_nspr_prcpucfg.h,v 1.2 2012/03/10 11:42:38 ryoon Exp $
 
---- mozilla/ipc/chromium/src/base/third_party/nspr/prcpucfg.h.orig	2011-06-15 21:57:27.000000000 +0000
+--- mozilla/ipc/chromium/src/base/third_party/nspr/prcpucfg.h.orig	2012-02-16 10:25:01.000000000 +0000
 +++ mozilla/ipc/chromium/src/base/third_party/nspr/prcpucfg.h
 @@ -34,7 +34,7 @@
  #include "base/third_party/nspr/prcpucfg_win.h"
@@ -9,5 +9,5 @@ $NetBSD: patch-ipc_chromium_src_base_third__party_nspr_prcpucfg.h,v 1.1 2011/08/
 -#elif defined(__linux__) || defined(ANDROID)
 +#elif defined(__linux__) || defined(ANDROID) || defined(__NetBSD__) || defined(__DragonFly__)
  #include "base/third_party/nspr/prcpucfg_linux.h"
- #else
- #error Provide a prcpucfg.h appropriate for your platform
+ #elif defined(__OpenBSD__)
+ #include "base/third_party/nspr/prcpucfg_openbsd.h"
