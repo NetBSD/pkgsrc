570c5c9b4cabb4169f81f441dda797ae13500685
markd <markd@pkgsrc.org>
2012-03-11 20:26:57 +0000

    Add the correct version of the previosu patches.

--- databases/bdb-xml/patches/patch-dbxml_src_dbxml_dataItem_DbXmlDocumentCacheImpl.cpp
+++ databases/bdb-xml/patches/patch-dbxml_src_dbxml_dataItem_DbXmlDocumentCacheImpl.cpp
@@ -1,9 +1,9 @@
-$NetBSD: patch-dbxml_src_dbxml_dataItem_DbXmlDocumentCacheImpl.cpp,v 1.1 2012/03/10 20:18:03 markd Exp $
+$NetBSD: patch-dbxml_src_dbxml_dataItem_DbXmlDocumentCacheImpl.cpp,v 1.2 2012/03/11 20:26:57 markd Exp $
 
 Fix build with gcc 4.6
 
---- dbxml/src/dbxml/dataItem/DbXmlDocumentCacheImpl.cpp.orig	2009-12-22 13:17:00.000000000 +0000
-+++ dbxml/src/dbxml/dataItem/DbXmlDocumentCacheImpl.cpp
+--- src/dbxml/dataItem/DbXmlDocumentCacheImpl.cpp.orig	2009-12-22 13:17:00.000000000 +0000
++++ src/dbxml/dataItem/DbXmlDocumentCacheImpl.cpp
 @@ -5,6 +5,7 @@
  //
  //
