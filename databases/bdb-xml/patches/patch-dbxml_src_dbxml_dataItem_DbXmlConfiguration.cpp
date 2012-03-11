aa40ef1f1f69562cd6cc6964b00512ccd3e922b8
markd <markd@pkgsrc.org>
2012-03-11 20:26:57 +0000

    Add the correct version of the previosu patches.

--- databases/bdb-xml/patches/patch-dbxml_src_dbxml_dataItem_DbXmlConfiguration.cpp
+++ databases/bdb-xml/patches/patch-dbxml_src_dbxml_dataItem_DbXmlConfiguration.cpp
@@ -1,9 +1,9 @@
-$NetBSD: patch-dbxml_src_dbxml_dataItem_DbXmlConfiguration.cpp,v 1.1 2012/03/10 20:18:03 markd Exp $
+$NetBSD: patch-dbxml_src_dbxml_dataItem_DbXmlConfiguration.cpp,v 1.2 2012/03/11 20:26:57 markd Exp $
 
 Fix build with gcc 4.6
 
---- dbxml/src/dbxml/dataItem/DbXmlConfiguration.cpp.orig	2009-12-22 13:17:00.000000000 +0000
-+++ dbxml/src/dbxml/dataItem/DbXmlConfiguration.cpp
+--- src/dbxml/dataItem/DbXmlConfiguration.cpp.orig	2009-12-22 13:17:00.000000000 +0000
++++ src/dbxml/dataItem/DbXmlConfiguration.cpp
 @@ -5,6 +5,7 @@
  //
  //
--- databases/bdb-xml/patches/patch-dbxml_src_dbxml_dataItem_Join.cpp
+++ databases/bdb-xml/patches/patch-dbxml_src_dbxml_dataItem_Join.cpp
@@ -1,9 +1,9 @@
-$NetBSD: patch-dbxml_src_dbxml_dataItem_Join.cpp,v 1.1 2012/03/10 20:18:03 markd Exp $
+$NetBSD: patch-dbxml_src_dbxml_dataItem_Join.cpp,v 1.2 2012/03/11 20:26:57 markd Exp $
 
 Fix build with gcc 4.6
 
---- dbxml/src/dbxml/dataItem/Join.cpp.orig	2009-12-22 13:17:00.000000000 +0000
-+++ dbxml-2.5.16/dbxml/src/dbxml/dataItem/Join.cpp
+--- src/dbxml/dataItem/Join.cpp.orig	2009-12-22 13:17:00.000000000 +0000
++++ src/dbxml/dataItem/Join.cpp
 @@ -5,6 +5,7 @@
  //
  //
