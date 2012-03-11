d4a78eddfe196dc23f4ac7d34e0352760b2a4d95
markd <markd>
2012-03-11 20:26:57 +0000

    Add the correct version of the previosu patches.

--- databases/bdb-xml/patches/patch-dbxml_src_dbxml_dataItem_DbXmlPrintAST.cpp
+++ databases/bdb-xml/patches/patch-dbxml_src_dbxml_dataItem_DbXmlPrintAST.cpp
@@ -1,9 +1,9 @@
-$NetBSD: patch-dbxml_src_dbxml_dataItem_DbXmlPrintAST.cpp,v 1.1 2012/03/10 20:18:03 markd Exp $
+$NetBSD: patch-dbxml_src_dbxml_dataItem_DbXmlPrintAST.cpp,v 1.2 2012/03/11 20:26:57 markd Exp $
 
 Fix build with gcc 4.6
 
---- dbxml/src/dbxml/dataItem/DbXmlPrintAST.cpp.orig	2009-12-22 13:17:00.000000000 +0000
-+++ dbxml/src/dbxml/dataItem/DbXmlPrintAST.cpp
+--- src/dbxml/dataItem/DbXmlPrintAST.cpp.orig	2009-12-22 13:17:00.000000000 +0000
++++ src/dbxml/dataItem/DbXmlPrintAST.cpp
 @@ -7,6 +7,7 @@
  
  #include <iostream>
