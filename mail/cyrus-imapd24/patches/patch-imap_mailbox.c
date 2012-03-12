c8c35d0af8fd7bbc788d50d6216bcf0acc712293
obache <obache@pkgsrc.org>
2012-03-12 13:40:12 +0000

    Update cyrus-imapd24 to 2.4.14
    Bugs resolved in 2.4.14
    * Bug #2685 folder delete and annotations
    * Bug #3381 tools/rehash script is not 64bit safe
    * Bug #3405 tls support for ctl_mboxlist
    * Bug #3452 sieve uses mailbox name in internal format in autoreply
    * Bug #3485 cyr_expire tries to delete parent directories that are not empty
    * Bug #3582 Extra LF+SP characters been inserted in the middle of the XML
      attachments
    * Bug #3598 lmtp auditlog for murder proxy mode
    * Bug #3623 OpenBSD (and other platforms without working mmap) multiple bugs
    * Bug #3636 Renaming a mailbox doesn't remove old folder
    * Bug #3628 folders with spaces and subfolders with same prefix as parent folder
      don't show up
    * Bug #3634 RENAME INBOX no longer works
    * Bug #3635 cmd_rename() fails to check error codes
    * Bug #3636 unexpunge locks mailbox until all output is emitted
    * Bug #3637 imapd and ipop3d in 2.4 no longer log expunge events to syslog
    * Bug #3640 LMTP accepts NULL bytes in headers, resulting in incorrect header/
      mail size in meta-data
    * Bug #3642 iPhone 5 expects SPECIAL-USE in LIST output
    * Bug #3645 Ability to delete folder "shared"
    * Bug #3649 unable to remove mailbox
    * Bug #3650 ENVELOPE contains unexpected CRLF

--- mail/cyrus-imapd24/patches/patch-imap_mailbox.c
+++ mail/cyrus-imapd24/patches/patch-imap_mailbox.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-imap_mailbox.c,v 1.1 2011/07/06 09:45:45 obache Exp $
+$NetBSD: patch-imap_mailbox.c,v 1.2 2012/03/12 13:40:12 obache Exp $
 
 * cast to unsigned long for platform sizeof(time_t) > sizeof(unsigned long).
   https://bugzilla.cyrusimap.org/show_bug.cgi?id=3376
 
---- imap/mailbox.c.orig	2011-07-04 15:05:01.000000000 +0000
+--- imap/mailbox.c.orig	2012-03-12 11:47:51.000000000 +0000
 +++ imap/mailbox.c
-@@ -1918,9 +1918,9 @@ bit32 make_sync_crc(struct mailbox *mail
+@@ -1954,9 +1954,9 @@ bit32 make_sync_crc(struct mailbox *mail
      }
  
      snprintf(buf, 4096, "%u " MODSEQ_FMT " %lu (%u) %lu %s",
