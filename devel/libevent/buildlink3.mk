1ad55d876cb6d19af560777826589009dcdbdc94
sbd <sbd>
2012-03-12 07:07:03 +0000

    Add security/openssl buildlink to the buildlink3.mk file.

--- devel/libevent/buildlink3.mk
+++ devel/libevent/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2011/02/11 21:13:47 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2012/03/12 07:08:40 sbd Exp $
 
 BUILDLINK_TREE+=	libevent
 
@@ -8,6 +8,8 @@ LIBEVENT_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libevent+=libevent>=0.6
 BUILDLINK_ABI_DEPENDS.libevent+=libevent>=2.0.10
 BUILDLINK_PKGSRCDIR.libevent?=	../../devel/libevent
+
+.include "../../security/openssl/buildlink3.mk"
 .endif # LIBEVENT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libevent
