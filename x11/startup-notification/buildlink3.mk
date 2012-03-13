58f4fea20f92b8b0fc74882222ad89627c57cd31
spz <spz>
2012-03-13 01:35:03 +0000

    Pullup ticket #3700 - requested by wiz x11/startup-notification: dependency pattern and build fixes
    Revisions pulled up:
    - x11/startup-notification/buildlink3.mk                        1.13-1.14
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	wiz
       Date:		Sat Mar 10 13:31:55 UTC 2012
    
       Modified Files:
       	pkgsrc/x11/startup-notification: buildlink3.mk
    
       Log Message:
       Fix patterns so that pbulk does not try to build startup-notification
       on systems where startup-notification010 is needed instead.
       Tested by bouyer.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.12 -r1.13 pkgsrc/x11/startup-notification/buildlink3.mk
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	wiz
       Date:		Sun Mar 11 10:37:51 UTC 2012
    
       Modified Files:
       	pkgsrc/x11/startup-notification: buildlink3.mk
    
       Log Message:
       Make ugly hack even uglier to fix build with modular.
       Fixes PR 46171 by David Shao.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.13 -r1.14 pkgsrc/x11/startup-notification/buildlink3.mk

--- x11/startup-notification/buildlink3.mk
+++ x11/startup-notification/buildlink3.mk
@@ -1,22 +1,25 @@
-# $NetBSD: buildlink3.mk,v 1.11.18.1 2012/02/08 20:11:25 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.11.18.2 2012/03/13 01:35:03 spz Exp $
 
 BUILDLINK_TREE+=	startup-notification
 
 .if !defined(STARTUP_NOTIFICATION_BUILDLINK3_MK)
 STARTUP_NOTIFICATION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5
-BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.8nb1
-
 .include "../../mk/bsd.fast.prefs.mk"
 .if ${X11_TYPE} == "modular"
 BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification
+BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5
+BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.8nb1
 .else
 .if ${OPSYS} == "NetBSD"
 .if !empty(OS_VERSION:M5.99.*) || !empty(OS_VERSION:M[6789].*)
 BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification
+BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5
+BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.8nb1
 .else
 BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification010
+BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5<0.12
+BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.8nb1<0.12
 .endif
 .else #!NetBSD
 BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification
