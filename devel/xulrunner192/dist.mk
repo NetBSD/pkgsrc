5b0eefe4415777e0ce5e98510f3cf30e6c9caf95
martin <martin@pkgsrc.org>
2012-03-13 14:18:32 +0000

    Update to Firefox 3.6.28

--- devel/xulrunner192/dist.mk
+++ devel/xulrunner192/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.6 2012/02/22 11:00:07 obache Exp $
+# $NetBSD: dist.mk,v 1.7 2012/03/13 14:18:32 martin Exp $
 #
 # used by devel/xulrunner192/Makefile
 # used by www/firefox36/Makefile
@@ -6,7 +6,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	3.6${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	1.9.2
-MOZ_BRANCH_MINOR=	.27
+MOZ_BRANCH_MINOR=	.28
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
