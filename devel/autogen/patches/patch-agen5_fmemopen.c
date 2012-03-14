87ae46d5429e94f1705206bde41e07bc292d2ca4
wiz <wiz>
2012-03-14 22:51:14 +0000

    Fix build on NetBSD-current, per patch from Kai-Uwe Eckhardt in PR 46045. Set LICENSE while here.

---
+++ devel/autogen/patches/patch-agen5_fmemopen.c
@@ -0,0 +1,21 @@
+$NetBSD: patch-agen5_fmemopen.c,v 1.1 2012/03/14 22:51:14 wiz Exp $
+
+--- agen5/fmemopen.c.orig	2011-06-26 14:39:38.000000000 +0000
++++ agen5/fmemopen.c
+@@ -61,13 +61,13 @@ typedef struct {
+    typedef int       seek_ret_t;
+ 
+ #elif defined(HAVE_FUNOPEN)
+-   typedef fpos_t  seek_off_t;
+-   typedef fpos_t  seek_ret_t;
++   typedef off_t  seek_off_t;
++   typedef off_t  seek_ret_t;
+ 
+ #  ifdef NEED_COOKIE_FUNCTION_TYPEDEFS
+      typedef int     (cookie_read_function_t )(void *, char *, int);
+      typedef int     (cookie_write_function_t)(void *, char const *, int);
+-     typedef fpos_t  (cookie_seek_function_t )(void *, fpos_t, int);
++     typedef off_t  (cookie_seek_function_t )(void *, off_t, int);
+      typedef int     (cookie_close_function_t)(void *);
+ #  endif /* NEED_COOKIE_FUNCTION_TYPEDEFS */
+ #endif
