5611b9d8b0a75a8084a0790725456809a803984d
abs <abs@pkgsrc.org>
2012-03-15 12:54:09 +0000

    Set --includedir to avoid conflict with unixodbc, adjust BUILDLINK_INCDIRS

--- databases/iodbc/buildlink3.mk
+++ databases/iodbc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:24:05 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2012/03/15 12:54:09 abs Exp $
 
 BUILDLINK_TREE+=	iodbc
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	iodbc
 IODBC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.iodbc+=	iodbc>=3.52.3
+BUILDLINK_INCDIRS.iodbc?=	${PG_SUBPREFIX}/include
 BUILDLINK_PKGSRCDIR.iodbc?=	../../databases/iodbc
 
 # ODBC drivers don't need these bits, but ODBC clients do.
