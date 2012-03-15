2c22fc5e023f4eca2e0e49b5c033bd1cb39e2cc5
abs <abs@pkgsrc.org>
2012-03-15 15:27:58 +0000

    fix BUILDLINK_INCDIRS & BUILDLINK_LIBDIRS - thanks wiz

--- databases/iodbc/buildlink3.mk
+++ databases/iodbc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2012/03/15 14:50:56 abs Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2012/03/15 15:27:58 abs Exp $
 
 BUILDLINK_TREE+=	iodbc
 
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	iodbc
 IODBC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.iodbc+=	iodbc>=3.52.3
-BUILDLINK_INCDIRS.iodbc?=	${PREFIX}/include
-BUILDLINK_LIBDIRS.iodbc?=	${PREFIX}/lib
+BUILDLINK_INCDIRS.iodbc?=	${PREFIX}/include/iodbc
+BUILDLINK_LIBDIRS.iodbc?=	${PREFIX}/lib/iodbc
 BUILDLINK_PKGSRCDIR.iodbc?=	../../databases/iodbc
 
 # ODBC drivers don't need these bits, but ODBC clients do.
