d20b7c296b4c8ad32e3225cb77b1dcb99f753f92
abs <abs@pkgsrc.org>
2012-03-15 14:50:56 +0000

    Also put libs in iodbc subdir, patch to ensure pkgconfigdir is not affected bump PKGREVISION. Test built soprano

--- databases/iodbc/buildlink3.mk
+++ databases/iodbc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2012/03/15 12:54:09 abs Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2012/03/15 14:50:56 abs Exp $
 
 BUILDLINK_TREE+=	iodbc
 
@@ -6,7 +6,8 @@ BUILDLINK_TREE+=	iodbc
 IODBC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.iodbc+=	iodbc>=3.52.3
-BUILDLINK_INCDIRS.iodbc?=	${PG_SUBPREFIX}/include
+BUILDLINK_INCDIRS.iodbc?=	${PREFIX}/include
+BUILDLINK_LIBDIRS.iodbc?=	${PREFIX}/lib
 BUILDLINK_PKGSRCDIR.iodbc?=	../../databases/iodbc
 
 # ODBC drivers don't need these bits, but ODBC clients do.
