bd0db25cd96dc9ab87127e0c2299d514763dcbbf
reinoud <reinoud>
2012-03-18 11:33:46 +0000

    pkgsrc/meta/boost/ is at version 1.58 but devel/boost-jam's buidlink3.mk file was referring to 1.59. References to the boost-jam's package were thus getting confused since they expect 1.59 to be there when 1.58 was installed.
    XXX shouldn't this be a version var in meta/boost/Makefile.common ?

--- devel/boost-jam/buildlink3.mk
+++ devel/boost-jam/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.15 2012/02/29 14:22:23 hans Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2012/03/18 11:33:46 reinoud Exp $
 
 BUILDLINK_TREE+=	boost-jam
 
 .if !defined(BOOST_JAM_BUILDLINK3_MK)
 BOOST_JAM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.49.*
+BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.48.*
 BUILDLINK_DEPMETHOD.boost-jam?=		build
 BUILDLINK_PKGSRCDIR.boost-jam?=		../../devel/boost-jam
 .endif # BOOST_JAM_BUILDLINK3_MK
