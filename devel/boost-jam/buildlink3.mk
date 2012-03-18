8b3bae6301f1e4da5bbaa5cfda993d2763a05be3
wiz <wiz>
2012-03-18 12:02:41 +0000

    Revert previous, boost packages are at 1.49.

--- devel/boost-jam/buildlink3.mk
+++ devel/boost-jam/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.16 2012/03/18 11:33:46 reinoud Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2012/03/18 12:02:41 wiz Exp $
 
 BUILDLINK_TREE+=	boost-jam
 
 .if !defined(BOOST_JAM_BUILDLINK3_MK)
 BOOST_JAM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.48.*
+BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.49.*
 BUILDLINK_DEPMETHOD.boost-jam?=		build
 BUILDLINK_PKGSRCDIR.boost-jam?=		../../devel/boost-jam
 .endif # BOOST_JAM_BUILDLINK3_MK
