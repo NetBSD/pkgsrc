a965d43f765d85c8a00a2a7d18c98bdcf1d9f11e
markd <markd@pkgsrc.org>
2012-03-19 23:12:19 +0000

    Update to KDE SC 4.8.0

--- graphics/libkexiv2/buildlink3.mk
+++ graphics/libkexiv2/buildlink3.mk
@@ -1,18 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.18 2012/03/03 00:12:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2012/03/19 23:12:19 markd Exp $
 
 BUILDLINK_TREE+=	libkexiv2
 
 .if !defined(LIBKEXIV2_BUILDLINK3_MK)
 LIBKEXIV2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkexiv2+=	libkexiv2>=0.1.1
-BUILDLINK_ABI_DEPENDS.libkexiv2+=	libkexiv2>=0.1.9nb12
+BUILDLINK_API_DEPENDS.libkexiv2+=	libkexiv2>=4.8.0
 BUILDLINK_PKGSRCDIR.libkexiv2?=	../../graphics/libkexiv2
 
-.include "../../graphics/exiv2/buildlink3.mk"
-.include "../../graphics/png/buildlink3.mk"
-.include "../../devel/zlib/buildlink3.mk"
-.include "../../x11/kdelibs3/buildlink3.mk"
-.endif # LIBKEXIV2_BUILDLINK3_MK
+.endif	# LIBKEXIV2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkexiv2
