ecd463f78f1b671164001420eca20c80c2a01357
markd <markd@pkgsrc.org>
2012-03-19 22:57:09 +0000

    Update to KDE SC 4.8.0

--- graphics/libkipi/buildlink3.mk
+++ graphics/libkipi/buildlink3.mk
@@ -1,17 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.26 2012/03/03 00:12:35 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2012/03/19 22:57:09 markd Exp $
 
 BUILDLINK_TREE+=	libkipi
 
 .if !defined(LIBKIPI_BUILDLINK3_MK)
 LIBKIPI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkipi+=	libkipi>=0.1.5
-BUILDLINK_ABI_DEPENDS.libkipi?=	libkipi>=0.1.6nb12
+BUILDLINK_API_DEPENDS.libkipi+=	libkipi>=4.8.0
 BUILDLINK_PKGSRCDIR.libkipi?=	../../graphics/libkipi
 
-.include "../../x11/kdelibs3/buildlink3.mk"
-.include "../../x11/libSM/buildlink3.mk"
-.include "../../x11/libXext/buildlink3.mk"
-.endif # LIBKIPI_BUILDLINK3_MK
+.include "../../x11/kdelibs4/buildlink3.mk"
+.endif	# LIBKIPI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkipi
