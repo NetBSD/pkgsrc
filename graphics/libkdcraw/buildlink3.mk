bb617ff79a805663c6323cb0a83659acfeadfcb3
markd <markd@pkgsrc.org>
2012-03-19 22:43:15 +0000

    Update to KDE SC 4.8.0

--- graphics/libkdcraw/buildlink3.mk
+++ graphics/libkdcraw/buildlink3.mk
@@ -1,16 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.16 2012/03/03 00:12:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2012/03/19 22:43:15 markd Exp $
 
 BUILDLINK_TREE+=	libkdcraw
 
 .if !defined(LIBKDCRAW_BUILDLINK3_MK)
 LIBKDCRAW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkdcraw+=	libkdcraw>=0.1.1
-BUILDLINK_ABI_DEPENDS.libkdcraw?=	libkdcraw>=0.1.9nb11
+BUILDLINK_API_DEPENDS.libkdcraw+=	libkdcraw>=4.8.0
 BUILDLINK_PKGSRCDIR.libkdcraw?=	../../graphics/libkdcraw
 
-.include "../../x11/qt3-libs/buildlink3.mk"
-.include "../../x11/kdelibs3/buildlink3.mk"
-.endif # LIBKDCRAW_BUILDLINK3_MK
+.include "../../x11/kdelibs4/buildlink3.mk"
+.endif	# LIBKDCRAW_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkdcraw
