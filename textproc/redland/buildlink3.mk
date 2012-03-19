4e007e0bc4cb25e62603be52feb88dde16075351
markd <markd@pkgsrc.org>
2012-03-19 09:40:08 +0000

    Update to 1.0.15
    Bug fixes, new features. Requires Raptor V2.

--- textproc/redland/buildlink3.mk
+++ textproc/redland/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2012/03/03 00:12:35 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2012/03/19 09:40:08 markd Exp $
 
 BUILDLINK_TREE+=	redland
 
@@ -10,7 +10,7 @@ BUILDLINK_ABI_DEPENDS.redland?=	redland>=1.0.12nb3
 BUILDLINK_PKGSRCDIR.redland?=	../../textproc/redland
 
 .include "../../security/openssl/buildlink3.mk"
-.include "../../textproc/raptor/buildlink3.mk"
+.include "../../textproc/raptor2/buildlink3.mk"
 .include "../../textproc/rasqal/buildlink3.mk"
 .include "../../mk/bdb.buildlink3.mk"
 .endif # REDLAND_BUILDLINK3_MK
