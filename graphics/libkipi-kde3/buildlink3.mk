a432be369801e8f0eeccfde196fa40d8000ee3e3
markd <markd@pkgsrc.org>
2012-03-19 22:48:15 +0000

    Reimport libkipi 0.1.6nb12 as libkipi-kde3

---
+++ graphics/libkipi-kde3/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/19 22:48:15 markd Exp $
+
+BUILDLINK_TREE+=	libkipi
+
+.if !defined(LIBKIPI_BUILDLINK3_MK)
+LIBKIPI_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libkipi+=	libkipi>=0.1.5
+BUILDLINK_ABI_DEPENDS.libkipi?=	libkipi>=0.1.6nb12
+BUILDLINK_PKGSRCDIR.libkipi?=	../../graphics/libkipi-kde3
+
+.include "../../x11/kdelibs3/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.endif # LIBKIPI_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libkipi
