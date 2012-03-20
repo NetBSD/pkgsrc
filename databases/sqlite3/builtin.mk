22aed8058e2b82193562af08b8fdf4a07f046135
adam <adam@pkgsrc.org>
2012-03-20 21:00:47 +0000

    Changes 3.7.11: * Enhance the INSERT syntax to allow multiple rows to be inserted via   the VALUES clause. * Enhance the CREATE VIRTUAL TABLE command to support the IF NOT EXISTS clause. * Added the sqlite3_stricmp() interface as a counterpart to sqlite3_strnicmp(). * Added the sqlite3_db_readonly() interface. * Added the SQLITE_FCNTL_PRAGMA file control, giving VFS implementations   the ability to add new PRAGMA statements or to override built-in PRAGMAs. * Queries of the form: "SELECT max(x), y FROM table" returns the value of y on   the same row that contains the maximum x value. * Added support for the FTS4 languageid option. * Documented support for the FTS4 content option. This feature has actually   been in the code since version 3.7.9 but is only now considered to be   officially supported. * Pending statements no longer block ROLLBACK. Instead, the pending statement   will return SQLITE_ABORT upon next access after the ROLLBACK. * Improvements to the handling of CSV inputs in the command-line shell * Fix a bug introduced in version 3.7.10 that might cause a LEFT JOIN to be   incorrectly converted into an INNER JOIN if the WHERE clause indexable terms   connected by OR.

--- databases/sqlite3/builtin.mk
+++ databases/sqlite3/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.2 2011/11/10 17:18:10 wiz Exp $
+# $NetBSD: builtin.mk,v 1.3 2012/03/20 21:00:47 adam Exp $
 
 BUILTIN_PKG:=	sqlite3
 
@@ -12,12 +12,12 @@ PKGCONFIG_BASE.sqlite3=	/usr
 ### solely to determine whether a built-in implementation exists.
 ###
 
-CHECK_BUILTIN.sqlite3?=      no
+CHECK_BUILTIN.sqlite3?=		no
 .if !empty(CHECK_BUILTIN.sqlite3:M[nN][oO])
 
 .  if !empty(USE_BUILTIN.sqlite3:M[yY][eE][sS])
-BUILDLINK_PREFIX.sqlite3=    /usr
-BUILDLINK_FILES.sqlite3+=    lib/pkgconfig/sqlite3.pc
+BUILDLINK_PREFIX.sqlite3=	/usr
+BUILDLINK_FILES.sqlite3+=	lib/pkgconfig/sqlite3.pc
 .  endif
 
 .endif  # CHECK_BUILTIN.sqlite3
