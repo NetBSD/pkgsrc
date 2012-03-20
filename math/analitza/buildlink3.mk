983fcf3b1a85f3ea475d43e142e30e80de9db905
markd <markd@pkgsrc.org>
2012-03-20 07:30:13 +0000

    add analitza, cantor, kalgebra.  Part of KDE SC 4.8.0

---
+++ math/analitza/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/03/20 07:30:13 markd Exp $
+
+BUILDLINK_TREE+=	analitza
+
+.if !defined(ANALITZA_BUILDLINK3_MK)
+ANALITZA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.analitza+=	analitza>=4.8.0
+BUILDLINK_PKGSRCDIR.analitza?=	../../math/analitza
+
+.include "../../x11/kdelibs4/buildlink3.mk"
+.endif	# ANALITZA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-analitza
