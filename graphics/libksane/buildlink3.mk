4c3bc40299104001c55ec48dc62c4b75718d2120
markd <markd@pkgsrc.org>
2012-03-20 00:14:34 +0000

    Update to KDE SC 4.8.0
    remove kdegraphics4
    add libksane, kamera, ksaneplugin, ksnapshot, kcolorchooser, kgamma,
        kruler, kolourpaint, kdegraphics-mobipocket, kdegraphics-thumbnailers,
        kdegraphics-strigi-analyzer, svgpart

---
+++ graphics/libksane/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/03/20 00:14:34 markd Exp $
+
+BUILDLINK_TREE+=	libksane
+
+.if !defined(LIBKSANE_BUILDLINK3_MK)
+LIBKSANE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libksane+=	libksane>=4.8.0
+BUILDLINK_PKGSRCDIR.libksane?=	../../graphics/libksane
+
+.include "../../x11/kdelibs4/buildlink3.mk"
+.endif	# LIBKSANE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libksane
