f0fd559e42185bebbf78c7bd69c260e3ff1e00cf
markd <markd@pkgsrc.org>
2012-03-20 08:53:09 +0000

    Add marble.  Part of KDE SC 4.8.0
    KDE desktop globe

---
+++ misc/marble/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/03/20 08:53:09 markd Exp $
+
+BUILDLINK_TREE+=	marble
+
+.if !defined(MARBLE_BUILDLINK3_MK)
+MARBLE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.marble+=	marble>=4.8.0
+BUILDLINK_PKGSRCDIR.marble?=	../../misc/marble
+
+.include "../../x11/kdelibs4/buildlink3.mk"
+.endif	# MARBLE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-marble
