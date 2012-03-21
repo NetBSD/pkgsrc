11f9cd103c91662952be02cca508d8970044d39b
markd <markd@pkgsrc.org>
2012-03-21 20:48:01 +0000

    Import libkvkontakte 2.5.0
    KDE library for asynchronous interaction with vkontakte.rusocial network

---
+++ misc/libkvkontakte/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/21 20:48:01 markd Exp $
+
+BUILDLINK_TREE+=	libkvkontakte
+
+.if !defined(LIBKVKONTAKTE_BUILDLINK3_MK)
+LIBKVKONTAKTE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libkvkontakte+=	libkvkontakte>=2.5.0
+BUILDLINK_PKGSRCDIR.libkvkontakte?=	../../misc/libkvkontakte
+
+.include "../../devel/qjson/buildlink3.mk"
+.include "../../x11/kdelibs4/buildlink3.mk"
+.endif	# LIBKVKONTAKTE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libkvkontakte
