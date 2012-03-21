fe539b79e4f8052e76e50843ee1e3c3c4b0fcdd4
drochner <drochner@pkgsrc.org>
2012-03-21 18:28:40 +0000

    update to 1.0.1 changes: -UI improvements, UTF-8/wcurses support -Performance improvements -XDG compliance -bugfixes

--- sysutils/htop/patches/patch-CRT.c
+++ sysutils/htop/patches/patch-CRT.c
@@ -1,18 +1,8 @@
-$NetBSD: patch-CRT.c,v 1.1 2011/08/06 16:36:41 cheusov Exp $
+$NetBSD: patch-CRT.c,v 1.2 2012/03/21 18:28:40 drochner Exp $
 
---- CRT.c.orig	2010-11-23 15:56:32.000000000 +0000
+--- CRT.c.orig	2011-12-26 21:52:55.000000000 +0000
 +++ CRT.c
-@@ -11,7 +11,9 @@ in the source distribution for its full 
- #include <signal.h>
- #include <stdlib.h>
- #include <stdbool.h>
-+#ifdef HAVE_EXECINFO_H
- #include <execinfo.h>
-+#endif
- 
- #include "String.h"
- 
-@@ -118,7 +120,9 @@ int CRT_colors[LAST_COLORELEMENT] = { 0 
+@@ -120,7 +120,9 @@ int CRT_cursorX = 0;
  
  char* CRT_termType;
  
@@ -22,15 +12,3 @@ $NetBSD: patch-CRT.c,v 1.1 2011/08/06 16:36:41 cheusov Exp $
  
  static void CRT_handleSIGSEGV(int sgn) {
     (void) sgn;
-@@ -128,9 +132,11 @@ static void CRT_handleSIGSEGV(int sgn) {
-    #else
-    fprintf(stderr, "\n\nhtop " VERSION " aborting. Unsupported platform.\n");
-    #endif
-+   #ifdef HAVE_EXECINFO_H
-    size_t size = backtrace(backtraceArray, sizeof(backtraceArray));
-    fprintf(stderr, "Backtrace: \n");
-    backtrace_symbols_fd(backtraceArray, size, 2);
-+   #endif
-    abort();
- }
- 
