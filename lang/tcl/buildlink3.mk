3b1d7c17802c3ce2e8411905e115633fbfa8a4f3
obache <obache@pkgsrc.org>
2012-03-21 15:33:29 +0000

    Set TCLSH here, same as PERL5 in lang/perl5/buildlink3.mk.

--- lang/tcl/buildlink3.mk
+++ lang/tcl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2012/03/05 13:35:20 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2012/03/21 15:33:29 obache Exp $
 
 BUILDLINK_TREE+=	tcl
 
@@ -20,6 +20,10 @@ BUILDLINK_TRANSFORM+=	l:tcl8.4:tcl84
 TCLCONFIG_SH?=		${BUILDLINK_PREFIX.tcl}/lib/tclConfig.sh
 
 _TOOLS_USE_PKGSRC.tclsh=	yes
+
+FIND_PREFIX:=		TOOLS_PREFIX.tclsh=tcl
+.include "../../mk/find-prefix.mk"
+TCLSH=			${TOOLS_PREFIX.tclsh}/bin/tclsh
 
 .include "../../mk/dlopen.buildlink3.mk"
 .endif # TCL_BUILDLINK3_MK
