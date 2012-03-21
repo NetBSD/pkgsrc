7deca86ab6bc77bce9f5a2d9d3e52b983f6fed61
markd <markd>
2012-03-21 20:57:24 +0000

    Import libmediawiki 2.5.0
    libmediawiki is a KDE C++ interface for MediaWiki based web service as
    wikipedia.org
    
    This library is used by kipi-plugins and digiKam.

---
+++ www/libmediawiki/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/21 20:57:24 markd Exp $
+
+BUILDLINK_TREE+=	libmediawiki
+
+.if !defined(LIBMEDIAWIKI_BUILDLINK3_MK)
+LIBMEDIAWIKI_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libmediawiki+=	libmediawiki>=2.5.0
+BUILDLINK_PKGSRCDIR.libmediawiki?=	../../www/libmediawiki
+
+.include "../../x11/kdelibs4/buildlink3.mk"
+.endif	# LIBMEDIAWIKI_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libmediawiki
