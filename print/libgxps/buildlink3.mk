273bd89da164c18bf65299a3b9a00d3c18f2f4b8
drochner <drochner@pkgsrc.org>
2012-03-21 17:38:39 +0000

    add libgxps-0.2.2, a library to read "xps" documents

---
+++ print/libgxps/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/21 17:38:39 drochner Exp $
+
+BUILDLINK_TREE+=	libgxps
+
+.if !defined(LIBGXPS_BUILDLINK3_MK)
+LIBGXPS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libgxps+=	libgxps>=0.2.1
+BUILDLINK_PKGSRCDIR.libgxps?=	../../print/libgxps
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
+.include "../../archivers/libarchive/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+.include "../../graphics/lcms2/buildlink3.mk"
+.endif	# LIBGXPS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libgxps
