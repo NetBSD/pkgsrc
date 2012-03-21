9add99d98a19c17aa92065cbe3d38dc47a19f041
markd <markd@pkgsrc.org>
2012-03-21 20:43:12 +0000

    Import libkgeomap 2.5.0
    Libkgeomap is a wrapper around world map components as Marble, OpenstreetMap
    and GoogleMap, for browsing and arranging photos on a map
    
    This library is used by kipi-plugins, digiKam and other kipi host programs.

---
+++ misc/libkgeomap/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/21 20:43:12 markd Exp $
+
+BUILDLINK_TREE+=	libkgeomap
+
+.if !defined(LIBKGEOMAP_BUILDLINK3_MK)
+LIBKGEOMAP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libkgeomap+=	libkgeomap>=2.5.0
+BUILDLINK_PKGSRCDIR.libkgeomap?=	../../misc/libkgeomap
+
+.include "../../misc/marble/buildlink3.mk"
+.include "../../x11/kdelibs4/buildlink3.mk"
+.endif	# LIBKGEOMAP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libkgeomap
