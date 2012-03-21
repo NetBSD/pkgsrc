24b59df041f22b3230e54139ba83ac2a9e651869
drochner <drochner@pkgsrc.org>
2012-03-21 16:37:15 +0000

    update to 1.2.1 changes: -Rewrite of VDPAU OSD handling -sndio plugin is now buildable -Various bug fixes

--- multimedia/xine-lib/buildlink3.mk
+++ multimedia/xine-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2009/03/20 19:25:06 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2012/03/21 16:37:15 drochner Exp $
 
 BUILDLINK_TREE+=	xine-lib
 
@@ -9,6 +9,8 @@ BUILDLINK_API_DEPENDS.xine-lib+=	xine-lib>=1rc3c
 BUILDLINK_ABI_DEPENDS.xine-lib+=xine-lib>=1.0.3a
 BUILDLINK_ABI_DEPENDS.xine-lib?=	xine-lib>=1.1.3nb1
 BUILDLINK_PKGSRCDIR.xine-lib?=	../../multimedia/xine-lib
+
+.include "../../multimedia/ffmpeg/buildlink3.mk"
 .endif # XINE_LIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xine-lib
