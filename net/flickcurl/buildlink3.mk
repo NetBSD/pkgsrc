cedecf510b4a0411664db33c553b7eb2b934da6c
wiz <wiz>
2012-03-21 08:13:27 +0000

    Make flickcurl use raptor2 instead of raptor. Bump PKGREVISION of the only dependency.

--- net/flickcurl/buildlink3.mk
+++ net/flickcurl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/08/25 13:30:23 jakllsch Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/21 08:13:27 wiz Exp $
 
 BUILDLINK_TREE+=	flickcurl
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	flickcurl
 FLICKCURL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.flickcurl+=	flickcurl>=1.21
+BUILDLINK_ABI_DEPENDS.flickcurl+=	flickcurl>=1.22nb1
 BUILDLINK_PKGSRCDIR.flickcurl?=	../../net/flickcurl
 
 .endif	# FLICKCURL_BUILDLINK3_MK
