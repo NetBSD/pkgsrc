6fc1de5866efa305a6cee5c12e99224d4cdd3ae1
obache <obache@pkgsrc.org>
2012-03-24 12:48:58 +0000

    fix pkgsrc-wip reference in buildlink3.mk PR 46252.

--- textproc/mythes/buildlink3.mk
+++ textproc/mythes/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/17 10:57:37 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/24 12:48:58 obache Exp $
 
 BUILDLINK_TREE+=	mythes
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	mythes
 MYTHES_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mythes+=	mythes>=1.2.2
-BUILDLINK_PKGSRCDIR.mythes?=	../../wip/libmythes
+BUILDLINK_PKGSRCDIR.mythes?=	../../textproc/mythes
 .endif	# MYTHES_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mythes
