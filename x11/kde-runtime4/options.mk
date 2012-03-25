2462a652811ca092d96eac03fd973289ff779245
markd <markd@pkgsrc.org>
2012-03-25 20:11:28 +0000

    s/PKG_DEFAULT_OPTIONS/PKG_SUGGESTED_OPTIONS/  noted by John Marino. Bump PKGREVISION

--- x11/kde-runtime4/options.mk
+++ x11/kde-runtime4/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2012/03/19 19:44:03 markd Exp $
+# $NetBSD: options.mk,v 1.2 2012/03/25 20:11:28 markd Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kde-runtime4
 PKG_SUPPORTED_OPTIONS=	samba alsa
-PKG_DEFAULT_OPTIONS=	samba alsa
+PKG_SUGGESTED_OPTIONS=	samba alsa
 
 .include "../../mk/bsd.options.mk"
 
