9773c5472459af285fea1fb25798a11f2c1c574f
joerg <joerg>
2012-03-26 13:07:34 +0000

    Don't pick up x11/fixesproto by accident.

--- x11/fixesproto4/buildlink3.mk
+++ x11/fixesproto4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/12/08 08:10:47 abs Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/03/26 13:07:34 joerg Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -9,7 +9,7 @@ BUILDLINK_TREE+=	fixesproto
 .if !defined(FIXESPROTO_BUILDLINK3_MK)
 FIXESPROTO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fixesproto+=	fixesproto>=3.0.0
+BUILDLINK_API_DEPENDS.fixesproto+=	fixesproto>=3.0.0<5
 BUILDLINK_PKGSRCDIR.fixesproto?=	../../x11/fixesproto4
 
 .include "../../x11/xextproto/buildlink3.mk"
