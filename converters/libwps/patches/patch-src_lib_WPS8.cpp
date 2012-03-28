6c1e00901c619487cf278b152fa0ca17d96ea9aa
wiz <wiz@pkgsrc.org>
2012-03-28 15:16:34 +0000

    Fix build with clang.

--- converters/libwps/patches/patch-src_lib_WPS8.cpp
+++ converters/libwps/patches/patch-src_lib_WPS8.cpp
@@ -1,6 +1,8 @@
-$NetBSD: patch-src_lib_WPS8.cpp,v 1.2 2012/02/16 18:04:08 hans Exp $
+$NetBSD: patch-src_lib_WPS8.cpp,v 1.3 2012/03/28 15:17:19 wiz Exp $
 
---- src/lib/WPS8.cpp.orig	2011-02-04 20:04:48.000000000 +0000
+3rd chunk: fix clang error: expression result unused [-Werror,-Wunused-value]
+
+--- src/lib/WPS8.cpp.orig	2011-05-03 13:00:13.000000000 +0000
 +++ src/lib/WPS8.cpp
 @@ -159,7 +159,7 @@ void WPS8Parser::readStreams(WPXInputStr
  
@@ -20,7 +22,16 @@ $NetBSD: patch-src_lib_WPS8.cpp,v 1.2 2012/02/16 18:04:08 hans Exp $
  			type = readU32(input);
  		} else input->seek(len-2,WPX_SEEK_CUR);
  
-@@ -251,7 +251,7 @@ void WPS8Parser::appendUTF16LE(WPXInputS
+@@ -196,8 +196,6 @@ void WPS8Parser::readNotes(std::vector<W
+ 	if (headerIndexTable.end() == pos)
+ 		return;
+ 
+-	pos->second.length;
+-
+ 	uint32_t unk1;
+ 	uint32_t count;
+ 	uint32_t boff;
+@@ -251,7 +249,7 @@ void WPS8Parser::appendUTF16LE(WPXInputS
  	uint16_t high_surrogate = 0;
  	bool fail = false;
  	uint16_t readVal;
