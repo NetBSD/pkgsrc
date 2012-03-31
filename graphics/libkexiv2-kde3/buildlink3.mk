82388c0ba560ed56d1e098e091b6bf612911068d
dholland <dholland@pkgsrc.org>
2012-03-31 23:46:50 +0000

    Restrict depends to avoid matching the kde4 package

--- graphics/libkexiv2-kde3/buildlink3.mk
+++ graphics/libkexiv2-kde3/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/19 23:01:43 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/31 23:47:37 dholland Exp $
 
 BUILDLINK_TREE+=	libkexiv2
 
 .if !defined(LIBKEXIV2_BUILDLINK3_MK)
 LIBKEXIV2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkexiv2+=	libkexiv2>=0.1.1
-BUILDLINK_ABI_DEPENDS.libkexiv2+=	libkexiv2>=0.1.9nb4
+BUILDLINK_API_DEPENDS.libkexiv2+=	libkexiv2>=0.1.1<4.0
+BUILDLINK_ABI_DEPENDS.libkexiv2+=	libkexiv2>=0.1.9nb4<4.0
 BUILDLINK_PKGSRCDIR.libkexiv2?=	../../graphics/libkexiv2-kde3
 
 .include "../../graphics/exiv2/buildlink3.mk"
