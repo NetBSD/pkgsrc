8e48f5e8a6d3cc2bcd251760ce4d50cfc4f0b6e1
dholland <dholland@pkgsrc.org>
2012-03-31 23:45:58 +0000

    Bump BUILDLINK_API_DEPENDS to require at least 1.1.0, which was the first kde4 version of this package in pkgsrc. Otherwise, the kde3 package can match, which causes difficulties.

--- graphics/kipi-plugins/buildlink3.mk
+++ graphics/kipi-plugins/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:41 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/03/31 23:45:58 dholland Exp $
 
 BUILDLINK_TREE+=	kipi-plugins
 
 .if !defined(KIPI_PLUGINS_BUILDLINK3_MK)
 KIPI_PLUGINS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kipi-plugins+=	kipi-plugins>=0.1.4nb6
+BUILDLINK_API_DEPENDS.kipi-plugins+=	kipi-plugins>=1.1.0
 BUILDLINK_PKGSRCDIR.kipi-plugins?=	../../graphics/kipi-plugins
 .endif # KIPI_PLUGINS_BUILDLINK3_MK
 
