fb21f6b8f5c3e2830e699cd764a21bde92220166
dholland <dholland>
2012-03-31 23:46:50 +0000

    Restrict depends to avoid matching the kde4 package

--- graphics/libkdcraw-kde3/buildlink3.mk
+++ graphics/libkdcraw-kde3/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/19 22:29:41 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/31 23:47:23 dholland Exp $
 
 BUILDLINK_TREE+=	libkdcraw
 
 .if !defined(LIBKDCRAW_BUILDLINK3_MK)
 LIBKDCRAW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkdcraw+=	libkdcraw>=0.1.1
-BUILDLINK_ABI_DEPENDS.libkdcraw?=	libkdcraw>=0.1.9nb11
+BUILDLINK_API_DEPENDS.libkdcraw+=	libkdcraw>=0.1.1<4.0
+BUILDLINK_ABI_DEPENDS.libkdcraw?=	libkdcraw>=0.1.9nb11<4.0
 BUILDLINK_PKGSRCDIR.libkdcraw?=	../../graphics/libkdcraw-kde3
 
 .include "../../x11/qt3-libs/buildlink3.mk"
