e77c5d749fd8862a35db3ae3f72d80c11ed6e738
dholland <dholland@pkgsrc.org>
2012-03-31 23:46:50 +0000

    Restrict depends to avoid matching the kde4 package

--- graphics/libkipi-kde3/buildlink3.mk
+++ graphics/libkipi-kde3/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/19 22:48:15 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/31 23:47:51 dholland Exp $
 
 BUILDLINK_TREE+=	libkipi
 
 .if !defined(LIBKIPI_BUILDLINK3_MK)
 LIBKIPI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkipi+=	libkipi>=0.1.5
-BUILDLINK_ABI_DEPENDS.libkipi?=	libkipi>=0.1.6nb12
+BUILDLINK_API_DEPENDS.libkipi+=	libkipi>=0.1.5<4.0
+BUILDLINK_ABI_DEPENDS.libkipi?=	libkipi>=0.1.6nb12<4.0
 BUILDLINK_PKGSRCDIR.libkipi?=	../../graphics/libkipi-kde3
 
 .include "../../x11/kdelibs3/buildlink3.mk"
