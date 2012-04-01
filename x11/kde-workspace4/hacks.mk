34bea0bef4e2197ba9b71645a7d2d97fe460d9c8
markd <markd@pkgsrc.org>
2012-04-01 20:53:16 +0000

    Rewrite to remove multiple post-wrapper if inet6 not defined.
    fix mkspecs/default link to point to correct directory rather than
    assuming netbsd-g++

--- x11/kde-workspace4/hacks.mk
+++ x11/kde-workspace4/hacks.mk
@@ -1,19 +1,21 @@
-# $NetBSD: hacks.mk,v 1.1 2012/03/19 19:44:03 markd Exp $
+# $NetBSD: hacks.mk,v 1.2 2012/04/01 20:53:16 markd Exp $
 
 .if !defined(KDEBASE4_HACKS_MK)
 KDEBASE4_HACKS_MK=	# defined
 
+# fix up buildlinks failure to build link tree correctly
+post-wrapper: post-wrapper-extra
+	ln -s ${BUILDLINK_PREFIX.qt4-libs}/qt4/mkspecs/default ${BUILDLINK_DIR}/qt4/mkspecs/default
+
 ### issue is specific to xproto IPv6 support
 .if empty(PKG_OPTIONS:Minet6)
 PKG_HACKS+=	X11_X_h
-post-wrapper:
+post-wrapper-extra:
 	${SED} 's,^#define FamilyInternet6.*,/* undef FamilyInternet6 */,' \
 		${BUILDLINK_DIR}/include/X11/X.h >${BUILDLINK_DIR}/include/X11/X.h.new
 	${MV} -f ${BUILDLINK_DIR}/include/X11/X.h.new ${BUILDLINK_DIR}/include/X11/X.h
+.else
+post-wrapper-extra:
 .endif
 
 .endif
-
-# fix up buildlinks failure to build link tree correctly
-post-wrapper:
-	ln -s netbsd-g++ ${BUILDLINK_DIR}/qt4/mkspecs/default
