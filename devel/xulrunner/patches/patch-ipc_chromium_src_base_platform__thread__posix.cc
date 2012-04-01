37bdcf07cab75b0f58b1e8b2135bb878eb73da69
ryoon <ryoon@pkgsrc.org>
2012-04-01 15:18:45 +0000

    Fix build on FreeBSD 9.0. * Add some conditional for FreeBSD. * Fix for FreeBSD's kinfo_proc * It seems that FreeBSD supports posix spawn, but xulrunner does not use it.   (I am not famiiar with it..., this should be fixed.) * FreeBSD 9.0 requires EXTRA_DSO_LDOPTS for nss an nspr like DragonFly 3.0.1.

--- devel/xulrunner/patches/patch-ipc_chromium_src_base_platform__thread__posix.cc
+++ devel/xulrunner/patches/patch-ipc_chromium_src_base_platform__thread__posix.cc
@@ -1,8 +1,8 @@
-$NetBSD: patch-ipc_chromium_src_base_platform__thread__posix.cc,v 1.3 2012/03/06 12:34:09 ryoon Exp $
+$NetBSD: patch-ipc_chromium_src_base_platform__thread__posix.cc,v 1.4 2012/04/01 15:18:45 ryoon Exp $
 
---- ipc/chromium/src/base/platform_thread_posix.cc.orig	2012-02-16 06:40:33.000000000 +0000
+--- ipc/chromium/src/base/platform_thread_posix.cc.orig	2012-03-13 01:36:53.000000000 +0000
 +++ ipc/chromium/src/base/platform_thread_posix.cc
-@@ -9,9 +9,13 @@
+@@ -9,9 +9,18 @@
  
  #if defined(OS_MACOSX)
  #include <mach/mach.h>
@@ -13,10 +13,15 @@ $NetBSD: patch-ipc_chromium_src_base_platform__thread__posix.cc,v 1.3 2012/03/06
  #include <unistd.h>
 +#elif defined(OS_DRAGONFLY)
 +#include <unistd.h>
++#elif defined(OS_FREEBSD)
++#include <sys/thr.h>
++_Pragma("GCC visibility push(default)")
++# include <pthread_np.h>
++_Pragma("GCC visibility pop")
  #endif
  
  #if defined(OS_MACOSX)
-@@ -33,6 +37,14 @@ PlatformThreadId PlatformThread::Current
+@@ -33,6 +42,18 @@ PlatformThreadId PlatformThread::Current
    // into the kernel.
  #if defined(OS_MACOSX)
    return mach_thread_self();
@@ -25,9 +30,13 @@ $NetBSD: patch-ipc_chromium_src_base_platform__thread__posix.cc,v 1.3 2012/03/06
 +#elif defined(OS_DRAGONFLY)
 +  return lwp_gettid();
 +#elif defined(OS_FREEBSD)
-+  lwpid_t lwpid;
-+  thr_self( &lwpid );
-+  return lwpid;
++#  if __FreeBSD_cc_version > 900000
++    return pthread_getthreadid_np();
++#  else
++    lwpid_t lwpid;
++    thr_self( &lwpid );
++    return lwpid;
++#  endif
  #elif defined (__OpenBSD__)
    // TODO(BSD): find a better thread ID
    return (intptr_t)(pthread_self());
