97dfe2209640037792459a48f70f0f4e57c3cf80
dholland <dholland>
2012-04-03 09:57:08 +0000

    Bump depends to require at least version 3.0, so evince2 isn't picked up. Bump PKGREVISION. I can't find any users of this package yet so I guess they don't need to be revbumped.

--- print/evince3/buildlink3.mk
+++ print/evince3/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/21 17:40:20 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/04/03 09:57:08 dholland Exp $
 
 BUILDLINK_TREE+=	evince
 
 .if !defined(EVINCE_BUILDLINK3_MK)
 EVINCE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.evince+=	evince>=2.30.1
-BUILDLINK_ABI_DEPENDS.evince?=	evince>=2.32.0nb9
+BUILDLINK_API_DEPENDS.evince+=	evince>=3.0
+BUILDLINK_ABI_DEPENDS.evince?=	evince>=3.0
 BUILDLINK_PKGSRCDIR.evince?=	../../print/evince
 
 .include "../../devel/glib2/buildlink3.mk"
