00ae69d88b45597bed202b99ec0f6397e249eded
abs <abs>
2012-04-03 16:32:21 +0000

    Set BUILDLINK_CPPFLAGS.iodbc - fixes at least myodbc & odbc-postgresql

--- databases/iodbc/buildlink3.mk
+++ databases/iodbc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2012/03/15 15:27:58 abs Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2012/04/03 16:32:21 abs Exp $
 
 BUILDLINK_TREE+=	iodbc
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.iodbc+=	iodbc>=3.52.3
 BUILDLINK_INCDIRS.iodbc?=	${PREFIX}/include/iodbc
 BUILDLINK_LIBDIRS.iodbc?=	${PREFIX}/lib/iodbc
 BUILDLINK_PKGSRCDIR.iodbc?=	../../databases/iodbc
+BUILDLINK_CPPFLAGS.iodbc+=	-I${BUILDLINK_PREFIX.iodbc}/include/iodbc
 
 # ODBC drivers don't need these bits, but ODBC clients do.
 .if !defined(ODBC_DRIVER)
