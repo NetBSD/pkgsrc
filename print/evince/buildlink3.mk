4fdd3e87028065bc9024e0dd234b737cd0f47b96
dholland <dholland@pkgsrc.org>
2012-04-03 09:53:44 +0000

    Restrict depends to exclude evince3.

--- print/evince/buildlink3.mk
+++ print/evince/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2012/03/03 00:12:01 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2012/04/03 09:53:44 dholland Exp $
 
 BUILDLINK_TREE+=	evince
 
 .if !defined(EVINCE_BUILDLINK3_MK)
 EVINCE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.evince+=	evince>=2.30.1
-BUILDLINK_ABI_DEPENDS.evince?=	evince>=2.32.0nb10
+BUILDLINK_API_DEPENDS.evince+=	evince>=2.30.1<3
+BUILDLINK_ABI_DEPENDS.evince?=	evince>=2.32.0nb10<3
 BUILDLINK_PKGSRCDIR.evince?=	../../print/evince
 
 .include "../../devel/glib2/buildlink3.mk"
