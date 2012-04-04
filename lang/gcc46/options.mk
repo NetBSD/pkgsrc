b7ab4816a37f2d2a2d2904e72ba78774854421c7
sbd <sbd>
2012-04-04 22:18:29 +0000

    Rename option "gcc-c++" to "gcc-cpp" (with legacy support) as pkglint complains with: "gcc-c++" is not a valid option name.

--- lang/gcc46/options.mk
+++ lang/gcc46/options.mk
@@ -1,13 +1,14 @@
-# $NetBSD: options.mk,v 1.4 2012/01/14 02:09:36 hans Exp $
+# $NetBSD: options.mk,v 1.5 2012/04/04 22:20:37 sbd Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gcc46
-PKG_SUPPORTED_OPTIONS=	nls gcc-c++ gcc-fortran gcc-java gcc-objc #gcc-ada
-PKG_SUGGESTED_OPTIONS=	gcc-c++ gcc-fortran gcc-objc
+PKG_SUPPORTED_OPTIONS=	nls gcc-cpp gcc-fortran gcc-java gcc-objc #gcc-ada
+PKG_SUGGESTED_OPTIONS=	gcc-cpp gcc-fortran gcc-objc
+PKG_OPTIONS_LEGACY_OPTS= gcc-c++:gcc-cpp
 .if ${OPSYS} == "NetBSD"
 PKG_SUGGESTED_OPTIONS+=	nls
 .endif
 
-PKG_OPTIONS_LEGACY_VARS+=	BUILD_CXX:gcc-c++
+PKG_OPTIONS_LEGACY_VARS+=	BUILD_CXX:gcc-cpp
 PKG_OPTIONS_LEGACY_VARS+=	BUILD_FORTRAN:gcc-fortran
 PKG_OPTIONS_LEGACY_VARS+=	BUILD_JAVA:gcc-java
 PKG_OPTIONS_LEGACY_VARS+=	BUILD_OBJC:gcc-objc
@@ -36,8 +37,8 @@ CONFIGURE_ARGS+=	--disable-nls
 LANGS=			c
 
 .if !empty(PKG_OPTIONS:Mgcc-java)
-.  if empty(PKG_OPTIONS:Mgcc-c++)
-PKG_OPTIONS+=		gcc-c++
+.  if empty(PKG_OPTIONS:Mgcc-cpp)
+PKG_OPTIONS+=		gcc-cpp
 .  endif
 
 LANGS+=			java
@@ -64,7 +65,7 @@ MAKE_ENV+=		ac_cv_prog_JAR=no
 .include "../../lang/python/application.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mgcc-c++)
+.if !empty(PKG_OPTIONS:Mgcc-cpp)
 LANGS+=			c++
 USE_TOOLS+=		perl
 CONFIGURE_ARGS+=	--enable-__cxa_atexit
