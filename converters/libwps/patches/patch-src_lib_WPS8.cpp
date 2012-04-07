496b14ac9264da924c4a198535ee7a025169918f
wiz <wiz@pkgsrc.org>
2012-04-07 15:08:02 +0000

    Update to 0.2.4:
    	Bump version for possible release
    	Update doxygen.cfg file
    	Ignore the unprintable characters which can appear in the text.
    	Ignore some more files
    	Add -Weffc++ option for GCC to avoid producing crappy code :)
    	Fix build inside a new LibreOffice
    	Add Laurent to main authors (my apology for this forgetful attitude)
    	Fix distcheck in build/win32
    	Fixing MSVC build
    	Bump version for a possible release + remove autopackage that nobody uses anyway
    	Update of src/conv/ by using libwpd/src/conv as model.
    	Small changes: - in WPS4.cpp: correction of an uninitialized variable, - in WPS8.cpp: small improvement of the footnote/endnote display, - in WPSContentListener.cpp: check that the lists are closed before closing a section.
    	Plant some .gitignore files all around the place
    	Generate ChangeLog on make dist
    	Fix callgraph when footnotes are present
    	check that the property remains in the FOD zone (patch Laurent Alonso)
    	Fix different errors (patch Laurent Alonso)
    	Fix some debug build warnings/errors
    	Cleanup and rewrite of build system (patch Tomas Chvatal)
    	Fix windows gcc build of libwps inside LibreOffice tree (patch Jan Holesovsky <kendy@suse.cz>)
    	It seems that cfod 0x54 is legitimate (patch Urmas D. <davian818@gmail.com>)
    	Update configure.ac to new syntax. Patch of Tomas Chvatal <tchvatal@suse.cz>
    	Rename configure.in to configure.ac Patch of Tomas Chvatal <tchvatal@suse.cz>
    	Fix makefile.am's to new syntax. Patch of Tomas Chvatal <tchvatal@suse.cz>
    	Fix gcc-4.6 build

--- converters/libwps/patches/patch-src_lib_WPS8.cpp
+++ converters/libwps/patches/patch-src_lib_WPS8.cpp
@@ -1,28 +1,11 @@
-$NetBSD: patch-src_lib_WPS8.cpp,v 1.3 2012/03/28 15:17:19 wiz Exp $
+$NetBSD: patch-src_lib_WPS8.cpp,v 1.4 2012/04/07 15:08:02 wiz Exp $
 
-3rd chunk: fix clang error: expression result unused [-Werror,-Wunused-value]
+Fix clang error: expression result unused [-Werror,-Wunused-value]
+https://sourceforge.net/tracker/?func=detail&aid=3512534&group_id=176121&atid=875977
 
---- src/lib/WPS8.cpp.orig	2011-05-03 13:00:13.000000000 +0000
+--- src/lib/WPS8.cpp.orig	2011-11-10 16:05:28.000000000 +0000
 +++ src/lib/WPS8.cpp
-@@ -159,7 +159,7 @@ void WPS8Parser::readStreams(WPXInputStr
- 
- 	for (unsigned i=0; i < n_streams; i++) {
- 		uint16_t len;
--		uint32_t unknown, type = 0;
-+		uint32_t type = 0;
- 
- 		len = readU16(input);
- 		if (len > 10) {
-@@ -168,7 +168,7 @@ void WPS8Parser::readStreams(WPXInputStr
- 		}
- 
- 		if (len > 4) {
--			unknown = readU32(input); // assume == 0x22000000
-+			(void) readU32(input); // assume == 0x22000000
- 			type = readU32(input);
- 		} else input->seek(len-2,WPX_SEEK_CUR);
- 
-@@ -196,8 +196,6 @@ void WPS8Parser::readNotes(std::vector<W
+@@ -205,8 +205,6 @@ void WPS8Parser::readNotes(std::vector<W
  	if (headerIndexTable.end() == pos)
  		return;
  
@@ -31,12 +14,3 @@ $NetBSD: patch-src_lib_WPS8.cpp,v 1.3 2012/03/28 15:17:19 wiz Exp $
  	uint32_t unk1;
  	uint32_t count;
  	uint32_t boff;
-@@ -251,7 +249,7 @@ void WPS8Parser::appendUTF16LE(WPXInputS
- 	uint16_t high_surrogate = 0;
- 	bool fail = false;
- 	uint16_t readVal;
--	uint32_t ucs4Character;
-+	uint32_t ucs4Character = 0;
- 	while (true) {
- 		if (input->atEOS()) {
- 			fail = true;
