6069895b0088d43c491fbad845301cbe317ae9b7
schwarz <schwarz@pkgsrc.org>
2012-04-07 15:14:04 +0000

    corrected patch-lib_isnan.c to correctly terminate with a '\' the line within a multi-line statement. Gcc does not seem to care about this, however for Sun cc the missing '\' broke things.

--- devel/bison/patches/patch-lib_isnan.c
+++ devel/bison/patches/patch-lib_isnan.c
@@ -1,11 +1,13 @@
-$NetBSD: patch-lib_isnan.c,v 1.1 2012/03/15 07:50:22 jklos Exp $
+$NetBSD: patch-lib_isnan.c,v 1.2 2012/04/07 15:14:04 schwarz Exp $
 
---- lib/isnan.c.orig	2011-04-16 19:08:41.000000000 +0000
-+++ lib/isnan.c
-@@ -119,6 +119,7 @@ FUNC (DOUBLE x)
+--- lib/isnan.c.orig	2011-04-16 21:08:41.000000000 +0200
++++ lib/isnan.c	2012-04-06 16:16:04.995001524 +0200
+@@ -118,7 +118,8 @@
+   /* Be careful to not do any floating-point operation on x, such as x == x,
       because x may be a signaling NaN.  */
  #  if defined __TINYC__ || defined __SUNPRO_C || defined __DECC \
-       || (defined __sgi && !defined __GNUC__) || defined __ICC
+-      || (defined __sgi && !defined __GNUC__) || defined __ICC
++      || (defined __sgi && !defined __GNUC__) || defined __ICC \
 +      || defined __vax__
    /* The Sun C 5.0, Intel ICC 10.0, and Compaq (ex-DEC) 6.4 compilers don't
       recognize the initializers as constant expressions.  The latter compiler
