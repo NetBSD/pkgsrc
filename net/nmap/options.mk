4faa20286079b6833043b7bbd12c0e72039551f6
wiz <wiz@pkgsrc.org>
2012-04-08 19:08:44 +0000

    Remove python24 and all traces of it from pkgsrc. Remove devel/py-ctypes (only needed by and supporting python24). Remove PYTHON_VERSIONS_ACCEPTED and PYTHON_VERSIONS_INCOMPATIBLE lines that just mirror defaults now. Miscellaneous cleanup while editing all these files.

--- net/nmap/options.mk
+++ net/nmap/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2011/11/22 12:18:08 pettai Exp $
+# $NetBSD: options.mk,v 1.7 2012/04/08 19:09:09 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nmap
 
@@ -20,7 +20,6 @@ CONFIGURE_ARGS+=	--disable-ipv6
 .if !empty(PKG_OPTIONS:Mzenmap)
 CONFIGURE_ARGS+=		--with-zenmap
 PLIST_SRC+=			${PKGDIR}/PLIST.zenmap
-PYTHON_VERSIONS_ACCEPTED=	27 26 25 24
 PY_PATCHPLIST=			yes
 REPLACE_PYTHON=			zenmap/zenmapCore/*.py
 REPLACE_PYTHON+=		zenmap/zenmapGUI/*.py
@@ -38,7 +37,6 @@ CONFIGURE_ARGS+=	--without-zenmap
 .if !empty(PKG_OPTIONS:Mndiff)
 CONFIGURE_ARGS+=		--with-ndiff
 PLIST_SRC+=			${PKGDIR}/PLIST.ndiff
-PYTHON_VERSIONS_ACCEPTED=	27 26 25 24
 .else
 CONFIGURE_ARGS+=		--without-ndiff
 .endif
