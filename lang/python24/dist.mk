7c2420d7f98fd81cad5e0f6b0bef4946dc1586fe
wiz <wiz@pkgsrc.org>
2012-04-08 19:08:44 +0000

    Remove python24 and all traces of it from pkgsrc. Remove devel/py-ctypes (only needed by and supporting python24). Remove PYTHON_VERSIONS_ACCEPTED and PYTHON_VERSIONS_INCOMPATIBLE lines that just mirror defaults now. Miscellaneous cleanup while editing all these files.

--- lang/python24/dist.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: dist.mk,v 1.2 2011/04/23 08:53:53 obache Exp $
-
-PY_DISTVERSION=	2.4.6
-DISTNAME=	Python-${PY_DISTVERSION}
-EXTRACT_SUFX=	.tar.bz2
-DISTINFO_FILE=	${.CURDIR}/../../lang/python24/distinfo
-PATCHDIR=	${.CURDIR}/../../lang/python24/patches
-MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION}/
