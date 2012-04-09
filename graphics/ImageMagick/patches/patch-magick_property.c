a1826209f30404a4efefb3c61e8278d5e1044449
tez <tez@pkgsrc.org>
2012-04-09 15:56:21 +0000

    Add patch for CVE-2012-1610 from http://www.imagemagick.org/discourse-server/viewtopic.php?t=20629

--- graphics/ImageMagick/patches/patch-magick_property.c
+++ graphics/ImageMagick/patches/patch-magick_property.c
@@ -1,10 +1,20 @@
-$NetBSD: patch-magick_property.c,v 1.1 2012/03/30 03:50:47 taca Exp $
+$NetBSD: patch-magick_property.c,v 1.2 2012/04/09 15:56:21 tez Exp $
 
 * Fix for CVE-2012-0259.
+* Fix for CVE-2012-1610.
 
 --- magick/property.c.orig	2012-03-01 01:41:19.000000000 +0000
 +++ magick/property.c
-@@ -1309,6 +1309,8 @@ static MagickBooleanType GetEXIFProperty
+@@ -1286,6 +1286,8 @@ static MagickBooleanType GetEXIFProperty
+         break;
+       components=(ssize_t) ((int) ReadPropertyLong(endian,q+4));
+       number_bytes=(size_t) components*tag_bytes[format];
++      if (number_bytes < components)
++        break; /* prevent overflow */
+       if (number_bytes <= 4)
+         p=q+8;
+       else
+@@ -1309,6 +1311,8 @@ static MagickBooleanType GetEXIFProperty
              buffer[MaxTextExtent],
              *value;
  
