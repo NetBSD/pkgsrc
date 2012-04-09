ce226a9d120d9d0a40b5414e81de24712f93614b
adam <adam@pkgsrc.org>
2012-04-09 09:12:49 +0000

    Changes 4.8.1: Qt 4.8.1 is the first patch release to the 4.8 series with over 200 functional improvements to the desktop and embedded platforms. The majority of the error corrections, made primarily by Digia, have been on desktop and embedded platforms with a large number of fixes with focus on QtCore, QtGUI and QtNetwork. These fixes benefit all desktop platforms. Qt 4.8.1 also includes Mac App Store support.

--- x11/qt4-libs/patches/patch-src_network_ssl_qsslsocket__openssl__symbols.cpp
+++ x11/qt4-libs/patches/patch-src_network_ssl_qsslsocket__openssl__symbols.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_network_ssl_qsslsocket__openssl__symbols.cpp,v 1.3 2012/01/14 10:23:18 obache Exp $
+$NetBSD: patch-src_network_ssl_qsslsocket__openssl__symbols.cpp,v 1.4 2012/04/09 09:12:49 adam Exp $
 
 * fixes build with OpenSSL 0.9.9-dev
 * fix build with -openssl-linked for OpenSSL 0.9.8*
   http://qt.gitorious.org/qt/qt/commit/4db91cbd6147e40f543342f22c05b7baddc52e5a
 
---- src/network/ssl/qsslsocket_openssl_symbols.cpp.orig	2011-12-08 05:06:02.000000000 +0000
+--- src/network/ssl/qsslsocket_openssl_symbols.cpp.orig	2012-03-14 14:01:30.000000000 +0000
 +++ src/network/ssl/qsslsocket_openssl_symbols.cpp
 @@ -171,7 +171,7 @@ DEFINEFUNC(int, SSL_CTX_check_private_ke
  #endif
@@ -15,18 +15,7 @@ $NetBSD: patch-src_network_ssl_qsslsocket__openssl__symbols.cpp,v 1.3 2012/01/14
  DEFINEFUNC(SSL_CTX *, SSL_CTX_new, const SSL_METHOD *a, a, return 0, return)
  #else
  DEFINEFUNC(SSL_CTX *, SSL_CTX_new, SSL_METHOD *a, a, return 0, return)
-@@ -210,18 +210,14 @@ DEFINEFUNC(int, SSL_library_init, void, 
- DEFINEFUNC(void, SSL_load_error_strings, void, DUMMYARG, return, DUMMYARG)
- DEFINEFUNC(SSL *, SSL_new, SSL_CTX *a, a, return 0, return)
- #if OPENSSL_VERSION_NUMBER >= 0x0090806fL && !defined(OPENSSL_NO_TLSEXT)
--#if OPENSSL_VERSION_NUMBER >= 0x10000000L
- DEFINEFUNC4(long, SSL_ctrl, SSL *a, a, int cmd, cmd, long larg, larg, void *parg, parg, return -1, return)
--#else
--DEFINEFUNC4(long, SSL_ctrl, SSL *a, a, int cmd, cmd, long larg, larg, const void *parg, parg, return -1, return)
--#endif
- #endif
- DEFINEFUNC3(int, SSL_read, SSL *a, a, void *b, b, int c, c, return -1, return)
- DEFINEFUNC3(void, SSL_set_bio, SSL *a, a, BIO *b, b, BIO *c, c, return, DUMMYARG)
+@@ -217,7 +217,7 @@ DEFINEFUNC3(void, SSL_set_bio, SSL *a, a
  DEFINEFUNC(void, SSL_set_accept_state, SSL *a, a, return, DUMMYARG)
  DEFINEFUNC(void, SSL_set_connect_state, SSL *a, a, return, DUMMYARG)
  DEFINEFUNC(int, SSL_shutdown, SSL *a, a, return -1, return)
