440e1953e0a7ea71df860597614464b26be8c5b0
tsutsui <tsutsui@pkgsrc.org>
2012-04-10 14:12:50 +0000

    Add a workaround hack for gcc failure on NetBSD/sh3el 6.0_BETA.

--- lang/ruby193-base/hacks.mk
+++ lang/ruby193-base/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1.1.1 2011/11/08 16:10:51 taca Exp $
+# $NetBSD: hacks.mk,v 1.2 2012/04/10 14:12:50 tsutsui Exp $
 
 .if !defined(RUBY19_BASE_HACKS_MK)
 RUBY19_BASE_HACKS_MK=	defined
@@ -16,6 +16,15 @@ RUBY19_BASE_HACKS_MK=	defined
 PKG_HACKS+=		optimisation
 BUILDLINK_TRANSFORM+=	rm:-O[0-9]*
 .  endif
+.endif
+
+# On NetBSD/sh3el 6.0, the default -Os causes an error on compiling node.c:
+#  {standard input}: Assembler messages: {standard input}:1458: \
+#  Error: pcrel too far
+# and -O1 works around.
+.if !empty(MACHINE_PLATFORM:MNetBSD-*-sh3*) && !empty(CC_VERSION:Mgcc-4.5.*)
+PKG_HACKS+=		optimisation
+BUILDLINK_TRANSFORM+=	rename:-Os:-O1 rm:-freorder-blocks
 .endif
 
 .endif	# RUBY19_BASE_HACKS_MK
