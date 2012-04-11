377503272c7da9e2b12d49f4ca7eabe3bd6c2a74
drochner <drochner@pkgsrc.org>
2012-04-11 11:40:18 +0000

    add librest-0.7.12 (needed for newer shotwell) This is API incompatible to pkgsrc/www/librest, and can coexist.

---
+++ www/librest07/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/04/11 11:40:18 drochner Exp $
+
+BUILDLINK_TREE+=	librest
+
+.if !defined(LIBREST_BUILDLINK3_MK)
+LIBREST_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.librest+=	librest07>=0.7
+BUILDLINK_ABI_DEPENDS.librest?=	librest07>=0.7
+BUILDLINK_PKGSRCDIR.librest?=	../../www/librest07
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../net/libsoup24/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.endif	# LIBREST_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-librest
