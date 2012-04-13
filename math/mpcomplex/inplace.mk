3754255605425bad2bf0ee87cbc12b379c591256
hans <hans@pkgsrc.org>
2012-04-13 11:00:14 +0000

    Add inplace.mk to allow building this inside another package.

---
+++ math/mpcomplex/inplace.mk
@@ -0,0 +1,13 @@
+# $NetBSD: inplace.mk,v 1.1 2012/04/13 11:00:14 hans Exp $
+#
+# Include this file to extract math/mpcomplex source into the WRKSRC of
+# another package. This is to be used by GCC packages to avoid the numerous
+# dependencies math/mpcomplex has.
+
+post-extract: extract-inplace-mpcomplex
+
+extract-inplace-mpcomplex:
+	(cd ../../math/mpcomplex && ${MAKE} WRKDIR=${WRKSRC}/.devel.mpcomplex EXTRACT_DIR=${WRKSRC} \
+		WRKSRC='$${EXTRACT_DIR}/$${DISTNAME}' SKIP_DEPENDS=YES fetch patch clean)
+	${MV} ${WRKSRC}/mpc-* ${WRKSRC}/mpc
+
