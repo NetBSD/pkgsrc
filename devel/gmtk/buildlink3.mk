86ec604c25f63fec444767d80cefbf0f95e4cc3b
obache <obache@pkgsrc.org>
2012-04-13 12:57:33 +0000

    Update gmtk to 1.0.6. (while here, fixes message catalogue installation with gmake)
    1.0.6
    	Assign NULL to freed strings
    	Updated Japanese translation
    	Remove debug message
    	Kill GTK widget oversize warning
        Apply recommendation from Issue #6
        Apply patch from Issue #7, codecs not reset to NULL
    	Only do the restart on decoding error if the position is 0 (window size issue)
    	Allow one decoding error restart
    	Fix error when shifting protocol from mms to mmsh
    	Updated Turkish translation
    1.0.6b1
        Track gtk2 and gtk3 in gmtk.pc file
        Enhance subtitle visibility tracking
        Handle the keyboard masks more correctly
        Only allocate a size for the child window if the parent widget is realized
        Fix volume tracking on default device, even when the device changes
        Switch "PulseAudio" from a softvol to a real PulseAudio control (still can be forced to softvol if needed)
            This allows for better volume control that is synced with the system volume and the default output device
            This should be the default setting on machines with PulseAudio
        Add -zoom to mplayer when vo is x11
        Updated German translation
        Change po file install location
        Do PKG_CHECK for GTK3 when --enable-gtk=yes is specified
        Use the localedir variable in the Makefile may fix Wolfgang's issue
        Add gmlib to the list of referenced libaries Issue #3
        Add pulse cflags and libs to libgmtk Issue #2
        Detect if libm should be used and use if needed Issue #1
        Add files to SVN as recommended by autoreconf -fiv

--- devel/gmtk/buildlink3.mk
+++ devel/gmtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2012/03/03 00:12:24 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2012/04/13 12:57:33 obache Exp $
 #
 
 BUILDLINK_TREE+=	gmtk
@@ -6,8 +6,7 @@ BUILDLINK_TREE+=	gmtk
 .if !defined(GMTK_BUILDLINK3_MK)
 GMTK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gmtk+=	gmtk>=1.0.5
-BUILDLINK_ABI_DEPENDS.gmtk?=	gmtk>=1.0.5nb3
+BUILDLINK_API_DEPENDS.gmtk+=	gmtk>=1.0.6
 BUILDLINK_PKGSRCDIR.gmtk?=	../../devel/gmtk
 
 pkgbase := gmtk
