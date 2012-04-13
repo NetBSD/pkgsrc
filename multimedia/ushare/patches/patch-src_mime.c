509248a7334d5c629bd8600f98291735739971c5
obache <obache@pkgsrc.org>
2012-04-13 23:44:38 +0000

    * fixes build with libupnp>=1.6.7. * Bump PKGREVISION from libupnp shlib bump.

---
+++ multimedia/ushare/patches/patch-src_mime.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-src_mime.c,v 1.1 2012/04/13 23:44:39 obache Exp $
+
+* some header files are not included by libupnp>=1.6.7.
+
+--- src/mime.c.orig	2007-12-09 13:03:36.000000000 +0000
++++ src/mime.c
+@@ -21,6 +21,7 @@
+ 
+ #include <stdlib.h>
+ #include <string.h>
++#include <stdio.h>
+ 
+ #include "mime.h"
+ #include "ushare.h"
