590cbada61e5d5f18d12ca165db386f1ff8497ff
hans <hans@pkgsrc.org>
2012-04-13 11:00:14 +0000

    Add inplace.mk to allow building this inside another package.

---
+++ devel/gmp/inplace.mk
@@ -0,0 +1,13 @@
+# $NetBSD: inplace.mk,v 1.1 2012/04/13 11:00:14 hans Exp $
+#
+# Include this file to extract devel/gmp source into the WRKSRC of
+# another package. This is to be used by GCC packages to avoid the
+# numerous dependencies devel/gmp has.
+
+post-extract: extract-inplace-gmp
+
+extract-inplace-gmp:
+	(cd ../../devel/gmp && ${MAKE} WRKDIR=${WRKSRC}/.devel.gmp EXTRACT_DIR=${WRKSRC} \
+		WRKSRC='$${EXTRACT_DIR}/$${DISTNAME}' SKIP_DEPENDS=YES fetch patch clean)
+	${MV} ${WRKSRC}/gmp-* ${WRKSRC}/gmp
+
