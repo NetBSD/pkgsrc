1ec5cc4edaad94bc5cc71597d166f151d35c3f4b
wiz <wiz@pkgsrc.org>
2012-04-15 15:43:43 +0000

    Update to 2.0.7. Fix a bug in buildlink3.mk while here.
    2012-03-22 Raptor2 Version 2.0.7 Released
    
       CVE-2012-0037 fixed
       Removed Expat support
       Removed internal Unicode NFC code for better and optional ICU
       Added options for denying file requests and XML entity loading
       Added options for SSL certificate verifying
       Fixed reported issues: 0000448 and 0000469

--- textproc/raptor2/buildlink3.mk
+++ textproc/raptor2/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/03/20 15:18:05 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/04/15 15:43:43 wiz Exp $
 
 BUILDLINK_TREE+=	raptor2
 
 .if !defined(RAPTOR2_BUILDLINK3_MK)
-RAPTOR_BUILDLINK3_MK:=
+RAPTOR2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.raptor2?=	raptor2>=2.0.6
 BUILDLINK_ABI_DEPENDS.raptor2+=	raptor2>=2.0.6
