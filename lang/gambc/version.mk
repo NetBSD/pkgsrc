d30e9e66555085664b2257c283a40dd3069e0164
asau <asau@pkgsrc.org>
2012-04-16 10:19:34 +0000

    Update to Gambit C 4.6.5
    Changes since version 4.6.3 include various build system fixes,
    iOS REPL improvements, addition of Xlib examples.

--- lang/gambc/version.mk
+++ lang/gambc/version.mk
@@ -1,10 +1,10 @@
-# $NetBSD: version.mk,v 1.3 2010/11/23 21:22:11 asau Exp $
+# $NetBSD: version.mk,v 1.4 2012/04/16 10:19:34 asau Exp $
 ###
 .if !defined(GAMBC_VERSION_MK)
 GAMBC_VERSION_MK=	# empty
 ###
 ### The package version sets the distribution version.
 ###
-GAMBC_VERSION=		4.6.0
+GAMBC_VERSION=		4.6.5
 GAMBC_DIST_VERSION=	${GAMBC_VERSION:C|^.*|v&|:S|.|_|g}
 .endif
