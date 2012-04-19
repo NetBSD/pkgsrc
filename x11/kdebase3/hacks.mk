20e05e7faa7156aac3afeaa97a40c9b5e85d907d
joerg <joerg@pkgsrc.org>
2012-04-19 21:10:24 +0000

    Can't disable exceptions with Clang, since OpenEXR depends on them.

--- x11/kdebase3/hacks.mk
+++ x11/kdebase3/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2007/10/31 11:56:44 markd Exp $
+# $NetBSD: hacks.mk,v 1.2 2012/04/19 21:10:24 joerg Exp $
 
 .if !defined(KDEBASE3_HACKS_MK)
 KDEBASE3_HACKS_MK=	# defined
@@ -10,6 +10,12 @@ post-wrapper:
 	${SED} 's,^#define FamilyInternet6.*,/* undef FamilyInternet6 */,' \
 		${BUILDLINK_DIR}/include/X11/X.h >${BUILDLINK_DIR}/include/X11/X.h.new
 	${MV} ${BUILDLINK_DIR}/include/X11/X.h.new ${BUILDLINK_DIR}/include/X11/X.h
+.endif
+
+.include "../../mk/compiler.mk"
+.if !empty(PKGSRC_COMPILER:Mclang)
+# OpenEXR uses exceptions in the header files
+BUILDLINK_TRANSFORM+=	rm:-fno-exceptions
 .endif
 
 .endif
