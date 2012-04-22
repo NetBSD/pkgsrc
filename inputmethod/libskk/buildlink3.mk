60949ea40387dc1ff4829edfb6b8deee035cafff
obache <obache@pkgsrc.org>
2012-04-22 13:21:04 +0000

    Import libskk-0.0.12 as inputmethod/libskk.
    libskk -- a library to deal with Japanese kana-to-kanji conversion method
    
    Features:
    
    * Support basic features of SKK including new word registration into
      dictionary, completion, numeric conversion, abbrev mode, kuten
      input, hankaku-katakana input, Lisp expression evaluation (concat
      only), and re-conversion.
    
    * Support various typing rules including romaji-to-kana, AZIK,
      TUT-Code, and NICOLA.
    
    * Support various dictionary types including file dictionary (such as
      SKK-JISYO.[SML]), user dictionary, skkserv, and CDB format
      dictionary.
    
    * GObject based API with gobject-introspection support.
    
    * Experimental support for intelligent kana-to-kanji conversion based
      on Viterbi algorithm.

---
+++ inputmethod/libskk/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/04/22 13:21:04 obache Exp $
+#
+
+BUILDLINK_TREE+=	libskk
+
+.if !defined(LIBSKK_BUILDLINK3_MK)
+LIBSKK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libskk+=	libskk>=0.0.12
+BUILDLINK_PKGSRCDIR.libskk?=	../../inputmethod/libskk
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/libgee/buildlink3.mk"
+.include "../../textproc/json-glib/buildlink3.mk"
+.endif	# LIBSKK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libskk
