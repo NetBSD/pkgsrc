6360b816e85a2f37175ee024cbf1daa277ebdac8
wiz <wiz@pkgsrc.org>
2012-04-22 15:13:26 +0000

    Update to 3.1.0:
    Changes from versions 3.0.* to version 3.1.0:
    - The "canard à l'orange" release.
    - The MPFR source has been reorganized.
    - Dropped ansi2knr support.
    - TLS support is now detected automatically. If TLS is supported, MPFR is
      built as thread safe by default. To disable TLS explicitly, configure
      MPFR with --disable-thread-safe.
    - New --enable-gmp-internals configure option to use GMP's undocumented
      functions (not from the public API). Note that library versioning is
      not guaranteed to work if this option is used.
    - The mpfr_urandom and mpfr_urandomb functions now return identical values
      on processors with different word size (assuming the same random seed, and
      since the GMP random generator does not depend itself on the word size,
      cf http://gmplib.org/list-archives/gmp-devel/2010-September/001642.html).
    - The mpfr_add_one_ulp and mpfr_sub_one_ulp macros (which are obsolete and
      no more documented) will be removed in a future release.
    - Speed improvement for the mpfr_sqr and mpfr_div functions using Mulders'
      algorithm. As a consequence, other functions using those routines are
      also faster.
    - Much faster formatted output (mpfr_printf, etc.) with %Rg and similar.
    - The --with-gmp-build configure option can now be used when the GMP
      source directory and the GMP build directory are different (without
      having to copy header files manually as before).
    - New functions mpfr_buildopt_gmpinternals_p, mpfr_buildopt_tune_case,
      mpfr_frexp, mpfr_grandom and mpfr_z_sub.
    - New divide-by-zero exception (flag) and associated functions.
    - The mpfr.h header can be included several times, while still supporting
      optional functions (see Section "Headers and Libraries" in the manual).
    - Updated tuning parameters.
    - Improved MPFR manual.
    - MPFR tests: libtool no longer generates wrapper scripts with "make check"
      (so that running the tests under valgrind or gdb is easier).
    - Bug fixes.
      Note: The mpfr_subnormalize implementation up to MPFR 3.0.0 did not change
      the flags. In particular, it did not follow the generic rule concerning
      the inexact flag (and no special behavior was specified). The case of the
      underflow flag was more a lack of specification.
    
    NetBSD-6.99.4/amd64:
    All 160 tests passed
    (1 test was not run)

--- math/mpfr/inplace.mk
+++ math/mpfr/inplace.mk
@@ -1,4 +1,4 @@
-# $NetBSD: inplace.mk,v 1.1 2012/04/13 11:00:14 hans Exp $
+# $NetBSD: inplace.mk,v 1.2 2012/04/22 15:13:26 wiz Exp $
 #
 # Include this file to extract math/mpfr source into the WRKSRC of
 # another package. This is to be used by GCC packages to avoid the
@@ -10,4 +10,3 @@ extract-inplace-mpfr:
 	(cd ../../math/mpfr && ${MAKE} WRKDIR=${WRKSRC}/.devel.mpfr EXTRACT_DIR=${WRKSRC} \
 		WRKSRC='$${EXTRACT_DIR}/$${DISTNAME}' SKIP_DEPENDS=YES fetch patch clean)
 	${MV} ${WRKSRC}/mpfr-* ${WRKSRC}/mpfr
-
