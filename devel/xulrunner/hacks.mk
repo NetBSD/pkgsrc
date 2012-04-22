a1036ef4abd5be3efb0a8f090c7efdd73acb09ed
joerg <joerg@pkgsrc.org>
2012-04-22 20:30:43 +0000

    Disable warning for using non-POD return type in combination with extern "C".

--- devel/xulrunner/hacks.mk
+++ devel/xulrunner/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.2 2011/07/08 15:51:02 wiz Exp $
+# $NetBSD: hacks.mk,v 1.3 2012/04/22 20:30:43 joerg Exp $
 
 .if !defined(XULRUNNER_HACKS_MK)
 XULRUNNER_HACKS_MK=	defined
@@ -10,6 +10,10 @@ XULRUNNER_HACKS_MK=	defined
 .if !empty(PKGSRC_COMPILER:Mgcc) && !empty(CC_VERSION:Mgcc-4.[0-3]*) && !empty(MACHINE_PLATFORM:M*-*-i386)
 PKG_HACKS+=		optimization
 BUILDLINK_TRANSFORM+=	rm:-march=[-_A-Za-z0-9]*
+.endif
+
+.if !empty(PKGSRC_COMPILER:Mclang)
+CXXFLAGS+=	-Wno-return-type-c-linkage
 .endif
 
 .endif	# XULRUNNER_HACKS_MK
