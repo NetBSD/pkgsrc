8b4f2e34432370e76297604e84d6dc6a87883476
obache <obache@pkgsrc.org>
2012-04-22 13:57:30 +0000

    Update ibus to 1.4.1.
    2012-02-07 ibus 1.4.1 stable release
    * Many bug fixings.
    
    2011-09-23 ibus 1.4.0 stable release
    * Remove libdbus dependence, use glib gdbus instead.
    * Support surrounding text feature.
    * Add dconf config backend.
    * Add memory config backend.
    * Support replacing ibus panel on the fly.
    * Add asynchronous API.
    * Remove most blocking function call in daemon.
    * Improve gobject-introspection support.
    * Improve vala language support.
    * Improve unit test.
    * Many bug fixes.

--- inputmethod/ibus/buildlink3.mk
+++ inputmethod/ibus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2012/03/03 00:12:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2012/04/22 13:57:30 obache Exp $
 #
 
 BUILDLINK_TREE+=	ibus
@@ -6,13 +6,10 @@ BUILDLINK_TREE+=	ibus
 .if !defined(IBUS_BUILDLINK3_MK)
 IBUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ibus+=	ibus>=1.2.0.20091024
-BUILDLINK_ABI_DEPENDS.ibus+=	ibus>=1.3.9nb5
+BUILDLINK_API_DEPENDS.ibus+=	ibus>=1.4.0
 BUILDLINK_PKGSRCDIR.ibus?=	../../inputmethod/ibus
 
-.include "../../devel/gettext-lib/buildlink3.mk"
-.include "../../sysutils/dbus-glib/buildlink3.mk"
-.include "../../x11/libX11/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
 .endif	# IBUS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ibus
