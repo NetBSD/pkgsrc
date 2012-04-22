4f07b5c319332bec1620b6487193c7f7ee0b5c1c
wiz <wiz@pkgsrc.org>
2012-04-22 16:11:13 +0000

    Update to 1.8, fix compilation issues. Removed glib2 dependency, bump depends in bl3.mk.
    Version 1.8
    ~~~~~~~~~~~
    
    - Changes:
      * Add cython bindings
      * Fix error in swig bindings
      * Fix memory corruption in libcnary
    
    Version 1.7
    ~~~~~~~~~~~
    
    - Changes:
      * Fix building on Big Endian systems
    
    Version 1.6
    ~~~~~~~~~~~
    
    - Changes:
      * Updated libcnary sources, adding license and copyright info
    
    Version 1.5
    ~~~~~~~~~~~
    
    - Changes:
      * Removed glib dependency, libplist now uses libcnary
        (http://github.com/Chronic-Dev/libcnary)
      * Fix building of python bindings with GCC 4.6

--- textproc/libplist/buildlink3.mk
+++ textproc/libplist/buildlink3.mk
@@ -1,16 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.3 2012/03/03 00:12:03 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2012/04/22 16:11:13 wiz Exp $
 
 BUILDLINK_TREE+=	libplist
 
 .if !defined(LIBPLIST_BUILDLINK3_MK)
 LIBPLIST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libplist+=	libplist>=1.3
-BUILDLINK_ABI_DEPENDS.libplist?=	libplist>=1.4nb3
+BUILDLINK_API_DEPENDS.libplist+=	libplist>=1.8
 BUILDLINK_PKGSRCDIR.libplist?=	../../textproc/libplist
 
 .include "../../textproc/libxml2/buildlink3.mk"
-.include "../../devel/glib2/buildlink3.mk"
 .endif	# LIBPLIST_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libplist
