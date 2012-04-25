ad04c19fc17258f5a4346c80964b03835d04af62
drochner <drochner@pkgsrc.org>
2012-04-25 15:22:47 +0000

    update to 0.8.0 changes: -Added tre_ prefix to all functions exported from libtre. This changes the  ABI -Bug fixes

--- devel/tre/buildlink3.mk
+++ devel/tre/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2011/04/22 13:42:07 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2012/04/25 15:22:47 drochner Exp $
 
 BUILDLINK_TREE+=	tre
 
@@ -6,9 +6,8 @@ BUILDLINK_TREE+=	tre
 TRE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.tre+=		tre>=0.7.2
-BUILDLINK_ABI_DEPENDS.tre?=		tre>=0.7.6nb2
+BUILDLINK_ABI_DEPENDS.tre?=		tre>=0.8.0
 BUILDLINK_PKGSRCDIR.tre?=		../../devel/tre
-BUILDLINK_CPPFLAGS.tre+=		-I${BUILDLINK_PREFIX.tre}/include/tre
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .endif # TRE_BUILDLINK3_MK
