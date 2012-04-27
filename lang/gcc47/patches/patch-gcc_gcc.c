f274bc017340913240834e8554bd79c0cbded636
sbd <sbd@pkgsrc.org>
2012-04-27 04:53:02 +0000

    Improve the patch that adds the %M spec string to handle multilib_os_dir being NULL.
    When building a single ABI capable gcc (e.g. 32bit systems), multilib_os_dir
    may be NULL and this would cause gcc to segfault when trying to link libgcc.
    
    Thanks to Filip Hajny for isolating the problem to the %M patch.

--- lang/gcc47/patches/patch-gcc_gcc.c
+++ lang/gcc47/patches/patch-gcc_gcc.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-gcc_gcc.c,v 1.1.1.1 2012/04/16 04:27:18 sbd Exp $
+$NetBSD: patch-gcc_gcc.c,v 1.2 2012/04/27 04:53:03 sbd Exp $
 
 Add %M spec string.
 
@@ -6,12 +6,15 @@ Submitted to GCC: http://gcc.gnu.org/bugzilla/show_bug.cgi?id=53002
 
 --- gcc/gcc.c.orig	2012-02-28 17:31:38.000000000 +0000
 +++ gcc/gcc.c
-@@ -5115,6 +5115,10 @@ do_spec_1 (const char *spec, int inswitc
+@@ -5115,6 +5115,13 @@ do_spec_1 (const char *spec, int inswitc
  	      return value;
  	    break;
  
 +	  case 'M':
-+	    obstack_grow (&obstack, multilib_os_dir, strlen (multilib_os_dir));
++	    if (multilib_os_dir == NULL)
++	      obstack_1grow (&obstack, '.');
++	    else
++	      obstack_grow (&obstack, multilib_os_dir, strlen(multilib_os_dir));
 +	    break;
 +
  	  case 'G':
