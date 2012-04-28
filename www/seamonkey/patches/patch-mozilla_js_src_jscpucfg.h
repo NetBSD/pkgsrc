6662afb025f4935ff0c2c90ab850ecc29c178b43
ryoon <ryoon>
2012-04-28 22:48:06 +0000

    Update to 2.9
    * Remove unused option.
    * Restore jemalloc option.
    
    Changelog:
    * The File and Move Bookmarks dialogs are resizable now.
    * HTML5 videos that do not start automatically show a large play button now.
    * Add-ons Sync can now be configured without the Add-ons Sync Prefs add-on.
    * Pasting a URL from the clipboard into the Download Manager window will
      download it.
    * Plugins can be disabled for the whole suite now in addition to
      Mail & Newsgroups only.
    
    * View Source now has line numbers.
    * Line breaks are now supported in the title attribute.
    * Find in Page search results are scrolled into view now.
    * The column-fill CSS property has been implemented.
    * Support for the text-align-last CSS property has been added.
    * Experimental support for ECMAScript 6 Map and Set objects has been
      implemented.
    * Fixed several stability issues.

--- www/seamonkey/patches/patch-mozilla_js_src_jscpucfg.h
+++ www/seamonkey/patches/patch-mozilla_js_src_jscpucfg.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-mozilla_js_src_jscpucfg.h,v 1.2 2012/03/19 10:36:00 ryoon Exp $
+$NetBSD: patch-mozilla_js_src_jscpucfg.h,v 1.3 2012/04/28 22:48:06 ryoon Exp $
 
---- mozilla/js/src/jscpucfg.h.orig	2012-03-13 05:32:42.000000000 +0000
+--- mozilla/js/src/jscpucfg.h.orig	2012-04-23 06:27:43.000000000 +0000
 +++ mozilla/js/src/jscpucfg.h
 @@ -77,6 +77,21 @@
  #  define IS_BIG_ENDIAN 1
--- www/seamonkey/patches/patch-mozilla_nsprpub_pr_src_pthreads_ptsynch.c
+++ www/seamonkey/patches/patch-mozilla_nsprpub_pr_src_pthreads_ptsynch.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-mozilla_nsprpub_pr_src_pthreads_ptsynch.c,v 1.2 2012/03/19 10:36:00 ryoon Exp $
+$NetBSD: patch-mozilla_nsprpub_pr_src_pthreads_ptsynch.c,v 1.3 2012/04/28 22:48:07 ryoon Exp $
 
---- mozilla/nsprpub/pr/src/pthreads/ptsynch.c.orig	2012-03-13 05:33:05.000000000 +0000
+--- mozilla/nsprpub/pr/src/pthreads/ptsynch.c.orig	2012-04-23 06:28:14.000000000 +0000
 +++ mozilla/nsprpub/pr/src/pthreads/ptsynch.c
 @@ -842,7 +842,7 @@ PR_IMPLEMENT(PRStatus) PR_DeleteSemaphor
   */
