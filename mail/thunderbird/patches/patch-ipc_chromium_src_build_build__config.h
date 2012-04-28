85939918c3ffa7b04d8970e3bc58e7db0534460d
ryoon <ryoon>
2012-04-28 16:56:58 +0000

    Update to 12.0
    * Remove unused option.
    * Update enigmail to 1.4.1
    
    Changelog:
    * Global Search results now include message extracts in the results
    * Various security fixes
    * Various improvements to RSS feed subscription and general feed handling
    * Thunderbird now supports add-ons that provide different types of
      local mail storage

--- mail/thunderbird/patches/patch-ipc_chromium_src_build_build__config.h
+++ mail/thunderbird/patches/patch-ipc_chromium_src_build_build__config.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-ipc_chromium_src_build_build__config.h,v 1.3 2012/04/01 20:53:43 ryoon Exp $
+$NetBSD: patch-ipc_chromium_src_build_build__config.h,v 1.4 2012/04/28 16:56:58 ryoon Exp $
 
---- mozilla/ipc/chromium/src/build/build_config.h.orig	2012-03-13 02:43:11.000000000 +0000
+--- mozilla/ipc/chromium/src/build/build_config.h.orig	2012-04-20 22:40:06.000000000 +0000
 +++ mozilla/ipc/chromium/src/build/build_config.h
 @@ -19,17 +19,23 @@
  #define OS_MACOSX 1
--- mail/thunderbird/patches/patch-mozilla_ipc_chromium_src_base_process__util__posix.cc
+++ mail/thunderbird/patches/patch-mozilla_ipc_chromium_src_base_process__util__posix.cc
@@ -1,6 +1,6 @@
-$NetBSD: patch-mozilla_ipc_chromium_src_base_process__util__posix.cc,v 1.1 2012/03/10 11:42:39 ryoon Exp $
+$NetBSD: patch-mozilla_ipc_chromium_src_base_process__util__posix.cc,v 1.2 2012/04/28 16:56:59 ryoon Exp $
 
---- mozilla/ipc/chromium/src/base/process_util_posix.cc.orig	2012-02-16 10:25:01.000000000 +0000
+--- mozilla/ipc/chromium/src/base/process_util_posix.cc.orig	2012-04-20 22:40:05.000000000 +0000
 +++ mozilla/ipc/chromium/src/base/process_util_posix.cc
 @@ -116,6 +116,11 @@ void CloseSuperfluousFds(const base::Inj
  #elif defined(OS_MACOSX)
