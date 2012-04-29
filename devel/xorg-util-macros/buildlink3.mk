fb08a644c1f9d28e9d8fc8b732a2fb5326e20684
wiz <wiz@pkgsrc.org>
2012-04-29 11:30:23 +0000

    Make it a build dependency, as joerg notes.

--- devel/xorg-util-macros/buildlink3.mk
+++ devel/xorg-util-macros/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/04/29 10:40:47 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/04/29 11:30:23 wiz Exp $
 
 BUILDLINK_TREE+=	xorg-util-macros
 
@@ -8,6 +8,8 @@ XORG_UTIL_MACROS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.xorg-util-macros+=	xorg-util-macros>=1.17
 BUILDLINK_ABI_DEPENDS.xorg-util-macros+=	xorg-util-macros>=1.17
 BUILDLINK_PKGSRCDIR.xorg-util-macros?=	../../devel/xorg-util-macros
+
+BUILDLINK_DEPMETHOD.xorg-util-macros?=	build
 .endif # XORG_UTIL_MACROS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xorg-util-macros
