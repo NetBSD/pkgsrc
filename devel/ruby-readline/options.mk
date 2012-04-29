8d0b7b6151bcac22f0e4decd7e7033d47abe72a7
taca <taca@pkgsrc.org>
2012-04-29 16:43:49 +0000

    Don't enable "readline" option default if RUBY_VER is prior to 193.
    Ruby 1.9.3 changes its license and solve the problem with GPL3 of readline
    license.  But Ruby 1.9.2/1.8.7 dosen't solve this problem.
    
    If you want to use ruby-readline with GNU readline on ruby18 or ruby192,
    please explictly set "readline" PKG_OPTION.
    
    Fix PR pkg/41943.
    
    Bump PKGREVISION.

--- devel/ruby-readline/options.mk
+++ devel/ruby-readline/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2009/09/13 01:51:05 taca Exp $
+# $NetBSD: options.mk,v 1.2 2012/04/29 16:43:49 taca Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ruby
@@ -7,9 +7,16 @@ PKG_SUPPORTED_OPTIONS=	readline ruby-build-ri-db
 #
 # GNU readline supports multi bytes characters.
 #
-PKG_SUGGESTED_OPTIONS=	readline ruby-build-ri-db
+PKG_SUGGESTED_OPTIONS=	ruby-build-ri-db
 
-.include "../../mk/bsd.options.mk"
+.include "../../lang/ruby/rubyversion.mk"
+
+# Since license of Ruby prior to 1.9.3 is incompatible with newer
+# readline's GPL3, enable it with 1.9.3 and lator.
+# 
+.if ${RUBY_VER} != 18 && ${RUBY_VER} != 19
+PKG_SUGGESTED_OPTIONS+=	readline
+.endif
 
 .if !empty(PKG_OPTIONS:Mreadline)
 USE_BUILTIN.readline=	no
