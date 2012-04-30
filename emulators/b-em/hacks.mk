efc92d5d96a01381d2b63b1be1b9332dac02147e
dholland <dholland@pkgsrc.org>
2012-04-30 21:54:59 +0000

    Update hack for new automake version.

--- emulators/b-em/hacks.mk
+++ emulators/b-em/hacks.mk
@@ -1,15 +1,15 @@
-# $NetBSD: hacks.mk,v 1.2 2012/03/26 03:30:45 dholland Exp $
+# $NetBSD: hacks.mk,v 1.3 2012/04/30 21:54:59 dholland Exp $
 
 # This package's distfile has the wonderful idea of containing
-# symlinks to various things in /usr/share/automake-1.11/. What
-# will they think of next?
+# symlinks into /usr/share/automake-1.11/. What will they think of
+# next?
 
-BUILD_DEPENDS+=		automake>=1.11<1.12:../../devel/automake
+BUILD_DEPENDS+=		automake>=1.12<1.13:../../devel/automake
 
 post-extract:
 	@echo 'Fixing symlinks to external files'
 .for FILE in compile COPYING depcomp INSTALL
 	[ -h ${WRKSRC}/${FILE} ] || exit 1
 	rm -f ${WRKSRC}/${FILE}
-	cp ${PREFIX}/share/automake-1.11/${FILE} ${WRKSRC}/${FILE}
+	cp ${PREFIX}/share/automake-1.12/${FILE} ${WRKSRC}/${FILE}
 .endfor
