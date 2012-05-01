8ec713ca6a21383ece209e1fe9243b40c2753fa2
adam <adam@pkgsrc.org>
2012-05-01 18:45:51 +0000

    Changes 2.32.2: * GApplication: can now have a NULL application ID * g_clear_object: fix warnings when using it on C++ (due to lack of   ability to implicitly cast void*) * GDBus:  - add our own implementation of the message bus for use on Windows only  - fix up a few bugs that use of this bus uncovered in GDBus  - escape nonce files in dbus addressess (think 'c:\')  - support initial underscores in dbus codegen namespace (for private) * Fix misdetection of GNUstep as Cocoa (for the MacOS GSettings backend) * make sure configure fails if AC_CHECK_ALIGNOF cannot detect the alignment * GAppInfo: overwrite the DISPLAY only if it is set in the launch context * glib/tests/date: force US locale running the GDateTime tests * GSocketControlMessage: Don't warn about unknown messages * Resources:  - fix broken use of GVDB on big endian machines  - set a 'display name' so that pretty file names appear in Gtk CSS    warning messages * GMainContext:  - block child sources when blocking the parent  - introduce more testcases for child sources * Translations updates:  Brazilian Portuguese  French  Galician  Italian  Lithuanian  Polish  Polish  Serbian  Simplified Chinese  Spanish * Bug fixed:  619026 avoid warning in gutils.h when using gcc with -Wconversion  669260 Open/Save dialog hangs waiting for data with libsoup  671249 GApplication: Allow a null application_id?  672786 goa-daemon: action in notification doesn't work  673409 g_resource_lookup_data may return stale data pointer  674172 glib-2.32.1 misdetects GNUstep as Cocoa  674345 cssprovider: Make sure to print out file name in css warnings  674483 broken configure results when cross-compiling with gcc >= 4.5

--- devel/glib2/hacks.mk
+++ devel/glib2/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.5 2011/01/04 09:55:15 adam Exp $
+# $NetBSD: hacks.mk,v 1.6 2012/05/01 18:45:52 adam Exp $
 
 .if !defined(GLIB2_HACKS_MK)
 GLIB2_HACKS_MK=	# defined
@@ -6,25 +6,25 @@ GLIB2_HACKS_MK=	# defined
 # Build without optimization. Avoids crash in libgobject on gtk2
 # initialization. Bug is triggered regardless of whether we're using gcc or
 # aCC.
-.  if !empty(MACHINE_PLATFORM:MHPUX-*-hppa)
+.if !empty(MACHINE_PLATFORM:MHPUX-*-hppa)
 PKG_HACKS+=	hppa-codegen
 CFLAGS:=	-O0 ${CFLAGS:C/[+,-]O[0-9]?//g}
-.  endif
+.endif
 
-.  if ${OPSYS} == "Darwin"
+.if ${OPSYS} == "Darwin"
 PKG_HACKS+=		darwin-iconv
 SUBST_CLASSES+=		iconv
 SUBST_STAGE.iconv=	pre-configure
 SUBST_MESSAGE.iconv=	Changing libiconv_open to iconv_open.
 SUBST_FILES.iconv=	configure
 SUBST_SED.iconv=	-e 's,libiconv_open,iconv_open,g'
-.  endif
+.endif
 
 # Work around unresolved symbol g_test_config_vars during build
 .include "../../mk/compiler.mk"
-.  if !empty(PKGSRC_COMPILER:Msunpro)
+.if !empty(PKGSRC_COMPILER:Msunpro)
 PKG_HACKS+=		sunpro-visibility
 CONFIGURE_ARGS+=	--disable-visibility
-.  endif
+.endif
 
 .endif
