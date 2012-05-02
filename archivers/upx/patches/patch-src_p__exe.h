bf2bcf95df80274cc7ba0068488359ed2a580086
adam <adam@pkgsrc.org>
2012-05-02 06:45:14 +0000

    Changes 3.08: * Fix allocation in runtime stub for darwin.macho-entry (i386 and amd64). * Compress shared library on ELF i386 only [ld.so threatens even this case]. * Attempt to suport ELF on QNX 6.3.0 for armel (experimental). * Better diangostic when ELF -fPIC is needed. * PT_NOTE improvements for *BSD. * Preserve more ELF .e_flags on ARM. * Minor code improvements for ELF stubs * Defend against another flavor of corrupt PE header. * bug fixes

--- archivers/upx/patches/patch-src_p__exe.h
+++ archivers/upx/patches/patch-src_p__exe.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_p__exe.h,v 1.1 2012/02/15 19:27:10 hans Exp $
+$NetBSD: patch-src_p__exe.h,v 1.2 2012/05/02 06:45:14 adam Exp $
 
 --- src/p_exe.h.orig	2002-06-01 13:20:17.000000000 +0200
 +++ src/p_exe.h	2012-01-11 15:33:21.472848400 +0100
-@@ -96,7 +96,7 @@ protected:
+@@ -105,7 +105,7 @@ protected:
      enum {
          NORELOC = 1,
          USEJUMP = 2,
