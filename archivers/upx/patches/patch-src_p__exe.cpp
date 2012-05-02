b4421c7a7508006c9e2c607d35c8fc61254e215b
adam <adam@pkgsrc.org>
2012-05-02 06:45:14 +0000

    Changes 3.08: * Fix allocation in runtime stub for darwin.macho-entry (i386 and amd64). * Compress shared library on ELF i386 only [ld.so threatens even this case]. * Attempt to suport ELF on QNX 6.3.0 for armel (experimental). * Better diangostic when ELF -fPIC is needed. * PT_NOTE improvements for *BSD. * Preserve more ELF .e_flags on ARM. * Minor code improvements for ELF stubs * Defend against another flavor of corrupt PE header. * bug fixes

--- archivers/upx/patches/patch-src_p__exe.cpp
+++ archivers/upx/patches/patch-src_p__exe.cpp
@@ -1,35 +1,35 @@
-$NetBSD: patch-src_p__exe.cpp,v 1.1 2012/02/15 19:27:10 hans Exp $
+$NetBSD: patch-src_p__exe.cpp,v 1.2 2012/05/02 06:45:14 adam Exp $
 
---- src/p_exe.cpp.orig	2004-03-25 06:36:19.000000000 +0100
-+++ src/p_exe.cpp	2012-01-11 15:35:13.755302407 +0100
-@@ -347,7 +347,7 @@ void PackExe::pack(OutputFile *fo)
-     {
-         set_le16(extra_info+eisize,ih.ss);
-         eisize += 2;
+--- src/p_exe.cpp.orig	2011-12-12 16:08:00.000000000 +0000
++++ src/p_exe.cpp
+@@ -110,7 +110,7 @@ int PackExe::fillExeHeader(struct exe_he
+         oh.ss += 0x20;
+ 
+     if (oh.ss != ih.ss)
 -        flag |= SS;
 +        flag |= upx_SS;
-     }
-     if (oh.sp != ih.sp)
-     {
-@@ -413,7 +413,7 @@ void PackExe::pack(OutputFile *fo)
-                   NULL
+     if (oh.sp != ih.sp || M_IS_LZMA(ph.method))
+         flag |= SP;
+     return flag;
+@@ -131,7 +131,7 @@ void PackExe::addLoaderEpilogue(int flag
                   );
      addLoader("EXEMAIN8",
+               device_driver ? "DEVICEEND" : "",
 -              (flag & SS) ? "EXESTACK" : "",
 +              (flag & upx_SS) ? "EXESTACK" : "",
                (flag & SP) ? "EXESTASP" : "",
                (flag & USEJUMP) ? "EXEJUMPF" : "",
                NULL
-@@ -480,7 +480,7 @@ void PackExe::pack(OutputFile *fo)
+@@ -556,7 +556,7 @@ void PackExe::pack(OutputFile *fo)
+     {
+         set_le16(extra_info+eisize,ih.ss);
+         eisize += 2;
+-        assert((flag & SS) != 0);   // set in fillExeHeader()
++        assert((flag & upx_SS) != 0);   // set in fillExeHeader()
      }
-     if (flag & SP)
-         patch_le16(loader,lsize,"SP",ih.sp);
--    if (flag & SS)
-+    if (flag & upx_SS)
-         patch_le16(loader,lsize,"SS",ih.ss);
-     if (relocsize)
-         patch_le16(loader,lsize,"RS",(ph.u_len <= DI_LIMIT || (ph.u_len & 0x7fff) >= relocsize ? 0 : MAXRELOCS) - relocsize);
-@@ -654,7 +654,7 @@ void PackExe::unpack(OutputFile *fo)
+     if (oh.sp != ih.sp)
+     {
+@@ -779,7 +779,7 @@ void PackExe::unpack(OutputFile *fo)
          { imagesize -= 2; oh.min = get_le16(ibuf+imagesize); }
      if (flag & SP)
          { imagesize -= 2; oh.sp = get_le16(ibuf+imagesize); }
