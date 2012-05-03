8e9538356dc15e056727fca7d15aee9de281a30b
obache <obache>
2012-05-03 11:52:33 +0000

    Reduce mimimal requirement of libdrm from 2.4.20 to 2.4.15, allow OpenBSD-5.1 builtin one, or failed to buildlink with MesaLib, because buitin MesaLib is allowed but depending libdrm not.
    * Previous pkgsrc version is 2.4.13
    * NetBSD-5.1's one is 2.4.11
    * NetBSD-6.0_BETA's one is 2.4.23
    
    So actual dependency should not be changed.

--- x11/libdrm/buildlink3.mk
+++ x11/libdrm/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2010/04/16 13:43:36 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2012/05/03 11:52:33 obache Exp $
 
 BUILDLINK_TREE+=	libdrm
 
 .if !defined(LIBDRM_BUILDLINK3_MK)
 LIBDRM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libdrm+=	libdrm>=2.4.20
+BUILDLINK_API_DEPENDS.libdrm+=	libdrm>=2.4.15
 BUILDLINK_PKGSRCDIR.libdrm?=	../../x11/libdrm
 .endif # LIBDRM_BUILDLINK3_MK
 
