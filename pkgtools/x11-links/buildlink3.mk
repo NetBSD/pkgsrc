f8fdbcdaae42c6311db6f25276d2984e2284deed
obache <obache>
2012-05-03 13:55:45 +0000

    Add files for OpenBSD-5.1.
    Bump PKGVERSION.

--- pkgtools/x11-links/buildlink3.mk
+++ pkgtools/x11-links/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.42 2012/04/08 05:55:48 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.43 2012/05/03 13:55:45 obache Exp $
 #
 # Don't include this file manually!  It will be included as necessary
 # by bsd.buildlink3.mk.
@@ -12,7 +12,7 @@ BUILDLINK_TREE+=	x11-links
 .if !defined(X11_LINKS_BUILDLINK3_MK)
 X11_LINKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.69
+BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.70
 BUILDLINK_PKGSRCDIR.x11-links?=	../../pkgtools/x11-links
 BUILDLINK_DEPMETHOD.x11-links?=	build
 
