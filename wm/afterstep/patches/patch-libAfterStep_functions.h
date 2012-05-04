483518e2db461d343d32bda0eaa7cee6919f15a2
joerg <joerg@pkgsrc.org>
2012-05-04 16:36:10 +0000

    Fix inline usage.

---
+++ wm/afterstep/patches/patch-libAfterStep_functions.h
@@ -0,0 +1,13 @@
+$NetBSD: patch-libAfterStep_functions.h,v 1.1 2012/05/04 16:36:10 joerg Exp $
+
+--- libAfterStep/functions.h.orig	2012-05-04 14:34:21.000000000 +0000
++++ libAfterStep/functions.h
+@@ -250,7 +250,7 @@ FunctionData *String2Func ( const char *
+ void init_func_data (FunctionData * data);
+ void copy_func_data (FunctionData * dst, FunctionData * src);
+ void dup_func_data (FunctionData * dst, FunctionData * src);
+-inline FunctionData *create_named_function( int func, char *name);
++FunctionData *create_named_function( int func, char *name);
+ void set_func_val (FunctionData * data, int arg, int value);
+ int free_func_data (FunctionData * data);
+ void destroy_func_data( FunctionData **pdata );
---
+++ wm/afterstep/patches/patch-libAfterStep_mylook.c
@@ -0,0 +1,22 @@
+$NetBSD: patch-libAfterStep_mylook.c,v 1.1 2012/05/04 16:36:11 joerg Exp $
+
+--- libAfterStep/mylook.c.orig	2012-05-04 14:35:51.000000000 +0000
++++ libAfterStep/mylook.c
+@@ -813,7 +813,7 @@ mylook_get_style(MyLook *look, const cha
+     return NULL;
+ }
+ 
+-inline MyDesktopConfig *
++MyDesktopConfig *
+ mylook_get_desk_config(MyLook *look, long desk)
+ {
+     if( look )
+@@ -827,7 +827,7 @@ mylook_get_desk_config(MyLook *look, lon
+ }
+ 
+ 
+-inline MyBackground *
++MyBackground *
+ mylook_get_desk_back(MyLook *look, long desk)
+ {
+ 	MyBackground *myback = NULL ;
