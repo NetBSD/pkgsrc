8a1ac76085309d95290c61a27b9ef893a5be671e
ryoon <ryoon@pkgsrc.org>
2012-05-04 00:08:07 +0000

    Add MASTER_SITE_MOZILLA_ESR, for Extended Support Release version of mozilla.org products (firefox and thunderbird). Suggested by obache on pkgsrc-changes@.

--- devel/xulrunner10/dist.mk
+++ devel/xulrunner10/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.3 2012/04/29 16:30:08 ryoon Exp $
+# $NetBSD: dist.mk,v 1.4 2012/05/04 00:08:07 ryoon Exp $
 #
 # used by devel/xulrunner10/Makefile
 # used by www/firefox10/Makefile
@@ -7,8 +7,7 @@ DISTNAME=	firefox-${FIREFOX_VER}esr.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	10.0
 MOZ_BRANCH_MINOR=	.4
-MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}esr/source/} \
-		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}esr/source/}
+MASTER_SITES=	${MASTER_SITE_MOZILLA_ESR:=firefox/releases/${FIREFOX_VER}esr/source/}
 EXTRACT_SUFX=	.tar.bz2
 
 DISTINFO_FILE=	${.CURDIR}/../../devel/xulrunner10/distinfo
