cb7406e850aa4339e39dfd52aa4dd65d6b89c3eb
jakllsch <jakllsch>
2012-05-05 19:18:33 +0000

    update darktable to 1.0.3
    Changes since 1.0:
    
    Usability improvements:
    - Filmstrip centers on selected image
    
    Behavioral changes:
    - Improved (hierarchical) tag export for flickr and friends
    
    Camera support:
    - Improved Sony NEX-7 support
    - Initial camera support for Nikon D800 and Sony SLT-A57 (color rendition
      of these cameras might still be suboptimal due to the lack of a proper
      color matrix, and is subject to future change.)
    - White balance updates for Canon EOS Rebel T3, Olympus E-5 & Nikon D800
    
    Platform support:
    - Fixes for FreeBSD
    
    Various:
    - New subtle denoise preset for equalizer
    - Various build fixes
    - Numerous other fixes

--- graphics/darktable/patches/patch-src_control_jobs_control__jobs.c
+++ graphics/darktable/patches/patch-src_control_jobs_control__jobs.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_control_jobs_control__jobs.c,v 1.2 2012/03/16 00:35:01 jakllsch Exp $
+$NetBSD: patch-src_control_jobs_control__jobs.c,v 1.3 2012/05/05 19:18:33 jakllsch Exp $
 
---- src/control/jobs/control_jobs.c.orig	2012-03-13 21:29:39.000000000 +0000
+--- src/control/jobs/control_jobs.c.orig	2012-04-27 20:25:52.000000000 +0000
 +++ src/control/jobs/control_jobs.c
-@@ -546,7 +546,7 @@ int32_t dt_control_export_job_run(dt_job
+@@ -534,7 +534,7 @@ int32_t dt_control_export_job_run(dt_job
    // GCC won't accept that this variable is used in a macro, considers
    // it set but not used, which makes for instance Fedora break.
    const __attribute__((__unused__)) int num_threads = MAX(1, MIN(full_entries, 8));
