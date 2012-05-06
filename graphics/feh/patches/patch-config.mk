4d30a4d64387ef0c2ba30906120c271f0e91b41a
tron <tron@pkgsrc.org>
2012-05-06 00:16:05 +0000

    Pullup ticket #3770 - requested by gls graphics/feh: build fix
    Revisions pulled up:
    - graphics/feh/Makefile                                         1.48
    - graphics/feh/distinfo                                         1.23
    - graphics/feh/patches/patch-config.mk                          1.2
    
    ---
       Module Name:	pkgsrc
       Committed By:	gls
       Date:		Tue May  1 14:58:47 UTC 2012
    
       Modified Files:
       	pkgsrc/graphics/feh: Makefile distinfo
       	pkgsrc/graphics/feh/patches: patch-config.mk
    
       Log Message:
       Update graphics/feh to 2.5.
    
       pkgsrc changes
       --------------
    
       Don't mess with CFLAGS. I can't remember why that was even necessary, and it
       breaks the Linux build.
    
       Should fix PR pkg/46390.
    
       upstream changes
       ----------------
    
       * Add R, * and / bindings for keypad begin, keypad * and keypad /
       * Try to convert unloadable files with imagemagick for up to 5 seconds
       * Add --magick-timeout option to set imagemagick conversion timeout or disable
       it altogether
       * Clean up temporary / to-delete files when receiving SIG{INT,TERM,QUIT}
       * Do not scroll past image borders when using key bindings
       * --loadable / --unloadable: indicate result in exit status

--- graphics/feh/patches/patch-config.mk
+++ graphics/feh/patches/patch-config.mk
@@ -1,9 +1,11 @@
-$NetBSD: patch-config.mk,v 1.1 2011/05/25 19:44:27 gls Exp $
+$NetBSD: patch-config.mk,v 1.1.8.1 2012/05/06 00:16:05 tron Exp $
 
---- config.mk.orig      2011-05-19 20:36:13.000000000 +0000
+Fix man pages installation path
+
+--- config.mk.orig	2012-03-25 11:16:59.000000000 +0000
 +++ config.mk
 @@ -6,7 +6,7 @@ PREFIX ?= /usr/local
-
+ 
  # Directories for manuals, executables, docs, data, etc.
  main_dir = ${DESTDIR}${PREFIX}
 -man_dir = ${main_dir}/share/man
