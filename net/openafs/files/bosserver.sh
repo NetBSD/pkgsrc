5f95f5aacaac3b8a01080a7ea5dd03762cc52300
christos <christos@pkgsrc.org>
2012-05-06 20:37:47 +0000

    we are not arla

--- net/openafs/files/bosserver.sh
+++ net/openafs/files/bosserver.sh
@@ -1,16 +1,16 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: bosserver.sh,v 1.2 2007/10/27 09:11:10 gendalia Exp $
+# $NetBSD: bosserver.sh,v 1.3 2012/05/06 20:37:47 christos Exp $
 #
 # PROVIDE: bosserver
-# BEFORE: arlad
+# BEFORE: afsd
 # REQUIRE: NETWORK
 
 . /etc/rc.subr
 
 name="bosserver"
 rcvar=$name
-hostname=`@HOSTNAME_CMD@`
+hostname=$(@HOSTNAME_CMD@)
 command="@PREFIX@/sbin/$name"
 start_precmd="ulimit -S -d 1048576;ulimit -S -c unlimited"
 stop_precmd="@PREFIX@/bin/bos shutdown $hostname -local -wait"
