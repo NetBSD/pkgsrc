98c2a6be790c2e93612d5e5de68595252297a95a
dholland <dholland@pkgsrc.org>
2012-05-06 20:08:32 +0000

    Set both BUILDLINK_ABI_DEPENDS and BUILDLINK_API_DEPENDS correctly (with +=, not ?=) caught by pkglint

--- converters/wv2/buildlink3.mk
+++ converters/wv2/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.17 2012/03/03 00:12:28 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2012/05/06 20:08:32 dholland Exp $
 
 BUILDLINK_TREE+=	wv2
 
 .if !defined(WV2_BUILDLINK3_MK)
 WV2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.wv2?=		wv2>=0.2.2nb1
-BUILDLINK_ABI_DEPENDS.wv2?=	wv2>=0.2.3nb8
+BUILDLINK_API_DEPENDS.wv2+=	wv2>=0.2.2nb1
+BUILDLINK_ABI_DEPENDS.wv2+=	wv2>=0.2.3nb8
 BUILDLINK_PKGSRCDIR.wv2?=	../../converters/wv2
 
 .include "../../converters/libiconv/buildlink3.mk"
