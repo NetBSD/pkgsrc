803d050a6db65ffa53897661768e783b15f85abd
obache <obache@pkgsrc.org>
2012-05-06 09:08:36 +0000

    Update libVNCServer to 0.9.9.
    0.9.9
       - Overall changes:
         * Added noVNC HTML5 VNC viewer (http://kanaka.github.com/noVNC/) connect possibility
           to our http server. Pure JavaScript, no Java plugin required anymore! (But a
           recent browser...)
         * Added a GTK+ VNC viewer example.
    
       - LibVNCServer/LibVNCClient:
         * Added support to build for Google Android.
         * Complete IPv6 support in both LibVNCServer and LibVNCClient.
    
       - LibVNCServer:
         * Split two event-loop related functions out of the rfbProcessEvents() mechanism.
           This is required to be able to do proper event loop integration with Qt. Idea was
           taken from Vino's libvncserver fork.
         * Added TightPNG (http://wiki.qemu.org/VNC_Tight_PNG) encoding support. Like the
           original Tight encoding, this still uses JPEG, but ZLIB encoded rects are encoded
           with PNG here.
         * Added suport for serving VNC sessions through WebSockets
           (http://en.wikipedia.org/wiki/WebSocket), a web technology providing for multiplexing
           bi-directional, full-duplex communications channels over a single TCP connection.
         * Support connections from the Mac OS X built-in VNC client to LibVNCServer
           instances running with no password.
         * Replaced the Tight encoder with a TurboVNC one which is tremendously faster in most
           cases, especially with high-color video or 3D workloads.
           (http://www.virtualgl.org/pmwiki/uploads/About/tighttoturbo.pdf)
    
       - LibVNCClient:
         * Added support to only listen for reverse connections on a specific IP address.
         * Support for using OpenSSL instead of GnuTLS. This could come in handy on embedded
           devices where only this TLS implementation is available.
         * Added support to connect to UltraVNC Single Click servers.

---
+++ net/libvncserver/patches/patch-libvncserver_websockets.c
@@ -0,0 +1,50 @@
+$NetBSD: patch-libvncserver_websockets.c,v 1.1 2012/05/06 09:08:36 obache Exp $
+
+* byte-order handling for !Linux.
+
+--- libvncserver/websockets.c.orig	2012-05-04 14:19:00.000000000 +0000
++++ libvncserver/websockets.c
+@@ -31,12 +31,34 @@
+ /* errno */
+ #include <errno.h>
+ 
+-#include <byteswap.h>
+ #include <string.h>
+ #include "rfbconfig.h"
+ #include "rfbssl.h"
+ #include "rfbcrypto.h"
+ 
++#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
++#include <sys/endian.h>
++#define WS_NTOH64(n) be64toh(n)
++#define WS_NTOH32(n) be32toh(n)
++#define WS_NTOH16(n) be16toh(n)
++#define WS_HTON64(n) htobe64(n)
++#define WS_HTON16(n) htobe16(n)
++#elif defined(__OpenBSD__)
++#include <sys/types.h>
++#define WS_NTOH64(n) betoh64(n)
++#define WS_NTOH32(n) betoh32(n)
++#define WS_NTOH16(n) betoh16(n)
++#define WS_HTON64(n) htobe64(n)
++#define WS_HTON16(n) htobe16(n)
++#elif defined(__APPLE__)
++#include <libkern/OSByteOrder.h>
++#define WS_NTOH64(n) OSSwapBigToHostInt64(n)
++#define WS_NTOH32(n) OSSwapBigToHostInt32(n)
++#define WS_NTOH16(n) OSSwapBigToHostInt16(n)
++#define WS_HTON64(n) OSSwapHostToBigInt64(n)
++#define WS_HTON16(n) OSSwapHostToBitInt16(n)
++#else
++#include <byteswap.h>
+ #if defined(__BYTE_ORDER) && defined(__BIG_ENDIAN) && __BYTE_ORDER == __BIG_ENDIAN
+ #define WS_NTOH64(n) (n)
+ #define WS_NTOH32(n) (n)
+@@ -50,6 +72,7 @@
+ #define WS_HTON64(n) bswap_64(n)
+ #define WS_HTON16(n) bswap_16(n)
+ #endif
++#endif
+ 
+ #define B64LEN(__x) (((__x + 2) / 3) * 12 / 3)
+ #define WSHLENMAX 14  /* 2 + sizeof(uint64_t) + sizeof(uint32_t) */
