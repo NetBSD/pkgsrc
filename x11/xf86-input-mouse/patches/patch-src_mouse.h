33d54b3ec3d1a319c5cfa34d1b5d2bd4b59d8315
wiz <wiz@pkgsrc.org>
2012-05-06 20:51:24 +0000

    Update to 1.7.2, provided by Francois Tigeot in private mail.
    1.7.2
    
    Return proper default for unknown values in pInfo->device_control
    No need to merge NULL options list with existing options
    sun_mouse.c: Ensure vuidMouse setup routines are called for protocol "Auto"
    Fix wrong read Protocol and Device from xorg.conf
    Avoid NULL pointer dereference in autoProbeMouse if proto == PROT_UNSUP
    Mark symtab_t.name in pnp.c as const to fix gcc -Wwrite-strings warnings
    sun_mouse.c: remove unused variables
    
    1.7.1:
    
    Fix OSMouse OS-defined protocol support
    Fix Hurd mouse driver with XInput ABI before 12
    Fix Solaris issues with new ABI12 init process
    
    1.7.0:
    
    Fix BSD issues with new ABI12 init process
    
    1.6.99.901:
    
    Replace libc wrappers to xcalloc and friends with libc calls
    Remove unused declaration of MouseUnInit
    Remove out-of-date comment
    XI86_POINTER_CAPABLE is gone from the server
    Replace LocalDevicePtr with InputInfoPtr
    Don't try to call conversion_proc anymore
    Remove refcnt field from InputDriverRec
    Assume ABI_XINPUT_VERSION 4 or higher
    Remove unused define NEED_X86_TYPES
    Remove includes for protocol headers
    Remove if 0'd out model
    Remove a bunch of unnecessary log prints
    Remove unused branch from if 1 condition
    De-duplicate Option "Device" handling
    Use single exit path for PreInit
    Move protocol ID detection into a separate function
    Remove convoluted do { } while loop
    Move a statement down to have all allocations close together
    Remove a bunch of unused fields from the MouseRec
    Rename xf86OSmouse.h to mouse.h
    Remove random History comment
    Rename xf86OSMouseInit to OSMouseInit
    Remove write-only field mouseFlags
    Use pInfo->options instead of conf-idev
    ABI 12 requires per-valuator modes
    Adjust to new Input ABI 12
    Add ABI checks around XI86_CONFIGURED uses in OS-specific PreInits
    Various manpage fixes
    
    1.6.0:
    
    Several driver modules do not have a ChangeLog target in Makefile.am #23814
    INSTALL, NEWS, README or AUTHORS files are missing/incorrect #24206
    Makefile.am: add ChangeLog and INSTALL on MAINTAINERCLEANFILES
    Plug minor memory leak
    Silence sed substitutions in make output
    configure.ac: remove unused sdkdir=$(pkg-config...) statement
    Update Sun license notices to current X.Org standard form
    config: remove unused -I$(top_srcdir)/src directive
    Solaris: Handle ENODEV & other errors from VUID protocol mice
    Bug 10866 - serial Genius NetScroll+ autodetection
    README: fix linuxdoc content
    README: update text content from sgml content
    Remove unused SCO driver files
    Various config fixes
    config: Upgrade X.Org macros to 1.4 for INSTALL file copying
    config: update AC_PREREQ statement to 2.60
    README: keep the text version of README, discard the sgml version
    config: upgrade to util-macros 1.8 for additional man page support
    Purge macro NEED_EVENTS
    xf86-input-mouse: Update sun_mouse devPrivates code #29049
    Do not return the address of a local buffer
    Check for NULL currentMode in vuidMouseSendScreenSize()
    Sun's copyrights belong to Oracle now
    MouseCtrl doesn't need to save values we never use
    Fix mouse data buffering
    Fix build warning
    Bring README a little closer to the current state of reality
    
    1.5.0:
    
    Fix crashes due to MouseBlockHandler/MouseWakeupHandler with invalid devices
    Require macros 1.3 for XORG_DEFAULT_OPTIONS
    
    1.4.99.1:
    
    Require xorg-server 1.5.99.901 or higher
    Remove xf86-misc extension related code
    Remove ModuleInfoRec and associated bits
    Protect against ABI_XINPUT_VERSION 7
    Conditionally declare some variables that are only used conditionally
    Allow 0 as wheel emulation button for unconditional scrolling (#20529)
    Restrict wheel emulation to a single axis at a time
    sun_mouse: Check if streams module is already on the stack before pushing it
    Remove check for OS/2
    sun_mouse: Don't try calling ioctls if mouse fd wasn't opened
    signed/unsigned fixes for delta x,y movement

--- x11/xf86-input-mouse/patches/patch-src_mouse.h
+++ x11/xf86-input-mouse/patches/patch-src_mouse.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-ad,v 1.1 2009/05/22 19:05:57 tnn Exp $
+$NetBSD: patch-src_mouse.h,v 1.1 2012/05/06 20:51:24 wiz Exp $
 
 Changes from NetBSD xsrc/external/mit/xorg-server/dist/\
 hw/xfree86/os-support/xf86OSmouse.h
@@ -10,9 +10,9 @@ add a SetupMouse proc to condition the fd. Convert NetBSD's SetupAuto to
 SetupMouse.
 ----------------------------
 
---- ./src/xf86OSmouse.h.orig	2009-05-19 15:15:31 +0300
-+++ ./src/xf86OSmouse.h	2009-05-19 15:18:54 +0300
-@@ -82,6 +82,7 @@ typedef Bool (*BuiltinPreInitProc)(Input
+--- src/mouse.h.orig	2012-05-05 07:22:19.910022000 +0000
++++ src/mouse.h
+@@ -91,6 +91,7 @@ typedef Bool (*BuiltinPreInitProc)(Input
  				   int flags);
  typedef const char *(*DefaultProtocolProc)(void);
  typedef const char *(*SetupAutoProc)(InputInfoPtr pInfo, int *protoPara);
@@ -20,7 +20,7 @@ SetupMouse.
  typedef void (*SetResProc)(InputInfoPtr pInfo, const char* protocol, int rate,
  			   int res);
  typedef const char *(*FindDeviceProc)(InputInfoPtr pInfo, const char *protocol,
-@@ -99,6 +100,7 @@ typedef struct {
+@@ -108,6 +109,7 @@ typedef struct {
  	BuiltinPreInitProc	PreInit;
  	DefaultProtocolProc	DefaultProtocol;
  	SetupAutoProc		SetupAuto;
@@ -28,7 +28,7 @@ SetupMouse.
  	SetResProc		SetPS2Res;
  	SetResProc		SetBMRes;
  	SetResProc		SetMiscRes;
-@@ -141,6 +143,8 @@ typedef struct {
+@@ -150,6 +152,8 @@ typedef struct {
   *
   * SetBMRes:	Set the resolution and sample rate for MSE_BM protocol types.
   *
