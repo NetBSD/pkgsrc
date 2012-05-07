aa3bb1d086903815f900b0797896e9f014cb0459
dholland <dholland@pkgsrc.org>
2012-05-07 02:45:47 +0000

    Set BUILDLINK_API_DEPENDS correctly (with +=, not ?=)

--- graphics/edje/buildlink3.mk
+++ graphics/edje/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/12/05 17:35:03 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/05/07 02:45:47 dholland Exp $
 
 BUILDLINK_TREE+=	edje
 
 .if !defined(EDJE_BUILDLINK3_MK)
 EDJE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.edje?=	edje>=1.1.0
+BUILDLINK_API_DEPENDS.edje+=	edje>=1.1.0
 BUILDLINK_PKGSRCDIR.edje?=	../../graphics/edje
 
 .include	"../../devel/ecore/buildlink3.mk"
