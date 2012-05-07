51a96009719ee996175bd06bf024ff90cf52034b
adam <adam@pkgsrc.org>
2012-05-07 08:33:26 +0000

    Changes 0.1.10: * LUT based speedups for gamma correction / uncorrection.

--- graphics/babl/buildlink3.mk
+++ graphics/babl/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:36 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/05/07 08:33:26 adam Exp $
 
 BUILDLINK_TREE+=	babl
 
 .if !defined(BABL_BUILDLINK3_MK)
 BABL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.babl+=	babl>=0.0.22
+BUILDLINK_API_DEPENDS.babl+=	babl>=0.1.10
 BUILDLINK_PKGSRCDIR.babl?=	../../graphics/babl
 
 .include "../../mk/dlopen.buildlink3.mk"
