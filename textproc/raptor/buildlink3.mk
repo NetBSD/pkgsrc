e583d3dbc2c1310627548f0aea309816f3085914
dholland <dholland@pkgsrc.org>
2012-05-07 02:45:47 +0000

    Set BUILDLINK_API_DEPENDS correctly (with +=, not ?=)

--- textproc/raptor/buildlink3.mk
+++ textproc/raptor/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.14 2011/04/22 13:42:09 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2012/05/07 02:45:47 dholland Exp $
 
 BUILDLINK_TREE+=	raptor
 
 .if !defined(RAPTOR_BUILDLINK3_MK)
 RAPTOR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.raptor?=	raptor>=1.0.0
+BUILDLINK_API_DEPENDS.raptor+=	raptor>=1.0.0
 BUILDLINK_ABI_DEPENDS.raptor+=	raptor>=1.4.21nb1
 BUILDLINK_PKGSRCDIR.raptor?=	../../textproc/raptor
 
--- x11/xcb-proto/buildlink3.mk
+++ x11/xcb-proto/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/04/06 10:22:12 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/05/07 02:45:47 dholland Exp $
 
 BUILDLINK_TREE+=	xcb-proto
 
 .if !defined(XCB_PROTO_BUILDLINK3_MK)
 XCB_PROTO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xcb-proto?=	xcb-proto>=1.4
+BUILDLINK_API_DEPENDS.xcb-proto+=	xcb-proto>=1.4
 BUILDLINK_PKGSRCDIR.xcb-proto?=	../../x11/xcb-proto
 BUILDLINK_DEPMETHOD.xcb-proto?=	build
 .endif # XCB_PROTO_BUILDLINK3_MK
