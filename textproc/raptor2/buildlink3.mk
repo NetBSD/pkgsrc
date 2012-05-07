fa62edb539f009304926375a41df8037bb0adb41
dholland <dholland@pkgsrc.org>
2012-05-07 02:45:47 +0000

    Set BUILDLINK_API_DEPENDS correctly (with +=, not ?=)

--- textproc/raptor2/buildlink3.mk
+++ textproc/raptor2/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2012/04/15 15:43:43 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2012/05/07 02:45:47 dholland Exp $
 
 BUILDLINK_TREE+=	raptor2
 
 .if !defined(RAPTOR2_BUILDLINK3_MK)
 RAPTOR2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.raptor2?=	raptor2>=2.0.6
+BUILDLINK_API_DEPENDS.raptor2+=	raptor2>=2.0.6
 BUILDLINK_ABI_DEPENDS.raptor2+=	raptor2>=2.0.6
 BUILDLINK_PKGSRCDIR.raptor2?=	../../textproc/raptor2
 
