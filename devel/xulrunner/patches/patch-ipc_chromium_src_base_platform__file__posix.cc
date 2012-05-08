e8a8cb25cf7bdf7d7eafd6744e54fb18f548ce7e
martin <martin@pkgsrc.org>
2012-05-08 19:29:36 +0000

    Note upstream bug report

--- devel/xulrunner/patches/patch-ipc_chromium_src_base_platform__file__posix.cc
+++ devel/xulrunner/patches/patch-ipc_chromium_src_base_platform__file__posix.cc
@@ -1,4 +1,6 @@
-$NetBSD: patch-ipc_chromium_src_base_platform__file__posix.cc,v 1.1 2011/07/12 15:12:36 tnn Exp $
+$NetBSD: patch-ipc_chromium_src_base_platform__file__posix.cc,v 1.2 2012/05/08 19:29:36 martin Exp $
+
+# Reported upstream: https://bugzilla.mozilla.org/show_bug.cgi?id=753046
 
 --- ipc/chromium/src/base/platform_file_posix.cc.orig	2011-06-15 21:57:27.000000000 +0000
 +++ ipc/chromium/src/base/platform_file_posix.cc
