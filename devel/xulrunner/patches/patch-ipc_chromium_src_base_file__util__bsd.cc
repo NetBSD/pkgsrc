cdca87369e1f0200d17212661c7be83411b1abbd
martin <martin@pkgsrc.org>
2012-05-08 19:29:36 +0000

    Note upstream bug report

--- devel/xulrunner/patches/patch-ipc_chromium_src_base_file__util__bsd.cc
+++ devel/xulrunner/patches/patch-ipc_chromium_src_base_file__util__bsd.cc
@@ -1,4 +1,6 @@
-$NetBSD: patch-ipc_chromium_src_base_file__util__bsd.cc,v 1.1 2012/03/06 12:34:09 ryoon Exp $
+$NetBSD: patch-ipc_chromium_src_base_file__util__bsd.cc,v 1.2 2012/05/08 19:29:36 martin Exp $
+
+# Reported upstream: https://bugzilla.mozilla.org/show_bug.cgi?id=753046
 
 --- ipc/chromium/src/base/file_util_bsd.cc.orig	2012-01-07 18:21:18.000000000 +0000
 +++ ipc/chromium/src/base/file_util_bsd.cc
