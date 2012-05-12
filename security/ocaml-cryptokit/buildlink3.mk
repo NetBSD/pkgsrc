5ddc803c10de7dd2b827a3cea8c5a4dd28e3504a
wiz <wiz>
2012-05-12 17:53:40 +0000

    Initial import of ocaml-cryptokit-1.5. Packaged byJaap Boender in PR 41691.
    The Cryptokit library for Objective Caml provides a variety of
    cryptographic primitives that can be used to implement cryptographic
    protocols in security-sensitive applications. The primitives provided
    include:
    
    Symmetric-key cryptography: AES, DES, Triple-DES, ARCfour, in ECB,
    CBC, CFB and OFB modes. Public-key cryptography: RSA encryption and
    signature; Diffie-Hellman key agreement. Hash functions and MACs:
    SHA-1, MD5, and MACs based on AES and DES. Random number generation.
    Encodings and compression: base 64, hexadecimal, Zlib compression.
    
    Additional ciphers and hashes can easily be used in conjunction
    with the library. In particular, basic mechanisms such as chaining
    modes, output buffering, and padding are provided by generic classes
    that can easily be composed with user-provided ciphers. More
    generally, the library promotes a "Lego"-like style of constructing
    and composing transformations over character streams.

---
+++ security/ocaml-cryptokit/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/05/12 17:53:40 wiz Exp $
+
+BUILDLINK_TREE+=	ocaml-cryptokit
+
+.if !defined(OCAML_CRYPTOKIT_BUILDLINK3_MK)
+OCAML_CRYPTOKIT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-cryptokit+=	ocaml-cryptokit>=1.5
+BUILDLINK_ABI_DEPENDS.ocaml-cryptokit?=	ocaml-cryptokit>=1.5
+BUILDLINK_PKGSRCDIR.ocaml-cryptokit?=	../../security/ocaml-cryptokit
+
+.include "../../lang/ocaml/buildlink3.mk"
+.endif # OCAML_CRYPTOKIT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-cryptokit
