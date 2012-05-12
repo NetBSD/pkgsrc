3302d6e912acd3f1b00fe1a25e740c7c5778132c
marino <marino@pkgsrc.org>
2012-05-12 21:01:47 +0000

    lang/openjdk7: Add support for DragonFly x86_64
    * Update bootstrap for i386-DragonFly
    * Add bootstrap for x86_64-DragonFly
    * Update patch-aa to handle missing EM_ALPHA definition (not used anyway)
    * Add patch for hotspot to handle x86_64 in uname
    * Update Makefile for parallel building of hotspot
    * Allow platform DragonFly 3.x
    
    Thanks for all the hard work building the bootstraps and testing:
    Francois Tigeot
    Chris Turner

--- lang/openjdk7/bootstrap.mk
+++ lang/openjdk7/bootstrap.mk
@@ -1,13 +1,15 @@
-# $NetBSD: bootstrap.mk,v 1.5 2012/02/22 13:25:14 ryoon Exp $
+# $NetBSD: bootstrap.mk,v 1.6 2012/05/12 21:01:47 marino Exp $
 
 ONLY_FOR_PLATFORM=	NetBSD-[56].*-i386 NetBSD-[56].*-x86_64
-ONLY_FOR_PLATFORM+=	DragonFly-2.*-i386
+ONLY_FOR_PLATFORM+=	DragonFly-[23].*
 
 BOOT.nb5-i386=		bootstrap-jdk7-bin-netbsd-5-i386-20110811.tar.bz2
 BOOT.nb5-amd64=		bootstrap-jdk7-bin-netbsd-5-amd64-20110811.tar.bz2
 BOOT.nb6-i386=		bootstrap-jdk7-bin-netbsd-6-i386-20110811.tar.bz2
 BOOT.nb6-amd64=		bootstrap-jdk7-bin-netbsd-6-amd64-20110811.tar.bz2
-BOOT.df210-i386=	bootstrap-jdk7-bin-dragonfly-2.10-i386-20110811.tar.bz2
+BOOT.df213-i386=	bootstrap-jdk7-bin-dragonfly-2.13-i386-20110811.tar.bz2
+# 1.7 partial bootstrap:
+BOOT.df213-amd64=	bootstrap-jdk7-bin-dragonfly-2.13-amd64-20110811.tar.bz2
 BOOT.common-20110811=	bootstrap-jdk7-bin-common-20110811.tar.bz2
 
 .if !empty(MACHINE_PLATFORM:MNetBSD-5.[0-8]*-i386) || make(distinfo)
@@ -35,8 +37,15 @@ EXTRACT_ONLY+=		${BOOT.nb6-amd64}
 .endif
 
 .if !empty(MACHINE_PLATFORM:MDragonFly-*-i386) || make(distinfo)
-DISTFILES+=		${BOOT.df210-i386}
-EXTRACT_ONLY+=		${BOOT.df210-i386}
+MASTER_SITES+=		http://dl.wolfpond.org/openjdk7/
+DISTFILES+=		${BOOT.df213-i386}
+EXTRACT_ONLY+=		${BOOT.df213-i386}
+.endif
+
+.if !empty(MACHINE_PLATFORM:MDragonFly-*-x86_64) || make(distinfo)
+MASTER_SITES+=		http://dl.wolfpond.org/openjdk7/
+DISTFILES+=		${BOOT.df213-amd64}
+EXTRACT_ONLY+=		${BOOT.df213-amd64}
 .endif
 
 .if !empty(DISTFILES:M*20110811*) || make(distinfo)
