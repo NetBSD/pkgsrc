0e4984a17bc977a5a7fffa7cdfa7cd1249925f2c
adam <adam@pkgsrc.org>
2012-05-15 15:07:22 +0000

    Fix g-ir-scanner issues on Mac OS X

--- devel/pango/patches/patch-pango_pangocoretext.h
+++ devel/pango/patches/patch-pango_pangocoretext.h
@@ -1,15 +1,17 @@
-$NetBSD: patch-pango_pangocoretext.h,v 1.1 2012/03/05 21:02:35 adam Exp $
+$NetBSD: patch-pango_pangocoretext.h,v 1.2 2012/05/15 15:07:23 adam Exp $
 
 Use correct CoreText header on Mac OS X.
 
---- pango/pangocoretext.h.orig	2012-03-05 20:36:03.000000000 +0000
+--- pango/pangocoretext.h.orig	2011-08-16 01:11:08.000000000 +0000
 +++ pango/pangocoretext.h
-@@ -25,7 +25,7 @@
+@@ -25,7 +25,9 @@
  
  #include <pango/pango-context.h>
  #include <pango/pango-font.h>
 -#include <Carbon/Carbon.h>
++#ifndef __GI_SCANNER__
 +#include <ApplicationServices/ApplicationServices.h>
++#endif
  
  G_BEGIN_DECLS
  
