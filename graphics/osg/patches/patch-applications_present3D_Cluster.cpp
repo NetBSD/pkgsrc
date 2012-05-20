cd6dcc07f08689d1d703a192d561425568b48417
marino <marino@pkgsrc.org>
2012-05-20 19:28:19 +0000

    graphics/osg: Update to version 3.0.1 broke DragonFly

--- graphics/osg/patches/patch-applications_present3D_Cluster.cpp
+++ graphics/osg/patches/patch-applications_present3D_Cluster.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-applications_present3D_Cluster.cpp,v 1.1 2012/03/07 16:27:35 adam Exp $
+$NetBSD: patch-applications_present3D_Cluster.cpp,v 1.2 2012/05/20 19:28:19 marino Exp $
 
 Support more platforms.
 
 --- applications/present3D/Cluster.cpp.orig	2012-03-05 16:33:33.000000000 +0000
 +++ applications/present3D/Cluster.cpp
-@@ -33,7 +33,7 @@
+@@ -33,9 +33,12 @@
  #if defined(__linux)
      #include <unistd.h>
      #include <linux/sockios.h>
@@ -12,13 +12,19 @@ Support more platforms.
 +#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
      #include <unistd.h>
      #include <sys/sockio.h>
++#elif defined(__DragonFly__)
++    #include <unistd.h>
++    #include <sys/sockio.h>
  #elif defined(__sgi)
-@@ -336,7 +336,7 @@ void Receiver::sync( void )
+     #include <unistd.h>
+     #include <net/soioctl.h>
+@@ -336,7 +339,8 @@ void Receiver::sync( void )
          return;
      }
  
 -#if defined(__linux) || defined(__FreeBSD__) || defined( __APPLE__ )
-+#if defined(__linux) || defined(__FreeBSD__) || defined( __APPLE__ ) || defined(__NetBSD__) || defined(__OpenBSD__)
++#if defined(__linux) || defined(__FreeBSD__) || defined( __APPLE__ ) \
++ || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
      socklen_t 
  #else
      int
