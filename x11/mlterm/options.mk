265648de74450a6769c3172f404cdb1b5e099815
obache <obache@pkgsrc.org>
2012-05-20 13:59:30 +0000

    Add indic support with `libind' option.
    requested by PR 46489.

--- x11/mlterm/options.mk
+++ x11/mlterm/options.mk
@@ -1,12 +1,18 @@
-# $NetBSD: options.mk,v 1.5 2012/05/20 06:07:31 tsutsui Exp $
+# $NetBSD: options.mk,v 1.6 2012/05/20 13:59:30 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mlterm
-PKG_SUPPORTED_OPTIONS=	m17nlib uim xft2
+PKG_SUPPORTED_OPTIONS=	libind m17nlib uim xft2
 PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		m17nlib uim xft2
+PLIST_VARS+=		ind m17nlib uim xft2
+
+.if !empty(PKG_OPTIONS:Mlibind)
+CONFIGURE_ARGS+=	--enable-ind
+PLIST.ind=		yes
+LICENSE+=		AND gnu-lgpl-v2
+.endif
 
 .if !empty(PKG_OPTIONS:Mm17nlib)
 .include "../../devel/m17n-lib/buildlink3.mk"
