4b008e6d229e5af5e415f76c2a52a0d276dc8964
tron <tron@pkgsrc.org>
2012-05-21 15:13:36 +0000

    Pullup ticket #3795 - requested by taca textproc/libxml2: security patch
    Revisions pulled up:
    - textproc/libxml2/Makefile                                     1.116
    - textproc/libxml2/distinfo                                     1.91-1.92
    - textproc/libxml2/patches/patch-threads.c                      1.2
    - textproc/libxml2/patches/patch-xpointer.c                     1.2
    
    ---
       Module Name:	pkgsrc
       Committed By:	mishka
       Date:		Fri Apr 27 15:05:02 UTC 2012
    
       Modified Files:
       	pkgsrc/textproc/libxml2: distinfo
       	pkgsrc/textproc/libxml2/patches: patch-threads.c
    
       Log Message:
       FreeBSD 6 and earlier misses the pthread_equal() stub as well.
       While here, improve comments a little bit.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Mon May 21 12:22:54 UTC 2012
    
       Modified Files:
       	pkgsrc/textproc/libxml2: Makefile distinfo
       	pkgsrc/textproc/libxml2/patches: patch-xpointer.c
    
       Log Message:
       Add fix for http://secunia.com/advisories/49177/ from repository.
    
       Bump PKGREVISION.

--- textproc/libxml2/patches/patch-xpointer.c
+++ textproc/libxml2/patches/patch-xpointer.c
@@ -1,8 +1,37 @@
-$NetBSD: patch-xpointer.c,v 1.1 2011/11/01 19:21:06 spz Exp $
+$NetBSD: patch-xpointer.c,v 1.1.6.1 2012/05/21 15:13:36 tron Exp $
+
+patches for these security problems:
+	http://secunia.com/advisories/46632/
+	http://secunia.com/advisories/49177/
 
 --- xpointer.c.orig	2010-10-12 06:25:33.000000000 +0000
 +++ xpointer.c
-@@ -1269,6 +1269,7 @@ xmlXPtrEvalXPointer(xmlXPathParserContex
+@@ -1007,21 +1007,14 @@ xmlXPtrEvalXPtrPart(xmlXPathParserContex
+ 		NEXT;
+ 		break;
+ 	    }
+-	    *cur++ = CUR;
+ 	} else if (CUR == '(') {
+ 	    level++;
+-	    *cur++ = CUR;
+ 	} else if (CUR == '^') {
+-	    NEXT;
+-	    if ((CUR == ')') || (CUR == '(') || (CUR == '^')) {
+-		*cur++ = CUR;
+-	    } else {
+-		*cur++ = '^';
+-		*cur++ = CUR;
++	    if ((NXT(1) == ')') || (NXT(1) == '(') || (NXT(1) == '^')) {
++		NEXT;
+ 	    }
+-	} else {
+-	    *cur++ = CUR;
+ 	}
++	*cur++ = CUR;
+ 	NEXT;
+     }
+     *cur = 0;
+@@ -1269,6 +1262,7 @@ xmlXPtrEvalXPointer(xmlXPathParserContex
  	ctxt->valueNr = 0;
  	ctxt->valueMax = 10;
  	ctxt->value = NULL;
