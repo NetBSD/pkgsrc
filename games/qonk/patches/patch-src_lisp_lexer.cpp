e9c37e967f9091e66430964c6becb4db98d8be72
dholland <dholland@pkgsrc.org>
2012-05-23 06:16:00 +0000

    Add missing <cstdio>, was failing in the linux build.

--- games/qonk/patches/patch-src_lisp_lexer.cpp
+++ games/qonk/patches/patch-src_lisp_lexer.cpp
@@ -1,12 +1,15 @@
-$NetBSD: patch-src_lisp_lexer.cpp,v 1.1 2011/12/05 22:45:19 joerg Exp $
+$NetBSD: patch-src_lisp_lexer.cpp,v 1.2 2012/05/23 06:16:00 dholland Exp $
 
---- src/lisp/lexer.cpp.orig	2011-12-05 21:37:43.000000000 +0000
+Add missing headers.
+
+--- src/lisp/lexer.cpp.orig	2007-09-14 05:43:03.000000000 +0000
 +++ src/lisp/lexer.cpp
-@@ -19,6 +19,7 @@
+@@ -19,6 +19,8 @@
  //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  #include <sstream>
  #include <stdexcept>
 +#include <cstring>
++#include <cstdio>
  
  #include "lexer.hpp"
  #include "translation.hpp"
