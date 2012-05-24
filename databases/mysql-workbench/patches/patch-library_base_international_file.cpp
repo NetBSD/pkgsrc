a319652231eb713688e892e345b4291f86527983
marino <marino>
2012-05-24 18:54:16 +0000

    databases/mysql-workbench: Fix unportable patch, add DragonFly fix
    One patch was hardcoded for NetBSD, return it closer to intention without
    breaking NetBSD.  It's still not great, but a little better.

--- databases/mysql-workbench/patches/patch-library_base_international_file.cpp
+++ databases/mysql-workbench/patches/patch-library_base_international_file.cpp
@@ -1,18 +1,13 @@
-$NetBSD: patch-library_base_international_file.cpp,v 1.1.1.1 2011/05/16 12:40:50 adam Exp $
+$NetBSD: patch-library_base_international_file.cpp,v 1.2 2012/05/24 18:54:17 marino Exp $
 
-Use const string for iconv on all platforms.
-
---- library/base/international_file.cpp.orig	2011-05-16 08:33:28.000000000 +0000
+--- library/base/international_file.cpp.orig	2011-03-17 12:48:42.000000000 +0000
 +++ library/base/international_file.cpp
-@@ -257,11 +257,7 @@ boost::int64_t read_char_from_intl_file(
+@@ -257,7 +257,7 @@ boost::int64_t read_char_from_intl_file(
    else
    {
      gunichar utf8_char= g_utf8_get_char(file->next_utf8_char);
 -#ifdef _WIN32
++#ifndef __DragonFly__
      const char * cur_utf8_char= file->next_utf8_char;
--#else
--    char * cur_utf8_char= file->next_utf8_char;
--#endif
-     file->next_utf8_char= g_utf8_next_char(file->next_utf8_char);
- 
-     *bytes_read= (int) (file->next_utf8_char - cur_utf8_char);
+ #else
+     char * cur_utf8_char= file->next_utf8_char;
