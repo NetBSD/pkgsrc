3e86abd0784d6e70f63442d76c6ef33986f42c3a
adam <adam@pkgsrc.org>
2012-05-24 08:07:33 +0000

    Changes 4.8.2: * Over 100 functional improvements * WebKit updated to version 2.2.2 * libpng updated to version 1.5.10 * Thirty improvements to documentation and tools

--- x11/qt4-libs/patches/patch-qmake_generators_makefile.cpp
+++ x11/qt4-libs/patches/patch-qmake_generators_makefile.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-qmake_generators_makefile.cpp,v 1.2 2012/04/09 09:12:49 adam Exp $
+$NetBSD: patch-qmake_generators_makefile.cpp,v 1.3 2012/05/24 08:07:33 adam Exp $
 
---- qmake/generators/makefile.cpp.orig	2012-03-14 14:01:12.000000000 +0000
+--- qmake/generators/makefile.cpp.orig	2012-05-24 05:03:21.000000000 +0000
 +++ qmake/generators/makefile.cpp
 @@ -3256,7 +3256,7 @@ MakefileGenerator::writePkgConfigFile()
              bundle = bundle.left(suffix);
@@ -9,5 +9,5 @@ $NetBSD: patch-qmake_generators_makefile.cpp,v 1.2 2012/04/09 09:12:49 adam Exp 
 -        pkgConfiglibDir = "-L${libdir}";
 +        pkgConfiglibDir = "-Wl,-R${libdir} -L${libdir}";
          pkgConfiglibName = "-l" + lname.left(lname.length()-Option::libtool_ext.length());
-         pkgConfiglibName += project->first("TARGET_VERSION_EXT");
-     }
+         if (project->isActiveConfig("shared"))
+             pkgConfiglibName += project->first("TARGET_VERSION_EXT");
