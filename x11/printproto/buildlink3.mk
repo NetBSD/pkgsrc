ea5ff44fff2ad165b4e90f653a6098ce44759337
wiz <wiz@pkgsrc.org>
2012-05-29 14:18:48 +0000

    Update to 1.0.5:
    Alan Coopersmith (7):
          Migrate to xorg macros 1.3 & XORG_DEFAULT_OPTIONS
          Move Xprint protocol spec & overview man pages from xorg-docs
          Sun's copyrights belong to Oracle now
          config: Add missing AC_CONFIG_SRCDIR & AM_MAINTAINER_MODE
          config: Replace obsolete usage of AC_OUTPUT with AC_CONFIG_FILES
          Add README with pointers to mailing lists, git, bugzilla, the true status, etc.
          printproto 1.0.5
    
    Gaetan Nadon (1):
          config: update AC_PREREQ statement to 2.60
    
    Paulo Cesar Pereira de Andrade (1):
          Janitor: Correct make distcheck and dont distribute autogen.sh
    
    Paulo Zanoni (1):
          Use docbookx.dtd version 4.3 for all docs

--- x11/printproto/buildlink3.mk
+++ x11/printproto/buildlink3.mk
@@ -1,6 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:48 joerg Exp $
-
-BUILDLINK_DEPMETHOD.printproto?=	build
+# $NetBSD: buildlink3.mk,v 1.3 2012/05/29 14:18:48 wiz Exp $
 
 BUILDLINK_TREE+=	printproto
 
@@ -9,6 +7,8 @@ PRINTPROTO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.printproto+=	printproto>=1.0.3
 BUILDLINK_PKGSRCDIR.printproto?=	../../x11/printproto
+
+BUILDLINK_DEPMETHOD.printproto?=	build
 
 .include "../../x11/xproto/buildlink3.mk"
 .endif # PRINTPROTO_BUILDLINK3_MK
