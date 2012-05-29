4948af0cbb3ebeb4b48ad3c0a9d83020edbcce61
wiz <wiz@pkgsrc.org>
2012-05-29 15:06:54 +0000

    Update to 1.2.1:
    Alan Coopersmith (1):
          damageproto 1.2.1
    
    Gaetan Nadon (10):
          .gitignore: use common defaults with custom section # 24239
          configure.ac: deploy the new XORG_DEFAULT_OPTIONS #24242
          Makefile.am: INSTALL file is missing or incorrect #24206
          Makefile.am: ChangeLog not required: EXTRA_DIST or *CLEANFILES #24432
          NEWS: remove empty file #24206
          README: file created or updated #24206
          Revert "NEWS: remove empty file #24206"
          Makefile.am: INSTALL file is missing or incorrect #24206
          Makefile.am: add ChangeLog and INSTALL on MAINTAINERCLEANFILES
          config: remove the pkgconfig pc.in file from EXTRA_DIST
    
    Rémi Cardona (1):
          Use $(docdir) for damageproto.txt install path

--- x11/damageproto/buildlink3.mk
+++ x11/damageproto/buildlink3.mk
@@ -1,6 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:25:39 joerg Exp $
-
-BUILDLINK_DEPMETHOD.damageproto?=	build
+# $NetBSD: buildlink3.mk,v 1.4 2012/05/29 15:06:54 wiz Exp $
 
 BUILDLINK_TREE+=	damageproto
 
@@ -9,6 +7,8 @@ DAMAGEPROTO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.damageproto+=	damageproto>=1.1.0
 BUILDLINK_PKGSRCDIR.damageproto?=	../../x11/damageproto
+
+BUILDLINK_DEPMETHOD.damageproto?=	build
 
 .include "../../x11/xproto/buildlink3.mk"
 .include "../../x11/fixesproto/buildlink3.mk"
