82967a70fac44a8b3ae3739c03a784174c9153ca
wiz <wiz@pkgsrc.org>
2012-05-29 13:31:38 +0000

    Initial import of libmusicbrainz5-5.0.1, a new major version of libmusicbrainz.
    Compared to v3 (in audio/libmusicbrainz) there were the following
    changes:
    
    Changes for libmusicbrainz 5.0.1
    --------------------------------
    
       - Fix LMB-32 - Correctly ignore unrecognised nodes
       - Don't compile using -Werror when building from tarball
    
    Changes for libmusicbrainz 5.0.0
    --------------------------------
    
       - Deprecated functions removed
    
    Changes for libmusicbrainz 4.0.3
    --------------------------------
    
       - Fix LMB-32 - Correctly ignore unrecognised nodes
       - Don't compile using -Werror when building from tarball
    
    Changes for libmusicbrainz 4.0.2
    --------------------------------
    
       - Add extra attributes to Alias
       - Add IPI lists to Artist and Label
       - Add 'Ended' to Lifespan
       - Add PrimaryType and SecondaryTypeList to ReleaseGroup
       - Add text based track number to Track
       - Add ISWC list to Work
       - Debug builds show queries and responses on stderr
       - Documentation updates
    
    Changes for libmusicbrainz 4.0.1
    --------------------------------
    
       - Fixed bug LMB-30 - Unable to access all relation lists on objects with
          multiple relation lists
       - Fixed bug LMB-31 - mb_relationlist_get_targettype always returns
          "target-type"
    
    Changes for libmusicbrainz 4.0.0
    --------------------------------
    
       - Added support for collection list being returned as part of a release
    
    Changes for libmusicbrainz 4.0.0 beta 2
    ---------------------------------------
    
       - Restructured for easier maintenance
       - List class interface rewritten
       - Added handling of extension elements and attributes
       - Add handling of all list attributes and elements
       - C interface is now automatically generated from an XML file template
    
    N.B. The API has changed with regard to lists since beta 1. Any
    applications are likely to need rewriting to take account of this
    and will certainly need to be recompiled due to the ABI change.
    
    Changes for libmusicbrainz 4.0.0 beta 1
    ---------------------------------------
    
       - Rewritten from scratch for NGS

---
+++ audio/libmusicbrainz5/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/05/29 13:31:38 wiz Exp $
+
+BUILDLINK_TREE+=	libmusicbrainz5
+
+.if !defined(LIBMUSICBRAINZ5_BUILDLINK3_MK)
+LIBMUSICBRAINZ5_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libmusicbrainz5+=	libmusicbrainz5>=5.0.1
+BUILDLINK_PKGSRCDIR.libmusicbrainz5?=	../../audio/libmusicbrainz5
+
+.include "../../www/neon/buildlink3.mk"
+.endif	# LIBMUSICBRAINZ5_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libmusicbrainz5
