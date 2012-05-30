14b24d1695f7dde5d4e3ad652cf5a8230de1a289
taca <taca@pkgsrc.org>
2012-05-30 03:27:05 +0000

    Fix path of the target file in patch files.

--- sysutils/xentools41/patches/patch-ioemu-qemu-xen_hw_pass-through.c
+++ sysutils/xentools41/patches/patch-ioemu-qemu-xen_hw_pass-through.c
@@ -1,7 +1,7 @@
-$NetBSD: patch-ioemu-qemu-xen_hw_pass-through.c,v 1.1 2012/05/29 15:01:34 bouyer Exp $
+$NetBSD: patch-ioemu-qemu-xen_hw_pass-through.c,v 1.2 2012/05/30 03:27:05 taca Exp $
 
---- /home/bouyer/pkgsrc/sysutils/xentools41/work/xen-4.1.2/tools/ioemu-qemu-xen/hw/pass-through.c.orig	2012-05-24 16:05:23.000000000 +0200
-+++ /home/bouyer/pkgsrc/sysutils/xentools41/work/xen-4.1.2/tools/ioemu-qemu-xen/hw/pass-through.c	2012-05-24 16:06:19.000000000 +0200
+--- ioemu-qemu-xen/hw/pass-through.c.orig	2012-05-24 16:05:23.000000000 +0200
++++ ioemu-qemu-xen/hw/pass-through.c	2012-05-24 16:06:19.000000000 +0200
 @@ -84,8 +84,6 @@
   */
  
