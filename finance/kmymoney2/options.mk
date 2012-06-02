e4790f68394b9f40032333aac632a63612102940
dholland <dholland@pkgsrc.org>
2012-06-02 18:33:32 +0000

    As the sqlite plugin in the PLIST is unconditional, and sqlite3 is innocuous and small compared to kde3, and built into netbsd-6 anyway, and the package finds sqlite3 regardless of whether it's buildlinked, always depend on sqlite3. Should fix build on netbsd-5.
    Remove commented-out traces of a sqlite3 option from options.mk.
    
    PKGREVISION++

--- finance/kmymoney2/options.mk
+++ finance/kmymoney2/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.3 2009/09/02 10:05:52 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2012/06/02 18:33:32 dholland Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kmymoney2
-PKG_SUPPORTED_OPTIONS=	libofx #sqlite3
-PKG_SUGGESTED_OPTIONS=	libofx #sqlite3
+PKG_SUPPORTED_OPTIONS=	libofx
+PKG_SUGGESTED_OPTIONS=	libofx
 
 .include "../../mk/bsd.options.mk"
 
@@ -12,8 +12,3 @@ PLIST_VARS+=		libofx #sqlite3
 .include "../../finance/libofx/buildlink3.mk"
 PLIST.libofx=		yes
 .endif
-
-#.if !empty(PKG_OPTIONS:Msqlite3)
-#.include "../../databases/sqlite3/buildlink3.mk"
-#PLIST.sqlite3=		yes
-#.endif
