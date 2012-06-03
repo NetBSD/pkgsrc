0b88121c41c4f0ad8c22e3b19b46fd4de252f97b
wiz <wiz@pkgsrc.org>
2012-06-03 10:08:37 +0000

    Add RCS Id.

--- misc/stellarium/patches/patch-src_core_modules_Skybright.cpp
+++ misc/stellarium/patches/patch-src_core_modules_Skybright.cpp
@@ -1,3 +1,5 @@
+$NetBSD: patch-src_core_modules_Skybright.cpp,v 1.2 2012/06/03 10:08:37 wiz Exp $
+
 --- src/core/modules/Skybright.cpp.orig	2011-07-02 15:22:18.000000000 +0200
 +++ src/core/modules/Skybright.cpp	2011-11-01 17:14:24.676826514 +0100
 @@ -29,6 +29,8 @@
