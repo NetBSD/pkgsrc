9f072035a1fbb750c63281d11f075e7e2df0bbf1
ryoon <ryoon@pkgsrc.org>
2012-06-05 18:09:21 +0000

    Update to 13.0
    Changelog:
    * When opening a new tab, users are now presented with their most visited pages
    * General performance improvements through incremental JavaScript garbage collection
    * The default home page now has quicker access to bookmarks, history, settings, and more SPDY protocol now enabled by default for faster browsing on supported sites
    * Restored background tabs are not loaded by default for faster startup
    * Smooth scrolling is now enabled by default
    * 72 total improvements to Page Inspector, HTML panel, Style Inspector, Scratchpad and Style Editor
    * The column-fill CSS property has been implemented
    * Experimental support for ECMAScript 6 Map and Set objects has been implemented
    * Support for the CSS3 background-position property extended syntax has been added
    * The :invalid pseudo-class can now be applied to the element
    * The CSS turn <angle> unit is now supported

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,12 +1,12 @@
-# $NetBSD: dist.mk,v 1.30 2012/04/26 13:30:29 ryoon Exp $
+# $NetBSD: dist.mk,v 1.31 2012/06/05 18:09:21 ryoon Exp $
 #
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	12.0
-MOZ_BRANCH_MINOR=	
+MOZ_BRANCH=	13.0
+MOZ_BRANCH_MINOR=
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
