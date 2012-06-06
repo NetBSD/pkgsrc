8254df8c3c8b0ca3a431764e7c9e5b0e563054cb
asau <asau@pkgsrc.org>
2012-06-06 23:14:10 +0000

    PCRE is no longer optional.

--- math/octave/options.mk
+++ math/octave/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.4 2010/06/02 18:35:38 adam Exp $
+# $NetBSD: options.mk,v 1.5 2012/06/06 23:14:10 asau Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.octave
-PKG_SUPPORTED_OPTIONS=	glpk graphicsmagick hdf5 pcre qhull
-PKG_SUGGESTED_OPTIONS=	glpk hdf5 pcre qhull
+PKG_SUPPORTED_OPTIONS=	glpk graphicsmagick hdf5 qhull
+PKG_SUGGESTED_OPTIONS=	glpk hdf5 qhull
 
 .include "../../mk/bsd.options.mk"
 
@@ -22,12 +22,6 @@ CONFIGURE_ENV+=		ac_cv_prog_MAGICK_CONFIG=no
 .include "../../devel/hdf5/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-hdf5
-.endif
-
-.if !empty(PKG_OPTIONS:Mpcre)
-.include "../../devel/pcre/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-pcre
 .endif
 
 .if !empty(PKG_OPTIONS:Mqhull)
