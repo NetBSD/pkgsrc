31efd3d332f6924b6b3cf08b97cd7bc1fae12aa3
ryoon <ryoon@pkgsrc.org>
2012-06-07 13:48:18 +0000

    Update to 4.9.3.1 (4.9.1 really)
    No ChangeLog is provided.

--- devel/nspr/patches/patch-nsprpub_pr_src_md_unix_uxrng.c
+++ devel/nspr/patches/patch-nsprpub_pr_src_md_unix_uxrng.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-nsprpub_pr_src_md_unix_uxrng.c,v 1.1 2012/04/18 20:57:33 ryoon Exp $
+$NetBSD: patch-nsprpub_pr_src_md_unix_uxrng.c,v 1.2 2012/06/07 13:48:21 ryoon Exp $
 
---- nsprpub/pr/src/md/unix/uxrng.c.orig	2011-09-02 20:15:44.000000000 +0000
+--- nsprpub/pr/src/md/unix/uxrng.c.orig	2012-05-18 21:55:29.000000000 +0000
 +++ nsprpub/pr/src/md/unix/uxrng.c
-@@ -126,7 +126,7 @@ GetHighResClock(void *buf, size_t maxbyt
+@@ -94,7 +94,7 @@ GetHighResClock(void *buf, size_t maxbyt
  
  #elif (defined(LINUX) || defined(FREEBSD) || defined(__FreeBSD_kernel__) \
      || defined(NETBSD) || defined(__NetBSD_kernel__) || defined(OPENBSD) \
--    || defined(SYMBIAN))
-+    || defined(SYMBIAN)) || defined(__DragonFly__)
+-    || defined(SYMBIAN) || defined(__GNU__))
++    || defined(SYMBIAN) || defined(__GNU__)) || defined(__DragonFly__)
  #include <sys/types.h>
  #include <sys/stat.h>
  #include <fcntl.h>
