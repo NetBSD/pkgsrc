c16b5ed31c7a92f09850f7f08d98c23e644a3a9b
adam <adam>
2012-06-09 11:51:36 +0000

    Changes 0.90: * cone/gettext.C: gettext can't deal with literal ^Ds, so provide a   workaround: "\\D" literal. * cone/cursesindexdisplay.C (drawLine): Misformatting of the date   column in some locales.

--- mail/cone/patches/patch-libmail-mboxfolder.C
+++ mail/cone/patches/patch-libmail-mboxfolder.C
@@ -1,10 +1,10 @@
-$NetBSD: patch-libmail-mboxfolder.C,v 1.1 2011/11/22 18:12:35 joerg Exp $
+$NetBSD: patch-libmail-mboxfolder.C,v 1.2 2012/06/09 11:51:36 adam Exp $
 
 Use stdio functions to ensure that the mail::file cast to FILE * happens.
 
 --- libmail/mboxfolder.C.orig	2011-11-22 16:30:20.000000000 +0000
 +++ libmail/mboxfolder.C
-@@ -40,6 +40,8 @@
+@@ -39,6 +39,8 @@
  
  #include <vector>
  
