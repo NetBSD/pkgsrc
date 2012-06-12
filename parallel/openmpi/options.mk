d1e7de01df1a2565f11e7782885245f188314060
asau <asau@pkgsrc.org>
2012-06-12 23:36:30 +0000

    Update to OpenMPI 1.6
    Disable automatic detection of SGE and SLURM to avoid PLIST divergence.
    
    
    Changes in OpenMPI 1.6
    
    v1.6 is a major release; it effectively deprecates both the v1.4.x
    and v1.5.x series, and replaces v1.4.x as the current "super stable" series.
    A forthcoming v1.7 series will become the new "feature" series.
    
    *All users* -- including production users and downstream providers --
    are encouraged to upgrade to v1.6.
    
    Note that v1.6 is ABI compatible with the entire v1.5.x series,
    but is not ABI compatible with the v1.4.x series.
    See http://www.open-mpi.org/software/ompi/versions/ for a description
    of Open MPI's release methodology.
    
    
    Here is a list of changes in v1.6 as compared to v1.5.5:
    
    - Fix some process affinity issues.  When binding a process, Open MPI
      will now bind to all available hyperthreads in a core (or socket,
      depending on the binding options specified).
      --> Note that "mpirun --bind-to-socket ..." does not work on POWER6-
          and POWER7-based systems with some Linux kernel versions.  See
          the FAQ on the Open MPI web site for more information.
    - Add support for ARM5 and ARM6 (in addition to the existing ARM7
      support).  Thanks to Evan Clinton for the patch.
    - Minor Mellanox MXM fixes.
    - Properly detect FDR10, FDR, and EDR OpenFabrics devices.
    - Minor fixes to the mpirun(1) and MPI_Comm_create(3) man pages.
    - Prevent segv if COMM_SPAWN_MULTIPLE fails.  Thanks to Fujitsu for
      the patch.
    - Disable interposed memory management in fakeroot environments.  This
      fixes a problem in some build environments.
    - Minor hwloc updates.
    - Array versions of MPI_TEST and MPI_WAIT with a count==0 will now
      return immediately with MPI_SUCCESS.  Thanks to Jeremiah Willcock
      for the suggestion.
    - Update VampirTrace to v5.12.2.
    - Properly handle forwarding stdin to all processes when "mpirun
      --stdin all" is used.
    - Workaround XLC assembly bug.
    - OS X Tiger (10.3) has not been supported for a while, so forcibly
      abort configure if we detect it.
    - Fix segv in the openib BTL when running on SPARC 64 systems.
    - Fix some include file ordering issues on some BSD-based platforms.
      Thanks to Paul Hargove for this (and many, many other) fixes.
    - Properly handle .FALSE. return parameter value to attribute copy
      callback functions.
    - Fix a bunch of minor C++ API issues; thanks to Fujitsu for the patch.
    - Fixed the default hostfile MCA parameter behavior.
    - Per the MPI spec, ensure not to touch the port_name parameter to
      MPI_CLOSE_PORT (it's an IN parameter).

--- parallel/openmpi/options.mk
+++ parallel/openmpi/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2010/05/14 20:12:58 asau Exp $
+# $NetBSD: options.mk,v 1.3 2012/06/12 23:36:31 asau Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.openmpi
 PKG_SUPPORTED_OPTIONS=	debug openmpi-vampirtrace
@@ -14,6 +14,10 @@ CONFIGURE_ARGS+=	--enable-debug
 .if !empty(PKG_OPTIONS:Mopenmpi-vampirtrace)
 CONFLICTS+=	libotf-[0-9]*
 PLIST_SRC+=	PLIST.vt
+
+CONF_FILES+=		share/vampirtrace/VampirTrace/examples/vt-java-default-filter.spec \
+			$(PKG_SYSCONFDIR)/vt-java-default-filter.spec
+
 .include "../../graphics/freetype2/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--enable-contrib-no-build=vt
