8900af09160e07a83d1c26dd86b48c0877c9b154
gdt <gdt@pkgsrc.org>
2012-06-13 14:29:16 +0000

    Drop USE_INET6 => inet6 option binding.
    For a long time, the norm in pkgsrc was that packages had an option
    for IPv6 support "inet6", and this was not in PKG_SUGGESTED_OPTIONS.
    On NetBSD (and probably other BSD), USE_INET6 was defined in system mk
    files, and pkgsrc noticed this and enabled the inet6 option globally.
    But, in some environments, this did not happen.
    
    The inet6 option has been added to PKG_SUGGESTED_OPTIONS for almost
    all packages.  This change decouples IPv6 support in pkgsrc from the
    base system.
    
    People building on systems that do not support IPv6, or who do not
    want IPv6 support in packages, can add
    
      PKG_DEFAULT_OPTIONS+=		-inet6
    
    to mk.conf.
    
    (Discussed for the last week on various lists, and ok wiz@.)

--- mk/defaults/obsolete.mk
+++ mk/defaults/obsolete.mk
@@ -1,4 +1,4 @@
-# $NetBSD: obsolete.mk,v 1.28 2007/01/30 07:09:40 wiz Exp $
+# $NetBSD: obsolete.mk,v 1.29 2012/06/13 14:29:16 gdt Exp $
 #
 # This file holds make(1) logic to allow obsolete or deprecated variables
 # still to be used.  These may eventually disappear over time as the contents
@@ -21,5 +21,3 @@ PKG_LEGACY_OPTIONS+=	kerberos
 PKG_OPTIONS_DEPRECATED_WARNINGS+="Deprecated variable KERBEROS used, use PKG_DEFAULT_OPTIONS+=kerberos instead."
 .  endif
 .endif
-
-PKG_OPTIONS_LEGACY_VARS+=	USE_INET6:inet6
