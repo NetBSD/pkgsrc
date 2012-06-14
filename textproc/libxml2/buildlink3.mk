17ac1a72390bc6cafaf63a684e7818cccbf3c7c3
sbd <sbd@pkgsrc.org>
2012-06-14 07:39:36 +0000

    Add missing archivers/xz buildlink.
    Bump PKGREVISION

--- textproc/libxml2/buildlink3.mk
+++ textproc/libxml2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2009/03/20 19:25:31 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2012/06/14 07:39:37 sbd Exp $
 
 BUILDLINK_TREE+=	libxml2
 
@@ -6,13 +6,14 @@ BUILDLINK_TREE+=	libxml2
 LIBXML2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libxml2+=	libxml2>=2.6.2
-BUILDLINK_ABI_DEPENDS.libxml2+=	libxml2>=2.6.23nb1
+BUILDLINK_ABI_DEPENDS.libxml2+=	libxml2>=2.8.0nb2
 BUILDLINK_PKGSRCDIR.libxml2?=	../../textproc/libxml2
 
 BUILDLINK_FILES.libxml2+=	bin/xml2-config
 
 USE_FEATURES+=		glob
 
+.include "../../archivers/xz/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .endif # LIBXML2_BUILDLINK3_MK
