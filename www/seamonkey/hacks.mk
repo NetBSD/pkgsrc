d1fdbcf4eabe3e0038c56df5a9903470f09def75
ryoon <ryoon@pkgsrc.org>
2012-06-14 11:27:33 +0000

    Update to 2.10
    Changelog: from http://www.seamonkey-project.org/releases/seamonkey2.10/
    SeaMonkey-specific changes
    
        The domain name is highlighted in the location bar by default now, configurable in Preferences.
        The amount of tabs to be restored concurrently can be configured in Preferences now.
        News username and password auth dialogs have been combined and show for which server the prompt is now.
    
    Mozilla platform changes
    
        The new minimum supported Windows version is now XP SP2, Windows 2000 support has been dropped.
        The SPDY protocol now enabled by default for faster browsing on supported sites
        The column-fill CSS property has been implemented.
        Experimental support for ECMAScript 6 Map and Set objects has been implemented.
        Support for the CSS3 background-position property extended syntax has been added.
        The :invalid pseudo-class can now be applied to the <form> element.
        The CSS turn <angle> unit is now supported.
        Fixed several stability issues.

--- www/seamonkey/hacks.mk
+++ www/seamonkey/hacks.mk
@@ -1,3 +1,6 @@
-# $NetBSD: hacks.mk,v 1.3 2012/03/19 10:35:57 ryoon Exp $
+# $NetBSD: hacks.mk,v 1.4 2012/06/14 11:27:34 ryoon Exp $
+
+# workround for link of thunderbird-bin etc.
+LDFLAGS+=	-Wl,-rpath,${PREFIX}/lib/seamonkey
 
 .include "../../devel/xulrunner/hacks.mk"
