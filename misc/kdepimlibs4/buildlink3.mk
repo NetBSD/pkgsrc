6ddc0f90d159bd390d502c01dc74d624d7dee121
sbd <sbd@pkgsrc.org>
2012-06-14 22:09:13 +0000

    Add missing devel/libuuid buildlink.
    Bump PKGREVISION.

--- misc/kdepimlibs4/buildlink3.mk
+++ misc/kdepimlibs4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2012/06/14 07:43:33 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2012/06/14 22:09:13 sbd Exp $
 
 BUILDLINK_TREE+=	kdepimlibs
 
@@ -6,11 +6,12 @@ BUILDLINK_TREE+=	kdepimlibs
 KDEPIMLIBS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kdepimlibs+=	kdepimlibs4>=4.0.0
-BUILDLINK_ABI_DEPENDS.kdepimlibs+=	kdepimlibs4>=4.8.2nb1
+BUILDLINK_ABI_DEPENDS.kdepimlibs+=	kdepimlibs4>=4.8.2nb2
 BUILDLINK_PKGSRCDIR.kdepimlibs?=	../../misc/kdepimlibs4
 
 .include "../../databases/openldap-client/buildlink3.mk"
 .include "../../devel/boost-libs/buildlink3.mk"
+.include "../../devel/libuuid/buildlink3.mk"
 .include "../../mail/akonadi/buildlink3.mk"
 .include "../../security/cyrus-sasl/buildlink3.mk"
 .include "../../security/gpgme/buildlink3.mk"
