5c14c5e170fb8dd874983725c9d04c0ec1f2417a
dholland <dholland@pkgsrc.org>
2012-06-16 21:03:41 +0000

    Add emacs24 package, based on wip/emacs-rc.

---
+++ editors/emacs24/files/site-init.el
@@ -0,0 +1,4 @@
+;; $NetBSD: site-init.el,v 1.1 2012/06/16 21:03:42 dholland Exp $
+
+(autoload 'send-pr "/usr/share/gnats/send-pr.el" 
+  "Command to create and send a problem report." t)
---
+++ editors/emacs24/version.mk
@@ -0,0 +1,7 @@
+# $NetBSD: version.mk,v 1.1 2012/06/16 21:03:42 dholland Exp $
+
+_EMACS_FLAVOR=	emacs
+_EMACS_REQD=	emacs>=24.1<25
+
+_EMACS_VERSION_MAJOR=	24
+_EMACS_VERSION_MINOR=	1
