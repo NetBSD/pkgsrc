80df4e8c06865c7dc9350c3d79baec70e1676835
wiz <wiz@pkgsrc.org>
2012-06-16 23:06:07 +0000

    Remove unnecessary include.

--- chat/xchat/options.mk
+++ chat/xchat/options.mk
@@ -1,7 +1,4 @@
-# $NetBSD: options.mk,v 1.14 2009/10/24 15:51:25 tnn Exp $
-
-.include "../../mk/bsd.prefs.mk"
-
+# $NetBSD: options.mk,v 1.15 2012/06/16 23:06:07 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xchat
 PKG_SUPPORTED_OPTIONS=	inet6 ssl socks5 mitshm gnome
