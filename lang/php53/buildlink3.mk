e55ad96c87f7f592e77078b157540473d7d6dcd9
taca <taca@pkgsrc.org>
2012-06-16 05:24:10 +0000

    Limit upper version of PHP.

--- lang/php53/buildlink3.mk
+++ lang/php53/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/06/14 07:43:11 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/06/16 05:24:10 taca Exp $
 
 BUILDLINK_TREE+=	php
 
 .if !defined(PHP_BUILDLINK3_MK)
 PHP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.php+=	php>=5.3.2
-BUILDLINK_ABI_DEPENDS.php+=	php>=5.3.13nb3
+BUILDLINK_API_DEPENDS.php+=	php>=5.3.2<5.4
+BUILDLINK_ABI_DEPENDS.php+=	php>=5.3.13nb3<5.4
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php53
 
 .include "../../textproc/libxml2/buildlink3.mk"
