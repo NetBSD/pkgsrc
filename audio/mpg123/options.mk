51d30a0ee079e6dbed4a707dd903aa7e0d19d5ef
tron <tron@pkgsrc.org>
2012-06-17 07:17:57 +0000

    Build with generic FPU code under Mac OS X Lion as the assembler code causes linker errors.

--- audio/mpg123/options.mk
+++ audio/mpg123/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2010/03/14 14:19:19 martin Exp $
+# $NetBSD: options.mk,v 1.2 2012/06/17 07:17:57 tron Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.mpg123
 PKG_OPTIONS_OPTIONAL_GROUPS=	fpu
@@ -8,8 +8,13 @@ PKG_SUGGESTED_OPTIONS+=		mpg123-fifo
 .include "../../mk/bsd.fast.prefs.mk"
 
 .if (${MACHINE_ARCH} == "i386")
+.  if empty(MACHINE_PLATFORM:MDarwin-11.*-i386)
 PKG_OPTIONS_GROUP.fpu=		mpg123-x86-dither mpg123-with-fpu
 PKG_SUGGESTED_OPTIONS+=		mpg123-x86-dither
+.  else
+PKG_OPTIONS_GROUP.fpu=		mpg123-with-fpu
+PKG_SUGGESTED_OPTIONS+=		mpg123-with-fpu
+.  endif
 .elif (${MACHINE_ARCH} == "arm") || (${MACHINE_ARCH} == "arm32")
 PKG_OPTIONS_GROUP.fpu=		mpg123-with-fpu
 .elif (${MACHINE_ARCH} == "powerpc")
