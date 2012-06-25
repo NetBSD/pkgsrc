122ae71505a89a5a5fa6a47a17fad767dbe443d7
joerg <joerg@pkgsrc.org>
2012-06-25 11:29:13 +0000

    Fix build with Clang.

---
+++ graphics/kdegraphics3/patches/patch-kviewshell_documentWidget.cpp
@@ -0,0 +1,14 @@
+$NetBSD: patch-kviewshell_documentWidget.cpp,v 1.1 2012/06/25 11:29:14 joerg Exp $
+
+--- kviewshell/documentWidget.cpp.orig	2012-06-18 18:22:45.000000000 +0000
++++ kviewshell/documentWidget.cpp
+@@ -287,7 +287,8 @@ void DocumentWidget::paintEvent(QPaintEv
+     if (KVSPrefs::changeColors() && KVSPrefs::renderMode() != KVSPrefs::EnumRenderMode::Paper)
+     {
+       // Paint widget contents with accessibility changes.
+-      bitBlt ( this, destRect.topLeft(), &pageData->accessiblePixmap(), pixmapRect, CopyROP);
++      QPixmap p(pageData->accessiblePixmap());
++      bitBlt ( this, destRect.topLeft(), &p, pixmapRect, CopyROP);
+     }
+     else
+     {
