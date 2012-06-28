f45cdc82f278fa4cdbb1cbfe27e2b6d592e7c21f
sbd <sbd@pkgsrc.org>
2012-06-28 08:05:27 +0000

    Make "Fix build on SunOS." patch a SunOS only fix as it breaks Linux build otherwise.

--- audio/libvisual0.2/patches/patch-libvisual_lv__types.h
+++ audio/libvisual0.2/patches/patch-libvisual_lv__types.h
@@ -1,23 +1,20 @@
-$NetBSD: patch-libvisual_lv__types.h,v 1.1 2012/02/15 22:01:43 hans Exp $
+$NetBSD: patch-libvisual_lv__types.h,v 1.2 2012/06/28 08:05:27 sbd Exp $
 
---- libvisual/lv_types.h.orig	2005-01-01 15:11:56.000000000 +0100
-+++ libvisual/lv_types.h	2012-01-11 15:00:23.565973572 +0100
-@@ -32,18 +32,6 @@ extern "C" {
+--- libvisual/lv_types.h.orig	2005-01-01 14:11:56.000000000 +0000
++++ libvisual/lv_types.h
+@@ -32,6 +32,7 @@ extern "C" {
  
  #define VISUAL_CHECK_CAST(uiobj, cast_type, cast)    ((cast*) (uiobj))
  
--#ifndef uint8_t
--#define uint8_t		u_int8_t
--#endif
--	
--#ifndef uint16_t
--#define uint16_t	u_int16_t
--#endif
--
--#ifndef uint32_t
--#define uint32_t	u_int32_t
--#endif
--
++#ifndef __SunOS
+ #ifndef uint8_t
+ #define uint8_t		u_int8_t
+ #endif
+@@ -43,6 +44,7 @@ extern "C" {
+ #ifndef uint32_t
+ #define uint32_t	u_int32_t
+ #endif
++#endif
+ 
  #ifdef __cplusplus
  }
- #endif /* __cplusplus */
