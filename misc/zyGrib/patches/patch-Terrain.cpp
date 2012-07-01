303b3d50cac75416290031c13740aa5e2e00ef2a
plunky <plunky@pkgsrc.org>
2012-07-01 07:21:35 +0000

    update to zyGrib 5.1.4
    2012-06-03 : zyGrib Version 5.1.4
    
        New translation: Finnish (paljon kiitoksia Kari Salovaara).
        Bug fixed: memory leak while saving images of animations.
        User can choose the type of wave arrows.

--- misc/zyGrib/patches/patch-Terrain.cpp
+++ misc/zyGrib/patches/patch-Terrain.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-Terrain.cpp,v 1.1 2012/06/15 18:56:58 joerg Exp $
+$NetBSD: patch-Terrain.cpp,v 1.2 2012/07/01 07:21:35 plunky Exp $
 
---- Terrain.cpp.orig	2012-06-15 13:44:42.000000000 +0000
+--- Terrain.cpp.orig	2012-06-03 21:03:41.000000000 +0000
 +++ Terrain.cpp
-@@ -1024,7 +1024,7 @@ void Terrain::paintEvent(QPaintEvent * /
+@@ -1032,7 +1032,7 @@ void Terrain::paintEvent(QPaintEvent * /
              
              if (showOrthodromie)
              {
