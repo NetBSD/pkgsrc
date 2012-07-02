adbd925544704aa39d10c5f75063c7217ce7477e
adam <adam@pkgsrc.org>
2012-07-02 07:02:25 +0000

    Changes 1.50:0 New Libraries * Algorithm: A collection of useful generic algorithms, from Marshall Clow.   This includes several different searching algorithms, as well as most of the   new algorithms from the C++11 standard library * Functional/OverloadedFunction: Overload different functions into a single   function object, from Lorenzo Caminiti. * LocalFunction: Program functions locally, within other functions, directly   within the scope where they are needed, from Lorenzo Caminiti. * Utility/IdentityType: Wrap types within round parenthesis so they can always   be passed as macro parameters, from Lorenzo Caminiti. More http://www.boost.org/users/history/version_1_50_0.html

--- devel/boost-jam/buildlink3.mk
+++ devel/boost-jam/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.17 2012/03/18 12:02:41 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2012/07/02 07:02:25 adam Exp $
 
 BUILDLINK_TREE+=	boost-jam
 
 .if !defined(BOOST_JAM_BUILDLINK3_MK)
 BOOST_JAM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.49.*
+BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.50.*
 BUILDLINK_DEPMETHOD.boost-jam?=		build
 BUILDLINK_PKGSRCDIR.boost-jam?=		../../devel/boost-jam
 .endif # BOOST_JAM_BUILDLINK3_MK
--- meta-pkgs/boost/patches/patch-libs_signals_src_named_slot_map.cpp
+++ meta-pkgs/boost/patches/patch-libs_signals_src_named_slot_map.cpp
@@ -1,3 +1,5 @@
+$NetBSD: patch-libs_signals_src_named_slot_map.cpp,v 1.2 2012/07/02 07:02:26 adam Exp $
+
 --- libs/signals/src/named_slot_map.cpp.orig	2009-11-08 02:48:37.000000000 +0100
 +++ libs/signals/src/named_slot_map.cpp	2011-10-24 16:04:24.749105300 +0200
 @@ -102,7 +102,7 @@ void named_slot_map::disconnect(const st
