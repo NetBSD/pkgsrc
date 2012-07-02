d477764739a951b87493b87bfcd6e35c7d111dc4
drochner <drochner@pkgsrc.org>
2012-07-02 18:53:02 +0000

    update to 3.0,20 This switches to the new stable release branch.

--- security/gnutls/buildlink3.mk
+++ security/gnutls/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.27 2011/07/11 16:10:29 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2012/07/02 18:53:02 drochner Exp $
 
 BUILDLINK_TREE+=	gnutls
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gnutls
 GNUTLS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gnutls+=	gnutls>=2.12.3
-BUILDLINK_ABI_DEPENDS.gnutls+=	gnutls>=2.12.3
+BUILDLINK_ABI_DEPENDS.gnutls+=	gnutls>=3.0.17
 BUILDLINK_PKGSRCDIR.gnutls?=	../../security/gnutls
 
 .include "../../archivers/lzo/buildlink3.mk"
