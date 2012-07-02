d8655f97be9de9096130dd6d24ac7ed27d3a0b1e
adam <adam@pkgsrc.org>
2012-07-02 07:02:25 +0000

    Changes 1.50:0 New Libraries * Algorithm: A collection of useful generic algorithms, from Marshall Clow.   This includes several different searching algorithms, as well as most of the   new algorithms from the C++11 standard library * Functional/OverloadedFunction: Overload different functions into a single   function object, from Lorenzo Caminiti. * LocalFunction: Program functions locally, within other functions, directly   within the scope where they are needed, from Lorenzo Caminiti. * Utility/IdentityType: Wrap types within round parenthesis so they can always   be passed as macro parameters, from Lorenzo Caminiti. More http://www.boost.org/users/history/version_1_50_0.html

--- devel/boost-build/buildlink3.mk
+++ devel/boost-build/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.21 2012/02/29 14:22:23 hans Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2012/07/02 07:02:25 adam Exp $
 
 BUILDLINK_TREE+=	boost-build
 
 .if !defined(BOOST_BUILD_BUILDLINK3_MK)
 BOOST_BUILD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.49.*
+BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.50.*
 BUILDLINK_DEPMETHOD.boost-build?=	build
 BUILDLINK_PKGSRCDIR.boost-build?=	../../devel/boost-build
 .endif # BOOST_BUILD_BUILDLINK3_MK
