866b730e71c87e850a31e2f2552256f17717e9aa
adam <adam@pkgsrc.org>
2012-07-02 13:37:35 +0000

    Revbump after updating boost

--- ham/usrp/buildlink3.mk
+++ ham/usrp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2012/05/07 01:53:38 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2012/07/02 13:37:40 adam Exp $
 
 BUILDLINK_TREE+=	usrp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	usrp
 USRP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.usrp+=	usrp>=3.0.4
-BUILDLINK_ABI_DEPENDS.usrp+=	usrp>=3.3.0nb5
+BUILDLINK_ABI_DEPENDS.usrp+=	usrp>=3.3.0nb6
 BUILDLINK_PKGSRCDIR.usrp?=	../../ham/usrp
 
 .include "../../ham/gnuradio-core/buildlink3.mk"
