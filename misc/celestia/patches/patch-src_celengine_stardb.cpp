5daaaadebf8630d74e1ad680f507f13e86cb6784
joerg <joerg@pkgsrc.org>
2012-07-03 17:40:35 +0000

    Don't use references to temporary objects.

--- misc/celestia/patches/patch-src_celengine_stardb.cpp
+++ misc/celestia/patches/patch-src_celengine_stardb.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_celengine_stardb.cpp,v 1.1 2011/11/25 22:08:30 joerg Exp $
+$NetBSD: patch-src_celengine_stardb.cpp,v 1.2 2012/07/03 17:40:36 joerg Exp $
 
---- src/celengine/stardb.cpp.orig	2011-11-25 16:27:06.000000000 +0000
+--- src/celengine/stardb.cpp.orig	2005-11-18 07:53:39.000000000 +0000
 +++ src/celengine/stardb.cpp
 @@ -10,6 +10,7 @@
  #include <cmath>
