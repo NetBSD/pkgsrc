551582198b9c3f0f16f9ba8c12dc198ea5b2489b
joerg <joerg@pkgsrc.org>
2012-07-03 17:40:35 +0000

    Don't use references to temporary objects.

--- misc/celestia/patches/patch-src_celestia_eclipsefinder.cpp
+++ misc/celestia/patches/patch-src_celestia_eclipsefinder.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_celestia_eclipsefinder.cpp,v 1.1 2011/11/25 22:08:30 joerg Exp $
+$NetBSD: patch-src_celestia_eclipsefinder.cpp,v 1.2 2012/07/03 17:40:36 joerg Exp $
 
---- src/celestia/eclipsefinder.cpp.orig	2011-11-25 16:35:03.000000000 +0000
+--- src/celestia/eclipsefinder.cpp.orig	2004-09-09 06:34:50.000000000 +0000
 +++ src/celestia/eclipsefinder.cpp
 @@ -10,6 +10,7 @@
  // as published by the Free Software Foundation; either version 2
