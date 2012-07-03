e7d5153e735e4cc53fcabc6188b8fe008d42f1a1
mef <mef@pkgsrc.org>
2012-07-03 11:31:35 +0000

    Bump version 1.6.3 to 1.8.1.1 based on PR pkg/46650
     Only the most recent Changes and New features are included.
     Others are listed date only, please refer following URL.
       http://www.stack.nl/~dimitri/doxygen/changelog.html
    
     Release 1.8.1.1 (2012/06/10)
      Changes
       - Integrated the root navigation tree in navtree.js for faster loading.
       - When the navigation tree is enabled, clicking jump to a line in
       - the source code view will now highlight the line.
      New features
       - VHDL code now has a new Design Overview page (thanks for Martin Kreis
         for the patch). Requires HAVE_DOT=YES, and DOT_IMAGE_FORMAT=svg.
       - id 677678: Added support for strong and weak attributes in Objective-C
          properties.
    
     Release 1.8.1   (2012/05/19)
     Release 1.8.0   (2012/02/25)
    
     Release 1.7.6.1 (2011/12/10)
     Release 1.7.6   (2011/12/03)
    
     Release 1.7.5.1 (2011/08/21)
     Release 1.7.5   (2011/08/14)
     Release 1.7.4   (2011/03/28)
     Release 1.7.3   (2011/01/03)
     Release 1.7.2   (2010/10/09)
     Release 1.7.1   (2010/06/25)
     Release 1.7.0   (2010/06/15)
    
    (Following patches are deleted, for upstream took the changes)
     patch-ai
     patch-ak
     patch-qtools-qvaluestack.h

--- devel/doxygen/options.mk
+++ devel/doxygen/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2011/10/26 21:44:55 minskim Exp $
+# $NetBSD: options.mk,v 1.5 2012/07/03 11:31:35 mef Exp $
 #
 PKG_OPTIONS_VAR=	PKG_OPTIONS.doxygen
 PKG_SUPPORTED_OPTIONS=	latex
@@ -21,18 +21,24 @@ BUILD_DEPENDS+=	tex-tocloft-[0-9]*:../../print/tex-tocloft
 DEPENDS+=	dvipsk-[0-9]*:../../print/dvipsk
 DEPENDS+=	makeindexk-[0-9]*:../../textproc/makeindexk
 DEPENDS+=	tex-a4wide>=2010nb1:../../print/tex-a4wide
+DEPENDS+=	tex-colortbl-[0-9]*:../../print/tex-colortbl
 DEPENDS+=	tex-ec-[0-9]*:../../fonts/tex-ec
 DEPENDS+=	tex-epstopdf-[0-9]*:../../graphics/tex-epstopdf
 DEPENDS+=	tex-fancyhdr-[0-9]*:../../print/tex-fancyhdr
 DEPENDS+=	tex-float-[0-9]*:../../print/tex-float
+DEPENDS+=	tex-geometry-[0-9]*:../../print/tex-geometry
 DEPENDS+=	tex-graphics-[0-9]*:../../print/tex-graphics
 DEPENDS+=	tex-hyperref-[0-9]*:../../print/tex-hyperref
 DEPENDS+=	tex-latex-[0-9]*:../../print/tex-latex
 DEPENDS+=	tex-latex-bin-[0-9]*:../../print/tex-latex-bin
 DEPENDS+=	tex-listings-[0-9]*:../../print/tex-listings
+DEPENDS+=	tex-natbib-[0-9]*:../../print/tex-natbib
 DEPENDS+=	tex-oberdiek-[0-9]*:../../print/tex-oberdiek
 DEPENDS+=	tex-psnfss>=9.2anb2:../../fonts/tex-psnfss
 DEPENDS+=	tex-pspicture-[0-9]*:../../print/tex-pspicture
+DEPENDS+=	tex-rsfs-[0-9]*:../../fonts/tex-rsfs
+DEPENDS+=	tex-sectsty-[0-9]*:../../print/tex-sectsty
 DEPENDS+=	tex-tools-[0-9]*:../../print/tex-tools
+DEPENDS+=	tex-xcolor-[0-9]*:../../print/tex-xcolor
 
 .endif
