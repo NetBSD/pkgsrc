797e5d98938ea0fb57f8eed24f7eba4ccb9bab8a
drochner <drochner@pkgsrc.org>
2012-07-04 20:17:31 +0000

    change bl3 path to "poppler016"

--- print/poppler-qt/buildlink3.mk
+++ print/poppler-qt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2012/05/07 01:53:55 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2012/07/04 20:17:31 drochner Exp $
 
 BUILDLINK_TREE+=	poppler-qt
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.poppler-qt+=	poppler-qt>=0.4.5nb1
 BUILDLINK_ABI_DEPENDS.poppler-qt+=	poppler-qt>=0.16.7
 BUILDLINK_PKGSRCDIR.poppler-qt?=	../../print/poppler-qt
 
-.include "../../print/poppler/buildlink3.mk"
+.include "../../print/poppler016/buildlink3.mk"
 .endif # POPPLER_QT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-poppler-qt
