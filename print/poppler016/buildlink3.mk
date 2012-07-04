af135d011a10c9459146c0be138d6a29d1277090
drochner <drochner@pkgsrc.org>
2012-07-04 20:15:45 +0000

    import the current poppler base pkg as "poppler016" This is the last version which supports the qt3 frontend, needed by KDE3 (until it switches to "trinity" which seems to maintain a qt3 frontend for newer poppler versions)

---
+++ print/poppler016/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/07/04 20:15:46 drochner Exp $
+
+BUILDLINK_TREE+=	poppler
+
+.if !defined(POPPLER_BUILDLINK3_MK)
+POPPLER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.16.7
+BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler016
+
+pkgbase := poppler
+.include "../../mk/pkg-build-options.mk"
+.if !empty(PKG_BUILD_OPTIONS.poppler:Mpoppler-cms)
+.include "../../graphics/lcms/buildlink3.mk"
+.endif
+
+.include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.endif # POPPLER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-poppler
