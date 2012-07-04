0dbd1804de043d0d96a482bca3ba5a7e9074939c
drochner <drochner@pkgsrc.org>
2012-07-04 20:26:58 +0000

    sync w/ base pkg update

--- print/poppler-includes/buildlink3.mk
+++ print/poppler-includes/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2011/03/09 13:06:49 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2012/07/04 20:26:58 drochner Exp $
 
 BUILDLINK_DEPMETHOD.poppler-includes?=	build
 
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	poppler-includes
 .if !defined(POPPLER_INCLUDES_BUILDLINK3_MK)
 POPPLER_INCLUDES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.poppler-includes+=	poppler-includes>=0.16.3
+BUILDLINK_API_DEPENDS.poppler-includes+=	poppler-includes>=0.20.1
 BUILDLINK_PKGSRCDIR.poppler-includes?=	../../print/poppler-includes
 .endif # POPPLER_INCLUDES_BUILDLINK3_MK
 
