356082e7b6cf2af668aa5ecf8d22feada4e7be76
drochner <drochner@pkgsrc.org>
2012-07-05 10:30:58 +0000

    fix build with gnutls-3 (inspired by gentoo patch for wmbiff-0.4.27)

--- mail/wmbiff/patches/patch-wmbiff_gnutls-common.c
+++ mail/wmbiff/patches/patch-wmbiff_gnutls-common.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-wmbiff_gnutls-common.c,v 1.1 2012/07/03 10:16:20 tron Exp $
+$NetBSD: patch-wmbiff_gnutls-common.c,v 1.2 2012/07/05 10:30:58 drochner Exp $
 
 Fix build with GnuTLS 3.0.
 
---- wmbiff/gnutls-common.c.orig	2004-06-23 03:52:42.000000000 +0100
-+++ wmbiff/gnutls-common.c	2012-07-03 10:56:31.000000000 +0100
+--- wmbiff/gnutls-common.c.orig	2004-06-23 02:52:42.000000000 +0000
++++ wmbiff/gnutls-common.c
 @@ -3,7 +3,6 @@
  #include <stdlib.h>
  #include <string.h>
@@ -12,7 +12,18 @@ Fix build with GnuTLS 3.0.
  #include <gnutls/x509.h>
  #include <gnutls/openpgp.h>
  #include <time.h>
-@@ -508,7 +507,6 @@
+@@ -334,8 +333,8 @@ void print_openpgp_info(gnutls_session s
+ void print_cert_vrfy(gnutls_session session)
+ {
+ 
+-	int status;
+-	status = gnutls_certificate_verify_peers(session);
++	int verify_ret, status;
++	verify_ret = gnutls_certificate_verify_peers2(session, &status);
+ 	printf("\n");
+ 
+ 	if (status == GNUTLS_E_NO_CERTIFICATE_FOUND) {
+@@ -508,7 +507,6 @@ void print_list(void)
  
  	printf("Compression methods:");
  	printf(" ZLIB");
@@ -20,7 +31,7 @@ Fix build with GnuTLS 3.0.
  	printf(", NULL\n");
  }
  
-@@ -636,8 +634,6 @@
+@@ -636,8 +634,6 @@ void parse_comp(char **comp, int ncomp, 
  				comp_priority[j++] = GNUTLS_COMP_NULL;
  			if (strncasecmp(comp[i], "ZLI", 3) == 0)
  				comp_priority[j++] = GNUTLS_COMP_ZLIB;
