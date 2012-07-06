11f0be0f920a7e3ecec6f439257098a8132fad64
wiz <wiz>
2012-07-06 09:29:14 +0000

    Update to 3.5.1, from Jaap Boender. 6 years of changes. 3.5.1 in particular has: it is time for another version of Ocamlnet. The new release 3.5 focuses on the system interface, and includes a long list of smaller improvements.
    The system interface, Netsys_posix, is extended by:
     - Support for POSIX clocks and POSIX timers (with nanosecond resolution)
     - Netsys_posix.spawn usses now the posix_spawn call if present on the OS
     - Adding support for pollable events (as e.g. provided by Linux via
       eventfd). For other OS an emulation is available.
     - Support for epoll on Linux
    
    Note that Netsys_posix covers now large parts of POSIX realtime.
    
    Other improvements:
    
     - The code generator for XDR has been improved. A new switch -direct for
       ocamlrpcgen can be used to generate direct mappings between OCaml values
       and binary representation (in many cases). Speedups up to 50% are
       possible for large XDR values.
     - The new module Uq_mt allows it to access an event-driven resource from
       several kernel threads (e.g. use an RPC client commonly from several
       threads).
     - The thread-safety of Netplex container functions has been improved.
     - Netmulticore condition variables can now be polled, for better
       integration into event-based programs.
     - Option greedy_accepts for Netplex to support servers that accept
       many connections per second. With this improvement, Netplex can
       now accept more than 5000 connections/s, and assign them to worker
       processes.
    
    Last but not least there is now a new tutorial for Equeue (event systems
    and engines). In particular, the section about combining Ocamlnet with Lwt
    might be interesting.

--- net/ocamlnet/buildlink3.mk
+++ net/ocamlnet/buildlink3.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/06/04 13:12:40 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/07/06 09:29:14 wiz Exp $
 
 BUILDLINK_TREE+=	ocamlnet
 
 .if !defined(OCAMLNET_BUILDLINK3_MK)
 OCAMLNET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocamlnet+=	ocamlnet>=1.1.2nb1
+BUILDLINK_API_DEPENDS.ocamlnet+=	ocamlnet>=3.5.1
 BUILDLINK_PKGSRCDIR.ocamlnet?=	../../net/ocamlnet
 
-.include "../../lang/ocaml/buildlink3.mk"
 .endif	# OCAMLNET_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ocamlnet
