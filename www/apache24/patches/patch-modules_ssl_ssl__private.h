2d338f0682344dd1a538aae26b585b096ca732db
tron <tron>
2012-07-07 17:59:51 +0000

    Pullup ticket #3853 - requested by ryoon www/apache24: bug fix
    Revisions pulled up:
    - www/apache24/Makefile                                         1.8
    - www/apache24/distinfo                                         1.4
    - www/apache24/patches/patch-modules_ssl_ssl__private.h         1.1
    
    ---
       Module Name:	pkgsrc
       Committed By:	ryoon
       Date:		Thu Jul  5 12:50:44 UTC 2012
    
       Modified Files:
       	pkgsrc/www/apache24: Makefile distinfo
       Added Files:
       	pkgsrc/www/apache24/patches: patch-modules_ssl_ssl__private.h
    
       Log Message:
       Bump PKGREVISION
    
       Fix PR pkg/46655.
    
       With NetBSD current and 6.0's OpenSSL, OPENSSL_NO_SSL_INTERN should not
       be defined, due to it lacks some functions.
       Exclude version 0x10001000 from OPENSSL_NO_SSL_INTERN definition.

---
+++ www/apache24/patches/patch-modules_ssl_ssl__private.h
@@ -0,0 +1,16 @@
+$NetBSD: patch-modules_ssl_ssl__private.h,v 1.2.2.2 2012/07/07 17:59:52 tron Exp $
+
+Exclude OpenSSL shipped with NetBSD current and 6.0.
+It is pre-beta snapshot of 1.0.1.
+
+--- modules/ssl/ssl_private.h.orig	2012-04-03 12:37:57.000000000 +0000
++++ modules/ssl/ssl_private.h
+@@ -83,7 +83,7 @@
+ 
+ /* OpenSSL headers */
+ #include <openssl/opensslv.h>
+-#if (OPENSSL_VERSION_NUMBER >= 0x10001000)
++#if (OPENSSL_VERSION_NUMBER >= 0x10001001)
+ /* must be defined before including ssl.h */
+ #define OPENSSL_NO_SSL_INTERN
+ #endif
