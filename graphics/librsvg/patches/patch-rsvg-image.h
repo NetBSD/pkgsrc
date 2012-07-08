83a7c912fa94be359964aed986079a356a47dc18
wiz <wiz@pkgsrc.org>
2012-07-08 21:54:04 +0000

    Add upstream bug report.

--- graphics/librsvg/patches/patch-rsvg-image.h
+++ graphics/librsvg/patches/patch-rsvg-image.h
@@ -1,7 +1,8 @@
-$NetBSD: patch-rsvg-image.h,v 1.1 2012/07/08 21:20:08 wiz Exp $
+$NetBSD: patch-rsvg-image.h,v 1.2 2012/07/08 21:54:04 wiz Exp $
 
 rsvg_get_file_path is an externally visible function.
 Fixes NetBSD-5.1/amd64 build.
+https://bugzilla.gnome.org/show_bug.cgi?id=679603
 
 --- rsvg-image.h.orig	2012-02-06 19:44:12.000000000 +0000
 +++ rsvg-image.h
