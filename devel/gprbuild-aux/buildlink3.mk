5c77d966d2f17690604f8d33037a9320b66a4742
marino <marino@pkgsrc.org>
2012-07-08 20:08:42 +0000

    devel/gprbuild-aux: Upgrade to version GPL 2012
    1) Replace custom tarball with Adacore's GPL 2012 release
    2) Update license to GPLv3
    3) Remove lang/gnat-aux dependency
    4) USE_LANGUAGES+= ada (invokes lang/gcc-aux dependency)
    5) Update buildlink3.mk to require GPL 2012 version
    6) Restore use of pkgsrc wrappers
    7) Add Library_Option "-R" support which ignores all automatically
       generated rpaths* which point to the WRKOBJDIR.  DESTDIR isn't
       supported by GPRBuild, so this rpath disabling is necessary to
       avoid references to work directories.
       * gcc and adalib rpaths are excluded with -R option.
    8) This Library_Option "-R" matches the same modification added to
       lang/gcc-aux and lang/gnat-aux's project handler.
    9) This modification is required to fix www/aws rpath issues which
       appears with recent changes to PKG_DEVELOPER=yes checks.

--- devel/gprbuild-aux/buildlink3.mk
+++ devel/gprbuild-aux/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/02/03 19:04:02 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/07/08 20:08:42 marino Exp $
 
 BUILDLINK_TREE+=	gprbuild-aux
 
 .if !defined(GPRBUILD_AUX_BUILDLINK3_MK)
 GPRBUILD_AUX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gprbuild-aux+=	gprbuild-aux>=20101120
+BUILDLINK_API_DEPENDS.gprbuild-aux+=	gprbuild-aux>=20120510
 BUILDLINK_PKGSRCDIR.gprbuild-aux?=	../../devel/gprbuild-aux
 
 .include "../../textproc/xmlada/buildlink3.mk"
