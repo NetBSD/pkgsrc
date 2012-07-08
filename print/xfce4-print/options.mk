b9d15fe0896108354b4d3aea45ded5c57f562c5e
sbd <sbd@pkgsrc.org>
2012-07-08 07:45:25 +0000

    1) Add 'bsdlpr' option.
    2) Make bsdlpr and cups options the members of a PKG_OPTIONS_NONEMPTY_SETS.
       (Because it is an error to build xfce4-print without lpr and cups support.)
    
    3) On SunOS and Linux make cups the suggested option, otherwise make
       bsdlpr the suggested options.
    
    Bump PKGREVISION.

--- print/xfce4-print/options.mk
+++ print/xfce4-print/options.mk
@@ -1,9 +1,26 @@
-# $NetBSD: options.mk,v 1.5 2008/04/12 22:43:09 jlam Exp $
+# $NetBSD: options.mk,v 1.6 2012/07/08 07:45:25 sbd Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xfce4-print
-PKG_SUPPORTED_OPTIONS=	cups
+
+PKG_OPTIONS_NONEMPTY_SETS+=		printing-system
+PKG_OPTIONS_SET.printing-system=	bsdlpr cups
+
+.include "../../mk/bsd.prefs.mk"
+
+.if ${OPSYS} == "SunOS" || ${OPSYS} == "Linux"
+PKG_SUGGESTED_OPTIONS+=	cups
+.else
+PKG_SUGGESTED_OPTIONS+=	bsdlpr
+.endif
 
 .include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=    bsdlpr
+.if !empty(PKG_OPTIONS:Mbsdlpr)
+PLIST.bsdlpr=   yes
+.else
+CONFIGURE_ARGS+=        --disable-bsdlpr
+.endif
 
 PLIST_VARS+=		cups
 .if !empty(PKG_OPTIONS:Mcups)
