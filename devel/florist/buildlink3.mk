271253b06561916c651a9ab9292fbac52a25bf0e
marino <marino@pkgsrc.org>
2012-07-09 05:52:14 +0000

    devel/florist: Upgrade from version 3.15p to GPL 2012
    From its beginning, Florist required a gcc 3.x series Ada compiler to build,
    and this compiler hasn't been building for most platforms for quite some
    time.  With the recent import of lang/gcc-aux which accepts
    USE_LANGUAGES+= ada, it's possible to build Florist again.
    
    1) Update to Adacore-maintained version, GPL 2012
    2) Update the license (GPLv3)
    
    The time elapsed between versions is approximately 12 years, but no
    Changelog exists to show what has been updated.

--- devel/florist/buildlink3.mk
+++ devel/florist/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2012/05/07 01:53:21 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2012/07/09 05:52:14 marino Exp $
 
 BUILDLINK_TREE+=	florist
 
 .if !defined(FLORIST_BUILDLINK3_MK)
 FLORIST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.florist+=	florist>=3.15p
-BUILDLINK_ABI_DEPENDS.florist+=	florist>=3.15pnb2
+BUILDLINK_API_DEPENDS.florist+=	florist>=2012
+BUILDLINK_ABI_DEPENDS.florist+=	florist>=2012
 BUILDLINK_PKGSRCDIR.florist?=	../../devel/florist
 .endif # FLORIST_BUILDLINK3_MK
 
