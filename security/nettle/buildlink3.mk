033abaddacd3026bb3b1c4699ed350cd15fdc620
chs <chs@pkgsrc.org>
2012-07-09 04:50:12 +0000

    update required version to 2.4, the new gnutls needs it.

--- security/nettle/buildlink3.mk
+++ security/nettle/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/04/26 09:54:55 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/07/09 04:50:12 chs Exp $
 
 BUILDLINK_TREE+=	libnettle
 
 .if !defined(LIBNETTLE_BUILDLINK3_MK)
 LIBNETTLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libnettle+=	nettle>=2.1
+BUILDLINK_API_DEPENDS.libnettle+=	nettle>=2.4
 BUILDLINK_PKGSRCDIR.libnettle?=		../../security/nettle
 
 .include "../../devel/gmp/buildlink3.mk"
