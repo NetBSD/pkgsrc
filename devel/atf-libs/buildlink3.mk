37de68c420295f30625048b2de7659ba4b8e3dc2
jmmv <jmmv@pkgsrc.org>
2012-07-10 22:02:59 +0000

    Update to ATF 0.16:
    Experimental version released on July 10th, 2012.
    
    * Added a --enable-tools flag to configure to request the build of the
      deprecated ATF tools, whose build is now disabled by default.  In order
      to continue running tests, you should migrate to Kyua instead of enabling
      the build of the deprecated tools.  The kyua-atf-compat package provides
      transitional compatibility versions of atf-run and atf-report built on
      top of Kyua.
    
    * Tweaked the ATF_TEST_CASE macro of atf-c++ so that the compiler can
      detect defined but unused test cases.
    
    * PR bin/45859: Fixed some XSLT bugs that resulted in the tc-time and
      tp-time XML tags leaking into the generated HTML file.  Also improved
      the CSS file slightly to correct alignment and color issues with the
      timestamps column.
    
    * Optimized atf-c++/macros.hpp so that GNU G++ consumes less memory during
      compilation with GNU G++.
    
    * Flipped the default to building shared libraries for atf-c and atf-c++,
      and started versioning them.  As a side-effect, this removes the
      --enable-unstable-shared flag from configure that appears to not work any
      more (under NetBSD).  Additionally, some distributions require the use of
      shared libraries for proper dependency tracking (e.g. Fedora), so it is
      better if we do the right versioning upstream.
    
    * Project hosting moved from an adhoc solution (custom web site and
      Monotone repository) to Google Code (standard wiki and Git).  ATF now
      lives in a subcomponent of the Kyua project.

--- devel/atf-libs/buildlink3.mk
+++ devel/atf-libs/buildlink3.mk
@@ -1,18 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/07/01 15:27:43 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/07/10 22:02:59 jmmv Exp $
 
 BUILDLINK_TREE+=	atf-libs
 
 .if !defined(ATF_LIBS_BUILDLINK3_MK)
 ATF_LIBS_BUILDLINK3_MK:=
 
-# The ATF libraries have not been properly versioned until now because, by
-# default, they were only installed as static libraries.  However, for some
-# obscure reason, the disabling of shared libraries is not working (at least
-# under NetBSD with pkgsrc).  Make sure we use a single API/ABI of the
-# abi-libs package to prevent unexpected incompatibilities until the ATF
-# libraries provide proper versioning on their own.
-BUILDLINK_API_DEPENDS.atf-libs+=	atf-libs>=0.15
-BUILDLINK_API_DEPENDS.atf-libs+=	atf-libs<0.16
+BUILDLINK_API_DEPENDS.atf-libs+=	atf-libs>=0.16
 BUILDLINK_PKGSRCDIR.atf-libs?=		../../devel/atf-libs
 .endif	# ATF_LIBS_BUILDLINK3_MK
 
