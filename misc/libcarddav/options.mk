6fae77134a46e0a26677605e87e5c21626eb9d7a
wiz <wiz@pkgsrc.org>
2012-07-11 09:25:22 +0000

    Add missing build dependencies when using doxygen-1.8.x. Closes PR 46664 by Makoto Fujiwara using his patch.
    While here, rename option to "doc" instead of unconventional "doxygen".

--- misc/libcarddav/options.mk
+++ misc/libcarddav/options.mk
@@ -1,18 +1,22 @@
-# $NetBSD: options.mk,v 1.1.1.1 2012/01/08 15:18:40 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2012/07/11 09:25:22 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libcarddav
-PKG_SUPPORTED_OPTIONS=	doxygen
-PKG_SUGGESTED_OPTIONS=	doxygen
+PKG_SUPPORTED_OPTIONS=	doc
+PKG_SUGGESTED_OPTIONS=	doc
+PKG_OPTIONS_LEGACY_OPTS+=	doxygen:doc
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	doxygen
+PLIST_VARS+=	doc
 
-.if !empty(PKG_OPTIONS:Mdoxygen)
-BUILD_DEPENDS+=		doxygen-[0-9]*:../../devel/doxygen
+.if !empty(PKG_OPTIONS:Mdoc)
+BUILD_DEPENDS+=		doxygen>=1.8.1.1:../../devel/doxygen
 BUILD_DEPENDS+=		tex-ec-[0-9]*:../../fonts/tex-ec
+BUILD_DEPENDS+=		tex-tocloft-[0-9]*:../../print/tex-tocloft
+# only xtab.sty is necessary from following
+BUILD_DEPENDS+=		teTeX-texmf-[0-9]*:../../print/teTeX3-texmf
 CONFIGURE_ARGS+=	--enable-doc
-PLIST.doxygen=		yes
+PLIST.doc=		yes
 .else
 CONFIGURE_ARGS+=	--disable-doxygen-doc
 .endif
