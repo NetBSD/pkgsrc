ac1c7c655b25dc80689ac111722f8e8e2c444b0c
marino <marino@pkgsrc.org>
2012-07-13 08:15:59 +0000

    news/pan: break caused by NetBSD iconv patch
    There have been several packages with patches that directly change an
    iconv function without the use of a __NetBSD__ macro.  This is a NetBSD-
    specific modification and changing iconv to match the NetBSD prototype
    breaks the package on other platforms.

--- news/pan/patches/patch-pan_usenet-utils_mime-utils.cc
+++ news/pan/patches/patch-pan_usenet-utils_mime-utils.cc
@@ -1,15 +1,18 @@
-$NetBSD: patch-pan_usenet-utils_mime-utils.cc,v 1.1 2012/04/09 23:00:49 wiz Exp $
+$NetBSD: patch-pan_usenet-utils_mime-utils.cc,v 1.2 2012/07/13 08:16:00 marino Exp $
 
 Fix build with NetBSD's iconv().
 
---- pan/usenet-utils/mime-utils.cc.orig	2012-04-08 15:30:36.000000000 +0000
+--- pan/usenet-utils/mime-utils.cc.orig	2012-06-29 22:24:54.000000000 +0000
 +++ pan/usenet-utils/mime-utils.cc
-@@ -77,7 +77,7 @@ namespace pan
+@@ -77,7 +77,11 @@ namespace pan
        outbuf = out + converted;
        outleft = outlen - converted;
  
--      converted = iconv (cd, (char **) &inbuf, &inleft, &outbuf, &outleft);
++#if defined(__NetBSD__)
 +      converted = iconv (cd, (const char **) &inbuf, &inleft, &outbuf, &outleft);
++#else
+       converted = iconv (cd, (char **) &inbuf, &inleft, &outbuf, &outleft);
++#endif
  
        if (converted != (size_t) -1 && errno == 0) {
          /*
