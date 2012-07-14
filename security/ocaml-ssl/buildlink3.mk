f569b4931207f6e8c714c4375caf0c2d918be342
wiz <wiz@pkgsrc.org>
2012-07-14 21:23:49 +0000

    Update to 0.4.6, from Jaap Boender.
    0.4.6 (2011-10-16)
    =====
    * Added write_certificate function.
    * Remove support for SSLv2, which was dropped upstream (thanks Dario Teixeira).
    * Added support for compiling under Win32 (thanks David Allsopp), see
      README.win32.
    * Check for pthreads in configure.
    
    0.4.5 (2011-03-01)
    =====
    * Use pthread mutexes for locking thread-safe version of ssl.
    
    0.4.4 (2010-01-06)
    =====
    * Use SSL_CTX_use_certificate_chain_file instead of
      SSL_CTX_use_certificate_file.
    * Added support for --enable-debugging configure option.
    * Don't link with unix library and don't build in custom mode.
    
    0.4.3 (2008-12-18)
    =====
    * Don't use blocking sections in finalizers since it causes segfaults (thanks
      Grégoire Henry and Stéphane Glondu).

--- security/ocaml-ssl/buildlink3.mk
+++ security/ocaml-ssl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2012/05/07 01:53:57 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2012/07/14 21:23:49 wiz Exp $
 
 BUILDLINK_TREE+=	ocaml-ssl
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.4.0
 BUILDLINK_ABI_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.4.2nb1
 BUILDLINK_PKGSRCDIR.ocaml-ssl?=	../../security/ocaml-ssl
 
+.include "../../lang/ocaml/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
 .endif # OCAML_SSL_BUILDLINK3_MK
 
