0747ea34a6bef10d7927ddfbb769a482ad37a4bb
wiz <wiz@pkgsrc.org>
2012-07-15 08:22:46 +0000

    Recursive bump for startup-notification* dependency change, requested by Obache.

--- devel/libwnck/buildlink3.mk
+++ devel/libwnck/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.28 2012/05/07 01:53:25 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.29 2012/07/15 08:22:48 wiz Exp $
 
 BUILDLINK_TREE+=	libwnck
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libwnck
 LIBWNCK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libwnck+=	libwnck>=2.20.0
-BUILDLINK_ABI_DEPENDS.libwnck+=	libwnck>=2.30.6nb5
+BUILDLINK_ABI_DEPENDS.libwnck+=	libwnck>=2.30.6nb6
 BUILDLINK_PKGSRCDIR.libwnck?=	../../devel/libwnck
 
 .include "../../devel/gettext-lib/buildlink3.mk"
