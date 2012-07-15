89d6a6453c73a16bd92c205aa3875c264ecc020d
jnemeth <jnemeth@pkgsrc.org>
2012-07-15 07:51:02 +0000

    Update to Asterisk 10.6.0:  this is a bugfix release
    The Asterisk Development Team has announced the release of Asterisk 10.6.0.
    
    The release of Asterisk 10.6.0 resolves several issues reported by the
    community and would have not been possible without your participation.
    Thank you!
    
    The following is a sample of the issues resolved in this release:
    
    * --- format_mp3: Fix a possible crash in mp3_read().
    
    * --- Fix local channel chains optimizing themselves out of a call.
    
    * --- Re-add LastMsgsSent value for SIP peers
    
    * --- Prevent sip_pvt refleak when an ast_channel outlasts its
          corresponding sip_pvt.
    
    * --- Send more accurate identification information in dialog-info SIP
          NOTIFYs.
    
    For a full list of changes in this release, please see the ChangeLog:
    
    http://downloads.asterisk.org/pub/telephony/asterisk/ChangeLog-10.6.0
    
    Thank you for your continued support of Asterisk!

--- comms/asterisk10/patches/patch-main_features.c
+++ comms/asterisk10/patches/patch-main_features.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-main_features.c,v 1.2 2012/04/07 20:05:57 jnemeth Exp $
+$NetBSD: patch-main_features.c,v 1.3 2012/07/15 07:51:02 jnemeth Exp $
 
---- main/features.c.orig	2012-02-27 16:05:24.000000000 +0000
+--- main/features.c.orig	2012-07-05 23:05:44.000000000 +0000
 +++ main/features.c
-@@ -1493,14 +1493,14 @@ static int park_call_full(struct ast_cha
+@@ -1547,14 +1547,14 @@ static int park_call_full(struct ast_cha
  		"Channel: %s\r\n"
  		"Parkinglot: %s\r\n"
  		"From: %s\r\n"
@@ -19,43 +19,43 @@ $NetBSD: patch-main_features.c,v 1.2 2012/04/07 20:05:57 jnemeth Exp $
  		S_COR(chan->caller.id.number.valid, chan->caller.id.number.str, "<unknown>"),
  		S_COR(chan->caller.id.name.valid, chan->caller.id.name.str, "<unknown>"),
  		S_COR(chan->connected.id.number.valid, chan->connected.id.number.str, "<unknown>"),
-@@ -2057,7 +2057,7 @@ static int builtin_automonitor(struct as
- 			len = strlen(touch_monitor) + 50;
- 			args = alloca(len);
- 			touch_filename = alloca(len);
--			snprintf(touch_filename, len, "%s-%ld-%s", S_OR(touch_monitor_prefix, "auto"), (long)time(NULL), touch_monitor);
-+			snprintf(touch_filename, len, "%s-%jd-%s", S_OR(touch_monitor_prefix, "auto"), (intmax_t)time(NULL), touch_monitor);
- 			snprintf(args, len, "%s,%s,m", S_OR(touch_format, "wav"), touch_filename);
- 		} else {
- 			caller_chan_id = ast_strdupa(S_COR(caller_chan->caller.id.number.valid,
-@@ -2067,7 +2067,7 @@ static int builtin_automonitor(struct as
- 			len = strlen(caller_chan_id) + strlen(callee_chan_id) + 50;
- 			args = alloca(len);
- 			touch_filename = alloca(len);
--			snprintf(touch_filename, len, "%s-%ld-%s-%s", S_OR(touch_monitor_prefix, "auto"), (long)time(NULL), caller_chan_id, callee_chan_id);
-+			snprintf(touch_filename, len, "%s-%jd-%s-%s", S_OR(touch_monitor_prefix, "auto"), (intmax_t)time(NULL), caller_chan_id, callee_chan_id);
- 			snprintf(args, len, "%s,%s,m", S_OR(touch_format, "wav"), touch_filename);
- 		}
+@@ -2113,7 +2113,7 @@ static int builtin_automonitor(struct as
+ 		len = strlen(touch_monitor) + 50;
+ 		args = alloca(len);
+ 		touch_filename = alloca(len);
+-		snprintf(touch_filename, len, "%s-%ld-%s", S_OR(touch_monitor_prefix, "auto"), (long)time(NULL), touch_monitor);
++		snprintf(touch_filename, len, "%s-%jd-%s", S_OR(touch_monitor_prefix, "auto"), (intmax_t)time(NULL), touch_monitor);
+ 		snprintf(args, len, "%s,%s,m", S_OR(touch_format, "wav"), touch_filename);
+ 	} else {
+ 		caller_chan_id = ast_strdupa(S_COR(caller_chan->caller.id.number.valid,
+@@ -2123,7 +2123,7 @@ static int builtin_automonitor(struct as
+ 		len = strlen(caller_chan_id) + strlen(callee_chan_id) + 50;
+ 		args = alloca(len);
+ 		touch_filename = alloca(len);
+-		snprintf(touch_filename, len, "%s-%ld-%s-%s", S_OR(touch_monitor_prefix, "auto"), (long)time(NULL), caller_chan_id, callee_chan_id);
++		snprintf(touch_filename, len, "%s-%jd-%s-%s", S_OR(touch_monitor_prefix, "auto"), (intmax_t)time(NULL), caller_chan_id, callee_chan_id);
+ 		snprintf(args, len, "%s,%s,m", S_OR(touch_format, "wav"), touch_filename);
+ 	}
  
-@@ -2173,7 +2173,7 @@ static int builtin_automixmonitor(struct
- 			len = strlen(touch_monitor) + 50;
- 			args = alloca(len);
- 			touch_filename = alloca(len);
--			snprintf(touch_filename, len, "auto-%ld-%s", (long)time(NULL), touch_monitor);
-+			snprintf(touch_filename, len, "auto-%jd-%s", (intmax_t)time(NULL), touch_monitor);
- 			snprintf(args, len, "%s.%s,b", touch_filename, (touch_format) ? touch_format : "wav");
- 		} else {
- 			caller_chan_id = ast_strdupa(S_COR(caller_chan->caller.id.number.valid,
-@@ -2183,7 +2183,7 @@ static int builtin_automixmonitor(struct
- 			len = strlen(caller_chan_id) + strlen(callee_chan_id) + 50;
- 			args = alloca(len);
- 			touch_filename = alloca(len);
--			snprintf(touch_filename, len, "auto-%ld-%s-%s", (long)time(NULL), caller_chan_id, callee_chan_id);
-+			snprintf(touch_filename, len, "auto-%jd-%s-%s", (intmax_t)time(NULL), caller_chan_id, callee_chan_id);
- 			snprintf(args, len, "%s.%s,b", touch_filename, S_OR(touch_format, "wav"));
- 		}
+@@ -2225,7 +2225,7 @@ static int builtin_automixmonitor(struct
+ 		len = strlen(touch_monitor) + 50;
+ 		args = alloca(len);
+ 		touch_filename = alloca(len);
+-		snprintf(touch_filename, len, "auto-%ld-%s", (long)time(NULL), touch_monitor);
++		snprintf(touch_filename, len, "auto-%jd-%s", (intmax_t)time(NULL), touch_monitor);
+ 		snprintf(args, len, "%s.%s,b", touch_filename, (touch_format) ? touch_format : "wav");
+ 	} else {
+ 		caller_chan_id = ast_strdupa(S_COR(caller_chan->caller.id.number.valid,
+@@ -2235,7 +2235,7 @@ static int builtin_automixmonitor(struct
+ 		len = strlen(caller_chan_id) + strlen(callee_chan_id) + 50;
+ 		args = alloca(len);
+ 		touch_filename = alloca(len);
+-		snprintf(touch_filename, len, "auto-%ld-%s-%s", (long)time(NULL), caller_chan_id, callee_chan_id);
++		snprintf(touch_filename, len, "auto-%jd-%s-%s", (intmax_t)time(NULL), caller_chan_id, callee_chan_id);
+ 		snprintf(args, len, "%s.%s,b", touch_filename, S_OR(touch_format, "wav"));
+ 	}
  
-@@ -6973,10 +6973,10 @@ static char *handle_parkedcalls(struct a
+@@ -7010,10 +7010,10 @@ static char *handle_parkedcalls(struct a
  
  		AST_LIST_LOCK(&curlot->parkings);
  		AST_LIST_TRAVERSE(&curlot->parkings, cur, list) {
@@ -68,7 +68,7 @@ $NetBSD: patch-main_features.c,v 1.2 2012/04/07 20:05:57 jnemeth Exp $
  			++lotparked;
  		}
  		AST_LIST_UNLOCK(&curlot->parkings);
-@@ -7032,7 +7032,7 @@ static int manager_parking_status(struct
+@@ -7069,7 +7069,7 @@ static int manager_parking_status(struct
  				"Exten: %d\r\n"
  				"Channel: %s\r\n"
  				"From: %s\r\n"
@@ -77,7 +77,7 @@ $NetBSD: patch-main_features.c,v 1.2 2012/04/07 20:05:57 jnemeth Exp $
  				"CallerIDNum: %s\r\n"
  				"CallerIDName: %s\r\n"
  				"ConnectedLineNum: %s\r\n"
-@@ -7041,7 +7041,7 @@ static int manager_parking_status(struct
+@@ -7078,7 +7078,7 @@ static int manager_parking_status(struct
  				"\r\n",
  				curlot->name,
  				cur->parkingnum, cur->chan->name, cur->peername,
