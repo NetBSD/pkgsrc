cfaad5891d3fce56b14b4faeaddebd8df0ee70eb
marino <marino@pkgsrc.org>
2012-07-15 11:50:53 +0000

    lang/gcc-aux: Add condition to buildlink3
    The NLS option is optional, but the buildlink pulled in gettext-lib
    unconditionally.  This caused failures in Tinderbox and pbuld chroot
    when gettext-lib couldn't be found in those clean environments.  The
    final result is that all Ada programs file to build in those environments.
    
    Also removed whitespace from DESC.

--- lang/gcc-aux/buildlink3.mk
+++ lang/gcc-aux/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/07/08 19:30:38 marino Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/07/15 11:50:53 marino Exp $
 
 BUILDLINK_TREE+= gcc-aux
 
@@ -9,10 +9,13 @@ BUILDLINK_API_DEPENDS.gcc-aux+= gcc-aux>=20120614
 BUILDLINK_PKGSRCDIR.gcc-aux?= ../../lang/gcc-aux
 
 .include "../../converters/libiconv/buildlink3.mk"
-.include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/gmp/buildlink3.mk"
 .include "../../math/mpfr/buildlink3.mk"
 .include "../../math/mpcomplex/buildlink3.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.gcc-aux:Mnls)
+.include "../../devel/gettext-lib/buildlink3.mk"
+.endif
 .endif
 
 BUILDLINK_TREE+= -gcc-aux
