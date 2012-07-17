30ccee98c3d36f04fd4bc13d1afc2c81a6401f8b
asau <asau@pkgsrc.org>
2012-07-17 21:00:34 +0000

    Update ipa to 2.1.2 From Andrey Simonenko in PR pkg/46712
    Changes:
     * autoconf 2.68 -> 2.69, automake 1.11.1 -> 1.12.2.
     * Corrected compilation with --enable-ctl-creds on NetBSD.
     * Corrected output for "ipactl -a ... status".
     * Corrected work with the -n switch in ipactl.
    
    Package changes:
     * Removed the ipa-memfunc-debug option.

--- sysutils/ipa/options.mk
+++ sysutils/ipa/options.mk
@@ -1,18 +1,11 @@
-# $NetBSD: options.mk,v 1.1 2006/10/18 11:45:22 markd Exp $
+# $NetBSD: options.mk,v 1.2 2012/07/17 21:00:34 asau Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ipa
-PKG_SUPPORTED_OPTIONS=	ipa-memfunc-debug ipa-without-autorules \
+PKG_SUPPORTED_OPTIONS=	ipa-without-autorules \
 			ipa-without-limits ipa-without-rules \
 			ipa-without-sublimits ipa-without-thresholds
 
 .include "../../mk/bsd.options.mk"
-
-###
-### Enable ipa_memfunc debugging
-###
-.if !empty(PKG_OPTIONS:Mipa-memfunc-debug)
-CONFIGURE_ARGS+= --enable-memfunc-debug
-.endif
 
 ###
 ### Disable dynamic rules support
