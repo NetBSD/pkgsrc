e4e4b1935460ba6896b9bf88e6ea66b022539dcd
ryoon <ryoon>
2012-07-18 16:10:06 +0000

    Update to 14.0.1
    Changelog:
    NEW
    Google searches now utilize HTTPS
    NEW
    Full screen support for Mac OS X Lion implemented
    NEW
    Plugins can now be configured to only load on click (requires an about:config change)
    NEW
    The Awesome Bar now auto-completes typed URLs
    CHANGED
    Improved site identity manager, to prevent spoofing of an SSL connection with favicons
    DEVELOPER
    Pointer Lock API implemented
    DEVELOPER
    New API to prevent your display from sleeping
    DEVELOPER
    New text-transform and font-variant CSS improvements for Turkic languages and Greek
    FIXED
    Various security fixes
    FIXED
    GIF animation can gets stuck when src and image size are changed (743598)
    FIXED
    OS X: nsCocoaWindow::ConstrainPosition uses wrong screen in multi-display setup (752149)
    FIXED
    CSS :hover regression when an element's class name is set by Javascript (758885

--- devel/xulrunner/patches/patch-netwerk_protocol_http_HttpChannelParent.cpp
+++ devel/xulrunner/patches/patch-netwerk_protocol_http_HttpChannelParent.cpp
@@ -1,24 +1,21 @@
-$NetBSD: patch-netwerk_protocol_http_HttpChannelParent.cpp,v 1.1 2012/03/22 21:14:24 ryoon Exp $
+$NetBSD: patch-netwerk_protocol_http_HttpChannelParent.cpp,v 1.2 2012/07/18 16:10:06 ryoon Exp $
 
 * Fix build with gcc<4.5, for example, DragonFly 3.0.1.
   See https://bugzilla.mozilla.org/show_bug.cgi?id=621446
 
---- netwerk/protocol/http/HttpChannelParent.cpp.orig	2012-03-13 01:37:10.000000000 +0000
+--- netwerk/protocol/http/HttpChannelParent.cpp.orig	2012-07-04 16:04:06.000000000 +0000
 +++ netwerk/protocol/http/HttpChannelParent.cpp
-@@ -333,10 +333,15 @@ HttpChannelParent::RecvUpdateAssociatedC
-   return true;
- }
- 
--// Bug 621446 investigation, we don't want conditional PR_Aborts bellow to be
-+// Bug 621446 investigation, we don't want conditional PR_Aborts below to be
- // merged to a single address.
- #pragma warning(disable : 4068)
+@@ -340,6 +340,13 @@ HttpChannelParent::RecvUpdateAssociatedC
+ // Compiling this with GCC <= 4.4 fails with an internal compiler error
+ #pragma GCC optimize ("O0")
+ #endif
 +// Compiling with a version of GCC <= 4.4 fails with an internal compiler
 +// error.
 +#if !defined(__GNUC__) || \
 +  (__GNUC__ >= 5) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 5)
- #pragma GCC optimize ("O0")
++#pragma GCC optimize ("O0")
 +#endif
++
  
  bool
  HttpChannelParent::RecvRedirect2Verify(const nsresult& result, 
