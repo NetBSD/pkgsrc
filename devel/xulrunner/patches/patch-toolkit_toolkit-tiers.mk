1a59fa91221699d8234bf2bc07041bdfe271078d
ryoon <ryoon@pkgsrc.org>
2012-07-18 16:10:06 +0000

    Update to 14.0.1
    Changelog:
    NEW
    Google searches now utilize HTTPS
    NEW
    Full screen support for Mac OS X Lion implemented
    NEW
    Plugins can now be configured to only load on click (requires an about:config change)
    NEW
    The Awesome Bar now auto-completes typed URLs
    CHANGED
    Improved site identity manager, to prevent spoofing of an SSL connection with favicons
    DEVELOPER
    Pointer Lock API implemented
    DEVELOPER
    New API to prevent your display from sleeping
    DEVELOPER
    New text-transform and font-variant CSS improvements for Turkic languages and Greek
    FIXED
    Various security fixes
    FIXED
    GIF animation can gets stuck when src and image size are changed (743598)
    FIXED
    OS X: nsCocoaWindow::ConstrainPosition uses wrong screen in multi-display setup (752149)
    FIXED
    CSS :hover regression when an element's class name is set by Javascript (758885

--- devel/xulrunner/patches/patch-toolkit_toolkit-tiers.mk
+++ devel/xulrunner/patches/patch-toolkit_toolkit-tiers.mk
@@ -1,6 +1,6 @@
-$NetBSD: patch-toolkit_toolkit-tiers.mk,v 1.2 2012/04/26 13:30:30 ryoon Exp $
+$NetBSD: patch-toolkit_toolkit-tiers.mk,v 1.3 2012/07/18 16:10:06 ryoon Exp $
 
---- toolkit/toolkit-tiers.mk.orig	2012-04-04 04:25:47.000000000 +0000
+--- toolkit/toolkit-tiers.mk.orig	2012-07-04 16:04:20.000000000 +0000
 +++ toolkit/toolkit-tiers.mk
 @@ -80,7 +80,7 @@ ifdef MOZ_UPDATER
  ifndef MOZ_NATIVE_BZ2
