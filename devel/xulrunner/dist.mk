2fb739265e2499f6d68219bc8ac4f34ab86df73e
ryoon <ryoon@pkgsrc.org>
2012-07-18 16:10:06 +0000

    Update to 14.0.1
    Changelog:
    NEW
    Google searches now utilize HTTPS
    NEW
    Full screen support for Mac OS X Lion implemented
    NEW
    Plugins can now be configured to only load on click (requires an about:config change)
    NEW
    The Awesome Bar now auto-completes typed URLs
    CHANGED
    Improved site identity manager, to prevent spoofing of an SSL connection with favicons
    DEVELOPER
    Pointer Lock API implemented
    DEVELOPER
    New API to prevent your display from sleeping
    DEVELOPER
    New text-transform and font-variant CSS improvements for Turkic languages and Greek
    FIXED
    Various security fixes
    FIXED
    GIF animation can gets stuck when src and image size are changed (743598)
    FIXED
    OS X: nsCocoaWindow::ConstrainPosition uses wrong screen in multi-display setup (752149)
    FIXED
    CSS :hover regression when an element's class name is set by Javascript (758885

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,12 +1,12 @@
-# $NetBSD: dist.mk,v 1.31 2012/06/05 18:09:21 ryoon Exp $
+# $NetBSD: dist.mk,v 1.32 2012/07/18 16:10:06 ryoon Exp $
 #
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	13.0
-MOZ_BRANCH_MINOR=
+MOZ_BRANCH=	14.0.1
+MOZ_BRANCH_MINOR=	
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
