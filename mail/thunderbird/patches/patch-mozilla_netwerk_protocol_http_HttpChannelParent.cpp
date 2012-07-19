61ad26e6bcb9ca88360af52634e0158a2804e341
ryoon <ryoon@pkgsrc.org>
2012-07-19 17:57:30 +0000

    Update to 14.0
    * Use Lightning 1.6 release
    * Enigmail is not tested fully
    
    Changelog: from http://www.mozilla.org/en-US/products/thunderbird/14.0/releasenotes/
        FIXED
        Various fixes and performance improvements
        FIXED
        Various security fixes

--- mail/thunderbird/patches/patch-mozilla_netwerk_protocol_http_HttpChannelParent.cpp
+++ mail/thunderbird/patches/patch-mozilla_netwerk_protocol_http_HttpChannelParent.cpp
@@ -1,24 +1,18 @@
-$NetBSD: patch-mozilla_netwerk_protocol_http_HttpChannelParent.cpp,v 1.2 2012/04/28 16:56:59 ryoon Exp $
+$NetBSD: patch-mozilla_netwerk_protocol_http_HttpChannelParent.cpp,v 1.3 2012/07/19 17:57:30 ryoon Exp $
 
-* Fix build with gcc<4.5, for example, DragonFly 3.0.1.
-  See https://bugzilla.mozilla.org/show_bug.cgi?id=621446
-
---- mozilla/netwerk/protocol/http/HttpChannelParent.cpp.orig	2012-04-20 22:40:20.000000000 +0000
+--- mozilla/netwerk/protocol/http/HttpChannelParent.cpp.orig	2012-07-12 15:25:25.000000000 +0000
 +++ mozilla/netwerk/protocol/http/HttpChannelParent.cpp
-@@ -331,10 +331,15 @@ HttpChannelParent::RecvUpdateAssociatedC
-   return true;
- }
- 
--// Bug 621446 investigation, we don't want conditional PR_Aborts bellow to be
-+// Bug 621446 investigation, we don't want conditional PR_Aborts below to be
- // merged to a single address.
- #pragma warning(disable : 4068)
+@@ -340,6 +340,13 @@ HttpChannelParent::RecvUpdateAssociatedC
+ // Compiling this with GCC <= 4.4 fails with an internal compiler error
+ #pragma GCC optimize ("O0")
+ #endif
 +// Compiling with a version of GCC <= 4.4 fails with an internal compiler
 +// error.
 +#if !defined(__GNUC__) || \
 +  (__GNUC__ >= 5) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 5)
- #pragma GCC optimize ("O0")
++#pragma GCC optimize ("O0")
 +#endif
++
  
  bool
  HttpChannelParent::RecvRedirect2Verify(const nsresult& result, 
