b6db861189a903e336daab16087a05d34a753482
taca <taca@pkgsrc.org>
2012-07-20 12:28:17 +0000

    Update php53 pacakge to 5.3.15 (PHP 5.3.15).
    19-July-2012
    
    o Zend Engine
    	* Fixed bug #51094 (parse_ini_file() with INI_SCANNER_RAW cuts a value
    	  that includes a semi-colon)
    
    o COM
    	* Fixed bug #62146 com_dotnet cannot be built shared
    
    o Core
    	* Fixed potential overflow in _php_stream_scandir, CVE-2012-2688
    	* Fixed bug #62432 (ReflectionMethod random corrupt memory on high
    	  concurrent)
    	* Fixed bug #62443 (Crypt SHA256/512 Segfaults With Malformed Salt)
    
    o Fileinfo
    	* Fixed magic file regex support
    
    o FPM
    	* Fixed bug #61045 (fpm don't send error log to fastcgi clients)
    	* Fixed bug #61835 (php-fpm is not allowed to run as root)
    	* Fixed bug #61295 (php-fpm should not fail with commented 'user' for
    	  non-root start)
    	* Fixed bug #61026 (FPM pools can listen on the same address)
    	* Fixed bug #62033 (php-fpm exits with status 0 on some failures to
    	  start)
    	* Fixed bug #62153 (when using unix sockets, multiples FPM instances
    	  can be launched without errors)
    	* Fixed bug #62160 (Add process.priority to set nice(2) priorities)
    	* Fixed bug #61218 (FPM drops connection while receiving some binary
    	  values in FastCGI requests)
    	* Fixed bug #62205 (php-fpm segfaults (null passed to strstr))
    
    o Intl
    	* Fixed bug #62083 (grapheme_extract() memory leaks)
    	* Fixed bug #62081 (IntlDateFormatter constructor leaks memory when
    	  called twice)
    	* Fixed bug #62070 (Collator::getSortKey() returns garbage)
    	* Fixed bug #62017 (datefmt_create with incorrectly encoded timezone
    	  leaks pattern)
    	* Fixed bug #60785 (memory leak in IntlDateFormatter constructor)
    
    o JSON
    	* Reverted fix for bug #61537
    
    o Phar
    	* Fixed bug #62227 (Invalid phar stream path causes crash)
    
    o Reflection
    	* Fixed bug #62384 (Attempting to invoke a Closure more than once
    	  causes segfault)
    	* Fixed bug #62202 (ReflectionParameter::getDefaultValue() memory
    	  leaks with constant)
    
    o SPL
    	* Fixed bug #62262 (RecursiveArrayIterator does not implement Countable)
    
    o SQLite
    	* Fixed open_basedir bypass, CVE-2012-3365
    
    o XML Write
    	* Fixed bug #62064 (memory leak in the XML Writer module)
    
    o Zip
    	* Upgraded libzip to 0.10

--- lang/php53/Makefile.php
+++ lang/php53/Makefile.php
@@ -1,4 +1,4 @@
-# $NetBSD: Makefile.php,v 1.18 2012/06/16 01:27:24 taca Exp $
+# $NetBSD: Makefile.php,v 1.19 2012/07/20 12:28:17 taca Exp $
 # used by lang/php53/Makefile
 # used by www/ap-php/Makefile
 
@@ -58,7 +58,7 @@ PKG_SUGGESTED_OPTIONS+=	inet6 ssl
 
 .if !empty(PKG_OPTIONS:Msuhosin)
 SUHOSIN_PHPVER=		5.3.9
-.  if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS} && ${PHP_BASE_VERS} != 5.3.14
+.  if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS} && ${PHP_BASE_VERS} != 5.3.15
 PKG_FAIL_REASON+=	"The suhosin patch is currently not available for"
 PKG_FAIL_REASON+=	"this version of PHP.  You may have to wait until"
 PKG_FAIL_REASON+=	"an updated patch is released or temporarily"
