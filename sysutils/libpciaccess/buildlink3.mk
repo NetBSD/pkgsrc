e4dbda3b0b8552998f3182908b5e777a378de68c
fhajny <fhajny@pkgsrc.org>
2012-07-20 11:24:02 +0000

    Needs zlib bl3

--- sysutils/libpciaccess/buildlink3.mk
+++ sysutils/libpciaccess/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:25 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/07/20 11:24:02 fhajny Exp $
 
 BUILDLINK_TREE+=	libpciaccess
 
@@ -7,6 +7,8 @@ LIBPCIACCESS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libpciaccess+=	libpciaccess>=0.10.4
 BUILDLINK_PKGSRCDIR.libpciaccess?=	../../sysutils/libpciaccess
+
+.include "../../devel/zlib/buildlink3.mk"
 .endif # LIBPCIACCESS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libpciaccess
