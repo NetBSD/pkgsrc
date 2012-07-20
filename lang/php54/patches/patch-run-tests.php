eb7dec9cdf4712d90c6f6938d3fd43a620e3e1c4
taca <taca@pkgsrc.org>
2012-07-20 12:29:05 +0000

    Update php54 package to 5.4.5 (PHP 5.4.5).
    19-July-2012
    
    o Core
    	* Fixed bug #62443 (Crypt SHA256/512 Segfaults With Malformed Salt)
    	* Fixed bug #62432 (ReflectionMethod random corrupt memory on high
    	  concurrent)
    	* Fixed bug #62373 (serialize() generates wrong reference to the
    	  object).
    	* Fixed bug #62357 (compile failure: (S) Arguments missing for
    	  built-in function __memcmp)
    	* Fixed bug #61998 (Using traits with method aliases appears to result
    	  in crash during execution)
    	* Fixed bug #51094 (parse_ini_file() with INI_SCANNER_RAW cuts a value
    	  that includes a semi-colon)
    	* Fixed potential overflow in _php_stream_scandir (CVE-2012-2688)
    
    o EXIF
    	* Fixed information leak in ext exi
    
    o FPM
    	* Fixed bug #62205 (php-fpm segfaults (null passed to strstr)
    	* Fixed bug #62160 (Add process.priority to set nice(2) priorities)
    	* Fixed bug #62153 (when using unix sockets, multiples FPM instances)
    	* Fixed bug #62033 (php-fpm exits with status 0 on some failures to
    	  start)
    	* Fixed bug #61839 (Unable to cross-compile PHP with --enable-fpm)
    	* Fixed bug #61835 (php-fpm is not allowed to run as root)
    	* Fixed bug #61295 (php-fpm should not fail with commented 'user'
    	* Fixed bug #61218 (FPM drops connection while receiving some binary
    	  values in FastCGI requests)
    	* Fixed bug #61045 (fpm don't send error log to fastcgi clients).
    	  (fat) for non-root start)
    	* Fixed bug #61026 (FPM pools can listen on the same address).
    	  (fat) can be launched without errors)
    
    o Iconv
    	* Fixed bug #55042 (Erealloc in iconv.c unsafe)
    
    o Intl
    	* Fixed bug #62083 (grapheme_extract() memory leaks)
    	* Fixed bug #62081 (IntlDateFormatter constructor leaks memory when
    	  called twice)
    	* Fixed bug #62070 (Collator::getSortKey() returns garbage)
    	* Fixed bug #62017 (datefmt_create with incorrectly encoded timezone
    	  leaks pattern)
    	* Fixed bug #60785 (memory leak in IntlDateFormatter constructor)
    	* ResourceBundle constructor now accepts NULL for the first two arguments
    
    o JSON
    	* Fixed bug #61359 (json_encode() calls too many reallocs)
    
    o libxml
    	* Fixed bug #62266 (Custom extension segfaults during xmlParseFile
    	  with FPM SAPI)
    
    o Phar
    	* Fixed bug #62227 (Invalid phar stream path causes crash)
    
    o Readline
    	* Fixed bug #62186 (readline fails to compile - void function should
    	  not return a value)
    
    o Reflection
    	* Fixed bug #62384 (Attempting to invoke a Closure more than once
    	  causes segfault)
    	* Fixed bug #62202 (ReflectionParameter::getDefaultValue() memory
    	  leaks with constant)
    
    o Sockets
    	* Fixed bug #62025 (__ss_family was changed on AIX 5.3)
    
    o SPL
    	* Fixed bug #62433 (Inconsistent behavior of
    	  RecursiveDirectoryIterator to dot files)
    	* Fixed bug #62262 (RecursiveArrayIterator does not implement
    	  Countable)
    
    o XML Writer
    	* Fixed bug #62064 (memory leak in the XML Writer module)
    
    o Zip
    	* Upgraded libzip to 0.10.
    {

--- lang/php54/patches/patch-run-tests.php
+++ lang/php54/patches/patch-run-tests.php
@@ -1,9 +1,11 @@
-$NetBSD: patch-run-tests.php,v 1.1 2012/06/16 05:21:55 taca Exp $
+$NetBSD: patch-run-tests.php,v 1.2 2012/07/20 12:29:05 taca Exp $
+
+Use proper shbang line.
 
 --- run-tests.php.orig	2012-05-03 17:10:43.000000000 +0000
 +++ run-tests.php
 @@ -1,4 +1,4 @@
--#!/usr/bin/php
+-#!/usr/bin/env php
 +#!@PREFIX@/bin/php
  <?php
  /*
