705eb7657dbabedba6d6f026b4a39a46491b29f2
sbd <sbd>
2012-07-20 08:06:28 +0000

    Pullup ticket #3862 - requested by wiz graphics/tiff security update
    Revisions pulled up:
    - graphics/tiff/Makefile                                        1.106
    - graphics/tiff/distinfo                                        1.57
    - graphics/tiff/patches/patch-tools_tiff2pdf.c                  1.1
    
    ---
       Module Name:	pkgsrc
       Committed By:	wiz
       Date:		Thu Jul 19 21:41:45 UTC 2012
    
       Modified Files:
       	pkgsrc/graphics/tiff: Makefile distinfo
       Added Files:
       	pkgsrc/graphics/tiff/patches: patch-tools_tiff2pdf.c
    
       Log Message:
       Fix CVE-2012-3401, buffer overflow in tiff2pdf.
       Bump PKGREVISION.

---
+++ graphics/tiff/patches/patch-tools_tiff2pdf.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-tools_tiff2pdf.c,v 1.1.2.2 2012/07/20 08:06:28 sbd Exp $
+
+CVE-2012-3401, buffer overflow in tiff2pdf
+
+--- tools/tiff2pdf.c.orig	2012-06-15 21:51:54.000000000 +0000
++++ tools/tiff2pdf.c
+@@ -1066,6 +1066,7 @@ void t2p_read_tiff_init(T2P* t2p, TIFF* 
+ 				"Can't set directory %u of input file %s", 
+ 				i,
+ 				TIFFFileName(input));
++			t2p->t2p_error = T2P_ERR_ERROR;
+ 			return;
+ 		}
+ 		if(TIFFGetField(input, TIFFTAG_PAGENUMBER, &pagen, &paged)){
