69e777e5e55cd22fc2a7bfd8a1781886690a24b3
jperkin <jperkin@pkgsrc.org>
2012-07-23 12:24:34 +0000

    Update to binutils-2.22, mostly from wip/binutils.
    Changes since binutils-2.17 are too numerous to mention, see the various
    NEWS files for details.
    
    Tested on NetBSD 6.0_BETA2 and SmartOS.

--- devel/binutils/buildlink3.mk
+++ devel/binutils/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.23 2009/03/20 19:24:10 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2012/07/23 12:24:34 jperkin Exp $
 
 BUILDLINK_TREE+=	binutils
 
 .if !defined(BINUTILS_BUILDLINK3_MK)
 BINUTILS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.binutils+=	binutils>=2.17
+BUILDLINK_API_DEPENDS.binutils+=	binutils>=2.22
 BUILDLINK_PKGSRCDIR.binutils?=	../../devel/binutils
 BUILDLINK_DEPMETHOD.binutils?=	build
 .endif # BINUTILS_BUILDLINK3_MK
