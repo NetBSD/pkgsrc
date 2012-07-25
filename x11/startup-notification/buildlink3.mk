84efa8aea931936f8547794b673c8eb9de95ec4a
obache <obache@pkgsrc.org>
2012-07-25 12:22:23 +0000

    buildlink with xcb-util036 instead of xcb-util for the case requireing x11/startup-notification010.
    fixes PR 46738.

--- x11/startup-notification/buildlink3.mk
+++ x11/startup-notification/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2012/07/25 12:12:26 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2012/07/25 12:22:23 obache Exp $
 
 BUILDLINK_TREE+=	startup-notification
 
@@ -11,16 +11,17 @@ STARTUP_NOTIFICATION_BUILDLINK3_MK:=
 BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification
 BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5
 BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.12nb2
+.include "../../x11/xcb-util/buildlink3.mk"
 .else
 BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification010
 BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5<0.12
 BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.10nb2<0.12
+.include "../../x11/xcb-util036/buildlink3.mk"
 .endif
 
 .include "../../x11/libSM/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/libxcb/buildlink3.mk"
-.include "../../x11/xcb-util/buildlink3.mk"
 .endif # STARTUP_NOTIFICATION_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-startup-notification
