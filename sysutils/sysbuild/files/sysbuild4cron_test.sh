9c4a5f209ba62d6ae87fd6dcc11f26216a50d946
jmmv <jmmv>
2012-07-25 12:18:22 +0000

    Initial (re)addition of sysbuild, version 2.0:
    sysbuild is a tool to automate the maintenance of the NetBSD sources and
    the build of releases by means of a single command and a configuration
    file that tells sysbuild what to do.
    
    Since NetBSD 1.6 (released on July 2011), the source tree has included a
    very flexible script (build.sh) to build a full NetBSD release for the
    current platform or to cross-build for any of the supported platforms.
    
    The flipside of the flexibility is that the script is inconvenient to use
    on a daily basis because of the myriad of options it takes.  Furthermore,
    managing the source trees that make up NetBSD is not in the scope of
    build.sh: the user must fetch these trees and keep them up to date by hand.
    
    While these details are all fine on their own, rebuilding NetBSD frequently
    (to keep a system up to date, or just for development tasks) is convoluted.
    Developers and users usually find themselves writing their own wrapper
    scripts over build.sh to simplify their daily tasks.
    
    sysbuild extends build.sh by adding support for configuration files and
    source tree management, allowing NetBSD rebuilds with a single and simple
    command.  Make sure to check the sysbuild-user package for a way to
    trivially set up periodic NetBSD rebuilds under an dedicated, unprivileged
    user.
    
    This is version 2.0 because sysbuild already existed 10 years ago.  It
    served the same purpose, but it was removed because it stopped working
    with NetBSD 2.0 (can't remember why though).  Also, I'm convinced the
    previous implementation was quite bad.

---
+++ sysutils/sysbuild/files/sysbuild4cron_test.sh
@@ -0,0 +1,196 @@
+# Copyright 2012 Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+# * Neither the name of Google Inc. nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+# Creates a fake program that records its invocations for later processing.
+#
+# The fake program, when invoked, will append its arguments to a commands.log
+# file in the test case's work directory.
+#
+# \param binary The path to the program to create.
+# \param get_stdin Whether to capture stdin or not.
+create_mock_binary() {
+    local binary="${1}"; shift
+    local get_stdin="${1}"; shift
+
+    cat >"${binary}" <<EOF
+#! /bin/sh
+
+logfile="${HOME}/commands.log"
+echo "Command: \${0##*/}" >>"\${logfile}"
+for arg in "\${@}"; do
+    echo "Arg: \${arg}" >>"\${logfile}"
+done
+    [ "${get_stdin}" = no ] || sed -e 's,^,stdin: ,' >>"\${logfile}"
+    echo >>"\${logfile}"
+EOF
+    chmod +x "${binary}"
+}
+
+
+setup_mocks() {
+    mkdir bin
+    create_mock_binary bin/mail yes
+    create_mock_binary bin/sysbuild no
+    PATH="$(pwd)/bin:${PATH}"
+    SYSBUILD_BINDIR="$(pwd)/bin"; export SYSBUILD_BINDIR
+}
+
+
+atf_test_case no_args
+no_args_body() {
+    setup_mocks
+    atf_check sysbuild4cron
+
+    cat >expout <<EOF
+Command: sysbuild
+
+EOF
+    atf_check -o file:expout cat commands.log
+}
+
+
+atf_test_case some_args
+some_args_body() {
+    setup_mocks
+    atf_check sysbuild4cron -- -k -Z foo bar
+
+    cat >expout <<EOF
+Command: sysbuild
+Arg: -k
+Arg: -Z
+Arg: foo
+Arg: bar
+
+EOF
+    atf_check -o file:expout cat commands.log
+}
+
+
+atf_test_case sysbuild_fails
+sysbuild_fails_body() {
+    setup_mocks
+    for number in $(seq 150); do
+        echo "echo line ${number}" >>bin/sysbuild
+    done
+    echo "exit 1" >>bin/sysbuild
+
+    atf_check sysbuild4cron a
+
+    name="$(cd sysbuild/log && echo sysbuild4cron.*.log)"
+    cat >expout <<EOF
+Command: sysbuild
+Arg: a
+
+Command: mail
+Arg: -s
+Arg: sysbuild failure report
+Arg: jmmv
+stdin: The following command has failed:
+stdin: 
+stdin:     $(pwd)/bin/sysbuild a
+stdin: 
+stdin: The output of the failed command has been left in:
+stdin: 
+stdin:     $(pwd)/sysbuild/log/${name}
+stdin: 
+stdin: The last 100 of the log follow:
+stdin: 
+EOF
+    for number in $(seq 51 150); do
+        echo "stdin: line ${number}" >>expout
+    done
+    echo >>expout
+
+    atf_check -o file:expout cat commands.log
+}
+
+
+atf_test_case custom_flags
+custom_flags_body() {
+    setup_mocks
+    echo "exit 1" >>bin/sysbuild
+
+    atf_check sysbuild4cron -l path/to/logs -r somebody@example.net
+
+    name="$(cd path/to/logs && echo sysbuild4cron.*.log)"
+    cat >expout <<EOF
+Command: sysbuild
+
+Command: mail
+Arg: -s
+Arg: sysbuild failure report
+Arg: somebody@example.net
+stdin: The following command has failed:
+stdin: 
+stdin:     $(pwd)/bin/sysbuild
+stdin: 
+stdin: The output of the failed command has been left in:
+stdin: 
+stdin:     $(pwd)/path/to/logs/${name}
+stdin: 
+stdin: The last 100 of the log follow:
+stdin: 
+
+EOF
+
+    atf_check -o file:expout cat commands.log
+}
+
+
+atf_test_case capture_out_and_err
+capture_out_and_err_body() {
+    setup_mocks
+    echo "echo foo" >>bin/sysbuild
+    echo "echo bar 1>&2" >>bin/sysbuild
+    echo "exit 1" >>bin/sysbuild
+
+    atf_check sysbuild4cron
+
+    atf_check -o match:"stdin: foo" -o match:"stdin: bar" cat commands.log
+}
+
+
+atf_test_case unknown_flag
+unknown_flag_body() {
+    cat >experr <<EOF
+sysbuild4cron: E: Unknown option -Z
+Type 'man sysbuild4cron' for help
+EOF
+    atf_check -s exit:1 -e file:experr sysbuild4cron -Z
+}
+
+
+atf_init_test_cases() {
+    atf_add_test_case no_args
+    atf_add_test_case some_args
+    atf_add_test_case sysbuild_fails
+    atf_add_test_case custom_flags
+    atf_add_test_case capture_out_and_err
+    atf_add_test_case unknown_flag
+}
