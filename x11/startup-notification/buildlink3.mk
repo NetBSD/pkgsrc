b9cf35e23e8d47cefe61b6850bd6a1a9af3c0231
obache <obache>
2012-07-25 12:12:26 +0000

    merge same conditions into one condition to be maintainance easier.

--- x11/startup-notification/buildlink3.mk
+++ x11/startup-notification/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2012/07/24 12:09:07 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2012/07/25 12:12:26 obache Exp $
 
 BUILDLINK_TREE+=	startup-notification
 
@@ -6,26 +6,15 @@ BUILDLINK_TREE+=	startup-notification
 STARTUP_NOTIFICATION_BUILDLINK3_MK:=
 
 .include "../../mk/bsd.fast.prefs.mk"
-.if ${X11_TYPE} == "modular"
+.if ${X11_TYPE} == "modular" || ${OPSYS} != "NetBSD" || ${OPSYS} == "NetBSD" \
+	&& (!empty(OS_VERSION:M5.99.*) || !empty(OS_VERSION:M[6789].*))
 BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification
 BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5
 BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.12nb2
 .else
-.  if ${OPSYS} == "NetBSD"
-.    if !empty(OS_VERSION:M5.99.*) || !empty(OS_VERSION:M[6789].*)
-BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification
-BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5
-BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.12nb2
-.    else
 BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification010
 BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5<0.12
 BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.10nb2<0.12
-.    endif
-.  else #!NetBSD
-BUILDLINK_PKGSRCDIR.startup-notification?=	../../x11/startup-notification
-BUILDLINK_API_DEPENDS.startup-notification+=	startup-notification>=0.5
-BUILDLINK_ABI_DEPENDS.startup-notification+=	startup-notification>=0.12nb2
-.  endif
 .endif
 
 .include "../../x11/libSM/buildlink3.mk"
