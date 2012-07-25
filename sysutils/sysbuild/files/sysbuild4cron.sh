f21da47225ca2b6f8876f25150323d6c2da1c2f5
jmmv <jmmv@pkgsrc.org>
2012-07-25 12:18:22 +0000

    Initial (re)addition of sysbuild, version 2.0:
    sysbuild is a tool to automate the maintenance of the NetBSD sources and
    the build of releases by means of a single command and a configuration
    file that tells sysbuild what to do.
    
    Since NetBSD 1.6 (released on July 2011), the source tree has included a
    very flexible script (build.sh) to build a full NetBSD release for the
    current platform or to cross-build for any of the supported platforms.
    
    The flipside of the flexibility is that the script is inconvenient to use
    on a daily basis because of the myriad of options it takes.  Furthermore,
    managing the source trees that make up NetBSD is not in the scope of
    build.sh: the user must fetch these trees and keep them up to date by hand.
    
    While these details are all fine on their own, rebuilding NetBSD frequently
    (to keep a system up to date, or just for development tasks) is convoluted.
    Developers and users usually find themselves writing their own wrapper
    scripts over build.sh to simplify their daily tasks.
    
    sysbuild extends build.sh by adding support for configuration files and
    source tree management, allowing NetBSD rebuilds with a single and simple
    command.  Make sure to check the sysbuild-user package for a way to
    trivially set up periodic NetBSD rebuilds under an dedicated, unprivileged
    user.
    
    This is version 2.0 because sysbuild already existed 10 years ago.  It
    served the same purpose, but it was removed because it stopped working
    with NetBSD 2.0 (can't remember why though).  Also, I'm convinced the
    previous implementation was quite bad.

---
+++ sysutils/sysbuild/files/sysbuild4cron.sh
@@ -0,0 +1,106 @@
+# Copyright 2012 Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+# * Neither the name of Google Inc. nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# \file sysbuild4cron.sh
+# Entry point and main program logic.
+
+
+# Paths to installed files.
+#
+# Can be overriden for test purposes only.
+: ${SYSBUILD_BINDIR="@SYSBUILD_BINDIR@"}
+
+
+# Sends an email notification upon a failure.
+#
+# \param logfile File containing the output of the failed command.
+# \param recipient Who to send the email to.
+# \param ... Command that failed.
+sysbuild4cron_email() {
+    local logfile="${1}"; shift
+    local recipient="${1}"; shift
+
+    local maxlines=100
+    {
+        echo "The following command has failed:"
+        echo
+        echo "    $*"
+        echo
+        echo "The output of the failed command has been left in:"
+        echo
+        echo "    ${logfile}"
+        echo
+        echo "The last ${maxlines} of the log follow:"
+        echo
+        tail -n${maxlines} "${logfile}"
+    } | mail -s "sysbuild failure report" "${recipient}"
+}
+
+
+# Entry point to the program.
+#
+# \param ... Command-line arguments to be processed.
+#
+# \return An exit code to be returned to the user.
+sysbuild4cron_main() {
+    local logdir="${HOME}/sysbuild/log"
+    local recipient="$(id -u -n)"
+
+    while getopts ':l:r:' arg "${@}"; do
+        case "${arg}" in
+            l)  # Location of logs.
+                case "${OPTARG}" in
+                    /*) logdir="${OPTARG}" ;;
+                    *) logdir="$(pwd)/${OPTARG}" ;;
+                esac
+                ;;
+
+            r)  # Email recipient.
+                recipient="${OPTARG}"
+                ;;
+
+            \?)
+                utils_usage_error "Unknown option -${OPTARG}"
+                ;;
+        esac
+    done
+    shift $((${OPTIND} - 1))
+
+    set -- "${SYSBUILD_BINDIR}/sysbuild" "${@}"
+
+    if [ ! -d "${logdir}" ]; then
+        mkdir -p "$(dirname "${logdir}")" >/dev/null 2>/dev/null || true
+        mkdir "${logdir}" \
+            || utils_error "Failed to create directory '${logdir}'"
+    fi
+
+    local exit_code=0
+    local logfile="${logdir}/${Utils_ProgName}.$(date +%Y%m%d%H%M%S).log"
+    "${@}" >"${logfile}" 2>&1 \
+        || sysbuild4cron_email "${logfile}" "${recipient}" "${@}"
+}
