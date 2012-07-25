82a989917f525c439600dd15e36ca4c973b7446b
obache <obache@pkgsrc.org>
2012-07-25 13:05:30 +0000

    Switch to usual manner instead of buildlink jail break.

--- x11/xcb-util036/buildlink3.mk
+++ x11/xcb-util036/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/07/13 14:19:47 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/07/25 13:05:30 obache Exp $
 
 BUILDLINK_TREE+=	xcb-util036
 
@@ -8,9 +8,9 @@ XCB_UTIL036_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.xcb-util036+=	xcb-util036>=0.3.6
 BUILDLINK_PKGSRCDIR.xcb-util036?=	../../x11/xcb-util036
 
-BUILDLINK_PASSTHRU_DIRS+=		${BUILDLINK_PREFIX.xcb-util036}/lib
-BUILDLINK_PASSTHRU_RPATHDIRS+=		${PREFIX}/xcb-util036/lib
-_PKG_CONFIG_LIBDIR=			${BUILDLINK_DIR}/xcb-util036/lib/pkgconfig:${BUILDLINK_DIR}/lib/pkgconfig:${BUILDLINK_DIR}/share/pkgconfig
+BUILDLINK_INCDIRS.xcb-util036+=		xcb-util036/include
+BUILDLINK_LIBDIRS.xcb-util036+=		xcb-util036/lib
+BUILDLINK_FNAME_TRANSFORM.xcb-util036+=	-e 's|xcb-util036/lib/pkgconfig/|lib/pkgconfig/|'
 
 .include "../../x11/libxcb/buildlink3.mk"
 .endif	# XCB_UTIL036_BUILDLINK3_MK
