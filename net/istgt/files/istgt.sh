bf2c7542173029202d7e79836660d14666212d63
sborrill <sborrill@pkgsrc.org>
2012-07-27 13:08:54 +0000

    Update to version 20120726.
    Major changes:
    - Introduce SIGHUP reloading.
    - The symlink device can be enabled by '--enable-symlink-device' configure
      option.
    - Fix the unknown error under some mutex condition.
    - Fix the wrong reservation key handling.
    - Use Clang as first choice compiler.
    - Use _GNU_SOURCE on linux platform.
    
    Current Limitations of SIGHUP support:
    - The parameters other than PG, IG, and LU are not reloaded by SIGHUP.
    - PG and IG mapped to LU can't be deleted by SIGHUP.
    - If you delete an active LU, all connections of the LU are closed by
      SIGHUP.
    - Can't modify LU by SIGHUP.
    - Updating IG is not affected until the next login.

--- net/istgt/files/istgt.sh
+++ net/istgt/files/istgt.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: istgt.sh,v 1.1.1.1 2010/02/02 16:09:51 sborrill Exp $
+# $NetBSD: istgt.sh,v 1.2 2012/07/27 13:08:54 sborrill Exp $
 #
 
 # PROVIDE: istgt
@@ -13,6 +13,7 @@ rcvar=$name
 command="@PREFIX@/bin/${name}"
 pidfile="/var/run/${name}.pid"
 required_files="@PKG_SYSCONFDIR@/istgt.conf"
+extra_commands="reload"
 
 load_rc_config $name
 run_rc_command "$1"
