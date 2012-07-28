9b2f9a41f0a9b6c3211220bdcca00f50bf9618db
dholland <dholland@pkgsrc.org>
2012-07-28 18:01:26 +0000

    Reference PR 46649 and add upstream bug report URL.

--- x11/libdrm/patches/patch-intel_intel__bufmgr__gem.c
+++ x11/libdrm/patches/patch-intel_intel__bufmgr__gem.c
@@ -1,6 +1,8 @@
-$NetBSD: patch-intel_intel__bufmgr__gem.c,v 1.1 2012/07/11 19:03:47 wiz Exp $
+$NetBSD: patch-intel_intel__bufmgr__gem.c,v 1.2 2012/07/28 18:01:26 dholland Exp $
 
 ETIME is not defined on DragonFly BSD, work around it.
+From PR 46649; upstream report at
+   https://bugs.freedesktop.org/show_bug.cgi?id=52549
 
 --- intel/intel_bufmgr_gem.c.orig	2012-07-02 15:22:14.326942000 +0000
 +++ intel/intel_bufmgr_gem.c
