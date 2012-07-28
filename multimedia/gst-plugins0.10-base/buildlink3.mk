d0ed2d42587e1085a70952d40329101e9bd60175
reed <reed>
2012-07-28 22:39:11 +0000

    Various gst-plugins needed newer gst-plugins0.10-base.

--- multimedia/gst-plugins0.10-base/buildlink3.mk
+++ multimedia/gst-plugins0.10-base/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.14 2012/06/14 07:43:39 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2012/07/28 22:39:11 reed Exp $
 
 BUILDLINK_TREE+=	gst-plugins0.10-base
 
 .if !defined(GST_PLUGINS0.10_BASE_BUILDLINK3_MK)
 GST_PLUGINS0.10_BASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gst-plugins0.10-base+=	gst-plugins0.10-base>=0.10.1
+BUILDLINK_API_DEPENDS.gst-plugins0.10-base+=	gst-plugins0.10-base>=0.10.31
 BUILDLINK_ABI_DEPENDS.gst-plugins0.10-base+=	gst-plugins0.10-base>=0.10.36nb2
 BUILDLINK_PKGSRCDIR.gst-plugins0.10-base?=	../../multimedia/gst-plugins0.10-base
 
