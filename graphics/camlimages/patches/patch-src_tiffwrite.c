016c69758e0bb9fe56f86e451bccb71827e01458
marino <marino@pkgsrc.org>
2012-07-29 12:52:55 +0000

    graphics/camlimages: Fix tiff-4.0 regression
    When tiff was upgraded to 4.0, camlimages stopped building.  Both
    caml and tiff redefine several common typedefs such as uint32.  Unlike
    the 3-series of tiff, tiff-4.0 also redefined int64 and uint64.  The
    existing hack didn't foresee int64 and uint64 getting used, and so
    camlimages broke.
    
    One patch was created and another revised to override the caml typedef
    definitions with macros before tiff.h is included.  The original
    tiffread.c patch was reworked to override uint16 and uint32 *again*
    after the tiff.h include and not before as it was originally.  Very ugly
    all around, but I just extended what camlimages was already doing.

--- graphics/camlimages/patches/patch-src_tiffwrite.c
+++ graphics/camlimages/patches/patch-src_tiffwrite.c
@@ -1,16 +1,14 @@
-$NetBSD: patch-src_tiffread.c,v 1.2 2012/07/29 12:52:55 marino Exp $
+$NetBSD: patch-src_tiffwrite.c,v 1.1 2012/07/29 12:52:55 marino Exp $
 
 Both ocaml/caml/config.h and tiff.h define int32, uint32, etc.
 The workaround of this name polution is to intentionally overwrite the
 first caml definition with a garbage macro before tiff.h is included and
 then unset it after the include.  After tiff version 4.0.1, the same hack
-has to be applied to 64-bit types.  Unlike tiffwrite.c, tiffread.c forgot
-to unset the bogus definitions.  The uint16 and uint32 then has to be
-redefined correctly.  (ugly hack)
+has to be applied to 64-bit types
 
---- src/tiffread.c.orig	2011-06-22 18:04:32.000000000 +0000
-+++ src/tiffread.c
-@@ -28,9 +28,20 @@
+--- src/tiffwrite.c.orig	2011-06-22 18:04:32.000000000 +0000
++++ src/tiffwrite.c
+@@ -25,6 +25,8 @@
  #define uint16 uint16tiff
  #define int32 int32tiff
  #define uint32 uint32tiff
@@ -19,15 +17,12 @@ redefined correctly.  (ugly hack)
  
  #include <tiffio.h>
  
-+#undef int16
-+#undef uint16
-+#undef int32
-+#undef uint32
+@@ -32,6 +34,8 @@
+ #undef uint16
+ #undef int32
+ #undef uint32
 +#undef int64
 +#undef uint64
-+#define uint16 uint16_t
-+#define uint32 uint32_t
-+
+ 
  extern value *imglib_error;
  
- value open_tiff_file_for_read( name )
