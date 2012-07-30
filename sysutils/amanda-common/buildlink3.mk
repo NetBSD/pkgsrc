9fa8b8d71b99eff4eba242a587cc167553f2ed68
sbd <sbd@pkgsrc.org>
2012-07-30 06:57:21 +0000

    Add a version.mk file with AMANDA_VERSION in it and use that in all amanda packages (including the meta-package) and in the buildlink.

--- sysutils/amanda-common/buildlink3.mk
+++ sysutils/amanda-common/buildlink3.mk
@@ -1,12 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.12 2009/03/20 19:25:23 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2012/07/30 06:57:21 sbd Exp $
 
 BUILDLINK_TREE+=	amanda-common
 
 .if !defined(AMANDA_COMMON_BUILDLINK3_MK)
 AMANDA_COMMON_BUILDLINK3_MK:=
+.include "version.mk"
 
-BUILDLINK_API_DEPENDS.amanda-common+=	amanda-common>=2.5.2p1
-BUILDLINK_ABI_DEPENDS.amanda-common+=	amanda-common>=2.5.2p1
+BUILDLINK_API_DEPENDS.amanda-common+=	amanda-common>=${AMANDA_VERSION}
+BUILDLINK_ABI_DEPENDS.amanda-common+=	amanda-common>=${AMANDA_VERSION}
 BUILDLINK_PKGSRCDIR.amanda-common?=	../../sysutils/amanda-common
 .endif # AMANDA_COMMON_BUILDLINK3_MK
 
