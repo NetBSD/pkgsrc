a71a5c6ff5389e1cf107fcb7c31a3f7e497bd7e9
wiz <wiz@pkgsrc.org>
2012-07-31 18:44:03 +0000

    Stop WRKSRC/CONFIGURE_DIRS abuse.
    WRKSRC should be the toplevel, while CONFIGURE_DIRS can be used
    for sub-hierarchies. Make it so.

--- print/xetex/patches/patch-texk_web2c_xetexdir_XeTeX__ext.c
+++ print/xetex/patches/patch-texk_web2c_xetexdir_XeTeX__ext.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-xetexdir_XeTeX__ext.c,v 1.2 2012/07/31 18:35:47 wiz Exp $
+$NetBSD: patch-texk_web2c_xetexdir_XeTeX__ext.c,v 1.1 2012/07/31 18:44:03 wiz Exp $
 
 Prepare for patching in absolute pkgsrc path to xdvipdfmx binary.
 
---- xetexdir/XeTeX_ext.c.orig	2011-02-25 11:49:36.000000000 +0000
-+++ xetexdir/XeTeX_ext.c
-@@ -182,7 +182,7 @@ const UInt32 byteMark				= 0x00000080UL;
+--- texk/web2c/xetexdir/XeTeX_ext.c.orig	2012-03-19 08:34:01.000000000 +0000
++++ texk/web2c/xetexdir/XeTeX_ext.c
+@@ -173,7 +173,7 @@ const UInt32 byteMark				= 0x00000080UL;
  
  /* if the user specifies a paper size or output driver program */
  const char *papersize;
