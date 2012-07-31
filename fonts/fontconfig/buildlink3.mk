97a47d922108d8541ea7a7c770d4a3e2b75a94d0
drochner <drochner@pkgsrc.org>
2012-07-31 18:12:28 +0000

    update to 2.10.1 changes: * FC_GLOBAL_ADVANCE now deprecated * Disable non-Unicode SFNT names by default (Use --enable-iconv to   enable it) * Available config files has been moved to   /usr/share/fontconfig/conf.avail * Warns multiple values of <test>s and <family>s in <match> and <alias> * Add XDG Base Directory Specification support * Add FC_LANG environment variable for default substitute language. * Add more options to configure the installation paths * Add "namelang" object to obtain the localized name of the family,   style and fullname. * Unicode 6.1 support * CACHEDIR.TAG support * Fix several build issues with older gcc, MinGW, Win32. * Fix a fail of creating caches on some filesystems * Fix memory leaks * Add/Update orthography files * Improve documents * and more bug fixes and enhancements.
    pkgsrc changes: some cleanup - install only useful stuff to /etc

--- fonts/fontconfig/buildlink3.mk
+++ fonts/fontconfig/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.28 2012/05/07 01:53:29 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.29 2012/07/31 18:12:28 drochner Exp $
 
 BUILDLINK_TREE+=	fontconfig
 
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.fontconfig+=	fontconfig>=2.4.2
 BUILDLINK_ABI_DEPENDS.fontconfig+=	fontconfig>=2.8.0nb2
 BUILDLINK_PKGSRCDIR.fontconfig?=	../../fonts/fontconfig
 
-.include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
