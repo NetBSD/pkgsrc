240a1606f5f44c297eb09a5fbee4a4f1767306f1
drochner <drochner@pkgsrc.org>
2012-08-01 15:30:45 +0000

    update dependency - shlib major had changed

--- graphics/exiv2/buildlink3.mk
+++ graphics/exiv2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2011/10/19 06:50:01 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2012/08/01 15:30:45 drochner Exp $
 
 BUILDLINK_TREE+=	exiv2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	exiv2
 EXIV2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.exiv2+=	exiv2>=0.18
-BUILDLINK_ABI_DEPENDS.exiv2+=	exiv2>=0.22
+BUILDLINK_ABI_DEPENDS.exiv2+=	exiv2>=0.23
 BUILDLINK_PKGSRCDIR.exiv2?=	../../graphics/exiv2
 
 .include "../../devel/gettext-lib/buildlink3.mk"
