465d3534c2e369daeb3d3654db80aebd4adc8b99
abs <abs@pkgsrc.org>
2012-08-03 10:35:31 +0000

    Updated pkgtools/pkg_chk to 2.0.4
    Fix regression introduced in 2.0.1 where available binary packages
    would not always be listed
    Add licence (2 clause BSD)

--- pkgtools/pkg_chk/files/pkg_chk.sh
+++ pkgtools/pkg_chk/files/pkg_chk.sh
@@ -1,11 +1,36 @@
 #!@SH@ -e
 #
-# $Id: pkg_chk.sh,v 1.68 2012/08/02 09:39:49 abs Exp $
+# $Id: pkg_chk.sh,v 1.69 2012/08/03 10:35:31 abs Exp $
 #
 # TODO: Make -g check dependencies and tsort
 # TODO: Make -g list user-installed packages first, followed by commented
 #	out automatically installed packages
 # TODO: List user-installed packages that are not in config
+
+# Copyright (c) 2012 David Brownlee (Standard 2 clause BSD licence)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 PATH=${PATH}:/usr/sbin:/usr/bin
 
@@ -82,9 +107,9 @@ check_packages_installed()
 			msg_n "$pkginstalled > $PKGNAME - ignoring"
 		    fi
 		fi
-		if is_binary_available $PKGNAME ;then
-		    msg_n " (has binary package)"
-		fi
+	    fi
+	    if is_binary_available $PKGNAME ;then
+		msg_n " (has binary package)"
 	    fi
 	    msg
 	else
