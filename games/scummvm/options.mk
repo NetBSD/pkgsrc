eda31fb580d84d3e767bd6f7c5f181f01f95edab
drochner <drochner>
2012-08-04 14:45:34 +0000

    update to 1.5.0 changes: -support more games -support for TrueType fonts via FreeType2 -custom game and engine options -speedup, UI improvements -bugfixes -translation update
    pkgsrc change: cleanup of options -- the "x11" backend doesn't work,
    so "sdl" is the only choice left

--- games/scummvm/options.mk
+++ games/scummvm/options.mk
@@ -1,23 +1,10 @@
-# $NetBSD: options.mk,v 1.7 2012/02/27 13:58:05 adam Exp $
+# $NetBSD: options.mk,v 1.8 2012/08/04 14:45:34 drochner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scummvm
-PKG_SUPPORTED_OPTIONS=	sdl x11 fluidsynth
-PKG_SUGGESTED_OPTIONS=	sdl
+PKG_SUPPORTED_OPTIONS=	fluidsynth
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mfluidsynth)
 .include "../../audio/fluidsynth/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Msdl)
-CONFIGURE_ARGS+=	--backend=sdl
-.include "../../devel/SDL/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mx11)
-CONFIGURE_ARGS+=	--backend=x11
-.include "../../mk/oss.buildlink3.mk"
-.include "../../x11/libXext/buildlink3.mk"
-.include "../../x11/xextproto/buildlink3.mk"
 .endif
