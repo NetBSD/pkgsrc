4eedcd254eb47811604b2adbd5edc957a1dc432e
drochner <drochner@pkgsrc.org>
2012-08-04 14:36:42 +0000

    update to 0.14.7 This switches to the new major release. The most significant change of this version is multiple rendering of same RAW data without re-opening RAW file. So, it is possible to implement near-realtime preview of entire image in half-resolution mode and realtime preview of selected area (e.g. around mouse pointer position) in full-resolution mode. +Support for more cameras

--- graphics/libraw/buildlink3.mk
+++ graphics/libraw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2010/12/14 07:43:45 gls Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2012/08/04 14:36:42 drochner Exp $
 
 BUILDLINK_TREE+=	libraw
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	libraw
 LIBRAW_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libraw+=	libraw>=0.12.0
+BUILDLINK_ABI_DEPENDS.libraw+=	libraw>=0.14.6
 BUILDLINK_PKGSRCDIR.libraw?=	../../graphics/libraw
 
 .include "../../graphics/lcms/buildlink3.mk"
