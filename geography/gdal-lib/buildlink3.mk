c7a214e634a0d747124f03f3ae0de2c5c309a50e
obache <obache@pkgsrc.org>
2012-08-05 10:02:09 +0000

    Bump PKGREVISION for change of PostgreSQL default version to 9.1.

--- geography/gdal-lib/buildlink3.mk
+++ geography/gdal-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2012/06/02 16:31:29 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2012/08/05 10:02:11 obache Exp $
 
 BUILDLINK_TREE+=	gdal-lib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gdal-lib
 GDAL_LIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gdal-lib+=	gdal-lib>=1.6.1
-BUILDLINK_ABI_DEPENDS.gdal-lib+=	gdal-lib>=1.9.1
+BUILDLINK_ABI_DEPENDS.gdal-lib+=	gdal-lib>=1.9.1nb1
 BUILDLINK_PKGSRCDIR.gdal-lib?=	../../geography/gdal-lib
 
 pkgbase := gdal-lib
