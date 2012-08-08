ca78834e3035d0b02e09e8c7fc4461ba14c2d46c
asau <asau@pkgsrc.org>
2012-08-08 06:55:32 +0000

    Update to Racket 5.3
    Changes in Racket 5.3
    
    * Submodules are nested module declarations that can be loaded and
      run independently from the enclosing module.  For an overview of
      submodules, see
    
        http://blog.racket-lang.org/2012/06/submodules.html
    
    * The futures visualizer is a graphical profiling tool for parallel
      programs using futures.  The tool shows a detailed execution
      timeline depicting the migration of futures between threads, and
      gives detailed information about each runtime synchronization that
      occurred during program execution.  In addition, `would-be-future'
      is a special type of future that always executes sequentially and
      records all potential barricades a regular future would encounter.
    
    * Optimization Coach (formerly Performance Report) reports
      information about Racket's inlining optimizations.  Optimization
      Coach can be launched in any language through the View menu.
    
    * The new `images/flomap' library defines floating-point bitmaps and
      fast image processing operations on them.  It is written in Typed
      Racket, so Typed Racket code may use it without the cost of
      contract checks.
    
    * The new `json' library supports parsing and generating JSON.
      (Originally based on Dave Herman's planet library.)
    
    * `racket/string' is extended with a set of simplified string
      manipulation functions that are more convenient than using
      regexps.  `regexp-match*' and friends can now be used with new
      keyword arguments to return specific matched regexp group/s and
      gaps between matches.
    
    * The new `racket/generic' library allows generic function
      definitions, which dispatch to methods added to a structure type
      via the new `#:methods' keyword.
    
    * The `class' form supports declaring a method abstract.  An
      abstract method prevents a class from being instantiated unless it
      is overridden.
    
    * The contract library comes with support for interfaces, generics,
      prompts, continuation-marks, and structs.
    
    * Most error messages use a new multi-line format that is more
      consistent with contract errors and accommodates more information.
    
    * Typed Racket supports function definitions with keyword arguments;
      the startup time of Typed Racket programs has been sharply
      reduced.
    
    * The new `ffi/com' library replaces MysterX; a compatibility
      `mysterx' library remains, but without ActiveX support.  The new
      `ffi/unsafe/com' library offers a more primitive and direct way to
      use COM classes and methods.
    
    * There is now a very complete completion code for zsh.  It is not
      included in the distribution though; get it at http://goo.gl/DU8JK
      (This script and the bash completions will be included in the
      standard installers in future versions.)
    
    --- DEPRECATION ----------------------------------------------------
    
    Effective this release:
    
      - The `tex2page' and `combinator-parser' libraries have been moved
        from the Racket distribution to PLaneT:
    
         (require (planet plt/tex2page))
         (require (planet plt/combinator-parser))
    
    The following has been deprecated and will be removed in the
    January 2013 release:
    
      - the `planet' command-line tool; use `raco planet' instead.
    
    The following has been deprecated and will be removed in the
    August 2013 release:
    
      - the `mzlib/class100' library; use `racket/class' instead.

--- lang/racket/patches/patch-collects_racket_draw_unsafe_png.rkt
+++ lang/racket/patches/patch-collects_racket_draw_unsafe_png.rkt
@@ -1,13 +1,13 @@
-$NetBSD: patch-collects_racket_draw_unsafe_png.rkt,v 1.1 2011/08/01 11:45:43 asau Exp $
+$NetBSD: patch-collects_racket_draw_unsafe_png.rkt,v 1.2 2012/08/08 06:55:32 asau Exp $
 
---- collects/racket/draw/unsafe/png.rkt.orig	2011-04-30 03:47:34.000000000 +0000
+--- collects/racket/draw/unsafe/png.rkt.orig	2012-06-02 05:21:31.000000000 +0000
 +++ collects/racket/draw/unsafe/png.rkt
-@@ -14,7 +14,7 @@
-    ;; variants often have just "libpng":
-    (with-handlers ([exn:fail:filesystem?
-                     (lambda (exn) (ffi-lib "libpng"))])
--     (ffi-lib "libpng12" '("0" "")))]
-+     (ffi-lib "libpng15" '("0" "")))]
-   [(macosx) (ffi-lib "libpng14.14.dylib")]
-   [(windows) 
-    (ffi-lib "zlib1.dll")
+@@ -10,7 +10,7 @@
+   [(unix)
+    ;; Most Linux distros supply "libpng12", while other Unix
+    ;; variants often have just "libpng", etc.
+-   (ffi-lib "libpng15" '("15" "")
++   (ffi-lib "libpng15" '("15" "0" "")
+ 	    #:fail (lambda ()
+ 		     (ffi-lib "libpng12" '("0" "")
+ 			      #:fail (lambda ()
