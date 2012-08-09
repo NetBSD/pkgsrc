962a164df8f2619f234cbd9519d0190a82be9d3e
marino <marino@pkgsrc.org>
2012-08-09 23:25:04 +0000

    editors/gummi: USE_TOOLS+= msgfmt, piggy-back on patch
    DragonFly needs <netinit/in.h> header just like NetBSD.

--- editors/gummi/patches/patch-src_update.c
+++ editors/gummi/patches/patch-src_update.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_update.c,v 1.1 2012/08/04 14:07:19 drochner Exp $
+$NetBSD: patch-src_update.c,v 1.2 2012/08/09 23:25:05 marino Exp $
 
 --- src/update.c.orig	2012-06-24 16:52:03.000000000 +0000
 +++ src/update.c
@@ -6,7 +6,7 @@ $NetBSD: patch-src_update.c,v 1.1 2012/08/04 14:07:19 drochner Exp $
  #include "environment.h"
  #include "utils.h"
  
-+#ifdef __NetBSD__
++#if defined(__NetBSD__) || defined(__DragonFly__)
 +#include <netinet/in.h>
 +#endif
 +
