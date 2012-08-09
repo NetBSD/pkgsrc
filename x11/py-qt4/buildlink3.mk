ad32cab103b18e7b584fbb87ec4af00d2e764683
sbd <sbd@pkgsrc.org>
2012-08-09 10:15:05 +0000

    Revert the below commit as it didn't work.  The py-qt4 buildlink3.mk may be included after pyversion.mk, which means the setting has no affect.
    ---
       Module Name:	pkgsrc
       Committed By:	sbd
       Date:		Tue Aug  7 09:44:03 UTC 2012
    
       Modified Files:
       	pkgsrc/x11/py-qt4: buildlink3.mk
    
       Log Message:
       Set PYTHON_VERSIONS_INCOMPATIBLE+=25 in the buildlink3.mk so that dependent
       packages won't try to build with python25.
    
       Fixes Global dependency resolution failure.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.3 -r1.4 pkgsrc/x11/py-qt4/buildlink3.mk

--- x11/py-qt4/buildlink3.mk
+++ x11/py-qt4/buildlink3.mk
@@ -1,11 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.4 2012/08/07 09:44:02 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2012/08/09 10:15:05 sbd Exp $
 
 BUILDLINK_TREE+=	py-qt4
 
 .if !defined(PY_QT4_BUILDLINK3_MK)
 PY_QT4_BUILDLINK3_MK:=
-
-PYTHON_VERSIONS_INCOMPATIBLE+=	25
 
 .include "../../lang/python/pyversion.mk"
 
