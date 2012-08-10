8ca4367a435c82cd5f9ffde6c6d1c35599096d12
ryoon <ryoon@pkgsrc.org>
2012-08-10 13:46:01 +0000

    Import vte029-0.32.2 as x11/vte029.
    Terminal widget with improved font, internationalization and
    accessibility support for the GNOME 3 desktop.

---
+++ x11/vte029/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/08/10 13:46:01 ryoon Exp $
+
+BUILDLINK_TREE+=	vte029
+
+.if !defined(VTE029_BUILDLINK3_MK)
+VTE029_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.vte029+=	vte029>=0.32.2
+BUILDLINK_ABI_DEPENDS.vte029+=	vte029>=0.32.2
+BUILDLINK_PKGSRCDIR.vte029?=	../../x11/vte029
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.include "../../mk/termcap.buildlink3.mk"
+.endif # VTE029_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-vte029
