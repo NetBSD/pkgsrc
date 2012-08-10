19dd74dbc01f413bbeeaebe0f552775fb588d2da
drochner <drochner@pkgsrc.org>
2012-08-10 15:14:53 +0000

    update to 4.6.0 changes: - New syntax supporting multi-line blocks of code delimited by curly braces      if (<cond>) { ... } else { ... }      do for [<iteration-spec>] { ... }      while (<cond>) { ... } - Time formats can handle fractional seconds to microsecond precision. - User-definable linetypes that can be used to establish a locally   preferred default sequence of colors or dash/thickness/point styles. - Statistical summary of the data to be plotted (new command "stats") - New terminal drivers:  qt  context  epscairo  cairolatex - Improved support for UTF-8, SJIS and other multi-byte encodings
    pkgsrc change: added "qt4" and "wxwindows" options (not enabled
     by default)

--- graphics/gnuplot/options.mk
+++ graphics/gnuplot/options.mk
@@ -1,12 +1,12 @@
-# $NetBSD: options.mk,v 1.4 2011/04/29 11:41:07 obache Exp $
+# $NetBSD: options.mk,v 1.5 2012/08/10 15:14:54 drochner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnuplot
-PKG_SUPPORTED_OPTIONS=	gd lua pdf x11
+PKG_SUPPORTED_OPTIONS=	gd lua pdf x11 qt4 wxwidgets
 PKG_SUGGESTED_OPTIONS=	gd x11
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	x11
+PLIST_VARS+=	x11 qt4
 
 .if !empty(PKG_OPTIONS:Mgd)
 .include "../../graphics/gd/buildlink3.mk"
@@ -29,4 +29,21 @@ PLIST.x11=	yes
 .include "../../x11/libXaw/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-x
+.endif
+
+.if !empty(PKG_OPTIONS:Mqt4)
+USE_LANGUAGES+=	c++
+CONFIGURE_ARGS+=	--enable-qt
+PLIST.qt4=      yes
+.include "../../x11/qt4-libs/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=        --disable-qt
+.endif
+
+.if !empty(PKG_OPTIONS:Mwxwidgets)
+USE_LANGUAGES+= c++
+CONFIGURE_ARGS+=        --enable-wxwidgets
+.include "../../x11/wxGTK28/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=        --disable-wxwidgets
 .endif
