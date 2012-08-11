1badcf7904e9fbbc749db0e49e56bce91be52329
ryoon <ryoon>
2012-08-11 18:51:16 +0000

    Update to 3.6.2
    Changelog:
    Major changes in release 3.6.2
    
    * Report correctly the Christmas day in Canada.

--- time/gcal/patches/patch-lib_fseeko.c
+++ time/gcal/patches/patch-lib_fseeko.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-lib_fseeko.c,v 1.1 2012/03/14 23:20:10 joerg Exp $
+$NetBSD: patch-lib_fseeko.c,v 1.2 2012/08/11 18:51:16 ryoon Exp $
 
---- lib/fseeko.c.orig	2011-07-15 19:08:43.000000000 +0000
+--- lib/fseeko.c.orig	2012-05-13 15:47:48.000000000 +0000
 +++ lib/fseeko.c
-@@ -111,7 +111,7 @@ fseeko (FILE *fp, off_t offset, int when
- #if defined _IO_ftrylockfile || __GNU_LIBRARY__ == 1 /* GNU libc, BeOS, Haiku, Linux libc5 */
+@@ -121,7 +121,7 @@ fseeko (FILE *fp, off_t offset, int when
        fp->_flags &= ~_IO_EOF_SEEN;
+       fp->_offset = pos;
  #elif defined __sferror || defined __DragonFly__ /* FreeBSD, NetBSD, OpenBSD, DragonFly, MacOS X, Cygwin */
 -# if defined __CYGWIN__
 +# if defined(__CYGWIN__) || defined(__NetBSD__)
