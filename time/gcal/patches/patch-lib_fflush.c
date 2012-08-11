8602dec7bc61da663e06785c06011721e216a19d
ryoon <ryoon@pkgsrc.org>
2012-08-11 18:51:16 +0000

    Update to 3.6.2
    Changelog:
    Major changes in release 3.6.2
    
    * Report correctly the Christmas day in Canada.

--- time/gcal/patches/patch-lib_fflush.c
+++ time/gcal/patches/patch-lib_fflush.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-lib_fflush.c,v 1.1 2012/03/14 23:20:10 joerg Exp $
+$NetBSD: patch-lib_fflush.c,v 1.2 2012/08/11 18:51:16 ryoon Exp $
 
---- lib/fflush.c.orig	2011-03-06 11:54:27.000000000 +0000
+--- lib/fflush.c.orig	2012-04-06 15:05:23.000000000 +0000
 +++ lib/fflush.c
-@@ -91,7 +91,7 @@ static inline void
- update_fpos_cache (FILE *fp, off_t pos)
+@@ -94,7 +94,7 @@ update_fpos_cache (FILE *fp _GL_UNUSED_P
+                    off_t pos _GL_UNUSED_PARAMETER)
  {
  #if defined __sferror || defined __DragonFly__ /* FreeBSD, NetBSD, OpenBSD, DragonFly, MacOS X, Cygwin */
 -# if defined __CYGWIN__
