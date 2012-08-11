2383757427fcc4d0261f6d10285904489ddc9907
marino <marino>
2012-08-11 17:34:04 +0000

    editors/Sigil: Fix patch phase, Repack all patches
    Sigil is delivered in a zip file, and the files have DOS line endings.
    The patches had unix line endings, and at least on some platforms
    including NetBSD 5, this resulted in rejected hunks.
    
    All three patches repacked, now contain DOS line endings and work fine.

--- editors/Sigil/patches/patch-src_ZipArchive_ZipPlatform__lnx.cpp
+++ editors/Sigil/patches/patch-src_ZipArchive_ZipPlatform__lnx.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_ZipArchive_ZipPlatform__lnx.cpp,v 1.1 2011/10/14 09:49:07 ryoon Exp $
+$NetBSD: patch-src_ZipArchive_ZipPlatform__lnx.cpp,v 1.2 2012/08/11 17:34:05 marino Exp $
 
 Add DragonFly support.
 
---- src/ZipArchive/ZipPlatform_lnx.cpp.orig	2011-10-14 18:30:33 +0000
+--- src/ZipArchive/ZipPlatform_lnx.cpp.orig	2012-02-25 10:54:10.000000000 +0000
 +++ src/ZipArchive/ZipPlatform_lnx.cpp
 @@ -16,7 +16,7 @@
  
