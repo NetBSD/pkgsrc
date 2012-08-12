6d3530bd3357425acba816f52d52e618e29abae2
imil <imil@pkgsrc.org>
2012-08-12 16:13:02 +0000

    Initial import of ruby-stompserver, version 0.9.9, into the NetBSD Packages Collection.
    Stompserver handles basic message queue processing.
    
    It is Stomp messaging server with file/dbm/memory/activerecord based FIFO
    queues, queue monitoring, and basic authentication.

---
+++ net/ruby-stompserver/patches/patch-lib_stomp__server.rb
@@ -0,0 +1,15 @@
+$NetBSD: patch-lib_stomp__server.rb,v 1.1 2012/08/12 16:13:03 imil Exp $
+
+Buggy code made pid relative to logdir
+
+--- lib/stomp_server.rb.orig	2012-08-12 15:28:59.000000000 +0000
++++ lib/stomp_server.rb
+@@ -67,7 +67,7 @@ module StompServer
+       opts[:storage] = File.join(opts[:working_dir],opts[:storage])
+       opts[:logdir] = File.join(opts[:working_dir],opts[:logdir])
+       opts[:logfile] = File.join(opts[:logdir],opts[:logfile])
+-      opts[:pidfile] = File.join(opts[:logdir],opts[:pidfile])
++      opts[:pidfile] = File.join(opts[:working_dir],opts[:pidfile])
+       if opts[:auth]
+         opts[:passwd] = File.join(opts[:etcdir],'.passwd')
+       end
