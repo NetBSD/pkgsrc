7e768e6c5451967d4702dc6710419b106f61ae04
marino <marino@pkgsrc.org>
2012-08-12 02:03:15 +0000

    emulators/b-em: Upgrade from version 2.1a to 2.2 (Fixes zlib 1.27)
    Version 2.1a is not compatible with zlib 1.26+.  Luckily this problem
    was fixed in version 2.2.  Three patches were removed, and a few were
    tweaked.
    
    src/linux.c and src/win.c now have dos line endings.  I used the
    technique seen in textproc/FlightCrew to convert these to unix
    endings after extraction.  That should allow this package to build
    on all platforms where zip handling of line ending differs.
    
    Also defined GPLv3 license.

--- emulators/b-em/patches/patch-src-win.c
+++ emulators/b-em/patches/patch-src-win.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-src-win.c,v 1.1 2012/02/20 15:45:19 reinoud Exp $
+$NetBSD: patch-src-win.c,v 1.2 2012/08/12 02:03:15 marino Exp $
 
---- src/win.c.orig	2011-02-12 17:34:10.000000000 +0000
+--- src/win.c.orig	2012-05-22 11:39:06.000000000 +0000
 +++ src/win.c
-@@ -346,6 +346,7 @@ int WINAPI WinMain (HINSTANCE hThisInsta
-         get_executable_name(exedir,511);
-         p=get_filename(exedir);
-         p[0]=0;
-+	strcpy(exedir, "@BEMDATADIR@");
+@@ -345,6 +345,7 @@ int WINAPI WinMain (HINSTANCE hThisInsta
+         get_executable_name(exedir, 511);
+         p = get_filename(exedir);
+         p[0] = 0;
++        strcpy(exedir, "@BEMDATADIR@");
  
-         loadconfig();
+         config_load();
  
