be018c2946c13ea648067a666d081fd325724de5
marino <marino@pkgsrc.org>
2012-08-12 02:03:15 +0000

    emulators/b-em: Upgrade from version 2.1a to 2.2 (Fixes zlib 1.27)
    Version 2.1a is not compatible with zlib 1.26+.  Luckily this problem
    was fixed in version 2.2.  Three patches were removed, and a few were
    tweaked.
    
    src/linux.c and src/win.c now have dos line endings.  I used the
    technique seen in textproc/FlightCrew to convert these to unix
    endings after extraction.  That should allow this package to build
    on all platforms where zip handling of line ending differs.
    
    Also defined GPLv3 license.

--- emulators/b-em/patches/patch-src-linux.c
+++ emulators/b-em/patches/patch-src-linux.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-src-linux.c,v 1.1 2012/02/20 15:45:19 reinoud Exp $
+$NetBSD: patch-src-linux.c,v 1.2 2012/08/12 02:03:15 marino Exp $
 
---- src/linux.c.orig	2011-02-12 15:52:50.000000000 +0000
+--- src/linux.c.orig	2012-04-16 15:37:00.000000000 +0000
 +++ src/linux.c
-@@ -62,6 +62,8 @@ int main(int argc, char *argv[])
-         get_executable_name(exedir,511);
-         p=get_filename(exedir);
-         p[0]=0;
-+	/* data was referred relative to the executable; fix: */
-+	strcpy(exedir, "@BEMDATADIR@");
-         loadconfig();
+@@ -63,6 +63,8 @@ int main(int argc, char *argv[])
+         get_executable_name(exedir, 511);
+         p = get_filename(exedir);
+         p[0] = 0;
++        /* data was referred relative to the executable; fix: */
++        strcpy(exedir, "@BEMDATADIR@");
+         config_load();
  //        printf("Main\n");
-         initbbc(argc,argv);
+         main_init(argc, argv);
