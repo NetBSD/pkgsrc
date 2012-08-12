1965aa699131302f722294622c4b13ce3d569a38
marino <marino@pkgsrc.org>
2012-08-12 11:23:59 +0000

    lang/konoha: Add DragonFly support, kill MySQL detection
    * Specific DragonFly configuration was added
    * A couple of K_USING_BSD macros were changed to __NetBSD__ along with
      other changes to support FreeBSD and DragonFly
    * If MySQL is on the system, CMake will find it and try to use it.
      Problem 1: It's broken, it can't link to libmysql
      Problem 2: If it could, it would affect PLIST
    
    So disable MySQL detection for now, sqlite3 is what's used.
    I suspect there are other CMake detection logic that aren't accounted
    for yet in Konoha.

--- lang/konoha/patches/patch-include_konoha1_konoha__config.h
+++ lang/konoha/patches/patch-include_konoha1_konoha__config.h
@@ -1,6 +1,7 @@
-$NetBSD: patch-include_konoha1_konoha__config.h,v 1.1 2012/08/07 16:06:06 ryoon Exp $
+$NetBSD: patch-include_konoha1_konoha__config.h,v 1.2 2012/08/12 11:24:00 marino Exp $
 
 * Add NetBSD support
+* Add DragonFly support
 
 --- include/konoha1/konoha_config.h.orig	2012-03-07 06:52:14.000000000 +0000
 +++ include/konoha1/konoha_config.h
@@ -22,3 +23,21 @@ $NetBSD: patch-include_konoha1_konoha__config.h,v 1.1 2012/08/07 16:06:06 ryoon 
  #if defined(__MINGW32__)  /* mingw */
  #define		K_USING_STDC_       1
  #define		K_USING_SETJMP_     1
+@@ -328,6 +339,17 @@ typedef struct knh_sysinfo_t {
+ 
+ /* experimetally supported */
+ 
++#if defined(__DragonFly__)
++#define K_USING_PTHREAD
++#define K_USING_STDC_       1
++#define K_USING_POSIX_      1
++#define K_USING_BSD_        1
++#define K_USING_BSDSYSCTL_  1
++#define K_USING_ICONV       1
++#define K_USING_SYSLOG      1
++#define K_USING_SQLITE3     1
++#endif
++
+ #if defined(__FreeBSD__) /* FreeBSD */
+ #define		K_USING_STDC_       1
+ #define 	K_USING_POSIX_      1
