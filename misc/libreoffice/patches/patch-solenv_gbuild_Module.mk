04d4802075af7ed6e3d9e1ce50ed80c6ef6cfc3b
wiz <wiz@pkgsrc.org>
2012-08-12 15:55:55 +0000

    Update to 3.5.6.2 based on ftigeot's version in wip. Security and bug fixes.

--- misc/libreoffice/patches/patch-solenv_gbuild_Module.mk
+++ misc/libreoffice/patches/patch-solenv_gbuild_Module.mk
@@ -1,4 +1,11 @@
-$NetBSD: patch-solenv_gbuild_Module.mk,v 1.1 2012/08/12 07:09:20 marino Exp $
+$NetBSD: patch-solenv_gbuild_Module.mk,v 1.2 2012/08/12 15:55:56 wiz Exp $
+
+Disable unit testing
+
+Some of the libreoffice unit tests, for example system fonts, require
+additional and currently unidentified dependencies to run.  As a
+result, Libreoffice will build outside a clean environment but fails
+to build in pbulk chroot or Tinderbox.
 
 --- solenv/gbuild/Module.mk.orig	2012-08-07 13:32:53.000000000 +0000
 +++ solenv/gbuild/Module.mk
