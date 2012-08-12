0e428e9a07ab013306d8d6ed389444e21fe1dfcf
marino <marino@pkgsrc.org>
2012-08-12 02:03:15 +0000

    emulators/b-em: Upgrade from version 2.1a to 2.2 (Fixes zlib 1.27)
    Version 2.1a is not compatible with zlib 1.26+.  Luckily this problem
    was fixed in version 2.2.  Three patches were removed, and a few were
    tweaked.
    
    src/linux.c and src/win.c now have dos line endings.  I used the
    technique seen in textproc/FlightCrew to convert these to unix
    endings after extraction.  That should allow this package to build
    on all platforms where zip handling of line ending differs.
    
    Also defined GPLv3 license.

--- emulators/b-em/patches/patch-src-resources.h
+++ emulators/b-em/patches/patch-src-resources.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-src-resources.h,v 1.1 2012/02/20 15:45:19 reinoud Exp $
+$NetBSD: patch-src-resources.h,v 1.2 2012/08/12 02:03:15 marino Exp $
 
---- src/resources.h.orig	2010-10-16 21:03:30.000000000 +0000
+--- src/resources.h.orig	2012-04-15 20:46:50.000000000 +0000
 +++ src/resources.h
-@@ -83,5 +83,7 @@
+@@ -87,5 +87,7 @@
  #define IDM_SID_INTERP       41200
  #define IDM_SID_RESAMP       41201
  
