b752038f0a4f5edbdeaa275c7219dec51cbdb851
marino <marino@pkgsrc.org>
2012-08-12 07:09:19 +0000

    misc/libreoffice: Disable unit testing
    Some of the libreoffice unit tests, for example system fonts, require
    additional and currently unidentified dependencies to run.  As a
    result, Libreoffice will build outside a clean environment but fails
    to build in pbulk chroot or Tinderbox.
    
    With this new patch, libreoffice builds cleanly in Tinderbox.
    The modified patch probably makes no difference but it's kept because
    the configuration is known to build in a clean environment.

--- misc/libreoffice/patches/patch-sw_Module_sw.mk
+++ misc/libreoffice/patches/patch-sw_Module_sw.mk
@@ -1,10 +1,12 @@
-$NetBSD: patch-sw_Module_sw.mk,v 1.1.1.1 2012/06/01 06:12:14 wiz Exp $
+$NetBSD: patch-sw_Module_sw.mk,v 1.2 2012/08/12 07:09:20 marino Exp $
 
-Disable unit tests crashing on NetBSD
+Disable an unit test crashing with the error message
+"No fonts could be found on the system"
+in unclear circumstances.
 
---- sw/Module_sw.mk.orig	2012-01-09 22:08:58.000000000 +0000
+--- sw/Module_sw.mk.orig	2012-08-07 13:32:53.000000000 +0000
 +++ sw/Module_sw.mk
-@@ -40,11 +40,13 @@ $(eval $(call gb_Module_add_targets,sw,\
+@@ -40,11 +40,12 @@ $(eval $(call gb_Module_add_targets,sw,\
      Package_xml \
  ))
  
@@ -12,7 +14,7 @@ Disable unit tests crashing on NetBSD
  $(eval $(call gb_Module_add_check_targets,sw,\
      CppunitTest_sw_filters_test \
      CppunitTest_sw_macros_test \
-     CppunitTest_sw_swdoc_test \
+-    CppunitTest_sw_swdoc_test \
  ))
 +endif
  
