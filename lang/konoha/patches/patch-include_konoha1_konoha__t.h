d49237d689ac04987e96a047ab12880a93e4842c
marino <marino@pkgsrc.org>
2012-08-12 11:23:59 +0000

    lang/konoha: Add DragonFly support, kill MySQL detection
    * Specific DragonFly configuration was added
    * A couple of K_USING_BSD macros were changed to __NetBSD__ along with
      other changes to support FreeBSD and DragonFly
    * If MySQL is on the system, CMake will find it and try to use it.
      Problem 1: It's broken, it can't link to libmysql
      Problem 2: If it could, it would affect PLIST
    
    So disable MySQL detection for now, sqlite3 is what's used.
    I suspect there are other CMake detection logic that aren't accounted
    for yet in Konoha.

--- lang/konoha/patches/patch-include_konoha1_konoha__t.h
+++ lang/konoha/patches/patch-include_konoha1_konoha__t.h
@@ -1,17 +1,22 @@
-$NetBSD: patch-include_konoha1_konoha__t.h,v 1.1 2012/08/07 16:06:06 ryoon Exp $
+$NetBSD: patch-include_konoha1_konoha__t.h,v 1.2 2012/08/12 11:24:00 marino Exp $
 
 * Try to support *BSD
+* FreeBSD and DragonFly have NSIG exposed
 
 --- include/konoha1/konoha_t.h.orig	2012-03-07 06:52:14.000000000 +0000
 +++ include/konoha1/konoha_t.h
-@@ -1041,6 +1041,10 @@ typedef struct kcontext_t {
+@@ -1041,6 +1041,14 @@ typedef struct kcontext_t {
  	void                           *siginfo;
  #if defined(K_USING_MINGW_)
  #define K_SIGNAL_MAX NSIG
 +#elif defined(K_USING_MACOSX_)
 +#define K_SIGNAL_MAX _POSIX_SIGQUEUE_MAX
 +#elif defined(K_USING_BSD_)
++#ifdef __NetBSD__
 +#define K_SIGNAL_MAX _NSIG /* I am not sure. */
++#else
++#define K_SIGNAL_MAX NSIG
++#endif
  #else
  #define K_SIGNAL_MAX _POSIX_SIGQUEUE_MAX
  #endif
