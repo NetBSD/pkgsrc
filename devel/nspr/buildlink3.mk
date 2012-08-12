45770c59136456a210ccedd939d65ac743d012ae
ryoon <ryoon@pkgsrc.org>
2012-08-12 15:19:07 +0000

    Update to 4.9.3.2 (really 4.9.2)
    * API have changed
    
    Changelog:
    The only change in NSPR 4.9.2 is the new functions to set and get thread names:
    Bug 758837 - Add functions to set and get thread names

--- devel/nspr/buildlink3.mk
+++ devel/nspr/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.22 2012/05/07 01:53:25 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2012/08/12 15:19:07 ryoon Exp $
 
 BUILDLINK_TREE+=	nspr
 
 .if !defined(NSPR_BUILDLINK3_MK)
 NSPR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.9.2beta1
+BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.9.3.2
 BUILDLINK_ABI_DEPENDS.nspr+=	nspr>=4.9.2beta1
 BUILDLINK_PKGSRCDIR.nspr?=	../../devel/nspr
 
