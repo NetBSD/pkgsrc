c6f7065307ab6fa741d47c5ac941d4f2fe32e2ad
wiz <wiz@pkgsrc.org>
2012-08-13 14:25:37 +0000

    Bump PKGREVISION for fix in librsvg/buildlink3.mk.

--- x11/gnome-desktop-sharp/buildlink3.mk
+++ x11/gnome-desktop-sharp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.27 2012/07/15 08:22:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2012/08/13 14:25:44 wiz Exp $
 
 BUILDLINK_TREE+=	gnome-desktop-sharp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gnome-desktop-sharp
 GNOME_DESKTOP_SHARP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gnome-desktop-sharp+=	gnome-desktop-sharp>=2.24.0
-BUILDLINK_ABI_DEPENDS.gnome-desktop-sharp+=	gnome-desktop-sharp>=2.26.0nb28
+BUILDLINK_ABI_DEPENDS.gnome-desktop-sharp+=	gnome-desktop-sharp>=2.26.0nb29
 BUILDLINK_PKGSRCDIR.gnome-desktop-sharp?=	../../x11/gnome-desktop-sharp
 
 .include "../../devel/libwnck/buildlink3.mk"
