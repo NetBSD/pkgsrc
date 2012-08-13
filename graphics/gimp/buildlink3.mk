3ebfad88245bf3cab49c490708fd606c342bf7ab
wiz <wiz@pkgsrc.org>
2012-08-13 14:25:37 +0000

    Bump PKGREVISION for fix in librsvg/buildlink3.mk.

--- graphics/gimp/buildlink3.mk
+++ graphics/gimp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.40 2012/06/14 07:43:24 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.41 2012/08/13 14:25:40 wiz Exp $
 
 BUILDLINK_TREE+=	gimp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gimp
 GIMP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gimp+=	gimp>=2.4.0
-BUILDLINK_ABI_DEPENDS.gimp+=	gimp>=2.8.0nb1
+BUILDLINK_ABI_DEPENDS.gimp+=	gimp>=2.8.0nb2
 BUILDLINK_PKGSRCDIR.gimp?=	../../graphics/gimp
 
 pkgbase := gimp
--- graphics/osg/buildlink3.mk
+++ graphics/osg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2012/06/14 07:43:24 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2012/08/13 14:25:41 wiz Exp $
 
 BUILDLINK_TREE+=	osg
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	osg
 OSG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.osg+=	osg>=2.8.3
-BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.0.1nb2
+BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.0.1nb4
 BUILDLINK_PKGSRCDIR.osg?=	../../graphics/osg
 
 .include "../../graphics/cairo/buildlink3.mk"
