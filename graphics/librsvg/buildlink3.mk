e68b116c96103fb7dee2401401ab6e74c156b730
wiz <wiz@pkgsrc.org>
2012-08-13 14:25:00 +0000

    Correct dependencies in buildlink3.mk: Does not forward libcroco dependency DOES forward cairo dependency (see .pc file)
    Bump PKGREVISION so that the upcoming revbump makes sense.

--- graphics/librsvg/buildlink3.mk
+++ graphics/librsvg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.24 2012/06/14 07:43:24 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2012/08/13 14:25:00 wiz Exp $
 
 BUILDLINK_TREE+=	librsvg
 
@@ -6,10 +6,10 @@ BUILDLINK_TREE+=	librsvg
 LIBRSVG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.librsvg+=		librsvg>=2.12.6nb1
-BUILDLINK_ABI_DEPENDS.librsvg+=		librsvg>=2.36.1nb1
+BUILDLINK_ABI_DEPENDS.librsvg+=		librsvg>=2.36.1nb2
 BUILDLINK_PKGSRCDIR.librsvg?=		../../graphics/librsvg
 
-.include "../../textproc/libcroco/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
 .include "../../graphics/gdk-pixbuf2/buildlink3.mk"
 .endif # LIBRSVG_BUILDLINK3_MK
 
