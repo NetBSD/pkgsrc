5913f4643532886a8865c62ae121f47b757661b1
wiz <wiz@pkgsrc.org>
2012-08-14 07:02:09 +0000

    Update to 0.11.3. shlib name changed, so update bl3.mk, but not package depends on it.
    3 years of changes, but no NEWS found.

--- audio/celt/buildlink3.mk
+++ audio/celt/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:23:52 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/08/14 07:02:09 wiz Exp $
 
 BUILDLINK_TREE+=	celt
 
 .if !defined(CELT_BUILDLINK3_MK)
 CELT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.celt+=	celt>=0.5.0
+BUILDLINK_API_DEPENDS.celt+=	celt>=0.11.3
 BUILDLINK_PKGSRCDIR.celt?=	../../audio/celt
 pkgbase:= celt
 .  include "../../mk/pkg-build-options.mk"
