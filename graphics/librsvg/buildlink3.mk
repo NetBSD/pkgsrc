a2c33566ed1bb38d8d39714f7be07281961e9ce6
drochner <drochner@pkgsrc.org>
2012-08-14 10:40:51 +0000

    librsvg links against libpangocairo, propagate this dependency too

--- graphics/librsvg/buildlink3.mk
+++ graphics/librsvg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2012/08/14 08:46:17 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2012/08/14 10:40:51 drochner Exp $
 
 BUILDLINK_TREE+=	librsvg
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.librsvg+=		librsvg>=2.12.6nb1
 BUILDLINK_ABI_DEPENDS.librsvg+=		librsvg>=2.36.1nb2
 BUILDLINK_PKGSRCDIR.librsvg?=		../../graphics/librsvg
 
+.include "../../devel/pango/buildlink3.mk"
 .include "../../graphics/cairo/buildlink3.mk"
 .include "../../graphics/gdk-pixbuf2/buildlink3.mk"
 .include "../../textproc/libcroco/buildlink3.mk"
