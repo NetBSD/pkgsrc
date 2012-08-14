fa34e67763f1dfc088a7a5c6d8b4e9b67480ccf4
markd <markd@pkgsrc.org>
2012-08-14 21:00:46 +0000

    Update libmtp to version 1.1.3
    Changes unknown other than now supports Samsung Galaxy phones.

--- devel/libmtp/buildlink3.mk
+++ devel/libmtp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:20 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/08/14 21:00:46 markd Exp $
 
 BUILDLINK_TREE+=	libmtp
 
@@ -8,7 +8,7 @@ LIBMTP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libmtp+=	libmtp>=0.3.0
 BUILDLINK_PKGSRCDIR.libmtp?=	../../devel/libmtp
 
-.include "../../devel/libusb/buildlink3.mk"
+.include "../../devel/libusb1/buildlink3.mk"
 .endif # LIBMTP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libmtp
