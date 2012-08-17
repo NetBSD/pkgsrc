fd7ffda9a2ea8b72a033f81fc79ecdd4d23f85ed
wiz <wiz>
2012-08-17 12:15:37 +0000

    Better support for socket buffer size limits. Clean up env usage (not needed any longer). Bump PKGREVISION.

--- shells/zsh/patches/patch-Src_Builtins_rlimits.c
+++ shells/zsh/patches/patch-Src_Builtins_rlimits.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-Src_Builtins_rlimits.c,v 1.1 2012/08/17 10:53:10 wiz Exp $
+$NetBSD: patch-Src_Builtins_rlimits.c,v 1.2 2012/08/17 12:15:37 wiz Exp $
 
-Add support for thread limits.
+Add support for thread limits and socket buffer size.
 From Peter Stephenson <Peter.Stephenson@csr.com> on zsh-workers.
 
 --- Src/Builtins/rlimits.c.orig	2011-11-04 20:47:24.000000000 +0000
@@ -18,7 +18,42 @@ From Peter Stephenson <Peter.Stephenson@csr.com> on zsh-workers.
  # if defined(HAVE_RLIMIT_VMEM) && (!defined(HAVE_RLIMIT_RSS) || !defined(RLIMIT_VMEM_IS_RSS))
      case RLIMIT_VMEM:
  	if (head)
-@@ -791,6 +797,11 @@ bin_ulimit(char *name, char **argv, UNUS
+@@ -359,9 +365,7 @@ printulimit(char *nam, int lim, int hard
+ # ifdef HAVE_RLIMIT_SBSIZE
+     case RLIMIT_SBSIZE:
+ 	if (head)
+-	    printf("-N %2d: socket buffer size (kb) ", RLIMIT_SBSIZE);
+-	if (limit != RLIM_INFINITY)
+-	    limit /= 1024;
++	    printf("-b: socket buffer size (bytes) ", RLIMIT_SBSIZE);
+ 	break;
+ # endif /* HAVE_RLIMIT_SBSIZE */
+ # ifdef HAVE_RLIMIT_PTHREAD
+@@ -776,21 +780,31 @@ bin_ulimit(char *name, char **argv, UNUS
+ 		case 'c':
+ 		    res = RLIMIT_CORE;
+ 		    break;
+-# ifdef HAVE_RLIMIT_RSS
+-		case 'm':
+-		    res = RLIMIT_RSS;
++# ifdef HAVE_RLIMIT_SBSIZE
++		case 'b':
++		    res = RLIMIT_SBSIZE;
+ 		    break;
+-# endif /* HAVE_RLIMIT_RSS */
++# endif /* HAVE_RLIMIT_SBSIZE */
+ # ifdef HAVE_RLIMIT_MEMLOCK
+ 		case 'l':
+ 		    res = RLIMIT_MEMLOCK;
+ 		    break;
+ # endif /* HAVE_RLIMIT_MEMLOCK */
++# ifdef HAVE_RLIMIT_RSS
++		case 'm':
++		    res = RLIMIT_RSS;
++		    break;
++# endif /* HAVE_RLIMIT_RSS */
+ # ifdef HAVE_RLIMIT_NOFILE
+ 		case 'n':
  		    res = RLIMIT_NOFILE;
  		    break;
  # endif /* HAVE_RLIMIT_NOFILE */
