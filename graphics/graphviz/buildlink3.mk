d884f5f9ecb1689eae5bcb713d0c82e0401c266e
mef <mef@pkgsrc.org>
2012-08-17 01:02:05 +0000

    Fix PR pkg/46681. PLIST of doxygen varies depending on graphviz configuration. Say, graphviz configured with PKG_OPTIONS.pangocairo yes or no.
    Graphviz/buildlink3.mk will set PKG_BUILD_OPTIONS.graphviz
    for use with doxygen PLIST variation.
    
    Thanks Ian McWilliam for reporting and testing the problem.

--- graphics/graphviz/buildlink3.mk
+++ graphics/graphviz/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2011/11/01 06:01:36 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2012/08/17 01:02:05 mef Exp $
 
 BUILDLINK_TREE+=	graphviz
 
@@ -8,6 +8,10 @@ GRAPHVIZ_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.graphviz+=	graphviz>=2.26.3
 BUILDLINK_ABI_DEPENDS.graphviz+=	graphviz>=2.26.3nb4
 BUILDLINK_PKGSRCDIR.graphviz?=	../../graphics/graphviz
+
+# doxygen PLIST varies with pangocairo of PKG_OPTIONS 
+pkgbase := graphviz
+.include "../../mk/pkg-build-options.mk"
 
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
