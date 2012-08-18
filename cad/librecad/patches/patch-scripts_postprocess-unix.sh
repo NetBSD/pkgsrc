d7dbf96521cf4e78f1f1fd7de553ede63efc0bf6
ryoon <ryoon@pkgsrc.org>
2012-08-18 06:58:15 +0000

    Update to 1.0.2
    * Use shells/bash for post preparation script
    
    Changelog:
    unknown

--- cad/librecad/patches/patch-scripts_postprocess-unix.sh
+++ cad/librecad/patches/patch-scripts_postprocess-unix.sh
@@ -1,31 +1,25 @@
-$NetBSD: patch-scripts_postprocess-unix.sh,v 1.1.1.1 2012/01/26 21:43:23 ryoon Exp $
+$NetBSD: patch-scripts_postprocess-unix.sh,v 1.2 2012/08/18 06:58:15 ryoon Exp $
 
 * Set qt4 tools path
 * Remove GNU find option
 
---- scripts/postprocess-unix.sh.orig	2012-01-18 05:38:57.000000000 +0000
+--- scripts/postprocess-unix.sh.orig	2012-04-15 20:54:52.000000000 +0000
 +++ scripts/postprocess-unix.sh
-@@ -7,7 +7,7 @@ DOCDIR="`pwd`/support/doc"
+@@ -7,7 +7,7 @@ DOCDIR="${THISDIR}/support/doc"
  
  # Generate Help Files
- cd "$DOCDIR"
+ cd "{$DOCDIR}"
 -qcollectiongenerator LibreCADdoc.qhcp
 +@QTDIR@/bin/qcollectiongenerator LibreCADdoc.qhcp
  
- cd "$THISDIR"
+ cd "${THISDIR}"
  
-@@ -20,11 +20,11 @@ cp support/patterns/*.dxf $RESOURCEDIR/p
- cp support/fonts/*.lff* $RESOURCEDIR/fonts
- #cp support/doc/*.qhc $RESOURCEDIR/doc
- #cp support/doc/*.qch $RESOURCEDIR/doc
--find support/library -type d -not -path "*.svn*"  | sed s/support// | xargs -IFILES  mkdir $RESOURCEDIR/FILES
--find support/library -type f -iname *.dxf -not -path "*.svn*"  | sed s/support// | xargs -IFILES  cp support/FILES $RESOURCEDIR/FILES
-+find support/library -type d  | sed s/support// | xargs -IFILES  mkdir $RESOURCEDIR/FILES
-+find support/library -type f -iname *.dxf | sed s/support// | xargs -IFILES  cp support/FILES $RESOURCEDIR/FILES
+@@ -24,7 +24,7 @@ find support/library -type d | sed s/sup
+ find support/library -type f -iname *.dxf | sed s/support// | xargs -IFILES  cp support/FILES "${RESOURCEDIR}"/FILES
  
  # Generate translations
 -lrelease librecad.pro
 +@QTDIR@/bin/lrelease librecad.pro
- mkdir -p $RESOURCEDIR/qm
+ mkdir -p "${RESOURCEDIR}"/qm
   
  # Go into translations directory
