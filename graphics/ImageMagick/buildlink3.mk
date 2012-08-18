abdf4fbd97e0d3f1fb286c2951a3ce56959e5444
ryoon <ryoon@pkgsrc.org>
2012-08-18 08:49:40 +0000

    Recursive revbump from graphics/libwebp update. Thank you, obache@.

--- graphics/ImageMagick/buildlink3.mk
+++ graphics/ImageMagick/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.41 2012/06/14 07:43:11 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.42 2012/08/18 08:49:43 ryoon Exp $
 
 BUILDLINK_TREE+=	ImageMagick
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ImageMagick
 IMAGEMAGICK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ImageMagick+=	ImageMagick>=5.5.7.11nb1
-BUILDLINK_ABI_DEPENDS.ImageMagick+=	ImageMagick>=6.7.6.6nb2
+BUILDLINK_ABI_DEPENDS.ImageMagick+=	ImageMagick>=6.7.6.6nb3
 BUILDLINK_PKGSRCDIR.ImageMagick?=	../../graphics/ImageMagick
 
 pkgbase := ImageMagick
