acbc708022ac7f33754df1d74e1fd8236439444c
ryoon <ryoon@pkgsrc.org>
2012-08-18 01:34:55 +0000

    Update to 0.2.0
    * Add BUILDLINK_ABI_DEPENDS to buildlink3.mk
    
    Changelog:
    - 8/3/12: version 0.2.0
      * Add support for ARGB -> YUVA conversion for lossless decoder
        New functions: WebPINewYUVA, WebPIDecGetYUVA
      * Add stats for lossless and alpha encoding
      * Security related hardening: allocation and size checks
      * Add PAM output support to dwebp
    
    - 7/19/12: version 0.1.99
      * This is a pre-release of 0.2.0, not an rc to allow for further
        incompatible changes based on user feedback.
      * Alpha channel encode/decode support.
      * Lossless encoder/decoder.
      * Add TIFF input support to cwebp.
      Incompatible changes:
        * The encode ABI has been modified to support alpha encoding.
        * Deprecated function WebPINew() has been removed.
        * Decode function signatures have changed to consistently use size_t over
          int/uint32_t.
        * decode_vp8.h is no longer installed system-wide.
        * cwebp will encode the alpha channel if present.

--- graphics/libwebp/buildlink3.mk
+++ graphics/libwebp/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/10/28 13:48:00 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/08/18 01:34:55 ryoon Exp $
 
 BUILDLINK_TREE+=	libwebp
 
 .if !defined(LIBWEBP_BUILDLINK3_MK)
 LIBWEBP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libwebp+=	libwebp>=0.1.3
+BUILDLINK_API_DEPENDS.libwebp+=	libwebp>=0.2.0
+BUILDLINK_ABI_DEPENDS.libwebp+=	libwebp>=0.2.0
 BUILDLINK_PKGSRCDIR.libwebp?=	../../graphics/libwebp
 
 .endif	# LIBWEBP_BUILDLINK3_MK
