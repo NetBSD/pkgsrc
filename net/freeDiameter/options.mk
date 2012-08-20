30d97275a4385aa0a2df6ca0c9d8a1a5d6fd6801
wiz <wiz>
2012-08-20 12:17:20 +0000

    Import freeDiameter-1.1.2 as net/freeDiameter.
    freeDiameter is a implementation of the Diameter protocol specified
    in RFC3588. Diameter is a protocol for carrying Authentication,
    Authorization and Accounting information. It is an evolution of
    the widely-deployed RADIUS protocol. freeDiameter also implements
    the changes to the Diameter protocol introduced by the rfc3588bis
    Internet Draft (work in progress).
    
    freeDiameter is open-source and released under a BSD compatible
    license. It is mainly written in the C language, which provides
    excellent performance and small memory footprint.

---
+++ net/freeDiameter/options.mk
@@ -0,0 +1,30 @@
+# $NetBSD: options.mk,v 1.1 2012/08/20 12:17:20 wiz Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.freeDiameter
+PKG_SUPPORTED_OPTIONS=	freediameter-all-extensions
+PKG_SUGGESTED_OPTIONS=	freediameter-all-extensions
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	ext
+
+.if !empty(PKG_OPTIONS:Mall-extensions)
+# enable all except debugging and test
+#CMAKE_ARGS+=    -DALL_EXTENSIONS:BOOL=ON
+CMAKE_ARGS+=    -DBUILD_APP_ACCT:BOOL=ON
+CMAKE_ARGS+=    -DBUILD_APP_DIAMEAP:BOOL=ON
+CMAKE_ARGS+=    -DBUILD_APP_RADGW:BOOL=ON
+CMAKE_ARGS+=    -DBUILD_APP_REDIRECT:BOOL=ON
+CMAKE_ARGS+=    -DBUILD_APP_SIP:BOOL=ON
+CMAKE_ARGS+=    -DBUILD_DICT_LEGACY_XML:BOOL=ON
+CMAKE_ARGS+=    -DBUILD_RT_EREG:BOOL=ON
+# for debug-interactive-extension only
+#.include "../../devel/swig/buildlink3.mk"
+#.include "../../lang/python/extension.mk"
+.include "../../mk/mysql.buildlink3.mk"
+.include "../../mk/pgsql.buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+PLIST.ext=	yes
+.else
+CMAKE_ARGS+=    -DALL_EXTENSIONS:BOOL=OFF
+.endif
