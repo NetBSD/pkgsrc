6adc246de423a71765b8d68fab73862b6dbbd44a
tron <tron@pkgsrc.org>
2012-08-20 06:50:21 +0000

    Fix name of the hack for Mac OS X Lion.

--- games/scummvm/hacks.mk
+++ games/scummvm/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.2 2012/06/17 00:22:10 tron Exp $
+# $NetBSD: hacks.mk,v 1.3 2012/08/20 06:50:21 tron Exp $
 
 ### [Sun 17 Jun 2012 00:21:30 UTC : tron]
 ### G++ 4.2.1 (Based on Apple Inc. build 5658) as shipped with
@@ -7,6 +7,6 @@
 ### the crash.
 
 .if !empty(MACHINE_PLATFORM:MDarwin-11.*-i386)
-PKG_HACKS+=		macosx-lion-compiler-hang
+PKG_HACKS+=		macosx-lion-compiler-crash
 BUILDLINK_TRANSFORM+=	rm:-O2
 .endif
