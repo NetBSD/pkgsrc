84e2a5935299b1f69dbe176eae88a001af59e047
prlw1 <prlw1@pkgsrc.org>
2012-08-23 16:27:43 +0000

    Add gnome-desktop 3.4.2 for GNOME 3

---
+++ x11/gnome-desktop3/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/08/23 16:27:43 prlw1 Exp $
+
+BUILDLINK_TREE+=	gnome-desktop3
+
+.if !defined(GNOME_DESKTOP3_BUILDLINK3_MK)
+GNOME_DESKTOP3_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gnome-desktop3+=	gnome-desktop3>=3.4.2
+BUILDLINK_ABI_DEPENDS.gnome-desktop3+=	gnome-desktop3>=3.4.2
+BUILDLINK_PKGSRCDIR.gnome-desktop3?=	../../x11/gnome-desktop3
+
+.include "../../x11/gtk3/buildlink3.mk"
+.include "../../x11/startup-notification/buildlink3.mk"
+.endif # GNOME_DESKTOP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gnome-desktop3
