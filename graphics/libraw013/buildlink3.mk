cc4ffbaaae5ee7fbd08620dc617f5929cf478549
dholland <dholland@pkgsrc.org>
2012-08-23 23:01:01 +0000

    Restrict the version to <0.14.

--- graphics/libraw013/buildlink3.mk
+++ graphics/libraw013/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/08/04 14:31:27 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/08/23 23:01:01 dholland Exp $
 
 BUILDLINK_TREE+=	libraw
 
 .if !defined(LIBRAW_BUILDLINK3_MK)
 LIBRAW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libraw+=	libraw>=0.12.0
+BUILDLINK_API_DEPENDS.libraw+=	libraw>=0.12.0<0.14
 BUILDLINK_PKGSRCDIR.libraw?=	../../graphics/libraw013
 
 .include "../../graphics/lcms/buildlink3.mk"
