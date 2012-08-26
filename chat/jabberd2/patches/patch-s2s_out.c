ddaee21c763e12feb219ab1b6a2b616fbd2b52cf
adam <adam@pkgsrc.org>
2012-08-26 18:25:47 +0000

    Changes 2.2.17: * Fixed possibility of Unsolicited Dialback Attacks * Use XML_SetHashSalt with expat 2.1.x only * Fix s2s crash with witelist enabled

--- chat/jabberd2/patches/patch-s2s_out.c
+++ chat/jabberd2/patches/patch-s2s_out.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-s2s_out.c,v 1.3 2012/05/12 05:44:46 adam Exp $
+$NetBSD: patch-s2s_out.c,v 1.4 2012/08/26 18:25:47 adam Exp $
 
 Non-void function must return a value.
 
 --- s2s/out.c.orig	2012-05-12 05:21:36.000000000 +0000
 +++ s2s/out.c
-@@ -632,7 +632,7 @@ int out_packet(s2s_t s2s, pkt_t pkt) {
+@@ -627,7 +627,7 @@ int out_packet(s2s_t s2s, pkt_t pkt) {
              nad_free(pkt->nad);
          free(pkt);
  
