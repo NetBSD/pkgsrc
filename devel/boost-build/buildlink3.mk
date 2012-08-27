03a0005aff3de0c8b5aa76b15e24e51e0b31edd3
adam <adam@pkgsrc.org>
2012-08-27 08:49:08 +0000

    Changes 1.51.0: New Libraries * Context: Context switching library, from Oliver Kowalke. Updated Libraries * Algorithm: - Fixed is_sorted_until ; now matches the c++11 standard behavior on ranges with   duplicate values. - Added range support for the rest of the search algorithms. - unhex now uses Boost.Exception to signal bad input. * Asio: - Fixed an incompatibility between ip::tcp::iostream and C++11. - Decorated GCC attribute names with underscores to prevent interaction with   user-defined macros. - Added missing #include <cctype>, needed for some versions of MinGW. - Changed to use gcc's atomic builtins on ARM CPUs, when available. - Changed strand destruction to be a no-op, to allow strand objects to be   destroyed after their associated io_service has been destroyed. - Added support for some newer versions of glibc which provide the   epoll_create1() function but always fail with ENOSYS. - Changed the SSL implementation to throw an exception if SSL engine   initialisation fails. - Fixed another regression in buffered_write_stream. - Implemented various minor performance improvements, primarily targeted at   Linux x86 and x86-64 platforms. * Config: - Deprecated a whole bunch of macros that were c++11 specific, but not named to   show that they were c++11 specific. Made new macros with better names, and   paired the old macros with the new ones. Updated the documentation to list   the deprecated macros. [more..]

--- devel/boost-build/buildlink3.mk
+++ devel/boost-build/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.22 2012/07/02 07:02:25 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2012/08/27 08:49:09 adam Exp $
 
 BUILDLINK_TREE+=	boost-build
 
 .if !defined(BOOST_BUILD_BUILDLINK3_MK)
 BOOST_BUILD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.50.*
+BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.51.*
 BUILDLINK_DEPMETHOD.boost-build?=	build
 BUILDLINK_PKGSRCDIR.boost-build?=	../../devel/boost-build
 .endif # BOOST_BUILD_BUILDLINK3_MK
