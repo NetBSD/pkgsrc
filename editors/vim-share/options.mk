7ae8be69fd86bd880102eaca84dc00c3aa84e800
morr <morr@pkgsrc.org>
2012-08-27 22:10:34 +0000

    regenerate distinfo and make pkglint happier

--- editors/vim-share/options.mk
+++ editors/vim-share/options.mk
@@ -1,3 +1,5 @@
+# $NetBSD: options.mk,v 1.2 2012/08/27 22:10:34 morr Exp $
+
 PKG_SUPPORTED_OPTIONS+=	ruby python
 PKG_OPTIONS_VAR=	PKG_OPTIONS.vim
 
@@ -13,4 +15,3 @@ CONFIGURE_ARGS+=	--enable-rubyinterp
 CONFIGURE_ARGS+=	--enable-pythoninterp
 CONFIGURE_ENV+=		vi_cv_path_python=${PYTHONBIN}
 .endif
-
