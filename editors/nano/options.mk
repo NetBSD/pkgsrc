9a794d85a6c10b796cacf93e8d83b174ca080616
ryoon <ryoon@pkgsrc.org>
2012-08-27 10:37:50 +0000

    Update to 2.2.6
    * Replace PKG_LEGACY_OPTS with PKG_OPTIONS_LEGACY_OPTS
    
    Changelog:
    2010.11.22 - GNU nano 2.2.6 "Pimp my BBS" wants you to go to
                    www.desertbus.org and donate a few bucks for the great
                    Child's Play Charity!  This is just a small release to
                    update a bug where restricted mode was not particularly
                    restricted since key bindings were introduced. It also
                    signals the return of win32 builds which now feature
                    nanorc support; please see the FAQ for details of how
                    to enable it, this feature is a bit of a kludge for now.
                    Remember that when all else fails, USE SPACE JUMP.
    
    2010.08.05 - GNU nano 2.2.5 "Inactivity timeout" is now available.
                    This release includes slightly less restrictive checkng
                    when writing files in strange environments (e.g.
                    when being used out of crontab).  For very strange situations
                    (such as where you cannot change the permissions on the
                    file you're writing, there is a new rc file option
                    "allow_insecure_backup" to be even more permissive and
                    allowing the write to proceed.  Also included are
                    some syntax highlighting updated, and that is about it.
                    Keep fighting the good fight children.

--- editors/nano/options.mk
+++ editors/nano/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2008/02/21 21:56:29 jlam Exp $
+# $NetBSD: options.mk,v 1.3 2012/08/27 10:37:51 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nano
 PKG_SUPPORTED_OPTIONS=	wide-curses
-PKG_LEGACY_OPTS+=	ncursesw:wide-curses
+PKG_OPTIONS_LEGACY_OPTS+=	ncursesw:wide-curses
 
 .include "../../mk/bsd.options.mk"
 
