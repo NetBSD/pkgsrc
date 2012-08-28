f3d0ede30a16d88f04e2792a61135e043349afd9
ryoon <ryoon@pkgsrc.org>
2012-08-28 13:29:15 +0000

    Bump PKGREVISION
    * Utilize pthread_set_name_np or pthread_setname_np on DragonFly or NetBSD.
      Fix build of devel/xulrunner.
    * Regen patches

--- devel/nspr/patches/patch-nsprpub_pr_src_pthreads_ptsynch.c
+++ devel/nspr/patches/patch-nsprpub_pr_src_pthreads_ptsynch.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-nsprpub_pr_src_pthreads_ptsynch.c,v 1.1 2012/04/18 20:57:33 ryoon Exp $
+$NetBSD: patch-nsprpub_pr_src_pthreads_ptsynch.c,v 1.2 2012/08/28 13:29:17 ryoon Exp $
 
---- nsprpub/pr/src/pthreads/ptsynch.c.orig	2011-09-02 20:15:44.000000000 +0000
+--- nsprpub/pr/src/pthreads/ptsynch.c.orig	2012-03-06 13:14:21.000000000 +0000
 +++ nsprpub/pr/src/pthreads/ptsynch.c
-@@ -842,7 +842,7 @@ PR_IMPLEMENT(PRStatus) PR_DeleteSemaphor
+@@ -810,7 +810,7 @@ PR_IMPLEMENT(PRStatus) PR_DeleteSemaphor
   */
  #if (defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)) \
      || defined(FREEBSD) || defined(OPENBSD) || defined(BSDI) \
