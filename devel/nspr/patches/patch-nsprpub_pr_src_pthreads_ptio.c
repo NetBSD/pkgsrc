51358e1dc89d22b8dbd3e52aca79c815a3cc89e1
ryoon <ryoon@pkgsrc.org>
2012-08-28 13:29:15 +0000

    Bump PKGREVISION
    * Utilize pthread_set_name_np or pthread_setname_np on DragonFly or NetBSD.
      Fix build of devel/xulrunner.
    * Regen patches

--- devel/nspr/patches/patch-nsprpub_pr_src_pthreads_ptio.c
+++ devel/nspr/patches/patch-nsprpub_pr_src_pthreads_ptio.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-nsprpub_pr_src_pthreads_ptio.c,v 1.1 2012/04/18 20:57:33 ryoon Exp $
+$NetBSD: patch-nsprpub_pr_src_pthreads_ptio.c,v 1.2 2012/08/28 13:29:17 ryoon Exp $
 
---- nsprpub/pr/src/pthreads/ptio.c.orig	2011-09-02 20:15:44.000000000 +0000
+--- nsprpub/pr/src/pthreads/ptio.c.orig	2012-03-06 13:14:21.000000000 +0000
 +++ nsprpub/pr/src/pthreads/ptio.c
-@@ -212,7 +212,8 @@ static PRBool _pr_ipv6_v6only_on_by_defa
+@@ -180,7 +180,8 @@ static PRBool _pr_ipv6_v6only_on_by_defa
      || defined(LINUX) || defined(__GNU__) || defined(__GLIBC__) \
      || defined(FREEBSD) || defined(NETBSD) || defined(OPENBSD) \
      || defined(BSDI) || defined(NTO) || defined(DARWIN) \
@@ -12,7 +12,7 @@ $NetBSD: patch-nsprpub_pr_src_pthreads_ptio.c,v 1.1 2012/04/18 20:57:33 ryoon Ex
  #define _PRSelectFdSetArg_t fd_set *
  #else
  #error "Cannot determine architecture"
-@@ -3285,7 +3286,7 @@ static PRIOMethods _pr_socketpollfd_meth
+@@ -3254,7 +3255,7 @@ static PRIOMethods _pr_socketpollfd_meth
      || defined(AIX) || defined(FREEBSD) || defined(NETBSD) \
      || defined(OPENBSD) || defined(BSDI) || defined(NTO) \
      || defined(DARWIN) || defined(UNIXWARE) || defined(RISCOS) \
