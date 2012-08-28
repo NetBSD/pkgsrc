86308733e53728b26fe1c67206e9a173501a0188
ryoon <ryoon>
2012-08-28 13:29:15 +0000

    Bump PKGREVISION
    * Utilize pthread_set_name_np or pthread_setname_np on DragonFly or NetBSD.
      Fix build of devel/xulrunner.
    * Regen patches

--- devel/nspr/patches/patch-nsprpub_pr_include_md__pth.h
+++ devel/nspr/patches/patch-nsprpub_pr_include_md__pth.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-nsprpub_pr_include_md__pth.h,v 1.1 2012/04/18 20:57:33 ryoon Exp $
+$NetBSD: patch-nsprpub_pr_include_md__pth.h,v 1.2 2012/08/28 13:29:17 ryoon Exp $
 
---- nsprpub/pr/include/md/_pth.h.orig	2011-09-02 20:15:44.000000000 +0000
+--- nsprpub/pr/include/md/_pth.h.orig	2012-05-18 21:55:29.000000000 +0000
 +++ nsprpub/pr/include/md/_pth.h
-@@ -146,7 +146,7 @@
+@@ -114,7 +114,7 @@
  #define _PT_PTHREAD_COPY_THR_HANDLE(st, dt)   (dt) = (st)
  #elif defined(IRIX) || defined(OSF1) || defined(AIX) || defined(SOLARIS) \
  	|| defined(LINUX) || defined(__GNU__) || defined(__GLIBC__) \
@@ -11,7 +11,7 @@ $NetBSD: patch-nsprpub_pr_include_md__pth.h,v 1.1 2012/04/18 20:57:33 ryoon Exp 
  	|| defined(NETBSD) || defined(OPENBSD) || defined(BSDI) \
  	|| defined(NTO) || defined(DARWIN) \
  	|| defined(UNIXWARE) || defined(RISCOS)	|| defined(SYMBIAN)
-@@ -237,7 +237,7 @@
+@@ -201,7 +201,7 @@
  #endif /* defined(_PR_DCETHREADS) */
  
  #elif defined(LINUX) || defined(__GNU__) || defined(__GLIBC__) \
@@ -20,7 +20,7 @@ $NetBSD: patch-nsprpub_pr_include_md__pth.h,v 1.1 2012/04/18 20:57:33 ryoon Exp 
  #define PT_PRIO_MIN            sched_get_priority_min(SCHED_OTHER)
  #define PT_PRIO_MAX            sched_get_priority_max(SCHED_OTHER)
  #elif defined(NTO)
-@@ -296,6 +296,7 @@ extern int (*_PT_aix_yield_fcn)();
+@@ -260,6 +260,7 @@ extern int (*_PT_aix_yield_fcn)();
  #elif defined(HPUX) || defined(SOLARIS) \
  	|| defined(LINUX) || defined(__GNU__) || defined(__GLIBC__) \
  	|| defined(FREEBSD) || defined(NETBSD) || defined(OPENBSD) \
