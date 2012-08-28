173c7786af3ac9ba4c992a4011ed5e04985d65a9
ryoon <ryoon@pkgsrc.org>
2012-08-28 12:42:00 +0000

    Update to 15.0
    * Use patches from https://bugzilla.mozilla.org/show_bug.cgi?id=753046
    * Fix firefox.sh
    
    Changelog:
    NEW Preliminary native PDF support (Aurora/Beta only)
    NEW Support for SPDY networking protocol v3
    NEW WebGL enhancements, including compressed textures for better performance
    CHANGED Optimized memory usage for add-ons
    DEVELOPER JavaScript debugger integrated into developer tools
    DEVELOPER New layout view added to Inspector
    DEVELOPER The CSS word-break property has been implemented.
    DEVELOPER High precision event timer implemented
    DEVELOPER New responsive design tool allows web developers to switch between desktop and mobile views of sites
    HTML5 Native support for the Opus audio codec added
    HTML5 The <source> element now supports the media attribute
    HTML5 The <audio> and <video> elements now support the played attribute

--- devel/xulrunner/patches/patch-ipc_chromium_src_base_message__loop.cc
+++ devel/xulrunner/patches/patch-ipc_chromium_src_base_message__loop.cc
@@ -1,9 +1,9 @@
-$NetBSD: patch-ipc_chromium_src_base_message__loop.cc,v 1.2 2012/05/08 19:29:36 martin Exp $
+$NetBSD: patch-ipc_chromium_src_base_message__loop.cc,v 1.3 2012/08/28 12:42:01 ryoon Exp $
 
 # Reported upstream: https://bugzilla.mozilla.org/show_bug.cgi?id=753046
 
---- ipc/chromium/src/base/message_loop.cc.orig	2012-02-16 07:40:33.000000000 +0100
-+++ ipc/chromium/src/base/message_loop.cc	2012-02-21 21:24:41.000000000 +0100
+--- ipc/chromium/src/base/message_loop.cc.orig	2012-08-08 20:20:07.000000000 +0000
++++ ipc/chromium/src/base/message_loop.cc
 @@ -19,7 +19,7 @@
  #if defined(OS_POSIX)
  #include "base/message_pump_libevent.h"
@@ -13,7 +13,7 @@ $NetBSD: patch-ipc_chromium_src_base_message__loop.cc,v 1.2 2012/05/08 19:29:36 
  #ifdef MOZ_WIDGET_GTK2
  #include "base/message_pump_glib.h"
  #endif
-@@ -119,7 +119,7 @@
+@@ -119,7 +119,7 @@ MessageLoop::MessageLoop(Type type)
    if (type_ == TYPE_UI) {
  #if defined(OS_MACOSX)
      pump_ = base::MessagePumpMac::Create();
--- devel/xulrunner/patches/patch-js_src_config_rules.mk
+++ devel/xulrunner/patches/patch-js_src_config_rules.mk
@@ -1,24 +1,4 @@
-$NetBSD: patch-js_src_config_rules.mk,v 1.2 2012/07/18 16:10:06 ryoon Exp $
+$NetBSD: patch-js_src_config_rules.mk,v 1.3 2012/08/28 12:42:01 ryoon Exp $
 
 https://bugzilla.mozilla.org/show_bug.cgi?id=736961
 
---- js/src/config/rules.mk.orig	2012-07-04 16:03:34.000000000 +0000
-+++ js/src/config/rules.mk
-@@ -1452,7 +1452,7 @@ $(XPIDL_GEN_DIR)/%.h: %.idl $(XPIDL_DEPS
- 	$(PYTHON_PATH) \
- 	  $(PLY_INCLUDE) \
- 	  -I$(topsrcdir)/xpcom/idl-parser \
--	  $(topsrcdir)/xpcom/idl-parser/header.py --cachedir=$(DEPTH)/xpcom/idl-parser $(XPIDL_FLAGS) $(_VPATH_SRCS) -d $(MDDEPDIR)/$(@F).pp -o $@
-+	  $(topsrcdir)/xpcom/idl-parser/header.py --cachedir=$(DEPTH)/xpcom/idl-parser/cache $(XPIDL_FLAGS) $(_VPATH_SRCS) -d $(MDDEPDIR)/$(@F).pp -o $@
- 	@if test -n "$(findstring $*.h, $(EXPORTS))"; \
- 	  then echo "*** WARNING: file $*.h generated from $*.idl overrides $(srcdir)/$*.h"; else true; fi
- 
-@@ -1465,7 +1465,7 @@ $(XPIDL_GEN_DIR)/%.xpt: %.idl $(XPIDL_DE
- 	  $(PLY_INCLUDE) \
- 	  -I$(topsrcdir)/xpcom/idl-parser \
- 	  -I$(topsrcdir)/xpcom/typelib/xpt/tools \
--	  $(topsrcdir)/xpcom/idl-parser/typelib.py --cachedir=$(DEPTH)/xpcom/idl-parser $(XPIDL_FLAGS) $(_VPATH_SRCS) -d $(MDDEPDIR)/$(@F).pp -o $@
-+	  $(topsrcdir)/xpcom/idl-parser/typelib.py --cachedir=$(DEPTH)/xpcom/idl-parser/cache $(XPIDL_FLAGS) $(_VPATH_SRCS) -d $(MDDEPDIR)/$(@F).pp -o $@
- 
- # no need to link together if XPIDLSRCS contains only XPIDL_MODULE
- ifneq ($(XPIDL_MODULE).idl,$(strip $(XPIDLSRCS)))
