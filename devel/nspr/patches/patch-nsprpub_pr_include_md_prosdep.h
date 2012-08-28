ecb062abdf31bf31d76025c088cdd51da860509c
ryoon <ryoon>
2012-08-28 13:29:15 +0000

    Bump PKGREVISION
    * Utilize pthread_set_name_np or pthread_setname_np on DragonFly or NetBSD.
      Fix build of devel/xulrunner.
    * Regen patches

--- devel/nspr/patches/patch-nsprpub_pr_include_md_prosdep.h
+++ devel/nspr/patches/patch-nsprpub_pr_include_md_prosdep.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-nsprpub_pr_include_md_prosdep.h,v 1.1 2012/04/18 20:57:33 ryoon Exp $
+$NetBSD: patch-nsprpub_pr_include_md_prosdep.h,v 1.2 2012/08/28 13:29:17 ryoon Exp $
 
---- nsprpub/pr/include/md/prosdep.h.orig	2011-09-02 20:15:44.000000000 +0000
+--- nsprpub/pr/include/md/prosdep.h.orig	2012-03-06 13:13:55.000000000 +0000
 +++ nsprpub/pr/include/md/prosdep.h
-@@ -66,7 +66,7 @@ PR_BEGIN_EXTERN_C
+@@ -34,7 +34,7 @@ PR_BEGIN_EXTERN_C
  #if defined(AIX)
  #include "md/_aix.h"
  
