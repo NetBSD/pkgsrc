6a2c0f7051d106597480d4b8a6cd6388f3e308b2
jakllsch <jakllsch@pkgsrc.org>
2012-08-30 17:37:54 +0000

    Move net/openafs to filesystems/openafs-devel. Re-add net/openafs as of 2012-05-01 as filesystems/openafs.

---
+++ filesystems/openafs/patches/patch-src_config_param.nbsd30.h
@@ -0,0 +1,12 @@
+$NetBSD: patch-src_config_param.nbsd30.h,v 1.1 2012/08/30 17:37:55 jakllsch Exp $
+
+--- src/config/param.nbsd30.h.orig	2010-12-17 15:58:42.000000000 +0000
++++ src/config/param.nbsd30.h
+@@ -23,7 +23,6 @@
+ #define AFS_NBSD20_ENV 1
+ #define AFS_NBSD30_ENV 1
+ #define AFS_NONFSTRANS 1
+-#define AFS_KERBEROS_ENV 1
+ 
+ #define AFS_VFSINCL_ENV 1
+ 
