3c4825fe9600e43b25b51b896f48f5450e0d03cb
prlw1 <prlw1@pkgsrc.org>
2012-08-31 19:43:49 +0000

    Add gtksourceview 3.4.2 - a source code formatter used by gedit

---
+++ x11/gtksourceview3/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/08/31 19:43:49 prlw1 Exp $
+
+BUILDLINK_TREE+=	gtksourceview3
+
+.if !defined(GTKSOURCEVIEW3_BUILDLINK3_MK)
+GTKSOURCEVIEW3_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gtksourceview3+=	gtksourceview3>=3.4.2
+BUILDLINK_PKGSRCDIR.gtksourceview3?=	../../x11/gtksourceview3
+
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.endif	# GTKSOURCEVIEW3_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gtksourceview3
