ce3bd7be5728b24d04fac58ba3e13eaf674d67f7
ryoon <ryoon>
2012-09-02 06:43:39 +0000

    Update to 15.0
    * Update Mozilla Lightning to 1.7
    * Update Enigmail to 1.4.4 (functionality is not tested yet; should
      be updated)
    * Regen patches
    
    Changelog:
        NEW Multi-Channel Chat: You now can enjoy real time conversation with your contacts, right from your favorite messaging application.
        NEW Do Not Track: This option has been implemented as an addition to Search the Web.
        NEW Ubuntu One is now supported in Filelink - the option to upload large attachments to online storage services.
        NEW New User Interface: Thunderbird is replicating the new look and feel of Mozilla Firefox in an effort to provide a similar user experience across all Mozilla software desktop or mobile and all platforms.
        FIXED Various fixes and performance improvements
        FIXED Various security fixes
    MFSA 2012-72 Web console eval capable of executing chrome-privileged code
    MFSA 2012-70 Location object security checks bypassed by chrome code
    MFSA 2012-68 DOMParser loads linked resources in extensions when parsing text/html
    MFSA 2012-67 Installer will launch incorrect executable following new installation
    MFSA 2012-65 Out-of-bounds read in format-number in XSLT
    MFSA 2012-64 Graphite 2 memory corruption
    MFSA 2012-63 SVG buffer overflow and use-after-free issues
    MFSA 2012-62 WebGL use-after-free and memory corruption
    MFSA 2012-61 Memory corruption with bitmap format images with negative height
    MFSA 2012-59 Location object can be shadowed using Object.defineProperty
    MFSA 2012-58 Use-after-free issues found using Address Sanitizer
    MFSA 2012-57 Miscellaneous memory safety hazards (rv:15.0/ rv:10.0.7)

--- mail/thunderbird/patches/patch-ipc_chromium_src_base_file__util__posix.cc
+++ mail/thunderbird/patches/patch-ipc_chromium_src_base_file__util__posix.cc
@@ -1,13 +1,13 @@
-$NetBSD: patch-ipc_chromium_src_base_file__util__posix.cc,v 1.3 2012/04/28 16:56:58 ryoon Exp $
+$NetBSD: patch-ipc_chromium_src_base_file__util__posix.cc,v 1.4 2012/09/02 06:43:40 ryoon Exp $
 
---- mozilla/ipc/chromium/src/base/file_util_posix.cc.orig	2012-04-20 22:40:05.000000000 +0000
+--- mozilla/ipc/chromium/src/base/file_util_posix.cc.orig	2012-08-25 00:31:11.000000000 +0000
 +++ mozilla/ipc/chromium/src/base/file_util_posix.cc
 @@ -31,7 +31,7 @@
  #include "base/time.h"
  
  // FreeBSD/OpenBSD lacks stat64, but its stat handles files >2GB just fine
 -#if defined(OS_FREEBSD) || defined(OS_OPENBSD)
-+#ifndef __linux__
++#ifndef OS_LINUX
  #define stat64 stat
  #endif
  
--- mail/thunderbird/patches/patch-mozilla_ipc_chromium_src_base_base__paths.h
+++ mail/thunderbird/patches/patch-mozilla_ipc_chromium_src_base_base__paths.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-mozilla_ipc_chromium_src_base_base__paths.h,v 1.2 2012/04/28 16:56:58 ryoon Exp $
+$NetBSD: patch-mozilla_ipc_chromium_src_base_base__paths.h,v 1.3 2012/09/02 06:43:41 ryoon Exp $
 
---- mozilla/ipc/chromium/src/base/base_paths.h.orig	2012-04-20 22:40:04.000000000 +0000
+--- mozilla/ipc/chromium/src/base/base_paths.h.orig	2012-08-25 00:31:11.000000000 +0000
 +++ mozilla/ipc/chromium/src/base/base_paths.h
 @@ -13,7 +13,7 @@
  #include "base/base_paths_win.h"
