aefbbffc948b14457ec81af10a951b7da89596ff
marino <marino>
2012-09-03 10:02:19 +0000

    audio/taglib: define BUILDLINK_INCDIRS in buildlink3.mk
    The new net/ktorrent package needs access to taglib headers.

--- audio/taglib/buildlink3.mk
+++ audio/taglib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:23:59 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2012/09/03 10:02:19 marino Exp $
 
 BUILDLINK_TREE+=	taglib
 
@@ -8,6 +8,7 @@ TAGLIB_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.taglib+=	taglib>=1.0
 BUILDLINK_ABI_DEPENDS.taglib+=	taglib>=1.3
 BUILDLINK_PKGSRCDIR.taglib?=	../../audio/taglib
+BUILDLINK_INCDIRS.taglib+=	include/taglib
 .endif # TAGLIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-taglib
