eb596ce647c17a956e18f411188358066a766d83
marino <marino>
2012-09-03 10:06:40 +0000

    Add new package net/libktorrent
    KTorrent has sbeen split into the application ktorrent and the library
    libktorrent.  Libktorrent contains all the torrent downloading code and
    ktorrent contains all application code and plugins.  The goal is to make
    libktorrent an independent library which can be used by other
    applications.

---
+++ net/libktorrent/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/09/03 10:06:40 marino Exp $
+
+BUILDLINK_TREE+=	libktorrent
+
+.if !defined(LIBKTORRENT_BUILDLINK3_MK)
+LIBKTORRENT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libktorrent+=	libktorrent>=1.2.1.2
+BUILDLINK_ABI_DEPENDS.libktorrent+=	libktorrent>=1.2.1.2
+BUILDLINK_PKGSRCDIR.libktorrent?=	../../net/libktorrent
+
+.include "../../devel/boost-libs/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/gmp/buildlink3.mk"
+.include "../../security/qca2/buildlink3.mk"
+.include "../../x11/kdelibs4/buildlink3.mk"
+.endif # LIBTORRENT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libktorrent
