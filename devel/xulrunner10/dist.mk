300e9e0d948ac9c5586b9532d86f030242c0a527
ryoon <ryoon@pkgsrc.org>
2012-09-06 17:24:59 +0000

    devel/xulrunner10 and www/firefox10: Update to 10.0.7
    Changelog:
    FIXED Security fixes can be found here
    FIXED Stability fixes can be found here
    FIXED Contenteditable breaks middle-click to open links (674770)
    FIXED Allow specifying wildcard that matches all simple netbiosnames in network.automatic-ntlm-auth.trusted-uris (452781)
    
    Fixed in Firefox ESR 10.0.7
    MFSA 2012-72 Web console eval capable of executing chrome-privileged code
    MFSA 2012-70 Location object security checks bypassed by chrome code
    MFSA 2012-69 Incorrect site SSL certificate data display
    MFSA 2012-67 Installer will launch incorrect executable following new installation
    MFSA 2012-65 Out-of-bounds read in format-number in XSLT
    MFSA 2012-63 SVG buffer overflow and use-after-free issues
    MFSA 2012-62 WebGL use-after-free and memory corruption
    MFSA 2012-61 Memory corruption with bitmap format images with negative height
    MFSA 2012-58 Use-after-free issues found using Address Sanitizer
    MFSA 2012-57 Miscellaneous memory safety hazards (rv:15.0/ rv:10.0.7)

--- devel/xulrunner10/dist.mk
+++ devel/xulrunner10/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.6 2012/07/22 00:03:10 ryoon Exp $
+# $NetBSD: dist.mk,v 1.7 2012/09/06 17:24:59 ryoon Exp $
 #
 # used by devel/xulrunner10/Makefile
 # used by www/firefox10/Makefile
@@ -6,7 +6,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}esr.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	10.0
-MOZ_BRANCH_MINOR=	.6
+MOZ_BRANCH_MINOR=	.7
 MASTER_SITES=	${MASTER_SITE_MOZILLA_ESR:=firefox/releases/${FIREFOX_VER}esr/source/}
 EXTRACT_SUFX=	.tar.bz2
 
