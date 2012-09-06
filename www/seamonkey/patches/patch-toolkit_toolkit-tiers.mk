8a2f75ce17c194d872dc0c28c8e3cbdfa8136e95
ryoon <ryoon>
2012-09-06 12:08:50 +0000

    Update to 2.12
    * Update Mozilla Lightning to 1.7
    * Update Enigmail to 1.4.4 (functionality is not tested yet; should
      be updated)
    * Regen patches
    
    Changelog:
    SeaMonkey-specific changes
        None.
    
    Mozilla platform changes
        Added support for SPDY networking protocol v3.
        Implemented WebGL enhancements, including compressed textures for better performance.
        Optimized memory usage for add-ons.
        Implemented the CSS word-break property.
        Implemented high precision event timer.
        HTML5: Added native support for the Opus audio codec.
        HTML5: Added support for the source element media attribute.
        HTML5: Added support for the audio element and video element played attribute.
        Fixed several stability issues.
    
    Fixed in SeaMonkey 2.12
    MFSA 2012-70 Location object security checks bypassed by chrome code
    MFSA 2012-69 Incorrect site SSL certificate data display
    MFSA 2012-68 DOMParser loads linked resources in extensions when parsing text/html
    MFSA 2012-65 Out-of-bounds read in format-number in XSLT
    MFSA 2012-64 Graphite 2 memory corruption
    MFSA 2012-63 SVG buffer overflow and use-after-free issues
    MFSA 2012-62 WebGL use-after-free and memory corruption
    MFSA 2012-61 Memory corruption with bitmap format images with negative height
    MFSA 2012-59 Location object can be shadowed using Object.defineProperty
    MFSA 2012-58 Use-after-free issues found using Address Sanitizer
    MFSA 2012-57 Miscellaneous memory safety hazards (rv:15.0/ rv:10.0.7)

--- www/seamonkey/patches/patch-toolkit_toolkit-tiers.mk
+++ www/seamonkey/patches/patch-toolkit_toolkit-tiers.mk
@@ -1,8 +1,8 @@
-$NetBSD: patch-toolkit_toolkit-tiers.mk,v 1.4 2012/04/28 22:48:07 ryoon Exp $
+$NetBSD: patch-toolkit_toolkit-tiers.mk,v 1.5 2012/09/06 12:08:52 ryoon Exp $
 
---- mozilla/toolkit/toolkit-tiers.mk.orig	2012-04-23 06:28:28.000000000 +0000
+--- mozilla/toolkit/toolkit-tiers.mk.orig	2012-08-27 04:49:32.000000000 +0000
 +++ mozilla/toolkit/toolkit-tiers.mk
-@@ -80,7 +80,7 @@ ifdef MOZ_UPDATER
+@@ -47,7 +47,7 @@ ifdef MOZ_UPDATER
  ifndef MOZ_NATIVE_BZ2
  tier_platform_dirs += modules/libbz2
  endif
