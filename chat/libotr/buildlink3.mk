275e926b6766d14c189db54b82d1949707c0b723
gdt <gdt@pkgsrc.org>
2012-09-06 00:23:18 +0000

    Update to 4.0.0.
    Note that while the protocol is compatible, the API is not, and hence
    there will be a pidgin-otr update within minutes.
    
    There is an apparent gcc 4.1.3 -O2/SSP bug, which is avoided by
    disabling SSP in libotr (which libotr finds and turns on).  This is
    temporary pending more fine-grained control and/or a fix.
    
    Update to libotr 4.0.0.  Note that libotr 4.x is API-incompatible with
    libotr 3.x; upstream thinks this is ok, so pkgsrc won't try to work
    around it.
    
    24 Aug 2012:
    - Release 4.0.0
    - Support v3 of the OTR protocol
    - The main new feature: sensibly handle the case where a user is logged
      in multiple times to the same IM account
    - API changes:
      - instance tags, to support multiple simultaneous logins
      - support for asynchronous private key generation
      - the ability to provide an "extra" symmetric key to applications
        (with forward secrecy)
      - applications can supply a formation conversion callback if they do
        not natively use XHTML-style UTF8 markup
      - error messages formerly provided by libotr are now handled using
        callbacks to the application, for better i18n support
      - otrl_message_sending now handles message fragmentation internally

--- chat/libotr/buildlink3.mk
+++ chat/libotr/buildlink3.mk
@@ -1,12 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.13 2012/05/07 01:53:17 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2012/09/06 00:23:18 gdt Exp $
 
 BUILDLINK_TREE+=	libotr
 
 .if !defined(LIBOTR_BUILDLINK3_MK)
 LIBOTR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libotr+=	libotr>=3.0.0
-BUILDLINK_ABI_DEPENDS.libotr+=	libotr>=3.2.0nb1
+# libotr 4 has backwards-incompatible API and ABI changes.
+BUILDLINK_API_DEPENDS.libotr+=	libotr>=4.0.0
+BUILDLINK_ABI_DEPENDS.libotr+=	libotr>=4.0.0
 BUILDLINK_PKGSRCDIR.libotr?=	../../chat/libotr
 
 .include "../../security/libgcrypt/buildlink3.mk"
