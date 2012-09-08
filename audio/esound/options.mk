6c9474bd5e7dc338e0e53318d9e4d1ee89533e75
cheusov <cheusov@pkgsrc.org>
2012-09-08 18:58:43 +0000

    Add option "arts" disabled by default

--- audio/esound/options.mk
+++ audio/esound/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.2 2012/06/12 15:45:54 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2012/09/08 18:58:43 cheusov Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.esound
-PKG_SUPPORTED_OPTIONS=	inet6
+PKG_SUPPORTED_OPTIONS=	inet6 arts
 PKG_SUGGESTED_OPTIONS=	inet6
 
 .include "../../mk/bsd.options.mk"
@@ -10,4 +10,10 @@ PKG_SUGGESTED_OPTIONS=	inet6
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
+.endif
+
+.if !empty(PKG_OPTIONS:Marts)
+.include "../../audio/arts/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-arts --disable-artstest
 .endif
