d3be5dd777ac8d8c5627a770bdfab13d540500df
obache <obache@pkgsrc.org>
2012-09-10 09:22:41 +0000

    Fixes PKGPREFIX for python and ruby by blbump(1).

--- x11/py-gnome2-extras/buildlink3.mk
+++ x11/py-gnome2-extras/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.29 2012/09/07 19:17:50 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2012/09/10 09:22:42 obache Exp $
 
 BUILDLINK_TREE+=	pygnome2ex
 
@@ -8,7 +8,7 @@ PYGNOME2EX_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.pygnome2ex+=	${PYPKGPREFIX}-gnome2-extras>=2.12.0nb3
-BUILDLINK_ABI_DEPENDS.pygnome2ex+=	py27-gnome2-extras>=2.25.3nb11
+BUILDLINK_ABI_DEPENDS.pygnome2ex+=	${PYPKGPREFIX}-gnome2-extras>=2.25.3nb11
 BUILDLINK_PKGSRCDIR.pygnome2ex?=	../../x11/py-gnome2-extras
 
 .include "../../x11/py-gnome2/buildlink3.mk"
--- x11/py-gnome2/buildlink3.mk
+++ x11/py-gnome2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.38 2012/09/07 19:17:50 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.39 2012/09/10 09:22:42 obache Exp $
 
 BUILDLINK_TREE+=	pygnome2
 
@@ -8,7 +8,7 @@ PYGNOME2_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.pygnome2+=	${PYPKGPREFIX}-gnome2>=2.10.0
-BUILDLINK_ABI_DEPENDS.pygnome2+=	py27-gnome2>=2.28.1nb13
+BUILDLINK_ABI_DEPENDS.pygnome2+=	${PYPKGPREFIX}-gnome2>=2.28.1nb13
 BUILDLINK_PKGSRCDIR.pygnome2?=	../../x11/py-gnome2
 
 .include "../../net/py-ORBit/buildlink3.mk"
