a29dad22000150c5787f4bd338aebec2ad1b654f
obache <obache@pkgsrc.org>
2012-09-10 09:22:41 +0000

    Fixes PKGPREFIX for python and ruby by blbump(1).

--- x11/py-vte/buildlink3.mk
+++ x11/py-vte/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2012/09/07 19:17:51 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2012/09/10 09:22:42 obache Exp $
 
 BUILDLINK_TREE+=	py-vte
 
@@ -8,7 +8,7 @@ PY_VTE_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.py-vte+=	${PYPKGPREFIX}-vte>=0.26.0
-BUILDLINK_ABI_DEPENDS.py-vte+=	py27-vte>=0.28.1nb6
+BUILDLINK_ABI_DEPENDS.py-vte+=	${PYPKGPREFIX}-vte>=0.28.1nb6
 BUILDLINK_PKGSRCDIR.py-vte?=	../../x11/vte
 
 .include "../../x11/vte/buildlink3.mk"
