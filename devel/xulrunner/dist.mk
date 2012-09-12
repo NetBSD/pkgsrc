b72c295f0655b7e3e968120b985c72409986be10
ryoon <ryoon@pkgsrc.org>
2012-09-12 14:27:14 +0000

    Update to 15.0.1
    Changelog:
        FIXED Sites visited while in Private Browsing mode could be found through manual browser cache inspection (787743)
        NEW Silent, background updates
        NEW Support for SPDY networking protocol v3
        NEW WebGL enhancements, including compressed textures for better performance
        NEW Localization in Maithili (see all available locales)
        CHANGED Optimized memory usage for add-ons
        DEVELOPER JavaScript debugger integrated into developer tools
        DEVELOPER New layout view added to Inspector
        DEVELOPER High precision event timer implemented
        DEVELOPER The CSS word-break property has been implemented.
        DEVELOPER New responsive design tool allows web developers to switch between desktop and mobile views of sites
        HTML5 Native support for the Opus audio codec added
        HTML5 The <audio> and <video> elements now support the played attribute
        HTML5 The <source> element now supports the media attribute
        FIXED Focus rings keep growing when repeatedly tabbing through elements (720987)

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.33 2012/08/28 12:42:01 ryoon Exp $
+# $NetBSD: dist.mk,v 1.34 2012/09/12 14:27:14 ryoon Exp $
 #
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
@@ -6,7 +6,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	15.0
-MOZ_BRANCH_MINOR=	
+MOZ_BRANCH_MINOR=	.1
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
