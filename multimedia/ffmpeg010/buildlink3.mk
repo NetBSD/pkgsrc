3567f0c99787216b7d8cdd9e2db6a2acb9fec3c2
drochner <drochner@pkgsrc.org>
2012-09-14 14:52:34 +0000

    prepare a pkg for ffmpeg-0.10.4 The idea is to iron out build problems and replace the old ffmpeg pkg after the freeze.

---
+++ multimedia/ffmpeg010/buildlink3.mk
@@ -0,0 +1,42 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/09/14 14:52:34 drochner Exp $
+
+BUILDLINK_TREE+=	ffmpeg
+
+.if !defined(FFMPEG_BUILDLINK3_MK)
+FFMPEG_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ffmpeg+=	ffmpeg>=0.6.1
+BUILDLINK_ABI_DEPENDS.ffmpeg+=	ffmpeg>=20111104.0.7.7nb1
+BUILDLINK_PKGSRCDIR.ffmpeg?=	../../multimedia/ffmpeg
+
+pkgbase := ffmpeg
+.  include "../../mk/pkg-build-options.mk"
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg:Mtheora)
+.  include "../../multimedia/libtheora/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg:Mxvid)
+.  include "../../multimedia/xvidcore/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg:Mx264)
+.  include "../../multimedia/x264-devel/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg:Mfaac)
+.  include "../../audio/faac/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg:Mlibvpx)
+.  include "../../multimedia/libvpx/buildlink3.mk"
+.endif
+
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../devel/libgetopt/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.endif # FFMPEG_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ffmpeg
