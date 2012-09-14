1056a951f3515d6bd9886293741cac114857698c
drochner <drochner@pkgsrc.org>
2012-09-14 12:15:01 +0000

    update to 0.20.4 changes: -Improvements regarding embedded file handling -bugfixes -cleanup
    pkgsrc note: shlib major changes, revbumps needed. also switch to
     lcms2 for the "cms" option

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.28 2012/08/13 11:19:39 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.29 2012/09/14 12:15:01 drochner Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,13 +6,13 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.20.3
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.20.4
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 pkgbase := poppler
 .include "../../mk/pkg-build-options.mk"
 .if !empty(PKG_BUILD_OPTIONS.poppler:Mpoppler-cms)
-.include "../../graphics/lcms/buildlink3.mk"
+.include "../../graphics/lcms2/buildlink3.mk"
 .endif
 
 .include "../../fonts/fontconfig/buildlink3.mk"
