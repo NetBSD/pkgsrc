4274fa03c8929e1298244fbc290ff2187bfc1ff6
taca <taca@pkgsrc.org>
2012-09-14 15:54:22 +0000

    Update PHP53_VERSION and PHP54_VERSION.

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.21 2012/08/17 15:37:30 taca Exp $
+# $NetBSD: phpversion.mk,v 1.22 2012/09/14 15:54:22 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -73,8 +73,8 @@ _SYS_VARS.php=	PKG_PHP_VERSION PKG_PHP PHPPKGSRCDIR PHP_PKG_PREFIX \
 
 .include "../../mk/bsd.prefs.mk"
 
-PHP53_VERSION=			5.3.16
-PHP54_VERSION=			5.4.6
+PHP53_VERSION=			5.3.17
+PHP54_VERSION=			5.4.7
 
 PHP_VERSION_DEFAULT?=		53
 PHP_VERSIONS_ACCEPTED?=		53 54
