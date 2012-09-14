aa2d48278868a738a4af2bc9489dce5636e73381
drochner <drochner@pkgsrc.org>
2012-09-14 12:16:11 +0000

    bump dependencies for shlib major change

--- print/poppler-cpp/buildlink3.mk
+++ print/poppler-cpp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2012/08/13 11:20:54 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2012/09/14 12:16:11 drochner Exp $
 
 BUILDLINK_TREE+=	poppler-cpp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-cpp
 POPPLER_CPP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-cpp+=	poppler-cpp>=0.16.7
-BUILDLINK_ABI_DEPENDS.poppler-cpp+=	poppler-cpp>=0.20.3
+BUILDLINK_ABI_DEPENDS.poppler-cpp+=	poppler-cpp>=0.20.4
 BUILDLINK_PKGSRCDIR.poppler-cpp?=	../../print/poppler-cpp
 
 .include "../../print/poppler/buildlink3.mk"
