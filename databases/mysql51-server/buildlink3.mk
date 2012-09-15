7a7faf9e36f4b7ebd11d10aeb709f91b37522fdd
obache <obache>
2012-09-15 15:03:21 +0000

    Recursive bump from mysql51-client library changed to use openssl.

--- databases/mysql51-server/buildlink3.mk
+++ databases/mysql51-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/09/12 10:53:58 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/09/15 15:03:22 obache Exp $
 
 BUILDLINK_TREE+=	mysql-server
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	mysql-server
 MYSQL_SERVER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mysql-server+=	mysql-server>=5.1.44<5.5
+BUILDLINK_ABI_DEPENDS.mysql-server?=	mysql-server>=5.1.65<5.5
 BUILDLINK_PKGSRCDIR.mysql-server?=	../../databases/mysql51-server
 BUILDLINK_LIBDIRS.mysql-server?=	lib/mysql
 
