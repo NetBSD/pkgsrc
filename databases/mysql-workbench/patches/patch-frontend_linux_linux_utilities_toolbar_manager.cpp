70cd0e351f596033cf42f4a8480fa6890c823059
obache <obache>
2012-09-15 05:14:07 +0000

    revert accidental incomplete update with revbump after updating graphics/cairo. then, just bump PKGREVISION to 11.

--- databases/mysql-workbench/patches/patch-frontend_linux_linux_utilities_toolbar_manager.cpp
+++ databases/mysql-workbench/patches/patch-frontend_linux_linux_utilities_toolbar_manager.cpp
@@ -1,15 +1,15 @@
-$NetBSD: patch-frontend_linux_linux_utilities_toolbar_manager.cpp,v 1.2 2012/09/07 19:16:20 adam Exp $
+$NetBSD: patch-frontend_linux_linux_utilities_toolbar_manager.cpp,v 1.3 2012/09/15 05:14:07 obache Exp $
 
 Fix building with newer Gtkmm.
 
---- frontend/linux/linux_utilities/toolbar_manager.cpp.orig	2012-08-09 20:57:01.000000000 +0000
+--- frontend/linux/linux_utilities/toolbar_manager.cpp.orig	2011-05-16 11:11:25.000000000 +0000
 +++ frontend/linux/linux_utilities/toolbar_manager.cpp
-@@ -30,7 +30,7 @@ static Gtk::ComboBox *create_color_combo
+@@ -31,7 +31,7 @@ static Gtk::ComboBox *create_color_combo
    }
  
    Glib::RefPtr<Gtk::ListStore> model= Gtk::ListStore::create(*color_combo_columns);
--  Gtk::ComboBox *combo= new Gtk::ComboBox(Glib::RefPtr<Gtk::TreeModel>(model));
-+  Gtk::ComboBox *combo= new Gtk::ComboBox(Glib::RefPtr<Gtk::TreeModel>(model), false);
+-  Gtk::ComboBox *combo= new Gtk::ComboBox(model);
++  Gtk::ComboBox *combo= new Gtk::ComboBox(model, false);
  
    combo->pack_start(color_combo_columns->image);
  
