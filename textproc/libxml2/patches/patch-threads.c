fbb9869c88da4d044a2718eee66276bbcb6e7193
adam <adam@pkgsrc.org>
2012-09-15 10:23:38 +0000

    Changes 2.9.0: Features: * A few new API entry points, * More resilient push parser mode, * A lot of portability improvement, * Faster XPath evaluation

--- textproc/libxml2/patches/patch-threads.c
+++ textproc/libxml2/patches/patch-threads.c
@@ -1,7 +1,8 @@
-$NetBSD: patch-threads.c,v 1.2 2012/04/27 15:05:02 mishka Exp $
+$NetBSD: patch-threads.c,v 1.3 2012/09/15 10:23:39 adam Exp $
 
-* Treat OpenBSD and MirBSD same as Linux to avoid linking with libpthread.
-* NetBSD<4.99.36 and FreeBSD<7.0 lack pthread_equal() stub function in libc.
+* Treat OpenBSD and MirBSD same as Linux to avoid linking with libpthread
+* NetBSD<4.99.36 and FreeBSD<7.0 lack pthread_equal() stub function in libc
+* pthread_once_t cast needed to avoid compilation error
 
 --- threads.c.orig	2010-10-15 17:28:30.000000000 +0000
 +++ threads.c
@@ -35,3 +36,12 @@ $NetBSD: patch-threads.c,v 1.2 2012/04/27 15:05:02 mishka Exp $
  #endif /* HAVE_PTHREAD_H */
  
  /*
+@@ -915,7 +923,7 @@ xmlCleanupThreads(void)
+ #ifdef HAVE_PTHREAD_H
+     if ((libxml_is_threaded)  && (pthread_key_delete != NULL))
+         pthread_key_delete(globalkey);
+-    once_control = PTHREAD_ONCE_INIT;
++    once_control = (pthread_once_t)PTHREAD_ONCE_INIT;
+ #elif defined(HAVE_WIN32_THREADS) && !defined(HAVE_COMPILER_TLS) && (!defined(LIBXML_STATIC) || defined(LIBXML_STATIC_FOR_DLL))
+     if (globalkey != TLS_OUT_OF_INDEXES) {
+         xmlGlobalStateCleanupHelperParams *p;
