3e008219a9973f9002be65b43214562fe4ad6edb
obache <obache>
2012-09-15 13:00:03 +0000

    It is impossible to buildlink mysql-server-5.0 together with mysql-client-5.1.

--- databases/mysql5-server/buildlink3.mk
+++ databases/mysql5-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2011/09/12 10:56:10 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/09/15 13:00:03 obache Exp $
 
 BUILDLINK_TREE+=	mysql-server
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.mysql-server+=	mysql-server>=5.0<5.1
 BUILDLINK_PKGSRCDIR.mysql-server?=	../../databases/mysql5-server
 BUILDLINK_LIBDIRS.mysql-server?=	lib/mysql
 
-.include "../../databases/mysql51-client/buildlink3.mk"
+.include "../../databases/mysql5-client/buildlink3.mk"
 .endif	# MYSQL_SERVER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mysql-server
