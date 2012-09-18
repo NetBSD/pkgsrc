3cde8ab093351ce4f69c93053991c691b9087560
sno <sno@pkgsrc.org>
2012-09-18 08:54:42 +0000

    Fix build of dependent code to find the include files ...

--- x11/p5-Wx/buildlink3.mk
+++ x11/p5-Wx/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2012/09/15 10:07:11 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2012/09/18 08:54:42 sno Exp $
 
 BUILDLINK_TREE+=	p5-Wx
 
@@ -8,10 +8,11 @@ P5_WX_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.p5-Wx+=	p5-Wx>=0.91
 BUILDLINK_ABI_DEPENDS.p5-Wx+=	p5-Wx>=0.9911nb1
 BUILDLINK_PKGSRCDIR.p5-Wx?=	../../x11/p5-Wx
-BUILDLINK_INCDIRS.p5-Wx?=	${PERL5_SUB_INSTALLVENDORARCH}/auto/Wx
+BUILDLINK_INCDIRS.p5-Wx?=	${PERL5_SUB_INSTALLVENDORARCH}/Wx/cpp
+BUILDLINK_LIBDIRS.p5-Wx?=	${PERL5_SUB_INSTALLVENDORARCH}/auto/Wx
 
-# We want all of the arch-dependent DBI files.
-BUILDLINK_CONTENTS_FILTER.p5-Wx?=	${GREP} '/auto/Wx/'
+# We want all of the arch-dependent Wx files.
+BUILDLINK_CONTENTS_FILTER.p5-Wx?=	${GREP} ${PERL5_SUB_INSTALLVENDORARCH}
 
 BUILD_DEPENDS+= p5-Alien-wxWidgets>=0.25:../../x11/p5-Alien-wxWidgets
 BUILD_DEPENDS+= p5-ExtUtils-XSpp>=0.05:../../devel/p5-ExtUtils-XSpp
