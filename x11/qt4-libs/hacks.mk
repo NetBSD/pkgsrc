e970e83ed5598ca6a3547c990725f78522df6797
apb <apb@pkgsrc.org>
2012-09-20 08:38:40 +0000

    Workaround for PR 46978: g++-4.5.3 and 4.5.4 on NetBSD/i386 fails with an internal compiler error when building gui/painting/qdrawhelper_mmx.cpp.

--- x11/qt4-libs/hacks.mk
+++ x11/qt4-libs/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2011/08/03 16:38:31 tron Exp $
+# $NetBSD: hacks.mk,v 1.2 2012/09/20 08:38:40 apb Exp $
 
 ### [Wed Aug  3 12:18:16 UTC 2011 : tron]
 ### If "qmake" is build with "-O2" or better with "g++" under Mac OS X the
@@ -8,5 +8,21 @@
 .  if !empty(CC_VERSION:Mgcc*)
 PKG_HACKS+=		macosx-codegen
 BUILDLINK_TRANSFORM+=	rename:-O[0-9]*:-O1
+.  endif
+.endif
+
+### [Wed Sep 19 09:24:11 UTC 2012 : apb]
+### g++-4.5.3 and 4.5.4 on NetBSD/i386 fails with an internal compiler error
+### when building gui/painting/qdrawhelper_mmx.cpp.
+### Building with -O1 instead of -O2 seems to work around the problem.
+.if ${OPSYS} == "NetBSD" && ${MACHINE_ARCH} == "i386"
+.  include "../../mk/compiler.mk"
+.  if !empty(CC_VERSION:Mgcc-4.5.*)
+PKG_HACKS+=		pr46978
+SUBST_CLASSES+=		pr46978
+SUBST_STAGE.pr46978=	post-configure
+SUBST_MESSAGE.pr46978=	Working around optimizer bug (NetBSD PR 46978)
+SUBST_FILES.pr46978=	src/gui/Makefile
+SUBST_SED.pr46978=	-e '/^CXXFLAGS/s/-O2/-O1/'
 .  endif
 .endif
