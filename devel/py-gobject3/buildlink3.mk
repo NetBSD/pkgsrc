6ca7ee563af0b453c15ed04e07c396d51bd93fd2
prlw1 <prlw1>
2012-09-20 12:00:00 +0000

    commit 4e4c87e3: Fix 64bit integer convertion from GValue
    (In the vain hope that it might fix
    https://bugzilla.gnome.org/show_bug.cgi?id=681157)

--- devel/py-gobject3/buildlink3.mk
+++ devel/py-gobject3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2012/09/15 10:04:20 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2012/09/20 12:00:00 prlw1 Exp $
 
 BUILDLINK_TREE+=	py-gobject3
 
@@ -8,7 +8,7 @@ PY_GOBJECT3_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.py-gobject3+=	${PYPKGPREFIX}-gobject3>=3.2.2
-BUILDLINK_ABI_DEPENDS.py-gobject3?=	${PYPKGPREFIX}-gobject3>=3.2.2nb2
+BUILDLINK_ABI_DEPENDS.py-gobject3?=	${PYPKGPREFIX}-gobject3>=3.2.2nb3
 BUILDLINK_PKGSRCDIR.py-gobject3?=	../../devel/py-gobject3
 
 .include "../../devel/glib2/buildlink3.mk"
