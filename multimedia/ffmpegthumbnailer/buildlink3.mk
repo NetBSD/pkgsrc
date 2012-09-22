296f76c77ea9ae2ce9ef51cfb4f379e41a3118de
ryoon <ryoon@pkgsrc.org>
2012-09-22 13:48:15 +0000

    Recursive revbump from multimedia/libvpx 1.1.0 update

--- multimedia/ffmpegthumbnailer/buildlink3.mk
+++ multimedia/ffmpegthumbnailer/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/02/19 20:07:42 gls Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/09/22 13:48:17 ryoon Exp $
 
 BUILDLINK_TREE+=	ffmpegthumbnailer
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ffmpegthumbnailer
 FFMPEGTHUMBNAILER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ffmpegthumbnailer+=	ffmpegthumbnailer>=2.0.6nb1
+BUILDLINK_ABI_DEPENDS.ffmpegthumbnailer?=	ffmpegthumbnailer>=2.0.6nb2
 BUILDLINK_PKGSRCDIR.ffmpegthumbnailer?=	../../multimedia/ffmpegthumbnailer
 
 #.include "../../graphics/png/buildlink3.mk"
