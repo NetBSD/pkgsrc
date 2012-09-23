09e3c10616747bfcfc92b37e60c035ad8737ff82
obache <obache@pkgsrc.org>
2012-09-23 05:36:22 +0000

    recursive bump from libcdio shlib major change.

--- misc/libcdio/buildlink3.mk
+++ misc/libcdio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2009/10/31 14:46:51 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2012/09/23 05:36:23 obache Exp $
 
 BUILDLINK_TREE+=	libcdio
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libcdio
 LIBCDIO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libcdio+=	libcdio>=0.65
-BUILDLINK_ABI_DEPENDS.libcdio+=	libcdio>=0.82
+BUILDLINK_ABI_DEPENDS.libcdio+=	libcdio>=0.83
 BUILDLINK_PKGSRCDIR.libcdio?=	../../misc/libcdio
 .endif # LIBCDIO_BUILDLINK3_MK
 
