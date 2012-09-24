0587e79c3de9b38495707ad6f2a48fdf12b13513
sbd <sbd@pkgsrc.org>
2012-09-24 05:12:54 +0000

    Remove PKGREVISION line and add note "Do NOT set PKGREVISION in this file!"

--- sysutils/amanda-common/version.mk
+++ sysutils/amanda-common/version.mk
@@ -1,3 +1,3 @@
-# $NetBSD: version.mk,v 1.3 2012/09/09 22:21:54 spz Exp $
+# $NetBSD: version.mk,v 1.4 2012/09/24 05:12:54 sbd Exp $
 AMANDA_VERSION=	3.3.1
-PKGREVISION=	1
+# Do NOT set PKGREVISION in this file!
