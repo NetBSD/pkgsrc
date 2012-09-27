f9dd42cd77c48eb8630ad401e9c8951ff108d5d1
jperkin <jperkin@pkgsrc.org>
2012-09-27 13:20:42 +0000

    Ensure that MODULE_ARCH is syncronised between pkgsrc and configure script.
    Fixes package on Solaris.

--- graphics/evas/patches/patch-src_modules_loaders_psd_evas__image__load__psd.c
+++ graphics/evas/patches/patch-src_modules_loaders_psd_evas__image__load__psd.c
@@ -1,3 +1,5 @@
+$NetBSD: patch-src_modules_loaders_psd_evas__image__load__psd.c,v 1.2 2012/09/27 13:20:42 jperkin Exp $
+
 --- src/modules/loaders/psd/evas_image_load_psd.c.orig	2011-08-11 07:46:01.000000000 +0200
 +++ src/modules/loaders/psd/evas_image_load_psd.c	2011-12-27 15:15:11.097276116 +0100
 @@ -1,4 +1,6 @@
