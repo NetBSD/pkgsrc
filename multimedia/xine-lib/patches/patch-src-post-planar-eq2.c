98452622795293180ae1511fabcaca6d2a50edc9
apb <apb@pkgsrc.org>
2012-09-27 10:28:14 +0000

    In affine_1d_MMX(), move the initialisation of %mm3 and %mm4 registers into a separate asm statement, to give the compiler more freedom for register allocation.  Fixes a problem with gcc-4.5.4 on NetBSD/i386 in which gcc complained:
    eq2.c: In function 'affine_1d_MMX':
    eq2.c:128:5: error: can't find a register in class 'GENERAL_REGS' while reloading 'asm'
    eq2.c:128:5: error: 'asm' operand has impossible constraints

--- multimedia/xine-lib/patches/patch-src-post-planar-eq2.c
+++ multimedia/xine-lib/patches/patch-src-post-planar-eq2.c
@@ -1,8 +1,42 @@
-$NetBSD: patch-src-post-planar-eq2.c,v 1.1 2011/09/04 14:34:52 joerg Exp $
+$NetBSD: patch-src-post-planar-eq2.c,v 1.2 2012/09/27 10:28:14 apb Exp $
 
---- src/post/planar/eq2.c.orig	2011-08-30 13:13:49.000000000 +0000
+In affine_1d_MMX(), move the initialisation of %mm3 and %mm4 registers
+into a separate asm statement, to give the compiler more freedom
+for register allocation.  Fixes a problem with gcc-4.5.4 on NetBSD/i386
+in which gcc complained:
+
+eq2.c: In function 'affine_1d_MMX':
+eq2.c:128:5: error: can't find a register in class 'GENERAL_REGS' while reloading 'asm'
+eq2.c:128:5: error: 'asm' operand has impossible constraints
+
+--- src/post/planar/eq2.c.orig	2012-02-05 19:17:02.000000000 +0000
 +++ src/post/planar/eq2.c
-@@ -288,19 +288,26 @@ typedef struct eq2_parameters_s {
+@@ -126,8 +126,13 @@ void affine_1d_MMX (eq2_param_t *par, un
+ 
+   while (h-- > 0) {
+     asm volatile (
+-      "movq (%5), %%mm3 \n\t"
+-      "movq (%6), %%mm4 \n\t"
++      "movq (%0), %%mm3 \n\t"
++      "movq (%1), %%mm4 \n\t"
++      :
++      : "r" (brvec), "r" (contvec)
++      :
++    );
++    asm volatile (
+       "pxor %%mm0, %%mm0 \n\t"
+       "movl %4, %%eax\n\t"
+       ASMALIGN(4)
+@@ -149,7 +154,7 @@ void affine_1d_MMX (eq2_param_t *par, un
+       "decl %%eax \n\t"
+       "jnz 1b \n\t"
+       : "=r" (src), "=r" (dst)
+-      : "0" (src), "1" (dst), "r" (w >> 3), "r" (brvec), "r" (contvec)
++      : "0" (src), "1" (dst), "r" (w >> 3)
+       : "%eax"
+     );
+ 
+@@ -288,19 +293,26 @@ typedef struct eq2_parameters_s {
   * description of params struct
   */
  START_PARAM_DESCR( eq2_parameters_t )
