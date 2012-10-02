0a43ca30b3bd8518e0ad245a6b1bb06eb18f7a6f
tron <tron@pkgsrc.org>
2012-10-02 17:18:28 +0000

    Revert unintentional revision bump.

--- graphics/cairo/buildlink3.mk
+++ graphics/cairo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.45 2012/10/02 17:11:07 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.46 2012/10/02 17:18:28 tron Exp $
 
 BUILDLINK_TREE+=	cairo
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	cairo
 CAIRO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cairo+=	cairo>=1.0.0nb2
-BUILDLINK_ABI_DEPENDS.cairo+=	cairo>=1.12.2nb3
+BUILDLINK_ABI_DEPENDS.cairo+=	cairo>=1.12.2nb2
 BUILDLINK_PKGSRCDIR.cairo?=	../../graphics/cairo
 
 BUILDLINK_API_DEPENDS.Xrender+=	Xrender>=0.8
