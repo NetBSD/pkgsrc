f43e35e1eed6f53ab4d0b53ecdc5d140a0feaedf
dholland <dholland@pkgsrc.org>
2012-10-03 23:49:30 +0000

    Prevent depends overlap with gtksourceview2; fixes bulk builds of seahorse-plugins.

--- x11/gtksourceview/buildlink3.mk
+++ x11/gtksourceview/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.32 2012/10/02 17:12:15 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.33 2012/10/03 23:49:30 dholland Exp $
 
 BUILDLINK_TREE+=	gtksourceview
 
 .if !defined(GTKSOURCEVIEW_BUILDLINK3_MK)
 GTKSOURCEVIEW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gtksourceview+=	gtksourceview>=1.8.5
-BUILDLINK_ABI_DEPENDS.gtksourceview+=	gtksourceview>=1.8.5nb15
+BUILDLINK_API_DEPENDS.gtksourceview+=	gtksourceview>=1.8.5<2
+BUILDLINK_ABI_DEPENDS.gtksourceview+=	gtksourceview>=1.8.5nb15<2
 BUILDLINK_PKGSRCDIR.gtksourceview?=	../../x11/gtksourceview
 
 .include "../../devel/gettext-lib/buildlink3.mk"
