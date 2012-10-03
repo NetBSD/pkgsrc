6d46782144dd8ed0a15cdb4713ffdfd035a9db0b
fhajny <fhajny@pkgsrc.org>
2012-10-03 12:49:12 +0000

    Add BUILDLINK_LIBDIRS to mysql55-* too, to help out packages like net/powerdns-mysql.

--- databases/mysql55-server/buildlink3.mk
+++ databases/mysql55-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/04/27 08:49:01 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/10/03 12:49:12 fhajny Exp $
 
 BUILDLINK_TREE+=	mysql-server
 
@@ -7,6 +7,7 @@ MYSQL_SERVER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mysql-server+=	mysql-server>=5.5.11
 BUILDLINK_PKGSRCDIR.mysql-server?=	../../databases/mysql55-server
+BUILDLINK_LIBDIRS.mysql-server?=	lib
 
 #.include "../../databases/mysql55-client/buildlink3.mk"
 .endif	# MYSQL_SERVER_BUILDLINK3_MK
