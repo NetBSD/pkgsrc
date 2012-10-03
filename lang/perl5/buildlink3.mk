cd9c46cba9c52d133d1e687635e983cfafd851eb
wiz <wiz@pkgsrc.org>
2012-10-03 21:23:25 +0000

    Update version in a comment.

--- lang/perl5/buildlink3.mk
+++ lang/perl5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.50 2012/10/03 19:59:47 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.51 2012/10/03 21:23:25 wiz Exp $
 
 BUILDLINK_TREE+=	perl
 
@@ -10,7 +10,7 @@ PERL5_REQD+=			5.16.0
 .for _perl5_ver_ in ${PERL5_REQD}
 BUILDLINK_API_DEPENDS.perl+=	perl>=${_perl5_ver_}
 .endfor
-# Prevent p5-* etc. packages built for 5.14.0 to be used
+# Prevent p5-* etc. packages built for 5.16.0 to be used
 # with the next incompatible version of perl:
 BUILDLINK_API_DEPENDS.perl+=	perl<5.18.0
 
