78944a7e5aa17fe5f84c77d8104713ce5da4cf87
wiz <wiz@pkgsrc.org>
2012-10-03 22:03:41 +0000

    Remove python25, as promised in March. EOL'd last year.

--- lang/python25/dist.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: dist.mk,v 1.2 2011/06/04 05:08:19 obache Exp $
-
-PY_DISTVERSION=	2.5.6
-DISTNAME=	Python-${PY_DISTVERSION}
-EXTRACT_SUFX=	.tar.bz2
-DISTINFO_FILE=	${.CURDIR}/../../lang/python25/distinfo
-PATCHDIR=	${.CURDIR}/../../lang/python25/patches
-MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION}/
