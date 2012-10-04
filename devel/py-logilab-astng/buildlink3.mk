d0706c3de5130ebe2f4d6ddbd3ed13539519611f
wiz <wiz>
2012-10-04 07:04:51 +0000

    Make include guard match package name.

--- devel/py-logilab-astng/buildlink3.mk
+++ devel/py-logilab-astng/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/01/23 22:28:17 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/10/04 07:04:51 wiz Exp $
 
 BUILDLINK_TREE+=	py-llab-astng
 
-.if !defined(PY26_LOGILAB_ASTNG_BUILDLINK3_MK)
-PY26_LOGILAB_ASTNG_BUILDLINK3_MK:=
+.if !defined(PY_LLAB_ASTNG_BUILDLINK3_MK)
+PY_LLAB_ASTNG_BUILDLINK3_MK:=
 
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.py-llab-astng+= ${PYPKGPREFIX}-logilab-astng>=0.21.0
 BUILDLINK_PKGSRCDIR.py-llab-astng?=	../../devel/py-logilab-astng
 
-.endif	# PY26_LOGILAB_ASTNG_BUILDLINK3_MK
+.endif	# PY_LLAB_ASTNG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-py-llab-astng
