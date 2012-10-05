0b4ada2f7ba27f665a2ba0a5f098282c139c994e
tron <tron>
2012-10-05 09:53:49 +0000

    Pullup ticket #3930 - requested by dholland x11/gtksourceview: build fix
    Revisions pulled up:
    - x11/gtksourceview/buildlink3.mk                               1.33
    
    ---
       Module Name:	pkgsrc
       Committed By:	dholland
       Date:		Wed Oct  3 23:49:30 UTC 2012
    
       Modified Files:
       	pkgsrc/x11/gtksourceview: buildlink3.mk
    
       Log Message:
       Prevent depends overlap with gtksourceview2; fixes bulk builds of
       seahorse-plugins.

--- x11/gtksourceview/buildlink3.mk
+++ x11/gtksourceview/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.31.2.1 2012/10/03 12:09:54 spz Exp $
+# $NetBSD: buildlink3.mk,v 1.31.2.2 2012/10/05 09:53:49 tron Exp $
 
 BUILDLINK_TREE+=	gtksourceview
 
 .if !defined(GTKSOURCEVIEW_BUILDLINK3_MK)
 GTKSOURCEVIEW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gtksourceview+=	gtksourceview>=1.8.5
-BUILDLINK_ABI_DEPENDS.gtksourceview+=	gtksourceview>=1.8.5nb15
+BUILDLINK_API_DEPENDS.gtksourceview+=	gtksourceview>=1.8.5<2
+BUILDLINK_ABI_DEPENDS.gtksourceview+=	gtksourceview>=1.8.5nb15<2
 BUILDLINK_PKGSRCDIR.gtksourceview?=	../../x11/gtksourceview
 
 .include "../../devel/gettext-lib/buildlink3.mk"
