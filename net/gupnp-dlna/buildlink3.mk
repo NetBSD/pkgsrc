9062b0f57d20fac5fb99371a86d15032d352bbd6
ryoon <ryoon@pkgsrc.org>
2012-10-06 12:41:09 +0000

    Import gupnp-dlna-0.6.6 as net/gupnp-dlna.
    GUPnP is an object-oriented open source framework for creating UPnP
    devices and control points, written in C using GObject and libsoup.
    The GUPnP API is intended to be easy to use, efficient and flexible.
    
    GUPnP DLNA is a small utility library that aims to ease the
    DLNA-related tasks such as media profile guessing, transcoding to
    a given profile, etc.
    
    GUPnP DLNA is free software released under the GNU LGPL.

---
+++ net/gupnp-dlna/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/10/06 12:41:09 ryoon Exp $
+
+BUILDLINK_TREE+=	gupnp-dlna
+
+.if !defined(GUPNP_DLNA_BUILDLINK3_MK)
+GUPNP_DLNA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gupnp-dlna+=	gupnp-dlna>=0.6.6
+BUILDLINK_PKGSRCDIR.gupnp-dlna?=	../../net/gupnp-dlna
+
+.include "../../net/gupnp/buildlink3.mk"
+#.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../multimedia/gstreamer0.10/buildlink3.mk"
+#.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
+.endif	# GUPNP_DLNA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gupnp-dlna
