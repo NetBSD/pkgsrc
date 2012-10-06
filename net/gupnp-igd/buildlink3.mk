9f09430d77c34d88b83ba96162f2b83f0230d46f
ryoon <ryoon@pkgsrc.org>
2012-10-06 08:18:53 +0000

    Update to 0.2.0
    * Bump BUILDLINK_API_DEPENDS and BUILDLINK_ABI_DEPENDS
    
    Changelog:
    
    *******************
    * GUPnP IGD 0.2.0 *
    *******************
    
    - Use the thread current GMainContext instead of passing it as a parameter
    - Make the GUPnPIgdThread object work with the new GUPnP 0.18
    
    
    ********************
    * GUPnP IGD 0.1.11 *
    ********************
    
    - Disable introspection correctly on --disable-introspection
    
    ********************
    * GUPnP IGD 0.1.10 *
    ********************
    
    - Port to gobject-introspection 0.10
    
    *******************
    * GUPnP IGD 0.1.9 *
    *******************
    
    - Set thread default main context
    
    *******************
    * GUPnP IGD 0.1.8 *
    *******************
    
    - Add GObject Introspection support
    - Return a valid GError if the other side returns an invalid IP
    - Make newer GNU Make and newer GCC happy

--- net/gupnp-igd/buildlink3.mk
+++ net/gupnp-igd/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2012/09/15 10:06:01 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2012/10/06 08:18:53 ryoon Exp $
 
 BUILDLINK_TREE+=	gupnp-igd
 
 .if !defined(GUPNP_IGD_BUILDLINK3_MK)
 GUPNP_IGD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gupnp-igd+=	gupnp-igd>=0.1.3
-BUILDLINK_ABI_DEPENDS.gupnp-igd+=	gupnp-igd>=0.1.7nb8
+BUILDLINK_API_DEPENDS.gupnp-igd+=	gupnp-igd>=0.2.0
+BUILDLINK_ABI_DEPENDS.gupnp-igd+=	gupnp-igd>=0.2.0
 BUILDLINK_PKGSRCDIR.gupnp-igd?=	../../net/gupnp-igd
 
 .include "../../net/gupnp/buildlink3.mk"
