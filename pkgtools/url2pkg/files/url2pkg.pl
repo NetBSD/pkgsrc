6b9956270d20e2aea027c5c78b3566553fa24eab
ryoon <ryoon@pkgsrc.org>
2012-10-06 13:06:31 +0000

    Bump version to 2.16
    * Recognize .tar.xz in distfile's name

--- pkgtools/url2pkg/files/url2pkg.pl
+++ pkgtools/url2pkg/files/url2pkg.pl
@@ -1,5 +1,5 @@
 #! @PERL@
-# $NetBSD: url2pkg.pl,v 1.19 2012/09/30 09:06:50 asau Exp $
+# $NetBSD: url2pkg.pl,v 1.20 2012/10/06 13:06:31 ryoon Exp $
 #
 
 # Copyright (c) 2010 The NetBSD Foundation, Inc.
@@ -306,7 +306,7 @@ sub generate_initial_package($) {
 		}
 	}
 
-	if ($distfile =~ qr"^(.*)(\.tgz|\.tar\.Z|\.tar\.gz|\.tar\.bz2)$") {
+	if ($distfile =~ qr"^(.*)(\.tgz|\.tar\.Z|\.tar\.gz|\.tar\.bz2|\.tar\.xz)$") {
 		($distname, $dist_sufx) = ($1, $2);
 	} elsif ($distfile =~ qr"^(.*)(\.[^.]+)$") {
 		($distname, $dist_sufx) = ($1, $2);
