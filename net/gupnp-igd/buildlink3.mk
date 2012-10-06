36630178c275f396641b55cbf97c258fc3084251
ryoon <ryoon>
2012-10-06 09:37:37 +0000

    Update 0.2.1
    Changelog:
    *******************
    * GUPnP IGD 0.2.1 *
    *******************
    
    - Restore the "main-context" property so that outsider can use the
      GUPnPIgdThread's GMainContext for timers, etc

--- net/gupnp-igd/buildlink3.mk
+++ net/gupnp-igd/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2012/10/06 08:18:53 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2012/10/06 09:37:37 ryoon Exp $
 
 BUILDLINK_TREE+=	gupnp-igd
 
 .if !defined(GUPNP_IGD_BUILDLINK3_MK)
 GUPNP_IGD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gupnp-igd+=	gupnp-igd>=0.2.0
-BUILDLINK_ABI_DEPENDS.gupnp-igd+=	gupnp-igd>=0.2.0
+BUILDLINK_API_DEPENDS.gupnp-igd+=	gupnp-igd>=0.2.1
+BUILDLINK_ABI_DEPENDS.gupnp-igd+=	gupnp-igd>=0.2.1
 BUILDLINK_PKGSRCDIR.gupnp-igd?=	../../net/gupnp-igd
 
 .include "../../net/gupnp/buildlink3.mk"
