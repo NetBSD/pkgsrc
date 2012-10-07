99a5eec808a29a1863a94c8c3ca32128b149834f
prlw1 <prlw1@pkgsrc.org>
2012-10-07 10:23:51 +0000

    Update gobject-introspection 1.34.0 glib 2.34.0 update seems to have broken gobject-introspection 1.32.1 build

--- devel/gobject-introspection/buildlink3.mk
+++ devel/gobject-introspection/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2012/09/15 10:04:10 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2012/10/07 10:23:51 prlw1 Exp $
 
 BUILDLINK_TREE+=	gobject-introspection
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gobject-introspection
 GOBJECT_INTROSPECTION_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gobject-introspection+=	gobject-introspection>=0.6.14nb1
-BUILDLINK_ABI_DEPENDS.gobject-introspection+=	gobject-introspection>=1.32.1nb2
+BUILDLINK_ABI_DEPENDS.gobject-introspection+=	gobject-introspection>=1.34.0
 BUILDLINK_PKGSRCDIR.gobject-introspection?=	../../devel/gobject-introspection
 
 
