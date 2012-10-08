f950a1e27da8c9f66b9e9194d36cd099bb50a656
jaapb <jaapb@pkgsrc.org>
2012-10-08 15:18:20 +0000

    Revision bump associated with the update of lang/ocaml to version 4.

--- security/ocaml-cryptokit/buildlink3.mk
+++ security/ocaml-cryptokit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/05/12 17:53:40 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/10/08 15:18:24 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cryptokit
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-cryptokit
 OCAML_CRYPTOKIT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-cryptokit+=	ocaml-cryptokit>=1.5
-BUILDLINK_ABI_DEPENDS.ocaml-cryptokit?=	ocaml-cryptokit>=1.5
+BUILDLINK_ABI_DEPENDS.ocaml-cryptokit?=	ocaml-cryptokit>=1.6nb1
 BUILDLINK_PKGSRCDIR.ocaml-cryptokit?=	../../security/ocaml-cryptokit
 
 .include "../../lang/ocaml/buildlink3.mk"
--- x11/ocaml-graphics/buildlink3.mk
+++ x11/ocaml-graphics/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2012/05/07 01:54:11 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2012/10/08 15:18:26 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-graphics
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-graphics
 OCAML_GRAPHICS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-graphics+=	ocaml-graphics>=3.08.2
-BUILDLINK_ABI_DEPENDS.ocaml-graphics+=	ocaml-graphics>=3.12.0nb5
+BUILDLINK_ABI_DEPENDS.ocaml-graphics+=	ocaml-graphics>=4.00.1
 BUILDLINK_PKGSRCDIR.ocaml-graphics?=	../../x11/ocaml-graphics
 
 .include "../../lang/ocaml/buildlink3.mk"
