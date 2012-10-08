014ee9209961131fa732ff199df2a64eddcd5462
prlw1 <prlw1@pkgsrc.org>
2012-10-08 20:19:31 +0000

    Update to 2.6.0
    * Bug 635332: Include C header information in GIR
    * Added atk_relation_set_contains_target to atk.symbols
    * Bug 672869: Required a method to check if a relationset contains a
        relation taking into account relationship and target
    * Added extra doc on atk_remove_[key/global]_event_listener
    * Fixed compilation with some gcc versions/combination of options
    * Provide a default implementation for
        atk_util_[add/remove]_global_event_listner
    * Bug 478587: "accessible-role" - invalid default value
    * Updated translations: Japanese, Brazilian Portuguese, Polish, Galician,
        Latvian, Catalan, Catalan (Valencian), Greek, Norwegian, Spanish

--- devel/atk/buildlink3.mk
+++ devel/atk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2012/09/15 10:04:05 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2012/10/08 20:19:31 prlw1 Exp $
 
 BUILDLINK_TREE+=	atk
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	atk
 ATK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.atk+=		atk>=1.11.4
-BUILDLINK_ABI_DEPENDS.atk+=	atk>=2.4.0nb1
+BUILDLINK_ABI_DEPENDS.atk+=	atk>=2.6.0
 BUILDLINK_PKGSRCDIR.atk?=	../../devel/atk
 
 .include "../../devel/gettext-lib/buildlink3.mk"
