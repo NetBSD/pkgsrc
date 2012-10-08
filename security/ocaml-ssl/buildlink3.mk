be0a6937d1d4d8afe95a4c561478fcf402169c83
jaapb <jaapb@pkgsrc.org>
2012-10-08 15:18:20 +0000

    Revision bump associated with the update of lang/ocaml to version 4.

--- security/ocaml-ssl/buildlink3.mk
+++ security/ocaml-ssl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2012/07/14 21:23:49 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2012/10/08 15:18:24 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ssl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ssl
 OCAML_SSL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.4.0
-BUILDLINK_ABI_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.4.2nb1
+BUILDLINK_ABI_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.4.6nb1
 BUILDLINK_PKGSRCDIR.ocaml-ssl?=	../../security/ocaml-ssl
 
 .include "../../lang/ocaml/buildlink3.mk"
