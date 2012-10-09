e3ee72fead79ccc06bbb8c6c3a6b68677ae5cafa
jnemeth <jnemeth@pkgsrc.org>
2012-10-09 01:36:31 +0000

    upgrade to Asterisk 10.9.0:  this is a bug fix release:
    The Asterisk Development Team has announced the release of Asterisk 10.9.0.
    
    The release of Asterisk 10.9.0 resolves several issues reported by the
    community and would have not been possible without your participation.
    Thank you!
    
    The following is a sample of the issues resolved in this release:
    
    * --- Fix channel reference leak in ChanSpy.
    
    * --- dsp.c: Fix multiple issues when no-interdigit delay is present,
          and fast DTMF 50ms/50ms
    
    * --- Fix bug where final queue member would not be removed from
          memory.
    
    * --- Fix memory leak when CEL is successfully written to PostgreSQL
          database
    
    * --- Fix DUNDi message routing bug when neighboring peer is
          unreachable
    
    For a full list of changes in this release, please see the ChangeLog:
    
    http://downloads.asterisk.org/pub/telephony/asterisk/ChangeLog-10.9.0
    
    Thank you for your continued support of Asterisk!

--- comms/asterisk10/patches/patch-apps_app__followme.c
+++ comms/asterisk10/patches/patch-apps_app__followme.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-apps_app__followme.c,v 1.1.1.1 2012/01/15 18:36:21 jnemeth Exp $
+$NetBSD: patch-apps_app__followme.c,v 1.2 2012/10/09 01:36:32 jnemeth Exp $
 
---- apps/app_followme.c.orig	2011-09-20 22:49:36.000000000 +0000
+--- apps/app_followme.c.orig	2012-09-06 00:56:47.000000000 +0000
 +++ apps/app_followme.c
-@@ -1011,12 +1011,12 @@ static void end_bridge_callback(void *da
+@@ -1127,12 +1127,12 @@ static void end_bridge_callback(void *da
  
  	ast_channel_lock(chan);
  	if (chan->cdr->answer.tv_sec) {
