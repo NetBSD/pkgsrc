af4fea3e2e68e6a6041c168650920922bf9951ad
kristerw <kristerw>
2012-10-09 01:41:59 +0000

    Correct BUILDLINK_ABI_DEPENDS.camlp5 to say 6.06 instead of 6.06nb1.

--- lang/camlp5/buildlink3.mk
+++ lang/camlp5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2012/10/08 15:31:14 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2012/10/09 01:41:59 kristerw Exp $
 #
 
 BUILDLINK_TREE+=	camlp5
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	camlp5
 CAMLP5_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.camlp5+=	camlp5>=5.01
-BUILDLINK_ABI_DEPENDS.camlp5+=	camlp5>=6.06nb1
+BUILDLINK_ABI_DEPENDS.camlp5+=	camlp5>=6.06
 BUILDLINK_PKGSRCDIR.camlp5?=	../../lang/camlp5
 
 .include "../../lang/ocaml/buildlink3.mk"
