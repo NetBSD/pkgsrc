80c051b17432c06661a3866c0550bd33d45ae8ab
drochner <drochner@pkgsrc.org>
2012-10-10 13:06:14 +0000

    switch to lcms2, bump PKGREV

--- graphics/libraw/buildlink3.mk
+++ graphics/libraw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2012/08/04 14:36:42 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2012/10/10 13:06:14 drochner Exp $
 
 BUILDLINK_TREE+=	libraw
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.libraw+=	libraw>=0.12.0
 BUILDLINK_ABI_DEPENDS.libraw+=	libraw>=0.14.6
 BUILDLINK_PKGSRCDIR.libraw?=	../../graphics/libraw
 
-.include "../../graphics/lcms/buildlink3.mk"
+.include "../../graphics/lcms2/buildlink3.mk"
 .endif	# LIBRAW_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libraw
