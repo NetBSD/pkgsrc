5c8da10d871154a4e721d1268254f3ac04b84f65
markd <markd@pkgsrc.org>
2012-10-10 19:45:49 +0000

    Add heimdal support patches from imap-uw package.

--- mail/alpine/options.mk
+++ mail/alpine/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.3 2011/05/10 01:33:12 obache Exp $
+# $NetBSD: options.mk,v 1.4 2012/10/10 19:45:49 markd Exp $
 #
 
 PKG_OPTIONS_VAR=        PKG_OPTIONS.alpine
-PKG_SUPPORTED_OPTIONS=  fancythreads ldap maildir pinepwd alpinesearchheader
+PKG_SUPPORTED_OPTIONS=  fancythreads ldap kerberos maildir pinepwd alpinesearchheader
 PKG_SUGGESTED_OPTIONS=  pinepwd
 
 .include "../../mk/bsd.options.mk"
@@ -13,11 +13,18 @@ PATCHFILES+=            fancy.patch.gz
 PATCH_DIST_STRIP=       -p1
 .endif
 
-# not tested
-#.if !empty(PKG_OPTIONS:Mkerberos)
-#CONFIGURE_ARGS+=       --with-krb5
-#.include "../../mk/krb5.buildlink3.mk"
-#.endif
+.if !empty(PKG_OPTIONS:Mkerberos)
+CONFIGURE_ARGS+=	--with-krb5
+.include "../../mk/krb5.buildlink3.mk"
+MAKE_ENV+=	KRB5_TYPE=${KRB5_TYPE}
+MAKE_FLAGS+=	EXTRAAUTHENTICATORS=gss
+EXTRASPECIALS+=	GSSDIR=${KRB5BASE}
+CFLAGS.heimdal=	-DHEIMDAL_KRB5
+CFLAGS+=	${CFLAGS.${KRB5_TYPE}}
+MAKE_FLAGS+=	EXTRALDFLAGS=${LDFLAGS:Q}
+MAKE_FLAGS+=	EXTRACFLAGS=${CFLAGS:Q}
+MAKE_FLAGS+=	EXTRASPECIALS=${EXTRASPECIALS:Q}
+.endif
 
 .if !empty(PKG_OPTIONS:Mldap)
 CONFIGURE_ARGS+=        --with-ldap-dir=${BUILDLINK_PREFIX.openldap-client}
