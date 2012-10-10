fdf7906fb93fab07002cc5636ef1f4b3ea63a995
drochner <drochner@pkgsrc.org>
2012-10-10 11:48:39 +0000

    update to 6.7.9.10 changes: many fixes and improvements pkgsrc change: switch to lcms2

--- graphics/ImageMagick/buildlink3.mk
+++ graphics/ImageMagick/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.42 2012/08/18 08:49:43 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.43 2012/10/10 11:48:39 drochner Exp $
 
 BUILDLINK_TREE+=	ImageMagick
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ImageMagick
 IMAGEMAGICK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ImageMagick+=	ImageMagick>=5.5.7.11nb1
-BUILDLINK_ABI_DEPENDS.ImageMagick+=	ImageMagick>=6.7.6.6nb3
+BUILDLINK_ABI_DEPENDS.ImageMagick+=	ImageMagick>=6.7.9.4
 BUILDLINK_PKGSRCDIR.ImageMagick?=	../../graphics/ImageMagick
 
 pkgbase := ImageMagick
@@ -35,7 +35,7 @@ pkgbase := ImageMagick
 .include "../../devel/libltdl/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
-.include "../../graphics/lcms/buildlink3.mk"
+.include "../../graphics/lcms2/buildlink3.mk"
 .include "../../graphics/libwebp/buildlink3.mk"
 .include "../../graphics/openexr/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
