59d49381843875021e44895c9c0c9e53fb2afb65
drochner <drochner>
2012-10-11 10:57:42 +0000

    propagate dependency on gst-plugins - the .pc file refers to it

--- net/gupnp-dlna/buildlink3.mk
+++ net/gupnp-dlna/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/10/06 12:41:09 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/10/11 10:57:42 drochner Exp $
 
 BUILDLINK_TREE+=	gupnp-dlna
 
@@ -11,7 +11,7 @@ BUILDLINK_PKGSRCDIR.gupnp-dlna?=	../../net/gupnp-dlna
 .include "../../net/gupnp/buildlink3.mk"
 #.include "../../textproc/libxml2/buildlink3.mk"
 .include "../../multimedia/gstreamer0.10/buildlink3.mk"
-#.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
+.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
 .endif	# GUPNP_DLNA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gupnp-dlna
