98b3e26b03bd314f4f0ed8eca41bf2b6f483290b
spz <spz@pkgsrc.org>
2012-10-11 18:42:19 +0000

    - fix FreeBSD/Dragonfly properly - update to newest version
    Upstream changelog:
    
    Xymon release 4.3.10 - released on Aug 6 2012
    =============================================
    
    Main features in this release is the addition of inode-checks
    on all major platforms, and a series of enhancements to the
    build procedure.
    
    Also, building Xymon on most common platforms is now explicitly
    described in the "install" document.
    
    * Fix build problems with "errno"
    * Fix build problems with OpenSSL in non-default locations
    * Fix build problems with certain LDAP configurations
    * Fix build problems with RRDtool on FreeBSD / OpenBSD
    * Fix problem with ifstat data from Fedora in graphs
    * "inode" check on FreeBSD, OpenBSD, OSX, Solaris, HP/UX, AIX
      in addition to existing support for Linux
    * Document building and installing Xymon on common platforms
      (Linux, FreeBSD, OpenBSD, Solaris)
    * Enhance xymoncfg so it can be used to import Xymon configuration
      settings into shell-scripts.
    
    
    Xymon release 4.3.9 - released on Jul 24 2012
    =============================================
    
    This release is mainly a bug-fix release.
    
    * Fix crash when XYMSRV is undefined but XYMSERVERS is
    * Fix error in calculating combo-status messages with
      forward references
    * Fix error in disable-until-TIME or disable-until-OK code
    * Fix documentation of DURATION in alerts.cfg / xymond_alert so
      it is consistenly listed as being in "minutes".
    * Permit explicit use of ">" and ">=" in alerts.cfg
    * Permit building without the RRDtool libraries, e.g. for
      a network-tester build, but with trend-graphing disabled.
    * Full compiler-warning cleanup
    * Various configuration/build-script issues fixed.
    
    
    Xymon release 4.3.8 - released on Jul 15 2012
    =============================================
    
    This release is mainly a bug-fix release.
    
    Bugfixes
    * Workaround for DNS timeout handling, now fixed at approximately 25
      seconds.
    * "hostinfo" command for xymond documented
    * confreport only shows processes that are monitored
    * analysis.cfg parsing of COLOR for UP rules was broken
    * RRD handlers no longer crash after receiving 1 billion updates
    * Using .netrc for authentication could crash xymonnet
    * "directory" includes would report the wrong filename for missing
      directories.
    * useradm CGI would invoke htpassword twice
    * "include" and "directory" now ignores trailing whitespace
    * SSLv2 support disabled if SSL-library does not support it
    * Minor bugfixes and cleanups of compiler warnings.
    
    Enhancements
    * Service status on info page now links to the detailed status page.
    * Add RRDGRAPHOPTS setting to permit global user-specified RRD options,
      e.g. for font to showgraph CGI
    * Add check for the size of public keys used in SSL certificates
      (enabled via --sslkeysize=N option for xymonnet)
    * Optionally disable the display of SSL ciphers in the sslcert status
      (the --no-cipherlist option for xymonnet)
    * Improved build-scripts works on newer systems with libraries in
      new and surprising places
    * Reduce xymonnet memory usage and runtime for ping tests when there
      are multiple hosts.cfg entries with the same IP-address.
    * Add code for inode-monitoring on Linux. Does not currently work on
      any other client platform.
    * Added the ability to disable tests until a specific time, instead of
      for some interval. Disabling a test also now computes the expire time
      for the disable to happen at the next closest minute.

--- net/xymon/patches/patch-build_rrd.sh
+++ net/xymon/patches/patch-build_rrd.sh
@@ -1,8 +1,8 @@
-$NetBSD: patch-build_rrd.sh,v 1.2 2011/10/16 15:19:09 spz Exp $
+$NetBSD: patch-build_rrd.sh,v 1.3 2012/10/11 18:42:19 spz Exp $
 
 make it possible to deal with nonstandard locations of libpng* and libz*
 
---- build/rrd.sh.orig	2011-03-08 17:20:28.000000000 +0000
+--- build/rrd.sh.orig	2012-07-31 13:51:12.000000000 +0000
 +++ build/rrd.sh
 @@ -70,6 +70,12 @@
  	if test "$USERRRDLIB" != ""; then
@@ -15,5 +15,5 @@ make it possible to deal with nonstandard locations of libpng* and libz*
 +		ZLIB="$USERZLIB"
 +	fi
  
- 	if test -z "$RRDINC" -o -z "$RRDLIB"; then
- 		echo "RRDtool include- or library-files not found. These are REQUIRED for Xymon"
+ 	# See if it builds
+ 	RRDOK="YES"
