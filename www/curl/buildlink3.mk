4df8535e320f90ad47993e16e120af2155e39ab3
adam <adam@pkgsrc.org>
2012-10-12 07:36:11 +0000

    Changes 7.28.0:
    SSH: added agent based authentication
    ftp: active conn, allow application to set sockopt after accept() call with CURLSOCKTYPE_ACCEPT
    multi: add curl_multi_wait()
    metalink: Added support for Microsoft Windows CryptoAPI
    md5: Added support for Microsoft Windows CryptoAPI
    parse_proxy: treat "socks://x" as a socks4 proxy
    socks: Added support for IPv6 connections through SOCKSv5 proxy
    Bugfixes:
    
    WSAPoll disabled on Windows builds due to its bugs
    segfault on request retries
    curl-config: parentheses fix
    VC build: add define for openssl
    globbing: fix segfault when >9 globs were used
    fixed a few clang-analyzer warnings
    metalink: change code order to build with gnutls-nettle
    gtls: fix build failure by including nettle-specific headers
    change preferred HTTP auth on a handle previously used for another auth
    file: use fdopen() to avoid race condition
    Added DWANT_IDN_PROTOTYPES define for MSVC too
    verbose: fixed (nil) output of hostnames in re-used connections
    metalink: Un-broke the build when building --with-darwinssl
    curl man page cleanup
    Avoid leak of local device string when reusing connection
    Curl_socket_check: fix return code for timeout
    nss: do not print misleading NSS error codes
    configure: remove the --enable/disable-nonblocking options
    darwinssl: add TLS 1.1 and 1.2 support, replace deprecated functions
    NTLM: re-use existing connection better
    schannel crash on multi and easy handle cleanup
    SOCKS: truly disable it if CURL_DISABLE_PROXY is defined
    mk-ca-bundle: detect start of trust section better
    gnutls: do not fail on non-fatal handshake errors
    SMTP: only send SIZE if supported
    ftpserver: respond with a 250 to SMTP EHLO
    ssh: do not crash if MD5 fingerprint is not provided by libssh2
    winbuild: Added support for building with SPNEGO enabled
    metalink: Fixed validation of binary files containing EOF
    setup.h: fixed for MS VC10 build
    cmake: use standard findxxx modules for cmake v2.8+
    HTTP_ONLY: disable more protocols
    Curl_reconnect_request: clear pointer on failure
    https.c example: remember to call curl_global_init()
    metalink: Filter resource URLs by type
    multi interface: CURLOPT_LOW_SPEED_* fix during rate limitation
    curl_schannel: Removed buffer limit and optimized buffer strategy

--- www/curl/buildlink3.mk
+++ www/curl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2011/10/05 02:14:19 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2012/10/12 07:36:11 adam Exp $
 
 BUILDLINK_TREE+=	curl
 
@@ -10,19 +10,19 @@ BUILDLINK_ABI_DEPENDS.curl+=	curl>=7.21.4nb1
 BUILDLINK_PKGSRCDIR.curl?=	../../www/curl
 
 pkgbase:= curl
-.  include "../../mk/pkg-build-options.mk"
-.  if !empty(PKG_BUILD_OPTIONS.curl:Mlibssh2)
-.    include "../../security/libssh2/buildlink3.mk"
-.  endif
-.  if !empty(PKG_BUILD_OPTIONS.curl:Mgssapi)
-.    include "../../mk/krb5.buildlink3.mk"
-.  endif
-.  if !empty(PKG_BUILD_OPTIONS.curl:Mldap)
-.    include "../../databases/openldap-client/buildlink3.mk"
-.  endif
-.  if !empty(PKG_BUILD_OPTIONS.curl:Mrtmp)
-.    include "../../net/rtmpdump/buildlink3.mk"
-.  endif
+.include "../../mk/pkg-build-options.mk"
+.if !empty(PKG_BUILD_OPTIONS.curl:Mlibssh2)
+.  include "../../security/libssh2/buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.curl:Mgssapi)
+.  include "../../mk/krb5.buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.curl:Mldap)
+.  include "../../databases/openldap-client/buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.curl:Mrtmp)
+.  include "../../net/rtmpdump/buildlink3.mk"
+.endif
 
 .include "../../devel/libidn/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
