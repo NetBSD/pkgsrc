a7d7616cc3911624b092c8b74f49a24214f683cb
ryoon <ryoon@pkgsrc.org>
2012-10-12 18:27:20 +0000

    Update to 16.0.1
    Changelog:
        FIXED
        16.0.1: Vulnerability outlined here
    	https://blog.mozilla.org/security/2012/10/10/security-vulnerability-in-firefox-16/
        NEW
        Firefox on Mac OS X now has preliminary VoiceOver support turned on by default
        NEW
        Initial web app support (Windows/Mac/Linux)
        NEW
        Acholi and Kazakh localizations added
        CHANGED
        Improvements around JavaScript responsiveness through incremental garbage collection
        DEVELOPER
        New Developer Toolbar with buttons for quick access to tools, error count for the Web Console, and a new command line for quick keyboard access
        DEVELOPER
        CSS3 Animations, Transitions, Transforms and Gradients unprefixed in Firefox 16
        DEVELOPER
        Recently opened files list in Scratchpad implemented
        FIXED
        16.0.1: Vulnerability outlined here
    	https://blog.mozilla.org/security/2012/10/10/security-vulnerability-in-firefox-16/
        FIXED
        Debugger breakpoints do not catch on page reload (783393)
        FIXED
        No longer supporting MD5 as a hash algorithm in digital signatures (650355)
        FIXED
        Opus support by default (772341)
        FIXED
        Reverse animation direction has been implemented (655920)
        FIXED
        Per tab reporting in about:memory (687724)
        FIXED
        User Agent strings for pre-release Firefox versions now show only major version (728831)

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,11 +1,11 @@
-# $NetBSD: dist.mk,v 1.34 2012/09/12 14:27:14 ryoon Exp $
+# $NetBSD: dist.mk,v 1.35 2012/10/12 18:27:20 ryoon Exp $
 #
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	15.0
+MOZ_BRANCH=	16.0
 MOZ_BRANCH_MINOR=	.1
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
