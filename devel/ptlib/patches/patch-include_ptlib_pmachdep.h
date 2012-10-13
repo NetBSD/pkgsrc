e6c40990216a92463f22a1d0a7a5e2fd56447247
darcy <darcy@pkgsrc.org>
2012-10-13 22:43:22 +0000

    Missed two more files in PLIST. Add another patch that stops an annoying warning.

---
+++ devel/ptlib/patches/patch-include_ptlib_pmachdep.h
@@ -0,0 +1,46 @@
+$NetBSD: patch-include_ptlib_pmachdep.h,v 1.1 2012/10/13 22:43:23 darcy Exp $
+
+- Prevent an annoying warning while building
+
+--- /usr/obj/devel/ptlib/work.x86_64/ptlib-2.10.7/include/ptlib/unix/ptlib/pmachdep.h.orig	2012-08-23 02:12:57.000000000 +0000
++++ /usr/obj/devel/ptlib/work.x86_64/ptlib-2.10.7/include/ptlib/unix/ptlib/pmachdep.h
+@@ -112,7 +112,9 @@ typedef int socklen_t;
+ #elif defined(P_OPENBSD)
+ 
+ #if defined(P_PTHREADS)
++#ifndef _THREAD_SAFE
+ #define _THREAD_SAFE
++#endif
+ #define P_THREAD_SAFE_CLIB
+ 
+ #include <pthread.h>
+@@ -137,7 +139,9 @@ typedef int socklen_t;
+ #elif defined(P_NETBSD)
+ 
+ #if defined(P_PTHREADS)
++#ifndef _THREAD_SAFE
+ #define _THREAD_SAFE
++#endif
+ #define P_THREAD_SAFE_CLIB
+ 
+ #include <pthread.h>
+@@ -447,7 +451,9 @@ struct hostent * Vx_gethostbyaddr(char *
+ #define strcasecmp strcmp
+ 
+ #define P_HAS_SEMAPHORES
++#ifndef _THREAD_SAFE
+ #define _THREAD_SAFE
++#endif
+ #define P_THREAD_SAFE_CLIB
+ typedef long PThreadIdentifier;
+ typedef long PProcessIdentifier;
+@@ -481,7 +487,9 @@ extern "C" {
+ #elif defined(P_QNX)
+ 
+ #if defined(P_PTHREADS)
++#ifndef _THREAD_SAFE
+ #define _THREAD_SAFE
++#endif
+ #define P_THREAD_SAFE_CLIB
+ 
+ #include <pthread.h>
