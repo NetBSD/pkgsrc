1cd0f9b913dd22658bcd3c16ae6d1e1d10178d0e
minskim <minskim>
2012-10-14 18:12:37 +0000

    TEX_FORMAT_NAMES is no longer used.

--- print/texlive-tetex/format.mk
+++ print/texlive-tetex/format.mk
@@ -1,4 +1,4 @@
-# $NetBSD: format.mk,v 1.5 2010/01/29 19:59:47 minskim Exp $
+# $NetBSD: format.mk,v 1.6 2012/10/14 18:12:37 minskim Exp $
 #
 # This Makefile fragment is intended to be included by packages that
 # install TeX format files.  It creates format files at
@@ -8,8 +8,6 @@
 #
 #    TEX_FORMATS - A list of parameters to build formats
 #
-#    TEX_FORMAT_NAMES (for TeX Live 2008 only) - A list of format names.
-#
 
 .if !defined(TEX_FORMAT_MK)
 TEX_FORMAT_MK=		# defined
@@ -18,7 +16,6 @@ DEPENDS+=		texlive-tetex>=2008:../../print/texlive-tetex
 
 EVAL_PREFIX+=		TETEX_PREFIX=texlive-tetex
 FILES_SUBST+=		FMTUTIL_SYS=${TETEX_PREFIX:Q}/bin/fmtutil-sys
-FILES_SUBST+=		FORMAT_NAMES=${TEX_FORMAT_NAMES:Q}
 FILES_SUBST+=		FORMATS=${TEX_FORMATS:Q}
 FILES_SUBST+=		PRINTF=${PRINTF:Q}
 INSTALL_TEMPLATES+=	../../print/texlive-tetex/files/format-install.tmpl
