882d2a2b91824e58ecece81f14dd8c168bc46f89
wiz <wiz>
2012-10-16 17:47:19 +0000

    Recursive bump for new gtkglext pangox-compat dependency.

--- graphics/gtkglext/buildlink3.mk
+++ graphics/gtkglext/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.30 2012/10/16 17:43:56 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2012/10/16 17:47:19 wiz Exp $
 
 BUILDLINK_TREE+=	gtkglext
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gtkglext
 GTKGLEXT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gtkglext+=	gtkglext>=1.2.0
-BUILDLINK_ABI_DEPENDS.gtkglext+=	gtkglext>=1.2.0nb17
+BUILDLINK_ABI_DEPENDS.gtkglext+=	gtkglext>=1.2.0nb18
 BUILDLINK_PKGSRCDIR.gtkglext?=	../../graphics/gtkglext
 
 .include "../../devel/pangox-compat/buildlink3.mk"
