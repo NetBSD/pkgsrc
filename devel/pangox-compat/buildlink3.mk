abc75eb29ec7c3c76e2864bc43f45516dffa6b57
wiz <wiz>
2012-10-16 17:50:03 +0000

    Add pangox-compat-0.0.1:
    This is a compatibility library providing the obsolete pangox library
    that is not shipped by Pango itself anymore.  If you are still using
    pangox, well, really, get a life...

---
+++ devel/pangox-compat/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/10/16 17:50:03 wiz Exp $
+
+BUILDLINK_TREE+=	pangox-compat
+
+.if !defined(PANGOX_COMPAT_BUILDLINK3_MK)
+PANGOX_COMPAT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.pangox-compat+=	pangox-compat>=0.0.1
+BUILDLINK_PKGSRCDIR.pangox-compat?=	../../devel/pangox-compat
+
+.include "../../devel/pango/buildlink3.mk"
+.endif	# PANGOX_COMPAT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-pangox-compat
