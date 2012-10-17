5e90f421684bab6aadb442e35a7871ea4d8978a5
asau <asau@pkgsrc.org>
2012-10-17 23:27:34 +0000

    Update to OpenMPI 1.6.2 Don't build VampirTrace anymore, it will be introduced as separate package.
    Changes in v1.6.2 as compared to v1.6.1:
    
    - Fix issue with MX MTL. Thanks to Doug Eadline for raising the issue.
    - Fix singleton MPI_COMM_SPAWN when the result job spans multiple nodes.
    - Fix MXM hang, and update for latest version of MXM.
    - Update to support Mellanox FCA 2.5.
    - Fix startup hang for large jobs.
    - Ensure MPI_TESTANY / MPI_WAITANY properly set the empty status when
      count==0.
    - Fix MPI_CART_SUB behavior of not copying periods to the new
      communicator properly. Thanks to John Craske for the bug report.
    - Add btl_openib_abort_not_enough_reg_mem MCA parameter to cause Open
      MPI to abort MPI jobs if there is not enough registered memory
      available on the system (vs. just printing a warning). Thanks to
      Brock Palen for raising the issue.
    - Minor fix to Fortran MPI_INFO_GET: only copy a value back to the
      user's buffer if the flag is .TRUE.
    - Fix VampirTrace compilation issue with the PGI compiler suite.

--- parallel/openmpi/options.mk
+++ parallel/openmpi/options.mk
@@ -1,26 +1,10 @@
-# $NetBSD: options.mk,v 1.3 2012/06/12 23:36:31 asau Exp $
+# $NetBSD: options.mk,v 1.4 2012/10/17 23:27:34 asau Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.openmpi
-PKG_SUPPORTED_OPTIONS=	debug openmpi-vampirtrace
-PKG_SUGGESTED_OPTIONS=	openmpi-vampirtrace
+PKG_SUPPORTED_OPTIONS=	debug
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-debug
 .endif
-
-# Allow coexistance of OpenMPI and libotf by disabling OpenMPI parts:
-.if !empty(PKG_OPTIONS:Mopenmpi-vampirtrace)
-CONFLICTS+=	libotf-[0-9]*
-PLIST_SRC+=	PLIST.vt
-
-CONF_FILES+=		share/vampirtrace/VampirTrace/examples/vt-java-default-filter.spec \
-			$(PKG_SYSCONFDIR)/vt-java-default-filter.spec
-
-.include "../../graphics/freetype2/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--enable-contrib-no-build=vt
-.endif
-
-PLIST_SRC+=	PLIST
