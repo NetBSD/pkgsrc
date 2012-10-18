f584ccb271720f626cc18885880e5d534b50ebce
cheusov <cheusov@pkgsrc.org>
2012-10-18 16:16:54 +0000

    Pass SYSCONFDIR=${PKG_SYSCONFDIR} to mkc-based projects through MAKE_ENV

--- devel/mk-configure/mk-c.mk
+++ devel/mk-configure/mk-c.mk
@@ -1,7 +1,8 @@
-# $NetBSD: mk-c.mk,v 1.1 2012/03/09 18:56:22 cheusov Exp $
+# $NetBSD: mk-c.mk,v 1.2 2012/10/18 16:16:54 cheusov Exp $
 
 MAKE_PROGRAM=	mkcmake
 MAKE_ENV+=	MANDIR=${PREFIX}/${PKGMANDIR} INSTALL=${INSTALL:Q}
+MAKE_ENV+=	SYSCONFDIR=${PKG_SYSCONFDIR}
 
 do-configure:
 	set -e; cd ${WRKSRC}; \
