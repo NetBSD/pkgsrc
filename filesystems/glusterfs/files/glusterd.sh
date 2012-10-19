a2b8d547bc2191dc8d94ea2d56825f46319efc29
manu <manu>
2012-10-19 04:15:21 +0000

    Bump to glusterfs-3.3.1, which brings - unified file and object storage - storage for Hadoop (not tested here) - proactive self-healing - much better performance

--- filesystems/glusterfs/files/glusterd.sh
+++ filesystems/glusterfs/files/glusterd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: glusterd.sh,v 1.9 2011/12/09 16:57:44 manu Exp $
+# $NetBSD: glusterd.sh,v 1.10 2012/10/19 04:15:21 manu Exp $
 #
 
 # PROVIDE: glusterd
@@ -9,12 +9,22 @@
 
 $_rc_subr_loaded . /etc/rc.subr
 
+glusterd_precmd()
+{
+	test -d @VARBASE@/lib/glusterd && return
+	test -d @PREFIX@/etc/glusterd || return
+	mv @PREFIX@/etc/glusterd @VARBASE@/lib/glusterd && \
+	    ln -s @VARBASE@/lib/glusterd @PREFIX@/etc/glusterd
+}
+
 name="glusterd"
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
+start_precmd="glusterd_precmd"
 pidfile="/var/run/${name}.pid"
 command_args="-p ${pidfile}"
 required_files="@PREFIX@/etc/glusterfs/${name}.vol"
 
 load_rc_config $name
 run_rc_command "$1"
+
