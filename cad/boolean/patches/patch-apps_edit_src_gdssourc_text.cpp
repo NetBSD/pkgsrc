41fb6109e3df0479b6124a563d388890294628f2
joerg <joerg@pkgsrc.org>
2012-10-20 22:03:01 +0000

    Taking the address of a temporary object is not valid C++.

---
+++ cad/boolean/patches/patch-apps_edit_src_gdssourc_text.cpp
@@ -0,0 +1,16 @@
+$NetBSD: patch-apps_edit_src_gdssourc_text.cpp,v 1.1 2012/10/20 22:03:01 joerg Exp $
+
+--- apps/edit/src/gdssourc/text.cpp.orig	2012-10-20 11:58:47.000000000 +0000
++++ apps/edit/src/gdssourc/text.cpp
+@@ -61,9 +61,10 @@ void Text::DoWrite(DrawDriver& dr)
+ 
+ 	dr.SetSelected(GetSelect());
+ 
++   wxTransformMatrix tmp(*dr.GetMatrix() * *GetRelativeMatrix());
+    dr.GDSDrawText(_text, min_point.GetX(), min_point.GetY(),
+ 										  max_point.GetX(), max_point.GetY(),
+-								   	  this, &(*dr.GetMatrix() * *GetRelativeMatrix()));
++								   	  this, &tmp);
+ 
+ }
+ 
