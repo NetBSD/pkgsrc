7c9d2d36b8f02a228273eef95675b8b72585378e
joerg <joerg@pkgsrc.org>
2012-10-20 22:03:01 +0000

    Taking the address of a temporary object is not valid C++.

---
+++ cad/boolean/patches/patch-apps_edit_src_drivers_printer.cpp
@@ -0,0 +1,14 @@
+$NetBSD: patch-apps_edit_src_drivers_printer.cpp,v 1.1 2012/10/20 22:03:01 joerg Exp $
+
+--- apps/edit/src/drivers/printer.cpp.orig	2012-10-20 12:02:42.000000000 +0000
++++ apps/edit/src/drivers/printer.cpp
+@@ -87,7 +87,8 @@ bool PrintDriver::OnPrintPage(int)
+       // Multiply the points with the relative matrix
+       for (int i = 0; i < 4; i++)
+       {
+-         corners[i].Transform(&GLOB->SCRDRV->GetInverseMappingMatrix());
++         wxTransformMatrix tmp(GLOB->SCRDRV->GetInverseMappingMatrix());
++         corners[i].Transform(&tmp);
+       }
+       x = (int) corners[0].GetX();
+       y = (int) corners[0].GetY();
