9bf1ab076a3f64fb85f2224fb81cd8476b7917b8
rh <rh@pkgsrc.org>
2012-10-22 02:48:40 +0000

    Update Addresses to 0.4.8
    Notable changes include:
    
    Since 0.4.8:
    ============
    
      o relative link during framework installing, helps building in a separate
        root, like when building RPMs
      o fixed some portability issues of the Framework to solaris and windows
      o handle also PNG images
      o fixed a link in the framework from absolute to relative, for FHS
        (fix by Richard Stonehouse)
      o Many fixes in VCFviewer GWorkspace inspector, thanks also to
        Philippe Roussel
      o use GSMimeDocument to decode base64 instead of custom code
      o Makefiles work on windows
      o Birthday support (for SimpleAgenda)
      o fixes in VCF import (encoding, CR-LF)
      o fixed deleting of a person
    
    Since 0.4.7:
    ============
    
      o Updated Makefiles to GNUstep Make 2.0
      o Fixed text editing to work with current GNUstep gui
      o Improved import of VCF files: tries to interpret HEX (by Guenther
        Noack) and the escape \, from Apple cards

--- misc/Addresses/buildlink3.mk
+++ misc/Addresses/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/04/13 12:37:23 rh Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2012/10/22 02:48:41 rh Exp $
 
 BUILDLINK_TREE+=	Addresses
 
 .if !defined(ADDRESSES_BUILDLINK3_MK)
 ADDRESSES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.Addresses+=	Addresses>=0.4.6nb7
-BUILDLINK_ABI_DEPENDS.Addresses+=	Addresses>=0.4.6nb7
+BUILDLINK_API_DEPENDS.Addresses+=	Addresses>=0.4.8
+BUILDLINK_ABI_DEPENDS.Addresses+=	Addresses>=0.4.8
 BUILDLINK_PKGSRCDIR.Addresses?=	../../misc/Addresses
 
 .include "../../devel/gnustep-make/gnustep.mk"
