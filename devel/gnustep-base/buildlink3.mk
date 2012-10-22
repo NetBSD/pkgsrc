7145fe3caec6dd935dea6f050690aa116c278d04
rh <rh@pkgsrc.org>
2012-10-22 02:17:16 +0000

    Update gnustep-base to 1.24.0.
    Noteworthy changes in version `1.24.0'
    ======================================
    
       * There is binary incompatibility on 64bit systems in that the value
         of the NSNotFound constant there is now 64bits rather than 32bits.
    
       * gcc-4.0 is the minimum official supported compiler for GNUstep
    
       * Support for the GNU LANGUAGES environment variable and other locale
         improvements
    
       * IPV6 support for NSHost and networking operations
    
       * Support for UTF-8 string literals in source (compiler permitting)
    
    Noteworthy changes in version `1.23.0'
    ======================================
    
       * Support for automatic reference counting (ARC) when using clasng
         and the GNUstep Objective-C 2.0 runtime 1.5 or later.
    
       * Full support for weak references in NSHashTable, NSMapTable and
         NSPointerArray when using clang and the GNUstep Objective-C 2.0
         runtime 1.5 or later.
    
    Noteworthy changes in version `1.22.0'
    ======================================
    
       * Many updates and new classes
    
       * There is now a regression test framework included with the package
    
       * New support for Objective-C 2.0

--- devel/gnustep-base/buildlink3.mk
+++ devel/gnustep-base/buildlink3.mk
@@ -1,16 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.16 2009/06/14 22:57:59 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2012/10/22 02:17:16 rh Exp $
 
 BUILDLINK_TREE+=	gnustep-base
 
 .if !defined(GNUSTEP_BASE_BUILDLINK3_MK)
 GNUSTEP_BASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnustep-base+=	gnustep-base>=1.18.0
-BUILDLINK_ABI_DEPENDS.gnustep-base+=	gnustep-base>=1.18.0
+BUILDLINK_API_DEPENDS.gnustep-base+=	gnustep-base>=1.24.0
+BUILDLINK_ABI_DEPENDS.gnustep-base+=	gnustep-base>=1.24.0
 BUILDLINK_PKGSRCDIR.gnustep-base?=	../../devel/gnustep-base
 
 .include "../../devel/gnustep-make/buildlink3.mk"
 .include "../../devel/gnustep-objc/buildlink3.mk"
+.include "../../textproc/icu/buildlink3.mk"
 .endif # GNUSTEP_BASE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnustep-base
