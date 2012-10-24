edcef2e3fe570f52daef51625f45a46f0ef01ab0
asau <asau@pkgsrc.org>
2012-10-24 05:49:25 +0000

    Reexport libxml2 from hwloc. Reexport hwloc from openmpi. This should fix builds with MPI_TYPE=openmpi.

--- parallel/hwloc/buildlink3.mk
+++ parallel/hwloc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/21 13:31:35 asau Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/10/24 05:49:25 asau Exp $
 
 BUILDLINK_TREE+=	hwloc
 
@@ -8,6 +8,7 @@ HWLOC_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hwloc+=	hwloc>=1.4
 BUILDLINK_PKGSRCDIR.hwloc?=	../../parallel/hwloc
 
+.include "../../textproc/libxml2/buildlink3.mk" # optional
 .endif	# HWLOC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hwloc
