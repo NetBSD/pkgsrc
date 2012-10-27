bb9d95ccd87dd3d8cb6fd665cda81d8e267b94b2
ryoon <ryoon>
2012-10-27 16:38:00 +0000

    Update to 16.0.2
    www/firefox is also updated to 16.0.2.
    
    Changelog:
    FIXED
    16.0.2: Security fixes can be found here
    
    Fixed in Firefox 16.0.2
    MFSA 2012-90 Fixes for Location object issues

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.35 2012/10/12 18:27:20 ryoon Exp $
+# $NetBSD: dist.mk,v 1.36 2012/10/27 16:38:00 ryoon Exp $
 #
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
@@ -6,7 +6,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	16.0
-MOZ_BRANCH_MINOR=	.1
+MOZ_BRANCH_MINOR=	.2
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
