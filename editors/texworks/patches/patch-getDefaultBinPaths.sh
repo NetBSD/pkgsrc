93980ab5f248612ec66ddc3ea2b9ed67a7e33893
markd <markd@pkgsrc.org>
2012-10-28 03:33:45 +0000

    Update to 0.4.4
    Release 0.4.4 (TL'12) [April 2012]
     * Fix crash caused by Qt when adding lines at the beginning of a document
     * Fix search/replace for multi-line strings
     * Work around layout issues which cause lines to disappear
     * Fix building with BSD make
     * Fix block selection of last paragraph
     * Fix overwrite of "Find all occurrences" option
    
     * Improve "Unable to execute..." error dialog
     * Rename "Show/Hide Output Panel" to "Show/Hide Console Output" in the menu
       and the preferences dialog
     * Remove subject and add instructions to body of the "email to mailinglist"
     * Bring the window running TeX to the top at the beginning of typesetting to
       ensure that the console output is visible
     * Allow symlinks and display only folders in "path for programs" in the
       preferences dialog
     * Add "Clear Recent Files" to "Open Recent" menu
     * Add an autocompletion entry to the preferences dialog
     * Add Lua(La)TeX to the default tools (and drop LaTeXmk by default)
     * Add an option to open log files
     * Add CMake support (experimental)
     * Update SyncTeX to version 1.17
     * Update URLs to http://www.tug.org/texworks/
    
     * Some enhancements to functions available to scripting
     * Avoid showing an empty message box for script results
     * Resolve symlinks when looking for scripts
     * Expose the scripts' titles, authors, filenames, etc. to scripting
     * Update/improve scripts: open pdf in default viewer, derive spellchecking
       language from babel package, log parser

--- editors/texworks/patches/patch-getDefaultBinPaths.sh
+++ editors/texworks/patches/patch-getDefaultBinPaths.sh
@@ -1,16 +1,18 @@
-$NetBSD: patch-getDefaultBinPaths.sh,v 1.1.1.1 2012/03/21 22:44:20 markd Exp $
+$NetBSD: patch-getDefaultBinPaths.sh,v 1.2 2012/10/28 03:33:45 markd Exp $
 
---- getDefaultBinPaths.sh.orig	2011-06-29 17:03:14.000000000 +0000
+--- getDefaultBinPaths.sh.orig	2012-04-29 16:00:44.000000000 +0000
 +++ getDefaultBinPaths.sh
-@@ -68,11 +68,11 @@ case $PLATFORM in
+@@ -74,12 +74,12 @@ case $PLATFORM in
  	*)			OS=`echo $PLATFORM | sed 's/.*-//'`
  esac
  
+-appendPath "/usr/local/texlive/2012/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2011/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2010/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2009/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2008/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2007/bin/$ARCH-$OS"
++#appendPath "/usr/local/texlive/2012/bin/$ARCH-$OS"
 +#appendPath "/usr/local/texlive/2011/bin/$ARCH-$OS"
 +#appendPath "/usr/local/texlive/2010/bin/$ARCH-$OS"
 +#appendPath "/usr/local/texlive/2009/bin/$ARCH-$OS"
