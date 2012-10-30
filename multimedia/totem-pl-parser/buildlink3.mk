90b1b4970c81ae7eb9c56ad80d90752bd194d5e4
drochner <drochner@pkgsrc.org>
2012-10-30 20:05:51 +0000

    update to 3.4.3 this switches to the current stable release branch (source and binary compatible afaict)

--- multimedia/totem-pl-parser/buildlink3.mk
+++ multimedia/totem-pl-parser/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2012/10/08 23:00:46 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2012/10/30 20:05:51 drochner Exp $
 
 BUILDLINK_TREE+=	totem-pl-parser
 
@@ -10,7 +10,6 @@ BUILDLINK_ABI_DEPENDS.totem-pl-parser+=	totem-pl-parser>=2.32.6nb9
 BUILDLINK_PKGSRCDIR.totem-pl-parser?=	../../multimedia/totem-pl-parser
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../mail/gmime24/buildlink3.mk"
 .include "../../net/libsoup24/buildlink3.mk"
