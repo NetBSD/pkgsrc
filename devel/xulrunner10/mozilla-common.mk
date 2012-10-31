52ae94c9b672e9389ee68afb66df841a3afeb667
asau <asau>
2012-10-31 11:16:30 +0000

    Drop superfluous PKG_DESTDIR_SUPPORT, "user-destdir" is default these days.

--- devel/xulrunner10/mozilla-common.mk
+++ devel/xulrunner10/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.1.1.1 2012/03/15 08:58:27 ryoon Exp $
+# $NetBSD: mozilla-common.mk,v 1.2 2012/10/31 11:19:54 asau Exp $
 #
 # common Makefile fragment for mozilla packages based on gecko 2.0.
 #
@@ -11,7 +11,6 @@ USE_TOOLS+=		pkg-config perl gmake autoconf213 unzip zip
 USE_LANGUAGES+=		c99 c++
 UNLIMIT_RESOURCES+=	datasize
 
-PKG_DESTDIR_SUPPORT=	user-destdir
 CHECK_PORTABILITY_SKIP+=${MOZILLA_DIR}security/nss/tests/libpkix/libpkix.sh
 CHECK_PORTABILITY_SKIP+=${MOZILLA_DIR}security/nss/tests/multinit/multinit.sh
 CHECK_INTERPRETER_SKIP+=lib/xulrunner10-sdk/sdk/bin/xpt.py
