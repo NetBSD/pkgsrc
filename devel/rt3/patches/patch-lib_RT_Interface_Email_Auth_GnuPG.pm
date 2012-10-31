ccd232b1c2260259759126fb8645ec1161263889
spz <spz>
2012-10-31 20:39:26 +0000

    Update to the latest version in the rt3 train. Contains security updates:
      3.8.15 Release Notes
    
       This release resolves a number of security vulnerabilities.
       It resolves CVE-2012-4730, CVE-2012-4732, CVE-2012-4734, CVE-2012-4735,
       and CVE-2012-4884.
    
       In addition to these security fixes, RT 3.8.15 contains support for
       partitioned PGP messages.
    
      3.8.14 Release Notes
    
       This release contains two fixes related to the 3.8.12 security release.
    
       Access to search results URLs is now CSRF whitelisted, based on user
       feedback.
       An error in rt-email-dashboards has been corrected.
    
      3.8.13 Release Notes
    
       This release contains an important bugfix over the 3.8.12 security
       release:
    
        * Fix sending email with the 'perl-script' mod_perl handler, by
          ensuring that STDIN was always on FD 0 before calling IPC::Open2.
          This failure showed as either SIGPIPE or abnormal exit codes when
          running sendmail.
    
        * Fix for "Undefined value assigned to typeglob" and "Bad file
          descriptor: core_output_filter" errors caused by the above change, by
          ensuring that both FD 0 and FD 1 are prevented from being claimed by
          Apache.  This error only arose with the perfork MPM and mod_perl <=
          2.0.4.

--- devel/rt3/patches/patch-lib_RT_Interface_Email_Auth_GnuPG.pm
+++ devel/rt3/patches/patch-lib_RT_Interface_Email_Auth_GnuPG.pm
@@ -1,19 +1,20 @@
-$NetBSD: patch-lib_RT_Interface_Email_Auth_GnuPG.pm,v 1.1 2011/10/25 19:38:10 spz Exp $
+$NetBSD: patch-lib_RT_Interface_Email_Auth_GnuPG.pm,v 1.2 2012/10/31 20:39:26 spz Exp $
 
 perl 5.14 qw() in for* fixes
 
---- lib/RT/Interface/Email/Auth/GnuPG.pm.orig	2011-04-14 00:32:21.000000000 +0000
+--- lib/RT/Interface/Email/Auth/GnuPG.pm.orig	2012-10-25 20:19:10.000000000 +0000
 +++ lib/RT/Interface/Email/Auth/GnuPG.pm
-@@ -76,10 +76,10 @@ sub GetCurrentUser {
+@@ -76,11 +76,11 @@ sub GetCurrentUser {
      );
  
      foreach my $p ( $args{'Message'}->parts_DFS ) {
 -        $p->head->delete($_) for qw(
-+        $p->head->delete($_) for ( qw(
-             X-RT-GnuPG-Status X-RT-Incoming-Encrypton
++        $p->head->delete($_) for (qw(
+             X-RT-GnuPG-Status X-RT-Incoming-Encryption
              X-RT-Incoming-Signature X-RT-Privacy
+             X-RT-Sign X-RT-Encrypt
 -        );
-+        ) );
++        ));
      }
  
      my $msg = $args{'Message'}->dup;
