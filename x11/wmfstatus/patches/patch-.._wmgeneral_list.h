ce3843db3490e7a814ba92b3251daacf063ebb23
joerg <joerg@pkgsrc.org>
2012-11-01 19:26:50 +0000

    Fix C99 inline usage.

---
+++ x11/wmfstatus/patches/patch-.._wmgeneral_list.h
@@ -0,0 +1,17 @@
+$NetBSD: patch-.._wmgeneral_list.h,v 1.1 2012/11/01 19:26:50 joerg Exp $
+
+--- ../wmgeneral/list.h.orig	2012-10-30 21:21:20.000000000 +0000
++++ ../wmgeneral/list.h
+@@ -29,11 +29,7 @@ Boston, MA 02111-1307, USA.  */
+ #ifndef __LIST_H_
+ #define __LIST_H_
+ 
+-#if defined(__GNUC__) && !defined(__STRICT_ANSI__)
+-# define INLINE inline
+-#else
+-# define INLINE
+-#endif
++#define INLINE
+ 
+ typedef struct LinkedList {
+   void *head;
