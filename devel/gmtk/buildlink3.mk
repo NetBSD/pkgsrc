ccb94d57bd1a2e4283866c244f6bc5bfd9663cb6
ryoon <ryoon>
2012-11-03 01:58:32 +0000

    Recursive revbump from audio/pulseaudio 2.1

--- devel/gmtk/buildlink3.mk
+++ devel/gmtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2012/10/08 23:00:45 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2012/11/03 01:58:32 ryoon Exp $
 #
 
 BUILDLINK_TREE+=	gmtk
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	gmtk
 GMTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gmtk+=	gmtk>=1.0.6
-BUILDLINK_ABI_DEPENDS.gmtk?=	gmtk>=1.0.6nb5
+BUILDLINK_ABI_DEPENDS.gmtk?=	gmtk>=1.0.6nb6
 BUILDLINK_PKGSRCDIR.gmtk?=	../../devel/gmtk
 
 pkgbase := gmtk
