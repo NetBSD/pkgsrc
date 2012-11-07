369c290ea913d646d7b3064ffdf6ebbf30018047
adam <adam@pkgsrc.org>
2012-11-07 20:59:50 +0000

    Changes 50.1: * Unicode 6.2: Turkish Lira Sign, improved word & line segmentation (BreakIterator) for symbols * CLDR 22.1: Data coverage & quality improved across all major languages; new short width type for weekday names; new zhuyin (Bopomofo) collation for Chinese; improved data for CompactDecimalFormat & RBNF * Time zone data: 2012h * Ordinal-number support in MessageFormat & PluralRules * Deprecate setLocale(locale) in PluralFormat * Dictionary-based break iterators (word segmentation): * Support Chinese & Japanese, use more compact dictionary format, port all but Khmer support to Java * Update Khmer dictionary * Change Java util.ListFormat to text.ListFormatter and other updates, use CLDR data, port to C++ * Add updated IBM-eucJP and IBM-5233 converter * Improve number formatting performance * C++ GenderInfo: Effective combined gender of a list of people's genders (ported from Java) * Thread safety support cannot be removed (see the Readme) * Default compilers: Clang is now used if available (see the Readme) * C++ Collator API cleanup, subclassing-API-breaking changes (see the Readme) * Add option to genrb tool for writing java resource bundle files * Time zone format APIs

--- textproc/icu/patches/patch-common_unicode_platform.h
+++ textproc/icu/patches/patch-common_unicode_platform.h
@@ -1,18 +1,17 @@
-$NetBSD: patch-common_unicode_platform.h,v 1.2 2012/07/03 18:37:26 adam Exp $
+$NetBSD: patch-common_unicode_platform.h,v 1.3 2012/11/07 20:59:51 adam Exp $
 
---- common/unicode/platform.h.orig	2012-06-01 14:52:38.000000000 +0000
+--- common/unicode/platform.h.orig	2012-11-05 17:14:34.000000000 +0000
 +++ common/unicode/platform.h
-@@ -131,7 +131,8 @@
+@@ -144,7 +144,7 @@
  #   include <android/api-level.h>
  #elif defined(linux) || defined(__linux__) || defined(__linux)
  #   define U_PLATFORM U_PF_LINUX
--#elif defined(BSD) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
-+#elif defined(BSD) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || \
-+	defined(__NetBSD__) || defined(__DragonFly__)
+-#elif defined(BSD) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__MirBSD__)
++#elif defined(BSD) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__MirBSD__) || defined(__DragonFly__)
  #   define U_PLATFORM U_PF_BSD
  #elif defined(sun) || defined(__sun)
      /* Check defined(__SVR4) || defined(__svr4__) to distinguish Solaris from SunOS? */
-@@ -346,6 +347,13 @@
+@@ -381,6 +381,13 @@
  #   define U_IS_BIG_ENDIAN 1
  #elif defined(__LITTLE_ENDIAN__) || defined(_LITTLE_ENDIAN)
  #   define U_IS_BIG_ENDIAN 0
