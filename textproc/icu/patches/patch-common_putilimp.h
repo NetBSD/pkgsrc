62183e98578a89fd2bcdd1c9917fa5395bf55574
adam <adam>
2012-11-07 20:59:50 +0000

    Changes 50.1: * Unicode 6.2: Turkish Lira Sign, improved word & line segmentation (BreakIterator) for symbols * CLDR 22.1: Data coverage & quality improved across all major languages; new short width type for weekday names; new zhuyin (Bopomofo) collation for Chinese; improved data for CompactDecimalFormat & RBNF * Time zone data: 2012h * Ordinal-number support in MessageFormat & PluralRules * Deprecate setLocale(locale) in PluralFormat * Dictionary-based break iterators (word segmentation): * Support Chinese & Japanese, use more compact dictionary format, port all but Khmer support to Java * Update Khmer dictionary * Change Java util.ListFormat to text.ListFormatter and other updates, use CLDR data, port to C++ * Add updated IBM-eucJP and IBM-5233 converter * Improve number formatting performance * C++ GenderInfo: Effective combined gender of a list of people's genders (ported from Java) * Thread safety support cannot be removed (see the Readme) * Default compilers: Clang is now used if available (see the Readme) * C++ Collator API cleanup, subclassing-API-breaking changes (see the Readme) * Add option to genrb tool for writing java resource bundle files * Time zone format APIs

--- textproc/icu/patches/patch-common_putilimp.h
+++ textproc/icu/patches/patch-common_putilimp.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-common_putilimp.h,v 1.1 2012/07/10 19:14:48 bsiegert Exp $
+$NetBSD: patch-common_putilimp.h,v 1.2 2012/11/07 20:59:51 adam Exp $
 
---- common/putilimp.h.orig	Fri Jun 22 08:38:46 2012
+--- common/putilimp.h.orig	2012-11-05 17:14:50.000000000 +0000
 +++ common/putilimp.h
-@@ -119,6 +119,8 @@ typedef size_t uintptr_t;
- #   define U_TIMEZONE _timezone
+@@ -123,6 +123,8 @@ typedef size_t uintptr_t;
+    /* not defined */
  #elif U_PLATFORM == U_PF_OS400
     /* not defined */
 +#elif defined(__MirBSD__)
