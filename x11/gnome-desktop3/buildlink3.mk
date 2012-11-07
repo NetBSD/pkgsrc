c7b89e5642e89cb9be6e6513bf2b27dadebc37c0
joerg <joerg@pkgsrc.org>
2012-11-07 15:38:26 +0000

    Fix build with modular Xorg.

--- x11/gnome-desktop3/buildlink3.mk
+++ x11/gnome-desktop3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2012/10/29 14:29:27 prlw1 Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2012/11/07 15:38:26 joerg Exp $
 
 BUILDLINK_TREE+=	gnome-desktop3
 
@@ -10,6 +10,7 @@ BUILDLINK_ABI_DEPENDS.gnome-desktop3+=	gnome-desktop3>=3.6.1
 BUILDLINK_PKGSRCDIR.gnome-desktop3?=	../../x11/gnome-desktop3
 
 .include "../../x11/gtk3/buildlink3.mk"
+.include "../../x11/libxkbfile/buildlink3.mk"
 .include "../../x11/startup-notification/buildlink3.mk"
 .endif # GNOME_DESKTOP_BUILDLINK3_MK
 
