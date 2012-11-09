adaa764baed4ca8b14c2c8ba3bfe0a6c41ff963f
gdt <gdt@pkgsrc.org>
2012-11-09 15:49:03 +0000

    Only match swig <2.0, for now.
    While swig2 may or may not work for various packages, 1) it installs
    swig2, which isn't API-compatible with calling 'swig' (since it won't
    be found) and 2) it hasn't been validated.

--- devel/swig/buildlink3.mk
+++ devel/swig/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:24:30 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2012/11/09 15:49:03 gdt Exp $
 
 BUILDLINK_TREE+=	swig
 
 .if !defined(SWIG_BUILDLINK3_MK)
 SWIG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.swig+=	swig>=1.3.24
-BUILDLINK_ABI_DEPENDS.swig+=	swig>=1.3.31
+BUILDLINK_API_DEPENDS.swig+=	swig>=1.3.24<2.0
+BUILDLINK_ABI_DEPENDS.swig+=	swig>=1.3.31<2.0
 BUILDLINK_PKGSRCDIR.swig?=	../../devel/swig
 # only contains a static library
 BUILDLINK_DEPMETHOD.swig?=	build
