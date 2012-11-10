724823c21c52ef9ee1f88fc489b0c12bda4ee01e
taca <taca@pkgsrc.org>
2012-11-10 14:34:27 +0000

    Update ruby193-base (and releated packages) to 1.9.3p327 (Ruby 1.9.3 patchlevel 327) to fix DoS security problem.
    http://www.ruby-lang.org/en/news/2012/11/09/ruby19-hashdos-cve-2012-5371/
    
    For other changes please refer Changelog file.

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.91 2012/10/12 15:27:48 taca Exp $
+# $NetBSD: rubyversion.mk,v 1.92 2012/11/10 14:34:27 taca Exp $
 #
 
 # This file determines which Ruby version is used as a dependency for
@@ -206,7 +206,7 @@ RUBY193_VERSION=	1.9.3
 
 # patch
 RUBY18_PATCHLEVEL=	pl371
-RUBY193_PATCHLEVEL=	p286
+RUBY193_PATCHLEVEL=	p327
 
 # current API compatible version; used for version of shared library
 RUBY18_API_VERSION=	1.8.7
