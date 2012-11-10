deee46189fae8809b88934e88ce9056faa4bb7b9
pettai <pettai>
2012-11-10 18:07:44 +0000

    Version 1.12.6 (released 2012-09-04)
    * liboath: The usersfile is now fflush'ed and fsync'ed.
    * liboath: A memory leak fixed.
    * oathtool: The --counter parameter now works on 32-bit platforms.
    * API and ABI is backwards compatible with the previous version.
      OATH_FILE_FLUSH_ERROR: Added.
      OATH_FILE_SYNC_ERROR: Added.
      OATH_FILE_CLOSE_ERROR: Added.
      OATH_LAST_ERROR: Added.
    
    Version 1.12.5 (released 2012-08-19)
    
    * oathtool: The --counter parameter now supports larger values.
      Before it used an 'int' type and now it uses a 'longlong' type.
      Needed for eSecuTech tokens as they use a 64-bit value for their
      initial counter. see <https://savannah.nongnu.org/support/?108114>.
    * Added gnulib self-tests.
    * API and ABI is backwards compatible with the previous version.
    
    Version 1.12.4 (released 2012-06-17)
    
    * liboath: Usersfile code handles multiple lines for a single user.
      This can be used when a single user carries multiple tokens (with
      different OATH secrets) and any of them should be permitted.
    * API and ABI is backwards compatible with the previous version.
    
    Version 1.12.3 (released 2012-05-31)
    
    * pam_oath: Fix "try_first_pass".
    * API and ABI is backwards compatible with the previous version.
    
    Version 1.12.2 (released 2012-04-04)
    
    * liboath: usersfile function now works on FreeBSD.
    * tests: liboath usersfile self-test is skipped if there is no datefudge.
    * API and ABI is backwards compatible with the previous version.
    
    Version 1.12.1 (released 2012-04-01)
    
    * liboath, oathtool: Base32 decoding now permit lowercase characters.
    * API and ABI is backwards compatible with the previous version.
    
    Version 1.12.0 (released 2012-04-01)
    
    * oathtool: Added --base32 parameter to decode base32 keys.
    * oathtool: Verbose output (-v) now print key data in base32 format too.
    * liboath: Added base32 functions.  Added hex encoding function.
      The new APIs are oath_bin2hex, oath_base32_decode, and
      oath_base32_encode.
    * liboath: Gnulib's snprintf is used for better portability.
      The system snprintf is known to have bugs on some systems, see the
      Gnulib manual for more information.
    * API and ABI is backwards compatible with the previous version.
      oath_bin2hex: New function.
      oath_base32_decode: New function.
      oath_base32_encode: New function.
      OATH_INVALID_BASE32: New error code.
      OATH_BASE32_OVERFLOW: New error code.
      OATH_MALLOC_ERROR: New error code.

--- security/oath-toolkit/patches/patch-liboath_gl_fseeko.c
+++ security/oath-toolkit/patches/patch-liboath_gl_fseeko.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-liboath_gl_fseeko.c,v 1.1 2012/04/07 19:34:15 joerg Exp $
+$NetBSD: patch-liboath_gl_fseeko.c,v 1.2 2012/11/10 18:07:44 pettai Exp $
 
---- liboath/gl/fseeko.c.orig	2012-01-03 19:18:22.000000000 +0000
+--- liboath/gl/fseeko.c.orig	2012-08-07 07:13:22.000000000 +0000
 +++ liboath/gl/fseeko.c
-@@ -109,7 +109,7 @@ fseeko (FILE *fp, off_t offset, int when
+@@ -119,7 +119,7 @@ fseeko (FILE *fp, off_t offset, int when
        fp->_flags &= ~_IO_EOF_SEEN;
        fp->_offset = pos;
- #elif defined __sferror || defined __DragonFly__ /* FreeBSD, NetBSD, OpenBSD, DragonFly, MacOS X, Cygwin */
+ #elif defined __sferror || defined __DragonFly__ /* FreeBSD, NetBSD, OpenBSD, DragonFly, Mac OS X, Cygwin */
 -# if defined __CYGWIN__
 +# if defined(__CYGWIN__) || defined(__NetBSD__)
        /* fp_->_offset is typed as an integer.  */
