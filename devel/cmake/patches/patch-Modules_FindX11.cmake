ab1c2f698594cbc50e40535692c206c4904145ce
adam <adam@pkgsrc.org>
2012-11-15 19:31:54 +0000

    With the 2.8.10 release, users now have a new way to arrange exported targets that depend on other targets into "export sets." In addition, there are new target properties for PDB_OUTPUT_DIRECTORY and PDB_NAME implemented for Visual Studio 7 and later.
    Generator expressions, which are used to introduce conditional statements at generate time rather than at CMake compile time, are now available in more contexts, notably in the INCLUDE_DIRECTORIES and COMPILE_DEFINITIONS target properties. There are also new generator expressions available in the 2.8.10 release.
    
    The file(DOWNLOAD command can accommodate https URLs. The pre-built binaries for this are available from Kitware and link to OpenSSL. The team has also added uniform compiler "id" and version number variables available for nearly all known compilers and platforms.

--- devel/cmake/patches/patch-Modules_FindX11.cmake
+++ devel/cmake/patches/patch-Modules_FindX11.cmake
@@ -1,26 +1,26 @@
-$NetBSD: patch-Modules_FindX11.cmake,v 1.4 2012/09/14 13:26:20 wiz Exp $
+$NetBSD: patch-Modules_FindX11.cmake,v 1.5 2012/11/15 19:31:55 adam Exp $
 
 Patch in pkgsrc paths for finding X.
 
---- Modules/FindX11.cmake.orig	2011-10-04 16:09:24.000000000 +0000
+--- Modules/FindX11.cmake.orig	2012-10-31 15:32:05.000000000 +0000
 +++ Modules/FindX11.cmake
-@@ -61,7 +61,8 @@ IF (UNIX)
-   SET(CMAKE_FIND_FRAMEWORK_SAVE ${CMAKE_FIND_FRAMEWORK})
-   SET(CMAKE_FIND_FRAMEWORK NEVER)
-   SET(X11_INC_SEARCH_PATH
+@@ -61,7 +61,8 @@ if (UNIX)
+   set(CMAKE_FIND_FRAMEWORK_SAVE ${CMAKE_FIND_FRAMEWORK})
+   set(CMAKE_FIND_FRAMEWORK NEVER)
+   set(X11_INC_SEARCH_PATH
 -    /usr/pkg/xorg/include
 +    @LOCALBASE@/xorg/include
-+    @X11BASE@/include
++    @X11BASE@/xorg/include
      /usr/X11R6/include
      /usr/X11R7/include
      /usr/include/X11
-@@ -71,7 +72,8 @@ IF (UNIX)
+@@ -71,7 +72,8 @@ if (UNIX)
    )
  
-   SET(X11_LIB_SEARCH_PATH
+   set(X11_LIB_SEARCH_PATH
 -    /usr/pkg/xorg/lib
 +    @LOCALBASE@/xorg/lib
-+    @X11BASE@/lib
++    @X11BASE@/xorg/lib
      /usr/X11R6/lib
      /usr/X11R7/lib
      /usr/openwin/lib
--- devel/cmake/patches/patch-Modules_Platform_SunOS.cmake
+++ devel/cmake/patches/patch-Modules_Platform_SunOS.cmake
@@ -1,34 +1,34 @@
-$NetBSD: patch-Modules_Platform_SunOS.cmake,v 1.2 2012/08/11 11:10:54 marino Exp $
+$NetBSD: patch-Modules_Platform_SunOS.cmake,v 1.3 2012/11/15 19:31:55 adam Exp $
 
 Don't use gcc to link c++ libraries on SunOS,
 unless we are really using a gcc lacking libstdc++.
 
 See http://public.kitware.com/pipermail/cmake/2011-July/045300.html
 
---- Modules/Platform/SunOS.cmake.orig	2012-08-09 18:15:19.000000000 +0000
+--- Modules/Platform/SunOS.cmake.orig	2012-11-14 19:47:33.000000000 +0000
 +++ Modules/Platform/SunOS.cmake
-@@ -7,12 +7,20 @@ IF(CMAKE_SYSTEM MATCHES "SunOS-4.*")
-    SET(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG_SEP ":")  
- ENDIF(CMAKE_SYSTEM MATCHES "SunOS-4.*")
+@@ -7,12 +7,20 @@ if(CMAKE_SYSTEM MATCHES "SunOS-4.*")
+    set(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG_SEP ":")
+ endif()
  
 +# Take the default c++ shared library creation rule from the
 +# CMakeDefaultMakeRuleVariables.cmake file unless using GCC and libstdc++.so
 +# does not exist, in which case fall back to the old implementation;
 +# using gcc to invoke the linker.
- IF(CMAKE_COMPILER_IS_GNUCXX)
-   IF(CMAKE_COMPILER_IS_GNUCC)
--    SET(CMAKE_CXX_CREATE_SHARED_LIBRARY
+ if(CMAKE_COMPILER_IS_GNUCXX)
+   if(CMAKE_COMPILER_IS_GNUCC)
+-    set(CMAKE_CXX_CREATE_SHARED_LIBRARY
 -        "<CMAKE_C_COMPILER> <CMAKE_SHARED_LIBRARY_CXX_FLAGS> <LINK_FLAGS> <CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS>  <SONAME_FLAG><TARGET_SONAME> -o <TARGET> <OBJECTS> <LINK_LIBRARIES>")
--  ELSE(CMAKE_COMPILER_IS_GNUCC)
+-  else()
 -    # Take default rule from CMakeDefaultMakeRuleVariables.cmake.
-+    EXECUTE_PROCESS(
++    execute_process(
 +      COMMAND ${CMAKE_CXX_COMPILER} -print-file-name=libstdc++.so
 +      OUTPUT_VARIABLE SHARED_LIBSTDCXX_FILENAME
 +      OUTPUT_STRIP_TRAILING_WHITESPACE)
-+    IF(NOT EXISTS "${SHARED_LIBSTDCXX_FILENAME}")
-+      SET(CMAKE_CXX_CREATE_SHARED_LIBRARY
++    if(NOT EXISTS "${SHARED_LIBSTDCXX_FILENAME}")
++      set(CMAKE_CXX_CREATE_SHARED_LIBRARY
 +          "<CMAKE_C_COMPILER> <CMAKE_SHARED_LIBRARY_CXX_FLAGS> <LINK_FLAGS> <CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS>  <CMAKE_SHARED_LIBRARY_SONAME_CXX_FLAG><TARGET_SONAME> -o <TARGET> <OBJECTS> <LINK_LIBRARIES>")
-+    ENDIF(NOT EXISTS "${SHARED_LIBSTDCXX_FILENAME}")
-   ENDIF(CMAKE_COMPILER_IS_GNUCC)
- ENDIF(CMAKE_COMPILER_IS_GNUCXX)
- INCLUDE(Platform/UnixPaths)
++    endif()
+   endif()
+ endif()
+ include(Platform/UnixPaths)
