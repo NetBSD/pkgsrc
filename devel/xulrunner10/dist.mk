d80078c6bf1e28df3e014fa2a88cc6517edb42ef
is <is@pkgsrc.org>
2012-11-15 09:48:30 +0000

    Update to 10.0.10. Fixes MFSA 2012-90 (Fixes for Location object issues)

--- devel/xulrunner10/dist.mk
+++ devel/xulrunner10/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.8 2012/10/13 10:16:23 ryoon Exp $
+# $NetBSD: dist.mk,v 1.9 2012/11/15 09:48:30 is Exp $
 #
 # used by devel/xulrunner10/Makefile
 # used by www/firefox10/Makefile
@@ -6,7 +6,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}esr.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	10.0
-MOZ_BRANCH_MINOR=	.9
+MOZ_BRANCH_MINOR=	.10
 MASTER_SITES=	${MASTER_SITE_MOZILLA_ESR:=firefox/releases/${FIREFOX_VER}esr/source/}
 EXTRACT_SUFX=	.tar.bz2
 
