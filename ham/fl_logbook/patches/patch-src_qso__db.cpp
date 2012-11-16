dd05acf1879afde1a37f86215e58605efea023c6
gdt <gdt@pkgsrc.org>
2012-11-16 00:33:36 +0000

    Update to 2.5.  Upstream Changelog is:
      2.3   1) Added the ability to do a partial search on callsign.  This allows
    	   you to search for a prefix<area> or on a full callsign as in
    	   prefix<area><suffix>
    
    pkgsrc changes:
    
      Set LICENSE.
      Adapt to more modern upstream.
      Change to fltk13.
      Rototill upstream's makefile to use CXX to compile C++.

--- ham/fl_logbook/patches/patch-src_qso__db.cpp
+++ ham/fl_logbook/patches/patch-src_qso__db.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_qso__db.cpp,v 1.1 2011/11/25 21:59:57 joerg Exp $
+$NetBSD: patch-src_qso__db.cpp,v 1.2 2012/11/16 00:33:36 gdt Exp $
 
---- src/qso_db.cpp.orig	2011-11-25 17:52:09.000000000 +0000
+--- src/qso_db.cpp.orig	2007-02-16 20:19:59.000000000 +0000
 +++ src/qso_db.cpp
 @@ -1,3 +1,4 @@
 +#include <cstring>
