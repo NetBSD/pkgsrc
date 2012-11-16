a8b1f42f5a13e5a5f03a18c6e1485816e26f8b5c
gdt <gdt>
2012-11-16 00:33:36 +0000

    Update to 2.5.  Upstream Changelog is:
      2.3   1) Added the ability to do a partial search on callsign.  This allows
    	   you to search for a prefix<area> or on a full callsign as in
    	   prefix<area><suffix>
    
    pkgsrc changes:
    
      Set LICENSE.
      Adapt to more modern upstream.
      Change to fltk13.
      Rototill upstream's makefile to use CXX to compile C++.

--- ham/fl_logbook/patches/patch-src_date.h
+++ ham/fl_logbook/patches/patch-src_date.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_date.h,v 1.1 2011/11/25 21:59:57 joerg Exp $
+$NetBSD: patch-src_date.h,v 1.2 2012/11/16 00:33:36 gdt Exp $
 
---- src/date.h.orig	2011-11-25 17:49:57.000000000 +0000
+--- src/date.h.orig	2006-09-11 15:40:15.000000000 +0000
 +++ src/date.h
 @@ -25,13 +25,15 @@
  #ifndef DATE_H
