882d35c8d7700e8e0c4eb44696d309add9f6146d
joerg <joerg@pkgsrc.org>
2012-11-19 03:01:44 +0000

    Fix build with new libxml2 API.

---
+++ textproc/gdome2/patches/patch-libgdome_gdomecore_gdome-xml-documentt.c
@@ -0,0 +1,13 @@
+$NetBSD: patch-libgdome_gdomecore_gdome-xml-documentt.c,v 1.1 2012/11/19 03:01:44 joerg Exp $
+
+--- libgdome/gdomecore/gdome-xml-documentt.c.orig	2012-11-19 01:08:40.000000000 +0000
++++ libgdome/gdomecore/gdome-xml-documentt.c
+@@ -342,7 +342,7 @@ gdome_xml_dt_internalSubset (GdomeDocume
+ 	}
+ 	xmlNodeDumpOutput (out_buff, NULL, (xmlNode *)is, 0, 0, NULL);
+ 	xmlOutputBufferFlush(out_buff);
+-	ret = g_strndup (out_buff->buffer->content, out_buff->buffer->use);
++	ret = g_strndup (xmlBufferContent(out_buff->buffer), xmlBufferLength(out_buff->buffer));
+ 	(void)xmlOutputBufferClose(out_buff);
+ 
+ 	return gdome_xml_str_mkref_own (ret);
