c2841ad2de528376b3c2f81b69bfabd898ec9e98
pettai <pettai@pkgsrc.org>
2012-11-19 11:40:27 +0000

    Version 1.9.0 (released 2012-11-08)
    * ykinfo: New tool to print information about YubiKey.
    * ykpersonalize: Add -z flag to zap configuration on YubiKey.
    * Fix PBKDF2 implementation.

--- security/ykpers/buildlink3.mk
+++ security/ykpers/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/13 15:30:07 pettai Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/11/19 11:40:27 pettai Exp $
 
 BUILDLINK_TREE+=	ykpers
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.ykpers+=	ykpers>=1.6.3
 BUILDLINK_PKGSRCDIR.ykpers?=	../../security/ykpers
 
 .include "../../security/libyubikey/buildlink3.mk"
-.include "../../devel/libusb/buildlink3.mk"
+.include "../../devel/libusb1/buildlink3.mk"
 .endif	# YKPERS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ykpers
