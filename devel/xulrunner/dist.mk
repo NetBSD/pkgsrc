22594712891300df850d41cb697f89f309de3aea
ryoon <ryoon@pkgsrc.org>
2012-11-21 21:46:30 +0000

    Use MASTER_SITE_MOZILLA_ESR instead of MASTER_SITE_MOZILLA in MASTER_SITES.

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.37 2012/11/21 15:26:49 ryoon Exp $
+# $NetBSD: dist.mk,v 1.38 2012/11/21 21:46:30 ryoon Exp $
 #
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
@@ -7,7 +7,7 @@ DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	17.0
 MOZ_BRANCH_MINOR=	esr
-MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/} \
+MASTER_SITES=	${MASTER_SITE_MOZILLA_ESR:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
