48c514097c04f94903fd533397c9c555609736c6
jnemeth <jnemeth@pkgsrc.org>
2012-11-22 10:01:09 +0000

    Upgrade to version 5.2.2:
    5.2.2 (10-Sep-2012)
    
      Bugs fixed:
      * Server disconnects on the execution of a query following execution of a SP
        returning resultset(s) and OUT parameters. (Bug# 14512187)
      * Crash while executing after binding BLOB datatype as INOUT parameter
        (Bug# 14501952)
      * SP having 2 BLOB OUT parameters crashes at execution (Bug# 14563386)
      * ASSERT for INOUT parameter of BIT(N>3) type (Bug#14560916)
      * Using ADO, driver returns wrong type and value for bit(>1) parameter
        (Bug# 14555713)
      * Blobs used in as INOUT parameters have inconsistent behaviour in ADO
        (Bug# 14580316)
      * my_isspace calls cause ODBC driver crashes (Bug# 14363601)
      * SQLNativeSql did not null-terminate output string after copying
        (Bug# 14559721)
      * Incorrect length value passed to SQLSetConnectAttr could cause
        driver crash. (Bug# 14620420)
    
      Built using MySQL 5.5.27.
    
    ----
    
    5.2.1 (09-Aug-2012)
    
      Functionality added or changed:
      * Driver prepares statements on server
      * Support of INOUT and OUT parameters(WL#6116)
      * Increased the timeout in tests from 30 to 60 seconds to avoid failures in
        slow tests that involve catalog functions and Information_Schema queries.
      Bugs fixed:
      * Long table names crash OBDC driver. (Bug# 14085211/65200)
      * Close proximity issue when handling OS signlals in myodbc (Bug# 14303803)
      * "String data right truncated" error is not always correct (Bug# 14285620)
    
      Built using MySQL 5.5.27.
    
    ----
    
    5.2.0
    
      Functionality added or changed:
      * Driver available in Unicode and ANSI versions(WL#5850)
      * Driver by default uses server side prepared statements
      * Connection string option NO_SSPS prevents using of server side prepared
        statements. It is configurable via GUI dialog.
    
      Built using MySQL 5.5.25.
    
    ----
    
    5.1.11
    
      Functionality added or changed:
      * MyODBC queries log is written to %TEMP%\myodbc.sql, or to C:\myodbc.sql if
        TEMP is not defined.
      * Added pre-fetch functionality implemented through LIMIT to avoid reading the
        entire resultset for full table SELECT queries. (WL#6211)
    
      Bugs fixed:
      * Fractional parts of second is ignored in SQLGetData. (Bug #12767740/60646)
      * ODBC prepared statements ignore fractional part of temporal data types.
        (Bug #12767761/60648)
      * Memory leak on Windows on each connection attepmt. (Bug #11766029/59059)
      * SQLTables() function does not return the catalog correctly if the wildcard
        or SQL_ALL_CATALOGS is used. (Bug #13914518)
    
    ----
    
    5.1.10(02-Feb-2012)
    
      Functionality added or changed:
    
      Bugs fixed:
      * In some cases TIMESTAMP field could be described as SQL_NO_NULLS.
        (Bug #13532987)
      * SQLFetch has to return error if indicator pointer is NULL for NULL value.
        (Bug #13542600)
      * A failure on one stmt causes another stmt to fail. (Bug #13097201/#62657)
    
      Built using MySQL 5.5.18.
    
    ----
    
    5.1.9 (04-Oct-2011)
    
      Functionality added or changed:
      * Support of windows authentication.
    
      Bugs fixed:
      * SQLFetch() did not return SQL_ERROR if connection was dropped due to a
        timeout. (Bug #39878)
      * MS Access with VARCHAR NOT NULL columns. (Bug #31067)
      * sqlwcharchr might read one SQLWCHAR after end of string. (Bug #61586)
      * Column parameter binding makes SQLExecute not to return SQL_ERROR on
        disconnect. (Bug #59772)
      * If pre-execution failed some catalog functions called right after that
        would return only one row. (Bug #12824839)
      * MyODBC driver does not call mysql_thread_end() when the thread ends causing
        error messages such as this: Error in my_thread_global_end(): 1 threads
        didn't exit. (Bug #57727)
      * When NO_BINARY_RESULT was set driver failed to perform the expected data
        conversion. (Bug#11765110/58038)
    
      Built using MySQL 5.5.16.

--- databases/myodbc/options.mk
+++ databases/myodbc/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2011/08/19 07:59:58 adam Exp $
+# $NetBSD: options.mk,v 1.6 2012/11/22 10:01:10 jnemeth Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.myodbc
 PKG_OPTIONS_REQUIRED_GROUPS=	db
@@ -12,21 +12,18 @@ PLIST_VARS+=		gui
 
 .if !empty(PKG_OPTIONS:Mgui)
 .  include "../../x11/qt3-tools/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-gui
-USE_LANGUAGES=		c c++
+#CONFIGURE_ARGS+=	--enable-gui
+#USE_LANGUAGES=		c c++
 PLIST.gui=		yes
 .else
-CONFIGURE_ARGS+=	--disable-gui
+#CONFIGURE_ARGS+=	--disable-gui
 .endif
 
 .if !empty(PKG_OPTIONS:Miodbc)
 .  include "../../databases/iodbc/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-iODBC=${BUILDLINK_PREFIX.iodbc}
-CONFIGURE_ARGS+=	--with-odbc-ini=${PKG_SYSCONFDIR.iodbc}/odbc.ini
 .endif
 
 .if !empty(PKG_OPTIONS:Munixodbc)
 .  include "../../databases/unixodbc/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-unixODBC=${BUILDLINK_PREFIX.unixodbc}
-CONFIGURE_ARGS+=	--with-odbc-ini=${PKG_SYSCONFDIR.unixodbc}/odbc.ini
+CMAKE_ARGS+=		-DWITH_UNIXODBC=1
 .endif
