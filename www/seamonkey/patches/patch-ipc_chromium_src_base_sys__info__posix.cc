501c243b6ef50e4eb5d1bb5653ec225fb05dd3fb
ryoon <ryoon>
2012-11-23 17:28:48 +0000

    Update to 2.14
    * Patches are synced with xulrunner-17.0, and regen patches
    * Update Mozilla Lightning to 1.9
    
    Changelog:
    SeaMonkey-specific changes
        None (see changes page for minor changes).
    
    Mozilla platform changes
        OS X 10.6 is now the minimum supported Mac version.
        JavaScript Maps and Sets are now iterable.
        SVG FillPaint and StrokePaint have been implemented.
        The sandbox attribute has been implemented for iframes, enabling increased security.
        Fixed several stability issues.
    
    Security fixes
    Fixed in SeaMonkey 2.14
    MFSA 2012-106 Use-after-free, buffer overflow, and memory corruption issues found using Address Sanitizer
    MFSA 2012-105 Use-after-free and buffer overflow issues found using Address Sanitizer
    MFSA 2012-103 Frames can shadow top.location
    MFSA 2012-101 Improper character decoding in HZ-GB-2312 charset
    MFSA 2012-100 Improper security filtering for cross-origin wrappers
    MFSA 2012-99 XrayWrappers exposes chrome-only properties when not in chrome compartment
    MFSA 2012-97 XMLHttpRequest inherits incorrect principal within sandbox
    MFSA 2012-96 Memory corruption in str_unescape
    MFSA 2012-94 Crash when combining SVG text on path with CSS
    MFSA 2012-93 evalInSanbox location context incorrectly applied
    MFSA 2012-92 Buffer overflow while rendering GIF images
    MFSA 2012-91 Miscellaneous memory safety hazards (rv:17.0/ rv:10.0.11)

--- www/seamonkey/patches/patch-ipc_chromium_src_base_sys__info__posix.cc
+++ www/seamonkey/patches/patch-ipc_chromium_src_base_sys__info__posix.cc
@@ -1,6 +1,6 @@
-$NetBSD: patch-ipc_chromium_src_base_sys__info__posix.cc,v 1.6 2012/09/06 12:08:51 ryoon Exp $
+$NetBSD: patch-ipc_chromium_src_base_sys__info__posix.cc,v 1.7 2012/11/23 17:28:49 ryoon Exp $
 
---- mozilla/ipc/chromium/src/base/sys_info_posix.cc.orig	2012-08-27 04:49:15.000000000 +0000
+--- mozilla/ipc/chromium/src/base/sys_info_posix.cc.orig	2012-11-18 10:19:38.000000000 +0000
 +++ mozilla/ipc/chromium/src/base/sys_info_posix.cc
 @@ -18,6 +18,11 @@
  #include <mach/mach_init.h>
