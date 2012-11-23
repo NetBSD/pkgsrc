c1ccd42b080febe45750e251ce374fda39923dc0
drochner <drochner@pkgsrc.org>
2012-11-23 11:51:14 +0000

    add grilo+plugins-0.2.3, a framework to browse/access various media sources tested UPnP only

---
+++ net/grilo/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/11/23 11:51:14 drochner Exp $
+
+BUILDLINK_TREE+=	grilo
+
+.if !defined(GRILO_BUILDLINK3_MK)
+GRILO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.grilo+=	grilo>=0.2.2
+BUILDLINK_PKGSRCDIR.grilo?=	../../net/grilo
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../net/libsoup24/buildlink3.mk"
+.endif	# GRILO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-grilo
