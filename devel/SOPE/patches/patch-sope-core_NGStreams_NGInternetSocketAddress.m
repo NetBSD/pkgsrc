bcdb83eb9bf1c81874cd184564c53e6b51dc239b
manu <manu@pkgsrc.org>
2012-11-24 14:06:43 +0000

    Update SOGo (and its SOPE dependency) to 2.0.2a
    ChangeLog since 2.0.0
    
    2.0.2a (2012-11-15)
    -------------------
    
    Enhancements
     - improved user rights editor in calendar module
     - disable alarms for newly subsribed calendars
    
    Bug fixes
     - fixed typos in Spanish (Spain) translation
     - fixed display of raw source for tasks
     - fixed title display of cards with a photo
     - fixed null address in reply-to header of messages
     - fixed scrolling for calendar/addressbooks lists
     - fixed display of invitations on BlackBerry devices
     - fixed sogo-tool rename-user for MySQL database
     - fixed corrupted attachments in Webmail
     - fixed parsing of URLs that can throw an exception
     - fixed password encoding in user sources
    
    2.0.2 (2012-10-24)
    ------------------
    
    New features
     - added support for SMTP AUTH
     - sogo configuration can now be set in /etc/sogo/sogo.conf
     - added support for GNU TLS
    
    Enhancements
     - speed up of the parsing of IMAP traffic
     - minor speed up of the web interface
     - speed up the scrolling of the message list in the mail module
     - speed up the deletion of a large amounts of entries in the contacts module
     - updated the timezone files to the 2012.g edition
     - openchange backend: miscellaneous speed up of the synchronization
       operations
     - open file descriptors are now closed when the process starts
    
    Bug fixes
     - the parameters included in the url of remote calendars are now taken into
       account
     - fixed an issue occurring with timezone definitions providing multiple entries
     - openchange backend: miscellaneous crashes during certain Outlook
       operations, which have appeared in version 2.0.0, have been fixed
     - fixed issues occuring on OpenBSD and potentially other BSD flavours
    
    2.0.1 (2012-10-10)
    -------------------
    
    Enhancements
     - deletion of contacts is now performed in batch, which speeds up the
       operation for large numbers of items
     - scalability enhancements in the OpenChange backend that enables the first
       synchronization of mailboxes in a more reasonable time and using less
       memory
     - the task list is now sortable
    
    Bug Fixes
     - improved support of IE 9

--- devel/SOPE/patches/patch-sope-core_NGStreams_NGInternetSocketAddress.m
+++ devel/SOPE/patches/patch-sope-core_NGStreams_NGInternetSocketAddress.m
@@ -1,13 +1,15 @@
-$NetBSD: patch-sope-core_NGStreams_NGInternetSocketAddress.m,v 1.1 2012/09/01 10:02:34 marino Exp $
+$NetBSD: patch-sope-core_NGStreams_NGInternetSocketAddress.m,v 1.2 2012/11/24 14:06:43 manu Exp $
 
---- sope-core/NGStreams/NGInternetSocketAddress.m.orig	2011-11-04 12:39:19.000000000 +0000
-+++ sope-core/NGStreams/NGInternetSocketAddress.m
-@@ -48,7 +48,7 @@
+--- sope-core/NGStreams/NGInternetSocketAddress.m.orig	2012-11-15 18:51:03.000000000 +0100
++++ sope-core/NGStreams/NGInternetSocketAddress.m	2012-11-22 18:39:01.000000000 +0100
+@@ -47,9 +47,9 @@
+ #include "NGInternetSocketAddress.h"
  #include "NGInternetSocketDomain.h"
  #include "common.h"
  
--#if defined(HAVE_GETHOSTBYNAME_R) && !defined(linux) && !defined(__FreeBSD__)
-+#if defined(HAVE_GETHOSTBYNAME_R) && !defined(linux) && !defined(__FreeBSD__) && !defined(__DragonFly__)
+-#if defined(HAVE_GETHOSTBYNAME_R) && !defined(linux) && !defined(__FreeBSD__) && !defined(__GLIBC__)
++#if defined(HAVE_GETHOSTBYNAME_R) && !defined(linux) && !defined(__FreeBSD__) && !defined(__GLIBC__) && !defined(__DragonFly__)
  #define USE_GETHOSTBYNAME_R 1
  #endif
  
+ @implementation NGInternetSocketAddress
