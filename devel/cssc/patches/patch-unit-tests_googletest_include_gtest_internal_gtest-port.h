142e8974de91ad5193d1d01f119a0deeed238541
marino <marino>
2012-11-24 08:43:18 +0000

    devel/cssc: Replace corrupt patch
    I am not sure how this new patch was overwritten with the distinfo.
    Replace it with the intended contents.

--- devel/cssc/patches/patch-unit-tests_googletest_include_gtest_internal_gtest-port.h
+++ devel/cssc/patches/patch-unit-tests_googletest_include_gtest_internal_gtest-port.h
@@ -1,8 +1,12 @@
-$NetBSD: patch-unit-tests_googletest_include_gtest_internal_gtest-port.h,v 1.1 2012/11/23 23:03:12 marino Exp $
+$NetBSD: patch-unit-tests_googletest_include_gtest_internal_gtest-port.h,v 1.2 2012/11/24 08:43:18 marino Exp $
 
-SHA1 (CSSC-1.3.0.tar.gz) = 5f9d7338c58efd9fed7b774843045433b61aed12
-RMD160 (CSSC-1.3.0.tar.gz) = 4318632f0c243ee59639ea9226e0a3d2409e9763
-Size (CSSC-1.3.0.tar.gz) = 1608481 bytes
-SHA1 (patch-aa) = 3f29e0b6835b85129088e36e3fd415a9535772df
-SHA1 (patch-ad) = aecbc9bc595bb48c090ce3bad87631400ce0a34b
-SHA1 (patch-unit-tests_googletest_include_gtest_internal_gtest-port.h) = f9f88c7c6540206d69b9b65b11818f15d2b9827a
+--- unit-tests/googletest/include/gtest/internal/gtest-port.h.orig	2010-05-15 22:30:14.000000000 +0000
++++ unit-tests/googletest/include/gtest/internal/gtest-port.h
+@@ -173,6 +173,7 @@
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <string.h>
++#include <unistd.h>
+ #ifndef _WIN32_WCE
+ #include <sys/stat.h>
+ #endif  // !_WIN32_WCE
