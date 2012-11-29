3db35ee949743c7f8b005dc00fc2513ab82e8506
adam <adam@pkgsrc.org>
2012-11-29 07:31:02 +0000

    patch-gssftp_ftp_ftp_var.h rewritten

--- security/mit-krb5-appl/patches/patch-gssftp_ftp_ftp_var.h
+++ security/mit-krb5-appl/patches/patch-gssftp_ftp_ftp_var.h
@@ -1,16 +1,38 @@
-$NetBSD: patch-gssftp_ftp_ftp_var.h,v 1.1 2012/02/26 13:16:32 adam Exp $
+$NetBSD: patch-gssftp_ftp_ftp_var.h,v 1.2 2012/11/29 07:31:02 adam Exp $
 
 Don't redefine sig_t and sigtype.
 
 --- gssftp/ftp/ftp_var.h.orig	2012-02-26 13:04:30.000000000 +0000
 +++ gssftp/ftp/ftp_var.h
-@@ -57,9 +57,7 @@ FILE* fdopen_socket(SOCKET s, char* mode
- typedef void (*sig_t)(int);
- typedef void sigtype;
- #else
--#define sig_t my_sig_t
- #define sigtype krb5_sigtype
--typedef sigtype (*sig_t)();
+@@ -53,15 +53,6 @@
+ #define PERROR_SOCKET(str) perror(str)
  #endif
  
+-#ifdef _WIN32
+-typedef void (*sig_t)(int);
+-typedef void sigtype;
+-#else
+-#define sig_t my_sig_t
+-#define sigtype krb5_sigtype
+-typedef sigtype (*sig_t)();
+-#endif
+-
  /*
+  * FTP global variables.
+  */
+@@ -168,6 +159,15 @@
+ extern struct macel macros[16];
+ extern char macbuf[4096];
+ 
++#ifdef _WIN32
++typedef void (*sig_t)(int);
++typedef void sigtype;
++#else
++#define sig_t my_sig_t
++#define sigtype krb5_sigtype
++typedef sigtype (*sig_t)();
++#endif
++
+ #ifdef DEFINITIONS
+ #undef extern
+ #endif
