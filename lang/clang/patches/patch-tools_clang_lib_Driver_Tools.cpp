9b5ae05fa20549de0f173a36d854273db612fd9a
marino <marino@pkgsrc.org>
2012-11-29 11:42:24 +0000

    lang/clang: Improve DragonFly support
    1) Don't pass both gcc 4.4 and gcc 4.7 paths to the driver.  Detect
       if gcc47 is available and use those paths, otherwise fall back to
       gcc44.
    2) Add support for exception handling
    3) Add rdynamic support
    4) Add gnu-hash style support
    5) Fix (!!) crtstuff (This was obsolete, include PIE support)
    6) Remove rpath-link
    7) Remove unneeded duplicate libgcc handling
    8) Make libgcc handling match gcc specs (different for gcc 4.4 and 4.7)
    9) Update dragonfly driver test

--- lang/clang/patches/patch-tools_clang_lib_Driver_Tools.cpp
+++ lang/clang/patches/patch-tools_clang_lib_Driver_Tools.cpp
@@ -1,33 +1,176 @@
-$NetBSD: patch-tools_clang_lib_Driver_Tools.cpp,v 1.1 2012/11/22 22:45:15 marino Exp $
+$NetBSD: patch-tools_clang_lib_Driver_Tools.cpp,v 1.2 2012/11/29 11:42:25 marino Exp $
 
-DragonFly no longer has gcc 4.1 in base, so clang stopped working.
-We prefer to use gcc 4.7 if available due to a better libstdc++.
-The fallback is gcc 4.4 which has been available for several years.
+* DragonFly no longer has gcc 4.1 in base, so clang stopped working.
+  We prefer to use gcc 4.7 if available due to a better libstdc++.
+  The fallback is gcc 4.4 which has been available for several years.
+  The libgcc specs are different between 4.4 and 4.7
+* DragonFly: Removed duplicate libgcc handling
+* DragonFly: Removed redundant rpath-link
+* DragonFly: Added -export-dynamic (matches gcc dumpspecs)
+* DragonFly: Added gnu-hash to shared objects
 
 --- tools/clang/lib/Driver/Tools.cpp.orig	2012-04-18 21:32:25.000000000 +0000
 +++ tools/clang/lib/Driver/Tools.cpp
-@@ -5499,14 +5499,21 @@ void dragonfly::Link::ConstructJob(Compi
+@@ -5441,21 +5441,28 @@ void dragonfly::Link::ConstructJob(Compi
+                                    const InputInfoList &Inputs,
+                                    const ArgList &Args,
+                                    const char *LinkingOutput) const {
++  bool UseGCC47;
+   const Driver &D = getToolChain().getDriver();
+   ArgStringList CmdArgs;
+ 
++  llvm::sys::fs::exists("/usr/lib/gcc47", UseGCC47);
++
+   if (!D.SysRoot.empty())
+     CmdArgs.push_back(Args.MakeArgString("--sysroot=" + D.SysRoot));
+ 
++  CmdArgs.push_back("--eh-frame-hdr");
+   if (Args.hasArg(options::OPT_static)) {
+     CmdArgs.push_back("-Bstatic");
+   } else {
++    if (Args.hasArg(options::OPT_rdynamic))
++      CmdArgs.push_back("-export-dynamic");
+     if (Args.hasArg(options::OPT_shared))
+       CmdArgs.push_back("-Bshareable");
+     else {
+       CmdArgs.push_back("-dynamic-linker");
+       CmdArgs.push_back("/usr/libexec/ld-elf.so.2");
+     }
++    CmdArgs.push_back("--hash-style=both");
+   }
+ 
+   // When building 32-bit code on DragonFly/pc64, we have to explicitly
+@@ -5475,18 +5482,26 @@ void dragonfly::Link::ConstructJob(Compi
+   if (!Args.hasArg(options::OPT_nostdlib) &&
+       !Args.hasArg(options::OPT_nostartfiles)) {
+     if (!Args.hasArg(options::OPT_shared)) {
+-      CmdArgs.push_back(
+-            Args.MakeArgString(getToolChain().GetFilePath("crt1.o")));
+-      CmdArgs.push_back(
+-            Args.MakeArgString(getToolChain().GetFilePath("crti.o")));
+-      CmdArgs.push_back(
+-            Args.MakeArgString(getToolChain().GetFilePath("crtbegin.o")));
+-    } else {
+-      CmdArgs.push_back(
+-            Args.MakeArgString(getToolChain().GetFilePath("crti.o")));
+-      CmdArgs.push_back(
+-            Args.MakeArgString(getToolChain().GetFilePath("crtbeginS.o")));
++      if (Args.hasArg(options::OPT_pg))
++        CmdArgs.push_back(Args.MakeArgString(
++                                getToolChain().GetFilePath("gcrt1.o")));
++      else {
++        if (Args.hasArg(options::OPT_pie))
++          CmdArgs.push_back(Args.MakeArgString(
++                                  getToolChain().GetFilePath("Scrt1.o")));
++        else
++          CmdArgs.push_back(Args.MakeArgString(
++                                  getToolChain().GetFilePath("crt1.o")));
++      }
+     }
++    CmdArgs.push_back(Args.MakeArgString(
++                            getToolChain().GetFilePath("crti.o")));
++    if (Args.hasArg(options::OPT_shared) || Args.hasArg(options::OPT_pie))
++      CmdArgs.push_back(Args.MakeArgString(
++                              getToolChain().GetFilePath("crtbeginS.o")));
++    else    
++      CmdArgs.push_back(Args.MakeArgString(
++                              getToolChain().GetFilePath("crtbegin.o")));
+   }
+ 
+   Args.AddAllArgs(CmdArgs, options::OPT_L);
+@@ -5499,20 +5514,19 @@ void dragonfly::Link::ConstructJob(Compi
        !Args.hasArg(options::OPT_nodefaultlibs)) {
      // FIXME: GCC passes on -lgcc, -lgcc_pic and a whole lot of
      //         rpaths
 -    CmdArgs.push_back("-L/usr/lib/gcc41");
-+    CmdArgs.push_back("-L/usr/lib/gcc47");
-+    CmdArgs.push_back("-L/usr/lib/gcc44");
++    if (UseGCC47)
++      CmdArgs.push_back("-L/usr/lib/gcc47");
++    else
++      CmdArgs.push_back("-L/usr/lib/gcc44");
  
      if (!Args.hasArg(options::OPT_static)) {
-       CmdArgs.push_back("-rpath");
+-      CmdArgs.push_back("-rpath");
 -      CmdArgs.push_back("/usr/lib/gcc41");
-+      CmdArgs.push_back("/usr/lib/gcc47");
- 
-       CmdArgs.push_back("-rpath-link");
+-
+-      CmdArgs.push_back("-rpath-link");
 -      CmdArgs.push_back("/usr/lib/gcc41");
-+      CmdArgs.push_back("/usr/lib/gcc47");
-+
-+      CmdArgs.push_back("-rpath");
-+      CmdArgs.push_back("/usr/lib/gcc44");
-+
-+      CmdArgs.push_back("-rpath-link");
-+      CmdArgs.push_back("/usr/lib/gcc44");
+-
+-      CmdArgs.push_back("-rpath");
+-      CmdArgs.push_back("/usr/lib");
+-
+-      CmdArgs.push_back("-rpath-link");
+-      CmdArgs.push_back("/usr/lib");
++      if (UseGCC47) {
++        CmdArgs.push_back("-rpath");
++        CmdArgs.push_back("/usr/lib/gcc47");
++      } else {
++        CmdArgs.push_back("-rpath");
++        CmdArgs.push_back("/usr/lib/gcc44");
++      }
+     }
  
-       CmdArgs.push_back("-rpath");
-       CmdArgs.push_back("/usr/lib");
+     if (D.CCCIsCXX) {
+@@ -5520,13 +5534,6 @@ void dragonfly::Link::ConstructJob(Compi
+       CmdArgs.push_back("-lm");
+     }
+ 
+-    if (Args.hasArg(options::OPT_shared)) {
+-      CmdArgs.push_back("-lgcc_pic");
+-    } else {
+-      CmdArgs.push_back("-lgcc");
+-    }
+-
+-
+     if (Args.hasArg(options::OPT_pthread))
+       CmdArgs.push_back("-lpthread");
+ 
+@@ -5534,23 +5541,42 @@ void dragonfly::Link::ConstructJob(Compi
+       CmdArgs.push_back("-lc");
+     }
+ 
+-    if (Args.hasArg(options::OPT_shared)) {
+-      CmdArgs.push_back("-lgcc_pic");
++    if (UseGCC47) {
++      if (Args.hasArg(options::OPT_static) ||
++          Args.hasArg(options::OPT_static_libgcc)) {
++        CmdArgs.push_back("-lgcc");
++        CmdArgs.push_back("-lgcc_eh");
++      } else {
++        if (Args.hasArg(options::OPT_shared_libgcc)) {
++          CmdArgs.push_back("-lgcc_pic");
++          if (!Args.hasArg(options::OPT_shared))
++            CmdArgs.push_back("-lgcc");
++        } else {
++          CmdArgs.push_back("-lgcc");
++          CmdArgs.push_back("--as-needed");
++          CmdArgs.push_back("-lgcc_pic");
++          CmdArgs.push_back("--no-as-needed");
++        }
++      }
+     } else {
+-      CmdArgs.push_back("-lgcc");
++      if (Args.hasArg(options::OPT_shared)) {
++        CmdArgs.push_back("-lgcc_pic");
++      } else {
++        CmdArgs.push_back("-lgcc");
++      }
+     }
+   }
+ 
+   if (!Args.hasArg(options::OPT_nostdlib) &&
+       !Args.hasArg(options::OPT_nostartfiles)) {
+-    if (!Args.hasArg(options::OPT_shared))
++    if (Args.hasArg(options::OPT_shared) || Args.hasArg(options::OPT_pie))
+       CmdArgs.push_back(Args.MakeArgString(
+-                              getToolChain().GetFilePath("crtend.o")));
++                              getToolChain().GetFilePath("crtendS.o")));
+     else
+       CmdArgs.push_back(Args.MakeArgString(
+-                              getToolChain().GetFilePath("crtendS.o")));
++                              getToolChain().GetFilePath("crtend.o")));
+     CmdArgs.push_back(Args.MakeArgString(
+-                              getToolChain().GetFilePath("crtn.o")));
++                            getToolChain().GetFilePath("crtn.o")));
+   }
+ 
+   addProfileRT(getToolChain(), Args, CmdArgs, getToolChain().getTriple());
