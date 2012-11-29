69994fb2763f17c85563b142e5b5d221af6685df
ryoon <ryoon@pkgsrc.org>
2012-11-29 13:52:51 +0000

    Update to 1.57
    * Drop cmake support (upstream does not cmake anymore)
    * To build GUI, qmake is needed
    
    Changelog:
    * Bug fixes
    * Many improvements
    * Improve translations

--- devel/cppcheck/options.mk
+++ devel/cppcheck/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2011/05/29 11:52:12 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2012/11/29 13:52:51 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.cppcheck
 PKG_SUPPORTED_OPTIONS=	qt
@@ -6,23 +6,14 @@ PKG_SUPPORTED_OPTIONS=	qt
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mqt)
-INSTALLATION_DIRS+=	lib share/cppcheck
-
 PLIST_SRC+=		PLIST PLIST.qt
-
-do-install:
-	${INSTALL_SCRIPT} ${WRKSRC}/cli/cppcheck ${DESTDIR}${PREFIX}/bin
-	${INSTALL_PROGRAM} ${WRKSRC}/gui/cppcheck-gui \
-		${DESTDIR}${PREFIX}/bin
-	${INSTALL_DATA} ${WRKSRC}/man/cppcheck.1 \
-		${DESTDIR}${PREFIX}/${PKGMANDIR}/man1
 
 .include "../../x11/qt4-libs/buildlink3.mk"
 BUILDLINK_DEPMETHOD.qt4-tools?= full
 .include "../../x11/qt4-tools/buildlink3.mk"
-.else
-do-install:
-	${INSTALL_PROGRAM} ${WRKSRC}/cli/cppcheck ${DESTDIR}${PREFIX}/bin/
-	${INSTALL_DATA} ${WRKSRC}/man/cppcheck.1 \
-		${DESTDIR}${PREFIX}/${PKGMANDIR}/man1
+
+post-build:
+	cd ${WRKSRC}/gui && \
+		${CONFIGURE_ENV} QTDIR=${QTDIR} HAVE_RULES=yes ${QTDIR}/bin/qmake gui.pro && \
+		${MAKE_ENV} QTDIR=${QTDIR} make
 .endif
