75c19ebd415bb668a09420788eb90653030ca0f2
wiz <wiz>
2012-11-30 22:34:44 +0000

    Import libdmapsharing3-2.9.15 as net/libdmapsharing3.
    Libdmapsharing is a library you may use to access and share media content
    using DMAP (DAAP, DPAP & DACP).  The library presently supports Linux and
    other POSIX-based systems.  It is written in C using GObject and libsoup.
    
    The DMAP family of protocols are used by products such as Apple iTunes(TM),
    Apple iPhoto(TM), and the Roku SoundBridge(TM) family to share media such
    as music and photos.
    
    Possible uses for libdmapsharing include:
    
    * Adding picture sharing capabilities to a photograph application.
    * Adding music sharing capabilities to an audio application.
    * Developing DPAP server software for a home networking product.
    * Developing DMAP client software for a media player appliance.
    
    This is major version 3 of the library.

---
+++ net/libdmapsharing3/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/11/30 22:34:44 wiz Exp $
+
+BUILDLINK_TREE+=	libdmapsharing3
+
+.if !defined(LIBDMAPSHARING3_BUILDLINK3_MK)
+LIBDMAPSHARING3_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libdmapsharing3+=	libdmapsharing3>=2.9.15
+BUILDLINK_PKGSRCDIR.libdmapsharing3?=	../../net/libdmapsharing3
+
+.include "../../net/libsoup24/buildlink3.mk"
+.endif	# LIBDMAPSHARING3_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libdmapsharing3
