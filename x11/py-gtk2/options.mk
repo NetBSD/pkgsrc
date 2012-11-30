5426517935305486f9f81b38b0b6cf0d76781e16
ryoon <ryoon@pkgsrc.org>
2012-11-30 12:38:26 +0000

    Remove unnecessarily and confusing amd64 conditional. Thank you, wiz@.

--- x11/py-gtk2/options.mk
+++ x11/py-gtk2/options.mk
@@ -1,9 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2012/11/30 12:32:50 ryoon Exp $
+# $NetBSD: options.mk,v 1.2 2012/11/30 12:38:26 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-gtk2
 PKG_SUPPORTED_OPTIONS=	py-numpy
 .if (!empty(MACHINE_ARCH:Mi386) || !empty(MACHINE_ARCH:Mx86_64) || \
-	!empty(MACHINE_ARCH:Mamd64) || \
 	!empty(MACHINE_ARCH:Mia64) || !empty(MACHINE_ARCH:Mpowerpc*) || \
 	!empty(MACHINE_ARCH:Mhppa) || !empty(MACHINE_ARCH:Msparc*) || \
 	!empty(MACHINE_ARCH:Malpha) || !empty(MACHINE_ARCH:Mmips*))
