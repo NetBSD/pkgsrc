4d596a6f45319effddb514cb9546c17cc13c6a2e
darcy <darcy@pkgsrc.org>
2012-12-01 15:28:03 +0000

    Clean up patches and add comments so that it passes pkglint.

--- devel/ptlib/patches/patch-src-unix-tlibthrd.cxx
+++ devel/ptlib/patches/patch-src-unix-tlibthrd.cxx
@@ -1,11 +1,11 @@
-$NetBSD: patch-src-unix-tlibthrd.cxx,v 1.1 2012/10/13 00:49:38 darcy Exp $
+$NetBSD: patch-src-unix-tlibthrd.cxx,v 1.2 2012/12/01 15:28:03 darcy Exp $
 
 - deal with lack of pthread_yield on NetBSD
 - XXX needs other platforms to be checked.
 
 --- src/ptlib/unix/tlibthrd.cxx.orig    2012-08-23 02:12:27.000000000 +0000
 +++ src/ptlib/unix/tlibthrd.cxx
-@@ -1531,7 +1531,11 @@ PTimedMutex::~PTimedMutex()
+@@ -1531,6 +1531,10 @@ PTimedMutex::~PTimedMutex()
      for (PINDEX i = 0; i < 100; ++i) {
        if ((result = pthread_mutex_destroy(&m_mutex)) != EBUSY)
          break;
@@ -16,4 +16,3 @@ $NetBSD: patch-src-unix-tlibthrd.cxx,v 1.1 2012/10/13 00:49:38 darcy Exp $
 +#endif
      }
    }
-
