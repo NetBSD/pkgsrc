8eb5bbd011686e63ac8bcc59752f0110fab2f113
cheusov <cheusov@pkgsrc.org>
2012-12-02 01:27:16 +0000

    BUILDLINK_API_DEPENDS.lirc += lirc>=0.9.0

--- comms/lirc/buildlink3.mk
+++ comms/lirc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/12/02 01:21:51 cheusov Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/12/02 01:27:16 cheusov Exp $
 
 # Check whether LIRC is available on this platform
 .include "available.mk"
@@ -9,7 +9,7 @@ BUILDLINK_TREE+=	lirc
 .  if !defined(LIRC_BUILDLINK3_MK)
 LIRC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lirc+=	lirc>=0.8.0
+BUILDLINK_API_DEPENDS.lirc+=	lirc>=0.9.0
 BUILDLINK_PKGSRCDIR.lirc?=	../../comms/lirc
 .  endif # LIRC_BUILDLINK3_MK
 
