85229e59ce29a2dae33e1777e4e792cce1e37318
cheusov <cheusov@pkgsrc.org>
2012-12-02 01:21:50 +0000

    Import LIRC, Linux Infrared Remote Control
      LIRC is a package that supports receiving and sending IR signals of
      the most common IR remote controls. It contains a daemon that decodes
      and sends IR signals, a mouse daemon that translates IR signals to
      mouse movements and a couple of user programs that allow to control
      your computer with a remote control.
    
      Tested on RHEL.

---
+++ comms/lirc/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/12/02 01:21:51 cheusov Exp $
+
+# Check whether LIRC is available on this platform
+.include "available.mk"
+.if ${LIRC_AVAILABLE} == "yes"
+
+BUILDLINK_TREE+=	lirc
+
+.  if !defined(LIRC_BUILDLINK3_MK)
+LIRC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.lirc+=	lirc>=0.8.0
+BUILDLINK_PKGSRCDIR.lirc?=	../../comms/lirc
+.  endif # LIRC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-lirc
+.endif # LIRC_AVAILABLE
