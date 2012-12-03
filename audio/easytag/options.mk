489b6348dc20792d7ecb39d97ef4bc1cb2c99b07
wiz <wiz@pkgsrc.org>
2012-12-03 22:39:04 +0000

    Add more complete id3 support by also linking against id3lib (easytag uses two different id3 support libraries.
    Fix mp4 support option. It was depending on libmp4v2, but that wasn't
    recognized by easytag's configure any longer. Switch to mp4v2 and rename
    the option to match.
    
    Add patches from gentoo to fix build with latest mp4v2 version.
    
    Bump PKGREVISION.

--- audio/easytag/options.mk
+++ audio/easytag/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.5 2012/03/08 09:49:26 wiz Exp $
+# $NetBSD: options.mk,v 1.6 2012/12/03 22:39:04 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.easytag
-PKG_SUPPORTED_OPTIONS=	flac mpeg4ip ogg speex wavpack
-PKG_SUGGESTED_OPTIONS=	flac mpeg4ip ogg speex wavpack
+PKG_SUPPORTED_OPTIONS=	flac mp4v2 ogg speex wavpack
+PKG_SUGGESTED_OPTIONS=	flac mp4v2 ogg speex wavpack
 
 .include "../../mk/bsd.options.mk"
 
@@ -14,11 +14,8 @@ CONFIGURE_ARGS+=	--enable-flac
 CONFIGURE_ARGS+=	--disable-flac
 .endif
 
-.if !empty(PKG_OPTIONS:Mmpeg4ip)
-BUILDLINK_API_DEPENDS.libmp4v2+= libmp4v2>=1.6.1
-.include "../../multimedia/libmp4v2/buildlink3.mk"
-# for mpeg4ip.h
-CPPFLAGS+=		-DHAVE_GTK
+.if !empty(PKG_OPTIONS:Mmp4v2)
+.include "../../multimedia/mp4v2/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mogg)
