b2d28a968d6c8d387f47d52f944bc5c5ff18f862
adam <adam@pkgsrc.org>
2012-12-07 10:06:13 +0000

    Qt 4.8.4 is a bug-fix release. It maintains both forward and backward compatibility (source and binary) with Qt 4.8.0, 4.8.1, 4.8.2 and 4.8.3. For more details, refer to the online documentation included in this distribution. The documentation is also available online:
    http://qt-project.org/doc/4.8
    
    The Qt version 4.8 series is binary compatible with the 4.7.x series.
    Applications compiled for 4.7 will continue to run with 4.8.
    
    Some of the changes listed in this file include issue tracking numbers
    corresponding to tasks in the Qt Bug Tracker or the Merge Request queue
    of the public source repository.

--- x11/qt4-libs/patches/patch-src_corelib_io_qfilesystemwatcher.cpp
+++ x11/qt4-libs/patches/patch-src_corelib_io_qfilesystemwatcher.cpp
@@ -1,18 +1,19 @@
-$NetBSD: patch-src_corelib_io_qfilesystemwatcher.cpp,v 1.1 2012/09/12 17:02:50 jaapb Exp $
+$NetBSD: patch-src_corelib_io_qfilesystemwatcher.cpp,v 1.2 2012/12/07 10:06:13 adam Exp $
 
 Use kqueue instead of fam to monitor the file system
---- src/corelib/io/qfilesystemwatcher.cpp.orig	2012-04-26 19:46:08.000000000 +0000
+
+--- src/corelib/io/qfilesystemwatcher.cpp.orig	2012-11-23 10:09:55.000000000 +0000
 +++ src/corelib/io/qfilesystemwatcher.cpp
-@@ -57,7 +57,7 @@
- #elif defined(Q_OS_LINUX)
- #  include "qfilesystemwatcher_inotify_p.h"
+@@ -59,7 +59,7 @@
  #  include "qfilesystemwatcher_dnotify_p.h"
+ #elif defined(Q_OS_QNX) && !defined(QT_NO_INOTIFY)
+ #  include "qfilesystemwatcher_inotify_p.h"
 -#elif defined(Q_OS_FREEBSD) || defined(Q_OS_MAC)
 +#elif defined(Q_OS_FREEBSD) || defined(Q_OS_MAC) || defined(Q_OS_NETBSD)
  #  if (defined Q_OS_MAC) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_5)
  #  include "qfilesystemwatcher_fsevents_p.h"
  #  endif //MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_5)
-@@ -253,7 +253,7 @@ QFileSystemWatcherEngine *QFileSystemWat
+@@ -257,7 +257,7 @@ QFileSystemWatcherEngine *QFileSystemWat
      if(!eng)
          eng = QDnotifyFileSystemWatcherEngine::create();
      return eng;
