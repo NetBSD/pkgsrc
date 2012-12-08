528d5c56408c2805a9e32faa5bde3426b90cee9b
adam <adam>
2012-12-08 22:47:40 +0000

    Release 1.1.2 (5 Dec 2012) -------------------------- * maketx fixes -- was botching creation of textures from source images that   were crop windows (pixel window smaller than display window). * Minor bug fix to Timer when repeatedly starting and restopping (Apple only). * Bug fix in ustring:find_last_not_of

--- graphics/openimageio/buildlink3.mk
+++ graphics/openimageio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/12/03 12:54:14 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/12/08 22:47:40 adam Exp $
 
 BUILDLINK_TREE+=	openimageio
 
@@ -8,6 +8,17 @@ OPENIMAGEIO_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.openimageio+=	openimageio>=1.1.1
 BUILDLINK_PKGSRCDIR.openimageio?=	../../graphics/openimageio
 
+.include "../../devel/boost-libs/buildlink3.mk"
+.include "../../devel/hdf5/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/glew/buildlink3.mk"
+.include "../../graphics/ilmbase/buildlink3.mk"
+.include "../../graphics/libwebp/buildlink3.mk"
+.include "../../graphics/opencv/buildlink3.mk"
+.include "../../graphics/openjpeg15/buildlink3.mk"
+.include "../../graphics/openexr/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+.include "../../x11/qt4-libs/buildlink3.mk"
 .endif	# OPENIMAGEIO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-openimageio
