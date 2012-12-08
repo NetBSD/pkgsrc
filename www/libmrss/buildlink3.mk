13eb555fa42f1b5b610ca6ca94b28fb398f2d1b6
ryoon <ryoon@pkgsrc.org>
2012-12-08 16:53:41 +0000

    Import libmrss-0.19.2nb1 as www/libmrss from pkgsrc-wip.
    mRSS is a C library for parsing, writing and creating RSS files or
    streams.

---
+++ www/libmrss/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/12/08 16:53:41 ryoon Exp $
+
+BUILDLINK_TREE+=	libmrss
+
+.if !defined(LIBMRSS_BUILDLINK3_MK)
+LIBMRSS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libmrss+=	libmrss>=0.19.2
+BUILDLINK_ABI_DEPENDS.libmrss+=	libmrss>=0.19.2nb1
+BUILDLINK_PKGSRCDIR.libmrss?=	../../www/libmrss
+
+.include "../../textproc/libnxml/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
+.endif	# LIBMRSS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libmrss
