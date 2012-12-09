d83599a8b93c640077cde73d64a22acd26693336
ryoon <ryoon>
2012-12-09 15:25:42 +0000

    Bump BUILDLINK_{API,ABI}_DEPENDS

--- graphics/openjpeg/buildlink3.mk
+++ graphics/openjpeg/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/03/24 13:25:01 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/12/09 15:25:42 ryoon Exp $
 
 BUILDLINK_TREE+=	openjpeg
 
 .if !defined(OPENJPEG_BUILDLINK3_MK)
 OPENJPEG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.openjpeg+=	openjpeg>=1.3
-BUILDLINK_ABI_DEPENDS.openjpeg+=	openjpeg>=1.5
+BUILDLINK_API_DEPENDS.openjpeg+=	openjpeg>=2.0.0
+BUILDLINK_ABI_DEPENDS.openjpeg+=	openjpeg>=2.0.0
 BUILDLINK_PKGSRCDIR.openjpeg?=	../../graphics/openjpeg
 .endif	# OPENJPEG_BUILDLINK3_MK
 
