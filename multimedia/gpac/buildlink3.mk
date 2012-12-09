9265c027428143eb351fd9c65983e375ff495fae
ryoon <ryoon@pkgsrc.org>
2012-12-09 17:15:06 +0000

    Update to 0.5.0
    Changelog:
    25/05/2012: GPAC 0.5.0
      - MPEG-DASH and Apple HLS support in GPAC Clients
      - MPEG-DASH segmenter for ISO files and MPEG-2 TS in MP4Box
      - MP42TS generator now supports HLS output
      - Support for MPEG-U and W3C widgets
      - UPnP and DLNA support in the player through Platinum libraries, interfaced in JavaScript
      - Better support for AVC and SVC muxing in MP4
      - Support for OpenSVC decoder
      - Stereo and Multi-view renderer for auto-stereoscopic screens
      - iOS and Android support (but Symbian support has been dropped)
      - Camera input through "camera://default" URLs on Windows, OSX 32bit, Linux V4L v1 and Android
      - experimental audio filters
      - Better T-DMB support
      - experimental DVB-MPE and DSM-CC support
      - BIFS ExtendedCore2D profile support
      - more work on GUI
      - and many many fixes and improvements in players and MP4Box

--- multimedia/gpac/buildlink3.mk
+++ multimedia/gpac/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:02 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/12/09 17:15:06 ryoon Exp $
 
 BUILDLINK_TREE+=	gpac
 
 .if !defined(GPAC_BUILDLINK3_MK)
 GPAC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gpac+=	gpac>=0.4.5
+BUILDLINK_API_DEPENDS.gpac+=	gpac>=0.5.0
+BUILDLINK_ABI_DEPENDS.gpac+=	gpac>=0.5.0
 BUILDLINK_PKGSRCDIR.gpac?=	../../multimedia/gpac
 .endif # GPAC_BUILDLINK3_MK
 
