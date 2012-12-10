e47116b2a8664a82e7e4386773e8f5c9a4ea0e82
ryoon <ryoon@pkgsrc.org>
2012-12-10 12:06:50 +0000

    Update to 1.0.23
    * Update MASTER_SITES to MASTER_SITE_LOCAL
    
    Changelog:
    New with 1.0.23, released 2012-08-19:
    
    * New backends: kvs40xx (Panasonic KV-S40xx),
      kodakaio (Kodak AiO ESP and Hero)
    * Significant enhancements to several backends.
    * 30 new scanner models supported.
    * V4L support improvements.
    * Improvements for builds on multiple platforms.
    * Improved udev rules.
    * Updated software deskew algorithm.
    * Documentation updates.
    * Bugfixes.

--- graphics/sane-backends/buildlink3.mk
+++ graphics/sane-backends/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2012/02/06 12:40:36 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2012/12/10 12:06:50 ryoon Exp $
 
 BUILDLINK_TREE+=	sane-backends
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	sane-backends
 SANE_BACKENDS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.sane-backends+=	sane-backends>=1.0.3
-BUILDLINK_ABI_DEPENDS.sane-backends+=	sane-backends>=1.0.22nb2
+BUILDLINK_ABI_DEPENDS.sane-backends+=	sane-backends>=1.0.23
 BUILDLINK_PKGSRCDIR.sane-backends?=	../../graphics/sane-backends
 
 .include "../../mk/bsd.fast.prefs.mk"
