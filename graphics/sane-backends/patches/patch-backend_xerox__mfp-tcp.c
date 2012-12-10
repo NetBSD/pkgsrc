30b28c4dec81b11e83edb89e2dd3a4f781eeb277
ryoon <ryoon@pkgsrc.org>
2012-12-10 12:06:50 +0000

    Update to 1.0.23
    * Update MASTER_SITES to MASTER_SITE_LOCAL
    
    Changelog:
    New with 1.0.23, released 2012-08-19:
    
    * New backends: kvs40xx (Panasonic KV-S40xx),
      kodakaio (Kodak AiO ESP and Hero)
    * Significant enhancements to several backends.
    * 30 new scanner models supported.
    * V4L support improvements.
    * Improvements for builds on multiple platforms.
    * Improved udev rules.
    * Updated software deskew algorithm.
    * Documentation updates.
    * Bugfixes.

--- graphics/sane-backends/patches/patch-backend_xerox__mfp-tcp.c
+++ graphics/sane-backends/patches/patch-backend_xerox__mfp-tcp.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-backend_xerox__mfp-tcp.c,v 1.1 2011/07/31 20:05:58 gls Exp $
+$NetBSD: patch-backend_xerox__mfp-tcp.c,v 1.2 2012/12/10 12:06:51 ryoon Exp $
 
 Fix for pkg/45196. Add sys/socket.h header.
 
---- backend/xerox_mfp-tcp.c.orig	2010-12-01 23:49:58.000000000 +0000
+--- backend/xerox_mfp-tcp.c.orig	2011-11-21 20:17:48.000000000 +0000
 +++ backend/xerox_mfp-tcp.c
-@@ -28,6 +28,10 @@
- #include <errno.h>
- #include <sys/time.h>
+@@ -32,6 +32,10 @@
+ #include <sys/socket.h>
+ #endif
  
 +#ifdef HAVE_SYS_SOCKET_H
 +#include <sys/socket.h>
