c5be928a7f1751fd5d7beffc56760683b44b66a1
ryoon <ryoon@pkgsrc.org>
2012-12-11 10:29:00 +0000

    Adjust buildlink3 ABI variable.

--- audio/libopus/buildlink3.mk
+++ audio/libopus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/09/16 14:17:20 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/12/11 10:29:00 ryoon Exp $
 
 BUILDLINK_TREE+=	opus
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	opus
 OPUS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.opus+=	opus>=1.0.1
+BUILDLINK_ABI_DEPENDS.opus+=	opus>=1.0.2
 BUILDLINK_PKGSRCDIR.opus?=	../../audio/libopus
 .endif	# OPUS_BUILDLINK3_MK
 
