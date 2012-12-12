c4e9a87d1c1fdcb0c48b8fe8d9d0697eb2d20964
dsainty <dsainty@pkgsrc.org>
2012-12-12 23:44:40 +0000

    There is already a 'static' keyword present for part of this patch, so patching in a second one is unnecessary, and for some compilers (GCC 4.4) fatal at build time.

--- cross/avr-gcc/patches/patch-gcc_cp_cfns.h
+++ cross/avr-gcc/patches/patch-gcc_cp_cfns.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-gcc_cp_cfns.h,v 1.1 2012/11/16 00:38:21 joerg Exp $
+$NetBSD: patch-gcc_cp_cfns.h,v 1.2 2012/12/12 23:44:40 dsainty Exp $
 
 --- gcc/cp/cfns.h.orig	2012-11-14 17:13:55.000000000 +0000
 +++ gcc/cp/cfns.h
-@@ -35,13 +35,13 @@ __inline
+@@ -35,7 +35,7 @@
  #endif
  static unsigned int hash (const char *, unsigned int);
  #ifdef __GNUC__
@@ -11,10 +11,3 @@ $NetBSD: patch-gcc_cp_cfns.h,v 1.1 2012/11/16 00:38:21 joerg Exp $
  #endif
  const char * libc_name_p (const char *, unsigned int);
  /* maximum key range = 391, duplicates = 0 */
- 
- #ifdef __GNUC__
--__inline
-+static __inline
- #else
- #ifdef __cplusplus
- inline
