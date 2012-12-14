3dbea674e1fb8158bdd6a7d9eee1715ddc8fb16e
jaapb <jaapb@pkgsrc.org>
2012-12-14 14:32:32 +0000

    Update of devel/ocaml-lwt to its newest version, 2.4.2. Changes include:
    2.4.2
    * fix the stub for Lwt_unix.readdir
    * change the default method for Lwt_glib.install (use the glib main loop by
      default: more portable)
    * ignore invalid file descriptors returned by glib (like the emulation of
      select in glib does)
    * use environment variables in discover.ml - use LIBRARY_PATH and
      C_INCLUDE_PATH for searching headers - allow to pass flags for a library
    	in <LIB>_CLFAGS and <LIB>_LIBS
    * add Lwt_unix.on_signal_full
    
    2.4.1
    * Add Lwt_stream.on_terminate
    * Fix Lwt_gc
    * Fix stubs for get_credentials on *BSD

--- devel/ocaml-lwt/buildlink3.mk
+++ devel/ocaml-lwt/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/05/30 13:40:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/12/14 14:32:32 jaapb Exp $
 
 BUILDLINK_TREE+=	ocamllwt
 
 .if !defined(OCAMLLWT_BUILDLINK3_MK)
 OCAMLLWT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocamllwt+=	ocaml-lwt>=2.0.0
+BUILDLINK_API_DEPENDS.ocamllwt+=	ocaml-lwt>=2.4.2
 BUILDLINK_PKGSRCDIR.ocamllwt?=	../../devel/ocaml-lwt
 
 .include "../../devel/libev/buildlink3.mk"
