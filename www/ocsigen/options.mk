d0b71a169ed4e4d857086f705db44e8d95719cfa
jaapb <jaapb@pkgsrc.org>
2012-12-15 11:16:07 +0000

    Updated www/ocsigen to its newest version, 2.2.0. Changes include:
    * Relative filenames when not running as daemon
    * Small change in ocsigen_lib: encoding of parameters with "
    * fix Ocsigen_http_client.get_url (and other) first "/" was missing
    * Installation: Do not try to chown files to a different user
    * Fix error on make logrotate
    * redirectmod: fixing default to permanent (as written in manual)
    * Minor additions in the API
    
    Also, the package was updated, mostly to use PLIST_VARS.

--- www/ocsigen/options.mk
+++ www/ocsigen/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2012/07/11 10:40:47 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2012/12/15 11:16:07 jaapb Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ocsigen
 PKG_SUPPORTED_OPTIONS=	sqlite camlzip
@@ -6,20 +6,20 @@ PKG_SUGGESTED_OPTIONS=	camlzip
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=	sqlite
 .if !empty(PKG_OPTIONS:Msqlite)
 DEPENDS+=		ocaml-sqlite3>=0.23.0:../../databases/ocaml-sqlite3
 CONFIGURE_ARGS+=	--with-sqlite
-PLIST_SRC+=		PLIST.sqlite
+PLIST.sqlite=		yes
 .else
 CONFIGURE_ARGS+=	--without-sqlite
 .endif
 
+PLIST_VARS+=	camlzip
 .if !empty(PKG_OPTIONS:Mcamlzip)
 DEPENDS+=		ocaml-zip-[0-9]*:../../archivers/ocaml-zip
 CONFIGURE_ARGS+=	--with-camlzip
-PLIST_SRC+=		PLIST.camlzip
+PLIST.camlzip=		yes
 .else
 CONFIGURE_ARGS+=	--without-camlzip
 .endif
-
-PLIST_SRC+=		PLIST
