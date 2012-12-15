3f6ff17a587a72a0ce0c942640e6f1f461d6380d
ryoon <ryoon@pkgsrc.org>
2012-12-15 10:36:18 +0000

    Bump PKGREVISION from devel/nss 3.14.0.

--- x11/gnome-control-center/buildlink3.mk
+++ x11/gnome-control-center/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.29 2012/10/08 23:00:39 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2012/12/15 10:36:23 ryoon Exp $
 
 BUILDLINK_TREE+=	gnome-control-center
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gnome-control-center
 GNOME_CONTROL_CENTER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gnome-control-center+=	gnome-control-center>=2.20.0.1
-BUILDLINK_ABI_DEPENDS.gnome-control-center+=	gnome-control-center>=2.32.1nb18
+BUILDLINK_ABI_DEPENDS.gnome-control-center+=	gnome-control-center>=2.32.1nb19
 BUILDLINK_PKGSRCDIR.gnome-control-center?=	../../x11/gnome-control-center
 
 .include "../../sysutils/gnome-menus/buildlink3.mk"
