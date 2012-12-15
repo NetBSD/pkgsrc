36ba32faeccc3a011926a9bed61cc1795d4eaa20
marino <marino@pkgsrc.org>
2012-12-15 01:09:40 +0000

    devel/xulrunner192: DRAGONFLY_CCVER=gcc44
    xulrunner192 will not build with gcc-4.6+.
    This problem has been fixed in later versions of xulrunner.
    Use the new DRAGONFLY_CCVER directory to direct DragonFly to use
    GCC 4.4.7 to build xulrunner192, even when GCC 4.7.2 is specified.

--- devel/xulrunner192/mozilla-common.mk
+++ devel/xulrunner192/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.2 2012/10/31 11:19:54 asau Exp $
+# $NetBSD: mozilla-common.mk,v 1.3 2012/12/15 01:09:40 marino Exp $
 #
 # common Makefile fragment for mozilla packages based on gecko 2.0.
 # 
@@ -9,6 +9,7 @@ GNU_CONFIGURE=		yes
 USE_TOOLS+=		pkg-config perl gmake autoconf213 unzip zip
 USE_LANGUAGES+=		c99 c++
 UNLIMIT_RESOURCES+=	datasize
+DRAGONFLY_CCVER=	gcc44
 
 CHECK_PORTABILITY_SKIP+=${MOZILLA_DIR}security/nss/tests/libpkix/libpkix.sh
 CHECK_PORTABILITY_SKIP+=${MOZILLA_DIR}security/nss/tests/multinit/multinit.sh
