3cfc797bf55602294c32e325573021b913a31096
ryoon <ryoon@pkgsrc.org>
2012-12-16 14:23:07 +0000

    Update to 3.4.1
    * Update HOMEPAGE
    
    Changelog:
    3.4.1 (2012.10.17)
    
            A bug that mutual exclusion was not performed has been fixed.
    
    3.4.0 (2012.10.15)
    
            SVG, UTF8, and ANSIUTF8 output supports have been added to the command line tool. (Thanks to Dan Storm, David Dahl, and Lennart Poettering)
            Colored QR Code support.
            Bug fixes. (Thanks to Terry Burton, Fred Steinhaeuser, and Yann Droneaud)
    
    3.3.1 (2012.4.18)
    
            Bugs in command line tool, manual, configure script, and libtool files have been fixed. (Thanks to Yutaka Niibe and Rob Ryan)
    
    3.3.0 (2012.4.1)
    
            EPS, ANSI, and ASCII text output supports have been added. (Thanks to Zapster, Colin, and Ral)
            QRcode_APIVersion() and QRcode_APIVersionString() have been added.
    
    3.2.1 (2012.4.1)
    
            Bugs in configure script and libtool file has been fixed. (Thanks to Yutaka Niibe)

--- converters/qrencode/buildlink3.mk
+++ converters/qrencode/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:04 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/12/16 14:23:07 ryoon Exp $
 
 BUILDLINK_TREE+=	qrencode
 
 .if !defined(QRENCODE_BUILDLINK3_MK)
 QRENCODE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qrencode+=	qrencode>=3.0.3
+BUILDLINK_API_DEPENDS.qrencode+=	qrencode>=3.4.1
 BUILDLINK_PKGSRCDIR.qrencode?=	../../converters/qrencode
 .endif # QRENCODE_BUILDLINK3_MK
 
