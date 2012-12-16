529d6eb8cd8149aaf036024380a0cffeb2ae68cf
khorben <khorben@pkgsrc.org>
2012-12-16 23:55:22 +0000

    Imported wip/deforaos-libsystem, the DeforaOS system library, under devel/deforaos-libsystem

---
+++ devel/deforaos-libsystem/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/12/16 23:55:22 khorben Exp $
+
+BUILDLINK_TREE+=	deforaos-libsystem
+
+.if !defined(DEFORAOS_LIBSYSTEM_BUILDLINK3_MK)
+DEFORAOS_LIBSYSTEM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.deforaos-libsystem+=	deforaos-libsystem>=0.1.2
+BUILDLINK_PKGSRCDIR.deforaos-libsystem?=	../../devel/deforaos-libsystem
+
+.endif	# DEFORAOS_LIBSYSTEM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-deforaos-libsystem
