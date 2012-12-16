6aa3367e4d2c3bf159b1db324004f5ff56aafcaa
drochner <drochner@pkgsrc.org>
2012-12-16 13:09:25 +0000

    propagate depemdency on fontconfig, it is referenced by the .pc file, might fix PR pkg/47329 by David Shao

--- multimedia/libass/buildlink3.mk
+++ multimedia/libass/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/12/14 21:50:46 cheusov Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/12/16 13:09:25 drochner Exp $
 
 BUILDLINK_TREE+=	libass
 
@@ -8,6 +8,7 @@ LIBASS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libass+=	libass>=0.9.12
 BUILDLINK_PKGSRCDIR.libass?=	../../multimedia/libass
 
+.include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../textproc/enca/buildlink3.mk"
 .endif # LIBASS_BUILDLINK3_MK
