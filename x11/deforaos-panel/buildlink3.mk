b806042627ebf1b704c1939e26cf7e446833d839
khorben <khorben@pkgsrc.org>
2012-12-17 00:32:49 +0000

    Imported wip/deforaos-panel, the DeforaOS desktop panel, under x11/deforaos-panel (version 0.2.16)

---
+++ x11/deforaos-panel/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/12/17 00:32:49 khorben Exp $
+#
+
+BUILDLINK_TREE+=	deforaos-panel
+
+.if !defined(DEFORAOS_PANEL_BUILDLINK3_MK)
+DEFORAOS_PANEL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.deforaos-panel+=	deforaos-panel>=0.2.12
+BUILDLINK_PKGSRCDIR.deforaos-panel?=	../../x11/deforaos-panel
+
+.include "../../devel/deforaos-libsystem/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# DEFORAOS_PANEL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-deforaos-panel
