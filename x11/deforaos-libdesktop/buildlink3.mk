3f9189b102bcc3445383f3dfc4e61db31fb97124
khorben <khorben@pkgsrc.org>
2012-12-17 00:09:34 +0000

    Imported wip/deforaos-libdesktop, the DeforaOS desktop library, under x11/deforaos-libdesktop (version 0.0.8)

---
+++ x11/deforaos-libdesktop/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/12/17 00:09:34 khorben Exp $
+
+BUILDLINK_TREE+=	deforaos-libdesktop
+
+.if !defined(DEFORAOS_LIBDESKTOP_BUILDLINK3_MK)
+DEFORAOS_LIBDESKTOP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.0.7
+BUILDLINK_PKGSRCDIR.deforaos-libdesktop?=	../../x11/deforaos-libdesktop
+
+.include "../../devel/deforaos-libsystem/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# DEFORAOS_LIBDESKTOP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-deforaos-libdesktop
