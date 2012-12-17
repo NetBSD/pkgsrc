978213105362b8369e3d67fe40b7444c80c9f928
khorben <khorben@pkgsrc.org>
2012-12-17 00:59:54 +0000

    Imported wip/deforaos-mailer, the DeforaOS desktop e-mail client, under mail/deforaos-mailer (version 0.1.5)

---
+++ mail/deforaos-mailer/options.mk
@@ -0,0 +1,10 @@
+# $NetBSD: options.mk,v 1.1 2012/12/17 00:59:55 khorben Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.deforaos-mailer
+PKG_SUPPORTED_OPTIONS=	embedded
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Membedded)
+MAKE_FLAGS+=	CPPFLAGS=-DEMBEDDED
+.endif
