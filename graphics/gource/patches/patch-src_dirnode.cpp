b5cb10976bb84ef0ccbe19bf600ec13b5fc053cd
ryoon <ryoon@pkgsrc.org>
2012-12-19 13:50:35 +0000

    Fix build again on gcc 4.5.4.

--- graphics/gource/patches/patch-src_dirnode.cpp
+++ graphics/gource/patches/patch-src_dirnode.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_dirnode.cpp,v 1.1 2012/12/19 13:42:04 ryoon Exp $
+$NetBSD: patch-src_dirnode.cpp,v 1.2 2012/12/19 13:50:35 ryoon Exp $
 
-* Fix build failure with gcc 4.7
+* Fix build failure with gcc 4.7 (and 5.4.4)
 
 --- src/dirnode.cpp.orig	2012-04-20 01:52:29.000000000 +0000
 +++ src/dirnode.cpp
@@ -9,7 +9,7 @@ $NetBSD: patch-src_dirnode.cpp,v 1.1 2012/12/19 13:42:04 ryoon Exp $
  
  //    this->parent_radius = std::max(1.0, parent_circ / PI);
 -    this->parent_radius = std::max(1.0, sqrt(total_file_area) * gGourceDirPadding);
-+    this->parent_radius = std::max(1.0f, sqrt(total_file_area) * gGourceDirPadding);
++    this->parent_radius = std::max(1.0f, (float) (sqrt(total_file_area) * gGourceDirPadding));
  }
  
  float RDirNode::distanceToParent() const{
