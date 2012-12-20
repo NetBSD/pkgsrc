c8c30ea1decb1215ef922cb2ecc529c33e425088
joerg <joerg@pkgsrc.org>
2012-12-20 22:00:42 +0000

    Using long as modifier for typedef'd names is invalid C.

--- devel/gcvs/patches/patch-common_UCvsConsole.cpp
+++ devel/gcvs/patches/patch-common_UCvsConsole.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-common_UCvsConsole.cpp,v 1.1 2011/11/25 21:42:44 joerg Exp $
+$NetBSD: patch-common_UCvsConsole.cpp,v 1.2 2012/12/20 22:00:42 joerg Exp $
 
---- common/UCvsConsole.cpp.orig	2011-11-25 17:57:07.000000000 +0000
+--- common/UCvsConsole.cpp.orig	2000-06-06 05:48:24.000000000 +0000
 +++ common/UCvsConsole.cpp
 @@ -22,6 +22,7 @@
   * 
@@ -10,3 +10,12 @@ $NetBSD: patch-common_UCvsConsole.cpp,v 1.1 2011/11/25 21:42:44 joerg Exp $
  #include "stdafx.h"
  
  #if qGTK
+@@ -230,7 +231,7 @@ extern "C"
+ 			nEndChar = w.selection_end_pos;
+ 		}
+ 		
+-		guint long length = nEndChar - nStartChar;
++		size_t length = nEndChar - nStartChar;
+ 		if(length > 0 && nStartChar < len)
+ 		{			
+ 			char *buf = (char *)malloc((length + 1) * sizeof(char));
