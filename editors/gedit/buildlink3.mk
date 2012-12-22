4bc7a989411aeb4fdad4327b78b44400f37362e6
joerg <joerg@pkgsrc.org>
2012-12-22 02:23:30 +0000

    Make sure that depending packages don't pick up newer gedit versions.

--- editors/gedit/buildlink3.mk
+++ editors/gedit/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.32 2012/10/08 23:00:40 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.33 2012/12/22 02:23:30 joerg Exp $
 
 BUILDLINK_TREE+=	gedit
 
 .if !defined(GEDIT_BUILDLINK3_MK)
 GEDIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gedit+=	gedit>=2.12.1nb4
-BUILDLINK_ABI_DEPENDS.gedit+=	gedit>=2.30.4nb12
+BUILDLINK_API_DEPENDS.gedit+=	gedit>=2.12.1nb4<3
+BUILDLINK_ABI_DEPENDS.gedit+=	gedit>=2.30.4nb12<3
 BUILDLINK_PKGSRCDIR.gedit?=	../../editors/gedit
 
 .include "../../x11/gtksourceview2/buildlink3.mk"
