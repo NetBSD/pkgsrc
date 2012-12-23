56df01c1ab0a23d2352a8350111c53d025d5da1e
dholland <dholland@pkgsrc.org>
2012-12-23 18:43:47 +0000

    Use c89. Pass gcc -Wall. Fix clang build. While here, use snprintf instead of sprintf. PKGREVISION -> 2.

--- games/dipmap/patches/patch-strrstr.c
+++ games/dipmap/patches/patch-strrstr.c
@@ -1,9 +1,30 @@
-$NetBSD: patch-strrstr.c,v 1.1 2011/12/06 19:55:43 joerg Exp $
+$NetBSD: patch-strrstr.c,v 1.2 2012/12/23 18:43:47 dholland Exp $
 
---- strrstr.c.orig	2011-12-06 15:46:38.000000000 +0000
+- use standard headers
+- use c89 preprocessor syntax
+- silence gcc warning about unused variable
+
+--- strrstr.c.orig	1993-10-29 21:10:48.000000000 +0000
 +++ strrstr.c
-@@ -1,3 +1,4 @@
+@@ -1,10 +1,11 @@
 +#include <string.h>
  /* Strrstr.c, included for those computers that do not have it. */
  /* Written by Kent Irwin, irwin@leland.stanford.edu.  I am
     responsible for bugs */
+ #ifdef STRRSTR_MISSING
+ #ifndef NULL
+ #define NULL    0
+-#endif NULL
++#endif
+ #ifdef ANSI
+ char *strrstr(char *s1,char *s2) {
+ #else
+@@ -14,7 +15,7 @@ char *s2;
+ {
+ #endif
+ 
+-	char *sc1, *sc2, *psc1, *ps1;
++	char *sc2, *psc1, *ps1;
+ 
+ 	if (*s2 == '\0')
+ 		return((char *)s1);
