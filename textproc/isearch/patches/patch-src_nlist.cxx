eb21358b09acb66ec60442ff9c42bc8253d720c1
dholland <dholland>
2012-12-25 18:50:17 +0000

    Fix implicit int on main; should fix clang build.

--- textproc/isearch/patches/patch-src_nlist.cxx
+++ textproc/isearch/patches/patch-src_nlist.cxx
@@ -1,10 +1,12 @@
-$NetBSD: patch-src_nlist.cxx,v 1.1 2012/12/21 10:29:47 dholland Exp $
+$NetBSD: patch-src_nlist.cxx,v 1.2 2012/12/25 18:50:18 dholland Exp $
 
 Chase after the C++ standard:
    - use the newfangled names for C++ headers
    - open the std namespace instead of using explicit "std" qualification
 
---- src/nlist.cxx~	2000-02-04 23:39:56.000000000 +0000
+Also, declare main's return type.
+
+--- src/nlist.cxx.orig	2000-02-04 23:39:56.000000000 +0000
 +++ src/nlist.cxx
 @@ -44,11 +44,13 @@ Description:	Class NUMERICLIST
  Author:		Jim Fullton, Jim.Fullton@cnidr.org
@@ -22,3 +24,11 @@ Chase after the C++ standard:
  static INT 
  SortCmp(const void* x, const void* y);
  
+@@ -875,6 +877,7 @@ NUMERICLIST::TempLoad()
+ }
+ 
+ 
++int
+ main()
+ {
+   NUMERICLIST list;
