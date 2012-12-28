9e18fc2d79ca60a5a3e151f5da32d4113159c1f4
jnemeth <jnemeth>
2012-12-28 05:27:06 +0000

    sendmail is incompatible with current versions of databases/db4 as the latter is now threaded, so delete the option

--- mail/sendmail/options.mk
+++ mail/sendmail/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.18 2012/06/12 15:45:58 wiz Exp $
+# $NetBSD: options.mk,v 1.19 2012/12/28 05:27:06 jnemeth Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sendmail
-PKG_SUPPORTED_OPTIONS=	inet6 db2 db4 ldap sasl tls tcpwrappers
+PKG_SUPPORTED_OPTIONS=	inet6 db2 ldap sasl tls tcpwrappers
 PKG_SUGGESTED_OPTIONS=	inet6 tcpwrappers
 
 PKG_OPTIONS_LEGACY_OPTS+=	starttls:tls
@@ -9,12 +9,10 @@ PKG_OPTIONS_LEGACY_OPTS+=	starttls:tls
 .include "../../mk/bsd.options.mk"
 
 ###
-### Berkeley DB version 2/4 format for on disk databases e.g. aliases
+### Berkeley DB version 2 format for on disk databases e.g. aliases
 ###
 .if !empty(PKG_OPTIONS:Mdb2)
 .  include "../../databases/db/buildlink3.mk"
-.elif !empty(PKG_OPTIONS:Mdb4)
-.  include "../../databases/db4/buildlink3.mk"
 .endif
 
 ###
