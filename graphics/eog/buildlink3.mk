ecb4a1ba603965bf9a8d0d565da0d482dbc86601
joerg <joerg@pkgsrc.org>
2012-12-28 22:21:14 +0000

    Don't accidently pick up eog-3 as dependency.

--- graphics/eog/buildlink3.mk
+++ graphics/eog/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2012/10/08 23:00:46 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2012/12/28 22:21:14 joerg Exp $
 
 BUILDLINK_TREE+=	eog
 
 .if !defined(EOG_BUILDLINK3_MK)
 EOG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.eog+=	eog>=2.30.2
-BUILDLINK_ABI_DEPENDS.eog+=	eog>=2.32.1nb13
+BUILDLINK_API_DEPENDS.eog+=	eog>=2.30.2<3
+BUILDLINK_ABI_DEPENDS.eog+=	eog>=2.32.1nb13<3
 BUILDLINK_PKGSRCDIR.eog?=	../../graphics/eog
 
 .include "../../devel/GConf/buildlink3.mk"
