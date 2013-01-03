3b1355a2a03550661b3bf9397336d7a741addbc9
adam <adam>
2013-01-03 15:19:53 +0000

    Changes 3.2: * Improvements to Clang's diagnostics * Support for tls_model attribute * Type safety attributes * Documentation comment support More...

--- lang/clang/patches/patch-tools_clang_lib_Frontend_InitHeaderSearch.cpp
+++ lang/clang/patches/patch-tools_clang_lib_Frontend_InitHeaderSearch.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-tools_clang_lib_Frontend_InitHeaderSearch.cpp,v 1.1 2012/11/29 11:42:25 marino Exp $
+$NetBSD: patch-tools_clang_lib_Frontend_InitHeaderSearch.cpp,v 1.2 2013/01/03 15:19:53 adam Exp $
 
 DragonFly no longer has gcc 4.1 in base, so clang stopped working.
 We prefer to use gcc 4.7 if available due to a better libstdc++.
 The fallback is gcc 4.4 which has been available for several years.
 
---- tools/clang/lib/Frontend/InitHeaderSearch.cpp.orig	2012-03-25 22:46:17.000000000 +0000
+--- tools/clang/lib/Frontend/InitHeaderSearch.cpp.orig	2012-10-24 16:19:39.000000000 +0000
 +++ tools/clang/lib/Frontend/InitHeaderSearch.cpp
 @@ -24,6 +24,7 @@
  #include "llvm/ADT/Twine.h"
@@ -14,8 +14,8 @@ The fallback is gcc 4.4 which has been available for several years.
  #include "llvm/Support/Path.h"
  
  #include "clang/Config/config.h" // C_INCLUDE_DIRS
-@@ -395,7 +396,10 @@ AddDefaultCPlusPlusIncludePaths(const ll
-     AddMinGWCPlusPlusIncludePaths("c:/MinGW/lib/gcc", "mingw32", "4.3.0");
+@@ -408,7 +409,10 @@ AddDefaultCPlusPlusIncludePaths(const ll
+ #endif
      break;
    case llvm::Triple::DragonFly:
 -    AddPath("/usr/include/c++/4.1", CXXSystem, true, false, false);
