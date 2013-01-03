f960159a8ed6819a348716935c71be55f253972f
adam <adam@pkgsrc.org>
2013-01-03 21:23:04 +0000

    Changes 5.5.29: * The SHOW AUTHORS and SHOW CONTRIBUTORS statements are now deprecated in MySQL 5.5 and have been removed in MySQL 5.6. * Bugs Fixed

--- databases/mysql55-client/patches/patch-sql_log_event.cc
+++ databases/mysql55-client/patches/patch-sql_log_event.cc
@@ -1,13 +1,13 @@
-$NetBSD: patch-sql_log_event.cc,v 1.2 2012/10/01 15:54:40 adam Exp $
+$NetBSD: patch-sql_log_event.cc,v 1.3 2013/01/03 21:23:04 adam Exp $
 
 * Portability: include <bstring.h> if exists.
 
---- sql/log_event.cc.orig	2011-03-09 15:11:38.000000000 +0000
+--- sql/log_event.cc.orig	2012-12-10 06:16:24.000000000 +0000
 +++ sql/log_event.cc
-@@ -45,6 +45,9 @@
- #include "rpl_record.h"
+@@ -46,6 +46,9 @@
  #include "transaction.h"
  #include <my_dir.h>
+ #include "sql_show.h"    // append_identifier
 +#ifdef NEEDS_BSTRING_H
 +#include <bstring.h> /* defines bcmp() */
 +#endif
