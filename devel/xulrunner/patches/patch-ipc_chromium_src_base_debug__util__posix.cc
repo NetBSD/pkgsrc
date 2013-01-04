c7010a1268902907fc19cd196bf50dc5c3c4e0e3
ryoon <ryoon@pkgsrc.org>
2013-01-04 17:16:54 +0000

    Fix build on recent NetBSD 6.99.16. See http://cvsweb.netbsd.org/bsdweb.cgi/src/sys/sys/sysctl.h.diff?r1=1.204&r2=1.205&f=h&only_with_tag=MAIN&f=u
    Tested on NetBSD/i386 5.2 and NetBSD/amd64 current.
    
    Patch is provided from nonaka@. Thank you.

--- devel/xulrunner/patches/patch-ipc_chromium_src_base_debug__util__posix.cc
+++ devel/xulrunner/patches/patch-ipc_chromium_src_base_debug__util__posix.cc
@@ -1,8 +1,8 @@
-$NetBSD: patch-ipc_chromium_src_base_debug__util__posix.cc,v 1.1 2012/08/29 03:01:19 ryoon Exp $
+$NetBSD: patch-ipc_chromium_src_base_debug__util__posix.cc,v 1.2 2013/01/04 17:16:54 ryoon Exp $
 
 # Reported upstream: https://bugzilla.mozilla.org/show_bug.cgi?id=753046
 
---- ipc/chromium/src/base/debug_util_posix.cc.orig	2012-08-24 22:55:37.000000000 +0000
+--- ipc/chromium/src/base/debug_util_posix.cc.orig	2012-11-29 04:44:15.000000000 +0000
 +++ ipc/chromium/src/base/debug_util_posix.cc
 @@ -5,7 +5,7 @@
  #include "build/build_config.h"
@@ -39,7 +39,30 @@ $NetBSD: patch-ipc_chromium_src_base_debug__util__posix.cc,v 1.1 2012/08/29 03:0
  
  // Based on Apple's recommended method as described in
  // http://developer.apple.com/qa/qa2004/qa1361.html
-@@ -71,7 +78,15 @@ bool DebugUtil::BeingDebugged() {
+@@ -51,14 +58,22 @@ bool DebugUtil::BeingDebugged() {
+   // we're looking for information about a specific process ID.
+   int mib[] = {
+     CTL_KERN,
++#if defined(OS_NETBSD)
++    KERN_PROC2,
++#else
+     KERN_PROC,
++#endif
+     KERN_PROC_PID,
+     getpid()
+   };
+ 
+   // Caution: struct kinfo_proc is marked __APPLE_API_UNSTABLE.  The source and
+   // binary interfaces may change.
++#if defined(OS_NETBSD)
++  struct kinfo_proc2 info;
++#else
+   struct kinfo_proc info;
++#endif
+   size_t info_size = sizeof(info);
+ 
+   int sysctl_result = sysctl(mib, arraysize(mib), &info, &info_size, NULL, 0);
+@@ -71,7 +86,17 @@ bool DebugUtil::BeingDebugged() {
  
    // This process is being debugged if the P_TRACED flag is set.
    is_set = true;
@@ -48,6 +71,8 @@ $NetBSD: patch-ipc_chromium_src_base_debug__util__posix.cc,v 1.1 2012/08/29 03:0
 +#elif defined(OS_FREEBSD)
 +  being_debugged = (info.ki_flag & P_TRACED) != 0;
 +#elif defined(OS_OPENBSD)
++  being_debugged = (info.p_flag & P_TRACED) != 0;
++#elif defined(OS_NETBSD)
 +  being_debugged = (info.p_flag & P_TRACED) != 0;
 +#else
    being_debugged = (info.kp_proc.p_flag & P_TRACED) != 0;
