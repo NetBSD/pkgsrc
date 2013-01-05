ddde8b462d5166ae8fe36f8da1ebf4360d1f02b4
taca <taca@pkgsrc.org>
2013-01-05 13:30:32 +0000

    Start update of Ruby on Rails 3.0.18.

--- lang/ruby/rails.mk
+++ lang/ruby/rails.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rails.mk,v 1.31 2012/12/16 14:06:53 taca Exp $
+# $NetBSD: rails.mk,v 1.32 2013/01/05 13:30:32 taca Exp $
 
 .if !defined(_RUBY_RAILS_MK)
 _RUBY_RAILS_MK=	# defined
@@ -38,7 +38,7 @@ _RUBY_RAILS_MK=	# defined
 #
 # current Ruby on Rails versions.
 #
-RUBY_RAILS3_VERSION?=	3.0.17
+RUBY_RAILS3_VERSION?=	3.0.18
 RUBY_RAILS31_VERSION?=	3.1.8
 RUBY_RAILS32_VERSION?=	3.2.9
 
