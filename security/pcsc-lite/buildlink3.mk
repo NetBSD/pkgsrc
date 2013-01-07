aa635903e7eaec266e835f3634ca621d7a4a7a1f
pettai <pettai@pkgsrc.org>
2013-01-07 19:33:22 +0000

    Updated buildlink3.mk to newer API version

--- security/pcsc-lite/buildlink3.mk
+++ security/pcsc-lite/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2012/12/14 23:50:33 pettai Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/01/07 19:33:22 pettai Exp $
 
 BUILDLINK_TREE+=	pcsc-lite
 
 .if !defined(PCSC_LITE_BUILDLINK3_MK)
 PCSC_LITE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.pcsc-lite+=	pcsc-lite>=1.6.5
+BUILDLINK_API_DEPENDS.pcsc-lite+=	pcsc-lite>=1.8.3
 BUILDLINK_PKGSRCDIR.pcsc-lite?=	../../security/pcsc-lite
 
 .include "../../devel/libusb1/buildlink3.mk"
