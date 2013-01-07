2d61142a52ff8a84bf14bc9273ec584f70612d59
ryoon <ryoon>
2013-01-07 21:55:29 +0000

    * Regen patches... * Fix build on recent NetBSD (kproc_info vs kproc_info2)

--- www/seamonkey/patches/patch-mozilla_ipc_chromium_src_base_process__util__bsd.cc
+++ www/seamonkey/patches/patch-mozilla_ipc_chromium_src_base_process__util__bsd.cc
@@ -1,6 +1,6 @@
-$NetBSD: patch-mozilla_ipc_chromium_src_base_process__util__bsd.cc,v 1.7 2012/11/23 17:28:49 ryoon Exp $
+$NetBSD: patch-mozilla_ipc_chromium_src_base_process__util__bsd.cc,v 1.8 2013/01/07 21:55:30 ryoon Exp $
 
---- mozilla/ipc/chromium/src/base/process_util_bsd.cc.orig	2012-11-23 10:13:59.000000000 +0000
+--- mozilla/ipc/chromium/src/base/process_util_bsd.cc.orig	2012-11-23 07:38:07.000000000 +0000
 +++ mozilla/ipc/chromium/src/base/process_util_bsd.cc
 @@ -0,0 +1,367 @@
 +// Copyright (c) 2008 The Chromium Authors. All rights reserved.
