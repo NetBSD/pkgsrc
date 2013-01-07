0f1ef9fb8bb2e1f10ca1f5379552438d1db826d0
taca <taca@pkgsrc.org>
2013-01-07 14:23:57 +0000

    Update php53 to 5.3.20.
    20 Dec 2012, PHP 5.3.20
    
    - Zend Engine:
      . Fixed bug #63635 (Segfault in gc_collect_cycles). (Dmitry)
      . Fixed bug #63512 (parse_ini_file() with INI_SCANNER_RAW removes quotes
        from value). (Pierrick)
      . Fixed bug #63468 (wrong called method as callback with inheritance).
        (Laruence)
    
    - Core:
      . Fixed bug #63451 (config.guess file does not have AIX 7 defined,
        shared objects are not created). (kemcline at au1 dot ibm dot com)
      . Fixed bug #63377 (Segfault on output buffer).
        (miau dot jp at gmail dot com, Laruence)
    
    - Apache2 Handler SAPI:
      . Enabled Apache 2.4 configure option for Windows (Pierre, Anatoliy)
    
    - Date:
      . Fixed bug #63435 (Datetime::format('u') sometimes wrong by 1 microsecond).
        (Remi)
    
    - Fileinfo:
      . Fixed bug #63248 (Load multiple magic files from a directory under Windows).
        (Anatoliy)
      . Fixed bug #63590 (Different results in TS and NTS under Windows).
        (Anatoliy)
    
    - FPM:
      . Fixed bug #63581 (Possible null dereference and buffer overflow). (Remi)
    
    - Imap:
      . Fixed bug #63126 (DISABLE_AUTHENTICATOR ignores array). (Remi)
    
    - MySQLnd:
      . Fixed bug #63398 (Segfault when polling closed link). (Laruence)
    
    - Reflection:
      . Fixed Bug #63614 (Fatal error on Reflection). (Laruence)
    
    - SOAP
      . Fixed bug #63271 (SOAP wsdl cache is not enabled after initial requests).
        (John Jawed, Dmitry)

--- lang/php53/Makefile.php
+++ lang/php53/Makefile.php
@@ -1,4 +1,4 @@
-# $NetBSD: Makefile.php,v 1.26 2012/11/23 13:20:03 taca Exp $
+# $NetBSD: Makefile.php,v 1.27 2013/01/07 14:23:57 taca Exp $
 # used by lang/php53/Makefile
 # used by www/ap-php/Makefile
 
@@ -58,7 +58,7 @@ PKG_SUGGESTED_OPTIONS+=	inet6 ssl
 
 .if !empty(PKG_OPTIONS:Msuhosin)
 SUHOSIN_PHPVER=		5.3.9
-.  if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS} && ${PHP_BASE_VERS} != 5.3.19
+.  if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS} && ${PHP_BASE_VERS} != 5.3.20
 PKG_FAIL_REASON+=	"The suhosin patch is currently not available for"
 PKG_FAIL_REASON+=	"this version of PHP.  You may have to wait until"
 PKG_FAIL_REASON+=	"an updated patch is released or temporarily"
