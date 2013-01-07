00d165d7ce0a8e19b8976e000e7bdabdc43ef5d4
wiz <wiz@pkgsrc.org>
2013-01-07 13:26:41 +0000

    Update to 3.4.1, based on othyro's package from wip. This version compiles with tcl-8.5.

--- lang/tcl-itcl/buildlink3.mk
+++ lang/tcl-itcl/buildlink3.mk
@@ -1,16 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.11 2012/08/21 23:49:23 marino Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2013/01/07 13:26:41 wiz Exp $
 
 BUILDLINK_TREE+=	tcl-itcl
 
 .if !defined(TCL_ITCL_BUILDLINK3_MK)
 TCL_ITCL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.tcl-itcl+=	tcl-itcl>=3.2.1
-BUILDLINK_ABI_DEPENDS.tcl-itcl+=	tcl-itcl>=3.2.1nb2
+BUILDLINK_API_DEPENDS.tcl-itcl+=	tcl-itcl>=3.4.1
 BUILDLINK_PKGSRCDIR.tcl-itcl?=	../../lang/tcl-itcl
 
-.include "../../lang/tcl/buildlink3.mk"
 .include "../../x11/tk/buildlink3.mk"
-.endif # TCL_ITCL_BUILDLINK3_MK
+.endif	# TCL_ITCL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-tcl-itcl
