4e31c273af48d39d75e6cc727506a82b8fe6dd78
ryoon <ryoon>
2013-01-10 16:17:09 +0000

    Import xulrunner17-17.0.2 as devel/xulrunner17.
    XULRunner is a runtime environment for applications using the
    XML User Interface Language, XUL. It is the successor of the "Gecko"
    runtime environment.
    
    This package tracks 17.0.x extended support release.

---
+++ devel/xulrunner17/patches/patch-media_libsydneyaudio_src_sydney_audio_pulseaudio.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-media_libsydneyaudio_src_sydney_audio_pulseaudio.c,v 1.1 2013/01/10 16:17:10 ryoon Exp $
+
+--- media/libsydneyaudio/src/sydney_audio_pulseaudio.c.orig	2012-08-24 22:55:55.000000000 +0000
++++ media/libsydneyaudio/src/sydney_audio_pulseaudio.c
+@@ -6,7 +6,9 @@
+ #include <stdio.h>
+ #include <string.h>
+ #include <pthread.h>
++_Pragma("GCC visibility push(default)")
+ #include <pulse/pulseaudio.h>
++_Pragma("GCC visibility pop")
+ #include "sydney_audio.h"
+ 
+ /* Pulseaudio implementation based heavily on sydney_audio_alsa.c */
