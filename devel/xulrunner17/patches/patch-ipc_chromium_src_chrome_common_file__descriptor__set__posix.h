1a5df8ed9fa80bdab5e9594f6bcf66d0ffc36b3e
ryoon <ryoon>
2013-01-10 16:17:09 +0000

    Import xulrunner17-17.0.2 as devel/xulrunner17.
    XULRunner is a runtime environment for applications using the
    XML User Interface Language, XUL. It is the successor of the "Gecko"
    runtime environment.
    
    This package tracks 17.0.x extended support release.

---
+++ devel/xulrunner17/patches/patch-ipc_chromium_src_chrome_common_file__descriptor__set__posix.h
@@ -0,0 +1,13 @@
+$NetBSD: patch-ipc_chromium_src_chrome_common_file__descriptor__set__posix.h,v 1.1 2013/01/10 16:17:10 ryoon Exp $
+
+--- ipc/chromium/src/chrome/common/file_descriptor_set_posix.h.orig	2012-08-24 22:55:37.000000000 +0000
++++ ipc/chromium/src/chrome/common/file_descriptor_set_posix.h
+@@ -30,7 +30,7 @@ class FileDescriptorSet : public base::R
+   // In debugging mode, it's a fatal error to try and add more than this number
+   // of descriptors to a FileDescriptorSet.
+   enum {
+-    MAX_DESCRIPTORS_PER_MESSAGE = 4,
++    MAX_DESCRIPTORS_PER_MESSAGE = 4
+   };
+ 
+   // ---------------------------------------------------------------------------
