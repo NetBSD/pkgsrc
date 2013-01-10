98c40df8f016655395b915756a4a189b9204e330
ryoon <ryoon@pkgsrc.org>
2013-01-10 15:01:29 +0000

    Update to 18.0
    * Many patches are included into upstream distribution.
    
    I have tested on NetBSD/amd64 6.99.16, I will test on other platforms later.
    
    Changelog: from http://www.mozilla.org/en-US/firefox/18.0/releasenotes/
        NEW
        Faster JavaScript performance via IonMonkey compiler
        NEW
        Support for Retina Display on OS X 10.7 and up
        NEW
        Preliminary support for WebRTC
        CHANGED
        Experience better image quality with our new HTML scaling algorithm
        CHANGED
        Performance improvements around tab switching
        DEVELOPER
        Support for new DOM property window.devicePixelRatio
        DEVELOPER
        Improvement in startup time through smart handling of signed extension certificates
        HTML5
        Support for W3C touch events implemented, taking the place of MozTouch events
        FIXED
        Disable insecure content loading on HTTPS pages (62178)
        FIXED
        Improved responsiveness for users on proxies (769764)

--- devel/xulrunner/patches/patch-netwerk_protocol_http_HttpChannelParent.cpp
+++ devel/xulrunner/patches/patch-netwerk_protocol_http_HttpChannelParent.cpp
@@ -1,11 +1,11 @@
-$NetBSD: patch-netwerk_protocol_http_HttpChannelParent.cpp,v 1.4 2012/08/28 23:27:10 ryoon Exp $
+$NetBSD: patch-netwerk_protocol_http_HttpChannelParent.cpp,v 1.5 2013/01/10 15:01:31 ryoon Exp $
 
 * Fix build with gcc<4.5, for example, DragonFly 3.0.1.
   See https://bugzilla.mozilla.org/show_bug.cgi?id=621446
 
---- netwerk/protocol/http/HttpChannelParent.cpp.orig	2012-08-24 22:55:58.000000000 +0000
+--- netwerk/protocol/http/HttpChannelParent.cpp.orig	2013-01-04 23:44:42.000000000 +0000
 +++ netwerk/protocol/http/HttpChannelParent.cpp
-@@ -299,6 +299,13 @@ HttpChannelParent::RecvUpdateAssociatedC
+@@ -334,6 +334,13 @@ HttpChannelParent::RecvUpdateAssociatedC
    return true;
  }
  
