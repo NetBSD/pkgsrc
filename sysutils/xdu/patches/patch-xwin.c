33a8079c2c8586bbe6bc881ae94217f362fc6dd7
joerg <joerg@pkgsrc.org>
2013-01-11 23:32:38 +0000

    Use more void.

---
+++ sysutils/xdu/patches/patch-xwin.c
@@ -0,0 +1,24 @@
+$NetBSD: patch-xwin.c,v 1.1 2013/01/11 23:32:39 joerg Exp $
+
+--- xwin.c.orig	2013-01-11 14:30:59.000000000 +0000
++++ xwin.c
+@@ -50,7 +50,6 @@ extern int xmainloop();
+ extern int xclear();
+ extern int xrepaint();
+ extern int xrepaint_noclear();
+-extern int xdrawrect();
+ 
+ /* internal routines */
+ static void help_popup();
+@@ -377,10 +376,7 @@ xrepaint_noclear()
+ 	repaint(xwa.width, xwa.height);
+ }
+ 
+-xdrawrect(name, size, x, y, width, height)
+-char *name;
+-int size;
+-int x, y, width, height;
++void xdrawrect(char *name, int size, int x, int y, int width, int height)
+ {
+ 	int	textx, texty;
+ 	char	label[1024];
