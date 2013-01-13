ae209070a3811a5ce31a88401f4a2da3bca9ccef
tron <tron@pkgsrc.org>
2013-01-13 13:21:18 +0000

    Simplify code to select BDB version. "db1" gets excluded on Solaris 11 automatically anyway. This finally fixes PR pkg/47420 by Richard Palo.

--- mail/milter-greylist/options.mk
+++ mail/milter-greylist/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.10 2013/01/12 15:58:53 tron Exp $
+# $NetBSD: options.mk,v 1.11 2013/01/13 13:21:18 tron Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.milter-greylist
 PKG_OPTIONS_REQUIRED_GROUPS=	mta
@@ -34,11 +34,6 @@ DRACD_DB?=		/etc/mail/dracd.db
 
 CONFIGURE_ARGS+=	--enable-drac --with-drac-db=${DRACD_DB}
 
-USE_DB185=	yes
-.  if ${OPSYS} == "SunOS"
-BDB_ACCEPTED=	db2 db3 db4 db5
-BDB185_DEFAULT=	${BDB_DEFAULT}
-.  endif
 .  include "../../mk/bdb.buildlink3.mk"
 .  if ${BDB_TYPE} != "db1"
 CPPFLAGS+=	-DUSE_DB185_EMULATION
