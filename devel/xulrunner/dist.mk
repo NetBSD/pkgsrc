c54971c7c9c5ec52fc67adaf6e6207b3a17400bd
tron <tron@pkgsrc.org>
2013-01-13 17:44:58 +0000

    Apply patch submitted by Ryo ONODERA in ticket #4010: Update "firefox" and "xulrunner" package to version 17.0.2. This fixes the following security vulnerabilities: - MFSA 2013-20 Mis-issued TURKTRUST certificates - MFSA 2013-19 Use-after-free in Javascript Proxy objects - MFSA 2013-18 Use-after-free in Vibrate - MFSA 2013-17 Use-after-free in ListenerManager - MFSA 2013-16 Use-after-free in serializeToStream - MFSA 2013-15 Privilege escalation through plugin objects - MFSA 2013-14 Chrome Object Wrapper (COW) bypass through changing prototype - MFSA 2013-13 Memory corruption in XBL with XML bindings containing SVG - MFSA 2013-12 Buffer overflow in Javascript string concatenation - MFSA 2013-11 Address space layout leaked in XBL objects - MFSA 2013-10 Event manipulation in plugin handler to bypass same-origin   policy - MFSA 2013-09 Compartment mismatch with quickstubs returned values - MFSA 2013-08 AutoWrapperChanger fails to keep objects alive during   garbage collection - MFSA 2013-07 Crash due to handling of SSL on threads - MFSA 2013-05 Use-after-free when displaying table with many columns and   column groups - MFSA 2013-04 URL spoofing in addressbar during page loads - MFSA 2013-03 Buffer Overflow in Canvas - MFSA 2013-02 Use-after-free and buffer overflow issues found using   Address Sanitizer - MFSA 2013-01 Miscellaneous memory safety hazards (rv:18.0/   rv:10.0.12 / rv:17.0.2) - MFSA 2012-98 Firefox installer DLL hijacking

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,11 +1,11 @@
-# $NetBSD: dist.mk,v 1.39 2012/12/05 11:47:14 ryoon Exp $
+# $NetBSD: dist.mk,v 1.39.2.1 2013/01/13 17:44:58 tron Exp $
 #
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	17.0.1
+MOZ_BRANCH=	17.0.2
 MOZ_BRANCH_MINOR=	esr
 MASTER_SITES=	${MASTER_SITE_MOZILLA_ESR:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
@@ -13,5 +13,7 @@ EXTRACT_SUFX=	.tar.bz2
 
 DISTINFO_FILE=	${.CURDIR}/../../devel/xulrunner/distinfo
 PATCHDIR=	${.CURDIR}/../../devel/xulrunner/patches
+
+DIST_SUBDIR=	firefox17.0.2esr
 
 WRKSRC=		${WRKDIR}/mozilla-esr17
