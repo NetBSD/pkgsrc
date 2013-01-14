4870b25ee6774ac5c8840644fa428d0bb32ec561
tron <tron@pkgsrc.org>
2013-01-14 13:34:10 +0000

    Pullup ticket #4016 - requested by wiz lang/tcl-itcl: build fix update
    Revisions pulled up:
    - cad/tnt-mmtl/Makefile                                         1.20
    - lang/tcl-itcl/DESCR                                           1.3
    - lang/tcl-itcl/Makefile                                        1.25
    - lang/tcl-itcl/PLIST                                           1.7
    - lang/tcl-itcl/buildlink3.mk                                   1.12
    - lang/tcl-itcl/distinfo                                        1.10
    - lang/tcl-itcl/patches/patch-aa                                deleted
    - lang/tcl-itcl/patches/patch-ab                                deleted
    - lang/tcl-itcl/patches/patch-ac                                deleted
    - lang/tcl-itcl/patches/patch-ad                                deleted
    - lang/tcl-itcl/patches/patch-ag                                deleted
    - lang/tcl-itcl/patches/patch-ah                                deleted
    - sysutils/tkdesk/Makefile                                      1.21
    
    ---
       Module Name:	pkgsrc
       Committed By:	wiz
       Date:		Mon Jan  7 13:26:41 UTC 2013
    
       Modified Files:
       	pkgsrc/lang/tcl-itcl: DESCR Makefile PLIST buildlink3.mk distinfo
    
       Log Message:
       Update to 3.4.1, based on othyro's package from wip.
       This version compiles with tcl-8.5.
    
    ---
       Module Name:	pkgsrc
       Committed By:	wiz
       Date:		Mon Jan  7 13:27:04 UTC 2013
    
       Modified Files:
       	pkgsrc/cad/tnt-mmtl: Makefile
    
       Log Message:
       Use tcl-itcl, bump PKGREVISION.
    
    ---
       Module Name:	pkgsrc
       Committed By:	wiz
       Date:		Mon Jan  7 13:27:29 UTC 2013
    
       Modified Files:
       	pkgsrc/sysutils/tkdesk: Makefile
    
       Log Message:
       Bump PKGREVISION for tcl-itcl shlib path change.
    
    ---
       Module Name:	pkgsrc
       Committed By:	wiz
       Date:		Mon Jan  7 13:28:30 UTC 2013
    
       Removed Files:
       	pkgsrc/lang/tcl-itcl/patches: patch-aa patch-ab patch-ac patch-ad
       	    patch-ag patch-ah
    
       Log Message:
       Remove unused patches (after 3.4.1 update)

--- lang/tcl-itcl/buildlink3.mk
+++ lang/tcl-itcl/buildlink3.mk
@@ -1,16 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.11 2012/08/21 23:49:23 marino Exp $
+# $NetBSD: buildlink3.mk,v 1.11.4.1 2013/01/14 13:34:11 tron Exp $
 
 BUILDLINK_TREE+=	tcl-itcl
 
 .if !defined(TCL_ITCL_BUILDLINK3_MK)
 TCL_ITCL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.tcl-itcl+=	tcl-itcl>=3.2.1
-BUILDLINK_ABI_DEPENDS.tcl-itcl+=	tcl-itcl>=3.2.1nb2
+BUILDLINK_API_DEPENDS.tcl-itcl+=	tcl-itcl>=3.4.1
 BUILDLINK_PKGSRCDIR.tcl-itcl?=	../../lang/tcl-itcl
 
-.include "../../lang/tcl/buildlink3.mk"
 .include "../../x11/tk/buildlink3.mk"
-.endif # TCL_ITCL_BUILDLINK3_MK
+.endif	# TCL_ITCL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-tcl-itcl
