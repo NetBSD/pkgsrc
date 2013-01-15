5e56d5f3d6220257255043dec01e3cadff53bc8b
joerg <joerg@pkgsrc.org>
2013-01-15 15:32:59 +0000

    Don't add default arguments during method implementation.

--- wm/3ddesktop/patches/patch-config.cpp
+++ wm/3ddesktop/patches/patch-config.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-config.cpp,v 1.1 2011/11/25 22:20:28 joerg Exp $
+$NetBSD: patch-config.cpp,v 1.2 2013/01/15 15:32:59 joerg Exp $
 
---- config.cpp.orig	2011-11-25 13:25:07.000000000 +0000
+--- config.cpp.orig	2005-06-20 11:20:44.000000000 +0000
 +++ config.cpp
 @@ -19,6 +19,7 @@
  //  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -10,3 +10,12 @@ $NetBSD: patch-config.cpp,v 1.1 2011/11/25 22:20:28 joerg Exp $
  #include "config.hpp"
  
  static void 
+@@ -64,7 +65,7 @@ get_boolean (char *value) 
+ }
+ 
+ 
+-Options::Options(char *n = NULL) 
++Options::Options(char *n) 
+ {
+ 
+     if (n)
