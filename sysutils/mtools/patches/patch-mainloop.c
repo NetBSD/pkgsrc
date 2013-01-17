536e64a43ffa8f4ccbd38d976e921d9967f9db84
joerg <joerg@pkgsrc.org>
2013-01-17 16:03:45 +0000

    Fix unused attribute usage.

---
+++ sysutils/mtools/patches/patch-mainloop.c
@@ -0,0 +1,22 @@
+$NetBSD: patch-mainloop.c,v 1.1 2013/01/17 16:03:46 joerg Exp $
+
+--- mainloop.c.orig	2013-01-15 16:21:14.000000000 +0000
++++ mainloop.c
+@@ -86,7 +86,7 @@ static const char *fix_mcwd(char *ans)
+ }
+ 
+ int unix_dir_loop(Stream_t *Stream, MainParam_t *mp); 
+-int unix_loop(UNUSED(Stream_t *Stream), MainParam_t *mp, char *arg,
++int unix_loop(Stream_t *Stream UNUSEDP, MainParam_t *mp, char *arg,
+ 	      int follow_dir_link);
+ 
+ static int _unix_loop(Stream_t *Dir, MainParam_t *mp,
+@@ -95,7 +95,7 @@ static int _unix_loop(Stream_t *Dir, Mai
+ 	return unix_dir_loop(Dir, mp);
+ }
+ 
+-int unix_loop(UNUSED(Stream_t *Stream), MainParam_t *mp,
++int unix_loop(Stream_t *Stream, MainParam_t *mp,
+ 	      char *arg, int follow_dir_link)
+ {
+ 	int ret;
