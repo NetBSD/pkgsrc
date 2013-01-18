afd64fdd032bc7f1198ff652166a059c08eff4e1
tron <tron@pkgsrc.org>
2013-01-18 12:18:31 +0000

    Pullup ticket #4019 - requested by taca databases/ruby-activerecord3: security update devel/ruby-activemodel: security update devel/ruby-activesupport3: security update devel/ruby-railties: security update mail/ruby-actionmailer3: security update www/ruby-actionpack3: security update www/ruby-activeresource3: security update www/ruby-rails3: security update
    Revisions pulled up:
    - databases/ruby-activerecord3/distinfo                         1.16
    - devel/ruby-activemodel/distinfo                               1.16
    - devel/ruby-activesupport3/distinfo                            1.17
    - devel/ruby-railties/distinfo                                  1.16
    - lang/ruby/rails.mk                                            1.32
    - mail/ruby-actionmailer3/distinfo                              1.18
    - www/ruby-actionpack3/distinfo                                 1.17
    - www/ruby-activeresource3/distinfo                             1.16
    - www/ruby-rails3/distinfo                                      1.17
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 13:30:33 UTC 2013
    
       Modified Files:
       	pkgsrc/lang/ruby: rails.mk
    
       Log Message:
       Start update of Ruby on Rails 3.0.18.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 13:32:15 UTC 2013
    
       Modified Files:
       	pkgsrc/devel/ruby-activesupport3: distinfo
    
       Log Message:
       Update ruby-activesupport3 to 3.0.18.
    
       Only version has updated.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 13:34:18 UTC 2013
    
       Modified Files:
       	pkgsrc/devel/ruby-activemodel: distinfo
    
       Log Message:
       Update ruby-activemodel to 3.0.18.
    
       Only version has updated.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 13:35:08 UTC 2013
    
       Modified Files:
       	pkgsrc/www/ruby-activeresource3: distinfo
    
       Log Message:
       Update ruby-activeresource3 to 3.0.18.
    
       Only version has updated.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 13:36:01 UTC 2013
    
       Modified Files:
       	pkgsrc/www/ruby-actionpack3: distinfo
    
       Log Message:
       Update ruby-actionpack3 to 3.0.18.
    
       Only version has updated.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 13:36:46 UTC 2013
    
       Modified Files:
       	pkgsrc/databases/ruby-activerecord3: distinfo
    
       Log Message:
       Update ruby-activerecord3 to 3.0.18.
    
       ## Rails 3.0.18
    
       * CVE-2012-5664 ensure that options are never taken from the first parameter.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 13:41:42 UTC 2013
    
       Modified Files:
       	pkgsrc/mail/ruby-actionmailer3: distinfo
    
       Log Message:
       Update ruby-actionmailer3 to 3.0.18.
    
       Only version has updated.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 13:43:15 UTC 2013
    
       Modified Files:
       	pkgsrc/devel/ruby-railties: distinfo
    
       Log Message:
       Update ruby-railties to 3.0.18.
    
       Only version has updated.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 13:43:39 UTC 2013
    
       Modified Files:
       	pkgsrc/www/ruby-rails3: distinfo
    
       Log Message:
       Update ruby-rails3 to 3.0.18.
    
       Only version has updated.

--- lang/ruby/rails.mk
+++ lang/ruby/rails.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rails.mk,v 1.31 2012/12/16 14:06:53 taca Exp $
+# $NetBSD: rails.mk,v 1.31.2.1 2013/01/18 12:18:31 tron Exp $
 
 .if !defined(_RUBY_RAILS_MK)
 _RUBY_RAILS_MK=	# defined
@@ -38,7 +38,7 @@ _RUBY_RAILS_MK=	# defined
 #
 # current Ruby on Rails versions.
 #
-RUBY_RAILS3_VERSION?=	3.0.17
+RUBY_RAILS3_VERSION?=	3.0.18
 RUBY_RAILS31_VERSION?=	3.1.8
 RUBY_RAILS32_VERSION?=	3.2.9
 
