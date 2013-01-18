be5c5e719524170fa066e49c48c4b4282730f71b
tron <tron>
2013-01-18 13:22:47 +0000

    Pullup ticket #4020 - requested by taca databases/ruby-activerecord31: security update devel/ruby-activemodel31: security update devel/ruby-activesupport31: security update devel/ruby-railties31: security update mail/ruby-actionmailer31: security update www/ruby-actionpack31: security update www/ruby-activeresource31: security update www/ruby-rails31: security update
    Revisions pulled up:
    - databases/ruby-activerecord31/distinfo                        1.7
    - devel/ruby-activemodel31/distinfo                             1.7
    - devel/ruby-activesupport31/distinfo                           1.8
    - devel/ruby-railties31/PLIST                                   1.3
    - devel/ruby-railties31/distinfo                                1.7
    - lang/ruby/rails.mk                                            1.33
    - mail/ruby-actionmailer31/distinfo                             1.7
    - www/ruby-actionpack31/distinfo                                1.8
    - www/ruby-activeresource31/distinfo                            1.7
    - www/ruby-rails31/distinfo                                     1.7
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 13:57:10 UTC 2013
    
       Modified Files:
       	pkgsrc/lang/ruby: rails.mk
    
       Log Message:
       Start update of Ruby on Rails 3.1.9.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 13:57:50 UTC 2013
    
       Modified Files:
       	pkgsrc/devel/ruby-activesupport31: distinfo
    
       Log Message:
       Update ruby-activesupport31 to 3.1.9.
    
       Only version has updated.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 13:58:23 UTC 2013
    
       Modified Files:
       	pkgsrc/devel/ruby-activemodel31: distinfo
    
       Log Message:
       Update ruby-activemodel31 to 3.1.9.
    
       Only version has updated.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 13:58:57 UTC 2013
    
       Modified Files:
       	pkgsrc/www/ruby-activeresource31: distinfo
    
       Log Message:
       Update ruby-activeresource31 to 3.1.9.
    
       Only version has updated.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 13:59:40 UTC 2013
    
       Modified Files:
       	pkgsrc/databases/ruby-activerecord31: distinfo
    
       Log Message:
       Update ruby-activerecord31 to 3.1.9.
    
       ## Rails 3.1.9
    
       *  CVE-2012-5664 ensure that options are never taken from the first parameter.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 14:00:19 UTC 2013
    
       Modified Files:
       	pkgsrc/www/ruby-actionpack31: distinfo
    
       Log Message:
       Update ruby-actionpack31 to 3.1.9.
    
       Only version has updated.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 14:00:49 UTC 2013
    
       Modified Files:
       	pkgsrc/mail/ruby-actionmailer31: distinfo
    
       Log Message:
       Update ruby-actionmailer31 to 3.1.9.
    
       Only version has updated.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 14:01:33 UTC 2013
    
       Modified Files:
       	pkgsrc/devel/ruby-railties31: PLIST distinfo
    
       Log Message:
       Update ruby-railties31 to 3.1.9.
    
       Only version has updated.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan  5 14:02:04 UTC 2013
    
       Modified Files:
       	pkgsrc/www/ruby-rails31: distinfo
    
       Log Message:
       Update ruby-rails31 to 3.1.9.
    
       Only version has updated.

--- lang/ruby/rails.mk
+++ lang/ruby/rails.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rails.mk,v 1.31.2.1 2013/01/18 12:18:31 tron Exp $
+# $NetBSD: rails.mk,v 1.31.2.2 2013/01/18 13:22:48 tron Exp $
 
 .if !defined(_RUBY_RAILS_MK)
 _RUBY_RAILS_MK=	# defined
@@ -39,7 +39,7 @@ _RUBY_RAILS_MK=	# defined
 # current Ruby on Rails versions.
 #
 RUBY_RAILS3_VERSION?=	3.0.18
-RUBY_RAILS31_VERSION?=	3.1.8
+RUBY_RAILS31_VERSION?=	3.1.9
 RUBY_RAILS32_VERSION?=	3.2.9
 
 RUBY_RAILS_SUPPORTED?=	# defined
