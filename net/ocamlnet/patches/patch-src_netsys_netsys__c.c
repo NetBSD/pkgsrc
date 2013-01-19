c5f5fdbaa8d02f597c5dd24536248293693d5492
riastradh <riastradh@pkgsrc.org>
2013-01-19 20:37:43 +0000

    Fix net/ocamlnet's AT_* flag build breakage on NetBSD.

--- net/ocamlnet/patches/patch-src_netsys_netsys__c.c
+++ net/ocamlnet/patches/patch-src_netsys_netsys__c.c
@@ -1,12 +1,25 @@
-$NetBSD: patch-src_netsys_netsys__c.c,v 1.2 2012/07/18 21:56:05 wiz Exp $
+$NetBSD: patch-src_netsys_netsys__c.c,v 1.3 2013/01/19 20:37:44 riastradh Exp $
 
 DragonFly doesn't define AT_SYMLINK_FOLLOW.  The added patch will
-appropriately disable linkat support.
+at least unbreak the build for some code that obviously won't work.
 https://sourceforge.net/tracker/?func=detail&aid=3545170&group_id=19774&atid=319774
 
---- src/netsys/netsys_c.c.orig	2012-02-29 18:02:52.000000000 +0000
+Also work around absence of AT_REMOVEDIR, as on NetBSD 6.0.
+
+--- src/netsys/netsys_c.c.orig	2012-07-19 23:25:25.000000000 +0000
 +++ src/netsys/netsys_c.c
-@@ -528,7 +528,7 @@ CAMLprim value netsys_renameat(value old
+@@ -428,6 +428,10 @@ CAMLprim value netsys_at_fdcwd(value dum
+ #define AT_SYMLINK_NOFOLLOW 0
+ #endif
+ 
++#ifndef AT_REMOVEDIR
++#define AT_REMOVEDIR 0
++#endif
++
+ static int at_flags_table[] = {
+     AT_EACCESS, AT_SYMLINK_NOFOLLOW, AT_REMOVEDIR
+ };
+@@ -528,7 +532,7 @@ CAMLprim value netsys_renameat(value old
  CAMLprim value netsys_linkat(value olddirfd, value oldpath,
  			     value newdirfd, value newpath, value flags)
  {
