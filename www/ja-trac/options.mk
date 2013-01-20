72ec7f8314fdf305707a4ce38195eadb2ccbefa9
obache <obache>
2013-01-20 08:56:48 +0000

    Update ja-trac to 1.0pl1, aka 1.0.ja1.
    Trac-1.0.ja1 (Oct 4, 2012)
     * Based on Trac 1.0 'Cell'
     * Change document and parameters for this distribution.
       - *.trac-ja
       - setup.py
       - trac/templates/about.html
       - trac/templates/theme.html
     * Translate default Wiki pages into Japanese.
       - trac/wiki/default-pages/*
     * Translate default reports into Japanese.
       - trac/db_default.py
     * Translate default workflow display into Japanese.
      * trac/ticket/templates/report.rss
      * trac/ticket/templates/report_view.html
     * [Patch] Add formatting for report using Japanese column name.
       - trac/ticket/report.py
       - trac/ticket/templates/report.rss
       - trac/ticket/templates/report_view.html
     * [Patch] Fix a bug that local time will not be applied
       when downloading repository files as Zip.
       - trac/versioncontrol/web_ui/changeset.py
     * [Patch] Small patch for Unicode
       - trac/about.py
     * [Patch] Fix based on ticket #10877.
      -trac/util/text.py
      -trac/util/test/text.py
    
    Trac 1.0 'Cell' (September 7, 2012)
    http://svn.edgewall.org/repos/trac/tags/trac-1.0
    
    Trac 1.0 is a major release adding refreshed user interface and
    improved DVCS repository support as the most visible changes.
    
    The following list contains only a few highlights:
    
     - The default theme looks more modern, especially on recent browsers
       (no effort has been made to make it look better on older browsers
       like IE6 or 7)
     - The TracHacks GitPlugin has been donated by Herbert Valerio Riedel
       to the Trac project (many thanks!) and is now maintained here as an
       optional component
     - As a consequence, the Subversion support has been moved below
       `tracopt.versioncontrol` as well
     - The Git and Mercurial log view feature a visualization of the
       branching structure
     - Usability improvements for the tickets, with a better support for
       conflict detection and resolution
     - Integration of the TracHacks BatchModifyPlugin, contributed by
       Brian Meeker (many thanks!) and is now maintained there as a
       default component
     - jQuery/UI integration, featuring a date picker for date fields
     - Improved integration with Pygments syntax highlighting
     - ... and numerous smaller features added and bugs fixed since 0.12!

--- www/ja-trac/options.mk
+++ www/ja-trac/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2011/03/15 06:04:03 obache Exp $
+# $NetBSD: options.mk,v 1.3 2013/01/20 08:56:48 obache Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.trac
 PKG_OPTIONS_REQUIRED_GROUPS=	db
@@ -9,11 +9,11 @@ PKG_OPTIONS_LEGACY_OPTS=	psycopg2:pgsql
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Msqlite)
-DEPENDS+=	${PYPKGPREFIX}-sqlite2>=2.3.2:../../databases/py-sqlite2
+DEPENDS+=	${PYPKGPREFIX}-sqlite2>=2.5.5:../../databases/py-sqlite2
 .endif
 .if !empty(PKG_OPTIONS:Mpgsql)
 DEPENDS+=	${PYPKGPREFIX}-psycopg2>=2:../../databases/py-psycopg2
 .endif
 .if !empty(PKG_OPTIONS:Mmysql)
-DEPENDS+=	${PYPKGPREFIX}-mysqldb>=1.2.1:../../databases/py-mysqldb
+DEPENDS+=	${PYPKGPREFIX}-mysqldb>=1.2.2:../../databases/py-mysqldb
 .endif
