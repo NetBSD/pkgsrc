812d21106b5c05e088dfcfacb7228315e4cb4df8
ryoon <ryoon@pkgsrc.org>
2013-01-20 11:17:23 +0000

    Update to 18.0.1
    Changelog:
    FIXED
    18.0.1: Problems involving HTTP Proxy Transactions (Associated bugs)
    FIXED
    18.0.1: Unity player crashes on Mac OS X (bug 828954)
    FIXED
    18.0.1: Disabled HIDPI support on external monitors to avoid rendering glitches (bug 814434)

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.40 2013/01/10 15:01:30 ryoon Exp $
+# $NetBSD: dist.mk,v 1.41 2013/01/20 11:17:23 ryoon Exp $
 #
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
@@ -6,7 +6,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	18.0
-MOZ_BRANCH_MINOR=
+MOZ_BRANCH_MINOR=	.1
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
