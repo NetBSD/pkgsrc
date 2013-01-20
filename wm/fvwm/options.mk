c41bf1d072a284e6a56f27b1bd12fb2b7694d6b6
mef <mef@pkgsrc.org>
2013-01-20 06:53:53 +0000

    (1) Bump version  2.4.20 to 2.6.5 based on PR pkg/47409.      The changes between them are too big, see NEWS      in source tar ball. The release dates in between      are recorded below.
    (2) patches/patch-a[ace] are removed, assuming
        they are included in upstream.
    
    (3) patches/patch-configure is added for modifying
        install path from fvwm to fvwm2.
    
    (4) patches/patch-ad: Add comment lines.
    
    (5) Add options xrender xcursor xft2, thanks
        Vitaly Shevtsov.
    
                    CVS HEAD (20-Apr-2012)
        stable release 2.6.4 (01-Feb-2012)
        stable release 2.6.3 (30-Sept-2011)
        stable release 2.6.2 (06-Aug-2011)
        stable release 2.6.1 (16-Apr-2011)
        stable release 2.6.0 (15-Apr-2011)
    
          beta release 2.5.31 (09-Aug-2010)
          beta release 2.5.30 (09-May-2010)
          beta release 2.5.29 (03-Apr-2010)
          beta release 2.5.28 (20-Sep-2009)
          beta release 2.5.27 (23-Feb-2009)
          beta release 2.5.26 (7-May-2008)
          beta release 2.5.25 (26-Feb-2008)
          beta release 2.5.24 (24-Nov-2007)
          beta release 2.5.23 (1-Sep-2007)
          beta release 2.5.22 (29-Aug-2007)
          beta release 2.5.21 (20-Jan-2007)
          beta release 2.5.20 (15-Jan-2007)
    
        stable release 2.4.20 (6-Dec-2006)

--- wm/fvwm/options.mk
+++ wm/fvwm/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1 2008/04/10 21:30:44 jlam Exp $
+# $NetBSD: options.mk,v 1.2 2013/01/20 06:53:53 mef Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.fvwm
-PKG_SUPPORTED_OPTIONS=		gtk rplay
+PKG_SUPPORTED_OPTIONS=		gtk rplay xrender xcursor xft2
 PKG_OPTIONS_LEGACY_VARS+=	FVWM2_USE_GTK:gtk
 PKG_OPTIONS_LEGACY_VARS+=	FVWM2_USE_RPLAY:rplay
 
@@ -18,4 +18,25 @@ CONFIGURE_ARGS+=	--without-gtk-prefix
 
 .if !empty(PKG_OPTIONS:Mrplay)
 .  include "../../audio/rplay/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mxcursor)
+CONFIGURE_ARGS+=	--enable-xcursor
+.include "../../x11/libXcursor/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-xcursor
+.endif
+
+.if !empty(PKG_OPTIONS:Mxft2)
+CONFIGURE_ARGS+=        --enable-xft
+.include "../../x11/libXft/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=        --disable-xft
+.endif
+
+.if !empty(PKG_OPTIONS:Mxrender)
+CONFIGURE_ARGS+=        --enable-xrender
+.include "../../x11/libXrender/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=        --disable-xrender
 .endif
