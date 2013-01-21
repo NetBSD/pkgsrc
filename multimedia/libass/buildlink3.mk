ab4a715ee54c5d426bd3a71b6b3a50798a21e125
wiz <wiz@pkgsrc.org>
2013-01-21 13:24:52 +0000

    Update to 0.10.1; bump ABI depends due to new fribidi dependency.
    libass (0.10.1)
     * Fix letter spacing
     * Add \rSTYLENAME syntax support (GC #43)
     * Fix border generation and border style reset (GC #56)
     * Fix various issues with bug-for-bug compatibility of
       transformations (\fax, \fay, etc.) to VSFilter (GC #46, GC #42)
     * Fix drawing parsing (GC #47)
     * Various fixes to shaper (GC #54, GC #55, GC#59)
     * Fix change detection
     * Add ass_set_line_position API to set a vertical subtitle offset
     * Fix scaling of drawing baseline offset (\pbo) values
     * Fix skipping of zero-width characters for FriBiDi shaper
     * Use LTR text base direction by default, similar to VSFilter
    
    libass (0.10.0)
     * Bidirectional layout and Arabic shaping via FriBidi (GC #13)
     * OpenType shaping via HarfBuzz-ng (GC #13)
     * Add API for shaper configuration
     * Add support for `Language' Script Info property, this can be used for
       hinting the text language
     * Vertical layout improvements
       * Use `vert' and `vkna' OpenType features for vertical glyph variants
       * Position rotated glyphs onto baseline
     * Parse font encoding property for base text direction hinting
     * Refactor cache system
     * Use generic outlines in place of FreeType glyphs
     * Direct outline bitmap rendering
     * Fix whitespace trimming (GC #35)
     * Do not render border if there's no shadow or glyph (GC #29)
     * Adjust spacing after a italic to non-italic style change (GC #37)
     * Fix fade timing
     * Fix x positioning with borders (GC #27)
     * Build system tweaks

--- multimedia/libass/buildlink3.mk
+++ multimedia/libass/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2012/12/16 13:09:25 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/01/21 13:24:52 wiz Exp $
 
 BUILDLINK_TREE+=	libass
 
@@ -6,8 +6,10 @@ BUILDLINK_TREE+=	libass
 LIBASS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libass+=	libass>=0.9.12
+BUILDLINK_ABI_DEPENDS.libass+=	libass>=0.10.1
 BUILDLINK_PKGSRCDIR.libass?=	../../multimedia/libass
 
+.include "../../converters/fribidi/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../textproc/enca/buildlink3.mk"
