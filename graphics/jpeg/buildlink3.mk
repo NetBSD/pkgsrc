802371f11c489970cc7db6f0abb819a3db3eb5c6
adam <adam@pkgsrc.org>
2013-01-24 21:49:18 +0000

    Changes 9: Add cjpeg -rgb1 option to create an RGB JPEG file, and insert a simple reversible color transform into the processing which significantly improves the compression. The recommended command for lossless coding of RGB images is now cjpeg -rgb1 -block 1 -arithmetic. As said, this option improves the compression significantly, but the files are not compatible with JPEG decoders prior to IJG v9 due to the included color transform. The used color transform and marker signaling is compatible with other JPEG standards (e.g., JPEG-LS part 2). Remove the automatic de-ANSI-fication support (Automake 1.12). Add remark for jpeg_mem_dest() in jdatadst.c. Support files with invalid component identifiers (created by Adobe PDF). Adapt full buffer case in jcmainct.c for use with scaled DCT. Add type identifier for declaration of noreturn functions. Correct argument type in format string, avoid compiler warnings. Add missing #include directives in configuration checks, avoid configuration errors.

--- graphics/jpeg/buildlink3.mk
+++ graphics/jpeg/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.15 2012/05/07 01:53:34 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2013/01/24 21:49:18 adam Exp $
 
 BUILDLINK_TREE+=	jpeg
 
 .if !defined(JPEG_BUILDLINK3_MK)
 JPEG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.jpeg+=	jpeg>=8nb1
-#BUILDLINK_ABI_DEPENDS.jpeg+=	jpeg>=8nb1
-BUILDLINK_ABI_DEPENDS.jpeg+=	jpeg>=8nb1
+BUILDLINK_API_DEPENDS.jpeg+=	jpeg>=9
+BUILDLINK_ABI_DEPENDS.jpeg+=	jpeg>=9
 BUILDLINK_PKGSRCDIR.jpeg?=	../../graphics/jpeg
 .endif # JPEG_BUILDLINK3_MK
 
