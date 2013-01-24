3278bfe3f94a023b459fa448fcad6360de6c89d2
wiz <wiz>
2013-01-24 21:52:28 +0000

    Reset API depends to 8nb1 until someone argues why it should be different.

--- graphics/jpeg/buildlink3.mk
+++ graphics/jpeg/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.16 2013/01/24 21:49:18 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2013/01/24 21:52:28 wiz Exp $
 
 BUILDLINK_TREE+=	jpeg
 
 .if !defined(JPEG_BUILDLINK3_MK)
 JPEG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.jpeg+=	jpeg>=9
+BUILDLINK_API_DEPENDS.jpeg+=	jpeg>=8nb1
 BUILDLINK_ABI_DEPENDS.jpeg+=	jpeg>=9
 BUILDLINK_PKGSRCDIR.jpeg?=	../../graphics/jpeg
 .endif # JPEG_BUILDLINK3_MK
