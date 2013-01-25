311a10b5fc33f1cb856a7547c41f4a866ef9789e
ryoon <ryoon@pkgsrc.org>
2013-01-25 20:02:24 +0000

    Update to 4.5.6
    * Add PostgreSQL support, not tested.
    
    Changelog:
    Version 4.5.6 Jan 22th 2013
    
        Improved language detection
        Improved translations
        Fix link to bugtracker
        Several IE 6/7/8 fixes
        SabreDAV updated to 1.6.6
        Improved error reporting
        Support special characters in mountpoint
        Interpret http 403 and 401 as not authorized in user_webdavauth
        Several fixes for special characters in files and folders
        Improved PostgreSQL support
        Check database names for valid characters
        Fix default email address calculation
        Remove debug output on send password page
        Add SMTP port configuration option
        Only show the max possible upload of 2GB on a 32 bit system
        Show progress during file downloads
        Security: Fix multiple XSS problems: CVE-2013-0201, CVE-2013-0202, CVE-2013-0203
        Security: Fix Code execution in external storage: CVE-2013-0204
        Security: Removed remoteStorage app because of unfixed security problems.

--- www/php-owncloud/options.mk
+++ www/php-owncloud/options.mk
@@ -1,16 +1,16 @@
-# $NetBSD: options.mk,v 1.3 2012/07/10 13:20:19 ryoon Exp $
+# $NetBSD: options.mk,v 1.4 2013/01/25 20:02:24 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.php-owncloud
 
 PKG_OPTIONS_REQUIRED_GROUPS=	db
-PKG_OPTIONS_GROUP.db=		mysql sqlite
+PKG_OPTIONS_GROUP.db=		mysql sqlite pgsql
 
 PKG_SUGGESTED_OPTIONS=	sqlite
 
 .include "../../mk/bsd.options.mk"
 
 ###
-### Use mysql or sqlite backend
+### Use mysql, pgsql, or sqlite backend
 ###
 .if !empty(PKG_OPTIONS:Mmysql)
 DEPENDS+=	${PHP_PKG_PREFIX}-mysql>=5.2.0:../../databases/php-mysql
@@ -23,4 +23,6 @@ DEPENDS+=	${PHP_PKG_PREFIX}-mysql>=5.2.0:../../databases/php-mysql
 # installed as version 2 and upgraded to 3 and then 4
 # complained/failed that PDO was not present.
 DEPENDS+=	${PHP_PKG_PREFIX}-pdo_sqlite>=5.2.0:../../databases/php-pdo_sqlite
+.elif !empty(PKG_OPTIONS:Mpgsql)
+DEPENDS+=	${PHP_PKG_PREFIX}-pgsql>=5.2.0:../../databases/php-pgsql
 .endif
