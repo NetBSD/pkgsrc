aa08cdfce5413662c00fb38c459156f7eb966d7f
obache <obache@pkgsrc.org>
2013-01-25 12:07:13 +0000

    Update libskk to 1.0.0.
    Noteworthy changes in version 1.0.0 (2012-08-27)
    ------------------------------------------------
    
     * Make sure to create parent directory of user-dict (Debian bug#685745).
     * Clear output and preedit on context reset.
     * Fix header include in tests (Issue#19).

--- inputmethod/libskk/patches/patch-libskk_state.c
+++ inputmethod/libskk/patches/patch-libskk_state.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-libskk_state.c,v 1.1 2012/11/16 01:02:54 joerg Exp $
+$NetBSD: patch-libskk_state.c,v 1.2 2013/01/25 12:07:13 obache Exp $
 
---- libskk/state.c.orig	2012-07-02 02:12:19.000000000 +0000
+--- libskk/state.c.orig	2012-08-27 02:38:28.000000000 +0000
 +++ libskk/state.c
-@@ -2085,7 +2085,7 @@ static gchar* skk_state_expand_numeric_r
- #line 263 "/home/ueno/devel/libskk/libskk/state.vala"
+@@ -1560,7 +1560,7 @@ static gchar* skk_state_expand_numeric_r
+ 					GError* e = NULL;
+ 					e = _inner_error_;
  					_inner_error_ = NULL;
- #line 272 "/home/ueno/devel/libskk/libskk/state.vala"
 -					g_return_if_reached ();
 +					g_return_val_if_reached (0);
- #line 263 "/home/ueno/devel/libskk/libskk/state.vala"
  					_g_error_free0 (e);
- #line 2092 "state.c"
+ 				}
+ 				__finally33:
