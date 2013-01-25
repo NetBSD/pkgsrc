b4d4f866dd901c6a23539adf580efd7c751689e7
obache <obache@pkgsrc.org>
2013-01-25 12:07:13 +0000

    Update libskk to 1.0.0.
    Noteworthy changes in version 1.0.0 (2012-08-27)
    ------------------------------------------------
    
     * Make sure to create parent directory of user-dict (Debian bug#685745).
     * Clear output and preedit on context reset.
     * Fix header include in tests (Issue#19).

--- inputmethod/libskk/patches/patch-libskk_expr.c
+++ inputmethod/libskk/patches/patch-libskk_expr.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-libskk_expr.c,v 1.1 2012/11/16 01:02:53 joerg Exp $
+$NetBSD: patch-libskk_expr.c,v 1.2 2013/01/25 12:07:13 obache Exp $
 
---- libskk/expr.c.orig	2012-11-15 12:30:02.000000000 +0000
+--- libskk/expr.c.orig	2012-08-27 02:38:28.000000000 +0000
 +++ libskk/expr.c
-@@ -495,7 +495,7 @@ void skk_expr_reader_read_string (SkkExp
- #line 62 "/home/ueno/devel/libskk/libskk/expr.vala"
+@@ -360,7 +360,7 @@ void skk_expr_reader_read_string (SkkExp
+ 		_tmp0_ = FALSE;
+ 	}
  	_tmp8_ = _tmp0_;
- #line 62 "/home/ueno/devel/libskk/libskk/expr.vala"
 -	g_return_val_if_fail (_tmp8_, NULL);
 +	g_return_if_fail (_tmp8_);
- #line 64 "/home/ueno/devel/libskk/libskk/expr.vala"
  	_tmp9_ = g_string_new ("");
- #line 64 "/home/ueno/devel/libskk/libskk/expr.vala"
+ 	builder = _tmp9_;
+ 	_tmp10_ = *index;
