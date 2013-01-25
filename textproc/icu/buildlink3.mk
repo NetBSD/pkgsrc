867909dca39e6e27603e1952f49f5f77e605ed2e
reed <reed@pkgsrc.org>
2013-01-25 19:18:08 +0000

    increase BUILDLINK_ABI_DEPENDS.icu due to so change from 49 to 50.
    Also bump PKGREVISION for a few packages using it.
    
    The packages I did this for:
    net/yaz
    lang/parrot
    misc/openoffice3 (where I noticed the run-time failure due to missing shared library)
    www/webkit-gtk
    sysutils/open-vm-tools
    inputmethod/ibus-qt
    
    I didn't do this recursively or for all packages using icu
    since I didn't know if they used the shared library directly,
    some use was optional. The list of packages I didn't touch:
    devel/devhelp
    databases/idzebra
    databases/sqlite3
    devel/gnustep-base/
    finance/gnucash
    games/openttd
    graphics/shotwell
    lang/mono
    meta-pkgs/boost
    misc/calibre
    misc/libreoffice
    news/tin
    textproc/php-intl
    www/deforaos-surfer
    www/epiphany
    www/liferea-current
    www/midori

--- textproc/icu/buildlink3.mk
+++ textproc/icu/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2012/04/27 12:32:04 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2013/01/25 19:18:08 reed Exp $
 
 BUILDLINK_TREE+=	icu
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	icu
 ICU_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.icu+=	icu>=3.4
-BUILDLINK_ABI_DEPENDS.icu+=	icu>=49.1.1nb1
+BUILDLINK_ABI_DEPENDS.icu+=	icu>=50
 BUILDLINK_PKGSRCDIR.icu?=	../../textproc/icu
 .endif # ICU_BUILDLINK3_MK
 
