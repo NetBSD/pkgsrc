837f86c10db2233ff1499166c2c254059c1594a6
bouyer <bouyer@pkgsrc.org>
2013-01-26 19:38:54 +0000

    Fix NetBSD quota support: - make sure to include /usr/include/quota.h to have the libquota prototypes,   fix a segfault on amd64 - do not leak quotahandle, make sure to quota_close() it.
    PKGREVISION++

--- mail/dovecot2/patches/patch-src_plugins_quota_quota-fs.c
+++ mail/dovecot2/patches/patch-src_plugins_quota_quota-fs.c
@@ -1,23 +1,10 @@
-$NetBSD: patch-src_plugins_quota_quota-fs.c,v 1.1 2013/01/19 05:25:51 manu Exp $
+$NetBSD: patch-src_plugins_quota_quota-fs.c,v 1.2 2013/01/26 19:38:54 bouyer Exp $
 
 Support NetBSD's libquota
 
---- src/plugins/quota/quota-fs.c.orig	2013-01-18 18:05:57.000000000 +0100
-+++ src/plugins/quota/quota-fs.c	2013-01-18 18:41:46.000000000 +0100
-@@ -76,8 +76,11 @@
- 
- 	unsigned int inode_per_mail:1;
- 	unsigned int user_disabled:1;
- 	unsigned int group_disabled:1;
-+#ifdef FS_QUOTA_NETBSD
-+	struct quotahandle *qh;
-+#endif
- };
- 
- extern struct quota_backend quota_backend_fs;
- 
-@@ -525,9 +528,10 @@
- #endif
+--- src/plugins/quota/quota-fs.c.orig	2012-10-30 18:57:35.000000000 +0100
++++ src/plugins/quota/quota-fs.c	2013-01-26 19:51:09.000000000 +0100
+@@ -526,7 +526,8 @@
  }
  #endif
  
@@ -27,9 +14,7 @@ Support NetBSD's libquota
  static void fs_quota_root_disable(struct fs_quota_root *root, bool group)
  {
  	if (group)
- 		root->group_disabled = TRUE;
-@@ -641,8 +645,46 @@
- 	return 1;
+@@ -642,6 +643,49 @@
  }
  #endif
  
@@ -38,45 +23,48 @@ Support NetBSD's libquota
 +fs_quota_get_netbsd(struct fs_quota_root *root, bool group, bool bytes,
 +		    uint64_t *value_r, uint64_t *limit_r)
 +{
++	struct quotahandle *qh;
 +	struct quotakey qk;
 +	struct quotaval qv;
++	int ret;
 +
-+	if (root->qh == NULL) {
-+		if ((root->qh = quota_open(root->mount->mount_path)) == NULL) {
-+			i_error("cannot open quota for %s: %m",
-+				root->mount->mount_path);
-+			fs_quota_root_disable(root, group);
-+			return 0;
-+		}
-+	} 
++	if ((qh = quota_open(root->mount->mount_path)) == NULL) {
++		i_error("cannot open quota for %s: %m",
++			root->mount->mount_path);
++		fs_quota_root_disable(root, group);
++		return 0;
++	}
 +
 +	qk.qk_idtype = group ? QUOTA_IDTYPE_GROUP : QUOTA_IDTYPE_USER;
 +	qk.qk_id = group ? root->gid : root->uid;
 +	qk.qk_objtype = bytes ? QUOTA_OBJTYPE_BLOCKS : QUOTA_OBJTYPE_FILES;
 +
-+	if (quota_get(root->qh, &qk, &qv) != 0) {
-+		if (errno == ESRCH) {
++	if (quota_get(qh, &qk, &qv) != 0) {
++		if (errno == EOPNOTSUPP || errno == ENXIO) {
 +			fs_quota_root_disable(root, group);
-+			return 0;
++			ret = 0;
++			goto end;
 +		}
 +		i_error("quotactl(Q_GETQUOTA, %s) failed: %m",
 +			root->mount->mount_path);
-+		return -1;
++		ret = -1;
++		goto end;
 +	}
 +
 +	*value_r = qv.qv_usage * DEV_BSIZE;
 +	*limit_r = qv.qv_softlimit * DEV_BSIZE;
 +
-+	return 1;
++	ret = 1;
++end:
++	quota_close(qh);
++	return ret;
 +}
 +#endif
 +
  #ifdef FS_QUOTA_HPUX
  static int
  fs_quota_get_hpux(struct fs_quota_root *root, bool bytes,
- 		  uint64_t *value_r, uint64_t *limit_r)
-@@ -714,8 +756,10 @@
- 			return 0;
+@@ -715,6 +759,8 @@
  	}
  #ifdef FS_QUOTA_LINUX
  	return fs_quota_get_linux(root, group, bytes, value_r, limit_r);
@@ -85,4 +73,3 @@ Support NetBSD's libquota
  #elif defined (FS_QUOTA_BSDAIX)
  	return fs_quota_get_bsdaix(root, group, bytes, value_r, limit_r);
  #else
- 	if (group) {
