f1d6a40a97ac78e2f78e0f01b012064977d78c81
bouyer <bouyer@pkgsrc.org>
2013-01-26 19:38:54 +0000

    Fix NetBSD quota support: - make sure to include /usr/include/quota.h to have the libquota prototypes,   fix a segfault on amd64 - do not leak quotahandle, make sure to quota_close() it.
    PKGREVISION++

--- mail/dovecot2/patches/patch-src_plugins_quota_quota-fs.h
+++ mail/dovecot2/patches/patch-src_plugins_quota_quota-fs.h
@@ -1,24 +1,10 @@
-$NetBSD: patch-src_plugins_quota_quota-fs.h,v 1.1 2013/01/19 05:25:51 manu Exp $
+$NetBSD: patch-src_plugins_quota_quota-fs.h,v 1.2 2013/01/26 19:38:54 bouyer Exp $
 
 Support NetBSD's libquota
 
 --- src/plugins/quota/quota-fs.h.orig	2011-12-13 12:35:29.000000000 +0100
-+++ src/plugins/quota/quota-fs.h	2013-01-18 20:10:34.000000000 +0100
-@@ -5,8 +5,12 @@
- 	defined (HAVE_STRUCT_DQBLK_CURSPACE)
- #  define HAVE_FS_QUOTA
- #endif
- 
-+#ifdef HAVE_QUOTA_OPEN
-+#  include <quota.h> /* NetBSD with libquota */
-+#endif
-+
- #ifdef HAVE_SYS_QUOTA_H
- #  include <sys/quota.h> /* Linux, HP-UX */
- #elif defined(HAVE_SYS_FS_UFS_QUOTA_H)
- #  include <sys/fs/ufs_quota.h> /* Solaris */
-@@ -36,5 +40,11 @@
- #else
++++ src/plugins/quota/quota-fs.h	2013-01-26 19:49:14.000000000 +0100
+@@ -37,4 +37,12 @@
  #  undef HAVE_FS_QUOTA
  #endif
  
@@ -26,6 +12,8 @@ Support NetBSD's libquota
 +#  define FS_QUOTA_NETBSD
 +#  define HAVE_FS_QUOTA
 +#  undef FS_QUOTA_LINUX /* obtained because we also have <sys/quota.h> */
++   /* absolute path to avoid confusion with ./quota.h */
++#  include "/usr/include/quota.h" /* absolute path to avoid confusion with ./quota.h */
 +#endif
 +
  #endif
