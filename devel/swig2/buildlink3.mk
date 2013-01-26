a54e34dcef1cdb548397e75094a63ef7ef7fb4fa
obache <obache>
2013-01-26 04:35:37 +0000

    fixes pkgsrc-wip ref.

--- devel/swig2/buildlink3.mk
+++ devel/swig2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/11/09 14:25:44 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/01/26 04:35:37 obache Exp $
 
 BUILDLINK_TREE+=	swig
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	swig
 SWIG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.swig+=	swig>=2.0.4
-BUILDLINK_PKGSRCDIR.swig?=	../../wip/swig2
+BUILDLINK_PKGSRCDIR.swig?=	../../devel/swig2
 
 .include "../../devel/pcre/buildlink3.mk"
 .endif	# SWIG_BUILDLINK3_MK
