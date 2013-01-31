5a5433650c4f5eef0aa7ffcc62a2e38fe7c68af2
adam <adam@pkgsrc.org>
2013-01-31 20:30:25 +0000

    Changes 5.1.0:
    BUGS FIXED
    * When reading a C++ number (like mpz_class) in an istream reaches the end
      of the stream, the eofbit is now set.
    
    * The result sign of mpz_rootrem's remainder is now always correct.
    * The mpz_remove function now handles negative divisors.
    * Contains all fixes from release 5.0.5.
    
    SPEEDUPS
    * The n-factorial and n-over-k functions have been reimplemented for great
      speedups for small and large operands.
    * New subquadratic algorithm for the Kronecker/Jacobi/Legendre symbol.
    * Major speedup for ARM, in particular ARM Cortex-A9 and A15, thanks to broad
      assembly support.
    * Significant speedup or POWER6 and POWER7 thanks to improved assembly.
    * The performance under M$ Windows' 64-bit ABI has been greatly improved
      thanks to complete assembly support.
    * Minor speed improvements of many functions and for many platforms.
    
    FEATURES
    * Many new CPUs recognised.
    * New functions for multi-factorials, and primorial: mpz_2fac_ui,
      mpz_mfac_uiui and mpz_primorial_ui.
    * The mpz_powm_sec function now uses side-channel silent division for
      converting into Montgomery residues.
    * The fat binary mechanism is now more robust in its CPU recognition.
    
    MISC
    * Inclusion of assembly code is now controlled by the configure options
      --enable-assembly and --disable-assembly.  The "none" CPU targets is gone.
    * In C++, the conversions mpq_class->mpz_class, mpf_class->mpz_class and
      mpf_class->mpq_class are now explicit.
    * Includes "mini-gmp", a small, portable, but less efficient, implementation
      of a subset of GMP's mpn and mpz interfaces. Used in GMP bootstrap, but it
      can also be bundled with applications as a fallback when the real GMP
      library is unavailable.
    * The ABIs under AIX are no longer called aix32 and aix64, but mode64 and 32.
      This is more consistent with other powerpc systems.
    * The coverage of the testsuite has been improved, using the lcov tool.  See
      also http://gmplib.org/devel/lcov/.
    * It is now possible to compile GMP using a C++ compiler.
    * K&R C compilers are no longer supported.
    * The BSD MP compatibility functions have been removed.

--- devel/gmp/inplace.mk
+++ devel/gmp/inplace.mk
@@ -1,4 +1,4 @@
-# $NetBSD: inplace.mk,v 1.1 2012/04/13 11:00:14 hans Exp $
+# $NetBSD: inplace.mk,v 1.2 2013/01/31 20:30:25 adam Exp $
 #
 # Include this file to extract devel/gmp source into the WRKSRC of
 # another package. This is to be used by GCC packages to avoid the
@@ -10,4 +10,3 @@ extract-inplace-gmp:
 	(cd ../../devel/gmp && ${MAKE} WRKDIR=${WRKSRC}/.devel.gmp EXTRACT_DIR=${WRKSRC} \
 		WRKSRC='$${EXTRACT_DIR}/$${DISTNAME}' SKIP_DEPENDS=YES fetch patch clean)
 	${MV} ${WRKSRC}/gmp-* ${WRKSRC}/gmp
-
