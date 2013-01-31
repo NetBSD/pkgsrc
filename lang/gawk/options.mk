96088f20b413b51cc290bc19b05f4a31416cd703
adam <adam@pkgsrc.org>
2013-01-31 22:01:43 +0000

    Changes 4.0.2: 1. Infrastructure upgrades: Autoconf 2.69, Automake 1.12.6, bison 2.7. 2. `fflush()', `nextfile', and `delete array' are all now part of POSIX. 3. fflush() behavior changed to match BWK awk and for POSIX - now both    fflush() and fflush("") flush all open output redirections. 4. Various minor bug fixes and documentation updates.

--- lang/gawk/options.mk
+++ lang/gawk/options.mk
@@ -1,13 +1,13 @@
-# $NetBSD: options.mk,v 1.2 2012/06/15 00:32:27 sbd Exp $
+# $NetBSD: options.mk,v 1.3 2013/01/31 22:01:43 adam Exp $
 
-PKG_OPTIONS_VAR=        PKG_OPTIONS.gawk
+PKG_OPTIONS_VAR=	PKG_OPTIONS.gawk
 PKG_SUPPORTED_OPTIONS=	portals readline
 PKG_SUGGESTED_OPTIONS=	readline
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mportals)
-CONFIGURE_ARGS+=        --enable-portals
+CONFIGURE_ARGS+=	--enable-portals
 .endif
 
 .if !empty(PKG_OPTIONS:Mreadline)
