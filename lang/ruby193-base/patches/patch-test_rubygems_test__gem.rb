a549fb696b957d6af0018be12e97f4b605f779bc
taca <taca>
2013-01-31 15:56:13 +0000

    Update ruby193 to 1.9.3p374 (Ruby 1.9.3-p374).
    These releasess don't include any security fixes.
    
    
    Ruby 1.9.3-p362:
    
    This release includes other many bug fixes.
    
        Resolved build problems on Solaris.
        Windows 8 support (hopefully).
        other many bug fixes.
    
    Ruby 1.9.3-p374:
    
    This release includes many bug fixes. Especially,
    
        Fixed randomly SEGV problem (often reported with Rails)
        Windows 8 support (maybe, hopefully)
        other many bug fixes.
    
    See tickets and ChangeLog for details.

--- lang/ruby193-base/patches/patch-test_rubygems_test__gem.rb
+++ lang/ruby193-base/patches/patch-test_rubygems_test__gem.rb
@@ -1,10 +1,10 @@
-$NetBSD: patch-test_rubygems_test__gem.rb,v 1.1.1.1 2011/11/08 16:10:51 taca Exp $
+$NetBSD: patch-test_rubygems_test__gem.rb,v 1.2 2013/01/31 15:56:14 taca Exp $
 
 Use vendordir instead of sitedir.
 
---- test/rubygems/test_gem.rb.orig	2011-08-26 01:12:57.000000000 +0000
+--- test/rubygems/test_gem.rb.orig	2012-04-19 22:43:16.000000000 +0000
 +++ test/rubygems/test_gem.rb
-@@ -821,12 +821,12 @@ class TestGem < Gem::TestCase
+@@ -844,12 +844,12 @@ class TestGem < Gem::TestCase
    end
  
    def test_self_prefix_sitelibdir
