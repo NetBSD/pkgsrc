6a382379ebd4eea331dd3aac9bc2b5217dd8158a
wiz <wiz@pkgsrc.org>
2013-01-31 10:25:30 +0000

    Shut up warnings about 6.0 packages on 6.0.1 (and similar). Welcome to 20130131.

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.164 2012/02/21 14:54:41 wiz Exp $	*/
+/*	$NetBSD: version.h,v 1.165 2013/01/31 10:25:30 wiz Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION 20120221
+#define PKGTOOLS_VERSION 20130131
 
 #endif /* _INST_LIB_VERSION_H_ */
