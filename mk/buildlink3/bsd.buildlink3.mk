1d8613a593e41178d252e8e48c202c5987a71621
hans <hans@pkgsrc.org>
2013-02-01 12:43:44 +0000

    Treat /usr/lib${LIBABISUFFIX} like /usr/lib to avoid picking up the wrong libraries from that dir.

--- mk/buildlink3/bsd.buildlink3.mk
+++ mk/buildlink3/bsd.buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.buildlink3.mk,v 1.216 2012/09/16 07:49:11 sbd Exp $
+# $NetBSD: bsd.buildlink3.mk,v 1.217 2013/02/01 12:43:44 hans Exp $
 #
 # Copyright (c) 2004 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -858,6 +858,7 @@ _BLNK_MANGLE_DIRS+=	${_BLNK_PASSTHRU_DIRS}
 _BLNK_MANGLE_DIRS+=	${_BLNK_PASSTHRU_RPATHDIRS}
 _BLNK_MANGLE_DIRS+=	/usr/include
 _BLNK_MANGLE_DIRS+=	/usr/lib
+_BLNK_MANGLE_DIRS+=	/usr/lib${LIBABISUFFIX}
 .if ${PKG_INSTALLATION_TYPE} == "pkgviews"
 _BLNK_MANGLE_DIRS+=	${PREFIX}
 .endif
@@ -887,6 +888,7 @@ _BLNK_PROTECT_DIRS+=	${_BLNK_PASSTHRU_DIRS}
 
 _BLNK_UNPROTECT_DIRS+=	/usr/include
 _BLNK_UNPROTECT_DIRS+=	/usr/lib
+_BLNK_UNPROTECT_DIRS+=	/usr/lib${LIBABISUFFIX}
 .if ${PKG_INSTALLATION_TYPE} == "pkgviews"
 _BLNK_UNPROTECT_DIRS+=	${PREFIX}
 .endif
