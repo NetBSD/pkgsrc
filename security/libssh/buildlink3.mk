455b7247c178565b696055eae63cf033201f8684
is <is@pkgsrc.org>
2013-02-01 13:33:48 +0000

    Update libssh to (upstream) 0.5.4 == (our) 0.54.
    (We need to keep the old numbering syntax to make versions compare
    correctly.)
    
    There are only two consumers in pkgsrc; one of them (remmina and
    remmina-plugins) actually needed library version 0.4 or later, and
    didn't build the ssh/sftp/nx plugins without. Hydra is also supposed
    to build with 0.4.x and later.)
    
    Upstream changelogs:
    
    0.5.4:
    	CVE-2013-0176 - NULL dereference leads to denial of service
    	Fixed several NULL pointer dereferences in SSHv1.
    	Fixed a free crash bug in options parsing.
    
    and for completeness 0.5.3:
    
    	This is an important SECURITY and maintenance release in
    	order to address CVE-2012-4559, CVE-2012-4560, CVE-2012-4561
    	and CVE-2012-4562.
    
    	CVE-2012-4559 - Fix multiple double free() flaws
    	CVE-2012-4560 - Fix multiple buffer overflow flaws
    	CVE-2012-4561 - Fix multiple invalid free() flaws
    	CVE-2012-4562 - Fix multiple improper overflow checks
    
    	(...)

--- security/libssh/buildlink3.mk
+++ security/libssh/buildlink3.mk
@@ -1,15 +1,25 @@
-# $NetBSD: buildlink3.mk,v 1.10 2012/05/07 01:53:57 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2013/02/01 13:33:49 is Exp $
 
 BUILDLINK_TREE+=	libssh
 
 .if !defined(LIBSSH_BUILDLINK3_MK)
 LIBSSH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libssh+=	libssh>=0.11
-BUILDLINK_ABI_DEPENDS.libssh+=	libssh>=0.11nb3
+BUILDLINK_API_DEPENDS.libssh+=	libssh>=0.5.4
+BUILDLINK_ABI_DEPENDS.libssh+=	libssh>=0.5.4
 BUILDLINK_PKGSRCDIR.libssh?=	../../security/libssh
 
+pkgbase := libssh
+.include "../../mk/pkg-build-options.mk"
+
+PKG_OPTIONS?=		# empty
+.if !empty(PKG_OPTIONS:Mzlib)
 .include "../../devel/zlib/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mopenssl)
+.include "../../security/openssl/buildlink3.mk"
+.endif
 .endif # LIBSSH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libssh
