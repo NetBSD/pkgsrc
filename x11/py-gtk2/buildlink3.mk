509e01851ef692a0f4c5b9c5baa9ea64438c09fd
tron <tron@pkgsrc.org>
2013-02-02 08:00:40 +0000

    Pullup ticket #4049 - requested by ryoon pkgsrc/x11/py-gtk2: build fix
    Revisions pulled up:
    - x11/py-gtk2/buildlink3.mk                                     1.42
    
    ---
       Module Name:	pkgsrc
       Committed By:	ryoon
       Date:		Thu Jan 31 18:30:50 UTC 2013
    
       Modified Files:
       	pkgsrc/x11/py-gtk2: buildlink3.mk
    
       Log Message:
       * py-numpy should be buildlinke-ed on g95 support platforms only like Makefile.

--- x11/py-gtk2/buildlink3.mk
+++ x11/py-gtk2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.41 2012/10/08 23:00:54 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.41.2.1 2013/02/02 08:00:40 tron Exp $
 
 BUILDLINK_TREE+=	pygtk2
 
@@ -13,7 +13,12 @@ BUILDLINK_PKGSRCDIR.pygtk2?=	../../x11/py-gtk2
 
 .include "../../devel/py-gobject/buildlink3.mk"
 .include "../../devel/libglade/buildlink3.mk"
+.if (!empty(MACHINE_ARCH:Mi386) || !empty(MACHINE_ARCH:Mx86_64) || \
+	!empty(MACHINE_ARCH:Mia64) || !empty(MACHINE_ARCH:Mpowerpc*) || \
+	!empty(MACHINE_ARCH:Mhppa) || !empty(MACHINE_ARCH:Msparc*) || \
+	!empty(MACHINE_ARCH:Malpha) || !empty(MACHINE_ARCH:Mmips*))
 .include "../../math/py-numpy/buildlink3.mk"
+.endif
 .include "../../x11/gtk2/buildlink3.mk"
 .endif # PYGTK2_BUILDLINK3_MK
 
