3c1549456009d933f94c6cf19b58764631e8e073
jmcneill <jmcneill@pkgsrc.org>
2013-02-02 12:00:20 +0000

    add a debug option

--- www/webkit-gtk/options.mk
+++ www/webkit-gtk/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2010/04/12 02:26:39 obache Exp $
+# $NetBSD: options.mk,v 1.2 2013/02/02 12:00:20 jmcneill Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.webkit-gtk
-PKG_SUPPORTED_OPTIONS=	webkit-jit
+PKG_SUPPORTED_OPTIONS=	webkit-jit debug
 PKG_SUGGESTED_OPTIONS=	webkit-jit
 
 .include "../../mk/bsd.options.mk"
@@ -11,4 +11,10 @@ PKG_SUGGESTED_OPTIONS=	webkit-jit
 CONFIGURE_ARGS+=	--enable-jit
 .else
 CONFIGURE_ARGS+=	--disable-jit
+.endif
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug
+.else
+CONFIGURE_ARGS+=	--disable-debug
 .endif
