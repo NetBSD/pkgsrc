0bf8e60a1f752d7e1999c143c52ea125068ac663
markd <markd>
2013-02-04 20:31:44 +0000

    Update to 0.93
    Changes unknown but 3 years of development since previous stable version.

--- lang/g95/patches/patch-libf95.a-0.92_io_atof.c
+++ lang/g95/patches/patch-libf95.a-0.92_io_atof.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-libf95.a-0.92_io_atof.c,v 1.1 2012/07/21 22:01:18 dholland Exp $
+$NetBSD: patch-libf95.a-0.92_io_atof.c,v 1.2 2013/02/04 20:31:44 markd Exp $
 
 Needs stdlib.h on netbsd to get alloca() declaration.
 
---- libf95.a-0.92/io/atof.c~	2009-04-22 17:44:56.000000000 +0000
-+++ libf95.a-0.92/io/atof.c
+--- libf95.a-0.93/io/atof.c~	2009-04-22 17:44:56.000000000 +0000
++++ libf95.a-0.93/io/atof.c
 @@ -2,6 +2,7 @@
  
  
