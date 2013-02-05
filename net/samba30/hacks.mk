69e03d7721659f3fd219b33cb0f4eeab7437c6a9
wiz <wiz@pkgsrc.org>
2013-02-05 07:45:22 +0000

    Remove samba30 and samba33, as stated by Aleksej Saushev on Jul 7, 2012.

--- net/samba30/hacks.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: hacks.mk,v 1.1.1.1 2011/12/15 22:23:24 asau Exp $
-
-# Enabling PIE results in a 'Unsupported relocation type 6 in non-PLT
-# relocations' error under NetBSD/macppc.
-.if ${OPSYS} == "NetBSD" && ${MACHINE_ARCH} == "powerpc"
-PKG_HACKS+=		pie
-CONFIGURE_ARGS+=	--disable-pie
-.endif
