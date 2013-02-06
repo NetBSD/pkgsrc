b484ae14cd37fc987ade4a33468b51d8d32a30e2
jperkin <jperkin@pkgsrc.org>
2013-02-06 18:07:04 +0000

    Update libpcap to 1.3.0.  Main pkgsrc change is support for Solaris 11/illumos.
    Summary for 1.3.0 libpcap release
            Handle DLT_PFSYNC in {FreeBSD, other *BSD+Mac OS X, other}.
            Linux: Don't fail if netfilter isn't enabled in the kernel.
            Add new link-layer type for NFC Forum LLCP.
            Put the CANUSB stuff into EXTRA_DIST, so it shows up in the release tarball.
            Add LINKTYPE_NG40/DLT_NG40.
            Add DLT_MPEG_2_TS/LINKTYPE_MPEG_2_TS for MPEG-2 transport streams.
            [PATCH] Fix AIX-3.5 crash with read failure during stress
            AIX fixes.
            Introduce --disable-shared configure option.
            Added initial support for canusb devices.
            Include the pcap(3PCAP) additions as 1.2.1 changes.
            many updates to documentation: pcap.3pcap.in
            Improve 'inbound'/'outbound' capture filters under Linux.
            Note the cleanup of handling of new DLT_/LINKTYPE_ values.
            On Lion, don't build for PPC.
            For mac80211 devices we need to clean up monitor mode on exit.
    
    Summary for 1.2.1 libpcap release
            Update README file.
            Fix typoes in README.linux file.
            Clean up some compiler warnings.
            Fix Linux compile problems and tests for ethtool.h.
            Treat Debian/kFreeBSD and GNU/Hurd as systems with GNU
             toolchains.
            Support 802.1 QinQ as a form of VLAN in filters.
            Treat "carp" as equivalent to "vrrp" in filters.
            Fix code generated for "ip6 protochain".
            Add some new link-layer header types.
            Support capturing NetFilter log messages on Linux.
            Clean up some error messages.
            Turn off monitor mode on exit for mac80211 interfaces on Linux.
            Fix problems turning monitor mode on for non-mac80211 interfaces
             on Linux.
            Properly fail if /sys/class/net or /proc/net/dev exist but can't
             be opened.
            Fail if pcap_activate() is called on an already-activated
             pcap_t, and add a test program for that.
            Fix filtering in pcap-ng files.
            Don't build for PowerPC on Mac OS X Lion.
            Simplify handling of new DLT_/LINKTYPE_ values.
            Expand pcap(3PCAP) man page.
    
    Summary for 1.2 libpcap release
            All of the changes listed below for 1.1.1 and 1.1.2.
            Changes to error handling for pcap_findalldevs().
            Fix the calculation of the frame size in memory-mapped captures.
            Add a link-layer header type for STANAG 5066 D_PDUs.
            Add a link-layer type for a variant of 3GPP TS 27.010.
            Noted real nature of LINKTYPE_ARCNET.
            Add a link-layer type for DVB-CI.
            Fix configure-script discovery of VLAN acceleration support.
             see http://netoptimizer.blogspot.com/2010/09/tcpdump-vs-vlan-tags.html
            Linux, HP-UX, AIX, NetBSD and OpenBSD compilation/conflict fixes.
            Protect against including AIX 5.x's <net/bpf.h> having been included.
            Add DLT_DBUS, for raw D-Bus messages.
            Treat either EPERM or EACCES as "no soup for you".
            Changes to permissions on DLPI systems.
            Add DLT_IEEE802_15_4_NOFCS for 802.15.4 interfaces.

---
+++ net/libpcap/patches/patch-gencode.c
@@ -0,0 +1,24 @@
+$NetBSD: patch-gencode.c,v 1.1 2013/02/06 18:07:04 jperkin Exp $
+
+* Correctly limit Linux interfaces to Linux-only.
+
+--- gencode.c.orig	2012-06-12 16:55:36.000000000 +0000
++++ gencode.c
+@@ -84,7 +84,7 @@ static const char rcsid[] _U_ =
+ #include "pcap/sll.h"
+ #include "pcap/ipnet.h"
+ #include "arcnet.h"
+-#if defined(PF_PACKET) && defined(SO_ATTACH_FILTER)
++#if defined(linux) && defined(PF_PACKET) && defined(SO_ATTACH_FILTER)
+ #include <linux/types.h>
+ #include <linux/if_packet.h>
+ #include <linux/filter.h>
+@@ -7583,7 +7583,7 @@ gen_inbound(dir)
+ 		 * check it, otherwise give up as this link-layer type
+ 		 * has nothing in the packet data.
+ 		 */
+-#if defined(PF_PACKET) && defined(SO_ATTACH_FILTER)
++#if defined(linux) && defined(PF_PACKET) && defined(SO_ATTACH_FILTER)
+ 		/*
+ 		 * We infer that this is Linux with PF_PACKET support.
+ 		 * If this is a *live* capture, we can look at
