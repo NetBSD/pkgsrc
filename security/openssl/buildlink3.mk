b6e0049ff94618e77657c67a3dfd84b3e9562583
tron <tron@pkgsrc.org>
2013-02-07 10:22:57 +0000

    Reduce minium required OpenSSL version to 1.0.1c (instead of 1.0.1d) which is what NetBSD 6.0* ships with.
    The minimum ABI version was incorrect anyway and a result of an unnecessary
    revision bump of the "openssl" package.

--- security/openssl/buildlink3.mk
+++ security/openssl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.44 2013/02/06 23:20:57 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.45 2013/02/07 10:22:57 tron Exp $
 
 BUILDLINK_TREE+=	openssl
 
@@ -7,8 +7,8 @@ OPENSSL_BUILDLINK3_MK:=
 
 .  include "../../mk/bsd.fast.prefs.mk"
 
-BUILDLINK_API_DEPENDS.openssl+=	openssl>=1.0.1d
-BUILDLINK_ABI_DEPENDS.openssl+=	openssl>=1.0.1dnb1
+BUILDLINK_API_DEPENDS.openssl+=	openssl>=1.0.1c
+BUILDLINK_ABI_DEPENDS.openssl+=	openssl>=1.0.1c
 BUILDLINK_PKGSRCDIR.openssl?=	../../security/openssl
 
 # Ensure that -lcrypt comes before -lcrypto when linking so that the
