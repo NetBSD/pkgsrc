b49d6b524ada5fdd3fc32d880fe45acbad679b7f
ryoon <ryoon@pkgsrc.org>
2013-02-07 20:32:42 +0000

    Update to 18.0.2.
    * Include patch for FreeBSD from Jan Beich (patch-bf).
    
    Changelog:
    FIXED
    18.0.2: Fix JavaScript related stability issues

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.41 2013/01/20 11:17:23 ryoon Exp $
+# $NetBSD: dist.mk,v 1.42 2013/02/07 20:32:42 ryoon Exp $
 #
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
@@ -6,7 +6,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	18.0
-MOZ_BRANCH_MINOR=	.1
+MOZ_BRANCH_MINOR=	.2
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
