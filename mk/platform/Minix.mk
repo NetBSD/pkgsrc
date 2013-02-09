55f6f7c33c0df3f8f8195dc2b9173e08386db1ba
tcort <tcort>
2013-02-09 03:40:01 +0000

    Minix now supports ELF shared libraries.
    OK by obache@

--- mk/platform/Minix.mk
+++ mk/platform/Minix.mk
@@ -1,4 +1,4 @@
-# $NetBSD: Minix.mk,v 1.2 2012/03/19 12:34:17 joerg Exp $
+# $NetBSD: Minix.mk,v 1.3 2013/02/09 03:40:01 tcort Exp $
 #
 # Variable definitions for the Minix operating system.
 
@@ -24,6 +24,7 @@ ROOT_USER?=		root
 SERIAL_DEVICES?=	/dev/null
 USERADD?=		/usr/bin/useradd
 ULIMIT_CMD_datasize?=	: 
+ULIMIT_CMD_stacksize?=	: 
 
 _OPSYS_HAS_INET6=	no	# IPv6 is not standard
 _OPSYS_HAS_JAVA=	no	# Java is not standard
@@ -31,10 +32,10 @@ _OPSYS_HAS_MANZ=	no	# no MANZ for gzipping of man pages
 _OPSYS_HAS_OSSAUDIO=	no	# libossaudio is available
 _OPSYS_PERL_REQD=		# no base version of perl required
 _OPSYS_PTHREAD_AUTO=	no	# -lpthread needed for pthreads
-_OPSYS_SHLIB_TYPE=	none	# No shared libraries on Minix (yet)
+_OPSYS_SHLIB_TYPE=	ELF	# ELF shared libraries on Minix
 _PATCH_CAN_BACKUP=	yes	# native patch(1) can make backups
 _PATCH_BACKUP_ARG?= 	-b -V simple --suffix	# switch to patch(1) for backup suffix
-_USE_RPATH=		no	# add rpath to LDFLAGS
+_USE_RPATH=		yes	# add rpath to LDFLAGS
 
 # flags passed to the linker to extract all symbols from static archives.
 # this is GNU ld.
@@ -44,4 +45,4 @@ _OPSYS_NO_WHOLE_ARCHIVE_FLAG=	-Wl,--no-whole-archive
 _STRIPFLAG_CC?=		${_INSTALL_UNSTRIPPED:D:U-s}	# cc(1) option to strip
 _STRIPFLAG_INSTALL?=	${_INSTALL_UNSTRIPPED:D:U-s}	# install(1) option to strip
 
-_OPSYS_CAN_CHECK_SHLIBS=	no # can't use readelf in check/bsd.check-vars.mk
+_OPSYS_CAN_CHECK_SHLIBS=	yes
