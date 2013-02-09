6ef7b3286a4045daf14d5ba37c9eae7af5740cf6
ryoon <ryoon>
2013-02-09 22:11:28 +0000

    Bump PKGREVISION from audio/jack.

--- net/kdenetwork3/buildlink3.mk
+++ net/kdenetwork3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.35 2013/02/06 23:21:07 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2013/02/09 22:11:30 ryoon Exp $
 
 BUILDLINK_TREE+=	kdenetwork
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kdenetwork
 KDENETWORK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kdenetwork+=	kdenetwork>=3.5.0nb1
-BUILDLINK_ABI_DEPENDS.kdenetwork+=	kdenetwork>=3.5.10nb23
+BUILDLINK_ABI_DEPENDS.kdenetwork+=	kdenetwork>=3.5.10nb24
 BUILDLINK_PKGSRCDIR.kdenetwork?=	../../net/kdenetwork3
 
 .include "../../converters/uulib/buildlink3.mk"
