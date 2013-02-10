2309fb5ad5611e6330a6a499684350ad4fecb99c
obache <obache@pkgsrc.org>
2013-02-10 12:25:17 +0000

    Add preference for Cygwin.

---
+++ mk/platform/Cygwin.mk
@@ -0,0 +1,89 @@
+# $NetBSD: Cygwin.mk,v 1.1 2013/02/10 12:25:18 obache Exp $
+#
+# Variable definitions for the Windows with Cygwin.
+
+###
+### Overrides to standard BSD .mk files
+###
+
+# "catinstall" not yet supported as there's no shipped [gn]roff
+MANINSTALL=	maninstall
+MAKE_FLAGS+=	MKCATPAGES=no NOLINT=1
+
+###
+### Alternate defaults to global pkgsrc settings, to help avoid
+### some of the excessive Interix fork(2) overhead, and reduce the
+### amount of settings required in the user's mk.conf
+###
+
+INSTALL?=		${PREFIX}/bin/install-sh
+PAX?=			${PREFIX}/bin/nbpax
+
+.if defined(BATCH)
+BULK_PREREQ+=		lang/perl5
+USE_BULK_BROKEN_CHECK?=	no
+USE_BULK_TIMESTAMPS?=	no
+.endif
+
+###
+### Platform definitions common to pkgsrc/mk/platform/*.mk
+###
+
+ECHO_N?=	${ECHO} -n
+IMAKE_MAKE?=	${GMAKE}		# program which gets invoked by imake
+IMAKEOPTS+=	-DBuildHtmlManPages=NO
+PKGLOCALEDIR?=	share
+PS?=		/bin/ps
+SU?=		/bin/su
+TYPE?=		type				# Shell builtin
+
+USERADD?=		${LOCALBASE}/sbin/useradd
+GROUPADD?=		${LOCALBASE}/sbin/groupadd
+_PKG_USER_HOME?=	# empty by default
+_USER_DEPENDS=		user>=20040426:../../sysutils/user_interix
+
+CPP_PRECOMP_FLAGS?=	# unset
+CONFIG_RPATH_OVERRIDE?=	config.rpath */config.rpath */*/config.rpath
+DEF_UMASK?=		002
+EXPORT_SYMBOLS_LDFLAGS?=-Wl,-E	# add symbols to the dynamic symbol table
+
+MOTIF_TYPE_DEFAULT?=	motif	# default 2.0 compatible libs type
+NOLOGIN?=		/bin/false
+PKG_TOOLS_BIN?=		${LOCALBASE}/sbin
+PKGDIRMODE?=		775
+# ROOT_USER might be numeric in the special case of Administrator; canonify it:
+ROOT_CMD?=		${SU} - "$$(id -un ${ROOT_USER})" -c
+ROOT_USER?=		${BINOWN}
+ROOT_GROUP?=		id -g Administraotr
+TOUCH_FLAGS?=
+ULIMIT_CMD_datasize?=	ulimit -d `ulimit -H -d`
+ULIMIT_CMD_stacksize?=	ulimit -s `ulimit -H -s`
+ULIMIT_CMD_memorysize?=	ulimit -v `ulimit -H -v`
+
+_OPSYS_HAS_INET6=	yes	# IPv6 is standard
+_OPSYS_HAS_JAVA=	no	# Java is not standard
+_OPSYS_HAS_MANZ=	yes	# MANZ controls gzipping of man pages
+_OPSYS_HAS_OSSAUDIO=	no	# libossaudio is available
+_OPSYS_PERL_REQD=	5.8.3nb1 # base version of perl required
+_OPSYS_PTHREAD_AUTO=	no	# -lpthread needed for pthreads
+_OPSYS_SHLIB_TYPE=	PEwin	# shared lib type
+_PATCH_CAN_BACKUP=	yes	# native patch(1) can make backups
+_PATCH_BACKUP_ARG?=	-b -V simple -z	# switch to patch(1) for backup suffix
+_USE_RPATH=		no	# add rpath to LDFLAGS
+
+# flags passed to the linker to extract all symbols from static archives.
+# this is GNU ld.
+_OPSYS_WHOLE_ARCHIVE_FLAG=	-Wl,--whole-archive
+_OPSYS_NO_WHOLE_ARCHIVE_FLAG=	-Wl,--no-whole-archive
+
+_STRIPFLAG_CC?=		${_INSTALL_UNSTRIPPED:D:U-s}	# cc(1) option to strip
+_STRIPFLAG_INSTALL?=	${_INSTALL_UNSTRIPPED:D:U-s}	# install(1) option to strip
+
+DEFAULT_SERIAL_DEVICE?=	/dev/ttyS0
+SERIAL_DEVICES?=	/dev/ttyS0 /dev/ttyS1 /dev/ttyS2 /dev/ttyS3
+
+_OPSYS_CAN_CHECK_SHLIBS=	no # can't use readelf in check/bsd.check-vars.mk
+
+# check for maximum command line length and set it in configure's environment,
+# to avoid a test required by the libtool script that takes forever.
+_OPSYS_MAX_CMDLEN_CMD=	${ECHO} 262144
