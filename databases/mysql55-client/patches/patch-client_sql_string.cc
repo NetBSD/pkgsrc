c087243707c385347363c7645f3b7ad52ef82ebc
adam <adam@pkgsrc.org>
2013-02-12 20:36:28 +0000

    Changes 5.5.30: Refer to http://dev.mysql.com/doc/relnotes/mysql/5.5/en/news-5-5-30.html

--- databases/mysql55-client/patches/patch-client_sql_string.cc
+++ databases/mysql55-client/patches/patch-client_sql_string.cc
@@ -1,6 +1,6 @@
-$NetBSD: patch-client_sql_string.cc,v 1.2 2012/10/01 15:54:40 adam Exp $
+$NetBSD: patch-client_sql_string.cc,v 1.3 2013/02/12 20:36:28 adam Exp $
 
-* Portability: include <bstring.h> if exists.
+Portability: include <bstring.h> if exists.
 
 --- client/sql_string.cc.orig	2010-02-04 11:36:53.000000000 +0000
 +++ client/sql_string.cc
