23d8d8d47743ce2312106eb03b05c08def21a3a4
drochner <drochner@pkgsrc.org>
2013-02-12 12:35:34 +0000

    needs libz, doesn't need bash

--- print/qpdf/patches/patch-make_libtool.mk
+++ print/qpdf/patches/patch-make_libtool.mk
@@ -1,9 +1,18 @@
-$NetBSD: patch-make_libtool.mk,v 1.1 2013/01/31 21:45:24 wiz Exp $
+$NetBSD: patch-make_libtool.mk,v 1.2 2013/02/12 12:35:36 drochner Exp $
 
 Sent upstream, will be fixed in 4.1.0.
 
 --- make/libtool.mk.orig	2013-01-17 14:51:04.000000000 +0000
 +++ make/libtool.mk
+@@ -1,7 +1,7 @@
+ # --- Required interface definitions ---
+ 
+ # LIBTOOL needs bash
+-SHELL=/bin/bash
++#SHELL=/bin/bash
+ 
+ OBJ=o
+ LOBJ=lo
 @@ -116,7 +116,6 @@ install: all
  		libqpdf/$(OUTPUT_DIR)/libqpdf.la \
  		$(DESTDIR)$(libdir)/libqpdf.la
