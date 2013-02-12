728506c209e922810e345545655c98929b61a45f
minskim <minskim@pkgsrc.org>
2013-02-12 04:47:31 +0000

    Update emacs-snapshot to 24.3.50.20130207.
    Patches provided by Makoto Fujiwara in PR 47552 and modified by me.
    
    Notable changes since 24.3:
    ** `apropos-variable' is now `apropos-user-option'
    ** `eval-defun' on an already defined defcustom calls the :set function,
       if there is one.
    ** The option `set-mark-default-inactive' has been deleted.
       This unfinished feature was introduced by accident in Emacs 23.1;
       simply disabling Transient Mark mode does the same thing.
    ** `initial-buffer-choice' can now specify a function to set up the
       initial buffer.
    ** ACL support has been added.
    ** New option `scroll-bar-adjust-thumb-portion'.
    ** Improved fullscreen support for Nextstep (Mac OS X).
    ** New commands `toggle-frame-fullscreen' and `toggle-frame-maximized',
       bound to <f11> and M-<f10>, respectively.
    ** Delphi mode is now called OPascal mode.
    ** jit-lock-debug-mode lets you use the debuggers on code run via jit-lock.
    ** completing-read-multiple's separator can now be a regexp.  The
       default separator is changed to allow surrounding spaces around the
       comma.
    
    See share/emacs/24.3.50/etc/NEWS for the complete list of changes.

--- editors/emacs-snapshot/version.mk
+++ editors/emacs-snapshot/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.12 2012/03/06 03:01:30 minskim Exp $
+# $NetBSD: version.mk,v 1.13 2013/02/12 04:47:31 minskim Exp $
 
 _EMACS_FLAVOR=	emacs
 _EMACS_REQD=	emacs>=23
@@ -6,5 +6,5 @@ _EMACS_PKGDEP.base=
 _EMACS_PKGDEP.leim=
 
 _EMACS_VERSION_MAJOR=	24
-_EMACS_VERSION_MINOR=	0
-_EMACS_VERSION_MICRO=	94
+_EMACS_VERSION_MINOR=	3
+_EMACS_VERSION_MICRO=	50
