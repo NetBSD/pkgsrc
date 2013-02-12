0d76bc25f8be9f17a1b0f8ca0ed91f4f1b18cc57
adam <adam@pkgsrc.org>
2013-02-12 21:07:21 +0000

    Changes 1.53.0: New Libraries: Atomic: C++11-style atomic<>, from Helge Bahmann, maintained by Tim Blechmann. Coroutine: Coroutine library, from Oliver Kowalke. Lockfree: Lockfree data structures, from Tim Blechmann. Multiprecision: Extended precision arithmetic types for floating point, integer and rational arithmetic from John Maddock and Christopher Kormanyos. Odeint: Solving ordinary differential equations, from Karsten Ahnert and Mario Mulansky.
    News
    This release contains a fix for ticket 7743, in Boost.Locale. For more details, see the security notice.
    
    More info http://www.boost.org/users/history/version_1_53_0.html

--- devel/boost-jam/buildlink3.mk
+++ devel/boost-jam/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.20 2012/11/07 21:04:10 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2013/02/12 21:07:21 adam Exp $
 
 BUILDLINK_TREE+=	boost-jam
 
 .if !defined(BOOST_JAM_BUILDLINK3_MK)
 BOOST_JAM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.52.*
+BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.53.*
 BUILDLINK_DEPMETHOD.boost-jam?=		build
 BUILDLINK_PKGSRCDIR.boost-jam?=		../../devel/boost-jam
 .endif # BOOST_JAM_BUILDLINK3_MK
