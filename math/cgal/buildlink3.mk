3e464b5455fbd86b0dcf3223c72fdf099f8562c1
obache <obache@pkgsrc.org>
2013-02-15 11:53:59 +0000

    recursive bump from boost-lib shlib update.

--- math/cgal/buildlink3.mk
+++ math/cgal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2013/02/06 23:21:14 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2013/02/15 11:54:00 obache Exp $
 
 BUILDLINK_TREE+=	cgal
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	cgal
 CGAL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cgal+=	cgal>=3.9
-BUILDLINK_ABI_DEPENDS.cgal+=	cgal>=3.9nb12
+BUILDLINK_ABI_DEPENDS.cgal+=	cgal>=3.9nb13
 BUILDLINK_PKGSRCDIR.cgal?=	../../math/cgal
 
 .include "../../devel/boost-headers/buildlink3.mk"
