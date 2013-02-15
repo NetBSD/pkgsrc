d11f246b66693c6ec93c801c5ac6f56a17c91dee
obache <obache>
2013-02-15 11:53:59 +0000

    recursive bump from boost-lib shlib update.

--- ham/usrp/buildlink3.mk
+++ ham/usrp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2012/07/02 13:37:40 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2013/02/15 11:54:00 obache Exp $
 
 BUILDLINK_TREE+=	usrp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	usrp
 USRP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.usrp+=	usrp>=3.0.4
-BUILDLINK_ABI_DEPENDS.usrp+=	usrp>=3.3.0nb6
+BUILDLINK_ABI_DEPENDS.usrp+=	usrp>=3.3.0nb8
 BUILDLINK_PKGSRCDIR.usrp?=	../../ham/usrp
 
 .include "../../ham/gnuradio-core/buildlink3.mk"
