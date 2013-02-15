fac7c21d661c3ab7e170729819bb39d22eff0219
obache <obache@pkgsrc.org>
2013-02-15 11:53:59 +0000

    recursive bump from boost-lib shlib update.

--- graphics/openimageio/buildlink3.mk
+++ graphics/openimageio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/02/06 23:21:09 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/02/15 11:54:00 obache Exp $
 
 BUILDLINK_TREE+=	openimageio
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	openimageio
 OPENIMAGEIO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.openimageio+=	openimageio>=1.1.1
-BUILDLINK_ABI_DEPENDS.openimageio?=	openimageio>=1.1.2nb2
+BUILDLINK_ABI_DEPENDS.openimageio?=	openimageio>=1.1.5nb1
 BUILDLINK_PKGSRCDIR.openimageio?=	../../graphics/openimageio
 
 .include "../../devel/boost-libs/buildlink3.mk"
