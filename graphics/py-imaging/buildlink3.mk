8f8fa8629afbe006aa0d1102390d44d473c8cdd2
wiz <wiz>
2013-02-16 12:07:26 +0000

    Fix incorrect expansion (use PYPKGPREFIX instead of hardcoded py27)

--- graphics/py-imaging/buildlink3.mk
+++ graphics/py-imaging/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2013/01/26 21:36:32 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2013/02/16 12:07:26 wiz Exp $
 
 BUILDLINK_TREE+=	pyimaging
 
@@ -8,7 +8,7 @@ PY_IMAGING_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.pyimaging+=	${PYPKGPREFIX}-imaging>=1.1.4
-BUILDLINK_ABI_DEPENDS.pyimaging+=	py27-imaging>=1.1.7nb5
+BUILDLINK_ABI_DEPENDS.pyimaging+=	${PYPKGPREFIX}-imaging>=1.1.7nb5
 BUILDLINK_PKGSRCDIR.pyimaging?=	../../graphics/py-imaging
 
 .include "../../mk/jpeg.buildlink3.mk"
