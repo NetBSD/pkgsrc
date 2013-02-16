4bb17e3339e932b4cabae6a831c85923fb5616f0
tron <tron@pkgsrc.org>
2013-02-16 15:10:46 +0000

    Security and usablity enhancements of the "gmediaserver" package: 1.) Run the daemon as an unprivileged user. 2.) Add support for a configuration file which can be used to specify     the shared directories and extra options. 3.) Re-write the startup script to work on platforms other than NetBSD.

--- multimedia/gmediaserver/files/gmediaserver.sh
+++ multimedia/gmediaserver/files/gmediaserver.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: gmediaserver.sh,v 1.1.1.1 2010/05/20 04:27:33 obache Exp $
+# $NetBSD: gmediaserver.sh,v 1.2 2013/02/16 15:10:46 tron Exp $
 #
 # PROVIDE: gmediaserver
 # REQUIRE: DAEMON
@@ -8,12 +8,69 @@
 . /etc/rc.subr
 
 name="gmediaserver"
-rcvar=${name}
+rcvar="${name}"
 command="@PREFIX@/bin/${name}"
-pidfile="@VARBASE@/run/${name}.pid"
-command_args="-b --pid-file=${pidfile}"
-exra_commands="reload"
-sig_reload=-USER1
+pidfile="@VARBASE@/gmediaserver/${name}.pid"
+command_flags="-b --pid-file=${pidfile} -o @VARBASE@/${name}/${name}.log"
+required_files="@CFGFILE@"
+start_cmd="gmediaserver_start"
+extra_commands="rescan"
+rescan_cmd="gmediaserver_rescan"
 
-load_rc_config ${name}
-run_rc_command "$1"
+gmediaserver_start()
+{
+	directories=/nonexistent
+	options=
+	if [ -f "@CFGFILE@" ]; then
+		. "@CFGFILE@"
+	fi
+	@ECHO@ "Starting ${name}."
+	@SU@ @GMEDIA_USER@ -c "$command" $command_flags $options $directories
+}
+
+gmediaserver_rescan()
+{
+	if [ -f "${pidfile}" ] ; then
+		@ECHO@ "Signalling ${name} to rescan directories."
+		kill -USR1 `@CAT@ "${pidfile}"`
+	fi
+}
+
+if [ -f /etc/rc.subr -a -f /etc/rc.conf -a -f /etc/rc.d/DAEMON ]; then
+	load_rc_config ${name}
+	run_rc_command "$1"
+else
+	if [ -f /etc/rc.conf ]; then
+		. /etc/rc.conf
+	fi
+	case $1 in
+	start)
+		gmediaserver_start
+		;;
+
+	stop)
+		if [ -f "${pidfile}" ] ; then
+			@ECHO@ "Stopping ${name}."
+			kill `@CAT@ "${pidfile}"`
+		else
+			@ECHO@ "${name} not running?"
+			exit 1
+		fi
+		;;
+
+	restart)
+		"$0" stop
+		exec "$0" start
+		;;
+
+	rescan)
+		gmediaserver_rescan
+		;;
+
+	*)
+		@ECHO@ "Usage: $0 {start|stop|restart|rescan}" 1>&2
+		exit 64
+		;;
+
+	esac
+fi
