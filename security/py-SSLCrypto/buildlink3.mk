f26628c9a6ca55215763eb7785df0ca98b72e216
wiz <wiz@pkgsrc.org>
2013-02-16 12:07:26 +0000

    Fix incorrect expansion (use PYPKGPREFIX instead of hardcoded py27)

--- security/py-SSLCrypto/buildlink3.mk
+++ security/py-SSLCrypto/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2013/02/06 23:21:02 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2013/02/16 12:07:26 wiz Exp $
 
 BUILDLINK_TREE+=	py-SSLCrypto
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	py-SSLCrypto
 PY_SSLCRYPTO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.py-SSLCrypto+=	${PYPKGPREFIX}-SSLCrypto>=0.1.1
-BUILDLINK_ABI_DEPENDS.py-SSLCrypto+=	py27-SSLCrypto>=0.1.1nb4
+BUILDLINK_ABI_DEPENDS.py-SSLCrypto+=	${PYPKGPREFIX}-SSLCrypto>=0.1.1nb4
 BUILDLINK_PKGSRCDIR.py-SSLCrypto?=	../../security/py-SSLCrypto
 
 #.include "../../security/openssl/buildlink3.mk"
