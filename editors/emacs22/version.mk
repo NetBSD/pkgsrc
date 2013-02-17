b2dee0160e7f2e564dfbb6dbaec7555df92833c4
dholland <dholland@pkgsrc.org>
2013-02-17 00:39:53 +0000

    Change PKGNAME from emacs-22.3 to emacs22-22.3.

--- editors/emacs22/version.mk
+++ editors/emacs22/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.3 2011/02/08 13:20:14 hiramatsu Exp $
+# $NetBSD: version.mk,v 1.4 2013/02/17 00:39:53 dholland Exp $
 
 _EMACS_FLAVOR=	emacs
-_EMACS_REQD=	emacs>=22.3<23
+_EMACS_REQD=	emacs22>=22.3<23
 _EMACS_PKGDEP.base=
 _EMACS_PKGDEP.leim=
 
