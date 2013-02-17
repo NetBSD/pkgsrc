58d4580ec94a08e113e4efc5ab8523f78b697736
dholland <dholland@pkgsrc.org>
2013-02-17 06:18:01 +0000

    Insert ${_EMACS_VERSION_MAJOR} in PKGNAME. While here, fix wrong (emacs23-era) pattern in version.mk.
    Caution: The distfile appears to have vanished, so I can't build this
    package and it may not work.

--- editors/emacs-snapshot/version.mk
+++ editors/emacs-snapshot/version.mk
@@ -1,7 +1,8 @@
-# $NetBSD: version.mk,v 1.13 2013/02/12 04:47:31 minskim Exp $
+# $NetBSD: version.mk,v 1.14 2013/02/17 06:18:01 dholland Exp $
 
 _EMACS_FLAVOR=	emacs
-_EMACS_REQD=	emacs>=23
+#_EMACS_REQD=	emacs25>=25
+_EMACS_REQD=	emacs24>=24.3
 _EMACS_PKGDEP.base=
 _EMACS_PKGDEP.leim=
 
