474c4251203bfd717478a0b5c530e960ffd6d385
obache <obache@pkgsrc.org>
2013-02-17 12:57:51 +0000

    Clean up tools for Cygwin: * expect all packages in Base category are installed * try to detect tools in optional packages

--- mk/tools/tools.Cygwin.mk
+++ mk/tools/tools.Cygwin.mk
@@ -1,8 +1,8 @@
-# $NetBSD: tools.Cygwin.mk,v 1.1 2013/02/10 12:25:18 obache Exp $
+# $NetBSD: tools.Cygwin.mk,v 1.2 2013/02/17 12:57:51 obache Exp $
 #
 # System-supplied tools for the Cygwin operating system.
 
-TOOLS_PLATFORM.[?=		/bin/[
+TOOLS_PLATFORM.[?=		[			# shell builtin
 TOOLS_PLATFORM.awk?=		/bin/awk
 TOOLS_PLATFORM.basename?=	/bin/basename
 TOOLS_PLATFORM.bash?=		/bin/bash
@@ -10,25 +10,30 @@ TOOLS_PLATFORM.bash?=		/bin/bash
 TOOLS_PLATFORM.bison?=		/bin/bison
 TOOLS_PLATFORM.bison-yacc?=	/bin/bison -y
 .endif
-.if exists(/bin/bzcat)
+.if exists(/bin/bsdtar)
+TOOLS_PLATFORM.bsdtar?=		/bin/bsdtar
+.endif
+.if exists(/bin/byacc)
+TOOLS_PLATFORM.byacc?=		/bin/byacc
+.endif
 TOOLS_PLATFORM.bzcat?=		/bin/bzcat
-.elif exists(/bin/bzip2)
-TOOLS_PLATFORM.bzcat?=		/bin/bzip2 -cd
-.endif
-.if exists(/bin/bzip2)
 TOOLS_PLATFORM.bzip2?=		/bin/bzip2
-.endif
-
 TOOLS_PLATFORM.cat?=		/bin/cat
 TOOLS_PLATFORM.chgrp?=		/bin/chgrp
 TOOLS_PLATFORM.chmod?=		/bin/chmod
 TOOLS_PLATFORM.chown?=		/bin/chown
 TOOLS_PLATFORM.cmp?=		/bin/cmp
 TOOLS_PLATFORM.cp?=		/bin/cp
-TOOLS_PLATFORM.csh?=		/bin/csh
+.if exists(/bin/tcsh)
+TOOLS_PLATFORM.csh?=		/bin/tcsh
+.endif
+.if exists(/bin/curl)
+TOOLS_PLATFORM.curl?=		/bin/curl
+.endif
 TOOLS_PLATFORM.cut?=		/bin/cut
 TOOLS_PLATFORM.date?=		/bin/date
 TOOLS_PLATFORM.diff?=		/bin/diff
+TOOLS_PLATFORM.diff3?=		/bin/diff3
 TOOLS_PLATFORM.dirname?=	/bin/dirname
 TOOLS_PLATFORM.echo?=		echo			# shell builtin
 TOOLS_PLATFORM.egrep?=		/bin/egrep
@@ -40,35 +45,58 @@ TOOLS_PLATFORM.file?=		/bin/file
 TOOLS_PLATFORM.find?=		/bin/find
 .if exists(/bin/flex)
 TOOLS_PLATFORM.flex?=		/bin/flex
-TOOLS_PLATFORM.lex?=		/bin/flex
+.endif
+.if exists(/bin/tnftp)
+TOOLS_PLATFORM.ftp?=		/bin/tnftp
+.endif
+TOOLS_PLATFORM.gawk?=		/bin/gawk
+.if exists(/bin/m4)
+TOOLS_PLATFORM.gm4?=		/bin/m4
 .endif
 TOOLS_PLATFORM.grep?=		/bin/grep
+TOOLS_PLATFORM.gsed?=		/bin/sed
+TOOLS_PLATFORM.gsoelim?=	/bin/soelim
+TOOLS_PLATFORM.gtar?=		/bin/tar
 TOOLS_PLATFORM.gunzip?=		/bin/gunzip -f
 TOOLS_PLATFORM.gzcat?=		/bin/zcat
 TOOLS_PLATFORM.gzip?=		/bin/gzip -nf ${GZIP}
 TOOLS_PLATFORM.head?=		/bin/head
 TOOLS_PLATFORM.hostname?=	/bin/hostname
 TOOLS_PLATFORM.id?=		/bin/id
+.if exists(/bin/ident)
+TOOLS_PLATFORM.ident?=		/bin/ident
+.endif
 TOOLS_PLATFORM.install?=	/bin/install
-.if exists(/bin/install-info)
 TOOLS_PLATFORM.install-info?=	/bin/install-info
+.if exists(/bin/flex)
+TOOLS_PLATFORM.lex?=		/bin/flex
 .endif
 TOOLS_PLATFORM.ln?=		/bin/ln
 TOOLS_PLATFORM.ls?=		/bin/ls
+.if exists(/bin/m4)
 TOOLS_PLATFORM.m4?=		/bin/m4
-TOOLS_PLATFORM.mail?=		/bin/mail
+.endif
 .if exists(/bin/make)
 TOOLS_PLATFORM.gmake?=		/bin/make
 .endif
-.if exists(/bin/makeinfo)
 TOOLS_PLATFORM.makeinfo?=	/bin/makeinfo
-.endif
 TOOLS_PLATFORM.mkdir?=		/bin/mkdir -p
 TOOLS_PLATFORM.mktemp?=		/bin/mktemp
+.if exists(/bin/msgfmt)
+TOOLS_PLATFORM.msgfmt?=		/bin/msgfmt
+.endif
 TOOLS_PLATFORM.mv?=		/bin/mv
 TOOLS_PLATFORM.nice?=		/bin/nice
+TOOLS_PLATFORM.nroff?=		/bin/nroff
+.if exists(/bin/openssl)
+TOOLS_PLATFORM.openssl?=	/bin/openssl
+.endif
+.if exists(/bin/pax)
+TOOLS_PLATFORM.pax?=		/bin/pax
+.endif
 .if exists(/bin/perl)
 TOOLS_PLATFORM.perl?=		/bin/perl
+TOOLS_PLATFORM.pod2man?=	/bin/pod2man
 .endif
 # Don't use GNU patch
 #.if exists(/bin/patch)
@@ -81,17 +109,39 @@ TOOLS_PLATFORM.printf?=		/bin/printf
 TOOLS_PLATFORM.pwd?=		/bin/pwd
 TOOLS_PLATFORM.rm?=		/bin/rm
 TOOLS_PLATFORM.rmdir?=		/bin/rmdir
+TOOLS_PLATFORM.sdiff?=		/bin/sdiff
+TOOLS_PLATFORM.sed?=		/bin/sed
 TOOLS_PLATFORM.sh?=		/bin/sh
 TOOLS_PLATFORM.sleep?=		/bin/sleep
+TOOLS_PLATFORM.soelim?=		/bin/soelim
 TOOLS_PLATFORM.sort?=		/bin/sort
+.if exists(/bin/strip)
 TOOLS_PLATFORM.strip?=		/bin/strip
+.endif
 TOOLS_PLATFORM.tail?=		/bin/tail
+TOOLS_PLATFORM.tar?=		/bin/tar
+TOOLS_PLATFORM.tbl?=		/bin/tbl
 TOOLS_PLATFORM.tee?=		/bin/tee
 TOOLS_PLATFORM.test?=		test			# shell builtin
+.if exists(/bin/texi2html)
+TOOLS_PLATFORM.texi2html?=	/bin/texi2html
+.endif
 TOOLS_PLATFORM.touch?=		/bin/touch
 TOOLS_PLATFORM.tr?=		/bin/tr
 TOOLS_PLATFORM.true?=		true			# shell builtin
 TOOLS_PLATFORM.tsort?=		/bin/tsort
+.if exists(/bin/unzip)
+TOOLS_PLATFORM.unzip?=		/bin/unzip
+.endif
 TOOLS_PLATFORM.wc?=		/bin/wc
 TOOLS_PLATFORM.xargs?=		/bin/xargs -r
-TOOLS_PLATFORM.sed?=		/bin/sed
+.if exists(/bin/xgettext)
+TOOLS_PLATFORM.xgettext?=	/bin/xgettext
+.endif
+TOOLS_PLATFORM.xzcat?=		/bin/xzcat
+.if exists(/bin/yacc)
+TOOLS_PLATFORM.yacc?=		/bin/yacc
+.endif
+.if exists(/bin/zip)
+TOOLS_PLATFORM.zip?=		/bin/zip
+.endif
