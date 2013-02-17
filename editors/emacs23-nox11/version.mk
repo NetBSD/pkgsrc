3ec8deee3717472be121a07ed10241fb96c73ce4
dholland <dholland@pkgsrc.org>
2013-02-17 07:09:55 +0000

    This was wrong again; didn't get incremented after the last update. (Not my doing.)

--- editors/emacs23-nox11/version.mk
+++ editors/emacs23-nox11/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.1 2013/02/17 05:00:08 dholland Exp $
+# $NetBSD: version.mk,v 1.2 2013/02/17 07:09:55 dholland Exp $
 
 _EMACS_FLAVOR=	emacs
 _EMACS_REQD=	emacs-nox11>=23.2<24
 
 _EMACS_VERSION_MAJOR=	23
-_EMACS_VERSION_MINOR=	3
+_EMACS_VERSION_MINOR=	4
