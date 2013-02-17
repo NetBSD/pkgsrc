4b29f08bec8d7812fd53bf686f8d030fda82714d
dholland <dholland@pkgsrc.org>
2013-02-17 00:18:58 +0000

    Change PKGNAME from emacs-21.4a to emacs21-21.4a.

--- editors/emacs21/version.mk
+++ editors/emacs21/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.2 2011/02/08 13:20:14 hiramatsu Exp $
+# $NetBSD: version.mk,v 1.3 2013/02/17 00:18:58 dholland Exp $
 
 _EMACS_FLAVOR=	emacs
-_EMACS_REQD=	emacs>=21.4<22
+_EMACS_REQD=	emacs21>=21.4<22
 _EMACS_PKGDEP.base=
 _EMACS_PKGDEP.leim=	leim>=21.${EMACS_VERSION_MINOR}<22:../../editors/leim21
 
