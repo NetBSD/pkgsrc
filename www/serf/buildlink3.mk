9f7e5f0d2c0b4091268a6ce329ad7bca73816279
taca <taca@pkgsrc.org>
2013-02-22 15:38:44 +0000

    Update serf to 1.1.1.
    Full changes are a little bit many to write here, but it change its API
    from prior to 1.0.
    
    Please refer http://code.google.com/p/serf/source/browse/tags/1.1.1/CHANGES
    for full changes.

--- www/serf/buildlink3.mk
+++ www/serf/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2013/02/06 23:20:57 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2013/02/22 15:38:44 taca Exp $
 
 BUILDLINK_TREE+=	serf
 
 .if !defined(SERF_BUILDLINK3_MK)
 SERF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.serf+=	serf>=0.1.0
-BUILDLINK_ABI_DEPENDS.serf+=	serf>=0.3.0nb5
+BUILDLINK_API_DEPENDS.serf+=	serf>=1.1.1
+BUILDLINK_ABI_DEPENDS.serf+=	serf>=1.1.1
 BUILDLINK_PKGSRCDIR.serf?=	../../www/serf
 
 .include "../../devel/apr/buildlink3.mk"
