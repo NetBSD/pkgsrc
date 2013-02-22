330addacf7bc4cd2552769cae952c116bcef9648
taca <taca@pkgsrc.org>
2013-02-22 15:32:06 +0000

    Add www/serf0 by copying from www/serf.  It provides serf version < 1.0, and it would be used by subversion16.

---
+++ www/serf0/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/02/22 15:32:06 taca Exp $
+
+BUILDLINK_TREE+=	serf
+
+.if !defined(SERF_BUILDLINK3_MK)
+SERF_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.serf+=	serf>=0.1.0
+BUILDLINK_ABI_DEPENDS.serf+=	serf>=0.3.0nb5
+BUILDLINK_PKGSRCDIR.serf?=	../../www/serf0
+
+.include "../../devel/apr/buildlink3.mk"
+.include "../../devel/apr-util/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.endif # SERF_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-serf
