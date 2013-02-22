9da1dbdad557d1f05c12e350e5b4e73ca7253b4e
taca <taca@pkgsrc.org>
2013-02-22 15:49:25 +0000

    Replace PKGNAME to serf0 for better handling of better binary package.

--- www/serf0/buildlink3.mk
+++ www/serf0/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/02/22 15:32:06 taca Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/02/22 15:49:25 taca Exp $
 
 BUILDLINK_TREE+=	serf
 
 .if !defined(SERF_BUILDLINK3_MK)
 SERF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.serf+=	serf>=0.1.0
-BUILDLINK_ABI_DEPENDS.serf+=	serf>=0.3.0nb5
+BUILDLINK_API_DEPENDS.serf+=	serf0>=0.1.0
+BUILDLINK_ABI_DEPENDS.serf+=	serf0>=0.3.0nb5
 BUILDLINK_PKGSRCDIR.serf?=	../../www/serf0
 
 .include "../../devel/apr/buildlink3.mk"
