d5dea81c70e04957d5922e0e300255c5415d315e
ryoon <ryoon@pkgsrc.org>
2013-02-22 14:48:06 +0000

    Update to 19.0
    Changelog:
        NEW
        Built-in PDF viewer
        CHANGED
        Canvas elements can export their content as an image blob using canvas.toBlob()
        CHANGED
        Startup performance improvements (bugs 715402 and 756313)
        DEVELOPER
        Debugger now supports pausing on exceptions and hiding non-enumerable properties
        DEVELOPER
        Remote Web Console is available for connecting to Firefox on Android or Firefox OS (experimental, set devtools.debugger.remote-enabled to true)
        DEVELOPER
        There is now a Browser Debugger available for add-on and browser developers (experimental, set devtools.chrome.enabled to true)
        DEVELOPER
        Web Console CSS links now open in the Style Editor
        HTML5
        CSS @page is now supported
        HTML5
        CSS viewport-percentage length units implemented (vh, vw, vmin and vmax)
        HTML5
        CSS text-transform now supports full-width
        FIXED
        Certain valid WebGL drawing operations are incorrectly rejected, leaving incomplete rendering in affected pages (825205)
        FIXED
        Starting Firefox with -private flag incorrectly claims you are not in Private Browsing mode (802274)
        FIXED
        Plugins stop rendering when the top half of the plugin is scrolled off the top of the page, in HiDPI mode (825734)

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,12 +1,12 @@
-# $NetBSD: dist.mk,v 1.42 2013/02/07 20:32:42 ryoon Exp $
+# $NetBSD: dist.mk,v 1.43 2013/02/22 14:48:06 ryoon Exp $
 #
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	18.0
-MOZ_BRANCH_MINOR=	.2
+MOZ_BRANCH=	19.0
+MOZ_BRANCH_MINOR=	
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
