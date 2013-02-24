5872ec9a65ebcba361a2b205d2c04d5ef0f42e7b
ryoon <ryoon>
2013-02-24 14:18:00 +0000

    Update to 17.0.3
    Changelog:
    FIXED
    Security fixes can be found here
    FIXED
    Improvements to the Click-to-Play vulnerable plugin blocklisting feature
    
    Fixed in Firefox ESR 17.0.3
    MFSA 2013-28 Use-after-free, out of bounds read, and buffer overflow issues found using Address Sanitizer
    MFSA 2013-27 Phishing on HTTPS connection through malicious proxy
    MFSA 2013-26 Use-after-free in nsImageLoadingContent
    MFSA 2013-25 Privacy leak in JavaScript Workers
    MFSA 2013-24 Web content bypass of COW and SOW security wrappers
    MFSA 2013-21 Miscellaneous memory safety hazards (rv:19.0 / rv:17.0.3)

--- devel/xulrunner17/dist.mk
+++ devel/xulrunner17/dist.mk
@@ -1,11 +1,11 @@
-# $NetBSD: dist.mk,v 1.2 2013/01/12 10:49:57 ryoon Exp $
+# $NetBSD: dist.mk,v 1.3 2013/02/24 14:18:00 ryoon Exp $
 #
 # used by devel/xulrunner17/Makefile
 # used by www/firefox17/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	17.0.2
+MOZ_BRANCH=	17.0.3
 MOZ_BRANCH_MINOR=	esr
 MASTER_SITES=	${MASTER_SITE_MOZILLA_ESR:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
