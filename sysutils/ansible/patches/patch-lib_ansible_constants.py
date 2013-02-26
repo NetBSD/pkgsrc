de24370464cd55328793d15771401a9d17898e98
riz <riz@pkgsrc.org>
2013-02-26 17:42:59 +0000

    Some fixes for the package:
    - install manpages
    - replace "etc" with PKG_SYSCONFDIR in a number of locations
    - replace "usr/share" with @PREFIX@/share in some places
    - do some cleanup so things install with PKG_DEVELOPER set.

--- sysutils/ansible/patches/patch-lib_ansible_constants.py
+++ sysutils/ansible/patches/patch-lib_ansible_constants.py
@@ -1,15 +1,48 @@
-$NetBSD: patch-lib_ansible_constants.py,v 1.1 2013/02/26 00:13:00 hubertf Exp $
+$NetBSD: patch-lib_ansible_constants.py,v 1.2 2013/02/26 17:42:59 riz Exp $
 
-diff --git a/lib/ansible/constants.py b/lib/ansible/constants.py
-index b43986e..2e60046 100644
---- lib/ansible/constants.py
-+++ lib/ansible/constants.py
-@@ -65,6 +65,8 @@ if getattr(sys, "real_prefix", None):
+--- lib/ansible/constants.py.orig	2013-02-01 18:15:55.000000000 -0800
++++ lib/ansible/constants.py	2013-02-25 11:58:52.000000000 -0800
+@@ -36,7 +36,7 @@
+     p = ConfigParser.ConfigParser()
+     path1 = os.path.expanduser(os.environ.get('ANSIBLE_CONFIG', "~/.ansible.cfg"))
+     path2 = os.getcwd() + "/ansible.cfg"
+-    path3 = "/etc/ansible/ansible.cfg"
++    path3 = "@PKG_SYSCONFDIR@/ansible/ansible.cfg"
+ 
+     if os.path.exists(path1):
+         p.read(path1)
+@@ -64,13 +64,13 @@
+ if getattr(sys, "real_prefix", None):
      DIST_MODULE_PATH = os.path.join(sys.prefix, 'share/ansible/')
  else:
-     DIST_MODULE_PATH = '/usr/share/ansible/'
-+    # HF: work as non-root with python install --prefix=...
+-    DIST_MODULE_PATH = '/usr/share/ansible/'
 +    DIST_MODULE_PATH = '@PREFIX@/share/ansible/'			
  
  # sections in config file
  DEFAULTS='defaults'
+ 
+ # configurable things
+-DEFAULT_HOST_LIST         = shell_expand_path(get_config(p, DEFAULTS, 'hostfile',         'ANSIBLE_HOSTS',            '/etc/ansible/hosts'))
++DEFAULT_HOST_LIST         = shell_expand_path(get_config(p, DEFAULTS, 'hostfile',         'ANSIBLE_HOSTS',            '@PKG_SYSCONFDIR@/ansible/hosts'))
+ DEFAULT_MODULE_PATH       = shell_expand_path(get_config(p, DEFAULTS, 'library',          'ANSIBLE_LIBRARY',          DIST_MODULE_PATH))
+ DEFAULT_REMOTE_TMP        = shell_expand_path(get_config(p, DEFAULTS, 'remote_tmp',       'ANSIBLE_REMOTE_TEMP',      '$HOME/.ansible/tmp'))
+ DEFAULT_MODULE_NAME       = get_config(p, DEFAULTS, 'module_name',      None,                       'command')
+@@ -95,12 +95,12 @@
+ DEFAULT_SUDO_FLAGS        = get_config(p, DEFAULTS, 'sudo_flags', 'ANSIBLE_SUDO_FLAGS', '-H')
+ DEFAULT_HASH_BEHAVIOUR    = get_config(p, DEFAULTS, 'hash_behaviour', 'ANSIBLE_HASH_BEHAVIOUR', 'replace')
+ 
+-DEFAULT_ACTION_PLUGIN_PATH     = shell_expand_path(get_config(p, DEFAULTS, 'action_plugins',     'ANSIBLE_ACTION_PLUGINS', '/usr/share/ansible_plugins/action_plugins'))
+-DEFAULT_CALLBACK_PLUGIN_PATH   = shell_expand_path(get_config(p, DEFAULTS, 'callback_plugins',   'ANSIBLE_CALLBACK_PLUGINS', '/usr/share/ansible_plugins/callback_plugins'))
+-DEFAULT_CONNECTION_PLUGIN_PATH = shell_expand_path(get_config(p, DEFAULTS, 'connection_plugins', 'ANSIBLE_CONNECTION_PLUGINS', '/usr/share/ansible_plugins/connection_plugins'))
+-DEFAULT_LOOKUP_PLUGIN_PATH     = shell_expand_path(get_config(p, DEFAULTS, 'lookup_plugins',     'ANSIBLE_LOOKUP_PLUGINS', '/usr/share/ansible_plugins/lookup_plugins'))
+-DEFAULT_VARS_PLUGIN_PATH     = shell_expand_path(get_config(p, DEFAULTS, 'vars_plugins',     'ANSIBLE_VARS_PLUGINS', '/usr/share/ansible_plugins/vars_plugins'))
+-DEFAULT_FILTER_PLUGIN_PATH     = shell_expand_path(get_config(p, DEFAULTS, 'filter_plugins',     'ANSIBLE_FILTER_PLUGINS', '/usr/share/ansible_plugins/filter_plugins'))
++DEFAULT_ACTION_PLUGIN_PATH     = shell_expand_path(get_config(p, DEFAULTS, 'action_plugins',     'ANSIBLE_ACTION_PLUGINS', '@PREFIX@/share/ansible_plugins/action_plugins'))
++DEFAULT_CALLBACK_PLUGIN_PATH   = shell_expand_path(get_config(p, DEFAULTS, 'callback_plugins',   'ANSIBLE_CALLBACK_PLUGINS', '@PREFIX@/share/ansible_plugins/callback_plugins'))
++DEFAULT_CONNECTION_PLUGIN_PATH = shell_expand_path(get_config(p, DEFAULTS, 'connection_plugins', 'ANSIBLE_CONNECTION_PLUGINS', '@PREFIX@/share/ansible_plugins/connection_plugins'))
++DEFAULT_LOOKUP_PLUGIN_PATH     = shell_expand_path(get_config(p, DEFAULTS, 'lookup_plugins',     'ANSIBLE_LOOKUP_PLUGINS', '@PREFIX@/share/ansible_plugins/lookup_plugins'))
++DEFAULT_VARS_PLUGIN_PATH     = shell_expand_path(get_config(p, DEFAULTS, 'vars_plugins',     'ANSIBLE_VARS_PLUGINS', '@PREFIX@/share/ansible_plugins/vars_plugins'))
++DEFAULT_FILTER_PLUGIN_PATH     = shell_expand_path(get_config(p, DEFAULTS, 'filter_plugins',     'ANSIBLE_FILTER_PLUGINS', '@PREFIX@/share/ansible_plugins/filter_plugins'))
+ 
+ # non-configurable things
+ DEFAULT_SUDO_PASS         = None
