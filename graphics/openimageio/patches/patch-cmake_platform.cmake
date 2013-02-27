5dc90a68645ca7a9bf5238fcc19b32710e3c7bef
ryoon <ryoon@pkgsrc.org>
2013-02-27 15:19:02 +0000

    Update to 1.1.7
    Changelog:
    Release 1.1.7 (21 Feb 2013)
    ---------------------------
    * Back out dangerous change to thread.h that was in 1.1.6, which could
      cause performance problems.
    * Compile fix for WIN32 in strutil.cpp
    * Compile fix for Windows XP - add implementation of InterlockedExchangeAdd64
    
    
    Release 1.1.6 (11 Feb 2013)
    ---------------------------
    * Fix bug that could generate NaNs or other bad values near the poles of
      very blurry environment lookups specifically from OpenEXR latlong env maps.
    * Fix bug in oiiotool --crop where it could mis-parse the geometric parameter.
    * Fix bug in ImageCache::invalidate() where it did not properly delete the
      fingerprint of an invalidated file.
    * Cleanup and fixes in the oiiotool command line help messages.
    * New function ImageBufAlgo::paste() copies a region from one IB to another.
    * oiiotool --fit resizes an image to fit into a given resolution (keeping the
      original aspect ratio and padding with black if needed).
    * ImageBufAlgo::channels() and "oiiotool --ch" have been extended to allow
      new channels (specified to be filled with numeric constants) to also be
      named.
    * New function ImageBufAlgo::mul() and "oiiotool --cmul" let you multiply
      an image by a scalar constant (or per-channel constant).
    * Important maketx bug fix: when creating latlong environment maps as
      OpenEXR files, it was adding energy near the poles, making low-res
      MIP levels too bright near the poles.
    * Fix to "oiiotool --text" and "oiiotool --fill" -- both were
      incorrectly making the base image black rather than drawing overtop of
      the previous image.
    * Fix FreeBSD compile when not using TBB.
    * New oiiotool --swap exchanges the top two items on the image stack.

--- graphics/openimageio/patches/patch-cmake_platform.cmake
+++ graphics/openimageio/patches/patch-cmake_platform.cmake
@@ -1,16 +1,23 @@
-$NetBSD: patch-cmake_platform.cmake,v 1.1 2012/12/03 12:54:14 ryoon Exp $
+$NetBSD: patch-cmake_platform.cmake,v 1.2 2013/02/27 15:19:02 ryoon Exp $
 
 * Add NetBSD support
 
---- cmake/platform.cmake.orig	2012-11-16 23:02:42.000000000 +0000
+--- cmake/platform.cmake.orig	2013-02-21 17:10:46.000000000 +0000
 +++ cmake/platform.cmake
-@@ -23,6 +23,9 @@ if (UNIX)
-     elseif (${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD")
-         set (platform "FreeBSD")
-         set (CXXFLAGS "${CXXFLAGS} -DFREEBSD")
+@@ -30,6 +30,16 @@ if (UNIX)
+                 set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=i586")
+             endif()
+         endif()
 +    elseif (${CMAKE_SYSTEM_NAME} STREQUAL "NetBSD")
-+        set (platform "NetBSD")
-+        set (CXXFLAGS "${CXXFLAGS} -DNETBSD")
++	set (platform "NetBSD")
++	set (CXXFLAGS "${CXXFLAGS} -DNETBSD")
++        if (${CMAKE_SYSTEM_PROCESSOR} STREQUAL "i386")
++            if (NOT USE_TBB)
++                # to use gcc atomics we need cpu instructions only available
++                # with arch of i586 or higher
++                set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=i586")
++            endif()
++        endif()
      else ()
          string (TOLOWER ${CMAKE_SYSTEM_NAME} platform)
      endif ()
