1c1a24052458868da7e42611aa51172a4c19eeec
ryoon <ryoon@pkgsrc.org>
2013-02-27 15:25:28 +0000

    Update to 2.66
    Changes:
    This release contains long awaited features like rigid
    body physics simulation, dynamic topology sculpting and matcap
    display.
    
    Other new features include Cycles hair rendering, support for high
    pixel density displays, much better handling of premultiplied and
    straight alpha transparency, a vertex bevel tool, a mesh cache
    modifier and a new SPH particle fluid dynamics solver.
    
    In detail, see http://wiki.blender.org/index.php/Dev:Ref/Release_Notes/2.66

--- graphics/blender/patches/patch-doc_python__api_sphinx__doc__gen.sh
+++ graphics/blender/patches/patch-doc_python__api_sphinx__doc__gen.sh
@@ -1,15 +1,24 @@
-$NetBSD: patch-doc_python__api_sphinx__doc__gen.sh,v 1.1 2012/12/03 13:15:51 ryoon Exp $
+$NetBSD: patch-doc_python__api_sphinx__doc__gen.sh,v 1.2 2013/02/27 15:25:28 ryoon Exp $
 
 * Improve portability
 
---- doc/python_api/sphinx_doc_gen.sh.orig	2012-10-09 18:42:01.000000000 +0000
+--- doc/python_api/sphinx_doc_gen.sh.orig	2013-02-20 14:22:36.000000000 +0000
 +++ doc/python_api/sphinx_doc_gen.sh
 @@ -32,7 +32,7 @@ blender_version_char=$(grep BLENDER_VERS
  blender_version_cycle=$(grep BLENDER_VERSION_CYCLE $blender_srcdir/source/blender/blenkernel/BKE_blender.h | awk '{print $3}')
  blender_subversion=$(grep BLENDER_SUBVERSION $blender_srcdir/source/blender/blenkernel/BKE_blender.h | awk '{print $3}')
  
--if [ "$blender_version_cycle" == "release" ]
-+if [ "$blender_version_cycle" = "release" ]
- then
+-if [ "$blender_version_cycle" == "release" ] ; then
++if [ "$blender_version_cycle" = "release" ] ; then
  	BLENDER_VERSION=$(expr $blender_version / 100)_$(expr $blender_version % 100)$blender_version_char"_release"
  else
+ 	BLENDER_VERSION=$(expr $blender_version / 100)_$(expr $blender_version % 100)_$blender_subversion
+@@ -109,7 +109,7 @@ if $DO_UPLOAD ; then
+ 	ssh $SSH_USER@emo.blender.org 'echo "<html><head><title>Redirecting...</title><meta http-equiv=\"REFRESH\" content=\"0;url=../blender_python_api_'$BLENDER_VERSION'/\"></head><body>Redirecting...</body></html>" > '$SSH_UPLOAD'/250PythonDoc/index.html'
+ 
+ 	# redirect for release only so wiki can point here
+-	if [ "$blender_version_cycle" == "release" ] ; then
++	if [ "$blender_version_cycle" = "release" ] ; then
+ 		ssh $SSH_USER@emo.blender.org 'echo "<html><head><title>Redirecting...</title><meta http-equiv=\"REFRESH\" content=\"0;url=../blender_python_api_'$BLENDER_VERSION'/\"></head><body>Redirecting...</body></html>" > '$SSH_UPLOAD'/blender_python_api/index.html'
+ 	fi
+ 
