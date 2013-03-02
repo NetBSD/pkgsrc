04a24487305aaa70f7283fb8d8d64b41ffeef478
joerg <joerg>
2013-03-02 18:05:46 +0000

    Use non-ambigious assembler syntax. Fix return types. Fix inline usage to allow building with Clang.

---
+++ lang/g95/patches/patch-libf95.a-0.93_io_read.c
@@ -0,0 +1,13 @@
+$NetBSD: patch-libf95.a-0.93_io_read.c,v 1.1 2013/03/02 18:05:46 joerg Exp $
+
+--- libf95.a-0.93/io/read.c.orig	2013-03-01 11:14:56.000000000 +0000
++++ libf95.a-0.93/io/read.c
+@@ -102,7 +102,7 @@ void set_real(int value, void *dest, int
+ 
+ #if HAVE_REAL_10
+     case 10:
+-	asm("fild %0\n"
++	asm("filds %0\n"
+ 	    "mov %1, %" EAX "\n"
+ 	    "fstpt (%" EAX ")\n" : : "m" (value), "m" (dest) : EAX);
+ 	break;
---
+++ lang/g95/patches/patch-libf95.a-0.93_math_ff.c
@@ -0,0 +1,31 @@
+$NetBSD: patch-libf95.a-0.93_math_ff.c,v 1.1 2013/03/02 18:05:46 joerg Exp $
+
+--- libf95.a-0.93/math/ff.c.orig	2013-03-01 11:15:29.000000000 +0000
++++ libf95.a-0.93/math/ff.c
+@@ -790,7 +790,7 @@ int exp;
+ 
+     exp = *i - 1;
+ 
+-    asm("fild %0\n"        /* i */
++    asm("filds %0\n"        /* i */
+ 	"fldt %1\n"        /* x : i */
+ 	"fxtract\n"        /* sig(x) : exp(x) : i */
+ 	"fstp %%st(1)\n"   /* sig(x) : i */
+@@ -961,7 +961,7 @@ int exp;
+ 
+     exp = *i;
+ 
+-    asm("fild %0\n"
++    asm("filds %0\n"
+ 	"fldt %1\n"
+ 	"fscale\n"
+ 	"fstp %%st(1)\n" : : "m" (exp), "m" (*x));
+@@ -1069,7 +1069,7 @@ int exp;
+     case FF_REGULAR:
+ 	exp = MAN10_LEN - 1;
+ 
+-	asm("fild %0\n"             /* p */
++	asm("filds %0\n"             /* p */
+ 	    "fldt %1\n"             /* x . p */
+ 	    "fxam\n"
+ 	    "fnstsw\n"
