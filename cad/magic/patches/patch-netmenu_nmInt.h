57a8114bdf60608bc79e8a36d6dd139ef2d5d7f8
joerg <joerg>
2013-03-03 01:11:10 +0000

    Fix inconsistent return types. Use stdarg.h more.

---
+++ cad/magic/patches/patch-netmenu_nmInt.h
@@ -0,0 +1,100 @@
+$NetBSD: patch-netmenu_nmInt.h,v 1.1 2013/03/03 01:11:11 joerg Exp $
+
+--- netmenu/nmInt.h.orig	2006-04-10 22:03:14.000000000 +0000
++++ netmenu/nmInt.h
+@@ -24,6 +24,8 @@
+ #define _NMINT_H
+ 
+ #include "windows/windows.h"
++#include "textio/txcommands.h"
++#include "tiles/tile.h"
+ 
+ /* The declarations below define the elements displayed in the
+  * net-list menu.  There are three kinds of things:
+@@ -108,26 +110,30 @@ extern void NMAddPoint();
+ extern void NMDeletePoint();
+ extern void NMClearPoints();
+ extern void NMUpdatePoints();
+-extern int NMRedrawPoints();
+ 
+-extern int NMRedrawCell();
+ extern void NMShowCell();
+ extern void NMUnsetCell();
+ extern void NMShowUnderBox();
+-extern int NMShowRoutedNet();
+ extern void NMShowLabel();
+ 
+-extern int NMRipup();
+ extern int NMRipupList();
+-extern int NMExtract();
+ extern void NMSelectNet();
+ 
+ extern bool NMCheckWritten();
+-extern int NMWriteAll();
+ 
+ extern void NMUndo();
+ extern void NMUndoInit();
+ 
++void NMcommand(MagWindow *w, TxCommand *cmd);
++void NMredisplay(MagWindow *w, Rect *rootArea, Rect *clipArea);
++void NMDeleteTerm(char *name);
++void NMWriteAll(void);
++void NMRedrawPoints(MagWindow *window, Plane *plane);
++void NMRedrawCell(MagWindow *window, Plane *plane);
++void NMShowRoutedNet(char *netName);
++void NMRipup(void);
++void NMExtract(void);
++
+ /* Various global variables (within this module): */
+ 
+ extern char * NMCurNetName;
+$NetBSD: patch-netmenu_nmInt.h,v 1.1 2013/03/03 01:11:11 joerg Exp $
+
+--- netmenu/nmInt.h.orig	2006-04-10 22:03:14.000000000 +0000
++++ netmenu/nmInt.h
+@@ -24,6 +24,8 @@
+ #define _NMINT_H
+ 
+ #include "windows/windows.h"
++#include "textio/txcommands.h"
++#include "tiles/tile.h"
+ 
+ /* The declarations below define the elements displayed in the
+  * net-list menu.  There are three kinds of things:
+@@ -108,26 +110,30 @@ extern void NMAddPoint();
+ extern void NMDeletePoint();
+ extern void NMClearPoints();
+ extern void NMUpdatePoints();
+-extern int NMRedrawPoints();
+ 
+-extern int NMRedrawCell();
+ extern void NMShowCell();
+ extern void NMUnsetCell();
+ extern void NMShowUnderBox();
+-extern int NMShowRoutedNet();
+ extern void NMShowLabel();
+ 
+-extern int NMRipup();
+ extern int NMRipupList();
+-extern int NMExtract();
+ extern void NMSelectNet();
+ 
+ extern bool NMCheckWritten();
+-extern int NMWriteAll();
+ 
+ extern void NMUndo();
+ extern void NMUndoInit();
+ 
++void NMcommand(MagWindow *w, TxCommand *cmd);
++void NMredisplay(MagWindow *w, Rect *rootArea, Rect *clipArea);
++void NMDeleteTerm(char *name);
++void NMWriteAll(void);
++void NMRedrawPoints(MagWindow *window, Plane *plane);
++void NMRedrawCell(MagWindow *window, Plane *plane);
++void NMShowRoutedNet(char *netName);
++void NMRipup(void);
++void NMExtract(void);
++
+ /* Various global variables (within this module): */
+ 
+ extern char * NMCurNetName;
