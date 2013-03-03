25b6570bf3fd8f2ea6cbaee3e3051fd69fa1a3bc
joerg <joerg>
2013-03-03 01:11:10 +0000

    Fix inconsistent return types. Use stdarg.h more.

---
+++ cad/magic/patches/patch-netmenu_NMmain.c
@@ -0,0 +1,66 @@
+$NetBSD: patch-netmenu_NMmain.c,v 1.1 2013/03/03 01:11:11 joerg Exp $
+
+--- netmenu/NMmain.c.orig	2013-03-02 22:39:15.000000000 +0000
++++ netmenu/NMmain.c
+@@ -238,13 +238,8 @@ NMreposition(window, newScreenArea, fina
+  * ----------------------------------------------------------------------------
+  */
+ 
+-int
+-NMredisplay(w, rootArea, clipArea)
+-    MagWindow *w;			/* The window containing the area. */
+-    Rect *rootArea;		/* Redisplay area in surface coordinates. */
+-    Rect *clipArea;		/* Screen area to clip to.  If NULL, use
+-				 * screen area of window. */
+-
++void
++NMredisplay(MagWindow *w, Rect *rootArea, Rect *clipArea)
+ {
+     Rect clip, screenR;
+     Point screenP;
+@@ -356,10 +351,8 @@ NMredisplay(w, rootArea, clipArea)
+  * ----------------------------------------------------------------------------
+  */
+ 
+-int
+-NMcommand(w, cmd)
+-    MagWindow *w;			/* Net-list menu window. */
+-    TxCommand *cmd;
++void
++NMcommand(MagWindow *w, TxCommand *cmd)
+ {
+     NetButton *nb;
+     Point surfacePoint;
+$NetBSD: patch-netmenu_NMmain.c,v 1.1 2013/03/03 01:11:11 joerg Exp $
+
+--- netmenu/NMmain.c.orig	2013-03-02 22:39:15.000000000 +0000
++++ netmenu/NMmain.c
+@@ -238,13 +238,8 @@ NMreposition(window, newScreenArea, fina
+  * ----------------------------------------------------------------------------
+  */
+ 
+-int
+-NMredisplay(w, rootArea, clipArea)
+-    MagWindow *w;			/* The window containing the area. */
+-    Rect *rootArea;		/* Redisplay area in surface coordinates. */
+-    Rect *clipArea;		/* Screen area to clip to.  If NULL, use
+-				 * screen area of window. */
+-
++void
++NMredisplay(MagWindow *w, Rect *rootArea, Rect *clipArea)
+ {
+     Rect clip, screenR;
+     Point screenP;
+@@ -356,10 +351,8 @@ NMredisplay(w, rootArea, clipArea)
+  * ----------------------------------------------------------------------------
+  */
+ 
+-int
+-NMcommand(w, cmd)
+-    MagWindow *w;			/* Net-list menu window. */
+-    TxCommand *cmd;
++void
++NMcommand(MagWindow *w, TxCommand *cmd)
+ {
+     NetButton *nb;
+     Point surfacePoint;
---
+++ cad/magic/patches/patch-plow_PlowRules2.c
@@ -0,0 +1,80 @@
+$NetBSD: patch-plow_PlowRules2.c,v 1.1 2013/03/03 01:11:11 joerg Exp $
+
+--- plow/PlowRules2.c.orig	2013-03-02 22:36:54.000000000 +0000
++++ plow/PlowRules2.c
+@@ -235,9 +235,8 @@ prFixedPenumbraTop(edge)
+     }
+ }
+ 
+-int
+-prFixedPenumbraBot(edge)
+-    Edge *edge;	/* Edge being moved */
++void
++prFixedPenumbraBot(Edge *edge)
+ {
+     struct applyRule ar;
+     PlowRule *pr;
+@@ -499,9 +498,8 @@ prCoverTop(edge)
+     }
+ }
+ 
+-int
+-prCoverBot(edge)
+-    Edge *edge;	/* Edge being moved */
++void
++prCoverBot(Edge *edge)
+ {
+     TileType ltype, rtype;
+     PlowRule *pr;
+@@ -585,9 +583,8 @@ prIllegalTop(edge)
+ 		plowCoverTopProc, (ClientData) &ar);
+ }
+ 
+-int
+-prIllegalBot(edge)
+-    Edge *edge;
++void
++prIllegalBot(Edge *edge)
+ {
+     TileTypeBitMask insideTypes;
+     struct applyRule ar;
+$NetBSD: patch-plow_PlowRules2.c,v 1.1 2013/03/03 01:11:11 joerg Exp $
+
+--- plow/PlowRules2.c.orig	2013-03-02 22:36:54.000000000 +0000
++++ plow/PlowRules2.c
+@@ -235,9 +235,8 @@ prFixedPenumbraTop(edge)
+     }
+ }
+ 
+-int
+-prFixedPenumbraBot(edge)
+-    Edge *edge;	/* Edge being moved */
++void
++prFixedPenumbraBot(Edge *edge)
+ {
+     struct applyRule ar;
+     PlowRule *pr;
+@@ -499,9 +498,8 @@ prCoverTop(edge)
+     }
+ }
+ 
+-int
+-prCoverBot(edge)
+-    Edge *edge;	/* Edge being moved */
++void
++prCoverBot(Edge *edge)
+ {
+     TileType ltype, rtype;
+     PlowRule *pr;
+@@ -585,9 +583,8 @@ prIllegalTop(edge)
+ 		plowCoverTopProc, (ClientData) &ar);
+ }
+ 
+-int
+-prIllegalBot(edge)
+-    Edge *edge;
++void
++prIllegalBot(Edge *edge)
+ {
+     TileTypeBitMask insideTypes;
+     struct applyRule ar;
