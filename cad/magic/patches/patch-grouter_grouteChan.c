65983538443640c6213e6ef82abbeaaec2c86f11
joerg <joerg@pkgsrc.org>
2013-03-03 01:11:10 +0000

    Fix inconsistent return types. Use stdarg.h more.

---
+++ cad/magic/patches/patch-grouter_grouteChan.c
@@ -0,0 +1,84 @@
+$NetBSD: patch-grouter_grouteChan.c,v 1.1 2013/03/03 01:11:10 joerg Exp $
+
+--- grouter/grouteChan.c.orig	2013-03-02 22:19:28.000000000 +0000
++++ grouter/grouteChan.c
+@@ -131,6 +131,11 @@ PaintArea *glChanPaintList;
+  * ----------------------------------------------------------------------------
+  */
+ 
++static void
++glChanShowTiles(char *mesg);
++static void
++glChanShowFunc(Tile *tile);
++
+ void
+ glChanBuildMap(chanList)
+     GCRChannel *chanList;	/* List of all channels in routing problem */
+@@ -435,11 +440,9 @@ glChanSetClient(tile, cdata)
+     return 0;
+ }
+ 
+-int
+-glChanShowTiles(mesg)
+-    char *mesg;
++static void
++glChanShowTiles(char *mesg)
+ {
+-    int glChanShowFunc();
+     char answer[100], m[1024];
+ 
+     DBWAreaChanged(glChanDef, &TiPlaneRect, DBW_ALLWINDOWS, 0);
+@@ -452,9 +455,8 @@ glChanShowTiles(mesg)
+ 	    &DBAllTypeBits, glChanShowFunc, (ClientData) NULL);
+ }
+ 
+-int
+-glChanShowFunc(tile)
+-    Tile *tile;
++static void
++glChanShowFunc(Tile *tile)
+ {
+     GCRChannel *ch;
+     char mesg[1024];
+$NetBSD: patch-grouter_grouteChan.c,v 1.1 2013/03/03 01:11:10 joerg Exp $
+
+--- grouter/grouteChan.c.orig	2013-03-02 22:19:28.000000000 +0000
++++ grouter/grouteChan.c
+@@ -131,6 +131,11 @@ PaintArea *glChanPaintList;
+  * ----------------------------------------------------------------------------
+  */
+ 
++static void
++glChanShowTiles(char *mesg);
++static void
++glChanShowFunc(Tile *tile);
++
+ void
+ glChanBuildMap(chanList)
+     GCRChannel *chanList;	/* List of all channels in routing problem */
+@@ -435,11 +440,9 @@ glChanSetClient(tile, cdata)
+     return 0;
+ }
+ 
+-int
+-glChanShowTiles(mesg)
+-    char *mesg;
++static void
++glChanShowTiles(char *mesg)
+ {
+-    int glChanShowFunc();
+     char answer[100], m[1024];
+ 
+     DBWAreaChanged(glChanDef, &TiPlaneRect, DBW_ALLWINDOWS, 0);
+@@ -452,9 +455,8 @@ glChanShowTiles(mesg)
+ 	    &DBAllTypeBits, glChanShowFunc, (ClientData) NULL);
+ }
+ 
+-int
+-glChanShowFunc(tile)
+-    Tile *tile;
++static void
++glChanShowFunc(Tile *tile)
+ {
+     GCRChannel *ch;
+     char mesg[1024];
---
+++ cad/magic/patches/patch-textio_textio.h
@@ -0,0 +1,40 @@
+$NetBSD: patch-textio_textio.h,v 1.1 2013/03/03 01:11:11 joerg Exp $
+
+--- textio/textio.h.orig	2013-03-02 22:26:17.000000000 +0000
++++ textio/textio.h
+@@ -25,6 +25,7 @@
+ #ifndef _TEXTIO_H
+ #define _TEXTIO_H
+ 
++#include <stdarg.h>
+ #include "utils/magic.h"
+ 
+ #ifdef MAGIC_WRAPPER
+@@ -65,6 +66,7 @@ extern void TxUseMore();
+ extern void TxStopMore();
+ 
+ /* printing procedures with variable arguments lists */
++extern void VTxError(char *, va_list);
+ extern void TxError(char *, ...);
+ extern void TxPrintf(char *, ...);
+ extern char *TxPrintString(char *, ...);
+$NetBSD: patch-textio_textio.h,v 1.1 2013/03/03 01:11:11 joerg Exp $
+
+--- textio/textio.h.orig	2013-03-02 22:26:17.000000000 +0000
++++ textio/textio.h
+@@ -25,6 +25,7 @@
+ #ifndef _TEXTIO_H
+ #define _TEXTIO_H
+ 
++#include <stdarg.h>
+ #include "utils/magic.h"
+ 
+ #ifdef MAGIC_WRAPPER
+@@ -65,6 +66,7 @@ extern void TxUseMore();
+ extern void TxStopMore();
+ 
+ /* printing procedures with variable arguments lists */
++extern void VTxError(char *, va_list);
+ extern void TxError(char *, ...);
+ extern void TxPrintf(char *, ...);
+ extern char *TxPrintString(char *, ...);
