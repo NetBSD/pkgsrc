1a5140cf99052bb9b7abf28a4a5be013803cfe7c
jperkin <jperkin@pkgsrc.org>
2013-03-06 16:58:57 +0000

    Add SunOS support.

--- lang/sun-jdk7/buildlink3.mk
+++ lang/sun-jdk7/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/12/11 14:51:33 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/03/06 16:58:57 jperkin Exp $
 
 BUILDLINK_TREE+=	sun-jdk7
 
@@ -12,9 +12,17 @@ BUILDLINK_DEPMETHOD.sun-jdk7?= build
 
 BUILDLINK_PASSTHRU_DIRS+=	${BUILDLINK_JAVA_PREFIX.sun-jre7}
 
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} == "SunOS"
+BUILDLINK_CPPFLAGS.sun-jdk7=						\
+	-I${BUILDLINK_JAVA_PREFIX.sun-jre7}/include			\
+	-I${BUILDLINK_JAVA_PREFIX.sun-jre7}/include/solaris
+.else
 BUILDLINK_CPPFLAGS.sun-jdk7=						\
 	-I${BUILDLINK_JAVA_PREFIX.sun-jre7}/include			\
 	-I${BUILDLINK_JAVA_PREFIX.sun-jre7}/include/linux
+.endif
 
 .include "../../lang/sun-jre7/buildlink3.mk"
 .endif # SUN_JDK7_BUILDLINK3_MK
