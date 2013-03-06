78edbf4b7e16f302f54d03f9a1b8d702375071a5
obache <obache@pkgsrc.org>
2013-03-06 12:37:15 +0000

    Import user-20130306 as sysutils/user_cygwin.
    This implements a subset of useradd(8)/groupadd(8) functionality on
    Cygwin that is sufficient for pkgsrc use.

---
+++ sysutils/user_cygwin/files/groupadd.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# $NetBSD: groupadd.sh,v 1.1 2013/03/06 12:37:15 obache Exp $
+
+export PATH=/bin:$PATH
+
+show_usage () {
+	echo "usage: $0 [-v] group" >&2
+	exit 1
+}
+
+verbose=false
+run_cmd () {
+	if $verbose; then printf '%s\n' "+ $*"; fi
+	"$@"
+}
+
+while getopts 'g:ov' f; do
+	case $f in
+	g)	echo "$0: numeric gid cannot be set manually; ignoring" >&2;;
+	o)	echo "$0: cannot reuse numeric gid of another group; aborting" >&2; exit 1;;
+	v)	verbose=true;;
+	\?)	show_usage;;
+	esac
+done
+shift $(($OPTIND - 1))
+
+if [ $# -ne 1 ]; then show_usage; fi
+$verbose || exec >/dev/null
+
+run_cmd net localgroup $1 /add /comment:"Group added by Cygwin groupadd command" || exit 1
+### regenerate cygwin /etc/group
+(/bin/flock -x -n 9 || exit 1; /bin/mkgroup -l -g "$1" >&9 ) 9>> /etc/group
---
+++ sysutils/user_cygwin/files/groupdel.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# $NetBSD: groupdel.sh,v 1.1 2013/03/06 12:37:16 obache Exp $
+
+export PATH=/bin:$PATH
+
+show_usage () {
+	echo "usage: $0 [-v] group" >&2
+	exit 1
+}
+
+verbose=false
+run_cmd () {
+	if $verbose; then printf '%s\n' "+ $*"; fi
+	"$@"
+}
+
+while getopts 'v' f; do
+	case $f in
+	v)	verbose=true;;
+	\?)	show_usage;;
+	esac
+done
+shift $(($OPTIND - 1))
+
+if [ $# -ne 1 ]; then show_usage; fi
+$verbose || exec >/dev/null
+
+run_cmd net localgroup $1 /delete || exit 1
+/bin/sed -i -e "/^$1.*/d" /etc/group
