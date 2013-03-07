cd590cf591d305d2b9c75c485e81116d13eaf676
joerg <joerg@pkgsrc.org>
2013-03-07 22:05:12 +0000

    Remove accidentally duplicated patches.

--- cad/magic/patches/patch-extflat_EFvisit.c
+++ cad/magic/patches/patch-extflat_EFvisit.c
@@ -1,17 +1,4 @@
-$NetBSD: patch-extflat_EFvisit.c,v 1.1 2013/03/03 01:11:10 joerg Exp $
-
---- extflat/EFvisit.c.orig	2013-03-02 22:43:10.000000000 +0000
-+++ extflat/EFvisit.c
-@@ -576,7 +576,7 @@ efVisitSingleResist(hc, name1, name2, re
- 	return 0;
- 
-     if ((he = EFHNLook(hc->hc_hierName, name2, "resist(2)")) == NULL)
--	return;
-+	return 0;
-     n2 = ((EFNodeName *) HashGetValue(he))->efnn_node;
-     if (n2->efnode_flags & EF_KILLED)
- 	return 0;
-$NetBSD: patch-extflat_EFvisit.c,v 1.1 2013/03/03 01:11:10 joerg Exp $
+$NetBSD: patch-extflat_EFvisit.c,v 1.2 2013/03/07 22:05:12 joerg Exp $
 
 --- extflat/EFvisit.c.orig	2013-03-02 22:43:10.000000000 +0000
 +++ extflat/EFvisit.c
--- cad/magic/patches/patch-netmenu_NMbutton.c
+++ cad/magic/patches/patch-netmenu_NMbutton.c
@@ -1,17 +1,4 @@
-$NetBSD: patch-netmenu_NMbutton.c,v 1.1 2013/03/03 01:11:11 joerg Exp $
-
---- netmenu/NMbutton.c.orig	2013-03-02 22:48:07.000000000 +0000
-+++ netmenu/NMbutton.c
-@@ -79,7 +79,7 @@ NMButtonNetList(window, cmd, nmButton, p
-     else
-     {
- 	TxPrintf("New net list name: ");
--	if (TxGetLine(newName, MAXLENGTH) == NULL) newName[0] == 0;
-+	if (TxGetLine(newName, MAXLENGTH) == NULL) newName[0] = 0;
- 	if (newName[0] == 0) return;
- 	NMNewNetlist(newName);
-     }
-$NetBSD: patch-netmenu_NMbutton.c,v 1.1 2013/03/03 01:11:11 joerg Exp $
+$NetBSD: patch-netmenu_NMbutton.c,v 1.2 2013/03/07 22:05:13 joerg Exp $
 
 --- netmenu/NMbutton.c.orig	2013-03-02 22:48:07.000000000 +0000
 +++ netmenu/NMbutton.c
