c71caa5015075c430fe91d1a24c000b270372081
joerg <joerg>
2013-03-07 22:05:12 +0000

    Remove accidentally duplicated patches.

--- cad/magic/patches/patch-grouter_grouteDens.c
+++ cad/magic/patches/patch-grouter_grouteDens.c
@@ -1,17 +1,4 @@
-$NetBSD: patch-grouter_grouteDens.c,v 1.1 2013/03/03 01:11:10 joerg Exp $
-
---- grouter/grouteDens.c.orig	2013-03-02 22:20:54.000000000 +0000
-+++ grouter/grouteDens.c
-@@ -78,7 +78,7 @@ glDensAdjust(dens, srcPin, dstPin, netid
-     ASSERT(srcPin->gcr_ch == dstPin->gcr_ch, "glDensAdjust");
- 
-     if (DebugIsSet(glDebugID, glDebGreedy))
--	return;
-+	return FALSE;
- 
-     /*
-      * Find the first and last column where this net (netId)
-$NetBSD: patch-grouter_grouteDens.c,v 1.1 2013/03/03 01:11:10 joerg Exp $
+$NetBSD: patch-grouter_grouteDens.c,v 1.2 2013/03/07 22:05:12 joerg Exp $
 
 --- grouter/grouteDens.c.orig	2013-03-02 22:20:54.000000000 +0000
 +++ grouter/grouteDens.c
--- cad/magic/patches/patch-netmenu_NMwiring.c
+++ cad/magic/patches/patch-netmenu_NMwiring.c
@@ -1,30 +1,4 @@
-$NetBSD: patch-netmenu_NMwiring.c,v 1.1 2013/03/03 01:11:11 joerg Exp $
-
---- netmenu/NMwiring.c.orig	2013-03-02 23:01:19.000000000 +0000
-+++ netmenu/NMwiring.c
-@@ -152,8 +152,8 @@ nmwRipTileFunc(tile, plane, listHead)
-  * ----------------------------------------------------------------------------
-  */
- 
--int
--NMRipup()
-+void
-+NMRipup(void)
- {
-     struct nmwarea *list;
-     Rect area;
-@@ -531,8 +531,8 @@ nmwNetTileFunc(tile, plane, netPtr)
-  * ----------------------------------------------------------------------------
-  */
- 
--int
--NMExtract()
-+void
-+NMExtract(void)
- {
-     Rect area;
-     char *net = NULL;
-$NetBSD: patch-netmenu_NMwiring.c,v 1.1 2013/03/03 01:11:11 joerg Exp $
+$NetBSD: patch-netmenu_NMwiring.c,v 1.2 2013/03/07 22:05:13 joerg Exp $
 
 --- netmenu/NMwiring.c.orig	2013-03-02 23:01:19.000000000 +0000
 +++ netmenu/NMwiring.c
