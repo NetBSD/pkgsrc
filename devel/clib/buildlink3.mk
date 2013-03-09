cebd6768aeaf0aa7b610186c09212610144f09da
tonnerre <tonnerre@pkgsrc.org>
2013-03-09 00:23:26 +0000

    Update clib to version 0.3.
    Changes since version 0.2.2:
     - Added additional socket functions (str2addrinfo, str2sockaddr,
       connect2addrinfo and bind2addrinfo).
     - C++ compatibility (no variables named new, etc).
     - Use more const.
     - NetBSD compatibility for getnameinfo().
     - Bugfixes.

--- devel/clib/buildlink3.mk
+++ devel/clib/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/04/27 23:52:53 tonnerre Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/03/09 00:23:26 tonnerre Exp $
 
 BUILDLINK_TREE+=	clib
 
 .if !defined(CLIB_BUILDLINK3_MK)
 CLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clib+=	clib>=0.2
-BUILDLINK_ABI_DEPENDS.clib+=	clib>=0.2
+BUILDLINK_API_DEPENDS.clib+=	clib>=0.3
+BUILDLINK_ABI_DEPENDS.clib+=	clib>=0.3
 BUILDLINK_PKGSRCDIR.clib?=	../../devel/clib
 .endif # CLIB_BUILDLINK3_MK
 
