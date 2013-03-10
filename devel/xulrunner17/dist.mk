c806c592d4a60ed987cd2f87703a1dffd1a88e57
ryoon <ryoon@pkgsrc.org>
2013-03-10 11:35:57 +0000

    Update to 17.0.4
    Changelog:
    FIXED
    17.0.4 : Security-driven release, see details in the associated security advisory
    
    Fixed in Firefox ESR 17.0.4
    MFSA 2013-29 Use-after-free in HTML Editor

--- devel/xulrunner17/dist.mk
+++ devel/xulrunner17/dist.mk
@@ -1,11 +1,11 @@
-# $NetBSD: dist.mk,v 1.3 2013/02/24 14:18:00 ryoon Exp $
+# $NetBSD: dist.mk,v 1.4 2013/03/10 11:35:57 ryoon Exp $
 #
 # used by devel/xulrunner17/Makefile
 # used by www/firefox17/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	17.0.3
+MOZ_BRANCH=	17.0.4
 MOZ_BRANCH_MINOR=	esr
 MASTER_SITES=	${MASTER_SITE_MOZILLA_ESR:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
@@ -13,7 +13,5 @@ EXTRACT_SUFX=	.tar.bz2
 
 DISTINFO_FILE=	${.CURDIR}/../../devel/xulrunner17/distinfo
 PATCHDIR=	${.CURDIR}/../../devel/xulrunner17/patches
-
-DIST_SUBDIR=	firefox17.0.2esr
 
 WRKSRC=		${WRKDIR}/mozilla-esr17
