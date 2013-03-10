8d5914731f0fb04fcc12b10c075fefc27eb8165d
ryoon <ryoon@pkgsrc.org>
2013-03-10 00:22:16 +0000

    Update to 19.0.2
    Changelog:
    FIXED
    19.0.2: Security-driven release, see details in the associated security advisory
    FIXED
    19.0.1 (Windows 8 only): Fixed stability issue for some AMD Radeon HD graphics cards
    
    Fixed in Firefox 19.0.2
    MFSA 2013-29 Use-after-free in HTML Editor

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.43 2013/02/22 14:48:06 ryoon Exp $
+# $NetBSD: dist.mk,v 1.44 2013/03/10 00:22:16 ryoon Exp $
 #
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
@@ -6,7 +6,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	19.0
-MOZ_BRANCH_MINOR=	
+MOZ_BRANCH_MINOR=	.2
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
