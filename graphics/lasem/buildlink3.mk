7645a52348cde5488f5e99279c42b8ed4e8572f3
drochner <drochner@pkgsrc.org>
2013-03-14 18:47:29 +0000

    add lasem, a library for rendering SVG and Mathml, from jihbed per pkgsrc-wip

---
+++ graphics/lasem/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/03/14 18:47:29 drochner Exp $
+
+BUILDLINK_TREE+=	lasem
+
+.if !defined(LASEM_BUILDLINK3_MK)
+LASEM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.lasem+=	lasem>=0.2.0
+BUILDLINK_ABI_DEPENDS.lasem?=	lasem>=0.2.0nb1
+BUILDLINK_PKGSRCDIR.lasem?=	../../graphics/lasem
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../graphics/gdk-pixbuf2/buildlink3.mk"
+
+.endif	# LASEM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-lasem
