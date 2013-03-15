e14eaf9d56388c2c18a538c5b6e5ea30de940e60
plunky <plunky@pkgsrc.org>
2013-03-15 08:18:43 +0000

    Update to ObexFTP 0.24
    There is a new maintainer, Hendrik Sattler and the sources are held
    at gitorius.org. Build is changed to CMake and although it does not appear
    in the ChangeLog below, I forwarded all the NetBSD related patches that
    pkgsrc had (and more in fact, to get it working) although I would still say
    that obexapp is the better program.
    
    
    From the ChangeLog:
    
    ObexFTP 0.24 (released 2013-03-05)
    ----------------------------------
    
    	* Maintainer changed from Christian to Hendrik
    	* change build system to using CMake
    	* integrate obexfs-0.12
    	* fix build for OpenOBEX-1.7
    
    
    ObexFTP 0.23 (released 2009-02-17)
    ----------------------------------
    
    	* allow win32 to use hci src names
    	* adding a simpler connect wrapper
    	* show OBEX_HandleInput errors
    	* catch errors and let the user know
    	* print timeout stats if available
    	* sdp unregister more verbose
    	* upgrading btkit
    	* fix for win32 without bt
    	* replacing deprecated automake vars
    	* sizeof() fixes
    	* removing bdaddr_t reference from obexftpd.c
    	* adding bootstrap helper
    	* concurrency bug in extconf.rb generated Makefile (fix by Alin Năstac)
    	* switching from POD to asciidoc
    	* clearing gnu-style implicit rules
    
    
    ObexFTP 0.22 (released 2008-06-15)
    ----------------------------------
    
    	* added proper unicode support
    	* added support for transparent OBEX-over-AT mode
    	* rewritten at-command function
    	* added specific error messages
    	* refactored to flexible bt_kit layer
    	* fixed cache root duplicates
    	* fixed off-by-one and unfreed mem in cache layer
    	* added pkg-config file
    	* added example code
    	* switched to doxygen
    	* added python binding callbacks
    	* portable packed structs
    	* enabled linux hci dev names for source selection
    	* Python binding uses distutils now, tested by Adam Williamson
    	* removed exit from bt discovery
    	* Better autodetection for possible language bindings
    	* reworked win32 support
    	* Motorola SLVR L2 cobex fix by Andrey Rahmatullin
    	* now using AC_HELP_STRING for compat with autoconf <=2.57
    	* added hci selection support, drafted by Manuel Naranjo
    	* switched obexftp cli to new discovery api
    	* prefer PCSUITE over FTP, req. by Martin Storsjö for Series 60 2nd Ed.
    	* fixed compile error with >=swig-1.3.28
    	* renamed sdp browse function
    	* fixed month/day swapping in atotime, spotted by Dr. Johannes Zellner
    	* added BFC compatibility for newer Siemens phones
    	* added PCSOFTWARE uuid support for SHARP phones
    	* added motorola support
    	* end bfb mode properly
    	* added CPROT=0 support from 3GPP 27.007
    	* fixed ericsson init
    	* fixed invalid conn_id in disconnect rep. by Alan J. McFarlane
    	* better create flag handling in setpath
    	* Changed LDADD to LIBADD sug. by Sergey Vlasov <vsu@altlinux.ru>
    	* obexftpd clean up by Hendrik Sattler
    	* 64-bit fixes by Hendrik Sattler
    	* Removed all (dangerous) obex_headerdata_t casts
    	* Reorganized all swig-dependant Makefiles
    	* Applied cobex write patch from Simon Ruggier <Simon80@gmail.com>
    	* Applied   from Frode Isaksen <fisaksen@bewan.com>
    
    
    ObexFTP 0.21 (released 2006-06-27)
    ----------------------------------
    
    2006-05-26  Christian W. Zuckschwerdt  <zany@triq.net>
    
    	* Fixes to obexftpd suggested by Hendrik Sattler
    
    2006-05-24  Christian W. Zuckschwerdt  <zany@triq.net>
    
    	* Added ruby binding
    	* Added preliminary discovery function

--- comms/obexftp/buildlink3.mk
+++ comms/obexftp/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:02 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/03/15 08:18:43 plunky Exp $
 
 BUILDLINK_TREE+=	obexftp
 
 .if !defined(OBEXFTP_BUILDLINK3_MK)
 OBEXFTP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.obexftp+=	obexftp>=0.20nb1
+BUILDLINK_API_DEPENDS.obexftp+=	obexftp>=0.24
 BUILDLINK_PKGSRCDIR.obexftp?=	../../comms/obexftp
 
 .include "../../comms/openobex/buildlink3.mk"
