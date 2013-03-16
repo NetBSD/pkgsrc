bfd2fb06d0908ccaf22a8f344bef9ee83c9b2d95
dholland <dholland@pkgsrc.org>
2013-03-16 21:32:00 +0000

    Copy the pre-update ghostscript 9.05 package as ghostscript-gpl.

---
+++ print/ghostscript-gpl/buildlink3.mk
@@ -0,0 +1,33 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/03/16 21:32:00 dholland Exp $
+
+BUILDLINK_TREE+=	ghostscript-gpl
+
+.if !defined(GHOSTSCRIPT_GPL_BUILDLINK3_MK)
+GHOSTSCRIPT_GPL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ghostscript-gpl+=	ghostscript-gpl>=8.63nb1
+BUILDLINK_ABI_DEPENDS.ghostscript-gpl+=	ghostscript-gpl>=9.05nb3
+BUILDLINK_PKGSRCDIR.ghostscript-gpl?=	../../print/ghostscript-gpl
+
+.include "../../graphics/png/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+
+pkgbase := ghostscript-gpl
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.ghostscript-gpl:Mcups)
+.include "../../print/cups/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ghostscript-gpl:Mfontconfig)
+.include "../../fonts/fontconfig/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ghostscript-gpl:Mx11)
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.endif
+.endif # GHOSTSCRIPT_GPL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ghostscript-gpl
