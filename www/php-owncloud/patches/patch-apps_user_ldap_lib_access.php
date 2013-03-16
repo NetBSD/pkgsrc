6ef203b350573789e00b1332bb70eef6320abe70
bouyer <bouyer@pkgsrc.org>
2013-03-16 17:27:27 +0000

    This requires php-pdo, add it to the depends list NetBSD's iconv doesn't support ASCII//TRANSLIT, use plain ASCII instead. bump PKGREVISION

---
+++ www/php-owncloud/patches/patch-apps_user_ldap_lib_access.php
@@ -0,0 +1,11 @@
+--- apps/user_ldap/lib/access.php.orig	2013-03-16 18:19:18.000000000 +0100
++++ apps/user_ldap/lib/access.php	2013-03-16 18:19:35.000000000 +0100
+@@ -602,7 +602,7 @@
+ 
+ 		// Translitaration
+ 		//latin characters to ASCII
+-		$name = iconv('UTF-8', 'ASCII//TRANSLIT', $name);
++		$name = iconv('UTF-8', 'ASCII', $name);
+ 
+ 		//REPLACEMENTS
+ 		$name = \OCP\Util::mb_str_replace(' ', '_', $name, 'UTF-8');
