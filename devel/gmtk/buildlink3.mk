b95385b12c17dbe5609adbe2f4234728743efb1c
obache <obache>
2013-03-16 10:43:43 +0000

    Update gmtk to 1.0.8.
    1.0.8
        Updated Spanish translation
        Updated Japanese translation
    	Rework initial subtitle visibility setting
    1.0.8b
        Updated Korean translation
        Run make update-po
        Add message when screenshot capture fails
        Switch to GLIB for GMLIB header files, since GTK is not included
        Switch to GTK VERSION tags when possible
        Updated Japanese translation
        Fix compliation of gmtk_media_tracker in generic application
        Run make update-po
        Remove audio export filter when codec cannot use it
        Remove cairo variable from gmtk_media_player
        Version bump to 1.0.8a
        Use cairo and the draw event to draw the background in GTK3
        Set background color in gmtk_media_player widget using specific realize events
        Address parallel build problem from Issue #11

--- devel/gmtk/buildlink3.mk
+++ devel/gmtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2013/02/16 11:18:53 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2013/03/16 10:43:43 obache Exp $
 #
 
 BUILDLINK_TREE+=	gmtk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gmtk
 .if !defined(GMTK_BUILDLINK3_MK)
 GMTK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gmtk+=	gmtk>=1.0.6
+BUILDLINK_API_DEPENDS.gmtk+=	gmtk>=1.0.7
 BUILDLINK_ABI_DEPENDS.gmtk+=	gmtk>=1.0.7nb1
 BUILDLINK_PKGSRCDIR.gmtk?=	../../devel/gmtk
 
