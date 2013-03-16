e68592e39156348f5fef894410d719c1f5e27443
obache <obache>
2013-03-16 06:20:45 +0000

    recursive bump from ffmpeg shlib major changes

--- net/libdlna/buildlink3.mk
+++ net/libdlna/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2012/09/22 13:48:20 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/03/16 06:20:45 obache Exp $
 #
 
 BUILDLINK_TREE+=	libdlna
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	libdlna
 LIBDLNA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libdlna+=	libdlna>=0.2.3
-BUILDLINK_ABI_DEPENDS.libdlna+=	libdlna>=0.2.3nb2
+BUILDLINK_ABI_DEPENDS.libdlna+=	libdlna>=0.2.4nb1
 BUILDLINK_PKGSRCDIR.libdlna?=	../../net/libdlna
 
 .include "../../multimedia/ffmpeg/buildlink3.mk"
