37ed4e10b22b32c717db0eb45a53744781c6901c
jperkin <jperkin>
2013-03-18 14:01:24 +0000

    Ensure the Darwin libiconv_open hack is only applied when using the native iconv.

--- devel/pkg-config/hacks.mk
+++ devel/pkg-config/hacks.mk
@@ -1,15 +1,20 @@
-# $NetBSD: hacks.mk,v 1.1 2012/08/23 13:34:44 adam Exp $
+# $NetBSD: hacks.mk,v 1.2 2013/03/18 14:01:24 jperkin Exp $
 
 .if !defined(PKGCONFIG_HACKS_MK)
 PKGCONFIG_HACKS_MK=	# defined
 
 .if ${OPSYS} == "Darwin"
+CHECK_BUILTIN.libiconv:=	yes
+.  include "../../converters/libiconv/builtin.mk"
+CHECK_BUILTIN.libiconv:=	no
+.  if !empty(USE_BUILTIN.iconv:M[Yy][Ee][Ss])
 PKG_HACKS+=		darwin-iconv
 SUBST_CLASSES+=		iconv
 SUBST_STAGE.iconv=	pre-configure
 SUBST_MESSAGE.iconv=	Changing libiconv_open to iconv_open.
 SUBST_FILES.iconv=	glib/configure
 SUBST_SED.iconv=	-e 's,libiconv_open,iconv_open,g'
+.  endif
 .endif
 
 .endif
