18b91750e2a23727b71f58d74e453ecdd40128e0
ryoon <ryoon@pkgsrc.org>
2013-03-19 15:10:09 +0000

    No PKGREVISION bump.
    * Add NetBSD keyword to patch.
    * Fix PLIST.

--- www/php-owncloud/patches/patch-apps_user_ldap_lib_access.php
+++ www/php-owncloud/patches/patch-apps_user_ldap_lib_access.php
@@ -1,3 +1,5 @@
+$NetBSD: patch-apps_user_ldap_lib_access.php,v 1.2 2013/03/19 15:10:09 ryoon Exp $
+
 --- apps/user_ldap/lib/access.php.orig	2013-03-16 18:19:18.000000000 +0100
 +++ apps/user_ldap/lib/access.php	2013-03-16 18:19:35.000000000 +0100
 @@ -602,7 +602,7 @@
