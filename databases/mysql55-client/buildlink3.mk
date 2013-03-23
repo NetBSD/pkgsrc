71449d339d5e71c169c4cd8b35e2c2ea945e2b0b
joerg <joerg@pkgsrc.org>
2013-03-23 16:01:58 +0000

    Don't pick up MySQL 5.6+

--- databases/mysql55-client/buildlink3.mk
+++ databases/mysql55-client/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/02/06 23:20:54 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/03/23 16:01:58 joerg Exp $
 
 BUILDLINK_TREE+=	mysql-client
 
 .if !defined(MYSQL_CLIENT_BUILDLINK3_MK)
 MYSQL_CLIENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mysql-client+=	mysql-client>=5.5.10
-BUILDLINK_ABI_DEPENDS.mysql-client+=	mysql-client>=5.5.29nb1
+BUILDLINK_API_DEPENDS.mysql-client+=	mysql-client>=5.5.10<5.6
+BUILDLINK_ABI_DEPENDS.mysql-client+=	mysql-client>=5.5.29nb1<5.6
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql55-client
 BUILDLINK_INCDIRS.mysql-client?=	include/mysql
 BUILDLINK_LIBDIRS.mysql-client?=	lib
--- databases/mysql55-server/buildlink3.mk
+++ databases/mysql55-server/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/02/06 23:21:30 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/03/23 16:01:58 joerg Exp $
 
 BUILDLINK_TREE+=	mysql-server
 
 .if !defined(MYSQL_SERVER_BUILDLINK3_MK)
 MYSQL_SERVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mysql-server+=	mysql-server>=5.5.11
-BUILDLINK_ABI_DEPENDS.mysql-server?=	mysql-server>=5.5.29nb1
+BUILDLINK_API_DEPENDS.mysql-server+=	mysql-server>=5.5.11<5.6
+BUILDLINK_ABI_DEPENDS.mysql-server?=	mysql-server>=5.5.29nb1<5.6
 BUILDLINK_PKGSRCDIR.mysql-server?=	../../databases/mysql55-server
 BUILDLINK_LIBDIRS.mysql-server?=	lib
 
