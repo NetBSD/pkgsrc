a03c8f359de2e8288c9ddadfd100f5a6ae48390e
joerg <joerg@pkgsrc.org>
2013-03-24 16:52:35 +0000

    Don't use VLAs of non-POD types.

--- math/aamath/patches/patch-expr.cc
+++ math/aamath/patches/patch-expr.cc
@@ -1,6 +1,6 @@
-$NetBSD: patch-expr.cc,v 1.1 2011/11/25 22:04:43 joerg Exp $
+$NetBSD: patch-expr.cc,v 1.2 2013/03/24 16:52:35 joerg Exp $
 
---- expr.cc.orig	2011-11-25 13:49:36.000000000 +0000
+--- expr.cc.orig	2005-03-08 02:00:02.000000000 +0000
 +++ expr.cc
 @@ -1192,7 +1192,7 @@ IntegralOnInterval::render_head() const
  
@@ -20,3 +20,21 @@ $NetBSD: patch-expr.cc,v 1.1 2011/11/25 22:04:43 joerg Exp $
  };
  
  void
+@@ -1612,7 +1612,7 @@ Matrix::render() const
+ 	int cols = num_cols();
+ 	int rows = num_rows();
+ 
+-	CanvasPtr ec[cols * rows];
++	CanvasPtr *ec = new CanvasPtr[cols * rows];
+ 	Size sz[cols * rows];
+ 
+ 	int row_height[rows];
+@@ -1692,6 +1692,8 @@ Matrix::render() const
+ 
+ 	canvas->center();
+ 
++	delete[] ec;
++
+ 	return canvas;
+ }
+ 
