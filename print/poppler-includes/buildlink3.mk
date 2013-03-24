524279ea5d26b63a97d2433a7780bfec75829814
spz <spz@pkgsrc.org>
2013-03-24 16:56:26 +0000

    poppler-includes and the buildlink3.mk escaped commit with the poppler 0.20.5 -> 0.22.2 update; fix that

--- print/poppler-includes/buildlink3.mk
+++ print/poppler-includes/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2012/07/04 20:26:58 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2013/03/24 16:56:26 spz Exp $
 
 BUILDLINK_DEPMETHOD.poppler-includes?=	build
 
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	poppler-includes
 .if !defined(POPPLER_INCLUDES_BUILDLINK3_MK)
 POPPLER_INCLUDES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.poppler-includes+=	poppler-includes>=0.20.1
+BUILDLINK_API_DEPENDS.poppler-includes+=	poppler-includes>=0.22.2
 BUILDLINK_PKGSRCDIR.poppler-includes?=	../../print/poppler-includes
 .endif # POPPLER_INCLUDES_BUILDLINK3_MK
 
