24a0ae3c4c5ecf9d8e6386112afb2b1b791e1970
ryoon <ryoon@pkgsrc.org>
2013-03-28 14:04:41 +0000

    Fix buildlink target.
    lib/sulrunner-sdk is obsolete now.
    Buildlink lib/xulrunner instead of -sdk.
    
    Fix firefox build.

--- devel/xulrunner/buildlink3.mk
+++ devel/xulrunner/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.33 2013/02/16 11:19:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2013/03/28 14:04:41 ryoon Exp $
 
 BUILDLINK_TREE+=	xulrunner
 
@@ -9,12 +9,12 @@ BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=11.0
 BUILDLINK_ABI_DEPENDS.xulrunner+=	xulrunner>=18.0.2nb1
 BUILDLINK_PKGSRCDIR.xulrunner?=		../../devel/xulrunner
 
-BUILDLINK_INCDIRS.xulrunner+=		lib/xulrunner-sdk/include
+BUILDLINK_INCDIRS.xulrunner+=		lib/xulrunner/include
 BUILDLINK_INCDIRS.xulrunner+=		include/xulrunner
-BUILDLINK_FILES.xulrunner+=		lib/xulrunner-sdk/include/*.h
+BUILDLINK_FILES.xulrunner+=		lib/xulrunner/include/*.h
 
-BUILDLINK_LIBDIRS.xulrunner+=		lib/xulrunner-sdk/lib
-BUILDLINK_FILES.xulrunner+=		lib/xulrunner-sdk/lib/*.so
+BUILDLINK_LIBDIRS.xulrunner+=		lib/xulrunner/lib
+BUILDLINK_FILES.xulrunner+=		lib/xulrunner/lib/*.so
 
 pkgbase := xulrunner
 .include "../../mk/pkg-build-options.mk"
