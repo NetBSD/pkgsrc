e48ee62e84880e6570d94b8fb72ac399e328feb9
joerg <joerg>
2013-03-28 21:43:05 +0000

    Forward declaration must not be qualified with a namespace. Adjust.

---
+++ sysutils/k3b-kde3/patches/patch-libk3b_projects_datacd_k3bisoimager.h
@@ -0,0 +1,12 @@
+$NetBSD: patch-libk3b_projects_datacd_k3bisoimager.h,v 1.1 2013/03/28 21:43:11 joerg Exp $
+
+--- libk3b/projects/datacd/k3bisoimager.h.orig	2013-03-27 21:04:37.000000000 +0000
++++ libk3b/projects/datacd/k3bisoimager.h
+@@ -29,7 +29,6 @@ class K3bFileItem;
+ class QTextStream;
+ class K3bProcess;
+ class KProcess;
+-class K3bDevice::Device;
+ class KTempFile;
+ 
+ 
---
+++ sysutils/k3b-kde3/patches/patch-libk3b_projects_k3babstractwriter.h
@@ -0,0 +1,12 @@
+$NetBSD: patch-libk3b_projects_k3babstractwriter.h,v 1.1 2013/03/28 21:43:11 joerg Exp $
+
+--- libk3b/projects/k3babstractwriter.h.orig	2013-03-27 21:05:23.000000000 +0000
++++ libk3b/projects/k3babstractwriter.h
+@@ -22,7 +22,6 @@
+ 
+ #include <qdatetime.h>
+ 
+-class K3bDevice::Device;
+ class K3bJobHandler;
+ 
+ 
