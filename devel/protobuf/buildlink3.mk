871fed09b4e7a134338364d900ffdeef781f54dd
asau <asau@pkgsrc.org>
2013-03-29 09:17:26 +0000

    Make it depend on zlib explicitly. This fixes linking problems on platforms with no builtin zlib. Found by Jason Bacon on CentOS.

--- devel/protobuf/buildlink3.mk
+++ devel/protobuf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/07/20 03:13:48 agc Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/03/29 09:17:26 asau Exp $
 
 BUILDLINK_TREE+=	protobuf
 
@@ -8,6 +8,7 @@ PROTOBUF_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.protobuf+=	protobuf>=2.4.1
 BUILDLINK_PKGSRCDIR.protobuf?=	../../devel/protobuf
 
+.include "../../devel/zlib/buildlink3.mk"
 .endif	# PROTOBUF_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-protobuf
