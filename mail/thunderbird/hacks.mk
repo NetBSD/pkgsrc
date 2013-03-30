605b5982fc7f7f6c8f4c6fe8c95c0bb5a35c4199
tron <tron>
2013-03-30 23:31:35 +0000

    Get this package closer to build under Solaris 10: 1.) Fix broken "yasm" version check which only accepts version numbers     like "a.b.c.d" but not like "a.b.c" and therefore fails with     Yasm 1.2.0. This probably affects other platforms (e.g. Linux     as well). 2.) Use "-R" instead of non-portable "-rpath" linker option.
    The build under Solaris 10 fails now during the build phase and not
    already in the configuration phase.

--- mail/thunderbird/hacks.mk
+++ mail/thunderbird/hacks.mk
@@ -1,6 +1,6 @@
-# $NetBSD: hacks.mk,v 1.2 2012/06/12 19:37:49 ryoon Exp $
+# $NetBSD: hacks.mk,v 1.3 2013/03/30 23:31:36 tron Exp $
 
 # workround for link of thunderbird-bin etc.
-LDFLAGS+=	-Wl,-rpath,${PREFIX}/lib/thunderbird
+LDFLAGS+=	-Wl,-R${PREFIX}/lib/thunderbird
 
 .include "../../devel/xulrunner/hacks.mk"
