48f1af76ee1375ef936fc734314a9cdf63abd4c5
obache <obache@pkgsrc.org>
2013-03-31 08:13:27 +0000

    missing recursive bump from poppler-0.22.2 shlib major change.

--- print/poppler-cpp/buildlink3.mk
+++ print/poppler-cpp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2013/02/16 11:19:29 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2013/03/31 08:13:28 obache Exp $
 
 BUILDLINK_TREE+=	poppler-cpp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-cpp
 POPPLER_CPP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-cpp+=	poppler-cpp>=0.16.7
-BUILDLINK_ABI_DEPENDS.poppler-cpp+=	poppler-cpp>=0.20.5nb3
+BUILDLINK_ABI_DEPENDS.poppler-cpp+=	poppler-cpp>=0.22.2
 BUILDLINK_PKGSRCDIR.poppler-cpp?=	../../print/poppler-cpp
 
 .include "../../print/poppler/buildlink3.mk"
