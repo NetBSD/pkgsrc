4015f157e0eb332dd9a7755ed6aff1a3a73ed59a
wiz <wiz>
2013-04-01 16:03:48 +0000

    revbump for libzip shlib major change.

--- textproc/ebook-tools/buildlink3.mk
+++ textproc/ebook-tools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2012/06/14 07:43:21 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/04/01 16:03:49 wiz Exp $
 
 BUILDLINK_TREE+=	ebook-tools
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ebook-tools
 EBOOK_TOOLS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ebook-tools+=	ebook-tools>=0.1.1
-BUILDLINK_ABI_DEPENDS.ebook-tools+=	ebook-tools>=0.1.1nb3
+BUILDLINK_ABI_DEPENDS.ebook-tools+=	ebook-tools>=0.1.1nb4
 BUILDLINK_PKGSRCDIR.ebook-tools?=	../../textproc/ebook-tools
 
 .include "../../archivers/libzip/buildlink3.mk"
