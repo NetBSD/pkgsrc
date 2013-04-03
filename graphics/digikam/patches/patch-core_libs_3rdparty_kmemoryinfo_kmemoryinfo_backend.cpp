794f08557b25aeaf812dfd348d6df0c5e2dfc56f
markd <markd@pkgsrc.org>
2013-04-03 12:52:04 +0000

    Update to 3.1.0
    digiKam 3.1.0 - Release date: 2013-03-10
    
    NEW FEATURES:
    
    General      : Remove internal lqr-1 library code. Set only an optional
      and external dependency.
    
    BUGFIXES FROM KDE BUGZILLA (alias B.K.O | http://bugs.kde.org):
    
    - OSM maps not refreshing.
    - No thumbnails on first start of geolocation module for newly imported
      images.
    - digiKam crashed after startup.
    - Print image does not work.
    - digiKam BQM RAW tool doesn't have options to work in 16 bits and to
      manage color profiles.
    - No progress indicator or cancel button.
    - Read face tags created by Picasa
    - digiKam Crash on Startup with active X11 color profile.
    - Cannot preview TIFF files on Mac OS due to "not enough memory" exceptions.
    - Error on startup: Failed to update the database schema from version 6
      to version 7.
    - SQLITE : convience copy in digiKam core [patch].
    - Switch to full screen mode crash under MACOSX.
    - digiKam crashes when quitting the program.
    - Large .tif files are not loaded.

--- graphics/digikam/patches/patch-core_libs_3rdparty_kmemoryinfo_kmemoryinfo_backend.cpp
+++ graphics/digikam/patches/patch-core_libs_3rdparty_kmemoryinfo_kmemoryinfo_backend.cpp
@@ -1,11 +1,11 @@
-$NetBSD: patch-core_libs_3rdparty_kmemoryinfo_kmemoryinfo_backend.cpp,v 1.1 2012/03/21 21:14:47 markd Exp $
+$NetBSD: patch-core_libs_3rdparty_kmemoryinfo_kmemoryinfo_backend.cpp,v 1.2 2013/04/03 12:52:04 markd Exp $
 
 Make memory info backend compile on NetBSD.  Need to check
 it actually returns the correct results.
 
---- core/libs/3rdparty/kmemoryinfo/kmemoryinfo_backend.cpp.orig	2012-01-03 00:32:23.000000000 +0000
+--- core/libs/3rdparty/kmemoryinfo/kmemoryinfo_backend.cpp.orig	2013-03-11 08:24:30.000000000 +0000
 +++ core/libs/3rdparty/kmemoryinfo/kmemoryinfo_backend.cpp
-@@ -57,7 +57,8 @@ static bool fillMemoryInfo(Digikam::KMem
+@@ -67,7 +67,8 @@ static int fillMemoryInfo(Digikam::KMemo
  #if defined(Q_OS_NETBSD)
  #include <sys/param.h>
  #include <sys/time.h>
@@ -15,21 +15,21 @@ it actually returns the correct results.
  #endif
  #if defined(Q_OS_OPENBSD)
  #include <sys/param.h>
-@@ -128,7 +129,7 @@ struct uvmexp *sg_get_uvmexp() {
-     int mib[2];
-     size_t size = sizeof(struct uvmexp);
+@@ -152,7 +153,7 @@ struct uvmexp* sg_get_uvmexp()
+     int                  mib[2];
+     size_t               size = sizeof(struct uvmexp);
      static struct uvmexp uvm;
--    struct uvmexp *new;
-+    /* struct uvmexp *new; */
+-    struct uvmexp*       new;
++    /* struct uvmexp*       new; */
  
      mib[0] = CTL_VM;
      mib[1] = VM_UVMEXP;
-@@ -332,7 +333,7 @@ bool get_mem_stats(Digikam::KMemoryInfo:
+@@ -422,7 +423,7 @@ int get_mem_stats(Digikam::KMemoryInfo::
  
      data->totalRam = uvm->pagesize * uvm->npages;
      data->cacheRam = uvm->pagesize * (uvm->filepages + uvm->execpages);
 -    data->freeRam  = uvm->pagesize * (uvm->free + uvm->inactive);
 +    data->freeRam  = uvm->pagesize * (uvm->free + uvm->paging);
      data->usedRam  = data->totalRam - data->freeRam;
- #endif
  
+     return 1;
--- graphics/digikam/patches/patch-core_libs_dimg_filters_icc_iccmanager.h
+++ graphics/digikam/patches/patch-core_libs_dimg_filters_icc_iccmanager.h
@@ -1,13 +1,13 @@
-$NetBSD: patch-core_libs_dimg_filters_icc_iccmanager.h,v 1.1 2012/11/16 20:06:08 joerg Exp $
+$NetBSD: patch-core_libs_dimg_filters_icc_iccmanager.h,v 1.2 2013/04/03 12:52:04 markd Exp $
 
---- core/libs/dimg/filters/icc/iccmanager.h.orig	2012-11-16 18:20:53.000000000 +0000
+--- core/libs/dimg/filters/icc/iccmanager.h.orig	2013-03-11 08:24:28.000000000 +0000
 +++ core/libs/dimg/filters/icc/iccmanager.h
-@@ -129,7 +129,7 @@ public:
+@@ -128,7 +128,7 @@ public:
      static void transformToSRGB(QImage& qimage, const IccProfile& inputProfile);
  
      /** Transforms the given QImage from sRGB to given display profile. */
--    static void transformForDisplay(QImage& qimage, const IccProfile& displayProfile = displayProfile());
-+    static void transformForDisplay(QImage& qimage, const IccProfile& displayProfile = IccProfile());
+-    static void transformForDisplay(QImage& qimage, const IccProfile& displayProfile1 = displayProfile());
++    static void transformForDisplay(QImage& qimage, const IccProfile& displayProfile1 = IccProfile());
  
  protected:
  
