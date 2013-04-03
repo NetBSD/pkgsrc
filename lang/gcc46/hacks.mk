5efb1bb37fe10eee0c6c1964cca9548127cb75ff
jmcneill <jmcneill@pkgsrc.org>
2013-04-03 19:29:23 +0000

    dont use cxx to build, just disable bootstrap -- faster and actually works

--- lang/gcc46/hacks.mk
+++ lang/gcc46/hacks.mk
@@ -1,10 +1,9 @@
-# $NetBSD: hacks.mk,v 1.1 2013/04/02 22:31:40 jmcneill Exp $
+# $NetBSD: hacks.mk,v 1.2 2013/04/03 19:29:23 jmcneill Exp $
 
 #
 # Avoid -fkeep-inline-functions with NetBSD/arm
 #
 .if ${OPSYS} == "NetBSD" && ${MACHINE_ARCH} == "arm"
 PKG_HACKS+=		netbsd-arm-stage1-fix
-USE_LANGUAGES+=		c c++
-CONFIGURE_ARGS+=	--enable-build-with-cxx
+CONFIGURE_ARGS+=	--disable-bootstrap
 .endif
