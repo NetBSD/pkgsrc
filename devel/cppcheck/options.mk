98838a6a4676ac945d660129e80dc749c225529d
adam <adam@pkgsrc.org>
2013-04-05 15:14:03 +0000

    Changes 1.59:
    Commandline/Settings changes:
     * New option to enable warnings but not style messages: --enable=warning
     * Cppcheck used to skip includes where the header filename is enclosed in <>. You can now include these headers also by using -I. It is still not required to include these headers so feel free to try it and then include the headers that give you the best results. The biggest problem with including many headers is that analysis gets slow.
    
    New checks:
     * New POSIX checks: pipe() buffer size, redundant calls of set/get user id, too big value passed to usleep(), buffer overflow when using write()
     * Storing getc() return value in char variable and comparing to EOF.
     * Detect redundant bitand operations
     * Find suspicious equality comparisons like: if(a == 0) a == 1;
     * Warn about using malloc() for classes containing virtual methods, std::-objects or constructors
     * Portability check that warns when using NULL as argument to variadic function. It has undefined behaviour on some implementations.
    
    Improvements:
     * Improved lookup for functions and types
     * Switched to TinyXml2 as XML library
     * Improved checking for uninitialized struct members, variable scopes that can be reduced and unused functions
    
    GUI:
     * Remember last path in open file dialog
     * Added command line parameter to open a results file
     * Bug in statistic calculation fixed
    
    Additionally, lots of false positives and bugs have been fixed and several existing checks have been improved.

--- devel/cppcheck/options.mk
+++ devel/cppcheck/options.mk
@@ -1,15 +1,17 @@
-# $NetBSD: options.mk,v 1.4 2012/11/29 13:52:51 ryoon Exp $
+# $NetBSD: options.mk,v 1.5 2013/04/05 15:14:03 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.cppcheck
 PKG_SUPPORTED_OPTIONS=	qt
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=	qt
+
 .if !empty(PKG_OPTIONS:Mqt)
-PLIST_SRC+=		PLIST PLIST.qt
+PLIST.qt=	yes
 
 .include "../../x11/qt4-libs/buildlink3.mk"
-BUILDLINK_DEPMETHOD.qt4-tools?= full
+BUILDLINK_DEPMETHOD.qt4-tools+= full
 .include "../../x11/qt4-tools/buildlink3.mk"
 
 post-build:
