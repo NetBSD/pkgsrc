0a17c12b4f5e10b15a6b7dbf4d22a1d98424a390
ryoon <ryoon@pkgsrc.org>
2013-04-05 13:30:17 +0000

    Update to 20.0
    Changelog:
        NEW
        Per-window Private Browsing. Learn more.
        NEW
        New download experience. Learn more.
        NEW
        Ability to close hanging plugins, without the browser hanging
        CHANGED
        Continued performance improvements around common browser tasks (page loads, downloads, shutdown, etc.)
        DEVELOPER
        Continued implementation of draft ECMAScript 6 - clear() and Math.imul
        DEVELOPER
        New JavaScript Profiler tool
        HTML5
        getUserMedia implemented for web access to the user's camera and microphone (with user permission)
        HTML5
        <canvas> now supports blend modes
        HTML5
        Various <audio> and <video> improvements
        FIXED
        Details button on Crash Reporter (793972)
        FIXED
        Unity plugin doesn't display in HiDPI mode (829284)
        FIXED
        20.0: Security fixes can be found here
    
    Fixed in Firefox 20
    MFSA 2013-40 Out-of-bounds array read in CERT_DecodeCertPackage
    MFSA 2013-39 Memory corruption while rendering grayscale PNG images
    MFSA 2013-38 Cross-site scripting (XSS) using timed history navigations
    MFSA 2013-37 Bypass of tab-modal dialog origin disclosure
    MFSA 2013-36 Bypass of SOW protections allows cloning of protected nodes
    MFSA 2013-35 WebGL crash with Mesa graphics driver on Linux
    MFSA 2013-34 Privilege escalation through Mozilla Updater
    MFSA 2013-33 World read and write access to app_tmp directory on Android
    MFSA 2013-32 Privilege escalation through Mozilla Maintenance Service
    MFSA 2013-31 Out-of-bounds write in Cairo library
    MFSA 2013-30 Miscellaneous memory safety hazards (rv:20.0 / rv:17.0.5)

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,12 +1,12 @@
-# $NetBSD: dist.mk,v 1.44 2013/03/10 00:22:16 ryoon Exp $
+# $NetBSD: dist.mk,v 1.45 2013/04/05 13:30:17 ryoon Exp $
 #
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	19.0
-MOZ_BRANCH_MINOR=	.2
+MOZ_BRANCH=	20.0
+MOZ_BRANCH_MINOR=	
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
