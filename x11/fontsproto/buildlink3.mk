aa1275dbab80e5dd53a18f003eb3a4e4a32001cb
rodent <rodent@pkgsrc.org>
2013-04-06 13:46:33 +0000

    "This line belongs inside the .ifdef block."

--- x11/fontsproto/buildlink3.mk
+++ x11/fontsproto/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:25:40 joerg Exp $
-
-BUILDLINK_DEPMETHOD.fontsproto?=	build
+# $NetBSD: buildlink3.mk,v 1.4 2013/04/06 13:46:34 rodent Exp $
 
 BUILDLINK_TREE+=	fontsproto
 
 .if !defined(FONTSPROTO_BUILDLINK3_MK)
 FONTSPROTO_BUILDLINK3_MK:=
+
+BUILDLINK_DEPMETHOD.fontsproto?=	build
 
 BUILDLINK_API_DEPENDS.fontsproto+=	fontsproto>=2.0
 BUILDLINK_PKGSRCDIR.fontsproto?=	../../x11/fontsproto
--- x11/xf86dgaproto/buildlink3.mk
+++ x11/xf86dgaproto/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2010/01/15 18:14:50 tnn Exp $
-
-BUILDLINK_DEPMETHOD.xf86dgaproto?=	build
+# $NetBSD: buildlink3.mk,v 1.5 2013/04/06 13:46:36 rodent Exp $
 
 BUILDLINK_TREE+=	xf86dgaproto
 
 .if !defined(XF86DGAPROTO_BUILDLINK3_MK)
 XF86DGAPROTO_BUILDLINK3_MK:=
+
+BUILDLINK_DEPMETHOD.xf86dgaproto?=	build
 
 BUILDLINK_API_DEPENDS.xf86dgaproto+=	xf86dgaproto>=2.1
 BUILDLINK_PKGSRCDIR.xf86dgaproto?=	../../x11/xf86dgaproto
