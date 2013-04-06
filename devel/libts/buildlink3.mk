c62d2f9804bdf1d149060925ef808202308dffba
rodent <rodent>
2013-04-06 13:46:33 +0000

    "This line belongs inside the .ifdef block."

--- devel/libts/buildlink3.mk
+++ devel/libts/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2009/03/20 19:24:23 joerg Exp $
-
-BUILDLINK_DEPMETHOD.libts?=	build
+# $NetBSD: buildlink3.mk,v 1.7 2013/04/06 13:46:33 rodent Exp $
 
 BUILDLINK_TREE+=	libts
 
 .if !defined(LIBTS_BUILDLINK3_MK)
 LIBTS_BUILDLINK3_MK:=
+
+BUILDLINK_DEPMETHOD.libts?=	build
 
 BUILDLINK_API_DEPENDS.libts+=	libts>=1.08
 BUILDLINK_PKGSRCDIR.libts?=	../../devel/libts
--- x11/xf86miscproto/buildlink3.mk
+++ x11/xf86miscproto/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:53 joerg Exp $
-
-BUILDLINK_DEPMETHOD.xf86miscproto?=	build
+# $NetBSD: buildlink3.mk,v 1.3 2013/04/06 13:46:36 rodent Exp $
 
 BUILDLINK_TREE+=	xf86miscproto
 
 .if !defined(XF86MISCPROTO_BUILDLINK3_MK)
 XF86MISCPROTO_BUILDLINK3_MK:=
+
+BUILDLINK_DEPMETHOD.xf86miscproto?=	build
 
 BUILDLINK_API_DEPENDS.xf86miscproto+=	xf86miscproto>=0.9.2
 BUILDLINK_PKGSRCDIR.xf86miscproto?=	../../x11/xf86miscproto
