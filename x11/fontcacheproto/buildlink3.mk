0991f5747823d305151d2ca8286fc028ac9f6605
rodent <rodent>
2013-04-06 13:46:33 +0000

    "This line belongs inside the .ifdef block."

--- x11/fontcacheproto/buildlink3.mk
+++ x11/fontcacheproto/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:40 joerg Exp $
-
-BUILDLINK_DEPMETHOD.fontcacheproto?=	build
+# $NetBSD: buildlink3.mk,v 1.3 2013/04/06 13:46:34 rodent Exp $
 
 BUILDLINK_TREE+=	fontcacheproto
 
 .if !defined(FONTCACHEPROTO_BUILDLINK3_MK)
 FONTCACHEPROTO_BUILDLINK3_MK:=
+
+BUILDLINK_DEPMETHOD.fontcacheproto?=	build
 
 BUILDLINK_API_DEPENDS.fontcacheproto+=	fontcacheproto>=0.1
 BUILDLINK_PKGSRCDIR.fontcacheproto?=	../../x11/fontcacheproto
