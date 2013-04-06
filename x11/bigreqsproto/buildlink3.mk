d590887fa36cf92b7e2167e239aa0e604f3e5146
rodent <rodent@pkgsrc.org>
2013-04-06 13:46:33 +0000

    "This line belongs inside the .ifdef block."

--- x11/bigreqsproto/buildlink3.mk
+++ x11/bigreqsproto/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:39 joerg Exp $
-
-BUILDLINK_DEPMETHOD.bigreqsproto?=	build
+# $NetBSD: buildlink3.mk,v 1.3 2013/04/06 13:46:34 rodent Exp $
 
 BUILDLINK_TREE+=	bigreqsproto
 
 .if !defined(BIGREQSPROTO_BUILDLINK3_MK)
 BIGREQSPROTO_BUILDLINK3_MK:=
+
+BUILDLINK_DEPMETHOD.bigreqsproto?=	build
 
 BUILDLINK_API_DEPENDS.bigreqsproto+=	bigreqsproto>=1.0
 BUILDLINK_PKGSRCDIR.bigreqsproto?=	../../x11/bigreqsproto
