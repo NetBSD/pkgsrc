f5470df9ca6a1147c5f019ee8f8e6fc79eb5b5de
abs <abs@pkgsrc.org>
2013-04-07 10:11:54 +0000

    Fix PKG_OPTIONS.nmap=ndiff to include python/application.mk - previously would only work when set in addition to PKG_OPTIONS.zenmap

--- net/nmap/options.mk
+++ net/nmap/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2012/04/08 19:09:09 wiz Exp $
+# $NetBSD: options.mk,v 1.8 2013/04/07 10:11:54 abs Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nmap
 
@@ -21,7 +21,7 @@ CONFIGURE_ARGS+=	--disable-ipv6
 CONFIGURE_ARGS+=		--with-zenmap
 PLIST_SRC+=			${PKGDIR}/PLIST.zenmap
 PY_PATCHPLIST=			yes
-REPLACE_PYTHON=			zenmap/zenmapCore/*.py
+REPLACE_PYTHON+=		zenmap/zenmapCore/*.py
 REPLACE_PYTHON+=		zenmap/zenmapGUI/*.py
 REPLACE_PYTHON+=		zenmap/zenmapGUI/higwidgets/*.py
 .include "../../lang/python/application.mk"
@@ -37,6 +37,12 @@ CONFIGURE_ARGS+=	--without-zenmap
 .if !empty(PKG_OPTIONS:Mndiff)
 CONFIGURE_ARGS+=		--with-ndiff
 PLIST_SRC+=			${PKGDIR}/PLIST.ndiff
+SUBST_CLASSES+=			paths
+SUBST_STAGE.paths=		post-patch
+SUBST_FILES.paths=		ndiff/setup.py
+SUBST_SED.paths=		-e "s|@@PKGMANDIR@@|${PKGMANDIR}|g"
+SUBST_MESSAGE.paths=		Fixing paths.
+.include "../../lang/python/application.mk"
 .else
 CONFIGURE_ARGS+=		--without-ndiff
 .endif
