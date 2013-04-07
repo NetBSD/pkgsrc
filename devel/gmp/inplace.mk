54a8c5e2e90fbe915a40bc8b1a3b410af1690a56
asau <asau@pkgsrc.org>
2013-04-07 16:13:56 +0000

    "fetch" isn't a good target to fetch distfiles, it doesn't calculate digests. Use "checksum" instead.

--- devel/gmp/inplace.mk
+++ devel/gmp/inplace.mk
@@ -1,4 +1,4 @@
-# $NetBSD: inplace.mk,v 1.2 2013/01/31 20:30:25 adam Exp $
+# $NetBSD: inplace.mk,v 1.3 2013/04/07 16:13:56 asau Exp $
 #
 # Include this file to extract devel/gmp source into the WRKSRC of
 # another package. This is to be used by GCC packages to avoid the
@@ -8,5 +8,5 @@ post-extract: extract-inplace-gmp
 
 extract-inplace-gmp:
 	(cd ../../devel/gmp && ${MAKE} WRKDIR=${WRKSRC}/.devel.gmp EXTRACT_DIR=${WRKSRC} \
-		WRKSRC='$${EXTRACT_DIR}/$${DISTNAME}' SKIP_DEPENDS=YES fetch patch clean)
+		WRKSRC='$${EXTRACT_DIR}/$${DISTNAME}' SKIP_DEPENDS=YES checksum patch clean)
 	${MV} ${WRKSRC}/gmp-* ${WRKSRC}/gmp
