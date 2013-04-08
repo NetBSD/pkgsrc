487757a3b16ccdb5425f97b669a89923eb7a8d0c
drochner <drochner@pkgsrc.org>
2013-04-08 16:48:22 +0000

    update to 0.6.1 changes: -bugfixes -API cleanup -Now includes calls to register and unregister custom XMP namespaces (soname changed)

--- graphics/libgexiv2/buildlink3.mk
+++ graphics/libgexiv2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2012/09/15 10:05:02 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2013/04/08 16:48:22 drochner Exp $
 
 BUILDLINK_TREE+=	libgexiv2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libgexiv2
 LIBGEXIV2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libgexiv2+=	libgexiv2>=0.2.0
-BUILDLINK_ABI_DEPENDS.libgexiv2+=	libgexiv2>=0.4.1nb1
+BUILDLINK_ABI_DEPENDS.libgexiv2+=	libgexiv2>=0.6.1
 BUILDLINK_PKGSRCDIR.libgexiv2?=	../../graphics/libgexiv2
 
 .include "../../devel/glib2/buildlink3.mk"
