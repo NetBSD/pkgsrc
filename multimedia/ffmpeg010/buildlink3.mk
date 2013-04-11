624588565b1c68314357d888ea6d20fc410aecee
drochner <drochner@pkgsrc.org>
2013-04-11 11:26:23 +0000

    update to 0.10.7 from the announcement: It is the latest stable FFmpeg release from the 0.10 release branch, which was cut from master on 2012-01-26. Amongst lots of other changes, it includes all changes from ffmpeg-mt, libav master of 2012-01-26, libav 0.8.6 as of 2013-04-10.

--- multimedia/ffmpeg010/buildlink3.mk
+++ multimedia/ffmpeg010/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2013/01/22 09:49:11 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2013/04/11 11:26:23 drochner Exp $
 
 BUILDLINK_TREE+=	ffmpeg010
 
 .if !defined(FFMPEG010_BUILDLINK3_MK)
 FFMPEG010_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ffmpeg010+=	ffmpeg010>=20120609.0.10.4<20121028.1.0
+BUILDLINK_API_DEPENDS.ffmpeg010+=	ffmpeg010>=20120609.0.10.4
 BUILDLINK_PKGSRCDIR.ffmpeg010?=	../../multimedia/ffmpeg010
 
 BUILDLINK_INCDIRS.ffmpeg010+=		include/ffmpeg010
