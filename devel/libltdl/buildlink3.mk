c2c5f5982efca6ce8331f454deac5ec90bbe849e
joerg <joerg@pkgsrc.org>
2013-04-12 13:44:07 +0000

    Update to libtool-2.4.2: - Initial support for Go. - On Mac OS X, try to open .dylib as well as .so in lt_dlopenext. - sysroot support - man pages for libtool and libtoolize - various smaller bug fixes
    pkgsrc side: remove support for pre-ELF NetBSD

--- devel/libltdl/buildlink3.mk
+++ devel/libltdl/buildlink3.mk
@@ -1,24 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.10 2009/12/15 17:35:36 joerg Exp $
-
-.include "../../mk/bsd.fast.prefs.mk"
-.if ${OPSYS} == "NetBSD"
-.  if !exists(/usr/libexec/ld.so) && !exists(/usr/libexec/ld.elf_so)
-_SKIP_LIBLTDL=		yes
-.  endif
-.endif
-
-.if !defined(_SKIP_LIBLTDL)
+# $NetBSD: buildlink3.mk,v 1.11 2013/04/12 13:44:07 joerg Exp $
 
 BUILDLINK_TREE+=	libltdl
 
 .if !defined(LIBLTDL_BUILDLINK3_MK)
 LIBLTDL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libltdl+=	libltdl>=2.2.6
+BUILDLINK_API_DEPENDS.libltdl+=	libltdl>=2.4.2
+BUILDLINK_ABI_DEPENDS.libltdl+=	libltdl>=2.2.6
 BUILDLINK_PKGSRCDIR.libltdl?=	../../devel/libltdl
 
 .include "../../mk/dlopen.buildlink3.mk"
-.endif
 .endif # LIBLTDL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libltdl
