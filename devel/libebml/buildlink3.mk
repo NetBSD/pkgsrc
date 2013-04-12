df1662e42611babb1f311268360c10d428240eae
drochner <drochner@pkgsrc.org>
2013-04-12 13:35:20 +0000

    update to 1.3.0 changes: several new API functions like GetValue()/SetValue(), new constructors, and a couple of bug fixes like calculating dates with 64bit integers preventing overflows in certain situations

--- devel/libebml/buildlink3.mk
+++ devel/libebml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2011/03/18 12:42:24 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2013/04/12 13:35:20 drochner Exp $
 
 BUILDLINK_TREE+=	libebml
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libebml
 LIBEBML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libebml+=	libebml>=0.8
-BUILDLINK_ABI_DEPENDS.libebml+=	libebml>=1.2.0
+BUILDLINK_ABI_DEPENDS.libebml+=	libebml>=1.3.0
 BUILDLINK_PKGSRCDIR.libebml?=	../../devel/libebml
 .endif # LIBEBML_BUILDLINK3_MK
 
