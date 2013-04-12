54352d7edbc563c7d6288983b92566dab057ca0a
drochner <drochner@pkgsrc.org>
2013-04-12 13:36:56 +0000

    update to 1.4.0 changes: several new elements that were added to the Matroska specifications: CueDuration, CueRelativePosition, ChapterStringUID, VideoAlphaMode and DefaultDecodedFieldDuration

--- multimedia/libmatroska/buildlink3.mk
+++ multimedia/libmatroska/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2011/10/07 19:04:46 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2013/04/12 13:36:56 drochner Exp $
 
 BUILDLINK_TREE+=	libmatroska
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libmatroska
 LIBMATROSKA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libmatroska+=	libmatroska>=0.8.0
-BUILDLINK_ABI_DEPENDS.libmatroska+=	libmatroska>=1.3.0
+BUILDLINK_ABI_DEPENDS.libmatroska+=	libmatroska>=1.4.0
 BUILDLINK_PKGSRCDIR.libmatroska?=	../../multimedia/libmatroska
 
 .include "../../devel/libebml/buildlink3.mk"
