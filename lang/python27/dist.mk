a1d032bbac5ac8ee7aee888e1e63d0bae77ab390
adam <adam@pkgsrc.org>
2013-04-12 09:03:41 +0000

    Changes 2.7.4: This is a 2.7 series bugfix release. It includes hundreds of bugfixes over 2.7.3.

--- lang/python27/dist.mk
+++ lang/python27/dist.mk
@@ -1,8 +1,8 @@
-# $NetBSD: dist.mk,v 1.3 2012/04/13 18:47:27 adam Exp $
+# $NetBSD: dist.mk,v 1.4 2013/04/12 09:03:41 adam Exp $
 
-PY_DISTVERSION=	2.7.3
+PY_DISTVERSION=	2.7.4
 DISTNAME=	Python-${PY_DISTVERSION}
-EXTRACT_SUFX=	.tar.bz2
+EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python27/distinfo
 PATCHDIR=	${.CURDIR}/../../lang/python27/patches
 MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION}/
