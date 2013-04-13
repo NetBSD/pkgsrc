0cbed63cc5baf00d6db82beec2bd4049d374ad05
riz <riz@pkgsrc.org>
2013-04-13 17:51:57 +0000

    Update ansible to version 1.1.  From the CHANGELOG.md:
    1.1 "Mean Street" -- 4/2/2013
    
    Core Features
    
    * added --check option for "dry run" mode
    * added --diff option to show how templates or copied files change, or
      might change
    * --list-tasks for the playbook will list the tasks without running them
    * able to set the environment by setting "environment:" as a dictionary
      on any task (go proxy support!)
    * added ansible_ssh_user and ansible_ssh_pass for per-host/group username
      and password
    * jinja2 extensions can now be loaded from the config file
    * support for complex arguments to modules (within reason)
    * can specify ansible_connection=X to define the connection type in
      inventory variables
    * a new chroot connection type
    * module common code now has basic type checking (and casting) capability
    * module common now supports a 'no_log' attribute to mark a field as
      not to be syslogged
    * inventory can now point to a directory containing multiple
      scripts/hosts files, if using this, put group_vars/host_vars
      directories inside this directory
    * added configurable crypt scheme for 'vars_prompt'
    * password generating lookup plugin -- $PASSWORD(path/to/save/data/in)
    * added --step option to ansible-playbook, works just like Linux
      interactive startup!
    
    Modules Added:
    
    * bzr (bazaar version control)
    * cloudformation
    * django-manage
    * gem (ruby gems)
    * homebrew
    * lvg (logical volume groups)
    * lvol (LVM logical volumes)
    * macports
    * mongodb_user
    * netscaler
    * okg
    * openbsd_pkg
    * rabbit_mq_plugin
    * rabbit_mq_user
    * rabbit_mq_vhost
    * rabbit_mq_parameter
    * rhn_channel
    * s3 -- allows putting file contents in buckets for sharing over s3
    * uri module -- can get/put/post/etc
    * vagrant -- launching VMs with vagrant, this is different from existing
      vagrant plugin
    * zfs

--- sysutils/ansible/patches/patch-lib_ansible_callbacks.py
+++ sysutils/ansible/patches/patch-lib_ansible_callbacks.py
@@ -1,8 +1,8 @@
-$NetBSD: patch-lib_ansible_callbacks.py,v 1.1 2013/02/26 00:13:00 hubertf Exp $
+$NetBSD: patch-lib_ansible_callbacks.py,v 1.2 2013/04/13 17:51:57 riz Exp $
 
---- lib/ansible/callbacks.py.orig	2013-01-26 16:50:21.000000000 +0000
-+++ lib/ansible/callbacks.py
-@@ -31,9 +31,9 @@ elif os.path.exists("/usr/bin/cowsay"):
+--- lib/ansible/callbacks.py.orig	2013-03-30 16:29:28.000000000 -0700
++++ lib/ansible/callbacks.py	2013-04-13 09:05:36.000000000 -0700
+@@ -30,9 +30,9 @@
      cowsay = "/usr/bin/cowsay"
  elif os.path.exists("/usr/games/cowsay"):
      cowsay = "/usr/games/cowsay"
@@ -11,6 +11,6 @@ $NetBSD: patch-lib_ansible_callbacks.py,v 1.1 2013/02/26 00:13:00 hubertf Exp $
      # BSD path for cowsay
 -    cowsay = "/usr/local/bin/cowsay"
 +    cowsay = "@LOCALBASE@/bin/cowsay"
- 
- def call_callback_module(method_name, *args, **kwargs):
- 
+ elif os.path.exists("/opt/local/bin/cowsay"):
+     # MacPorts path for cowsay
+     cowsay = "/opt/local/bin/cowsay"
