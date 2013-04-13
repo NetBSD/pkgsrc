b2c5ea3a939c67a1c281be3273bcbef5874d76a7
ryoon <ryoon@pkgsrc.org>
2013-04-13 12:44:36 +0000

    Update to 20.0.1
    Changelog:
    FIXED
    20.0.1 - Windows-only update to handle issues around handling UNC paths (see bug 846848)

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.45 2013/04/05 13:30:17 ryoon Exp $
+# $NetBSD: dist.mk,v 1.46 2013/04/13 12:44:36 ryoon Exp $
 #
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
@@ -6,7 +6,7 @@
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	20.0
-MOZ_BRANCH_MINOR=	
+MOZ_BRANCH_MINOR=	.1
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
