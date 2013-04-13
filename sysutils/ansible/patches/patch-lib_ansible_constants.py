e16cb535cbb842aa014d8486bc1e5f84ceb1bba5
riz <riz@pkgsrc.org>
2013-04-13 17:51:57 +0000

    Update ansible to version 1.1.  From the CHANGELOG.md:
    1.1 "Mean Street" -- 4/2/2013
    
    Core Features
    
    * added --check option for "dry run" mode
    * added --diff option to show how templates or copied files change, or
      might change
    * --list-tasks for the playbook will list the tasks without running them
    * able to set the environment by setting "environment:" as a dictionary
      on any task (go proxy support!)
    * added ansible_ssh_user and ansible_ssh_pass for per-host/group username
      and password
    * jinja2 extensions can now be loaded from the config file
    * support for complex arguments to modules (within reason)
    * can specify ansible_connection=X to define the connection type in
      inventory variables
    * a new chroot connection type
    * module common code now has basic type checking (and casting) capability
    * module common now supports a 'no_log' attribute to mark a field as
      not to be syslogged
    * inventory can now point to a directory containing multiple
      scripts/hosts files, if using this, put group_vars/host_vars
      directories inside this directory
    * added configurable crypt scheme for 'vars_prompt'
    * password generating lookup plugin -- $PASSWORD(path/to/save/data/in)
    * added --step option to ansible-playbook, works just like Linux
      interactive startup!
    
    Modules Added:
    
    * bzr (bazaar version control)
    * cloudformation
    * django-manage
    * gem (ruby gems)
    * homebrew
    * lvg (logical volume groups)
    * lvol (LVM logical volumes)
    * macports
    * mongodb_user
    * netscaler
    * okg
    * openbsd_pkg
    * rabbit_mq_plugin
    * rabbit_mq_user
    * rabbit_mq_vhost
    * rabbit_mq_parameter
    * rhn_channel
    * s3 -- allows putting file contents in buckets for sharing over s3
    * uri module -- can get/put/post/etc
    * vagrant -- launching VMs with vagrant, this is different from existing
      vagrant plugin
    * zfs

--- sysutils/ansible/patches/patch-lib_ansible_constants.py
+++ sysutils/ansible/patches/patch-lib_ansible_constants.py
@@ -1,7 +1,7 @@
-$NetBSD: patch-lib_ansible_constants.py,v 1.2 2013/02/26 17:42:59 riz Exp $
+$NetBSD: patch-lib_ansible_constants.py,v 1.3 2013/04/13 17:51:57 riz Exp $
 
---- lib/ansible/constants.py.orig	2013-02-01 18:15:55.000000000 -0800
-+++ lib/ansible/constants.py	2013-02-25 11:58:52.000000000 -0800
+--- lib/ansible/constants.py.orig	2013-03-04 13:18:12.000000000 -0800
++++ lib/ansible/constants.py	2013-04-13 09:48:31.000000000 -0700
 @@ -36,7 +36,7 @@
      p = ConfigParser.ConfigParser()
      path1 = os.path.expanduser(os.environ.get('ANSIBLE_CONFIG', "~/.ansible.cfg"))
@@ -27,9 +27,9 @@ $NetBSD: patch-lib_ansible_constants.py,v 1.2 2013/02/26 17:42:59 riz Exp $
  DEFAULT_MODULE_PATH       = shell_expand_path(get_config(p, DEFAULTS, 'library',          'ANSIBLE_LIBRARY',          DIST_MODULE_PATH))
  DEFAULT_REMOTE_TMP        = shell_expand_path(get_config(p, DEFAULTS, 'remote_tmp',       'ANSIBLE_REMOTE_TEMP',      '$HOME/.ansible/tmp'))
  DEFAULT_MODULE_NAME       = get_config(p, DEFAULTS, 'module_name',      None,                       'command')
-@@ -95,12 +95,12 @@
- DEFAULT_SUDO_FLAGS        = get_config(p, DEFAULTS, 'sudo_flags', 'ANSIBLE_SUDO_FLAGS', '-H')
+@@ -96,12 +96,12 @@
  DEFAULT_HASH_BEHAVIOUR    = get_config(p, DEFAULTS, 'hash_behaviour', 'ANSIBLE_HASH_BEHAVIOUR', 'replace')
+ DEFAULT_JINJA2_EXTENSIONS = get_config(p, DEFAULTS, 'jinja2_extensions', 'ANSIBLE_JINJA2_EXTENSIONS', None)
  
 -DEFAULT_ACTION_PLUGIN_PATH     = shell_expand_path(get_config(p, DEFAULTS, 'action_plugins',     'ANSIBLE_ACTION_PLUGINS', '/usr/share/ansible_plugins/action_plugins'))
 -DEFAULT_CALLBACK_PLUGIN_PATH   = shell_expand_path(get_config(p, DEFAULTS, 'callback_plugins',   'ANSIBLE_CALLBACK_PLUGINS', '/usr/share/ansible_plugins/callback_plugins'))
