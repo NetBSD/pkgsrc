1756b0120974f9d1f832ed797b4e54e2ad1ee115
adam <adam@pkgsrc.org>
2013-04-15 20:46:16 +0000

    Version 1.7.0 ------------- * New operating system backends   + Add BlueGene/Q compute node kernel (CNK) support. See the FAQ in the     documentation for details. Thanks to Jeff Hammond, Christopher Samuel     and Erik Schnetter for their help.   + Add NetBSD support, thanks to Aleksej Saushev. * New I/O device discovery   + Add co-processor OS devices such as "mic0" for Intel Xeon Phi (MIC)     on Linux. Thanks to Jerome Vienne for helping.   + Add co-processor OS devices such as "cuda0" for NVIDIA CUDA-capable GPUs.   + Add co-processor OS devices such as "opencl0d0" for OpenCL GPU devices     on the AMD OpenCL implementation.   + Add GPU OS devices such as ":0.0" for NVIDIA X11 displays.   + Add GPU OS devices such as "nvml0" for NVIDIA GPUs.     Thanks to Marwan Abdellah and Stefan Eilemann for helping.   These new OS devices have some string info attributes such as CoProcType,   GPUModel, etc. to better identify them.   See the I/O Devices and Attributes documentation sections for details. * New components   + Add the "opencl", "cuda", "nvml" and "gl" components for I/O device     discovery.   + "nvml" also improves the discovery of NVIDIA GPU PCIe link speed.   All of these new components may be built as plugins. They may also be   disabled entirely by passing --disable-opencl/cuda/nvml/gl to configure.   See the I/O Devices, Components and Plugins, and FAQ documentation   sections for details. * API   + Add hwloc_topology_get_flags().   + Add hwloc/plugins.h for building external plugins.     See the Adding new discovery components and plugins section. * Interoperability   + Add hwloc/opencl.h, hwloc/nvml.h, hwloc/gl.h and hwloc/intel-mic.h     to retrieve the locality of OS devices that correspond to AMD OpenCL     GPU devices or indexes, to NVML devices or indexes, to NVIDIA X11     displays, or to Intel Xeon Phi (MIC) device indexes.   + Add new helpers in hwloc/cuda.h and hwloc/cudart.h to convert     between CUDA devices or indexes and hwloc OS devices.   + Add hwloc_ibv_get_device_osdev() and clarify the requirements     of the OpenFabrics Verbs helpers in hwloc/openfabrics-verbs.h. * Tools   + hwloc-info is not only a synonym of lstopo -s anymore, it also     dumps information about objects given on the command-line. * Documentation   + Add a section "Existing components and plugins".   + Add a list of common OS devices in section "Software devices".   + Add a new FAQ entry "Why is lstopo slow?" about lstopo slowness     issues because of GPUs.   + Clarify the documentation of inline helpers in hwloc/myriexpress.h     and hwloc/openfabrics-verbs.h. * Misc   + Improve cache detection on AIX.   + The HWLOC_COMPONENTS variable now excludes the components whose     names are prefixed with '-'.   + lstopo --ignore PU now works when displaying the topology in     graphical and textual mode (not when exporting to XML).   + The `libpci' component is now called `pci' but the old name is still     accepted in the HWLOC_COMPONENTS variable for backward compatibility.

--- parallel/hwloc/buildlink3.mk
+++ parallel/hwloc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/10/24 05:49:25 asau Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/04/15 20:46:17 adam Exp $
 
 BUILDLINK_TREE+=	hwloc
 
@@ -8,7 +8,7 @@ HWLOC_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hwloc+=	hwloc>=1.4
 BUILDLINK_PKGSRCDIR.hwloc?=	../../parallel/hwloc
 
-.include "../../textproc/libxml2/buildlink3.mk" # optional
+.include "../../textproc/libxml2/buildlink3.mk"
 .endif	# HWLOC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hwloc
