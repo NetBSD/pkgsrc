509b26be2ea983759da95815e7afb3fa56a98d04
obache <obache@pkgsrc.org>
2013-04-18 13:22:06 +0000

    also try to detect *.pc files in share/pkgconfig.

--- x11/xtrans/builtin.mk
+++ x11/xtrans/builtin.mk
@@ -1,7 +1,8 @@
-# $NetBSD: builtin.mk,v 1.2 2008/10/05 21:36:34 cube Exp $
+# $NetBSD: builtin.mk,v 1.3 2013/04/18 13:22:06 obache Exp $
 
 BUILTIN_PKG:=	xtrans
 PKGCONFIG_FILE.xtrans=	${X11BASE}/lib/pkgconfig/xtrans.pc
+PKGCONFIG_FILE.xtrans+=	${X11BASE}/share/pkgconfig/xtrans.pc
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
