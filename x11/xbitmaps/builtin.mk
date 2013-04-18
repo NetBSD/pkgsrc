8b2902f2a2eb6ea45fa0b12dd082e783c70f4319
obache <obache@pkgsrc.org>
2013-04-18 13:22:06 +0000

    also try to detect *.pc files in share/pkgconfig.

--- x11/xbitmaps/builtin.mk
+++ x11/xbitmaps/builtin.mk
@@ -1,7 +1,8 @@
-# $NetBSD: builtin.mk,v 1.1 2011/09/21 14:58:37 joerg Exp $
+# $NetBSD: builtin.mk,v 1.2 2013/04/18 13:22:06 obache Exp $
 
 BUILTIN_PKG:=	xbitmaps
 PKGCONFIG_FILE.xbitmaps=	${X11BASE}/lib/pkgconfig/xbitmaps.pc
+PKGCONFIG_FILE.xbitmaps+=	${X11BASE}/share/pkgconfig/xbitmaps.pc
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
