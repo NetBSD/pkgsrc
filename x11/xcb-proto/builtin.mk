a988dfba87dff6e1fdda89be1e00d3cf773be402
obache <obache@pkgsrc.org>
2013-04-18 13:22:06 +0000

    also try to detect *.pc files in share/pkgconfig.

--- x11/xcb-proto/builtin.mk
+++ x11/xcb-proto/builtin.mk
@@ -1,7 +1,8 @@
-# $NetBSD: builtin.mk,v 1.1 2010/09/11 19:46:59 tron Exp $
+# $NetBSD: builtin.mk,v 1.2 2013/04/18 13:22:06 obache Exp $
 
 BUILTIN_PKG:=	xcb-proto
 PKGCONFIG_FILE.xcb-proto=	${X11BASE}/lib/pkgconfig/xcb-proto.pc
+PKGCONFIG_FILE.xcb-proto+=	${X11BASE}/share/pkgconfig/xcb-proto.pc
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
