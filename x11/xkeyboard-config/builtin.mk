53e4f4af49ac360df0bbde1086a618318b739445
obache <obache@pkgsrc.org>
2013-04-18 13:22:06 +0000

    also try to detect *.pc files in share/pkgconfig.

--- x11/xkeyboard-config/builtin.mk
+++ x11/xkeyboard-config/builtin.mk
@@ -1,7 +1,8 @@
-# $NetBSD: builtin.mk,v 1.1 2010/06/04 10:49:01 ghen Exp $
+# $NetBSD: builtin.mk,v 1.2 2013/04/18 13:22:06 obache Exp $
 
 BUILTIN_PKG:=				xkeyboard-config
 PKGCONFIG_FILE.xkeyboard-config=	${X11BASE}/lib/pkgconfig/xkeyboard-config.pc
+PKGCONFIG_FILE.xkeyboard-config+=	${X11BASE}/share/pkgconfig/xkeyboard-config.pc
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
