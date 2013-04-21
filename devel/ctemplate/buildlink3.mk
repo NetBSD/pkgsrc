465e4a164187c36696c3143c78c741a3daa327f5
rodent <rodent@pkgsrc.org>
2013-04-21 11:23:38 +0000

    Update ctemplate to version 2.2. Add pthread in buildlink3.mk. Set self as MAINTAINER, since I originally packaged this and no one seems to have cared to touch it since. The only dependency, mysql-workbench, builds fine with this version of ctemplate.
    Since version 0.99 (from ChangeLog):
    	* Fixed several issues reported by G++ 4.7
    	* Added operator[] to TemplateDictionary
    	* Added begin(), end(), data() and size() to TemplateString
    	* Refactored code
    	* Changed the 'official' ctemplate email in setup.py/etc
    	* Rewrote docs to refer to Ctemplate, not Google Template
    	* Renamed google-ctemplate.sln to ctemplate.sln
    	* Changed copyright text to reflect Google's relinquished ownership
    	* Make reloads always prefer files earlier on the search patch (ssas)
    	* PORTING: Add a default BSWAP32, needed for AIX (csilvers)
    	* Die at configure-time when g++ isn't installed
    	* BUGFIX: removed #includes of non-installed headers from installed ones
    	* BUGFIX: Make compile-test test against an install
    	* BUGFIX: Dumpers were ignoring a tpl-string 'length' (csilvers)
    	* BUGFIX: Add a needed $(top_srcdir) to Makefile.am
    	* Replace 0x00-0x1F with space in xml_escape (alexvod)
    	* Fix xml encoding to handle form-feed properly (ghurley)
    	* DOCUMENTATION: Wer missing some docs in the tarball (csilvers)
    	* BUILD: Update acx_pthread.m4 for nostdlib (Kacper)
    	* BUGFIX: Make CreateSectionIterator check tpl-globals (williasr)
    	* PORTING: Fix solaris build by getting rid of PATH_MAX (csilvers)

--- devel/ctemplate/buildlink3.mk
+++ devel/ctemplate/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/05/12 12:32:09 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/04/21 11:23:38 rodent Exp $
 
 BUILDLINK_TREE+=	ctemplate
 
@@ -7,6 +7,8 @@ CTEMPLATE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ctemplate+=	ctemplate>=0.97
 BUILDLINK_PKGSRCDIR.ctemplate?=		../../devel/ctemplate
+
+.include "../../mk/pthread.buildlink3.mk"
 .endif # CTEMPLATE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ctemplate
