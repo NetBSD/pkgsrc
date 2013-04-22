e088190ca3fb36b1f6311295bd1f213b194e3f54
obache <obache@pkgsrc.org>
2013-04-22 12:33:47 +0000

    some clean up:
    Makefile
    * xmlto is just required to docbook_docs, move dependency to `doc' option.
    * remove buildlinking to gobject-introspection, it is alrady in option.mk
      conditionally.
    * gdk_pixbuf2 is really requied by this package, drop specification of `build'
      dependency.
    * exactly specify required glib2 version.
    * from NEWS, libnotify>=7.0 use GBus instead of dbus-glib, so drop dependency
      on dbus and dbus-glib.
    * change dependency on gtk3 to `build',  it is just required to test build.
    
    buildlink3.mk
    * change ABI_DEPENDS to reasonable version.
    * drop `doc' and `introspection' option condition handling , it will not affect
      to packages usind this file or should be handled packages by themselves.
    * drop buildlinking to dbus and dbus-glib and add to gdk-pixbuf2, same reason
      as Makefile.
    
    options.mk
    * gtk-doc documents are already in release taball, so dtk-doc is not required
      to build, reuse as docbook-docs to match PLIST.doc condition.
    * exactly specify required gobject introspection version.
    
    Bump PKGREVISION.

--- sysutils/libnotify07/options.mk
+++ sysutils/libnotify07/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2013/04/21 00:50:04 rodent Exp $
+# $NetBSD: options.mk,v 1.2 2013/04/22 12:33:47 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libnotify07
 PKG_SUPPORTED_OPTIONS=	doc introspection
@@ -8,16 +8,15 @@ PLIST_VARS+=		doc introspection
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdoc)
-.include "../../textproc/gtk-doc/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-gtk-doc=yes --enable-gtk-doc-html=yes
-CONFIGURE_ARGS+=	--enable-gtk-doc-pdf=yes
+BUILD_DEPENDS+=	xmlto-[0-9]*:../../textproc/xmlto
+CONFIGURE_ARGS+=	--enable-docbook-docs
 PLIST.doc=		yes
 .else
-CONFIGURE_ARGS+=	--enable-gtk-doc=no --enable-gtk-doc-html=no
-CONFIGURE_ARGS+=	--enable-gtk-doc-pdf=no
+CONFIGURE_ARGS+=	--disable-docbook-docs
 .endif
 
 .if !empty(PKG_OPTIONS:Mintrospection)
+BUILDLINK_API_DEPENDS.gobject-introspection+=	gobject-introspection>=0.9.12
 .include "../../devel/gobject-introspection/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-introspection=yes
 PLIST.introspection=	yes
