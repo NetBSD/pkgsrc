3c07fbcb53fef290d180b03c1f568cd0611b392e
rodent <rodent@pkgsrc.org>
2013-04-22 06:03:36 +0000

    Reduce included dependencies to the bare minimum.

--- net/ccrtp/buildlink3.mk
+++ net/ccrtp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/04/17 00:54:16 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/04/22 06:03:36 rodent Exp $
 
 BUILDLINK_TREE+=	ccrtp
 
@@ -8,12 +8,7 @@ CCRTP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.ccrtp+=	ccrtp>=2.0.0
 BUILDLINK_PKGSRCDIR.ccrtp?=	../../net/ccrtp
 
-.include "../../converters/libiconv/buildlink3.mk"
-.include "../../devel/gettext-lib/buildlink3.mk"
-.include "../../security/libgpg-error/buildlink3.mk"
 .include "../../security/libgcrypt/buildlink3.mk"
-.include "../../security/openssl/buildlink3.mk"
-.include "../../mk/pthread.buildlink3.mk"
 .include "../../devel/ucommon/buildlink3.mk"
 .endif # CCRTP_BUILDLINK3_MK
 
