3cd1adc5404e628e51da754691b675da781e34ea
wiz <wiz@pkgsrc.org>
2013-04-23 09:02:26 +0000

    Update to 1.1.0. Bump ABI depends due to shlib major bump; but no packages use this version yet.
    Changes between 1.0.0 and 1.1.0:
    --------------------------------
    
     * Remove deprecated APIs and bump library version.
     * Corrected use of dvbpsi_ValidPSISection() to not use b_syntax_indicator as check,
       the function tries to always check the CRC32 of the given PSI section. Take care
       to only use the function for PSI sections that have a CRC32 field. Use the function
       dvbpsi_has_CRC32() for this purpose.
     * Remove workaround for checking CRC32 in TOT PSI table.
     * Add function dvbpsi_CalculateCRC32() to calculate a CRC32 on the payload.
     * Fix offset for description parsing in tables;
       - CAT
       - BAT
       - TOT
       - NIT
     * New descriptor:
       - 0x54 content descriptor
     * EIT: add NVOD event detection.

--- multimedia/libdvbpsi/buildlink3.mk
+++ multimedia/libdvbpsi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:25:03 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2013/04/23 09:02:26 wiz Exp $
 
 BUILDLINK_TREE+=	libdvbpsi
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libdvbpsi
 LIBDVBPSI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libdvbpsi+=	libdvbpsi>=0.1.3
-BUILDLINK_ABI_DEPENDS.libdvbpsi+=	libdvbpsi>=0.1.6
+BUILDLINK_ABI_DEPENDS.libdvbpsi+=	libdvbpsi>=1.1.0
 BUILDLINK_PKGSRCDIR.libdvbpsi?=	../../multimedia/libdvbpsi
 .endif # LIBDVBPSI_BUILDLINK3_MK
 
