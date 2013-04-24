f1f4422e5a619c88fefa603ee64556b8f8e566e0
tron <tron>
2013-04-24 21:11:06 +0000

    Pullup ticket #4124 - requested by bouyer sysutils/xentools41: security patch
    Revisions pulled up:
    - sysutils/xentools41/Makefile                                  1.30-1.31
    - sysutils/xentools41/distinfo                                  1.27-1.28
    - sysutils/xentools41/patches/patch-.._.._ipxe_src_arch_i386_include_librm.h 1.1
    - sysutils/xentools41/patches/patch-.._.._ipxe_src_core_settings.c 1.1
    - sysutils/xentools41/patches/patch-.._.._ipxe_src_net_tls.c    1.1
    - sysutils/xentools41/patches/patch-.._Config.mk                1.1
    - sysutils/xentools41/patches/patch-CVE-2013-0215-1             1.1
    - sysutils/xentools41/patches/patch-CVE-2013-0215-2             1.1
    - sysutils/xentools41/patches/patch-firmware_hvmloader_Makefile 1.1
    - sysutils/xentools41/patches/patch-libcx_xc__dom__boot.c       1.1
    - sysutils/xentools41/patches/patch-xenstat_libxenstat_Makefile 1.1
    
    ---
       Module Name:    pkgsrc
       Committed By:   joerg
       Date:           Thu Apr 11 19:57:53 UTC 2013
    
       Modified Files:
               pkgsrc/sysutils/xenkernel3: Makefile distinfo
               pkgsrc/sysutils/xenkernel3/patches: patch-cw
               pkgsrc/sysutils/xenkernel33: Makefile distinfo
               pkgsrc/sysutils/xenkernel41: Makefile distinfo
               pkgsrc/sysutils/xentools3: Makefile distinfo
               pkgsrc/sysutils/xentools3-hvm: Makefile distinfo
               pkgsrc/sysutils/xentools3/patches: patch-.._patch-Config.mk patch-cg
               pkgsrc/sysutils/xentools33: Makefile distinfo
               pkgsrc/sysutils/xentools33/patches: patch-ab
               pkgsrc/sysutils/xentools41: Makefile distinfo
       Added Files:
               pkgsrc/sysutils/xenkernel3/patches: patch-xen_arch_x86_hvm_io.c
                   patch-xen_arch_x86_string.c
               pkgsrc/sysutils/xenkernel33/patches: patch-Config.mk patch-xen_Makefile
                   patch-xen_arch_x86_Rules.mk
               pkgsrc/sysutils/xenkernel41/patches: patch-Config.mk patch-xen_Makefile
                   patch-xen_arch_x86_Rules.mk patch-xen_arch_x86_cpu_mcheck_vmce.c
                   patch-xen_arch_x86_time.c
               pkgsrc/sysutils/xentools3-hvm/patches: patch-.._patch-Config.mk
               pkgsrc/sysutils/xentools3/patches: patch-libxc_xc__dom__x86.c
               pkgsrc/sysutils/xentools33/patches: patch-blktap_drivers_block-qcow.c
                   patch-ioemu_block-vvfat.c
               pkgsrc/sysutils/xentools41/patches:
                   patch-.._.._ipxe_src_arch_i386_include_librm.h
                   patch-.._.._ipxe_src_core_settings.c patch-.._.._ipxe_src_net_tls.c
                   patch-.._Config.mk patch-firmware_hvmloader_Makefile
                   patch-libcx_xc__dom__boot.c patch-xenstat_libxenstat_Makefile
    
       Log Message:
       Allow building Xen infrastructure with Clang. Fix various bugs in
       xenkernel3, xenkernel41, xentools3 and xentools41 exposed by Clang
       default warnings. Bump revisions for those.
    
    ---
       Module Name:    pkgsrc
       Committed By:   bouyer
       Date:           Fri Apr 19 14:03:52 UTC 2013
    
       Modified Files:
               pkgsrc/sysutils/xentools41: Makefile distinfo
       Added Files:
               pkgsrc/sysutils/xentools41/patches: patch-CVE-2013-0215-1
                   patch-CVE-2013-0215-2
    
       Log Message:
       Add patch from Xen security advisory:
       http://lists.xen.org/archives/html/xen-announce/2013-02/msg00005.html

---
+++ sysutils/xentools41/patches/patch-.._Config.mk
@@ -0,0 +1,22 @@
+$NetBSD: patch-.._Config.mk,v 1.1.2.2 2013/04/24 21:11:06 tron Exp $
+
+--- ../Config.mk.orig	2013-03-25 09:51:57.000000000 +0000
++++ ../Config.mk
+@@ -15,7 +15,7 @@ SHELL     ?= /bin/sh
+ # Tools to run on system hosting the build
+ HOSTCC      = gcc
+ HOSTCFLAGS  = -Wall -Werror -Wstrict-prototypes -O2 -fomit-frame-pointer
+-HOSTCFLAGS += -fno-strict-aliasing
++HOSTCFLAGS += -fno-strict-aliasing ${EXTRA_HOSTCFLAGS}
+ 
+ DISTDIR     ?= $(XEN_ROOT)/dist
+ DESTDIR     ?= /
+@@ -141,6 +141,8 @@ CFLAGS += -Wall -Wstrict-prototypes
+ # result of any casted expression causes a warning.
+ CFLAGS += -Wno-unused-value
+ 
++CFLAGS += ${EXTRA_CFLAGS}
++
+ $(call cc-option-add,HOSTCFLAGS,HOSTCC,-Wdeclaration-after-statement)
+ $(call cc-option-add,CFLAGS,CC,-Wdeclaration-after-statement)
+ $(call cc-option-add,CFLAGS,CC,-Wno-unused-but-set-variable)
