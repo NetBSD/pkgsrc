3a2370645c5db38eeea4e8bca3abccf71c7087da
sbd <sbd@pkgsrc.org>
2013-04-24 04:32:34 +0000

    If you change the buildlink3.mk's included in the Makefile you need to have the packages buildlink3.mk match.

--- graphics/gle/buildlink3.mk
+++ graphics/gle/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:24:39 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2013/04/24 04:32:34 sbd Exp $
 
 BUILDLINK_TREE+=	gle
 
@@ -9,7 +9,12 @@ BUILDLINK_API_DEPENDS.gle+=		gle>=3.0.3
 BUILDLINK_ABI_DEPENDS.gle+=	gle>=3.1.0nb2
 BUILDLINK_PKGSRCDIR.gle?=	../../graphics/gle
 
-.include "../../graphics/Mesa/buildlink3.mk"
+# GLUT is used only by the examples, which aren't built or installed.
+#.include "../../graphics/glut/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../x11/libXmu/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
 .endif # GLE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gle
