28a8dbf112c8785ad67c7ea89c6b463b0dd77925
sbd <sbd@pkgsrc.org>
2013-04-24 09:55:03 +0000

    Use the ghostscript meta-package instead of ghostscript-gpl or ghostscript-agpl.
    Reverts revisions 1.255 and 1.254
    
       ----------------------------
       revision 1.255
       date: 2013/03/16 23:03:33;  author: dholland;  state: Exp;  lines: +3 -3
       print/ghostscript -> print/ghostscript-agpl
       ----------------------------
       revision 1.254
       date: 2013/03/16 21:47:14;  author: dholland;  state: Exp;  lines: +13 -3
       Choose ghostscript package for ghostscript tools based on whether
       gnu-agpl-* is in ACCEPTABLE_LICENSES.
    
       This is mostly the same as the old ghostscript type logic that was
       removed in version 1.223.

--- mk/tools/replace.mk
+++ mk/tools/replace.mk
@@ -1,4 +1,4 @@
-# $NetBSD: replace.mk,v 1.255 2013/03/16 23:03:33 dholland Exp $
+# $NetBSD: replace.mk,v 1.256 2013/04/24 09:55:03 sbd Exp $
 #
 # Copyright (c) 2005 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -1074,18 +1074,8 @@ GHOSTSCRIPT_REQD?=	6.01
 # various package options.
 #
 .if !defined(TOOLS_DEPENDS.ghostscript)
-_TOOLS_DEP.ghostscript:=	ghostscript-gpl
-_TOOLS_DEP.ghostscript:=	${_TOOLS_DEP.ghostscript},ghostscript-agpl
-#
-# Determine the default Ghostscript package to build based on
-# ACCEPTABLE_LICENSES.
-#
-.  if !empty(ACCEPTABLE_LICENSES:Mgnu-agpl-*)
-_TOOLS_PKGSRCDIR.ghostscript=	../../print/ghostscript-agpl
-.  else
-_TOOLS_PKGSRCDIR.ghostscript=	../../print/ghostscript-gpl
-.  endif
-TOOLS_DEPENDS.ghostscript=	{${_TOOLS_DEP.ghostscript}}>=${GHOSTSCRIPT_REQD}:${_TOOLS_PKGSRCDIR.ghostscript}
+_TOOLS_DEP.ghostscript:=	ghostscript
+TOOLS_DEPENDS.ghostscript=	ghostscript>=${GHOSTSCRIPT_REQD}:../../print/ghostscript
 MAKEVARS+=			TOOLS_DEPENDS.ghostscript
 .endif
 
