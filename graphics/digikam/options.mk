caaeb78445a49cb488e2cb1a210174fd14688703
markd <markd@pkgsrc.org>
2013-04-28 02:03:37 +0000

    Make options optional. switch lensfun option off by default as seems to cause segfaults. remove nonexistant marble option. Set build type to Debug to assist tracking down segfaults. Bump PKGREVISION.

--- graphics/digikam/options.mk
+++ graphics/digikam/options.mk
@@ -1,23 +1,25 @@
-# $NetBSD: options.mk,v 1.2 2012/03/21 21:14:47 markd Exp $
+# $NetBSD: options.mk,v 1.3 2013/04/28 02:03:38 markd Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.digikam
-PKG_SUPPORTED_OPTIONS=	lensfun liblqr marble pim
-PKG_SUGGESTED_OPTIONS=	lensfun liblqr marble pim
+PKG_SUPPORTED_OPTIONS=	lensfun liblqr pim
+PKG_SUGGESTED_OPTIONS=	lensfun liblqr pim
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mlensfun)
 .include "../../graphics/lensfun/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DWITH_LensFun:BOOL=OFF
 .endif
 
 .if !empty(PKG_OPTIONS:Mliblqr)
 .include "../../graphics/liblqr/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mmarble)
-.include "../../misc/marble/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DWITH_Lqr-1:BOOL=OFF
 .endif
 
 .if !empty(PKG_OPTIONS:Mpim)
 .include "../../misc/kdepimlibs4/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DWITH_KdepimLibs:BOOL=OFF
 .endif
