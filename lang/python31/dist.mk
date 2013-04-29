ea96a49d3f7dd595124430efb5e204d8e6d83dbb
wiz <wiz@pkgsrc.org>
2013-04-29 10:49:37 +0000

    Remove python31.
    There are three python3 versions in pkgsrc, python31, python32 and
    python33.
    
    The last published update for python was only released for python27,
    python32 and python33 -- not for python31.
    
    No reason for keeping python31 was brought up in two weeks
    on pkgsrc-users, so remove it.

--- lang/python31/dist.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: dist.mk,v 1.2 2012/04/14 11:22:07 obache Exp $
-
-PY_DISTVERSION=	3.1.5
-DISTNAME=	Python-${PY_DISTVERSION}
-EXTRACT_SUFX=	.tar.bz2
-DISTINFO_FILE=	${.CURDIR}/../../lang/python31/distinfo
-PATCHDIR=	${.CURDIR}/../../lang/python31/patches
-MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION}/
