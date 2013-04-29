dbd451a95da8291926f53caa8d49e3c245250cd2
sborrill <sborrill@pkgsrc.org>
2013-04-29 11:22:42 +0000

    Move version of ipxe-git to a variable in version.mk rather than being hardwired in Makefile. This allows other packages (namely xenstoretools) to retrieve the version number.

--- sysutils/xentools41/version.mk
+++ sysutils/xentools41/version.mk
@@ -1,5 +1,6 @@
-# $NetBSD: version.mk,v 1.4 2013/01/18 11:27:43 drochner Exp $
+# $NetBSD: version.mk,v 1.5 2013/04/29 11:22:42 sborrill Exp $
 
 # Version number is used by xentools41 and xenstoretools
 
 VERSION=	4.1.4
+VERSION_IPXE=	1.0.0
