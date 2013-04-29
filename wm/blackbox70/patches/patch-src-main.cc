b05110f5bd37d7aaa7c4cb6a5e35cbdafc0cf264
joerg <joerg@pkgsrc.org>
2013-04-29 21:27:15 +0000

    Add missing includes from libstdc++'s namespace pollution. Let the compiler figure out which types std::advance is used with.

--- wm/blackbox70/patches/patch-src-main.cc
+++ wm/blackbox70/patches/patch-src-main.cc
@@ -1,12 +1,13 @@
-$NetBSD: patch-src-main.cc,v 1.1 2011/11/22 20:02:48 joerg Exp $
+$NetBSD: patch-src-main.cc,v 1.2 2013/04/29 21:27:15 joerg Exp $
 
---- src/main.cc.orig	2011-11-22 18:56:19.000000000 +0000
+--- src/main.cc.orig	2005-01-03 09:42:57.000000000 +0000
 +++ src/main.cc
-@@ -35,7 +35,7 @@
+@@ -35,7 +35,8 @@
  #include "../version.h"
  
  #include <stdio.h>
 -
++#include <stdlib.h>
 +#include <string.h>
  
  static void showHelp(int exitval) {
