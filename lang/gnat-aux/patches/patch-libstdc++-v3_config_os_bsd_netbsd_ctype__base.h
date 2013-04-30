c62a3b37d7e81bd355307a2b86d570578895c2e4
joerg <joerg@pkgsrc.org>
2013-04-30 22:24:15 +0000

    Deal with 16bit ctype table change in NetBSD/current.

--- lang/gnat-aux/patches/patch-libstdc++-v3_config_os_bsd_netbsd_ctype__base.h
+++ lang/gnat-aux/patches/patch-libstdc++-v3_config_os_bsd_netbsd_ctype__base.h
@@ -1,20 +1,26 @@
-$NetBSD: patch-libstdc++-v3_config_os_bsd_netbsd_ctype__base.h,v 1.2 2013/04/30 22:24:16 joerg Exp $
+$NetBSD: patch-libstdc++-v3_config_os_bsd_netbsd_ctype__base.h,v 1.1 2013/04/30 22:24:16 joerg Exp $
 
---- libstdc++-v3/config/os/bsd/netbsd/ctype_base.h.orig	2011-01-30 22:39:36.000000000 +0000
+--- libstdc++-v3/config/os/bsd/netbsd/ctype_base.h.orig	2012-06-20 08:30:55.000000000 +0000
 +++ libstdc++-v3/config/os/bsd/netbsd/ctype_base.h
-@@ -30,8 +30,6 @@
- // Full details can be found from the CVS files at:
- //   anoncvs@anoncvs.netbsd.org:/cvsroot/basesrc/include/ctype.h
- // See www.netbsd.org for details of access.
--  
--#include <sys/param.h>
- 
- namespace std _GLIBCXX_VISIBILITY(default)
- {
-@@ -45,21 +43,21 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
+@@ -43,21 +43,22 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
  
      // NB: Offsets into ctype<char>::_M_table force a particular size
      // on the mask type. Because of this, we don't use an enum.
+-    typedef unsigned char      	mask;
+ 
+-#ifndef _CTYPE_U
+-    static const mask upper    	= _U;
+-    static const mask lower 	= _L;
+-    static const mask alpha 	= _U | _L;
+-    static const mask digit 	= _N;
+-    static const mask xdigit 	= _N | _X;
+-    static const mask space 	= _S;
+-    static const mask print 	= _P | _U | _L | _N | _B;
+-    static const mask graph 	= _P | _U | _L | _N;
+-    static const mask cntrl 	= _C;
+-    static const mask punct 	= _P;
+-    static const mask alnum 	= _U | _L | _N;
+-#else
 +#if defined(_CTYPE_BL)
 +    typedef unsigned short      mask;
 +    static const mask upper     = _CTYPE_U;
@@ -29,25 +35,11 @@ $NetBSD: patch-libstdc++-v3_config_os_bsd_netbsd_ctype__base.h,v 1.2 2013/04/30 
 +    static const mask punct     = _CTYPE_P;
 +    static const mask alnum     = _CTYPE_A | _CTYPE_D;
 +#elif defined(_CTYPE_U)
-     typedef unsigned char      	mask;
--
--#if __NetBSD_Version__ < 599004100
--    static const mask upper    	= _U;
--    static const mask lower 	= _L;
--    static const mask alpha 	= _U | _L;
--    static const mask digit 	= _N;
--    static const mask xdigit 	= _N | _X;
--    static const mask space 	= _S;
--    static const mask print 	= _P | _U | _L | _N | _B;
--    static const mask graph 	= _P | _U | _L | _N;
--    static const mask cntrl 	= _C;
--    static const mask punct 	= _P;
--    static const mask alnum 	= _U | _L | _N;
--#else
++    typedef unsigned char      	mask;
      static const mask upper    	= _CTYPE_U;
      static const mask lower 	= _CTYPE_L;
      static const mask alpha 	= _CTYPE_U | _CTYPE_L;
-@@ -71,6 +69,19 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
+@@ -69,6 +70,19 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
      static const mask cntrl 	= _CTYPE_C;
      static const mask punct 	= _CTYPE_P;
      static const mask alnum 	= _CTYPE_U | _CTYPE_L | _CTYPE_N;
