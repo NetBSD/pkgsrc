fd3aa87008ad254e440e73fb43d3bab64086a6f4
joerg <joerg>
2013-04-30 22:34:29 +0000

    Fix various case of missing includes due to libstdc++'s namespace pollution.

--- graphics/gfract/patches/patch-color.cpp
+++ graphics/gfract/patches/patch-color.cpp
@@ -1,9 +1,10 @@
-$NetBSD: patch-color.cpp,v 1.1 2011/11/24 14:02:54 joerg Exp $
+$NetBSD: patch-color.cpp,v 1.2 2013/04/30 22:34:30 joerg Exp $
 
---- color.cpp.orig	2011-11-24 00:30:21.000000000 +0000
+--- color.cpp.orig	2004-08-13 15:42:29.000000000 +0000
 +++ color.cpp
-@@ -1,5 +1,6 @@
+@@ -1,5 +1,7 @@
  #include <math.h>
++#include <stdlib.h>
  #include <stdio.h>
 +#include <string.h>
  #include <algorithm>
