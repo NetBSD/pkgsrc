24e78e464fb16ee75b563dfc12127d0669ef165c
joerg <joerg@pkgsrc.org>
2013-04-30 22:32:43 +0000

    Use <memory> for libc++.

---
+++ x11/gtkmm-utils/patches/patch-gtkmm-utils_tile-view.h
@@ -0,0 +1,12 @@
+$NetBSD: patch-gtkmm-utils_tile-view.h,v 1.1 2013/04/30 22:32:43 joerg Exp $
+
+--- gtkmm-utils/tile-view.h.orig	2013-04-30 21:42:42.000000000 +0000
++++ gtkmm-utils/tile-view.h
+@@ -26,7 +26,6 @@
+ 
+ #include <list>
+ #include <memory>
+-#include <tr1/memory>
+ #include <gtkmm/adjustment.h>
+ #include <gtkmm/box.h>
+ #include <gtkmm/scrolledwindow.h>
