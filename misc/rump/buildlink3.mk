c9388054a7410ff2935bc638d6ab2a9636d0eb07
stacktic <stacktic@pkgsrc.org>
2013-05-01 17:16:06 +0000

    Update misc/rump and filesystems/fs-utils to 20130430 to use github versions which are available respectively at https://github.com/anttikantee/buildrump.sh and https://github.com/stacktic/fs-utils

--- misc/rump/buildlink3.mk
+++ misc/rump/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/11/16 15:29:57 stacktic Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/05/01 17:16:06 stacktic Exp $
 
 BUILDLINK_TREE+=	rump
 
 .if !defined(RUMP_BUILDLINK3_MK)
 RUMP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rump+=	rump>=20091108nb3
+BUILDLINK_API_DEPENDS.rump+=	rump>=20130430
 BUILDLINK_PKGSRCDIR.rump?=	../../misc/rump
 .endif	# RUMP_BUILDLINK3_MK
 
