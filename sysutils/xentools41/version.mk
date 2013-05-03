f2717e9cd39c474855087437814abf2ff9b56e66
drochner <drochner>
2013-05-03 16:48:37 +0000

    update to 4.1.5 This integrates fixes for all vulnerabilities which were patched in pkgsrc before. Among many bug fixes and improvements (around 50 since Xen 4.1.4):  * ACPI APEI/ERST finally working on production systems  * Bug fixes for other low level system state handling  * Support for xz compressed Dom0 and DomU kernels

--- sysutils/xentools41/version.mk
+++ sysutils/xentools41/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.5 2013/04/29 11:22:42 sborrill Exp $
+# $NetBSD: version.mk,v 1.6 2013/05/03 16:48:38 drochner Exp $
 
 # Version number is used by xentools41 and xenstoretools
 
-VERSION=	4.1.4
+VERSION=	4.1.5
 VERSION_IPXE=	1.0.0
