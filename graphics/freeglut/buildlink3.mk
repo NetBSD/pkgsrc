5585eef0d0a90434c99d0a360051d8d2c189f54b
rodent <rodent@pkgsrc.org>
2013-05-04 14:32:20 +0000

    Updated to latest stable release. Added dependencies required during configure. Added dependencies which are linked to buildlink3.mk. Removed glu from buildlink3.mk, as the resulting library doesn't link to it. patches/patch-{a,b} were commited upstream - removed. Added patch to fix usb.h include on NetBSD.
    From NEWS:
        * Tons of bug fixes
        * Added deprecated, but working Joystick API.
        * Added new constant GLUT_INIT_STATE for glutGet() to check if freeglut is
          already initialized.
        * Added new API entry for full-screen mode
            void glutFullScreenToggle( void );
          with a related new constant GLUT_FULL_SCREEN for glutGet().
        * Added new API entry to de-initialize freeglut:
            void glutExit( void );
        * Added more special keys: GLUT_KEY_NUM_LOCK, GLUT_KEY_BEGIN GLUT_KEY_DELETE
        * Added support for windows without captions and/or borders via two new
          constants GLUT_CAPTIONLESS and GLUT_BORDERLESS for glutInitDisplayMode
          (currently works for Windows only).
        * Added support for multisampling: The number of samples per pixel to use
          when GLUT_MULTISAMPLE is specified in glutInitDisplayMode() can be set via
          glutSetOption() with parameter GLUT_MULTISAMPLE now. glutGet() with the
          same token retrieves that value. The possible number of samples per pixels
          can be queried via the new API entry
            int *glutGetModeValues( GLenum mode, int *size );
          with mode GLUT_MULTISAMPLE. (glutGetModeValues() currently only works for
          X11)
        * Added new constant GLUT_AUX for glutSetOption() to set the number of
          auxiliary buffers. The possible number of auxiliary buffers can be
          queried via glutGetModeValues with mode GLUT_AUX.
        * Added support for versioned (i.e. 3.0) OpenGL contexts: New API entries
            void glutInitContextVersion( int majorVersion, int minorVersion );
            void glutInitContextFlags( int flags );
          with related new constants GLUT_DEBUG and GLUT_FORWARD_COMPATIBLE for
          the latter API entry. Added new constants GLUT_INIT_MAJOR_VERSION,
          GLUT_INIT_MINOR_VERSION and GLUT_INIT_FLAGS  for glutGet().

--- graphics/freeglut/buildlink3.mk
+++ graphics/freeglut/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:24:38 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2013/05/04 14:32:20 rodent Exp $
 
 BUILDLINK_TREE+=	freeglut
 
@@ -10,7 +10,11 @@ BUILDLINK_ABI_DEPENDS.freeglut+=	freeglut>=2.2.0nb3
 BUILDLINK_PKGSRCDIR.freeglut?=	../../graphics/freeglut
 
 .include "../../graphics/MesaLib/buildlink3.mk"
-.include "../../graphics/glu/buildlink3.mk"
+.include "../../x11/libXi/buildlink3.mk"
+.include "../../x11/libXrandr/buildlink3.mk"
+.include "../../x11/libXxf86vm/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../mk/dlopen.buildlink3.mk"
 .endif # FREEGLUT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-freeglut
