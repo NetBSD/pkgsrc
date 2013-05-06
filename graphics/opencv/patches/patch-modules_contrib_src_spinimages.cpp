b0c9f8c2874a16e749789c27271b8705d3a01511
joerg <joerg@pkgsrc.org>
2013-05-06 14:48:22 +0000

    Add missing include. Require C++11 when building with Clang.

---
+++ graphics/opencv/patches/patch-modules_contrib_src_spinimages.cpp
@@ -0,0 +1,12 @@
+$NetBSD: patch-modules_contrib_src_spinimages.cpp,v 1.1 2013/05/06 14:48:22 joerg Exp $
+
+--- modules/contrib/src/spinimages.cpp.orig	2013-05-05 18:32:00.000000000 +0000
++++ modules/contrib/src/spinimages.cpp
+@@ -46,6 +46,7 @@
+ #include <functional>
+ #include <fstream>
+ #include <limits>
++#include <numeric>
+ #include <set>
+ 
+ using namespace cv;
