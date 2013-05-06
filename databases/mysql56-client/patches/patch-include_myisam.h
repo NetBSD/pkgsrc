1b9de7ee6d8868e22b7874e9299a1709611cf372
joerg <joerg>
2013-05-06 14:41:07 +0000

    Add more test() changes for the server.

--- databases/mysql56-client/patches/patch-include_myisam.h
+++ databases/mysql56-client/patches/patch-include_myisam.h
@@ -1,15 +1,35 @@
-$NetBSD: patch-include_myisam.h,v 1.1 2013/02/13 21:00:05 adam Exp $
+$NetBSD: patch-include_myisam.h,v 1.2 2013/05/06 14:41:08 joerg Exp $
 
 Adjust MI_MAX_KEY_LENGTH x4.
 
---- include/myisam.h.orig	2010-02-04 11:37:06.000000000 +0000
+--- include/myisam.h.orig	2013-05-05 21:55:07.000000000 +0000
 +++ include/myisam.h
-@@ -49,7 +49,7 @@ extern "C" {
-   The following defines can be increased if necessary.
-   But beware the dependency of MI_MAX_POSSIBLE_KEY_BUFF and MI_MAX_KEY_LENGTH.
- */
--#define MI_MAX_KEY_LENGTH           1000            /* Max length in bytes */
-+#define MI_MAX_KEY_LENGTH           4000            /* Max length in bytes */
- #define MI_MAX_KEY_SEG              16              /* Max segments for key */
+@@ -80,8 +80,8 @@ extern "C" {
  
- #define MI_MAX_KEY_BUFF  (MI_MAX_KEY_LENGTH+MI_MAX_KEY_SEG*6+8+8)
+ #define mi_is_key_active(_keymap_,_keyno_) \
+                             (((_keyno_) < MI_KEYMAP_BITS) ? \
+-                             test((_keymap_) & (ULL(1) << (_keyno_))) : \
+-                             test((_keymap_) & MI_KEYMAP_HIGH_MASK))
++                             my_test((_keymap_) & (ULL(1) << (_keyno_))) : \
++                             my_test((_keymap_) & MI_KEYMAP_HIGH_MASK))
+ #define mi_set_key_active(_keymap_,_keyno_) \
+                             (_keymap_)|= (((_keyno_) < MI_KEYMAP_BITS) ? \
+                                           (ULL(1) << (_keyno_)) : \
+@@ -94,7 +94,7 @@ extern "C" {
+ #else
+ 
+ #define mi_is_key_active(_keymap_,_keyno_) \
+-                            test((_keymap_) & (ULL(1) << (_keyno_)))
++                            my_test((_keymap_) & (ULL(1) << (_keyno_)))
+ #define mi_set_key_active(_keymap_,_keyno_) \
+                             (_keymap_)|= (ULL(1) << (_keyno_))
+ #define mi_clear_key_active(_keymap_,_keyno_) \
+@@ -103,7 +103,7 @@ extern "C" {
+ #endif
+ 
+ #define mi_is_any_key_active(_keymap_) \
+-                            test((_keymap_))
++                            my_test((_keymap_))
+ #define mi_is_all_keys_active(_keymap_,_keys_) \
+                             ((_keymap_) == mi_get_mask_all_keys_active(_keys_))
+ #define mi_set_all_keys_active(_keymap_,_keys_) \
