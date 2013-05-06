aa9addac51a5cfe559d2ce502a8598c488029091
joerg <joerg@pkgsrc.org>
2013-05-06 14:54:57 +0000

    Use nullptr, 0 is ambigious. Prefer C++11 interfaces over tr1.

---
+++ multimedia/kmix/patches/patch-core_mixertoolbox.cpp
@@ -0,0 +1,20 @@
+$NetBSD: patch-core_mixertoolbox.cpp,v 1.1 2013/05/06 14:54:57 joerg Exp $
+
+--- core/mixertoolbox.cpp.orig	2013-05-04 01:52:48.000000000 +0000
++++ core/mixertoolbox.cpp
+@@ -249,13 +249,13 @@ void MixerToolBox::initMixerInternal(Mul
+ 
+    
+     // Add a master device (if we haven't defined one yet)
+-   if ( Mixer::getGlobalMasterMD(false) == 0 ) {
++   if ( Mixer::getGlobalMasterMD(false) == nullptr ) {
+       // We have no master card yet. This actually only happens when there was
+       // not one defined in the kmixrc.
+       // So lets just set the first card as master card.
+       if ( Mixer::mixers().count() > 0 ) {
+     	  shared_ptr<MixDevice> master = Mixer::mixers().first()->getLocalMasterMD();
+-         if ( master != 0 ) {
++         if ( master != nullptr ) {
+              QString controlId = master->id();
+              Mixer::setGlobalMaster( Mixer::mixers().first()->id(), controlId, true);
+          }
