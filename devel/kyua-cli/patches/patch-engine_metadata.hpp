b4b4fe798c5703c4e8682cf53a0e31369f52f785
joerg <joerg@pkgsrc.org>
2013-05-06 14:46:58 +0000

    Fix missing includes. Prefer C++11 over TR1 when using libc++ or C++11.

--- devel/kyua-cli/patches/patch-engine_metadata.hpp
+++ devel/kyua-cli/patches/patch-engine_metadata.hpp
@@ -1,8 +1,20 @@
-$NetBSD: patch-engine_metadata.hpp,v 1.1 2013/03/02 18:16:39 joerg Exp $
+$NetBSD: patch-engine_metadata.hpp,v 1.2 2013/05/06 14:46:58 joerg Exp $
 
---- engine/metadata.hpp.orig	2013-02-28 21:58:41.000000000 +0000
+--- engine/metadata.hpp.orig	2013-01-26 03:01:41.000000000 +0000
 +++ engine/metadata.hpp
-@@ -44,7 +44,7 @@
+@@ -38,13 +38,19 @@
+ #include <set>
+ #include <string>
+ 
++#if defined(_LIBCPP_VERSION) || __cplusplus >= 201103L
++#include <memory>
++using std::shared_ptr;
++#else
+ #include <tr1/memory>
++using std::tr1::shared_ptr;
++#endif
+ 
+ #include "utils/noncopyable.hpp"
  
  namespace utils {
  namespace config { class tree; }
@@ -11,3 +23,12 @@ $NetBSD: patch-engine_metadata.hpp,v 1.1 2013/03/02 18:16:39 joerg Exp $
  namespace fs { class path; }
  namespace units { class bytes; }
  }  // namespace utils
+@@ -73,7 +79,7 @@ class metadata {
+     struct impl;
+ 
+     /// Pointer to the shared internal implementation.
+-    std::tr1::shared_ptr< impl > _pimpl;
++    shared_ptr< impl > _pimpl;
+ 
+     friend class metadata_builder;
+ 
