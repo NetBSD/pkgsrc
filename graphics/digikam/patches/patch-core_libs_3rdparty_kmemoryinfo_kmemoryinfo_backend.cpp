f846e806c64d6c20fa9d1704201903654565696c
markd <markd@pkgsrc.org>
2013-05-06 20:40:12 +0000

    Report memory sizes correctly.

--- graphics/digikam/patches/patch-core_libs_3rdparty_kmemoryinfo_kmemoryinfo_backend.cpp
+++ graphics/digikam/patches/patch-core_libs_3rdparty_kmemoryinfo_kmemoryinfo_backend.cpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-core_libs_3rdparty_kmemoryinfo_kmemoryinfo_backend.cpp,v 1.2 2013/04/03 12:52:04 markd Exp $
+$NetBSD: patch-core_libs_3rdparty_kmemoryinfo_kmemoryinfo_backend.cpp,v 1.3 2013/05/06 20:40:12 markd Exp $
 
 Make memory info backend compile on NetBSD.  Need to check
 it actually returns the correct results.
@@ -24,12 +24,16 @@ it actually returns the correct results.
  
      mib[0] = CTL_VM;
      mib[1] = VM_UVMEXP;
-@@ -422,7 +423,7 @@ int get_mem_stats(Digikam::KMemoryInfo::
+@@ -420,9 +421,9 @@ int get_mem_stats(Digikam::KMemoryInfo::
+         return 0;
+     }
  
-     data->totalRam = uvm->pagesize * uvm->npages;
-     data->cacheRam = uvm->pagesize * (uvm->filepages + uvm->execpages);
+-    data->totalRam = uvm->pagesize * uvm->npages;
+-    data->cacheRam = uvm->pagesize * (uvm->filepages + uvm->execpages);
 -    data->freeRam  = uvm->pagesize * (uvm->free + uvm->inactive);
-+    data->freeRam  = uvm->pagesize * (uvm->free + uvm->paging);
++    data->totalRam = (qint64) uvm->pagesize * (qint64) uvm->npages;
++    data->cacheRam = (qint64) uvm->pagesize * (qint64) (uvm->filepages + uvm->execpages);
++    data->freeRam  = (qint64) uvm->pagesize * (qint64) (uvm->free + uvm->paging);
      data->usedRam  = data->totalRam - data->freeRam;
  
      return 1;
