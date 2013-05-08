3da339cd7ff1193f2f2eac6a43dfa49402d5bae0
adam <adam@pkgsrc.org>
2013-05-08 17:48:33 +0000

    Version 2.0.0         * Bumped version to track OpenEXR         * Numerous minor fixes, missing includes etc

--- graphics/ilmbase/patches/patch-Iex_IexThrowErrnoExc.cpp
+++ graphics/ilmbase/patches/patch-Iex_IexThrowErrnoExc.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-Iex_IexThrowErrnoExc.cpp,v 1.1 2012/07/26 10:58:47 bsiegert Exp $
+$NetBSD: patch-Iex_IexThrowErrnoExc.cpp,v 1.2 2013/05/08 17:48:33 adam Exp $
 
 --- Iex/IexThrowErrnoExc.cpp.orig	Thu Jul 26 10:46:21 2012
 +++ Iex/IexThrowErrnoExc.cpp
-@@ -410,7 +410,7 @@ void throwErrnoExc (const std::string &t
+@@ -419,7 +419,7 @@ void throwErrnoExc (const std::string &t
  	    throw EnametoolongExc (tmp);
        #endif
  
