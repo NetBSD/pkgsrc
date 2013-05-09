933b6c62f97c01989964346c55aaf256c23c47a5
adam <adam>
2013-05-09 07:39:04 +0000

    Massive revbump after updating graphics/ilmbase, graphics/openexr, textproc/icu.

--- graphics/openexr/buildlink3.mk
+++ graphics/openexr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:24:44 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2013/05/09 07:39:11 adam Exp $
 
 BUILDLINK_TREE+=	openexr
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	openexr
 OPENEXR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.openexr+=	openexr>=1.6.1
+BUILDLINK_ABI_DEPENDS.openexr?=	openexr>=2.0.0
 BUILDLINK_PKGSRCDIR.openexr?=	../../graphics/openexr
 
 PTHREAD_OPTS+=	require
