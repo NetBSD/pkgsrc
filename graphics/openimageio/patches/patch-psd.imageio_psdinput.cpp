7517afa7c2317cf7a1161a9639b0fe559fbc651b
joerg <joerg@pkgsrc.org>
2013-05-09 13:58:54 +0000

    Add missing includes. Don't use forward defines of the STL.

---
+++ graphics/openimageio/patches/patch-psd.imageio_psdinput.cpp
@@ -0,0 +1,13 @@
+$NetBSD: patch-psd.imageio_psdinput.cpp,v 1.1 2013/05/09 13:58:54 joerg Exp $
+
+--- psd.imageio/psdinput.cpp.orig	2013-02-21 17:10:46.000000000 +0000
++++ psd.imageio/psdinput.cpp
+@@ -328,7 +328,7 @@ private:
+         // For debugging, numeric_cast will throw if precision is lost:
+         // value = boost::numeric_cast<TVariable>(buffer);
+         value = buffer;
+-        return m_file;
++        return m_file.good();
+     }
+ 
+     int read_pascal_string (std::string &s, uint16_t mod_padding);
