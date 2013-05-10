d7bf697125d49ca49f54334b4d112563f5cb498e
adam <adam@pkgsrc.org>
2013-05-10 07:13:59 +0000

    Added dvdcss as an option.

--- multimedia/libdvdread/buildlink3.mk
+++ multimedia/libdvdread/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2011/08/09 20:53:15 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2013/05/10 07:13:59 adam Exp $
 
 BUILDLINK_TREE+=	libdvdread
 
@@ -8,6 +8,10 @@ LIBDVDREAD_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libdvdread+=	libdvdread>=4.1.3nb1
 BUILDLINK_ABI_DEPENDS.libdvdread+=	libdvdread>=4.1.3nb1
 BUILDLINK_PKGSRCDIR.libdvdread?=	../../multimedia/libdvdread
+
+.if !empty(PKG_BUILD_OPTIONS.libdvdread:Mdvdcss)
+.include "../../multimedia/libdvdcss/buildlink3.mk"
+.endif
 .endif # LIBDVDREAD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libdvdread
