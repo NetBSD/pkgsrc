f4fd3e1fbfe763af9a8d6337e58a8ce750109284
adam <adam@pkgsrc.org>
2013-05-10 08:24:57 +0000

    Changes 3.0.4: This release adds several performance features for the Hydra process manager, support for communicator-specific tuning of eager/rendezvous thresholds, and fixes several bugs present in 3.0.3. Please use this release instead of 3.0.3.

--- parallel/mpi-ch/options.mk
+++ parallel/mpi-ch/options.mk
@@ -1,14 +1,24 @@
-# $NetBSD: options.mk,v 1.2 2013/04/15 21:40:17 adam Exp $
+# $NetBSD: options.mk,v 1.3 2013/05/10 08:24:57 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mpich
-PKG_SUPPORTED_OPTIONS=	debug mpich-mpd threads
+PKG_SUPPORTED_OPTIONS=	debug f90 mpich-mpd threads
 PKG_SUGGESTED_OPTIONS=	threads
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=	f90 mpd
+
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-g=debug	# problems
 INSTALL_UNSTRIPPED=	yes
+.endif
+
+.if !empty(PKG_OPTIONS:Mf90)
+USE_LANGUAGES+=		fortran
+PLIST.f90=		yes
+.else
+USE_LANGUAGES+=		fortran77
+CONFIGURE_ARGS+=	--disable-fc
 .endif
 
 .if !empty(PKG_OPTIONS:Mthreads)
@@ -21,9 +31,7 @@ CONFIGURE_ARGS+=	--with-thread-package=posix # aka pthreads
 CONFLICTS+=		musicpd-[0-9]*	# installs bin/mpd
 CONFIGURE_ARGS+=	--with-pm=hydra:mpd:gforker
 CONFIGURE_ARGS+=	--with-python=python${PYVERSSUFFIX}
-PLIST_SRC+=		PLIST.mpd
+PLIST.mpd=		yes
 REPLACE_PYTHON+=	src/pm/mpd/*.py src/pm/mpd/test/*.py
 .include "../../lang/python/application.mk"
 .endif
-
-PLIST_SRC+=	PLIST
