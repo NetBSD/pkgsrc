7cba17f0985f1d5244a63e6910cf1fbcc90d9d7c
riastradh <riastradh@pkgsrc.org>
2013-05-11 20:47:15 +0000

    Make dbus and gnome-keyring options (default enabled) in print/evince3.

--- print/evince3/options.mk
+++ print/evince3/options.mk
@@ -1,11 +1,26 @@
-# $NetBSD: options.mk,v 1.1.1.1 2012/03/21 17:40:20 drochner Exp $
+# $NetBSD: options.mk,v 1.2 2013/05/11 20:47:15 riastradh Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.evince
-PKG_SUPPORTED_OPTIONS=	dvi djvu xps
+PKG_SUPPORTED_OPTIONS=	dbus djvu dvi gnome-keyring xps
+PKG_SUGGESTED_OPTIONS=	dbus gnome-keyring
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=		${PKG_SUPPORTED_OPTIONS}
+
+.if !empty(PKG_OPTIONS:Mdbus)
+.include "../../sysutils/dbus/buildlink3.mk"
+BUILDLINK_API_DEPENDS.dbus-glib+=       dbus-glib>=0.70
+.include "../../sysutils/dbus-glib/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-dbus
+.endif
+
+.if !empty(PKG_OPTIONS:Mgnome-keyring)
+.include "../../security/libgnome-keyring/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-keyring
+.endif
 
 .if !empty(PKG_OPTIONS:Mdvi)
 PLIST.dvi=		yes
