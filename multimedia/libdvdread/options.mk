80f049cc9c513b76e88eb22fa22ed5fb69c0a636
obache <obache@pkgsrc.org>
2013-05-12 03:04:26 +0000

    missing include of key feature file.

--- multimedia/libdvdread/options.mk
+++ multimedia/libdvdread/options.mk
@@ -1,7 +1,9 @@
-# $NetBSD: options.mk,v 1.1 2013/05/10 07:13:59 adam Exp $
+# $NetBSD: options.mk,v 1.2 2013/05/12 03:04:26 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libdvdread
 PKG_SUPPORTED_OPTIONS=	dvdcss
+
+.include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdvdcss)
 CPPFLAGS+=		-DHAVE_DVDCSS_DVDCSS_H=1
