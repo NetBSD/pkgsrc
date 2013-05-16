a94dffbe39c7150bb3dda4da3262cd888be5d0c0
taca <taca@pkgsrc.org>
2013-05-16 16:19:14 +0000

    Update php53 to 5.3.25 (PHP 5.3.25).
    09 May 2013, PHP 5.3.25
    
    - Core:
      . Fixed bug #64578 (debug_backtrace in set_error_handler corrupts zend heap:
        segfault). (Laruence)
      . Fixed bug #64458 (dns_get_record result with string of length -1). (Stas)
      . Fixed bugs #47675 and #64577 (fd leak on Solaris). (Rasmus)
    
    - Streams:
      . Fixed Windows x64 version of stream_socket_pair() and improved error
        handling. (Anatol Belski)
    
    - Zip:
      . Fixed bug #64342 (ZipArchive::addFile() has to check for file existence).
        (Anatol)

--- lang/php53/Makefile.php
+++ lang/php53/Makefile.php
@@ -1,4 +1,4 @@
-# $NetBSD: Makefile.php,v 1.32 2013/05/09 14:31:48 taca Exp $
+# $NetBSD: Makefile.php,v 1.33 2013/05/16 16:19:14 taca Exp $
 # used by lang/php53/Makefile
 # used by www/ap-php/Makefile
 
@@ -57,8 +57,8 @@ PKG_SUGGESTED_OPTIONS+=	inet6 ssl
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Msuhosin)
-SUHOSIN_PHPVER=		5.3.23
-.  if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS} && ${PHP_BASE_VERS} != "5.3.24"
+SUHOSIN_PHPVER=		5.3.25
+.  if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS} && ${PHP_BASE_VERS} != "5.3.25"
 PKG_FAIL_REASON+=	"The suhosin patch is currently not available for"
 PKG_FAIL_REASON+=	"this version of PHP.  You may have to wait until"
 PKG_FAIL_REASON+=	"an updated patch is released or temporarily"
