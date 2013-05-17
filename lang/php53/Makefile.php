ec1b38885e8b01f641990bd18ed085729e2baf76
spz <spz@pkgsrc.org>
2013-05-17 07:26:48 +0000

    Pullup ticket #4132 - requested by taca lang/php53: build fix
    Revisions pulled up:
    - lang/php53/Makefile.php                                       1.32
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Thu May  9 14:31:48 UTC 2013
    
       Modified Files:
       	pkgsrc/lang/php53: Makefile.php
    
       Log Message:
       Fix wrong condition check; make's variable should be referenced as variable.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.31 -r1.32 pkgsrc/lang/php53/Makefile.php

--- lang/php53/Makefile.php
+++ lang/php53/Makefile.php
@@ -1,4 +1,4 @@
-# $NetBSD: Makefile.php,v 1.29.2.1 2013/04/15 22:18:29 tron Exp $
+# $NetBSD: Makefile.php,v 1.29.2.2 2013/05/17 07:26:48 spz Exp $
 # used by lang/php53/Makefile
 # used by www/ap-php/Makefile
 
@@ -58,7 +58,7 @@ PKG_SUGGESTED_OPTIONS+=	inet6 ssl
 
 .if !empty(PKG_OPTIONS:Msuhosin)
 SUHOSIN_PHPVER=		5.3.23
-.  if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS}
+.  if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS} && ${PHP_BASE_VERS} != "5.3.23"
 PKG_FAIL_REASON+=	"The suhosin patch is currently not available for"
 PKG_FAIL_REASON+=	"this version of PHP.  You may have to wait until"
 PKG_FAIL_REASON+=	"an updated patch is released or temporarily"
