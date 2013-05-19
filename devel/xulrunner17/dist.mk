55452a55d55f80e63cefc57457ecc72eb1d60263
ryoon <ryoon@pkgsrc.org>
2013-05-19 12:49:42 +0000

    Update to 17.0.6
    Changelog:
    FIXED
    Security fixes can be found here
    
    Fixed in Firefox ESR 17.0.6
    MFSA 2013-48 Memory corruption found using Address Sanitizer
    MFSA 2013-47 Uninitialized functions in DOMSVGZoomEvent
    MFSA 2013-46 Use-after-free with video and onresize event
    MFSA 2013-44 Local privilege escalation through Mozilla Maintenance Service
    MFSA 2013-42 Privileged access for content level constructor
    MFSA 2013-41 Miscellaneous memory safety hazards (rv:21.0 / rv:17.0.6)

--- devel/xulrunner17/dist.mk
+++ devel/xulrunner17/dist.mk
@@ -1,11 +1,11 @@
-# $NetBSD: dist.mk,v 1.5 2013/04/09 19:07:03 ryoon Exp $
+# $NetBSD: dist.mk,v 1.6 2013/05/19 12:49:42 ryoon Exp $
 #
 # used by devel/xulrunner17/Makefile
 # used by www/firefox17/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	17.0.5
+MOZ_BRANCH=	17.0.6
 MOZ_BRANCH_MINOR=	esr
 MASTER_SITES=	${MASTER_SITE_MOZILLA_ESR:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
