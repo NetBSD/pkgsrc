06c54188e81425b897a86c86c30dbf1087953ad5
ryoon <ryoon@pkgsrc.org>
2013-05-19 08:50:24 +0000

    Update to 21.0
    * This release of firefox is built with internal xulrunner.
      Because separated (system) xulrunner has prefs and chrome load problem.
    * gnome option is broken in libnkmozgnomevfs.so build.
    
    Changelog:
    NEW
    The Social API now supports multiple providers
    NEW
    Enhanced three-state UI for Do Not Track (DNT)
    NEW
    Firefox will suggest how to improve your application startup time if needed
    NEW
    Preliminary implementation of Firefox Health Report
    CHANGED
    Ability to restore removed thumbnails on New Tab Page
    CHANGED
    CSS -moz-user-select:none selection changed to improve compatibility with -webkit-user-select:none (bug 816298)
    CHANGED
    Graphics related performance improvements (bug 809821)
    CHANGED
    Removed E4X support from Spidermonkey
    DEVELOPER
    Implemented Remote Profiling
    DEVELOPER
    Integrated add-on SDK loader and API libraries into Firefox
    HTML5
    Added support for <main> element
    HTML5
    Implemented scoped stylesheets
    HTML5
    Added support for window.crypto.getRandomValues
    FIXED
    Some function keys may not work when pressed (833719)
    FIXED
    Browsing and Download history clearing needs unification to avoid confusion on clearing download history (847627)
    FIXED
    21.0: Security fixes can be found here
    
    Fixed in Firefox 21
    MFSA 2013-48 Memory corruption found using Address Sanitizer
    MFSA 2013-47 Uninitialized functions in DOMSVGZoomEvent
    MFSA 2013-46 Use-after-free with video and onresize event
    MFSA 2013-45 Mozilla Updater fails to update some Windows Registry entries
    MFSA 2013-44 Local privilege escalation through Mozilla Maintenance Service
    MFSA 2013-43 File input control has access to full path
    MFSA 2013-42 Privileged access for content level constructor
    MFSA 2013-41 Miscellaneous memory safety hazards (rv:21.0 / rv:17.0.6)

--- www/firefox/options.mk
+++ www/firefox/options.mk
@@ -1,9 +1,52 @@
-# $NetBSD: options.mk,v 1.10 2011/07/11 13:17:40 tnn Exp $
+# $NetBSD: options.mk,v 1.11 2013/05/19 08:50:24 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.firefox
 PKG_SUPPORTED_OPTIONS=	official-mozilla-branding
 
+# XXX for xulrunner integrated firefox.
+PKG_SUPPORTED_OPTIONS+=	debug mozilla-jemalloc gnome pulseaudio
+PLIST_VARS+=		gnome jemalloc debug
+
+.if ${OPSYS} == "Linux" || ${OPSYS} == "SunOS"
+PKG_SUGGESTED_OPTIONS+=	mozilla-jemalloc
+.endif
+
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgnome)
+.include "../../devel/libgnomeui/buildlink3.mk"
+.include "../../sysutils/gnome-vfs/buildlink3.mk"
+.include "../../sysutils/libnotify/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-gnomevfs --enable-dbus --enable-gnomeui
+CONFIGURE_ARGS+=	--enable-libnotify
+CONFIGURE_ARGS+=	--enable-extensions
+PLIST.gnome=		yes
+.else
+CONFIGURE_ARGS+=	--disable-gnomevfs --disable-dbus --disable-gnomeui
+CONFIGURE_ARGS+=	--disable-libnotify
+.endif
+
+.if !empty(PKG_OPTIONS:Mmozilla-jemalloc)
+PLIST.jemalloc=		yes
+CONFIGURE_ARGS+=	--enable-jemalloc
+.else
+CONFIGURE_ARGS+=	--disable-jemalloc
+.endif
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug --enable-debug-symbols
+CONFIGURE_ARGS+=	--disable-install-strip
+PLIST.debug=		yes
+.else
+CONFIGURE_ARGS+=	--disable-debug --disable-debug-symbols
+CONFIGURE_ARGS+=	--enable-install-strip
+.endif
+
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+.include "../../audio/pulseaudio/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-pulseaudio
+.endif
+# XXX end
 
 PLIST_VARS+=		nobranding
 .if !empty(PKG_OPTIONS:Mofficial-mozilla-branding)
