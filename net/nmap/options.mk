036bc1b4f37bb6047e194d0af2357546bda31a09
adam <adam>
2013-05-20 06:21:22 +0000

    Nmap 6.25: o [NSE] Added CPE to smb-os-discovery output. o [Ncat] Fixed the printing of warning messages for large arguments to   the -i and -w options. o [Ncat] Shut down the write part of connected sockets in listen mode   when stdin hits EOF, just as was already done in connect mode. o [Zenmap] Removed a crashing error that could happen when canceling a   "Print to File" on Windows:   Traceback (most recent call last):     File "zenmapGUI\MainWindow.pyo", line 831, in _print_cb     File "zenmapGUI\Print.pyo", line 156, in run_print_operation   GError: Error from StartDoc o [NSE] Added new fingerprints for http-enum: Sitecore, Moodle, typo3,   SquirrelMail, RoundCube. o Added some new checks for failed library calls.

--- net/nmap/options.mk
+++ net/nmap/options.mk
@@ -1,11 +1,13 @@
-# $NetBSD: options.mk,v 1.8 2013/04/07 10:11:54 abs Exp $
+# $NetBSD: options.mk,v 1.9 2013/05/20 06:21:22 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nmap
 
-PKG_SUPPORTED_OPTIONS=	inet6 zenmap ndiff
+PKG_SUPPORTED_OPTIONS=	inet6 ndiff zenmap
 PKG_SUGGESTED_OPTIONS=	inet6
 
 .include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	ndiff zenmap
 
 .if !empty(PKG_OPTIONS:Minet6)
 CONFIGURE_ARGS+=	--enable-ipv6
@@ -13,17 +15,28 @@ CONFIGURE_ARGS+=	--enable-ipv6
 CONFIGURE_ARGS+=	--disable-ipv6
 .endif
 
-###
-### Enable dynamically loadable preprocessors, detection engine
-### and rules libraries.
-###
+.if !empty(PKG_OPTIONS:Mndiff)
+CONFIGURE_ARGS+=	--with-ndiff
+PLIST.ndiff=		yes
+SUBST_CLASSES+=		paths
+SUBST_STAGE.paths=	post-patch
+SUBST_FILES.paths=	ndiff/setup.py
+SUBST_SED.paths=	-e "s|@@PKGMANDIR@@|${PKGMANDIR}|g"
+SUBST_MESSAGE.paths=	Fixing paths.
+.include "../../lang/python/application.mk"
+.else
+CONFIGURE_ARGS+=		--without-ndiff
+.endif
+
+# Enable dynamically loadable preprocessors, detection engine
+# and rules libraries.
 .if !empty(PKG_OPTIONS:Mzenmap)
-CONFIGURE_ARGS+=		--with-zenmap
-PLIST_SRC+=			${PKGDIR}/PLIST.zenmap
-PY_PATCHPLIST=			yes
-REPLACE_PYTHON+=		zenmap/zenmapCore/*.py
-REPLACE_PYTHON+=		zenmap/zenmapGUI/*.py
-REPLACE_PYTHON+=		zenmap/zenmapGUI/higwidgets/*.py
+CONFIGURE_ARGS+=	--with-zenmap
+PLIST.zenmap=		yes
+PY_PATCHPLIST=		yes
+REPLACE_PYTHON+=	zenmap/zenmapCore/*.py
+REPLACE_PYTHON+=	zenmap/zenmapGUI/*.py
+REPLACE_PYTHON+=	zenmap/zenmapGUI/higwidgets/*.py
 .include "../../lang/python/application.mk"
 .include "../../lang/python/extension.mk"
 .include "../../x11/py-gtk2/buildlink3.mk"
@@ -32,17 +45,4 @@ REPLACE_PYTHON+=		zenmap/zenmapGUI/higwidgets/*.py
 .include "../../sysutils/desktop-file-utils/desktopdb.mk"
 .else
 CONFIGURE_ARGS+=	--without-zenmap
-.endif
-
-.if !empty(PKG_OPTIONS:Mndiff)
-CONFIGURE_ARGS+=		--with-ndiff
-PLIST_SRC+=			${PKGDIR}/PLIST.ndiff
-SUBST_CLASSES+=			paths
-SUBST_STAGE.paths=		post-patch
-SUBST_FILES.paths=		ndiff/setup.py
-SUBST_SED.paths=		-e "s|@@PKGMANDIR@@|${PKGMANDIR}|g"
-SUBST_MESSAGE.paths=		Fixing paths.
-.include "../../lang/python/application.mk"
-.else
-CONFIGURE_ARGS+=		--without-ndiff
 .endif
