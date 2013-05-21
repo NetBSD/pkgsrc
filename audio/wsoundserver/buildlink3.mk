5dc9adb6f8911dc23ba57ef3445620c2ee13306a
hans <hans>
2013-05-21 14:41:32 +0000

    Revbump for WindowMaker update.

--- audio/wsoundserver/buildlink3.mk
+++ audio/wsoundserver/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2013/02/16 11:19:57 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2013/05/21 14:41:32 hans Exp $
 
 BUILDLINK_TREE+=	wsoundserver
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	wsoundserver
 WSOUNDSERVER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.wsoundserver+=	wsoundserver>=0.4.0nb4
-BUILDLINK_ABI_DEPENDS.wsoundserver+=	wsoundserver>=0.4.0nb19
+BUILDLINK_ABI_DEPENDS.wsoundserver+=	wsoundserver>=0.4.0nb20
 BUILDLINK_PKGSRCDIR.wsoundserver?=	../../audio/wsoundserver
 
 .include "../../audio/libaudiofile/buildlink3.mk"
