e7885ee59d1437197541c035b1ad7ea222551ec1
joerg <joerg@pkgsrc.org>
2013-05-21 16:37:46 +0000

    Support C++11 STL implementations.

---
+++ security/HElib/patches/patch-NumbTh.h
@@ -0,0 +1,24 @@
+$NetBSD: patch-NumbTh.h,v 1.1 2013/05/21 16:37:46 joerg Exp $
+
+--- NumbTh.h.orig	2013-05-21 14:43:10.000000000 +0000
++++ NumbTh.h
+@@ -32,13 +32,18 @@
+ #include <NTL/mat_GF2E.h>
+ #include <NTL/lzz_pXFactoring.h>
+ #include <NTL/GF2XFactoring.h>
++#if __cplusplus >= 201103L || defined(_LIBCPP_VERSION)
++#include <unordered_map>
++#else
+ #include <tr1/unordered_map>
++using std::tr1::unordered_map;
++#endif
+ #include <string>
+ NTL_CLIENT
+ 
+ 
+ //! @typedef
+-typedef tr1::unordered_map<string, const char *> argmap_t;
++typedef unordered_map<string, const char *> argmap_t;
+ 
+ 
+ //! @brief Code for parsing command line arguments.
