6f00c7089bacedc2bfa53b855c93d26da2fee8fe
mspo <mspo>
2013-05-22 15:17:07 +0000

    add nodejs package, previously in wip/node

---
+++ lang/nodejs/options.mk
@@ -0,0 +1,27 @@
+# $NetBSD: options.mk,v 1.1 2013/05/22 15:17:07 mspo Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.node
+PKG_SUPPORTED_OPTIONS=	openssl dtrace
+PKG_SUGGESTED_OPTIONS=	openssl
+
+.if ${OPSYS} == "SunOS" && exists(/usr/sbin/dtrace)
+PKG_SUGGESTED_OPTIONS+=	dtrace
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mdtrace)
+CONFIGURE_ARGS+=	--with-dtrace
+.else
+CONFIGURE_ARGS+=	--without-dtrace
+.endif
+
+.if !empty(PKG_OPTIONS:Mopenssl)
+.include "../../security/openssl/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-openssl
+.endif
+
+.if empty(PKG_OPTIONS:Msnapshot)
+CONFIGURE_ARGS+=	--without-snapshot
+.endif
