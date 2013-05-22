004e9760c14b7f42b15cf70ac4bec2b300974681
obache <obache@pkgsrc.org>
2013-05-22 13:14:29 +0000

    Update cabocha to 0.66.
    ChaSen is not supported anymore, using MeCab.
    and use CRF++ instead of YamCha.

--- textproc/cabocha/buildlink3.mk
+++ textproc/cabocha/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/05/19 12:16:25 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/05/22 13:14:29 obache Exp $
 
 BUILDLINK_TREE+=	cabocha
 
 .if !defined(CABOCHA_BUILDLINK3_MK)
 CABOCHA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cabocha+=	cabocha>=0.53
+BUILDLINK_API_DEPENDS.cabocha+=	cabocha>=0.66
 BUILDLINK_PKGSRCDIR.cabocha?=	../../textproc/cabocha
 
-.include "../../textproc/yamcha/buildlink3.mk"
+.include "../../textproc/CRF++/buildlink3.mk"
+.include "../../textproc/mecab/buildlink3.mk"
 .endif	# CABOCHA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-cabocha
