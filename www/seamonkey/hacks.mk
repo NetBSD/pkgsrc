d883cff186e6b7dea17f47e9cf10102c4597b851
ryoon <ryoon>
2013-05-23 13:25:30 +0000

    Update to 18.0beta4
    * Use common files for www/firefox.
    
    SeaMonkey-specific changes
        Basic Private Browsing support has been added (experimental for now).
        Added support for safe browsing which blocks potentially malicious websites reported as attack sites (malware) or web forgeries (phishing).
        Information like preview text, subject and sender can be shown in new mail notifications now.
        See the changes page for minor changes.
    
    Mozilla platform changes
        CSS -moz-user-select:none selection has been changed to improve compatibility with -webkit-user-select:none (bug 816298).
        Applied graphics-related performance improvements (bug 809821).
        Removed E4X support from SpiderMonkey.
        Added support for the <main> element.
        Implemented scoped stylesheets.
        Fixed some function keys not working when pressed (bug 833719).
        Fixed several stability issues.

--- www/seamonkey/hacks.mk
+++ www/seamonkey/hacks.mk
@@ -1,6 +1,6 @@
-# $NetBSD: hacks.mk,v 1.4 2012/06/14 11:27:34 ryoon Exp $
+# $NetBSD: hacks.mk,v 1.5 2013/05/23 13:25:30 ryoon Exp $
 
-# workround for link of thunderbird-bin etc.
-LDFLAGS+=	-Wl,-rpath,${PREFIX}/lib/seamonkey
+# workround for link of seamonkey-bin etc.
+LDFLAGS+=	${COMPILER_RPATH_FLAG}${PREFIX}/lib/seamonkey
 
-.include "../../devel/xulrunner/hacks.mk"
+.include "../../www/firefox/hacks.mk"
