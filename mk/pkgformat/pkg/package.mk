5f5e2d61adf55a78853a0175eb3d6af8054be637
obache <obache@pkgsrc.org>
2013-05-23 13:18:56 +0000

    prevent to generate binary package in ${PACKAGES} from the side effect of `install' with USE_DESTDIR=yes. This changes prevent to unwanted overwite of existing binary packages with test installation (`stage-install', `replace' & `undo-replace', and so on).
    To do both `install' and `package', you can still use `package-install' target,
    same as USE_DESTDIR=no.

--- mk/pkgformat/pkg/package.mk
+++ mk/pkgformat/pkg/package.mk
@@ -1,4 +1,4 @@
-# $NetBSD: package.mk,v 1.1 2011/10/15 00:23:09 reed Exp $
+# $NetBSD: package.mk,v 1.2 2013/05/23 13:18:56 obache Exp $
 
 .if defined(PKG_SUFX)
 WARNINGS+=		"PKG_SUFX is deprecated, please use PKG_COMPRESSION"
@@ -13,6 +13,11 @@ WARNINGS+=		"Unsupported value for PKG_SUFX"
 PKG_SUFX?=		.tgz
 FILEBASE?=		${PKGBASE}
 PKGFILE?=		${PKGREPOSITORY}/${FILEBASE}-${PKGVERSION}${PKG_SUFX}
+.if ${_USE_DESTDIR} == "no"
+STAGE_PKGFILE?=		${PKGFILE}
+.else
+STAGE_PKGFILE?=		${WRKDIR}/.packages/${FILEBASE}-${PKGVERSION}${PKG_SUFX}
+.endif
 PKGREPOSITORY?=		${PACKAGES}/${PKGREPOSITORYSUBDIR}
 PKGREPOSITORYSUBDIR?=	All
 
@@ -35,6 +40,18 @@ package-check-installed:
 .PHONY: package-create
 package-create: package-remove ${PKGFILE} package-links
 
+######################################################################
+### stage-package-create (PRIVATE, pkgsrc/mk/package/package.mk)
+######################################################################
+### stage-package-create creates the binary package for stage install.
+###
+.PHONY: stage-package-create
+.if ${_USE_DESTDIR} == "no"
+stage-package-create:	package-create
+.else
+stage-package-create:	stage-install stage-package-remove ${STAGE_PKGFILE}
+.endif
+
 _PKG_ARGS_PACKAGE+=	${_PKG_CREATE_ARGS}
 _PKG_ARGS_PACKAGE+=	-F ${PKG_COMPRESSION}
 .if ${_USE_DESTDIR} == "no"
@@ -49,7 +66,7 @@ _PKG_ARGS_PACKAGE+=	-u ${REAL_ROOT_USER} -g ${REAL_ROOT_GROUP}
 _PKG_ARGS_PACKAGE+=	-E
 .endif
 
-${PKGFILE}: ${_CONTENTS_TARGETS}
+${STAGE_PKGFILE}: ${_CONTENTS_TARGETS}
 	${RUN} ${MKDIR} ${.TARGET:H}
 	@${STEP_MSG} "Creating binary package ${.TARGET}"
 	${RUN} tmpname=${.TARGET:S,${PKG_SUFX}$,.tmp${PKG_SUFX},};	\
@@ -58,6 +75,14 @@ ${PKGFILE}: ${_CONTENTS_TARGETS}
 	else								\
 		exitcode=$$?; ${RM} -f "$$tmpname"; exit $$exitcode;	\
 	fi
+
+.if ${_USE_DESTDIR} != "no"
+${PKGFILE}: ${STAGE_PKGFILE}
+	${RUN} ${MKDIR} ${.TARGET:H}
+	@${STEP_MSG} "Creating binary package ${.TARGET}"
+	${LN} -f ${STAGE_PKGFILE} ${PKGFILE} 2>/dev/null || \
+		${CP} -pf ${STAGE_PKGFILE} ${PKGFILE}
+.endif
 
 ######################################################################
 ### package-remove (PRIVATE)
@@ -68,6 +93,15 @@ ${PKGFILE}: ${_CONTENTS_TARGETS}
 .PHONY: package-remove
 package-remove:
 	${RUN} ${RM} -f ${PKGFILE}
+
+######################################################################
+### stage-package-remove (PRIVATE)
+######################################################################
+### stage-package-remove removes the binary package for stage install
+###
+.PHONY: stage-package-remove
+stage-package-remove:
+	${RUN} ${RM} -f ${STAGE_PKGFILE}
 
 ######################################################################
 ### package-links (PRIVATE)
@@ -132,6 +166,13 @@ package-install: package real-package-install
 package-install: barrier
 .endif
 
+.PHONY: stage-package-install
+.if defined(_PKGSRC_BARRIER)
+stage-package-install: stage-package-create real-package-install
+.else
+stage-package-install: barrier
+.endif
+
 .if ${_USE_DESTDIR} != "no"
 .  if !empty(USE_CROSS_COMPILE:M[yY][eE][sS])
 real-package-install: su-real-package-install
@@ -147,13 +188,13 @@ su-real-package-install:
 	@${PHASE_MSG} "Install binary package of "${PKGNAME:Q}
 .if !empty(USE_CROSS_COMPILE:M[yY][eE][sS])
 	@${MKDIR} ${_CROSS_DESTDIR}${PREFIX}
-	${PKG_ADD} -m ${MACHINE_ARCH} -I -p ${_CROSS_DESTDIR}${PREFIX} ${PKGFILE}
+	${PKG_ADD} -m ${MACHINE_ARCH} -I -p ${_CROSS_DESTDIR}${PREFIX} ${STAGE_PKGFILE}
 	@${ECHO} "Fixing recorded cwd..."
 	@${SED} -e 's|@cwd ${_CROSS_DESTDIR}|@cwd |' ${_PKG_DBDIR}/${PKGNAME:Q}/+CONTENTS > ${_PKG_DBDIR}/${PKGNAME:Q}/+CONTENTS.tmp
 	@${MV} ${_PKG_DBDIR}/${PKGNAME:Q}/+CONTENTS.tmp ${_PKG_DBDIR}/${PKGNAME:Q}/+CONTENTS
 .else
 	${RUN} case ${_AUTOMATIC:Q}"" in					\
-	[yY][eE][sS])	${PKG_ADD} -A ${PKGFILE} ;;			\
-	*)		${PKG_ADD} ${PKGFILE} ;;			\
+	[yY][eE][sS])	${PKG_ADD} -A ${STAGE_PKGFILE} ;;		\
+	*)		${PKG_ADD} ${STAGE_PKGFILE} ;;			\
 	esac
 .endif
