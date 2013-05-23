d6869a8a93a6b0ad250692de4ab00c6309c885b8
ryoon <ryoon@pkgsrc.org>
2013-05-23 13:12:13 +0000

    Bump PKGREVISION.
    * Remove reference to devel/xulrunner.
    * Move some common files for firefox/xulrunner-21.0.
    * Move patches from devel/sulrunner.
    * Take MAINTAINERship.

---
+++ www/firefox/patches/patch-media_webrtc_trunk_webrtc_typedefs.h
@@ -0,0 +1,13 @@
+$NetBSD: patch-media_webrtc_trunk_webrtc_typedefs.h,v 1.1 2013/05/23 13:12:13 ryoon Exp $
+
+--- media/webrtc/trunk/webrtc/typedefs.h.orig	2013-05-11 19:19:46.000000000 +0000
++++ media/webrtc/trunk/webrtc/typedefs.h
+@@ -21,7 +21,7 @@
+ // For access to standard POSIXish features, use WEBRTC_POSIX instead of a
+ // more specific macro.
+ #if defined(WEBRTC_MAC) || defined(WEBRTC_LINUX) || \
+-    defined(WEBRTC_ANDROID)
++    defined(WEBRTC_ANDROID) || defined(WEBRTC_BSD)
+ #define WEBRTC_POSIX
+ #endif
+ 
