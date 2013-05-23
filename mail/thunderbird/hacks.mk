df1248d0be7af314b43045e4194ac907fe0db764
ryoon <ryoon@pkgsrc.org>
2013-05-23 13:14:28 +0000

    Fix hack.mk include.

--- mail/thunderbird/hacks.mk
+++ mail/thunderbird/hacks.mk
@@ -1,6 +1,6 @@
-# $NetBSD: hacks.mk,v 1.3 2013/03/30 23:31:36 tron Exp $
+# $NetBSD: hacks.mk,v 1.4 2013/05/23 13:14:28 ryoon Exp $
 
 # workround for link of thunderbird-bin etc.
 LDFLAGS+=	-Wl,-R${PREFIX}/lib/thunderbird
 
-.include "../../devel/xulrunner/hacks.mk"
+.include "../../devel/xulrunner17/hacks.mk"
