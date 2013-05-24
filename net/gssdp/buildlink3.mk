7beb3ce74bb54e46bb85d926109fa85376fad7f4
wiz <wiz@pkgsrc.org>
2013-05-24 18:23:32 +0000

    Bump PKGREVISION for libproxy shlib major bump.

--- net/gssdp/buildlink3.mk
+++ net/gssdp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2013/05/09 07:39:16 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2013/05/24 18:23:33 wiz Exp $
 
 BUILDLINK_TREE+=	gssdp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gssdp
 GSSDP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gssdp+=	gssdp>=0.12.2.1
-BUILDLINK_ABI_DEPENDS.gssdp+=	gssdp>=0.14.2nb1
+BUILDLINK_ABI_DEPENDS.gssdp+=	gssdp>=0.14.2nb3
 BUILDLINK_PKGSRCDIR.gssdp?=	../../net/gssdp
 
 .include "../../devel/glib2/buildlink3.mk"
--- net/libnice/buildlink3.mk
+++ net/libnice/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2013/01/26 21:36:41 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2013/05/24 18:23:34 wiz Exp $
 
 BUILDLINK_TREE+=	libnice
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libnice
 LIBNICE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libnice+=	libnice>=0.0.9
-BUILDLINK_ABI_DEPENDS.libnice+=	libnice>=0.1.2nb5
+BUILDLINK_ABI_DEPENDS.libnice+=	libnice>=0.1.2nb7
 BUILDLINK_PKGSRCDIR.libnice?=	../../net/libnice
 
 .include "../../devel/glib2/buildlink3.mk"
