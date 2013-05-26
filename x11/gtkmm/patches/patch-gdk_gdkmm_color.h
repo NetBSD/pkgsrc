281c8809cd983335d50f5439a065ccd6ff16685c
adam <adam>
2013-05-26 09:14:57 +0000

    Fix missing dependencies in various headers (part two). Bump revision.

--- x11/gtkmm/patches/patch-gdk_gdkmm_color.h
+++ x11/gtkmm/patches/patch-gdk_gdkmm_color.h
@@ -1,4 +1,6 @@
-$NetBSD: patch-gdk_gdkmm_color.h,v 1.1 2013/05/24 19:31:59 joerg Exp $
+$NetBSD: patch-gdk_gdkmm_color.h,v 1.2 2013/05/26 09:14:57 adam Exp $
+
+Fix missing dependencies.
 
 --- gdk/gdkmm/color.h.orig	2013-05-24 17:18:47.000000000 +0000
 +++ gdk/gdkmm/color.h
