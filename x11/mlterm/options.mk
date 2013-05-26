3550774f0dac7a788077ba0b9d34feba26e8cef4
tsutsui <tsutsui@pkgsrc.org>
2013-05-26 18:18:25 +0000

    Update mlterm to 3.2.0.
    In this version, mlterm now supports NetBSD wsfb framebuffer
    so NetBSD users can use a multi-lingual terminal without
    Xserver on ports that support wsdisplay framebuffer console,
    i.e. NetBSD/i386 with VESA framebuffer, NetBSD/dreamcast,
    or even NetBSD/luna68k with 1bpp framebuffer.
    
    Special thanks to Araki Ken, an auther of mlterm, for
    various efforts and improvements for NetBSD wsfb support,
    including dumb 1bpp optimizations on luna68k.
    mlterm-fb demonstration movies are available on YouTube:
     http://youtu.be/5IH1NYSVpHY  (NetBSD/i386 6.1)
     http://youtu.be/BP8AIceWgxA  (NetBSD/luna68k 6.1)
    
    
    pkgsrc changes:
    
     - update descriptions per the framebuffer support
     - add a MESSAGE file to mention miscellaneous configurations to use mlterm
     - remove MAKE_JOBS_SAFE=no which has been fixed in upstream Makefiles
     - split SUBST rules into ones for Makefiles, man page, and config files
     - install misc README files in doc dirs which are useful after installation
     - add "mlterm-fb" and "ibus" options and make mlterm-fb default
       on supported platforms
     - update PLIST per above changes (XXX tested only on NetBSD)
    
    
    Upstream Changes per doc/en/ReleaseNote:
    
    ver 3.2.0
    * Support framebuffer on NetBSD.
    * Support 1 bpp framebuffer.
    * Support sixel graphics and wall pictures on 1 and 8 bpp framebuffer.
    (See http://www.youtube.com/watch?v=djbEw0G_LMI and http://www.youtube.com/watch?v=5IH1NYSVpHY)
    (Thanks to @tsutsuii san)
    * Add "use_urgent_bell" option which is equivalent to "bellIsUrgent" option of
      xterm and support CSI ? 1042.
    * "only_use_unicode_font" and "not_use_unicode_font" options have an effect
      on preedit and candidate characters of input method plugins.
    * Add "sound|visual" to the value of "bel_mode" option.
    * Enable to change "not_use_unicode_font" and "only_use_unicode_font" dynamically.
    * Add "box_drawing_font" option.
    * Support efont-unicode fonts for framebuffer.
    * Bug fixes:
      Fix the bug which disabled some shortcut keys in FreeBSD/framebuffer.
      Fix the bug which ignored the specified encoding of "default_server" option
      in opening a new pty.
      Fix the bug which disabled sixel graphic images in framebuffer 16bpp.
      Fix the bug which disabled inline pictures if use_dynamic_comb option is true.
      Fix the bug of iBus input method plugin which disabled to erase the last
      preedited character even if backspace key was pressed.
      Fix the wrong height of the preediting cursor of the input method plugin
      if the value of "inner_border" option isn't 2. (Thanks to @tsutsuii san)
      Fix "use_dynamic_comb" option which didn't work anymore.
      Fix the bug which disabled "UNUSED" key in ~/.mlterm/key.
      Change key seuqences for modifier keys + cursor keys in application cursor key
      mode. (e.g. Ctrl+Up: "\x1bO1;5A" -> "\x1b[1;5A") (Thanks to Mohammad Alsaleh)

--- x11/mlterm/options.mk
+++ x11/mlterm/options.mk
@@ -1,12 +1,28 @@
-# $NetBSD: options.mk,v 1.6 2012/05/20 13:59:30 obache Exp $
+# $NetBSD: options.mk,v 1.7 2013/05/26 18:18:25 tsutsui Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mlterm
-PKG_SUPPORTED_OPTIONS=	libind m17nlib uim xft2
+PKG_SUPPORTED_OPTIONS=	ibus libind m17nlib mlterm-fb uim xft2
 PKG_SUGGESTED_OPTIONS=
+.if ${OPSYS} == "NetBSD" || ${OPSYS} == "FreeBSD" || ${OPSYS} == "Linux"
+PKG_SUGGESTED_OPTIONS+=	mlterm-fb
+.endif
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		ind m17nlib uim xft2
+PLIST_VARS+=		fb ibus ind m17nlib uim xft2
+
+.if !empty(PKG_OPTIONS:Mmlterm-fb)
+CONFIGURE_ARGS+=	--with-gui=xlib,fb
+PLIST.fb=		yes
+SPECIAL_PERMS+=		${PREFIX:Q}/bin/mlterm-fb ${SETUID_ROOT_PERMS}
+CONF_FILES+=		${EGDIR}/font-fb ${PKG_SYSCONFDIR}/font-fb
+.endif
+
+.if !empty(PKG_OPTIONS:Mibus)
+.include "../../inputmethod/ibus/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-ibus
+PLIST.ibus=		yes
+.endif
 
 .if !empty(PKG_OPTIONS:Mlibind)
 CONFIGURE_ARGS+=	--enable-ind
