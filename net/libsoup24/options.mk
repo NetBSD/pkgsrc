bec85ccda64047c46b867295151ce52e461a4633
drochner <drochner>
2013-05-27 11:57:19 +0000

    remove stale dependencies, ride on recent revbump

--- net/libsoup24/options.mk
+++ net/libsoup24/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2010/06/14 17:58:44 drochner Exp $
+# $NetBSD: options.mk,v 1.3 2013/05/27 11:57:19 drochner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libsoup24
 PKG_SUPPORTED_OPTIONS=	gnome
@@ -9,7 +9,6 @@ PKG_SUGGESTED_OPTIONS=	gnome
 PLIST_VARS+= gnome
 
 .if !empty(PKG_OPTIONS:Mgnome)
-.include "../../devel/GConf/buildlink3.mk"
 .include "../../security/libgnome-keyring/buildlink3.mk"
 PKGCONFIG_OVERRIDE+=	libsoup-gnome-2.4.pc.in
 PLIST.gnome=	yes
