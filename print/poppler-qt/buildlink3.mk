c5b6a36aaf34a18a6e516cbfc546f2e46036f1d9
wiz <wiz@pkgsrc.org>
2013-05-29 22:13:16 +0000

    Recursive revbump because poppler updated its shlib major for a x.y.z+1 change, again...
    Noted by tron.

--- print/poppler-qt/buildlink3.mk
+++ print/poppler-qt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2013/02/16 11:19:57 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2013/05/29 22:13:16 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-qt
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-qt
 POPPLER_QT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-qt+=	poppler-qt>=0.4.5nb1
-BUILDLINK_ABI_DEPENDS.poppler-qt+=	poppler-qt>=0.16.7nb3
+BUILDLINK_ABI_DEPENDS.poppler-qt+=	poppler-qt>=0.16.7nb4
 BUILDLINK_PKGSRCDIR.poppler-qt?=	../../print/poppler-qt
 
 .include "../../print/poppler016/buildlink3.mk"
