5c59666e0bdab01b81bb08c550245209da3a07ac
adam <adam@pkgsrc.org>
2013-05-31 20:15:04 +0000

    Changes 8.5.14: Bug-fix release.

--- x11/tk/options.mk
+++ x11/tk/options.mk
@@ -1,10 +1,15 @@
-# $NetBSD: options.mk,v 1.2 2012/12/12 15:30:52 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2013/05/31 20:15:32 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tk
 PKG_SUPPORTED_OPTIONS=	threads debug xft2
-PKG_SUGGESTED_OPTIONS=	threads xft2
+PKG_SUGGESTED_OPTIONS=	threads
 
 .include "../../mk/bsd.prefs.mk"
+
+.if ${OPSYS} != "Darwin"
+PKG_SUGGESTED_OPTIONS+=	xft2
+.endif
+
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdebug)
