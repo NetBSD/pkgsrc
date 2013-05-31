9f02c2e97a0e1073581b1cbab92a35a0ee1b6780
adam <adam@pkgsrc.org>
2013-05-31 10:55:15 +0000

    We are excited to announce perl v5.18.0, the first stable release of version 18 of Perl 5.
    You can find a full list of changes in the file "perldelta.pod" located in
    the "pod" directory inside the release and on the web.
    
    Perl v5.18.0 represents approximately 12 months of development since Perl
    v5.16.0 and contains approximately 400,000 lines of changes across 2,100
    files from 113 authors.

--- lang/perl5/buildlink3.mk
+++ lang/perl5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.51 2012/10/03 21:23:25 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.52 2013/05/31 10:55:15 adam Exp $
 
 BUILDLINK_TREE+=	perl
 
@@ -6,13 +6,13 @@ BUILDLINK_TREE+=	perl
 PERL_BUILDLINK3_MK:=
 
 .include "../../mk/bsd.fast.prefs.mk"
-PERL5_REQD+=			5.16.0
+PERL5_REQD+=			5.18.0
 .for _perl5_ver_ in ${PERL5_REQD}
 BUILDLINK_API_DEPENDS.perl+=	perl>=${_perl5_ver_}
 .endfor
-# Prevent p5-* etc. packages built for 5.16.0 to be used
+# Prevent p5-* etc. packages built for 5.18.0 to be used
 # with the next incompatible version of perl:
-BUILDLINK_API_DEPENDS.perl+=	perl<5.18.0
+BUILDLINK_API_DEPENDS.perl+=	perl<5.20.0
 
 BUILDLINK_PKGSRCDIR.perl?=	../../lang/perl5
 
