0543c3d7dc559a7de52b48832b0c6fdbde8b4919
adam <adam@pkgsrc.org>
2013-05-31 10:55:15 +0000

    We are excited to announce perl v5.18.0, the first stable release of version 18 of Perl 5.
    You can find a full list of changes in the file "perldelta.pod" located in
    the "pod" directory inside the release and on the web.
    
    Perl v5.18.0 represents approximately 12 months of development since Perl
    v5.16.0 and contains approximately 400,000 lines of changes across 2,100
    files from 113 authors.

--- lang/perl5/patches/patch-hv.c
+++ lang/perl5/patches/patch-hv.c
@@ -1,37 +1,21 @@
-$NetBSD: patch-hv.c,v 1.1 2013/01/17 13:29:59 manu Exp $
+$NetBSD: patch-hv.c,v 1.2 2013/05/31 10:55:15 adam Exp $
 
 Work around a crash with apaache-1.3.42 and mod_perl-1.31:
 
-Program received signal SIGSEGV, Segmentation fault.
-[Switching to LWP 1]
-Perl_hv_undef_flags (my_perl=0xbb537800, hv=0x0, flags=0) at hv.c:1788
-1788        const bool save = !!SvREFCNT(hv);
-(gdb) bt
-#0  Perl_hv_undef_flags (my_perl=0xbb537800, hv=0x0, flags=0) at hv.c:1788
-#1  0xbb646f8e in perl_util_cleanup () from /usr/pkg/lib/httpd/mod_perl.so
-#2  0xbb639b10 in perl_shutdown () from /usr/pkg/lib/httpd/mod_perl.so
-#3  0xbb639ec3 in mp_dso_unload () from /usr/pkg/lib/httpd/mod_perl.so
-#4  0x08073524 in ap_clear_pool ()
-#5  0x08081956 in main ()
-
-
---- hv.c.orig	2013-01-17 08:54:13.000000000 +0100
-+++ hv.c	2013-01-17 08:55:42.000000000 +0100
-@@ -1784,14 +1784,16 @@
- {
+--- hv.c.orig	2013-05-09 13:47:39.000000000 +0000
++++ hv.c
+@@ -1728,12 +1728,13 @@ Perl_hv_undef_flags(pTHX_ HV *hv, U32 fl
      dVAR;
-     register XPVHV* xhv;
+     XPVHV* xhv;
      const char *name;
 -    const bool save = !!SvREFCNT(hv);
 +    bool save;
  
      if (!hv)
  	return;
-+
      DEBUG_A(Perl_hv_assert(aTHX_ hv));
      xhv = (XPVHV*)SvANY(hv);
 +    save = !!SvREFCNT(hv);
  
      /* The name must be deleted before the call to hfreeeeentries so that
         CVs are anonymised properly. But the effective name must be pre-
-        served until after that call (and only deleted afterwards if the
