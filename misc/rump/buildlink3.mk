09c52cc05d63f05906f3a2362d172c06038b7d12
stacktic <stacktic@pkgsrc.org>
2013-06-01 15:42:03 +0000

    Update misc/rump to 20130601 - non-NetBSD sockin compatibility

--- misc/rump/buildlink3.mk
+++ misc/rump/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/05/01 17:16:06 stacktic Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/06/01 15:42:03 stacktic Exp $
 
 BUILDLINK_TREE+=	rump
 
 .if !defined(RUMP_BUILDLINK3_MK)
 RUMP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rump+=	rump>=20130430
+BUILDLINK_API_DEPENDS.rump+=	rump>=20130601
 BUILDLINK_PKGSRCDIR.rump?=	../../misc/rump
 .endif	# RUMP_BUILDLINK3_MK
 
