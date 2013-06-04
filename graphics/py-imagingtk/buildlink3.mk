023dd50fda20816b360ef48d9446e3178b668e95
wiz <wiz>
2013-06-04 07:45:01 +0000

    Replace py-Tk/buildlink3.mk with DEPENDS.
    There are no buildlink3-relevant files here, just a loadable module.

--- graphics/py-imagingtk/buildlink3.mk
+++ graphics/py-imagingtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/06/03 10:05:19 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/06/04 07:45:01 wiz Exp $
 
 BUILDLINK_TREE+=	py-imagingtk
 
@@ -12,7 +12,6 @@ BUILDLINK_ABI_DEPENDS.py-imagingtk+=	${PYPKGPREFIX}-imagingtk>=1.1.7nb4
 BUILDLINK_PKGSRCDIR.py-imagingtk?=	../../graphics/py-imagingtk
 
 .include "../../graphics/py-imaging/buildlink3.mk"
-.include "../../x11/py-Tk/buildlink3.mk"
 .endif	# PY_IMAGINGTK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-py-imagingtk
