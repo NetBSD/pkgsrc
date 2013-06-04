39fb73e0c41f9acf33fbb80a76db8248a4f78561
fhajny <fhajny@pkgsrc.org>
2013-06-04 21:17:17 +0000

    Use stdint.h instead of cstdint in patches to prevent c++0x/c++11 being dragged in as requirement.

--- devel/libmemcached/patches/patch-tests_libmemcached-1.0_pool.cc
+++ devel/libmemcached/patches/patch-tests_libmemcached-1.0_pool.cc
@@ -1,4 +1,4 @@
-$NetBSD: patch-tests_libmemcached-1.0_pool.cc,v 1.1 2013/05/09 13:50:54 joerg Exp $
+$NetBSD: patch-tests_libmemcached-1.0_pool.cc,v 1.2 2013/06/04 21:17:17 fhajny Exp $
 
 --- tests/libmemcached-1.0/pool.cc.orig	2013-05-05 20:36:30.000000000 +0000
 +++ tests/libmemcached-1.0/pool.cc
@@ -15,7 +15,7 @@ $NetBSD: patch-tests_libmemcached-1.0_pool.cc,v 1.1 2013/05/09 13:50:54 joerg Ex
  #include <iostream>
  #include <string>
  #include <cerrno>
-+#include <cstdint>
++#include <stdint.h>
  
  #include <semaphore.h>
  
