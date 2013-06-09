761218110924bcb955596025780015be2485b872
ryoon <ryoon@pkgsrc.org>
2013-06-09 06:30:14 +0000

    Update to 0.12.1
    * Fix MASTER_SITES to standard way.
    * Strip end of line CR for patching.
    
    Changlog:
    unknown

--- games/xye/patches/patch-src_gen.cpp
+++ games/xye/patches/patch-src_gen.cpp
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_gen.cpp,v 1.1 2013/04/29 21:31:11 joerg Exp $
+$NetBSD: patch-src_gen.cpp,v 1.2 2013/06/09 06:30:14 ryoon Exp $
 
---- src/gen.cpp.orig	2013-04-29 18:55:25.000000000 +0000
+--- src/gen.cpp.orig	2013-06-09 06:21:13.000000000 +0000
 +++ src/gen.cpp
-@@ -19,6 +19,7 @@ Permission is granted to anyone to use t
- #include "gen.h"
- #include "vxsdl.h"
+@@ -21,6 +21,7 @@ Permission is granted to anyone to use t
  #include<cstdio>
+ #include<iostream>
+ #include<fstream>
 +#include<ctime>
  
  const float F_RAND_MAX = (float)(RAND_MAX);
