540cdd5c6e7b05324cba56368b166ce2b7647332
taca <taca@pkgsrc.org>
2013-06-09 22:23:24 +0000

    Fix suhosi patch part. Thanks to Volkmar Seifert notified me the problem via private e-mail.
    (I should modify my local mk.conf to handle better...)

--- lang/php53/Makefile.php
+++ lang/php53/Makefile.php
@@ -1,4 +1,4 @@
-# $NetBSD: Makefile.php,v 1.33 2013/05/16 16:19:14 taca Exp $
+# $NetBSD: Makefile.php,v 1.34 2013/06/09 22:23:24 taca Exp $
 # used by lang/php53/Makefile
 # used by www/ap-php/Makefile
 
@@ -58,7 +58,7 @@ PKG_SUGGESTED_OPTIONS+=	inet6 ssl
 
 .if !empty(PKG_OPTIONS:Msuhosin)
 SUHOSIN_PHPVER=		5.3.25
-.  if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS} && ${PHP_BASE_VERS} != "5.3.25"
+.  if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS} && ${PHP_BASE_VERS} != "5.3.26"
 PKG_FAIL_REASON+=	"The suhosin patch is currently not available for"
 PKG_FAIL_REASON+=	"this version of PHP.  You may have to wait until"
 PKG_FAIL_REASON+=	"an updated patch is released or temporarily"
