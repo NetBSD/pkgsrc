4d9f3ceba34f55ff31ab692bc9e0bab7d433fef6
obache <obache@pkgsrc.org>
2013-06-10 11:59:38 +0000

    Import dbus-sharp-0.7.0 as sysutils/dbus-sharp. based on pkgsrc-wip/dbus-sharp.
    This is a fork of ndesk-dbus, which is a C# implementation of D-Bus. It's often
    referred to as "managed D-Bus" to avoid confusion with existing bindings (which
    wrap libdbus).

---
+++ sysutils/dbus-sharp/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/06/10 11:59:38 obache Exp $
+
+BUILDLINK_TREE+=	dbus-sharp
+
+.if !defined(DBUS_SHARP_BUILDLINK3_MK)
+DBUS_SHARP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.dbus-sharp+=	dbus-sharp>=0.7.0
+BUILDLINK_PKGSRCDIR.dbus-sharp?=	../../sysutils/dbus-sharp
+
+.include "../../lang/mono/buildlink3.mk"
+.endif	# DBUS_SHARP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-dbus-sharp
