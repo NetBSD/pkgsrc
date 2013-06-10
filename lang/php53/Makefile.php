d545849c3b6c53331cdd038c8825089089c2bdbb
tron <tron>
2013-06-10 13:12:29 +0000

    Pullup ticket #4158 - requested by taca lang/php53: fix build with "suhosi" option
    Revisions pulled up:
    - lang/php53/Makefile.php                                       1.34
    - lang/php53/distinfo                                           1.65
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sun Jun  9 22:23:24 UTC 2013
    
       Modified Files:
       	pkgsrc/lang/php53: Makefile.php distinfo
    
       Log Message:
       Fix suhosi patch part.
       Thanks to Volkmar Seifert notified me the problem via private e-mail.
    
       (I should modify my local mk.conf to handle better...)

--- lang/php53/Makefile.php
+++ lang/php53/Makefile.php
@@ -1,4 +1,4 @@
-# $NetBSD: Makefile.php,v 1.29.2.3 2013/06/10 10:39:28 tron Exp $
+# $NetBSD: Makefile.php,v 1.29.2.4 2013/06/10 13:12:29 tron Exp $
 # used by lang/php53/Makefile
 # used by www/ap-php/Makefile
 
@@ -58,7 +58,7 @@ PKG_SUGGESTED_OPTIONS+=	inet6 ssl
 
 .if !empty(PKG_OPTIONS:Msuhosin)
 SUHOSIN_PHPVER=		5.3.25
-.  if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS} && ${PHP_BASE_VERS} != "5.3.25"
+.  if ${SUHOSIN_PHPVER} != ${PHP_BASE_VERS} && ${PHP_BASE_VERS} != "5.3.26"
 PKG_FAIL_REASON+=	"The suhosin patch is currently not available for"
 PKG_FAIL_REASON+=	"this version of PHP.  You may have to wait until"
 PKG_FAIL_REASON+=	"an updated patch is released or temporarily"
