f6570d4b0ad5b861e6c003daa2391d01102457e3
obache <obache@pkgsrc.org>
2013-06-10 12:03:52 +0000

    Import dbus-sharp-glib-0.5.0 as sysutils/dbus-sharp-glib. based on pkgsrc-wip/dbus-sharp-glib.
    This is a fork of ndesk-dbus-glib, which provides GLib main loop integration for
    Managed D-Bus.

---
+++ sysutils/dbus-sharp-glib/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/06/10 12:03:52 obache Exp $
+
+BUILDLINK_TREE+=	dbus-sharp-glib
+
+.if !defined(DBUS_SHARP_GLIB_BUILDLINK3_MK)
+DBUS_SHARP_GLIB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.dbus-sharp-glib+=	dbus-sharp-glib>=0.5.0
+BUILDLINK_PKGSRCDIR.dbus-sharp-glib?=	../../sysutils/dbus-sharp-glib
+
+.include "../../sysutils/dbus-sharp/buildlink3.mk"
+.endif	# DBUS_SHARP_GLIB_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-dbus-sharp-glib
