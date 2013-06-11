a2bebfae70933699fb6d4f0ad6a7db4aa81fdb72
ryoon <ryoon@pkgsrc.org>
2013-06-11 11:54:47 +0000

    Update to 3.6.3
    Changelog:
    Major changes in release 3.6.3
    
    * Remove duplicate for All Saints Day for DE_BY.
    * Inherit some portability fixes from gnulib.

--- time/gcal/patches/patch-lib_fflush.c
+++ time/gcal/patches/patch-lib_fflush.c
@@ -1,13 +1,15 @@
-$NetBSD: patch-lib_fflush.c,v 1.2 2012/08/11 18:51:16 ryoon Exp $
+$NetBSD: patch-lib_fflush.c,v 1.3 2013/06/11 11:54:47 ryoon Exp $
 
---- lib/fflush.c.orig	2012-04-06 15:05:23.000000000 +0000
+* Support fpos_t change.
+
+--- lib/fflush.c.orig	2013-01-19 12:35:32.000000000 +0000
 +++ lib/fflush.c
-@@ -94,7 +94,7 @@ update_fpos_cache (FILE *fp _GL_UNUSED_P
+@@ -96,7 +96,7 @@ update_fpos_cache (FILE *fp _GL_UNUSED_P
                     off_t pos _GL_UNUSED_PARAMETER)
  {
- #if defined __sferror || defined __DragonFly__ /* FreeBSD, NetBSD, OpenBSD, DragonFly, MacOS X, Cygwin */
--# if defined __CYGWIN__
-+# if defined(__CYGWIN__) || defined(__NetBSD__)
+ #  if defined __sferror || defined __DragonFly__ /* FreeBSD, NetBSD, OpenBSD, DragonFly, Mac OS X, Cygwin */
+-#   if defined __CYGWIN__
++#   if defined(__CYGWIN__) || defined(__NetBSD__)
    /* fp_->_offset is typed as an integer.  */
    fp_->_offset = pos;
- # else
+ #   else
--- time/gcal/patches/patch-lib_fseeko.c
+++ time/gcal/patches/patch-lib_fseeko.c
@@ -1,11 +1,13 @@
-$NetBSD: patch-lib_fseeko.c,v 1.2 2012/08/11 18:51:16 ryoon Exp $
+$NetBSD: patch-lib_fseeko.c,v 1.3 2013/06/11 11:54:47 ryoon Exp $
 
---- lib/fseeko.c.orig	2012-05-13 15:47:48.000000000 +0000
+* Support fpos_t change.
+
+--- lib/fseeko.c.orig	2013-03-29 20:35:20.000000000 +0000
 +++ lib/fseeko.c
 @@ -121,7 +121,7 @@ fseeko (FILE *fp, off_t offset, int when
        fp->_flags &= ~_IO_EOF_SEEN;
        fp->_offset = pos;
- #elif defined __sferror || defined __DragonFly__ /* FreeBSD, NetBSD, OpenBSD, DragonFly, MacOS X, Cygwin */
+ #elif defined __sferror || defined __DragonFly__ /* FreeBSD, NetBSD, OpenBSD, DragonFly, Mac OS X, Cygwin */
 -# if defined __CYGWIN__
 +# if defined(__CYGWIN__) || defined(__NetBSD__)
        /* fp_->_offset is typed as an integer.  */
