abb688fb7e0aa7f679993e89d09dd7b56b4c455c
ryoon <ryoon@pkgsrc.org>
2013-06-11 12:58:42 +0000

    Update to 0.26.1
    Changelog:
    * Bugfixes
    * Support new chips.

--- emulators/gpsim/patches/patch-cli_cmd__list.cc
+++ emulators/gpsim/patches/patch-cli_cmd__list.cc
@@ -1,15 +1,15 @@
-$NetBSD: patch-cli_cmd__list.cc,v 1.1 2011/12/19 15:52:20 wiz Exp $
+$NetBSD: patch-cli_cmd__list.cc,v 1.2 2013/06/11 12:58:42 ryoon Exp $
 
 Avoid conflict with C++ list<>.
 
---- cli/cmd_list.cc.orig	2005-05-11 13:29:01.000000000 +0000
+--- cli/cmd_list.cc.orig	2008-06-06 20:50:55.000000000 +0000
 +++ cli/cmd_list.cc
-@@ -76,7 +76,7 @@ void cmd_list::list(void)
-   if(!have_cpu(1))
-     return;
+@@ -75,7 +75,7 @@ void cmd_list::list(void)
  
--  GetActiveCPU()->list(file_id,GetActiveCPU()->pc->value,starting_line,ending_line);
-+  GetActiveCPU()->plist(file_id,GetActiveCPU()->pc->value,starting_line,ending_line);
+   Processor *pCpu = GetActiveCPU(true);
+   if(pCpu)
+-    pCpu->list(file_id,pCpu->pc->value,starting_line,ending_line);
++    pCpu->plist(file_id,pCpu->pc->value,starting_line,ending_line);
  
  }
  void cmd_list::list(cmd_options *opt)
