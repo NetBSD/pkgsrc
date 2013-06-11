44c12d7a9e6de529b324e72821edea734598a5bd
ryoon <ryoon@pkgsrc.org>
2013-06-11 12:58:42 +0000

    Update to 0.26.1
    Changelog:
    * Bugfixes
    * Support new chips.

--- emulators/gpsim/patches/patch-src_program__files.cc
+++ emulators/gpsim/patches/patch-src_program__files.cc
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_program__files.cc,v 1.1 2011/12/19 15:52:21 wiz Exp $
+$NetBSD: patch-src_program__files.cc,v 1.2 2013/06/11 12:58:42 ryoon Exp $
 
 Fix namespace.
 
---- src/program_files.cc.orig	2005-08-25 21:57:41.000000000 +0000
+--- src/program_files.cc.orig	2007-02-11 20:49:21.000000000 +0000
 +++ src/program_files.cc
-@@ -136,7 +136,7 @@ ProgramFileBuf::int_type ProgramFileBuf:
+@@ -139,7 +139,7 @@ ProgramFileBuf::int_type ProgramFileBuf:
    if (numPutback > 4) {
      numPutback = 4;
    }
 -  std::memcpy (m_Buffer+(4-numPutback), gptr() - numPutback, numPutback);
 +  memcpy (m_Buffer+(4-numPutback), gptr() - numPutback, numPutback);
- 
    int num;
    if((num = ::fread((void*)( m_Buffer + 4), 1, m_iBufferSize - 4, m_pFile)) <= 0) {
+     if(errno != 0)
