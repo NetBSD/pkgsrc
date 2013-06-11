e2d8f6c280a6bb45971453d81d0f4e4a5ee59810
adam <adam@pkgsrc.org>
2013-06-11 08:28:30 +0000

    Changes 1.6: The "--keep" (-k) option was added to not delete input files, in line with other tools such as xz, lzip, and bzip2. A decompression issue with certain invalid data in the 'pack' format was fixed. An incorrect overwrite when compiled with optimization was fixed. zgrep's handling of multi-digit context options was fixed. zmore now acts more like 'more'.

--- archivers/gzip/patches/patch-lib_fflush.c
+++ archivers/gzip/patches/patch-lib_fflush.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-lib_fflush.c,v 1.1 2013/06/10 11:56:58 ryoon Exp $
+$NetBSD: patch-lib_fflush.c,v 1.2 2013/06/11 08:28:30 adam Exp $
 
---- lib/fflush.c.orig	2012-06-17 17:05:17.000000000 +0000
+--- lib/fflush.c.orig	2013-06-09 20:37:56.000000000 +0000
 +++ lib/fflush.c
-@@ -94,7 +94,7 @@ update_fpos_cache (FILE *fp _GL_UNUSED_P
+@@ -96,7 +96,7 @@ update_fpos_cache (FILE *fp _GL_UNUSED_P
                     off_t pos _GL_UNUSED_PARAMETER)
  {
- #if defined __sferror || defined __DragonFly__ /* FreeBSD, NetBSD, OpenBSD, DragonFly, MacOS X, Cygwin */
--# if defined __CYGWIN__
-+# if defined(__CYGWIN__) || defined(__NetBSD__)
+ #  if defined __sferror || defined __DragonFly__ /* FreeBSD, NetBSD, OpenBSD, DragonFly, Mac OS X, Cygwin */
+-#   if defined __CYGWIN__
++#   if defined(__CYGWIN__) || defined(__NetBSD__)
    /* fp_->_offset is typed as an integer.  */
    fp_->_offset = pos;
- # else
+ #   else
