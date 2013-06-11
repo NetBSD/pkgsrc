a32f83e037f58123c8ae6f30c5eaea212b8b3ec6
taca <taca>
2013-06-11 14:55:54 +0000

    Update rubygems to 2.0.3.
    THere are too many changes to write here, please refer these release notes.
    
    http://blog.rubygems.org/2013/03/11/2.0.3-released.html
    http://blog.rubygems.org/2013/03/06/2.0.2-released.html
    http://blog.rubygems.org/2013/03/05/2.0.1-released.html
    http://blog.rubygems.org/2013/02/24/2.0.0-released.html
    http://blog.rubygems.org/2013/02/08/2.0.0.rc.2-released.html
    http://blog.rubygems.org/2012/12/03/2.0.0-preview2.html

--- misc/rubygems/patches/patch-test_rubygems_test__gem.rb
+++ misc/rubygems/patches/patch-test_rubygems_test__gem.rb
@@ -1,24 +1,22 @@
-$NetBSD: patch-test_rubygems_test__gem.rb,v 1.2 2011/08/12 15:50:44 taca Exp $
+$NetBSD: patch-test_rubygems_test__gem.rb,v 1.3 2013/06/11 14:55:54 taca Exp $
 
 Use vendorlibdir.
 
---- test/rubygems/test_gem.rb.orig	2011-05-26 04:01:53.000000000 +0000
+--- test/rubygems/test_gem.rb.orig	2013-02-12 20:20:45.000000000 +0000
 +++ test/rubygems/test_gem.rb
-@@ -806,13 +806,13 @@ class TestGem < Gem::TestCase
-     Gem::ConfigMap[:libdir] = orig_libdir
+@@ -952,12 +952,12 @@ class TestGem < Gem::TestCase
    end
  
--  def test_self_prefix_sitelibdir
+   def test_self_prefix_sitelibdir
 -    orig_sitelibdir = Gem::ConfigMap[:sitelibdir]
 -    Gem::ConfigMap[:sitelibdir] = @@project_dir
-+  def test_self_prefix_vendorlibdir
-+    orig_vendorlibdir = Gem::ConfigMap[:vendorlibdir]
++    orig_sitelibdir = Gem::ConfigMap[:vendorlibdir]
 +    Gem::ConfigMap[:vendorlibdir] = @@project_dir
  
      assert_nil Gem.prefix
    ensure
 -    Gem::ConfigMap[:sitelibdir] = orig_sitelibdir
-+    Gem::ConfigMap[:vendorlibdir] = orig_vendorlibdir
++    Gem::ConfigMap[:vendorlibdir] = orig_sitelibdir
    end
  
    def test_self_refresh
