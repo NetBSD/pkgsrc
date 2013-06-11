ff1b878e8968e94f8d97b8af1c33aae6683b638f
ryoon <ryoon>
2013-06-11 12:58:42 +0000

    Update to 0.26.1
    Changelog:
    * Bugfixes
    * Support new chips.

--- emulators/gpsim/patches/patch-src_processor.h
+++ emulators/gpsim/patches/patch-src_processor.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_processor.h,v 1.1 2011/12/19 15:52:21 wiz Exp $
+$NetBSD: patch-src_processor.h,v 1.2 2013/06/11 12:58:42 ryoon Exp $
 
 Avoid conflict with C++ list<>.
 
---- src/processor.h.orig	2005-08-31 03:57:38.000000000 +0000
+--- src/processor.h.orig	2011-03-12 09:34:27.000000000 +0000
 +++ src/processor.h
-@@ -544,7 +544,7 @@ public:
+@@ -581,7 +581,7 @@ public:
  
    virtual void disassemble (signed int start_address, 
- 			    signed int end_address);
+                             signed int end_address);
 -  virtual void list(unsigned int file_id, 
 +  virtual void plist(unsigned int file_id, 
- 		    unsigned int pcval, 
- 		    unsigned int start_line, 
- 		    unsigned int end_line);
+                     unsigned int pcval, 
+                     int start_line, 
+                     int end_line);
