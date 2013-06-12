9da23dfe8330f6a36ba4dd90347214408f9192b9
ryoon <ryoon@pkgsrc.org>
2013-06-12 12:00:08 +0000

    Update to 5.6.2
    * Update buildlink3.mk.
    
    Changelog:
    5.6.0 - added AuthenticatedSymmetricCipher interface class and Filter wrappers
          - added CCM, GCM (with SSE2 assembly), EAX, CMAC, XSalsa20, and SEED
          - added support for variable length IVs
          - added OIDs for Brainpool elliptic curve parameters
          - improved AES and SHA-256 speed on x86 and x64
          - changed BlockTransformation interface to no longer assume data alignment
          - fixed incorrect VMAC computation on message lengths
            that are >64 mod 128 (x86 assembly version is not affected)
          - fixed compiler error in vmac.cpp on x86 with GCC -fPIC
          - fixed run-time validation error on x86-64 with GCC 4.3.2 -O2
          - fixed HashFilter bug when putMessage=true
          - fixed AES-CTR data alignment bug that causes incorrect encryption on ARM
          - removed WORD64_AVAILABLE; compiler support for 64-bit int is now required
          - ported to GCC 4.3, C++Builder 2009, Sun CC 5.10, Intel C++ Compiler 11
    
    5.6.1 - added support for AES-NI and CLMUL instruction sets in AES and GMAC/GCM
          - removed WAKE-CFB
          - fixed several bugs in the SHA-256 x86/x64 assembly code:
              * incorrect hash on non-SSE2 x86 machines on non-aligned input
              * incorrect hash on x86 machines when input crosses 0x80000000
              * incorrect hash on x64 when compiled with GCC with optimizations enabled
          - fixed bugs in AES x86 and x64 assembly causing crashes in some MSVC build configurations
          - switched to a public domain implementation of MARS
          - ported to MSVC 2010, GCC 4.5.1, Sun Studio 12u1, C++Builder 2010, Intel C++ Compiler 11.1
          - renamed the MSVC DLL project to "cryptopp" for compatibility with MSVC 2010
    
    5.6.2 - changed license to Boost Software License 1.0
          - added SHA-3 (Keccak)
          - updated DSA to FIPS 186-3 (see DSA2 class)
          - fixed Blowfish minimum keylength to be 4 bytes (32 bits)
          - fixed Salsa validation failure when compiling with GCC 4.6
          - fixed infinite recursion when on x64, assembly disabled, and no AESNI
          - ported to MSVC 2012, GCC 4.7, Clang 3.2, Solaris Studio 12.3, Intel C++ Compiler 13.0

--- security/crypto++/buildlink3.mk
+++ security/crypto++/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.10 2012/05/07 01:53:55 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2013/06/12 12:00:08 ryoon Exp $
 
 BUILDLINK_TREE+=	cryptopp
 
 .if !defined(CRYPTOPP_BUILDLINK3_MK)
 CRYPTOPP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cryptopp+=	cryptopp>=5.1
+BUILDLINK_API_DEPENDS.cryptopp+=	cryptopp>=5.6.2
 BUILDLINK_DEPMETHOD.cryptopp?=	build
-BUILDLINK_ABI_DEPENDS.cryptopp+=	cryptopp>=5.2.1nb1
+BUILDLINK_ABI_DEPENDS.cryptopp+=	cryptopp>=5.6.2
 BUILDLINK_PKGSRCDIR.cryptopp?=	../../security/crypto++
 .endif # CRYPTOPP_BUILDLINK3_MK
 
