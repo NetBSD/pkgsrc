3cd649d6d3505ac32c108ec99e9e060e798b4464
obache <obache@pkgsrc.org>
2013-06-13 09:02:50 +0000

    mk/compiler.mk is included after mk/bsd.hacks.mk in mk/bsd.pkg.mk, so it must be included in hacks.mk exactly before using CC_VERSION.

--- graphics/Coin/hacks.mk
+++ graphics/Coin/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.5 2013/06/11 15:22:47 ryoon Exp $
+# $NetBSD: hacks.mk,v 1.6 2013/06/13 09:02:50 obache Exp $
 
 .if !defined(COIN_HACKS_MK)
 COIN_HACKS_MK=	defined
@@ -8,6 +8,7 @@ COIN_HACKS_MK=	defined
 ### compiler error.  This is a workaround for PR toolchain/33097.
 ### Not sure if other platforms have the same issue.
 ###
+.include "../../mk/compiler.mk"
 .if ${OPSYS} == "NetBSD" && !empty(CC_VERSION:Mgcc-3.*)
 PKG_HACKS+=		optimization
 BUILDLINK_TRANSFORM+=	rm:-O[0-9]*
