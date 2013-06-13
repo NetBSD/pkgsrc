9083dc146c0bb0696dd07c55bbb921ab36011d54
fhajny <fhajny@pkgsrc.org>
2013-06-13 20:11:05 +0000

    Update nodejs to 0.10.11.
    2013.06.13, Version 0.10.11 (Stable)
    * uv: upgrade to 0.10.11
    * npm: Upgrade to 1.2.30
    * openssl: add missing configuration pieces for MIPS (Andrei Sedoi)
    * Revert "http: remove bodyHead from 'upgrade' events" (isaacs)
    * v8: fix pointer arithmetic undefined behavior (Trevor Norris)
    * crypto: fix utf8/utf-8 encoding check (Ben Noordhuis)
    * net: Fix busy loop on POLLERR|POLLHUP on older linux kernels
      (Ben Noordhuis, isaacs)

--- lang/nodejs/patches/patch-deps_uv_src_unix_netbsd.c
+++ lang/nodejs/patches/patch-deps_uv_src_unix_netbsd.c
@@ -1,5 +1,6 @@
-$NetBSD: patch-deps_uv_src_unix_netbsd.c,v 1.1 2013/06/05 13:27:05 mspo Exp $
+$NetBSD: patch-deps_uv_src_unix_netbsd.c,v 1.2 2013/06/13 20:11:05 fhajny Exp $
 
+Fix build on NetBSD-current.
 --- deps/uv/src/unix/netbsd.c-orig      2013-06-05 12:34:12.000000000 +0000
 +++ deps/uv/src/unix/netbsd.c
 @@ -37,6 +37,7 @@
@@ -7,6 +8,6 @@ $NetBSD: patch-deps_uv_src_unix_netbsd.c,v 1.1 2013/06/05 13:27:05 mspo Exp $
  #include <sys/types.h>
  #include <sys/sysctl.h>
 +#include <uvm/uvm.h>
-
+ 
  #include <unistd.h>
  #include <time.h>
