04b59253bc971b92e59666c5c17c12aa4f106ca2
joerg <joerg@pkgsrc.org>
2013-06-13 21:48:09 +0000

    Don't use rel_ops, it can create ambiguity for types that define more than the minimal operator set. Rename out_of_range.

--- net/xorp/patches/patch-libxorp_ipv6.hh
+++ net/xorp/patches/patch-libxorp_ipv6.hh
@@ -1,8 +1,27 @@
-$NetBSD: patch-libxorp_ipv6.hh,v 1.1 2013/05/30 15:37:24 joerg Exp $
+$NetBSD: patch-libxorp_ipv6.hh,v 1.2 2013/06/13 21:48:09 joerg Exp $
 
---- libxorp/ipv6.hh.orig	2013-04-29 19:22:56.000000000 +0000
+--- libxorp/ipv6.hh.orig	2009-01-05 18:30:58.000000000 +0000
 +++ libxorp/ipv6.hh
-@@ -459,7 +459,7 @@ public:
+@@ -281,6 +281,18 @@ public:
+      */
+     bool operator!=(const IPv6& other) const;
+ 
++    bool operator<=(const IPv6& other) const {
++	return other < *this || other == *this;
++    }
++
++    bool operator>=(const IPv6& other) const {
++	return other <= *this;
++    }
++
++    bool operator>(const IPv6& other) const {
++	return other < *this;
++    }
++
+     /**
+      * Equality Operator for @ref IPv6 against @ref IPv6Range operand.
+      *
+@@ -459,7 +471,7 @@ public:
       * @return address size in number of octets.
       */
      static size_t addr_bytelen() {
