8e5950fb4789abe4ae82758ad42c6138b865339e
joerg <joerg@pkgsrc.org>
2013-06-13 21:42:22 +0000

    Fix redefinition in the same scope. Add missing unistd.h.

--- games/xye/patches/patch-src_gen.cpp
+++ games/xye/patches/patch-src_gen.cpp
@@ -1,12 +1,13 @@
-$NetBSD: patch-src_gen.cpp,v 1.2 2013/06/09 06:30:14 ryoon Exp $
+$NetBSD: patch-src_gen.cpp,v 1.3 2013/06/13 21:42:22 joerg Exp $
 
---- src/gen.cpp.orig	2013-06-09 06:21:13.000000000 +0000
+--- src/gen.cpp.orig	2013-06-13 19:51:44.000000000 +0000
 +++ src/gen.cpp
-@@ -21,6 +21,7 @@ Permission is granted to anyone to use t
+@@ -21,6 +21,8 @@ Permission is granted to anyone to use t
  #include<cstdio>
  #include<iostream>
  #include<fstream>
 +#include<ctime>
++#include <unistd.h>
  
  const float F_RAND_MAX = (float)(RAND_MAX);
  
