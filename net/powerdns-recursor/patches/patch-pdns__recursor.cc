815f48493f9032f5338f985bfd9332964e35ef40
dholland <dholland@pkgsrc.org>
2013-06-14 22:13:03 +0000

    Patch up C++ semantic whitespace.

--- net/powerdns-recursor/patches/patch-pdns__recursor.cc
+++ net/powerdns-recursor/patches/patch-pdns__recursor.cc
@@ -1,6 +1,12 @@
-$NetBSD: patch-pdns__recursor.cc,v 1.1 2012/02/28 10:49:29 roy Exp $
+$NetBSD: patch-pdns__recursor.cc,v 1.2 2013/06/14 22:13:03 dholland Exp $
 
 Cast to int so we use the correct overload.
+Fix C++ semantic whitespace.
+
+Caution: this file is also SUBST'd at pre-configure time to insert
+more C++ semantic whitespace around the VERSION macro. Don't roll
+those changes back into the pkgsrc patch as one of them contains
+an rcsid string. (sigh)
 
 --- pdns_recursor.cc.orig	2010-09-21 16:22:09.000000000 +0000
 +++ pdns_recursor.cc
@@ -13,3 +19,12 @@ Cast to int so we use the correct overload.
      }
      lastOutputTime = now;
      lastQueryCount = SyncRes::s_queries;
+@@ -2017,7 +2017,7 @@ int main(int argc, char **argv) 
+       exit(99);
+     }
+     if(::arg().mustDo("version")) {
+-      cerr<<"version: "VERSION<<endl;
++      cerr<<"version: " VERSION <<endl;
+       exit(99);
+     }
+ 
