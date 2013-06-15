4eca92ac1c82aa2070a60ce96d93b452adbf0901
khorben <khorben@pkgsrc.org>
2013-06-15 12:42:12 +0000

    Packaged DeforaOS Player 0.1.6, with: - more portable Makefiles - improvements to the user interface

---
+++ multimedia/deforaos-player/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/06/15 12:42:12 khorben Exp $
+#
+
+BUILDLINK_TREE+=	deforaos-player
+
+.if !defined(DEFORAOS_PLAYER_BUILDLINK3_MK)
+DEFORAOS_PLAYER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.deforaos-player+=	deforaos-player>=0.1.6
+BUILDLINK_ABI_DEPENDS.deforaos-player?=	deforaos-player>=0.1.6
+BUILDLINK_PKGSRCDIR.deforaos-player?=	../../multimedia/deforaos-player
+
+.include "../../x11/deforaos-libdesktop/buildlink3.mk"
+.endif	# DEFORAOS_PLAYER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-deforaos-player
