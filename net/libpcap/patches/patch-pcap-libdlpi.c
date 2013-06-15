61fcb44707b4e8336227d06fd6feabdd7bd2d0e1
adam <adam>
2013-06-15 21:01:22 +0000

    Summary for 1.4.0 libpcap release         Add netfilter/nfqueue interface.         If we don't have support for IPv6 address resolution, support,             in filter expressions, what IPv6 stuff we can.         Checks added for malloc()/realloc()/etc. failures.         Fix pcap-config to include -lpthread if canusb support is             present         Try to fix "pcap_parse not defined" problems when --without-flex             and --without-bison are used when you have Flex and Bison         Fix some issues with the pcap_loop man page.         Fix pcap_getnonblock() and pcap_setnonblock() to fill in the             supplied error message buffer         Fix typo that, it appeared, would cause pcap-libdlpi.c not to             compile (perhaps systems with libdlpi also have BPF and use             that instead)         Catch attempts to call pcap_compile() on a non-activated pcap_t         Fix crash on Linux with CAN-USB support without usbfs         Fix addition of VLAN tags for Linux cooked captures         Check for both EOPNOTSUPP and EINVAL after SIOCETHTOOL ioctl, so             that the driver can report either one if it doesn't support             SIOCETHTOOL         Add DLT_INFINIBAND and DLT_SCTP         Describe "proto XXX" and "protochain XXX" in the pcap-filter man             page         Handle either directories, or symlinks to directories, that             correspond to interfaces in /sys/class/net         Fix handling of VLAN tag insertion to check, on Linux 3.x             kernels, for VLAN tag valid flag         Clean up some man pages         Support libnl3 as well as libnl1 and libnl2 on Linux

--- net/libpcap/patches/patch-pcap-libdlpi.c
+++ net/libpcap/patches/patch-pcap-libdlpi.c
@@ -1,18 +1,15 @@
-$NetBSD: patch-pcap-libdlpi.c,v 1.1 2013/02/06 18:07:04 jperkin Exp $
+$NetBSD: patch-pcap-libdlpi.c,v 1.2 2013/06/15 21:01:22 adam Exp $
 
 * Fix compile on Solaris.
 
---- pcap-libdlpi.c.orig	2012-03-29 12:57:32.000000000 +0000
+--- pcap-libdlpi.c.orig	2013-02-21 03:51:49.000000000 +0000
 +++ pcap-libdlpi.c
-@@ -235,9 +235,9 @@ bad:
+@@ -235,7 +235,7 @@ bad:
  static int
  dlpromiscon(pcap_t *p, bpf_u_int32 level)
  {
 -	int err;
 +	int err, retv;
  
--	retv = dlpi_promiscon(p->hd, level);
-+	retv = dlpi_promiscon(p->dlpi_hd, level);
+ 	retv = dlpi_promiscon(p->dlpi_hd, level);
  	if (retv != DLPI_SUCCESS) {
- 		if (retv == DL_SYSERR &&
- 		    (errno == EPERM || errno == EACCES))
