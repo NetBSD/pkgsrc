118a19a953073ff3db428fa22560d3fea7893052
jmmv <jmmv@pkgsrc.org>
2013-06-15 00:30:31 +0000

    Update to 0.3:
    Released on 2013/06/14.
    
    * Issue 1: Added support for Lua 5.2 while maintaining support for Lua
      5.1.  Applications using Lutok can be modified to use the new
      interface in this new version and thus support both Lua releases.
      However, because of incompatible changes to the Lua API, this release
      of Lutok is incompatible with previous releases as well.
    
    * Issue 3: Tweaked configure to look for Lua using the pkg-config names
      lua-5.2 and lua-5.1.  These are the names used by FreeBSD.
    
    Interface changes:
    
    * New global constants: registry_index.
    
    * New methods added to the state class: get_global_table.
    
    * Removed global constants: globals_index.

--- devel/lutok/buildlink3.mk
+++ devel/lutok/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/02/26 14:02:31 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/06/15 00:30:31 jmmv Exp $
 
 BUILDLINK_TREE+=	lutok
 
 .if !defined(LUTOK_BUILDLINK3_MK)
 LUTOK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lutok+=	lutok>=0.1
+BUILDLINK_API_DEPENDS.lutok+=	lutok>=0.3
 BUILDLINK_PKGSRCDIR.lutok?=	../../devel/lutok
 
 .include "../../lang/lua/buildlink3.mk"
