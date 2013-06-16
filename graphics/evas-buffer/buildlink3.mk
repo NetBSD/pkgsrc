f126c56934188128be4fc000b9904cce70682a80
sno <sno@pkgsrc.org>
2013-06-16 18:56:04 +0000

    Updating enlightenment libraries to 1.7.7, add missing enlightenment 0.17.3 libraries and enlightenment 0.17.3 itself.
    Upstream changes of Eina (to get an impression):
    Eina 1.7.7
    
    Changes since Eina 1.7.6:
    -------------------------
    
    No changes, just updating to keep in sync with last release.
    
    
    Changes since Eina 1.7.5:
    -------------------------
    
    Improvements:
        * Honor tile size in Eina_Tiler.
    
    Fixes:
        * Prevent denial of service on Eina_Hash function.
        * Fix map leak in Eina_File infrastructure.
        * Fix portability issue on 64bits system for Eina_CList.
        * Fix magic failure in eina_value_array_count when array has not been allocated
    
    Changes since Eina 1.7.4:
    -------------------------
    
    No changes, just updating to keep in sync with last release.
    
    Changes since Eina 1.7.3:
    -------------------------
    
    Fixes:
        * Fix EINA_INLIST_FOREACH_SAFE macro
        * Add XML output to doc
        * Add installation rule for doc
        * Fix build for Windows platforms.
    
    Changes since Eina 1.7.2:
    -------------------------
    
        * Fix Solaris build.
        * Don't leak fd after exec.
    
    Changes since Eina 1.7.1:
    -------------------------
    
    No changes, just updating to keep in sync with last release.

--- graphics/evas-buffer/buildlink3.mk
+++ graphics/evas-buffer/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2011/12/05 17:33:32 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2013/06/16 18:56:04 sno Exp $
 
 BUILDLINK_TREE+=	evas-buffer
 
 .if !defined(EVAS_BUFFER_BUILDLINK3_MK)
 EVAS_BUFFER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.evas-buffer+=	evas-buffer>=1.1.0beta
+BUILDLINK_API_DEPENDS.evas-buffer+=	evas-buffer>=1.7.7
 BUILDLINK_PKGSRCDIR.evas-buffer?=	../../graphics/evas-buffer
 .endif # EVAS_BUFFER_BUILDLINK3_MK
 
--- sysutils/edbus/buildlink3.mk
+++ sysutils/edbus/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2013/06/04 22:17:04 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2013/06/16 18:56:05 sno Exp $
 
 BUILDLINK_TREE+=	edbus
 
 .if !defined(EDBUS_BUILDLINK3_MK)
 EDBUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.edbus+=	edbus>=1.1.0
-BUILDLINK_ABI_DEPENDS.edbus?=	edbus>=1.1.0nb5
+BUILDLINK_API_DEPENDS.edbus+=	edbus>=1.7.7
+BUILDLINK_ABI_DEPENDS.edbus?=	edbus>=1.7.7
 BUILDLINK_PKGSRCDIR.edbus?=	../../sysutils/edbus
 
 .include "../../devel/ecore/buildlink3.mk"
