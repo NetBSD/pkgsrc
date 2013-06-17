7e52a8e656b68a67913c0c3a22ba883a17822118
wiz <wiz>
2013-06-17 12:44:53 +0000

    Make mono packages depend on lang/mono2 instead of lang/mono.
    The latter contains the unstable version 3 of mono, which breaks many mono
    packages.
    
    Bump dependencies and PKGREVISIONs.
    
    Ok during freeze: gdt@

--- devel/mono-addins/buildlink3.mk
+++ devel/mono-addins/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2013/05/09 07:39:07 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2013/06/17 12:44:53 wiz Exp $
 
 BUILDLINK_TREE+=	mono-addins
 
@@ -6,11 +6,11 @@ BUILDLINK_TREE+=	mono-addins
 MONO_ADDINS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mono-addins+=	mono-addins>=0.3
-BUILDLINK_ABI_DEPENDS.mono-addins+=	mono-addins>=1.0nb2
+BUILDLINK_ABI_DEPENDS.mono-addins+=	mono-addins>=1.0nb3
 BUILDLINK_PKGSRCDIR.mono-addins?=	../../devel/mono-addins
 BUILDLINK_CONTENTS_FILTER.mono-addins+=	${EGREP} '^lib/'
 
-.include "../../lang/mono/buildlink3.mk"
+.include "../../lang/mono2/buildlink3.mk"
 .include "../../x11/gtk-sharp/buildlink3.mk"
 .endif # MONO_ADDINS_BUILDLINK3_MK
 
--- www/mono-xsp/buildlink3.mk
+++ www/mono-xsp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2013/01/26 21:36:50 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2013/06/17 12:44:55 wiz Exp $
 
 BUILDLINK_TREE+=	mono-xsp
 
@@ -6,11 +6,11 @@ BUILDLINK_TREE+=	mono-xsp
 MONO_XSP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mono-xsp+=	mono-xsp>=2.0
-BUILDLINK_ABI_DEPENDS.mono-xsp+=	mono-xsp>=2.10.2nb4
+BUILDLINK_ABI_DEPENDS.mono-xsp+=	mono-xsp>=2.10.2nb6
 BUILDLINK_PKGSRCDIR.mono-xsp?=	../../www/mono-xsp
 
 .include "../../databases/sqlite3/buildlink3.mk"
-.include "../../lang/mono/buildlink3.mk"
+.include "../../lang/mono2/buildlink3.mk"
 .endif # MONO_XSP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mono-xsp
