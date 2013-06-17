3cfacb772dbc514a7c0a30de664d8c882c35e7d8
wiz <wiz>
2013-06-17 12:44:53 +0000

    Make mono packages depend on lang/mono2 instead of lang/mono.
    The latter contains the unstable version 3 of mono, which breaks many mono
    packages.
    
    Bump dependencies and PKGREVISIONs.
    
    Ok during freeze: gdt@

--- lang/mono/buildlink3.mk
+++ lang/mono/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.46 2013/05/29 11:11:12 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.47 2013/06/17 12:44:54 wiz Exp $
 
 BUILDLINK_TREE+=	mono
 
 .if !defined(MONO_BUILDLINK3_MK)
 MONO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mono+=	mono>=2.8
-BUILDLINK_ABI_DEPENDS.mono+=	mono>=2.10.6nb3
+BUILDLINK_API_DEPENDS.mono+=	mono>=3
 BUILDLINK_PKGSRCDIR.mono?=	../../lang/mono
 ALL_ENV+=			MONO_SHARED_DIR=${WRKDIR:Q}
 BUILDLINK_CONTENTS_FILTER.mono=	${EGREP} '(^include/|^lib/)'
