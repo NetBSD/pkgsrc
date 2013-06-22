d63d58d3c5ad6ac10a16857af471dc814dc510cb
dholland <dholland>
2013-06-22 22:12:42 +0000

    Rather than try to guess which flex version changed the prototype of yyget_leng() from returning int to returning size_t, since it was apparently somewhere between the flex I have and the flex Joerg's build machine has, skip the declaration, since it's apparently no longer necessary to provide it for flex. This should fix the build on netbsd-6.

--- editors/vile/patches/patch-filters_filters.h
+++ editors/vile/patches/patch-filters_filters.h
@@ -1,13 +1,14 @@
-$NetBSD: patch-filters_filters.h,v 1.1 2013/05/25 23:42:45 joerg Exp $
+$NetBSD: patch-filters_filters.h,v 1.2 2013/06/22 22:12:42 dholland Exp $
 
 --- filters/filters.h.orig	2013-05-25 20:14:11.000000000 +0000
 +++ filters/filters.h
-@@ -210,7 +210,7 @@ extern FILE *yyget_in (void);
+@@ -210,7 +210,9 @@ extern FILE *yyget_in (void);
  extern FILE *yyget_out (void);
  extern char *yyget_text (void);
  extern int yyget_debug (void);
--extern int yyget_leng (void);
-+extern size_t yyget_leng (void);
++#if 0
+ extern int yyget_leng (void);
++#endif
  extern int yyget_lineno (void);
  extern int yylex_destroy (void);
  extern void yyset_debug (int bdebug);
