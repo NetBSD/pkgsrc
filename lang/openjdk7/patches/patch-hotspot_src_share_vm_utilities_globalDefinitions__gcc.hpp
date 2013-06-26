0caf72de0b5f53bbbd9d3c4f5f96c11d89eaea23
ryoon <ryoon>
2013-06-26 12:52:34 +0000

    Bump PKGREVISION.
    * Fix build on DragonFly 3.4.1 and 3.5-DEVELOPMENT.
      Use newer bootstrap binary for DragonFly.
    * DragonFly 3.5 switches to FreeBSD style math.h.
      Add conditional.
    
    I have no idea about build on very recent DragonFly 3.5.

--- lang/openjdk7/patches/patch-hotspot_src_share_vm_utilities_globalDefinitions__gcc.hpp
+++ lang/openjdk7/patches/patch-hotspot_src_share_vm_utilities_globalDefinitions__gcc.hpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-hotspot_src_share_vm_utilities_globalDefinitions__gcc.hpp,v 1.1 2013/06/15 09:31:06 jperkin Exp $
+$NetBSD: patch-hotspot_src_share_vm_utilities_globalDefinitions__gcc.hpp,v 1.2 2013/06/26 12:52:35 ryoon Exp $
 
 SunOS fixes.
 
---- hotspot/src/share/vm/utilities/globalDefinitions_gcc.hpp.orig	2013-02-19 23:21:59.000000000 +0000
+--- hotspot/src/share/vm/utilities/globalDefinitions_gcc.hpp.orig	2013-06-25 13:37:39.000000000 +0000
 +++ hotspot/src/share/vm/utilities/globalDefinitions_gcc.hpp
 @@ -31,6 +31,10 @@
  // globally used constants & types, class (forward)
@@ -15,3 +15,20 @@ SunOS fixes.
  #include <ctype.h>
  #include <string.h>
  #include <stdarg.h>
+@@ -241,6 +245,7 @@ extern "C" void breakpoint();
+ #define BREAKPOINT ::breakpoint()
+ #endif
+ 
++#include <sys/param.h> /* for __DragonFly_version */
+ // checking for nanness
+ #ifdef SOLARIS
+ #ifdef SPARC
+@@ -252,7 +257,7 @@ inline int g_isnan(float  f) { return is
+ inline int g_isnan(double f) { return isnand(f); }
+ #elif defined(__APPLE__)
+ inline int g_isnan(double f) { return isnan(f); }
+-#elif defined(__DragonFly__)
++#elif defined(__DragonFly__) && __DragonFly_version < 300500
+ inline int g_isnan(float  f) { return __isnanf(f); }
+ inline int g_isnan(double f) { return __isnand(f); }
+ #elif defined(LINUX) || defined(_ALLBSD_SOURCE)
