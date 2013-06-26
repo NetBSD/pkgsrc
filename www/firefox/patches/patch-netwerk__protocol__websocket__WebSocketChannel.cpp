a40b55b87587408e4a78a48655b22caed2d693cd
ryoon <ryoon@pkgsrc.org>
2013-06-26 11:32:12 +0000

    Update to 22.0
    * On NetBSD WebRTC support is disabled, because libxul.so has some errors
      in link stage. WebRTC support should be tested on non-NetBSD platforms.
    * It seems that OSS sound support is not working properly on NetBSD.
    
    Changelog:
        NEW
        WebRTC is now enabled by default!
        NEW
        Windows: Firefox now follows display scaling options to render text larger on high-res displays
        NEW
        Mac OS X: Download progress in Dock application icon
        NEW
        HTML5 audio/video playback rate can now be changed
        NEW
        Social services management implemented in Add-ons Manager
        NEW
        asm.js optimizations (OdinMonkey) enabled for major performance improvements
        CHANGED
        Improved WebGL rendering performance through asynchronous canvas updates
        CHANGED
        Plain text files displayed within Firefox will now word-wrap
        CHANGED
        For user security, the |Components| object is no longer accessible from web content
        CHANGED
        Pointer Lock API can now be used outside of fullscreen
        DEVELOPER
        CSS3 Flexbox implemented and enabled by default
        DEVELOPER
        New Web Notifications API implemented
        DEVELOPER
        Added clipboardData API for JavaScript access to a user's clipboard
        DEVELOPER
        New built-in font inspector
        HTML5
        New HTML5 <data> and <time> elements
        FIXED
        Various security fixes
        FIXED
        Scrolling using some high-resolution-scroll aware touchpads feels slow (829952)
    
    Fixed in Firefox 22
    MFSA 2013-62 Inaccessible updater can lead to local privilege escalation
    MFSA 2013-61 Homograph domain spoofing in .com, .net and .name
    MFSA 2013-60 getUserMedia permission dialog incorrectly displays location
    MFSA 2013-59 XrayWrappers can be bypassed to run user defined methods in a privileged context
    MFSA 2013-58 X-Frame-Options ignored when using server push with multi-part responses
    MFSA 2013-57 Sandbox restrictions not applied to nested frame elements
    MFSA 2013-56 PreserveWrapper has inconsistent behavior
    MFSA 2013-55 SVG filters can lead to information disclosure
    MFSA 2013-54 Data in the body of XHR HEAD requests leads to CSRF attacks
    MFSA 2013-53 Execution of unmapped memory through onreadystatechange event
    MFSA 2013-52 Arbitrary code execution within Profiler
    MFSA 2013-51 Privileged content access and execution via XBL
    MFSA 2013-50 Memory corruption found using Address Sanitizer
    MFSA 2013-49 Miscellaneous memory safety hazards (rv:22.0 / rv:17.0.7)

--- www/firefox/patches/patch-netwerk__protocol__websocket__WebSocketChannel.cpp
+++ www/firefox/patches/patch-netwerk__protocol__websocket__WebSocketChannel.cpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-netwerk__protocol__websocket__WebSocketChannel.cpp,v 1.1 2013/06/21 23:11:42 ryoon Exp $
+$NetBSD: patch-netwerk__protocol__websocket__WebSocketChannel.cpp,v 1.2 2013/06/26 11:32:12 ryoon Exp $
 
 # HG changeset patch
 # User Martin Husemann <martin@netbsd.org>
@@ -8,14 +8,9 @@ $NetBSD: patch-netwerk__protocol__websocket__WebSocketChannel.cpp,v 1.1 2013/06/
 Bug 871555: Use memcpy and properly aligned temporary variables instead of pointer casts with unclear alignment r=mcmanus
 
 diff netwerk/protocol/websocket/WebSocketChannel.cpp netwerk/protocol/websocket/WebSocketChannel.cpp
---- netwerk/protocol/websocket/WebSocketChannel.cpp
+--- netwerk/protocol/websocket/WebSocketChannel.cpp.orig	2013-06-17 22:13:23.000000000 +0000
 +++ netwerk/protocol/websocket/WebSocketChannel.cpp
-@@ -1627,17 +1627,18 @@ WebSocketChannel::PrimeNewOutgoingMessag
-     // payload is offset 6 including 4 for the mask
-     payload = mOutHeader + 6;
- 
-     // The close reason code sits in the first 2 bytes of payload
-     // If the channel user provided a code and reason during Close()
+@@ -1632,7 +1632,8 @@ WebSocketChannel::PrimeNewOutgoingMessag
      // and there isn't an internal error, use that.
      if (NS_SUCCEEDED(mStopOnClose)) {
        if (mScriptCloseCode) {
@@ -25,17 +20,7 @@ diff netwerk/protocol/websocket/WebSocketChannel.cpp netwerk/protocol/websocket/
          mOutHeader[1] += 2;
          mHdrOutToSend = 8;
          if (!mScriptCloseReason.IsEmpty()) {
-           NS_ABORT_IF_FALSE(mScriptCloseReason.Length() <= 123,
-                             "Close Reason Too Long");
-           mOutHeader[1] += mScriptCloseReason.Length();
-           mHdrOutToSend += mScriptCloseReason.Length();
-           memcpy (payload + 2,
-@@ -1646,17 +1647,18 @@ WebSocketChannel::PrimeNewOutgoingMessag
-         }
-       } else {
-         // No close code/reason, so payload length = 0.  We must still send mask
-         // even though it's not used.  Keep payload offset so we write mask
-         // below.
+@@ -1651,7 +1652,8 @@ WebSocketChannel::PrimeNewOutgoingMessag
          mHdrOutToSend = 6;
        }
      } else {
@@ -45,17 +30,7 @@ diff netwerk/protocol/websocket/WebSocketChannel.cpp netwerk/protocol/websocket/
        mOutHeader[1] += 2;
        mHdrOutToSend = 8;
      }
- 
-     if (mServerClosed) {
-       /* bidi close complete */
-       mReleaseOnTransmit = 1;
-     } else if (NS_FAILED(mStopOnClose)) {
-@@ -1734,17 +1736,18 @@ WebSocketChannel::PrimeNewOutgoingMessag
-       LOG(("WebSocketChannel::PrimeNewOutgoingMessage(): "
-            "GenerateRandomBytes failure %x\n", rv));
-       StopSession(rv);
-       return;
-     }
+@@ -1739,7 +1741,8 @@ WebSocketChannel::PrimeNewOutgoingMessag
      mask = * reinterpret_cast<uint32_t *>(buffer);
      NS_Free(buffer);
    } while (!mask);
@@ -65,9 +40,3 @@ diff netwerk/protocol/websocket/WebSocketChannel.cpp netwerk/protocol/websocket/
  
    LOG(("WebSocketChannel::PrimeNewOutgoingMessage() using mask %08x\n", mask));
  
-   // We don't mask the framing, but occasionally we stick a little payload
-   // data in the buffer used for the framing. Close frames are the current
-   // example. This data needs to be masked, but it is never more than a
-   // handful of bytes and might rotate the mask, so we can just do it locally.
-   // For real data frames we ship the bulk of the payload off to ApplyMask()
-
