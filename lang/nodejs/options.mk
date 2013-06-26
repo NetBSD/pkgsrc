bb2ebf7e27284e665ecd14caa2e2c2d32de571d5
jperkin <jperkin@pkgsrc.org>
2013-06-26 11:51:50 +0000

    Make DTrace a default option on OSX.

--- lang/nodejs/options.mk
+++ lang/nodejs/options.mk
@@ -1,10 +1,11 @@
-# $NetBSD: options.mk,v 1.2 2013/05/23 11:22:44 fhajny Exp $
+# $NetBSD: options.mk,v 1.3 2013/06/26 11:51:50 jperkin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.node
 PKG_SUPPORTED_OPTIONS=	openssl dtrace
 PKG_SUGGESTED_OPTIONS=	openssl
 
-.if ${OPSYS} == "SunOS" && exists(/usr/sbin/dtrace)
+.if (${OPSYS} == "SunOS" || ${OPSYS} == "Darwin") \
+    && exists(/usr/sbin/dtrace)
 PKG_SUGGESTED_OPTIONS+=	dtrace
 .endif
 
