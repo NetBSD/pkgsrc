07ccd1375e3e1ca46e9858cdc3936a2673457cb5
obache <obache>
2013-06-28 08:39:21 +0000

    BUILDLINK_PKGSRCDIR must be defined before using pkg-build-options.

--- devel/ucommon/buildlink3.mk
+++ devel/ucommon/buildlink3.mk
@@ -1,20 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/06/27 01:36:25 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/06/28 08:39:21 obache Exp $
 
 BUILDLINK_TREE+=	ucommon
 
 .if !defined(UCOMMON_BUILDLINK3_MK)
 UCOMMON_BUILDLINK3_MK:=
 
+BUILDLINK_API_DEPENDS.ucommon+=	ucommon>=5.0.6
+BUILDLINK_PKGSRCDIR.ucommon?=	../../devel/ucommon
+
 pkgbase := ucommon
 .include "../../mk/pkg-build-options.mk"
-
 
 .if !empty(PKG_BUILD_OPTIONS.ucommon:Mstatic)
 BUILDLINK_DEPMETHOD.ucommon?=	build
 .endif
-
-BUILDLINK_API_DEPENDS.ucommon+=	ucommon>=5.0.6
-BUILDLINK_PKGSRCDIR.ucommon?=	../../devel/ucommon
 
 .include "../../mk/dlopen.buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
