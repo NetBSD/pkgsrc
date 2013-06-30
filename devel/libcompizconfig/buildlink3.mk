7f7b56d9e75422e5741dc4e6e95c9a95f0c2f90f
rodent <rodent@pkgsrc.org>
2013-06-30 20:11:47 +0000

    Updated to latest release (before the rewrite). No NEWS/ChangeLog. Commented patches, Buildlink'd protobuf, libinotify, startup-notification, libxslt, libSM, libICE, libXrandr, and libX11 in Makefile. Added protobuf and libX11 to buildlink3.mk.

--- devel/libcompizconfig/buildlink3.mk
+++ devel/libcompizconfig/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2012/06/14 07:43:18 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/06/30 20:11:47 rodent Exp $
 
 BUILDLINK_TREE+=	libcompizconfig
 
@@ -9,6 +9,8 @@ BUILDLINK_API_DEPENDS.libcompizconfig+=	libcompizconfig>=0.6.0
 BUILDLINK_ABI_DEPENDS.libcompizconfig+=	libcompizconfig>=0.8.4nb9
 BUILDLINK_PKGSRCDIR.libcompizconfig?=	../../devel/libcompizconfig
 
+.include "../../devel/protobuf/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .endif # LIBCOMPIZCONFIG_BUILDLINK3_MK
 
