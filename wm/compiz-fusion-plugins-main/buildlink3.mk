e8c5e55d1fb7c43f88a0a8b369f7a1477a5d13f2
rodent <rodent@pkgsrc.org>
2013-06-30 20:53:46 +0000

    Update to last release before rewrite. No NEWS/ChangeLog. USE_TOOLS+= msgmerge xgettext; Remove disabling of libinotify in configure. Buildlink libXrandr, libxslt, startup-notification, pango, and cairo.

--- wm/compiz-fusion-plugins-main/buildlink3.mk
+++ wm/compiz-fusion-plugins-main/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2013/06/06 12:53:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2013/06/30 20:53:46 rodent Exp $
 
 BUILDLINK_TREE+=	compiz-fusion-plugins-main
 
@@ -9,7 +9,13 @@ BUILDLINK_API_DEPENDS.compiz-fusion-plugins-main+=	compiz-fusion-plugins-main>=0
 BUILDLINK_ABI_DEPENDS.compiz-fusion-plugins-main+=	compiz-fusion-plugins-main>=0.8.4nb19
 BUILDLINK_PKGSRCDIR.compiz-fusion-plugins-main?=	../../wm/compiz-fusion-plugins-main
 
-.include "../../wm/compiz/buildlink3.mk"
+.include "../../x11/libXrandr/buildlink3.mk"
+.include "../../textproc/libxslt/buildlink3.mk"
+.include "../../x11/startup-notification/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
+
 .endif # COMPIZ_FUSION_PLUGINS_MAIN_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-compiz-fusion-plugins-main
