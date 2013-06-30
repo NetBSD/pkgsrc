b3d069fe5ea59f9a968a613c5f546d600b56d309
rodent <rodent@pkgsrc.org>
2013-06-30 20:16:15 +0000

    Updated to latest release (before the rewrite). No NEWS/ChangeLog. Replaced LOCALBASE with PREFIX and fixed SUBST_SED command. Cleaned up buildlink3.mk

--- devel/compiz-bcop/buildlink3.mk
+++ devel/compiz-bcop/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/04/06 13:46:33 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/06/30 20:16:15 rodent Exp $
 
 BUILDLINK_TREE+=	compiz-bcop
 
 .if !defined(COMPIZ_BCOP_BUILDLINK3_MK)
-
 COMPIZ_BCOP_BUILDLINK3_MK:=
-BUILDLINK_DEPMETHOD.compiz-bcop?=	build
 
 BUILDLINK_API_DEPENDS.compiz-bcop+=	compiz-bcop>=0.6.0
 BUILDLINK_PKGSRCDIR.compiz-bcop?=	../../devel/compiz-bcop
+
+BUILDLINK_DEPMETHOD.compiz-bcop?=	build
+
 .endif # COMPIZ_BCOP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-compiz-bcop
