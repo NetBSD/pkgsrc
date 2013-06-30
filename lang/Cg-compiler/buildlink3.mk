52bdadcb5aeb22bf141289b0db9533df22ae10f0
rodent <rodent@pkgsrc.org>
2013-06-30 03:14:43 +0000

    Updated Cg-compiler to latest release, 3.1 from April 2012. This provides a more useful package than the previous version, complete with examples and handling distfiles for the i386 and amd64 architectures. This was originally in pkgsrc-wip and cleaned up for pkgsrc HEAD. Resolved PR/46914 Not a complete ChangeLog since the version in pkgsrc, but here's what could be scraped from HOMEPAGE:
    The April 2012 version of Cg 3.1 added these improvements:
    
        Improved GLSL support for clip semantics
        Fixed a runtime bug concerning gp4 and gp5 geometry programs
        Various documentation updates
    
    The February 2012 version of Cg 3.1 added these improvements:
    
        Added Cg language support for uniform buffers
        Added OpenGL Uni\ufb01ed Buffer Object (UBO) support for buffers
        Added OpenGL GLSL version 110 and 120 translation support
        New tessellation examples added
        New uniform buffer examples added
        VC10 projects added for examples
    
    The February 2011 version of Cg 3.0 added these improvements:
    
        Improved DX11 tessellation support
        Resolved an issue with nearly identical user defined types
        Resolved an issue with default values from unreferenced uniform parameters
        Support setting matrices beyond 96 float constants in the vp30 profile
        Application supplied compilation options now override those set by cgGLSetOptimalOptions
        Improved support for 'const' variables in the GLSL profiles
        Added sampler state documentation

--- lang/Cg-compiler/buildlink3.mk
+++ lang/Cg-compiler/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2012/05/07 01:53:39 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2013/06/30 03:14:43 rodent Exp $
 
 BUILDLINK_TREE+=	Cg-compiler
 
 .if !defined(CG_COMPILER_BUILDLINK3_MK)
 CG_COMPILER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.Cg-compiler+=		Cg-compiler>=1.2.1
-BUILDLINK_ABI_DEPENDS.Cg-compiler+=	Cg-compiler>=1.3beta2nb1
+BUILDLINK_API_DEPENDS.Cg-compiler+=	Cg-compiler>=3.1
 BUILDLINK_PKGSRCDIR.Cg-compiler?=	../../lang/Cg-compiler
-.endif # CG_COMPILER_BUILDLINK3_MK
+.endif	# CG_COMPILER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-Cg-compiler
