c99e23e6ec397677f8462646783d28cd9766e90b
rodent <rodent@pkgsrc.org>
2013-06-30 20:30:09 +0000

    Update to last release before the rewrite. Added options framework for inotify (enabled by default), gnome, kde{3,4}, and fuse PKG_OPTIONS. PLIST is handled accordingly. USE_TOOLS+=xgettext msgmerge; Moved PKG_OPTIONS in Makefile to options.mk. Fix DLLIBS using BUILDLINK_TRANSFORM. Buildlink pango (build), dlopen, glib2, libxml2, libXext, and libX11. From NEWS:
    Release 0.8.8 (2011-03-30 Danny Baumann <dannybaumann@web.de>)
    ==============================================================
    Maintenance release.
    
    Various focus and stacking fixes.
    
    Removed usages of deprecated GTK API.
    
    Fixed drawing of switcher background with KDE4 window decorator.
    
    Release 0.8.6 (2010-03-28 Danny Baumann <dannybaumann@web.de>)
    ==============================================================
    Maintenance release.
    
    Various focus and window placement fixes.
    
    Fixed handling of windows that have a (server-drawn) border.
    
    Fixed handling of window icons that have a colour depth of 1 bit.
    
    Added KDE 4.4 support to KDE4 window decorator.

---
+++ wm/compiz/options.mk
@@ -0,0 +1,60 @@
+# $NetBSD: options.mk,v 1.1 2013/06/30 20:30:09 rodent Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.compiz
+PKG_SUPPORTED_OPTIONS=	fuse gnome inotify kde3 kde4
+PKG_SUGGESTED_OPTIONS+=	inotify
+PLIST_VARS+=		gnome inotify kde kde3
+
+.include "../../mk/bsd.options.mk"
+
+# XXX configure needs fuse.pc, which isn't in NetBSD's base for some reason
+.if !empty(PKG_OPTIONS:Mfuse)
+.include "../../filesystems/fuse/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-fuse
+.else
+CONFIGURE_ARGS+=	--disable-fuse
+.endif
+
+.if !empty(PKG_OPTIONS:Mgnome)
+CONFIGURE_ARGS+=	--enable-librsvg
+PLIST.gnome=		yes
+BUILDLINK_DEPMETHOD.gnome-desktop=	build
+.include "../../x11/gnome-desktop/buildlink3.mk"
+BUILDLINK_DEPMETHOD.gnome-control-center=	build
+.include "../../x11/gnome-control-center/buildlink3.mk"
+BUILDLINK_DEPMETHOD.metacity=	build
+.include "../../wm/metacity/buildlink3.mk"
+BUILDLINK_DEPMETHOD.librsvg=	build
+.include "../../graphics/librsvg/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-gnome --disable-gnome-keybindings
+.endif
+
+.if !empty(PKG_OPTIONS:Minotify)
+.include "../../devel/libinotify/buildlink3.mk"
+PLIST.inotify=		yes
+.else
+CONFIGURE_ARGS+=	--disable-inotify
+.endif
+
+.if !empty(PKG_OPTIONS:Mkde3)
+# XXX This option is broken on NetBSD 6.0.1/amd64 + pkgsrc-current
+.include "../../x11/kdebase3/buildlink3.mk"
+.include "../../x11/kdelibs3/buildlink3.mk"
+.include "../../sysutils/dbus-qt3/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-kconfig
+PLIST.kde=		yes
+PLIST.kde3=		yes
+.else
+CONFIGURE_ARGS+=	--disable-kde --disable-kconfig
+.endif
+
+.if !empty(PKG_OPTIONS:Mkde4)
+# XXX This option is broken on NetBSD 6.0.1/amd64 + pkgsrc-current
+.include "../../x11/kde-workspace4/buildlink3.mk"
+# XXX Test to see which PLIST entries are KDE{3,4}-specific, besides kconfig.
+PLIST.kde=		yes
+#PLIST.kde4=		yes
+.else
+CONFIGURE_ARGS+=	--disable-kde4
+.endif
