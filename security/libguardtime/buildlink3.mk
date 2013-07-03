f8f4a83406488e05a3b2a6ed65207798e982d33b
jperkin <jperkin@pkgsrc.org>
2013-07-03 13:33:50 +0000

    Import libguardtime version 0.3.11 into pkgsrc.
    The GuardTime Client SDK for C is intended for software developers who
    want to integrate GuardTime Keyless Signature Service (KSS) into their
    C and C++ based applications.
    
    See http://www.guardtime.com/ for more information.

---
+++ security/libguardtime/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/07/03 13:33:50 jperkin Exp $
+
+BUILDLINK_TREE+=	libguardtime
+
+.if !defined(LIBGUARDTIME_BUILDLINK3_MK)
+LIBGUARDTIME_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libguardtime+=	libguardtime>=0.3.11
+BUILDLINK_ABI_DEPENDS.libguardtime+=	libguardtime>=0.3.11
+BUILDLINK_PKGSRCDIR.libguardtime?=	../../security/libguardtime
+
+.include "../../security/openssl/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
+.endif
+
+BUILDLINK_TREE+=	-libguardtime
