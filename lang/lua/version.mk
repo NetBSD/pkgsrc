83a9dad1bf1e51bb5b487106b3d462ffacef61c2
adam <adam@pkgsrc.org>
2013-07-04 18:04:42 +0000

    Lua 5.2 was released on 16 Dec 2011. Its main new features are yieldable pcall and metamethods, new lexical scheme for globals, ephemeron tables, new library for bitwise operations, light C functions, emergency garbage collector, goto statement, and finalizers for tables.

--- lang/lua/version.mk
+++ lang/lua/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.2 2012/03/12 15:49:27 wiz Exp $
+# $NetBSD: version.mk,v 1.3 2013/07/04 18:04:42 adam Exp $
 
-LUA_VERSION=	5.1.5
+LUA_VERSION=	5.2.2
 
 LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
 LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
