dd3be8ef705a73a9cb69d7f536bc4a6c42ee628b
wiz <wiz@pkgsrc.org>
2013-07-04 13:44:07 +0000

    Fix buildlink3 file.

--- misc/goffice0.10/buildlink3.mk
+++ misc/goffice0.10/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/06/06 12:53:46 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/07/04 13:44:07 wiz Exp $
 
-BUILDLINK_TREE+=	goffice0.8
+BUILDLINK_TREE+=	goffice0.10
 
-.if !defined(GOFFICE0.8_BUILDLINK3_MK)
-GOFFICE0.8_BUILDLINK3_MK:=
+.if !defined(GOFFICE0.10_BUILDLINK3_MK)
+GOFFICE0.10_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.goffice0.8+=	goffice0.10>=0.10.0
-BUILDLINK_ABI_DEPENDS.goffice0.8+=	goffice0.10>=0.10.2nb3
-BUILDLINK_PKGSRCDIR.goffice0.8?=	../../misc/goffice0.10
+BUILDLINK_API_DEPENDS.goffice0.10+=	goffice0.10>=0.10.0
+BUILDLINK_ABI_DEPENDS.goffice0.10+=	goffice0.10>=0.10.2nb3
+BUILDLINK_PKGSRCDIR.goffice0.10?=	../../misc/goffice0.10
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/libgsf/buildlink3.mk"
@@ -17,6 +17,6 @@ BUILDLINK_PKGSRCDIR.goffice0.8?=	../../misc/goffice0.10
 .include "../../graphics/lasem/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
-.endif # GOFFICE0.8_BUILDLINK3_MK
+.endif # GOFFICE0.10_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-goffice0.8
+BUILDLINK_TREE+=	-goffice0.10
