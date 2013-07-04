3c027b0c291fbb170c5cf15234f3c3cf0c35c293
adam <adam@pkgsrc.org>
2013-07-04 18:04:42 +0000

    Lua 5.2 was released on 16 Dec 2011. Its main new features are yieldable pcall and metamethods, new lexical scheme for globals, ephemeron tables, new library for bitwise operations, light C functions, emergency garbage collector, goto statement, and finalizers for tables.

--- lang/lua/buildlink3.mk
+++ lang/lua/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2011/01/19 13:46:13 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2013/07/04 18:04:42 adam Exp $
 
 BUILDLINK_TREE+=	lua
 
 .if !defined(LUA_BUILDLINK3_MK)
 LUA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lua+=	lua>=5.1.1
-BUILDLINK_ABI_DEPENDS.lua+=	lua>=5.1.4nb1
+BUILDLINK_API_DEPENDS.lua+=	lua>=5.2.0
+BUILDLINK_ABI_DEPENDS.lua+=	lua>=5.2.0
 BUILDLINK_PKGSRCDIR.lua?=	../../lang/lua
 
 .include "../../devel/readline/buildlink3.mk"
