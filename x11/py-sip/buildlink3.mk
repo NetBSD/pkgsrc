bb8a4bee74974beefc844796a9634c3a09021809
wiz <wiz@pkgsrc.org>
2013-07-04 17:03:02 +0000

    Update to 4.14.7:
    v4.14.7 16th June 2013
      - The internal API version is increased to 10.0 requiring the regeneration of
        all modules.
      - Added the /Sequence/ function annotation.
      - %ConvertFromTypeCode can now be specified for classes.
      - Added sipEnableAutoconversion() to the C API.
      - Added sipSetDestroyOnExit() to the C API.
      - Bug fixes.
    
    Bump coming (for first point).

--- x11/py-sip/buildlink3.mk
+++ x11/py-sip/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2012/10/10 16:52:17 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2013/07/04 17:03:02 wiz Exp $
 
 BUILDLINK_TREE+=	py-sip
 
@@ -7,7 +7,7 @@ PY_SIP_BUILDLINK3_MK:=
 
 .include "../../lang/python/pyversion.mk"
 
-BUILDLINK_API_DEPENDS.py-sip+=	${PYPKGPREFIX}-sip>=4.14
+BUILDLINK_API_DEPENDS.py-sip+=	${PYPKGPREFIX}-sip>=4.14.7
 BUILDLINK_PKGSRCDIR.py-sip?=	../../x11/py-sip
 .endif # PY_SIP_BUILDLINK3_MK
 
