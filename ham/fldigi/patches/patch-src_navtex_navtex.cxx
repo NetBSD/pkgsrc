423ec26d6d5f98a65d78b1472cbdeb5a0c1f22b3
joerg <joerg@pkgsrc.org>
2013-07-05 21:34:11 +0000

    operator bool doesn't equal good(), but !bad(). This makes a difference when it comes to handling EOF. Adjust.

--- ham/fldigi/patches/patch-src_navtex_navtex.cxx
+++ ham/fldigi/patches/patch-src_navtex_navtex.cxx
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_navtex_navtex.cxx,v 1.1 2013/05/06 14:49:32 joerg Exp $
+$NetBSD: patch-src_navtex_navtex.cxx,v 1.2 2013/07/05 21:34:12 joerg Exp $
 
 --- src/navtex/navtex.cxx.orig	2013-05-03 14:31:34.000000000 +0000
 +++ src/navtex/navtex.cxx
@@ -7,13 +7,13 @@ $NetBSD: patch-src_navtex_navtex.cxx,v 1.1 2013/05/06 14:49:32 joerg Exp $
  	std::stringstream sstrm( parsed_str );
  	sstrm >> ref ;
 -	return sstrm ;
-+	return sstrm.good();
++	return !sstrm.bad();
  }
  
  static bool read_until_delim( std::istream & istrm, std::string & ref )
  {
 -	return std::getline( istrm, ref, delim );
-+	return std::getline( istrm, ref, delim ).good();
++	return !std::getline( istrm, ref, delim ).bad();
  }
  
  class NavtexRecord
