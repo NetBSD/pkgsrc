07ac4c367880a52d83b87f930362b80532e0b944
joerg <joerg@pkgsrc.org>
2013-07-05 21:34:11 +0000

    operator bool doesn't equal good(), but !bad(). This makes a difference when it comes to handling EOF. Adjust.

--- net/synergy/patches/patch-src_lib_server_CConfig.cpp
+++ net/synergy/patches/patch-src_lib_server_CConfig.cpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_lib_server_CConfig.cpp,v 1.1 2013/05/11 21:40:14 joerg Exp $
+$NetBSD: patch-src_lib_server_CConfig.cpp,v 1.2 2013/07/05 21:34:12 joerg Exp $
 
 --- src/lib/server/CConfig.cpp.orig	2011-01-21 03:51:35.000000000 +0000
 +++ src/lib/server/CConfig.cpp
@@ -10,7 +10,7 @@ $NetBSD: patch-src_lib_server_CConfig.cpp,v 1.1 2013/05/11 21:40:14 joerg Exp $
 +CConfigReadContext::operator bool() const
  {
 -	return m_stream;
-+	return m_stream.good();
++	return !m_stream.bad();
  }
  
  bool
