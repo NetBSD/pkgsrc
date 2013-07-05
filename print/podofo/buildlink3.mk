248f2dd2f7090646171dc9d99fd42e9790f7a8d1
joerg <joerg@pkgsrc.org>
2013-07-05 21:12:45 +0000

    Update to Podofo 0.9.2 for various bug fixes and improved encryption support. Additionally, build shared libraries.

--- print/podofo/buildlink3.mk
+++ print/podofo/buildlink3.mk
@@ -1,14 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2013/01/26 21:36:44 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2013/07/05 21:12:45 joerg Exp $
 
 BUILDLINK_TREE+=	podofo
 
 .if !defined(PODOFO_BUILDLINK3_MK)
 PODOFO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.podofo+=	podofo>=0.7.0
-BUILDLINK_ABI_DEPENDS.podofo+=	podofo>=0.9.1nb3
+BUILDLINK_API_DEPENDS.podofo+=	podofo>=0.9.2
 BUILDLINK_PKGSRCDIR.podofo?=	../../print/podofo
-BUILDLINK_DEPMETHOD.podofo?=	build
 
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
