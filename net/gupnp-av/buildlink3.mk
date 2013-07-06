705a0712b5df90a4cd442440d61a2c50b0af106c
ryoon <ryoon@pkgsrc.org>
2013-07-06 16:20:52 +0000

    Update to 0.12.2
    * Enable gobject-introspection support.
    
    Changelog:
    0.12.2
    ======
    
    Changes since 0.12.1:
    - Fix VAPI generation if GObject-introspection version is >= 1.36.
    - Fix vala bindings for LastChangeParser.parse_last_change.
    - Small improvement to resource matching.
    - Fix DIDLLiteContainer.get_total_deleted_child_count return value.
    - Fix compiling with -Wall -Werror.
    - Fix gtk-doc warnings.
    
    All contributors to this release:
      Jens Georg <mail@jensge.org>
      Ludovic Ferrandis <ludovic.ferrandis@intel.com>
    
    0.12.1
    ======
    
    Changes since 0.12.0:
    - Fix compiler warnings.
    - Update .gitignore.
    
    All contributors to this release:
      Andreas Henriksson <andreas@fatal.se>
      Krzesimir Nowak <qdlacz@gmail.com>

--- net/gupnp-av/buildlink3.mk
+++ net/gupnp-av/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.16 2013/05/24 18:23:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2013/07/06 16:20:52 ryoon Exp $
 
 BUILDLINK_TREE+=	gupnp-av
 
 .if !defined(GUPNP_AV_BUILDLINK3_MK)
 GUPNP_AV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gupnp-av+=	gupnp-av>=0.10.3
-BUILDLINK_ABI_DEPENDS.gupnp-av+=	gupnp-av>=0.12.0nb2
+BUILDLINK_API_DEPENDS.gupnp-av+=	gupnp-av>=0.12.2
+BUILDLINK_ABI_DEPENDS.gupnp-av+=	gupnp-av>=0.12.2
 BUILDLINK_PKGSRCDIR.gupnp-av?=	../../net/gupnp-av
 
 .include "../../net/gupnp/buildlink3.mk"
