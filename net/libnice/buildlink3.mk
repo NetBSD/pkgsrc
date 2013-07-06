bd916dc7d2bead26be461b4a3e55a717160efe35
ryoon <ryoon@pkgsrc.org>
2013-07-06 16:27:35 +0000

    Recursive revbump from net/gupnp.

--- net/libnice/buildlink3.mk
+++ net/libnice/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2013/05/24 18:23:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2013/07/06 16:27:35 ryoon Exp $
 
 BUILDLINK_TREE+=	libnice
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libnice
 LIBNICE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libnice+=	libnice>=0.0.9
-BUILDLINK_ABI_DEPENDS.libnice+=	libnice>=0.1.2nb7
+BUILDLINK_ABI_DEPENDS.libnice+=	libnice>=0.1.4nb1
 BUILDLINK_PKGSRCDIR.libnice?=	../../net/libnice
 
 .include "../../devel/glib2/buildlink3.mk"
