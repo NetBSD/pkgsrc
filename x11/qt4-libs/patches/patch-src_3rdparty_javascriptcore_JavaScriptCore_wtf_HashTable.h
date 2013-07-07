84d47a3f96735663ae69513bdaa2463771f86dc1
adam <adam@pkgsrc.org>
2013-07-07 18:13:56 +0000

    Qt 4.8.5 provides overall close to 400 improvements, for example: 35 fixes to Qt Core module 65 fixes to Qt Gui module 21 Mac OS X specific fixes 15 Windows specific fixes 8 Linux/X11 specific fixes Webkit upgrade to 2.2.4 (minor improvements) Change all shmget calls to user-only memory (security) Several important fixes for the VxWorks port in the Qt Enterprise version

--- x11/qt4-libs/patches/patch-src_3rdparty_javascriptcore_JavaScriptCore_wtf_HashTable.h
+++ x11/qt4-libs/patches/patch-src_3rdparty_javascriptcore_JavaScriptCore_wtf_HashTable.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_3rdparty_javascriptcore_JavaScriptCore_wtf_HashTable.h,v 1.1 2013/05/09 14:07:08 joerg Exp $
+$NetBSD: patch-src_3rdparty_javascriptcore_JavaScriptCore_wtf_HashTable.h,v 1.2 2013/07/07 18:13:56 adam Exp $
 
 --- src/3rdparty/javascriptcore/JavaScriptCore/wtf/HashTable.h.orig	2013-04-29 18:21:34.000000000 +0000
 +++ src/3rdparty/javascriptcore/JavaScriptCore/wtf/HashTable.h
@@ -7,7 +7,7 @@ $NetBSD: patch-src_3rdparty_javascriptcore_JavaScriptCore_wtf_HashTable.h,v 1.1 
      using std::swap;
  
 -#if !COMPILER(MSVC) && !OS(QNX)
-+#if !COMPILER(MSVC) && !OS(QNX) && !defined(_LIBCPP_VERSION)
++#if !COMPILER(MSVC) && !OS(QNX) && !defined(_LIBCPP_VERSION) && !defined(__GXX_EXPERIMENTAL_CXX0X__)
      // The Dinkumware C++ library (used by MSVC and QNX) has a swap for pairs defined.
  
      // swap pairs by component, in case of pair members that specialize swap
--- x11/qt4-libs/patches/patch-src_3rdparty_webkit_Source_JavaScriptCore_wtf_Atomics.h
+++ x11/qt4-libs/patches/patch-src_3rdparty_webkit_Source_JavaScriptCore_wtf_Atomics.h
@@ -1,36 +1,33 @@
-$NetBSD: patch-src_3rdparty_webkit_Source_JavaScriptCore_wtf_Atomics.h,v 1.1 2013/05/09 14:07:08 joerg Exp $
+$NetBSD: patch-src_3rdparty_webkit_Source_JavaScriptCore_wtf_Atomics.h,v 1.2 2013/07/07 18:13:56 adam Exp $
 
---- src/3rdparty/webkit/Source/JavaScriptCore/wtf/Atomics.h.orig	2012-11-23 10:10:05.000000000 +0000
+--- src/3rdparty/webkit/Source/JavaScriptCore/wtf/Atomics.h.orig	2013-06-07 05:16:54.000000000 +0000
 +++ src/3rdparty/webkit/Source/JavaScriptCore/wtf/Atomics.h
-@@ -61,7 +61,9 @@
- 
- #include "Platform.h"
- 
--#if OS(WINDOWS)
-+#if __cplusplus >= 201103L
+@@ -69,6 +69,8 @@
+ #include <cutils/atomic.h>
+ #elif OS(QNX)
+ #include <atomic.h>
++#elif __cplusplus >= 201103L
 +#include <atomic>
-+#elif OS(WINDOWS)
- #include <windows.h>
- #elif OS(DARWIN)
- #include <libkern/OSAtomic.h>
-@@ -79,7 +82,11 @@
+ #elif COMPILER(GCC) && !OS(SYMBIAN)
+ #if (__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 2))
+ #include <ext/atomicity.h>
+@@ -106,6 +108,11 @@ inline int atomicDecrement(int volatile*
+ inline int atomicIncrement(int volatile* addend) { return (int) atomic_add_value((unsigned int volatile*)addend, 1); }
+ inline int atomicDecrement(int volatile* addend) { return (int) atomic_sub_value((unsigned int volatile*)addend, 1); }
  
- namespace WTF {
- 
--#if OS(WINDOWS)
-+#if __cplusplus >= 201103L
++#elif __cplusplus >= 201103L
 +#define WTF_USE_LOCKFREE_THREADSAFEREFCOUNTED 1
 +inline int atomicIncrement(std::atomic<int> * addend) { return *addend++; }
 +inline int atomicDecrement(std::atomic<int> * addend) { return *addend--; }
-+#elif OS(WINDOWS)
++
+ #elif COMPILER(GCC) && !CPU(SPARC64) && !OS(SYMBIAN) // sizeof(_Atomic_word) != sizeof(int) on sparc64 gcc
  #define WTF_USE_LOCKFREE_THREADSAFEREFCOUNTED 1
  
- #if COMPILER(MINGW) || COMPILER(MSVC7_OR_LOWER) || OS(WINCE)
-@@ -114,9 +126,15 @@ inline int atomicDecrement(int volatile*
+@@ -114,9 +121,15 @@ inline int atomicDecrement(int volatile*
  
  #endif
  
-+#if __cplusplus >= 201103L
++#if !OS(DARWIN) && __cplusplus >= 201103L
 +typedef std::atomic<int> atomic_int;
 +#else
 +typedef int volatile atomic_int;
