e448e19e7ef71e9dab9b192764e543b5c32957f4
rodent <rodent@pkgsrc.org>
2013-07-07 15:07:48 +0000

    Import gtksourceviewmm-3.2.0 as x11/gtksourceviewmm.
    gtksourceviewmm is a C++ wrapper for the gtksourceview widget library. It offers
    all the power of gtksourceview with an interface familiar to C++ developers,
    including users of the gtkmm library.

---
+++ x11/gtksourceviewmm/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/07/07 15:07:48 rodent Exp $
+
+BUILDLINK_TREE+=	gtksourceviewmm
+
+.if !defined(GTKSOURCEVIEWMM_BUILDLINK3_MK)
+GTKSOURCEVIEWMM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gtksourceviewmm+=	gtksourceviewmm>=3.2.0
+BUILDLINK_PKGSRCDIR.gtksourceviewmm?=	../../x11/gtksourceviewmm
+
+.include "../../devel/atkmm/buildlink3.mk"
+.include "../../graphics/cairomm/buildlink3.mk"
+.include "../../devel/glibmm/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/pangomm/buildlink3.mk"
+.include "../../devel/libsigc++/buildlink3.mk"
+.include "../../x11/gtkmm3/buildlink3.mk"
+.include "../../x11/gtksourceview3/buildlink3.mk"
+.endif	# GTKSOURCEVIEWMM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gtksourceviewmm
