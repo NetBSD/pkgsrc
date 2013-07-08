0e56656cb4e627b56f132422da8334bc2cf6d5a8
taca <taca@pkgsrc.org>
2013-07-08 13:16:21 +0000

    Correct condition for NetBSD 6.0 and later to fix build problem on NetBSD 6.0.  Reported by J«Órn Clausen as PR pkg/48029.

---
+++ lang/php54/patches/patch-main_streams_cast.c
@@ -0,0 +1,15 @@
+$NetBSD: patch-main_streams_cast.c,v 1.3 2013/07/08 13:16:21 taca Exp $
+
+* For NetBSD 6.0+, __NetBSD_Version__ should includes 600000000.
+
+--- main/streams/cast.c.orig	2013-07-03 06:10:53.000000000 +0000
++++ main/streams/cast.c
+@@ -33,7 +33,7 @@
+ #if defined(HAVE_FUNOPEN) && !defined(HAVE_FOPENCOOKIE)
+ 
+ /* NetBSD 6.0+ uses off_t instead of fpos_t in funopen */
+-# if defined(__NetBSD__) && (__NetBSD_Version__ > 600000000)
++# if defined(__NetBSD__) && (__NetBSD_Version__ >= 600000000)
+ #  define PHP_FPOS_T off_t
+ # else
+ #  define PHP_FPOS_T fpos_t
