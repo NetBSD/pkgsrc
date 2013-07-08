48bf009b041d04fda97b6b71239d0eeb70c6d433
jperkin <jperkin@pkgsrc.org>
2013-07-08 20:18:52 +0000

    Split the extract phase into fetch and extract, to ensure that distfiles can be fetched correctly, keeping in sync with devel/gmp inplace.mk

--- math/mpfr/inplace.mk
+++ math/mpfr/inplace.mk
@@ -1,12 +1,18 @@
-# $NetBSD: inplace.mk,v 1.2 2012/04/22 15:13:26 wiz Exp $
+# $NetBSD: inplace.mk,v 1.3 2013/07/08 20:18:52 jperkin Exp $
 #
 # Include this file to extract math/mpfr source into the WRKSRC of
 # another package. This is to be used by GCC packages to avoid the
 # numerous dependencies math/mpfr has.
 
+post-fetch: fetch-inplace-mpfr
+
 post-extract: extract-inplace-mpfr
+
+fetch-inplace-mpfr:
+	(cd ../../math/mpfr && ${MAKE} WRKDIR=${WRKSRC}/.devel.mpfr EXTRACT_DIR=${WRKSRC} \
+		WRKSRC='$${EXTRACT_DIR}/$${DISTNAME}' SKIP_DEPENDS=YES checksum clean)
 
 extract-inplace-mpfr:
 	(cd ../../math/mpfr && ${MAKE} WRKDIR=${WRKSRC}/.devel.mpfr EXTRACT_DIR=${WRKSRC} \
-		WRKSRC='$${EXTRACT_DIR}/$${DISTNAME}' SKIP_DEPENDS=YES fetch patch clean)
+		WRKSRC='$${EXTRACT_DIR}/$${DISTNAME}' SKIP_DEPENDS=YES patch clean)
 	${MV} ${WRKSRC}/mpfr-* ${WRKSRC}/mpfr
