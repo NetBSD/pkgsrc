d14e468337e9a6c4eb3f32a7e5e729343533cf9e
marino <marino@pkgsrc.org>
2013-07-09 10:16:02 +0000

    devel/gps: Upgrade GNAT Programming Studio to version 5.2.1
    This is a significant upgrade over version 5.0.1 which is currently in
    pkgsrc, representing approximately two years of work.  The latest online
    documentation can be browsed here:
    
       http://docs.adacore.com/gps-docs/users_guide/_build/html/
    
    Changes to the package itself include:
    * python now works and is a default option
    * readline support is now an option and is default
    * Multiple job support enable
    * Documentation now generated by sphinx
    
    Also the problem described in PR#47824 no longer occurs, so this PR will
    be closed.

--- devel/gps/patches/patch-gnatlib-gnat_src-mlib-utl.adb
+++ devel/gps/patches/patch-gnatlib-gnat_src-mlib-utl.adb
@@ -1,4 +1,6 @@
-$NetBSD: patch-ak,v 1.1 2011/10/14 17:22:06 marino Exp $
+$NetBSD: patch-gnatlib-gnat_src-mlib-utl.adb,v 1.1 2013/07/09 10:16:02 marino Exp $
+
+Use unique ada executable rather than generic gcc
 
 --- gnatlib/gnat_src/mlib-utl.adb.orig	2010-02-14 02:40:00.000000000 +0100
 +++ gnatlib/gnat_src/mlib-utl.adb	2011-10-09 04:11:21.000000000 +0200
@@ -7,7 +9,7 @@ $NetBSD: patch-ak,v 1.1 2011/10/14 17:22:06 marino Exp $
           if Gcc_Exec = null then
              if Gcc_Name = null then
 -               Gcc_Name := Osint.Program_Name ("gcc", "gnatmake");
-+               Gcc_Name := Osint.Program_Name ("gnatgcc", "gnatmake");
++               Gcc_Name := Osint.Program_Name ("ada", "gnatmake");
              end if;
  
              Gcc_Exec := Locate_Exec_On_Path (Gcc_Name.all);
