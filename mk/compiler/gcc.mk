189a4afbe75dfe183a135d0e56752073b63fc1a8
jperkin <jperkin@pkgsrc.org>
2013-07-09 10:13:43 +0000

    Match against CC_VERSION rather than _GCC_VERSION, the latter is not defined in the USE_PKGSRC_GCC case.

--- mk/compiler/gcc.mk
+++ mk/compiler/gcc.mk
@@ -1,4 +1,4 @@
-# $NetBSD: gcc.mk,v 1.141 2013/06/15 22:32:18 wiz Exp $
+# $NetBSD: gcc.mk,v 1.142 2013/07/09 10:13:43 jperkin Exp $
 #
 # This is the compiler definition for the GNU Compiler Collection.
 #
@@ -794,9 +794,9 @@ PREPEND_PATH+=	${_GCC_DIR}/bin
 .if (defined(_USE_GCC_SHLIB) && !empty(_USE_GCC_SHLIB:M[Yy][Ee][Ss])) && !empty(USE_PKGSRC_GCC_RUNTIME:M[Yy][Ee][Ss])
 #  Special case packages which are themselves a dependency of gcc runtime.
 .  if empty(PKGPATH:Mdevel/libtool-base) && empty(PKGPATH:Mdevel/binutils) && empty(PKGPATH:Mlang/gcc??)
-.    if !empty(_GCC_VERSION:M4.7*)
+.    if !empty(CC_VERSION:Mgcc-4.7*)
 .      include "../../lang/gcc47-libs/buildlink3.mk"
-.    elif !empty(_GCC_VERSION:M4.8*)
+.    elif !empty(CC_VERSION:Mgcc-4.8*)
 .      include "../../lang/gcc48-libs/buildlink3.mk"
 .    else
 PKG_FAIL_REASON=	"No USE_PKGSRC_GCC_RUNTIME support for ${CC_VERSION}"
