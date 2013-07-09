aecbf8569355abf5e9cbcc3a286059b09ffa21b5
tsutsui <tsutsui@pkgsrc.org>
2013-07-09 15:49:43 +0000

    Disable py-numpy option (which requires lang/g95 support) by default until lang/g95 issue is resolved. Also bump PKGREVISION for the option default change.
    "Please do that" from wiz@ in PR/48023.

--- x11/py-gtk2/options.mk
+++ x11/py-gtk2/options.mk
@@ -1,12 +1,16 @@
-# $NetBSD: options.mk,v 1.2 2012/11/30 12:38:26 ryoon Exp $
+# $NetBSD: options.mk,v 1.3 2013/07/09 15:49:43 tsutsui Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-gtk2
 PKG_SUPPORTED_OPTIONS=	py-numpy
+PKG_SUGGESTED_OPTIONS=
+# Enable py-numpy (which requires fortran support) by default on
+# platforms supported by lang/g95.
 .if (!empty(MACHINE_ARCH:Mi386) || !empty(MACHINE_ARCH:Mx86_64) || \
 	!empty(MACHINE_ARCH:Mia64) || !empty(MACHINE_ARCH:Mpowerpc*) || \
 	!empty(MACHINE_ARCH:Mhppa) || !empty(MACHINE_ARCH:Msparc*) || \
 	!empty(MACHINE_ARCH:Malpha) || !empty(MACHINE_ARCH:Mmips*))
-PKG_SUGGESTED_OPTIONS=	py-numpy
+# ...but disable it until lang/g95 issue is resolved.
+#PKG_SUGGESTED_OPTIONS+=	py-numpy
 .endif
 
 .include "../../mk/bsd.options.mk"
