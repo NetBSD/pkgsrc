501b83ccb57ffcf12e82f3c4abba3c2b84fbdf49
tsutsui <tsutsui@pkgsrc.org>
2013-07-09 15:46:45 +0000

    Disable fftw-fortran option by default until lang/g95 issue is resolved. Also bump PKGREVISION for the option default change.
    "Please do that" from wiz@ in PR/48023.

--- math/fftw/options.mk
+++ math/fftw/options.mk
@@ -1,13 +1,15 @@
-# $NetBSD: options.mk,v 1.1 2013/06/29 18:59:43 tsutsui Exp $
+# $NetBSD: options.mk,v 1.2 2013/07/09 15:46:45 tsutsui Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fftw
 PKG_SUPPORTED_OPTIONS=	fftw-fortran
+PKG_SUGGESTED_OPTIONS=
 # Enable fortran support by default on platforms supported by lang/g95.
 .if (!empty(MACHINE_ARCH:Mi386) || !empty(MACHINE_ARCH:Mx86_64) || \
 	!empty(MACHINE_ARCH:Mia64) || !empty(MACHINE_ARCH:Mpowerpc*) || \
 	!empty(MACHINE_ARCH:Mhppa) || !empty(MACHINE_ARCH:Msparc*) || \
 	!empty(MACHINE_ARCH:Malpha) || !empty(MACHINE_ARCH:Mmips*))
-PKG_SUGGESTED_OPTIONS=	fftw-fortran
+# ...but disable it until lang/g95 issue is resolved.
+#PKG_SUGGESTED_OPTIONS+=	fftw-fortran
 .endif
 
 .include "../../mk/bsd.options.mk"
