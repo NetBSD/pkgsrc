9a46f7bf53288371a8888e44ecc3041adc4c77ba
wiz <wiz@pkgsrc.org>
2013-07-11 11:44:07 +0000

    Add option for toolbar mode.
    Thomas E. Dickey says it has hard-to-fix layout issues and slows
    the startup, so turn the option default off.
    
    Bump PKGREVISION for the default change from on to off.

--- x11/xterm/options.mk
+++ x11/xterm/options.mk
@@ -1,20 +1,20 @@
-# $NetBSD: options.mk,v 1.13 2012/10/10 16:29:35 wiz Exp $
+# $NetBSD: options.mk,v 1.14 2013/07/11 11:44:07 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.xterm
-PKG_SUPPORTED_OPTIONS=		debug pcre freetype luit xpm
+PKG_SUPPORTED_OPTIONS=		debug pcre freetype luit xpm xterm-toolbar
 PKG_SUGGESTED_OPTIONS=		freetype luit xpm
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdebug)
-CONFIGURE_ARGS+= --enable-trace
-CONFIGURE_ARGS+= --enable-warnings
+CONFIGURE_ARGS+=	--enable-trace
+CONFIGURE_ARGS+=	--enable-warnings
 .elif defined(PKG_DEVELOPER) && !empty(PKG_DEVELOPER:M[Yy][Ee][Ss])
-CONFIGURE_ARGS+= --enable-warnings
+CONFIGURE_ARGS+=	--enable-warnings
 .endif
 
 .if !empty(PKG_OPTIONS:Mfreetype)
-CONFIGURE_ARGS+= --enable-freetype
+CONFIGURE_ARGS+=	--enable-freetype
 .  include "../../x11/libXft/buildlink3.mk"
 .else
 CONFIGURE_ARGS+= --disable-freetype
@@ -33,13 +33,19 @@ CONFIGURE_ENV+=		ac_cv_lib_pcreposix_pcreposix_regcomp=yes
 
 .if !empty(PKG_OPTIONS:Mluit)
 DEPENDS+=	luit-[0-9]*:../../x11/luit
-CONFIGURE_ARGS+= --enable-luit --enable-mini-luit
+CONFIGURE_ARGS+=	--enable-luit --enable-mini-luit
 .else
-CONFIGURE_ARGS+= --disable-luit --disable-mini-luit
+CONFIGURE_ARGS+=	--disable-luit --disable-mini-luit
 .endif
 
 .if !empty(PKG_OPTIONS:Mxpm)
 ### nothing
 .else
-CONFIGURE_ARGS+= --without-xpm
+CONFIGURE_ARGS+=	--without-xpm
+.endif
+
+.if !empty(PKG_OPTIONS:Mxterm-toolbar)
+CONFIGURE_ARGS+=	--enable-toolbar
+.else
+CONFIGURE_ARGS+=	--disable-toolbar
 .endif
