d9b8e1f0da353c468ab572c37512342e715d846d
ryoon <ryoon>
2013-07-12 12:24:10 +0000

    Update to 2.19
    * Update enigmail to 1.5.2.
    
    Changelog:
    SeaMonkey-specific changes
    
        Mark -> As Read now checks the state of all selected messages instead of only the first one's.
        Notifications for mixed content blocker have been implemented.
        A new 3rd-party cookie restriction to visited websites option has been added to the Cookies pref pane.
        The context menu Search option is now available for textareas and input fields.
        Website storage mechanisms are now available in the Data Manager (localStorage, indexedDB, etc.).
        "Open Containing Folder" is now already available during download.
        See the changes page for minor changes.
    
    Mozilla platform changes
    
        asm.js optimizations (OdinMonkey) have been enabled for major performance improvements.
        Improved WebGL rendering performance through asynchronous canvas updates.
        Plain text files displayed within the browser will now word-wrap.
        For user security, the Components object is no longer accessible from web content.
        Improved memory usage and display time when rendering images.
        The Pointer Lock API can now be used outside of fullscreen.
        CSS3 Flexbox has been implemented and enabled by default.
        The new Web Notifications API has been implemented.
        Added clipboardData API for JavaScript access to a user's clipboard.
        Support for new HTML5 <data> and <time> elements has been added.
        Fixed several stability issues.

--- www/seamonkey/patches/patch-mozilla_js_src_vm_ObjectImpl-inl.h
+++ www/seamonkey/patches/patch-mozilla_js_src_vm_ObjectImpl-inl.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-mozilla_js_src_vm_ObjectImpl-inl.h,v 1.1 2013/05/23 13:25:30 ryoon Exp $
+$NetBSD: patch-mozilla_js_src_vm_ObjectImpl-inl.h,v 1.2 2013/07/12 12:24:10 ryoon Exp $
 
---- mozilla/js/src/vm/ObjectImpl-inl.h.orig	2013-05-03 03:08:00.000000000 +0000
+--- mozilla/js/src/vm/ObjectImpl-inl.h.orig	2013-06-20 04:35:09.000000000 +0000
 +++ mozilla/js/src/vm/ObjectImpl-inl.h
-@@ -164,9 +164,9 @@ js::ObjectImpl::initializeSlotRange(uint
+@@ -181,9 +181,9 @@ js::ObjectImpl::initializeSlotRange(uint
      JSRuntime *rt = runtime();
      uint32_t offset = start;
      for (HeapSlot *sp = fixedStart; sp < fixedEnd; sp++)
@@ -14,7 +14,7 @@ $NetBSD: patch-mozilla_js_src_vm_ObjectImpl-inl.h,v 1.1 2013/05/23 13:25:30 ryoo
  }
  
  inline bool
-@@ -321,7 +321,7 @@ js::ObjectImpl::sizeOfThis() const
+@@ -353,7 +353,7 @@ ZoneOfValue(const JS::Value &value)
  js::ObjectImpl::readBarrier(ObjectImpl *obj)
  {
  #ifdef JSGC_INCREMENTAL
@@ -23,7 +23,7 @@ $NetBSD: patch-mozilla_js_src_vm_ObjectImpl-inl.h,v 1.1 2013/05/23 13:25:30 ryoo
      if (zone->needsBarrier()) {
          MOZ_ASSERT(!zone->rt->isHeapBusy());
          JSObject *tmp = obj->asObjectPtr();
-@@ -335,7 +335,7 @@ inline void
+@@ -367,7 +367,7 @@ inline void
  js::ObjectImpl::privateWriteBarrierPre(void **old)
  {
  #ifdef JSGC_INCREMENTAL
@@ -32,8 +32,8 @@ $NetBSD: patch-mozilla_js_src_vm_ObjectImpl-inl.h,v 1.1 2013/05/23 13:25:30 ryoo
      if (zone->needsBarrier()) {
          if (*old && getClass()->trace)
              getClass()->trace(zone->barrierTracer(), this->asObjectPtr());
-@@ -362,7 +362,7 @@ js::ObjectImpl::writeBarrierPre(ObjectIm
-     if (uintptr_t(obj) < 32)
+@@ -394,7 +394,7 @@ js::ObjectImpl::writeBarrierPre(ObjectIm
+     if (IsNullTaggedPointer(obj))
          return;
  
 -    Zone *zone = obj->zone();
--- www/seamonkey/patches/patch-mozilla_media_webrtc_trunk_third__party_libyuv_libyuv.gyp
+++ www/seamonkey/patches/patch-mozilla_media_webrtc_trunk_third__party_libyuv_libyuv.gyp
@@ -1,18 +1,10 @@
-$NetBSD: patch-mozilla_media_webrtc_trunk_third__party_libyuv_libyuv.gyp,v 1.1 2013/05/23 13:25:30 ryoon Exp $
+$NetBSD: patch-mozilla_media_webrtc_trunk_third__party_libyuv_libyuv.gyp,v 1.2 2013/07/12 12:24:10 ryoon Exp $
 
---- mozilla/media/webrtc/trunk/third_party/libyuv/libyuv.gyp.orig	2013-05-03 03:08:06.000000000 +0000
+--- mozilla/media/webrtc/trunk/third_party/libyuv/libyuv.gyp.orig	2013-06-20 04:35:15.000000000 +0000
 +++ mozilla/media/webrtc/trunk/third_party/libyuv/libyuv.gyp
-@@ -9,6 +9,7 @@
- {
-   'variables': {
-      'use_system_libjpeg%': 0,
-+     'yuv_disable_asm%': 0,
-   },
-   'targets': [
-     {
-@@ -50,6 +51,11 @@
-         ],
-       },
+@@ -58,6 +58,11 @@
+         }],
+       ],
        'conditions': [
 +        ['yuv_disable_asm==1', {
 +          'defines': [
