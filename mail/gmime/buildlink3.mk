e065e0ff2310f165595ca9459a99958a8d64b46e
joerg <joerg@pkgsrc.org>
2013-07-13 19:23:36 +0000

    Update to gmime-2.6.10. Changes unknown, but old version doesn't build against newer glib2 anyway.

--- mail/gmime/buildlink3.mk
+++ mail/gmime/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.19 2012/09/15 10:05:27 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2013/07/13 19:23:36 joerg Exp $
 
 BUILDLINK_TREE+=	gmime
 
 .if !defined(GMIME_BUILDLINK3_MK)
 GMIME_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gmime+=	gmime>=2.1.7
-BUILDLINK_ABI_DEPENDS.gmime+=	gmime>=2.2.25nb6
+BUILDLINK_API_DEPENDS.gmime+=	gmime>=2.6.15
 BUILDLINK_PKGSRCDIR.gmime?=	../../mail/gmime
 
 .include "../../devel/glib2/buildlink3.mk"
