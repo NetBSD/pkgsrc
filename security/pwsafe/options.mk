da22b3f54029f7a294ea14d7ead9a99a5d6d87f7
ryoon <ryoon@pkgsrc.org>
2013-07-15 02:02:17 +0000

    * .include "../../devel/readline/buildlink3.mk" with USE_GNU_READLINE=yes   are replaced with .include "../../devel/readline/buildlink3.mk", and   USE_GNU_READLINE are removed,
    * .include "../../devel/readline/buildlink3.mk" without USE_GNU_READLINE
      are replaced with .include "../../mk/readline.buildlink3.mk".

--- security/pwsafe/options.mk
+++ security/pwsafe/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2007/01/30 20:53:24 joerg Exp $
+# $NetBSD: options.mk,v 1.3 2013/07/15 02:02:28 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pwsafe
 PKG_SUPPORTED_OPTIONS=	readline x11
@@ -7,10 +7,6 @@ PKG_SUPPORTED_OPTIONS=	readline x11
 
 .if !empty(PKG_OPTIONS:Mreadline)
 CONFIGURE_ARGS+=	--with-readline
-.  if ${OPSYS} == "NetBSD"
-# The NetBSD readline implementation isn't GNU compatible
-USE_GNU_READLINE=	yes
-.  endif
 .  include "../../devel/readline/buildlink3.mk"
 .  include "../../devel/ncurses/buildlink3.mk"
 .else
