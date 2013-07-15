3800f631f5b17d7d108583e5bccd463763ec7a65
ryoon <ryoon@pkgsrc.org>
2013-07-15 15:14:29 +0000

    Bump PKGREVISION.
    * Create include/editline/history.h that is symlink to readline.h like
      NetBSD base libedit.
      This change fixes insufficient buildlinking of editline.
      So fix build of devel/elf or other editline consumer on my private
      Debian GNU/kFreeBSD pkgsrc environemnt. It should fix editline consumer
      build on other platforms.

--- devel/editline/buildlink3.mk
+++ devel/editline/buildlink3.mk
@@ -1,17 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/07/15 01:42:47 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/07/15 15:14:29 ryoon Exp $
 
 BUILDLINK_TREE+=	editline
 
 .if !defined(EDITLINE_BUILDLINK3_MK)
 EDITLINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.editline+=	editline>=3.0.20130712
+BUILDLINK_API_DEPENDS.editline+=	editline>=3.0.20130712nb1
 BUILDLINK_PKGSRCDIR.editline?=	../../devel/editline
 
 BUILDLINK_FILES.editline+=	include/history.h
 BUILDLINK_FILES.editline+=	include/readline.h
 
-BUILDLINK_FNAME_TRANSFORM.editline+=	-e 's|include/editline/readline\.h|include/readline/history.h|g'
+BUILDLINK_FNAME_TRANSFORM.editline+=	-e 's|include/editline/history\.h|include/readline/history.h|g'
 BUILDLINK_FNAME_TRANSFORM.editline+=	-e 's|include/editline/readline\.h|include/readline/readline.h|g'
 
 CHECK_BUILTIN.editline:=	yes
