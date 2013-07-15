e2b3dd53411df2bff362e879c2a5c16b7a896e1a
obache <obache>
2013-07-15 01:19:04 +0000

    Import cogl-1.14.0 as graphics/cogl.
    Cogl is a small open source library for using 3D graphics hardware for
    rendering. The API departs from the flat state machine style of OpenGL and is
    designed to make it easy to write orthogonal components that can render without
    stepping on each others toes.
    
    As well as aiming for a nice API, we think having a single library as opposed
    to an API specification like OpenGL has a few advantages too; like being
    able to paper over the inconsistencies/bugs of different OpenGL
    implementations in a centralized place, not to mention the myriad of OpenGL
    extensions. It also means we are in a better position to provide utility
    APIs that help software developers since they only need to be implemented
    once and there is no risk of inconsistency between implementations.
    
    Having other backends, besides OpenGL, such as drm, Gallium or D3D are
    options we are interested in for the future.

---
+++ graphics/cogl/options.mk
@@ -0,0 +1,41 @@
+# $NetBSD: options.mk,v 1.1 2013/07/15 01:19:04 obache Exp $
+#
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.cogl
+PKG_SUPPORTED_OPTIONS=	introspection x11
+PKG_SUGGESTED_OPTIONS=	x11
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	introspection x11
+
+.if !empty(PKG_OPTIONS:Mintrospection)
+PLIST.introspection=		yes
+BUILDLINK_API_DEPENDS.gobject-introspection+=	gobject-introspection>=0.9.5
+BUILDLINK_DEPMETHOD.gobject-introspection+=	build
+.include "../../devel/gobject-introspection/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-introspection=yes
+.else
+CONFIGURE_ARGS+=	--enable-introspection=no
+.endif
+
+.if !empty(PKG_OPTIONS:Mx11)
+PLIST.x11=		yes
+CONFIGURE_ARGS+=	--with-x
+CONFIGURE_ARGS+=	--enable-gdk-pixbuf
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXdamage/buildlink3.mk"
+.include "../../x11/libXfixes/buildlink3.mk"
+.include "../../x11/libXcomposite/buildlink3.mk"
+.include "../../x11/libXrandr/buildlink3.mk"
+.include "../../graphics/gdk-pixbuf2/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-x
+.include "../../mk/bsd.prefs.mk"
+.  if ${OPSYS} == Darwin
+PLIST.osx=		yes
+CONFIGURE_ARGS+=	--with-quartz-image
+.  endif
+.endif
