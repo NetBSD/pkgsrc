21daa4a34ba7957718e80049e1f96e696c37cc11
jperkin <jperkin@pkgsrc.org>
2013-07-17 11:00:13 +0000

    Add SunOS/x86 patchset.  This produces a package, but the resulting firefox binary does not yet work correctly.

--- www/firefox/patches/patch-ipc_glue_GeckoChildProcessHost.cpp
+++ www/firefox/patches/patch-ipc_glue_GeckoChildProcessHost.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-ipc_glue_GeckoChildProcessHost.cpp,v 1.1 2013/05/23 13:12:13 ryoon Exp $
+$NetBSD: patch-ipc_glue_GeckoChildProcessHost.cpp,v 1.2 2013/07/17 11:00:13 jperkin Exp $
 
---- ipc/glue/GeckoChildProcessHost.cpp.orig	2013-01-04 23:44:34.000000000 +0000
+--- ipc/glue/GeckoChildProcessHost.cpp.orig	2013-06-18 11:01:23.000000000 +0000
 +++ ipc/glue/GeckoChildProcessHost.cpp
 @@ -4,7 +4,13 @@
   * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -16,3 +16,39 @@ $NetBSD: patch-ipc_glue_GeckoChildProcessHost.cpp,v 1.1 2013/05/23 13:12:13 ryoo
  
  #include "base/command_line.h"
  #include "base/path_service.h"
+@@ -494,7 +500,7 @@ GeckoChildProcessHost::PerformAsyncLaunc
+   // and passing wstrings from one config to the other is unsafe.  So
+   // we split the logic here.
+ 
+-#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_BSD)
++#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_BSD) || defined(OS_SOLARIS)
+   base::environment_map newEnvVars;
+   ChildPrivileges privs = mPrivileges;
+   if (privs == base::PRIVILEGES_DEFAULT) {
+@@ -513,7 +519,7 @@ GeckoChildProcessHost::PerformAsyncLaunc
+       if (NS_SUCCEEDED(rv)) {
+         nsCString path;
+         greDir->GetNativePath(path);
+-# if defined(OS_LINUX) || defined(OS_BSD)
++# if defined(OS_LINUX) || defined(OS_BSD) || defined(OS_SOLARIS)
+ #  if defined(MOZ_WIDGET_ANDROID)
+         path += "/lib";
+ #  endif  // MOZ_WIDGET_ANDROID
+@@ -636,7 +642,7 @@ GeckoChildProcessHost::PerformAsyncLaunc
+   childArgv.push_back(pidstring);
+ 
+ #if defined(MOZ_CRASHREPORTER)
+-#  if defined(OS_LINUX) || defined(OS_BSD)
++#  if defined(OS_LINUX) || defined(OS_BSD) || defined(OS_SOLARIS)
+   int childCrashFd, childCrashRemapFd;
+   if (!CrashReporter::CreateNotificationPipeForChild(
+         &childCrashFd, &childCrashRemapFd))
+@@ -673,7 +679,7 @@ GeckoChildProcessHost::PerformAsyncLaunc
+ #endif
+ 
+   base::LaunchApp(childArgv, mFileMap,
+-#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_BSD)
++#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_BSD) || defined(OS_SOLARIS)
+                   newEnvVars, privs,
+ #endif
+                   false, &process, arch);
