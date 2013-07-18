dfb05846bd67f7f3f0dc2ae7887cf43e4105379e
sborrill <sborrill@pkgsrc.org>
2013-07-18 11:59:28 +0000

    Update to 6.2.0 SDK for use with XenServer 6.2 and earlier (support for XenServer 4.0 has been removed from this release).

--- devel/libxenserver/patches/patch-xen_common.c
+++ devel/libxenserver/patches/patch-xen_common.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-xen_common.c,v 1.1.1.1 2011/02/17 09:54:02 sborrill Exp $
+$NetBSD: patch-xen_common.c,v 1.2 2013/07/18 11:59:28 sborrill Exp $
 
---- src/xen_common.c.orig	2011-02-11 12:59:37.000000000 +0000
-+++ src/xen_common.c	2011-02-11 13:00:20.000000000 +0000
-@@ -1131,6 +1131,7 @@
+--- src/xen_common.c.orig	2013-06-14 14:51:06.000000000 +0100
++++ src/xen_common.c	2013-07-18 12:52:57.000000000 +0100
+@@ -1310,6 +1310,7 @@
      default:
          assert(false);
      }
@@ -10,9 +10,9 @@ $NetBSD: patch-xen_common.c,v 1.1.1.1 2011/02/17 09:54:02 sborrill Exp $
  }
  
  
-@@ -1638,6 +1639,7 @@
-     default:
-         assert(false);
+@@ -1826,6 +1827,7 @@
+             assert(false);
+             break;
      }
 +    return NULL;
  }
