0dc87221616c6ad400d05684adafafe5a133e42c
joerg <joerg>
2013-07-19 12:53:09 +0000

    Fix friend definition.

--- math/ordCalc/patches/patch-src_ordBase.h
+++ math/ordCalc/patches/patch-src_ordBase.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_ordBase.h,v 1.1 2012/12/20 21:47:31 joerg Exp $
+$NetBSD: patch-src_ordBase.h,v 1.2 2013/07/19 12:53:09 joerg Exp $
 
---- src/ordBase.h.orig	2012-12-20 18:34:49.000000000 +0000
+--- src/ordBase.h.orig	2010-01-17 04:50:43.000000000 +0000
 +++ src/ordBase.h
 @@ -5,9 +5,9 @@
  #include <string>
@@ -21,8 +21,8 @@ $NetBSD: patch-src_ordBase.h,v 1.1 2012/12/20 21:47:31 joerg Exp $
 -		    const Ordinal& iter, const Ordinal* const * params,
 -		    const Ordinal* drillDown);
 +	friend const Ordinal& admisLevelFunctional(const Ordinal& levCK,
-+		const Ordinal& iter, const Ordinal* const * params=NULL,
-+		const Ordinal& drillDown=Ordinal::zero);
++		const Ordinal& iter, const Ordinal* const * params,
++		const Ordinal& drillDown);
  
  		static int idCount ;
  		bool inDeleteList ;
