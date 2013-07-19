b79b4551c9aa3b6d5a4c8131f9cd26e718d834ed
joerg <joerg@pkgsrc.org>
2013-07-19 12:51:56 +0000

    Don't use false as pointer. Don't use default arguments in friend definition.

---
+++ lang/yap/patches/patch-packages_swi-minisat2_C_SolverTypes.h
@@ -0,0 +1,13 @@
+$NetBSD: patch-packages_swi-minisat2_C_SolverTypes.h,v 1.3 2013/07/19 12:51:56 joerg Exp $
+
+--- packages/swi-minisat2/C/SolverTypes.h.orig	2012-06-29 21:19:52.000000000 +0000
++++ packages/swi-minisat2/C/SolverTypes.h
+@@ -119,7 +119,7 @@ public:
+ 
+     // -- use this function instead:
+     template<class V>
+-    friend Clause* Clause_new(const V& ps, bool learnt = false);
++    friend Clause* Clause_new(const V& ps, bool learnt);
+ 
+     int          size        ()      const   { return size_etc >> 3; }
+     void         shrink      (int i)         { assert(i <= size()); size_etc = (((size_etc >> 3) - i) << 3) | (size_etc & 7); }
