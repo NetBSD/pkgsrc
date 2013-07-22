c88480c4d1c7d79238cff89ac12905590f981360
degroote <degroote@pkgsrc.org>
2013-07-22 19:47:18 +0000

    Update awesome to 3.5.1
    Lots of change since 3.4.13, in particular support for lua 5.2.
    For details changelot, see http://awesome.naquadah.org/changelogs/short/v3.5

--- wm/awesome/patches/patch-awesomeConfig.cmake
+++ wm/awesome/patches/patch-awesomeConfig.cmake
@@ -1,17 +1,17 @@
-$NetBSD: patch-awesomeConfig.cmake,v 1.1 2012/08/26 20:28:17 is Exp $
+$NetBSD: patch-awesomeConfig.cmake,v 1.2 2013/07/22 19:47:18 degroote Exp $
 
---- awesomeConfig.cmake.orig	2011-01-17 11:38:13.000000000 +0000
-+++ awesomeConfig.cmake
-@@ -16,7 +16,7 @@ option(COMPRESS_MANPAGES "compress manpa
- option(GENERATE_LUADOC "generate luadoc" ON)
+--- awesomeConfig.cmake.orig	2013-04-01 11:44:46.000000000 +0000
++++ awesomeConfig.cmake	2013-07-21 21:05:57.000000000 +0000
+@@ -16,7 +16,7 @@
+ option(GENERATE_DOC "generate API documentation" ON)
  
  # {{{ CFLAGS
--add_definitions(-std=gnu99 -ggdb3 -fno-strict-aliasing -Wall -Wextra
-+add_definitions(-std=gnu99 -fno-strict-aliasing -Wall -Wextra
+-add_definitions(-O1 -std=gnu99 -ggdb3 -rdynamic -fno-strict-aliasing -Wall -Wextra
++add_definitions(-O1 -std=gnu99 -rdynamic -fno-strict-aliasing -Wall -Wextra
      -Wchar-subscripts -Wundef -Wshadow -Wcast-align -Wwrite-strings
      -Wsign-compare -Wunused -Wno-unused-parameter -Wuninitialized -Winit-self
-     -Wpointer-arith -Wredundant-decls -Wformat-nonliteral
-@@ -155,7 +155,7 @@ if(NOT AWESOME_REQUIRED_FOUND OR NOT AWE
+     -Wpointer-arith -Wformat-nonliteral
+@@ -153,7 +153,7 @@
  endif()
  
  macro(a_find_library variable library)
