8d1802e9c6e00f7235a137ef919927956379e104
adam <adam>
2013-07-31 18:26:07 +0000

    Version 2.0.1         * Temporarily turning off optimisation code path         * Added additional tests for future optimisation refactoring         * Fixes for StringVectors         * Additional checks for type mismatches         * Fix for Composite Deep Scanline

--- graphics/openexr/buildlink3.mk
+++ graphics/openexr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2013/05/09 07:39:11 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2013/07/31 18:26:07 adam Exp $
 
 BUILDLINK_TREE+=	openexr
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	openexr
 OPENEXR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.openexr+=	openexr>=1.6.1
-BUILDLINK_ABI_DEPENDS.openexr?=	openexr>=2.0.0
+BUILDLINK_ABI_DEPENDS.openexr+=	openexr>=2.0.0
 BUILDLINK_PKGSRCDIR.openexr?=	../../graphics/openexr
 
 PTHREAD_OPTS+=	require
