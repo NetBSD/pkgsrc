d3fe25dba456a282f420e6a89f829ddef72fff6e
fhajny <fhajny@pkgsrc.org>
2013-08-05 12:27:48 +0000

    Update opencv to 2.4.6.1
    Changes in 2.4.6.1:
    * Hotfix for camera pipeline for Linux (V4L).
    
    Changes in 2.4.6:
    * Windows RT: added video file i/o and sample application using camera,
      enabled parallelization with TBB or MS Concurrency
    * CUDA 5.5: added support for desktop and ARM
    * Added Qt 5 support
    * Binary compatiblility with both OpenCL 1.1/1.2 platforms. Now the binaries
      compiled with any of AMD/Intel/Nvidia's SDK can run on all other platforms.
    * New functions ported, CLAHE, GoodFeaturesToTrack, TVL1 optical flow and more
    * Performance optimizations, HOG and more.
    * More kernel binary cache options though setBinaryDiskCache interface.
    * OpenCL binaries are now included into the superpack for Windows (for VS2010
      and VS2012 only)
    * Switched all the remaining parallel loops from TBB-only
      'tbb::parallel_for()' to universal 'cv::parallel_for_()' with many possible
      backends (MS Concurrency, Apple's GDC, OpenMP, Intel TBB etc.)
    * iOS build scripts (together with Android ones) moved to 'opencv/platforms'
      directory
    * Fixed bug with incorrect saved video from camera through CvVideoCamera
    * Added 'rotateVideo' flag to the CvVideoCamera class to control camera
      preview rotation on device rotation
    * Added functions to convert between UIImage and cv::Mat (just include
      opencv2/highgui/ios.h)
    * Numerous bug-fixes across all the library

--- graphics/opencv/patches/patch-modules_ts_include_opencv2_ts_ts__perf.hpp
+++ graphics/opencv/patches/patch-modules_ts_include_opencv2_ts_ts__perf.hpp
@@ -1,12 +1,12 @@
-$NetBSD: patch-modules_ts_include_opencv2_ts_ts__perf.hpp,v 1.1 2013/02/26 23:38:08 joerg Exp $
+$NetBSD: patch-modules_ts_include_opencv2_ts_ts__perf.hpp,v 1.2 2013/08/05 12:27:48 fhajny Exp $
 
---- modules/ts/include/opencv2/ts/ts_perf.hpp.orig	2013-02-26 16:47:16.000000000 +0000
-+++ modules/ts/include/opencv2/ts/ts_perf.hpp
-@@ -478,7 +478,6 @@ CV_EXPORTS void PrintTo(const Size& sz, 
- #define CV_PERF_TEST_MAIN(testsuitname, ...) \
- int main(int argc, char **argv)\
- {\
+--- /tmp/pkgsrc/graphics/opencv/work/opencv-2.4.6.1/modules/ts/include/opencv2/ts/ts_perf.hpp.orig	Wed Jul 10 11:49:00 2013
++++ /tmp/pkgsrc/graphics/opencv/work/opencv-2.4.6.1/modules/ts/include/opencv2/ts/ts_perf.hpp	Mon Aug  5 11:34:21 2013
+@@ -477,7 +477,6 @@
+ 
+ 
+ #define CV_PERF_TEST_MAIN_INTERNALS(modulename, impls, ...) \
 -    while (++argc >= (--argc,-1)) {__VA_ARGS__; break;} /*this ugly construction is needed for VS 2005*/\
-     ::perf::Regression::Init(#testsuitname);\
-     ::perf::TestBase::Init(argc, argv);\
-     ::testing::InitGoogleTest(&argc, argv);\
+     ::perf::Regression::Init(#modulename);\
+     ::perf::TestBase::Init(std::vector<std::string>(impls, impls + sizeof impls / sizeof *impls),\
+                            argc, argv);\
