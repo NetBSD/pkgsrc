e44aefc49399200906a94b233a4ca9488d125568
tron <tron@pkgsrc.org>
2013-08-06 14:55:05 +0000

    Pullup ticket #4202 - requested by taca lang/php54: security update
    Revisions pulled up:
    - lang/php54/Makefile.common                                1.14 via patch
    - lang/php54/distinfo                                       1.19-1.20 via patch
    - lang/php54/patches/patch-main_streams_cast.c              0-1.3 via patch
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sun Jul  7 13:37:53 UTC 2013
    
       Modified Files:
       	pkgsrc/lang/php54: Makefile.common distinfo
       Removed Files:
       	pkgsrc/lang/php54/patches: patch-main_streams_cast.c
    
       Log Message:
       Update php54 to 5.4.17.
    
       04 Jul 2013, PHP 5.4.17
    
       - Core:
         . Fixed bug #64988 (Class loading order affects E_STRICT warning). (Laruence)
         . Fixed bug #64966 (segfault in zend_do_fcall_common_helper_SPEC). (Laruence)
         . Fixed bug #64960 (Segfault in gc_zval_possible_root). (Laruence)
         . Fixed bug #64936 (doc comments picked up from previous scanner run). (Stas,
           Jonathan Oddy)
         . Fixed bug #64934 (Apache2 TS crash with get_browser()). (Anatol)
         . Fixed bug #64166 (quoted-printable-encode stream filter incorrectly
           discarding whitespace). (Michael M Slusarz)
    
       - DateTime:
         . Fixed bug #53437 (Crash when using unserialized DatePeriod instance).
           (Gustavo, Derick, Anatol)
    
       - FPM:
         . Fixed Bug #64915 (error_log ignored when daemonize=0). (Remi)
         . Implemented FR #64764 (add support for FPM init.d script). (Lior Kaplan)
    
       - PDO:
         . Fixed bug #63176 (Segmentation fault when instantiate 2 persistent PDO to
           the same db server). (Laruence)
    
       - PDO_DBlib:
         . Fixed bug #63638 (Cannot connect to SQL Server 2008 with PDO dblib).
           (Stanley Sufficool)
         . Fixed bug #64338 (pdo_dblib can't connect to Azure SQL). (Stanley
           Sufficool)
         . Fixed bug #64808 (FreeTDS PDO getColumnMeta on a prepared but not executed
           statement crashes). (Stanley Sufficool)
    
       - PDO_firebird:
         . Fixed bug #64037 (Firebird return wrong value for numeric field).
           (Matheus Degiovani, Matteo)
         . Fixed bug #62024 (Cannot insert second row with null using parametrized
           query). (patch by james@kenjim.com, Matheus Degiovani, Matteo)
    
       - PDO_mysql:
         . Fixed bug #48724 (getColumnMeta() doesn't return native_type for BIT,
           TINYINT and YEAR). (Antony, Daniel Beardsley)
    
       - PDO_pgsql:
         . Fixed Bug #64949 (Buffer overflow in _pdo_pgsql_error). (Remi)
    
       - pgsql:
         . Fixed bug #64609 (pg_convert enum type support). (Matteo)
    
       - Readline:
         . Implement FR #55694 (Expose additional readline variable to prevent
           default filename completion). (Hartmel)
    
       - SPL:
         . Fixed bug #64997 (Segfault while using RecursiveIteratorIterator on
           64-bits systems). (Laruence)
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Mon Jul  8 13:16:22 UTC 2013
    
       Modified Files:
       	pkgsrc/lang/php54: distinfo
       Added Files:
       	pkgsrc/lang/php54/patches: patch-main_streams_cast.c
    
       Log Message:
       Correct condition for NetBSD 6.0 and later to fix build problem on NetBSD
       6.0.  Reported by Jörn Clausen as PR pkg/48029.

--- lang/php54/patches/patch-main_streams_cast.c
+++ lang/php54/patches/patch-main_streams_cast.c
@@ -1,27 +1,15 @@
-$NetBSD: patch-main_streams_cast.c,v 1.1 2012/06/16 05:21:55 taca Exp $
+$NetBSD: patch-main_streams_cast.c,v 1.1.10.1 2013/08/06 14:55:05 tron Exp $
 
---- main/streams/cast.c.orig	2012-05-03 17:10:43.000000000 +0000
+* For NetBSD 6.0+, __NetBSD_Version__ should includes 600000000.
+
+--- main/streams/cast.c.orig	2013-07-03 06:10:53.000000000 +0000
 +++ main/streams/cast.c
-@@ -34,7 +34,7 @@
- typedef struct {
- 	int (*reader)(void *, char *, int);
- 	int (*writer)(void *, const char *, int);
--	fpos_t (*seeker)(void *, fpos_t, int);
-+	off_t (*seeker)(void *, off_t, int);
- 	int (*closer)(void *);
- } COOKIE_IO_FUNCTIONS_T;
+@@ -33,7 +33,7 @@
+ #if defined(HAVE_FUNOPEN) && !defined(HAVE_FOPENCOOKIE)
  
-@@ -68,11 +68,11 @@ static int stream_cookie_writer(void *co
- 	return php_stream_write((php_stream *)cookie, (char *)buffer, size);
- }
- 
--static fpos_t stream_cookie_seeker(void *cookie, off_t position, int whence)
-+static off_t stream_cookie_seeker(void *cookie, off_t position, int whence)
- {
- 	TSRMLS_FETCH();
- 
--	return (fpos_t)php_stream_seek((php_stream *)cookie, position, whence);
-+	return (off_t)php_stream_seek((php_stream *)cookie, position, whence);
- }
- 
- static int stream_cookie_closer(void *cookie)
+ /* NetBSD 6.0+ uses off_t instead of fpos_t in funopen */
+-# if defined(__NetBSD__) && (__NetBSD_Version__ > 600000000)
++# if defined(__NetBSD__) && (__NetBSD_Version__ >= 600000000)
+ #  define PHP_FPOS_T off_t
+ # else
+ #  define PHP_FPOS_T fpos_t
