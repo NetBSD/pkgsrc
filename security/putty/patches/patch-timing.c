a8f5ad920c1474082c6c89f7dab3bd8322d59ef4
drochner <drochner@pkgsrc.org>
2013-08-07 11:06:39 +0000

    update to 0.63 This fixes a buffer overflow which was patched in pkgsrc (CVE-2013-4852), two other buffer overflows (CVE-2013-4206, CVE-2013-4207), and it clears private keys after use now (CVE-2013-4208). Other than that, there are mostly bug fixes from 0.62 and a few small features.

--- security/putty/patches/patch-timing.c
+++ security/putty/patches/patch-timing.c
@@ -1,19 +1,17 @@
-$NetBSD: patch-timing.c,v 1.1 2012/11/01 19:32:44 joerg Exp $
+$NetBSD: patch-timing.c,v 1.2 2013/08/07 11:06:39 drochner Exp $
 
---- timing.c.orig	2012-10-30 22:23:57.000000000 +0000
+--- timing.c.orig	2012-09-19 22:12:00.000000000 +0000
 +++ timing.c
-@@ -41,21 +41,10 @@ static int compare_timers(void *av, void
+@@ -60,19 +60,10 @@ static int compare_timers(void *av, void
       * Failing that, compare on the other two fields, just so that
       * we don't get unwanted equality.
       */
--#ifdef __LCC__
+-#if defined(__LCC__) || defined(__clang__)
 -    /* lcc won't let us compare function pointers. Legal, but annoying. */
 -    {
 -	int c = memcmp(&a->fn, &b->fn, sizeof(a->fn));
--	if (c < 0)
--	    return -1;
--	else if (c > 0)
--	    return +1;
+-	if (c)
+-	    return c;
 -    }
 -#else    
 -    if (a->fn < b->fn)
