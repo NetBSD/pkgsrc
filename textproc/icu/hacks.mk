af656b088ea41f277c6cbc6e396c88bca0b062db
jmcneill <jmcneill@pkgsrc.org>
2013-08-07 10:19:04 +0000

    apply arm-gcc-atomics hack to earm too

--- textproc/icu/hacks.mk
+++ textproc/icu/hacks.mk
@@ -1,10 +1,10 @@
-# $NetBSD: hacks.mk,v 1.1 2013/02/01 03:44:22 jmcneill Exp $
+# $NetBSD: hacks.mk,v 1.2 2013/08/07 10:19:04 jmcneill Exp $
 
 .if !defined(ICU_HACKS_MK)
 ICU_HACKS_MK=	# defined
 
 # NetBSD arm gcc <= 4.5 missing builtin atomics
-.if ${OPSYS} == "NetBSD" && ${MACHINE_ARCH} == "arm"
+.if ${OPSYS} == "NetBSD" && (${MACHINE_ARCH} == "arm" || ${MACHINE_ARCH} == "earm")
 .  if !empty(CC_VERSION:Mgcc-4.[0-5].*)
 PKG_HACKS+=	arm-gcc-atomics
 CFLAGS+=	-DU_HAVE_GCC_ATOMICS=0
