9f99c4b9b28505d90df2caf57af79b6c093c62d3
drochner <drochner@pkgsrc.org>
2013-08-07 11:06:39 +0000

    update to 0.63 This fixes a buffer overflow which was patched in pkgsrc (CVE-2013-4852), two other buffer overflows (CVE-2013-4206, CVE-2013-4207), and it clears private keys after use now (CVE-2013-4208). Other than that, there are mostly bug fixes from 0.62 and a few small features.

--- security/putty/patches/patch-unix_uxucs.c
+++ security/putty/patches/patch-unix_uxucs.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-unix_uxucs.c,v 1.1 2012/11/01 19:32:44 joerg Exp $
+$NetBSD: patch-unix_uxucs.c,v 1.2 2013/08/07 11:06:39 drochner Exp $
 
---- unix/uxucs.c.orig	2012-10-30 22:26:02.000000000 +0000
+--- unix/uxucs.c.orig	2013-07-22 07:12:05.000000000 +0000
 +++ unix/uxucs.c
-@@ -76,7 +76,7 @@ int wc_to_mb(int codepage, int flags, wc
- 	setlocale(LC_CTYPE, "");
+@@ -72,7 +72,7 @@ int wc_to_mb(int codepage, int flags, co
+ 	memset(&state, 0, sizeof state);
  
  	while (wclen > 0) {
 -	    int i = wcrtomb(output, wcstr[0], &state);
