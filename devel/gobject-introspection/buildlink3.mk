39e35ea9208539b98c2ca4e759f43b3805809e48
richard <richard>
2013-08-08 04:54:28 +0000

    gobject-introspection uses by default $HOME/.cache/g-ir-scanner for a cache. Disable this globally for packages using buildlink3.mk. Suggested by Patrick Welche" <prlw1@netbsd.org>

--- devel/gobject-introspection/buildlink3.mk
+++ devel/gobject-introspection/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2012/10/07 10:23:51 prlw1 Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2013/08/08 04:54:28 richard Exp $
 
 BUILDLINK_TREE+=	gobject-introspection
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.gobject-introspection+=	gobject-introspection>=0.6.14nb1
 BUILDLINK_ABI_DEPENDS.gobject-introspection+=	gobject-introspection>=1.34.0
 BUILDLINK_PKGSRCDIR.gobject-introspection?=	../../devel/gobject-introspection
 
+ALL_ENV+=	GI_SCANNER_DISABLE_CACHE=yes
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/libffi/buildlink3.mk"
