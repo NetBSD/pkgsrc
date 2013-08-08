0178f721ce02898c2b28372d8cafecb8714fc71e
mef <mef@pkgsrc.org>
2013-08-08 03:29:59 +0000

    Fix typo, not 2.32 but 2.23, sorry and thanks.

--- cross/avr-binutils/buildlink3.mk
+++ cross/avr-binutils/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/08/08 03:14:57 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/08/08 03:29:59 mef Exp $
 
 BUILDLINK_TREE+=	avr-binutils
 
 .if !defined(AVR_BINUTILS_BUILDLINK3_MK)
 AVR_BINUTILS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.avr-binutils+=	avr-binutils>=2.32
+BUILDLINK_API_DEPENDS.avr-binutils+=	avr-binutils>=2.23
 BUILDLINK_PKGSRCDIR.avr-binutils?=		../../cross/avr-binutils
 #BUILDLINK_FNAME_TRANSFORM.avr-binutils+=	-e "s,${AVR_TARGET}/lib,lib/,g"
 
