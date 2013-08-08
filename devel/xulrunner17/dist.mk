abec388e54cc7ee5a0f938d81a9392f883902b64
ryoon <ryoon>
2013-08-08 13:01:38 +0000

    Update to 17.0.8
    Changelog:
    Fixed in Firefox ESR 17.0.8
    MFSA 2013-75 Local Java applets may read contents of local file system
    MFSA 2013-73 Same-origin bypass with web workers and XMLHttpRequest
    MFSA 2013-72 Wrong principal used for validating URI for some Javascript components
    MFSA 2013-71 Further Privilege escalation through Mozilla Updater
    MFSA 2013-69 CRMF requests allow for code execution and XSS attacks
    MFSA 2013-68 Document URI misrepresentation and masquerading
    MFSA 2013-66 Buffer overflow in Mozilla Maintenance Service and Mozilla Updater
    MFSA 2013-63 Miscellaneous memory safety hazards (rv:23.0 / rv:17.0.8)

--- devel/xulrunner17/dist.mk
+++ devel/xulrunner17/dist.mk
@@ -1,11 +1,11 @@
-# $NetBSD: dist.mk,v 1.7 2013/07/09 10:53:14 ryoon Exp $
+# $NetBSD: dist.mk,v 1.8 2013/08/08 13:01:38 ryoon Exp $
 #
 # used by devel/xulrunner17/Makefile
 # used by www/firefox17/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	17.0.7
+MOZ_BRANCH=	17.0.8
 MOZ_BRANCH_MINOR=	esr
 MASTER_SITES=	${MASTER_SITE_MOZILLA_ESR:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
