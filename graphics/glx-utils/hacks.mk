8b8be5a832027c6be7000f403a964093c9f78cfb
khorben <khorben@pkgsrc.org>
2013-08-10 23:02:26 +0000

    Crude compilation fix for graphics/glx-utils: - MesaDemos 7.11.2 was never released - fall back to MesaDemos 7.4.4 for the time being (as still found in distinfo)

---
+++ graphics/glx-utils/hacks.mk
@@ -0,0 +1,6 @@
+# $NetBSD: hacks.mk,v 1.1 2013/08/10 23:02:26 khorben Exp $
+
+### [Sun Aug 11 01:00:40 CEST 2013 : khorben]
+### MesaDemos $MESA_VERSION may not exist, so forcing 7.4.4 for now
+###
+MESA_VERSION=	7.4.4
