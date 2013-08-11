4a58087cf61a7bbed46dcee713faf41a78fd63f8
jaapb <jaapb@pkgsrc.org>
2013-08-11 16:06:25 +0000

    Updated package to version 2.3.3.4. This is an update of a very old version, so I'm not including the entire changelog here; see the package changelog for more details. The patch was originally submitted by Francois Tigeot as PR pkg/46156 - I've updated it since.

--- textproc/libclucene/buildlink3.mk
+++ textproc/libclucene/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:30 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/08/11 16:06:25 jaapb Exp $
 
 BUILDLINK_TREE+=	libclucene
 
 .if !defined(LIBCLUCENE_BUILDLINK3_MK)
 LIBCLUCENE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libclucene+=	libclucene>=0.9.21
+BUILDLINK_API_DEPENDS.libclucene+=	libclucene>=2.2.0
 BUILDLINK_PKGSRCDIR.libclucene?=	../../textproc/libclucene
 .endif # LIBCLUCENE_BUILDLINK3_MK
 
