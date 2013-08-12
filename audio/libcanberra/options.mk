4685f920d1cd349722aa5601279b816f6ad52829
obache <obache@pkgsrc.org>
2013-08-12 07:09:59 +0000

    Update libcanberra to 0.27 while here, add gtk3 option and enabled by default.
    News
    
       Fri 18 Feb 2011:
    
       Version 0.27 released; Gtk+ 3.x fixes; add new tool to implement
       boot-up sounds when used with systemd; other fixes

--- audio/libcanberra/options.mk
+++ audio/libcanberra/options.mk
@@ -1,15 +1,24 @@
-# $NetBSD: options.mk,v 1.1 2011/05/12 13:39:24 adam Exp $
+# $NetBSD: options.mk,v 1.2 2013/08/12 07:09:59 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libcanberra
-PKG_SUPPORTED_OPTIONS=	gtk
-PKG_SUGGESTED_OPTIONS=	gtk
+PKG_SUPPORTED_OPTIONS=	gtk gtk3
+PKG_SUGGESTED_OPTIONS=	gtk gtk3
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		gtk
+PLIST_VARS+=		gtk-any gtk gtk3
+
+.if !empty(PKG_OPTIONS:Mgtk) || !empty(PKG_OPTIONS:Mgtk3)
+GCONF_SCHEMAS+=		libcanberra.schemas
+PLIST.gtk-any=		yes
+.endif
 
 .if !empty(PKG_OPTIONS:Mgtk)
-GCONF_SCHEMAS+=		libcanberra.schemas
 PLIST.gtk=		yes
 .include "../../x11/gtk2/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mgtk3)
+PLIST.gtk3=		yes
+.include "../../x11/gtk3/buildlink3.mk"
 .endif
