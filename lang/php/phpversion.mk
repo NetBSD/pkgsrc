aa890d507ab9baf76bbbeab9af18fe3d95ddd642
taca <taca>
2013-08-14 14:53:03 +0000

    Correct checking condition of PHP_CHECK_INSTALLED.

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.38 2013/07/29 16:38:12 taca Exp $
+# $NetBSD: phpversion.mk,v 1.39 2013/08/14 14:53:03 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -166,7 +166,7 @@ PKG_PHP_MAJOR_VERS:=	5
 PHP_CHECK_INSTALLED?=	Yes
 
 # if installed PHP isn't compatible with required PHP, bail out
-.if !empty(PHP_CHECK_INSTALLED:M[nN][oO])
+.if empty(PHP_CHECK_INSTALLED:M[nN][oO])
 .if defined(_PHP_INSTALLED) && !defined(_PHP_VERSION_${_PHP_VERSION}_INSTALLED)
 PKG_SKIP_REASON+=	"Package accepts ${PKG_PHP}, but different version is installed"
 .endif
