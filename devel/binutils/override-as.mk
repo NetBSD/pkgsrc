ebbc151c5be1b7fadeb419fa56e783d838ed9528
richard <richard@pkgsrc.org>
2013-08-15 06:48:50 +0000

    fix -specs for ccache

--- devel/binutils/override-as.mk
+++ devel/binutils/override-as.mk
@@ -1,4 +1,4 @@
-# $NetBSD: override-as.mk,v 1.6 2012/11/21 10:53:53 jperkin Exp $
+# $NetBSD: override-as.mk,v 1.7 2013/08/15 06:48:50 richard Exp $
 
 # This Makefile fragment can be included by packages that wish to
 # override the default selection of as(1) and use the one from devel/binutils
@@ -18,7 +18,7 @@ ${SPECS_AS}:
 	@${ECHO} "%{!S:-o %|.s |" >> ${SPECS_AS}
 	@${ECHO} "${PKG_AS} %(asm_options) %m.s %A }" >> ${SPECS_AS}
 
-_WRAP_EXTRA_ARGS.CC+=	-specs ${SPECS_AS}
-_WRAP_EXTRA_ARGS.CXX+=	-specs ${SPECS_AS}
+_WRAP_EXTRA_ARGS.CC+=	-specs=${SPECS_AS}
+_WRAP_EXTRA_ARGS.CXX+=	-specs=${SPECS_AS}
 
 .endif
