288964eb45d4d8b588f8d840661fffaa2fa3871b
schmonz <schmonz@pkgsrc.org>
2013-08-15 15:43:48 +0000

    Rename and comment pkgsrc patches. No functional change.

--- mail/qmail/patches/patch-spawn.c
+++ mail/qmail/patches/patch-spawn.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-spawn.c,v 1.1 2012/05/20 08:48:56 marino Exp $
+$NetBSD: patch-spawn.c,v 1.2 2013/08/15 15:43:48 schmonz Exp $
+
+Fix qmail-*spawn segfaults on DragonFly (pkg/46333).
 
 --- spawn.c.orig	2012-04-14 13:35:08.381341000 +0000
 +++ spawn.c
