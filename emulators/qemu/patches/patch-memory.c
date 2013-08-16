40a10203a7f75e6079c7f60dca013b3ee446b0bb
adam <adam>
2013-08-16 21:20:11 +0000

    Changes 1.6.0: Block devices * New device "nvme" provides a PCI device that implements the NVMe standard. * Fix for a possible data loss on crashes with IDE disks (due to mishandling of FLUSH requests)
    Device assignment
    * Legacy PCI assignment supports CPU affinity for MSI interrupts.
    
    USB
    * The XHCI (USB 3.0) controller supports live migration.
    
    PCI
    * ACPI hotplug of devices behind a PCI bridge is supported.
    
    ARM
    * The 32-bit ARMv8 LDA/STL instructions for load-acquire/store-release are supported (only with -cpu any); the remainder of the new v8 instructions will follow in a future release.
    * The vexpress-a9 and vexpress-a15 boards have experimental support for virtio devices via a virtio-mmio transport. Note that the virtio command lines used with x86 systems won't work as they will create PCI virtio devices; use the devices "virtio-blk-device", "virtio-net-device", etc instead. Note that this functionality may change in future releases.
    * The -initrd option now accepts ramdisks with a U-Boot header.
    * A model of the Calxeda ECX-2000 / Midway system is now supported ("midway").
    
    PPC
    * Mac OS X guests supported (10.2-10.4 for PPC, 10.4 for PPC64)
    * pSeries guests support live migration and savevm.
    
    s390x
    * If the kernel supports it, virtio-ccw supports ioeventfd and vhost when running on KVM.
    * The dump-guest-memory command is supported.
    
    SPARC
    * Sun4c and Sun4d architectures and related CPUs were not fully implemented and have been removed.
    * 24-bit display mode works
    
    x86
    * Firmware can be exposed to the guest as a flash device (using -pflash) also when running on KVM.
    * In addition to the "xenfv" machine type, which is kept for backwards compatibility, Xen can now use the pc machine type (including versioned machine types) using "-M pc -machine accel=xen". Note that using PV-on-HVM drivers requires "-M pc -machine accel=xen -device xen-platform".
    * QEMU can expose the full set of ACPI tables to the guest: See Features/ACPITableGeneration
    * Conroe, Penryn and Nehalem CPUs have had their CPUID values fixed
    * "-M isapc" now works on KVM too.
    * -pflash can be used with KVM too.
    
    more...

--- emulators/qemu/patches/patch-memory.c
+++ emulators/qemu/patches/patch-memory.c
@@ -1,32 +1,16 @@
-$NetBSD: patch-memory.c,v 1.5 2013/07/09 17:00:58 tsutsui Exp $
+$NetBSD: patch-memory.c,v 1.6 2013/08/16 21:20:11 adam Exp $
 
 Avoid conflicts with SSP read() macro in NetBSD's <ssp/unistd.h>
 (PR lib/43832: ssp causes common names to be defines)
 
---- memory.c.orig	2013-06-26 21:47:29.000000000 +0000
+--- memory.c.orig	2013-08-15 19:56:23.000000000 +0000
 +++ memory.c
-@@ -313,7 +313,7 @@ static void memory_region_read_accessor(
+@@ -404,7 +404,7 @@ static void memory_region_read_accessor(
      if (mr->flush_coalesced_mmio) {
          qemu_flush_coalesced_mmio_buffer();
      }
 -    tmp = mr->ops->read(mr->opaque, addr, size);
 +    tmp = (*mr->ops->read)(mr->opaque, addr, size);
+     trace_memory_region_ops_read(mr, addr, tmp, size);
      *value |= (tmp & mask) << shift;
  }
- 
-@@ -396,12 +396,12 @@ static void memory_region_iorange_read(I
- 
-         *data = ((uint64_t)1 << (width * 8)) - 1;
-         if (mrp) {
--            *data = mrp->read(mr->opaque, offset);
-+            *data = (*mrp->read)(mr->opaque, offset);
-         } else if (width == 2) {
-             mrp = find_portio(mr, offset - mrio->offset, 1, false);
-             assert(mrp);
--            *data = mrp->read(mr->opaque, offset) |
--                    (mrp->read(mr->opaque, offset + 1) << 8);
-+            *data = (*mrp->read)(mr->opaque, offset) |
-+                    ((*mrp->read)(mr->opaque, offset + 1) << 8);
-         }
-         return;
-     }
