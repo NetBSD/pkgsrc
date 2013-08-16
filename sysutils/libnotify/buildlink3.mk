9334f490d7ce989e86434030c2d26acbbb6a0d92
obache <obache@pkgsrc.org>
2013-08-16 09:17:33 +0000

    Update libnotify to 0.7.5.
    New in 0.7.5
    ============
    
    - Build fixes (Colin Walters)
    - Documentation improvements (Javier Jardón)
    - Spec clarifications (Matthias Clasen)
    
    New in 0.7.4
    ============
    
    - Improve documentation of supported image data formats (Colin Walters)
    - Honor aclocal flags (Craig Keogh)
    - Update autotools configuration (Javier Jardón)
    - Test notification removal (William Jon McCann)
    
    NEW in 0.7.3:
    ==============
    
    - Add a way to change the application name for
      individual notifications (Richard Hughes)
    
    NEW in 0.7.2:
    ==============
    
    - Add a way to change the application name (William Jon McCann)
    - Mark image_path as deprecated (William Jon McCann)
    - Allow properties to be set in any order (Matthias Clasen)
    
    NEW in 0.7.1:
    ==============
    
     - Don't export private symbols (Emilio Pozuelo Monfort)
     - Introspection support and annotations (Jonathan Matthew, Martin Pitt)
     - Documentation cleanups and fixes (Javier Jardón)
     - Ref the pixbuf passed to set_image_from_pixbuf (Guillaume Desmottes)
     - Add tests for new capabilities and hints (William Jon McCann)
     - Update spec to mention "persistence" and "action-icons" server
       capability and "action-icons", "transient" and "resident" hints.
       Bump spec version to 1.2  (William Jon McCann)
    
    NEW in 0.7.0:
    ==============
    
     - Remove the ability to attach notifications to widgets or positions
     - Add a test for the persistence server capability
     - Port to use GBus instead of dbus-glib (Christian Persch)
     - Add a macro to get library version number
     - Remove GTK+ dependency from library
     - Port tests to GTK+ 3
    
    NEW in 0.6.0:
    ==============
    
    The 0.5.1 release included changes that should not have been made on a
    stable branch.  It should have been a 0.6 series release.  So here it is.
    
    - Fixed #623096 Fix notify_get_server_info results
    
    
    NEW in 0.5.1:
    ==============
    - Remove explicit GTK+ 2.x linking, so that libnotify can link against
      both GTK+ 2.x or GTK+ 3.x
    
    NEW in 0.5.0:
    ==============
    - Support for version 1.1 of notification spec (Aurélien Gâteau, A. Walton)
    - Fixed #608089, Applications aborted at finalizing when the dbus session is being closed. (William Jon McCann)
    - Fixed #608082, Arabic text is misplaced in bubbles (RTL) (William Jon McCann)
    - Fixed #608086, wrong return value of notify_init() (William Jon McCann)
    - Fixed #612720, Does not compile with -DGSEAL_ENABLE (William Jon McCann)
    - Fixed #613999, Use g_strcompress in notify-send to parse \n from command line (William Jon McCann)
    - Fixed #610880, lazily initialize the daemon (Martin Pitt)

--- sysutils/libnotify/buildlink3.mk
+++ sysutils/libnotify/buildlink3.mk
@@ -1,18 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.25 2013/06/06 12:53:57 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2013/08/16 09:17:33 obache Exp $
 
 BUILDLINK_TREE+=	libnotify
 
 .if !defined(LIBNOTIFY_BUILDLINK3_MK)
 LIBNOTIFY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libnotify+=	libnotify>=0.3.2
-BUILDLINK_ABI_DEPENDS.libnotify+=	libnotify>=0.4.5nb17
+BUILDLINK_API_DEPENDS.libnotify+=	libnotify>=0.7.0
 BUILDLINK_PKGSRCDIR.libnotify?=	../../sysutils/libnotify
 
+pkgbase := libnotify
+.include "../../mk/pkg-build-options.mk"
+
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../sysutils/dbus/buildlink3.mk"
-.include "../../sysutils/dbus-glib/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
+.include "../../graphics/gdk-pixbuf2/buildlink3.mk"
 .endif # LIBNOTIFY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libnotify
