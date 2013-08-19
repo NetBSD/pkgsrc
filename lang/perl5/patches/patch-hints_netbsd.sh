0060bd412f3d7ef1bfe6bf4f2e9a6f136823b1b7
christos <christos@pkgsrc.org>
2013-08-19 12:31:11 +0000

    Update function invisibility list. This time for host releated reentrant functions, recently introduced.

--- lang/perl5/patches/patch-hints_netbsd.sh
+++ lang/perl5/patches/patch-hints_netbsd.sh
@@ -1,11 +1,12 @@
-$NetBSD: patch-hints_netbsd.sh,v 1.6 2013/08/19 08:22:09 martin Exp $
+$NetBSD: patch-hints_netbsd.sh,v 1.7 2013/08/19 12:31:11 christos Exp $
 
 --whole-archive is a linker flag, not a compiler flag
 Better defaults for paths.
+Stop grovelling for functions we don't want to provide (*host*)
 
---- hints/netbsd.sh.orig	2013-05-01 02:52:55.000000000 +0000
-+++ hints/netbsd.sh
-@@ -41,8 +41,8 @@ case "$osvers" in
+--- hints/netbsd.sh.orig	2013-08-19 21:23:34.000000000 +0300
++++ hints/netbsd.sh	2013-08-19 21:23:12.000000000 +0300
+@@ -41,8 +41,8 @@
  # system gcc to build correctly, so check for it
  echo 'int f(void) { return 0; }' >try.c
  if ${cc:-cc} $cccdlflags -c try.c -otry.o 2>&1 &&
@@ -16,7 +17,33 @@ Better defaults for paths.
  fi
  rm try.c try.o try.so 2>/dev/null
  EOCBU
-@@ -186,10 +186,12 @@ esac
+@@ -100,6 +100,12 @@
+ 	d_getservbyport_r="$undef"
+ 	d_setservent_r="$undef"
+ 	d_endservent_r="$undef"
++	d_gethostbyname_r="$undef"
++	d_gethostbyaddr2_r="$undef"
++	d_gethostbyaddr_r="$undef"
++	d_sethostent_r="$undef"
++	d_gethostent_r="$undef"
++	d_endhostent_r="$undef"
+ 	d_getprotoent_r_proto="0"
+ 	d_getprotobyname_r_proto="0"
+ 	d_getprotobynumber_r_proto="0"
+@@ -110,6 +116,12 @@
+ 	d_getservbyport_r_proto="0"
+ 	d_setservent_r_proto="0"
+ 	d_endservent_r_proto="0"
++	d_gethostbyname_r_proto="0"
++	d_gethostbyaddr2_r_proto="0"
++	d_gethostbyaddr_r_proto="0"
++	d_sethostent_r_proto="0"
++	d_endhostent_r_proto="0"
++	d_gethostent_r_proto="0"
+ 	;;
+ esac
+ 
+@@ -186,10 +198,12 @@
  EOCBU
  
  # Set sensible defaults for NetBSD: look for local software in
