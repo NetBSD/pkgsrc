be09f3c4f86d507d8b2d359dc646b907394bf9c0
shattered <shattered@pkgsrc.org>
2013-08-21 10:23:41 +0000

    Update to 0.2.0. Changes:
    Integrated commands DIR and era.  Date and time functions.

--- emulators/aliados/patches/patch-cpm.cpp
+++ emulators/aliados/patches/patch-cpm.cpp
@@ -1,12 +1,12 @@
-$NetBSD: patch-cpm.cpp,v 1.1 2011/11/23 23:27:07 joerg Exp $
+$NetBSD: patch-cpm.cpp,v 1.2 2013/08/21 10:23:41 shattered Exp $
 
---- cpm.cpp.orig	2011-11-23 16:40:21.000000000 +0000
+--- cpm.cpp.orig	2005-05-16 20:19:55.000000000 +0000
 +++ cpm.cpp
-@@ -14,6 +14,7 @@
- #include <stdexcept>
+@@ -19,6 +19,7 @@
+ #include <memory>
  
  #include <stdio.h>
 +#include <string.h>
  #include <unistd.h>
- #include <sys/types.h>
- #include <sys/stat.h>
+ #include <errno.h>
+ #include <ctype.h>
