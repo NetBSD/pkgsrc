61e15877ac90f6c29bd8d7355b9c10535a37a3bf
prlw1 <prlw1>
2013-08-22 09:11:16 +0000

    Update poppler to 0.24.0.
    Release 0.24.0
      core:
       * TextOutputDev: Do not draw ligatures more than once when
         selected. Bug #9001
       * PSOutputDev: Make some pdftops conversions much faster
       * PSOutputDev: Initialize t3FillColorOnly
       * SplashOutputDev: Fallback to 1x1 bitmap if we fail to create
         the real size
    
    Release 0.23.4
      core:
       * TextOutputDev: clip the selected text rendering to the selection
         box. Bug #66983
       * CairoImageOutputDev: Fix the bounding box of saved images
    
      build system:
       * Improve linking against pthreads
    
    Release 0.23.3
      core:
       * Annotation improvements
       * Fix crashes on malformed files
       * TextSelectionPainter: Draw glyphs after selection background
       * TextOutputDev: add a method to TextPage to get the selection
         as a list of words
    
      qt5:
       * Initial Qt5 port
    
      qt4:
       * Windows compile fixes
       * Demo: Allow the choose the page rotation
    
      build system:
       * Fix mingw build
       * Minor autotools fixes
    
    Release 0.23.2
      core:
       * SplashOutputDev: Speed-up some tiling on a 10x factor
       * Improve caching of lcms2 ICC color profiles
       * Put some private classes in an anonymous namespace
    
      qt4:
       * Add a thread stresser tool
    
      build system:
       * Fix mingw build
    
    Release 0.23.1
      core:
       * XRef stream writing: Write 32-bit offsets when possible
       * Fix splashModeBGR8 rendering (Bug #64381)
    
      glib:
       * Do not use deprecated gtk_scrolled_window_add_with_viewport()
         (Bug #64683)
    
      build system:
       * Fix Large file support when using cmake
    
    Release 0.23.0
      core:
       * Make rendering thread safe
       * Large file support
       * Implement Crypt filter (Bug #62800)
       * Fix endstream detection (Bug #62985)
       * CairoOutputDev: support uncolored tiling patterns (Bug #59179)
       * SplashOutputDev: Introduce Thin Line mode support (Bug #37347)
    
      qt4:
       * Expose Thin Line mode support

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.36 2013/06/04 22:16:59 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.37 2013/08/22 09:11:16 prlw1 Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.22.4nb2
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.24.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 pkgbase := poppler
