fe497e558c823e151ce11804559d0cc91ef760ce
taca <taca@pkgsrc.org>
2013-08-23 03:11:55 +0000

    Update php54 to 5.4.19.
    22 Aug 2013, PHP 5.4.19
    - Core:
      . Fixed bug #64503 (Compilation fails with error: conflicting types for
        'zendparse'). (Laruence)
    
    - Openssl:
      . Fixed UMR in fix for CVE-2013-4248.

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.41 2013/08/18 20:56:32 taca Exp $
+# $NetBSD: phpversion.mk,v 1.42 2013/08/23 03:11:55 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -82,7 +82,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP53_VERSION=	5.3.27
-PHP54_VERSION=	5.4.18
+PHP54_VERSION=	5.4.19
 PHP55_VERSION=	5.5.2
 
 # Define initial release of major version.
