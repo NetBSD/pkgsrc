fdb609e6fcf83e3f1e6446f9a3493553f6301ae1
jaapb <jaapb@pkgsrc.org>
2013-08-24 21:50:24 +0000

    Updated package to latest version, 0.9.9. Removed obsolete patches, took over maintainership (all this okayed by the old maintainer, Jeremy C. Reed).
    Changes from 0.9.2 include (apart from various bugfixes):
    - WPX_SEEK_END for WPXMemoryInputStream (Laurent Alonso)
    - Metadata for wpd2html (David Hislop)
    - Change source code layout so that public headers be in inc/ and its
      subdirectories
    - libwpd.h and libwpd-stream.h are not generated headers anymore
    - Change the way we get version from libwpd build
    - Improvements to WPXOLEStream class
    - Added MPL2+ to the licenses libwpd is released under
    - Revert an accidental ABI change in WPXPropertyList
    - Link the Windows static libraries with DLL runtimes
    - libwpd-stream: Add experimental support for ZIP storages
    - libwpd-stream: Add WPX_SEEK_END to the WPXSeekType enum
      (Be sure that you are at offset 0 using it and check return value
      in order to be compatible with lower versions of libwpd)
    - New Visual Studio 2008 and Visual Studio 2010 build files
    - Don't output points as integer. This solves some rounding issues along
      with possibility of writing 10.5 point font size (Fridrich)
    - General improvement of code quality by buiding with -Weffc++ option (Fridrich)
    - Improvement of textbox code (Laurent Alonso)
    - Improvement of arabic charset mapping (Smokey Ardisson)
    - Build system rewrite and improvement (Tomas Chvatal)
    - Cleanup of return values (Thomas Klausner)
    - Conversion of extended characters in WP 42 parser (Fridrich)

--- converters/libwpd/buildlink3.mk
+++ converters/libwpd/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.15 2010/11/25 10:41:17 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2013/08/24 21:50:24 jaapb Exp $
 
 BUILDLINK_TREE+=	libwpd
 
 .if !defined(LIBWPD_BUILDLINK3_MK)
 LIBWPD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libwpd+=	libwpd>=0.9.0
+BUILDLINK_API_DEPENDS.libwpd+=	libwpd>=0.9.5
 BUILDLINK_PKGSRCDIR.libwpd?=	../../converters/libwpd
 
 .endif # LIBWPD_BUILDLINK3_MK
