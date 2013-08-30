395f7fc74c15bd812448663e2d568100a6e77d5d
joerg <joerg@pkgsrc.org>
2013-08-30 12:59:22 +0000

    Don't use IP_PKTINFO on NetBSD, it doesn't support all required fields.

---
+++ security/mit-krb5/patches/patch-lib_apputils_net-server.c
@@ -0,0 +1,13 @@
+$NetBSD: patch-lib_apputils_net-server.c,v 1.1 2013/08/30 12:59:22 joerg Exp $
+
+--- lib/apputils/net-server.c.orig	2013-08-30 12:14:00.000000000 +0000
++++ lib/apputils/net-server.c
+@@ -1477,7 +1477,7 @@ send_to_from(int s, void *buf, size_t le
+     msg.msg_controllen = 0;
+ 
+     switch (from->sa_family) {
+-#if defined(IP_PKTINFO)
++#if defined(IP_PKTINFO) && !defined(__NetBSD__)
+     case AF_INET:
+         if (fromlen != sizeof(struct sockaddr_in))
+             goto use_sendto;
