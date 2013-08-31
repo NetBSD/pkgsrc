f4746712bdc5a6c09748b4e7205c6a2e88cea4c1
adam <adam@pkgsrc.org>
2013-08-31 22:20:24 +0000

    Changes 0.9.20:
    General:
    - Misc substitute_closure() fixes.
    - Build fixes.
    
    Documentation:
    - gtk-doc boilerplate integrated.  Docs are built now, but
      contain no contents.  By next release hopefully we have
      some content in.  Enable using --enable-gtk-doc.
    
    GObject and Introspection:
    - Added harfbuzz-gobject library (hb-gobject.h) that has type
      bindings for all HarfBuzz objects and enums.  Enable using
      --with-gobject.
    - Added gobject-introspection boilerplate.  Nothing useful
      right now.  Work in progress.  Gets enabled automatically if
      --with-gobject is used.  Override with --disable-introspection.
    
    OpenType shaper:
    - Apply 'mark' in Myanmar shaper.
    - Don't apply 'dlig' by default.
    
    Uniscribe shaper:
    - Support user features.
    - Fix loading of fonts that are also installed on the system.
    - Fix shaping of Arabic Presentation Forms.
    - Fix build with wide chars.
    
    CoreText shaper:
    - Support user features.
    
    Source changes:
    - hb_face_t code moved to hb-face.h / hb-face.cc.
    - Added hb-deprecated.h.
    
    API changes:
    - Added HB_DISABLE_DEPRECATED.
    - Deprecated HB_SCRIPT_CANADIAN_ABORIGINAL; replaced by
      HB_SCRIPT_CANADIAN_SYLLABICS.
    - Deprecated HB_BUFFER_FLAGS_DEFAULT; replaced by
      HB_BUFFER_FLAG_DEFAULT.
    - Deprecated HB_BUFFER_SERIALIZE_FLAGS_DEFAULT; replaced by
      HB_BUFFER_SERIALIZE_FLAG_DEFAULT.

--- fonts/harfbuzz/options.mk
+++ fonts/harfbuzz/options.mk
@@ -1,15 +1,16 @@
-# $NetBSD: options.mk,v 1.1 2013/08/22 15:34:32 jaapb Exp $
+# $NetBSD: options.mk,v 1.2 2013/08/31 22:20:24 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.harfbuzz
 PKG_SUPPORTED_OPTIONS+= icu
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=		icu
+
 .if !empty(PKG_OPTIONS:Micu)
 .include "../../textproc/icu/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-icu=yes
-PKGCONFIG_OVERRIDE+=	src/harfbuzz-icu.pc.in
-PLIST.icu=	yes
+PLIST.icu=		yes
 .else
 CONFIGURE_ARGS+=	--with-icu=no
 .endif
