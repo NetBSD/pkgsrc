0485f9d34c53f86aebb7a161f031294af69894af
adam <adam>
2013-08-31 22:19:32 +0000

    Changes 1.12.16: Bug fixes * Set the correct orientation for simple boxes with a negative scale factor. * Fix the creation of the shading dictionary in PDF. * Fix a crash in PDF when incorporating an image with CAIRO_EXTEND_PAD. * Avoid upscaling bitmap fonts if possible. * Fix an assertion failure within the mempool allocator for shared memory. * Fix allocation size for CFF subsets. * Export cairo_matrix_t for GObject bindings. * Fix a double free in the Quartz backend. * Fix origin of GDI StretchBlits for the Windows backend * Fix error propagation for requests to create a similar surface with   negative size. * Fix complex clipping of trapezoids with regions * Stop leaking the image data when loading PNGs * Fix unbounded operations with a clip mask through the span compositor * Add missing checks before rendering to a finished surface - so we return   an error rather than hit an assert. * Prevent an assertion failure when creating similar GL surfaces larger   than supported by hardware. * Prevent a double free of a similar image under Windows.

--- graphics/cairo-gobject/buildlink3.mk
+++ graphics/cairo-gobject/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2013/06/04 22:16:14 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2013/08/31 22:19:32 adam Exp $
 
 BUILDLINK_TREE+=	cairo-gobject
 
@@ -9,13 +9,8 @@ BUILDLINK_API_DEPENDS.cairo-gobject+=	cairo-gobject>=1.10.2
 BUILDLINK_ABI_DEPENDS.cairo-gobject+=	cairo-gobject>=1.12.14nb4
 BUILDLINK_PKGSRCDIR.cairo-gobject?=	../../graphics/cairo-gobject
 
-.include "../../graphics/cairo/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/zlib/buildlink3.mk"
-.include "../../fonts/fontconfig/buildlink3.mk"
-.include "../../graphics/freetype2/buildlink3.mk"
-.include "../../graphics/png/buildlink3.mk"
-.include "../../x11/pixman/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
 .endif	# CAIRO_GOBJECT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-cairo-gobject
