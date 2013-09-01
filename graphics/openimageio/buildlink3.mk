b7c666cdca4e7e35bb4ddf746e9211511d71e8eb
obache <obache>
2013-09-01 12:14:06 +0000

    recursive bump from hdf5 shlib major bump.

--- graphics/openimageio/buildlink3.mk
+++ graphics/openimageio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2013/06/06 12:53:42 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2013/09/01 12:14:06 obache Exp $
 
 BUILDLINK_TREE+=	openimageio
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	openimageio
 OPENIMAGEIO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.openimageio+=	openimageio>=1.1.1
-BUILDLINK_ABI_DEPENDS.openimageio?=	openimageio>=1.1.7nb4
+BUILDLINK_ABI_DEPENDS.openimageio?=	openimageio>=1.1.7nb6
 BUILDLINK_PKGSRCDIR.openimageio?=	../../graphics/openimageio
 
 .include "../../devel/boost-libs/buildlink3.mk"
