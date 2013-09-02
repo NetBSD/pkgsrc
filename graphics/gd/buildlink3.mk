5653b661fa7e3aad165229cd84363f286b9ec9c3
adam <adam@pkgsrc.org>
2013-09-02 21:17:42 +0000

    Changes 2.1.0: * gdColorMapLookup() answers the RGB values according to given color map * Added support of variable resolution * new filter gdImagePixelate() * merged improvements that PHP GD team had made to GD Graphics Library * bugfixes

--- graphics/gd/buildlink3.mk
+++ graphics/gd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.28 2013/06/04 22:16:17 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.29 2013/09/02 21:17:42 adam Exp $
 
 BUILDLINK_TREE+=	gd
 
@@ -14,16 +14,15 @@ pkgbase := gd
 
 .if !empty(PKG_BUILD_OPTIONS.gd:Mx11)
 .include "../../x11/libXpm/buildlink3.mk"
-#.include "../../x11/libXp/buildlink3.mk"
-#.include "../../mk/xaw.buildlink3.mk"
 .endif
 
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
-.include "../../mk/jpeg.buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
-
+.include "../../graphics/tiff/buildlink3.mk"
+.include "../../multimedia/libvpx/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 .endif # GD_BUILDLINK3_MK
 
