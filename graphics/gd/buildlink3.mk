dedaa2d801e6de44ec67de68645337a613702047
obache <obache@pkgsrc.org>
2013-09-04 12:31:24 +0000

    recursive bump from graphics/gd shlib major bump.

--- graphics/gd/buildlink3.mk
+++ graphics/gd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.29 2013/09/02 21:17:42 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2013/09/04 12:31:24 obache Exp $
 
 BUILDLINK_TREE+=	gd
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gd
 GD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gd+=	gd>=2.0.15nb1
-BUILDLINK_ABI_DEPENDS.gd+=	gd>=2.0.35nb15
+BUILDLINK_ABI_DEPENDS.gd+=	gd>=2.1.0
 BUILDLINK_PKGSRCDIR.gd?=	../../graphics/gd
 
 pkgbase := gd
--- graphics/gdchart/buildlink3.mk
+++ graphics/gdchart/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2013/06/04 22:16:18 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2013/09/04 12:31:24 obache Exp $
 
 BUILDLINK_TREE+=	gdchart
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gdchart
 GDCHART_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gdchart+=	gdchart>=0.11.4nb1
-BUILDLINK_ABI_DEPENDS.gdchart+=	gdchart>=0.11.5nb11
+BUILDLINK_ABI_DEPENDS.gdchart+=	gdchart>=0.11.5nb13
 BUILDLINK_PKGSRCDIR.gdchart?=	../../graphics/gdchart
 
 .include "../../graphics/gd/buildlink3.mk"
