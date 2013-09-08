77e4d7531f953cb2d4d083981620b5678dd96084
ryoon <ryoon@pkgsrc.org>
2013-09-08 16:33:25 +0000

    Update to 0.1pre1718
    Changelog:
    unknown.

--- devel/gyp/patches/patch-pylib_gyp_common.py
+++ devel/gyp/patches/patch-pylib_gyp_common.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-pylib_gyp_common.py,v 1.1 2013/01/18 11:32:45 ryoon Exp $
+$NetBSD: patch-pylib_gyp_common.py,v 1.2 2013/09/08 16:33:25 ryoon Exp $
 
 * Add NetBSD support
 
---- pylib/gyp/common.py.orig	2012-12-15 10:46:11.000000000 +0000
+--- pylib/gyp/common.py.orig	2013-09-06 23:07:36.000000000 +0000
 +++ pylib/gyp/common.py
-@@ -380,6 +380,8 @@ def GetFlavor(params):
-     return 'freebsd'
+@@ -403,6 +403,8 @@ def GetFlavor(params):
+     return 'openbsd'
    if sys.platform.startswith('aix'):
      return 'aix'
 +  if sys.platform.startswith('netbsd'):
