31622f0e8967a9aeb50b3dc7fb27fd84ab0dd86e
taca <taca@pkgsrc.org>
2013-09-10 16:25:51 +0000

    Add a fix for CVE-2013-4287 in rubygems.
    Bump PKGREVISION.

--- lang/ruby193-base/patches/patch-lib_rubygems_version.rb
+++ lang/ruby193-base/patches/patch-lib_rubygems_version.rb
@@ -1,9 +1,19 @@
-$NetBSD: patch-lib_rubygems_version.rb,v 1.1 2013/02/02 08:11:54 taca Exp $
+$NetBSD: patch-lib_rubygems_version.rb,v 1.2 2013/09/10 16:25:51 taca Exp $
 
-A small fix from Ruby 2.0.0 and Rubygem 1.8.25.
+* A fix for CVE-2013-4287.
+* A small fix from Ruby 2.0.0 and Rubygem 1.8.25.
 
 --- lib/rubygems/version.rb.orig	2012-04-19 22:43:16.000000000 +0000
 +++ lib/rubygems/version.rb
+@@ -145,7 +145,7 @@ class Gem::Version
+ 
+   include Comparable
+ 
+-  VERSION_PATTERN = '[0-9]+(\.[0-9a-zA-Z]+)*' # :nodoc:
++  VERSION_PATTERN = '[0-9]+(?>\.[0-9a-zA-Z]+)*' # :nodoc:
+   ANCHORED_VERSION_PATTERN = /\A\s*(#{VERSION_PATTERN})*\s*\z/ # :nodoc:
+ 
+   ##
 @@ -187,7 +187,7 @@ class Gem::Version
      raise ArgumentError, "Malformed version number string #{version}" unless
        self.class.correct?(version)
