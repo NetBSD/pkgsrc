39bb8b17eb76f23b10b037b5ddfdfec6c8b10753
ryoon <ryoon@pkgsrc.org>
2013-09-11 15:50:32 +0000

    Update to 1.2.0
    * Buildlink to devel/googletest to test-libvpx (not installed and not tested).
    
    Changelog:
    2012-12-21 v1.2.0
      This release acts as a checkpoint for a large amount of internal refactoring
      and testing. It also contains a number of small bugfixes, so all users are
      encouraged to upgrade.
    
      - Upgrading:
        This release is ABI and API compatible with Duclair (v1.0.0). Users
        of older releases should refer to the Upgrading notes in this
        document for that release.
    
      - Enhancements:
          VP8 optimizations for MIPS dspr2
          vpxenc: add -quiet option
    
      - Speed:
          Encoder and decoder speed is consistent with the Eider release.
    
      - Quality:
          In general, quality is consistent with the Eider release.
    
          Minor tweaks to ARNR filtering
          Minor improvements to real time encoding with multiple temporal layers
    
      - Bug Fixes:
          Fixes multithreaded encoder race condition in loopfilter
          Fixes multi-resolution threaded encoding
          Fix potential encoder dead-lock after picture resize

--- multimedia/libvpx/patches/patch-build_make_gen_asm_deps.sh
+++ multimedia/libvpx/patches/patch-build_make_gen_asm_deps.sh
@@ -1,17 +1,17 @@
-$NetBSD: patch-build_make_gen_asm_deps.sh,v 1.1 2013/01/24 10:22:08 tron Exp $
+$NetBSD: patch-build_make_gen_asm_deps.sh,v 1.2 2013/09/11 15:50:32 ryoon Exp $
 
 Fix build problems under Mac OS X with Xcode 4.4 and newer. Patch
 taken from here:
 
 https://github.com/mxcl/homebrew/issues/12567#issuecomment-6434000
 
---- build/make/gen_asm_deps.sh.old	2012-05-09 00:14:00.000000000 +0100
-+++ build/make/gen_asm_deps.sh	2013-01-24 10:02:54.000000000 +0000
-@@ -42,7 +42,7 @@
+--- build/make/gen_asm_deps.sh.orig	2013-09-04 18:49:22.000000000 +0000
++++ build/make/gen_asm_deps.sh
+@@ -42,7 +42,7 @@ done
  
  [ -n "$srcfile" ] || show_help
  sfx=${sfx:-asm}
--includes=$(LC_ALL=C egrep -i "include +\"?+[a-z0-9_/]+\.${sfx}" $srcfile |
+-includes=$(LC_ALL=C egrep -i "include +\"?[a-z0-9_/]+\.${sfx}" $srcfile |
 +includes=$(LC_ALL=C egrep -i "include +\"[a-z0-9_/]+\.${sfx}" $srcfile |
             perl -p -e "s;.*?([a-z0-9_/]+.${sfx}).*;\1;")
  #" restore editor state
