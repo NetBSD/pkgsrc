df7bd73b6b0d7a143b0545339f827a2e358762fa
drochner <drochner@pkgsrc.org>
2013-09-11 18:00:33 +0000

    update to 4.1.6.1 This release fixes the following critical vulnerabilities: CVE-2013-1918 / XSA-45: Several long latency operations are not  preemptible CVE-2013-1952 / XSA-49: VT-d interrupt remapping source validation flaw  for bridges CVE-2013-2076 / XSA-52: Information leak on XSAVE/XRSTOR capable AMD CPUs CVE-2013-2077 / XSA-53: Hypervisor crash due to missing exception  recovery on XRSTOR CVE-2013-2078 / XSA-54: Hypervisor crash due to missing exception  recovery on XSETBV CVE-2013-2194, CVE-2013-2195, CVE-2013-2196 / XSA-55: Multiple  vulnerabilities in libelf PV kernel handling CVE-2013-2072 / XSA-56: Buffer overflow in xencontrol Python bindings  affecting xend CVE-2013-2211 / XSA-57: libxl allows guest write access to sensitive  console related xenstore keys CVE-2013-1432 / XSA-58: Page reference counting error due to  XSA-45/CVE-2013-1918 fixes XSA-61: libxl partially sets up HVM passthrough even with disabled iommu This release contains many bug fixes and improvements. The highlights are: addressing a regression from the fix for XSA-21 addressing a regression from the fix for XSA-46 bug fixes to low level system state handling, including certain  hardware errata workarounds
    (CVE-2013-1918 and CVE-2013-1952 were patched in pkgsrc before)

--- sysutils/xentools41/version.mk
+++ sysutils/xentools41/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.6 2013/05/03 16:48:38 drochner Exp $
+# $NetBSD: version.mk,v 1.7 2013/09/11 18:00:33 drochner Exp $
 
 # Version number is used by xentools41 and xenstoretools
 
-VERSION=	4.1.5
+VERSION=	4.1.6.1
 VERSION_IPXE=	1.0.0
