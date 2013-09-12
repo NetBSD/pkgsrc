5bc82238079ed8a954aa56a4fb53a4ebe966f34f
jperkin <jperkin@pkgsrc.org>
2013-09-12 11:26:30 +0000

    Restore the catman page installation.  This reverts the fix for PR#42533, and ensures that the manual pages can now be read on SunOS, however it should not affect HP-UX support as this package now pulls in nroff as a tool to correctly generate the catman page.
    Bump PKGREVISION.

--- lang/f2c/files/f2c.mk
+++ lang/f2c/files/f2c.mk
@@ -1,6 +1,7 @@
 # Makefile for f2c, a Fortran 77 to C converter
 
 PROG=	f2c
+MAN=	f2c.1
 SRCS=	main.c init.c gram.c lex.c proc.c equiv.c data.c format.c \
 	expr.c exec.c intr.c io.c misc.c error.c mem.c names.c \
 	output.c p1output.c pread.c put.c putpcc.c vax.c formatdata.c \
