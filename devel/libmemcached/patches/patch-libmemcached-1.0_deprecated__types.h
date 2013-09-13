486d8753976e29c8de067643049ef9a68475d703
fhajny <fhajny@pkgsrc.org>
2013-09-13 07:59:01 +0000

    Fix typo in patch file. Bump PKGREVISION.

--- devel/libmemcached/patches/patch-libmemcached-1.0_deprecated__types.h
+++ devel/libmemcached/patches/patch-libmemcached-1.0_deprecated__types.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-libmemcached-1.0_deprecated__types.h,v 1.1 2013/09/12 14:20:56 fhajny Exp $
+$NetBSD: patch-libmemcached-1.0_deprecated__types.h,v 1.2 2013/09/13 07:59:01 fhajny Exp $
 
 Add type to overcome a breaking API change.
 https://bugs.launchpad.net/libmemcached/+bug/1190240
@@ -8,5 +8,5 @@ https://bugs.launchpad.net/libmemcached/+bug/1190240
  typedef memcached_trigger_key_fn memcached_trigger_key;
  typedef memcached_trigger_delete_key_fn memcached_trigger_delete_key;
  typedef memcached_dump_fn memcached_dump_func;
-+typedef memcached_instance_st memcached_server_instance_st;
++typedef memcached_instance_st *memcached_server_instance_st;
  
