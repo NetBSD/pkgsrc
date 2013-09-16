608a98a165beed638945bed347e246e2e585c61c
fhajny <fhajny@pkgsrc.org>
2013-09-16 16:46:32 +0000

    Fix bl3 for the sasl option.

--- devel/libmemcached/buildlink3.mk
+++ devel/libmemcached/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/02/07 11:34:00 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/09/16 16:46:32 fhajny Exp $
 
 BUILDLINK_TREE+=	libmemcached
 
@@ -8,6 +8,13 @@ LIBMEMCACHED_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libmemcached+=	libmemcached>=0.53
 BUILDLINK_ABI_DEPENDS.libmemcached+=	libmemcached>=0.53
 BUILDLINK_PKGSRCDIR.libmemcached?=	../../devel/libmemcached
+
+pkgbase:=		libmemcached
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.libmemcached:Msasl)
+.  include "../../security/cyrus-sasl/buildlink3.mk"
+.endif
 .endif # LIBMEMCACHED_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libmemcached
