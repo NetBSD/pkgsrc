0b4cc3360522f77b00ee33445e92f5611e9aed3e
fhajny <fhajny@pkgsrc.org>
2013-09-17 11:58:57 +0000

    Fix build with -Woverloaded-virtual. Add missing openssl & pcre dependencies. Bump PKGREVISION.

--- devel/rudiments/buildlink3.mk
+++ devel/rudiments/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:24:29 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2013/09/17 11:58:57 fhajny Exp $
 #
 # This Makefile fragment is included by packages that use rudiments.
 #
@@ -13,6 +13,9 @@ RUDIMENTS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.rudiments+=		rudiments>=0.25.1
 BUILDLINK_ABI_DEPENDS.rudiments+=	rudiments>=0.28.2nb1
 BUILDLINK_PKGSRCDIR.rudiments?=		../../devel/rudiments
+
+.include "../../devel/pcre/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
 .endif # RUDIMENTS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-rudiments
