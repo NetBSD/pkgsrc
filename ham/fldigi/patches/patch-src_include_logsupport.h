deeae013a573ed2a4fd86ec5bed7449a266ad6b2
joerg <joerg@pkgsrc.org>
2013-09-17 21:12:25 +0000

    Don't depend on implicit boolean conversion for streams. Fix a header guard to reduce warnings.

---
+++ ham/fldigi/patches/patch-src_include_logsupport.h
@@ -0,0 +1,11 @@
+$NetBSD: patch-src_include_logsupport.h,v 1.1 2013/09/17 21:12:25 joerg Exp $
+
+--- src/include/logsupport.h.orig	2013-09-17 19:27:33.000000000 +0000
++++ src/include/logsupport.h
+@@ -1,5 +1,5 @@
+ #ifndef SUPPORT_H
+-#define SUPPORT_h
++#define SUPPORT_H
+ 
+ #include <string>
+ 
