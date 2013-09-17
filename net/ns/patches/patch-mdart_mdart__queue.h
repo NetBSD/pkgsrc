3321d77dc57e6f2c37e9e2d7533251939f92f2f1
joerg <joerg>
2013-09-17 21:13:45 +0000

    Fix a header guard. Remove non-portable comparision to NULL iterator. Be explicit about using the custom hash.

---
+++ net/ns/patches/patch-mdart_mdart__queue.h
@@ -0,0 +1,13 @@
+$NetBSD: patch-mdart_mdart__queue.h,v 1.1 2013/09/17 21:13:45 joerg Exp $
+
+--- mdart/mdart_queue.h.orig	2013-09-17 20:00:56.000000000 +0000
++++ mdart/mdart_queue.h
+@@ -45,7 +45,7 @@
+ 
+ 
+ #ifndef __mdart_rqueue_h__
+-#define __mdart_rqueue_h_
++#define __mdart_rqueue_h__
+ 
+ 
+ 
