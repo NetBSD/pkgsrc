7a485fbd6d988e43acf904098c9c1a461e119923
joerg <joerg@pkgsrc.org>
2013-09-17 21:10:32 +0000

    Needs _KMEMUSER to get kinfo_proc on NetBSD/current.

---
+++ filesystems/glusterfs/patches/patch-xlators_mount_fuse_src_fuse-helpers.c
@@ -0,0 +1,15 @@
+$NetBSD: patch-xlators_mount_fuse_src_fuse-helpers.c,v 1.1 2013/09/17 21:10:32 joerg Exp $
+
+--- xlators/mount/fuse/src/fuse-helpers.c.orig	2013-09-17 13:41:20.000000000 +0000
++++ xlators/mount/fuse/src/fuse-helpers.c
+@@ -7,6 +7,10 @@
+    later), or the GNU General Public License, version 2 (GPLv2), in all
+    cases as published by the Free Software Foundation.
+ */
++#ifdef __NetBSD__
++#define _KMEMUSER
++#endif
++
+ #include "fuse-bridge.h"
+ #if defined(GF_SOLARIS_HOST_OS)
+ #include <sys/procfs.h>
