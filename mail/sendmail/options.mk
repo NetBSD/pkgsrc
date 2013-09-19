889560a30caecb93508d1627de725fa1bd064fa9
manu <manu@pkgsrc.org>
2013-09-19 10:54:05 +0000

    Add a ffr_tls_1 option to sendmail, which enable the O CipherList option for sendmail.cf. it is required in order to remove weak ciphers, and enforce Forward Secrecy on modern MUA
    Usage example:
    O CipherList=DH@STRENGTH:HIGH:!MD5:!DES:!aNULL:!eNULL

--- mail/sendmail/options.mk
+++ mail/sendmail/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.19 2012/12/28 05:27:06 jnemeth Exp $
+# $NetBSD: options.mk,v 1.20 2013/09/19 10:54:05 manu Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sendmail
-PKG_SUPPORTED_OPTIONS=	inet6 db2 ldap sasl tls tcpwrappers
+PKG_SUPPORTED_OPTIONS=	inet6 db2 ldap sasl tls tcpwrappers  ffr_tls_1
 PKG_SUGGESTED_OPTIONS=	inet6 tcpwrappers
 
 PKG_OPTIONS_LEGACY_OPTS+=	starttls:tls
@@ -35,6 +35,11 @@ PKG_OPTIONS_LEGACY_OPTS+=	starttls:tls
 .if !empty(PKG_OPTIONS:Mtls)
 .  include "../../security/openssl/buildlink3.mk"
 .endif
+
+###
+### 'For Future Release' FFR_TLS_1 options: CipherList, multiple certs
+###
+# Nothing to do here, activation is done in Makefile
 
 ###
 ### Use tcpwrappers for network access control to sendmail
