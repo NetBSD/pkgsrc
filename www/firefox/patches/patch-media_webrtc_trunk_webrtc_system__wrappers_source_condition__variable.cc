73a670f2250735b41a4f8a99408b186d160e5e0b
ryoon <ryoon@pkgsrc.org>
2013-09-19 12:37:49 +0000

    Update to 24.0, ESR edition.
    * Merge some patches via FreeBSD ports.
    * Tested on NetBSD/amd64 6.99.23 and DragonFly/amd64 3.4.1.
    * Use system hunspell dictionaries.
    * DuckDuckGo search window.
    * Enable system icu support.
    
    Changelog:
    NEW
    Support for new scrollbar style in Mac OS X 10.7 and newer
    NEW
    Implemented Close tabs to the right
    NEW
    Social: Ability to tear-off chat windows to view separately by simply dragging them out
    CHANGED
    Accessibility related improvements on using pinned tabs (see 577727)
    CHANGED
    Removed support for Revocation Lists feature (see 867465)
    CHANGED
    Performance improvements on New Tab Page loads (see 791670)
    DEVELOPER
    Major SVG rendering improvements around Image tiling and scaling (see 600207 )
    DEVELOPER
    Improved and unified Browser console for enhanced debugging experience, replacing existing Error console
    DEVELOPER
    Removed support for sherlock files that are loaded from application or profile directory
    FIXED
    Replace fixed-ratio audio resampler in webrtc.org capture code with Speex resampler and eliminate pseudo-44000Hz rate ( see 886886)
    FIXED
    24.0: Security fixes can be found here
    
    Fixed in Firefox 24
    MFSA 2013-92 GC hazard with default compartments and frame chain restoration
    MFSA 2013-91 User-defined properties on DOM proxies get the wrong "this" object
    MFSA 2013-90 Memory corruption involving scrolling
    MFSA 2013-89 Buffer overflow with multi-column, lists, and floats
    MFSA 2013-88 compartment mismatch re-attaching XBL-backed nodes
    MFSA 2013-87 Shared object library loading from writable location
    MFSA 2013-86 WebGL Information disclosure through OS X NVIDIA graphic drivers
    MFSA 2013-85 Uninitialized data in IonMonkey
    MFSA 2013-84 Same-origin bypass through symbolic links
    MFSA 2013-83 Mozilla Updater does not lock MAR file after signature verification
    MFSA 2013-82 Calling scope for new Javascript objects can lead to memory corruption
    MFSA 2013-81 Use-after-free with select element
    MFSA 2013-80 NativeKey continues handling key messages after widget is destroyed
    MFSA 2013-79 Use-after-free in Animation Manager during stylesheet cloning
    MFSA 2013-78 Integer overflow in ANGLE library
    MFSA 2013-77 Improper state in HTML5 Tree Builder with templates
    MFSA 2013-76 Miscellaneous memory safety hazards (rv:24.0 / rv:17.0.9)

--- www/firefox/patches/patch-media_webrtc_trunk_webrtc_system__wrappers_source_condition__variable.cc
+++ www/firefox/patches/patch-media_webrtc_trunk_webrtc_system__wrappers_source_condition__variable.cc
@@ -1,33 +1,13 @@
-$NetBSD: patch-media_webrtc_trunk_webrtc_system__wrappers_source_condition__variable.cc,v 1.1 2013/05/23 13:12:13 ryoon Exp $
+$NetBSD: patch-media_webrtc_trunk_webrtc_system__wrappers_source_condition__variable.cc,v 1.2 2013/09/19 12:37:50 ryoon Exp $
 
---- media/webrtc/trunk/webrtc/system_wrappers/source/condition_variable.cc.orig	2013-05-11 19:19:46.000000000 +0000
+--- media/webrtc/trunk/webrtc/system_wrappers/source/condition_variable.cc.orig	2013-09-10 03:43:47.000000000 +0000
 +++ media/webrtc/trunk/webrtc/system_wrappers/source/condition_variable.cc
-@@ -8,14 +8,16 @@
-  *  be found in the AUTHORS file in the root of the source tree.
-  */
- 
-+#include "condition_variable_wrapper.h"
-+
- #if defined(_WIN32)
- #include <windows.h>
- #include "condition_variable_win.h"
--#include "condition_variable_wrapper.h"
--#elif defined(WEBRTC_LINUX) || defined(WEBRTC_MAC)
-+#elif defined(WEBRTC_LINUX) || defined(WEBRTC_BSD) || defined(WEBRTC_MAC)
+@@ -16,6 +16,8 @@
+ #elif defined(WEBRTC_LINUX) || defined(WEBRTC_BSD) || defined(WEBRTC_MAC)
  #include <pthread.h>
  #include "condition_variable_posix.h"
--#include "condition_variable_wrapper.h"
 +#else
 +#include <stddef.h>  // for NULL
  #endif
  
  namespace webrtc {
-@@ -23,7 +25,7 @@ namespace webrtc {
- ConditionVariableWrapper* ConditionVariableWrapper::CreateConditionVariable() {
- #if defined(_WIN32)
-   return new ConditionVariableWindows;
--#elif defined(WEBRTC_LINUX) || defined(WEBRTC_MAC)
-+#elif defined(WEBRTC_LINUX) || defined(WEBRTC_BSD) || defined(WEBRTC_MAC)
-   return ConditionVariablePosix::Create();
- #else
-   return NULL;
