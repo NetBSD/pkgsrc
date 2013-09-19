b392cbae3ee6d3dc757d1ca2d9d5c980eb5aafe9
ryoon <ryoon@pkgsrc.org>
2013-09-19 12:37:49 +0000

    Update to 24.0, ESR edition.
    * Merge some patches via FreeBSD ports.
    * Tested on NetBSD/amd64 6.99.23 and DragonFly/amd64 3.4.1.
    * Use system hunspell dictionaries.
    * DuckDuckGo search window.
    * Enable system icu support.
    
    Changelog:
    NEW
    Support for new scrollbar style in Mac OS X 10.7 and newer
    NEW
    Implemented Close tabs to the right
    NEW
    Social: Ability to tear-off chat windows to view separately by simply dragging them out
    CHANGED
    Accessibility related improvements on using pinned tabs (see 577727)
    CHANGED
    Removed support for Revocation Lists feature (see 867465)
    CHANGED
    Performance improvements on New Tab Page loads (see 791670)
    DEVELOPER
    Major SVG rendering improvements around Image tiling and scaling (see 600207 )
    DEVELOPER
    Improved and unified Browser console for enhanced debugging experience, replacing existing Error console
    DEVELOPER
    Removed support for sherlock files that are loaded from application or profile directory
    FIXED
    Replace fixed-ratio audio resampler in webrtc.org capture code with Speex resampler and eliminate pseudo-44000Hz rate ( see 886886)
    FIXED
    24.0: Security fixes can be found here
    
    Fixed in Firefox 24
    MFSA 2013-92 GC hazard with default compartments and frame chain restoration
    MFSA 2013-91 User-defined properties on DOM proxies get the wrong "this" object
    MFSA 2013-90 Memory corruption involving scrolling
    MFSA 2013-89 Buffer overflow with multi-column, lists, and floats
    MFSA 2013-88 compartment mismatch re-attaching XBL-backed nodes
    MFSA 2013-87 Shared object library loading from writable location
    MFSA 2013-86 WebGL Information disclosure through OS X NVIDIA graphic drivers
    MFSA 2013-85 Uninitialized data in IonMonkey
    MFSA 2013-84 Same-origin bypass through symbolic links
    MFSA 2013-83 Mozilla Updater does not lock MAR file after signature verification
    MFSA 2013-82 Calling scope for new Javascript objects can lead to memory corruption
    MFSA 2013-81 Use-after-free with select element
    MFSA 2013-80 NativeKey continues handling key messages after widget is destroyed
    MFSA 2013-79 Use-after-free in Animation Manager during stylesheet cloning
    MFSA 2013-78 Integer overflow in ANGLE library
    MFSA 2013-77 Improper state in HTML5 Tree Builder with templates
    MFSA 2013-76 Miscellaneous memory safety hazards (rv:24.0 / rv:17.0.9)

--- www/firefox/patches/patch-media_mtransport_third__party_nICEr_src_stun_addrs.c
+++ www/firefox/patches/patch-media_mtransport_third__party_nICEr_src_stun_addrs.c
@@ -1,21 +1,36 @@
-$NetBSD: patch-media_mtransport_third__party_nICEr_src_stun_addrs.c,v 1.2 2013/06/26 11:32:12 ryoon Exp $
+$NetBSD: patch-media_mtransport_third__party_nICEr_src_stun_addrs.c,v 1.3 2013/09/19 12:37:50 ryoon Exp $
 
---- media/mtransport/third_party/nICEr/src/stun/addrs.c.orig	2013-06-17 22:13:19.000000000 +0000
+--- media/mtransport/third_party/nICEr/src/stun/addrs.c.orig	2013-09-10 03:43:46.000000000 +0000
 +++ media/mtransport/third_party/nICEr/src/stun/addrs.c
-@@ -54,11 +54,15 @@ static char *RCSSTRING __UNUSED__="$Id: 
- #include <linux/sysctl.h>
+@@ -55,7 +55,13 @@ static char *RCSSTRING __UNUSED__="$Id: 
  #endif
  #include <net/if.h>
--#ifndef LINUX
-+#if defined(__FreeBSD__)
+ #ifndef LINUX
++#if !defined(__OpenBSD__) && !defined(__NetBSD__)
  #include <net/if_var.h>
- #include <net/if_dl.h>
- #include <net/if_types.h>
- #include <sys/sockio.h>
-+#elif defined(__NetBSD__)
++#endif
 +#include <net/if_dl.h>
 +#include <net/if_types.h>
 +#include <sys/sockio.h>
- #else
- #include <linux/if.h>
- #endif
++#elif defined(__NetBSD__)
+ #include <net/if_dl.h>
+ #include <net/if_types.h>
+ #include <sys/sockio.h>
+@@ -80,7 +86,7 @@ static char *RCSSTRING __UNUSED__="$Id: 
+ 
+ 
+ 
+-#ifdef DARWIN
++#if defined(BSD) || defined(DARWIN)
+ /*
+  * Copyright (c) 1983, 1993
+  *    The Regents of the University of California.  All rights reserved.
+@@ -695,7 +701,7 @@ nr_stun_get_addrs(nr_transport_addr addr
+     int _status=0;
+     int i;
+ 
+-#ifdef DARWIN
++#if defined(BSD) || defined(DARWIN)
+     _status = stun_get_mib_addrs(addrs, maxaddrs, count);
+ #elif defined(WIN32)
+     _status = stun_get_win32_addrs(addrs, maxaddrs, count);
