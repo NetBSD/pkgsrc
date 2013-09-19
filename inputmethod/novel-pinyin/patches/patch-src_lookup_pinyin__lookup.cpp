5ff7bd3431542ce77e5bac60d6f63d1baf7694e0
obache <obache>
2013-09-19 13:01:01 +0000

    resolve "call of overloaded 'log(int)' is ambiguous".

--- inputmethod/novel-pinyin/patches/patch-src_lookup_pinyin__lookup.cpp
+++ inputmethod/novel-pinyin/patches/patch-src_lookup_pinyin__lookup.cpp
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_lookup_pinyin__lookup.cpp,v 1.1 2013/03/20 05:25:58 obache Exp $
+$NetBSD: patch-src_lookup_pinyin__lookup.cpp,v 1.2 2013/09/19 13:01:01 obache Exp $
 
 * use C++ style include files.
+* resolve "call of overloaded `log(int)' is ambiguous"
 * fixes bogus NULL usage, as Integer 0.
 
 --- src/lookup/pinyin_lookup.cpp.orig	2009-03-08 03:43:51.000000000 +0000
@@ -16,6 +17,15 @@ $NetBSD: patch-src_lookup_pinyin__lookup.cpp,v 1.1 2013/03/20 05:25:58 obache Ex
  #include <iostream>
  #include "stl_lite.h"
  #include "novel_types.h"
+@@ -158,7 +158,7 @@ bool PinyinLookup::get_best_match(Pinyin
+     }
+     
+     lookup_key_t initial_key = sentence_start;
+-    lookup_value_t initial_value(log(1));
++    lookup_value_t initial_value(log((gfloat)1));
+     initial_value.m_handles[1] = sentence_start;
+     GArray * initial_step_content = (GArray *) g_ptr_array_index(m_steps_content, 0);
+     initial_step_content = g_array_append_val(initial_step_content, initial_value);
 @@ -379,7 +379,7 @@ bool PinyinLookup::final_step(MatchResul
      g_array_set_size(results, m_steps_content->len);
      for ( size_t i = 0 ; i < m_steps_content->len ; ++i){
