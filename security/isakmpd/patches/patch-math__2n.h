db10c2a1d22271f6eb765fbed38f89627ef6cc4e
joerg <joerg@pkgsrc.org>
2013-09-20 23:07:44 +0000

    Fix include guards. Fix format string. Fix uninitialised variables. Not MAKE_JOBS_SAFE. Bump revision.

---
+++ security/isakmpd/patches/patch-math__2n.h
@@ -0,0 +1,13 @@
+$NetBSD: patch-math__2n.h,v 1.1 2013/09/20 23:07:44 joerg Exp $
+
+--- math_2n.h.orig	2013-09-20 20:49:55.000000000 +0000
++++ math_2n.h
+@@ -30,7 +30,7 @@
+  * This code was written under funding by Ericsson Radio Systems.
+  */
+ 
+-#ifndef _MATH_2N_H
++#ifndef _MATH_2N_H_
+ #define _MATH_2N_H_
+ 
+ /*
