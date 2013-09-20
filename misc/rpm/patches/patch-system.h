df9eddc79a15046798f4c769d4ad5a15da8636c2
ryoon <ryoon@pkgsrc.org>
2013-09-20 17:26:14 +0000

    Fix build on OmniOS.
    * Add -fno-stack-protector to CFLAGS to fix link error for SunOS.
    * Patch configure.ac and regenerate the related stuff.
    * Buildlink sysutils/file for libmagic.
    * Detect dirfd, setprogname and htonll in configure script.
    * Create PLIST for NetBSD and SunOS.

--- misc/rpm/patches/patch-system.h
+++ misc/rpm/patches/patch-system.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-system.h,v 1.1 2013/09/13 12:41:32 ryoon Exp $
+$NetBSD: patch-system.h,v 1.2 2013/09/20 17:26:14 ryoon Exp $
 
-* NetBSD has setprogname(3).
+* Detect setprogname(3) in configure script.
 
 --- system.h.orig	2013-01-30 15:33:12.000000000 +0000
 +++ system.h
@@ -9,7 +9,7 @@ $NetBSD: patch-system.h,v 1.1 2013/09/13 12:41:32 ryoon Exp $
  
  /* Retrofit glibc __progname */
 -#if defined __GLIBC__ && __GLIBC__ >= 2
-+#if (defined __GLIBC__ && __GLIBC__ >= 2) || defined(__NetBSD__)
++#if (defined __GLIBC__ && __GLIBC__ >= 2) || defined(HAVE_SETPROGNAME)
  #if __GLIBC_MINOR__ >= 1
  #define	__progname	__assert_program_name
  #endif
