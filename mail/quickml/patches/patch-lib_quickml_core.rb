5ca7c18aa02b8a5807b7ef372dc1fcbbcef2a940
obache <obache>
2013-09-26 09:42:23 +0000

    Update patchest for Ruby 1.9 from Miwa Susumu.
    Bump PKGREVISION.

--- mail/quickml/patches/patch-lib_quickml_core.rb
+++ mail/quickml/patches/patch-lib_quickml_core.rb
@@ -1,6 +1,7 @@
-$NetBSD: patch-lib_quickml_core.rb,v 1.1 2012/08/11 13:51:20 obache Exp $
+$NetBSD: patch-lib_quickml_core.rb,v 1.2 2013/09/26 09:42:23 obache Exp $
 
 * specify character coding system with ruby-1.9 way
+* use fileutils instead of deprecated ftools
 
 --- lib/quickml/core.rb.orig	2004-06-08 20:15:11.000000000 +0000
 +++ lib/quickml/core.rb
@@ -16,4 +17,45 @@ $NetBSD: patch-lib_quickml_core.rb,v 1.1 2012/08/11 13:51:20 obache Exp $
 -$KCODE='e'
  require 'quickml/utils'
  require 'quickml/gettext'
+ 
+@@ -318,7 +318,7 @@ module QuickML
+     end
+ 
+     def remove_alertedp_file
+-      File.safe_unlink(@alertedp_file)
++      FileUtils.safe_unlink(@alertedp_file)
+     end
+ 
+     def _submit (mail)
+@@ -545,13 +545,13 @@ module QuickML
+     end
+ 
+     def close
+-      File.safe_unlink(@members_file)
+-      File.safe_unlink(@count_file)
+-      File.safe_unlink(@charset_file)
+-      File.safe_unlink(@alertedp_file)
+-      File.safe_unlink(@waiting_members_file)
+-      File.safe_unlink(@waiting_message_file)
+-      File.safe_unlink(@ml_config_file)
++      FileUtils.safe_unlink(@members_file)
++      FileUtils.safe_unlink(@count_file)
++      FileUtils.safe_unlink(@charset_file)
++      FileUtils.safe_unlink(@alertedp_file)
++      FileUtils.safe_unlink(@waiting_members_file)
++      FileUtils.safe_unlink(@waiting_message_file)
++      FileUtils.safe_unlink(@ml_config_file)
+       @logger.log "[#{@name}]: ML Closed"
+     end
+ 
+@@ -610,8 +610,8 @@ module QuickML
+         end
+       }
+       submit(mail)
+-      File.safe_unlink(@waiting_members_file)
+-      File.safe_unlink(@waiting_message_file)
++      FileUtils.safe_unlink(@waiting_members_file)
++      FileUtils.safe_unlink(@waiting_message_file)
+       @logger.log "[#{@name}]: Accept confirmation: #{@addressconfirmation_address} #{@address}"
+     end
  
--- mail/quickml/patches/patch-lib_quickml_mail.rb
+++ mail/quickml/patches/patch-lib_quickml_mail.rb
@@ -1,6 +1,7 @@
-$NetBSD: patch-lib_quickml_mail.rb,v 1.1 2012/08/11 13:51:20 obache Exp $
+$NetBSD: patch-lib_quickml_mail.rb,v 1.2 2013/09/26 09:42:23 obache Exp $
 
 * specify character coding system with ruby-1.9 way
+* to_a has been removed from the String
 
 --- lib/quickml/mail.rb.orig	2004-06-07 08:50:59.000000000 +0000
 +++ lib/quickml/mail.rb
@@ -9,3 +10,12 @@ $NetBSD: patch-lib_quickml_mail.rb,v 1.1 2012/08/11 13:51:20 obache Exp $
  #
  # quickml/mail - a part of quickml server
  #
+@@ -284,7 +285,7 @@ module QuickML
+       def encode_field (field)
+ 	field.toeuc.gsub(/[¡¡-ô¤]\S*\s*/) {|x|
+ 	  x.scan(/.{1,10}/).map {|y|
+-	    "=?iso-2022-jp?B?" + y.tojis.to_a.pack('m').chomp + "?="
++	    "=?iso-2022-jp?B?" + [y.tojis].pack('m').chomp + "?="
+ 	  }.join("\n ")
+ 	}
+       end
