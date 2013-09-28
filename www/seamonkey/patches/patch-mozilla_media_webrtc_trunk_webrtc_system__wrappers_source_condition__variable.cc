9b5b3114cbd6d435eb6a656af54599f3dc78049a
ryoon <ryoon>
2013-09-28 14:37:05 +0000

    Update to 2.21
    Changelog:
    SeaMonkey-specific changes
    
        Implemented an option to thread messages received by date.
        Allowed deletion of news posts by default.
        Implemented optional taskbar preview-per-tab.
        Added support (permission prompt) for desktop notifications.
        Added Isn't operator for searching by Priority.
        See the changes page for a more complete overview.
    
    Mozilla platform changes
    
        Support for new scrollbar style on Mac OS X 10.7 and newer.
        Accessibility related improvements on using pinned tabs (bug 577727).
        Major SVG rendering improvements around Image tiling and scaling (bug 600207).
        Removed support for sherlock files that are loaded from application or profile directory.
        Support for W3C touch events disabled (bug 888304).
        Fixed several stability issues.
    
    Fixed in SeaMonkey 2.21
    MFSA 2013-92 GC hazard with default compartments and frame chain restoration
    MFSA 2013-91 User-defined properties on DOM proxies get the wrong "this" object
    MFSA 2013-90 Memory corruption involving scrolling
    MFSA 2013-89 Buffer overflow with multi-column, lists, and floats
    MFSA 2013-88 compartment mismatch re-attaching XBL-backed nodes
    MFSA 2013-85 Uninitialized data in IonMonkey
    MFSA 2013-83 Mozilla Updater does not lock MAR file after signature verification
    MFSA 2013-82 Calling scope for new Javascript objects can lead to memory corruption
    MFSA 2013-81 Use-after-free with select element
    MFSA 2013-80 NativeKey continues handling key messages after widget is destroyed
    MFSA 2013-79 Use-after-free in Animation Manager during stylesheet cloning
    MFSA 2013-78 Integer overflow in ANGLE library
    MFSA 2013-77 Improper state in HTML5 Tree Builder with templates
    MFSA 2013-76 Miscellaneous memory safety hazards (rv:24.0 / rv:17.0.9)

--- www/seamonkey/patches/patch-mozilla_media_webrtc_trunk_webrtc_system__wrappers_source_condition__variable.cc
+++ www/seamonkey/patches/patch-mozilla_media_webrtc_trunk_webrtc_system__wrappers_source_condition__variable.cc
@@ -1,33 +1,13 @@
-$NetBSD: patch-mozilla_media_webrtc_trunk_webrtc_system__wrappers_source_condition__variable.cc,v 1.1 2013/05/23 13:25:30 ryoon Exp $
+$NetBSD: patch-mozilla_media_webrtc_trunk_webrtc_system__wrappers_source_condition__variable.cc,v 1.2 2013/09/28 14:37:05 ryoon Exp $
 
---- mozilla/media/webrtc/trunk/webrtc/system_wrappers/source/condition_variable.cc.orig	2013-05-03 03:08:07.000000000 +0000
+--- mozilla/media/webrtc/trunk/webrtc/system_wrappers/source/condition_variable.cc.orig	2013-09-16 18:26:51.000000000 +0000
 +++ mozilla/media/webrtc/trunk/webrtc/system_wrappers/source/condition_variable.cc
-@@ -8,14 +8,16 @@
-  *  be found in the AUTHORS file in the root of the source tree.
-  */
- 
-+#include "condition_variable_wrapper.h"
-+
- #if defined(_WIN32)
- #include <windows.h>
- #include "condition_variable_win.h"
--#include "condition_variable_wrapper.h"
--#elif defined(WEBRTC_LINUX) || defined(WEBRTC_MAC)
-+#elif defined(WEBRTC_LINUX) || defined(WEBRTC_BSD) || defined(WEBRTC_MAC)
+@@ -16,6 +16,8 @@
+ #elif defined(WEBRTC_LINUX) || defined(WEBRTC_BSD) || defined(WEBRTC_MAC)
  #include <pthread.h>
  #include "condition_variable_posix.h"
--#include "condition_variable_wrapper.h"
 +#else
 +#include <stddef.h>  // for NULL
  #endif
  
  namespace webrtc {
-@@ -23,7 +25,7 @@ namespace webrtc {
- ConditionVariableWrapper* ConditionVariableWrapper::CreateConditionVariable() {
- #if defined(_WIN32)
-   return new ConditionVariableWindows;
--#elif defined(WEBRTC_LINUX) || defined(WEBRTC_MAC)
-+#elif defined(WEBRTC_LINUX) || defined(WEBRTC_BSD) || defined(WEBRTC_MAC)
-   return ConditionVariablePosix::Create();
- #else
-   return NULL;
