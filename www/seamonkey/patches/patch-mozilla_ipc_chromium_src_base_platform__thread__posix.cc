ed6ec1dde349ae7c2c2a46a14dd21f4af2783072
ryoon <ryoon>
2013-09-28 14:37:05 +0000

    Update to 2.21
    Changelog:
    SeaMonkey-specific changes
    
        Implemented an option to thread messages received by date.
        Allowed deletion of news posts by default.
        Implemented optional taskbar preview-per-tab.
        Added support (permission prompt) for desktop notifications.
        Added Isn't operator for searching by Priority.
        See the changes page for a more complete overview.
    
    Mozilla platform changes
    
        Support for new scrollbar style on Mac OS X 10.7 and newer.
        Accessibility related improvements on using pinned tabs (bug 577727).
        Major SVG rendering improvements around Image tiling and scaling (bug 600207).
        Removed support for sherlock files that are loaded from application or profile directory.
        Support for W3C touch events disabled (bug 888304).
        Fixed several stability issues.
    
    Fixed in SeaMonkey 2.21
    MFSA 2013-92 GC hazard with default compartments and frame chain restoration
    MFSA 2013-91 User-defined properties on DOM proxies get the wrong "this" object
    MFSA 2013-90 Memory corruption involving scrolling
    MFSA 2013-89 Buffer overflow with multi-column, lists, and floats
    MFSA 2013-88 compartment mismatch re-attaching XBL-backed nodes
    MFSA 2013-85 Uninitialized data in IonMonkey
    MFSA 2013-83 Mozilla Updater does not lock MAR file after signature verification
    MFSA 2013-82 Calling scope for new Javascript objects can lead to memory corruption
    MFSA 2013-81 Use-after-free with select element
    MFSA 2013-80 NativeKey continues handling key messages after widget is destroyed
    MFSA 2013-79 Use-after-free in Animation Manager during stylesheet cloning
    MFSA 2013-78 Integer overflow in ANGLE library
    MFSA 2013-77 Improper state in HTML5 Tree Builder with templates
    MFSA 2013-76 Miscellaneous memory safety hazards (rv:24.0 / rv:17.0.9)

--- www/seamonkey/patches/patch-mozilla_ipc_chromium_src_base_platform__thread__posix.cc
+++ www/seamonkey/patches/patch-mozilla_ipc_chromium_src_base_platform__thread__posix.cc
@@ -1,6 +1,6 @@
-$NetBSD: patch-mozilla_ipc_chromium_src_base_platform__thread__posix.cc,v 1.2 2013/08/11 03:18:46 ryoon Exp $
+$NetBSD: patch-mozilla_ipc_chromium_src_base_platform__thread__posix.cc,v 1.3 2013/09/28 14:37:05 ryoon Exp $
 
---- mozilla/ipc/chromium/src/base/platform_thread_posix.cc.orig	2013-08-04 03:05:29.000000000 +0000
+--- mozilla/ipc/chromium/src/base/platform_thread_posix.cc.orig	2013-09-16 18:26:38.000000000 +0000
 +++ mozilla/ipc/chromium/src/base/platform_thread_posix.cc
 @@ -10,7 +10,9 @@
  #if defined(OS_MACOSX)
@@ -12,12 +12,13 @@ $NetBSD: patch-mozilla_ipc_chromium_src_base_platform__thread__posix.cc,v 1.2 20
  #elif defined(OS_LINUX)
  #include <sys/syscall.h>
  #include <sys/prctl.h>
-@@ -108,7 +110,7 @@ void PlatformThread::SetName(const char*
-   pthread_set_name_np(pthread_self(), name);
- #elif defined(OS_NETBSD)
+@@ -110,7 +112,8 @@ void PlatformThread::SetName(const char*
    pthread_setname_np(pthread_self(), "%s", (void *)name);
+ #elif defined(OS_BSD) && !defined(__GLIBC__)
+   pthread_set_name_np(pthread_self(), name);
 -#else
 +#elif !defined(OS_SOLARIS)
-   prctl(PR_SET_NAME, reinterpret_cast<uintptr_t>(name), 0, 0, 0); 
++  prctl(PR_SET_NAME, reinterpret_cast<uintptr_t>(name), 0, 0, 0);
  #endif
  }
+ #endif // !OS_MACOSX
