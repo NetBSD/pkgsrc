e3d21bd0dfb3448380e2c051f44aa6bb2a185998
taca <taca@pkgsrc.org>
2013-09-30 03:15:03 +0000

    Update a patch for CVE-2013-4363 (previous CVE-2013-4287).
    Bump PKGREVISION.

--- lang/ruby193-base/patches/patch-lib_rubygems_version.rb
+++ lang/ruby193-base/patches/patch-lib_rubygems_version.rb
@@ -1,19 +1,21 @@
-$NetBSD: patch-lib_rubygems_version.rb,v 1.2 2013/09/10 16:25:51 taca Exp $
+$NetBSD: patch-lib_rubygems_version.rb,v 1.3 2013/09/30 03:15:03 taca Exp $
 
-* A fix for CVE-2013-4287.
+* A fix for CVE-2013-4363.
 * A small fix from Ruby 2.0.0 and Rubygem 1.8.25.
 
 --- lib/rubygems/version.rb.orig	2012-04-19 22:43:16.000000000 +0000
 +++ lib/rubygems/version.rb
-@@ -145,7 +145,7 @@ class Gem::Version
+@@ -145,8 +145,8 @@ class Gem::Version
  
    include Comparable
  
 -  VERSION_PATTERN = '[0-9]+(\.[0-9a-zA-Z]+)*' # :nodoc:
+-  ANCHORED_VERSION_PATTERN = /\A\s*(#{VERSION_PATTERN})*\s*\z/ # :nodoc:
 +  VERSION_PATTERN = '[0-9]+(?>\.[0-9a-zA-Z]+)*' # :nodoc:
-   ANCHORED_VERSION_PATTERN = /\A\s*(#{VERSION_PATTERN})*\s*\z/ # :nodoc:
++  ANCHORED_VERSION_PATTERN = /\A\s*(#{VERSION_PATTERN})?\s*\z/ # :nodoc:
  
    ##
+   # A string representation of this Version.
 @@ -187,7 +187,7 @@ class Gem::Version
      raise ArgumentError, "Malformed version number string #{version}" unless
        self.class.correct?(version)
