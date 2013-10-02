272c696ef4a93485f60cddd8b0f2a09a6f66bb6f
adam <adam@pkgsrc.org>
2013-10-02 18:30:13 +0000

    ClamAV 0.98 includes many new features, across many different components of ClamAV. There are new scanning options, extensions to the libclamav API, support for additional filetypes, and internal upgrades.

--- security/clamav/options.mk
+++ security/clamav/options.mk
@@ -1,9 +1,11 @@
-# $NetBSD: options.mk,v 1.1.1.1 2010/12/24 07:11:05 kefren Exp $
+# $NetBSD: options.mk,v 1.2 2013/10/02 18:30:13 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.clamav
 PKG_SUPPORTED_OPTIONS=	milter clamav-experimental
 
 .include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	milter
 
 .if !empty(PKG_OPTIONS:Mmilter)
 # force use of pkgsrc version of libmilter -- clamav uses the sendmail binary
@@ -12,7 +14,7 @@ PKG_SUPPORTED_OPTIONS=	milter clamav-experimental
 USE_BUILTIN.libmilter=	no
 .  include "../../mail/libmilter/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-milter
-PLIST_SRC+=		${PKGDIR}/PLIST.milter
+PLIST.milter=		yes
 .else
 CONFIGURE_ARGS+=	--disable-milter
 # XXX --disable-milter doesn't work as expected, so we need this
