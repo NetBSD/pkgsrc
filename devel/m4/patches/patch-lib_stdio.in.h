3935153ea23e78b2760bb9837d808169634ba0e9
wiz <wiz@pkgsrc.org>
2013-10-04 21:02:11 +0000

    Update to 1.4.17:
    * Noteworthy changes in release 1.4.17 (2013-09-22) [stable]
    
    ** Fix compilation with newer glibc headers.
    
    ** Fix a failure with diverting large amounts of text on mingw (does
       not affect platforms that can rename an open file).
    
    ** A number of portability improvements inherited from gnulib.

--- devel/m4/patches/patch-lib_stdio.in.h
+++ devel/m4/patches/patch-lib_stdio.in.h
@@ -1,16 +1,16 @@
-$NetBSD: patch-lib_stdio.in.h,v 1.2 2013/07/19 14:49:28 ryoon Exp $
+$NetBSD: patch-lib_stdio.in.h,v 1.3 2013/10/04 21:02:11 wiz Exp $
 
 glibc 2.16's /usr/include/stdio.h does not provide gets(3) anymore,
 and this line causes breakage.
 From: http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/sys-devel/m4/files/m4-1.4.16-no-gets.patch
 
---- lib/stdio.in.h.orig	2011-03-01 16:39:29.000000000 +0000
+--- lib/stdio.in.h.orig	2013-09-22 06:22:58.000000000 +0000
 +++ lib/stdio.in.h
-@@ -162,7 +162,6 @@ _GL_WARN_ON_USE (fflush, "fflush is not 
-    so any use of gets warrants an unconditional warning.  Assume it is
-    always declared, since it is required by C89.  */
+@@ -719,7 +719,6 @@ _GL_WARN_ON_USE (getline, "getline is un
+    removed it.  */
  #undef gets
+ #if HAVE_RAW_DECL_GETS
 -_GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
+ #endif
  
- #if @GNULIB_FOPEN@
- # if @REPLACE_FOPEN@
+ 
