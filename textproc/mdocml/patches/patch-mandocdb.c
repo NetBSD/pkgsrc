89809b1aac7bdc7141375d8b7f2770b28d3e3158
wiz <wiz@pkgsrc.org>
2013-10-06 15:48:42 +0000

    Try fixing the SunOS build, from Ingo Schwarze (upstream).

---
+++ textproc/mdocml/patches/patch-mandocdb.c
@@ -0,0 +1,34 @@
+$NetBSD: patch-mandocdb.c,v 1.4 2013/10/06 15:48:42 wiz Exp $
+
+--- mandocdb.c.orig	2013-10-05 14:09:07.000000000 +0000
++++ mandocdb.c
+@@ -34,19 +34,21 @@
+ #include <string.h>
+ #include <unistd.h>
+ 
+-#if defined(__linux__) || defined(__sun)
+-# include <endian.h>
+-# include <db_185.h>
+-#elif defined(__APPLE__)
++#if defined(__APPLE__)
+ # include <libkern/OSByteOrder.h>
+-# include <db.h>
++#elif defined(__linux__)
++# include <endian.h>
++#elif defined(__sun)
++# include <sys/byteorder.h>
++# include <sys/stat.h>
+ #else
+ # include <sys/endian.h>
+-# include <db.h>
+ #endif
+ 
+-#if defined(__sun)
+-#include <sys/stat.h>
++#if defined(__linux__)
++# include <db_185.h>
++#else
++# include <db.h>
+ #endif
+ 
+ #include "man.h"
