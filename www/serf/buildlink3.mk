4e66864378f9b599927e9d351bd6e95a6055629e
wiz <wiz@pkgsrc.org>
2013-10-06 16:45:04 +0000

    Update to 1.3.2.
    Upstream downgraded their shlib major version (at least on NetBSD).
    Since there are so few packages in pkgsrc depending on it, follow suit.
    Recursive revbump coming next.
    
    Serf 1.3.2 [2013-10-04, from /tags/1.3.2, r????]
      Fix issue 130: HTTP headers should be treated case-insensitively
      Fix issue 126: Compilation breaks with Codewarrior compiler
      Fix crash during cleanup of SSL buckets in apr_terminate() (r2145)
      Fix Windows build: Also export functions with capital letters in .def file
      Fix host header when url contains a username or password (r2170)
      Ensure less TCP package fragmentation on Windows (r2145)
      Handle authentication for responses to HEAD requests (r2178,-9)
      Improve serf_get: add option to add request headers, allow url with query,
         allow HEAD requests (r2143,r2175,-6)
      Improve RFC conformance: don't expect body for certain responses (r2011,-2)
      Do not invoke progress callback when no data was received (r2144)
      And more test suite fixes and build warning cleanups
      SCons-related fixes:
       Fix build when GSSAPI not in default include path (2155)
       Fix OpenBSD build: always map all LIBPATH entries into RPATH (r2156)
       Checksum generation in Windows shared libraries for release builds (2162)
       Mac OS X: Use MAJOR version only in dylib install name (r2161)
       Use both MAJOR and MINOR version for the shared library name (2163)
       Fix the .pc file when installing serf in a non-default LIBDIR (r2191)

--- www/serf/buildlink3.mk
+++ www/serf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2013/09/07 19:38:41 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2013/10/06 16:45:04 wiz Exp $
 
 BUILDLINK_TREE+=	serf
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	serf
 SERF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.serf+=	serf>=1.1.1
-BUILDLINK_ABI_DEPENDS.serf+=	serf>=1.3.1
+BUILDLINK_ABI_DEPENDS.serf+=	serf>=1.3.2
 BUILDLINK_PKGSRCDIR.serf?=	../../www/serf
 
 .include "../../devel/apr/buildlink3.mk"
