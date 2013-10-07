67f0ce197c8314bc2035ce3128f74c1d8612ce9f
morr <morr@pkgsrc.org>
2013-10-07 19:26:28 +0000

    Update to version 7.4.052.
    Patches for vim 7.4:
    
    7.4.001  'ic' doesn't work for patterns such as [a-z]
    7.4.002  pattern with two alternative look-behind matches doesn't match
    7.4.003  memory access error in Ruby syntax highlighting
    7.4.004  when closing a window fails ":bwipe" may hang
    7.4.005  "vaB" while 'virtualedit' is set selects the wrong area
    7.4.006  mkdir("foo/bar/", "p") gives an error message
    7.4.007  creating a preview window on startup messes up the screen
    7.4.008  new regexp engine can't be interrupted
    7.4.009  too easy to write a file was not decrypted (yet)
    7.4.010  (after 7.4.006) crash with invalid argument to mkdir()
    7.4.011  cannot find out if "acl" and "xpm" features are supported
    7.4.012  MS-Windows: resolving multi-bye shortcut does not work
    7.4.013  MS-Windows: File name buffer too small for utf-8
    7.4.014  MS-Windows: check for writing to device does not work
    7.4.015  MS-Windows: Detecting node type fails for multi-byte chars
    7.4.016  MS-Windows: File name completion has problem with Chinese
    7.4.017  ":help !!" does not find the "!!" tag in the help file
    7.4.018  when completing item becomes unselected
    7.4.019  file name completion fails with Chinese characters
    7.4.020  NFA engine matches too much with \@&gt;
    7.4.021  NFA regexp: Using \ze may result in wrong end
    7.4.022  deadlock while exiting, because of allocating memory
    7.4.023  MS-Windows: warning for 64 bit type
    7.4.024  current user cannot always use the undo file he created
    7.4.025  reading before start of a string
    7.4.026  clang warning for int shift overflow
    7.4.027  valgrind error when using CTRL-X CTRL-F at start of the line
    7.4.028  equivalence classes are not working for multi-byte characters
    7.4.029  an error in a pattern is reported twice
    7.4.030  the -mno-cygwin argument is no longer supported by Cygwin
    7.4.031  ":diffoff!" resets options even when 'diff' is not set
    7.4.032  NFA engine does not match the NUL character
    7.4.033  if terminal has 20 lines test 92 and 93 overwrite input file
    7.4.034  using "p" in Visual block mode only changes the first line
    7.4.035  MS-Windows: mouse pointer flickers when going to Normal mode
    7.4.036  NFA engine does not capture group correctly when using \@&gt;
    7.4.037  "\ze" in a sub-pattern does set the end of the match
    7.4.038  Using "zw" and "zg" when 'spell' is off give confusing error
    7.4.039  MS-Windows: &lt; MSCV10 can't handle symlinks to a directory
    7.4.040  error on exit when variable holds reference to a script scope
    7.4.041  Visual selection does not remain after being copied over
    7.4.042  after ":setlocal" 'spell'/'spellang' :spelldump doesn't work
    7.4.043  VMS can't handle long function names
    7.4.044  can't build with old MSVC
    7.4.045  substitute() has a problem with pattern starting with "\ze".
    7.4.046  can't use Tcl 8.6
    7.4.047  input() does not work in a function invoked by a mapping
    7.4.048  recent clang version complains about -fno-strength-reduce
    7.4.049  in Ex mode substitute prompt is wrong with line numbers
    7.4.050  "gn" may select too much when there are two matching lines
    7.4.051  syntax highlighting a Yaml file causes a crash
    7.4.052  cursor may end up in the wrong position when auto-formatting

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,5 @@
-# $NetBSD: version.mk,v 1.90 2013/06/12 14:53:56 morr Exp $
+# $NetBSD: version.mk,v 1.91 2013/10/07 19:26:28 morr Exp $
 
-VIM_VERSION=		7.3
-VIM_PATCHLEVEL=		1140
-VIM_SUBDIR=		vim73
+VIM_VERSION=		7.4
+VIM_PATCHLEVEL=		052
+VIM_SUBDIR=		vim74
