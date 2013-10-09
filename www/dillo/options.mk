80c3695bdb48241444c551b9bfd3d7ed74ef749c
ryoon <ryoon@pkgsrc.org>
2013-10-09 14:23:42 +0000

    Bump PKGREVISION.
    * Enable ssl (https) option by default.

--- www/dillo/options.mk
+++ www/dillo/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.3 2012/06/12 15:46:04 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2013/10/09 14:23:42 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dillo
 PKG_SUPPORTED_OPTIONS=	inet6 ssl
-PKG_SUGGESTED_OPTIONS=	inet6
+PKG_SUGGESTED_OPTIONS=	inet6 ssl
 
 .include "../../mk/bsd.options.mk"
 
