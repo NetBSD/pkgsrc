ca996ba5f6ff3c5f3ae7bc139171c926487f3fca
ryoon <ryoon@pkgsrc.org>
2013-10-09 14:29:47 +0000

    Bump PKGREVISION.
    * Install more header files.

--- devel/mdds/buildlink3.mk
+++ devel/mdds/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/08/22 14:42:57 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/10/09 14:29:47 ryoon Exp $
 
 BUILDLINK_TREE+=	mdds
 
 .if !defined(MDDS_BUILDLINK3_MK)
 MDDS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mdds+=	mdds>=0.9.0
+BUILDLINK_API_DEPENDS.mdds+=	mdds>=0.9.0nb1
 BUILDLINK_PKGSRCDIR.mdds?=	../../devel/mdds
 # only contains header files
 BUILDLINK_DEPMETHOD.mdds?=	build
