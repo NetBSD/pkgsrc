ca87ad0fba5be6721f09839f0859318fccc95082
jperkin <jperkin@pkgsrc.org>
2013-10-10 09:34:00 +0000

    Sync version with main emacs24 package.  Fixes dependencies with EMACS_TYPE set to emacs24nox.

--- editors/emacs24-nox11/version.mk
+++ editors/emacs24-nox11/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.5 2013/03/23 10:03:06 joerg Exp $
+# $NetBSD: version.mk,v 1.6 2013/10/10 09:34:00 jperkin Exp $
 
 _EMACS_FLAVOR=	emacs
 _EMACS_REQD=	emacs24-nox11>=24.1<24.3.50
 
 _EMACS_VERSION_MAJOR=	24
-_EMACS_VERSION_MINOR=	2
+_EMACS_VERSION_MINOR=	3
