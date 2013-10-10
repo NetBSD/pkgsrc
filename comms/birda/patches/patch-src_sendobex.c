541b9cdb953345cfe70be9e4ea1a8342e9776720
joerg <joerg@pkgsrc.org>
2013-10-10 00:02:17 +0000

    Fix undefined and broken loop. Fix obviously broken format string. Bump revision.

--- comms/birda/patches/patch-src_sendobex.c
+++ comms/birda/patches/patch-src_sendobex.c
@@ -1,7 +1,16 @@
-$NetBSD: patch-src_sendobex.c,v 1.1 2012/02/16 16:40:34 hans Exp $
+$NetBSD: patch-src_sendobex.c,v 1.2 2013/10/10 00:02:17 joerg Exp $
 
---- src/sendobex.c.orig	2005-11-14 08:57:16.000000000 +0100
-+++ src/sendobex.c	2012-01-25 16:30:35.349985033 +0100
+--- src/sendobex.c.orig	2005-11-14 07:57:16.000000000 +0000
++++ src/sendobex.c
+@@ -302,7 +302,7 @@ static int in_bind_ne(short port, int ty
+   if (sd < 0)
+     switch (sd) {
+     case SYSCALL_ERROR:
+-      fprintf(stderr, "in_bind_ne: bind", "port = %d, type = %s\n", 
++      fprintf(stderr, "in_bind_ne: bind port = %d, type = %s\n", 
+ 	      port, socket_type_name(type));
+       /* NOTREACHED */
+     case RESOLVER_ERROR:
 @@ -327,7 +327,11 @@ void createOBEXSender(int port)
      //struct sockaddr addr;
      //socklen_t len;
