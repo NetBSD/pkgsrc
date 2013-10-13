856595627cf4e430628047adfaca99d3596935f2
wiz <wiz@pkgsrc.org>
2013-10-13 12:59:11 +0000

    Remove tolua++: does not work with lua-5.2, upstream not active nor other patches available, no user in pkgsrc.

--- devel/tolua++/files/custom.py
+++
@@ -1,7 +0,0 @@
-CCFLAGS = ['-I@PREFIX@/include', '-O2', '-ansi']
-LIBPATH = ['@PREFIX@/lib']
-LIBS = ['lua', 'm']
-LINKFLAGS = ['-Wl,-rpath,@PREFIX@/lib']
-tolua_bin = 'tolua++'
-tolua_lib = 'tolua++'
-TOLUAPP = 'tolua++'
