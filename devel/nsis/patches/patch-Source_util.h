83a9d9467b8ca668cb92f5307f6c9730344e2367
joerg <joerg@pkgsrc.org>
2013-10-15 14:41:36 +0000

    Honor wrappers. Fix build with libc++.

---
+++ devel/nsis/patches/patch-Source_util.h
@@ -0,0 +1,12 @@
+$NetBSD: patch-Source_util.h,v 1.1 2013/10/15 14:41:36 joerg Exp $
+
+--- Source/util.h.orig	2013-10-14 21:47:58.000000000 +0000
++++ Source/util.h
+@@ -10,6 +10,7 @@
+ #  include <iconv.h>
+ #  include <stdio.h>
+ #  include <glob.h>
++#  include <unistd.h>
+ #endif
+ 
+ 
