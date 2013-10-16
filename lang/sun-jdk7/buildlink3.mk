d0c9165c9c219382f1d42413751f38a4e03e0933
richard <richard@pkgsrc.org>
2013-10-16 16:48:55 +0000

    improve builtin versioning and update BUILDLINK_DEPMETHOD correctly for sun-jdk7

--- lang/sun-jdk7/buildlink3.mk
+++ lang/sun-jdk7/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/03/06 16:58:57 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/10/16 16:48:55 richard Exp $
 
 BUILDLINK_TREE+=	sun-jdk7
 
@@ -24,6 +24,7 @@ BUILDLINK_CPPFLAGS.sun-jdk7=						\
 	-I${BUILDLINK_JAVA_PREFIX.sun-jre7}/include/linux
 .endif
 
+BUILDLINK_DEPMETHOD.sun-jre7?=	${BUILDLINK_DEPMETHOD.sun-jdk7}
 .include "../../lang/sun-jre7/buildlink3.mk"
 .endif # SUN_JDK7_BUILDLINK3_MK
 
