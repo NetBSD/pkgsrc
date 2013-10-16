bd4b873e396c6240e92a3c9c7ccc063031d28183
roy <roy@pkgsrc.org>
2013-10-16 08:49:43 +0000

    Does not use curses or even termcap

--- comms/pilot-link-libs/buildlink3.mk
+++ comms/pilot-link-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2013/07/17 12:24:41 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2013/10/16 08:50:17 roy Exp $
 
 BUILDLINK_TREE+=	pilot-link-libs
 
@@ -10,7 +10,6 @@ BUILDLINK_ABI_DEPENDS.pilot-link-libs+=	pilot-link-libs>=0.12.3
 BUILDLINK_PKGSRCDIR.pilot-link-libs?=	../../comms/pilot-link-libs
 
 .include "../../converters/libiconv/buildlink3.mk"
-.include "../../devel/ncurses/buildlink3.mk"
 .include "../../mk/readline.buildlink3.mk"
 .endif # PILOT_LINK_LIBS_BUILDLINK3_MK
 
