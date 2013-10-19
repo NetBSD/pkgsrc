5cd96d64ef04d63bbb65cf1a3c726f2421aec16d
ryoon <ryoon@pkgsrc.org>
2013-10-19 07:55:54 +0000

    Rename swig-2.* to swig2, so swig-1.* and swig2 can coexist

--- devel/swig2/buildlink3.mk
+++ devel/swig2/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/06/14 18:41:30 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/10/19 07:55:54 ryoon Exp $
 
 BUILDLINK_TREE+=	swig2
 
 .if !defined(SWIG2_BUILDLINK3_MK)
 SWIG2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.swig2+=	swig>=2.0.8
+BUILDLINK_API_DEPENDS.swig2+=	swig2>=2.0.8
 BUILDLINK_PKGSRCDIR.swig2?=	../../devel/swig2
 
 .include "../../devel/pcre/buildlink3.mk"
