ee979e914974a0e5af66e5f197939ae83492c663
adam <adam@pkgsrc.org>
2013-10-19 08:47:36 +0000

    Changes 52.1: Unicode 6.3: New bidi control codes, new Bidi_Class property values, two new bidi "bracket" properties; for other property value changes see the UAX 44 summary. The bidi algorithm implementation has also been updated to support the new properties and to match the updated algorithm in the Unicode 6.3 version of UAX 9. Note: ICU 52 still uses collation root data based on Unicode Collation Algorithm 6.2 (UCA 6.2). (However, ICU 52 does use CLDR 24 collation tailoring data.) CLDR 24: Improved coverage for top 70+ languages, fractional plural rules and forms, many new measurement units, major simplification of collation rule syntax, preliminary version of European Ordering Rules, new relative fields such as “last Sunday” and “now”, and much more. Time zone data: 2013g. Support new variants of Islamic calendar: "islamic-umalqura": Umm al-Qura. "islamic-tbla": Tabular (fixed intercalary years), with astronomical epoch. Made Calendar getDayOfWeekType behave as documented. New API for converting between Windows time zone ID and IANA tz database ID. Technology Preview: New API for more granular control of DateFormat parse leniency. DateTimePatternGenerator: Support recently-added time zone pattern characters O, X, x and updated support for V, Z. Support newly-defined skeleton character ‘J’ to generate preferred hour cycle without any day period indicator (such as AM/PM for h). Implement support for plurals that depend on displayed fractional values. MessageFormat and currency formatting etc. select appropriate plural forms for values with decimal digits (after the decimal point). Segmentation: Add dictionary-based word & line break for Lao.

--- textproc/icu/patches/patch-common_putilimp.h
+++ textproc/icu/patches/patch-common_putilimp.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-common_putilimp.h,v 1.2 2012/11/07 20:59:51 adam Exp $
+$NetBSD: patch-common_putilimp.h,v 1.3 2013/10/19 08:47:36 adam Exp $
 
---- common/putilimp.h.orig	2012-11-05 17:14:50.000000000 +0000
+--- common/putilimp.h.orig	2013-10-04 20:49:30.000000000 +0000
 +++ common/putilimp.h
-@@ -123,6 +123,8 @@ typedef size_t uintptr_t;
+@@ -128,6 +128,8 @@ typedef size_t uintptr_t;
     /* not defined */
- #elif U_PLATFORM == U_PF_OS400
+ #elif U_PLATFORM == U_PF_IPHONE
     /* not defined */
 +#elif defined(__MirBSD__)
 +   /* not defined */
