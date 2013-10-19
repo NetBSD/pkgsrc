a470615f500d07c58b784ea14c1c87e284b3f106
adam <adam>
2013-10-19 09:06:55 +0000

    Revbump after updating textproc/icu

--- mail/evolution-data-server/buildlink3.mk
+++ mail/evolution-data-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.49 2013/05/24 18:23:33 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.50 2013/10/19 09:06:56 adam Exp $
 
 BUILDLINK_TREE+=	evolution-data-server
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	evolution-data-server
 EVOLUTION_DATA_SERVER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.evolution-data-server+=	evolution-data-server>=1.8.0
-BUILDLINK_ABI_DEPENDS.evolution-data-server+=	evolution-data-server>=2.32.3nb20
+BUILDLINK_ABI_DEPENDS.evolution-data-server+=	evolution-data-server>=2.32.3nb26
 BUILDLINK_PKGSRCDIR.evolution-data-server?=	../../mail/evolution-data-server
 
 BDB_ACCEPTED?=		db4 db5
--- net/monotorrent/buildlink3.mk
+++ net/monotorrent/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2013/06/17 12:44:54 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2013/10/19 09:06:58 adam Exp $
 
 BUILDLINK_TREE+=	monotorrent
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	monotorrent
 MONOTORRENT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.monotorrent+=	monotorrent>=0.72
-BUILDLINK_ABI_DEPENDS.monotorrent+=	monotorrent>=0.72nb7
+BUILDLINK_ABI_DEPENDS.monotorrent+=	monotorrent>=0.72nb8
 BUILDLINK_PKGSRCDIR.monotorrent?=	../../net/monotorrent
 
 .include "../../lang/mono2/buildlink3.mk"
