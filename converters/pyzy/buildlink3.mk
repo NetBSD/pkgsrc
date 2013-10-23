d249d28ddb806fd4fa90bf5e9c46d9d62e1d555a
obache <obache@pkgsrc.org>
2013-10-23 11:41:25 +0000

    Import pyzy-0.1.0 as converters/pyzy.
    This is the Chinese PinYin / Bopomofo (a.k.a Zhuying) conversion library.
    You can easily write an application which handles the Chinese with it.

---
+++ converters/pyzy/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/10/23 11:41:25 obache Exp $
+#
+
+BUILDLINK_TREE+=	pyzy
+
+.if !defined(PYZY_BUILDLINK3_MK)
+PYZY_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.pyzy+=	pyzy>=0.1.0
+BUILDLINK_PKGSRCDIR.pyzy?=	../../converters/pyzy
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../databases/sqlite3/buildlink3.mk"
+.endif	# PYZY_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-pyzy
