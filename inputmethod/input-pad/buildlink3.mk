d3cf5fdc3fe9d1153f33a6f5b180b6723e1a79b4
obache <obache@pkgsrc.org>
2013-10-23 12:31:14 +0000

    Update input-pad to 1.0.2.
        Released 1.0.2
    
        Added Gothic group.
        Added Shavian data.
        Added data for IPA.
        Fix ABI breakage of eekboard 1.0.6.
        Added gobject-introspection.
        Added bold, italic and script.

--- inputmethod/input-pad/buildlink3.mk
+++ inputmethod/input-pad/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2013/10/10 14:41:50 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2013/10/23 12:31:14 obache Exp $
 #
 
 BUILDLINK_TREE+=	input-pad
@@ -6,14 +6,13 @@ BUILDLINK_TREE+=	input-pad
 .if !defined(INPUT_PAD_BUILDLINK3_MK)
 INPUT_PAD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.input-pad+=	input-pad>=1.0.1
-BUILDLINK_ABI_DEPENDS.input-pad?=	input-pad>=1.0.1nb11
+BUILDLINK_API_DEPENDS.input-pad+=	input-pad>=1.0.2
 BUILDLINK_PKGSRCDIR.input-pad?=	../../inputmethod/input-pad
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/libxkbfile/buildlink3.mk"
 .include "../../x11/libxklavier/buildlink3.mk"
