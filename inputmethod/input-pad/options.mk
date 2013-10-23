5e54fbfe3828eb8604951638edf2806bdede4898
obache <obache@pkgsrc.org>
2013-10-23 12:31:14 +0000

    Update input-pad to 1.0.2.
        Released 1.0.2
    
        Added Gothic group.
        Added Shavian data.
        Added data for IPA.
        Fix ABI breakage of eekboard 1.0.6.
        Added gobject-introspection.
        Added bold, italic and script.

--- inputmethod/input-pad/options.mk
+++ inputmethod/input-pad/options.mk
@@ -1,13 +1,13 @@
-# $NetBSD: options.mk,v 1.3 2010/11/30 11:20:16 obache Exp $
+# $NetBSD: options.mk,v 1.4 2013/10/23 12:31:14 obache Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.input-pad
-PKG_SUPPORTED_OPTIONS=	eekboard xtest
-PKG_SUGGESTED_OPTIONS=	eekboard
+PKG_SUPPORTED_OPTIONS=	eekboard introspection xtest
+PKG_SUGGESTED_OPTIONS=	introspection
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	eek xtest
+PLIST_VARS+=	eek introspection xtest
 
 #
 # Draw keyboard using libeek
@@ -25,4 +25,13 @@ PLIST.eek=		yes
 .include "../../x11/libXtst/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-xtest=yes
 PLIST.xtest=		yes
+.endif
+
+#
+# Introspection support
+#
+.if !empty(PKG_OPTIONS:Mintrospection)
+BUILDLINK_DEPMETHOD.gobject-introspection=	build
+.include "../../devel/gobject-introspection/buildlink3.mk"
+PLIST.introspection=	yes
 .endif
