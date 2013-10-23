2acf2efcf752e818e39fd9624deb6932e5bd8ba9
obache <obache@pkgsrc.org>
2013-10-23 12:27:26 +0000

    Update eekboard to 1.0.8.
    * switch to use gtk3
    * add introspection support

--- x11/eekboard/buildlink3.mk
+++ x11/eekboard/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2013/10/10 14:41:46 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2013/10/23 12:27:26 obache Exp $
 #
 
 BUILDLINK_TREE+=	eekboard
@@ -6,23 +6,13 @@ BUILDLINK_TREE+=	eekboard
 .if !defined(EEKBOARD_BUILDLINK3_MK)
 EEKBOARD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.eekboard+=	eekboard>=1.0.5
-BUILDLINK_ABI_DEPENDS.eekboard?=	eekboard>=1.0.5nb12
+BUILDLINK_API_DEPENDS.eekboard+=	eekboard>=1.0.6
 BUILDLINK_PKGSRCDIR.eekboard?=	../../x11/eekboard
-
-.include "../../mk/bsd.fast.prefs.mk"
-
-pkgbase := eekboard
-.include "../../mk/pkg-build-options.mk"
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/pango/buildlink3.mk"
-.if !empty(PKG_BUILD_OPTIONS.eekboard:Mclutter)
-.include "../../graphics/clutter/buildlink3.mk"
-.include "../../graphics/clutter-gtk/buildlink3.mk"
-.endif
 .include "../../textproc/libcroco/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
 .include "../../x11/libxklavier/buildlink3.mk"
 .endif	# EEKBOARD_BUILDLINK3_MK
 
