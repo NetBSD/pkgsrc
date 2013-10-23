5dce0fc457d5b443f91674593ff7f09aa4d24c9a
obache <obache@pkgsrc.org>
2013-10-23 11:50:04 +0000

    Update ibus to 1.5.4.
    News of 1.5.0
    * Improve ibus performance by replace some python components with vala implementations.
    * Improve IME switch UI and logic, and support shortcut customization.
    Integrate IME and XKB keyboard layouts. Now ibus can manage both IME and XKB layouts.
    * Improve gobject-introspection support.
    * Improve vala language support.
    * New functions to restrict config signal: ibus_config_watch/unwatch
    * Remove gnome-icon-theme-legacy dependency.
    * Add version property in IBusEngineDesc.
    * Add IBusProperty.symbol for the short label.
    * Support dconf 0.13.4
    * Add ibus command line tool.
    * Improve unit test.
    * ibus-daemon now monitors .xml files by default
    * "make check" does not require "make install"
    * Support out-of-tree build
    * pygobject override is added to help migrate from the old python binding
    * Many bug fixings.

--- inputmethod/ibus/buildlink3.mk
+++ inputmethod/ibus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2012/09/15 10:05:11 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2013/10/23 11:50:04 obache Exp $
 #
 
 BUILDLINK_TREE+=	ibus
@@ -6,8 +6,7 @@ BUILDLINK_TREE+=	ibus
 .if !defined(IBUS_BUILDLINK3_MK)
 IBUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ibus+=	ibus>=1.4.0
-BUILDLINK_ABI_DEPENDS.ibus?=	ibus>=1.4.2nb2
+BUILDLINK_API_DEPENDS.ibus+=	ibus>=1.5.0
 BUILDLINK_PKGSRCDIR.ibus?=	../../inputmethod/ibus
 
 .include "../../devel/glib2/buildlink3.mk"
