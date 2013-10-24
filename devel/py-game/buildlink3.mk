231dcdcb147db0b3d1c0eb55f471651dc8dc4bab
ryoon <ryoon@pkgsrc.org>
2013-10-24 12:08:22 +0000

    Update to 1.9.1
    Changelog:
    1.9.1
    Support for setuptools-based installs was fixed.
    
    1.9.0
    Many fixes and improvements were made. More changes have gone
    into this release than any other pygame release. Experimental camera
    webcam, midi, and gfxdraw modules were added. Bugfixes were made
    for backwards compatibility issues introduced in the pygame 1.8.x
    series. Old games like solarwolf and libraries like PGU work again.
    Py3k and Nokia mobile phone S60 are supported. Mac OS X support
    was improved by dropping the pyobjc dependency, improving the
    installer, and making sysfont work. pygame.examples and pygame.tests
    are available as modules. The examples were cleaned up. py2app and
    py2exe support were improved.

--- devel/py-game/buildlink3.mk
+++ devel/py-game/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2013/06/04 22:15:55 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2013/10/24 12:08:22 ryoon Exp $
 
 BUILDLINK_TREE+=	${PYPKGPREFIX}-game
 
@@ -7,14 +7,17 @@ PY_GAME_BUILDLINK3_MK:=
 
 .include "../../lang/python/pyversion.mk"
 
-BUILDLINK_API_DEPENDS.${PYPKGPREFIX}-game+=	${PYPKGPREFIX}-game>=1.6
-BUILDLINK_ABI_DEPENDS.${PYPKGPREFIX}-game+=	${PYPKGPREFIX}-game>=1.8.1nb14
+BUILDLINK_API_DEPENDS.${PYPKGPREFIX}-game+=	${PYPKGPREFIX}-game>=1.9.1
+BUILDLINK_ABI_DEPENDS.${PYPKGPREFIX}-game+=	${PYPKGPREFIX}-game>=1.9.1
 BUILDLINK_PKGSRCDIR.${PYPKGPREFIX}-game?=	../../devel/py-game
 
 .include "../../audio/SDL_mixer/buildlink3.mk"
+.include "../../devel/SDL/buildlink3.mk"
 .include "../../devel/SDL_ttf/buildlink3.mk"
 .include "../../graphics/SDL_image/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
 .include "../../multimedia/smpeg/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
 .endif # PY_GAME_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-${PYPKGPREFIX}-game
