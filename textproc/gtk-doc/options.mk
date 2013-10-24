258ff636ee5f8570ec2cac168b0c7b527f34cca2
wiz <wiz@pkgsrc.org>
2013-10-24 08:48:09 +0000

    Switch to openjade, since jade was removed (untested)

--- textproc/gtk-doc/options.mk
+++ textproc/gtk-doc/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2008/11/18 11:55:58 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2013/10/24 08:48:09 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gtkdoc
 PKG_SUPPORTED_OPTIONS=	sgml
@@ -8,5 +8,5 @@ PKG_SUPPORTED_OPTIONS=	sgml
 .if !empty(PKG_OPTIONS:Msgml)
 DEPENDS+= dsssl-docbook-modular-[0-9]*:../../textproc/dsssl-docbook-modular
 SGML_CATALOGS= ${PREFIX}/share/sgml/gtk-doc/gtk-doc.cat
-.include "../../textproc/jade/buildlink3.mk"
+.include "../../textproc/openjade/buildlink3.mk"
 .endif
