ebcd261201b21cff8c335710e10c2553e6eb0f4f
dholland <dholland@pkgsrc.org>
2013-10-27 21:21:26 +0000

    Update to 1.6.1. Reset PKGREVISION from 38 (!). Upstream changes: 5+ years of development; try these links:    http://www.shatters.net/celestia/161changes.txt    http://www.shatters.net/celestia/features/160-features.html    http://www.shatters.net/celestia/151changes.txt    http://www.shatters.net/celestia/150-feature-summary.html
    Restrictions:
       1. The Lua support still does not build, and given that I had to
          patch it to compile at all with Lua disabled it may not work all
          that well this way.
       2. The optional KDE (kde3) build is almost certainly broken; I will
          get to this in a few days if nobody beats me to it.

--- misc/celestia/options.mk
+++ misc/celestia/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2013/10/27 17:17:57 dholland Exp $
+# $NetBSD: options.mk,v 1.6 2013/10/27 21:21:26 dholland Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.celestia
 PKG_SUPPORTED_OPTIONS=		lua
@@ -24,6 +24,7 @@ CONFIGURE_ARGS+=	--with-glut
 CONFIGURE_ARGS+=	--with-gtk
 PLIST_SRC=		${PKGDIR}/PLIST.gtk
 .  include "../../graphics/gtkglext/buildlink3.mk"
+.  include "../../multimedia/libtheora/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mgnome)
@@ -32,12 +33,14 @@ PLIST_SRC=		${PKGDIR}/PLIST.gtk
 .  include "../../devel/GConf/schemas.mk"
 .  include "../../devel/libgnomeui/buildlink3.mk"
 .  include "../../graphics/gtkglext/buildlink3.mk"
+.  include "../../multimedia/libtheora/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mkde)
 CONFIGURE_ARGS+=	--with-kde
 PLIST_SRC=		${PKGDIR}/PLIST.kde
 .  include "../../graphics/hicolor-icon-theme/buildlink3.mk"
+.  include "../../multimedia/libtheora/buildlink3.mk"
 .  include "../../x11/kdelibs3/buildlink3.mk"
 .endif
 
