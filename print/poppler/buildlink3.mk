5ba34ab0d643170faa059fc9b388fb5938d0a288
drochner <drochner>
2013-10-29 19:05:23 +0000

    update to 0.24.3 changes: bugfixes, among them a format string problem (CVE-2013-4474)
    pkgsrc change: kill the poppler-cms/lcms option, it got broken recently
    and it is not useful enough to justify the complexity

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.37 2013/08/22 09:11:16 prlw1 Exp $
+# $NetBSD: buildlink3.mk,v 1.38 2013/10/29 19:05:23 drochner Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -9,12 +9,7 @@ BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
 BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.24.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
-pkgbase := poppler
-.include "../../mk/pkg-build-options.mk"
-.if !empty(PKG_BUILD_OPTIONS.poppler:Mpoppler-cms)
 .include "../../graphics/lcms2/buildlink3.mk"
-.endif
-
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
