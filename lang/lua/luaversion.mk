77d7c93312c4fe31f30c8b758fe3ef24d699319b
dholland <dholland>
2013-10-30 06:18:09 +0000

    Reimport lang/lua as lang/lua52, and bring back the previous Lua 5.1 package as lang/lua51.
    I've adjusted the depends and switched the PKGNAMEs but these packages
    are otherwise unchanged. This means that you can't install both at
    once. There are preliminary patches for that (see tech-pkg) but they
    aren't quite ready yet.
    
    This also doesn't include agc's builtin.mk, although it should be
    possible to drop it into the lua51 package with only very minor
    modifications. I don't think the builtin packages will allow having
    one builtin.mk for both lua51 and lua52, but I'd be happy to be shown
    wrong about that.
    
    Add infrastructure support for Lua module packages (including
    multiversion support), application packages written in Lua, and a
    redirecting bl3.mk file for packages that just link Lua in. This is
    based on the Python infrastructure.
    
    The new variable LUA_VERSION_DEFAULT picks which Lua package you get
    by default; it is set to 52 so as to maintain the status quo.
    
    Packages can set LUA_VERSIONS_ACCEPTED and/or LUA_VERSIONS_INCOMPATIBLE.
    I have found (I think) most or all the packages that don't work with
    lua51 and will be marking those; I have not tracked down most of the
    ones that don't work with lua52 yet as I was originally intending to
    roll back to 5.1 as the default.

---
+++ lang/lua/luaversion.mk
@@ -0,0 +1,130 @@
+# $NetBSD: luaversion.mk,v 1.1 2013/10/30 06:18:09 dholland Exp $
+
+# This file determins which Lua version is used as a dependency for
+# a package.
+#
+# === User-settable variables ===
+#
+# LUA_VERSION_DEFAULT
+#	The preferred lua version to use.
+#
+#	Possible values: 51 52
+#	Default: 52
+#
+# === Infrastructure variables ===
+#
+# LUA_VERSION_REQD
+#	Lua version to use. This variable should not be set by
+#	packages; it is ordinarily set by multiversion builds.
+#
+#	Possible values: ${LUA_VERSIONS_ACCEPTED}
+#	Default: ${LUA_VERSION_DEFAULT}
+#
+# === Package-settable variables ===
+#
+# LUA_VERSIONS_ACCEPTED
+#	The Lua versions that the package can build against. Order
+#	is significant; those listed earlier are preferred over those
+#	listed later.
+#
+#	Possible values: 51 52
+#	Default: 52 51
+#
+# LUA_VERSIONS_INCOMPATIBLE
+#	The Lua versions that the package *cannot* build against.
+#
+#	Possible values: 51 52
+#	Default: <empty>
+#
+# LUA_SELF_CONFLICT
+#	If set to "yes", CONFLICTS entries are added among the various
+#	per-Lua-version variants of a multiversion package.
+#
+# === Defined variables ===
+#
+# LUA_PKGPREFIX
+#	The prefix to use in PKGNAME for multiversion packages.
+#
+#	Example: lua51
+#
+# Keywords: Lua
+#
+
+.if !defined (LUA_LUAVERSION_MK)
+LUA_LUAVERSION_MK=	# defined
+
+.include "../../mk/bsd.prefs.mk"
+
+BUILD_DEFS+=		LUA_VERSION_DEFAULT
+BUILD_DEFS_EFFECTS+=	LUAPACKAGE
+
+LUA_VERSION_DEFAULT?=	52
+LUA_VERSIONS_ACCEPTED?=	52 51
+LUA_VERSIONS_INCOMPATIBLE?=# empty
+
+#
+# Resolve LUA_VERSIONS_INCOMPATBLE and generate the _OK vars.
+#
+.for v in ${LUA_VERSIONS_ACCEPTED}
+.if empty(LUA_VERSIONS_INCOMPATIBLE:M${v})
+_LUA_VERSION_${v}_OK=		yes
+_LUA_VERSIONS_ACCEPTED+=	${v}
+.endif
+.endfor
+
+# Pick a version
+.if defined(LUA_VERSION_REQD)
+.  if defined(_LUA_VERSION_${LUA_VERSION_REQD}_OK)
+_LUA_VERSION=		${LUA_VERSION_REQD}
+.  endif
+.else
+.  if defined(_LUA_VERSION_${LUA_VERSION_DEFAULT}_OK)
+_LUA_VERSION?=		${LUA_VERSION_DEFAULT}
+.  endif
+.  for v in ${_LUA_VERSIONS_ACCEPTED}
+.    if defined(_LUA_VERSION_${v}_OK)
+_LUA_VERSION?=		${v}
+.    endif
+.  endfor
+.endif
+
+#
+# Enable multiversion builds.
+#
+MULTI+=	LUA_VERSION_REQD=${_LUA_VERSION}
+
+# In case nothing matched
+_LUA_VERSION?=		none
+
+#
+# Handle self-conflicts
+#
+.if !empty(LUA_SELF_CONFLICT:M[Yy][Ee][Ss])
+.for v in ${_LUA_VERSIONS_ACCEPTED:N${_LUA_VERSION}}
+CONFLICTS+=	${PKGNAME:S/lua${_LUA_VERSION}/lua${v}/:C/-[0-9].*$/-[0-9]*/}
+.endfor
+.endif
+
+.if ${_LUA_VERSION} == "52"
+LUA_PACKAGE=		lua52
+LUA_PKGSRCDIR=		../../lang/lua52
+LUA_PKGPREFIX=		lua52
+LUA_BASEDEPENDS=	lua52>=5.2<5.3${LUA_PKGSRCDIR}
+
+.elif ${_LUA_VERSION} == "51"
+LUA_PACKAGE=		lua51
+LUA_PKGSRCDIR=		../../lang/lua51
+LUA_PKGPREFIX=		lua51
+LUA_BASEDEPENDS=	lua51>=5.1<5.2:${LUA_PKGSRCDIR}
+
+.else
+PKG_FAIL_REASION+=	"No valid Lua version found"
+
+.endif
+
+#LUA_DEPENDS=	${BUILDLINK_API_DEPENDS.${LUA_PACKAGE}}:${LUA_PKGSRCDIR}
+
+.include "${LUA_PKGSRCDIR}/version.mk"
+_LUA_DOT_VERSION=	${LUA_VERSION_MAJOR}.${LUA_VERSION_MINOR}
+
+.endif  # LUA_LUAVERSION_MK
