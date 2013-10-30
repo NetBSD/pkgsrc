ced89db98c0990495a5f3a3241dc03d716912af3
gls <gls>
2013-10-30 21:15:49 +0000

    Update devel/libconfig to 1.4.9. From Nils Ratusznik via PR pkg/48316.
    5 years worth of upstream changes, bugfixes and new features, too long
    to copy and paste here.
    
    pkgsrc changes:
    ---------------
    Add LICENSE

--- devel/libconfig/buildlink3.mk
+++ devel/libconfig/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:17 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/10/30 21:15:49 gls Exp $
 
 BUILDLINK_TREE+=	libconfig
 
 .if !defined(LIBCONFIG_BUILDLINK3_MK)
 LIBCONFIG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libconfig+=	libconfig>=1.1.3
+BUILDLINK_API_DEPENDS.libconfig+=	libconfig>=1.4
 BUILDLINK_PKGSRCDIR.libconfig?=		../../devel/libconfig
 .endif # LIBCONFIG_BUILDLINK3_MK
 
