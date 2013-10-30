6ec2c66894d62e57d1b1cc6cc941b0a7b28dc13c
obache <obache>
2013-10-30 09:02:06 +0000

    No need to define BUILDLINK_ABI_DEPENDS for flesh package. Set uppper limit to BUILDLINK_API_DEPENDS, same as lang/lua/luaversion.mk.

--- lang/lua52/buildlink3.mk
+++ lang/lua52/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/10/30 06:18:10 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/10/30 09:02:06 obache Exp $
 
 BUILDLINK_TREE+=	lua52
 
 .if !defined(LUA52_BUILDLINK3_MK)
 LUA52_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lua52+=	lua52>=5.2.0
-BUILDLINK_ABI_DEPENDS.lua52+=	lua52>=5.2.0
+BUILDLINK_API_DEPENDS.lua52+=	lua52>=5.2.0<5.3
 BUILDLINK_PKGSRCDIR.lua52?=	../../lang/lua52
 
 .include "../../mk/readline.buildlink3.mk"
