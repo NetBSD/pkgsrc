92ce6c20c117dba256826658b78c7ceca5718d08
adam <adam@pkgsrc.org>
2013-10-30 07:08:23 +0000

    Changes 2.1: - aecho filter - perspective filter ported from libmpcodecs - ffprobe -show_programs option - compand filter - RTMP seek support - when transcoding with ffmpeg (i.e. not streamcopying), -ss is now accurate   even when used as an input option. Previous behavior can be restored with   the -noaccurate_seek option. - ffmpeg -t option can now be used for inputs, to limit the duration of   data read from an input file - incomplete Voxware MetaSound decoder - read EXIF metadata from JPEG - DVB teletext decoder - phase filter ported from libmpcodecs - w3fdif filter - Opus support in Matroska - FFV1 version 1.3 is stable and no longer experimental - FFV1: YUVA(444,422,420) 9, 10 and 16 bit support - changed DTS stream id in lavf mpeg ps muxer from 0x8a to 0x88, to be   more consistent with other muxers. - adelay filter - pullup filter ported from libmpcodecs - ffprobe -read_intervals option - Lossless and alpha support for WebP decoder - Error Resilient AAC syntax (ER AAC LC) decoding - Low Delay AAC (ER AAC LD) decoding - mux chapters in ASF files - SFTP protocol (via libssh) - libx264: add ability to encode in YUVJ422P and YUVJ444P - Fraps: use BT.709 colorspace by default for yuv, as reference fraps decoder does - make decoding alpha optional for prores, ffv1 and vp6 by setting   the skip_alpha flag. - ladspa wrapper filter - native VP9 decoder - dpx parser - max_error_rate parameter in ffmpeg - PulseAudio output device - ReplayGain scanner - Enhanced Low Delay AAC (ER AAC ELD) decoding (no LD SBR support) - Linux framebuffer output device - HEVC decoder, raw HEVC demuxer, HEVC demuxing in TS, Matroska and MP4 - mergeplanes filter

--- multimedia/ffmpeg2/patches/patch-libavutil_x86_cpu.c
+++ multimedia/ffmpeg2/patches/patch-libavutil_x86_cpu.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-libavutil_x86_cpu.c,v 1.1 2013/08/04 10:29:02 adam Exp $
+$NetBSD: patch-libavutil_x86_cpu.c,v 1.2 2013/10/30 07:08:23 adam Exp $
 
---- libavutil/x86/cpu.c.orig	2013-01-06 21:53:29.000000000 +0000
+--- libavutil/x86/cpu.c.orig	2013-10-28 00:58:06.000000000 +0000
 +++ libavutil/x86/cpu.c
-@@ -115,6 +115,7 @@ int ff_get_cpu_flags_x86(void)
+@@ -116,6 +116,7 @@ int ff_get_cpu_flags_x86(void)
  #if HAVE_SSE
          if (std_caps & (1 << 25))
              rval |= AV_CPU_FLAG_SSE;
@@ -10,9 +10,9 @@ $NetBSD: patch-libavutil_x86_cpu.c,v 1.1 2013/08/04 10:29:02 adam Exp $
          if (std_caps & (1 << 26))
              rval |= AV_CPU_FLAG_SSE2;
          if (ecx & 1)
-@@ -134,6 +135,7 @@ int ff_get_cpu_flags_x86(void)
-                 rval |= AV_CPU_FLAG_AVX;
-         }
+@@ -143,6 +144,7 @@ int ff_get_cpu_flags_x86(void)
+     }
+ #endif /* HAVE_AVX2 */
  #endif /* HAVE_AVX */
 +#endif /* gcc >= 4.2 */
  #endif /* HAVE_SSE */
