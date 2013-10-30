16de1af22d7711eb51d6fbf3c4c19644f43fc452
wiz <wiz@pkgsrc.org>
2013-10-30 23:53:10 +0000

    Try fixing build on Solaris 10, from Akira TAGOH (upstream).

--- fonts/fontconfig/patches/patch-test_test-migration.c
+++ fonts/fontconfig/patches/patch-test_test-migration.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-test_test-migration.c,v 1.1 2013/10/21 08:21:04 wiz Exp $
+$NetBSD: patch-test_test-migration.c,v 1.2 2013/10/30 23:53:10 wiz Exp $
 
 Fix build on Solaris.
 From Akira TAGOH (upstream).
 
 --- test/test-migration.c.orig	2013-10-11 03:10:18.000000000 +0000
 +++ test/test-migration.c
-@@ -4,6 +4,11 @@
+@@ -4,8 +4,26 @@
  #include <unistd.h>
  #include <sys/types.h>
  #include <dirent.h>
@@ -16,8 +16,23 @@ From Akira TAGOH (upstream).
 +#endif
  #include <fontconfig/fontconfig.h>
  
++#ifdef HAVE_MKDTEMP
++#define fc_mkdtemp	mkdtemp
++#else
++char *
++fc_mkdtemp (char *template)
++{
++    if (!mktemp (template) || mkdir (template, 0700))
++	return NULL;
++
++    return template;
++}
++#endif
++
  FcBool
-@@ -36,6 +41,9 @@ unlink_dirs(const char *dir)
+ mkdir_p(const char *dir)
+ {
+@@ -36,6 +54,9 @@ unlink_dirs(const char *dir)
      size_t len = strlen (dir);
      char *n = NULL;
      FcBool ret = FcTrue;
@@ -27,7 +42,7 @@ From Akira TAGOH (upstream).
  
      if (!d)
  	return FcFalse;
-@@ -53,7 +61,17 @@ unlink_dirs(const char *dir)
+@@ -53,7 +74,17 @@ unlink_dirs(const char *dir)
  	strcpy (n, dir);
  	n[len] = '/';
  	strcpy (&n[len + 1], e->d_name);
@@ -45,3 +60,12 @@ From Akira TAGOH (upstream).
  	{
  	    if (!unlink_dirs (n))
  	    {
+@@ -89,7 +120,7 @@ int
+ main(void)
+ {
+     char template[32] = "fontconfig-XXXXXXXX";
+-    char *tmp = mkdtemp (template);
++    char *tmp = fc_mkdtemp (template);
+     size_t len = strlen (tmp), xlen, dlen;
+     char xdg[256], confd[256], fn[256], nfn[256], ud[256], nud[256];
+     int ret = -1;
