ce8728c9e74e707320df99809381f31f7fc64fc2
wiz <wiz@pkgsrc.org>
2013-10-31 09:05:32 +0000

    Use DEPENDS for openjade; there is no buildlink3.mk file for it.

--- textproc/gtk-doc/options.mk
+++ textproc/gtk-doc/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2013/10/24 08:48:09 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2013/10/31 09:05:32 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gtkdoc
 PKG_SUPPORTED_OPTIONS=	sgml
@@ -8,5 +8,5 @@ PKG_SUPPORTED_OPTIONS=	sgml
 .if !empty(PKG_OPTIONS:Msgml)
 DEPENDS+= dsssl-docbook-modular-[0-9]*:../../textproc/dsssl-docbook-modular
 SGML_CATALOGS= ${PREFIX}/share/sgml/gtk-doc/gtk-doc.cat
-.include "../../textproc/openjade/buildlink3.mk"
+DEPENDS+= openjade-[0-9]*:../../textproc/openjade
 .endif
