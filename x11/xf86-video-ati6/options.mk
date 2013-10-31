e2e1908b51665e0419cc367041db2cfec302acff
wiz <wiz@pkgsrc.org>
2013-10-31 00:07:23 +0000

    Since this package is only used on NetBSD, pull over all patches from xsrc. Bump PKGREVISION.

--- x11/xf86-video-ati6/options.mk
+++ x11/xf86-video-ati6/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2013/06/18 17:13:11 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2013/10/31 00:07:23 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xf86-video-ati6
 PKG_SUPPORTED_OPTIONS=	xf86-video-ati6-kms
-PKG_SUGGESTED_OPTIONS=	
+PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
 
