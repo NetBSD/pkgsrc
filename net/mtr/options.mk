92f4339d94f503c17ecb4f3fdc8a10ee2a8d8cd8
wiz <wiz@pkgsrc.org>
2013-10-31 00:38:20 +0000

    Make glib2 optional, but leave it default-on to stay as before. Based on patch by Jean-Yves Moulin <jym@baaz.fr> with slight changes.

--- net/mtr/options.mk
+++ net/mtr/options.mk
@@ -1,7 +1,8 @@
-# $NetBSD: options.mk,v 1.3 2009/03/22 16:21:44 smb Exp $
+# $NetBSD: options.mk,v 1.4 2013/10/31 00:38:20 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mtr
-PKG_SUPPORTED_OPTIONS=	gtk
+PKG_SUPPORTED_OPTIONS=	gtk glib
+PKG_SUGGESTED_OPTIONS=	glib
 
 .include "../../mk/bsd.options.mk"
 
@@ -9,4 +10,10 @@ PKG_SUPPORTED_OPTIONS=	gtk
 .include "../../x11/gtk2/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-gtk
+.endif
+
+.if !empty(PKG_OPTIONS:Mglib) || !empty(PKG_OPTIONS:Mgtk)
+.include "../../devel/glib2/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-glib
 .endif
