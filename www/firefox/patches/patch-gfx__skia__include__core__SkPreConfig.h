69a1f066083a847d963bd8374defdfd0ec44a03b
ryoon <ryoon@pkgsrc.org>
2013-11-02 22:57:55 +0000

    Update to 25.0
    * Enable pulseaudio by default, OSS support is dropped, and ALSA support
      on NetBSD does not work properly for me
    * Enable GStremer support for non-webm and non-theora video support
    * Create alsa option, and enabled on Linux by default
    
    Changelog:
     NEW
    Web Audio support
    NEW
    The find bar is no longer shared between tabs
    CHANGED
    If away from Firefox for months, you now will be offered the option to reset it to its default state while preserving your essential information
    CHANGED
    Resetting Firefox no longer clears your browsing session
    DEVELOPER
    CSS3 background-attachment:local support to control background scrolling
    DEVELOPER
    Many new ES6 functions implemented
    HTML5
    iframe document content can now be specified inline
    FIXED
    Blank or missing page thumbnails when opening a new tab
    FIXED
    Security fixes can be found here
    
    Fixed in Firefox 25
    MFSA 2013-102 Use-after-free in HTML document templates
    MFSA 2013-101 Memory corruption in workers
    MFSA 2013-100 Miscellaneous use-after-free issues found through ASAN fuzzing
    MFSA 2013-99 Security bypass of PDF.js checks using iframes
    MFSA 2013-98 Use-after-free when updating offline cache
    MFSA 2013-97 Writing to cycle collected object during image decoding
    MFSA 2013-96 Improperly initialized memory and overflows in some JavaScript functions
    MFSA 2013-95 Access violation with XSLT and uninitialized data
    MFSA 2013-94 Spoofing addressbar though SELECT element
    MFSA 2013-93 Miscellaneous memory safety hazards (rv:25.0 / rv:24.1 / rv:17.0.10)

--- www/firefox/patches/patch-gfx__skia__include__core__SkPreConfig.h
+++ www/firefox/patches/patch-gfx__skia__include__core__SkPreConfig.h
@@ -1,16 +1,16 @@
-$NetBSD: patch-gfx__skia__include__core__SkPreConfig.h,v 1.1 2013/06/21 23:11:42 ryoon Exp $
+$NetBSD: patch-gfx__skia__include__core__SkPreConfig.h,v 1.2 2013/11/02 22:57:55 ryoon Exp $
 
 Configure sparc cpus as big endian (Bugzilla #884376)
 
 diff -r 581ea0e6531e gfx/skia/include/core/SkPreConfig.h
---- gfx/skia/include/core/SkPreConfig.h	Tue May 07 09:47:43 2013 -0400
-+++ gfx/skia/include/core/SkPreConfig.h	Tue Jun 18 17:36:55 2013 +0200
-@@ -94,7 +94,7 @@
- //////////////////////////////////////////////////////////////////////
+--- gfx/skia/include/core/SkPreConfig.h.orig	2013-10-25 22:27:18.000000000 +0000
++++ gfx/skia/include/core/SkPreConfig.h
+@@ -100,7 +100,7 @@
  
  #if !defined(SK_CPU_BENDIAN) && !defined(SK_CPU_LENDIAN)
--#if defined (__ppc__) || defined(__PPC__) || defined(__ppc64__) || defined(__PPC64__)
-+#if defined (__ppc__) || defined(__PPC__) || defined(__ppc64__) || defined(__PPC64__) || defined(__sparc) || defined(__sparc__)
+     #if defined (__ppc__) || defined(__PPC__) || defined(__ppc64__) \
+-        || defined(__PPC64__)
++        || defined(__PPC64__) || defined(__sparc) || defined(__sparc__)
          #define SK_CPU_BENDIAN
      #else
          #define SK_CPU_LENDIAN
