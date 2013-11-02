0fc0b5c6b49deda945d2a152741cd472505c7e6b
wiz <wiz>
2013-11-02 09:38:34 +0000

    Update to 1.1.2:
    1.1.2 - 2013-10-22
    ------------------
    
    BUG FIXES
    
     * #442: [Linux] psutil won't compile on Debian 6.0 because of missing
             prlimit(2) syscall.
    
    
    1.1.1 - 2013-10-08
    ------------------
    
    BUG FIXES
    
     * #438: [Linux] psutil won't compile on kernels < 2.6.36 due to missing
             prlimit(2) syscall.
    
    
    1.1.0 - 2013-09-28
    ------------------
    
    ENHANCEMENTS
    
     * #410: tar.gz and windows binary files are now hosted on PYPI.
     * #412: [Linux] get/set process resource limits.
     * #415: [Windows] Process.get_children() is an order of magnitude faster.
     * #426: [Windows] Process.name is an order of magnitude faster.
     * #431: [UNIX] Process.name is slightly faster because it unnecessarily
             retrieved also process cmdline.
    
    BUG FIXES
    
     * #391: [Windows] psutil.cpu_times_percent() returns negative percentages.
     * #408: STATUS_* and CONN_* constants don't properly serialize on JSON.
     * #411: [Windows] examples/disk_usage.py may pop-up a GUI error.
     * #413: [Windows] Process.get_memory_info() leaks memory.
     * #414: [Windows] Process.exe on Windows XP may raise ERROR_INVALID_PARAMETER.
     * #416: psutil.disk_usage() doesn't work well with unicode path names.
     * #430: [Linux] process IO counters report wrong number of r/w syscalls.
     * #435: [Linux] psutil.net_io_counters() might report erreneous NIC names.
     * #436: [Linux] psutil.net_io_counters() reports a wrong 'dropin' value.
    
    API CHANGES
    
     * #408: STATUS_* and CONN_* constants (returned by Process' status() and
             get_connections() methods respectively) have been turned from constant
             objects to plain Python strings.

--- sysutils/py-psutil/patches/patch-setup.py
+++ sysutils/py-psutil/patches/patch-setup.py
@@ -1,22 +1,25 @@
-$NetBSD: patch-setup.py,v 1.1 2013/10/12 13:32:36 wiz Exp $
+$NetBSD: patch-setup.py,v 1.2 2013/11/02 09:38:34 wiz Exp $
 
 Port to NetBSD.
 
---- setup.py.orig	2013-06-17 10:35:09.000000000 +0000
+--- setup.py.orig	2013-10-22 18:12:21.000000000 +0000
 +++ setup.py
-@@ -108,6 +108,15 @@ elif sys.platform.startswith("freebsd"):
-                             libraries=["devstat"],
-                             ),
-                   posix_extension]
+@@ -145,6 +145,18 @@ elif sys.platform.startswith("freebsd"):
+         libraries=["devstat"]),
+         posix_extension,
+     ]
 +# NetBSD
 +elif sys.platform.startswith("netbsd"):
-+    extensions = [Extension('_psutil_netbsd',
-+                            sources = ['psutil/_psutil_netbsd.c',
-+                                       'psutil/_psutil_common.c',
-+                                       'psutil/arch/netbsd/process_info.c'],
-+                            libraries=[],
-+                            ),
-+                  posix_extension]
++    extensions = [Extension(
++        '_psutil_netbsd',
++        sources = [
++            'psutil/_psutil_netbsd.c',
++            'psutil/_psutil_common.c',
++            'psutil/arch/netbsd/process_info.c'
++        ],
++        libraries=[]),
++        posix_extension,
++    ]
  # Linux
  elif sys.platform.startswith("linux"):
-     extensions = [Extension('_psutil_linux',
+     kernel_ver = get_kernel_version()
