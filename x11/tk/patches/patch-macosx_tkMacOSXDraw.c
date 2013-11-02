efe44056a6b5364b699c0f6415ce26e515c91ba9
adam <adam@pkgsrc.org>
2013-11-02 20:22:02 +0000

    Changes 8.5.15: Bug-fix release.

--- x11/tk/patches/patch-macosx_tkMacOSXDraw.c
+++
@@ -1,15 +0,0 @@
-$NetBSD: patch-macosx_tkMacOSXDraw.c,v 1.1 2012/11/24 21:38:50 adam Exp $
-
-Void function should not return any values.
-
---- macosx/tkMacOSXDraw.c.orig	2012-11-24 20:55:20.000000000 +0000
-+++ macosx/tkMacOSXDraw.c
-@@ -744,7 +744,7 @@ DrawCGImage(
-     } else {
- 	TkMacOSXDbgMsg("Drawing of empty CGImage requested");
-     }
--    return Success;
-+    return;
- }
- 
- /*
