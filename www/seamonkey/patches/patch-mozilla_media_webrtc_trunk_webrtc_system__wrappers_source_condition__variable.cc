1bcccbeb353ca481c6550df46a6dc0eff683d89a
ryoon <ryoon>
2013-11-04 06:01:46 +0000

    Update to 2.22
    Changelog:
    SeaMonkey-specific changes
    
        Sorting messages by date can now be configured to look at the thread root instead of the newest message in it (pref: mailnews.sort_threads_by_root).
        Plugins doorhangers now allow to activate different plugin types independently.
        The proxy popup is now also available from the MailNews main window.
        A new Recipients column has been added that shows all recipients (To, CC, BCC).
        The default HTML5 audio/video player controls allow to change the playback rate now.
        A "Validate this page" entry has been added to Tools/Web Development.
        The Firefox devtools debugger can now be used to debug SeaMonkey remotely.
        See the changes page for a more complete overview.
    
    Mozilla platform changes
    
        Web Audio support has been added.
        CSS3 background-attachment:local support to control background scrolling has been implemented.
        Many new ES6 functions have been implemented.
        iframe document content can now be specified inline.
        Fixed several stability issues.
    
    Fixed in SeaMonkey 2.22
    MFSA 2013-102 Use-after-free in HTML document templates
    MFSA 2013-101 Memory corruption in workers
    MFSA 2013-100 Miscellaneous use-after-free issues found through ASAN fuzzing
    MFSA 2013-98 Use-after-free when updating offline cache
    MFSA 2013-97 Writing to cycle collected object during image decoding
    MFSA 2013-96 Improperly initialized memory and overflows in some JavaScript functions
    MFSA 2013-95 Access violation with XSLT and uninitialized data
    MFSA 2013-94 Spoofing addressbar though SELECT element
    MFSA 2013-93 Miscellaneous memory safety hazards (rv:25.0 / rv:24.1 / rv:17.0.10)

--- www/seamonkey/patches/patch-mozilla_media_webrtc_trunk_webrtc_system__wrappers_source_condition__variable.cc
+++ www/seamonkey/patches/patch-mozilla_media_webrtc_trunk_webrtc_system__wrappers_source_condition__variable.cc
@@ -1,11 +1,11 @@
-$NetBSD: patch-mozilla_media_webrtc_trunk_webrtc_system__wrappers_source_condition__variable.cc,v 1.2 2013/09/28 14:37:05 ryoon Exp $
+$NetBSD: patch-mozilla_media_webrtc_trunk_webrtc_system__wrappers_source_condition__variable.cc,v 1.3 2013/11/04 06:01:46 ryoon Exp $
 
---- mozilla/media/webrtc/trunk/webrtc/system_wrappers/source/condition_variable.cc.orig	2013-09-16 18:26:51.000000000 +0000
+--- mozilla/media/webrtc/trunk/webrtc/system_wrappers/source/condition_variable.cc.orig	2013-10-29 01:21:06.000000000 +0000
 +++ mozilla/media/webrtc/trunk/webrtc/system_wrappers/source/condition_variable.cc
-@@ -16,6 +16,8 @@
+@@ -17,6 +17,8 @@
  #elif defined(WEBRTC_LINUX) || defined(WEBRTC_BSD) || defined(WEBRTC_MAC)
  #include <pthread.h>
- #include "condition_variable_posix.h"
+ #include "webrtc/system_wrappers/source/condition_variable_posix.h"
 +#else
 +#include <stddef.h>  // for NULL
  #endif
--- www/seamonkey/patches/patch-mozilla_unwind.h
+++
@@ -1,9 +0,0 @@
-$NetBSD: patch-mozilla_unwind.h,v 1.3 2013/09/28 14:37:05 ryoon Exp $
-
---- mozilla/unwind.h.orig	2013-09-28 01:07:15.000000000 +0000
-+++ mozilla/unwind.h
-@@ -0,0 +1,4 @@
-+#pragma GCC system_header
-+#pragma GCC visibility push(default)
-+#include_next <unwind.h>
-+#pragma GCC visibility pop
