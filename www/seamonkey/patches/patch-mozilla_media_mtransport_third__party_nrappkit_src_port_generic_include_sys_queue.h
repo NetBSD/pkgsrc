21bb7d108b6e4f877dc695d89602fbb62074ada9
ryoon <ryoon@pkgsrc.org>
2013-11-04 06:01:46 +0000

    Update to 2.22
    Changelog:
    SeaMonkey-specific changes
    
        Sorting messages by date can now be configured to look at the thread root instead of the newest message in it (pref: mailnews.sort_threads_by_root).
        Plugins doorhangers now allow to activate different plugin types independently.
        The proxy popup is now also available from the MailNews main window.
        A new Recipients column has been added that shows all recipients (To, CC, BCC).
        The default HTML5 audio/video player controls allow to change the playback rate now.
        A "Validate this page" entry has been added to Tools/Web Development.
        The Firefox devtools debugger can now be used to debug SeaMonkey remotely.
        See the changes page for a more complete overview.
    
    Mozilla platform changes
    
        Web Audio support has been added.
        CSS3 background-attachment:local support to control background scrolling has been implemented.
        Many new ES6 functions have been implemented.
        iframe document content can now be specified inline.
        Fixed several stability issues.
    
    Fixed in SeaMonkey 2.22
    MFSA 2013-102 Use-after-free in HTML document templates
    MFSA 2013-101 Memory corruption in workers
    MFSA 2013-100 Miscellaneous use-after-free issues found through ASAN fuzzing
    MFSA 2013-98 Use-after-free when updating offline cache
    MFSA 2013-97 Writing to cycle collected object during image decoding
    MFSA 2013-96 Improperly initialized memory and overflows in some JavaScript functions
    MFSA 2013-95 Access violation with XSLT and uninitialized data
    MFSA 2013-94 Spoofing addressbar though SELECT element
    MFSA 2013-93 Miscellaneous memory safety hazards (rv:25.0 / rv:24.1 / rv:17.0.10)

--- www/seamonkey/patches/patch-mozilla_media_mtransport_third__party_nrappkit_src_port_generic_include_sys_queue.h
+++ www/seamonkey/patches/patch-mozilla_media_mtransport_third__party_nrappkit_src_port_generic_include_sys_queue.h
@@ -1,13 +1,41 @@
-$NetBSD: patch-mozilla_media_mtransport_third__party_nrappkit_src_port_generic_include_sys_queue.h,v 1.1 2013/05/23 13:25:30 ryoon Exp $
+$NetBSD: patch-mozilla_media_mtransport_third__party_nrappkit_src_port_generic_include_sys_queue.h,v 1.2 2013/11/04 06:01:46 ryoon Exp $
 
---- mozilla/media/mtransport/third_party/nrappkit/src/port/generic/include/sys/queue.h.orig	2013-05-03 03:08:06.000000000 +0000
+--- mozilla/media/mtransport/third_party/nrappkit/src/port/generic/include/sys/queue.h.orig	2013-10-29 01:21:04.000000000 +0000
 +++ mozilla/media/mtransport/third_party/nrappkit/src/port/generic/include/sys/queue.h
-@@ -33,7 +33,7 @@
- #ifndef _SYS_QUEUE_H_
+@@ -30,12 +30,15 @@
+  * $FreeBSD: src/sys/sys/queue.h,v 1.58 2004/04/07 04:19:49 imp Exp $
+  */
+ 
+-#ifndef _SYS_QUEUE_H_
++#if (defined(BSD) && !defined(__OpenBSD__)) || defined(DARWIN)
++#include_next <sys/queue.h>
++#elif !defined(_SYS_QUEUE_H_)
  #define	_SYS_QUEUE_H_
  
 -#ifndef DARWIN
-+#if !defined(__FreeBSD__) && !defined(DARWIN)
  #include <stddef.h>
- #define __offsetof offsetof
+-#define __offsetof offsetof
++
++#ifndef offsetof
++#define offsetof(type, field) ((size_t)(&((type *)0)->field))
  #endif
+ 
+ #define STAILQ_FOREACH_SAFE(var, head, field, tvar)                     \
+@@ -43,8 +46,6 @@
+              (var) && ((tvar) = STAILQ_NEXT((var), field), 1);           \
+              (var) = (tvar))
+ 
+-// #define __offsetof(type, field) ((size_t)(&((type *)0)->field))
+-
+ /*
+  * This file defines four types of data structures: singly-linked lists,
+  * singly-linked tail queues, lists and tail queues.
+@@ -285,7 +286,7 @@ struct {								\
+ 	(STAILQ_EMPTY((head)) ?						\
+ 		NULL :							\
+ 	        ((struct type *)					\
+-		((char *)((head)->stqh_last) - __offsetof(struct type, field))))
++		((char *)((head)->stqh_last) - offsetof(struct type, field))))
+ 
+ #define	STAILQ_NEXT(elm, field)	((elm)->field.stqe_next)
+ 
