f9a201fd2cd738c2a3c8571cf6c703cb39577c63
markd <markd@pkgsrc.org>
2013-11-07 13:03:07 +0000

    Update to KDE SC 4.11.3
    bugfixes, many new features, improved stability and performance.

--- net/kopete/patches/patch-protocols_jabber_libiris_src_jdns_jdns_sys.c
+++ net/kopete/patches/patch-protocols_jabber_libiris_src_jdns_jdns_sys.c
@@ -1,7 +1,7 @@
-$NetBSD: patch-ab,v 1.1.1.1 2009/10/02 20:17:16 markd Exp $
+$NetBSD: patch-protocols_jabber_libiris_src_jdns_jdns_sys.c,v 1.1 2013/11/07 13:03:07 markd Exp $
 
---- kopete/protocols/jabber/libiris/iris/jdns/jdns_sys.c.orig	2009-02-19 05:29:30.000000000 +1300
-+++ kopete/protocols/jabber/libiris/iris/jdns/jdns_sys.c
+--- protocols/jabber/libiris/src/jdns/jdns_sys.c.orig	2009-02-19 05:29:30.000000000 +1300
++++ protocols/jabber/libiris/src/jdns/jdns_sys.c
 @@ -682,6 +682,9 @@ static jdns_dnsparams_t *dnsparams_get_u
  #if defined(__GLIBC__) && ((__GLIBC__ > 2) || ((__GLIBC__ == 2) && (__GLIBC_MINOR__ >= 3)))
  # define JDNS_MODERN_RES_API
