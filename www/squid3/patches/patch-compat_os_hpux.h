91898bd92db70205de5d00809011ce5e9d756808
joerg <joerg@pkgsrc.org>
2013-11-07 00:47:04 +0000

    Fix header guard. Make sure that replacement operators are defined globally as required by C++ spec.

---
+++ www/squid3/patches/patch-compat_os_hpux.h
@@ -0,0 +1,11 @@
+$NetBSD: patch-compat_os_hpux.h,v 1.1 2013/11/07 00:47:05 joerg Exp $
+
+--- compat/os/hpux.h.orig	2013-11-07 00:29:30.000000000 +0000
++++ compat/os/hpux.h
+@@ -1,5 +1,5 @@
+ #ifndef SQUID_OS_HPUX_H
+-#define SQUID_OS_PHUX_H
++#define SQUID_OS_HPUX_H
+ 
+ #if _SQUID_HPUX_
+ 
