3fac79f9efd4cea716483b172b01468573f86be7
markd <markd>
2013-11-07 13:35:56 +0000

    Update to KDE SC 4.11.3
    bugfixes, many new features, improved stability and performance.

--- x11/kdelibs4/buildlink3.mk
+++ x11/kdelibs4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.33 2013/07/05 13:29:44 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2013/11/07 13:35:56 markd Exp $
 
 BUILDLINK_TREE+=	kdelibs
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kdelibs
 KDELIBS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kdelibs+=	kdelibs4>=4.0.0
-BUILDLINK_ABI_DEPENDS.kdelibs+=	kdelibs4>=4.10.3nb4
+BUILDLINK_ABI_DEPENDS.kdelibs+=	kdelibs4>=4.11.3
 BUILDLINK_PKGSRCDIR.kdelibs?=	../../x11/kdelibs4
 
 .include "../../archivers/bzip2/buildlink3.mk"
