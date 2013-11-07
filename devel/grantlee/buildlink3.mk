dbe113d3664e3f23f9feb7e6e4852538ff1a69d0
markd <markd@pkgsrc.org>
2013-11-07 08:13:51 +0000

    Import grantlee 0.3.0
    String template engine in QT based on Django template system

---
+++ devel/grantlee/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/11/07 08:13:51 markd Exp $
+
+BUILDLINK_TREE+=	grantlee
+
+.if !defined(GRANTLEE_BUILDLINK3_MK)
+GRANTLEE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.grantlee+=	grantlee>=0.3.0
+BUILDLINK_PKGSRCDIR.grantlee?=	../../devel/grantlee
+
+.include "../../x11/qt4-libs/buildlink3.mk"
+.endif	# GRANTLEE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-grantlee
