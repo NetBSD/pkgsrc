36514f9d7dc69e63e7c4b3768316e9b40185c2cc
joerg <joerg@pkgsrc.org>
2013-11-13 13:36:02 +0000

    Don't include -Wl,--whole-archive in the default LDFLAGS on NetBSD. It breaks the build in packages when the compiler doesn't implicitly disable it later. Bump revision.

--- lang/perl5/patches/patch-hints_netbsd.sh
+++ lang/perl5/patches/patch-hints_netbsd.sh
@@ -1,23 +1,29 @@
-$NetBSD: patch-hints_netbsd.sh,v 1.7 2013/08/19 12:31:11 christos Exp $
+$NetBSD: patch-hints_netbsd.sh,v 1.8 2013/11/13 13:36:02 joerg Exp $
 
 --whole-archive is a linker flag, not a compiler flag
 Better defaults for paths.
 Stop grovelling for functions we don't want to provide (*host*)
 
---- hints/netbsd.sh.orig	2013-08-19 21:23:34.000000000 +0300
-+++ hints/netbsd.sh	2013-08-19 21:23:12.000000000 +0300
-@@ -41,8 +41,8 @@
- # system gcc to build correctly, so check for it
- echo 'int f(void) { return 0; }' >try.c
- if ${cc:-cc} $cccdlflags -c try.c -otry.o 2>&1 &&
+--- hints/netbsd.sh.orig	2013-05-23 23:38:10.000000000 +0000
++++ hints/netbsd.sh
+@@ -36,16 +36,6 @@ case "$osvers" in
+ 		d_dlerror=$define
+ 		cccdlflags="-DPIC -fPIC $cccdlflags"
+ 		lddlflags="-shared $lddlflags"
+-		cat >UU/cc.cbu <<'EOCBU'
+-# gcc 4.6 doesn't support --whole-archive, but it's required for the
+-# system gcc to build correctly, so check for it
+-echo 'int f(void) { return 0; }' >try.c
+-if ${cc:-cc} $cccdlflags -c try.c -otry.o 2>&1 &&
 -   ${cc:-cc} --whole-archive $lddlflags try.o -otry.so 2>&1 ; then
 -    lddlflags="--whole-archive $lddlflags"
-+   ${cc:-cc} -Wl,--whole-archive $lddlflags try.o -otry.so 2>&1 ; then
-+    lddlflags="-Wl,--whole-archive $lddlflags"
- fi
- rm try.c try.o try.so 2>/dev/null
- EOCBU
-@@ -100,6 +100,12 @@
+-fi
+-rm try.c try.o try.so 2>/dev/null
+-EOCBU
+ 		rpathflag="-Wl,-rpath,"
+ 		case "$osvers" in
+ 		1.[0-5]*)
+@@ -100,6 +90,12 @@ case "$osvers" in
  	d_getservbyport_r="$undef"
  	d_setservent_r="$undef"
  	d_endservent_r="$undef"
@@ -30,7 +36,7 @@ Stop grovelling for functions we don't want to provide (*host*)
  	d_getprotoent_r_proto="0"
  	d_getprotobyname_r_proto="0"
  	d_getprotobynumber_r_proto="0"
-@@ -110,6 +116,12 @@
+@@ -110,6 +106,12 @@ case "$osvers" in
  	d_getservbyport_r_proto="0"
  	d_setservent_r_proto="0"
  	d_endservent_r_proto="0"
@@ -43,7 +49,7 @@ Stop grovelling for functions we don't want to provide (*host*)
  	;;
  esac
  
-@@ -186,10 +198,12 @@
+@@ -186,10 +188,12 @@ esac
  EOCBU
  
  # Set sensible defaults for NetBSD: look for local software in
