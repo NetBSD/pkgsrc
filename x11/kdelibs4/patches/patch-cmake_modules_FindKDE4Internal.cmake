8ccb24f2bfe0558cbae7c0f7bf8aaadc64e00aba
joerg <joerg>
2013-11-13 22:52:23 +0000

    Be more aggressive in the removal of -ansi. Bump revision.

--- x11/kdelibs4/patches/patch-cmake_modules_FindKDE4Internal.cmake
+++ x11/kdelibs4/patches/patch-cmake_modules_FindKDE4Internal.cmake
@@ -1,19 +1,24 @@
-$NetBSD: patch-cmake_modules_FindKDE4Internal.cmake,v 1.1 2013/04/03 10:51:44 markd Exp $
+$NetBSD: patch-cmake_modules_FindKDE4Internal.cmake,v 1.2 2013/11/13 22:52:23 joerg Exp $
 
-Dont pass -std=iso9899:1990 on NetBSD. Breaks ksysguardd at least.
+Don't force -std=iso9899:1990.
 
---- cmake/modules/FindKDE4Internal.cmake.orig	2013-03-01 06:26:13.000000000 +0000
+--- cmake/modules/FindKDE4Internal.cmake.orig	2013-11-01 15:45:56.000000000 +0000
 +++ cmake/modules/FindKDE4Internal.cmake
-@@ -1204,7 +1204,11 @@ if (CMAKE_COMPILER_IS_GNUCXX)
+@@ -1204,7 +1204,7 @@ if (CMAKE_COMPILER_IS_GNUCXX)
     set(CMAKE_C_FLAGS_DEBUGFULL        "-g3 -fno-inline")
     set(CMAKE_C_FLAGS_PROFILE          "-g3 -fno-inline -ftest-coverage -fprofile-arcs")
  
 -   set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -Wno-long-long -std=iso9899:1990 -Wundef -Wcast-align -Werror-implicit-function-declaration -Wchar-subscripts -Wall -W -Wpointer-arith -Wwrite-strings -Wformat-security -Wmissing-format-attribute -fno-common")
-+   if (CMAKE_SYSTEM_NAME MATCHES NetBSD)
-+     set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -Wno-long-long -Wundef -Wcast-align -Werror-implicit-function-declaration -Wchar-subscripts -Wall -W -Wpointer-arith -Wwrite-strings -Wformat-security -Wmissing-format-attribute -fno-common")
-+   else (CMAKE_SYSTEM_NAME MATCHES NetBSD)
-+     set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -Wno-long-long -std=iso9899:1990 -Wundef -Wcast-align -Werror-implicit-function-declaration -Wchar-subscripts -Wall -W -Wpointer-arith -Wwrite-strings -Wformat-security -Wmissing-format-attribute -fno-common")
-+   endif (CMAKE_SYSTEM_NAME MATCHES NetBSD)
++   set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -Wno-long-long -Wundef -Wcast-align -Werror-implicit-function-declaration -Wchar-subscripts -Wall -W -Wpointer-arith -Wwrite-strings -Wformat-security -Wmissing-format-attribute -fno-common")
     # As of Qt 4.6.x we need to override the new exception macros if we want compile with -fno-exceptions
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wnon-virtual-dtor -Wno-long-long -Wundef -Wcast-align -Wchar-subscripts -Wall -W -Wpointer-arith -Wformat-security -fno-exceptions -DQT_NO_EXCEPTIONS -fno-check-new -fno-common")
  
+@@ -1336,7 +1336,7 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "Clan
+    set(CMAKE_C_FLAGS_DEBUGFULL        "-g3 -fno-inline")
+    set(CMAKE_C_FLAGS_PROFILE          "-g3 -fno-inline -ftest-coverage -fprofile-arcs")
+ 
+-   set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -Wno-long-long -std=iso9899:1990 -Wundef -Wcast-align -Werror-implicit-function-declaration -Wchar-subscripts -Wall -W -Wpointer-arith -Wwrite-strings -Wformat-security -Wmissing-format-attribute -fno-common")
++   set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -Wno-long-long -Wundef -Wcast-align -Werror-implicit-function-declaration -Wchar-subscripts -Wall -W -Wpointer-arith -Wwrite-strings -Wformat-security -Wmissing-format-attribute -fno-common")
+    # As of Qt 4.6.x we need to override the new exception macros if we want compile with -fno-exceptions
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wnon-virtual-dtor -Wno-long-long -Wundef -Wcast-align -Wchar-subscripts -Wall -W -Wpointer-arith -Wformat-security -Woverloaded-virtual -fno-exceptions -DQT_NO_EXCEPTIONS -fno-common -fvisibility=hidden -Werror=return-type -fvisibility-inlines-hidden")
+    set(KDE4_C_FLAGS    "-fvisibility=hidden")
