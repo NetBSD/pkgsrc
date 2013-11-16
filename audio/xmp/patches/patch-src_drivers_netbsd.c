398f2f791a764d84e1be0cfe4bb5774e4cde314d
shattered <shattered@pkgsrc.org>
2013-11-16 07:41:02 +0000

    Update to 4.0.6. Changes:
    - Rewrite player to use libxmp 4.0
    - Give Pulseaudio higher priority than ALSA
    - Give ALSA output higher priority than OSS
    - Fix native-endian coreaudio output
    - Fix module-specifc flags to override built-in quirks
    - Fix loop when skipping to first file and it's not playable
    
    - Add commands '<' and '>' to jump to previous/next sequence
    - Add command 'z' to toggle the subsong explorer on/off
    - Add option -z <num> to play a specified sequence
    - Add option -Z to play all sequences in module
    - Add option --loop-all to loop over entire module list

--- audio/xmp/patches/patch-src_drivers_netbsd.c
+++ audio/xmp/patches/patch-src_drivers_netbsd.c
@@ -1,8 +1,34 @@
-$NetBSD: patch-src_drivers_netbsd.c,v 1.1 2011/04/05 05:22:52 rxg Exp $
+$NetBSD: patch-src_drivers_netbsd.c,v 1.2 2013/11/16 07:41:02 shattered Exp $
 
---- src/drivers/netbsd.c.orig	2010-01-09 22:34:20.000000000 +0000
-+++ src/drivers/netbsd.c
-@@ -174,7 +174,7 @@ static void bufdump(struct xmp_context *
+--- src/sound_netbsd.c.orig	2013-04-06 13:38:02.000000000 +0000
++++ src/sound_netbsd.c
+@@ -68,21 +68,23 @@ static int init(struct options *options)
+ 
+ 	AUDIO_INITINFO(&ainfo);
+ 
++	ainfo.mode = AUMODE_PLAY_ALL;
+ 	ainfo.play.sample_rate = options->rate;
+ 	ainfo.play.channels = options->format & XMP_FORMAT_MONO ? 1 : 2;
+ 
+ 	if (options->format & XMP_FORMAT_8BIT) {
+ 		ainfo.play.precision = 8;
+-		ainfo.play.precision = AUDIO_ENCODING_ULINEAR;
++		ainfo.play.encoding = AUDIO_ENCODING_ULINEAR;
+ 		options->format |= XMP_FORMAT_UNSIGNED;
+ 	} else {
+ 		ainfo.play.precision = 16;
+-		ainfo.play.precision = AUDIO_ENCODING_SLINEAR;
++		ainfo.play.encoding = AUDIO_ENCODING_SLINEAR;
+ 		options->format &= ~XMP_FORMAT_UNSIGNED;
+ 	}
+ 
+ 	ainfo.play.gain = gain;
+ 	ainfo.play.buffer_size = bsize;
++	ainfo.blocksize = 0;
+ 
+ 	if (ioctl(audio_fd, AUDIO_SETINFO, &ainfo) == -1) {
+ 		close(audio_fd);
+@@ -99,7 +101,7 @@ static void play(void *b, int i)
  	while (i) {
  		if ((j = write(audio_fd, b, i)) > 0) {
  			i -= j;
