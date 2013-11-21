1e22912fc17508654a6134c5689cb0e1d7ca7eef
adam <adam@pkgsrc.org>
2013-11-21 14:06:43 +0000

    Changes 3.3.3: PEP 380, syntax for delegating to a subgenerator (yield from) PEP 393, flexible string representation (doing away with the distinction between "wide" and "narrow" Unicode builds) A C implementation of the "decimal" module, with up to 120x speedup for decimal-heavy applications The import system (__import__) is based on importlib by default The new "lzma" module with LZMA/XZ support PEP 397, a Python launcher for Windows PEP 405, virtual environment support in core PEP 420, namespace package support PEP 3151, reworking the OS and IO exception hierarchy PEP 3155, qualified name for classes and functions PEP 409, suppressing exception context PEP 414, explicit Unicode literals to help with porting PEP 418, extended platform-independent clocks in the "time" module PEP 412, a new key-sharing dictionary implementation that significantly saves memory for object-oriented code PEP 362, the function-signature object The new "faulthandler" module that helps diagnosing crashes The new "unittest.mock" module The new "ipaddress" module The "sys.implementation" attribute A policy framework for the email package, with a provisional (see PEP 411) policy that adds much improved unicode support for email header parsing A "collections.ChainMap" class for linking mappings to a single unit Wrappers for many more POSIX functions in the "os" and "signal" modules, as well as other useful functions such as "sendfile()" Hash randomization, introduced in earlier bugfix releases, is now switched on by default

--- lang/python33/dist.mk
+++ lang/python33/dist.mk
@@ -1,8 +1,8 @@
-# $NetBSD: dist.mk,v 1.2 2013/05/26 17:56:09 wiz Exp $
+# $NetBSD: dist.mk,v 1.3 2013/11/21 14:06:43 adam Exp $
 
-PY_DISTVERSION=	3.3.2
+PY_DISTVERSION=	3.3.3
 DISTNAME=	Python-${PY_DISTVERSION}
-EXTRACT_SUFX=	.tar.bz2
+EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python33/distinfo
 PATCHDIR=	${.CURDIR}/../../lang/python33/patches
 MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION}/
