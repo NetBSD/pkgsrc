ccd96b6fddd24274865951b49e56933db3b81f32
taca <taca@pkgsrc.org>
2013-11-24 15:26:30 +0000

    Fix wrong parameter from [ruby-list:49643] in ruby-tk package.
    Bump PKGREVISION.

---
+++ lang/ruby18-base/patches/patch-ext_tk_lib_tkextlib_tktable_tktable.rb
@@ -0,0 +1,15 @@
+$NetBSD: patch-ext_tk_lib_tkextlib_tktable_tktable.rb,v 1.1 2013/11/24 15:26:30 taca Exp $
+
+Fix wrong parameter from [ruby-list:49643].
+
+--- ext/tk/lib/tkextlib/tktable/tktable.rb.orig	2008-05-23 05:22:13.000000000 +0000
++++ ext/tk/lib/tkextlib/tktable/tktable.rb
+@@ -70,7 +70,7 @@ module Tk::TkTable::ConfigMethod
+   private :__item_strval_optkeys
+ 
+   def __item_val2ruby_optkeys(id)  # { key=>method, ... }
+-    super(id).update('window'=>proc{|v| window(v)})
++    super(id).update('window'=>proc{|k, v| window(v)})
+   end
+   private :__item_val2ruby_optkeys
+ 
