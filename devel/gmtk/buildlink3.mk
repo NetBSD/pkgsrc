eb176050d3e831a2fa012b527b03c37f80526d42
wiz <wiz@pkgsrc.org>
2013-11-25 12:00:45 +0000

    PKGREVISION bump for json-c shlib rename.

--- devel/gmtk/buildlink3.mk
+++ devel/gmtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2013/10/10 14:41:49 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2013/11/25 12:00:45 wiz Exp $
 #
 
 BUILDLINK_TREE+=	gmtk
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	gmtk
 GMTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gmtk+=	gmtk>=1.0.7
-BUILDLINK_ABI_DEPENDS.gmtk+=	gmtk>=1.0.8nb5
+BUILDLINK_ABI_DEPENDS.gmtk+=	gmtk>=1.0.8nb6
 BUILDLINK_PKGSRCDIR.gmtk?=	../../devel/gmtk
 
 pkgbase := gmtk
