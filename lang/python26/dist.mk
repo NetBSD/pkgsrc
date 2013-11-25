364394a5a38c6a7215452b7579061ee20523c92b
tron <tron>
2013-11-25 10:30:45 +0000

    Pullup ticket #4256 - requested by spz lang/python26: security update
    Revisions pulled up:
    - lang/python26/Makefile                                        1.56
    - lang/python26/PLIST.common                                    1.15
    - lang/python26/buildlink3.mk                                   1.6
    - lang/python26/dist.mk                                         1.4
    - lang/python26/distinfo                                        1.52
    - lang/python26/patches/patch-CVE-2013-4238                     deleted
    - lang/python26/patches/patch-al                                1.12
    
    ---
       Module Name:	pkgsrc
       Committed By:	adam
       Date:		Wed Nov  6 07:25:49 UTC 2013
    
       Modified Files:
       	pkgsrc/lang/python26: Makefile PLIST.common buildlink3.mk dist.mk
       	    distinfo
       	pkgsrc/lang/python26/patches: patch-al
       Removed Files:
       	pkgsrc/lang/python26/patches: patch-CVE-2013-4238
    
       Log Message:
       Python 2.6.9 is a security-fix source-only release for Python 2.6.8, fixing several reported security issues: issue 16037, issue 16038, issue 16039, issue 16040, issue 16041, and issue 16042 (CVE-2013-1752, long lines consuming too much memory), as well as issue 14984 (security enforcement on $HOME/.netrc files), issue 16248 (code execution vulnerability in tkinter), and issue 18709 (CVE-2013-4238, SSL module handling of NULL bytes inside subjectAltName).

--- lang/python26/dist.mk
+++ lang/python26/dist.mk
@@ -1,8 +1,8 @@
-# $NetBSD: dist.mk,v 1.3 2012/04/14 10:47:17 obache Exp $
+# $NetBSD: dist.mk,v 1.3.12.1 2013/11/25 10:30:45 tron Exp $
 
-PY_DISTVERSION=	2.6.8
+PY_DISTVERSION=	2.6.9
 DISTNAME=	Python-${PY_DISTVERSION}
-EXTRACT_SUFX=	.tar.bz2
+EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python26/distinfo
 PATCHDIR=	${.CURDIR}/../../lang/python26/patches
 MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION}/
