9f466b2037f478be2b3dd4aa001f08f27c8884df
adam <adam@pkgsrc.org>
2013-12-01 11:56:41 +0000

    GTK+ Integration for the Mac OS X desktop, like the menubar, dock and app bundles. This library provides two implementations: An older Carbon implementation (gtk-mac-integration) and a new implementation in Cocoa (GtkOSXApplication). PyGtk bindings are provided for both.

---
+++ x11/gtk-mac-integration/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/12/01 11:56:41 adam Exp $
+
+BUILDLINK_TREE+=	gtk-mac-integration
+
+.if !defined(GTK_MAC_INTEGRATION_BUILDLINK3_MK)
+GTK_MAC_INTEGRATION_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gtk-mac-integration+=	gtk-mac-integration>=2.0.0
+BUILDLINK_PKGSRCDIR.gtk-mac-integration?=	../../x11/gtk-mac-integration
+
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# GTK_MAC_INTEGRATION_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gtk-mac-integration
