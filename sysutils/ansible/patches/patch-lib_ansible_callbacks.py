375873f43da8e3fdd4bcc9c2f73d056cc57442bb
hubertf <hubertf@pkgsrc.org>
2013-12-02 22:54:46 +0000

    Update ansible to 1.4.1.
    Some changes:
     * Many new modules, sorted in the following subdirs:
       cloud commands database files internal inventory messaging
       monitoring net_infrastructure network notification packaging
       source_control system utilities web_infrastructure
     * accelerated mode: An enhanced fireball mode that requires zero
       bootstrapping and fewer requirements plus adds capabilities like
       sudo commands
     * Connection default is now "smart", which discovers if the system
       openssh can support ControlPersist, and uses it if so, if not
       falls back to paramiko
     * external inventory scripts may now return host variables in one
       pass, which allows them to be much more efficient for large numbers
       of hosts (AWS!)
     * "~" now expanded on each component of configured plugin paths
     * Countless feature enhancements and bugfixes
    
    
    For a full list, see https://github.com/ansible/ansible/blob/devel/CHANGELOG.md

--- sysutils/ansible/patches/patch-lib_ansible_callbacks.py
+++ sysutils/ansible/patches/patch-lib_ansible_callbacks.py
@@ -1,16 +1,16 @@
-$NetBSD: patch-lib_ansible_callbacks.py,v 1.2 2013/04/13 17:51:57 riz Exp $
+$NetBSD: patch-lib_ansible_callbacks.py,v 1.3 2013/12/02 22:54:46 hubertf Exp $
 
---- lib/ansible/callbacks.py.orig	2013-03-30 16:29:28.000000000 -0700
-+++ lib/ansible/callbacks.py	2013-04-13 09:05:36.000000000 -0700
-@@ -30,9 +30,9 @@
-     cowsay = "/usr/bin/cowsay"
- elif os.path.exists("/usr/games/cowsay"):
-     cowsay = "/usr/games/cowsay"
--elif os.path.exists("/usr/local/bin/cowsay"):
-+elif os.path.exists("@LOCALBASE@/bin/cowsay"):
-     # BSD path for cowsay
--    cowsay = "/usr/local/bin/cowsay"
-+    cowsay = "@LOCALBASE@/bin/cowsay"
- elif os.path.exists("/opt/local/bin/cowsay"):
-     # MacPorts path for cowsay
-     cowsay = "/opt/local/bin/cowsay"
+--- lib/ansible/callbacks.py.orig	2013-11-19 19:12:32.000000000 +0000
++++ lib/ansible/callbacks.py
+@@ -55,9 +55,9 @@ def get_cowsay_info():
+         cowsay = "/usr/bin/cowsay"
+     elif os.path.exists("/usr/games/cowsay"):
+         cowsay = "/usr/games/cowsay"
+-    elif os.path.exists("/usr/local/bin/cowsay"):
++    elif os.path.exists("@LOCALBASE@/bin/cowsay"):
+         # BSD path for cowsay
+-        cowsay = "/usr/local/bin/cowsay"
++        cowsay = "@LOCALBASE@/bin/cowsay"
+     elif os.path.exists("/opt/local/bin/cowsay"):
+         # MacPorts path for cowsay
+         cowsay = "/opt/local/bin/cowsay"
