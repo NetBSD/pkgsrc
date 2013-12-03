66b598593cfc8672bd18f62e6ed23c8a0453892b
adam <adam@pkgsrc.org>
2013-12-03 20:02:41 +0000

    Changes 5.6.15: * Previously, MySQL Server distributions included the MySQL Reference Manual in Info format (the Docs/mysql.info file). Because the license for the manual restricts redistribution, its inclusion in Community packages caused problems for downstream redistributors, such as those who create Linux distributions. Community distributions of MySQL Server no longer include the mysql.info file, to make the repackaging and redistribution process easier (for example, the source tarball and its checksum can be used directly). This change applies to all source and binary Community packaging formats. Commercial (Enterprise) distributions are unchanged. * Incompatible Change: Several statement instruments in the setup_instruments table are used by the Performance Schema during the early stages of statement classification before the exact statement type is known. * The Performance Schema now instruments the read/write lock Delegate::lock * A new CMake option, WITH_ASAN, permits enabling address sanitization for compilers that support it. * The hash function used for metadata locking was modified to reduce overhead. * Bugs Fixed

--- databases/mysql56-client/patches/patch-vio_viossl.c
+++ databases/mysql56-client/patches/patch-vio_viossl.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-vio_viossl.c,v 1.1 2013/04/28 19:24:44 adam Exp $
+$NetBSD: patch-vio_viossl.c,v 1.2 2013/12/03 20:02:42 adam Exp $
 
 Compatibility fix.
 
 --- vio/viossl.c.orig	2013-04-05 12:27:17.000000000 +0000
 +++ vio/viossl.c
-@@ -327,7 +327,7 @@ static int ssl_do(struct st_VioSSLFd *pt
+@@ -391,7 +391,7 @@ static int ssl_do(struct st_VioSSLFd *pt
    SSL_clear(ssl);
    SSL_SESSION_set_timeout(SSL_get_session(ssl), timeout);
    SSL_set_fd(ssl, sd);
