6599fc6fc2199721431c620d84139199bb925204
adam <adam@pkgsrc.org>
2013-12-03 20:02:41 +0000

    Changes 5.6.15: * Previously, MySQL Server distributions included the MySQL Reference Manual in Info format (the Docs/mysql.info file). Because the license for the manual restricts redistribution, its inclusion in Community packages caused problems for downstream redistributors, such as those who create Linux distributions. Community distributions of MySQL Server no longer include the mysql.info file, to make the repackaging and redistribution process easier (for example, the source tarball and its checksum can be used directly). This change applies to all source and binary Community packaging formats. Commercial (Enterprise) distributions are unchanged. * Incompatible Change: Several statement instruments in the setup_instruments table are used by the Performance Schema during the early stages of statement classification before the exact statement type is known. * The Performance Schema now instruments the read/write lock Delegate::lock * A new CMake option, WITH_ASAN, permits enabling address sanitization for compilers that support it. * The hash function used for metadata locking was modified to reduce overhead. * Bugs Fixed

--- databases/mysql56-client/patches/patch-include_my__global.h
+++ databases/mysql56-client/patches/patch-include_my__global.h
@@ -1,17 +1,8 @@
-$NetBSD: patch-include_my__global.h,v 1.1 2013/04/29 21:16:56 joerg Exp $
+$NetBSD: patch-include_my__global.h,v 1.2 2013/12/03 20:02:42 adam Exp $
 
 --- include/my_global.h.orig	2013-04-28 14:06:29.000000000 +0000
 +++ include/my_global.h
-@@ -465,7 +465,7 @@ typedef unsigned short ushort;
- #endif
- 
- #define swap_variables(t, a, b) { t dummy; dummy= a; a= b; b= dummy; }
--#define test(a)		((a) ? 1 : 0)
-+#define my_test(a)		((a) ? 1 : 0)
- #define set_if_bigger(a,b)  do { if ((a) < (b)) (a)=(b); } while(0)
- #define set_if_smaller(a,b) do { if ((a) > (b)) (a)=(b); } while(0)
- #define test_all_bits(a,b) (((a) & (b)) == (b))
-@@ -1125,7 +1125,8 @@ typedef char		my_bool; /* Small bool */
+@@ -1119,7 +1119,8 @@ typedef char		my_bool; /* Small bool */
  
  /* Provide __func__ macro definition for platforms that miss it. */
  #if __STDC_VERSION__ < 199901L
