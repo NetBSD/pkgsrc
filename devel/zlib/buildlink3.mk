f48bf932d2f114a2f4e13e3f3b50ef4dd60449fc
obache <obache@pkgsrc.org>
2013-12-04 14:14:33 +0000

    Drop nb1 from API_DEPENDS.
    zlib-1.1.4nb1 is just recommended for CAN-2003-0107, not API change.

--- devel/zlib/buildlink3.mk
+++ devel/zlib/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.24 2009/03/20 19:24:31 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2013/12/04 14:14:33 obache Exp $
 
 BUILDLINK_TREE+=	zlib
 
 .if !defined(ZLIB_BUILDLINK3_MK)
 ZLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.zlib+=	zlib>=1.1.4nb1
+BUILDLINK_API_DEPENDS.zlib+=	zlib>=1.1.4
 BUILDLINK_ABI_DEPENDS.zlib+=	zlib>=1.2.3
 BUILDLINK_PKGSRCDIR.zlib?=	../../devel/zlib
 .endif # ZLIB_BUILDLINK3_MK
