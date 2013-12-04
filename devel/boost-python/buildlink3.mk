f720a83f49ab9addd267527689f223c77948272e
jperkin <jperkin@pkgsrc.org>
2013-12-04 11:48:40 +0000

    Fix typo in dependency.

--- devel/boost-python/buildlink3.mk
+++ devel/boost-python/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.31 2013/11/12 16:49:07 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2013/12/04 11:48:40 jperkin Exp $
 
 BUILDLINK_TREE+=	boost-python
 
@@ -7,7 +7,7 @@ BOOST_PYTHON_BUILDLINK3_MK:=
 
 # Use a dependency pattern that guarantees the proper ABI.
 BUILDLINK_API_DEPENDS.boost-python+=	boost-python-1.55.*
-BUILDLINK_ABI_DEPENDS.boost-python+=	boost-python=1.55.*
+BUILDLINK_ABI_DEPENDS.boost-python+=	boost-python-1.55.*
 BUILDLINK_PKGSRCDIR.boost-python?=	../../devel/boost-python
 
 .include "../../devel/boost-headers/buildlink3.mk"
