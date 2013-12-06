be95202f94d076c5120bd5e0a826366c4cbfa5b7
wiz <wiz@pkgsrc.org>
2013-12-06 13:23:45 +0000

    Recursive PKGREVISION bump for poppler-0.24.4.

--- print/poppler-glib/buildlink3.mk
+++ print/poppler-glib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.38 2013/09/02 19:50:39 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.39 2013/12/06 13:23:45 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-glib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-glib
 POPPLER_GLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-glib+=	poppler-glib>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler-glib+=	poppler-glib>=0.24.1nb1
+BUILDLINK_ABI_DEPENDS.poppler-glib+=	poppler-glib>=0.24.4
 BUILDLINK_PKGSRCDIR.poppler-glib?=	../../print/poppler-glib
 
 .include "../../devel/glib2/buildlink3.mk"
--- print/poppler-qt4/buildlink3.mk
+++ print/poppler-qt4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2013/08/22 09:11:16 prlw1 Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2013/12/06 13:23:45 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-qt4
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-qt4
 POPPLER_QT4_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-qt4+=	poppler-qt4>=0.6.1
-BUILDLINK_ABI_DEPENDS.poppler-qt4+=	poppler-qt4>=0.24.0
+BUILDLINK_ABI_DEPENDS.poppler-qt4+=	poppler-qt4>=0.24.4
 BUILDLINK_PKGSRCDIR.poppler-qt4?=	../../print/poppler-qt4
 
 .include "../../print/poppler/buildlink3.mk"
