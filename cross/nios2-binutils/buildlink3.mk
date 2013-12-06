2cfa66359cf8e4bfd4b99983590e26bacd0c2c44
mef <mef@pkgsrc.org>
2013-12-06 01:08:27 +0000

    Very minor typo in comment part (- to _) .

--- cross/nios2-binutils/buildlink3.mk
+++ cross/nios2-binutils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/11/18 06:41:27 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/12/06 01:08:27 mef Exp $
 
 BUILDLINK_TREE+=	nios2-binutils
 
@@ -11,6 +11,6 @@ BUILDLINK_FNAME_TRANSFORM.nios2-binutils+=	-e "s,${NIOS2_TARGET}/lib,lib/,g"
 #BUILDLINK_CONTENTS_FILTER.nios2-binutils=	${EGREP} '.*'
 BUILDLINK_CONTENTS_FILTER.nios2-binutils=	${GREP} '${CROSSBASE}'
 
-.endif # NIOS2-BINUTILS_BUILDLINK3_MK
+.endif # NIOS2_BINUTILS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-nios2-binutils
