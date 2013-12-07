dde826cda0676ae86a9ba1914712433a18616c12
richard <richard@pkgsrc.org>
2013-12-07 21:25:23 +0000

    Update to 1.2.1, changelog follows:
    1.2.1 - 2013-11-25
    ------------------
    
    BUG FIXES
    
     * #348: [Windows XP] fixed "ImportError: DLL load failed" occurring on module
             import.
     * #425: [Solaris] crash on import due to failure at determining BOOT_TIME.
     * #443: [Linux] can't set CPU affinity on systems with more than 64 cores.
    
    
    1.2.0 - 2013-11-20
    ------------------
    
    ENHANCEMENTS
    
     * #439: assume os.getpid() if no argument is passed to psutil.Process
             constructor.
     * #440: new psutil.wait_procs() utility function which waits for multiple
             processes to terminate.
    
    BUG FIXES
    
     * #348: [Windows XP/Vista] fixed "ImportError: DLL load failed" occurring on
             module import.
    
    
    1.1.3 - 2013-11-07
    ------------------
    
    BUG FIXES
    
     * #442: [Linux] psutil won't compile on certain version of Linux because of
             missing prlimit(2) syscall.

--- sysutils/py-psutil/patches/patch-setup.py
+++ sysutils/py-psutil/patches/patch-setup.py
@@ -1,25 +1,25 @@
-$NetBSD: patch-setup.py,v 1.2 2013/11/02 09:38:34 wiz Exp $
+$NetBSD: patch-setup.py,v 1.3 2013/12/07 21:25:24 richard Exp $
 
 Port to NetBSD.
 
---- setup.py.orig	2013-10-22 18:12:21.000000000 +0000
+--- setup.py.orig	2013-11-20 19:29:05.000000000 +0000
 +++ setup.py
-@@ -145,6 +145,18 @@ elif sys.platform.startswith("freebsd"):
+@@ -122,6 +122,18 @@ elif sys.platform.startswith("freebsd"):
          libraries=["devstat"]),
          posix_extension,
      ]
 +# NetBSD
 +elif sys.platform.startswith("netbsd"):
-+    extensions = [Extension(
-+        '_psutil_netbsd',
-+        sources = [
-+            'psutil/_psutil_netbsd.c',
-+            'psutil/_psutil_common.c',
-+            'psutil/arch/netbsd/process_info.c'
-+        ],
-+        libraries=[]),
-+        posix_extension,
-+    ]
++	extensions = [Extension(
++		'_psutil_netbsd',
++		sources = [
++			'psutil/_psutil_netbsd.c',
++			'psutil/_psutil_common.c',
++			'psutil/arch/netbsd/process_info.c'
++		],
++		libraries=[]),
++		posix_extension,
++	]
  # Linux
  elif sys.platform.startswith("linux"):
-     kernel_ver = get_kernel_version()
+     extensions = [Extension(
--- sysutils/py-psutil/patches/patch-test_test__psutil.py
+++ sysutils/py-psutil/patches/patch-test_test__psutil.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-test_test__psutil.py,v 1.1 2013/10/12 13:32:36 wiz Exp $
+$NetBSD: patch-test_test__psutil.py,v 1.2 2013/12/07 21:25:24 richard Exp $
 
 Port to NetBSD.
 
---- test/test_psutil.py.orig	2013-07-10 14:02:21.000000000 +0000
+--- test/test_psutil.py.orig	2013-11-22 21:26:42.000000000 +0000
 +++ test/test_psutil.py
-@@ -62,7 +62,7 @@ POSIX = os.name == 'posix'
+@@ -77,7 +77,7 @@ POSIX = os.name == 'posix'
  LINUX = sys.platform.startswith("linux")
  WINDOWS = sys.platform.startswith("win32")
  OSX = sys.platform.startswith("darwin")
 -BSD = sys.platform.startswith("freebsd")
 +BSD = sys.platform.startswith("freebsd") or sys.platform.startswith("netbsd")
  SUNOS = sys.platform.startswith("sunos")
- 
- 
+ VALID_PROC_STATUSES = [getattr(psutil, x) for x in dir(psutil)
+                        if x.startswith('STATUS_')]
