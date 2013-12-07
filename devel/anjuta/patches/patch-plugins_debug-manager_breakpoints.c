f8d9797f6e4861b951a4d61613c78b2cb03f8f2a
dholland <dholland@pkgsrc.org>
2013-12-07 23:47:02 +0000

    Fix unbuildable patches.
    When inserting attributes, insert them in places where they're
    syntactically valid. When inserting gcc pragmas, insert them only in
    places where gcc accepts them.
    
    HI JOERG.

--- devel/anjuta/patches/patch-plugins_debug-manager_breakpoints.c
+++ devel/anjuta/patches/patch-plugins_debug-manager_breakpoints.c
@@ -1,26 +1,21 @@
-$NetBSD: patch-plugins_debug-manager_breakpoints.c,v 1.1 2013/10/06 20:01:27 joerg Exp $
+$NetBSD: patch-plugins_debug-manager_breakpoints.c,v 1.2 2013/12/07 23:47:02 dholland Exp $
 
 --- plugins/debug-manager/breakpoints.c.orig	2013-08-09 00:30:17.000000000 +0000
 +++ plugins/debug-manager/breakpoints.c
-@@ -445,7 +445,10 @@ breakpoints_dbase_update_in_treeview (Br
- 		filename = bi->bp.function;
- 		format = filename == NULL ? "??" : "%s";
+@@ -423,6 +423,8 @@ breakpoints_dbase_clear_in_editor (Break
  	}
+ }
+ 
 +#pragma GCC diagnostic push
 +#pragma GCC diagnostic ignored "-Wformat-nonliteral"
- 	location = g_strdup_printf (format, filename, bi->bp.line, bi->bp.function);
-+#pragma GCC diagnostic pop
- 	if (bi->bp.id == 0)
- 	{
- 		pass = g_strdup_printf ("%d", bi->bp.ignore);
-@@ -474,7 +477,10 @@ breakpoints_dbase_update_in_treeview (Br
- 	{
- 		format = "permanent (%d)";
- 	}
-+#pragma GCC diagnostic push
-+#pragma GCC diagnostic ignored "-Wformat-nonliteral"
- 	state = g_strdup_printf (format, bi->bp.id);
+ static void
+ breakpoints_dbase_update_in_treeview (BreakpointsDBase *bd, BreakpointItem *bi)
+ {
+@@ -490,6 +492,7 @@ breakpoints_dbase_update_in_treeview (Br
+ 	g_free (location);
+ 	g_free (adr);
+ }
 +#pragma GCC diagnostic pop
  
- 	gtk_list_store_set (bd->model, &bi->iter,
- 						ENABLED_COLUMN, bi->bp.enable,
+ static void
+ breakpoints_dbase_breakpoint_removed (BreakpointsDBase *bd, BreakpointItem *bi)
