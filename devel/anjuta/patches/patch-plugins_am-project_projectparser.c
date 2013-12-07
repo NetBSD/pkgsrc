0fdf8635323a8adb0e2925fc5f3037562061004c
dholland <dholland@pkgsrc.org>
2013-12-07 23:47:02 +0000

    Fix unbuildable patches.
    When inserting attributes, insert them in places where they're
    syntactically valid. When inserting gcc pragmas, insert them only in
    places where gcc accepts them.
    
    HI JOERG.

--- devel/anjuta/patches/patch-plugins_am-project_projectparser.c
+++ devel/anjuta/patches/patch-plugins_am-project_projectparser.c
@@ -1,22 +1,22 @@
-$NetBSD: patch-plugins_am-project_projectparser.c,v 1.1 2013/10/06 20:01:27 joerg Exp $
+$NetBSD: patch-plugins_am-project_projectparser.c,v 1.2 2013/12/07 23:47:02 dholland Exp $
 
 --- plugins/am-project/projectparser.c.orig	2013-08-09 00:30:17.000000000 +0000
 +++ plugins/am-project/projectparser.c
-@@ -58,7 +58,7 @@ close_output (void)
+@@ -57,7 +57,7 @@ close_output (void)
+ 	output_stream = NULL;
  }
  
- static void
--print (const gchar *message, ...)
-+print (const gchar *message, ...) G_GNUC_PRINTF(1,2)
+-static void
++static G_GNUC_PRINTF(1,2) void
+ print (const gchar *message, ...)
  {
  	va_list args;
- 
-@@ -71,7 +71,7 @@ print (const gchar *message, ...)
+@@ -70,7 +70,7 @@ print (const gchar *message, ...)
+ 	fputc('\n', output_stream);
  }
  
- static void
--print_error (const gchar *message, ...)
-+print_error (const gchar *message, ...) G_GNUC_PRINTF(1,2)
+-static void
++static G_GNUC_PRINTF(1,2) void
+ print_error (const gchar *message, ...)
  {
  	va_list args;
- 
