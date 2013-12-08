dabd78a9af944be8e66652187bd0911d45fdda6f
prlw1 <prlw1>
2013-12-08 13:28:27 +0000

    Update gobject-introspection to 1.38.0
    patch-ad removed as fixed in 82870258 (Bug 698438)
    
    g-ir-scanner has a grown --xpath-assertions, --format and --inject options
    Many bug fixes and translation updates.

--- devel/gobject-introspection/patches/patch-giscanner_dumper.py
+++ devel/gobject-introspection/patches/patch-giscanner_dumper.py
@@ -1,17 +1,18 @@
-$NetBSD: patch-giscanner_dumper.py,v 1.2 2013/05/15 19:17:59 adam Exp $
+$NetBSD: patch-giscanner_dumper.py,v 1.3 2013/12/08 13:28:27 prlw1 Exp $
 
 Don't treat 'clang' as Visual C++ compiler.
 
---- giscanner/dumper.py.orig	2013-05-15 15:30:07.000000000 +0000
+--- giscanner/dumper.py.orig	2013-09-24 13:41:09.000000000 +0000
 +++ giscanner/dumper.py
-@@ -86,11 +86,6 @@ class DumpCompiler(object):
+@@ -86,12 +86,6 @@ class DumpCompiler(object):
          self._linker_cmd = os.environ.get('CC', self._compiler_cmd)
          self._pkgconfig_cmd = os.environ.get('PKG_CONFIG', 'pkg-config')
          self._pkgconfig_msvc_flags = ''
 -        # Enable the --msvc-syntax pkg-config flag when
 -        # the Microsoft compiler is used
 -        # (This is the other way to check whether Visual C++ is used subsequently)
--        if 'cl' in self._compiler_cmd:
+-        args = self._compiler_cmd.split()
+-        if 'cl.exe' in args or 'cl' in args:
 -            self._pkgconfig_msvc_flags = '--msvc-syntax'
          self._uninst_srcdir = os.environ.get(
              'UNINSTALLED_INTROSPECTION_SRCDIR')
