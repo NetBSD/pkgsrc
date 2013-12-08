178b1f611fa7630d10a4b2cb7f95c55be6c016a5
jmmv <jmmv@pkgsrc.org>
2013-12-08 01:23:10 +0000

    Force dependency on >= 0.4.  (Only affects kyua-cli just-updated so no need for any revbumps.)

--- devel/lutok/buildlink3.mk
+++ devel/lutok/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/07/04 21:27:57 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/12/08 01:23:10 jmmv Exp $
 
 BUILDLINK_TREE+=	lutok
 
 .if !defined(LUTOK_BUILDLINK3_MK)
 LUTOK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lutok+=	lutok>=0.3
-BUILDLINK_ABI_DEPENDS.lutok?=	lutok>=0.3nb1
+BUILDLINK_API_DEPENDS.lutok+=	lutok>=0.4
 BUILDLINK_PKGSRCDIR.lutok?=	../../devel/lutok
 
 .include "../../lang/lua/buildlink3.mk"
