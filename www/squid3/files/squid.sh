43205e768d6afd0246df4a9d71ecd268dfa9cd2a
adam <adam@pkgsrc.org>
2013-12-10 14:48:26 +0000

    Changes 3.4.1: * Bug 3589: intercepted and ICAP modified request using a cache_peer * OpenBSD portability fix in DiskThreads * Bug 3935: Invalid pointer dereference when peeking at origin server certificate * Destroy ACLs in the reverse order of creation to avoid destruction segfaults * Portability: sleep() is sometimes a macro * Windows: fix compile errors in WinSvc.cc * Portability: std::string:npos is not always appropriate for String::npos * Portability: refresh_pattern requires regex * librfcnb: portability fixes

---
+++ www/squid3/files/squid.sh
@@ -0,0 +1,65 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: squid.sh,v 1.1 2013/12/10 14:48:26 adam Exp $
+#
+# PROVIDE: squid
+# REQUIRE: DAEMON
+# KEYWORD: shutdown
+
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
+fi
+
+: ${squid_conf:=@PKG_SYSCONFDIR@/squid.conf}
+
+name="squid"
+rcvar=$name
+command="@PREFIX@/sbin/${name}"
+pidfile="@VARBASE@/run/${name}.pid"
+required_files="${squid_conf} @PKG_SYSCONFDIR@/mime.conf"
+command_args="-Y -f ${squid_conf}"
+
+start_precmd='ulimit -n 4096'
+# Note: 'shutdown' waits 30 seconds, while 'interrupt' stops immediately
+stop_cmd="${command} ${squid_flags} ${command_args} -k interrupt"
+reload_cmd="${command} ${squid_flags} ${command_args} -k reconfigure"
+rotate_cmd="${command} ${squid_flags} ${command_args} -k rotate"
+createdirs_cmd="${command} ${squid_flags} ${command_args} -z"
+extra_commands="createdirs reload rotate"
+
+if [ -f /etc/rc.subr -a -f /etc/rc.conf -a -f /etc/rc.d/DAEMON ]; then
+	load_rc_config $name
+	run_rc_command "$1"
+else
+	if [ -f /etc/rc.conf ]; then
+		. /etc/rc.conf
+	fi
+	case $1 in
+	start)
+		start_cmd="${command} ${squid_flags} ${command_args}"
+		if [ -x ${command} -a -f ${squid_conf} ] ; then
+			${start_cmd}
+		fi
+		;;
+	stop)
+		${stop_cmd}
+		;;
+	createdirs)
+		${createdirs_cmd}
+		;;
+	reload)
+		if [ -r "${pidfile}" ] ; then
+			${reload_cmd}
+		fi
+		;;
+	rotate)
+		if [ -r "${pidfile}" ] ; then
+			${rotate_cmd}
+		fi
+		;;
+	*)
+		@ECHO@ "Usage: $0 {start|stop|reload|rotate|createdirs}" 1>&2
+		exit 64
+		;;
+	esac
+fi
