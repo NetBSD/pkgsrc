2a8f320eb0a26644755996d4d09e0b4bfb3e91d1
adam <adam@pkgsrc.org>
2013-12-10 14:48:26 +0000

    Changes 3.4.1: * Bug 3589: intercepted and ICAP modified request using a cache_peer * OpenBSD portability fix in DiskThreads * Bug 3935: Invalid pointer dereference when peeking at origin server certificate * Destroy ACLs in the reverse order of creation to avoid destruction segfaults * Portability: sleep() is sometimes a macro * Windows: fix compile errors in WinSvc.cc * Portability: std::string:npos is not always appropriate for String::npos * Portability: refresh_pattern requires regex * librfcnb: portability fixes

--- www/squid3/patches/patch-compat_os_hpux.h
+++ www/squid3/patches/patch-compat_os_hpux.h
@@ -1,4 +1,6 @@
-$NetBSD: patch-compat_os_hpux.h,v 1.1 2013/11/07 00:47:05 joerg Exp $
+$NetBSD: patch-compat_os_hpux.h,v 1.2 2013/12/10 14:48:26 adam Exp $
+
+Typo fix.
 
 --- compat/os/hpux.h.orig	2013-11-07 00:29:30.000000000 +0000
 +++ compat/os/hpux.h
