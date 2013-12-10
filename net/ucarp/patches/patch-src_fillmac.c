54d6b6dbc22d7a97e20f2325d3133dc65290297e
jperkin <jperkin@pkgsrc.org>
2013-12-10 17:56:23 +0000

    Add SIOCGLIFHWADDR support on newer SunOS.  Bump PKGREVISION.

--- net/ucarp/patches/patch-src_fillmac.c
+++ net/ucarp/patches/patch-src_fillmac.c
@@ -1,14 +1,30 @@
-$NetBSD: patch-src_fillmac.c,v 1.1 2012/06/14 17:16:23 fhajny Exp $
+$NetBSD: patch-src_fillmac.c,v 1.2 2013/12/10 17:56:23 jperkin Exp $
 
-SIOCGIFHWADDR defined on recent SunOS, doesn't have the usual structure.
+Add SIOCGLIFHWADDR support on newer SunOS.
+
 --- src/fillmac.c.orig	2007-07-04 13:34:59.000000000 +0000
 +++ src/fillmac.c
-@@ -43,7 +43,7 @@ int fill_mac_address(void)
+@@ -43,7 +43,22 @@ int fill_mac_address(void)
                  strerror(errno));
          return -1;
      }
 -#ifdef SIOCGIFHWADDR
-+#if defined(SIOCGIFHWADDR) && !defined(__sun)
++#if defined(SIOCGLIFHWADDR) && defined(__sun)
++    {
++	struct lifreq lifr;
++	struct sockaddr_dl *sdl;
++	strncpy(lifr.lifr_name, interface, sizeof lifr.lifr_name);
++	if (ioctl(s, SIOCGLIFHWADDR, &lifr) != 0) {
++		logfile(LOG_ERR,
++		     _("Unable to get hardware info about an interface: %s"),
++		    strerror(errno));
++		(void) close(s);
++		return -1;
++	}
++	sdl = (struct sockaddr_dl *)&lifr.lifr_addr;
++	memcpy(hwaddr, LLADDR(sdl), sdl->sdl_alen);
++    }
++#elif defined(SIOCGIFHWADDR)
      {
          struct ifreq ifr;
          
