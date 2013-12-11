9285c7855497f444b7e27fdc2972b0681369260a
markd <markd@pkgsrc.org>
2013-12-11 23:38:52 +0000

    Populate .buildlink tree with *.pc files so that pkgconfig tests work when using builtin.

--- x11/xcb-util/builtin.mk
+++ x11/xcb-util/builtin.mk
@@ -1,7 +1,15 @@
-# $NetBSD: builtin.mk,v 1.1 2010/09/11 20:24:00 wiz Exp $
+# $NetBSD: builtin.mk,v 1.2 2013/12/11 23:38:52 markd Exp $
 
 BUILTIN_PKG:=	xcb-util
 PKGCONFIG_FILE.xcb-util=	${X11BASE}/lib/pkgconfig/xcb-atom.pc
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
+
+.if !empty(USE_BUILTIN.xcb-util:M[yY][eE][sS])
+BUILDLINK_FILES.xcb-util+=  lib/pkgconfig/xcb-atom.pc
+BUILDLINK_FILES.xcb-util+=  lib/pkgconfig/xcb-aux.pc
+BUILDLINK_FILES.xcb-util+=  lib/pkgconfig/xcb-event.pc
+BUILDLINK_FILES.xcb-util+=  lib/pkgconfig/xcb-util.pc
+.endif
+
