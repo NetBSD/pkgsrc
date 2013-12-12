41a64b29be6762dce2a16962b6b735213d8dd625
obache <obache@pkgsrc.org>
2013-12-12 12:47:17 +0000

    Import ghc-7.6.3 as lang/ghc7. Based on wip/ghc, mostly worked by phonohawk, with patches from jperkin@. And some modifications and buit of Bootstrap binaries for 7.6.3 by me.
    GHC: The Glasgow Haskell Compiler.
    
    The Glasgow Haskell Compiler is a robust, fully-featured, optimising
    compiler for the functional programming language Haskell 98
    (http://www.haskell.org). GHC compiles Haskell to either native code
    or C. It implements numerous experimental language extensions to
    Haskell, including concurrency, a foreign language interface, several
    type-system extensions, exceptions, and so on. GHC comes with a
    generational garbage collector, a space and time profiler, and a
    comprehensive set of libraries.

---
+++ lang/ghc7/files/bootstrap.build.mk
@@ -0,0 +1,35 @@
+# -*- makefile-gmake -*-
+#
+# This is a build conf for bootstrapping compilers.
+#
+
+# We don't want any docs.
+HADDOCK_DOCS       = NO
+BUILD_DOCBOOK_HTML = NO
+BUILD_DOCBOOK_PS   = NO
+BUILD_DOCBOOK_PDF  = NO
+BUILD_MAN          = NO
+
+# Enabling evil-splitter reduces bindist size, which is good. However
+# it introduces a run-time dependency on perl, which is unacceptable.
+SplitObjs          = NO
+
+# We only want vanilla libraries and rts. No profiling, no shared
+# libraries, no debugging, no event logging.
+GhcLibWays         = v
+GhcRTSWays         =
+
+# I'm not sure disabling GHCi significantly reduces the bindist size,
+# but we don't need it anyway.
+GhcWithInterpreter = NO
+
+# We don't want in-place GMP to be statically linked as it increases
+# the bindist size. Dynamic linkage is even worse.
+INTEGER_LIBRARY    = integer-simple
+
+# We'd like to distinguish bootstrapping bindists from normal ones.
+BIN_DIST_NAME      = ghc-$(ProjectVersion)-boot
+
+libraries/terminfo_CONFIGURE_OPTS += \
+	  --configure-option=--with-curses-includes=@NCURSES_PREFIX@/include \
+	  --configure-option=--with-curses-libraries=@NCURSES_PREFIX@/lib
