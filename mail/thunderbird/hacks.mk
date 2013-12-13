5968c7b48a6fb16449f01a840d4cab0e8a826e2d
richard <richard>
2013-12-13 07:57:25 +0000

    SunOS workaround for strip issues with libxul

--- mail/thunderbird/hacks.mk
+++ mail/thunderbird/hacks.mk
@@ -1,6 +1,11 @@
-# $NetBSD: hacks.mk,v 1.4 2013/05/23 13:14:28 ryoon Exp $
+# $NetBSD: hacks.mk,v 1.5 2013/12/13 07:57:25 richard Exp $
 
 # workround for link of thunderbird-bin etc.
 LDFLAGS+=	-Wl,-R${PREFIX}/lib/thunderbird
 
+.if ${OPSYS} == "SunOS"
+# workaround for strip problems with libxul.so
+# https://www.illumos.org/issues/4383
+INSTALL_UNSTRIPPED=yes
+.endif
 .include "../../devel/xulrunner17/hacks.mk"
