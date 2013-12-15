2da2c12f84150f840202e8b890e63e5482a7d374
ryoon <ryoon@pkgsrc.org>
2013-12-15 14:07:13 +0000

    Import xulrunner24-24.2.0 as devel/xulrunner24.
    XULRunner is a runtime environment for applications using the
    XML User Interface Language, XUL. It is the successor of the "Gecko"
    runtime environment.
    
    This package tracks 24 extended support release.

---
+++ devel/xulrunner24/buildlink3.mk
@@ -0,0 +1,30 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/12/15 14:07:13 ryoon Exp $
+
+BUILDLINK_TREE+=	xulrunner24
+
+.if !defined(XULRUNNER24_BUILDLINK3_MK)
+XULRUNNER24_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.xulrunner24+=	xulrunner24>=24.0
+BUILDLINK_ABI_DEPENDS.xulrunner24+=	xulrunner24>=24.0
+BUILDLINK_PKGSRCDIR.xulrunner24?=		../../devel/xulrunner24
+
+BUILDLINK_INCDIRS.xulrunner24+=		lib/xulrunner24/include
+BUILDLINK_INCDIRS.xulrunner24+=		include/xulrunner24
+BUILDLINK_FILES.xulrunner24+=		lib/xulrunner24/include/*.h
+
+BUILDLINK_LIBDIRS.xulrunner24+=		lib/xulrunner24/lib
+BUILDLINK_FILES.xulrunner24+=		lib/xulrunner24/lib/*.so
+BUILDLINK_FILES.xulrunner24+=		lib/xulrunner24/pkgconfig/*pc
+BUILDLINK_FNAME_TRANSFORM.xulrunner24+=	-e "s|lib/xulrunner24/pkgconfig|lib/pkgconfig|"
+
+pkgbase := xulrunner24
+.include "../../mk/pkg-build-options.mk"
+.if !empty(PKG_BUILD_OPTIONS.xulrunner24:Mgnome)
+. include "../../devel/libgnomeui/buildlink3.mk"
+. include "../../sysutils/gnome-vfs/buildlink3.mk"
+.endif
+.include "../../devel/nspr/buildlink3.mk"
+.endif # XULRUNNER24_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-xulrunner24
