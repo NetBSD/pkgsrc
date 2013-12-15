b94d1efaf4af2b1585c82aaec922763e29e08169
joerg <joerg@pkgsrc.org>
2013-12-15 19:42:04 +0000

    Force explicit bool conversion for C++11 mode.

--- multimedia/gnash/patches/patch-libcore_swf_DefineButtonTag.h
+++ multimedia/gnash/patches/patch-libcore_swf_DefineButtonTag.h
@@ -1,7 +1,16 @@
-$NetBSD: patch-libcore_swf_DefineButtonTag.h,v 1.1 2013/02/26 11:04:25 joerg Exp $
+$NetBSD: patch-libcore_swf_DefineButtonTag.h,v 1.2 2013/12/15 19:42:04 joerg Exp $
 
---- libcore/swf/DefineButtonTag.h.orig	2013-02-25 19:42:00.000000000 +0000
+--- libcore/swf/DefineButtonTag.h.orig	2012-01-19 19:17:48.000000000 +0000
 +++ libcore/swf/DefineButtonTag.h
+@@ -98,7 +98,7 @@ public:
+     /// A ButtonRecord is invalid if it refers to a DisplayObject
+     /// which has not been defined.
+     bool valid() const {
+-        return (_definitionTag);
++        return bool(_definitionTag);
+     }
+ 
+ private:
 @@ -157,8 +157,6 @@ public:
          return (_conditions & KEYPRESS);
      }
