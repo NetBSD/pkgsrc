c044bbc445628c6f665fc3808df5d95ed9ffafc0
ryoon <ryoon@pkgsrc.org>
2013-12-15 09:42:43 +0000

    Update to 5.2.0
    Changelog:
    No changes from Qt 5.2.0rc1 is available.
    * Some header files has chenged its name.

--- x11/qt5-qtserialport/buildlink3.mk
+++ x11/qt5-qtserialport/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/12/13 14:38:36 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/12/15 09:42:44 ryoon Exp $
 
 BUILDLINK_TREE+=	qt5-qtserialport
 
 .if !defined(QT5_QTSERIALPORT_BUILDLINK3_MK)
 QT5_QTSERIALPORT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qt5-qtserialport+=	qt5-qtserialport>=5.2.0rc1
+BUILDLINK_API_DEPENDS.qt5-qtserialport+=	qt5-qtserialport>=5.2.0
 BUILDLINK_PKGSRCDIR.qt5-qtserialport?=	../../x11/qt5-qtserialport
 
 BUILDLINK_INCDIRS.qt5-qtserialport+=	qt5/include
