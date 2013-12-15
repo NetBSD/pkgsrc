57d82c4d9fdaee5553ec6698c6ab208fe77c3e43
ryoon <ryoon@pkgsrc.org>
2013-12-15 13:54:37 +0000

    Update to 26.0
    * Build outside WRKSRC, fix build
    
    Changelog:
    NEW
    All Java plug-ins are defaulted to 'click to play'
    NEW
    Password manager now supports script-generated password fields
    NEW
    Updates can now be performed by Windows users without write permissions to Firefox install directory (requires Mozilla Maintenance Service)
    NEW
    Support for H.264 on Linux if the appropriate gstreamer plug-ins are installed
    CHANGED
    Support for MP3 decoding on Windows XP, completing MP3 support across Windows OS versions
    CHANGED
    CSP implementation now supports multiple policies, including the case of both an enforced and Report-Only policy, per the spec
    DEVELOPER
    Social API now supports Social Bookmarking for multiple providers through its SocialMarks functionality (see MDN docs)
    DEVELOPER
    Math.ToFloat32 takes a JS value and converts it to a Float32, whenever possible
    DEVELOPER
    There is no longer a prompt when websites use appcache
    DEVELOPER
    Support for the CSS image orientation property
    DEVELOPER
    New App Manager allows you to deploy and debug HTML5 webapps on Firefox OS phones and the Firefox OS Simulator
    DEVELOPER
    IndexedDB can now be used as a "optimistic" storage area so it doesn't require any prompts and data is stored in a pool with LRU eviction policy, in short temporary storage
    FIXED
    When displaying a standalone image, Firefox matches the EXIF orientation information contained within the JPEG image (298619)
    FIXED
    Text Rendering Issues on Windows 7 with Platform Update KB2670838 (MSIE 10 Prerequisite) or on Windows 8.1 (812695)
    FIXED
    Improved page load times due to no longer decoding images that aren't visible (847223)
    FIXED
    AudioToolbox MP3 backend for OSX (914479)
    FIXED
    Various security fixes
    
    Fixed in Firefox 26
    MFSA 2013-117 Mis-issued ANSSI/DCSSI certificate
    MFSA 2013-116 JPEG information leak
    MFSA 2013-115 GetElementIC typed array stubs can be generated outside observed typesets
    MFSA 2013-114 Use-after-free in synthetic mouse movement
    MFSA 2013-113 Trust settings for built-in roots ignored during EV certificate validation
    MFSA 2013-112 Linux clipboard information disclosure though selection paste
    MFSA 2013-111 Segmentation violation when replacing ordered list elements
    MFSA 2013-110 Potential overflow in JavaScript binary search algorithms
    MFSA 2013-109 Use-after-free during Table Editing
    MFSA 2013-108 Use-after-free in event listeners
    MFSA 2013-107 Sandbox restrictions not applied to nested object elements
    MFSA 2013-106 Character encoding cross-origin XSS attack
    MFSA 2013-105 Application Installation doorhanger persists on navigation
    MFSA 2013-104 Miscellaneous memory safety hazards (rv:26.0 / rv:24.2)

--- www/firefox/patches/patch-js__src__vm__SPSProfiler.cpp
+++ www/firefox/patches/patch-js__src__vm__SPSProfiler.cpp
@@ -1,12 +1,20 @@
-$NetBSD: patch-js__src__vm__SPSProfiler.cpp,v 1.3 2013/11/02 22:57:55 ryoon Exp $
+$NetBSD: patch-js__src__vm__SPSProfiler.cpp,v 1.4 2013/12/15 13:54:37 ryoon Exp $
 
 Unclear fallout from our version of bug 840242, attachment v1: some architectures
 fail to link if we do not add the inlines here - might be compiler/arch specific.
 
 
---- js/src/vm/SPSProfiler.cpp.orig	2013-10-25 22:27:26.000000000 +0000
+--- js/src/vm/SPSProfiler.cpp.orig	2013-12-05 16:07:40.000000000 +0000
 +++ js/src/vm/SPSProfiler.cpp
-@@ -10,6 +10,7 @@
+@@ -4,12 +4,15 @@
+  * License, v. 2.0. If a copy of the MPL was not distributed with this
+  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ 
++#include "jscntxtinlines.h"
++
+ #include "vm/SPSProfiler.h"
+ 
+ #include "mozilla/DebugOnly.h"
  
  #include "jsnum.h"
  #include "jsscript.h"
