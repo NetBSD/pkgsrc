a711a389da19ae4d72c7182a432a02e76735e03c
asau <asau@pkgsrc.org>
2013-12-15 19:58:11 +0000

    FreeBSD 9 lacks the same set of functions as NetBSD 6.

--- math/R/patches/patch-src_nmath_pnbeta.c
+++ math/R/patches/patch-src_nmath_pnbeta.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_nmath_pnbeta.c,v 1.1 2013/08/13 19:53:30 markd Exp $
+$NetBSD: patch-src_nmath_pnbeta.c,v 1.2 2013/12/15 19:58:11 asau Exp $
 
-No logl() in NetBSD 6.x
+No logl() in NetBSD 6.x and FreeBSD 9.
 
 --- src/nmath/pnbeta.c.orig	2013-03-05 23:02:41.000000000 +0000
 +++ src/nmath/pnbeta.c
@@ -9,7 +9,7 @@ No logl() in NetBSD 6.x
      /* return R_DT_val(ans), but we want to warn about cancellation here */
      if (lower_tail)
 -#ifdef HAVE_LONG_DOUBLE
-+#if defined(HAVE_LONG_DOUBLE) && ! defined(__NetBSD__)
++#if defined(HAVE_LONG_DOUBLE) && ! defined(__NetBSD__) && ! defined(__FreeBSD__)
  	return (double) (log_p ? logl(ans) : ans);
  #else
  	return log_p ? log(ans) : ans;
