f23436a69e43796fce38fea977aae28657737fad
ryoon <ryoon>
2013-12-15 09:42:43 +0000

    Update to 5.2.0
    Changelog:
    No changes from Qt 5.2.0rc1 is available.
    * Some header files has chenged its name.

--- x11/qt5-qtscript/buildlink3.mk
+++ x11/qt5-qtscript/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/12/13 14:38:36 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/12/15 09:42:44 ryoon Exp $
 
 BUILDLINK_TREE+=	qt5-qtscript
 
 .if !defined(QT5_QTSCRIPT_BUILDLINK3_MK)
 QT5_QTSCRIPT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qt5-qtscript+=	qt5-qtscript>=5.1.1
+BUILDLINK_API_DEPENDS.qt5-qtscript+=	qt5-qtscript>=5.2.0
 BUILDLINK_PKGSRCDIR.qt5-qtscript?=	../../x11/qt5-qtscript
 
 BUILDLINK_INCDIRS.qt5-qtscript+=	qt5/include
--- x11/qt5-qtxmlpatterns/buildlink3.mk
+++ x11/qt5-qtxmlpatterns/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/12/13 14:38:36 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/12/15 09:42:45 ryoon Exp $
 
 BUILDLINK_TREE+=	qt5-qtxmlpatterns
 
 .if !defined(QT5_QTXMLPATTERNS_BUILDLINK3_MK)
 QT5_QTXMLPATTERNS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qt5-qtxmlpatterns+=	qt5-qtxmlpatterns>=5.2.0rc1
+BUILDLINK_API_DEPENDS.qt5-qtxmlpatterns+=	qt5-qtxmlpatterns>=5.2.0
 BUILDLINK_PKGSRCDIR.qt5-qtxmlpatterns?=	../../x11/qt5-qtxmlpatterns
 
 BUILDLINK_INCDIRS.qt5-qtxmlpatterns+=	qt5/include
