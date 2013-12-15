2780553e4f7c7f60dd5d88d0acb48333f2c5621f
ryoon <ryoon@pkgsrc.org>
2013-12-15 09:42:43 +0000

    Update to 5.2.0
    Changelog:
    No changes from Qt 5.2.0rc1 is available.
    * Some header files has chenged its name.

--- x11/qt5-dbus/buildlink3.mk
+++ x11/qt5-dbus/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/12/13 14:38:34 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/12/15 09:42:44 ryoon Exp $
 
 BUILDLINK_TREE+=	qt5-dbus
 
 .if !defined(QT5_DBUS_BUILDLINK3_MK)
 QT5_DBUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qt5-dbus+=	qt5-dbus>=5.2.0rc1
+BUILDLINK_API_DEPENDS.qt5-dbus+=	qt5-dbus>=5.2.0
 BUILDLINK_PKGSRCDIR.qt5-dbus?=	../../x11/qt5-dbus
 
 .include "../../sysutils/dbus/buildlink3.mk"
