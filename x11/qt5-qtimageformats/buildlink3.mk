338bf721294278ca52de7aab8c0c8456f69eb914
ryoon <ryoon@pkgsrc.org>
2013-12-15 09:42:43 +0000

    Update to 5.2.0
    Changelog:
    No changes from Qt 5.2.0rc1 is available.
    * Some header files has chenged its name.

--- x11/qt5-qtimageformats/buildlink3.mk
+++ x11/qt5-qtimageformats/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/12/13 14:38:35 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/12/15 09:42:44 ryoon Exp $
 
 BUILDLINK_TREE+=	qt5-qtimageformats
 
 .if !defined(QT5_QTIMAGEFORMATS_BUILDLINK3_MK)
 QT5_QTIMAGEFORMATS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qt5-qtimageformats+=	qt5-qtimageformats>=5.1.1
+BUILDLINK_API_DEPENDS.qt5-qtimageformats+=	qt5-qtimageformats>=5.2.0
 BUILDLINK_PKGSRCDIR.qt5-qtimageformats?=	../../x11/qt5-qtimageformats
 
 BUILDLINK_INCDIRS.qt5-qtimageformats+=	qt5/include
