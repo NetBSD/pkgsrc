5628bbf7202e5b226b4d6e890ad588839958b331
ryoon <ryoon>
2013-12-16 03:10:12 +0000

    Update to 17.0.11
    * pkgsrc xulrunner17/firefox17 use external NSS
    
    Changelog:
    FIXED
    Update ESR17 to NSS 3.14.5 RTM (see 936951)

--- devel/xulrunner17/dist.mk
+++ devel/xulrunner17/dist.mk
@@ -1,11 +1,11 @@
-# $NetBSD: dist.mk,v 1.10 2013/11/14 13:16:32 ryoon Exp $
+# $NetBSD: dist.mk,v 1.11 2013/12/16 03:10:12 ryoon Exp $
 #
 # used by devel/xulrunner17/Makefile
 # used by www/firefox17/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	17.0.10
+MOZ_BRANCH=	17.0.11
 MOZ_BRANCH_MINOR=	esr
 MASTER_SITES=	${MASTER_SITE_MOZILLA_ESR:=firefox/releases/${FIREFOX_VER}/source/} \
 		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
