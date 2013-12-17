702e48168583ceb0c14a4b12ab631b25b0050287
joerg <joerg@pkgsrc.org>
2013-12-17 23:22:35 +0000

    Explicitly include unistd.h for getconf defines.

--- x11/qt5-qtbase/patches/patch-qtwebkit_Source_WTF_wtf_NumberOfCores.cpp
+++ x11/qt5-qtbase/patches/patch-qtwebkit_Source_WTF_wtf_NumberOfCores.cpp
@@ -1,10 +1,18 @@
-$NetBSD: patch-qtwebkit_Source_WTF_wtf_NumberOfCores.cpp,v 1.1 2013/12/13 14:38:35 ryoon Exp $
+$NetBSD: patch-qtwebkit_Source_WTF_wtf_NumberOfCores.cpp,v 1.2 2013/12/17 23:22:35 joerg Exp $
 
 * Add NetBSD support
 
---- qtwebkit/Source/WTF/wtf/NumberOfCores.cpp.orig	2013-11-27 01:01:21.000000000 +0000
+--- qtwebkit/Source/WTF/wtf/NumberOfCores.cpp.orig	2013-12-08 17:09:59.000000000 +0000
 +++ qtwebkit/Source/WTF/wtf/NumberOfCores.cpp
-@@ -50,7 +50,7 @@ int numberOfProcessorCores()
+@@ -32,6 +32,7 @@
+ // data types defined in the former. See sysctl(3) and style(9).
+ #include <sys/types.h>
+ #include <sys/sysctl.h>
++#include <unistd.h>
+ #elif OS(LINUX) || OS(AIX) || OS(SOLARIS)
+ #include <unistd.h>
+ #elif OS(WINDOWS)
+@@ -50,7 +51,7 @@ int numberOfProcessorCores()
      if (s_numberOfCores > 0)
          return s_numberOfCores;
  
@@ -13,7 +21,7 @@ $NetBSD: patch-qtwebkit_Source_WTF_wtf_NumberOfCores.cpp,v 1.1 2013/12/13 14:38:
      unsigned result;
      size_t length = sizeof(result);
      int name[] = {
-@@ -60,7 +60,7 @@ int numberOfProcessorCores()
+@@ -60,7 +61,7 @@ int numberOfProcessorCores()
      int sysctlResult = sysctl(name, sizeof(name) / sizeof(int), &result, &length, 0, 0);
  
      s_numberOfCores = sysctlResult < 0 ? defaultIfUnavailable : result;
