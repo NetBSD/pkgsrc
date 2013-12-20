2b6f5f6d8dd2e282f9aca0f9245be409f1f577b1
agc <agc@pkgsrc.org>
2013-12-20 17:16:48 +0000

    Update netpgpverify and libnetpgpverify to version 20131219
    Fix a call to mp_radix_size to use a pointer to the BIGNUM, not the
    address of the pointer, when accessing.  Fixes a problem observed in
    other software when using the same code.  The problem was obscured
    from the compiler because of the use of __UNCONST().  This makes the
    BN_dec2bn() and BN_hex2bn() functions (in libnetpgpverify) work
    properly.
    
    OK: wiz

--- security/netpgpverify/files/verify.h
+++ security/netpgpverify/files/verify.h
@@ -23,9 +23,9 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #ifndef NETPGP_VERIFY_H_
-#define NETPGP_VERIFY_H_	20130426
+#define NETPGP_VERIFY_H_	20131219
 
-#define NETPGPVERIFY_VERSION	"netpgpverify portable 20130426"
+#define NETPGPVERIFY_VERSION	"netpgpverify portable 20131219"
 
 #include <sys/types.h>
 
