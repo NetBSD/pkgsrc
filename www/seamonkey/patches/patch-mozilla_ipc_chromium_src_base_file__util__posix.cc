7d3956ea94b7f4ed04a8b0f70c119d6d038db8b6
ryoon <ryoon@pkgsrc.org>
2013-12-26 13:17:37 +0000

    Update to 2.23
    Changelog:
    SeaMonkey-specific changes
    
        Download progress is now shown in the Mac OS X app dock icon.
        EXIF orientation is now being used when displaying attached images in MailNews.
        "This folder is being processed... to get messages." alerts on active MailNews folders now identify the account or folder.
        MailNews notifications have a new look.
        See the changes page for a more complete overview.
    
    Mozilla platform changes
    
        All plugins, with the exception of recent Flash plugins, now default to click-to-play.
        The password manager now supports script-generated password fields.
        Support for H.264 on Linux is now available if the appropriate GStreamer plugins are installed.
        Support for MP3 decoding on Windows XP has been added, completing MP3 support across Windows OS versions.
        The CSP implementation now supports multiple policies, including the case of both an enforced and Report-Only policy, per the spec.
        There is no longer a prompt when websites use appcache.
        Support for the CSS image orientation property has been added.
        IndexedDB can now be used as an "optimistic" storage area so it does not require any prompts and data is stored in a pool with LRU eviction policy, in short temporary storage.
        When displaying a standalone images, the EXIF orientation information contained within the JPEG image is now matched (bug 298619).
        Page load times have been improved due to no longer decoding images that are not visible (bug 847223).
        Support for the AudioToolbox MP3 backend has been added on Mac OS X (bug 914479).
        Fixed several stability issues.
    
    Fixed in SeaMonkey 2.23
    MFSA 2013-117 Mis-issued ANSSI/DCSSI certificate
    MFSA 2013-116 JPEG information leak
    MFSA 2013-115 GetElementIC typed array stubs can be generated outside observed typesets
    MFSA 2013-114 Use-after-free in synthetic mouse movement
    MFSA 2013-113 Trust settings for built-in roots ignored during EV certificate validation
    MFSA 2013-112 Linux clipboard information disclosure though selection paste
    MFSA 2013-111 Segmentation violation when replacing ordered list elements
    MFSA 2013-110 Potential overflow in JavaScript binary search algorithms
    MFSA 2013-109 Use-after-free during Table Editing
    MFSA 2013-108 Use-after-free in event listeners
    MFSA 2013-107 Sandbox restrictions not applied to nested object elements
    MFSA 2013-106 Character encoding cross-origin XSS attack
    MFSA 2013-104 Miscellaneous memory safety hazards (rv:26.0 / rv:24.2)

--- www/seamonkey/patches/patch-mozilla_ipc_chromium_src_base_file__util__posix.cc
+++ www/seamonkey/patches/patch-mozilla_ipc_chromium_src_base_file__util__posix.cc
@@ -1,6 +1,6 @@
-$NetBSD: patch-mozilla_ipc_chromium_src_base_file__util__posix.cc,v 1.1 2013/08/11 03:18:46 ryoon Exp $
+$NetBSD: patch-mozilla_ipc_chromium_src_base_file__util__posix.cc,v 1.2 2013/12/26 13:17:37 ryoon Exp $
 
---- mozilla/ipc/chromium/src/base/file_util_posix.cc.orig	2013-08-04 03:05:29.000000000 +0000
+--- mozilla/ipc/chromium/src/base/file_util_posix.cc.orig	2013-12-11 04:28:40.000000000 +0000
 +++ mozilla/ipc/chromium/src/base/file_util_posix.cc
 @@ -8,7 +8,7 @@
  #include <errno.h>
@@ -11,7 +11,7 @@ $NetBSD: patch-mozilla_ipc_chromium_src_base_file__util__posix.cc,v 1.1 2013/08/
  #include <fts.h>
  #endif
  #include <libgen.h>
-@@ -121,7 +121,7 @@ bool Delete(const FilePath& path, bool r
+@@ -67,7 +67,7 @@ bool Delete(const FilePath& path, bool r
    if (!recursive)
      return (rmdir(path_str) == 0);
  
@@ -20,7 +20,7 @@ $NetBSD: patch-mozilla_ipc_chromium_src_base_file__util__posix.cc,v 1.1 2013/08/
    // XXX Need ftsless impl for bionic
    return false;
  #else
-@@ -194,7 +194,7 @@ bool CopyDirectory(const FilePath& from_
+@@ -140,7 +140,7 @@ bool CopyDirectory(const FilePath& from_
      return false;
    }
  
@@ -29,30 +29,3 @@ $NetBSD: patch-mozilla_ipc_chromium_src_base_file__util__posix.cc,v 1.1 2013/08/
    // XXX Need ftsless impl for bionic
    return false;
  #else
-@@ -613,7 +613,7 @@ FileEnumerator::FileEnumerator(const Fil
- }
- 
- FileEnumerator::~FileEnumerator() {
--#ifndef ANDROID
-+#if !defined(ANDROID) && !defined(OS_SOLARIS)
-   if (fts_)
-     fts_close(fts_);
- #endif
-@@ -625,7 +625,7 @@ void FileEnumerator::GetFindInfo(FindInf
-   if (!is_in_find_op_)
-     return;
- 
--#ifndef ANDROID
-+#if !defined(ANDROID) && !defined(OS_SOLARIS)
-   memcpy(&(info->stat), fts_ent_->fts_statp, sizeof(info->stat));
-   info->filename.assign(fts_ent_->fts_name);
- #endif
-@@ -636,7 +636,7 @@ void FileEnumerator::GetFindInfo(FindInf
- // large directories with many files this can be quite deep.
- // TODO(erikkay) - get rid of this recursive pattern
- FilePath FileEnumerator::Next() {
--#ifdef ANDROID
-+#if defined(ANDROID) || defined(OS_SOLARIS)
-   return FilePath();
- #else
-   if (!is_in_find_op_) {
