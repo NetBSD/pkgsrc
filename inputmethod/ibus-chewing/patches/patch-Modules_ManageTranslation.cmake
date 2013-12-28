cf3e2f209f7e3065e9809fa00c6a3a684f64fe32
obache <obache@pkgsrc.org>
2013-12-28 04:59:36 +0000

    Update ibus-chewing to 1.4.4.
    PRJ_VER=1.4.4
    SUMMARY=Fixed RHBZ 842856, 1027030; Added translations:fr_FR, ja_JP, ko_KR
    REDHAT_BUGZILLA=842856, 1027030
    [Changes]
    - Resolves Bug 842856 - ibus-chewing 1.4.3-1 not built with $RPM_OPT_FLAGS
    - Resolves Bug 1027030 - CVE-2013-4509 ibus-chewing: ibus: visible
      password entry flaw [fedora-all]
      Thanks czchen for the GitHub pull request 39.
    - Added translations: fr_FR, ja_JP, ko_KR
    - Adopt cmake-fedora-1.2.0

--- inputmethod/ibus-chewing/patches/patch-Modules_ManageTranslation.cmake
+++ inputmethod/ibus-chewing/patches/patch-Modules_ManageTranslation.cmake
@@ -1,15 +1,15 @@
-$NetBSD: patch-Modules_ManageTranslation.cmake,v 1.2 2013/10/08 12:06:23 obache Exp $
+$NetBSD: patch-Modules_ManageTranslation.cmake,v 1.3 2013/12/28 04:59:36 obache Exp $
 
 * PKGLOCALEDIR support
 
---- Modules/ManageTranslation.cmake.orig	2012-11-26 07:37:40.000000000 +0000
+--- Modules/ManageTranslation.cmake.orig	2013-12-17 15:57:40.000000000 +0000
 +++ Modules/ManageTranslation.cmake
-@@ -198,7 +198,7 @@ IF(NOT DEFINED _MANAGE_TRANSLATION_CMAKE
+@@ -199,7 +199,7 @@ IF(NOT DEFINED _MANAGE_TRANSLATION_CMAKE
+ 		## No need to use MANAGE_FILE_INSTALL
+ 		## As this will handle by rpmbuild
+ 		INSTALL(FILES ${_gmoFile} DESTINATION 
+-		    ${DATA_DIR}/locale/${_locale}/LC_MESSAGES 
++		    ${CMAKE_INSTALL_LOCALEDIR}/${_locale}/LC_MESSAGES 
+ 		    RENAME ${_opt_POTFILE_NAME}.mo
  		    )
- 
- 		#MESSAGE("_absPoFile=${_absPoFile} _absPotDir=${_absPotDir} _lang=${_lang} curr_bin=${CMAKE_CURRENT_BINARY_DIR}")
--		INSTALL(FILES ${_absGmoFile} DESTINATION share/locale/${_locale}/LC_MESSAGES RENAME ${_potBasename}.mo)
-+		INSTALL(FILES ${_absGmoFile} DESTINATION ${CMAKE_INSTALL_LOCALEDIR}/${_locale}/LC_MESSAGES RENAME ${_potBasename}.mo)
- 		LIST(APPEND _absGmoFileList ${_absGmoFile})
- 		LIST(APPEND _absPoFileList ${_absPoFile})
- 	    ENDFOREACH(_locale ${_localeList})
+ 	    ENDFOREACH(_locale ${_opt_LOCALES})
