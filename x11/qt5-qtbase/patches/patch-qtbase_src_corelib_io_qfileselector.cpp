021fb7b429c03a41920b822cdaf7b42b49f3d790
jperkin <jperkin@pkgsrc.org>
2013-12-30 11:49:48 +0000

    SunOS needs sys/utsname.h too.

--- x11/qt5-qtbase/patches/patch-qtbase_src_corelib_io_qfileselector.cpp
+++ x11/qt5-qtbase/patches/patch-qtbase_src_corelib_io_qfileselector.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-qtbase_src_corelib_io_qfileselector.cpp,v 1.1 2013/12/13 14:38:35 ryoon Exp $
+$NetBSD: patch-qtbase_src_corelib_io_qfileselector.cpp,v 1.2 2013/12/30 11:49:48 jperkin Exp $
 
-* NetBSD needs sys/utsname.h for uname(3)
+* NetBSD and SunOS need sys/utsname.h for uname(3)
 
 --- qtbase/src/corelib/io/qfileselector.cpp.orig	2013-11-27 01:01:16.000000000 +0000
 +++ qtbase/src/corelib/io/qfileselector.cpp
@@ -8,7 +8,7 @@ $NetBSD: patch-qtbase_src_corelib_io_qfileselector.cpp,v 1.1 2013/12/13 14:38:35
  #include <QtCore/QLocale>
  #include <QtCore/QDebug>
  
-+#if defined(Q_OS_NETBSD)
++#if defined(Q_OS_NETBSD) || defined(Q_OS_SOLARIS)
 +#include <sys/utsname.h>
 +#endif
 +
