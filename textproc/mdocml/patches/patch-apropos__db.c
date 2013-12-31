ed6ea228587ea54ab503e76b202f46b4558c05db
wiz <wiz@pkgsrc.org>
2013-12-31 11:06:29 +0000

    Update to 1.12.3:
    Changes in version 1.12.3, released on December 31, 2013
    
     * In the mdoc(7) SYNOPSIS, line breaks and hanging indentation
       now work correctly for .Fo/.Fa/.Fc and .Fn blocks.
       Thanks to Franco Fichtner for doing part of the work.
     * The mdoc(7) .Bk macro got some addititonal bugfixes.
     * In mdoc(7) macro arguments, double quotes can now be quoted
       by doubling them, just like in man(7).
       Thanks to Tsugutomo ENAMI for the patch.
     * At the end of man(7) macro lines, end-of-sentence spacing
       now works.  Thanks to Franco Fichtner for the patch.
     * For backward compatibility, the man(7) parser now supports the
       man-ext .UR/.UE (uniform resource identifier) block macros.
     * The man(7) parser now handles closing blocks that are not open
       more gracefully.
     * The man(7) parser now ignores blank lines right after .SH and .SS.
     * In the man(7) formatter, reset indentation when leaving a block,
       not just when entering the next one.
     * The roff(7) .nr request now supports incrementing and decrementing
       number registers and stops parsing the number right before the
       first non-digit character.
     * The roff(7) parser now supports the alternative escape sequence
       syntax \C'uXXXX' for Unicode characters.
     * The roff(7) parser now parses and ignores the .fam (font family)
       and .hw (hyphenation points) requests and the \d and \u escape
       sequences.
     * The roff(7) manual got a new ESCAPE SEQUENCE REFERENCE.

--- textproc/mdocml/patches/patch-apropos__db.c
+++ textproc/mdocml/patches/patch-apropos__db.c
@@ -1,34 +1,9 @@
-$NetBSD: patch-apropos__db.c,v 1.3 2013/12/29 19:19:03 tron Exp $
+$NetBSD: patch-apropos__db.c,v 1.4 2013/12/31 11:06:30 wiz Exp $
 
 SunOS fix.
 
 --- apropos_db.c.orig	2013-10-05 15:09:07.000000000 +0100
 +++ apropos_db.c	2013-12-29 19:07:21.000000000 +0000
-@@ -30,14 +30,19 @@
- #include <string.h>
- #include <unistd.h>
- 
--#if defined(__linux__)
--# include <endian.h>
--# include <db_185.h>
--#elif defined(__APPLE__)
-+#if defined(__APPLE__)
- # include <libkern/OSByteOrder.h>
--# include <db.h>
-+#elif defined(__linux__)
-+# include <endian.h>
-+#elif defined(__sun)
-+# include <sys/byteorder.h>
- #else
- # include <sys/endian.h>
-+#endif
-+
-+#if defined(__linux__) || defined(__sun) 
-+# include <db_185.h>
-+#else
- # include <db.h>
- #endif
- 
 @@ -141,6 +146,14 @@
  			const struct expr *, size_t terms,
  			struct mchars *, int);
