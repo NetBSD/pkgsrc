9422dc62b521fd644f3c157a3e85bc15c642fda8
ryoon <ryoon>
2013-12-31 23:21:08 +0000

    Fix path for libzen

--- multimedia/libmediainfo/buildlink3.mk
+++ multimedia/libmediainfo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/12/31 23:19:46 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/12/31 23:21:08 ryoon Exp $
 
 BUILDLINK_TREE+=	libmediainfo
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.libmediainfo+=	libmediainfo>=0.7.65
 BUILDLINK_PKGSRCDIR.libmediainfo?=	../../multimedia/libmediainfo
 
 .include "../../devel/zlib/buildlink3.mk"
-.include "../../multimedia/libzen/buildlink3.mk"
+.include "../../devel/libzen/buildlink3.mk"
 .endif	# LIBMEDIAINFO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libmediainfo
