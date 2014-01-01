a32ffa21f6a3de0a2fcaa31ff2769344e622ba19
wiz <wiz>
2014-01-01 11:49:59 +0000

    Update to 1.6.0:
    Noteworthy changes in version 1.6.0 (2013-12-16)
    ------------------------------------------------
    
     * Removed the long deprecated gcry_ac interface.  Thus Libgcrypt is
       not anymore ABI compatible to previous versions if they used the ac
       interface.
    
     * Removed the module register subsystem.
    
     * The deprecated message digest debug macros have been removed.  Use
       gcry_md_debug instead.
    
     * Removed deprecated control codes.
    
     * Improved performance of most cipher algorithms as well as for the
       SHA family of hash functions.
    
     * Added support for the IDEA cipher algorithm.
    
     * Added support for the Salsa20 and reduced Salsa20/12 stream ciphers.
    
     * Added limited support for the GOST 28147-89 cipher algorithm.
    
     * Added support for the GOST R 34.11-94 and R 34.11-2012 (Stribog)
       hash algorithms.
    
     * Added a random number generator to directly use the system's RNG.
       Also added an interface to prefer the use of a specified RNG.
    
     * Added support for the SCRYPT algorithm.
    
     * Mitigated the Yarom/Falkner flush+reload side-channel attack on RSA
       secret keys.  See <http://eprint.iacr.org/2013/448> [CVE-2013-4242].
    
     * Added support for Deterministic DSA as per RFC-6969.
    
     * Added support for curve Ed25519.
    
     * Added a scatter gather hash convenience function.
    
     * Added several MPI amd SEXP helper functions.
    
     * Added support for negative numbers to gcry_mpi_print,
       gcry_mpi_aprint and gcry_mpi_scan.
    
     * The algorithm ids GCRY_PK_ECDSA and GCRY_PK_ECDH are now
       deprecated.  Use GCRY_PK_ECC if you need an algorithm id.
    
     * Changed gcry_pk_genkey for "ecc" to only include the curve name and
       not the parameters.  The flag "param" may be used to revert this.
    
     * Added a feature to globally disable selected hardware features.
    
     * Added debug helper functions.

--- security/libgcrypt/buildlink3.mk
+++ security/libgcrypt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2011/04/22 13:42:47 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2014/01/01 11:49:59 wiz Exp $
 
 BUILDLINK_TREE+=	libgcrypt
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libgcrypt
 LIBGCRYPT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libgcrypt+=	libgcrypt>=1.2.0
-BUILDLINK_ABI_DEPENDS.libgcrypt+=	libgcrypt>=1.4.6nb2
+BUILDLINK_ABI_DEPENDS.libgcrypt+=	libgcrypt>=1.6.0
 BUILDLINK_PKGSRCDIR.libgcrypt?=	../../security/libgcrypt
 
 .include "../../security/libgpg-error/buildlink3.mk"
