302f9b16009428a84c4b0f7cbcdf6b92eac6d2c7
schmonz <schmonz@pkgsrc.org>
2014-01-01 15:50:01 +0000

    Fix previous for OS X 10.9 with clang (and maybe other non-glibc systems).

--- converters/libiconv/patches/patch-srclib_stdio.in.h
+++ converters/libiconv/patches/patch-srclib_stdio.in.h
@@ -1,18 +1,19 @@
-$NetBSD: patch-srclib_stdio.in.h,v 1.1 2013/12/31 10:14:14 ryoon Exp $
+$NetBSD: patch-srclib_stdio.in.h,v 1.2 2014/01/01 15:50:01 schmonz Exp $
 
 * Fix build on glibc>=2.16
   From http://www.itkb.ro/kb/linux/patch-libiconv-pentru-glibc-216
 
 --- srclib/stdio.in.h.orig	2011-08-07 13:42:06.000000000 +0000
 +++ srclib/stdio.in.h
-@@ -695,8 +695,10 @@ _GL_CXXALIASWARN (gets);
+@@ -695,7 +695,11 @@ _GL_CXXALIASWARN (gets);
  /* It is very rare that the developer ever has full control of stdin,
     so any use of gets warrants an unconditional warning.  Assume it is
     always declared, since it is required by C89.  */
-+#if defined(__GLIBC__) && !defined(__UCLIBC__) && !__GLIBC_PREREQ(2, 16)
++#if defined(__GLIBC__) && !defined(__UCLIBC__)
++# if !__GLIBC_PREREQ(2, 16)
  _GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
- #endif
++# endif
 +#endif
+ #endif
  
  
- #if @GNULIB_OBSTACK_PRINTF@ || @GNULIB_OBSTACK_PRINTF_POSIX@
