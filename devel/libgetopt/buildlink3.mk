e23c4fd2bd9143e288023f6ff19faa4271354636
ryoon <ryoon@pkgsrc.org>
2014-01-01 02:48:31 +0000

    Update to 1.4.5
    * Fix link error on Linux (Fedora 22) against libgetopt from devel/libgetopt
      For example, fix audio/speex build in PREFER_PKGSRC=yes case

--- devel/libgetopt/buildlink3.mk
+++ devel/libgetopt/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/03/20 19:24:18 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2014/01/01 02:48:31 ryoon Exp $
 
 BUILDLINK_TREE+=	getopt
 
 .if !defined(LIBGETOPT_BUILDLINK3_MK)
 LIBGETOPT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.getopt+=	libgetopt>=1.3
+BUILDLINK_API_DEPENDS.getopt+=	libgetopt>=1.4.5
 BUILDLINK_PKGSRCDIR.getopt?=	../../devel/libgetopt
 .endif # LIBGETOPT_BUILDLINK3_MK
 
