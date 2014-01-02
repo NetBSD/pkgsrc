360f4ef22e9cb6e2cdabc4c7bbbc79358f0e5d9f
wiz <wiz@pkgsrc.org>
2014-01-02 19:57:39 +0000

    Update to 5.2.3:
    Lua 5.2.3 fixes all bugs listed in http://www.lua.org/bugs.html#5.2.2 .
    
    Like all minor releases, this is strictly a bug-fix release;
    no new features or improvements have been added.

--- lang/lua52/version.mk
+++ lang/lua52/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.1 2013/10/30 06:18:10 dholland Exp $
+# $NetBSD: version.mk,v 1.2 2014/01/02 19:57:39 wiz Exp $
 
-LUA_VERSION=	5.2.2
+LUA_VERSION=	5.2.3
 
 LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
 LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
