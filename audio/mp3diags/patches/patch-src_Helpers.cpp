43fbc5c06e7eb61fa4dcf15fb5fc35a16f41ad8d
wiz <wiz@pkgsrc.org>
2014-01-02 22:18:37 +0000

    Remove unnecessary chunk (after communication with upstream). This is C++, so "struct" is not necessary.

--- audio/mp3diags/patches/patch-src_Helpers.cpp
+++ audio/mp3diags/patches/patch-src_Helpers.cpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_Helpers.cpp,v 1.2 2012/12/15 11:10:38 marino Exp $
+$NetBSD: patch-src_Helpers.cpp,v 1.3 2014/01/02 22:18:37 wiz Exp $
 
 --- src/Helpers.cpp.orig	2011-09-10 17:51:46.000000000 +0000
 +++ src/Helpers.cpp
@@ -10,12 +10,3 @@ $NetBSD: patch-src_Helpers.cpp,v 1.2 2012/12/15 11:10:38 marino Exp $
  #include  <iostream>
  #include  "fstream_unicode.h"
  #include  <sstream>
-@@ -914,7 +915,7 @@ QString getSystemInfo() //ttt2 perhaps s
-     filters << "*-release" << "*_version";
-     dir.setNameFilters(filters);
-     QStringList lFiles (dir.entryList(QDir::Files));
--    utsname utsInfo;
-+    struct utsname utsInfo;
-     uname(&utsInfo);
-     s += utsInfo.sysname; s += " ";
-     s += utsInfo.release; s += " ";
