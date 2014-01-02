e2861c2ed952b76ce9fc8353e667954743a898c4
ryoon <ryoon@pkgsrc.org>
2014-01-02 15:18:01 +0000

    Update to 1.3.0
    * tarball is not distributed by Google, use Debian's one
    
    Changelog:
    2013-11-15 v1.3.0 "Forest"
      This release introduces the VP9 codec in a backward-compatible way.
      All existing users of VP8 can continue to use the library without
      modification. However, some VP8 options do not map to VP9 in the same manner.
    
      The VP9 encoder in this release is not feature complete. Users interested in
      the encoder are advised to use the git master branch and discuss issues on
      libvpx mailing lists.
    
      - Upgrading:
        This release is ABI and API compatible with Duclair (v1.0.0). Users
        of older releases should refer to the Upgrading notes in this document
        for that release.
    
      - Enhancements:
          Get rid of bashisms in the main build scripts
          Added usage info on command line options
          Add lossless compression mode
          Dll build of libvpx
          Add additional Mac OS X targets: 10.7, 10.8 and 10.9 (darwin11-13)
          Add option to disable documentation
          configure: add --enable-external-build support
          make: support V=1 as short form of verbose=yes
          configure: support mingw-w64
          configure: support hardfloat armv7 CHOSTS
          configure: add support for android x86
          Add estimated completion time to vpxenc
          Don't exit on decode errors in vpxenc
          vpxenc: support scaling prior to encoding
          vpxdec: support scaling output
          vpxenc: improve progress indicators with --skip
          msvs: Don't link to winmm.lib
          Add a new script for producing vcxproj files
          Produce Visual Studio 10 and 11 project files
          Produce Windows Phone project files
          msvs-build: use msbuild for vs >= 2005
          configure: default configure log to config.log
          Add encoding option --static-thresh
    
      - Speed:
          Miscellaneous speed optimizations for VP8 and VP9.
    
      - Quality:
          In general, quality is consistent with the Eider release.
    
      - Bug Fixes:
          This release represents approximately a year of engineering effort,
          and contains multiple bug fixes. Please refer to git history for details.

--- multimedia/libvpx/patches/patch-test_sad__test.cc
+++ multimedia/libvpx/patches/patch-test_sad__test.cc
@@ -1,8 +1,8 @@
-$NetBSD: patch-test_sad__test.cc,v 1.1 2013/09/13 15:00:43 joerg Exp $
+$NetBSD: patch-test_sad__test.cc,v 1.2 2014/01/02 15:18:01 ryoon Exp $
 
---- test/sad_test.cc.orig	2013-09-12 10:19:23.000000000 +0000
+--- test/sad_test.cc.orig	2013-11-18 22:18:52.000000000 +0000
 +++ test/sad_test.cc
-@@ -182,7 +182,11 @@ TEST_P(SADTest, MaxSAD) {
+@@ -296,7 +296,11 @@ TEST_P(SADTest, MaxSAD) {
    CheckSad(128);
  }
  
@@ -12,5 +12,5 @@ $NetBSD: patch-test_sad__test.cc,v 1.1 2013/09/13 15:00:43 joerg Exp $
  using std::tr1::make_tuple;
 +#endif
  
+ #if CONFIG_VP8_ENCODER
  const sad_m_by_n_fn_t sad_16x16_c = vp8_sad16x16_c;
- const sad_m_by_n_fn_t sad_8x16_c = vp8_sad8x16_c;
