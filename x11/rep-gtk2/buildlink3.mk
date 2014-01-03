3fc4fe4e499d57fbf84ac9a04e39722af4b695cc
ryoon <ryoon@pkgsrc.org>
2014-01-03 05:03:51 +0000

    Update to 0.90.8.1
    Changelog:
    0.90.8.1:
    
    * fixed bug that prevented uninstallation of rep-gtk
    
    0.90.8:
    
    * fixed building on ARM
    * synced spec file [Kim B. Heino]
    
    0.90.7:
    
    * re-arranged tree (all new widget-definitons in widgets/<widget>)
    * minimum GTK+ bumped to 2.24
    * minimum GDK bumped to 2.23
    * New API wrapped:
    
    GtkAccelGroup
      - gtk_accel_group_get_is_locked
      - gtk_accel_group_get_modifier_mask
      - GtkAccelKey
      - GtkAccelGroupEntry
    
    GtkAccelMap:
      - gtk_accel_map_add_entry
      - gtk_accel_map_lookup_entry
      - gtk_accel_map_change_entry
      - gtk_accel_map_load
      - gtk_accel_map_save
      - gtk_accel_map_foreach
      - gtk_accel_map_load_fd
      - gtk_accel_map_save_fd
      - gtk_accel_map_lock_path
      - gtk_accel_map_unlock_path
      - gtk_accel_map_add_filter
      - gtk_accel_map_foreach_unfiltered
    
    GtkExpander:
      - gtk_expander_set_label_fill
      - gtk_expander_get_label_fill
    
    GtkComboBox:
      - gtk_combo_box_new_with_entry
      - gtk_combo_box_new_with_model_and_entry
      - gtk_combo_box_popup
      - gtk_combo_box_popdown
      - gtk_combo_box_set_button_sensitivity
      - gtk_combo_box_get_button_sensitivity
      - gtk_combo_box_get_has_entry
      - gtk_combo_box_get_entry_text_column
      - gtk_combo_box_set_entry_text_column
      - gtk_combo_box_set_row_separator_func
      - gtk_combo_box_get_row_separator_func
    
    GtkComboBoxText:
      - added completely
    
    GtkScale:
      - gtk_scale_add_mark
      - gtk_scale_clear_marks
    
    GtkRC:
      - gtk_rc_parse_string
    
    GdkPixbuf:
      - gdk_pixbuf_new_from_file_at_size
      - gdk_pixbuf_new_from_file_at_scale
    
    GtkImage:
      - gtk_image_clear
    
    * Fixed wrapped API:
    
    GtkFileChooser:
      - gtk_file_chooser_set_filename
      - gtk_file_chooser_select_filename
    
    * Deprecated API no longer wrapped:
    
    GtkComboBox
      - gtk_combo_box_get_active_text
      - gtk_combo_box_remove_text
      - gtk_combo_box_append_text
      - gtk_combo_box_insert_text
      - gtk_combo_box_prepend_text
    
    GtkComboBoxEntry:
      - removed completely
    
    GtkButton
      - gtk_button_pressed
      - gtk_button_released
      - gtk_button_enter
      - gtk_button_leave
      - gtk_file_chooser_button_new_with_backend
    
    
    0.90.6:
    
    * bump minimum librep to 0.92.0
    * updated for changes in installation-paths in librep 0.92.0
    * merged Debian scripts from Ian Zimmermann
    * improved Makefile clean rules
    * combined gtk and gtk-types modules [Kim B. Heino]
    
    0,90,5:
    
    * fixed a bug in configure, which could create an incomplete rep-gtk.pc
    * updated the debian packaging scripts (mostly) to the new spec

--- x11/rep-gtk2/buildlink3.mk
+++ x11/rep-gtk2/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/01/23 23:56:53 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/01/03 05:03:51 ryoon Exp $
 
 BUILDLINK_TREE+=	rep-gtk2
 
 .if !defined(REP_GTK2_BUILDLINK3_MK)
 REP_GTK2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rep-gtk2+=	rep-gtk2>=0.90.4
+BUILDLINK_API_DEPENDS.rep-gtk2+=	rep-gtk2>=0.90.8.1
 BUILDLINK_PKGSRCDIR.rep-gtk2?=	../../x11/rep-gtk2
 
 .endif	# REP_GTK2_BUILDLINK3_MK
