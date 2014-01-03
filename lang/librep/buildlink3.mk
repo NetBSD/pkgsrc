27b4478d073fb189ead960842aa9a435cce6ace4
ryoon <ryoon@pkgsrc.org>
2014-01-03 05:01:36 +0000

    Update to 0.92.3
    Changelog:
    0.92.3
    ======
    
       * More entities support for xml-reader (gt, umlauts, esszett)
    
       * Fix address of FSF in all files [Togan Muftouglu]
    
       * Assume stack-direciton `downwards' for all ppc and s390 variants
         [Kimb B. Heino]
    
       * Updated `install-sh' to version 2011-01-19.21.
    
       * Added `program-exists-p' [Mark Triggs]
    
    0.92.2.1
    ========
    
       * Assume stack-direction `downwards' on ARM [Kim B. Heino]
    
    
    0.92.2
    ======
    
       * Sync librep.spec with Fedora [Kim B. Heino]
    
       * Assume stack-direction `downwards' for x86_64.  Self-detection
         doesn't work with gcc >= 4.7.0 [Kim B. Heino]
    
       * read_line no longer aborts at 400 characters [Timo Korvola]
    
    0.92.1b
    =======
    
       * Fixed a bug in librep.pc
    
    0.92.1
    ======
    
       * Fixed building REP on OS-X 10.6 by not exporting librepm.sym via
         libtool[John Harper]
    
       * Fixed building REP on OS-X 10.7 by defining a proper ALIGN if
         ffi.h doesn't provide one [John Harper]
    
       * Fixed detection of FFI, if ffi.h isn't found [John Harper]
    
       * Added various missing symbols in librep.sym [John Harper]
    
       * Fixed a warning from configure regarding librep.pc [Christopher
         Bratusek]
    
       * Removed VPATH from Makefiles, to allow building REP using `makepp'
         [Daniel Pfeiffer]
    
       * Added `positon' meta-function [Jeremy Hankins] [Eli Barzilay]
    
       * Imported utility-functions from Sawfish: `beautify-symbol',
         `remove-newlines', `option-index' and `string->symbol'.
    
    0.92.0
    ======
    
       * Bumped soname to 16.0.0 [Ian Zimmermann]
    
       * Make librep loading shared-objects, rather than libtool-archives
         [Kim B. Heino]
    
       * Removed architecture and version from installation paths
         [Christopher Bratusek]
    
       * Improved debian packaging-scripts [Ian Zimmermann, Christopher
         Bratusek]
    
    0.91.1
    ======
    
       * `rep.ffi.util' module [Sergey Bolshakov]
    
         A new module containing utils for rep's ffi binding is added.
    
       * Fixed librep.pc to prevent a possible build issue in rep-gtk or
         sawfish (occured on fedora 14) [Christopher Bratusek]
    
       * New functions [Teika Kazura]
    
         `function-name' returns the name of the function object. (*note
         Functions::)
    
         `remove-hook-by-name' removes functions from a hook by their name.
         (*note Normal Hooks::)
    
       * Documentation [Teika kazura]
    
         Function `in-hook-p' now has the documentation. (*note Normal
         Hooks::)
    
         New sections "Module Aliases" (*note Module Aliases::), "Module
         Limits" (*note Module Limits::).
    
       * Minor bugfix: `remove-hook' used to emit an error if the hook was
         unbound, but it's fixed. [Teika kazura]
    
       * fixed the spec-file [Kim B. Heino]
    
       * updated debian packaging-scripts (mostly) to the new format
         [Christopher Bratusek]

--- lang/librep/buildlink3.mk
+++ lang/librep/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2012/09/15 10:05:19 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2014/01/03 05:01:36 ryoon Exp $
 
 BUILDLINK_TREE+=	librep
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	librep
 LIBREP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.librep+=	librep>=0.91.0
-BUILDLINK_ABI_DEPENDS.librep+=	librep>=0.91.0nb4
+BUILDLINK_ABI_DEPENDS.librep+=	librep>=0.92.3
 BUILDLINK_PKGSRCDIR.librep?=	../../lang/librep
 MAKE_FLAGS+=			rep_LIBTOOL=${LIBTOOL:Q}
 
