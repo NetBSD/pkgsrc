19aad68e26cf83c99f69dfec21c1e7b74c211610
obache <obache@pkgsrc.org>
2014-01-03 12:36:30 +0000

    recursive bump from cogl shlib major bump.

--- graphics/clutter-gtk/buildlink3.mk
+++ graphics/clutter-gtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2013/10/10 14:41:51 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2014/01/03 12:36:30 obache Exp $
 #
 
 BUILDLINK_TREE+=	clutter-gtk
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	clutter-gtk
 CLUTTER_GTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.clutter-gtk+=	clutter-gtk>=0.10.0
-BUILDLINK_ABI_DEPENDS.clutter-gtk+=	clutter-gtk>=0.10.8nb16
+BUILDLINK_ABI_DEPENDS.clutter-gtk+=	clutter-gtk>=0.10.8nb17
 BUILDLINK_PKGSRCDIR.clutter-gtk?=	../../graphics/clutter-gtk
 
 .include "../../x11/gtk2/buildlink3.mk"
