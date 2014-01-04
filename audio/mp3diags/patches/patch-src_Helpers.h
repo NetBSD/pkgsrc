0ec079e0f0c9a1b4ef920e8ad67091c035afe673
wiz <wiz>
2014-01-04 22:57:19 +0000

    Remove an unneeded patch, and change a patch so it matches what was committed upstream.

--- audio/mp3diags/patches/patch-src_Helpers.h
+++ audio/mp3diags/patches/patch-src_Helpers.h
@@ -1,16 +1,16 @@
-$NetBSD: patch-src_Helpers.h,v 1.2 2014/01/04 15:11:43 wiz Exp $
+$NetBSD: patch-src_Helpers.h,v 1.3 2014/01/04 22:57:19 wiz Exp $
 
 Add missing headers for streamsize type.
 http://sourceforge.net/p/mp3diags/code/279/
 
 --- src/Helpers.h.orig	2010-03-19 17:35:22.000000000 +0000
 +++ src/Helpers.h
-@@ -24,7 +24,9 @@
+@@ -24,7 +24,8 @@
  #define HelpersH
  
  
+-#include  <iosfwd>
 +#include  <ios>
- #include  <iosfwd>
 +#include  <iostream>
  #include  <string>
  #include  <stdexcept>
