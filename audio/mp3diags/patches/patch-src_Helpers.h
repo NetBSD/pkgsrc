fbbcfa166192e14cd4c514a44e5d2f107ecdc921
wiz <wiz@pkgsrc.org>
2014-01-04 15:11:43 +0000

    Add upstream SVN commit revision.

--- audio/mp3diags/patches/patch-src_Helpers.h
+++ audio/mp3diags/patches/patch-src_Helpers.h
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_Helpers.h,v 1.1 2014/01/02 15:52:56 wiz Exp $
+$NetBSD: patch-src_Helpers.h,v 1.2 2014/01/04 15:11:43 wiz Exp $
 
 Add missing headers for streamsize type.
+http://sourceforge.net/p/mp3diags/code/279/
 
 --- src/Helpers.h.orig	2010-03-19 17:35:22.000000000 +0000
 +++ src/Helpers.h
--- audio/mp3diags/patches/patch-src_Notes.h
+++ audio/mp3diags/patches/patch-src_Notes.h
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_Notes.h,v 1.1 2012/01/11 19:25:08 hans Exp $
+$NetBSD: patch-src_Notes.h,v 1.2 2014/01/04 15:11:43 wiz Exp $
+
+http://sourceforge.net/p/mp3diags/code/279/
 
 --- src/Notes.h.orig	2010-02-13 07:58:04.000000000 +0100
 +++ src/Notes.h	2012-01-02 11:26:42.140106730 +0100
