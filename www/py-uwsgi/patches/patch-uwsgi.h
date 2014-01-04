16a414f0553f815e2c5ddd782d17c19f56f17c67
rodent <rodent@pkgsrc.org>
2014-01-04 13:56:01 +0000

    Update to 1.4.10, the oldest version supported by upstream. See py-uwsgi in -wip for the newest supported version. Partial ChangeLog:
     * 1.4.1
    
    - fixed typos in corerouter plugins
    - fixed offloading when the number of threads is higher than 1
    - fixed static_maps for non-existent paths
    - fixed uwsgi_connect() on modern Linux systems to reset the socket to blocking mode
    
     * 1.4
    
    - gevent improvements
    - improved http/https router and fastrouter
    - Go official support
    - a new set of infos are exported to the stats system
    - improved systemd support
    - log filtering and routing
    - improved tracebacker
    - offload transfer for static files, and network transfers
    - matheval support
    - plugins can be written in Obj-C
    - smart attach daemon
    - added support for PEP 405 virtualenvs
    - rawrouter with xclient support
    - internal routing plugin for cache
    
     * 1.3
    
    - python tracebacker
    - user-governed harakiri
    - simplified external plugin development
    - Linux namespace mountpoint improvements
    - secured subscription system
    - merged routers codebase (fastrouter, http, rawrouter)
    - https support in the http router
    - config report at the end of uWSGI build process
    - improved subscription system (multicast and unix socket)
    - custom options
    - graceful reloads on shared sockets
    - configurable log-master buffer size
    - extreme-optimizations for the stats subsystem
    - redislog and mongodblog plugins
    - added python logger
    - mongodb and postgres imperial monitors
    - implemented psgix.logger and psgix.cleanup
    - full rack spec compliance
    - preliminary ipv6 support
    - gevent graceful reloads
    - support for multiple loggers and logformat
    - lazy-apps to load apps after fork() but without changing reloading subsystem
    - emperor heartbeat subsystem
    - cheaper busyness plugin
    - pluggable clock sources
    - added router_rewrite and router_http plugins
    - external spoolers
    - support for section:// and fd:// loaders
    - alarm subsystem (with curl and xmpp plugins)

--- www/py-uwsgi/patches/patch-uwsgi.h
+++ www/py-uwsgi/patches/patch-uwsgi.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-uwsgi.h,v 1.3 2012/05/15 12:56:38 marino Exp $
+$NetBSD: patch-uwsgi.h,v 1.4 2014/01/04 13:56:01 rodent Exp $
 
---- uwsgi.h.orig	2011-09-11 05:50:42.000000000 +0000
+--- uwsgi.h.orig	2013-08-23 12:51:18.000000000 +0000
 +++ uwsgi.h
-@@ -210,6 +210,10 @@ extern int pivot_root(const char *new_ro
+@@ -278,6 +278,10 @@ extern int pivot_root(const char *new_ro
  #include <sys/devpoll.h>
  #elif defined(__HAIKU__)
  #else
--- www/py-uwsgi/patches/patch-uwsgiconfig.py
+++ www/py-uwsgi/patches/patch-uwsgiconfig.py
@@ -1,6 +1,8 @@
---- uwsgiconfig.py.orig	2012-05-14 06:58:20.000000000 +0100
-+++ uwsgiconfig.py	2012-07-06 19:25:44.000000000 +0100
-@@ -443,6 +443,8 @@
+--- uwsgiconfig.py.orig	2013-08-23 12:51:18.000000000 +0000
++++ uwsgiconfig.py
+--- uwsgiconfig.py.orig	2013-08-23 12:51:18.000000000 +0000
++++ uwsgiconfig.py
+@@ -635,6 +635,8 @@ class uConf(object):
          if locking_mode == 'auto':
              if uwsgi_os == 'Linux' or uwsgi_os == 'SunOS':
                  locking_mode = 'pthread_mutex'
@@ -9,7 +11,7 @@
              # FreeBSD umtx is still not ready for process shared locking
              # starting from FreeBSD 9 posix semaphores can be shared between processes
              elif uwsgi_os == 'FreeBSD':
-@@ -475,7 +477,7 @@
+@@ -672,7 +674,7 @@ class uConf(object):
                  if int(sun_major) >= 5:
                      if int(sun_minor) >= 10:
                          event_mode = 'port'
@@ -18,7 +20,7 @@
                  event_mode = 'kqueue'
  
          if event_mode == 'epoll':
-@@ -579,7 +581,7 @@
+@@ -791,7 +793,7 @@ class uConf(object):
          has_pcre = False
  
          # re-enable after pcre fix
@@ -27,7 +29,7 @@
              if self.get('pcre') == 'auto':
                  pcreconf = spcall('pcre-config --libs')
                  if pcreconf:
-@@ -620,7 +622,7 @@
+@@ -854,7 +856,7 @@ class uConf(object):
          has_json = False
          has_uuid = False
  
@@ -36,45 +38,45 @@
              has_uuid = True
              self.cflags.append("-DUWSGI_UUID")
              if uwsgi_os == 'Linux' or os.path.exists('/usr/lib/libuuid.so') or os.path.exists('/usr/local/lib/libuuid.so') or os.path.exists('/usr/lib64/libuuid.so') or os.path.exists('/usr/local/lib64/libuuid.so'):
-@@ -718,7 +720,7 @@
-             self.cflags.append("-DUWSGI_INI")
-             self.gcc_list.append('ini')
+@@ -956,7 +958,7 @@ class uConf(object):
+             self.gcc_list.append('core/ini')
+             report['ini'] = True
  
 -        if self.get('yaml'):
 +        if self.get('yamlOFF'):
              self.cflags.append("-DUWSGI_YAML")
-             self.gcc_list.append('yaml')
-             if self.get('yaml_implementation') == 'libyaml':
-@@ -729,7 +731,7 @@
-                     self.cflags.append("-DUWSGI_LIBYAML")
+             self.gcc_list.append('core/yaml')
+             report['yaml'] = True
+@@ -970,7 +972,7 @@ class uConf(object):
                      self.libs.append('-lyaml')
+                     report['yaml'] = 'libyaml'
  
 -        if self.get('json'):
 +        if self.get('jsonOFF'):
              if self.get('json') == 'auto':
                  jsonconf = spcall("pkg-config --cflags jansson")
                  if jsonconf:
-@@ -749,7 +751,7 @@
-                 self.libs.append('-ljansson')
-                 has_json = True
+@@ -993,7 +995,7 @@ class uConf(object):
+         if has_json:
+             report['json'] = True
  
 -        if self.get('ldap'):
 +        if self.get('ldapOFF'):
              if self.get('ldap') == 'auto':
                  if self.has_include('ldap.h'):
                      self.cflags.append("-DUWSGI_LDAP")
-@@ -772,7 +774,7 @@
-                 self.cflags.append("-DUWSGI_SCTP")
-                 self.libs.append('-lsctp')
+@@ -1020,7 +1022,7 @@ class uConf(object):
+                 report['ssl'] = True
+ 
  
 -        if has_uuid and self.get('zeromq'):
-+        if has_uuid and self.get('zeromqiOFF'):
++        if has_uuid and self.get('zeromqOFF'):
              if self.get('zeromq') == 'auto':
                  if self.has_include('zmq.h'):
                      self.cflags.append("-DUWSGI_ZEROMQ")
-@@ -832,7 +834,7 @@
-                 self.libs.append('-lexpat')
-                 self.gcc_list.append('xmlconf')
+@@ -1080,7 +1082,7 @@ class uConf(object):
+                 self.gcc_list.append('core/xmlconf')
+                 report['xml'] = 'expat'
  
 -        if self.get('sqlite3'):
 +        if self.get('sqlite3OFF'):
