c82a5190894e0879b4eb5a07066e3a28ac68259a
obache <obache@pkgsrc.org>
2014-01-04 13:46:55 +0000

    Switch to use clutter-gtk0.10.
    Bump PKGREVISION.

--- geography/libchamplain04/buildlink3.mk
+++ geography/libchamplain04/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.27 2014/01/03 12:36:31 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2014/01/04 13:46:55 obache Exp $
 
 BUILDLINK_TREE+=	libchamplain
 
@@ -6,12 +6,12 @@ BUILDLINK_TREE+=	libchamplain
 LIBCHAMPLAIN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libchamplain+=	libchamplain04>=0.4.6
-BUILDLINK_ABI_DEPENDS.libchamplain+=	libchamplain04>=0.4.7nb20
+BUILDLINK_ABI_DEPENDS.libchamplain+=	libchamplain04>=0.4.7nb21
 BUILDLINK_PKGSRCDIR.libchamplain?=	../../geography/libchamplain04
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
-.include "../../graphics/clutter-gtk/buildlink3.mk"
+.include "../../graphics/clutter-gtk0.10/buildlink3.mk"
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../net/libsoup24/buildlink3.mk"
 .endif	# LIBCHAMPLAIN_BUILDLINK3_MK
--- geography/libchamplain06/buildlink3.mk
+++ geography/libchamplain06/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.27 2014/01/03 12:36:31 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2014/01/04 13:46:55 obache Exp $
 
 BUILDLINK_TREE+=	libchamplain06
 
@@ -6,12 +6,12 @@ BUILDLINK_TREE+=	libchamplain06
 LIBCHAMPLAIN06_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libchamplain06+=	libchamplain06>=0.6.1
-BUILDLINK_ABI_DEPENDS.libchamplain06+=	libchamplain06>=0.6.1nb26
+BUILDLINK_ABI_DEPENDS.libchamplain06+=	libchamplain06>=0.6.1nb27
 BUILDLINK_PKGSRCDIR.libchamplain06?=	../../geography/libchamplain06
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
-.include "../../graphics/clutter-gtk/buildlink3.mk"
+.include "../../graphics/clutter-gtk0.10/buildlink3.mk"
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../net/libsoup24/buildlink3.mk"
 .endif	# LIBCHAMPLAIN06_BUILDLINK3_MK
