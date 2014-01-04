887885c0f72df4b7cbabfe724325aace2fcebfc8
wiz <wiz@pkgsrc.org>
2014-01-04 15:11:43 +0000

    Add upstream SVN commit revision.

--- audio/mp3diags/patches/patch-src_FileRenamerDlgImpl.h
+++ audio/mp3diags/patches/patch-src_FileRenamerDlgImpl.h
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_FileRenamerDlgImpl.h,v 1.1 2012/01/11 19:25:08 hans Exp $
+$NetBSD: patch-src_FileRenamerDlgImpl.h,v 1.2 2014/01/04 15:11:43 wiz Exp $
+
+http://sourceforge.net/p/mp3diags/code/279/
 
 --- src/FileRenamerDlgImpl.h.orig	2009-10-20 16:15:39.000000000 +0200
 +++ src/FileRenamerDlgImpl.h	2012-01-02 11:33:04.351881930 +0100
