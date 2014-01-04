b9e1dbb60ebae33ecaa35bfa4be09c73489799d4
rodent <rodent@pkgsrc.org>
2014-01-04 13:56:01 +0000

    Update to 1.4.10, the oldest version supported by upstream. See py-uwsgi in -wip for the newest supported version. Partial ChangeLog:
     * 1.4.1
    
    - fixed typos in corerouter plugins
    - fixed offloading when the number of threads is higher than 1
    - fixed static_maps for non-existent paths
    - fixed uwsgi_connect() on modern Linux systems to reset the socket to blocking mode
    
     * 1.4
    
    - gevent improvements
    - improved http/https router and fastrouter
    - Go official support
    - a new set of infos are exported to the stats system
    - improved systemd support
    - log filtering and routing
    - improved tracebacker
    - offload transfer for static files, and network transfers
    - matheval support
    - plugins can be written in Obj-C
    - smart attach daemon
    - added support for PEP 405 virtualenvs
    - rawrouter with xclient support
    - internal routing plugin for cache
    
     * 1.3
    
    - python tracebacker
    - user-governed harakiri
    - simplified external plugin development
    - Linux namespace mountpoint improvements
    - secured subscription system
    - merged routers codebase (fastrouter, http, rawrouter)
    - https support in the http router
    - config report at the end of uWSGI build process
    - improved subscription system (multicast and unix socket)
    - custom options
    - graceful reloads on shared sockets
    - configurable log-master buffer size
    - extreme-optimizations for the stats subsystem
    - redislog and mongodblog plugins
    - added python logger
    - mongodb and postgres imperial monitors
    - implemented psgix.logger and psgix.cleanup
    - full rack spec compliance
    - preliminary ipv6 support
    - gevent graceful reloads
    - support for multiple loggers and logformat
    - lazy-apps to load apps after fork() but without changing reloading subsystem
    - emperor heartbeat subsystem
    - cheaper busyness plugin
    - pluggable clock sources
    - added router_rewrite and router_http plugins
    - external spoolers
    - support for section:// and fd:// loaders
    - alarm subsystem (with curl and xmpp plugins)

--- www/py-uwsgi/patches/patch-logging.c
+++ www/py-uwsgi/patches/patch-logging.c
@@ -1,27 +1,27 @@
-$NetBSD: patch-logging.c,v 1.2 2012/07/12 16:21:53 roy Exp $
+$NetBSD: patch-logging.c,v 1.3 2014/01/04 13:56:01 rodent Exp $
 
---- logging.c.orig	2012-05-14 06:58:20.000000000 +0100
-+++ logging.c	2012-07-06 15:31:08.000000000 +0100
-@@ -263,7 +263,7 @@
- 	kv = kvm_open(NULL, NULL, NULL, O_RDONLY, NULL);
- #endif
- 	if (kv) {
--#if defined(__FreeBSD__) || defined(__DragonFly__)
-+#if defined(__FreeBSD__)
- 
+--- core/logging.c.orig	2013-08-23 12:51:18.000000000 +0000
++++ core/logging.c
+@@ -306,7 +306,7 @@ void uwsgi_setup_log_master(void) {
+ 			int is_id = 1;
+ 			int i;
+ 			for (i = 0; i < (space - name); i++) {
+-				if (!isalnum(name[i])) {
++				if (!isalnum((int) name[i])) {
+ 					is_id = 0;
+ 					break;
+ 				}
+@@ -695,8 +695,13 @@ void get_memusage(uint64_t * rss, uint64
  		struct kinfo_proc *kproc;
  		kproc = kvm_getprocs(kv, KERN_PROC_PID, uwsgi.mypid, &cnt);
-@@ -271,6 +271,13 @@
+ 		if (kproc && cnt > 0) {
++#if defined(__FreeBSD__)
  			*vsz = kproc->ki_size;
  			*rss = kproc->ki_rssize * uwsgi.page_size;
- 		}
 +#elif defined(__DragonFly__)
-+		struct kinfo_proc *kproc;
-+		kproc = kvm_getprocs(kv, KERN_PROC_PID, uwsgi.mypid, &cnt);
-+		if (kproc && cnt > 0) {
 +			*vsz = kproc->kp_vm_map_size;
 +			*rss = kproc->kp_vm_rssize * uwsgi.page_size;
-+		}
++#endif
+ 		}
  #elif defined(UWSGI_NEW_OPENBSD)
- 		struct kinfo_proc *kproc; 
- 		kproc = kvm_getprocs(kv, KERN_PROC_PID, uwsgi.mypid, sizeof(struct kinfo_proc), &cnt);
+ 		struct kinfo_proc *kproc;
