c1a7c2cdfb085b78278101608a87f52714fc4766
tron <tron@pkgsrc.org>
2014-01-05 13:36:52 +0000

    Add reference to PR port-sparc64/48501.

--- multimedia/libvpx/hacks.mk
+++ multimedia/libvpx/hacks.mk
@@ -1,8 +1,8 @@
-# $NetBSD: hacks.mk,v 1.1 2014/01/05 13:27:10 tron Exp $
+# $NetBSD: hacks.mk,v 1.2 2014/01/05 13:36:52 tron Exp $
 
 ### [Sun Jan  5 12:58:28 UTC 2014 : tron]
 ### GCC 4.5.3 gets stuck while compiline "vp9/vp9_cx_iface.c".
-###
+### See PR port-sparc64/48501 for more details.
 .if !empty(MACHINE_PLATFORM:MNetBSD-*-sparc64)
 .  include "../../mk/compiler.mk"
 .  if !empty(CC_VERSION:Mgcc*)
