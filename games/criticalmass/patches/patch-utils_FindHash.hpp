ec1fa930ba026698d7eb2042b54e686ea0b30f1c
dholland <dholland@pkgsrc.org>
2014-01-06 17:08:20 +0000

    Adjust joerg's patches in a way that fixes the gcc build and hopefully doesn't rebreak the clang build. (sigh, C++)

--- games/criticalmass/patches/patch-utils_FindHash.hpp
+++ games/criticalmass/patches/patch-utils_FindHash.hpp
@@ -1,8 +1,7 @@
-$NetBSD: patch-utils_FindHash.hpp,v 1.1 2013/06/16 20:40:40 joerg Exp $
-
+$NetBSD: patch-utils_FindHash.hpp,v 1.2 2014/01/06 17:08:20 dholland Exp $
 Incomplete.
 
---- utils/FindHash.hpp.orig	2013-04-30 12:15:10.000000000 +0000
+--- utils/FindHash.hpp.orig	2005-09-11 23:21:57.000000000 +0000
 +++ utils/FindHash.hpp
 @@ -18,9 +18,9 @@
  template< class _KeyT, class _ValT >
@@ -16,7 +15,7 @@ Incomplete.
      ci = hashMap.find( trigger);
      if( ci == hashMap.end())
      {
-@@ -33,9 +33,24 @@ _ValT* findHash( 
+@@ -33,9 +33,9 @@ _ValT* findHash( 
  template< class _KeyT, class _ValT >
  _ValT* findHash( 
      const _KeyT &trigger, 
@@ -24,6 +23,20 @@ Incomplete.
 +    hash_map< _KeyT, _ValT*, ::HASH_NAMESPACE::hash<_KeyT>, equal_to<_KeyT> > & hashMap )
  {
 -    typename hash_map< const _KeyT, _ValT*, hash<const _KeyT> >::const_iterator ci;
++    typename hash_map< const _KeyT, _ValT*, ::HASH_NAMESPACE::hash<const _KeyT> >::const_iterator ci;
+     ci = hashMap.find( trigger);
+     if( ci == hashMap.end())
+     {
+@@ -45,4 +45,21 @@ _ValT* findHash( 
+     return ci->second;
+ }
+ 
++#ifdef __clang__
++template< class _KeyT, class _ValT >
++_ValT* findHash( 
++    const _KeyT &trigger, 
++    hash_map< const _KeyT, _ValT*, ::HASH_NAMESPACE::hash<const _KeyT>, equal_to<const _KeyT> > & hashMap )
++{
 +    typename hash_map< const _KeyT, _ValT*, ::HASH_NAMESPACE::hash<const _KeyT> >::const_iterator ci;
 +    ci = hashMap.find( trigger);
 +    if( ci == hashMap.end())
@@ -33,13 +46,6 @@ Incomplete.
 +
 +    return ci->second;
 +}
++#endif
 +
-+template< class _KeyT, class _ValT >
-+_ValT* findHash( 
-+    const _KeyT &trigger, 
-+    hash_map< const _KeyT, _ValT*, ::HASH_NAMESPACE::hash<const _KeyT>, equal_to<const _KeyT> > & hashMap )
-+{
-+    typename hash_map< const _KeyT, _ValT*, ::HASH_NAMESPACE::hash<const _KeyT> >::const_iterator ci;
-     ci = hashMap.find( trigger);
-     if( ci == hashMap.end())
-     {
+ #endif
