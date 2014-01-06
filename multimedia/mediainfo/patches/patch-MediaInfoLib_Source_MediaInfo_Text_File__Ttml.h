78b621acf15374200136a21d8ed69139f32a54ef
joerg <joerg>
2014-01-06 22:14:29 +0000

    Forward declarations may not be namespace qualified.

---
+++ multimedia/mediainfo/patches/patch-MediaInfoLib_Source_MediaInfo_Text_File__Ttml.h
@@ -0,0 +1,17 @@
+$NetBSD: patch-MediaInfoLib_Source_MediaInfo_Text_File__Ttml.h,v 1.1 2014/01/06 22:14:29 joerg Exp $
+
+--- MediaInfoLib/Source/MediaInfo/Text/File_Ttml.h.orig	2014-01-06 09:53:28.000000000 +0000
++++ MediaInfoLib/Source/MediaInfo/Text/File_Ttml.h
+@@ -19,8 +19,10 @@
+ #include "MediaInfo/File__Analyze.h"
+ //---------------------------------------------------------------------------
+ 
+-class tinyxml2::XMLDocument;
+-class tinyxml2::XMLElement;
++namespace tinyxml2 {
++class XMLDocument;
++class XMLElement;
++}
+ 
+ namespace MediaInfoLib
+ {
