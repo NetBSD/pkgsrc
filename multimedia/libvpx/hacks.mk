281b576a88ca0b099362e01b8900b588f644733f
martin <martin>
2014-01-06 11:16:23 +0000

    Restrict hack to gcc 4.5 or older (4.8 works)

--- multimedia/libvpx/hacks.mk
+++ multimedia/libvpx/hacks.mk
@@ -1,11 +1,11 @@
-# $NetBSD: hacks.mk,v 1.2 2014/01/05 13:36:52 tron Exp $
+# $NetBSD: hacks.mk,v 1.3 2014/01/06 11:16:23 martin Exp $
 
 ### [Sun Jan  5 12:58:28 UTC 2014 : tron]
 ### GCC 4.5.3 gets stuck while compiline "vp9/vp9_cx_iface.c".
 ### See PR port-sparc64/48501 for more details.
 .if !empty(MACHINE_PLATFORM:MNetBSD-*-sparc64)
 .  include "../../mk/compiler.mk"
-.  if !empty(CC_VERSION:Mgcc*)
+.  if !empty(CC_VERSION:Mgcc-4.[0-5].*)
 PKG_HACKS+=		sparc64-gcc-lock-up
 BUILDLINK_TRANSFORM+=	rm:-O[0-9]*
 .  endif
