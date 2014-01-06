4f129b33df66ef10bf73138bbb5993ce73501797
rodent <rodent@pkgsrc.org>
2014-01-06 04:44:16 +0000

    Update to latest release, 0.9.13. Move SDL and OpenGL to the options framework, the former being enabled while the latter not. The OpenGL renderer has caused crashes on the prior few versions and now we're getting reports of these crashes on non-NetBSD, non-pkgsrc platforms. There's no need to enable this option by default if it's not working properly. We can re-enable it when it's fixed upstream. This version adds Dutch language support. Also, we have SDL2 support in this version. From Changelog:
    0.9.12 -> 0.9.13
     general:
      - Removed Carbon, PSP, Wii and Windows ports
      - Removed autotools based build
      - Removed autopackage
      - mdf/mds images support (CyberWarriorX)
      - Safeguard to reject unsupported cue files (CyberWarriorX)
      - CD+G support (CyberWarriorX)
      - Improved "manual mode" (Guillaume)
      - Made it possible to build Yabause to ouput 16BPP (Guillaume)
      - Hat support in SDL joystick code (anonymous)
      - gdb stub (Guillaume)
      - Improved emulated bios (CyberWarriorX)
     sound:
      - Fixed most of the distorted cd audio issues with the scsp core (CyberWarriorX)
     video:
      - Improvements to the software renderer (transparency, frameskip,  (Guillaume)
     qt port:
      - Memory editor and search (CyberWarriorX)
      - Improvements to the SH2 debugger: Back trace support, step over, step out (CyberWarriorX)
      - Infinite loop tracking (CyberWarriorX)
      - Don't restart emulation after every settings change (AmonX)
      - Made Yabause compilable with Qt5 and SDL2 (CyberWarriorX)
      - Log window can now be hidden (Guillaume)
      - Save and restore window position (Guillaume)
      - Can use .ini files in the application dir (Guillaume)
      - Mouse and 3D control pad support (CyberWarriorX)
      - Made drives and languages settings more user friendly (CyberWarriorX)
      - Changing to fullscreen now updates resolution too (CyberWarriorX)
      - Shortcuts editing (CyberWarriorX)

--- emulators/yabause/options.mk
+++ emulators/yabause/options.mk
@@ -1,11 +1,12 @@
-# $NetBSD: options.mk,v 1.2 2012/10/31 22:48:54 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2014/01/06 04:44:16 rodent Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.yabause
-PKG_SUPPORTED_OPTIONS=	openal
-PKG_OPTIONS_OPTIONAL_GROUPS=	GUI
+PKG_SUPPORTED_OPTIONS=	openal opengl
+PKG_OPTIONS_OPTIONAL_GROUPS=	GUI SDL
 PKG_OPTIONS_GROUP.GUI=		gtk qt
+PKG_OPTIONS_GROUP.SDL=		sdl sdl2
 
-PKG_SUGGESTED_OPTIONS=	qt
+PKG_SUGGESTED_OPTIONS=	qt sdl
 
 .include "../../mk/bsd.options.mk"
 
@@ -29,4 +30,25 @@ BUILDLINK_API_DEPENDS.gtkglext+=        gtkglext>=1.2.0nb4
 
 .if !empty(PKG_OPTIONS:Mopenal)
 .include "../../audio/openal/buildlink3.mk"
+CMAKE_ARGS+=	-DYAB_WANT_OPENAL=ON
+.else
+CMAKE_ARGS+=	-DYAB_WANT_OPENAL=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mopengl)
+.include "../../graphics/glut/buildlink3.mk"
+CMAKE_ARGS+=	-DYAB_WANT_OPENGL=ON
+.else
+CMAKE_ARGS+=	-DYAB_WANT_OPENGL=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Msdl)
+.include "../../devel/SDL/buildlink3.mk"
+CMAKE_ARGS+=	-DYAB_WANT_SDL=ON
+.elif !empty(PKG_OPTIONS:Msdl2)
+.include "../../devel/SDL2/buildlink3.mk"
+CMAKE_ARGS+=	-DYAB_WANT_SDL=ON
+BUILDLINK_TRANSFORM+=	l:SDL:SDL2
+.else
+CMAKE_ARGS+=	-DYAB_WANT_SDL=OFF
 .endif
