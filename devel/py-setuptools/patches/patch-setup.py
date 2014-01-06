e8b405216327784e27d3e039c1f08977e2a18fe3
wiz <wiz>
2014-01-06 14:42:34 +0000

    Update to 2.0.2:
    -----
    2.0.2
    -----
    
    * Fix NameError during installation with Python implementations (e.g. Jython)
      not containing parser module.
    * Fix NameError in ``sdist:re_finder``.
    
    -----
    2.0.1
    -----
    
    * Issue #124: Fixed error in list detection in upload_docs.
    
    ---
    2.0
    ---
    
    * Issue #121: Exempt lib2to3 pickled grammars from DirectorySandbox.
    * Issue #41: Dropped support for Python 2.4 and Python 2.5. Clients requiring
      setuptools for those versions of Python should use setuptools 1.x.
    * Removed ``setuptools.command.easy_install.HAS_USER_SITE``. Clients
      expecting this boolean variable should use ``site.ENABLE_USER_SITE``
      instead.
    * Removed ``pkg_resources.ImpWrapper``. Clients that expected this class
      should use ``pkgutil.ImpImporter`` instead.
    
    -----
    1.4.2
    -----
    
    * Issue #116: Correct TypeError when reading a local package index on Python
      3.
    
    -----
    1.4.1
    -----
    
    * Issue #114: Use ``sys.getfilesystemencoding`` for decoding config in
      ``bdist_wininst`` distributions.
    
    * Issue #105 and Issue #113: Establish a more robust technique for
      determining the terminal encoding::
    
        1. Try ``getpreferredencoding``
        2. If that returns US_ASCII or None, try the encoding from
           ``getdefaultlocale``. If that encoding was a "fallback" because Python
           could not figure it out from the environment or OS, encoding remains
           unresolved.
        3. If the encoding is resolved, then make sure Python actually implements
           the encoding.
        4. On the event of an error or unknown codec, revert to fallbacks
           (UTF-8 on Darwin, ASCII on everything else).
        5. On the encoding is 'mac-roman' on Darwin, use UTF-8 as 'mac-roman' was
           a bug on older Python releases.
    
        On a side note, it would seem that the encoding only matters for when SVN
        does not yet support ``--xml`` and when getting repository and svn version
        numbers. The ``--xml`` technique should yield UTF-8 according to some
        messages on the SVN mailing lists. So if the version numbers are always
        7-bit ASCII clean, it may be best to only support the file parsing methods
        for legacy SVN releases and support for SVN without the subprocess command
        would simple go away as support for the older SVNs does.
    
    ---
    1.4
    ---
    
    * Issue #27: ``easy_install`` will now use credentials from .pypirc if
      present for connecting to the package index.
    * Pull Request #21: Omit unwanted newlines in ``package_index._encode_auth``
      when the username/password pair length indicates wrapping.
    
    -----
    1.3.2
    -----
    
    * Issue #99: Fix filename encoding issues in SVN support.
    
    -----
    1.3.1
    -----
    
    * Remove exuberant warning in SVN support when SVN is not used.

--- devel/py-setuptools/patches/patch-setup.py
+++ devel/py-setuptools/patches/patch-setup.py
@@ -1,16 +1,16 @@
-$NetBSD: patch-setup.py,v 1.1 2013/07/04 06:43:13 wiz Exp $
+$NetBSD: patch-setup.py,v 1.2 2014/01/06 14:42:34 wiz Exp $
 
 Do not install easy_install without python version suffix to allow
 parallel installation of this package for multiple python versions.
 
---- setup.py.orig	2013-07-02 15:45:46.000000000 +0000
+--- setup.py.orig	2013-12-13 16:31:50.000000000 +0000
 +++ setup.py
-@@ -54,7 +54,7 @@ from setuptools.command.test import test
- 
+@@ -30,7 +30,7 @@ from setuptools.command.test import test
  scripts = []
  
--console_scripts = ["easy_install = setuptools.command.easy_install:main"]
-+console_scripts = []
+ def _gen_console_scripts():
+-    yield "easy_install = setuptools.command.easy_install:main"
++    yield ""
  
- # Gentoo distributions manage the python-version-specific scripts themselves,
- # so they define an environment variable to suppress the creation of the
+     # Gentoo distributions manage the python-version-specific scripts
+     # themselves, so those platforms define an environment variable to
