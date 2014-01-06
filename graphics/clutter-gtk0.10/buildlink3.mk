b892d6d6c0aa07b397e27b6d0d6179c1361e5fab
obache <obache>
2014-01-06 09:09:16 +0000

    fixes rename of package.

--- graphics/clutter-gtk0.10/buildlink3.mk
+++ graphics/clutter-gtk0.10/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/01/04 13:34:16 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/01/06 09:09:16 obache Exp $
 #
 
 BUILDLINK_TREE+=	clutter-gtk0.10
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	clutter-gtk0.10
 .if !defined(CLUTTER_GTK0.10_BUILDLINK3_MK)
 CLUTTER_GTK0.10_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clutter-gtk0.10+=	clutter-gtk>=0.10.0
+BUILDLINK_API_DEPENDS.clutter-gtk0.10+=	clutter-gtk0.10>=0.10.0
 BUILDLINK_PKGSRCDIR.clutter-gtk0.10?=	../../graphics/clutter-gtk0.10
 
 .include "../../x11/gtk2/buildlink3.mk"
