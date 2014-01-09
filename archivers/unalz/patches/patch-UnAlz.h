6ce17ab4b783ea0b81a49ba014bd1e3630c26cba
jperkin <jperkin@pkgsrc.org>
2014-01-09 15:54:44 +0000

    Avoid existing ERR macros.  Fixes SunOS.

--- archivers/unalz/patches/patch-UnAlz.h
+++ archivers/unalz/patches/patch-UnAlz.h
@@ -1,7 +1,8 @@
-$NetBSD: patch-UnAlz.h,v 1.1 2011/03/27 08:00:57 obache Exp $
+$NetBSD: patch-UnAlz.h,v 1.2 2014/01/09 15:54:44 jperkin Exp $
 
 * <cstring> for memset(3) and strlen(3)
 * <cstdlib> for free(3)
+* Avoid ERR macro
 
 --- UnAlz.h.orig	2007-02-10 05:55:10.000000000 +0000
 +++ UnAlz.h
@@ -14,3 +15,11 @@ $NetBSD: patch-UnAlz.h,v 1.1 2011/03/27 08:00:57 obache Exp $
  using namespace std;
  
  
+@@ -426,6 +428,7 @@ public :
+ 	FileList::iterator	GetCurFileHeader() { return m_posCur; };		///<  현재 (SetCurrentFile() 로 세팅된) 파일 정보
+ 
+ public :
++#undef ERR
+ 	enum ERR							///< 에러 코드 - 정리 필요..
+ 	{
+ 		ERR_NOERR,
