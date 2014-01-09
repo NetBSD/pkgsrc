9cf92fb3a5f87baa4b01acecbe242eae1d2ca6a7
jperkin <jperkin@pkgsrc.org>
2014-01-09 14:38:33 +0000

    Needs zlib.  Provide dummy Minisat::memUsedPeak().

--- math/minisat/patches/patch-utils_System.cc
+++ math/minisat/patches/patch-utils_System.cc
@@ -1,10 +1,10 @@
-$NetBSD: patch-utils_System.cc,v 1.1.1.1 2013/10/28 04:15:11 asau Exp $
+$NetBSD: patch-utils_System.cc,v 1.2 2014/01/09 14:38:33 jperkin Exp $
 
 Support NetBSD.
 
 --- utils/System.cc.orig	2010-07-10 16:07:36.000000000 +0000
 +++ utils/System.cc
-@@ -72,13 +72,13 @@ double Minisat::memUsedPeak() { 
+@@ -72,13 +72,13 @@ double Minisat::memUsedPeak() {
      double peak = memReadPeak() / 1024;
      return peak == 0 ? memUsed() : peak; }
  
@@ -20,3 +20,10 @@ Support NetBSD.
  
  
  #elif defined(__APPLE__)
+@@ -92,4 +92,6 @@ double Minisat::memUsed(void) {
+ #else
+ double Minisat::memUsed() { 
+     return 0; }
++double Minisat::memUsedPeak() { 
++    return 0; }
+ #endif
