4dfd0f1d26337a1dc6a70464efd23d1c51d6e4c9
khorben <khorben>
2014-01-11 21:01:18 +0000

    Packaged DeforaOS libDesktop 0.1.0, with:
    * API update for desktop messages (unregistering handlers)

--- x11/deforaos-libdesktop/buildlink3.mk
+++ x11/deforaos-libdesktop/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2013/10/10 14:41:48 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2014/01/11 21:01:18 khorben Exp $
+#
 
 BUILDLINK_TREE+=	deforaos-libdesktop
 
 .if !defined(DEFORAOS_LIBDESKTOP_BUILDLINK3_MK)
 DEFORAOS_LIBDESKTOP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.0.10
-BUILDLINK_ABI_DEPENDS.deforaos-libdesktop?=	deforaos-libdesktop>=0.0.10nb2
+BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.1.0
 BUILDLINK_PKGSRCDIR.deforaos-libdesktop?=	../../x11/deforaos-libdesktop
 
 .include "../../devel/deforaos-libsystem/buildlink3.mk"
