e4010742a4167bc7ae45a02c57796d16e9c209ad
adam <adam>
2014-01-11 14:42:00 +0000

    Revbump after updating tcl/tk.

--- x11/blt/buildlink3.mk
+++ x11/blt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2013/06/06 12:53:47 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2014/01/11 14:42:01 adam Exp $
 
 BUILDLINK_TREE+=	blt
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	blt
 BLT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.blt+=		blt>=2.4z
-BUILDLINK_ABI_DEPENDS.blt+=	blt>=2.4znb6
+BUILDLINK_ABI_DEPENDS.blt+=	blt>=2.4znb7
 BUILDLINK_PKGSRCDIR.blt?=	../../x11/blt
 
 .include "../../lang/tcl/buildlink3.mk"
