b435fd0850601b428adf5697ed2ef662d1d6ba42
jperkin <jperkin@pkgsrc.org>
2014-01-13 14:53:37 +0000

    Fix build with Apple GCC 4.2.1.

--- audio/openal/patches/patch-src_arch_i386_x86__floatmul.c
+++ audio/openal/patches/patch-src_arch_i386_x86__floatmul.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_arch_i386_x86__floatmul.c,v 1.3 2012/04/17 17:40:50 joerg Exp $
+$NetBSD: patch-src_arch_i386_x86__floatmul.c,v 1.4 2014/01/13 14:53:37 jperkin Exp $
 
 Fix build with gcc-4.5.
 
@@ -17,14 +17,14 @@ Fix build with gcc-4.5.
 +				v_one.s[2] = 0;
 +				v_one.s[3] = 0;
  				temp = __builtin_ia32_pmulhw(*(v4hi*)bpt, v_sa.v);
-+#if defined(__GNUC__) && defined(__GNUC_MINOR__) && (__GNUC__ <= 3 || (__GNUC__ == 4 && __GNUC_MINOR__ <= 2)) && !defined(__clang__)
++#if defined(__GNUC__) && defined(__GNUC_MINOR__) && (__GNUC__ <= 3 || (__GNUC__ == 4 && __GNUC_MINOR__ <= 2)) && !defined(__clang__) && !defined(__APPLE__)
  				*(v4hi*)bpt = __builtin_ia32_psllw(temp, 1LL);
 +#else
 +				*(v4hi*)bpt = __builtin_ia32_psllw(temp, v_one.v);
 +#endif
  				bpt += 4;
  				temp = __builtin_ia32_pmulhw(*(v4hi*)bpt, v_sa.v);
-+#if defined(__GNUC__) && defined(__GNUC_MINOR__) && (__GNUC__ <= 3 || (__GNUC__ == 4 && __GNUC_MINOR__ <= 2)) && !defined(__clang__)
++#if defined(__GNUC__) && defined(__GNUC_MINOR__) && (__GNUC__ <= 3 || (__GNUC__ == 4 && __GNUC_MINOR__ <= 2)) && !defined(__clang__) && !defined(__APPLE__)
  				*(v4hi*)bpt = __builtin_ia32_psllw(temp, 1LL);
 +#else
 +				*(v4hi*)bpt = __builtin_ia32_psllw(temp, v_one.v);
