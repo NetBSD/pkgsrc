3a56ba1574ef79e5da4c04d37c2db23206f8586b
markd <markd@pkgsrc.org>
2014-01-14 08:45:58 +0000

    Correct the fix to missing first arg of of_filename().  Fixes afpd segfault on moving files.  Bump PKGREVISION.

--- net/netatalk/patches/patch-etc_afpd_filedir.c
+++ net/netatalk/patches/patch-etc_afpd_filedir.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-etc_afpd_filedir.c,v 1.1 2013/07/23 13:28:17 hauke Exp $
+$NetBSD: patch-etc_afpd_filedir.c,v 1.2 2014/01/14 08:45:58 markd Exp $
 
 --- etc/afpd/filedir.c.orig	2013-07-23 09:10:55.000000000 +0000
 +++ etc/afpd/filedir.c
@@ -7,7 +7,7 @@ $NetBSD: patch-etc_afpd_filedir.c,v 1.1 2013/07/23 13:28:17 hauke Exp $
          opened = of_findnameat(sdir_fd, &path);
  #else
 -        opened = of_findname(&path);
-+        opened = of_findname(sdir_fd, &path);
++        opened = of_findname(vol, &path);
  #endif /* HAVE_ATFUNCS */
  
          if (opened) {
