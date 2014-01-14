f78adb48ce1df4c34cc8ed7e602e3d49fd5a9b36
markd <markd@pkgsrc.org>
2014-01-14 08:56:08 +0000

    Update to texworks 0.4.5
    Release 0.4.5 (TL'13) [April 2013]
      * Fix "Restore defaults" in preferences dialog
      * Fix removal and selection of text moved via drag'n'drop
      * Fix default saving location
      * Keep UTF-8 BOMs in files that have them
    
      * Make "Goto Source" lead to the last active source window
      * Implement close button for the console output panel
      * Implement "Open PDF with TeX" option
      * Improve compatibility with OS color schemes
      * New encoding support for "Mac Central European Roman"
      * Code cleanup
    
      * Updated log parser (improve handling of long messages and file paths)
      * New script properties/functions: cursorPosition, getEngineList()

--- editors/texworks/patches/patch-getDefaultBinPaths.sh
+++ editors/texworks/patches/patch-getDefaultBinPaths.sh
@@ -1,17 +1,19 @@
-$NetBSD: patch-getDefaultBinPaths.sh,v 1.2 2012/10/28 03:33:45 markd Exp $
+$NetBSD: patch-getDefaultBinPaths.sh,v 1.3 2014/01/14 08:56:08 markd Exp $
 
---- getDefaultBinPaths.sh.orig	2012-04-29 16:00:44.000000000 +0000
+--- getDefaultBinPaths.sh.orig	2013-04-14 13:45:23.000000000 +0000
 +++ getDefaultBinPaths.sh
-@@ -74,12 +74,12 @@ case $PLATFORM in
+@@ -74,13 +74,13 @@ case $PLATFORM in
  	*)			OS=`echo $PLATFORM | sed 's/.*-//'`
  esac
  
+-appendPath "/usr/local/texlive/2013/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2012/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2011/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2010/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2009/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2008/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2007/bin/$ARCH-$OS"
++#appendPath "/usr/local/texlive/2013/bin/$ARCH-$OS"
 +#appendPath "/usr/local/texlive/2012/bin/$ARCH-$OS"
 +#appendPath "/usr/local/texlive/2011/bin/$ARCH-$OS"
 +#appendPath "/usr/local/texlive/2010/bin/$ARCH-$OS"
