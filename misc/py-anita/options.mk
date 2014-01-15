53103b2a430d96c70eca83e67b94f038129ba4c7
martin <martin@pkgsrc.org>
2014-01-15 19:28:11 +0000

    Ooops, copy & pasto: remove bogus lua dependency

--- misc/py-anita/options.mk
+++ misc/py-anita/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2014/01/15 19:05:34 martin Exp $
+# $NetBSD: options.mk,v 1.2 2014/01/15 19:28:11 martin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-anita
 PKG_SUPPORTED_OPTIONS=		qemu-0
@@ -9,7 +9,6 @@ PKG_SUGGESTED_OPTIONS=		qemu-0
 # qemu1 does not work reliably.  See the anita(1) man page.
 .if empty(PKG_OPTIONS:Mqemu-0)
 DEPENDS+=	qemu:../../emulators/qemu
-.  include "../../lang/lua/buildlink3.mk"
 .else
 DEPENDS+=	qemu>=0.12.3nb2:../../emulators/qemu0
 .endif
