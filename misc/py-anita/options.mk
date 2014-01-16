9b98990e66ac80fbcd98abfed1212fa81affc877
wiz <wiz@pkgsrc.org>
2014-01-16 13:48:26 +0000

    Fix DEPENDS pattern.

--- misc/py-anita/options.mk
+++ misc/py-anita/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2014/01/15 19:28:11 martin Exp $
+# $NetBSD: options.mk,v 1.3 2014/01/16 13:48:26 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-anita
 PKG_SUPPORTED_OPTIONS=		qemu-0
@@ -8,7 +8,7 @@ PKG_SUGGESTED_OPTIONS=		qemu-0
 
 # qemu1 does not work reliably.  See the anita(1) man page.
 .if empty(PKG_OPTIONS:Mqemu-0)
-DEPENDS+=	qemu:../../emulators/qemu
+DEPENDS+=	qemu>=1.0:../../emulators/qemu
 .else
 DEPENDS+=	qemu>=0.12.3nb2:../../emulators/qemu0
 .endif
