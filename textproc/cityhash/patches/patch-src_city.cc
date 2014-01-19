2af6c413f9deca381a1f51eb953ba578b5910266
rodent <rodent@pkgsrc.org>
2014-01-19 02:54:56 +0000

    Make this build on FreeBSD. Patch addition taken from FreeBSD's ports.

--- textproc/cityhash/patches/patch-src_city.cc
+++ textproc/cityhash/patches/patch-src_city.cc
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_city.cc,v 1.1 2013/07/08 00:56:45 rodent Exp $
+$NetBSD: patch-src_city.cc,v 1.2 2014/01/19 02:54:56 rodent Exp $
 
 Add support for SunOS.
 
 --- src/city.cc.orig	2013-06-17 19:45:57.000000000 +0000
 +++ src/city.cc
-@@ -69,6 +69,11 @@ static uint32 UNALIGNED_LOAD32(const cha
+@@ -69,6 +69,16 @@ static uint32 UNALIGNED_LOAD32(const cha
  #define bswap_64(x) bswap64(x)
  #endif
  
@@ -12,6 +12,11 @@ Add support for SunOS.
 +#include <sys/byteorder.h>
 +#define bswap_32(x) htonl(x)
 +#define bswap_64(x) htonll(x)
++
++#elif defined(__FreeBSD__)
++#include <sys/endian.h>
++#define bswap_32(x) bswap32(x)
++#define bswap_64(x) bswap64(x)
 +
  #else
  
