a30942f3b9c83b908cc713bcd096eebc0545d218
ryoon <ryoon@pkgsrc.org>
2014-01-19 01:18:50 +0000

    Update to 1.13.1651.102
    * This version requires -std=c++11, then set GCC_REQD=4.7
    
    Changelog:
    r185 2014-01-06 1.13.1651.102
    
        FIX: Refactor placeholder management ( Issue 147 )
        FIX: Specify _NET_WM_WINDOW_TYPE to mozc_renderer ( Issue 195 )
        FIX: header-line-format breaks candidate window on Emacs 24 ( Issue 196 )
        Support Android NDK r9b.

--- inputmethod/mozc-server/patches/patch-base_password__manager.cc
+++ inputmethod/mozc-server/patches/patch-base_password__manager.cc
@@ -1,11 +1,11 @@
-$NetBSD: patch-base_password__manager.cc,v 1.1 2013/04/29 09:52:17 ryoon Exp $
+$NetBSD: patch-base_password__manager.cc,v 1.2 2014/01/19 01:18:50 ryoon Exp $
 
---- base/password_manager.cc.orig	2012-08-31 05:37:06.000000000 +0000
+--- base/password_manager.cc.orig	2014-01-06 07:10:31.000000000 +0000
 +++ base/password_manager.cc
-@@ -366,7 +366,7 @@ class DeprecatedMacPasswordManager : pub
- // this module to other Linux distro, you might want to implement
- // a new password manager which adopts some secure mechanism such
- // like gnome-keyring.
+@@ -264,7 +264,7 @@ bool WinMacPasswordManager::RemovePasswo
+ // We use plain text file for password storage on Linux. If you port this module
+ // to other Linux distro, you might want to implement a new password manager
+ // which adopts some secure mechanism such like gnome-keyring.
 -#if defined OS_LINUX
 +#if defined(OS_LINUX) || defined(OS_NETBSD)
  typedef PlainPasswordManager DefaultPasswordManager;
