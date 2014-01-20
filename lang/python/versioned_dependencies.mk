6f62fa9a68be80d4fe1054884972c447777be8be
wiz <wiz@pkgsrc.org>
2014-01-20 12:51:29 +0000

    + ephem

--- lang/python/versioned_dependencies.mk
+++ lang/python/versioned_dependencies.mk
@@ -1,4 +1,4 @@
-# $NetBSD: versioned_dependencies.mk,v 1.7 2014/01/20 12:36:28 wiz Exp $
+# $NetBSD: versioned_dependencies.mk,v 1.8 2014/01/20 12:51:29 wiz Exp $
 #
 # This file determines which separate distribution of a Python
 # package is used as dependency, depending on the Python version
@@ -21,6 +21,7 @@ _SUPPORTED_PACKAGES+=X x11/py-X2 x11/py-X
 #_SUPPORTED_PACKAGES+=cairo graphics/py-cairo graphics/py-cairo3
 _SUPPORTED_PACKAGES+=dateutil time/py-dateutil time/py-dateutil2
 _SUPPORTED_PACKAGES+=dns net/py-dns net/py-dns3
+_SUPPORTED_PACKAGES+=ephem math/py-ephem math/py-ephem3
 _SUPPORTED_PACKAGES+=flup www/py-flup www/py-flup3
 _SUPPORTED_PACKAGES+=jsonlib textproc/py-jsonlib textproc/py-jsonlib3
 
