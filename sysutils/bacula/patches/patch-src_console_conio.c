c29e98c1d4d4f291508fe69a18763a2ca816c169
jperkin <jperkin@pkgsrc.org>
2014-01-22 10:45:21 +0000

    Spell HAVE_TERM_H correctly, fixes SunOS build.

--- sysutils/bacula/patches/patch-src_console_conio.c
+++ sysutils/bacula/patches/patch-src_console_conio.c
@@ -1,11 +1,12 @@
-$NetBSD: patch-src_console_conio.c,v 1.2 2014/01/14 10:58:17 markd Exp $
+$NetBSD: patch-src_console_conio.c,v 1.3 2014/01/22 10:45:21 jperkin Exp $
 
 - need termios.h, our term.h does not include it
 - t_insert_line and t_delete_line are macros in term.h; undef them
+- spell HAVE_TERM_H correctly
 
 --- src/console/conio.c.orig	2013-02-19 19:21:35.000000000 +0000
 +++ src/console/conio.c
-@@ -64,6 +64,7 @@
+@@ -64,11 +64,12 @@
  
  #ifdef HAVE_CONIO
  
@@ -13,6 +14,12 @@ $NetBSD: patch-src_console_conio.c,v 1.2 2014/01/14 10:58:17 markd Exp $
  #include <curses.h>
  #include <term.h>
  
+ #if defined(HAVE_SUN_OS)
+-#if !defined(_TERM_H)
++#if !defined(HAVE_TERM_H)
+ extern "C" int tgetent(void *, const char *);
+ extern "C" int tgetnum(const char *);
+ extern "C" char *tgetstr (const char*, char**);
 @@ -250,7 +251,9 @@ static char * getnext(void);
  static char * getprev(void);
  static void putline(char *newl, int newlen);
