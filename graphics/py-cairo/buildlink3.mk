c09d2ddd77b063fe7706148af1d889826861307f
wiz <wiz@pkgsrc.org>
2014-01-23 11:12:43 +0000

    Explain the difference.

--- graphics/py-cairo/buildlink3.mk
+++ graphics/py-cairo/buildlink3.mk
@@ -1,17 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.21 2013/09/02 19:50:40 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2014/01/23 11:12:43 wiz Exp $
 
-BUILDLINK_TREE+=	pycairo
+BUILDLINK_TREE+=	py-cairo
 
 .if !defined(PY_CAIRO_BUILDLINK3_MK)
 PY_CAIRO_BUILDLINK3_MK:=
 
 .  include "../../lang/python/pyversion.mk"
 
-BUILDLINK_API_DEPENDS.pycairo+=	${PYPKGPREFIX}-cairo>=1.0.2
-BUILDLINK_ABI_DEPENDS.pycairo+=	${PYPKGPREFIX}-cairo>=1.10.0nb1
-BUILDLINK_PKGSRCDIR.pycairo?=	../../graphics/py-cairo
+BUILDLINK_API_DEPENDS.py-cairo+=	${PYPKGPREFIX}-cairo>=1.10.0
+BUILDLINK_PKGSRCDIR.py-cairo?=	../../graphics/py-cairo
 
 .include "../../graphics/cairo/buildlink3.mk"
-.endif # PY_CAIRO_BUILDLINK3_MK
+.endif	# PY_CAIRO_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-pycairo
+BUILDLINK_TREE+=	-py-cairo
