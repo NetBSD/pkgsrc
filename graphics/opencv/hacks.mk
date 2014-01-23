925a4240c249fc78d8906e82b09f6fc952ccef98
wiz <wiz@pkgsrc.org>
2014-01-23 15:14:30 +0000

    Improve sed script so it survives OBJDIRs with '-o' in their name. From Tobias Nygren in PR 48544.

--- graphics/opencv/hacks.mk
+++ graphics/opencv/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2012/10/10 18:11:08 wiz Exp $
+# $NetBSD: hacks.mk,v 1.2 2014/01/23 15:14:30 wiz Exp $
 
 .if !defined(OPENCV_HACKS_MK)
 OPENCV_HACKS_MK=	defined
@@ -13,7 +13,7 @@ SUBST_CLASSES+=		opt-hack
 SUBST_STAGE.opt-hack=	post-configure
 SUBST_MESSAGE.opt-hack=	Working around gcc-4.5.4 bug.
 SUBST_FILES.opt-hack=	${WRKSRC}/modules/calib3d/CMakeFiles/opencv_calib3d.dir/build.make
-SUBST_SED.opt-hack=	-e '/stereosgbm.cpp.o/s/-o/-fno-tree-pre -o/'
+SUBST_SED.opt-hack=	-e '/stereosgbm.cpp.o/s/-o /-fno-tree-pre -o /'
 .endif
 
 .endif	# OPENCV_HACKS_MK
