66f6f58e4e79e048b39ad194752447d9037cfef4
wiz <wiz@pkgsrc.org>
2014-01-23 14:47:05 +0000

    Update to 2.0 (python-3.x compatible) based on the wip package by othyro.
    Changes:
    uWSGI 2.0
    
    Changelog [20131230] Important changes
    
    Dynamic options have been definitely removed as well as the
    broken_plugins directory Bugfixes and improvements
    
        improved log rotation do not rely on unix signals to print
        request status during harakiri added magic vars for uid and
        gid various Lua fixes a tons of coverity-governed bugfixes made
        by Riccardo Magliocchetti
    
    New features --attach-daemon2
    
    this is a keyval based option for configuring external daemons.
    
    Updated docs are: :doc:`AttachingDaemons` Linux setns() support
    
    One of the biggest improvements in uWSGI 1.9-2.0 has been the total
    support for Linux namespaces.
    
    This last patch adds support for the setns() syscall.
    
    This syscall allows a process to "attach" to a running namespace.
    
    uWSGI instances can exposes their namespaces file descriptors
    (basically they are the files in /proc/self/ns) via a unix socket.
    
    External instances connects to that unix socket and automatically
    enters the mapped namespace.
    
    to spawn an instance in "namespace server mode", you use the
    --setns-socket <addr> option
    
    uwsgi --setns-socket /var/run/ns.socket --unshare net,ipc,uts ...
    
    to attach you simply use --setns <addr>
    
    uwsgi --setns /var/run/ns.socket ...
    
    Updated docs: :doc:`Namespaces` "private" hooks
    
    When uWSGI runs your hooks, it verbosely print the whole hook action
    line. This could be a security problem in some scenario (for example
    when you run initial phases as root user but allows unprivileged
    access to logs).
    
    Prefixing your action with a '!' will suppress full logging:
    
    [uwsgi] hook-asap = !exec:my_secret_command
    
    Support for yajl library (JSON parser)
    
    Til now uWSGI only supported jansson as the json parser required
    for managing .js config files.
    
    You can now use the yajl library (available in centos) as alternative
    JSON parser (will be automatically detected) Perl spooler support
    
    The perl/PSGI plugin can now be used as a spooler server:
    
    uwsgi::spooler(sub {
         my $args = shift; print Dumper($args); return -2; });
    
    The client part is still missing as we need to fix some internal
    api problem.
    
    Expect it in 2.0.1 ;) Gateways can drop privileges
    
    Gateways (like http router, sslrouter, rawrouter, forkptyrouter
    ...) can now drop privileges independently by the master.
    
    Currently only the http/https/spdy router exposes the new option
    (--http-uid/--http-gid) Subscriptions-governed SNI contexts
    
    The subscription subsystem now supports 3 additional keys (you can
    set them with the --subscribe2 option):
    
    sni_key
    
    sni_cert
    
    sni_ca
    
    all of the takes a path to the relevant ssl files.

--- www/py-uwsgi/patches/patch-uwsgiconfig.py
+++ www/py-uwsgi/patches/patch-uwsgiconfig.py
@@ -1,85 +1,46 @@
-$NetBSD: patch-uwsgiconfig.py,v 1.7 2014/01/05 03:46:11 rodent Exp $
+$NetBSD: patch-uwsgiconfig.py,v 1.8 2014/01/23 14:47:05 wiz Exp $
 
---- uwsgiconfig.py.orig	2013-08-23 12:51:18.000000000 +0000
+Disable inclusion of certain packages by default. Use options framework instead.
+
+--- uwsgiconfig.py.orig	2013-12-30 08:44:59.000000000 +0000
 +++ uwsgiconfig.py
-@@ -635,6 +635,8 @@ class uConf(object):
+@@ -68,6 +68,7 @@ report = {
+     'timer': False,
+     'filemonitor': False,
+     'pcre': False,
++    'uuid': False,
+     'routing': False,
+     'capabilities': False,
+     'yaml': False,
+@@ -734,7 +735,7 @@ class uConf(object):
+         if 'UWSGI_PROFILE_OVERRIDE' in os.environ:
+             for item in os.environ['UWSGI_PROFILE_OVERRIDE'].split(';'):
+                 k,v = item.split('=', 2)
+-                uc.set(k, v)
++                self.set(k, v)
+ 
+         if 'UWSGI_AS_LIB' in os.environ:
+             self.set('as_shared_library', 'true')
+@@ -816,6 +817,8 @@ class uConf(object):
          if locking_mode == 'auto':
              if uwsgi_os == 'Linux' or uwsgi_os == 'SunOS':
                  locking_mode = 'pthread_mutex'
-+	    elif uwsgi_os == 'NetBSD':
-+		locking_mode = 'posix_sem';
++            elif uwsgi_os == 'NetBSD':
++                locking_mode = 'posix_sem';
              # FreeBSD umtx is still not ready for process shared locking
              # starting from FreeBSD 9 posix semaphores can be shared between processes
-             elif uwsgi_os == 'FreeBSD':
-@@ -672,7 +674,7 @@ class uConf(object):
-                 if int(sun_major) >= 5:
-                     if int(sun_minor) >= 10:
-                         event_mode = 'port'
--            elif uwsgi_os in ('Darwin', 'FreeBSD', 'OpenBSD', 'NetBSD'):
-+            elif uwsgi_os in ('Darwin', 'FreeBSD', 'OpenBSD', 'NetBSD', 'DragonFly'):
-                 event_mode = 'kqueue'
- 
-         if event_mode == 'epoll':
-@@ -791,7 +793,7 @@ class uConf(object):
-         has_pcre = False
- 
-         # re-enable after pcre fix
--        if self.get('pcre'):
-+        if self.get('pcreOFF'):
-             if self.get('pcre') == 'auto':
-                 pcreconf = spcall('pcre-config --libs')
-                 if pcreconf:
-@@ -854,7 +856,7 @@ class uConf(object):
-         has_json = False
-         has_uuid = False
+             elif uwsgi_os in ('FreeBSD', 'GNU/kFreeBSD'):
+@@ -1019,10 +1022,11 @@ class uConf(object):
+             self.libs.append('-lcap')
+             report['capabilities'] = True
  
 -        if self.has_include('uuid/uuid.h'):
-+        if False and self.has_include('uuid/uuid.h'):
-             has_uuid = True
++        if self.get('uuid') and self.get('uuid') != 'false' and self.has_include('uuid/uuid.h'):
              self.cflags.append("-DUWSGI_UUID")
-             if uwsgi_os == 'Linux' or os.path.exists('/usr/lib/libuuid.so') or os.path.exists('/usr/local/lib/libuuid.so') or os.path.exists('/usr/lib64/libuuid.so') or os.path.exists('/usr/local/lib64/libuuid.so'):
-@@ -956,7 +958,7 @@ class uConf(object):
-             self.gcc_list.append('core/ini')
-             report['ini'] = True
+-            if uwsgi_os in ('Linux', 'GNU', 'GNU/kFreeBSD') or uwsgi_os.startswith('CYGWIN') or os.path.exists('/usr/lib/libuuid.so') or os.path.exists('/usr/local/lib/libuuid.so') or os.path.exists('/usr/lib64/libuuid.so') or os.path.exists('/usr/local/lib64/libuuid.so'):
++            if uwsgi_os in ('Linux', 'GNU', 'GNU/kFreeBSD') or uwsgi_os.startswith('CYGWIN') or os.path.exists('/usr/lib/libuuid.so') or os.path.exists('/usr/local/lib/libuuid.so') or os.path.exists('/usr/lib64/libuuid.so') or os.path.exists('/usr/local/lib64/libuuid.so') or os.path.exists(os.environ['PREFIX'] + '/lib/libuuid.so'):
+                 self.libs.append('-luuid')
++            report['uuid'] = True
  
--        if self.get('yaml'):
-+        if self.get('yamlOFF'):
-             self.cflags.append("-DUWSGI_YAML")
-             self.gcc_list.append('core/yaml')
-             report['yaml'] = True
-@@ -970,7 +972,7 @@ class uConf(object):
-                     self.libs.append('-lyaml')
-                     report['yaml'] = 'libyaml'
- 
--        if self.get('json'):
-+        if self.get('jsonOFF'):
-             if self.get('json') == 'auto':
-                 jsonconf = spcall("pkg-config --cflags jansson")
-                 if jsonconf:
-@@ -993,7 +995,7 @@ class uConf(object):
-         if has_json:
-             report['json'] = True
- 
--        if self.get('ldap'):
-+        if self.get('ldapOFF'):
-             if self.get('ldap') == 'auto':
-                 if self.has_include('ldap.h'):
-                     self.cflags.append("-DUWSGI_LDAP")
-@@ -1020,7 +1022,7 @@ class uConf(object):
-                 report['ssl'] = True
- 
- 
--        if has_uuid and self.get('zeromq'):
-+        if has_uuid and self.get('zeromqOFF'):
-             if self.get('zeromq') == 'auto':
-                 if self.has_include('zmq.h'):
-                     self.cflags.append("-DUWSGI_ZEROMQ")
-@@ -1080,7 +1082,7 @@ class uConf(object):
-                 self.gcc_list.append('core/xmlconf')
-                 report['xml'] = 'expat'
- 
--        if self.get('sqlite3'):
-+        if self.get('sqlite3OFF'):
-             if self.get('sqlite3') == 'auto':
-                 if self.has_include('sqlite3.h'):
-                     self.cflags.append("-DUWSGI_SQLITE3")
+         if self.get('append_version'):
+             if not self.get('append_version').startswith('-'):
