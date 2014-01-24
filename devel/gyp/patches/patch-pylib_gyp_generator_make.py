8e02dc103db710597086106f736ec997dc816be4
fhajny <fhajny@pkgsrc.org>
2014-01-24 13:28:30 +0000

    Fix gyp for Darwin, where platform libtool should be used. Bump PKGREVISION.

--- devel/gyp/patches/patch-pylib_gyp_generator_make.py
+++ devel/gyp/patches/patch-pylib_gyp_generator_make.py
@@ -1,8 +1,19 @@
-$NetBSD: patch-pylib_gyp_generator_make.py,v 1.1 2013/01/18 11:32:45 ryoon Exp $
+$NetBSD: patch-pylib_gyp_generator_make.py,v 1.2 2014/01/24 13:28:30 fhajny Exp $
 
---- pylib/gyp/generator/make.py.orig	2012-12-15 10:46:06.000000000 +0000
+Force platform libtool na Darwin, see
+https://code.google.com/p/gyp/issues/detail?id=354&q=libtool
+--- pylib/gyp/generator/make.py.orig	2013-09-06 23:07:36.000000000 +0000
 +++ pylib/gyp/generator/make.py
-@@ -328,7 +328,7 @@ sed -e "s|^$(notdir $@)|$@|" $(depfile).
+@@ -161,7 +161,7 @@ cmd_solink_module = $(LINK.$(TOOLSET)) -
+ 
+ LINK_COMMANDS_MAC = """\
+ quiet_cmd_alink = LIBTOOL-STATIC $@
+-cmd_alink = rm -f $@ && ./gyp-mac-tool filter-libtool libtool $(GYP_LIBTOOLFLAGS) -static -o $@ $(filter %.o,$^)
++cmd_alink = rm -f $@ && ./gyp-mac-tool filter-libtool /usr/bin/libtool $(GYP_LIBTOOLFLAGS) -static -o $@ $(filter %.o,$^)
+ 
+ quiet_cmd_link = LINK($(TOOLSET)) $@
+ cmd_link = $(LINK.$(TOOLSET)) $(GYP_LDFLAGS) $(LDFLAGS.$(TOOLSET)) -o "$@" $(LD_INPUTS) $(LIBS)
+@@ -324,7 +324,7 @@ sed -e "s|^$(notdir $@)|$@|" $(depfile).
  # We remove slashes and replace spaces with new lines;
  # remove blank lines;
  # delete the first line and append a colon to the remaining lines.
