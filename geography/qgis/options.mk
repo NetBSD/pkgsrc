82be0821214ec70d6933f5aa2d5737cb84ab1b91
wiz <wiz@pkgsrc.org>
2014-01-25 08:59:57 +0000

    Remove unnecessary USE_X11=yes. Mark as not ready for python-3.x.

--- geography/qgis/options.mk
+++ geography/qgis/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2010/08/27 11:42:58 adam Exp $
+# $NetBSD: options.mk,v 1.2 2014/01/25 08:59:57 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.qgis
 PKG_SUPPORTED_OPTIONS=	python
@@ -14,6 +14,7 @@ CMAKE_ARGS+=		-DWITH_BINDINGS:BOOL=FALSE
 CMAKE_ARGS+=		-DWITH_BINDINGS:BOOL=TRUE
 CMAKE_ARGS+=		-DSIP_BINARY_PATH:PATH=${BUILDLINK_PREFIX.py-sip}/bin
 PLIST.python=		yes
+PYTHON_VERSIONS_INCOMPATIBLE=	33 # not yet ported as of 1.7.4
 .include "../../lang/python/application.mk"
 .include "../../x11/py-qt4/buildlink3.mk"
 .include "../../x11/py-sip/buildlink3.mk"
