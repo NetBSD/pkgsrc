0d2a6f23b1d3011bcf6534a2a6e373459d314e63
wiz <wiz@pkgsrc.org>
2014-01-27 18:49:45 +0000

    Remove gnome-keyring option, libsecret is now used and on by default.

--- print/evince3/options.mk
+++ print/evince3/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.3 2013/05/15 14:57:35 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2014/01/27 18:49:45 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.evince
-PKG_SUPPORTED_OPTIONS=	dbus djvu dvi gnome-keyring xps
-PKG_SUGGESTED_OPTIONS=	dbus gnome-keyring
+PKG_SUPPORTED_OPTIONS=	dbus djvu dvi xps
+PKG_SUGGESTED_OPTIONS=	dbus
 
 .include "../../mk/bsd.options.mk"
 
@@ -15,12 +15,6 @@ BUILDLINK_API_DEPENDS.dbus-glib+=       dbus-glib>=0.70
 .include "../../sysutils/dbus-glib/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-dbus
-.endif
-
-.if !empty(PKG_OPTIONS:Mgnome-keyring)
-.include "../../security/libgnome-keyring/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-keyring
 .endif
 
 .if !empty(PKG_OPTIONS:Mdvi)
