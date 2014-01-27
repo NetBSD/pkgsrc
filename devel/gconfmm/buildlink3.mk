d3a768ef59085720a0f42c43102737c59d39abd9
drochner <drochner@pkgsrc.org>
2014-01-27 17:10:46 +0000

    flatten hierarchy - the gtkmm dependency is introduced by libgnomeuimm

--- devel/gconfmm/buildlink3.mk
+++ devel/gconfmm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.33 2013/10/10 14:41:47 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2014/01/27 17:10:46 drochner Exp $
 
 BUILDLINK_TREE+=	gconfmm
 
@@ -10,7 +10,7 @@ BUILDLINK_ABI_DEPENDS.gconfmm+=	gconfmm>=2.28.3nb15
 BUILDLINK_PKGSRCDIR.gconfmm?=	../../devel/gconfmm
 
 .include "../../devel/GConf/buildlink3.mk"
-.include "../../x11/gtkmm/buildlink3.mk"
+.include "../../devel/glibmm/buildlink3.mk"
 .endif # GCONFMM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gconfmm
