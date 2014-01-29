fc9f6966bbf44f20a051e089dd0dfb64d1c69bd8
ryoon <ryoon@pkgsrc.org>
2014-01-29 13:01:53 +0000

    Restrict lib/dbus-1.0/include/dbus/dbus-arch-deps.h buildlink Avoid errors in non-qt5-dbus packages.

--- sysutils/dbus/buildlink3.mk
+++ sysutils/dbus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2013/12/13 14:51:23 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2014/01/29 13:01:53 ryoon Exp $
 
 BUILDLINK_TREE+=	dbus
 
@@ -8,8 +8,12 @@ DBUS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.dbus+=	dbus>=0.91
 BUILDLINK_PKGSRCDIR.dbus?=	../../sysutils/dbus
 
+.if defined(USE_DBUS-ARCH-DEPS_H)
+.  if !empty(USE_DBUS-ARCH-DEPS_H:M[yY][eE][sS])
 BUILDLINK_INCDIRS.dbus+=	lib/dbus-1.0/include/dbus
 BUILDLINK_FNAME_TRANSFORM.dbus+=	-e "s|lib/dbus-1.0/||"
+.  endif
+.endif
 
 .include "../../textproc/expat/buildlink3.mk"
 .endif # DBUS_BUILDLINK3_MK
