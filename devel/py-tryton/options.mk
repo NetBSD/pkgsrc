ac0f617098216fed1182374f28d39f946e4af009
rodent <rodent@pkgsrc.org>
2014-01-30 03:46:18 +0000

    Import py27-tryton-3.0.2 as devel/py-tryton.
    Tryton is a three-tiers high-level general purpose application platform under
    the license GPL-3 written in Python and using PostgreSQL as database engine.
    
    It is the core base of a complete business solution providing modularity,
    scalability and security.

---
+++ devel/py-tryton/options.mk
@@ -0,0 +1,23 @@
+# $NetBSD: options.mk,v 1.1 2014/01/30 03:46:18 rodent Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.py-tryton
+PKG_SUPPORTED_OPTIONS=	cdecimal goocalendar pytz simplejson
+PKG_SUGGESTED_OPTIONS+=	cdecimal pytz simplejson
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mcdecimal)
+DEPENDS+=		${PYPKGPREFIX}-cdecimal-[0-9]*:../../math/py-cdecimal
+.endif
+
+.if !empty(PKG_OPTIONS:Mgoocalendar)
+DEPENDS+=		${PYPKGPREFIX}-goocalendar-[0-9]*:../../time/py-goocalendar
+.endif
+
+.if !empty(PKG_OPTIONS:Mpytz)
+DEPENDS+=		${PYPKGPREFIX}-pytz-[0-9]*:../../time/py-pytz
+.endif
+
+.if !empty(PKG_OPTIONS:Msimplejson)
+DEPENDS+=		${PYPKGPREFIX}-simplejson-[0-9]*:../../converters/py-simplejson
+.endif
