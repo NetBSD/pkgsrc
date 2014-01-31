c3f90413e006747f1defe02452327d8ff90f77ca
wiz <wiz>
2014-01-31 19:32:04 +0000

    Mark some stuff not available for python and use versioned_dependencies.mk, so that pbulk can finish a scan again.

--- devel/py-tryton/options.mk
+++ devel/py-tryton/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2014/01/30 03:46:18 rodent Exp $
+# $NetBSD: options.mk,v 1.2 2014/01/31 19:32:04 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-tryton
 PKG_SUPPORTED_OPTIONS=	cdecimal goocalendar pytz simplejson
@@ -7,10 +7,12 @@ PKG_SUGGESTED_OPTIONS+=	cdecimal pytz simplejson
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mcdecimal)
+PYTHON_VERSIONS_INCOMPATIBLE=	33 # py-cdecimal not available because obsolete
 DEPENDS+=		${PYPKGPREFIX}-cdecimal-[0-9]*:../../math/py-cdecimal
 .endif
 
 .if !empty(PKG_OPTIONS:Mgoocalendar)
+PYTHON_VERSIONS_INCOMPATIBLE=	33 # py-gtk2 via py-goocanvas via py-goocalendar
 DEPENDS+=		${PYPKGPREFIX}-goocalendar-[0-9]*:../../time/py-goocalendar
 .endif
 
