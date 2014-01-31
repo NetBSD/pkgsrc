91e0ff3d1be7787b68374731135bcfa71c23bd41
wiz <wiz@pkgsrc.org>
2014-01-31 11:26:12 +0000

    Add unittest2.

--- lang/python/versioned_dependencies.mk
+++ lang/python/versioned_dependencies.mk
@@ -1,4 +1,4 @@
-# $NetBSD: versioned_dependencies.mk,v 1.10 2014/01/23 16:18:42 wiz Exp $
+# $NetBSD: versioned_dependencies.mk,v 1.11 2014/01/31 11:26:12 wiz Exp $
 #
 # This file determines which separate distribution of a Python
 # package is used as dependency, depending on the Python version
@@ -24,6 +24,7 @@ _SUPPORTED_PACKAGES+=dns net/py-dns net/py-dns3
 _SUPPORTED_PACKAGES+=ephem math/py-ephem math/py-ephem3
 _SUPPORTED_PACKAGES+=flup www/py-flup www/py-flup3
 _SUPPORTED_PACKAGES+=jsonlib textproc/py-jsonlib textproc/py-jsonlib3
+_SUPPORTED_PACKAGES+=unittest2 devel/py-unittest2 devel/py-unittest2py3k
 
 .for pattern in ${PYTHON_VERSIONED_DEPENDENCIES}
 _PKG_MATCHED=	no
