9a908b39ecc6c46de41468f3beec0dd8ca100611
markd <markd@pkgsrc.org>
2014-02-01 02:20:43 +0000

    Update to KDE SC 4.11.5
    bug fixes

--- x11/kdelibs4/buildlink3.mk
+++ x11/kdelibs4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.35 2014/01/01 11:52:04 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2014/02/01 02:20:53 markd Exp $
 
 BUILDLINK_TREE+=	kdelibs
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kdelibs
 KDELIBS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kdelibs+=	kdelibs4>=4.0.0
-BUILDLINK_ABI_DEPENDS.kdelibs+=	kdelibs4>=4.11.3nb3
+BUILDLINK_ABI_DEPENDS.kdelibs+=	kdelibs4>=4.11.5
 BUILDLINK_PKGSRCDIR.kdelibs?=	../../x11/kdelibs4
 
 .include "../../archivers/bzip2/buildlink3.mk"
--- x11/kdelibs4/patches/patch-cmake_modules_FindKDE4Internal.cmake
+++ x11/kdelibs4/patches/patch-cmake_modules_FindKDE4Internal.cmake
@@ -1,8 +1,8 @@
-$NetBSD: patch-cmake_modules_FindKDE4Internal.cmake,v 1.2 2013/11/13 22:52:23 joerg Exp $
+$NetBSD: patch-cmake_modules_FindKDE4Internal.cmake,v 1.3 2014/02/01 02:20:53 markd Exp $
 
 Don't force -std=iso9899:1990.
 
---- cmake/modules/FindKDE4Internal.cmake.orig	2013-11-01 15:45:56.000000000 +0000
+--- cmake/modules/FindKDE4Internal.cmake.orig	2014-01-02 19:26:54.000000000 +0000
 +++ cmake/modules/FindKDE4Internal.cmake
 @@ -1204,7 +1204,7 @@ if (CMAKE_COMPILER_IS_GNUCXX)
     set(CMAKE_C_FLAGS_DEBUGFULL        "-g3 -fno-inline")
@@ -13,12 +13,12 @@ Don't force -std=iso9899:1990.
     # As of Qt 4.6.x we need to override the new exception macros if we want compile with -fno-exceptions
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wnon-virtual-dtor -Wno-long-long -Wundef -Wcast-align -Wchar-subscripts -Wall -W -Wpointer-arith -Wformat-security -fno-exceptions -DQT_NO_EXCEPTIONS -fno-check-new -fno-common")
  
-@@ -1336,7 +1336,7 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "Clan
+@@ -1347,7 +1347,7 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "Clan
     set(CMAKE_C_FLAGS_DEBUGFULL        "-g3 -fno-inline")
     set(CMAKE_C_FLAGS_PROFILE          "-g3 -fno-inline -ftest-coverage -fprofile-arcs")
  
 -   set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -Wno-long-long -std=iso9899:1990 -Wundef -Wcast-align -Werror-implicit-function-declaration -Wchar-subscripts -Wall -W -Wpointer-arith -Wwrite-strings -Wformat-security -Wmissing-format-attribute -fno-common")
 +   set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -Wno-long-long -Wundef -Wcast-align -Werror-implicit-function-declaration -Wchar-subscripts -Wall -W -Wpointer-arith -Wwrite-strings -Wformat-security -Wmissing-format-attribute -fno-common")
-    # As of Qt 4.6.x we need to override the new exception macros if we want compile with -fno-exceptions
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wnon-virtual-dtor -Wno-long-long -Wundef -Wcast-align -Wchar-subscripts -Wall -W -Wpointer-arith -Wformat-security -Woverloaded-virtual -fno-exceptions -DQT_NO_EXCEPTIONS -fno-common -fvisibility=hidden -Werror=return-type -fvisibility-inlines-hidden")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wnon-virtual-dtor -Wno-long-long -Wundef -Wcast-align -Wchar-subscripts -Wall -W -Wpointer-arith -Wformat-security -Woverloaded-virtual -fno-common -fvisibility=hidden -Werror=return-type -fvisibility-inlines-hidden")
     set(KDE4_C_FLAGS    "-fvisibility=hidden")
+ 
