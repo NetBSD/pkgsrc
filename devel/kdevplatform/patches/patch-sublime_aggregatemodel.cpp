e1b3f744e10b617e142d3e6ea721922818316ea0
markd <markd@pkgsrc.org>
2014-02-01 09:54:23 +0000

    Update to kdevplatform 1.6.0 (kdevelop 4.6.0)
    Aside from many bug fixes and general improvements such as performance
    optimizations in various areas, which make KDevelop 4.6.0 faster and less
    memory-hungry, a few changes are especially noteworthy:
    
    The ReviewBoard plugin now supports updating existing review requests.
    There is a new plugin which adds support for building projects with ninja
    instead of make. CMake support was also stabilized and improved, adding
    support for missing or new CMake features.

--- devel/kdevplatform/patches/patch-sublime_aggregatemodel.cpp
+++ devel/kdevplatform/patches/patch-sublime_aggregatemodel.cpp
@@ -1,13 +1,13 @@
-$NetBSD: patch-sublime_aggregatemodel.cpp,v 1.1 2013/10/14 14:34:25 joerg Exp $
+$NetBSD: patch-sublime_aggregatemodel.cpp,v 1.2 2014/02/01 09:54:23 markd Exp $
 
---- sublime/aggregatemodel.cpp.orig	2013-10-14 11:07:07.000000000 +0000
+--- sublime/aggregatemodel.cpp.orig	2013-12-07 18:45:39.000000000 +0000
 +++ sublime/aggregatemodel.cpp
 @@ -81,7 +81,7 @@ Qt::ItemFlags AggregateModel::flags(cons
  {
      if (!index.isValid())
          return 0;
--    return Qt::ItemIsEnabled & Qt::ItemIsSelectable;
-+    return Qt::ItemFlags(Qt::ItemIsEnabled & Qt::ItemIsSelectable);
+-    return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
++    return Qt::ItemFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable);
  }
  
  QVariant AggregateModel::headerData(int section, Qt::Orientation orientation, int role) const
