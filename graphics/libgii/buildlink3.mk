f10119758e74e7654a88d27bb67402a59ac48077
joerg <joerg@pkgsrc.org>
2014-02-01 19:39:17 +0000

    Expose libraries needed for -lgii.

--- graphics/libgii/buildlink3.mk
+++ graphics/libgii/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2010/01/30 14:54:48 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2014/02/01 19:39:17 joerg Exp $
 
 BUILDLINK_TREE+=	libgii
 
@@ -9,6 +9,8 @@ BUILDLINK_API_DEPENDS.libgii+=	libgii>=1.0.2
 BUILDLINK_PKGSRCDIR.libgii?=	../../graphics/libgii
 
 .include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXxf86dga/buildlink3.mk"
 .endif # LIBGII_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libgii
