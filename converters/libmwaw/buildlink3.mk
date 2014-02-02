c53aa36691bbc7a286860b557884b255be5ddb2c
ryoon <ryoon@pkgsrc.org>
2014-02-02 07:25:39 +0000

    Update to 0.2.0
    * Fix dependency
    
    Changelog:
    Not available.

--- converters/libmwaw/buildlink3.mk
+++ converters/libmwaw/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/08/24 16:09:56 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/02/02 07:25:39 ryoon Exp $
 
 BUILDLINK_TREE+=	libmwaw
 
 .if !defined(LIBMWAW_BUILDLINK3_MK)
 LIBMWAW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libmwaw+=	libmwaw>=0.1.10
+BUILDLINK_API_DEPENDS.libmwaw+=	libmwaw>=0.2.0
 BUILDLINK_PKGSRCDIR.libmwaw?=	../../converters/libmwaw
 
-.include "../../converters/libwpd/buildlink3.mk"
+.include "../../converters/libwpg/buildlink3.mk"
 #.include "../../devel/zlib/buildlink3.mk"
 .endif	# LIBMWAW_BUILDLINK3_MK
 
