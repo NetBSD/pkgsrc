d3facc1c723fa403e6e9900e6352294e9b11aec9
rodent <rodent@pkgsrc.org>
2014-02-02 15:24:13 +0000

    Import smpeg2-2.0.0 as multimedia/smpeg2.
    SMPEG is a free MPEG1 video player library with sound support. Video playback is
    based on the ubiquitous Berkeley MPEG player, mpeg_play v2.2. Audio is played
    through a slightly modified mpegsound library, part of Splay v0.8.2. SMPEG
    supports MPEG audio (MP3), MPEG-1 video, and MPEG system streams.

---
+++ multimedia/smpeg2/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/02/02 15:24:13 rodent Exp $
+
+BUILDLINK_TREE+=	smpeg2
+
+.if !defined(SMPEG2_BUILDLINK3_MK)
+SMPEG2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.smpeg2+=	smpeg2>=2.0.0
+BUILDLINK_PKGSRCDIR.smpeg2?=	../../multimedia/smpeg2
+
+.include "../../devel/SDL2/buildlink3.mk"
+.endif	# SMPEG2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-smpeg2
