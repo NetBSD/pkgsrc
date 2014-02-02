3b3575e1e743f2e95b3f98ab50398d99449b6e06
ryoon <ryoon@pkgsrc.org>
2014-02-02 09:48:31 +0000

    Update to 0.0.3
    Changelog:
    libodfgen 0.0.3
    
    - handle more table properties
    - more SVG path commands
    - allow to define center of rotation for a textbox
    - make linked text boxes more robust
    - add support for generating presentations (using libetonyek)
    - start on API documentation

--- textproc/libodfgen/buildlink3.mk
+++ textproc/libodfgen/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/08/24 16:17:12 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/02/02 09:48:31 ryoon Exp $
 
 BUILDLINK_TREE+=	libodfgen
 
 .if !defined(LIBODFGEN_BUILDLINK3_MK)
 LIBODFGEN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libodfgen+=	libodfgen>=0.0.2
+BUILDLINK_API_DEPENDS.libodfgen+=	libodfgen>=0.0.3
 BUILDLINK_PKGSRCDIR.libodfgen?=	../../textproc/libodfgen
 
 .include "../../converters/libwpd/buildlink3.mk"
