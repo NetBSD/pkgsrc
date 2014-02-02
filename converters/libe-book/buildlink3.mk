ce46246d919311ef8f076944b638c9a8290fdacb
ryoon <ryoon@pkgsrc.org>
2014-02-02 07:28:11 +0000

    Import libe-book-0.0.2 as converters/libe-book.
    libe-book is a library and a set of tools for reading and converting
    various non-HTML reflowable e-book formats.
    
    Currently supported are:
    - eReader .pdb
    - FictionBook v. 2 (including zipped files)
    - PalmDoc Ebook
    - Plucker .pdb
    - QiOO (mobile format, for java-enabled cellphones)
    - TCR (simple compressed text format)
    - TealDoc
    - zTXT
    - ZVR (simple compressed text format)
    
    For convenience of use in third-party projects the library is available under
    two licenses: GNU Lesser GPL 2.1+ and MPL 2.0+.

---
+++ converters/libe-book/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/02/02 07:28:11 ryoon Exp $
+
+BUILDLINK_TREE+=	libe-book
+
+.if !defined(LIBE_BOOK_BUILDLINK3_MK)
+LIBE_BOOK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libe-book+=	libe-book>=0.0.2
+BUILDLINK_PKGSRCDIR.libe-book?=	../../converters/libe-book
+
+.include "../../converters/libwpd/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.endif	# LIBE_BOOK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libe-book
