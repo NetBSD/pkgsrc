59dce65218804b7f9e72f6b2f223421d3c169d35
ryoon <ryoon@pkgsrc.org>
2014-02-03 16:01:19 +0000

    Update to 0.4.0
    CHangelog:
    libxkbcommon 0.4.0
    ==================
    
    - Add a new add-on library, xkbcommon-x11, to support creating keymaps
      with the XKB X11 protocol, by querying the X server directly.
      See the xkbcommon/xkbcommon-x11.h header file for more details.
      This library requires libxcb-xkb >= 1.10, and is enabled by default.
      It can be disabled with the --disable-x11 configure switch.
      Distributions are encouraged to split the necessary files for this
      library (libxkbcommon-x11.so, xkbcommon-x11.pc, xkbcommon/xkbcommon-x11.h)
      to a separate package, such that the main package does not depend on
      X11 libraries.
    
    - Fix the keysym <-> name lookup table to not require huge amounts of
      relocations.
    
    - Fix a bug in the keysym <-> name lookup, whereby lookup might fail in
      some rare cases.
    
    - Reduce memory usage during keymap compilation.
    
    - New API:
      New keysyms from xproto 7.0.25 (German T3 layout keysyms).
      XKB_MOD_NAME_NUM for the usual NumLock modifier.
      xkb_x11_* types and functions, XKB_X11_* constants.

--- x11/libxkbcommon/buildlink3.mk
+++ x11/libxkbcommon/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/12/13 14:47:33 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/02/03 16:01:19 ryoon Exp $
 
 BUILDLINK_TREE+=	libxkbcommon
 
 .if !defined(LIBXKBCOMMON_BUILDLINK3_MK)
 LIBXKBCOMMON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxkbcommon+=	libxkbcommon>=0.3.2
+BUILDLINK_API_DEPENDS.libxkbcommon+=	libxkbcommon>=0.4.0
 BUILDLINK_PKGSRCDIR.libxkbcommon?=	../../x11/libxkbcommon
 .endif	# LIBXKBCOMMON_BUILDLINK3_MK
 
