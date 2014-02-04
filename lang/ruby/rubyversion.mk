a4f8971f99b5672a551c82ba90864a27a818308c
taca <taca@pkgsrc.org>
2014-02-04 03:16:44 +0000

    Take care for older bmake, tested on old NetBSD 4's make(1). Noted by wiz@ via private E-mail.

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.108 2014/02/02 07:46:16 taca Exp $
+# $NetBSD: rubyversion.mk,v 1.109 2014/02/04 03:16:44 taca Exp $
 #
 
 # This file determines which Ruby version is used as a dependency for
@@ -200,8 +200,10 @@ _RUBYVERSION_MK=	# defined
 
 .include "../../mk/bsd.prefs.mk"
 
-.if defined(PKGNAME_REQD) && (!empty(PKGNAME_REQD:Mruby[0-9][0-9][0-9]-*) || !empty(PKGNAME_REQD:Mruby[0-9][0-9]-*))
+.if defined(PKGNAME_REQD)
+. if !empty(PKGNAME_REQD:Mruby[0-9][0-9][0-9]-*) || !empty(PKGNAME_REQD:Mruby[0-9][0-9]-*)
 RUBY_VERSION_REQD?= ${PKGNAME_REQD:C/ruby([0-9][0-9]+)-.*/\1/}
+. endif
 .endif
 
 # current supported Ruby's version
@@ -226,13 +228,13 @@ RUBY_VERSION_SUPPORTED?= 200 193 18
 
 .if defined(RUBY_VERSION_REQD)
 . for rv in ${RUBY_VERSION_SUPPORTED}
-.  if ${rv} == ${RUBY_VERSION_REQD}
+.  if "${rv}" == ${RUBY_VERSION_REQD}
 RUBY_VER=	${rv}
 .  endif
 . endfor
 .elif !defined(RUBY_VER)
 . for rv in ${RUBY_VERSION_SUPPORTED}
-.  if ${rv} == ${RUBY_VERSION_DEFAULT}
+.  if "${rv}" == ${RUBY_VERSION_DEFAULT}
 RUBY_VER=	${rv}
 .  endif
 . endfor
