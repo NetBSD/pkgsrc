db3340d8487f253b9e1245835087654e8a100069
jperkin <jperkin@pkgsrc.org>
2014-02-08 17:08:33 +0000

    Update x11-links to 0.82.  Changes:
     - Add xcb-util files.
    
    Fixes various builds on Darwin when using Xquartz.

--- pkgtools/x11-links/buildlink3.mk
+++ pkgtools/x11-links/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.50 2013/12/21 12:40:46 mrg Exp $
+# $NetBSD: buildlink3.mk,v 1.51 2014/02/08 17:08:33 jperkin Exp $
 #
 # Don't include this file manually!  It will be included as necessary
 # by bsd.buildlink3.mk.
@@ -12,7 +12,7 @@ BUILDLINK_TREE+=	x11-links
 .if !defined(X11_LINKS_BUILDLINK3_MK)
 X11_LINKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.80
+BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.82
 BUILDLINK_PKGSRCDIR.x11-links?=	../../pkgtools/x11-links
 BUILDLINK_DEPMETHOD.x11-links?=	build
 
