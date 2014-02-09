107329c717fe68ba518962fc08b1b1a8e98d5227
taca <taca@pkgsrc.org>
2014-02-09 05:15:21 +0000

    Use versions in rubyversion.mk.

--- lang/ruby/rdoc.mk
+++ lang/ruby/rdoc.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rdoc.mk,v 1.18 2014/02/02 07:48:35 taca Exp $
+# $NetBSD: rdoc.mk,v 1.19 2014/02/09 05:15:21 taca Exp $
 
 .if !defined(_RUBY_RDOC_MK)
 _RUBY_RDOC_MK=	# defined
@@ -19,25 +19,15 @@ _RUBY_RDOC_MK=	# defined
 
 .if !empty(RUBY_RDOC_REQD)
 
-RUBY18_RDOC_VERS=	1.0.1
-RUBY193_RDOC_VERS=	3.9.4
-RUBY200_RDOC_VERS=	4.0.0
+. if empty(RUBY_VER)
+.   include "../../lang/ruby/rubyversion.mk"
+. endif
 
 _RDOC_REQD_MAJOR=	${RUBY_RDOC_REQD:C/\.[0-9\.]+$//}
 _RDOC_REQD_MINORS=	${RUBY_RDOC_REQD:C/^([0-9]+)\.*//}
 
-. if ${RUBY_VER} == "18"
-_RUBY_RDOC_MAJOR=	${RUBY18_RDOC_VERS:C/\.[0-9\.]+$//}
-_RUBY_RDOC_MINORS=	${RUBY18_RDOC_VERS:C/^([0-9]+)\.*//}
-.elif ${RUBY_VER} == "193"
-_RUBY_RDOC_MAJOR=	${RUBY193_RDOC_VERS:C/\.[0-9\.]+$//}
-_RUBY_RDOC_MINORS=	${RUBY193_RDOC_VERS:C/^([0-9]+)\.*//}
-.elif ${RUBY_VER} == "200"
-_RUBY_RDOC_MAJOR=	${RUBY200_RDOC_VERS:C/\.[0-9\.]+$//}
-_RUBY_RDOC_MINORS=	${RUBY200_RDOC_VERS:C/^([0-9]+)\.*//}
-.else
-PKG_FAIL_REASON+= "Unknown Ruby version specified: ${RUBY_VER}."
-. endif
+_RUBY_RDOC_MAJOR=	${RUBY_RDOC_VERSION:C/\.[0-9\.]+$//}
+_RUBY_RDOC_MINORS=	${RUBY_RDOC_VERSION:C/^([0-9]+)\.*//}
 
 _RUBY_RDOC_REQD=	NO
 
