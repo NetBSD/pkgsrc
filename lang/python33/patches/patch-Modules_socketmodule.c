7afc3c1f2fa288369675bae98c9533998fa20bd0
tron <tron@pkgsrc.org>
2014-02-11 20:42:37 +0000

    Pullup ticket #4320 - requested by obache lang/python33: security patch
    Revisions pulled up:
    - lang/python33/Makefile                                        1.14
    - lang/python33/distinfo                                        1.8
    - lang/python33/patches/patch-Modules_socketmodule.c            1.1
    
    ---
       Module Name:	pkgsrc
       Committed By:	obache
       Date:		Tue Feb 11 05:52:56 UTC 2014
    
       Modified Files:
       	pkgsrc/lang/python33: Makefile distinfo
       Added Files:
       	pkgsrc/lang/python33/patches: patch-Modules_socketmodule.c
    
       Log Message:
       Fixes SA56624, taken from upstream.
    
       Bump PKGREVISION.

---
+++ lang/python33/patches/patch-Modules_socketmodule.c
@@ -0,0 +1,19 @@
+$NetBSD: patch-Modules_socketmodule.c,v 1.2.2.2 2014/02/11 20:42:37 tron Exp $
+
+* Fix vulnerability reported in SA56624, taken from upstream:
+  http://hg.python.org/cpython/rev/7f176a45211f/
+
+--- Modules/socketmodule.c.orig	2013-11-17 07:23:02.000000000 +0000
++++ Modules/socketmodule.c
+@@ -2935,6 +2935,11 @@ sock_recvfrom_into(PySocketSockObject *s
+     if (recvlen == 0) {
+         /* If nbytes was not specified, use the buffer's length */
+         recvlen = buflen;
++    } else if (recvlen > buflen) {
++        PyBuffer_Release(&pbuf);
++        PyErr_SetString(PyExc_ValueError,
++                        "nbytes is greater than the length of the buffer");
++        return NULL;
+     }
+ 
+     readlen = sock_recvfrom_guts(s, buf, recvlen, flags, &addr);
