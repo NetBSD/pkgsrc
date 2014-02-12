b9e711887b9ba3393d26bfc890082bb37dd6c292
obache <obache@pkgsrc.org>
2014-02-12 11:31:50 +0000

    Bump PKGREVISION from gtksourceview shlib major bump.

--- x11/gtksourceviewmm/buildlink3.mk
+++ x11/gtksourceviewmm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/01/01 11:52:06 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/02/12 11:31:50 obache Exp $
 
 BUILDLINK_TREE+=	gtksourceviewmm
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gtksourceviewmm
 GTKSOURCEVIEWMM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gtksourceviewmm+=	gtksourceviewmm>=3.2.0
-BUILDLINK_ABI_DEPENDS.gtksourceviewmm?=	gtksourceviewmm>=3.2.0nb3
+BUILDLINK_ABI_DEPENDS.gtksourceviewmm?=	gtksourceviewmm>=3.2.0nb4
 BUILDLINK_PKGSRCDIR.gtksourceviewmm?=	../../x11/gtksourceviewmm
 
 .include "../../devel/atkmm/buildlink3.mk"
