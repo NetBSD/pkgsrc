29f9cd43be201c793d8a316ced43c629da5f9919
prlw1 <prlw1@pkgsrc.org>
2014-02-12 01:07:01 +0000

    Update gtksourceview3 to 3.10.2
    News in 3.10.0
    --------------
    * Small bugfix
    * Updated translations
    
    News in 3.9.92
    --------------
    * SearchSettings: change default value of wrap-around to FALSE
    * Misc bugfixes
    * Updated translations
    
    News in 3.9.91
    --------------
    * Improvements to the completion
    * API break for the search and replace
    * Deprecate the GtkSourceMarkAttributes:stock-id property
    * Misc bugfixes
    * Updated translations
    
    News in 3.9.90
    --------------
    * Modify the search and replace API: SearchContext and SearchSettings classes
    * Regular expression search
    * Misc bugfixes
    * Updated translations
    
    News in 3.9.4
    -------------
    * Warning: the search and replace API is unstable
    * Misc bugfixes
    * Updated translations
    
    News in 3.9.3
    -------------
    * Add a higher-level asynchronous search and replace API
    * Deprecate gtk_source_completion_item_new_from_stock()
    * Deprecate the GtkSourceGutterRendererPixbuf:stock-id property
    * Updated translations
    
    News in 3.9.2
    -------------
    * Add syntax highlighting for the Julia, mediawiki and Scala languages
    * Improvements to the word completion provider
    * Add the GtkSourceCompletionWords:activation property
    * More compact completion popup window
    * Misc bugfixes
    * Updated translations
    
    News in 3.9.1
    -------------
    * Deprecate the GtkSourceCompletionInfo::before-show signal
    * Hide CompletionInfo when focus-out-event on the attached-to widget
    * Improvements to the main completion window
    * Some code improvements to the words completion provider
    * Improvements to the C++ and Perl language specs
    * Documentation improvements
    * Misc bugfixes
    * Updated translations
    
    News in 3.8.1
    -------------
    * Improvements to the LaTeX and Perl language specs
    * Update to gtk-mac-integration 2.0 API
    * More unit tests
    * Misc bugfixes
    * Updated translations
    
    News in 3.8.0
    -------------
    * Add Libtool versioning. There was an ABI break during this development
      cycle, because some private functions were accidentally exported. So
      the Libtool's -version-info is bumped from 0:0:0 to 1:0:0. On
      GNU/Linux, this results in a version bump of the soname.
    * Updated translations
    
    News in 3.7.92
    --------------
    * New style id for warnings
    * Improvements to the completion code and documentation
    * Misc bugfixes
    * Updated translations
    
    News in 3.7.91
    --------------
    * Simplify code for the completion
    * Improvements of the Perl, R and Ruby language specs
    * Misc bugfixes
    * Updated translations
    
    News in 3.7.90
    --------------
    * Improvements in completion
    * Add Modelica language spec
    * Misc bugfixes
    * Updated translations
    
    News in 3.7.3
    -------------
    * Deprecate gtk_source_completion_move_window()
    * Deprecate gtk_source_completion_info_get_widget()
    * Deprecate gtk_source_completion_info_set_widget()
    * CompletionInfo: always a natural size
    * Simplify internal code for the completion (new CompletionModel)
    * Fixes for the style schemes and GtkSourceMark
    * Other misc bugfixes
    * Updated translations
    
    News in 3.7.2
    -------------
    * Improved unit tests and code coverage support
    * Misc bugfixes
    * Updated translations
    
    News in 3.7.1
    -------------
    * Add Bluespec SystemVerilog syntax highlighter
    * Improvements in completion
    * Add language spec for IDL
    * Lots of fixes in gtk doc
    * Misc bugfixes
    * Updated translations

--- x11/gtksourceview3/buildlink3.mk
+++ x11/gtksourceview3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2013/10/10 14:41:56 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2014/02/12 01:07:01 prlw1 Exp $
 
 BUILDLINK_TREE+=	gtksourceview3
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gtksourceview3
 GTKSOURCEVIEW3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gtksourceview3+=	gtksourceview3>=3.4.2
-BUILDLINK_ABI_DEPENDS.gtksourceview3?=	gtksourceview3>=3.6.0nb6
+BUILDLINK_ABI_DEPENDS.gtksourceview3?=	gtksourceview3>=3.10.2
 BUILDLINK_PKGSRCDIR.gtksourceview3?=	../../x11/gtksourceview3
 
 .include "../../textproc/libxml2/buildlink3.mk"
