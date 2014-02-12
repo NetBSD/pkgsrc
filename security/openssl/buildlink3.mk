6ad77da70f8f24d3000eace366dc954a8ee079d6
tron <tron>
2014-02-12 23:23:17 +0000

    Undo undesired ABI version bump done by "blbump" script.

--- security/openssl/buildlink3.mk
+++ security/openssl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.48 2014/02/12 23:16:21 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.49 2014/02/12 23:23:17 tron Exp $
 
 BUILDLINK_TREE+=	openssl
 
@@ -8,7 +8,7 @@ OPENSSL_BUILDLINK3_MK:=
 .  include "../../mk/bsd.fast.prefs.mk"
 
 BUILDLINK_API_DEPENDS.openssl+=	openssl>=1.0.1c
-BUILDLINK_ABI_DEPENDS.openssl+=	openssl>=1.0.1fnb1
+BUILDLINK_ABI_DEPENDS.openssl+=	openssl>=1.0.1c
 BUILDLINK_PKGSRCDIR.openssl?=	../../security/openssl
 
 # Ensure that -lcrypt comes before -lcrypto when linking so that the
