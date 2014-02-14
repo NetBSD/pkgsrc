9d9938f94bf8bc9a90fad584e8df054a23169f5d
christos <christos>
2014-02-14 22:06:39 +0000

    Upgrade to the head of the tree version.

--- net/xl2tpd/patches/patch-xl2tpd.c
+++ net/xl2tpd/patches/patch-xl2tpd.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-xl2tpd.c,v 1.1 2013/07/02 00:22:17 christos Exp $
+$NetBSD: patch-xl2tpd.c,v 1.2 2014/02/14 22:06:39 christos Exp $
 
-Expose functions, more headers
+Expose functions
 
---- xl2tpd.c	2013-06-17 06:17:24.000000000 -0400
-+++ xl2tpd.c	2013-06-11 19:51:00.000000000 -0400
+--- xl2tpd.c.orig	2014-01-16 17:02:04.000000000 -0500
++++ xl2tpd.c	2014-02-14 14:27:24.000000000 -0500
 @@ -19,6 +19,7 @@
  #define _BSD_SOURCE
  #define _XOPEN_SOURCE_EXTENDED
@@ -12,20 +12,3 @@ Expose functions, more headers
  
  #include <stdlib.h>
  #include <sys/types.h>
-@@ -28,6 +29,7 @@
- #include <stdio.h>
- #include <stdarg.h>
- #include <errno.h>
-+#include <signal.h>
- #include <unistd.h>
- #include <time.h>
- #if (__GLIBC__ < 2)
-@@ -35,7 +37,7 @@
- #  include <sys/signal.h>
- # elif defined(LINUX)
- #  include <bsd/signal.h>
--# elif defined(SOLARIS)
-+# elif defined(SOLARIS) || defined(NETBSD)
- #  include <signal.h>
- # endif
- #else
