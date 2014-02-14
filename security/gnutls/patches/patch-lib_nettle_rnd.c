d60525f99de0d3b57b1af1f6af2197177f7fe195
drochner <drochner>
2014-02-14 17:24:27 +0000

    update to 3.2.11 changes: Fix bug that prevented the rejection of v1 intermediate CA certificates (CVE-2014-1959)

--- security/gnutls/patches/patch-lib_nettle_rnd.c
+++ security/gnutls/patches/patch-lib_nettle_rnd.c
@@ -1,20 +1,20 @@
-$NetBSD: patch-lib_nettle_rnd.c,v 1.1 2013/11/29 22:55:29 wiz Exp $
+$NetBSD: patch-lib_nettle_rnd.c,v 1.2 2014/02/14 17:24:27 drochner Exp $
 
 http://lists.gnupg.org/pipermail/gnutls-devel/2013-November/006588.html
 
---- lib/nettle/rnd.c.orig	2013-11-10 17:59:14.000000000 +0000
+--- lib/nettle/rnd.c.orig	2014-01-27 18:27:10.000000000 +0000
 +++ lib/nettle/rnd.c
-@@ -90,8 +90,7 @@ static int do_trivia_source(int init)
- 	memcpy(&event.now, &current_time, sizeof(event.now));
- #ifdef HAVE_GETRUSAGE
- 	if (getrusage(RUSAGE_SELF, &event.rusage) < 0) {
+@@ -97,8 +97,7 @@ static void _rnd_get_event(struct event_
+ # else
+ 	if (getrusage(RUSAGE_SELF, &e->rusage) < 0) {
+ # endif
 -		_gnutls_debug_log("getrusage failed: %s\n",
 -				  strerror(errno));
 +		_gnutls_debug_log("getrusage failed\n");
  		abort();
  	}
  #endif
-@@ -244,8 +243,7 @@ static int do_device_source_urandom(int 
+@@ -263,8 +262,7 @@ static int do_device_source_urandom(int 
  			if (res <= 0) {
  				if (res < 0) {
  					_gnutls_debug_log
