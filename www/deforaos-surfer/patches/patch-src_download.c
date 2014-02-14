17b4a68e0a444b8210b1ed216b4518fa96a7f62e
joerg <joerg@pkgsrc.org>
2014-02-14 13:29:23 +0000

    Include correct header for h_errno.

--- www/deforaos-surfer/patches/patch-src_download.c
+++ www/deforaos-surfer/patches/patch-src_download.c
@@ -1,12 +1,14 @@
-$NetBSD: patch-src_download.c,v 1.1 2014/01/20 18:21:29 khorben Exp $
+$NetBSD: patch-src_download.c,v 1.2 2014/02/14 13:29:23 joerg Exp $
 
 Added missing include for download(1) with WebKit (as by default)
 
 --- src/download.c.orig	2012-10-06 01:34:55.000000000 +0000
 +++ src/download.c
-@@ -32,6 +32,7 @@
+@@ -31,7 +31,9 @@
+ #include <libintl.h>
  #include <gtk/gtk.h>
  #include <System.h>
++#include <netdb.h>
  #ifdef WITH_WEBKIT
 +# include <arpa/inet.h>
  # include <webkit/webkit.h>
