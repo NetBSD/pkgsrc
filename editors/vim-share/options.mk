d1ddc53fc6265c3fc6292339011dd707e188af87
schmonz <schmonz@pkgsrc.org>
2014-02-15 03:37:27 +0000

    Add an option to link with perl, and make the ruby option specify which interpreter we mean (to avoid getting a system Ruby by mistake). Approved by morr@.

--- editors/vim-share/options.mk
+++ editors/vim-share/options.mk
@@ -1,6 +1,6 @@
-# $NetBSD: options.mk,v 1.2 2012/08/27 22:10:34 morr Exp $
+# $NetBSD: options.mk,v 1.3 2014/02/15 03:37:27 schmonz Exp $
 
-PKG_SUPPORTED_OPTIONS+=	ruby python
+PKG_SUPPORTED_OPTIONS+=	ruby python perl
 PKG_OPTIONS_VAR=	PKG_OPTIONS.vim
 
 .include "../../mk/bsd.options.mk"
@@ -8,10 +8,17 @@ PKG_OPTIONS_VAR=	PKG_OPTIONS.vim
 .if !empty(PKG_OPTIONS:Mruby)
 .include "../../lang/ruby/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-rubyinterp
+CONFIGURE_ENV+=		vi_cv_path_ruby=${RUBY}
 .endif
 
 .if !empty(PKG_OPTIONS:Mpython)
 .include "../../lang/python/pyversion.mk"
 CONFIGURE_ARGS+=	--enable-pythoninterp
 CONFIGURE_ENV+=		vi_cv_path_python=${PYTHONBIN}
+.endif
+
+.if !empty(PKG_OPTIONS:Mperl)
+.include "../../lang/perl5/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-perlinterp
+CONFIGURE_ENV+=		vi_cv_path_perl=${PERL5}
 .endif
