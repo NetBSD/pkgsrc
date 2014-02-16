f62e39374f1b2e5228e4695ef03621e4f5450204
agc <agc@pkgsrc.org>
2014-02-16 17:15:48 +0000

    Update the libnetpgpverify package to version 20140210
    Switch over to using the zero-pre-requisite netpgpverify sources by
    using reachover infrastructure to make sure we have one set of
    sources.
    
    This also brings with it the benefit of being able to use SSH
    public keys, as well as PGP pub keys, when verifying signatures.
    
    Extend the package building mechanism so that it can be built using
    libtool (the default), or without libtool, depending on whether
    "BOOTSTRAP" is defined at package build time.

--- security/libnetpgpverify/buildlink3.mk
+++ security/libnetpgpverify/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2013/02/23 21:04:26 agc Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/02/16 17:15:48 agc Exp $
 
 BUILDLINK_TREE+=	libnetpgpverify
 
 .if !defined(LIBNETPGPVERIFY_BUILDLINK3_MK)
 LIBNETPGPVERIFY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libnetpgpverify+=	libnetpgpverify>=20120928
+BUILDLINK_API_DEPENDS.libnetpgpverify+=	libnetpgpverify>=20140210
 BUILDLINK_PKGSRCDIR.libnetpgpverify?=	../../security/libnetpgpverify
 
 .endif	# LIBNETPGPVERIFY_BUILDLINK3_MK
