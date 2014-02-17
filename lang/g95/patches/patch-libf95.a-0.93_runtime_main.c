f17e53cd2e01f273c597a724c4535f32a6b3c34f
jperkin <jperkin@pkgsrc.org>
2014-02-17 16:49:14 +0000

    Weak aliases are unsupported on Darwin, so don't do it.  Fixes lots of dependencies.
    Bump PKGREVISION.

--- lang/g95/patches/patch-libf95.a-0.93_runtime_main.c
+++ lang/g95/patches/patch-libf95.a-0.93_runtime_main.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-libf95.a-0.93_runtime_main.c,v 1.2 2013/09/20 23:06:07 joerg Exp $
+$NetBSD: patch-libf95.a-0.93_runtime_main.c,v 1.3 2014/02/17 16:49:14 jperkin Exp $
 
 * fixes missing restore stack address.  PR pkg/47906
 * Make MAIN_ weak to allow linking libf95 dynamically.
 
 --- libf95.a-0.93/runtime/main.c.orig	2010-04-18 15:47:28.000000000 +0000
 +++ libf95.a-0.93/runtime/main.c
-@@ -37,11 +37,12 @@
+@@ -37,11 +37,14 @@
  void __main(void) {}
  #endif
  
@@ -14,7 +14,9 @@ $NetBSD: patch-libf95.a-0.93_runtime_main.c,v 1.2 2013/09/20 23:06:07 joerg Exp 
 -
  /* main()-- Entry point for the fortran program */
 -void MAIN_(void);
-+#if __GNUC__ - 0 == 4 && __GNUC_MINOR__ < 2
++#ifdef __APPLE__
++void MAIN_(void);
++#elif __GNUC__ - 0 == 4 && __GNUC_MINOR__ < 2
 +extern void weak_MAIN_(void) __attribute__((__weakref__("MAIN_")));
 +#else
 +static void weak_MAIN_(void) __attribute__((__weakref__("MAIN_")));
@@ -22,7 +24,7 @@ $NetBSD: patch-libf95.a-0.93_runtime_main.c,v 1.2 2013/09/20 23:06:07 joerg Exp 
  
  int main(int argc, char *argv[]) {
  
-@@ -50,17 +51,25 @@ int main(int argc, char *argv[]) {
+@@ -50,17 +53,30 @@ int main(int argc, char *argv[]) {
   * unaligned. */
  
  #if HAVE_REAL_10 == 1 || HAVE_REAL_10 == 2
@@ -37,8 +39,13 @@ $NetBSD: patch-libf95.a-0.93_runtime_main.c,v 1.2 2013/09/20 23:06:07 joerg Exp 
      g95_runtime_start(argc, argv);
  
 -    MAIN_();      /* Call the Fortran main program. */
++#ifdef __APPLE__
++    if (MAIN_)
++        MAIN_();      /* Call the Fortran main program. */
++#else
 +    if (weak_MAIN_)
 +        weak_MAIN_();      /* Call the Fortran main program. */
++#endif
  
      memory_done();
      g95_runtime_stop();
