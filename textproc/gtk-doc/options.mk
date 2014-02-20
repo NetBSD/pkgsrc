3e155f94c4680f0aebc5056b1d9570eaad298b93
wiz <wiz@pkgsrc.org>
2014-02-20 09:43:33 +0000

    Update to 1.20. Add some dependencies so that gtk-depscan runs. Mark as not ready for python-3.x.
    GTK-Doc 1.20    (Feb 16 2014)
    ============
    
     Important notice - starting with the next release these long deprecated
     features will be removed, please write to gtk-doc-list@gnome.org and tell us
     your concerns:
     o gtkdoc-scanobj (only used for gtkobject)
     o return values need to be documented using "Returns:". We will remove the
       heuristic that turns a paragraph starting with "Returns " into return docs.
     o gtkdoc-mktmpl - please move all the comments into the sources.
     o generating html via sgml tools (jade/openjade), using xsltproc and
       docbook-xslt is pretty common and preferred since version 1.6
    
     Changes
    
     o 605537 : Returns: listed among parameters
     o 624001 : Support attribute-based method to deprecate symbols
     o 662424 : Class hierarchy about interfaces not generated by default
     o 665926 : should not have to document object structures
     o 666509 : Specifying --enable-gtk-doc should not be required to build source packages
     o 669417 : Duplicate IDs generated for unions registered as boxed types
     o 671519 : Self-test relies on nonportable (GNU enhanced) 'date' command
     o 692367 : gtkdocize should take a srcdir argument
     o 696570 : style updates
     o 697940 : Race condition between setup-build.stamp and scan-build.stamp
     o 701259 : /bin/bash: -chmod: command not found
     o 701638 : Support automake parallel test harness (fix error with GTKDOC_CHECK)
     o 706404 : Minor bug in gtkdoc-mkdb
     o 706438 : Empty lines added at the beginning and at the end of a programlisting
     o 707426 : master is broken in picking up flavour from configure.ac
     o 707717 : Support &quot; Deprecated: X.Y &quot;
     o 708268 : New *-insensitive.png files are not distributes
     o 710478 : gtkdoc-mkdb: Don't complain about annotations with hyphen
     o 711111 : gtkdoc-mkdb: sort entries in the glossary
     o 711598 : gtkdoc-scan doesn't ignore decorators containing parentheses
     o 719644 : docs: make yelp usage conditional in manual
     o 719645 : configure: emit message when looking for yelp
     o 720061 : make: create subdirectories of the content_files to the  builddir
     o 721228 : configure: search for xml catalog in XDG_DATA_DIRS
     o 722479 : cp -u is not portable
     o 723118 : Mark |[ blocks as CDATA
     o 723288 : Fix MarkDown support
     o 723417 : New MarkDown parser
     o 723696 : Add support for blockquote
     o 723812 : Add support for reference links
     o 723913 : List in markdown: &lt; p &gt; inside the last element of a &lt; ul &gt; list
     o 723991 : Improve the display of the synopsis
     o 724002 : gtkdoc-scan: Fix use of uninitialised value in trace logging
    
     Contributors
    
     Alban Browaeys
     Alexander Kanavin
     Carlos Garcia Campos
     Christian Kirbach
     Daniel Mustieles
     David King
     David Nečas
     Dieter Verfaillie
     Dimitris Spingos
     Hib Eris
     Matthias Clasen
     Philip Chimento
     Philip Withnall
     Rafael Ferreira
     Rico Tzschichholz
     Ryan Lortie
     Sebastian Rasmussen
     Sébastien Wilmet
     Stefan Sauer
     Stef Walter
     William Jon McCann
     Zbigniew Jędrzejewski-Szmek

--- textproc/gtk-doc/options.mk
+++ textproc/gtk-doc/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2013/10/31 09:05:32 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2014/02/20 09:43:33 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gtkdoc
 PKG_SUPPORTED_OPTIONS=	sgml
@@ -6,6 +6,7 @@ PKG_SUPPORTED_OPTIONS=	sgml
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Msgml)
+# XXX: will be removed after 1.20
 DEPENDS+= dsssl-docbook-modular-[0-9]*:../../textproc/dsssl-docbook-modular
 SGML_CATALOGS= ${PREFIX}/share/sgml/gtk-doc/gtk-doc.cat
 DEPENDS+= openjade-[0-9]*:../../textproc/openjade
