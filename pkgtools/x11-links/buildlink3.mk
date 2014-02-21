62eea2db8c0eaf3e1550b500510160e9c57f4054
jperkin <jperkin>
2014-02-21 17:45:08 +0000

    x11-links-0.83: Assume Xorg if x11.pc exists.
    This fixes detection on RHEL-based distributions which have the X11 libraries
    and development files installed but not the X server.

--- pkgtools/x11-links/buildlink3.mk
+++ pkgtools/x11-links/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.51 2014/02/08 17:08:33 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.52 2014/02/21 17:45:08 jperkin Exp $
 #
 # Don't include this file manually!  It will be included as necessary
 # by bsd.buildlink3.mk.
@@ -12,7 +12,7 @@ BUILDLINK_TREE+=	x11-links
 .if !defined(X11_LINKS_BUILDLINK3_MK)
 X11_LINKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.82
+BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.83
 BUILDLINK_PKGSRCDIR.x11-links?=	../../pkgtools/x11-links
 BUILDLINK_DEPMETHOD.x11-links?=	build
 
