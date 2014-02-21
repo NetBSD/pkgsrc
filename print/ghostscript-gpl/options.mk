3ca569fec584aaadba90e52c6ea496bd0ccd7fba
richard <richard@pkgsrc.org>
2014-02-21 18:09:02 +0000

    add utf8 [default] option to ghostscript-gpl

--- print/ghostscript-gpl/options.mk
+++ print/ghostscript-gpl/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2013/03/16 21:32:00 dholland Exp $
+# $NetBSD: options.mk,v 1.2 2014/02/21 18:09:02 richard Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ghostscript
-PKG_SUPPORTED_OPTIONS=	x11 cups debug fontconfig disable-compile-inits
-PKG_SUGGESTED_OPTIONS=	x11 fontconfig
+PKG_SUPPORTED_OPTIONS=	x11 cups debug fontconfig disable-compile-inits utf8
+PKG_SUGGESTED_OPTIONS=	x11 fontconfig utf8
 
 .include "../../mk/bsd.options.mk"
 
@@ -59,4 +59,11 @@ CONFIGURE_ARGS+=        --disable-compile-inits
 PLIST.no_cidfmap=	YES
 .else
 PLIST.cidfmap=		YES
+.endif
+
+.if !empty(PKG_OPTIONS:Mutf8)
+.include "../../converters/libiconv/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-libiconv=${ICONV_TYPE}
+.else
+CONFIGURE_ARGS+=	--with-libiconv=no
 .endif
