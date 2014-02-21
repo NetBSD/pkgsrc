f977484cefc8e3345ed9b0438d02a29011ee11a5
prlw1 <prlw1@pkgsrc.org>
2014-02-21 01:13:00 +0000

    Actually use my DIOCGDISKINFO ioctl implementation by regenerating configure.

--- sysutils/diskscrub/patches/patch-src_genrand.c
+++ sysutils/diskscrub/patches/patch-src_genrand.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_genrand.c,v 1.1 2013/12/15 19:44:39 joerg Exp $
+$NetBSD: patch-src_genrand.c,v 1.2 2014/02/21 01:13:00 prlw1 Exp $
+
+Fix missing return value.
 
 --- src/genrand.c.orig	2013-12-15 13:03:04.000000000 +0000
 +++ src/genrand.c
