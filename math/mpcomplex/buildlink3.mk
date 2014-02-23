5695b885fcbed36286ad8f884e3b57b0f95184c5
adam <adam>
2014-02-23 15:32:10 +0000

    Changes 1.0.2: - Fixed mpc_atan, mpc_atanh for (+-0, +-1), see   http://gcc.gnu.org/bugzilla/show_bug.cgi?id=57994#c7 - Fixed mpc_log10 for purely imaginary argument, see   http://lists.gforge.inria.fr/pipermail/mpc-discuss/2012-September/001208.html

--- math/mpcomplex/buildlink3.mk
+++ math/mpcomplex/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/09/14 17:14:09 hans Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/02/23 15:32:10 adam Exp $
 
 BUILDLINK_TREE+=	mpcomplex
 
@@ -8,9 +8,9 @@ MPCOMPLEX_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.mpcomplex+=	mpcomplex>=0.8.2
 BUILDLINK_PKGSRCDIR.mpcomplex?=	../../math/mpcomplex
 
-BUILDLINK_API_DEPENDS.gmp+=     gmp>=4.3.2
+BUILDLINK_API_DEPENDS.gmp+=	gmp>=4.3.2
 .include "../../devel/gmp/buildlink3.mk"
-BUILDLINK_API_DEPENDS.mpfr+=    mpfr>=2.4.2
+BUILDLINK_API_DEPENDS.mpfr+=	mpfr>=2.4.2
 .include "../../math/mpfr/buildlink3.mk"
 .endif	# MPCOMPLEX_BUILDLINK3_MK
 
