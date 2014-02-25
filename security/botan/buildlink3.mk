2f6ceabef93cd017388d235cd91156e888169c0a
joerg <joerg@pkgsrc.org>
2014-02-25 11:51:32 +0000

    Add upper limit to the botan dependency. Bump revisions as needed.

--- security/botan/buildlink3.mk
+++ security/botan/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.8 2011/11/25 21:25:26 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2014/02/25 11:51:32 joerg Exp $
 
 BUILDLINK_TREE+=	botan
 
 .if !defined(BOTAN_BUILDLINK3_MK)
 BOTAN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.botan+=	botan>=1.8.13
+BUILDLINK_API_DEPENDS.botan+=	botan>=1.8.13<1.9
 BUILDLINK_PKGSRCDIR.botan?=	../../security/botan
 .endif # BOTAN_BUILDLINK3_MK
 
