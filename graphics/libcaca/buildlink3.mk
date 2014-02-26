226f1992187fec35e5a29fdd57864da2bfeab37e
wiedi <wiedi@pkgsrc.org>
2014-02-26 10:34:30 +0000

    libcaca.so references stuff from zlib, fixes misc/toilet

--- graphics/libcaca/buildlink3.mk
+++ graphics/libcaca/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/02/23 00:35:32 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/02/26 10:34:30 wiedi Exp $
 
 BUILDLINK_TREE+=	libcaca
 
@@ -9,6 +9,8 @@ BUILDLINK_DEPMETHOD.libcaca?=	build
 
 BUILDLINK_API_DEPENDS.libcaca+=	libcaca>=0.99.18
 BUILDLINK_PKGSRCDIR.libcaca?=	../../graphics/libcaca
+
+.include "../../devel/zlib/buildlink3.mk"
 .endif	# LIBCACA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libcaca
