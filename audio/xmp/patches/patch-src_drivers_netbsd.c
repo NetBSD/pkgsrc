0ef30d521c55e3bcf1d3ac43ad033e3d922b1e4c
rxg <rxg>
2014-02-27 03:40:10 +0000

    4.0.7 (20140224):         - Fix NetBSD sound driver (by shattered)         - Fix Haiku sound driver         - Fix info message display time         - Start instrument numbering at 1         - Minor cosmetic fixes

--- audio/xmp/patches/patch-src_drivers_netbsd.c
+++ audio/xmp/patches/patch-src_drivers_netbsd.c
@@ -1,33 +1,7 @@
-$NetBSD: patch-src_drivers_netbsd.c,v 1.2 2013/11/16 07:41:02 shattered Exp $
+$NetBSD: patch-src_drivers_netbsd.c,v 1.3 2014/02/27 03:40:10 rxg Exp $
 
 --- src/sound_netbsd.c.orig	2013-04-06 13:38:02.000000000 +0000
 +++ src/sound_netbsd.c
-@@ -68,21 +68,23 @@ static int init(struct options *options)
- 
- 	AUDIO_INITINFO(&ainfo);
- 
-+	ainfo.mode = AUMODE_PLAY_ALL;
- 	ainfo.play.sample_rate = options->rate;
- 	ainfo.play.channels = options->format & XMP_FORMAT_MONO ? 1 : 2;
- 
- 	if (options->format & XMP_FORMAT_8BIT) {
- 		ainfo.play.precision = 8;
--		ainfo.play.precision = AUDIO_ENCODING_ULINEAR;
-+		ainfo.play.encoding = AUDIO_ENCODING_ULINEAR;
- 		options->format |= XMP_FORMAT_UNSIGNED;
- 	} else {
- 		ainfo.play.precision = 16;
--		ainfo.play.precision = AUDIO_ENCODING_SLINEAR;
-+		ainfo.play.encoding = AUDIO_ENCODING_SLINEAR;
- 		options->format &= ~XMP_FORMAT_UNSIGNED;
- 	}
- 
- 	ainfo.play.gain = gain;
- 	ainfo.play.buffer_size = bsize;
-+	ainfo.blocksize = 0;
- 
- 	if (ioctl(audio_fd, AUDIO_SETINFO, &ainfo) == -1) {
- 		close(audio_fd);
 @@ -99,7 +101,7 @@ static void play(void *b, int i)
  	while (i) {
  		if ((j = write(audio_fd, b, i)) > 0) {
