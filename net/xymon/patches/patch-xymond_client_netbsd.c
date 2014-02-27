76d06d7b3f30039272848404b043cba1a23b8854
spz <spz@pkgsrc.org>
2014-02-27 20:23:56 +0000

    file escaped cvs add: from PR 48575

---
+++ net/xymon/patches/patch-xymond_client_netbsd.c
@@ -0,0 +1,28 @@
+$NetBSD: patch-xymond_client_netbsd.c,v 1.1 2014/02/27 20:23:56 spz Exp $
+
+--- xymond/client/netbsd.c.orig	2012-07-14 16:32:11.000000000 +0000
++++ xymond/client/netbsd.c
+@@ -24,6 +24,7 @@ void handle_netbsd_client(char *hostname
+ 	char *psstr;
+ 	char *topstr;
+ 	char *dfstr;
++	char *inodestr;
+ 	char *meminfostr;
+ 	char *msgsstr;
+ 	char *netstatstr;
+@@ -47,6 +48,7 @@ void handle_netbsd_client(char *hostname
+ 	psstr = getdata("ps");
+ 	topstr = getdata("top");
+ 	dfstr = getdata("df");
++	inodestr = getdata("inode");
+ 	meminfostr = getdata("meminfo");
+ 	msgsstr = getdata("msgsstr");
+ 	netstatstr = getdata("netstat");
+@@ -57,6 +59,7 @@ void handle_netbsd_client(char *hostname
+ 	unix_cpu_report(hostname, clienttype, os, hinfo, fromline, timestr, uptimestr, clockstr, msgcachestr, 
+ 			whostr, 0, psstr, 0, topstr);
+ 	unix_disk_report(hostname, clienttype, os, hinfo, fromline, timestr, "Avail", "Capacity", "Mounted", dfstr);
++	unix_inode_report(hostname, clienttype, os, hinfo, fromline, timestr, "ifree", "%iused", "Mounted", inodestr);
+ 	unix_procs_report(hostname, clienttype, os, hinfo, fromline, timestr, "COMMAND", NULL, psstr);
+ 	unix_ports_report(hostname, clienttype, os, hinfo, fromline, timestr, 3, 4, 5, portsstr);
+ 
