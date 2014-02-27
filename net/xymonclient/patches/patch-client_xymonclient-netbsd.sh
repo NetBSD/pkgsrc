266063b221a7b43d67975905c75209aabdbf1803
spz <spz@pkgsrc.org>
2014-02-27 20:22:41 +0000

    Update to 4.3.17. The last two teeny versions were for bug fixes. Also contains the inode check expansion for NetBSD from PR 48575
    Upstream changelog:
    
    Changes for 4.3.15 - 4.3.17
    ===========================
    No significant changes.
    
    
    Changes for 4.3.14
    ==================
    In previous Xymon versions, a client-only configuration (i.e. one
    configured with "./configure --client") would place the client
    files in a "client" subdirectory below the directory specified
    during configuration. This is the same directory layout as a server
    installation, where the server and client parts of Xymon are
    in separate subdirectories.
    In 4.3.14, the default has changed so a client-only installation
    now installs in the directory given during the configure-step.
    The "/client" has been eliminated, so if you are upgrading an
    existing client you must either move the old client installation
    one level up from the "client/" directory, or change the Makefile
    generated by "configure --client" and add "/client" to the
    XYMONTOPDIR setting.
    
    The SNI support added in 4.3.13 causes problems with some older
    webservers, whose SSL implementation cannot handshake correctly
    when SNI is used. The failed handshake causes Xymon to report
    the site as down. In 4.3.14, the default is changed so SNI is
    disabled. A new "--sni" option was added to xymonnet to control the
    default setting, and two new tags "sni" and "nosni" can be used in
    hosts.cfg to control SNI for each host that is tested.
    
    
    Changes for 4.3.13
    ==================
    This is mostly a bugfix release. Apart from simple bugs (see
    the Changes file), there are some enhancements:
    
    Alerts sent via e-mail have <CR><NL> line-endings converted
    to plain <NL>, since the carriage-return characters would
    cause some mailers to send alerts as a (binary) attachment
    to an empty mail message.
    
    https-URL's can be forced to use TLS only, by using
    "httpst://..." similar to how SSLv2 and SSLv3 can be chosen.
    
    SSL connections (e.g. for https URL's) now use the TLS
    "Server Name Indication" (SNI) if your OpenSSL library
    supports it. This allows testing of systems that have
    multiple SSL websites located on the same physical IP+port
    (i.e. virtual name-based hosts).
    
    
    Changes for 4.3.12
    ==================
    NOTE: This release includes a bugfix for a security issue
    in the xymond_history and xymond_rrd modules. A "drophost"
    command sent to the xymond port (default: 1984) from an IP
    listed in the --admin-senders access control list can be
    used to delete files owned by the user running the xymond
    daemon. This is allowed by default, so it is highly recommended
    to install this update.
    
    
    Changes for 4.3.2 - 4.3.11
    ==========================
    See the Changes file for a list of significant changes.
    These releases are mostly to fix bugs.
    
    NOTE: Some configuration parameters have changed, so you must
    regenerate the top-level Makefile by running the "configure"
    script before compiling the new version.
    
    The inode-check introduced in 4.3.8 and 4.3.10 requires
    that you update both the Xymon server installation and the
    Xymon client on the systems where you want to monitor how
    many inodes are being used.

--- net/xymonclient/patches/patch-client_xymonclient-netbsd.sh
+++ net/xymonclient/patches/patch-client_xymonclient-netbsd.sh
@@ -1,15 +1,21 @@
-$NetBSD: patch-client_xymonclient-netbsd.sh,v 1.1 2011/11/16 21:45:01 spz Exp $
+$NetBSD: patch-client_xymonclient-netbsd.sh,v 1.2 2014/02/27 20:22:41 spz Exp $
 
 add to filesystems whose 'level of filling' is uninteresting
+add an inode check
 
 --- client/xymonclient-netbsd.sh.orig	2011-07-31 21:01:52.000000000 +0000
 +++ client/xymonclient-netbsd.sh
-@@ -20,7 +20,7 @@ uptime
+@@ -20,7 +20,12 @@ uptime
  echo "[who]"
  who
  echo "[df]"
 -df -P -tnonfs,kernfs,procfs,cd9660,null | sed -e '/^[^ 	][^ 	]*$/{
 +df -P -tnonfs,kernfs,procfs,cd9660,null,ptyfs,fdesc | sed -e '/^[^ 	][^ 	]*$/{
++N
++s/[ 	]*\n[ 	]*/ /
++}'
++echo "[inode]"
++df -i -tnonfs,kernfs,procfs,cd9660,null,ptyfs,fdesc | sed -e '/^[^ 	][^ 	]*$/{
  N
  s/[ 	]*\n[ 	]*/ /
  }'
