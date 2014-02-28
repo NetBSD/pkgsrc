34ec3c750f139db61afb4ab97e0b4383cba64529
adam <adam@pkgsrc.org>
2014-02-28 09:43:10 +0000

    Changes 1.8.0: * New, no 2to3, Python 2 and Python 3 are supported by a common code base. * New, gufuncs for linear algebra, enabling operations on stacked arrays. * New, inplace fancy indexing for ufuncs with the ``.at`` method. * New, ``partition`` function, partial sorting via selection for fast median. * New, ``nanmean``, ``nanvar``, and ``nanstd`` functions skipping NaNs. * New, ``full`` and ``full_like`` functions to create value initialized arrays. * New, ``PyUFunc_RegisterLoopForDescr``, better ufunc support for user dtypes. * Numerous performance improvements in many areas. * Support for Python versions 2.4 and 2.5 has been dropped. * Support for SCons has been removed.

--- math/py-numpy/patches/patch-numpy_distutils_fcompiler_g95.py
+++ math/py-numpy/patches/patch-numpy_distutils_fcompiler_g95.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-numpy_distutils_fcompiler_g95.py,v 1.1 2011/12/20 16:38:30 joerg Exp $
+$NetBSD: patch-numpy_distutils_fcompiler_g95.py,v 1.2 2014/02/28 09:43:11 adam Exp $
 
 Band aid to make sure that pic_flags is actually used.
 
 --- numpy/distutils/fcompiler/g95.py.orig	2010-04-18 10:06:17.000000000 +0000
 +++ numpy/distutils/fcompiler/g95.py
-@@ -18,9 +18,9 @@ class G95FCompiler(FCompiler):
+@@ -19,9 +19,9 @@ class G95FCompiler(FCompiler):
  
      executables = {
          'version_cmd'  : ["<F90>", "--version"],
@@ -14,6 +14,6 @@ Band aid to make sure that pic_flags is actually used.
 +        'compiler_f77' : ["g95", "-ffixed-form", "-fpic"],
 +        'compiler_fix' : ["g95", "-ffixed-form", "-fpic"],
 +        'compiler_f90' : ["g95", "-fpic"],
-         'linker_so'    : ["<F90>","-shared"],
+         'linker_so'    : ["<F90>", "-shared"],
          'archiver'     : ["ar", "-cr"],
          'ranlib'       : ["ranlib"]
--- math/py-numpy/patches/patch-numpy_distutils_fcompiler_gnu.py
+++ math/py-numpy/patches/patch-numpy_distutils_fcompiler_gnu.py
@@ -1,30 +1,40 @@
-$NetBSD: patch-numpy_distutils_fcompiler_gnu.py,v 1.4 2013/02/21 10:59:39 jperkin Exp $
+$NetBSD: patch-numpy_distutils_fcompiler_gnu.py,v 1.5 2014/02/28 09:43:11 adam Exp $
 
-Do not run a shell command when it is "None".
 Linker needs -shared explictly (at least with GCC 4.7 on SunOS), plus
 any ABI flags as appropriate.
+On OS X, do not use '-bundle' and 'dynamic_lookup' (to avoid Python.framework).
+Do not run a shell command when it is "None".
 
---- numpy/distutils/fcompiler/gnu.py.orig	2011-09-13 20:39:16.000000000 +0000
+--- numpy/distutils/fcompiler/gnu.py.orig	2013-10-30 18:31:40.000000000 +0000
 +++ numpy/distutils/fcompiler/gnu.py
-@@ -70,7 +70,7 @@ class GnuFCompiler(FCompiler):
+@@ -72,7 +72,7 @@ class GnuFCompiler(FCompiler):
          'compiler_f77' : [None, "-g", "-Wall", "-fno-second-underscore"],
          'compiler_f90' : None, # Use --fcompiler=gnu95 for f90 codes
          'compiler_fix' : None,
 -        'linker_so'    : [None, "-g", "-Wall"],
-+        'linker_so'    : [None, "-g", "-Wall", "-shared", "@COMPILER_ABI_FLAG@"],
++        'linker_so'    : [None, "-g", "-Wall", "-shared", "-m64"],
          'archiver'     : ["ar", "-cr"],
          'ranlib'       : ["ranlib"],
          'linker_exe'   : [None, "-g", "-Wall"]
-@@ -255,7 +255,7 @@ class Gnu95FCompiler(GnuFCompiler):
+@@ -127,7 +127,7 @@ class GnuFCompiler(FCompiler):
+                     s = 'Env. variable MACOSX_DEPLOYMENT_TARGET set to 10.3'
+                     warnings.warn(s)
+ 
+-            opt.extend(['-undefined', 'dynamic_lookup', '-bundle'])
++            opt.extend(['-undefined'])
+         else:
+             opt.append("-shared")
+         if sys.platform.startswith('sunos'):
+@@ -257,7 +257,7 @@ class Gnu95FCompiler(GnuFCompiler):
          'compiler_f90' : [None, "-Wall", "-fno-second-underscore"] + _EXTRAFLAGS,
          'compiler_fix' : [None, "-Wall", "-ffixed-form",
                            "-fno-second-underscore"] + _EXTRAFLAGS,
 -        'linker_so'    : ["<F90>", "-Wall"],
-+        'linker_so'    : ["<F90>", "-Wall", "-shared", "@COMPILER_ABI_FLAG@"],
++        'linker_so'    : ["<F90>", "-Wall", "-shared", "-m64"],
          'archiver'     : ["ar", "-cr"],
          'ranlib'       : ["ranlib"],
          'linker_exe'   : [None, "-Wall"]
-@@ -268,7 +268,7 @@ class Gnu95FCompiler(GnuFCompiler):
+@@ -270,7 +270,7 @@ class Gnu95FCompiler(GnuFCompiler):
  
      def _universal_flags(self, cmd):
          """Return a list of -arch flags for every supported architecture."""
