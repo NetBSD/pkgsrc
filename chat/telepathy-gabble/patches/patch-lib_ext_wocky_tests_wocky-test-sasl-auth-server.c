738a87724a4351b49ea7ebb7f60b980eab00f638
joerg <joerg@pkgsrc.org>
2014-03-06 15:43:23 +0000

    Fix test case to make sense.

--- chat/telepathy-gabble/patches/patch-lib_ext_wocky_tests_wocky-test-sasl-auth-server.c
+++ chat/telepathy-gabble/patches/patch-lib_ext_wocky_tests_wocky-test-sasl-auth-server.c
@@ -1,7 +1,16 @@
-$NetBSD: patch-lib_ext_wocky_tests_wocky-test-sasl-auth-server.c,v 1.1 2011/11/27 19:35:28 joerg Exp $
+$NetBSD: patch-lib_ext_wocky_tests_wocky-test-sasl-auth-server.c,v 1.2 2014/03/06 15:43:23 joerg Exp $
 
---- lib/ext/wocky/tests/wocky-test-sasl-auth-server.c.orig	2011-11-27 01:52:00.000000000 +0000
+--- lib/ext/wocky/tests/wocky-test-sasl-auth-server.c.orig	2010-10-20 14:56:06.000000000 +0000
 +++ lib/ext/wocky/tests/wocky-test-sasl-auth-server.c
+@@ -154,7 +154,7 @@ test_sasl_auth_server_dispose (GObject *
+   priv->stream = NULL;
+ 
+ #ifdef HAVE_LIBSASL2
+-  if (&priv->sasl_conn != NULL)
++  if (priv->sasl_conn != NULL)
+     sasl_dispose (&priv->sasl_conn);
+   priv->sasl_conn = NULL;
+ #endif
 @@ -929,8 +929,8 @@ test_sasl_auth_server_new (GIOStream *st
    static gboolean sasl_initialized = FALSE;
    int ret;
