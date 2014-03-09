2cfaead71b9aa9cdde40a5c25194f8202b1b7f46
taca <taca@pkgsrc.org>
2014-03-09 14:09:20 +0000

    Update php55 to 5.5.10 (PHP 5.5.10).
    Version 5.5.10
    
    6-Mar-2014
    
    * Core:
    
      - Fixed bug #66574 (Allow multiple paths in php_ini_scanned_path).
    
    * Date:
    
      - Fixed bug #45528 (Allow the DateTimeZone constructor to accept timezones
        per offset too).
    
    * Fileinfo:
    
      - Fixed bug #66731 (file: infinite recursion (CVE-2014-1943)).
      - Fixed bug #66820 (out-of-bounds memory access in fileinfo (CVE-2014-2270)).
    
    * GD:
    
      - Fixed bug #66815 (imagecrop(): insufficient fix for NULL defer
        (CVE-2013-7327)).
    
    * JSON:
    
      - Fixed bug #65753 (JsonSerializeable couldn't implement on module extension).
    * LDAP:
      - Implemented ldap_modify_batch (https://wiki.php.net/rfc/ldap_modify_batch).
    * Openssl:
      - Fixed bug #66501 (Add EC key support to php_openssl_is_private_key).
    
    * PCRE:
    
      - Upgraded to PCRE 8.34.
    
    * Pgsql:
    
      - Added warning for dangerous client encoding and remove possible injections
        for pg_insert()/pg_update()/pg_delete()/pg_select().

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.57 2014/03/09 14:08:16 taca Exp $
+# $NetBSD: phpversion.mk,v 1.58 2014/03/09 14:09:20 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -83,7 +83,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP53_VERSION=	5.3.28
 PHP54_VERSION=	5.4.26
-PHP55_VERSION=	5.5.9
+PHP55_VERSION=	5.5.10
 
 # Define initial release of major version.
 PHP53_RELDATE=	20090630
