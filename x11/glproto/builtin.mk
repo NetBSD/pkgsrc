f98222f99d48cd732f973e19fdb0f36482e67926
jperkin <jperkin@pkgsrc.org>
2014-03-10 11:05:51 +0000

    Search for pkgconfig files in lib${LIBABISUFFIX} and share directories as appropriate.  This is required for X11_TYPE=native on Red Hat Linux.

--- x11/glproto/builtin.mk
+++ x11/glproto/builtin.mk
@@ -1,7 +1,8 @@
-# $NetBSD: builtin.mk,v 1.2 2008/10/05 21:36:33 cube Exp $
+# $NetBSD: builtin.mk,v 1.3 2014/03/10 11:05:51 jperkin Exp $
 
 BUILTIN_PKG:=	glproto
-PKGCONFIG_FILE.glproto=	${X11BASE}/lib/pkgconfig/glproto.pc
+PKGCONFIG_FILE.glproto=		${X11BASE}/lib/pkgconfig/glproto.pc
+PKGCONFIG_FILE.glproto+=	${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/glproto.pc
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
