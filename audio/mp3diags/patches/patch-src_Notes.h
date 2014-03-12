331fa9a6d38ec68169d80dcf3075e6b0be68a840
wiz <wiz@pkgsrc.org>
2014-03-12 12:28:27 +0000

    Update to 1.2.01:
    17.01.2014 - 1.2.01
    - 1.2 as the new stable branch
    - build fix for clang and for Solaris
    - fixed broken links in documentation and moved alternate downloads from Dropbox to my ISP
    
    31.03.2013 - 1.2.00
    - new version number to support the new "stable" branch
    
    01.12.2012 - 1.1.21.080
    - fixed bug that caused a "%s" to be shown instead of the list of files
        about to be changed

--- audio/mp3diags/patches/patch-src_Notes.h
+++ audio/mp3diags/patches/patch-src_Notes.h
@@ -1,17 +1,22 @@
-$NetBSD: patch-src_Notes.h,v 1.2 2014/01/04 15:11:43 wiz Exp $
+$NetBSD: patch-src_Notes.h,v 1.3 2014/03/12 12:28:28 wiz Exp $
 
-http://sourceforge.net/p/mp3diags/code/279/
+Fix
+cd src/ && /usr/bin/make -f Makefile all
+/usr/pkg/qt4/bin/moc -DQT_NO_DEBUG -DQT_XML_LIB -DQT_GUI_LIB -DQT_NETWORK_LIB -DQT_CORE_LIB -I/usr/pkg/qt4/mkspecs/netbsd-g++ -I. -I/usr/pkg/qt4/include/QtCore -I/usr/pkg/qt4/include/QtNetwork -I/usr/pkg/qt4/include/QtGui -I/usr/pkg/qt4/include/QtXml -I/usr/pkg/qt4/include -I. -I. -I/usr/pkg/include CommonData.h -o moc_CommonData.cpp
+usr/pkg/include/boost/type_traits/detail/has_binary_operator.hp:50: Parse error at "BOOST_JOIN"
 
---- src/Notes.h.orig	2010-02-13 07:58:04.000000000 +0100
-+++ src/Notes.h	2012-01-02 11:26:42.140106730 +0100
-@@ -32,6 +32,10 @@
+using patch similar to one reported in
+https://bugreports.qt-project.org/browse/QTBUG-22829
+
+--- src/Notes.h.orig	2014-01-16 13:37:29.000000000 +0000
++++ src/Notes.h
+@@ -33,7 +33,9 @@
+ 
  #include  "SerSupport.h"
  #include  <boost/serialization/split_member.hpp>
- 
-+#ifdef ERR
-+#undef ERR
++#ifndef Q_MOC_RUN  // See: https://bugreports.qt-project.org/browse/QTBUG-22829
+ #include  <boost/lexical_cast.hpp>
 +#endif
-+
- /*
- Stores a message that is added while parsing a file. Multiple messages may be added, depending on the issues found.
  
+ #ifdef ERR
+ #undef ERR
