48d3191929ff1a32ebab45f3d5af72dae2535cff
wiz <wiz@pkgsrc.org>
2014-03-12 09:48:38 +0000

    Add upstream bug report URL.

--- finance/gnucash/patches/patch-src_scm_price-quotes.scm
+++ finance/gnucash/patches/patch-src_scm_price-quotes.scm
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_scm_price-quotes.scm,v 1.1 2013/07/29 18:16:53 wiz Exp $
+$NetBSD: patch-src_scm_price-quotes.scm,v 1.2 2014/03/12 09:48:38 wiz Exp $
 
 Fix for guile-www-2.37, which deprecated 'main'.
+https://bugzilla.gnome.org/show_bug.cgi?id=726164
 
 --- src/scm/price-quotes.scm.orig	2013-04-20 21:07:10.000000000 +0000
 +++ src/scm/price-quotes.scm
