f34fb27ca4f231b6ec007c862c3446eeb8b51692
adam <adam@pkgsrc.org>
2014-03-13 13:25:49 +0000

    Changes 2.0.2:
    General:
    * Added SDL_GL_ResetAttributes() to reset OpenGL attributes to default values
    * Added an API to load a database of game controller mappings from a file:
        SDL_GameControllerAddMappingsFromFile(), SDL_GameControllerAddMappingsFromRW()
    * Added game controller mappings for the PS4 and OUYA controllers
    * Added SDL_GetDefaultAssertionHandler() and SDL_GetAssertionHandler()
    * Added SDL_DetachThread()
    * Added SDL_HasAVX() to determine if the CPU has AVX features
    * Added SDL_vsscanf(), SDL_acos(), and SDL_asin() to the stdlib routines
    * EGL can now create/manage OpenGL and OpenGL ES 1.x/2.x contexts, and share
      them using SDL_GL_SHARE_WITH_CURRENT_CONTEXT
    * Added a field "clicks" to the mouse button event which records whether the event is a single click, double click, etc.
    * The screensaver is now disabled by default, and there is a hint SDL_HINT_VIDEO_ALLOW_SCREENSAVER that can change that behavior.
    * Added a hint SDL_HINT_MOUSE_RELATIVE_MODE_WARP to specify whether mouse relative mode should be emulated using mouse warping.
    * testgl2 does not need to link with libGL anymore
    * Added testgles2 test program to demonstrate working with OpenGL ES 2.0
    * Added controllermap test program to visually map a game controller
    
    Windows:
    * Support for OpenGL ES 2.x contexts using either WGL or EGL (natively via
      the driver or emulated through ANGLE)
    * Added a hint SDL_HINT_VIDEO_WIN_D3DCOMPILER to specify which D3D shader compiler to use for OpenGL ES 2 support through ANGLE
    * Added a hint SDL_HINT_VIDEO_WINDOW_SHARE_PIXEL_FORMAT that is useful when creating multiple windows that should share the same OpenGL context.
    * Added an event SDL_RENDER_TARGETS_RESET that is sent when D3D9 render targets are reset after the device has been restored.
    
    Mac OS X:
    * Added a hint SDL_HINT_MAC_CTRL_CLICK_EMULATE_RIGHT_CLICK to control whether Ctrl+click should be treated as a right click on Mac OS X. This is off by default.
    
    Linux:
    * Fixed fullscreen and focused behavior when receiving NotifyGrab events
    * Added experimental Wayland and Mir support, disabled by default
    
    Android:
    * Joystick support (minimum SDK version required to build SDL is now 12,
      the required runtime version remains at 10, but on such devices joystick
      support won't be available).
    * Hotplugging support for joysticks
    * Added a hint SDL_HINT_ACCELEROMETER_AS_JOYSTICK to control whether the accelerometer should be listed as a 3 axis joystick, which it will by default.

--- devel/SDL2/patches/patch-src_cpuinfo_SDL_cpuinfo.c
+++ devel/SDL2/patches/patch-src_cpuinfo_SDL_cpuinfo.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_cpuinfo_SDL_cpuinfo.c,v 1.1 2013/11/02 20:20:07 adam Exp $
+$NetBSD: patch-src_cpuinfo_SDL_cpuinfo.c,v 1.2 2014/03/13 13:25:49 adam Exp $
 
 Fix building on NetBSD and OpenBSD.
 
---- src/cpuinfo/SDL_cpuinfo.c.orig	2013-11-02 19:57:32.000000000 +0000
+--- src/cpuinfo/SDL_cpuinfo.c.orig	2014-03-08 04:36:51.000000000 +0000
 +++ src/cpuinfo/SDL_cpuinfo.c
-@@ -621,7 +621,7 @@ SDL_GetSystemRAM(void)
+@@ -665,7 +665,7 @@ SDL_GetSystemRAM(void)
  #endif
  #ifdef HAVE_SYSCTLBYNAME
          if (SDL_SystemRAM <= 0) {
--#ifdef __FreeBSD__
-+#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
+-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
++#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__OpenBSD__)
  #ifdef HW_REALMEM
              int mib[2] = {CTL_HW, HW_REALMEM};
  #else
