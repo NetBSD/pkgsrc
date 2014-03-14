4653bc1b213fa311c10aead4f187e97f0b15ea4b
obache <obache>
2014-03-14 13:05:26 +0000

    Bump ABI_DEPENDS from ruby-gnome2-glib for safe side.

--- devel/ruby-gnome2-pango/buildlink3.mk
+++ devel/ruby-gnome2-pango/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.34 2013/10/10 14:41:45 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.35 2014/03/14 13:05:26 obache Exp $
 
 BUILDLINK_TREE+=	ruby-gnome2-pango
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ruby-gnome2-pango
 RUBY_GNOME2_PANGO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-pango+=${RUBY_PKGPREFIX}-gnome2-pango>=0.16.0
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-pango+=${RUBY_PKGPREFIX}-gnome2-pango>=2.0.2nb2
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-pango+=${RUBY_PKGPREFIX}-gnome2-pango>=2.2.0
 BUILDLINK_PKGSRCDIR.ruby-gnome2-pango?=	../../devel/ruby-gnome2-pango
 
 .include "../../devel/ruby-gnome2-glib/buildlink3.mk"
--- graphics/ruby-gnome2-gdk3/buildlink3.mk
+++ graphics/ruby-gnome2-gdk3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2013/10/10 14:41:56 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2014/03/14 13:05:26 obache Exp $
 
 BUILDLINK_TREE+=	ruby-gnome2-gdk3
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ruby-gnome2-gdk3
 RUBY_GNOME2_GDK3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-gdk3+=${RUBY_PKGPREFIX}-gnome2-gtk3>=1.2
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gdk3+=${RUBY_PKGPREFIX}-gnome2-gdk3>=2.0.2nb2
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-gdk3+=${RUBY_PKGPREFIX}-gnome2-gdk3>=2.2.0
 BUILDLINK_PKGSRCDIR.ruby-gnome2-gdk3?=	../../graphics/ruby-gnome2-gdk3
 
 .include "../../x11/gtk3/buildlink3.mk"
