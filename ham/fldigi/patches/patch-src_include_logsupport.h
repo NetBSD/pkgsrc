c0f98c21817000a418ca3324643ec88f96f5ec72
mef <mef@pkgsrc.org>
2014-03-14 14:39:22 +0000

    (pkgsrc) - Add comment on patch (based on commit msg)
    (Upstream update 3.21.76 to 3.21.79)
    =Version 3.21.79=
      2014-03-07  David Freese  <w1hkj at w1hkj.com>
    	RigCAT initialize
    	Modem cleanup
    	Missing build script
    	Choice-->ListBox
    	combobox-listbox
    	FreeBSD One More Time
    	RTTY normalize
    	Sound.cxx cleanup
    	Portaudio ring buffer
    	Headers update
    	Portaudio error reporting
    
      2014-01-28  Kamal Mostafa  <kamal at whence.com>
    	Avoid nitems fltk kfreebsd collision
    
      2014-01-13  David Freese  <w1hkj at w1hkj.com>
    	modem timing test
    	XMLRPC seg fault
    	Mailing address
    
      2014-01-11  John Phelps  <kl4yfd at gmail.com>
    	64 bit stacktrace
    
      2014-01-11  David Freese  <w1hkj at w1hkj.com>
    	Developer update
    	CW timing
    
    =Version 3.21.78=
    	SKED macro
    	FreeBSD issues
    	WF max size
    	PSKR softbits
    
      2013-11-09  Andrej Lajovic  <s57ln at hamradio.si>
    	CharsetDistiller
    	UTF-8
    
      2013-11-08  Arvo Jarve  <arvo at softshark.ee>
    	RigCAT close
    	Save Wordwrap
    
      2013-11-05  David Freese  <w1hkj at w1hkj.com>
    	ARQ timeouts
    	Deutsch
    	DTMF
    	WF latency
    
    =Version 3.21.77=
    	Load preferences
    	Waterfall race condition
    
      2013-10-22  John Phelps  <kl4yfd at gmail.com>
    	MFSK HF Long Modes
    	* Added MFSK64L and MFSL128L new modes
    	* 6.25 Second interleaver for very-robust HF reception
    	* Assigned new RSIDs
    	* Mode MFSK-64L : 1026
    	* Mode MFSK-128L : 1029
    
      2013-10-20  David Freese  <w1hkj at w1hkj.com>
    	Waterfall improvements
    
      2013-10-19  Remi Chateauneu  <remi.chateauneu at gmail.com>
    	Fix test_process on MacOS
    	Various speed-ups, fixes and cleanups
    
      2013-10-16  David Freese  <w1hkj at w1hkj.com>
    	Cfft removal
    	fldigi.pot update
    	Multi-channel decode
    	fft filter
    	g_fft
    	 WF fft
    	rsid fft
    	Wefax Config tab
    	Autostart programs
    	RsID Error Control
    	Macro tag Image
    
      2013-09-17  Kamal Mostafa  <kamal at whence.com>
    	Avoid-nitems-fltk-kfreebsd-collision.patch
    
      2013-09-14  David Freese  <w1hkj at w1hkj.com>
    	MFSKpic improvements
    	ADIF log database
    	MFSK update
    	Thor update

--- ham/fldigi/patches/patch-src_include_logsupport.h
+++ ham/fldigi/patches/patch-src_include_logsupport.h
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_include_logsupport.h,v 1.1 2013/09/17 21:12:25 joerg Exp $
+$NetBSD: patch-src_include_logsupport.h,v 1.2 2014/03/14 14:39:22 mef Exp $
+
+Fix a header guard to reduce warnings.
 
 --- src/include/logsupport.h.orig	2013-09-17 19:27:33.000000000 +0000
 +++ src/include/logsupport.h
