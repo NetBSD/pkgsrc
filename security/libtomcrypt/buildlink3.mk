d17a0aba8b3a9348878dc29d1dd8912902d38ce0
agc <agc@pkgsrc.org>
2014-03-14 22:38:39 +0000

    Only 1 package in the tree uses a buildlink dependency on libtomcrypt (security/dropbear), so bump the BUILDLINK_API_DEPENDS to reflect the current value of the package.

--- security/libtomcrypt/buildlink3.mk
+++ security/libtomcrypt/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/03/10 10:02:34 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/03/14 22:38:39 agc Exp $
 
 BUILDLINK_TREE+=	libtomcrypt
 
 .if !defined(LIBTOMCRYPT_BUILDLINK3_MK)
 LIBTOMCRYPT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libtomcrypt+=	libtomcrypt>=1.17
+BUILDLINK_API_DEPENDS.libtomcrypt+=	libtomcrypt>=1.17nb2
 BUILDLINK_PKGSRCDIR.libtomcrypt?=	../../security/libtomcrypt
 
 .include "../../math/ltm/buildlink3.mk"
