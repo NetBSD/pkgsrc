ffd323360bbaa856d70a1361cffd44299e9d6cec
tsutsui <tsutsui@pkgsrc.org>
2014-03-14 17:42:17 +0000

    Pull more post-3.3.3 fixes from upstream.
    https://bitbucket.org/arakiken/mlterm/commits/1b9ecfa74b0f305c89cff60310f671aef7479246#chg-xwindow/x_screen.c
     - x_color_cache and x_color_manager are reloaded after loading a wall
       picture on NetBSD/OpenBSD 4bpp framebuffer.
    
    https://bitbucket.org/arakiken/mlterm/commits/fa6c82190cc62d356285d7dde71e49a9f671b132#chg-xwindow/x_screen.c
     - update mouse cursor fg and bg colors after loading a wall picture
       on NetBSD/OpenBSD 4bpp framebuffer.
    
    https://bitbucket.org/arakiken/mlterm/commits/097ac4f2b78e1dea5a53a55fa070007655d85add#chg-xwindow/fb/x_display_x68kgrf.c
     - Also allow non-privileged users to use Text VRAM by --multivram=true option
       on NetBSD/x68k
    
    Bump PKGREVISION.

--- x11/mlterm/patches/patch-xwindow_x__screen.c
+++ x11/mlterm/patches/patch-xwindow_x__screen.c
@@ -1,12 +1,49 @@
-$NetBSD: patch-xwindow_x__screen.c,v 1.5 2014/03/07 18:22:50 tsutsui Exp $
+$NetBSD: patch-xwindow_x__screen.c,v 1.6 2014/03/14 17:42:17 tsutsui Exp $
 
 Pull mlterm-3.3.3-fixes.patch from upstream:
 http://sourceforge.net/projects/mlterm/files/01release/mlterm-3.3.3/mlterm-3.3.3-fixes.patch/download
  - Fix the bug which disabled to start mlconfig
 
+https://bitbucket.org/arakiken/mlterm/commits/1b9ecfa74b0f305c89cff60310f671aef7479246#chg-xwindow/x_screen.c
+ - x_color_cache and x_color_manager are reloaded after loading a wall
+   picture on NetBSD/OpenBSD 4bpp framebuffer.
+
+https://bitbucket.org/arakiken/mlterm/commits/fa6c82190cc62d356285d7dde71e49a9f671b132#chg-xwindow/x_screen.c
+ - update mouse cursor fg and bg colors after loading a wall picture
+   on NetBSD/OpenBSD 4bpp framebuffer.
+
 --- xwindow/x_screen.c.orig	2014-02-21 16:51:45.000000000 +0000
 +++ xwindow/x_screen.c
-@@ -5268,11 +5268,13 @@ change_im(
+@@ -1275,6 +1275,28 @@ set_wall_picture(
+ 		return  0 ;
+ 	}
+ 
++#if  defined(USE_FRAMEBUFFER) && (defined(__NetBSD__) || defined(__OpenBSD__))
++	if( screen->window.disp->depth == 4 && strstr( screen->pic_file_path , "six"))
++	{
++		/*
++		 * Color pallette of x_display can be changed by x_acquire_bg_picture().
++		 * (see x_display_set_cmap() called from fb/x_imagelib.c.)
++		 */
++		x_color_cache_unload( screen->color_man->color_cache) ;
++
++		x_color_manager_reload( screen->color_man) ;
++		x_window_set_fg_color( &screen->window ,
++			x_get_xcolor( screen->color_man , ML_FG_COLOR)) ;
++		x_xic_fg_color_changed( &screen->window) ;
++		/* XXX should change scrollbar fg color */
++
++		x_window_set_bg_color( &screen->window ,
++			x_get_xcolor( screen->color_man , ML_BG_COLOR)) ;
++		x_xic_bg_color_changed( &screen->window) ;
++		/* XXX should change scrollbar bg color */
++	}
++#endif
++
+ 	if( ! x_window_set_wall_picture( &screen->window , pic->pixmap))
+ 	{
+ 		x_release_picture( pic) ;
+@@ -5268,11 +5290,13 @@ change_im(
   * Callbacks of x_config_event_listener_t events.
   */
  
@@ -22,7 +59,7 @@ http://sourceforge.net/projects/mlterm/files/01release/mlterm-3.3.3/mlterm-3.3.3
  	)
  {
  	ml_term_t *  term ;
-@@ -5285,7 +5287,7 @@ get_config_intern(
+@@ -5285,7 +5309,7 @@ get_config_intern(
  		if( ( term = (*screen->system_listener->get_pty)( screen->system_listener->self ,
  				dev)) == NULL)
  		{
@@ -31,7 +68,7 @@ http://sourceforge.net/projects/mlterm/files/01release/mlterm-3.3.3/mlterm-3.3.3
  		}
  	}
  	else
-@@ -5829,7 +5831,34 @@ get_config_intern(
+@@ -5829,7 +5853,34 @@ get_config_intern(
  		}
  	}
  
@@ -67,7 +104,7 @@ http://sourceforge.net/projects/mlterm/files/01release/mlterm-3.3.3/mlterm-3.3.3
  }
  
  static void
-@@ -5840,31 +5869,7 @@ get_config(
+@@ -5840,31 +5891,7 @@ get_config(
  	int  to_menu
  	)
  {
@@ -100,16 +137,16 @@ http://sourceforge.net/projects/mlterm/files/01release/mlterm-3.3.3/mlterm-3.3.3
  }
  
  static void
-@@ -8286,18 +8291,21 @@ x_screen_set_config(
+@@ -8286,18 +8313,21 @@ x_screen_set_config(
  	}
  	else if( strcmp( value , "switch") == 0)
  	{
 -		char *  val ;
 +		int  flag ;
-+
-+		get_config_intern( screen , /* dev */ NULL , key , -1 , &flag) ;
  
 -		if( ( val = get_config_intern( screen , /* dev */ NULL , key)))
++		get_config_intern( screen , /* dev */ NULL , key , -1 , &flag) ;
++
 +		if( flag == 1)
  		{
 -			if( strcmp( val , "true") == 0)
@@ -132,7 +169,7 @@ http://sourceforge.net/projects/mlterm/files/01release/mlterm-3.3.3/mlterm-3.3.3
  		}
  	}
  
-@@ -8312,7 +8320,7 @@ x_screen_set_config(
+@@ -8312,7 +8342,7 @@ x_screen_set_config(
  		if( ( term = (*screen->system_listener->get_pty)( screen->system_listener->self ,
  				dev)) == NULL)
  		{
@@ -141,7 +178,7 @@ http://sourceforge.net/projects/mlterm/files/01release/mlterm-3.3.3/mlterm-3.3.3
  		}
  	}
  	else
-@@ -8838,7 +8846,7 @@ x_screen_set_config(
+@@ -8838,7 +8868,7 @@ x_screen_set_config(
  			}
  			else
  			{
