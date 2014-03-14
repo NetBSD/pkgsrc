1bc756b08dff392e95cee2e81cbca115870becdc
obache <obache>
2014-03-14 13:43:17 +0000

    To get USE_BUILTIN.freetype2, builtin.mk must be included.

--- graphics/freetype2/buildlink3.mk
+++ graphics/freetype2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.40 2014/03/14 13:38:20 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.41 2014/03/14 13:43:17 obache Exp $
 
 BUILDLINK_TREE+=	freetype2
 
@@ -14,6 +14,10 @@ BUILDLINK_FILES.freetype2+=	bin/freetype-config
 
 FREETYPE_CONFIG?=	${BUILDLINK_PREFIX.freetype2}/bin/freetype-config
 CONFIGURE_ENV+=		FREETYPE_CONFIG=${FREETYPE_CONFIG:Q}
+
+CHECK_BUILTIN.freetype2:=	yes
+.include "../../graphics/freetype2/builtin.mk"
+CHECK_BUILTIN.freetype2:=	no
 
 .include "../../archivers/bzip2/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
