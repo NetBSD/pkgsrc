6a7b1398abccfec75783e842b590d12ac84d32da
ryoon <ryoon@pkgsrc.org>
2014-03-14 22:11:45 +0000

    Fix PATH_MAX value for SCO OpenServer 5.0.7/3.2. It is 1024 really.

--- net/tnftp/files/src/ftp_var.h
+++ net/tnftp/files/src/ftp_var.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: ftp_var.h,v 1.7 2014/03/14 22:10:49 ryoon Exp $	*/
+/*	$NetBSD: ftp_var.h,v 1.8 2014/03/14 22:11:45 ryoon Exp $	*/
 /*	from	NetBSD: ftp_var.h,v 1.75 2007/07/22 05:02:50 lukem Exp	*/
 
 /*-
@@ -199,7 +199,7 @@ enum {
  */
 #if !defined(PATH_MAX)
 #if defined(_SCO_DS)
-#define PATH_MAX	256
+#define PATH_MAX	1024
 #endif
 #endif
 
