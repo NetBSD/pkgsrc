6488a89da25134f5ba8ca1ec742e06e990bb11e6
gdt <gdt@pkgsrc.org>
2014-03-17 17:44:23 +0000

    Drop libusb1 from pcsc-lite's bl3.
    sbin/pcscd is linked with libusb1, but the installed libraries and
    headers do not reference libusb at all.  Not including libusb1 avoids
    having pcsc-lite's bl3 pull in pthreads to depending packages.

--- security/pcsc-lite/buildlink3.mk
+++ security/pcsc-lite/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2013/01/08 22:51:39 pettai Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2014/03/17 17:44:23 gdt Exp $
 
 BUILDLINK_TREE+=	pcsc-lite
 
@@ -8,7 +8,6 @@ PCSC_LITE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.pcsc-lite+=	pcsc-lite>=1.6.5
 BUILDLINK_PKGSRCDIR.pcsc-lite?=	../../security/pcsc-lite
 
-.include "../../devel/libusb1/buildlink3.mk"
 .endif # PCSC_LITE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-pcsc-lite
