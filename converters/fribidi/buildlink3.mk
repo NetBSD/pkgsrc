3db1e7bb2f132de29720fc785378b042d20466cd
gdt <gdt@pkgsrc.org>
2014-03-18 01:02:28 +0000

    Update BUILDLINK_API_DEPENDS to 0.19.1.
    Based on a report from Bob Bernstein, m17n-lib does not build with
    0.10.9.  According to
      http://upstream-tracker.org/versions/fribidi.html
    there was an API and ABI break at 0.19.1, and then it has been
    essentially stable.

--- converters/fribidi/buildlink3.mk
+++ converters/fribidi/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:24:03 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2014/03/18 01:02:28 gdt Exp $
 
 BUILDLINK_TREE+=	fribidi
 
 .if !defined(FRIBIDI_BUILDLINK3_MK)
 FRIBIDI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fribidi+=	fribidi>=0.10.4
-BUILDLINK_ABI_DEPENDS.fribidi+=	fribidi>=0.10.4nb1
+BUILDLINK_API_DEPENDS.fribidi+=	fribidi>=0.19.1
 BUILDLINK_PKGSRCDIR.fribidi?=	../../converters/fribidi
 .endif # FRIBIDI_BUILDLINK3_MK
 
