e73e0eaf40ac5f76c67f2ab7ccaa0d9c3dd377c3
gdt <gdt@pkgsrc.org>
2014-03-18 14:41:27 +0000

    Undo change of ABI_DEPENDS for fribidi.
    This commit restores the ABI_DEPENDS line that was there until
    yesterday, 0.10.4nb1.  It's unclear what the right answer is for
    fribidi and ABI_DEPENDS, and if revbumping is needed - so for now
    avoid the issue.

--- converters/fribidi/buildlink3.mk
+++ converters/fribidi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2014/03/18 01:02:28 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2014/03/18 14:41:27 gdt Exp $
 
 BUILDLINK_TREE+=	fribidi
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	fribidi
 FRIBIDI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.fribidi+=	fribidi>=0.19.1
+BUILDLINK_ABI_DEPENDS.fribidi+=	fribidi>=0.10.4nb1
 BUILDLINK_PKGSRCDIR.fribidi?=	../../converters/fribidi
 .endif # FRIBIDI_BUILDLINK3_MK
 
