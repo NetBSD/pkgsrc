d758ad0a3369211d01bbc00532696e4a096c326c
asau <asau@pkgsrc.org>
2014-03-19 22:05:39 +0000

    limits.h is needed for PATH_MAX on FreeBSD too.

--- parallel/slurm/patches/patch-src_plugins_acct__gather__filesystem_lustre_acct__gather__filesystem__lustre.c
+++ parallel/slurm/patches/patch-src_plugins_acct__gather__filesystem_lustre_acct__gather__filesystem__lustre.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_plugins_acct__gather__filesystem_lustre_acct__gather__filesystem__lustre.c,v 1.1 2013/12/06 06:05:30 asau Exp $
+$NetBSD: patch-src_plugins_acct__gather__filesystem_lustre_acct__gather__filesystem__lustre.c,v 1.2 2014/03/19 22:05:39 asau Exp $
 
---- src/plugins/acct_gather_filesystem/lustre/acct_gather_filesystem_lustre.c.orig	2013-11-04 21:19:15.000000000 +0000
+--- src/plugins/acct_gather_filesystem/lustre/acct_gather_filesystem_lustre.c.orig	2013-12-23 19:38:25.000000000 +0000
 +++ src/plugins/acct_gather_filesystem/lustre/acct_gather_filesystem_lustre.c
 @@ -49,6 +49,9 @@
  #include <getopt.h>
  #include <netinet/in.h>
  
-+#if defined(__NetBSD__)
++#if defined(__NetBSD__) || defined(__FreeBSD__)
 +#include <limits.h>	/* PATH_MAX */
 +#endif
  
