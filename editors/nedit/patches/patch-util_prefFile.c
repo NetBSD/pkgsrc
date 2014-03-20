b62307a5dfd5d6109c3035bf6814e52a32b504e3
gdt <gdt>
2014-03-20 12:38:26 +0000

    Include <stdint.h> before using uintptr_t.
    Pointed out by Iain Morgan on pkgsrc-users as causing a build failure
    on Linux.

--- editors/nedit/patches/patch-util_prefFile.c
+++ editors/nedit/patches/patch-util_prefFile.c
@@ -1,10 +1,18 @@
-$NetBSD: patch-util_prefFile.c,v 1.1 2013/04/28 20:35:23 dholland Exp $
+$NetBSD: patch-util_prefFile.c,v 1.2 2014/03/20 12:38:26 gdt Exp $
 
 Silence gcc warnings about unsafe pointer casts.
 
---- util/prefFile.c~	2004-07-21 11:32:07.000000000 +0000
+--- util/prefFile.c.orig	2004-07-21 11:32:07.000000000 +0000
 +++ util/prefFile.c
-@@ -353,9 +353,9 @@ static int stringToPref(const char *stri
+@@ -35,6 +35,7 @@ static const char CVSID[] = "$Id: prefFi
+ #include "fileUtils.h"
+ #include "utils.h"
+ 
++#include <stdint.h>
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <string.h>
+@@ -353,9 +354,9 @@ static int stringToPref(const char *stri
        	*(int *)rsrcDescrip->valueAddr = 0;
      	return False;
        case PREF_STRING:
