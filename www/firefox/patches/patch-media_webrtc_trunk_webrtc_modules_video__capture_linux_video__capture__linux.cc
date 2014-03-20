278f6db5d50b1b9500a1a8d94b0a22ec846e1d4d
ryoon <ryoon@pkgsrc.org>
2014-03-20 21:02:00 +0000

    Update to 28.0
    Changelog:
    NEW
    VP9 video decoding implemented
    NEW
    Mac OS X: Notification Center support for web notifications
    NEW
    Horizontal HTML5 audio/video volume control
    NEW
    Support for Opus in WebM
    CHANGED
    Now that spdy/3 is implemented support for spdy/2 has been removed and servers without spdy/3 will negotiate to http/1 without any penalty
    DEVELOPER
    Support for MathML 2.0 'mathvariant' attribute
    DEVELOPER
    Background thread hang reporting
    DEVELOPER
    Support for multi-line flexbox in layout
    FIXED
    Various security fixes
    
    Fixed in Firefox 28
    MFSA 2014-32 Out-of-bounds write through TypedArrayObject after neutering
    MFSA 2014-31 Out-of-bounds read/write through neutering ArrayBuffer objects
    MFSA 2014-30 Use-after-free in TypeObject
    MFSA 2014-29 Privilege escalation using WebIDL-implemented APIs
    MFSA 2014-28 SVG filters information disclosure through feDisplacementMap
    MFSA 2014-27 Memory corruption in Cairo during PDF font rendering
    MFSA 2014-26 Information disclosure through polygon rendering in MathML
    MFSA 2014-25 Firefox OS DeviceStorageFile object vulnerable to relative path escape
    MFSA 2014-24 Android Crash Reporter open to manipulation
    MFSA 2014-23 Content Security Policy for data: documents not preserved by session restore
    MFSA 2014-22 WebGL content injection from one domain to rendering in another
    MFSA 2014-21 Local file access via Open Link in new tab
    MFSA 2014-20 onbeforeunload and Javascript navigation DOS
    MFSA 2014-19 Spoofing attack on WebRTC permission prompt
    MFSA 2014-18 crypto.generateCRMFRequest does not validate type of key
    MFSA 2014-17 Out of bounds read during WAV file decoding
    MFSA 2014-16 Files extracted during updates are not always read only
    MFSA 2014-15 Miscellaneous memory safety hazards (rv:28.0 / rv:24.4)

--- www/firefox/patches/patch-media_webrtc_trunk_webrtc_modules_video__capture_linux_video__capture__linux.cc
+++ www/firefox/patches/patch-media_webrtc_trunk_webrtc_modules_video__capture_linux_video__capture__linux.cc
@@ -1,9 +1,9 @@
-$NetBSD: patch-media_webrtc_trunk_webrtc_modules_video__capture_linux_video__capture__linux.cc,v 1.5 2014/02/20 13:19:03 ryoon Exp $
+$NetBSD: patch-media_webrtc_trunk_webrtc_modules_video__capture_linux_video__capture__linux.cc,v 1.6 2014/03/20 21:02:00 ryoon Exp $
 
---- media/webrtc/trunk/webrtc/modules/video_capture/linux/video_capture_linux.cc.orig	2013-10-25 22:27:35.000000000 +0000
+--- media/webrtc/trunk/webrtc/modules/video_capture/linux/video_capture_linux.cc.orig	2014-03-15 05:19:30.000000000 +0000
 +++ media/webrtc/trunk/webrtc/modules/video_capture/linux/video_capture_linux.cc
 @@ -18,13 +18,16 @@
- #include <string.h>
+ #include <unistd.h>
  
  //v4l includes
 -#if defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__)
@@ -20,10 +20,19 @@ $NetBSD: patch-media_webrtc_trunk_webrtc_modules_video__capture_linux_video__cap
  
  #include <new>
  
-@@ -34,6 +37,15 @@
- #include "critical_section_wrapper.h"
- #include "video_capture_linux.h"
+@@ -34,6 +37,24 @@
+ #include "webrtc/system_wrappers/interface/thread_wrapper.h"
+ #include "webrtc/system_wrappers/interface/trace.h"
  
++#ifdef HAVE_LIBV4L2
++#define open	v4l2_open
++#define close	v4l2_close
++#define dup	v4l2_dup
++#define ioctl	v4l2_ioctl
++#define mmap	v4l2_mmap
++#define munmap	v4l2_munmap
++#endif
++
 +#ifdef HAVE_LIBV4L2
 +#define open	v4l2_open
 +#define close	v4l2_close
