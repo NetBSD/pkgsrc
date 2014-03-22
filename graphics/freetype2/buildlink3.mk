9af5b72fdce0c8f8fdbe273d5d192182d1cea88e
spz <spz>
2014-03-22 18:21:02 +0000

    Pullup ticket #4350 - requested by tron graphics/freetype2: build fix
    Revisions pulled up:
    - graphics/freetype2/buildlink3.mk                              1.44
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	jperkin
       Date:		Wed Mar 19 10:16:33 UTC 2014
    
       Modified Files:
       	pkgsrc/graphics/freetype2: buildlink3.mk
    
       Log Message:
       Move bzip2 and zlib buildlink3 out of the png PKG_OPTION check, they are
       unconditional dependencies.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.43 -r1.44 pkgsrc/graphics/freetype2/buildlink3.mk

--- graphics/freetype2/buildlink3.mk
+++ graphics/freetype2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.36.6.2 2014/03/17 09:30:55 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.36.6.3 2014/03/22 18:21:02 spz Exp $
 
 BUILDLINK_TREE+=	freetype2
 
@@ -15,6 +15,9 @@ BUILDLINK_FILES.freetype2+=	bin/freetype-config
 FREETYPE_CONFIG?=	${BUILDLINK_PREFIX.freetype2}/bin/freetype-config
 CONFIGURE_ENV+=		FREETYPE_CONFIG=${FREETYPE_CONFIG:Q}
 
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+
 pkgbase := freetype2
 .include "../../mk/pkg-build-options.mk"
 
@@ -23,12 +26,11 @@ CHECK_BUILTIN.freetype2:=	yes
 .include "../../graphics/freetype2/builtin.mk"
 CHECK_BUILTIN.freetype2:=	no
 
-.include "../../archivers/bzip2/buildlink3.mk"
-.include "../../devel/zlib/buildlink3.mk"
 .if empty(USE_BUILTIN.freetype2:M[yY][eE][sS])
 .include "../../graphics/png/buildlink3.mk"
 .endif
 .endif
+
 .endif # FREETYPE2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-freetype2
