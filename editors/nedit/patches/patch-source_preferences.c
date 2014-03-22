54e8d5393449a240c6d9634f86663d815c74fde9
gdt <gdt@pkgsrc.org>
2014-03-22 17:26:12 +0000

    Munge patches using intptr_t to include stdint.h.
    Patch changes are from Iain Morgan.  Beyond that, I have:
      omitted changing patches for line # and ~/orig nits
      used INSTALLATION_DIRS for share/doc/nedit
    
    I sent mail to upstream about the patches.  After all, the distfile is
    just about 9.5 years old, so surely they are due for at least a point
    release.

--- editors/nedit/patches/patch-source_preferences.c
+++ editors/nedit/patches/patch-source_preferences.c
@@ -1,10 +1,18 @@
-$NetBSD: patch-source_preferences.c,v 1.1 2013/04/28 20:35:22 dholland Exp $
+$NetBSD: patch-source_preferences.c,v 1.2 2014/03/22 17:26:13 gdt Exp $
 
 Silence gcc warnings about unsafe pointer casts.
 
---- source/preferences.c~	2004-08-20 19:33:20.000000000 +0000
+--- source/preferences.c.orig	2004-08-20 19:33:20.000000000 +0000
 +++ source/preferences.c
-@@ -2161,7 +2161,7 @@ void SetLanguageMode(WindowInfo *window,
+@@ -59,6 +59,7 @@ static const char CVSID[] = "$Id: prefer
+ #include <string.h>
+ #include <stdio.h>
+ #include <ctype.h>
++#include <stdint.h>
+ #ifdef VMS
+ #include "../util/VMSparam.h"
+ #else
+@@ -2161,7 +2162,7 @@ void SetLanguageMode(WindowInfo *window,
  	XtVaGetValues(menu, XmNchildren, &items, XmNnumChildren, &nItems, NULL);
  	for (n=0; n<(int)nItems; n++) {
      	    XtVaGetValues(items[n], XmNuserData, &userData, NULL);
@@ -13,7 +21,7 @@ Silence gcc warnings about unsafe pointer casts.
  	}
      }
  }
-@@ -4946,7 +4946,7 @@ static void updateLanguageModeSubmenu(Wi
+@@ -4946,7 +4947,7 @@ static void updateLanguageModeSubmenu(Wi
              	xmToggleButtonGadgetClass, menu,
              	XmNlabelString, s1=XmStringCreateSimple(LanguageModes[i]->name),
   	    	XmNmarginHeight, 0,
@@ -22,7 +30,7 @@ Silence gcc warnings about unsafe pointer casts.
      		XmNset, window->languageMode==i, NULL);
          XmStringFree(s1);
  	XtAddCallback(btn, XmNvalueChangedCallback, setLangModeCB, window);
-@@ -4967,14 +4967,14 @@ static void setLangModeCB(Widget w, XtPo
+@@ -4967,14 +4968,14 @@ static void setLangModeCB(Widget w, XtPo
      XtVaGetValues(w, XmNuserData, &mode, NULL);
      
      /* If the mode didn't change, do nothing */
