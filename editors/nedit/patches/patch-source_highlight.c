56ffa9ef03ab70d1a7980eedbc8ecc22132e5cab
gdt <gdt@pkgsrc.org>
2014-03-25 12:54:22 +0000

    Add another missing include of stdint.h, to resolve build on Linux.
    From Iain Morgan (again; this was in the previous set and I missed it
    while pruning changes that just regenerated distfiles without real
    changes).

--- editors/nedit/patches/patch-source_highlight.c
+++ editors/nedit/patches/patch-source_highlight.c
@@ -1,10 +1,18 @@
-$NetBSD: patch-source_highlight.c,v 1.1 2013/04/28 20:35:22 dholland Exp $
+$NetBSD: patch-source_highlight.c,v 1.2 2014/03/25 12:54:22 gdt Exp $
 
 Silence gcc warnings about unsafe pointer casts.
 
---- source/highlight.c~	2004-08-01 10:06:10.000000000 +0000
+--- source/highlight.c.orig	2004-08-01 10:06:10.000000000 +0000
 +++ source/highlight.c
-@@ -510,7 +510,7 @@ void* GetHighlightInfo(WindowInfo *windo
+@@ -50,6 +50,7 @@ static const char CVSID[] = "$Id: highli
+ #include <math.h>
+ #include <stdlib.h>
+ #include <string.h>
++#include <stdint.h>
+ #ifdef VMS
+ #include "../util/VMSparam.h"
+ #else
+@@ -510,7 +511,7 @@ void* GetHighlightInfo(WindowInfo *windo
      if (!pattern) {
  	return NULL;
      }
