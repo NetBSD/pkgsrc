919a9d5201d5b3c994dd29c0973064a84d2a485f
obache <obache@pkgsrc.org>
2014-03-26 12:13:17 +0000

    fixes missing include of string.h for strcat(3) and strcpy(3).

--- inputmethod/scim-fcitx/patches/patch-src_sp.cpp
+++ inputmethod/scim-fcitx/patches/patch-src_sp.cpp
@@ -1,14 +1,16 @@
-$NetBSD: patch-src_sp.cpp,v 1.1 2013/03/25 12:55:04 obache Exp $
+$NetBSD: patch-src_sp.cpp,v 1.2 2014/03/26 12:13:17 obache Exp $
 
 * require unistd.h for access(2)
+* require string.h for strcat(3) and strcpy(3)
 
 --- src/sp.cpp.orig	2005-05-08 16:11:29.000000000 +0000
 +++ src/sp.cpp
-@@ -8,6 +8,7 @@
+@@ -8,6 +8,8 @@
  using namespace scim;
  
  #include <stdio.h>
 +#include <unistd.h>
++#include <string.h>
  #include <sys/stat.h>
  #include <limits.h>
  #include <ctype.h>
