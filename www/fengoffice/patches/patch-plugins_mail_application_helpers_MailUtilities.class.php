eaeafae4d5eaa6a5194348ff241fd8ccbe2a7480
taca <taca@pkgsrc.org>
2014-03-29 14:17:06 +0000

    Update fengoffice to 2.5.1.
    Changelog
    =========
    
    Since 2.5.1-rc
    ----------------
    bugfix: can't view object link in notification when an email does not have a subject.
    
    Since 2.5.1-beta
    ----------------
    bugfix: cannot add milestones in templates
    bugfix: when adding template, after adding milestone cannot select it when adding a task
    
    Since 2.5.0.6
    ----------------
    bugfix: Template view broken by single quote in property name.
    bugfix: when edit a template if have milestones you can't see tasks.
    bugfix: don't show members that cannot be used in member selector.
    
    Since 2.5.0.5
    ----------------
    bugfix: Tasks grouping by dimension fixed.
    
    Since 2.5.0.4
    ----------------
    performance: Issue when loading persons dim.
    bugfix: Imap folders are not saved when editing an email account.
    bugfix: Cannot unclassify mails from classify form.
    bugfix: Emessage not shown when inputing dates with incorrect format.
    bugfix: Add start date to task view.
    bugfix: Get tasks by range query does not include logged user's timezone.
    bugfix: In task complete edition form, assigned to are not displayed correctly.
    bugfix: Issue in include myself in document notifications.
    bugfix: Set db charset when reconnecting in abstract db adapter.
    
    Since 2.5.0.3
    ----------------
    bugfix: Add attachments column in queued_emails in upgrade scripts.
    bugfix: Set db charset when reconnecting in abstract db adapter.
    
    Since 2.5.0.2
    ----------------
    bugfix: Render member selectors with preloaded member info.
    bugfix: Order by name doesn't work on object list.
    bugfix: People widget only display users.
    
    Since 2.5.0.1
    ----------------
    bugfix: on mysql 5.6 have_innodb variable is deprecated
    
    Since 2.5
    ----------------
    
    feature: Allow to configure dashboard widget position and order for each user.
    feature: Allow to configure default dashboard widget position and order for all users.
    feature: Comments dashboard widget.
    feature: Email dashboard widget.
    feature: choose to filter calendar widget or not.
    feature: choose the user to filter the tasks widget.
    
    bugfix: when add a timeslot by clock on tasks update the percent complete.
    bugfix: if a file doesn't have revision when classify create one.
    bugfix: several minor fixes of undefined variables, missing langs, etc.
    bugfix: when disabling or reactivating users from company view, users list is not reloaded.
    bugfix: member selector displayed wrong data
    bugfix: on task add/edit view, assignee combo displayed wrong data
    bugfix: subscribers and invited people were not shown correctly
    bugfix: encoding when receiving emails
    bugfix: when editing a classified timeslot, its context was not shown
    bugfix: in file upload, the name is not changed if a new name is entered
    bugfix: missing langs and sql changes for email user config options
    
    Since 2.5-rc
    ----------------
    
    bugfix: general search form submitted by enter key doesn't work in Google Chrome
    bugfix: links are now saved as such when using WYSIWYG
    bugfix: primary-breadcrumb show exact context
    bugfix: mysql transaction problem when sending emails without using a cronjob
    bugfix: when making a new installation, users were not shown by default
    
    Since 2.5-beta
    ----------------
    
    bugfix: if a file doesn't have a revision, when classifying an email create one.
    bugfix: when adding a timeslot by clock on tasks, task progress bar was not updated correctly.
    bugfix: fixed custom reports using boolean conditions in false.
    bugfix: problems with paging on the overview list.
    bugfix: on activity widget, when clicking on a member, change dimension.

--- www/fengoffice/patches/patch-plugins_mail_application_helpers_MailUtilities.class.php
+++ www/fengoffice/patches/patch-plugins_mail_application_helpers_MailUtilities.class.php
@@ -1,12 +1,12 @@
-$NetBSD: patch-plugins_mail_application_helpers_MailUtilities.class.php,v 1.5 2013/12/20 15:49:33 taca Exp $
+$NetBSD: patch-plugins_mail_application_helpers_MailUtilities.class.php,v 1.6 2014/03/29 14:17:06 taca Exp $
 
 * Fix variable mistakes to prevent garbled characters.  Still needs to
   fix essential problem when a header contains multiple encoded strings
   with diffreent charset.
 
---- plugins/mail/application/helpers/MailUtilities.class.php.orig	2013-12-13 18:33:05.000000000 +0000
+--- plugins/mail/application/helpers/MailUtilities.class.php.orig	2014-02-17 10:24:14.000000000 +0000
 +++ plugins/mail/application/helpers/MailUtilities.class.php
-@@ -274,7 +274,7 @@ class MailUtilities {
+@@ -287,7 +287,7 @@ class MailUtilities {
  			if ($from_name == ''){
  				$from_name = $from;
  			} else if (strtoupper($encoding) =='KOI8-R' || strtoupper($encoding) =='CP866' || $from_encoding != 'UTF-8' || !$enc_conv->isUtf8RegExp($from_name)){ //KOI8-R and CP866 are Russian encodings which PHP does not detect
@@ -15,12 +15,3 @@ $NetBSD: patch-plugins_mail_application_helpers_MailUtilities.class.php,v 1.5 20
  	
  				if ($enc_conv->hasError()) {
  					$utf8_from = utf8_encode($from_name);
-@@ -289,7 +289,7 @@ class MailUtilities {
- 			$subject_encoding = detect_encoding($subject_aux);
- 			
- 			if (strtoupper($encoding) =='KOI8-R' || strtoupper($encoding) =='CP866' || $subject_encoding != 'UTF-8' || !$enc_conv->isUtf8RegExp($subject_aux)){ //KOI8-R and CP866 are Russian encodings which PHP does not detect
--				$utf8_subject = $enc_conv->convert($encoding, 'UTF-8', $subject_aux);
-+                $utf8_subject = $enc_conv->convert($subject_encoding, 'UTF-8', $subject_aux);
- 				
- 				if ($enc_conv->hasError()) {
- 					$utf8_subject = utf8_encode($subject_aux);
