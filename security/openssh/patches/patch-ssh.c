4ac07884a14c4782a3a78e4deca129ba2521231a
taca <taca@pkgsrc.org>
2014-03-29 09:38:11 +0000

    Update openssh to 6.6.1 (OpenSSH 6.6p1).
    pkgsrc change
    
        * Use PLIST_VARS.
        * Update hpn-patch based on openssh-6.5p1-hpnssh14v4.diff.gz.
    
    Fixes security problem (SA57488).
    
    For full changes, please refer below release notes.
    
    	http://www.openssh.com/txt/release-6.5
    	http://www.openssh.com/txt/release-6.6

--- security/openssh/patches/patch-ssh.c
+++ security/openssh/patches/patch-ssh.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-ssh.c,v 1.2 2013/12/01 06:11:41 taca Exp $
+$NetBSD: patch-ssh.c,v 1.3 2014/03/29 09:38:11 taca Exp $
 
 Interix support
 
---- ssh.c.orig	2013-07-25 01:55:53.000000000 +0000
+--- ssh.c.orig	2014-02-26 23:17:13.000000000 +0000
 +++ ssh.c
-@@ -820,7 +820,7 @@ main(int ac, char **av)
- 	if (ssh_connect(host, &hostaddr, options.port,
- 	    options.address_family, options.connection_attempts, &timeout_ms,
- 	    options.tcp_keep_alive, 
--#ifdef HAVE_CYGWIN
+@@ -943,7 +943,7 @@ main(int ac, char **av)
+ 	    strcmp(options.proxy_command, "-") == 0 &&
+ 	    options.proxy_use_fdpass)
+ 		fatal("ProxyCommand=- and ProxyUseFDPass are incompatible");
+-#ifndef HAVE_CYGWIN
 +#if defined(HAVE_CYGWIN) || defined(HAVE_INTERIX)
- 	    options.use_privileged_port,
- #else
- 	    original_effective_uid == 0 && options.use_privileged_port,
+ 	if (original_effective_uid != 0)
+ 		options.use_privileged_port = 0;
+ #endif
