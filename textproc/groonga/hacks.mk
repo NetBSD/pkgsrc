fb1b8c3d32753513bdacf431c1e47b8f2cff6f84
obache <obache@pkgsrc.org>
2014-04-03 07:16:49 +0000

    Change hack for clang with optimization. unlimit datasize (require over 7GB memory on FreeBSD-10.0 (amd64).

--- textproc/groonga/hacks.mk
+++ textproc/groonga/hacks.mk
@@ -1,14 +1,14 @@
-# $NetBSD: hacks.mk,v 1.1 2014/02/25 05:14:37 obache Exp $
+# $NetBSD: hacks.mk,v 1.2 2014/04/03 07:16:49 obache Exp $
 
 .if !defined(GROONGA_HACKS_MK)
 GROONGA_HACKS_MK=	# defined
 
 .include "../../mk/compiler.mk"
 
-# Build without optimization. Avoids crash in expr.c
-.if !empty(MACHINE_PLATFORM:MNetBSD-6.99.[0-9]*-x86_64) && !empty(CC_VERSION:Mclang-*)
+# Build with optimization. Avoids crash in expr.c
+.if !empty(CC_VERSION:Mclang-*) && !empty(CFLAGS:M-O*)
 PKG_HACKS+=	clang-optimization
-BUILDLINK_TRANSFORM+=	rename:-O[0-9]*:-O0
+UNLIMIT_RESOURCES=	datasize
 .endif
 
 .endif
