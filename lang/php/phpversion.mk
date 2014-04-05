191752fe8ebb7648c469f27484d498d5b62a708b
taca <taca@pkgsrc.org>
2014-04-05 03:43:40 +0000

    Update php54 to 5.4.27.  CVE-2013-7345 is already fixed in 5.4.26nb2.
    03 Apr 2014, PHP 5.4.27
    
    - Core:
      . Fixed bug #60602 (proc_open() changes environment array) (Tjerk)
    
    - Fileinfo:
      . Fixed bug #66946 (fileinfo: extensive backtracking in awk rule regular
        expression). (CVE-2013-7345) (Remi)
    
    - FPM:
      . Added clear_env configuration directive to disable clearenv() call.
      (Github PR# 598, Paul Annesley)
    
    - GMP
      . fixed bug#66872 (invalid argument crashes gmp_testbit) (Pierre)
    
    - Mail:
      . Fixed bug #66535 (Don't add newline after X-PHP-Originating-Script) (Tjerk)
    
    - MySQLi:
      . Fixed bug #66762 (Segfault in mysqli_stmt::bind_result() when link closed)
      (Remi)
    
    - Openssl:
      . Fixed bug #66833 (Default disgest algo is still MD5, switch to SHA1). (Remi)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.59 2014/04/04 03:04:59 taca Exp $
+# $NetBSD: phpversion.mk,v 1.60 2014/04/05 03:43:40 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -82,7 +82,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP53_VERSION=	5.3.28
-PHP54_VERSION=	5.4.26
+PHP54_VERSION=	5.4.27
 PHP55_VERSION=	5.5.11
 
 # Define initial release of major version.
