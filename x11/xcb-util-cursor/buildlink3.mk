21e7471fe58149c7508c808f8085bb63be6efac0
obache <obache>
2014-04-06 07:57:57 +0000

    Add buildlink with Requires.private packages.

--- x11/xcb-util-cursor/buildlink3.mk
+++ x11/xcb-util-cursor/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/04/06 06:25:16 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/04/06 07:57:57 obache Exp $
 #
 
 BUILDLINK_TREE+=	xcb-util-cursor
@@ -11,6 +11,8 @@ BUILDLINK_ABI_DEPENDS.xcb-util-cursor+=	xcb-util-cursor>=0.1.1nb1
 BUILDLINK_PKGSRCDIR.xcb-util-cursor?=	../../x11/xcb-util-cursor
 
 .include "../../x11/libxcb/buildlink3.mk"
+.include "../../x11/xcb-util-image/buildlink3.mk"
+.include "../../x11/xcb-util-renderutil/buildlink3.mk"
 .endif	# XCB_UTIL_CURSOR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xcb-util-cursor
