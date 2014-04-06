6f29a5a07a1c271aba6f0c78e140a83abaee5355
obache <obache@pkgsrc.org>
2014-04-06 06:10:28 +0000

    from xcb-image.pc, xcb and xcb-shm is required, so need to buildlink with x11/libxcb.

--- x11/xcb-util-image/buildlink3.mk
+++ x11/xcb-util-image/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/08/27 10:04:31 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/04/06 06:10:28 obache Exp $
 
 BUILDLINK_TREE+=	xcb-util-image
 
@@ -8,7 +8,7 @@ XCB_UTIL_IMAGE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.xcb-util-image+=	xcb-util-image>=0.3.9
 BUILDLINK_PKGSRCDIR.xcb-util-image?=	../../x11/xcb-util-image
 
-#.include "../../x11/libxcb/buildlink3.mk"
+.include "../../x11/libxcb/buildlink3.mk"
 #.include "../../x11/xcb-util/buildlink3.mk"
 .endif	# XCB_UTIL_IMAGE_BUILDLINK3_MK
 
