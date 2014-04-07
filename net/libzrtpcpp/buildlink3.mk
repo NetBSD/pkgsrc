86ccbbfbaa244fc1c67da98c4da42b5da5771b98
rodent <rodent@pkgsrc.org>
2014-04-07 05:34:23 +0000

    Bumping BUILDLINK_ABI_DEPENDS.libzrtpcpp.

--- net/libzrtpcpp/buildlink3.mk
+++ net/libzrtpcpp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/02/12 23:18:22 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/04/07 05:34:23 rodent Exp $
 
 BUILDLINK_TREE+=	libzrtpcpp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libzrtpcpp
 LIBZRTPCPP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libzrtpcpp+=	libzrtpcpp>=0.9.0
-BUILDLINK_ABI_DEPENDS.libzrtpcpp?=	libzrtpcpp>=4.1.2nb1
+BUILDLINK_ABI_DEPENDS.libzrtpcpp?=	libzrtpcpp>=4.2.0
 BUILDLINK_PKGSRCDIR.libzrtpcpp?=	../../net/libzrtpcpp
 
 .include "../../mk/pthread.buildlink3.mk"
