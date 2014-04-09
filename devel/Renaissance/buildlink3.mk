fda76a8c780377525f006113146d9079426bbaa0
obache <obache>
2014-04-09 07:26:56 +0000

    recursive bump from icu shlib major bump.

--- devel/Renaissance/buildlink3.mk
+++ devel/Renaissance/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2013/10/19 09:06:59 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2014/04/09 07:27:01 obache Exp $
 
 BUILDLINK_TREE+=	Renaissance
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	Renaissance
 RENAISSANCE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.Renaissance+=	Renaissance>=0.9.0
-BUILDLINK_ABI_DEPENDS.Renaissance+=	Renaissance>=0.9.0nb4
+BUILDLINK_ABI_DEPENDS.Renaissance+=	Renaissance>=0.9.0nb5
 BUILDLINK_PKGSRCDIR.Renaissance?=	../../devel/Renaissance
 
 .include "../../x11/gnustep-back/buildlink3.mk"
--- sysutils/dbus-sharp/buildlink3.mk
+++ sysutils/dbus-sharp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/10/19 09:06:58 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/04/09 07:27:01 obache Exp $
 
 BUILDLINK_TREE+=	dbus-sharp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	dbus-sharp
 DBUS_SHARP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.dbus-sharp+=	dbus-sharp>=0.7.0
-BUILDLINK_ABI_DEPENDS.dbus-sharp?=	dbus-sharp>=0.7.0nb3
+BUILDLINK_ABI_DEPENDS.dbus-sharp?=	dbus-sharp>=0.7.0nb4
 BUILDLINK_PKGSRCDIR.dbus-sharp?=	../../sysutils/dbus-sharp
 
 .include "../../lang/mono2/buildlink3.mk"
