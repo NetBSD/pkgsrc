cd22932aed8d7315549b72ed7da51f1e482ce546
obache <obache@pkgsrc.org>
2014-04-10 05:39:14 +0000

    recursive bump from x264-devel builld number change (i.e. API change).

--- multimedia/xine-lib/buildlink3.mk
+++ multimedia/xine-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2013/03/16 06:20:45 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2014/04/10 05:39:14 obache Exp $
 
 BUILDLINK_TREE+=	xine-lib
 
@@ -7,7 +7,7 @@ XINE_LIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xine-lib+=	xine-lib>=1rc3c
 BUILDLINK_ABI_DEPENDS.xine-lib+=xine-lib>=1.0.3a
-BUILDLINK_ABI_DEPENDS.xine-lib+=	xine-lib>=1.2.2nb8
+BUILDLINK_ABI_DEPENDS.xine-lib+=	xine-lib>=1.2.4nb3
 BUILDLINK_PKGSRCDIR.xine-lib?=	../../multimedia/xine-lib
 
 .include "../../multimedia/ffmpeg/buildlink3.mk"
--- net/libdlna/buildlink3.mk
+++ net/libdlna/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2013/03/16 06:20:45 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2014/04/10 05:39:14 obache Exp $
 #
 
 BUILDLINK_TREE+=	libdlna
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	libdlna
 LIBDLNA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libdlna+=	libdlna>=0.2.3
-BUILDLINK_ABI_DEPENDS.libdlna+=	libdlna>=0.2.4nb1
+BUILDLINK_ABI_DEPENDS.libdlna+=	libdlna>=0.2.4nb2
 BUILDLINK_PKGSRCDIR.libdlna?=	../../net/libdlna
 
 .include "../../multimedia/ffmpeg/buildlink3.mk"
