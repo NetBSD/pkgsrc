6c71fbf7e8ff670822d6492ace35ef74feded5c4
obache <obache>
2014-04-10 06:21:18 +0000

    recursive bump from ilmbase shlib version change.

--- games/kdegames3/buildlink3.mk
+++ games/kdegames3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.41 2014/02/12 23:17:55 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.42 2014/04/10 06:21:19 obache Exp $
 
 BUILDLINK_TREE+=	kdegames
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kdegames
 KDEGAMES_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kdegames+=	kdegames>=3.5.0nb1
-BUILDLINK_ABI_DEPENDS.kdegames+=	kdegames>=3.5.10nb27
+BUILDLINK_ABI_DEPENDS.kdegames+=	kdegames>=3.5.10nb28
 BUILDLINK_PKGSRCDIR.kdegames?=	../../games/kdegames3
 
 .include "../../x11/kdebase3/buildlink3.mk"
--- graphics/libkipi-kde3/buildlink3.mk
+++ graphics/libkipi-kde3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2014/02/12 23:18:02 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2014/04/10 06:21:19 obache Exp $
 
 BUILDLINK_TREE+=	libkipi
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libkipi
 LIBKIPI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libkipi+=	libkipi>=0.1.5<4.0
-BUILDLINK_ABI_DEPENDS.libkipi+=	libkipi>=0.1.6nb25
+BUILDLINK_ABI_DEPENDS.libkipi+=	libkipi>=0.1.6nb26
 BUILDLINK_PKGSRCDIR.libkipi?=	../../graphics/libkipi-kde3
 
 .include "../../x11/kdelibs3/buildlink3.mk"
