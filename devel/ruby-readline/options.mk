9b6432cbd71324e464379e6b946c10a8cf79f986
taca <taca@pkgsrc.org>
2014-04-12 16:37:46 +0000

    Fix some problems and clean up.
    * RUBY_VER dose not take "19" any more.
    * Make readline PKG_OPTION work as it should be.
    * Always include readline.buildlink3.mk.
    
    Bump PKGREVISION.

--- devel/ruby-readline/options.mk
+++ devel/ruby-readline/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2013/08/21 20:02:50 jperkin Exp $
+# $NetBSD: options.mk,v 1.7 2014/04/12 16:37:46 taca Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ruby
@@ -14,17 +14,19 @@ PKG_SUGGESTED_OPTIONS=	ruby-build-ri-db
 # Since license of Ruby prior to 1.9.3 is incompatible with newer
 # readline's GPL3, enable it with 1.9.3 and lator.
 #
-.if ${RUBY_VER} != 18 && ${RUBY_VER} != 19
+.if ${RUBY_VER} != 18
 PKG_SUGGESTED_OPTIONS+=	readline
 .endif
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mreadline)
-.  include "../../mk/readline.buildlink3.mk"
-.  if ${READLINE_TYPE} == "editline"
+READLINE_DEFAULT?=	readline
+.endif
+.include "../../mk/readline.buildlink3.mk"
+
+.if ${READLINE_TYPE} == "editline"
 CONFIGURE_ARGS+=	--enable-libedit
-.  else
+.else
 CONFIGURE_ARGS+=	--disable-libedit
-.  endif
 .endif
