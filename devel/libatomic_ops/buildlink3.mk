0027782eecd8f834934216d16acd4b0b490b9539
wiz <wiz>
2014-04-16 14:11:31 +0000

    To be on the safe side, put an upper limit on the dependency to avoid matching the previous version number.

--- devel/libatomic_ops/buildlink3.mk
+++ devel/libatomic_ops/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2014/04/16 11:21:32 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2014/04/16 14:11:31 wiz Exp $
 
 BUILDLINK_TREE+=	libatomic_ops
 
 .if !defined(LIBATOMIC_OPS_BUILDLINK3_MK)
 LIBATOMIC_OPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libatomic_ops+=	libatomic_ops>=7.4.0
+BUILDLINK_API_DEPENDS.libatomic_ops+=	libatomic_ops>=7.4.0<2000
 BUILDLINK_PKGSRCDIR.libatomic_ops?=	../../devel/libatomic_ops
 .endif # LIBATOMIC_OPS_BUILDLINK3_MK
 
