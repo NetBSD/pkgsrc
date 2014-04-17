a3648c3d89332eeb32b891be61c4cbb0f994363a
wiz <wiz>
2014-04-17 13:53:47 +0000

    Update to 1.2.5:
    xine-lib (1.2.5) 2014-04-08
      * Update german translation.
      * Add bass downmix support to AC3 decoder.
      * Add experimental YCgCo colorspace support.
      * Add avformat demux plugin.
        - Support for new formats and protocols.
        - Proper rtsp support (with avformat+rtsp://, rtsp+tcp:// and rtsp+http:// mrls).
        - by default native xine demux plugins are probed first.
          This can be overridden in config, by selecting demuxer in mrl (#demux:avformat),
          or by using "avformat+" mrl prefix.
      * Add avio (libavformat) input plugin:
        - Support for new protocols (https://, ftp://, sftp://, ...).
        - Support for seekable http streams (with "avio+http://" mrl).
        - By default native xine input plugins are used. This can be overridden in config or by using "avio+" mrl prefix.
      * Recognise & use HEVC/H.265. (ffmpeg)
      * Add support for HEVC/H.265 to the mpeg-ts demuxer.
      * Add support for HEVC/H.265 and VP9 to the matroska demuxer.
      * Add VP8/VP9 decoder using libvpx.
      * Modernise Quicktime demuxer:
        - Multiple user selectable audio traks.
        - Support AC3, EAC3, vorbis, Purevoice audio.
        - Support ISO fragment media files.
        - More safety on broken files.
        - Slightly faster.
      * Recognise & use ADPCM G.726 and ATRAC3. (ffmpeg/libva)
      * Build fixes.
      * Fix HDMV subtitle channel.
      * FFmpeg compatibility fixes.
      * Some colour format conversion fixes.
      * Fix RealVideo re-ordered PTS: pass through, let the decoder re-order it.
      * Fix some problems with COOK audio.

--- multimedia/xine-lib/patches/patch-src-demuxers-demux_qt.c
+++ multimedia/xine-lib/patches/patch-src-demuxers-demux_qt.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-src-demuxers-demux_qt.c,v 1.1 2011/09/04 14:34:52 joerg Exp $
+$NetBSD: patch-src-demuxers-demux_qt.c,v 1.2 2014/04/17 13:53:47 wiz Exp $
 
---- src/demuxers/demux_qt.c.orig	2011-08-30 10:17:53.000000000 +0000
+--- src/demuxers/demux_qt.c.orig	2014-02-26 15:49:10.000000000 +0000
 +++ src/demuxers/demux_qt.c
-@@ -126,7 +126,7 @@ typedef unsigned int qt_atom;
- #define QTIM_ATOM QT_ATOM('q', 't', 'i', 'm')
+@@ -151,7 +151,7 @@ typedef unsigned int qt_atom;
+ #define TRUN_ATOM QT_ATOM('t', 'r', 'u', 'n')
  
- /* placeholder for cutting and pasting */
+ /* placeholder for cutting and pasting
 -#define _ATOM QT_ATOM('', '', '', '')
 +//#define _ATOM QT_ATOM('', '', '', '')
+ */
  
  #define ATOM_PREAMBLE_SIZE 8
- #define PALETTE_COUNT 256
