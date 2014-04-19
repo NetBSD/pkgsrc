11e4c2dab42519c157cd0fe927d2a589ac801c65
joerg <joerg@pkgsrc.org>
2014-04-19 15:00:37 +0000

    Share patches with lang/classpath-gui. Fix freetype use.

---
+++ lang/classpath/patches/patch-native_jni_gtk-peer_gnu__java__awt__peer__gtk__FreetypeGlyphVector.c
@@ -0,0 +1,16 @@
+$NetBSD: patch-native_jni_gtk-peer_gnu__java__awt__peer__gtk__FreetypeGlyphVector.c,v 1.1 2014/04/19 15:00:37 joerg Exp $
+
+--- native/jni/gtk-peer/gnu_java_awt_peer_gtk_FreetypeGlyphVector.c.orig	2014-04-19 12:53:50.000000000 +0000
++++ native/jni/gtk-peer/gnu_java_awt_peer_gtk_FreetypeGlyphVector.c
+@@ -41,8 +41,9 @@ exception statement from your version. *
+ #include <pango/pango.h>
+ #include <pango/pangoft2.h>
+ #include <pango/pangofc-font.h>
+-#include <freetype/ftglyph.h>
+-#include <freetype/ftoutln.h>
++#include <ft2build.h>
++#include FT_GLYPH_H
++#include FT_OUTLINE_H
+ #include "native_state.h"
+ #include "gdkfont.h"
+ #include "gnu_java_awt_peer_gtk_FreetypeGlyphVector.h"
