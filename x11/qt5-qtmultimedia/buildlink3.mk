f23eb36a78e539ec25180e5bd6b2dc0d10243655
ryoon <ryoon@pkgsrc.org>
2014-04-20 02:27:49 +0000

    Recursive revbump from json-c 0.12 update

--- x11/qt5-qtmultimedia/buildlink3.mk
+++ x11/qt5-qtmultimedia/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2014/04/16 11:22:00 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2014/04/20 02:27:49 ryoon Exp $
 
 BUILDLINK_TREE+=	qt5-qtmultimedia
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qt5-qtmultimedia
 QT5_QTMULTIMEDIA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt5-qtmultimedia+=	qt5-qtmultimedia>=5.2.0
-BUILDLINK_ABI_DEPENDS.qt5-qtmultimedia?=	qt5-qtmultimedia>=5.2.0nb6
+BUILDLINK_ABI_DEPENDS.qt5-qtmultimedia?=	qt5-qtmultimedia>=5.2.0nb7
 BUILDLINK_PKGSRCDIR.qt5-qtmultimedia?=	../../x11/qt5-qtmultimedia
 
 BUILDLINK_INCDIRS.qt5-qtmultimedia+=	qt5/include
