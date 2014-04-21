dbbe37ad2dc9ad12b84b823d1127102cd2bc5f9b
joerg <joerg@pkgsrc.org>
2014-04-21 18:15:32 +0000

    Fix build against readline 6.3.

---
+++ lang/yap/patches/patch-os_pl-rl.c
@@ -0,0 +1,12 @@
+$NetBSD: patch-os_pl-rl.c,v 1.1 2014/04/21 18:15:32 joerg Exp $
+
+--- os/pl-rl.c.orig	2014-04-20 18:59:41.000000000 +0000
++++ os/pl-rl.c
+@@ -103,7 +103,6 @@ extern void add_history(char *);	/* shou
+ 					/* readline.h versions */
+ extern int rl_begin_undo_group(void);	/* delete when conflict arrises! */
+ extern int rl_end_undo_group(void);
+-extern Function *rl_event_hook;
+ #ifndef HAVE_RL_FILENAME_COMPLETION_FUNCTION
+ #define rl_filename_completion_function filename_completion_function
+ extern char *filename_completion_function(const char *, int);
