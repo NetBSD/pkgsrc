687506e2051c84739239c99e72ed0d84bb33fefc
wiz <wiz@pkgsrc.org>
2014-04-21 13:25:41 +0000

    Add libatomic_ops bl3.mk, and bump API depends since only the latest boehm-gc version depends on it, and older ones will fail because libatomic_ops is not installed.

--- devel/boehm-gc/buildlink3.mk
+++ devel/boehm-gc/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/03/20 19:24:10 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2014/04/21 13:25:41 wiz Exp $
 
 BUILDLINK_TREE+=	boehm-gc
 
 .if !defined(BOEHM_GC_BUILDLINK3_MK)
 BOEHM_GC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boehm-gc+=	boehm-gc>=6.3nb1
-BUILDLINK_ABI_DEPENDS.boehm-gc+=	boehm-gc>=6.6nb1
+BUILDLINK_API_DEPENDS.boehm-gc+=	boehm-gc>=7.4
 BUILDLINK_PKGSRCDIR.boehm-gc?=	../../devel/boehm-gc
+
+.include "../../devel/libatomic_ops/buildlink3.mk"
 .endif # BOEHM_GC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-boehm-gc
