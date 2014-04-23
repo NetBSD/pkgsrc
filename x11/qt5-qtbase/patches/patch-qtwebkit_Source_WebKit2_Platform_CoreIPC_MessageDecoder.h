ab216bc7fa3201f5adc13f21a63b1b3c7cc38d0d
adam <adam>
2014-04-23 07:05:31 +0000

    Changes 5.2.1: It provides many improvements over Qt 5.2.0 as well as packages Qt Creator 3.0.1. As a patch release, it does not add new features and remains binary compatible with Qt 5.2.0.

---
+++ x11/qt5-qtbase/patches/patch-qtwebkit_Source_WebKit2_Platform_CoreIPC_MessageDecoder.h
@@ -0,0 +1,22 @@
+$NetBSD: patch-qtwebkit_Source_WebKit2_Platform_CoreIPC_MessageDecoder.h,v 1.1 2014/04/23 07:05:31 adam Exp $
+
+--- qtwebkit/Source/WebKit2/Platform/CoreIPC/MessageDecoder.h.orig	2014-04-22 06:56:09.000000000 +0000
++++ qtwebkit/Source/WebKit2/Platform/CoreIPC/MessageDecoder.h
+@@ -46,7 +46,7 @@ public:
+     bool isSyncMessage() const;
+     bool shouldDispatchMessageWhenWaitingForSyncReply() const;
+ 
+-#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
++#if defined(Q_OS_MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
+     void setImportanceAssertion(PassOwnPtr<ImportanceAssertion>);
+ #endif
+ 
+@@ -57,7 +57,7 @@ private:
+     StringReference m_messageReceiverName;
+     StringReference m_messageName;
+ 
+-#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
++#if defined(Q_OS_MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
+     OwnPtr<ImportanceAssertion> m_importanceAssertion;
+ #endif
+ };
--- x11/qt5-qtwebkit/buildlink3.mk
+++ x11/qt5-qtwebkit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2014/04/20 02:27:50 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2014/04/23 07:05:32 adam Exp $
 
 BUILDLINK_TREE+=	qt5-qtwebkit
 
@@ -21,9 +21,9 @@ BUILDLINK_LIBDIRS.qt5-qtwebkit+=	qt5/plugins
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
 .include "../../textproc/libxslt/buildlink3.mk"
+.if ${OPSYS} != "Darwin"
 .include "../../x11/libXcomposite/buildlink3.mk"
-.include "../../x11/libXrender/buildlink3.mk"
-.include "../../x11/libX11/buildlink3.mk"
+.endif
 .include "../../x11/qt5-qtdeclarative/buildlink3.mk"
 .include "../../x11/qt5-qtlocation/buildlink3.mk"
 .include "../../x11/qt5-qtmultimedia/buildlink3.mk"
