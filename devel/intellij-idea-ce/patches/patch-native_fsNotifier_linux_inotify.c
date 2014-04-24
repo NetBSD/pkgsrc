2207517429f5f0a01e2dd22d54277a93c9bd58c7
ryoon <ryoon@pkgsrc.org>
2014-04-24 16:19:40 +0000

    Update to 13.1.2
    * There is "Unable to load JNA library" runtime error...
    
    Changelog:
    enhancement and bugfixes
    See http://confluence.jetbrains.com/display/IDEADEV/IntelliJ+IDEA+13.1.2+Release+Notes

--- devel/intellij-idea-ce/patches/patch-native_fsNotifier_linux_inotify.c
+++ devel/intellij-idea-ce/patches/patch-native_fsNotifier_linux_inotify.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-native_fsNotifier_linux_inotify.c,v 1.1 2013/11/24 10:44:34 ryoon Exp $
+$NetBSD: patch-native_fsNotifier_linux_inotify.c,v 1.2 2014/04/24 16:19:40 ryoon Exp $
 
 * Non-Linux (*BSD?) support
 
---- native/fsNotifier/linux/inotify.c.orig	2013-11-23 23:48:48.000000000 +0000
+--- native/fsNotifier/linux/inotify.c.orig	2013-11-21 22:26:48.000000000 +0000
 +++ native/fsNotifier/linux/inotify.c
 @@ -18,7 +18,11 @@
  
@@ -16,3 +16,17 @@ $NetBSD: patch-native_fsNotifier_linux_inotify.c,v 1.1 2013/11/24 10:44:34 ryoon
  #include <stdio.h>
  #include <stdlib.h>
  #include <string.h>
+@@ -27,11 +31,13 @@
+ #include <syslog.h>
+ #include <unistd.h>
+ 
++#if defined(__linux__)
+ #ifdef __amd64__
+ __asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
+ #else
+ __asm__(".symver memcpy,memcpy@GLIBC_2.0");
+ #endif
++#endif
+ 
+ 
+ #define WATCH_COUNT_NAME "/proc/sys/fs/inotify/max_user_watches"
