c20af03278b05ba8690a5fa11670b31182ac538c
rodent <rodent@pkgsrc.org>
2014-04-27 01:28:01 +0000

    Update to latest stable release, 1.4.25. Some changes to files/haproxy.sh:
    * Don't hardcode PREFIX nor PKG_SYSCONFDIR;
    * Add a configtest() function to test the configuration file before restart
      so lazy SysOps (me) don't have to remember command arguments;
    
    From CHANGELOG:
    
        - DOC: typo: nosepoll self reference in config guide
        - BUG/MINOR: deinit: free fdinfo while doing cleanup
        - BUG/MEDIUM: server: set the macro for server's max weight SRV_UWGHT_MAX to SRV_UWGHT_RANGE
        - BUG/MINOR: use the same check condition for server as other algorithms
        - BUG/MINOR: stream-int: also consider ENOTCONN in addition to EAGAIN for recv()
        - BUG/MINOR: fix forcing fastinter in "on-error"
        - BUG/MEDIUM: http/auth: Sometimes the authentication credentials can be mix between two requests
        - BUG/MAJOR: http: don't emit the send-name-header when no server is available
        - BUG/MEDIUM: http: "option checkcache" fails with the no-cache header
        - MEDIUM: session: disable lingering on the server when the client aborts
        - MINOR: config: warn when a server with no specific port uses rdp-cookie
        - MEDIUM: increase chunk-size limit to 2GB-1
        - DOC: add a mention about the limited chunk size
        - MEDIUM: http: add "redirect scheme" to ease HTTP to HTTPS redirection
        - BUILD: proto_tcp: remove a harmless warning
        - BUG/MINOR: acl: remove patterns from the tree before freeing them
        - BUG/MEDIUM: checks: fix slow start regression after fix attempt
        - BUG/MAJOR: server: weight calculation fails for map-based algorithms
        - BUG/MINOR: backend: fix target address retrieval in transparent mode
        - BUG/MEDIUM: stick: completely remove the unused flag from the store entries
        - BUG/MEDIUM: stick-tables: complete the latest fix about store-responses
        - BUG/MEDIUM: checks: tracking servers must not inherit the MAINT flag
        - BUG/MINOR: stats: report correct throttling percentage for servers in slowstart
        - BUG/MINOR: stats: correctly report throttle rate of low weight servers
        - BUG/MINOR: checks: successful check completion must not re-enable MAINT servers
        - BUG/MEDIUM: stats: the web interface must check the tracked servers before enabling
        - BUG/MINOR: channel: initialize xfer_small/xfer_large on new buffers
        - BUG/MINOR: stream-int: also consider ENOTCONN in addition to EAGAIN
        - BUG/MEDIUM: http: don't start to forward request data before the connect
        - DOC: fix misleading information about SIGQUIT
        - BUILD: simplify the date and version retrieval in the makefile
        - BUILD: prepare the makefile to skip format lines in SUBVERS and VERDATE
        - BUILD: use format tags in VERDATE and SUBVERS files

--- net/haproxy/files/haproxy.sh
+++ net/haproxy/files/haproxy.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: haproxy.sh,v 1.1.1.1 2011/10/31 23:34:07 morr Exp $
+# $NetBSD: haproxy.sh,v 1.2 2014/04/27 01:28:01 rodent Exp $
 #
 
 # PROVIDE: haproxy
@@ -10,10 +10,21 @@ $_rc_subr_loaded . /etc/rc.subr
 
 name="haproxy"
 rcvar=$name
-command="/usr/pkg/sbin/${name}"
-conf_file="/usr/pkg/etc/${name}.cfg"
+command="@PREFIX@/sbin/${name}"
+conf_file="@PKG_SYSCONFDIR@/${name}.cfg"
 required_files="${conf_file}"
-command_args="-f $conf_file"
+command_args="-f ${conf_file}"
+extra_commands="configtest"
+configtest_cmd="haproxy_configtest"
+
+haproxy_configtest()
+{
+	if [ ! -f ${conf_file} ]; then
+		warn "${conf_file} does not exist."
+		return 1;
+	fi
+	${command} -c -f ${conf_file}
+}
 
 load_rc_config $name
 run_rc_command "$1"
