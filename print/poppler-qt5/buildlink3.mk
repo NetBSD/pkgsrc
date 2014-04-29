4f63509dddd3e24a55903ceb1605748b1da4d3d5
wiz <wiz@pkgsrc.org>
2014-04-29 08:05:13 +0000

    Import poppler-qt5-0.26.0 as print/poppler-qt5, based on wip package by nros.
    This is the QT5 wrapper for poppler, a PDF rendering library. It
    is a fork of the xpdf 3.0 PDF viewer developed by Derek Noonburg
    of Glyph and Cog, LLC.
    
    Poppler provides PDF rendering functionality as a shared library,
    to centralize the maintenence effort and to reduce duplicated
    effort. And the poppler developers would like to move libpoppler
    forward in a number of areas that doesn't fit within the goals of
    xpdf.

---
+++ print/poppler-qt5/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/04/29 08:05:13 wiz Exp $
+
+BUILDLINK_TREE+=	poppler-qt5
+
+.if !defined(POPPLER_QT5_BUILDLINK3_MK)
+POPPLER_QT5_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.poppler-qt5+=	poppler-qt5>=0.26.0
+BUILDLINK_PKGSRCDIR.poppler-qt5?=	../../print/poppler-qt5
+
+.include "../../print/poppler/buildlink3.mk"
+.endif # POPPLER_QT5_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-poppler-qt5
