4a8a67b02fb850ceed5379aafdee42a5b1ea347e
obache <obache@pkgsrc.org>
2014-04-29 06:30:26 +0000

    No reference to header files from neither glproto nor libdrm, just libpciaccess.

--- x11/xf86driproto/buildlink3.mk
+++ x11/xf86driproto/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/04/06 13:46:36 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/04/29 06:30:26 obache Exp $
 
 BUILDLINK_TREE+=	xf86driproto
 
@@ -10,8 +10,8 @@ BUILDLINK_DEPMETHOD.xf86driproto?=	build
 BUILDLINK_API_DEPENDS.xf86driproto+=	xf86driproto>=2.0.3
 BUILDLINK_PKGSRCDIR.xf86driproto?=	../../x11/xf86driproto
 
-.include "../../x11/glproto/buildlink3.mk"
-.include "../../x11/libdrm/buildlink3.mk"
+BUILDLINK_DEPMETHOD.libpciaccess?=	build
+.include "../../sysutils/libpciaccess/buildlink3.mk"
 .endif # XF86DRIPROTO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xf86driproto
