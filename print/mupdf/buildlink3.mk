9cb35494958da486632c0af3170167b973fa9f0b
degroote <degroote@pkgsrc.org>
2014-04-29 22:02:21 +0000

    Fix dependencies registered by mupdf buildlink3.mk
    Patch has been provided by Leonardo Taccari on #netbsd @ freenode.
    Nothing seems to use it at the moment so don't bump revision.

--- print/mupdf/buildlink3.mk
+++ print/mupdf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/01/09 14:25:23 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/04/29 22:02:21 degroote Exp $
 
 BUILDLINK_TREE+=	mupdf
 
@@ -8,6 +8,9 @@ MUPDF_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.mupdf+=	mupdf>=1.3
 BUILDLINK_PKGSRCDIR.mupdf?=	../../print/mupdf
 
+.include "../../graphics/jbig2dec/buildlink3.mk"
+.include "../../graphics/openjpeg/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
 .endif	# MUPDF_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mupdf
