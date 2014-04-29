0fd8a6f430ddc6ac223eb3c4748a04fd01696c65
wiz <wiz@pkgsrc.org>
2014-04-29 07:07:26 +0000

    Update to 1.7.0:
    Version 1.7.0
      - Allow user/group names up to 32 characters before clipping.
      - Made -i compress XML and JSON output as much as possible by eliminating
        extraneous whitespace.
      - Added --caseinsensitive (renamed --ignore-case ala grep) flag so patterns
        match without regard to case, courtesy of Jason A Donenfeld.
      - Added --matchdirs option courtesy of Brian Mattern & Jason A. Donenfeld
        <Jason@zx2c4.com>.
      - Fixed possible buffer overflow on large uid/gids w/o user names/group
        names (Alexandre Wendling <alexandrerw@celepar.pr.gov.br>)
      - Added JSON support courtesy of Florian Sesser <fs@it-agenten.com>.
      - Fixed formatting error with HTML output when -L 1 specified. (Sascha Zorn
        <sascha.zorn@gmail.com>)
      - Added file size sorting (Philipp M?ller <philippausmuensing@googlemail.com>)
      - Added '--sort[=]<name>' option, ala ls.
      - Fixed OS X makefile problems (Ryan Hollis <theryanhollis@gmail.com>)
      - Fixed possible memory overflow in read_dir (path/lbuf not equal in size
        to pathsize/lbufsize.) (Han Hui <hanhui03@163.com>)
      - Fix S_ISDOOR/S_IFDOOR spelling mistake for Solaris. (Tim Mooney
        <Tim.Mooney@ndsu.edu>)
      - Make tree more reliably detect UTF-8 locales. (Mantas Mikulnas
        <grawity@gmail.com> and others.)
      - Return non-zero exit status on option errors, print usage to stdout when
        not an error, add the posix '--' option terminator, Change -S description
        to mean CP437 (console) output codes, not ASCII. (Ivan Shmakov
        <oneingray@gmail.com>)

--- sysutils/tree/patches/patch-tree.c
+++ sysutils/tree/patches/patch-tree.c
@@ -1,16 +1,15 @@
-$NetBSD: patch-tree.c,v 1.2 2013/08/25 14:29:52 richard Exp $
+$NetBSD: patch-tree.c,v 1.3 2014/04/29 07:07:26 wiz Exp $
 
-* ensure that both S_ISPORT and S_ISDOOR are defined prior to ifmt
-* declaration.
-*
---- tree.c.orig	2011-06-24 14:26:30.000000000 +0000
+Check for the symbol we're using.
+
+--- tree.c.orig	2014-04-23 21:38:24.000000000 +0000
 +++ tree.c
-@@ -45,7 +45,7 @@ int mb_cur_max;
- #ifdef __EMX__
- const u_short ifmt[]={ FILE_ARCHIVED, FILE_DIRECTORY, FILE_SYSTEM, FILE_HIDDEN, FILE_READONLY, 0};
- #else
--  #ifdef S_ISPORT
-+  #if defined(S_ISPORT) && defined(S_ISDOOR)
-   const u_int ifmt[] = {S_IFREG, S_IFDIR, S_IFLNK, S_IFCHR, S_IFBLK, S_IFSOCK, S_IFIFO, S_ISDOOR, S_ISPORT, 0};
-   const char fmt[] = "-dlcbspDP?";
-   const char *ftype[] = {"file", "directory", "link", "char", "block", "socket", "fifo", "door", "port", "unknown", NULL};
+@@ -1258,7 +1258,7 @@ char Ftype(mode_t mode)
+   else if (m == S_IFIFO) return '|';
+   else if (m == S_IFLNK) return '@'; /* Here, but never actually used though. */
+ #ifdef S_IFDOOR
+-  else if (m == S_ISDOOR) return '>';
++  else if (m == S_IFDOOR) return '>';
+ #endif
+   else if ((m == S_IFREG) && (mode & (S_IXUSR | S_IXGRP | S_IXOTH))) return '*';
+   return 0;
