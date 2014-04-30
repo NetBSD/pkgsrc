b66a8ccfa4c6b32cd5077fc06107bb9ab679dd97
marino <marino>
2014-04-30 16:24:39 +0000

    lang/gcc-aux: Change base of gcc 4.7.3 to gcc 4.9.0
    It appears that gcc-aux is the first gcc 4.9.0 in pkgsrc.  It's purpose
    is to build the Ada packages in pkgsrc, but it also supports C, C++,
    Objective-C, and Fortran languages by default.  GCC 4.9 was released on
    22 April 2014.
    
    Of note, GCC 4.9 fully supports the new Ada-2012 standard.  The rest of
    the changes since GCC 4.8.2 are too numerous to mention, but a decent
    summary can be found here: http://gcc.gnu.org/gcc-4.9/changes.html
    
    FreeBSD support was added, but SunOS support was temporarily disabled.
    This is due to all new bootstraps being provided, but the SunOS
    bootstrap is not yet ready.  Currently supported platforms are
    
     * NetBSD    i386 and x86-64
     * DragonFly i386 and x86-64
     * FreeBSD   i386 and x86-64
    
    All platform fully pass the Ada testsuite:
    
     * http://www.dragonlace.net/gnataux/netbsd32/
     * http://www.dragonlace.net/gnataux/netbsd64/
     * http://www.dragonlace.net/gnataux/dragonfly32/
     * http://www.dragonlace.net/gnataux/dragonfly64/
     * http://www.dragonlace.net/gnataux/freebsd32/
     * http://www.dragonlace.net/gnataux/freebsd64/

--- lang/gcc-aux/buildlink3.mk
+++ lang/gcc-aux/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2012/07/16 19:34:27 marino Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/04/30 16:24:39 marino Exp $
 
 BUILDLINK_TREE+= gcc-aux
 
 .if !defined(GCC_AUX_BUILDLINK3_MK)
 GCC_AUX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gcc-aux+= gcc-aux>=20120614
+BUILDLINK_API_DEPENDS.gcc-aux+= gcc-aux>=20140422
 BUILDLINK_PKGSRCDIR.gcc-aux?= ../../lang/gcc-aux
 
 .include "../../converters/libiconv/buildlink3.mk"
