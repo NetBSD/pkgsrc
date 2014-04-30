ee64cba3e9e19478de8a7a282656152efa401e38
obache <obache@pkgsrc.org>
2014-04-30 05:45:03 +0000

    Note upstream Bug#.

--- print/poppler/patches/patch-glib_poppler-structure-element.h
+++ print/poppler/patches/patch-glib_poppler-structure-element.h
@@ -1,6 +1,7 @@
-$NetBSD: patch-glib_poppler-structure-element.h,v 1.1 2014/04/30 05:32:18 obache Exp $
+$NetBSD: patch-glib_poppler-structure-element.h,v 1.2 2014/04/30 05:45:03 obache Exp $
 
 * PopplerTextSpan is already defined in poppler.h.
+  https://bugs.freedesktop.org/show_bug.cgi?id=78103
 
 --- glib/poppler-structure-element.h.orig	2014-03-11 19:48:54.000000000 +0000
 +++ glib/poppler-structure-element.h
