1be973f1d84a81e160769275305e1d69bb07e243
marino <marino@pkgsrc.org>
2014-04-30 16:28:09 +0000

    devel/gprbuild-aux: Update version GPL 2012 to GPL 2013
    GPRBuild needs to be built with GNAT sources, so it now depends on
    the new library lang/gnat_util.  It installs two new programs:
    gprinstall and gprslave.  This is the latest version published by
    Adacore.

--- devel/gprbuild-aux/buildlink3.mk
+++ devel/gprbuild-aux/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/07/08 20:08:42 marino Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/04/30 16:28:09 marino Exp $
 
 BUILDLINK_TREE+=	gprbuild-aux
 
 .if !defined(GPRBUILD_AUX_BUILDLINK3_MK)
 GPRBUILD_AUX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gprbuild-aux+=	gprbuild-aux>=20120510
+BUILDLINK_API_DEPENDS.gprbuild-aux+=	gprbuild-aux>=20130416
 BUILDLINK_PKGSRCDIR.gprbuild-aux?=	../../devel/gprbuild-aux
 
 .include "../../textproc/xmlada/buildlink3.mk"
