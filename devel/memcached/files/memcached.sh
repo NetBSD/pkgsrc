6e0614c3512b3eb3e00a850051f135fa9ec0300f
rodent <rodent@pkgsrc.org>
2014-04-30 05:39:19 +0000

    Make rc.d init script work more like www/spawn-fcgi to facilitate the execution of multiple memcached instances. Create home directory in VARBASE/chroot/memcached for these PID files. Add MESSAGE to explain usage to user. Usage is simple - set the following in rc.conf:
    memcached=YES
    memcached_jobs="job0 ... jobN"
    memcached_job0_args="-p 11211 -m 64 -c 1024"
    ...
    memcached_jobN_args="-p 1121N -m 64 -c 1024"
    
    Bump PKGREVISION.

--- devel/memcached/files/memcached.sh
+++ devel/memcached/files/memcached.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: memcached.sh,v 1.1 2010/02/27 20:40:52 morr Exp $
+# $NetBSD: memcached.sh,v 1.2 2014/04/30 05:39:19 rodent Exp $
 #
 
 # PROVIDE: memcached
@@ -11,7 +11,74 @@ $_rc_subr_loaded . /etc/rc.subr
 name="memcached"
 rcvar=$name
 command="@PREFIX@/bin/${name}"
-command_args="-d -u @MEMCACHED_USER@"
+command_args="-d"
+start_cmd="memcached_start"
+stop_cmd="memcached_stop"
+status_cmd="memcached_status"
+pidfile_base="@PKG_HOME@/memcached-"
+
+memcached_pid()
+{
+	pidfile="${pidfile_base}$1.pid"
+	rc_pid=`check_pidfile "$pidfile" $command`
+}
+
+memcached_start()
+{
+	rv=0
+	for job in "" $memcached_jobs; do
+		[ -z $job ] && continue
+		memcached_pid $job
+		if [ -n "${rc_pid}" ]; then
+			echo "${name}/$job already running (pid=${rc_pid})."
+			rv=1
+			continue
+		fi
+		job_args=$(eval echo \$${name}_${job}_args)
+
+		echo "Starting ${name}/$job."
+		$command \
+			-u @MEMCACHED_USER@ \
+			$command_args \
+			${job_args} \
+			-P "${pidfile_base}${job}.pid"
+	done
+	return $rv
+}
+
+memcached_stop()
+{
+	rv=0
+	for job in "" $memcached_jobs; do
+		[ -z $job ] && continue
+		memcached_pid $job
+		if [ -n "${rc_pid}" ]; then
+			echo "Stopping ${name}/$job."
+			kill -TERM ${rc_pid}
+			wait_for_pids ${rc_pid}
+		else
+			echo "${name}/$job is not running."
+			rv=1
+		fi
+	done
+	return $rv
+}
+
+memcached_status()
+{
+	rv=0
+	for job in "" $memcached_jobs; do
+		[ -z $job ] && continue
+		memcached_pid $job
+		if [ -n "${rc_pid}" ]; then
+			echo "${name}/${job} is running as pid ${rc_pid}."
+		else
+			echo "${name}/${job} is not running."
+			rv=1
+		fi
+	done
+	return $rv
+}
 
 load_rc_config $name
 run_rc_command "$1"
