02041c2428ab92acaebfe8e20d939cb33b06b6e1
obache <obache@pkgsrc.org>
2014-05-03 07:14:45 +0000

    Change to upstream way used in other places. Note reported to upstream as Issue#3.

--- converters/pyzy/patches/patch-src_Database.h
+++ converters/pyzy/patches/patch-src_Database.h
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_Database.h,v 1.1 2013/11/22 15:43:05 dholland Exp $
+$NetBSD: patch-src_Database.h,v 1.2 2014/05/03 07:14:45 obache Exp $
 
-Work around C++ build error with gcc 4.5.
+* resolve ambiguous operator, changed to same way used in other place.
+  http://code.google.com/p/pyzy/issues/detail?id=3
 
 --- src/Database.h.orig	2012-07-13 02:46:33.000000000 +0000
 +++ src/Database.h
@@ -9,7 +10,7 @@ Work around C++ build error with gcc 4.5.
      static Database & instance (void)
      {
 -        if (m_instance == NULL) {
-+        if (!m_instance) {
++        if (m_instance.get () == NULL) {
              g_error ("Error: Please call InputContext::init () !");
          }
          return *m_instance;
