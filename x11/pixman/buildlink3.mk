67690c7de55508e5cb98c3c4e69fde0dd3113882
ryoon <ryoon@pkgsrc.org>
2014-05-04 22:06:34 +0000

    Bump BUILDLINK_{API,ABI}_DEPENDS
    * pixman_region32_clear() is implemented in 0.25.2 or later.
      It is required by firefox-29.0.

--- x11/pixman/buildlink3.mk
+++ x11/pixman/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2010/09/14 19:07:00 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/05/04 22:06:34 ryoon Exp $
 
 BUILDLINK_TREE+=	pixman
 
 .if !defined(PIXMAN_BUILDLINK3_MK)
 PIXMAN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.pixman+=	pixman>=0.18.4
-BUILDLINK_ABI_DEPENDS.pixman+=	pixman>=0.18.4
+BUILDLINK_API_DEPENDS.pixman+=	pixman>=0.25.2
+BUILDLINK_ABI_DEPENDS.pixman+=	pixman>=0.25.2
 BUILDLINK_PKGSRCDIR.pixman?=	../../x11/pixman
 .endif # PIXMAN_BUILDLINK3_MK
 
