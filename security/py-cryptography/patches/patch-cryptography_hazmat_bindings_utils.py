9c1164dc8acd5362614d2df2d54790a245b3d251
wiz <wiz@pkgsrc.org>
2014-05-06 10:30:54 +0000

    Update to 0.4:
    0.4 - 2014-05-03
    ~~~~~~~~~~~~~~~~
    
    * Deprecated ``salt_length`` on
      :class:`~cryptography.hazmat.primitives.asymmetric.padding.MGF1` and added it
      to :class:`~cryptography.hazmat.primitives.asymmetric.padding.PSS`. It will be
      removed from ``MGF1`` in two releases per our :doc:`/api-stability` policy.
    * Added :class:`~cryptography.hazmat.primitives.ciphers.algorithms.SEED` support.
    * Added :class:`~cryptography.hazmat.primitives.cmac.CMAC`.
    * Added decryption support to
      :class:`~cryptography.hazmat.primitives.asymmetric.rsa.RSAPrivateKey`
      and encryption support to
      :class:`~cryptography.hazmat.primitives.asymmetric.rsa.RSAPublicKey`.
    * Added signature support to
      :class:`~cryptography.hazmat.primitives.asymmetric.dsa.DSAPrivateKey`
      and verification support to
      :class:`~cryptography.hazmat.primitives.asymmetric.dsa.DSAPublicKey`.
    
    0.3 - 2014-03-27
    ~~~~~~~~~~~~~~~~
    
    * Added :class:`~cryptography.hazmat.primitives.twofactor.hotp.HOTP`.
    * Added :class:`~cryptography.hazmat.primitives.twofactor.totp.TOTP`.
    * Added :class:`~cryptography.hazmat.primitives.ciphers.algorithms.IDEA` support.
    * Added signature support to
      :class:`~cryptography.hazmat.primitives.asymmetric.rsa.RSAPrivateKey`
      and verification support to
      :class:`~cryptography.hazmat.primitives.asymmetric.rsa.RSAPublicKey`.
    * Moved test vectors to the new ``cryptography_vectors`` package.

--- security/py-cryptography/patches/patch-cryptography_hazmat_bindings_utils.py
+++ security/py-cryptography/patches/patch-cryptography_hazmat_bindings_utils.py
@@ -1,14 +1,15 @@
-$NetBSD: patch-cryptography_hazmat_bindings_utils.py,v 1.1 2014/03/21 14:02:45 wiz Exp $
+$NetBSD: patch-cryptography_hazmat_bindings_utils.py,v 1.2 2014/05/06 10:30:54 wiz Exp $
 
 Fix soname so cffi doesn't invent one for each python version + platform.
 
---- cryptography/hazmat/bindings/utils.py.orig	2014-03-04 00:51:26.000000000 +0000
+--- cryptography/hazmat/bindings/utils.py.orig	2014-05-03 14:19:18.000000000 +0000
 +++ cryptography/hazmat/bindings/utils.py
-@@ -69,6 +69,7 @@ def build_ffi(module_prefix, modules, pr
-             customizations
-         ),
-         libraries=libraries,
-+        modulename=module_name,
-         ext_package="cryptography",
+@@ -73,7 +73,7 @@ def build_ffi(module_prefix, modules, pr
      )
- 
+     lib = ffi.verify(
+         source=source,
+-        modulename=_create_modulename(cdef_sources, source, sys.version),
++        modulename=module_name,
+         libraries=libraries,
+         ext_package="cryptography",
+         extra_compile_args=extra_compile_args,
