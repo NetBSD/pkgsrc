85b629542a62942144335a500150b1c2e0d015d6
marino <marino>
2014-05-09 09:50:47 +0000

    lang/gcc-aux: Primarily add Solaris i386 and x86_64 targets
    I was never able to build gcc-aux with multilib support when it was
    based on gcc 4.7.  It turns out that this is because Ada support for
    multilib was broken per GCC's bugzilla.  It's been fixed since.  After
    some significant effort and many hours, I finally produced a multilib
    bootstrap for Solaris.  It can produce other multilib compilers (x86-64)
    and it can produce 32-bit only compilers (i386).  This was tested on a
    stock OmniOS x86-64 installation and the Joyent dev area (i386).
    
    Solaris has not supported static linking since Solaris 9, so it is not
    possible to create a static bootstrap like the BSDs have, nor is it
    possible to build the compiler statically, so that option is disabled.
    That means it is unlikely that the bootstrap will run on Solaris 10 or
    Solaris 11, but this theory has not been tested.
    
    Much of the changes to the diff-* patchsets are a result of OpenBSD work.
    An OpenBSD static bootstrap has been created against devel/binutils, and
    as a result it fails to find system libraries such as libc and libm. That
    is a side note explaining the OpenBSD additions.  There are some DragonFly
    and NetBSD changes in the diff-* patches though.
    
    The bootstrap target was significantly modified, mainly to capture the
    special needs of creating a Solaris bootstrap.  The creation of the
    bootstrap tarball was put in it's own target.  The zlib and math libraries
    were connected to the bootstrap option.  It's not a good idea to use
    system zlib on Illumos because the zlib header is not guaranteed to be
    installed.
    
    The feature to rebuild the compiler with the installed gcc-aux was fixed;
    it should use it's own c++ compiler instead of the system c++ compiler.
    
    OpenBSD 5.5 does not support "cp -a", so this was changed to "cp -RpP" for
    portability reasons.

--- lang/gcc-aux/options.mk
+++ lang/gcc-aux/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2014/04/30 16:24:39 marino Exp $
+# $NetBSD: options.mk,v 1.3 2014/05/09 09:50:47 marino Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gcc-aux
 PKG_SUPPORTED_OPTIONS=  fortran objc testsuite nls static bootstrap
@@ -20,7 +20,7 @@ EXTRA_CONFARGS+= --disable-libquadmath
 .else
 LANGS+= fortran
 APPLY_DIFFS+= fortran
-.if ${OPSYS} == "NetBSD"
+.if ${OPSYS} == NetBSD
 EXTRA_CONFARGS+= --disable-libquadmath
 .else
 EXTRA_CONFARGS+= --enable-libquadmath
@@ -72,7 +72,11 @@ EXTRA_CONFARGS+= --disable-nls
 ###############################
 
 .if !empty(PKG_OPTIONS:Mstatic) && empty(PKG_OPTIONS:Mbootstrap)
+.  if ${OPSYS} == SunOS
+PKG_FAIL_REASON+= SunOS does not support static builds
+.  else
 EXTRA_CONFARGS+= --with-stage1-ldflags=-static
+.  endif
 .endif
 
 
@@ -80,11 +84,34 @@ EXTRA_CONFARGS+= --with-stage1-ldflags=-static
 ##  BOOTSTRAP COMPILER OPTION  ##
 #################################
 
+# Solaris does not support static linking, so bootstraps on SunOS require
+# gmp&co to be built with gcc.  In addition, the multilib-bootstrap compiler
+# will choke on the 32ELF gmp&co libraries on i836 targets.  It's extra work
+# but always rebuilding these libraries solves both issues on SunOS.
+
+.if ${OPSYS} == SunOS && !empty(PKG_OPTIONS:Mbootstrap)
+.include "../../devel/gmp/inplace.mk"
+.include "../../math/mpcomplex/inplace.mk"
+.include "../../math/mpfr/inplace.mk"
+.else
+EXTRA_CONFARGS+= --with-gmp=${BUILDLINK_PREFIX.gmp}
+EXTRA_CONFARGS+= --with-mpfr=${BUILDLINK_PREFIX.mpfr}
+EXTRA_CONFARGS+= --with-mpc=${BUILDLINK_PREFIX.mpcomplex}
+.include "../../devel/gmp/buildlink3.mk"
+.include "../../math/mpfr/buildlink3.mk"
+.include "../../math/mpcomplex/buildlink3.mk"
+.endif
+
 .if !empty(PKG_OPTIONS:Mbootstrap)
 EXTRA_CONFARGS+= --disable-shared --disable-lto
+EXTRA_CONFARGS+= --disable-libstdcxx-pch
+.  if ${OPSYS} != SunOS
 EXTRA_CONFARGS+= --with-stage1-ldflags=-static
 EXTRA_CONFARGS+= --with-boot-ldflags=-static
+EXTRA_CONFARGS+= --with-system-zlib
+.  endif
 .else
 EXTRA_CONFARGS+= --enable-shared
 EXTRA_CONFARGS+= --disable-bootstrap
+
 .endif
