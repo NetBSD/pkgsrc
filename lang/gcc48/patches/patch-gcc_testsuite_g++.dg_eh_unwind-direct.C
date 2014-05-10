712e3807c298e0835a76de6f2f45333db2c03c53
richard <richard@pkgsrc.org>
2014-05-10 14:14:58 +0000

    x86-64 sunos fix for g++ and -Wl,-Bdirect involving libtool references:http://debbugs.gnu.org/cgi/bugreport.cgi?bug=16452 and http://gcc.gnu.org/bugzilla/show_bug.cgi?id=59788
    backport to gcc47 and gcc48

---
+++ lang/gcc48/patches/patch-gcc_testsuite_g++.dg_eh_unwind-direct.C
@@ -0,0 +1,20 @@
+$NetBSD: patch-gcc_testsuite_g++.dg_eh_unwind-direct.C,v 1.1 2014/05/10 14:14:59 richard Exp $
+
+--- gcc/testsuite/g++.dg/eh/unwind-direct.C.orig	2014-01-18 17:52:15.019246553 +0000
++++ gcc/testsuite/g++.dg/eh/unwind-direct.C
+@@ -0,0 +1,15 @@
++// PR target/59788
++// { dg-do run { target { *-*-solaris2* && { ! gld } } } }
++// { dg-options "-Wl,-Bdirect" }
++
++#include <stdexcept>
++
++int
++main(void)
++{
++  try
++    { throw std::runtime_error( "Catch me if you can!"); }
++  catch(...)
++    { return 0; }
++  return 1;
++}
