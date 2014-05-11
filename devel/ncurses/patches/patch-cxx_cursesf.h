503ccfa4b84945850c6e5b5bdc3d76cb259f35a3
rodent <rodent>
2014-05-11 16:55:17 +0000

    Fix build for OpenBSD >= 5.5. It doesn't have sys/ttydev.h. Regenerate patchset and checksums.

--- devel/ncurses/patches/patch-cxx_cursesf.h
+++ devel/ncurses/patches/patch-cxx_cursesf.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-cxx_cursesf.h,v 1.1 2011/02/28 11:02:46 adam Exp $
+$NetBSD: patch-cxx_cursesf.h,v 1.2 2014/05/11 16:55:17 rodent Exp $
 
---- c++/cursesf.h.orig	2011-02-28 09:23:33.000000000 +0000
+--- c++/cursesf.h.orig	Sat Aug 13 18:08:24 2005
 +++ c++/cursesf.h
-@@ -677,7 +677,7 @@ protected:
+@@ -677,7 +677,7 @@ template<class T> class NCURSES_IMPEXP NCursesUserForm
    }
  
  public:
@@ -11,7 +11,7 @@ $NetBSD: patch-cxx_cursesf.h,v 1.1 2011/02/28 11:02:46 adam Exp $
  		   const T* p_UserData = STATIC_CAST(T*)(0),
  		   bool with_frame=FALSE,
  		   bool autoDelete_Fields=FALSE)
-@@ -686,7 +686,7 @@ public:
+@@ -686,7 +686,7 @@ template<class T> class NCURSES_IMPEXP NCursesUserForm
  	set_user (const_cast<void *>(p_UserData));
    };
  
