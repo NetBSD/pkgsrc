3b52a37ab63cdb09bfe237fd5de9c1a2d572948b
jaapb <jaapb@pkgsrc.org>
2014-05-13 10:17:44 +0000

    Added comments to gethostbyname/addr patches

--- lang/ocaml/patches/patch-config_auto__aux_gethostbyname.c
+++ lang/ocaml/patches/patch-config_auto__aux_gethostbyname.c
@@ -1,5 +1,6 @@
-$NetBSD: patch-config_auto__aux_gethostbyname.c,v 1.1 2013/12/21 16:21:20 christos Exp $
+$NetBSD: patch-config_auto__aux_gethostbyname.c,v 1.2 2014/05/13 10:17:44 jaapb Exp $
 
+Fix gethostbyname support for NetBSD
 --- config/auto-aux/gethostbyname.c.orig	2012-10-15 13:50:56.000000000 -0400
 +++ config/auto-aux/gethostbyname.c	2013-12-21 11:16:39.000000000 -0500
 @@ -19,6 +19,10 @@
