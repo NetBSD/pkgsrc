4297fb113a8e32d4227e207e76e7dfa64f2f4cc4
rodent <rodent>
2014-05-13 02:23:11 +0000

    Fix build on OpenBSD/sparc64. Defuzz patches (sorry if this is annoying).

--- security/openssl/patches/patch-crypto_dso_dso__dlfcn.c
+++ security/openssl/patches/patch-crypto_dso_dso__dlfcn.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-crypto_dso_dso__dlfcn.c,v 1.1 2013/07/26 11:27:02 ryoon Exp $
+$NetBSD: patch-crypto_dso_dso__dlfcn.c,v 1.2 2014/05/13 02:23:11 rodent Exp $
 
 * Debian GNU/kFreeBSD needs _GNU_SOURCE.
 
---- crypto/dso/dso_dlfcn.c.orig	2013-02-11 15:26:04.000000000 +0000
+--- crypto/dso/dso_dlfcn.c.orig	Mon Mar 17 16:14:20 2014
 +++ crypto/dso/dso_dlfcn.c
 @@ -60,7 +60,7 @@
     that handle _GNU_SOURCE and other similar macros.  Defining it later
