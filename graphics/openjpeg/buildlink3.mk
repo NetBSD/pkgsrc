5bcbe7e45f2b3b8dac5c8871df55157fee4243ed
adam <adam@pkgsrc.org>
2014-05-15 14:14:54 +0000

    Changes 2.1.0:
    New Features:
        * Digital Cinema profiles have been fixed and updated
            * New option to disable MCT if needed
        * extended RAW support: it is now possible to input raw images
              with subsampled color components (422, 420, etc)
        * New way to deal with profiles
    
    API/ABI modifications: (see abi_compat_report in dev-utils/scripts)
        * Removed deprecated functions
                - opj_stream_create_default_file_stream(FILE*,...)
            - opj_stream_create_file_stream(FILE*,...)
            - opj_stream_set_user_data (opj_stream_t* p_stream, void * p_data)
            * Added
            - opj_stream_create_default_file_stream(char*,...)
            - opj_stream_create_file_stream(char*,...)
            - opj_stream_destroy(opj_stream_t*)
            - opj_stream_set_user_data (opj_stream_t* p_stream, void * p_data,
                ... opj_stream_free_user_data_fn p_function)
            - JPEG 2000 profiles and Part-2 extensions defined through '#define'
        * Changed
            - 'alpha' field added to 'opj_image_comp' structure
            - 'OPJ_CLRSPC_EYCC' added to enum COLOR_SPACE
            - 'OPJ_CLRSPC_CMYK' added to enum COLOR_SPACE
            - 'OPJ_CODEC_JPP' and 'OPJ_CODEC_JPX' added to CODEC_FORMAT
              (not yet used in use)
            - 'max_cs_size' and 'rsiz' fields added to opj_cparameters_t
    
    Misc:
        * OpenJPEG is now officialy conformant with JPEG 2000 Part-1
              and will soon become official reference software at the
              JPEG committee.
            * Huge amount of bug fixes. See CHANGES for details.

--- graphics/openjpeg/buildlink3.mk
+++ graphics/openjpeg/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2012/12/09 15:25:42 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/05/15 14:14:54 adam Exp $
 
 BUILDLINK_TREE+=	openjpeg
 
 .if !defined(OPENJPEG_BUILDLINK3_MK)
 OPENJPEG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.openjpeg+=	openjpeg>=2.0.0
-BUILDLINK_ABI_DEPENDS.openjpeg+=	openjpeg>=2.0.0
+BUILDLINK_API_DEPENDS.openjpeg+=	openjpeg>=2.1.0
+BUILDLINK_ABI_DEPENDS.openjpeg+=	openjpeg>=2.1.0
 BUILDLINK_PKGSRCDIR.openjpeg?=	../../graphics/openjpeg
 .endif	# OPENJPEG_BUILDLINK3_MK
 
