be96dae82eb5ef5a446f883f382e2e426e394d57
adam <adam>
2014-05-15 14:41:45 +0000

    Revbump after updating graphics/giflib

--- graphics/fnlib/buildlink3.mk
+++ graphics/fnlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2013/07/05 13:29:34 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2014/05/15 14:41:51 adam Exp $
 
 BUILDLINK_TREE+=	fnlib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	fnlib
 FNLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.fnlib+=	fnlib>=0.5nb6
-BUILDLINK_ABI_DEPENDS.fnlib+=	fnlib>=0.5nb20
+BUILDLINK_ABI_DEPENDS.fnlib+=	fnlib>=0.5nb21
 BUILDLINK_PKGSRCDIR.fnlib?=	../../graphics/fnlib
 
 .include "../../graphics/imlib/buildlink3.mk"
--- math/analitza/buildlink3.mk
+++ math/analitza/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2014/04/10 06:21:20 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2014/05/15 14:41:53 adam Exp $
 
 BUILDLINK_TREE+=	analitza
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	analitza
 ANALITZA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.analitza+=	analitza>=4.8.0
-BUILDLINK_ABI_DEPENDS.analitza?=	analitza>=4.11.5nb2
+BUILDLINK_ABI_DEPENDS.analitza?=	analitza>=4.11.5nb3
 BUILDLINK_PKGSRCDIR.analitza?=	../../math/analitza
 
 .include "../../x11/kdelibs4/buildlink3.mk"
