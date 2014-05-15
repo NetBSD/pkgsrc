4e8ed292d2b49589fbee5f33412b44088d2de143
joerg <joerg@pkgsrc.org>
2014-05-15 21:16:16 +0000

    Define initialisers in the headers like most cases already did, otherwise they might not get emitted correctly. Deal with the freetype header move.

---
+++ devel/xulrunner10/patches/patch-content_svg_content_src_nsSVGFilters.cpp
@@ -0,0 +1,13 @@
+$NetBSD: patch-content_svg_content_src_nsSVGFilters.cpp,v 1.1 2014/05/15 21:16:16 joerg Exp $
+
+--- content/svg/content/src/nsSVGFilters.cpp.orig	2014-05-14 13:17:00.000000000 +0000
++++ content/svg/content/src/nsSVGFilters.cpp
+@@ -119,7 +119,7 @@ nsSVGElement::LengthInfo nsSVGFE::sLengt
+ NS_IMPL_ADDREF_INHERITED(nsSVGFE,nsSVGFEBase)
+ NS_IMPL_RELEASE_INHERITED(nsSVGFE,nsSVGFEBase)
+ 
+-NS_DEFINE_STATIC_IID_ACCESSOR(nsSVGFE, NS_SVG_FE_CID)
++//NS_DEFINE_STATIC_IID_ACCESSOR(nsSVGFE, NS_SVG_FE_CID)
+ 
+ NS_INTERFACE_MAP_BEGIN(nsSVGFE)
+    // nsISupports is an ambiguous base of nsSVGFE so we have to work
