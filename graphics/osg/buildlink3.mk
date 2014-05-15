9fb682424d1c73aa812e701ee82259fc1d0a3282
adam <adam@pkgsrc.org>
2014-05-15 14:41:45 +0000

    Revbump after updating graphics/giflib

--- graphics/osg/buildlink3.mk
+++ graphics/osg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.33 2014/05/05 00:47:59 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2014/05/15 14:41:52 adam Exp $
 
 BUILDLINK_TREE+=	osg
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	osg
 OSG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.osg+=	osg>=2.8.3
-BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.0.1nb27
+BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.0.1nb28
 BUILDLINK_PKGSRCDIR.osg?=	../../graphics/osg
 
 .include "../../graphics/cairo/buildlink3.mk"
--- x11/kde-workspace4/buildlink3.mk
+++ x11/kde-workspace4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2014/04/10 06:21:20 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2014/05/15 14:41:59 adam Exp $
 
 BUILDLINK_TREE+=	kde-workspace
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kde-workspace
 KDE_WORKSPACE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kde-workspace+=	kde-workspace4>=4.0.0
-BUILDLINK_ABI_DEPENDS.kde-workspace+=	kde-workspace4>=4.11.5nb2
+BUILDLINK_ABI_DEPENDS.kde-workspace+=	kde-workspace4>=4.11.5nb3
 BUILDLINK_PKGSRCDIR.kde-workspace?=	../../x11/kde-workspace4
 
 .include "../../graphics/qimageblitz/buildlink3.mk"
