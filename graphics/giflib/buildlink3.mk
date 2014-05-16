53814f7b4d37eb626012d0e9686eba55d21c4f21
obache <obache>
2014-05-16 10:23:28 +0000

    5.1.0 introduce API changes.

--- graphics/giflib/buildlink3.mk
+++ graphics/giflib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/07/05 13:23:20 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/05/16 10:23:28 obache Exp $
 
 .if !defined(MK_GIFLIB_BUILDLINK3_MK)
 PKG_FAIL_REASON= "please use giflib.buildlink3.mk"
@@ -9,7 +9,7 @@ BUILDLINK_TREE+=	giflib
 .if !defined(GIFLIB_BUILDLINK3_MK)
 GIFLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.giflib+=	giflib>=5.0.4
+BUILDLINK_API_DEPENDS.giflib+=	giflib>=5.1.0
 BUILDLINK_PKGSRCDIR.giflib?=	../../graphics/giflib
 .endif	# GIFLIB_BUILDLINK3_MK
 
