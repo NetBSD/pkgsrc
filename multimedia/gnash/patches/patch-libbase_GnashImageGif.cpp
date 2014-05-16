150784824ca6b9258ce807cd6abec18a47a6b46e
obache <obache@pkgsrc.org>
2014-05-16 11:56:58 +0000

    fixes build with giflib>=5.1

--- multimedia/gnash/patches/patch-libbase_GnashImageGif.cpp
+++ multimedia/gnash/patches/patch-libbase_GnashImageGif.cpp
@@ -1,15 +1,30 @@
-$NetBSD: patch-libbase_GnashImageGif.cpp,v 1.1 2013/07/14 02:32:34 obache Exp $
+$NetBSD: patch-libbase_GnashImageGif.cpp,v 1.2 2014/05/16 11:56:58 obache Exp $
 
 * for giflib-5.0 API changes
   https://savannah.gnu.org/bugs/index.php?39482
 
 --- libbase/GnashImageGif.cpp.orig	2012-01-19 19:17:48.000000000 +0000
 +++ libbase/GnashImageGif.cpp
-@@ -269,7 +269,11 @@ GifInput::processRecord(GifRecordType re
+@@ -121,7 +121,13 @@ GifInput::GifInput(boost::shared_ptr<IOC
+ GifInput::~GifInput()
+ {
+     // Clean up allocated data.
+-    DGifCloseFile(_gif);
++#if (GIFLIB_MAJOR + 0) == 5 && (GIFLIB_MINOR + 0) >= 1 || (GIFLIB_MAJOR + 0) > 5
++    DGifCloseFile(_gif, NULL);
++#else
++    if (DGifCloseFile(_gif) != GIF_OK) {
++        free(_gif);
++    }
++#endif
+ }
+ 
+ size_t
+@@ -269,7 +275,11 @@ GifInput::processRecord(GifRecordType re
  void
  GifInput::read()
  {
-+#if GIFLIB_MAJOR >= 5
++#if (GIFLIB_MAJOR + 0) >= 5
 +    _gif = DGifOpen(_inStream.get(), &readData, NULL); 
 +#else
      _gif = DGifOpen(_inStream.get(), &readData); 
