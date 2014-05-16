07c83d3fcfe653e910c8fc2e85f5a8602c77c22e
tron <tron@pkgsrc.org>
2014-05-16 09:08:57 +0000

    Fix build with giflib 5.1.

--- editors/emacs24/patches/patch-src_image.c
+++ editors/emacs24/patches/patch-src_image.c
@@ -1,10 +1,19 @@
-$NetBSD: patch-src_image.c,v 1.1 2013/07/05 13:33:40 ryoon Exp $
+$NetBSD: patch-src_image.c,v 1.2 2014/05/16 09:08:57 tron Exp $
 
-* Fix build with giflib 5.0.
+Fix build with giflib 5.1.
 
 --- src/image.c.orig	2013-01-01 20:37:17.000000000 +0000
-+++ src/image.c
-@@ -7192,7 +7192,7 @@ gif_load (struct frame *f, struct image 
++++ src/image.c	2014-05-16 09:42:14.000000000 +0100
+@@ -7121,7 +7121,7 @@
+ 
+ #else
+ 
+-#define fn_DGifCloseFile	DGifCloseFile
++#define fn_DGifCloseFile(gf)	DGifCloseFile((gf), NULL)
+ #define fn_DGifSlurp		DGifSlurp
+ #define fn_DGifOpen		DGifOpen
+ #define fn_DGifOpenFileName	DGifOpenFileName
+@@ -7192,7 +7192,7 @@
  	}
  
        /* Open the GIF file.  */
@@ -13,7 +22,7 @@ $NetBSD: patch-src_image.c,v 1.1 2013/07/05 13:33:40 ryoon Exp $
        if (gif == NULL)
  	{
  	  image_error ("Cannot open `%s'", file, Qnil);
-@@ -7213,7 +7213,7 @@ gif_load (struct frame *f, struct image 
+@@ -7213,7 +7213,7 @@
        memsrc.len = SBYTES (specified_data);
        memsrc.index = 0;
  
