8fa42464321e6442674a07e02de687b95cd0bedc
ryoon <ryoon>
2014-05-16 12:56:26 +0000

    Use libtool(1) under OpenBSD, the build with pkgsrc/devel/libtool-base hould be fixed.

--- multimedia/gst-plugins1-base/plugins.mk
+++ multimedia/gst-plugins1-base/plugins.mk
@@ -1,4 +1,4 @@
-# $NetBSD: plugins.mk,v 1.1 2012/11/29 08:22:29 ryoon Exp $
+# $NetBSD: plugins.mk,v 1.2 2014/05/16 12:56:26 ryoon Exp $
 #
 # This file is shared across the gst-plugins-{base,good} packages to
 # simplify their code.  It provides a framework to write simple packages
@@ -76,6 +76,20 @@ SUBST_FILES.libs+=	${_d_}/Makefile.in
 SUBST_SED.libs=		-e 's|$$(top_builddir)/gst-libs/gst/.*/libgst|${BUILDLINK_PREFIX.gst-plugins1-${GST_PLUGINS1_TYPE}}/lib/libgst|g'
 
 .include "../../multimedia/gst-plugins1-${GST_PLUGINS1_TYPE}/buildlink3.mk"
+.endif
+
+.include "../../mk/bsd.prefs.mk"
+.if ${OPSYS} == "OpenBSD"
+# libtool(1) of /usr/bin/libtool is required to build gstreamer 1.0.
+# I have no idea how to fix the build with libtool-base from pkgsrc...
+# https://bugzilla.gnome.org/show_bug.cgi?id=726518
+SUBST_CLASSES+=			fix-libtool
+SUBST_STAGE.fix-libtool=	pre-configure
+SUBST_MESSAGE.fix-libtool=	Fix libtool path
+.for _d_ in app audio fft pbutils riff rtp rtsp sdp tag video
+SUBST_FILES.fix-libtool+=	gst-libs/gst/${_d_}/Makefile.in
+.endfor
+SUBST_SED.fix-libtool=		-e 's,--libtool=\"$$(top_builddir)/libtool\",--libtool=\"/usr/bin/libtool\",g'
 .endif
 
 .include "../../converters/libiconv/buildlink3.mk"
