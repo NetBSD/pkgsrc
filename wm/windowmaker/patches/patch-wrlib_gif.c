9ff17431ce35425dce8873a6c72a0b233da4ab0d
tron <tron@pkgsrc.org>
2014-05-16 09:29:39 +0000

    Fix build with giflib 5.1 and newer.

--- wm/windowmaker/patches/patch-wrlib_gif.c
+++ wm/windowmaker/patches/patch-wrlib_gif.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-wrlib_gif.c,v 1.1 2013/07/06 10:19:31 tron Exp $
+$NetBSD: patch-wrlib_gif.c,v 1.2 2014/05/16 09:29:39 tron Exp $
 
-Fix build with GIFLIB 5.0 and newer.
+Fix build with giflib 5.1 and newer.
 
---- wrlib/gif.c.orig	2013-01-09 21:42:39.000000000 +0000
-+++ wrlib/gif.c	2013-07-06 11:13:26.000000000 +0100
-@@ -50,6 +50,7 @@
+--- wrlib/gif.c.orig	2013-09-12 22:43:54.000000000 +0100
++++ wrlib/gif.c	2014-05-16 10:10:04.000000000 +0100
+@@ -52,6 +52,7 @@
  	unsigned char rmap[256];
  	unsigned char gmap[256];
  	unsigned char bmap[256];
@@ -12,7 +12,7 @@ Fix build with GIFLIB 5.0 and newer.
  
  	if (index < 0)
  		index = 0;
-@@ -57,10 +58,10 @@
+@@ -59,10 +60,10 @@
  	/* default error message */
  	RErrorCode = RERR_BADINDEX;
  
@@ -25,7 +25,16 @@ Fix build with GIFLIB 5.0 and newer.
  		case D_GIF_ERR_OPEN_FAILED:
  			RErrorCode = RERR_OPEN;
  			break;
-@@ -197,7 +198,7 @@
+@@ -77,7 +78,7 @@
+ 	}
+ 
+ 	if (gif->SWidth < 1 || gif->SHeight < 1) {
+-		DGifCloseFile(gif);
++		DGifCloseFile(gif, NULL);
+ 		RErrorCode = RERR_BADIMAGEFILE;
+ 		return NULL;
+ 	}
+@@ -199,7 +200,7 @@
  	/* yuck! */
  	goto did_not_get_any_errors;
   giferr:
@@ -34,3 +43,12 @@ Fix build with GIFLIB 5.0 and newer.
  	case D_GIF_ERR_OPEN_FAILED:
  		RErrorCode = RERR_OPEN;
  		break;
+@@ -220,7 +221,7 @@
+ 		free(buffer);
+ 
+ 	if (gif)
+-		DGifCloseFile(gif);
++		DGifCloseFile(gif, NULL);
+ 
+ 	return image;
+ }
