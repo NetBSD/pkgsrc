bdb07eb96b0d1b7ffe91dc9e4ffc28a3e656e505
adam <adam>
2014-05-18 11:31:09 +0000

    Changes 1.10.0: * VIGRA got a tutorial. * Significant simplification of the API: MultiArrayView arguments can now be passed to functions directly. The old syntax with Argument Object Factories (srcImageRange(), srcMultiArray() and relatives) remains valid, but is only required when the arguments are old-style BasicImages. * Made StridedArrayTag the default for vigra::MultiArrayView . * Added an efficient multi-dimensional vigra::GridGraph class which support both the LEMON and boost::graph APIs. * Generalized various algorithms to arbitrary dimensions (gaussianGradientMultiArray(), hessianOfGaussianMultiArray(), gaussianDivergenceMultiArray(), localMinima(), localMaxima(), labelMultiArray(), watershedsMultiArray()). * Added slicSuperpixels() for arbitrary dimensions. * Added automatic differentiation (see vigra::autodiff::DualVector). * Added nonlinearLeastSquares() using the Levenberg-Marquardt algorithm and automatic differentiation. More information about the changes can be found on the changelog page.

--- graphics/vigra/buildlink3.mk
+++ graphics/vigra/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2013/02/16 11:18:11 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2014/05/18 11:31:09 adam Exp $
 
 BUILDLINK_TREE+=	vigra
 
 .if !defined(VIGRA_BUILDLINK3_MK)
 VIGRA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vigra+=	vigra>=1.7.0
-BUILDLINK_ABI_DEPENDS.vigra+=	vigra>=1.9.0nb2
+BUILDLINK_API_DEPENDS.vigra+=	vigra>=1.10.0
+BUILDLINK_ABI_DEPENDS.vigra+=	vigra>=1.10.0
 BUILDLINK_PKGSRCDIR.vigra?=	../../graphics/vigra
 
 .include "../../devel/zlib/buildlink3.mk"
