4ce35a4155db799214d798095d73cd5480320f89
szptvlfn <szptvlfn@pkgsrc.org>
2014-05-19 21:14:35 +0000

    Update to 0.3.8
    CHANGELOG:
    0.3.8
    -----
    * Performance tweaks
    * bytestring-in-base flag default to False

--- devel/hs-utf8-string/buildlink3.mk
+++ devel/hs-utf8-string/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/05/18 21:16:55 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/05/19 21:14:35 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-utf8-string
 
 .if !defined(HS_UTF8_STRING_BUILDLINK3_MK)
 HS_UTF8_STRING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-utf8-string+=	hs-utf8-string>=0.3.7
+BUILDLINK_API_DEPENDS.hs-utf8-string+=	hs-utf8-string>=0.3.8
 BUILDLINK_PKGSRCDIR.hs-utf8-string?=	../../devel/hs-utf8-string
 .endif	# HS_UTF8_STRING_BUILDLINK3_MK
 
