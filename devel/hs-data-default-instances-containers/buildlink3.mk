108452ee427e9b5930876394ec575af8552b9093
szptvlfn <szptvlfn@pkgsrc.org>
2014-05-19 21:21:55 +0000

    Import data-default-instances-containers-0.0.1 as devel/hs-data-default-instances-containers, packaged for wip.
    Default instances for types in containers

---
+++ devel/hs-data-default-instances-containers/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/05/19 21:21:55 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-data-default-instances-containers
+
+.if !defined(HS_DATA_DEFAULT_INSTANCES_CONTAINERS_BUILDLINK3_MK)
+HS_DATA_DEFAULT_INSTANCES_CONTAINERS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-data-default-instances-containers+=	hs-data-default-instances-containers>=0.0.1
+BUILDLINK_PKGSRCDIR.hs-data-default-instances-containers?=	../../devel/hs-data-default-instances-containers
+
+.include "../../devel/hs-data-default-class/buildlink3.mk"
+.endif	# HS_DATA_DEFAULT_INSTANCES_CONTAINERS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-data-default-instances-containers
