dcf9fe01c24ae9d5dea164b03479936d869d6e1d
szptvlfn <szptvlfn@pkgsrc.org>
2014-05-19 21:23:29 +0000

    Import data-default-instances-dlist-0.0.1 as devel/hs-data-default-instances-dlist, packaged for wip.
    Default instances for types in dlist

---
+++ devel/hs-data-default-instances-dlist/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/05/19 21:23:29 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-data-default-instances-dlist
+
+.if !defined(HS_DATA_DEFAULT_INSTANCES_DLIST_BUILDLINK3_MK)
+HS_DATA_DEFAULT_INSTANCES_DLIST_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-data-default-instances-dlist+=	hs-data-default-instances-dlist>=0.0.1
+BUILDLINK_PKGSRCDIR.hs-data-default-instances-dlist?=	../../devel/hs-data-default-instances-dlist
+
+.include "../../devel/hs-data-default-class/buildlink3.mk"
+.include "../../devel/hs-dlist/buildlink3.mk"
+.endif	# HS_DATA_DEFAULT_INSTANCES_DLIST_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-data-default-instances-dlist
