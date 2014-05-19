8e206fa3bd2156cddbc92037603e6686353949f9
szptvlfn <szptvlfn@pkgsrc.org>
2014-05-19 21:15:55 +0000

    Import parsec-3.1.5 as textproc/hs-parsec, packaged for wip by pho.
    Parsec is designed from scratch as an industrial-strength parser
    library. It is simple, safe, well documented (on the package
    homepage), has extensive libraries and good error messages, and is
    also fast. It is defined as a monad transformer that can be stacked on
    arbitrary monads, and it is also parametric in the input stream type.

---
+++ textproc/hs-parsec/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/05/19 21:15:55 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-parsec
+
+.if !defined(HS_PARSEC_BUILDLINK3_MK)
+HS_PARSEC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-parsec+=	hs-parsec>=3.1.5
+BUILDLINK_PKGSRCDIR.hs-parsec?=	../../textproc/hs-parsec
+
+.include "../../devel/hs-mtl/buildlink3.mk"
+.include "../../devel/hs-text/buildlink3.mk"
+.endif	# HS_PARSEC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-parsec
