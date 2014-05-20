f6a17f9f8ac88198ea92516b3c47d1ac59be5984
adam <adam@pkgsrc.org>
2014-05-20 19:11:07 +0000

    Fix building with newer giflib.

--- net/driftnet/patches/patch-gif.c
+++ net/driftnet/patches/patch-gif.c
@@ -1,6 +1,8 @@
-$NetBSD: patch-gif.c,v 1.1 2013/08/13 10:23:26 joerg Exp $
+$NetBSD: patch-gif.c,v 1.2 2014/05/20 19:11:07 adam Exp $
 
---- gif.c.orig	2013-08-12 16:49:30.000000000 +0000
+Fix building with newer giflib.
+
+--- gif.c.orig	2002-07-09 19:26:41.000000000 +0000
 +++ gif.c
 @@ -20,7 +20,7 @@ static const char rcsid[] = "$Id: gif.c,
   */
@@ -11,3 +13,21 @@ $NetBSD: patch-gif.c,v 1.1 2013/08/13 10:23:26 joerg Exp $
      if (!I->us) {
          I->err = IE_HDRFORMAT;
          return 0;
+@@ -36,7 +36,7 @@ int gif_load_hdr(img I) {
+  * Abort loading a GIF file after the header is done.
+  */
+ int gif_abort_load(img I) {
+-    DGifCloseFile((GifFileType*)I->us);
++    DGifCloseFile((GifFileType*)I->us, NULL);
+     return 1;
+ }
+ 
+@@ -114,7 +114,7 @@ int gif_load_img(img I) {
+     ret = 1;
+ fail:
+ 
+-    DGifCloseFile(g);
++    DGifCloseFile(g, NULL);
+     
+     return ret;
+ }
