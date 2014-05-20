5f143878fb459d4fe7a979fa9a1a64dcee958199
szptvlfn <szptvlfn@pkgsrc.org>
2014-05-20 21:17:34 +0000

    Import extensible-exceptions-0.1.1.4 as devel/hs-extensible-exceptions, packaged for wip by pho.
    This package provides extensible exceptions for both new and old
    versions of GHC (i.e., < 6.10).

---
+++ devel/hs-extensible-exceptions/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/05/20 21:17:34 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-extensible-exceptions
+
+.if !defined(HS_EXTENSIBLE_EXCEPTIONS_BUILDLINK3_MK)
+HS_EXTENSIBLE_EXCEPTIONS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-extensible-exceptions+=	hs-extensible-exceptions>=0.1.1
+BUILDLINK_PKGSRCDIR.hs-extensible-exceptions?=	../../devel/hs-extensible-exceptions
+.endif	# HS_EXTENSIBLE_EXCEPTIONS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-extensible-exceptions
