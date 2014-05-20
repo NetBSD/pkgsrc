1216a13b51f0a705818b237a382097298e979e3c
szptvlfn <szptvlfn@pkgsrc.org>
2014-05-20 21:19:36 +0000

    Import data-default-0.5.3 as devel/hs-data-default, packaged for wip by pho.
    This module defines a class for types with a default value. Instances
    are provided for '()', Set, Map, Int, Integer, Float, Double, Ratio,
    Maybe, '[]', '->' and IO.

---
+++ devel/hs-data-default/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/05/20 21:19:36 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-data-default
+
+.if !defined(HS_DATA_DEFAULT_BUILDLINK3_MK)
+HS_DATA_DEFAULT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-data-default+=	hs-data-default>=0.5.3
+BUILDLINK_PKGSRCDIR.hs-data-default?=	../../devel/hs-data-default
+
+.include "../../devel/hs-data-default-class/buildlink3.mk"
+.include "../../devel/hs-data-default-instances-base/buildlink3.mk"
+.include "../../devel/hs-data-default-instances-containers/buildlink3.mk"
+.include "../../devel/hs-data-default-instances-dlist/buildlink3.mk"
+.include "../../devel/hs-data-default-instances-old-locale/buildlink3.mk"
+.endif	# HS_DATA_DEFAULT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-data-default
