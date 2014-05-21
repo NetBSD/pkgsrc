e9e7650572d4f83cc722b719cb5f92750ee00838
jperkin <jperkin@pkgsrc.org>
2014-05-21 16:07:59 +0000

    Apply a number of fixes:
      * Install the libffi libtool archive.  Whilst it is unused, it allows
        us to automatically determine the shared library suffix.
    
      * Switch the i386 SunOS bootstrap to .xz.
    
      * Use PKG_CC to build lndir to help environments where the compiler is
        not in $PATH.
    
      * Apply patch from Alain O'Dea to fix issues with files residing on
        read-only file systems.
    
      * Support shared libraries on 64-bit SunOS.
    
      * Fix typo in patches/patch-rts_Linker.c.
    
    Fixes build on SunOS.  Bump PKGREVISION.

--- lang/ghc7/bootstrap.mk
+++ lang/ghc7/bootstrap.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bootstrap.mk,v 1.9 2014/02/05 06:38:05 obache Exp $
+# $NetBSD: bootstrap.mk,v 1.10 2014/05/21 16:07:59 jperkin Exp $
 # -----------------------------------------------------------------------------
 # Select a bindist of bootstrapping compiler based on a per-platform
 # basis.
@@ -31,7 +31,7 @@ BOOT_ARCHIVE:=	${PKGNAME}-boot-x86_64-unknown-linux.tar.xz
 BOOT_ARCHIVE:=	${PKGNAME}-boot-x86_64-unknown-netbsd.tar.xz
 
 .elif !empty(MACHINE_PLATFORM:MSunOS-5.11-i386)
-BOOT_ARCHIVE:=  ${PKGNAME}-boot-i386-unknown-solaris2.tar.gz
+BOOT_ARCHIVE:=  ${PKGNAME}-boot-i386-unknown-solaris2.tar.xz
 
 .elif !empty(MACHINE_PLATFORM:MSunOS-5.11-x86_64)
 BOOT_ARCHIVE:=  ${PKGNAME}-boot-x86_64-unknown-solaris2.tar.xz
@@ -40,7 +40,7 @@ BOOT_ARCHIVE:=  ${PKGNAME}-boot-x86_64-unknown-solaris2.tar.xz
 PKG_FAIL_REASON+=	"internal error: unsupported platform"
 .endif
 
-BOOT_TARBALL=	${BOOT_ARCHIVE:C/\.xz$//:C/\.gz$//}
+BOOT_TARBALL=	${BOOT_ARCHIVE:C/\.xz$//}
 
 
 
@@ -98,7 +98,7 @@ pre-bootstrap:
 ${WRKDIR}/lndir:
 	@${PHASE_MSG} "Building lndir(1) to duplicate the source tree."
 	cd ${WRKSRC:Q}/utils/lndir && \
-		${CC:Q} lndir.c -o ${.TARGET}
+		${PKG_CC:Q} lndir.c -o ${.TARGET}
 
 ${WRKDIR}/stamp-lndir-boot: ${WRKDIR}/lndir
 	@${PHASE_MSG} "Duplicating the source tree for bootstrapping ${PKGNAME_NOREV}"
