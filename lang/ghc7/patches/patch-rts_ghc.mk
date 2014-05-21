59f311cbe6e9361f3e3a8a64104c187c79be0524
jperkin <jperkin@pkgsrc.org>
2014-05-21 16:07:59 +0000

    Apply a number of fixes:
      * Install the libffi libtool archive.  Whilst it is unused, it allows
        us to automatically determine the shared library suffix.
    
      * Switch the i386 SunOS bootstrap to .xz.
    
      * Use PKG_CC to build lndir to help environments where the compiler is
        not in $PATH.
    
      * Apply patch from Alain O'Dea to fix issues with files residing on
        read-only file systems.
    
      * Support shared libraries on 64-bit SunOS.
    
      * Fix typo in patches/patch-rts_Linker.c.
    
    Fixes build on SunOS.  Bump PKGREVISION.

--- lang/ghc7/patches/patch-rts_ghc.mk
+++ lang/ghc7/patches/patch-rts_ghc.mk
@@ -1,4 +1,4 @@
-$NetBSD: patch-rts_ghc.mk,v 1.1 2013/12/12 12:47:17 obache Exp $
+$NetBSD: patch-rts_ghc.mk,v 1.2 2014/05/21 16:07:59 jperkin Exp $
 
 This is pkgsrc specific: dtrace(1) gets confused when we have gcc
 wrappers in the PATH so we have to hide them:
@@ -8,10 +8,19 @@ wrappers in the PATH so we have to hide them:
   dtrace: failed to compile script rts/RtsProbes.d: Preprocessor failed to pr
   ocess input program
 
+Also install libffi.{a,la} so we can expand to figure out .so extensions.
 
---- rts/ghc.mk.orig	2013-01-27 13:21:52.000000000 +0000
+--- rts/ghc.mk.orig	2013-04-18 21:22:47.000000000 +0000
 +++ rts/ghc.mk
-@@ -484,7 +484,7 @@ endif
+@@ -114,6 +114,7 @@ $(rts_ffi_objs_stamp): $(libffi_STATIC_L
+ # depend on libffi.so, but copy libffi.so*
+ rts/dist/build/libffi$(soext): libffi/build/inst/lib/libffi$(soext)
+ 	cp libffi/build/inst/lib/libffi$(soext)* rts/dist/build
++	cp libffi/build/inst/lib/libffi.{a,la} rts/dist/build
+ 
+ rts/dist/build/$(LIBFFI_DLL): libffi/build/inst/bin/$(LIBFFI_DLL)
+ 	cp $< $@
+@@ -484,7 +485,7 @@ endif
  
  DTRACEPROBES_SRC = rts/RtsProbes.d
  $(DTRACEPROBES_H): $(DTRACEPROBES_SRC) includes/ghcplatform.h | $$(dir $$@)/.
@@ -20,3 +29,11 @@ wrappers in the PATH so we have to hide them:
  endif
  
  # -----------------------------------------------------------------------------
+@@ -510,6 +511,7 @@ endif
+ 
+ INSTALL_LIBS += $(ALL_RTS_LIBS)
+ INSTALL_LIBS += $(wildcard rts/dist/build/libffi$(soext)*)
++INSTALL_LIBS += $(wildcard rts/dist/build/libffi.*a)
+ INSTALL_LIBS += $(wildcard rts/dist/build/$(LIBFFI_DLL))
+ 
+ install: install_libffi_headers
