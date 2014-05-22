da72e92d13c9dec061e4083f2158eb7ead9cbd54
szptvlfn <szptvlfn@pkgsrc.org>
2014-05-22 21:34:24 +0000

    Import xmonad-0.11 as wm/xmonad, packaged for wip by esg.
    xmonad is a tiling window manager for X. Windows are arranged
    automatically to tile the screen without gaps or overlap, maximising
    screen use. All features of the window manager are accessible from
    the keyboard: a mouse is strictly optional. xmonad is written and
    extensible in Haskell. Custom layout algorithms, and other
    extensions, may be written by the user in config files. Layouts are
    applied dynamically, and different layouts may be used on each
    workspace. Xinerama is fully supported, allowing windows to be tiled
    on several screens.

---
+++ wm/xmonad/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/05/22 21:34:24 szptvlfn Exp $
+
+BUILDLINK_TREE+=	xmonad
+
+.if !defined(XMONAD_BUILDLINK3_MK)
+XMONAD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.xmonad+=	xmonad>=0.11
+BUILDLINK_PKGSRCDIR.xmonad?=	../../wm/xmonad
+
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../devel/hs-extensible-exceptions/buildlink3.mk"
+.include "../../devel/hs-mtl/buildlink3.mk"
+.include "../../devel/hs-utf8-string/buildlink3.mk"
+.include "../../x11/hs-X11/buildlink3.mk"
+.endif	# XMONAD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-xmonad
