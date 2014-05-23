8e41ab27b286932e3247a546c1425834f641ea63
szptvlfn <szptvlfn@pkgsrc.org>
2014-05-23 21:46:44 +0000

    Import ghc-paths-0.1.0.9 as devel/hs-ghc-paths, packaged for wip by pho.
    Knowledge of GHC's installation directories.

---
+++ devel/hs-ghc-paths/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/05/23 21:46:44 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-ghc-paths
+
+.if !defined(HS_GHC_PATHS_BUILDLINK3_MK)
+HS_GHC_PATHS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-ghc-paths+=	hs-ghc-paths>=0.1.0.9
+BUILDLINK_PKGSRCDIR.hs-ghc-paths?=	../../devel/hs-ghc-paths
+.endif	# HS_GHC_PATHS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-ghc-paths
