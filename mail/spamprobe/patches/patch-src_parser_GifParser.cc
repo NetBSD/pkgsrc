5074959fcad46e0310c8fad0557283d1aa51a157
obache <obache>
2014-05-25 08:04:00 +0000

    Fixes build with giflib-5.1.

--- mail/spamprobe/patches/patch-src_parser_GifParser.cc
+++ mail/spamprobe/patches/patch-src_parser_GifParser.cc
@@ -1,7 +1,18 @@
-$NetBSD: patch-src_parser_GifParser.cc,v 1.1 2014/01/05 00:29:35 htodd Exp $
+$NetBSD: patch-src_parser_GifParser.cc,v 1.2 2014/05/25 08:04:00 obache Exp $
+
+* giflib>=5 API changes
 
 --- src/parser/GifParser.cc.orig	2006-11-17 07:24:49.000000000 +0000
 +++ src/parser/GifParser.cc
+@@ -81,7 +81,7 @@ GifParser::GifParser(Message *message,
+ GifParser::~GifParser()
+ {
+   if (m_gif) {
+-    DGifCloseFile(m_gif);
++    DGifCloseFile(m_gif, NULL);
+   }
+ }
+ 
 @@ -99,7 +99,7 @@ bool GifParser::parseImage()
  void GifParser::openImage()
  {
