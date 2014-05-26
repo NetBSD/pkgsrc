2335c86815f8504aa1d4a7a02133b1d0a676272c
tron <tron>
2014-05-26 18:10:22 +0000

    Disable compiler optimization under NetBSD/sparc as well because GCC seems to get stuck on this platform, too.
    Patched provided by Carlos Sanchez, see port-sparc64/48501.

--- multimedia/libvpx/hacks.mk
+++ multimedia/libvpx/hacks.mk
@@ -1,12 +1,12 @@
-# $NetBSD: hacks.mk,v 1.3 2014/01/06 11:16:23 martin Exp $
+# $NetBSD: hacks.mk,v 1.4 2014/05/26 18:10:22 tron Exp $
 
 ### [Sun Jan  5 12:58:28 UTC 2014 : tron]
 ### GCC 4.5.3 gets stuck while compiline "vp9/vp9_cx_iface.c".
 ### See PR port-sparc64/48501 for more details.
-.if !empty(MACHINE_PLATFORM:MNetBSD-*-sparc64)
+.if !empty(MACHINE_PLATFORM:MNetBSD-*-sparc*)
 .  include "../../mk/compiler.mk"
 .  if !empty(CC_VERSION:Mgcc-4.[0-5].*)
-PKG_HACKS+=		sparc64-gcc-lock-up
+PKG_HACKS+=		sparc-gcc-lock-up
 BUILDLINK_TRANSFORM+=	rm:-O[0-9]*
 .  endif
 .endif
