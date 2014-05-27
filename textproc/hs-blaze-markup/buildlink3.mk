9d6c7308b8cf93155ec5ef045a73006a41f95900
szptvlfn <szptvlfn@pkgsrc.org>
2014-05-27 21:36:09 +0000

    Import blaze-markup-0.6.1.0 as textproc/hs-blaze-markup, packaged for wip.
    Core modules of a blazingly fast markup combinator library for the Haskell
    programming language. The Text.Blaze module is a good starting point, as
    well as this tutorial: http://jaspervdj.be/blaze/tutorial.html.

---
+++ textproc/hs-blaze-markup/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/05/27 21:36:09 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-blaze-markup
+
+.if !defined(HS_BLAZE_MARKUP_BUILDLINK3_MK)
+HS_BLAZE_MARKUP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-blaze-markup+=	hs-blaze-markup>=0.6.1
+BUILDLINK_PKGSRCDIR.hs-blaze-markup?=	../../textproc/hs-blaze-markup
+
+.include "../../devel/hs-blaze-builder/buildlink3.mk"
+.include "../../devel/hs-text/buildlink3.mk"
+.endif	# HS_BLAZE_MARKUP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-blaze-markup
