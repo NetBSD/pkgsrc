f874dcf64c3c010bcdfa2b4721d3952f27e4340e
markd <markd@pkgsrc.org>
2014-05-28 10:29:12 +0000

    Update to 6.6.5
    3 years of bug fixes and updates.

--- lang/swi-prolog-lite/patches/patch-packages_clib_sha1_brg_endian.h
+++ lang/swi-prolog-lite/patches/patch-packages_clib_sha1_brg_endian.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-packages_clib_sha1_brg_endian.h,v 1.1 2011/12/17 16:11:25 marino Exp $
+$NetBSD: patch-packages_clib_sha1_brg_endian.h,v 1.2 2014/05/28 10:29:12 markd Exp $
 
---- packages/clib/sha1/brg_endian.h.orig	2011-03-18 18:18:22.000000000 +0000
+--- packages/clib/sha1/brg_endian.h.orig	2014-04-13 08:40:33.000000000 +0000
 +++ packages/clib/sha1/brg_endian.h
 @@ -39,7 +39,7 @@
  /* Include files where endian defines and byteswap functions may reside */
@@ -10,4 +10,4 @@ $NetBSD: patch-packages_clib_sha1_brg_endian.h,v 1.1 2011/12/17 16:11:25 marino 
 +#elif defined( __FreeBSD__ ) || defined( __OpenBSD__ ) || defined( __NetBSD__ ) || defined( __DragonFly__)
  #  include <sys/endian.h>
  #elif defined( BSD ) && ( BSD >= 199103 ) || defined( __APPLE__ ) || \
-       defined( __CYGWIN32__ ) || defined( __DJGPP__ ) || defined( __osf__ )
+       defined( __CYGWIN__ ) || defined( __DJGPP__ ) || defined( __osf__ )
