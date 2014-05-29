962d48b1b1b0a4ade8959d751b745f77671cb189
rodent <rodent>
2014-05-29 00:39:47 +0000

    Needs to link against pthread on OpenBSD. print/ghostscript-gpl exposed the issue during configure.

--- x11/libxcb/buildlink3.mk
+++ x11/libxcb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2012/10/02 12:09:41 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/05/29 00:39:47 rodent Exp $
 
 BUILDLINK_TREE+=	libxcb
 
@@ -9,6 +9,10 @@ BUILDLINK_API_DEPENDS.libxcb+=	libxcb>=1.6
 BUILDLINK_ABI_DEPENDS.libxcb+=	libxcb>=1.6
 BUILDLINK_PKGSRCDIR.libxcb?=	../../x11/libxcb
 
+.include "../../mk/bsd.fast.prefs.mk"
+.if ${OPSYS} == "OpenBSD"
+.include "../../mk/pthread.buildlink3.mk"
+.endif
 .include "../../x11/xcb-proto/buildlink3.mk"
 .include "../../x11/libXdmcp/buildlink3.mk"
 .include "../../x11/libXau/buildlink3.mk"
