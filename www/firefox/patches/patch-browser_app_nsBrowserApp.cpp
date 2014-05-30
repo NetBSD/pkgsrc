19ef557fee0fed76efe2f91d04399ece96e33eb8
pho <pho@pkgsrc.org>
2014-05-30 03:03:36 +0000

    PR pkg/48840: Don't assume cocoa toolkit just because OS_ARCH is Darwin

--- www/firefox/patches/patch-browser_app_nsBrowserApp.cpp
+++ www/firefox/patches/patch-browser_app_nsBrowserApp.cpp
@@ -1,8 +1,33 @@
-$NetBSD: patch-browser_app_nsBrowserApp.cpp,v 1.3 2014/04/30 15:07:18 ryoon Exp $
+$NetBSD: patch-browser_app_nsBrowserApp.cpp,v 1.4 2014/05/30 03:03:36 pho Exp $
 
---- browser/app/nsBrowserApp.cpp.orig	2014-04-18 02:02:30.000000000 +0000
+* Don't assume cocoa toolkit just because OS_ARCH is Darwin.
+
+--- browser/app/nsBrowserApp.cpp.orig	2014-05-06 22:55:09.000000000 +0000
 +++ browser/app/nsBrowserApp.cpp
-@@ -586,6 +586,7 @@ int main(int argc, char* argv[])
+@@ -20,6 +20,8 @@
+ 
+ #ifdef XP_MACOSX
+ #include <mach/mach_time.h>
++#endif
++#ifdef MOZ_WIDGET_COCOA
+ #include "MacQuirks.h"
+ #endif
+ 
+@@ -499,7 +501,7 @@ InitXPCOMGlue(const char *argv0, nsIFile
+   lastSlash += sizeof(XPCOM_PATH) - sizeof(XPCOM_DLL);
+ 
+   if (!FileExists(exePath)) {
+-#if defined(LIBXUL_SDK) && defined(XP_MACOSX)
++#if defined(LIBXUL_SDK) && defined(MOZ_WIDGET_COCOA)
+     // Check for <bundle>/Contents/Frameworks/XUL.framework/libxpcom.dylib
+     bool greFound = false;
+     CFBundleRef appBundle = CFBundleGetMainBundle();
+@@ -582,10 +584,11 @@ int main(int argc, char* argv[])
+ #endif
+   uint64_t start = TimeStamp_Now();
+ 
+-#ifdef XP_MACOSX
++#ifdef MOZ_WIDGET_COCOA
    TriggerQuirks();
  #endif
  
@@ -10,3 +35,12 @@ $NetBSD: patch-browser_app_nsBrowserApp.cpp,v 1.3 2014/04/30 15:07:18 ryoon Exp 
    int gotCounters;
  #if defined(XP_UNIX)
    struct rusage initialRUsage;
+@@ -644,7 +647,7 @@ int main(int argc, char* argv[])
+ 
+   NS_LogTerm();
+ 
+-#ifdef XP_MACOSX
++#ifdef MOZ_WIDGET_COCOA
+   // Allow writes again. While we would like to catch writes from static
+   // destructors to allow early exits to use _exit, we know that there is
+   // at least one such write that we don't control (see bug 826029). For
