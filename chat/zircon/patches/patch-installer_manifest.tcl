b78a5e61d56ebe03f752b4191e42cfbcd185c99c
joerg <joerg@pkgsrc.org>
2014-05-30 11:32:22 +0000

    Fix build with TCL 8.6

--- chat/zircon/patches/patch-installer_manifest.tcl
+++ chat/zircon/patches/patch-installer_manifest.tcl
@@ -1,4 +1,4 @@
-$NetBSD: patch-installer_manifest.tcl,v 1.1 2012/08/21 21:41:03 marino Exp $
+$NetBSD: patch-installer_manifest.tcl,v 1.2 2014/05/30 11:32:22 joerg Exp $
 
 --- installer/manifest.tcl.orig	2000-07-17 12:37:18.000000000 +0000
 +++ installer/manifest.tcl
@@ -6,7 +6,7 @@ $NetBSD: patch-installer_manifest.tcl,v 1.1 2012/08/21 21:41:03 marino Exp $
      [file join installer file82.tcl]		{} \
      [file join installer file83.tcl]		{} \
      [file join installer file84.tcl]		{} \
-+    [file join installer file85.tcl]		{} \
++    [file join installer file86.tcl]		{} \
      [file join installer support.tcl]		{} \
      [file join installer mac.tcl]		{} \
      [file join installer macfile.tcl]		{} \
@@ -14,7 +14,7 @@ $NetBSD: patch-installer_manifest.tcl,v 1.1 2012/08/21 21:41:03 marino Exp $
      [file join lib tcl82.tcl]			{copy} \
      [file join lib tcl83.tcl]			{copy} \
      [file join lib tcl84.tcl]			{copy} \
-+    [file join lib tcl85.tcl]			{copy} \
++    [file join lib tcl86.tcl]			{copy} \
      [file join lib Unix.tcl]			{copy} \
      [file join lib Windows.tcl]			{copy} \
      [file join lib debug.tcl]			{copy} \
