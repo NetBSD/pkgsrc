8ab3b03658d097b68a51871f2d4d2528cde482d0
wiz <wiz@pkgsrc.org>
2014-05-30 21:39:37 +0000

    Remove curl from Makefile, it's an option. Add proper curl option handling to buildlink3.mk.
    From Leonardo Taccari.

--- print/mupdf/buildlink3.mk
+++ print/mupdf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2014/05/15 14:41:58 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2014/05/30 21:39:37 wiz Exp $
 
 BUILDLINK_TREE+=	mupdf
 
@@ -8,6 +8,14 @@ MUPDF_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.mupdf+=	mupdf>=1.3
 BUILDLINK_ABI_DEPENDS.mupdf?=	mupdf>=1.4nb1
 BUILDLINK_PKGSRCDIR.mupdf?=	../../print/mupdf
+
+pkgbase := mupdf
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.mupdf:Mcurl)
+.include "../../www/curl/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+.endif
 
 .include "../../graphics/jbig2dec/buildlink3.mk"
 .include "../../graphics/openjpeg/buildlink3.mk"
