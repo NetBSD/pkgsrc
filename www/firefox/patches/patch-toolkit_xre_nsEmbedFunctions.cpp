d56ece01ea122831212287cc1f9181d01c6ccb13
pho <pho>
2014-05-30 10:22:05 +0000

    PR pkg/48840: Remove most patches that I have recently added
    The preprocessor macro XP_MACOSX is misused everywhere. They tend to
    say "#if defined(XP_MACOSX)" to actually mean
    "#if defined(MOZ_WIDGET_COCOA)". It's okay to equate XP_MACOSX with
    XP_DARWIN but equating it with MOZ_WIDGET_COCOA is just wrong. As a
    result, cairo-gtk2 build on Darwin is completely broken and needs
    heavy patching. Ideally they should be fixed individually but that
    will be an extremely hard work, so don't define XP_MACOSX for now.

--- www/firefox/patches/patch-toolkit_xre_nsEmbedFunctions.cpp
+++ www/firefox/patches/patch-toolkit_xre_nsEmbedFunctions.cpp
@@ -1,49 +1,17 @@
-$NetBSD: patch-toolkit_xre_nsEmbedFunctions.cpp,v 1.4 2014/05/29 15:38:19 pho Exp $
+$NetBSD: patch-toolkit_xre_nsEmbedFunctions.cpp,v 1.5 2014/05/30 10:22:06 pho Exp $
 
-* Don't assume cocoa toolkit just because OS_ARCH is Darwin.
-
---- toolkit/xre/nsEmbedFunctions.cpp.orig	2014-05-06 22:56:36.000000000 +0000
+--- toolkit/xre/nsEmbedFunctions.cpp.orig	2013-05-11 19:19:55.000000000 +0000
 +++ toolkit/xre/nsEmbedFunctions.cpp
-@@ -42,7 +42,7 @@
- #include "nsXREDirProvider.h"
- 
- #include "mozilla/Omnijar.h"
--#if defined(XP_MACOSX)
-+#if defined(MOZ_WIDGET_COCOA)
- #include "nsVersionComparator.h"
- #include "chrome/common/mach_ipc_mac.h"
- #endif
-@@ -228,9 +228,9 @@ XRE_TakeMinidumpForChild(uint32_t aChild
- bool
- XRE_SetRemoteExceptionHandler(const char* aPipe/*= 0*/)
+@@ -239,7 +239,7 @@ XRE_SetRemoteExceptionHandler(const char
  {
--#if defined(XP_WIN) || defined(XP_MACOSX)
-+#if defined(XP_WIN) || defined(MOZ_WIDGET_COCOA)
+ #if defined(XP_WIN) || defined(XP_MACOSX)
    return CrashReporter::SetRemoteExceptionHandler(nsDependentCString(aPipe));
 -#elif defined(OS_LINUX)
 +#elif defined(OS_LINUX) || defined(OS_SOLARIS)
    return CrashReporter::SetRemoteExceptionHandler();
  #else
  #  error "OOP crash reporter unsupported on this platform"
-@@ -306,7 +306,7 @@ XRE_InitChildProcess(int aArgc,
- 
-   // Complete 'task_t' exchange for Mac OS X. This structure has the same size
-   // regardless of architecture so we don't have any cross-arch issues here.
--#ifdef XP_MACOSX
-+#ifdef MOZ_WIDGET_COCOA
-   if (aArgc < 1)
-     return NS_ERROR_FAILURE;
-   const char* const mach_port_name = aArgv[--aArgc];
-@@ -359,7 +359,7 @@ XRE_InitChildProcess(int aArgc,
-     return NS_ERROR_FAILURE;
-   const char* const crashReporterArg = aArgv[--aArgc];
-   
--#  if defined(XP_WIN) || defined(XP_MACOSX)
-+#  if defined(XP_WIN) || defined(MOZ_WIDGET_COCOA)
-   // on windows and mac, |crashReporterArg| is the named pipe on which the
-   // server is listening for requests, or "-" if crash reporting is
-   // disabled.
-@@ -368,7 +368,7 @@ XRE_InitChildProcess(int aArgc,
+@@ -351,7 +351,7 @@ XRE_InitChildProcess(int aArgc,
      // Bug 684322 will add better visibility into this condition
      NS_WARNING("Could not setup crash reporting\n");
    }
@@ -52,39 +20,3 @@ $NetBSD: patch-toolkit_xre_nsEmbedFunctions.cpp,v 1.4 2014/05/29 15:38:19 pho Ex
    // on POSIX, |crashReporterArg| is "true" if crash reporting is
    // enabled, false otherwise
    if (0 != strcmp("false", crashReporterArg) && 
-@@ -636,7 +636,7 @@ XRE_RunAppShell()
- {
-     nsCOMPtr<nsIAppShell> appShell(do_GetService(kAppShellCID));
-     NS_ENSURE_TRUE(appShell, NS_ERROR_FAILURE);
--#if defined(XP_MACOSX)
-+#if defined(MOZ_WIDGET_COCOA)
-     {
-       // In content processes that want XPCOM (and hence want
-       // AppShell), we usually run our hybrid event loop through
-@@ -675,7 +675,7 @@ XRE_RunAppShell()
- 
-       loop->SetNestableTasksAllowed(couldNest);
-     }
--#endif  // XP_MACOSX
-+#endif  // MOZ_WIDGET_COCOA
-     return appShell->Run();
- }
- 
-@@ -701,7 +701,7 @@ XRE_ShutdownChildProcess()
-   //  (4) ProcessChild joins the IO thread
-   //  (5) exit()
-   MessageLoop::current()->Quit();
--#if defined(XP_MACOSX)
-+#if defined(MOZ_WIDGET_COCOA)
-   nsCOMPtr<nsIAppShell> appShell(do_GetService(kAppShellCID));
-   if (appShell) {
-       // On Mac, we might be only above nsAppShell::Run(), not
-@@ -710,7 +710,7 @@ XRE_ShutdownChildProcess()
-       // above MessagePump::Run(), this Exit() is just superfluous.
-       appShell->Exit();
-   }
--#endif // XP_MACOSX
-+#endif // MOZ_WIDGET_COCOA
- }
- 
- namespace {
