b860f7550bf4a7f5790f6b4ed4c7894c3ad0671e
szptvlfn <szptvlfn@pkgsrc.org>
2014-05-31 21:31:57 +0000

    Import scientific-0.2.0.2 as math/hs-scientific, packaged for wip.
    A Scientific number is an arbitrary-precision floating-point number
    represented using scientific notation.
    
    A scientific number with coefficient c and base10Exponent e corresponds to
    the Fractional number: fromInteger c * 10 ^^ e
    
    Its primary use-case is to serve as the target of parsing floating point
    numbers. Since the textual representation of floating point numbers use
    scientific notation they can be efficiently parsed to a Scientific number.

---
+++ math/hs-scientific/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/05/31 21:31:57 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-scientific
+
+.if !defined(HS_SCIENTIFIC_BUILDLINK3_MK)
+HS_SCIENTIFIC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-scientific+=	hs-scientific>=0.2.0
+BUILDLINK_PKGSRCDIR.hs-scientific?=	../../math/hs-scientific
+
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-text/buildlink3.mk"
+.endif	# HS_SCIENTIFIC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-scientific
