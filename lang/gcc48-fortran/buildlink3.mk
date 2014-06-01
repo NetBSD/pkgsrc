dd2122dd51ed744672f6d38d7af9be8f0fa9b161
ryoon <ryoon@pkgsrc.org>
2014-06-01 08:11:13 +0000

    Set gcc48-libs as runtime dependency

--- lang/gcc48-fortran/buildlink3.mk
+++ lang/gcc48-fortran/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/04/22 20:46:41 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/06/01 08:11:13 ryoon Exp $
 
 BUILDLINK_TREE+=	gcc48-fortran
 
@@ -12,5 +12,11 @@ BUILDLINK_GCC48_LIBDIRS=		gcc48/lib
 BUILDLINK_LIBDIRS.gcc48-fortran+=	${BUILDLINK_GCC48_LIBDIRS}
 BUILDLINK_DEPMETHOD.gcc48-fortran?=	build
 .endif	# GCC48_FORTRAN_BUILDLINK3_MK
+
+# Packages that link against shared libraries need a full dependency.
+.if defined(_USE_GCC_SHLIB)
+DEPENDS+=	gcc48-libs>=${_GCC_REQD}:../../lang/gcc48-libs
+ABI_DEPENDS+=	gcc48-libs>=4.8.0:../../lang/gcc48-libs
+.endif
 
 BUILDLINK_TREE+=	-gcc48-fortran
