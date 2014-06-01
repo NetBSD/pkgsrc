4e990eb984e3de684d2dea0a14f1a488cd03e818
rodent <rodent@pkgsrc.org>
2014-06-01 23:51:25 +0000

    Updated to 0.22.3. No ChangeLog. Remove python/extension.mk from Makefile, since egg.mk includes that. PGP module seems to have been removed from package.

--- security/py-m2crypto/patches/patch-setup.py
+++ security/py-m2crypto/patches/patch-setup.py
@@ -1,17 +1,17 @@
-$NetBSD: patch-setup.py,v 1.1 2012/01/22 10:05:19 apb Exp $
+$NetBSD: patch-setup.py,v 1.2 2014/06/01 23:51:25 rodent Exp $
 
 Don't pass "-includeall" to swig.  It causes compiler-dependent
 tests in system include files to break (e.g. "#ifdef __GNUC__"
 doesn't work properly.)
 
---- setup.py.orig	2011-01-15 19:10:06.000000000 +0000
+--- setup.py.orig	2014-01-22 19:36:53.000000000 +0000
 +++ setup.py
-@@ -54,7 +54,7 @@ class _M2CryptoBuildExt(build_ext.build_
-         
+@@ -75,7 +75,7 @@ class _M2CryptoBuildExt(build_ext.build_
+ 
          self.swig_opts = ['-I%s' % i for i in self.include_dirs + \
-                           [opensslIncludeDir]]
+                           [opensslIncludeDir, os.path.join(opensslIncludeDir, "openssl")]]
 -        self.swig_opts.append('-includeall')
 +        #self.swig_opts.append('-includeall')
-         #self.swig_opts.append('-D__i386__') # Uncomment for early OpenSSL 0.9.7 versions, or on Fedora Core if build fails
-         #self.swig_opts.append('-DOPENSSL_NO_EC') # Try uncommenting if you can't build with EC disabled
-         
+         self.swig_opts.append('-modern')
+ 
+         # Fedora does hat tricks.
