1dedd49e65ad104d05e9a399a6b75a42d1e7c929
adam <adam>
2014-06-04 11:22:04 +0000

    Changes 0.11.2: * Improve yasm version check * Fix \fscx0 crashes * Actually export ass_library_version * Fix change detection with vector clips * SIMD assembly bugfixes * Parse drawing numbers as floats * Fix shaper sign bug * Initialize \pbo correctly

--- multimedia/libass/buildlink3.mk
+++ multimedia/libass/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2013/10/20 21:53:54 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2014/06/04 11:22:04 adam Exp $
 
 BUILDLINK_TREE+=	libass
 
@@ -6,11 +6,12 @@ BUILDLINK_TREE+=	libass
 LIBASS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libass+=	libass>=0.9.12
-BUILDLINK_ABI_DEPENDS.libass+=	libass>=0.10.2
+BUILDLINK_ABI_DEPENDS.libass+=	libass>=0.11.2
 BUILDLINK_PKGSRCDIR.libass?=	../../multimedia/libass
 
 .include "../../converters/fribidi/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../fonts/harfbuzz/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../textproc/enca/buildlink3.mk"
 .endif # LIBASS_BUILDLINK3_MK
