8c2f69a4bab917339c9a3505d0c4ffcb94aca3b3
obache <obache>
2014-06-06 12:09:47 +0000

    Update KAKASI to 2.3.6. (in addtion to backported patches, dropped prototype patch of kakasi_do().  Its committer doesn't know the reason anymore, and implemented part patches  had been merged long time ago with    "patch-aa and patch-ac patched the same file.  Merge them"  but patch-aa was for lib/libkakasi.c, and patch-ac is for src/kakasi.c.  From no problem reportes, it is considered this patch set is not relevant).
    Changes from KAKASI 2.3.5 to 2.3.6
    
    * Check runtime environment for test codes, and skip impossible tests.
    
    * With configure, check compiler supported option.
      (for the issue old gcc cannot accept -Wno-unused-result option)
    
    * Fixed to check iconv availability at configure instead of autconf.
    
    * Fixed and added manual (by Osamu Aoki).
    
    * Fixed a bug to ouput redundant delimiter at wakatigaki if the line
      start with ASCII character.
    
    * Changed unitptr_t from macro to typedef (to avoid the issue that
      come compilers cannot handle such macro)
    
    * Fixed missing iconv related casts.
    
    Changes from KAKASI 2.3.4 to 2.3.5
    
    * Added UTF-8 input/output support (iconv is required)
    
    * Improve handling of japanese long-vowel.
    
    * Fixed bug of do_kakasi(), may return invald memory area.
    
    * Fixed a wrong entriy in kakasidict.
    
    * Fixed bad hepburn romaji table.
    
    * Added use_old_romaji_table variable and -t option.
    
    * Fixed segfault on invalid SS2 sequences.
    
    * Fixes warnings at compile.
    
    * Added a test script.
    
    * Added -S option for mutable separator (with -w or -s option).
    
    * Added -l and -L option for level furigana and hiragana conversion.
      (See doc/README.level for more information)
    
    * Newly added -F option for mutable parentheses around furigana
      (with -JH -f or -L option).
    
    * Added -y option to display all yomi per one Kanji.
    
    * Add endian independent dictionary format support.
    
    * Fixed bad hepburn romaji table.
      (specify -t option fo use  old romaji table)

--- textproc/kakasi/buildlink3.mk
+++ textproc/kakasi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:25:30 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2014/06/06 12:09:47 obache Exp $
 
 BUILDLINK_TREE+=	kakasi
 
@@ -8,6 +8,14 @@ KAKASI_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.kakasi+=	kakasi>=2.3.0
 BUILDLINK_ABI_DEPENDS.kakasi+=	kakasi>=2.3.4nb4
 BUILDLINK_PKGSRCDIR.kakasi?=	../../textproc/kakasi
+
+pkgbase := kakasi
+.include "../../mk/pkg-build-options.mk"
+
+.  if !empty(PKG_BUILD_OPTIONS.kakasi:Mutf8)
+.include "../../converters/libiconv/buildlink3.mk"
+.  endif
+
 .endif # KAKASI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kakasi
