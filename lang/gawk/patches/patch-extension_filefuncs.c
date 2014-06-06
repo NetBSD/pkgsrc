e1f853b3892039902660142fa32ed4a360a0b929
ryoon <ryoon>
2014-06-06 23:24:10 +0000

    Update to 4.1.1
    Changelog:
    Changes from 4.1.0 to 4.1.1
    ---------------------------
    
    1. The "stat" extension now includes a "devbsize" element which indicates
       the units for the "nblocks" element.
    
    2. The extension facility now works on MinGW. Many of the extensions can be
       built and used directly.
    
    3. A number of bugs in the pretty-printing / profiling code have been fixed.
    
    4. Sockets and two-way pipes now work under MinGW.
    
    5. The debugger now lists source code correctly under Cygwin.
    
    6. Configuration and building with the Mac OS X libreadline should work now.
    
    7. The -O option now works again.
    
    8. The --include option, documented since 4.0, now actually works.
    
    9. Infrastructure updated to automake 1.13.4, bison 3.0.2, and
       libtool 2.4.2.418.
    
    10. The configure script now accepts a --disable-extensions option,
        which disables checking for and building the extensions.
    
    11. The VMS port has been considerably improved. In particular config.h
        is now generated by a DCL script. Also, the extension facility works
        and several of the extensions can be built and used. Currently, the
        extension facility only works on Alpha and Itanium.
    
    12. The API now provides functions pointers for malloc(), calloc(),
        realloc() and free(), to insure that the same memory allocation
        functions are always used. This bumps the minor version by one.
    
    13. The printf quote flag now works correctly in locales with a different
        decimal point character but without a thousands separator character.
        If the thousands separator is a string, it will be correctly added
        to decimal numbers.
    
    14. The readfile extension now has an input parser that will read whole
        files as a single record.
    
    15. A number of bugs have been fixed. See the ChangeLog.

--- lang/gawk/patches/patch-extension_filefuncs.c
+++ lang/gawk/patches/patch-extension_filefuncs.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-extension_filefuncs.c,v 1.1 2013/12/26 19:19:32 tron Exp $
+$NetBSD: patch-extension_filefuncs.c,v 1.2 2014/06/06 23:24:10 ryoon Exp $
 
 Fix build problem under Solaris. The more elegant fix is available here:
 
 http://lists.gnu.org/archive/html/bug-gawk/2013-06/msg00010.html
 
---- extension/filefuncs.c.orig	2013-05-02 20:51:30.000000000 +0100
-+++ extension/filefuncs.c	2013-12-26 19:12:54.000000000 +0000
-@@ -75,6 +75,10 @@
- #define minor(s) (0)
+--- extension/filefuncs.c.orig	2014-03-31 19:17:46.000000000 +0000
++++ extension/filefuncs.c
+@@ -137,6 +137,10 @@ get_inode(const char *fname)
+ }
  #endif
  
 +#ifdef __sun
