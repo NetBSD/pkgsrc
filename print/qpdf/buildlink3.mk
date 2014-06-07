013b03c5d72536d90cb9e485d8ca6aabd2b22329
wiz <wiz@pkgsrc.org>
2014-06-07 10:44:30 +0000

    Add buildlink3.mk file.

---
+++ print/qpdf/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/06/07 10:44:30 wiz Exp $
+
+BUILDLINK_TREE+=	qpdf
+
+.if !defined(QPDF_BUILDLINK3_MK)
+QPDF_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qpdf+=	qpdf>=5.0.1nb2
+BUILDLINK_PKGSRCDIR.qpdf?=	../../print/qpdf
+
+.endif	# QPDF_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qpdf
