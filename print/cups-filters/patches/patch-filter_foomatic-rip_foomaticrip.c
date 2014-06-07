055e7259361b348d64c3e0a51ebfe534689abf56
wiz <wiz@pkgsrc.org>
2014-06-07 13:53:41 +0000

    Add upstream bug report URLs.

--- print/cups-filters/patches/patch-filter_foomatic-rip_foomaticrip.c
+++ print/cups-filters/patches/patch-filter_foomatic-rip_foomaticrip.c
@@ -1,7 +1,8 @@
-$NetBSD: patch-filter_foomatic-rip_foomaticrip.c,v 1.1 2014/06/07 11:47:47 wiz Exp $
+$NetBSD: patch-filter_foomatic-rip_foomaticrip.c,v 1.2 2014/06/07 13:53:41 wiz Exp $
 
 mkstemps doesn't exist on NetBSD either.
 (Should be a configure test instead.)
+https://bugs.linuxfoundation.org/show_bug.cgi?id=1211
 
 --- filter/foomatic-rip/foomaticrip.c.orig	2014-06-06 08:49:59.000000000 +0000
 +++ filter/foomatic-rip/foomaticrip.c
--- print/cups-filters/patches/patch-utils_cups-browsed.c
+++ print/cups-filters/patches/patch-utils_cups-browsed.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-utils_cups-browsed.c,v 1.1 2014/06/07 11:47:47 wiz Exp $
+$NetBSD: patch-utils_cups-browsed.c,v 1.2 2014/06/07 13:53:41 wiz Exp $
 
 Move header below those providing u_int on NetBSD.
+https://bugs.linuxfoundation.org/show_bug.cgi?id=1212
 
 --- utils/cups-browsed.c.orig	2014-05-07 14:09:53.000000000 +0000
 +++ utils/cups-browsed.c
