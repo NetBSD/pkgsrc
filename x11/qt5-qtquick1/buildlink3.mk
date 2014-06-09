229fa0d7a20ac46c35f067787d36ee05b6dd9975
ryoon <ryoon@pkgsrc.org>
2014-06-09 14:18:04 +0000

    Recursive revbump from pulseaudio-5.0

--- x11/qt5-qtquick1/buildlink3.mk
+++ x11/qt5-qtquick1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2014/04/23 07:05:32 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2014/06/09 14:18:04 ryoon Exp $
 
 BUILDLINK_TREE+=	qt5-qtquick1
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qt5-qtquick1
 QT5_QTQUICK1_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt5-qtquick1+=	qt5-qtquick1>=5.2.0
-BUILDLINK_ABI_DEPENDS.qt5-qtquick1?=	qt5-qtquick1>=5.2.0nb7
+BUILDLINK_ABI_DEPENDS.qt5-qtquick1?=	qt5-qtquick1>=5.2.1
 BUILDLINK_PKGSRCDIR.qt5-qtquick1?=	../../x11/qt5-qtquick1
 
 BUILDLINK_INCDIRS.qt5-qtquick1+=	qt5/include
--- x11/qt5-qtwebkit/buildlink3.mk
+++ x11/qt5-qtwebkit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2014/04/23 07:05:32 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2014/06/09 14:18:04 ryoon Exp $
 
 BUILDLINK_TREE+=	qt5-qtwebkit
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qt5-qtwebkit
 QT5_QTWEBKIT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt5-qtwebkit+=	qt5-qtwebkit>=5.2.0
-BUILDLINK_ABI_DEPENDS.qt5-qtwebkit?=	qt5-qtwebkit>=5.2.0nb8
+BUILDLINK_ABI_DEPENDS.qt5-qtwebkit?=	qt5-qtwebkit>=5.2.1
 BUILDLINK_PKGSRCDIR.qt5-qtwebkit?=	../../x11/qt5-qtwebkit
 
 BUILDLINK_INCDIRS.qt5-qtwebkit+=	qt5/include
