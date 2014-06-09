988efd8490a8958e358f1f5f2346c3338c32d223
obache <obache>
2014-06-09 23:42:57 +0000

    Set CHECK_BUILTIN properly for builtin check.

--- net/libfetch/options.mk
+++ net/libfetch/options.mk
@@ -1,10 +1,12 @@
-# $NetBSD: options.mk,v 1.3 2009/02/09 13:34:02 joerg Exp $
+# $NetBSD: options.mk,v 1.4 2014/06/09 23:42:57 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libfetch
 PKG_SUPPORTED_OPTIONS=	inet6 openssl
 PKG_SUGGESTED_OPTIONS=	inet6
 
+CHECK_BUILTIN.openssl:=	yes
 .include "../../security/openssl/builtin.mk"
+CHECK_BUILTIN.openssl:=	no
 
 .if !empty(USE_BUILTIN.openssl:M[yY][eE][sS])
 PKG_SUGGESTED_OPTIONS+=	openssl
