e62aabc75e6e9f6a35a4585cc290821c04f6f950
jperkin <jperkin@pkgsrc.org>
2014-06-09 10:52:47 +0000

    Fix PKGNAME of python3 python-digest dependency.

--- www/py-django-tastypie/options.mk
+++ www/py-django-tastypie/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2014/06/02 05:12:35 rodent Exp $
+# $NetBSD: options.mk,v 1.2 2014/06/09 10:52:47 jperkin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-django-tastypie
 PKG_SUPPORTED_OPTIONS=	digest
@@ -9,7 +9,7 @@ PKG_SUGGESTED_OPTIONS+=	digest
 .if !empty(PKG_OPTIONS:Mdigest)
 .include "../../lang/python/pyversion.mk"
 .  if ${PYVERSSUFFIX} > 2.7
-DEPENDS+=	${PYPKGPREFIX}-python3-digest-[0-9]*:../../www/py-python3-digest
+DEPENDS+=	${PYPKGPREFIX}-python-digest-[0-9]*:../../www/py-python3-digest
 .  else
 DEPENDS+=	${PYPKGPREFIX}-python-digest-[0-9]*:../../www/py-python-digest
 .  endif
