00652187caaf44b3d8de105e07ef7bc8d268412a
fhajny <fhajny@pkgsrc.org>
2014-06-11 13:50:19 +0000

    Update libbson to 0.8.2.
    Changes in 0.8.2
    ----------------
     * A fix for BCON when used from C++.
     * Change bson_next_power_of_two() to accept size_t. This should not be
       an ABI break since it is static inline.
    
    Changes in 0.8.0
    ----------------
    This cycle includes much, much more documentation for your perusing. There is
    much more cross-referencing and structure for your navigation pleasure.
    
    We've improved support for Libbson on a few more exotic platforms. SPARC
    support is looking pretty good these days.
    
    You'll also find some new examples in this release to help you get started a
    bit faster. If there is something you'd like to see, just ask!
    
    There are a few ABI breaks this cycle, as we are well on the road to a 1.0 and
    would like things as clean as possible. I anticipate a few more during the next
    couple of cycles, but we will try to keep them to a minimum.  With that said,
    you *WILL* need to recompile your application against 0.8.0.

--- devel/libbson/buildlink3.mk
+++ devel/libbson/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/05/07 15:43:59 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/06/11 13:50:19 fhajny Exp $
 
 BUILDLINK_TREE+=	libbson
 
 .if !defined(LIBBSON_BUILDLINK3_MK)
 LIBBSON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libbson+=	libbson>=0.6.8
+BUILDLINK_API_DEPENDS.libbson+=	libbson>=0.8.0
 BUILDLINK_PKGSRCDIR.libbson?=	../../devel/libbson
 .endif	# LIBBSON_BUILDLINK3_MK
 
