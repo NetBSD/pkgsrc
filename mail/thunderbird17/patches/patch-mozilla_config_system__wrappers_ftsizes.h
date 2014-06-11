d7afed7c832e134ae083dbd2445542acf6820c3e
joerg <joerg@pkgsrc.org>
2014-06-11 12:36:16 +0000

    Retire old thunderbird versions.

--- mail/thunderbird17/patches/patch-mozilla_config_system__wrappers_ftsizes.h
+++
@@ -1,9 +0,0 @@
-$NetBSD: patch-mozilla_config_system__wrappers_ftsizes.h,v 1.1 2014/05/27 09:58:51 joerg Exp $
-
---- mozilla/config/system_wrappers/ftsizes.h.orig	2014-05-26 20:06:38.000000000 +0000
-+++ mozilla/config/system_wrappers/ftsizes.h
-@@ -0,0 +1,4 @@
-+#pragma GCC system_header
-+#pragma GCC visibility push(default)
-+#include_next <ftsizes.h>
-+#pragma GCC visibility pop
