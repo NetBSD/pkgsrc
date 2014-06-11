1849bdb8cd8cde17b02bca7446a21dd5ca782cea
ryoon <ryoon@pkgsrc.org>
2014-06-11 14:45:09 +0000

    Fix build under NetBSD 6, PR pkg/48893.

--- audio/alsa-utils/patches/patch-alsaloop_alsaloop.c
+++ audio/alsa-utils/patches/patch-alsaloop_alsaloop.c
@@ -1,19 +1,25 @@
-$NetBSD: patch-alsaloop_alsaloop.c,v 1.1 2014/06/09 12:34:26 ryoon Exp $
+$NetBSD: patch-alsaloop_alsaloop.c,v 1.2 2014/06/11 14:45:09 ryoon Exp $
 
 --- alsaloop/alsaloop.c.orig	2013-07-09 15:28:37.000000000 +0000
 +++ alsaloop/alsaloop.c
-@@ -24,6 +24,10 @@
+@@ -24,6 +24,16 @@
  #include <stdlib.h>
  #include <string.h>
  #include <sched.h>
 +/* For ERESTART */
 +#if defined(__NetBSD__)
++#include <sys/param.h>
++/* NetBSD 6 does not have _KMEMUSER. And _KERNEL breaks build of alsa-utils. */
++#if __NetBSD_Version__ < 699000000
++#define ERESTART	-3
++#else
 +#define _KMEMUSER	1
++#endif
 +#endif
  #include <errno.h>
  #include <getopt.h>
  #include <alsa/asoundlib.h>
-@@ -34,6 +38,10 @@
+@@ -34,6 +44,10 @@
  #include <sys/signal.h>
  #include "alsaloop.h"
  
