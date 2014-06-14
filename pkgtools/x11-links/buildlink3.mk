e556954f87c1a52b08dd7d8cd3d51e489504050c
jperkin <jperkin@pkgsrc.org>
2014-06-14 21:06:52 +0000

    Update x11-links to 0.91.
    On Linux with LIBABISUFFIX, only include lib$LIBABISUFFIX directories rather
    than both, as it can cause issues with ld finding suitable libraries.  Fixes
    a bunch of packages.

--- pkgtools/x11-links/buildlink3.mk
+++ pkgtools/x11-links/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.57 2014/06/13 22:42:18 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.58 2014/06/14 21:06:52 jperkin Exp $
 #
 # Don't include this file manually!  It will be included as necessary
 # by bsd.buildlink3.mk.
@@ -12,7 +12,7 @@ BUILDLINK_TREE+=	x11-links
 .if !defined(X11_LINKS_BUILDLINK3_MK)
 X11_LINKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.90
+BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.91
 BUILDLINK_PKGSRCDIR.x11-links?=	../../pkgtools/x11-links
 BUILDLINK_DEPMETHOD.x11-links?=	build
 
