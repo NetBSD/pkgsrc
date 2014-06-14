7e7c89c8b2a8f5b913ab2b70dadfa25544166c7d
wiz <wiz@pkgsrc.org>
2014-06-14 21:48:34 +0000

    Add upstream bug report URL (patch already applied for 1.9.x series)

--- sysutils/dbus/patches/patch-dbus_dbus-sysdeps-unix.c
+++ sysutils/dbus/patches/patch-dbus_dbus-sysdeps-unix.c
@@ -1,7 +1,8 @@
-$NetBSD: patch-dbus_dbus-sysdeps-unix.c,v 1.1 2014/04/24 09:40:08 wiz Exp $
+$NetBSD: patch-dbus_dbus-sysdeps-unix.c,v 1.2 2014/06/14 21:48:34 wiz Exp $
 
 On Linux, poll accepts any negative value as infinity.
 On at least FreeBSD and NetBSD, only -1 is acceptable.
+https://bugs.freedesktop.org/show_bug.cgi?id=78480
 
 --- dbus/dbus-sysdeps-unix.c.orig	2014-01-17 16:59:48.000000000 +0000
 +++ dbus/dbus-sysdeps-unix.c
