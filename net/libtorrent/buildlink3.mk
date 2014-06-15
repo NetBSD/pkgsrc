0c0022b979128a784731de01d4402cb178cdadc1
tron <tron@pkgsrc.org>
2014-06-15 22:44:15 +0000

    Update libtorrent to version 0.13.4 and rtorrent to version 0.9.4. A list of changes is not available unfortunately. But the update seems to prevent crashes with a message like this one:
    rtorrent: Read past initial payload after incoming encrypted handshake

--- net/libtorrent/buildlink3.mk
+++ net/libtorrent/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.21 2014/02/12 23:18:22 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2014/06/15 22:44:15 tron Exp $
 
 BUILDLINK_TREE+=	libtorrent
 
 .if !defined(LIBTORRENT_BUILDLINK3_MK)
 LIBTORRENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libtorrent+=	libtorrent>=0.13.2
-BUILDLINK_ABI_DEPENDS.libtorrent+=	libtorrent>=0.13.2nb6
+BUILDLINK_API_DEPENDS.libtorrent+=	libtorrent>=0.13.4
+BUILDLINK_ABI_DEPENDS.libtorrent+=	libtorrent>=0.13.4
 BUILDLINK_PKGSRCDIR.libtorrent?=	../../net/libtorrent
 
 .include "../../devel/libsigc++/buildlink3.mk"
