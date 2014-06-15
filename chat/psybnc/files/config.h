490520e04bc0bd8b8ff3c6971659d1ca00e8a56c
pettai <pettai@pkgsrc.org>
2014-06-15 21:55:17 +0000

    psyBNC is an easy-to-use, multi-user, permanent IRC-Bouncer with many features. Some of its features include symmetric ciphering of talk and connections (Blowfish and IDEA), the possibility of linking multiple bouncers to an internal network including a shared partyline, vhost- and relay support to connected bouncers and an extensive online help system.

---
+++ chat/psybnc/files/config.h
@@ -0,0 +1,98 @@
+/*
+ * Configuration file for psyBNC, created by menuconf
+ */
+
+/* Encryption */
+
+#define CRYPT
+
+/* Encryption Type*/
+
+#define BLOWFISH
+
+/* Allow Translation */
+
+#define TRANSLATE
+
+/* Allow internal network */
+
+#define INTNET
+
+/* Allow traffic logging */
+
+#define TRAFFICLOG
+
+/* Allow linkage of bouncers */
+
+#define LINKAGE
+
+/* Allow the dcc File-Functions */
+
+#define DCCFILES
+
+/* Pipe dcc Chats */
+
+#define DCCCHAT
+
+/* Number of max. Users */
+
+#define MAXUSER 10
+
+/* Number of max. Connections per User */
+
+#define MAXCONN 0
+
+/* Allow the usage of scripts */
+
+#define SCRIPTING
+
+/* Support oIdentd */
+
+/* #define OIDENTD */
+
+/* Allow multiple irc connections per user */
+
+#define NETWORK
+
+/* Allow Proxy Support */
+
+/* #define PROXYS */
+
+/* Set psyBNC anonymous */
+
+/* #define ANONYMOUS */
+
+/* Connections arent permanent */
+
+/* #define DYNAMIC */
+
+/* The logging level */
+
+#define LOGLEVEL 0
+
+/* We still use the 2.1.1 stylish partychannel */
+
+/* #define PARTYCHANNEL */
+
+/* MySQL-based IP-check on incoming client connections */
+
+/* #define MYSQL_IPCHECK
+   #define MYSQL_SERVER "localhost"
+   #define MYSQL_USER "psybnc"
+   #define MYSQL_PASS "AxKps4ATS4uNyv3r"
+   #define MYSQL_DB "psybnc"
+   #define MYSQL_TABLE "iplock"
+   #define MYSQL_TIMEOUT 2 */
+
+/* The Path to SSL */
+
+#define SSLPATH "/usr/"
+
+/* SSL-Security */
+
+#define SSLSEC 0
+
+/* Insure */
+
+#define INSURE
+
