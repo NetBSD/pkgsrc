8902d43dd25224199cb305b062b906b8a83e9a50
wiz <wiz@pkgsrc.org>
2014-06-17 13:17:12 +0000

    Import cups-1.5.4nb12 as print/cups15, for those that prefer it to cups-1.7.x
    The Common UNIX Printing System provides a portable printing layer for
    UNIX operating systems. It has been developed by Easy Software Products
    to promote a standard printing solution for all UNIX vendors and users.
    CUPS provides the System V and Berkeley command-line interfaces.
    
    CUPS uses the Internet Printing Protocol (IETF-IPP) as the basis for
    managing print jobs and queues.  The Line Printer Daemon (LPD, RFC1179)
    and AppSocket protocols are also supported with reduced functionality.
    
    CUPS adds network printer browsing and PostScript Printer Description
    ("PPD")-based printing options to support real world applications under
    UNIX.
    
    This package contains the older 1.5 series, before Apple stopped
    maintaining the filters.

---
+++ print/cups15/buildlink3.mk
@@ -0,0 +1,29 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/06/17 13:17:12 wiz Exp $
+
+BUILDLINK_TREE+=	cups
+
+.if !defined(CUPS_BUILDLINK3_MK)
+CUPS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.cups+=	cups>=1.1.19nb3
+BUILDLINK_ABI_DEPENDS.cups+=	cups>=1.5.4nb9
+BUILDLINK_PKGSRCDIR.cups?=	../../print/cups
+
+pkgbase := cups
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.cups:Mkerberos)
+.include "../../mk/krb5.buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.cups:Mdnssd)
+.include "../../net/mDNSResponder/buildlink3.mk"
+.endif
+
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.endif # CUPS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-cups
