b5792780a0ce5e1932cf2b958e60f086aca1bbc9
wiz <wiz@pkgsrc.org>
2014-06-25 12:19:09 +0000

    Update to upgraded patch from upstream. Ride bump.

--- shells/zsh/patches/patch-Src_Builtins_rlimits.c
+++ shells/zsh/patches/patch-Src_Builtins_rlimits.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-Src_Builtins_rlimits.c,v 1.4 2014/06/25 08:39:11 wiz Exp $
+$NetBSD: patch-Src_Builtins_rlimits.c,v 1.5 2014/06/25 12:19:09 wiz Exp $
 
 Fix thread limit display in ulimit.
 From upstream, Peter Stephenson <p.stephenson@samsung.com>.
 
 --- Src/Builtins/rlimits.c.orig	2013-11-27 19:00:20.000000000 +0000
 +++ Src/Builtins/rlimits.c
-@@ -32,12 +32,14 @@
+@@ -32,12 +32,17 @@
  
  #if defined(HAVE_GETRLIMIT) && defined(RLIM_INFINITY)
  
@@ -19,10 +19,36 @@ From upstream, Peter Stephenson <p.stephenson@samsung.com>.
 +#if defined(HAVE_RLIMIT_NTHR) && !defined(HAVE_RLIMIT_PTHREAD)
  #  define RLIMIT_PTHREAD	RLIMIT_NTHR
 +#  define HAVE_RLIMIT_PTHREAD   1
++#  define THREAD_FMT            "-T: threads                         "
++#else
++#  define THREAD_FMT            "-T: threads per process             "
  #endif
  
  enum {
-@@ -876,6 +878,11 @@ bin_ulimit(char *name, char **argv, UNUS
+@@ -373,7 +378,7 @@ printulimit(char *nam, int lim, int hard
+ # ifdef HAVE_RLIMIT_PTHREAD
+     case RLIMIT_PTHREAD:
+ 	if (head)
+-	    printf("-T: threads per process             ");
++	    printf(THREAD_FMT);
+ 	break;
+ # endif /* HAVE_RLIMIT_PTHREAD */
+ # ifdef HAVE_RLIMIT_NICE
+@@ -860,6 +865,13 @@ bin_ulimit(char *name, char **argv, UNUS
+ 		case 'r':
+ 		    res = RLIMIT_RTPRIO;
+ 		    break;
++# else
++#  ifdef HAVE_RLIMIT_NTHR
++		    /* For compatibility with sh on NetBSD */
++		case 'r':
++		    res = RLIMIT_NTHR;
++		    break;
++#  endif /* HAVE_RLIMIT_NTHR */
+ # endif
+ # ifdef HAVE_RLIMIT_NPTS
+ 		case 'p':
+@@ -876,6 +888,11 @@ bin_ulimit(char *name, char **argv, UNUS
  		    res = RLIMIT_KQUEUES;
  		    break;
  # endif
