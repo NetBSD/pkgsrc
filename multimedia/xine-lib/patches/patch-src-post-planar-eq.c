19be22ea7c20d847872a77028fd453ae72c00ae3
jperkin <jperkin@pkgsrc.org>
2014-06-27 12:17:22 +0000

    Disable MMX sections on SunOS.

--- multimedia/xine-lib/patches/patch-src-post-planar-eq.c
+++ multimedia/xine-lib/patches/patch-src-post-planar-eq.c
@@ -1,9 +1,20 @@
-$NetBSD: patch-src-post-planar-eq.c,v 1.2 2014/05/18 15:38:18 wiz Exp $
+$NetBSD: patch-src-post-planar-eq.c,v 1.3 2014/06/27 12:17:22 jperkin Exp $
 
 https://bugs.xine-project.org/show_bug.cgi?id=524
 
---- src/post/planar/eq.c.orig	2011-08-30 13:13:46.000000000 +0000
+Disable MMX sections on SunOS.
+
+--- src/post/planar/eq.c.orig	2013-09-18 10:04:54.000000000 +0000
 +++ src/post/planar/eq.c
+@@ -31,7 +31,7 @@
+ #include <pthread.h>
+ 
+ 
+-#if defined(ARCH_X86) || defined(ARCH_X86_64)
++#if (defined(ARCH_X86) || defined(ARCH_X86_64)) && !defined(__sun)
+ static void process_MMX(unsigned char *dest, int dstride, unsigned char *src, int sstride,
+ 		    int w, int h, int brightness, int contrast)
+ {
 @@ -138,9 +138,11 @@ typedef struct eq_parameters_s {
   * description of params struct
   */
@@ -18,3 +29,12 @@ https://bugs.xine-project.org/show_bug.cgi?id=524
              "contrast" )
  END_PARAM_DESCR( param_descr )
  
+@@ -259,7 +261,7 @@ static post_plugin_t *eq_open_plugin(pos
+   }
+ 
+   process = process_C;
+-#if defined(ARCH_X86) || defined(ARCH_X86_64)
++#if (defined(ARCH_X86) || defined(ARCH_X86_64)) && !defined(__sun)
+   if( xine_mm_accel() & MM_ACCEL_X86_MMX )
+     process = process_MMX;
+ #endif
