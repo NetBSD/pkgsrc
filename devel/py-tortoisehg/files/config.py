e28e1ecf3e15ab03aedc5127fc18402f1fe7ac38
wiz <wiz@pkgsrc.org>
2014-07-05 15:33:03 +0000

    Import py27-tortoisehg-3.0.1 as devel/py-tortoisehg, packaged for wip by nros, krytarowski and myself.
    Tortoisehg is a GUI front end for the Mercurial dvcs.

---
+++ devel/py-tortoisehg/files/config.py
@@ -0,0 +1,6 @@
+bin_path     = "@PREFIX@/bin"
+license_path = "@PREFIX@/share/doc/tortoisehg/COPYING.txt"
+locale_path  = "@PREFIX@/share/locale"
+icon_path    = "@PREFIX@/share/pixmaps/tortoisehg/icons"
+doc_path     = "@PREFIX@/share/doc/tortoisehg"
+nofork       = True
