fc22dc5b857a736d4c5da82b8f0f24e9af063f7b
ryoon <ryoon>
2014-07-09 20:01:08 +0000

    Update to 2.71
    Changelog:
    Many bugfixes.
    I detail see http://wiki.blender.org/index.php/Dev:Ref/Release_Notes/2.70/Bug_Fixes
    and http://wiki.blender.org/index.php/Dev:Ref/Release_Notes/2.71/Bug_Fixes

--- graphics/blender/patches/patch-build__files_scons_tools_btools.py
+++ graphics/blender/patches/patch-build__files_scons_tools_btools.py
@@ -1,10 +1,25 @@
-$NetBSD: patch-build__files_scons_tools_btools.py,v 1.1 2012/12/03 13:15:51 ryoon Exp $
+$NetBSD: patch-build__files_scons_tools_btools.py,v 1.2 2014/07/09 20:01:08 ryoon Exp $
 
 From Gentoo's 2.64a/0300-scons-failure.patch
 
---- build_files/scons/tools/btools.py.orig	2012-10-09 18:41:53.000000000 +0000
+--- build_files/scons/tools/btools.py.orig	2014-06-25 20:43:52.000000000 +0000
 +++ build_files/scons/tools/btools.py
-@@ -482,7 +482,9 @@ def read_opts(env, cfg, args):
+@@ -56,13 +56,7 @@ def get_version():
+     raise Exception("%s: missing version string" % fname)
+ 
+ def get_hash():
+-    try:
+-        build_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip()
+-    except OSError:
+-        build_hash = None
+-        print("WARNING: could not use git to retrieve current Blender repository hash...")
+-    if build_hash == '' or build_hash == None:
+-        build_hash = 'UNKNOWN'
++    build_hash = 'UNKNOWN'
+ 
+     return build_hash
+ 
+@@ -478,7 +472,9 @@ def read_opts(env, cfg, args):
          ('BF_3DMOUSE_LIB', '3d mouse library', ''),
          ('BF_3DMOUSE_LIBPATH', '3d mouse library path', ''),
          ('BF_3DMOUSE_LIB_STATIC', '3d mouse static library', ''),
