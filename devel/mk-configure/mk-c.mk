9bb925f348e2952be680d814ecdba27441c1ec89
cheusov <cheusov@pkgsrc.org>
2014-07-13 15:23:41 +0000

    Fix for projects that require MKC_FEATURES

--- devel/mk-configure/mk-c.mk
+++ devel/mk-configure/mk-c.mk
@@ -1,4 +1,6 @@
-# $NetBSD: mk-c.mk,v 1.2 2012/10/18 16:16:54 cheusov Exp $
+# $NetBSD: mk-c.mk,v 1.3 2014/07/13 15:23:41 cheusov Exp $
+
+TOOL_DEPENDS+=	${BUILDLINK_API_DEPENDS.mk-c}:${BUILDLINK_PKGSRCDIR.mk-c}
 
 MAKE_PROGRAM=	mkcmake
 MAKE_ENV+=	MANDIR=${PREFIX}/${PKGMANDIR} INSTALL=${INSTALL:Q}
@@ -9,3 +11,17 @@ do-configure:
 	env ${MAKE_ENV} \
 	    ${MAKE_PROGRAM} ${MAKE_FLAGS} ${BUILD_MAKE_FLAGS} \
 		-f ${MAKE_FILE} errorcheck
+
+###
+BUILDLINK_TREE+=	mk-c
+
+.if !defined(MK_CONFIGURE_BUILDLINK3_MK)
+MK_CONFIGURE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.mk-c+=	mk-configure>=0.26.0
+BUILDLINK_PKGSRCDIR.mk-c=	../../devel/mk-configure
+BUILDLINK_DEPMETHOD.mk-c=	build
+BUILDLINK_INCDIRS.mk-c=		include share/mk-configure/features
+.endif # MK_CONFIGURE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-mk-c
