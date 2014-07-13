26f8ec6c3e9fca67e919231a7e7f8caf3879a986
obache <obache@pkgsrc.org>
2014-07-13 13:00:33 +0000

    libpciacess on NetBSD-5 is missing linkage with libz whereas using it.

--- sysutils/libpciaccess/builtin.mk
+++ sysutils/libpciaccess/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.1 2014/04/29 06:18:42 obache Exp $
+# $NetBSD: builtin.mk,v 1.2 2014/07/13 13:00:33 obache Exp $
 
 BUILTIN_PKG:=	libpciaccess
 PKGCONFIG_FILE.libpciaccess=	${X11BASE}/lib/pkgconfig/pciaccess.pc
@@ -6,3 +6,15 @@ PKGCONFIG_FILE.libpciaccess+=	${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/pciaccess.
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
+
+###
+### The section below only applies if we are not including this file
+### solely to determine whether a built-in implementation exists.
+###
+CHECK_BUILTIN.libpciaccess?=	no
+.if !empty(CHECK_BUILTIN.libpciaccess:M[nN][oO])
+.  if !empty(MACHINE_PLATFORM:MNetBSD-5.[0-9]*-*) && \
+	!empty(USE_BUILTIN.libpciaccess:M[yY][eE][sS])
+BUILDLINK_TRANSFORM+=	l:pciaccess:pciaccess:z
+.  endif
+.endif  # CHECK_BUILTIN.libpciaccess
