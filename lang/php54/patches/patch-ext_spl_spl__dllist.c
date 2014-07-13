7946d51f22ef8721e950c7d5d06899663985a676
taca <taca@pkgsrc.org>
2014-07-13 15:25:03 +0000

    Add fix for CVE-2014-4698 and CVE-2014-4670.
    Bump PKGREVISION.

---
+++ lang/php54/patches/patch-ext_spl_spl__dllist.c
@@ -0,0 +1,18 @@
+$NetBSD: patch-ext_spl_spl__dllist.c,v 1.1 2014/07/13 15:25:03 taca Exp $
+
+Fix for CVE-2014-4670.
+
+--- ext/spl/spl_dllist.c.orig	2014-06-25 22:53:57.000000000 +0000
++++ ext/spl/spl_dllist.c
+@@ -916,6 +916,11 @@ SPL_METHOD(SplDoublyLinkedList, offsetUn
+ 			llist->dtor(element TSRMLS_CC);
+ 		}
+ 
++		if (intern->traverse_pointer == element) {
++			SPL_LLIST_DELREF(element);
++			intern->traverse_pointer = NULL;
++		}
++
+ 		zval_ptr_dtor((zval **)&element->data);
+ 		element->data = NULL;
+ 
