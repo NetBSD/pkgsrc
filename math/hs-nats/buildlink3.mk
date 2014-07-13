70d470fc2d3f19c435d03295f9016afc6fa7f859
szptvlfn <szptvlfn>
2014-07-13 21:16:40 +0000

    Update to 0.2
    CHANGELOG:
    0.2
    ---
    * Added `Hashable` and `Data` support.
    * Will build as full-fledged `Safe` Haskell if you configure with -f-hashable, merely `Trustworthy` otherwise.
    * Allow for manual removal of the `hashable` dependency to support advanced sandbox users who explicitly want to avoid compiling certain dependencies
      they know they aren't using.
    
      We will fix bugs caused by any combination of these package flags, but the API of the package should be considered the default build
      configuration with all of the package dependency flags enabled.

--- math/hs-nats/buildlink3.mk
+++ math/hs-nats/buildlink3.mk
@@ -1,12 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/05/28 22:01:27 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/07/13 21:16:40 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-nats
 
 .if !defined(HS_NATS_BUILDLINK3_MK)
 HS_NATS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-nats+=	hs-nats>=0.1.3
+BUILDLINK_API_DEPENDS.hs-nats+=	hs-nats>=0.2
 BUILDLINK_PKGSRCDIR.hs-nats?=	../../math/hs-nats
+
+.include "../../devel/hs-hashable/buildlink3.mk"
 .endif	# HS_NATS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-nats
