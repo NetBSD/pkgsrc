ba36059974645d961b45fa06ac7e4c597851f609
szptvlfn <szptvlfn@pkgsrc.org>
2014-07-15 21:01:04 +0000

    Update to 2.2.1
    CHANGELOG:
    2.2.1
    -------
    * Provide MINIMAL pragmas for `MonadState`, `MonadWriter`, `MonadReader`
    * Added a cyclic definition of `ask` in terms of `reader` for consistency with `get`/`put` vs. `state` and `tell` vs. `writer`
    * Fix deprecation warnings caused by `transformers` 0.4 deprecating `ErrorT`.
    * Added `Control.Monad.Except` in the style of the other `mtl` re-export modules

--- devel/hs-mtl/buildlink3.mk
+++ devel/hs-mtl/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/05/16 20:31:58 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/07/15 21:01:04 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-mtl
 
 .if !defined(HS_MTL_BUILDLINK3_MK)
 HS_MTL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mtl+=	hs-mtl>=2.2.0
+BUILDLINK_API_DEPENDS.hs-mtl+=	hs-mtl>=2.2.1
 BUILDLINK_PKGSRCDIR.hs-mtl?=	../../devel/hs-mtl
 
 .include "../../devel/hs-transformers/buildlink3.mk"
