ade6ff5a5553029d8f1c99a68f1ccfeb5808fd9e
szptvlfn <szptvlfn@pkgsrc.org>
2014-07-16 21:02:35 +0000

    Update to 0.7.1
    ChangeLog:
    Version 0.7.1 (2014-06-28) *100th Anniversary of the Assassination of Franz Ferdinand*
    --------------------------------------------------------------------------------------
    
    #### Package changes
    
    * Add `IsList` instance for GHC >= 7.8 ([Icelandjack](https://github.com/Icelandjack))

--- devel/hs-dlist/buildlink3.mk
+++ devel/hs-dlist/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/05/18 21:29:39 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/07/16 21:02:35 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-dlist
 
 .if !defined(HS_DLIST_BUILDLINK3_MK)
 HS_DLIST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-dlist+=	hs-dlist>=0.7.0
+BUILDLINK_API_DEPENDS.hs-dlist+=	hs-dlist>=0.7.1
 BUILDLINK_PKGSRCDIR.hs-dlist?=	../../devel/hs-dlist
 .endif	# HS_DLIST_BUILDLINK3_MK
 
