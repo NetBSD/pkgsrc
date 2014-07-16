9fd0e0330846baabe187cbd5b9feea99bfa12e7e
szptvlfn <szptvlfn@pkgsrc.org>
2014-07-16 21:00:42 +0000

    Update to 0.2.5.0
    Changes from https://github.com/tibbe/unordered-containers
    0.2.5.0
     - Test Read/Show instances
     - Read instances
     - Proper Show instance for HashMap
     - IsList instances for GHC ≥ 7.8

--- devel/hs-unordered-containers/buildlink3.mk
+++ devel/hs-unordered-containers/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/05/27 21:33:50 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/07/16 21:00:42 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-unordered-containers
 
 .if !defined(HS_UNORDERED_CONTAINERS_BUILDLINK3_MK)
 HS_UNORDERED_CONTAINERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.4
+BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.5
 BUILDLINK_PKGSRCDIR.hs-unordered-containers?=	../../devel/hs-unordered-containers
 
 .include "../../devel/hs-hashable/buildlink3.mk"
