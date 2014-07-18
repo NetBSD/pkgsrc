9624318630840de21181e13cd7027619d93c85ab
ryoon <ryoon>
2014-07-18 13:42:24 +0000

    Fix build under SCO OpenServer 5.0.7/3.2.

--- databases/mysql55-client/patches/patch-client_mysqladmin.cc
+++ databases/mysql55-client/patches/patch-client_mysqladmin.cc
@@ -1,10 +1,22 @@
-$NetBSD: patch-client_mysqladmin.cc,v 1.2 2012/10/01 15:54:40 adam Exp $
+$NetBSD: patch-client_mysqladmin.cc,v 1.3 2014/07/18 13:42:24 ryoon Exp $
 
 Redefine signals for Irix.
 
---- client/mysqladmin.cc.orig	2010-02-04 11:36:52.000000000 +0000
+--- client/mysqladmin.cc.orig	2014-01-14 08:16:26.000000000 +0000
 +++ client/mysqladmin.cc
-@@ -332,8 +332,14 @@ int main(int argc,char *argv[])
+@@ -29,6 +29,11 @@
+ #define SHUTDOWN_DEF_TIMEOUT 3600		/* Wait for shutdown */
+ #define MAX_TRUNC_LENGTH 3
+ 
++#if defined(_SCO_DS)
++#include "../strtoull.c"
++#define strtoull	pkgsrc_strtoull
++#endif
++
+ char *host= NULL, *user= 0, *opt_password= 0,
+      *default_charset= (char*) MYSQL_AUTODETECT_CHARSET_NAME;
+ char truncated_var_names[MAX_MYSQL_VAR][MAX_TRUNC_LENGTH];
+@@ -332,8 +337,14 @@ int main(int argc,char *argv[])
    if (tty_password)
      opt_password = get_tty_password(NullS);
  
