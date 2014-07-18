f3fed4b42c92a5125c49b2217688b83b36d3e226
fhajny <fhajny@pkgsrc.org>
2014-07-18 10:31:48 +0000

    Update libbson to 0.98.
    Changes in 0.98:
    - This release includes a new memory callback vtable to help in embedding
      situations that have their own custom allocator such as various language
      runtimes.
    - A few compilation fixes for various C++ compilers have also been included.
    
    Changes in 0.8.4:
    - Alignment fixes for Solaris Studio C compiler.
    - RPM and Debian packaging helpers.
    - bson_gettimeofday() has dropped the deprecated timezone field used when
      calling posix gettimeofday(). This eases portability concerns. It is
      technically an ABI break, but since the field was never set, in reality
      it shouldn't be an issue.
    - Multi-byte optimizations for bson_oid_to_string() have been disabled on
      non-x86 based platforms. This should aid in architecture portability.
    - The JSON parser can now support $numberLong.
    - bson_ascii_strtoll() has been added, which is a portable strtoll()
      implementation. This is primarily useful for Windows users and is used
      by the JSON parser.
    - A bug was fixed in bson_iter_find_descendant() where the wrong field
      could be matched if it's prefix matched the query.
    - bson_array_as_json() has been added to convert a bson_t as a top-level
      array.

--- devel/libbson/buildlink3.mk
+++ devel/libbson/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/06/11 13:50:19 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/07/18 10:31:48 fhajny Exp $
 
 BUILDLINK_TREE+=	libbson
 
 .if !defined(LIBBSON_BUILDLINK3_MK)
 LIBBSON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libbson+=	libbson>=0.8.0
+BUILDLINK_API_DEPENDS.libbson+=	libbson>=0.8.4
 BUILDLINK_PKGSRCDIR.libbson?=	../../devel/libbson
 .endif	# LIBBSON_BUILDLINK3_MK
 
