b414d6dcfca6e20f09215a3437ba0bbfeca25f72
obache <obache@pkgsrc.org>
2014-07-18 05:25:42 +0000

    Fixes for old native modular X, as NetBSD-5. * go to buildlink with fixesproto4 if using monolithic X. * bump API_DEPENDS, older will be handled by fixesproto4. * set xextprot requirement if using fixesproto from pkgsrc (XXX >=5.0). * simplify builtin.mk, older will be handled by fixesproto4.

--- x11/fixesproto/buildlink3.mk
+++ x11/fixesproto/buildlink3.mk
@@ -1,6 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:25:40 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2014/07/18 05:25:42 obache Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
+
+.if ${X11_TYPE} != "modular" && \
+	!exists(${X11BASE}/lib/pkgconfig/fixesproto.pc) && \
+	!exists(${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/fixesproto.pc)
+.include "../../x11/fixesproto4/buildlink3.mk"
+.else
 
 BUILDLINK_DEPMETHOD.fixesproto?=	build
 
@@ -9,11 +15,23 @@ BUILDLINK_TREE+=	fixesproto
 .if !defined(FIXESPROTO_BUILDLINK3_MK)
 FIXESPROTO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fixesproto+=	fixesproto>=3.0.0
+BUILDLINK_API_DEPENDS.fixesproto+=	fixesproto>=4.0.0
 BUILDLINK_PKGSRCDIR.fixesproto?=	../../x11/fixesproto
+
+.  if !defined(USE_BUILTIN.fixesproto)
+CHECK_BUILTIN.fixesproto:=	yes
+.include "../../x11/fixesproto/builtin.mk"
+CHECK_BUILTIN.fixesproto:=	no
+.  endif
+
+.  if !empty(USE_BUILTIN.fixesproto:M[Nn][Oo])
+BUILDLINK_API_DEPENDS.xextproto+=	xextproto>=7.0.99.1
+.  endif
 
 .include "../../x11/xextproto/buildlink3.mk"
 .include "../../x11/xproto/buildlink3.mk"
 .endif # FIXESPROTO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-fixesproto
+
+.endif
--- x11/fixesproto/builtin.mk
+++ x11/fixesproto/builtin.mk
@@ -1,62 +1,10 @@
-# $NetBSD: builtin.mk,v 1.7 2008/10/05 21:36:33 cube Exp $
+# $NetBSD: builtin.mk,v 1.8 2014/07/18 05:25:42 obache Exp $
 
 BUILTIN_PKG:=	fixesproto
+PKGCONFIG_FILE.fixesproto=	${X11BASE}/lib/pkgconfig/fixesproto.pc
+PKGCONFIG_FILE.fixesproto+=	${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/fixesproto.pc
 
-BUILTIN_FIND_FILES_VAR:=		H_XFIXESPROTO H_XFIXESWIRE
-BUILTIN_FIND_FILES.H_XFIXESPROTO=	\
-	${X11BASE}/include/X11/extensions/xfixesproto.h
-BUILTIN_FIND_FILES.H_XFIXESWIRE=	\
-	${X11BASE}/include/X11/extensions/xfixeswire.h
-
-.include "../../mk/buildlink3/bsd.builtin.mk"
-
-###
-### Determine if there is a built-in implementation of the package and
-### set IS_BUILTIN.<pkg> appropriately ("yes" or "no").
-###
-.if ${X11BASE} == ${LOCALBASE}
-IS_BUILTIN.fixesproto=	no
-.elif !defined(IS_BUILTIN.fixesproto)
-IS_BUILTIN.fixesproto=	no
-#
-# Here, we skip checking whether the files are under ${LOCALBASE} since
-# we'll consider this X11 package to be built-in even if it's a part
-# of one of the pkgsrc-installed X11 distributions.
-#
-.  if empty(H_XFIXESPROTO:M__nonexistent__) && \
-      empty(H_XFIXESWIRE:M__nonexistent__)
-IS_BUILTIN.fixesproto=	yes
-.  endif
-.endif
-MAKEVARS+=	IS_BUILTIN.fixesproto
-
-###
-### Determine whether we should use the built-in implementation if it
-### exists, and set USE_BUILTIN.<pkg> appropriate ("yes" or "no").
-###
-.if !defined(USE_BUILTIN.fixesproto)
-.  if ${PREFER.fixesproto} == "pkgsrc"
-USE_BUILTIN.fixesproto=	no
-.  else
-USE_BUILTIN.fixesproto=	${IS_BUILTIN.fixesproto}
-.    if defined(BUILTIN_PKG.fixesproto) && \
-        !empty(IS_BUILTIN.fixesproto:M[yY][eE][sS])
-USE_BUILTIN.fixesproto=	yes
-.      for _dep_ in ${BUILDLINK_API_DEPENDS.fixesproto}
-.        if !empty(USE_BUILTIN.fixesproto:M[yY][eE][sS])
-USE_BUILTIN.fixesproto!=						\
-	if ${PKG_ADMIN} pmatch ${_dep_:Q} ${BUILTIN_PKG.fixesproto:Q}; then \
-		${ECHO} yes;						\
-	else								\
-		${ECHO} no;						\
-	fi
-.        endif
-.      endfor
-.    endif
-.  endif  # PREFER.fixesproto
-.endif
-MAKEVARS+=	USE_BUILTIN.fixesproto
-
+.include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
 
 CHECK_BUILTIN.fixesproto?=	no
