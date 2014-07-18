95c2d6c413e171cf2e30198adf7d88e3e3c7c0ae
fhajny <fhajny@pkgsrc.org>
2014-07-18 10:41:43 +0000

    Update mongo-c-driver to 0.98.0.
    Changes in 0.98.0:
    - This release is primarily a bugfix release and stabilization effort as we
      approach 1.0 of the MongoDB C driver.
    - This release requires 0.98.0 of Libbson for improvements to the memory
      management system. You can now setup custom memory allocators at the
      start of the process.
    
    Changes in 0.96.4:
    - build/mci.sh script for automatically building Debian packages, RPMs,
      and Solaris packaging based on the host operating system.
    - Various libbson improvements, now depending on 0.8.4.
    - Alignment fixes for Solaris Studio C compiler via libbson.
    - Addition of mongoc_gridfs_remove_by_filename() for removing a file from
      gridfs by filename.
    - client command functions can now take a fully qualified namespace.
    - collections can now support names that indicate a command namespace.
    - Commands will no longer fail if they do not contain an "ok" field.
    - OP_QUERY will now set the slaveOk bit in the wire protocol if
      readPreferences are set to non-PRIMARY.
    - Various documentation and build fixes.

--- databases/mongo-c-driver/buildlink3.mk
+++ databases/mongo-c-driver/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/05/07 15:47:00 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/07/18 10:41:43 fhajny Exp $
 
 BUILDLINK_TREE+=	mongo-c-driver
 
 .if !defined(MONGO_C_DRIVER_BUILDLINK3_MK)
 MONGO_C_DRIVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mongo-c-driver+=	mongo-c-driver>=0.94.2
+BUILDLINK_API_DEPENDS.mongo-c-driver+=	mongo-c-driver>=0.98
 BUILDLINK_PKGSRCDIR.mongo-c-driver?=	../../databases/mongo-c-driver
 
 pkgbase := mongo-c-driver
