fec15ea3aa5670f8768337b2bc6b0d8a352ff32e
szptvlfn <szptvlfn>
2014-07-19 13:16:28 +0000

    Update to 0.10.11.0
    changelog:
    Changes in version 0.10.11.0
    
    * Support OverloadedLists for boxed Vector in GHC >= 7.8
    
    Changes in version 0.10.10.0
    
     * Minor version bump to rectify PVP violation occured in 0.10.9.3 release
    
    Changes in version 0.10.9.3 (deprecated)
    
     * Add support for OverloadedLists in GHC >= 7.8

--- devel/hs-vector/buildlink3.mk
+++ devel/hs-vector/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/05/26 22:02:40 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/07/19 13:16:28 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-vector
 
 .if !defined(HS_VECTOR_BUILDLINK3_MK)
 HS_VECTOR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.10.9
+BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.10.11
 BUILDLINK_PKGSRCDIR.hs-vector?=	../../devel/hs-vector
 
 .include "../../devel/hs-primitive/buildlink3.mk"
