f0f869a12cd5beeab42024546dc36515c3fca0d5
szptvlfn <szptvlfn@pkgsrc.org>
2014-07-19 13:12:02 +0000

    Update to 0.4.2
    Changes from https://github.com/dreixel/syb
    0.4.2
     - Remove duplicate instance in GHC > 7.8
     - Merge pull request #8 from yallop/patch-1
       (Trivial fix): Add a type signature for 'marlow'

--- devel/hs-syb/buildlink3.mk
+++ devel/hs-syb/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/05/21 21:00:19 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/07/19 13:12:02 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-syb
 
 .if !defined(HS_SYB_BUILDLINK3_MK)
 HS_SYB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-syb+=	hs-syb>=0.4.1
+BUILDLINK_API_DEPENDS.hs-syb+=	hs-syb>=0.4.2
 BUILDLINK_PKGSRCDIR.hs-syb?=	../../devel/hs-syb
 .endif	# HS_SYB_BUILDLINK3_MK
 
