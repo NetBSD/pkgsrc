bf23ee84f8c4614aad4ab1780880dbcf2a82bea7
ryoon <ryoon@pkgsrc.org>
2014-07-19 09:08:36 +0000

    Update to 0.1pre1949
    Changelog:
    * Bugfixes
    * More tests

--- devel/gyp/patches/patch-gyptest.py
+++ devel/gyp/patches/patch-gyptest.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-gyptest.py,v 1.2 2013/09/08 16:33:25 ryoon Exp $
+$NetBSD: patch-gyptest.py,v 1.3 2014/07/19 09:08:36 ryoon Exp $
 
 * Add NetBSD 5 and 6 target
 
---- gyptest.py.orig	2013-09-06 23:07:42.000000000 +0000
+--- gyptest.py.orig	2014-07-14 14:19:50.000000000 +0000
 +++ gyptest.py
-@@ -218,6 +218,9 @@ def main(argv=None):
-     format_list = {
+@@ -219,6 +219,9 @@ def main(argv=None):
+       'aix5':     ['make'],
        'freebsd7': ['make'],
        'freebsd8': ['make'],
 +      'freebsd9': ['make'],
