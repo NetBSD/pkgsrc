aaa08975b57ec95d8c3cd5e2e093a1a9b1f2ec4f
adam <adam@pkgsrc.org>
2014-07-20 17:43:29 +0000

    Changes 2.3.4: The most important change in this release is that TLS version negotiation is no longer used unless it's explicitly turned on in the configuration files, thus reverting back to the 2.3.2 behaviour as interoperability issues were encountered in 2.3.3. Other notable changes include addition of SSL library version reporting, fixing of SOCKSv5 authentication logic and making serial env exporting consistent between OpenSSL and PolarSSL. This release also contains a number of other bug fixes and small enhancements.

--- net/openvpn/patches/patch-src_openvpn_socket.c
+++ net/openvpn/patches/patch-src_openvpn_socket.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_openvpn_socket.c,v 1.1 2013/07/31 06:53:21 adam Exp $
+$NetBSD: patch-src_openvpn_socket.c,v 1.2 2014/07/20 17:43:29 adam Exp $
 
 Fix for systems without ipi_spec_dst in struct in_pktinfo.
 
---- src/openvpn/socket.c.orig	2013-07-31 06:14:52.000000000 +0000
+--- src/openvpn/socket.c.orig	2014-05-01 11:12:22.000000000 +0000
 +++ src/openvpn/socket.c
-@@ -652,7 +652,7 @@ create_socket_udp (const unsigned int fl
+@@ -654,7 +654,7 @@ create_socket_udp (const unsigned int fl
    else if (flags & SF_USE_IP_PKTINFO)
      {
        int pad = 1;
@@ -13,7 +13,7 @@ Fix for systems without ipi_spec_dst in struct in_pktinfo.
        if (setsockopt (sd, SOL_IP, IP_PKTINFO,
  		      (void*)&pad, sizeof(pad)) < 0)
          msg(M_ERR, "UDP: failed setsockopt for IP_PKTINFO");
-@@ -2252,7 +2252,7 @@ print_link_socket_actual_ex (const struc
+@@ -2254,7 +2254,7 @@ print_link_socket_actual_ex (const struc
  		  struct openvpn_sockaddr sa;
  		  CLEAR (sa);
  		  sa.addr.in4.sin_family = AF_INET;
@@ -22,7 +22,7 @@ Fix for systems without ipi_spec_dst in struct in_pktinfo.
  		  sa.addr.in4.sin_addr = act->pi.in4.ipi_spec_dst;
  		  if_indextoname(act->pi.in4.ipi_ifindex, ifname);
  #elif defined(IP_RECVDSTADDR)
-@@ -2649,7 +2649,7 @@ link_socket_read_tcp (struct link_socket
+@@ -2651,7 +2651,7 @@ link_socket_read_tcp (struct link_socket
  struct openvpn_in4_pktinfo
  {
    struct cmsghdr cmsghdr;
@@ -31,7 +31,7 @@ Fix for systems without ipi_spec_dst in struct in_pktinfo.
    struct in_pktinfo pi4;
  #elif defined(IP_RECVDSTADDR)
    struct in_addr pi4;
-@@ -2694,7 +2694,7 @@ link_socket_read_udp_posix_recvmsg (stru
+@@ -2696,7 +2696,7 @@ link_socket_read_udp_posix_recvmsg (stru
        cmsg = CMSG_FIRSTHDR (&mesg);
        if (cmsg != NULL
  	  && CMSG_NXTHDR (&mesg, cmsg) == NULL
@@ -40,7 +40,7 @@ Fix for systems without ipi_spec_dst in struct in_pktinfo.
  	  && cmsg->cmsg_level == SOL_IP 
  	  && cmsg->cmsg_type == IP_PKTINFO
  #elif defined(IP_RECVDSTADDR)
-@@ -2705,7 +2705,7 @@ link_socket_read_udp_posix_recvmsg (stru
+@@ -2707,7 +2707,7 @@ link_socket_read_udp_posix_recvmsg (stru
  #endif
  	  && cmsg->cmsg_len >= sizeof (struct openvpn_in4_pktinfo))
  	{
@@ -49,12 +49,12 @@ Fix for systems without ipi_spec_dst in struct in_pktinfo.
  	  struct in_pktinfo *pkti = (struct in_pktinfo *) CMSG_DATA (cmsg);
  	  from->pi.in4.ipi_ifindex = pkti->ipi_ifindex;
  	  from->pi.in4.ipi_spec_dst = pkti->ipi_spec_dst;
-@@ -2803,7 +2803,7 @@ link_socket_write_udp_posix_sendmsg (str
+@@ -2802,7 +2802,7 @@ link_socket_write_udp_posix_sendmsg (str
+         mesg.msg_namelen = sizeof (struct sockaddr_in);
+         mesg.msg_control = &opi;
          mesg.msg_flags = 0;
-         cmsg = CMSG_FIRSTHDR (&mesg);
-         cmsg->cmsg_len = sizeof (struct openvpn_in4_pktinfo);
 -#ifdef HAVE_IN_PKTINFO
 +#if defined(HAVE_IN_PKTINFO) && defined(HAVE_IPI_SPEC_DST)
-         cmsg->cmsg_level = SOL_IP;
-         cmsg->cmsg_type = IP_PKTINFO;
- 	{
+         mesg.msg_controllen = sizeof (struct openvpn_in4_pktinfo);
+         cmsg = CMSG_FIRSTHDR (&mesg);
+         cmsg->cmsg_len = sizeof (struct openvpn_in4_pktinfo);
