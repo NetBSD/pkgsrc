6e119f209ef089ae077601f0cb8e7f3b5d81f475
tsutsui <tsutsui>
2014-07-21 15:42:56 +0000

    Update mlterm to 3.3.7.
    pkgsrc changes:
     - remove post-3.3.6 fix patches
     - pull post-3.3.7 fix patches
       (see patch comments for details)
    
    Changes noted in doc/en/ReleaseNote:
    
    ver 3.3.7
    * Enable image processing on background terminals.
    * Improve the performance of processing images on 8 or less bpp framebuffer.
    * Support SGR 5, 9, 21, 25 and 29.
    * Support DECFRA, SL and SR.
    * Starting mlterm with -j option executes mlclient if daemon process has already
      started.
    * Change the maximum number of "U+XXXX-XXXX" in ~/.mlterm/(t,v,)(aa)font
      from 7 to 511.
    * Add "unlimted" to "Backlog size" entry in mlconfig.
      Add "UNICODE (ARABIC)" and "UNICODE (EMOJI)" to "Font name" entry.
    * Add --it/it_color, --bl/bl_color and --co/co_color options.
    * Add --itfont/use_italic_font option.
    * Add --bisep/bidi_separators option.
    * Add --fullwidth/unicode_full_width_areas option.
    * Add --working-directory/working_directory option.
    * Add --deffont option.
    * Disable "_HALFWIDTH" suffix in ~/.mlterm/(t,v)(aa)font files.
    * Drop --ucsprop/use_unicode_property option.
    * Bug fixes:
      Fix the failure of saving font configurations from mlconfig.
      Fix the bug which sometimes causes the failure of searching closest colors on X/8bpp.

--- x11/mlterm/patches/patch-xwindow_fb_x.h
+++ x11/mlterm/patches/patch-xwindow_fb_x.h
@@ -1,39 +1,17 @@
-$NetBSD: patch-xwindow_fb_x.h,v 1.1 2014/06/08 16:46:25 tsutsui Exp $
+$NetBSD: patch-xwindow_fb_x.h,v 1.2 2014/07/21 15:42:56 tsutsui Exp $
 
-Pull upstream changes:
- * fb/x_display.c, x.h: x_display_get_closest_color() caches 512 + 64 pixels.
-https://bitbucket.org/arakiken/mlterm/commits/f3ea4bb79134409b0b66ba22676b9b4d1f5e1c02
+- pull post 3.3.7 fixes:
+http://sourceforge.net/projects/mlterm/files/01release/mlterm-3.3.7/mlterm-3.3.7-fixes.patch/download
+ * x.h: KS_Print -> KS_Print_Screen
 
---- xwindow/fb/x.h.orig	2014-05-25 05:42:24.000000000 +0000
+--- xwindow/fb/x.h.orig	2014-07-05 21:40:41.000000000 +0000
 +++ xwindow/fb/x.h
-@@ -72,8 +72,28 @@ typedef struct
- 	fb_cmap_t *  cmap ;
- 	fb_cmap_t *  cmap_orig ;
- 
--	u_long  prev_pixel ;
--	u_long  prev_closest_color ;
-+	struct
-+	{
-+		/*
-+		 * Closest color is searched by 15 bits.
-+		 *   R(3)G(3)B(3): segment(9bits)
-+		 *   R(2)G(2)B(2): offset(6bits)
-+		 */
-+
-+		/* Closest pixels cached by every segment. */
-+		u_int8_t  pixels[512] ;		/* 2^9 */
-+		u_int8_t  offsets[512] ;	/* 2^9 */
-+
-+		/* Closest pixels cached in recent segment. */
-+		u_int8_t  seg_pixels[64] ;
-+		struct
-+		{
-+			u_int32_t  offsets ;
-+			u_int  segment ;
-+
-+		} seg[2] ;
-+
-+	} *  color_cache ;
- 
- 	int  key_state ;
- 	int  lock_state ;
+@@ -309,7 +309,7 @@ typedef int XFontSet ;	/* dummy */
+ #define XK_Right	KS_Right
+ #define XK_Down		KS_Down
+ #define XK_Select	KS_Select
+-#define XK_Print	KS_Print
++#define XK_Print	KS_Print_Screen
+ #define XK_Execute	KS_Execute
+ #define XK_Insert	KS_Insert
+ #define XK_Delete	KS_Delete
