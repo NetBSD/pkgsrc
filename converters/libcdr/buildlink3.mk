65a23361d2f75dad820a5b8f9e09bdd9ad9c265e
wiz <wiz@pkgsrc.org>
2014-07-22 11:07:58 +0000

    Update to 0.1.0. Ride libwpd bump.
    libcdr 0.1.0
    
    * switch to librevenge

--- converters/libcdr/buildlink3.mk
+++ converters/libcdr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/07/22 10:53:29 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/07/22 11:07:58 wiz Exp $
 
 BUILDLINK_TREE+=	libcdr
 
@@ -6,11 +6,12 @@ BUILDLINK_TREE+=	libcdr
 LIBCDR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libcdr+=	libcdr>=0.0.6
-BUILDLINK_ABI_DEPENDS.libcdr?=	libcdr>=0.0.16nb1
+BUILDLINK_ABI_DEPENDS.libcdr?=	libcdr>=0.1.0
 BUILDLINK_PKGSRCDIR.libcdr?=	../../converters/libcdr
 
 .include "../../converters/libwpd/buildlink3.mk"
 .include "../../converters/libwpg/buildlink3.mk"
+.include "../../converters/librevenge/buildlink3.mk"
 .include "../../graphics/lcms2/buildlink3.mk"
 .endif	# LIBCDR_BUILDLINK3_MK
 
