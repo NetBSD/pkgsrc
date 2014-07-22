e053400e9fa66a6d7c313530e4b046d66f6e34b9
wiz <wiz@pkgsrc.org>
2014-07-22 11:17:44 +0000

    Update to 0.1.1. Ride libwpd bump.
    libmspub 0.1.1
    
    - Fix a crash when opening certain documents in LibreOffice, caused by a symbol
      clash.
    - Several minor code improvements.
    
    libmspub 0.1.0
    
    - switch to librevenge

--- converters/libmspub/buildlink3.mk
+++ converters/libmspub/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/07/22 10:53:30 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/07/22 11:17:44 wiz Exp $
 
 BUILDLINK_TREE+=	libmspub
 
@@ -6,11 +6,12 @@ BUILDLINK_TREE+=	libmspub
 LIBMSPUB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libmspub+=	libmspub>=0.0.6
-BUILDLINK_ABI_DEPENDS.libmspub?=	libmspub>=0.0.6nb5
+BUILDLINK_ABI_DEPENDS.libmspub?=	libmspub>=0.1.1
 BUILDLINK_PKGSRCDIR.libmspub?=	../../converters/libmspub
 
 .include "../../converters/libwpd/buildlink3.mk"
 .include "../../converters/libwpg/buildlink3.mk"
+.include "../../converters/librevenge/buildlink3.mk"
 #.include "../../devel/boost-libs/buildlink3.mk"
 #.include "../../devel/zlib/buildlink3.mk"
 #.include "../../textproc/icu/buildlink3.mk"
