332d7c08e5ca2f854a5eb4fe9b07508738711d53
wiz <wiz@pkgsrc.org>
2014-07-22 10:55:14 +0000

    Fix scripto.

--- converters/libwpd/buildlink3.mk
+++ converters/libwpd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2014/07/22 10:52:24 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2014/07/22 10:55:14 wiz Exp $
 
 BUILDLINK_TREE+=	libwpd
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libwpd
 LIBWPD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libwpd+=	libwpd>=0.9.5
-BUILDLINK_ABI_DEPENDS.libwpd+=	libwpd>=0.10.0nb1
+BUILDLINK_ABI_DEPENDS.libwpd+=	libwpd>=0.10.0
 BUILDLINK_PKGSRCDIR.libwpd?=	../../converters/libwpd
 
 .include "../../converters/librevenge/buildlink3.mk"
