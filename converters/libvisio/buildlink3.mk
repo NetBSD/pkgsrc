cf09a06bc95d96a0f016725a910f93ddfaf3d201
wiz <wiz@pkgsrc.org>
2014-07-22 11:20:33 +0000

    Update to 0.1.0. Ride libwpd bump.
    libvisio 0.1.0
    
    - switch to librevenge

--- converters/libvisio/buildlink3.mk
+++ converters/libvisio/buildlink3.mk
@@ -1,11 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/16 17:40:37 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/07/22 11:20:33 wiz Exp $
 
 BUILDLINK_TREE+=	libvisio
 
 .if !defined(LIBVISIO_BUILDLINK3_MK)
 LIBVISIO_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libvisio+=	libvisio>=0.0.15
+BUILDLINK_ABI_DEPENDS.libvisio+=	libvisio>=0.1.0
 BUILDLINK_PKGSRCDIR.libvisio?=		../../converters/libvisio
+
+.include "../../converters/librevenge/buildlink3.mk"
 .endif
 
 BUILDLINK_TREE+=	-libvisio
