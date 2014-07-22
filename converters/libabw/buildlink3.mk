c8396c0552c4b2454a7b835ab6597f44f0fbdc1a
wiz <wiz@pkgsrc.org>
2014-07-22 11:01:53 +0000

    Update to 0.1.0. Ride libwpd bump.
    0.0.1 -> 0.1.0
     - Switch to librevenge
     - Do not add extra tab for list indentation
     - Avoid possible crash on file format detection
     - Ignore cells that are not inside a table

--- converters/libabw/buildlink3.mk
+++ converters/libabw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/07/22 10:53:29 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/07/22 11:01:53 wiz Exp $
 
 BUILDLINK_TREE+=	libabw
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libabw
 LIBABW_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libabw+=	libabw>=0.0.1
-BUILDLINK_ABI_DEPENDS.libabw?=	libabw>=0.0.2nb2
+BUILDLINK_ABI_DEPENDS.libabw?=	libabw>=0.1.0
 BUILDLINK_PKGSRCDIR.libabw?=	../../converters/libabw
 
 .include "../../converters/libwpd/buildlink3.mk"
