6b54ef0a384bfeb72e0e3d2fc424b451aedbe847
wiz <wiz@pkgsrc.org>
2014-07-22 11:13:26 +0000

    Update to 0.1.1. Ride libwpd bump.
    libetonyek 0.1.1
    
    - Fix detection of Keynote 3 documents.
    
    libetonyek 0.1.0
    
    - switch to librevenge
    - add BIPU support for import of Pages documents
    - add skeleton for (future) import of Numbers spreadsheets

--- converters/libetonyek/buildlink3.mk
+++ converters/libetonyek/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/07/22 10:53:30 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/07/22 11:13:26 wiz Exp $
 
 BUILDLINK_TREE+=	libetonyek
 
@@ -6,10 +6,11 @@ BUILDLINK_TREE+=	libetonyek
 LIBETONYEK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libetonyek+=	libetonyek>=0.0.3
-BUILDLINK_ABI_DEPENDS.libetonyek?=	libetonyek>=0.0.4nb1
+BUILDLINK_ABI_DEPENDS.libetonyek?=	libetonyek>=0.1.1
 BUILDLINK_PKGSRCDIR.libetonyek?=	../../converters/libetonyek
 
 .include "../../converters/libwpd/buildlink3.mk"
+.include "../../converters/librevenge/buildlink3.mk"
 .endif	# LIBETONYEK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libetonyek
