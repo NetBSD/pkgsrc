80fc32d67b47a004100b12e830aa0c65b6bb154c
wiz <wiz@pkgsrc.org>
2014-07-22 10:58:04 +0000

    Update to 0.3.0. Ride libwpg bump.
    0.2.2 - 0.3.0
    
    - Switch to librevenge
    - Miscellaneous fixes and cleanups

--- converters/libwpg/buildlink3.mk
+++ converters/libwpg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/07/22 10:53:30 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/07/22 10:58:04 wiz Exp $
 
 BUILDLINK_TREE+=	libwpg
 
@@ -6,10 +6,11 @@ BUILDLINK_TREE+=	libwpg
 LIBWPG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libwpg+=	libwpg>=0.1.3
-BUILDLINK_ABI_DEPENDS.libwpg+=	libwpg>=0.2.2nb2
+BUILDLINK_ABI_DEPENDS.libwpg+=	libwpg>=0.3.0
 BUILDLINK_PKGSRCDIR.libwpg?=	../../converters/libwpg
 
 .include "../../converters/libwpd/buildlink3.mk"
+.include "../../converters/librevenge/buildlink3.mk"
 .endif	# LIBWPG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libwpg
