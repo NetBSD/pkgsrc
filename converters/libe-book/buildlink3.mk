66983f84e492835b5436a96ca226f9a089da696d
wiz <wiz@pkgsrc.org>
2014-07-23 06:15:22 +0000

    Update to 0.1.1. Ride libwpd bump.
    libe-book 0.1.1
    
    - Fix various problems when using --disable-experimental.
    - Work around MSVC bug.
    - Fix parsing of zipped FictionBook 2 files.
    
    libe-book 0.1.0
    
    - switch to librevenge
    - PeanutPress: make sure the output is UTF-8
    - TCR: make sure the output is UTF-8
    - sanitize format detection

--- converters/libe-book/buildlink3.mk
+++ converters/libe-book/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/07/22 10:53:29 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/07/23 06:15:22 wiz Exp $
 
 BUILDLINK_TREE+=	libe-book
 
@@ -6,9 +6,10 @@ BUILDLINK_TREE+=	libe-book
 LIBE_BOOK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libe-book+=	libe-book>=0.0.2
-BUILDLINK_ABI_DEPENDS.libe-book?=	libe-book>=0.0.3nb2
+BUILDLINK_ABI_DEPENDS.libe-book?=	libe-book>=0.1.1
 BUILDLINK_PKGSRCDIR.libe-book?=	../../converters/libe-book
 
+.include "../../converters/librevenge/buildlink3.mk"
 .include "../../converters/libwpd/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .endif	# LIBE_BOOK_BUILDLINK3_MK
