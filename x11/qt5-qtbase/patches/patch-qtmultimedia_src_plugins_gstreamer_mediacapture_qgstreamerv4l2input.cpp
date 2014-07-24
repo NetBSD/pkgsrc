9b134fa945c946c229a6ebdc7c7ddf253cf3f5ba
jperkin <jperkin>
2014-07-24 15:51:56 +0000

    Apply some patches to fix x11/qt5-qtmultimedia build on SunOS.

--- x11/qt5-qtbase/patches/patch-qtmultimedia_src_plugins_gstreamer_mediacapture_qgstreamerv4l2input.cpp
+++ x11/qt5-qtbase/patches/patch-qtmultimedia_src_plugins_gstreamer_mediacapture_qgstreamerv4l2input.cpp
@@ -1,11 +1,13 @@
-$NetBSD: patch-qtmultimedia_src_plugins_gstreamer_mediacapture_qgstreamerv4l2input.cpp,v 1.1 2013/12/30 06:37:11 ryoon Exp $
+$NetBSD: patch-qtmultimedia_src_plugins_gstreamer_mediacapture_qgstreamerv4l2input.cpp,v 1.2 2014/07/24 15:51:56 jperkin Exp $
 
-NetBSD doesn't have support for v4l2_frmsizeenum and related logic.
+NetBSD and SunOS don't have support for v4l2_frmsizeenum and related logic.
 Should be fixed properly.
 
---- qtmultimedia/src/plugins/gstreamer/mediacapture/qgstreamerv4l2input.cpp.orig	2013-12-20 09:12:19.000000000 +0000
+Pull in correct includes on NetBSD and SunOS.
+
+--- qtmultimedia/src/plugins/gstreamer/mediacapture/qgstreamerv4l2input.cpp.orig	2014-02-01 20:37:59.000000000 +0000
 +++ qtmultimedia/src/plugins/gstreamer/mediacapture/qgstreamerv4l2input.cpp
-@@ -44,7 +44,12 @@
+@@ -44,7 +44,14 @@
  #include <QtCore/qdebug.h>
  #include <QtCore/qfile.h>
  
@@ -14,11 +16,13 @@ Should be fixed properly.
  #include <linux/types.h>
 +#elif defined(__NetBSD__)
 +#include <sys/videoio.h>
++#elif defined(__sun)
++#include <sys/videodev2.h>
 +#endif
  #include <sys/time.h>
  #include <sys/ioctl.h>
  #include <sys/poll.h>
-@@ -54,7 +59,6 @@
+@@ -54,7 +61,6 @@
  #include <string.h>
  #include <stdlib.h>
  #include <sys/mman.h>
@@ -26,16 +30,16 @@ Should be fixed properly.
  
  QT_BEGIN_NAMESPACE
  static inline uint qHash(const QSize& key) { return uint(key.width()*256+key.height()); }
-@@ -154,7 +158,7 @@ void QGstreamerV4L2Input::updateSupporte
+@@ -154,7 +160,7 @@ void QGstreamerV4L2Input::updateSupporte
  
  
      //get the list of resolutions:
 -
-+#if !defined(__NetBSD__)
++#if !defined(__NetBSD__) && !defined(__sun)
      foreach (quint32 format, supportedFormats) {
          struct v4l2_frmsizeenum formatSize;
          memset(&formatSize, 0, sizeof(formatSize));
-@@ -256,6 +260,7 @@ void QGstreamerV4L2Input::updateSupporte
+@@ -256,6 +262,7 @@ void QGstreamerV4L2Input::updateSupporte
              m_ratesByResolution[s].unite(frameRates.toSet());
          }
      }
