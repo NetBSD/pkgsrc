fb89f0e52265a3995dbebbe653da7d5cc85d6fba
jperkin <jperkin@pkgsrc.org>
2014-07-24 15:51:56 +0000

    Apply some patches to fix x11/qt5-qtmultimedia build on SunOS.

--- x11/qt5-qtbase/patches/patch-qtmultimedia_src_gsttools_qgstreamervideoinputdevicecontrol.cpp
+++ x11/qt5-qtbase/patches/patch-qtmultimedia_src_gsttools_qgstreamervideoinputdevicecontrol.cpp
@@ -1,8 +1,10 @@
-$NetBSD: patch-qtmultimedia_src_gsttools_qgstreamervideoinputdevicecontrol.cpp,v 1.2 2014/04/23 07:05:31 adam Exp $
+$NetBSD: patch-qtmultimedia_src_gsttools_qgstreamervideoinputdevicecontrol.cpp,v 1.3 2014/07/24 15:51:56 jperkin Exp $
+
+Pull in correct includes on NetBSD and SunOS.
 
 --- qtmultimedia/src/gsttools/qgstreamervideoinputdevicecontrol.cpp.orig	2014-02-01 20:37:58.000000000 +0000
 +++ qtmultimedia/src/gsttools/qgstreamervideoinputdevicecontrol.cpp
-@@ -44,7 +44,12 @@
+@@ -44,7 +44,14 @@
  #include <QtCore/QDir>
  #include <QtCore/QDebug>
  
@@ -11,11 +13,13 @@ $NetBSD: patch-qtmultimedia_src_gsttools_qgstreamervideoinputdevicecontrol.cpp,v
 +#include <linux/videodev2.h>
 +#elif defined(__NetBSD__)
 +#include <sys/videoio.h>
++#elif defined(__sun)
++#include <sys/videodev2.h>
 +#endif
  #include <sys/time.h>
  #include <sys/ioctl.h>
  #include <sys/poll.h>
-@@ -54,7 +59,6 @@
+@@ -54,7 +61,6 @@
  #include <string.h>
  #include <stdlib.h>
  #include <sys/mman.h>
