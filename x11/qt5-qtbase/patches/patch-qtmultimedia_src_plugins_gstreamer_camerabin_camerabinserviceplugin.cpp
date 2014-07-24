1b2e6d1517fd547ffda79dea4b0b9862ba7f7257
jperkin <jperkin@pkgsrc.org>
2014-07-24 15:51:56 +0000

    Apply some patches to fix x11/qt5-qtmultimedia build on SunOS.

--- x11/qt5-qtbase/patches/patch-qtmultimedia_src_plugins_gstreamer_camerabin_camerabinserviceplugin.cpp
+++ x11/qt5-qtbase/patches/patch-qtmultimedia_src_plugins_gstreamer_camerabin_camerabinserviceplugin.cpp
@@ -1,8 +1,10 @@
-$NetBSD: patch-qtmultimedia_src_plugins_gstreamer_camerabin_camerabinserviceplugin.cpp,v 1.1 2013/12/30 06:37:11 ryoon Exp $
+$NetBSD: patch-qtmultimedia_src_plugins_gstreamer_camerabin_camerabinserviceplugin.cpp,v 1.2 2014/07/24 15:51:56 jperkin Exp $
 
---- qtmultimedia/src/plugins/gstreamer/camerabin/camerabinserviceplugin.cpp.orig	2013-12-20 09:28:04.000000000 +0000
+Pull in correct includes on NetBSD and SunOS.
+
+--- qtmultimedia/src/plugins/gstreamer/camerabin/camerabinserviceplugin.cpp.orig	2014-02-01 20:37:59.000000000 +0000
 +++ qtmultimedia/src/plugins/gstreamer/camerabin/camerabinserviceplugin.cpp
-@@ -50,7 +50,12 @@
+@@ -50,7 +50,14 @@
  #include "camerabinservice.h"
  #include <private/qgstutils_p.h>
  
@@ -11,11 +13,13 @@ $NetBSD: patch-qtmultimedia_src_plugins_gstreamer_camerabin_camerabinserviceplug
 +#include <linux/videodev2.h>
 +#elif defined(__NetBSD__)
 +#include <sys/videoio.h>
++#elif defined(__sun)
++#include <sys/videodev2.h>
 +#endif
  #include <sys/time.h>
  #include <sys/ioctl.h>
  #include <sys/poll.h>
-@@ -60,7 +65,6 @@
+@@ -60,7 +67,6 @@
  #include <string.h>
  #include <stdlib.h>
  #include <sys/mman.h>
