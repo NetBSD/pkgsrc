3daa6120c58e4858c6f0c473cbaf357fd1a53142
ryoon <ryoon@pkgsrc.org>
2014-07-24 14:57:12 +0000

    Update to 31.0
    Changelog:
        New
        Add the search field to the new tab page
    
        New
        Support of Prefer:Safe http header for parental control (learn more)
    
        New
        mozilla::pkix as default certificate verifier (learn more)
    
        New
        Block malware from downloaded files (learn more)
    
        New
        Partial implementation of the OpenType MATH table (section 6.3.6) see documentation about mathematical fonts and the MathML Torture Test for details
    
        New
        audio/video .ogg and .pdf files handled by Firefox if no application specified (Windows only)
    
        New
        Upper Sorbian [hsb] locale added
    
        Changed
        Removal of the CAPS infrastructure for specifying site-specific permissions (via capability.policy.* preferences). Most notably, attempts to use this functionality to grant access to the clipboard will no longer work. The sole exception is the checkloaduri permission, which may still be used as before to allow sites to load file:// URIs.
    
        HTML5
        WebVTT implemented and enabled (learn more)
    
        HTML5
        CSS3 variables implemented (learn more)
    
        Developer
        Developer Tools: Add-on Debugger (learn more)
    
        Developer
        Developer Tools: Canvas Debugger (learn more)
    
        Developer
        New Array built-in: Array.prototype.fill() (learn more)
    
        Developer
        New Object built-in: Object.setPrototypeOf() (learn more)
    
        Developer
        CSP 1.1 nonce-source and hash-source enabled by default
    
        Developer
        Developer Tools: Eyedropper tool added to the color picker (learn more)
    
        Developer
        Developer Tools: Editable Box Model (learn more)
    
        Developer
        Developer Tools: Code Editor improvements (learn more)
    
        Developer
        Developer Tools: Console stack traces (learn more)
    
        Developer
        Developer Tools: Copy as cURL (learn more)
    
        Developer
        Developer Tools: Styled console logs (learn more)
    
        Developer
        navigator.sendBeacon enabled by default (learn more)
    
        Developer
        Dialogs spawned from the onbeforeunload event no longer block access to the rest of the browser
    
        Fixed
        Search for partially selected link text from context menu (985824)
    
        Fixed
        Various security fixes
    
    Fixed in Firefox 31
    MFSA 2014-66 IFRAME sandbox same-origin access through redirect
    MFSA 2014-65 Certificate parsing broken by non-standard character encoding
    MFSA 2014-64 Crash in Skia library when scaling high quality images
    MFSA 2014-63 Use-after-free while when manipulating certificates in the trusted cache
    MFSA 2014-62 Exploitable WebGL crash with Cesium JavaScript library
    MFSA 2014-61 Use-after-free with FireOnStateChange event
    MFSA 2014-60 Toolbar dialog customization event spoofing
    MFSA 2014-59 Use-after-free in DirectWrite font handling
    MFSA 2014-58 Use-after-free in Web Audio due to incorrect control message ordering
    MFSA 2014-57 Buffer overflow during Web Audio buffering for playback
    MFSA 2014-56 Miscellaneous memory safety hazards (rv:31.0 / rv:24.7)

--- www/firefox/patches/patch-ipc_ipdl_ipdl_cxx_cgen.py
+++ www/firefox/patches/patch-ipc_ipdl_ipdl_cxx_cgen.py
@@ -1,28 +1,27 @@
-$NetBSD: patch-ipc_ipdl_ipdl_cxx_cgen.py,v 1.1 2014/06/19 20:31:03 martin Exp $
+$NetBSD: patch-ipc_ipdl_ipdl_cxx_cgen.py,v 1.2 2014/07/24 14:57:12 ryoon Exp $
 
 Part of https://bugzilla.mozilla.org/show_bug.cgi?id=1026499:
 Make sure storage provided for attributes is properly aligned
 in the generated C++ code.
 
---- ipc/ipdl/ipdl/cxx/cgen.py.orig	2014-06-06 03:15:27.000000000 +0200
-+++ ipc/ipdl/ipdl/cxx/cgen.py	2014-06-18 18:10:27.000000000 +0200
-@@ -101,6 +101,7 @@
+--- ipc/ipdl/ipdl/cxx/cgen.py.orig	2014-07-17 01:45:18.000000000 +0000
++++ ipc/ipdl/ipdl/cxx/cgen.py
+@@ -101,6 +101,7 @@ class CxxCodeGen(CodePrinter, Visitor):
      def visitDecl(self, d):
          # C-syntax arrays make code generation much more annoying
          if isinstance(d.type, TypeArray):
-+	    self.write('MOZ_ALIGNED_DECL(')
++            self.write('MOZ_ALIGNED_DECL(')
              d.type.basetype.accept(self)
          else:
              d.type.accept(self)
-@@ -111,7 +112,9 @@
+@@ -111,7 +112,9 @@ class CxxCodeGen(CodePrinter, Visitor):
          if isinstance(d.type, TypeArray):
              self.write('[')
              d.type.nmemb.accept(self)
 -            self.write(']')
 +            self.write('], MOZ_ALIGNOF(')
-+	    d.type.alignType.accept(self)
-+	    self.write('))')
++            d.type.alignType.accept(self)
++            self.write('))')
  
      def visitParam(self, p):
          self.visitDecl(p)
-
