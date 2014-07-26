b7e22d21b092f331492df740141c049995538e2f
szptvlfn <szptvlfn@pkgsrc.org>
2014-07-26 21:50:10 +0000

    Import uniplate-1.6.12 as devel/hs-uniplate, packaged for wip by pho.
    Uniplate is library for writing simple and concise generic operations.
    Uniplate has similar goals to the original Scrap Your Boilerplate work, but
    is substantially simpler and faster. The Uniplate manual is available at
    http://community.haskell.org/~ndm/darcs/uniplate/uniplate.htm.

---
+++ devel/hs-uniplate/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/07/26 21:50:10 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-uniplate
+
+.if !defined(HS_UNIPLATE_BUILDLINK3_MK)
+HS_UNIPLATE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-uniplate+=	hs-uniplate>=1.6.12
+BUILDLINK_PKGSRCDIR.hs-uniplate?=	../../devel/hs-uniplate
+
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-syb/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_UNIPLATE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-uniplate
