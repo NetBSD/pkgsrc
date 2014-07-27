ae784744dae65f90c87a17f36bf8714aed5ba6cd
ryoon <ryoon@pkgsrc.org>
2014-07-27 20:04:59 +0000

    Update to 31.0
    * Update enigmail to 1.7
    
    Changelog:
        NEW
        Autocompleting email addresses now matches against any part of the name or email (bug 529584)
    
        NEW
        Composing a mail to a newsgroup will now autocomplete newsgroup names (bug 61491)
    
        FIXED
        Insecure NTLM (pre-NTLMv2) authentication disabled (see 828183)
    
    Fixed in Thunderbird 31
    MFSA 2014-66 IFRAME sandbox same-origin access through redirect
    MFSA 2014-65 Certificate parsing broken by non-standard character encoding
    MFSA 2014-64 Crash in Skia library when scaling high quality images
    MFSA 2014-63 Use-after-free while when manipulating certificates in the trusted cache
    MFSA 2014-62 Exploitable WebGL crash with Cesium JavaScript library
    MFSA 2014-61 Use-after-free with FireOnStateChange event
    MFSA 2014-59 Use-after-free in DirectWrite font handling
    MFSA 2014-58 Use-after-free in Web Audio due to incorrect control message ordering
    MFSA 2014-57 Buffer overflow during Web Audio buffering for playback
    MFSA 2014-56 Miscellaneous memory safety hazards (rv:31.0 / rv:24.7)

--- mail/thunderbird/patches/patch-ipc_chromium_src_base_sys__info__posix.cc
+++ mail/thunderbird/patches/patch-ipc_chromium_src_base_sys__info__posix.cc
@@ -1,6 +1,6 @@
-$NetBSD: patch-ipc_chromium_src_base_sys__info__posix.cc,v 1.6 2013/11/12 20:50:51 ryoon Exp $
+$NetBSD: patch-ipc_chromium_src_base_sys__info__posix.cc,v 1.7 2014/07/27 20:04:59 ryoon Exp $
 
---- mozilla/ipc/chromium/src/base/sys_info_posix.cc.orig	2013-10-23 22:09:00.000000000 +0000
+--- mozilla/ipc/chromium/src/base/sys_info_posix.cc.orig	2014-07-18 00:05:24.000000000 +0000
 +++ mozilla/ipc/chromium/src/base/sys_info_posix.cc
 @@ -119,7 +119,11 @@ std::wstring SysInfo::GetEnvVar(const wc
  
@@ -18,18 +18,6 @@ $NetBSD: patch-ipc_chromium_src_base_sys__info__posix.cc,v 1.6 2013/11/12 20:50:
  
  // static
  std::string SysInfo::OperatingSystemVersion() {
-+#ifdef OS_SOLARIS
-+  struct utsname info;
-+#else
-   utsname info;
-+#endif
-   if (uname(&info) < 0) {
-     NOTREACHED();
-     return "";
-@@ -139,7 +147,11 @@ std::string SysInfo::OperatingSystemVers
- 
- // static
- std::string SysInfo::CPUArchitecture() {
 +#ifdef OS_SOLARIS
 +  struct utsname info;
 +#else
