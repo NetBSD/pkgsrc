d63dbb76a40417f50cb8509a538b481cd408fbc0
ryoon <ryoon@pkgsrc.org>
2014-07-27 05:36:07 +0000

    Import thunderbird24-24.7.0 as mail/thunderbird24.
    Mozilla Thunderbird is a redesign of the Mozilla mail component. The
    goal is to produce a cross platform stand alone mail application using
    the XUL user interface language. This version uses the gtk2 toolkit.
    
    This package tracks Thunderbird 24 branch.

---
+++ mail/thunderbird24/patches/patch-mozilla_config_baseconfig.mk
@@ -0,0 +1,18 @@
+$NetBSD: patch-mozilla_config_baseconfig.mk,v 1.1 2014/07/27 05:36:07 ryoon Exp $
+
+--- mozilla/config/baseconfig.mk.orig	2013-10-23 22:08:50.000000000 +0000
++++ mozilla/config/baseconfig.mk
+@@ -1,9 +1,9 @@
+ INCLUDED_AUTOCONF_MK = 1
+ 
+-includedir := $(includedir)/$(MOZ_APP_NAME)-$(MOZ_APP_VERSION)
+-idldir = $(datadir)/idl/$(MOZ_APP_NAME)-$(MOZ_APP_VERSION)
+-installdir = $(libdir)/$(MOZ_APP_NAME)-$(MOZ_APP_VERSION)
+-sdkdir = $(libdir)/$(MOZ_APP_NAME)-devel-$(MOZ_APP_VERSION)
++includedir := $(includedir)/${MOZILLA_PKG_NAME}
++idldir = $(datadir)/idl/${MOZILLA_PKG_NAME}
++installdir = $(libdir)/${MOZILLA_PKG_NAME}
++sdkdir = $(libdir)/${MOZILLA_PKG_NAME}-sdk
+ DIST = $(DEPTH)/dist
+ 
+ # We do magic with OBJ_SUFFIX in config.mk, the following ensures we don't
