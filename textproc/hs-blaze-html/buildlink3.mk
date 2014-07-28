bb4c785ae600fe2a6b21b66f6ca8bb498324bf10
szptvlfn <szptvlfn@pkgsrc.org>
2014-07-28 21:03:24 +0000

    Import blaze-html-0.7.0.2 as textproc/hs-blaze-html, packaged for wip by pho.
    A blazingly fast HTML combinator library for the Haskell programming
    language. The Text.Blaze module is a good starting point, as well as
    this tutorial: http://jaspervdj.be/blaze/tutorial.html.

---
+++ textproc/hs-blaze-html/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/07/28 21:03:24 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-blaze-html
+
+.if !defined(HS_BLAZE_HTML_BUILDLINK3_MK)
+HS_BLAZE_HTML_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-blaze-html+=	hs-blaze-html>=0.7.0
+BUILDLINK_PKGSRCDIR.hs-blaze-html?=	../../textproc/hs-blaze-html
+
+.include "../../devel/hs-blaze-builder/buildlink3.mk"
+.include "../../textproc/hs-blaze-markup/buildlink3.mk"
+.include "../../devel/hs-text/buildlink3.mk"
+.endif	# HS_BLAZE_HTML_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-blaze-html
