965b1350164c3c20db9f01eee20557a60413097b
szptvlfn <szptvlfn@pkgsrc.org>
2014-07-28 21:35:23 +0000

    Update to 0.10.9
    CHANGES:
    0.10.9
        #10, fix versionArgs (broken in 0.10.8)
    0.10.8
        Avoid compilation warnings on GHC 7.8
        #9, add --numeric-version flag
        Update the copyright year
        Change GetOpt.usageInfo to be more like GetOpt

--- devel/hs-cmdargs/buildlink3.mk
+++ devel/hs-cmdargs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/05/22 21:39:23 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/07/28 21:35:23 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cmdargs
 
 .if !defined(HS_CMDARGS_BUILDLINK3_MK)
 HS_CMDARGS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.7
+BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.9
 BUILDLINK_PKGSRCDIR.hs-cmdargs?=	../../devel/hs-cmdargs
 
 .include "../../devel/hs-transformers/buildlink3.mk"
