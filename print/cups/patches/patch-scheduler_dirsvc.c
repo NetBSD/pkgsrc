c13b4bee766de08bf18636d01276509a16b92936
wiz <wiz>
2014-07-29 11:38:40 +0000

    Update to 1.7.4:
    CHANGES IN CUPS V1.7.4
    
    	- Security: The web interface incorrectly served symlinked files and
    	  files that were not world-readable, potentially leading to a
    	  disclosure of information (STR #4450)
    	- The CUPS headers incorrectly needed libdispatch for blocks support
    	  (STR #4397)
    	- CUPS did not compile when Avahi or mDNSResponder was not present
    	  (STR #4402, STR #4424)
    	- The "snmp" option did not work with the network backends (STR #4422)
    	- The User directive in client.conf did not override the USER
    	  environment variable (STR #4426)
    	- The web interface now properly shows a "Go" button for all text-based
    	  browsers (STR #4425)
    	- The MaxJobTime directive now properly supports time values (STR #4434)
    	- The RPM spec file did not work due to the new Brazilian Portuguese
    	  localization (STR #4436)
    	- Fixed an "IPP read error" issue (STR #4440)
    	- Fixed the --disable-libusb configure option (STR #4439)
    	- Fixed the debug output from the DNS-SD backend when using Avahi
    	  (STR #4444)
    	- Fixed a bug in the CUPS_SC_GET_DEVICE_ID handling by the network
    	  backends (STR #4447)
    	- Added USB quirk rule for Lexmark E230 (STR #4448)
    	- The LPD backend did not work with some versions of glibc (STR #4452)

--- print/cups/patches/patch-scheduler_dirsvc.c
+++ print/cups/patches/patch-scheduler_dirsvc.c
@@ -1,42 +1,11 @@
-$NetBSD: patch-scheduler_dirsvc.c,v 1.4 2014/06/09 09:27:10 wiz Exp $
+$NetBSD: patch-scheduler_dirsvc.c,v 1.5 2014/07/29 11:38:40 wiz Exp $
 
-o Fix building without dnssd option.
 o net/mDNSResponder-258.14 does not define kDNSServiceErr_Timeout, just threat
  it like kDNSServiceErr_Unknown.
 
---- scheduler/dirsvc.c.orig	2014-03-05 21:11:32.000000000 +0000
+--- scheduler/dirsvc.c.orig	2014-06-09 18:33:24.000000000 +0000
 +++ scheduler/dirsvc.c
-@@ -237,11 +237,13 @@ cupsdStartBrowsing(void)
-   if (BrowseLocalProtocols & BROWSE_SMB)
-     update_smb(1);
- 
-+#if defined(HAVE_DNSSD) || defined(HAVE_AVAHI)
-  /*
-   * Register the individual printers
-   */
- 
-   dnssdRegisterAllPrinters(0);
-+#endif
- }
- 
- 
-@@ -255,6 +257,7 @@ cupsdStopBrowsing(void)
-   if (!Browsing || !BrowseLocalProtocols)
-     return;
- 
-+#if defined(HAVE_DNSSD) || defined(HAVE_AVAHI)
-  /*
-   * De-register the individual printers
-   */
-@@ -265,7 +268,6 @@ cupsdStopBrowsing(void)
-   * Shut down browsing sockets...
-   */
- 
--#if defined(HAVE_DNSSD) || defined(HAVE_AVAHI)
-   if ((BrowseLocalProtocols & BROWSE_DNSSD) && DNSSDMaster)
-     dnssdStop();
- #endif /* HAVE_DNSSD || HAVE_AVAHI */
-@@ -829,9 +831,6 @@ dnssdErrorString(int error)		/* I - Erro
+@@ -831,9 +833,6 @@ dnssdErrorString(int error)		/* I - Erro
  
      case kDNSServiceErr_PollingMode :
          return ("Service polling mode error.");
