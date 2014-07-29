ef6db2b25f23af9c53fedf9dc8cc9fdc83fb8b45
adam <adam@pkgsrc.org>
2014-07-29 07:03:27 +0000

    Changes 0.48.5: Adobe Illustrator SVG files containing entities can be loaded again Support for Poppler 0.26 Support for Boehm-GC 7.4.0 and later Several crash bugs were fixed 64-bit compilation is now possible on Windows

--- graphics/inkscape/patches/patch-src_ui_tool_node.h
+++ graphics/inkscape/patches/patch-src_ui_tool_node.h
@@ -1,28 +1,30 @@
-$NetBSD: patch-src_ui_tool_node.h,v 1.1 2014/01/11 17:48:52 wiz Exp $
+$NetBSD: patch-src_ui_tool_node.h,v 1.2 2014/07/29 07:03:27 adam Exp $
 
---- src/ui/tool/node.h.orig	2012-02-14 03:22:17.000000000 +0000
+--- src/ui/tool/node.h.orig	2014-07-29 04:46:51.000000000 +0000
 +++ src/ui/tool/node.h
-@@ -16,7 +16,9 @@
- #include <iosfwd>
+@@ -17,11 +17,7 @@
  #include <stdexcept>
  #include <cstddef>
+ 
+-#if __cplusplus >= 201103L
+ #include <functional>
+-#else
 -#include <tr1/functional>
-+
-+#include <functional>
-+
+-#endif
+ 
  #include <boost/utility.hpp>
  #include <boost/shared_ptr.hpp>
- #include <boost/optional.hpp>
-@@ -32,12 +34,6 @@ template <typename> class NodeIterator;
+@@ -38,13 +34,6 @@ template <typename> class NodeIterator;
  }
  }
  
+-#if __cplusplus < 201103L
 -namespace std {
 -namespace tr1 {
 -template <typename N> struct hash< Inkscape::UI::NodeIterator<N> >;
 -}
 -}
--
+-#endif
+ 
  namespace Inkscape {
  namespace UI {
- 
