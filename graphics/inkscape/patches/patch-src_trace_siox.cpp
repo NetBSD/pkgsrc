583fba0ba69a3704f07e50625d16f81b316fa956
adam <adam@pkgsrc.org>
2014-07-29 07:03:27 +0000

    Changes 0.48.5: Adobe Illustrator SVG files containing entities can be loaded again Support for Poppler 0.26 Support for Boehm-GC 7.4.0 and later Several crash bugs were fixed 64-bit compilation is now possible on Windows

--- graphics/inkscape/patches/patch-src_trace_siox.cpp
+++ graphics/inkscape/patches/patch-src_trace_siox.cpp
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_trace_siox.cpp,v 1.1 2014/01/11 17:48:52 wiz Exp $
+$NetBSD: patch-src_trace_siox.cpp,v 1.2 2014/07/29 07:03:27 adam Exp $
 
 Fix compilation with clang/libc++.
 
---- src/trace/siox.cpp.orig	2011-07-08 18:25:09.000000000 +0000
+--- src/trace/siox.cpp.orig	2014-06-09 13:24:41.000000000 +0000
 +++ src/trace/siox.cpp
-@@ -19,6 +19,7 @@
+@@ -9,6 +9,7 @@
  
  #include <math.h>
  #include <stdarg.h>
 +#include <stdlib.h>
  #include <map>
  #include <algorithm>
- 
+ #include <cstdlib>
