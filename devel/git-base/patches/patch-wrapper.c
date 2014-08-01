0a637f6b8b7147b765a46bf562049c4ad86658b6
schmonz <schmonz@pkgsrc.org>
2014-08-01 19:18:39 +0000

    Even though we specify OpenSSL, on sufficiently new Mac OS X, Git has been linking with Apple's CommonCrypto instead. Add an "apple-common-crypto" option that explicitly avoids CommonCrypto when it's off. Turn it off by default, and set PKG_FAIL_REASON if it's enabled on an unsuitable system.
    While here, regenerate fuzzy patches.
    
    Addresses pkg/49051. Bump PKGREVISION.

--- devel/git-base/patches/patch-wrapper.c
+++ devel/git-base/patches/patch-wrapper.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-wrapper.c,v 1.1 2013/09/09 13:54:44 wiz Exp $
+$NetBSD: patch-wrapper.c,v 1.2 2014/08/01 19:18:39 schmonz Exp $
 
 git's wrapper around mkstemp assumes that the libc function will not
 modify the argument when the call fails.  This causes one of git's
 tests to fail.  This patch is from Junio C. Hamano, and will likely be
 included in future versions of git.
 
---- wrapper.c.orig	2012-11-26 03:18:01.000000000 +0000
+--- wrapper.c.orig	2014-05-30 21:38:10.000000000 +0000
 +++ wrapper.c
-@@ -229,7 +229,7 @@ int xmkstemp(char *template)
+@@ -378,7 +378,7 @@ int xmkstemp_mode(char *template, int mo
  		int saved_errno = errno;
  		const char *nonrelative_template;
  
