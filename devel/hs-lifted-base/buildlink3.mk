95d4c27802b7b3bb2076fb587d6dbb76d6293771
szptvlfn <szptvlfn>
2014-08-01 20:53:58 +0000

    Import lifted-base-0.2.3.0 as devel/hs-lifted-base, packaged for wip.
    lifted-base exports IO operations from the base library lifted to any
    instance of MonadBase or MonadBaseControl.

---
+++ devel/hs-lifted-base/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/08/01 20:53:58 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-lifted-base
+
+.if !defined(HS_LIFTED_BASE_BUILDLINK3_MK)
+HS_LIFTED_BASE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-lifted-base+=	hs-lifted-base>=0.2.3
+BUILDLINK_PKGSRCDIR.hs-lifted-base?=	../../devel/hs-lifted-base
+
+.include "../../devel/hs-monad-control/buildlink3.mk"
+.include "../../devel/hs-transformers-base/buildlink3.mk"
+.endif	# HS_LIFTED_BASE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-lifted-base
