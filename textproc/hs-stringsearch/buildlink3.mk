49e1858bac7983c86ec599c51b4e09d340db46ee
szptvlfn <szptvlfn@pkgsrc.org>
2014-08-01 12:09:42 +0000

    Import stringsearch-0.3.6.5 as textproc/hs-stringsearch, packaged for wip by pho.
    This package provides several functions to quickly search for
    substrings in strict or lazy ByteStrings. It also provides functions
    for breaking or splitting on substrings and replacing all occurrences
    of a substring (the first in case of overlaps) with another.

---
+++ textproc/hs-stringsearch/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/08/01 12:09:42 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-stringsearch
+
+.if !defined(HS_STRINGSEARCH_BUILDLINK3_MK)
+HS_STRINGSEARCH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-stringsearch+=	hs-stringsearch>=0.3.6
+BUILDLINK_PKGSRCDIR.hs-stringsearch?=	../../textproc/hs-stringsearch
+.endif	# HS_STRINGSEARCH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-stringsearch
