3a86d99a2e16d594e7299efc05f71e073dc6b22f
wiz <wiz@pkgsrc.org>
2014-08-03 22:08:42 +0000

    Import webkit-gtk3-1.10.2nb12 as www/webkit1-gtk3 to match webkit1-gtk.
    WebKit is an open source web browser engine. WebKit is also the name of
    the Mac OS X system framework version of the engine that's used by
    Safari, Dashboard, Mail, and many other OS X applications. WebKit's HTML
    and JavaScript code began as a branch of the KHTML and KJS libraries
    from KDE.
    
    This is the GTK3+ port of major version 1 of the engine.

---
+++ www/webkit1-gtk3/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/08/03 22:08:42 wiz Exp $
+
+BUILDLINK_TREE+=	webkit-gtk3
+
+.if !defined(WEBKIT_GTK3_BUILDLINK3_MK)
+WEBKIT_GTK3_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.webkit-gtk3+=	webkit-gtk3>=1.4.2
+BUILDLINK_ABI_DEPENDS.webkit-gtk3+=	webkit-gtk3>=1.10.2nb12<2
+BUILDLINK_PKGSRCDIR.webkit-gtk3?=	../../www/webkit1-gtk3
+
+.include "../../databases/sqlite3/buildlink3.mk"
+.include "../../devel/gperf/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../multimedia/gstreamer0.10/buildlink3.mk"
+.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
+.include "../../net/libsoup24/buildlink3.mk"
+.include "../../textproc/enchant/buildlink3.mk"
+.include "../../textproc/icu/buildlink3.mk"
+.include "../../textproc/libxslt/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.include "../../x11/libXdamage/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
+.endif # WEBKIT_GTK3_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-webkit-gtk3
