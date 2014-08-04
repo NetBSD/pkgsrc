f8dfcd6f5a2e5795eef43ed7382179aa12071fa0
wiz <wiz@pkgsrc.org>
2014-08-04 09:27:38 +0000

    Switch to webkit-gtk, bump PKGREVISION.

--- www/epiphany/buildlink3.mk
+++ www/epiphany/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.65 2014/08/03 22:13:19 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.66 2014/08/04 09:27:38 wiz Exp $
 
 BUILDLINK_TREE+=	epiphany
 
@@ -6,14 +6,14 @@ BUILDLINK_TREE+=	epiphany
 EPIPHANY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.epiphany+=		epiphany>=2.28
-BUILDLINK_ABI_DEPENDS.epiphany+=		epiphany>=2.30.6nb37
+BUILDLINK_ABI_DEPENDS.epiphany+=		epiphany>=2.30.6nb38
 BUILDLINK_PKGSRCDIR.epiphany?=		../../www/epiphany
 
 .include "../../mk/bsd.fast.prefs.mk"
 
 .include "../../devel/GConf/buildlink3.mk"
 .include "../../devel/nss/buildlink3.mk"
-.include "../../www/webkit1-gtk/buildlink3.mk"
+.include "../../www/webkit-gtk/buildlink3.mk"
 .include "../../net/ORBit2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
