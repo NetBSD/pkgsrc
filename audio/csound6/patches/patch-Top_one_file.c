23b97cef8e83f435dcd44dfbca1d4c201a76ba44
mrg <mrg>
2014-08-05 05:12:38 +0000

    enable portaudio-devel now that it has a supported version. now csound6 can output directly to the audio card on netbsd. add comments to all the patches that were missing them. bump pkg version.

--- audio/csound6/patches/patch-Top_one_file.c
+++ audio/csound6/patches/patch-Top_one_file.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-Top_one_file.c,v 1.1 2014/03/06 10:16:53 mrg Exp $
+$NetBSD: patch-Top_one_file.c,v 1.2 2014/08/05 05:12:38 mrg Exp $
+
+Add NetBSD and DragonFlyBSD support.
 
 --- Top/one_file.c.orig	2014-01-07 08:54:20.000000000 -0800
 +++ Top/one_file.c	2014-02-12 23:53:28.000000000 -0800
