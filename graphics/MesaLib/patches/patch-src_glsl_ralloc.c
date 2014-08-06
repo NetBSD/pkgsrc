9ca9934b4d92fd893dad309e0b11cfca102cdc62
wiz <wiz@pkgsrc.org>
2014-08-06 10:25:14 +0000

    Use __attribute__((__destructor__)) instead of magic __dso_handle, and fix another place. Patches from Taylor R Campbell (NetBSD xsrc). Bump PKGREVISION.

--- graphics/MesaLib/patches/patch-src_glsl_ralloc.c
+++ graphics/MesaLib/patches/patch-src_glsl_ralloc.c
@@ -1,16 +1,23 @@
-$NetBSD: patch-src_glsl_ralloc.c,v 1.1 2014/01/24 23:00:15 ryoon Exp $
+$NetBSD: patch-src_glsl_ralloc.c,v 1.2 2014/08/06 10:25:14 wiz Exp $
 
 * Fix exit time segfault of qt5 application with modular xorg
 
 --- src/glsl/ralloc.c.orig	2011-10-15 00:43:58.000000000 +0000
 +++ src/glsl/ralloc.c
-@@ -283,7 +283,8 @@ ralloc_autofree_context(void)
+@@ -272,7 +272,7 @@ ralloc_parent(const void *ptr)
+ 
+ static void *autofree_context = NULL;
+ 
+-static void
++static void __attribute__((__destructor__))
+ autofree(void)
+ {
+    ralloc_free(autofree_context);
+@@ -283,7 +283,6 @@ ralloc_autofree_context(void)
  {
     if (unlikely(autofree_context == NULL)) {
        autofree_context = ralloc_context(NULL);
 -      atexit(autofree);
-+      extern __attribute__((visibility("hidden"))) void *__dso_handle;
-+      __cxa_atexit(autofree, NULL, __dso_handle);
     }
     return autofree_context;
  }
