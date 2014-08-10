c4e0f98616fe4b626bbbcb38d924df2192351a95
fhajny <fhajny@pkgsrc.org>
2014-08-10 10:16:24 +0000

    Add openssl to bl3 if built with the ssl option.

--- net/net-snmp/buildlink3.mk
+++ net/net-snmp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2009/03/20 19:25:11 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2014/08/10 10:16:24 fhajny Exp $
 
 BUILDLINK_TREE+=	net-snmp
 
@@ -8,6 +8,13 @@ NET_SNMP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.net-snmp+=	net-snmp>=5.0.9nb3
 BUILDLINK_ABI_DEPENDS.net-snmp+=	net-snmp>=5.2.1.2
 BUILDLINK_PKGSRCDIR.net-snmp?=	../../net/net-snmp
+
+pkgbase := net-snmp
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.net-snmp:Mssl)
+.  include "../../security/openssl/buildlink3.mk"
+.endif
 
 .include "../../security/tcp_wrappers/buildlink3.mk"
 .endif # NET_SNMP_BUILDLINK3_MK
