5ac4daad12514b1a26adbe76460affedbdfe5ff4
obache <obache@pkgsrc.org>
2014-08-10 02:27:24 +0000

    Update libkkc to 0.3.4.
        Update translations
        Port to libgee 0.8 API
        rom-kana: Retain partial input when "convert-*" commands are called
        rom-kana: Assign "ly<X>" to HIRAGANA SMALL LETTER <X>

--- converters/libkkc/buildlink3.mk
+++ converters/libkkc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/05/24 13:10:04 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/08/10 02:27:24 obache Exp $
 #
 
 BUILDLINK_TREE+=	libkkc
@@ -6,11 +6,11 @@ BUILDLINK_TREE+=	libkkc
 .if !defined(LIBKKC_BUILDLINK3_MK)
 LIBKKC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkkc+=	libkkc>=0.3.3
+BUILDLINK_API_DEPENDS.libkkc+=	libkkc>=0.3.4
 BUILDLINK_PKGSRCDIR.libkkc?=	../../converters/libkkc
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/libgee/buildlink3.mk"
+.include "../../devel/libgee0.8/buildlink3.mk"
 .include "../../textproc/json-glib/buildlink3.mk"
 .include "../../textproc/marisa/buildlink3.mk"
 .endif	# LIBKKC_BUILDLINK3_MK
