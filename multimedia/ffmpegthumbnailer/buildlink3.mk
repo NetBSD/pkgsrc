0e0a9a12bad8947706998b541346c43db66599c3
wiz <wiz>
2014-08-11 23:09:44 +0000

    Switch to ffmpeg1 instead of ffmpeg.
    Note: some of these packages didn't build for me, e.g. osg, so I couldn't
    verify them all.

--- multimedia/ffmpegthumbnailer/buildlink3.mk
+++ multimedia/ffmpegthumbnailer/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/02/16 11:18:02 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/08/11 23:09:45 wiz Exp $
 
 BUILDLINK_TREE+=	ffmpegthumbnailer
 
@@ -10,7 +10,7 @@ BUILDLINK_ABI_DEPENDS.ffmpegthumbnailer?=	ffmpegthumbnailer>=2.0.8nb1
 BUILDLINK_PKGSRCDIR.ffmpegthumbnailer?=	../../multimedia/ffmpegthumbnailer
 
 #.include "../../graphics/png/buildlink3.mk"
-#.include "../../multimedia/ffmpeg/buildlink3.mk"
+#.include "../../multimedia/ffmpeg1/buildlink3.mk"
 .endif	# FFMPEGTHUMBNAILER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ffmpegthumbnailer
