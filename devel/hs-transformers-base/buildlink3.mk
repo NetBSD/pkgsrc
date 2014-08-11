3da4e0b4fa5bdcf60411dec1d8434861d7d5ae02
szptvlfn <szptvlfn@pkgsrc.org>
2014-08-11 21:03:58 +0000

    Update to 0.4.3
    Changes from https://github.com/mvv/transformers-base
    0.4.3
     - Merge pull request #6 from tensor5/master
       Enable Safe Haskell extension

--- devel/hs-transformers-base/buildlink3.mk
+++ devel/hs-transformers-base/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/05/26 22:03:29 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/08/11 21:03:58 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-transformers-base
 
 .if !defined(HS_TRANSFORMERS_BASE_BUILDLINK3_MK)
 HS_TRANSFORMERS_BASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-transformers-base+=	hs-transformers-base>=0.4.2
+BUILDLINK_API_DEPENDS.hs-transformers-base+=	hs-transformers-base>=0.4.3
 BUILDLINK_PKGSRCDIR.hs-transformers-base?=	../../devel/hs-transformers-base
 
 .include "../../devel/hs-transformers/buildlink3.mk"
+.include "../../devel/hs-stm/buildlink3.mk"
 .endif	# HS_TRANSFORMERS_BASE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-transformers-base
