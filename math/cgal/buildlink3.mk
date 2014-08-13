72ed0307ebfd54441b4c154c1a663e82708b8477
adam <adam@pkgsrc.org>
2014-08-13 10:57:32 +0000

    Revbump after boost-libs update

--- math/cgal/buildlink3.mk
+++ math/cgal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2014/02/12 23:18:11 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2014/08/13 10:57:32 adam Exp $
 
 BUILDLINK_TREE+=	cgal
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	cgal
 CGAL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cgal+=	cgal>=3.9
-BUILDLINK_ABI_DEPENDS.cgal+=	cgal>=3.9nb21
+BUILDLINK_ABI_DEPENDS.cgal+=	cgal>=3.9nb22
 BUILDLINK_PKGSRCDIR.cgal?=	../../math/cgal
 
 .include "../../devel/boost-headers/buildlink3.mk"
