8b022d7b85d4d45f549433434966dbd6ec5f6a7b
jaapb <jaapb@pkgsrc.org>
2014-08-17 13:34:06 +0000

    New package devel/ocaml-odn, a data dumping library for ocaml. Mostly important as a dependency for other packages.

---
+++ devel/ocaml-odn/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/08/17 13:34:06 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-odn
+
+.if !defined(OCAML_ODN_BUILDLINK3_MK)
+OCAML_ODN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-odn+=	ocaml-odn>=0.0.11
+BUILDLINK_PKGSRCDIR.ocaml-odn?=	../../devel/ocaml-odn
+
+.include "../../lang/ocaml/buildlink3.mk"
+.endif	# OCAML_ODN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-odn
