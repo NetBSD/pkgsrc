b120825b095a04b6ce09bc9c9710def4e6fd2cfa
jaapb <jaapb>
2014-08-17 12:11:07 +0000

    Revamped package to use ocaml.mk infrastructure. Package name is now ocaml-optcomp rather than optcomp, in line with most other OCaml packages. This only affects devel/ocaml-deriving-ocsigen, which will be updated shortly.

--- devel/ocaml-optcomp/buildlink3.mk
+++ devel/ocaml-optcomp/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/11/01 11:40:00 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/08/17 12:11:07 jaapb Exp $
 
-BUILDLINK_TREE+=	optcomp
+BUILDLINK_TREE+=	ocaml-optcomp
 
-.if !defined(OPTCOMP_BUILDLINK3_MK)
-OPTCOMP_BUILDLINK3_MK:=
+.if !defined(OCAML_OPTCOMP_BUILDLINK3_MK)
+OCAML_OPTCOMP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.optcomp+=	optcomp>=1.4
-BUILDLINK_PKGSRCDIR.optcomp?=	../../devel/ocaml-optcomp
+BUILDLINK_API_DEPENDS.ocaml-optcomp+=	ocaml-optcomp>=1.4
+BUILDLINK_PKGSRCDIR.ocaml-optcomp?=	../../devel/ocaml-optcomp
 
 .include "../../lang/ocaml/buildlink3.mk"
-.endif	# OPTCOMP_BUILDLINK3_MK
+.endif	# OCAML_OPTCOMP_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-optcomp
+BUILDLINK_TREE+=	-ocaml-optcomp
