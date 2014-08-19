fa7e4d119a06c0854d57c5a2f162229cafe65eda
joerg <joerg@pkgsrc.org>
2014-08-19 13:30:39 +0000

    Redo ext/hashmap handling.

--- audio/daapd/patches/patch-types.h
+++ audio/daapd/patches/patch-types.h
@@ -1,55 +1,40 @@
-$NetBSD: patch-types.h,v 1.1 2013/05/06 14:17:28 joerg Exp $
+$NetBSD: patch-types.h,v 1.2 2014/08/19 13:30:39 joerg Exp $
 
---- types.h.orig	2013-05-04 14:53:09.000000000 +0000
+--- types.h.orig	2014-08-19 12:33:06.000000000 +0000
 +++ types.h
-@@ -32,6 +32,7 @@
- #include <sys/stat.h>
- #include <strings.h>
- #include <pthread.h>
-+#include <cstdlib>
- 
- // #include "mmgr.h"
- 
-@@ -40,41 +41,7 @@
- 
- #include "dboutput.h"
- #include "songcache.h"
--
--
--// STL defines no hash function for std::string.
--// How annoying.
--
--#ifdef __GNUC__
--// gcc version < 3.1.0 ?
--#if __GNUC__ < 3 || \
--(__GNUC__ == 3 && __GNUC_MINOR__ < 1)
--	__STL_BEGIN_NAMESPACE
--		template<> struct hash<std::string> {
--			size_t operator()(std::string __s) const { 
+@@ -52,7 +52,7 @@
+ 	__STL_BEGIN_NAMESPACE
+ 		template<> struct hash<std::string> {
+ 			size_t operator()(std::string __s) const { 
 -				return __stl_hash_string( (const char*) __s.c_str() ); 
--			}
--		};
--	__STL_END_NAMESPACE
--#else
--	namespace __gnu_cxx {
--		template<> struct hash<std::string> {
--			size_t operator()(std::string __s) const { 
++				return hash<const char *>()(__s.c_str()); 
+ 			}
+ 		};
+ 	__STL_END_NAMESPACE
+@@ -60,7 +60,7 @@
+ 	namespace __gnu_cxx {
+ 		template<> struct hash<std::string> {
+ 			size_t operator()(std::string __s) const { 
 -				return __stl_hash_string( (const char*) __s.c_str() ); 
--			}
--		};
--	}
--#endif // GCC_VERSION
--#else
--	__STL_BEGIN_NAMESPACE
--		template<> struct hash<std::string> {
--			size_t operator()(std::string __s) const { 
++				return hash<const char *>()(__s.c_str()); 
+ 			}
+ 		};
+ 	}
+@@ -69,7 +69,7 @@
+ 	__STL_BEGIN_NAMESPACE
+ 		template<> struct hash<std::string> {
+ 			size_t operator()(std::string __s) const { 
 -				return __stl_hash_string( (const char*) __s.c_str() ); 
--			}
--		};
--	__STL_END_NAMESPACE
--#endif // __GNUC__
--
-+#include <string>
++				return hash<const char *>()(__s.c_str()); 
+ 			}
+ 		};
+ 	__STL_END_NAMESPACE
+@@ -109,7 +109,7 @@ struct InitParams {
+ 	}
  
- // database/server init parameters
- struct InitParams {
+ 	~InitParams() {
+-		free(dirs);
++		delete dirs;
+ 	}
+ };
+ 
