1a6b1c5bf82a7e6b5026422c643d3df0d04dd1a6
wiz <wiz@pkgsrc.org>
2014-08-21 12:14:16 +0000

    Add upstream bug report URLs.

--- audio/musicpd/patches/patch-src_output_HttpdOutputPlugin.cxx
+++ audio/musicpd/patches/patch-src_output_HttpdOutputPlugin.cxx
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_output_HttpdOutputPlugin.cxx,v 1.1 2014/05/16 00:33:47 pho Exp $
+$NetBSD: patch-src_output_HttpdOutputPlugin.cxx,v 1.2 2014/08/21 12:14:16 wiz Exp $
 
 Inlining this method results in the following error on Mach-O systems
 (or at least on Darwin 9):
@@ -6,6 +6,8 @@ Inlining this method results in the following error on Mach-O systems
   {standard input}:1322:FATAL:symbol: __ZZN11HttpdOutput7SendTagEPK3TagE5types
   can't be a weak_definition (currently only supported in section of type
   coalesced)
+
+http://bugs.musicpd.org/view.php?id=4059
 
 --- src/output/HttpdOutputPlugin.cxx.orig	2014-05-16 00:08:45.000000000 +0000
 +++ src/output/HttpdOutputPlugin.cxx
