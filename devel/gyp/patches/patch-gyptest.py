71b6560c31b23265de6edfed34f5f9c185fe9d35
he <he>
2014-08-21 14:49:43 +0000

    Fix gyp so that all but one of the self-tests pass. The one test remaining I've not been able to figure out what goes wrong (it tries overriding the compiler to use, but ends up passing the source to cc anyway, which wasn't supposed to happen).
    Updated DESCR to be a little more verbose.  It now starts with
    
       GYP is a tool for the Chromium project that generates native
       Visual Studio, Xcode and SCons and/or make build files from a
       platform-independent input format.
    
    Other changes:
     * Substitute /usr/bin/python and a bare "python" in a couple of files
     * Replace python in the interpreter line in all *.py files,
       done in a separate Makefile.replalce-python due to its size
     * Add a tool.mk make fragment for pkgsrc, for users of "gyp" within
       pkgsrc.  It makes a "python" available, via the python package's
       own tool.mk.  The GYP language spec says it's kosher to invoke
       Python from a gyp file by just saying "python".
     * Fix the bash interpreter in the "gyp" script
     * Add config for "netbsd7" as well
     * For some reason "thin" ar archives sometimes fail to produce an archive
       which "ar t" recognizes as valid, so avoid using them on NetBSD
     * Fix up some of the tests; our GNU make says "nothing to be done"
       with different quotes, add NetBSD platform support in some tests,
       and regenerate a couple of output files (should have been done
       upstream).
    
    PKGREVISION bumped.

--- devel/gyp/patches/patch-gyptest.py
+++ devel/gyp/patches/patch-gyptest.py
@@ -1,16 +1,17 @@
-$NetBSD: patch-gyptest.py,v 1.3 2014/07/19 09:08:36 ryoon Exp $
+$NetBSD: patch-gyptest.py,v 1.4 2014/08/21 14:49:43 he Exp $
 
-* Add NetBSD 5 and 6 target
+* Add NetBSD 5, 6 and 7 target
 
 --- gyptest.py.orig	2014-07-14 14:19:50.000000000 +0000
 +++ gyptest.py
-@@ -219,6 +219,9 @@ def main(argv=None):
+@@ -219,6 +219,10 @@ def main(argv=None):
        'aix5':     ['make'],
        'freebsd7': ['make'],
        'freebsd8': ['make'],
 +      'freebsd9': ['make'],
-+      'netbsd5': ['make'],
-+      'netbsd6': ['make'],
++      'netbsd5':  ['make'],
++      'netbsd6':  ['make'],
++      'netbsd7':  ['make'],
        'openbsd5': ['make'],
        'cygwin':   ['msvs'],
        'win32':    ['msvs', 'ninja'],
--- devel/gyp/patches/patch-pylib_gyp_common.py
+++ devel/gyp/patches/patch-pylib_gyp_common.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-pylib_gyp_common.py,v 1.2 2013/09/08 16:33:25 ryoon Exp $
+$NetBSD: patch-pylib_gyp_common.py,v 1.3 2014/08/21 14:49:43 he Exp $
 
 * Add NetBSD support
 
---- pylib/gyp/common.py.orig	2013-09-06 23:07:36.000000000 +0000
+--- pylib/gyp/common.py.orig	2014-07-14 14:19:49.000000000 +0000
 +++ pylib/gyp/common.py
-@@ -403,6 +403,8 @@ def GetFlavor(params):
+@@ -420,6 +420,8 @@ def GetFlavor(params):
      return 'openbsd'
    if sys.platform.startswith('aix'):
      return 'aix'
@@ -13,3 +13,11 @@ $NetBSD: patch-pylib_gyp_common.py,v 1.2 2013/09/08 16:33:25 ryoon Exp $
  
    return 'linux'
  
+@@ -432,6 +434,7 @@ def CopyTool(flavor, out_path):
+   prefix = {
+       'aix': 'flock',
+       'solaris': 'flock',
++      'netbsd': 'flock',
+       'mac': 'mac',
+       'win': 'win'
+       }.get(flavor, None)
