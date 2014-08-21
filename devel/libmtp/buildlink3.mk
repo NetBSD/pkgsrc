64368e9d8eccc5acf68bc9401230a76acbac70df
jperkin <jperkin@pkgsrc.org>
2014-08-21 14:03:45 +0000

    Pull in devel/libusb on SunOS instead of the unsupported devel/libusb1, the package detects which version is used at build time and handles both.

--- devel/libmtp/buildlink3.mk
+++ devel/libmtp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2012/08/14 21:00:46 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/08/21 14:03:45 jperkin Exp $
 
 BUILDLINK_TREE+=	libmtp
 
@@ -8,7 +8,14 @@ LIBMTP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libmtp+=	libmtp>=0.3.0
 BUILDLINK_PKGSRCDIR.libmtp?=	../../devel/libmtp
 
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} == "SunOS"
+.include "../../devel/libusb/buildlink3.mk"
+.else
 .include "../../devel/libusb1/buildlink3.mk"
+.endif
+
 .endif # LIBMTP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libmtp
