d8c4a52802ec3791cf920f6c7633315f4346ce7c
he <he@pkgsrc.org>
2014-08-21 14:49:43 +0000

    Fix gyp so that all but one of the self-tests pass. The one test remaining I've not been able to figure out what goes wrong (it tries overriding the compiler to use, but ends up passing the source to cc anyway, which wasn't supposed to happen).
    Updated DESCR to be a little more verbose.  It now starts with
    
       GYP is a tool for the Chromium project that generates native
       Visual Studio, Xcode and SCons and/or make build files from a
       platform-independent input format.
    
    Other changes:
     * Substitute /usr/bin/python and a bare "python" in a couple of files
     * Replace python in the interpreter line in all *.py files,
       done in a separate Makefile.replalce-python due to its size
     * Add a tool.mk make fragment for pkgsrc, for users of "gyp" within
       pkgsrc.  It makes a "python" available, via the python package's
       own tool.mk.  The GYP language spec says it's kosher to invoke
       Python from a gyp file by just saying "python".
     * Fix the bash interpreter in the "gyp" script
     * Add config for "netbsd7" as well
     * For some reason "thin" ar archives sometimes fail to produce an archive
       which "ar t" recognizes as valid, so avoid using them on NetBSD
     * Fix up some of the tests; our GNU make says "nothing to be done"
       with different quotes, add NetBSD platform support in some tests,
       and regenerate a couple of output files (should have been done
       upstream).
    
    PKGREVISION bumped.

--- devel/gyp/patches/patch-pylib_gyp_generator_make.py
+++ devel/gyp/patches/patch-pylib_gyp_generator_make.py
@@ -1,11 +1,54 @@
-$NetBSD: patch-pylib_gyp_generator_make.py,v 1.2 2014/01/24 13:28:30 fhajny Exp $
+$NetBSD: patch-pylib_gyp_generator_make.py,v 1.3 2014/08/21 14:49:43 he Exp $
 
 Force platform libtool na Darwin, see
 https://code.google.com/p/gyp/issues/detail?id=354&q=libtool
---- pylib/gyp/generator/make.py.orig	2013-09-06 23:07:36.000000000 +0000
+
+Also, don't try to use thin archives on NetBSD, they appear not to work
+("ar t <archive>" says "Malformed archive").
+
+--- pylib/gyp/generator/make.py.orig	2014-07-14 14:19:49.000000000 +0000
 +++ pylib/gyp/generator/make.py
-@@ -161,7 +161,7 @@ cmd_solink_module = $(LINK.$(TOOLSET)) -
+@@ -167,9 +167,46 @@ quiet_cmd_solink_module = SOLINK_MODULE(
+ cmd_solink_module = $(LINK.$(TOOLSET)) -shared $(GYP_LDFLAGS) $(LDFLAGS.$(TOOLSET)) -Wl,-soname=$(@F) -o $@ -Wl,--start-group $(filter-out FORCE_DO_CMD, $^) -Wl,--end-group $(LIBS)
+ """
  
++LINK_COMMANDS_NETBSD = """\
++quiet_cmd_alink = AR($(TOOLSET)) $@
++cmd_alink = rm -f $@ && $(AR.$(TOOLSET)) crs $@ $(filter %.o,$^)
++
++quiet_cmd_alink_thin = AR($(TOOLSET)) $@
++# Thin archives do not appear to work with the NetBSD-supplied version of GNU ar, so work around that
++cmd_alink_thin = rm -f $@ && $(AR.$(TOOLSET)) crs $@ $(filter %.o,$^)
++
++# Due to circular dependencies between libraries :(, we wrap the
++# special "figure out circular dependencies" flags around the entire
++# input list during linking.
++quiet_cmd_link = LINK($(TOOLSET)) $@
++cmd_link = $(LINK.$(TOOLSET)) $(GYP_LDFLAGS) $(LDFLAGS.$(TOOLSET)) -o $@ -Wl,--start-group $(LD_INPUTS) -Wl,--end-group $(LIBS)
++
++# We support two kinds of shared objects (.so):
++# 1) shared_library, which is just bundling together many dependent libraries
++# into a link line.
++# 2) loadable_module, which is generating a module intended for dlopen().
++#
++# They differ only slightly:
++# In the former case, we want to package all dependent code into the .so.
++# In the latter case, we want to package just the API exposed by the
++# outermost module.
++# This means shared_library uses --whole-archive, while loadable_module doesn't.
++# (Note that --whole-archive is incompatible with the --start-group used in
++# normal linking.)
++
++# Other shared-object link notes:
++# - Set SONAME to the library filename so our binaries don't reference
++# the local, absolute paths used on the link command-line.
++quiet_cmd_solink = SOLINK($(TOOLSET)) $@
++cmd_solink = $(LINK.$(TOOLSET)) -shared $(GYP_LDFLAGS) $(LDFLAGS.$(TOOLSET)) -Wl,-soname=$(@F) -o $@ -Wl,--whole-archive $(LD_INPUTS) -Wl,--no-whole-archive $(LIBS)
++
++quiet_cmd_solink_module = SOLINK_MODULE($(TOOLSET)) $@
++cmd_solink_module = $(LINK.$(TOOLSET)) -shared $(GYP_LDFLAGS) $(LDFLAGS.$(TOOLSET)) -Wl,-soname=$(@F) -o $@ -Wl,--start-group $(filter-out FORCE_DO_CMD, $^) -Wl,--end-group $(LIBS)
++"""
++
  LINK_COMMANDS_MAC = """\
  quiet_cmd_alink = LIBTOOL-STATIC $@
 -cmd_alink = rm -f $@ && ./gyp-mac-tool filter-libtool libtool $(GYP_LIBTOOLFLAGS) -static -o $@ $(filter %.o,$^)
@@ -13,7 +56,7 @@ https://code.google.com/p/gyp/issues/detail?id=354&q=libtool
  
  quiet_cmd_link = LINK($(TOOLSET)) $@
  cmd_link = $(LINK.$(TOOLSET)) $(GYP_LDFLAGS) $(LDFLAGS.$(TOOLSET)) -o "$@" $(LD_INPUTS) $(LIBS)
-@@ -324,7 +324,7 @@ sed -e "s|^$(notdir $@)|$@|" $(depfile).
+@@ -350,7 +387,7 @@ sed -e "s|^$(notdir $@)|$@|" $(depfile).
  # We remove slashes and replace spaces with new lines;
  # remove blank lines;
  # delete the first line and append a colon to the remaining lines.
@@ -22,3 +65,15 @@ https://code.google.com/p/gyp/issues/detail?id=354&q=libtool
    grep -v '^$$'                             |\
    sed -e 1d -e 's|$$|:|'                     \
      >> $(depfile)
+@@ -2056,6 +2093,11 @@ def GenerateOutput(target_list, target_d
+         'flock': './gyp-flock-tool flock',
+         'flock_index': 2,
+     })
++  elif flavor == 'netbsd':
++    header_params.update({
++        'link_commands': LINK_COMMANDS_NETBSD,
++    })
++        
+ 
+   header_params.update({
+     'CC.target':   GetEnvironFallback(('CC_target', 'CC'), '$(CC)'),
