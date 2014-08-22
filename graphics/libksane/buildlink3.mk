5276a5f302f22bc1c275f946dea6a29963e74b6a
wiz <wiz>
2014-08-22 11:24:25 +0000

    Bump PKGREVISION for ilmbase shlib major bump.

--- graphics/libksane/buildlink3.mk
+++ graphics/libksane/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2014/05/15 14:41:52 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2014/08/22 11:24:26 wiz Exp $
 
 BUILDLINK_TREE+=	libksane
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libksane
 LIBKSANE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libksane+=	libksane>=4.8.0
-BUILDLINK_ABI_DEPENDS.libksane?=	libksane>=4.11.5nb3
+BUILDLINK_ABI_DEPENDS.libksane?=	libksane>=4.11.5nb4
 BUILDLINK_PKGSRCDIR.libksane?=	../../graphics/libksane
 
 .include "../../x11/kdelibs4/buildlink3.mk"
--- x11/kde-workspace4/buildlink3.mk
+++ x11/kde-workspace4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2014/05/15 14:41:59 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2014/08/22 11:24:27 wiz Exp $
 
 BUILDLINK_TREE+=	kde-workspace
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kde-workspace
 KDE_WORKSPACE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kde-workspace+=	kde-workspace4>=4.0.0
-BUILDLINK_ABI_DEPENDS.kde-workspace+=	kde-workspace4>=4.11.5nb3
+BUILDLINK_ABI_DEPENDS.kde-workspace+=	kde-workspace4>=4.11.5nb5
 BUILDLINK_PKGSRCDIR.kde-workspace?=	../../x11/kde-workspace4
 
 .include "../../graphics/qimageblitz/buildlink3.mk"
