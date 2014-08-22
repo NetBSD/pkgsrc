f88f253419fc84634e4b722e1bfde148c426925c
obache <obache>
2014-08-22 10:25:09 +0000

    Update libsixel to 1.0.0.
    Bump ABI to 1.0.0.

--- graphics/libsixel/buildlink3.mk
+++ graphics/libsixel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/07/13 10:10:56 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/08/22 10:25:09 obache Exp $
 #
 
 BUILDLINK_TREE+=	libsixel
@@ -7,6 +7,7 @@ BUILDLINK_TREE+=	libsixel
 LIBSIXEL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libsixel+=	libsixel>=0.11.0
+BUILDLINK_ABI_DEPENDS.libsixel+=	libsixel>=1.0.0
 BUILDLINK_PKGSRCDIR.libsixel?=	../../graphics/libsixel
 .endif	# LIBSIXEL_BUILDLINK3_MK
 
