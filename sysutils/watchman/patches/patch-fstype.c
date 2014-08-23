92d0603e02a3aff963034f4ca033c01efa68b0fd
wiz <wiz@pkgsrc.org>
2014-08-23 20:13:59 +0000

    Fix build on NetBSD 6 and FreeBSD 9. From Niclas Rosenvik. Bump PKGREVISION.

--- sysutils/watchman/patches/patch-fstype.c
+++ sysutils/watchman/patches/patch-fstype.c
@@ -1,22 +1,24 @@
-$NetBSD: patch-fstype.c,v 1.2 2014/08/09 23:08:41 wiz Exp $
+$NetBSD: patch-fstype.c,v 1.3 2014/08/23 20:13:59 wiz Exp $
 
 NetBSD also supports statvfs, adapt checks.
 https://github.com/facebook/watchman/issues/49
+https://github.com/facebook/watchman/pull/54
 
 --- fstype.c.orig	2014-05-20 21:14:24.000000000 +0000
 +++ fstype.c
-@@ -53,11 +53,15 @@ w_string_t *w_fstype(const char *path)
+@@ -53,11 +53,16 @@ w_string_t *w_fstype(const char *path)
    }
  
    return w_string_new(name);
 -#elif defined(sun)
-+#elif HAVE_SYS_STATVFS_H
++#elif STATVFS_HAS_FSTYPE_AS_STRING
    struct statvfs sfs;
  
    if (statvfs(path, &sfs) == 0) {
-+#ifdef __NetBSD__
++#ifdef HAVE_STRUCT_STATVFS_F_FSTYPENAME
 +    return w_string_new(sfs.f_fstypename);
-+#else
++#endif
++#ifdef HAVE_STRUCT_STATVFS_F_BASETYPE
      return w_string_new(sfs.f_basetype);
 +#endif
    }
