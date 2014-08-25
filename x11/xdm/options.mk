5c61d123b937e02c7da290071df1b4b59c0e2216
obache <obache>
2014-08-25 08:30:46 +0000

    Let xft to be optional. PR pkg/49101.

--- x11/xdm/options.mk
+++ x11/xdm/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2012/06/12 15:46:06 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2014/08/25 08:30:46 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xdm
-PKG_SUPPORTED_OPTIONS+=	inet6
-PKG_SUGGESTED_OPTIONS=	inet6
+PKG_SUPPORTED_OPTIONS+=	inet6 xft2
+PKG_SUGGESTED_OPTIONS=	inet6 xft2
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,4 +10,11 @@ PKG_SUGGESTED_OPTIONS=	inet6
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
+.endif
+
+.if !empty(PKG_OPTIONS:Mxft2)
+.include "../../x11/libXft/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-xft
+.else
+CONFIGURE_ARGS+=	--without-xft
 .endif
