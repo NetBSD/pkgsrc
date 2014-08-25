b870f87f5079659beda3fdd6f53ce655945b4f50
spz <spz>
2014-08-25 09:17:26 +0000

    Pullup ticket #4485 - requested by tron lang/perl5: build fix
    Revisions pulled up:
    - lang/perl5/distinfo                                           1.122
    - lang/perl5/patches/patch-hints_netbsd.sh                      1.9
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	mrg
       Date:		Tue Aug 12 05:41:39 UTC 2014
    
       Modified Files:
       	pkgsrc/lang/perl5: distinfo
       	pkgsrc/lang/perl5/patches: patch-hints_netbsd.sh
    
       Log Message:
       adjust a pattern to match 0.8 vs everything else.
    
       fixes build on netbsd-7, which was matching "not everything else"
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.121 -r1.122 pkgsrc/lang/perl5/distinfo
       cvs rdiff -u -r1.8 -r1.9 pkgsrc/lang/perl5/patches/patch-hints_netbsd.sh

--- lang/perl5/patches/patch-hints_netbsd.sh
+++ lang/perl5/patches/patch-hints_netbsd.sh
@@ -1,12 +1,12 @@
-$NetBSD: patch-hints_netbsd.sh,v 1.8 2013/11/13 13:36:02 joerg Exp $
+$NetBSD: patch-hints_netbsd.sh,v 1.8.6.1 2014/08/25 09:17:26 spz Exp $
 
 --whole-archive is a linker flag, not a compiler flag
 Better defaults for paths.
 Stop grovelling for functions we don't want to provide (*host*)
 
---- hints/netbsd.sh.orig	2013-05-23 23:38:10.000000000 +0000
-+++ hints/netbsd.sh
-@@ -36,16 +36,6 @@ case "$osvers" in
+--- hints/netbsd.sh.orig	2014-08-11 22:30:50.000000000 -0700
++++ hints/netbsd.sh	2014-08-11 22:32:13.000000000 -0700
+@@ -36,16 +36,6 @@
  		d_dlerror=$define
  		cccdlflags="-DPIC -fPIC $cccdlflags"
  		lddlflags="-shared $lddlflags"
@@ -23,7 +23,18 @@ Stop grovelling for functions we don't want to provide (*host*)
  		rpathflag="-Wl,-rpath,"
  		case "$osvers" in
  		1.[0-5]*)
-@@ -100,6 +90,12 @@ case "$osvers" in
+@@ -89,7 +79,9 @@
+ 	;;
+ esac
+ case "$osvers" in
+-0.9*|1.*|2.*|3.*|4.*|5.*|6.*)
++0.8*)
++	;;
++*)
+ 	d_getprotoent_r="$undef"
+ 	d_getprotobyname_r="$undef"
+ 	d_getprotobynumber_r="$undef"
+@@ -100,6 +92,12 @@
  	d_getservbyport_r="$undef"
  	d_setservent_r="$undef"
  	d_endservent_r="$undef"
@@ -36,7 +47,7 @@ Stop grovelling for functions we don't want to provide (*host*)
  	d_getprotoent_r_proto="0"
  	d_getprotobyname_r_proto="0"
  	d_getprotobynumber_r_proto="0"
-@@ -110,6 +106,12 @@ case "$osvers" in
+@@ -110,6 +108,12 @@
  	d_getservbyport_r_proto="0"
  	d_setservent_r_proto="0"
  	d_endservent_r_proto="0"
@@ -49,7 +60,7 @@ Stop grovelling for functions we don't want to provide (*host*)
  	;;
  esac
  
-@@ -186,10 +188,12 @@ esac
+@@ -186,10 +190,12 @@
  EOCBU
  
  # Set sensible defaults for NetBSD: look for local software in
