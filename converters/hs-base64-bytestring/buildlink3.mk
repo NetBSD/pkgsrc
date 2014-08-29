f1998a4388533a73f20c27c86a7ff8bc385f9214
szptvlfn <szptvlfn>
2014-08-29 14:08:38 +0000

    make it clear what package depend on
    discussed with wiz@.

--- converters/hs-base64-bytestring/buildlink3.mk
+++ converters/hs-base64-bytestring/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/05/31 21:33:50 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:38 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-base64-bytestring
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	hs-base64-bytestring
 HS_BASE64_BYTESTRING_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=1.0.0
+BUILDLINK_ABI_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=1.0.0.1
 BUILDLINK_PKGSRCDIR.hs-base64-bytestring?=	../../converters/hs-base64-bytestring
 .endif	# HS_BASE64_BYTESTRING_BUILDLINK3_MK
 
--- www/hs-HTTP/buildlink3.mk
+++ www/hs-HTTP/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/07/24 21:18:24 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:41 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-HTTP
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	hs-HTTP
 HS_HTTP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.17
+BUILDLINK_ABI_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.17
 BUILDLINK_PKGSRCDIR.hs-HTTP?=	../../www/hs-HTTP
 
 .include "../../devel/hs-mtl/buildlink3.mk"
