ba7b26dfc074956be984a9b790fb21ee041a6f27
szptvlfn <szptvlfn>
2014-09-02 20:57:11 +0000

    Update to 1.0.4
    Changes from https://github.com/Gabriel439/Haskell-MMorph-Library
    1.0.4
     - Added MFunctor and MMonad instances for ExceptT

--- devel/hs-mmorph/buildlink3.mk
+++ devel/hs-mmorph/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:39 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/09/02 20:57:11 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-mmorph
 
 .if !defined(HS_MMORPH_BUILDLINK3_MK)
 HS_MMORPH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.3
-BUILDLINK_ABI_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.3
+BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.4
+BUILDLINK_ABI_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.4
 BUILDLINK_PKGSRCDIR.hs-mmorph?=	../../devel/hs-mmorph
 
 .include "../../devel/hs-transformers/buildlink3.mk"
