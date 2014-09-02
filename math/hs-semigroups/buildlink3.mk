af884f6b55f2dd93e9ce0e7131730a46051a3504
szptvlfn <szptvlfn@pkgsrc.org>
2014-09-02 20:58:16 +0000

    Import semigroups-0.15.2 as math/hs-semigroups, packaged for wip by pho.
    In mathematics, a semigroup is an algebraic structure consisting of a set
    together with an associative binary operation. A semigroup generalizes a
    monoid in that there might not exist an identity element. It also
    (originally) generalized a group (a monoid with all inverses) to a type
    where every element did not have to have an inverse, thus the name
    semigroup.

---
+++ math/hs-semigroups/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/09/02 20:58:16 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-semigroups
+
+.if !defined(HS_SEMIGROUPS_BUILDLINK3_MK)
+HS_SEMIGROUPS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.15.2
+BUILDLINK_ABI_DEPENDS.hs-semigroups+=	hs-semigroups>=0.15.2
+BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../math/hs-semigroups
+
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../math/hs-nats/buildlink3.mk"
+.include "../../devel/hs-text/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.endif	# HS_SEMIGROUPS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-semigroups
