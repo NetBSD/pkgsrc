0841ce4b1852cbc31312505e3df2ed0f6e3b48c5
szptvlfn <szptvlfn@pkgsrc.org>
2014-09-03 21:16:55 +0000

    Import void-0.6.1 as devel/hs-void, packaged for wip.
    A Haskell 98 logically uninhabited data type,
    used to indicate that a given term should not exist.

---
+++ devel/hs-void/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/09/03 21:16:55 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-void
+
+.if !defined(HS_VOID_BUILDLINK3_MK)
+HS_VOID_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-void+=	hs-void>=0.6.1
+BUILDLINK_ABI_DEPENDS.hs-void+=	hs-void>=0.6.1
+BUILDLINK_PKGSRCDIR.hs-void?=	../../devel/hs-void
+
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
+.endif	# HS_VOID_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-void
