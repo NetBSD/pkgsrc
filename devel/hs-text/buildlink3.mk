c80ae0b7420e9c32a2a9ac82ac80f9aaf6d93daa
szptvlfn <szptvlfn@pkgsrc.org>
2014-09-05 21:23:59 +0000

    Update to 1.1.1.3
    Changes from https://github.com/bos/text
    1.1.1.3
     - Export Array and MArray constructors

--- devel/hs-text/buildlink3.mk
+++ devel/hs-text/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:40 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/09/05 21:23:59 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-text
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-text
 HS_TEXT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=1.1.1
-BUILDLINK_ABI_DEPENDS.hs-text+=	hs-text>=1.1.1.2
+BUILDLINK_ABI_DEPENDS.hs-text+=	hs-text>=1.1.1.3
 BUILDLINK_PKGSRCDIR.hs-text?=	../../devel/hs-text
 .endif	# HS_TEXT_BUILDLINK3_MK
 
