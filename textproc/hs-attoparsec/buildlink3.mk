ec62ae1d453cdeacd2fef95a9d54e592528ef079
szptvlfn <szptvlfn@pkgsrc.org>
2014-09-06 21:20:06 +0000

    Import attoparsec-0.12.1.2 as textproc/hs-attoparsec, packaged for wip by pho.
    A fast parser combinator library, aimed particularly at dealing efficiently
    with network protocols and complicated text/binary file formats.

---
+++ textproc/hs-attoparsec/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/09/06 21:20:06 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-attoparsec
+
+.if !defined(HS_ATTOPARSEC_BUILDLINK3_MK)
+HS_ATTOPARSEC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.12.1
+BUILDLINK_ABI_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.12.1.2
+BUILDLINK_PKGSRCDIR.hs-attoparsec?=	../../textproc/hs-attoparsec
+
+.include "../../math/hs-scientific/buildlink3.mk"
+.include "../../devel/hs-text/buildlink3.mk"
+.endif	# HS_ATTOPARSEC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-attoparsec
