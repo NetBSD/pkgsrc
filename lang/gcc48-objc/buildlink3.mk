1e2fd8ff017395e1650bafaede0e065a4c4ce777
jperkin <jperkin>
2014-09-06 08:20:28 +0000

    Replace all references to _GCC_REQD with the appropriate logical value for each GCC version.  Using the variable causes impossible version constraints when a specific GCC is depended upon but the user is using something newer, as _GCC_REQD will be set to the higher value.

--- lang/gcc48-objc/buildlink3.mk
+++ lang/gcc48-objc/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/06/01 08:11:13 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/09/06 08:20:29 jperkin Exp $
 
 BUILDLINK_TREE+=	gcc48-objc
 
 .if !defined(GCC48_OBJC_BUILDLINK3_MK)
 GCC48_OBJC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gcc48-objc+=	gcc48-objc>=${_GCC_REQD}
+BUILDLINK_API_DEPENDS.gcc48-objc+=	gcc48-objc>=4.8
 BUILDLINK_PKGSRCDIR.gcc48-objc?=	../../lang/gcc48-objc
 
 BUILDLINK_GCC48_LIBDIRS=		gcc48/lib
@@ -15,7 +15,7 @@ BUILDLINK_DEPMETHOD.gcc48-objc?=	build
 
 # Packages that link against shared libraries need a full dependency.
 .if defined(_USE_GCC_SHLIB)
-DEPENDS+=	gcc48-libs>=${_GCC_REQD}:../../lang/gcc48-libs
+DEPENDS+=	gcc48-libs>=4.8:../../lang/gcc48-libs
 ABI_DEPENDS+=	gcc48-libs>=4.8.0:../../lang/gcc48-libs
 .endif
 
