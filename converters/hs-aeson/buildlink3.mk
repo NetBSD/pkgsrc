a307119c35ace9e72cc0bce7adccc1545109d284
szptvlfn <szptvlfn@pkgsrc.org>
2014-09-07 21:07:53 +0000

    Import aeson-0.7.0.6 as converters/hs-aeson, packaged for wip.
    A JSON parsing and encoding library optimized for ease of use and high
    performance.
    To get started, see the documentation for the Data.Aeson module.
    (A note on naming: in Greek mythology, Aeson was the father of Jason.)

---
+++ converters/hs-aeson/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/09/07 21:07:53 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-aeson
+
+.if !defined(HS_AESON_BUILDLINK3_MK)
+HS_AESON_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-aeson+=	hs-aeson>=0.7.0
+BUILDLINK_ABI_DEPENDS.hs-aeson+=	hs-aeson>=0.7.0.6
+BUILDLINK_PKGSRCDIR.hs-aeson?=	../../converters/hs-aeson
+
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../devel/hs-dlist/buildlink3.mk"
+.include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../devel/hs-mtl/buildlink3.mk"
+.include "../../math/hs-scientific/buildlink3.mk"
+.include "../../devel/hs-syb/buildlink3.mk"
+.include "../../devel/hs-text/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_AESON_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-aeson
