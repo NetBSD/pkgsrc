82ec7aa2d48e8a16921d0c9bef008e07e65759b2
wiz <wiz@pkgsrc.org>
2014-09-08 11:51:30 +0000

    buildlink3.mk should not set API depends. The package already pulls in those versions if needed.

--- x11/gtk3/buildlink3.mk
+++ x11/gtk3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2014/05/05 00:48:30 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2014/09/08 11:51:30 wiz Exp $
 
 BUILDLINK_TREE+=	gtk3
 
@@ -25,11 +25,9 @@ pkgbase := gtk3
 .include "../../graphics/gdk-pixbuf2/buildlink3.mk"
 .if !empty(PKG_BUILD_OPTIONS.gtk3:Mx11)
 .  if !empty(PKG_BUILD_OPTIONS.gtk3:Mgtk3-atk-bridge)
-BUILDLINK_API_DEPENDS.at-spi2-atk+=	at-spi2-atk>=2.6.1
 .  include "../../devel/at-spi2-atk/buildlink3.mk"
 .  endif
 .include "../../x11/libXcursor/buildlink3.mk"
-BUILDLINK_API_DEPENDS.Xft2+=	Xft2>=2.1.2nb2
 .include "../../x11/libXft/buildlink3.mk"
 .include "../../x11/libXrandr/buildlink3.mk"
 .include "../../x11/libXinerama/buildlink3.mk"
