52cfef569ebe1618f0ce0c7c2666e83582fd4c8f
szptvlfn <szptvlfn@pkgsrc.org>
2014-09-09 19:09:09 +0000

    Import pandoc-types-1.12.4.1 as textproc/hs-pandoc-types, packaged for wip.
    Text.Pandoc.Definition defines the Pandoc data structure, which is used by
    pandoc to represent structured documents. This module used to live in the
    pandoc package, but starting with pandoc 1.7, it has been split off, so
    that other packages can use it without drawing in all of pandoc's
    dependencies, and pandoc itself can depend on packages (like citeproc-hs)
    that use them.
    
    Text.Pandoc.Builder provides functions for building up Pandoc structures
    programmatically.
    
    Text.Pandoc.Generic provides generic functions for manipulating Pandoc
    documents.
    
    Text.Pandoc.Walk provides faster, nongeneric functions for manipulating
    Pandoc documents.
    
    Text.Pandoc.JSON provides functions for serializing and deserializing a
    Pandoc structure to and from JSON.

---
+++ textproc/hs-pandoc-types/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/09/09 19:09:09 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-pandoc-types
+
+.if !defined(HS_PANDOC_TYPES_BUILDLINK3_MK)
+HS_PANDOC_TYPES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.4
+BUILDLINK_ABI_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.4.1
+BUILDLINK_PKGSRCDIR.hs-pandoc-types?=	../../textproc/hs-pandoc-types
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-deepseq-generics/buildlink3.mk"
+.include "../../devel/hs-syb/buildlink3.mk"
+.endif	# HS_PANDOC_TYPES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-pandoc-types
