4bc487b7b67bbe7f545014f14da64e91c68b0851
wiz <wiz>
2014-09-10 12:16:55 +0000

    Add upstream bug report URL.

--- audio/musicpd/patches/patch-src_thread_PosixMutex.hxx
+++ audio/musicpd/patches/patch-src_thread_PosixMutex.hxx
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_thread_PosixMutex.hxx,v 1.1 2014/09/10 12:10:23 joerg Exp $
+$NetBSD: patch-src_thread_PosixMutex.hxx,v 1.2 2014/09/10 12:16:55 wiz Exp $
+
+http://bugs.musicpd.org/view.php?id=4110
 
 --- src/thread/PosixMutex.hxx.orig	2014-09-09 20:07:45.000000000 +0000
 +++ src/thread/PosixMutex.hxx
