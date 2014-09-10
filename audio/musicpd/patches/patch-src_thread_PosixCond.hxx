de02c4ff9e4e6a47ef09368668cf38539fff1c7d
wiz <wiz@pkgsrc.org>
2014-09-10 12:16:55 +0000

    Add upstream bug report URL.

--- audio/musicpd/patches/patch-src_thread_PosixCond.hxx
+++ audio/musicpd/patches/patch-src_thread_PosixCond.hxx
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_thread_PosixCond.hxx,v 1.1 2014/09/10 12:10:23 joerg Exp $
+$NetBSD: patch-src_thread_PosixCond.hxx,v 1.2 2014/09/10 12:16:55 wiz Exp $
+
+http://bugs.musicpd.org/view.php?id=4110
 
 --- src/thread/PosixCond.hxx.orig	2014-09-09 20:08:12.000000000 +0000
 +++ src/thread/PosixCond.hxx
