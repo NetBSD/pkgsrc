89277e1a064bc2df249588344754437a4cf9d47c
szptvlfn <szptvlfn@pkgsrc.org>
2014-09-11 21:57:46 +0000

    Import hint-0.4.2.0 as devel/hs-hint, packaged for wip by pho.
    This library defines an Interpreter monad. It allows to load Haskell
    modules, browse them, type-check and evaluate strings with Haskell
    expressions and even coerce them into values. The library is thread-safe
    and type-safe (even the coercion of expressions to values). It is,
    esentially, a huge subset of the GHC API wrapped in a simpler API.

---
+++ devel/hs-hint/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/09/11 21:57:46 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-hint
+
+.if !defined(HS_HINT_BUILDLINK3_MK)
+HS_HINT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hint+=	hs-hint>=0.4.2
+BUILDLINK_ABI_DEPENDS.hs-hint+=	hs-hint>=0.4.2.0
+BUILDLINK_PKGSRCDIR.hs-hint?=	../../devel/hs-hint
+
+.include "../../devel/hs-exceptions/buildlink3.mk"
+.include "../../devel/hs-extensible-exceptions/buildlink3.mk"
+.include "../../devel/hs-ghc-mtl/buildlink3.mk"
+.include "../../devel/hs-ghc-paths/buildlink3.mk"
+.include "../../devel/hs-mtl/buildlink3.mk"
+.include "../../devel/hs-random/buildlink3.mk"
+.include "../../devel/hs-utf8-string/buildlink3.mk"
+.endif	# HS_HINT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hint
