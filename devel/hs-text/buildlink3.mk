5e22b2270f1483407dc43baaa76ab8b8efecb63e
szptvlfn <szptvlfn@pkgsrc.org>
2014-09-13 22:03:04 +0000

    Update to 1.2.0.0
    changelog:
    1.2.0.0
    
    * Fixed an integer overflow in the replace function
      (https://github.com/bos/text/issues/81)
    
    * Fixed a hang in lazy decodeUtf8With
      (https://github.com/bos/text/issues/87)
    
    * Reduced codegen bloat caused by use of empty and single-character
      literals
    
    * Added an instance of IsList for GHC 7.8 and above

--- devel/hs-text/buildlink3.mk
+++ devel/hs-text/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/09/05 21:23:59 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/09/13 22:03:04 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-text
 
 .if !defined(HS_TEXT_BUILDLINK3_MK)
 HS_TEXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=1.1.1
-BUILDLINK_ABI_DEPENDS.hs-text+=	hs-text>=1.1.1.3
+BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=1.2.0
+BUILDLINK_ABI_DEPENDS.hs-text+=	hs-text>=1.2.0.0
 BUILDLINK_PKGSRCDIR.hs-text?=	../../devel/hs-text
 .endif	# HS_TEXT_BUILDLINK3_MK
 
