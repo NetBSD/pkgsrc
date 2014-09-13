f85b5fccce690ffe6558f40e127bf470ec023782
szptvlfn <szptvlfn@pkgsrc.org>
2014-09-13 22:23:27 +0000

    Bump PKGREVISION for hs-text-1.2.0.0

--- devel/hs-void/buildlink3.mk
+++ devel/hs-void/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/09/03 21:16:55 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/09/13 22:23:28 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-void
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-void
 HS_VOID_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-void+=	hs-void>=0.6.1
-BUILDLINK_ABI_DEPENDS.hs-void+=	hs-void>=0.6.1
+BUILDLINK_ABI_DEPENDS.hs-void+=	hs-void>=0.6.1nb2
 BUILDLINK_PKGSRCDIR.hs-void?=	../../devel/hs-void
 
 .include "../../devel/hs-hashable/buildlink3.mk"
