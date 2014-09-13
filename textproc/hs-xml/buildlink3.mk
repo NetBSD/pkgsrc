e3f1fcb52b56f9b9196e8b53221a498be4402c58
szptvlfn <szptvlfn>
2014-09-13 22:23:27 +0000

    Bump PKGREVISION for hs-text-1.2.0.0

--- textproc/hs-xml/buildlink3.mk
+++ textproc/hs-xml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:41 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/09/13 22:23:28 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-xml
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-xml
 HS_XML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-xml+=	hs-xml>=1.3.13
-BUILDLINK_ABI_DEPENDS.hs-xml+=	hs-xml>=1.3.13
+BUILDLINK_ABI_DEPENDS.hs-xml+=	hs-xml>=1.3.13nb2
 BUILDLINK_PKGSRCDIR.hs-xml?=	../../textproc/hs-xml
 
 .include "../../devel/hs-text/buildlink3.mk"
