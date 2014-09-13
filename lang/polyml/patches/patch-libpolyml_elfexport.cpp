c8e8ae7c740f80d2802d3701bea96417c3920219
asau <asau@pkgsrc.org>
2014-09-13 18:23:38 +0000

    Update to Poly/ML 5.5.2
    Most of the changes are bug fixes.  One major fix was to the
    intermediate code optimiser reported back in September.
    Other changes are the addition of PrettyStringWithWidth and
    PrettyLineBreak constructors to the PolyML.pretty datatype,
    Thread.Thread.numPhysicalProcessors to return the number of
    physical processors and printing ?.t in circumstances where t is
    bound to a different type.  The debugger has been extended to
    include structures and the arguments to functors.

--- lang/polyml/patches/patch-libpolyml_elfexport.cpp
+++ lang/polyml/patches/patch-libpolyml_elfexport.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-libpolyml_elfexport.cpp,v 1.2 2013/12/15 18:35:23 asau Exp $
+$NetBSD: patch-libpolyml_elfexport.cpp,v 1.3 2014/09/13 18:23:38 asau Exp $
 
---- libpolyml/elfexport.cpp.orig	2013-06-11 11:13:52.000000000 +0000
+--- libpolyml/elfexport.cpp.orig	2013-10-15 16:25:39.000000000 +0000
 +++ libpolyml/elfexport.cpp
 @@ -65,6 +65,13 @@
  #ifdef HAVE_SYS_ELF_AMD64_H
@@ -13,6 +13,6 @@ $NetBSD: patch-libpolyml_elfexport.cpp,v 1.2 2013/12/15 18:35:23 asau Exp $
 +/* Missing definition taken from NetBSD: */
 +#define	R_386_PC32	2
 +#endif
- #ifdef HAVE_STRING_H
- #include <string.h>
- #endif
+ 
+ // Android has the ARM relocation symbol here
+ #ifdef HAVE_ASM_ELF_H
