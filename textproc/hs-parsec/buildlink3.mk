5853737469f78246ed1feb2e0a6458d484285dc9
szptvlfn <szptvlfn>
2014-09-13 22:06:52 +0000

    Update to 3.1.6
    CHANGES:
    3.1.6
    
    - Export 'Text' instances from Text.Parsec
    - Make Text.Parsec exports more visible
    - Re-arrange Text.Parsec exports
    - Add functions 'crlf' and 'endOfLine' to Text.Parsec.Char for handling
      input streams that do not have normalized line terminators.
    - Fix off-by-one error in Token.charControl

--- textproc/hs-parsec/buildlink3.mk
+++ textproc/hs-parsec/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:41 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/09/13 22:06:52 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-parsec
 
 .if !defined(HS_PARSEC_BUILDLINK3_MK)
 HS_PARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-parsec+=	hs-parsec>=3.1.5
-BUILDLINK_ABI_DEPENDS.hs-parsec+=	hs-parsec>=3.1.5nb1
+BUILDLINK_API_DEPENDS.hs-parsec+=	hs-parsec>=3.1.6
+BUILDLINK_ABI_DEPENDS.hs-parsec+=	hs-parsec>=3.1.6
 BUILDLINK_PKGSRCDIR.hs-parsec?=	../../textproc/hs-parsec
 
 .include "../../devel/hs-mtl/buildlink3.mk"
