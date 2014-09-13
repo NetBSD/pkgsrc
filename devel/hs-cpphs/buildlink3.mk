a3d7f111bddf95cb55af675ddcc86ce3efa75683
szptvlfn <szptvlfn@pkgsrc.org>
2014-09-13 22:23:27 +0000

    Bump PKGREVISION for hs-text-1.2.0.0

--- devel/hs-cpphs/buildlink3.mk
+++ devel/hs-cpphs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:38 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/09/13 22:23:28 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cpphs
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-cpphs
 HS_CPPHS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.5
-BUILDLINK_ABI_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.5
+BUILDLINK_ABI_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.5nb2
 BUILDLINK_PKGSRCDIR.hs-cpphs?=	../../devel/hs-cpphs
 
 .include "../../textproc/hs-polyparse/buildlink3.mk"
--- devel/hs-vault/buildlink3.mk
+++ devel/hs-vault/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:40 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/09/13 22:23:28 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-vault
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-vault
 HS_VAULT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-vault+=	hs-vault>=0.3.0
-BUILDLINK_ABI_DEPENDS.hs-vault+=	hs-vault>=0.3.0.3
+BUILDLINK_ABI_DEPENDS.hs-vault+=	hs-vault>=0.3.0.3nb2
 BUILDLINK_PKGSRCDIR.hs-vault?=	../../devel/hs-vault
 
 .include "../../devel/hs-hashable/buildlink3.mk"
