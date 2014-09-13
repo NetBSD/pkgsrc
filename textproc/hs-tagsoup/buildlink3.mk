c9eb285af9cad1d3ccdf2c56456f6cdb8c2dfbc6
szptvlfn <szptvlfn@pkgsrc.org>
2014-09-13 22:23:27 +0000

    Bump PKGREVISION for hs-text-1.2.0.0

--- textproc/hs-tagsoup/buildlink3.mk
+++ textproc/hs-tagsoup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:41 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/09/13 22:23:28 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-tagsoup
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-tagsoup
 HS_TAGSOUP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.1
-BUILDLINK_ABI_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.1
+BUILDLINK_ABI_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.1nb2
 BUILDLINK_PKGSRCDIR.hs-tagsoup?=	../../textproc/hs-tagsoup
 
 .include "../../devel/hs-text/buildlink3.mk"
--- www/hs-HTTP/buildlink3.mk
+++ www/hs-HTTP/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:41 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/09/13 22:23:29 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-HTTP
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-HTTP
 HS_HTTP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.17
-BUILDLINK_ABI_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.17
+BUILDLINK_ABI_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.17nb2
 BUILDLINK_PKGSRCDIR.hs-HTTP?=	../../www/hs-HTTP
 
 .include "../../devel/hs-mtl/buildlink3.mk"
