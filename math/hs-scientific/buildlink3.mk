5d1c61243fce3844ba7b7a333badcd1f38dded3a
szptvlfn <szptvlfn@pkgsrc.org>
2014-09-14 18:25:25 +0000

    Update to 0.3.3.1
    changelog:
    0.3.3.1
    	*  Allow newer tasty, tasty-hunit and criterion

--- math/hs-scientific/buildlink3.mk
+++ math/hs-scientific/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/08/29 14:08:41 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/09/14 18:25:25 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-scientific
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-scientific
 HS_SCIENTIFIC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-scientific+=	hs-scientific>=0.3.3
-BUILDLINK_ABI_DEPENDS.hs-scientific+=	hs-scientific>=0.3.3.0
+BUILDLINK_ABI_DEPENDS.hs-scientific+=	hs-scientific>=0.3.3.1
 BUILDLINK_PKGSRCDIR.hs-scientific?=	../../math/hs-scientific
 
 .include "../../devel/hs-hashable/buildlink3.mk"
