3996eae7e17dd33c3e5a7c17eb9c61ab033e27a8
szptvlfn <szptvlfn@pkgsrc.org>
2014-09-14 18:26:13 +0000

    Update to 0.15.3
    CHANGELOG:
    0.15.3
    ------
    * `instance NFData a => NFData (NonEmpty a)`
    * Added `NFData` instances for the types in Data.Semigroup

--- math/hs-semigroups/buildlink3.mk
+++ math/hs-semigroups/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/09/02 20:58:16 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/09/14 18:26:13 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-semigroups
 
 .if !defined(HS_SEMIGROUPS_BUILDLINK3_MK)
 HS_SEMIGROUPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.15.2
-BUILDLINK_ABI_DEPENDS.hs-semigroups+=	hs-semigroups>=0.15.2
+BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.15.3
+BUILDLINK_ABI_DEPENDS.hs-semigroups+=	hs-semigroups>=0.15.3
 BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../math/hs-semigroups
 
 .include "../../devel/hs-hashable/buildlink3.mk"
