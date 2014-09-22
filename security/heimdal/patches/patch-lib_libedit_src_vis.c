c11db8016df4ecdb4ed1b94740f442eac3e620de
joerg <joerg@pkgsrc.org>
2014-09-22 11:55:37 +0000

    Clean up RCS IDs in patches and PLIST to avoid permanent rebuilds.

--- security/heimdal/patches/patch-lib_libedit_src_vis.c
+++ security/heimdal/patches/patch-lib_libedit_src_vis.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-lib_libedit_src_vis.c,v 1.1 2014/05/16 12:49:42 ryoon Exp $
+$NetBSD: patch-lib_libedit_src_vis.c,v 1.2 2014/09/22 11:55:37 joerg Exp $
 
 * Fix OpenBSD 5.5 build
 
 --- lib/libedit/src/vis.c.orig	2012-12-09 22:06:44.000000000 +0000
 +++ lib/libedit/src/vis.c
-@@ -67,6 +67,7 @@ __RCSID("$NetBSD: vis.c,v 1.41 2009/11/2
+@@ -67,6 +67,7 @@
  #include <vis.h>
  #include <stdlib.h>
  
