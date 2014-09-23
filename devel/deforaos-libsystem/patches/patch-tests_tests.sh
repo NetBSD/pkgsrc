7dc03998aa72269560ae345eddba3789a655556e
jperkin <jperkin>
2014-09-23 12:06:49 +0000

    Fix some build issues exposed by the cwrappers build:
      - Use REPLACE_SH for echo -n.
      - Use LD_LIBRARY_PATH for tests to find pre-installed libraries.
      - Fix parallel build issues.

--- devel/deforaos-libsystem/patches/patch-tests_tests.sh
+++ devel/deforaos-libsystem/patches/patch-tests_tests.sh
@@ -1,9 +1,19 @@
-$NetBSD: patch-tests_tests.sh,v 1.1 2014/09/16 19:25:51 khorben Exp $
+$NetBSD: patch-tests_tests.sh,v 1.2 2014/09/23 12:06:49 jperkin Exp $
 
 Avoid building the (experimental) Python binding.
+Use LD_LIBRARY_PATH to find pre-installed libraries.
 
 --- tests/tests.sh.orig	2014-08-29 19:23:48.000000000 +0000
 +++ tests/tests.sh
+@@ -50,7 +50,7 @@
+ 	echo -n "$test:" 1>&2
+ 	(echo
+ 	echo "Testing: ./$test" "$@"
+-	"./$test" "$@") >> "$target" 2>&1
++	env LD_LIBRARY_PATH=../src "./$test" "$@") >> "$target" 2>&1
+ 	res=$?
+ 	if [ $res -ne 0 ]; then
+ 		echo " FAIL" 1>&2
 @@ -103,7 +103,7 @@ _test "includes"
  _test "string"
  _test "variable"
