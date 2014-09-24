8ab732cf32a206d055b6fe8cfaabd7322454d564
jperkin <jperkin@pkgsrc.org>
2014-09-24 11:10:38 +0000

    USE_TOOLS+=gsed for -r support, plus various patches to fix SunOS build.

--- audio/alsa-utils/patches/patch-alsamixer_volume__mapping.c
+++ audio/alsa-utils/patches/patch-alsamixer_volume__mapping.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-alsamixer_volume__mapping.c,v 1.1 2014/06/09 12:34:26 ryoon Exp $
+$NetBSD: patch-alsamixer_volume__mapping.c,v 1.2 2014/09/24 11:10:38 jperkin Exp $
 
-* NetBSD has no exp10()
+* NetBSD and SunOS have no exp10()
 
 --- alsamixer/volume_mapping.c.orig	2013-07-09 15:28:37.000000000 +0000
 +++ alsamixer/volume_mapping.c
@@ -9,11 +9,11 @@ $NetBSD: patch-alsamixer_volume__mapping.c,v 1.1 2014/06/09 12:34:26 ryoon Exp $
  #include "volume_mapping.h"
  
 -#ifdef __UCLIBC__
-+#if defined(__UCLIBC__) || defined(__NetBSD__)
++#if defined(__UCLIBC__) || defined(__NetBSD__) || defined(__sun)
  /* 10^x = 10^(log e^x) = (e^x)^log10 = e^(x * log 10) */
  #define exp10(x) (exp((x) * log(10)))
 -#endif /* __UCLIBC__ */
-+#endif /* __UCLIBC__ || __NetBSD__ */
++#endif /* __UCLIBC__ || __NetBSD__ || __sun */
  
  #define MAX_LINEAR_DB_SCALE	24
  
