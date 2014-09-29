8629cd65e0c92394ba7d9c97397713c07a084214
wiz <wiz@pkgsrc.org>
2014-09-29 12:08:12 +0000

    Add another upstream security fix patch. Welcome to 4.3.027.

--- shells/bash/patches/patch-variables.c
+++ shells/bash/patches/patch-variables.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-variables.c,v 1.1 2014/09/25 20:28:32 christos Exp $
+$NetBSD: patch-variables.c,v 1.2 2014/09/29 12:08:12 wiz Exp $
 
 Only read functions from environment if flag is set.
 
---- variables.c.christos	2014-09-25 16:09:41.000000000 -0400
-+++ variables.c	2014-09-25 16:12:10.000000000 -0400
-@@ -105,6 +105,7 @@
+--- variables.c.orig	2014-09-29 12:04:58.000000000 +0000
++++ variables.c
+@@ -110,6 +110,7 @@ extern time_t shell_start_time;
  extern int assigning_in_environment;
  extern int executing_builtin;
  extern int funcnest_max;
@@ -12,12 +12,12 @@ Only read functions from environment if flag is set.
  
  #if defined (READLINE)
  extern int no_line_editing;
-@@ -349,7 +350,7 @@
+@@ -354,7 +355,7 @@ initialize_shell_variables (env, privmod
  
        /* If exported function, define it now.  Don't import functions from
  	 the environment in privileged mode. */
--      if (privmode == 0 && read_but_dont_execute == 0 && STREQN ("() {", string, 4))
-+      if (import_functions && privmode == 0 && read_but_dont_execute == 0 && STREQN ("() {", string, 4))
- 	{
- 	  string_length = strlen (string);
- 	  temp_string = (char *)xmalloc (3 + string_length + char_index);
+-      if (privmode == 0 && read_but_dont_execute == 0 && 
++      if (import_functions && privmode == 0 && read_but_dont_execute == 0 && 
+           STREQN (BASHFUNC_PREFIX, name, BASHFUNC_PREFLEN) &&
+           STREQ (BASHFUNC_SUFFIX, name + char_index - BASHFUNC_SUFFLEN) &&
+ 	  STREQN ("() {", string, 4))
