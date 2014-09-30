e98b86db65927bce756678e3184bbe97059f53d9
taca <taca@pkgsrc.org>
2014-09-30 08:16:10 +0000

    Update to php54 to 5.4.33, aprroved by wiz@.
    18 Sep 2014, PHP 5.4.33
    
    - Core:
      . Fixed bug #47358 (glob returns error, should be empty array()). (Pierre)
      . Fixed bug #65463 (SIGSEGV during zend_shutdown()). (Keyur Govande)
      . Fixed bug #66036 (Crash on SIGTERM in apache process). (Keyur Govande)
    
    - OpenSSL:
      . Fixed bug #41631 (socket timeouts not honored in blocking SSL reads).
        (Daniel Lowrey)
    
    - Date:
      . Fixed bug #66091 (memory leaks in DateTime constructor). (Tjerk)
    
    - FPM:
      . Fixed #67606 (FPM with mod_fastcgi/apache2.4 is broken). (David Zuelke)
    
    - GD:
      . Made fontFetch's path parser thread-safe. (Sara)
    
    - Wddx:
      . Fixed bug #67873 (Segfaults in php_wddx_serialize_var). (Anatol, Remi)
    
    - Zlib:
      . Fixed bug #67724 (chained zlib filters silently fail with large amounts of
        data). (Mike)
      . Fixed bug #67865 (internal corruption phar error). (Mike)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.72 2014/09/30 08:14:25 taca Exp $
+# $NetBSD: phpversion.mk,v 1.73 2014/09/30 08:16:10 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -82,7 +82,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP53_VERSION=	5.3.29
-PHP54_VERSION=	5.4.32
+PHP54_VERSION=	5.4.33
 PHP55_VERSION=	5.5.17
 
 # Define initial release of major version.
