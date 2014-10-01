286fa15e65e93799c64b246bd4c944de0df39a44
wiz <wiz@pkgsrc.org>
2014-10-01 16:09:34 +0000

    Recursive bump for ffmpeg2 shared library major version bump.

--- graphics/osg/buildlink3.mk
+++ graphics/osg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.37 2014/08/22 11:24:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.38 2014/10/01 16:09:34 wiz Exp $
 
 BUILDLINK_TREE+=	osg
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	osg
 OSG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.osg+=	osg>=2.8.3
-BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.0.1nb33
+BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.0.1nb34
 BUILDLINK_PKGSRCDIR.osg?=	../../graphics/osg
 
 .include "../../graphics/cairo/buildlink3.mk"
