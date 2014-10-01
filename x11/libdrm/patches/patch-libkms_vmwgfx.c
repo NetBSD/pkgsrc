f82919db2123c3d4945725cc92abcb5c60d7a5f1
wiz <wiz@pkgsrc.org>
2014-10-01 09:39:30 +0000

    Update to 2.4.58:
    2.4.58:
    
        Rob Clark (3):
          fix compile error on 32bit systems
          more compile fix
          configure.ac: bump version to 2.4.58 for release
    
    2.4.57:
    
        Alex Deucher (2):
          radeon: add new CIK pci ids
          radeon: add new SI pci ids
    
        Benjamin Gaignard (1):
          modetest: Add support of STI driver
    
        Damien Lespiau (1):
          intel: Don't leak the test page in an has_userptr() error path
    
        Emil Velikov (27):
          all: include config.h only when available and use its defines
          libkms: remove explicit define _FILE_OFFSET_BITS 64
          libdrm, freedreno, intel, nouveau, radeon: add Makefile.sources
          libdrm,intel: Add Android build
          libdrm,intel: rework android header handling
          radeon: add Android build support
          nouveau: add Android build support
          freedreno: add Android build support
          android: add CleanSpec.mk
          freedreno: fd_bo_from_handle() remove duplicate declaration
          automake: remove obsolete makefiles
          Remove i810_drm.h and i830_drm.h from the distribution tarball
          automake: fix 'make commit-headers'
          configure: unconditionally check for atomic ops/primitives
          libkms: build the intel backend only when needed
          libkms: move sources lists to makefile.sources
          libkms: add Android build
          modetest: move sources lists to makefiles.sources
          modetest: add Android build
          automake: pick up all files for distribution.
          Add private mmap/munmap wrappers
          drm: use drm_mmap/drm_munmap wrappers
          libkms: use drm_mmap/drm_munmap wrappers
          nouveau: use drm_mmap/drm_munmap wrappers
          radeon: use drm_mmap/drm_munmap wrappers
          freedreno: use drm_mmap/drm_munmap wrappers
          intel: use drm_mmap/drm_munmap wrappers
    
        François Tigeot (1):
          drm: Implement drmCheckModesettingSupported() for DragonFly
    
        Lionel Landwerlin (4):
          atomic: add atomic_add_unless()
          intel: make bufmgr_gem shareable from different API
          intel: make bo_unreference() thread safe
          intel: make drm_intel_gem_bo_get_reloc_count() thread safe
    
        Maarten Lankhorst (6):
          nouveau: Only export public functions.
          radeon: Use symbol visibility.
          intel: Use symbol visibility.
          omap: Use symbol visibility.
          freedreno: Use symbol visibility.
          exynos: Use symbol visibility.
    
        Maks Naumov (1):
          radeon: Fix surf->bankh init by default value when surf->tile_split == 0
    
        Rafal Sapala (1):
          intel: Adding locks for drm objects synchronization.
    
        Rob Clark (2):
          freedreno: add dmabuf import/export helpers
          configure.ac: bump version to 2.4.57 for release
    
        Thierry Reding (1):
          configure: Support symbol visibility when available
    
        Tvrtko Ursulin (1):
          intel: Add support for userptr objects

--- x11/libdrm/patches/patch-libkms_vmwgfx.c
+++ x11/libdrm/patches/patch-libkms_vmwgfx.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-libkms_vmwgfx.c,v 1.1 2014/04/12 09:28:42 wiz Exp $
+$NetBSD: patch-libkms_vmwgfx.c,v 1.2 2014/10/01 09:39:30 wiz Exp $
 
 Provide compatibility errno number for non-Linux.
 
---- libkms/vmwgfx.c.orig	2013-08-12 18:01:22.000000000 +0000
+--- libkms/vmwgfx.c.orig	2014-09-28 16:19:54.000000000 +0000
 +++ libkms/vmwgfx.c
-@@ -38,6 +38,10 @@
- #include "xf86drm.h"
+@@ -39,6 +39,10 @@
+ #include "libdrm.h"
  #include "vmwgfx_drm.h"
  
 +#ifndef ERESTART
