5e7dffba4c7d7a257abb59595a1bdc6a5b7be00c
wiz <wiz>
2014-10-01 16:09:34 +0000

    Recursive bump for ffmpeg2 shared library major version bump.

--- multimedia/kdemultimedia3/buildlink3.mk
+++ multimedia/kdemultimedia3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.59 2014/08/22 11:24:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.60 2014/10/01 16:09:34 wiz Exp $
 
 BUILDLINK_TREE+=	kdemultimedia
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kdemultimedia
 KDEMULTIMEDIA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kdemultimedia+=	kdemultimedia>=3.5.0nb2
-BUILDLINK_ABI_DEPENDS.kdemultimedia+=	kdemultimedia>=3.5.10nb39
+BUILDLINK_ABI_DEPENDS.kdemultimedia+=	kdemultimedia>=3.5.10nb40
 BUILDLINK_PKGSRCDIR.kdemultimedia?=	../../multimedia/kdemultimedia3
 
 .include "../../audio/arts/buildlink3.mk"
