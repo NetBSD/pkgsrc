124a5cd105e27b0f1d64945de462849e56f61d73
wiz <wiz@pkgsrc.org>
2014-10-01 16:09:34 +0000

    Recursive bump for ffmpeg2 shared library major version bump.

--- multimedia/xine-lib/buildlink3.mk
+++ multimedia/xine-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.29 2014/08/13 08:23:50 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2014/10/01 16:09:34 wiz Exp $
 
 BUILDLINK_TREE+=	xine-lib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	xine-lib
 XINE_LIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xine-lib+=	xine-lib>=1rc3c
-BUILDLINK_ABI_DEPENDS.xine-lib+=	xine-lib>=1.2.5nb5
+BUILDLINK_ABI_DEPENDS.xine-lib+=	xine-lib>=1.2.5nb6
 BUILDLINK_PKGSRCDIR.xine-lib?=	../../multimedia/xine-lib
 
 .include "../../multimedia/ffmpeg2/buildlink3.mk"
