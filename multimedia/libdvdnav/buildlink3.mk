08f4dd4b7f78b1a38650ad64c2483c3ef321090e
wiz <wiz@pkgsrc.org>
2014-10-04 12:49:05 +0000

    Bump ABI depends since libdvdnav-5 removed a library. Recursive PKGREVISION bump.

--- multimedia/libdvdnav/buildlink3.mk
+++ multimedia/libdvdnav/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:25:03 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2014/10/04 12:49:05 wiz Exp $
 
 BUILDLINK_TREE+=	libdvdnav
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libdvdnav
 LIBDVDNAV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libdvdnav+=	libdvdnav>=4.1.3
-BUILDLINK_ABI_DEPENDS.libdvdnav+=	libdvdnav>=4.1.3
+BUILDLINK_ABI_DEPENDS.libdvdnav+=	libdvdnav>=5.0
 BUILDLINK_PKGSRCDIR.libdvdnav?=	../../multimedia/libdvdnav
 
 .include "../../multimedia/libdvdread/buildlink3.mk"
