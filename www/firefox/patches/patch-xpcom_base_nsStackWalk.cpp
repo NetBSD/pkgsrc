2e9b793192535d51545ee193cffca85098fd647d
ryoon <ryoon>
2014-10-05 01:59:08 +0000

    Update to 32.0.3
    Changelog:
    
    Fixed
    32.0.3: New security fixes can be found here
    
    New
    New HTTP cache provides improved performance including crash recovery
    
    New
    Integration of generational garbage collection
    
    New
    Public key pinning support enabled
    
    New
    View historical use information for logins stored in password manager
    
    New
    Display the number of found items in the find toolbar
    
    New
    Easier back, forward, reload, and bookmarking through the context menu
    
    New
    Lower Sorbian [dsb] locale added
    
    Changed
    Removed and turned off trust bit for some 1024-bit root certificates
    
    Changed
    Performance improvements to Password Manager and Add-on Manager
    
    HTML5
    drawFocusIfNeeded enabled by default
    
    HTML5
    ECMAScript 6 built-in method Array#copyWithin implemented
    
    HTML5
    CSS position:sticky enabled by default
    
    HTML5
    mix-blend-mode enabled by default
    
    HTML5
    New Array built-in: Array.from()
    
    HTML5
    navigator.languages property and languagechange event implemented
    
    HTML5
    Vibration API updated to latest W3C spec
    
    HTML5
    CSS box-decoration-break replaces -moz-background-inline-policy
    
    HTML5
    box-decoration-break enabled by default
    
    Developer
    HiDPI support in Developer Tools UI
    
    Developer
    Inspector button moved to the top left
    
    Developer
    Hidden nodes displayed differently in the markup-view
    
    Developer
    New Web Audio Editor
    
    Developer
    Code completion and inline documentation added to Scratchpad
    
    Fixed
    32.0.2 - Corrupt installations cause Firefox to crash on update
    
    Fixed
    32.0.1 - Stability issues for computers with multiple graphics cards
    
    Fixed
    32.0.1 - Mixed content icon may be incorrectly displayed instead of lock icon for SSL sites
    
    Fixed
    32.0.1 - WebRTC: setRemoteDescription() silently fails if no success callback is specified
    
    Fixed
    Various security fixes
    
    Fixed
    Mac OS X: cmd-L does not open a new window when no window is available
    
    Fixed
    Text Rendering Issues on Windows 7 with Platform Update KB2670838 (MSIE 10 Prerequisite) or on Windows 8.1
    
    
    Security fixes:
    Fixed in Firefox 32.0.3
    MFSA 2014-73 RSA Signature Forgery in NSS
    
    Fixed in Firefox 32
    MFSA 2014-72 Use-after-free setting text directionality
    MFSA 2014-71 Profile directory file access through file: protocol
    MFSA 2014-70 Out-of-bounds read in Web Audio audio timeline
    MFSA 2014-69 Uninitialized memory use during GIF rendering
    MFSA 2014-68 Use-after-free during DOM interactions with SVG
    MFSA 2014-67 Miscellaneous memory safety hazards (rv:32.0 / rv:31.1 / rv:24.8)

--- www/firefox/patches/patch-xpcom_base_nsStackWalk.cpp
+++ www/firefox/patches/patch-xpcom_base_nsStackWalk.cpp
@@ -1,36 +1,20 @@
-$NetBSD: patch-xpcom_base_nsStackWalk.cpp,v 1.7 2014/06/11 00:40:59 ryoon Exp $
+$NetBSD: patch-xpcom_base_nsStackWalk.cpp,v 1.8 2014/10/05 01:59:08 ryoon Exp $
 
 * Replace XP_MACOSX with XP_DARWIN as the former is not defined when
   the toolkit is not cocoa.
 
---- xpcom/base/nsStackWalk.cpp.orig	2014-05-29 23:31:50.000000000 +0000
+--- xpcom/base/nsStackWalk.cpp.orig	2014-09-24 01:05:41.000000000 +0000
 +++ xpcom/base/nsStackWalk.cpp
-@@ -33,8 +33,8 @@ static CriticalAddress gCriticalAddress;
- #include <dlfcn.h>
- #endif
- 
--#define NSSTACKWALK_SUPPORTS_MACOSX \
--    (defined(XP_MACOSX) && \
-+#define NSSTACKWALK_SUPPORTS_DARWIN \
-+    (defined(XP_DARWIN) && \
-      (defined(__i386) || defined(__ppc__) || defined(HAVE__UNWIND_BACKTRACE)))
- 
- #define NSSTACKWALK_SUPPORTS_LINUX \
-@@ -42,11 +42,11 @@ static CriticalAddress gCriticalAddress;
-      ((defined(__GNUC__) && (defined(__i386) || defined(PPC))) || \
-       defined(HAVE__UNWIND_BACKTRACE)))
- 
--#define NSSTACKWALK_SUPPORTS_SOLARIS \
-+#define notNSSTACKWALK_SUPPORTS_SOLARIS \
-     (defined(__sun) && \
-      (defined(__sparc) || defined(sparc) || defined(__i386) || defined(i386)))
+@@ -47,7 +47,7 @@ static CriticalAddress gCriticalAddress;
+   (defined(__sun) && \
+    (defined(__sparc) || defined(sparc) || defined(__i386) || defined(i386)))
  
 -#if NSSTACKWALK_SUPPORTS_MACOSX
 +#if NSSTACKWALK_SUPPORTS_DARWIN
  #include <pthread.h>
  #include <CoreServices/CoreServices.h>
  
-@@ -838,7 +838,7 @@ NS_FormatCodeAddressDetails(void *aPC, c
+@@ -866,7 +866,7 @@ NS_FormatCodeAddressDetails(void* aPC, c
  
  // WIN32 x86 stack walking code
  // i386 or PPC Linux stackwalking code or Solaris
@@ -39,16 +23,16 @@ $NetBSD: patch-xpcom_base_nsStackWalk.cpp,v 1.7 2014/06/11 00:40:59 ryoon Exp $
  
  #include <stdlib.h>
  #include <string.h>
-@@ -1162,7 +1162,7 @@ FramePointerStackWalk(NS_WalkStackCallba
+@@ -1191,7 +1191,7 @@ FramePointerStackWalk(NS_WalkStackCallba
          (long(next) & 3)) {
        break;
      }
 -#if (defined(__ppc__) && defined(XP_MACOSX)) || defined(__powerpc64__)
 +#if (defined(__ppc__) && defined(XP_DARWIN)) || defined(__powerpc64__)
      // ppc mac or powerpc64 linux
-     void *pc = *(bp+2);
+     void* pc = *(bp + 2);
      bp += 3;
-@@ -1192,7 +1192,7 @@ FramePointerStackWalk(NS_WalkStackCallba
+@@ -1221,7 +1221,7 @@ FramePointerStackWalk(NS_WalkStackCallba
  }
  
  #define X86_OR_PPC (defined(__i386) || defined(PPC) || defined(__ppc__))
