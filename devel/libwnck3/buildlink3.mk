2c0a43853b7dd8cb757eec322a1a4a2abb337805
prlw1 <prlw1@pkgsrc.org>
2014-10-07 23:59:50 +0000

    Update libwnck3 to 3.4.9
    =============
    Version 3.4.9
    =============
    
      Fixes
    
      * TaskList: remove new Startup Notification API
    
    =============
    Version 3.4.8
    =============
    
      Features
    
      * Add support for _GTK_FRAME_EXTENTS (Rico Tzschichholz)
    
      Fixes
    
      * WindowActionMenu: unset window and stop async events on dispose
        (Marco Trevisan)
      * TaskList: use new Startup Notification API (Stefan Sperling)
    
      Translators
    
      * Fran Dieguez (gl)
      * GunChleoc (gd)
      * Milo Casagrande (it)
      * Noriko Mizumoto (ja)
      * Rudolfs Mazurs (lv)
      * Wylmer Wang (zh_CN)

--- devel/libwnck3/buildlink3.mk
+++ devel/libwnck3/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.8 2014/05/05 00:47:45 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2014/10/07 23:59:50 prlw1 Exp $
 
 BUILDLINK_TREE+=	libwnck3
 
 .if !defined(LIBWNCK3_BUILDLINK3_MK)
 LIBWNCK3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libwnck3+=	libwnck>=3.2.1
+BUILDLINK_API_DEPENDS.libwnck3+=	libwnck3>=3.2.1
 BUILDLINK_ABI_DEPENDS.libwnck3?=	libwnck3>=3.4.7nb2
 BUILDLINK_PKGSRCDIR.libwnck3?=	../../devel/libwnck3
 
