35338204471b7a158bc9162713fb0e817fa7e2a3
tron <tron@pkgsrc.org>
2014-10-07 07:27:18 +0000

    Pass "--remote-time" option to "curl" to get correct time stamps on downloaded files.

--- mk/fetch/fetch.mk
+++ mk/fetch/fetch.mk
@@ -1,4 +1,4 @@
-# $NetBSD: fetch.mk,v 1.63 2014/02/14 07:43:46 obache Exp $
+# $NetBSD: fetch.mk,v 1.64 2014/10/07 07:27:18 tron Exp $
 
 _MASTER_SITE_BACKUP=	${MASTER_SITE_BACKUP:=${DIST_SUBDIR}${DIST_SUBDIR:D/}}
 _MASTER_SITE_OVERRIDE=	${MASTER_SITE_OVERRIDE:=${DIST_SUBDIR}${DIST_SUBDIR:D/}}
@@ -222,7 +222,7 @@ _FETCH_OUTPUT_ARGS.wget=	-O
 _FETCH_CMD.wget=		${TOOLS_PATH.wget}
 
 _FETCH_BEFORE_ARGS.curl=	${PASSIVE_FETCH:D--ftp-pasv} \
-				--fail --insecure --location \
+				--fail --insecure --location --remote-time \
 				${FETCH_TIMEOUT:D--connect-timeout ${FETCH_TIMEOUT}} \
 				${FETCH_TIMEOUT:D--speed-time ${FETCH_TIMEOUT}} \
 				${FETCH_USE_IPV4_ONLY:D--ipv4}
