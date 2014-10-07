243c29c4cc258f12cf3a97cb44d597e40eeb3d0e
adam <adam@pkgsrc.org>
2014-10-07 16:47:10 +0000

    Revbump after updating libwebp and icu

--- lang/parrot/buildlink3.mk
+++ lang/parrot/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2014/04/09 07:26:56 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2014/10/07 16:47:12 adam Exp $
 
 BUILDLINK_TREE+=	parrot
 
@@ -8,7 +8,7 @@ PARROT_BUILDLINK3_MK:=
 .include "../../lang/parrot/version.mk"
 
 BUILDLINK_API_DEPENDS.parrot+=		parrot>=0.4.0
-BUILDLINK_ABI_DEPENDS.parrot+=		parrot>=5.7.0nb3
+BUILDLINK_ABI_DEPENDS.parrot+=		parrot>=5.7.0nb5
 BUILDLINK_PKGSRCDIR.parrot?=		../../lang/parrot
 
 .include "../../textproc/icu/buildlink3.mk"
--- net/libdmapsharing3/buildlink3.mk
+++ net/libdmapsharing3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2014/04/09 07:26:59 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2014/10/07 16:47:13 adam Exp $
 
 BUILDLINK_TREE+=	libdmapsharing3
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libdmapsharing3
 LIBDMAPSHARING3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libdmapsharing3+=	libdmapsharing3>=2.9.15
-BUILDLINK_ABI_DEPENDS.libdmapsharing3?=	libdmapsharing3>=2.9.15nb14
+BUILDLINK_ABI_DEPENDS.libdmapsharing3?=	libdmapsharing3>=2.9.15nb16
 BUILDLINK_PKGSRCDIR.libdmapsharing3?=	../../net/libdmapsharing3
 
 .include "../../net/libsoup24/buildlink3.mk"
