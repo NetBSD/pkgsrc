8412c8e3bfaff52f84a05f8c7366428e0b3f9be3
prlw1 <prlw1@pkgsrc.org>
2014-10-07 23:18:36 +0000

    Update glib to 2.42.0
    Patches removed:
    - patch-ba
        Fixed in 4f775b7b and 369c8bd5 (Bug 583330)
    - patch-gio_gcredentials.c
      patch-gio_gcredentialsprivate.h
      patch-gio_gsocket.c
      patch-gio_gunixcredentialsmessage.c
      patch-gio_tests_credentials.c
        Fixed in 3428bba1 and e763d934 (Bugs 728256 735819)
    - patch-glib_gmessages.c
        Fixed in ab9f63fa (Bug 720708)
    
    Patches changed:
    - patch-gio_gioenums.h
        Still need 45344f36 (Bug 728256)
    - patch-ak (=> patch-aa)
        Hunk 9 fixed in 74ec9470
    
    Highlights from the release notes:
    
    * GApplication now has binding-friendly API to handle
      commandline options: g_application_add_main_option
    
    * G_GNUC_BEGIN_IGNORE_DEPRECATIONS works with clang
    
    * g_clear_pointer and g_clear_object no longer use atomics
    
    * The Unicode support has been updated to version 7.0
      of the Unicode standard
    
    * GNotification now supports priorities for notifications
    
    * GCredentials has gained NetBSD support
    
    * GMutex now uses a faster, native implementation on Linux
    
    * Many bugfixes found by static analysis, including potential fd leaks
      and NULL pointer dereferences.
    
    * Increased use of (nullable) attribute on out values and return types
      now that it is supported (mostly from porting Vala metadata).
    
    * use XDG_CURRENT_DESKTOP for OnlyShowIn/NotShowIn handling of desktop
      files, deprecating g_desktop_app_info_set_desktop_env()
    
    * add support for g_desktop_app_info_get_implementations() to find
      desktop files that have an Implements= line for a given interface
    
    * GHmac has gained SHA-512 support
    
    * support the new mimeapps specification (most notably, moving the
      assoications/defaults configuration to ~/.config/mimeapps.list).
    
    * libgobject is now linked -Wl,-z,nodelete when possible to avoid errors
      when gobject is used from a module for a program that does not itself
      use gobject and that module is unloaded/reloaded
    
    * ... and many other bug fixes.
    
    * Many translations updated.
    
    For full details see:
    
    https://git.gnome.org/browse/glib/tree/NEWS?h=glib-2-42

--- devel/glib2/patches/patch-gio_gioenums.h
+++ devel/glib2/patches/patch-gio_gioenums.h
@@ -1,21 +1,26 @@
-$NetBSD: patch-gio_gioenums.h,v 1.2 2014/10/01 12:14:42 prlw1 Exp $
+$NetBSD: patch-gio_gioenums.h,v 1.3 2014/10/07 23:18:36 prlw1 Exp $
 
-gcredentials: add NetBSD support
+Revert unintentional change to enum values
 https://bugzilla.gnome.org/show_bug.cgi?id=728256
 
---- gio/gioenums.h.orig	2014-03-24 18:50:42.000000000 +0000
+--- gio/gioenums.h.orig	2014-09-22 13:42:12.000000000 +0000
 +++ gio/gioenums.h
-@@ -1354,6 +1354,7 @@ typedef enum
+@@ -1364,9 +1364,9 @@ typedef enum
+  * @G_CREDENTIALS_TYPE_INVALID: Indicates an invalid native credential type.
+  * @G_CREDENTIALS_TYPE_LINUX_UCRED: The native credentials type is a <type>struct ucred</type>.
   * @G_CREDENTIALS_TYPE_FREEBSD_CMSGCRED: The native credentials type is a <type>struct cmsgcred</type>.
+- * @G_CREDENTIALS_TYPE_NETBSD_UNPCBID: The native credentials type is a <type>struct unpcbid</type>.
   * @G_CREDENTIALS_TYPE_OPENBSD_SOCKPEERCRED: The native credentials type is a <type>struct sockpeercred</type>. Added in 2.30.
   * @G_CREDENTIALS_TYPE_SOLARIS_UCRED: The native credentials type is a <type>ucred_t</type>. Added in 2.40.
 + * @G_CREDENTIALS_TYPE_NETBSD_UNPCBID: The native credentials type is a <type>struct unpcbid</type>.
   *
   * Enumeration describing different kinds of native credential types.
   *
-@@ -1365,7 +1366,8 @@ typedef enum
+@@ -1377,9 +1377,9 @@ typedef enum
+   G_CREDENTIALS_TYPE_INVALID,
    G_CREDENTIALS_TYPE_LINUX_UCRED,
    G_CREDENTIALS_TYPE_FREEBSD_CMSGCRED,
+-  G_CREDENTIALS_TYPE_NETBSD_UNPCBID,
    G_CREDENTIALS_TYPE_OPENBSD_SOCKPEERCRED,
 -  G_CREDENTIALS_TYPE_SOLARIS_UCRED
 +  G_CREDENTIALS_TYPE_SOLARIS_UCRED,
