b16da9cb662edca493f722159e4e9ddcf3cc741a
szptvlfn <szptvlfn@pkgsrc.org>
2014-10-08 21:56:17 +0000

    Update to 0.10.10
    CHANGES:
    0.10.10
        #14, fix @ file arguments
    	deprecated tyConString

--- devel/hs-cmdargs/buildlink3.mk
+++ devel/hs-cmdargs/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/08/29 14:08:38 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/10/08 21:56:17 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cmdargs
 
 .if !defined(HS_CMDARGS_BUILDLINK3_MK)
 HS_CMDARGS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.9
-BUILDLINK_ABI_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.9
+BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.10
+BUILDLINK_ABI_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.10
 BUILDLINK_PKGSRCDIR.hs-cmdargs?=	../../devel/hs-cmdargs
 
 .include "../../devel/hs-transformers/buildlink3.mk"
