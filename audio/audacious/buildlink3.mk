54dfb867e9f761be51b2f6ec6a19e31831b98d79
adam <adam@pkgsrc.org>
2014-10-09 15:02:42 +0000

    Changes 3.5.1: This release contains updated translations and fixes for a few bugs.

--- audio/audacious/buildlink3.mk
+++ audio/audacious/buildlink3.mk
@@ -1,25 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.28 2014/05/05 00:47:35 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.29 2014/10/09 15:02:42 adam Exp $
 
 BUILDLINK_TREE+=	audacious
 
 .if !defined(AUDACIOUS_BUILDLINK3_MK)
 AUDACIOUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.audacious+=	audacious>=1.5.0
-BUILDLINK_ABI_DEPENDS.audacious+=	audacious>=1.5.1nb23
+BUILDLINK_API_DEPENDS.audacious+=	audacious>=3.5.1
+BUILDLINK_ABI_DEPENDS.audacious+=	audacious>=3.5.1
 BUILDLINK_PKGSRCDIR.audacious?=	../../audio/audacious
 
 .include "../../devel/atk/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/libmcs/buildlink3.mk"
-.include "../../devel/libmowgli/buildlink3.mk"
 .include "../../devel/pango/buildlink3.mk"
 .include "../../sysutils/dbus/buildlink3.mk"
 .include "../../sysutils/dbus-glib/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
-.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
 .endif # AUDACIOUS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-audacious
