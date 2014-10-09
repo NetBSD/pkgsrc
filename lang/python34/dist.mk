bd6c831f90a64b7cce2ead92cc8d3cea9da309eb
adam <adam@pkgsrc.org>
2014-10-09 09:15:38 +0000

    Python 3.4.2 has many bugfixes and other small improvements over 3.4.1. One new feature for Mac OS X users: the OS X installers are now distributed as signed installer package files compatible with the OS X Gatekeeper security feature.

--- lang/python34/dist.mk
+++ lang/python34/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.2 2014/05/20 08:59:31 wen Exp $
+# $NetBSD: dist.mk,v 1.3 2014/10/09 09:15:38 adam Exp $
 
-PY_DISTVERSION=	3.4.1
+PY_DISTVERSION=	3.4.2
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python34/distinfo
