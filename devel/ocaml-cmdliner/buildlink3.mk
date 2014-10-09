b0004458fdc75729e9f08231a885e8e7f8910471
jaapb <jaapb@pkgsrc.org>
2014-10-09 20:07:36 +0000

    New package: devel/ocaml-cmdliner. It's a module for the declarative definition of command line interfaces, and needed as a dependency for js_of_ocaml.

---
+++ devel/ocaml-cmdliner/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/10/09 20:07:36 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-cmdliner
+
+.if !defined(OCAML_CMDLINER_BUILDLINK3_MK)
+OCAML_CMDLINER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-cmdliner+=	ocaml-cmdliner>=0.9.4
+BUILDLINK_PKGSRCDIR.ocaml-cmdliner?=	../../devel/ocaml-cmdliner
+
+.include "../../devel/ocaml-findlib/buildlink3.mk"
+.include "../../lang/ocaml/buildlink3.mk"
+.endif	# OCAML_CMDLINER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-cmdliner
