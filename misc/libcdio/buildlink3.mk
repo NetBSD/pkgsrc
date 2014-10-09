3f68e38a2b4302d952d9e43c97d33b1b24b112a8
adam <adam@pkgsrc.org>
2014-10-09 12:35:05 +0000

    Switched to .tar.bz2; change API in buidlink3.mk to match cd-paranoia split off

--- misc/libcdio/buildlink3.mk
+++ misc/libcdio/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2012/09/23 05:36:23 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2014/10/09 12:35:05 adam Exp $
 
 BUILDLINK_TREE+=	libcdio
 
 .if !defined(LIBCDIO_BUILDLINK3_MK)
 LIBCDIO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libcdio+=	libcdio>=0.65
-BUILDLINK_ABI_DEPENDS.libcdio+=	libcdio>=0.83
+BUILDLINK_API_DEPENDS.libcdio+=	libcdio>=0.93
+BUILDLINK_ABI_DEPENDS.libcdio+=	libcdio>=0.93
 BUILDLINK_PKGSRCDIR.libcdio?=	../../misc/libcdio
 .endif # LIBCDIO_BUILDLINK3_MK
 
