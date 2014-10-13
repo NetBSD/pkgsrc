513c2bc2496b8c707c59294a2d7e5d442bd00b27
wiz <wiz@pkgsrc.org>
2014-10-13 09:36:29 +0000

    Add comment to patch.

--- multimedia/mediainfo/patches/patch-MediaInfoLib_Source_MediaInfo_Text_File__Ttml.h
+++ multimedia/mediainfo/patches/patch-MediaInfoLib_Source_MediaInfo_Text_File__Ttml.h
@@ -1,4 +1,6 @@
-$NetBSD: patch-MediaInfoLib_Source_MediaInfo_Text_File__Ttml.h,v 1.1 2014/01/06 22:14:29 joerg Exp $
+$NetBSD: patch-MediaInfoLib_Source_MediaInfo_Text_File__Ttml.h,v 1.2 2014/10/13 09:36:29 wiz Exp $
+
+Forward declarations may not be namespace qualified.
 
 --- MediaInfoLib/Source/MediaInfo/Text/File_Ttml.h.orig	2014-01-06 09:53:28.000000000 +0000
 +++ MediaInfoLib/Source/MediaInfo/Text/File_Ttml.h
