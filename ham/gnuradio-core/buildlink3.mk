01cd9505b9d578ad9ec9517c5d4e2313841bcbca
mef <mef@pkgsrc.org>
2014-10-13 12:46:33 +0000

    (pkgsrc) Update GNU Radio 3.3.0 (2010-09-15) to 3.7.5   See the thread for modularity issues etc:   http://mail-index.netbsd.org/pkgsrc-users/2014/09/07/msg020346.html
    (upstream)
    Release of 3.7.5  2014-09-05  http://gnuradio.org/redmine/news/43
      Changelog:
      http://gnuradio.org/redmine/projects/gnuradio/wiki/ChangeLogV3_7_5
    Release of 3.7.4  2014-07-15  http://gnuradio.org/redmine/news/38
    Release of 3.7.3  2014-03-16  http://gnuradio.org/redmine/news/35
    
    Release of 3.7.2  2013-11-10  http://gnuradio.org/redmine/news/30
    Release of 3.7.1  2013-08-29  http://gnuradio.org/redmine/news/26
    Release of 3.7.0  2013-07-03  http://gnuradio.org/redmine/news/25
    
    Release of 3.6.0  2012-04-21  http://gnuradio.org/redmine/news/13
    Release of 3.5.0  2011-12-08  http://gnuradio.org/redmine/news/6
    Release of 3.4.0  2011-10-09  http://gnuradio.org/redmine/news/2
    
    Release of 3.3.0  2010-06-03  ftp://ftp.gnu.org/gnu/gnuradio/
    Release of 3.3.0-rc0
                      2010-05-13  http://gnuradio.org/redmine/news/1

--- ham/gnuradio-core/buildlink3.mk
+++ ham/gnuradio-core/buildlink3.mk
@@ -1,24 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.26 2014/08/13 10:57:32 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2014/10/13 12:46:33 mef Exp $
 
 BUILDLINK_TREE+=	gnuradio-core
 
 .if !defined(GNURADIO_CORE_BUILDLINK3_MK)
 GNURADIO_CORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-core+=	gnuradio-core>=3.0.4
-BUILDLINK_ABI_DEPENDS.gnuradio-core+=	gnuradio-core>=3.3.0nb11
+BUILDLINK_API_DEPENDS.gnuradio-core+=	gnuradio-core>=3.7.5
+BUILDLINK_ABI_DEPENDS.gnuradio-core+=	gnuradio-core>=3.7.5
 BUILDLINK_PKGSRCDIR.gnuradio-core?=	../../ham/gnuradio-core
-BUILDLINK_CPPFLAGS.gnuradio-core+=    -I${BUILDLINK_PREFIX.gnuradio-core}/include/gnuradio
-
-.include "../../devel/boost-headers/buildlink3.mk"
-.include "../../devel/boost-libs/buildlink3.mk"
-.include "../../devel/cppunit/buildlink3.mk"
-.include "../../devel/swig/buildlink3.mk"
-.include "../../lang/python/application.mk"
-.include "../../lang/python/extension.mk"
-.include "../../lang/python/pyversion.mk"
-.include "../../math/fftwf/buildlink3.mk"
-.include "../../math/gsl/buildlink3.mk"
 .endif # GNURADIO_CORE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnuradio-core
