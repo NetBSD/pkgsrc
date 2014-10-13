32f8a1beb27a9828bb9e1a9292b64549116199ad
mef <mef@pkgsrc.org>
2014-10-13 12:51:08 +0000

    Update GNU Radio 3.3.0 (2010-09-15) to 3.7.5   See the thread for modularity issues etc:   http://mail-index.netbsd.org/pkgsrc-users/2014/09/07/msg020346.html See more info at recent  gnuradio-core/Makefile commit log, thanks.

---
+++ ham/gnuradio-dtv/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/10/13 12:51:09 mef Exp $
+
+BUILDLINK_TREE+=	gnuradio-dtv
+
+.if !defined(GNURADIO_DTV_BUILDLINK3_MK)
+GNURADIO_DTV_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gnuradio-dtv+=	gnuradio-dtv>=3.7.5
+BUILDLINK_ABI_DEPENDS.gnuradio-dtv+=	gnuradio-dtv>=3.7.5
+BUILDLINK_PKGSRCDIR.gnuradio-dtv?=	../../ham/gnuradio-dtv
+.endif # GNURADIO_DTV_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gnuradio-dtv
