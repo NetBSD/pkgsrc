6af2080e2971b9115b9b234a507e88e4e67146fe
jperkin <jperkin@pkgsrc.org>
2014-10-13 08:32:54 +0000

    Include devel/boost-libs' buildlink3.mk, it sets GCC_REQD and we need to be in sync with that, otherwise we may end up with two different libstdc++ runtimes.  Seen in Linux build.

--- textproc/libclucene/buildlink3.mk
+++ textproc/libclucene/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/08/11 16:06:25 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/10/13 08:32:54 jperkin Exp $
 
 BUILDLINK_TREE+=	libclucene
 
@@ -9,4 +9,7 @@ BUILDLINK_API_DEPENDS.libclucene+=	libclucene>=2.2.0
 BUILDLINK_PKGSRCDIR.libclucene?=	../../textproc/libclucene
 .endif # LIBCLUCENE_BUILDLINK3_MK
 
+# boost-libs sets GCC_REQD, so we need to ensure we are in sync otherwise
+# we may end up with the wrong libstdc++ runtime.
+.include "../../devel/boost-libs/buildlink3.mk"
 BUILDLINK_TREE+=	-libclucene
