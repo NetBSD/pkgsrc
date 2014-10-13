b6ad224c36c914378afaf3712da0229ca572a925
wiz <wiz@pkgsrc.org>
2014-10-13 09:14:29 +0000

    Update to 4.10.7:
    NSPR 4.10.7 has the following bug fixes:
    - Bug 836658: VC11+ defaults to SSE2 builds by default. Contributed by
      David Major.
    - Bug 979278: TSan: data race nsprpub/pr/src/threads/prtpd.c:103
      PR_NewThreadPrivateIndex.
    - Bug 1026129: Replace some manual declarations of MSVC intrinsics with
      #include <intrin.h>. Contributed by Ehsan Akhgari.
    - Bug 1026469: Use AC_CHECK_LIB instead of MOZ_CHECK_PTHREADS. Skip
      compiler checks when using MSVC, even when $CC is not literally "cl".
      Contributed by Mike Hommey.
    - Bug 1034415: NSPR hardcodes the C compiler to cl on Windows.
      Contributed by Ehsan Akhgari.
    - Bug 1042408: Compilation fix for Android > API level 19. Contributed
      by Zach Anderson.
    - Bug 1043082: NSPR's build system hardcodes -MD.

--- devel/nspr/buildlink3.mk
+++ devel/nspr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2012/08/12 15:19:07 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2014/10/13 09:14:29 wiz Exp $
 
 BUILDLINK_TREE+=	nspr
 
@@ -6,7 +6,6 @@ BUILDLINK_TREE+=	nspr
 NSPR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.9.3.2
-BUILDLINK_ABI_DEPENDS.nspr+=	nspr>=4.9.2beta1
 BUILDLINK_PKGSRCDIR.nspr?=	../../devel/nspr
 
 BUILDLINK_FILES.nspr+=          lib/nspr/*
