43d848607d145f4a2b313d683ac0ba191e09ff64
ryoon <ryoon@pkgsrc.org>
2014-10-13 21:43:20 +0000

    Update to 0.7.0
    * Change to 0.8.0 branch
    * Change license to mpl-2.0 from mit
    
    Changelog:
    Add some more formats.

--- converters/orcus/buildlink3.mk
+++ converters/orcus/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/08/24 16:12:10 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/10/13 21:43:20 ryoon Exp $
 
 BUILDLINK_TREE+=	orcus
 
 .if !defined(ORCUS_BUILDLINK3_MK)
 ORCUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.orcus+=	orcus>=0.5.1
+BUILDLINK_API_DEPENDS.orcus+=	orcus>=0.7.0
 BUILDLINK_PKGSRCDIR.orcus?=	../../converters/orcus
 
 #.include "../../devel/boost-libs/buildlink3.mk"
