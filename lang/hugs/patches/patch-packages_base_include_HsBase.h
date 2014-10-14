49585c5c0f73283bf7e6c849e755535fed75d692
joerg <joerg@pkgsrc.org>
2014-10-14 14:04:19 +0000

    Deal with systems providing execvpe.

--- lang/hugs/patches/patch-packages_base_include_HsBase.h
+++ lang/hugs/patches/patch-packages_base_include_HsBase.h
@@ -1,7 +1,16 @@
-$NetBSD: patch-packages_base_include_HsBase.h,v 1.1 2012/11/11 22:30:09 joerg Exp $
+$NetBSD: patch-packages_base_include_HsBase.h,v 1.2 2014/10/14 14:04:19 joerg Exp $
 
 --- packages/base/include/HsBase.h.orig	2006-09-20 22:01:52.000000000 +0000
 +++ packages/base/include/HsBase.h
+@@ -149,7 +149,7 @@ int inputReady(int fd, int msecs, int is
+ /* in Signals.c */
+ extern HsInt nocldstop;
+ 
+-#if !defined(_MSC_VER) && !defined(__MINGW32__) && !defined(_WIN32)
++#if !defined(_MSC_VER) && !defined(__MINGW32__) && !defined(_WIN32) && !HAVE_EXECVPE
+ /* in execvpe.c */
+ extern int execvpe(char *name, char *const argv[], char **envp);
+ extern void pPrPr_disableITimers (void);
 @@ -221,7 +221,11 @@ StgWord64 stg_integerToWord64 (StgInt sa
  # if defined(_MSC_VER)
  #  define INLINE extern __inline
