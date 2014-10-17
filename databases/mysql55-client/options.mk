0ee62b31799d92837b791ef15d2b9b63d3abf9a2
fhajny <fhajny>
2014-10-17 17:07:44 +0000

    Add optional Sphinx SE plugin support to MySQL 5.5 and 5.6. Intented as a clean implementation (replacement) of wip/mysql-sphinxse.
    SphinxSE is MySQL storage engine which can be compiled into MySQL server 5.x
    using its pluggable architecture. Despite the name, SphinxSE does not
    actually store any data itself. It is actually a built-in client which
    allows MySQL server to talk to searchd, run search queries, and obtain
    search results. All indexing and searching happen outside MySQL.
    
    See http://sphinxsearch.com/docs/current.html#sphinxse-overview

--- databases/mysql55-client/options.mk
+++ databases/mysql55-client/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.4 2014/03/25 13:56:38 jperkin Exp $
+# $NetBSD: options.mk,v 1.5 2014/10/17 17:07:44 fhajny Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mysql5
 
 # ndb-cluster does not configure with cmake
-PKG_SUPPORTED_OPTIONS+=	dtrace embedded-server ndb-cluster ssl
+PKG_SUPPORTED_OPTIONS+=	dtrace embedded-server ndb-cluster sphinx ssl
 PKG_SUGGESTED_OPTIONS+=	embedded-server ssl
 
 .include "../../mk/bsd.options.mk"
@@ -19,4 +19,19 @@ CMAKE_ARGS+=		-DWITH_SSL=no
 # Enable DTrace support
 .if !empty(PKG_OPTIONS:Mdtrace)
 CMAKE_ARGS+=		-DENABLE_DTRACE=ON
+.endif
+
+# Enable Sphinx SE support
+# http://sphinxsearch.com/docs/current.html#sphinxse-overview
+PLIST_VARS+=	sphinx
+.if !empty(PKG_OPTIONS:Msphinx)
+SPHINX_VER=	2.2.5
+DISTFILES=	${DEFAULT_DISTFILES} sphinx-${SPHINX_VER}-release${EXTRACT_SUFX}
+.if !empty(PKGPATH:Mserver)
+MESSAGE_SRC=	${PKGDIR}/MESSAGE ${PKGDIR}/MESSAGE.sphinx
+.endif
+PLIST.sphinx=	yes
+
+post-extract:
+	${CP} -R ${WRKDIR}/sphinx-${SPHINX_VER}-release/mysqlse ${WRKSRC}/storage/sphinx
 .endif
