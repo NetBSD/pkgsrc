253d53c533409fbcf3b4d0c1c3b69dc3d34880db
szptvlfn <szptvlfn@pkgsrc.org>
2014-10-17 21:02:36 +0000

    Update to 1.18.6
    CHANGELOG:
    * (1.18.6): bugfix to reject a macro usage with different arity than
                its definition

--- devel/hs-cpphs/buildlink3.mk
+++ devel/hs-cpphs/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/09/13 22:23:28 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/10/17 21:02:36 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cpphs
 
 .if !defined(HS_CPPHS_BUILDLINK3_MK)
 HS_CPPHS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.5
-BUILDLINK_ABI_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.5nb2
+BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.6
+BUILDLINK_ABI_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.6
 BUILDLINK_PKGSRCDIR.hs-cpphs?=	../../devel/hs-cpphs
 
 .include "../../textproc/hs-polyparse/buildlink3.mk"
