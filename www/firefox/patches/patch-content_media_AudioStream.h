ccccef2a74cd34f4cb176ccf565ddf81fa287f93
joerg <joerg@pkgsrc.org>
2014-10-17 16:49:09 +0000

    Do not provide conflicing forward defines, take three.

--- www/firefox/patches/patch-content_media_AudioStream.h
+++ www/firefox/patches/patch-content_media_AudioStream.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-content_media_AudioStream.h,v 1.8 2014/10/15 13:07:07 ryoon Exp $
+$NetBSD: patch-content_media_AudioStream.h,v 1.9 2014/10/17 16:49:09 joerg Exp $
 
 --- content/media/AudioStream.h.orig	2014-10-11 09:06:20.000000000 +0000
 +++ content/media/AudioStream.h
@@ -10,12 +10,11 @@ $NetBSD: patch-content_media_AudioStream.h,v 1.8 2014/10/15 13:07:07 ryoon Exp $
  
  template <>
  class nsAutoRefTraits<cubeb_stream> : public nsPointerRefTraits<cubeb_stream>
-@@ -26,7 +27,7 @@ public:
+@@ -26,7 +27,6 @@ public:
  };
  
  namespace soundtouch {
 -class SoundTouch;
-+class MOZ_IMPORT_API SoundTouch;
  }
  
  namespace mozilla {
