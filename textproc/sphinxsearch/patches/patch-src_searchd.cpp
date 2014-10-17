b9b9b2215d9733475492695de30be24088a522cd
fhajny <fhajny@pkgsrc.org>
2014-10-17 17:28:15 +0000

    Update sphinxsearch to 2.2.5.
    Changes:
    - added OPTION rand_seed which affects ORDER BY RAND()
    - fixed #2042, indextool fails with field mask on 32+ fields
    - fixed #2031, wrong encoding with UnixODBC/Oracle source
    - fixed #2056, several bugs in RLP tokenizer
    - fixed #2054, SHOW THREADS hangs if queries in prefork mode
    - fixed #2057, WARNING at indexer on duplicated wordforms
    - fixed #2066, snippet generation with weight_order enabled
    - fixed exception parsing in queries
    - fixed crash in config parser
    - fixed MySQL protocol response when daemon maxed out

--- textproc/sphinxsearch/patches/patch-src_searchd.cpp
+++ textproc/sphinxsearch/patches/patch-src_searchd.cpp
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_searchd.cpp,v 1.1 2014/03/05 13:44:23 fhajny Exp $
+$NetBSD: patch-src_searchd.cpp,v 1.2 2014/10/17 17:28:15 fhajny Exp $
 
 Avoid hard-coded paths.
 
---- src/searchd.cpp.orig	2014-02-19 19:13:38.000000000 +0000
+--- src/searchd.cpp.orig	2014-09-26 17:59:01.000000000 +0000
 +++ src/searchd.cpp
-@@ -19128,7 +19128,7 @@ void ShowHelp ()
+@@ -21264,7 +21264,7 @@ void ShowHelp ()
  		"--safetrace\t\tonly use system backtrace() call in crash reports\n"
  		"\n"
  		"Examples:\n"
 -		"searchd --config /usr/local/sphinx/etc/sphinx.conf\n"
-+		"searchd --config @PKG_SYSCONFDIR@/sphinx.conf\n"
++		"searchd --config /opt/local/etc/sphinx.conf\n"
  #if USE_WINDOWS
  		"searchd --install --config c:\\sphinx\\sphinx.conf\n"
  #endif
