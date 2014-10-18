34b973b419c551cbb4e58940247e2d788a860acd
szptvlfn <szptvlfn@pkgsrc.org>
2014-10-18 21:28:58 +0000

    Bump PKGREVISION for hs-vector-0.10.12.1

--- textproc/hs-pandoc-types/buildlink3.mk
+++ textproc/hs-pandoc-types/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/09/13 22:23:28 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/10/18 21:28:59 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-pandoc-types
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-pandoc-types
 HS_PANDOC_TYPES_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.4
-BUILDLINK_ABI_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.4.1nb1
+BUILDLINK_ABI_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.4.1nb2
 BUILDLINK_PKGSRCDIR.hs-pandoc-types?=	../../textproc/hs-pandoc-types
 
 .include "../../converters/hs-aeson/buildlink3.mk"
