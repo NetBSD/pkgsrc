8304470c1ed6338b7173bc6d93ae04fa3a24b7e5
szptvlfn <szptvlfn@pkgsrc.org>
2014-10-18 21:27:53 +0000

    Update to 0.10.12.1
    changelog:
    Changes in version 0.10.12.1
    
     * Fixed compilation on non-head GHCs
    
    Changes in version 0.10.12.0
    
     * Export MVector constructor from Data.Vector.Primitive to match Vector's
       (which was already exported).
    
     * Fix building on GHC 7.9 by adding Applicative instances for Id and Box

--- devel/hs-vector/buildlink3.mk
+++ devel/hs-vector/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/08/29 14:08:40 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/10/18 21:27:53 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-vector
 
 .if !defined(HS_VECTOR_BUILDLINK3_MK)
 HS_VECTOR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.10.11
-BUILDLINK_ABI_DEPENDS.hs-vector+=	hs-vector>=0.10.11.0
+BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.10.12
+BUILDLINK_ABI_DEPENDS.hs-vector+=	hs-vector>=0.10.12.1
 BUILDLINK_PKGSRCDIR.hs-vector?=	../../devel/hs-vector
 
 .include "../../devel/hs-primitive/buildlink3.mk"
