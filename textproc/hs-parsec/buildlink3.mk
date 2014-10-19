ba28b08d6f9d303242e2c125269075645ddf15c0
szptvlfn <szptvlfn@pkgsrc.org>
2014-10-19 21:25:47 +0000

    Update to 3.1.7
    CHANGES:
    3.1.7
    
    - Fix a regression from 3.1.6 related to the reported position of error messages.
      See bug #9 for details.
    - Reset the current error position on success of 'lookAhead'.

--- textproc/hs-parsec/buildlink3.mk
+++ textproc/hs-parsec/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/09/13 22:06:52 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/10/19 21:25:47 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-parsec
 
 .if !defined(HS_PARSEC_BUILDLINK3_MK)
 HS_PARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-parsec+=	hs-parsec>=3.1.6
-BUILDLINK_ABI_DEPENDS.hs-parsec+=	hs-parsec>=3.1.6
+BUILDLINK_API_DEPENDS.hs-parsec+=	hs-parsec>=3.1.7
+BUILDLINK_ABI_DEPENDS.hs-parsec+=	hs-parsec>=3.1.7
 BUILDLINK_PKGSRCDIR.hs-parsec?=	../../textproc/hs-parsec
 
 .include "../../devel/hs-mtl/buildlink3.mk"
