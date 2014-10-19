cedc0dff67ac72c541be7d00adad673308f082d8
szptvlfn <szptvlfn@pkgsrc.org>
2014-10-19 21:27:08 +0000

    Bump PKGREVISION for hs-parsec-3.1.7

--- www/hs-HTTP/buildlink3.mk
+++ www/hs-HTTP/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/09/13 22:23:29 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/10/19 21:27:08 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-HTTP
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-HTTP
 HS_HTTP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.17
-BUILDLINK_ABI_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.17nb2
+BUILDLINK_ABI_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.17nb3
 BUILDLINK_PKGSRCDIR.hs-HTTP?=	../../www/hs-HTTP
 
 .include "../../devel/hs-mtl/buildlink3.mk"
