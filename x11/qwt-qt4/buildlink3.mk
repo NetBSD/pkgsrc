a7fe415d933db39a5849036c5cacc7e7c0505b10
joerg <joerg@pkgsrc.org>
2014-10-20 20:08:09 +0000

    Make sure that qwt-qt4 users don't pick up qwt6-qt4.

--- x11/qwt-qt4/buildlink3.mk
+++ x11/qwt-qt4/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.24 2013/06/06 12:53:43 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2014/10/20 20:08:09 joerg Exp $
 
 BUILDLINK_TREE+=	qwt
 
 .if !defined(QWT_BUILDLINK3_MK)
 QWT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qwt+=	qwt>=5.2.0
-BUILDLINK_ABI_DEPENDS.qwt+=	qwt>=5.2.2nb14
+BUILDLINK_API_DEPENDS.qwt+=	qwt>=5.2.0<6
+BUILDLINK_ABI_DEPENDS.qwt+=	qwt>=5.2.2nb14<6
 BUILDLINK_PKGSRCDIR.qwt?=	../../x11/qwt-qt4
 
 .include "../../x11/qt4-libs/buildlink3.mk"
