10c8711e826e0e876f8dc0127e6172aaeceb14a7
szptvlfn <szptvlfn@pkgsrc.org>
2014-10-21 21:28:58 +0000

    Bump PKGREVISION for hs-random-1.1

--- devel/hs-tf-random/buildlink3.mk
+++ devel/hs-tf-random/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:40 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/10/21 21:28:58 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-tf-random
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-tf-random
 HS_TF_RANDOM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-tf-random+=	hs-tf-random>=0.5
-BUILDLINK_ABI_DEPENDS.hs-tf-random+=	hs-tf-random>=0.5nb1
+BUILDLINK_ABI_DEPENDS.hs-tf-random+=	hs-tf-random>=0.5nb2
 BUILDLINK_PKGSRCDIR.hs-tf-random?=	../../devel/hs-tf-random
 
 .include "../../devel/hs-primitive/buildlink3.mk"
