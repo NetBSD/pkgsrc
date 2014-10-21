8d09f95403edbcaf42bdf7cd493e6f973ad36315
szptvlfn <szptvlfn@pkgsrc.org>
2014-10-21 21:27:51 +0000

    Update to 1.1
    from CHANGELOG & https://github.com/haskell/random
    # 1.1
      * support for base < 4.6 (which doesnt provide strict atomicModifyIORef')
        and integrating Travis CI support.
        https://github.com/haskell/random/pull/12
      * fix C type in test suite https://github.com/haskell/random/pull/9
      - Fix compilation with GHC HEAD.

--- devel/hs-random/buildlink3.mk
+++ devel/hs-random/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:39 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/10/21 21:27:51 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-random
 
 .if !defined(HS_RANDOM_BUILDLINK3_MK)
 HS_RANDOM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-random+=	hs-random>=1.0.1
-BUILDLINK_ABI_DEPENDS.hs-random+=	hs-random>=1.0.1.3
+BUILDLINK_API_DEPENDS.hs-random+=	hs-random>=1.1
+BUILDLINK_ABI_DEPENDS.hs-random+=	hs-random>=1.1
 BUILDLINK_PKGSRCDIR.hs-random?=	../../devel/hs-random
 .endif	# HS_RANDOM_BUILDLINK3_MK
 
