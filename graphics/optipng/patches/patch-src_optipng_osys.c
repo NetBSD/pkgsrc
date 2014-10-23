ae800d0ef6d83cd473909cd91c129878848c79b5
adam <adam@pkgsrc.org>
2014-10-23 12:24:41 +0000

    Fix building on OS X 10.10

--- graphics/optipng/patches/patch-src_optipng_osys.c
+++ graphics/optipng/patches/patch-src_optipng_osys.c
@@ -1,12 +1,19 @@
-$NetBSD: patch-src_optipng_osys.c,v 1.2 2014/03/31 07:15:19 adam Exp $
+$NetBSD: patch-src_optipng_osys.c,v 1.3 2014/10/23 12:24:41 adam Exp $
+
+Compatibility fix.
 
 --- src/optipng/osys.c.orig	2014-02-23 16:37:00.000000000 +0000
 +++ src/optipng/osys.c
-@@ -522,8 +522,13 @@ osys_copy_attr(const char *src_path, con
+@@ -518,12 +518,17 @@ osys_copy_attr(const char *src_path, con
+     if (chmod(dest_path, sbuf.st_mode) != 0)
+         result = -1;
+ 
+-#ifdef AT_FDCWD
++#if defined(AT_FDCWD) && !defined(__APPLE__)
      {
          struct timespec times[2];
  
-+#ifdef __NetBSD__
++#if defined(__NetBSD__)
 +        times[0] = sbuf.st_atimespec;
 +        times[1] = sbuf.st_mtimespec;
 +#else
