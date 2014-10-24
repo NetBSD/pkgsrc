83a7b2968b18c36f75076699ee4c85c8639e8916
khorben <khorben>
2014-10-24 18:21:06 +0000

    Packaged DeforaOS Panel 0.3.1, with: * dropped dependency on x11/deforaos-keyboard moved to DeforaOS Integration) * new and updated applets for network interfaces, username, wpa_supplicant... * additional wireless browser * improved compatibility with Gtk+ 3 * support for lateral panels...

--- x11/deforaos-panel/patches/patch-src_applets_main.c
+++ x11/deforaos-panel/patches/patch-src_applets_main.c
@@ -1,16 +1,16 @@
-$NetBSD: patch-src_applets_main.c,v 1.1 2014/03/21 14:38:01 jperkin Exp $
+$NetBSD: patch-src_applets_main.c,v 1.2 2014/10/24 18:21:06 khorben Exp $
 
-Need sys/fcntl.h for O_RDONLY on SunOS.
+Need fcntl.h for open() on SunOS.
 
---- src/applets/main.c.orig	2013-06-01 23:34:31.000000000 +0000
+--- src/applets/main.c.orig	2014-10-23 17:34:42.000000000 +0000
 +++ src/applets/main.c
-@@ -23,6 +23,9 @@
- #include <errno.h>
- #include <libintl.h>
- #include <System.h>
-+#ifdef __sun
-+#include <sys/fcntl.h>
+@@ -16,6 +16,9 @@
+ 
+ 
+ #include <sys/stat.h>
++#if defined(__sun__)
++# include <fcntl.h>
 +#endif
- #include "Panel.h"
- #include "../../config.h"
- #define _(string) gettext(string)
+ #include <dirent.h>
+ #include <unistd.h>
+ #include <stdlib.h>
