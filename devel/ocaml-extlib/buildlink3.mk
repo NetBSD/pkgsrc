fde6d73e85fe672dbc39b5e197f4f8de0491258d
jaapb <jaapb@pkgsrc.org>
2014-10-26 10:54:36 +0000

    Added devel/ocaml-extlib, an alternative standard library for OCaml. Will come in handy as a dependency for a few things.

---
+++ devel/ocaml-extlib/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/10/26 10:54:36 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-extlib
+
+.if !defined(OCAML_EXTLIB_BUILDLINK3_MK)
+OCAML_EXTLIB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-extlib+=	ocaml-extlib>=1.6.1
+BUILDLINK_PKGSRCDIR.ocaml-extlib?=	../../devel/ocaml-extlib
+
+.endif	# OCAML_EXTLIB_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-extlib
