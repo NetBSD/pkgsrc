eb0c1628a0c4b690a532534c9fba6d40e5050ded
jnemeth <jnemeth>
2014-10-27 08:57:06 +0000

    libgee was renamed to libgee0.6

--- chat/libfolks/buildlink3.mk
+++ chat/libfolks/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2012/09/15 10:03:51 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2014/10/27 08:57:06 jnemeth Exp $
 
 BUILDLINK_TREE+=	libfolks
 
@@ -12,7 +12,7 @@ BUILDLINK_PKGSRCDIR.libfolks?=	../../chat/libfolks
 .include "../../devel/glib2/buildlink3.mk"
 #.include "../../devel/gobject-introspection/buildlink3.mk"
 #.include "../../sysutils/dbus-glib/buildlink3.mk"
-.include "../../devel/libgee/buildlink3.mk"
+.include "../../devel/libgee0.6/buildlink3.mk"
 .include "../../chat/telepathy-glib/buildlink3.mk"
 .endif	# LIBFOLKS_BUILDLINK3_MK
 
