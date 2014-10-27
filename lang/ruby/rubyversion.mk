6bc69e05e1baf1c2f62f5f80c57f83714e981291
taca <taca@pkgsrc.org>
2014-10-27 16:29:57 +0000

    Update ruby21/ruby21-base package to 2.1.4.
    Ruby 2.1.4 is released
    
    Ruby 2.1.4 has been released.
    
    This release includes security fixes for the following vulnerabilities:
    
        * CVE-2014-8080: Denial Of Service XML Expansion
        * Changed default settings of ext/openssl related to CVE-2014-3566
    
    And there are some bug-fixes.
    
    See tickets and ChangeLog for details.
    
    
    Ruby 2.1.3 Released
    
    We are pleased to announce the release of Ruby 2.1.3. This is a patchlevel
    release of the stable 2.1 series.
    
    This release contains a change of full GC timing to reduce memory consumption
    (see Bug #9607), and many bugfixes.
    
    See tickets and ChangeLog for details.

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.126 2014/10/27 16:20:16 taca Exp $
+# $NetBSD: rubyversion.mk,v 1.127 2014/10/27 16:29:57 taca Exp $
 #
 
 # This file determines which Ruby version is used as a dependency for
@@ -223,13 +223,13 @@ RUBY_VERSION_REQD?= ${PKGNAME_REQD:C/ruby([0-9][0-9]+)-.*/\1/}
 RUBY18_VERSION=		1.8.7
 RUBY193_VERSION=	1.9.3
 RUBY200_VERSION=	2.0.0
-RUBY21_VERSION=		2.1.2
+RUBY21_VERSION=		2.1.4
 
 # patch
 RUBY18_PATCHLEVEL=	pl374
 RUBY193_PATCHLEVEL=	p550
 RUBY200_PATCHLEVEL=	p594
-#RUBY21_PATCHLEVEL=	p95
+#RUBY21_PATCHLEVEL=	p265
 
 # current API compatible version; used for version of shared library
 RUBY18_API_VERSION=	1.8.7
