c11375ba26c75a1c8c57fa18667d5a8a830a1b3b
fhajny <fhajny@pkgsrc.org>
2014-10-29 13:49:02 +0000

    Fix build on NetBSD. Fix mistake in patch file. Bump PKGREVISION.

--- textproc/sphinxsearch/patches/patch-src_sphinxstd.h
+++ textproc/sphinxsearch/patches/patch-src_sphinxstd.h
@@ -1,16 +1,17 @@
-$NetBSD: patch-src_sphinxstd.h,v 1.2 2014/10/17 17:28:15 fhajny Exp $
+$NetBSD: patch-src_sphinxstd.h,v 1.3 2014/10/29 13:49:02 fhajny Exp $
 
 Portability fixes.
 
 --- src/sphinxstd.h.orig	2014-09-26 17:21:01.000000000 +0000
 +++ src/sphinxstd.h
-@@ -75,6 +75,13 @@ typedef int __declspec("SAL_nokernel") _
+@@ -75,6 +75,14 @@ typedef int __declspec("SAL_nokernel") _
  #endif
  #endif
  
 +#ifdef __NetBSD__
 +#define pthread_mutexattr_setpshared(m,a)	(0)
 +#define pthread_condattr_setpshared(m,a)	(0)
++#define pthread_rwlockattr_setpshared(m,a)	(0)
 +#include "sys/signal.h"
 +#define PTHREAD_STACK_MIN MINSIGSTKSZ
 +#endif
