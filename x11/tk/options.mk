31f0b177af2ec14d8866e85faa571b442cffe5a9
adam <adam@pkgsrc.org>
2014-10-31 21:11:18 +0000

    Changes 8.6.2: This is a bug-fix release.

--- x11/tk/options.mk
+++ x11/tk/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2013/05/31 20:15:32 adam Exp $
+# $NetBSD: options.mk,v 1.4 2014/10/31 21:11:18 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tk
 PKG_SUPPORTED_OPTIONS=	threads debug xft2
@@ -6,11 +6,25 @@ PKG_SUGGESTED_OPTIONS=	threads
 
 .include "../../mk/bsd.prefs.mk"
 
+PLIST_VARS+=	aqua
+
 .if ${OPSYS} != "Darwin"
 PKG_SUGGESTED_OPTIONS+=	xft2
+.else
+PKG_SUPPORTED_OPTIONS+=	aqua
 .endif
 
 .include "../../mk/bsd.options.mk"
+
+# IMPORTANT: This option should be enabled on a system without X11.
+# Otherwise some X11 headers are installed and will break X11 compatibility.
+.if !empty(PKG_OPTIONS:Maqua)
+CONFIGURE_ARGS+=	--enable-aqua
+PLIST.aqua=		yes
+.else
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.endif
 
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-symbols
