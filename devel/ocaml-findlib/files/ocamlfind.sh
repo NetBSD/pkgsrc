fd2a44599a74ce32b512dd3946fc7495bb862c74
hiramatsu <hiramatsu@pkgsrc.org>
2014-10-31 01:00:30 +0000

    Improve wrapper script as suggested by Joerg Sonnenberger.

--- devel/ocaml-findlib/files/ocamlfind.sh
+++ devel/ocaml-findlib/files/ocamlfind.sh
@@ -1,28 +1,25 @@
 #!@SH@
 
-# $NetBSD: ocamlfind.sh,v 1.4 2014/10/27 03:44:39 hiramatsu Exp $
+# $NetBSD: ocamlfind.sh,v 1.5 2014/10/31 01:00:30 hiramatsu Exp $
 
-case "$1" in
-install)
-	args="$@"
-	install_args=
-	case "$args" in
-	*-destdir*) ;;
-	*)
-		install_args="${install_args} -destdir @DESTDIR@@OCAML_FINDLIB_PREFIX@/lib/ocaml/site-lib"
-		;;
-	esac
-	case "$args" in
-	*-ldconf*) ;;
-	*)
-		install_args="${install_args} -ldconf @BUILDLINK_DIR@/lib/ocaml/ld.conf"
-		;;
-	esac
-	args="install ${install_args} ${args#install}"
-	exec "@OCAML_FINDLIB_PREFIX@/bin/`basename $0`" ${args}
-	;;
-*)
-	exec "@OCAML_FINDLIB_PREFIX@/bin/`basename $0`" "$@"
-	;;
-esac
-
+if [ "$1" = install ]; then
+	need_destdir=yes
+	need_ldconf=yes
+	for arg in "$@"; do
+		case "$arg" in
+		-destdir)
+			need_destdir=no
+			;;
+		-ldconf)
+			need_ldconf=no
+			;;
+		esac
+	done
+	if [ "$need_destdir" = "yes" ]; then
+		set -- "$@" -destdir "@DESTDIR@@OCAML_FINDLIB_PREFIX@/lib/ocaml/site-lib"
+	fi
+	if [ "$need_ldconf" = "yes" ]; then
+		set -- "$@" -ldconf "@BUILDLINK_DIR@/lib/ocaml/ld.conf"
+	fi
+fi
+exec "@OCAML_FINDLIB_PREFIX@/bin/`basename $0`" "$@"
