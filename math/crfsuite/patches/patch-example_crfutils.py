1d68d5336f7a260e0b2cba80bf6116b58cc24903
cheusov <cheusov@pkgsrc.org>
2014-10-31 00:54:02 +0000

    Include example scripts, some of the are rather useful. ++pkgrevision.

---
+++ math/crfsuite/patches/patch-example_crfutils.py
@@ -0,0 +1,10 @@
+$NetBSD: patch-example_crfutils.py,v 1.1 2014/10/31 00:54:02 cheusov Exp $
+
+--- example/crfutils.py.orig	2011-08-11 03:02:41.000000000 +0000
++++ example/crfutils.py
+@@ -1,3 +1,5 @@
++#!/usr/bin/env python
++
+ """
+ A miscellaneous utility for sequential labeling.
+ Copyright 2010,2011 Naoaki Okazaki.
