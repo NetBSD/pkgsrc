3f7a8d483fcd84274ab33e309162abb5d01dac54
prlw1 <prlw1@pkgsrc.org>
2014-11-01 11:20:53 +0000

    Update libgee to 0.16.1
    It seems that after the ABI break in 0.7, which probably should have
    incurred a major version increment, libgee since 0.8 has been
    backwardly compatible.
    
    Highlights from the release notes since 0.8.8, devel/libgee0.8:
    
     * Workaround GC thread consuming 100% of CPU
     * Fix HashMap destructor name
     * Add explicit tee implementations
     * Add UnrolledLinkedList class implementing List and Deque interface
     * Clarify the add and insert operation on list iterators and make the behaviour consistent for ArrayList and LinkedList
     * Allow (partially) disable internal asserts
     * Internal symbols are hidden
     * Fix HazardPointer bugs
     * Fix List test
     * Remove workaround for 'old' (<0.23.2) vala
     * Use WeakRef for keeping of read-only proxy for collection
     * The collection cloning no longer leaks memory
     * Override read_only_view property in ReadOnlySortedMap and its subclasses
     * Add adding to collection based on iterators
     * Make Collection.add_all_array, contains_all_array and remove_all_array virtual
     * Avoid destroy coroutine creation in hazard pointer implementation
     * Add Traversable.tee
     * Add exception handling to Future
     * Add ability of getting Future from Lazy
     * Make Traversable.flat_map a virtual function
     * Add more specializations to .foreach
     * Add flat_map method to Traversable
     * Add zip method to Future
     * Specialize stream iterator (4-22% speed improvement in tests)
     * Extend the Traversable.Stream with Stream.WAIT
     * Fix lazy value usage in unfold
     * Add futures and promises to libgee
     * Return true from HashSet.Iterator.foreach() if we fall off the end
     * Fix memory leak in TreeSet and TreeMap
     * ConcurrentSet implements SortedSet
     * Fix HashMultiMap, TreeMultiSet constructor
     * Add add/contains/remove_all working on array
     * Allow creation of ArrayList from array
     * Add read_only_view to Gee.MultiSet and Gee.MultiMap
     * ConcurrentSet preliminary implementation
     * Allow slice in ReadOnlyList
     * Fix Iterator.remove in PriorityQueue
     * Add foreach specializations
     * Use foreach function instead of iterators
    
    For full details see:
    https://git.gnome.org/browse/libgee/tree/NEWS?h=0.16.1

--- devel/libgee/buildlink3.mk
+++ devel/libgee/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2012/12/08 23:22:41 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2014/11/01 11:20:53 prlw1 Exp $
 
 BUILDLINK_TREE+=	libgee
 
 .if !defined(LIBGEE_BUILDLINK3_MK)
 LIBGEE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgee+=	libgee>=0.5.3
-BUILDLINK_ABI_DEPENDS.libgee+=	libgee>=0.6.5nb1
-BUILDLINK_ABI_DEPENDS.libgee+=	libgee<0.8
+BUILDLINK_API_DEPENDS.libgee+=	libgee>=0.8.2
+BUILDLINK_ABI_DEPENDS.libgee+=	libgee>=0.16.1
 BUILDLINK_PKGSRCDIR.libgee?=	../../devel/libgee
 
 .include "../../devel/glib2/buildlink3.mk"
