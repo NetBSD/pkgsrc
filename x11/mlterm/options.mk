0ac153975bcc4c7693083a41c4239a131f75824f
tsutsui <tsutsui@pkgsrc.org>
2014-11-02 19:38:12 +0000

    Update mlterm to 3.4.0.
    pkgsrc changes:
    - add options for cairo and fribidi and enable them and xft2 by default
      per upstream default changes
    - also make gdk_pixbuf2 support optional (enabled by default)
    - regen a patch to adjust offset
    - take over maintainership to reflect reality
    
    Changes noted in doc/en/ReleaseNote:
    
    ver 3.4.0
    * Support DEC Locator mode (which is necessary for Xsixel
      (http://github.com/saitoha/xserver-SIXEL)).
    * Support CSI > 2 p (for SDL-sixel and Xsixel) which always hides mouse pointer
      even if it moves. (Not compatible with xterm though.)
    * Add --point/use_point_size option.
    * Support software keyboard on rotated display.
    * Support HLS in sixel sequence.
    * Change the default mouse pointer on a mlterm window from IDC_ARROW to IDC_IBEAM
      on win32.
    * Embed sixel processing to mlterm which is built without --with-imagelib option
      instead of using 'mlimgloader' to improve performance.
    * './configure;make install' (without 'make') is available.
    * Build with fribidi, xft and cairo by default.
    * Change the default value of "type_engine" option from "xcore" to "xft".
      (If you want to use xcore, add "type_engine = xcore" to ~/.mlterm/main.)
    * Bug fixes:
      Fix corrupt scrolling within left-right margin which contains full-width characters.
      Fix a bug which disabled to show inline pictures if logsize=unlimited.
      Fix a bug which calls select(2) with an invalid file descriptor after restarting ibus.
      Fix a libvte bug which disabled color configurations on roxterm or sakura.

--- x11/mlterm/options.mk
+++ x11/mlterm/options.mk
@@ -1,21 +1,40 @@
-# $NetBSD: options.mk,v 1.7 2013/05/26 18:18:25 tsutsui Exp $
+# $NetBSD: options.mk,v 1.8 2014/11/02 19:38:12 tsutsui Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mlterm
-PKG_SUPPORTED_OPTIONS=	ibus libind m17nlib mlterm-fb uim xft2
-PKG_SUGGESTED_OPTIONS=
+PKG_SUPPORTED_OPTIONS=	cairo fribidi gdk_pixbuf2 ibus libind m17nlib mlterm-fb uim xft2
+PKG_SUGGESTED_OPTIONS=	cairo fribidi gdk_pixbuf2 xft2
 .if ${OPSYS} == "NetBSD" || ${OPSYS} == "FreeBSD" || ${OPSYS} == "Linux"
 PKG_SUGGESTED_OPTIONS+=	mlterm-fb
 .endif
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		fb ibus ind m17nlib uim xft2
+PLIST_VARS+=		bidi cairo fb ibus ind m17nlib uim xft2
 
 .if !empty(PKG_OPTIONS:Mmlterm-fb)
 CONFIGURE_ARGS+=	--with-gui=xlib,fb
 PLIST.fb=		yes
 SPECIAL_PERMS+=		${PREFIX:Q}/bin/mlterm-fb ${SETUID_ROOT_PERMS}
 CONF_FILES+=		${EGDIR}/font-fb ${PKG_SYSCONFDIR}/font-fb
+.endif
+
+.if !empty(PKG_OPTIONS:Mcairo)
+.include "../../graphics/cairo/buildlink3.mk"
+PLIST.cairo=		yes
+.else
+.endif
+
+.if !empty(PKG_OPTIONS:Mfribidi)
+.include "../../converters/fribidi/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-fribidi
+PLIST.bidi=		yes
+.else
+CONFIGURE_ARGS+=	--disable-fribidi
+.endif
+
+.if !empty(PKG_OPTIONS:Mgdk_pixbuf2)
+CONFIGURE_ARGS+=	--with-imagelib=gdk-pixbuf
+.include "../../graphics/gdk-pixbuf2/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mibus)
@@ -44,6 +63,16 @@ LICENSE+=		AND gnu-lgpl-v2
 .endif
 
 .if !empty(PKG_OPTIONS:Mxft2)
-CONFIGURE_ARGS+=	--with-type-engines=xft
+.include "../../x11/libXft/buildlink3.mk"
 PLIST.xft2=		yes
+.endif
+
+.if !empty(PKG_OPTIONS:Mcairo) && !empty(PKG_OPTIONS:Mxft2)
+CONFIGURE_ARGS+=	--with-type-engines=xcore,xft,cairo
+.elif !empty(PKG_OPTIONS:Mcairo)
+CONFIGURE_ARGS+=	--with-type-engines=xcore,cairo
+.elif !empty(PKG_OPTIONS:Mxft2)
+CONFIGURE_ARGS+=	--with-type-engines=xcore,xft
+.else
+CONFIGURE_ARGS+=	--with-type-engines=xcore
 .endif
