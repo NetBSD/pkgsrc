bc710575c9788ccf9fdb0583ca734c1e28b78360
ryoon <ryoon@pkgsrc.org>
2014-11-03 12:25:36 +0000

    Update to 1.7.71
    * Update icedtea* to latest
    * UTF-8 build patch
    These are based on the patches from richard@.
    
    Changelog:
    From: http://www.oracle.com/technetwork/java/javase/7u71-relnotes-2296187.html
    
    The full version string for this update release is 1.7.0_71-b14 (where "b" means "build"). The version number is 7u71.
    IANA Data 2014c
    
    JDK 7u71 contains IANA time zone data version 2014c. For more information, refer to Timezone Data Versions in the JRE Software.

--- lang/openjdk7/icedtea-extras.mk
+++ lang/openjdk7/icedtea-extras.mk
@@ -1,20 +1,20 @@
-# $NetBSD: icedtea-extras.mk,v 1.13 2014/07/25 23:24:13 ryoon Exp $
+# $NetBSD: icedtea-extras.mk,v 1.14 2014/11/03 12:25:36 ryoon Exp $
 
 # Needed to extract icedtea
 EXTRACT_USING=			bsdtar
 
 DIST_SUBDIR=			openjdk7
 
-ICEDTEA=			icedtea-2.5.1
+ICEDTEA=			icedtea-2.5.3
 ICEDTEA_TGZ=			${ICEDTEA}.tar.xz
 SITES.${ICEDTEA_TGZ}=		http://icedtea.wildebeest.org/download/source/
 DISTFILES+=			${ICEDTEA_TGZ}
 EXTRACT_ONLY+=			${ICEDTEA_TGZ}
 
 # Keep icedtea-web distfile in sync with www/icedtea-web/Makefile
-ICEDTEA_WEB=			icedtea-web-81dfc0552a1c
-ICEDTEA_WEB_TGZ=		icedtea-web-81dfc0552a1c.tar.bz2
-SITES.${ICEDTEA_WEB_TGZ}=	-http://icedtea.classpath.org/hg/icedtea-web/archive/81dfc0552a1c.tar.bz2
+ICEDTEA_WEB=			icedtea-web-1.5.1
+ICEDTEA_WEB_TGZ=		icedtea-web-1.5.1.tar.gz
+SITES.${ICEDTEA_WEB_TGZ}=	http://icedtea.wildebeest.org/download/source/
 DISTFILES+=			${ICEDTEA_WEB_TGZ}
 EXTRACT_ONLY+=			${ICEDTEA_WEB_TGZ}
 
