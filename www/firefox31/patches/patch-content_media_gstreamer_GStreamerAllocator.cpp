45efe636b46d124bfbe155a1a7994c8c69ebfd03
ryoon <ryoon>
2014-11-03 12:18:31 +0000

    Import firefox31-31.2.0 as www/firefox31.
    Mozilla Firefox is a free, open-source and cross-platform web browser
    for Windows, Linux, MacOS X and many other operating systems.
    
    It is fast and easy to use, and offers many advantages over other web
    browsers, such as tabbed browsing and the ability to block pop-up
    windows.
    
    Firefox also offers excellent bookmark and history management, and it
    can be extended by developers using industry standards such as XML,
    CSS, JavaScript, C++, etc. Many extensions are available.

---
+++ www/firefox31/patches/patch-content_media_gstreamer_GStreamerAllocator.cpp
@@ -0,0 +1,12 @@
+$NetBSD: patch-content_media_gstreamer_GStreamerAllocator.cpp,v 1.1 2014/11/03 12:18:32 ryoon Exp $
+
+--- content/media/gstreamer/GStreamerAllocator.cpp.orig	2014-05-29 23:30:33.000000000 +0000
++++ content/media/gstreamer/GStreamerAllocator.cpp
+@@ -51,7 +51,6 @@ typedef struct
+ #pragma GCC diagnostic ignored "-Wunused-function"
+ G_DEFINE_TYPE(MozGfxMemoryAllocator, moz_gfx_memory_allocator, GST_TYPE_ALLOCATOR);
+ G_DEFINE_TYPE(MozGfxBufferPool, moz_gfx_buffer_pool, GST_TYPE_VIDEO_BUFFER_POOL);
+-#pragma GCC diagnostic pop
+ 
+ void
+ moz_gfx_memory_reset(MozGfxMemory *mem)
