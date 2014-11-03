7c2cb4608e0ca1d0b378ecc3158cf1b374e30e14
ryoon <ryoon@pkgsrc.org>
2014-11-03 12:18:31 +0000

    Import firefox31-31.2.0 as www/firefox31.
    Mozilla Firefox is a free, open-source and cross-platform web browser
    for Windows, Linux, MacOS X and many other operating systems.
    
    It is fast and easy to use, and offers many advantages over other web
    browsers, such as tabbed browsing and the ability to block pop-up
    windows.
    
    Firefox also offers excellent bookmark and history management, and it
    can be extended by developers using industry standards such as XML,
    CSS, JavaScript, C++, etc. Many extensions are available.

---
+++ www/firefox31/patches/patch-browser_app_nsBrowserApp.cpp
@@ -0,0 +1,36 @@
+$NetBSD: patch-browser_app_nsBrowserApp.cpp,v 1.1 2014/11/03 12:18:32 ryoon Exp $
+
+* Replace XP_MACOSX with XP_DARWIN as the former is not defined when
+  the toolkit is not cocoa.
+
+--- browser/app/nsBrowserApp.cpp.orig	2014-05-06 22:55:09.000000000 +0000
++++ browser/app/nsBrowserApp.cpp
+@@ -18,8 +18,10 @@
+ #include <unistd.h>
+ #endif
+ 
+-#ifdef XP_MACOSX
++#ifdef XP_DARWIN
+ #include <mach/mach_time.h>
++#endif
++#ifdef MOZ_WIDGET_COCOA
+ #include "MacQuirks.h"
+ #endif
+ 
+@@ -447,7 +449,7 @@ TimeStamp_Now()
+   }
+ 
+   return sGetTickCount64() * freq.QuadPart;
+-#elif defined(XP_MACOSX)
++#elif defined(XP_DARWIN)
+   return mach_absolute_time();
+ #elif defined(HAVE_CLOCK_MONOTONIC)
+   struct timespec ts;
+@@ -586,6 +588,7 @@ int main(int argc, char* argv[])
+   TriggerQuirks();
+ #endif
+ 
++  setenv("MOZ_PLUGIN_PATH", "%%LOCALBASE%%/lib/browser_plugins/symlinks/gecko", 0);
+   int gotCounters;
+ #if defined(XP_UNIX)
+   struct rusage initialRUsage;
---
+++ www/firefox31/patches/patch-ipc_glue_GeckoChildProcessHost.cpp
@@ -0,0 +1,48 @@
+$NetBSD: patch-ipc_glue_GeckoChildProcessHost.cpp,v 1.1 2014/11/03 12:18:32 ryoon Exp $
+
+* Just because OS_ARCH is Darwin does not mean MacOS X specific
+  kludges are needed.
+
+--- ipc/glue/GeckoChildProcessHost.cpp.orig	2014-05-29 23:30:53.000000000 +0000
++++ ipc/glue/GeckoChildProcessHost.cpp
+@@ -4,7 +4,13 @@
+  * License, v. 2.0. If a copy of the MPL was not distributed with this
+  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ 
++#if defined(__NetBSD__)
++_Pragma("GCC visibility push(default)")
++#endif
+ #include "GeckoChildProcessHost.h"
++#if defined(__NetBSD__)
++_Pragma("GCC visibility pop")
++#endif
+ 
+ #if defined(XP_WIN) && defined(MOZ_CONTENT_SANDBOX)
+ #include "sandboxBroker.h"
+@@ -548,7 +554,7 @@ GeckoChildProcessHost::PerformAsyncLaunc
+   // and passing wstrings from one config to the other is unsafe.  So
+   // we split the logic here.
+ 
+-#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_BSD)
++#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_BSD) || defined(OS_SOLARIS)
+   base::environment_map newEnvVars;
+   ChildPrivileges privs = mPrivileges;
+   if (privs == base::PRIVILEGES_DEFAULT) {
+@@ -671,7 +677,7 @@ GeckoChildProcessHost::PerformAsyncLaunc
+   childArgv.push_back(pidstring);
+ 
+ #if defined(MOZ_CRASHREPORTER)
+-#  if defined(OS_LINUX) || defined(OS_BSD)
++#  if defined(OS_LINUX) || defined(OS_BSD) || defined(OS_SOLARIS)
+   int childCrashFd, childCrashRemapFd;
+   if (!CrashReporter::CreateNotificationPipeForChild(
+         &childCrashFd, &childCrashRemapFd))
+@@ -704,7 +710,7 @@ GeckoChildProcessHost::PerformAsyncLaunc
+   childArgv.push_back(childProcessType);
+ 
+   base::LaunchApp(childArgv, mFileMap,
+-#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_BSD)
++#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_BSD) || defined(OS_SOLARIS)
+                   newEnvVars, privs,
+ #endif
+                   false, &process, arch);
