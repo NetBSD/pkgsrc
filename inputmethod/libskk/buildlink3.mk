7ceaa3781a91879f40c2a7358842b1dfbbd57815
prlw1 <prlw1@pkgsrc.org>
2014-11-03 18:45:42 +0000

    Remove libgee0.8.
    libgee now contains the most recent version of libgee, which follows
    the 0.8 API. If in the future, that API changes, we can copy libgee
    back to libgee0.8, and carry on updating libgee.

--- inputmethod/libskk/buildlink3.mk
+++ inputmethod/libskk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/09/10 09:45:31 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/11/03 18:45:43 prlw1 Exp $
 #
 
 BUILDLINK_TREE+=	libskk
@@ -10,7 +10,7 @@ BUILDLINK_API_DEPENDS.libskk+=	libskk>=1.0.2
 BUILDLINK_PKGSRCDIR.libskk?=	../../inputmethod/libskk
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/libgee0.8/buildlink3.mk"
+.include "../../devel/libgee/buildlink3.mk"
 .include "../../textproc/json-glib/buildlink3.mk"
 .endif	# LIBSKK_BUILDLINK3_MK
 
