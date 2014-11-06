25c274cd4002fcd875e76c0ffd2ffc2a963e194b
szptvlfn <szptvlfn@pkgsrc.org>
2014-11-06 13:00:44 +0000

    Update to 0.6.2.1
    CHANGELOG:
    Version 0.6.2.1
    ---------------
    
    Restore compatibility with GHC 7.4 and older
    
    Version 0.6.2
    -------------
    
    * Add `hSupportsANSI`
    * Drop support for `base < 4`

--- devel/hs-ansi-terminal/buildlink3.mk
+++ devel/hs-ansi-terminal/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:38 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/11/06 13:00:44 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-ansi-terminal
 
 .if !defined(HS_ANSI_TERMINAL_BUILDLINK3_MK)
 HS_ANSI_TERMINAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.6.1
-BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.6.1.1
+BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.6.2
+BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.6.2.1
 BUILDLINK_PKGSRCDIR.hs-ansi-terminal?=	../../devel/hs-ansi-terminal
 .endif	# HS_ANSI_TERMINAL_BUILDLINK3_MK
 
