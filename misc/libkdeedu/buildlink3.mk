8e3b70e3140e0358b2761118292dbdf33e297c57
adam <adam@pkgsrc.org>
2014-11-07 19:39:24 +0000

    Revbump after updating boost

--- misc/libkdeedu/buildlink3.mk
+++ misc/libkdeedu/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2014/08/22 11:24:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2014/11/07 19:39:25 adam Exp $
 
 BUILDLINK_TREE+=	libkdeedu
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libkdeedu
 LIBKDEEDU_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libkdeedu+=	libkdeedu>=4.8.0
-BUILDLINK_ABI_DEPENDS.libkdeedu?=	libkdeedu>=4.11.5nb4
+BUILDLINK_ABI_DEPENDS.libkdeedu?=	libkdeedu>=4.11.5nb5
 BUILDLINK_PKGSRCDIR.libkdeedu?=	../../misc/libkdeedu
 
 .include "../../x11/kdelibs4/buildlink3.mk"
--- misc/marble/buildlink3.mk
+++ misc/marble/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2014/08/22 11:24:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2014/11/07 19:39:25 adam Exp $
 
 BUILDLINK_TREE+=	marble
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	marble
 MARBLE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.marble+=	marble>=4.8.0
-BUILDLINK_ABI_DEPENDS.marble?=	marble>=4.11.5nb4
+BUILDLINK_ABI_DEPENDS.marble?=	marble>=4.11.5nb5
 BUILDLINK_PKGSRCDIR.marble?=	../../misc/marble
 
 .include "../../x11/kdelibs4/buildlink3.mk"
