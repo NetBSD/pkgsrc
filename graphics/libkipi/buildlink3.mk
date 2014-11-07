39be97ff7d9f648c316042602f477ff87dff0590
adam <adam@pkgsrc.org>
2014-11-07 19:39:24 +0000

    Revbump after updating boost

--- graphics/libkipi/buildlink3.mk
+++ graphics/libkipi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.45 2014/08/22 11:24:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.46 2014/11/07 19:39:25 adam Exp $
 
 BUILDLINK_TREE+=	libkipi
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libkipi
 LIBKIPI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libkipi+=	libkipi>=4.8.0
-BUILDLINK_ABI_DEPENDS.libkipi?=	libkipi>=4.11.5nb4
+BUILDLINK_ABI_DEPENDS.libkipi?=	libkipi>=4.11.5nb5
 BUILDLINK_PKGSRCDIR.libkipi?=	../../graphics/libkipi
 
 .include "../../x11/kdelibs4/buildlink3.mk"
