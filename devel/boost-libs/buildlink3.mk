e41029cd7fe256ad7ef3b6d4d3b4d22aee135e03
adam <adam@pkgsrc.org>
2014-11-07 19:28:38 +0000

    Chamges 1.57.0: Updated Libraries Any: RTTI is not required any more (TypeIndex library is used for RTTI emulation) Fixed 8958
    Asio:
    Fixed the kqueue reactor so that it works on FreeBSD.
    Fixed an issue in the kqueue reactor which resulted in spinning when using serial ports on Mac OS.
    Fixed kqueue reactor support for read-only file descriptors.
    Fixed a compile error when using the /dev/poll reactor.
    Changed the Windows backend to use WSASocketW, as WSASocketA has been deprecated.
    Fixed some warnings reported by Visual C++ 2013.
    Fixed integer type used in the WinRT version of the byte-order conversion functions.
    Changed documentation to indicate that use_future and spawn() are not made available when including the asio.hpp convenience header.
    Explicitly marked asio::strand as deprecated. Use asio::io_service::strand instead.
    
    Circular Buffer:
    Fixed some warnings and move_if_noexcept from Boost.Move is used
    
    Config:
    BOOST_HAS_PRAGMA_DETECT_MISMATCH macro was added
    
    Container:
    Added support for initializer_list.
    Fixed double destruction bugs in vector and backward expansion capable allocators.
    
    Coroutine:
    optionally register stacks with valgrind
    10386 MSVC link error in asymmetric_coroutine.hpp: symbol already defined
    10536 call to 'begin(...pull_coroutine< R > & c)' is ambiguous
    
    Flyweight:
    Added serialization support via Boost Serialization.
    flyweight default constructor was made explicit in Boost 1.56, which introduces a regression in some initialization scenarios. The former non-explicit default constructor has been restored
    
    More...

--- devel/boost-libs/buildlink3.mk
+++ devel/boost-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.34 2014/08/13 10:56:35 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.35 2014/11/07 19:28:38 adam Exp $
 
 BUILDLINK_TREE+=	boost-libs
 
@@ -6,11 +6,11 @@ BUILDLINK_TREE+=	boost-libs
 BOOST_LIBS_BUILDLINK3_MK:=
 
 # Use a dependency pattern that guarantees the proper ABI.
-BUILDLINK_API_DEPENDS.boost-libs+=	boost-libs-1.56.*
-BUILDLINK_ABI_DEPENDS.boost-libs?=	boost-libs>=1.56.0
+BUILDLINK_API_DEPENDS.boost-libs+=	boost-libs-1.57.*
+BUILDLINK_ABI_DEPENDS.boost-libs?=	boost-libs>=1.57.0
 BUILDLINK_PKGSRCDIR.boost-libs?=	../../devel/boost-libs
 
-.include "../../mk/bsd.prefs.mk"
+.include "../../mk/bsd.fast.prefs.mk"
 # Sync with meta-pkgs/boost/Makefile.common
 .if ${OPSYS} == "OpenBSD"
 GCC_REQD+=		4.6
