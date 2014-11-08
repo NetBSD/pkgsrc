000df3e0414f6bc48fab77bb9ca1235ad6094b03
prlw1 <prlw1@pkgsrc.org>
2014-11-08 23:15:06 +0000

    Update gnome-desktop3 to 3.14.1
    - Fixed the build on systems lacking openat()
    - Translation updates
    - gnome-xkb-info: Fix some layouts not being associated to a locale
    - gnome-rr: Add missing introspection annotations
    - wall-clock: Add ref-tests for clock output
    - wall-clock: Always use the ratio character (#726232)
    - gnome-rr: Output the EDID vendor/product info as well
    - gnome-rr: Add backlight output in debug
    - thumbnailer: Try harder to create a failed thumbnail (#728775)
    - Fix the build on OpenBSD
    - gnome-rr: Improve debug output
    - gnome-rr: Actually define output_get_possible_crtcs (#727034)
    - gnome-rr: Initialize GnomeRROutputInfo:rotation to GNOME_RR_ROTATION_0 (#727023)
    - Always set the DPMS mode when requested
    - gnome-xkb-info: Remove xkbfile dependency (#719942) - API and ABI break
    - gnome-xkb-info: Apply main layout locale metadata to variants (#711291)
    - wall-clock: Replace ratio with colon on non-UTF-8 locales (#722664)
    - more new gnome-rr api
    - drop using glibc internal headers
    - docs for thumbnailers
    - idle monitor fixes
    - use newer libgsystem internally
    - gnome-rr: Improve debug output
    - Add gnome_rr_output_get_min_backlight_step() API

--- x11/gnome-desktop3/patches/patch-libgnome-desktop_libgsystem_gsystem-file-utils.c
+++ x11/gnome-desktop3/patches/patch-libgnome-desktop_libgsystem_gsystem-file-utils.c
@@ -1,18 +1,16 @@
-$NetBSD: patch-libgnome-desktop_libgsystem_gsystem-file-utils.c,v 1.3 2014/09/25 15:10:21 prlw1 Exp $
+$NetBSD: patch-libgnome-desktop_libgsystem_gsystem-file-utils.c,v 1.4 2014/11/08 23:15:06 prlw1 Exp $
 
 https://bugzilla.gnome.org/show_bug.cgi?id=737369
 
---- libgnome-desktop/libgsystem/gsystem-file-utils.c.orig	2013-10-04 22:18:43.000000000 +0000
+--- libgnome-desktop/libgsystem/gsystem-file-utils.c.orig	2014-06-03 15:05:13.000000000 +0000
 +++ libgnome-desktop/libgsystem/gsystem-file-utils.c
-@@ -444,7 +444,11 @@ gs_file_open_dir_fd (GFile         *path
+@@ -453,6 +453,9 @@ gs_file_open_dir_fd (GFile         *path
+                      GCancellable  *cancellable,
                       GError       **error)
  {
-   /* Linux specific probably */
-+#ifdef O_DIRECTORY
-   *out_fd = open (gs_file_get_path_cached (path), O_RDONLY | O_NONBLOCK | O_DIRECTORY | O_CLOEXEC);
-+#else
-+  *out_fd = open (gs_file_get_path_cached (path), O_RDONLY | O_NONBLOCK | O_CLOEXEC);
++#ifndef O_DIRECTORY
++#define O_DIRECTORY 0
 +#endif
+   /* Linux specific probably */
+   *out_fd = open (gs_file_get_path_cached (path), O_RDONLY | O_NONBLOCK | O_DIRECTORY | O_CLOEXEC);
    if (*out_fd == -1)
-     {
-       _set_error_from_errno (error);
--- x11/gnome-desktop3/patches/patch-libgnome-desktop_libgsystem_gsystem-shutil.c
+++ x11/gnome-desktop3/patches/patch-libgnome-desktop_libgsystem_gsystem-shutil.c
@@ -1,18 +1,16 @@
-$NetBSD: patch-libgnome-desktop_libgsystem_gsystem-shutil.c,v 1.1 2014/09/25 15:10:21 prlw1 Exp $
+$NetBSD: patch-libgnome-desktop_libgsystem_gsystem-shutil.c,v 1.2 2014/11/08 23:15:06 prlw1 Exp $
 
 https://bugzilla.gnome.org/show_bug.cgi?id=737369
 
---- libgnome-desktop/libgsystem/gsystem-shutil.c.orig	2013-10-04 22:18:43.000000000 +0000
+--- libgnome-desktop/libgsystem/gsystem-shutil.c.orig	2014-06-03 15:05:13.000000000 +0000
 +++ libgnome-desktop/libgsystem/gsystem-shutil.c
-@@ -271,7 +271,11 @@ gs_shutil_rm_rf_children (DIR           
+@@ -322,6 +322,9 @@ gs_shutil_rm_rf_children (DIR           
            
        if (dent->d_type == DT_DIR)
          {
-+#ifdef O_DIRECTORY
-           int child_dfd = openat (dfd, dent->d_name, O_RDONLY | O_NONBLOCK | O_DIRECTORY | O_CLOEXEC | O_NOFOLLOW);
-+#else
-+          int child_dfd = openat (dfd, dent->d_name, O_RDONLY | O_NONBLOCK | O_CLOEXEC | O_NOFOLLOW);
++#ifndef O_DIRECTORY
++#define O_DIRECTORY 0
 +#endif
+           int child_dfd = openat (dfd, dent->d_name, O_RDONLY | O_NONBLOCK | O_DIRECTORY | O_CLOEXEC | O_NOFOLLOW);
  
            if (child_dfd == -1)
-             {
