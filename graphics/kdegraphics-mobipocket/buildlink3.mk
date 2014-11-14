cd98166cd33dd8ad8be462fbe4ccf1121fb832f9
joerg <joerg>
2014-11-14 14:13:44 +0000

    Add missing b3.mk to fix dependencies of other parts of KDE.

---
+++ graphics/kdegraphics-mobipocket/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/11/14 14:13:44 joerg Exp $
+
+BUILDLINK_TREE+=	kdegraphics-mobipocket
+
+.if !defined(KDEGRAPHICS_MOBIPOCKET_BUILDLINK3_MK)
+KDEGRAPHICS_MOBIPOCKET_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kdegraphics-mobipocket+=	kdegraphics-mobipocket>=4.14.3
+BUILDLINK_PKGSRCDIR.kdegraphics-mobipocket?=	../../graphics/kdegraphics-mobipocket
+
+.include "../../print/okular/buildlink3.mk"
+.include "../../x11/kdelibs4/buildlink3.mk"
+.endif	# KDEGRAPHICS_MOBIPOCKET_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kdegraphics-mobipocket
