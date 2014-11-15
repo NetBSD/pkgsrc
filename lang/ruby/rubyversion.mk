38aecf585e2cb917087b0e94d5f8b5523d56ba1b
taca <taca>
2014-11-15 14:42:20 +0000

    Update ruby21-base (and ruby21) package to 2.1.5 (Ruby 2.1.5).
    	* CVE-2014-8090: Another Denial of Service XML Expansion.

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.129 2014/11/15 14:39:09 taca Exp $
+# $NetBSD: rubyversion.mk,v 1.130 2014/11/15 14:42:20 taca Exp $
 #
 
 # This file determines which Ruby version is used as a dependency for
@@ -223,13 +223,13 @@ RUBY_VERSION_REQD?= ${PKGNAME_REQD:C/ruby([0-9][0-9]+)-.*/\1/}
 RUBY18_VERSION=		1.8.7
 RUBY193_VERSION=	1.9.3
 RUBY200_VERSION=	2.0.0
-RUBY21_VERSION=		2.1.4
+RUBY21_VERSION=		2.1.5
 
 # patch
 RUBY18_PATCHLEVEL=	pl374
 RUBY193_PATCHLEVEL=	p551
 RUBY200_PATCHLEVEL=	p598
-#RUBY21_PATCHLEVEL=	p265
+#RUBY21_PATCHLEVEL=	p273
 
 # current API compatible version; used for version of shared library
 RUBY18_API_VERSION=	1.8.7
