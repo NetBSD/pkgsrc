11039b6bc181c9a9954efcd4679ff719173430a7
mef <mef@pkgsrc.org>
2014-11-20 09:05:56 +0000

    (upstream) update 2.3.0 to 2.3.2
    (ChangeLog)
    2012-08-20  Paolo Tosco <paolo.tosco@unito.it>
            * src/formats/mol2format.cpp: added a check for N.4 nitrogens
            (fixes PR#3557898)
    
    2012-06-09  Paolo Tosco <paolo.tosco@unito.it>
            * src/kekulize.cpp: reverted the r4862 patch to kekulize.cpp;
            the incorrect aromaticity perception of oxonium salts concerned
            only the MOL2 format, so the fix was applied to mol2format.cpp
            instead
            * src/formats/mol2format.cpp: added a check to improve downstream
            aromaticity perception on charged molecules containing oxygen
    
    2012-06-07  Paolo Tosco <paolo.tosco@unito.it>
            * include/openbabel/atom.h: added protos for CountFreeSulfurs() and
            IsThiocarboxylSulfur() functions which are equivalent to
            CountFreeOxygens() and IsCarboxylOxygen() and address
            (di)thiocarboxyl groups
            * src/atom.cpp: added the CountFreeSulfurs() and
            IsThiocarboxylSulfur() functions
            * src/forcefields/forcefieldmmff94.cpp: added some additional
            checks to make MMFF94 atom type assignment more robust
            * src/formats/mol2format.cpp: added some checks to improve downstream
            aromaticity perception on charged molecules containing nitrogen,
            oxygen and sulfur
            * src/kekulize.cpp: added a check to fix incorrect perception of
            aromatic oxonium and thionium cations
    
    (NEWS)
    Open Babel 2.3.1 (2011-10-14)
    
    This release represents a major bug-fix release and is a stable
    upgrade, strongly recommended for all users of Open Babel. Many bugs
    and enhancements have been added since the 2.3.0 release.
    
    Citation:
    
    Please consider citing this work if you publish work which used Open Babel:
    Noel M. O'Boyle , Michael Banck , Craig A. James , Chris Morley , Tim
    Vandermeersch  and Geoffrey R. Hutchison. "Open Babel: An open
    chemical toolbox." Journal of Cheminformatics 2011, 3:33.
    
    http://dx.doi.org/10.1186/1758-2946-3-33

--- biology/openbabel/patches/patch-include_openbabel_shared__ptr.h
+++ biology/openbabel/patches/patch-include_openbabel_shared__ptr.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-include_openbabel_shared__ptr.h,v 1.1 2013/05/06 14:22:43 joerg Exp $
+$NetBSD: patch-include_openbabel_shared__ptr.h,v 1.2 2014/11/20 09:05:56 mef Exp $
 
---- include/openbabel/shared_ptr.h.orig	2013-05-05 20:23:37.000000000 +0000
-+++ include/openbabel/shared_ptr.h
-@@ -21,10 +21,14 @@ GNU General Public License for more deta
+--- include/openbabel/shared_ptr.h.orig	2011-10-13 05:24:02.000000000 +0900
++++ include/openbabel/shared_ptr.h	2014-11-20 17:51:30.000000000 +0900
+@@ -21,10 +21,19 @@ GNU General Public License for more deta
    #define shared_ptr boost::shared_ptr
  #else
    #include <memory>
@@ -11,10 +11,15 @@ $NetBSD: patch-include_openbabel_shared__ptr.h,v 1.1 2013/05/06 14:22:43 joerg E
 +  #if defined(_LIBCPP_VERSION) || __cplusplus >= 201103L
 +    using std::shared_ptr;
 +  #else
-+    #if __GNUC__ == 4  //&& __GNUC_MINOR__ < 3  removed at the suggestion of Konstantin Tokarev
-+      #include <tr1/memory>
++   #if defined(_LIBCPP_VERSION) || __cplusplus >= 201103L
++     using std::shared_ptr;
++   #else
++     #if __GNUC__ == 4  //&& __GNUC_MINOR__ < 3  removed at the suggestion of Konstantin Tokarev
++       #include <tr1/memory>
++     #endif
++     using std::tr1::shared_ptr;
++
 +    #endif
-+    using std::tr1::shared_ptr;
    #endif
 -  using std::tr1::shared_ptr;
  #endif
