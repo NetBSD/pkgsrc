19b304a8e21ac90ca8846c40832aca5a985e5327
mef <mef@pkgsrc.org>
2014-11-22 03:01:33 +0000

    (pkgsrc)   - Add comment for patch-util_avutil.h  (from commit log)   - Remove patches, found the same change incorporated:       patches/patch-libasync_itree.h       patches/patch-libasync_qhash.h       patches/patch-libasync_vec.h (upstream)   - update 0.8.2 to 0.8.4
    * Changes in release 0.8.4
      - Switch to new resolver library interface, as previous way of
        reloading resolv.conf is no longer compatible with glibc.
      - Set IP_FREEBIND option if available.
      - Added systemd avenger.service file.
    * Changes in release 0.8.3
      - Updated to work with gcc 4.7.

--- mail/avenger/patches/patch-libasync_ihash.h
+++ mail/avenger/patches/patch-libasync_ihash.h
@@ -1,35 +1,36 @@
-$NetBSD: patch-libasync_ihash.h,v 1.1 2012/01/18 14:32:10 adam Exp $
+$NetBSD: patch-libasync_ihash.h,v 1.2 2014/11/22 03:01:33 mef Exp $
 
 Fix C++ errors
 
---- libasync/ihash.h.orig	2012-01-18 14:07:53.000000000 +0000
-+++ libasync/ihash.h
+--- libasync/ihash.h.orig	2013-01-21 20:01:04.000000000 +0900
++++ libasync/ihash.h	2014-11-22 11:47:06.000000000 +0900
 @@ -235,7 +235,7 @@ public:
    ihash () : eq (E ()), hash (H ()) {}
    ihash (const E &e, const H &h) : eq (e), hash (h) {}
  
--  void insert (V *elm) { insert_val (elm, hash (elm->*key)); }
-+  void insert (V *elm) { this->insert_val (elm, hash (elm->*key)); }
+-  void insert (V *elm) { ihash::insert_val (elm, hash (elm->*key)); }
++  void insert (V *elm) { this->ihash::insert_val (elm, hash (elm->*key)); }
  
  #if 0
    template<class T> V *operator[] (const T &k) const {
-@@ -243,16 +243,16 @@ public:
-   V *operator[] (const K &k) const {
- #endif
+@@ -278,9 +278,9 @@ public:
+ 
+   V *operator() (const K1 &k1, const K2 &k2) const {
      V *v;
--    for (v = lookup_val (hash (k));
-+    for (v = this->lookup_val (hash (k));
- 	 v && !eq (k, v->*key);
+-    for (v = lookup_val (hash (k1, k2));
++    for (v = this->lookup_val (hash (k1, k2));
+ 	 v && !(eq1 (k1, v->*key1) && eq2 (k2, v->*key2));
 -	 v = next_val (v))
 +	 v = this->next_val (v))
        ;
      return v;
    }
+@@ -320,7 +320,7 @@ public:
  
    V *nextkeq (V *v) {
-     const K &k = v->*key;
--    while ((v = next_val (v)) && !eq (k, v->*key))
-+    while ((v = this->next_val (v)) && !eq (k, v->*key))
+     const V &k = *v;
+-    while ((v = next_val (v)) && !eq (k, *v))
++    while ((v = this->next_val (v)) && !eq (k, *v))
        ;
      return v;
-   };
+   }
