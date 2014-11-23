b6f0dfa129d4ede854168991dbf96166cc41e91a
wiz <wiz@pkgsrc.org>
2014-11-23 19:27:29 +0000

    Update to 0.8.1. Add comment to patch.
    Sigil 0.8.1 2014.10.12
    - Set minimum OS X version to 10.9.0 in Info.plist so users trying to
      run on older versions of OS X will receive an error dialog instead of
      a crash dialog.
    - Set minimum Windows version to Vista in Installer so installation will
      error when trying to install on XP (which is not supported and Sigil
      binary packages won't run on).
    - Fix issue #21: Use Hunspell WORDCHARS to help in tokenization of words.
    - Pull request #19: Moving plugin description to ToolTips.
    - Pull request #20: Preserve current file name for future save as actions
      if appropriate.
    - Fix bug where save after using input plugin would fail.
    
    Sigil 0.8.0 2014.09.27
    - Plugin framework.
    - Add the svg image tag as an svg inline element (not a block element) and
      add it as an empty element (TidyEmptyTags).
    - Allow user defined list of entities to preserve.
    - Pull request #16: Hardcode menu Plugins in UI, move it before menu Help.
    - Pull request #10: Add ability to move entries in TOC up and down.
    - Pull request #8: Allow pasting HTML as HTML or plain text in BookView.

--- editors/Sigil/patches/patch-src_minizip_ioapi.c
+++ editors/Sigil/patches/patch-src_minizip_ioapi.c
@@ -1,6 +1,9 @@
-$NetBSD: patch-src_minizip_ioapi.c,v 1.1 2012/11/19 10:57:45 ryoon Exp $
+$NetBSD: patch-src_minizip_ioapi.c,v 1.2 2014/11/23 19:27:29 wiz Exp $
 
---- src/minizip/ioapi.c.orig	2012-10-27 11:24:40.000000000 +0000
+Fix linking on NetBSD and DragonFly BSD, where fopen, fseek, and ftello
+are 64-bit filesize safe.
+
+--- src/minizip/ioapi.c.orig	2014-10-12 14:45:14.000000000 +0000
 +++ src/minizip/ioapi.c
 @@ -14,6 +14,12 @@
          #define _CRT_SECURE_NO_WARNINGS
