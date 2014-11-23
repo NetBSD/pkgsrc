fbca9db00da55bdd6db43c2b1c153ab2233d2a5c
mef <mef@pkgsrc.org>
2014-11-23 09:46:49 +0000

    (pkgsrc)  - Add two CMAKE_ARGS (silence and) to maintain the similar PLIST leve     # silence warning     CMAKE_ARGS+=    -Wno-dev     CMAKE_ARGS+=    -DINSTALL_DOC=ON  - Add post-install: target to Remove PaxHeader garbage  - Add comment on patch
    (upstream) Update 1.26.1 to 1.33.0

--- comms/gammu/patches/patch-contrib_smscgi_sms__cgi.c
+++ comms/gammu/patches/patch-contrib_smscgi_sms__cgi.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-contrib_smscgi_sms__cgi.c,v 1.1 2012/02/16 17:13:03 hans Exp $
+$NetBSD: patch-contrib_smscgi_sms__cgi.c,v 1.2 2014/11/23 09:46:49 mef Exp $
+
+Fix build on SunOS.
 
 --- contrib/smscgi/sms_cgi.c.orig	2009-09-08 09:57:12.000000000 +0200
 +++ contrib/smscgi/sms_cgi.c	2012-01-25 19:47:48.177845692 +0100
--- comms/gammu/patches/patch-libgammu_phone_at_at-sms.c
+++ comms/gammu/patches/patch-libgammu_phone_at_at-sms.c
@@ -1,15 +1,17 @@
-$NetBSD: patch-libgammu_phone_at_at-sms.c,v 1.1 2012/07/09 19:17:52 joerg Exp $
+$NetBSD: patch-libgammu_phone_at_at-sms.c,v 1.2 2014/11/23 09:46:50 mef Exp $
 
---- libgammu/phone/at/at-sms.c.orig	2012-07-06 11:57:04.000000000 +0000
-+++ libgammu/phone/at/at-sms.c
-@@ -83,7 +83,9 @@ GSM_Error ATGEN_ReplyGetSMSMemories(GSM_
- 		}
+Don't use strcharnul from the helper library, it doesn't end up as PIC
+when compiled with Clang.
+
+--- libgammu/phone/at/at-sms.c~	2014-11-23 18:13:31.000000000 +0900
++++ libgammu/phone/at/at-sms.c	2014-11-23 18:15:42.000000000 +0900
+@@ -112,6 +112,9 @@ GSM_Error ATGEN_ReplyGetSMSMemories(GSM_
  		if (pos_start != NULL) {
  			/* Detect which memories we can use for saving */
--			pos_end = strchrnul(pos_start + 1, ')');
+ 			pos_end = strchrnul(pos_start + 1, ')');
 +			pos_end = strchr(pos_start + 1, ')');
 +			if (pos_end == NULL)
 +				pos_end = pos_start + strlen(pos_start);
  			pos_tmp = strstr(pos_start, "\"SM\"");
+ 
  			if (pos_tmp != NULL && pos_tmp < pos_end) {
- 				s->Phone.Data.Priv.ATGEN.SIMSaveSMS = AT_AVAILABLE;
