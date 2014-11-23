40402edc8f9c0a076c198a9a64a56fc51ec0fc29
prlw1 <prlw1@pkgsrc.org>
2014-11-23 13:46:07 +0000

    Update gtksourceview3 to 3.14.2
    News in 3.14.2
    --------------
    * Bug fix
    * Translation updates
    
    News in 3.14.1
    --------------
    * Fix UndoManager with a new implementation
    * Various other improvements
    * Translation updates
    
    News in 3.14.0
    --------------
    * Translation updates
    
    News in 3.13.91
    ---------------
    * Improved Mac OS X support
    * Gutter improvements
    * Various other improvements and code clean-up
    * Translation updates
    
    News in 3.13.90
    ---------------
    * Adjustments to the file loading and saving API:
      - Make gtk_source_encoding_get_default_candidates() private
      - Replace gtk_source_encoding_foreach() by gtk_source_encoding_get_all()
      The file loading and saving API can be considered stable now.
    
    * Add Vala bindings
    * Various other improvements
    * Translation updates
    
    News in 3.13.3
    --------------
    * Extend the api to obtain candidate encodings
    * Rework drawing of spaces and margins (requires new Gtk+)
    * Various other improvements and bugfixes
    * Translation updates
    
    News in 3.13.2
    --------------
    * New feature: file loading and saving
    * Split testfiles.sh to tests/syntax-highlighting/
    * GtkSourceView is now fully relicensed to LGPL
    * Various other improvements and bugfixes
    * Translation updates
    
    News in 3.13.1
    --------------
    * Improvements to the words completion provider
    * Completion window sizing fixes
    * Improvements to some lang files
    * Various other improvements and bugfixes
    * Updated translations
    
    News in 3.12.1
    --------------
    * Renovate test-widget
    * Various improvements and bugfixes
    * Updated translations
    
    News in 3.12.0
    --------------
    * Small improvements to the documentation
    * Updated translations
    
    News in 3.11.91
    ---------------
    * Updated translations
    
    News in 3.11.90
    ---------------
    * Add syntax highlighting for LLVM IR and Sweave
    * Improvements to some lang files
    * Updated translations
    
    News in 3.11.4
    --------------
    * Add the popular Solarized style schemes
    * Improvements to some lang files and a new lang file for lex
    * Optimize the draw-spaces code
    * Various fixes to the completion framework
    * Misc bugfixes
    * Updated translations
    
    News in 3.11.3
    --------------
    * Font scaling support for headings/titles in LaTeX and HTML
    * Add Change Case submenu to the right click context menu
    * Add ANS Forth 94 syntax highlighting
    * Misc bugfixes
    * Updated translations
    
    News in 3.11.2
    --------------
    * Add API to change text case
    * Add reStructuredText and yaml syntax highlighting
    * Improve LaTeX, m4 and D syntax highlighting
    * Documentation improvements (including best practices for language definition files)
    * Installed tests
    * Misc bugfixes
    * Updated translations
    
    News in 3.11.1
    --------------
    * Deprecate the Gutter:xpad and Gutter:ypad properties
    * Deprecate gtk_source_gutter_get_window()
    * Better implementation of GtkSourceMarks (private class GtkSourceMarksSequence)
    * Style schemes: more visible right margin
    * Documentation improvements
    * Misc bugfixes
    * Updated translations

--- x11/gtksourceview3/buildlink3.mk
+++ x11/gtksourceview3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2014/05/05 00:48:30 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2014/11/23 13:46:07 prlw1 Exp $
 
 BUILDLINK_TREE+=	gtksourceview3
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gtksourceview3
 GTKSOURCEVIEW3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gtksourceview3+=	gtksourceview3>=3.4.2
-BUILDLINK_ABI_DEPENDS.gtksourceview3?=	gtksourceview3>=3.10.2nb1
+BUILDLINK_ABI_DEPENDS.gtksourceview3?=	gtksourceview3>=3.14.2
 BUILDLINK_PKGSRCDIR.gtksourceview3?=	../../x11/gtksourceview3
 
 .include "../../textproc/libxml2/buildlink3.mk"
