09584e5fd385d44be19eb6c4be228b5c2c357b3a
szptvlfn <szptvlfn>
2014-11-23 12:52:31 +0000

    Bump PKGREVISION for hs-transformers-0.4.2.0

--- devel/hs-mmorph/buildlink3.mk
+++ devel/hs-mmorph/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/09/02 20:57:11 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/11/23 12:52:32 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-mmorph
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-mmorph
 HS_MMORPH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.4
-BUILDLINK_ABI_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.4
+BUILDLINK_ABI_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.4nb1
 BUILDLINK_PKGSRCDIR.hs-mmorph?=	../../devel/hs-mmorph
 
 .include "../../devel/hs-transformers/buildlink3.mk"
--- textproc/hs-regex-posix/buildlink3.mk
+++ textproc/hs-regex-posix/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:41 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/11/23 12:52:33 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-regex-posix
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-regex-posix
 HS_REGEX_POSIX_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-regex-posix+=	hs-regex-posix>=0.95.2
-BUILDLINK_ABI_DEPENDS.hs-regex-posix+=	hs-regex-posix>=0.95.2
+BUILDLINK_ABI_DEPENDS.hs-regex-posix+=	hs-regex-posix>=0.95.2nb1
 BUILDLINK_PKGSRCDIR.hs-regex-posix?=	../../textproc/hs-regex-posix
 
 .include "../../textproc/hs-regex-base/buildlink3.mk"
