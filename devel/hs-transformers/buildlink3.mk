afc4efd90cdeb7e5467e96327fe77adb7aab816f
szptvlfn <szptvlfn@pkgsrc.org>
2014-11-23 12:51:34 +0000

    Update to 0.4.2.0
    changelog:
    0.4.2.0 Ross Paterson <ross@soi.city.ac.uk> Nov 2014
        * Dropped compatibility with base-1.x
        * Data.Functor.Identity in base for GHC >= 7.10
        * Added mapLift and runErrors to Control.Applicative.Lift
        * Added AutoDeriveTypeable for GHC >= 7.10
        * Expanded messages from mfix on ExceptT and MaybeT

--- devel/hs-transformers/buildlink3.mk
+++ devel/hs-transformers/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:40 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/11/23 12:51:34 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-transformers
 
 .if !defined(HS_TRANSFORMERS_BUILDLINK3_MK)
 HS_TRANSFORMERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-transformers+=	hs-transformers>=0.4.1
-BUILDLINK_ABI_DEPENDS.hs-transformers+=	hs-transformers>=0.4.1.0
+BUILDLINK_API_DEPENDS.hs-transformers+=	hs-transformers>=0.4.2
+BUILDLINK_ABI_DEPENDS.hs-transformers+=	hs-transformers>=0.4.2.0
 BUILDLINK_PKGSRCDIR.hs-transformers?=	../../devel/hs-transformers
 .endif	# HS_TRANSFORMERS_BUILDLINK3_MK
 
