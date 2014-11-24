ba795e0da84115077ac10bdf94a76090f5e9f01b
joerg <joerg>
2014-11-24 00:32:31 +0000

    Add cwrappers glue as developed during Google's Summer of Code 2007.

--- mk/wrapper/bsd.wrapper.mk
+++ mk/wrapper/bsd.wrapper.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.wrapper.mk,v 1.89 2014/03/06 10:15:54 uebayasi Exp $
+# $NetBSD: bsd.wrapper.mk,v 1.90 2014/11/24 00:32:31 joerg Exp $
 #
 # Copyright (c) 2005 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -65,7 +65,9 @@ ECHO_WRAPPER_MSG?=	${TRUE}
 # Prepend ${WRAPPER_BINDIR} to the PATH so that the wrappers are found
 # first when searching for executables.
 #
+.if ${USE_CWRAPPERS:tl} == "no"
 PREPEND_PATH+=		${WRAPPER_BINDIR}
+.endif
 
 ###
 ### BEGIN: after the barrier
