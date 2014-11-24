a981429bbf97530bf61491ad5aff07a25752cafe
joerg <joerg@pkgsrc.org>
2014-11-24 00:47:48 +0000

    Adjust !TR1 support again.

--- biology/openbabel/patches/patch-src_ops_unique.cpp
+++ biology/openbabel/patches/patch-src_ops_unique.cpp
@@ -1,23 +1,28 @@
-$NetBSD: patch-src_ops_unique.cpp,v 1.1 2013/05/06 14:22:43 joerg Exp $
+$NetBSD: patch-src_ops_unique.cpp,v 1.2 2014/11/24 00:47:48 joerg Exp $
 
---- src/ops/unique.cpp.orig	2013-05-05 20:34:38.000000000 +0000
+--- src/ops/unique.cpp.orig	2014-11-23 16:04:26.000000000 +0000
 +++ src/ops/unique.cpp
-@@ -23,6 +23,9 @@ GNU General Public License for more deta
+@@ -21,10 +21,12 @@ GNU General Public License for more deta
+ #include <openbabel/obconversion.h>
+ #include <openbabel/descriptor.h>
  #include <openbabel/inchiformat.h>
- #ifdef _MSC_VER
+-#ifdef _MSC_VER
++#include <cstddef>
++#if defined(_MSC_VER) || defined(_LIBCPP_VERSION) || __cplusplus>=201103L
    #include <unordered_map>
-+#elif defined(_LIBCPP_VERSION) || __cplusplus >= 201103L
-+# include <unordered_map>
-+using std::unordered_map;
  #elif (__GNUC__ == 4 && __GNUC_MINOR__ >= 1 && !defined(__APPLE_CC__))
    #include <tr1/unordered_map>
++  using std::tr1::unordered_map;
  #else
-@@ -35,7 +38,7 @@ GNU General Public License for more deta
+   #ifdef USE_BOOST
+     #include <boost/tr1/unordered_map.hpp>
+@@ -35,9 +37,6 @@ GNU General Public License for more deta
  #endif
  
  using namespace std;
 -#ifndef NO_UNORDERED_MAP
-+#if !defined(NO_UNORDERED_MAP) && !defined(_LIBCPP_VERSION) && __cplusplus < 201103L
- using std::tr1::unordered_map;
- #endif
+-using std::tr1::unordered_map;
+-#endif
  namespace OpenBabel
+ {
+ 
