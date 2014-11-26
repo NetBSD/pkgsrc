3631a2c3dcdc3d17f925f34f347a7256b5fb2b2a
ryoon <ryoon@pkgsrc.org>
2014-11-26 14:56:28 +0000

    Try to build under NetBSD/evbearmv7hf-el. Still broken.

---
+++ www/firefox/patches/patch-ipc_chromium_src_base_atomicops.h
@@ -0,0 +1,15 @@
+$NetBSD: patch-ipc_chromium_src_base_atomicops.h,v 1.1 2014/11/26 14:56:28 ryoon Exp $
+
+* Avoid invalid cast errors under NetBSD/evbearmv7hf-el.
+
+--- ipc/chromium/src/base/atomicops.h.orig	2014-11-13 22:49:59.000000000 +0000
++++ ipc/chromium/src/base/atomicops.h
+@@ -45,7 +45,7 @@ typedef int64_t Atomic64;
+ 
+ // Use AtomicWord for a machine-sized pointer.  It will use the Atomic32 or
+ // Atomic64 routines below, depending on your architecture.
+-#ifdef OS_OPENBSD
++#if defined(OS_OPENBSD) || (defined(OS_NETBSD) && defined(ARCH_CPU_ARM_FAMILY))
+ #ifdef ARCH_CPU_64_BITS
+ typedef Atomic64 AtomicWord;
+ #else
--- www/firefox/patches/patch-mfbt_Poison.cpp
+++ www/firefox/patches/patch-mfbt_Poison.cpp
@@ -1,18 +1,17 @@
-$NetBSD: patch-mfbt_Poison.cpp,v 1.5 2014/10/15 13:07:07 ryoon Exp $
+$NetBSD: patch-mfbt_Poison.cpp,v 1.6 2014/11/26 14:56:28 ryoon Exp $
 
 * Use posix_madvise(3) instead of madvise(3) for Solaris/SunOS.
   Solaris/SunOS's madvise(3) is not enabled in this context,
   and its first argument type is different from NetBSD/Linux's one.
 
---- mfbt/Poison.cpp.orig	2014-10-11 09:06:41.000000000 +0000
+--- mfbt/Poison.cpp.orig	2014-11-13 22:50:12.000000000 +0000
 +++ mfbt/Poison.cpp
 @@ -129,7 +129,11 @@ ReleaseRegion(void* aRegion, uintptr_t a
  static bool
  ProbeRegion(uintptr_t aRegion, uintptr_t aSize)
  {
--  if (madvise(reinterpret_cast<void*>(aRegion), aSize, MADV_NORMAL)) {
 +#if !defined(__sun__)
-+  if (madvise(reinterpret_cast<void*>(aRegion), aSize, MADV_NORMAL)) {
+   if (madvise(reinterpret_cast<void*>(aRegion), aSize, MADV_NORMAL)) {
 +#else
 +  if (posix_madvise(reinterpret_cast<void*>(aRegion), aSize, MADV_NORMAL)) {
 +#endif
