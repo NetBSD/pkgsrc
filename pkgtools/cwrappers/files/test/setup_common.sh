05ad1433aa17091a9637d1ddf502bb916fd3c601
joerg <joerg@pkgsrc.org>
2014-11-27 20:36:43 +0000

    cwrappers-20141127: Rename environment variable to CWRAPPERS_CONFIG_DIR for consistency.

--- pkgtools/cwrappers/files/test/setup_common.sh
+++ pkgtools/cwrappers/files/test/setup_common.sh
@@ -4,21 +4,21 @@ wrapper_test_setup() {
 
     # variables available to tests
     WRAPPER="$(atf_get_srcdir)/../bin/${_wrappee}-wrapper"
-    WRAPPER_CONFIG_DIR=wrapperconfig; export WRAPPER_CONFIG_DIR
-    mkdir ${WRAPPER_CONFIG_DIR}
+    CWRAPPERS_CONFIG_DIR=wrapperconfig; export CWRAPPERS_CONFIG_DIR
+    mkdir ${CWRAPPERS_CONFIG_DIR}
     LOCALBASE=/local/base
     BUILDLINK_DIR=/wrksrc/build/link
     COMPILER_RPATH_FLAG="-Wl,-R"	# or -L on Darwin, or...
 
     # minimal config file for test purposes
-    cat > ${WRAPPER_CONFIG_DIR}/${_wrappee} << EOF
+    cat > ${CWRAPPERS_CONFIG_DIR}/${_wrappee} << EOF
 worklog=/dev/null
 exec=echo
 transform=I:${LOCALBASE}:${BUILDLINK_DIR}
 EOF
     # plus any requested transforms
     while [ $# -gt 0 ]; do
-	echo "transform=$1" >> ${WRAPPER_CONFIG_DIR}/${_wrappee}
+	echo "transform=$1" >> ${CWRAPPERS_CONFIG_DIR}/${_wrappee}
 	shift
     done
 
