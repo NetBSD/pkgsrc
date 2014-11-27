646b789398c296c7ad2d97884a0709748ac91bdf
jmcneill <jmcneill@pkgsrc.org>
2014-11-27 13:47:02 +0000

    earmv7hf build fixes

--- www/webkit-gtk/patches/patch-Source_WTF_wtf_Platform.h
+++ www/webkit-gtk/patches/patch-Source_WTF_wtf_Platform.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.2 2014/08/03 22:30:05 wiz Exp $
+$NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.3 2014/11/27 13:47:03 jmcneill Exp $
 
---- Source/WTF/wtf/Platform.h.orig	2014-04-14 06:40:45.000000000 +0000
+--- Source/WTF/wtf/Platform.h.orig	2014-10-22 08:16:09.000000000 +0000
 +++ Source/WTF/wtf/Platform.h
-@@ -618,6 +618,11 @@
+@@ -632,6 +632,11 @@
  #define USE_SYSTEM_MALLOC 1
  #endif
  
@@ -14,3 +14,12 @@ $NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.2 2014/08/03 22:30:05 wiz Exp $
  #if !defined(ENABLE_GLOBAL_FASTMALLOC_NEW)
  #define ENABLE_GLOBAL_FASTMALLOC_NEW 1
  #endif
+@@ -723,7 +728,7 @@
+    low-level interpreter. */
+ #if !defined(ENABLE_LLINT) \
+     && ENABLE(JIT) \
+-    && (OS(DARWIN) || OS(LINUX) || OS(FREEBSD)) \
++    && (OS(DARWIN) || OS(LINUX) || OS(FREEBSD) || OS(NETBSD)) \
+     && (PLATFORM(MAC) || PLATFORM(IOS) || PLATFORM(GTK)) \
+     && (CPU(X86) || CPU(X86_64) || CPU(ARM_THUMB2) || CPU(ARM_TRADITIONAL) || CPU(ARM64) || CPU(MIPS) || CPU(SH4))
+ #define ENABLE_LLINT 1
