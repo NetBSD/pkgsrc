e8ed570fe548ad39815f50301f9be58da0361935
obache <obache>
2014-11-28 09:57:32 +0000

    Bump PKGREVISION. * Fixes regressions, revive parts removed at "Fix build under OpenBSD/amd64 5.5" * Drop default behaviour changes and local features from OpenBSD ports,   it break other platforms, at least, NetBSD-6.0/i386. * Remove needless patches after "Restore monolithic gcc48 and gcc48-libs".

--- lang/gcc48/patches/patch-gcc_go_go-lang.c
+++ lang/gcc48/patches/patch-gcc_go_go-lang.c
@@ -1,16 +1,18 @@
-$NetBSD: patch-gcc_go_go-lang.c,v 1.1 2014/05/31 13:06:25 ryoon Exp $
+$NetBSD: patch-gcc_go_go-lang.c,v 1.2 2014/11/28 09:57:32 obache Exp $
 
 --- gcc/go/go-lang.c.orig	2013-12-12 02:29:08.000000000 +0000
 +++ gcc/go/go-lang.c
-@@ -457,6 +457,12 @@ go_localize_identifier (const char *iden
+@@ -457,6 +457,14 @@ go_localize_identifier (const char *iden
    return identifier_to_locale (ident);
  }
  
++#ifdef __OpenBSD__
 +const char *
 +fname_as_string(int pretty_p __attribute__((__unused__)))
 +{
 +	return NULL;
 +}
++#endif
 +
  #undef LANG_HOOKS_NAME
  #undef LANG_HOOKS_INIT
