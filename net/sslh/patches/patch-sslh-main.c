58ae13af3a9a216c8546bddf14719950d5f81c7d
bsiegert <bsiegert@pkgsrc.org>
2014-11-28 10:45:27 +0000

    Update sslh to 1.16. Patch provided by Nils Ratusznik in PR pkg/49257.
    Changelog for sslh is the following :
    v1.16: 11FEB2014
    Probes made more resilient, to incoming data containing NULLs. Also made
    them behave properly when receiving too short packets to probe on the
    first incoming packet.
    (Ondrej Kuzník)
    Libcap support: Keep only CAP_NET_ADMIN if started as root with
    transparent proxying and dropping priviledges (enable USELIBCAP in
    Makefile). This avoids having to mess with filesystem capabilities.
    (Sebastian Schmidt/yath)
    Fixed bugs related to getpeername that would cause sslh to quit
    erroneously (getpeername can return actual errors if connections are
    dropped before getting to getpeername).
    Set IP_FREEDBIND if available to bind to addresses that don't yet exist.
    
    Changelog for pkgsrc :
    - now uses Github for fetching source, ${HOMEPAGE} says : "sslh is
      managed in Git and pushed to Github" ;
    - replaced #!/bin/bash with #!/bin/sh in genver.sh, choice of sh instead
      of bash is because of a commit in sslh github :
    https://github.com/yrutschle/sslh/commit/62cbb55b8e9c9b0bc878b094c5be815d58276b39

--- net/sslh/patches/patch-sslh-main.c
+++ net/sslh/patches/patch-sslh-main.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-sslh-main.c,v 1.1 2013/11/08 13:33:55 obache Exp $
+$NetBSD: patch-sslh-main.c,v 1.2 2014/11/28 10:45:28 bsiegert Exp $
 
 * getopt_long_only does not exist on NetBSD getopt.
 
---- sslh-main.c.orig	2013-07-27 14:25:04.000000000 +0000
+--- sslh-main.c.orig	2014-02-11 21:06:01.000000000 +0000
 +++ sslh-main.c
-@@ -337,7 +337,7 @@ static void cmdline_config(int argc, cha
+@@ -358,7 +358,7 @@ static void cmdline_config(int argc, cha
  #ifdef LIBCONFIG
      optind = 1;
      opterr = 0; /* we're missing protocol options at this stage so don't output errors */
@@ -13,7 +13,7 @@ $NetBSD: patch-sslh-main.c,v 1.1 2013/11/08 13:33:55 obache Exp $
          if (c == 'F') {
              config_filename = optarg;
              /* find the end of the listen list */
-@@ -362,7 +362,7 @@ static void parse_cmdline(int argc, char
+@@ -383,7 +383,7 @@ static void parse_cmdline(int argc, char
      optind = 1;
      opterr = 1;
  next_arg:
