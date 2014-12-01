63a06513e95dc28aa12bfddca2073a62735c7cac
wiz <wiz@pkgsrc.org>
2014-12-01 08:13:04 +0000

    Add upstream bug report URL.

--- multimedia/gst-plugins1-base/patches/patch-ext_alsa_gstalsasink.c
+++ multimedia/gst-plugins1-base/patches/patch-ext_alsa_gstalsasink.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-ext_alsa_gstalsasink.c,v 1.1 2012/11/29 08:22:29 ryoon Exp $
+$NetBSD: patch-ext_alsa_gstalsasink.c,v 1.2 2014/12/01 08:13:04 wiz Exp $
 
 * NetBSD has no ESTRPIPE
+https://bugzilla.gnome.org/show_bug.cgi?id=740952
 
 --- ext/alsa/gstalsasink.c.orig	2012-09-19 14:07:16.000000000 +0000
 +++ ext/alsa/gstalsasink.c
