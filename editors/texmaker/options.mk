9a6297009fcced7367afb5431ee9daf4768fc8c7
mef <mef@pkgsrc.org>
2014-12-02 12:16:29 +0000

    This commit is based on wip/texmaker by Niclas Rosenvik, thanks. (pkgsrc) Now option qt5 or qt4 is at PKG_OPTIONS_REQUIRED_GROUPS= qt so that one of those two should be selected.
    (upstream)
    update 4.3. to 4.4.1
    ( Release note at http://www.xm1math.net/texmaker/log.html )
    
    version 4.4 -> 4.4.1 :
    Bugs fixed :
    - a crash if the "% !TEX encoding=" meta tag is not valid has been fixed
    - a bug related to some shorctuts (ctrl+shift+?) has been fixed
    
    Note :
    The macosx Lion/Mountain/Maverick package is again compiled with Qt
    5.2.1 (two much issues with Qt 5.3.2 webkit and macdeployqt on MacOsX
    >=10.10)
    
    version 4.3 -> 4.4 :
    Features :
    - a new wizard for a standard XeLaTeX document has been added
    - a script ("User" menu to execute it) to apply a hard word wrap at 80
      columns to the selected text has been added (will work like the php
      wordwrap() function - just change the textwidth value in the script
      if you need another value)
    - the list of furnished scripts is displayed in the "Run Script"
      sub-menu of the "User" menu (if a script is located at the same
      place than the furnished scripts, it will appear automtically in
      this menu)
    - support for the meta tags "% !TEX encoding=" and "% !TEX spellcheck"
      has been added (the meta tag "% !TEX root" is already supported)
    - .vrb,.listing and .spx extensions have been added to the "Clean"
      command (users requests)
    - cs, de, el, hu, pl, ru, se and vi translations have been updated
      (thanks to all the translators)
    
    Bugs fixed :
    - the xelatex command is now included in the "Tools" menu
    - a bad behavior when spelling the document while a text is selected
      has been fixed (issue 1393)
    
    Note :
    - The windows versions are now compiled with poppler 0.26.
    - The macosx Lion/Mountain/Maverick package is now compiled with Qt
      5.3.2 and poppler 0.26
    - The obsolete macosx 32 bits version is no more furnished (the 4.3
      version will stay available on the download page)

--- editors/texmaker/options.mk
+++ editors/texmaker/options.mk
@@ -1,15 +1,19 @@
-# $NetBSD: options.mk,v 1.1 2014/11/18 13:20:35 mef Exp $
+# $NetBSD: options.mk,v 1.2 2014/12/02 12:16:29 mef Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.texmaker
-PKG_SUPPORTED_OPTIONS=	qt5
+PKG_OPTIONS_REQUIRED_GROUPS= qt
+PKG_OPTIONS_GROUP.qt = qt4 qt5
+PKG_SUGGESTED_OPTIONS=	qt4
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mqt5)
 .include "../../x11/qt5-qtscript/buildlink3.mk"
 .include "../../x11/qt5-qtwebkit/buildlink3.mk"
 .include "../../print/poppler-qt5/buildlink3.mk"
-.else
+.endif
+
+.if !empty(PKG_OPTIONS:Mqt4)
 .include "../../x11/qt4-libs/buildlink3.mk"
 .include "../../x11/qt4-tools/buildlink3.mk"
 .include "../../print/poppler-qt4/buildlink3.mk"
