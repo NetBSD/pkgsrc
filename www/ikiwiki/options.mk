be954aba920616a473007eb0967ec2971d61dffe
schmonz <schmonz@pkgsrc.org>
2014-12-04 17:28:43 +0000

    Add 'l10n' option to enable po4a-managed translations of site content. Bump PKGREVISION.

--- www/ikiwiki/options.mk
+++ www/ikiwiki/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.17 2014/10/17 11:50:42 schmonz Exp $
+# $NetBSD: options.mk,v 1.18 2014/12/04 17:28:43 schmonz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.ikiwiki
-PKG_SUPPORTED_OPTIONS=		cgi imagemagick python w3m
+PKG_SUPPORTED_OPTIONS=		cgi imagemagick l10n python w3m
 PKG_SUPPORTED_OPTIONS+=		cvs git svn	# not mutually exclusive
 PKG_SUPPORTED_OPTIONS+=		ikiwiki-amazon-s3 ikiwiki-highlight ikiwiki-search
 PKG_SUGGESTED_OPTIONS=		cgi
@@ -47,6 +47,17 @@ DEPENDS+=	xapian-omega-[0-9]*:../../textproc/xapian-omega
 DEPENDS+=	p5-PerlMagick-[0-9]*:../../graphics/p5-PerlMagick
 # suggest ghostscript (required for PDF-to-PNG thumbnailing)
 # and libmagickcore-extra (required for SVG-to-PNG thumbnailing)
+.endif
+
+PLIST_VARS+=		l10n
+.if !empty(PKG_OPTIONS:Ml10n)
+DEPENDS+=		po4a>=0.35:../../textproc/po4a
+PLIST.l10n=		yes
+.else
+SUBST_CLASSES+=		l10n
+SUBST_STAGE.l10n=	pre-configure
+SUBST_FILES.l10n=	po/Makefile
+SUBST_SED.l10n+=	-e 's|\(Locale::Po4a::Common\)|\1::Iff::PKG_OPTIONS|'
 .endif
 
 .if !empty(PKG_OPTIONS:Mpython)
