17e1ad57ab811619a2849f863a29eb973c137265
khorben <khorben@pkgsrc.org>
2014-12-05 12:25:42 +0000

    Packaged gnutls 3.2.19
    * Version 3.2.19 (released 2014-10-13)
    
    ** libgnutls: Fixes in the transparent import of PKCS #11 certificates.
    Reported by Joseph Peruski.
    
    ** libgnutls: Fixed issue with unexpected non-fatal errors resetting the
    handshake's hash buffer, in applications using the heartbeat extension
    or DTLS. Reported by Joeri de Ruiter.
    
    ** libgnutls: fix issue in DTLS retransmission when session tickets
    were in use; reported by Manuel Pégourié-Gonnard.
    
    ** libgnutls: Prevent abort() in library if getrusage() fails. Try to
    detect instead which of RUSAGE_THREAD and RUSAGE_SELF would work.
    
    ** guile: new 'set-session-server-name!' procedure; see the manual for
    details.
    
    ** API and ABI modifications:
    No changes since last version.

--- security/gnutls/patches/patch-gl_stdio.in.h
+++ security/gnutls/patches/patch-gl_stdio.in.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-gl_stdio.in.h,v 1.1 2012/08/02 09:37:32 jperkin Exp $
+$NetBSD: patch-gl_stdio.in.h,v 1.2 2014/12/05 12:25:42 khorben Exp $
 
 Avoid conflict between gets() and std::gets()
 
---- gl/stdio.in.h.orig	Thu Aug  2 09:29:29 2012
-+++ gl/stdio.in.h	Thu Aug  2 09:30:17 2012
-@@ -699,6 +699,7 @@
+--- gl/stdio.in.h.orig	2014-04-18 17:03:19.000000000 +0000
++++ gl/stdio.in.h
+@@ -714,6 +714,7 @@ _GL_WARN_ON_USE (getline, "getline is un
  # endif
  #endif
  
@@ -12,7 +12,7 @@ Avoid conflict between gets() and std::gets()
  /* It is very rare that the developer ever has full control of stdin,
     so any use of gets warrants an unconditional warning; besides, C11
     removed it.  */
-@@ -706,6 +707,7 @@
+@@ -721,6 +722,7 @@ _GL_WARN_ON_USE (getline, "getline is un
  #if HAVE_RAW_DECL_GETS
  _GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
  #endif
