01f468da19ea95138861b70350abe0596652d5e7
rodent <rodent@pkgsrc.org>
2014-12-05 21:44:10 +0000

    +google-api-python-client

--- lang/python/versioned_dependencies.mk
+++ lang/python/versioned_dependencies.mk
@@ -1,4 +1,4 @@
-# $NetBSD: versioned_dependencies.mk,v 1.15 2014/06/07 14:27:02 wiz Exp $
+# $NetBSD: versioned_dependencies.mk,v 1.16 2014/12/05 21:44:10 rodent Exp $
 #
 # This file determines which separate distribution of a Python
 # package is used as dependency, depending on the Python version
@@ -23,6 +23,7 @@ _SUPPORTED_PACKAGES+=dateutil time/py-dateutil time/py-dateutil2
 _SUPPORTED_PACKAGES+=dns net/py-dns net/py-dns3
 _SUPPORTED_PACKAGES+=ephem math/py-ephem math/py-ephem3
 _SUPPORTED_PACKAGES+=flup www/py-flup www/py-flup3
+_SUPPORTED_PACKAGES+=google-api-python-client www/py-google-api-python-client www/py-google-api-python-client-py3
 _SUPPORTED_PACKAGES+=jsonlib textproc/py-jsonlib textproc/py-jsonlib3
 _SUPPORTED_PACKAGES+=python-digest www/py-python-digest www/py-python3-digest
 _SUPPORTED_PACKAGES+=unittest2 devel/py-unittest2 devel/py-unittest2py3k
