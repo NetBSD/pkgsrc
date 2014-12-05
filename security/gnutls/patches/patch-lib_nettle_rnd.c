6dd0993e35c3ccbbf14c4f2fb190986f323b6e49
khorben <khorben@pkgsrc.org>
2014-12-05 12:43:24 +0000

    Packaged gnutls 3.2.20
    * Version 3.2.20 (released 2014-11-10)
    
    ** libgnutls: Removed superfluous random generator refresh on every call
    of gnutls_deinit(). That reduces load and usage of /dev/urandom.
    
    ** libgnutls: Corrected issue in export of ECC parameters to X9.63 format.
    Reported by Sean Burford [GNUTLS-SA-2014-5].
    
    ** API and ABI modifications:
    No changes since last version.

--- security/gnutls/patches/patch-lib_nettle_rnd.c
+++ security/gnutls/patches/patch-lib_nettle_rnd.c
@@ -1,20 +1,20 @@
-$NetBSD: patch-lib_nettle_rnd.c,v 1.3 2014/12/05 12:25:42 khorben Exp $
+$NetBSD: patch-lib_nettle_rnd.c,v 1.4 2014/12/05 12:43:24 khorben Exp $
 
 http://lists.gnupg.org/pipermail/gnutls-devel/2013-November/006588.html
 
---- lib/nettle/rnd.c.orig	2014-10-11 17:43:54.000000000 +0000
+--- lib/nettle/rnd.c.orig	2014-10-14 19:06:59.000000000 +0000
 +++ lib/nettle/rnd.c
-@@ -104,8 +104,7 @@ static void _rnd_get_event(struct event_
+@@ -101,8 +101,7 @@ static void _rnd_get_event(struct event_
+ 
  #ifdef HAVE_GETRUSAGE
- 	if (rusage_arg != -1) {
- 		if (getrusage(rusage_arg, &e->rusage) < 0) {
--			_gnutls_debug_log("getrusage failed: %s\n",
--				  strerror(errno));
-+			_gnutls_debug_log("getrusage failed\n");
- 		}
+ 	if (getrusage(ARG_RUSAGE, &e->rusage) < 0) {
+-		_gnutls_debug_log("getrusage failed: %s\n",
+-			  strerror(errno));
++		_gnutls_debug_log("getrusage failed\n");
  	}
  #endif
-@@ -270,8 +269,7 @@ static int do_device_source_urandom(int 
+ 
+@@ -266,8 +265,7 @@ static int do_device_source_urandom(int 
  			if (res <= 0) {
  				if (res < 0) {
  					_gnutls_debug_log
