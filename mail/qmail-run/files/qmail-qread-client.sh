ed48ab089ce81ed61d35bef2e32d9c6111d607b5
schmonz <schmonz@pkgsrc.org>
2014-12-06 22:14:27 +0000

    Placate pkglint by abolishing LOCALBASE (no functional change intended) and adding LICENSE and NO_CHECKSUM. Bump version.

--- mail/qmail-run/files/qmail-qread-client.sh
+++ mail/qmail-run/files/qmail-qread-client.sh
@@ -1,10 +1,10 @@
 #!@SH@
 #
-# $NetBSD: qmail-qread-client.sh,v 1.2 2004/08/03 08:59:24 schmonz Exp $
+# $NetBSD: qmail-qread-client.sh,v 1.3 2014/12/06 22:14:27 schmonz Exp $
 #
 # @PKGNAME@ client program to allow local non-root users to see
 # the queue. Requires that the corresponding server program be running.
 # From Steinar Haug.
 #
 
-exec @LOCALBASE@/bin/tcpclient -RHl0 -- 127.0.0.1 20025 @SH@ -c 'exec @CAT@ <&6'
+exec @UCSPI_TCP_PREFIX@/bin/tcpclient -RHl0 -- 127.0.0.1 20025 @SH@ -c 'exec @CAT@ <&6'
