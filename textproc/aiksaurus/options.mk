012325950ba85656aabffe85ccbf28b44c6a7604
dholland <dholland@pkgsrc.org>
2014-12-06 19:05:07 +0000

    oops, whitespace.

--- textproc/aiksaurus/options.mk
+++ textproc/aiksaurus/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1 2014/12/06 19:04:13 dholland Exp $
+# $NetBSD: options.mk,v 1.2 2014/12/06 19:05:07 dholland Exp $
 
-PKG_OPTIONS_VAR=        PKG_OPTIONS.aiksaurus
-PKG_SUPPORTED_OPTIONS=  gtk
+PKG_OPTIONS_VAR=	PKG_OPTIONS.aiksaurus
+PKG_SUPPORTED_OPTIONS=	gtk
 PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
@@ -9,8 +9,8 @@ PKG_SUGGESTED_OPTIONS=
 PLIST_VARS+=		gtk
 .if !empty(PKG_OPTIONS:Mgtk)
 PLIST.gtk=		yes
-CONFIGURE_ARGS+=        --with-gtk
+CONFIGURE_ARGS+=	--with-gtk
 .include "../../x11/gtk2/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=        --without-gtk
+CONFIGURE_ARGS+=	--without-gtk
 .endif
