49a7828c68c6c67e57b4e9318feb071a4b41b4ce
wiz <wiz>
2014-12-09 11:42:10 +0000

    Make libvpx a default-on option instead of a hard dependency. Based on patch by Thomas Orgis on pkgsrc-users.

--- graphics/gd/options.mk
+++ graphics/gd/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2013/09/02 21:17:42 adam Exp $
+# $NetBSD: options.mk,v 1.3 2014/12/09 11:42:10 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gd
-PKG_SUPPORTED_OPTIONS=	x11
-PKG_SUGGESTED_OPTIONS=	# empty
+PKG_SUPPORTED_OPTIONS=	libvpx x11
+PKG_SUGGESTED_OPTIONS=	libvpx
 
 .include "../../mk/bsd.options.mk"
 
@@ -12,4 +12,11 @@ CONFIGURE_ENV+=		X11PREFIX=${X11PREFIX} X11BASE=${X11BASE}
 CONFIGURE_ARGS+=	--with-xpm=${BUILDLINK_PREFIX.libXpm}
 .else
 CONFIGURE_ARGS+=	--without-xpm
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibvpx)
+.include "../../multimedia/libvpx/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-vpx=${BUILDLINK_PREFIX.libvpx}
+.else
+CONFIGURE_ARGS+=	--without-vpx
 .endif
