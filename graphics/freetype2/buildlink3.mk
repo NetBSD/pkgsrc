cbdcb9ab9b373e827f929f0915bd54b179d15782
adam <adam@pkgsrc.org>
2014-12-09 10:50:04 +0000

    Changes 2.5.4: I. IMPORTANT BUG FIXES   - A   variant  of   vulnerability  CVE-2014-2240   was  identified     (cf.  http://savannah.nongnu.org/bugs/?43661) and  fixed in  the     new CFF driver.  All users should upgrade.   - The new auto-hinter code using HarfBuzz crashed for some invalid     fonts.   - Many fixes to better protect against malformed input.
    II. IMPORTANT CHANGES
      - Full auto-hinter support of the Devanagari script.
      - Experimental auto-hinter support of the Telugu script.
      - CFF stem darkening behaviour can now be controlled at build time
        using the eight macros
          CFF_CONFIG_OPTION_DARKENING_PARAMETER_{X,Y}{1,2,3,4}    .
      - Some fields in the `FT_Bitmap'  structure have been changed from
        signed to unsigned type, which better reflects the actual usage.
        It  is also  an additional  means to  protect against  malformed
        input.
        This  change doesn't  break  the ABI;  however,  it might  cause
        compiler warnings.
    
    III. MISCELLANEOUS
      - Improvements to  the auto-hinter's algorithm to  recognize stems
        and local extrema.
      - Function `FT_Get_SubGlyph_Info' always returned an error even in
        case of success.
      - Version  2.5.1 introduced  major bugs  in  the cjk  part of  the
        auto-hinter, which are now fixed.
      - The  `FT_Sfnt_Tag'  enumeration  values  have  been  changed  to
        uppercase,  e.g.  `FT_SFNT_HEAD'.   The lowercase  variants  are
        deprecated.    This  is   for  orthogonality   with  all   other
        enumeration (and enumeration-like) values in FreeType.
      - `cmake' now supports builds of FreeType as an OS X framework and
        for iOS.
      - Improved project files for vc2010, introducing a property file.
      - The  documentation generator  for  the API  reference  has  been
        updated to produce  better HTML code (with proper  CSS).  At the
        same time, the documentation got a better structure.
      - The FT_LOAD_BITMAP_CROP flag is obsolete;  it is not used by any
        driver.
      - The  TrueType  DELTAP[123]  bytecode instructions  now  work  in
        subpixel hinting  mode as described in  the ClearType whitepaper
        (i.e., for touched points in the non-subpixel direction).
      - Many small improvements to the internal arithmetic routines.

--- graphics/freetype2/buildlink3.mk
+++ graphics/freetype2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.44 2014/03/19 10:16:33 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.45 2014/12/09 10:50:04 adam Exp $
 
 BUILDLINK_TREE+=	freetype2
 
@@ -26,9 +26,9 @@ CHECK_BUILTIN.freetype2:=	yes
 .include "../../graphics/freetype2/builtin.mk"
 CHECK_BUILTIN.freetype2:=	no
 
-.if empty(USE_BUILTIN.freetype2:M[yY][eE][sS])
-.include "../../graphics/png/buildlink3.mk"
-.endif
+.  if empty(USE_BUILTIN.freetype2:M[yY][eE][sS])
+.  include "../../graphics/png/buildlink3.mk"
+.  endif
 .endif
 
 .endif # FREETYPE2_BUILDLINK3_MK
