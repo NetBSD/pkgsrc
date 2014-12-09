473890854b244c8ece3a7a47d84d5c21b0c7b871
he <he@pkgsrc.org>
2014-12-09 10:45:51 +0000

    It can happen that times() returns ((uint32_t)-1).  If that happens, retry a limited number of times, and log the events.  Export log() from the main program for use in dbeacon_posix.cpp.
    Fix the matrix.pl script since qw() no longer acts as parenteses, so add
    some of our own where called for.
    
    Bump PKGREVISION.

--- mbone/dbeacon/patches/patch-contrib_matrix.pl
+++ mbone/dbeacon/patches/patch-contrib_matrix.pl
@@ -1,12 +1,13 @@
-$NetBSD: patch-contrib_matrix.pl,v 1.1 2014/10/06 10:04:17 he Exp $
+$NetBSD: patch-contrib_matrix.pl,v 1.2 2014/12/09 10:45:51 he Exp $
 
-Fix syntax errors flagged by newer perl.
+Fix syntax errors flagged by newer perl: qw() doesn't act as a pair
+of parentheses, so we need to add some of our own.
 Fix flag pointer, ref.
 https://lists.ubuntu.com/archives/ubuntu-motu/2010-January/006495.html
 
---- contrib/matrix.pl.orig	2013-07-05 19:12:49.000000000 +0200
-+++ contrib/matrix.pl	2014-10-06 11:46:41.000000000 +0200
-@@ -29,7 +29,7 @@
+--- contrib/matrix.pl.orig	2007-07-12 10:17:53.000000000 +0000
++++ contrib/matrix.pl
+@@ -29,7 +29,7 @@ our $history_enabled = 0;
  our $css_file;
  our $dump_update_delay = 5;	# time between each normal dumps
  				# (used to detect outdated dump files)
@@ -15,7 +16,7 @@ https://lists.ubuntu.com/archives/ubuntu-motu/2010-January/006495.html
  our $default_ssm_group = 'ff3e::beac/10000';
  our $debug = 0;
  our $matrix_link_title = 0;
-@@ -423,7 +423,7 @@
+@@ -423,7 +423,7 @@ sub start_handler {
  			$adj{$current_beacon}[RX_LOCAL] = $atts{'rxlocal'} if defined $atts{'rxlocal'};
  		}
  	} elsif ($tag eq 'asm' or $tag eq 'ssm') {
@@ -24,7 +25,21 @@ https://lists.ubuntu.com/archives/ubuntu-motu/2010-January/006495.html
  			if (defined $atts{$att}) {
  				my $index = $tag eq 'ssm' ? 2 : 1;
  
-@@ -884,7 +884,10 @@
+@@ -534,11 +534,11 @@ sub build_header {
+ 	my $whatatt = "what=$attwhat&amp;";
+ 	my $fullatt = "full=$full_matrix&amp;";
+ 
+-	my @view = qw(ttl loss delay jitter);
++	my @view = (qw(ttl loss delay jitter));
+ 	my @view_name = ('TTL', 'Loss', 'Delay', 'Jitter');
+ 	my @view_type = ('hop count', 'percentage', 'ms', 'ms');
+ 
+-	my @sources = qw(asm ssm both ssmorasm);
++	my @sources = (qw(asm ssm both ssmorasm));
+ 	my @sources_name = ('ASM', 'SSM', 'Both', 'SSM or ASM');
+ 
+ 	my $view_len = scalar(@view);
+@@ -884,7 +884,10 @@ sub render_matrix {
  				}
  			} elsif ($a eq $b) {
  				printx '<td ', $what_td, ' class="corner">&nbsp;</td>';
@@ -36,7 +51,7 @@ https://lists.ubuntu.com/archives/ubuntu-motu/2010-January/006495.html
  				printx '<td ', $what_td, ' class="noreport">N/R</td>';
  			} else {
  				printx '<td ', $what_td, ' class="blackhole">XX</td>';
-@@ -1171,7 +1174,7 @@
+@@ -1171,7 +1174,7 @@ sub store_data_one {
  	update_ttl_hist(build_history_file_path($dst_h, $src_h) . "/$tag-ttl-hist",
  		$adj{$dst}[NEIGH]{$src}[$index]{'ttl'}) if defined $adj{$dst}[NEIGH]{$src}[$index]{'ttl'};
  
@@ -45,7 +60,7 @@ https://lists.ubuntu.com/archives/ubuntu-motu/2010-January/006495.html
  		$values{$type} = $adj{$dst}[NEIGH]{$src}[$index]{$type};
  		$good++ if defined $values{$type};
  	}
-@@ -1263,7 +1266,7 @@
+@@ -1263,7 +1266,7 @@ sub storedata {
  	# Update rrd with new values
  
  	my $updatestring = 'N';
@@ -54,7 +69,7 @@ https://lists.ubuntu.com/archives/ubuntu-motu/2010-January/006495.html
  		# Store it in s and not ms
  		$values{$valuetype} = $values{$valuetype} / 1000. if $valuetype eq 'delay' or $valuetype eq 'jitter';
  		$updatestring .= ':' . $values{$valuetype};
-@@ -1644,7 +1647,7 @@
+@@ -1644,7 +1647,7 @@ sub list_graph {
  
  		my $count = 0;
  
