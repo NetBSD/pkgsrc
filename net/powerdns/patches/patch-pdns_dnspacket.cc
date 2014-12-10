06671e16722831462056ee3be9da31a00c495076
fhajny <fhajny@pkgsrc.org>
2014-12-10 14:50:08 +0000

    Update PowerDNS to 3.4.1.
    pkgsrc changes:
    - SQLite 2.x support no longer exists
    - SQLite 3.x support cannot be compiled outside the main package because
      of how symbols are distributed, so making it a compile time option
      for net/powerdns now.
    
    Too many changes since 2.9.22.5 (over 2 years ago), see the full changelog:
    
      http://doc.powerdns.com/md/changelog/
    
    Upgrade notes:
    
    - PowerDNS 3.4 comes with a mandatory database schema upgrade coming from
      any previous 3.x release.
    - PowerDNS 3.1 introduces native SQLite3 support for storing key material for
      DNSSEC in the bindbackend. With this change, support for bind+gsql-setups
      ('hybrid mode') has been dropped.
    - PowerDNS 3.0 introduces full DNSSEC support which requires changes
      to database schemas. By default, old non-DNSSEC schema is assumed.
    
    Please see the docs on upgrading for particular steps that need to be taken:
    
      http://doc.powerdns.com/md/authoritative/upgrading/

--- net/powerdns/patches/patch-pdns_dnspacket.cc
+++ net/powerdns/patches/patch-pdns_dnspacket.cc
@@ -1,13 +1,14 @@
-$NetBSD: patch-pdns_dnspacket.cc,v 1.1 2013/05/09 20:06:53 joerg Exp $
+$NetBSD: patch-pdns_dnspacket.cc,v 1.2 2014/12/10 14:50:09 fhajny Exp $
 
---- pdns/dnspacket.cc.orig	2013-05-09 15:25:33.000000000 +0000
+Resolve boost symbol ambiguity.
+--- pdns/dnspacket.cc.orig	2014-10-30 10:18:22.000000000 +0000
 +++ pdns/dnspacket.cc
-@@ -335,7 +335,7 @@ void DNSPacket::wrapup(void)
- 	}
- 	if(pos->content.empty())  // empty contents confuse the MOADNS setup
- 	  pos->content=".";
--	shared_ptr<DNSRecordContent> drc(DNSRecordContent::mastermake(pos->qtype.getCode(), 1, pos->content)); 
-+	boost::shared_ptr<DNSRecordContent> drc(DNSRecordContent::mastermake(pos->qtype.getCode(), 1, pos->content)); 
- 	drc->toPacket(pw);
-       }
-       if(!opts.empty())
+@@ -312,7 +312,7 @@ void DNSPacket::wrapup()
+           pos->content=".";
+         
+         pw.startRecord(pos->qname, pos->qtype.getCode(), pos->ttl, pos->qclass, (DNSPacketWriter::Place)pos->d_place); 
+-        shared_ptr<DNSRecordContent> drc(DNSRecordContent::mastermake(pos->qtype.getCode(), 1, pos->content)); 
++        boost::shared_ptr<DNSRecordContent> drc(DNSRecordContent::mastermake(pos->qtype.getCode(), 1, pos->content)); 
+               drc->toPacket(pw);
+         if(pw.size() + 20U > (d_tcp ? 65535 : getMaxReplyLen())) { // 20 = room for EDNS0
+           pw.rollback();
