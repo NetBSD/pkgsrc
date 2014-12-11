cc3857b786f114265d8c31dd7332d759a02bff0c
szptvlfn <szptvlfn@pkgsrc.org>
2014-12-11 12:24:45 +0000

    Update to 1.1.3.1
    ChangeLog:
    1.1.3.1 ( from https://github.com/snoyberg/conduit/tree/master/resourcet )
    Add back support for exceptions 0.5
    
    ## 1.1.3
    Provide the `withEx` function to interact nicely with the exceptions package.

--- devel/hs-resourcet/buildlink3.mk
+++ devel/hs-resourcet/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/11/23 12:52:32 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/12/11 12:24:45 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-resourcet
 
 .if !defined(HS_RESOURCET_BUILDLINK3_MK)
 HS_RESOURCET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=1.1.2
-BUILDLINK_ABI_DEPENDS.hs-resourcet+=	hs-resourcet>=1.1.2.3nb1
+BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=1.1.3
+BUILDLINK_ABI_DEPENDS.hs-resourcet+=	hs-resourcet>=1.1.3.1
 BUILDLINK_PKGSRCDIR.hs-resourcet?=	../../devel/hs-resourcet
 
 .include "../../devel/hs-exceptions/buildlink3.mk"
