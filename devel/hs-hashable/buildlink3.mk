d141e71ed51e06bed01bae938cb166cf77833d13
szptvlfn <szptvlfn@pkgsrc.org>
2014-12-12 22:03:04 +0000

    Update to 1.2.3.0
    CHANGES:
    ## Version 1.2.3.0
    
     * Silence integer literal overflow warning
    
     * Add support for GHC 7.10 `integer-gmp2` & `Natural`
    
     * Add instance for Data.Void
    
     * Make the SSE .cabal flags manual
    
     * Add an upper bound on bytestring

--- devel/hs-hashable/buildlink3.mk
+++ devel/hs-hashable/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/09/13 22:23:28 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/12/12 22:03:04 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-hashable
 
 .if !defined(HS_HASHABLE_BUILDLINK3_MK)
 HS_HASHABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.2.2
-BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.2.2.0nb2
+BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.2.3
+BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.2.3.0
 BUILDLINK_PKGSRCDIR.hs-hashable?=	../../devel/hs-hashable
 
 .include "../../devel/hs-text/buildlink3.mk"
