f5a756a2eea085a622373e72f7408d3c89622e4e
szptvlfn <szptvlfn@pkgsrc.org>
2014-12-12 22:04:21 +0000

    Bump PKGREVISION for hs-text-1.2.0.2 || hs-hashable-1.2.3.0

--- textproc/hs-polyparse/buildlink3.mk
+++ textproc/hs-polyparse/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/09/13 22:23:28 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/12/12 22:04:23 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-polyparse
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-polyparse
 HS_POLYPARSE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-polyparse+=	hs-polyparse>=1.9
-BUILDLINK_ABI_DEPENDS.hs-polyparse+=	hs-polyparse>=1.9nb2
+BUILDLINK_ABI_DEPENDS.hs-polyparse+=	hs-polyparse>=1.9nb3
 BUILDLINK_PKGSRCDIR.hs-polyparse?=	../../textproc/hs-polyparse
 
 .include "../../devel/hs-text/buildlink3.mk"
--- textproc/hs-tagsoup/buildlink3.mk
+++ textproc/hs-tagsoup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/09/13 22:23:28 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/12/12 22:04:23 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-tagsoup
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-tagsoup
 HS_TAGSOUP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.1
-BUILDLINK_ABI_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.1nb2
+BUILDLINK_ABI_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.1nb3
 BUILDLINK_PKGSRCDIR.hs-tagsoup?=	../../textproc/hs-tagsoup
 
 .include "../../devel/hs-text/buildlink3.mk"
