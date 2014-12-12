615938228adc460f5e856627bbe0bee4aa12a9ba
mef <mef@pkgsrc.org>
2014-12-12 08:32:42 +0000

    Update 1.3.0 to 1.4.0 --------------------- New in CSSC-1.4.0, 2014-07-11
             * New stable release, almost identical to the test release
               1.3.2 - the only changes are version numbering and updates
               to the ChangeLog and NEWS files.  The previous stable
               release was 1.3.0, so the following two sections of this
               file set out what has changed since 1.3.0.
    
    New in CSSC-1.3.2, 2014-06-30
    
            * This release is based on updated versions of gnulib and of
              the googletest unit test framework (the test framework is of
              course not linked into the installed CSSC binaries, it is
              just used for unit tests).
    
            * This release uses a new tar format.  Up to now we have used
              the v7 Unix tar format, but this does not cope correctly
              with filenames longer than 99 characters, and we now have
              one of those.  We now use the the ustar format defined by
              POSIX 1003.1-1988.  If you need to unpack a CSSC
              distrubition with a version of tar which only deals with the
              v7 format, unpack the distribution somewhere else, rename or
              delete the long files (they are not essential to the build
              process) and re-pack it. This new tar format will likely
              also be used for all future releases.
    
            * Fix Savannah bug 33633, prs -d":GB:" coredumps when
              expanding %I%.
    
    New in CSSC-1.3.1, 2011-05-10
    
            * prs now prints ignored deltas when expanding the :DI:
              keyword.  The expansion of :DI: varies between SCCS
              implementations, as described in the manual.  This partly
              fixes Savannah bug #33154: SCCS versions vary wrt ignored
              deltas (that is, this change covers only the prs binary).
    
            * Fix Savannah bug bug #33153: "prs" includes "AUTO NULL
              DELTA"s.  "prs -r" will no longer print any additional
              deltas that happen to share the same timestamp with the last
              matching delta.
    
            * If the SID identified by "prs -r" is not found, prs now
              exits with status 1, not 0.  This fixes SourceForge bug
              #1300722.
    
    	* The headers produced by sccsdiff have changed.  A different
              header is used when the -p option is specified and when
              it's not.  The new behaviour matches OpenSolaris 2009.06.
    
    	* The ordering of the ouput of "sccs unedit" is now different;
              the new order matches OpenSolaris 2009.06.
    
    	* "get -L" will now send the ID of the gotten SID to stderr,
              which better matches OpenSolaris 2009.06.  The indentation
              of the delta summary has also been adjusted.
    
    	* To pass an empty list of MRs to admin, you now have to
              provide an argument (as opposed to omitting it).  For
              example, "admin -i/dev/null -fv -m' ' s.foo".  This aligns
              better with OpenSolaris 2009.06 but it is a
              backward-incompatible change.

--- devel/cssc/patches/patch-unit-tests_googletest_include_gtest_internal_gtest-port.h
+++ devel/cssc/patches/patch-unit-tests_googletest_include_gtest_internal_gtest-port.h
@@ -1,29 +1,12 @@
-$NetBSD: patch-unit-tests_googletest_include_gtest_internal_gtest-port.h,v 1.3 2013/05/09 20:05:35 joerg Exp $
+$NetBSD: patch-unit-tests_googletest_include_gtest_internal_gtest-port.h,v 1.4 2014/12/12 08:32:42 mef Exp $
 
---- unit-tests/googletest/include/gtest/internal/gtest-port.h.orig	2013-05-07 19:58:35.000000000 +0000
-+++ unit-tests/googletest/include/gtest/internal/gtest-port.h
-@@ -173,6 +173,7 @@
+--- unit-tests/googletest/include/gtest/internal/gtest-port.h.orig	2014-07-01 06:33:31.000000000 +0900
++++ unit-tests/googletest/include/gtest/internal/gtest-port.h	2014-12-12 16:46:31.000000000 +0900
+@@ -261,6 +261,7 @@
  #include <stdlib.h>
  #include <stdio.h>
  #include <string.h>
 +#include <unistd.h>
  #ifndef _WIN32_WCE
- #include <sys/stat.h>
- #endif  // !_WIN32_WCE
-@@ -432,6 +432,16 @@
- #define BOOST_TR1_DETAIL_CONFIG_HPP_INCLUDED
- #include <tuple>
- 
-+# elif __cplusplus >= 201103L
-+#  include <tuple>
-+namespace std {
-+  namespace tr1 {
-+    using ::std::tuple;
-+    using ::std::tuple_element;
-+    using ::std::tuple_size;
-+    using ::std::get;
-+  }
-+}
- #elif defined(__GNUC__) && (GTEST_GCC_VER_ >= 40000)
- // GCC 4.0+ implements tr1/tuple in the <tr1/tuple> header.  This does
- // not conform to the TR1 spec, which requires the header to be <tuple>.
+ # include <sys/types.h>
+ # include <sys/stat.h>
