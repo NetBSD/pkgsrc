1cecd65b133a487fd94f42545d16cbde60b0a836
szptvlfn <szptvlfn>
2014-12-12 22:55:24 +0000

    Bump PKGREVISION for hs-text-1.2.0.3

--- math/hs-semigroups/buildlink3.mk
+++ math/hs-semigroups/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/12/12 22:04:22 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/12/12 22:55:25 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-semigroups
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-semigroups
 HS_SEMIGROUPS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.15.3
-BUILDLINK_ABI_DEPENDS.hs-semigroups+=	hs-semigroups>=0.15.3nb1
+BUILDLINK_ABI_DEPENDS.hs-semigroups+=	hs-semigroups>=0.15.3nb2
 BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../math/hs-semigroups
 
 .include "../../devel/hs-hashable/buildlink3.mk"
