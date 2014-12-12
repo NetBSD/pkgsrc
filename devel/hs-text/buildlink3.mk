6b11cc15f99bb0a36ce3349bb678ff247238182a
szptvlfn <szptvlfn@pkgsrc.org>
2014-12-12 22:02:19 +0000

    Update to 1.2.0.2
    changelog:
    1.2.0.2
    
    * Bumped lower bound on deepseq to 1.4 for compatibility with the
      upcoming GHC 7.10
    
    1.2.0.1
    
    * Fixed a buffer overflow in rendering of large Integers
      (https://github.com/bos/text/issues/99)

--- devel/hs-text/buildlink3.mk
+++ devel/hs-text/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/09/13 22:03:04 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/12/12 22:02:19 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-text
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-text
 HS_TEXT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=1.2.0
-BUILDLINK_ABI_DEPENDS.hs-text+=	hs-text>=1.2.0.0
+BUILDLINK_ABI_DEPENDS.hs-text+=	hs-text>=1.2.0.2
 BUILDLINK_PKGSRCDIR.hs-text?=	../../devel/hs-text
 .endif	# HS_TEXT_BUILDLINK3_MK
 
