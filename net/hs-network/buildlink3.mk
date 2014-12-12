d4dd0fc1a2cedf504ded8fd6dfe21d7f2e1a457d
szptvlfn <szptvlfn@pkgsrc.org>
2014-12-12 22:04:21 +0000

    Bump PKGREVISION for hs-text-1.2.0.2 || hs-hashable-1.2.3.0

--- net/hs-network/buildlink3.mk
+++ net/hs-network/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2014/11/23 12:52:33 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2014/12/12 22:04:22 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-network
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-network
 HS_NETWORK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=2.5.0
-BUILDLINK_ABI_DEPENDS.hs-network+=	hs-network>=2.5.0.0nb5
+BUILDLINK_ABI_DEPENDS.hs-network+=	hs-network>=2.5.0.0nb6
 BUILDLINK_PKGSRCDIR.hs-network?=	../../net/hs-network
 
 .include "../../textproc/hs-parsec/buildlink3.mk"
