fd2a11e36df695c4454d66099576fb17897704b5
szptvlfn <szptvlfn@pkgsrc.org>
2014-12-12 22:53:35 +0000

    Update to 1.2.0.3
    changelog:
    1.2.0.3
    
    * Update formatRealFloat to correspond to the definition in versions
      of base newer than 4.5 (https://github.com/bos/text/issues/105)

--- devel/hs-text/buildlink3.mk
+++ devel/hs-text/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2014/12/12 22:02:19 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2014/12/12 22:53:35 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-text
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-text
 HS_TEXT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=1.2.0
-BUILDLINK_ABI_DEPENDS.hs-text+=	hs-text>=1.2.0.2
+BUILDLINK_ABI_DEPENDS.hs-text+=	hs-text>=1.2.0.3
 BUILDLINK_PKGSRCDIR.hs-text?=	../../devel/hs-text
 .endif	# HS_TEXT_BUILDLINK3_MK
 
