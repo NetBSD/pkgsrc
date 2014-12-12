f3da8cbbd32e0db6aa77180eaf1c54b4ced88133
szptvlfn <szptvlfn@pkgsrc.org>
2014-12-12 22:55:24 +0000

    Bump PKGREVISION for hs-text-1.2.0.3

--- math/hs-nats/buildlink3.mk
+++ math/hs-nats/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2014/12/12 22:04:22 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2014/12/12 22:55:25 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-nats
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-nats
 HS_NATS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-nats+=	hs-nats>=0.2
-BUILDLINK_ABI_DEPENDS.hs-nats+=	hs-nats>=0.2nb3
+BUILDLINK_ABI_DEPENDS.hs-nats+=	hs-nats>=0.2nb4
 BUILDLINK_PKGSRCDIR.hs-nats?=	../../math/hs-nats
 
 .include "../../devel/hs-hashable/buildlink3.mk"
