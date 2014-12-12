8ec92f6832f2238b0b01c568f74ff7a16c5e5d4d
wiz <wiz@pkgsrc.org>
2014-12-12 09:41:40 +0000

    Update to 1.4, ok bsiegert:
    Today we announce Go 1.4, the fifth major stable release of Go,
    arriving six months after our previous major release Go 1.3. It
    contains a small language change, support for more operating systems
    and processor architectures, and improvements to the tool chain
    and libraries. As always, Go 1.4 keeps the promise of compatibility,
    and almost everything will continue to compile and run without
    change when moved to 1.4. For the full details, see the Go 1.4
    release notes.
    
    The most notable new feature in this release is official support
    for Android. Using the support in the core and the libraries in
    the golang.org/x/mobile repository, it is now possible to write
    simple Android apps using only Go code. At this stage, the support
    libraries are still nascent and under heavy development. Early
    adopters should expect a bumpy ride, but we welcome the community
    to get involved.
    
    The language change is a tweak to the syntax of for-range loops.
    You may now write "for range s {" to loop over each item from s,
    without having to assign the value, loop index, or map key. See
    the release notes for details.
    
    The go command has a new subcommand, go generate, to automate the
    running of tools to generate source code before compilation. For
    example, it can be used to automate the generation of String methods
    for typed constants using the new stringer tool. For more information,
    see the design document.
    
    Most programs will run about the same speed or slightly faster in
    1.4 than in 1.3; some will be slightly slower. There are many
    changes, making it hard to be precise about what to expect. See
    the release notes for more discussion.
    
    And, of course, there are many more improvements and bug fixes.
    
    In case you missed it, a few weeks ago the sub-repositories were
    moved to new locations. For example, the go.tools packages are now
    imported from "golang.org/x/tools". See the announcement post for
    details.
    
    This release also coincides with the project's move from Mercurial
    to Git (for source control), Rietveld to Gerrit (for code review),
    and Google Code to Github (for issue tracking and wiki). The move
    affects the core Go repository and its sub-repositories. You can
    find the canonical Git repositories at go.googlesource.com, and
    the issue tracker and wiki at the golang/go GitHub repo.

--- lang/go/patches/patch-src_crypto_x509_root__unix.go
+++ lang/go/patches/patch-src_crypto_x509_root__unix.go
@@ -1,9 +1,10 @@
-$NetBSD: patch-src_pkg_crypto_x509_root__unix.go,v 1.1 2014/10/11 16:41:47 khorben Exp $
+$NetBSD: patch-src_crypto_x509_root__unix.go,v 1.1 2014/12/12 09:41:40 wiz Exp $
 
 Also look for the right path for the SSL certificate repository on NetBSD.
+https://github.com/golang/go/issues/9285
 
---- src/pkg/crypto/x509/root_unix.go.orig	2014-10-08 19:16:18.000000000 +0000
-+++ src/pkg/crypto/x509/root_unix.go
+--- src/crypto/x509/root_unix.go.orig	2014-12-11 01:18:10.000000000 +0000
++++ src/crypto/x509/root_unix.go
 @@ -13,6 +13,7 @@ var certFiles = []string{
  	"/etc/ssl/certs/ca-certificates.crt",     // Debian/Ubuntu/Gentoo etc.
  	"/etc/pki/tls/certs/ca-bundle.crt",       // Fedora/RHEL
@@ -11,4 +12,4 @@ Also look for the right path for the SSL certificate repository on NetBSD.
 +	"/etc/openssl/certs/ca-certificates.crt", // NetBSD
  	"/etc/ssl/cert.pem",                      // OpenBSD
  	"/usr/local/share/certs/ca-root-nss.crt", // FreeBSD/DragonFly
- }
+ 	"/etc/pki/tls/cacert.pem",                // OpenELEC
