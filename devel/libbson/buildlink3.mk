027ed401e01db0a0ffaf6d6cbc91a49ae9abaf50
fhajny <fhajny@pkgsrc.org>
2014-12-12 11:29:32 +0000

    Update libbson to 1.0.2. - bson_init_from_json supports top level arrays - fixes for bson_strerror_r - fix for timeouts on OS X - house cleaning for various integer types

--- devel/libbson/buildlink3.mk
+++ devel/libbson/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/07/18 10:31:48 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/12/12 11:29:32 fhajny Exp $
 
 BUILDLINK_TREE+=	libbson
 
 .if !defined(LIBBSON_BUILDLINK3_MK)
 LIBBSON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libbson+=	libbson>=0.8.4
+BUILDLINK_API_DEPENDS.libbson+=	libbson>=1.0.0
 BUILDLINK_PKGSRCDIR.libbson?=	../../devel/libbson
 .endif	# LIBBSON_BUILDLINK3_MK
 
