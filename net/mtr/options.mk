c4ed50e514f54a7cf957030d7d68c5c7a110ff31
wiz <wiz@pkgsrc.org>
2014-12-14 15:57:44 +0000

    Remove now unnecessary glib option. Ride update.

--- net/mtr/options.mk
+++ net/mtr/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.5 2014/11/19 09:01:51 wiz Exp $
+# $NetBSD: options.mk,v 1.6 2014/12/14 15:57:44 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mtr
-PKG_SUPPORTED_OPTIONS=	gtk glib inet6
-PKG_SUGGESTED_OPTIONS=	glib inet6
+PKG_SUPPORTED_OPTIONS=	gtk inet6
+PKG_SUGGESTED_OPTIONS=	inet6
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,12 +10,6 @@ PKG_SUGGESTED_OPTIONS=	glib inet6
 .include "../../x11/gtk2/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-gtk
-.endif
-
-.if !empty(PKG_OPTIONS:Mglib) || !empty(PKG_OPTIONS:Mgtk)
-.include "../../devel/glib2/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-glib
 .endif
 
 .if !empty(PKG_OPTIONS:Minet6)
