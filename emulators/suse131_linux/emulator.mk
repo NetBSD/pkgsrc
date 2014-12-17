ada3af575be889b86d7e6f8536f2120d6abe8e01
rodent <rodent>
2014-12-17 01:40:51 +0000

    Added the suse131 gstreamer and sqlite3 versions and their 32-bit variants. OK'd by pkgsrc-pmc@ in private mail. This fixes build failure with wip/skype4.

--- emulators/suse131_linux/emulator.mk
+++ emulators/suse131_linux/emulator.mk
@@ -1,4 +1,4 @@
-# $NetBSD: emulator.mk,v 1.4 2014/04/17 00:08:34 wiz Exp $
+# $NetBSD: emulator.mk,v 1.5 2014/12/17 01:40:51 rodent Exp $
 #
 # This file is included by linux-suse.mk in the emulator framework.
 #
@@ -35,6 +35,7 @@ DEPENDS_suse-13.1.expat?=	suse_expat${_SUSE_VERSION_REQD}:../../emulators/suse13
 DEPENDS_suse-13.1.fontconfig?=	suse_fontconfig${_SUSE_VERSION_REQD}:../../emulators/suse131_fontconfig
 DEPENDS_suse-13.1.freetype2?=	suse_freetype2${_SUSE_VERSION_REQD}:../../emulators/suse131_freetype2
 DEPENDS_suse-13.1.glx?=		suse_glx${_SUSE_VERSION_REQD}:../../emulators/suse131_glx
+DEPENDS_suse-13.1.gstreamer?=	suse_gstreamer${_SUSE_VERSION_REQD}:../../emulators/suse131_gstreamer
 DEPENDS_suse-13.1.gtk2?=	suse_gtk2${_SUSE_VERSION_REQD}:../../emulators/suse131_gtk2
 DEPENDS_suse-13.1.jpeg?=	suse_libjpeg${_SUSE_VERSION_REQD}:../../emulators/suse131_libjpeg
 DEPENDS_suse-13.1.krb5?=	suse_krb5${_SUSE_VERSION_REQD}:../../emulators/suse131_krb5
@@ -58,6 +59,7 @@ DEPENDS_suse-13.1.png?=		suse_libpng${_SUSE_VERSION_REQD}:../../emulators/suse13
 DEPENDS_suse-13.1.pulseaudio?=	suse_pulseaudio${_SUSE_VERSION_REQD}:../../emulators/suse131_pulseaudio
 DEPENDS_suse-13.1.qt4?=		suse_qt4${_SUSE_VERSION_REQD}:../../emulators/suse131_qt4
 DEPENDS_suse-13.1.slang?=	suse_slang${_SUSE_VERSION_REQD}:../../emulators/suse131_slang
+DEPENDS_suse-13.1.sqlite3?=	suse_sqlite3${_SUSE_VERSION_REQD}:../../emulators/suse131_sqlite3
 DEPENDS_suse-13.1.tiff?=	suse_libtiff${_SUSE_VERSION_REQD}:../../emulators/suse131_libtiff
 DEPENDS_suse-13.1.x11?=		suse_x11${_SUSE_VERSION_REQD}:../../emulators/suse131_x11
 DEPENDS_suse-13.1.xml2?=	suse_libxml2${_SUSE_VERSION_REQD}:../../emulators/suse131_libxml2
