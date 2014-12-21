1ba0c42c9b8d45a223779f2457e51ca2b9cf2165
obache <obache@pkgsrc.org>
2014-12-21 09:11:40 +0000

    broken configure script, "ssl" option will not be switched with "--enable-ssl", unrecognized "--with-ssl" option instead.

--- net/tnftp/options.mk
+++ net/tnftp/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2014/11/03 04:07:36 obache Exp $
+# $NetBSD: options.mk,v 1.7 2014/12/21 09:11:40 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tnftp
 PKG_SUPPORTED_OPTIONS=		inet6 ssl
@@ -30,9 +30,9 @@ CONFIGURE_ARGS+=	--disable-ipv6
 .endif
 
 .if !empty(PKG_OPTIONS:Mssl)
-CONFIGURE_ARGS+=	--enable-ssl
+CONFIGURE_ARGS+=	--with-ssl=yes
 .include "../../security/openssl/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-openssl=${SSLBASE}
 .else
-CONFIGURE_ARGS+=	--disable-ssl
+CONFIGURE_ARGS+=	--with-ssl=no
 .endif
