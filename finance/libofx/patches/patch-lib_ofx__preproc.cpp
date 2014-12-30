c6c56b3090230ff4792b17401bc86bc2047749b7
wiz <wiz>
2014-12-30 13:13:21 +0000

    Update to 0.9.10:
    2014-09-12, LibOFX 0.9.10:
    
            - Fix missing balance dates (ledger_balance_date, available_balance_date)
    
    2013-09-09, LibOFX 0.9.9:
    
            - Fix missing increment of SO_CURRENT number in libofx-0.9.6
            to avoid crashes when mixing this with older applications
    
            - bug#41: Fix a crash that occurs while importing a possibly
            invalid OFX file. Patch by Cristian Onet.
    
    2013-04-17, LibOFX 0.9.8:
    
            - Fix errorneous reading of files with empty lines
    
            - bug#24: Fix build error on solaris.
    
            - bug#40: Fix compiler warnings of OFX clang
    
    2013-04-11, LibOFX 0.9.7:
    
            - Fix errorneous reading of files with long lines
    
    2013-03-30, LibOFX 0.9.6:
    
            - Extend buffer for TRANSACTION_NAME to 96 bytes due to UTF-8
            multibyte characters.
    
            - Patch by Geert Janssens to fix typo in info message
    
            - Patch by Geert Janssens to allow lines longer than 1024
            caracters
    
            - Fix treatment of empty date strings: Must return a zero date
            instead of a bogus one.
    
            - Replace unlink() with remove() to fix compile problems with
            gcc 4.7
    
            - Make configure fail if gengetopt is missing and generated
            files are not present (as is the case when we checkout from
            git)
    
            - Fix garbled character encoding for UTF-8 encoded OFX 2.0
            (XML) files.  Sadly, because of a bug in OpenSP with xml
            decoding this currently means that iso-8859-1 OFX-2.0 files
            will still not be properly decoded.
    
    LibOFX 0.9.5:
    	Benoit Grégoire <benoitg@coeus.ca>
     		- Workaround OFX files specifying invalid encoding values
     		   (specifically:  UNICODE and CP1252).  This should fix most encoding
     		   problems reported.
    		- Look for DTD in source directory (simplifies developpement)
    	Christian Stimming <christian@cstimming.de>
    		- Add minor argument checking
    		- Expose field: OfxSecurityData::fiid in API
    		- Make string arguments a const reference where appropriate
    		- Add configure check for help2man tool
        Ryan Donlan <bdonlan@gmail.com>
    		- Improve build system and autoconf 2.68 compatibility
    		- Improve generated man pages
    		- Use GCC's -fvisibility=hidden to hide internal symbols from external
    		   users.
    
    LibOFX 0.9.4:
        - Patch to fix segfault on some malformed date formats.  Inspired by Zach's patch on launchpad.
        - Packages-oriented changes:
            - LibOFX will now look for DTDs in env variable OFX_DTD_PATH (if present).
            - Better handling of paths (tolerates trailing path separator, or lack thereof)
            - No longer ignore return value of mkstemp()
            - Integrate all changes in Ubuntu's package that weren't already upstream
            - Move to LibXML++ 2.6, as 1.0 is deprecated
    	- Add generated man pages with html2man
    
    LibOFX 0.9.3:
    	- Fix segfault on some files containing missing closing tags (bug #2969817)
    
    LibOFX 0.9.2:
    	- Win32: Add gnucash patch that looks up the dtd installation directory from the current executable's location.
    	- Apply patch by Geert Janssens to fix a crash on invalid date format
    	- Apply patch by ajseward with some additional fixes to allow wraping the library in python.
    	- Apply patch by Thomas Baumgart which fixes bug #5 (Transaction posting date off by one)
    	- Apply patch by Bill Nottingham <notting@redhat.com>  with various C++ include fixes for building with recent compilers.

---
+++ finance/libofx/patches/patch-lib_ofx__preproc.cpp
@@ -0,0 +1,15 @@
+$NetBSD: patch-lib_ofx__preproc.cpp,v 1.1 2014/12/30 13:13:21 wiz Exp $
+
+Fix build on NetBSD where iconv has a different prototype.
+
+--- lib/ofx_preproc.cpp.orig	2014-09-12 19:26:30.000000000 +0000
++++ lib/ofx_preproc.cpp
+@@ -310,7 +310,7 @@ int ofx_proc_file(LibofxContextPtr ctx, 
+             size_t outbytesleft = inbytesleft * 2 - 1;
+             iconv_buffer = (char*) malloc (inbytesleft * 2);
+             memset(iconv_buffer, 0, inbytesleft * 2);
+-#if defined(OS_WIN32) || defined(__sun)
++#if defined(OS_WIN32) || defined(__sun) || defined(__NetBSD__)
+             const char * inchar = (const char *)s_buffer.c_str();
+ #else
+             char * inchar = (char *)s_buffer.c_str();
