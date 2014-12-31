69691610b96425b501b16958e04f2eb077dd2496
adam <adam>
2014-12-31 10:06:57 +0000

    Changes 5.7.3: Many many bug fixes and minor improvements
    snmpd, snmptrapd and apps:
      - Patch 2525: from Ryan Steinmetz: Fix argument length parsing of the
        host resources mib
      - Make ENV_SEPARATOR_CHAR configurable
      - SECURITY: a denial of service attack vector was discovered on
        the linux implementation of the ICMP-MIB.  This release fixes
        this bug and all users are encouraged to update their SNMP
        agent if they make use of the ICMP-MIB table objects.
    
    perl:
      - BUG: 2402: Add support for SNMPv3 traps
    
    Windows:
      - Port batch build infrastructure to Visual Studio 2010 and later
        From Visual Studio 2010 on it is no longer possible to specify
        include or library directories globally - these have to be
        specified per project. Hence two additional menu entries in
        build.bat that allow to specify these directories.
      - Patch from Bart Van Assche to improve cygwin building

--- net/net-snmp/patches/patch-agent_mibgroup_host_data__access_swrun__kinfo.c
+++ net/net-snmp/patches/patch-agent_mibgroup_host_data__access_swrun__kinfo.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-agent_mibgroup_host_data__access_swrun__kinfo.c,v 1.2 2013/03/17 00:15:29 gdt Exp $
+$NetBSD: patch-agent_mibgroup_host_data__access_swrun__kinfo.c,v 1.3 2014/12/31 10:06:57 adam Exp $
 
---- agent/mibgroup/host/data_access/swrun_kinfo.c.orig	2012-10-09 22:28:58.000000000 +0000
+--- agent/mibgroup/host/data_access/swrun_kinfo.c.orig	2014-12-08 20:23:22.000000000 +0000
 +++ agent/mibgroup/host/data_access/swrun_kinfo.c
 @@ -5,6 +5,10 @@
   *
-  * NB: later FreeBSD uses a different kinfo_proc structure
+  * NB: later FreeBSD and OpenBSD use different kinfo_proc structures
   */
 +#ifdef __NetBSD__
 +#define _KMEMUSER
