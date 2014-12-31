1f5c86c1f5da76a557fafaa2df60b546a3c1a55b
adam <adam>
2014-12-31 10:06:57 +0000

    Changes 5.7.3: Many many bug fixes and minor improvements
    snmpd, snmptrapd and apps:
      - Patch 2525: from Ryan Steinmetz: Fix argument length parsing of the
        host resources mib
      - Make ENV_SEPARATOR_CHAR configurable
      - SECURITY: a denial of service attack vector was discovered on
        the linux implementation of the ICMP-MIB.  This release fixes
        this bug and all users are encouraged to update their SNMP
        agent if they make use of the ICMP-MIB table objects.
    
    perl:
      - BUG: 2402: Add support for SNMPv3 traps
    
    Windows:
      - Port batch build infrastructure to Visual Studio 2010 and later
        From Visual Studio 2010 on it is no longer possible to specify
        include or library directories globally - these have to be
        specified per project. Hence two additional menu entries in
        build.bat that allow to specify these directories.
      - Patch from Bart Van Assche to improve cygwin building

--- net/net-snmp/patches/patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c
+++ net/net-snmp/patches/patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c
@@ -1,7 +1,7 @@
-$NetBSD: patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c,v 1.5 2013/04/14 19:45:31 joerg Exp $
+$NetBSD: patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c,v 1.6 2014/12/31 10:06:57 adam Exp $
 
---- agent/mibgroup/hardware/cpu/cpu_sysctl.c.orig	2012-10-09 18:28:58.000000000 -0400
-+++ agent/mibgroup/hardware/cpu/cpu_sysctl.c	2013-04-04 20:55:47.000000000 -0400
+--- agent/mibgroup/hardware/cpu/cpu_sysctl.c.orig	2014-12-08 20:23:22.000000000 +0000
++++ agent/mibgroup/hardware/cpu/cpu_sysctl.c
 @@ -10,6 +10,7 @@
  
  #include <stdlib.h>
@@ -20,7 +20,7 @@ $NetBSD: patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c,v 1.5 2013/04/14 19:45:
  #ifdef HAVE_VM_VM_PARAM_H
  #include <vm/vm_param.h>
  #endif
-@@ -43,7 +47,7 @@
+@@ -43,7 +47,7 @@ void init_cpu_sysctl( void ) {
      int               i, n;
      size_t            siz;
      int               ncpu_mib[]  = { CTL_HW, HW_NCPU };
@@ -29,7 +29,7 @@ $NetBSD: patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c,v 1.5 2013/04/14 19:45:
      int               model_mib[] = { CTL_HW, HW_MODEL };
  #endif
      char              descr[ SNMP_MAXBUF ];
-@@ -51,14 +55,17 @@
+@@ -51,14 +55,17 @@ void init_cpu_sysctl( void ) {
      strcpy(cpu->name, "Overall CPU statistics");
  
      siz = sizeof(n);
@@ -51,10 +51,10 @@ $NetBSD: patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c,v 1.5 2013/04/14 19:45:
  #endif
      for ( i = 0; i < n; i++ ) {
          cpu = netsnmp_cpu_get_byIdx( i, 1 );
-@@ -174,9 +181,11 @@
+@@ -190,9 +197,11 @@ int netsnmp_cpu_arch_load( netsnmp_cache
      netsnmp_cpu_info *cpu = netsnmp_cpu_get_byIdx( -1, 0 );
  
- #if (defined(__FreeBSD__) || defined(__NetBSD__))
+ #if defined(__FreeBSD__) || defined(__NetBSD__)
 -    sysctlbyname("kern.cp_time", cpu_stats, &cpu_size, NULL, 0);
 +    if (sysctlbyname("kern.cp_time", cpu_stats, &cpu_size, NULL, 0) == -1)
 +	snmp_log(LOG_ERR, "sysctl kern.cp_time failed (errno %d)\n", errno);
@@ -63,22 +63,22 @@ $NetBSD: patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c,v 1.5 2013/04/14 19:45:
 +    if (sysctl(cpu_mib, 2,  cpu_stats, &cpu_size, NULL, 0) == -1)
 +	snmp_log(LOG_ERR, "sysctl kern.cpu failed (errno %d)\n", errno);
  #endif
-     cpu->user_ticks = (unsigned long long)cpu_stats[CP_USER];
-     cpu->nice_ticks = (unsigned long long)cpu_stats[CP_NICE];
-@@ -190,7 +199,8 @@
+     cpu->user_ticks = cpu_stats[CP_USER];
+     cpu->nice_ticks = cpu_stats[CP_NICE];
+@@ -206,7 +215,8 @@ int netsnmp_cpu_arch_load( netsnmp_cache
           * Interrupt/Context Switch statistics
           *   XXX - Do these really belong here ?
           */
 -    sysctl(mem_mib, 2, &mem_stats, &mem_size, NULL, 0);
 +    if (sysctl(mem_mib, 2, &mem_stats, &mem_size, NULL, 0) == -1)
 +	snmp_log(LOG_ERR, "sysctl vm.vm_meter failed (errno %d)\n", errno);
-     cpu->nInterrupts  = (unsigned long long)mem_stats.NS_VM_INTR;
-     cpu->nCtxSwitches = (unsigned long long)mem_stats.NS_VM_SWTCH;
-     cpu->swapIn       = (unsigned long long)mem_stats.NS_VM_SWAPIN;
-@@ -201,11 +211,37 @@
- #ifdef NS_VM_PAGEOUT
-     cpu->pageOut      = (unsigned long long)mem_stats.NS_VM_PAGEOUT;
+     cpu->nInterrupts  = mem_stats.NS_VM_INTR;
+     cpu->nCtxSwitches = mem_stats.NS_VM_SWTCH;
+     cpu->swapIn       = mem_stats.NS_VM_SWAPIN;
+@@ -218,6 +228,32 @@ int netsnmp_cpu_arch_load( netsnmp_cache
+     cpu->pageOut      = mem_stats.NS_VM_PAGEOUT;
  #endif
+ 
 +#if defined(__NetBSD__)
 +    {
 +	NETSNMP_CPU_STATS *ncpu_stats;
@@ -104,13 +104,17 @@ $NetBSD: patch-agent_mibgroup_hardware_cpu_cpu__sysctl.c,v 1.5 2013/04/14 19:45:
 +	free(ncpu_stats);
 +    }
 +#endif
- 
++
  #ifdef NETSNMP_KERN_MCPU
-     mcpu_size  = cpu_num*sizeof(NETSNMP_KERN_MCPU_TYPE);
+ #if defined(KERN_CPTIME2)
+     mcpu_size  = cpu_num*sizeof(cpu_stats);
+@@ -225,7 +261,8 @@ int netsnmp_cpu_arch_load( netsnmp_cache
+ #elif defined(__NetBSD__)
+     mcpu_size  = cpu_num*sizeof(cpu_stats);
      mcpu_stats = malloc(mcpu_size);
--    sysctl(mcpu_mib, 2, mcpu_stats, &mcpu_size, NULL, 0);
-+    if (sysctl(mcpu_mib, 2, mcpu_stats, &mcpu_size, NULL, 0) == -1)
+-    sysctlbyname("kern.cp_time", mcpu_stats, &mcpu_size, NULL, 0);
++    if (sysctlbyname("kern.cp_time", mcpu_stats, &mcpu_size, NULL, 0) == -1)
 +	snmp_log(LOG_ERR, "sysctl kern.mcpu failed (errno %d)\n", errno);
-     for ( i = 0; i < cpu_num; i++ ) {
-         cpu = netsnmp_cpu_get_byIdx( i, 0 );
-         /* XXX - per-CPU statistics - mcpu_mib[i].??? */
+ #elif defined(__FreeBSD__)
+     if (cp_times == -1) {
+ 	int ret = sysctlbyname("kern.cp_times", NULL, &mcpu_size, NULL, 0);
