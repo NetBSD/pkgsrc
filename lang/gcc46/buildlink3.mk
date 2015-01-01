b54f95c8378793d3da7022e3b0cb9c10727db81b
ryoon <ryoon@pkgsrc.org>
2015-01-01 01:29:56 +0000

    Bump PKGREVISION
    * Add support code for gcc46-libs.

--- lang/gcc46/buildlink3.mk
+++ lang/gcc46/buildlink3.mk
@@ -1,74 +1,35 @@
-# $NetBSD: buildlink3.mk,v 1.7 2014/09/06 08:20:29 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2015/01/01 01:29:56 ryoon Exp $
 
 BUILDLINK_TREE+=	gcc46
 
 .if !defined(GCC46_BUILDLINK3_MK)
 GCC46_BUILDLINK3_MK:=
 
-FIND_PREFIX:=	BUILDLINK_PREFIX.gcc46=gcc46
-.include "../../mk/find-prefix.mk"
-
-_GCC46_SUBDIR=	gcc46
-_GCC46_PREFIX=	${BUILDLINK_PREFIX.gcc46}/${_GCC46_SUBDIR}
-
 BUILDLINK_API_DEPENDS.gcc46+=	gcc46>=4.6
 BUILDLINK_ABI_DEPENDS.gcc46+=	gcc46>=4.6.0
-BUILDLINK_PKGSRCDIR.gcc46?=	../../lang/gcc46
+BUILDLINK_PKGSRCDIR.gcc46=	../../lang/gcc46
+BUILDLINK_DEPMETHOD.gcc46?=	build
 
-.if exists(${_GCC46_PREFIX}/bin/gcc)
+FIND_PREFIX:=	BUILDLINK_PREFIX.gcc46=gcc46
+.include "../../mk/find-prefix.mk"
+BUILDLINK_PASSTHRU_DIRS+=	${BUILDLINK_PREFIX.gcc46}/gcc46
 
-# logic for detecting the ADA compiler (not yet supported)
-#gcc46_GNAT1!=${_GCC46_PREFIX}/bin/gcc -print-prog-name=gnat1
-#.  if exists(${gcc46_GNAT1})
-#CONFIGURE_ENV+=	ADAC=${_GCC46_PREFIX}/bin/gcc
-#MAKE_ENV+=	ADAC=${_GCC46_PREFIX}/bin/gcc
-#.  endif
-
-# add libraries
-BUILDLINK_LIBDIRS.gcc46+=	${_GCC46_SUBDIR}/lib
-
-# find the gcc architecture
-gcc46_GCC_ARCHDIR!=	${DIRNAME} `${_GCC46_PREFIX}/bin/gcc --print-libgcc-file-name`
-
-# add the architecture dep libraries
-.  if empty(gcc46_GCC_ARCHDIR:M*not_found*)
-BUILDLINK_LIBDIRS.gcc46+=	${gcc46_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc46}\///}/
-
-# add the ada libraries (not yet supported)
-#.    if exists(${gcc46_GNAT1})
-#BUILDLINK_LIBDIRS.gcc46+=	${gcc46_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc46}\///}/adalib
-#.    endif
-
-# add the header files
-BUILDLINK_INCDIRS.gcc46+=	${_GCC46_SUBDIR}/include ${gcc46_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc46}\///}/include
-.  endif
-.endif
-
-BUILDLINK_FILES_CMD.gcc46=	\
-	(cd  ${BUILDLINK_PREFIX.gcc46} &&	\
-	${FIND} ${_GCC46_SUBDIR}/bin ${_GCC46_SUBDIR}/include ${_GCC46_SUBDIR}/libexec ${_GCC46_SUBDIR}/lib \( -type f -o -type l \) -print)
-
-# When not using the GNU linker, gcc will always link shared libraries
-# against the shared version of libgcc. Always enable _USE_GCC_SHILB on
-# platforms that don't use the GNU linker, such as SunOS.
-.include "../../mk/bsd.fast.prefs.mk"
-.if ${OPSYS} == "SunOS"
-_USE_GCC_SHLIB= yes
-.endif
+BUILDLINK_FILES.gcc46=		#empty
+BUILDLINK_AUTO_VARS.gcc46=	no
 
 # Packages that link against shared libraries need a full dependency.
 .if defined(_USE_GCC_SHLIB)
-BUILDLINK_DEPMETHOD.gcc46+=	full
-.else
-BUILDLINK_DEPMETHOD.gcc46?=	build
+DEPENDS+=	{gcc46,gcc46-libs}>=4.6:../../lang/gcc46-libs
+ABI_DEPENDS+=	{gcc46,gcc46-libs}>=4.6.0:../../lang/gcc46-libs
 .endif
 
-.include "../../mk/pthread.buildlink3.mk"
 pkgbase := gcc46
 .include "../../mk/pkg-build-options.mk"
-.if !empty(PKG_BUILD_OPTIONS.gcc46:Mnls)
-.include "../../devel/gettext-lib/buildlink3.mk"
+.if !empty(PKG_BUILD_OPTIONS.gcc46:Mgcc-java)
+.include "../../devel/zlib/buildlink3.mk"
 .endif
-.endif # GCC46_BUILDLINK3_MK
 
+.include "../../mk/dlopen.buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+.endif # GCC46_BUILDLINK3_MK
 BUILDLINK_TREE+=	-gcc46
