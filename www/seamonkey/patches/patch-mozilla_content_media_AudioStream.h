99ab6fd3eb6df9d3bc4ff456f6c2053159cd23eb
ryoon <ryoon@pkgsrc.org>
2015-01-02 04:26:21 +0000

    Update to 2.31
    Changelog:
    # What's New in SeaMonkey 2.31
    
    SeaMonkey 2.31 contains the following major changes relative to SeaMonkey 2.30:
    
    ## SeaMonkey-specific changes
    
        Text zoom is now available in Composer.
        GStreamer and PulseAudio support has been re-enabled on Linux.
    
    ## Mozilla platform changes
    
        Support for H264 (MP4) is now built in on Mac OS X Snow Leopard (10.6) and newer through native APIs.
        HTTP/2 (draft14) and ALPN have been implemented.
        Added ability to recover from a locked process in the "SeaMonkey is already running" dialog on Windows.
        Added ECDH support for WebCrypto.
        The console.table function has been added to the Error Console.
        CSS transitions start correctly now when started at the same time as changes to display, position, overflow, and similar properties.
        Also see Firefox 34 for Developers.
        Fixed several stability issues.
    
    # Security fixes
    Fixed in SeaMonkey 2.31
        2014-91 Privileged access to security wrapped protected objects
        2014-89 Bad casting from the BasicThebesLayer to BasicContainerLayer
        2014-88 Buffer overflow while parsing media content
        2014-87 Use-after-free during HTML5 parsing
        2014-86 CSP leaks redirect data via violation reports
        2014-85 XMLHttpRequest crashes with some input streams
        2014-84 XBL bindings accessible via improper CSS declarations
        2014-83 Miscellaneous memory safety hazards (rv:34.0 / rv:31.3)

--- www/seamonkey/patches/patch-mozilla_content_media_AudioStream.h
+++ www/seamonkey/patches/patch-mozilla_content_media_AudioStream.h
@@ -1,20 +1,13 @@
-$NetBSD: patch-mozilla_content_media_AudioStream.h,v 1.2 2014/11/02 05:40:31 ryoon Exp $
+$NetBSD: patch-mozilla_content_media_AudioStream.h,v 1.3 2015/01/02 04:26:21 ryoon Exp $
 
---- mozilla/content/media/AudioStream.h.orig	2014-10-14 06:36:05.000000000 +0000
+--- mozilla/content/media/AudioStream.h.orig	2014-12-03 06:22:34.000000000 +0000
 +++ mozilla/content/media/AudioStream.h
-@@ -17,6 +17,7 @@
- #include "mozilla/RefPtr.h"
- 
- #include "cubeb/cubeb.h"
-+#include "soundtouch/SoundTouch.h"
- 
- template <>
- class nsAutoRefTraits<cubeb_stream> : public nsPointerRefTraits<cubeb_stream>
-@@ -26,7 +27,6 @@ public:
- };
+@@ -17,7 +17,7 @@
+ #include "CubebUtils.h"
  
  namespace soundtouch {
 -class SoundTouch;
++class MOZ_IMPORT_API SoundTouch;
  }
  
  namespace mozilla {
