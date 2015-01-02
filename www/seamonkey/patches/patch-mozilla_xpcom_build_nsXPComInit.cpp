48d5673b58134f5ad9ed52daf009d7946aa0c63f
ryoon <ryoon>
2015-01-02 04:26:21 +0000

    Update to 2.31
    Changelog:
    # What's New in SeaMonkey 2.31
    
    SeaMonkey 2.31 contains the following major changes relative to SeaMonkey 2.30:
    
    ## SeaMonkey-specific changes
    
        Text zoom is now available in Composer.
        GStreamer and PulseAudio support has been re-enabled on Linux.
    
    ## Mozilla platform changes
    
        Support for H264 (MP4) is now built in on Mac OS X Snow Leopard (10.6) and newer through native APIs.
        HTTP/2 (draft14) and ALPN have been implemented.
        Added ability to recover from a locked process in the "SeaMonkey is already running" dialog on Windows.
        Added ECDH support for WebCrypto.
        The console.table function has been added to the Error Console.
        CSS transitions start correctly now when started at the same time as changes to display, position, overflow, and similar properties.
        Also see Firefox 34 for Developers.
        Fixed several stability issues.
    
    # Security fixes
    Fixed in SeaMonkey 2.31
        2014-91 Privileged access to security wrapped protected objects
        2014-89 Bad casting from the BasicThebesLayer to BasicContainerLayer
        2014-88 Buffer overflow while parsing media content
        2014-87 Use-after-free during HTML5 parsing
        2014-86 CSP leaks redirect data via violation reports
        2014-85 XMLHttpRequest crashes with some input streams
        2014-84 XBL bindings accessible via improper CSS declarations
        2014-83 Miscellaneous memory safety hazards (rv:34.0 / rv:31.3)

--- www/seamonkey/patches/patch-mozilla_xpcom_build_nsXPComInit.cpp
+++ www/seamonkey/patches/patch-mozilla_xpcom_build_nsXPComInit.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-mozilla_xpcom_build_nsXPComInit.cpp,v 1.1 2014/11/02 05:40:31 ryoon Exp $
+$NetBSD: patch-mozilla_xpcom_build_nsXPComInit.cpp,v 1.2 2015/01/02 04:26:21 ryoon Exp $
 
---- mozilla/xpcom/build/nsXPComInit.cpp.orig	2014-10-14 06:36:46.000000000 +0000
+--- mozilla/xpcom/build/nsXPComInit.cpp.orig	2014-12-03 06:23:27.000000000 +0000
 +++ mozilla/xpcom/build/nsXPComInit.cpp
-@@ -137,7 +137,9 @@ extern nsresult nsStringInputStreamConst
+@@ -138,7 +138,9 @@ extern nsresult nsStringInputStreamConst
  #include "mozilla/VisualEventTracer.h"
  #endif
  
@@ -12,17 +12,17 @@ $NetBSD: patch-mozilla_xpcom_build_nsXPComInit.cpp,v 1.1 2014/11/02 05:40:31 ryo
  #if defined(MOZ_VPX) && !defined(MOZ_VPX_NO_MEM_REPORTING)
  #include "vpx_mem/vpx_mem.h"
  #endif
-@@ -629,11 +631,13 @@ NS_InitXPCOM2(nsIServiceManager* *result
-     // this oddness.
-     mozilla::SetICUMemoryFunctions();
+@@ -652,11 +654,13 @@ NS_InitXPCOM2(nsIServiceManager** aResul
+   // this oddness.
+   mozilla::SetICUMemoryFunctions();
  
 +#ifndef MOZ_OGG_NO_MEM_REPORTING
-     // Do the same for libogg.
-     ogg_set_mem_functions(OggReporter::CountingMalloc,
-                           OggReporter::CountingCalloc,
-                           OggReporter::CountingRealloc,
-                           OggReporter::CountingFree);
+   // Do the same for libogg.
+   ogg_set_mem_functions(OggReporter::CountingMalloc,
+                         OggReporter::CountingCalloc,
+                         OggReporter::CountingRealloc,
+                         OggReporter::CountingFree);
 +#endif
  
  #if defined(MOZ_VPX) && !defined(MOZ_VPX_NO_MEM_REPORTING)
-     // And for VPX.
+   // And for VPX.
