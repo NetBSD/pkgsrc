f54c9eb962a12798a8cf7fc9b7e2d52ab9f7c238
wiz <wiz@pkgsrc.org>
2015-01-03 10:01:40 +0000

    Remove code for DRM_IOCTL_MMAP hack. Never needed on NetBSD-5 and -6; and chuq improved mmap in NetBSD-7 and -current, so it's not needed there any longer either. Confirmed by mrg.
    Bump PKGREVISION.

--- x11/libdrm/patches/patch-include_drm_drm.h
+++ x11/libdrm/patches/patch-include_drm_drm.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-include_drm_drm.h,v 1.2 2014/10/26 10:20:10 wiz Exp $
+$NetBSD: patch-include_drm_drm.h,v 1.3 2015/01/03 10:01:40 wiz Exp $
 
 Fix compilation on systems that don't provide O_CLOEXEC.
 
@@ -16,24 +16,3 @@ Fix compilation on systems that don't provide O_CLOEXEC.
  struct drm_prime_handle {
  	__u32 handle;
  
-@@ -759,6 +763,20 @@ struct drm_prime_handle {
- #define DRM_IOCTL_MODE_OBJ_SETPROPERTY	DRM_IOWR(0xBA, struct drm_mode_obj_set_property)
- #define DRM_IOCTL_MODE_CURSOR2		DRM_IOWR(0xBB, struct drm_mode_cursor2)
- 
-+#ifdef __NetBSD__
-+/*
-+ * Instrumenting mmap is trickier than just making an ioctl to do it.
-+ */
-+struct drm_mmap {
-+	void		*dnm_addr;  /* in/out */
-+	size_t		dnm_size;   /* in */
-+	int		dnm_prot;   /* in */
-+	int		dnm_flags;  /* in */
-+	off_t		dnm_offset; /* in */
-+};
-+#define	DRM_IOCTL_MMAP	DRM_IOWR(0xff, struct drm_mmap)
-+#endif
-+
- /**
-  * Device specific ioctls should only be in their respective headers
-  * The device specific ioctl range is from 0x40 to 0x99.
