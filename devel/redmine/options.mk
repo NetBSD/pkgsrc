41e51e46115313398c3ab5efa246cfa044b7be86
ryoon <ryoon>
2015-01-03 18:55:43 +0000

    Import ruby215-redmine-2.6.0 as devel/redmine.
    Redmine is a flexible project management web application. Written
    using the Ruby on Rails framework, it is cross-platform and
    cross-database.
    
    Redmine is open source and released under the terms of the GNU
    General Public License v2 (GPL).
    
    Features
    
    Some of the main features of Redmine are:
    
       Multiple projects support
       Flexible role based access control
       Flexible issue tracking system
       Gantt chart and calendar
       News, documents & files management
       Feeds & email notifications
       Per project wiki
       Per project forums
       Time tracking
       Custom fields for issues, time-entries, projects and users
       SCM integration (SVN, CVS, Git, Mercurial, Bazaar and Darcs)
       Issue creation via email
       Multiple LDAP authentication support
       User self-registration support
       Multilanguage support
       Multiple databases support
    
    This package contains all required Ruby gems in it.
    Th code in Makefile should be moved to lang/ruby/*.mk later.

---
+++ devel/redmine/options.mk
@@ -0,0 +1,69 @@
+# $NetBSD: options.mk,v 1.1 2015/01/03 18:55:43 ryoon Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.redmine
+
+PKG_OPTIONS_REQUIRED_GROUPS=	db
+PKG_OPTIONS_GROUP.db=		mysql pgsql sqlite3
+PKG_SUPPORTED_OPTIONS+=		unicorn
+
+PKG_SUGGESTED_OPTIONS=	mysql unicorn
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	mysql pgsql sqlite3
+
+###
+### Use mysql, pgsql, or sqlite3 backend
+###
+MYSQL_DISTFILE=		mysql2-0.3.17.gem
+PGSQL_DISTFILE=		pg-0.18.0.pre20141117110243.gem
+SQLITE3_DISTFILE=	sqlite3-1.3.10.gem
+
+.if make (distinfo) || make (mdi) # for checksum generation only
+DISTFILES+=	${MYSQL_DISTFILE}
+DISTFILES+=	${PGSQL_DISTFILE}
+DISTFILES+=	${SQLITE3_DISTFILE}
+.elif !empty(PKG_OPTIONS:Mmysql)
+DISTFILES+=	${MYSQL_DISTFILE}
+.include "../../mk/mysql.buildlink3.mk"
+PLIST.mysql=	yes
+.elif !empty(PKG_OPTIONS:Mpgsql)
+DISTFILES+=	${PGSQL_DISTFILE}
+.include "../../mk/pgsql.buildlink3.mk"
+PLIST.pgsql=	yes
+.elif !empty(PKG_OPTIONS:Msqlite)
+DISTFILES+=	${SQLITE3_DISTFILE}
+.include "../../databases/sqlite3/buildlink3.mk"
+PLIST.sqlite3=	yes
+.endif
+
+###
+### Use Unicorn web server
+###
+.if !empty(PKG_OPTIONS:Municorn) || make (distinfo) || make (mdi)
+PLIST_SRC=	${PLIST_SRC_DFLT} PLIST.unicorn
+DISTFILES+=	kgio-2.9.2.gem \
+		raindrops-0.13.0.gem \
+		unicorn-4.8.3.gem
+
+SUBST_CLASSES+=			prefix
+SUBST_STAGE.prefix=		pre-configure
+SUBST_MESSAGE.prefix=		Setting PREFIX.
+SUBST_FILES.prefix=		${WRKDIR}/unicorn.rb
+SUBST_VARS.prefix+=		PREFIX
+
+RCD_SCRIPTS+=	redmine_unicorn
+
+post-extract:
+	${CP} ${FILESDIR}/unicorn.rb ${WRKDIR}/unicorn.rb
+
+.PHONY: unicorn-post-install
+unicorn-post-install:
+	${CP} ${WRKDIR}/unicorn.rb \
+		${DESTDIR}${PREFIX}/share/examples/redmine/unicorn.rb.example
+	${CP} ${FILESDIR}/Gemfile.local \
+		${DESTDIR}${PREFIX}/share/redmine/app
+.endif
+.PHONY: unicorn-post-install
+unicorn-post-install:
+# nothing
