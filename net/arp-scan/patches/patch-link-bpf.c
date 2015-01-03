46a323268922919ded629a69ef3f660d67b1bccf
schwarz <schwarz@pkgsrc.org>
2015-01-03 14:44:36 +0000

    updated net/arp-scan to 1.9

--- net/arp-scan/patches/patch-link-bpf.c
+++ net/arp-scan/patches/patch-link-bpf.c
@@ -1,10 +1,7 @@
-$NetBSD: patch-link-bpf.c,v 1.1 2012/08/07 18:21:30 marino Exp $
+$NetBSD: patch-link-bpf.c,v 1.2 2015/01/03 14:44:36 schwarz Exp $
 
-DragonFly's pcap.h includes pcap/bpf.h which clashes with net/bpf.h
-pcap.h is included by arp-scan.h
-
---- link-bpf.c.orig	2011-01-09 10:37:09.000000000 +0000
-+++ link-bpf.c
+--- link-bpf.c.orig	2013-04-27 10:39:08.000000000 +0200
++++ link-bpf.c	2015-01-03 15:34:53.000000000 +0100
 @@ -32,6 +32,9 @@
   *
   */
@@ -14,4 +11,4 @@ pcap.h is included by arp-scan.h
 +#endif
  #include "arp-scan.h"
  
- #ifdef HAVE_FCNTL_H
+ #ifdef HAVE_NET_IF_H
