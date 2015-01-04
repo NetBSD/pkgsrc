7a117f9a3a652217d9cb60dd1d045d6144e1d970
schmonz <schmonz@pkgsrc.org>
2015-01-04 07:24:59 +0000

    Add a buildlink3.mk.

---
+++ textproc/source-highlight/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/01/04 07:24:59 schmonz Exp $
+
+BUILDLINK_TREE+=	source-highlight
+
+.if !defined(SOURCE_HIGHLIGHT_BUILDLINK3_MK)
+SOURCE_HIGHLIGHT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.source-highlight+=	source-highlight>=3.1.7nb2
+BUILDLINK_PKGSRCDIR.source-highlight?=	../../textproc/source-highlight
+
+.include "../../devel/boost-libs/buildlink3.mk"
+.endif	# SOURCE_HIGHLIGHT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-source-highlight
