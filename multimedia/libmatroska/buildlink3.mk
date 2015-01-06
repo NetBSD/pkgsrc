f68d8450bce943670fa5f9e63d5642532fabecdc
wiz <wiz@pkgsrc.org>
2015-01-06 14:59:18 +0000

    The last updates of libebml and libmatroska, despite only being X.Y.Z -> X.Y.Z+1 updates, bumped the shared library major versions.
    Do recursive bump for that.

--- multimedia/libmatroska/buildlink3.mk
+++ multimedia/libmatroska/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2013/04/12 13:36:56 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2015/01/06 14:59:18 wiz Exp $
 
 BUILDLINK_TREE+=	libmatroska
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libmatroska
 LIBMATROSKA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libmatroska+=	libmatroska>=0.8.0
-BUILDLINK_ABI_DEPENDS.libmatroska+=	libmatroska>=1.4.0
+BUILDLINK_ABI_DEPENDS.libmatroska+=	libmatroska>=1.4.2
 BUILDLINK_PKGSRCDIR.libmatroska?=	../../multimedia/libmatroska
 
 .include "../../devel/libebml/buildlink3.mk"
