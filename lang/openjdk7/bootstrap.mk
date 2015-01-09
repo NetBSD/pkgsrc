48bec98c8cb482e5929b447f71ce37fd374795ad
rumko <rumko@pkgsrc.org>
2015-01-09 16:29:04 +0000

    lang/openjdk7: make it work on FreeBSD 10.* x86_64
    * -Wno-new-returns-null is not currently known to installed clang on fbsd, make it optional
    * on fbsd, openjdk builds libjsoundalsa and libsctp, add them optionally to PLIST
    * add necessary patches to make it build on fbsd
    
    Reviewed by wiz

--- lang/openjdk7/bootstrap.mk
+++ lang/openjdk7/bootstrap.mk
@@ -1,7 +1,7 @@
-# $NetBSD: bootstrap.mk,v 1.13 2014/08/15 19:39:36 wiz Exp $
+# $NetBSD: bootstrap.mk,v 1.14 2015/01/09 16:29:04 rumko Exp $
 
 ONLY_FOR_PLATFORM=	NetBSD-[567].*-i386 NetBSD-[567].*-x86_64
-ONLY_FOR_PLATFORM+=	DragonFly-[23].*-* SunOS-*-*
+ONLY_FOR_PLATFORM+=	DragonFly-[23].*-* SunOS-*-* FreeBSD-10.*-x86_64
 
 BOOT.nb5-i386=		bootstrap-jdk7u60-bin-netbsd-5-i386-20140719.tar.bz2
 BOOT.nb5-amd64=		bootstrap-jdk7u60-bin-netbsd-5-amd64-20140719.tar.bz2
@@ -11,6 +11,7 @@ BOOT.nb7-i386=		bootstrap-jdk7u60-bin-netbsd-7-i386-20140719.tar.bz2
 BOOT.nb7-amd64=		bootstrap-jdk7u60-bin-netbsd-7-amd64-20140719.tar.bz2
 BOOT.dfly3.6-amd64=	bootstrap-jdk7u60-bin-dragonfly-3.6-amd64-20140719.tar.bz2
 BOOT.dfly3.8-amd64=	bootstrap-jdk7u60-bin-dragonfly-3.8-amd64-20140719.tar.bz2
+BOOT.fbsd10-amd64=	bootstrap-jdk7u71-bin-freebsd-10-x86_64-20150101.tar.bz2
 
 .if !empty(MACHINE_PLATFORM:MNetBSD-5.[0-8]*-i386) || make(distinfo)
 DISTFILES+=		${BOOT.nb5-i386}
@@ -50,6 +51,11 @@ EXTRACT_ONLY+=		${BOOT.dfly3.6-amd64}
 .if !empty(MACHINE_PLATFORM:MDragonFly-3.[8-9]*-x86_64) || make(distinfo)
 DISTFILES+=		${BOOT.dfly3.8-amd64}
 EXTRACT_ONLY+=		${BOOT.dfly3.8-amd64}
+.endif
+
+.if !empty(MACHINE_PLATFORM:MFreeBSD-10.*-x86_64) || make(distinfo)
+DISTFILES+=		${BOOT.fbsd10-amd64}
+EXTRACT_ONLY+=		${BOOT.fbsd10-amd64}
 .endif
 
 .if ${OPSYS} == "SunOS"
