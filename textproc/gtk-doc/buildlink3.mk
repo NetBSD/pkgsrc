5256f332726d373dab2703e28799f4eba79e9691
prlw1 <prlw1@pkgsrc.org>
2015-01-10 15:21:41 +0000

    Update gtk-doc to 1.21
    As per warning in 1.20, gtkdoc-scanobj and generating html via
    sgml tools (options.mk) have been removed.
    
    Added
    
      # needed together with pkg-config by gtkdoc-depscan at runtime
      .include "../../devel/glib2/buildlink3.mk"
    
    to buildlink3.mk
    
    GTK-Doc 1.21    (Jul 17 2014)
    ============
    
     Important notice - starting with the next release these long deprecated
     features will be removed, please write to gtk-doc-list@gnome.org and tell us
     your concerns:
     o gtkdoc-mktmpl - please move all the comments into the sources.
     o generating html via sgml tools (jade/openjade), using xsltproc and
       docbook-xslt is pretty common and preferred since version 1.6
    
     Changes
    
     o 170860 : gtk-doc should have definitions for stability
     o 644111 : one cannot specify against which libs gtkdoc-fixxref should
                resolve links (problematic with multiple versions)
     o 657444 :  " enum foo { ... } " ; not recognized
     o 671519 : Self-test relies on nonportable (GNU enhanced) 'date' command
     o 678094 : the word "returns" in a function description can be parsed as
                the "Returns:" section
     o 722621 : gtk-doc tarball was created with 32bit uid/gid, unusable with
                mingw/msys tar
     o 724739 : Self-test fail: gtkdoc-mkdb misusing perl datatype
     o 725505 : new syntax highlighting for code is weird
     o 725663 : configure: non POSIX test usage
     o 730658 : Deprecation warning for non-deprecated type GParamFlags
     o 730777 : Add support for nullable and optional annotations

--- textproc/gtk-doc/buildlink3.mk
+++ textproc/gtk-doc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2011/11/10 09:42:48 abs Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2015/01/10 15:21:41 prlw1 Exp $
 #
 # provided mostly for pkg-config
 
@@ -9,6 +9,8 @@ GTK_DOC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gtk-doc+=	gtk-doc>=1.18
 BUILDLINK_PKGSRCDIR.gtk-doc?=	../../textproc/gtk-doc
+# needed together with pkg-config by gtkdoc-depscan at runtime
+.include "../../devel/glib2/buildlink3.mk"
 
 BUILDLINK_DEPMETHOD.gtk-doc?=	build
 .endif # GTK_DOC_BUILDLINK3_MK
