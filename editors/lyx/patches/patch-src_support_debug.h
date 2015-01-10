cf913695c4b993a49412346a2412471590263688
rumko <rumko@pkgsrc.org>
2015-01-10 18:13:22 +0000

    editors/lyx: update to 2.1.2.2
    Summary of changelog:
    * Native IPA inset and IPA toolbar
    * New supported languages
    * Tex Fonts Additions
    * Basic support for Math OpenType fonts
    * Document class categories
    * Enhanced support for LaTeX options
    * New supported LaTeX commands
    * New export command-line option
    * Table rotations
    * Layout enhancements
    * New modules
    * Equation Editor "mode"
    
    Ok@ reed

--- editors/lyx/patches/patch-src_support_debug.h
+++ editors/lyx/patches/patch-src_support_debug.h
@@ -1,12 +1,11 @@
-$NetBSD: patch-src_support_debug.h,v 1.2 2014/01/19 14:04:39 rumko Exp $
+$NetBSD: patch-src_support_debug.h,v 1.3 2015/01/10 18:13:22 rumko Exp $
 
---- src/support/debug.h.orig	2013-04-29 15:58:31.000000000 +0000
+--- src/support/debug.h.orig	2014-11-15 18:11:26.000000000 +0000
 +++ src/support/debug.h
-@@ -16,16 +16,8 @@
- #define LYXDEBUG_H
- 
- #include "support/strfwd.h"
--
+@@ -21,14 +21,8 @@
+ // but ios_base has already been defined in strfwd
+ // if compiling with it
+ #ifndef  _LIBCPP_VERSION
 -namespace std {
 -
 -class ios_base;
@@ -15,9 +14,8 @@ $NetBSD: patch-src_support_debug.h,v 1.2 2014/01/19 14:04:39 rumko Exp $
 -typedef basic_streambuf<char, char_traits<char> > streambuf;
 -
 -}
--
 +#include <ios>
 +#include <streambuf>
+ #endif
  
- namespace lyx {
  
