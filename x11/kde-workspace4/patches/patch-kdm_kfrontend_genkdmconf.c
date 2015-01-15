04de92a4b099a498caf2a1a17edd12be90fd9d11
rumko <rumko>
2015-01-15 17:59:10 +0000

    x11/kde-workspace4: fix build on fbsd
    * genkdmconf: FreeBSD 10+ does not have utmp, only utmpx. Use utmpx
    when available.
    * not only Linux, but also FreeBSD can have libutempter present. Make
    sure PLIST is correct in both cases.
    
    Ok@ markd

--- x11/kde-workspace4/patches/patch-kdm_kfrontend_genkdmconf.c
+++ x11/kde-workspace4/patches/patch-kdm_kfrontend_genkdmconf.c
@@ -1,14 +1,27 @@
-$NetBSD: patch-kdm_kfrontend_genkdmconf.c,v 1.1 2012/04/15 05:55:00 markd Exp $
+$NetBSD: patch-kdm_kfrontend_genkdmconf.c,v 1.2 2015/01/15 17:59:10 rumko Exp $
 
---- kdm/kfrontend/genkdmconf.c.orig	2012-03-29 22:02:31.000000000 +0000
+On some systems (e.g. fbsd 10+) utmp is no longer available, use utmpx instead.
+
+--- kdm/kfrontend/genkdmconf.c.orig	2014-11-06 22:36:58.000000000 +0000
 +++ kdm/kfrontend/genkdmconf.c
-@@ -46,9 +46,7 @@ Foundation, Inc., 51 Franklin Street, Fi
+@@ -46,12 +46,18 @@ Foundation, Inc., 51 Franklin Street, Fi
  #include <sys/wait.h>
  #include <sys/param.h>
  #ifdef BSD
 -# ifdef BSD_UTMP
++# ifdef HAVE_UTMPX
++#  include <utmpx.h>
++# else
  #  include <utmp.h>
--# endif
+ # endif
  # ifndef _PATH_UTMP
  #  if defined(__FreeBSD_version) && __FreeBSD_version >= 900007
- #   define _PATH_UTMP "/var/run/utmp"
+-#   define _PATH_UTMP "/var/run/utmp"
++#   if __FreeBSD_version >= 1001000
++#    define _PATH_UTMP "/var/run/utx.active"
++#   else
++#    define _PATH_UTMP "/var/run/utmp"
++#   endif
+ #  else
+ #   error "_PATH_UTMP must be defined"
+ #  endif
