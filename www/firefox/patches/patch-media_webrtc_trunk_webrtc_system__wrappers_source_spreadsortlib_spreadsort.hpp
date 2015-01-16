4bd9b10350da1fdb1152a191d84a1c5cdae849f1
ryoon <ryoon>
2015-01-16 22:42:09 +0000

    Update to 35.0
    Changelog:
    New Firefox Hello with new rooms-based conversations model
    New New search UI improved and enabled for more locales
    New Access the Firefox Marketplace from the Tools menu and optional toolbar button
    New Built-in support for H264 (MP4) on Mac OS X Snow Leopard (10.6) and newer through native APIs
    New Use tiled rendering on OS X
    New Improved high quality image resizing performance
    New Improved handling of dynamic styling changes to increase responsiveness
    HTML5 Added support for the CSS Font Loading API
    HTML5 Resource Timing API implemented
    HTML5 CSS filters enabled by default
    HTML5 Changed JavaScript 'let' semantics to conform better to the ES6 specification
    Developer Support for inspecting ::before and ::after pseudo elements
    Developer Computed view: Nodes matching the hovered selector are now highlighted
    Developer Network Monitor: New request/response headers view (more info)
    Developer Added support for the EXT_blend_minmax WebGL extension
    Fixed Show DOM Properties context menu item in inspector
    Fixed Reduced resource usage for scaled images
    Fixed PDF.js updated to version 1.0.907
    Fixed Non-HTTP(S) XHR now returns correct status code
    Fixed Various security fixes
    
    Security fixes:
        2015-09 XrayWrapper bypass through DOM objects
        2015-08 Delegated OCSP responder certificates failure with id-pkix-ocsp-nocheck extension
        2015-07 Gecko Media Plugin sandbox escape
        2015-06 Read-after-free in WebRTC
        2015-05 Read of uninitialized memory in Web Audio
        2015-04 Cookie injection through Proxy Authenticate responses
        2015-03 sendBeacon requests lack an Origin header
        2015-02 Uninitialized memory use during bitmap rendering
        2015-01 Miscellaneous memory safety hazards (rv:35.0 / rv:31.4)

--- www/firefox/patches/patch-media_webrtc_trunk_webrtc_system__wrappers_source_spreadsortlib_spreadsort.hpp
+++ www/firefox/patches/patch-media_webrtc_trunk_webrtc_system__wrappers_source_spreadsortlib_spreadsort.hpp
@@ -1,8 +1,22 @@
-$NetBSD: patch-media_webrtc_trunk_webrtc_system__wrappers_source_spreadsortlib_spreadsort.hpp,v 1.5 2014/03/20 21:02:00 ryoon Exp $
+$NetBSD: patch-media_webrtc_trunk_webrtc_system__wrappers_source_spreadsortlib_spreadsort.hpp,v 1.6 2015/01/16 22:42:09 ryoon Exp $
 
---- media/webrtc/trunk/webrtc/system_wrappers/source/spreadsortlib/spreadsort.hpp.orig	2014-03-15 05:19:30.000000000 +0000
+--- media/webrtc/trunk/webrtc/system_wrappers/source/spreadsortlib/spreadsort.hpp.orig	2015-01-09 04:38:22.000000000 +0000
 +++ media/webrtc/trunk/webrtc/system_wrappers/source/spreadsortlib/spreadsort.hpp
-@@ -21,6 +21,13 @@ Scott McMurray
+@@ -14,6 +14,13 @@ Cygwin fix provided by:
+ Scott McMurray
+ */
+ 
++#ifdef __FreeBSD__
++# include <osreldate.h>
++# if __FreeBSD_version < 900506
++#  define getchar boost_getchar
++# endif
++#endif
++
+ #ifndef BOOST_SPREAD_SORT_H
+ #define BOOST_SPREAD_SORT_H
+ #include <algorithm>
+@@ -21,6 +28,13 @@ Scott McMurray
  #include <vector>
  #include "webrtc/system_wrappers/source/spreadsortlib/constants.hpp"
  
