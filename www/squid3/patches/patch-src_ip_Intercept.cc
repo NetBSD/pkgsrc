ed436f9e2ef0448acd84ef8e950ed4d05d1e6803
adam <adam@pkgsrc.org>
2015-01-16 11:41:11 +0000

    Changes 3.4.11: * cachemgr.cgi: memory leak in request parser * Fix typo on commStartSslClose * Fix SQUID_CC_REQUIRE_ARGUMENT autoconf macro * Bug 3760: squidclient ignores --disable-ipv6 * Bug 3664: ssl_crtd fails to build on OpenSolaris/OpenIndiana/Solaris 11 * Bug 3754: configure doesnt detect IPFilter 5.1.2 system headers * Bug 4164: SEGFAULT when %W formating code used in errorpages * Deleting first fs left psstate->servers pointing to uninitialized memory * Maintenance: check release notes on packaging * Bug 4057: Avoid on-exit crashes when adaptation is enabled.

--- www/squid3/patches/patch-src_ip_Intercept.cc
+++ www/squid3/patches/patch-src_ip_Intercept.cc
@@ -1,21 +1,21 @@
-$NetBSD: patch-src_ip_Intercept.cc,v 1.2 2013/04/27 17:51:24 tron Exp $
+$NetBSD: patch-src_ip_Intercept.cc,v 1.3 2015/01/16 11:41:11 adam Exp $
 
 Avoid conflict with IP Fitler's debug().
 
---- src/ip/Intercept.cc.orig	2013-04-27 04:34:48.000000000 +0100
-+++ src/ip/Intercept.cc	2013-04-27 18:40:24.000000000 +0100
-@@ -38,6 +38,10 @@
+--- src/ip/Intercept.cc.orig	2015-01-13 11:53:17.000000000 +0000
++++ src/ip/Intercept.cc
+@@ -42,6 +42,10 @@
  
  #if IPF_TRANSPARENT
  
 +#ifdef debug
-+#undef debug			// XXX: IP Filter might declare debug().
++#undef debug		// XXX: IP Filter might declare debug().
 +#endif
 +
- #if HAVE_SYS_IOCTL_H
- #include <sys/ioctl.h>
+ #if !defined(IPFILTER_VERSION)
+ #define IPFILTER_VERSION        5000004
  #endif
-@@ -75,6 +79,10 @@
+@@ -92,6 +96,10 @@
  #include <errno.h>
  #endif
  
