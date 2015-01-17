1e614257c1329eeb14e977288be1c040e51aee38
adam <adam@pkgsrc.org>
2015-01-17 13:29:55 +0000

    Changes 3.1.0: * Windows Phone and Windows Store support has been added to  Visual Studio 11   (2012) and above Generators. * NVIDIA Nsight Tegra support has been added to  Visual Studio 10 (2010) and   above Generators. * New "target_compile_features" command allows populating target based compile   features. CMake uses this information to ensure that the compiler in use is   capable of building the target, and to add any necessary compile flags   such as -std=gnu++11 to support language features.   More information on this is found at:   - http://www.cmake.org/cmake/help/v3.1/manual/cmake-compile-features.7.html * The syntax for *Variable References* and *Escape Sequences* was simplified in   order to allow a much faster implementation. See policy "CMP0053". * The "if" command no longer automatically dereferences variables named in   quoted or bracket arguments.  See policy "CMP0054". * The target property "SOURCES" now generally supports "Generator Expressions".   The generator expressions may be used in the "add_library" and   "add_executable" commands. * It is now possible to write and append to the target property "SOURCES".   The variable "CMAKE_DEBUG_TARGET_PROPERTIES" can be used to trace the   origin of sources. * CPack gained "7Z" and "TXZ" generators supporting lzma-compressed archives. * The ExternalProject module has learned to support lzma-compressed   source tarballs with ".7z", ".tar.xz", and ".txz" extensions. * The ExternalProject module ExternalProject_Add command learned a new   BUILD_ALWAYS option to cause the external project build step to run every   time the host project is built. * The ctest_coverage command learned to support Intel coverage files with the   "codecov" tool. * The ctest_memcheck command learned to support sanitizer modes, including   "AddressSanitizer", "MemorySanitizer", "ThreadSanitizer", and   "UndefinedBehaviorSanitizer".

--- devel/cmake/patches/patch-Modules_FindX11.cmake
+++ devel/cmake/patches/patch-Modules_FindX11.cmake
@@ -1,12 +1,12 @@
-$NetBSD: patch-Modules_FindX11.cmake,v 1.5 2012/11/15 19:31:55 adam Exp $
+$NetBSD: patch-Modules_FindX11.cmake,v 1.6 2015/01/17 13:29:55 adam Exp $
 
 Patch in pkgsrc paths for finding X.
 
---- Modules/FindX11.cmake.orig	2012-10-31 15:32:05.000000000 +0000
+--- Modules/FindX11.cmake.orig	2014-12-15 20:07:43.000000000 +0000
 +++ Modules/FindX11.cmake
-@@ -61,7 +61,8 @@ if (UNIX)
-   set(CMAKE_FIND_FRAMEWORK_SAVE ${CMAKE_FIND_FRAMEWORK})
-   set(CMAKE_FIND_FRAMEWORK NEVER)
+@@ -73,7 +73,8 @@ if (UNIX)
+   set(CMAKE_REQUIRED_QUIET_SAVE ${CMAKE_REQUIRED_QUIET})
+   set(CMAKE_REQUIRED_QUIET ${X11_FIND_QUIETLY})
    set(X11_INC_SEARCH_PATH
 -    /usr/pkg/xorg/include
 +    @LOCALBASE@/xorg/include
@@ -14,7 +14,7 @@ Patch in pkgsrc paths for finding X.
      /usr/X11R6/include
      /usr/X11R7/include
      /usr/include/X11
-@@ -71,7 +72,8 @@ if (UNIX)
+@@ -84,7 +85,8 @@ if (UNIX)
    )
  
    set(X11_LIB_SEARCH_PATH
--- devel/cmake/patches/patch-Source_kwsys_SystemTools.cxx
+++ devel/cmake/patches/patch-Source_kwsys_SystemTools.cxx
@@ -1,19 +1,20 @@
-$NetBSD: patch-Source_kwsys_SystemTools.cxx,v 1.2 2014/08/10 10:02:51 adam Exp $
+$NetBSD: patch-Source_kwsys_SystemTools.cxx,v 1.3 2015/01/17 13:29:55 adam Exp $
 
 * SCO OpenServer 5.0.7/3.2's command has 711 permission.
 
---- Source/kwsys/SystemTools.cxx.orig	2014-07-31 15:03:57.000000000 +0000
+--- Source/kwsys/SystemTools.cxx.orig	2014-12-15 20:07:43.000000000 +0000
 +++ Source/kwsys/SystemTools.cxx
-@@ -1081,7 +1081,12 @@ bool SystemTools::FileExists(const char*
-   return (GetFileAttributesW(Encoding::ToWide(filename).c_str())
+@@ -1120,8 +1120,13 @@ bool SystemTools::FileExists(const kwsys
+             SystemTools::ConvertToWindowsExtendedPath(filename).c_str())
            != INVALID_FILE_ATTRIBUTES);
  #else
 +// SCO OpenServer 5.0.7/3.2's command has 711 permission.
-+#  if defined(_SCO_DS)
-+  return access(filename, F_OK) == 0;
-+#  else
-   return access(filename, R_OK) == 0;
-+#  endif
++#if defined(_SCO_DS)
++  return access(filename.c_str(), F_OK) == 0;
++#else
+   return access(filename.c_str(), R_OK) == 0;
  #endif
++#endif
  }
  
+ //----------------------------------------------------------------------------
