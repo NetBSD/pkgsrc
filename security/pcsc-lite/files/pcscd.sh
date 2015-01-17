32bbb6ed445aa003363a92ae1f9c29927dd0f5b7
wiz <wiz@pkgsrc.org>
2015-01-17 18:43:15 +0000

    Fix startup script. Bump PKGREVISION. From ISIHARA Takanori. (Ooops, what happened here.)

--- security/pcsc-lite/files/pcscd.sh
+++ security/pcsc-lite/files/pcscd.sh
@@ -1,33 +1,6 @@
->Number:         49574
->Category:       pkg
->Synopsis:       security/pcsc-lite: add rc.d file, please
->Confidential:   no
->Severity:       non-critical
->Priority:       low
->Responsible:    pkg-manager
->State:          open
->Class:          change-request
->Submitter-Id:   net
->Arrival-Date:   Thu Jan 15 15:50:00 +0000 2015
->Originator:     ISIHARA Takanori
->Release:        pkgsrc -current 2015/1/16
->Organization:
-Nagoya *BSD Users' Group :-)
->Environment:
-NetBSD ishitPc9.home 6.1_STABLE NetBSD 6.1_STABLE (ISHITPC9) #21: Sun Sep 28 05:31:48 JST 2014  sysbuild@ishitPc9.home:/opt/sysbuild/netbsd-6/obj/i386/sys/arch/i386/compile/ISHITPC9 i386
-
->Description:
-Please add "/usr/pkg/share/example/rc.d/pcscd" to install files.
-
-
->How-To-Repeat:
-
->Fix:
-example rc.d file:
-
-#!!@RCD_SCRIPTS_SHELL@
+#!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: pcscd.sh,v 1.1 2015/01/15 20:56:03 wiz Exp $
+# $NetBSD: pcscd.sh,v 1.2 2015/01/17 18:43:15 wiz Exp $
 #
 
 # PROVIDE: pcscd
