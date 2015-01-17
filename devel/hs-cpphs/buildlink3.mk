69d4f9fabe0b4df5b399c87987b25b4c61fb3167
szptvlfn <szptvlfn>
2015-01-17 14:05:10 +0000

    Update to 1.18.7
    CHANGELOG:
    * (1.18.7): bugfix to accept a #include with absolute filepath

--- devel/hs-cpphs/buildlink3.mk
+++ devel/hs-cpphs/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2014/12/12 22:55:24 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2015/01/17 14:05:10 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cpphs
 
 .if !defined(HS_CPPHS_BUILDLINK3_MK)
 HS_CPPHS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.6
-BUILDLINK_ABI_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.6nb2
+BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.7
+BUILDLINK_ABI_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.7
 BUILDLINK_PKGSRCDIR.hs-cpphs?=	../../devel/hs-cpphs
 
 .include "../../textproc/hs-polyparse/buildlink3.mk"
