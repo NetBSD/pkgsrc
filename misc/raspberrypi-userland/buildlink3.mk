365dfaaa2a530610e2a2aebd617d2954b7f84d67
jmcneill <jmcneill@pkgsrc.org>
2015-01-18 21:41:36 +0000

    install vcos_attr.h header, bump pkg revision

--- misc/raspberrypi-userland/buildlink3.mk
+++ misc/raspberrypi-userland/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2013/04/01 21:02:02 jmcneill Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/01/18 21:41:36 jmcneill Exp $
 
 BUILDLINK_TREE+=	raspberrypi-userland
 
 .if !defined(RASPBERRYPI_USERLAND_BUILDLINK3_MK)
 RASPBERRYPI_USERLAND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.raspberrypi-userland+=	raspberrypi-userland>=20130128
+BUILDLINK_API_DEPENDS.raspberrypi-userland+=	raspberrypi-userland>=20130128nb1
 BUILDLINK_PKGSRCDIR.raspberrypi-userland?=	../../misc/raspberrypi-userland
 .endif	# RASPBERRYPI_USERLAND_BUILDLINK3_MK
 
