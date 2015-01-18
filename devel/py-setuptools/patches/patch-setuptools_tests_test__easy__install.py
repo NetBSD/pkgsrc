7341acfa02abb658132d8497a8aae192149ba93c
wiz <wiz@pkgsrc.org>
2015-01-18 17:32:51 +0000

    Add upstream bug report URL.

--- devel/py-setuptools/patches/patch-setuptools_tests_test__easy__install.py
+++ devel/py-setuptools/patches/patch-setuptools_tests_test__easy__install.py
@@ -1,6 +1,7 @@
-$NetBSD: patch-setuptools_tests_test__easy__install.py,v 1.1 2015/01/18 17:29:01 wiz Exp $
+$NetBSD: patch-setuptools_tests_test__easy__install.py,v 1.2 2015/01/18 17:32:51 wiz Exp $
 
 Fix comment -- this is not an interpreter.
+https://bitbucket.org/pypa/setuptools/issue/333/1201-probably-unintended-shebang-in
 
 --- setuptools/tests/test_easy_install.py.orig	2015-01-16 22:16:09.000000000 +0000
 +++ setuptools/tests/test_easy_install.py
