8eb5efaf46bd81ad3d7c4a720d50df5cc1cf2474
wiz <wiz@pkgsrc.org>
2015-01-18 18:52:01 +0000

    Update to 2.4.8:
    =================
    WebKitGTK+  2.4.8
    =================
    
      - Fix SSL connection issues with some websites after the POODLE vulnerability fix.
      - Fix a crash when loading flash plugins.
      - Fix build on GNU Hurd
      - Fix build on OS X.
      - Fix documentation of webkit_print_operation_get_page_setup().
      - Security fixes: CVE-2014-1344, CVE-2014-1384, CVE-2014-1385, CVE-2014-1386,
        CVE-2014-1387, CVE-2014-1388, CVE-2014-1389, CVE-2014-1390.

--- www/webkit-gtk/patches/patch-Source_WTF_wtf_Platform.h
+++ www/webkit-gtk/patches/patch-Source_WTF_wtf_Platform.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.3 2014/11/27 13:47:03 jmcneill Exp $
+$NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.4 2015/01/18 18:52:01 wiz Exp $
 
---- Source/WTF/wtf/Platform.h.orig	2014-10-22 08:16:09.000000000 +0000
+--- Source/WTF/wtf/Platform.h.orig	2015-01-07 09:45:42.000000000 +0000
 +++ Source/WTF/wtf/Platform.h
 @@ -632,6 +632,11 @@
  #define USE_SYSTEM_MALLOC 1
@@ -18,8 +18,8 @@ $NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.3 2014/11/27 13:47:03 jmcneill Exp 
     low-level interpreter. */
  #if !defined(ENABLE_LLINT) \
      && ENABLE(JIT) \
--    && (OS(DARWIN) || OS(LINUX) || OS(FREEBSD)) \
-+    && (OS(DARWIN) || OS(LINUX) || OS(FREEBSD) || OS(NETBSD)) \
+-    && (OS(DARWIN) || OS(LINUX) || OS(FREEBSD) || OS(HURD)) \
++    && (OS(DARWIN) || OS(LINUX) || OS(FREEBSD) || OS(NETBSD) || OS(HURD)) \
      && (PLATFORM(MAC) || PLATFORM(IOS) || PLATFORM(GTK)) \
      && (CPU(X86) || CPU(X86_64) || CPU(ARM_THUMB2) || CPU(ARM_TRADITIONAL) || CPU(ARM64) || CPU(MIPS) || CPU(SH4))
  #define ENABLE_LLINT 1
