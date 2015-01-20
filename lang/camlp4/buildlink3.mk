8dcab9becc2b47e307dbac41a52c72e6b73f9212
jaapb <jaapb@pkgsrc.org>
2015-01-20 15:40:14 +0000

    Revbump associated with ocaml 4.02.1

--- lang/camlp4/buildlink3.mk
+++ lang/camlp4/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/10/09 20:39:27 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/01/20 15:40:14 jaapb Exp $
 
 BUILDLINK_TREE+=	camlp4
 
 .if !defined(CAMLP4_BUILDLINK3_MK)
 CAMLP4_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.camlp4+=	camlp4>=4.02.0
+BUILDLINK_API_DEPENDS.camlp4+=	camlp4>=4.02.0nb1
 BUILDLINK_PKGSRCDIR.camlp4?=	../../lang/camlp4
 .endif	# CAMLP4_BUILDLINK3_MK
 
