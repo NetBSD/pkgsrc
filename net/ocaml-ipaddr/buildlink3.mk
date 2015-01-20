410fc482d07a6d7ea1d26667936c53d9f4841818
jaapb <jaapb@pkgsrc.org>
2015-01-20 16:31:26 +0000

    Updated net/ocaml-ipaddr to version 2.5.0. Changes include:
    * Add `with sexp` (de)serializers to all of the Ipaddr and Macaddr types.
    * Add `Ipaddr.V6.Prefix.of_netmask` for conversion from an IPv6
      address/netmask to prefix (useful for some binary interfaces).
    * Add `Ipaddr.V6.Prefix.netmask` to generate a netmask address from a
      prefix (useful for some binary interfaces).
    * Add `Ipaddr.Prefix.network` for generic prefix -> address conversion
    * Add `Ipaddr.Prefix.netmask` for generic prefix -> netmask conversion
    * Add `Ipaddr.V4.Prefix.of_netmask` for conversion from an
      address/netmask to prefix
    * Add `Ipaddr.V4.Prefix.netmask` to generate a netmask address from a prefix
    * Add an [Ipaddr_unix] module to convert to-and-from the standard library.
    * Add a toplevel pretty printer in the `ipaddr.top` package.
    * Add `of_string_raw` to `Ipaddr.V4.Prefix` and `Ipaddr.V6.Prefix`
    * Add `of_addr` to `Ipaddr.V4.Prefix` and `Ipaddr.V6.Prefix`
    * Add type `('v4,'v6) v4v6` to `Ipaddr` to represent version disjuncts
    * Add `Ipaddr.Prefix` module for generic prefix manipulation
    * Change `Ipaddr.V4.make` to accept `int` rather than `int32` (breaking)
    * Add IPv6 support
    * Add generic IP address support
    * Add type `scope` for classifying address scopes
    * Add `Ipaddr.V4.of_string_raw` for parsing inside of larger strings
    * Add `Ipaddr.V4.to_int16` and `Ipaddr.V4.of_int16`
    * Add `unspecified`, `nodes`, and `routers` constants to `Ipaddr.V4`
    * Add `Ipaddr.V4.Prefix.network_address` to put an address into a subnet
    * Add `of_address_string_exn`, `of_address_string`, `to_address_string`,
      `to_address_buffer` to `Ipaddr.V4.Prefix` to parse/print combined addr/prefix
    * Add `multicast_org`, `multicast_admin`, `multicast_link` subnet constants to
      `Ipaddr.V4.Prefix`
    * Add `Ipaddr.V4.scope` to classify IPv4 addresses
    * Add `Ipaddr.V4.is_global` and `Ipaddr.V4.is_multicast` predicates
    * Add optional `sep:char` argument to `Macaddr.to_string`
    * Remove internal use of Scanf.scanf

--- net/ocaml-ipaddr/buildlink3.mk
+++ net/ocaml-ipaddr/buildlink3.mk
@@ -1,12 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/11/19 12:00:48 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/01/20 16:31:26 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ipaddr
 
 .if !defined(OCAML_IPADDR_BUILDLINK3_MK)
 OCAML_IPADDR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ipaddr+=	ocaml-ipaddr>=1.0.0
+BUILDLINK_API_DEPENDS.ocaml-ipaddr+=	ocaml-ipaddr>=2.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-ipaddr?=	ocaml-ipaddr>=2.5.0
 BUILDLINK_PKGSRCDIR.ocaml-ipaddr?=	../../net/ocaml-ipaddr
+BUILDLINK_FILES.ocaml-ipaddr+=	lib/ocaml/site-lib/*
+
+.include "../../lang/ocaml/buildlink3.mk"
 .endif	# OCAML_IPADDR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ocaml-ipaddr
