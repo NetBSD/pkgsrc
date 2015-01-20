f89644b8baf663a130854d3557378f7a925dab2f
jaapb <jaapb>
2015-01-20 15:59:18 +0000

    Revbump associated with ocaml 4.02.1.

--- devel/ocamlify/buildlink3.mk
+++ devel/ocamlify/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/08/17 13:38:10 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/01/20 15:59:55 jaapb Exp $
 
 BUILDLINK_TREE+=	ocamlify
 
 .if !defined(OCAMLIFY_BUILDLINK3_MK)
 OCAMLIFY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocamlify+=	ocamlify>=0.0.2
+BUILDLINK_API_DEPENDS.ocamlify+=	ocamlify>=0.0.2nb1
 BUILDLINK_PKGSRCDIR.ocamlify?=	../../devel/ocamlify
 .endif	# OCAMLIFY_BUILDLINK3_MK
 
