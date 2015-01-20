4cb9daebd3dfbf7a2a0b11d2a020f67d93abdda7
jaapb <jaapb@pkgsrc.org>
2015-01-20 15:22:53 +0000

    Revbump associated with ocaml 4.02.1

--- devel/ocaml-react/buildlink3.mk
+++ devel/ocaml-react/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/11/21 07:14:32 hiramatsu Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/01/20 15:22:53 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-react
 
 .if !defined(OCAML_REACT_BUILDLINK3_MK)
 OCAML_REACT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-react+=	ocaml-react>=1.2.0
+BUILDLINK_API_DEPENDS.ocaml-react+=	ocaml-react>=1.2.0nb1
 BUILDLINK_PKGSRCDIR.ocaml-react?=	../../devel/ocaml-react
 .endif	# OCAML_REACT_BUILDLINK3_MK
 
