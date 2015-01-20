9098d7f62fae431d383ed94fa111097e759b6cce
jaapb <jaapb@pkgsrc.org>
2015-01-20 16:27:02 +0000

    Added package devel/ocaml-sexplib, a library for S-Expressions in OCaml.

---
+++ devel/ocaml-sexplib/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/01/20 16:27:02 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-sexplib
+
+.if !defined(OCAML_SEXPLIB_BUILDLINK3_MK)
+OCAML_SEXPLIB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=112.01.00
+BUILDLINK_PKGSRCDIR.ocaml-sexplib?=	../../devel/ocaml-sexplib
+
+.endif	# OCAML_SEXPLIB_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-sexplib
