ed77297105a5bc85880fd1f6d8af42b91d784fd3
jaapb <jaapb@pkgsrc.org>
2015-01-20 16:04:57 +0000

    Revbump associated with ocaml 4.02.1.

--- textproc/ocaml-uutf/buildlink3.mk
+++ textproc/ocaml-uutf/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/11/06 11:29:15 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/01/20 16:04:57 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-uutf
 
 .if !defined(OCAML_UUTF_BUILDLINK3_MK)
 OCAML_UUTF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-uutf+=	ocaml-uutf>=0.9.3
+BUILDLINK_API_DEPENDS.ocaml-uutf+=	ocaml-uutf>=0.9.3nb1
 BUILDLINK_PKGSRCDIR.ocaml-uutf?=	../../textproc/ocaml-uutf
 .endif	# OCAML_UUTF_BUILDLINK3_MK
 
