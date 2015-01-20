fe0688b531530dd09324f69344527986b70088a4
jaapb <jaapb@pkgsrc.org>
2015-01-20 15:59:18 +0000

    Revbump associated with ocaml 4.02.1.

--- devel/ocamlmod/buildlink3.mk
+++ devel/ocamlmod/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/08/17 13:41:42 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/01/20 15:59:55 jaapb Exp $
 
 BUILDLINK_TREE+=	ocamlmod
 
 .if !defined(OCAMLMOD_BUILDLINK3_MK)
 OCAMLMOD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocamlmod+=	ocamlmod>=0.0.7
+BUILDLINK_API_DEPENDS.ocamlmod+=	ocamlmod>=0.0.7nb1
 BUILDLINK_PKGSRCDIR.ocamlmod?=	../../devel/ocamlmod
 .endif	# OCAMLMOD_BUILDLINK3_MK
 
