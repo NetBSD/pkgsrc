d9252893c73e4f197cfb64ce650517bc103da348
jaapb <jaapb@pkgsrc.org>
2015-01-20 14:24:34 +0000

    Revbump associated with update of lang/ocaml.

--- devel/ocaml-type_conv/buildlink3.mk
+++ devel/ocaml-type_conv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/10/09 19:14:03 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/01/20 14:24:35 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-type_conv
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-type_conv
 OCAML_TYPE_CONV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-type_conv+=	ocaml-type_conv>=109.60.01
-BUILDLINK_ABI_DEPENDS.ocaml-type_conv?=	ocaml-type_conv>=111.13.00nb1
+BUILDLINK_ABI_DEPENDS.ocaml-type_conv?=	ocaml-type_conv>=111.13.00nb2
 BUILDLINK_PKGSRCDIR.ocaml-type_conv?=	../../devel/ocaml-type_conv
 
 .include "../../lang/ocaml/buildlink3.mk"
