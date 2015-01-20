0ebad5f783099118791a00e09a9cb48770c49103
jaapb <jaapb@pkgsrc.org>
2015-01-20 14:24:34 +0000

    Revbump associated with update of lang/ocaml.

--- x11/lablgtk1/buildlink3.mk
+++ x11/lablgtk1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2014/10/09 19:14:05 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2015/01/20 14:24:38 jaapb Exp $
 
 BUILDLINK_TREE+=	lablgtk1
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	lablgtk1
 LABLGTK1_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.lablgtk1+=	lablgtk1>=1.2.5nb3
-BUILDLINK_ABI_DEPENDS.lablgtk1+=	lablgtk1>=1.2.7nb9
+BUILDLINK_ABI_DEPENDS.lablgtk1+=	lablgtk1>=1.2.7nb10
 BUILDLINK_PKGSRCDIR.lablgtk1?=		../../x11/lablgtk1
 
 .include "../../x11/gtk/buildlink3.mk"
