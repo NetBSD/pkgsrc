3b218276a64dc29f61c2114535deb1a06769546b
obache <obache>
2015-01-21 09:12:41 +0000

    recuesive bump from libarchive major update.

--- print/libgxps/buildlink3.mk
+++ print/libgxps/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2014/05/05 00:48:17 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2015/01/21 09:12:41 obache Exp $
 
 BUILDLINK_TREE+=	libgxps
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libgxps
 LIBGXPS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libgxps+=	libgxps>=0.2.1
-BUILDLINK_ABI_DEPENDS.libgxps?=	libgxps>=0.2.2nb14
+BUILDLINK_ABI_DEPENDS.libgxps?=	libgxps>=0.2.2nb15
 BUILDLINK_PKGSRCDIR.libgxps?=	../../print/libgxps
 
 .include "../../devel/glib2/buildlink3.mk"
