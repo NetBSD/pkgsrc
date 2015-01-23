cc2f107ce97f150496665b3ec0501e6adef5597b
joerg <joerg@pkgsrc.org>
2015-01-23 15:06:00 +0000

    Apply strtoi fix for NetBSD/current to all PostgreSQL versions.

---
+++ databases/postgresql91/patches/patch-src_backend_utils_adt_datetime.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-src_backend_utils_adt_datetime.c,v 1.1 2015/01/23 15:06:01 joerg Exp $
+
+--- src/backend/utils/adt/datetime.c.orig	2014-07-21 19:14:13.000000000 +0000
++++ src/backend/utils/adt/datetime.c
+@@ -29,6 +29,9 @@
+ #include "utils/memutils.h"
+ #include "utils/tzparser.h"
+ 
++#if defined(__NetBSD__)
++#define strtoi pg_strtoi
++#endif
+ 
+ static int DecodeNumber(int flen, char *field, bool haveTextMonth,
+ 			 int fmask, int *tmask,
