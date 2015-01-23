415433dc65b00ed84c817889d325f3bdebce2921
joerg <joerg@pkgsrc.org>
2015-01-23 15:06:00 +0000

    Apply strtoi fix for NetBSD/current to all PostgreSQL versions.

---
+++ databases/postgresql90/patches/patch-src_interfaces_ecpg_pgtypeslib_interval.c
@@ -0,0 +1,15 @@
+$NetBSD: patch-src_interfaces_ecpg_pgtypeslib_interval.c,v 1.1 2015/01/23 15:06:01 joerg Exp $
+
+--- src/interfaces/ecpg/pgtypeslib/interval.c.orig	2014-07-21 19:16:01.000000000 +0000
++++ src/interfaces/ecpg/pgtypeslib/interval.c
+@@ -14,6 +14,10 @@
+ #include "pgtypes_error.h"
+ #include "pgtypes_interval.h"
+ 
++#if defined(__NetBSD__)
++#define strtoi pg_strtoi
++#endif
++
+ /* copy&pasted from .../src/backend/utils/adt/datetime.c */
+ static int
+ strtoi(const char *nptr, char **endptr, int base)
---
+++ databases/postgresql92/patches/patch-src_backend_utils_adt_datetime.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-src_backend_utils_adt_datetime.c,v 1.1 2015/01/23 15:06:01 joerg Exp $
+
+--- src/backend/utils/adt/datetime.c.orig	2014-07-21 19:12:31.000000000 +0000
++++ src/backend/utils/adt/datetime.c
+@@ -30,6 +30,9 @@
+ #include "utils/memutils.h"
+ #include "utils/tzparser.h"
+ 
++#if defined(__NetBSD__)
++#define strtoi pg_strtoi
++#endif
+ 
+ static int DecodeNumber(int flen, char *field, bool haveTextMonth,
+ 			 int fmask, int *tmask,
