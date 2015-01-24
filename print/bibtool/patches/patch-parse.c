046951e533e569be06e17c793f8999eca554b5b3
mef <mef>
2015-01-24 13:40:14 +0000

    (pkgsrc)  - Remove patch-main.c (it was for 'Remove wrong casts.'). (upstream)  - update 2.48 to 2.57 ---------------------------------------------- Release 2.57 (January 24, 2015, Gerd Neugebauer)  - Segfault in deTeX fixed.  - Compiler warnings silenced.
    Release 2.56
     - Disambiguating numbers adapted to fit to documentation.
     - Configuration of regex fixed to work on Linux.
     - Documentation typos fixed.
     - Signed characters fro translation tables changed to unsigned.
     - Autoconf configuration improved.
    
    Release 2.55
     - Library biblatex.rsc added. It contains capitalizations of fields used in bibLATEX.
     - Fix for a misbehaviour when selecting entries according to an aux file with deeply
    nested @strings.
    
    Release 2.54
     - Command line parameter -V documented.
     - Resource key.make.alias added to create new @ALIAS records for any newly generated key.
     - Resource apply.alias added to expand the @ALIAS records.
     - Resource apply.include added to expand the @INCLUDE records.
     - Resource apply.modify added to expand the @MODIFY records.
     - Error message for -o without parameter added.
     - @ALIAS and @INCLUDE records where not printed. This has been fixed.
     - A test suite for BibTool has been integrated. It requires Perl to be present on
    the system on which the tests should be run.
    
    Release 2.53
     - In tex.define spaces before the = are ignored instead of leading to unwanted
       definitions.
     - An intialization error showed up on MacOS. This has been fixed.
     - The prepared makefiles for various operating systems missed an entry for crossref.[cho].
     - Typo in help text and copyright year fixed.
    
    Release 2.52
     - A few incompatibilities with signed and unsigned characters which showed up on
    MacOS only have been fixed.
     - A few compiler warnings have been fixed.
    
    Release 2.51
     - Bugfix in names.c: The classification of name parts erroneously has used a wrong
    translation under certain circumstances.
    
    Release 2.50
     - Bugfix in names.c: The classification of name parts erroneously has used a wrong
       translation under certain circumstances.
     - Bugfix in key.c: The classification of name parts erroneously has considered, to be
       a first name under certain circumstances. This has been improved.
     - Improvement in names.c and key.c concerning printing in debug mode.
    
    Release 2.49
     - New resource expand.crossref introduced. This resource enables the expansion
       of fields inherited via the crossref mechanism.
     - Bugfix in names.c: The classification of name parts erroneously has considered the
       initials V., I., and X. to be Roman numerals belonging to the jr part under special
       circumstances. This has been improved.
     - Name formatting now uses a fractional part to restrict the number of name parts
       considered.
     - New resource expand.crossref introduced. This resource enables the expansion
       of fields inherited via the crossref mechanism.
     - Bugfix in print.c: Some national characters have been lost. It was necessary to use
       an unsigned character as elsewhere.

--- print/bibtool/patches/patch-parse.c
+++ print/bibtool/patches/patch-parse.c
@@ -1,23 +1,25 @@
-$NetBSD: patch-parse.c,v 1.1 2012/12/24 01:50:58 dholland Exp $
+$NetBSD: patch-parse.c,v 1.2 2015/01/24 13:40:14 mef Exp $
 
 Fix bug handling EOF, detected by gcc.
 
---- parse.c~	2004-02-08 09:41:45.000000000 +0000
-+++ parse.c
-@@ -533,13 +533,15 @@ static void parse_number()			   /*			   
+--- parse.c.orig	2014-01-14 04:16:16.000000000 +0900
++++ parse.c	2015-01-24 22:13:12.000000000 +0900
+@@ -536,14 +536,16 @@ static void parse_number()			   /*			   
  static int parse_string(quotep)			   /*			     */
-   int   quotep;				   	   /*			     */
- { Uchar c;					   /*                        */
-+  int   c1;					   /*                        */
-   int   left;				   	   /*			     */
-   int   start_flno = flno;			   /*                        */
+   int quotep;				   	   /*			     */
+ { int c;					   /*                        */
++  int c1;
+   int left;				   	   /*			     */
+   int start_flno = flno;			   /*                        */
  						   /*			     */
    left = 0;					   /*			     */
    if ( quotep ) (void)sbputchar('"',parse_sb);	   /*"			     */
    do						   /*			     */
 -  { switch ( c=skip_nl() )			   /*			     */
-+  { c=c1=skip_nl();				   /*                        */
+-    { case EOF:					   /*                        */
++  { c=c1=skip_nl();				   /*			     */
 +    switch ( c1 )				   /*			     */
-     { case EOF:					   /*                        */
- 	UnterminatedError("Unterminated double quote",
- 			  start_flno);
++    { case EOF:					   /*			     */
+ 	UnterminatedError("Unterminated double quote",/*                     */
+ 			  start_flno);		   /*                        */
+ 	return(FALSE);	   			   /*			     */
