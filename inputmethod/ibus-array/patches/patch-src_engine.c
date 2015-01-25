e393cdd67b807ac56df8ccc862a0b03d4fdf862f
joerg <joerg@pkgsrc.org>
2015-01-25 13:12:02 +0000

    Don't declare a function non-void if it never returns a value and no return value is expected.

---
+++ inputmethod/ibus-array/patches/patch-src_engine.c
@@ -0,0 +1,22 @@
+$NetBSD: patch-src_engine.c,v 1.4 2015/01/25 13:12:02 joerg Exp $
+
+--- src/engine.c.orig	2015-01-24 16:30:52.000000000 +0000
++++ src/engine.c
+@@ -67,7 +67,7 @@ static gboolean ibus_array_engine_commit
+ 
+ static void ibus_array_engine_space_press(IBusArrayEngine *arrayeng);
+ 
+-static gboolean ibus_array_engine_update_symbol_lookup_table (IBusArrayEngine *arrayeng);
++static void ibus_array_engine_update_symbol_lookup_table (IBusArrayEngine *arrayeng);
+ 
+ static void ibus_array_engine_update_auxiliary_text (IBusArrayEngine *arrayeng, gchar* aux_string);
+ 
+@@ -313,7 +313,7 @@ static void ibus_array_engine_update_pre
+     g_string_free(array_preedit, TRUE);
+ }
+ 
+-static gboolean ibus_array_engine_update_symbol_lookup_table (IBusArrayEngine *arrayeng) 
++static void ibus_array_engine_update_symbol_lookup_table (IBusArrayEngine *arrayeng) 
+ {
+     gint i;
+     gboolean retval;
