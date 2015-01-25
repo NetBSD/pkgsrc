e0b4803fca61e917065394d8401c5b9b0832482a
joerg <joerg@pkgsrc.org>
2015-01-25 13:15:34 +0000

    utmpx change for FreeBSD broke NetBSD, so try to do the right thing and always use it.

--- x11/kde-workspace4/patches/patch-kdm_kfrontend_genkdmconf.c
+++ x11/kde-workspace4/patches/patch-kdm_kfrontend_genkdmconf.c
@@ -1,27 +1,42 @@
-$NetBSD: patch-kdm_kfrontend_genkdmconf.c,v 1.2 2015/01/15 17:59:10 rumko Exp $
+$NetBSD: patch-kdm_kfrontend_genkdmconf.c,v 1.3 2015/01/25 13:15:34 joerg Exp $
 
-On some systems (e.g. fbsd 10+) utmp is no longer available, use utmpx instead.
+Use utmpx on all BSDs.
 
 --- kdm/kfrontend/genkdmconf.c.orig	2014-11-06 22:36:58.000000000 +0000
 +++ kdm/kfrontend/genkdmconf.c
-@@ -46,12 +46,18 @@ Foundation, Inc., 51 Franklin Street, Fi
+@@ -46,16 +46,7 @@ Foundation, Inc., 51 Franklin Street, Fi
  #include <sys/wait.h>
  #include <sys/param.h>
  #ifdef BSD
 -# ifdef BSD_UTMP
-+# ifdef HAVE_UTMPX
-+#  include <utmpx.h>
-+# else
- #  include <utmp.h>
- # endif
- # ifndef _PATH_UTMP
- #  if defined(__FreeBSD_version) && __FreeBSD_version >= 900007
+-#  include <utmp.h>
+-# endif
+-# ifndef _PATH_UTMP
+-#  if defined(__FreeBSD_version) && __FreeBSD_version >= 900007
 -#   define _PATH_UTMP "/var/run/utmp"
-+#   if __FreeBSD_version >= 1001000
-+#    define _PATH_UTMP "/var/run/utx.active"
-+#   else
-+#    define _PATH_UTMP "/var/run/utmp"
-+#   endif
- #  else
- #   error "_PATH_UTMP must be defined"
- #  endif
+-#  else
+-#   error "_PATH_UTMP must be defined"
+-#  endif
+-# endif
++#  include <utmpx.h>
+ #endif
+ 
+ #define WANT_CONF_GEN
+@@ -1875,7 +1866,7 @@ edit_startup(File *file)
+ #else
+ # ifdef BSD
+             delstr(file, "\n"
+-"exec sessreg -a -l $DISPLAY -x */Xservers -u " _PATH_UTMP " $USER\n") |
++"exec sessreg -a -l $DISPLAY -x */Xservers -u " _PATH_UTMPX " $USER\n") |
+ # endif
+ #endif /* _AIX */
+             delstr(file, "\n"
+@@ -1924,7 +1915,7 @@ edit_reset(File *file)
+ #else
+ # ifdef BSD
+         delstr(file, "\n"
+-"exec sessreg -d -l $DISPLAY -x */Xservers -u " _PATH_UTMP " $USER\n") |
++"exec sessreg -d -l $DISPLAY -x */Xservers -u " _PATH_UTMPX " $USER\n") |
+ # endif
+ #endif /* _AIX */
+         delstr(file, "\n"
