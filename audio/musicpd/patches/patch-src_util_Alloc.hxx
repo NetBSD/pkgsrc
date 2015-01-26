9a963a189e1c359c78eec6c5e502f9c1b6e84bb6
pho <pho>
2015-01-26 08:22:25 +0000

    patches/patch-src_util_Alloc.{c,h}xx: Add a note that the revised patch has also been rejected.
    The upstream is not willing to accept any patches to fix build failures on outdated platforms, regardless of whether the patch is rational or not.

--- audio/musicpd/patches/patch-src_util_Alloc.hxx
+++ audio/musicpd/patches/patch-src_util_Alloc.hxx
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_util_Alloc.hxx,v 1.3 2015/01/26 04:17:53 pho Exp $
+$NetBSD: patch-src_util_Alloc.hxx,v 1.4 2015/01/26 08:22:25 pho Exp $
 
 Some platforms (e.g. Darwin 9) don't have strndup(3), but the function
 xstrndup() isn't used in the first place.
 
 http://bugs.musicpd.org/view.php?id=4289 (rejected)
-http://bugs.musicpd.org/view.php?id=4291 (revised; pending)
+http://bugs.musicpd.org/view.php?id=4291 (revised; rejected too)
 
 --- src/util/Alloc.hxx.orig	2014-10-24 22:28:53.000000000 +0000
 +++ src/util/Alloc.hxx
