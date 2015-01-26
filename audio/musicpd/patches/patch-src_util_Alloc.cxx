906540362d90dcdf9c376d416d37a426dc59bf2c
pho <pho@pkgsrc.org>
2015-01-26 04:17:53 +0000

    patches/patch-src_util_Alloc.{cxx,hxx}: Add upstream bug report URL
    The previous report 4289 was rejected as the upstream doesn't have interest in Darwin support. I created a better patch and sent it to the upstream to see if it has any chance to be merged.

--- audio/musicpd/patches/patch-src_util_Alloc.cxx
+++ audio/musicpd/patches/patch-src_util_Alloc.cxx
@@ -1,8 +1,10 @@
-$NetBSD: patch-src_util_Alloc.cxx,v 1.2 2015/01/25 14:45:35 wiz Exp $
+$NetBSD: patch-src_util_Alloc.cxx,v 1.3 2015/01/26 04:17:53 pho Exp $
 
 Some platforms (e.g. Darwin 9) don't have strndup(3), but the function
 xstrndup() isn't used in the first place.
-http://bugs.musicpd.org/view.php?id=4289
+
+http://bugs.musicpd.org/view.php?id=4289 (rejected)
+http://bugs.musicpd.org/view.php?id=4291 (revised; pending)
 
 --- src/util/Alloc.cxx.orig	2014-10-24 22:28:53.000000000 +0000
 +++ src/util/Alloc.cxx
