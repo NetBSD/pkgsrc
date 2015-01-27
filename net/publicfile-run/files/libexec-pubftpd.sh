49caad88e0403a961a963e8201ba540be8390ef5
schmonz <schmonz@pkgsrc.org>
2015-01-27 23:01:31 +0000

    Add publicfile-run, a package that creates the users and group needed to run publicfile services and provides NetBSD-style rc.d scripts, in the pkgsrc tradition of qmail-run and djbdns-run. By J. Lewis Muir, from wip.

---
+++ net/publicfile-run/files/libexec-pubftpd.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#
+# Executes tcpserver with "pubftpd" as the 0th argument and with a fixed banner
+# appropriate for DJB's publicfile FTP server.
+#
+# The reason for this script, as opposed to invoking tcpserver directly, is
+# that the banner (specified with the -B option) needs to include a trailing
+# carriage-return and newline (\015\012) which is difficult, if not impossible,
+# to specify in a NetBSD rc(8) script because rc(8) uses "eval" to evaluate the
+# constructed command.
+#
+
+exec @UCSPI_TCP_PREFIX@/bin/argv0 @UCSPI_TCP_PREFIX@/bin/tcpserver pubftpd \
+-B'220 Features: a p .
+' "$@"
---
+++ net/publicfile-run/files/pubftpd.sh
@@ -0,0 +1,54 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: pubftpd.sh,v 1.1 2015/01/27 23:01:31 schmonz Exp $
+#
+# @PKGNAME@ script to control publicfile ftpd (FTP server)
+#
+
+# PROVIDE: pubftpd
+# REQUIRE: NETWORKING mountcritremote syslogd
+# BEFORE:  DAEMON
+
+name="pubftpd"
+
+# User-settable rc.conf variables and their default values:
+: ${pubftpd_root:="@VARBASE@/public"}
+: ${pubftpd_postenv:=""}
+: ${pubftpd_datalimit:="50000"}
+: ${pubftpd_localname:="0"}
+: ${pubftpd_backlog:="20"}
+: ${pubftpd_conlimit:="40"}
+: ${pubftpd_host:="0"}
+: ${pubftpd_port:="21"}
+: ${pubftpd_log:="YES"}
+: ${pubftpd_logcmd:="logger -t nb${name} -p ftp.info"}
+: ${pubftpd_nologcmd:="@DAEMONTOOLS_PREFIX@/bin/multilog -*"}
+
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
+fi
+
+rcvar=${name}
+required_dirs="${pubftpd_root}"
+command="@PREFIX@/libexec/pubftpd"
+procname=${name}
+start_precmd="pubftpd_precmd"
+
+pubftpd_precmd()
+{
+	if [ -f /etc/rc.subr ]; then
+		checkyesno pubftpd_log || pubftpd_logcmd=${pubftpd_nologcmd}
+	fi
+ 	command="@SETENV@ - ${pubftpd_postenv} @DAEMONTOOLS_PREFIX@/bin/envuidgid pubftp @DAEMONTOOLS_PREFIX@/bin/softlimit -o20 -d${pubftpd_datalimit} @PREFIX@/libexec/pubftpd -vDRH -l${pubftpd_localname} -b${pubftpd_backlog} -c${pubftpd_conlimit} ${pubftpd_host} ${pubftpd_port} @PUBLICFILE_PREFIX@/publicfile/bin/ftpd ${pubftpd_root} 2>&1 | @DAEMONTOOLS_PREFIX@/bin/setuidgid publog ${pubftpd_logcmd}"
+	command_args="&"
+	rc_flags=""
+}
+
+if [ -f /etc/rc.subr ]; then
+	load_rc_config $name
+	run_rc_command "$1"
+else
+	@ECHO_N@ " ${name}"
+	pubftpd_precmd
+	eval ${command} ${pubftpd_flags} ${command_args}
+fi
