6c0f31ed00c4a674dc0b42ee4eb6800e7c15cc64
dbj <dbj@pkgsrc.org>
2015-01-27 06:10:15 +0000

    don't assume the native python on Darwin ignore the mac qt distfile, which only differs in documentation

--- x11/py-qt4/patches/patch-configure.py
+++ x11/py-qt4/patches/patch-configure.py
@@ -1,4 +1,4 @@
-$NetBSD: patch-aa,v 1.11 2014/08/21 21:33:07 wiz Exp $
+$NetBSD: patch-configure.py,v 1.1 2015/01/27 06:10:15 dbj Exp $
 
 --- configure.py.orig	2014-07-03 14:00:02.000000000 +0000
 +++ configure.py
@@ -22,6 +22,26 @@ $NetBSD: patch-aa,v 1.11 2014/08/21 21:33:07 wiz Exp $
          check_module("QtAssistant", "qassistantclient.h",
                  "new QAssistantClient(\"foo\")", extra_lib_dirs=ass_lib_dirs,
                  extra_libs=ass_libs)
+@@ -968,7 +968,7 @@ include(%s)
+ 
+                 config_args = ducfg.get("CONFIG_ARGS", "")
+ 
+-                if sys.platform == "darwin":
++                if False: # sys.platform == "darwin":
+                     dynamic_pylib = "--enable-framework" in config_args
+ 
+                     # It's probably a Python bug that the library name doesn't
+@@ -1963,8 +1963,8 @@ def get_qt_configuration():
+         else:
+             exe_file = os.path.join("release", app + ".exe")
+             make_target = " release"
+-    elif sys.platform == "darwin":
+-        exe_file = os.path.join(app + ".app", "Contents", "MacOS", app)
++#    elif sys.platform == "darwin":
++#        exe_file = os.path.join(app + ".app", "Contents", "MacOS", app)
+     else:
+         exe_file = os.path.join(".", app)
+ 
 @@ -2027,11 +2027,7 @@ int main(int argc, char **argv)
  
      out << QLibraryInfo::licensee() << '\\n';
