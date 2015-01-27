5609e54764c0c8c48f0f181388feb828a9224e26
dbj <dbj@pkgsrc.org>
2015-01-27 06:03:17 +0000

    Add PLIST support for Darwin audio fix Nocore.mk to handle PLIST.Darwin and use _PLIST_SHLIB_AWK to fix shared library names

--- ham/gnuradio-core/Nocore.mk
+++ ham/gnuradio-core/Nocore.mk
@@ -1,4 +1,4 @@
-# $NetBSD: Nocore.mk,v 1.4 2014/10/28 16:59:33 mef Exp $
+# $NetBSD: Nocore.mk,v 1.5 2015/01/27 06:03:17 dbj Exp $
 # Default list for reducing PLIST
 PLIST_MINUS=	core
 DEPENDS+=	gnuradio-core-[0-9]*:../../ham/gnuradio-core
@@ -10,15 +10,19 @@ post-install:
 # Take care CONF_FILES stuff
 	${MV}		    ${DESTDIR}${PREFIX}/etc/gnuradio/conf.d/* \
 			    ${DESTDIR}${PREFIX}/${EGDIR}/
-# to avoid sh: /bin/rm: argument list too long in case repeated..
-	${RM} -f ${WRKDIR}/.PLIST.minus;
-	for i in ${PLIST_MINUS} ; do			\
-	  sed  -e 's,$${PYSITELIB},${PYSITELIB},'	\
+	for i in ${PLIST_MINUS} ; do				\
+	  for p in PLIST PLIST.${OPSYS} ; do			\
+	    f="${PKGDIR}/../../ham/gnuradio-$${i}/$${p}";	\
+	    if [ -f "$${f}" ]; then				\
+	      ${SED} -e 's,$${PYSITELIB},${PYSITELIB},'		\
 	       -e 's,$${PKGVERSION},${PKGVERSION_NOREV},'	\
-	       -e '/^@comment/d'			\
-	       -e '/^@pkgdir/d'				\
-	       ${PKGDIR}/../../ham/gnuradio-$$i/PLIST	\
-		 >> ${WRKDIR}/.PLIST.minus;		\
-		done;					\
-	  (cd ${WRKDIR}/.destdir/${PREFIX};		\
-	${RM} -f `cat ${WRKDIR}/.PLIST.minus`	);
+	       -e '/^@comment/d'				\
+	       -e '/^@pkgdir/d'					\
+	       "$${f}";						\
+	    fi							\
+	  done							\
+	done							\
+	| ${AWK} ${_PLIST_SHLIB_AWK}				\
+	> ${WRKDIR}/.PLIST.minus;
+	(cd ${WRKDIR}/.destdir/${PREFIX};			\
+	${RM} -f $$(cat ${WRKDIR}/.PLIST.minus)	);
