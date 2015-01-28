281bd39fb7ef5bde27329191b38f91ac350f3c59
wiz <wiz@pkgsrc.org>
2015-01-28 20:39:54 +0000

    Add ldap option, and mark as 2.x only when either option is selected.

--- net/Radicale/options.mk
+++ net/Radicale/options.mk
@@ -1,10 +1,16 @@
-# $NetBSD: options.mk,v 1.1 2015/01/28 07:22:51 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2015/01/28 20:39:54 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.Radicale
-PKG_SUPPORTED_OPTIONS=	git
+PKG_SUPPORTED_OPTIONS=	git ldap
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgit)
+PYTHON_VERSIONS_INCOMPATIBLE=	33 34 # py-dulwich
 DEPENDS+=	${PYPKGPREFIX}-dulwich-[0-9]*:../../devel/py-dulwich
+.endif
+
+.if !empty(PKG_OPTIONS:Mldap)
+PYTHON_VERSIONS_INCOMPATIBLE=	33 34 # py-ldap
+DEPENDS+=	${PYPKGPREFIX}-ldap-[0-9]*:../../databases/py-ldap
 .endif
