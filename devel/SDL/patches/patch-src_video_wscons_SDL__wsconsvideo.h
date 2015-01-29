fbbb065800abad70d4ffba53a4c4d1706fafb6a2
jmcneill <jmcneill@pkgsrc.org>
2015-01-29 11:28:50 +0000

    fix wscons keyboard

--- devel/SDL/patches/patch-src_video_wscons_SDL__wsconsvideo.h
+++ devel/SDL/patches/patch-src_video_wscons_SDL__wsconsvideo.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_video_wscons_SDL__wsconsvideo.h,v 1.1 2015/01/29 01:56:23 jmcneill Exp $
+$NetBSD: patch-src_video_wscons_SDL__wsconsvideo.h,v 1.2 2015/01/29 11:28:50 jmcneill Exp $
 
 --- src/video/wscons/SDL_wsconsvideo.h.orig	2012-01-19 06:30:06.000000000 +0000
 +++ src/video/wscons/SDL_wsconsvideo.h
@@ -10,3 +10,11 @@ $NetBSD: patch-src_video_wscons_SDL__wsconsvideo.h,v 1.1 2015/01/29 01:56:23 jmc
    struct wsdisplay_fbinfo info; /* frame buffer characteristics */
    int physlinebytes;            /* number of bytes per row */
    int redMask, greenMask, blueMask;
+@@ -70,6 +71,7 @@ struct SDL_PrivateVideoData {
+   unsigned int kbdType;
+   int did_save_tty;
+   struct termios saved_tty;
++  struct wskbd_keyrepeat_data saved_repeat;
+ };
+ 
+ 
