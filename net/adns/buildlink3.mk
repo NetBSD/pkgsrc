c17310ef201de9585c11e847d14c1d99d7fa5c3a
mef <mef@pkgsrc.org>
2015-01-29 09:53:29 +0000

    recursive revbump due to net/adns update 1.4 to 1.5.0

--- net/adns/buildlink3.mk
+++ net/adns/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:25:07 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2015/01/29 09:53:29 mef Exp $
 
 BUILDLINK_TREE+=	adns
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	adns
 ADNS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.adns+=	adns>=1.0
-BUILDLINK_ABI_DEPENDS.adns+=	adns>=1.1nb2
+BUILDLINK_ABI_DEPENDS.adns+=	adns>=1.5.0
 BUILDLINK_PKGSRCDIR.adns?=	../../net/adns
 .endif # ADNS_BUILDLINK3_MK
 
