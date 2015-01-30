11da2829e7ac766fa698e2782909affa0f70b204
mef <mef@pkgsrc.org>
2015-01-30 15:02:23 +0000

    Fix another problem on PLIST for gnuradio-*. Add PLIST.oss to be anonther removing PLIST other than gnuradio-core packages. (One more, move the line location of comment lines next to the line to explain)

--- ham/gnuradio-core/Nocore.mk
+++ ham/gnuradio-core/Nocore.mk
@@ -1,7 +1,10 @@
-# $NetBSD: Nocore.mk,v 1.5 2015/01/27 06:03:17 dbj Exp $
-# Default list for reducing PLIST
-PLIST_MINUS=	core
+# $NetBSD: Nocore.mk,v 1.6 2015/01/30 15:02:23 mef Exp $
+
 DEPENDS+=	gnuradio-core-[0-9]*:../../ham/gnuradio-core
+
+# Default list for reducing PLIST
+# gnuradio-* names to be common (to get the list to reduce)
+PLIST_MINUS=	core
 
 # Remove the same files gnuradio-core installs
 # Do a part of PLIST_SUBST and remove non real file listings
@@ -11,7 +14,7 @@ post-install:
 	${MV}		    ${DESTDIR}${PREFIX}/etc/gnuradio/conf.d/* \
 			    ${DESTDIR}${PREFIX}/${EGDIR}/
 	for i in ${PLIST_MINUS} ; do				\
-	  for p in PLIST PLIST.${OPSYS} ; do			\
+	  for p in PLIST PLIST.oss PLIST.${OPSYS} ; do		\
 	    f="${PKGDIR}/../../ham/gnuradio-$${i}/$${p}";	\
 	    if [ -f "$${f}" ]; then				\
 	      ${SED} -e 's,$${PYSITELIB},${PYSITELIB},'		\
