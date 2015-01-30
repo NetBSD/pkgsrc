0d7d34e0e829104fcab0a02698fdaf4e5b7aba66
obache <obache@pkgsrc.org>
2015-01-30 11:06:36 +0000

    Add unlink as a tool.

--- mk/tools/tools.AIX.mk
+++ mk/tools/tools.AIX.mk
@@ -1,4 +1,4 @@
-# $NetBSD: tools.AIX.mk,v 1.17 2014/03/06 07:34:20 uebayasi Exp $
+# $NetBSD: tools.AIX.mk,v 1.18 2015/01/30 11:06:36 obache Exp $
 #
 # System-supplied tools for the AIX operating system.
 
@@ -64,6 +64,7 @@ TOOLS_PLATFORM.tr?=		/usr/bin/tr
 TOOLS_PLATFORM.true?=		true			# shell builtin
 TOOLS_PLATFORM.tsort?=		/usr/bin/tsort
 TOOLS_PLATFORM.uniq?=		/usr/bin/uniq
+TOOLS_PLATFORM.unlink?=		/usr/sbin/unlink
 TOOLS_PLATFORM.wc?=		/usr/bin/wc
 TOOLS_PLATFORM.xargs?=		/usr/bin/xargs
 
