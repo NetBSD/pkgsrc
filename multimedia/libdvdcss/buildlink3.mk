33a1a772ca258fa363d063ae8e576909f764291b
adam <adam@pkgsrc.org>
2015-01-31 21:24:11 +0000

    Changes 1.3.99: * Drop support for HP-UX. * Drop support for Windows 9x and Windows NT.   Windows 2000 is now required. * Replace BeOS support by Haiku support. * Add dvdcss_open_stream() to public API. This allows installing custom   callback functions for accessing DVD, e.g. over the network. * dvdcss_error() now returns "const char *" instad of "char *". * Drop support for MSVC versions before 2010. * Raw device access now errors out if the device cannot be opened. * Miscellaneous cleanups to code, documentation, build system.

--- multimedia/libdvdcss/buildlink3.mk
+++ multimedia/libdvdcss/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:25:03 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2015/01/31 21:24:11 adam Exp $
 
 BUILDLINK_TREE+=	libdvdcss
 
 .if !defined(LIBDVDCSS_BUILDLINK3_MK)
 LIBDVDCSS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libdvdcss+=	libdvdcss>=1.2.4
-BUILDLINK_ABI_DEPENDS.libdvdcss+=	libdvdcss>=1.2.8nb1
+BUILDLINK_API_DEPENDS.libdvdcss+=	libdvdcss>=1.3.99
+BUILDLINK_ABI_DEPENDS.libdvdcss+=	libdvdcss>=1.3.99
 BUILDLINK_PKGSRCDIR.libdvdcss?=	../../multimedia/libdvdcss
 .endif # LIBDVDCSS_BUILDLINK3_MK
 
