0dc8b78c47d21817009eb107919964c2bf2d1580
mef <mef@pkgsrc.org>
2015-01-31 06:18:42 +0000

    Update 2.2.3 to 2.3.6 --------------------- ChangeLog unknown.

--- biology/primer3/patches/patch-src_primer3__boulder__main.c
+++ biology/primer3/patches/patch-src_primer3__boulder__main.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_primer3__boulder__main.c,v 1.1.1.1 2011/10/31 21:25:57 brook Exp $
+$NetBSD: patch-src_primer3__boulder__main.c,v 1.2 2015/01/31 06:18:43 mef Exp $
 
 getopt_long_only is not very portable; use getopt_long instead.
 
---- src/primer3_boulder_main.c.orig	2010-07-08 17:58:25.000000000 +0000
-+++ src/primer3_boulder_main.c
-@@ -133,7 +133,7 @@ main(int argc, char *argv[]) 
-   
+--- src/primer3_boulder_main.c.orig	2013-10-21 16:49:57.000000000 +0900
++++ src/primer3_boulder_main.c	2015-01-31 15:12:56.000000000 +0900
+@@ -128,7 +128,7 @@ main(int argc, char *argv[])
+ 
    /* Read in the flags provided with the program call */
    opterr = 0;
 -  while ((opt = getopt_long_only(argc, argv, "", long_options, &option_index)) != -1) {
