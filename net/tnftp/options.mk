c1f8e20ca6f3bc590603f31d60dc7738fe438f22
obache <obache@pkgsrc.org>
2015-02-01 08:58:52 +0000

    Revert "ssl" option handling changes.
    "configure fix patch" was sent to MAINTAINER, but not approved yet.
    Moreover, this package is a part of bootstrap dependency, so changes must be
    proposed/discussed first.

--- net/tnftp/options.mk
+++ net/tnftp/options.mk
@@ -1,18 +1,10 @@
-# $NetBSD: options.mk,v 1.8 2015/01/19 11:15:37 obache Exp $
+# $NetBSD: options.mk,v 1.9 2015/02/01 08:58:52 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tnftp
-PKG_SUPPORTED_OPTIONS=		inet6 ssl
+PKG_SUPPORTED_OPTIONS=		inet6
 PKG_OPTIONS_OPTIONAL_GROUPS+=	socks
 PKG_OPTIONS_GROUP.socks=	socks5
 PKG_SUGGESTED_OPTIONS=		inet6
-
-CHECK_BUILTIN.openssl:=	yes
-.include "../../security/openssl/builtin.mk"
-CHECK_BUILTIN.openssl:=	no
-
-.if !empty(USE_BUILTIN.openssl:M[yY][eE][sS])
-PKG_SUGGESTED_OPTIONS+=	ssl
-.endif
 
 .include "../../mk/bsd.options.mk"
 
@@ -27,12 +19,4 @@ CONFIGURE_ARGS+=	--without-socks
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
-.endif
-
-.if !empty(PKG_OPTIONS:Mssl)
-CONFIGURE_ARGS+=	--enable-ssl
-.include "../../security/openssl/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-openssl=${SSLBASE}
-.else
-CONFIGURE_ARGS+=	--disable-ssl
 .endif
