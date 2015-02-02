a5c0913179d6fa2d307acc17e8eab65c2ff3c873
adam <adam@pkgsrc.org>
2015-02-02 11:14:55 +0000

    Fixed buildlinking

--- converters/libcdr/buildlink3.mk
+++ converters/libcdr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/07/22 11:07:58 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2015/02/02 11:14:55 adam Exp $
 
 BUILDLINK_TREE+=	libcdr
 
@@ -6,13 +6,15 @@ BUILDLINK_TREE+=	libcdr
 LIBCDR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libcdr+=	libcdr>=0.0.6
-BUILDLINK_ABI_DEPENDS.libcdr?=	libcdr>=0.1.0
+BUILDLINK_ABI_DEPENDS.libcdr+=	libcdr>=0.1.0
 BUILDLINK_PKGSRCDIR.libcdr?=	../../converters/libcdr
 
 .include "../../converters/libwpd/buildlink3.mk"
 .include "../../converters/libwpg/buildlink3.mk"
 .include "../../converters/librevenge/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
 .include "../../graphics/lcms2/buildlink3.mk"
+.include "../../textproc/icu/buildlink3.mk"
 .endif	# LIBCDR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libcdr
