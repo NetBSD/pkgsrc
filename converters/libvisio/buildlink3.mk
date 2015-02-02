d0107c079747dab0e817afac3ad85e16502ba84f
adam <adam>
2015-02-02 11:34:39 +0000

    Fixed buildlinking; disabled documentation building as in other libraries.

--- converters/libvisio/buildlink3.mk
+++ converters/libvisio/buildlink3.mk
@@ -1,14 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/07/22 11:20:33 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/02/02 11:34:39 adam Exp $
 
 BUILDLINK_TREE+=	libvisio
 
 .if !defined(LIBVISIO_BUILDLINK3_MK)
 LIBVISIO_BUILDLINK3_MK:=
+
 BUILDLINK_API_DEPENDS.libvisio+=	libvisio>=0.0.15
 BUILDLINK_ABI_DEPENDS.libvisio+=	libvisio>=0.1.0
 BUILDLINK_PKGSRCDIR.libvisio?=		../../converters/libvisio
 
 .include "../../converters/librevenge/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../textproc/icu/buildlink3.mk"
 .endif
 
 BUILDLINK_TREE+=	-libvisio
