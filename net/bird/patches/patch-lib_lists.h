088d33e43c5c20381262b81143b0e241c947de34
mef <mef@pkgsrc.org>
2015-02-03 13:58:32 +0000

    (pkgsrc)  - Add comment on patches, by picking from cvs log (upstream)  - Update bird and bird6 from 1.3.11 to 1.4.5 ---------------------- Version 1.4.5 (2014-10-06)   o New 'show route noexport' command option.   o Port option for BGP sessions.   o Better constant handling in set literals.   o Better rate filtering of log messages.   o Several minor bugfixes.
    Version 1.4.4 (2014-07-09)
      o Extended OSPF multipath support.
      o Default router preference for RAdv.
      o Significant changes in socket layer.
      o Important bugfix in BGP.
      o Several minor bugfixes.
    
    Version 1.4.3 (2014-04-14)
      o Important bugfix in IPv6 BGP.
    
    Version 1.4.2 (2014-04-02)
      o Important bugfix in BFD.
    
    Version 1.4.1 (2014-03-31)
      o BGP add-path support (RFC draft).
      o BGP graceful restart (RFC 4724).
      o OSPF: many changes in socket layer.
      o OSPF: support for secondary addresses in BSD.
      o OSPF: names for vlink pseudointerfaces (vlinkX).
      o Several bugfixes.
    
    Version 1.4.0 (2013-11-25)
      o BFD protocol (RFC 5880).
      o BFD support for OSPF and BGP.
      o New 'allow local as' option for BGP.
      o Filters allows setting gw, ifname and ifindex.
      o Filter operator 'delete/filter' extended to bgp_paths.
      o Filter operator 'len' extended to [e]clists.
      o BIRD client now allows shorthands for noninteractive commands.
      o Flag -P for PID file support.
      o Flag -f added to force BIRD to run in foreground.
      o Protocol export/import/receive limits are checked during reconfiguration.
      o Several bugfixes and minor improvements.
      o Several minor but incompatible changes:
         - IBGP is multihop by default.
         - Changes primary address selection on BSD to the first one.
         - Integers in filters are handled as unsigned.
         - ISO 8601 time formats used by default.
         - Import of device routes from kernel protocol allowed.
         - Last state change now tracks just protocol state change.
         - Minor changes to default router ID calculation.

--- net/bird/patches/patch-lib_lists.h
+++ net/bird/patches/patch-lib_lists.h
@@ -1,31 +1,34 @@
-$NetBSD: patch-lib_lists.h,v 1.1 2012/06/29 12:37:45 joerg Exp $
+$NetBSD: patch-lib_lists.h,v 1.2 2015/02/03 13:58:32 mef Exp $
 
---- lib/lists.h.orig	2012-06-25 12:34:50.000000000 +0000
-+++ lib/lists.h
-@@ -47,19 +47,19 @@ typedef struct list {			/* In fact two o
+date: 2012-06-29 21:37:45 +0900;  author: joerg;  state: Exp;
+Fix inline usage.
+
+--- lib/lists.h.orig	2014-04-01 18:24:10.000000000 +0900
++++ lib/lists.h	2015-02-03 22:46:25.000000000 +0900
+@@ -50,13 +50,6 @@ typedef struct list {			/* In fact two o
  
  #define EMPTY_LIST(list) (!(list).head->next)
  
-+#ifndef _BIRD_LISTS_C_
-+#define LIST_INLINE static inline
-+#include "lib/lists.c"
-+#undef LIST_INLINE
-+#else
-+#define LIST_INLINE
-+
- void add_tail(list *, node *);
- void add_head(list *, node *);
- void rem_node(node *);
- void add_tail_list(list *, list *);
- void init_list(list *);
- void insert_node(node *, node *);
--
--#ifndef _BIRD_LISTS_C_
--#define LIST_INLINE extern inline
--#include "lib/lists.c"
--#undef LIST_INLINE
--#else
--#define LIST_INLINE
+-void add_tail(list *, node *);
+-void add_head(list *, node *);
+-void rem_node(node *);
+-void rem2_node(node *);
+-void add_tail_list(list *, list *);
+-void init_list(list *);
+-void insert_node(node *, node *);
+ 
+ #ifndef _BIRD_LISTS_C_
+ #define LIST_INLINE extern inline
+@@ -66,4 +59,12 @@ void insert_node(node *, node *);
+ #define LIST_INLINE
  #endif
  
++void add_tail(list *, node *);
++void add_head(list *, node *);
++void rem_node(node *);
++void rem2_node(node *);
++void add_tail_list(list *, list *);
++void init_list(list *);
++void insert_node(node *, node *);
++
  #endif
