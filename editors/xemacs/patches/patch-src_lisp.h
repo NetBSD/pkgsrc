9ffcf17a5cec3ff0e8418e3792a3c7938c455db6
hauke <hauke>
2015-02-04 09:19:20 +0000

    Update to XEmacs 21.4.23
    2015-01-29  Vin Shelton <acs@xemacs.org>
    
            * XEmacs 21.4.23 is released
    
    2015-01-08  Vin Shelton  <acs@xemacs.org>
    
            Fix progress bar crashes.
            Thanks to Ralf Soergel for diagnosis and a patch.
    
            * configure.in (Athena widgets):
            Test for "international" resource in SimpleWidgetClass in libXaw3d.
            * configure: Rebuild.

--- editors/xemacs/patches/patch-src_lisp.h
+++ editors/xemacs/patches/patch-src_lisp.h
@@ -1,8 +1,6 @@
-$NetBSD: patch-src_lisp.h,v 1.1 2012/04/27 13:53:46 hauke Exp $
+$NetBSD: patch-src_lisp.h,v 1.2 2015/02/04 09:19:20 hauke Exp $
 
-Fix CVE-2009-2688, via <https://bugzilla.redhat.com/show_bug.cgi?id=511994>
-
---- src/lisp.h.orig	2005-02-01 03:55:02.000000000 +0000
+--- src/lisp.h.orig	2015-01-29 15:04:29.000000000 +0000
 +++ src/lisp.h
 @@ -265,6 +265,11 @@ void assert_failed (const char *, int, c
  /*#define REGISTER register*/
