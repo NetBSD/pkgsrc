534f4031c910be95c8b0fffce87fffb5ea8f2996
joerg <joerg@pkgsrc.org>
2015-02-04 20:50:50 +0000

    Restore inline support to working state.

--- net/bird/patches/patch-lib_lists.h
+++ net/bird/patches/patch-lib_lists.h
@@ -1,34 +1,36 @@
-$NetBSD: patch-lib_lists.h,v 1.2 2015/02/03 13:58:32 mef Exp $
+$NetBSD: patch-lib_lists.h,v 1.3 2015/02/04 20:50:50 joerg Exp $
 
 date: 2012-06-29 21:37:45 +0900;  author: joerg;  state: Exp;
 Fix inline usage.
 
 --- lib/lists.h.orig	2014-04-01 18:24:10.000000000 +0900
 +++ lib/lists.h	2015-02-03 22:46:25.000000000 +0900
-@@ -50,13 +50,6 @@ typedef struct list {			/* In fact two o
+@@ -50,6 +50,14 @@ typedef struct list {			/* In fact two o
  
  #define EMPTY_LIST(list) (!(list).head->next)
  
--void add_tail(list *, node *);
--void add_head(list *, node *);
--void rem_node(node *);
--void rem2_node(node *);
--void add_tail_list(list *, list *);
--void init_list(list *);
--void insert_node(node *, node *);
- 
- #ifndef _BIRD_LISTS_C_
- #define LIST_INLINE extern inline
-@@ -66,4 +59,12 @@ void insert_node(node *, node *);
- #define LIST_INLINE
++
++#ifndef _BIRD_LISTS_C_
++#define LIST_INLINE static inline
++#include "lib/lists.c"
++#undef LIST_INLINE
++#else
++#define LIST_INLINE
++
+ void add_tail(list *, node *);
+ void add_head(list *, node *);
+ void rem_node(node *);
+@@ -57,13 +64,6 @@ void rem2_node(node *);
+ void add_tail_list(list *, list *);
+ void init_list(list *);
+ void insert_node(node *, node *);
+-
+-#ifndef _BIRD_LISTS_C_
+-#define LIST_INLINE extern inline
+-#include "lib/lists.c"
+-#undef LIST_INLINE
+-#else
+-#define LIST_INLINE
  #endif
  
-+void add_tail(list *, node *);
-+void add_head(list *, node *);
-+void rem_node(node *);
-+void rem2_node(node *);
-+void add_tail_list(list *, list *);
-+void init_list(list *);
-+void insert_node(node *, node *);
-+
  #endif
