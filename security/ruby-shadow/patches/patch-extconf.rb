4629de0e8a92a0d4e0e56b329a3334d112a912c0
taca <taca@pkgsrc.org>
2015-02-04 16:14:57 +0000

    Update ruby-shadow to 2.4.1.
    [2014/12/02]
    * Version 2.4.1
      - sp_loginclass support should NOT have been added to password implementation
    [2014/12/01]
    * Version 2.4.0
      - Add support for sp_loginclass via pwd.h

--- security/ruby-shadow/patches/patch-extconf.rb
+++ security/ruby-shadow/patches/patch-extconf.rb
@@ -1,15 +1,25 @@
-$NetBSD: patch-extconf.rb,v 1.1 2014/07/25 16:21:26 jperkin Exp $
+$NetBSD: patch-extconf.rb,v 1.2 2015/02/04 16:14:57 taca Exp $
 
-Do not override LDFLAGS.
+* Relax condition to make more operating system support.
+* Do not override LDFLAGS.
 
---- extconf.rb.orig	2014-07-25 16:13:16.994762284 +0000
+--- extconf.rb.orig	2015-01-26 14:06:33.000000000 +0000
 +++ extconf.rb
-@@ -26,7 +26,7 @@ when 'shadow'
+@@ -18,8 +18,7 @@ implementation = case CONFIG['host_os']
+                  when /sunos|solaris/i; 'shadow'
+                  when /freebsd|mirbsd|netbsd|openbsd/i; 'pwd'
+                  when /darwin/i; 'pwd'
+-                 else; nil
+-                   "This library works on OS X, FreeBSD, MirBSD, NetBSD, OpenBSD, Solaris and Linux."
++                 else; 'pwd'
+                  end
+ 
+ ok = true
+@@ -29,7 +28,6 @@ when 'shadow'
    #$LDFLAGS = "-lshadow"
  
    if( ! (ok &= have_library("shadow","getspent")) )
 -    $LDFLAGS = ""
-+    #$LDFLAGS = ""
      ok = have_func("getspent")
    end
  
