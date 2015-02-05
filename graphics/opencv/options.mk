fff610719173d134c96edf28e903cd184538885f
tnn <tnn@pkgsrc.org>
2015-02-05 03:51:13 +0000

    Add ffmpeg option. From Mansour Moufid on pkgsrc-users@

--- graphics/opencv/options.mk
+++ graphics/opencv/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.4 2013/08/13 19:47:20 markd Exp $
+# $NetBSD: options.mk,v 1.5 2015/02/05 03:51:13 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.opencv
-PKG_SUPPORTED_OPTIONS=	python
+PKG_SUPPORTED_OPTIONS=	python ffmpeg
 
 .include "../../mk/bsd.options.mk"
 
@@ -16,6 +16,14 @@ PLIST.python=	yes
 CMAKE_ARGS+=	-D BUILD_NEW_PYTHON_SUPPORT=OFF
 .endif
 
+.if !empty(PKG_OPTIONS:Mffmpeg)
+CMAKE_ARGS+=	-D WITH_FFMPEG=ON
+CMAKE_ARGS+=	-D FFMPEG_INCLUDE_DIR=${PREFIX}/include/ffmpeg2
+CMAKE_ARGS+=	-D FFMPEG_LIB_DIR=${PREFIX}/lib/ffmpeg2
+.include "../../multimedia/ffmpeg2/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-D WITH_FFMPEG=OFF
+.endif
+
 # FIXME: should be option.mk'ed instead
 CMAKE_ARGS+=	-D BUILD_DOCS=OFF
-CMAKE_ARGS+=	-D WITH_FFMPEG=OFF
