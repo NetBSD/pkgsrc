14bdc1910fec30fdde299a3d547f71806af6c8c7
pho <pho>
2015-02-05 17:45:20 +0000

    patches/patch-rts_ghc.mk: Fix build failure on Darwin
    See the comment for details.

--- lang/ghc7/patches/patch-rts_ghc.mk
+++ lang/ghc7/patches/patch-rts_ghc.mk
@@ -1,4 +1,6 @@
-$NetBSD: patch-rts_ghc.mk,v 1.3 2014/05/24 04:49:40 obache Exp $
+$NetBSD: patch-rts_ghc.mk,v 1.4 2015/02/05 17:45:20 pho Exp $
+
+Hunk #2:
 
 This is pkgsrc specific: dtrace(1) gets confused when we have gcc
 wrappers in the PATH so we have to hide them:
@@ -8,14 +10,34 @@ wrappers in the PATH so we have to hide them:
   dtrace: failed to compile script rts/RtsProbes.d: Preprocessor failed to pr
   ocess input program
 
-Also install libffi.{a,la} so we can expand to figure out .so extensions.
+Hunk #1, #3:
+
+Install libffi.{a,la} too, so that the plist framework can deal with
+.so name being different between platforms. Note that
+"libffi$(soext)*" matches to libffi.{so,so.6,so.6.0} but not to
+libffi.6.dylib so the pattern must be corrected to "libffi*$(soext)*",
+otherwise libtool-expand will emit the following error on Mach-O
+platforms:
+
+  libtool-expand: `lib/ghc-7.6.3/libffi.6.dylib' was not found
+
+For what it's worth, this ain't the right way to install libtool
+libraries. They should always be installed by running
+"libtool --mode=install install -c foo.la /path/to/destination", which
+can't be achieved without a major refactoring of the GHC build system.
+And for that matter, GHC shouldn't install a local copy of libffi in
+the first place. In fact GHC 7.8 seems to have a configure option
+"--with-system-libffi" so we can hopefully remove these unpleasant
+hunks in the future.
 
 --- rts/ghc.mk.orig	2013-04-18 21:22:47.000000000 +0000
 +++ rts/ghc.mk
-@@ -114,6 +114,7 @@ $(rts_ffi_objs_stamp): $(libffi_STATIC_L
+@@ -113,7 +113,8 @@ $(rts_ffi_objs_stamp): $(libffi_STATIC_L
+ # This is a little hacky. We don't know the SO version, so we only
  # depend on libffi.so, but copy libffi.so*
  rts/dist/build/libffi$(soext): libffi/build/inst/lib/libffi$(soext)
- 	cp libffi/build/inst/lib/libffi$(soext)* rts/dist/build
+-	cp libffi/build/inst/lib/libffi$(soext)* rts/dist/build
++	cp libffi/build/inst/lib/libffi*$(soext)* rts/dist/build
 +	cp libffi/build/inst/lib/libffi.*a rts/dist/build
  
  rts/dist/build/$(LIBFFI_DLL): libffi/build/inst/bin/$(LIBFFI_DLL)
@@ -29,10 +51,12 @@ Also install libffi.{a,la} so we can expand to figure out .so extensions.
  endif
  
  # -----------------------------------------------------------------------------
-@@ -510,6 +511,7 @@ endif
+@@ -509,7 +510,8 @@ endif
+ # installing
  
  INSTALL_LIBS += $(ALL_RTS_LIBS)
- INSTALL_LIBS += $(wildcard rts/dist/build/libffi$(soext)*)
+-INSTALL_LIBS += $(wildcard rts/dist/build/libffi$(soext)*)
++INSTALL_LIBS += $(wildcard rts/dist/build/libffi*$(soext)*)
 +INSTALL_LIBS += $(wildcard rts/dist/build/libffi.*a)
  INSTALL_LIBS += $(wildcard rts/dist/build/$(LIBFFI_DLL))
  
