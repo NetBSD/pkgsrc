b0aa68206ff806f0e82a0fec189e6ec3bd963f46
wiz <wiz@pkgsrc.org>
2015-02-05 17:33:12 +0000

    Use MesaLib only on non-arm. Support dynarec option on i386 and x86_64 too.

--- emulators/libretro-mupen64plus/options.mk
+++ emulators/libretro-mupen64plus/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2015/02/05 16:57:36 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2015/02/05 17:33:12 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libretro-mupen64plus
 
@@ -8,6 +8,14 @@ PKG_OPTIONS_VAR=	PKG_OPTIONS.libretro-mupen64plus
 PKG_SUPPORTED_OPTIONS+=	rpi dynarec
 PKG_SUGGESTED_OPTIONS+=	rpi dynarec
 MUPEN64_DYNAREC_ARCH=	arm
+.elif !empty(MACHINE_ARCH:Mi386)
+PKG_SUPPORTED_OPTIONS+=	dynarec
+PKG_SUGGESTED_OPTIONS+=	dynarec
+MUPEN64_DYNAREC_ARCH=	x86
+.elif !empty(MACHINE_ARCH:Mx86_64)
+PKG_SUPPORTED_OPTIONS+=	dynarec
+PKG_SUGGESTED_OPTIONS+=	dynarec
+MUPEN64_DYNAREC_ARCH=	x86_64
 .endif
 
 .include "../../mk/bsd.options.mk"
