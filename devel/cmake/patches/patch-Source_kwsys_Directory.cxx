5de23bae0f19e59bd34651cc85bd6bfb60f22da9
adam <adam@pkgsrc.org>
2015-02-05 17:45:22 +0000

    Changes 3.1.2: * install: Fix regression in default configuration selection * CPack: Fix packaging of source tarballs with symbolic links * KWSys Directory: Check opendir return value before using it * Help: Clarify status of link_libraries command * Normalize OBJECT_DEPENDS paths to match custom commands * MSVC: Fix initialization of RelWithDebInfo shared library link flags * FeatureSummary: Fix bracket in documentation. * FindOpenSSL: fix detection of OpenSSL 1.0.2 * ctest_build: Update GNU make error message matching

--- devel/cmake/patches/patch-Source_kwsys_Directory.cxx
+++ devel/cmake/patches/patch-Source_kwsys_Directory.cxx
@@ -1,14 +1,14 @@
-$NetBSD: patch-Source_kwsys_Directory.cxx,v 1.1 2015/01/23 04:06:08 wiz Exp $
+$NetBSD: patch-Source_kwsys_Directory.cxx,v 1.2 2015/02/05 17:45:22 adam Exp $
 
 When opendir() fails, readdir() is called with a NULL pointer, leading to
 a segfault.
 
 http://public.kitware.com/Bug/view.php?id=15367
 
---- Source/kwsys/Directory.cxx.orig	2014-12-15 20:07:43.000000000 +0000
+--- Source/kwsys/Directory.cxx.orig	2015-02-04 20:36:44.000000000 +0000
 +++ Source/kwsys/Directory.cxx
-@@ -240,11 +240,14 @@ unsigned long Directory::GetNumberOfFile
-   DIR* dir = opendir(name.c_str());
+@@ -245,11 +245,14 @@ unsigned long Directory::GetNumberOfFile
+     }
  
    unsigned long count = 0;
 -  for (dirent* d = readdir(dir); d; d = readdir(dir) )
