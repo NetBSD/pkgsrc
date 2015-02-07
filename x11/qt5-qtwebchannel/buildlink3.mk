b39904fecb3aa6b2487ab933145ef2886d43832b
wiz <wiz@pkgsrc.org>
2015-02-07 13:18:14 +0000

    Import qt5-qtwebchannel-5.4.0 as x11/qt5-qtwebchannel.
    Qt5 WebChannel module

---
+++ x11/qt5-qtwebchannel/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/02/07 13:18:14 wiz Exp $
+
+BUILDLINK_TREE+=	qt5-qtwebchannel
+
+.if !defined(QT5_QTWEBCHANNEL_BUILDLINK3_MK)
+QT5_QTWEBCHANNEL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt5-qtwebchannel+=	qt5-qtwebchannel>=5.4.0
+BUILDLINK_ABI_DEPENDS.qt5-qtwebchannel+=	qt5-qtwebchannel>=5.4.0
+BUILDLINK_PKGSRCDIR.qt5-qtwebchannel?=	../../x11/qt5-qtwebchannel
+
+BUILDLINK_INCDIRS.qt5-qtwebchannel+=	qt5/include
+BUILDLINK_LIBDIRS.qt5-qtwebchannel+=	qt5/lib
+BUILDLINK_LIBDIRS.qt5-qtwebchannel+=	qt5/plugins
+
+.include "../../x11/qt5-qtdeclarative/buildlink3.mk"
+.endif	# QT5_QTWEBCHANNEL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt5-qtwebchannel
