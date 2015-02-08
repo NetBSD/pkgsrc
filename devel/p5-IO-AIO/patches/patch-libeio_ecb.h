31e9baca55e82649d7151bf22d63f12724bd9aad
mef <mef@pkgsrc.org>
2015-02-08 02:49:50 +0000

    (pkgsrc)  - Add comment on patch, picking from cvs log (upstream)  - Update 4.18 to 4.31 ------------------- 4.31  Tue Jun  3 03:29:27 CEST 2014         - work around more 5.20 bugs. backwards compatibility my ass.
    4.3   Fri Apr 11 06:22:38 CEST 2014
            - perl5porters broke Async::Interrupt, BDB, EV, IO::AIO, OpenCL
              without warning by switching the meaning of USE_SOCKETS_AS_HANDLES
              in 5.18. What's so attractive about giving a shit about backwards
              compatibility - I will never understand.
    
    4.2   Sat Jan 25 01:13:14 CET 2014
            - aio_group could corrupt memory because it didn't restore
              the stack after req_submit.
            - be more careful on (e.g. permission) errors in bin/treescan.
            - work around changes in ExtUtils::MakeMaker.
            - (libeio) implement aio_realpath for win32.
            - (xthread) work around compile time bugs in ptw32.
            - added IO::AIO::pipesize.
            - (libecb) insignificant update.
    
    4.19  Sun Jan  6 12:47:26 CET 2013
            - avoid endless loop in fiemap with some XFS files.
            - in aio_rename and aio_rmdir, specialcase the case of [$wd, "."]
              and call rename/rmdir instead of renameat/unlinkat.

--- devel/p5-IO-AIO/patches/patch-libeio_ecb.h
+++ devel/p5-IO-AIO/patches/patch-libeio_ecb.h
@@ -1,4 +1,6 @@
-$NetBSD: patch-libeio_ecb.h,v 1.1 2014/11/10 18:09:10 joerg Exp $
+$NetBSD: patch-libeio_ecb.h,v 1.2 2015/02/08 02:49:50 mef Exp $
+
+Use __builtin_unreachable on Clang instead of the broken C11 hack.
 
 --- libeio/ecb.h.orig	2014-11-09 21:03:05.000000000 +0000
 +++ libeio/ecb.h
