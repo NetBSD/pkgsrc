b584c7f5b207494694bc58cd5bce4b23eef69f4e
wiz <wiz@pkgsrc.org>
2015-02-09 12:57:41 +0000

    Update to 0.8.4. Update HOMEPAGE. Add bug report URL to patch.
    With only one day after the 0.8.3 release 0.8.4 is being released.
    My deepest apologies because this means there is a bad bug. Admittedly
    it will only impact a very small amount of users but it was big
    enough to warrant a new release.
    
    Unfortunately during refactoring of "remove all plugins" code the
    actual remove code was moved to the wrong place. With 0.8.3 if you
    say no it will still remove all plugins. This has been corrected
    and is the only change in 0.8.4.

--- editors/Sigil/patches/patch-src_minizip_ioapi.c
+++ editors/Sigil/patches/patch-src_minizip_ioapi.c
@@ -1,7 +1,8 @@
-$NetBSD: patch-src_minizip_ioapi.c,v 1.2 2014/11/23 19:27:29 wiz Exp $
+$NetBSD: patch-src_minizip_ioapi.c,v 1.3 2015/02/09 12:57:41 wiz Exp $
 
 Fix linking on NetBSD and DragonFly BSD, where fopen, fseek, and ftello
 are 64-bit filesize safe.
+https://github.com/nmoinvaz/minizip/pull/20
 
 --- src/minizip/ioapi.c.orig	2014-10-12 14:45:14.000000000 +0000
 +++ src/minizip/ioapi.c
