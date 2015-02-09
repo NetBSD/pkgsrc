05176c8d5bc114005e4d864692e41457db6cad85
gdt <gdt@pkgsrc.org>
2015-02-09 18:35:12 +0000

    In amanda*, reduce default options.
    Add an 'ndmp' option, disabled by default.
    Disable kerberos option by default.
    
    These two changes allow amanda to build again on OS X.  My belief,
    posited on pkgsrc-users without contradiction, is that no pkgsrc users
    use these features anyway.  Normal amanda usage these days is over ssh
    (which gets one PFS).  NDMP is for direct dumping of NAS: usage is
    probably rare and also in large installations where rebuilding is not
    hard.
    
    All in all, I thought it better for the pkgsrc/amanda universe to have
    consistent options across platforms than to selectively disable on OS
    X.

--- sysutils/amanda-common/options.mk
+++ sysutils/amanda-common/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.10 2013/12/13 12:42:12 jperkin Exp $
+# $NetBSD: options.mk,v 1.11 2015/02/09 18:35:12 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.amanda
 # Common options.
-PKG_SUPPORTED_OPTIONS+=	inet6 amanda-fqdn amanda-ssh kerberos
-PKG_SUGGESTED_OPTIONS+=	inet6 amanda-fqdn amanda-ssh kerberos
+PKG_SUPPORTED_OPTIONS+=	inet6 amanda-fqdn amanda-ssh kerberos ndmp
+PKG_SUGGESTED_OPTIONS+=	inet6 amanda-fqdn amanda-ssh
 # Client options.
 PKG_SUPPORTED_OPTIONS+=	amanda-smb amanda-dump-snap
 PKG_SUGGESTED_OPTIONS+=	amanda-dump-snap
@@ -42,4 +42,16 @@ USE_BUILTIN.mit-krb5=	no
 .  endif
 CONFIGURE_ARGS+=	--with-krb5-security=${KRB5BASE:Q}
 .  include "../../mk/krb5.buildlink3.mk"
+.endif
+
+# The ndmp code fails to build on OS X >= 10.9, and upstream questions why
+# it is enabled.  So it is a disabled-by-default option.
+# http://www.redhat.com/archives/libvir-list/2013-October/msg01153.html
+PLIST_VARS+=		ndmp
+CONFIGURE_ARGS+=	--without-ndmp
+.if !empty(PKG_OPTIONS:Mndmp)
+CONFIGURE_ARGS+=	--with-ndmp
+PLIST.ndmp=		yes
+.else
+CONFIGURE_ARGS+=	--without-ndmp
 .endif
