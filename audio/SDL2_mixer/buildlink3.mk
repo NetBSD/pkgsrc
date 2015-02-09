85d3d06b58508a7c99e78b6fce2050cf04b33e80
snj <snj>
2015-02-09 08:43:13 +0000

    Recursive bump for SDL2-using packages.

--- audio/SDL2_mixer/buildlink3.mk
+++ audio/SDL2_mixer/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/02/02 15:39:01 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/02/09 08:43:13 snj Exp $
 
 BUILDLINK_TREE+=	SDL2_mixer
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	SDL2_mixer
 SDL2_MIXER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.SDL2_mixer+=	SDL2_mixer>=2.0.0
+BUILDLINK_ABI_DEPENDS.SDL2_mixer?=	SDL2_mixer>=2.0.0nb1
 BUILDLINK_PKGSRCDIR.SDL2_mixer?=	../../devel/SDL2_mixer
 
 .include "../../devel/SDL2/buildlink3.mk"
