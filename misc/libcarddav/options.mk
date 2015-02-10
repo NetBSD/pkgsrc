da7d99c62e6b1bc3d8fcdfd50ec2f0dafdfdb33f
wiz <wiz@pkgsrc.org>
2015-02-10 23:58:53 +0000

    Re-enable doc by default now that tex-adjustbox is fixed and the pdf builds. Bump PKGREVISION.

--- misc/libcarddav/options.mk
+++ misc/libcarddav/options.mk
@@ -1,9 +1,8 @@
-# $NetBSD: options.mk,v 1.4 2015/02/10 08:12:43 wiz Exp $
+# $NetBSD: options.mk,v 1.5 2015/02/10 23:58:53 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libcarddav
 PKG_SUPPORTED_OPTIONS=	doc
-# TODO: tc-pgf.def needs packaging before PDF output works
-#PKG_SUGGESTED_OPTIONS=	doc
+PKG_SUGGESTED_OPTIONS=	doc
 PKG_OPTIONS_LEGACY_OPTS+=	doxygen:doc
 
 .include "../../mk/bsd.options.mk"
@@ -12,7 +11,7 @@ PLIST_VARS+=	doc
 
 .if !empty(PKG_OPTIONS:Mdoc)
 BUILD_DEPENDS+=		doxygen>=1.8.1.1:../../devel/doxygen
-BUILD_DEPENDS+=		tex-adjustbox-[0-9]*:../../print/tex-adjustbox
+BUILD_DEPENDS+=		tex-adjustbox>=1.0nb1:../../print/tex-adjustbox
 BUILD_DEPENDS+=		tex-ec-[0-9]*:../../fonts/tex-ec
 BUILD_DEPENDS+=		tex-tocloft-[0-9]*:../../print/tex-tocloft
 BUILD_DEPENDS+=		tex-xtab-[0-9]*:../../print/tex-xtab
