59b06c48420e0dc8bed7c47cf5623fddc38dc18d
wiz <wiz>
2015-02-10 08:12:43 +0000

    Add some more dependencies for the 'doc' option; additionally needs ! LaTeX Error: File `tc-pgf.def' not found. which doesn't seem to be packaged yet; so disable doc option by default.
    Bump PKGREVISION.

--- misc/libcarddav/options.mk
+++ misc/libcarddav/options.mk
@@ -1,8 +1,9 @@
-# $NetBSD: options.mk,v 1.3 2012/07/11 14:47:23 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2015/02/10 08:12:43 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libcarddav
 PKG_SUPPORTED_OPTIONS=	doc
-PKG_SUGGESTED_OPTIONS=	doc
+# TODO: tc-pgf.def needs packaging before PDF output works
+#PKG_SUGGESTED_OPTIONS=	doc
 PKG_OPTIONS_LEGACY_OPTS+=	doxygen:doc
 
 .include "../../mk/bsd.options.mk"
@@ -11,10 +12,13 @@ PLIST_VARS+=	doc
 
 .if !empty(PKG_OPTIONS:Mdoc)
 BUILD_DEPENDS+=		doxygen>=1.8.1.1:../../devel/doxygen
+BUILD_DEPENDS+=		tex-adjustbox-[0-9]*:../../print/tex-adjustbox
 BUILD_DEPENDS+=		tex-ec-[0-9]*:../../fonts/tex-ec
 BUILD_DEPENDS+=		tex-tocloft-[0-9]*:../../print/tex-tocloft
 BUILD_DEPENDS+=		tex-xtab-[0-9]*:../../print/tex-xtab
 BUILD_DEPENDS+=		tex-multirow-[0-9]*:../../print/tex-multirow
+BUILD_DEPENDS+=		tex-xkeyval-[0-9]*:../../print/tex-xkeyval
+BUILD_DEPENDS+=		tex-collectbox-[0-9]*:../../print/tex-collectbox
 CONFIGURE_ARGS+=	--enable-doc
 PLIST.doc=		yes
 .else
