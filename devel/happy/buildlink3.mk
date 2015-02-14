82c84d71d406f34866734d9fbf41b31ad7446e88
pho <pho>
2015-02-14 04:46:33 +0000

    Set BUILDLINK_DEPMETHOD.happy to "build" as happy is not a library but a build tool

--- devel/happy/buildlink3.mk
+++ devel/happy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/01/08 11:50:54 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/02/14 04:46:33 pho Exp $
 
 BUILDLINK_TREE+=	happy
 
@@ -8,6 +8,7 @@ HAPPY_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.happy+=	happy>=1.19.5
 BUILDLINK_ABI_DEPENDS.happy+=	happy>=1.19.5
 BUILDLINK_PKGSRCDIR.happy?=	../../devel/happy
+BUILDLINK_DEPMETHOD.happy?=	build
 
 .include "../../devel/hs-mtl/buildlink3.mk"
 .endif	# HAPPY_BUILDLINK3_MK
