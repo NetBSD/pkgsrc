bfdd0bb9a7b47163f3a477f9a394e5c6d89a0d5e
pho <pho@pkgsrc.org>
2015-02-14 04:10:27 +0000

    Update to 0.5.4.0
    ChangeLog:
    
     * Changed primitive_ to work around an oddity with GHC's code
       generation on certain versions that led to side effects not
       happening when used in conjunction with certain very unsafe IO
       performers.
    
     * Allow primitive to build on GHC 7.9
    CVS: ----------------------------------------------------------------------
    CVS: CVSROOT  cvs.NetBSD.org:/cvsroot
    CVS: please use "PR category/123" to have the commitmsg appended to PR 123

--- devel/hs-primitive/buildlink3.mk
+++ devel/hs-primitive/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:39 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/02/14 04:10:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-primitive
 
 .if !defined(HS_PRIMITIVE_BUILDLINK3_MK)
 HS_PRIMITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.5.3
-BUILDLINK_ABI_DEPENDS.hs-primitive+=	hs-primitive>=0.5.3.0
+BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.5.4
+BUILDLINK_ABI_DEPENDS.hs-primitive+=	hs-primitive>=0.5.4.0
 BUILDLINK_PKGSRCDIR.hs-primitive?=	../../devel/hs-primitive
 .endif	# HS_PRIMITIVE_BUILDLINK3_MK
 
