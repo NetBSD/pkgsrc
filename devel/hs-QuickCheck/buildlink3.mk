7136c14296ce1db4a89f198376cb8250ab3d044f
pho <pho>
2015-02-14 04:57:28 +0000

    Forgot to add a dependency on devel/hs-transformers. Bump revision
    I badly want buildlink for Haskell packages, though it's not very easy
    to implement...

--- devel/hs-QuickCheck/buildlink3.mk
+++ devel/hs-QuickCheck/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/02/14 04:34:33 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/02/14 04:57:28 pho Exp $
 
 BUILDLINK_TREE+=	hs-QuickCheck
 
@@ -6,11 +6,12 @@ BUILDLINK_TREE+=	hs-QuickCheck
 HS_QUICKCHECK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.7.6
-BUILDLINK_ABI_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.7.6
+BUILDLINK_ABI_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.7.6nb1
 BUILDLINK_PKGSRCDIR.hs-QuickCheck?=	../../devel/hs-QuickCheck
 
 .include "../../devel/hs-random/buildlink3.mk"
 .include "../../devel/hs-tf-random/buildlink3.mk"
+.include "../../devel/hs-transformers/buildlink3.mk"
 .endif	# HS_QUICKCHECK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-QuickCheck
