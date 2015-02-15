29efdf3d9252997e47a92d5ba3d53308e9245a55
ryoon <ryoon>
2015-02-15 02:11:03 +0000

    Update to 2.32.1
    Changelog:
    What's New in SeaMonkey 2.32.1
    
    Mostly regression fixes, including:
    
        MailNews feeds not updating
        Selected profile in Profile Manager not remembered
        Opening a bookmark folder in tabs on Linux
        Troubleshooting Information (about:support) with the Modern theme
    
    
    What's New in SeaMonkey 2.32
    
    SeaMonkey 2.32 contains the following major changes relative to SeaMonkey 2.31:
    SeaMonkey-specific changes
    
        The Spell Check dialog is now resizable.
        Generational Garbage Collection has been enabled.
    
    Mozilla platform changes
    
        Improved handling of dynamic styling changes to increase responsiveness.
        Implemented HTTP Public Key Pinning Extension (for enhanced authentication of encrypted connections).
        Reduced resource usage for scaled images.
        Also see Firefox 35 for Developers.
        Fixed several stability issue
    
    Fixed in SeaMonkey 2.32
    
        2015-09 XrayWrapper bypass through DOM objects
        2015-08 Delegated OCSP responder certificates failure with id-pkix-ocsp-nocheck extension
        2015-06 Read-after-free in WebRTC
        2015-05 Read of uninitialized memory in Web Audio
        2015-04 Cookie injection through Proxy Authenticate responses
        2015-03 sendBeacon requests lack an Origin header
        2015-02 Uninitialized memory use during bitmap rendering
        2015-01 Miscellaneous memory safety hazards (rv:35.0 / rv:31.4)

--- www/seamonkey/patches/patch-config_baseconfig.mk
+++ www/seamonkey/patches/patch-config_baseconfig.mk
@@ -1,19 +1,2 @@
-$NetBSD: patch-config_baseconfig.mk,v 1.5 2015/01/02 04:26:21 ryoon Exp $
+$NetBSD: patch-config_baseconfig.mk,v 1.6 2015/02/15 02:11:03 ryoon Exp $
 
---- config/baseconfig.mk.orig	2014-12-03 06:11:58.000000000 +0000
-+++ config/baseconfig.mk
-@@ -2,10 +2,10 @@
- # directly in python/mozbuild/mozbuild/base.py for gmake validation.
- # We thus use INCLUDED_AUTOCONF_MK to enable/disable some parts depending
- # whether a normal build is happening or whether the check is running.
--includedir := $(includedir)/$(MOZ_APP_NAME)-$(MOZ_APP_VERSION)
--idldir = $(datadir)/idl/$(MOZ_APP_NAME)-$(MOZ_APP_VERSION)
--installdir = $(libdir)/$(MOZ_APP_NAME)-$(MOZ_APP_VERSION)
--sdkdir = $(libdir)/$(MOZ_APP_NAME)-devel-$(MOZ_APP_VERSION)
-+includedir := $(includedir)/$(MOZILLA_PKG_NAME)
-+idldir = $(datadir)/idl/$(MOZILLA_PKG_NAME)
-+installdir = $(libdir)/$(MOZILLA_PKG_NAME)
-+sdkdir = $(libdir)/$(MOZILLA_PKG_NAME)-sdk
- MOZILLA_SRCDIR = $(topsrcdir)/mozilla
- MOZDEPTH = $(DEPTH)
- DIST = $(MOZDEPTH)/dist
