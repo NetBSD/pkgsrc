9587568e19eba3ea7e0771440a7d6202a5207be9
adam <adam>
2015-02-16 15:33:32 +0000

    Changes 1.1.0: Allow searching across multiple folders Improved support for screen readers and assistive technology using WCAG 2.0 and WAI ARIA standards Update to TinyMCE 4.1 to support images in HTML signatures (copy & paste) Added namespace filter and folder searching in folder manager New config option to disable UI elements/actions Stronger password encryption using OpenSSL Support for the IMAP SPECIAL-USE extension Support for Oracle as database backend Manage 3rd party libs with Composer

--- mail/roundcube/patches/patch-installer_check.php
+++ mail/roundcube/patches/patch-installer_check.php
@@ -1,14 +1,14 @@
-$NetBSD: patch-installer_check.php,v 1.1 2013/09/13 11:13:49 taca Exp $
+$NetBSD: patch-installer_check.php,v 1.2 2015/02/16 15:33:32 adam Exp $
 
 Roundcube dosen't use php-exif.
 
---- installer/check.php.orig	2013-09-06 12:20:52.000000000 +0000
+--- installer/check.php.orig	2015-02-08 13:48:55.000000000 +0000
 +++ installer/check.php
 @@ -24,7 +24,6 @@ $optional_php_exts = array(
      'OpenSSL'   => 'openssl',
      'Mcrypt'    => 'mcrypt',
      'Intl'      => 'intl',
 -    'Exif'      => 'exif',
+     'LDAP'      => 'ldap',
  );
  
- $required_libs = array(
