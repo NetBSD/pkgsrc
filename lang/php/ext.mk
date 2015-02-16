f451a011f1918dd6c61433ec57aa60042f8eddce
jperkin <jperkin@pkgsrc.org>
2015-02-16 10:30:15 +0000

    Apologies for not noticing sooner, the previous commit was incorrect as taca has already implemented different handling for zend_extension.

--- lang/php/ext.mk
+++ lang/php/ext.mk
@@ -1,4 +1,4 @@
-# $NetBSD: ext.mk,v 1.36 2015/02/16 10:20:24 jperkin Exp $
+# $NetBSD: ext.mk,v 1.37 2015/02/16 10:30:15 jperkin Exp $
 #
 # PHP extension package framework, for both PECL and bundled PHP extensions.
 #
@@ -67,11 +67,7 @@ LDFLAGS+=		${EXPORT_SYMBOLS_LDFLAGS}
 MAKE_ENV+=		EXPORT_SYMBOLS_LDFLAGS="${EXPORT_SYMBOLS_LDFLAGS}"
 
 PLIST_SRC+=		${.CURDIR}/../../lang/php/PLIST.module
-.if !empty(PHP_ZEND_EXTENSION:U:M[Yy][Ye][Ss])
-MESSAGE_SRC=		${.CURDIR}/../../lang/php/MESSAGE.zend-module
-.else
 MESSAGE_SRC=		${.CURDIR}/../../lang/php/MESSAGE.module
-.endif
 MESSAGE_SUBST+=		MODNAME=${PKGMODNAME}
 MESSAGE_SUBST+=		PHP_EXTENSION_DIR=${PHP_EXTENSION_DIR}
 .if !empty(PHP_ZEND_EXTENSION:U:M[Yy][Ye][Ss])
