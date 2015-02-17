074d4588e42345a7eac19ccf3cbedb9ab23e5061
rodent <rodent>
2015-02-17 02:00:07 +0000

    Include <machine/endian.h> for NetBSD too, which keeps BYTE_ORDER from being redefined. This was a problem on OpenBSD in a previous version of the package too. Defuzz patches.

--- net/powerdns/patches/patch-pdns_dns.hh
+++ net/powerdns/patches/patch-pdns_dns.hh
@@ -1,4 +1,4 @@
-$NetBSD: patch-pdns_dns.hh,v 1.2 2014/12/10 14:50:09 fhajny Exp $
+$NetBSD: patch-pdns_dns.hh,v 1.3 2015/02/17 02:00:07 rodent Exp $
 
 Add DragonFly support.
 --- pdns/dns.hh.orig	2014-10-30 10:18:22.000000000 +0000
@@ -8,7 +8,7 @@ Add DragonFly support.
  };
  
 -#if __FreeBSD__ || __APPLE__ || __OpenBSD__ ||  defined(__FreeBSD_kernel__)
-+#if __FreeBSD__ || __APPLE__ || __OpenBSD__ || __DragonFly__ || defined(__FreeBSD_kernel__)
++#if defined(__FreeBSD__) || defined(__APPLE__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__)
  #include <machine/endian.h>
  #elif __linux__ || __GNU__
  # include <endian.h>
