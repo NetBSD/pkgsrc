0739057f5f8684ab46c48fbf2d31ff12a0565684
roy <roy@pkgsrc.org>
2015-02-18 19:21:54 +0000

    Fix compile with clang on i386. Patch taken from FreeBSD.

---
+++ multimedia/gst-plugins0.10-good/patches/patch-gst_goom_mmx.h
@@ -0,0 +1,23 @@
+$NetBSD: patch-gst_goom_mmx.h,v 1.1 2015/02/18 19:21:54 roy Exp $
+
+Fixes build with clang on i386
+Taken from FreeBSD
+
+--- gst/goom/mmx.h.orig	2011-12-11 19:48:11.000000000 +0100
++++ gst/goom/mmx.h	2013-02-08 00:54:14.000000000 +0100
+@@ -715,13 +715,13 @@ void zoom_filter_xmmx (int prevX, int pr
+ 	{ \
+ 		printf("emms()\n"); \
+ 		__asm__ __volatile__ ("emms" \
+-                        "st(1)","st(2)","st(3)","st(4)","st(5)","st(6)","st(7)"); \
++                        "st", "st(1)","st(2)","st(3)","st(4)","st(5)","st(6)","st(7)"); \
+ 	}
+ 
+ #else
+ 
+ #define	emms() __asm__ __volatile__ ("emms"::: \
+-                      "st(1)","st(2)","st(3)","st(4)","st(5)","st(6)","st(7)")
++                      "st", "st(1)","st(2)","st(3)","st(4)","st(5)","st(6)","st(7)")
+ 
+ #endif
+ 
