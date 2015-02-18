aad8e198634a5936d5797799d6a9696d2d468a42
wiz <wiz@pkgsrc.org>
2015-02-18 09:16:52 +0000

    Add ssl option that's default-on for systems that provide openssl. (restoring v1.8 from obache)

--- net/tnftp/options.mk
+++ net/tnftp/options.mk
@@ -1,10 +1,18 @@
-# $NetBSD: options.mk,v 1.9 2015/02/01 08:58:52 obache Exp $
+# $NetBSD: options.mk,v 1.10 2015/02/18 09:16:52 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tnftp
-PKG_SUPPORTED_OPTIONS=		inet6
+PKG_SUPPORTED_OPTIONS=		inet6 ssl
 PKG_OPTIONS_OPTIONAL_GROUPS+=	socks
 PKG_OPTIONS_GROUP.socks=	socks5
 PKG_SUGGESTED_OPTIONS=		inet6
+
+CHECK_BUILTIN.openssl:=	yes
+.include "../../security/openssl/builtin.mk"
+CHECK_BUILTIN.openssl:=	no
+
+.if !empty(USE_BUILTIN.openssl:M[yY][eE][sS])
+PKG_SUGGESTED_OPTIONS+=	ssl
+.endif
 
 .include "../../mk/bsd.options.mk"
 
@@ -19,4 +27,12 @@ CONFIGURE_ARGS+=	--without-socks
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
+.endif
+
+.if !empty(PKG_OPTIONS:Mssl)
+CONFIGURE_ARGS+=	--enable-ssl
+.include "../../security/openssl/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-openssl=${SSLBASE}
+.else
+CONFIGURE_ARGS+=	--disable-ssl
 .endif
