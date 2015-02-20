4122a812637511a75e462baf49eee90615b3d575
pho <pho>
2015-02-20 19:10:29 +0000

    Leave CONF_LD_LINKER_OPTS_STAGE{0,1,2} undefined
    These flags are only used for creating static GHCi libraries
    (HS*.o). Setting them to ${LDFLAGS} does more harm than good because
    our ${LDFLAGS} contains -Wl,* flags. It's true that
    ../../mk/wrapper/cmd-sink-ld transforms them but those flags will also
    be baked into the compiler (see ${WRKSRC}/compiler/ghc.mk) so they
    cause problems when used outside the buildlink.

--- lang/ghc7/buildlink3.mk
+++ lang/ghc7/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2015/02/13 19:28:40 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2015/02/20 19:10:29 pho Exp $
 
 BUILDLINK_TREE+=	ghc
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ghc
 GHC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ghc+=	ghc>=7.6.3
-BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=7.6.3nb5
+BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=7.6.3nb6
 BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc7
 
 # On FreeBSD we need pkgsrc libiconv. See bootstrap.mk for details.
