283023cf882e4fd511b3e708a0fadedc90288723
ryoon <ryoon>
2015-02-22 08:15:01 +0000

    Add upstream bug URLs.

--- misc/libreoffice4/patches/patch-sw_Executable__tiledrendering.mk
+++ misc/libreoffice4/patches/patch-sw_Executable__tiledrendering.mk
@@ -1,4 +1,6 @@
-$NetBSD: patch-sw_Executable__tiledrendering.mk,v 1.1 2015/02/04 18:19:34 ryoon Exp $
+$NetBSD: patch-sw_Executable__tiledrendering.mk,v 1.2 2015/02/22 08:15:01 ryoon Exp $
+
+https://bugs.documentfoundation.org/show_bug.cgi?id=89551
 
 --- sw/Executable_tiledrendering.mk.orig	2015-01-22 20:05:28.000000000 +0000
 +++ sw/Executable_tiledrendering.mk
