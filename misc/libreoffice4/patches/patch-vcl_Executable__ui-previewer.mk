25803220e3682a1b7a3e134d6cfefb06109649fa
ryoon <ryoon@pkgsrc.org>
2015-02-22 08:15:01 +0000

    Add upstream bug URLs.

--- misc/libreoffice4/patches/patch-vcl_Executable__ui-previewer.mk
+++ misc/libreoffice4/patches/patch-vcl_Executable__ui-previewer.mk
@@ -1,4 +1,6 @@
-$NetBSD: patch-vcl_Executable__ui-previewer.mk,v 1.1 2015/02/04 18:19:34 ryoon Exp $
+$NetBSD: patch-vcl_Executable__ui-previewer.mk,v 1.2 2015/02/22 08:15:01 ryoon Exp $
+
+https://bugs.documentfoundation.org/show_bug.cgi?id=89551
 
 --- vcl/Executable_ui-previewer.mk.orig	2015-01-22 20:05:28.000000000 +0000
 +++ vcl/Executable_ui-previewer.mk
