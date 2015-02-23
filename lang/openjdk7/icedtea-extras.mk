f5def9406b7929b3c9a0f4504e810d85e31b546a
tnn <tnn@pkgsrc.org>
2015-02-23 13:34:56 +0000

    Remove icedtea-web stuff from the base runtime. It seems to not be needed by www/icedtea-web any longer. Bump PKGREVISION.

--- lang/openjdk7/icedtea-extras.mk
+++ lang/openjdk7/icedtea-extras.mk
@@ -1,26 +1,8 @@
-# $NetBSD: icedtea-extras.mk,v 1.15 2014/11/06 16:13:56 ryoon Exp $
+# $NetBSD: icedtea-extras.mk,v 1.16 2015/02/23 13:34:56 tnn Exp $
 
-# Needed to extract icedtea
-EXTRACT_USING=			gtar
-
-DIST_SUBDIR=			openjdk7
-
-ICEDTEA=			icedtea-2.5.3
-ICEDTEA_TGZ=			${ICEDTEA}.tar.xz
-SITES.${ICEDTEA_TGZ}=		http://icedtea.wildebeest.org/download/source/
-DISTFILES+=			${ICEDTEA_TGZ}
-EXTRACT_ONLY+=			${ICEDTEA_TGZ}
-
-# Keep icedtea-web distfile in sync with www/icedtea-web/Makefile
-ICEDTEA_WEB=			icedtea-web-1.5.1
-ICEDTEA_WEB_TGZ=		icedtea-web-1.5.1.tar.gz
-SITES.${ICEDTEA_WEB_TGZ}=	http://icedtea.wildebeest.org/download/source/
-DISTFILES+=			${ICEDTEA_WEB_TGZ}
-EXTRACT_ONLY+=			${ICEDTEA_WEB_TGZ}
-
-RHINO=				rhino1_7R4
+RHINO=				rhino1_7R5
 RHINO_TGZ=			${RHINO}.zip
-SITES.${RHINO_TGZ}=		https://github.com/downloads/mozilla/rhino/
+SITES.${RHINO_TGZ}=		https://github.com/mozilla/rhino/releases/download/Rhino1_7R5_RELEASE/
 DISTFILES+=			${RHINO_TGZ}
 EXTRACT_ONLY+=			${RHINO_TGZ}
 
