59a68ab473c97f3d869898a010c44d528e5bb9ae
ryoon <ryoon@pkgsrc.org>
2015-02-25 20:41:44 +0000

    Remove automatically generated comment.

--- devel/pthread-stublib/buildlink3.mk
+++ devel/pthread-stublib/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2009/03/20 19:24:27 joerg Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.10.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.pthread-stublib?=	build
+# $NetBSD: buildlink3.mk,v 1.7 2015/02/25 20:41:44 ryoon Exp $
 
 BUILDLINK_TREE+=	pthread-stublib
 
