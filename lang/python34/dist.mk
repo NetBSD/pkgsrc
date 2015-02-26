f1bfc9b95d183e145d6ef9494e318a8ec8c048a4
adam <adam@pkgsrc.org>
2015-02-26 06:23:06 +0000

    Python 3.4.3 has many bugfixes and other small improvements over 3.4.2.
    Issue 6639: Module-level turtle functions no longer raise TclError after closing the window.
    Issues 814253, 9179: Warnings now are raised when group references and conditional group references are used in lookbehind assertions in regular expressions.
    Issue 23215: Multibyte codecs with custom error handlers that ignores errors consumed too much memory and raised SystemError or MemoryError. Original patch by Aleksi Torhamo.
    Issue 5700: io.FileIO() called flush() after closing the file. flush() was not called in close() if closefd=False.
    Issue 23374: Fixed pydoc failure with non-ASCII files when stdout encoding differs from file system encoding (e.g. on Mac OS).
    Issue 23481: Remove RC4 from the SSL module’s default cipher list.
    Issue 21548: Fix pydoc.synopsis() and pydoc.apropos() on modules with empty docstrings.
    Issue 22885: Fixed arbitrary code execution vulnerability in the dbm.dumb module. Original patch by Claudiu Popa.
    Issue 23146: Fix mishandling of absolute Windows paths with forward slashes in pathlib.
    Issue 23421: Fixed compression in tarfile CLI. Patch by wdv4758h.
    Issue 23361: Fix possible overflow in Windows subprocess creation code.

--- lang/python34/dist.mk
+++ lang/python34/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.3 2014/10/09 09:15:38 adam Exp $
+# $NetBSD: dist.mk,v 1.4 2015/02/26 06:23:06 adam Exp $
 
-PY_DISTVERSION=	3.4.2
+PY_DISTVERSION=	3.4.3
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python34/distinfo
