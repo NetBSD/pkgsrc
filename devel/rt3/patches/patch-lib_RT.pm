4882563c25c36db07f4f3370cd38a75a9d61aa79
spz <spz@pkgsrc.org>
2015-03-01 22:45:26 +0000

    apply the Request Tracker 4.0.0 patch for CVE-2014-9472, CVE-2015-1165 and CVE-2015-1464.

--- devel/rt3/patches/patch-lib_RT.pm
+++ devel/rt3/patches/patch-lib_RT.pm
@@ -1,10 +1,20 @@
-$NetBSD: patch-lib_RT.pm,v 1.1 2011/10/25 19:38:10 spz Exp $
+$NetBSD: patch-lib_RT.pm,v 1.2 2015/03/01 22:45:26 spz Exp $
 
 perl 5.14 qw() in for* fixes
+Fix for CVE-2014-9472 taken from the patch for RT 4.0.0
 
---- lib/RT.pm.orig	2011-04-14 01:10:13.000000000 +0000
+--- lib/RT.pm.orig	2013-05-22 19:04:26.000000000 +0000
 +++ lib/RT.pm
-@@ -459,7 +459,7 @@ sub InitClasses {
+@@ -362,6 +362,8 @@ sub InitSignalHandlers {
+ ## mechanism (see above).
+ 
+     $SIG{__WARN__} = sub {
++        return 'IGNORE' if $_[0] and $_[0] =~ /^Code point \S+ is not Unicode, may not be portable/;
++
+         # The 'wide character' warnings has to be silenced for now, at least
+         # until HTML::Mason offers a sane way to process both raw output and
+         # unicode strings.
+@@ -459,7 +461,7 @@ sub InitClasses {
      # in the session, as we deserialize it so we never call constructor
      # of the class, so the list of accessible fields is empty and we die
      # with "Method xxx is not implemented in RT::SomeClass"
@@ -13,7 +23,7 @@ perl 5.14 qw() in for* fixes
          RT::Ticket
          RT::Transaction
          RT::Attachment
-@@ -477,7 +477,7 @@ sub InitClasses {
+@@ -477,7 +479,7 @@ sub InitClasses {
          RT::ObjectCustomField
          RT::ObjectCustomFieldValue
          RT::Attribute
