de8cabf02df4b4eff6f857b56ff6b2deabbb087a
tnn <tnn@pkgsrc.org>
2015-03-02 03:53:26 +0000

    - put -D__STDC_LIMIT_MACROS on command line instead of patching it everywhere - don't use ALT_JDK_IMPORT_PATH on SunOS, just build like on other platforms - regen all bootstraps to facilitate removal of the +UseSerialGC hacks - bootstrap binaries on FTP are now PGP signed - remove a handful of obsolete patches

--- lang/openjdk8/patches/patch-hotspot_src_share_vm_utilities_globalDefinitions__gcc.hpp
+++ lang/openjdk8/patches/patch-hotspot_src_share_vm_utilities_globalDefinitions__gcc.hpp
@@ -1,33 +1,13 @@
-$NetBSD: patch-hotspot_src_share_vm_utilities_globalDefinitions__gcc.hpp,v 1.1 2015/03/01 21:43:50 tnn Exp $
+$NetBSD: patch-hotspot_src_share_vm_utilities_globalDefinitions__gcc.hpp,v 1.2 2015/03/02 03:53:26 tnn Exp $
 
 --- hotspot/src/share/vm/utilities/globalDefinitions_gcc.hpp.orig	2015-02-02 15:32:37.000000000 +0000
 +++ hotspot/src/share/vm/utilities/globalDefinitions_gcc.hpp
-@@ -31,6 +31,16 @@
- // globally used constants & types, class (forward)
- // declarations and a few frequently used utility functions.
- 
-+#ifndef __STDC_CONSTANT_MACROS
-+#define __STDC_CONSTANT_MACROS
-+#endif
-+#ifndef __STDC_FORMAT_MACROS
-+#define __STDC_FORMAT_MACROS
-+#endif
-+#ifndef __STDC_LIMIT_MACROS
-+#define __STDC_LIMIT_MACROS
-+#endif
-+
- #include <ctype.h>
- #include <string.h>
- #include <stdarg.h>
-@@ -76,10 +86,7 @@
+@@ -76,7 +76,7 @@
  # include <sys/procfs.h>
  # endif
  
 -#if defined(LINUX) || defined(_ALLBSD_SOURCE)
--#ifndef __STDC_LIMIT_MACROS
--#define __STDC_LIMIT_MACROS
--#endif // __STDC_LIMIT_MACROS
 +#if defined(LINUX) || defined(_ALLBSD_SOURCE) || defined(SOLARIS)
- #include <inttypes.h>
- #include <signal.h>
- #ifndef __OpenBSD__
+ #ifndef __STDC_LIMIT_MACROS
+ #define __STDC_LIMIT_MACROS
+ #endif // __STDC_LIMIT_MACROS
