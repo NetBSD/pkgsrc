2b3c36fad18154c32eb753e7d4aa38e7a28823d7
tnn <tnn@pkgsrc.org>
2015-03-05 22:31:56 +0000

    bump bl3 due to API change

--- security/mhash/buildlink3.mk
+++ security/mhash/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:25:20 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2015/03/05 22:31:56 tnn Exp $
 
 BUILDLINK_TREE+=	mhash
 
 .if !defined(MHASH_BUILDLINK3_MK)
 MHASH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mhash+=	mhash>=0.8.3
-BUILDLINK_ABI_DEPENDS.mhash+=	mhash>=0.9.2nb1
+BUILDLINK_API_DEPENDS.mhash+=	mhash>=0.9.9nb6
 BUILDLINK_PKGSRCDIR.mhash?=	../../security/mhash
 .endif # MHASH_BUILDLINK3_MK
 
