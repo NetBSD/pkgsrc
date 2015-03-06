9b33056ed4a6a3bddb265130795f9dbc4b78e8c8
tnn <tnn@pkgsrc.org>
2015-03-06 13:22:03 +0000

    Replace user-exec.c patch with cleaned up version which was sent upstream. Includes PR pkg/49723 fix for sparc64.

--- emulators/qemu/patches/patch-user-exec.c
+++ emulators/qemu/patches/patch-user-exec.c
@@ -1,10 +1,36 @@
-$NetBSD: patch-user-exec.c,v 1.4 2014/01/15 18:26:20 wiz Exp $
+$NetBSD: patch-user-exec.c,v 1.5 2015/03/06 13:22:03 tnn Exp $
 
-Add OS dependent code for NetBSD/arm.
+From 3f9744aaaeeaea9c55c814b9e7be0f8cce1fb7f9 Mon Sep 17 00:00:00 2001
+From: Tobias Nygren <tnn@NetBSD.org>
+To: qemu-devel@nongnu.org
+Cc: Tobias Nygren <tnn@NetBSD.org>
+Date: Thu, 5 Mar 2015 22:15:08 +0100
+Subject: [PATCH] user-exec.c: fix build on NetBSD/sparc64 and NetBSD/arm
 
---- user-exec.c.orig	2013-11-27 22:15:55.000000000 +0000
+A couple of #ifdef changes necessary to use NetBSD's ucontext
+structs on sparc64 and arm.
+
+Signed-off-by: Tobias Nygren <tnn@NetBSD.org>
+---
+ user-exec.c | 16 +++++++++++++++-
+ 1 file changed, 15 insertions(+), 1 deletion(-)
+
+diff --git a/user-exec.c b/user-exec.c
+index 1ff8673..8f57e8a 100644
+--- user-exec.c
 +++ user-exec.c
-@@ -434,19 +434,31 @@ int cpu_signal_handler(int host_signum, 
+@@ -404,6 +404,10 @@ int cpu_signal_handler(int host_signum, void *pinfo,
+     struct sigcontext *uc = puc;
+     unsigned long pc = uc->sc_pc;
+     void *sigmask = (void *)(long)uc->sc_mask;
++#elif defined(__NetBSD__)
++    ucontext_t *uc = puc;
++    unsigned long pc = _UC_MACHINE_PC(uc);
++    void *sigmask = (void *)&uc->uc_sigmask;
+ #endif
+ #endif
+ 
+@@ -441,15 +445,25 @@ int cpu_signal_handler(int host_signum, void *pinfo,
  
  #elif defined(__arm__)
  
@@ -24,15 +50,10 @@ Add OS dependent code for NetBSD/arm.
      unsigned long pc;
      int is_write;
  
+-#if defined(__GLIBC__) && (__GLIBC__ < 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ <= 3))
 +#if defined(__NetBSD__)
 +    pc = uc->uc_mcontext.__gregs[_REG_R15];
-+#else
- #if defined(__GLIBC__) && (__GLIBC__ < 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ <= 3))
++#elif defined(__GLIBC__) && (__GLIBC__ < 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ <= 3))
      pc = uc->uc_mcontext.gregs[R15];
  #else
      pc = uc->uc_mcontext.arm_pc;
- #endif
-+#endif
- 
-     /* error_code is the FSR value, in which bit 11 is WnR (assuming a v6 or
-      * later processor; on v5 we will always report this as a read).
