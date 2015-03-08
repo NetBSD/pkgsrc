796b0fdf0d7130d586f0aee4c3cc798f87d8b374
taca <taca@pkgsrc.org>
2015-03-08 15:08:33 +0000

    * Add patch ot fix rdoc bug; Get rid of infitite loop with '\r' only in text. * Change "os" of Gem::Platform, now it change the case of "netbsd" only.
    Bump PKGREVISION.

--- lang/ruby21-base/patches/patch-lib_rubygems.rb
+++ lang/ruby21-base/patches/patch-lib_rubygems.rb
@@ -1,22 +1,31 @@
-$NetBSD: patch-lib_rubygems.rb,v 1.1 2014/03/14 19:40:47 taca Exp $
+$NetBSD: patch-lib_rubygems.rb,v 1.2 2015/03/08 15:08:33 taca Exp $
 
 * Add install_root option for pkgsrc's rubygems support.
 
 --- lib/rubygems.rb.orig	2014-02-06 02:59:36.000000000 +0000
 +++ lib/rubygems.rb
-@@ -257,7 +257,10 @@ module Gem
+@@ -257,10 +257,16 @@ module Gem
    ##
    # The path where gem executables are to be installed.
  
 -  def self.bindir(install_dir=Gem.dir)
+-    return File.join install_dir, 'bin' unless
+-      install_dir.to_s == Gem.default_dir.to_s
+-    Gem.default_bindir
 +  def self.bindir(install_dir=Gem.dir, install_root=nil)
-+    unless install_root.nil? or install_root.empty?
-+      install_dir = install_dir.sub(install_root, "")
++    if install_dir.to_s == Gem.default_dir.to_s
++      install_dir = Gem.default_bindir
++    else
++      install_dir = File.join install_dir, 'bin'
 +    end
-     return File.join install_dir, 'bin' unless
-       install_dir.to_s == Gem.default_dir.to_s
-     Gem.default_bindir
-@@ -572,7 +575,7 @@ module Gem
++    unless install_root.nil? or install_root.empty?
++      install_dir = File.join install_root, install_dir
++    end
++    install_dir
+   end
+ 
+   ##
+@@ -572,7 +578,7 @@ module Gem
    # gem's paths are inserted before site lib directory by default.
  
    def self.load_path_insert_index
@@ -25,7 +34,7 @@ $NetBSD: patch-lib_rubygems.rb,v 1.1 2014/03/14 19:40:47 taca Exp $
  
      index
    end
-@@ -743,7 +746,7 @@ module Gem
+@@ -743,7 +749,7 @@ module Gem
    def self.prefix
      prefix = File.dirname RUBYGEMS_DIR
  
