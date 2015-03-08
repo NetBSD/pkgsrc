ce7d850f87d641d7af93631d5779f509fb032cae
alnsn <alnsn@pkgsrc.org>
2015-03-08 10:55:19 +0000

    Update lang/lua52 to version 5.2.4.
    Lua 5.2.4 fixes all bugs listed in
    http://www.lua.org/bugs.html#5.2.3 [*]
    and is now the current release of Lua 5.2.
    
    Like all minor releases, this is strictly a bug-fix release;
    no new features or improvements have been added.
    
    [*] http://www.lua.org/bugs.html#5.2.3
    1. Compiler can optimize away overflow check in table.unpack.
    2. Ephemeron table can wrongly collect entry with strong key.
    3. Chunk with too many lines may crash Lua.

--- lang/lua52/version.mk
+++ lang/lua52/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.2 2014/01/02 19:57:39 wiz Exp $
+# $NetBSD: version.mk,v 1.3 2015/03/08 10:55:19 alnsn Exp $
 
-LUA_VERSION=	5.2.3
+LUA_VERSION=	5.2.4
 
 LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
 LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
