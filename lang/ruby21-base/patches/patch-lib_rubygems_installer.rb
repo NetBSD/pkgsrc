b9471d6e9cc5a16be9db6d4ec8335d3e38b6962e
taca <taca@pkgsrc.org>
2015-03-08 15:08:33 +0000

    * Add patch ot fix rdoc bug; Get rid of infitite loop with '\r' only in text. * Change "os" of Gem::Platform, now it change the case of "netbsd" only.
    Bump PKGREVISION.

--- lang/ruby21-base/patches/patch-lib_rubygems_installer.rb
+++ lang/ruby21-base/patches/patch-lib_rubygems_installer.rb
@@ -1,43 +1,36 @@
-$NetBSD: patch-lib_rubygems_installer.rb,v 1.1 2014/03/14 19:40:47 taca Exp $
+$NetBSD: patch-lib_rubygems_installer.rb,v 1.2 2015/03/08 15:08:33 taca Exp $
 
 * Add install_root option for pkgsrc's rubygems support.
 * Tweak build_info directory with destdir to store build_args.
 
---- lib/rubygems/installer.rb.orig	2013-12-19 03:05:37.000000000 +0000
+--- lib/rubygems/installer.rb.orig	2014-02-06 02:59:36.000000000 +0000
 +++ lib/rubygems/installer.rb
-@@ -95,6 +95,9 @@ class Gem::Installer
+@@ -94,6 +94,9 @@ class Gem::Installer
+   #                      If your Ruby is ruby18, foo_exec will be installed as
    #                      foo_exec18.
    # :ignore_dependencies:: Don't raise if a dependency is missing.
-   # :install_dir:: The directory to install the gem into.
 +  # :install_root:: The directory to use as a buildroot for "destdir"-style
 +  #		  installation.  All paths during installation are relative
 +  #               to the buildroot.
+   # :install_dir:: The directory to install the gem into.
    # :security_policy:: Use the specified security policy.  See Gem::Security
    # :user_install:: Indicate that the gem should be unpacked into the users
-   #                 personal gem directory.
-@@ -583,12 +586,20 @@ class Gem::Installer
-     @format_executable   = options[:format_executable]
-     @security_policy     = options[:security_policy]
-     @wrappers            = options[:wrappers]
-+    install_root         = options[:install_root]
-+    unless install_root.nil? or install_root.empty?
-+      @install_root = File.expand_path install_root
-+      @gem_home = File.join(@install_root, @gem_home)
-+    end
-     @only_install_dir    = options[:only_install_dir]
- 
+@@ -588,7 +591,13 @@ class Gem::Installer
      # If the user has asked for the gem to be installed in a directory that is
      # the system gem directory, then use the system bin directory, else create
      # (or use) a new bin dir under the gem_home.
 -    @bin_dir             = options[:bin_dir] || Gem.bindir(gem_home)
-+    @bin_dir             = options[:bin_dir] || Gem.bindir(gem_home, @install_root)
-+    unless @install_root.nil? or @install_root.empty?
-+      @bin_dir = File.join(@install_root, @bin_dir)
++    install_root         = options[:install_root]
++    @bin_dir             = options[:bin_dir] || Gem.bindir(gem_home, install_root)
++    @development         = options[:development]
++    unless install_root.nil? or install_root.empty?
++      @install_root = File.expand_path install_root
++      @gem_home = File.join(@install_root, @gem_home)
 +    end
      @development         = options[:development]
  
      @build_args          = options[:build_args] || Gem::Command.build_args
-@@ -769,6 +780,9 @@ TEXT
+@@ -769,6 +778,9 @@ TEXT
      return if @build_args.empty?
  
      build_info_dir = File.join gem_home, 'build_info'
--- lang/ruby21-base/patches/patch-lib_rubygems_platform.rb
+++ lang/ruby21-base/patches/patch-lib_rubygems_platform.rb
@@ -1,32 +1,29 @@
-$NetBSD: patch-lib_rubygems_platform.rb,v 1.2 2014/03/30 17:15:01 taca Exp $
+$NetBSD: patch-lib_rubygems_platform.rb,v 1.3 2015/03/08 15:08:33 taca Exp $
 
 * Don't replace "i486" to "x86".
-* Allow simple "netbsd" as Gem::Platform.
+* Make various NetBSD to simple "netbsd".
 
 --- lib/rubygems/platform.rb.orig	2014-02-06 02:59:36.000000000 +0000
 +++ lib/rubygems/platform.rb
-@@ -63,7 +63,7 @@ class Gem::Platform
+@@ -62,10 +62,7 @@ class Gem::Platform
+ 
        cpu = arch.shift
  
-       @cpu = case cpu
+-      @cpu = case cpu
 -             when /i\d86/ then 'x86'
-+             when /noti\d86/ then 'x86'
-              else cpu
-              end
+-             else cpu
+-             end
++      @cpu = cpu
  
-@@ -94,8 +94,9 @@ class Gem::Platform
+       if arch.length == 2 and arch.last =~ /^\d+(\.\d+)?$/ then # for command-line
+         @os, @version = arch
+@@ -93,7 +90,8 @@ class Gem::Platform
+                         os, version = $1, $3
                          @cpu = 'x86' if @cpu.nil? and os =~ /32$/
                          [os, version]
-                       when /netbsdelf/ then             [ 'netbsdelf', nil ]
-+                      when /netbsd/ then                [ 'netbsd', nil ]
+-                      when /netbsdelf/ then             [ 'netbsdelf', nil ]
++                      when /netbsd(elf)?([\d\.]*)-*.*/ then
++                        ['netbsd', ($2.empty?)? nil: $2]
                        when /openbsd(\d+\.\d+)?/ then    [ 'openbsd',   $1  ]
--                      when /solaris(\d+\.\d+)?/ then    [ 'solaris',   $1  ]
-+                      when /solaris(\d+\.\d+)?/ then    [ "solaris#{$1}", nil ]
+                       when /solaris(\d+\.\d+)?/ then    [ 'solaris',   $1  ]
                        # test
-                       when /^(\w+_platform)(\d+)?/ then [ $1,          $2  ]
-                       else                              [ 'unknown',   nil ]
-@@ -201,3 +202,4 @@ class Gem::Platform
-   CURRENT = 'current'
- end
- 
-+
