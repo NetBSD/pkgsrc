d0c5fda5e0d2444a79ea3c8fd221f83525fbd232
pettai <pettai@pkgsrc.org>
2015-03-09 11:59:26 +0000

    update the rc-script so it works correctly for the unbound user

--- net/unbound/files/unbound.sh
+++ net/unbound/files/unbound.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: unbound.sh,v 1.2 2009/11/24 01:08:06 roy Exp $
+# $NetBSD: unbound.sh,v 1.3 2015/03/09 11:59:26 pettai Exp $
 #
 # PROVIDE: unbound
 # REQUIRE: DAEMON
@@ -13,7 +13,25 @@ rcvar=${name}
 
 command=@PREFIX@/sbin/${name}
 pidfile=/var/run/${name}.pid
+required_files="@PKG_SYSCONFDIR@/unbound.conf"
 extra_commands="reload"
+start_precmd="unbound_precmd"
 
-load_rc_config ${name}
-run_rc_command "$1"
+unbound_precmd()
+{
+	if [ ! -d @VARBASE@/run/unbound ]; then
+		@MKDIR@ @VARBASE@/run/unbound
+		@CHMOD@ 0750 @VARBASE@/run/unbound
+		@CHOWN@ @UNBOUND_USER@:@UNBOUND_GROUP@ @VARBASE@/run/unbound
+	fi
+}
+
+unbound_flags=${unbound_flags:-""}
+
+if [ -f /etc/rc.subr ]; then
+	load_rc_config ${name}
+	run_rc_command "$1"
+else
+	echo -n " ${name}"
+	${command} ${nsd_flags}
+fi
