496db0dc2c278e294f4c188cf1d5664982a6a084
pettai <pettai@pkgsrc.org>
2015-03-09 11:34:26 +0000

    update the rc-script so it works correctly for the nsd user

--- net/nsd/files/nsd.sh
+++ net/nsd/files/nsd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: nsd.sh,v 1.3 2008/04/24 09:38:30 joerg Exp $
+# $NetBSD: nsd.sh,v 1.4 2015/03/09 11:34:26 pettai Exp $
 #
 # PROVIDE: nsd
 # REQUIRE: DAEMON
@@ -12,9 +12,25 @@ name="nsd"
 rcvar=${name}
 
 command=@PREFIX@/sbin/${name}
-pidfile=/var/run/${name}.pid
+pidfile=/var/run/${name}/${name}.pid
+required_files="@PKG_SYSCONFDIR@/nsd.conf"
+start_precmd="nsd_precmd"
+
+nsd_precmd()
+{
+	if [ ! -d @VARBASE@/run/nsd ]; then
+		@MKDIR@ @VARBASE@/run/nsd
+		@CHMOD@ 0750 @VARBASE@/run/nsd
+		@CHOWN@ @NSD_USER@:@NSD_GROUP@ @VARBASE@/run/nsd
+	fi
+}
 
 nsd_flags=${nsd_flags:-""}
 
-load_rc_config ${name}
-run_rc_command "$1"
+if [ -f /etc/rc.subr ]; then
+	load_rc_config ${name}
+	run_rc_command "$1"
+else
+	echo -n " ${name}"
+	${command} ${nsd_flags}
+fi
