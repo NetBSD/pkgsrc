99698e559032fcad5504c2cee9efb02f6af97ff3
jperkin <jperkin>
2015-03-10 13:11:36 +0000

    Implement cgo support for illumos.  Patches by Keith Wesolowski at Joyent.
    Bump PKGREVISION.

---
+++ lang/go/patches/patch-src_cmd_go_build.go
@@ -0,0 +1,17 @@
+$NetBSD: patch-src_cmd_go_build.go,v 1.3 2015/03/10 13:11:36 jperkin Exp $
+
+Support cgo on illumos.
+
+--- src/cmd/go/build.go.orig	2014-12-11 01:18:12.000000000 +0000
++++ src/cmd/go/build.go
+@@ -2330,6 +2330,10 @@ func (b *builder) cgo(p *Package, cgoExe
+ 	if pie { // we need to use -pie for Linux/ARM to get accurate imported sym
+ 		cgoLDFLAGS = append(cgoLDFLAGS, "-pie")
+ 	}
++	lgcc_s := goos == "solaris"
++	if lgcc_s {
++		cgoLDFLAGS = append(cgoLDFLAGS, "-lgcc_s");
++	}
+ 	if err := b.gccld(p, dynobj, cgoLDFLAGS, linkobj); err != nil {
+ 		return nil, nil, err
+ 	}
