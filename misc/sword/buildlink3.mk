9227dff74ec60ae5c9fc323ad10cbcc58c128898
nros <nros@pkgsrc.org>
2015-03-12 13:30:28 +0000

    Import wip/sword as misc/sword, reviewed by sevan and wiz.
    The SWORD Project is the CrossWire Bible Society's free Bible software
    project. Its purpose is to create cross-platform open-source tools that
    allow programmers and Bible societies to write new Bible software more
    quickly and easily.

---
+++ misc/sword/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/03/12 13:30:28 nros Exp $
+
+BUILDLINK_TREE+=	sword
+
+.if !defined(SWORD_BUILDLINK3_MK)
+SWORD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.sword+=	sword>=1.7.4
+BUILDLINK_PKGSRCDIR.sword?=	../../misc/sword
+
+.include "../../textproc/icu/buildlink3.mk"
+.include "../../textproc/libclucene/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
+.endif	# SWORD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-sword
