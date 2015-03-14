80f89e26ce0f883da2e0493cfda952315ea225af
gdt <gdt@pkgsrc.org>
2015-03-14 18:39:05 +0000

    Adjust ABI/API for new proj (which changes both).
    CVS: ----------------------------------------------------------------------
    CVS: CVSROOT  cvs.NetBSD.org:/cvsroot
    CVS: please use "PR category/123" to have the commitmsg appended to PR 123

--- geography/proj/buildlink3.mk
+++ geography/proj/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/05/07 01:53:31 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/03/14 18:39:05 gdt Exp $
 
 BUILDLINK_TREE+=	proj
 
 .if !defined(PROJ_BUILDLINK3_MK)
 PROJ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.proj+=	proj>=4.4.7nb1
-BUILDLINK_ABI_DEPENDS.proj+=	proj>=4.4.7nb2
+BUILDLINK_API_DEPENDS.proj+=	proj>=4.9.1
+BUILDLINK_ABI_DEPENDS.proj+=	proj>=4.9.1
 BUILDLINK_PKGSRCDIR.proj?=	../../geography/proj
 .endif # PROJ_BUILDLINK3_MK
 
