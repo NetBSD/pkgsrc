38c67de5f8ba4fe2149d2dd5d86e5bdc947a2e6a
tnn <tnn@pkgsrc.org>
2015-03-14 14:33:37 +0000

    defuzz patch context so it applies

--- net/dnstracer/patches/patch-dnstracer.c
+++ net/dnstracer/patches/patch-dnstracer.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-dnstracer.c,v 1.1 2014/02/26 18:17:39 wiedi Exp $
+$NetBSD: patch-dnstracer.c,v 1.2 2015/03/14 14:33:37 tnn Exp $
 There is no gethostbyname2 on SunOS, always use the wrapper
 
---- dnstracer.c.orig	2008-02-09 12:47:37.000000000 +0000
+--- dnstracer.c.orig	2008-06-11 12:50:53.000000000 +0000
 +++ dnstracer.c
 @@ -66,7 +66,7 @@
- #define DEFAULT_QUERYTYPE	ns_t_a
- #define DEFAULT_NOIPV6	0
+ #define DEFAULT_NOIPV6			0
+ #define DEFAULT_EDNS0SIZE		1500
  
 -#ifdef NOIPV6
 +#if defined(NOIPV6) || defined(__sun)
