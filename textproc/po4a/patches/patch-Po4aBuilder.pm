e755cc3e6440f6e78d6ebd9b7f696cca65626c51
mef <mef@pkgsrc.org>
2015-03-18 02:25:23 +0000

    Fix build problems (thanks joerg for the report)  - Add Two DEPENDS    p5-CPAN-Meta>=2.141170    p5-PerlIO-utf8_strict-[0-9]*  - Fix strange issue msgmerge PATH not found problem (by SUBST)  - Another strange --previous option is said unknown by msgmerge    (with patch-Po4aBuilder.pm)

--- textproc/po4a/patches/patch-Po4aBuilder.pm
+++ textproc/po4a/patches/patch-Po4aBuilder.pm
@@ -1,8 +1,31 @@
-$NetBSD: patch-Po4aBuilder.pm,v 1.1 2014/12/03 07:08:36 ryoon Exp $
+$NetBSD: patch-Po4aBuilder.pm,v 1.2 2015/03/18 02:25:23 mef Exp $
 
---- Po4aBuilder.pm.orig	2013-08-21 20:11:04.000000000 +0000
-+++ Po4aBuilder.pm
-@@ -253,8 +253,6 @@ sub ACTION_man {
+(1) ryoon: remove gzip at initial import
+(2) mef: Add PerlIO::F_UTF8 () macro
+(3) mef: to avoid --previous option is said unknown to msgmerge
+    (but sounds strange)
+
+--- Po4aBuilder.pm.orig	2013-08-22 05:11:04.000000000 +0900
++++ Po4aBuilder.pm	2015-03-18 11:10:49.000000000 +0900
+@@ -8,6 +8,8 @@ use File::stat;
+ 
+ @ISA = qw(Module::Build);
+ 
++sub PerlIO::F_UTF8 () { 0x00008000 } # from perliol.h
++
+ sub ACTION_build {
+     my $self = shift;
+     $self->depends_on('code');
+@@ -104,7 +106,7 @@ sub ACTION_binpo {
+         my $lang = fileparse($_, qw{.po});
+         unless ($self->up_to_date("po/bin/po4a.pot", $_)) {
+             print "XX Sync $_: ";
+-            system("msgmerge --previous $_ po/bin/po4a.pot -o $_.new") && die;
++            system("msgmerge $_ po/bin/po4a.pot -o $_.new") && die;
+             # Typically all that changes was a date. I'd
+             # prefer not to commit such changes, so detect
+             # and ignore them.
+@@ -253,8 +255,6 @@ sub ACTION_man {
          }
          $parser->parse_from_file ($file, $out);
  
@@ -11,7 +34,7 @@ $NetBSD: patch-Po4aBuilder.pm,v 1.1 2014/12/03 07:08:36 ryoon Exp $
      }
  
      # Install the manpages written in XML DocBook
-@@ -265,9 +263,7 @@ sub ACTION_man {
+@@ -265,9 +265,7 @@ sub ACTION_man {
          if ($file =~ m,(.*/man(.))/([^/]*)\.xml$,) {
              my ($outdir, $section, $outfile) = ($1, $2, $3);
              system("xsltproc -o $outdir/$outfile.$section --nonet http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl $file") and die;
