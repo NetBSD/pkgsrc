7da7b37da00f80719f8d68476d6adbf2e30d58d3
he <he@pkgsrc.org>
2015-03-19 08:12:50 +0000

    Add a comment to the patch.

--- lang/php55/patches/patch-sapi_fpm_fpm_fpm__sockets.c
+++ lang/php55/patches/patch-sapi_fpm_fpm_fpm__sockets.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-sapi_fpm_fpm_fpm__sockets.c,v 1.1 2015/03/05 11:16:28 he Exp $
+$NetBSD: patch-sapi_fpm_fpm_fpm__sockets.c,v 1.2 2015/03/19 08:12:50 he Exp $
+
+Treat NetBSD the same as FreeBSD wrt. tcp_info struct field usage.
 
 --- ./sapi/fpm/fpm/fpm_sockets.c.orig	2014-09-17 07:03:27.000000000 +0000
 +++ ./sapi/fpm/fpm/fpm_sockets.c
