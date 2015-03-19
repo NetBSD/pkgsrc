f869c30f820d23e7a00496ea16b54e8f06cb5f10
he <he@pkgsrc.org>
2015-03-19 09:20:30 +0000

    Update to version 3.4.9.
    Pkgsrc changes:
     * fix up the set of REPLACE_PYTHON settings to match
     * remove patch which has been accepted upstream, adapt remaining patch
     * install the newly contributed man pages
     * adapt PLIST
    
    Upstream changes:
    3.4.9:
      This release mostly fixes a very embarrassing issue introduced with
      3.4.8 with non IPv4 unicast route decoding, the handlers were badly
      registered.
    
    3.4.8:
      This is a bug fix release. It fixes an obscure bug when routes
      would not reach the backend API as two NLRI could be seen as
      identical due to a bad hashing of the attributes.
    
      * Fix: bug with multiple configuration files
      * Fix: allow generic attribute not only in single line but also
        in multiple lines patch by: Eiichiro Watanabe (issue #214)
      * Fix: issue with parsing extended-community origin reported by: Tim Preston
      * Fix: handle numeric community parsing correctly reported by: Aaron Kalin
      * Fix: bug in AS_PATH with AS_SET handling patch by: Eiichiro Watanabe
      * Fix: off by one for the maximum message size generation reported
        by: Eiichiro Watanabe
      * Fix: issue with handling of some generic attributes reported
        by: Hiroshi Yokoi
      * Fix: restore old api syntax broken by mistake patch by: David Waring
      * Fix: issue with E-VPN NLRI patch by: Thomas Morin
      * Fix: bad iteration for JSON generation ( bug created during
        3.4.8 dev ) patch by: Ian bobbitt
      * Fix: healthcheck.py: optionally match "alias" in ifconfig output
        patch by: Havard Eidnes
      * Fix: healthcheck.py: make the ifconfig path work; regexp fix
        patch by: Havard Eidnes
      * Change: moved the netlink library within exabgp
      * Feature: qa/bin/ip and qa/bin/route

--- net/exabgp/patches/patch-setup.py
+++ net/exabgp/patches/patch-setup.py
@@ -1,18 +1,19 @@
-$NetBSD: patch-setup.py,v 1.3 2015/02/19 12:23:52 he Exp $
+$NetBSD: patch-setup.py,v 1.4 2015/03/19 09:20:30 he Exp $
 
-Do away with the special handling for NetBSD, to avoid PLIST issues.
+Do away with the special handling for NetBSD, to avoid PLIST issues
+between different pkgsrc-using platforms.
 Install helper scripts outside of the config tree (use share/exabgp).
 On the other hand, only do the systemd dance on Linux.
 
---- setup.py.orig	2015-01-08 09:15:39.000000000 +0000
+--- setup.py.orig	2015-03-14 15:32:13.000000000 +0000
 +++ setup.py
-@@ -371,14 +371,10 @@ def configuration (etc):
+@@ -364,14 +364,10 @@ def configuration (etc):
  
  os_name = platform.system()
  
 -if os_name == 'NetBSD':
 -	files_definition= [
--		('bin/exabgp',configuration('etc/exabgp')),
+-		('share/exabgp',configuration('etc/exabgp')),
 -	]
 -else:
 -	files_definition = [
