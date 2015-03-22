5bbf0110120ae8aac0cbf2d18a83815cfbb59071
bsiegert <bsiegert@pkgsrc.org>
2015-03-22 17:20:24 +0000

    SECURITY: Add a patch for CVE-2014-5461, taken from the Debian package. Bump PKGREVISION.

---
+++ lang/lua51/patches/patch-src_ldo.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-src_ldo.c,v 1.1 2015/03/22 17:20:24 bsiegert Exp $
+
+Patch for CVS-2014-5461, from Debian.
+--- src/ldo.c.orig	2015-03-22 17:11:01.000000000 +0000
++++ src/ldo.c
+@@ -274,7 +274,7 @@ int luaD_precall (lua_State *L, StkId fu
+     CallInfo *ci;
+     StkId st, base;
+     Proto *p = cl->p;
+-    luaD_checkstack(L, p->maxstacksize);
++    luaD_checkstack(L, p->maxstacksize + p->numparams);
+     func = restorestack(L, funcr);
+     if (!p->is_vararg) {  /* no varargs? */
+       base = func + 1;
