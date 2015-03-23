bb98100b38fdf1dd4d84331b74167b6e79ecf824
fhajny <fhajny@pkgsrc.org>
2015-03-23 22:24:11 +0000

    Update archivers/lz4 to 127.
    r127:
    N/A   : added a file on SVN
    
    r126:
    New   : lz4frame API is now integrated into liblz4
    Fixed : GCC 4.9 bug on highest performance settings, reported by Greg Slazinski
    Fixed : bug within LZ4 HC streaming mode, reported by James Boyle
    Fixed : older compiler don't like nameless unions, reported by Cheyi Lin
    Changed : lz4 is C90 compatible
    Changed : added -pedantic option, fixed a few mminor warnings
    
    r125:
    Changed : endian and alignment code
    Changed : directory structure : new "lib" directory
    Updated : lz4io, now uses lz4frame
    Improved: slightly improved decoding speed
    Fixed : LZ4_compress_limitedOutput(); Special thanks to Christopher Speller !
    Fixed : some alignment warnings under clang
    Fixed : deprecated function LZ4_slideInputBufferHC()

--- archivers/lz4/buildlink3.mk
+++ archivers/lz4/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/08/08 13:16:45 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/03/23 22:24:11 fhajny Exp $
 
 BUILDLINK_TREE+=	lz4
 
 .if !defined(LZ4_BUILDLINK3_MK)
 LZ4_BUILDLINK3_MK:=
 
-BUILDLINK_ABI_DEPENDS.lz4+=	lz4>=120
-BUILDLINK_API_DEPENDS.lz4+=	lz4>=120
+BUILDLINK_ABI_DEPENDS.lz4+=	lz4>=126
+BUILDLINK_API_DEPENDS.lz4+=	lz4>=126
 BUILDLINK_PKGSRCDIR.lz4?=	../../archivers/lz4
 .endif # LZ4_BUILDLINK3_MK
 
--- archivers/lz4/patches/patch-programs_fuzzer.c
+++ archivers/lz4/patches/patch-programs_fuzzer.c
@@ -1,24 +1,24 @@
-$NetBSD: patch-programs_fuzzer.c,v 1.3 2014/12/12 11:20:56 fhajny Exp $
+$NetBSD: patch-programs_fuzzer.c,v 1.4 2015/03/23 22:24:11 fhajny Exp $
 
 Add portable code for ftime for NetBSD.
 
 Based on https://code.google.com/p/lz4/source/detail?r=81.
---- programs/fuzzer.c.orig	2014-11-06 18:39:42.000000000 +0000
+--- programs/fuzzer.c.orig	2014-12-19 08:51:32.000000000 +0000
 +++ programs/fuzzer.c
-@@ -30,6 +30,7 @@ Remove Visual warning messages
- #  pragma warning(disable : 4127)        /* disable: C4127: conditional expression is constant */
- #  pragma warning(disable : 4146)        /* disable: C4146: minus unsigned expression */
- #  pragma warning(disable : 4310)        /* disable: C4310: constant char value > 127 */
+@@ -32,6 +32,7 @@
+ #  pragma warning(disable : 4127)    /* disable: C4127: conditional expression is constant */
+ #  pragma warning(disable : 4146)    /* disable: C4146: minus unsigned expression */
+ #  pragma warning(disable : 4310)    /* disable: C4310: constant char value > 127 */
 +#  define BMK_LEGACY_TIMER 1
  #endif
  
  
-@@ -38,12 +39,18 @@ Includes
+@@ -40,12 +41,17 @@
  **************************************/
  #include <stdlib.h>
- #include <stdio.h>      // fgets, sscanf
--#include <sys/timeb.h>  // timeb
- #include <string.h>     // strcmp
+ #include <stdio.h>      /* fgets, sscanf */
+-#include <sys/timeb.h>  /* timeb */
+ #include <string.h>     /* strcmp */
  #include "lz4.h"
  #include "lz4hc.h"
  #include "xxhash.h"
@@ -29,11 +29,10 @@ Based on https://code.google.com/p/lz4/source/detail?r=81.
 +#else
 +#  include <sys/time.h>    // gettimeofday
 +#endif
-+
  
  /**************************************
- Basic Types
-@@ -99,8 +106,11 @@ static U32 g_time = 0;
+ * Basic Types
+@@ -100,8 +106,11 @@ static U32 g_time = 0;
  /*********************************************************
    Fuzzer functions
  *********************************************************/
@@ -45,7 +44,7 @@ Based on https://code.google.com/p/lz4/source/detail?r=81.
      struct timeb tb;
      U32 nCount;
      ftime( &tb );
-@@ -108,6 +118,21 @@ static U32 FUZ_GetMilliStart(void)
+@@ -109,6 +118,21 @@ static U32 FUZ_GetMilliStart(void)
      return nCount;
  }
  
