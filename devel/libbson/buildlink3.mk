2f222dfc941d1b271d15214adb877a28fc4fdf40
fhajny <fhajny@pkgsrc.org>
2015-03-24 13:21:43 +0000

    Update devel/libbson to 1.1.2.
    Libbson 1.1.2
    =============
    * sscanf_s doesn't exist for mingw.
    
    Libbson 1.1.0
    =============
    ABI/API changes include:
    * Deprecation of bson_copy_to_excluding
    * Addition of bson_copy_to_excluding_noinit
    * Removal of MIN, MAX and ABS macros in favor of BSON_MIN, BSON_MAX and
      BSON_ABS.  Note this is a breaking source level change if you relied on
      these from bson.h.  Also note that this is not a breaking ABI change.
    * Addition of BSON_ERROR_BUFFER_SIZE macro
    
    Other changes include:
    * Addition of a versioned ABI for the libbson shared library
    * fixed bson_get_monotonic_time fallback when a system monotonic clock can not
      be found.  Formerly failed to compile with an incorrect call to
      bson_gettimeofday
    * Allow the "dbref" convention in bson_validate when BSON_VALIDATE_DOLLAR_KEYS
      is present
    * Support for ISO-8601 or $numberLong dates in bson <-> json parsing
    * Quiet various compiler warnings

--- devel/libbson/buildlink3.mk
+++ devel/libbson/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/12/12 11:29:32 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2015/03/24 13:21:43 fhajny Exp $
 
 BUILDLINK_TREE+=	libbson
 
 .if !defined(LIBBSON_BUILDLINK3_MK)
 LIBBSON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libbson+=	libbson>=1.0.0
+BUILDLINK_API_DEPENDS.libbson+=	libbson>=1.1.0
 BUILDLINK_PKGSRCDIR.libbson?=	../../devel/libbson
 .endif	# LIBBSON_BUILDLINK3_MK
 
