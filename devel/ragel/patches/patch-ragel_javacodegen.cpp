0931d3d5fb58d1083f52e25652c16421f34cce07
rodent <rodent>
2015-03-27 03:50:55 +0000

    Update to latest release, 6.9. Fix HOMEPAGE and MASTER_SITES. Some patches were applied upstream, so remove. Comment, update, and defuzz remaining ones.
    Ragel 6.9 - Oct 13, 2014
    ========================
     -updated command-line synopsis
     -ocaml: fix missing semicolon
     -ocaml: support -G1
     -ocaml: choose a unique name for type state
     -ruby: reduce the amount of calls to GET_WIDE_KEY()
     -union test case: warning fix
     -omit line directives around expression-oriented write statements
     -use AS_IF and test command to check if the DIST file is present
     -added missing std:: using
     -go: added '//line' directive support
    
    Ragel 6.8 - Feb 11, 2013
    ========================
    
     -The -G2 code generator for Go1 was rewritten. Table, flat and switch-based
      code generators were added. (Anton Ageev)
     -The CXXFLAGS variable is not longer set in the configure script.

--- devel/ragel/patches/patch-ragel_javacodegen.cpp
+++ devel/ragel/patches/patch-ragel_javacodegen.cpp
@@ -1,4 +1,6 @@
-$NetBSD: patch-ragel_javacodegen.cpp,v 1.1 2013/06/10 18:56:05 joerg Exp $
+$NetBSD: patch-ragel_javacodegen.cpp,v 1.2 2015/03/27 03:50:55 rodent Exp $
+
+Make sure setiosflags is visible in the default scope.
 
 --- ragel/javacodegen.cpp.orig	2013-06-07 15:59:16.000000000 +0000
 +++ ragel/javacodegen.cpp
