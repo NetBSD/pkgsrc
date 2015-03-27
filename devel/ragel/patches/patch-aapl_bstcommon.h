4ae6b975a92a1802d47046b36a49ea552c4531fb
rodent <rodent@pkgsrc.org>
2015-03-27 03:50:55 +0000

    Update to latest release, 6.9. Fix HOMEPAGE and MASTER_SITES. Some patches were applied upstream, so remove. Comment, update, and defuzz remaining ones.
    Ragel 6.9 - Oct 13, 2014
    ========================
     -updated command-line synopsis
     -ocaml: fix missing semicolon
     -ocaml: support -G1
     -ocaml: choose a unique name for type state
     -ruby: reduce the amount of calls to GET_WIDE_KEY()
     -union test case: warning fix
     -omit line directives around expression-oriented write statements
     -use AS_IF and test command to check if the DIST file is present
     -added missing std:: using
     -go: added '//line' directive support
    
    Ragel 6.8 - Feb 11, 2013
    ========================
    
     -The -G2 code generator for Go1 was rewritten. Table, flat and switch-based
      code generators were added. (Anton Ageev)
     -The CXXFLAGS variable is not longer set in the configure script.

--- devel/ragel/patches/patch-aapl_bstcommon.h
+++ devel/ragel/patches/patch-aapl_bstcommon.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-aapl_bstcommon.h,v 1.1 2012/11/19 02:53:28 joerg Exp $
+$NetBSD: patch-aapl_bstcommon.h,v 1.2 2015/03/27 03:50:55 rodent Exp $
 
 --- aapl/bstcommon.h.orig	2012-11-19 02:08:41.000000000 +0000
 +++ aapl/bstcommon.h
@@ -8,66 +8,6 @@ $NetBSD: patch-aapl_bstcommon.h,v 1.1 2012/11/19 02:53:28 joerg Exp $
  		mid = lower + ((upper-lower)>>1);
 -		keyRelation = compare(key, GET_KEY(*mid));
 +		keyRelation = this->compare(key, GET_KEY(*mid));
- 
- 		if ( keyRelation < 0 )
- 			upper = mid - 1;
-@@ -373,12 +373,12 @@ template <BST_TEMPL_DEF> bool BstTable<B
- 
- 			lower = mid - 1;
- 			while ( lower != lowEnd && 
--					compare(key, GET_KEY(*lower)) == 0 )
-+					this->compare(key, GET_KEY(*lower)) == 0 )
- 				lower--;
- 
- 			upper = mid + 1;
- 			while ( upper != highEnd && 
--					compare(key, GET_KEY(*upper)) == 0 )
-+					this->compare(key, GET_KEY(*upper)) == 0 )
- 				upper++;
- 			
- 			low = (Element*)lower + 1;
-@@ -419,7 +419,7 @@ template <BST_TEMPL_DEF> Element *BstTab
- 		}
- 
- 		mid = lower + ((upper-lower)>>1);
--		keyRelation = compare(key, GET_KEY(*mid));
-+		keyRelation = this->compare(key, GET_KEY(*mid));
- 
- 		if ( keyRelation < 0 )
- 			upper = mid - 1;
-@@ -457,7 +457,7 @@ template <BST_TEMPL_DEF> Element *BstTab
- 		}
- 
- 		mid = lower + ((upper-lower)>>1);
--		keyRelation = compare(key, GET_KEY(*mid));
-+		keyRelation = this->compare(key, GET_KEY(*mid));
- 
- 		if ( keyRelation < 0 )
- 			upper = mid - 1;
-@@ -508,7 +508,7 @@ template <BST_TEMPL_DEF> Element *BstTab
- 		}
- 
- 		mid = lower + ((upper-lower)>>1);
--		keyRelation = compare(key, GET_KEY(*mid));
-+		keyRelation = this->compare(key, GET_KEY(*mid));
- 
- 		if ( keyRelation < 0 )
- 			upper = mid - 1;
-@@ -603,7 +603,7 @@ template <BST_TEMPL_DEF> Element *BstTab
- 		}
- 
- 		mid = lower + ((upper-lower)>>1);
--		keyRelation = compare(GET_KEY(el), GET_KEY(*mid));
-+		keyRelation = this->compare(GET_KEY(el), GET_KEY(*mid));
- 
- 		if ( keyRelation < 0 )
- 			upper = mid - 1;
-@@ -662,7 +662,7 @@ template <BST_TEMPL_DEF> Element *BstTab
- 		}
- 
- 		mid = lower + ((upper-lower)>>1);
--		keyRelation = compare(GET_KEY(el), GET_KEY(*mid));
-+		keyRelation = this->compare(GET_KEY(el), GET_KEY(*mid));
  
  		if ( keyRelation < 0 )
  			upper = mid - 1;
