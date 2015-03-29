4195208a2a52e08a586844cbdd66660f21322323
rodent <rodent@pkgsrc.org>
2015-03-29 15:28:43 +0000

    Updated to latest release, 5.1.0.3599. Now requires mk/resolv, net/net-snmp devel/zlib, textproc/libxslt, textproc/libxml2, devel/cppunit, mk/pthread. Patches were defuzzed and updated. Patch added to fix LIBDIR expansion. List of changes since the last version and this one is 2788 lines long. Interested parties can find the ChangeLog in the doc/ directory of the distfile or at: http://sourceforge.net/p/fwbuilder/code/ci/builds/tree/doc/ChangeLog

--- security/fwbuilder/patches/patch-src_gui_main.cpp
+++ security/fwbuilder/patches/patch-src_gui_main.cpp
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_gui_main.cpp,v 1.1 2011/04/12 13:44:03 adam Exp $
+$NetBSD: patch-src_gui_main.cpp,v 1.2 2015/03/29 15:28:43 rodent Exp $
 
 Fix buildling on Mac OS X.
 
---- src/gui/main.cpp.orig	2011-04-11 20:07:30.000000000 +0000
+--- src/gui/main.cpp.orig	2012-03-23 06:10:54.000000000 +0000
 +++ src/gui/main.cpp
-@@ -71,9 +71,6 @@
- #include "fwbuilder/Resources.h"
- #include "fwbuilder/FWException.h"
+@@ -72,9 +72,6 @@
+ #include "fwbuilder/Constants.h"
+ 
  
 -#if defined(Q_WS_MAC)
 -extern void connectOdocHandler();
