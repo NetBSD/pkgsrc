efc49c2d1bbd27a4b293ddc431d667df6894e5d3
rodent <rodent@pkgsrc.org>
2015-04-01 11:36:00 +0000

    Forgot .include "../../lang/python/pyversion.mk".

--- devel/py-logan/options.mk
+++ devel/py-logan/options.mk
@@ -1,10 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2015/04/01 02:39:02 rodent Exp $
+# $NetBSD: options.mk,v 1.2 2015/04/01 11:36:00 rodent Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-logan
 PKG_SUPPORTED_OPTIONS=	tests
 PKG_SUGGESTED_OPTIONS+=	# blank
 
 .include "../../mk/bsd.options.mk"
+
+.include "../../lang/python/pyversion.mk"
 
 .if !empty(PKG_OPTIONS:Mtests)
 DEPENDS+=	${PYPKGPREFIX}-django>=1.5:../../www/py-django
