5dc4fef08c3f6720769b4dfc826a7d2d1b18f69b
wiz <wiz@pkgsrc.org>
2015-04-02 22:40:04 +0000

    Update to 2.6.1:
    version 2.6.1:
    - avformat/mov: Disallow ".." in dref unless use_absolute_path is set
    - avfilter/palettegen: make sure at least one frame was sent to the filter
    - avformat/mov: Check for string truncation in mov_open_dref()
    - ac3_fixed: fix out-of-bound read
    - mips/asmdefs: use _ABI64 as defined by gcc
    - hevc: delay ff_thread_finish_setup for hwaccel
    - avcodec/012v: Check dimensions more completely
    - asfenc: fix leaking asf->index_ptr on error
    - roqvideoenc: set enc->avctx in roq_encode_init
    - avcodec/options_table: remove extradata_size from the AVOptions table
    - ffmdec: limit the backward seek to the last resync position
    - Add dependencies to configure file for vf_fftfilt
    - ffmdec: make sure the time base is valid
    - ffmdec: fix infinite loop at EOF
    - ffmdec: initialize f_cprv, f_stvi and f_stau
    - arm: Suppress tags about used cpu arch and extensions
    - mxfdec: Fix the error handling for when strftime fails
    - avcodec/opusdec: Fix delayed sample value
    - avcodec/opusdec: Clear out pointers per packet
    - avcodec/utils: Align YUV411 by as much as the other YUV variants
    - lavc/hevcdsp: Fix compilation for arm with --disable-neon.
    - vp9: fix segmentation map retention with threading enabled.
    - Revert "avutil/opencl: is_compiled flag not being cleared in av_opencl_uninit"

--- multimedia/ffmpeg2/patches/patch-libavutil_opencl.c
+++ multimedia/ffmpeg2/patches/patch-libavutil_opencl.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-libavutil_opencl.c,v 1.1 2015/03/08 08:10:13 adam Exp $
+$NetBSD: patch-libavutil_opencl.c,v 1.2 2015/04/02 22:40:04 wiz Exp $
 
 Fix undeclared identifier error.
 
---- libavutil/opencl.c.orig	2015-03-08 07:52:00.000000000 +0000
+--- libavutil/opencl.c.orig	2015-03-16 19:26:04.000000000 +0000
 +++ libavutil/opencl.c
-@@ -611,7 +611,7 @@ void av_opencl_uninit(void)
-         }
-         opencl_ctx.context = NULL;
-     }
+@@ -423,7 +423,7 @@ cl_program av_opencl_compile(const char 
+     cl_program program = NULL;
+ 
+     LOCK_OPENCL;
 -    for (i = 0; i < opencl_ctx.kernel_code_count; i++) {
 +    for (int i = 0; i < opencl_ctx.kernel_code_count; i++) {
-         opencl_ctx.kernel_code[i].is_compiled = 0;
-     }
-     free_device_list(&opencl_ctx.device_list);
+         // identify a program using a unique name within the kernel source
+         ptr = av_stristr(opencl_ctx.kernel_code[i].kernel_string, program_name);
+         if (ptr && !opencl_ctx.kernel_code[i].is_compiled) {
