8cb9b08f5931ec73651803a07729fb7e7d1fa37f
rodent <rodent@pkgsrc.org>
2015-04-03 23:35:49 +0000

    Import openocd-0.8.0 as devel/openocr.
    Free and open on-chip debugging, in-system programming and boundary-scan
    testing.

---
+++ devel/openocr/options.mk
@@ -0,0 +1,25 @@
+# $NetBSD: options.mk,v 1.1 2015/04/03 23:35:49 rodent Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.openocr
+PKG_SUPPORTED_OPTIONS=	libhidapi libusb libusb1
+PKG_SUGGESTED_OPTIONS+=	${PKG_SUPPORTED_OPTIONS}
+PLIST_VARS+=		libusb1
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mlibhidapi)
+.include "../../comms/libhidapi/buildlink3.mk"
+HIDAPI_CFLAGS=		-I${BUILDLINK_PREFIX.libhidapi}/include/hidapi
+HIDAPI_LIBS=		-L/usr/pkgsrc/pkg/lib -Wl,-R/usr/pkgsrc/pkg/lib -lhidapi
+CONFIGURE_ENV+=		HIDAPI_CFLAGS=${HIDAPI_CFLAGS:Q}
+CONFIGURE_ENV+=		HIDAPI_LIBS=${HIDAPI_LIBS:Q}
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibusb)
+.include "../../devel/libusb/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibusb1)
+.include "../../devel/libusb1/buildlink3.mk"
+PLIST.libusb1=		yes
+.endif
