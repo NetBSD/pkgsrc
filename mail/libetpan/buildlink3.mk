3ef67cc1199a6a2f8d324fc5b3e3d3bb77cc655b
nros <nros@pkgsrc.org>
2015-04-03 12:04:30 +0000

    Update libetpan to version 1.6. Update homepage.
    changelog:
    1.6:
        Improved RFC822 and MIME parser
        Implemented SEARCH using LITERAL+ for Gmail
        Fixed SSL connections (don't force SSLv3)
    1.5:
        Gmail search features: X-GM-MSGID, X-GM-THRID, X-GM-RAW
        Added POP stat
        Various fixes for Windows
        Fixed OpenSSL / GnuTLS implementation of retrieval of certificates
        Fixed IMAP IDLE in case of COMPRESS-ed streams
        Fixed base64 encoding
    1.4:
        Gmail search features: X-GM-MSGID, X-GM-THRID, X-GM-RAW
        Added POP stat
        Various fixes for Windows
        Fixed OpenSSL / GnuTLS implementation of retrieval of certificates
        Fixed IMAP IDLE in case of COMPRESS-ed streams
        Fixed base64 encoding

--- mail/libetpan/buildlink3.mk
+++ mail/libetpan/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.27 2014/02/12 23:18:08 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2015/04/03 12:04:30 nros Exp $
 
 BUILDLINK_TREE+=	libetpan
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libetpan
 LIBETPAN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libetpan+=	libetpan>=1.0
-BUILDLINK_ABI_DEPENDS.libetpan+=	libetpan>=1.1nb3
+BUILDLINK_ABI_DEPENDS.libetpan+=	libetpan>=1.6
 BUILDLINK_PKGSRCDIR.libetpan?=	../../mail/libetpan
 
 BDB_ACCEPTED?=	db4 db5
