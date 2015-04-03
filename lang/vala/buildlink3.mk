e613213ea41b76cc2d6f18faf4e3ae41c8b8f889
wiz <wiz>
2015-04-03 07:38:17 +0000

    Update to 0.28.0:
    Vala 0.28.0
    ===========
    
     * Binding updates.
    
    Vala 0.27.2
    ===========
    
     * Bug fixes and binding updates.
    
    Vala 0.27.1
    ===========
    
     * Print compiler messages in color.
     * Add clutter-gdk-1.0 bindings.
     * Add clutter-gst-3.0 bindings.
     * Add clutter-x11-1.0 bindings.
     * Add rest-extras-0.7 bindings.
     * Bug fix and binding updates.

--- lang/vala/buildlink3.mk
+++ lang/vala/buildlink3.mk
@@ -1,16 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.11 2014/11/01 10:25:20 prlw1 Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2015/04/03 07:38:17 wiz Exp $
 
 BUILDLINK_TREE+=	vala
 
 .if !defined(VALA_BUILDLINK3_MK)
 VALA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vala+=	vala>=0.26
-BUILDLINK_ABI_DEPENDS.vala+=	vala>=0.26
+BUILDLINK_API_DEPENDS.vala+=	vala>=0.28
 BUILDLINK_PKGSRCDIR.vala?=	../../lang/vala
 
-VALAC=${LOCALBASE}/bin/valac-0.26
-VAPIGEN=${LOCALBASE}/bin/vapigen-0.26
+VALAC=${LOCALBASE}/bin/valac-0.28
+VAPIGEN=${LOCALBASE}/bin/vapigen-0.28
 .if defined(GNU_CONFIGURE)
 CONFIGURE_ENV+=	VALAC=${VALAC} VAPIGEN=${VAPIGEN}
 .endif
