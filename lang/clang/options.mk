d054e51544cef58b8adffacf82bd041343797115
tnn <tnn@pkgsrc.org>
2015-04-03 11:06:57 +0000

    Remove shlib option. lang/libLLVM provides equivalent functionality.

--- lang/clang/options.mk
+++ lang/clang/options.mk
@@ -1,27 +1,12 @@
-# $NetBSD: options.mk,v 1.2 2015/03/03 16:27:33 tnn Exp $
+# $NetBSD: options.mk,v 1.3 2015/04/03 11:06:57 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.clang
-PKG_SUPPORTED_OPTIONS=	clang-static-analyzer shlib
+PKG_SUPPORTED_OPTIONS=	clang-static-analyzer
 PKG_SUGGESTED_OPTIONS=	# clang-static-analyzer disabled since it needs python and perl
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_SRC=		${PLIST_SRC_DFLT}
-
-# XXX should be spun off to a libLLVM package when we have a use case for it
-.if !empty(PKG_OPTIONS:Mshlib)
-PLIST_SRC+=		PLIST.shlib
-CONFIGURE_ARGS+=	--enable-shared
-MAKE_ENV+=		SHARED_LIBRARY=1
-MAKE_ENV+=		LD_LIBRARY_PATH=${WRKSRC}/Release/lib
-# fixup shared library dependencies in a less intrusive way than patching
-BUILDLINK_TRANSFORM+=	l:clangTooling:clangToolingCore:clangASTMatchers:clangTooling
-BUILDLINK_TRANSFORM+=	l:LLVMMipsDesc:LLVMMipsAsmPrinter:LLVMMipsDesc
-BUILDLINK_TRANSFORM+=	l:LLVMARMAsmParser:LLVMARMInfo:LLVMARMAsmParser
-BUILDLINK_TRANSFORM+=	l:clangARCMigrate:clangStaticAnalyzerCore:clangStaticAnalyzerCheckers:clangARCMigrate
-BUILDLINK_TRANSFORM+=	l:clangStaticAnalyzerCheckers:clangStaticAnalyzerCore:clangStaticAnalyzerCheckers
-BUILDLINK_TRANSFORM+=	l:clangFrontendTool:clangStaticAnalyzerFrontend:clangFrontendTool
-.endif
 
 .if !empty(PKG_OPTIONS:Mclang-static-analyzer)
 PLIST_SRC+=		PLIST.static-analyzer
