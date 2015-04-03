4d7b635e48511ab8b77e0c673df61d1e1b89b486
rodent <rodent@pkgsrc.org>
2015-04-03 17:50:27 +0000

    Updated to 2.0.3. The NEWS is almost 3000 lines and interested parties may find it here: https://github.com/nagios-plugins/nagios-plugins/blob/master/NEWS For pkgsrc, the changes aren't very interesting. Upstream applied a lot of the patches we had, notably integrating IDE S.M.A.R.T. plugin support for NetBSD.

--- net/nagios-plugins/patches/patch-plugins__check_swap.c
+++ net/nagios-plugins/patches/patch-plugins__check_swap.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-plugins__check_swap.c,v 1.1 2012/07/25 12:01:55 fhajny Exp $
+$NetBSD: patch-plugins__check_swap.c,v 1.2 2015/04/03 17:50:27 rodent Exp $
 
 Disable large file support when building 32bit on SunOS.
---- plugins/check_swap.c.orig	2010-07-27 20:47:16.000000000 +0000
+--- plugins/check_swap.c.orig	2014-04-15 22:19:03.000000000 +0000
 +++ plugins/check_swap.c
-@@ -35,6 +35,10 @@ const char *email = "nagiosplug-devel@li
+@@ -35,6 +35,10 @@ const char *email = "devel@nagios-plugin
  #include "popen.h"
  #include "utils.h"
  
