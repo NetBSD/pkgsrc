cec6a459e9e61d1576c9958316d342739628ca2a
tnn <tnn@pkgsrc.org>
2015-04-03 09:55:57 +0000

    Ensure the xf86-* driver packages have their server ABI dependencies correctly expressed depending on which server is in use.

--- x11/modular-xorg-server/buildlink3.mk
+++ x11/modular-xorg-server/buildlink3.mk
@@ -1,13 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.12 2015/03/12 15:49:20 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2015/04/03 09:55:57 tnn Exp $
 
 BUILDLINK_TREE+=	modular-xorg-server
 
 .if !defined(MODULAR_XORG_SERVER_BUILDLINK3_MK)
 MODULAR_XORG_SERVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.modular-xorg-server+=	modular-xorg-server>=1.12.0
-BUILDLINK_ABI_DEPENDS.modular-xorg-server+=	modular-xorg-server>=1.12.4nb10
+.include "../../mk/bsd.fast.prefs.mk"
+
+# default to yes until the dust settles
+USE_OLD_MODULAR_XORG_SERVER?=	yes
+
+.if !empty(USE_OLD_MODULAR_XORG_SERVER:M[yY][eE][sS])
+BUILDLINK_API_DEPENDS.modular-xorg-server+=	modular-xorg-server>=1.12.4nb12<1.17
+BUILDLINK_PKGSRCDIR.modular-xorg-server?=	../../x11/modular-xorg-server112
+.else
+BUILDLINK_API_DEPENDS.modular-xorg-server+=	modular-xorg-server>=1.17
 BUILDLINK_PKGSRCDIR.modular-xorg-server?=	../../x11/modular-xorg-server
+.endif
 
 .include "../../sysutils/libpciaccess/buildlink3.mk"
 .include "../../x11/pixman/buildlink3.mk"
@@ -15,6 +24,12 @@ BUILDLINK_PKGSRCDIR.modular-xorg-server?=	../../x11/modular-xorg-server
 .include "../../x11/compositeproto/buildlink3.mk"
 .include "../../x11/dri2proto/buildlink3.mk"
 .include "../../x11/fontsproto/buildlink3.mk"
+.if empty(USE_OLD_MODULAR_XORG_SERVER:M[yY][eE][sS])
+.include "../../graphics/MesaLib/buildlink3.mk" # XXX should not be needed for non-dri build
+.include "../../x11/resourceproto/buildlink3.mk"
+.include "../../x11/scrnsaverproto/buildlink3.mk"
+.include "../../x11/presentproto/buildlink3.mk"
+.endif
 .include "../../x11/inputproto/buildlink3.mk"
 .include "../../x11/randrproto/buildlink3.mk"
 .include "../../x11/renderproto/buildlink3.mk"
