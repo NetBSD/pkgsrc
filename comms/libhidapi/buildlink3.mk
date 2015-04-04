47d353dbf7153945e1c1131ea2dbcad799965e36
rodent <rodent@pkgsrc.org>
2015-04-04 13:18:54 +0000

    Include devel/libusb1/buildlink3.mk here.

--- comms/libhidapi/buildlink3.mk
+++ comms/libhidapi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/04/03 22:29:12 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/04/04 13:18:54 rodent Exp $
 
 BUILDLINK_TREE+=	libhidapi
 
@@ -7,6 +7,7 @@ LIBHIDAPI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libhidapi+=	libhidapi>=0.7.0
 BUILDLINK_PKGSRCDIR.libhidapi?=	../../comms/libhidapi
+.include "../../devel/libusb1/buildlink3.mk"
 .endif
 
 BUILDLINK_TREE+=	-libhidapi
