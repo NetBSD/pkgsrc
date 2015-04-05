826bc336fb380915a9faf67c7acd2fc0370c654b
dsainty <dsainty@pkgsrc.org>
2015-04-05 08:51:08 +0000

    Update ZoneMinder from 1.25.0 to 1.28.1.
    Numerous changes, documented at:
     https://github.com/ZoneMinder/ZoneMinder/releases
    
    Addresses two security advisories:
    
      https://github.com/ZoneMinder/ZoneMinder/releases/tag/v1.28.0
      http://secunia.com/advisories/62918/
    
    Pkgsrc changes:
    
    patch-src_zm_signal_h is no longer necessary because zm_signal.h uses
    HAVE_EXECINFO_H.
    
    patch-src_zmf_cpp appears to be applied upstream.
    
    patch-configure_ac no longer needs to set PATH_BUILD to
    PREFIX/share/zoneminder, so that zmupdate.pl can locate the database build
    scripts as installed files.  Upstream has now implemented this via the
    ZM_PATH_DATA entry in zm.conf, and adds a ZM_PATH_DATA/db subdirectory.
    
    src/Makefile.am no longer setuid's zmfix, as zmfix was removed from
    ZoneMinder 1.26.6.
    
    The code now uses clock_gettime(), which on some systems (like Linux), calls
    for -lrt.  Since the build system isn't aware of this, but Pkgsrc is, just set
    PTHREAD_AUTO_VARS=yes.
    
    The PHP code now uses PDO for DB access, but it looks like there are some
    straggling dependencies on the raw MySQL driver, so both are pulled in.

--- security/zoneminder/patches/patch-src_zm__thread.h
+++ security/zoneminder/patches/patch-src_zm__thread.h
@@ -1,21 +1,13 @@
-$NetBSD: patch-src_zm__thread.h,v 1.2 2013/04/29 21:31:13 joerg Exp $
+$NetBSD: patch-src_zm__thread.h,v 1.3 2015/04/05 08:51:08 dsainty Exp $
 
---- src/zm_thread.h.orig	2009-06-08 09:20:17.000000000 +0000
-+++ src/zm_thread.h
-@@ -20,6 +20,7 @@
- #ifndef ZM_THREAD_H
- #define ZM_THREAD_H
- 
-+#include <unistd.h>
- #include <pthread.h>
- #include "zm_exception.h"
- #include "zm_utils.h"
-@@ -27,7 +28,7 @@
+--- src/zm_thread.h.orig	2015-02-05 15:52:37.000000000 +1300
++++ src/zm_thread.h	2015-04-01 17:04:46.728233504 +1300
+@@ -32,7 +32,7 @@
  class ThreadException : public Exception
  {
  public:
--    ThreadException( const std::string &message ) : Exception( stringtf( "(%d) "+message, (long int)syscall(224) ) )
-+    ThreadException( const std::string &message ) : Exception( stringtf( ("(%d) "+message).c_str(), (long int)syscall(224) ) )
+-    ThreadException( const std::string &message ) : Exception( stringtf( "(%d) "+message, (long int)syscall(SYS_gettid) ) )
++    ThreadException( const std::string &message ) : Exception( stringtf( ("(%d) "+message).c_str(), (long int)syscall(SYS_gettid) ) )
      {
      }
  };
