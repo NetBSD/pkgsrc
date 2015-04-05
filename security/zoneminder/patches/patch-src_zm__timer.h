0b94cfe69e9dd553628c5ca6b12e49f89d123ff3
dsainty <dsainty>
2015-04-05 08:51:08 +0000

    Update ZoneMinder from 1.25.0 to 1.28.1.
    Numerous changes, documented at:
     https://github.com/ZoneMinder/ZoneMinder/releases
    
    Addresses two security advisories:
    
      https://github.com/ZoneMinder/ZoneMinder/releases/tag/v1.28.0
      http://secunia.com/advisories/62918/
    
    Pkgsrc changes:
    
    patch-src_zm_signal_h is no longer necessary because zm_signal.h uses
    HAVE_EXECINFO_H.
    
    patch-src_zmf_cpp appears to be applied upstream.
    
    patch-configure_ac no longer needs to set PATH_BUILD to
    PREFIX/share/zoneminder, so that zmupdate.pl can locate the database build
    scripts as installed files.  Upstream has now implemented this via the
    ZM_PATH_DATA entry in zm.conf, and adds a ZM_PATH_DATA/db subdirectory.
    
    src/Makefile.am no longer setuid's zmfix, as zmfix was removed from
    ZoneMinder 1.26.6.
    
    The code now uses clock_gettime(), which on some systems (like Linux), calls
    for -lrt.  Since the build system isn't aware of this, but Pkgsrc is, just set
    PTHREAD_AUTO_VARS=yes.
    
    The PHP code now uses PDO for DB access, but it looks like there are some
    straggling dependencies on the raw MySQL driver, so both are pulled in.

--- security/zoneminder/patches/patch-src_zm__timer.h
+++ security/zoneminder/patches/patch-src_zm__timer.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_zm__timer.h,v 1.1 2013/03/24 16:47:47 joerg Exp $
+$NetBSD: patch-src_zm__timer.h,v 1.2 2015/04/05 08:51:08 dsainty Exp $
 
 --- src/zm_timer.h.orig	2008-07-25 09:33:24.000000000 +0000
 +++ src/zm_timer.h
@@ -6,8 +6,8 @@ $NetBSD: patch-src_zm__timer.h,v 1.1 2013/03/24 16:47:47 joerg Exp $
      class TimerException : public Exception
      {
      public:
--        TimerException( const std::string &message ) : Exception( stringtf( "(%d) "+message, (long int)syscall(224) ) )
-+        TimerException( const std::string &message ) : Exception( stringtf( ("(%d) "+message).c_str(), (long int)syscall(224) ) )
+-        TimerException( const std::string &message ) : Exception( stringtf( "(%d) "+message, (long int)syscall(SYS_gettid) ) )
++        TimerException( const std::string &message ) : Exception( stringtf( ("(%d) "+message).c_str(), (long int)syscall(SYS_gettid) ) )
          {
          }
      };
