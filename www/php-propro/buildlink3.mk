7fc92ed10696af8a2c146f2a5f3846da70021cea
rodent <rodent@pkgsrc.org>
2015-04-07 23:59:40 +0000

    s/devel/www

--- www/php-propro/buildlink3.mk
+++ www/php-propro/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/04/07 23:56:07 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/04/07 23:59:40 rodent Exp $
 
 .include "../../lang/php/phpversion.mk"
 
@@ -8,7 +8,7 @@ BUILDLINK_TREE+=	${PHP_PKG_PREFIX}-propro
 ${PHP_PKG_PREFIX}_PROPRO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.${PHP_PKG_PREFIX}-propro+=	${PHP_PKG_PREFIX}-propro>=1.0.0
-BUILDLINK_PKGSRCDIR.${PHP_PKG_PREFIX}-propro?=	../../devel/php-propro
+BUILDLINK_PKGSRCDIR.${PHP_PKG_PREFIX}-propro?=	../../www/php-propro
 .endif	# ${PHP_PKG_PREFIX}_PROPRO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-${PHP_PKG_PREFIX}-propro
