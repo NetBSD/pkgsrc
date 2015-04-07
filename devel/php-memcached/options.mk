db4b4b1eaea4b74e34304e6376a8a39a33984f01
rodent <rodent@pkgsrc.org>
2015-04-07 23:09:39 +0000

    Add igbinary option, disabled by default. This option enables binary protocol communication with memcached. Bump PKGREVISION.

--- devel/php-memcached/options.mk
+++ devel/php-memcached/options.mk
@@ -1,10 +1,18 @@
-# $NetBSD: options.mk,v 1.1 2015/03/26 03:32:33 rodent Exp $
+# $NetBSD: options.mk,v 1.2 2015/04/07 23:09:39 rodent Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.php-memcached
-PKG_SUPPORTED_OPTIONS=	sasl
+PKG_SUPPORTED_OPTIONS=	igbinary sasl
 PKG_SUGGESTED_OPTIONS=	sasl
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Migbinary)
+.include "../../lang/php/phpversion.mk" 
+DEPENDS+=		${PHP_PKG_PREFIX}-igbinary-[0-9]*:../../devel/php-igbinary
+CONFIGURE_ARGS+=	--enable-memcached-igbinary
+.else
+CONFIGURE_ARGS+=	--disable-memcached-igbinary
+.endif
 
 .if !empty(PKG_OPTIONS:Msasl)
 .include "../../security/cyrus-sasl/buildlink3.mk"
