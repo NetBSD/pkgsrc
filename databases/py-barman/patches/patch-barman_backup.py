7361a41e7f213d2425f4fd44e5d37ff779eb0375
fhajny <fhajny@pkgsrc.org>
2015-04-07 09:42:55 +0000

    Update py-barman to 1.4.0.
    Version 1.4.0 - 26 Jan 2015
    
      * Incremental base backup implementation through the reuse_backup
        global/server option. Possible values are off (disabled,
        default), copy (preventing unmodified files from being
        transferred) and link (allowing for deduplication through hard
        links).
      * Store and show deduplication effects when using reuse_backup=
        link.
      * Added transparent support of pg_stat_archiver (PostgreSQL 9.4) in
        check, show-server and status commands.
      * Improved administration by invoking WAL maintenance at the end of
        a successful backup.
      * Changed the way unused WAL files are trashed, by differentiating
        between concurrent and exclusive backup cases.
      * Improved performance of WAL statistics calculation.
      * Treat a missing pg_ident.conf as a WARNING rather than an error.
      * Refactored output layer by removing remaining yield calls.
      * Check that rsync is in the system path.
      * Include history files in WAL management.
      * Improved robustness through more unit tests.
      * Fixed bug #55: Ignore fsync EINVAL errors on directories.
      * Fixed bug #58: retention policies delete.

--- databases/py-barman/patches/patch-barman_backup.py
+++ databases/py-barman/patches/patch-barman_backup.py
@@ -1,23 +1,23 @@
-$NetBSD: patch-barman_backup.py,v 1.1 2014/11/04 12:56:14 fhajny Exp $
+$NetBSD: patch-barman_backup.py,v 1.2 2015/04/07 09:42:55 fhajny Exp $
 
 Make portable for non-glibc platforms too.
---- barman/backup.py.orig	2014-08-14 16:10:43.000000000 +0000
+--- barman/backup.py.orig	2015-01-23 13:23:54.000000000 +0000
 +++ barman/backup.py
-@@ -956,7 +956,7 @@ class BackupManager(object):
-         backup_size = 0
+@@ -1043,7 +1043,7 @@ class BackupManager(object):
+         deduplicated_size = 0
          for dirpath, _, filenames in os.walk(backup_dest):
              # execute fsync() on the containing directory
 -            dir_fd = os.open(dirpath, os.O_DIRECTORY)
 +            dir_fd = os.open(dirpath, os.O_RDONLY)
-             os.fsync(dir_fd)
-             os.close(dir_fd)
-             # execute fsync() on all the contained files
-@@ -1166,7 +1166,7 @@ class BackupManager(object):
-             shutil.move(filename, destfile)
+             try:
+                 os.fsync(dir_fd)
+             except OSError, e:
+@@ -1302,7 +1302,7 @@ class BackupManager(object):
+             shutil.move(srcfile, destfile)
  
          # execute fsync() on the archived WAL containing directory
--        dir_fd = os.open(os.path.dirname(destfile), os.O_DIRECTORY)
-+        dir_fd = os.open(os.path.dirname(destfile), os.O_RDONLY)
-         os.fsync(dir_fd)
-         os.close(dir_fd)
-         # execute fsync() on the archived WAL file
+-        dir_fd = os.open(destdir, os.O_DIRECTORY)
++        dir_fd = os.open(destdir, os.O_RDONLY)
+         try:
+             os.fsync(dir_fd)
+         except OSError, e:
