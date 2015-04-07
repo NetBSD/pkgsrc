f386880db1e79b92dfc3b923ad2cf319526f1e56
fhajny <fhajny@pkgsrc.org>
2015-04-07 08:57:32 +0000

    Update net/rabbitmq-c to 0.6.0.
    Changes since v0.5.2:
    - Tools: Enable support for SSL in tools.
    - Lib: ABI CHANGE: enable support for auto_delete, internal flags
      to amqp_exchange_declare
    - Lib: check for double-close in SSL/TCP socket impl
    - Lib: allocate struct when method has no field.
    - Lib: add support for SANs in OpenSSL socket impl.
    - Lib: add functions to get negotiated frame_max and heartbeat parms.

--- net/rabbitmq-c/buildlink3.mk
+++ net/rabbitmq-c/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/02/13 09:46:27 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/04/07 08:57:32 fhajny Exp $
 
 BUILDLINK_TREE+=	rabbitmq-c
 
 .if !defined(RABBITMQ_C_BUILDLINK3_MK)
 RABBITMQ_C_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rabbitmq-c+=	rabbitmq-c>=0.5.0
-BUILDLINK_ABI_DEPENDS.rabbitmq-c+=	rabbitmq-c>=0.5.0
+BUILDLINK_API_DEPENDS.rabbitmq-c+=	rabbitmq-c>=0.6.0
+BUILDLINK_ABI_DEPENDS.rabbitmq-c+=	rabbitmq-c>=0.6.0
 BUILDLINK_PKGSRCDIR.rabbitmq-c?=	../../net/rabbitmq-c
 
 .include "../../devel/popt/buildlink3.mk"
