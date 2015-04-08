601eab37e27e70c99fed634e3f87fbd264eedc7d
nros <nros@pkgsrc.org>
2015-04-08 10:31:28 +0000

    Fix build on FreeBSD 10 and maybe other BSD:s.

--- x11/qt5-qtbase/patches/patch-src_corelib_io_qstorageinfo_unix.cpp
+++ x11/qt5-qtbase/patches/patch-src_corelib_io_qstorageinfo_unix.cpp
@@ -1,8 +1,18 @@
-$NetBSD: patch-src_corelib_io_qstorageinfo_unix.cpp,v 1.2 2015/02/14 22:12:34 nros Exp $
+$NetBSD: patch-src_corelib_io_qstorageinfo_unix.cpp,v 1.3 2015/04/08 10:31:28 nros Exp $
+* make statvfs available on non-NetBSD BSD platforms
+* NetBSD uses struct statvfs as first argument to getmntinfo
 * fix build on SunOS
 --- src/corelib/io/qstorageinfo_unix.cpp.orig	2014-12-05 16:24:37.000000000 +0000
 +++ src/corelib/io/qstorageinfo_unix.cpp
-@@ -63,6 +63,8 @@
+@@ -52,6 +52,7 @@
+ 
+ #if defined(Q_OS_BSD4)
+ #  include <sys/mount.h>
++#  include <sys/statvfs.h>
+ #elif defined(Q_OS_ANDROID)
+ #  include <sys/mount.h>
+ #  include <sys/vfs.h>
+@@ -63,6 +64,8 @@
  #  include <sys/statvfs.h>
  #elif defined(Q_OS_SOLARIS)
  #  include <sys/mnttab.h>
@@ -11,7 +21,7 @@ $NetBSD: patch-src_corelib_io_qstorageinfo_unix.cpp,v 1.2 2015/02/14 22:12:34 nr
  #endif
  
  #if defined(Q_OS_BSD4)
-@@ -74,6 +76,9 @@
+@@ -74,6 +77,9 @@
  #  if !defined(ST_RDONLY)
  #    define ST_RDONLY 1 // hack for missing define on Android
  #  endif
@@ -21,16 +31,20 @@ $NetBSD: patch-src_corelib_io_qstorageinfo_unix.cpp,v 1.2 2015/02/14 22:12:34 nr
  #else
  #  if defined(QT_LARGEFILE_SUPPORT)
  #    define QT_STATFSBUF struct statvfs64
-@@ -118,7 +123,7 @@ public:
+@@ -118,7 +124,11 @@ public:
      inline QByteArray device() const;
  private:
  #if defined(Q_OS_BSD4)
 -    statfs *stat_buf;
++#if defined(Q_OS_NETBSD)
 +    QT_STATFSBUF *stat_buf;
++#else
++    struct statfs *stat_buf;
++#endif
      int entryCount;
      int currentIndex;
  #elif defined(Q_OS_SOLARIS)
-@@ -196,22 +201,22 @@ inline bool QStorageIterator::isValid()
+@@ -196,22 +206,22 @@ inline bool QStorageIterator::isValid() 
  
  inline bool QStorageIterator::next()
  {
