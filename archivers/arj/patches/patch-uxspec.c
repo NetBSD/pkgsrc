7e561118b1a2394de330caab51ce1027bd87677a
tnn <tnn@pkgsrc.org>
2015-04-12 15:56:08 +0000

    defuzz

--- archivers/arj/patches/patch-uxspec.c
+++ archivers/arj/patches/patch-uxspec.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-uxspec.c,v 1.2 2015/04/12 15:54:02 tnn Exp $
+$NetBSD: patch-uxspec.c,v 1.3 2015/04/12 15:56:08 tnn Exp $
 
 Fix build on systems without lchown.
 Fix CVE-2015-0556. Via Debian security-traversal-symlink.patch.
 
---- uxspec.c.orig	2015-04-12 15:46:11.000000000 +0000
+--- uxspec.c.orig	2004-04-17 11:39:42.000000000 +0000
 +++ uxspec.c
-@@ -125,6 +125,58 @@ int query_uxspecial(char FAR **dest, cha
+@@ -120,6 +120,58 @@ int query_uxspecial(char FAR **dest, cha
  }
  #endif
  
@@ -64,7 +64,7 @@ Fix CVE-2015-0556. Via Debian security-traversal-symlink.patch.
  /* Restores the UNIX special file data */
  
  int set_uxspecial(char FAR *storage, char *name)
-@@ -161,6 +213,8 @@ int set_uxspecial(char FAR *storage, cha
+@@ -156,6 +208,8 @@ int set_uxspecial(char FAR *storage, cha
       l=sizeof(tmp_name)-1;
      far_memmove((char FAR *)tmp_name, dptr, l);
      tmp_name[l]='\0';
