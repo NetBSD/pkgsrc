1f4f1201befbc35b94afd77dc2cd65875ca45cc0
fhajny <fhajny>
2015-04-13 11:33:35 +0000

    Update redis to 3.0.0.
    --[ Redis 3.0.0 ] Release date: 1 Apr 2015
    
    >> What's new in Redis 3.0 compared to Redis 2.8?
    
    * Redis Cluster: a distributed implementation of a subset of Redis.
    * New "embedded string" object encoding resulting in less cache
      misses. Big speed gain under certain work loads.
    * AOF child -> parent final data transmission to minimize latency due
      to "last write" during AOF rewrites.
    * Much improved LRU approximation algorithm for keys eviction.
    * WAIT command to block waiting for a write to be transmitted to
      the specified number of slaves.
    * MIGRATE connection caching. Much faster keys migraitons.
    * MIGARTE new options COPY and REPLACE.
    * CLIENT PAUSE command: stop processing client requests for a
      specified amount of time.
    * BITCOUNT performance improvements.
    * CONFIG SET accepts memory values in different units (for example
      you can use "CONFIG SET maxmemory 1gb").
    * Redis log format slightly changed reporting in each line the role
      of the instance (master/slave) or if it's a saving child log.
    * INCR performance improvements.
    
    >> Refactoring changes (no new features nor bug fixes)
    
    * Blocking operations full refactoring (blocked.c)
    * Client output buffer memory tracking refactored.
    
    See full release notes for 3.0.0:
    
    https://raw.githubusercontent.com/antirez/redis/3.0/00-RELEASENOTES

--- databases/redis/patches/patch-src_object.c
+++ databases/redis/patches/patch-src_object.c
@@ -1,17 +1,15 @@
-$NetBSD: patch-src_object.c,v 1.2 2014/01/02 17:26:19 fhajny Exp $
+$NetBSD: patch-src_object.c,v 1.3 2015/04/13 11:33:35 fhajny Exp $
 
 Add MirBSD support.
---- src/object.c.orig	Wed Aug 28 09:36:00 2013
+
+--- src/object.c.orig	2015-04-01 14:01:44.000000000 +0000
 +++ src/object.c
-@@ -450,7 +450,11 @@ int getLongDoubleFromObject(robj *o, lon
-         redisAssertWithInfo(NULL,o,o->type == REDIS_STRING);
-         if (o->encoding == REDIS_ENCODING_RAW) {
-             errno = 0;
-+#ifdef __MirBSD__
-+            value = (long double)strtod(o->ptr, &eptr);
-+#else
-             value = strtold(o->ptr, &eptr);
-+#endif
-             if (isspace(((char*)o->ptr)[0]) || eptr[0] != '\0' ||
-                 errno == ERANGE || isnan(value))
-                 return REDIS_ERR;
+@@ -32,7 +32,7 @@
+ #include <math.h>
+ #include <ctype.h>
+ 
+-#ifdef __CYGWIN__
++#if defined(__CYGWIN__) || defined(__MirBSD__)
+ #define strtold(a,b) ((long double)strtod((a),(b)))
+ #endif
+ 
