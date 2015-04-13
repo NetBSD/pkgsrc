91678d9a38e0b58f854ee16c9416ca59bf3e5620
rodent <rodent>
2015-04-13 23:12:40 +0000

    Removing python26. EOL'd quite some ago and discussed a couple times on tech-pkg@ and pkgsrc-users@.

--- lang/python/extension.mk
+++ lang/python/extension.mk
@@ -1,4 +1,4 @@
-# $NetBSD: extension.mk,v 1.37 2014/01/24 12:42:52 obache Exp $
+# $NetBSD: extension.mk,v 1.38 2015/04/13 23:12:44 rodent Exp $
 
 .include "../../lang/python/pyversion.mk"
 
@@ -53,7 +53,6 @@ do-test:
 # appears to be that creating egg info files was new in Python 2.5.
 PY_NO_EGG?=		yes
 .if !empty(PY_NO_EGG:M[yY][eE][sS])
-# see python26/patches/patch-av
 INSTALL_ENV+=		PKGSRC_PYTHON_NO_EGG=defined
 .endif
 
