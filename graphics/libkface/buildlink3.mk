1c5e1a8452da2bb76816c67ebe6f7fa49b89b7ef
adam <adam>
2015-04-17 15:52:56 +0000

    Revbump after updating devel/boost-libs

--- graphics/libkface/buildlink3.mk
+++ graphics/libkface/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2014/11/07 19:39:25 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2015/04/17 15:52:56 adam Exp $
 
 BUILDLINK_TREE+=	libkface
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libkface
 LIBKFACE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libkface+=	libkface>=2.5.0
-BUILDLINK_ABI_DEPENDS.libkface?=	libkface>=3.5.0nb6
+BUILDLINK_ABI_DEPENDS.libkface?=	libkface>=4.4.0nb1
 BUILDLINK_PKGSRCDIR.libkface?=	../../graphics/libkface
 
 .include "../../graphics/opencv/buildlink3.mk"
