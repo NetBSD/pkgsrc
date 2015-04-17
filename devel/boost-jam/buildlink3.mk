85e840e72c4f5beb1308d338165f8e608b93fad2
adam <adam@pkgsrc.org>
2015-04-17 14:04:47 +0000

    Changes 1.58.0: * New Libraries:   - Endian: Types and conversion functions for correct byte ordering and more regardless of processor endianness.   - Sort: Includes spreadsort, a general-case hybrid radix sort that is faster than O(n*log(n)) * Updated Libraries:

--- devel/boost-jam/buildlink3.mk
+++ devel/boost-jam/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.24 2014/11/07 19:28:38 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2015/04/17 14:04:47 adam Exp $
 
 BUILDLINK_TREE+=	boost-jam
 
 .if !defined(BOOST_JAM_BUILDLINK3_MK)
 BOOST_JAM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.57.*
+BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.58.*
 BUILDLINK_DEPMETHOD.boost-jam?=		build
 BUILDLINK_PKGSRCDIR.boost-jam?=		../../devel/boost-jam
 .endif # BOOST_JAM_BUILDLINK3_MK
--- meta-pkgs/boost/patches/patch-boost_config_stdlib_libstdcpp3.hpp
+++ meta-pkgs/boost/patches/patch-boost_config_stdlib_libstdcpp3.hpp
@@ -1,18 +1,16 @@
-$NetBSD: patch-boost_config_stdlib_libstdcpp3.hpp,v 1.1 2014/03/16 04:40:25 ryoon Exp $
+$NetBSD: patch-boost_config_stdlib_libstdcpp3.hpp,v 1.2 2015/04/17 14:04:47 adam Exp $
 
 * Fix build under NetBSD with GCC 4.8.3 in base.
   GCC 4.8.3 does not define _GLIBCXX_HAVE_GTHR_DEFAULT
   in /usr/include/g++/bits/c++config.h.
 
---- boost/config/stdlib/libstdcpp3.hpp.orig	2014-03-08 14:33:38.000000000 +0000
+--- boost/config/stdlib/libstdcpp3.hpp.orig	2015-03-24 18:27:48.000000000 +0000
 +++ boost/config/stdlib/libstdcpp3.hpp
-@@ -36,7 +36,8 @@
-         || defined(_GLIBCXX__PTHREADS) \
+@@ -37,6 +37,7 @@
          || defined(_GLIBCXX_HAS_GTHREADS) \
          || defined(_WIN32) \
--        || defined(_AIX)
-+        || defined(_AIX) \
-+	|| defined(__NetBSD__)
+         || defined(_AIX) \
++        || defined(__NetBSD__) \
+         || defined(__HAIKU__)
        //
        // If the std lib has thread support turned on, then turn it on in Boost
-       // as well.  We do this because some gcc-3.4 std lib headers define _REENTANT
