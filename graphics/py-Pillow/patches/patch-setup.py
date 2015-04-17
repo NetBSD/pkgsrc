1bee7cfba54a7842021d55c800849b89b40aae16
adam <adam@pkgsrc.org>
2015-04-17 15:10:02 +0000

    Changes 2.8.1: - Bug fix: Catch struct.error on invalid JPEG

--- graphics/py-Pillow/patches/patch-setup.py
+++ graphics/py-Pillow/patches/patch-setup.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-setup.py,v 1.3 2014/07/06 12:15:53 obache Exp $
+$NetBSD: patch-setup.py,v 1.4 2015/04/17 15:10:02 adam Exp $
 
 * Prevent to detect optional tkinter.
 * Disable demo programs.
 
---- setup.py.orig	2014-06-30 22:42:26.000000000 +0000
+--- setup.py.orig	2015-04-02 09:05:22.000000000 +0000
 +++ setup.py
 @@ -82,11 +82,7 @@ def _lib_include(root):
  def _read(file):
@@ -18,7 +18,7 @@ $NetBSD: patch-setup.py,v 1.3 2014/07/06 12:15:53 obache Exp $
  
  
  NAME = 'Pillow'
-@@ -570,7 +566,7 @@ class pil_build_ext(build_ext):
+@@ -575,7 +571,7 @@ class pil_build_ext(build_ext):
              exts.append(Extension(
                  "PIL._webp", ["_webp.c"], libraries=libs, define_macros=defs))
  
@@ -27,12 +27,11 @@ $NetBSD: patch-setup.py,v 1.3 2014/07/06 12:15:53 obache Exp $
              # locate Tcl/Tk frameworks
              frameworks = []
              framework_roots = [
-@@ -746,7 +742,7 @@ setup(
+@@ -753,7 +749,6 @@ setup(
      ext_modules=[Extension("PIL._imaging", ["_imaging.c"])],
      include_package_data=True,
      packages=find_packages(),
 -    scripts=glob.glob("Scripts/pil*.py"),
-+#    scripts=glob.glob("Scripts/pil*.py"),
-     test_suite='PIL.tests',
+     test_suite='nose.collector',
      keywords=["Imaging", ],
      license='Standard PIL License',
