3516ef860c56baa4f3b27f097e9f79a0eeafac9e
adam <adam>
2015-04-17 14:04:47 +0000

    Changes 1.58.0: * New Libraries:   - Endian: Types and conversion functions for correct byte ordering and more regardless of processor endianness.   - Sort: Includes spreadsort, a general-case hybrid radix sort that is faster than O(n*log(n)) * Updated Libraries:

--- meta-pkgs/boost/patches/patch-libs_log_src_timestamp.cpp
+++ meta-pkgs/boost/patches/patch-libs_log_src_timestamp.cpp
@@ -1,15 +1,14 @@
-$NetBSD: patch-libs_log_src_timestamp.cpp,v 1.1 2014/05/13 18:48:49 ryoon Exp $
+$NetBSD: patch-libs_log_src_timestamp.cpp,v 1.2 2015/04/17 14:04:47 adam Exp $
 
 * OpenBSD 5.5 has no _POSIX_TIMES, fix build under OpenBSD 5.5
 
---- libs/log/src/timestamp.cpp.orig	2013-08-25 14:20:22.000000000 +0000
+--- libs/log/src/timestamp.cpp.orig	2015-04-04 17:33:00.000000000 +0000
 +++ libs/log/src/timestamp.cpp
-@@ -202,7 +202,7 @@ BOOST_LOG_API get_tick_count_t get_tick_
- 
+@@ -203,6 +203,7 @@ BOOST_LOG_API get_tick_count_t get_tick_
  #endif // _WIN32_WINNT >= 0x0600
  
--#elif defined(_POSIX_TIMERS) && _POSIX_TIMERS > 0
-+#elif (defined(_POSIX_TIMERS) && _POSIX_TIMERS > 0) || defined(__OpenBSD__)
+ #elif (defined(_POSIX_TIMERS) && _POSIX_TIMERS > 0)  /* POSIX timers supported */ \
++      || defined(__OpenBSD__) \
+       || defined(__GNU__)  /* GNU Hurd does not support POSIX timers fully but does provide clock_gettime() */
  
  BOOST_LOG_API int64_t duration::milliseconds() const
- {
