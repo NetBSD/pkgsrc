c0c5c5d6501dbe8e789885895c82c5b2562abb4e
jaapb <jaapb@pkgsrc.org>
2015-04-17 09:12:12 +0000

    Extended gdt's i386 ucontext signal handler patch to amd64. This shouldn't affect anything that isn't NetBSD/amd64.

--- lang/ocaml/patches/patch-asmrun_signals_osdep.h
+++ lang/ocaml/patches/patch-asmrun_signals_osdep.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-asmrun_signals_osdep.h,v 1.4 2015/03/14 15:17:15 gdt Exp $
+$NetBSD: patch-asmrun_signals_osdep.h,v 1.5 2015/04/17 09:12:12 jaapb Exp $
 
 Use correct data structures for power architecture; also do signal
 handling correctly for i386/NetBSD.
@@ -8,7 +8,31 @@ First hunk taken from upstream tracker at:
 
 --- asmrun/signals_osdep.h.orig	2014-09-28 19:46:24.000000000 +0000
 +++ asmrun/signals_osdep.h
-@@ -163,14 +163,24 @@
+@@ -146,6 +146,23 @@
+  #define CONTEXT_YOUNG_PTR (context->sc_r15)
+  #define CONTEXT_FAULTING_ADDRESS ((char *) info->si_addr)
+ 
++/****************** AMD64, NetBSD */
++
++#elif defined(TARGET_amd64) && defined (SYS_netbsd)
++
++ #include <ucontext.h>
++ #define DECLARE_SIGNAL_HANDLER(name) \
++ static void name(int sig, siginfo_t * info, ucontext_t * context)
++
++ #define SET_SIGACT(sigact,name) \
++ sigact.sa_sigaction = (void (*)(int,siginfo_t *,void *)) (name); \
++ sigact.sa_flags = SA_SIGINFO
++
++ #define CONTEXT_PC (_UC_MACHINE_PC(context))
++ #define CONTEXT_EXCEPTION_POINTER (context->uc_mcontext.gregs[REG_R14])
++ #define CONTEXT_YOUNG_PTR (context->uc_mcontext.gregs[REG_R15])
++ #define CONTEXT_FAULTING_ADDRESS ((char *) info->si_addr)
++
+ /****************** I386, Linux */
+ 
+ #elif defined(TARGET_i386) && defined(SYS_linux_elf)
+@@ -163,14 +180,24 @@
  
  #elif defined(TARGET_i386) && defined(SYS_bsd_elf)
  
@@ -36,7 +60,7 @@ First hunk taken from upstream tracker at:
   #define CONTEXT_FAULTING_ADDRESS ((char *) info->si_addr)
  
  /****************** I386, BSD */
-@@ -278,12 +294,21 @@
+@@ -278,12 +305,21 @@
       sigact.sa_handler = (void (*)(int)) (name); \
       sigact.sa_flags = 0
  
