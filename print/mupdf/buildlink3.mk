06e762a3e1b472c46c089e0d68a320239cc94ded
wiz <wiz@pkgsrc.org>
2015-04-17 07:18:21 +0000

    Update print/mupdf to 1.7, provided by Leonardo Taccari in PR 49843.
    Changes:
    MuPDF 1.7 (2015-04-16)
    
       The 1.7 release is here!
    
    MuPDF 1.7 RC1 (2015-04-08)
    
       The 1.7 release is almost here!
    
       Headline changes:
         * New 'Indie dev' licensing options - contact sales@artifex.com
           for more details.
         * New HTML layout engine, and (DRM-free) EPUB viewer.
         * Reduced memory usage - displaylists and internal PDF representation
           take much less memory, reducing total memory use on some files by
           over 60%.
    
       Important API changes:
         * Bound contexts have been removed; we now pass fz_contexts
           explicitly.
         * Reference counting of paths and text structures.
    
       Features:
         * Add mutool pages option.
         * Tweaked rendering to prevent feature dropout in common cases.
         * Viewer tweaks
              + Better mouse wheel handling
              + Shift-space support
              + Mouse button control of presentation mode
              + Failed reloads do not close document
    
       Internal changes:
         * PDF filter revamp - simpler interface.
         * Devices use derived structures rather than user pointer.
         * Sparse PDF xrefs held in more compact form.
         * New gsview viewer split out to its own repository.
    
       Bug fixes
         * Improved handling of broken files.
         * Fix BBoxes of Type 3 fonts.
         * Updated fonts (including greek and cyrillic).
         * Various memory leaks and crashes.
         * And many more.

--- print/mupdf/buildlink3.mk
+++ print/mupdf/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2014/05/30 21:39:37 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2015/04/17 07:18:21 wiz Exp $
 
 BUILDLINK_TREE+=	mupdf
 
 .if !defined(MUPDF_BUILDLINK3_MK)
 MUPDF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mupdf+=	mupdf>=1.3
-BUILDLINK_ABI_DEPENDS.mupdf?=	mupdf>=1.4nb1
+BUILDLINK_API_DEPENDS.mupdf+=	mupdf>=1.7
 BUILDLINK_PKGSRCDIR.mupdf?=	../../print/mupdf
 
 pkgbase := mupdf
