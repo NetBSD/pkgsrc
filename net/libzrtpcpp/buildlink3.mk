0f839fee4e9e7574f0a9ea2ef6c672c2010f2120
rodent <rodent@pkgsrc.org>
2015-04-18 03:36:23 +0000

    Add sqlite3 PKG_OPTION. USE_CMAKE instead of autotools. Update openssl dependency. Bump BUILDLINK_A{B,P}I* in buildlink3.mk due to API changes.
    From NEWS:
    
    == GNU ZRTP 4.4.0 ==
    
    Changes the handling of HMAC and Hash contexts to avoild tool
    many malloc/free calls and thus memory pointer problems.
    
    Enhance the handling an check the nonce when using multi-stream
    mode. This required a modification to the class file and some
    modifications on the API. The old functions are now deprecated
    but still usable. Nevertheless you should change your application
    to use the new fuctions which support the new nonce handling and
    checks.
    
    Some bug fixing as well.
    
    == GNU ZRTP 4.3.1 ==
    
    This is a bugfix release. It fixes several compiler issues in
    iOS8 Clang, Mircosoft C++ compiler (VS 2012) etc.
    
    This release also adds a fix to address a possible problem when
    using 'memset(...)' on a memory area immediately followed by a
    'free(...)' call to free this memory area. Some compilers may
    otpimize the code and do not call 'memset(...)'. That's bad for
    software that deals with secure keys :-) . The fix removes this
    possible vulnerability.
    
    
    == GNU ZRTP 4.3.0 ==
    
    This version adds some new API that provide to set retry timer
    values and to get some retry counters.
    
    Application may now set some values of the retry counters during
    the discovery (Hello) and the negotiation phase. Applications may
    increase the number of retries or modify the capping to support
    slow or bad networks.
    
    To get some idea about the actual number of retries during ZRTP
    negotiation an application may now use the new API to get an array
    of counters. The ZRTP state engine records how many retries occured
    during the different protocol states.
    
    Note: only the ZRTP initiator performs packet retries after the
    discovery (Hello) phase. The responder would always return zero
    alues for the other retry counters.
    
    Because we have a new set of functions the API changed, thus it's
    necessary to recompile applications that use the new library version.
    
    
    == GNU ZRTP 4.2.4 ==
    
    Only small changes to enable Android X86 (see clients/tivi/android)
    as an example.
    
    Rename functions aes_init() to aes_init_zrtp() to avoid names clashes
    with other libreries that may include own AES modules.
    
    
    == GNU ZRTP 4.2.3 ==
    
    The optional SAS relay feature (refer to RFC6189, chapter 7.3) is
    not longer compiled by default. If your project needs this support
    then modify the CMakeLists.txt file and uncomment a 'add_definition'
    statments. See comment in the CMakelists.txt file.
    
    The reasons to disable this optional feature in the default build:
    it's rarely used and some concerns about misusing this feature.
    
    
    == GNU ZRTP 4.2.2 ==
    
    A small enhancement in SRTP handling to provide a longer bit-shift
    register with 128 bits. The replay now check accepts packets which
    are up to 127 sequence number behing the current packet. The upper
    layer (codecs) gets more packets on slower/bad networks that we may
    see on mobile 3G/4G connections.
    
    If the codecs do not remove silence then this may lead to some longer
    audio replay, similar to sattelite communication.
    
    
    == GNU ZRTP 4.2.1 ==
    
    Bug fixes in the SRTP part that checks for replay and updates the ROC.
    
    The wrong computations lead to false replay indications and to wrong
    HMAC, thus they dropped to much packets, in particular under bad network
    conditions.
    
    Changed the handling the the zrtp_getSasType function the the ZrtpCWrapper.
    Please check the inline documentation and the compiler warning how to
    use the return value of the function.

--- net/libzrtpcpp/buildlink3.mk
+++ net/libzrtpcpp/buildlink3.mk
@@ -1,13 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.5 2014/04/07 05:34:23 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2015/04/18 03:36:23 rodent Exp $
 
 BUILDLINK_TREE+=	libzrtpcpp
 
 .if !defined(LIBZRTPCPP_BUILDLINK3_MK)
 LIBZRTPCPP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libzrtpcpp+=	libzrtpcpp>=0.9.0
-BUILDLINK_ABI_DEPENDS.libzrtpcpp?=	libzrtpcpp>=4.2.0
+BUILDLINK_API_DEPENDS.libzrtpcpp+=	libzrtpcpp>=4.4.0
+BUILDLINK_ABI_DEPENDS.libzrtpcpp?=	libzrtpcpp>=4.4.0
 BUILDLINK_PKGSRCDIR.libzrtpcpp?=	../../net/libzrtpcpp
+
+pkgbase := libzrtpcpp
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.libzrtpcpp:Msqlite3)
+.include "../../databases/sqlite3/buildlink3.mk"
+.endif
 
 .include "../../mk/pthread.buildlink3.mk"
 .include "../../security/libgcrypt/buildlink3.mk"
