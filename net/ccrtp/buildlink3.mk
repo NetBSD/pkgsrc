c3b968833d17b311d2d2943c1ab9c5d33bfa5c7a
rodent <rodent@pkgsrc.org>
2015-04-18 03:11:47 +0000

    libgcrypt and openssl are PKG_OPTIONS, with openssl enabled by default. libgcrypt doesn't get used if openssl is installed. There's no way of disabling openssl if it's found. Fix infodir using SUBST. Depends on latest version of ucommon. From ChangeLog:
    Changes from 2.1.1 to 2.1.2
    - use ucommon cmake macros
    - copyright assignment to Cherokees of Idaho
    - copyright updates and corrections to bring current
    
    Changes from 2.1.0 to 2.1.1
    - fix endianness checks
    - cleanup alloc/dealloc
    - configure: fix libtoolize warning
    - requires ucommon 6.2.2 for endian fixes
    
    Changes from 2.0.9 to 2.1.0
    - configure: add option to disable compilation of demos
    - OSX: Check for macports glibtoolize.
    - modernized cmake
    - use standard header for malloc
    - uptick of abi version for ucommon
    
    2014-04-14  David Sugar (for 2.0.9)
    
            Merged fix from Alexandre Lision for initial rtcp seq #

--- net/ccrtp/buildlink3.mk
+++ net/ccrtp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/02/12 23:18:19 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2015/04/18 03:11:47 rodent Exp $
 
 BUILDLINK_TREE+=	ccrtp
 
@@ -9,7 +9,17 @@ BUILDLINK_API_DEPENDS.ccrtp+=	ccrtp>=2.0.0
 BUILDLINK_ABI_DEPENDS.ccrtp?=	ccrtp>=2.0.6nb2
 BUILDLINK_PKGSRCDIR.ccrtp?=	../../net/ccrtp
 
+pkgbase := ccrtp
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.ccrtp:Mlibgcrypt)
 .include "../../security/libgcrypt/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ccrtp:Mopenssl)
+.include "../../security/openssl/buildlink3.mk"
+.endif
+
 .include "../../devel/ucommon/buildlink3.mk"
 .endif # CCRTP_BUILDLINK3_MK
 
--- net/ccrtp/options.mk
+++ net/ccrtp/options.mk
@@ -1,11 +1,17 @@
-# $NetBSD: options.mk,v 1.1 2013/04/17 00:54:16 rodent Exp $
+# $NetBSD: options.mk,v 1.2 2015/04/18 03:11:47 rodent Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.ccrtp
-PKG_SUPPORTED_OPTIONS=	debug
-PKG_SUGGESTED_OPTIONS+=	# blank
+PKG_OPTIONS_VAR=		PKG_OPTIONS.ccrtp
+PKG_OPTIONS_GROUP.tls=		libgcrypt openssl
+PKG_OPTIONS_REQUIRED_GROUPS=	tls
+PKG_SUGGESTED_OPTIONS+=		openssl
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mdebug)
-CONFIGURE_ARGS+=	--enable-debug
+.if !empty(PKG_OPTIONS:Mlibgcrypt)
+.include "../../security/libgcrypt/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mopenssl)
+BUILDLINK_API_DEPENDS.openssl+=	openssl>=0.9.8
+.include "../../security/openssl/buildlink3.mk"
 .endif
