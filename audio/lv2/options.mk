dedc65a2d7b57f11b72bb297917b9081068372db
rodent <rodent@pkgsrc.org>
2015-04-19 22:54:55 +0000

    Eep, move last doc option dependency to options.mk.

--- audio/lv2/options.mk
+++ audio/lv2/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2015/04/19 22:52:04 rodent Exp $
+# $NetBSD: options.mk,v 1.2 2015/04/19 22:54:55 rodent Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.lv2
 PKG_SUPPORTED_OPTIONS=	debug doc tests valgrind
@@ -17,6 +17,7 @@ BUILD_DEBUG=		# blank
 BUILD_DOC=		--docs
 BUILD_DEPENDS+=		asciidoc-[0-9]*:../../textproc/asciidoc
 BUILD_DEPENDS+=		doxygen-[0-9]*:../../devel/doxygen
+BUILD_DEPENDS+=		${PYPKGPREFIX}-rdflib-[0-9]*:../../textproc/py-rdflib
 PLIST.doc=		yes
 .else
 BUILD_DOC=		# blank
