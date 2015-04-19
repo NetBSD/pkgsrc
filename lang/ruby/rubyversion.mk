b28b71985bbf287144d497fcf07761883e93003c
taca <taca@pkgsrc.org>
2015-04-19 16:12:23 +0000

    Update ruby200 package to 2.0.0p645 (Ruby 2.0.0-p645).
    From release announce:
    
    We are pleased to announce the release of Ruby 2.0.0-p645.
    
    This release includes a security fix for OpenSSL extension. Please view the
    topic below for more details.
    
        CVE-2015-1855: Ruby OpenSSL Hostname Verification
    
    Ruby 2.0.0 is now under the state of the security maintenance phase, until
    Feb. 24th, 2016. After the date, maintenance of Ruby 2.0.0 will be ended. We
    recommend you start planning migration to newer versions of Ruby, such as 2.1
    or 2.2.
    
    This release includes the security fix mentioned above along with small
    changes required for test environment (that shouldn¡Çt affect normal users).
    
    See ChangeLog for full details.

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.138 2015/03/15 14:00:57 taca Exp $
+# $NetBSD: rubyversion.mk,v 1.139 2015/04/19 16:12:23 taca Exp $
 #
 
 # This file determines which Ruby version is used as a dependency for
@@ -235,7 +235,7 @@ RUBY22_VERSION=		2.2.1
 # patch
 RUBY18_PATCHLEVEL=	pl374
 RUBY193_PATCHLEVEL=	p551
-RUBY200_PATCHLEVEL=	p643
+RUBY200_PATCHLEVEL=	p645
 #RUBY21_PATCHLEVEL=	p273
 #RUBY22_PATCHLEVEL=	p85
 
