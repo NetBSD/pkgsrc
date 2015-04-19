43c769be77a13f78e87eaa1450b11b1837983169
jperkin <jperkin>
2015-04-19 14:30:07 +0000

    Portability fix, use err.h from libnbcompat.

--- pkgtools/cwrappers/files/bin/alloc.c
+++ pkgtools/cwrappers/files/bin/alloc.c
@@ -1,4 +1,4 @@
-/* $NetBSD: alloc.c,v 1.1 2014/09/17 12:40:56 joerg Exp $ */
+/* $NetBSD: alloc.c,v 1.2 2015/04/19 14:30:07 jperkin Exp $ */
 
 /*-
  * Copyright (c) 2007 Joerg Sonnenberger <joerg@NetBSD.org>.
@@ -31,7 +31,8 @@
  * SUCH DAMAGE.
  */
 
-#include <err.h>
+#include <nbcompat.h>
+#include <nbcompat/err.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
