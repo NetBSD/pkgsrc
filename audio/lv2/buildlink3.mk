7dd220ca86558f911a24dcdf2197fdc8cd90b2c0
rodent <rodent@pkgsrc.org>
2015-04-19 22:52:04 +0000

    Import lv2-1.12.0 as audio/lv2.
    LV2 is an open standard for audio plugins, used by hundreds of plugins and other
    projects. At its core, LV2 is a simple stable interface, accompanied by
    extensions which add functionality to support the needs of increasingly powerful
    audio software.

---
+++ audio/lv2/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/04/19 22:52:04 rodent Exp $
+
+BUILDLINK_TREE+=	lv2
+
+.if !defined(LV2_BUILDLINK3_MK)
+LV2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.lv2+=	lv2>=1.12.0
+BUILDLINK_PKGSRCDIR.lv2?=	../../audio/lv2
+
+.include "../../graphics/cairo/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../audio/libsndfile/buildlink3.mk"
+.endif	# LV2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-lv2
