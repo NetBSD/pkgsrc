e865b0f2d1486ca953d1d48fec1ee5e3f3c0394e
taca <taca@pkgsrc.org>
2015-04-19 16:25:10 +0000

    Update ruby22-base and ruby22 package to 2.2.2.
    From release announce:
    
    We are pleased to announce the release of Ruby 2.2.2. This is a TEENY version
    release of the stable 2.2 series.
    
    This release includes the security fix for a OpenSSL extension¡Çs hostname
    verification vulnerability.
    
        CVE-2015-1855: Ruby OpenSSL Hostname Verification
    
    There are also some bugfixes. See ChangeLog for details.

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.140 2015/04/19 16:19:00 taca Exp $
+# $NetBSD: rubyversion.mk,v 1.141 2015/04/19 16:25:10 taca Exp $
 #
 
 # This file determines which Ruby version is used as a dependency for
@@ -230,14 +230,14 @@ RUBY18_VERSION=		1.8.7
 RUBY193_VERSION=	1.9.3
 RUBY200_VERSION=	2.0.0
 RUBY21_VERSION=		2.1.6
-RUBY22_VERSION=		2.2.1
+RUBY22_VERSION=		2.2.2
 
 # patch
 RUBY18_PATCHLEVEL=	pl374
 RUBY193_PATCHLEVEL=	p551
 RUBY200_PATCHLEVEL=	p645
 #RUBY21_PATCHLEVEL=	p336
-#RUBY22_PATCHLEVEL=	p85
+#RUBY22_PATCHLEVEL=	p95
 
 # current API compatible version; used for version of shared library
 RUBY18_API_VERSION=	1.8.7
