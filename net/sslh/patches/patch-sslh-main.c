90c309ceceedafda8f9ed77cd51c307b60ec363e
wiz <wiz>
2015-04-19 19:02:35 +0000

    Update to 1.17, switch to github framework:
    v1.17: 	09MAR2015
    	Support RFC5952-style IPv6 addresses, e.g. [::]:443.
    
    	Transparant proxy support for FreeBSD.
    	(Ruben van Staveren)
    
    	Using -F with no argument will try
    	/etc/sslh/sslh.cfg and then /etc/sslh.cfg as
    	configuration files. (argument to -F can no longer
    	be separated from the option by a space, e.g. must
    	be -Ffoo.cfg)
    
    	Call setgroups() before setgid() (fixes potential
    	privilege escalation).
    	(Lars Vogdt)
    
    	Use portable way of getting modified time for OSX
    	support.
    	(Aaron Madlon-Kay)
    
    	Example configuration for fail2ban.
    	(Every Mouw)

--- net/sslh/patches/patch-sslh-main.c
+++ net/sslh/patches/patch-sslh-main.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-sslh-main.c,v 1.2 2014/11/28 10:45:28 bsiegert Exp $
+$NetBSD: patch-sslh-main.c,v 1.3 2015/04/19 19:02:35 wiz Exp $
 
 * getopt_long_only does not exist on NetBSD getopt.
 
---- sslh-main.c.orig	2014-02-11 21:06:01.000000000 +0000
+--- sslh-main.c.orig	2015-03-09 20:51:39.000000000 +0000
 +++ sslh-main.c
-@@ -358,7 +358,7 @@ static void cmdline_config(int argc, cha
+@@ -362,7 +362,7 @@ static void cmdline_config(int argc, cha
  #ifdef LIBCONFIG
      optind = 1;
      opterr = 0; /* we're missing protocol options at this stage so don't output errors */
@@ -12,8 +12,8 @@ $NetBSD: patch-sslh-main.c,v 1.2 2014/11/28 10:45:28 bsiegert Exp $
 +    while ((c = getopt_long(argc, argv, optstr, all_options, NULL)) != -1) {
          if (c == 'F') {
              config_filename = optarg;
-             /* find the end of the listen list */
-@@ -383,7 +383,7 @@ static void parse_cmdline(int argc, char
+             if (config_filename) {
+@@ -397,7 +397,7 @@ static void parse_cmdline(int argc, char
      optind = 1;
      opterr = 1;
  next_arg:
