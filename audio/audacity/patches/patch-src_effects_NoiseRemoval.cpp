4e6f46cae6c832a7d6040fbbe446ff8722791849
rodent <rodent@pkgsrc.org>
2015-04-20 13:38:23 +0000

    There are quite a lot of changes to this package. They are spread across eight (8) wiki pages here: http://wiki.audacityteam.org/wiki/Release_Notes. For pkgsrc changes, some patches were removed which were integrated upstream or don't exist in source anymore. PKG_OPTIONS framework was added to enable the user to built with gtk{2,3}, jack, alsa, NLS, ladspa, and debug options. gtk3, jack, and alsa options need work and aren't supported currently. This package uses a cc-by-v3.0-license which needs to be imported sometime too. Using portaudio-devel from pkgsrc is disabled, due to requiring the Pa_GetStreamHostApiType() function which is not present in our stable version. The in-tree portaudio is used instead. Nyquist support is not building as well at the moment along with the portsmf midi support. Package adds many additional dependencies like lv2, lilv, libogg, twolame, libsbsms, libsndfile, and more. Enjoy.

--- audio/audacity/patches/patch-src_effects_NoiseRemoval.cpp
+++ audio/audacity/patches/patch-src_effects_NoiseRemoval.cpp
@@ -1,6 +1,10 @@
---- src/effects/NoiseRemoval.cpp.orig	2006-11-15 05:12:10.000000000 +0100
-+++ src/effects/NoiseRemoval.cpp	2012-01-15 19:19:20.622909031 +0100
-@@ -16,6 +16,10 @@
+$NetBSD: patch-src_effects_NoiseRemoval.cpp,v 1.2 2015/04/20 13:38:23 rodent Exp $
+
+Fix build for SunOS.
+
+--- src/effects/NoiseRemoval.cpp.orig	2015-03-02 01:06:57.000000000 +0000
++++ src/effects/NoiseRemoval.cpp
+@@ -50,6 +50,10 @@
  
  #include <math.h>
  
