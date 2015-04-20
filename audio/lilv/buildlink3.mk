4f748d440e96e0662636b29c2c6d11ff03748bcf
rodent <rodent>
2015-04-20 03:21:58 +0000

    Import lilv-0.20.0 as audio/lilv.
    Lilv is a C library to make the use of LV2 plugins as simple as possible for
    applications. Lilv is the successor to SLV2, rewritten to be significantly
    faster and have minimal dependencies. It is stable, well-tested software (the
    included test suite covers over 90% of the code) in use by several applications.

---
+++ audio/lilv/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/04/20 03:21:58 rodent Exp $
+
+BUILDLINK_TREE+=	lilv
+
+.if !defined(LILV_BUILDLINK3_MK)
+LILV_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.lilv+=	lilv>=0.20.0
+BUILDLINK_PKGSRCDIR.lilv?=	../../audio/lilv
+
+.include "../../converters/sratom/buildlink3.mk"
+.include "../../textproc/sord/buildlink3.mk"
+.include "../../textproc/serd/buildlink3.mk"
+.endif	# LILV_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-lilv
