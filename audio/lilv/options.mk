c708c0258c114856ad95b790f5cd1525ff96dcab
rodent <rodent@pkgsrc.org>
2015-04-20 03:21:58 +0000

    Import lilv-0.20.0 as audio/lilv.
    Lilv is a C library to make the use of LV2 plugins as simple as possible for
    applications. Lilv is the successor to SLV2, rewritten to be significantly
    faster and have minimal dependencies. It is stable, well-tested software (the
    included test suite covers over 90% of the code) in use by several applications.

---
+++ audio/lilv/options.mk
@@ -0,0 +1,29 @@
+# $NetBSD: options.mk,v 1.1 2015/04/20 03:21:58 rodent Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.lilv
+PKG_SUPPORTED_OPTIONS=	debug doc tests
+PKG_SUGGESTED_OPTIONS+=	# blank
+PLIST_VARS+=		doc
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mdebug)
+BUILD_DEBUG=		--debug
+.else
+BUILD_DEBUG=		# blank
+.endif
+
+.if !empty(PKG_OPTIONS:Mdoc)
+BUILD_DOC=		--docs
+BUILD_DEPENDS+=		doxygen-[0-9]*:../../devel/doxygen
+BUILD_DEPENDS+=		graphviz-[0-9]*:../../graphics/graphviz
+PLIST.doc=		yes
+.else
+BUILD_DOC=		# blank
+.endif
+
+.if !empty(PKG_OPTIONS:Mtests)
+BUILD_TESTS=		--test
+.else
+BUILD_TESTS=		# blank
+.endif
