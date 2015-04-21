d058d69b35ff0e0bdb32e0d719f8af88eddd0728
joerg <joerg@pkgsrc.org>
2015-04-21 17:07:29 +0000

    Support building on ARM.

--- emulators/openmsx/patches/patch-build_detectsys.py
+++ emulators/openmsx/patches/patch-build_detectsys.py
@@ -1,9 +1,19 @@
-$NetBSD: patch-build_detectsys.py,v 1.1 2012/08/26 09:07:23 marino Exp $
+$NetBSD: patch-build_detectsys.py,v 1.2 2015/04/21 17:07:29 joerg Exp $
 
 Set os to "dragonfly" when built on "DragonFly"
+Support evbarm and similar variants.
 
 --- build/detectsys.py.orig	2012-08-12 12:29:37.000000000 +0000
 +++ build/detectsys.py
+@@ -23,7 +23,7 @@ def detectCPU():
+ 		return 'x86'
+ 	elif cpu.startswith('ppc') or cpu.endswith('ppc') or cpu.startswith('power'):
+ 		return 'ppc64' if cpu.endswith('64') else 'ppc'
+-	elif cpu.startswith('arm'):
++	elif cpu.startswith('arm') or cpu.endswith('arm'):
+ 		return 'arm'
+ 	elif cpu.startswith('mips'):
+ 		return 'mipsel' if cpu.endswith('el') else 'mips'
 @@ -58,7 +58,7 @@ def detectOS():
  	Raises ValueError if no known OS is detected.
  	'''
