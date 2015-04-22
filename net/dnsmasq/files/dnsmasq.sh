3236bed2c68f4fd3cb486496201b2d2c50dd237c
abs <abs@pkgsrc.org>
2015-04-22 14:57:22 +0000

    Add a 'reload' rc.d option (automatically uses the pid file), which will make dnsmasq reload the dhcp file. Add patch comment and mps Bump PKGREVISION

--- net/dnsmasq/files/dnsmasq.sh
+++ net/dnsmasq/files/dnsmasq.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: dnsmasq.sh,v 1.2 2011/10/07 22:37:04 shattered Exp $
+# $NetBSD: dnsmasq.sh,v 1.3 2015/04/22 14:57:22 abs Exp $
 #
 
 # PROVIDE: dnsmasq
@@ -12,6 +12,7 @@ name="dnsmasq"
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
 pidfile="/var/run/${name}.pid"
+extra_commands="reload"
 
 load_rc_config $name
 run_rc_command "$1"
--- net/dnsmasq/patches/patch-src_bpf.c
+++ net/dnsmasq/patches/patch-src_bpf.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_bpf.c,v 1.3 2015/01/17 13:52:13 bsiegert Exp $
+$NetBSD: patch-src_bpf.c,v 1.4 2015/04/22 14:57:22 abs Exp $
+
+Add inet6 include for OS X
 
 --- src/bpf.c.orig     2015-01-02 04:23:05.000000000 +0000
 +++ src/bpf.c
