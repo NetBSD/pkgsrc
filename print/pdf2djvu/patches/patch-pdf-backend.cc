00cefc32d3d07b24054718a39053b5b4900c4429
joerg <joerg@pkgsrc.org>
2015-04-24 20:18:48 +0000

    Build with Poppler 0.32.

--- print/pdf2djvu/patches/patch-pdf-backend.cc
+++ print/pdf2djvu/patches/patch-pdf-backend.cc
@@ -1,6 +1,6 @@
-$NetBSD: patch-pdf-backend.cc,v 1.1 2013/08/31 14:54:23 joerg Exp $
+$NetBSD: patch-pdf-backend.cc,v 1.2 2015/04/24 20:18:48 joerg Exp $
 
---- pdf-backend.cc.orig	2013-08-31 13:41:40.000000000 +0000
+--- pdf-backend.cc.orig	2012-09-18 15:27:41.000000000 +0000
 +++ pdf-backend.cc
 @@ -54,7 +54,7 @@ static void poppler_error_handler(int po
    error_log << std::endl;
@@ -11,3 +11,19 @@ $NetBSD: patch-pdf-backend.cc,v 1.1 2013/08/31 14:54:23 joerg Exp $
  {
    std::string format;
    const char *category_name = _("PDF error");
+@@ -119,15 +119,6 @@ pdf::Environment::Environment(const char
+ #endif
+ }
+ 
+-void pdf::Environment::set_antialias(bool value)
+-{
+-  if (!globalParams->setAntialias(const_cast<char*>(value ? "yes" : "no")))
+-    throw UnableToSetParameter(_("Unable to set antialias parameter"));
+-  if (!globalParams->setVectorAntialias(const_cast<char*>(value ? "yes" : "no")))
+-    throw UnableToSetParameter(_("Unable to set vector antialias parameter"));
+-}
+-
+-
+ /* class pdf::Document
+  * ===================
+  */
