4f29201aebca7f40dc589f521d748a6179591afc
wiz <wiz@pkgsrc.org>
2015-04-25 10:32:07 +0000

    Re-Import gcc2-2.95.3nb7 as lang/gcc2 (previously lang/gcc).
    This directory contains the GNU Compiler Collection (GCC) version 2.95.
    It includes all of the support for compiling C, C++, Objective C, Fortran,
    Java, and Chill.
    
    The GNU Compiler Collection is free software.  See the file COPYING for copying
    permission.
    
    See the file gcc.texi (together with other files that it includes) for
    installation and porting information.  The file INSTALL contains a
    copy of the installation information, as plain ASCII.
    
    See the Bugs chapter of the GCC Manual for how to report bugs
    usefully.  An online readable version of the manual is in the files
    gcc.info*.

---
+++ lang/gcc2/files/gcc.mk
@@ -0,0 +1,7 @@
+# $NetBSD: gcc.mk,v 1.1 2015/04/25 10:32:07 wiz Exp $
+#
+# make configuration file for @DISTNAME@
+
+CC=		@GCC_PREFIX@/bin/gcc
+CPP=		@GCC_PREFIX@/bin/cpp
+CXX=		@GCC_PREFIX@/bin/g++
