9cbacc8c301d59429125891706a7ea7aaea6db91
bsiegert <bsiegert@pkgsrc.org>
2015-04-25 19:28:44 +0000

    Pullup ticket #4682 - requested by hiramatsu inputmethod/librime: build fix
    Revisions pulled up:
    - inputmethod/librime/distinfo                                  1.3
    - inputmethod/librime/patches/patch-src_dict_table.cc           1.2
    
    ---
       Module Name:	pkgsrc
       Committed By:	hiramatsu
       Date:		Tue Apr 21 13:59:31 UTC 2015
    
       Modified Files:
       	pkgsrc/inputmethod/librime: distinfo
       	pkgsrc/inputmethod/librime/patches: patch-src_dict_table.cc
    
       Log Message:
       Fix build break with gcc.

--- inputmethod/librime/patches/patch-src_dict_table.cc
+++ inputmethod/librime/patches/patch-src_dict_table.cc
@@ -1,13 +1,17 @@
-$NetBSD: patch-src_dict_table.cc,v 1.1.2.2 2015/04/18 12:02:41 bsiegert Exp $
+$NetBSD: patch-src_dict_table.cc,v 1.1.2.3 2015/04/25 19:28:44 bsiegert Exp $
 
---- src/dict/table.cc.orig	2013-11-10 10:20:21.000000000 +0000
-+++ src/dict/table.cc
-@@ -403,14 +403,15 @@ bool Table::BuildEntryList(const DictEnt
+Entries in the output file are generally not correctly aligned,
+so tell the compiler explicitly about this fact and use memcpy
+for writing the data.
+
+--- src/dict/table.cc.orig	2013-11-10 19:20:21.000000000 +0900
++++ src/dict/table.cc	2015-04-18 12:14:33.000000000 +0900
+@@ -403,14 +403,15 @@
    return true;
  }
  
 -bool Table::BuildEntry(const DictEntry &dict_entry, table::Entry *entry) {
-+bool Table::BuildEntry(const DictEntry &dict_entry, __attribute__((aligned(1))) table::Entry *entry) {
++bool Table::BuildEntry(const DictEntry &dict_entry, table::Entry * __attribute__((aligned(1))) entry) {
    if (!entry)
      return false;
    if (!CopyString(dict_entry.text, &entry->text)) {
