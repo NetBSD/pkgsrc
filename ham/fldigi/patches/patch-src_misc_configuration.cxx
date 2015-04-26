d7c8b8ad2558a485ecaab9220874ac92001ed494
mef <mef@pkgsrc.org>
2015-04-26 03:04:59 +0000

    (pkgsrc)   - Drop patches, upstream seems to fix the problems.   - (flxmlrpc is not supported yet, to be added later) (upstream)   - Update 3.22.07 to 3.22.08 ------------------------- =Version 3.22.08=
    2015-04-23  David Freese  <iam_w1hkj@w1hkj.com>
    
            2bda963: Analysis
            967e7c5: rx/tx queues
            b5aa6a3: flxmlrpc
    
    2015-04-16  Dave Freese  <w1hkj@bellsouth.net>
    
            28839d7: call focus
    
    2015-04-14  David Freese  <iam_w1hkj@w1hkj.com>
    
            a4d4faa: 599 update
            41beca0: US units
            91ab73f: olivia cxx
            a45006d: fldigi-shell
            ee19668: Write log
            8d4f0ef: Config dialog

--- ham/fldigi/patches/patch-src_misc_configuration.cxx
+++ ham/fldigi/patches/patch-src_misc_configuration.cxx
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_misc_configuration.cxx,v 1.2 2014/09/05 07:29:28 mef Exp $
+$NetBSD: patch-src_misc_configuration.cxx,v 1.3 2015/04/26 03:04:59 mef Exp $
 
 For DragonFly build to fix following problem:
 misc/configuration.cxx: In member function 'void configuration::testCommPorts()':
@@ -6,9 +6,18 @@ misc/configuration.cxx:934:2: error: 'glob_t' was not declared in this scope
 misc/configuration.cxx:934:9: error: expected ';' before 'gbuf'
 gmake[3]: *** [fldigi-configuration.o] Error 1
 
---- src/misc/configuration.cxx.orig	2013-04-12 23:24:15.000000000 +0900
-+++ src/misc/configuration.cxx	2013-04-18 21:29:15.000000000 +0900
-@@ -925,7 +925,7 @@ void configuration::testCommPorts()
+--- src/misc/configuration.cxx.orig	2015-04-18 22:26:19.000000000 +0900
++++ src/misc/configuration.cxx	2015-04-26 11:26:35.000000000 +0900
+@@ -863,7 +863,7 @@
+ 		"/dev/ttyACM%u",
+ 		"/dev/usb/ttyACM%u",
+ 		"/opt/vttyS%u"
+-#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
++#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+ 		"/dev/tty%2.2u"
+ #elif defined(__CYGWIN__)
+ 		"/dev/ttyS%u"
+@@ -877,7 +877,7 @@
  
  #if defined(__WOE32__)
  #  define TTY_MAX 255
