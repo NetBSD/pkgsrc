9421acea2d9287a39ab2cc90135966e8803ceb20
spz <spz@pkgsrc.org>
2015-04-26 19:19:42 +0000

    Update to Freeciv 2.5. The qt client would be qt5; it is not enabled in pkgsrc yet.
    Upstream ChangeLog is at http://freeciv.wikia.com/wiki/NEWS-2.5.0,
    excerpt:
     What's changed since 2.4
    
     The main changes in Freeciv 2.5 are the inclusion of the 'civ2civ3'
     ruleset, a new Qt client, and much more flexibility for those
     creating their own rulesets. The AI has also had some improvements.
    
     As is usual for major releases, 2.5 clients cannot interoperate with
     pre-2.5 servers, and vice versa. Pre-2.5 savegames can however be
     loaded into 2.5, and in most cases, the supplied rulesets have not
     changed so much as to make it difficult to complete a game started
     with 2.4.x's rules.

--- games/freeciv-client/options.mk
+++ games/freeciv-client/options.mk
@@ -1,31 +1,57 @@
-# $NetBSD: options.mk,v 1.6 2009/01/17 22:09:39 shattered Exp $
+# $NetBSD: options.mk,v 1.7 2015/04/26 19:19:42 spz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.freeciv-client
 PKG_OPTIONS_REQUIRED_GROUPS=	backend
-PKG_OPTIONS_GROUP.backend=	gtk2 xaw xaw3d sdl
+#PKG_OPTIONS_GROUP.backend=	gtk2 gtk3 qt5 sdl xaw xaw3d
+PKG_OPTIONS_GROUP.backend=	gtk2 gtk3 sdl xaw xaw3d
 PKG_SUGGESTED_OPTIONS=		gtk2
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgtk2)
 CONFIGURE_ARGS+=	--enable-client=gtk2
+CONFIGURE_ARGS+=	--enable-fcmp=gtk2
 .include "../../x11/gtk2/buildlink3.mk"
+PLIST_SRC=		${PKGDIR}/PLIST.gtk2
 .endif
 
-.if !empty(PKG_OPTIONS:Mxaw)
-CONFIGURE_ARGS+=	--enable-client=xaw
-.include "../../mk/xaw.buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mgtk3)
+CONFIGURE_ARGS+=	--enable-client=gtk3
+CONFIGURE_ARGS+=	--enable-fcmp=gtk3
+.include "../../x11/gtk3/buildlink3.mk"
+PLIST_SRC=		${PKGDIR}/PLIST.gtk3
 .endif
 
-.if !empty(PKG_OPTIONS:Mxaw3d)
-CONFIGURE_ARGS+=	--enable-client=xaw3d
-.include "../../x11/Xaw3d/buildlink3.mk"
-.include "../../x11/xextproto/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mqt5)
+CONFIGURE_ARGS+=	--enable-client=qt
+CONFIGURE_ARGS+=	--enable-fcmp=qt
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+PLIST_SRC=		${PKGDIR}/PLIST.qt
 .endif
 
 .if !empty(PKG_OPTIONS:Msdl)
 CONFIGURE_ARGS+=	--enable-client=sdl
-#CONFIGURE_ARGS+=	--enable-ftwl=sdl
+CONFIGURE_ARGS+=	--enable-fcmp=cli
 .include "../../audio/SDL_mixer/buildlink3.mk"
 .include "../../graphics/SDL_image/buildlink3.mk"
+.include "../../devel/SDL_gfx/buildlink3.mk"
+.include "../../devel/SDL_ttf/buildlink3.mk"
+PLIST_SRC=		${PKGDIR}/PLIST.sdl
+.endif
+
+.if !empty(PKG_OPTIONS:Mxaw)
+CONFIGURE_ARGS+=	--enable-client=xaw
+CONFIGURE_ARGS+=	--enable-fcmp=cli
+CONFIGURE_ARGS+=	--with-xaw
+.include "../../mk/xaw.buildlink3.mk"
+PLIST_SRC=		${PKGDIR}/PLIST.xaw
+.endif
+
+.if !empty(PKG_OPTIONS:Mxaw3d)
+CONFIGURE_ARGS+=	--enable-client=xaw
+CONFIGURE_ARGS+=	--enable-fcmp=cli
+CONFIGURE_ARGS+=	--with-xaw3d
+.include "../../x11/Xaw3d/buildlink3.mk"
+.include "../../x11/xextproto/buildlink3.mk"
+PLIST_SRC=		${PKGDIR}/PLIST.xaw
 .endif
