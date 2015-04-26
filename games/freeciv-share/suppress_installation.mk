a42360b06afee5d3c384b078db29d10225eecc20
spz <spz@pkgsrc.org>
2015-04-26 19:19:42 +0000

    Update to Freeciv 2.5. The qt client would be qt5; it is not enabled in pkgsrc yet.
    Upstream ChangeLog is at http://freeciv.wikia.com/wiki/NEWS-2.5.0,
    excerpt:
     What's changed since 2.4
    
     The main changes in Freeciv 2.5 are the inclusion of the 'civ2civ3'
     ruleset, a new Qt client, and much more flexibility for those
     creating their own rulesets. The AI has also had some improvements.
    
     As is usual for major releases, 2.5 clients cannot interoperate with
     pre-2.5 servers, and vice versa. Pre-2.5 savegames can however be
     loaded into 2.5, and in most cases, the supplied rulesets have not
     changed so much as to make it difficult to complete a game started
     with 2.4.x's rules.

--- games/freeciv-share/suppress_installation.mk
+++ games/freeciv-share/suppress_installation.mk
@@ -1,13 +1,21 @@
-# $NetBSD: suppress_installation.mk,v 1.2 2013/05/23 15:02:53 joerg Exp $
+# $NetBSD: suppress_installation.mk,v 1.3 2015/04/26 19:19:42 spz Exp $
 
 # Makefile snippet to be included by freeciv-client and freeciv-server to
 # prevent files installed by freeciv-share from being installed again.
 
+CONFIGURE_ARGS+=		--without-freeciv-manual
+
 SUBST_CLASSES+=			installmanfix
 SUBST_STAGE.installmanfix=	pre-configure
 SUBST_FILES.installmanfix=	Makefile.in
-SUBST_SED.installmanfix+=	-e 's|po doc||g'
+SUBST_SED.installmanfix+=	-e 's|translations doc ||g'
 SUBST_MESSAGE.installmanfix=	Suppressing installation of man pages and localization files already in freeciv-share.
+
+SUBST_CLASSES+=			installmanfix2
+SUBST_STAGE.installmanfix2=	pre-configure
+SUBST_FILES.installmanfix2=	Makefile.in
+SUBST_SED.installmanfix2+=	-e 's|install-data-am: install-docDATA|install-data-am: \# install-docDATA|'
+SUBST_MESSAGE.installmanfix2=	Suppressing installation of doc files already in freeciv-share
 
 SUBST_CLASSES+=			installfix
 SUBST_STAGE.installfix=		pre-configure
@@ -15,14 +23,21 @@ SUBST_FILES.installfix=		common/Makefile.in
 SUBST_SED.installfix+=		-e 's|^install-exec-am: install-libLTLIBRARIES|install-exec-am: \# install-libLTLIBRARIES|'
 SUBST_MESSAGE.installfix=	Suppressing installation of libs already in freeciv-share.
 
-SUBST_CLASSES+=			installmanfix2
-SUBST_STAGE.installmanfix2=	post-configure
-SUBST_FILES.installmanfix2=	Makefile
-SUBST_SED.installmanfix2+=	-e 's|po doc||g'
-SUBST_MESSAGE.installmanfix2=	Suppressing installation of man pages and localization files already in freeciv-share.
+SUBST_CLASSES+=			installmanfix3
+SUBST_STAGE.installmanfix3=	post-configure
+SUBST_FILES.installmanfix3=	Makefile
+SUBST_SED.installmanfix3+=	-e 's|translations doc ||g'
+SUBST_MESSAGE.installmanfix3=	Suppressing installation of man pages and localization files already in freeciv-share.
+
+SUBST_CLASSES+=			installmanfix4
+SUBST_STAGE.installmanfix4=	post-configure
+SUBST_FILES.installmanfix4=	Makefile
+SUBST_SED.installmanfix4+=	-e 's|install-data-am: install-docDATA|install-data-am: \# install-docDATA|'
+SUBST_MESSAGE.installmanfix4=	Suppressing installation of doc files already in freeciv-share
 
 SUBST_CLASSES+=			installfix2
-SUBST_STAGE.installfix2=		post-configure
-SUBST_FILES.installfix2=		common/Makefile
+SUBST_STAGE.installfix2=	post-configure
+SUBST_FILES.installfix2=	common/Makefile
 SUBST_SED.installfix2+=		-e 's|^install-exec-am: install-libLTLIBRARIES|install-exec-am: \# install-libLTLIBRARIES|'
 SUBST_MESSAGE.installfix2=	Suppressing installation of libs already in freeciv-share.
+
