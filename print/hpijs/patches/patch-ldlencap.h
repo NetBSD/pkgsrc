205d4e8f9d49509263c1801e16248fb9d8b2e1cb
joerg <joerg@pkgsrc.org>
2015-04-26 13:28:23 +0000

    Drop ONLY_FOR_COMPILER. Fix a bunch of warnings from clang, missing includes warranting a revision bump.

---
+++ print/hpijs/patches/patch-ldlencap.h
@@ -0,0 +1,13 @@
+$NetBSD: patch-ldlencap.h,v 1.1 2015/04/26 13:28:23 joerg Exp $
+
+--- ldlencap.h.orig	2015-04-26 12:32:01.000000000 +0000
++++ ldlencap.h
+@@ -28,7 +28,7 @@
+   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   \***************************************************************************/
+ #ifndef _LDLENCAP
+-#define _LDLECNAP
++#define _LDLENCAP
+ 
+ // Wait times
+ #define PACKET_WAIT                 5
