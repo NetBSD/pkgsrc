3549501c675fbbf7ca600596ee2f0ac9b53a74c5
spz <spz>
2015-04-29 18:40:55 +0000

    Pullup ticket #4706 - requested by roy net/dnsmasq: security patch
    Revisions pulled up:
    - net/dnsmasq/Makefile                                          1.28,1.27
    - net/dnsmasq/distinfo                                          1.26,1.25
    - net/dnsmasq/files/dnsmasq.sh                                  1.3
    - net/dnsmasq/patches/patch-src_bpf.c                           1.4
    - net/dnsmasq/patches/patch-src_rfc1035.c                       1.1
    
    -------------------------------------------------------------------
       Module Name:    pkgsrc
       Committed By:   roy
       Date:           Wed Apr 29 12:53:02 UTC 2015
    
       Modified Files:
               pkgsrc/net/dnsmasq: Makefile distinfo
       Added Files:
               pkgsrc/net/dnsmasq/patches: patch-src_rfc1035.c
    
       Log Message:
       Add a patch to fix CVE-2015-3294.
       Remove myself as maintainer.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.27 -r1.28 pkgsrc/net/dnsmasq/Makefile
       cvs rdiff -u -r1.25 -r1.26 pkgsrc/net/dnsmasq/distinfo
       cvs rdiff -u -r0 -r1.1 pkgsrc/net/dnsmasq/patches/patch-src_rfc1035.c
    
    -------------------------------------------------------------------
       Module Name:    pkgsrc
       Committed By:   abs
       Date:           Wed Apr 22 14:57:22 UTC 2015
    
       Modified Files:
               pkgsrc/net/dnsmasq: Makefile distinfo
               pkgsrc/net/dnsmasq/files: dnsmasq.sh
               pkgsrc/net/dnsmasq/patches: patch-src_bpf.c
    
       Log Message:
       Add a 'reload' rc.d option (automatically uses the pid file), which will
       make dnsmasq reload the dhcp file. Add patch comment and mps
       Bump PKGREVISION
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.26 -r1.27 pkgsrc/net/dnsmasq/Makefile
       cvs rdiff -u -r1.24 -r1.25 pkgsrc/net/dnsmasq/distinfo
       cvs rdiff -u -r1.2 -r1.3 pkgsrc/net/dnsmasq/files/dnsmasq.sh
       cvs rdiff -u -r1.3 -r1.4 pkgsrc/net/dnsmasq/patches/patch-src_bpf.c

--- net/dnsmasq/patches/patch-src_bpf.c
+++ net/dnsmasq/patches/patch-src_bpf.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_bpf.c,v 1.3 2015/01/17 13:52:13 bsiegert Exp $
+$NetBSD: patch-src_bpf.c,v 1.3.2.1 2015/04/29 18:40:55 spz Exp $
+
+Add inet6 include for OS X
 
 --- src/bpf.c.orig     2015-01-02 04:23:05.000000000 +0000
 +++ src/bpf.c
