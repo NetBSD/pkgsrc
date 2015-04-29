5dc75df583a640654bc6afe408550cdebb0b8040
sevan <sevan@pkgsrc.org>
2015-04-29 14:12:54 +0000

    Unbreak on FreeBSD by also ensuring that ERSTART is defined and set to the expected value for it.
    Reviewed by bsiegert@

--- graphics/MesaLib/patches/patch-src_gallium_winsys_svga_drm_vmw__screen__ioctl.c
+++ graphics/MesaLib/patches/patch-src_gallium_winsys_svga_drm_vmw__screen__ioctl.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_gallium_winsys_svga_drm_vmw__screen__ioctl.c,v 1.1 2015/04/25 11:19:18 tnn Exp $
+$NetBSD: patch-src_gallium_winsys_svga_drm_vmw__screen__ioctl.c,v 1.2 2015/04/29 14:12:54 sevan Exp $
 
 Make sure ERESTART gets defined.
 
 --- src/gallium/winsys/svga/drm/vmw_screen_ioctl.c.orig	2015-03-21 00:51:18.000000000 +0000
 +++ src/gallium/winsys/svga/drm/vmw_screen_ioctl.c
-@@ -48,6 +48,13 @@
+@@ -48,6 +48,21 @@
  
  #include "os/os_mman.h"
  
@@ -15,6 +15,14 @@ Make sure ERESTART gets defined.
 +#define ERESTART -3
 +#endif
 +#endif
++
++#if defined(__FreeBSD__)
++#include <errno.h>
++#if !defined(ERESTART)
++#define ERESTART -1
++#endif
++#endif
++
  #include <errno.h>
  #include <unistd.h>
  
