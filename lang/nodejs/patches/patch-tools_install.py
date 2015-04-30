4bd308c93f30e7c0515a4a283f59fc3adfc97609
ryoon <ryoon@pkgsrc.org>
2015-04-30 15:04:56 +0000

    Update to 0.12.2
    Changelog:
    2015.03.31, Version 0.12.2 (Stable)
    
    * uv: Upgrade to 1.4.2
    
    * npm: Upgrade to 2.7.4
    
    * V8: do not add extra newline in log file (Julien Gilli)
    
    * V8: Fix --max_old_space_size=4096 integer overflow (Andrei Sedoi)
    
    * asyncwrap: fix constructor condition for early ret (Trevor Norris)
    
    * buffer: align chunks on 8-byte boundary (Fedor Indutny)
    
    * buffer: fix pool offset adjustment (Trevor Norris)
    
    * build: fix use of strict aliasing (Trevor Norris)
    
    * console: allow Object.prototype fields as labels (Colin Ihrig)
    
    * fs: make F_OK/R_OK/W_OK/X_OK not writable (Jackson Tian)
    
    * fs: properly handle fd passed to truncate() (Bruno Jouhier)
    
    * http: fix assert on data/end after socket error (Fedor Indutny)
    
    * lib: fix max size check in Buffer constructor (Ben Noordhuis)
    
    * lib: fix stdio/ipc sync i/o regression (Ben Noordhuis)
    
    * module: replace NativeModule.require (Herbert Vojčík)
    
    * net: allow port 0 in connect() (cjihrig)
    
    * net: unref timer in parent sockets (Fedor Indutny)
    
    * path: refactor for performance and consistency (Nathan Woltman)
    
    * smalloc: extend user API (Trevor Norris)
    
    * src: fix for SIGINT crash on FreeBSD (Fedor Indutny)
    
    * src: fix builtin modules failing with --use-strict (Julien Gilli)
    
    * watchdog: fix timeout for early polling return (Saúl Ibarra Corretgé)
    
    
    2015.03.23, Version 0.12.1 (Stable), 0034086b49f22cfde765a7e9f55db25f8eb310b6
    
    * openssl: upgrade to 1.0.1m (Addressing multiple CVES)
    
    
    2015.02.06, Version 0.12.0 (Stable), 2b18916ff054309a07408719b62e2b6a4f1e056a
    
    * npm: Upgrade to 2.5.1
    
    * mdb_v8: update for v0.12 (Dave Pacheco)

--- lang/nodejs/patches/patch-tools_install.py
+++ lang/nodejs/patches/patch-tools_install.py
@@ -1,12 +1,15 @@
-$NetBSD: patch-tools_install.py,v 1.1 2013/05/22 15:17:07 mspo Exp $
+$NetBSD: patch-tools_install.py,v 1.2 2015/04/30 15:04:56 ryoon Exp $
 
 Install man pages under the right directory.
 
---- tools/install.py.orig	2013-03-21 01:15:04.000000000 +0000
+--- tools/install.py.orig	2015-03-31 22:13:01.000000000 +0000
 +++ tools/install.py
-@@ -129,7 +129,7 @@ def files(action):
+@@ -136,9 +136,9 @@ def files(action):
+   action(['src/node.stp'], 'share/systemtap/tapset/')
+ 
    if 'freebsd' in sys.platform or 'openbsd' in sys.platform:
-     action(['doc/node.1'], 'man/man1/')
+-    action(['doc/node.1'], 'man/man1/')
++    action(['doc/node.1'], '@PKGMANDIR@/man1/')
    else:
 -    action(['doc/node.1'], 'share/man/man1/')
 +    action(['doc/node.1'], '@PKGMANDIR@/man1/')
