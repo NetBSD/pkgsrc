299a724ce1658288309413b3342332a47afc856e
wiz <wiz>
2015-05-04 21:10:40 +0000

    Update to 1.0.1:
    1.0.1 [2015/05/04]
    
    * Build fixes for Windows.
    
    While here, bump ABI depends for the shlib major bump in the 1.0 release.

--- archivers/libzip/buildlink3.mk
+++ archivers/libzip/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2013/04/01 16:03:31 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2015/05/04 21:10:40 wiz Exp $
 
 BUILDLINK_TREE+=	libzip
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libzip
 LIBZIP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libzip+=	libzip>=0.7.1
-BUILDLINK_ABI_DEPENDS.libzip+=	libzip>=0.11
+BUILDLINK_ABI_DEPENDS.libzip+=	libzip>=1.0
 BUILDLINK_PKGSRCDIR.libzip?=	../../archivers/libzip
 
 BUILDLINK_INCDIRS.libzip=	include lib/libzip/include
