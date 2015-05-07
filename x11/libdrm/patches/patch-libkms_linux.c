888d6f1623345e28cd9be539ec23cb71528d4ba5
wiz <wiz@pkgsrc.org>
2015-05-07 06:31:06 +0000

    Update to 2.4.61:
    Release to fix regression in nouveau in 2.4.60,
    but brings lots of android changes and other fixes.
    
    Ben Skeggs (2):
          nouveau: restore check that avoids multiple user bos per kernel bo
          configure.ac: bump version to 2.4.61 for release
    
    Chih-Wei Huang (3):
          android: simplify the including rule of subdirs
          android: get rid of LIBDRM_TOP
          android: remove unnecessary TARGET_OUT_HEADERS variable
    
    Chris Wilson (2):
          mode: Retrieve only the current information for a Connector
          intel: Delay testing for userptr until first use
    
    Connor Behan (1):
          xf86drm: Fix ioctl struct clearing in drmAgpEnable
    
    Damien Lespiau (3):
          RELEASING: Fix releasing instructions to match the latest release.sh
          RELEASING: Fix the step numbering
          RELEASING: Fix annouce typo
    
    Daniel Kurtz (3):
          proptest: install it with --enable-install-test-programs
          tests: add rockchip to modetest, kmstest, vbltest and proptest
          xf86drmMode.h: inline -> __inline for use with gcc -std=c89 -pedantic
    
    Emil Velikov (39):
          android: correcly set LOCAL_EXPORT_C_INCLUDE_DIRS
          android: simplify LOCAL_C_INCLUDES
          android: remove ${srcdir} from the includes
          android: remove LOCAL_COPY_HEADERS* variables
          android: add the missing tag "optional" to libkms
          autotools: remove ${srcdir} from the includes
          android: remove explicit include to libpciaccess
          tests/hash: extract test out of xf86drmHash.c
          tests/hash: misc compilation fixes
          tests/hash: style fixes
          tests/hash: return non-zero on failure
          tests/random: extract test out of xf86drmRandom.c
          tests/random: return non-zero on test failure
          drm: replace HASH_DEBUG with DEBUG
          drm: use correct printf modifiers
          configure.ac: split -fvisibility and __attribute__((visibility)) checks
          radeon: move bof.[ch] out of libdrm_radeon
          radeon: add symbols test
          freedreno: annotate the private symbols
          freedreno: add symbols test
          intel: remove the drm_mm* symbol workarounds
          intel: remove unused mmFindBlock
          intel: annotate the private symbols
          intel: add symbols test
          nouveau: annotate the private symbols
          nouveau: add symbols test
          libkms: annotate private symbols
          libkms: add symbols test
          exynos: add symbols test
          omap: add symbols test
          tegra: add symbols test
          drm: rename libdrm{,_macros}.h
          drm: remove no longer needed VISIBILITY_CFLAGS
          drm: remove drm_public macro
          configure: request/set the compiler in C99 mode
          drm: use c99 __func__ over __FUNCTION__
          man: rework the Makefile.am
          android: set the HAVE_VISIBILITY define
          freedreno: link against CLOCK_LIB
    
    Greg Hackmann (1):
          Add missing <strings.h> includes
    
    Jan Vesely (5):
          Fix unused function warnings
          Remove drmSetDebugMsgFunction and related infrastructure
          tests/exynos: Fix missing static keyword
          drmSL: Fix neighbor lookup
          tests/drmsl: Extract tests out of xf86drmSL.c
    
    Joonyoung Shim (6):
          modetest: fix Segmentation fault
          modetest: make use of drmModeRmFB
          modetest: fix the error path handling
          modetest: clear buffer and framebuffer for planes
          modetest: destroy the cursor bo
          modetest: fix the arguments of the MAKE_RGB_INFO define
    
    Neil Roberts (1):
          intel: Merge latest i915_drm.h
    
    Rob Clark (2):
          modeprint: add missing encoder/connector type names
          modetest: fix allocation for yuv420/yvu420
    
    Tobias Jakobi (1):
          modetest: initialize handles/pitches in set_plane()
    
    Tvrtko Ursulin (1):
          intel: Leak the userptr test bo

--- x11/libdrm/patches/patch-libkms_linux.c
+++ x11/libdrm/patches/patch-libkms_linux.c
@@ -1,16 +1,16 @@
-$NetBSD: patch-libkms_linux.c,v 1.1 2012/02/16 20:30:56 hans Exp $
+$NetBSD: patch-libkms_linux.c,v 1.2 2015/05/07 06:31:06 wiz Exp $
 
---- libkms/linux.c.orig	2010-03-06 00:21:13.000000000 +0100
-+++ libkms/linux.c	2012-01-10 20:40:06.019270181 +0100
+--- libkms/linux.c.orig	2015-05-06 23:04:31.000000000 +0000
++++ libkms/linux.c
 @@ -40,6 +40,11 @@
- 
+ #include <string.h>
+ #include <unistd.h>
  #include <sys/stat.h>
- 
 +#ifdef __sun
 +#include <sys/types.h>
 +#include <sys/mkdev.h>
 +#endif
 +
- #include "internal.h"
  
- #define PATH_SIZE 512
+ #include "libdrm_macros.h"
+ #include "internal.h"
--- x11/libdrm/patches/patch-libkms_vmwgfx.c
+++ x11/libdrm/patches/patch-libkms_vmwgfx.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-libkms_vmwgfx.c,v 1.2 2014/10/01 09:39:30 wiz Exp $
+$NetBSD: patch-libkms_vmwgfx.c,v 1.3 2015/05/07 06:31:06 wiz Exp $
 
 Provide compatibility errno number for non-Linux.
 
---- libkms/vmwgfx.c.orig	2014-09-28 16:19:54.000000000 +0000
+--- libkms/vmwgfx.c.orig	2015-05-06 23:04:31.000000000 +0000
 +++ libkms/vmwgfx.c
 @@ -39,6 +39,10 @@
- #include "libdrm.h"
+ #include "libdrm_macros.h"
  #include "vmwgfx_drm.h"
  
 +#ifndef ERESTART
