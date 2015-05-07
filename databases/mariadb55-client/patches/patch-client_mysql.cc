92d194c357837e3a7c8b454ae72d9dafb3e21f0f
fhajny <fhajny@pkgsrc.org>
2015-05-07 12:45:28 +0000

    Update mariadb55-* to 5.5.43.
    pkgsrc changes:
    * Fix PLIST for dtrace and embedded-server options on and off.
    * Patch for when readline>6 is used where some deprecated symbols were
      removed. Fixes build with devel/readline.
    * DTrace support still not fixes for recent SunOS platforms.
    
    upstream changes:
    * XtraDB updated to 5.5.42-37.1
    * TokuDB updated to version 7.5.6
    * Fix for CVE-2015-0501
    * Fix for CVE-2015-2571
    * Fix for CVE-2015-0505
    * Fix for CVE-2015-0499
    
    See full changelog:
    
      https://mariadb.com/kb/en/mariadb/mariadb-5543-changelog/

--- databases/mariadb55-client/patches/patch-client_mysql.cc
+++ databases/mariadb55-client/patches/patch-client_mysql.cc
@@ -1,4 +1,4 @@
-$NetBSD: patch-client_mysql.cc,v 1.1 2015/04/16 20:20:15 ryoon Exp $
+$NetBSD: patch-client_mysql.cc,v 1.2 2015/05/07 12:45:29 fhajny Exp $
 
 --- client/mysql.cc.orig	2015-02-13 12:07:00.000000000 +0000
 +++ client/mysql.cc
@@ -11,3 +11,24 @@ $NetBSD: patch-client_mysql.cc,v 1.1 2015/04/16 20:20:15 ryoon Exp $
  #define HAVE_READLINE
  #define USE_POPEN
  #endif
+@@ -2640,7 +2640,11 @@ static void initialize_readline (char *n
+   rl_completion_entry_function= &no_completion;
+   rl_add_defun("magic-space", (Function*)&fake_magic_space, -1);
+ #else
++# if defined(USE_READLINE_V6)
++  rl_attempted_completion_function= (rl_completion_func_t*)&new_mysql_completion;
++# else
+   rl_attempted_completion_function= (CPPFunction*)&new_mysql_completion;
++# endif
+   rl_completion_entry_function= &no_completion;
+ #endif
+ }
+@@ -2657,7 +2661,7 @@ static char **new_mysql_completion(const
+                                    int end __attribute__((unused)))
+ {
+   if (!status.batch && !quick)
+-#if defined(USE_NEW_READLINE_INTERFACE)
++#if defined(USE_NEW_READLINE_INTERFACE) || defined(USE_READLINE_V6)
+     return rl_completion_matches(text, new_command_generator);
+ #else
+     return completion_matches((char *)text, (CPFunction *)new_command_generator);
