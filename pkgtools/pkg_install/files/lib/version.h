eb93ecd7fe71cb3656773d6fe0e8712c2d798161
agc <agc@pkgsrc.org>
2015-05-08 16:29:37 +0000

    Fix format string in pkg_add's help message - from J. Lewis Muir
    Bump version number for pkg_install to 20150508

--- pkgtools/pkg_install/files/lib/version.h
+++ pkgtools/pkg_install/files/lib/version.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: version.h,v 1.167 2014/12/30 15:13:21 wiz Exp $	*/
+/*	$NetBSD: version.h,v 1.168 2015/05/08 16:29:37 agc Exp $	*/
 
 /*
  * Copyright (c) 2001 Thomas Klausner.  All rights reserved.
@@ -27,6 +27,6 @@
 #ifndef _INST_LIB_VERSION_H_
 #define _INST_LIB_VERSION_H_
 
-#define PKGTOOLS_VERSION 20141227
+#define PKGTOOLS_VERSION 20150508
 
 #endif /* _INST_LIB_VERSION_H_ */
