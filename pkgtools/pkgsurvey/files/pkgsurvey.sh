d064697df0534164956f75645825fc143be8f0f2
joerg <joerg@pkgsrc.org>
2015-05-10 19:47:03 +0000

    pkgsurvey-1.0: Build the package list in a more useful format by including package name, package location and automatic flag. Send to a new list to keep the new format separate from the old one.

--- pkgtools/pkgsurvey/files/pkgsurvey.sh
+++ pkgtools/pkgsurvey/files/pkgsurvey.sh
@@ -1,5 +1,12 @@
 #! /bin/sh
 
-${PKG_INFO} | sort | mail -s "`hostname` `${UNAME} -s`-`${UNAME} -p`-`${UNAME} -r`" agc-pkgsurvey@pkgsrc.org
+${PKG_INFO} -e '*' | while read pkg; do
+	path=`${PKG_INFO} -Q PKGPATH "$pkg"`
+	automatic=`${PKG_INFO} -Q automatic "$pkg"`
+	if [ -z "$automatic" ]; then
+		automatic=no
+	fi
+	echo "$path $pkg $automatic"
+done | sort | mail -s "`hostname` `${UNAME} -s`-`${UNAME} -p`-`${UNAME} -r`" pkgsurvey1.0@pkgsrc.org
 
 exit 0
