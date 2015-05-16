d0a6c8bc67cbaf7b400df9efaba6573eab60af1b
taca <taca@pkgsrc.org>
2015-05-16 11:16:40 +0000

    Update php54 to 5.4.41.
    14 May 2015 PHP 5.4.41
    
    - Core:
      . Fixed bug #69364 (PHP Multipart/form-data remote dos Vulnerability). (Stas)
      . Fixed bug #69403 (str_repeat() sign mismatch based memory corruption).
        (Stas)
      . Fixed bug #69418 (CVE-2006-7243 fix regressions in 5.4+). (Stas)
      . Fixed bug #69522 (heap buffer overflow in unpack()). (Stas)
    
    - FTP:
      . Fixed bug #69545 (Integer overflow in ftp_genlist() resulting in heap
        overflow). (Stas)
    
    - PCNTL:
      . Fixed bug #68598 (pcntl_exec() should not allow null char). (Stas)
    
    - PCRE
      . Upgraded pcrelib to 8.37.
    
    - Phar:
      . Fixed bug #69453 (Memory Corruption in phar_parse_tarfile when entry
        filename starts with null). (Stas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.96 2015/04/17 16:42:43 taca Exp $
+# $NetBSD: phpversion.mk,v 1.97 2015/05/16 11:16:40 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -81,7 +81,7 @@
 PHPVERSION_MK=	defined
 
 # Define each PHP's version.
-PHP54_VERSION=	5.4.40
+PHP54_VERSION=	5.4.41
 PHP55_VERSION=	5.5.24
 PHP56_VERSION=	5.6.8
 
