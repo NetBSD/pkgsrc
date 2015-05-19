805bcab8e5e12a06240738d2d93199a624e1cdcf
ryoon <ryoon@pkgsrc.org>
2015-05-19 11:48:05 +0000

    Restrict buildlink3 requirement, samba<4.

--- net/samba/buildlink3.mk
+++ net/samba/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2012/10/30 13:24:32 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2015/05/19 11:48:05 ryoon Exp $
 
 BUILDLINK_TREE+=	samba
 
 .if !defined(SAMBA_BUILDLINK3_MK)
 SAMBA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.samba+=	samba>=3.0.7
-BUILDLINK_ABI_DEPENDS.samba+=	samba>=3.0.34nb1
+BUILDLINK_API_DEPENDS.samba+=	samba>=3.0.7<4.0
+BUILDLINK_ABI_DEPENDS.samba+=	samba>=3.0.34nb1<4.0
 BUILDLINK_PKGSRCDIR.samba?=	../../net/samba
 
 .include "../../converters/libiconv/buildlink3.mk"
