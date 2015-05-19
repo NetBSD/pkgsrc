341f1e98cba74bf4c2a05366738adc2775331c28
tron <tron@pkgsrc.org>
2015-05-19 17:57:02 +0000

    Pullup ticket #4725 - requested by taca lang/php55: security update
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.98
    - lang/php55/distinfo                                           1.40
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat May 16 11:17:45 UTC 2015
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php55: distinfo
    
       Log Message:
       Update php55 to 5.5.25.
    
       14 May 2015, PHP 5.5.25
    
       - Core:
         . Fixed bug #69364 (PHP Multipart/form-data remote dos Vulnerability). (Stas)
         . Fixed bug #69403 (str_repeat() sign mismatch based memory corruption).
           (Stas)
         . Fixed bug #69418 (CVE-2006-7243 fix regressions in 5.4+). (Stas)
         . Fixed bug #69522 (heap buffer overflow in unpack()). (Stas)
         . Fixed bug #69467 (Wrong checked for the interface by using Trait).
           (Laruence)
         . Fixed bug #69420 (Invalid read in zend_std_get_method). (Laruence)
         . Fixed bug #60022 ("use statement [...] has no effect" depends on leading
           backslash). (Nikita)
         . Fixed bug #67314 (Segmentation fault in gc_remove_zval_from_buffer).
           (Dmitry)
         . Fixed bug #68652 (segmentation fault in destructor). (Dmitry)
         . Fixed bug #69419 (Returning compatible sub generator produces a warning).
           (Nikita)
         . Fixed bug #69472 (php_sys_readlink ignores misc errors from
           GetFinalPathNameByHandleA). (Jan Starke)
    
       - FTP:
         . Fixed bug #69545 (Integer overflow in ftp_genlist() resulting in heap
           overflow). (Stas)
    
       - ODBC:
         . Fixed bug #69474 (ODBC: Query with same field name from two tables returns
           incorrect result). (Anatol)
         . Fixed bug #69381 (out of memory with sage odbc driver). (Frederic Marchall,
           Anatol Belski)
    
       - OpenSSL:
         . Fixed bug #69402 (Reading empty SSL stream hangs until timeout).
           (Daniel Lowrey)
    
       - PCNTL:
         . Fixed bug #68598 (pcntl_exec() should not allow null char). (Stas)
    
       - Phar:
         . Fixed bug #69453 (Memory Corruption in phar_parse_tarfile when entry filename
           starts with null). (Stas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.93.2.4 2015/05/19 17:52:32 tron Exp $
+# $NetBSD: phpversion.mk,v 1.93.2.5 2015/05/19 17:57:02 tron Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -82,7 +82,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP54_VERSION=	5.4.41
-PHP55_VERSION=	5.5.24
+PHP55_VERSION=	5.5.25
 PHP56_VERSION=	5.6.8
 
 # Define initial release of major version.
