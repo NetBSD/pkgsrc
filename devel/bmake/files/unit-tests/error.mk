7b9bb7a047473e6f45f2251291d4f7ff8f437dcf
joerg <joerg>
2015-05-19 21:36:43 +0000

    Import bmake-20150505

---
+++ devel/bmake/files/unit-tests/error.mk
@@ -0,0 +1,10 @@
+# $Id: error.mk,v 1.1.1.1 2015/05/19 21:36:45 joerg Exp $
+
+.info just FYI
+.warning this could be serious
+.error this is fatal
+
+all:
+
+.info.html:
+	@echo this should be ignored
