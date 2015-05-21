ba9e9e19d079793d6cdf7e58c55ca4f392f66119
rodent <rodent@pkgsrc.org>
2015-05-21 22:21:08 +0000

    Fix this the right way, thanks to joerg@. Noticed this is linking against pthread too, so let's included that bl3.mk here as well.

--- devel/memcached/patches/patch-memcached.c
+++ devel/memcached/patches/patch-memcached.c
@@ -1,15 +1,17 @@
-$NetBSD: patch-memcached.c,v 1.1 2015/05/21 14:07:10 rodent Exp $
+$NetBSD: patch-memcached.c,v 1.2 2015/05/21 22:21:08 rodent Exp $
 
-Silences: format specifies type 'int' but the argument has type 'long'
+Fix this the right way thanks to joerg@.
 
 --- memcached.c.orig	2015-01-01 07:50:52.000000000 +0000
 +++ memcached.c
-@@ -2765,7 +2765,7 @@ static void process_stats_conns(ADD_STAT
+@@ -2764,8 +2764,8 @@ static void process_stats_conns(ADD_STAT
+                 APPEND_NUM_STAT(i, "addr", "%s", conn_name);
                  APPEND_NUM_STAT(i, "state", "%s",
                          state_text(conns[i]->state));
-                 APPEND_NUM_STAT(i, "secs_since_last_cmd", "%d",
+-                APPEND_NUM_STAT(i, "secs_since_last_cmd", "%d",
 -                        current_time - conns[i]->last_cmd_time);
-+                        (int)(current_time - conns[i]->last_cmd_time));
++                APPEND_NUM_STAT(i, "secs_since_last_cmd", "%jd",
++                        (intmax_t)(current_time - conns[i]->last_cmd_time));
              }
          }
      }
