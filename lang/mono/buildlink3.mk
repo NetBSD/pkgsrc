cfdcdf83bc4c7f75347b50e5dabca58666397637
kefren <kefren@pkgsrc.org>
2015-05-21 20:03:23 +0000

    sync requirements and comment with reality

--- lang/mono/buildlink3.mk
+++ lang/mono/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.51 2015/04/06 08:17:15 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.52 2015/05/21 20:03:23 kefren Exp $
 
 BUILDLINK_TREE+=	mono
 
 .if !defined(MONO_BUILDLINK3_MK)
 MONO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mono+=	mono>=3
-BUILDLINK_ABI_DEPENDS.mono?=	mono>=3.10.0nb1
+BUILDLINK_API_DEPENDS.mono+=	mono>=4
+BUILDLINK_ABI_DEPENDS.mono?=	mono>=4.0.1
 BUILDLINK_PKGSRCDIR.mono?=	../../lang/mono
 ALL_ENV+=			MONO_SHARED_DIR=${WRKDIR:Q}
 BUILDLINK_CONTENTS_FILTER.mono=	${EGREP} '(^include/|^lib/)'
