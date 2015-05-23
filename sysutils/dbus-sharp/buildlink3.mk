6062dad2699b285f81c2adce4f267945d86964ac
kefren <kefren@pkgsrc.org>
2015-05-23 06:40:15 +0000

    switch dbus-sharp, dbus-sharp-glib, ndesk-dbus and ndesk-dbus-glib to the latest mono

--- sysutils/dbus-sharp/buildlink3.mk
+++ sysutils/dbus-sharp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2015/04/06 08:17:17 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2015/05/23 06:40:15 kefren Exp $
 
 BUILDLINK_TREE+=	dbus-sharp
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.dbus-sharp+=	dbus-sharp>=0.7.0
 BUILDLINK_ABI_DEPENDS.dbus-sharp?=	dbus-sharp>=0.7.0nb6
 BUILDLINK_PKGSRCDIR.dbus-sharp?=	../../sysutils/dbus-sharp
 
-.include "../../lang/mono2/buildlink3.mk"
+.include "../../lang/mono/buildlink3.mk"
 .endif	# DBUS_SHARP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-dbus-sharp
