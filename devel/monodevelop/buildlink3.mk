0b6074f382385ae38cce77a8d10da9d17fd7ff3f
kefren <kefren>
2015-05-23 15:59:14 +0000

    Update to 5.0.1 - the latest compilable version until we get nuget and a newer nunit in pkgsrc Probably still needs a lot of love.

--- devel/monodevelop/buildlink3.mk
+++ devel/monodevelop/buildlink3.mk
@@ -1,19 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.37 2015/04/25 14:20:37 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.38 2015/05/23 15:59:14 kefren Exp $
 
 BUILDLINK_TREE+=	monodevelop
 
 .if !defined(MONODEVELOP_BUILDLINK3_MK)
 MONODEVELOP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.monodevelop+=	monodevelop>=2.4
-BUILDLINK_ABI_DEPENDS.monodevelop+=	monodevelop>=2.4.2nb38
+BUILDLINK_API_DEPENDS.monodevelop+=	monodevelop>=5.0
+BUILDLINK_ABI_DEPENDS.monodevelop+=	monodevelop>=5.0
 BUILDLINK_PKGSRCDIR.monodevelop?=	../../devel/monodevelop
-BUILDLINK_CONTENTS_FILTER.monodevelop+=	${EGREP} '^lib/'
+BUILDLINK_CONTENTS_FILTER.monodevelop=	${EGREP} '^lib/'
 
 .include "../../databases/shared-mime-info/buildlink3.mk"
 .include "../../devel/mono-addins/buildlink3.mk"
 .include "../../graphics/libgdiplus/buildlink3.mk"
-.include "../../lang/mono2/buildlink3.mk"
+.include "../../lang/mono/buildlink3.mk"
 .include "../../x11/gtksourceview-sharp2/buildlink3.mk"
 .include "../../x11/gtk-sharp/buildlink3.mk"
 .include "../../x11/gnome-sharp/buildlink3.mk"
