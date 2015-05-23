1d58518a2d99b9ee19945a4385b5c8af665731d0
kefren <kefren@pkgsrc.org>
2015-05-23 06:28:58 +0000

    Update xsp and ap-mono to the latest and the greatest from mono-project Apache 2.4 linking of ap-mono is now possible

--- www/mono-xsp/buildlink3.mk
+++ www/mono-xsp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2015/04/06 08:17:17 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2015/05/23 06:28:58 kefren Exp $
 
 BUILDLINK_TREE+=	mono-xsp
 
@@ -10,7 +10,7 @@ BUILDLINK_ABI_DEPENDS.mono-xsp+=	mono-xsp>=2.10.2nb10
 BUILDLINK_PKGSRCDIR.mono-xsp?=	../../www/mono-xsp
 
 .include "../../databases/sqlite3/buildlink3.mk"
-.include "../../lang/mono2/buildlink3.mk"
+.include "../../lang/mono/buildlink3.mk"
 .endif # MONO_XSP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mono-xsp
