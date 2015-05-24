1b95cf35de737f08772a0bd8b45c262cdeb5aab8
jym <jym@pkgsrc.org>
2015-05-24 14:48:54 +0000

    - install SQL update scripts used by installer for upgrading DB schemas; - change rights for the spool, log and tmp directories from 0755 to 0750,   they contain sensitive information depending on configuration; - fix the default paths of potential mime.types files; - change config.inc.php to respect pkgsrc paths especially VARBASE;
    No regression expected. Bump rev.
    
    ok taca@.

---
+++ mail/roundcube/patches/patch-config.inc.php
@@ -0,0 +1,15 @@
+$NetBSD: patch-config.inc.php,v 1.1 2015/05/24 14:48:54 jym Exp $
+Add default paths for log, tmp and MIME types.
+--- config/config.inc.php.sample	2015-03-16 20:54:49.000000000 +0000
++++ config/config.inc.php.sample.18555.sample
+@@ -83,3 +83,10 @@ $config['plugins'] = array(
+ 
+ // skin name: folder from skins/
+ $config['skin'] = 'larry';
++
++// use this folder to store log files (must be writeable for apache user)
++// This is used by the 'file' log driver.
++$config['log_dir'] = '@VARBASE@/log/roundcube/';
++
++// use this folder to store temp files (must be writeable for apache user)
++$config['temp_dir'] = '@VARBASE@/tmp/roundcube/';
