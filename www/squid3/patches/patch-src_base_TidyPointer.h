3972f29403f5211c699a4c3686ebe90f6c2c445a
adam <adam>
2015-05-29 07:50:59 +0000

    Changes 3.5.5: * Portability: migrate auto_ptr to C++11 unique_ptr * Portability: Define nullptr if not provided * Cleanup: sync ModSelect.cc and ModSelectWin32.cc * Fix segmentation fault inside Adaptation::Icap::Xaction::swanSong * Fix "Not enough space to hold server hello message" error message * Bug 4132: regression in short_icon_urls with global_internal_static on * Prevent unused ssl_crtd helpers being run * Docs: fix debug output on https_port context failure * HTTP/2: publish RFC 7540 * Fix incorrect use of errno in various libcomm.la places * Bug 4236: SSL negotiation error of 'success' * Fix signal.h usage to resolve compiler warning * Bug 3930: assertion 'connIsUsable(http->getConn())' * Fix missing external ACL helper notes * Bug 4238: assertion Read.cc:205: "params.data == data" * Docs: remove 4.0-only info added by rev.13823 * comm_connect_addr on failures returns Comm:OK * Docs: shuffle SMP specific options to the top of squid.conf * CacheMgr: display 'client_db off' instead of 0 clients accessing cache * Fix assertion errorpage.cc:600: "entry->isEmpty()" * Fix assertion MemBuf.cc:380: "new_cap > (size_t) capacity" in SSL I/O buffer

--- www/squid3/patches/patch-src_base_TidyPointer.h
+++ www/squid3/patches/patch-src_base_TidyPointer.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_base_TidyPointer.h,v 1.1 2013/02/10 18:16:53 adam Exp $
+$NetBSD: patch-src_base_TidyPointer.h,v 1.2 2015/05/29 07:50:59 adam Exp $
 
 Define NULL if not defined.
 
---- src/base/TidyPointer.h.orig	2011-07-08 09:42:57.000000000 +0000
+--- src/base/TidyPointer.h.orig	2015-05-28 11:06:38.000000000 +0000
 +++ src/base/TidyPointer.h
-@@ -1,6 +1,10 @@
+@@ -9,6 +9,10 @@
  #ifndef SQUID_BASE_TIDYPOINTER_H
  #define SQUID_BASE_TIDYPOINTER_H
  
@@ -14,4 +14,4 @@ Define NULL if not defined.
 +
  /**
   * A pointer that deletes the object it points to when the pointer's owner or
-  * context is gone. Similar to std::auto_ptr but without confusing assignment
+  * context is gone. Similar to std::unique_ptr but without confusing assignment
