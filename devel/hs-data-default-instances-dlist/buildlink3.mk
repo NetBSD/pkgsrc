38f88f381cc4a39a54b3de41681d614643febd6b
szptvlfn <szptvlfn@pkgsrc.org>
2015-05-31 12:59:15 +0000

    Bump PKGREVISION for hs-dlist-0.7.1.1

--- devel/hs-data-default-instances-dlist/buildlink3.mk
+++ devel/hs-data-default-instances-dlist/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:38 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/05/31 12:59:15 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-data-default-instances-dlist
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-data-default-instances-dlist
 HS_DATA_DEFAULT_INSTANCES_DLIST_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-data-default-instances-dlist+=	hs-data-default-instances-dlist>=0.0.1
-BUILDLINK_ABI_DEPENDS.hs-data-default-instances-dlist+=	hs-data-default-instances-dlist>=0.0.1nb1
+BUILDLINK_ABI_DEPENDS.hs-data-default-instances-dlist+=	hs-data-default-instances-dlist>=0.0.1nb2
 BUILDLINK_PKGSRCDIR.hs-data-default-instances-dlist?=	../../devel/hs-data-default-instances-dlist
 
 .include "../../devel/hs-data-default-class/buildlink3.mk"
