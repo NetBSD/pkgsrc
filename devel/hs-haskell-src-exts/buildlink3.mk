8ccc1102a1422494d3ad7f5eccec6931ac17a8ce
szptvlfn <szptvlfn@pkgsrc.org>
2015-06-04 20:08:59 +0000

    Bump PKGREVISION for hs-text-1.2.1.1

--- devel/hs-haskell-src-exts/buildlink3.mk
+++ devel/hs-haskell-src-exts/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/01/28 18:46:53 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/06/04 20:08:59 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-haskell-src-exts
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-haskell-src-exts
 HS_HASKELL_SRC_EXTS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.16.0
-BUILDLINK_ABI_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.16.0.1
+BUILDLINK_ABI_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.16.0.1nb1
 BUILDLINK_PKGSRCDIR.hs-haskell-src-exts?=	../../devel/hs-haskell-src-exts
 
 .include "../../devel/hs-cpphs/buildlink3.mk"
