64bcae310e30fd12e4e61dab5dba379f3193adb3
szptvlfn <szptvlfn@pkgsrc.org>
2015-06-04 13:06:12 +0000

    Update to 0.10.13
    CHANGES:
    0.10.13
        #24, support Ratio in some places
        https://github.com/ndmitchell/cmdargs/issues/24
    0.10.12
        GHC 7.2 compatibility
    0.10.11
        #15, never put [brackets] around optional args in Explicit
        https://github.com/ndmitchell/cmdargs/issues/15

--- devel/hs-cmdargs/buildlink3.mk
+++ devel/hs-cmdargs/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2014/11/23 12:52:32 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2015/06/04 13:06:12 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cmdargs
 
 .if !defined(HS_CMDARGS_BUILDLINK3_MK)
 HS_CMDARGS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.10
-BUILDLINK_ABI_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.10nb1
+BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.13
+BUILDLINK_ABI_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.13
 BUILDLINK_PKGSRCDIR.hs-cmdargs?=	../../devel/hs-cmdargs
 
 .include "../../devel/hs-transformers/buildlink3.mk"
