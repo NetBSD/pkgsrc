1fa02f86a9d6e9b3bc1523afcaced6d30821f605
sevan <sevan@pkgsrc.org>
2015-06-04 15:48:45 +0000

    Introduce preliminary support for Bitrig to pkgsrc Initial patchset to add support by rodent@ Further adjustments made based on feedback by joerg@ Tested by myself with numerous bulkbuilds thanks to Patrick Wildt @ Bitrig
    Reviewed by bsiegert@ joerg@ wiz@

---
+++ mk/platform/Bitrig.mk
@@ -0,0 +1,75 @@
+# $NetBSD: Bitrig.mk,v 1.1 2015/06/04 15:48:45 sevan Exp $
+#
+# Variable definitions for the Bitrig operating system.
+
+ECHO_N?=	${ECHO} -n
+LDD?=		/usr/bin/ldd
+LDD_ENV?=	LD_TRACE_LOADED_OBJECTS_FMT1='\t-l%o => %p\n' \
+		LD_TRACE_LOADED_OBJECTS_FMT2=
+IMAKE_MAKE?=	${MAKE}		# program which gets invoked by imake
+PKGLOCALEDIR?=	share
+PS?=		/bin/ps
+SU?=		/usr/bin/su
+TYPE?=		type				# Shell builtin
+
+USERADD?=	/usr/sbin/useradd
+GROUPADD?=	/usr/sbin/groupadd
+
+CPP_PRECOMP_FLAGS?=	# unset
+DEF_UMASK?=		0022
+EXPORT_SYMBOLS_LDFLAGS?=-Wl,-E	# add symbols to the dynamic symbol table
+MOTIF_TYPE_DEFAULT?=	motif	# default 2.0 compatible libs type
+NOLOGIN?=		/sbin/nologin
+PKG_TOOLS_BIN?=		${LOCALBASE}/sbin
+ROOT_CMD?=		${SU} - root -c
+ROOT_USER?=		root
+ROOT_GROUP?=	wheel
+ULIMIT_CMD_datasize?=	ulimit -d `ulimit -H -d`
+ULIMIT_CMD_stacksize?=	ulimit -s `ulimit -H -s`
+ULIMIT_CMD_memorysize?=	ulimit -m `ulimit -H -m`
+
+X11_TYPE?=		native
+
+_OPSYS_SYSTEM_RPATH?=	/usr/lib
+_OPSYS_LIB_DIRS?=	/usr/lib
+_OPSYS_INCLUDE_DIRS?=	/usr/include
+
+.if exists(/usr/include/netinet6)
+_OPSYS_HAS_INET6=	yes	# IPv6 is standard
+.else
+_OPSYS_HAS_INET6=	no	# IPv6 is not standard
+.endif
+_OPSYS_HAS_JAVA=	no	# Java is not standard
+_OPSYS_HAS_MANZ=	yes	# MANZ controls gzipping of man pages
+_OPSYS_HAS_OSSAUDIO=	yes	# libossaudio is available
+_OPSYS_PERL_REQD=		# no base version of perl required
+_OPSYS_PTHREAD_AUTO=	no	# -lpthread needed for pthreads
+_OPSYS_SHLIB_TYPE=	ELF	# shared lib type
+_PATCH_CAN_BACKUP=	yes	# native patch(1) can make backups
+_PATCH_BACKUP_ARG?=	-V simple -z 	# switch to patch(1) for backup suffix
+_USE_RPATH=		yes	# add rpath to LDFLAGS
+
+# flags passed to the linker to extract all symbols from static archives.
+# this is GNU ld.
+_OPSYS_WHOLE_ARCHIVE_FLAG=	-Wl,--whole-archive
+_OPSYS_NO_WHOLE_ARCHIVE_FLAG=	-Wl,--no-whole-archive
+
+_STRIPFLAG_CC?=		${_INSTALL_UNSTRIPPED:D:U-s}	# cc(1) option to strip
+_STRIPFLAG_INSTALL?=	${_INSTALL_UNSTRIPPED:D:U-s}	# install(1) option to strip
+
+.if (${MACHINE_ARCH} == "amd64") || (${MACHINE_ARCH} == "arm")
+DEFAULT_SERIAL_DEVICE?=	/dev/tty00
+SERIAL_DEVICES?=	/dev/tty00 \
+			/dev/tty01
+.else
+DEFAULT_SERIAL_DEVICE?=	/dev/null
+SERIAL_DEVICES?=	/dev/null
+.endif
+
+PKG_HAVE_KQUEUE=	# defined
+
+_OPSYS_CAN_CHECK_SHLIBS=	no
+
+# check for maximum command line length and set it in configure's environment,
+# to avoid a test required by the libtool script that takes forever.
+_OPSYS_MAX_CMDLEN_CMD=	/sbin/sysctl -n kern.argmax
