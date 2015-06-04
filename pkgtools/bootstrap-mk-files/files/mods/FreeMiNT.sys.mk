ad9d81444f87119fdf17f7fc11e93175625e9d78
ryoon <ryoon>
2015-06-04 17:11:33 +0000

    Ride previous version bump.
    My previous commit is wrong.
    From share/mk/bsd.README of NetBSD src/:
    
    NOGCCERROR      If defined, prevents passing certain ${CFLAGS} to GCC
                    that cause warnings to be fatal, such as:
                            -Werror -Wa,--fatal-warnings
                    (The latter being for as(1).)
    
    In !defined(NOGCCERROR) case, -Werror should be passed to CFLAGS.
    This change may break many packages.

--- pkgtools/bootstrap-mk-files/files/mods/FreeMiNT.sys.mk
+++ pkgtools/bootstrap-mk-files/files/mods/FreeMiNT.sys.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: FreeMiNT.sys.mk,v 1.1 2013/09/08 16:27:23 ryoon Exp $
+#	$NetBSD: FreeMiNT.sys.mk,v 1.2 2015/06/04 17:11:33 ryoon Exp $
 #	NetBSD: sys.mk,v 1.58 2000/08/22 17:38:49 bjh21 Exp 
 #	@(#)sys.mk	8.2 (Berkeley) 3/21/94
 
@@ -8,7 +8,7 @@ OS!=		uname -s
 # At least Ubuntu 8.1 sets __attribute__((warn_unused_result)) on fwrite()
 # http://gcc.gnu.org/bugzilla/show_bug.cgi?id=25509
 .if !defined(NOGCCERROR)
-CFLAGS+= -Wno-error
+CFLAGS+= -Werror
 .endif
 
 .SUFFIXES: .out .a .ln .o .s .S .c .cc .cpp .cxx .C .F .f .r .y .l .cl .p .h
