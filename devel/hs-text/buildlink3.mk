3c454c7a4e6f36bc24e974d269022f389416b527
szptvlfn <szptvlfn@pkgsrc.org>
2015-06-04 20:07:57 +0000

    Update to 1.2.1.1
    changelog:
    1.2.1.1
    
    * Expose unpackCString#, which you should never use.
    
    1.2.1.0
    
    * Added Binary instances for both Text types. (If you have previously
      been using the text-binary package to get a Binary instance, it is
      now obsolete.)
    
    1.2.0.6
    
    * Fixed a space leak in UTF-8 decoding
    
    1.2.0.5
    
    * Feature parity: repeat, cycle, iterate are now implemented for lazy
      Text, and the Data instance is more complete
    
    * Build speed: an inliner space explosion has been fixed with toCaseFold
    
    * Bug fix: encoding Int to a Builder would infinite-loop if the
      integer-simple package was used
    
    * Deprecation: OnEncodeError and EncodeError are deprecated, as they
      are never used
    
    * Internals: some types that are used internally in fusion-related
      functions have moved around, been renamed, or been deleted (we don't
      bump the major version if .Internal modules change)
    
    * Spec compliance: toCaseFold now follows the Unicode 7.0 spec
      (updated from 6.3)
    
    1.2.0.4
    
    * Fixed an incompatibility with base < 4.5

--- devel/hs-text/buildlink3.mk
+++ devel/hs-text/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2014/12/12 22:53:35 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2015/06/04 20:07:57 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-text
 
 .if !defined(HS_TEXT_BUILDLINK3_MK)
 HS_TEXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=1.2.0
-BUILDLINK_ABI_DEPENDS.hs-text+=	hs-text>=1.2.0.3
+BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=1.2.1
+BUILDLINK_ABI_DEPENDS.hs-text+=	hs-text>=1.2.1.1
 BUILDLINK_PKGSRCDIR.hs-text?=	../../devel/hs-text
 .endif	# HS_TEXT_BUILDLINK3_MK
 
