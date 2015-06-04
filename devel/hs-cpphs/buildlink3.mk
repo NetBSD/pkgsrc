b1f3667397f5fe969b5ec9ad387076bd483f2513
szptvlfn <szptvlfn@pkgsrc.org>
2015-06-04 20:08:59 +0000

    Bump PKGREVISION for hs-text-1.2.1.1

--- devel/hs-cpphs/buildlink3.mk
+++ devel/hs-cpphs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2015/01/17 14:05:10 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2015/06/04 20:08:59 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cpphs
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-cpphs
 HS_CPPHS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.7
-BUILDLINK_ABI_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.7
+BUILDLINK_ABI_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.7nb1
 BUILDLINK_PKGSRCDIR.hs-cpphs?=	../../devel/hs-cpphs
 
 .include "../../textproc/hs-polyparse/buildlink3.mk"
