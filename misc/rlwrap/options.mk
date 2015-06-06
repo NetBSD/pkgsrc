d61ec567d3f8af1460f1b06a98d189d24dd82a2b
youri <youri>
2015-06-06 17:11:43 +0000

    PR/4997 from Leonardo Taccary, thanks!
    Update misc/rlwrap to rlwrap-0.42.
    
    Changes:
    0.42
    ----
     * Added --mirror-arguments (-U) option
     * On SunOS tcgetattr(slave pty) failed with "Invalid argument"
     * If the completion list contained two words, one of which a
       prefix of the other (e.g. "sea" and "seagull") the shorter one would
       be skipped when displaying a list of completions
     * reading completion files (with the -f option, or from
       $RLWRAP_HOME/<command>_completions) could fail with an incorrect
       ENOENT ("No such file or directory")
     * rlwrap -z listing wouldn't list filters
     * When both master and slave pty are unfit for sensing slave
       commands terminal settings, rlwrap now bails out with an error
       "cannot determine terminal mode of slave command"
    
    0.41
    ----
     * Slightly late SIGCHLD could cause an I/O error on master pty
     * Added -W (-polling) option to poll slave pty for changes in its
       interrupt character and ISIG flag.
     * if $TERM is not found in termcap/terminfo database, use vt100
    
    0.40
    ----
     * configure will now find tgetent() in libtinfo
     * compiling with gcc -O2 made rlwrap hang after CTRL-D on empty line
     * HP-UX 11 has weird tgetent() return values, confusing rlwrap
     * On Solaris, rlwrap would sometimes fail with "TIOCSWINSZ failed on
       slave pty"
     * Single quote ' is now word-breaking by default
     * multi-line edit would mangle upper ASCII and UTF-8 (and still does
       that to UTF16 :( )
     * added --extra-char-after-completion and -multi-line-ext options
     * rlwrap now recognises the 'rmcup' and 'rmkx' character sequences used by
       programs that use an alternate screen (like editors and pagers) to return
       from it.
     * configure will now correctly determine pty type on SCO OpenServer
     * rlwrap --no-children would leak file descriptors to /proc/<command_pid>/wchan
     * non-ASCII characters in multi-line input are no longer replaced
       by spaces after calling an external editor
     * running rlwrap within emacs would crash (division by zero).
       rlwrap now bails out whenever terminal width == 0
     * added --enable-proc-mountpoint config option to use alternate linux-like
       proc filesystems (like in FreeBSD) for finding <command>s working dir (-c option)
       and kernel function in which <command> is sleeping (-N option)
     * added prototype for copy_without_ignore_markers, fixing a segfault on NetBSD/amd64
     * commands final output before dying was lost on FreeBSD
     * Filters now get complete echo lines even if the echo comes back in chunks

--- misc/rlwrap/options.mk
+++ misc/rlwrap/options.mk
@@ -1,10 +1,9 @@
-# $NetBSD: options.mk,v 1.1 2008/01/09 13:40:38 heinz Exp $
+# $NetBSD: options.mk,v 1.2 2015/06/06 17:11:43 youri Exp $
 #
 PKG_OPTIONS_VAR=	PKG_OPTIONS.rlwrap
 PKG_SUPPORTED_OPTIONS=	debug
 PKG_SUGGESTED_OPTIONS=	# empty
 
-.include "../../mk/bsd.prefs.mk"
 .include "../../mk/bsd.options.mk"
 
 ###
