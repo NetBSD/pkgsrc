8f5613e6ba091e386ec3c0bbed5db3812640249b
youri <youri>
2015-06-07 14:14:04 +0000

    Update libsoup references in buildlink files.

--- geography/libchamplain08/buildlink3.mk
+++ geography/libchamplain08/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.30 2015/04/25 14:23:06 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2015/06/07 14:14:04 youri Exp $
 
 BUILDLINK_TREE+=	libchamplain08
 
@@ -13,7 +13,7 @@ BUILDLINK_PKGSRCDIR.libchamplain08?=	../../geography/libchamplain08
 .include "../../x11/gtk2/buildlink3.mk"
 .include "../../graphics/clutter-gtk0.10/buildlink3.mk"
 .include "../../databases/sqlite3/buildlink3.mk"
-.include "../../net/libsoup24/buildlink3.mk"
+.include "../../net/libsoup/buildlink3.mk"
 
 #.include "../../geography/memphis/buildlink3.mk"
 .endif	# LIBCHAMPLAIN08_BUILDLINK3_MK
