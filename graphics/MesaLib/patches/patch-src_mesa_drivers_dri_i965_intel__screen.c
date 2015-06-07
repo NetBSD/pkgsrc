e315510387e954a7ddfdc83bcc6838b80d2e3c7b
tnn <tnn@pkgsrc.org>
2015-06-07 15:48:12 +0000

    Update to MesaLib-10.5.7.
    MesaLib-10.5.7 2015-06-07
    "The current release includes over a dozen nouveau fixes (mostly targeting
    nv30/nv40 era hardware), i965 patches and a crash fix common to all the
    gallium dri modules."
    
    MesaLib-10.5.6 2015-05-24
    "This time around we have fixes from all over mesa - core mesa, driver
    specific patches (i965, nouveau, freedreno), reentrancy improvements
    in Clover (OpenCL), GLX and EGL loader patches, and Darwin build fixes."

--- graphics/MesaLib/patches/patch-src_mesa_drivers_dri_i965_intel__screen.c
+++ graphics/MesaLib/patches/patch-src_mesa_drivers_dri_i965_intel__screen.c
@@ -1,37 +1,39 @@
-$NetBSD: patch-src_mesa_drivers_dri_i965_intel__screen.c,v 1.1 2015/04/25 11:19:18 tnn Exp $
+$NetBSD: patch-src_mesa_drivers_dri_i965_intel__screen.c,v 1.2 2015/06/07 15:48:12 tnn Exp $
 
 Fix build on older NetBSD w/o _SC_PHYS_PAGES.
 
---- src/mesa/drivers/dri/i965/intel_screen.c.orig	2015-03-28 18:20:39.000000000 +0000
+--- src/mesa/drivers/dri/i965/intel_screen.c.orig	2015-06-07 10:06:53.000000000 +0000
 +++ src/mesa/drivers/dri/i965/intel_screen.c
-@@ -42,6 +42,11 @@
+@@ -42,6 +42,10 @@
  #include "utils.h"
  #include "xmlpool.h"
  
-+#if defined(__NetBSD__)
-+#include <sys/param.h>
-+#include <sys/sysctl.h>
++#ifdef HAVE_SYS_SYSCTL_H
++# include <sys/sysctl.h>
 +#endif
 +
  static const __DRIconfigOptionsExtension brw_config_options = {
     .base = { __DRI_CONFIG_OPTIONS, 1 },
     .xml =
-@@ -829,7 +834,18 @@ brw_query_renderer_integer(__DRIscreen *
+@@ -829,6 +833,13 @@ brw_query_renderer_integer(__DRIscreen *
        const unsigned gpu_mappable_megabytes =
           (aper_size / (1024 * 1024)) * 3 / 4;
  
-+#if !defined(_SC_PHYS_PAGES) && defined(__NetBSD__)
-+      uint64_t physmem64;
-+      uint32_t pagesize;
-+      size_t len;
-+      int ret1 = sysctlbyname("hw.physmem64", &physmem64, &len, (void*)NULL, 0);
-+      int ret2 = sysctlbyname("hw.pagesize", &pagesize, &len, (void*)NULL, 0);
-+      const long system_memory_pages = \
-+        (ret1 == 0 && ret2 == 0 && physmem64 > 0 && pagesize > 0) ?
-+        (physmem64 / pagesize) : -1;
++#if defined(HW_PHYSMEM64)
++      int mib[2] = { CTL_HW, HW_PHYSMEM64 };
++      const uint64_t system_memory_bytes;
++      size_t len = sizeof(system_memory_bytes);
++      if (sysctl(mib, 2, &system_memory_bytes, &len, NULL, 0) != 0)
++         return -1;
 +#else
        const long system_memory_pages = sysconf(_SC_PHYS_PAGES);
-+#endif
        const long system_page_size = sysconf(_SC_PAGE_SIZE);
  
-       if (system_memory_pages <= 0 || system_page_size <= 0)
+@@ -837,6 +848,7 @@ brw_query_renderer_integer(__DRIscreen *
+ 
+       const uint64_t system_memory_bytes = (uint64_t) system_memory_pages
+          * (uint64_t) system_page_size;
++#endif
+ 
+       const unsigned system_memory_megabytes =
+          (unsigned) (system_memory_bytes / (1024 * 1024));
