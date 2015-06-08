c43e5bf1d4b88f05f05009300d80ab797f6fc534
joerg <joerg>
2015-06-08 00:44:46 +0000

    As the seemingly untested -Werror changes break the bootstrap at least on Linux, update libnbcompat to 20150615: - Assume a C89 environment and malloc, vprintf, memcpy, memset, memmove   to be present and working. - Use full prototypes for isblank, snprintf, asprintf and strdup.   This avoids GCC failing to do conflicts with the builtins.

--- pkgtools/libnbcompat/files/nbcompat/string.h
+++ pkgtools/libnbcompat/files/nbcompat/string.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: string.h,v 1.2 2008/04/29 05:46:08 martin Exp $	*/
+/*	$NetBSD: string.h,v 1.3 2015/06/08 00:44:46 joerg Exp $	*/
 
 /*-
  * Copyright (c) 2004 The NetBSD Foundation, Inc.
@@ -40,11 +40,7 @@
  * Declare functions and macros that may be missing in <string.h>.
  */
 
-#if !HAVE_MEMMOVE
-# define memmove(a,b,c)	bcopy((b),(a),(c))
-#endif
-
-#if !HAVE_STRDUP
+#if !HAVE_DECL_STRDUP
 char	*strdup(const char *);
 #endif
 
