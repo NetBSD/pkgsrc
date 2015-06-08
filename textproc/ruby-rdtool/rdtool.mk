891edacd8db6bde086ea964e0afabf4f55a4a7bf
joerg <joerg>
2015-06-08 13:45:25 +0000

    Fix DEPENDS spelling (hi taca!).

--- textproc/ruby-rdtool/rdtool.mk
+++ textproc/ruby-rdtool/rdtool.mk
@@ -1,10 +1,10 @@
-# $NetBSD: rdtool.mk,v 1.1 2015/06/04 13:53:32 taca Exp $
+# $NetBSD: rdtool.mk,v 1.2 2015/06/08 13:45:25 joerg Exp $
 
 .if !defined(RUBY_RDTOOL_MK)
 
 RUBY_RDTOOL_MK	:=
 
 RUBY_RD=	${PREFIX}/bin/rd2${RUBY_SUFFIX}
-BUILD_DENEDS+=	${RUBY_PKGPREFIX}-rdtool>=0.6.14:../../textproc/ruby-rdtool
+BUILD_DEPENDS+=	${RUBY_PKGPREFIX}-rdtool>=0.6.14:../../textproc/ruby-rdtool
 
 .endif # RUBY_RDTOOL_MK
