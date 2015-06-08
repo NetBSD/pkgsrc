51bc9452206f67959db904024285557ceb5b184e
szptvlfn <szptvlfn@pkgsrc.org>
2015-06-08 20:56:14 +0000

    Bump PKGREVISION for hs-hashable-1.2.3.2

--- devel/hs-vault/buildlink3.mk
+++ devel/hs-vault/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2015/06/04 20:08:59 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2015/06/08 20:56:15 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-vault
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-vault
 HS_VAULT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-vault+=	hs-vault>=0.3.0
-BUILDLINK_ABI_DEPENDS.hs-vault+=	hs-vault>=0.3.0.3nb5
+BUILDLINK_ABI_DEPENDS.hs-vault+=	hs-vault>=0.3.0.3nb6
 BUILDLINK_PKGSRCDIR.hs-vault?=	../../devel/hs-vault
 
 .include "../../devel/hs-hashable/buildlink3.mk"
