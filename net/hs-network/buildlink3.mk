cd56acd4fcd3f718df3b64407a086e1e9207a92e
szptvlfn <szptvlfn@pkgsrc.org>
2015-06-09 22:09:41 +0000

    Bump PKGREVISION for hs-parsec-3.1.9

--- net/hs-network/buildlink3.mk
+++ net/hs-network/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2015/06/04 20:08:59 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2015/06/09 22:09:41 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-network
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-network
 HS_NETWORK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=2.5.0
-BUILDLINK_ABI_DEPENDS.hs-network+=	hs-network>=2.5.0.0nb8
+BUILDLINK_ABI_DEPENDS.hs-network+=	hs-network>=2.5.0.0nb9
 BUILDLINK_PKGSRCDIR.hs-network?=	../../net/hs-network
 
 .include "../../textproc/hs-parsec/buildlink3.mk"
--- www/hs-HTTP/buildlink3.mk
+++ www/hs-HTTP/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2015/06/04 20:09:00 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2015/06/09 22:09:42 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-HTTP
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-HTTP
 HS_HTTP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.17
-BUILDLINK_ABI_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.17nb7
+BUILDLINK_ABI_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.17nb8
 BUILDLINK_PKGSRCDIR.hs-HTTP?=	../../www/hs-HTTP
 
 .include "../../devel/hs-mtl/buildlink3.mk"
