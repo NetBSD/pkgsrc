b75de540afa21f4176ff7d6eed4d1fd09ea385f4
ryoon <ryoon@pkgsrc.org>
2015-06-09 20:24:39 +0000

    Update to 2.25
    Changelog:
    Not available?
    As far as I know:
    * Add Andes NDS32 architecture support.
    * Improve Solaris support.

--- devel/binutils/patches/patch-ld_emulparams_elf__x86__64__obsd.sh
+++
@@ -1,7 +0,0 @@
-$NetBSD: patch-ld_emulparams_elf__x86__64__obsd.sh,v 1.1 2014/01/26 03:49:01 ryoon Exp $
-
---- ld/emulparams/elf_x86_64_obsd.sh.orig	2014-01-26 00:08:31.000000000 +0000
-+++ ld/emulparams/elf_x86_64_obsd.sh
-@@ -0,0 +1,2 @@
-+. ${srcdir}/emulparams/elf_x86_64.sh
-+. ${srcdir}/emulparams/elf_obsd.sh
