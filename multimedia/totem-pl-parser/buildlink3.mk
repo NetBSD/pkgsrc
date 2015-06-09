074361c9f644960cc14a60fc2a21d6b15a5fd247
joerg <joerg@pkgsrc.org>
2015-06-09 14:19:16 +0000

    Pass down libgcrypt.

--- multimedia/totem-pl-parser/buildlink3.mk
+++ multimedia/totem-pl-parser/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.35 2015/06/07 14:14:04 youri Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2015/06/09 14:19:16 joerg Exp $
 
 BUILDLINK_TREE+=	totem-pl-parser
 
@@ -10,9 +10,10 @@ BUILDLINK_ABI_DEPENDS.totem-pl-parser+=	totem-pl-parser>=3.4.5nb1
 BUILDLINK_PKGSRCDIR.totem-pl-parser?=	../../multimedia/totem-pl-parser
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../textproc/libxml2/buildlink3.mk"
 .include "../../mail/gmime/buildlink3.mk"
 .include "../../net/libsoup/buildlink3.mk"
+.include "../../security/libgcrypt/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
 .endif # TOTEM_PL_PARSER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-totem-pl-parser
