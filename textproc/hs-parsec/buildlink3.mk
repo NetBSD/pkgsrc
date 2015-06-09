169ce0aed63d34300394bda9205c9f40cefba895
szptvlfn <szptvlfn@pkgsrc.org>
2015-06-09 22:08:41 +0000

    Update to 3.1.9
    CHANGES:
    3.1.9
    
    - Many and various updates to documentation and package description (inlcuding
      the homepage links).
    - Add an 'Eq' instance for 'ParseError'
    - Fixed a regression from 3.1.6: 'runP' is again exported from module
      Text.Parsec.
    
    3.1.8
    
    - Fix a regression from 3.1.6 related to exports from the main module.

--- textproc/hs-parsec/buildlink3.mk
+++ textproc/hs-parsec/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2015/06/04 20:09:00 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2015/06/09 22:08:41 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-parsec
 
 .if !defined(HS_PARSEC_BUILDLINK3_MK)
 HS_PARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-parsec+=	hs-parsec>=3.1.7
-BUILDLINK_ABI_DEPENDS.hs-parsec+=	hs-parsec>=3.1.7nb4
+BUILDLINK_API_DEPENDS.hs-parsec+=	hs-parsec>=3.1.9
+BUILDLINK_ABI_DEPENDS.hs-parsec+=	hs-parsec>=3.1.9
 BUILDLINK_PKGSRCDIR.hs-parsec?=	../../textproc/hs-parsec
 
 .include "../../devel/hs-mtl/buildlink3.mk"
