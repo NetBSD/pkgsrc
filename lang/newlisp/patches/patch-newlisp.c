44a7f1849976ea63b947f8c3823a3ab3d1fc8e42
joerg <joerg@pkgsrc.org>
2015-06-10 14:59:43 +0000

    Fix readline detection and use.

--- lang/newlisp/patches/patch-newlisp.c
+++ lang/newlisp/patches/patch-newlisp.c
@@ -1,25 +1,51 @@
-$NetBSD: patch-newlisp.c,v 1.1 2013/07/18 12:03:52 joerg Exp $
+$NetBSD: patch-newlisp.c,v 1.2 2015/06/10 14:59:43 joerg Exp $
 
---- newlisp.c.orig	2013-07-18 09:58:06.000000000 +0000
+--- newlisp.c.orig	2014-04-08 14:02:27.000000000 +0000
 +++ newlisp.c
-@@ -886,7 +886,7 @@ if(errorReg && !isNil((CELL*)errorEvent-
+@@ -138,7 +138,7 @@ char preLoad[] = 
+     "(define (Class:Class) (cons (context) (args)))";
+ void printHelpText(void);
+ #ifdef READLINE
+-char ** newlisp_completion (char * text, int start, int end);
++char ** newlisp_completion (const char * text, int start, int end);
+ #endif
+ /* --------------------- globals -------------------------------------- */
+ 
+@@ -904,8 +904,8 @@ if(errorReg && !isNil((CELL*)errorEvent-
+ 
  #ifdef READLINE
  rl_readline_name = "newlisp";
- rl_attempted_completion_function = (CPPFunction *)newlisp_completion;
+-rl_attempted_completion_function = (CPPFunction *)newlisp_completion;
 -#if defined(LINUX) || defined(_BSD)
++rl_attempted_completion_function = newlisp_completion;
 +#if defined(LINUX)
  /* in Bash .inputrc put 'set blink-matching-paren on' */
  rl_set_paren_blink_timeout(300000); /* 300 ms */
  #endif
-@@ -965,11 +965,7 @@ while((name = primitive[list_index].name
+@@ -959,7 +959,7 @@ return 0;
+ #endif /* not LIBRARY */
+ 
+ #ifdef READLINE
+-char * command_generator(char * text, int state)
++char * command_generator(const char * text, int state)
+ {
+ static int list_index, len, clen;
+ char * name;
+@@ -984,15 +984,9 @@ while((name = primitive[list_index].name
  return ((char *)NULL);
  }
  
 -#ifdef _BSD
 -extern char **completion_matches PARAMS((char *, rl_compentry_func_t *));
 -#else
- char ** completion_matches(const char * text, CPFunction commands);
+-char ** completion_matches(const char * text, CPFunction commands);
 -#endif
- 
- char ** newlisp_completion (char * text, int start, int end)
+-
+-char ** newlisp_completion (char * text, int start, int end)
++char ** newlisp_completion (const char * text, int start, int end)
  {
+-return(completion_matches(text, (CPFunction *)command_generator));
++return(completion_matches(text, command_generator));
+ }
+ #endif /* READLINE */
+ 
