0a8a04a9689814e9cb7acb114555b62afd3289fd
joerg <joerg@pkgsrc.org>
2015-06-10 14:58:50 +0000

    Explicitly use local array type. Fix deletes on arrays. Bump revision.

--- lang/basic256/patches/patch-Interpreter.cpp
+++ lang/basic256/patches/patch-Interpreter.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-Interpreter.cpp,v 1.1 2013/05/04 12:48:33 joerg Exp $
+$NetBSD: patch-Interpreter.cpp,v 1.2 2015/06/10 14:58:50 joerg Exp $
 
 Avoid overlap with std::mutex.
 
---- Interpreter.cpp.orig	2013-05-04 00:26:28.000000000 +0000
+--- Interpreter.cpp.orig	2012-08-06 14:57:58.000000000 +0000
 +++ Interpreter.cpp
 @@ -69,6 +69,8 @@ using namespace std;
  QMutex keymutex;
@@ -13,3 +13,39 @@ Avoid overlap with std::mutex.
  extern QMutex mutex;
  extern QMutex debugmutex;
  extern QWaitCondition waitCond;
+@@ -2446,7 +2448,7 @@ Interpreter::execByteCode()
+ 					poly.drawPolygon(points, pairs);
+ 					poly.end();
+ 					if (!fastgraphics) waitForGraphics();
+-					delete points;
++					delete[] points;
+ 				}
+ 			} else {
+ 				errornum = ERROR_POLYARRAY;
+@@ -2487,7 +2489,7 @@ Interpreter::execByteCode()
+ 				poly.end();
+ 	
+ 				if (!fastgraphics) waitForGraphics();
+-				delete points;
++				delete[] points;
+ 			}
+ 		}
+ 		break;
+@@ -2538,7 +2540,7 @@ Interpreter::execByteCode()
+ 						poly.end();
+ 						if (!fastgraphics) waitForGraphics();
+ 
+-						delete points;
++						delete[] points;
+ 					}
+ 				}
+ 			} else {
+@@ -2608,7 +2610,7 @@ Interpreter::execByteCode()
+ 					poly.end();
+ 					
+ 					if (!fastgraphics) waitForGraphics();
+-					delete points;
++					delete[] points;
+ 				}
+ 			}
+ 		}
