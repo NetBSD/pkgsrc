d1d06c6d504ab0a351a00e4b987b3006b4bb0318
wiedi <wiedi@pkgsrc.org>
2015-06-10 01:31:51 +0000

    Update protobuf to 2.6.1
    Changelog:
    
    2014-10-20 version 2.6.1:
    
      C++
      * Added atomicops support for Solaris.
      * Released memory allocated by InitializeDefaultRepeatedFields() and
        GetEmptyString(). Some memory sanitizers reported them as memory leaks.
    
      Java
      * Updated DynamicMessage.setField() to handle repeated enum values
        correctly.
      * Fixed a bug that caused NullPointerException to be thrown when
        converting manually constructed FileDescriptorProto to
        FileDescriptor.
    
      Python
      * Fixed WhichOneof() to work with de-serialized protobuf messages.
      * Fixed a missing file problem of Python C++ implementation.
    
    2014-08-15 version 2.6.0:
    
      General
      * Added oneofs(unions) feature. Fields in the same oneof will share
        memory and at most one field can be set at the same time. Use the
        oneof keyword to define a oneof like:
          message SampleMessage {
            oneof test_oneof {
              string name = 4;
              YourMessage sub_message = 9;
            }
          }
      * Files, services, enums, messages, methods and enum values can be marked
        as deprecated now.
      * Added Support for list values, including lists of mesaages, when
        parsing text-formatted protos in C++ and Java.
          For example:  foo: [1, 2, 3]
    
      C++
      * Enhanced customization on TestFormat printing.
      * Added SwapFields() in reflection API to swap a subset of fields.
        Added SetAllocatedMessage() in reflection API.
      * Repeated primitive extensions are now packable. The
        [packed=true] option only affects serializers. Therefore, it is
        possible to switch a repeated extension field to packed format
        without breaking backwards-compatibility.
      * Various speed optimizations.
    
      Java
      * writeTo() method in ByteString can now write a substring to an
        output stream. Added endWith() method for ByteString.
      * ByteString and ByteBuffer are now supported in CodedInputStream
        and CodedOutputStream.
      * java_generate_equals_and_hash can now be used with the LITE_RUNTIME.
    
      Python
      * A new C++-backed extension module (aka "cpp api v2") that replaces the
        old ("cpp api v1") one.  Much faster than the pure Python code.  This one
        resolves many bugs and is recommended for general use over the
        pure Python when possible.
      * Descriptors now have enum_types_by_name and extension_types_by_name dict
        attributes.
      * Support for Python 3.

--- devel/protobuf/patches/patch-src_google_protobuf_stubs_atomicops.h
+++ devel/protobuf/patches/patch-src_google_protobuf_stubs_atomicops.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_google_protobuf_stubs_atomicops.h,v 1.1 2015/02/19 14:12:33 joerg Exp $
+$NetBSD: patch-src_google_protobuf_stubs_atomicops.h,v 1.2 2015/06/10 01:31:51 wiedi Exp $
 
---- src/google/protobuf/stubs/atomicops.h.orig	2015-02-09 22:44:41.000000000 +0000
+--- src/google/protobuf/stubs/atomicops.h.orig	2014-10-21 00:01:40.000000000 +0000
 +++ src/google/protobuf/stubs/atomicops.h
-@@ -77,7 +77,11 @@ typedef intptr_t Atomic64;
+@@ -78,7 +78,11 @@ typedef intptr_t Atomic64;
  
  // Use AtomicWord for a machine-sized pointer.  It will use the Atomic32 or
  // Atomic64 routines below, depending on your architecture.
