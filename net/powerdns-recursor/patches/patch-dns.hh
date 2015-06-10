85ceabdd41eadf8b6f7c06d806e5f8972e03d67d
fhajny <fhajny@pkgsrc.org>
2015-06-10 14:22:29 +0000

    Update net/powerdns-recursor to 3.7.3. Add SMF support. Defuzz patches.
    PowerDNS Recursor 3.7.3
    - Limit the maximum length of a qname
    - pdnssec: check for glue and delegations in parent zones
    
    PowerDNS Recursor 3.7.2
    - Fix handling of forward references in label compressed packets;
      fixes CVE-2015-1868.
    - Minor improvements and bugfixes.
    
    PowerDNS Recursor 3.7.1
    - New root-nx-trust flag makes PowerDNS generalize NXDOMAIN responses
      from the root-servers
    - getregisteredname() for Lua, which turns 'www.bbc.co.uk' into 'bbc.co.uk'
    - Lua preoutquery filter
    - Lua IP-based filter (ipfilter) before parsing packets
    - iputils class for Lua, to quickly process IP addresses and netmasks
      in their native format
    - getregisteredname function for Lua, to find the registered domain
      for a given name
    - Various new ringbuffers: top-servfail-remotes, top-largeanswer-remotes,
      top-servfail-queries
    - Minor improvements and bugfixes.
    
    PowerDNS Recursor 3.6.2
    - Minor improvements and bugfixes.
    
    PowerDNS Recursor 3.6.1
    - Fix for a crash under a specific sequence of packets.
    
    PowerDNS Recursor 3.6.0
    - Implement minimum-ttl-override config setting, plus runtime configurability
      via 'rec_control set-minimum-ttl'.
    - Lots of work on the JSON API, which is exposed via Aki Tuomi's 'yahttp'.
    - Lua modules can now use 'pdnslog(INFO..')
    - Adopt any-to-tcp feature to the recursor.
    - Implement built-in statistics dumper using the 'carbon' protocol, which
      is also understood by metronome (our mini-graphite). Use 'carbon-server',
      'carbon-ourname' and 'carbon-interval' settings.
    - New setting 'udp-truncation-threshold' to configure from how many bytes
      we should truncate. commit a09a8ce.
    - Proper support for CHaos class for CHAOS TXT queries.
    - Added support for Lua scripts to drop queries w/o further processing.
    - Kevin Holly added qtype statistics to recursor and rec_control.
    - Add support for include-files in configuration, also reload ACLs and zones
      defined in them.
    - Paulo Anes contributed server-down-max-fails which helps combat
      Recursive DNS based amplification attacks.
    - Implement "followCNAMERecords" feature in the Lua hooks.
    - Minor improvements and bugfixes.
    
    PowerDNS Recursor 3.5.3
    - This is a bugfix and performance update to 3.5.2. It brings serious
      performance improvements for dual stack users.
    
    PowerDNS Recursor 3.5.2
    - This is a stability and bugfix update to 3.5.1. It contains important
      fixes that improve operation for certain domains.
    
    PowerDNS Recursor 3.5.1
    - This is a stability and bugfix update to 3.5.
    
    PowerDNS Recursor 3.5
    - The local zone server now understands wildcards.
    - The Lua postresolve and nodata hooks.
    - A new feature, rec_control trace-regex allows the tracing of lookups
      for specific names
    - A new setting, export-etc-hosts-search-suffix, adds a configurable
      suffix to names imported from /etc/hosts
    - Minor improvements & bugfixes
    
    PowerDNS Recursor 3.3.1
    - Small number of important fixes, adds some memory usage statistics,
      but no new features

--- net/powerdns-recursor/patches/patch-dns.hh
+++ net/powerdns-recursor/patches/patch-dns.hh
@@ -1,18 +1,11 @@
-$NetBSD: patch-dns.hh,v 1.2 2012/08/07 23:31:03 marino Exp $
+$NetBSD: patch-dns.hh,v 1.3 2015/06/10 14:22:29 fhajny Exp $
 
 Only attempt to define byte order if unset.
-Add DragonFly support
 
---- dns.hh.orig	2012-08-07 23:18:28.526524000 +0000
+--- dns.hh.orig	2015-04-21 13:02:57.000000000 +0000
 +++ dns.hh
-@@ -176,12 +176,12 @@ enum  {
- #ifdef WIN32
- #define BYTE_ORDER 1
- #define LITTLE_ENDIAN 1
--#elif __FreeBSD__ || __APPLE__
-+#elif defined(__FreeBSD__) || defined(__APPLE__) || defined(__DragonFly__)
- #include <machine/endian.h>
- #elif __linux__
+@@ -149,7 +149,7 @@ struct EDNS0Record
+ #elif __linux__ || __GNU__
  # include <endian.h>
  
 -#else  // with thanks to <arpa/nameser.h> 
