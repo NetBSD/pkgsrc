4974279599f7f4dc6848e943710dab9bc14edf95
richard <richard@pkgsrc.org>
2015-06-12 18:42:25 +0000

    Update FindX11 patch and avoid isfinite redefinition for jsoncpp on SunOS Revbump as well...

--- devel/cmake/patches/patch-Modules_FindX11.cmake
+++ devel/cmake/patches/patch-Modules_FindX11.cmake
@@ -1,26 +1,24 @@
-$NetBSD: patch-Modules_FindX11.cmake,v 1.6 2015/01/17 13:29:55 adam Exp $
+$NetBSD: patch-Modules_FindX11.cmake,v 1.7 2015/06/12 18:42:25 richard Exp $
 
 Patch in pkgsrc paths for finding X.
 
---- Modules/FindX11.cmake.orig	2014-12-15 20:07:43.000000000 +0000
+--- Modules/FindX11.cmake.orig	2015-06-01 15:56:08.000000000 +0000
 +++ Modules/FindX11.cmake
-@@ -73,7 +73,8 @@ if (UNIX)
+@@ -73,7 +73,7 @@ if (UNIX)
    set(CMAKE_REQUIRED_QUIET_SAVE ${CMAKE_REQUIRED_QUIET})
    set(CMAKE_REQUIRED_QUIET ${X11_FIND_QUIETLY})
    set(X11_INC_SEARCH_PATH
 -    /usr/pkg/xorg/include
-+    @LOCALBASE@/xorg/include
-+    @X11BASE@/xorg/include
++    @X11BASE@/include
      /usr/X11R6/include
      /usr/X11R7/include
      /usr/include/X11
-@@ -84,7 +85,8 @@ if (UNIX)
+@@ -84,7 +84,7 @@ if (UNIX)
    )
  
    set(X11_LIB_SEARCH_PATH
 -    /usr/pkg/xorg/lib
-+    @LOCALBASE@/xorg/lib
-+    @X11BASE@/xorg/lib
++    @X11BASE@/lib
      /usr/X11R6/lib
      /usr/X11R7/lib
      /usr/openwin/lib
