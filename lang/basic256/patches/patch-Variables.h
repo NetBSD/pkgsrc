8c61ac0b5cc9dcf9189d17cc27b94c7e23b52374
joerg <joerg@pkgsrc.org>
2015-06-12 13:12:27 +0000

    Add forgotten patch.

---
+++ lang/basic256/patches/patch-Variables.h
@@ -0,0 +1,13 @@
+$NetBSD: patch-Variables.h,v 1.1 2015/06/12 13:12:27 joerg Exp $
+
+--- Variables.h.orig	2015-06-09 23:15:06.000000000 +0000
++++ Variables.h
+@@ -34,7 +34,7 @@ struct variable
+   union {
+     char *string;
+     double floatval; 
+-    array *arr;
++    ::array *arr;
+   } value;
+ };
+ 
