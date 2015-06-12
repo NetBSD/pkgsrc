3b31ad1ecbbbcc0eca1754987052b51a073ae263
wiz <wiz@pkgsrc.org>
2015-06-12 10:48:20 +0000

    Recursive PKGREVISION bump for all packages mentioning 'perl', having a PKGNAME of p5-*, or depending such a package, for perl-5.22.0.

--- devel/p5-EV/buildlink3.mk
+++ devel/p5-EV/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2010/04/14 02:19:27 hiramatsu Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/06/12 10:48:21 wiz Exp $
 
 BUILDLINK_TREE+=	p5-EV
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	p5-EV
 P5_EV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.p5-EV+=	p5-EV>=3.9
+BUILDLINK_ABI_DEPENDS.p5-EV?=	p5-EV>=4.18nb1
 BUILDLINK_PKGSRCDIR.p5-EV?=	../../devel/p5-EV
 
 .include "../../lang/perl5/buildlink3.mk"
