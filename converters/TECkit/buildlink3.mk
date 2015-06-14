81b221ce75630b9bee918083a1a464b518776d90
markd <markd@pkgsrc.org>
2015-06-14 10:01:41 +0000

    Update to 2.5.3
    Minor bugfixes. Addition of pkg-config file.

--- converters/TECkit/buildlink3.mk
+++ converters/TECkit/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:03 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/06/14 10:01:41 markd Exp $
 
 BUILDLINK_TREE+=	TECkit
 
 .if !defined(TECKIT_BUILDLINK3_MK)
 TECKIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.TECkit+=	TECkit>=2.5.1
+BUILDLINK_API_DEPENDS.TECkit+=	TECkit>=2.5.3
 BUILDLINK_PKGSRCDIR.TECkit?=	../../converters/TECkit
 
 .include "../../devel/zlib/buildlink3.mk"
