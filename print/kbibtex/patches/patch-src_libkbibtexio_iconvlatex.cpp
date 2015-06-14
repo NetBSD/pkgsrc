d381733413caa396ff795d213f202c041d1a15da
markd <markd@pkgsrc.org>
2015-06-14 12:50:00 +0000

    Update to texlive 2015.
    pdfTEX: Support JPEG Exif as well as JFIF; do not even emit a warning if
     \pdfinclusionerrorlevel is negative; sync with xpdf 3.04.
    LuaTEX: New library newtokenlib for scanning tokens; bug fixes in the normal
     random number generator and other places.
    XeTEX: Image handling fixes; xdvipdfmx binary looked for first as a sibling
     to xetex; internal XDV opcodes changed.
    MetaPost: New numbersystem binary; new Japanese-enabled upmpost and
     updvitomp programs, analogous to up*tex.
    Infrastructure: The fmtutil script has been reimplemented to read
     fmtutil.cnf on a per-tree basis, analogous to updmap. Web2C mktex* scripts
     (including mktexlsr, mktextfm, mktexpk) now prefer programs in their own
     directory, instead of always using the existing PATH.

--- print/kbibtex/patches/patch-src_libkbibtexio_iconvlatex.cpp
+++ print/kbibtex/patches/patch-src_libkbibtexio_iconvlatex.cpp
@@ -1,13 +1,13 @@
-$NetBSD: patch-src_libkbibtexio_iconvlatex.cpp,v 1.1 2012/03/22 07:06:25 markd Exp $
+$NetBSD: patch-src_libkbibtexio_iconvlatex.cpp,v 1.2 2015/06/14 12:50:00 markd Exp $
 
---- src/libkbibtexio/iconvlatex.cpp.orig	2011-11-20 20:36:55.000000000 +0000
+--- src/libkbibtexio/iconvlatex.cpp.orig	2014-05-19 18:28:50.000000000 +0000
 +++ src/libkbibtexio/iconvlatex.cpp
-@@ -57,7 +57,7 @@ IConvLaTeX::~IConvLaTeX()
- QByteArray IConvLaTeX::encode(const QString &input)
- {
-     QByteArray inputByteArray = input.toUtf8();
+@@ -62,7 +62,7 @@ QByteArray IConvLaTeX::encode(const QStr
+     /// Limit the size of the output buffer
+     /// by making an educated guess of its maximum size
+     size_t maxOutputByteArraySize = inputByteArray.size() * 4 + 1024;
 -#ifdef Q_WS_WIN
-+#if defined(Q_WS_WIN) || defined(Q_OS_NETBSD) 
++#if defined(Q_WS_WIN) || defined(Q_OS_NETBSD)
      /// iconv on Windows likes to have it as const char *
      const char *inputBuffer = inputByteArray.data();
  #else
