95d474bf3ef727329da8ed0f8b19bc5c384a5e9a
marino <marino@pkgsrc.org>
2015-06-15 09:46:27 +0000

    Import lang/gcc5-aux based on gcc-5.1
    This compiler package is not like the lang/gcc4* packages, but rather
    the lang/gcc-aux package which exists to bring GNAT, the Ada language
    compiler.  The lang/gcc-aux package is based on gcc-4.9, and this
    package is based on gcc-5.  This is the first gcc5 package in pkgsrc,
    and it does support C, C++, ObjC, and Fortran in addition to Ada thus
    it is very useful, but it does have a different purpose than other
    gcc ports.
    
    This port has had minimal testing.  I verified GNAT passes 100% of the
    testsuite on NetBSD 6.1/amd64, but it has not been tested on any
    NetBSD 5, 7.0 or 7.99 platform yet.  I don't have any hardware, so it
    will require using a VM or having others report failure/success.  Due
    to similarity with lang/gcc-aux, chances are good that it will build
    and function properly on other platforms.
    
    See http://gcc.gnu.org/gcc-5/changes.html for more information about
    improvements over the gcc-4.9 series.

---
+++ lang/gcc5-aux/options.mk
@@ -0,0 +1,125 @@
+# $NetBSD: options.mk,v 1.1 2015/06/15 09:46:27 marino Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.gcc5-aux
+PKG_SUPPORTED_OPTIONS=  fortran objc testsuite static bootstrap
+PKG_SUGGESTED_OPTIONS=  fortran objc 
+
+# disable nls for now (build error involving iconv)
+
+.include "../../mk/bsd.options.mk"
+
+
+# Bootstrap is essentially a maintainer option and it will ignore
+# all other options because it is intent on building a bootstrap compiler.
+
+
+#############################
+##  ADD LANGUAGE: Fortran  ##
+#############################
+
+.if empty(PKG_OPTIONS:Mfortran) || !empty(PKG_OPTIONS:Mbootstrap)
+EXTRA_CONFARGS+= --disable-libquadmath
+.else
+LANGS+= fortran
+APPLY_DIFFS+= fortran
+.if ${OPSYS} == NetBSD
+EXTRA_CONFARGS+= --disable-libquadmath
+.else
+EXTRA_CONFARGS+= --enable-libquadmath
+.endif
+.endif
+
+
+#################################
+##  ADD LANGUAGE: Objective-C  ##
+#################################
+
+.if !empty(PKG_OPTIONS:Mobjc) && empty(PKG_OPTIONS:Mbootstrap)
+LANGS+= objc
+.endif
+
+
+#########################
+##  TESTSUITE SUPPORT  ##
+#########################
+
+.if !empty(PKG_OPTIONS:Mtestsuite) && empty(PKG_OPTIONS:Mbootstrap)
+BUILD_DEPENDS+= dejagnu>=1.4:../../devel/dejagnu
+APPLY_DIFFS+= ada-testsuite
+APPLY_DIFFS+= cxx-testsuite
+APPLY_DIFFS+= gcc-testsuite
+.endif
+
+
+#################################
+##  NATIONAL LANGUAGE SUPPORT  ##
+#################################
+
+.if !empty(PKG_OPTIONS:Mnls) && empty(PKG_OPTIONS:Mbootstrap)
+USE_BUILTIN.iconv= no
+USE_TOOLS+= msgfmt
+EXTRA_CONFARGS+= --enable-nls
+EXTRA_CONFARGS+= --with-libiconv-prefix=${BUILDLINK_PREFIX.iconv}
+MY_MAKE_ENV+= ICONVPREFIX=${BUILDLINK_PREFIX.iconv}
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
+.else
+EXTRA_CONFARGS+= --disable-nls
+.endif
+
+
+###############################
+##  STATICALLY BUILT OPTION  ##
+###############################
+
+.if !empty(PKG_OPTIONS:Mstatic)
+STATIC_BUILD = yes
+.endif
+
+.if defined(STATIC_BUILD) && empty(PKG_OPTIONS:Mbootstrap)
+.  if ${OPSYS} == SunOS
+PKG_FAIL_REASON+= SunOS does not support static builds
+.  else
+EXTRA_CONFARGS+= --with-stage1-ldflags=-static
+.  endif
+.endif
+
+
+#################################
+##  BOOTSTRAP COMPILER OPTION  ##
+#################################
+
+# Solaris does not support static linking system libraries, so bootstraps
+# on SunOS require gmp&co to be built with gcc.  Also, OpenBSD x86-64
+# fails configuration during the linking of -lmpc -lmpfr -lgmp in a contest;
+# it complains of missing references in libm.  These libraries are built by
+# a different compiler (the base) so perhaps that's the problem.  Building
+# gmp&co in the tree allows the configure step to succeed on OpenBSD.
+
+.if ${OPSYS} == OpenBSD || ${OPSYS} == MirBSD || \
+   (${OPSYS} == SunOS && !empty(PKG_OPTIONS:Mbootstrap))
+.include "../../devel/gmp/inplace.mk"
+.include "../../math/mpcomplex/inplace.mk"
+.include "../../math/mpfr/inplace.mk"
+.else
+EXTRA_CONFARGS+= --with-gmp=${BUILDLINK_PREFIX.gmp}
+EXTRA_CONFARGS+= --with-mpfr=${BUILDLINK_PREFIX.mpfr}
+EXTRA_CONFARGS+= --with-mpc=${BUILDLINK_PREFIX.mpcomplex}
+.include "../../devel/gmp/buildlink3.mk"
+.include "../../math/mpfr/buildlink3.mk"
+.include "../../math/mpcomplex/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mbootstrap)
+EXTRA_CONFARGS+= --disable-shared --disable-lto
+EXTRA_CONFARGS+= --disable-libstdcxx-pch
+.  if ${OPSYS} != SunOS
+EXTRA_CONFARGS+= --with-stage1-ldflags=-static
+EXTRA_CONFARGS+= --with-boot-ldflags=-static
+EXTRA_CONFARGS+= --with-system-zlib
+.  endif
+.else
+EXTRA_CONFARGS+= --enable-shared
+EXTRA_CONFARGS+= --disable-bootstrap
+
+.endif
