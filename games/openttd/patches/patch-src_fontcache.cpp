c3485e2a104d8b047e6dedc0fce4079011214c48
dholland <dholland@pkgsrc.org>
2015-06-19 00:49:14 +0000

    fix joerg's patch to work with more fontconfig versions

--- games/openttd/patches/patch-src_fontcache.cpp
+++ games/openttd/patches/patch-src_fontcache.cpp
@@ -1,6 +1,10 @@
-$NetBSD: patch-src_fontcache.cpp,v 1.1 2014/12/11 22:10:04 joerg Exp $
+$NetBSD: patch-src_fontcache.cpp,v 1.2 2015/06/19 00:49:14 dholland Exp $
 
-bitmap.width and bitmap.rows are unsigned, so std::max needs unsigned 1 too.
+bitmap.width and bitmap.rows are unsigned in some versions of freetype
+(but not others) -- because C++ requires max() to be homogeneous with
+respect to sign, if it's unsigned the constant needs to be 1U, and if
+it's not, it needs not to be. Which becomes a problem. So, since none
+of this should ever be negative, force it all to unsigned.
 
 --- src/fontcache.cpp.orig	2014-12-11 21:17:16.000000000 +0000
 +++ src/fontcache.cpp
@@ -10,8 +14,8 @@ bitmap.width and bitmap.rows are unsigned, so std::max needs unsigned 1 too.
  	/* Add 1 pixel for the shadow on the medium font. Our sprite must be at least 1x1 pixel */
 -	int width  = max(1, slot->bitmap.width + (this->fs == FS_NORMAL));
 -	int height = max(1, slot->bitmap.rows  + (this->fs == FS_NORMAL));
-+	int width  = max(1U, slot->bitmap.width + (this->fs == FS_NORMAL));
-+	int height = max(1U, slot->bitmap.rows  + (this->fs == FS_NORMAL));
++	int width  = max(1U, (unsigned)(slot->bitmap.width + (this->fs == FS_NORMAL)));
++	int height = max(1U, (unsigned)(slot->bitmap.rows  + (this->fs == FS_NORMAL)));
  
  	/* Limit glyph size to prevent overflows later on. */
  	if (width > 256 || height > 256) usererror("Font glyph is too large");
