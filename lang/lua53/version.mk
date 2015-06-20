b0d2103749f11141f14a3c47a939376e948346e5
alnsn <alnsn@pkgsrc.org>
2015-06-20 18:22:56 +0000

    Update lang/lua53 to 5.3.1.
    The following bugs have been fixed (from http://www.lua.org/bugs.html):
        1. string.format("%f") can cause a buffer overflow (only when
           'lua_Number' is long double!).
           reported by Roberto on 13 Jan 2015. existed since 5.3. fixed in
           5.3.1.
        2. debug.getlocal on a coroutine suspended in a hook can crash the
           interpreter.
           reported by on 11 Feb 2015. existed since 5.2. fixed in 5.3.1.
           Example: See
           http://lua-users.org/lists/lua-l/2015-02/msg00146.html.
        3. Suspended __le metamethod can give wrong result.
           reported by Eric Zhong on 07 Apr 2015. existed since 5.2. fixed in
           5.3.1.
           Example:
    mt = {__le = function (a,b) coroutine.yield("yield"); return a.x <= b.x end}
    t1 = setmetatable({x=1}, mt)
    t2 = {x=2}
    co = coroutine.wrap(function (a,b) return t2 <= t1 end)
    co()
    print(co())   --> true  (should be false)
        4. Return hook may not see correct values for active local variables
           when function returns.
           reported by Philipp Janda and Peng Yi on 19 May 2015. existed since
           5.0. fixed in 5.3.1.
           Example: See
           http://lua-users.org/lists/lua-l/2015-05/msg00376.html.

--- lang/lua53/version.mk
+++ lang/lua53/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.1.1.1 2015/01/17 17:50:23 alnsn Exp $
+# $NetBSD: version.mk,v 1.2 2015/06/20 18:22:56 alnsn Exp $
 
-LUA_VERSION=	5.3.0
+LUA_VERSION=	5.3.1
 
 LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
 LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
