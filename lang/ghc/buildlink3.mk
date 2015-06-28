325204a1bb57a4b8c0be0d63aec0e00665b6ab58
joerg <joerg@pkgsrc.org>
2015-06-28 16:32:25 +0000

    Explicitly restrict upper version, ghc and ghc7 have incompatible buildlink3.mk files.

--- lang/ghc/buildlink3.mk
+++ lang/ghc/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2012/05/07 01:53:41 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2015/06/28 16:32:25 joerg Exp $
 
 BUILDLINK_TREE+=	ghc
 
 .if !defined(GHC_BUILDLINK3_MK)
 GHC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ghc+=	ghc>=6.8.3
-BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=6.8.3nb2
+BUILDLINK_API_DEPENDS.ghc+=	ghc>=6.8.3<7
+BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=6.8.3nb2<7
 BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc
 
 BUILDLINK_DEPMETHOD.ghc?=	build
