b0ef51d90cc7ff11ce37cefb9c8b4606ceb60e12
ryoon <ryoon@pkgsrc.org>
2015-06-30 09:53:17 +0000

    Update to 1.4.0
    Changelog:
    2015-04-03 v1.4.0 "Indian Runner Duck"
      This release includes significant improvements to the VP9 codec.
    
      - Upgrading:
        This release is ABI incompatible with 1.3.0. It drops the compatibility
        layer, requiring VPX_IMG_FMT_* instead of IMG_FMT_*, and adds several codec
        controls for VP9.
    
      - Enhancements:
        Faster VP9 encoding and decoding
        Multithreaded VP9 decoding (tile and frame-based)
        Multithreaded VP9 encoding - on by default
        YUV 4:2:2 and 4:4:4 support in VP9
        10 and 12bit support in VP9
        64bit ARM support by replacing ARM assembly with intrinsics
    
      - Bug Fixes:
        Fixes a VP9 bitstream issue in Profile 1. This only affected non-YUV 4:2:0
        files.
    
      - Known Issues:
        Frame Parallel decoding fails for segmented and non-420 files.

--- multimedia/libvpx/buildlink3.mk
+++ multimedia/libvpx/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/01/31 11:53:51 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/06/30 09:53:17 ryoon Exp $
 
 BUILDLINK_TREE+=	libvpx
 
 .if !defined(LIBVPX_BUILDLINK3_MK)
 LIBVPX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libvpx+=	libvpx>=0.9.2
-BUILDLINK_ABI_DEPENDS.libvpx+=	libvpx>=1.0.0
+BUILDLINK_API_DEPENDS.libvpx+=	libvpx>=1.4.0
+BUILDLINK_ABI_DEPENDS.libvpx+=	libvpx>=1.4.0
 BUILDLINK_PKGSRCDIR.libvpx?=	../../multimedia/libvpx
 
 .include "../../mk/pthread.buildlink3.mk"
