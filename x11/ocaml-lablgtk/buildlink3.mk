be01dbe9af5f9d481372b9a743a577d3ae770716
jaapb <jaapb@pkgsrc.org>
2015-06-30 11:13:39 +0000

    This is not actually a version update, yet it is. The story: on the ocamlcore forge, the file downloaded doesn't depend on the filename, but on the number at the end of the URL. I forgot to update that last time around, so it was still downloading (and hence installing) 2.18.2 at that point. I have corrected this now - the version being installed is definitely 2.18.3. Have bumped the PKGREVISION to make sure things get reinstalled.

--- x11/ocaml-lablgtk/buildlink3.mk
+++ x11/ocaml-lablgtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2015/04/25 14:25:14 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2015/06/30 11:13:39 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lablgtk
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-lablgtk
 OCAML_LABLGTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-lablgtk+=	ocaml-lablgtk>=2.6.0nb3
-BUILDLINK_ABI_DEPENDS.ocaml-lablgtk+=	ocaml-lablgtk>=2.18.3nb3
+BUILDLINK_ABI_DEPENDS.ocaml-lablgtk+=	ocaml-lablgtk>=2.18.3nb4
 BUILDLINK_PKGSRCDIR.ocaml-lablgtk?=	../../x11/ocaml-lablgtk
 
 # gnome-panel must be exposed if and only if we built with it
