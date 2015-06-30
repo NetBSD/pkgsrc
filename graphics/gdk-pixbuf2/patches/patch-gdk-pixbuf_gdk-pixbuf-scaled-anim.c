993a1447c816f61d122beb2d860afedefe2cf4c7
ryoon <ryoon@pkgsrc.org>
2015-06-30 10:47:55 +0000

    Update to 2.30.8
    Changelog:
    2.30.8
    ======
    
    * Add missing nullability annotations
    
    * Documentation improvements
    
    * Translation updates
    
    2.30.7
    ======
    
    * Documentation fixes
    
    2.30.6
    ======
    
    * Add annotations to gdk_pixbuf_save_to_buffer
    
    * Win32 build fixes
    
    * Translation updates
    
    2.30.5
    ======
    
    * Convert documentation to markdown
    
    * Translation updates
    
    2.30.4
    ======
    
    * Make test suite robust against disabled formats
    
    * Fix test failure when using libpng without iTXt support
    
    * Translation updates

--- graphics/gdk-pixbuf2/patches/patch-gdk-pixbuf_gdk-pixbuf-scaled-anim.c
+++ graphics/gdk-pixbuf2/patches/patch-gdk-pixbuf_gdk-pixbuf-scaled-anim.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-gdk-pixbuf_gdk-pixbuf-scaled-anim.c,v 1.1 2013/08/26 14:38:56 richard Exp $
+$NetBSD: patch-gdk-pixbuf_gdk-pixbuf-scaled-anim.c,v 1.2 2015/06/30 10:47:55 ryoon Exp $
 
 * include config.h early to avoid redefinition warnings on solaris
 *
---- gdk-pixbuf/gdk-pixbuf-scaled-anim.c.orig	2012-07-31 23:45:20.000000000 +0000
+--- gdk-pixbuf/gdk-pixbuf-scaled-anim.c.orig	2014-03-06 04:36:45.000000000 +0000
 +++ gdk-pixbuf/gdk-pixbuf-scaled-anim.c
-@@ -21,7 +21,7 @@
-  * Boston, MA 02111-1307, USA.
+@@ -19,7 +19,7 @@
+  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
   *
   */
 -
