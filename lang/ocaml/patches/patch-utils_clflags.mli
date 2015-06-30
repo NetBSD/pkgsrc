779b465a6fe741dab2892af587421aad70495d13
jaapb <jaapb@pkgsrc.org>
2015-06-30 11:08:47 +0000

    Updated lang/ocaml to newest version 4.02.2. Changes apart from bugfixes include: - #6583: add a new class of binary operators with the same syntactic   precedence as method calls; these operators start with # followed   by a non-empty sequence of operator symbols (for instance #+, #!?).   It is also possible to use '#' as part of these extra symbols   (for instance ##, or #+#); this is rejected by the type-checker,   but can be used e.g. by ppx rewriters. * #6016: add a "nonrec" keyword for type declarations - #6600: make -short-paths faster by building the printing map   incrementally - #6642: replace $CAMLORIGIN in -ccopt with the path to cma or cmxa   to output an object file with included runtime and autolink libraries - #6845: -no-check-prims to tell ocamlc not to check primitives in runtime - G#149: Attach documentation comments to parse tree - G#159: Better locations for structure/signature items - #5958: generalized polymorphic #install_printer - #6237: explicit "infer" tag to control or disable menhir --infer - #6625: pass -linkpkg to files built with -output-obj. - #6712: Ignore common VCS directories - #6720: pass -g to C compilers when tag 'debug' is set - #6733: add .byte.so and .native.so targets to pass   -output-obj -cclib -shared. - #6733: "runtime_variant(X)" to pass -runtime-variant X option. - #6774: new menhir-specific flags "only_tokens" and "external_tokens(Foo)" - #6285: Add support for nanosecond precision in Unix.stat() - #6781: Add higher baud rates to Unix termios - #6834: Add Obj.{first,last}_non_constant_constructor_tag - #6078: Release the runtime system when calling caml_dlopen - #6675: GC hooks - #5418 (comments) : generate dependencies with $(CC) instead of gcc - #6266: Cross compilation for iOs, Android etc - Update instructions for x86-64 PIC mode and POWER architecture builds - #6452, G#140: add internal suport for custom printing formats - #6641: add -g, -ocamlcflags, -ocamloptflags options to ocamlmklib - #6693: also build libasmrun_shared.so and lib{asm,caml}run_pic.a - #6842: export Typemod.modtype_of_package - G#139: more versatile specification of locations of .annot - G#157: store the path of cmos inside debug section at link time - G#191: Making gc.h and some part of memory.h public

--- lang/ocaml/patches/patch-utils_clflags.mli
+++ lang/ocaml/patches/patch-utils_clflags.mli
@@ -1,13 +1,13 @@
-$NetBSD: patch-utils_clflags.mli,v 1.1 2015/01/20 14:12:25 jaapb Exp $
+$NetBSD: patch-utils_clflags.mli,v 1.2 2015/06/30 11:08:47 jaapb Exp $
 
 pkgsrc_runtime flag
---- utils/clflags.mli.orig	2014-08-28 16:24:52.000000000 +0000
+--- utils/clflags.mli.orig	2015-05-12 15:31:26.000000000 +0000
 +++ utils/clflags.mli
-@@ -90,6 +90,7 @@ val std_include_dir : unit -> string lis
+@@ -92,6 +92,7 @@ val std_include_dir : unit -> string lis
  val shared : bool ref
  val dlcode : bool ref
  val runtime_variant : string ref
 +val pkgsrc_runtime : bool ref
  val force_slash : bool ref
+ val keep_docs : bool ref
  val keep_locs : bool ref
- val unsafe_string : bool ref
