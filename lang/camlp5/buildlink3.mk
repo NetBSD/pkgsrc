cd4705f60dc9472d35ee3ec7bccec21b75c31c37
jaapb <jaapb@pkgsrc.org>
2015-06-30 11:10:15 +0000

    Updated lang/camlp5 to newest version 6.13 (to work with ocaml 4.02.2). There is one other change: * [20 Apr 15] Accept 'fun/try' case list like the change of 22nd Apr 13   on 'match' in revised syntax, allowing them to write "|" .. "end" or   "[" .. "]", both being accepted.

--- lang/camlp5/buildlink3.mk
+++ lang/camlp5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2015/01/20 14:24:36 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2015/06/30 11:10:15 jaapb Exp $
 #
 
 BUILDLINK_TREE+=	camlp5
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	camlp5
 CAMLP5_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.camlp5+=	camlp5>=5.01
-BUILDLINK_ABI_DEPENDS.camlp5+=	camlp5>=6.12nb1
+BUILDLINK_ABI_DEPENDS.camlp5+=	camlp5>=6.13
 BUILDLINK_PKGSRCDIR.camlp5?=	../../lang/camlp5
 
 .include "../../lang/ocaml/buildlink3.mk"
