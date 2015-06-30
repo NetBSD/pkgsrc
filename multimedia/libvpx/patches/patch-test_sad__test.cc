526541f609f9000aeb921841e5d4d7222a793425
ryoon <ryoon@pkgsrc.org>
2015-06-30 09:53:17 +0000

    Update to 1.4.0
    Changelog:
    2015-04-03 v1.4.0 "Indian Runner Duck"
      This release includes significant improvements to the VP9 codec.
    
      - Upgrading:
        This release is ABI incompatible with 1.3.0. It drops the compatibility
        layer, requiring VPX_IMG_FMT_* instead of IMG_FMT_*, and adds several codec
        controls for VP9.
    
      - Enhancements:
        Faster VP9 encoding and decoding
        Multithreaded VP9 decoding (tile and frame-based)
        Multithreaded VP9 encoding - on by default
        YUV 4:2:2 and 4:4:4 support in VP9
        10 and 12bit support in VP9
        64bit ARM support by replacing ARM assembly with intrinsics
    
      - Bug Fixes:
        Fixes a VP9 bitstream issue in Profile 1. This only affected non-YUV 4:2:0
        files.
    
      - Known Issues:
        Frame Parallel decoding fails for segmented and non-420 files.

--- multimedia/libvpx/patches/patch-test_sad__test.cc
+++ multimedia/libvpx/patches/patch-test_sad__test.cc
@@ -1,9 +1,54 @@
-$NetBSD: patch-test_sad__test.cc,v 1.2 2014/01/02 15:18:01 ryoon Exp $
+$NetBSD: patch-test_sad__test.cc,v 1.3 2015/06/30 09:53:17 ryoon Exp $
 
---- test/sad_test.cc.orig	2013-11-18 22:18:52.000000000 +0000
+--- test/sad_test.cc.orig	2015-04-03 18:49:19.000000000 +0000
 +++ test/sad_test.cc
-@@ -296,7 +296,11 @@ TEST_P(SADTest, MaxSAD) {
-   CheckSad(128);
+@@ -36,28 +36,44 @@ typedef unsigned int (*SadMxNFunc)(const
+                                    const unsigned char *reference_ptr,
+                                    int reference_stride,
+                                    unsigned int max_sad);
++#if __cplusplus >= 201103L
++typedef std::tuple<int, int, SadMxNFunc, int> SadMxNParam;
++#else
+ typedef std::tr1::tuple<int, int, SadMxNFunc, int> SadMxNParam;
+ #endif
++#endif
+ #if CONFIG_VP9_ENCODER
+ typedef unsigned int (*SadMxNVp9Func)(const unsigned char *source_ptr,
+                                       int source_stride,
+                                       const unsigned char *reference_ptr,
+                                       int reference_stride);
++#if __cplusplus >= 201103L
++typedef std::tuple<int, int, SadMxNVp9Func, int> SadMxNVp9Param;
++#else
+ typedef std::tr1::tuple<int, int, SadMxNVp9Func, int> SadMxNVp9Param;
++#endif
+ typedef uint32_t (*SadMxNAvgVp9Func)(const uint8_t *source_ptr,
+                                      int source_stride,
+                                      const uint8_t *reference_ptr,
+                                      int reference_stride,
+                                      const uint8_t *second_pred);
++#if __cplusplus >= 201103L
++typedef std::tuple<int, int, SadMxNAvgVp9Func, int> SadMxNAvgVp9Param;
++#else
+ typedef std::tr1::tuple<int, int, SadMxNAvgVp9Func, int> SadMxNAvgVp9Param;
+ #endif
++#endif
+ 
+ typedef void (*SadMxNx4Func)(const uint8_t *src_ptr,
+                              int src_stride,
+                              const uint8_t *const ref_ptr[],
+                              int ref_stride,
+                              uint32_t *sad_array);
++#if __cplusplus >= 201103L
++typedef std::tuple<int, int, SadMxNx4Func, int> SadMxNx4Param;
++#else
+ typedef std::tr1::tuple<int, int, SadMxNx4Func, int> SadMxNx4Param;
++#endif
+ 
+ using libvpx_test::ACMRandom;
+ 
+@@ -637,7 +653,11 @@ TEST_P(SADx4Test, SrcAlignedByWidth) {
+   source_data_ = tmp_source_data;
  }
  
 +#if __cplusplus >= 201103L
@@ -12,5 +57,5 @@ $NetBSD: patch-test_sad__test.cc,v 1.2 2014/01/02 15:18:01 ryoon Exp $
  using std::tr1::make_tuple;
 +#endif
  
- #if CONFIG_VP8_ENCODER
- const sad_m_by_n_fn_t sad_16x16_c = vp8_sad16x16_c;
+ //------------------------------------------------------------------------------
+ // C functions
