0e394c6a2515577978afbdb61236194379a230f8
fhajny <fhajny@pkgsrc.org>
2015-07-03 20:40:59 +0000

    Add support for building with GCC on SunOS, largely based on openjdk7 patches by jesse et al. Fixes build on SunOS.

--- lang/openjdk8/patches/patch-hotspot_src_os_posix_vm_os__posix.cpp
+++ lang/openjdk8/patches/patch-hotspot_src_os_posix_vm_os__posix.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-hotspot_src_os_posix_vm_os__posix.cpp,v 1.2 2015/06/10 11:38:51 tnn Exp $
+$NetBSD: patch-hotspot_src_os_posix_vm_os__posix.cpp,v 1.3 2015/07/03 20:40:59 fhajny Exp $
 
 rlim_t fixes.
 
---- hotspot/src/os/posix/vm/os_posix.cpp.orig	2015-06-09 13:49:55.000000000 +0000
+--- hotspot/src/os/posix/vm/os_posix.cpp.orig	2015-06-10 10:31:52.000000000 +0000
 +++ hotspot/src/os/posix/vm/os_posix.cpp
-@@ -180,12 +180,12 @@ void os::Posix::print_rlimit_info(output
+@@ -180,25 +180,25 @@ void os::Posix::print_rlimit_info(output
  
    st->print(" STACK ");
    getrlimit(RLIMIT_STACK, &rlim);
@@ -19,7 +19,13 @@ rlim_t fixes.
    else st->print("%uk", rlim.rlim_cur >> 10);
  
    // Isn't there on solaris
-@@ -198,7 +198,7 @@ void os::Posix::print_rlimit_info(output
+ #if !defined(TARGET_OS_FAMILY_solaris) && !defined(TARGET_OS_FAMILY_aix)
+   st->print(", NPROC ");
+   getrlimit(RLIMIT_NPROC, &rlim);
+-  if (rlim.rlim_cur == RLIM_INFINITY) st->print("infinity");
++  if (rlim.rlim_cur == (rlim_t)RLIM_INFINITY) st->print("infinity");
+   else st->print("%d", rlim.rlim_cur);
+ #endif
  
    st->print(", NOFILE ");
    getrlimit(RLIMIT_NOFILE, &rlim);
