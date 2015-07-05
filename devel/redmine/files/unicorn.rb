2a66ccd096da591841531ac57a7c35ab83401165
rodent <rodent@pkgsrc.org>
2015-07-05 23:57:39 +0000

    s/redmine/ruby-redmine and collate. Remove the redmine package, as it was moved to ruby-redmine.

--- devel/redmine/files/unicorn.rb
+++
@@ -1,8 +0,0 @@
-working_directory "@PREFIX@/share/redmine/app"
-pid "@PREFIX@/share/redmine/unicorn.pid"
-stderr_path "@PREFIX@/share/redmine/unicorn.log"
-stdout_path "@PREFIX@/share/redmine/unicorn.log"
- 
-worker_processes 3
-listen "@PREFIX@/share/redmine/unicorn.redmine.sock"
-timeout 60
