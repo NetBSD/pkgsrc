1eaa141e5c2339c09b3dd0ba2e0a73c2f9ae1c6f
richard <richard@pkgsrc.org>
2015-07-06 10:39:35 +0000

    SunOS doesn't currently define SUSPEND or HIBERNATE, so be careful to only SUSPEND or HIBERNATE when actually defined.

--- x11/xfce4-session/patches/patch-xfsm-shutdown-helper_main.c
+++ x11/xfce4-session/patches/patch-xfsm-shutdown-helper_main.c
@@ -1,6 +1,8 @@
-$NetBSD: patch-xfsm-shutdown-helper_main.c,v 1.1 2015/07/02 13:15:58 youri Exp $
+$NetBSD: patch-xfsm-shutdown-helper_main.c,v 1.2 2015/07/06 10:39:35 richard Exp $
 
 Add NetBSD commands.
+... and be careful to only SUSPEND or HIBERNATE when defined.
+
 --- xfsm-shutdown-helper/main.c.orig	2014-08-28 15:52:27.000000000 +0000
 +++ xfsm-shutdown-helper/main.c
 @@ -91,7 +91,10 @@
@@ -15,3 +17,30 @@ Add NetBSD commands.
  
  static gboolean
  run (const gchar *command)
+@@ -217,22 +220,26 @@ main (int argc, char **argv)
+     }
+   else if(suspend)
+     {
++#if defined(UP_BACKEND_SUSPEND_COMMAND)
+       if (run (UP_BACKEND_SUSPEND_COMMAND))
+           {
+             return EXIT_CODE_SUCCESS;
+           }
+         else
++#endif
+           {
+             return EXIT_CODE_FAILED;
+           }
+     }
+   else if(hibernate)
+     {
++#if defined (UP_BACKEND_HIBERNATE_COMMAND)
+       if (run (UP_BACKEND_HIBERNATE_COMMAND))
+           {
+             return EXIT_CODE_SUCCESS;
+           }
+         else
++#endif
+           {
+             return EXIT_CODE_FAILED;
+           }
