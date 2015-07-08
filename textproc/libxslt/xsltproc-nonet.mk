2c2f88686493fe6f5d99de3d61a719242dea6996
richard <richard>
2015-07-08 10:34:46 +0000

    Add docbook-xml and docbook-xsl to avoid nonet load failures as well as add to xsltproc-nonet.mk a variable XSLTPROC_PATH allowing packages to specify where to find locally files such as dtds, avoiding warnings like 'warning: failed to load external entity'.
    At the same time add a BUILD_DEPENDS to libxslt for xsltproc-nonet.mk
    and bump PKGREVISION.

--- textproc/libxslt/xsltproc-nonet.mk
+++ textproc/libxslt/xsltproc-nonet.mk
@@ -1,9 +1,15 @@
-# $NetBSD: xsltproc-nonet.mk,v 1.1 2009/10/27 14:37:13 drochner Exp $
+# $NetBSD: xsltproc-nonet.mk,v 1.2 2015/07/08 10:34:46 richard Exp $
+
+BUILD_DEPENDS+=	libxslt-[0-9]*:../../textproc/libxslt
 
 pre-configure: create-xsltproc-wrapper
 
+# --nonet avoids network connections during builds
+# XLSTPROC_PATH allows any necessary files (e.g. dtds) to be found
+# locally in the paths provided with the default being '--path .'
 create-xsltproc-wrapper:
 	${PRINTF} "#! ${SH}\\n\
-	  ${LOCALBASE}/bin/xsltproc --nonet \$$*\\n\
+	${LOCALBASE}/bin/xsltproc --nonet \
+	--path ${XLSTPROC_PATH:U.:O:u:ts::Q} \$$*\\n\
 	" > ${BUILDLINK_DIR}/bin/xsltproc
 	${CHMOD} +x ${BUILDLINK_DIR}/bin/xsltproc
