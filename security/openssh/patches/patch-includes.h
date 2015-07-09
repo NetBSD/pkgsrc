52de5b77801046c5876ccbc7b3a9b959220426e4
taca <taca@pkgsrc.org>
2015-07-09 16:14:23 +0000

    Update openssh to 6.9.1 (OpenSSH 6.9p1) which contains security fix.
    pkgsrc change:
    
    * tcp_wrappers support was removed from release 6.7, but add it refering
      FreeBSD's ports.
    * hpn-patch is also based on FreeBSD's ports.
    
    
    Security
    --------
    
     * ssh(1): when forwarding X11 connections with ForwardX11Trusted=no,
       connections made after ForwardX11Timeout expired could be permitted
       and no longer subject to XSECURITY restrictions because of an
       ineffective timeout check in ssh(1) coupled with "fail open"
       behaviour in the X11 server when clients attempted connections with
       expired credentials. This problem was reported by Jann Horn.
    
     * ssh-agent(1): fix weakness of agent locking (ssh-add -x) to
       password guessing by implementing an increasing failure delay,
       storing a salted hash of the password rather than the password
       itself and using a timing-safe comparison function for verifying
       unlock attempts. This problem was reported by Ryan Castellucci.
    
    For more information, please refer release announce.
    
    	http://www.openssh.com/txt/release-6.9
    	http://www.openssh.com/txt/release-6.8
    	http://www.openssh.com/txt/release-6.7

--- security/openssh/patches/patch-includes.h
+++ security/openssh/patches/patch-includes.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-includes.h,v 1.2 2013/12/01 06:11:41 taca Exp $
+$NetBSD: patch-includes.h,v 1.3 2015/07/09 16:14:23 taca Exp $
 
 Interix support
 
---- includes.h.orig	2013-03-22 01:51:09.000000000 +0000
+--- includes.h.orig	2015-07-01 02:35:31.000000000 +0000
 +++ includes.h
-@@ -126,6 +126,10 @@
+@@ -127,6 +127,10 @@
  #ifdef HAVE_READPASSPHRASE_H
  # include <readpassphrase.h>
  #endif
