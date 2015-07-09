56e072c7659dbc99b03ece414643a56cbb0cf951
ryoon <ryoon@pkgsrc.org>
2015-07-09 14:13:51 +0000

    Import firefox38-38.1.0 as www/firefox38.
    Mozilla Firefox is a free, open-source and cross-platform web browser
    for Windows, Linux, MacOS X and many other operating systems.
    
    It is fast and easy to use, and offers many advantages over other web
    browsers, such as tabbed browsing and the ability to block pop-up
    windows.
    
    Firefox also offers excellent bookmark and history management, and it
    can be extended by developers using industry standards such as XML,
    CSS, JavaScript, C++, etc. Many extensions are available.
    
    This package tracks 38 ESR.

---
+++ www/firefox38/patches/patch-build_pgo_profileserver.py
@@ -0,0 +1,13 @@
+$NetBSD: patch-build_pgo_profileserver.py,v 1.1 2015/07/09 14:13:52 ryoon Exp $
+
+--- build/pgo/profileserver.py.orig	2014-03-15 05:19:09.000000000 +0000
++++ build/pgo/profileserver.py
+@@ -59,7 +59,7 @@ if __name__ == '__main__':
+       env["MOZ_JAR_LOG_FILE"] = os.path.abspath(jarlog)
+       print "jarlog: %s" % env["MOZ_JAR_LOG_FILE"]
+ 
+-    cmdargs = ["http://localhost:%d/index.html" % PORT]
++    cmdargs = ["http://127.0.0.1:%d/index.html" % PORT]
+     runner = FirefoxRunner(profile=profile,
+                            binary=build.get_binary_path(where="staged-package"),
+                            cmdargs=cmdargs,
