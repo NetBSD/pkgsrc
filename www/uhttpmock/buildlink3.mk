8b217bceb2bb15a22e0880dd53cf8d97d04edc5e
wiz <wiz@pkgsrc.org>
2015-07-10 09:05:06 +0000

    Import uhttpmock-0.4.0 as www/uhttpmock.
    uhttpmock is a project for mocking web service APIs which use HTTP
    or HTTPS. It provides a library, libuhttpmock, which implements
    recording and playback of HTTP request-response traces.

---
+++ www/uhttpmock/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/07/10 09:05:06 wiz Exp $
+
+BUILDLINK_TREE+=	uhttpmock
+
+.if !defined(UHTTPMOCK_BUILDLINK3_MK)
+UHTTPMOCK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.uhttpmock+=	uhttpmock>=0.4.0
+BUILDLINK_PKGSRCDIR.uhttpmock?=	../../www/uhttpmock
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../net/libsoup/buildlink3.mk"
+.endif	# UHTTPMOCK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-uhttpmock
