0bb36cfb7342a2bfbb2132e09122e3e1aa27f482
taca <taca@pkgsrc.org>
2015-07-11 00:31:01 +0000

    Update php56 to 5.6.11.
    10 Jul 2015, PHP 5.6.11
    
    - Core:
      . Fixed bug #69768 (escapeshell*() doesn't cater to !). (cmb)
      . Fixed bug #69703 (Use __builtin_clzl on PowerPC).
        (dja at axtens dot net, Kalle)
      . Fixed bug #69732 (can induce segmentation fault with basic php code).
        (Dmitry)
      . Fixed bug #69642 (Windows 10 reported as Windows 8).
        (Christian Wenz, Anatol Belski)
      . Fixed bug #69551 (parse_ini_file() and parse_ini_string() segmentation
        fault). (Christoph M. Becker)
      . Fixed bug #69781 (phpinfo() reports Professional Editions of Windows
        7/8/8.1/10 as "Business"). (Christian Wenz)
      . Fixed bug #69740 (finally in generator (yield) swallows exception in
        iteration). (Nikita)
      . Fixed bug #69835 (phpinfo() does not report many Windows SKUs).
        (Christian Wenz)
      . Fixed bug #69892 (Different arrays compare indentical due to integer key
        truncation). (Nikita)
      . Fixed bug #69874 (Can't set empty additional_headers for mail()), regression
        from fix to bug #68776. (Yasuo)
    
    - GD:
      . Fixed bug #61221 (imagegammacorrect function loses alpha channel). (cmb)
    
    - GMP:
      . Fixed bug #69803 (gmp_random_range() modifies second parameter if GMP
        number). (Nikita)
    
    - PCRE:
      . Fixed Bug #53823 (preg_replace: * qualifier on unicode replace garbles the
        string). (cmb)
      . Fixed bug #69864 (Segfault in preg_replace_callback) (cmb, ab)
    
    - PDO_pgsql:
      . Fixed bug #69752 (PDOStatement::execute() leaks memory with DML
        Statements when closeCuror() is u). (Philip Hofstetter)
      . Fixed bug #69362 (PDO-pgsql fails to connect if password contains a
        leading single quote). (Matteo)
      . Fixed bug #69344 (PDO PgSQL Incorrect binding numeric array with gaps).
        (Matteo)
    
    - SimpleXML:
      . Refactored the fix for bug #66084 (simplexml_load_string() mangles empty
        node name). (Christoph Michael Becker)
    
    - SPL:
      . Fixed bug #69737 (Segfault when SplMinHeap::compare produces fatal error).
        (Stas)
      . Fixed bug #67805 (SplFileObject setMaxLineLength). (Willian Gustavo Veiga).
      . Fixed bug #69970 (Use-after-free vulnerability in
        spl_recursive_it_move_forward_ex()). (Laruence)
    
    - Sqlite3:
      . Fixed bug #69972 (Use-after-free vulnerability in
        sqlite3SafetyCheckSickOrOk()). (Laruence)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.104 2015/07/11 00:30:11 taca Exp $
+# $NetBSD: phpversion.mk,v 1.105 2015/07/11 00:31:01 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -83,7 +83,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP54_VERSION=	5.4.43
 PHP55_VERSION=	5.5.27
-PHP56_VERSION=	5.6.10
+PHP56_VERSION=	5.6.11
 
 # Define initial release of major version.
 PHP54_RELDATE=	20120301
