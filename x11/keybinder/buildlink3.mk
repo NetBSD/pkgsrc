482fdbc5845e3dcdefb7b8c2b925bae2f85ba6a6
kamil <kamil@pkgsrc.org>
2015-07-11 11:13:28 +0000

    Add x11/{,lua-,py-}keybinder version 0.3.0
    The Keybinder package contains a library for registering global keyboard
    shortcuts.
    Keybinder works with GTK-based applications using the X Window System.
    
    The library contains:
    - A C library, libkeybinder
    - Python bindings, python-keybinder
    - An examples directory with programs in C, Lua, Python and Vala.
    
    There are: Python (py-keybinder) and Lua (lua-keybinder) bindings.
    
    Approved by <riastradh>
    
    Nobody complains to import it since 2015-04-08
    http://mail-index.netbsd.org/tech-pkg/2015/04/07/msg014589.html
    
    Originally packaged in pkgsrc-wip by myself.

---
+++ x11/keybinder/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/07/11 11:13:28 kamil Exp $
+
+BUILDLINK_TREE+=	keybinder
+
+.if !defined(KEYBINDER_BUILDLINK3_MK)
+KEYBINDER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.keybinder+=	keybinder>=0.3.0
+BUILDLINK_PKGSRCDIR.keybinder?=	../../x11/keybinder
+
+.include "../../devel/gobject-introspection/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# KEYBINDER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-keybinder
---
+++ x11/lua-keybinder/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/07/11 11:13:28 kamil Exp $
+
+BUILDLINK_TREE+=	lua-keybinder
+
+.if !defined(LUA_KEYBINDER_BUILDLINK3_MK)
+LUA_KEYBINDER_BUILDLINK3_MK:=
+
+.include "../../lang/lua/luaversion.mk"
+
+BUILDLINK_API_DEPENDS.lua-keybinder+=	${LUA_PKGPREFIX}-keybinder>=0.3.0
+BUILDLINK_PKGSRCDIR.lua-keybinder?=	../../x11/lua-keybinder
+
+.include "../../x11/keybinder/buildlink3.mk"
+.endif	# LUA_KEYBINDER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-lua-keybinder
