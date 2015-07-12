5d48077f30b402a838fc3ff84e152ca82c38f8cf
wiz <wiz@pkgsrc.org>
2015-07-12 00:44:46 +0000

    Update to 2.8 and new 4.0 webkit API. From Leonardo Taccari in pkgsrc-wip.
    2.8.1
    
    What’s new in the WebKitGTK+ 2.8.1 release?
    
        Handle keep-alive connections in GStreamer HTTP source element.
        Fix a crash in DOMObjectCache when a wrapped object owned by the cache is unreffed by the user.
        Fix rendering of drag and drop icon.
        Fix the build with REDIRECTED_XCOMPOSITE_WINDOW disabled in X11 platform.
        Fix the build with Wayland target enabled.
        Fix the build for HPPA.
    
    
    2.8
    
    Highlights of the WebKitGTK+ 2.8.0 release
    
        Initial gestures support.
        HTML5 notifications.
        User script messages.
        HTML5 color input.
        APNG support.
        Performance improvements.
        Playing audio notification signal.
        Web view background colors.
    
    and lots of changes in the 2.6 series; major changes in 2.6 were:
    
    Highlights of the WebKitGTK+ 2.6.0 release
    
        WebKit1 API has been removed.
        Switch to CMake build system.
        Binary version bump to make WebKit1 and WebKit2 parallel installable.
        Several API changes.
        The DOM bindings API has been split into stable and unstable parts.
        Support for browser plugins using GTK+3, leaving the GTK+2
        dependency optional for building a plugin process with support
        for GTK+2 plugins.
        HighDPI support for non-accelerated compositing contents.
        Dynamic user agent string depending on the site.
        User scripts API.

--- www/webkit-gtk/patches/patch-Source_WTF_wtf_MathExtras.h
+++ www/webkit-gtk/patches/patch-Source_WTF_wtf_MathExtras.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-Source_WTF_wtf_MathExtras.h,v 1.3 2014/09/16 11:56:13 jperkin Exp $
+$NetBSD: patch-Source_WTF_wtf_MathExtras.h,v 1.4 2015/07/12 00:44:46 wiz Exp $
 
 Avoid broken section for SunOS/gcc.
 
---- Source/WTF/wtf/MathExtras.h.orig	2014-08-25 12:50:32.000000000 +0000
+--- Source/WTF/wtf/MathExtras.h.orig	2015-03-23 08:08:04.000000000 +0000
 +++ Source/WTF/wtf/MathExtras.h
-@@ -76,7 +76,7 @@ inline double wtf_ceil(double x) { retur
+@@ -84,7 +84,7 @@ inline double wtf_ceil(double x) { retur
  
  #endif
  
