7eb2ae6af461ec3a2d86a89300b843422c4ef8c0
khorben <khorben@pkgsrc.org>
2015-07-14 00:02:34 +0000

    Revert part of the patch assuming strnstr(3) on NetBSD
    strnstr(3) was added to NetBSD -current but is not in netbsd-7 (or
    earlier releases). This patch was confirmed to still work on -current by
    wiz@ (7.99.19 amd64), and on netbsd-7 by myself (7.0_RC1, amd64).

--- www/webkit-gtk/patches/patch-Source_WTF_wtf_Platform.h
+++ www/webkit-gtk/patches/patch-Source_WTF_wtf_Platform.h
@@ -1,16 +1,7 @@
-$NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.5 2015/07/12 00:44:46 wiz Exp $
+$NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.6 2015/07/14 00:02:34 khorben Exp $
 
 --- Source/WTF/wtf/Platform.h.orig	2015-03-23 08:08:04.000000000 +0000
 +++ Source/WTF/wtf/Platform.h
-@@ -579,7 +579,7 @@
- #endif
- 
- #if !defined(HAVE_STRNSTR)
--#if OS(DARWIN) || (OS(FREEBSD) && !defined(__GLIBC__))
-+#if OS(DARWIN) || ((OS(FREEBSD) || OS(NETBSD)) && !defined(__GLIBC__))
- #define HAVE_STRNSTR 1
- #endif
- #endif
 @@ -632,6 +632,11 @@
  #define USE_SYSTEM_MALLOC 1
  #endif
