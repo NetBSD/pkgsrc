4f8c7a13adee7c84b85677ddc30d2e48abc7f813
ryoon <ryoon>
2015-07-16 12:03:36 +0000

    Import gcc5-5.1.0 as lang/gcc5.
    The GNU Compiler Collection (GCC) includes front ends for C, C++, Objective-C,
    Fortran, Java, and Go, as well as libraries for these languages (libstdc++,
    libgcj,...). This package provides the snapshot release of version 5.
    
    On NetBSD a working cabsl function must be present in libm to build gfortran.
    This has been added to -current on 2014/10/10 and to NetBSD7 on 2014/10/13.

---
+++ lang/gcc5/patches/patch-libcilkrts_runtime_os-unix.c
@@ -0,0 +1,33 @@
+$NetBSD: patch-libcilkrts_runtime_os-unix.c,v 1.1 2015/07/16 12:03:36 ryoon Exp $
+
+--- libcilkrts/runtime/os-unix.c.orig	2014-05-21 11:08:58.000000000 +0000
++++ libcilkrts/runtime/os-unix.c
+@@ -56,7 +56,9 @@
+     // Uses sysconf(_SC_NPROCESSORS_ONLN) in verbose output
+ #elif defined  __DragonFly__
+ // No additional include files
+-#elif defined  __FreeBSD__
++#elif defined  __FreeBSD__ 
++// No additional include files
++#elif defined  __NetBSD__ 
+ // No additional include files
+ #elif defined __CYGWIN__
+ // Cygwin on Windows - no additional include files
+@@ -376,7 +378,7 @@ COMMON_SYSDEP int __cilkrts_hardware_cpu
+     assert((unsigned)count == count);
+ 
+     return count;
+-#elif defined  __FreeBSD__ || defined __CYGWIN__ || defined __DragonFly__
++#elif defined  __FreeBSD__ || defined __CYGWIN__ || defined __DragonFly__ || defined __NetBSD__
+     int ncores = sysconf(_SC_NPROCESSORS_ONLN);
+ 
+     return ncores;
+@@ -400,7 +402,7 @@ COMMON_SYSDEP void __cilkrts_sleep(void)
+ 
+ COMMON_SYSDEP void __cilkrts_yield(void)
+ {
+-#if __APPLE__ || __FreeBSD__ || __VXWORKS__
++#if __APPLE__ || __FreeBSD__ || __NetBSD__ || __VXWORKS__
+     // On MacOS, call sched_yield to yield quantum.  I'm not sure why we
+     // don't do this on Linux also.
+     sched_yield();
---
+++ lang/gcc5/patches/patch-libdecnumber_decNumber.c
@@ -0,0 +1,13 @@
+$NetBSD: patch-libdecnumber_decNumber.c,v 1.1 2015/07/16 12:03:36 ryoon Exp $
+
+--- libdecnumber/decNumber.c.orig	2013-08-14 08:42:04.000000000 +0000
++++ libdecnumber/decNumber.c
+@@ -770,7 +770,7 @@ decNumber * decNumberAbs(decNumber *res,
+ decNumber * decNumberAdd(decNumber *res, const decNumber *lhs,
+ 			 const decNumber *rhs, decContext *set) {
+   uInt status=0;			/* accumulator */
+-  decAddOp(res, lhs, rhs, set, 0, &status);
++  decAddOp(res, lhs, rhs, set, (uByte)0, &status);
+   if (status!=0) decStatus(res, status, set);
+   #if DECCHECK
+   decCheckInexact(res, set);
