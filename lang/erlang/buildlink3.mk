4c9820cafb8ddda68d89bf9cdc402b45d5561d61
fhajny <fhajny@pkgsrc.org>
2015-07-18 08:55:16 +0000

    Update erlang to 18.0.
    - Starting from 18.0 Erlang/OTP is released under the APL 2.0
    - erts: The time functionality has been extended. This includes a new API
      for time, as well as "time warp" modes which alters the behavior when
      system time changes. You are strongly encouraged to use the new API
      instead of the old API based on erlang:now/0. erlang:now/0 has been
      deprecated since it is a scalability bottleneck.
    - erts: Beside the API changes and time warp modes a lot of scalability
      and performance improvements regarding time management has been made
    - erts: The previously introduced "eager check I/O" feature is now enabled
      by default.
    - erts/compiler: enhanced support for maps. Big maps new uses a HAMT
      (Hash Array Mapped Trie) representation internally which makes them more
      efficient. There is now also support for variables as map keys.
    - dialyzer: The -dialyzer() attribute can be used for suppressing warnings
      in a module by specifying functions or warning options.
    - ssl: Remove default support for SSL-3.0 and added padding check
      for TLS-1.0 due to the Poodle vulnerability.
    - ssl: Remove default support for RC4 cipher suites, as they are consider
      too weak.
    - stdlib: Allow maps for supervisor flags and child specs
    - stdlib: New functions in ets:
      - take/2. Works the same as ets:delete/2 but
                also returns the deleted object(s).
      - ets:update_counter/4 with a default object as
                argument
    
    Full changelog:
    
      http://www.erlang.org/download/otp_src_18.0.readme

--- lang/erlang/buildlink3.mk
+++ lang/erlang/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2015/04/28 14:34:20 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2015/07/18 08:55:16 fhajny Exp $
 
 BUILDLINK_TREE+=	erlang
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	erlang
 ERLANG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.erlang+=	erlang>=10.1
-BUILDLINK_ABI_DEPENDS.erlang+=	erlang>=17.5
+BUILDLINK_ABI_DEPENDS.erlang+=	erlang>=18.0
 BUILDLINK_PKGSRCDIR.erlang?=	../../lang/erlang
 
 pkgbase := erlang
