797b6f9807858cb7da61ac8590d6d86287534d6f
adam <adam>
2015-07-18 10:40:20 +0000

    Changes 0.16.2: Fixed several bugs detected by running American Fuzzy Lop

--- graphics/libraw/buildlink3.mk
+++ graphics/libraw/buildlink3.mk
@@ -1,15 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.5 2012/10/10 13:06:14 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2015/07/18 10:40:20 adam Exp $
 
 BUILDLINK_TREE+=	libraw
 
 .if !defined(LIBRAW_BUILDLINK3_MK)
 LIBRAW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libraw+=	libraw>=0.12.0
-BUILDLINK_ABI_DEPENDS.libraw+=	libraw>=0.14.6
+BUILDLINK_API_DEPENDS.libraw+=	libraw>=0.16.0
+BUILDLINK_ABI_DEPENDS.libraw+=	libraw>=0.16.0
 BUILDLINK_PKGSRCDIR.libraw?=	../../graphics/libraw
 
+.include "../../graphics/jasper/buildlink3.mk"
 .include "../../graphics/lcms2/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
 .endif	# LIBRAW_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libraw
