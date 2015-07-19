d91a1f6ab6284bb0e5c6a27efa9f4e1c8a550c63
kamil <kamil@pkgsrc.org>
2015-07-19 16:02:51 +0000

    Bump PKGREVISION to 1
    This change is for new devel/capstone 3.0.4

--- devel/radare2/buildlink3.mk
+++ devel/radare2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/07/03 16:05:02 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/07/19 16:02:51 kamil Exp $
 
 BUILDLINK_TREE+=	radare2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	radare2
 RADARE2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.radare2+=	radare2>=0.9.9
-BUILDLINK_ABI_DEPENDS.radare2?=	radare2>=0.9.9
+BUILDLINK_ABI_DEPENDS.radare2?=	radare2>=0.9.9nb1
 BUILDLINK_PKGSRCDIR.radare2?=	../../devel/radare2
 
 .endif	# RADARE2_BUILDLINK3_MK
