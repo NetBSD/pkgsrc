21ccbf1fde7512486d0b730ca28ec6b0b27e787c
wiz <wiz@pkgsrc.org>
2015-07-20 11:30:54 +0000

    Instead of having only lua.pc, provide both the versioned pkg-config file and the unversioned one.
    As discussed on tech-pkg.

--- lang/lua/buildlink3.mk
+++ lang/lua/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2013/10/30 06:18:09 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2015/07/20 11:30:54 wiz Exp $
 #
 # This is a fake buildlink3.mk file. It figures out the Lua version
 # and then includes the real buildlink3.mk file for that Lua version.
@@ -10,3 +10,12 @@
 .include "${LUA_PKGSRCDIR}/buildlink3.mk"
 
 BUILDLINK_PREFIX.lua=	${BUILDLINK_PREFIX.${LUA_PACKAGE}}
+
+.if !defined(LUA_BUILDLINK3_MK)
+LUA_BUILDLINK3_MK:=
+
+BUILDLINK_TARGETS+=     buildlink-bin-unversioned
+
+buildlink-bin-unversioned:
+	${LN} -s ${BUILDLINK_DIR}/lib/pkgconfig/lua-${_LUA_DOT_VERSION}.pc ${BUILDLINK_DIR}/lib/pkgconfig/lua.pc
+.endif # LUA_BUILDLINK3_MK
