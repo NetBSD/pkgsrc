e6151a19759b2acaa7c52cb9421d3143ddef37e8
markd <markd@pkgsrc.org>
2015-07-21 21:51:39 +0000

    Update tigervnc to 1.5.0
    TigerVNC 1.5.0 - Lots of changes have been made since
    the last release, but the highlights are:
    
      - IPv6 support in the servers
      - You can now have two passwords, one for full access and one for
        "view only".
      - syslog support in Xvnc
      - GnuTLS priority configuration
      - Performance fixes
      - You can now easily start more clients on OS X
      - More translations
    
    TigerVNC 1.4.3 - This release addresses the following issues:
    
      - Upstream patches applied to the underlying Xorg code base to
        mitigate CVE-2015-0255.
      - Fixes for performance regressions introduced in 1.4.0.
      - Character encoding of clipboard text send by Java viewer now
        strictly adheres to the RFB specification.
    
    TigerVNC 1.4.1 - This is release is in response to the recent Xorg
      Security Advisory. There are no known vulnerabilities in TigerVNC itself
      related to this advisory, however some of the changes to the Xorg
      codebase were not compatible with TigerVNC.
    
    TigerVNC 1.4.0 - Lots of changes have been made since
    the last release, but the highlights are:
    
     - Colour map (aka indexed, palette) mode is largely removed. TigerVNC
       is still compatible with other VNC implementations, but you can no
       longer run Xvnc in colour map mode.
     - Improvements to the keyboard handling both on the server and client
     - Support for newer Xorg versions in the server build
     - x0vncserver now supports XDamage for instant updates, making it
       slightly less useless
     - WinVNC now works in service mode on newer Windows versions
     - Better full screen mode for the Java client on OS X
     - man pages should now be up to date
     - Improved TLS implementation in the Java client
     - Lots and lots of cleanups and bug fixes

--- net/tigervnc/patches/patch-unix_xserver_hw_vnc_InputXKB.cc
+++ net/tigervnc/patches/patch-unix_xserver_hw_vnc_InputXKB.cc
@@ -1,97 +1,94 @@
-$NetBSD: patch-unix_xserver_hw_vnc_InputXKB.cc,v 1.1 2015/02/08 19:42:09 wiz Exp $
+$NetBSD: patch-unix_xserver_hw_vnc_InputXKB.cc,v 1.2 2015/07/21 21:51:39 markd Exp $
 
---- unix/xserver/hw/vnc/InputXKB.cc.orig	2013-05-30 14:53:40.000000000 +0000
-+++ unix/xserver/hw/vnc/InputXKB.cc
-@@ -232,10 +232,7 @@ void InputDevice::PrepareInputDevices(vo
- 
- unsigned InputDevice::getKeyboardState(void)
+--- unix/xserver/hw/vnc/InputXKB.c.orig	2015-07-11 13:00:36.000000000 +0000
++++ unix/xserver/hw/vnc/InputXKB.c
+@@ -212,7 +212,7 @@ unsigned vncGetKeyboardState(void)
  {
--	DeviceIntPtr master;
--
--	master = GetMaster(keyboardDev, KEYBOARD_OR_FLOAT);
--	return XkbStateFieldFromRec(&master->key->xkbInfo->state);
-+	return XkbStateFieldFromRec(&keyboardDev->master->key->xkbInfo->state);
+ 	DeviceIntPtr master;
+ 
+-	master = GetMaster(vncKeyboardDev, KEYBOARD_OR_FLOAT);
++	master = vncKeyboardDev->master;
+ 	return XkbStateFieldFromRec(&master->key->xkbInfo->state);
  }
  
- unsigned InputDevice::getLevelThreeMask(void)
-@@ -256,7 +253,7 @@ unsigned InputDevice::getLevelThreeMask(
+@@ -234,7 +234,7 @@ unsigned vncGetLevelThreeMask(void)
  			return 0;
  	}
  
--	xkb = GetMaster(keyboardDev, KEYBOARD_OR_FLOAT)->key->xkbInfo->desc;
-+	xkb = keyboardDev->master->key->xkbInfo->desc;
+-	xkb = GetMaster(vncKeyboardDev, KEYBOARD_OR_FLOAT)->key->xkbInfo->desc;
++	xkb = vncKeyboardDev->master->key->xkbInfo->desc;
  
  	act = XkbKeyActionPtr(xkb, keycode, state);
  	if (act == NULL)
-@@ -281,7 +278,7 @@ KeyCode InputDevice::pressShift(void)
+@@ -259,7 +259,7 @@ KeyCode vncPressShift(void)
  	if (state & ShiftMask)
  		return 0;
  
--	xkb = GetMaster(keyboardDev, KEYBOARD_OR_FLOAT)->key->xkbInfo->desc;
-+	xkb = keyboardDev->master->key->xkbInfo->desc;
+-	xkb = GetMaster(vncKeyboardDev, KEYBOARD_OR_FLOAT)->key->xkbInfo->desc;
++	xkb = vncKeyboardDev->master->key->xkbInfo->desc;
  	for (key = xkb->min_key_code; key <= xkb->max_key_code; key++) {
  		XkbAction *act;
  		unsigned char mask;
-@@ -318,7 +315,7 @@ std::list<KeyCode> InputDevice::releaseS
- 	if (!(state & ShiftMask))
- 		return keys;
+@@ -299,7 +299,7 @@ size_t vncReleaseShift(KeyCode *keys, si
  
--	master = GetMaster(keyboardDev, KEYBOARD_OR_FLOAT);
-+	master = keyboardDev->master;
+ 	count = 0;
+ 
+-	master = GetMaster(vncKeyboardDev, KEYBOARD_OR_FLOAT);
++	master = vncKeyboardDev->master;
  	xkb = master->key->xkbInfo->desc;
  	for (key = xkb->min_key_code; key <= xkb->max_key_code; key++) {
  		XkbAction *act;
-@@ -371,7 +368,7 @@ KeyCode InputDevice::pressLevelThree(voi
+@@ -355,7 +355,7 @@ KeyCode vncPressLevelThree(void)
  			return 0;
  	}
  
--	xkb = GetMaster(keyboardDev, KEYBOARD_OR_FLOAT)->key->xkbInfo->desc;
-+	xkb = keyboardDev->master->key->xkbInfo->desc;
+-	xkb = GetMaster(vncKeyboardDev, KEYBOARD_OR_FLOAT)->key->xkbInfo->desc;
++	xkb = vncKeyboardDev->master->key->xkbInfo->desc;
  
  	act = XkbKeyActionPtr(xkb, keycode, state);
  	if (act == NULL)
-@@ -399,7 +396,7 @@ std::list<KeyCode> InputDevice::releaseL
- 	if (!(state & mask))
- 		return keys;
+@@ -386,7 +386,7 @@ size_t vncReleaseLevelThree(KeyCode *key
  
--	master = GetMaster(keyboardDev, KEYBOARD_OR_FLOAT);
-+	master = keyboardDev->master;
+ 	count = 0;
+ 
+-	master = GetMaster(vncKeyboardDev, KEYBOARD_OR_FLOAT);
++	master = vncKeyboardDev->master;
  	xkb = master->key->xkbInfo->desc;
  	for (key = xkb->min_key_code; key <= xkb->max_key_code; key++) {
  		XkbAction *act;
-@@ -440,7 +437,7 @@ KeyCode InputDevice::keysymToKeycode(Key
+@@ -429,7 +429,7 @@ KeyCode vncKeysymToKeycode(KeySym keysym
  	if (new_state != NULL)
  		*new_state = state;
  
--	xkb = GetMaster(keyboardDev, KEYBOARD_OR_FLOAT)->key->xkbInfo->desc;
-+	xkb = keyboardDev->master->key->xkbInfo->desc;
+-	xkb = GetMaster(vncKeyboardDev, KEYBOARD_OR_FLOAT)->key->xkbInfo->desc;
++	xkb = vncKeyboardDev->master->key->xkbInfo->desc;
  	for (key = xkb->min_key_code; key <= xkb->max_key_code; key++) {
  		unsigned int state_out;
  		KeySym dummy;
-@@ -497,7 +494,7 @@ bool InputDevice::isLockModifier(KeyCode
+@@ -486,7 +486,7 @@ int vncIsLockModifier(KeyCode keycode, u
  	XkbDescPtr xkb;
  	XkbAction *act;
  
--	xkb = GetMaster(keyboardDev, KEYBOARD_OR_FLOAT)->key->xkbInfo->desc;
-+	xkb = keyboardDev->master->key->xkbInfo->desc;
+-	xkb = GetMaster(vncKeyboardDev, KEYBOARD_OR_FLOAT)->key->xkbInfo->desc;
++	xkb = vncKeyboardDev->master->key->xkbInfo->desc;
  
  	act = XkbKeyActionPtr(xkb, keycode, state);
  	if (act == NULL)
-@@ -535,7 +532,7 @@ bool InputDevice::isAffectedByNumLock(Ke
+@@ -524,7 +524,7 @@ int vncIsAffectedByNumLock(KeyCode keyco
  	if (numlock_keycode == 0)
- 		return false;
+ 		return 0;
  
--	xkb = GetMaster(keyboardDev, KEYBOARD_OR_FLOAT)->key->xkbInfo->desc;
-+	xkb = keyboardDev->master->key->xkbInfo->desc;
+-	xkb = GetMaster(vncKeyboardDev, KEYBOARD_OR_FLOAT)->key->xkbInfo->desc;
++	xkb = vncKeyboardDev->master->key->xkbInfo->desc;
  
  	act = XkbKeyActionPtr(xkb, numlock_keycode, state);
  	if (act == NULL)
-@@ -569,7 +566,7 @@ KeyCode InputDevice::addKeysym(KeySym ke
+@@ -558,7 +558,7 @@ KeyCode vncAddKeysym(KeySym keysym, unsi
  	KeySym *syms;
  	KeySym upper, lower;
  
--	master = GetMaster(keyboardDev, KEYBOARD_OR_FLOAT);
-+	master = keyboardDev->master;
+-	master = GetMaster(vncKeyboardDev, KEYBOARD_OR_FLOAT);
++	master = vncKeyboardDev->master;
  	xkb = master->key->xkbInfo->desc;
  	for (key = xkb->max_key_code; key >= xkb->min_key_code; key--) {
  		if (XkbKeyNumGroups(xkb, key) == 0)
