4a7ee7838e523d8951a8b8ef0c34cca8af4f134b
markd <markd@pkgsrc.org>
2015-07-23 20:54:54 +0000

    Add a pdf option to gimp package.

--- graphics/gimp/options.mk
+++ graphics/gimp/options.mk
@@ -1,12 +1,12 @@
-# $NetBSD: options.mk,v 1.5 2012/10/28 23:40:24 wiz Exp $
+# $NetBSD: options.mk,v 1.6 2015/07/23 20:54:54 markd Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gimp
-PKG_SUPPORTED_OPTIONS=	aalib mng svg
+PKG_SUPPORTED_OPTIONS=	aalib mng pdf svg
 PKG_SUGGESTED_OPTIONS=	mng svg
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		aalib mng svg
+PLIST_VARS+=		aalib mng pdf svg
 
 .if !empty(PKG_OPTIONS:Maalib)
 .include "../../graphics/aalib/buildlink3.mk"
@@ -16,6 +16,11 @@ PLIST.aalib=		yes
 .if !empty(PKG_OPTIONS:Mmng)
 .include "../../graphics/mng/buildlink3.mk"
 PLIST.mng=		yes
+.endif
+
+.if !empty(PKG_OPTIONS:Mpdf)
+.include "../../print/poppler-glib/buildlink3.mk"
+PLIST.pdf=		yes
 .endif
 
 .if !empty(PKG_OPTIONS:Msvg)
