80b0a99558ff3a76f90a937c7492bdd190a4e593
szptvlfn <szptvlfn@pkgsrc.org>
2015-07-25 21:58:55 +0000

    Update to 0.2.5.1
    Changes from
    https://github.com/tibbe/unordered-containers/blob/master/CHANGES.md
    ## 0.2.5.1 (2014-10-11)
    
     * Support base-4.8

--- devel/hs-unordered-containers/buildlink3.mk
+++ devel/hs-unordered-containers/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2015/06/08 20:56:14 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2015/07/25 21:58:55 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-unordered-containers
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-unordered-containers
 HS_UNORDERED_CONTAINERS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.5
-BUILDLINK_ABI_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.5.0nb6
+BUILDLINK_ABI_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.5.1
 BUILDLINK_PKGSRCDIR.hs-unordered-containers?=	../../devel/hs-unordered-containers
 
 .include "../../devel/hs-hashable/buildlink3.mk"
