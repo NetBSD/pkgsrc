43dce68cfa4ff02f1b0020df6e8205cc9cfd9422
szptvlfn <szptvlfn@pkgsrc.org>
2015-07-25 21:59:57 +0000

    Bump PKGREVISION for hs-unordered-containers-0.2.5.1

--- textproc/hs-texmath/buildlink3.mk
+++ textproc/hs-texmath/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2015/06/09 22:09:42 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2015/07/25 21:59:58 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-texmath
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-texmath
 HS_TEXMATH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.8
-BUILDLINK_ABI_DEPENDS.hs-texmath+=	hs-texmath>=0.8nb13
+BUILDLINK_ABI_DEPENDS.hs-texmath+=	hs-texmath>=0.8nb14
 BUILDLINK_PKGSRCDIR.hs-texmath?=	../../textproc/hs-texmath
 
 .include "../../devel/hs-mtl/buildlink3.mk"
