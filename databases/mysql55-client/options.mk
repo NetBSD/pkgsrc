b67ded7914427e157e3c540dc81cd4e1fe6f30cd
adam <adam@pkgsrc.org>
2015-07-30 14:36:34 +0000

    Changes 5.5.45: * Security Fix: Due to the LogJam issue (https://weakdh.org/), OpenSSL has changed the Diffie-Hellman key length parameters for openssl-1.0.1n and up. OpenSSL has provided a detailed explanation at http://openssl.org/news/secadv_20150611.txt. To adopt this change in MySQL, the key length used in vio/viosslfactories.c for creating Diffie-Hellman keys has been increased from 512 to 2,048 bits. * InnoDB: On Unix-like platforms, os_file_create_simple_no_error_handling_func and os_file_create_func opened files in different modes when innodb_flush_method was set to O_DIRECT. * InnoDB: An assertion was raised when InnoDB attempted to dereference a NULL foreign key object. * InnoDB: An index record was not found on rollback due to inconsistencies in the purge_node_t structure. * The Spencer regex library used for the REGEXP operator could be subject to heap overflow in some circumstances. * A buffer-overflow error could occur for mysqlslap during option parsing. * GROUP BY or ORDER BY on a CHAR(0) NOT NULL column could lead to a server exit. * mysql-systemd-start failed if datadir was set in /etc/my.cnf. * On OS X 10.10 (Yosemite), mysqld failed to start automatically. The startup item has been replaced with a launchd job, which enables the preference pane checkbox for automatic startup to work again.

--- databases/mysql55-client/options.mk
+++ databases/mysql55-client/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.8 2015/02/07 16:04:46 fhajny Exp $
+# $NetBSD: options.mk,v 1.9 2015/07/30 14:36:34 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mysql5
 
@@ -25,9 +25,9 @@ CMAKE_ARGS+=		-DENABLE_DTRACE=ON
 # http://sphinxsearch.com/docs/current.html#sphinxse-overview
 PLIST_VARS+=	sphinx
 .if !empty(PKG_OPTIONS:Msphinx) || make(distinfo) || make(makesum) || make(mdi)
-SPHINX_VER=	2.2.7
+SPHINX_VER=	2.2.9
 DISTFILES=	${DEFAULT_DISTFILES} sphinx-${SPHINX_VER}-release${EXTRACT_SUFX}
-SITES.sphinx-2.2.7-release.tar.gz=	http://sphinxsearch.com/files/
+SITES.sphinx-2.2.9-release.tar.gz=	http://sphinxsearch.com/files/
 .if !empty(PKGPATH:Mdatabases/mysql55-server)
 MESSAGE_SRC=	${PKGDIR}/MESSAGE ${PKGDIR}/MESSAGE.sphinx
 .endif
