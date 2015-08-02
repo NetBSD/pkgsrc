d8d38094e0f72b201234af56b7ad14db0742895b
szptvlfn <szptvlfn@pkgsrc.org>
2015-08-02 11:32:55 +0000

    Update to 1.11
    changelog:
    Changes in polyparse-1.11
    
        A fix for the Applicative/Monad/Functor classes rearrangement in ghc-7.10.*.
    
    Changes in polyparse-1.10
    
        A new basic text-accepting combinator "literal", accepts the given string, without lexing it into Haskell-like words ("isWord").
        A more correct implementation of "manyFinally".
        A new combinator "satisfyMsg" which has a string describing the predicate, in order to produce more informative error messages.

--- textproc/hs-polyparse/buildlink3.mk
+++ textproc/hs-polyparse/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2015/06/04 20:09:00 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2015/08/02 11:32:55 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-polyparse
 
 .if !defined(HS_POLYPARSE_BUILDLINK3_MK)
 HS_POLYPARSE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-polyparse+=	hs-polyparse>=1.9
-BUILDLINK_ABI_DEPENDS.hs-polyparse+=	hs-polyparse>=1.9nb5
+BUILDLINK_API_DEPENDS.hs-polyparse+=	hs-polyparse>=1.11
+BUILDLINK_ABI_DEPENDS.hs-polyparse+=	hs-polyparse>=1.11
 BUILDLINK_PKGSRCDIR.hs-polyparse?=	../../textproc/hs-polyparse
 
 .include "../../devel/hs-text/buildlink3.mk"
