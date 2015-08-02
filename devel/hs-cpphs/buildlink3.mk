32aeaa06aee731351615a24a31985778de743cdb
szptvlfn <szptvlfn@pkgsrc.org>
2015-08-02 11:33:26 +0000

    Update to 1.19.2
    CHANGELOG:
    Version 1.19
    ------------
      * expose more of the cpphs API
      * allow the static linking exception to the LGPL
      * (1.19.1): don't warn about trailing comments in #ifdefs
      * (1.19.2): fix build error
    
    Version 1.18
    ------------
      * (1.18.8): fix version number
      * (1.18.9): accept #if defined foo as well as #if defined(foo)

--- devel/hs-cpphs/buildlink3.mk
+++ devel/hs-cpphs/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2015/06/04 20:08:59 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2015/08/02 11:33:26 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cpphs
 
 .if !defined(HS_CPPHS_BUILDLINK3_MK)
 HS_CPPHS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.7
-BUILDLINK_ABI_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.7nb1
+BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.19.2
+BUILDLINK_ABI_DEPENDS.hs-cpphs+=	hs-cpphs>=1.19.2
 BUILDLINK_PKGSRCDIR.hs-cpphs?=	../../devel/hs-cpphs
 
 .include "../../textproc/hs-polyparse/buildlink3.mk"
