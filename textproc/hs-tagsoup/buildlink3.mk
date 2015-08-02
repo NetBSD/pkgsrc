cbf41b4ba484edca830a28b3f6b417772098fbcc
szptvlfn <szptvlfn>
2015-08-02 11:27:04 +0000

    Bump PKGREVISION for hs-text-1.2.1.3

--- textproc/hs-tagsoup/buildlink3.mk
+++ textproc/hs-tagsoup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2015/06/04 20:09:00 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2015/08/02 11:27:05 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-tagsoup
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-tagsoup
 HS_TAGSOUP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.1
-BUILDLINK_ABI_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.1nb5
+BUILDLINK_ABI_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.1nb6
 BUILDLINK_PKGSRCDIR.hs-tagsoup?=	../../textproc/hs-tagsoup
 
 .include "../../devel/hs-text/buildlink3.mk"
