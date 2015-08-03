2413b7203a316adb985b9c0c55b3736c400a0d48
jperkin <jperkin@pkgsrc.org>
2015-08-03 09:53:01 +0000

    Look for pkg-config files under share/pkgconfig, as found on EL6.

--- x11/xproto/builtin.mk
+++ x11/xproto/builtin.mk
@@ -1,8 +1,9 @@
-# $NetBSD: builtin.mk,v 1.5 2015/05/12 13:59:11 hiramatsu Exp $
+# $NetBSD: builtin.mk,v 1.6 2015/08/03 09:53:02 jperkin Exp $
 
 BUILTIN_PKG:=	xproto
 PKGCONFIG_FILE.xproto=	${X11BASE}/lib/pkgconfig/xproto.pc
 PKGCONFIG_FILE.xproto+=	${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/xproto.pc
+PKGCONFIG_FILE.xproto+=	${X11BASE}/share/pkgconfig/xproto.pc
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
