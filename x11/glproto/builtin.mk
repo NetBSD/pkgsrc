87ef878462d73f8308af64b2e0c4a2444e4c3008
jperkin <jperkin@pkgsrc.org>
2015-08-03 09:53:01 +0000

    Look for pkg-config files under share/pkgconfig, as found on EL6.

--- x11/glproto/builtin.mk
+++ x11/glproto/builtin.mk
@@ -1,8 +1,9 @@
-# $NetBSD: builtin.mk,v 1.3 2014/03/10 11:05:51 jperkin Exp $
+# $NetBSD: builtin.mk,v 1.4 2015/08/03 09:53:01 jperkin Exp $
 
 BUILTIN_PKG:=	glproto
 PKGCONFIG_FILE.glproto=		${X11BASE}/lib/pkgconfig/glproto.pc
 PKGCONFIG_FILE.glproto+=	${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/glproto.pc
+PKGCONFIG_FILE.glproto+=	${X11BASE}/share/pkgconfig/glproto.pc
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
--- x11/xproxymanagementprotocol/builtin.mk
+++ x11/xproxymanagementprotocol/builtin.mk
@@ -1,8 +1,9 @@
-# $NetBSD: builtin.mk,v 1.3 2014/03/10 11:05:54 jperkin Exp $
+# $NetBSD: builtin.mk,v 1.4 2015/08/03 09:53:02 jperkin Exp $
 
 BUILTIN_PKG:=	xproxymanagementprotocol
 PKGCONFIG_FILE.xproxymanagementprotocol=	${X11BASE}/lib/pkgconfig/xproxymngproto.pc
 PKGCONFIG_FILE.xproxymanagementprotocol+=	${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/xproxymngproto.pc
+PKGCONFIG_FILE.xproxymanagementprotocol+=	${X11BASE}/share/pkgconfig/xproxymngproto.pc
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
