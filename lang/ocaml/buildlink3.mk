4f1628fb1534d31dbcce7865c9be1a4309a422d7
jaapb <jaapb@pkgsrc.org>
2015-08-07 11:16:30 +0000

    This is an update of lang/ocaml to its latest version, 4.02.3. Changes include:
    Bug fixes:
    - Top-level custom printing for GADTs: interface change in 4.02.2
      (Grégoire Henry, report by Jeremy Yallop)
    - corrupted final_table (ygrek)
    - R#6926: Regression: ocamldoc lost unattached comment
      (Damien Doligez, report by François Bobot)
    - Aliased result type of GADT constructor results in assertion failure
      (Jacques Garrigue)
    
    Feature wishes:
    - PR#6691: install .cmt[i] files for stdlib and compiler-libs
      (David Sheets, request by Gabriel Radanne)
    - New primitive: caml_alloc_dummy_function
      (Hugo Heuzard)

--- lang/ocaml/buildlink3.mk
+++ lang/ocaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.32 2015/06/30 11:08:47 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.33 2015/08/07 11:16:30 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml
 OCAML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml+=	ocaml>=3.08.2
-BUILDLINK_ABI_DEPENDS.ocaml+=	ocaml>=4.02.2
+BUILDLINK_ABI_DEPENDS.ocaml+=	ocaml>=4.02.3
 BUILDLINK_PKGSRCDIR.ocaml?=	../../lang/ocaml
 #BUILDLINK_DEPMETHOD.ocaml?=	build
 
