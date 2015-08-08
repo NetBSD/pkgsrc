8814a3af89c13df5eae2a1b11e580bbbd078e3c4
taca <taca@pkgsrc.org>
2015-08-08 00:12:22 +0000

    Update php55 to 5.5.28.
    06 Aug 2015, PHP 5.5.28
    
    - Core:
      . Fixed bug #69793 (Remotely triggerable stack exhaustion via recursive
        method calls). (Stas)
      . Fixed bug #69892 (Different arrays compare indentical due to integer key
        truncation). (Nikita)
      . Fixed bug #70002 (TS issues with temporary dir handling). (Anatol)
      . Fixed bug #70121 (unserialize() could lead to unexpected methods execution
        / NULL pointer deref). (Stas)
    
    - OpenSSL:
      . Fixed bug #70014 (openssl_random_pseudo_bytes() is not cryptographically
        secure). (Stas)
    
    - Phar:
      . Improved fix for bug #69441. (Anatol Belski)
      . Fixed bug #70019 (Files extracted from archive may be placed outside of
        destination directory). (Anatol Belski)
    
    - SOAP:
      . Fixed bug #70081 (SoapClient info leak / null pointer dereference via
         multiple type confusions). (Stas)
    
    - SPL:
      . Fixed bug #70068 (Dangling pointer in the unserialization of ArrayObject
        items). (sean.heelan)
      . Fixed bug #70166 (Use After Free Vulnerability in unserialize() with
        SPLArrayObject). (taoguangchen at icloud dot com)
      . Fixed bug #70168 (Use After Free Vulnerability in unserialize() with
        SplObjectStorage). (taoguangchen at icloud dot com)
      . Fixed bug #70169 (Use After Free Vulnerability in unserialize() with
        SplDoublyLinkedList). (taoguangchen at icloud dot com)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.106 2015/08/08 00:11:29 taca Exp $
+# $NetBSD: phpversion.mk,v 1.107 2015/08/08 00:12:22 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -82,7 +82,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP54_VERSION=	5.4.44
-PHP55_VERSION=	5.5.27
+PHP55_VERSION=	5.5.28
 PHP56_VERSION=	5.6.11
 
 # Define initial release of major version.
