30a3eb38e315cfdde7c523db3ed01f8314d912b9
taca <taca@pkgsrc.org>
2015-08-08 00:11:29 +0000

    Update phpt54 to 5.4.44.
    06 Aug 2015 PHP 5.4.44
    
    - Core:
      . Fixed bug #69793 (Remotely triggerable stack exhaustion via recursive
        method calls). (Stas)
      . Fixed bug #69892 (Different arrays compare indentical due to integer key
        truncation). (Nikita)
      . Fixed bug #70121 (unserialize() could lead to unexpected methods execution
        / NULL pointer deref). (Stas)
    
    - OpenSSL:
      . Fixed bug #70014 (openssl_random_pseudo_bytes() is not cryptographically
        secure). (Stas)
    
    - Phar:
      . Improved fix for bug #69441. (Anatol Belski)
      . Fixed bug #70019 (Files extracted from archive may be placed outside of
        destination directory). (Anatol Belski)
    
    - SOAP:
      . Fixed bug #70081 (SoapClient info leak / null pointer dereference via
         multiple type confusions). (Stas)
    
    - SPL:
      . Fixed bug #70068 (Dangling pointer in the unserialization of ArrayObject
        items). (sean.heelan)
      . Fixed bug #70166 (Use After Free Vulnerability in unserialize() with
        SPLArrayObject). (taoguangchen at icloud dot com)
      . Fixed bug #70168 (Use After Free Vulnerability in unserialize() with
        SplObjectStorage). (taoguangchen at icloud dot com)
      . Fixed bug #70169 (Use After Free Vulnerability in unserialize() with
        SplDoublyLinkedList). (taoguangchen at icloud dot com)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.105 2015/07/11 00:31:01 taca Exp $
+# $NetBSD: phpversion.mk,v 1.106 2015/08/08 00:11:29 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -81,7 +81,7 @@
 PHPVERSION_MK=	defined
 
 # Define each PHP's version.
-PHP54_VERSION=	5.4.43
+PHP54_VERSION=	5.4.44
 PHP55_VERSION=	5.5.27
 PHP56_VERSION=	5.6.11
 
