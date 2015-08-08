3d4da3e38b5c74688fc5daf0cf463f70ed1d4ec5
taca <taca@pkgsrc.org>
2015-08-08 00:11:29 +0000

    Update phpt54 to 5.4.44.
    06 Aug 2015 PHP 5.4.44
    
    - Core:
      . Fixed bug #69793 (Remotely triggerable stack exhaustion via recursive
        method calls). (Stas)
      . Fixed bug #69892 (Different arrays compare indentical due to integer key
        truncation). (Nikita)
      . Fixed bug #70121 (unserialize() could lead to unexpected methods execution
        / NULL pointer deref). (Stas)
    
    - OpenSSL:
      . Fixed bug #70014 (openssl_random_pseudo_bytes() is not cryptographically
        secure). (Stas)
    
    - Phar:
      . Improved fix for bug #69441. (Anatol Belski)
      . Fixed bug #70019 (Files extracted from archive may be placed outside of
        destination directory). (Anatol Belski)
    
    - SOAP:
      . Fixed bug #70081 (SoapClient info leak / null pointer dereference via
         multiple type confusions). (Stas)
    
    - SPL:
      . Fixed bug #70068 (Dangling pointer in the unserialization of ArrayObject
        items). (sean.heelan)
      . Fixed bug #70166 (Use After Free Vulnerability in unserialize() with
        SPLArrayObject). (taoguangchen at icloud dot com)
      . Fixed bug #70168 (Use After Free Vulnerability in unserialize() with
        SplObjectStorage). (taoguangchen at icloud dot com)
      . Fixed bug #70169 (Use After Free Vulnerability in unserialize() with
        SplDoublyLinkedList). (taoguangchen at icloud dot com)

--- lang/php/pear.mk
+++ lang/php/pear.mk
@@ -1,9 +1,17 @@
-# $NetBSD: pear.mk,v 1.24 2015/07/04 14:36:56 bsiegert Exp $
+# $NetBSD: pear.mk,v 1.25 2015/08/08 00:11:29 taca Exp $
 #
 # This Makefile fragment is intended to be included by packages that build
 # and install pear packages.
 #
 # If you don't know what pear is, you can visit: http://pear.php.net/
+#
+# === Package-settable variables ===
+#
+# PEAR_PACKAGE_XML	Use package.xml to build package with ${PEAR_CMD}.
+#			Default: yes
+#		
+#
+# === Variables defined by this file ===
 #
 # PEAR_CMD	this is the actual script to execute to install the
 #		package.
@@ -15,6 +23,8 @@
 _PEAR_PACKAGE_MK=       # defined
 
 .include "../../mk/bsd.prefs.mk"
+
+PEAR_PACKAGE_XML?=	yes
 
 EXTRACT_SUFX=	.tgz
 
@@ -38,6 +48,10 @@ PEAR_LIB=	lib/php
 PEAR_DESTDIR=	-P ${DESTDIR} -f
 .endif
 
+NO_BUILD=	yes
+
+.if empty(PEAR_PACKAGE_XML:M[nN][oO])
+
 # Dynamic PLIST, generated via a helper PHP script, which use registry.
 PEAR_GENERATE_PLIST=	\
 	${ECHO} "@comment The following lines are automatically generated"; \
@@ -47,8 +61,6 @@ PEAR_GENERATE_PLIST=	\
 		-d log_errors=On -d display_errors=Off \
 		-C -n ${PKGDIR}/../../lang/php/pear_plist.php;
 GENERATE_PLIST+=	${PEAR_GENERATE_PLIST}
-
-NO_BUILD=	yes
 
 .if ${_USE_DESTDIR} != "no"
 PEAR_FILES_SKIP+=	${PREFIX}/lib/php/\.channels/\.alias/.*\.txt
@@ -67,6 +79,15 @@ do-install:
 	cd ${WRKSRC} && ${SETENV} TZ=UTC \
 		${PEAR_CMD} "install" ${PEAR_DESTDIR} -n -O package.xml || exit 1
 	${RM} -f ${PEAR_FILES_SKIP:S/^/${DESTDIR}&/}
+
+.else	# PEAR_PACKAGE_XML == no
+
+PLIST_SUBST+=	PEAR_LIB=${PEAR_LIB:Q}
+
+do-install:
+	cd ${WRKSRC}/lib && ${PAX} -wr * ${DESTDIR}${PREFIX}/${PEAR_LIB}/
+
+.endif
 
 .include "../../lang/php/phpversion.mk"
 .include "${PHPPKGSRCDIR}/buildlink3.mk"
