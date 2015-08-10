4eacb008ce6c7eca9955ef1140d67913c5ca7d28
manu <manu@pkgsrc.org>
2015-08-10 12:56:27 +0000

    Fix build of packages depending on mariadb55-client
    When a package depends on mysql-client, in includes mk/mysql-buildlink3.mk
    and may get buildlinked with mariadb55-client if MYSQL_VERSION_DEFAULT is
    set to MARIADB55.
    
    mariadb55-client's buildlink3.mk defined BUILDLINK_*.mariadb-client
    variables, wich caused build problems with packages expecting

--- databases/mariadb55-client/buildlink3.mk
+++ databases/mariadb55-client/buildlink3.mk
@@ -1,18 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/05/07 12:45:28 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/08/10 12:56:27 manu Exp $
 
-BUILDLINK_TREE+=	mariadb-client
+BUILDLINK_TREE+=	mysql-client
 
 .if !defined(MARIADB_CLIENT_BUILDLINK3_MK)
 MARIADB_CLIENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mariadb-client+=	mariadb-client>=5.5.43
-BUILDLINK_PKGSRCDIR.mariadb-client?=	../../databases/mariadb55-client
-BUILDLINK_PKGSRCDIR.mariadb-client?=	../../databases/mariadb55-client
-BUILDLINK_INCDIRS.mariadb-client?=	include/mysql
-BUILDLINK_LIBDIRS.mariadb-client?=	lib
+BUILDLINK_API_DEPENDS.mysql-client+=	mariadb-client>=5.5.43
+BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mariadb55-client
+BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mariadb55-client
+BUILDLINK_INCDIRS.mysql-client?=	include/mysql
+BUILDLINK_LIBDIRS.mysql-client?=	lib
 
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
 .endif	# MARIADB_CLIENT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-mariadb-client
+BUILDLINK_TREE+=	-mysql-client
