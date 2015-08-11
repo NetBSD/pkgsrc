126e0d60a366607f6cff954d892daf5cdf96c086
ryoon <ryoon>
2015-08-11 23:48:17 +0000

    Update to 40.0
    Changelog:
        New Support for Windows 10
        New Added protection against unwanted software downloads
        New User can receive suggested tiles in the new tab page based on categories Firefox matches to browsing history (en-US only).
        New Hello allows adding a link to conversations to provide context on what the conversation will be about
        New New style for add-on manager based on the in-content preferences style
        New Improved scrolling, graphics, and video playback performance with off main thread compositing (GNU/Linux only)
        New Graphic blocklist mechanism improved: Firefox version ranges can be specified, limiting the number of devices blocked
        Changed Add-on extensions that are not signed by Mozilla will display a warning
        Changed NPAPI Plug-in performance improved via asynchronous initialization
        Changed Smoother animation and scrolling with hardware vsync (Windows only)
        Changed JPEG images use less memory when scaled and can be painted faster
        Changed Sub-resources can no longer request HTTP authentication, thus protecting users from inadvertently disclosing login data
        HTML5 IndexedDB transactions are now non-durable by default
        HTML5 Implemented AudioBufferSourceNode.detune to modulate playback rate in cents, a logarithmic unit of measure used for musical intervals
        Developer Improved Performance tools in the developer tools: Waterfall view, Call Tree view and a Flame Chart view
        Developer New rules view tooltip in the Inspector to tweak CSS Filter values
        Developer Console API messages from SharedWorker and ServiceWorker are now displayed in web console
        Developer New page ruler highlighting tool that displays lightweight horizontal and vertical rules on a page
        Developer Inspector now searches across all content frames in a page
        Fixed Kannada text does not display properly in built-in pdf viewer
        Fixed Various security fixes
    
    Known Issues
        unresolved If Firefox is restarted from an add-on install notification, on-going private browsing downloads might be canceled without warning (1185294)
    
    
    Fixed in Firefox 40
        2015-92 Use-after-free in XMLHttpRequest with shared workers
        2015-91 Mozilla Content Security Policy allows for asterisk wildcards in violation of CSP specification
        2015-90 Vulnerabilities found through code inspection
        2015-89 Buffer overflows on Libvpx when decoding WebM video
        2015-88 Heap overflow in gdk-pixbuf when scaling bitmap images
        2015-87 Crash when using shared memory in JavaScript
        2015-86 Feed protocol with POST bypasses mixed content protections
        2015-85 Out-of-bounds write with Updater and malicious MAR file
        2015-84 Arbitrary file overwriting through Mozilla Maintenance Service with hard links
        2015-83 Overflow issues in libstagefright
        2015-82 Redefinition of non-configurable JavaScript object properties
        2015-81 Use-after-free in MediaStream playback
        2015-80 Out-of-bounds read with malformed MP3 file
        2015-79 Miscellaneous memory safety hazards (rv:40.0 / rv:38.2)

--- www/firefox/patches/patch-gfx_skia_generate__mozbuild.py
+++ www/firefox/patches/patch-gfx_skia_generate__mozbuild.py
@@ -1,11 +1,14 @@
-$NetBSD: patch-gfx_skia_generate__mozbuild.py,v 1.1 2014/12/01 18:11:14 ryoon Exp $
+$NetBSD: patch-gfx_skia_generate__mozbuild.py,v 1.2 2015/08/11 23:48:18 ryoon Exp $
 
---- gfx/skia/generate_mozbuild.py.orig	2014-11-21 03:37:30.000000000 +0000
+--- gfx/skia/generate_mozbuild.py.orig	2015-08-07 15:54:04.000000000 +0000
 +++ gfx/skia/generate_mozbuild.py
-@@ -140,6 +140,9 @@ if CONFIG['GNU_CXX']:
-     if not CONFIG['CLANG_CXX']:
-         CXXFLAGS += ['-Wno-logical-op']
+@@ -142,6 +142,12 @@ if CONFIG['GNU_CXX']:
+     if CONFIG['CPU_ARCH'] == 'arm':
+         SOURCES['trunk/src/opts/SkBlitRow_opts_arm.cpp'].flags += ['-fomit-frame-pointer']
  
++if CONFIG['MOZ_NATIVE_HARFBUZZ']:
++    CXXFLAGS += CONFIG['MOZ_HARFBUZZ_CFLAGS']
++
 +if CONFIG['MOZ_NATIVE_HARFBUZZ']:
 +    CXXFLAGS += CONFIG['MOZ_HARFBUZZ_CFLAGS']
 +
--- www/firefox/patches/patch-media_webrtc_trunk_webrtc_system__wrappers_source_spreadsortlib_spreadsort.hpp
+++ www/firefox/patches/patch-media_webrtc_trunk_webrtc_system__wrappers_source_spreadsortlib_spreadsort.hpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-media_webrtc_trunk_webrtc_system__wrappers_source_spreadsortlib_spreadsort.hpp,v 1.7 2015/04/05 12:54:12 ryoon Exp $
+$NetBSD: patch-media_webrtc_trunk_webrtc_system__wrappers_source_spreadsortlib_spreadsort.hpp,v 1.8 2015/08/11 23:48:18 ryoon Exp $
 
---- media/webrtc/trunk/webrtc/system_wrappers/source/spreadsortlib/spreadsort.hpp.orig	2015-03-27 02:20:26.000000000 +0000
+--- media/webrtc/trunk/webrtc/system_wrappers/source/spreadsortlib/spreadsort.hpp.orig	2015-08-07 15:54:15.000000000 +0000
 +++ media/webrtc/trunk/webrtc/system_wrappers/source/spreadsortlib/spreadsort.hpp
 @@ -14,6 +14,13 @@ Cygwin fix provided by:
  Scott McMurray
@@ -16,7 +16,7 @@ $NetBSD: patch-media_webrtc_trunk_webrtc_system__wrappers_source_spreadsortlib_s
  #ifndef BOOST_SPREAD_SORT_H
  #define BOOST_SPREAD_SORT_H
  #include <algorithm>
-@@ -21,6 +28,13 @@ Scott McMurray
+@@ -21,10 +28,24 @@ Scott McMurray
  #include <vector>
  #include "webrtc/system_wrappers/source/spreadsortlib/constants.hpp"
  
@@ -30,3 +30,14 @@ $NetBSD: patch-media_webrtc_trunk_webrtc_system__wrappers_source_spreadsortlib_s
  #ifdef getchar
  #undef getchar
  #endif
+ 
++#ifdef __FreeBSD__
++# include <osreldate.h>
++# if __FreeBSD_version < 900506
++#  define getchar boost_getchar
++# endif
++#endif
++
+ namespace boost {
+   namespace detail {
+   	//This only works on unsigned data types
