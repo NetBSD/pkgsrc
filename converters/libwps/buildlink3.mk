0a5c5bb2757c66b217f69dcde1c0bec027acfb8c
ryoon <ryoon@pkgsrc.org>
2015-08-12 00:27:57 +0000

    Update to 0.4.0
    Changelog:
    0.3.x -> 0.4.0
    - add support for unknown encoding files (ie. DOS file)
    - add potential support for converting Lotus, ... documents,
    - accept to convert all Lotus Wk1 files and Symphony Wk1 files,
    - add support for Lotus Wk3 and Wk4 documents,
    - add support for Quattro Pro Wq1 and Wq2 documents,
    - only in debug mode, add pre-support for Lotus Wk5..., must allow to
      retrieve the main sheets content's with no formatting,
    - add potential support for asking the document's password ( but do nothing )
    - correct some compiler warnings when compiling in debug mode.

--- converters/libwps/buildlink3.mk
+++ converters/libwps/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/07/23 06:29:53 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2015/08/12 00:27:57 ryoon Exp $
 
 BUILDLINK_TREE+=	libwps
 
 .if !defined(LIBWPS_BUILDLINK3_MK)
 LIBWPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libwps+=	libwps>=0.2.0
-BUILDLINK_ABI_DEPENDS.libwps?=	libwps>=0.3.0
+BUILDLINK_API_DEPENDS.libwps+=	libwps>=0.4.0
 BUILDLINK_PKGSRCDIR.libwps?=	../../converters/libwps
 
 .include "../../converters/librevenge/buildlink3.mk"
