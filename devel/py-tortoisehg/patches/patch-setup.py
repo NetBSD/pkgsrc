d031709ef50554d27acaad807e8fb966c9792dcc
wiz <wiz@pkgsrc.org>
2015-08-14 18:00:56 +0000

    Update to 3.5:
    TortoiseHg 3.5
    
    TortoiseHg 3.5 is a major feature release, including the new
    Mercurial 3.5 major release and a small number of bug-fix commits.
    
    Bugs Fixes
    
        filectxactions: open save dialog where repository file exists (fixes #3445)
        grep: split inc/exc patterns on Comma instead of Comma+Space (fixes #836)
        lfprompt: suppress exception on getting size of inaccessible file (fixes #4240)
        qtlib: drop support for old-style icon lookup
        qtlib: load icons from filesystem unless frozen
        reporegistry: look for parent repo in model on removeSubrepo (fixes #4214)
        repowatcher: check mtime of bookmarks file (hg 3.5)
        repowatcher: detect rollback of repository states (fixes #4193)
        sync: sort bookmarks in target combobox alphabetically (fixes #4222)
    
    Improvements
    
        names: experimental support for displaying any name space
        repoview: add an action to resize columns ignoring settings
        repowidget: add menu to filter ancestors, by author or branch (closes #1455)
        sync: allow to select user certificates by file dialog
        sync: open the save dialog when Enter is pressed in URL field
        workbench: add icon to Web Server menu item (closes #4211)
    
    Installer
    
        update tsvn doc-diff scripts (fixes #3404, #4244)
        setup: build Qt resources only for py2exe target
        setup: do not pack unnecessary files to icon resource
        setup: generate config module according to installation options (refs #2178)
        setup: generate list of .mo files from existing .po files (fixes #2431)
        setup: move library.zip and dlls to lib/ directory (refs #4164, #4194)
        setup: respect prefix where nautilus extension will be installed (fixes #2178)

--- devel/py-tortoisehg/patches/patch-setup.py
+++ devel/py-tortoisehg/patches/patch-setup.py
@@ -1,26 +1,27 @@
-$NetBSD: patch-setup.py,v 1.1 2014/07/05 15:33:03 wiz Exp $
+$NetBSD: patch-setup.py,v 1.2 2015/08/14 18:00:56 wiz Exp $
 
 * nautilus-python is not available in pkgsrc so don't install
   the nautilus extension.
 * Install html documention, desktop file and license file.
 
---- setup.py.orig	2013-11-16 07:12:03.000000000 +0000
+--- setup.py.orig	2015-08-06 01:30:23.000000000 +0000
 +++ setup.py
-@@ -11,6 +11,7 @@ import time
- import sys
+@@ -11,6 +11,7 @@ import sys
  import os
  import shutil
-+import string
- import subprocess
  import cgi
++import string
  import tempfile
-@@ -504,8 +505,13 @@ def setup_posix():
-     _data_files += [(os.path.join('share', root),
-         [os.path.join(root, file_) for file_ in files])
-         for root, dirs, files in os.walk('locale')]
--    _data_files += [('/usr/share/nautilus-python/extensions/',
+ import re
+ import tarfile
+@@ -569,8 +570,14 @@ def setup_posix():
+                             [os.path.join(root, f) for f in files]))
+     _data_files.extend((os.path.join('share', modir), [mofile])
+                        for pofile, modir, mofile in _walklocales())
+-    _data_files += [('share/nautilus-python/extensions',
 -                     ['contrib/nautilus-thg.py'])]
-+    _data_files += [(os.path.join('share/doc/tortoisehg', 
++
++    _data_files += [(os.path.join('share/doc/tortoisehg',
 +                                  string.replace(root,'doc/build/html','html',1)),
 +        [os.path.join(root, file_) for file_ in files])
 +        for root, dirs, files in os.walk('doc/build/html')]
@@ -28,5 +29,5 @@ $NetBSD: patch-setup.py,v 1.1 2014/07/05 15:33:03 wiz Exp $
 +    _data_files += [('share/applications', ['contrib/thg.desktop'])]
 +    _data_files += [('share/tortoisehg', ['contrib/mergetools.rc'])]
  
-     # Create a config.py.  Distributions will need to supply their own
-     cfgfile = os.path.join('tortoisehg', 'util', 'config.py')
+     return _scripts, _packages, _data_files, _extra
+ 
