c817fc06eb7356e8c8377714f4496d0d3c8329c4
adam <adam@pkgsrc.org>
2015-08-14 07:54:04 +0000

    Changes 1.59.0: New Libraries Convert: An extendible and configurable type-conversion framework, from Vladimir Batov. Coroutine2: (C++14) Coroutine library, from Oliver Kowalke.
    Updated Libraries...

--- meta-pkgs/boost/patches/patch-boost_config_stdlib_libcpp.hpp
+++ meta-pkgs/boost/patches/patch-boost_config_stdlib_libcpp.hpp
@@ -1,12 +1,13 @@
-$NetBSD: patch-boost_config_stdlib_libcpp.hpp,v 1.1 2014/02/21 20:22:09 joerg Exp $
+$NetBSD: patch-boost_config_stdlib_libcpp.hpp,v 1.2 2015/08/14 07:54:04 adam Exp $
 
---- boost/config/stdlib/libcpp.hpp.orig	2014-02-21 00:34:58.000000000 +0000
+--- boost/config/stdlib/libcpp.hpp.orig	2015-08-04 11:44:49.000000000 +0000
 +++ boost/config/stdlib/libcpp.hpp
-@@ -33,5 +33,7 @@
+@@ -66,6 +66,8 @@
  
  // libc++ uses a non-standard messages_base
  #define BOOST_NO_STD_MESSAGES
 +// libc++ uses inline namespaces
 +#define BOOST_DETAIL_NO_CONTAINER_FWD
  
- //  --- end ---
+ #if defined(__has_include)
+ #if !__has_include(<shared_mutex>)
