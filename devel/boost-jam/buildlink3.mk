98598742b0622c649ac62ef632efd3bdc52bd0fe
adam <adam@pkgsrc.org>
2015-08-14 07:54:04 +0000

    Changes 1.59.0: New Libraries Convert: An extendible and configurable type-conversion framework, from Vladimir Batov. Coroutine2: (C++14) Coroutine library, from Oliver Kowalke.
    Updated Libraries...

--- devel/boost-jam/buildlink3.mk
+++ devel/boost-jam/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.25 2015/04/17 14:04:47 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2015/08/14 07:54:04 adam Exp $
 
 BUILDLINK_TREE+=	boost-jam
 
 .if !defined(BOOST_JAM_BUILDLINK3_MK)
 BOOST_JAM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.58.*
+BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.59.*
 BUILDLINK_DEPMETHOD.boost-jam?=		build
 BUILDLINK_PKGSRCDIR.boost-jam?=		../../devel/boost-jam
 .endif # BOOST_JAM_BUILDLINK3_MK
