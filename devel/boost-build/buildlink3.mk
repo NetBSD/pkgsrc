ee0046412c9e158c8e2257f584da23143937b754
adam <adam@pkgsrc.org>
2015-08-14 07:54:04 +0000

    Changes 1.59.0: New Libraries Convert: An extendible and configurable type-conversion framework, from Vladimir Batov. Coroutine2: (C++14) Coroutine library, from Oliver Kowalke.
    Updated Libraries...

--- devel/boost-build/buildlink3.mk
+++ devel/boost-build/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.29 2015/04/17 14:04:47 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2015/08/14 07:54:04 adam Exp $
 
 BUILDLINK_TREE+=	boost-build
 
 .if !defined(BOOST_BUILD_BUILDLINK3_MK)
 BOOST_BUILD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.58.*
+BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.59.*
 BUILDLINK_DEPMETHOD.boost-build?=	build
 BUILDLINK_PKGSRCDIR.boost-build?=	../../devel/boost-build
 .endif # BOOST_BUILD_BUILDLINK3_MK
