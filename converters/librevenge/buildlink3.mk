a26061b1430113a3097f0e2d11592d3ddfda16d3
wiz <wiz@pkgsrc.org>
2015-08-15 22:42:22 +0000

    Apply adam's patch to fix build with latest boost. Bump PKGREVISION. Add boost-libs to bl3.mk and bump ABI depends for that.

--- converters/librevenge/buildlink3.mk
+++ converters/librevenge/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/07/22 10:47:52 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/08/15 22:42:22 wiz Exp $
 
 BUILDLINK_TREE+=	librevenge
 
@@ -6,8 +6,10 @@ BUILDLINK_TREE+=	librevenge
 LIBREVENGE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.librevenge+=	librevenge>=0.0.1
+BUILDLINK_ABI_DEPENDS.librevenge+=	librevenge>=0.0.2nb1
 BUILDLINK_PKGSRCDIR.librevenge?=	../../converters/librevenge
 
+.include "../../devel/boost-libs/buildlink3.mk"
 .endif	# LIBREVENGE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-librevenge
