36d1dbad5473cee1950d68c29ece5beb3da11d7e
leot <leot@pkgsrc.org>
2015-08-17 16:42:53 +0000

    Update comms/gammu to gammu-1.36.4. ok wiz@.
    pkgsrc changes:
     * Now comms/gammu depends on devel/libusb1 (instead of devel/libusb)
     * Get rid of Makefile.common: it is no more needed now that comms/py-gammu is
       distribuited also upstream as a separate package.
    
    Changes:
    20150814 - 1.36.4
    
    [-] * Use advisory locking to prevent two Gammu instances share one device.
    [!] * Include child process stdout and stderr in SMSD logs to ease debugging.
    [-] * Fix string quoting with ODBC driver.
    [+] * Added RunOnSent option to SMSD.
    [+] * Store message reference in outbox in files SMSD.
    [-] * Improved C API documentation in manual.
    
    20150707 - 1.36.3
    
    [-] * Updated list of GSM country codes and networks.
    [-] * Fixed bash completition install path (Ville Skyttä).
    [-] * Better logging of delivery report failures in SMSD.
    [-] * Improved support for Huawei E3372.
    
    20150615 - 1.36.2
    
    [-] * Fixed compilation using MSVC.
    [-] * Fix siemenssatnetmon (Daniel Glöckner).
    [-] * Documentation improvements.
    [-] * Fixed smsd startup with non existing folders.
    [-] * Fixed possible stack overflows on Windows.
    
    20150520 - 1.36.1
    
    [-] * Compatibility with libdbi from git.
    [-] * Fix siemenssatnetmon (Daniel Glöckner).
    [-] * Fixed reconnecting to SQL server.
    [+] * Don't split a surrogate pair between message segments (David Brown).
    
    20150413 - 1.36.0
    
    [!] * The python-gammu module is now shipped separately.
    [!] * Removed usage of __TIME__ and __DATE__ macros in codebase.
    [-] * Fixed encoding of special chars to iCalendar format.
    [-] * Fixed decoding of priority from vTODO.
    [-] * Avoid infinite loops with ignored messages.
    [-] * Improved stability of checking phone SMS memory.
    [-] * Fixed parsing of some backup files.
    
    20150302 - 1.35.0
    
    [-] * Fixed encoding of UTF-8 for higher code points.
    [-] * Improved provided udev rules.
    [-] * Fixed possible lock while getting network status in SMSD.
    [-] * Various localization updates.
    
    20141230 - 1.34.0
    
    [+] * Add phone power ON/OFF function.
    [!] * Removed deprecated Python modules gammu.Data and gammu.Worker.
    [+] * Store network name and code in SMSD tables.
    [-] * Fixed build with recent clang compiler.
    [-] * Fixed several possible issues found by Coverity scan.
    [-] * Fixed possible crash on SMSD startup.
    [-] * Fixed decoding unicode SMS messages.
    [-] * Added identification for several Nokia phones.
    [-] * Fixed compilation issues on various platforms.
    [-] * SMSD now honors loglevel for all logging targets.
    [+] * SMSD can automatically hangup incoming calls.
    [-] * Correctly detect Network errors.

--- comms/gammu/buildlink3.mk
+++ comms/gammu/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2009/05/26 21:31:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/08/17 16:42:53 leot Exp $
 
 BUILDLINK_TREE+=	gammu
 
 .if !defined(GAMMU_BUILDLINK3_MK)
 GAMMU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gammu+=	gammu>=1.24.0
+BUILDLINK_API_DEPENDS.gammu+=	gammu>=1.34.0
 BUILDLINK_PKGSRCDIR.gammu?=	../../comms/gammu
 
 .endif	# GAMMU_BUILDLINK3_MK
