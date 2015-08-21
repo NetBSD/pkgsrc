a85381ae3a5f26622120b67bb4f11086cdafa8cb
is <is@pkgsrc.org>
2015-08-21 11:08:36 +0000

    There were a few places where time_t was passed to printf-like functions, but the format string specifies %d. As all of them are time differences, and a fax transmission shouldn't need more than 2^31 (normally not even 2^15) seconds, cast to (int), like already in a few other places. Needed because sizeof(time_t) > sizeof(int) in NetBSD-6 and later.

--- comms/mgetty+sendfax/patches/patch-faxrec.c
+++ comms/mgetty+sendfax/patches/patch-faxrec.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-faxrec.c,v 1.1 2013/03/28 21:17:57 joerg Exp $
+$NetBSD: patch-faxrec.c,v 1.2 2015/08/21 11:08:36 is Exp $
 
---- faxrec.c.orig	2013-03-28 20:03:52.000000000 +0000
+--- faxrec.c.orig	2006-01-01 17:13:18.000000000 +0000
 +++ faxrec.c
 @@ -22,6 +22,10 @@
  #include <sys/times.h>
@@ -13,3 +13,12 @@ $NetBSD: patch-faxrec.c,v 1.1 2013/03/28 21:17:57 joerg Exp $
  #ifndef sunos4
  #include <sys/ioctl.h>
  #endif
+@@ -178,7 +182,7 @@ int pagenum = 0, ppagenum = 0;		/* pages
+ "fax dev=%s, pid=%d, caller='%s', name='%s', id='%s', +FHNG=%03d, pages=%d/%d, time=%02d:%02d:%02d\n",
+ 	Device, getpid(), CallerId, CallName, fax_remote_id, 
+ 	fax_hangup_code, pagenum, ppagenum,
+-	call_done / 3600, (call_done / 60) % 60, call_done % 60);
++	(int)(call_done / 3600), (int)(call_done / 60) % 60, (int)(call_done % 60));
+ }
+ 
+ void fax_notify_mail _P3( (pagenum, ppagenum, mail_to),
