633bd301a1d0418cdd77dd6c8c4be2d3828bbed4
wiz <wiz@pkgsrc.org>
2015-08-23 14:30:35 +0000

    Bump PKGREVISION for nettle shlib major bump.

--- devel/ucommon/buildlink3.mk
+++ devel/ucommon/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2015/04/18 02:48:50 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2015/08/23 14:30:35 wiz Exp $
 
 BUILDLINK_TREE+=	ucommon
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ucommon
 UCOMMON_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ucommon+=	ucommon>=6.0.0
-BUILDLINK_ABI_DEPENDS.ucommon?=	ucommon>=6.3.1
+BUILDLINK_ABI_DEPENDS.ucommon?=	ucommon>=6.3.1nb1
 BUILDLINK_PKGSRCDIR.ucommon?=	../../devel/ucommon
 
 pkgbase := ucommon
