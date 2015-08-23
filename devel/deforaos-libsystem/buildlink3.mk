6ec8eabde0971087cfd4314aad4596def3eb4c00
khorben <khorben@pkgsrc.org>
2015-08-23 14:40:57 +0000

    Package DeforaOS libSystem 0.2.2
    Among the changes:
    - extended String API (trimming)
    - reworked the Array and Config classes
    - build with SSP and PIE by default
    - fixes and improvements to the build system
    - additional tests

--- devel/deforaos-libsystem/buildlink3.mk
+++ devel/deforaos-libsystem/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/12/16 23:55:22 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/08/23 14:40:57 khorben Exp $
 
 BUILDLINK_TREE+=	deforaos-libsystem
 
 .if !defined(DEFORAOS_LIBSYSTEM_BUILDLINK3_MK)
 DEFORAOS_LIBSYSTEM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-libsystem+=	deforaos-libsystem>=0.1.2
+BUILDLINK_API_DEPENDS.deforaos-libsystem+=	deforaos-libsystem>=0.2.2
 BUILDLINK_PKGSRCDIR.deforaos-libsystem?=	../../devel/deforaos-libsystem
 
 .endif	# DEFORAOS_LIBSYSTEM_BUILDLINK3_MK
