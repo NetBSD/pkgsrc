7f24077b336b387920772c4739f6e11103136897
leot <leot@pkgsrc.org>
2015-08-23 00:41:27 +0000

    Import www/webkit24-gtk3 as webkit24-gtk3-2.4.9.
    WebKit is an open source web browser engine. WebKit is also the name of
    the Mac OS X system framework version of the engine that's used by
    Safari, Dashboard, Mail, and many other OS X applications. WebKit's HTML
    and JavaScript code began as a branch of the KHTML and KJS libraries
    from KDE.
    
    This is the GTK3+ port of the engine.

---
+++ www/webkit24-gtk3/buildlink3.mk
@@ -0,0 +1,41 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/08/23 00:41:27 leot Exp $
+
+BUILDLINK_TREE+=	webkit24-gtk3
+
+.if !defined(WEBKIT24_GTK3_BUILDLINK3_MK)
+WEBKIT24_GTK3_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.webkit24-gtk3+=	webkit24-gtk3>=2.0
+BUILDLINK_ABI_DEPENDS.webkit24-gtk3?=	webkit24-gtk3>=2.4.8nb2
+BUILDLINK_PKGSRCDIR.webkit24-gtk3?=	../../www/webkit24-gtk3
+
+# Linking with this library may require newer libstdc++ features than are
+# available under the native compiler, so possibly upgrade the compiler
+# used by dependent packages too.
+GCC_REQD+=	4.8
+
+pkgbase := webkit24-gtk3
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.webkit24-gtk3:Menchant)
+.include "../../textproc/enchant/buildlink3.mk"
+.endif
+
+.include "../../databases/sqlite3/buildlink3.mk"
+.include "../../devel/gperf/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/libwebp/buildlink3.mk"
+.include "../../multimedia/gstreamer1/buildlink3.mk"
+.include "../../multimedia/gst-plugins1-base/buildlink3.mk"
+.include "../../net/libsoup/buildlink3.mk"
+.include "../../security/libsecret/buildlink3.mk"
+.include "../../textproc/icu/buildlink3.mk"
+.include "../../textproc/libxslt/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.include "../../x11/libXdamage/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
+.endif # WEBKIT24_GTK3_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-webkit24-gtk3
