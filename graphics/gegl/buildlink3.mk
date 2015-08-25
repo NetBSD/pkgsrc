bb80dea2ab2e38083a88391ad37e5bdb9f133775
wiz <wiz@pkgsrc.org>
2015-08-25 13:25:54 +0000

    Update to 0.3.0.
    Header location and library names changed, so bump API version.
    No users in pkgsrc (yet).
    
    Changes in GEGL 0.3.0
    ---------------------
    
     • Improvements to thread safety and parallelism.
     • Lower overhead graph travesal due from rewrite of visitors
     • OpenCL support now enabled by default when detected.
     • Experimental multithreading, enable by setting
       GEGL_THREADS=<number of threads> in the environment.
     • Experimental mipmap rendering, which permits transparent rendering of
       previews on smaller sized versions, enable by setting
       GEGL_MIPMAP_RENDERING=true in the environment.
     • Operations:
       • new operations: alien-map, antialias, apply-lens, bilateral-filter,
         bump.map, cartoon, channel-mixer, color-enhance, color-exchange,
         color-reduction, color-rotate, convolution-matrix, copy-buffer, cubism,
         deinterlace, diffraction-patterns, distance-transform, displace, edge,
         emboss, engrave, exposure, fractal-trace, high-pass, image-compare,
         illusion, invert-gamma, lens-flare, linear, linear-gradient, mosaic,
         motion-blur-circular, motion-blur-zoom, noise-cell noise-cie-lch,
         noise-hsv, noise-hurl, noise-pick, noise-rgb, noise-simplex, noise-spread,
         n-point deformation ops, oilify, panorama-projection, photocopy, plasma,
         radial-gradient, red-eye-removal, scale-size-keep-aspect, softglow,
         stretch-contrast, texturize-canvas, tile-glass, tile-seamless, tile-paper,
         tile, warp, whirl-pinch, wind, cache, cast-format, lcms-from-profile,
         npy-save, webp-load, webp-save, scale-ratio, scale-size, seamless-clone,
         sinus, supernova, value-propagate, video-degradation
       • reimplementation of gaussian-blur faster and more accurate
       • support for using URIs in image loaders
     • Buffer:
       • New default tile backend, doing disk writes in a separate thread.

--- graphics/gegl/buildlink3.mk
+++ graphics/gegl/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.30 2015/04/25 14:23:10 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2015/08/25 13:25:54 wiz Exp $
 
 BUILDLINK_TREE+=	gegl
 
 .if !defined(GEGL_BUILDLINK3_MK)
 GEGL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gegl+=	gegl>=0.2.0
-BUILDLINK_ABI_DEPENDS.gegl+=	gegl>=0.2.0nb19
+BUILDLINK_API_DEPENDS.gegl+=	gegl>=0.3.0
 BUILDLINK_PKGSRCDIR.gegl?=	../../graphics/gegl
 
 pkgbase := gegl
@@ -17,6 +16,7 @@ pkgbase := gegl
 .endif
 
 .include "../../graphics/babl/buildlink3.mk"
+.include "../../textproc/json-glib/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .endif # GEGL_BUILDLINK3_MK
 
