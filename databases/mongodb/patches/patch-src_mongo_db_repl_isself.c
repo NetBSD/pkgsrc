9663b205fce10950d926778ca52cdeffe700fa47
fhajny <fhajny@pkgsrc.org>
2015-08-26 05:03:00 +0000

    Update databases/mongodb to 3.0.6.
    - Improvements to WiredTiger Stability.
    - Fixed issue with the interaction between SSL and Auditing.
    - Fixed issue with aggregation $sort on sharded systems.

--- databases/mongodb/patches/patch-src_mongo_db_repl_isself.c
+++ databases/mongodb/patches/patch-src_mongo_db_repl_isself.c
@@ -1,19 +1,19 @@
-$NetBSD: patch-src_mongo_db_repl_isself.c,v 1.1 2015/05/02 08:10:33 ryoon Exp $
+$NetBSD: patch-src_mongo_db_repl_isself.c,v 1.2 2015/08/26 05:03:00 fhajny Exp $
 
 * Add NetBSD support.
 
---- src/mongo/db/repl/isself.cpp.orig	2015-04-08 20:28:08.000000000 +0000
+--- src/mongo/db/repl/isself.cpp.orig	2015-08-24 00:39:52.000000000 +0000
 +++ src/mongo/db/repl/isself.cpp
-@@ -47,7 +47,7 @@
- #include "mongo/util/scopeguard.h"
+@@ -48,7 +48,7 @@
  #include "mongo/util/log.h"
  
--#if defined(__linux__) || defined(__APPLE__) || defined(__freebsd__) || defined(__sunos__) || defined(__openbsd__)
-+#if defined(__linux__) || defined(__APPLE__) || defined(__freebsd__) || defined(__sunos__) || defined(__openbsd__) || defined(__netbsd__)
+ #if defined(__linux__) || defined(__APPLE__) || defined(__freebsd__) || defined(__sunos__) || \
+-    defined(__openbsd__)
++    defined(__openbsd__) || defined(__netbsd__)
  #define FASTPATH_UNIX 1
  #endif
  
-@@ -60,7 +60,7 @@
+@@ -61,7 +61,7 @@
  #include <ifaddrs.h>
  #include <netdb.h>
  
--- databases/mongodb/patches/patch-src_mongo_platform_random.cpp
+++ databases/mongodb/patches/patch-src_mongo_platform_random.cpp
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_mongo_platform_random.cpp,v 1.1 2015/05/02 08:10:33 ryoon Exp $
+$NetBSD: patch-src_mongo_platform_random.cpp,v 1.2 2015/08/26 05:03:00 fhajny Exp $
 
 Add NetBSD support.
---- src/mongo/platform/random.cpp.orig	2015-04-08 20:28:08.000000000 +0000
+--- src/mongo/platform/random.cpp.orig	2015-08-24 00:39:52.000000000 +0000
 +++ src/mongo/platform/random.cpp
-@@ -117,7 +117,7 @@ namespace mongo {
-         return new WinSecureRandom();
-     }
+@@ -116,7 +116,7 @@ SecureRandom* SecureRandom::create() {
+     return new WinSecureRandom();
+ }
  
 -#elif defined(__linux__) || defined(__sunos__) || defined(__APPLE__) || defined(__freebsd__)
 +#elif defined(__linux__) || defined(__sunos__) || defined(__APPLE__) || defined(__freebsd__) || defined(__netbsd__)
  
-     class InputStreamSecureRandom : public SecureRandom {
-     public:
+ class InputStreamSecureRandom : public SecureRandom {
+ public:
