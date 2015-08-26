de52c2dd098259bc73856c0eac3d580f96107189
fhajny <fhajny@pkgsrc.org>
2015-08-26 05:03:00 +0000

    Update databases/mongodb to 3.0.6.
    - Improvements to WiredTiger Stability.
    - Fixed issue with the interaction between SSL and Auditing.
    - Fixed issue with aggregation $sort on sharded systems.

--- databases/mongodb/patches/patch-src_mongo_shell_linenoise__utf8.h
+++ databases/mongodb/patches/patch-src_mongo_shell_linenoise__utf8.h
@@ -1,7 +1,7 @@
-$NetBSD: patch-src_mongo_shell_linenoise__utf8.h,v 1.1 2015/05/02 08:10:33 ryoon Exp $
+$NetBSD: patch-src_mongo_shell_linenoise__utf8.h,v 1.2 2015/08/26 05:03:00 fhajny Exp $
 
 Use Boost swap instead of std. Fixes build on SunOS.
---- src/mongo/shell/linenoise_utf8.h.orig	2015-04-08 20:28:08.000000000 +0000
+--- src/mongo/shell/linenoise_utf8.h.orig	2015-08-24 00:39:52.000000000 +0000
 +++ src/mongo/shell/linenoise_utf8.h
 @@ -27,6 +27,7 @@
   *    then also delete it in the license file.
@@ -11,16 +11,16 @@ Use Boost swap instead of std. Fixes build on SunOS.
  #include <boost/smart_ptr/scoped_array.hpp>
  #include <algorithm>
  #include <string.h>
-@@ -155,9 +156,9 @@ struct UtfStringMixin {
-     size_t chars() const { return _chars; }
+@@ -173,9 +174,9 @@ struct UtfStringMixin {
+     }
  
-     void swap( UtfStringMixin& other ) {
--        std::swap( _len, other._len );
--        std::swap( _cap, other._cap );
--        std::swap( _chars, other._chars );
-+        boost::swap( _len, other._len );
-+        boost::swap( _cap, other._cap );
-+        boost::swap( _chars, other._chars );
-         _str.swap( other._str );
+     void swap(UtfStringMixin& other) {
+-        std::swap(_len, other._len);
+-        std::swap(_cap, other._cap);
+-        std::swap(_chars, other._chars);
++        boost::swap(_len, other._len);
++        boost::swap(_cap, other._cap);
++        boost::swap(_chars, other._chars);
+         _str.swap(other._str);
      }
  
