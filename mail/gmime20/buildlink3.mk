17c470fee599ff56311e77f8fdee2086d61785d0
joerg <joerg@pkgsrc.org>
2015-08-29 22:58:21 +0000

    Belatedly update the location to match the new position in the tree.

--- mail/gmime20/buildlink3.mk
+++ mail/gmime20/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/09/21 13:21:29 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/08/29 22:58:21 joerg Exp $
 
 BUILDLINK_TREE+=	gmime20
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gmime20
 GMIME20_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gmime20+=	gmime20>=2.1.7
-BUILDLINK_PKGSRCDIR.gmime20?=	../../mail/gmime
+BUILDLINK_PKGSRCDIR.gmime20?=	../../mail/gmime20
 
 .include "../../devel/glib2/buildlink3.mk"
 .endif # GMIME20_BUILDLINK3_MK
