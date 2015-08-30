5438197404de0f548be32cad0629911d69c0ed65
markd <markd@pkgsrc.org>
2015-08-30 09:56:45 +0000

    Fix g++ 5.0 build. Fix build on Linux.

--- x11/qt5-qtwebkit/patches/patch-Source_WTF_wtf_Platform.h
+++ x11/qt5-qtwebkit/patches/patch-Source_WTF_wtf_Platform.h
@@ -1,9 +1,11 @@
-$NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.1 2014/12/30 17:23:47 adam Exp $
+$NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.2 2015/08/30 09:56:45 markd Exp $
 
 * Disable JIT support for NetBSD, fix segfault of qtwebkit consumers
 * Use system's malloc for NetBSD, do not use fastmalloc from qt5,
   fix segfault of qtwebkit consumers
 
+* Disable DISASSEMBLER on Linux. Unresolved symbols as not building
+  UDis86Disassembler.cpp for some reason.
 --- Source/WTF/wtf/Platform.h.orig	2013-11-27 01:01:21.000000000 +0000
 +++ Source/WTF/wtf/Platform.h
 @@ -36,6 +36,9 @@
@@ -27,4 +29,15 @@ $NetBSD: patch-Source_WTF_wtf_Platform.h,v 1.1 2014/12/30 17:23:47 adam Exp $
 +
  #if PLATFORM(EFL)
  #define ENABLE_GLOBAL_FASTMALLOC_NEW 0
+ #endif
+@@ -804,6 +812,10 @@
+ #define WTF_USE_ARMV7_DISASSEMBLER 1
+ #endif
+ 
++#if OS(LINUX)
++#define ENABLE_DISASSEMBLER 0
++#endif
++
+ #if !defined(ENABLE_DISASSEMBLER) && (USE(UDIS86) || USE(ARMV7_DISASSEMBLER))
+ #define ENABLE_DISASSEMBLER 1
  #endif
