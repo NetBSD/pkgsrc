36628ffb47ec45f37bb5873dada90b023b21412a
khorben <khorben@pkgsrc.org>
2015-08-31 00:13:26 +0000

    Import buildlink3.mk file for mail/deforaos-mailer

---
+++ mail/deforaos-mailer/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/08/31 00:13:26 khorben Exp $
+
+BUILDLINK_TREE+=	deforaos-mailer
+
+.if !defined(DEFORAOS_MAILER_BUILDLINK3_MK)
+DEFORAOS_MAILER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.deforaos-mailer+=	deforaos-mailer>=0.1.6
+BUILDLINK_ABI_DEPENDS.deforaos-mailer?=	deforaos-mailer>=0.1.6
+BUILDLINK_PKGSRCDIR.deforaos-mailer?=	../../mail/deforaos-mailer
+
+.include "../../x11/deforaos-libdesktop/buildlink3.mk"
+.endif	# DEFORAOS_MAILER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-deforaos-mailer
