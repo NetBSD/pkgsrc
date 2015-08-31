1f485babe959db10e09a7cacaab1610f3032c27c
khorben <khorben@pkgsrc.org>
2015-08-31 22:24:05 +0000

    Bump the buildlink3.mk file as well

--- databases/deforaos-libdatabase/buildlink3.mk
+++ databases/deforaos-libdatabase/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/03/04 23:13:49 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/08/31 22:24:05 khorben Exp $
 
 BUILDLINK_TREE+=	deforaos-libdatabase
 
 .if !defined(DEFORAOS_LIBDATABASE_BUILDLINK3_MK)
 DEFORAOS_LIBDATABASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-libdatabase+=	deforaos-libdatabase>=0.0.0
+BUILDLINK_API_DEPENDS.deforaos-libdatabase+=	deforaos-libdatabase>=0.0.1
 BUILDLINK_PKGSRCDIR.deforaos-libdatabase?=	../../databases/deforaos-libdatabase
 
 .include "../../devel/deforaos-libsystem/buildlink3.mk"
