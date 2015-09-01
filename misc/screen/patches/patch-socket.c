7bbeed11a3a56734304c44cb94c754c61270cadd
kim <kim>
2015-09-01 13:34:47 +0000

    Upgrade misc/screen to version 4.3.1 and address stack overflog bug 45713.
    Ref: https://savannah.gnu.org/bugs/?45713
    
    Changes since version 4.2.1
    ---------------------------
    
    Version 4.3.1 (28/06/2015):
      * Fix resize bug
    
    Version 4.3.0 (13/06/2015):
      * Introduce Xx string escape showing the executed command of a window
      * Implement dead/zombie window polling, allowing for auto reconnecting
      * Allow setting hardstatus on first line
    
      New Commands:
      * 'sort' command sorting windows by title
      * 'bumpleft', 'bumpright' - manually move windows on window list
      * 'collapse' removing numbering 'gaps' between windows, by renumbering
      * 'windows' command now accepts arguments for use with querying

--- misc/screen/patches/patch-socket.c
+++ misc/screen/patches/patch-socket.c
@@ -1,12 +1,15 @@
-$NetBSD: patch-socket.c,v 1.1 2015/02/13 04:44:40 rodent Exp $
+$NetBSD: patch-socket.c,v 1.2 2015/09/01 13:34:47 kim Exp $
 
---- socket.c.orig	2014-04-26 16:22:43.000000000 +0000
-+++ socket.c
-@@ -30,6 +30,7 @@
- #include <sys/types.h>
- #include <sys/stat.h>
+--- socket.c.orig	2015-06-28 21:22:56.000000000 +0000
++++ socket.c	2015-09-01 12:42:34.000000000 +0000
+@@ -32,9 +32,7 @@
  #include <fcntl.h>
-+#include <sys/uio.h>
  #if !defined(NAMEDPIPE)
- #include <sys/socket.h>
- #include <sys/un.h>
+ # include <sys/socket.h>
+-# ifdef _OpenBSD_
+-#  include <sys/uio.h>
+-# endif
++# include <sys/uio.h>
+ # include <sys/un.h>
+ #endif
+ 
