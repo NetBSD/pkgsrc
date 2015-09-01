c0017cdd663088f9e9b77829b59ecfe8b3fc2fb7
kim <kim@pkgsrc.org>
2015-09-01 13:34:47 +0000

    Upgrade misc/screen to version 4.3.1 and address stack overflog bug 45713.
    Ref: https://savannah.gnu.org/bugs/?45713
    
    Changes since version 4.2.1
    ---------------------------
    
    Version 4.3.1 (28/06/2015):
      * Fix resize bug
    
    Version 4.3.0 (13/06/2015):
      * Introduce Xx string escape showing the executed command of a window
      * Implement dead/zombie window polling, allowing for auto reconnecting
      * Allow setting hardstatus on first line
    
      New Commands:
      * 'sort' command sorting windows by title
      * 'bumpleft', 'bumpright' - manually move windows on window list
      * 'collapse' removing numbering 'gaps' between windows, by renumbering
      * 'windows' command now accepts arguments for use with querying

--- misc/screen/patches/patch-tty.sh
+++ misc/screen/patches/patch-tty.sh
@@ -1,23 +1,23 @@
-$NetBSD: patch-tty.sh,v 1.1 2015/01/13 08:27:28 he Exp $
+$NetBSD: patch-tty.sh,v 1.2 2015/09/01 13:34:47 kim Exp $
 
 Change condition for using TIOCSCTTY.
 
 NetBSD on the netbsd-5 branch insists on a non-NULL second argument
 to the realpath() function.  Make it so.
 
---- tty.sh.orig	2014-04-26 16:29:40.000000000 +0000
-+++ tty.sh
-@@ -812,7 +812,8 @@ int fd;
+--- tty.sh.orig	2015-06-28 21:22:56.000000000 +0000
++++ tty.sh	2015-09-01 12:46:28.000000000 +0000
+@@ -812,7 +812,8 @@
    /*
     * Under BSD we have to set the controlling terminal again explicitly.
     */
--# if (defined(__FreeBSD_kernel__) || defined(__GNU__)) && defined(TIOCSCTTY)
-+# if (defined(__FreeBSD_kernel__) || defined(__GNU__) || (BSD >= 199103)) \
-+  && defined(TIOCSCTTY)
+-# if (defined(__FreeBSD_kernel__) || defined(__DragonFly__) || defined(__GNU__)) && defined(TIOCSCTTY)
++# if (defined(__FreeBSD_kernel__) || defined(__DragonFly__) \
++      || defined(__GNU__) || (BSD >= 199103)) && defined(TIOCSCTTY)
    ioctl(fd, TIOCSCTTY, (char *)0);
  # endif
  
-@@ -1508,8 +1509,10 @@ char *tty;
+@@ -1508,8 +1509,10 @@
    struct stat st;
    char * real;
    int rc;
