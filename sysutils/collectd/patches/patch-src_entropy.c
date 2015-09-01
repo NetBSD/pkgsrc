1ce013e79bcb0caf0c8d0e7ab47dbcfe81c808fc
he <he@pkgsrc.org>
2015-09-01 09:31:46 +0000

    Re-work the entropy patch to put the error section up front, and the Linux code above the NetBSD code.  No code change, except ensure to forward-declare the common functions in all cases.

--- sysutils/collectd/patches/patch-src_entropy.c
+++ sysutils/collectd/patches/patch-src_entropy.c
@@ -1,15 +1,51 @@
-$NetBSD: patch-src_entropy.c,v 1.3 2015/08/18 07:47:46 he Exp $
+$NetBSD: patch-src_entropy.c,v 1.4 2015/09/01 09:31:46 he Exp $
 
 Provide a NetBSD implementation for graphing available entropy.
 
 --- src/entropy.c.orig	2015-03-10 14:14:45.000000000 +0000
 +++ src/entropy.c
-@@ -29,27 +29,47 @@
+@@ -28,27 +28,16 @@
+ #include "common.h"
  #include "plugin.h"
  
- #if !KERNEL_LINUX
+-#if !KERNEL_LINUX
 -# error "No applicable input method."
-+# if KERNEL_NETBSD
+-#endif
+-
+-#define ENTROPY_FILE "/proc/sys/kernel/random/entropy_avail"
++static void entropy_submit (double);
++static int entropy_read (void);
+ 
+-static void entropy_submit (double entropy)
+-{
+-	value_t values[1];
+-	value_list_t vl = VALUE_LIST_INIT;
++#if !KERNEL_LINUX && !KERNEL_NETBSD
++#  error "No applicable input method."
++#endif
+ 
+-	values[0].gauge = entropy;
++#if KERNEL_LINUX
+ 
+-	vl.values = values;
+-	vl.values_len = 1;
+-	sstrncpy (vl.host, hostname_g, sizeof (vl.host));
+-	sstrncpy (vl.plugin, "entropy", sizeof (vl.plugin));
+-	sstrncpy (vl.type, "entropy", sizeof (vl.type));
+-
+-	plugin_dispatch_values (&vl);
+-}
++#define ENTROPY_FILE "/proc/sys/kernel/random/entropy_avail"
+ 
+ static int entropy_read (void)
+ {
+@@ -74,6 +63,56 @@ static int entropy_read (void)
+ 
+ 	return (0);
+ }
++#endif /* KERNEL_LINUX */
++
++#if KERNEL_NETBSD
 +/* Provide a NetBSD implementation, partial from rndctl.c */
 +
 +#include <sys/types.h>
@@ -18,57 +54,29 @@ Provide a NetBSD implementation for graphing available entropy.
 +#include <sys/rnd.h>
 +#if HAVE_SYS_RNDIO_H
 +# include <sys/rndio.h>
- #endif
++#endif
 +#include <paths.h>
- 
--#define ENTROPY_FILE "/proc/sys/kernel/random/entropy_avail"
-+static void entropy_submit (double);
-+static int entropy_read (void);
- 
--static void entropy_submit (double entropy)
++
 +static int
 +entropy_read (void)
- {
--	value_t values[1];
--	value_list_t vl = VALUE_LIST_INIT;
++{
 +	rndpoolstat_t rs;
 +	int fd;
- 
--	values[0].gauge = entropy;
++
 +	fd = open(_PATH_URANDOM, O_RDONLY, 0644);
 +	if (fd < 0)
 +		return -1;
- 
--	vl.values = values;
--	vl.values_len = 1;
--	sstrncpy (vl.host, hostname_g, sizeof (vl.host));
--	sstrncpy (vl.plugin, "entropy", sizeof (vl.plugin));
--	sstrncpy (vl.type, "entropy", sizeof (vl.type));
++
 +	if (ioctl(fd, RNDGETPOOLSTAT, &rs) < 0)
 +		return -1;
- 
--	plugin_dispatch_values (&vl);
++
 +	entropy_submit (rs.curentropy);
 +
 +	close(fd);
 +	return 0;
- }
- 
-+# else /* KERNEL_NETBSD */
-+#  error "No applicable input method."
-+# endif
-+#else /* Linux */
++}
 +
-+#define ENTROPY_FILE "/proc/sys/kernel/random/entropy_avail"
-+
- static int entropy_read (void)
- {
- 	double entropy;
-@@ -74,6 +94,23 @@ static int entropy_read (void)
- 
- 	return (0);
- }
-+#endif /* not Linux */
++#endif /* KERNEL_NETBSD */
 +
 +static void entropy_submit (double entropy)
 +{
