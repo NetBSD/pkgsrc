6305d8616c5d87b1fe099a0f0dc8a13f832bb0fa
agc <agc@pkgsrc.org>
2015-09-01 19:41:17 +0000

    Bump API requirement to 20150901 for the renamed symbols in libnetpgpverify

--- security/libnetpgpverify/buildlink3.mk
+++ security/libnetpgpverify/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/02/16 17:15:48 agc Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/09/01 19:41:17 agc Exp $
 
 BUILDLINK_TREE+=	libnetpgpverify
 
 .if !defined(LIBNETPGPVERIFY_BUILDLINK3_MK)
 LIBNETPGPVERIFY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libnetpgpverify+=	libnetpgpverify>=20140210
+BUILDLINK_API_DEPENDS.libnetpgpverify+=	libnetpgpverify>=20150901
 BUILDLINK_PKGSRCDIR.libnetpgpverify?=	../../security/libnetpgpverify
 
 .endif	# LIBNETPGPVERIFY_BUILDLINK3_MK
