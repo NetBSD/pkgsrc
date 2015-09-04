7edd1a2aa027448a838b3b0d598a1ffe76ec3155
sborrill <sborrill>
2015-09-04 07:24:48 +0000

    Fix path in patch

--- www/dansguardian/patches/patch-src_FOptionContainer.hpp
+++ www/dansguardian/patches/patch-src_FOptionContainer.hpp
@@ -1,9 +1,9 @@
-$NetBSD: patch-src_FOptionContainer.hpp,v 1.1 2015/09/03 11:55:40 sborrill Exp $
+$NetBSD: patch-src_FOptionContainer.hpp,v 1.2 2015/09/04 07:24:48 sborrill Exp $
 
 maxuploadsize is a filtergroup settings
 
---- /usr/obj/pkgsrc/www/dansguardian/work.builder7/dansguardian-2.12.0.3/src/FOptionContainer.hpp.orig	2015-09-03 12:20:34.000000000 +0100
-+++ /usr/obj/pkgsrc/www/dansguardian/work.builder7/dansguardian-2.12.0.3/src/FOptionContainer.hpp	2015-09-03 12:20:46.000000000 +0100
+--- src/FOptionContainer.hpp.orig	2015-09-03 12:20:34.000000000 +0100
++++ src/FOptionContainer.hpp	2015-09-03 12:20:46.000000000 +0100
 @@ -36,6 +36,7 @@
  	int weighted_phrase_mode;
  	int group_mode;
