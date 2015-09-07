269cc080457a6f6ece5b406fc6153fc761460e27
gdt <gdt>
2015-09-07 23:24:46 +0000

    Remove CONFIGURE_ARGS.enable scheme, in favor of normal CONFIGURE_ARGS.  No functional change intended, so no PKGREVISION++. From David Shao via PR pkg/50075 and private followup.

--- sysutils/dbus/options.mk
+++ sysutils/dbus/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.8 2015/01/21 13:45:18 pho Exp $
+# $NetBSD: options.mk,v 1.9 2015/09/07 23:24:46 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dbus
 PKG_SUPPORTED_OPTIONS+=	debug x11
@@ -22,31 +22,33 @@ PKG_SUPPORTED_OPTIONS+=	launchd
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdebug)
-CONFIGURE_ARGS.enable+=	asserts verbose-mode
+CONFIGURE_ARGS+=	--enable-asserts
+CONFIGURE_ARGS+=	--enable-verbose-mode
 .else
-CONFIGURE_ARGS.disable+= asserts verbose-mode
+CONFIGURE_ARGS+=	--disable-asserts
+CONFIGURE_ARGS+=	--disable-verbose-mode
 .endif
 
 .if !empty(PKG_OPTIONS:Mkqueue)
-CONFIGURE_ARGS.enable+= 	kqueue
+CONFIGURE_ARGS+= 	--enable-kqueue
 .else
-CONFIGURE_ARGS.disable+=	kqueue
+CONFIGURE_ARGS+=	--disable-kqueue
 .endif
 
 .if !empty(PKG_OPTIONS:Mx11)
-CONFIGURE_ARGS.with+=	x
+CONFIGURE_ARGS+=	--with-x
 .  include "../../x11/libX11/buildlink3.mk"
 BUILDLINK_DEPMETHOD.libXt=	build
 .  include "../../x11/libXt/buildlink3.mk"
 .else
-CONFIGURE_ARGS.without=	x
+CONFIGURE_ARGS=		--without-x
 .endif
 
 .if !empty(PKG_OPTIONS:Mlaunchd)
 MESSAGE_SRC+=			MESSAGE.launchd
 PLIST.launchd=			yes
-CONFIGURE_ARGS.enable+=		launchd
-CONFIGURE_ARGS.with+=		launchd-agent-dir=${PREFIX}/Library/LaunchAgents
+CONFIGURE_ARGS+=		--enable-launchd
+CONFIGURE_ARGS+=		--with-launchd-agent-dir=${PREFIX}/Library/LaunchAgents
 .else
-CONFIGURE_ARGS.disable+=	launchd
+CONFIGURE_ARGS+=		--disable-launchd
 .endif
