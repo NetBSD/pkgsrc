b27f6bf2b672e6e1d42289a858b4e434e35abdfb
dsainty <dsainty@pkgsrc.org>
2015-09-07 01:02:00 +0000

    Update mediainfo and libmediainfo to 0.7.77
    The change logs between 0.7.65 and 0.7.77 total several hundred lines, and
    consists primarily of very detailed per-codec trivia.  The complete change
    logs can be found in the source distribution as MediaInfo/History_CLI.txt and
    MediaInfoLib/History_DLL.txt.
    
    Pkgsrc:
    
      - patch-MediaInfoLib_Source_MediaInfo_Text_File__Ttml.h applied upstream.
    
      - pkglint a bit.
    
      - mediainfo appears to have done some work in avoiding bashisms, so remove
        "bash" from the tools.
    
      - CONFIG_SHELL doesn't need to be explicitly bash any longer.  Possibly this
        was only ever necessary because bash seems to be the only shell that was
        willing to treat a garbage shell substitution (caused by a bug in the
        configure.ac) as somehow legal.
    
      - ... and libmediainfo now actually builds if you accidentally on purpose
        override CONFIG_SHELL to be pdksh.

--- multimedia/libmediainfo/buildlink3.mk
+++ multimedia/libmediainfo/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/12/31 23:21:08 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/09/07 01:02:00 dsainty Exp $
 
 BUILDLINK_TREE+=	libmediainfo
 
 .if !defined(LIBMEDIAINFO_BUILDLINK3_MK)
 LIBMEDIAINFO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libmediainfo+=	libmediainfo>=0.7.65
+BUILDLINK_API_DEPENDS.libmediainfo+=	libmediainfo>=0.7.77
 BUILDLINK_PKGSRCDIR.libmediainfo?=	../../multimedia/libmediainfo
 
 .include "../../devel/zlib/buildlink3.mk"
