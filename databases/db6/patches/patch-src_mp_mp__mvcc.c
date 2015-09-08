d058d88e18beaafe14df953429dce16d4eb58bd4
joerg <joerg@pkgsrc.org>
2015-09-08 18:51:27 +0000

    Avoid overlap between db's atomic.h and stdatomic.h.

---
+++ databases/db6/patches/patch-src_mp_mp__mvcc.c
@@ -0,0 +1,22 @@
+$NetBSD: patch-src_mp_mp__mvcc.c,v 1.1 2015/09/08 18:51:28 joerg Exp $
+
+--- src/mp/mp_mvcc.c.orig	2015-09-02 11:18:15.000000000 +0000
++++ src/mp/mp_mvcc.c
+@@ -281,7 +281,7 @@ __memp_bh_freeze(dbmp, infop, hp, bhp, n
+ #else
+ 	memcpy(frozen_bhp, bhp, SSZA(BH, buf));
+ #endif
+-	atomic_init(&frozen_bhp->ref, 0);
++	db_atomic_init(&frozen_bhp->ref, 0);
+ 	if (mutex != MUTEX_INVALID)
+ 		frozen_bhp->mtx_buf = mutex;
+ 	else if ((ret = __mutex_alloc(env, MTX_MPOOL_BH,
+@@ -440,7 +440,7 @@ __memp_bh_thaw(dbmp, infop, hp, frozen_b
+ #endif
+ 		alloc_bhp->mtx_buf = mutex;
+ 		MUTEX_LOCK(env, alloc_bhp->mtx_buf);
+-		atomic_init(&alloc_bhp->ref, 1);
++		db_atomic_init(&alloc_bhp->ref, 1);
+ 		F_CLR(alloc_bhp, BH_FROZEN);
+ 	}
+ 
