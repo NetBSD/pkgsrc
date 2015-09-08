c6cb9301ba652645944d170769f3ebb93351893a
joerg <joerg@pkgsrc.org>
2015-09-08 18:54:41 +0000

    Avoid UB of left-shift of negative values by replacing it with a multiplication. Remove duplicate PKGREVISION. Bump revision.

---
+++ lang/ocaml/patches/patch-byterun_caml_mlvalues.h
@@ -0,0 +1,13 @@
+$NetBSD: patch-byterun_caml_mlvalues.h,v 1.1 2015/09/08 18:54:41 joerg Exp $
+
+--- byterun/caml/mlvalues.h.orig	2015-09-03 12:17:44.000000000 +0000
++++ byterun/caml/mlvalues.h
+@@ -68,7 +68,7 @@ typedef uintnat mark_t;
+ 
+ /* Conversion macro names are always of the form  "to_from". */
+ /* Example: Val_long as in "Val from long" or "Val of long". */
+-#define Val_long(x)     (((intnat)(x) << 1) + 1)
++#define Val_long(x)     ((intnat)(x) * 2 + 1)
+ #define Long_val(x)     ((x) >> 1)
+ #define Max_long (((intnat)1 << (8 * sizeof(value) - 2)) - 1)
+ #define Min_long (-((intnat)1 << (8 * sizeof(value) - 2)))
