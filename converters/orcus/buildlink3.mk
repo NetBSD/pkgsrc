f4b61a5f9b0aab267276533959c898f55ea0072d
wiz <wiz@pkgsrc.org>
2015-09-08 19:40:59 +0000

    Update to 0.9.2:
    No Changelog found. Major API change -- 0.10 in directory names instead
    of 0.8 before.

--- converters/orcus/buildlink3.mk
+++ converters/orcus/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/10/13 21:43:20 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/09/08 19:40:59 wiz Exp $
 
 BUILDLINK_TREE+=	orcus
 
 .if !defined(ORCUS_BUILDLINK3_MK)
 ORCUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.orcus+=	orcus>=0.7.0
+BUILDLINK_API_DEPENDS.orcus+=	orcus>=0.9.0
 BUILDLINK_PKGSRCDIR.orcus?=	../../converters/orcus
 
 #.include "../../devel/boost-libs/buildlink3.mk"
