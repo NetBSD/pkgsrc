e8ebfcefbaf0b00b72abde16d5634bfd3398ce35
dsainty <dsainty@pkgsrc.org>
2015-09-10 00:54:47 +0000

    This package uses zlib and libxml2, link them in.
    Fixes build on a Linux system with PREFER_PKGSRC=yes.
    
    Bump PKGREVISION.

--- devel/libgphoto2/buildlink3.mk
+++ devel/libgphoto2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2011/04/28 11:16:35 hans Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2015/09/10 00:54:47 dsainty Exp $
 
 BUILDLINK_TREE+=	libgphoto2
 
@@ -13,8 +13,10 @@ BUILDLINK_PKGSRCDIR.libgphoto2?=	../../devel/libgphoto2
 .include "../../devel/libusb/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/libltdl/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../graphics/libexif/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
 .endif # LIBGPHOTO2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libgphoto2
