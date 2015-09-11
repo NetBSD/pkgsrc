9688c4f80f540b283c15e1a18ddd93e6b2512b51
he <he@pkgsrc.org>
2015-09-11 21:37:00 +0000

    Fix the workaround for missing frexpl() on netbsd-6: use frexp() as a substitute for frexpl() on NetBSD before 6.99.17

--- misc/goffice0.10/patches/patch-goffice_math_go-dtoa.c
+++ misc/goffice0.10/patches/patch-goffice_math_go-dtoa.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-goffice_math_go-dtoa.c,v 1.2 2015/09/03 14:53:23 gdt Exp $
+$NetBSD: patch-goffice_math_go-dtoa.c,v 1.3 2015/09/11 21:37:00 he Exp $
 
-Ensure frexpl() is declared on NetBSD before 6.99.17.
+Use frexp() as a substitute for frexpl() on NetBSD before 6.99.17.
 
---- goffice/math/go-dtoa.c.orig	2014-04-30 00:44:52.000000000 +0000
+--- goffice/math/go-dtoa.c.orig	2014-10-27 23:55:13.000000000 +0000
 +++ goffice/math/go-dtoa.c
-@@ -43,6 +43,13 @@ typedef GString FAKE_FILE;
+@@ -43,6 +43,14 @@ typedef GString FAKE_FILE;
  #define go_finitel isfinite
  #endif
  
@@ -12,7 +12,8 @@ Ensure frexpl() is declared on NetBSD before 6.99.17.
 +#if (__NetBSD_Version__ >= 699001700)
 +/* in <math.h> in newer NetBSDs, should already be included */
 +#else
-+extern long double frexpl(long double, int*);
++/* OK, this will lose precision and range, but what else to do? */
++#define frexpl(ldv,i)	(long double)frexp((double)ldv, i)
 +#endif
 +#endif /* __NetBSD__ */
  
