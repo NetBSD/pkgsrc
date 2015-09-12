4d868439c9920cd160050c67c2959ce27c2e7a25
he <he@pkgsrc.org>
2015-09-12 19:04:56 +0000

    Add a plugin for NetBSD which submits the equivalent of the UDP and UDP6 parts of "netstat -s" output.  Useful for monitoring e.g. packet discards due to full UDP socket buffers. Bump PKGREVISION.

--- sysutils/collectd/options.mk
+++ sysutils/collectd/options.mk
@@ -1,15 +1,15 @@
-# $NetBSD: options.mk,v 1.5 2015/08/18 07:47:46 he Exp $
+# $NetBSD: options.mk,v 1.6 2015/09/12 19:04:56 he Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.collectd
 PKG_SUPPORTED_OPTIONS=	cpu df interface load memory syslog uptime
 PKG_SUGGESTED_OPTIONS+=	cpu df interface load memory syslog uptime
 
 .if ${OPSYS} == "NetBSD"
-PKG_SUPPORTED_OPTIONS+=	contextswitch disk entropy irq pf processes
-PKG_SUPPORTED_OPTIONS+=	swap tcpconns users
+PKG_SUPPORTED_OPTIONS+=	contextswitch disk entropy irq pf netstat_udp
+PKG_SUPPORTED_OPTIONS+= processes swap tcpconns users
 
-PKG_SUGGESTED_OPTIONS+=	contextswitch disk entropy irq pf processes
-PKG_SUGGESTED_OPTIONS+= swap tcpconns users
+PKG_SUGGESTED_OPTIONS+=	contextswitch disk entropy irq pf netstat_udp
+PKG_SUGGESTED_OPTIONS+= processes swap tcpconns users
 .endif
 
 .if ${OPSYS} == "FreeBSD"
@@ -33,7 +33,8 @@ PKG_SUGGESTED_OPTIONS+=	disk nfs swap users zfs-arc
 
 PLIST_VARS+=		apple-sensors battery contextswitch cpu entropy \
 			df disk		\
-			interface irq load memory nfs pf processes swap	\
+			interface irq load netstat_udp \
+			memory nfs pf processes swap	\
 			syslog tcpconns uptime users zfs-arc
 
 .for option in ${PLIST_VARS}
