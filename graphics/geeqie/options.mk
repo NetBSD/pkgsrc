1eca8379b7e2d3bb13794468f9998a62e855f8de
gdt <gdt@pkgsrc.org>
2015-09-12 12:54:10 +0000

    Disable libchamplain option by default.
    The libchamplain option used to work, but it doesn't seem to work in
    this version, and it pulls in a lot of dependencies.  Disable it by
    default until this is resolved.

--- graphics/geeqie/options.mk
+++ graphics/geeqie/options.mk
@@ -1,14 +1,18 @@
-# $NetBSD: options.mk,v 1.1 2013/05/07 11:08:40 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2015/09/12 12:54:10 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.geeqie
 PKG_SUPPORTED_OPTIONS=	libchamplain
-PKG_SUGGESTED_OPTIONS=	libchamplain
+# The libchamplain option doesn't actually work, so leave it off
+# to avoid the pain of dependencies with no benefit, until debugged.
+PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mlibchamplain)
 CONFIGURE_ARGS+=	--enable-gps
+CONFIGURE_ARGS+=	--enable-clutter
 .include "../../geography/libchamplain04/buildlink3.mk"
+.include "../../graphics/clutter/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-gps
 .endif
