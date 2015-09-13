48d06ab1f580dce7d6444ece3c295fd921511e16
joerg <joerg@pkgsrc.org>
2015-09-13 15:05:35 +0000

    Deal with new freetype header. Remove unwind.h override, the normal system header list already includes it.

--- www/seamonkey/patches/patch-mozilla_config_system__wrappers_unwind.h
+++
@@ -1,9 +0,0 @@
-$NetBSD: patch-mozilla_config_system__wrappers_unwind.h,v 1.5 2015/01/02 04:26:21 ryoon Exp $
-
---- mozilla/config/system_wrappers/unwind.h.orig	2015-01-01 17:44:51.000000000 +0000
-+++ mozilla/config/system_wrappers/unwind.h
-@@ -0,0 +1,4 @@
-+#pragma GCC system_header
-+#pragma GCC visibility push(default)
-+#include_next <unwind.h>
-+#pragma GCC visibility pop
