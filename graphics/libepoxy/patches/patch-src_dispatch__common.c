1d1ec46f6a44c818da2f4d5d6d1a057da586610d
tnn <tnn@pkgsrc.org>
2015-09-13 04:44:48 +0000

    Update to libepoxy-1.3.1 (2015-07-16)
    Changes:
    GL API registry update
    fixes for a nonconformant GL implementation
    some library size reductions
    support reproducible builds

--- graphics/libepoxy/patches/patch-src_dispatch__common.c
+++ graphics/libepoxy/patches/patch-src_dispatch__common.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_dispatch__common.c,v 1.1 2015/06/14 21:50:07 prlw1 Exp $
+$NetBSD: patch-src_dispatch__common.c,v 1.2 2015/09/13 04:44:49 tnn Exp $
 
 Native X has /usr/X11R7/lib/libGL.so.2.
 
---- src/dispatch_common.c.orig	2014-05-14 00:22:08.000000000 +0000
+--- src/dispatch_common.c.orig	2015-07-15 23:46:36.000000000 +0000
 +++ src/dispatch_common.c
-@@ -105,7 +105,7 @@
- #ifdef __APPLE__
- #define GLX_LIB "/opt/X11/lib/libGL.1.dylib"
+@@ -107,7 +107,7 @@
+ #elif defined(ANDROID)
+ #define GLX_LIB "libGLESv2.so"
  #else
 -#define GLX_LIB "libGL.so.1"
 +#define GLX_LIB "libGL.so"
  #endif
  
- struct api {
+ #ifdef ANDROID
