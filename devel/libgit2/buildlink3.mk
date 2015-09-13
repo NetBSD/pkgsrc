cbbd8ce0d0a5277abfcf8418aea2682d7f629b15
taca <taca@pkgsrc.org>
2015-09-13 12:21:13 +0000

    Update libgit2 to 0.23.2.
    Changes are too many to write here, please refer:
    	<https://github.com/libgit2/libgit2/releases>.

--- devel/libgit2/buildlink3.mk
+++ devel/libgit2/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/10/01 13:12:31 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/09/13 12:21:13 taca Exp $
 
 BUILDLINK_TREE+=	libgit2
 
 .if !defined(LIBGIT2_BUILDLINK3_MK)
 LIBGIT2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgit2+=	libgit2>=0.17.0
+BUILDLINK_API_DEPENDS.libgit2+=	libgit2>=0.23.0
 BUILDLINK_PKGSRCDIR.libgit2?=	../../devel/libgit2
 
 .include "../../devel/zlib/buildlink3.mk"
