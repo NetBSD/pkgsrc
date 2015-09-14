6d0843cbae025c84a5aadee484da72151b34633d
gdt <gdt@pkgsrc.org>
2015-09-14 14:40:37 +0000

    Re-enable glx on OS X.
    Upstream disabled glx on OS X (in an apparent attempt to make it build
    on systems without X11).  However, libepoxy without glx is defective,
    and gtk3 fails with it.  So it seems like the right thing is to ensure
    X11 presence for libepoxy.
    
    This patch reverts upstream's disabling of glx (setting configure
    variables/defines), making it be like libepoxy 1.2.  The resulting
    libepoxy has glx, and gtk3+ builds fine against it on OS X 10.9.
    
    (It remains TBD to sort out how we feel about X vs quartz, but that's
    a separable issue.)
    
    Reviewed by ttn@ and tron@.

--- graphics/libepoxy/patches/patch-src_dispatch__common.h
+++ graphics/libepoxy/patches/patch-src_dispatch__common.h
@@ -1,10 +1,19 @@
-$NetBSD: patch-src_dispatch__common.h,v 1.1 2015/06/13 19:13:10 tnn Exp $
+$NetBSD: patch-src_dispatch__common.h,v 1.2 2015/09/14 14:40:37 gdt Exp $
 
 Override EGL detection.
 
---- src/dispatch_common.h.orig	2014-05-14 00:22:08.000000000 +0000
+--- src/dispatch_common.h.orig	2015-07-15 23:46:36.000000000 +0000
 +++ src/dispatch_common.h
-@@ -34,7 +34,12 @@
+@@ -30,7 +30,7 @@
+ #define EPOXY_IMPORTEXPORT __declspec(dllexport)
+ #elif defined(__APPLE__)
+ #define PLATFORM_HAS_EGL 0
+-#define PLATFORM_HAS_GLX 0
++#define PLATFORM_HAS_GLX 1
+ #define PLATFORM_HAS_WGL 0
+ #define EPOXY_IMPORTEXPORT
+ #elif defined(ANDROID)
+@@ -39,7 +39,12 @@
  #define PLATFORM_HAS_WGL 0
  #define EPOXY_IMPORTEXPORT
  #else
