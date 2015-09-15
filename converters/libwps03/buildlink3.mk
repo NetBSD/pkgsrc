fe969711e75acb65c5fad05e79fb04c5a06fd544
joerg <joerg@pkgsrc.org>
2015-09-15 14:12:15 +0000

    Fix dependencies.

--- converters/libwps03/buildlink3.mk
+++ converters/libwps03/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/08/15 22:46:39 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/09/15 14:12:15 joerg Exp $
 
 BUILDLINK_TREE+=	libwps
 
 .if !defined(LIBWPS_BUILDLINK3_MK)
 LIBWPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libwps+=	libwps>=0.2.0
-BUILDLINK_ABI_DEPENDS.libwps?=	libwps>=0.3.1nb1
+BUILDLINK_API_DEPENDS.libwps+=	libwps>=0.2.0<0.4
+BUILDLINK_ABI_DEPENDS.libwps?=	libwps>=0.3.1nb1<0.4
 BUILDLINK_PKGSRCDIR.libwps?=	../../converters/libwps03
 
 .include "../../converters/librevenge/buildlink3.mk"
