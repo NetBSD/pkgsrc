7698ef4240c576c4f8018b44a5f33c9aa18a1992
joerg <joerg@pkgsrc.org>
2015-09-15 20:56:33 +0000

    Don't clean intermediate files after checksum/patch phase. The work directories are almost empty, so it doesn't save much space. It makes debugging annoying and it breaks local DISTDIR settings.

--- math/mpcomplex/inplace.mk
+++ math/mpcomplex/inplace.mk
@@ -1,4 +1,4 @@
-# $NetBSD: inplace.mk,v 1.3 2013/07/08 20:18:52 jperkin Exp $
+# $NetBSD: inplace.mk,v 1.4 2015/09/15 20:56:33 joerg Exp $
 #
 # Include this file to extract math/mpcomplex source into the WRKSRC of
 # another package. This is to be used by GCC packages to avoid the numerous
@@ -10,9 +10,9 @@ post-extract: extract-inplace-mpcomplex
 
 fetch-inplace-mpcomplex:
 	(cd ../../math/mpcomplex && ${MAKE} WRKDIR=${WRKSRC}/.devel.mpcomplex EXTRACT_DIR=${WRKSRC} \
-		WRKSRC='$${EXTRACT_DIR}/$${DISTNAME}' SKIP_DEPENDS=YES checksum clean)
+		WRKSRC='$${EXTRACT_DIR}/$${DISTNAME}' SKIP_DEPENDS=YES checksum)
 
 extract-inplace-mpcomplex:
 	(cd ../../math/mpcomplex && ${MAKE} WRKDIR=${WRKSRC}/.devel.mpcomplex EXTRACT_DIR=${WRKSRC} \
-		WRKSRC='$${EXTRACT_DIR}/$${DISTNAME}' SKIP_DEPENDS=YES patch clean)
+		WRKSRC='$${EXTRACT_DIR}/$${DISTNAME}' SKIP_DEPENDS=YES patch)
 	${MV} ${WRKSRC}/mpc-* ${WRKSRC}/mpc
