4a43ba053b909b57e06a766ec7e138eb81840883
he <he>
2015-09-16 21:12:39 +0000

    Pick up and apply some patches based on
    http://pkgs.fedoraproject.org/cgit/libreoffice.git/commit/?id=57cfb98d1c45259f946ff3444eeb6891a030e063
    
    which makes libreoffice43 build with boost 1.59.  This time it is
    verified to build with the rest of pkgsrc updated (as of today) on
    NetBSD/i386 6.1.5.

---
+++ misc/libreoffice43/patches/patch-solenv_gbuild_platform_com__MSC__defs.mk
@@ -0,0 +1,20 @@
+$NetBSD: patch-solenv_gbuild_platform_com__MSC__defs.mk,v 1.1 2015/09/16 21:12:39 he Exp $
+
+Pick up and apply some diffs based on
+
+http://pkgs.fedoraproject.org/cgit/libreoffice.git/commit/?id=57cfb98d1c45259f946ff3444eeb6891a030e063
+
+which makes libreoffice43 build with boost 1.59.
+
+--- solenv/gbuild/platform/com_MSC_defs.mk.orig	2015-04-20 19:36:29.000000000 +0000
++++ solenv/gbuild/platform/com_MSC_defs.mk
+@@ -41,6 +41,9 @@ gb_CXX := $(CXX)
+ endif
+ 
+ gb_COMPILERDEFS := \
++	-DBOOST_ERROR_CODE_HEADER_ONLY \
++	-DBOOST_OPTIONAL_USE_OLD_DEFINITION_OF_NONE \
++	-DBOOST_SYSTEM_NO_DEPRECATED \
+ 	-D_CRT_NON_CONFORMING_SWPRINTFS \
+ 	-D_CRT_NONSTDC_NO_DEPRECATE \
+ 	-D_CRT_SECURE_NO_DEPRECATE \
