fb3c73bde6202ef91bf8ee2e2c302b9642c33924
joerg <joerg@pkgsrc.org>
2015-09-17 17:12:19 +0000

    Don't trigger unnecessary rebuilds.

--- security/heimdal/patches/patch-lib_libedit_src_vis.c
+++ security/heimdal/patches/patch-lib_libedit_src_vis.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-lib_libedit_src_vis.c,v 1.3 2015/07/11 04:26:57 rodent Exp $
+$NetBSD: patch-lib_libedit_src_vis.c,v 1.4 2015/09/17 17:12:19 joerg Exp $
 
 * Fix OpenBSD 5.5 build
 
 --- lib/libedit/src/vis.c.orig	2012-12-09 22:06:44.000000000 +0000
 +++ lib/libedit/src/vis.c
-@@ -67,6 +67,7 @@ __RCSID("$NetBSD: vis.c,v 1.41 2009/11/2
+@@ -67,6 +67,7 @@
  #include <vis.h>
  #include <stdlib.h>
  
