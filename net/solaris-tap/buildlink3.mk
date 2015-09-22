5d94d544bb978af4fa0648093bef90fbc1516e24
tnn <tnn@pkgsrc.org>
2015-09-22 02:38:32 +0000

    use the correct version in solaris-tap's bl3.mk. Revbump dependees.

--- net/solaris-tap/buildlink3.mk
+++ net/solaris-tap/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:12 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/09/22 02:38:32 tnn Exp $
 
 BUILDLINK_TREE+=	solaris-tap
 
 .if !defined(SOLARIS_TAP_BUILDLINK3_MK)
 SOLARIS_TAP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.solaris-tap+=	solaris-tap>=20070525
+BUILDLINK_API_DEPENDS.solaris-tap+=	solaris-tap>=1.1.0
 BUILDLINK_PKGSRCDIR.solaris-tap?=	../../net/solaris-tap
 BUILDLINK_DEPMETHOD.solaris-tap?=	build
 .endif # SOLARIS_TAP_BUILDLINK3_MK
