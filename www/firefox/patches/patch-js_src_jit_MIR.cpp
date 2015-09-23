0db7e1cc1b1fb014947ae5cecb5c99b093a2061b
ryoon <ryoon@pkgsrc.org>
2015-09-23 06:44:41 +0000

    Update to 41.0
    Changelog:
        New Enhance IME support on Windows (Vista +) using TSF (Text Services Framework)
        New Ability to set a profile picture for your Firefox Account
        New Firefox Hello now includes instant messaging
        New SVG images can be used as favicons
        New Improved box-shadow rendering performance
        Changed WebRTC now requires perfect forward secrecy
        Changed WARP is disabled on Windows 7
        Changed Updates to image decoding process
        Changed Support for running animations of 'transform' and 'opacity' on the compositor thread
        HTML5 MessageChannel and MessagePort API enabled by default
        HTML5 Added support for the transform-origin property on SVG elements
        HTML5 CSS Font Loading API enabled by default
        HTML5 Navigator.onLine now varies with actual internet connectivity (Windows and Mac OS X only)
        HTML5 Copy/Cut Web content from JavaScript to the OS clipboard with document.execCommand("cut"/"copy")
        HTML5 Implemented Cache API for querying named caches that are accessible Window, Worker, and ServiceWorker
        Developer Removed support for binary XPCOM components in extensions, use addon SDK "system/child_process" pipe mechanism for native binaries instead
        Developer Network requests can be exported in HAR format
        Developer Quickly add new CSS rule with New Rule button in the Inspector
        Developer Screenshot a node or element from markup view with the Screenshot Node context menu item
        Developer Copy element CSS rule declarations with the Copy Rule Declaration context menu item in the Inspector
        Developer Pseudo-Class panel in the Inspector
        Fixed Picture element does not react to resize/viewport changes
        Fixed Various security fixes
    
    Security fixes:
    Fixed in Firefox 41
        2015-114 Information disclosure via the High Resolution Time API
        2015-113 Memory safety errors in libGLES in the ANGLE graphics library
        2015-112 Vulnerabilities found through code inspection
        2015-111 Errors in the handling of CORS preflight request headers
        2015-110 Dragging and dropping images exposes final URL after redirects
        2015-109 JavaScript immutable property enforcement can be bypassed
        2015-108 Scripted proxies can access inner window
        2015-107 Out-of-bounds read during 2D canvas display on Linux 16-bit color depth systems
        2015-106 Use-after-free while manipulating HTML media content
        2015-105 Buffer overflow while decoding WebM video
        2015-104 Use-after-free with shared workers and IndexedDB
        2015-103 URL spoofing in reader mode
        2015-102 Crash when using debugger with SavedStacks in JavaScript
        2015-101 Buffer overflow in libvpx while parsing vp9 format video
        2015-100 Arbitrary file manipulation by local user through Mozilla updater
        2015-99 Site attribute spoofing on Android by pasting URL with unknown scheme
        2015-98 Out of bounds read in QCMS library with ICC V4 profile attributes
        2015-97 Memory leak in mozTCPSocket to servers
        2015-96 Miscellaneous memory safety hazards (rv:41.0 / rv:38.3)

--- www/firefox/patches/patch-js_src_jit_MIR.cpp
+++ www/firefox/patches/patch-js_src_jit_MIR.cpp
@@ -1,13 +1,13 @@
-$NetBSD: patch-js_src_jit_MIR.cpp,v 1.3 2015/05/12 22:48:54 ryoon Exp $
+$NetBSD: patch-js_src_jit_MIR.cpp,v 1.4 2015/09/23 06:44:42 ryoon Exp $
 
---- js/src/jit/MIR.cpp.orig	2015-05-04 00:43:27.000000000 +0000
+--- js/src/jit/MIR.cpp.orig	2015-08-24 21:53:12.000000000 +0000
 +++ js/src/jit/MIR.cpp
-@@ -71,7 +71,7 @@ MDefinition::PrintOpcodeName(FILE* fp, M
+@@ -72,7 +72,7 @@ MDefinition::PrintOpcodeName(GenericPrin
      const char* name = names[op];
      size_t len = strlen(name);
      for (size_t i = 0; i < len; i++)
--        fprintf(fp, "%c", tolower(name[i]));
-+        fprintf(fp, "%c", tolower(((unsigned char)name[i])));
+-        out.printf("%c", tolower(name[i]));
++        out.printf("%c", tolower((unsigned char)name[i]));
  }
  
  const Value&
--- www/firefox/patches/patch-js_src_jit_arm_Architecture-arm.cpp
+++ www/firefox/patches/patch-js_src_jit_arm_Architecture-arm.cpp
@@ -1,12 +1,12 @@
-$NetBSD: patch-js_src_jit_arm_Architecture-arm.cpp,v 1.5 2015/05/12 22:48:54 ryoon Exp $
+$NetBSD: patch-js_src_jit_arm_Architecture-arm.cpp,v 1.6 2015/09/23 06:44:42 ryoon Exp $
 
---- js/src/jit/arm/Architecture-arm.cpp.orig	2015-05-04 00:43:27.000000000 +0000
+--- js/src/jit/arm/Architecture-arm.cpp.orig	2015-08-24 21:53:12.000000000 +0000
 +++ js/src/jit/arm/Architecture-arm.cpp
 @@ -16,7 +16,7 @@
  #include "jit/arm/Assembler-arm.h"
  #include "jit/RegisterSets.h"
  
--#if !defined(__linux__) || defined(ANDROID) || defined(JS_ARM_SIMULATOR)
+-#if !defined(__linux__) || defined(ANDROID) || defined(JS_SIMULATOR_ARM)
 +#if !defined(__linux__) || defined(ANDROID) || defined(JS_ARM_SIMULATOR) || defined(__NetBSD__)
  // The Android NDK and B2G do not include the hwcap.h kernel header, and it is not
  // defined when building the simulator, so inline the header defines we need.
