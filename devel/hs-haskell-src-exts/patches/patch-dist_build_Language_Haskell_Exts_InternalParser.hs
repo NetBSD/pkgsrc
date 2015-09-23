b779fff41ef0983926f171172eb2f13d764d0434
joerg <joerg@pkgsrc.org>
2015-09-23 11:42:57 +0000

    Drop pointless preprocessor noise.

---
+++ devel/hs-haskell-src-exts/patches/patch-dist_build_Language_Haskell_Exts_InternalParser.hs
@@ -0,0 +1,35 @@
+$NetBSD: patch-dist_build_Language_Haskell_Exts_InternalParser.hs,v 1.1 2015/09/23 11:42:57 joerg Exp $
+
+--- dist/build/Language/Haskell/Exts/InternalParser.hs.orig	2015-09-22 21:11:24.000000000 +0000
++++ dist/build/Language/Haskell/Exts/InternalParser.hs
+@@ -8914,30 +8914,6 @@ applyFixities' (Just fixs) ast = applyFi
+ {-# LINE 1 "<built-in>" #-}
+ {-# LINE 1 "<command-line>" #-}
+ {-# LINE 8 "<command-line>" #-}
+-# 1 "/usr/include/stdc-predef.h" 1 3 4
+-
+-# 17 "/usr/include/stdc-predef.h" 3 4
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+ 
+ 
+ {-# LINE 8 "<command-line>" #-}
