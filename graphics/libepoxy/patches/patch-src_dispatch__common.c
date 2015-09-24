a8e134f24c939dabd5fc99c1af69499164f87326
tnn <tnn@pkgsrc.org>
2015-09-24 22:51:29 +0000

    PR pkg/50256: pretend to return NULL in epoxy_get_proc_address after errx()

--- graphics/libepoxy/patches/patch-src_dispatch__common.c
+++ graphics/libepoxy/patches/patch-src_dispatch__common.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_dispatch__common.c,v 1.2 2015/09/13 04:44:49 tnn Exp $
+$NetBSD: patch-src_dispatch__common.c,v 1.3 2015/09/24 22:51:29 tnn Exp $
 
 Native X has /usr/X11R7/lib/libGL.so.2.
+Function must return a value if errx not declared with attr noreturn.
 
 --- src/dispatch_common.c.orig	2015-07-15 23:46:36.000000000 +0000
 +++ src/dispatch_common.c
@@ -13,3 +14,11 @@ Native X has /usr/X11R7/lib/libGL.so.2.
  #endif
  
  #ifdef ANDROID
+@@ -672,6 +672,7 @@ epoxy_get_proc_address(const char *name)
+ #endif
+     }
+     errx(1, "Couldn't find current GLX or EGL context.\n");
++    return NULL;
+ #endif
+ }
+ 
