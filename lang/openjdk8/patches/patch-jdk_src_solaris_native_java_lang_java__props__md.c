9c894da2697aa64b9b1b903ea87276e602bba9bf
tnn <tnn@pkgsrc.org>
2015-09-24 22:54:28 +0000

    add/fix patch comments

--- lang/openjdk8/patches/patch-jdk_src_solaris_native_java_lang_java__props__md.c
+++ lang/openjdk8/patches/patch-jdk_src_solaris_native_java_lang_java__props__md.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-jdk_src_solaris_native_java_lang_java__props__md.c,v 1.1 2015/02/08 08:41:25 tnn Exp $
+$NetBSD: patch-jdk_src_solaris_native_java_lang_java__props__md.c,v 1.2 2015/09/24 22:54:28 tnn Exp $
+
+DragonFly support.
 
 --- jdk/src/solaris/native/java/lang/java_props_md.c.orig	2015-02-02 15:37:12.000000000 +0000
 +++ jdk/src/solaris/native/java/lang/java_props_md.c
