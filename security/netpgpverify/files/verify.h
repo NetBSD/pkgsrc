df3910554799c1ac4e12352e47db575074285686
agc <agc@pkgsrc.org>
2015-09-25 15:46:58 +0000

    Update netpgpverify, and libnetpgpverify, from version 20150911 to version 20150919
    Changes:
    
    + get rid of unnecessary header inclusion (sys/syslog.h), which gives
    problems on HP/UX and is unused
    
    from Tobias Nygren

--- security/netpgpverify/files/verify.h
+++ security/netpgpverify/files/verify.h
@@ -23,9 +23,9 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #ifndef NETPGP_VERIFY_H_
-#define NETPGP_VERIFY_H_	20150911
+#define NETPGP_VERIFY_H_	20150919
 
-#define NETPGPVERIFY_VERSION	"netpgpverify portable 20150901"
+#define NETPGPVERIFY_VERSION	"netpgpverify portable 20150919"
 
 #include <sys/types.h>
 
