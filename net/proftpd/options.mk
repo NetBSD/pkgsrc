eca73f2d85550fa6358a86f9c9deea21c8b1bcb5
wiedi <wiedi@pkgsrc.org>
2015-09-25 10:01:36 +0000

    Package ProFTPD using DSO (Dynamic Shared Objects) support.
    This is preferable for binary package use as it allowes the user to choose
    which features to enable by changeing the configuration file instead of
    recompiling. This is also how ProFTPD is usually packaged in other systems.
    
    For details about ProFTPD and DSO see:
    	http://www.proftpd.org/docs/howto/DSO.html
    
    This change removes the following PKG_OPTIONS.proftpd:
    	ban, ldap, mysql, pgsql, proftpd-readme, quota, tls and wrap
    
    The modules that were previously compiled when enabling ban, proftpd-readme,
    quota or tls are now always included. To load them use a configuration
    directive like:
    	LoadModule mod_ban.c
    
    In addition the proftpd package includes by default many other modules that
    were previously unavailble like: mod_load, mod_radius, mod_sftp and more.
    
    The module that was provided by the wrap option is replaced by the wrap2 module
    which is also always included.
    
    The ldap option is superseded by the proftpd-ldap package.
    The mysql option is superseded by the proftpd-mysql package.
    The pgsql option is superseded by the proftpd-postgresql package.
    Using proftpd-postgresql will create one binary package for each PostgreSQL
    version in pkgsrc.
    
    In addition the following added packages provide new functionality:
    	- proftpd-geoip (access GeoIP details)
    	- proftpd-memcached (mod_memcache and mod_tls_memcache)
    	- proftpd-odbc (access any ODBC database)
    	- proftpd-sqlite (access to sqlite3)

--- net/proftpd/options.mk
+++ net/proftpd/options.mk
@@ -1,18 +1,20 @@
-# $NetBSD: options.mk,v 1.12 2013/10/23 15:46:22 jperkin Exp $
+# $NetBSD: options.mk,v 1.13 2015/09/25 10:01:37 wiedi Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.proftpd
-PKG_SUPPORTED_OPTIONS=	pam inet6 wrap tls quota ldap proftpd-readme ban
-PKG_OPTIONS_OPTIONAL_GROUPS+=	sql
-PKG_OPTIONS_GROUP.sql=		mysql pgsql
+PKG_SUPPORTED_OPTIONS=	pam inet6
 PKG_SUGGESTED_OPTIONS=	pam inet6
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	quota sql tls
+PLIST_VARS+=	pam
+
+MODULES=	# empty
 
 .if !empty(PKG_OPTIONS:Mpam)
 CONFIGURE_ARGS+=	--enable-auth-pam
 .include "../../mk/pam.buildlink3.mk"
+MODULES+=	mod_sftp_pam
+PLIST.pam=	yes
 .else
 CONFIGURE_ARGS+=	--disable-auth-pam
 .endif
@@ -21,63 +23,4 @@ CONFIGURE_ARGS+=	--disable-auth-pam
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
-.endif
-
-MODULES=	# empty
-
-.if !empty(PKG_OPTIONS:Mwrap)
-MODULES:=	${MODULES}:mod_wrap
-.include "../../security/tcp_wrappers/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mtls)
-MODULES:=	${MODULES}:mod_tls
-PLIST.tls=	yes
-.include "../../security/openssl/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mquota)
-MODULES:=	${MODULES}:mod_quotatab:mod_quotatab_file
-PLIST.quota=	yes
-.endif
-
-.if !empty(PKG_OPTIONS:Mquota) && !empty(PKG_OPTIONS:Mmysql)
-MODULES:=	${MODULES}:mod_quotatab_sql
-.endif
-
-.if !empty(PKG_OPTIONS:Mquota) && !empty(PKG_OPTIONS:Mpgsql)
-MODULES:=	${MODULES}:mod_quotatab_sql
-.endif
-
-.if !empty(PKG_OPTIONS:Mquota) && !empty(PKG_OPTIONS:Mldap)
-MODULES:=	${MODULES}:mod_quotatab_ldap
-.endif
-
-.if !empty(PKG_OPTIONS:Mldap)
-MODULES:=	${MODULES}:mod_ldap
-.include "../../databases/openldap-client/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mmysql)
-MODULES:=	${MODULES}:mod_sql:mod_sql_mysql
-PLIST.sql=	yes
-.include "../../mk/mysql.buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mpgsql)
-MODULES:=	${MODULES}:mod_sql:mod_sql_postgres
-PLIST.sql=	yes
-.include "../../mk/pgsql.buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mproftpd-readme)
-MODULES:=	${MODULES}:mod_readme
-.endif
-
-.if !empty(PKG_OPTIONS:Mban)
-MODULES:=	${MODULES}:mod_ban
-.endif
-
-.if !empty(MODULES)
-CONFIGURE_ARGS+=	--with-modules=${MODULES:C/^://}
 .endif
