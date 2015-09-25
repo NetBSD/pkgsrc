ba0e2b97b1ba348bd526f6cc298eff9aeefdb6d2
ryoon <ryoon@pkgsrc.org>
2015-09-25 14:37:27 +0000

    Update to 7.0
    Changelog:
    PulseAudio 7.0
    
    Changes at a glance:
    
      * LFE channel synthesis with low-pass filtering
      * New libsoxr based resamplers
      * Socket activation support for TCP
      * The "srbchannel" IPC mechanism enabled by default
      * More flexible jack detection support when using UCM
      * Exiting due to SIGTERM isn't considered a failure
      * Better support for Creative SoundBlaster Omni Surround 5.1

--- audio/pulseaudio/patches/patch-src_daemon_main.c
+++ audio/pulseaudio/patches/patch-src_daemon_main.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_daemon_main.c,v 1.2 2015/02/21 23:13:10 ryoon Exp $
+$NetBSD: patch-src_daemon_main.c,v 1.3 2015/09/25 14:37:27 ryoon Exp $
 
 Only display Linux-specific warning on Linux.
 
---- src/daemon/main.c.orig	2015-02-12 14:10:35.000000000 +0000
+--- src/daemon/main.c.orig	2015-09-15 04:46:06.000000000 +0000
 +++ src/daemon/main.c
 @@ -997,10 +997,12 @@ int main(int argc, char *argv[]) {
  
@@ -10,9 +10,9 @@ Only display Linux-specific warning on Linux.
  
 +#if defined(__linux__)
      if (pa_rtclock_hrtimer())
-         pa_log_info("Fresh high-resolution timers available! Bon appetit!");
+         pa_log_info("Fresh high-resolution timers available! Bon appetit.");
      else
-         pa_log_info("Dude, your kernel stinks! The chef's recommendation today is Linux with high-resolution timers enabled!");
+         pa_log_info("Dude, your kernel stinks! The chef's recommendation today is Linux with high-resolution timers enabled.");
 +#endif
  
      if (conf->lock_memory) {
