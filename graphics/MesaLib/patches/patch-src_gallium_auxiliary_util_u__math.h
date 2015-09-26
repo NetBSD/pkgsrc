afe9679f2f386746897f6c8c00d73677d5d04d04
tnn <tnn@pkgsrc.org>
2015-09-26 08:45:02 +0000

    Update to mesa-11.0.0 (2015-09-12)
    Tested on:
    - NetBSD 7.0/i386 EeePC 1001PXD with i915 (Pineview)
    - NetBSD 7.99.21/amd64 with Radeon HD 5450 (Evergreen/Cedar)
    
    pkgsrc changes:
    - make i386 & x86_64 glx-tls dispatch assembly stubs aware of that the
      dispatch table pointer may be NULL due to TLS implementation limitations
    - work around run time loader issue on NetBSD (#50277)
    - TLS dispatch support is now enabled again on NetBSD
    - llvm option now requires libLLVM 3.7.0 with AMDGPU target
    - build xatracker library on x86, useful for xf86-video-vmware acceleration
    - always use shared glapi
    - always include dri3 support (it will still use dri2 if dri3 not supported)
    
    New in Mesa 11:
    - New hardware support for AMD GCN 1.2 GPUs: Tonga, Iceland, Carrizo, Fiji
    - OpenGL 4.1 on radeonsi, nvc0
    - OpenGL ES 3.0 on freedreno (a3xx, a4xx)
    - GL_AMD_vertex_shader_viewport_index on radeonsi
    - GL_ARB_conditional_render_inverted on r600, radeonsi
    - GL_ARB_depth_buffer_float on a4xx
    - GL_ARB_derivative_control on radeonsi
    - GL_ARB_draw_buffers, GL_ARB_draw_buffers_blend on a4xx
    - GL_ARB_fragment_layer_viewport on radeonsi
    - GL_ARB_framebuffer_no_attachments on i965
    - GL_ARB_get_texture_sub_image for all drivers
    - GL_ARB_gpu_shader5 on radeonsi
    - GL_ARB_gpu_shader_fp64 on llvmpipe, radeonsi
    - GL_ARB_shader_image_load_store on i965
    - GL_ARB_shader_precision on radeonsi, nvc0
    - GL_ARB_shader_image_size on i965
    - GL_ARB_shader_stencil_export on llvmpipe
    - GL_ARB_shader_subroutine on core profile all drivers
    - GL_ARB_tessellation_shader on nvc0, radeonsi
    - GL_ARB_transform_feedback2, GL_ARB_transform_feedback_instanced,
      GL_EXT_transform_feedback on a3xx, a4xx
    - GL_ARB_vertex_attrib_64bit on llvmpipe, radeonsi
    - GL_ARB_viewport_array on radeonsi
    - GL_EXT_depth_bounds_test on radeonsi, nv30, nv50, nvc0
    - GL_EXT_texture_compression_s3tc on freedreno (a3xx)
    - GL_NV_read_depth (GLES) on all drivers
    - GL_NV_read_depth_stencil (GLES) on all drivers
    - GL_NV_read_stencil (GLES) on all drivers
    - GL_OES_texture_float on all r300, r600, radeonsi, nv30, nv50, nvc0, softpipe,
      llvmpipe
    - GL_OES_texture_half_float on all r300, r600, radeonsi, nv30, nv50, nvc0,
      softpipe, llvmpipe
    - GL_OES_texture_float_linear on all r300, r600, radeonsi, nv30, nv50, nvc0,
      softpipe, llvmpipe
    - GL_OES_texture_half_float_linear on all r300, r600, radeonsi, nv30, nv50,
      nvc0, softpipe, llvmpipe
    - GL_EXT_draw_buffers2 on a4xx
    - GLX_ARB_create_context_robustness on r600, radeonsi
    - EGL_EXT_create_context_robustness on r600, radeonsi
    - EGL_KHR_gl_colorspace on r600, radeonsi, nv50, nvc0
    - EGL_KHR_gl_texture_3D_image on r600, radeonsi, nv50, nvc0
    - EGL 1.5 on r600, radeonsi, nv50, nvc0

--- graphics/MesaLib/patches/patch-src_gallium_auxiliary_util_u__math.h
+++ graphics/MesaLib/patches/patch-src_gallium_auxiliary_util_u__math.h
@@ -1,21 +1,11 @@
-$NetBSD: patch-src_gallium_auxiliary_util_u__math.h,v 1.3 2015/09/11 00:03:36 tnn Exp $
+$NetBSD: patch-src_gallium_auxiliary_util_u__math.h,v 1.4 2015/09/26 08:45:02 tnn Exp $
 
 The C99 "restrict" keyword is not valid in C++, but most compilers will
 understand __restrict.
 
---- src/gallium/auxiliary/util/u_math.h.orig	2015-09-10 17:40:30.000000000 +0000
+--- src/gallium/auxiliary/util/u_math.h.orig	2015-09-11 17:41:47.000000000 +0000
 +++ src/gallium/auxiliary/util/u_math.h
-@@ -401,6 +401,9 @@ unsigned ffs( unsigned u )
- #if defined(__MINGW32__) || defined(PIPE_OS_ANDROID) || \
-     defined(HAVE___BUILTIN_FFSLL)
- #define ffsll __builtin_ffsll
-+#elif defined(__NetBSD__)
-+#include <sys/bitops.h>
-+#define ffsll ffs64
- #endif
- 
- #endif /* FFSLL_DEFINED */
-@@ -690,6 +693,11 @@ util_bswap16(uint16_t n)
+@@ -709,6 +712,11 @@ util_bswap16(uint16_t n)
            (n << 8);
  }
  
@@ -24,10 +14,10 @@ understand __restrict.
 +#define restrict __restrict
 +#endif
 +
- static INLINE void*
+ static inline void*
  util_memcpy_cpu_to_le32(void * restrict dest, const void * restrict src, size_t n)
  {
-@@ -708,6 +716,11 @@ util_memcpy_cpu_to_le32(void * restrict 
+@@ -727,6 +735,11 @@ util_memcpy_cpu_to_le32(void * restrict 
  #endif
  }
  
