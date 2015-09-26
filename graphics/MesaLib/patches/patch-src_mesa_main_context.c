d1e98c9a4da102855bc5aced0530ac3c584173b9
tnn <tnn@pkgsrc.org>
2015-09-26 08:45:02 +0000

    Update to mesa-11.0.0 (2015-09-12)
    Tested on:
    - NetBSD 7.0/i386 EeePC 1001PXD with i915 (Pineview)
    - NetBSD 7.99.21/amd64 with Radeon HD 5450 (Evergreen/Cedar)
    
    pkgsrc changes:
    - make i386 & x86_64 glx-tls dispatch assembly stubs aware of that the
      dispatch table pointer may be NULL due to TLS implementation limitations
    - work around run time loader issue on NetBSD (#50277)
    - TLS dispatch support is now enabled again on NetBSD
    - llvm option now requires libLLVM 3.7.0 with AMDGPU target
    - build xatracker library on x86, useful for xf86-video-vmware acceleration
    - always use shared glapi
    - always include dri3 support (it will still use dri2 if dri3 not supported)
    
    New in Mesa 11:
    - New hardware support for AMD GCN 1.2 GPUs: Tonga, Iceland, Carrizo, Fiji
    - OpenGL 4.1 on radeonsi, nvc0
    - OpenGL ES 3.0 on freedreno (a3xx, a4xx)
    - GL_AMD_vertex_shader_viewport_index on radeonsi
    - GL_ARB_conditional_render_inverted on r600, radeonsi
    - GL_ARB_depth_buffer_float on a4xx
    - GL_ARB_derivative_control on radeonsi
    - GL_ARB_draw_buffers, GL_ARB_draw_buffers_blend on a4xx
    - GL_ARB_fragment_layer_viewport on radeonsi
    - GL_ARB_framebuffer_no_attachments on i965
    - GL_ARB_get_texture_sub_image for all drivers
    - GL_ARB_gpu_shader5 on radeonsi
    - GL_ARB_gpu_shader_fp64 on llvmpipe, radeonsi
    - GL_ARB_shader_image_load_store on i965
    - GL_ARB_shader_precision on radeonsi, nvc0
    - GL_ARB_shader_image_size on i965
    - GL_ARB_shader_stencil_export on llvmpipe
    - GL_ARB_shader_subroutine on core profile all drivers
    - GL_ARB_tessellation_shader on nvc0, radeonsi
    - GL_ARB_transform_feedback2, GL_ARB_transform_feedback_instanced,
      GL_EXT_transform_feedback on a3xx, a4xx
    - GL_ARB_vertex_attrib_64bit on llvmpipe, radeonsi
    - GL_ARB_viewport_array on radeonsi
    - GL_EXT_depth_bounds_test on radeonsi, nv30, nv50, nvc0
    - GL_EXT_texture_compression_s3tc on freedreno (a3xx)
    - GL_NV_read_depth (GLES) on all drivers
    - GL_NV_read_depth_stencil (GLES) on all drivers
    - GL_NV_read_stencil (GLES) on all drivers
    - GL_OES_texture_float on all r300, r600, radeonsi, nv30, nv50, nvc0, softpipe,
      llvmpipe
    - GL_OES_texture_half_float on all r300, r600, radeonsi, nv30, nv50, nvc0,
      softpipe, llvmpipe
    - GL_OES_texture_float_linear on all r300, r600, radeonsi, nv30, nv50, nvc0,
      softpipe, llvmpipe
    - GL_OES_texture_half_float_linear on all r300, r600, radeonsi, nv30, nv50,
      nvc0, softpipe, llvmpipe
    - GL_EXT_draw_buffers2 on a4xx
    - GLX_ARB_create_context_robustness on r600, radeonsi
    - EGL_EXT_create_context_robustness on r600, radeonsi
    - EGL_KHR_gl_colorspace on r600, radeonsi, nv50, nvc0
    - EGL_KHR_gl_texture_3D_image on r600, radeonsi, nv50, nvc0
    - EGL 1.5 on r600, radeonsi, nv50, nvc0

--- graphics/MesaLib/patches/patch-src_mesa_main_context.c
+++ graphics/MesaLib/patches/patch-src_mesa_main_context.c
@@ -1,14 +1,28 @@
-$NetBSD: patch-src_mesa_main_context.c,v 1.2 2015/04/25 11:19:18 tnn Exp $
+$NetBSD: patch-src_mesa_main_context.c,v 1.3 2015/09/26 08:45:02 tnn Exp $
 
 * Fix exit time segfault of qt5 application with modular xorg
 
---- src/mesa/main/context.c.orig	2015-03-28 18:20:39.000000000 +0000
+--- src/mesa/main/context.c.orig	2015-09-11 17:41:47.000000000 +0000
 +++ src/mesa/main/context.c
-@@ -378,10 +378,10 @@ mtx_t OneTimeLock = _MTX_INITIALIZER_NP;
-  *
-  * \sa _math_init().
+@@ -350,11 +350,14 @@ mtx_t OneTimeLock = _MTX_INITIALIZER_NP;
+  * Calls all the various one-time-fini functions in Mesa
   */
+ 
+-static void
 +static GLbitfield api_init_mask = 0x0;
++static void __attribute__((__destructor__))
+ one_time_fini(void)
+ {
+-   _mesa_destroy_shader_compiler();
+-   _mesa_locale_fini();
++   if (api_init_mask) {
++      _mesa_destroy_shader_compiler();
++      _mesa_locale_fini();
++   }
+ }
+ 
+ /**
+@@ -369,7 +372,6 @@ one_time_fini(void)
  static void
  one_time_init( struct gl_context *ctx )
  {
@@ -16,25 +30,12 @@ $NetBSD: patch-src_mesa_main_context.c,v 1.2 2015/04/25 11:19:18 tnn Exp $
  
     mtx_lock(&OneTimeLock);
  
-@@ -428,14 +428,16 @@ one_time_init( struct gl_context *ctx )
+@@ -394,8 +396,6 @@ one_time_init( struct gl_context *ctx )
+          _mesa_ubyte_to_float_color_tab[i] = (float) i / 255.0F;
+       }
  
-    mtx_unlock(&OneTimeLock);
- 
--   /* Hopefully atexit() is widely available.  If not, we may need some
--    * #ifdef tests here.
--    */
--   atexit(_mesa_destroy_shader_compiler);
+-      atexit(one_time_fini);
 -
-    dummy_enum_func();
- }
- 
-+static void __attribute__((__destructor__))
-+one_time_fini(void)
-+{
-+  if (api_init_mask)
-+    _mesa_destroy_shader_compiler();
-+}
-+
- 
- /**
-  * Initialize fields of gl_current_attrib (aka ctx->Current.*)
+ #if defined(DEBUG) && defined(__DATE__) && defined(__TIME__)
+       if (MESA_VERBOSE != 0) {
+ 	 _mesa_debug(ctx, "Mesa %s DEBUG build %s %s\n",
