4d6e789382e1b5aad4a3d7c564d0df2a6fe7a5c1
tnn <tnn@pkgsrc.org>
2015-09-27 10:07:52 +0000

    remove games/foobillard (dead homepage and master site) and instead add games/foobillardplus which is a continuation of the old code by a new maintainer.

---
+++ games/foobillardplus/patches/patch-src_billard3d.c
@@ -0,0 +1,15 @@
+$NetBSD: patch-src_billard3d.c,v 1.1 2015/09/27 10:07:53 tnn Exp $
+
+Don't use Linux specific getopt_long_only.
+
+--- src/billard3d.c.orig	2012-01-02 19:23:49.000000000 +0000
++++ src/billard3d.c
+@@ -7597,7 +7597,7 @@ int main( int argc, char *argv[] )
+ 
+    /* config file */
+    load_config( &confv, &confc, argv, argc );
+-   while( ( act_option = getopt_long_only(confc, confv, "+", long_options, &option_index) ) >= 0){
++   while( ( act_option = getopt_long(confc, confv, "+", long_options, &option_index) ) >= 0){
+        //fprintf(stderr,"processing option %d=%s\n",act_option,optarg);
+        process_option(act_option);
+    }
