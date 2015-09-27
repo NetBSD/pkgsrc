6843d99c2584bb11e8b59898865d85fcfaaf1482
tnn <tnn>
2015-09-27 10:07:52 +0000

    remove games/foobillard (dead homepage and master site) and instead add games/foobillardplus which is a continuation of the old code by a new maintainer.

---
+++ games/foobillardplus/patches/patch-src_sound__stuff.c
@@ -0,0 +1,16 @@
+$NetBSD: patch-src_sound__stuff.c,v 1.1 2015/09/27 10:07:53 tnn Exp $
+
+Wow. Amazing that this doesn't crash for anyone else.
+Reported upstream in private mail.
+
+--- src/sound_stuff.c.orig	2011-12-26 22:41:48.000000000 +0000
++++ src/sound_stuff.c
+@@ -96,7 +96,7 @@ int strsound ( char s1[] )
+    int i = 0;
+    char s[10];
+    if(strlen(s1) > 4) {
+-      strcpy(s,&s[strlen(s)-4]);
++      strcpy(s,&s1[strlen(s1)-4]);
+       while (s[i]) {
+          s[i] = toupper(s[i]);
+          ++i;
---
+++ games/foobillardplus/patches/patch-src_sys__stuff.c
@@ -0,0 +1,25 @@
+$NetBSD: patch-src_sys__stuff.c,v 1.1 2015/09/27 10:07:53 tnn Exp $
+
+Use pkgsrc's installation prefix instead of /proc vooodoo.
+
+--- src/sys_stuff.c.orig	2012-01-02 19:23:49.000000000 +0000
++++ src/sys_stuff.c
+@@ -760,6 +760,7 @@ static char data_dir[512];
+ 
+ void enter_data_dir() {
+     int success = 1;
++#if 0
+ 
+ #ifdef POSIX
+     char proc_exe[20];
+@@ -805,6 +806,10 @@ void enter_data_dir() {
+         success = 1;
+     } while (0);
+ 
++#endif
++    strncpy(data_dir, "@DATADIR@", sizeof(data_dir));
++    if (chdir(data_dir) < 0)
++        success = 0;
+     if (!success) {
+         fprintf(stderr,
+             "Foobillard++ seems not to be correctly installed\n"
