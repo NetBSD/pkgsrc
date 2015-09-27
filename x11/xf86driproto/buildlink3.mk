f7a7f696326bbc30e468ee9d57fbc55bcc69603b
tnn <tnn@pkgsrc.org>
2015-09-27 05:17:49 +0000

    Afaict xf86driproto does in fact not need libpciaccess. Direct rendering is available on platforms without PCI. Drop libpciaccess and bump PKGREVISION.

--- x11/xf86driproto/buildlink3.mk
+++ x11/xf86driproto/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/04/29 06:30:26 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2015/09/27 05:17:49 tnn Exp $
 
 BUILDLINK_TREE+=	xf86driproto
 
@@ -10,8 +10,6 @@ BUILDLINK_DEPMETHOD.xf86driproto?=	build
 BUILDLINK_API_DEPENDS.xf86driproto+=	xf86driproto>=2.0.3
 BUILDLINK_PKGSRCDIR.xf86driproto?=	../../x11/xf86driproto
 
-BUILDLINK_DEPMETHOD.libpciaccess?=	build
-.include "../../sysutils/libpciaccess/buildlink3.mk"
 .endif # XF86DRIPROTO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xf86driproto
