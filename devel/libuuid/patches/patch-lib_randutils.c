67d28dad4e250850e73fe2f3c613b7e9340c39fa
wiz <wiz@pkgsrc.org>
2015-09-30 20:38:16 +0000

    Update to 2.27:
    libuuid:
       - return correct value for uuid_generate_time_safe  [Romain Bouvier]

--- devel/libuuid/patches/patch-lib_randutils.c
+++ devel/libuuid/patches/patch-lib_randutils.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-lib_randutils.c,v 1.1 2014/05/11 04:59:07 pho Exp $
+$NetBSD: patch-lib_randutils.c,v 1.2 2015/09/30 20:38:16 wiz Exp $
 
 O_CLOEXEC is not available on every platform (e.g. MacOS X < 10.7). It
 was introduced in POSIX 2008.
 
---- lib/randutils.c.orig	2014-04-23 13:28:29.000000000 +0000
+--- lib/randutils.c.orig	2015-04-29 07:52:21.000000000 +0000
 +++ lib/randutils.c
-@@ -28,6 +28,10 @@
+@@ -30,6 +30,10 @@
  THREAD_LOCAL unsigned short ul_jrand_seed[3];
  #endif
  
