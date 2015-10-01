5de666fde3b62c8e546529df21261a877c40a67a
tnn <tnn@pkgsrc.org>
2015-10-01 13:50:05 +0000

    Deal with hardcoded libGL.1.dylib location. Fixes gtk3 apps on OSX.

--- graphics/libepoxy/patches/patch-src_dispatch__common.c
+++ graphics/libepoxy/patches/patch-src_dispatch__common.c
@@ -1,11 +1,17 @@
-$NetBSD: patch-src_dispatch__common.c,v 1.3 2015/09/24 22:51:29 tnn Exp $
+$NetBSD: patch-src_dispatch__common.c,v 1.4 2015/10/01 13:50:05 tnn Exp $
 
-Native X has /usr/X11R7/lib/libGL.so.2.
+Deal with hardcoded libGL locations and versions.
 Function must return a value if errx not declared with attr noreturn.
 
 --- src/dispatch_common.c.orig	2015-07-15 23:46:36.000000000 +0000
 +++ src/dispatch_common.c
-@@ -107,7 +107,7 @@
+@@ -103,11 +103,13 @@
+ #include "dispatch_common.h"
+ 
+ #ifdef __APPLE__
++#ifndef GLX_LIB
+ #define GLX_LIB "/opt/X11/lib/libGL.1.dylib"
++#endif
  #elif defined(ANDROID)
  #define GLX_LIB "libGLESv2.so"
  #else
@@ -14,7 +20,7 @@ Function must return a value if errx not declared with attr noreturn.
  #endif
  
  #ifdef ANDROID
-@@ -672,6 +672,7 @@ epoxy_get_proc_address(const char *name)
+@@ -672,6 +674,7 @@ epoxy_get_proc_address(const char *name)
  #endif
      }
      errx(1, "Couldn't find current GLX or EGL context.\n");
