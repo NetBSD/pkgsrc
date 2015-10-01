9dfa8cf8da0470739a39d2842053335e2643d4c0
wiz <wiz>
2015-10-01 05:36:36 +0000

    Update poppler* to 0.36.0:
    Release 0.36.0
            core:
             * Patch to support RichMedia annotations
             * Splash: Fix wrong memory access. Bug #91686
             * Cairo: fix size of transparency group surface. Bug #66229
             * Fix bounds check in Linearization::getPageFirst. Bug #91200
             * File Saving improvements
             * Add premultiplied alpha channel to SplashBitmap
             * Fix for xref table creation. Bug #90790
             * Fix JBIG2Decode infinite loop and stack overflow. Bug #91186
             * Minor optimization in text extraction
    
            qt4:
             * Basic support for RichMedia annotations
             * Change default image format
             * Minor optimizations
    
            qt5:
             * Basic support for RichMedia annotations
             * Change default image format
             * Minor optimizationsHEADmaster
    
            cpp:
             * Fix utf8/utf16 conversion. Bug #91644
    
            build system:
             * Do not hardcode -fPIC in Makefile.am
             * cmake: Allow configuring SHARE_INSTALL_DIR. Bug #90293
    
            glib:
             *
    
            utils:
             * pdfunite: Insert embedded files in result pdf. Bug #90066
             * pdftotext: Add -bbox-layout option. Bug #89941
    
    Release 0.35.0
            core:
             * Fix assert in broken file. Bug #91344
             * Adjust memory layout computation of GooString
             * Make SplashBitmap XBGR transfer alpha channel
             * Splash: Fix wrong writes on non rgb outputs. Bug #90570
             * Splash: remove ifndef in Windows code
             * GlobalParamsWin bugfixes. Bug #91053
    
            qt4:
             * Switch default image format
             * Add IgnorePaperColor render flag
    
            qt5:
             * Improve efficiency of Poppler::Page::renderToImage
             * Switch default image format
             * Add IgnorePaperColor render flag
    
            build system:
             * Allow configuring SPLASH_CMYK support
             * Add configure --enable-build-type. Bug #90796
    
            glib:
             * Explicitly link against pthread
             * Deprecation fixes
    
            utils:
             * pdftocairo: Fix cast to pointer from integer of different size on win64

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.44 2015/07/15 15:09:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.45 2015/10/01 05:36:36 wiz Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.34.0
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.36.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 .include "../../graphics/lcms2/buildlink3.mk"
