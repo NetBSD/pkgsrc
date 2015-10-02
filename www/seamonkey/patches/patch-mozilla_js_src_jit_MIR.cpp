ea74fdb7e47fb244d402680af589f0a5d8b076c6
ryoon <ryoon@pkgsrc.org>
2015-10-02 22:49:35 +0000

    Update to 2.38
    Changelog:
    Based on xulrunner 41.0
    
    Security fixes:
        2015-114 Information disclosure via the High Resolution Time API
        2015-113 Memory safety errors in libGLES in the ANGLE graphics library
        2015-112 Vulnerabilities found through code inspection
        2015-111 Errors in the handling of CORS preflight request headers
        2015-110 Dragging and dropping images exposes final URL after redirects
        2015-109 JavaScript immutable property enforcement can be bypassed
        2015-108 Scripted proxies can access inner window
        2015-107 Out-of-bounds read during 2D canvas display on Linux 16-bit color depth systems
        2015-106 Use-after-free while manipulating HTML media content
        2015-105 Buffer overflow while decoding WebM video
        2015-104 Use-after-free with shared workers and IndexedDB
        2015-103 URL spoofing in reader mode
        2015-102 Crash when using debugger with SavedStacks in JavaScript
        2015-101 Buffer overflow in libvpx while parsing vp9 format video
        2015-100 Arbitrary file manipulation by local user through Mozilla updater
        2015-99 Site attribute spoofing on Android by pasting URL with unknown scheme
        2015-98 Out of bounds read in QCMS library with ICC V4 profile attributes
        2015-97 Memory leak in mozTCPSocket to servers
        2015-96 Miscellaneous memory safety hazards (rv:41.0 / rv:38.3)

--- www/seamonkey/patches/patch-mozilla_js_src_jit_MIR.cpp
+++ www/seamonkey/patches/patch-mozilla_js_src_jit_MIR.cpp
@@ -1,13 +1,13 @@
-$NetBSD: patch-mozilla_js_src_jit_MIR.cpp,v 1.2 2015/09/21 05:03:45 ryoon Exp $
+$NetBSD: patch-mozilla_js_src_jit_MIR.cpp,v 1.3 2015/10/02 22:49:36 ryoon Exp $
 
---- mozilla/js/src/jit/MIR.cpp.orig	2015-09-02 09:27:30.000000000 +0000
+--- mozilla/js/src/jit/MIR.cpp.orig	2015-09-25 07:35:44.000000000 +0000
 +++ mozilla/js/src/jit/MIR.cpp
-@@ -71,7 +71,7 @@ MDefinition::PrintOpcodeName(FILE* fp, M
+@@ -72,7 +72,7 @@ MDefinition::PrintOpcodeName(GenericPrin
      const char* name = names[op];
      size_t len = strlen(name);
      for (size_t i = 0; i < len; i++)
--        fprintf(fp, "%c", tolower(name[i]));
-+        fprintf(fp, "%c", tolower(((unsigned char)name[i])));
+-        out.printf("%c", tolower(name[i]));
++        out.printf("%c", tolower((unsigned char)name[i]));
  }
  
  const Value&
