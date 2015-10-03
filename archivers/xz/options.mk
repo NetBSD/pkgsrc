be251802be3f043d0453ea8312337107b7d43ba8
tnn <tnn>
2015-10-03 14:57:10 +0000

    revert previous, should probably be handled in gettext-tools like joerg suggested.

--- archivers/xz/options.mk
+++ archivers/xz/options.mk
@@ -1,15 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2015/10/03 12:28:45 tnn Exp $
-
-CHECK_BUILTIN.gettext:=yes
-.include "../../devel/gettext-lib/builtin.mk"
-CHECK_BUILTIN.gettext:=no
+# $NetBSD: options.mk,v 1.3 2015/10/03 14:57:10 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xz
 PKG_SUPPORTED_OPTIONS=	nls
-PKG_SUGGESTED_OPTIONS=
-.if !empty(USE_BUILTIN.gettext:M[yY][eE][sS])
-PKG_SUGGESTED_OPTIONS+=	nls
-.endif
+PKG_SUGGESTED_OPTIONS=	nls
 
 .include "../../mk/bsd.options.mk"
 
@@ -18,7 +11,6 @@ PLIST_VARS+=		nls
 .if !empty(PKG_OPTIONS:Mnls)
 PLIST.nls=		yes
 CONFIGURE_ARGS+=	--enable-nls
-USE_TOOLS+=		msgfmt msgmerge xgettext
 .include "../../devel/gettext-lib/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-nls
