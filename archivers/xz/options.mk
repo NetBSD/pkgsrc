76e385e7b255418958956900617e070beac30300
tnn <tnn@pkgsrc.org>
2015-10-03 12:28:45 +0000

    Try to resolve circular dependency between xz and gettext; don't suggest to enable the nls option by default if gettext is not builtin. Also, the nls option needs the msg* tools.

--- archivers/xz/options.mk
+++ archivers/xz/options.mk
@@ -1,8 +1,15 @@
-# $NetBSD: options.mk,v 1.1 2014/10/12 13:07:11 bsiegert Exp $
+# $NetBSD: options.mk,v 1.2 2015/10/03 12:28:45 tnn Exp $
+
+CHECK_BUILTIN.gettext:=yes
+.include "../../devel/gettext-lib/builtin.mk"
+CHECK_BUILTIN.gettext:=no
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xz
 PKG_SUPPORTED_OPTIONS=	nls
-PKG_SUGGESTED_OPTIONS=	nls
+PKG_SUGGESTED_OPTIONS=
+.if !empty(USE_BUILTIN.gettext:M[yY][eE][sS])
+PKG_SUGGESTED_OPTIONS+=	nls
+.endif
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,6 +18,7 @@ PLIST_VARS+=		nls
 .if !empty(PKG_OPTIONS:Mnls)
 PLIST.nls=		yes
 CONFIGURE_ARGS+=	--enable-nls
+USE_TOOLS+=		msgfmt msgmerge xgettext
 .include "../../devel/gettext-lib/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-nls
