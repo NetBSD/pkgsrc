1cbc38ad69b1468d25128f72b750a0502c5bf10c
khorben <khorben@pkgsrc.org>
2015-10-05 23:43:55 +0000

    Package DeforaOS Browser 0.4.14
    This first release in almost two years brings:
    - more complete VFS abstraction
    - new plug-in to manage Gtk+ bookmarks
    - improved CVS, Git, Make, Preview, SVN, and Volumes plug-ins
    - new plug-in to track the current selection
    - new experimental plug-in for the XDG trash
    - improvements to the view(1) helper binary
    - re-usable library, libBrowser
    - Spanish translation
    - support for building outside of the source tree
    - further improvements to the build system
    - portability fixes (SunOS)
    - support for drawing the desktop as a window
    - improved support for Gtk+ 3
    - introduction of a test suite
    - additional bugfixes and improvements (UI, performance...)

--- sysutils/deforaos-browser/buildlink3.mk
+++ sysutils/deforaos-browser/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.10 2015/04/25 14:24:51 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2015/10/05 23:43:55 khorben Exp $
 
 BUILDLINK_TREE+=	deforaos-browser
 
 .if !defined(DEFORAOS_BROWSER_BUILDLINK3_MK)
 DEFORAOS_BROWSER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-browser+=	deforaos-browser>=0.4.5
-BUILDLINK_ABI_DEPENDS.deforaos-browser?=	deforaos-browser>=0.4.13nb2
+BUILDLINK_API_DEPENDS.deforaos-browser+=	deforaos-browser>=0.4.14
+BUILDLINK_ABI_DEPENDS.deforaos-browser?=	deforaos-browser>=0.4.14
 BUILDLINK_PKGSRCDIR.deforaos-browser?=	../../sysutils/deforaos-browser
 
-.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/deforaos-libdesktop/buildlink3.mk"
 .endif	# DEFORAOS_BROWSER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-deforaos-browser
