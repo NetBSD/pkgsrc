169ca51f119e674525995088aac3cac657074f6f
richard <richard@pkgsrc.org>
2015-10-05 04:47:02 +0000

    conditionalise libusb for non-SunOS platforms for the time-being, as there is not yet any working libusb-1.0 implementation for solaris. No revbump needed since these didn't build on SunOS prior

--- devel/libgphoto2/buildlink3.mk
+++ devel/libgphoto2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2015/09/10 00:54:47 dsainty Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2015/10/05 04:47:02 richard Exp $
 
 BUILDLINK_TREE+=	libgphoto2
 
@@ -10,7 +10,9 @@ BUILDLINK_ABI_DEPENDS.libgphoto2+=	libgphoto2>=2.4.7nb3
 BUILDLINK_PKGSRCDIR.libgphoto2?=	../../devel/libgphoto2
 
 .include "../../mk/bsd.fast.prefs.mk"
+.if ${OPSYS} != "SunOS"
 .include "../../devel/libusb/buildlink3.mk"
+.endif
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/libltdl/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
