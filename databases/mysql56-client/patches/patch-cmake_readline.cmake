04ac919539d447c85a14ba1244d048efd1035c0c
adam <adam@pkgsrc.org>
2015-10-07 11:22:00 +0000

    Fix building with some libedit and libreadline, including those on NetBSD

--- databases/mysql56-client/patches/patch-cmake_readline.cmake
+++ databases/mysql56-client/patches/patch-cmake_readline.cmake
@@ -1,4 +1,4 @@
-$NetBSD: patch-cmake_readline.cmake,v 1.2 2015/06/03 03:20:04 ryoon Exp $
+$NetBSD: patch-cmake_readline.cmake,v 1.3 2015/10/07 11:22:00 adam Exp $
 
 * If devel/editline is installed and use base libedit, cmake
   readline.h detection fails. Disable mis-detection of
@@ -16,3 +16,21 @@ $NetBSD: patch-cmake_readline.cmake,v 1.2 2015/06/03 03:20:04 ryoon Exp $
    IF(FOUND_EDITLINE_READLINE)
      SET(EDITLINE_INCLUDE_DIR "${FOUND_EDITLINE_READLINE}/editline")
    ELSE()
+@@ -160,7 +157,7 @@ MACRO (FIND_SYSTEM_EDITLINE)
+     SET(CMAKE_REQUIRED_LIBRARIES ${EDITLINE_LIBRARY})
+     CHECK_CXX_SOURCE_COMPILES("
+     #include <stdio.h>
+-    #include <readline.h>
++    #include <readline/readline.h>
+     int main(int argc, char **argv)
+     {
+        HIST_ENTRY entry;
+@@ -170,7 +167,7 @@ MACRO (FIND_SYSTEM_EDITLINE)
+ 
+     CHECK_CXX_SOURCE_COMPILES("
+     #include <stdio.h>
+-    #include <readline.h>
++    #include <readline/readline.h>
+     int main(int argc, char **argv)
+     {
+       typedef int MYFunction(const char*, int);
