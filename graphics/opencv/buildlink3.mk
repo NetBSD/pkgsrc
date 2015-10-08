338c2c248b506d180b6dd69a84e0db5b5cf14dc9
fhajny <fhajny@pkgsrc.org>
2015-10-08 17:45:59 +0000

    Update graphics/opencv to 3.0.0.
    Major changes (besides bugfixes):
    - opencv_contrib (http://github.com/itseez/opencv_contrib) repository
      has been added.
    - a subset of Intel IPP (IPPCV) is given to us and our users free
      of charge, free of licensing fees, for commercial and non-commerical
      use.
    - T-API (transparent API) has been introduced, this is transparent GPU
      acceleration layer using OpenCL. It does not add any compile-time or
      runtime dependency of OpenCL. When OpenCL is available, it's detected
      and used, but it can be disabled at compile time or at runtime.
    - ~40 OpenCV functions have been accelerated using NEON intrinsics and
      because these are mostly basic functions, some higher-level functions
      got accelerated as well.
    - There is also new OpenCV HAL layer that will simplifies creation
      of NEON-optimized code and that should form a base for the open-source
      and proprietary OpenCV accelerators.
    - The documentation is now in Doxygen: http://docs.opencv.org/master/
    - We cleaned up API of many high-level algorithms from features2d, calib3d,
      objdetect etc. They now follow the uniform "abstract interface - hidden
      implementation" pattern and make extensive use of smart pointers (Ptr<>).
    - Greatly improved and extended Python & Java bindings (also, see below on
      the Python bindings), newly introduced Matlab bindings
    - Improved Android support - now OpenCV Manager is in Java and supports
      both 2.4 and 3.0.
    - Greatly improved WinRT support, including video capturing and
      multi-threading capabilities. Thanks for Microsoft team for this!
    - Big thanks to Google who funded several successive GSoC programs and
      let OpenCV in. The results of many successful GSoC 2013 and 2014 projects
      have been integrated in opencv 3.0 and opencv_contrib (earlier results
      are also available in OpenCV 2.4.x). We can name:
      - text detection
      - many computational photography algorithms (HDR, inpainting, edge-aware
        filters, superpixels,...)
      - tracking and optical flow algorithms
      - new features, including line descriptors, KAZE/AKAZE
      - general use optimization (hill climbing, linear programming)
      - greatly improved Python support, including Python 3.0 support, many new
        tutorials & samples on how to use OpenCV with Python.
      - 2d shape matching module and 3d surface matching module
      - RGB-D module
      - VTK-based 3D visualization module
    
    For full changelog see:
    
      http://code.opencv.org/projects/opencv/wiki/ChangeLog
    
    For 2.4 to 3.0 transition, see the transition guide:
    
      http://docs.opencv.org/master/db/dfa/tutorial_transition_guide.html

--- graphics/opencv/buildlink3.mk
+++ graphics/opencv/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2009/11/18 23:08:48 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/10/08 17:45:59 fhajny Exp $
 
 BUILDLINK_TREE+=	opencv
 
 .if !defined(OPENCV_BUILDLINK3_MK)
 OPENCV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.opencv+=	opencv>=1.0.0nb3
+BUILDLINK_API_DEPENDS.opencv+=	opencv>=3.0.0
 BUILDLINK_PKGSRCDIR.opencv?=	../../graphics/opencv
 
 .endif	# OPENCV_BUILDLINK3_MK
--- graphics/opencv/patches/patch-modules_core_include_opencv2_core_types_c.h
+++ graphics/opencv/patches/patch-modules_core_include_opencv2_core_types_c.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-modules_core_include_opencv2_core_types_c.h,v 1.1 2012/12/07 21:37:10 adam Exp $
+$NetBSD: patch-modules_core_include_opencv2_core_types_c.h,v 1.2 2015/10/08 17:45:59 fhajny Exp $
 
 Define 'depth' as unsigned int, or IPL_DEPTH_SIGN=0x8000000 won't fit in int.
 
---- modules/core/include/opencv2/core/types_c.h.orig	2012-12-07 21:15:58.000000000 +0000
+--- modules/core/include/opencv2/core/types_c.h.orig	2015-06-03 17:21:34.000000000 +0000
 +++ modules/core/include/opencv2/core/types_c.h
-@@ -473,7 +473,7 @@ typedef struct _IplImage
-     int  ID;                /* version (=0)*/
-     int  nChannels;         /* Most of OpenCV functions support 1,2,3 or 4 channels */
-     int  alphaChannel;      /* Ignored by OpenCV */
--    int  depth;             /* Pixel depth in bits: IPL_DEPTH_8U, IPL_DEPTH_8S, IPL_DEPTH_16S,
-+    unsigned int  depth;             /* Pixel depth in bits: IPL_DEPTH_8U, IPL_DEPTH_8S, IPL_DEPTH_16S,
+@@ -303,7 +303,7 @@ _IplImage
+     int  ID;                /**< version (=0)*/
+     int  nChannels;         /**< Most of OpenCV functions support 1,2,3 or 4 channels */
+     int  alphaChannel;      /**< Ignored by OpenCV */
+-    int  depth;             /**< Pixel depth in bits: IPL_DEPTH_8U, IPL_DEPTH_8S, IPL_DEPTH_16S,
++    unsigned int  depth;             /**< Pixel depth in bits: IPL_DEPTH_8U, IPL_DEPTH_8S, IPL_DEPTH_16S,
                                 IPL_DEPTH_32S, IPL_DEPTH_32F and IPL_DEPTH_64F are supported.  */
-     char colorModel[4];     /* Ignored by OpenCV */
-     char channelSeq[4];     /* ditto */
+     char colorModel[4];     /**< Ignored by OpenCV */
+     char channelSeq[4];     /**< ditto */
