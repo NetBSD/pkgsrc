b8f828ac528f6557805abfba37e19657d23af386
fhajny <fhajny@pkgsrc.org>
2015-10-08 17:45:59 +0000

    Update graphics/opencv to 3.0.0.
    Major changes (besides bugfixes):
    - opencv_contrib (http://github.com/itseez/opencv_contrib) repository
      has been added.
    - a subset of Intel IPP (IPPCV) is given to us and our users free
      of charge, free of licensing fees, for commercial and non-commerical
      use.
    - T-API (transparent API) has been introduced, this is transparent GPU
      acceleration layer using OpenCL. It does not add any compile-time or
      runtime dependency of OpenCL. When OpenCL is available, it's detected
      and used, but it can be disabled at compile time or at runtime.
    - ~40 OpenCV functions have been accelerated using NEON intrinsics and
      because these are mostly basic functions, some higher-level functions
      got accelerated as well.
    - There is also new OpenCV HAL layer that will simplifies creation
      of NEON-optimized code and that should form a base for the open-source
      and proprietary OpenCV accelerators.
    - The documentation is now in Doxygen: http://docs.opencv.org/master/
    - We cleaned up API of many high-level algorithms from features2d, calib3d,
      objdetect etc. They now follow the uniform "abstract interface - hidden
      implementation" pattern and make extensive use of smart pointers (Ptr<>).
    - Greatly improved and extended Python & Java bindings (also, see below on
      the Python bindings), newly introduced Matlab bindings
    - Improved Android support - now OpenCV Manager is in Java and supports
      both 2.4 and 3.0.
    - Greatly improved WinRT support, including video capturing and
      multi-threading capabilities. Thanks for Microsoft team for this!
    - Big thanks to Google who funded several successive GSoC programs and
      let OpenCV in. The results of many successful GSoC 2013 and 2014 projects
      have been integrated in opencv 3.0 and opencv_contrib (earlier results
      are also available in OpenCV 2.4.x). We can name:
      - text detection
      - many computational photography algorithms (HDR, inpainting, edge-aware
        filters, superpixels,...)
      - tracking and optical flow algorithms
      - new features, including line descriptors, KAZE/AKAZE
      - general use optimization (hill climbing, linear programming)
      - greatly improved Python support, including Python 3.0 support, many new
        tutorials & samples on how to use OpenCV with Python.
      - 2d shape matching module and 3d surface matching module
      - RGB-D module
      - VTK-based 3D visualization module
    
    For full changelog see:
    
      http://code.opencv.org/projects/opencv/wiki/ChangeLog
    
    For 2.4 to 3.0 transition, see the transition guide:
    
      http://docs.opencv.org/master/db/dfa/tutorial_transition_guide.html

--- graphics/opencv/patches/patch-modules_ts_src_ts.cpp
+++ graphics/opencv/patches/patch-modules_ts_src_ts.cpp
@@ -1,10 +1,19 @@
-$NetBSD: patch-modules_ts_src_ts.cpp,v 1.2 2013/06/19 11:54:04 fhajny Exp $
+$NetBSD: patch-modules_ts_src_ts.cpp,v 1.3 2015/10/08 17:45:59 fhajny Exp $
 
 Avoid std::foo conflict.
 
---- modules/ts/src/ts.cpp.orig	2013-04-05 09:00:20.000000000 +0000
+--- modules/ts/src/ts.cpp.orig	2015-06-03 17:21:34.000000000 +0000
 +++ modules/ts/src/ts.cpp
-@@ -562,7 +562,7 @@ void TS::vprintf( int streams, const cha
+@@ -235,7 +235,7 @@ void BaseTest::safe_run( int start_from
+         }
+         catch (const TS::FailureCode& fc)
+         {
+-            std::string errorStr = TS::str_from_code(fc);
++            ::std::string errorStr = TS::str_from_code(fc);
+             ts->printf(TS::LOG, "General failure:\n\t%s (%d)\n", errorStr.c_str(), fc);
+ 
+             ts->set_failed_test_info( fc );
+@@ -558,7 +558,7 @@ void TS::vprintf( int streams, const cha
      for( int i = 0; i < MAX_IDX; i++ )
          if( (streams & (1 << i)) )
          {
@@ -13,7 +22,7 @@ Avoid std::foo conflict.
              // in the new GTest-based framework we do not use
              // any output files (except for the automatically generated xml report).
              // if a test fails, all the buffers are printed, so we do not want to duplicate the information and
-@@ -617,7 +617,7 @@ void smoothBorder(Mat& img, const Scalar
+@@ -613,7 +613,7 @@ void smoothBorder(Mat& img, const Scalar
      Scalar s;
      uchar *p = NULL;
      int n = 100/delta;
