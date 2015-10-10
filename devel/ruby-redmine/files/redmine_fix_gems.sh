ba8d578b7bc0695fa15e60294d022177368b7b99
ryoon <ryoon@pkgsrc.org>
2015-10-10 02:03:04 +0000

    Update to 3.1.1
    Changhelog:
    == 2015-09-20 v3.1.1
    
    * Feature #11253: Total time spent from subtasks on the issue list
    * Feature #20688: Add Total estimated hours column on issue list
    * Feature #20738: Upgrade Rails 4.2.4
    * Defect #19577: Open redirect vulnerability
    * Defect #20761: Fix typo of Japanese translation for notice_gantt_chart_truncated
    * Defect #20427: Cannot create a custom query visibility is "to these roles only"
    * Defect #20454: Mail handler: unwanted assignment to a group occurs
    * Defect #20278: Wrong syntax for resizing inline images will throw a 500 error
    * Defect #20401: "Spent time" panel: columns not wrapping
    * Defect #20407: Monospace font-family values are differ between application.css and scm.css
    * Defect #20456: 3.1-stable/3.1.0: missing commits (omitted from being merged from trunk)
    * Defect #20466: Broken email notification layout in Outlook
    * Defect #20490: WARNING: Can't mass-assign protected attributes for User
    * Defect #20633: Help cursor showing up since r14154
    * Patch #20293: Russian translation for 2.6-stable
    * Patch #20294: Russian translation for 2.6-stable
    * Patch #20408: Turkish translation for 2.6-stable
    * Patch #20557: Czech translation for 2.6-stable
    * Patch #20735: Markdown: Upgrade redcarpet gem to 3.3 (ruby 1.9 and higher)
    * Patch #20745: Portuguese translation for 2.6-stable
    * Patch #20512: Project.copy_from deletes enabled_modules on source
    * Patch #20737: Czech translation for 3.0-stable
    * Patch #20746: Portuguese translation for 3.0-stable
    * Patch #20243: Use https links instead of http links in ApplicationHelper#avatar_edit_link and Redmine::Info class methods
    * Patch #20410: Turkish translation for 3.1-stable
    * Patch #20452: Czech localisation update
    * Patch #20731: Change Japanese translation for "spent time"
    * Patch #20747: Portuguese translation for 3.1-stable

--- devel/ruby-redmine/files/redmine_fix_gems.sh
+++ devel/ruby-redmine/files/redmine_fix_gems.sh
@@ -9,9 +9,9 @@ cd @PREFIX@/share/@RUBY_PKGPREFIX@-redmine/app
 
 @GEM@ install nokogiri --version 1.6.6.2 -- --use-system-libraries --with-xml2-config=@PREFIX@/bin/xml2-config --with-xslt-config=@PREFIX@/bin/xslt-config
 @GEM@ pristine json --version 1.8.3
-@GEM@ pristine kgio --version 2.9.3
-@GEM@ pristine mysql2 --version 0.3.18
-@GEM@ pristine raindrops --version 0.13.0
-@GEM@ pristine redcarpet --version 3.1.2
-@GEM@ pristine rmagick --version 2.15.3
+@GEM@ pristine kgio --version 2.10.0
+@GEM@ pristine mysql2 --version 0.3.20
+@GEM@ pristine raindrops --version 0.15.0
+@GEM@ pristine redcarpet --version 3.3.3
+@GEM@ pristine rmagick --version 2.15.4
 @GEM@ pristine unicorn --version 4.9.0
