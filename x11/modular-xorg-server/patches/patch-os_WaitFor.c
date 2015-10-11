f2a9d3f86c229c90d7974459fed59827b5ae6ed8
tnn <tnn@pkgsrc.org>
2015-10-11 17:40:23 +0000

    Adjust the PR misc/50073 patches so they only apply on NetBSD. It turns out that moving Xpoll.h before osdep.h breaks on Linux because it doesn't get the feature test macros for select.h.

--- x11/modular-xorg-server/patches/patch-os_WaitFor.c
+++ x11/modular-xorg-server/patches/patch-os_WaitFor.c
@@ -1,24 +1,18 @@
-$NetBSD: patch-os_WaitFor.c,v 1.1 2015/07/25 04:10:53 markd Exp $
+$NetBSD: patch-os_WaitFor.c,v 1.2 2015/10/11 17:40:24 tnn Exp $
 
 move X11/Xpoll.h include earlier than sys/select.h to avoid missing
 out on setting FD_SETSIZE early enough.
 
---- os/WaitFor.c.orig	2015-07-21 21:58:16.000000000 +0000
+--- os/WaitFor.c.orig	2015-05-21 14:23:54.000000000 +0000
 +++ os/WaitFor.c
-@@ -52,6 +52,8 @@ SOFTWARE.
+@@ -52,6 +52,10 @@ SOFTWARE.
   *
   *****************************************************************/
  
++#ifdef __NetBSD__
 +#include <X11/Xpoll.h>
++#endif
 +
  #ifdef HAVE_DIX_CONFIG_H
  #include <dix-config.h>
  #endif
-@@ -66,7 +68,6 @@ SOFTWARE.
- #include "misc.h"
- 
- #include "osdep.h"
--#include <X11/Xpoll.h>
- #include "dixstruct.h"
- #include "opaque.h"
- #ifdef DPMSExtension
--- x11/modular-xorg-server/patches/patch-os_io.c
+++ x11/modular-xorg-server/patches/patch-os_io.c
@@ -1,24 +1,18 @@
-$NetBSD: patch-os_io.c,v 1.1 2015/07/25 04:10:53 markd Exp $
+$NetBSD: patch-os_io.c,v 1.2 2015/10/11 17:40:24 tnn Exp $
 
 move X11/Xpoll.h include earlier than sys/select.h to avoid missing
 out on setting FD_SETSIZE early enough.
 
---- os/io.c.orig	2015-07-21 21:58:59.000000000 +0000
+--- os/io.c.orig	2015-05-21 14:23:54.000000000 +0000
 +++ os/io.c
-@@ -51,6 +51,8 @@ SOFTWARE.
+@@ -51,6 +51,10 @@ SOFTWARE.
   *
   *****************************************************************/
  
++#ifdef __NetBSD__
 +#include <X11/Xpoll.h>
++#endif
 +
  #ifdef HAVE_DIX_CONFIG_H
  #include <dix-config.h>
  #endif
-@@ -74,7 +76,6 @@ SOFTWARE.
- #include <X11/Xproto.h>
- #include "os.h"
- #include "osdep.h"
--#include <X11/Xpoll.h>
- #include "opaque.h"
- #include "dixstruct.h"
- #include "misc.h"
