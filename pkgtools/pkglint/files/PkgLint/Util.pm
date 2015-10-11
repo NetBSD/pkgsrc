e1af5f6ec8971c9622e7fe655a3916b5b8004f41
rillig <rillig@pkgsrc.org>
2015-10-11 21:23:34 +0000

    Fixed some of the errors reported by perlcritic in --stern mode

--- pkgtools/pkglint/files/PkgLint/Util.pm
+++ pkgtools/pkglint/files/PkgLint/Util.pm
@@ -1,4 +1,4 @@
-# $NetBSD: Util.pm,v 1.2 2015/10/11 21:06:20 rillig Exp $
+# $NetBSD: Util.pm,v 1.3 2015/10/11 21:23:34 rillig Exp $
 #
 # This package is a catch-all for subroutines that are not application-spe-
 # cific. Currently it contains the boolean constants C<false> and C<true>,
@@ -10,6 +10,7 @@
 package PkgLint::Util;
 
 use strict;
+use warnings;
 
 BEGIN {
 	use Exporter;
