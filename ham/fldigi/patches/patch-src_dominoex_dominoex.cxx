71955608a0afa57144b296d9ef40506b15cf314a
mef <mef@pkgsrc.org>
2015-10-11 03:59:56 +0000

    (pkgsrc)  - Add comments to patches picked from cvs log  - Update HOMEPAGE and MASTER_SITES (to SF)  - (move LICENSE= line to the second paragraph)
    (upstream)
     - Update 3.22.13 to 3.23.03
    ----------------------------
    =Version 3.23.03=
    2015-09-28  David Freese  <iam_w1hkj@w1hkj.com>
    	c3b610b: FSQ trigger bug
    
    =Version 3.23.02=
    	0424764: fsq cqcqcq
    	198c5a0: fsq sounder/heard-aging
    	dbb169b: FSQ directive
    	d47b1e3: rigcat delayed startup
    	e6ebe70: OS-X serial
    
    2015-09-23  Kamal Mostafa  <kamal@whence.com>
    	f75b92a: asciidoc timezone
    
    =Version 3.23.01=
    2015-09-22  David Freese  <iam_w1hkj@w1hkj.com>
    	8ad0a8b: WX configure
    	0caa5ea: PA debug
    	838e112: WFALL-ONLY
    	f472a17: Logbook read
    	7cb4c66: ifkp avatar depth
    	ea18942: IFKP image slant
    
    =Version 3.23.00=
    	f78aa38: Clear Tx Panel
    	cbff7c3: Olivia/Contestia start stop tones
    	37175e2: macro bug
    	3d6e0b9: FSQ updates
    	ed3b09d: ifkp modem
    	c82de8e: MXE update
    	7b42e01: Macros
    	0606d1a: Doxygen help
    	d4b6308: QSO rec
    	2df24b8: MS Read logbook
    	30ad2a9: WEFAX hang on INFO
    	6704797: Nu opBW combos
    	db69617: LSB modes

--- ham/fldigi/patches/patch-src_dominoex_dominoex.cxx
+++ ham/fldigi/patches/patch-src_dominoex_dominoex.cxx
@@ -1,4 +1,8 @@
-$NetBSD: patch-src_dominoex_dominoex.cxx,v 1.5 2015/04/27 23:51:52 joerg Exp $
+$NetBSD: patch-src_dominoex_dominoex.cxx,v 1.6 2015/10/11 03:59:56 mef Exp $
+
+(cvs log says)
+date: 2015-04-28 08:51:52 +0900;  author: joerg;  state: Exp;  lines: +3 -7;  commitid: 2iTQbBPEdUwBShjy;
+Deal with C++11 user defined string literals. Add missing includes.
 
 --- src/dominoex/dominoex.cxx.orig	2015-04-27 23:30:32.000000000 +0000
 +++ src/dominoex/dominoex.cxx
--- ham/fldigi/patches/patch-src_include_qrunner.h
+++ ham/fldigi/patches/patch-src_include_qrunner.h
@@ -1,4 +1,9 @@
-$NetBSD: patch-src_include_qrunner.h,v 1.5 2015/04/27 23:51:52 joerg Exp $
+$NetBSD: patch-src_include_qrunner.h,v 1.6 2015/10/11 03:59:56 mef Exp $
+
+date: 2015-04-28 08:51:52 +0900;  author: joerg;  state: Exp;  lines: +2 -6;  commitid: 2iTQbBPEdUwBShjy;
+Avoid implicit conversions to bool for streams.
+Require C++11 for Clang and prefer std version over tr1 when in C++11
+mode.
 
 --- src/include/qrunner.h.orig	2015-04-27 23:31:20.000000000 +0000
 +++ src/include/qrunner.h
