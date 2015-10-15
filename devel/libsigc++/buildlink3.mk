a8af78265b1c0c9dd6a98e0639de0b1682a97bb4
wiz <wiz@pkgsrc.org>
2015-10-15 12:24:21 +0000

    Set GCC_REQD to 4.7.
    This actually wants c++11, but we currently have no way of setting that.

--- devel/libsigc++/buildlink3.mk
+++ devel/libsigc++/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:24:22 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2015/10/15 12:24:21 wiz Exp $
 
 BUILDLINK_TREE+=	libsigcpp
 
@@ -8,6 +8,10 @@ LIBSIGCPP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libsigcpp+=	libsigc++>=2.0.3
 BUILDLINK_ABI_DEPENDS.libsigcpp+=	libsigc++>=2.0.17nb1
 BUILDLINK_PKGSRCDIR.libsigcpp?=		../../devel/libsigc++
+
+# needs C++11
+GCC_REQD+=	4.7
+
 .endif # LIBSIGCPP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libsigcpp
