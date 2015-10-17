91135bc0046091834794d72a15ac2d81c7e5a0f0
richard <richard@pkgsrc.org>
2015-10-17 06:06:49 +0000

    fix patch to use strlen instead of d_reclen which is the length of the whole allocated structure including padding.  Thanks joerg@.

--- www/bozohttpd/patches/patch-bozohttpd.c
+++ www/bozohttpd/patches/patch-bozohttpd.c
@@ -1,17 +1,19 @@
-$NetBSD: patch-bozohttpd.c,v 1.1 2015/10/16 20:09:02 richard Exp $
+$NetBSD: patch-bozohttpd.c,v 1.2 2015/10/17 06:06:49 richard Exp $
 
 SunOS doesn't define d_namlen element of dirent structure
-but uses d_reclen instead, so define an alias.
+only d_reclen, so use strlen to determine d_namlen.
 
 --- bozohttpd.c.orig	2015-03-25 23:38:50.000000000 +0000
 +++ bozohttpd.c
-@@ -1093,6 +1093,9 @@ check_virtual(bozo_httpreq_t *request)
+@@ -1093,7 +1093,11 @@ check_virtual(bozo_httpreq_t *request)
  				}
  				debug((httpd, DEBUG_OBESE, "looking at dir``%s''",
  			 	   d->d_name));
-+#ifdef __sun
-+#define d_namlen d_reclen
-+#endif
++#ifndef __sun
  				if (d->d_namlen == len && strcmp(d->d_name,
++#else
++				if (strlen(d->d_name) == len && strcmp(d->d_name,
++#endif
  				    request->hr_host) == 0) {
  					/* found it, punch it */
+ 					debug((httpd, DEBUG_OBESE, "found it punch it"));
