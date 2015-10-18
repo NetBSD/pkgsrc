bcbf9d8bc25b653e8ec1bf2d3b743ed67a44a6f8
adam <adam@pkgsrc.org>
2015-10-18 15:47:10 +0000

    Changes 3.0.0: * Saving Multipage Images * Tiff ImageFileDirectory Rewrite * Deprecated Methods * LibJpeg and Zlib are Required by Default

--- graphics/py-Pillow/patches/patch-setup.py
+++ graphics/py-Pillow/patches/patch-setup.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-setup.py,v 1.4 2015/04/17 15:10:02 adam Exp $
+$NetBSD: patch-setup.py,v 1.5 2015/10/18 15:47:10 adam Exp $
 
 * Prevent to detect optional tkinter.
 * Disable demo programs.
 
---- setup.py.orig	2015-04-02 09:05:22.000000000 +0000
+--- setup.py.orig	2015-10-01 15:27:21.000000000 +0000
 +++ setup.py
 @@ -82,11 +82,7 @@ def _lib_include(root):
  def _read(file):
@@ -18,20 +18,11 @@ $NetBSD: patch-setup.py,v 1.4 2015/04/17 15:10:02 adam Exp $
  
  
  NAME = 'Pillow'
-@@ -575,7 +571,7 @@ class pil_build_ext(build_ext):
-             exts.append(Extension(
-                 "PIL._webp", ["_webp.c"], libraries=libs, define_macros=defs))
- 
--        if sys.platform == "darwin":
-+        if _tkinter and sys.platform == "darwin":
-             # locate Tcl/Tk frameworks
-             frameworks = []
-             framework_roots = [
-@@ -753,7 +749,6 @@ setup(
+@@ -760,7 +756,6 @@ setup(
      ext_modules=[Extension("PIL._imaging", ["_imaging.c"])],
      include_package_data=True,
      packages=find_packages(),
--    scripts=glob.glob("Scripts/pil*.py"),
+-    scripts=glob.glob("Scripts/*.py"),
      test_suite='nose.collector',
      keywords=["Imaging", ],
      license='Standard PIL License',
