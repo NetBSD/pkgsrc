e069d8e6c6c85af6e03fe4670787488624bf7531
tsutsui <tsutsui@pkgsrc.org>
2015-10-18 03:58:31 +0000

    Update canna packages to 3.7pl3.
    pkgsrc changes:
     - reorganize patch filenames for maintainability
      - patch-ac is also merged into existing patch-dic_phono_Imakefile
     - fix a missed 64 bit time_t issue in libRK
     - add a hook in cmd.tmpl to disable installation of cmd/forcpp and
       cmd/kpdic in canna-lib
     - tweak SUBDIRS patches in Makefiles to avoid unnecessary builds
       for each packages
     - pull various updates to canuum from the original FreeWnn's uum,
       including posix_openpt(3) support, and disable the default but
       obsolete libspt support
     - reorganize curses(3) ops in canuum per curses(3) documents
     - fix misc compiler warnings
     - take maintainership
    
    Upstream changes:
    
     All changes are noted in ChangeLog and CHANGES.jp in distfile.
     I won't bother to quote ~1700 line ChangeLogs here,
     because this latest 3.7pl3 was released on 2004/05/20 and
     the previous 3.6pl4 was 2003/09/22.
     Highlight points are:
      - binary dictionary format is changed
      - shlib versions are bumped to 1.2 from 1.1
      - new APIs for wchar
      - experimental 64 bit architecture support
      - IPv6 support
    
    Both server and clients (canuum and mule-1.1) are tested on
    NetBSD/i386 7.0, NetBSD/amd64 7.0, and NetBSD/luna68k 7.0.
    Also thanks to Nobuyoshi SATO, for his initial patch posted
    to tech-pkg-ja@.

--- inputmethod/canna/patches/patch-canna_widedef.h
+++ inputmethod/canna/patches/patch-canna_widedef.h
@@ -1,10 +1,12 @@
-$NetBSD: patch-ae,v 1.1 2005/12/29 15:29:12 joerg Exp $
+$NetBSD: patch-canna_widedef.h,v 1.1 2015/10/18 03:58:31 tsutsui Exp $
 
---- canna/widedef.h.orig	2003-09-12 14:32:52.000000000 +0000
+DragonFly wchar_t support.
+
+--- canna/widedef.h.orig	2003-12-27 17:15:20.000000000 +0000
 +++ canna/widedef.h
-@@ -66,7 +66,10 @@
- #endif /* !nec_ews_svr2 */
- #endif /* HAVE_WCHAR_OPERATION */
+@@ -36,7 +36,10 @@
+ # include <machine/ansi.h>
+ #endif
  
 -#if (defined(__FreeBSD__) && __FreeBSD_version < 500000) \
 +#if defined(__DragonFly__)
