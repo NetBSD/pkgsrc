36b86ea4b6e7cd53f709e65ff089dda3f7b3a836
tsutsui <tsutsui@pkgsrc.org>
2015-10-18 03:58:31 +0000

    Update canna packages to 3.7pl3.
    pkgsrc changes:
     - reorganize patch filenames for maintainability
      - patch-ac is also merged into existing patch-dic_phono_Imakefile
     - fix a missed 64 bit time_t issue in libRK
     - add a hook in cmd.tmpl to disable installation of cmd/forcpp and
       cmd/kpdic in canna-lib
     - tweak SUBDIRS patches in Makefiles to avoid unnecessary builds
       for each packages
     - pull various updates to canuum from the original FreeWnn's uum,
       including posix_openpt(3) support, and disable the default but
       obsolete libspt support
     - reorganize curses(3) ops in canuum per curses(3) documents
     - fix misc compiler warnings
     - take maintainership
    
    Upstream changes:
    
     All changes are noted in ChangeLog and CHANGES.jp in distfile.
     I won't bother to quote ~1700 line ChangeLogs here,
     because this latest 3.7pl3 was released on 2004/05/20 and
     the previous 3.6pl4 was 2003/09/22.
     Highlight points are:
      - binary dictionary format is changed
      - shlib versions are bumped to 1.2 from 1.1
      - new APIs for wchar
      - experimental 64 bit architecture support
      - IPv6 support
    
    Both server and clients (canuum and mule-1.1) are tested on
    NetBSD/i386 7.0, NetBSD/amd64 7.0, and NetBSD/luna68k 7.0.
    Also thanks to Nobuyoshi SATO, for his initial patch posted
    to tech-pkg-ja@.

--- inputmethod/canna-lib/buildlink3.mk
+++ inputmethod/canna-lib/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:24:46 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2015/10/18 03:58:31 tsutsui Exp $
 
 BUILDLINK_TREE+=	Canna-lib
 
 .if !defined(CANNA_LIB_BUILDLINK3_MK)
 CANNA_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.Canna-lib+=	Canna-lib>=3.6pl4
+BUILDLINK_API_DEPENDS.Canna-lib+=	Canna-lib>=3.7pl3
 BUILDLINK_PKGSRCDIR.Canna-lib?=	../../inputmethod/canna-lib
 .endif # CANNA_LIB_BUILDLINK3_MK
 
