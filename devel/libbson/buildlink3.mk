ffe3126e60308229473795f3d27af9a2cc01b075
fhajny <fhajny@pkgsrc.org>
2015-10-19 11:42:18 +0000

    Update devel/libbson 1.1.10 to 1.2.0.
    libbson 1.2.0
    - Add bson_mem_restore_vtable(), the inverse of bson_mem_set_vtable().
    - Enable runtime asserts in release build.
    - Fixed compiler warnings and build failures on various platforms.
    - Improvements to the formatting and contents of the documentation.
    
    libbson 1.1.11
    - Document bson streaming reads with an example, bson-streaming-reader.c.
    - Document callback function types bson_reader_destroy_func_t and
      bson_reader_read_func_t.

--- devel/libbson/buildlink3.mk
+++ devel/libbson/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2015/03/24 13:21:43 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2015/10/19 11:42:18 fhajny Exp $
 
 BUILDLINK_TREE+=	libbson
 
 .if !defined(LIBBSON_BUILDLINK3_MK)
 LIBBSON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libbson+=	libbson>=1.1.0
+BUILDLINK_API_DEPENDS.libbson+=	libbson>=1.2.0
 BUILDLINK_PKGSRCDIR.libbson?=	../../devel/libbson
 .endif	# LIBBSON_BUILDLINK3_MK
 
