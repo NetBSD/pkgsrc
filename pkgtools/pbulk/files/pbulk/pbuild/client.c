a7dce9e9fd8e9f7e1e15caa9209c6ff4d4cdc4c9
joerg <joerg@pkgsrc.org>
2015-10-21 23:03:17 +0000

    pbulk-base-0.49: When shutting down the master after all jobs are done, send a zero length request to the clients and wait a second for them to shutdown the connection. Calling shutdown on the server side doesn't avoid TIME_WAIT as originally intended.

--- pkgtools/pbulk/files/pbulk/pbuild/client.c
+++ pkgtools/pbulk/files/pbulk/pbuild/client.c
@@ -1,4 +1,4 @@
-/* $NetBSD: client.c,v 1.4 2012/01/19 18:53:32 joerg Exp $ */
+/* $NetBSD: client.c,v 1.5 2015/10/21 23:03:17 joerg Exp $ */
 
 /*-
  * Copyright (c) 2007 Joerg Sonnenberger <joerg@NetBSD.org>.
@@ -84,6 +84,8 @@ loop:
 	if (recv_bytes != 4)
 		errx(1, "Premature end while reading build info from socket");
 	build_info_len = ntohl(build_info_len);
+	if (build_info_len == 0)
+		exit(0);
 	if (build_info_len < 10 || build_info_len > 0xffffff)
 		errx(1, "Invalid build info length from master");
 
