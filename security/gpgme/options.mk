bb56a0acd4a4a3f5ba961a9744777fd204f2e6d5
tnn <tnn@pkgsrc.org>
2015-10-22 19:18:34 +0000

    Add an option to toggle gnupg21 since people for unknown reasons still want 2.0 to be the default, despite not supporting the latest crypto.

--- security/gpgme/options.mk
+++ security/gpgme/options.mk
@@ -1,15 +1,20 @@
-# $NetBSD: options.mk,v 1.2 2015/09/24 23:33:08 ryoon Exp $
+# $NetBSD: options.mk,v 1.3 2015/10/22 19:18:34 tnn Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.gpgme
-PKG_SUPPORTED_OPTIONS=	gpgsm
-PKG_SUGGESTED_OPTIONS=	gpgsm
+PKG_OPTIONS_VAR=		PKG_OPTIONS.gpgme
+PKG_OPTIONS_REQUIRED_GROUPS=	gnupg
+PKG_OPTIONS_GROUP.gnupg=	gnupg1 gnupg2 gnupg21
+PKG_SUGGESTED_OPTIONS=		gnupg2
 
 .include "../../mk/bsd.options.mk"
 
-##
-## Option to build with gpgsm. This provides SMIME support
-##
-.if !empty(PKG_OPTIONS:Mgpgsm)
+.if !empty(PKG_OPTIONS:Mgnupg21)
+DEPENDS+=		gnupg21>=2.1:../../security/gnupg21
+FIND_PREFIX:=	GPG2DIR=gnupg21
+.include "../../mk/find-prefix.mk"
+CONFIGURE_ARGS+=	--with-gpgsm=${GPG2DIR}/bin/gpgsm
+CONFIGURE_ARGS+=	--with-gpg=${GPG2DIR}/bin/gpg2
+REPLACE_SH+=		tests/gpg/pinentry
+.elif !empty(PKG_OPTIONS:Mgnupg2)
 DEPENDS+=		gnupg2>=2.0<2.1:../../security/gnupg2
 FIND_PREFIX:=	GPG2DIR=gnupg2
 .include "../../mk/find-prefix.mk"
