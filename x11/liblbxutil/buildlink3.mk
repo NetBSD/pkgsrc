14bff801faadd8185b056cb3e6b0fa14b996a77b
tnn <tnn@pkgsrc.org>
2015-10-22 14:34:45 +0000

    Does not need a fortran compiler. Please do not add fortran to USE_LANGUAGES just to appease broken configure scripts.

--- x11/liblbxutil/buildlink3.mk
+++ x11/liblbxutil/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2015/07/02 12:38:32 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2015/10/22 14:34:45 tnn Exp $
 
 BUILDLINK_TREE+=	liblbxutil
 
 .if !defined(LIBLBXUTIL_BUILDLINK3_MK)
 LIBLBXUTIL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liblbxutil+=	liblbxutil>=1.1.0
+BUILDLINK_API_DEPENDS.liblbxutil+=	liblbxutil>=1.1.0nb1
 BUILDLINK_PKGSRCDIR.liblbxutil?=	../../x11/liblbxutil
 
 .include "../../devel/zlib/buildlink3.mk"
