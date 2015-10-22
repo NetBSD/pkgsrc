6cdd5d3ea087c9ecd8ad1a9e5b46d4748a891195
bouyer <bouyer@pkgsrc.org>
2015-10-22 15:51:47 +0000

    Import sunxi-tools as of 20150915. Note that only the fexc utility has been tested at this time.
    Tools to help hacking Allwinner A10 (aka sun4i) based devices and possibly
    it's successors, that's why the 'x' in the package name.
    
    fexc:
    	`.fex` file (de)compiler
    
    	Usage: ./fexc [-vq] [-I <infmt>] [-O <outfmt>] [<input> [<output>]]
    
    	infmt:  fex, bin  (default:fex)
    	outfmt: fex, bin  (default:bin)
    
    bin2fex:
    	compatibility shortcut to call `fexc` to decompile an script.bin
    	blob back into `.fex` format used by allwinner's SDK to configure
    	the boards.
    
    fex2bin:
    	compatiblity shortcut to call `fexc` to compile a `.fex` file into
    	the binary form used by the sun4i kernel.
    
    fel:
    	script interface for talking to the FEL USB handler built in to
    	th CPU. You activate FEL mode by pushing the usboot/recovery
    	button at poweron. See http://linux-sunxi.org/FEL/USBBoot for
    	a detailed usage guide.
    
    fel-gpio:
    	Simple wrapper around fel-pio and fel to allow GPIO manipulations
    	via FEL
    
    fel-sdboot:
    	ARM native sdcard bootloader forcing the device into FEL mode
    
    fel-pio:
    	ARM native helper for fel-gpio
    
    pio:
    	Manipulate PIO register dumps
    
    jtag-loop.sunxi:
    	ARM native boot helper to force the SD port into JTAG
    	and then stop, to ease debugging of bootloaders.
    
    bootinfo:
    	Dump information from Allwinner boot files (boot0/boot1)
    	--type=sd	include SD boot info
    	--type=nand	include NAND boot info (not implemented)
    
    phoenix_info:
    	gives information about a phoenix image created by the
    	phoenixcard utility and optionally extracts the embedded boot
    	code & firmware file from their hidden partitions.

---
+++ sysutils/sunxi-tools/patches/patch-include_endian_compat.h
@@ -0,0 +1,13 @@
+$NetBSD: patch-include_endian_compat.h,v 1.1 2015/10/22 15:51:47 bouyer Exp $
+
+--- include/endian_compat.h.orig	2015-10-01 16:09:19.000000000 +0200
++++ include/endian_compat.h	2015-10-01 16:20:48.000000000 +0200
+@@ -29,6 +29,8 @@
+ #define le32toh(x) CFSwapInt32LittleToHost(x)
+ #define htole16(x) CFSwapInt16HostToLittle(x)
+ #define le16toh(x) CFSwapInt16LittleToHost(x)
++#elif defined(__NetBSD__)
++#include <sys/endian.h>
+ #else
+ #include <endian.h>
+ #endif
