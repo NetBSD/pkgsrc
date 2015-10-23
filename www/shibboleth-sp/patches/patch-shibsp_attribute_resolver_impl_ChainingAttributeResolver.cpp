6bd69f67feea2611b3d211a9b77a0197c776ce6f
pettai <pettai@pkgsrc.org>
2015-10-23 07:35:07 +0000

    Version 2.5.5
    Bug:
        * [SSPCPP-656] - NameID insert logic appears wrong for ODBC Session store
        * [SSPCPP-657] - Update Windows libraries
        * [SSPCPP-663] - BOOST autoconf macros break with gcc5
        * [SSPCPP-665] - Use of systemd breaks on reboot due to disappearance of /run/shibboleth
    
    Improvement:
        * [SSPCPP-654] - Move fork wait timeout from init script to sysconfig
    
    Task:
        * [SSPCPP-661] - Preparation of 2.5.5 release
        * [SSPCPP-662] - Set AllowSameVersionUpgrades to 'yes'
    
    Version 2.5.4
    
    Bug:
        * [SSPCPP-612] - Old DiscoveryFeed cache files are not correctly removed
        * [SSPCPP-616] - SP does not build with C++11
        * [SSPCPP-621] - log4shib. RemoteSyslogAppender doesn't work in debian.
        * [SSPCPP-623] - Attribute mapper interprets attribute name with leading/trailing whitespace
        * [SSPCPP-624] - Trailing whitespace in authnContextClassRef attribute parsed incorrectly
        * [SSPCPP-627] - SyslogAppender is not working on windows
        * [SSPCPP-646] - When triggered by file size limit, native.log does not rotate correctly and logs are missing
    
    Improvement:
        * [SSPCPP-618] - Add support for Amazon Linux 2014.3 via attached patch
        * [SSPCPP-629] - attribute-map.xml missing "uid" attribute (eduPerson)
        * [SSPCPP-645] - Adjust ownership of /var/cache/shibboleth in the init script of RPM-based Linux distributions
        * [SSPCPP-647] - consider not permitting RC4 on back channel queries
    
    Task:
        * [SSPCPP-644] - Release log4shib 1.0.9
        * [SSPCPP-648] - Release process for 2.5.4
    
    Version 2.5.3
    
    Bug:
        * [SSPCPP-578] - Example Apache config uses require valid-user
        * [SSPCPP-580] - FastCGI programs use libxmltooling but don't link with it
        * [SSPCPP-584] - Limit on preserved POST data size is not enforced
        * [SSPCPP-585] - POST data replay in Firefox fails if data contains key "submit"
        * [SSPCPP-589] - Relative paths in Shibboleth XML catalogs are resolved against /usr/share/xml/opensaml
        * [SSPCPP-595] - postTemplat.html form submission bug
        * [SSPCPP-596] - Red Hat init script produces spurious restorecon warning at startup
        * [SSPCPP-603] - Directory Indexes don't work when using file-based basic auth (ShibCompatValidUser is On)
    
    Documentation:
        * [SSPCPP-591] - Errors partialLogout attribute not documented
    
    Improvement:
        * [SSPCPP-598] - Dynamic metadata provider in SP should avoid unmarshalling non-EntityDescriptor results
        * [SSPCPP-605] - Rephrase error log lines for AuthnFailed responses
    
    Task:
        * [SSPCPP-609] - Release of 2.5.3

--- www/shibboleth-sp/patches/patch-shibsp_attribute_resolver_impl_ChainingAttributeResolver.cpp
+++ www/shibboleth-sp/patches/patch-shibsp_attribute_resolver_impl_ChainingAttributeResolver.cpp
@@ -1,22 +1,22 @@
-$NetBSD: patch-shibsp_attribute_resolver_impl_ChainingAttributeResolver.cpp,v 1.1 2013/09/29 10:19:47 joerg Exp $
+$NetBSD: patch-shibsp_attribute_resolver_impl_ChainingAttributeResolver.cpp,v 1.2 2015/10/23 07:35:08 pettai Exp $
 
---- shibsp/attribute/resolver/impl/ChainingAttributeResolver.cpp.orig	2013-09-29 08:55:53.000000000 +0000
-+++ shibsp/attribute/resolver/impl/ChainingAttributeResolver.cpp
-@@ -240,7 +240,7 @@ ChainingAttributeResolver::ChainingAttri
+--- shibsp/attribute/resolver/impl/ChainingAttributeResolver.cpp.orig	2015-10-19 17:29:03.000000000 -0700
++++ shibsp/attribute/resolver/impl/ChainingAttributeResolver.cpp	2015-10-19 17:29:32.000000000 -0700
+@@ -240,7 +240,7 @@
                  m_resolvers.push_back(np.get());
                  np.release();
              }
 -            catch (exception& ex) {
 +            catch (std::exception& ex) {
-                 Category::getInstance(SHIBSP_LOGCAT".AttributeResolver."CHAINING_ATTRIBUTE_RESOLVER).error(
+                 Category::getInstance(SHIBSP_LOGCAT ".AttributeResolver." CHAINING_ATTRIBUTE_RESOLVER).error(
                      "caught exception processing embedded AttributeResolver element: %s", ex.what()
                      );
-@@ -274,7 +274,7 @@ void ChainingAttributeResolver::resolveA
+@@ -274,7 +274,7 @@
              chain.m_ownedAssertions.insert(chain.m_ownedAssertions.end(), context->getResolvedAssertions().begin(), context->getResolvedAssertions().end());
              context->getResolvedAssertions().clear();
          }
 -        catch (exception& ex) {
 +        catch (std::exception& ex) {
-             Category::getInstance(SHIBSP_LOGCAT".AttributeResolver."CHAINING_ATTRIBUTE_RESOLVER).error(
+             Category::getInstance(SHIBSP_LOGCAT ".AttributeResolver." CHAINING_ATTRIBUTE_RESOLVER).error(
                  "caught exception applying AttributeResolver in chain: %s", ex.what()
                  );
