ccf222b6d054bdf467bb947f743d7289e91ca71f
joerg <joerg@pkgsrc.org>
2015-10-24 21:18:12 +0000

    Drop EOLed PostgreSQL 8.4 and 9.0 from the supported list of PG versions.

--- databases/postgresql-postgis2/buildlink3.mk
+++ databases/postgresql-postgis2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2015/06/10 01:43:00 wiedi Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2015/10/24 21:18:12 joerg Exp $
 
 BUILDLINK_TREE+=	postgresql-postgis
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.postgresql-postgis+=	postgresql${PGSQL_VERSION}-postgis>=1
 BUILDLINK_ABI_DEPENDS.postgresql-postgis?=	postgresql${PGSQL_VERSION}-postgis>=2.1.4nb2
 BUILDLINK_PKGSRCDIR.postgresql-postgis?=	../../databases/postgresql-postgis2
 
-PGSQL_VERSIONS_ACCEPTED=	94 93 92 91 90
+PGSQL_VERSIONS_ACCEPTED=	94 93 92 91
 
 .include "../../geography/geos/buildlink3.mk"
 .include "../../geography/proj/buildlink3.mk"
