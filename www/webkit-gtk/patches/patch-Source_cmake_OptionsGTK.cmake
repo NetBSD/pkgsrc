45c7134766f310bcf340b48f9e3b519731e9103a
leot <leot>
2015-10-24 13:37:45 +0000

    Update www/webkit-gtk to webkit-gtk-2.10.2.
    Changes:
    =================
    WebKitGTK+ 2.10.2
    =================
    
    What's new in WebKitGTK+ 2.10.2?
    
      - Fix a regression introduced in 2.10.1 that disabled accelerated
        compositing.
      - Fix build with cmake 3.4.
    
    =================
    WebKitGTK+ 2.10.1
    =================
    
    What's new in WebKitGTK+ 2.10.1?
    
      - Fix rendering of accelerated content in HiDPI screens.
      - Fix several media controls rendering issues.
      - Fix rendering of progress element with recent versions of GTK+.
      - Add and update some web inspector icons.
      - Correctly handle websites sending an invalid auth header.
      - Fix a crash when creating the UI process backing store in Wayland.
      - Fix the build with spellchecker disabled.
      - Fix the build with touch events disabled.
      - Fix the build with OpenGL disabled.
      - Several build fixes on Mac OSX.
      - Fix several crashes and rendering issues.

--- www/webkit-gtk/patches/patch-Source_cmake_OptionsGTK.cmake
+++ www/webkit-gtk/patches/patch-Source_cmake_OptionsGTK.cmake
@@ -1,4 +1,4 @@
-$NetBSD: patch-Source_cmake_OptionsGTK.cmake,v 1.2 2015/09/26 10:16:38 leot Exp $
+$NetBSD: patch-Source_cmake_OptionsGTK.cmake,v 1.3 2015/10/24 13:37:45 leot Exp $
 
 Do not conflict with CMAKE_MODULE_PATH during the linking phase of
 libwebkit2gtk-4.0.so:
@@ -10,14 +10,14 @@ libwebkit2gtk-4.0.so:
  Source/WebKit2/CMakeFiles/WebKit2.dir/build.make:16139: recipe for target 'lib/libwebkit2gtk-4.0.so.37.2.6' failed
  [...]
 
---- Source/cmake/OptionsGTK.cmake.orig	2015-09-21 08:37:26.000000000 +0000
+--- Source/cmake/OptionsGTK.cmake.orig	2015-10-15 09:18:14.000000000 +0000
 +++ Source/cmake/OptionsGTK.cmake
-@@ -103,7 +103,7 @@ if (DEVELOPER_MODE)
- else ()
+@@ -105,7 +105,7 @@ else ()
      WEBKIT_OPTION_DEFAULT_PORT_VALUE(ENABLE_MINIBROWSER PUBLIC OFF)
      WEBKIT_OPTION_DEFAULT_PORT_VALUE(ENABLE_API_TESTS PRIVATE OFF)
--    set(WebKit2_VERSION_SCRIPT "-Wl,--version-script,${CMAKE_MODULE_PATH}/gtksymbols.filter")
-+    set(WebKit2_VERSION_SCRIPT "-Wl,--version-script,${CMAKE_SOURCE_DIR}/Source/cmake/gtksymbols.filter")
+     if (NOT CMAKE_SYSTEM_NAME MATCHES "Darwin")
+-        set(WebKit2_VERSION_SCRIPT "-Wl,--version-script,${CMAKE_MODULE_PATH}/gtksymbols.filter")
++        set(WebKit2_VERSION_SCRIPT "-Wl,--version-script,${CMAKE_SOURCE_DIR}/Source/cmake/gtksymbols.filter")
+     endif ()
  endif ()
  
- if (CMAKE_SYSTEM_NAME MATCHES "Linux")
