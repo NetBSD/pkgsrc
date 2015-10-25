d24fd3385da6d08a40e92a283e8c0f71970106d9
jmcneill <jmcneill@pkgsrc.org>
2015-10-25 11:53:24 +0000

    Add a pkg option for building with gtk3 support.

---
+++ www/midori/options.mk
@@ -0,0 +1,22 @@
+# $NetBSD: options.mk,v 1.1 2015/10/25 11:53:24 jmcneill Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.midori
+
+PKG_OPTIONS_REQUIRED_GROUPS=	gtk
+PKG_OPTIONS_GROUP.gtk=		gtk2 gtk3
+
+PKG_SUGGESTED_OPTIONS=		gtk2
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgtk2)
+.include "../../www/webkit24-gtk/buildlink3.mk"  
+.include "../../x11/gtk2/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mgtk3)
+CONFIGURE_ARGS+=	--enable-gtk3
+CMAKE_ARGS+=		-DUSE_GTK3=1
+.include "../../www/webkit24-gtk3/buildlink3.mk"  
+.include "../../x11/gtk3/buildlink3.mk"
+.endif
