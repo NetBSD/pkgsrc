260963c3114696a3da7893035360b6d010001bbe
jmcneill <jmcneill@pkgsrc.org>
2015-10-25 15:59:37 +0000

    NEON runtime detection for NetBSD/arm.

--- www/firefox/patches/patch-gfx_skia_trunk_src_core_SkUtilsArm.cpp
+++ www/firefox/patches/patch-gfx_skia_trunk_src_core_SkUtilsArm.cpp
@@ -1,8 +1,19 @@
-$NetBSD: patch-gfx_skia_trunk_src_core_SkUtilsArm.cpp,v 1.1 2015/10/16 12:59:36 jmcneill Exp $
+$NetBSD: patch-gfx_skia_trunk_src_core_SkUtilsArm.cpp,v 1.2 2015/10/25 15:59:37 jmcneill Exp $
 
 --- gfx/skia/trunk/src/core/SkUtilsArm.cpp.orig	2015-09-29 21:44:49.000000000 +0000
 +++ gfx/skia/trunk/src/core/SkUtilsArm.cpp
-@@ -47,6 +47,11 @@
+@@ -16,6 +16,10 @@
+ #include <string.h>
+ #include <pthread.h>
+ 
++#if defined(__NetBSD__)
++#include <sys/sysctl.h>
++#endif
++
+ // Set USE_ANDROID_NDK_CPU_FEATURES to use the Android NDK's
+ // cpu-features helper library to detect NEON at runtime. See
+ // http://crbug.com/164154 to see why this is needed in Chromium
+@@ -47,6 +51,11 @@
  #  endif
  #endif
  
@@ -14,3 +25,21 @@ $NetBSD: patch-gfx_skia_trunk_src_core_SkUtilsArm.cpp,v 1.1 2015/10/16 12:59:36 
  // A function used to determine at runtime if the target CPU supports
  // the ARM NEON instruction set. This implementation is Linux-specific.
  static bool sk_cpu_arm_check_neon(void) {
+@@ -82,6 +91,17 @@ static bool sk_cpu_arm_check_neon(void) 
+ 
+   result = (android_getCpuFeatures() & ANDROID_CPU_ARM_FEATURE_NEON) != 0;
+ 
++#elif defined(__NetBSD__)
++
++    size_t len;
++    int flag;
++    len = sizeof(flag);
++    if (sysctlbyname("machdep.neon_present", &flag, &len, NULL, 0) == 0) {
++        result = flag != 0;
++    } else {
++        result = false;
++    }
++
+ #else  // USE_ANDROID_NDK_CPU_FEATURES
+ 
+     // There is no user-accessible CPUID instruction on ARM that we can use.
