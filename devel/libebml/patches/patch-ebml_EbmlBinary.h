380a1e7cdc9e8f017315b00ae665509452a643b4
wiz <wiz@pkgsrc.org>
2015-10-25 09:47:34 +0000

    Add upstream bug report URL.

--- devel/libebml/patches/patch-ebml_EbmlBinary.h
+++ devel/libebml/patches/patch-ebml_EbmlBinary.h
@@ -1,4 +1,9 @@
-$NetBSD: patch-ebml_EbmlBinary.h,v 1.1 2013/04/29 21:31:10 joerg Exp $
+$NetBSD: patch-ebml_EbmlBinary.h,v 1.2 2015/10/25 09:47:34 wiz Exp $
+
+Add cstdlib for free/malloc.
+Needed when compiling with clang/libc++.
+
+https://github.com/Matroska-Org/libebml/pull/13
 
 --- ebml/EbmlBinary.h.orig	2013-04-28 06:51:13.000000000 +0000
 +++ ebml/EbmlBinary.h
