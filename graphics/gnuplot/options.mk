1e9d33866c1284ab9743910d85a037acc408759b
wiz <wiz@pkgsrc.org>
2015-10-25 10:13:53 +0000

    Add options for pdf docs and cairo.
    From Rin Okuyama in PR 50363.
    
    While here, clean up some unrecognized configure arguments
    and fix the build with the qt4 option.
    
    XXX: this supports qt5 now too, does someone want to add an option for that?

--- graphics/gnuplot/options.mk
+++ graphics/gnuplot/options.mk
@@ -1,12 +1,19 @@
-# $NetBSD: options.mk,v 1.8 2014/05/08 20:48:55 bad Exp $
+# $NetBSD: options.mk,v 1.9 2015/10/25 10:13:53 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnuplot
-PKG_SUPPORTED_OPTIONS=	gd lua pdf x11 qt4 wxwidgets
-PKG_SUGGESTED_OPTIONS=	gd x11
+PKG_SUPPORTED_OPTIONS=	cairo gd lua pdf gnuplot-pdf-doc x11 qt4 wxwidgets
+PKG_SUGGESTED_OPTIONS=	cairo gd gnuplot-pdf-doc x11
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	x11 qt4
+PLIST_VARS+=	gnuplot-pdf-doc x11 qt4
+
+.if !empty(PKG_OPTIONS:Mcairo)
+.include "../../devel/pango/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-cairo
+.endif
 
 .if !empty(PKG_OPTIONS:Mgd)
 .include "../../graphics/gd/buildlink3.mk"
@@ -24,6 +31,18 @@ CONFIGURE_ARGS+=	--with-pdf
 .include "../../print/pdflib-lite/buildlink3.mk"
 .endif
 
+# to build doc/gnuplot.pdf
+.if !empty(PKG_OPTIONS:Mgnuplot-pdf-doc)
+PLIST.gnuplot-pdf-doc=	yes
+BUILD_DEPENDS+=	tex-latex-bin-[0-9]*:../../print/tex-latex-bin
+BUILD_DEPENDS+=	tex-ucs-[0-9]*:../../print/tex-ucs
+post-build:
+	(cd ${WRKSRC}/docs; ${GMAKE} gnuplot.pdf)
+post-install:
+	(cd ${WRKSRC}/docs; \
+	${INSTALL_DATA} gnuplot.pdf ${DESTDIR}${PREFIX}/share/gnuplot/${API_VERSION})
+.endif
+
 .if !empty(PKG_OPTIONS:Mx11)
 PLIST.x11=	yes
 .include "../../x11/libXaw/buildlink3.mk"
@@ -33,11 +52,12 @@ CONFIGURE_ARGS+=	--without-x
 
 .if !empty(PKG_OPTIONS:Mqt4)
 USE_LANGUAGES+=		c++
-CONFIGURE_ARGS+=	--enable-qt
+CONFIGURE_ARGS+=	--with-qt=qt4
 PLIST.qt4=		yes
 .include "../../x11/qt4-libs/buildlink3.mk"
+.include "../../x11/qt4-tools/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=	--disable-qt
+CONFIGURE_ARGS+=	--with-qt=no
 .endif
 
 .if !empty(PKG_OPTIONS:Mwxwidgets)
