5036a1c1b2da488bd20e0881cc1bb990ccce17cc
adam <adam@pkgsrc.org>
2015-10-27 06:48:57 +0000

    * Fix building on Darwin. * Switch to FFmpeg-2.x, so we can get rid of FFmpeg-1 easier.

--- audio/chromaprint/patches/patch-cmake_modules_FindFFmpeg.cmake
+++ audio/chromaprint/patches/patch-cmake_modules_FindFFmpeg.cmake
@@ -1,6 +1,6 @@
-$NetBSD: patch-cmake_modules_FindFFmpeg.cmake,v 1.1 2014/08/11 23:21:11 wiz Exp $
+$NetBSD: patch-cmake_modules_FindFFmpeg.cmake,v 1.2 2015/10/27 06:48:57 adam Exp $
 
-Include ffmpeg1 pkgsrc paths in search path.
+Include ffmpeg2 pkgsrc paths in search path.
 
 --- cmake/modules/FindFFmpeg.cmake.orig	2013-11-23 15:43:42.000000000 +0000
 +++ cmake/modules/FindFFmpeg.cmake
@@ -8,7 +8,7 @@ Include ffmpeg1 pkgsrc paths in search path.
  	FIND_PATH(FFMPEG_${varname}_INCLUDE_DIRS lib${shortname}/${headername}
  		PATHS
  		${FFMPEG_ROOT}/include
-+		${FFMPEG_ROOT}/include/ffmpeg1
++		${FFMPEG_ROOT}/include/ffmpeg2
  		$ENV{FFMPEG_DIR}/include
  		~/Library/Frameworks
  		/Library/Frameworks
@@ -17,7 +17,7 @@ Include ffmpeg1 pkgsrc paths in search path.
  	)
  	FIND_PATH(FFMPEG_${varname}_INCLUDE_DIRS lib${shortname}/${headername}
 -		PATH_SUFFIXES ffmpeg
-+		PATH_SUFFIXES ffmpeg ffmpeg1
++		PATH_SUFFIXES ffmpeg ffmpeg2
  		DOC "Location of FFMPEG Headers"
  	)
  
@@ -25,7 +25,7 @@ Include ffmpeg1 pkgsrc paths in search path.
          NAMES ${shortname}
          PATHS
          ${FFMPEG_ROOT}/lib
-+        ${FFMPEG_ROOT}/lib/ffmpeg1
++        ${FFMPEG_ROOT}/lib/ffmpeg2
          $ENV{FFMPEG_DIR}/lib
          ~/Library/Frameworks
          /Library/Frameworks
