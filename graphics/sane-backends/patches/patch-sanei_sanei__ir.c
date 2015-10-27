811cc48964a17fd11ac0ecb6bb9a2cda3198d9ef
wiz <wiz@pkgsrc.org>
2015-10-27 11:29:35 +0000

    Remove some obsolete patches. Add upstream bug report URLs for some others.
    XXX:
    patch-a[bhijk] are not sent upstream since I don't understand the need.

--- graphics/sane-backends/patches/patch-sanei_sanei__ir.c
+++ graphics/sane-backends/patches/patch-sanei_sanei__ir.c
@@ -1,4 +1,7 @@
-$NetBSD: patch-sanei_sanei__ir.c,v 1.1 2015/10/25 10:39:13 wiz Exp $
+$NetBSD: patch-sanei_sanei__ir.c,v 1.2 2015/10/27 11:29:35 wiz Exp $
+
+Remove unportable header inclusion.
+https://alioth.debian.org/tracker/index.php?func=detail&aid=315208&group_id=30186&atid=410366
 
 --- sanei/sanei_ir.c.orig	2015-09-29 01:10:43.000000000 +0000
 +++ sanei/sanei_ir.c
