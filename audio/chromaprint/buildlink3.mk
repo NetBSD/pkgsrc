8ab0e151bc80f7e053af1c86b77c82d29647b82e
adam <adam@pkgsrc.org>
2015-10-27 06:48:57 +0000

    * Fix building on Darwin. * Switch to FFmpeg-2.x, so we can get rid of FFmpeg-1 easier.

--- audio/chromaprint/buildlink3.mk
+++ audio/chromaprint/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/11 23:09:44 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/10/27 06:48:57 adam Exp $
 
 BUILDLINK_TREE+=	chromaprint
 
@@ -8,7 +8,7 @@ CHROMAPRINT_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.chromaprint+=	chromaprint>=1.1
 BUILDLINK_PKGSRCDIR.chromaprint?=	../../audio/chromaprint
 
-#.include "../../multimedia/ffmpeg1/buildlink3.mk"
+.include "../../multimedia/ffmpeg2/buildlink3.mk"
 .endif	# CHROMAPRINT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-chromaprint
