fda7967591769c31b3980c15be9c5b84abfcf03d
wiz <wiz@pkgsrc.org>
2015-10-27 11:29:35 +0000

    Remove some obsolete patches. Add upstream bug report URLs for some others.
    XXX:
    patch-a[bhijk] are not sent upstream since I don't understand the need.

--- graphics/sane-backends/patches/patch-backend_pieusb__buffer.c
+++ graphics/sane-backends/patches/patch-backend_pieusb__buffer.c
@@ -1,4 +1,8 @@
-$NetBSD: patch-backend_pieusb__buffer.c,v 1.1 2015/10/25 10:39:13 wiz Exp $
+$NetBSD: patch-backend_pieusb__buffer.c,v 1.2 2015/10/27 11:29:35 wiz Exp $
+
+Remove unportable header inclusion.
+XXX: should be fixed better, hope upstream does that.
+https://alioth.debian.org/tracker/index.php?func=detail&aid=315209&group_id=30186&atid=410366
 
 --- backend/pieusb_buffer.c.orig	2015-09-01 00:31:33.000000000 +0000
 +++ backend/pieusb_buffer.c
