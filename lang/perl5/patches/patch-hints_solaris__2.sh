d1e201e450c9d1694892205322685565e468b964
jperkin <jperkin@pkgsrc.org>
2015-10-27 09:10:44 +0000

    Support SunOS/clang, fixes 64-bit build.

--- lang/perl5/patches/patch-hints_solaris__2.sh
+++ lang/perl5/patches/patch-hints_solaris__2.sh
@@ -1,9 +1,18 @@
-$NetBSD: patch-hints_solaris__2.sh,v 1.1 2015/05/15 14:32:27 ryoon Exp $
+$NetBSD: patch-hints_solaris__2.sh,v 1.2 2015/10/27 09:10:44 jperkin Exp $
 
 Redo PR pkg/44999.
 
---- hints/solaris_2.sh.orig	2014-12-27 11:48:52.000000000 +0000
+--- hints/solaris_2.sh.orig	2015-05-13 20:19:29.000000000 +0000
 +++ hints/solaris_2.sh
+@@ -585,7 +585,7 @@ EOM
+ 		fi
+ 	    fi
+ 	    case "${cc:-cc} -v 2>/dev/null" in
+-	    *gcc*)
++	    *gcc*|clang*)
+ 		echo 'int main() { return 0; }' > try.c
+ 		case "`${cc:-cc} $ccflags -mcpu=v9 -m64 -S try.c 2>&1 | grep 'm64 is not supported by this configuration'`" in
+ 		*"m64 is not supported"*)
 @@ -622,7 +622,7 @@ EOM
  		# use that with Solaris 11 and later, but keep
  		# the old behavior for older Solaris versions.
