75f91f0028c92cebf5d8ca4aed566333bf6815dc
leot <leot>
2015-11-02 18:42:50 +0000

    INT_MIN and DBL_MIN are respectively included in <limits.h> and <float.h>. Fixes build on Linux. PKGREVISION++
    Noted and patch provided by Matthias Ferdinand via pkgsrc-users@.

--- graphics/sane-backends/patches/patch-sanei_sanei__ir.c
+++ graphics/sane-backends/patches/patch-sanei_sanei__ir.c
@@ -1,15 +1,17 @@
-$NetBSD: patch-sanei_sanei__ir.c,v 1.2 2015/10/27 11:29:35 wiz Exp $
+$NetBSD: patch-sanei_sanei__ir.c,v 1.3 2015/11/02 18:42:50 leot Exp $
 
 Remove unportable header inclusion.
 https://alioth.debian.org/tracker/index.php?func=detail&aid=315208&group_id=30186&atid=410366
 
 --- sanei/sanei_ir.c.orig	2015-09-29 01:10:43.000000000 +0000
 +++ sanei/sanei_ir.c
-@@ -29,7 +29,6 @@
+@@ -29,7 +29,8 @@
  
  #include <stdlib.h>
  #include <string.h>
 -#include <values.h>
++#include <limits.h>
++#include <float.h>
  #include <math.h>
  
  #define BACKEND_NAME sanei_ir	/* name of this module for debugging */
