101a0817f2b7fbd9c7b6fb9d87e3ca5c6fd55246
markd <markd@pkgsrc.org>
2015-11-03 20:08:06 +0000

    Add opencv-contrib-face 3.0.0
    The contributed face recognition module for OpenCV.

---
+++ graphics/opencv-contrib-face/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/11/03 20:08:06 markd Exp $
+
+BUILDLINK_TREE+=	opencv-contrib-face
+
+.if !defined(OPENCV_CONTRIB_FACE_BUILDLINK3_MK)
+OPENCV_CONTRIB_FACE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.opencv-contrib-face+=	opencv-contrib-face>=3.0.0
+BUILDLINK_PKGSRCDIR.opencv-contrib-face?=	../../graphics/opencv-contrib-face
+
+.include "../../graphics/opencv/buildlink3.mk"
+.endif	# OPENCV_CONTRIB_FACE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-opencv-contrib-face
