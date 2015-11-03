5a6bbe66ff1d4d4da336339e1aec80824764a8ac
schnoebe <schnoebe@pkgsrc.org>
2015-11-03 15:08:50 +0000

    A group of updates from Khorben, correcting some mangled paths.
    Thank you Khorben!

---
+++ print/hplip/patches/patch-diagnose_queues.py
@@ -0,0 +1,13 @@
+$NetBSD: patch-diagnose_queues.py,v 1.1 2015/11/03 15:08:50 schnoebe Exp $
+
+--- diagnose_queues.py.orig	2015-08-19 13:25:37.000000000 +0000
++++ diagnose_queues.py
+@@ -22,7 +22,7 @@
+ from __future__ import print_function
+ __version__ = '1.1'
+ __title__ = 'AutoConfig Utility to check queues configuration'
+-__mod__ = 'hp-daignose-queues'
++__mod__ = 'hp-diagnose-queues'
+ __doc__ = """Auto config utility for HPLIP supported multifunction Devices to diagnose queues configuration."""
+ 
+ # Std Lib
--- print/hplip/patches/patch-installer_dcheck.py
+++ print/hplip/patches/patch-installer_dcheck.py
@@ -1,4 +1,4 @@
-$NetBSD: patch-installer_dcheck.py,v 1.1 2015/08/27 23:53:24 khorben Exp $
+$NetBSD: patch-installer_dcheck.py,v 1.2 2015/11/03 15:08:50 schnoebe Exp $
 
 Locate libraries on systems without ldconfig(8)
 
@@ -6,6 +6,12 @@ This only looks for libraries in pkgsrc's own library folder.
 
 --- installer/dcheck.py.orig	2015-06-07 19:25:11.000000000 +0000
 +++ installer/dcheck.py
+@@ -1,4 +1,4 @@
+-#!/usr/bin/env python
++#!/usr/pkg/bin/python2.7
+ # -*- coding: utf-8 -*-
+ #
+ # (c) Copyright 2003-2007 Hewlett-Packard Development Company, L.P.
 @@ -99,6 +99,8 @@ def check_lib(lib, min_ver=0):
          #    pass
          #else:
