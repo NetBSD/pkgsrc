5ca6a4c8b66811c79432014afd9f09c07ae43132
fhajny <fhajny@pkgsrc.org>
2015-11-05 12:23:50 +0000

    Update ejabberd to 15.10.
    Switch dependencies to use standalone packages for Erlang modules.
    
    Core
    - c2s: Let stop/1 close XEP-0198 sessions
    - c2s: Close socket when waiting for resume
    - s2s: Don't crash on certificates without extensions
    - xml: Optimizations in memory allocator
    - OAuth: Fix cyrsasl_oauth:mech_new call
    
    Modules
    - mod_http_upload: New HTTP File Upload module (XEP-0363)
    - mod_http_upload_quota: New module to support user quotas
    - mod_metrics: New simple metrics module for use with grapherl
    - mod_mam: Advertise XEP-0313 v0.4 support in room disco
    - mod_mam: Avoid MAM dups when routing to multiple resources
    
    Compilation
    - Make: Pass {C,CPP,LD}FLAGS used in main ./configure to deps
    - Use OTP directory structure for `make install`
    - mix.lock should be commited to ensure consistent builds
    - Code cosmetic cleanup
    
    Commands
    - Let modules_update_specs report failures
    - Fix problem with -auth
    - Fix unauthenticated calls to commands with policy=user
    - Improve access_commands option backward compatibility
    
    Configuration
    - ejabberdctl.cfg: New CONTRIB_MODULES_CONF_DIR
    - Be able to merge old style configs with {listen,...}
    - Fix support for macros
    - mod_ping: New option ping_ack_timeout

--- chat/ejabberd/options.mk
+++ chat/ejabberd/options.mk
@@ -1,19 +1,21 @@
-# $NetBSD: options.mk,v 1.1 2015/04/29 10:03:58 fhajny Exp $
+# $NetBSD: options.mk,v 1.2 2015/11/05 12:23:50 fhajny Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ejabberd
-PKG_SUPPORTED_OPTIONS=	sqlite
+PKG_SUPPORTED_OPTIONS=	redis sqlite
 PKG_SUGGESTED_OPTIONS=	#
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_SRC=		${PLIST_SRC_DFLT:Q}
+#
+# Add Redis support
+#
+.if !empty(PKG_OPTIONS:Mredis)
+DEPENDS+=		erlang-eredis-[0-9]*:../../databases/erlang-eredis
+.endif
 
 #
 # Add SQLite 3 support
 #
 .if !empty(PKG_OPTIONS:Msqlite)
-CONFIGURE_ARGS+=	--enable-sqlite
-CONFIGURE_ARGS+=	--with-sqlite3=${BUILDLINK_PREFIX.sqlite3}
-PLIST_SRC+=		PLIST.sqlite
-. include "../../databases/sqlite3/buildlink3.mk"
+DEPENDS+=		erlang-sqlite3-[0-9]*:../../databases/erlang-sqlite3
 .endif
