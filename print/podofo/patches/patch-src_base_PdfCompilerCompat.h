9ad3a4837ae38c91a3cc7dde7c15b1b66723bff3
adam <adam@pkgsrc.org>
2015-11-05 19:46:53 +0000

    Changes 0.9.3: OSX build fixes Fixed AES decryption Improved load of (broken) PDF files Sign PDF file as an incremental update Added partial support for Type3 font encoding and metrics Drawing API improvements Unicode file names for attachments Font subset embedding

--- print/podofo/patches/patch-src_base_PdfCompilerCompat.h
+++ print/podofo/patches/patch-src_base_PdfCompilerCompat.h
@@ -1,3 +1,5 @@
+$NetBSD: patch-src_base_PdfCompilerCompat.h,v 1.2 2015/11/05 19:46:53 adam Exp $
+
 --- src/base/PdfCompilerCompat.h.orig	2011-09-30 13:20:59.000000000 +0200
 +++ src/base/PdfCompilerCompat.h	2014-08-26 14:58:16.000000000 +0200
 @@ -72,6 +72,9 @@
