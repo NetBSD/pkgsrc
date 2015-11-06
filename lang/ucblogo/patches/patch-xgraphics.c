beacd8ba466694cf6fcfeffe76b3d481acdf1b71
joerg <joerg@pkgsrc.org>
2015-11-06 20:33:21 +0000

    Fix a number of cases of plainly invalid C and portability issues.

--- lang/ucblogo/patches/patch-xgraphics.c
+++ lang/ucblogo/patches/patch-xgraphics.c
@@ -1,8 +1,17 @@
-$NetBSD: patch-xgraphics.c,v 1.1 2012/11/01 19:55:37 joerg Exp $
+$NetBSD: patch-xgraphics.c,v 1.2 2015/11/06 20:33:21 joerg Exp $
 
---- xgraphics.c.orig	2012-10-30 22:05:56.000000000 +0000
+--- xgraphics.c.orig	2007-12-05 21:00:23.000000000 +0000
 +++ xgraphics.c
-@@ -289,7 +289,7 @@ void placate_x()
+@@ -314,7 +314,7 @@ void check_X11_stop() {
+ 
+ int get_button()
+ {
+-  checkX;
++  checkX2(-1);
+ 
+   placate_x();
+ 
+@@ -323,7 +323,7 @@ int get_button()
  
  int get_mouse_x()
  {
@@ -11,7 +20,7 @@ $NetBSD: patch-xgraphics.c,v 1.1 2012/11/01 19:55:37 joerg Exp $
  
    placate_x();
  
-@@ -299,7 +299,7 @@ int get_mouse_x()
+@@ -333,7 +333,7 @@ int get_mouse_x()
  
  int get_mouse_y()
  {
