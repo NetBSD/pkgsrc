f7aaf64b6c70df92ae7b8415e5bafad3eda50e1f
adam <adam@pkgsrc.org>
2015-11-06 17:35:26 +0000

    Added qt5 as an option. Avoid SDK build on OS X.

--- net/wireshark/options.mk
+++ net/wireshark/options.mk
@@ -1,18 +1,39 @@
-# $NetBSD: options.mk,v 1.10 2015/10/21 09:12:10 wiz Exp $
+# $NetBSD: options.mk,v 1.11 2015/11/06 17:35:26 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.wireshark
-PKG_SUPPORTED_OPTIONS=	gtk3 lua
+PKG_SUPPORTED_OPTIONS=	gtk3 lua qt5
 PKG_SUGGESTED_OPTIONS=	gtk3 lua
 PKG_OPTIONS_LEGACY_OPTS+=	gtk2:gtk3
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		gtk3
-PLIST_VARS+=		icons
+PLIST_VARS+=		gtk3 icons qt5
 
-.if !empty(PKG_OPTIONS:Mgtk3)
-CONFIGURE_ARGS+=	--enable-wireshark
+.if empty(PKG_OPTIONS:Mqt5)
+CONFIGURE_ARGS+=	--without-qt
+.else
+CONFIGURE_ARGS+=	--with-qt
+CONFIGURE_ENV+=		MOC=${QTDIR}/bin/moc
+CONFIGURE_ENV+=		RCC=${QTDIR}/bin/rcc
+CONFIGURE_ENV+=		UIC=${QTDIR}/bin/uic
+# RCC is not configured, so fix it here; see patch-ui_qt_Makefile.in
+SUBST_CLASSES+=		fix-rcc
+SUBST_MESSAGE.fix-rcc=	Fixing rcc path.
+SUBST_STAGE.fix-rcc=	pre-configure
+SUBST_FILES.fix-rcc=	ui/qt/Makefile.in
+SUBST_SED.fix-rcc=	-e 's,@RCC@,${QTDIR}/bin/rcc,'
+PLIST.qt5=		yes
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.  if ${OPSYS} == "Darwin"
+.include "../../x11/qt5-qtmacextras/buildlink3.mk"
+.  else
+.include "../../x11/qt5-qtx11extras/buildlink3.mk"
+.  endif
+.endif
+
+.if empty(PKG_OPTIONS:Mgtk3)
+CONFIGURE_ARGS+=	--without-gtk3
+.else
 CONFIGURE_ARGS+=	--with-gtk3
-CONFIGURE_ARGS+=	--without-qt # XXX make this an option
 PLIST.gtk3=		yes
 POST_INSTALL_TARGETS+=	install-gtk-desktop
 .include "../../x11/gtk3/buildlink3.mk"
@@ -22,13 +43,12 @@ install-gtk-desktop:
 	${INSTALL_DATA} ${WRKSRC}/wireshark.desktop \
 		${DESTDIR}${PREFIX}/share/applications
 
-.else
-CONFIGURE_ARGS+=	--disable-wireshark
 .endif
 
 # We might install the qt front end one day as well,
 # so have a generic icon target
-.if !empty(PKG_OPTIONS:Mgtk3)
+.if !empty(PKG_OPTIONS:Mgtk3) || !empty(PKG_OPTIONS:Mqt5)
+CONFIGURE_ARGS+=	--enable-wireshark
 PLIST.icons=		yes
 POST_INSTALL_TARGETS+=	install-icons
 INSTALLATION_DIRS+=	share/applications
@@ -37,15 +57,15 @@ ICON_COLORS=		hi lo
 ICON_SIZES=		16 32 48
 MIMEICON_SIZES=		16 24 32 48 64 128 256
 
-.for c in ${ICON_COLORS}
-.for d in ${ICON_SIZES}
+.  for c in ${ICON_COLORS}
+.    for d in ${ICON_SIZES}
 INSTALLATION_DIRS+=	share/icons/${c}color/${d}x${d}/apps
-.endfor
-.endfor
+.    endfor
+.  endfor
 
-.for d in ${MIMEICON_SIZES}
+.  for d in ${MIMEICON_SIZES}
 INSTALLATION_DIRS+=	share/icons/hicolor/${d}x${d}/mimetypes
-.endfor
+.  endfor
 
 .include "../../sysutils/desktop-file-utils/desktopdb.mk"
 .include "../../graphics/hicolor-icon-theme/buildlink3.mk"
@@ -54,17 +74,19 @@ INSTALLATION_DIRS+=	share/icons/hicolor/${d}x${d}/mimetypes
 install-icons:
 	${INSTALL_DATA} ${WRKSRC}/image/wsicon.svg \
 		${DESTDIR}${PREFIX}/share/icons/hicolor/scalable/apps/wireshark.svg
-.for c in ${ICON_COLORS}
-.for d in ${ICON_SIZES}
+.  for c in ${ICON_COLORS}
+.    for d in ${ICON_SIZES}
 	${INSTALL_DATA} ${WRKSRC}/image/${c}${d}-app-wireshark.png \
 		${DESTDIR}${PREFIX}/share/icons/${c}color/${d}x${d}/apps/wireshark.png
-.endfor
-.endfor
+.    endfor
+.  endfor
 
-.for d in ${MIMEICON_SIZES}
+.  for d in ${MIMEICON_SIZES}
 	${INSTALL_DATA} ${WRKSRC}/image/WiresharkDoc-${d}.png \
 		${DESTDIR}${PREFIX}/share/icons/hicolor/${d}x${d}/mimetypes/application-vnd.tcpdump.pcap.png
-.endfor
+.  endfor
+.else
+CONFIGURE_ARGS+=	--disable-wireshark
 .endif
 
 PLIST_VARS+=		lua
