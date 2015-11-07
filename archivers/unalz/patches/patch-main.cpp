d2c3135f5402adf4db70488ca0bfbab48b21daf5
dholland <dholland@pkgsrc.org>
2015-11-07 20:44:23 +0000

    Remove unused #include of <sys/timeb.h>, which is an old legacy header that doesn't exist on many platforms.

---
+++ archivers/unalz/patches/patch-main.cpp
@@ -0,0 +1,14 @@
+$NetBSD: patch-main.cpp,v 1.1 2015/11/07 20:44:23 dholland Exp $
+
+Remove unused legacy header.
+
+--- main.cpp~	2009-04-01 06:03:52.000000000 +0000
++++ main.cpp
+@@ -49,7 +49,6 @@
+ #include "UnAlz.h"
+ #include "UnAlzUtils.h"
+ 
+-#include <sys/timeb.h>
+ 
+ 
+ BOOL	g_bPipeMode=FALSE;
