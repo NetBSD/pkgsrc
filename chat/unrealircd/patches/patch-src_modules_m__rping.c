67353ea714b3efa7f0ba88a52b83c1f08aa0e333
dholland <dholland>
2015-11-07 22:46:25 +0000

    Avoid unnecessary use of legacy sys/timeb.h, which doesn't exist on OpenBSD.

---
+++ chat/unrealircd/patches/patch-src_modules_m__rping.c
@@ -0,0 +1,16 @@
+$NetBSD: patch-src_modules_m__rping.c,v 1.1 2015/11/07 22:46:25 dholland Exp $
+
+Avoid unnecessary use of legacy sys/timeb.h, which doesn't exist on OpenBSD.
+
+--- src/modules/m_rping.c~	2014-07-26 15:42:10.000000000 +0000
++++ src/modules/m_rping.c
+@@ -32,8 +32,8 @@
+ #include <string.h>
+ #ifdef _WIN32
+ #include <io.h>
+-#endif
+ #include <sys/timeb.h>
++#endif
+ #include <fcntl.h>
+ #include "h.h"
+ #include "proto.h"
