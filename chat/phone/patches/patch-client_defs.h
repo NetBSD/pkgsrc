bd91e395f848de90329e2945a5c50b334d3ba018
dholland <dholland>
2015-11-07 22:19:46 +0000

    Add missing <limits.h>, seen on MacOS.

--- chat/phone/patches/patch-client_defs.h
+++ chat/phone/patches/patch-client_defs.h
@@ -1,22 +1,24 @@
-$NetBSD: patch-client_defs.h,v 1.1 2014/09/11 07:15:56 dholland Exp $
+$NetBSD: patch-client_defs.h,v 1.2 2015/11/07 22:19:46 dholland Exp $
 
 - Don't rely on __unused from netbsd's <cdefs.h> existing.
 - Use a more portable username size symbol for the sake of Solaris.
+- Also include limits.h to make sure that symbol is defined.
 
---- client/defs.h~	2013-01-02 23:00:42.000000000 +0000
+--- client/defs.h.orig	2013-01-02 23:00:42.000000000 +0000
 +++ client/defs.h
-@@ -25,6 +25,10 @@
+@@ -24,6 +24,11 @@
+ #include <poll.h>
  #include <curses.h>
  #include <netdb.h>
- 
++#include <limits.h>
++
 +#ifndef __unused
 +#define __unused __attribute__((__unused__))
 +#endif
-+
+ 
  int	msgpending;
  int	killedchild;
- int	changed_size;
-@@ -38,7 +38,7 @@ char	*mytty;			    /* tty he's on	      
+@@ -38,7 +43,7 @@ char	*mytty;			    /* tty he's on	      
  char	*home;			    /* home directory	       */
  const char *shell;		    /* preferred shell	       */
  char	myhost[MAXHOSTNAMELEN];	    /* local host name	       */
