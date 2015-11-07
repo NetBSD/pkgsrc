bde6a4d4ec1e30443037c7e8579663144d4dc8f0
dholland <dholland@pkgsrc.org>
2015-11-07 20:28:30 +0000

    Add gssapi option, off by default; unless enabled, explicitly disable Kerberos support. Otherwise the configure script may find it and then things fail; seen in the last PPC MacOS build.

--- chat/ctrlproxy/options.mk
+++ chat/ctrlproxy/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1.1.1 2009/01/04 01:05:46 adrianp Exp $
+# $NetBSD: options.mk,v 1.2 2015/11/07 20:28:30 dholland Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ctrlproxy
 
-PKG_SUPPORTED_OPTIONS=	ssl
+PKG_SUPPORTED_OPTIONS=	ssl gssapi
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,4 +11,10 @@ PKG_SUPPORTED_OPTIONS=	ssl
 ###
 .if !empty(PKG_OPTIONS:Mssl)
 .include "../../security/gnutls/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mgssapi)
+.include "../../mk/krb5.buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	ac_cv_path_KRB5_CONFIG="no"
 .endif
