ffdd7611b16508b9c9382b0dbe79394dc80647ac
dholland <dholland@pkgsrc.org>
2015-11-08 00:02:03 +0000

    Treat FreeBSD, DragonFly, OpenBSD, and Bitrig the same as NetBSD.

--- cross/bossa/patches/patch-PortFactory.h
+++ cross/bossa/patches/patch-PortFactory.h
@@ -1,12 +1,15 @@
-$NetBSD: patch-PortFactory.h,v 1.2 2014/02/10 20:18:04 reinoud Exp $
+$NetBSD: patch-PortFactory.h,v 1.3 2015/11/08 00:02:03 dholland Exp $
 
---- src/PortFactory.h.orig	2012-04-08 13:55:52.000000000 +0000
+Add support for NetBSD, and use it for other 4.4 derivatives as well.
+
+--- src/PortFactory.h.orig	2012-04-08 19:55:52.000000000 +0000
 +++ src/PortFactory.h
-@@ -45,6 +45,9 @@ typedef LinuxPortFactory PortFactory;
+@@ -45,6 +45,10 @@ typedef LinuxPortFactory PortFactory;
  #elif defined(__APPLE__)
  #include "OSXPortFactory.h"
  typedef OSXPortFactory PortFactory;
-+#elif defined(__NetBSD__)
++#elif defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__) ||\
++      defined(__OpenBSD__) || defined(__Bitrig__)
 +#include "NetBSDPortFactory.h"
 +typedef NetBSDPortFactory PortFactory;
  #else
