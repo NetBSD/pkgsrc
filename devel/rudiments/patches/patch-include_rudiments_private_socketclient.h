e2963f971ce6fb48463796da81dbc7cfd7f811ef
fhajny <fhajny@pkgsrc.org>
2015-11-10 10:04:40 +0000

    Update devel/rudiments to 0.54.
    - tweaked code in configure script to remove -Werror* to be
      non-greedy
    - updated semaphoreset/sharedmemory destructors to remove
      the semaphoreset/sharedmemory segment last
    - added url class that uses libcurl (read-only for now)
    - added support for urls to xmlsax (and thus xmldom and
      codetree)
    - added http support to the url class for platforms that
      lack an appropriate libcurl
    - charstring 1-argument trim methods trim all whitespace
      now, not just spaces
    - added charstring::isNullOrEmpty()
    - added read-until-terminator methods with maxbytes option
    - applied George Carrette's listener class patch to not close
      invalid-epoll filedescriptors

--- devel/rudiments/patches/patch-include_rudiments_private_socketclient.h
+++ devel/rudiments/patches/patch-include_rudiments_private_socketclient.h
@@ -1,13 +1,13 @@
-$NetBSD: patch-include_rudiments_private_socketclient.h,v 1.1 2014/04/28 12:15:20 fhajny Exp $
+$NetBSD: patch-include_rudiments_private_socketclient.h,v 1.2 2015/11/10 10:04:40 fhajny Exp $
 
 Fix build with -Woverloaded-virtual.
---- include/rudiments/private/socketclient.h.orig	2013-09-23 04:57:18.000000000 +0000
+--- include/rudiments/private/socketclient.h.orig	2015-10-10 10:07:55.000000000 +0000
 +++ include/rudiments/private/socketclient.h
-@@ -5,6 +5,7 @@
+@@ -6,6 +6,7 @@
  #ifdef RUDIMENTS_HAS_SSL
  		void	*newSSLBIO() const;
  #endif
 +		using ::client::connect;
  		int32_t	connect(const struct sockaddr *addr,
  				socklen_t addrlen, int32_t sec, int32_t usec);
- 		ssize_t	lowLevelRead(void *buf, ssize_t count) const;
+ 		ssize_t	lowLevelRead(void *buf, ssize_t count);
