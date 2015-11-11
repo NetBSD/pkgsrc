fe4b598a29c1040b739953467a8623a01ba066e3
wiz <wiz@pkgsrc.org>
2015-11-11 11:00:06 +0000

    When dtrace option is not enabled, explicitly disable dtrace.
    Fixes build on NetBSD built with dtrace.

--- databases/mysql-cluster/options.mk
+++ databases/mysql-cluster/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2014/12/01 05:58:03 jnemeth Exp $
+# $NetBSD: options.mk,v 1.2 2015/11/11 11:00:06 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mysql-cluster
 
@@ -18,4 +18,6 @@ CMAKE_ARGS+=		-DWITH_SSL=no
 # Enable DTrace support
 .if !empty(PKG_OPTIONS:Mdtrace)
 CMAKE_ARGS+=		-DENABLE_DTRACE=ON
+.else
+CMAKE_ARGS+=		-DENABLE_DTRACE=OFF
 .endif
