d614c1cc81fcc93ada046a942fbf312aec356b79
leot <leot@pkgsrc.org>
2015-11-11 16:12:10 +0000

    Update wm/dwm to 6.1.
    Changes:
    Features:
      - Now uses Xft for font rendering by default with fallback-font
    support, this also allows anti-aliased fonts and such.
        The font string matching will be different, check out
    http://www.freedesktop.org/software/fontconfig/fontconfig-user.html
    for the font string syntax, huge thanks to Eric Pruitt and the other
    people who worked on the dwm-xft patch (previously on the wiki).
      - Uses a shared libdraw codebase (also used for dmenu right now).
      - _NET_ACTIVE_WINDOW improvements (Andreas Amanns).
      - _NET_CLIENT_LIST support (Andreas Amanns).
      - Spawn dmenu on the selected monitor in dwm (dmenu -m option).
      - Use st as default terminal from now on (used to be xterm).
    
    Notable fixes:
      - Barwin memory leak fix (Eckehard Berns).
      - Drw_text width check fix (Jochen).
      - Setfullscreen: don't process the property twice (Quentin Rameau,
    Markus Teich).
      - Smoother resizing of floating windows (capped at 60fps), (Hiltjo).
      - Unfocus on slow sloppy monitor focus (Connor Lane Smith).
    
    Build improvements:
      - config.mk: add $FREETYPELIBS and $FREETYPEINC, simpler to override
    (ports and *BSDs).

--- wm/dwm/options.mk
+++ wm/dwm/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2011/06/17 11:59:57 obache Exp $
+# $NetBSD: options.mk,v 1.3 2015/11/11 16:12:10 leot Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dwm
 PKG_SUPPORTED_OPTIONS=	xinerama
@@ -16,9 +16,8 @@ PKG_SUGGESTED_OPTIONS=	xinerama
 .  include "../../x11/libXinerama/buildlink3.mk"
 .else
 SUBST_CLASSES+=		options
-SUBST_STAGE.options=	pre-build
+SUBST_STAGE.options=	pre-patch
 SUBST_MESSAGE.options=	Toggle the Xinerama support
 SUBST_FILES.options=	config.mk
 SUBST_SED.options+=	-e '/^XINERAMA/d'
-.  include "../../x11/libX11/buildlink3.mk"
 .endif
