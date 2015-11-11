af03480e919bb49381e3b4cac5007e7f5ea4e766
wiz <wiz@pkgsrc.org>
2015-11-11 12:40:03 +0000

    Replace patch with upstream version.

--- graphics/sane-backends/patches/patch-backend_pieusb__buffer.c
+++ graphics/sane-backends/patches/patch-backend_pieusb__buffer.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-backend_pieusb__buffer.c,v 1.2 2015/10/27 11:29:35 wiz Exp $
+$NetBSD: patch-backend_pieusb__buffer.c,v 1.3 2015/11/11 12:40:03 wiz Exp $
 
 Remove unportable header inclusion.
 XXX: should be fixed better, hope upstream does that.
@@ -6,13 +6,20 @@ https://alioth.debian.org/tracker/index.php?func=detail&aid=315209&group_id=3018
 
 --- backend/pieusb_buffer.c.orig	2015-09-01 00:31:33.000000000 +0000
 +++ backend/pieusb_buffer.c
-@@ -100,7 +100,9 @@
+@@ -100,15 +100,8 @@
  #include <stdio.h>
  #include <fcntl.h>
  #include <sys/mman.h>
-+#ifdef HAVE_ENDIAN_H
- #include <endian.h>
-+#endif
+-#include <endian.h>
  
- /* When creating the release backend, make complains about unresolved external
-  * le16toh, although it finds the include <endian.h> */
+-/* When creating the release backend, make complains about unresolved external
+- * le16toh, although it finds the include <endian.h> */
+-#if __BYTE_ORDER == __LITTLE_ENDIAN
+- #define le16toh(x) (x)
+-#else
+- #define le16toh(x) __bswap_16 (x)
+-#endif
++#include "byteorder.h"
+ 
+ static void buffer_update_read_index(struct Pieusb_Read_Buffer* buffer, int increment);
+ 
