3e69585fbb9e8483833d78490e39e36f2a5b750b
wiz <wiz@pkgsrc.org>
2015-11-11 11:56:21 +0000

    py-dulwich is python-3.x compatible now.

--- net/Radicale/options.mk
+++ net/Radicale/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2015/01/28 20:39:54 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2015/11/11 11:56:21 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.Radicale
 PKG_SUPPORTED_OPTIONS=	git ldap
@@ -6,7 +6,6 @@ PKG_SUPPORTED_OPTIONS=	git ldap
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgit)
-PYTHON_VERSIONS_INCOMPATIBLE=	33 34 # py-dulwich
 DEPENDS+=	${PYPKGPREFIX}-dulwich-[0-9]*:../../devel/py-dulwich
 .endif
 
