079b8cb0bc50a630107dbd6f66fd93793e6f9a26
msaitoh <msaitoh@pkgsrc.org>
2015-11-12 08:54:52 +0000

    Allow accessing the extended configuration space. Tested on amd64-current (support the extended configuration space) and i386 netbsd-7(not support).

---
+++ sysutils/pciutils/patches/patch-lib_nbsd_libpci.c
@@ -0,0 +1,11 @@
+--- lib/nbsd-libpci.c.orig	2011-01-08 06:04:28.000000000 +0900
++++ lib/nbsd-libpci.c	2015-11-12 17:45:08.000000000 +0900
+@@ -71,7 +71,7 @@ nbsd_read(struct pci_dev *d, int pos, by
+   if (!(len == 1 || len == 2 || len == 4))
+     return pci_generic_block_read(d, pos, buf, len);
+ 
+-  if (pos >= 256)
++  if (pos >= 4096)
+     return 0;
+ 
+   shift = 8*(pos % 4);
