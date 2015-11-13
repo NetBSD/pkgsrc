835010c68340f11b2a134f40ce2c766eb88779ed
wiz <wiz>
2015-11-13 14:55:48 +0000

    Remove OpenBSD chunk from patch.
    Not needed, tested by sevan@

--- devel/SDL2/patches/patch-src_cpuinfo_SDL_cpuinfo.c
+++ devel/SDL2/patches/patch-src_cpuinfo_SDL_cpuinfo.c
@@ -1,7 +1,6 @@
-$NetBSD: patch-src_cpuinfo_SDL_cpuinfo.c,v 1.3 2015/11/13 14:13:26 wiz Exp $
+$NetBSD: patch-src_cpuinfo_SDL_cpuinfo.c,v 1.4 2015/11/13 14:55:48 wiz Exp $
 
-Fix building on NetBSD and OpenBSD.
-NetBSD part submitted as
+Fix building on NetBSD.
 https://bugzilla.libsdl.org/show_bug.cgi?id=3176
 
 --- src/cpuinfo/SDL_cpuinfo.c.orig	2014-03-08 04:36:51.000000000 +0000
@@ -11,7 +10,7 @@ https://bugzilla.libsdl.org/show_bug.cgi?id=3176
  #ifdef HAVE_SYSCTLBYNAME
          if (SDL_SystemRAM <= 0) {
 -#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
-+#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__OpenBSD__)
++#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__)
  #ifdef HW_REALMEM
              int mib[2] = {CTL_HW, HW_REALMEM};
  #else
