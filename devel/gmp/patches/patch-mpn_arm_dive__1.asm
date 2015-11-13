9e8ac76b70f1660f7e473dd11448539f504c1f7f
wiz <wiz@pkgsrc.org>
2015-11-13 11:36:44 +0000

    Update gmp to 6.1.0:
    Changes between GMP version 6.0.* and 6.1.0
    
      BUGS FIXED
      * The public function mpn_com is now correctly declared in gmp.h.
    
      * Healed possible failures of mpn_sec_sqr for non-cryptographic sizes for
        some obsolete CPUs.
    
      * The option --disable-assembly now disables all inlined asm.
    
      * Fixed bug affecting mini-gmp's bitwise functions mpz_setbit, mpz_clrbit,
        and mpz_combit.
    
      * Various problems related to precision for mpf have been fixed.
    
      * Fixed ABI incompatible stack alignment in calls from assembly code.
    
      * Fixed PIC bug in popcount affecting Intel processors using the 32-bit ABI.
    
      SPEEDUPS
      * Speedup for Intel Broadwell and Skylake though assembly code making use of
        new ADX instructions.
    
      * Square root is now faster when the remainder is not needed. Also the speed
        to compute the k-th root improved, for small sizes.
    
      * Improved arm64 support.
    
      FEATURES
      * New C++ functions gcd and lcm for mpz_class.
    
      * New public mpn functions mpn_divexact_1, mpn_zero_p, and mpn_cnd_swap.
    
      * New public mpq_cmp_z function, to efficiently compare rationals with
        integers.
    
      * Support for Darwin in all x86 code, thereby enabling fat builds on Darwin.
    
      * Support for more 32-bit arm processors.
    
      * Support for compilation with clang/llvm on more platforms.  Caution: GMP
        triggers mis-compilation bugs in clang for many platforms, such as arm, x86
        (32-bit and 64-bit), powerpc, mips.
    
      * Support for AVX-less modern x86 CPUs. (Such support might be missing either
        because the CPU vendor chose to disable AVX, or because the running kernel
        lacks AVX context switch support.)
    
      * Stack usage trimmed; we believe 512 KiB is now sufficient for any GMP
        call, irrespective of operand size.
    
      * Support for NetBSD under Xen; we switch off AVX unconditionally under
        NetBSD since a bug in NetBSD makes AVX fail under Xen.
    
      MISC
      * We now use manufacturers' code names for x86 CPUs, e.g., "haswell" instead
        of names derived from the commercial brands.
    
      * Small improvements and better coverage for the test suite.
    
      * The various FreeBSD problems listed for 6.0.0 affect this release too.
    
      * Tuned values for FFT multiplications are provided for larger number on
        many platforms.

--- devel/gmp/patches/patch-mpn_arm_dive__1.asm
+++ devel/gmp/patches/patch-mpn_arm_dive__1.asm
@@ -1,6 +1,6 @@
-$NetBSD: patch-mpn_arm_dive__1.asm,v 1.1 2015/11/13 10:42:23 wiz Exp $
+$NetBSD: patch-mpn_arm_dive__1.asm,v 1.2 2015/11/13 11:36:44 wiz Exp $
 
---- mpn/arm/dive_1.asm.orig	2014-03-25 14:37:55.000000000 +0000
+--- mpn/arm/dive_1.asm.orig	2015-11-01 15:19:49.000000000 +0000
 +++ mpn/arm/dive_1.asm
 @@ -112,7 +112,10 @@ L(end):	sbc	cy, r5, cy
  	mul	r9, r4, cy
@@ -25,4 +25,4 @@ $NetBSD: patch-mpn_arm_dive__1.asm,v 1.1 2015/11/13 10:42:23 wiz Exp $
 +')
  EPILOGUE()
  
- 	.section .rodata
+ 	RODATA
--- devel/gmp/patches/patch-mpn_arm_invert__limb.asm
+++ devel/gmp/patches/patch-mpn_arm_invert__limb.asm
@@ -1,6 +1,6 @@
-$NetBSD: patch-mpn_arm_invert__limb.asm,v 1.5 2015/11/13 10:42:23 wiz Exp $
+$NetBSD: patch-mpn_arm_invert__limb.asm,v 1.6 2015/11/13 11:36:44 wiz Exp $
 
---- mpn/arm/invert_limb.asm.orig	2014-03-25 14:37:55.000000000 +0000
+--- mpn/arm/invert_limb.asm.orig	2015-11-01 15:19:49.000000000 +0000
 +++ mpn/arm/invert_limb.asm
 @@ -52,7 +52,10 @@ PROLOGUE(mpn_invert_limb)
  	adds	r1, r12, r0
@@ -13,4 +13,4 @@ $NetBSD: patch-mpn_arm_invert__limb.asm,v 1.5 2015/11/13 10:42:23 wiz Exp $
 +')
  EPILOGUE()
  
- 	.section .rodata
+ 	RODATA
