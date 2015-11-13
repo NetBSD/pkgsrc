abf9a1aab84f35e91192235408811f987316c34a
wiz <wiz@pkgsrc.org>
2015-11-13 14:18:23 +0000

    Add upstream bug report URL.

--- devel/SDL2/patches/patch-src_audio_bsd_SDL__bsdaudio.c
+++ devel/SDL2/patches/patch-src_audio_bsd_SDL__bsdaudio.c
@@ -1,6 +1,8 @@
-$NetBSD: patch-src_audio_bsd_SDL__bsdaudio.c,v 1.1 2015/05/31 09:20:48 nat Exp $
+$NetBSD: patch-src_audio_bsd_SDL__bsdaudio.c,v 1.2 2015/11/13 14:18:23 wiz Exp $
 
 # Remove delay as there is already an inherent delay in writing audio.
+https://bugzilla.libsdl.org/show_bug.cgi?id=3177
+
 --- src/audio/bsd/SDL_bsdaudio.c.orig	2014-03-16 02:31:44.000000000 +0000
 +++ src/audio/bsd/SDL_bsdaudio.c
 @@ -181,11 +181,6 @@ BSDAUDIO_PlayDevice(_THIS)
