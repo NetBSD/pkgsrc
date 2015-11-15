cda4d143c7efcd54dfea6a010294e5456e8e65ef
szptvlfn <szptvlfn@pkgsrc.org>
2015-11-15 08:22:00 +0000

    Update to 0.1.5.1
    Changes from https://github.com/ygale/timezone-series
    0.1.5
    - Bump time to 1.5.

--- time/hs-timezone-series/buildlink3.mk
+++ time/hs-timezone-series/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/11/24 21:22:37 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/11/15 08:22:00 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-timezone-series
 
 .if !defined(HS_TIMEZONE_SERIES_BUILDLINK3_MK)
 HS_TIMEZONE_SERIES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-timezone-series+=	hs-timezone-series>=0.1.3
-BUILDLINK_ABI_DEPENDS.hs-timezone-series+=	hs-timezone-series>=0.1.3
+BUILDLINK_API_DEPENDS.hs-timezone-series+=	hs-timezone-series>=0.1.5
+BUILDLINK_ABI_DEPENDS.hs-timezone-series+=	hs-timezone-series>=0.1.5.1
 BUILDLINK_PKGSRCDIR.hs-timezone-series?=	../../time/hs-timezone-series
 .endif	# HS_TIMEZONE_SERIES_BUILDLINK3_MK
 
