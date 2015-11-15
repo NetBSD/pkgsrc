2cb2b8915ab8fbff2b5ceb8c6fbdb54b0eacac71
szptvlfn <szptvlfn>
2015-11-15 08:22:15 +0000

    Update to 0.1.7
    Changes from https://github.com/ygale/timezone-olson
    0.1.7
    - Bump time to 1.5.
    0.1.6
    - Update link in package description.
    - Update comment to indicate support for version 3 format.

--- time/hs-timezone-olson/buildlink3.mk
+++ time/hs-timezone-olson/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/11/24 21:24:01 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/11/15 08:22:15 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-timezone-olson
 
 .if !defined(HS_TIMEZONE_OLSON_BUILDLINK3_MK)
 HS_TIMEZONE_OLSON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-timezone-olson+=	hs-timezone-olson>=0.1.4
-BUILDLINK_ABI_DEPENDS.hs-timezone-olson+=	hs-timezone-olson>=0.1.4
+BUILDLINK_API_DEPENDS.hs-timezone-olson+=	hs-timezone-olson>=0.1.7
+BUILDLINK_ABI_DEPENDS.hs-timezone-olson+=	hs-timezone-olson>=0.1.7
 BUILDLINK_PKGSRCDIR.hs-timezone-olson?=	../../time/hs-timezone-olson
 
 .include "../../devel/hs-extensible-exceptions/buildlink3.mk"
