83448be8881d741ff37bf7b47a90faf24e3b7f3a
szptvlfn <szptvlfn@pkgsrc.org>
2015-11-18 21:31:14 +0000

    Update to 1.17.0
    CHANGELOG:
    1.16.0.1 --> 1.17.0
    ===================
    
    AST changes:
    
    * Replaced VarA with AppA in Asst (#168).
    * Promoted list/tuple members changed from Promoted to Type (#162).
    * Update PatBind, Match, and Alt from containing Binds to Maybe Binds to
      distinguish between empty where clauses and where clauses with no
      binds (#244).
    * Add RoleAnnotDecl and Role to support Role annotations (#215).
    * Move NameSpace field from EVar/IVar to EAbs/IAbs.
    
    Other changes:
    
    * Add standalone parsers for ImportDecl.
    * Fix pretty-printer bugs for HaRP (#160).
    * Insert parentheses when pretty-printing non-atomic bang types (#169).
    * Un-reverse confusion of left and right arrow (#175).
    * Prettyprint option pragmas like 1.15 (#172).
    * Conditionally insert lines when pretty-printing declarations (#171).
    * Distinguish deriving (Show) from deriving Show (#189).
    * Allow parsing of unicode subscript and superscript functions (#173).
    * Pretty print unboxed tuples with spaces (#193).
    * Improve performance when parsing long extension lists (#200).
    * Properly pretty print constructor and class operators (#204).
    * Read Haskell source files as UTF-8 (#223).
    * Fix ExplicitNamespaces parsing (#216).
    * PolyKinds implies KindSignatures (#220).
    * Preserve location information for infix binds (#205).
    * Preserve positional information in checkPattern (#231).
    * Maintain correct line numbers when parsing multiline GHC_OPTIONS (#218).
    * Correctly parse "*" with TypeOperators (#81).
    * Export the "pretty" method (#222).
    * Add javascript calling convention for foreign imports (#236).
    * Add non-greedy parsers for module heads (#191).
    * Add a flag to disable arity checking when parsing (#260).
    * Parse "-" in type signatures (#206).
    * Add support for type wildcards and expression holes (#252).
    * Add support for Pattern Synonyms (#197).
    * Bump the happy lower bound (#250).
    * Make test suite pass on GHC 7.12 (#224).
    * Support linking Haddock comments to AST nodes (#213).
    * Parse multiline LANGUAGE pragmas (#217).
    * Parse trailing where (#25).
    * Parse modules starting with pragmas and indented
      "module" keyword (#122).
    * Use pretty-show to get human readable test outputs.
    * Respect fixity declarations inside where/let/class in
      `applyFixities` (#212).
    * Correctly parse the combination of view patterns and bang patterns. (#276)

--- devel/hs-haskell-src-exts/buildlink3.mk
+++ devel/hs-haskell-src-exts/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2015/08/02 11:37:31 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2015/11/18 21:31:14 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-haskell-src-exts
 
 .if !defined(HS_HASKELL_SRC_EXTS_BUILDLINK3_MK)
 HS_HASKELL_SRC_EXTS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.16.0
-BUILDLINK_ABI_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.16.0.1nb2
+BUILDLINK_API_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.17.0
+BUILDLINK_ABI_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.17.0
 BUILDLINK_PKGSRCDIR.hs-haskell-src-exts?=	../../devel/hs-haskell-src-exts
 
 .include "../../devel/hs-cpphs/buildlink3.mk"
