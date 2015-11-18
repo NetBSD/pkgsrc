f4f14b5f3cc4f15d63a9274b2ad551a2e0d035f4
nros <nros@pkgsrc.org>
2015-11-18 17:12:43 +0000

    Fix py-tortoisehg. tortoisehg had a change that made it overwrite config.py on install. due to this change behavior: patch setup-py instead of providing a config.py. Bump PKGREVISION.

--- devel/py-tortoisehg/patches/patch-setup.py
+++ devel/py-tortoisehg/patches/patch-setup.py
@@ -1,10 +1,12 @@
-$NetBSD: patch-setup.py,v 1.2 2015/08/14 18:00:56 wiz Exp $
+$NetBSD: patch-setup.py,v 1.3 2015/11/18 17:12:44 nros Exp $
 
+* in config.py: adjust license filename and add doc_path
+  to point at the local docs
 * nautilus-python is not available in pkgsrc so don't install
   the nautilus extension.
 * Install html documention, desktop file and license file.
 
---- setup.py.orig	2015-08-06 01:30:23.000000000 +0000
+--- setup.py.orig	2015-11-09 16:49:02.000000000 +0000
 +++ setup.py
 @@ -11,6 +11,7 @@ import sys
  import os
@@ -14,7 +16,20 @@ $NetBSD: patch-setup.py,v 1.2 2015/08/14 18:00:56 wiz Exp $
  import tempfile
  import re
  import tarfile
-@@ -569,8 +570,14 @@ def setup_posix():
+@@ -205,10 +206,11 @@ class build_config(Command):
+         data = {
+             'bin_path': installcmd.install_scripts[rootlen:],
+             'license_path': os.path.join(sharedir, 'doc', 'tortoisehg',
+-                                         'Copying.txt.gz'),
++                                         'COPYING.txt'),
+             'locale_path': os.path.join(sharedir, 'locale'),
+             'icon_path': os.path.join(sharedir, 'pixmaps', 'tortoisehg',
+                                       'icons'),
++            'doc_path' : os.path.join(sharedir, 'doc', 'tortoisehg'),
+             'nofork': True,
+             }
+         # Distributions will need to supply their own
+@@ -569,8 +571,14 @@ def setup_posix():
                              [os.path.join(root, f) for f in files]))
      _data_files.extend((os.path.join('share', modir), [mofile])
                         for pofile, modir, mofile in _walklocales())
