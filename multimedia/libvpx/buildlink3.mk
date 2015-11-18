ec3f114df8ca1b0415258f61f2afc6abe129ecc4
ryoon <ryoon@pkgsrc.org>
2015-11-18 14:09:25 +0000

    Update to 1.5.0
    * Fix HOMEPAGE
    
    Changelog:
    2015-11-09 v1.5.0 "Javan Whistling Duck"
      This release improves upon the VP9 encoder and speeds up the encoding and
      decoding processes.
    
      - Upgrading:
        This release is ABI incompatible with 1.4.0. It drops deprecated VP8
        controls and adds a variety of VP9 controls for testing.
    
        The vpxenc utility now prefers VP9 by default.
    
      - Enhancements:
        Faster VP9 encoding and decoding
        Smaller library size by combining functions used by VP8 and VP9
    
      - Bug Fixes:
        A variety of fuzzing issues

--- multimedia/libvpx/buildlink3.mk
+++ multimedia/libvpx/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2015/06/30 09:53:17 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2015/11/18 14:09:25 ryoon Exp $
 
 BUILDLINK_TREE+=	libvpx
 
 .if !defined(LIBVPX_BUILDLINK3_MK)
 LIBVPX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libvpx+=	libvpx>=1.4.0
-BUILDLINK_ABI_DEPENDS.libvpx+=	libvpx>=1.4.0
+BUILDLINK_API_DEPENDS.libvpx+=	libvpx>=1.5.0
+BUILDLINK_ABI_DEPENDS.libvpx+=	libvpx>=1.5.0
 BUILDLINK_PKGSRCDIR.libvpx?=	../../multimedia/libvpx
 
 .include "../../mk/pthread.buildlink3.mk"
