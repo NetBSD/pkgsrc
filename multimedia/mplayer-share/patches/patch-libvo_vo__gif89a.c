6fc96200c52510ad934de81fd5e6c14994bc00f6
leot <leot@pkgsrc.org>
2015-11-21 09:47:23 +0000

    Update gmplayer, mencoder, mplayer and mplayer-share to 1.2.
    pkgsrc changes:
     * Various misc fixes (thanks to pkglint)
     * Do not pass --with-dvdread-config and --with-dvdnav-config, the
       dvdread-config and dvdnav-config seems that no longer exist.
     * Avoid to pass --enable-libvorbis otherwise libvorbis can not be recognised by
       the mplayer configure script.
     * (re)Use patches from multimedia/ffmpeg2 instead of using mplayer-specific
       ones
    
    Changes:
    1.2: "FrameCounter"
      Decoders:
      * Hardware accelerated decoding on OS X via VDA.
      * Internal libmpeg2 disabled by default, on the assumption
        that nobody really needs it anymore.
    
      Other:
      * "run" slave/keybinding command now supports expanding properties
      * better support for > 8 bit formats in -vo gl
      * support for XYZ color space in -vo gl
      * -vo gl now supports OSD (not EOSD though) with GLES backend.
      * GLES backend now uses GLES v2 with YUV support via shaders
      * -vo gl supports rotated display without needing video filters via
        -monitor-orientation command-line option.
      * -vo gl now has an OSX/Cocoa backend, so it should be a better
        alternative to -vo corevideo now.
      * -vo gl2 renamed to -vo gl_tiled to stop people from assuming it is
        better than -vo gl (it is in many ways worse).
      * Fixes for DVB, teletext and closed-caption based subtitles.
      * Support teletext and CC subtitles in WTV.
      * Support binding keys corresponding to non-ASCII characters.
      * Limited -endpos support for -dump*
      * Danish messages header file renamed from help_mp-dk.h to help_mp-da.h
        in order to comply with ISO 639 language code (configure language
        options will now require da instead of dk)
      * configure: options --enable-gtk1, --with-glib-config= and
        --with-gtk-config= removed
      * Change ID3 genre name for genre ID 67 from "Psychadelic"
        to "Psychedelic"
      * support for audio bin/cue images
    
      Ports:
      * Windows: support file names as UTF-8 in slave mode and passing
        file names as wchar command line arguments.
      * Android: MPlayer can be run from adb shell and display videos
        via OpenGL ES.
        Note that just a bit of seeking etc. would cause reproducible
        system reboots after just a few seconds on my Galaxy S2.
      * Android: -wid is supported. This should allow using MPlayer in slave
        mode, a native window pointer needs to be passed as -wid. Untested.
    
      GUI:
      * Support for TV/DVB
      * Various improvements of the context menu and the preferences dialog
      * Revision of the user interface texts
      * Console message with information on deprecated (but still supported)
        entries in the skin configuration file
      * New symbol character (r) and new dynamic label variables ($D, $U, $P)
      * New items (pimage, rpotmeter)
      * Updated skins (avifile, Blue, Blue-small, Clearlooks, Corelian,
        disappearer, mentalic, mplayer_red, nativeRed, neutron, phony, plastic,
        slim, softgrip, standard, trium, tvisor, xanim)
      * Video can be rotated
      * New GUI message: evSetRotation
      * GUI now officially needs GTK+ and GLib versions 2 (it hadn't compiled
        with versions 1 for quite some time)
      * Support for audio and video bin/cue image playback
      * Support for Audio CD / (Super) Video CD / DVD image and DVD copy playback
        through the respective scheme (cd://, vcd://, dvd://)
      * Support for arbitrary non-binary data file cue sheets (i.e. cue sheets
        describing playlists for data files containing multiple titles)
    
      FFmpeg:
      * Version 2.8 with local patches
    
    Thanks to wiz@ for misc private discussion.

--- multimedia/mplayer-share/patches/patch-libvo_vo__gif89a.c
+++ multimedia/mplayer-share/patches/patch-libvo_vo__gif89a.c
@@ -1,27 +1,18 @@
-$NetBSD: patch-libvo_vo__gif89a.c,v 1.2 2014/05/16 09:49:06 wiz Exp $
+$NetBSD: patch-libvo_vo__gif89a.c,v 1.3 2015/11/21 09:47:23 leot Exp $
 
 * Fix build with giflib 5.1.
 
---- libvo/vo_gif89a.c.orig	2011-05-07 10:59:11.000000000 +0000
+--- libvo/vo_gif89a.c.orig	2014-10-05 02:39:08.000000000 +0000
 +++ libvo/vo_gif89a.c
 @@ -44,6 +44,7 @@
   * entire argument being interpretted as the filename.
   */
  
 +#include <stddef.h>
- #include <gif_lib.h>
- 
  #include <stdio.h>
-@@ -196,7 +197,7 @@ static int config(uint32_t s_width, uint
- 	mp_msg(MSGT_VO, MSGL_ERR, "GIF89a: Some functionality has been disabled.\n");
- #endif
- 
--	new_gif = EGifOpenFileName(gif_filename, 0);
-+	new_gif = EGifOpenFileName(gif_filename, 0, NULL);
- 	if (new_gif == NULL) {
- 		mp_msg(MSGT_VO, MSGL_ERR, "GIF89a: error opening file \"%s\" for output.\n", gif_filename);
- 		return 1;
-@@ -215,7 +216,7 @@ static int config(uint32_t s_width, uint
+ #include <stdlib.h>
+ #include <string.h>
+@@ -227,7 +228,7 @@ static int config(uint32_t s_width, uint
  		return 1;
  	}
  
@@ -30,7 +21,7 @@ $NetBSD: patch-libvo_vo__gif89a.c,v 1.2 2014/05/16 09:49:06 wiz Exp $
  	if (reduce_cmap == NULL) {
  		free(slice_data); slice_data = NULL;
  		free(reduce_data); reduce_data = NULL;
-@@ -267,7 +268,7 @@ static int gif_reduce(int width, int hei
+@@ -284,7 +285,7 @@ static int gif_reduce(int width, int hei
  	}
  
  	R = Ra; G = Ga; B = Ba;
@@ -39,15 +30,7 @@ $NetBSD: patch-libvo_vo__gif89a.c,v 1.2 2014/05/16 09:49:06 wiz Exp $
  }
  
  static void flip_page(void)
-@@ -358,14 +359,14 @@ static void uninit(void)
- 			MPLAYER_VERSION, VO_GIF_REVISION,
- 			"joey@nicewarrior.org");
- 		EGifPutComment(new_gif, temp);
--		EGifCloseFile(new_gif); // also frees gif storage space.
-+		EGifCloseFile(new_gif, NULL); // also frees gif storage space.
- 	}
- 
- 	// free our allocated ram
+@@ -377,7 +378,7 @@ static void uninit(void)
  	free(gif_filename);
  	free(slice_data);
  	free(reduce_data);
