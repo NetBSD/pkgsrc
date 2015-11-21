1f03429d7ee88c5a2bb530348c949d10861a27b0
ryoon <ryoon@pkgsrc.org>
2015-11-21 09:32:07 +0000

    Update to 3.1.2
    Changelog:
    3.1.2 (2015-11-14)
    
        Defect #20992: Parent priority "Independent of subtasks" setting doesn't work
        Defect #20360: Project copy does not copy custom field settings
        Defect #20380: Cannot assign users to projects with IE set to compatibility mode
        Defect #20591: PDF export does not determine picture (.png) height correctly
        Defect #20677: Custom fields with multiple values required by worklow can be blank
        Defect #20811: long pre lines are missing from PDF export of wiki pages
        Defect #21136: Issues API may disclose changeset messages that are not visible
        Defect #21150: Time logging form may disclose subjects of issues that are not visible
        Defect #21155: Deleting invalid wiki page version deletes whole page content
        Defect #20282: Error message when editing a child project without add project/subprojects permissions
        Defect #20730: Fix tokenization of phrases with non-ascii chars
        Defect #21071: find_referenced_issue_by_id fails with RangeError for large numbers
        Patch #21031: Polish translation update for 3.0-stable
        Patch #21105: Japanese wiki_syntax_detailed_textile.html translation for 3.0-stable
        Patch #20785: Polish translation update for 3.1-stable
        Patch #20837: Bulgarian translation
        Patch #20892: Spanish translation for r14637
        Patch #20906: Fix mulitple tab navigation highlighting and content hiding
        Patch #21019: Traditional Chinese translation (to r14689)
        Patch #21076: Move inline CSS to application.css for private checkbox
        Patch #21085: Optimize issue edit description link

--- devel/ruby-redmine/files/redmine_fix_gems.sh
+++ devel/ruby-redmine/files/redmine_fix_gems.sh
@@ -8,11 +8,11 @@ export GEM_HOME GEM_PATH PATH
 
 cd @PREFIX@/share/@RUBY_PKGPREFIX@-redmine/app
 
-@GEM@ pristine nokogiri --version 1.6.6.2 -- --use-system-libraries --with-xml2-config=@PREFIX@/bin/xml2-config --with-xslt-config=@PREFIX@/bin/xslt-config
+@GEM@ pristine nokogiri --version 1.6.6.4 -- --use-system-libraries --with-xml2-config=@PREFIX@/bin/xml2-config --with-xslt-config=@PREFIX@/bin/xslt-config
 @GEM@ pristine json --version 1.8.3
 @GEM@ pristine kgio --version 2.10.0
 @GEM@ pristine mysql2 --version 0.3.20
 @GEM@ pristine raindrops --version 0.15.0
 @GEM@ pristine redcarpet --version 3.3.3
 @GEM@ pristine rmagick --version 2.15.4
-@GEM@ pristine unicorn --version 4.9.0
+@GEM@ pristine unicorn --version 5.0.1
