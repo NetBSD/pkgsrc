4aee81fc8b3520de956478cf8bb1d3eacd7d5902
wiz <wiz@pkgsrc.org>
2015-11-23 10:30:15 +0000

    Bump PKGREVISION for poppler shlib major bump.

--- print/poppler-includes/buildlink3.mk
+++ print/poppler-includes/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2015/10/01 05:36:36 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2015/11/23 10:30:15 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-includes
 
@@ -8,7 +8,7 @@ POPPLER_INCLUDES_BUILDLINK3_MK:=
 BUILDLINK_DEPMETHOD.poppler-includes?=	build
 
 BUILDLINK_API_DEPENDS.poppler-includes+=	poppler-includes>=0.22.2
-BUILDLINK_ABI_DEPENDS.poppler-includes?=	poppler-includes>=0.36.0
+BUILDLINK_ABI_DEPENDS.poppler-includes?=	poppler-includes>=0.38.0
 BUILDLINK_PKGSRCDIR.poppler-includes?=	../../print/poppler-includes
 .endif # POPPLER_INCLUDES_BUILDLINK3_MK
 
