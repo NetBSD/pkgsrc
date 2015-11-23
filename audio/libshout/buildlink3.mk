af0c950f41aabec199af73e2a5d5df57741e1b6c
wiz <wiz@pkgsrc.org>
2015-11-23 09:51:48 +0000

    Update libshout to 2.4.1.
    Set LICENSE. Fix installed pkg-config file. Remove unnecessary includes
    from bl3.mk. Enable speex support.
    
    libshout 2.4.1 (20151120)
    
    * Fixed issue with missing file in distribution
    
    libshout 2.4.0 (20151111)
    
    * Audio only WebM support.
    * Protocol level meta data support improved.
      Some API calls got replaced and marked as obsolete.
    * Code hardened.
    * Fixed overlinking.
    * Removed Debian packaging.
    * TLS support (RFC2818 and RFC2817, mode can be autodetected).
    * Improved HTTP protocol:
      * Set Host:-header (vhosting),
      * Check for server capabilities.
    * Basic support for RoarAudio protocol.

--- audio/libshout/buildlink3.mk
+++ audio/libshout/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:23:56 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2015/11/23 09:51:48 wiz Exp $
 
 BUILDLINK_TREE+=	libshout
 
@@ -6,11 +6,9 @@ BUILDLINK_TREE+=	libshout
 LIBSHOUT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libshout+=	libshout>=2.0
-BUILDLINK_ABI_DEPENDS.libshout+=libshout>=2.0nb1
+BUILDLINK_ABI_DEPENDS.libshout+=	libshout>=2.0nb1
 BUILDLINK_PKGSRCDIR.libshout?=	../../audio/libshout
 
-.include "../../audio/libvorbis/buildlink3.mk"
-.include "../../multimedia/libtheora/buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 .endif # LIBSHOUT_BUILDLINK3_MK
 
