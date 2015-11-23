e2ecce00a8f752e4c1b538443564e7135f174daf
szptvlfn <szptvlfn@pkgsrc.org>
2015-11-23 12:54:53 +0000

    revert previous commit, sorry.

--- textproc/libcroco/buildlink3.mk
+++ textproc/libcroco/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.28 2015/11/23 12:37:46 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.29 2015/11/23 12:54:53 szptvlfn Exp $
 
 BUILDLINK_TREE+=	libcroco
 
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.libcroco+=	libcroco>=0.6.0
 BUILDLINK_ABI_DEPENDS.libcroco+=	libcroco>=0.6.5nb2
 BUILDLINK_PKGSRCDIR.libcroco?=	../../textproc/libcroco
 
-.include "../../archivers/lzmalib/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .endif # LIBCROCO_BUILDLINK3_MK
