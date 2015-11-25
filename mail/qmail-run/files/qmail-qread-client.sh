9dd839146bbc869c3ecf09c738b4286a9ce4747c
jperkin <jperkin>
2015-11-25 12:51:30 +0000

    Remove mk/find-prefix.mk usage from the mail category.
    The find-prefix infrastructure was required in a pkgviews world where
    packages installed from pkgsrc could have different installation
    prefixes, and this was a way for a dependency prefix to be determined.
    
    Now that pkgviews has been removed there is no longer any need for the
    overhead of this infrastructure.  Instead we use BUILDLINK_PREFIX.pkg
    for dependencies pulled in via buildlink, or LOCALBASE/PREFIX where the
    dependency is coming from pkgsrc.
    
    Provides a reasonable performance win due to the reduction of `pkg_info
    -qp` calls, some of which were redundant anyway as they were duplicating
    the same information provided by BUILDLINK_PREFIX.pkg.

--- mail/qmail-run/files/qmail-qread-client.sh
+++ mail/qmail-run/files/qmail-qread-client.sh
@@ -1,10 +1,10 @@
 #!@SH@
 #
-# $NetBSD: qmail-qread-client.sh,v 1.3 2014/12/06 22:14:27 schmonz Exp $
+# $NetBSD: qmail-qread-client.sh,v 1.4 2015/11/25 12:51:30 jperkin Exp $
 #
 # @PKGNAME@ client program to allow local non-root users to see
 # the queue. Requires that the corresponding server program be running.
 # From Steinar Haug.
 #
 
-exec @UCSPI_TCP_PREFIX@/bin/tcpclient -RHl0 -- 127.0.0.1 20025 @SH@ -c 'exec @CAT@ <&6'
+exec @PREFIX@/bin/tcpclient -RHl0 -- 127.0.0.1 20025 @SH@ -c 'exec @CAT@ <&6'
