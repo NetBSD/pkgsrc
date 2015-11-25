ce0a8db11cb3371e3f19784cfdf2fba5015c7e60
jperkin <jperkin@pkgsrc.org>
2015-11-25 12:50:57 +0000

    Remove mk/find-prefix.mk usage from the inputmethod category.
    The find-prefix infrastructure was required in a pkgviews world where
    packages installed from pkgsrc could have different installation
    prefixes, and this was a way for a dependency prefix to be determined.
    
    Now that pkgviews has been removed there is no longer any need for the
    overhead of this infrastructure.  Instead we use BUILDLINK_PREFIX.pkg
    for dependencies pulled in via buildlink, or LOCALBASE/PREFIX where the
    dependency is coming from pkgsrc.
    
    Provides a reasonable performance win due to the reduction of `pkg_info
    -qp` calls, some of which were redundant anyway as they were duplicating
    the same information provided by BUILDLINK_PREFIX.pkg.

--- inputmethod/multiskkserv/files/multiskkserv.sh
+++ inputmethod/multiskkserv/files/multiskkserv.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: multiskkserv.sh,v 1.1.1.1 2010/09/18 11:04:17 obache Exp $
+# $NetBSD: multiskkserv.sh,v 1.2 2015/11/25 12:50:58 jperkin Exp $
 #
 # PROVIDE: multiskkserv
 # REQUIRE: DAEMON
@@ -11,7 +11,7 @@ name="multiskkserv"
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
 
-multiskkserv_flags="@SKKJISYOCDBDIR@/share/skk/SKK-JISYO.L.cdb &"
+multiskkserv_flags="@PREFIX@/share/skk/SKK-JISYO.L.cdb &"
 
 load_rc_config $name
 run_rc_command "$1"
