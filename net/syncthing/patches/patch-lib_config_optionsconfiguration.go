901a73e2bc5c359bcc35bd2a6c62a569bafa5057
gdt <gdt@pkgsrc.org>
2015-11-26 00:18:56 +0000

    Add patch comment

--- net/syncthing/patches/patch-lib_config_optionsconfiguration.go
+++ net/syncthing/patches/patch-lib_config_optionsconfiguration.go
@@ -1,4 +1,11 @@
-$NetBSD: patch-lib_config_optionsconfiguration.go,v 1.1 2015/11/25 23:36:33 gdt Exp $
+$NetBSD: patch-lib_config_optionsconfiguration.go,v 1.2 2015/11/26 00:18:56 gdt Exp $
+
+By default, disable automatic upgrade checking, because it is a
+privacy leak, of the sort that would earn a warning on f-droid a la:
+  https://f-droid.org/wiki/page/Antifeature:Tracking
+
+Not reported upstream because they appear to think upgrade
+checks/web-bugs are a feature.
 
 --- lib/config/optionsconfiguration.go.orig	2015-11-22 15:05:20.000000000 +0000
 +++ lib/config/optionsconfiguration.go
