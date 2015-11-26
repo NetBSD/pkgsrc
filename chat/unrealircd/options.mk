8bb53518789846171fc2a6a3ff8a780cf91039c3
jperkin <jperkin@pkgsrc.org>
2015-11-26 16:36:00 +0000

    Update chat/unrealircd to 3.2.10.5.
    pkgsrc changes:
    
     - Remove obsolete and broken MESSAGE files.
     - Find zlib correctly when enabled.
     - Add SMF manifest.
     - Update patch files and add comments where necessary.
    
    Upstream changes:
    
     - This release fixes a SASL Denial of Service issue
    
    Patch provided by Marco Wessel in joyent/pkgsrc#306.

--- chat/unrealircd/options.mk
+++ chat/unrealircd/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.15 2015/11/07 22:52:04 dholland Exp $
+# $NetBSD: options.mk,v 1.16 2015/11/26 16:36:00 jperkin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.unrealircd
 
@@ -17,7 +17,6 @@ PKG_SUGGESTED_OPTIONS=	unrealircd-showlistmodes unrealircd-prefixaq
 ###
 .if !empty(PKG_OPTIONS:Minet6)
 CONFIGURE_ARGS+=	--enable-inet6
-MESSAGE_SRC+=		MESSAGE_SRC.inet6
 .else
 CONFIGURE_ARGS+=	--disable-inet6
 CONFIGURE_ENV+=		ac_cv_ip6=no
@@ -43,7 +42,7 @@ CONFIGURE_ARGS+=	--enable-nospoof
 ### server <-> server with zlib.
 ###
 .if !empty(PKG_OPTIONS:Munrealircd-ziplinks)
-CONFIGURE_ARGS+=		--enable-ziplinks
+CONFIGURE_ARGS+=		--enable-ziplinks=${BUILDLINK_PREFIX.zlib}
 .	include "../../devel/zlib/buildlink3.mk"
 .endif
 
