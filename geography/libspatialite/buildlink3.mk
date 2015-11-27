97c976fe1d4ba33ebbdb9d4caaab4170849a54df
adam <adam@pkgsrc.org>
2015-11-27 14:03:33 +0000

    SpatiaLite is an open source library intended to extend the SQLite core to support fully fledged Spatial SQL capabilities. SQLite is intrinsically simple and lightweight: * a single lightweight library implementing the full SQL engine * standard SQL implementation: almost complete SQL-92 * no complex client/server architecture * a whole database simply corresponds to a single monolithic file (no size   limits) * any DB-file can be safely exchanged across different platforms, because the   internal architecture is universally portable * no installation, no configuration SpatiaLite is smoothly integrated into SQLite to provide a complete and powerful Spatial DBMS (mostly OGC-SFS compliant). Using SQLite + SpatiaLite you can effectively deploy an alternative open source Spatial DBMS roughly equivalent to PostgreSQL + PostGIS.

---
+++ geography/libspatialite/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/11/27 14:03:33 adam Exp $
+
+BUILDLINK_TREE+=	libspatialite
+
+.if !defined(LIBSPATIALITE_BUILDLINK3_MK)
+LIBSPATIALITE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libspatialite+=	libspatialite>=4.3.0a
+BUILDLINK_PKGSRCDIR.libspatialite?=	../../geography/libspatialite
+
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../databases/sqlite3/buildlink3.mk"
+.include "../../geography/geos/buildlink3.mk"
+.include "../../geography/proj/buildlink3.mk"
+.include "../../textproc/freexl/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.endif	# LIBSPATIALITE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libspatialite
