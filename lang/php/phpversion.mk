5f3463070dbe239730bc5587725894bf06de1578
taca <taca@pkgsrc.org>
2015-11-28 07:09:38 +0000

    Update php56 to 5.6.16 (PHP 5.6.16).
    26 Nov 2015, PHP 5.6.16
    
    - Core:
      . Fixed bug #70828 (php-fpm 5.6 with opcache crashes when referencing a
        non-existent constant). (Laruence)
      . Fixed bug #70748 (Segfault in ini_lex () at Zend/zend_ini_scanner.l).
        (Laruence)
    
    - Mysqlnd:
      . Fixed bug #68344 (MySQLi does not provide way to disable peer certificate
        validation) by introducing MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT
            connection flag. (Andrey)
    
    - OCI8:
      . Fixed bug #68298 (OCI int overflow). (Senthil)
    
    - PDO_DBlib:
      . Fixed bug #69757 (Segmentation fault on nextRowset).
        (miracle at rpz dot name)
    
    - SOAP:
      . Fixed bug #70875 (Segmentation fault if wsdl has no targetNamespace
        attribute). (Matteo)
    
    - SPL:
      . Fixed bug #70852 (Segfault getting NULL offset of an ArrayObject).
        (Reeze Xia)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.115 2015/10/31 01:58:37 taca Exp $
+# $NetBSD: phpversion.mk,v 1.116 2015/11/28 07:09:38 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -83,7 +83,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP54_VERSION=	5.4.45
 PHP55_VERSION=	5.5.30
-PHP56_VERSION=	5.6.15
+PHP56_VERSION=	5.6.16
 
 # Define initial release of major version.
 PHP54_RELDATE=	20120301
