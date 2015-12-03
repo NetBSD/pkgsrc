b336e41c075f290031853588f3a6985b03819336
ryoon <ryoon@pkgsrc.org>
2015-12-03 12:22:27 +0000

    Update to 2.39
    Changelog:
    Not available.
    Gecko 42.0 based.

--- www/seamonkey/patches/patch-mozilla_gfx_skia_generate__mozbuild.py
+++ www/seamonkey/patches/patch-mozilla_gfx_skia_generate__mozbuild.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-mozilla_gfx_skia_generate__mozbuild.py,v 1.2 2015/10/02 22:49:36 ryoon Exp $
+$NetBSD: patch-mozilla_gfx_skia_generate__mozbuild.py,v 1.3 2015/12/03 12:22:27 ryoon Exp $
 
---- mozilla/gfx/skia/generate_mozbuild.py.orig	2015-09-25 07:36:46.000000000 +0000
+--- mozilla/gfx/skia/generate_mozbuild.py.orig	2015-11-04 01:55:27.000000000 +0000
 +++ mozilla/gfx/skia/generate_mozbuild.py
-@@ -144,6 +144,12 @@ if CONFIG['GNU_CXX']:
+@@ -147,6 +147,12 @@ if CONFIG['GNU_CXX']:
      if CONFIG['CPU_ARCH'] == 'arm':
-         SOURCES['trunk/src/opts/SkBlitRow_opts_arm.cpp'].flags += ['-fomit-frame-pointer']
+         SOURCES['skia/src/opts/SkBlitRow_opts_arm.cpp'].flags += ['-fomit-frame-pointer']
  
 +if CONFIG['MOZ_NATIVE_HARFBUZZ']:
 +    CXXFLAGS += CONFIG['MOZ_HARFBUZZ_CFLAGS']
