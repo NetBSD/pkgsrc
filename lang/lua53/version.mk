225552cb24fbd296ce0313f28488c1ac2f17d1a2
alnsn <alnsn@pkgsrc.org>
2015-12-05 22:50:26 +0000

    Update Lua to version 5.3.2 and link with pthreads to fix pkg/47587.
    Lua 5.3.2 fixes all bugs listed in
            http://www.lua.org/bugs.html#5.3.1 [1]
    
    Lua 5.3.2 also contains several internal improvements and includes a
    revised reference manual:
    	http://www.lua.org/manual/5.3/
    
    [1]
        1. io.lines does not check maximum number of options.
           reported by Patrick Donnell on 10 Jul 2015. existed since 3.0.
           Example:
    -- can crash in some machines
    t ={}; for i = 1, 253 do t[i] = 1 end
    io.lines("someexistingfile", table.unpack(t))()

--- lang/lua53/version.mk
+++ lang/lua53/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.2 2015/06/20 18:22:56 alnsn Exp $
+# $NetBSD: version.mk,v 1.3 2015/12/05 22:50:26 alnsn Exp $
 
-LUA_VERSION=	5.3.1
+LUA_VERSION=	5.3.2
 
 LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
 LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
