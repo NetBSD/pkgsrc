17e4942e9c81f06253bead30f6dcd3773281f6ee
rillig <rillig@pkgsrc.org>
2015-12-05 15:16:29 +0000

    Updated pkglint to 5.2.1.
    Changes since 5.2:
    * Fixed wrong warning about patches/CVS being an unrecorded patch
    * pkgsrc-wip also needs NetBSD as RCS Id, instead of Id
    * Code cleanup: grouped the checklineMk functions into one file

--- pkgtools/pkglint/files/deprecated_test.go
+++ pkgtools/pkglint/files/deprecated_test.go
@@ -7,8 +7,8 @@ import (
 func (s *Suite) TestDeprecated(c *check.C) {
 	G.globalData.deprecated = getDeprecatedVars()
 
-	line := NewLine("Makefile", "5", "dummy", nil)
-	checklineMkVarassign(line, "USE_PERL5", "=", "yes", "")
+	line := NewLine("Makefile", "5", "USE_PERL5=\tyes", nil)
+	NewMkLine(line).checkVarassign()
 
 	c.Check(s.Output(), equals, "WARN: Makefile:5: Definition of USE_PERL5 is deprecated. Use USE_TOOLS+=perl or USE_TOOLS+=perl:run instead.\n")
 }
