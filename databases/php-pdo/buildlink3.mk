456275082d49bca0801108c3b4b079fc162afdf8
taca <taca@pkgsrc.org>
2015-12-06 14:10:22 +0000

    Do not hard code the major version of PHP.

--- databases/php-pdo/buildlink3.mk
+++ databases/php-pdo/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.12 2012/05/07 01:53:18 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2015/12/06 14:10:22 taca Exp $
 
 BUILDLINK_TREE+=	php-pdo
 
 .if !defined(PHP_PDO_BUILDLINK3_MK)
 PHP_PDO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.php-pdo+=	${PHP_PKG_PREFIX}-pdo-5.*
+BUILDLINK_API_DEPENDS.php-pdo+=	${PHP_PKG_PREFIX}-pdo-${PKG_PHP_MAJOR_VERS}.*
 BUILDLINK_ABI_DEPENDS.php-pdo+=	${PHP_PKG_PREFIX}-pdo>=5.2.9
 BUILDLINK_PKGSRCDIR.php-pdo?=	../../databases/php-pdo
 BUILDLINK_INCDIRS.php-pdo?=	include/php/ext/pdo
