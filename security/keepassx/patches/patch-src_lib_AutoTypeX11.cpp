1421d2ab6098b6a76f49c9f4c7850798fb581d06
bsiegert <bsiegert@pkgsrc.org>
2015-12-06 20:10:39 +0000

    Pullup ticket #4868 - requested by wiz security/keepassx: security fix
    Revisions pulled up:
    - security/keepassx/Makefile                                    1.31
    - security/keepassx/distinfo                                    1.8
    - security/keepassx/patches/patch-src_lib_AutoTypeX11.cpp       1.2
    - security/keepassx/patches/patch-src_lib_FileDialogs.cpp       1.1
    - security/keepassx/patches/patch-src_lib_random.cpp            1.4
    
    ---
       Module Name:	pkgsrc
       Committed By:	wiz
       Date:		Sun Dec  6 14:20:34 UTC 2015
    
       Modified Files:
       	pkgsrc/security/keepassx: Makefile distinfo
       	pkgsrc/security/keepassx/patches: patch-src_lib_AutoTypeX11.cpp
       	    patch-src_lib_random.cpp
       Added Files:
       	pkgsrc/security/keepassx/patches: patch-src_lib_FileDialogs.cpp
    
       Log Message:
       Fix CVE-2015-8378 using the patch from Debian.
       Bump PKGREVISION.
       While here, clean up pkglint.

--- security/keepassx/patches/patch-src_lib_AutoTypeX11.cpp
+++ security/keepassx/patches/patch-src_lib_AutoTypeX11.cpp
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_lib_AutoTypeX11.cpp,v 1.1 2013/05/04 12:59:48 joerg Exp $
+$NetBSD: patch-src_lib_AutoTypeX11.cpp,v 1.1.20.1 2015/12/06 20:10:39 bsiegert Exp $
+
+Add a bunch of missing includes hidden by libstdc++ namespace pollution.
 
 --- src/lib/AutoTypeX11.cpp.orig	2013-05-04 00:34:27.000000000 +0000
 +++ src/lib/AutoTypeX11.cpp
