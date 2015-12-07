8b270d30a585f25de255b5dc4844434559d68685
joerg <joerg>
2015-12-07 17:40:27 +0000

    Fix build on platforms that actually need -ldl. Improve the ftime removal.

--- math/lp_solve/patches/patch-lp__utils.c
+++ math/lp_solve/patches/patch-lp__utils.c
@@ -1,16 +1,14 @@
-$NetBSD: patch-lp__utils.c,v 1.1 2014/05/12 11:41:46 ryoon Exp $
+$NetBSD: patch-lp__utils.c,v 1.2 2015/12/07 17:40:27 joerg Exp $
 
-OpenBSD 5.5 has no sys/timeb.h.
+Don't use obsolete headers.
 
 --- lp_utils.c.orig	2009-08-27 19:52:00.000000000 +0000
 +++ lp_utils.c
-@@ -5,7 +5,9 @@
+@@ -5,7 +5,6 @@
  #include "lp_lib.h"
  #include "lp_utils.h"
  #include <time.h>
-+#if !defined(__OpenBSD__)
- #include <sys/timeb.h>
-+#endif
+-#include <sys/timeb.h>
  #include "lp_bit.h"
  
  #ifdef FORTIFY
