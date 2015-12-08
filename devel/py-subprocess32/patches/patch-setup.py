07f1f1aeef13e051b3619db7a180d43fa6be802b
nonaka <nonaka@pkgsrc.org>
2015-12-08 14:12:02 +0000

    Updated devel/py-subprocess32 to 3.2.7
    -----------------
    2015-11-15 3.2.7
    -----------------
    
    * Issue #6973: When we know a subprocess.Popen process has died, do
      not allow the send_signal(), terminate(), or kill() methods to do
      anything as they could potentially signal a different process.
    * Issue #23564: Fixed a partially broken sanity check in the _posixsubprocess
      internals regarding how fds_to_pass were passed to the child.  The bug had
      no actual impact as subprocess32.py already avoided it.
    
    -----------------
    2015-11-14 3.2.7rc2
    -----------------
    
    * Moved the repository from code.google.com to github.
    * Added a _WAIT_TIMEOUT to satisfy the unsupported people entirely on
      their own trying to use this on Windows.
    * Similarly: Updated setup.py to not build the extension on non-posix.
    
    -----------------
    2014-06-01 3.2.7rc1
    -----------------
    
    * Issue #21618: The subprocess module could fail to close open fds that were
      inherited by the calling process and already higher than POSIX resource
      limits would otherwise allow.  On systems with a functioning /proc/self/fd
      or /dev/fd interface the max is now ignored and all fds are closed.

--- devel/py-subprocess32/patches/patch-setup.py
+++ devel/py-subprocess32/patches/patch-setup.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-setup.py,v 1.1 2015/08/01 15:29:43 bsiegert Exp $
+$NetBSD: patch-setup.py,v 1.2 2015/12/08 14:12:03 nonaka Exp $
 
---- setup.py.orig	2014-04-23 17:13:49.000000000 +0900
-+++ setup.py	2015-07-20 11:56:46.000000000 +0900
-@@ -1,6 +1,7 @@
- #!/usr/bin/python
+--- setup.py.orig	2015-11-16 13:32:05.000000000 +0900
++++ setup.py	2015-12-08 22:57:38.000000000 +0900
+@@ -2,6 +2,7 @@
  
+ import os
  import sys
 +import setuptools
  from distutils.core import setup, Extension
