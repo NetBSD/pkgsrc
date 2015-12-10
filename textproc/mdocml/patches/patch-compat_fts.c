48c8126ad6d325f13c9696185ddb985b27a3605c
joerg <joerg@pkgsrc.org>
2015-12-10 02:50:19 +0000

    dirfd is missing on HP-UX. Don't include getopt.h -- no getopt_long here.

--- textproc/mdocml/patches/patch-compat_fts.c
+++ textproc/mdocml/patches/patch-compat_fts.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-compat_fts.c,v 1.5 2015/03/26 19:59:53 sevan Exp $
+$NetBSD: patch-compat_fts.c,v 1.6 2015/12/10 02:50:19 joerg Exp $
 
-Fix build under Solaris 10.
+Fix build under Solaris 10 and HP-UX.
 
 --- compat_fts.c.orig	2015-03-13 12:38:38.000000000 +0000
-+++ compat_fts.c	2015-03-19 08:22:11.000000000 +0000
-@@ -76,6 +76,10 @@
++++ compat_fts.c
+@@ -76,6 +76,10 @@ static int	 fts_safe_changedir(FTS *, FT
  
  #define	FCHDIR(sp, fd)	(!ISSET(FTS_NOCHDIR) && fchdir(fd))
  
-+#ifdef __sun__
++#if defined(__sun__) || defined(__hpux)
 +#define dirfd(fd) ((fd)->dd_fd)
 +#endif
 +
