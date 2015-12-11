f185909fddfc35318a7688c8529332416d5d6559
taca <taca>
2015-12-11 16:16:48 +0000

    Avoid to use deprecated syntax.  Fix runtime problem with PHP 7.

--- lang/php/pear_plist.php
+++ lang/php/pear_plist.php
@@ -1,5 +1,5 @@
 <?php
-# $NetBSD: pear_plist.php,v 1.8 2010/08/29 06:08:07 obache Exp $
+# $NetBSD: pear_plist.php,v 1.9 2015/12/11 16:16:48 taca Exp $
 # Parses package XML file and outputs appropriate PLIST
 
 include_once "PEAR/Registry.php";
@@ -10,14 +10,14 @@ $PEAR_LIB = getenv('PEAR_LIB');
 $WRKSRC = getenv('WRKSRC');
 if(! $DESTDIR = getenv('DESTDIR')) $DESTDIR='';
 
-$config = &PEAR_Config::singleton();
-$package = &new PEAR_PackageFile($config);
+$config = PEAR_Config::singleton();
+$package = new PEAR_PackageFile($config);
 $info = $package->fromAnyFile("$WRKSRC/package.xml", PEAR_VALIDATE_INSTALLING);
 
 $pkg = $info->getName();
 $channel = $info->getChannel();
 
-$registry = &new PEAR_Registry($DESTDIR.$PREFIX."/".$PEAR_LIB);
+$registry = new PEAR_Registry($DESTDIR.$PREFIX."/".$PEAR_LIB);
 $flist = $registry->packageInfo($pkg, 'filelist', $channel);
 
 $regfile = $PEAR_LIB.'/.registry/.channel.'.$channel.'/'.strtolower($pkg).'.reg';
