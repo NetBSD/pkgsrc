f97a36220e8f9aeb8fa6c375c7fc25ef3383fd2a
wiz <wiz@pkgsrc.org>
2015-12-11 13:35:04 +0000

    Re-Import keepassx-0.4.4 as security/keepassx0 before update to new major version.
    KeePass is a free/open-source password manager or safe which helps you to
    manage your passwords in a secure way. You can put all your passwords in one
    database, which is locked with one master key or a key-disk. So you only have
    to remember one single master password or insert the key-disk to unlock the
    whole database. The databases are encrypted using the best and most secure
    encryption algorithms currently known (AES and Twofish).
    
    This package contains v0.4.x of the program with KeePass v1 database
    format (kdb) support.

---
+++ security/keepassx0/patches/patch-src_crypto_aes__endian.h
@@ -0,0 +1,15 @@
+$NetBSD: patch-src_crypto_aes__endian.h,v 1.1 2015/12/11 13:35:04 wiz Exp $
+
+Fix build on DragonFly.
+
+--- src/crypto/aes_endian.h.orig	2008-08-01 19:03:05.000000000 +0000
++++ src/crypto/aes_endian.h
+@@ -34,7 +34,7 @@
+ /* Include files where endian defines and byteswap functions may reside */
+ #if defined( __sun )
+ #  include <sys/isa_defs.h>
+-#elif defined( __FreeBSD__ ) || defined( __OpenBSD__ ) || defined( __NetBSD__ )
++#elif defined( __FreeBSD__ ) || defined( __OpenBSD__ ) || defined( __NetBSD__ ) || defined( __DragonFly__ )
+ #  include <sys/endian.h>
+ #elif defined( BSD ) && ( BSD >= 199103 ) || defined( __APPLE__ ) || \
+       defined( __CYGWIN32__ ) || defined( __DJGPP__ ) || defined( __osf__ )
