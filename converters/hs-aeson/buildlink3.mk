d794217ec8f464a0da5cfe7720f4d8bc74d0af76
szptvlfn <szptvlfn@pkgsrc.org>
2015-12-13 14:14:34 +0000

    Bump PKGREVISION for hs-primitive-0.6.1.0 || hs-vector-0.11.0.0 || hs-mwc-random-0.13.3.2 || hs-vector-algorithms-0.7.0.1

--- converters/hs-aeson/buildlink3.mk
+++ converters/hs-aeson/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2015/08/02 11:27:04 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2015/12/13 14:14:34 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-aeson
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-aeson
 HS_AESON_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-aeson+=	hs-aeson>=0.7.0
-BUILDLINK_ABI_DEPENDS.hs-aeson+=	hs-aeson>=0.7.0.6nb13
+BUILDLINK_ABI_DEPENDS.hs-aeson+=	hs-aeson>=0.7.0.6nb14
 BUILDLINK_PKGSRCDIR.hs-aeson?=	../../converters/hs-aeson
 
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
