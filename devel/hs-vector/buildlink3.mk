149f0c79076dcbc7f4e328d27acdd41c3f119bb1
szptvlfn <szptvlfn@pkgsrc.org>
2015-12-13 14:08:12 +0000

    Update to 0.11.0.0
    changelog:
    Changes in version 0.11.0.0
    
     * Define `Applicative` instances for `Data.Vector.Fusion.Util.{Box,Id}`
     * Define non-bottom `fail` for `instance Monad Vector`
     * New generalized stream fusion framework
     * Various safety fixes
       - Various overflows due to vector size have been eliminated
       - Memory is initialized on creation of unboxed vectors
     * Changes to SPEC usage to allow building under more conditions
    
    Changes in version 0.10.12.3
    
     * Allow building with `primtive-0.6`

--- devel/hs-vector/buildlink3.mk
+++ devel/hs-vector/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2015/05/09 11:22:26 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2015/12/13 14:08:12 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-vector
 
 .if !defined(HS_VECTOR_BUILDLINK3_MK)
 HS_VECTOR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.10.12
-BUILDLINK_ABI_DEPENDS.hs-vector+=	hs-vector>=0.10.12.2nb1
+BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.11.0
+BUILDLINK_ABI_DEPENDS.hs-vector+=	hs-vector>=0.11.0.0
 BUILDLINK_PKGSRCDIR.hs-vector?=	../../devel/hs-vector
 
 .include "../../devel/hs-primitive/buildlink3.mk"
