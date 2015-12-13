67bcb78232b26ee54adbfcfdc0c06722b7febac1
szptvlfn <szptvlfn@pkgsrc.org>
2015-12-13 14:06:53 +0000

    Update to 0.6.1.0
    changelog.md:
    ## Changes in version 0.6.1.0
    
     * Use more appropriate types in internal memset functions, which prevents
       overflows/segfaults on 64-bit systems.
    
     * Fixed a warning on GHC 7.10
    
     * Worked around a -dcore-lint bug in GHC 7.6/7.7
    
    ## Changes in version 0.6
    
     * Split PrimMonad into two classes to allow automatic lifting of primitive
       operations into monad transformers. The `internal` operation has moved to the
       `PrimBase` class.
    
     * Fixed the test suite on older GHCs

--- devel/hs-primitive/buildlink3.mk
+++ devel/hs-primitive/buildlink3.mk
@@ -1,13 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.3 2015/02/14 04:10:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2015/12/13 14:06:53 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-primitive
 
 .if !defined(HS_PRIMITIVE_BUILDLINK3_MK)
 HS_PRIMITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.5.4
-BUILDLINK_ABI_DEPENDS.hs-primitive+=	hs-primitive>=0.5.4.0
+BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.6.1
+BUILDLINK_ABI_DEPENDS.hs-primitive+=	hs-primitive>=0.6.1.0
 BUILDLINK_PKGSRCDIR.hs-primitive?=	../../devel/hs-primitive
+
+.include "../../devel/hs-transformers/buildlink3.mk"
 .endif	# HS_PRIMITIVE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-primitive
