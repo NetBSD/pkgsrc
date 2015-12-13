f146012e1554e62b59a592e460a1770496ef2419
taca <taca@pkgsrc.org>
2015-12-13 14:59:35 +0000

    Update SOGo to 2.3.3a.
    2.3.3a (2015-11-18)
    -------------------
    
    Bug fixes
     - expanded mail folders list is not saved (#3386)
     - cleanup translations
    
    2.3.3 (2015-11-11)
    ------------------
    
    New features
     - initial S/MIME support for EAS (#3327)
     - now possible to choose which folders to sync over EAS
    
    Enhancements
     - we no longer always entirely rewrite messages for Outlook 2013 when using EAS
     - support for ghosted elements on contacts over EAS
     - added Macedonian (mk_MK) translation - thanks to Miroslav Jovanovic
     - added Portuguese (pt) translation - thanks to Eduardo Crispim
    
    Bug fixes
     - numerous EAS fixes when connections are dropped before the EAS client receives the response (#3058, #2849)
     - correctly handle the References header over EAS (#3365)
     - make sure English is always used when generating Date headers using EAS (#3356)
     - don't escape quoted strings during versit generation
     - we now return all cards when we receive an empty addressbook-query REPORT
     - avoid crash when replying to a mail with no recipients (#3359)
     - inline images sent from SOGo webmail are not displayed in Mozilla Thunderbird (#3271)
     - prevent postal address showing on single line over EAS (#2614)
     - display missing events when printing working hours only
     - fix corner case making server crash when syncing hard deleted messages when clear offline items was set up (Zentyal)
     - avoid infinite Outlook client loops trying to set read flag when it is already set (Zentyal)
     - avoid crashing when calendar metadata is missing in the cache (Zentyal)
     - fix recurrence pattern event corner case created by Mozilla Thunderbird which made server crash (Zentyal)
     - fix corner case that removes attachments on sending messages from Outlook (Zentyal)
     - freebusy on web interface works again in multidomain environments (Zentyal)
     - fix double creation of folders in Outlook when the folder name starts with a digit (Zentyal)
     - avoid crashing Outlook after setting a custom view in a calendar folder (Zentyal)
     - handle emails having an attachment as their content
     - fixed JavaScript syntax error in attendees editor
     - fixed wrong comparison of meta vs. META tag in HTML mails
     - fixed popup menu position when moved to the left (#3381)
     - fixed dialog position when at the bottom of the window (#2646, #3378)
     - fixed addressbrook-only source entires having a c_uid set

--- www/SOGo/patches/patch-SoObjects_SOGo_NSData+Crypto.m
+++ www/SOGo/patches/patch-SoObjects_SOGo_NSData+Crypto.m
@@ -1,12 +1,12 @@
-$NetBSD: patch-SoObjects_SOGo_NSData+Crypto.m,v 1.2 2013/03/16 15:12:57 taca Exp $
+$NetBSD: patch-SoObjects_SOGo_NSData+Crypto.m,v 1.3 2015/12/13 14:59:35 taca Exp $
 
---- SoObjects/SOGo/NSData+Crypto.m.orig	2013-02-04 20:13:13.000000000 +0000
+--- SoObjects/SOGo/NSData+Crypto.m.orig	2015-11-18 15:19:39.000000000 +0000
 +++ SoObjects/SOGo/NSData+Crypto.m
 @@ -23,7 +23,7 @@
   * Boston, MA 02111-1307, USA.
   */
  
--#ifndef __OpenBSD__
+-#if !defined(__OpenBSD__) && !defined(__FreeBSD__)
 +#ifdef __sun
  #include <crypt.h>
  #endif
