96e43e1674f943d5d5a29f1040ab1c6124b94e42
szptvlfn <szptvlfn@pkgsrc.org>
2015-12-13 14:14:34 +0000

    Bump PKGREVISION for hs-primitive-0.6.1.0 || hs-vector-0.11.0.0 || hs-mwc-random-0.13.3.2 || hs-vector-algorithms-0.7.0.1

--- devel/hs-QuickCheck/buildlink3.mk
+++ devel/hs-QuickCheck/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2015/05/09 11:22:26 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2015/12/13 14:14:35 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-QuickCheck
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-QuickCheck
 HS_QUICKCHECK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.7.6
-BUILDLINK_ABI_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.7.6nb2
+BUILDLINK_ABI_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.7.6nb3
 BUILDLINK_PKGSRCDIR.hs-QuickCheck?=	../../devel/hs-QuickCheck
 
 .include "../../devel/hs-random/buildlink3.mk"
