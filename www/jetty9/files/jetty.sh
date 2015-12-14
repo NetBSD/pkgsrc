c90baba49993bd96d362de2c3c411c78eefc853d
jperkin <jperkin@pkgsrc.org>
2015-12-14 13:05:30 +0000

    Import www/jetty9 version 9.2.14.v20151106 into pkgsrc.
    Jetty provides a Web server and javax.servlet container, plus support for
    HTTP/2, WebSocket, OSGi, JMX, JNDI, JAAS and many other integrations.  These
    components are open source and available for commercial use and distribution.
    
    This package builds on the existing www/jetty7 package which is retained for
    users of that maintenance release, simplifies the packaging, and adds SMF
    support.

---
+++ www/jetty9/files/jetty.sh
@@ -0,0 +1,91 @@
+#! @RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: jetty.sh,v 1.1 2015/12/14 13:05:30 jperkin Exp $
+#
+# PROVIDE: jetty
+# REQUIRE: DAEMON
+#
+
+if [ -r /etc/rc.subr ]; then
+	. /etc/rc.subr
+fi
+
+name="jetty"
+rcvar=$name
+command="@JETTY_HOME@/bin/jetty.sh"
+pidfile="jetty.pid"
+jetty_user="@JETTY_USER@"
+jetty_group="@JETTY_GROUP@"
+start_cmd="$command start"
+stop_cmd="$command stop"
+restart_cmd="$stop_cmd && $start_cmd"
+run_cmd="jetty_run"
+version_cmd="jetty_version"
+debug_cmd="jetty_debug"
+extra_commands="run version debug"
+start_precmd="jetty_precmd"
+
+PREFIX=@PREFIX@
+
+JAVA_OPTIONS="-Xmx768M -Djava.nio.channels.spi.SelectorProvider=sun.nio.ch .PollSelectorProvider"
+
+if [ -z "${JAVA_HOME}" ]; then
+	JAVA_HOME="@JAVA_HOME@"
+	export JAVA_HOME
+	JAVA="@JAVA_HOME@/bin/java"
+	export JAVA
+fi
+
+JETTY_HOME=@JETTY_HOME@
+JETTY_BASE=@JETTY_BASE@
+JETTY_LOGS=@JETTY_LOGS@
+JETTY_RUN=@JETTY_RUN@
+JETTY_PID=${pidfile}
+export JETTY_HOME
+export JETTY_BASE
+export JETTY_LOGS
+export JETTY_RUN
+export JETTY_PID
+
+CLASSPATH=${CLASSPATH}:${PREFIX}/lib/java/servlet.jar:${PREFIX}/lib/java/jaxp.jar:${PREFIX}/lib/java/parser.jar:${PREFIX}/lib/java/crimson.jar:${PREFIX}/lib/java/ant.jar
+
+if [ -n "${jetty_classpath}" ]; then
+	CLASSPATH=${CLASSPATH}:${jetty_classpath}
+fi
+export CLASSPATH
+
+jetty_precmd()
+{
+	if [ ! -d ${JETTY_RUN} ]; then
+		@MKDIR@ ${JETTY_RUN}
+		@CHMOD@ 0750 ${JETTY_RUN}
+		@CHOWN@ ${jetty_user}:${jetty_group} ${JETTY_RUN}
+	fi
+}
+
+jetty_start()
+{
+	cd ${JETTY_HOME}/logs
+	@SU@ ${jetty_user} -c "${command} start"
+}
+
+jetty_stop()
+{
+	rc_pid=$(check_pidfile ${JETTY_RUN}/${JETTY_PID} "${JAVA_HOME}/bin/java")
+	@SU@ ${jetty_user} -c "${command} stop"
+	wait_for_pids $rc_pid
+}
+
+jetty_run()
+{
+	cd ${JETTY_HOME}/logs
+	@SU@ ${jetty_user} -c "${command} run"
+}
+
+if [ -f /etc/rc.subr ]; then
+	load_rc_config "$name"
+	run_rc_command "$1"
+else
+	jetty_precmd
+	jetty_start
+fi
