67a69e3a4055d34c6636d0f7cccfe4342ec13eb8
bouyer <bouyer@pkgsrc.org>
2015-12-14 16:57:29 +0000

    Fix comment, pointed out by wiz@

--- x11/wxGTK30/hacks.mk
+++ x11/wxGTK30/hacks.mk
@@ -1,7 +1,7 @@
-# $NetBSD: hacks.mk,v 1.1 2015/12/14 16:49:22 bouyer Exp $
+# $NetBSD: hacks.mk,v 1.2 2015/12/14 16:57:29 bouyer Exp $
 
 ### [Mon Dec 14 16:58:00 MET 2015 : bouyer ]
-### g++-4.5.3 and 4.5.4 on NetBSD/i386 fails with an internal compiler error
+### g++-4.8 on NetBSD/i386 fails with an internal compiler error
 ### when PCH is used. Be conservative and disable on all MACHINE_ARCH
 ### 
 .if ${OPSYS} == "NetBSD" 
