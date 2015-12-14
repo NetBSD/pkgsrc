dbedab266986d64ea3cab872bb884558d9fff26e
joerg <joerg@pkgsrc.org>
2015-12-14 19:50:55 +0000

    Check that the compiler is actually gcc.

--- x11/wxGTK30/hacks.mk
+++ x11/wxGTK30/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.2 2015/12/14 16:57:29 bouyer Exp $
+# $NetBSD: hacks.mk,v 1.3 2015/12/14 19:50:55 joerg Exp $
 
 ### [Mon Dec 14 16:58:00 MET 2015 : bouyer ]
 ### g++-4.8 on NetBSD/i386 fails with an internal compiler error
@@ -6,7 +6,7 @@
 ### 
 .if ${OPSYS} == "NetBSD" 
 .  include "../../mk/compiler.mk"
-.  if !empty(CC_VERSION:Mgcc-4.8.*)
+.  if ${PKGSRC_COMPILER} == "gcc" && !empty(CC_VERSION:Mgcc-4.8.*)
 CONFIGURE_ARGS+=	--disable-precomp-headers
-.endif
+.  endif
 .endif
