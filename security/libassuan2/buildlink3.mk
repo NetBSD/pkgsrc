024a61cc2988c5c5782786ee2d724f3aafa29de8
ryoon <ryoon@pkgsrc.org>
2015-12-14 14:07:42 +0000

    Update to 2.4.2
    Changelog:
    Noteworthy changes in version 2.4.2 (2015-12-02) [C7/A7/R2]
    ------------------------------------------------
    
     * The nPth version of the connect system hook does now wrap the call
       with npth_unprotec/npth_protect to avoid blocking during a connect.
    
     * Add feature to assuan_sock_connect_byname to test for SOCKS5
       availability.
    
    
    Noteworthy changes in version 2.4.1 (2015-11-23) [C7/A7/R1]
    ------------------------------------------------
    
     * In Tor mode fallback to port 9150 if 9050 is not listening.
    
     * Allow building with older mingw-w64 versions.
    
    
    Noteworthy changes in version 2.4.0 (2015-11-03) [C7/A7/R0]
    ------------------------------------------------
    
     * New flags "socks" and "tor-mode" for assuan_sock_{set,get}_flag.
    
     * New function assuan_sock_connect_byname.
    
     * Require at least libgpg-error 1.17.
    
     * Interface changes relative to the 2.3.0 release:
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     assuan_sock_connect_byname    NEW.
     ASSUAN_SOCK_TOR               NEW.
     ASSUAN_SOCK_SOCKS             NEW.
     assuan_sock_set_flag          EXTENDED.
     assuan_sock_get_flag          EXTENDED.
    
    
    Noteworthy changes in version 2.3.0 (2015-08-28) [C6/A6/R0]
    ------------------------------------------------
    
     * Now wipes out the memory of the context structure before freeing.
       The context may have stored sensitive data in its line buffers.
    
     * Fixed a problem with the data length limit in assuan_inquire.
    
     * Returns GPG_ERR_SOURCE_ASSUAN with errors from functions w/o a
       context.
    
     * Two new functions to tweak the behaviour of the socket wrappers.
    
     * Experimental code to support Cygwin's local sockets.
    
     * By default build without a build timestamp.
    
     * Interface changes relative to the 2.2.1 release:
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     assuan_sock_set_flag     NEW.
     assuan_sock_get_flag     NEW.

--- security/libassuan2/buildlink3.mk
+++ security/libassuan2/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/04/22 13:42:47 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2015/12/14 14:07:42 ryoon Exp $
 
 BUILDLINK_TREE+=	libassuan
 
 .if !defined(LIBASSUAN_BUILDLINK3_MK)
 LIBASSUAN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libassuan+=	libassuan>=2.0.0
-BUILDLINK_ABI_DEPENDS.libassuan+=	libassuan>=2.0.1nb2
+BUILDLINK_API_DEPENDS.libassuan+=	libassuan>=2.4.2
+BUILDLINK_ABI_DEPENDS.libassuan+=	libassuan>=2.4.2
 BUILDLINK_PKGSRCDIR.libassuan?=	../../security/libassuan2
 
 .include "../../security/libgpg-error/buildlink3.mk"
