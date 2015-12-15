de87c6bd73b147ada8ff399f1006a4f1bd0b25db
markd <markd@pkgsrc.org>
2015-12-15 09:46:27 +0000

    Update blender to 2.74
    Blender 2.74, released Mar 31 2015!
    - Cycles got several precision, noise, speed, memory improvements, new
      Pointiness attribute.
    - Viewport has new cool compositing features, outliner now manages
      orphan datablocks better.
    - Modeling now allows custom normals, and transferring data layers
      between meshes!
    - Massive improvements to hair dynamics and editing tools: a big step
      forward with more to come…
    - A new tool “Cavity Mask” was added to  sculpting/painting, along with
      other improvements.
    - Great memory consumption optimization in Freestyle NPR stroke rendering.
    - Grease pencil and Game engine improvements, and more…
    
    Blender 2.73, released January 8 2015!
    - Several Cycles Rendering updates, improvements and new features, support
      for GeForce 9xx GPUs.
    - New fullscreen mode, improved Pie Menus, 3D View can now display the
      world background.
    - Knife-tool now allows freehand smart cuts, and more improvements were
      made in selection tools, Bevel Modifier and Shapekeys.
    - Unwrapping UVs was improved, strength is now enabled for Grab and Snake
      Hook brushes, better visual feedback mode for painting with masks.
    - Sequencer now can show a backdrop, strips can be snapped to others along
      with other improvements.
    - Freestyle NPR Rendering has a new SVG exporter add-on, along with other
      improvements.
    - Grease Pencil had a major update, it is now possible to edit and animate
      strokes! New draw styles, and imporvements in user interface.
    - Collada Importer was updated, improving the compatibility with other
      software’s bone concepts.
    - Add-ons: FBX improved importing/exporting armatures, some Python API
      changes  could need some Add-ons to be updated!
    - As usual several other minor updates and improvements, and lot of
      bugfixing also in this release.
    
    Blender 2.72, relased Oct 4 2014.
    Cycles GPU rendering supports Volume and Subsurface Scattering now. The
    User Interface comes with optional Pie Menus now and the Tooltips have been
    improved. For Modeling, a new intersection tool has been added in Edit Mode.
    Texture painting workflow has been streamlined with easy access to painted
    images and UV layers. The Compositor now comes with a Sun Beam node.
    Freestyle NPR rendering is now available with Cycles as well.

--- graphics/blender/patches/patch-build__files_scons_tools_btools.py
+++ graphics/blender/patches/patch-build__files_scons_tools_btools.py
@@ -1,25 +1,28 @@
-$NetBSD: patch-build__files_scons_tools_btools.py,v 1.2 2014/07/09 20:01:08 ryoon Exp $
+$NetBSD: patch-build__files_scons_tools_btools.py,v 1.3 2015/12/15 09:46:27 markd Exp $
 
 From Gentoo's 2.64a/0300-scons-failure.patch
 
---- build_files/scons/tools/btools.py.orig	2014-06-25 20:43:52.000000000 +0000
+--- build_files/scons/tools/btools.py.orig	2015-04-01 02:44:23.000000000 +0000
 +++ build_files/scons/tools/btools.py
-@@ -56,13 +56,7 @@ def get_version():
+@@ -71,16 +71,7 @@ def get_version():
      raise Exception("%s: missing version string" % fname)
  
  def get_hash():
 -    try:
--        build_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip()
+-        build_hash = get_command_output(['git', 'rev-parse', '--short', 'HEAD']).strip()
 -    except OSError:
 -        build_hash = None
 -        print("WARNING: could not use git to retrieve current Blender repository hash...")
+-    except subprocess.CalledProcessError as e:
+-        build_hash = None
+-        print("WARNING: git errored while retrieving current Blender repository hash (%d)..." % e.returncode)
 -    if build_hash == '' or build_hash == None:
 -        build_hash = 'UNKNOWN'
 +    build_hash = 'UNKNOWN'
  
      return build_hash
  
-@@ -478,7 +472,9 @@ def read_opts(env, cfg, args):
+@@ -517,7 +508,9 @@ def read_opts(env, cfg, args):
          ('BF_3DMOUSE_LIB', '3d mouse library', ''),
          ('BF_3DMOUSE_LIBPATH', '3d mouse library path', ''),
          ('BF_3DMOUSE_LIB_STATIC', '3d mouse static library', ''),
