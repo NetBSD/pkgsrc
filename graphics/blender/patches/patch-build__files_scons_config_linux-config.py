6c40939a8f9cc6accdc291f859da0261eff3c2f8
markd <markd@pkgsrc.org>
2015-12-15 09:46:27 +0000

    Update blender to 2.74
    Blender 2.74, released Mar 31 2015!
    - Cycles got several precision, noise, speed, memory improvements, new
      Pointiness attribute.
    - Viewport has new cool compositing features, outliner now manages
      orphan datablocks better.
    - Modeling now allows custom normals, and transferring data layers
      between meshes!
    - Massive improvements to hair dynamics and editing tools: a big step
      forward with more to come…
    - A new tool “Cavity Mask” was added to  sculpting/painting, along with
      other improvements.
    - Great memory consumption optimization in Freestyle NPR stroke rendering.
    - Grease pencil and Game engine improvements, and more…
    
    Blender 2.73, released January 8 2015!
    - Several Cycles Rendering updates, improvements and new features, support
      for GeForce 9xx GPUs.
    - New fullscreen mode, improved Pie Menus, 3D View can now display the
      world background.
    - Knife-tool now allows freehand smart cuts, and more improvements were
      made in selection tools, Bevel Modifier and Shapekeys.
    - Unwrapping UVs was improved, strength is now enabled for Grab and Snake
      Hook brushes, better visual feedback mode for painting with masks.
    - Sequencer now can show a backdrop, strips can be snapped to others along
      with other improvements.
    - Freestyle NPR Rendering has a new SVG exporter add-on, along with other
      improvements.
    - Grease Pencil had a major update, it is now possible to edit and animate
      strokes! New draw styles, and imporvements in user interface.
    - Collada Importer was updated, improving the compatibility with other
      software’s bone concepts.
    - Add-ons: FBX improved importing/exporting armatures, some Python API
      changes  could need some Add-ons to be updated!
    - As usual several other minor updates and improvements, and lot of
      bugfixing also in this release.
    
    Blender 2.72, relased Oct 4 2014.
    Cycles GPU rendering supports Volume and Subsurface Scattering now. The
    User Interface comes with optional Pie Menus now and the Tooltips have been
    improved. For Modeling, a new intersection tool has been added in Edit Mode.
    Texture painting workflow has been streamlined with easy access to painted
    images and UV layers. The Compositor now comes with a Sun Beam node.
    Freestyle NPR rendering is now available with Cycles as well.

--- graphics/blender/patches/patch-build__files_scons_config_linux-config.py
+++ graphics/blender/patches/patch-build__files_scons_config_linux-config.py
@@ -1,8 +1,8 @@
-$NetBSD: patch-build__files_scons_config_linux-config.py,v 1.1 2014/02/01 03:57:52 markd Exp $
+$NetBSD: patch-build__files_scons_config_linux-config.py,v 1.2 2015/12/15 09:46:27 markd Exp $
 
---- ./build_files/scons/config/linux-config.py.orig	2013-02-07 14:10:01.000000000 +0000
-+++ ./build_files/scons/config/linux-config.py
-@@ -178,7 +178,7 @@ BF_JEMALLOC_LIBPATH = '${BF_JEMALLOC}/li
+--- build_files/scons/config/linux-config.py.orig	2015-03-30 23:32:22.000000000 +0000
++++ build_files/scons/config/linux-config.py
+@@ -175,7 +175,7 @@ BF_JEMALLOC_LIBPATH = '${BF_JEMALLOC}/li
  BF_JEMALLOC_LIB = 'jemalloc'
  BF_JEMALLOC_LIB_STATIC = '${BF_JEMALLOC_LIBPATH}/libjemalloc.a'
  
@@ -11,7 +11,7 @@ $NetBSD: patch-build__files_scons_config_linux-config.py,v 1.1 2014/02/01 03:57:
  WITH_BF_STATICOIIO = False
  BF_OIIO = '/usr'
  BF_OIIO_INC = '${BF_OIIO}/include'
-@@ -218,7 +218,7 @@ WITH_BF_RAYOPTIMIZATION = True
+@@ -215,7 +215,7 @@ WITH_BF_RAYOPTIMIZATION = True
  BF_RAYOPTIMIZATION_SSE_FLAGS = ['-msse','-pthread']
  
  #SpaceNavigator and friends
@@ -20,3 +20,11 @@ $NetBSD: patch-build__files_scons_config_linux-config.py,v 1.1 2014/02/01 03:57:
  WITH_BF_STATIC3DMOUSE = False
  BF_3DMOUSE = '/usr'
  BF_3DMOUSE_INC = '${BF_3DMOUSE}/include'
+@@ -261,6 +261,7 @@ BF_INSTALLDIR='../install/linux'
+ 
+ #Link against pthread
+ PLATFORM_LINKFLAGS = ['-pthread']
++PLATFORM_LINKFLAGS += ['-Wl,-R${BF_OPENEXR}/lib']
+ 
+ #Fix for LLVM conflict with Mesa llvmpipe, SDL dynload also requires symbols to be hidden.
+ # TODO(sergey): Move this to SConstruct, so we can have this line depended on user config.
