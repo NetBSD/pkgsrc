5144c7b372a401bb20ae0e472005480fc22809ed
taca <taca@pkgsrc.org>
2015-12-16 16:05:19 +0000

    Update ruby22{-base} to 2.2.4 (Ruby 2.2.4).
    Ruby 2.2.4 Released
    
    Posted by nagachika on 16 Dec 2015
    
    Ruby 2.2.4 has been released.
    
    This release includes a security fix for Fiddle extension. Please view the
    topic below for more details.
    
        * CVE-2015-7551: Unsafe tainted string usage in Fiddle and DL
    
    There are also some bugfixes. See ChangeLog for details.

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.152 2015/12/16 16:03:12 taca Exp $
+# $NetBSD: rubyversion.mk,v 1.153 2015/12/16 16:05:19 taca Exp $
 #
 
 # This file determines which Ruby version is used as a dependency for
@@ -229,7 +229,7 @@ RUBY_VERSION_REQD?= ${PKGNAME_REQD:C/ruby([0-9][0-9]+)-.*/\1/}
 RUBY18_VERSION=		1.8.7
 RUBY200_VERSION=	2.0.0
 RUBY21_VERSION=		2.1.8
-RUBY22_VERSION=		2.2.3
+RUBY22_VERSION=		2.2.4
 
 # patch
 RUBY18_PATCHLEVEL=	pl374
