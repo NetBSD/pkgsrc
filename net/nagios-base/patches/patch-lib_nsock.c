d41d3963d6660debc99225326d9cce64b0db7614
rodent <rodent@pkgsrc.org>
2015-12-18 00:55:17 +0000

    Upstream set the maximum queue length of the socket to INT_MAX. Increase this value to 1024 to accommodate larger deployments until we get a proper tunable.

--- net/nagios-base/patches/patch-lib_nsock.c
+++ net/nagios-base/patches/patch-lib_nsock.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-lib_nsock.c,v 1.1 2015/06/15 01:29:40 rodent Exp $
+$NetBSD: patch-lib_nsock.c,v 1.2 2015/12/18 00:55:17 rodent Exp $
+
+Increase maximum queue length for socket to avoid disaster on large deployments.
 
 --- lib/nsock.c.orig	2014-08-12 15:00:01.000000000 +0000
 +++ lib/nsock.c
@@ -7,7 +9,7 @@ $NetBSD: patch-lib_nsock.c,v 1.1 2015/06/15 01:29:40 rodent Exp $
  		return sock;
  
 -	if(listen(sock, 3) < 0) {
-+	if(listen(sock, 128) < 0) {
++	if(listen(sock, 1024) < 0) {
  		close(sock);
  		return NSOCK_ELISTEN;
  	}
