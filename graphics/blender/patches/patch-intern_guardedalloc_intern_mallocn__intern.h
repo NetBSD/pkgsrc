6907d4e9651a619dfaa11e0a2481a3e95c9a3671
markd <markd>
2015-12-19 20:52:12 +0000

    Update blender to 2.76b
    Fixes build with python35  (PR pkg/50566)
    
    Blender 2.76, released Oct 9, 2015. (Update 2.76b is from Nov 3 2015).
    - Initial support for Pixar's OpenSubdiv geometry subdivision technology.
    - A huge view-port performance boost.
    - Big file browser performance boost and arrow keys navigation support.
    - Node auto-offset feature that helps organizing node layouts.
    - Absolute grid snapping in the 3D view.
    - Sculpting with tiled strokes.
    - Text effect strips for the sequencer, supporting subtitle export
    - As usual, hundreds of bugs fixed and other improvements!
    
    Blender 2.75a release.
    - Blender now supports a fully integrated Multi-View and Stereo 3D pipeline
    - Cycles has much awaited initial support for AMD GPUs, and a new Light
      Portals feature.
    - UI now allows font previews in the file browser.
    - High quality options for viewport depth of field were added
    - Modeling has a new Corrective Smooth modifier.
    - The Decimate modifier was improved significantly.
    - 3D viewport painting now supports symmetry and the distribution of Dynamic
      Topology was improved
    - Video Sequence Editor: Placeholders can now replace missing frames of
      image sequences
    - Game Engine now allows smoother LOD transitions, and supports mist
      attributes animation
    - And: 100s of bug fixes and smaller feature improvements.

--- graphics/blender/patches/patch-intern_guardedalloc_intern_mallocn__intern.h
+++ graphics/blender/patches/patch-intern_guardedalloc_intern_mallocn__intern.h
@@ -1,13 +1,13 @@
-$NetBSD: patch-intern_guardedalloc_intern_mallocn__intern.h,v 1.1 2014/04/07 12:21:25 ryoon Exp $
+$NetBSD: patch-intern_guardedalloc_intern_mallocn__intern.h,v 1.2 2015/12/19 20:52:12 markd Exp $
 
---- intern/guardedalloc/intern/mallocn_intern.h.orig	2014-03-19 22:05:25.000000000 +0000
+--- intern/guardedalloc/intern/mallocn_intern.h.orig	2015-10-07 00:09:33.000000000 +0000
 +++ intern/guardedalloc/intern/mallocn_intern.h
-@@ -62,6 +62,8 @@
+@@ -63,6 +63,8 @@
  #elif defined(WIN32)
  #  include <malloc.h>
  #  define malloc_usable_size _msize
 +#elif defined(__NetBSD__)
 +#  define malloc_usable_size(x) 0
  #else
- #  error "We don't know how to use malloc_usable_size on your platform"
- #endif
+ #  pragma message "We don't know how to use malloc_usable_size on your platform"
+ #  undef USE_MALLOC_USABLE_SIZE
