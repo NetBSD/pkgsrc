aa4982a8430fe08382c989d88712803e61defdd0
sborrill <sborrill@pkgsrc.org>
2015-12-22 10:09:15 +0000

    rinetd support SIGHUP, so add reload command to rc.d script. Bump PKGREVISION

--- net/rinetd/files/rinetd.sh
+++ net/rinetd/files/rinetd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: rinetd.sh,v 1.1.1.1 2003/10/01 01:15:44 grant Exp $
+# $NetBSD: rinetd.sh,v 1.2 2015/12/22 10:09:15 sborrill Exp $
 #
 # PROVIDE: rinetd
 # REQUIRE: DAEMON
@@ -14,6 +14,7 @@ rcvar=${name}
 command="@PREFIX@/sbin/${name}"
 pidfile="/var/run/${name}.pid"
 required_files="@PKG_SYSCONFDIR@/rinetd.conf"
+extra_commands="reload"
 
 load_rc_config $name
 run_rc_command "$1"
