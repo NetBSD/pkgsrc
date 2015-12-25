7f01abd30b21a62671d0cc7981ea4201da47a892
ryoon <ryoon@pkgsrc.org>
2015-12-25 23:09:49 +0000

    Fix high CPU usage from Onno van der Linden. Bump PKGREVISION

--- audio/pulseaudio/patches/patch-src_modules_module-detect.c
+++ audio/pulseaudio/patches/patch-src_modules_module-detect.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_modules_module-detect.c,v 1.2 2014/06/25 15:26:40 ryoon Exp $
+$NetBSD: patch-src_modules_module-detect.c,v 1.3 2015/12/25 23:09:49 ryoon Exp $
 
 Hack to set proper sound device on NetBSD.
 
---- src/modules/module-detect.c.orig	2014-01-23 18:57:55.000000000 +0000
+--- src/modules/module-detect.c.orig	2015-09-10 04:51:41.000000000 +0000
 +++ src/modules/module-detect.c
-@@ -122,6 +122,17 @@ static int detect_oss(pa_core *c, int ju
+@@ -120,6 +120,17 @@ static int detect_oss(pa_core *c, int ju
      FILE *f;
      int n = 0, b = 0;
  
@@ -14,7 +14,7 @@ Hack to set proper sound device on NetBSD.
 + * And only primary sound device will be used (n=1).
 + */
 +    char args[64];
-+    pa_snprintf(args, sizeof(args), "device=/dev/audio");
++    pa_snprintf(args, sizeof(args), "mmap=0 device=/dev/audio");
 +    if (!pa_module_load(c, "module-oss", args))
 +	exit(1);
 +    n = 1;
@@ -22,7 +22,7 @@ Hack to set proper sound device on NetBSD.
      if (!(f = pa_fopen_cloexec("/dev/sndstat", "r")) &&
          !(f = pa_fopen_cloexec("/proc/sndstat", "r")) &&
          !(f = pa_fopen_cloexec("/proc/asound/oss/sndstat", "r"))) {
-@@ -173,6 +184,7 @@ static int detect_oss(pa_core *c, int ju
+@@ -171,6 +182,7 @@ static int detect_oss(pa_core *c, int ju
      }
  
      fclose(f);
