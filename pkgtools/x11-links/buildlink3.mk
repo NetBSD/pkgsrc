b71bb2ec064f2c0faefb925648a1c8eff9585198
sevan <sevan@pkgsrc.org>
2015-12-27 18:56:33 +0000

    Add more library & headers from OpenBSD-current 11/12/2015 / Bitrig Rev bump

--- pkgtools/x11-links/buildlink3.mk
+++ pkgtools/x11-links/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.64 2015/11/17 11:18:38 sevan Exp $
+# $NetBSD: buildlink3.mk,v 1.65 2015/12/27 18:56:33 sevan Exp $
 #
 # Don't include this file manually!  It will be included as necessary
 # by bsd.buildlink3.mk.
@@ -12,7 +12,7 @@ BUILDLINK_TREE+=	x11-links
 .if !defined(X11_LINKS_BUILDLINK3_MK)
 X11_LINKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=1.00
+BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=1.01
 BUILDLINK_PKGSRCDIR.x11-links?=	../../pkgtools/x11-links
 BUILDLINK_DEPMETHOD.x11-links?=	build
 
