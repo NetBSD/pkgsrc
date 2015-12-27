01c7558a13103b0d8776a8cc8d04cbb907d327e8
adam <adam@pkgsrc.org>
2015-12-27 12:41:42 +0000

    Changes 1.60.0: New Libraries VMD: Variadic Macro Data library, from Edward Diener.
    Updated Libraries
    Atomic:
    * Enforced proper alignment of atomic<> storage. This should fix possible issues on platforms that support atomic operations on data units larger than the native word size. This may also change binary layout of user's data structures that have atomic<> members.
    * Fixed compilation for PowerPC with IBM XL C++ compiler. Corrected memory barriers in PowerPC assembler.
    * Fixed compilation with MSVC-8 for ARM.
    * Fixed compilation with gcc 4.4 for x86-64, when 128-bit atomic operations were used. (10994)
    * Optimized some gcc assembler blocks for x86/x86-64 to reduce the number of used registers. This may require binutils 2.10 or later.
    
    Chrono - 2.0.5:
    Fixes:
    * 10778 VC14 CTP4 Atomic don't compile with boost::chrono durations which are not TriviallyCopyable
    * 10840 Missing std:: qualifier for puts call in test_7868.cpp
    * 10851 Missing std:: qualifier for puts call in time_point_output.cpp
    * 10893 Minor doc typo in Boost.Chrono
    * 10992 Chrono IO state savers inconsistent with Boost.IO state savers
    * 10995 duration_put::put_value truncates fractional part
    * 11006 Impossible to instantiate time_fmt_io_saver due to several errors.
    * 11012 chrono_io v2 fail to compile with boost::chrono::duration< boost::rational<int> >
    
    Container:
    * Implemented Polymorphic Memory Resources.
    * Add more BOOST_ASSERT checks to test preconditions in some operations (like pop_back, pop_front, back, front, etc.)
    * Added C++11 back/front operations to basic_string.
    Fixed bugs:
    * Trac 11627: "small_vector<T,n>::swap() appears to be broken".
    * Trac 11628: "small_vector<int,n> iterates over elements in destructor".
    * Trac 11697: "Wrong initialization order in tuple copy-constructor".
    * Trac 11698: "Missing return statement in static_storage_allocator".
    * GitHub 29: Doc fixes for flap_map complexity requirements.
    * GitHub 31: DL_SIZE_IMPL also dereference addr.
    
    Context:
    * 11603 typo in PPC32/MACH-O asm
    * support for CYGWIN
    
    Core:
    * In symbol demangling tools, added support for gabi++ ABI implementation used in Android NDK.
    * Added a new enable_if_has_type tool for performing SFINAE checks for presence of a nested type.
    
    more...

--- devel/py-boost/buildlink3.mk
+++ devel/py-boost/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/08/14 20:57:49 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/12/27 12:41:42 adam Exp $
 
 BUILDLINK_TREE+=	py-boost
 
@@ -8,8 +8,8 @@ PY_BOOST_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 # Use a dependency pattern that guarantees the proper ABI.
-BUILDLINK_API_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.59.*
-BUILDLINK_ABI_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.59.*
+BUILDLINK_API_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.60.*
+BUILDLINK_ABI_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.60.*
 BUILDLINK_PKGSRCDIR.py-boost?=	../../devel/py-boost
 
 .include "../../devel/boost-headers/buildlink3.mk"
