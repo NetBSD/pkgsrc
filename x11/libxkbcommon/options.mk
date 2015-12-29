98785d5e4affb6557d62111b953c336f8af8ea21
dholland <dholland@pkgsrc.org>
2015-12-29 04:54:34 +0000

    Whitespace.

--- x11/libxkbcommon/options.mk
+++ x11/libxkbcommon/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2013/12/18 18:51:03 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2015/12/29 04:54:39 dholland Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libxkbcommon
 PKG_SUPPORTED_OPTIONS=	doc
@@ -8,9 +8,9 @@ PLIST_VARS+=		doc
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdoc)
-BUILD_DEPENDS+= 	doxygen-[0-9]*:../../devel/doxygen
+BUILD_DEPENDS+=		doxygen-[0-9]*:../../devel/doxygen
 PLIST.doc=		yes
-CONFIGURE_ARGS+= 	--enable-docs=yes
+CONFIGURE_ARGS+=	--enable-docs=yes
 .else
-CONFIGURE_ARGS+= 	--enable-docs=no
+CONFIGURE_ARGS+=	--enable-docs=no
 .endif
