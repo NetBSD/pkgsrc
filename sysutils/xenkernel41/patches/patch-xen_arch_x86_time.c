ffed977f73a90eb819c45227998c475d8e785de1
dholland <dholland@pkgsrc.org>
2015-12-29 04:04:26 +0000

    Fix missing/broken rcsids.

--- sysutils/xenkernel41/patches/patch-xen_arch_x86_time.c
+++ sysutils/xenkernel41/patches/patch-xen_arch_x86_time.c
@@ -1,3 +1,5 @@
+$NetBSD: patch-xen_arch_x86_time.c,v 1.3 2015/12/29 04:04:32 dholland Exp $
+
 --- xen/arch/x86/time.c.orig	2013-09-10 06:42:18.000000000 +0000
 +++ xen/arch/x86/time.c	2013-09-11 14:30:13.000000000 +0000
 @@ -105,7 +105,7 @@
