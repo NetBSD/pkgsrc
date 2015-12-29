6e8f2cd07a2327c21978306d5604bad86cb7de9b
dholland <dholland@pkgsrc.org>
2015-12-29 04:04:26 +0000

    Fix missing/broken rcsids.

--- comms/synce-rra/patches/patch-lib_syncmgr.c
+++ comms/synce-rra/patches/patch-lib_syncmgr.c
@@ -1,3 +1,5 @@
+$NetBSD: patch-lib_syncmgr.c,v 1.2 2015/12/29 04:04:27 dholland Exp $
+
 --- lib/syncmgr.c.orig	2005-07-08 21:25:22.000000000 +0200
 +++ lib/syncmgr.c	2012-01-26 21:40:31.623129670 +0100
 @@ -11,6 +11,10 @@
--- textproc/helpdeco/patches/patch-splitmrp.c
+++ textproc/helpdeco/patches/patch-splitmrp.c
@@ -1,3 +1,5 @@
+$NetBSD: patch-splitmrp.c,v 1.2 2015/12/29 04:04:32 dholland Exp $
+
 --- splitmrb.c.orig	2005-11-17 12:19:38.000000000 +0100
 +++ splitmrb.c	2012-03-02 13:05:31.593391213 +0100
 @@ -237,9 +237,9 @@ int GetPackedByte(FILE *f) // RulLen dec
