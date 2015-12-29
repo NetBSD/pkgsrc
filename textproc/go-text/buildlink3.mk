d5e121e45be58be31d988ea680be113e99ea9bbf
bsiegert <bsiegert@pkgsrc.org>
2015-12-29 21:47:48 +0000

    Real buildlink support for Go, hacked with wiz@.
    Go packages now define a set of files to buildlink in their buildlink3.mk.
    go-packages.mk no longer looks in ${PREFIX}/gopkg during the build. This
    should also fix the spurious issues with rebuilds of .a files during bulk
    builds of Go packages.

--- textproc/go-text/buildlink3.mk
+++ textproc/go-text/buildlink3.mk
@@ -1,11 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/03/10 19:57:18 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/12/29 21:47:48 bsiegert Exp $
 
 BUILDLINK_TREE+=	go-text
 
 .if !defined(GO_TEXT_BUILDLINK3_MK)
 GO_TEXT_BUILDLINK3_MK:=
 
+BUILDLINK_CONTENTS_FILTER.go-text= ${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-text?=	build
+
 BUILDLINK_API_DEPENDS.go-text+=	go-text>=20150310
 BUILDLINK_PKGSRCDIR.go-text?=	../../textproc/go-text
 .endif	# GO_TEXT_BUILDLINK3_MK
