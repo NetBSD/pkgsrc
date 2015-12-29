1e05bc9a329253b68a5bb7b6a7c575863e840796
bsiegert <bsiegert@pkgsrc.org>
2015-12-29 21:47:48 +0000

    Real buildlink support for Go, hacked with wiz@.
    Go packages now define a set of files to buildlink in their buildlink3.mk.
    go-packages.mk no longer looks in ${PREFIX}/gopkg during the build. This
    should also fix the spurious issues with rebuilds of .a files during bulk
    builds of Go packages.

--- security/go-oauth2/buildlink3.mk
+++ security/go-oauth2/buildlink3.mk
@@ -1,10 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/05/11 13:27:16 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/12/29 21:47:48 bsiegert Exp $
 
 BUILDLINK_TREE+=	go-oauth2
 
 .if !defined(GO_OAUTH2_BUILDLINK3_MK)
 GO_OAUTH2_BUILDLINK3_MK:=
 
+BUILDLINK_CONTENTS_FILTER.go-oauth2= ${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-oauth2?=	build
 BUILDLINK_API_DEPENDS.go-oauth2+=	go-oauth2>=20150310
 BUILDLINK_PKGSRCDIR.go-oauth2?=	../../security/go-oauth2
