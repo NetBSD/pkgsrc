3b7a1100ffc9606411ab00ed2e2a28f5f1b5c3d7
dholland <dholland@pkgsrc.org>
2015-12-29 04:54:34 +0000

    Whitespace.

--- audio/mp3blaster/options.mk
+++ audio/mp3blaster/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2012/04/14 10:12:01 hans Exp $
+# $NetBSD: options.mk,v 1.2 2015/12/29 04:54:35 dholland Exp $
 
-PKG_OPTIONS_VAR=        PKG_OPTIONS.mp3blaster
-PKG_SUPPORTED_OPTIONS=  esound oss sdl
-PKG_SUGGESTED_OPTIONS=  oss
+PKG_OPTIONS_VAR=	PKG_OPTIONS.mp3blaster
+PKG_SUPPORTED_OPTIONS=	esound oss sdl
+PKG_SUGGESTED_OPTIONS=	oss
 
 .if ${OPSYS} == "Darwin"
 PKG_SUGGESTED_OPTIONS=	sdl
@@ -11,16 +11,16 @@ PKG_SUGGESTED_OPTIONS=	sdl
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mesound)
-CONFIGURE_ARGS+=        --with-esd=yes
+CONFIGURE_ARGS+=	--with-esd=yes
 .include "../../audio/esound/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Moss)
-CONFIGURE_ARGS+=        --with-oss=yes
+CONFIGURE_ARGS+=	--with-oss=yes
 .include "../../mk/oss.buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Msdl)
-CONFIGURE_ARGS+=        --with-sdl=yes
+CONFIGURE_ARGS+=	--with-sdl=yes
 .include "../../devel/SDL/buildlink3.mk"
 .endif
--- x11/neXtaw/buildlink3.mk
+++ x11/neXtaw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2013/01/15 16:21:36 morr Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2015/12/29 04:54:39 dholland Exp $
 
 BUILDLINK_TREE+=	neXtaw
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.neXtaw+=	neXtaw>=0.15.1
 BUILDLINK_ABI_DEPENDS.neXtaw+=	neXtaw>=0.15.1nb2
 BUILDLINK_PKGSRCDIR.neXtaw?=	../../x11/neXtaw
 
-BUILDLINK_TRANSFORM+=           l:Xaw:neXtaw
+BUILDLINK_TRANSFORM+=		l:Xaw:neXtaw
 
 .include "../../mk/bsd.fast.prefs.mk"
 
