1726914d23d3e35083b461671f4cd5f1d5200a91
bsiegert <bsiegert@pkgsrc.org>
2015-12-29 21:47:48 +0000

    Real buildlink support for Go, hacked with wiz@.
    Go packages now define a set of files to buildlink in their buildlink3.mk.
    go-packages.mk no longer looks in ${PREFIX}/gopkg during the build. This
    should also fix the spurious issues with rebuilds of .a files during bulk
    builds of Go packages.

--- devel/google-api-go-client/buildlink3.mk
+++ devel/google-api-go-client/buildlink3.mk
@@ -1,10 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/05/11 14:28:43 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/12/29 21:47:48 bsiegert Exp $
 
 BUILDLINK_TREE+=	google-api-go-client
 
 .if !defined(GOOGLE_API_GO_CLIENT_BUILDLINK3_MK)
 GOOGLE_API_GO_CLIENT_BUILDLINK3_MK:=
 
+BUILDLINK_CONTENTS_FILTER.google-api-go-client=  ${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.google-api-go-client?=	build
 BUILDLINK_API_DEPENDS.google-api-go-client+=	google-api-go-client>=20150507
 BUILDLINK_PKGSRCDIR.google-api-go-client?=	../../devel/google-api-go-client
