2ea2213d83e76a0c7c66a5b93319c7d473649e9e
dholland <dholland@pkgsrc.org>
2015-12-29 04:22:50 +0000

    pkglint

--- editors/elvis/options.mk
+++ editors/elvis/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2011/12/19 15:27:47 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2015/12/29 04:22:50 dholland Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.elvis
 PKG_SUPPORTED_OPTIONS=		x11
@@ -6,15 +6,15 @@ PKG_SUPPORTED_OPTIONS=		x11
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mx11)
-BUILDLINK_DEPMETHOD.libXt?=	build
+BUILDLINK_DEPMETHOD.libXt=	build
 .  include "../../x11/libXft/buildlink3.mk"
 .  include "../../graphics/freetype2/buildlink3.mk"
 .  include "../../x11/libXpm/buildlink3.mk"
 .  include "../../x11/libXt/buildlink3.mk"
 CONFIGURE_ARGS+=		--with-x
 CONFIGURE_ARGS+=		--with-xft
-CONFIGURE_ARGS+=		--x-includes=${X11BASE:Q}/include
-CONFIGURE_ARGS+=		--x-libraries=${X11BASE:Q}/lib
+CONFIGURE_ARGS+=		--x-includes=${X11BASE}/include
+CONFIGURE_ARGS+=		--x-libraries=${X11BASE}/lib
 USE_TOOLS+=			pkg-config
 .else
 CONFIGURE_ARGS+=		--without-x
