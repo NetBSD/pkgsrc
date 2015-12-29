2191b73a15b1ab111143fddbe0e943e131209223
dholland <dholland>
2015-12-29 04:04:26 +0000

    Fix missing/broken rcsids.

--- sysutils/pciutils/patches/patch-lib_nbsd_libpci.c
+++ sysutils/pciutils/patches/patch-lib_nbsd_libpci.c
@@ -1,3 +1,5 @@
+$NetBSD: patch-lib_nbsd_libpci.c,v 1.2 2015/12/29 04:04:31 dholland Exp $
+
 --- lib/nbsd-libpci.c.orig	2011-01-08 06:04:28.000000000 +0900
 +++ lib/nbsd-libpci.c	2015-11-12 17:45:08.000000000 +0900
 @@ -71,7 +71,7 @@ nbsd_read(struct pci_dev *d, int pos, by
