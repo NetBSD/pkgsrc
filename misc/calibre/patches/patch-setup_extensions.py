02894e53e7ed84155cabc5096b8cf58f0af69bfe
wiz <wiz@pkgsrc.org>
2015-12-30 00:08:33 +0000

    Update calibre to 2.47.0:
    2.47
    
    New Features
    * A new tool to easily export and import all calibre data -- books,
    settings and plugins
    * Get Books: Add plugins for Amazon Australia and Amazon India.
    Also restore the Amazon EU plugins.
    
    Bug Fixes
    * PDF Input: Fix conversion of PDF documents that contain ASCII
    control codes in their outlines not working.
    * Edit book: Fix image compression utilities opening a new console
    per invocation on windows
    * Image compression: If the compression tools return a zero byte
    image ignore it and use the original image
    * Fix a regression that caused book titles in the Book Details
    panel to become clickable
    
    2.46
    
    New Features
    
        PDF Input: Add support for PDF outlines (bookmarks), if present, they are used as the metadata Table of Contents.
        Book polishing: Add tool to losslessly compress images in the book in order to reduce its filesize, without affecting image quality
        Edit Book: Add a new tool to compress images in the book losslessly, accessed from the Tools menu
        Kobo driver: add support for new Kobo firmware
        Bulk metadata edit dialog: Allow entering fractional numbers into the series number start with control.
        Speed up moving libraries by using hardlinks instead of file copies when moving to a location on the same filesystem
        Get Books: Disable the Amazon EU stores, at Amazon's request
    
    Bug Fixes
    
        Fix moving libraries via calibre leaving behind a copy of the metadata_db_prefs_backup.json file in the original library folder
        MOBI Input: Warn about corrupted trailing data entries, instead of aborting. Getting some, even partially corrupted text is better than no text.
        Book details: Fix single value custom text column not clickable.
        Saving to disk: Fix custom date column being rendered in GMT instead of the local time zone when used in a save to disk template.

--- misc/calibre/patches/patch-setup_extensions.py
+++ misc/calibre/patches/patch-setup_extensions.py
@@ -1,12 +1,12 @@
-$NetBSD: patch-setup_extensions.py,v 1.5 2015/02/08 00:37:10 wiz Exp $
+$NetBSD: patch-setup_extensions.py,v 1.6 2015/12/30 00:08:33 wiz Exp $
 
 Fix build for pictureflow.
 
---- setup/extensions.py.orig	2014-08-29 03:59:29.000000000 +0000
+--- setup/extensions.py.orig	2015-12-21 03:57:33.000000000 +0000
 +++ setup/extensions.py
-@@ -602,7 +602,7 @@ class Build(Command):
+@@ -636,7 +636,7 @@ class Build(Command):
          if iswindows:
-             qmc += ['-spec', 'win32-msvc2008']
+             qmc += ['-spec', qmakespec]
          fext = 'dll' if iswindows else 'dylib' if isosx else 'so'
 -        name = '%s%s.%s' % ('release/' if iswindows else 'lib', sip['target'], fext)
 +        name = '%s%s.%s' % ('release/' if iswindows else '.libs/lib', sip['target'], fext)
