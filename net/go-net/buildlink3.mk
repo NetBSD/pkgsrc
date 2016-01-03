4e710b916fbdaa63b68fef85f76b7df52f6142fb
bsiegert <bsiegert@pkgsrc.org>
2016-01-03 20:22:53 +0000

    Update go-net to 20151229. Instead of boring commitlogs, my summary of changes is:
    - http2 package replaces spdy. New interactive HTTP2 debugger, h2i.
    - New context/ctxhttp for context-aware HTTP request handlers.
    - New xsrftoken package for generating and checking XSRF tokens.
    - Improved HTML5-capable HTML parser.

--- net/go-net/buildlink3.mk
+++ net/go-net/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/12/29 21:47:48 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/01/03 20:22:53 bsiegert Exp $
 
 BUILDLINK_TREE+=	go-net
 
@@ -12,6 +12,7 @@ BUILDLINK_API_DEPENDS.go-net+=	go-net>=20150310
 BUILDLINK_PKGSRCDIR.go-net?=	../../net/go-net
 
 .include "../../textproc/go-text/buildlink3.mk"
+.include "../../security/go-crypto/buildlink3.mk"
 .endif	# GO_NET_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-go-net
