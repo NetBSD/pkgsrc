5600175b46ab70cfb713663cf6139e7b75e4f7bd
szptvlfn <szptvlfn@pkgsrc.org>
2016-01-04 12:45:36 +0000

    Update to 0.12
    CHANGES:
    ## 0.12 (December 14, 2015)
    
      * Compiles with GHC 7.10.2, 7.8.4, and 7.6.3
    
      * Use of [data-default][] allows using `def` where previously you
        had to write `defaultConfig`, `defaultXPConfig`, etc.
    
      * The [setlocale][] package is now used instead of a binding shipped
        with xmonad proper allowing the use of `Main.hs` instead of
        `Main.hsc`
    
      * No longer encodes paths for `spawnPID`
    
      * The default `manageHook` no longer floats Gimp windows
    
      * Doesn't crash when there are fewer workspaces than screens
    
      * `Query` is now an instance of `Applicative`
    
      * Various improvements to the example configuration file
    
    [data-default]: http://hackage.haskell.org/package/data-default
    [setlocale]: https://hackage.haskell.org/package/setlocale

--- wm/xmonad/buildlink3.mk
+++ wm/xmonad/buildlink3.mk
@@ -1,17 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.4 2015/05/31 12:59:15 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2016/01/04 12:45:36 szptvlfn Exp $
 
 BUILDLINK_TREE+=	xmonad
 
 .if !defined(XMONAD_BUILDLINK3_MK)
 XMONAD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xmonad+=	xmonad>=0.11
-BUILDLINK_ABI_DEPENDS.xmonad+=	xmonad>=0.11nb4
+BUILDLINK_API_DEPENDS.xmonad+=	xmonad>=0.12
+BUILDLINK_ABI_DEPENDS.xmonad+=	xmonad>=0.12
 BUILDLINK_PKGSRCDIR.xmonad?=	../../wm/xmonad
 
 .include "../../x11/libX11/buildlink3.mk"
+.include "../../devel/hs-data-default/buildlink3.mk"
 .include "../../devel/hs-extensible-exceptions/buildlink3.mk"
 .include "../../devel/hs-mtl/buildlink3.mk"
+.include "../../devel/hs-setlocale/buildlink3.mk"
 .include "../../devel/hs-utf8-string/buildlink3.mk"
 .include "../../x11/hs-X11/buildlink3.mk"
 .endif	# XMONAD_BUILDLINK3_MK
