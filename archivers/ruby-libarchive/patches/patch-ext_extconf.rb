9e6719ea0ca3506b38cd665f9e396af09e937388
taca <taca@pkgsrc.org>
2016-01-04 02:48:05 +0000

    Fix "work-directory references" problem another way.

---
+++ archivers/ruby-libarchive/patches/patch-ext_extconf.rb
@@ -0,0 +1,12 @@
+$NetBSD: patch-ext_extconf.rb,v 1.1 2016/01/04 02:48:05 taca Exp $
+
+* Avoid to index by rdoc.
+
+--- ext/extconf.rb.orig	2016-01-04 02:44:40.000000000 +0000
++++ ext/extconf.rb
+@@ -7,4 +7,5 @@ if system('/bin/sh configure') and have_
+   have_header('bzlib.h')
+ 
+   create_makefile('rblibarchive')
++  FileUtils.rm_f %w(config.log config.status)
+ end
