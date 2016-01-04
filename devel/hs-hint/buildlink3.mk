98acd5f0fc98e23839434c8cbab535b35f0145d4
szptvlfn <szptvlfn@pkgsrc.org>
2016-01-04 12:23:05 +0000

    Update to 0.4.2.3
    CHANGELOG:
    0.4.2.3
    -------
    
     * It builds against 7.4.2 (not 7.4.1), so update the constraints.
    
    0.4.2.2
    -------
    
     * Builds with ghc 7.10
     * Builds again with ghc 7.4
     * Drops dependency on utf8-string
    
    0.4.2.1
    -------
    
     * Better error reporting (thanks to Corentin Dupont)

--- devel/hs-hint/buildlink3.mk
+++ devel/hs-hint/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/11/23 12:52:32 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/01/04 12:23:05 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-hint
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-hint
 HS_HINT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-hint+=	hs-hint>=0.4.2
-BUILDLINK_ABI_DEPENDS.hs-hint+=	hs-hint>=0.4.2.0nb2
+BUILDLINK_ABI_DEPENDS.hs-hint+=	hs-hint>=0.4.2.3
 BUILDLINK_PKGSRCDIR.hs-hint?=	../../devel/hs-hint
 
 .include "../../devel/hs-exceptions/buildlink3.mk"
@@ -15,7 +15,6 @@ BUILDLINK_PKGSRCDIR.hs-hint?=	../../devel/hs-hint
 .include "../../devel/hs-ghc-paths/buildlink3.mk"
 .include "../../devel/hs-mtl/buildlink3.mk"
 .include "../../devel/hs-random/buildlink3.mk"
-.include "../../devel/hs-utf8-string/buildlink3.mk"
 .endif	# HS_HINT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-hint
