97e03cf57e9863c3d626c22536deb840586a746f
szptvlfn <szptvlfn@pkgsrc.org>
2016-01-04 12:22:20 +0000

    Update to 1.0.1.1
    CHANGELOG:
    1.0.1.1
    -----
    * Build correctly on GHC-7.0 (#14)
    
    1.0.1
    -----
    * Improve the performance of Data.ByteString.Lazy.UTF8.fromString. (Thanks, ndmitchell)
    
    1
    -----
    * Remove out all the old utf8 IO support. GHC supports utf8 now.

--- devel/hs-utf8-string/buildlink3.mk
+++ devel/hs-utf8-string/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/08/29 14:08:40 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/01/04 12:22:20 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-utf8-string
 
 .if !defined(HS_UTF8_STRING_BUILDLINK3_MK)
 HS_UTF8_STRING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-utf8-string+=	hs-utf8-string>=0.3.8
-BUILDLINK_ABI_DEPENDS.hs-utf8-string+=	hs-utf8-string>=0.3.8
+BUILDLINK_API_DEPENDS.hs-utf8-string+=	hs-utf8-string>=1.0.1
+BUILDLINK_ABI_DEPENDS.hs-utf8-string+=	hs-utf8-string>=1.0.1.1
 BUILDLINK_PKGSRCDIR.hs-utf8-string?=	../../devel/hs-utf8-string
 .endif	# HS_UTF8_STRING_BUILDLINK3_MK
 
