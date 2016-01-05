b6b7c1f2de85f41c74c9eb6ceb79a59fd799c970
tnn <tnn@pkgsrc.org>
2016-01-05 13:02:57 +0000

    Update to mesa-11.1.0 (2015-12-15)
    New features:
    OpenGL 3.1 support on freedreno (a3xx, a4xx)
    OpenGL 3.3 support for VMware guest VM driver
      (supported by Workstation 12 and Fusion 8).
    GL_AMD_performance_monitor on nv50
    GL_ARB_arrays_of_arrays on i965
    GL_ARB_blend_func_extended on freedreno (a3xx)
    GL_ARB_clear_texture on nv50, nvc0
    GL_ARB_clip_control on freedreno/a4xx
    GL_ARB_copy_image on nv50, nvc0, radeonsi
    GL_ARB_depth_clamp on freedreno/a4xx
    GL_ARB_fragment_layer_viewport on i965 (gen6+)
    GL_ARB_gpu_shader_fp64 on r600 for Cypress/Cayman/Aruba chips
    GL_ARB_gpu_shader5 on r600 for Evergreen and later chips
    GL_ARB_seamless_cubemap_per_texture on freedreno/a4xx
    GL_ARB_shader_clock on i965 (gen7+)
    GL_ARB_shader_stencil_export on i965 (gen9+)
    GL_ARB_shader_storage_buffer_object on i965
    GL_ARB_shader_texture_image_samples on i965, nv50, nvc0, r600, radeonsi
    GL_ARB_texture_barrier / GL_NV_texture_barrier on i965
    GL_ARB_texture_buffer_range on freedreno/a3xx
    GL_ARB_texture_compression_bptc on freedreno/a4xx
    GL_ARB_texture_query_lod on softpipe
    GL_ARB_texture_view on radeonsi and r600 (for evergeen and newer)
    GL_ARB_vertex_type_2_10_10_10_rev on freedreno (a3xx, a4xx)
    GL_EXT_blend_func_extended on all drivers that support the ARB version
    GL_EXT_buffer_storage implemented for when ES 3.1 support is gained
    GL_EXT_draw_elements_base_vertex on all drivers
    GL_EXT_texture_compression_rgtc / latc on freedreno (a3xx & a4xx)
    GL_KHR_debug (GLES)
    GL_NV_conditional_render on freedreno
    GL_OES_draw_elements_base_vertex on all drivers
    EGL_KHR_create_context on softpipe, llvmpipe
    EGL_KHR_gl_colorspace on softpipe, llvmpipe
    new virgl gallium driver for qemu virtio-gpu
    16x multisampling on i965 (gen9+)
    GL_EXT_shader_samples_identical on i965.

--- graphics/MesaLib/patches/patch-src_egl_drivers_dri2_platform__drm.c
+++ graphics/MesaLib/patches/patch-src_egl_drivers_dri2_platform__drm.c
@@ -1,16 +1,16 @@
-$NetBSD: patch-src_egl_drivers_dri2_platform__drm.c,v 1.1 2015/09/26 11:01:13 tnn Exp $
+$NetBSD: patch-src_egl_drivers_dri2_platform__drm.c,v 1.2 2016/01/05 13:02:57 tnn Exp $
 
 netbsd-5 build fix
 
---- src/egl/drivers/dri2/platform_drm.c.orig	2015-09-10 18:07:21.000000000 +0000
+--- src/egl/drivers/dri2/platform_drm.c.orig	2015-12-09 16:10:13.000000000 +0000
 +++ src/egl/drivers/dri2/platform_drm.c
-@@ -638,7 +638,9 @@ dri2_initialize_drm(_EGLDriver *drv, _EG
-    }
- 
-    if (fd < 0) {
+@@ -626,7 +626,9 @@ dri2_initialize_drm(_EGLDriver *drv, _EG
+       if (gbm == NULL)
+          goto cleanup;
+    } else {
 +#ifdef F_DUPFD_CLOEXEC
        fd = fcntl(gbm_device_get_fd(gbm), F_DUPFD_CLOEXEC, 3);
 +#endif
-       if (fd < 0) {
-          free(dri2_dpy);
-          return EGL_FALSE;
+       if (fd < 0)
+          goto cleanup;
+    }
