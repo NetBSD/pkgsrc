baac0dbc6fc0b74c7497c8b090b7b318ec8177bb
joerg <joerg@pkgsrc.org>
2016-01-07 09:08:08 +0000

    Place the upper limit on the ABI dependency as well. This means bin-install will pick up the right fltk version when both are present as binary package.

--- x11/fltk/buildlink3.mk
+++ x11/fltk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.27 2015/04/25 14:25:03 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2016/01/07 09:08:08 joerg Exp $
 
 BUILDLINK_TREE+=	fltk
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	fltk
 FLTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.fltk+=	fltk>=1.1.5rc1<1.3
-BUILDLINK_ABI_DEPENDS.fltk?=	fltk>=1.1.10nb7
+BUILDLINK_ABI_DEPENDS.fltk?=	fltk>=1.1.10nb7<1.3
 BUILDLINK_PKGSRCDIR.fltk?=	../../x11/fltk
 BUILDLINK_FILES.fltk+=		include/Fl/*
 
