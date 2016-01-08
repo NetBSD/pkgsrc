1de6b382a6ef7a50f458925cd25e365fa4dc4c02
taca <taca@pkgsrc.org>
2016-01-08 03:27:23 +0000

    Update php55 to 5.5.31, security fix.
    07 Jan 2015, PHP 5.5.31
    
    - FPM:
      . Fixed bug #70755 (fpm_log.c memory leak and buffer overflow). (Stas)
    
    - GD:
      . Fixed bug #70976 (Memory Read via gdImageRotateInterpolated Array Index
        Out of Bounds). (emmanuel dot law at gmail dot com).
    
    - WDDX:
      . Fixed bug #70661 (Use After Free Vulnerability in WDDX Packet Deserialization).
        (taoguangchen at icloud dot com)
      . Fixed bug #70741 (Session WDDX Packet Deserialization Type Confusion
        Vulnerability). (taoguangchen at icloud dot com)
    
    - XMLRPC:
      . Fixed bug #70728 (Type Confusion Vulnerability in PHP_to_XMLRPC_worker()).
        (Julien)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.119 2015/12/18 14:58:14 taca Exp $
+# $NetBSD: phpversion.mk,v 1.120 2016/01/08 03:27:23 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -81,7 +81,7 @@
 PHPVERSION_MK=	defined
 
 # Define each PHP's version.
-PHP55_VERSION=	5.5.30
+PHP55_VERSION=	5.5.31
 PHP56_VERSION=	5.6.16
 PHP70_VERSION=	7.0.1
 
