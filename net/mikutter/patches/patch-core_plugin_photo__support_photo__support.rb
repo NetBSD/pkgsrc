236fcd941dbac95cd1451d2c273946cc409e2fbb
tsutsui <tsutsui@pkgsrc.org>
2016-01-09 19:36:12 +0000

    Update mikutter to 3.3.3.
    #mikutter 3.3.3
    
    * update language po files
    * fix of "crash on click of setting button" was missed
    
    #mikutter 3.3.2
    
    * Happy new year
    * several crash issue
    * crash on click of setting button on certain condition
    * avoid use of lacacy methods deprecated by Ruby 2.3
    
    #mikutter 3.3.1
    
    * crash on UserStream process in some case
    * crash on adding list
    
    #mikutter 3.3.0
    
    * use external libraries
     * Delayer-Deferred
     * Pluggaloid
    * retweeted retweet
    * liked retweet
    * retweet with comments
    * show icons for protected accounts
    * add settings to show icons for verified accounts
    * change method of counting a number of chars, to reflect URL conversion
    * improvements of daemon mode
    * notice function
    * imrovements of support of some image services

---
+++ net/mikutter/patches/patch-core_plugin_photo__support_photo__support.rb
@@ -0,0 +1,33 @@
+$NetBSD: patch-core_plugin_photo__support_photo__support.rb,v 1.1 2016/01/09 19:36:12 tsutsui Exp $
+
+- temporary workaround to skip totorijp gems (sorry, @toshi_a)
+
+--- core/plugin/photo_support/photo_support.rb.orig	2015-12-27 21:42:08.000000000 +0000
++++ core/plugin/photo_support/photo_support.rb
+@@ -1,7 +1,7 @@
+ # coding: utf-8
+ require 'nokogiri'
+ require 'httpclient'
+-require 'totoridipjp'
++#require 'totoridipjp'
+ 
+ module Plugin::PhotoSupport
+   INSTAGRAM_PATTERN = %r{^https?://(?:instagr\.am|instagram\.com)/p/([a-zA-Z0-9_\-]+)}
+@@ -178,9 +178,14 @@ Plugin.create :photo_support do
+ 
+   # totori.dip.jp
+   defimageopener('totori.dip.jp', %r#\Ahttp://totori\.dip\.jp/?\Z#) do |display_url|
+-    iwashi = Totoridipjp.イワシがいっぱいだあ…ちょっとだけもらっていこうかな
+-    if iwashi.url
+-      open(iwashi.url) end
++#    iwashi = Totoridipjp.イワシがいっぱいだあ…ちょっとだけもらっていこうかな
++#    if iwashi.url
++#      open(iwashi.url) end
++    connection = HTTPClient.new
++    page = connection.get_content(display_url)
++    next nil if page.empty?
++    doc = Nokogiri::HTML(page)
++    open(doc.css('meta[property="og:image"]').first.attribute('content'))
+   end
+ 
+   # 600eur.gochiusa.net
