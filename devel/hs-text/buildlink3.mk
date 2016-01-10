bb4c2d7a0279fb55004f36ce4332515ec1d76452
szptvlfn <szptvlfn@pkgsrc.org>
2016-01-10 11:48:07 +0000

    Update to 1.2.2.0
    changelog:
    1.2.2.0
    
    * The `integer-simple` package, upon which this package optionally
      depended, has been replaced with `integer-pure`.  The build flag has
      been renamed accordingly.
    
    * Bug fix: For the `Binary` instance, If UTF-8 decoding fails during a
      `get`, the error is propagated via `fail` instead of an uncatchable
      crash.
    
    * New function: `takeWhileEnd`
    
    * New instances for the `Text` types:
        * if `base` >= 4.7: `PrintfArg`
        * if `base` >= 4.9: `Semigroup`

--- devel/hs-text/buildlink3.mk
+++ devel/hs-text/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2015/08/02 11:26:15 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2016/01/10 11:48:07 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-text
 
 .if !defined(HS_TEXT_BUILDLINK3_MK)
 HS_TEXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=1.2.1
-BUILDLINK_ABI_DEPENDS.hs-text+=	hs-text>=1.2.1.3
+BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=1.2.2
+BUILDLINK_ABI_DEPENDS.hs-text+=	hs-text>=1.2.2.0
 BUILDLINK_PKGSRCDIR.hs-text?=	../../devel/hs-text
 .endif	# HS_TEXT_BUILDLINK3_MK
 
