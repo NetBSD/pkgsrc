ef65f03ea8f1835cdfbe83af5c24efeddd1f8dac
szptvlfn <szptvlfn@pkgsrc.org>
2016-01-10 11:49:09 +0000

    Bump PKGREVISION for hs-text-1.2.2.0 || hs-hashable-1.2.3.3

--- math/hs-nats/buildlink3.mk
+++ math/hs-nats/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2015/08/02 11:27:05 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2016/01/10 11:49:10 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-nats
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-nats
 HS_NATS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-nats+=	hs-nats>=0.2
-BUILDLINK_ABI_DEPENDS.hs-nats+=	hs-nats>=0.2nb7
+BUILDLINK_ABI_DEPENDS.hs-nats+=	hs-nats>=0.2nb8
 BUILDLINK_PKGSRCDIR.hs-nats?=	../../math/hs-nats
 
 .include "../../devel/hs-hashable/buildlink3.mk"
--- net/hs-network/buildlink3.mk
+++ net/hs-network/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2015/08/02 11:27:05 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2016/01/10 11:49:10 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-network
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-network
 HS_NETWORK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=2.5.0
-BUILDLINK_ABI_DEPENDS.hs-network+=	hs-network>=2.5.0.0nb10
+BUILDLINK_ABI_DEPENDS.hs-network+=	hs-network>=2.5.0.0nb11
 BUILDLINK_PKGSRCDIR.hs-network?=	../../net/hs-network
 
 .include "../../textproc/hs-parsec/buildlink3.mk"
