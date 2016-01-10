c6604daa03ba07366b3c041c550fbd9a24132652
szptvlfn <szptvlfn@pkgsrc.org>
2016-01-10 11:49:09 +0000

    Bump PKGREVISION for hs-text-1.2.2.0 || hs-hashable-1.2.3.3

--- devel/hs-blaze-builder/buildlink3.mk
+++ devel/hs-blaze-builder/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2015/08/02 11:27:04 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2016/01/10 11:49:09 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-blaze-builder
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-blaze-builder
 HS_BLAZE_BUILDER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.3.3
-BUILDLINK_ABI_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.3.3.3nb4
+BUILDLINK_ABI_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.3.3.3nb5
 BUILDLINK_PKGSRCDIR.hs-blaze-builder?=	../../devel/hs-blaze-builder
 
 .include "../../devel/hs-text/buildlink3.mk"
