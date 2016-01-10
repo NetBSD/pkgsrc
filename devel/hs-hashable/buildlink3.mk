522e15aac478e8791c7e507642f461ea1ffde564
szptvlfn <szptvlfn@pkgsrc.org>
2016-01-10 11:48:24 +0000

    Update to 1.2.3.3
    CHANGES:
    ## Version 1.2.3.3
    
     * Support integer-simple.

--- devel/hs-hashable/buildlink3.mk
+++ devel/hs-hashable/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2015/08/02 11:27:04 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2016/01/10 11:48:24 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-hashable
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-hashable
 HS_HASHABLE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.2.3
-BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.2.3.2nb1
+BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.2.3.3
 BUILDLINK_PKGSRCDIR.hs-hashable?=	../../devel/hs-hashable
 
 .include "../../devel/hs-text/buildlink3.mk"
