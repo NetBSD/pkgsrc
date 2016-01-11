f85a375c50ef6c09ec0d8f49aaa95bde6b849784
wiz <wiz@pkgsrc.org>
2016-01-11 23:46:50 +0000

    Import ruby-cairo-1.14.3 as graphics/ruby-cairo, updating ruby-rcairo.
    Cairo is a 2D graphics library with support for multiple output
    devices.  rcairo provides ruby bindings to cairo.

---
+++ graphics/ruby-cairo/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/01/11 23:46:50 wiz Exp $
+
+BUILDLINK_TREE+=	ruby-cairo
+
+.if !defined(RUBY_CAIRO_BUILDLINK3_MK)
+RUBY_CAIRO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ruby-cairo+=	${RUBY_PKGPREFIX}-cairo>=1.6.0
+BUILDLINK_ABI_DEPENDS.ruby-cairo+=	${RUBY_PKGPREFIX}-cairo>=1.14.1nb1
+BUILDLINK_PKGSRCDIR.ruby-cairo?=	../../graphics/ruby-cairo
+
+.include "../../graphics/cairo/buildlink3.mk"
+.endif # RUBY_CAIRO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ruby-cairo
