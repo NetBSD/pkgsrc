19934f0be741f69b8820bf91e017aac69afd3c37
leot <leot@pkgsrc.org>
2016-01-16 17:04:32 +0000

    Update multimedia/ffmpeg2 to 2.8.5.
    Changes:
    version 2.8.5
    - avformat/hls: Even stricter URL checks
    - avformat/hls: More strict url checks
    - avcodec/pngenc: Fix mixed up linesizes
    - avcodec/pngenc: Replace memcpy by av_image_copy()
    - swscale/vscale: Check that 2 tap filters are bilinear before using bilinear code
    - swscale: Move VScalerContext into vscale.c
    - swscale/utils: Detect and skip unneeded sws_setColorspaceDetails() calls
    - swscale/yuv2rgb: Increase YUV2RGB table headroom
    - swscale/yuv2rgb: Factor YUVRGB_TABLE_LUMA_HEADROOM out
    - avformat/hls: forbid all protocols except http(s) & file
    - avformat/aviobuf: Fix end check in put_str16()
    - avformat/asfenc: Check pts
    - avcodec/mpeg4video: Check time_incr
    - avcodec/wavpackenc: Check the number of channels
    - avcodec/wavpackenc: Headers are per channel
    - avcodec/aacdec_template: Check id_map
    - avcodec/dvdec: Fix "left shift of negative value -254"
    - avcodec/g2meet: Check for ff_els_decode_bit() failure in epic_decode_run_length()
    - avcodec/mjpegdec: Fix negative shift
    - avcodec/mss2: Check for repeat overflow
    - avformat: Add integer fps from 31 to 60 to get_std_framerate()
    - avformat/ivfenc: fix division by zero
    - avcodec/mpegvideo_enc: Clip bits_per_raw_sample within valid range
    - avfilter/vf_scale: set proper out frame color range
    - avcodec/motion_est: Fix mv_penalty table size
    - avcodec/h264_slice: Fix integer overflow in implicit weight computation
    - swscale/utils: Use normal bilinear scaler if fast cannot be used due to tiny dimensions
    - avcodec/put_bits: Always check buffer end before writing
    - mjpegdec: extend check for incompatible values of s->rgb and s->ls
    - swscale/utils: Fix intermediate format for cascaded alpha downscaling
    - avformat/mov: Update handbrake_version threshold for full mp3 parsing
    - x86/float_dsp: zero extend offset from ff_scalarproduct_float_sse
    - avfilter/vf_zoompan: do not free frame we pushed to lavfi
    - nuv: sanitize negative fps rate
    - nutdec: reject negative value_len in read_sm_data
    - xwddec: prevent overflow of lsize * avctx->height
    - nutdec: only copy the header if it exists
    - exr: fix out of bounds read in get_code
    - on2avc: limit number of bits to 30 in get_egolomb

--- multimedia/ffmpeg2/patches/patch-libavutil_x86_asm.h
+++ multimedia/ffmpeg2/patches/patch-libavutil_x86_asm.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-libavutil_x86_asm.h,v 1.1 2014/08/30 11:22:03 richard Exp $
+$NetBSD: patch-libavutil_x86_asm.h,v 1.2 2016/01/16 17:04:33 leot Exp $
 
 avoid SunOS regset.h definition for REG_SP
 
---- libavutil/x86/asm.h.orig	2014-08-17 13:33:17.000000000 +0000
+--- libavutil/x86/asm.h.orig	2016-01-15 16:58:37.000000000 +0000
 +++ libavutil/x86/asm.h
 @@ -27,6 +27,11 @@
  typedef struct xmm_reg { uint64_t a, b; } xmm_reg;
