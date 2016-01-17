fac0f4fd775b232102d431036067b42cc04e2edc
wiz <wiz>
2016-01-17 15:21:34 +0000

    Fix path.

--- fonts/SDL2_ttf/buildlink3.mk
+++ fonts/SDL2_ttf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2016/01/17 14:07:37 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2016/01/17 15:21:34 wiz Exp $
 
 BUILDLINK_TREE+=	SDL2_ttf
 
@@ -7,7 +7,7 @@ SDL2_TTF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.SDL2_ttf+=	SDL2_ttf>=2.0.12
 BUILDLINK_ABI_DEPENDS.SDL2_ttf+=	SDL2_ttf>=2.0.12nb2
-BUILDLINK_PKGSRCDIR.SDL2_ttf?=	../../devel/SDL2_ttf
+BUILDLINK_PKGSRCDIR.SDL2_ttf?=	../../fonts/SDL2_ttf
 
 .include "../../devel/SDL2/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
