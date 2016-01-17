4b3bf75318a65fda53cb591ebb294f5f3a61decf
wiz <wiz@pkgsrc.org>
2016-01-17 14:07:37 +0000

    Merge updated version from devel/SDL2_ttf into this.

--- fonts/SDL2_ttf/buildlink3.mk
+++ fonts/SDL2_ttf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2015/04/25 14:20:48 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/01/17 14:07:37 wiz Exp $
 
 BUILDLINK_TREE+=	SDL2_ttf
 
@@ -6,11 +6,12 @@ BUILDLINK_TREE+=	SDL2_ttf
 SDL2_TTF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.SDL2_ttf+=	SDL2_ttf>=2.0.12
-BUILDLINK_ABI_DEPENDS.SDL2_ttf?=	SDL2_ttf>=2.0.12nb2
-BUILDLINK_PKGSRCDIR.SDL2_ttf?=	../../fonts/SDL2_ttf
+BUILDLINK_ABI_DEPENDS.SDL2_ttf+=	SDL2_ttf>=2.0.12nb2
+BUILDLINK_PKGSRCDIR.SDL2_ttf?=	../../devel/SDL2_ttf
 
-.include "../../graphics/freetype2/buildlink3.mk"
 .include "../../devel/SDL2/buildlink3.mk"
-.endif	# SDL2_TTF_BUILDLINK3_MK
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.endif # SDL2_TTF_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-SDL2_ttf
