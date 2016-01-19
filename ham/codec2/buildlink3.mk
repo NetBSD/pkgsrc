db78ef66b64829bbf1aea32bcdf18357c1dfec88
dbj <dbj@pkgsrc.org>
2016-01-19 04:02:07 +0000

    update to codec2-0.5

--- ham/codec2/buildlink3.mk
+++ ham/codec2/buildlink3.mk
@@ -1,12 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/07/21 07:30:50 dbj Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/01/19 04:02:07 dbj Exp $
 
 BUILDLINK_TREE+=	codec2
 
 .if !defined(CODEC2_BUILDLINK3_MK)
 CODEC2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.codec2+=	codec2>=0.3.0.1698
+BUILDLINK_API_DEPENDS.codec2+=	codec2>=0.5
 BUILDLINK_PKGSRCDIR.codec2?=	../../ham/codec2
+
+BUILDLINK_INCDIRS.codec2+=	include/codec2
+
 .endif	# CODEC2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-codec2
