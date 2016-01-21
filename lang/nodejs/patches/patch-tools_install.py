761cbaf6b8314e0f0ff0882e58841796a03b5281
fhajny <fhajny@pkgsrc.org>
2016-01-21 11:45:30 +0000

    Update lang/nodejs to 5.5.0.
    * events: make sure console functions exist (Dave) #4479
    * fs: add autoClose option to fs.createWriteStream (Saquib) #3679
    * http: improves expect header handling (Daniel Sellers) #4501
    * node: allow preload modules with -i (Evan Lucas) #4696
    * v8,src: expose statistics about heap spaces (v8.getHeapSpaceStatistics())
      (Ben Ripkens) #4463
    * Minor performance improvements:
      - lib: Use arrow functions instead of bind where possible (Minwoo Jung)
        #3622.
      - module: cache stat() results more aggressively (Ben Noordhuis) #4575
      - querystring: improve parse() performance (Brian White) #4675

--- lang/nodejs/patches/patch-tools_install.py
+++ lang/nodejs/patches/patch-tools_install.py
@@ -1,18 +1,18 @@
-$NetBSD: patch-tools_install.py,v 1.3 2015/09/09 00:04:54 ryoon Exp $
+$NetBSD: patch-tools_install.py,v 1.4 2016/01/21 11:45:30 fhajny Exp $
 
 Install man pages under the right directory.
 
---- tools/install.py.orig	2015-09-08 15:30:49.000000000 +0000
+--- tools/install.py.orig	2016-01-21 01:08:27.000000000 +0000
 +++ tools/install.py
-@@ -141,9 +141,9 @@ def files(action):
+@@ -141,10 +141,7 @@ def files(action):
+ 
    action(['deps/v8/tools/gdbinit'], 'share/doc/node/')
  
-   if 'freebsd' in sys.platform or 'openbsd' in sys.platform:
+-  if 'freebsd' in sys.platform or 'openbsd' in sys.platform:
 -    action(['doc/node.1'], 'man/man1/')
-+    action(['doc/node.1'], '@PKGMANDIR@/man1/')
-   else:
+-  else:
 -    action(['doc/node.1'], 'share/man/man1/')
-+    action(['doc/node.1'], '@PKGMANDIR@/man1/')
++  action(['doc/node.1'], os.environ.get('PKGMANDIR') + '/man1/')
  
    if 'true' == variables.get('node_install_npm'): npm_files(action)
  
