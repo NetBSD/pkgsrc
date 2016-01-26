93106cc7656742328c1da544e04c41084413b122
dbj <dbj@pkgsrc.org>
2016-01-26 09:10:39 +0000

    install pkg-config support remove accidental stdout printf fix issue with canceling with signals

--- ham/rtl-sdr/buildlink3.mk
+++ ham/rtl-sdr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/10/13 13:05:36 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/01/26 09:10:39 dbj Exp $
 
 BUILDLINK_TREE+=	rtl-sdr
 
@@ -6,7 +6,6 @@ BUILDLINK_TREE+=	rtl-sdr
 RTL_SDR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.rtl-sdr+=	rtl-sdr>=0.5.3
-BUILDLINK_ABI_DEPENDS.rtl-sdr+=	rtl-sdr>=0.5.3
 BUILDLINK_PKGSRCDIR.rtl-sdr?=	../../ham/rtl-sdr
 
 .endif # RTL_SDR_BUILDLINK3_MK
