825896829da5236d63c7f754c4f5408874c3b747
dbj <dbj@pkgsrc.org>
2016-01-26 08:47:28 +0000

    disable fl_update_focus calls on WIN32 and __APPLE_ since they are not defined there

--- x11/fltk13/patches/patch-src_Fl.cxx
+++ x11/fltk13/patches/patch-src_Fl.cxx
@@ -1,24 +1,28 @@
-$NetBSD: patch-src_Fl.cxx,v 1.1 2015/07/21 21:36:41 markd Exp $
+$NetBSD: patch-src_Fl.cxx,v 1.2 2016/01/26 08:47:28 dbj Exp $
 
 part of tigervnc.patch from archlinux package. 
 
 --- src/Fl.cxx.orig	2014-10-08 12:47:20.000000000 +0000
 +++ src/Fl.cxx
-@@ -70,6 +70,8 @@ void fl_cleanup_dc_list(void);
+@@ -70,6 +70,10 @@ void fl_cleanup_dc_list(void);
  extern double fl_mac_flush_and_wait(double time_to_wait);
  #endif // WIN32
  
++#if !defined(WIN32) && !defined(__APPLE__)
 +extern void fl_update_focus(void);
++#endif
 +
  //
  // Globals...
  //
-@@ -982,6 +984,8 @@ void Fl::focus(Fl_Widget *o) {
+@@ -982,6 +986,10 @@ void Fl::focus(Fl_Widget *o) {
        fl_oldfocus = p;
      }
      e_number = old_event;
++#if !defined(WIN32) && !defined(__APPLE__)
 +    // let the platform code do what it needs
 +    fl_update_focus();
++#endif
    }
  }
  
