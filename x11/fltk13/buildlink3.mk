0bb2fa08b7cf22075501c5a2d8c414120cde2eb8
dbj <dbj@pkgsrc.org>
2016-01-26 08:47:28 +0000

    disable fl_update_focus calls on WIN32 and __APPLE_ since they are not defined there

--- x11/fltk13/buildlink3.mk
+++ x11/fltk13/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2015/04/25 14:25:04 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2016/01/26 08:47:28 dbj Exp $
 
 BUILDLINK_TREE+=	fltk
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	fltk
 FLTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.fltk+=	fltk>=1.3.0
-BUILDLINK_ABI_DEPENDS.fltk?=	fltk>=1.3.2nb5
+BUILDLINK_ABI_DEPENDS.fltk?=	fltk>=1.3.3nb1
 BUILDLINK_PKGSRCDIR.fltk?=	../../x11/fltk13
 BUILDLINK_FILES.fltk+=		include/Fl/*
 
