350389d76d7c4b099e0f589a4efbcee09e51a759
fhajny <fhajny@pkgsrc.org>
2016-01-26 15:20:33 +0000

    Update sysutils/rsyslog to 8.16.0.
    Version 8.16.0 [v8-stable] 2016-01-26
    - rsgtutil: Added extraction support including loglines and hash chains.
    - clean up doAction output module interface
    - new system properties for $NOW properties based on UTC
    - impstats: support broken ElasticSearch JSON implementation
    - omelasticsearch: craft better URLs
    - imfile: add experimental "reopenOnTruncate" parameter
    - bugfix imfile: proper handling of inotify initialization failure
    - bugfix imfile: potential segfault due to improper handling of ev var
    - bugfix imfile: potential segfault under heavey load.
    - bugfix ommail: invalid handling of server response
    - bugfix omelasticsearch: custom serverport was ignored on some platforms
    - bugfix: tarball did not include some testbench files
    - bugfix: memory misadressing during config parsing string template
    - bugfix imzmq: memory leak
    - bugfix imzmq: memory leak
    - bugfix omzmq: memory leak
    - some code improvement and cleanup

--- sysutils/rsyslog/patches/patch-plugins_imfile_imfile.c
+++ sysutils/rsyslog/patches/patch-plugins_imfile_imfile.c
@@ -1,21 +1,20 @@
-$NetBSD: patch-plugins_imfile_imfile.c,v 1.4 2015/08/26 00:55:33 fhajny Exp $
+$NetBSD: patch-plugins_imfile_imfile.c,v 1.5 2016/01/26 15:20:33 fhajny Exp $
 
 Avoid unresolved symbol on inotify-less platforms.
---- plugins/imfile/imfile.c.orig	2015-08-10 10:24:49.000000000 +0000
+--- plugins/imfile/imfile.c.orig	2016-01-22 07:54:28.000000000 +0000
 +++ plugins/imfile/imfile.c
-@@ -1840,11 +1840,14 @@ BEGINrunInput
+@@ -1839,10 +1839,14 @@ BEGINrunInput
  CODESTARTrunInput
- 	DBGPRINTF("imfile: working in %s mode\n", 
+ 	DBGPRINTF("imfile: working in %s mode\n",
  		 (runModConf->opMode == OPMODE_POLLING) ? "polling" : "inotify");
 +#if HAVE_INOTIFY_INIT
  	if(runModConf->opMode == OPMODE_POLLING)
  		iRet = doPolling();
  	else
  		iRet = do_inotify();
--
 +#else
 +	iRet = doPolling();
 +#endif
+ 
  	DBGPRINTF("imfile: terminating upon request of rsyslog core\n");
  ENDrunInput
- 
