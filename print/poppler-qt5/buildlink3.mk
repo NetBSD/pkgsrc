70c3d92cd848b7fe3832341c1d9f2740bb693d0e
wiz <wiz>
2016-01-27 10:31:28 +0000

    Recursive PKGREVISION bump for poppler shlib bump.

--- print/poppler-qt5/buildlink3.mk
+++ print/poppler-qt5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2015/11/23 10:30:15 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2016/01/27 10:31:29 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-qt5
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-qt5
 POPPLER_QT5_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-qt5+=	poppler-qt5>=0.26.0
-BUILDLINK_ABI_DEPENDS.poppler-qt5?=	poppler-qt5>=0.38.0
+BUILDLINK_ABI_DEPENDS.poppler-qt5?=	poppler-qt5>=0.40.0
 BUILDLINK_PKGSRCDIR.poppler-qt5?=	../../print/poppler-qt5
 
 .include "../../print/poppler/buildlink3.mk"
