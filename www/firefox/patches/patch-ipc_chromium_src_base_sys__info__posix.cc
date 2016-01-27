a3ad1181a62c831ea197b0c4f2ae297ac0fc94a7
ryoon <ryoon@pkgsrc.org>
2016-01-27 00:08:26 +0000

    Update to 44.0
    Changelog:
    New
        Improved warning pages for certificate errors and untrusted connections
        Enable H.264 if system decoder is available
        Enable WebM/VP9 video support on systems that don't support MP4/H.264
        In the animation-inspector timeline, lightning bolt icon next to animations running on the compositor thread
        Support the brotli compression format via HTTPS content-encoding
        Screenshot commands allow user choice of pixel ratio in Developer Tools
    
    Fixed
        Windows XP and Vista screensaver doesn't disable when watching videos (Bug 1193610)
        Various security fixes
    
    Changed
        To support unicode-range descriptor for webfonts, font matching under Linux now uses the same font matching code as other platforms
        Use a SHA-256 signing certificate for Windows builds, to meet new signing requirements
        Firefox has removed support for the RC4 decipher
        Firefox will no longer trust the Equifax Secure Certificate Authority 1024-bit root certificate or the UTN - DATACorp SGC to validate secure website certificates
        Stricter validation of web fonts
        On-screen keyboard support temporarily turned off for Windows 8 and Windows 8.1
    
    Developer
        Right click on a logged object in the console to store it as a global variable on the page
        Visual tools for Animation:
            View/Edit CSS animation keyframe rules directly in the inspector
            Visually modify the cubic-bezier curve that drives the way animations progress through time
            Discover and scrub through all CSS animations and transitions playing on the page
            Learn more: http://devtoolschallenger.com/
        Visual tools for Layout and Styles:
            Display rulers along the viewport to verify size and position and use the measurement tool to easily detect spacing and alignment problems
            Use CSS filters to preview and create real-time effects like drop-shadows, sepia, etc
            Learn more: http://devtoolschallenger.com/
        New memory tool for inspecting the memory heap
        Service Workers API
        Built-in JSON reader to intuitively view, search, copy and save data without extensions
        Jump to function definitions in the debugger with Cmd-Click
        WebSocket Debugging API and add-on
        The rule view now displays styles using their authored text, and edits in the rule view are now linked to the style editor
    
    Security bugs:
    Fixed in Firefox 44
        2016-12 Lightweight themes on Firefox for Android do not verify a secure connection
        2016-11 Application Reputation service disabled in Firefox 43
        2016-10 Unsafe memory manipulation found through code inspection
        2016-09 Addressbar spoofing attacks
        2016-08 Delay following click events in file download dialog too short on OS X
        2016-07 Errors in mp_div and mp_exptmod cryptographic functions in NSS
        2016-06 Missing delay following user click events in protocol handler dialog
        2016-05 Addressbar spoofing through stored data url shortcuts on Firefox for Android
        2016-04 Firefox allows for control characters to be set in cookie names
        2016-03 Buffer overflow in WebGL after out of memory allocation
        2016-02 Out of Memory crash when parsing GIF format images
        2016-01 Miscellaneous memory safety hazards (rv:44.0 / rv:38.6)

--- www/firefox/patches/patch-ipc_chromium_src_base_sys__info__posix.cc
+++ www/firefox/patches/patch-ipc_chromium_src_base_sys__info__posix.cc
@@ -1,23 +1,11 @@
-$NetBSD: patch-ipc_chromium_src_base_sys__info__posix.cc,v 1.4 2014/04/30 15:07:18 ryoon Exp $
+$NetBSD: patch-ipc_chromium_src_base_sys__info__posix.cc,v 1.5 2016/01/27 00:08:26 ryoon Exp $
 
---- ipc/chromium/src/base/sys_info_posix.cc.orig	2014-04-18 02:03:04.000000000 +0000
+--- ipc/chromium/src/base/sys_info_posix.cc.orig	2016-01-15 01:01:44.000000000 +0000
 +++ ipc/chromium/src/base/sys_info_posix.cc
 @@ -119,7 +119,11 @@ std::wstring SysInfo::GetEnvVar(const wc
  
  // static
  std::string SysInfo::OperatingSystemName() {
-+#ifdef OS_SOLARIS
-+  struct utsname info;
-+#else
-   utsname info;
-+#endif
-   if (uname(&info) < 0) {
-     NOTREACHED();
-     return "";
-@@ -129,7 +133,11 @@ std::string SysInfo::OperatingSystemName
- 
- // static
- std::string SysInfo::OperatingSystemVersion() {
 +#ifdef OS_SOLARIS
 +  struct utsname info;
 +#else
