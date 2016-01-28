7337ea50c0ab95f05ba3b46b02ee9df4b4a5135d
fhajny <fhajny@pkgsrc.org>
2016-01-28 16:58:18 +0000

    Fix incorrect patch file. Fixes build on SunOS.

--- x11/qt5-qtdeclarative/patches/patch-src_qml_jsruntime_qv4engine.cpp
+++ x11/qt5-qtdeclarative/patches/patch-src_qml_jsruntime_qv4engine.cpp
@@ -1,16 +1,18 @@
-$NetBSD: patch-src_qml_jsruntime_qv4engine.cpp,v 1.2 2015/10/26 19:03:59 adam Exp $
+$NetBSD: patch-src_qml_jsruntime_qv4engine.cpp,v 1.3 2016/01/28 16:58:18 fhajny Exp $
 
 Get stack address/size on SunOS.
 
---- src/qml/jsruntime/qv4engine.cpp.orig	2015-06-29 20:12:38.000000000 +0000
+--- src/qml/jsruntime/qv4engine.cpp.orig	2015-10-13 04:37:50.000000000 +0000
 +++ src/qml/jsruntime/qv4engine.cpp
-@@ -136,13 +136,19 @@ quintptr getStackLimit()
+@@ -139,13 +139,21 @@ quintptr getStackLimit()
  #if HAVE(PTHREAD_NP_H) && OS(FREEBSD)
      // on FreeBSD pthread_attr_init() must be called otherwise getting the attrs crashes
      if (pthread_attr_init(&attr) == 0 && pthread_attr_get_np(pthread_self(), &attr) == 0) {
 -#else
 +#elif !OS(SOLARIS)
      if (pthread_getattr_np(pthread_self(), &attr) == 0) {
++#else
++    if (true) {
  #endif
          void *stackBottom = Q_NULLPTR;
          size_t stackSize = 0;
