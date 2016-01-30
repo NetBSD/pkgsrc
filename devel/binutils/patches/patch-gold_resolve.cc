fce69ca2c3cfcbf75499a4814a411d6e951f42e8
ryoon <ryoon@pkgsrc.org>
2016-01-30 22:15:11 +0000

    Update to 2.26
    Changelog:
    Changes in 2.26:
    
    * Add option to objcopy to insert new symbols into a file:
      --add-symbol <name>=[<section>:]<value>[,<flags>]
    
    * Add support for the ARC EM/HS, and ARC600/700 architectures.
    
    * Extend objcopy --compress-debug-sections option to support
      --compress-debug-sections=[none|zlib|zlib-gnu|zlib-gabi] for ELF
      targets.
    
    * Add --update-section option to objcopy.
    
    * Add --output-separator option to strings.

--- devel/binutils/patches/patch-gold_resolve.cc
+++ devel/binutils/patches/patch-gold_resolve.cc
@@ -1,4 +1,4 @@
-$NetBSD: patch-gold_resolve.cc,v 1.1 2014/05/12 19:10:08 marino Exp $
+$NetBSD: patch-gold_resolve.cc,v 1.2 2016/01/30 22:15:11 ryoon Exp $
 
  The gold linker is overly pedantic for dynamic references to symbols
  with hidden visibility.  It will spew error messages about weak symbols
@@ -8,9 +8,9 @@ $NetBSD: patch-gold_resolve.cc,v 1.1 2014/05/12 19:10:08 marino Exp $
 
  https://sourceware.org/bugzilla/show_bug.cgi?id=15574
 
---- gold/resolve.cc.orig	2013-11-04 15:33:39.000000000 +0000
+--- gold/resolve.cc.orig	2015-11-13 08:27:42.000000000 +0000
 +++ gold/resolve.cc
-@@ -276,8 +276,7 @@ Symbol_table::resolve(Sized_symbol<size>
+@@ -284,8 +284,7 @@ Symbol_table::resolve(Sized_symbol<size>
        to->set_in_reg();
      }
    else if (st_shndx == elfcpp::SHN_UNDEF
@@ -18,5 +18,5 @@ $NetBSD: patch-gold_resolve.cc,v 1.1 2014/05/12 19:10:08 marino Exp $
 -               || to->visibility() == elfcpp::STV_INTERNAL))
 +           && (to->visibility() == elfcpp::STV_INTERNAL))
      {
-       // A dynamic object cannot reference a hidden or internal symbol
-       // defined in another object.
+       // The symbol is hidden, so a reference from a shared object
+       // cannot bind to it.  We tried issuing a warning in this case,
