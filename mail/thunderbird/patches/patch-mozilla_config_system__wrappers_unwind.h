daa53daa42bfe90ce30297d5e1c2d086cd7d76cc
joerg <joerg@pkgsrc.org>
2016-01-31 23:43:48 +0000

    Don't request static graphite, it breaks the build against newer external graphite. Add all the graphite headers to the magic wrapper list to avoid future fun. Remove manual unwind.h header where it still exists.

--- mail/thunderbird/patches/patch-mozilla_config_system__wrappers_unwind.h
+++
@@ -1,9 +0,0 @@
-$NetBSD: patch-mozilla_config_system__wrappers_unwind.h,v 1.1 2013/05/14 12:41:02 joerg Exp $
-
---- mozilla/config/system_wrappers/unwind.h.orig	2013-05-13 19:56:18.000000000 +0000
-+++ mozilla/config/system_wrappers/unwind.h
-@@ -0,0 +1,4 @@
-+#pragma GCC system_header
-+#pragma GCC visibility push(default)
-+#include_next <unwind.h>
-+#pragma GCC visibility pop
