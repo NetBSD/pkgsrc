7954ade2e974a2373b624a92a9374dfee9550d2b
rillig <rillig@pkgsrc.org>
2016-02-01 21:40:39 +0000

    Fixed boolean expression for selecting the proper pkglint version
    It had been wrong for SunOS-*-i386, which is not supported by the Go
    programming language, but was forced to use the Go version nevertheless.

--- pkgtools/pkglint/select.mk
+++ pkgtools/pkglint/select.mk
@@ -1,4 +1,4 @@
-# $NetBSD: select.mk,v 1.1 2016/01/12 20:08:17 rillig Exp $
+# $NetBSD: select.mk,v 1.2 2016/02/01 21:40:39 rillig Exp $
 #
 # Selects the proper version of pkglint, depending on whether the
 # platform supports the Go programming language.
@@ -7,7 +7,7 @@
 .include "../../mk/bsd.fast.prefs.mk"
 
 # See lang/go/version.mk
-.if !empty(MACHINE_ARCH:Ni386:Nx86_64:Nevbarm) && !empty(MACHINE_PLATFORM:NSunOS-*-i386)
+.if ${MACHINE_ARCH:Ni386:Nx86_64:Nevbarm} || ${MACHINE_PLATFORM:MSunOS-*-i386}
 DEPENDS+=	pkglint>=4.82<5:../../pkgtools/pkglint4
 .else
 DEPENDS+=	pkglint>=5:../../pkgtools/pkglint
