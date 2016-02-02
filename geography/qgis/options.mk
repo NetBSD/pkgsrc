928b8b71655bf1d081eb94ddd2b1aabced7f4b64
dbj <dbj@pkgsrc.org>
2016-02-02 04:33:57 +0000

    update qgis to 12.2.3
    New Features in QGIS 2.12 ‘Lyon’
    
    This is the next release in our 4-monthly release series. It
    gives you access to the new features we have been working on and
    represents the ‘cutting edge’ of QGIS development.
    
    QGIS ‘Lyon’ is jam-packed with awesome new features. Special
    highlights are support for rule based labelling, rule based
    styling of attribute tables, and advanced geometry checker,
    support for digitising curve based geometries, better
    authentication management and much, much more! QGIS 2.12 also has
    many bug fixes and memory leaks addressed. The features provided
    in QGIS 2.12 will be included in the next LTR release (slated for
    release in 2016), so using this release provides you with an
    excellent opportunity to test new features that will make their
    way into the next LTR.

--- geography/qgis/options.mk
+++ geography/qgis/options.mk
@@ -1,21 +1,25 @@
-# $NetBSD: options.mk,v 1.4 2015/12/05 21:25:44 adam Exp $
+# $NetBSD: options.mk,v 1.5 2016/02/02 04:33:57 dbj Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.qgis
 PKG_SUPPORTED_OPTIONS=	python
-PKG_SUGGESTED_OPTIONS+= python
+# Don't enable python by default because it is not
+# supported by gdal-lib and as a result we get a
+# warning when qgis is launched.
+# XXX fix gdal-lib and reenable this.
+#PKG_SUGGESTED_OPTIONS+= python
 
 .include "../../mk/bsd.options.mk"
-
-PLIST_VARS+=		python
 
 .if empty(PKG_OPTIONS:Mpython)
 CMAKE_ARGS+=		-DWITH_BINDINGS:BOOL=FALSE
 .else
 CMAKE_ARGS+=		-DWITH_BINDINGS:BOOL=TRUE
 CMAKE_ARGS+=		-DSIP_BINARY_PATH:PATH=${BUILDLINK_PREFIX.py-sip}/bin
-PLIST.python=		yes
-PYTHON_VERSIONS_INCOMPATIBLE=	33 34 35 # not yet ported as of 1.7.4
+PLIST_SRC+=             ${PKGDIR}/PLIST.python
+#PYTHON_VERSIONS_INCOMPATIBLE=	33 34 35 # ?not yet ported as of ?
 .include "../../lang/python/application.mk"
+.include "../../math/py-numpy/buildlink3.mk"
 .include "../../x11/py-qt4/buildlink3.mk"
+.include "../../x11/py-qt4-qscintilla/buildlink3.mk"
 .include "../../x11/py-sip/buildlink3.mk"
 .endif
