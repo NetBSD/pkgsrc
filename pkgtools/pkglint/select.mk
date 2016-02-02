04c0ce151cddfb9cca23c947fe27c1a0f4b4c9b2
rillig <rillig>
2016-02-02 08:00:10 +0000

    Fixed dependency pattern for pkglint4

--- pkgtools/pkglint/select.mk
+++ pkgtools/pkglint/select.mk
@@ -1,4 +1,4 @@
-# $NetBSD: select.mk,v 1.2 2016/02/01 21:40:39 rillig Exp $
+# $NetBSD: select.mk,v 1.3 2016/02/02 08:00:10 rillig Exp $
 #
 # Selects the proper version of pkglint, depending on whether the
 # platform supports the Go programming language.
@@ -8,7 +8,7 @@
 
 # See lang/go/version.mk
 .if ${MACHINE_ARCH:Ni386:Nx86_64:Nevbarm} || ${MACHINE_PLATFORM:MSunOS-*-i386}
-DEPENDS+=	pkglint>=4.82<5:../../pkgtools/pkglint4
+DEPENDS+=	pkglint4>=4.82<5:../../pkgtools/pkglint4
 .else
 DEPENDS+=	pkglint>=5:../../pkgtools/pkglint
 .endif
