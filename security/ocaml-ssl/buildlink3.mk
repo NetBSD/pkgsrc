39d33176419213272c648dafd921e6e7f768dd6c
jaapb <jaapb>
2016-02-03 12:48:38 +0000

    Updated package to the newest version, 0.5.2. Changes include:
    0.5.2 (2015-11-23)
    =====
    * Add OPENSSL_NO_SSL3 preprocessor flag to disable SSLv3 (thanks Jérémie
      Courrèges-Anglas).
    
    0.5.1 (2015-05-27)
    =====
    
    * Fix META file for versions of OCaml older than 4.02.0 (thanks Anil
      Madhavapeddy, closes #20).
    
    0.5.0 (2015-05-18)
    =====
    * Allow to honor server cipher preferences (thanks mfp, closes #18).
    * Add functions for reading into/writing from bigarrays, avoiding copy (thanks
      mfp, closes #15).
    * Support disabling SSL protocol versions (thanks Edwin Török, closes #13).
    * Use Bytes instead of String for read and write, changes the ABI thus the
      version bump (thanks Vincent Bernardoff, closes #16, and mfp, closes #19).
    * Make verbosity of client_verify_callback configurable (thanks Nicolas Trangez,
      closes #12).
    * Fix build with old versions of SSL (thanks Edwin Török, closes #10).

--- security/ocaml-ssl/buildlink3.mk
+++ security/ocaml-ssl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2015/08/07 11:20:33 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2016/02/03 12:48:38 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ssl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ssl
 OCAML_SSL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.4.0
-BUILDLINK_ABI_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.4.7nb3
+BUILDLINK_ABI_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.5.2
 BUILDLINK_PKGSRCDIR.ocaml-ssl?=	../../security/ocaml-ssl
 
 .include "../../lang/ocaml/buildlink3.mk"
