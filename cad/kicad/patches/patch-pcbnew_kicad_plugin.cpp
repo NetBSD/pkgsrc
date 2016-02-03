bfb73f13be7709f2d74e851ddda434202ad067f7
bouyer <bouyer@pkgsrc.org>
2016-02-03 19:34:33 +0000

    Update kicad to the 4.0.1 release. Major changes:     New graphics rendering backend GAL (OpenGL and Cairo) [currently pcbnew 	only and does not yet support all legacy tools]
        New s-expression based pcb format (.kicad_pcb)
    
        New footprint library format (.pretty folder with .kicad_mod footprints)
    
        Updated footprint editor (only usable with GAL renderer)
    
        More advanced footprint manager concept called footprint library
    	table (fp-lib-table)
    
        Ability to download footprints on the fly from git repositories
    
        Official libraries are now stored on github and regularly updated
    
        Awesome/Advanced Push and Shove (PnS) router (only usable with GAL renderer)
    
        Interactive differential trace routing and tuning
    
        Interactive trace length tuning
    
        Much more realistic 3D board rendering
    
        Intelligent library search with preview in Eeschema
    
        Initial Python api for Pcbnew
    
    Notes:
    Symbols have changed size, been renamed, etc to follow the new KiCad library
    conventions so you need to take care when working with older schematics if
    you used the symbols packaged with KiCad.
    
    The new pcb format cannot be loaded in older versions of KiCad.

--- cad/kicad/patches/patch-pcbnew_kicad_plugin.cpp
+++ cad/kicad/patches/patch-pcbnew_kicad_plugin.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-pcbnew_kicad_plugin.cpp,v 1.1.1.1 2014/02/17 20:38:59 bouyer Exp $
+$NetBSD: patch-pcbnew_kicad_plugin.cpp,v 1.2 2016/02/03 19:34:34 bouyer Exp $
 64bit time_t fix
 Reported upstream as bug id 1280901
 
---- pcbnew/kicad_plugin.cpp.orig	2014-02-15 15:55:03.000000000 +0100
-+++ pcbnew/kicad_plugin.cpp	2014-02-15 15:56:10.000000000 +0100
-@@ -673,7 +673,7 @@
+--- pcbnew/kicad_plugin.cpp.orig	2015-12-11 18:05:12.000000000 +0100
++++ pcbnew/kicad_plugin.cpp	2015-12-11 18:14:06.000000000 +0100
+@@ -770,7 +770,7 @@
      formatLayer( aDimension );
  
      if( aDimension->GetTimeStamp() )
@@ -13,7 +13,7 @@ Reported upstream as bug id 1280901
  
      m_out->Print( 0, "\n" );
  
-@@ -782,7 +782,7 @@
+@@ -879,7 +879,7 @@
          m_out->Print( 0, " (width %s)", FMT_IU( aSegment->GetWidth() ).c_str() );
  
      if( aSegment->GetTimeStamp() )
@@ -22,16 +22,7 @@ Reported upstream as bug id 1280901
  
      if( aSegment->GetStatus() )
          m_out->Print( 0, " (status %X)", aSegment->GetStatus() );
-@@ -855,7 +855,7 @@
-     /*  11-Nov-2021 remove if no one whines after a couple of months.  Simple graphic items
-         perhaps do not need these.
-     if( aModuleDrawing->GetTimeStamp() )
--        m_out->Print( 0, " (tstamp %lX)", aModuleDrawing->GetTimeStamp() );
-+        m_out->Print( 0, " (tstamp %lX)", (u_long)aModuleDrawing->GetTimeStamp() );
- 
-     if( aModuleDrawing->GetStatus() )
-         m_out->Print( 0, " (status %X)", aModuleDrawing->GetStatus() );
-@@ -879,7 +879,7 @@
+@@ -966,7 +966,7 @@
      formatLayer( aTarget );
  
      if( aTarget->GetTimeStamp() )
@@ -40,16 +31,16 @@ Reported upstream as bug id 1280901
  
      m_out->Print( 0, ")\n" );
  }
-@@ -901,7 +901,7 @@
+@@ -1003,7 +1003,7 @@
+ 
      if( !( m_ctl & CTL_OMIT_TSTAMPS ) )
      {
-         m_out->Print( 0, " (tedit %lX) (tstamp %lX)\n",
--                       aModule->GetLastEditTime(), aModule->GetTimeStamp() );
-+                       (u_long)aModule->GetLastEditTime(), (u_long)aModule->GetTimeStamp() );
+-        m_out->Print( 0, " (tstamp %lX)\n", aModule->GetTimeStamp() );
++        m_out->Print( 0, " (tstamp %lX)\n", (u_long)aModule->GetTimeStamp() );
      }
      else
          m_out->Print( 0, "\n" );
-@@ -1216,7 +1216,7 @@
+@@ -1344,7 +1344,7 @@
      formatLayer( aText );
  
      if( aText->GetTimeStamp() )
@@ -58,8 +49,8 @@ Reported upstream as bug id 1280901
  
      m_out->Print( 0, "\n" );
  
-@@ -1324,7 +1324,7 @@
-     m_out->Print( 0, " (net %d)", aTrack->GetNet() );
+@@ -1452,7 +1452,7 @@
+     m_out->Print( 0, " (net %d)", m_mapping->Translate( aTrack->GetNetCode() ) );
  
      if( aTrack->GetTimeStamp() != 0 )
 -        m_out->Print( 0, " (tstamp %lX)", aTrack->GetTimeStamp() );
@@ -67,7 +58,7 @@ Reported upstream as bug id 1280901
  
      if( aTrack->GetStatus() != 0 )
          m_out->Print( 0, " (status %X)", aTrack->GetStatus() );
-@@ -1345,7 +1345,7 @@
+@@ -1473,7 +1473,7 @@
  
      formatLayer( aZone );
  
