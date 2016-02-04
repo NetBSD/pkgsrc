846ce41114edcb9eb0c9bcb47dafcd50066b0272
wiz <wiz@pkgsrc.org>
2016-02-04 12:12:36 +0000

    Update vala to 0.30.
    Vala 0.30.0
    ===========
    
     * Binding updates.
    
    Vala 0.29.3
    ===========
    
     * Add --shared-library option for GIR files.
     * Bug fixes and binding updates.
    
    Vala 0.29.2
    ===========
    
     * Bug fixes and binding updates.
    
    Vala 0.29.1
    ===========
    
     * Binding updates.

--- lang/vala/buildlink3.mk
+++ lang/vala/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.12 2015/04/03 07:38:17 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2016/02/04 12:12:36 wiz Exp $
 
 BUILDLINK_TREE+=	vala
 
 .if !defined(VALA_BUILDLINK3_MK)
 VALA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vala+=	vala>=0.28
+BUILDLINK_API_DEPENDS.vala+=	vala>=0.30
 BUILDLINK_PKGSRCDIR.vala?=	../../lang/vala
 
-VALAC=${LOCALBASE}/bin/valac-0.28
-VAPIGEN=${LOCALBASE}/bin/vapigen-0.28
+VALAC=${LOCALBASE}/bin/valac-0.30
+VAPIGEN=${LOCALBASE}/bin/vapigen-0.30
 .if defined(GNU_CONFIGURE)
 CONFIGURE_ENV+=	VALAC=${VALAC} VAPIGEN=${VAPIGEN}
 .endif
