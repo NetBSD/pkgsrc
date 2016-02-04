a0af8871e11d2a81d5ea96232a53fdc1bdade4d8
leot <leot@pkgsrc.org>
2016-02-04 11:50:24 +0000

    Add glfw option to mupdf (disabled by default).
    Bump PKGREVISION++.

--- print/mupdf/options.mk
+++ print/mupdf/options.mk
@@ -1,9 +1,11 @@
-# $NetBSD: options.mk,v 1.3 2014/06/15 19:15:51 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2016/02/04 11:50:24 leot Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mupdf
-PKG_SUPPORTED_OPTIONS=	curl
+PKG_SUPPORTED_OPTIONS=	curl glfw
 
 .include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		glfw
 
 #
 # curl support
@@ -11,4 +13,13 @@ PKG_SUPPORTED_OPTIONS=	curl
 .if !empty(PKG_OPTIONS:Mcurl)
 .include "../../www/curl/buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
+.endif
+
+#
+# glfw support
+#
+.if !empty(PKG_OPTIONS:Mglfw)
+PLIST.glfw=	yes
+.include "../../graphics/glut/buildlink3.mk"
+.include "../../graphics/glfw/buildlink3.mk"
 .endif
