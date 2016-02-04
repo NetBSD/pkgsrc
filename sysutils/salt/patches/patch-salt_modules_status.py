d5ee30c9f88a10a1834ea8fd8f081ec4b34e2acb
khorben <khorben@pkgsrc.org>
2016-02-04 22:05:36 +0000

    Avoid a crash in "status.diskusage" when not on Linux or FreeBSD
    Bump PKGREVISION in the process.

---
+++ sysutils/salt/patches/patch-salt_modules_status.py
@@ -0,0 +1,15 @@
+$NetBSD: patch-salt_modules_status.py,v 1.1 2016/02/04 22:05:36 khorben Exp $
+
+Avoid a crash in "status.diskusage" when not on Linux or FreeBSD
+
+--- salt/modules/status.py.orig	2016-02-04 21:56:06.000000000 +0000
++++ salt/modules/status.py
+@@ -443,6 +443,8 @@ def diskusage(*args):
+             ifile = salt.utils.fopen(procf, 'r').readlines()
+         elif __grains__['kernel'] == 'FreeBSD':
+             ifile = __salt__['cmd.run']('mount -p').splitlines()
++        else:
++            ifile = []
+ 
+         for line in ifile:
+             comps = line.split()
