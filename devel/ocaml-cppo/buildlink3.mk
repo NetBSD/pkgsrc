85c9f36405f42df09c06a102ee18e8871b2cfb6e
jaapb <jaapb@pkgsrc.org>
2016-02-06 13:34:52 +0000

    Added package devel/ocaml-cppo, a cpp-like preprocessor for OCaml.

---
+++ devel/ocaml-cppo/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/02/06 13:34:52 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-cppo
+
+.if !defined(OCAML_CPPO_BUILDLINK3_MK)
+OCAML_CPPO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.3.1
+BUILDLINK_PKGSRCDIR.ocaml-cppo?=	../../devel/ocaml-cppo
+.endif	# OCAML_CPPO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-cppo
