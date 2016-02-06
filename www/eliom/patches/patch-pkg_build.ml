99b36b773f32885605ab405c42b17f473a533e95
jaapb <jaapb@pkgsrc.org>
2016-02-06 14:50:22 +0000

    Updated packate to the latest version, 5.0.0. Changes include:
    ===== 5.0 (2016-01-28) =====
    
    * Add Eliom_shared and server-side Eliom_content.{Html5,Svg}.R modules
    * Add PPX syntax extension
    * Clean-up form APIs, now available under Eliom_content.Html5.{D,F}.Form
    * Patches to significantly reduce the size of request data
    * Compatibility with TyXML 3.6, Js_of_ocaml 2.7, and reactiveData 0.2
    * Various bugfixes and enhancements
    
    ===== 4.2 (2015-07-21) =====
    
    * Add Manip.children, to get the html children of an element.
    * Simplify Html5 and Svg signatures using tyxml's signature functors.
    * Various logging improvements, in particular in Eliom_client.
    * Fix eliomdep's exit code when calling option "-sort".
    * Fix #168: call_ocaml_service always sends boolean true.
    * Makes server function return types covariant.
    * Restore compatibility with ocsigenserver 2.6 and lwt 2.4.7.
    * Various bugfixes and wiki updates.

--- www/eliom/patches/patch-pkg_build.ml
+++ www/eliom/patches/patch-pkg_build.ml
@@ -1,12 +1,12 @@
-$NetBSD: patch-pkg_build.ml,v 1.1 2015/02/11 15:30:13 jaapb Exp $
+$NetBSD: patch-pkg_build.ml,v 1.2 2016/02/06 14:50:22 jaapb Exp $
 
 Do not install mime types
---- pkg/build.ml.orig	2014-10-01 15:19:35.000000000 +0000
+--- pkg/build.ml.orig	2016-01-28 18:22:25.000000000 +0000
 +++ pkg/build.ml
-@@ -96,8 +96,7 @@ let () =
+@@ -107,8 +107,7 @@ let () =
      (* MISC *)
  
-     Pkg.doc "README";
+     Pkg.doc "README.md";
 -    Pkg.doc "CHANGES";
 -    Pkg.etc "pkg/etc/mime.types"
 +    Pkg.doc "CHANGES"
