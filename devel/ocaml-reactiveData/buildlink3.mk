b4076eb9987815e7d630bb6f8f4672cf4981b577
jaapb <jaapb@pkgsrc.org>
2016-02-06 11:23:41 +0000

    Updated version to 0.2 and changed master Github repository. Changes include:
      * Add `from_signal`, which converts React signals to ReactiveData
        containers. `from_signal` uses a diffing algorithm to detect what
        changes, thus minimizing the updates needed downstream.
      * Optimize common cases of `merge`.
      * Provide documentation and make the naming more consistent.

--- devel/ocaml-reactiveData/buildlink3.mk
+++ devel/ocaml-reactiveData/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/01/20 15:19:54 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/02/06 11:23:41 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-reactiveData
 
 .if !defined(OCAML_REACTIVEDATA_BUILDLINK3_MK)
 OCAML_REACTIVEDATA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-reactiveData+=	ocaml-reactiveData>=0.1
+BUILDLINK_API_DEPENDS.ocaml-reactiveData+=	ocaml-reactiveData>=0.2
 BUILDLINK_PKGSRCDIR.ocaml-reactiveData?=	../../devel/ocaml-reactiveData
 
 .include "../../devel/ocaml-react/buildlink3.mk"
