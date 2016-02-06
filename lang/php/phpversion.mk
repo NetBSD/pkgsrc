97d80ef2d2b35ff3f39b8f228d0fdd5650433d25
taca <taca@pkgsrc.org>
2016-02-06 07:13:02 +0000

    Update php56 to 5.6.18 (PHP 5.6.18).
    04 Feb 2016, PHP 5.6.18
    
    - Core:
      . Fixed bug #71039 (exec functions ignore length but look for NULL termination).
        (Anatol)
      . Fixed bug #71089 (No check to duplicate zend_extension). (Remi)
      . Fixed bug #71201 (round() segfault on 64-bit builds). (Anatol)
      . Added support for new HTTP 451 code. (Julien)
      . Fixed bug #71273 (A wrong ext directory setup in php.ini leads to crash).
        (Anatol)
      . Fixed bug #71323 (Output of stream_get_meta_data can be falsified by its
        input). (Leo Gaspard)
      . Fixed bug #71459 (Integer overflow in iptcembed()). (Stas)
    
    - Apache2handler:
      . Fix >2G Content-Length headers in apache2handler. (Adam Harvey)
    
    - FTP:
      . Implemented FR #55651 (Option to ignore the returned FTP PASV address).
        (abrender at elitehosts dot com)
    
    - Opcache:
      . Fixed bug #71127 (Define in auto_prepend_file is overwrite). (Laruence)
      . Fixed bug #71024 (Unable to use PHP 7.0 x64 side-by-side with PHP 5.6 x32
        on the same server). (Anatol)
    
    - Phar:
      . Fixed bug #71354 (Heap corruption in tar/zip/phar parser). (Stas)
      . Fixed bug #71391 (NULL Pointer Dereference in phar_tar_setupmetadata()).
        (Stas)
      . Fixed bug #71488 (Stack overflow when decompressing tar archives). (Stas)
    
    - Session:
      . Fixed bug #69111 (Crash in SessionHandler::read()). (Anatol)
    
    - SOAP:
      . Fixed bug #70979 (crash with bad soap request). (Anatol)
    
    - SPL:
      . Fixed bug #71204 (segfault if clean spl_autoload_funcs while autoloading).
        (Laruence)
    
    - WDDX:
      . Fixed bug #71335 (Type Confusion in WDDX Packet Deserialization). (Stas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.123 2016/02/06 07:11:06 taca Exp $
+# $NetBSD: phpversion.mk,v 1.124 2016/02/06 07:13:02 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -82,7 +82,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP55_VERSION=	5.5.32
-PHP56_VERSION=	5.6.17
+PHP56_VERSION=	5.6.18
 PHP70_VERSION=	7.0.2
 
 # Define initial release of major version.
