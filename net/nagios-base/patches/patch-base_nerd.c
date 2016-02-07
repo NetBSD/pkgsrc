f8fef2d64436b4fd56fdc392e0869592f3648482
bouyer <bouyer@pkgsrc.org>
2016-02-07 21:52:06 +0000

    more 64bit time_t fixes. Now compiles without -Wformat warnings. Ride previous PKGREVISION bump

---
+++ net/nagios-base/patches/patch-base_nerd.c
@@ -0,0 +1,15 @@
+$NetBSD: patch-base_nerd.c,v 1.1 2016/02/07 21:52:06 bouyer Exp $
+
+64bit time_t workaround
+
+--- base/nerd.c.orig	2016-02-07 21:13:59.000000000 +0100
++++ base/nerd.c	2016-02-07 21:14:15.000000000 +0100
+@@ -337,7 +337,7 @@
+ 	} else
+ 		return 0;
+ 
+-	asprintf(&buf, "%lu|%s|M|%s/%s|%06X\n", cr->finish_time.tv_sec,
++	asprintf(&buf, "%lu|%s|M|%s/%s|%06X\n", (unsigned long)cr->finish_time.tv_sec,
+ 			 check_result_source(cr), host_parent_path(h, '/'), name, color);
+ 	nerd_broadcast(chan_opath_checks_id, buf, strlen(buf));
+ 	free(buf);
