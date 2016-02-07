10997040410880cd99ceb0b74b331345bf58385e
leot <leot@pkgsrc.org>
2016-02-07 15:29:34 +0000

    Update comms/gammu to 1.37.0.
    Changes:
    20160203 - 1.37.0
    [-] * Improved compatibility with ZTE MF190.
    [-] * Improved compatibility with Huawei E1750.
    [-] * Improved compatibility with Huawei E1752.
    [-] * Increased detail of reported errors from SMSD.
    
    20151208 - 1.36.8
    [-] * Changed default value for ReceiveFrequency.
    [-] * Fixed compatibility for PostgreSQL.
    [-] * Fixed build failure with all disabled SMSD backends.
    [-] * Documentation improvements.
    [-] * Fixed mixing C++ code with SMSD.
    
    20151129 - 1.36.7
    [-] * Support devices which do not report full network status.
    [-] * Disable Huawei unsolicited messages on startup.
    [-] * Various improvements for Huawei modems.
    [-] * Fixed compilation on Windows.
    [-] * Fixed regression with Siemens AX75.
    [-] * Improved decoding of USSD responses.
    [-] * Properly decode emojis to console or files backend.
    [+] * Added support for proxying the connection through arbitrary command.
    [+] * SMSD now tracks retries count per message.
    
    20151012 - 1.36.6
    [-] * Fixed installation of bash-completion script.
    [-] * Fixed timezone manipulation in SMSD.
    [-] * Documentation improvements.
    [-] * Fixed licensing of helper/win32-dirent.*.
    [*] * Increased default speed for AT connection to 115200.
    [*] * Improve AT module initialization.
    
    20150826 - 1.36.5
    [-] * Properly use timezones with SQLite in SMSD.
    [-] * Improve support for Huawei E1752.
    [-] * Fixed compilation on distros with old Glib.

--- comms/gammu/buildlink3.mk
+++ comms/gammu/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/08/17 16:42:53 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/02/07 15:29:34 leot Exp $
 
 BUILDLINK_TREE+=	gammu
 
 .if !defined(GAMMU_BUILDLINK3_MK)
 GAMMU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gammu+=	gammu>=1.34.0
+BUILDLINK_API_DEPENDS.gammu+=	gammu>=1.37.0
 BUILDLINK_PKGSRCDIR.gammu?=	../../comms/gammu
 
 .endif	# GAMMU_BUILDLINK3_MK
