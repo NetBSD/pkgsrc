9574799bae058d57748d3507375a3014facf0188
ryoon <ryoon@pkgsrc.org>
2016-02-12 03:37:24 +0000

    Update to 3.2.1
    * Change JavaScript engine from V8 to SpiderMonkey.
    
    Changelog:
    3.2.1 – Jan 12, 2016
    
        Fixed error where during a regular shutdown of a replica set, secondaries may mark certain replicated but yet to be applied operations as successfully applied: SERVER-21868.
        Improve insert workload performance with WiredTiger on Windows: SERVER-20262.
        Fixed long-running transactions during chunk moves: SERVER-21366
        All issues closed in 3.2.1
    
    3.0.9 – Jan 26, 2016
    
        Fixed issue where queries which specify sort and batch size can return results out of order if documents are concurrently updated. SERVER-19996
        Fixed performance issue where large amounts of create and drop collections can cause listDatabases to be slow under WiredTiger. SERVER-20961
        Modified the authentication failure message to include the client IP address. SERVER-22054
        All issues closed in 3.0.9
    
    3.0.8 – Dec 15, 2015
    
        Fixed issue where findAndModify on mongos can upsert to the wrong shard. SERVER-20407.
        Fixed WiredTiger commit visibility issue which caused document not found. SERVER-21275.
        Fixed issue where the oplog can grow to 3x configured size. SERVER-21553
        All issues closed in 3.0.8

--- databases/mongodb/patches/patch-src_mongo_util_intrusive__counter.h
+++ databases/mongodb/patches/patch-src_mongo_util_intrusive__counter.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_mongo_util_intrusive__counter.h,v 1.1 2015/05/05 15:54:49 joerg Exp $
+$NetBSD: patch-src_mongo_util_intrusive__counter.h,v 1.2 2016/02/12 03:37:24 ryoon Exp $
 
---- src/mongo/util/intrusive_counter.h.orig	2015-05-05 12:50:27.000000000 +0000
+--- src/mongo/util/intrusive_counter.h.orig	2016-01-05 18:31:44.000000000 +0000
 +++ src/mongo/util/intrusive_counter.h
 @@ -28,6 +28,7 @@
  
@@ -8,5 +8,5 @@ $NetBSD: patch-src_mongo_util_intrusive__counter.h,v 1.1 2015/05/05 15:54:49 joe
  
 +#include <cstdlib>
  #include <boost/intrusive_ptr.hpp>
- #include <boost/noncopyable.hpp>
- #include "mongo/platform/atomic_word.h"
+ #include <stdlib.h>
+ 
--- databases/mongodb/patches/patch-src_third__party_s2_base_port.h
+++ databases/mongodb/patches/patch-src_third__party_s2_base_port.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_third__party_s2_base_port.h,v 1.1 2015/05/02 08:10:33 ryoon Exp $
+$NetBSD: patch-src_third__party_s2_base_port.h,v 1.2 2016/02/12 03:37:24 ryoon Exp $
 
 * Add NetBSD support.
 
---- src/third_party/s2/base/port.h.orig	2015-04-08 20:28:08.000000000 +0000
+--- src/third_party/s2/base/port.h.orig	2016-01-05 18:31:44.000000000 +0000
 +++ src/third_party/s2/base/port.h
 @@ -102,7 +102,7 @@ typedef uint16_t u_int16_t;
  
  #endif
  
--#if defined __sunos__ || defined __freebsd__ || defined __openbsd__
-+#if defined __sunos__ || defined __freebsd__ || defined __openbsd__ || defined __netbsd__
+-#if defined __sun || defined __FreeBSD__ || defined __OpenBSD__
++#if defined __sun || defined __FreeBSD__ || defined __OpenBSD__ || defined __DragonFly__ || defined __NetBSD__
  #ifdef _LITTLE_ENDIAN
  #define IS_LITTLE_ENDIAN
  #elif defined _BIG_ENDIAN
@@ -17,7 +17,7 @@ $NetBSD: patch-src_third__party_s2_base_port.h,v 1.1 2015/05/02 08:10:33 ryoon E
  #define bswap_16(x) swap16(x)
  #define bswap_32(x) swap32(x)
  #define bswap_64(x) swap64(x)
-+#elif defined __netbsd__
++#elif defined __NetBSD__
 +#include <machine/bswap.h>
 +#define bswap_16(x) bswap16(x)
 +#define bswap_32(x) bswap32(x)
