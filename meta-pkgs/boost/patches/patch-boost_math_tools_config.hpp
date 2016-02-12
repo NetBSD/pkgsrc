20ee2faa4fd827e7f6af1fdfc36c058f0fcd3819
ryoon <ryoon@pkgsrc.org>
2016-02-12 14:24:01 +0000

    Disable long double math functions for NetBSD and FreeBSD. Bump PKGREVISION for devel/boost-headers and devel/boost-libs.
    This fixes databases/mongodb build.

--- meta-pkgs/boost/patches/patch-boost_math_tools_config.hpp
+++ meta-pkgs/boost/patches/patch-boost_math_tools_config.hpp
@@ -1,18 +1,9 @@
-$NetBSD: patch-boost_math_tools_config.hpp,v 1.2 2015/12/28 13:52:18 ryoon Exp $
+$NetBSD: patch-boost_math_tools_config.hpp,v 1.3 2016/02/12 14:24:01 ryoon Exp $
 
-Enable long double math functions on FreeBSD and NetBSD.
+* NetBSD has no int128_t
 
 --- boost/math/tools/config.hpp.orig	2015-12-11 18:00:52.000000000 +0000
 +++ boost/math/tools/config.hpp
-@@ -28,7 +28,7 @@
- 
- #include <boost/math/tools/user.hpp>
- 
--#if (defined(__CYGWIN__) || defined(__FreeBSD__) || defined(__NetBSD__) \
-+#if (defined(__CYGWIN__) \
-    || (defined(__hppa) && !defined(__OpenBSD__)) || (defined(__NO_LONG_DOUBLE_MATH) && (DBL_MANT_DIG != LDBL_MANT_DIG))) \
-    && !defined(BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS)
- #  define BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
 @@ -279,7 +279,7 @@
  //
  // And then the actual configuration:
