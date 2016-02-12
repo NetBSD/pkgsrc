48c47a8d029ff1e24bb087ed0d0b347accda8ddc
ryoon <ryoon@pkgsrc.org>
2016-02-12 03:37:24 +0000

    Update to 3.2.1
    * Change JavaScript engine from V8 to SpiderMonkey.
    
    Changelog:
    3.2.1 – Jan 12, 2016
    
        Fixed error where during a regular shutdown of a replica set, secondaries may mark certain replicated but yet to be applied operations as successfully applied: SERVER-21868.
        Improve insert workload performance with WiredTiger on Windows: SERVER-20262.
        Fixed long-running transactions during chunk moves: SERVER-21366
        All issues closed in 3.2.1
    
    3.0.9 – Jan 26, 2016
    
        Fixed issue where queries which specify sort and batch size can return results out of order if documents are concurrently updated. SERVER-19996
        Fixed performance issue where large amounts of create and drop collections can cause listDatabases to be slow under WiredTiger. SERVER-20961
        Modified the authentication failure message to include the client IP address. SERVER-22054
        All issues closed in 3.0.9
    
    3.0.8 – Dec 15, 2015
    
        Fixed issue where findAndModify on mongos can upsert to the wrong shard. SERVER-20407.
        Fixed WiredTiger commit visibility issue which caused document not found. SERVER-21275.
        Fixed issue where the oplog can grow to 3x configured size. SERVER-21553
        All issues closed in 3.0.8

--- databases/mongodb/patches/patch-src_mongo_platform_random.cpp
+++ databases/mongodb/patches/patch-src_mongo_platform_random.cpp
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_mongo_platform_random.cpp,v 1.2 2015/08/26 05:03:00 fhajny Exp $
+$NetBSD: patch-src_mongo_platform_random.cpp,v 1.3 2016/02/12 03:37:24 ryoon Exp $
 
 Add NetBSD support.
---- src/mongo/platform/random.cpp.orig	2015-08-24 00:39:52.000000000 +0000
+--- src/mongo/platform/random.cpp.orig	2016-01-05 18:31:44.000000000 +0000
 +++ src/mongo/platform/random.cpp
-@@ -116,7 +116,7 @@ SecureRandom* SecureRandom::create() {
+@@ -145,7 +145,7 @@ SecureRandom* SecureRandom::create() {
      return new WinSecureRandom();
  }
  
--#elif defined(__linux__) || defined(__sunos__) || defined(__APPLE__) || defined(__freebsd__)
-+#elif defined(__linux__) || defined(__sunos__) || defined(__APPLE__) || defined(__freebsd__) || defined(__netbsd__)
+-#elif defined(__linux__) || defined(__sun) || defined(__APPLE__) || defined(__FreeBSD__)
++#elif defined(__linux__) || defined(__sun) || defined(__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__)
  
  class InputStreamSecureRandom : public SecureRandom {
  public:
