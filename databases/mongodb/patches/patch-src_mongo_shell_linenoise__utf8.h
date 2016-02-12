6e4588d867a29c3d199127c8e967d261482cd3b1
ryoon <ryoon@pkgsrc.org>
2016-02-12 03:37:24 +0000

    Update to 3.2.1
    * Change JavaScript engine from V8 to SpiderMonkey.
    
    Changelog:
    3.2.1 – Jan 12, 2016
    
        Fixed error where during a regular shutdown of a replica set, secondaries may mark certain replicated but yet to be applied operations as successfully applied: SERVER-21868.
        Improve insert workload performance with WiredTiger on Windows: SERVER-20262.
        Fixed long-running transactions during chunk moves: SERVER-21366
        All issues closed in 3.2.1
    
    3.0.9 – Jan 26, 2016
    
        Fixed issue where queries which specify sort and batch size can return results out of order if documents are concurrently updated. SERVER-19996
        Fixed performance issue where large amounts of create and drop collections can cause listDatabases to be slow under WiredTiger. SERVER-20961
        Modified the authentication failure message to include the client IP address. SERVER-22054
        All issues closed in 3.0.9
    
    3.0.8 – Dec 15, 2015
    
        Fixed issue where findAndModify on mongos can upsert to the wrong shard. SERVER-20407.
        Fixed WiredTiger commit visibility issue which caused document not found. SERVER-21275.
        Fixed issue where the oplog can grow to 3x configured size. SERVER-21553
        All issues closed in 3.0.8

--- databases/mongodb/patches/patch-src_mongo_shell_linenoise__utf8.h
+++ databases/mongodb/patches/patch-src_mongo_shell_linenoise__utf8.h
@@ -1,17 +1,17 @@
-$NetBSD: patch-src_mongo_shell_linenoise__utf8.h,v 1.2 2015/08/26 05:03:00 fhajny Exp $
+$NetBSD: patch-src_mongo_shell_linenoise__utf8.h,v 1.3 2016/02/12 03:37:24 ryoon Exp $
 
 Use Boost swap instead of std. Fixes build on SunOS.
---- src/mongo/shell/linenoise_utf8.h.orig	2015-08-24 00:39:52.000000000 +0000
+--- src/mongo/shell/linenoise_utf8.h.orig	2016-01-05 18:31:44.000000000 +0000
 +++ src/mongo/shell/linenoise_utf8.h
 @@ -27,6 +27,7 @@
   *    then also delete it in the license file.
   */
  
 +#include <boost/core/swap.hpp>
- #include <boost/smart_ptr/scoped_array.hpp>
  #include <algorithm>
+ #include <memory>
  #include <string.h>
-@@ -173,9 +174,9 @@ struct UtfStringMixin {
+@@ -178,9 +179,9 @@ struct UtfStringMixin {
      }
  
      void swap(UtfStringMixin& other) {
