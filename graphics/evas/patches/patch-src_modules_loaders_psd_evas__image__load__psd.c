2ad698121f7a37d61088cc0c17088d187704abbf
jperkin <jperkin@pkgsrc.org>
2016-02-14 13:14:06 +0000

    Fix build on SunOS.

--- graphics/evas/patches/patch-src_modules_loaders_psd_evas__image__load__psd.c
+++ graphics/evas/patches/patch-src_modules_loaders_psd_evas__image__load__psd.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_modules_loaders_psd_evas__image__load__psd.c,v 1.3 2013/06/16 18:56:05 sno Exp $
+$NetBSD: patch-src_modules_loaders_psd_evas__image__load__psd.c,v 1.4 2016/02/14 13:14:06 jperkin Exp $
 
 jperkin knows o.O (but didn't use pkglint)
 
---- src/modules/loaders/psd/evas_image_load_psd.c.orig	2011-08-11 07:46:01.000000000 +0200
-+++ src/modules/loaders/psd/evas_image_load_psd.c	2011-12-27 15:15:11.097276116 +0100
+--- src/modules/loaders/psd/evas_image_load_psd.c.orig	2012-11-23 16:35:06.000000000 +0000
++++ src/modules/loaders/psd/evas_image_load_psd.c
 @@ -1,4 +1,6 @@
-+#ifndef _XOPEN_SOURCE
++#if !defined(_XOPEN_SOURCE) && !defined(__sun)
  #define _XOPEN_SOURCE
 +#endif
  
