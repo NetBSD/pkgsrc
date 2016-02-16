a31b5bafcd3be991c751eb0f0114b34f5c793322
dbj <dbj@pkgsrc.org>
2016-02-16 03:37:30 +0000

    help dependencies find the correct version of wxGTK This is a partial fix for PR pkg/50389

--- x11/wxGTK30/buildlink3.mk
+++ x11/wxGTK30/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2015/04/25 14:25:20 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2016/02/16 03:37:30 dbj Exp $
 
 BUILDLINK_TREE+=	wxGTK30
 
@@ -8,6 +8,11 @@ WXGTK30_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.wxGTK30+=	wxGTK30>=3.0.1
 BUILDLINK_ABI_DEPENDS.wxGTK30+=	wxGTK30>=3.0.2nb2
 BUILDLINK_PKGSRCDIR.wxGTK30?=	../../x11/wxGTK30
+
+BUILDLINK_INCDIRS.wxGTK30+=	include/wx-3.0
+PREPEND_PATH+=      		${PREFIX}/libexec/wx-3.0
+CONFIGURE_ARGS+=		--with-wx-version=3.0
+CONFIGURE_ARGS+=		--with-wx-config=${PREFIX}/libexec/wx-3.0/wx-config
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
