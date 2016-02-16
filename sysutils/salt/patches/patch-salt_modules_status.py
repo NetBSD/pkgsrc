2b34a802934f404e1b685677eb68b2d34c4ed8b6
khorben <khorben@pkgsrc.org>
2016-02-16 01:52:34 +0000

    Avoid a crash on NetBSD when not every minion is present
    From the pull request pending, #31320:
    
      On NetBSD, Salt currently defaults to using lsof(8) to determine which
      minions are connected. It is however not always available, and even
      then quite unreliable. I found that just like on FreeBSD, sockstat(1)
      is a much safer alternative. Unfortunately its output is not exactly
      the same on NetBSD, where the port delimiter is a dot character
      instead. As a consequence I have decided to duplicate the relevant
      function for NetBSD; let me know if I should try to re-use the code
      supporting FreeBSD instead.
    
    See also https://github.com/saltstack/salt/pull/31230.

--- sysutils/salt/patches/patch-salt_modules_status.py
+++ sysutils/salt/patches/patch-salt_modules_status.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-salt_modules_status.py,v 1.1 2016/02/04 22:05:36 khorben Exp $
+$NetBSD: patch-salt_modules_status.py,v 1.2 2016/02/16 01:52:34 khorben Exp $
 
 Avoid a crash in "status.diskusage" when not on Linux or FreeBSD
 
---- salt/modules/status.py.orig	2016-02-04 21:56:06.000000000 +0000
+--- salt/modules/status.py.orig	2016-02-01 19:40:31.000000000 +0000
 +++ salt/modules/status.py
-@@ -443,6 +443,8 @@ def diskusage(*args):
+@@ -457,6 +457,8 @@ def diskusage(*args):
              ifile = salt.utils.fopen(procf, 'r').readlines()
          elif __grains__['kernel'] == 'FreeBSD':
              ifile = __salt__['cmd.run']('mount -p').splitlines()
