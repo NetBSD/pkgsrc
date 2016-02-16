130763f9d7e2c4de438d14e6407fe9020c87ffb8
fhajny <fhajny@pkgsrc.org>
2016-02-16 15:17:51 +0000

    Fix deps missing from bl3.

--- sysutils/liblognorm/buildlink3.mk
+++ sysutils/liblognorm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/09/04 19:53:36 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/02/16 15:17:51 fhajny Exp $
 
 BUILDLINK_TREE+=	liblognorm
 
@@ -8,6 +8,8 @@ LIBLOGNORM_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.liblognorm+=	liblognorm>=1.0.0
 BUILDLINK_PKGSRCDIR.liblognorm?=	../../sysutils/liblognorm
 
+.include "../../devel/libestr/buildlink3.mk"
+.include "../../textproc/json-c/buildlink3.mk"
 .endif # LIBLOGNORM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-liblognorm
