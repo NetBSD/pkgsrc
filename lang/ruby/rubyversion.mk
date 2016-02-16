e51ea86a760cec4673aeef40252aaa17f7736f1f
taca <taca@pkgsrc.org>
2016-02-16 14:16:43 +0000

    Update ruby-rdoc to 4.2.2.
    === 4.2.2 / 2016-02-09
    
    * Bug fixes
      * Include lib/rdoc/generator/pot/* in built gem

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.157 2016/02/06 10:59:55 taca Exp $
+# $NetBSD: rubyversion.mk,v 1.158 2016/02/16 14:16:43 taca Exp $
 #
 
 # This file determines which Ruby version is used as a dependency for
@@ -250,7 +250,7 @@ RUBY23_API_VERSION=	2.3.0
 RUBY_GEMS_PKGSRC_VERS=	2.4.8
 
 # pkgsrc's rdoc's version
-RUBY_RDOC_PKGSRC_VERS=	4.2.1
+RUBY_RDOC_PKGSRC_VERS=	4.2.2
 
 #
 RUBY_VERSION_DEFAULT?=	200
