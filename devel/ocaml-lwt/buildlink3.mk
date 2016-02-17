17eb446dd72b1439fb0c943545bb300fe4374aad
jaapb <jaapb@pkgsrc.org>
2016-02-17 17:00:38 +0000

    Revamped the ocaml-lwt package. No upstream changes, but I added two compilation options.

--- devel/ocaml-lwt/buildlink3.mk
+++ devel/ocaml-lwt/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/12/14 14:32:32 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/02/17 17:00:38 jaapb Exp $
 
-BUILDLINK_TREE+=	ocamllwt
+BUILDLINK_TREE+=	ocaml-lwt
 
-.if !defined(OCAMLLWT_BUILDLINK3_MK)
-OCAMLLWT_BUILDLINK3_MK:=
+.if !defined(OCAML_LWT_BUILDLINK3_MK)
+OCAML_LWT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocamllwt+=	ocaml-lwt>=2.4.2
-BUILDLINK_PKGSRCDIR.ocamllwt?=	../../devel/ocaml-lwt
+BUILDLINK_API_DEPENDS.ocaml-lwt+=	ocaml-lwt>=2.4.2
+BUILDLINK_PKGSRCDIR.ocaml-lwt?=	../../devel/ocaml-lwt
 
 .include "../../devel/libev/buildlink3.mk"
 .endif	# OCAMLLWT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-ocamllwt
+BUILDLINK_TREE+=	-ocaml-lwt
