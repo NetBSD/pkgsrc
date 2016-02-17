c2beb4b99f93f70cccbecfd305196ad5e78b0c9e
jaapb <jaapb@pkgsrc.org>
2016-02-17 17:00:38 +0000

    Revamped the ocaml-lwt package. No upstream changes, but I added two compilation options.

--- devel/ocaml-lwt/options.mk
+++ devel/ocaml-lwt/options.mk
@@ -1,8 +1,9 @@
-# $NetBSD: options.mk,v 1.1 2012/05/30 13:40:59 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2016/02/17 17:00:38 jaapb Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ocaml-lwt
-PKG_SUPPORTED_OPTIONS=	ssl
-PKG_SUGGESTED_OPTIONS=	ssl
+PKG_SUPPORTED_OPTIONS=	ssl ppx camlp4
+PKG_SUGGESTED_OPTIONS=	ssl ppx camlp4
+PLIST_VARS+=	ppx camlp4
 
 .include "../../mk/bsd.prefs.mk"
 
@@ -16,4 +17,25 @@ PKG_SUGGESTED_OPTIONS=	ssl
 CONFIGURE_ARGS+=	--enable-ssl
 .else
 CONFIGURE_ARGS+=	--disable-ssl
+.endif
+
+###
+### Camlp4 support
+###
+.if !empty(PKG_OPTIONS:Mcamlp4)
+.include "../../lang/camlp4/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-camlp4
+PLIST.camlp4=	yes
+.else
+CONFIGURE_ARGS+=	--disable-camlp4
+.endif
+
+###
+### SSL support
+###
+.if !empty(PKG_OPTIONS:Mppx)
+CONFIGURE_ARGS+=	--enable-ppx
+PLIST.ppx=	yes
+.else
+CONFIGURE_ARGS+=	--disable-ppx
 .endif
