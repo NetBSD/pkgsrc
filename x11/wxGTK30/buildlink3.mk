448689c6dbddff4fefc6c21da1909546878f1176
dbj <dbj>
2016-02-17 10:16:05 +0000

    build wxGTK30 using cocoa by default on Darwin we might want to consider renaming this package

--- x11/wxGTK30/buildlink3.mk
+++ x11/wxGTK30/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2016/02/16 03:37:30 dbj Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2016/02/17 10:16:05 dbj Exp $
 
 BUILDLINK_TREE+=	wxGTK30
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	wxGTK30
 WXGTK30_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.wxGTK30+=	wxGTK30>=3.0.1
-BUILDLINK_ABI_DEPENDS.wxGTK30+=	wxGTK30>=3.0.2nb2
+BUILDLINK_ABI_DEPENDS.wxGTK30+=	wxGTK30>=3.0.2nb5
 BUILDLINK_PKGSRCDIR.wxGTK30?=	../../x11/wxGTK30
 
 BUILDLINK_INCDIRS.wxGTK30+=	include/wx-3.0
@@ -16,14 +16,16 @@ CONFIGURE_ARGS+=		--with-wx-config=${PREFIX}/libexec/wx-3.0/wx-config
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
-.include "../../graphics/MesaLib/buildlink3.mk"
-.include "../../graphics/glu/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
+.if ${OPSYS} != "Darwin"
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .include "../../x11/libSM/buildlink3.mk"
 .include "../../x11/libXxf86vm/buildlink3.mk"
+.endif
 .endif # WXGTK30_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-wxGTK30
