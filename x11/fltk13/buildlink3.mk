dfa604788b2e3d357d94deb6c415cdda66461bea
dbj <dbj@pkgsrc.org>
2016-02-17 06:05:41 +0000

    On Darwin, this package builds against quartz, not x11, so remove unnecessary dependencies

--- x11/fltk13/buildlink3.mk
+++ x11/fltk13/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2016/01/26 08:47:28 dbj Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2016/02/17 06:05:41 dbj Exp $
 
 BUILDLINK_TREE+=	fltk
 
@@ -10,14 +10,16 @@ BUILDLINK_ABI_DEPENDS.fltk?=	fltk>=1.3.3nb1
 BUILDLINK_PKGSRCDIR.fltk?=	../../x11/fltk13
 BUILDLINK_FILES.fltk+=		include/Fl/*
 
+.if ${OPSYS} != "Darwin"
 .include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../graphics/glu/buildlink3.mk"
-.include "../../mk/jpeg.buildlink3.mk"
-.include "../../graphics/png/buildlink3.mk"
-.include "../../mk/pthread.buildlink3.mk"
 .include "../../x11/libXext/buildlink3.mk"
 .include "../../x11/libXft/buildlink3.mk"
 .include "../../x11/libXinerama/buildlink3.mk"
+.endif
+.include "../../mk/jpeg.buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
 .endif # FLTK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-fltk
