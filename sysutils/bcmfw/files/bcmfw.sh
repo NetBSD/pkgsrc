7497fb96b9469f1319dd8b9d23e381bac2fd8475
plunky <plunky@pkgsrc.org>
2016-02-17 10:58:19 +0000

    update to bcmfw version 2.0
    this version now handles modern Broadcom chips, which attach
    as normal with the builtin firmware but can be upgraded over
    the Host Controller Interface.

--- sysutils/bcmfw/files/bcmfw.sh
+++ sysutils/bcmfw/files/bcmfw.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: bcmfw.sh,v 1.1.1.1 2006/12/07 20:30:48 plunky Exp $
+# $NetBSD: bcmfw.sh,v 1.2 2016/02/17 10:58:19 plunky Exp $
 #
 
 # PROVIDE: bcmfw
@@ -12,7 +12,7 @@ $_rc_subr_loaded . /etc/rc.subr
 name="bcmfw"
 rcvar=${name}
 command="@PREFIX@/sbin/${name}"
-command_args="${bcmfw_args:-ugen0}"
+command_args="${bcmfw_args:--q ugen0}"
 
 load_rc_config ${name}
 run_rc_command "$1"
