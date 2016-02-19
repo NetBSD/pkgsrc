4d76f23f822ab7851eaf3f6ebb54048296fbe15d
khorben <khorben@pkgsrc.org>
2016-02-19 20:32:04 +0000

    Package DeforaOS libSystem 0.3.0
    Among the changes:
    - more convenience functions for configuration files
    - reworked the error-handling API
    - progress with the variable conversion framework
    
    Unfortunately this will break a number of packages using this library:
    there is an API change in the Error class. I will update them all as soon
    as I can.

--- devel/deforaos-libsystem/buildlink3.mk
+++ devel/deforaos-libsystem/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2015/10/18 12:01:19 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/02/19 20:32:04 khorben Exp $
 
 BUILDLINK_TREE+=	deforaos-libsystem
 
 .if !defined(DEFORAOS_LIBSYSTEM_BUILDLINK3_MK)
 DEFORAOS_LIBSYSTEM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-libsystem+=	deforaos-libsystem>=0.2.3
+BUILDLINK_API_DEPENDS.deforaos-libsystem+=	deforaos-libsystem>=0.3.0
 BUILDLINK_PKGSRCDIR.deforaos-libsystem?=	../../devel/deforaos-libsystem
 
 .endif	# DEFORAOS_LIBSYSTEM_BUILDLINK3_MK
