e4392ae686366a30c45afa79da2845ee3799b95c
agc <agc@pkgsrc.org>
2016-02-19 22:41:50 +0000

    Remove an item from the netpgpverify/libnetpgpverify TODO list:
    + get rid of calls to snprintf which simply add the returned value to
    the number of characters used so far. This practice is unsafe. Instead,
    use a dynamic buffer and grow its size to accommodate the contents.
    
    + add USE_ARG definition to some files which use it but don't check to
    see that it's been defined
    
    pkgsrc changes:
    
    + Bump version number to 20160214
    
    + Use the same method as libnetpgpverify for finding the version number
    from the sources.

--- security/netpgpverify/files/verify.h
+++ security/netpgpverify/files/verify.h
@@ -23,9 +23,9 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #ifndef NETPGP_VERIFY_H_
-#define NETPGP_VERIFY_H_	20151103
+#define NETPGP_VERIFY_H_	20160214
 
-#define NETPGPVERIFY_VERSION	"netpgpverify portable 20151103"
+#define NETPGPVERIFY_VERSION	"netpgpverify portable 20160214"
 
 #include <sys/types.h>
 
