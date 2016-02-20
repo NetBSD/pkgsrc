247ded5d00a06b98569a0b2ff5b9284e4b9cf14e
khorben <khorben@pkgsrc.org>
2016-02-20 00:12:55 +0000

    Package DeforaOS Browser 0.5.0
    This release brings:
    - compatibility with Gtk+ 3
    - new plug-in, "Places" (for Gtk+ 3 only)
    - file manager now available as a Widget plug-in
    - improvements to the "Favorites" and "Preview" plug-ins
    - further bugfixes and improvements

--- sysutils/deforaos-browser/buildlink3.mk
+++ sysutils/deforaos-browser/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2015/10/05 23:43:55 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2016/02/20 00:12:55 khorben Exp $
 
 BUILDLINK_TREE+=	deforaos-browser
 
 .if !defined(DEFORAOS_BROWSER_BUILDLINK3_MK)
 DEFORAOS_BROWSER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-browser+=	deforaos-browser>=0.4.14
-BUILDLINK_ABI_DEPENDS.deforaos-browser?=	deforaos-browser>=0.4.14
+BUILDLINK_API_DEPENDS.deforaos-browser+=	deforaos-browser>=0.5.0
+BUILDLINK_ABI_DEPENDS.deforaos-browser?=	deforaos-browser>=0.5.0
 BUILDLINK_PKGSRCDIR.deforaos-browser?=	../../sysutils/deforaos-browser
 
 .include "../../x11/deforaos-libdesktop/buildlink3.mk"
