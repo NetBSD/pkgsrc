62820d82aaa0df577fc6083ff70726e813bc1776
khorben <khorben@pkgsrc.org>
2016-02-20 03:03:42 +0000

    Fix the path to devel/deforaos-cpp

--- devel/deforaos-cpp/buildlink3.mk
+++ devel/deforaos-cpp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/02/20 02:54:45 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/02/20 03:03:42 khorben Exp $
 #
 
 BUILDLINK_TREE+=	deforaos-cpp
@@ -8,7 +8,7 @@ DEFORAOS_CPP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.deforaos-cpp+=	deforaos-cpp>=0.0.3
 BUILDLINK_ABI_DEPENDS.deforaos-cpp?=	deforaos-cpp>=0.0.3
-BUILDLINK_PKGSRCDIR.deforaos-cpp?=	../../x11/deforaos-cpp
+BUILDLINK_PKGSRCDIR.deforaos-cpp?=	../../devel/deforaos-cpp
 
 .include "../../devel/deforaos-libsystem/buildlink3.mk"
 .endif	# DEFORAOS_CPP_BUILDLINK3_MK
