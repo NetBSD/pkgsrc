2dbbd6ebfeea66b07b9c2d932785071da33de0e2
khorben <khorben@pkgsrc.org>
2016-02-20 00:47:39 +0000

    Update buildlink3.mk as well

--- x11/deforaos-panel/buildlink3.mk
+++ x11/deforaos-panel/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2015/09/02 23:18:45 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2016/02/20 00:47:39 khorben Exp $
 
 BUILDLINK_TREE+=	deforaos-panel
 
 .if !defined(DEFORAOS_PANEL_BUILDLINK3_MK)
 DEFORAOS_PANEL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-panel+=	deforaos-panel>=0.3.2
-BUILDLINK_ABI_DEPENDS.deforaos-panel?=	deforaos-panel>=0.3.2
+BUILDLINK_API_DEPENDS.deforaos-panel+=	deforaos-panel>=0.3.6
+BUILDLINK_ABI_DEPENDS.deforaos-panel?=	deforaos-panel>=0.3.6
 BUILDLINK_PKGSRCDIR.deforaos-panel?=	../../x11/deforaos-panel
 
 .include "../../devel/deforaos-libsystem/buildlink3.mk"
