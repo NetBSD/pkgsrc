7d0de2ee78558cc3e11a5f1d02ef0e405f421719
fhajny <fhajny@pkgsrc.org>
2016-02-21 14:10:46 +0000

    Update graphics/opencv to 3.1.0.
    * A lot of new functionality has been introduced during GSoC 2015:
      - "Omnidirectional Cameras Calibration and Stereo 3D Reconstruction"
        opencv_contrib/ccalib module
      - "Structure From Motion" - opencv_contrib/sfm module
      - "Improved Deformable Part-based Models" - opencv_contrib/dpm module
      - "Real-time Multi-object Tracking using Kernelized Correlation Filter"
        - opencv_contrib/tracking module
      - "Improved and expanded Scene Text Detection" - opencv_contrib/text
        module
      - "Stereo correspondence improvements" - opencv_contrib/stereo module
      - "Structured-Light System Calibration" - opencv_contrib/structured_light
      - "Chessboard+ArUco for camera calibration" - opencv_contrib/aruco
      - "Implementation of universal interface for deep neural network
        frameworks" - opencv_contrib/dnn module
      - "Recent advances in edge-aware filtering, improved SGBM stereo
        algorithm" - opencv/calib3d and opencv_contrib/ximgproc
      - "Improved ICF detector, waldboost implementation"
        - opencv_contrib/xobjdetect
      - "Multi-target TLD tracking" - opencv_contrib/tracking module
      - "3D pose estimation using CNNs" - opencv_contrib/cnn_3dobj
    
    * Many great contributions made by the community, such as:
      - Support for HDF5 format
      - New/Improved optical flow algorithms
      - Multiple new image processing algorithms for filtering, segmentation
        and feature detection
      - Superpixel segmentation
    
    * IPPICV is now based on IPP 9.0.1, which should make OpenCV even faster
      on modern Intel chips
    * opencv_contrib modules can now be included into the opencv2.framework
      for iOS
    * Newest operating systems are supported: Windows 10 and OSX 10.11
      (Visual Studio 2015 and XCode 7.1.1)
    * Interoperability between T-API and OpenCL, OpenGL, DirectX and Video
      Acceleration API on Linux, as well as Android 5 camera.
    * HAL (Hardware Acceleration Layer) module functionality has been moved
      into corresponding basic modules; the HAL replacement mechanism has
      been implemented along with the examples
    
    See full changelog:
    
      https://github.com/Itseez/opencv/wiki/ChangeLog

--- graphics/opencv/patches/patch-cmake_OpenCVModule.cmake
+++ graphics/opencv/patches/patch-cmake_OpenCVModule.cmake
@@ -1,11 +1,11 @@
-$NetBSD: patch-cmake_OpenCVModule.cmake,v 1.1 2016/02/08 02:04:18 dbj Exp $
+$NetBSD: patch-cmake_OpenCVModule.cmake,v 1.2 2016/02/21 14:10:46 fhajny Exp $
 
 Fix library id handling on Darwin
 
---- cmake/OpenCVModule.cmake.orig	2015-06-03 17:21:34.000000000 +0000
+--- cmake/OpenCVModule.cmake.orig	2015-12-18 15:02:16.000000000 +0000
 +++ cmake/OpenCVModule.cmake
-@@ -754,7 +754,7 @@ macro(_ocv_create_module)
-     ARCHIVE_OUTPUT_DIRECTORY ${LIBRARY_OUTPUT_PATH}
+@@ -834,7 +834,7 @@ macro(_ocv_create_module)
+     COMPILE_PDB_OUTPUT_DIRECTORY ${LIBRARY_OUTPUT_PATH}
      LIBRARY_OUTPUT_DIRECTORY ${LIBRARY_OUTPUT_PATH}
      RUNTIME_OUTPUT_DIRECTORY ${EXECUTABLE_OUTPUT_PATH}
 -    INSTALL_NAME_DIR lib
