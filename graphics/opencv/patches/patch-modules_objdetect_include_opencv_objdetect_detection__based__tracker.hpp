83aa77cc9b0bdc238a2f6598c06e45d979152678
fhajny <fhajny@pkgsrc.org>
2016-02-21 14:10:46 +0000

    Update graphics/opencv to 3.1.0.
    * A lot of new functionality has been introduced during GSoC 2015:
      - "Omnidirectional Cameras Calibration and Stereo 3D Reconstruction"
        opencv_contrib/ccalib module
      - "Structure From Motion" - opencv_contrib/sfm module
      - "Improved Deformable Part-based Models" - opencv_contrib/dpm module
      - "Real-time Multi-object Tracking using Kernelized Correlation Filter"
        - opencv_contrib/tracking module
      - "Improved and expanded Scene Text Detection" - opencv_contrib/text
        module
      - "Stereo correspondence improvements" - opencv_contrib/stereo module
      - "Structured-Light System Calibration" - opencv_contrib/structured_light
      - "Chessboard+ArUco for camera calibration" - opencv_contrib/aruco
      - "Implementation of universal interface for deep neural network
        frameworks" - opencv_contrib/dnn module
      - "Recent advances in edge-aware filtering, improved SGBM stereo
        algorithm" - opencv/calib3d and opencv_contrib/ximgproc
      - "Improved ICF detector, waldboost implementation"
        - opencv_contrib/xobjdetect
      - "Multi-target TLD tracking" - opencv_contrib/tracking module
      - "3D pose estimation using CNNs" - opencv_contrib/cnn_3dobj
    
    * Many great contributions made by the community, such as:
      - Support for HDF5 format
      - New/Improved optical flow algorithms
      - Multiple new image processing algorithms for filtering, segmentation
        and feature detection
      - Superpixel segmentation
    
    * IPPICV is now based on IPP 9.0.1, which should make OpenCV even faster
      on modern Intel chips
    * opencv_contrib modules can now be included into the opencv2.framework
      for iOS
    * Newest operating systems are supported: Windows 10 and OSX 10.11
      (Visual Studio 2015 and XCode 7.1.1)
    * Interoperability between T-API and OpenCL, OpenGL, DirectX and Video
      Acceleration API on Linux, as well as Android 5 camera.
    * HAL (Hardware Acceleration Layer) module functionality has been moved
      into corresponding basic modules; the HAL replacement mechanism has
      been implemented along with the examples
    
    See full changelog:
    
      https://github.com/Itseez/opencv/wiki/ChangeLog

--- graphics/opencv/patches/patch-modules_objdetect_include_opencv_objdetect_detection__based__tracker.hpp
+++ graphics/opencv/patches/patch-modules_objdetect_include_opencv_objdetect_detection__based__tracker.hpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-modules_objdetect_include_opencv_objdetect_detection__based__tracker.hpp,v 1.1 2015/10/13 14:37:43 joerg Exp $
+$NetBSD: patch-modules_objdetect_include_opencv_objdetect_detection__based__tracker.hpp,v 1.2 2016/02/21 14:10:46 fhajny Exp $
 
---- modules/objdetect/include/opencv2/objdetect/detection_based_tracker.hpp.orig	2015-10-13 12:31:00.000000000 +0000
+--- modules/objdetect/include/opencv2/objdetect/detection_based_tracker.hpp.orig	2015-12-18 15:02:16.000000000 +0000
 +++ modules/objdetect/include/opencv2/objdetect/detection_based_tracker.hpp
-@@ -45,7 +45,7 @@
- #define __OPENCV_OBJDETECT_DBT_HPP__
+@@ -46,7 +46,7 @@
  
+ // After this condition removal update blacklist for bindings: modules/python/common.cmake
  #if defined(__linux__) || defined(LINUX) || defined(__APPLE__) || defined(__ANDROID__) || \
 -  (defined(__cplusplus) &&  __cplusplus > 201103L) || (defined(_MSC_VER) && _MSC_VER >= 1700)
 +  (defined(__cplusplus) &&  __cplusplus >= 201103L) || (defined(_MSC_VER) && _MSC_VER >= 1700)
