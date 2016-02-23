631401e57a766ed867ea7c5e80ed0ed301a6396b
jperkin <jperkin@pkgsrc.org>
2016-02-23 11:31:54 +0000

    Use the compat strndup on Darwin 10.6 and older too.

--- x11/xdg-user-dirs/patches/patch-xdg-user-dirs-update.c
+++ x11/xdg-user-dirs/patches/patch-xdg-user-dirs-update.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-xdg-user-dirs-update.c,v 1.1 2015/06/17 17:31:38 tron Exp $
+$NetBSD: patch-xdg-user-dirs-update.c,v 1.2 2016/02/23 11:31:54 jperkin Exp $
 
-Fix build under Solaris 10.
+Fix build under Solaris 10 and Darwin 10.6.
 
---- xdg-user-dirs-update.c.orig	2007-08-20 09:12:20.000000000 +0100
-+++ xdg-user-dirs-update.c	2015-06-17 13:18:17.000000000 +0100
-@@ -328,6 +328,24 @@
+--- xdg-user-dirs-update.c.orig	2013-06-26 15:38:46.000000000 +0000
++++ xdg-user-dirs-update.c
+@@ -328,6 +328,24 @@ freev (char **strs)
      }
  }
  
-+#ifdef __sun__
++#if defined(__sun__) || (defined(__APPLE__) && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__-0 < 1070)
 +static char *
 +my_strndup(const char *s, size_t n)
 +{
