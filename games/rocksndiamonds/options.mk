32470c0d201e6264cc9227812b7204cdddc4902c
jperkin <jperkin@pkgsrc.org>
2016-02-25 13:15:15 +0000

    Remove broken OPSYS tests.

--- games/rocksndiamonds/options.mk
+++ games/rocksndiamonds/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2009/11/01 15:46:07 tnn Exp $
+# $NetBSD: options.mk,v 1.6 2016/02/25 13:15:15 jperkin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.rocksndiamonds
 PKG_SUPPORTED_OPTIONS=	sdl x11
@@ -7,11 +7,7 @@ PKG_SUGGESTED_OPTIONS=	sdl
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Msdl)
-.  if ${OPSYS} == "Solaris"
-BUILD_TARGET=	solaris-sdl
-.  else
 BUILD_TARGET=	sdl
-.  endif
 .  include "../../audio/SDL_mixer/buildlink3.mk"
 .  include "../../graphics/SDL_image/buildlink3.mk"
 .  include "../../multimedia/smpeg/buildlink3.mk"
@@ -19,11 +15,7 @@ BUILD_TARGET=	sdl
 .endif
 
 .if !empty(PKG_OPTIONS:Mx11)
-.  if ${OPSYS} == "Solaris"
-BUILD_TARGET=	solaris
-.  else
 BUILD_TARGET=	x11
-.  endif
 .  include "../../mk/oss.buildlink3.mk"
 .  include "../../x11/libXt/buildlink3.mk"
 .endif
