36911d99c41be5e6247948e34e7c9233ab7c9f2b
tnn <tnn@pkgsrc.org>
2016-02-25 21:37:35 +0000

    Add postgresql95 packages, converted from corresponding postgresql94 packages. postgresql95-upgrade is gone, the pg_upgrade tool is shipped with postgresql95-client now.
    Major enhancements in PostgreSQL 9.5 include:
    
      Allow INSERTs that would generate constraint conflicts to be turned into
        UPDATEs or ignored
      Add GROUP BY analysis features GROUPING SETS, CUBE and ROLLUP
      Add row-level security control
      Create mechanisms for tracking the progress of replication, including
        methods for identifying the origin of individual changes during logical
        replication
      Add Block Range Indexes (BRIN)
      Substantial performance improvements for sorting
      Substantial performance improvements for multi-CPU machines

---
+++ databases/postgresql95/patches/patch-src_backend_utils_adt_datetime.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-src_backend_utils_adt_datetime.c,v 1.1 2016/02/25 21:37:35 tnn Exp $
+
+--- src/backend/utils/adt/datetime.c.orig	2014-12-16 01:07:34.000000000 +0000
++++ src/backend/utils/adt/datetime.c
+@@ -31,6 +31,9 @@
+ #include "utils/memutils.h"
+ #include "utils/tzparser.h"
+ 
++#if defined(__NetBSD__)
++#define strtoi pg_strtoi
++#endif
+ 
+ static int DecodeNumber(int flen, char *field, bool haveTextMonth,
+ 			 int fmask, int *tmask,
