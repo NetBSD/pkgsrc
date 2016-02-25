5716b96e65f30cdadd6e9188c9af4e85b2bd18b3
jperkin <jperkin@pkgsrc.org>
2016-02-25 23:35:44 +0000

    Revert OPSYSVARS change, variables don't support lazy evaluation.

--- multimedia/libva/available.mk
+++ multimedia/libva/available.mk
@@ -1,12 +1,18 @@
-# $NetBSD: available.mk,v 1.3 2016/02/25 15:49:44 jperkin Exp $
+# $NetBSD: available.mk,v 1.4 2016/02/25 23:35:44 jperkin Exp $
+
+.include "../../mk/bsd.prefs.mk"
 
 # At the moment VAAPI is available only for XXX.  The following
 # condition is here to NOT list these platforms in multiple places,
 # i.e. in mplayer, xine, xbmc etc.  Have a look at buildlink3.mk too.
 
 # The following should be kept up-to-date!
-OPSYSVARS+=			VAAPI_AVAILABLE
-VAAPI_AVAILABLE.Linux=		yes
-VAAPI_AVAILABLE.FreeBSD=	yes
-VAAPI_AVAILABLE.OpenBSD=	yes
-VAAPI_AVAILABLE.*=		no
+.if ${OPSYS} == "Linux"
+VAAPI_AVAILABLE=	yes
+.elif ${OPSYS} == "FreeBSD"
+VAAPI_AVAILABLE=	yes
+.elif ${OPSYS} == "OpenBSD"
+VAAPI_AVAILABLE=	yes
+.else
+VAAPI_AVAILABLE=	no
+.endif
