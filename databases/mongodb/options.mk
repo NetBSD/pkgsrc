dfa3f3a8b8bdd46c2908192982e669086a0a6010
fhajny <fhajny@pkgsrc.org>
2016-02-26 12:33:26 +0000

    Make wiredtiger an option, only make it available on 64bit platforms.

--- databases/mongodb/options.mk
+++ databases/mongodb/options.mk
@@ -1,11 +1,25 @@
-# $NetBSD: options.mk,v 1.1 2015/05/02 08:10:33 ryoon Exp $
+# $NetBSD: options.mk,v 1.2 2016/02/26 12:33:26 fhajny Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mongodb
-PKG_SUPPORTED_OPTIONS=	ssl
+PKG_SUPPORTED_OPTIONS=	ssl wiredtiger
+PKG_SUGGESTED_OPTIONS=	#
+
+.if !empty(MACHINE_ARCH:Mx86_64)
+PKG_SUGGESTED_OPTIONS+=	wiredtiger
+.endif
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mssl)
 .include "../../security/openssl/buildlink3.mk"
 SCONS_ARGS+=		--ssl
+.endif
+
+.if !empty(PKG_OPTIONS:Mwiredtiger)
+.  if empty(MACHINE_ARCH:Mx86_64)
+PKG_FAIL_REASON+=	"WiredTiger is not supported on 32-bit platforms"
+.  endif
+SCONS_ARGS+=		--wiredtiger=on
+.else
+SCONS_ARGS+=		--wiredtiger=off
 .endif
