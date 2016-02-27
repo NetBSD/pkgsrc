23fb4c3734a73615bb20365e9fa91addbf2c63f0
tsutsui <tsutsui@pkgsrc.org>
2016-02-27 02:30:24 +0000

    Update mikutter to 3.3.8.
    Upstream changes:
    
    * cannot preview instagram pictures
    * twitter authentication on the first startup didn't work with
      some library version
    
    Remarks:
    
    OAuth::RequestToken#build_authorize_url is broken in the latest
    oauth gem 0.5.0:
    https://github.com/oauth-xx/oauth-ruby/pull/108

--- net/mikutter/patches/patch-core_plugin_photo__support_photo__support.rb
+++ net/mikutter/patches/patch-core_plugin_photo__support_photo__support.rb
@@ -1,8 +1,8 @@
-$NetBSD: patch-core_plugin_photo__support_photo__support.rb,v 1.1 2016/01/09 19:36:12 tsutsui Exp $
+$NetBSD: patch-core_plugin_photo__support_photo__support.rb,v 1.2 2016/02/27 02:30:24 tsutsui Exp $
 
 - temporary workaround to skip totorijp gems (sorry, @toshi_a)
 
---- core/plugin/photo_support/photo_support.rb.orig	2015-12-27 21:42:08.000000000 +0000
+--- core/plugin/photo_support/photo_support.rb.orig	2016-02-26 09:02:04.000000000 +0000
 +++ core/plugin/photo_support/photo_support.rb
 @@ -1,7 +1,7 @@
  # coding: utf-8
@@ -12,8 +12,8 @@ $NetBSD: patch-core_plugin_photo__support_photo__support.rb,v 1.1 2016/01/09 19:
 +#require 'totoridipjp'
  
  module Plugin::PhotoSupport
-   INSTAGRAM_PATTERN = %r{^https?://(?:instagr\.am|instagram\.com)/p/([a-zA-Z0-9_\-]+)}
-@@ -178,9 +178,14 @@ Plugin.create :photo_support do
+   INSTAGRAM_PATTERN = %r{\Ahttps?://(?:instagr\.am|(?:www\.)?instagram\.com)/p/([a-zA-Z0-9_\-]+)/}
+@@ -182,9 +182,14 @@ Plugin.create :photo_support do
  
    # totori.dip.jp
    defimageopener('totori.dip.jp', %r#\Ahttp://totori\.dip\.jp/?\Z#) do |display_url|
