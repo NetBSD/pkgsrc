398894aea0ba28d1e82d445dc2ffddbd5c8cf536
jaapb <jaapb@pkgsrc.org>
2016-02-28 12:10:18 +0000

    Completed upgrade of devel/ocaml-type_conv to 113.00.02 (some files apparently did not get committed, apologies for this)

--- devel/ocaml-type_conv/buildlink3.mk
+++ devel/ocaml-type_conv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2015/08/07 11:20:32 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2016/02/28 12:10:18 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-type_conv
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-type_conv
 OCAML_TYPE_CONV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-type_conv+=	ocaml-type_conv>=109.60.01
-BUILDLINK_ABI_DEPENDS.ocaml-type_conv?=	ocaml-type_conv>=111.13.00nb4
+BUILDLINK_ABI_DEPENDS.ocaml-type_conv?=	ocaml-type_conv>=113.00.02
 BUILDLINK_PKGSRCDIR.ocaml-type_conv?=	../../devel/ocaml-type_conv
 
 .include "../../lang/ocaml/buildlink3.mk"
