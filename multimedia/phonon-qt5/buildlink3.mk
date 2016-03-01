9546bbd667c40daa11d6551eb682ebe328889287
markd <markd@pkgsrc.org>
2016-03-01 08:59:54 +0000

    Add phonon-qt5 4.8.3
    QT5 version of phonon.

---
+++ multimedia/phonon-qt5/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/03/01 08:59:54 markd Exp $
+
+BUILDLINK_TREE+=	phonon-qt5
+
+.if !defined(PHONON_QT5_BUILDLINK3_MK)
+PHONON_QT5_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.phonon-qt5+=	phonon-qt5>=4.8.3
+BUILDLINK_PKGSRCDIR.phonon-qt5?=	../../multimedia/phonon-qt5
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# PHONON_QT5_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-phonon-qt5
