131ff989ce019ddce84e6e5ca4e0f669413af698
ryoon <ryoon@pkgsrc.org>
2016-03-02 23:08:22 +0000

    Update to 0.9.3
    * Disable debug build
    
    Changelog:
    The engine is written completely and get EPUB 3 support.

--- editors/Sigil/patches/patch-3rdparty_minizip_ioapi.c
+++ editors/Sigil/patches/patch-3rdparty_minizip_ioapi.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_minizip_ioapi.c,v 1.3 2015/02/09 12:57:41 wiz Exp $
+$NetBSD: patch-3rdparty_minizip_ioapi.c,v 1.1 2016/03/02 23:08:22 ryoon Exp $
 
 Fix linking on NetBSD and DragonFly BSD, where fopen, fseek, and ftello
 are 64-bit filesize safe.
 https://github.com/nmoinvaz/minizip/pull/20
 
---- src/minizip/ioapi.c.orig	2014-10-12 14:45:14.000000000 +0000
-+++ src/minizip/ioapi.c
+--- 3rdparty/minizip/ioapi.c.orig	2016-01-30 15:39:54.000000000 +0000
++++ 3rdparty/minizip/ioapi.c
 @@ -14,6 +14,12 @@
          #define _CRT_SECURE_NO_WARNINGS
  #endif
