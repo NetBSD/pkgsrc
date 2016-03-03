e2822252045a4f8707f2eb01ac894626d62ad1b8
agc <agc@pkgsrc.org>
2016-03-03 22:11:07 +0000

    Update digest to version 20160216, as proposed on tech-pkg.
    Changes since previous version:
    
    + add Taylor Campbell's implementation of SHA3 digests.  This includes
    code to calculate 224, 256, 384 and 512bit length digests.
    
    + change the license on all the code I wrote to be 2-clause BSD
    
    + modify license years for things that have been changed
    
    + add self-test command line option to digest(1) via the -t switch
    
    pkgsrc changes
    
    + derive version number for the package automatically from the source
    code

--- pkgtools/digest/files/tiger.h
+++ pkgtools/digest/files/tiger.h
@@ -1,7 +1,8 @@
-/* $NetBSD: tiger.h,v 1.6 2010/01/23 13:25:12 obache Exp $ */
+/* $NetBSD: tiger.h,v 1.7 2016/03/03 22:11:07 agc Exp $ */
 
-/*
- * Copyright © 2005 Alistair Crooks.  All rights reserved.
+/*-
+ * Copyright (c) 2005-2016 Alistair Crooks <agc@NetBSD.org>
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -11,25 +12,17 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by Alistair Crooks
- *	for the NetBSD project.
- * 4. The name of the author may not be used to endorse or promote
- *    products derived from this software without specific prior written
- *    permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #ifndef TIGER_H_
 #define TIGER_H_
@@ -48,7 +41,7 @@
 
 typedef struct tiger_context_t {
 	uint64_t	ctx[3];
-	int first_time;
+	int		first_time;
 } tiger_context_t;
 
 void TIGERInit(tiger_context_t *);
