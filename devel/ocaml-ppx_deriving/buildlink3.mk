74c5a5710b999167f13e62488203cf3ec995d1c5
jaapb <jaapb@pkgsrc.org>
2016-03-04 15:40:36 +0000

    Added buildlink3.mk file

---
+++ devel/ocaml-ppx_deriving/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/03/04 15:40:36 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-ppx_deriving
+
+.if !defined(OCAML_PPX_DERIVING_BUILDLINK3_MK)
+OCAML_PPX_DERIVING_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-ppx_deriving+=	ocaml-ppx_deriving>=3.1
+BUILDLINK_PKGSRCDIR.ocaml-ppx_deriving?=	../../devel/ocaml-ppx_deriving
+.endif	# OCAML_PPX_DERIVING_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-ppx_deriving
