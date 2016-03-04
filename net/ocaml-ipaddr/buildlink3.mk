b8dd8bb2a980345f8c82eedf30752a23758aca5e
jaapb <jaapb@pkgsrc.org>
2016-03-04 16:15:21 +0000

    Updated package to latest version, 2.7.0. Changes include: 2.7.0 (2016-02-14): * Remove sexplib.syntax, type_conv deps and camlp4 transitive dependency * Add ppx_sexp_conv dependency * Require OCaml 4.02.2+ * Add `Ipaddr.Prefix.subset`, `Ipaddr.V4.Prefix.subset` and `Ipaddr.V6.subset`   predicates to test containment of subnets (#52 from @seliopou)
    2.6.1 (2015-02-20):
    * Fix findlib requires in oasis to restore pre-4.02.1 compatibility
    
    2.6.0 (2015-02-19):
    * Change IPv6 link-local address prefix from fe80::/10 to fe80::/64. (#39)
    * Remove type bytes = string alias (potentially breaking)
    * Turn on -safe-string (#41)
    * {V4,V6}.to_bytes_raw now uses Bytes.t rather than string (potentially breaking)
    * Add multicast MAC conversions from RFC 1112 and RFC 2464
    * Add to_domain_name conversions to DNS label lists (in-addr.arpa and ip6.arpa)
    * Add V6.interface_routers, V6.site_routers, and V6.Prefix.solicited_node
    * Add V6.link_address_of_mac to convert a MAC into a link local IP address

--- net/ocaml-ipaddr/buildlink3.mk
+++ net/ocaml-ipaddr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2015/08/07 11:20:32 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2016/03/04 16:15:21 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ipaddr
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ipaddr
 OCAML_IPADDR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ipaddr+=	ocaml-ipaddr>=2.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-ipaddr?=	ocaml-ipaddr>=2.5.0nb2
+BUILDLINK_ABI_DEPENDS.ocaml-ipaddr?=	ocaml-ipaddr>=2.7.0
 BUILDLINK_PKGSRCDIR.ocaml-ipaddr?=	../../net/ocaml-ipaddr
 BUILDLINK_FILES.ocaml-ipaddr+=	lib/ocaml/site-lib/*
 
