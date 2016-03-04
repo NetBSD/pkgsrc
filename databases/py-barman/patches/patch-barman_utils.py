82f91d7038f2dbd1427f2ddcadf41298f05d4fd4
fhajny <fhajny@pkgsrc.org>
2016-03-04 16:55:45 +0000

    Update databases/py-barman to 1.6.0.
    Version 1.6.0 - 29 Feb 2016
    
    -   Support for streaming replication connection through the
        streaming_conninfo server option
    -   Support for the streaming_archiver option that allows Barman to
        receive WAL files through PostgreSQL's native streaming protocol.
        When set to 'on', it relies on pg_receivexlog to receive WAL data,
        reducing Recovery Point Objective. Currently, WAL streaming is an
        additional feature (standard log archiving is still required)
    -   Implement the receive-wal command that, when streaming_archiver is
        on, wraps pg_receivexlog for WAL streaming. Add --stop option to
        stop receiving WAL files via streaming protocol. Add --reset option
        to reset the streaming status and restart from the current xlog
        in Postgres.
    -   Automatic management (startup and stop) of receive-wal command via
        cron command
    -   Support for the path_prefix configuration option
    -   Introduction of the archiver option (currently fixed to on) which
        enables continuous WAL archiving for a specific server, through log
        shipping via PostgreSQL's archive_command
    -   Support for streaming_wals_directory and errors_directory options
    -   Management of WAL duplicates in archive-wal command and integration
        with check command
    -   Verify if pg_receivexlog is running in check command when
        streaming_archiver is enabled
    -   Verify if failed backups are present in check command
    -   Accept compressed WAL files in incoming directory
    -   Add support for the pigz compressor (thanks to Stefano Zacchiroli
        zack@upsilon.cc)
    -   Implement pygzip and pybzip2 compressors (based on an initial idea
        of Christoph Moench-Tegeder christoph@2ndquadrant.de)
    -   Creation of an implicit restore point at the end of a backup
    -   Current size of the PostgreSQL data files in barman status
    -   Permit archive_mode=always for PostgreSQL 9.5 servers (thanks to
        Christoph Moench-Tegeder christoph@2ndquadrant.de)
    -   Complete refactoring of the code responsible for connecting to
        PostgreSQL
    -   Improve messaging of cron command regarding sub-processes
    -   Native support for Python >= 3.3
    -   Changes of behaviour:
        -   Stop trashing WAL files during archive-wal (commit:e3a1d16)
    -   Bug fixes:
        -   Atomic WAL file archiving (#9 and #12)
        -   Propagate "-c" option to any Barman subprocess (#19)
        -   Fix management of backup ID during backup deletion (#22)
        -   Improve archive-wal robustness and log messages (#24)
        -   Improve error handling in case of missing parameters
    
    Version 1.5.1 - 16 Nov 2015
    
    -   Add support for the 'archive-wal' command which performs WAL
        maintenance operations on a given server
    -   Add support for "per-server" concurrency of the 'cron' command
    -   Improved management of xlog.db errors
    -   Add support for mixed compression types in WAL files (SF.net#61)
    -   Bug fixes:
        -   Avoid retention policy checks during the recovery
        -   Avoid 'wal_level' check on PostgreSQL version < 9.0 (#3)
        -   Fix backup size calculation (#5)

--- databases/py-barman/patches/patch-barman_utils.py
+++ databases/py-barman/patches/patch-barman_utils.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-barman_utils.py,v 1.1 2015/06/09 15:06:39 fhajny Exp $
+$NetBSD: patch-barman_utils.py,v 1.2 2016/03/04 16:55:45 fhajny Exp $
 
 Make portable for non-glibc platforms too.
---- barman/utils.py.orig	2015-04-17 13:05:38.000000000 +0000
+--- barman/utils.py.orig	2016-02-22 08:01:31.000000000 +0000
 +++ barman/utils.py
-@@ -239,7 +239,7 @@ def fsync_dir(dir_path):
+@@ -253,7 +253,7 @@ def fsync_dir(dir_path):
      :param str dir_path: The directory to sync
      :raise OSError: If fail opening the directory
      """
@@ -11,4 +11,4 @@ Make portable for non-glibc platforms too.
 +    dir_fd = os.open(dir_path, os.O_RDONLY)
      try:
          os.fsync(dir_fd)
-     except OSError, e:
+     except OSError as e:
