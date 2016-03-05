20492b687148ab12877f96b0a3e3c97274436c7a
ryoon <ryoon@pkgsrc.org>
2016-03-05 08:52:23 +0000

    Import ffmpeg3-3.0 as multimedia/ffmpeg3.
    FFmpeg is a complete, cross-platform solution to record, convert
    and stream audio and video.  It includes libavcodec - the leading
    audio/video codec library.
    
    This package contains major version 3.

---
+++ multimedia/ffmpeg3/patches/patch-libavutil_x86_cpu.c
@@ -0,0 +1,20 @@
+$NetBSD: patch-libavutil_x86_cpu.c,v 1.1 2016/03/05 08:52:23 ryoon Exp $
+
+--- libavutil/x86/cpu.c.orig	2016-01-15 16:58:37.000000000 +0000
++++ libavutil/x86/cpu.c
+@@ -116,6 +116,7 @@ int ff_get_cpu_flags_x86(void)
+ #if HAVE_SSE
+         if (std_caps & (1 << 25))
+             rval |= AV_CPU_FLAG_SSE;
++#if !defined(__GNUC__) || (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 2))
+         if (std_caps & (1 << 26))
+             rval |= AV_CPU_FLAG_SSE2;
+         if (ecx & 1)
+@@ -138,6 +139,7 @@ int ff_get_cpu_flags_x86(void)
+             }
+         }
+ #endif /* HAVE_AVX */
++#endif /* gcc >= 4.2 */
+ #endif /* HAVE_SSE */
+     }
+     if (max_std_level >= 7) {
