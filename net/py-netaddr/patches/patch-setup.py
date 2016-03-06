2a99f9ef628064bf71ef830f77f3371194e20e0d
adam <adam>
2016-03-06 15:07:07 +0000

    Changes 0.7.18: * cidr_merge() algorithm is now O(n) and much faster. * nmap target specification now fully supported including IPv4 CIDR   prefixes and IPv6 addresses.

--- net/py-netaddr/patches/patch-setup.py
+++ net/py-netaddr/patches/patch-setup.py
@@ -1,19 +1,27 @@
-$NetBSD: patch-setup.py,v 1.1 2014/09/02 13:48:13 he Exp $
+$NetBSD: patch-setup.py,v 1.2 2016/03/06 15:07:07 adam Exp $
 
 Prefer setup from setuptools, the older one may not
 support install_requires or setup_requires
 
---- setup.py.orig	2014-07-06 05:19:54.000000000 +0000
+--- setup.py.orig	2015-08-25 21:54:04.000000000 +0000
 +++ setup.py
 @@ -8,7 +8,10 @@ A distutils Python setup file. For setup
  import os
  import sys
  
--from distutils.core import setup
+-from distutils.core import setup, Command
 +try:
-+    from setuptools import setup
++    from setuptools import setup, Command
 +except ImportError:
-+    from distutils.core import setup
++    from distutils.core import setup, Command
  
  if os.path.exists('MANIFEST'):
      os.remove('MANIFEST')
+@@ -64,7 +67,6 @@ def main():
+         version          = release.version,
+         options = {
+             'build_scripts': {
+-                'executable': '/usr/bin/env python',
+             },
+         },
+     )
