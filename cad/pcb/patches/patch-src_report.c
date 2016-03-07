37ebd80c55676633de110fb531f7c4cddf2bf482
dsainty <dsainty@pkgsrc.org>
2016-03-07 23:40:47 +0000

    Correct RCSID to NetBSD, and update distinfo to match the file.

--- cad/pcb/patches/patch-src_report.c
+++ cad/pcb/patches/patch-src_report.c
@@ -1,4 +1,4 @@
-$Id: patch-src_report.c,v 1.1 2016/01/22 21:56:37 dmcmahill Exp $
+$NetBSD: patch-src_report.c,v 1.2 2016/03/07 23:40:47 dsainty Exp $
 
 --- src/report.c.orig	2014-03-10 23:09:51.000000000 -0400
 +++ src/report.c	2016-01-22 10:05:47.000000000 -0500
