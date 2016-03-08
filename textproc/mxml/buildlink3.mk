fe5e1774a3d5a5bc1970cc62cb601592403700c6
nros <nros@pkgsrc.org>
2016-03-08 13:50:46 +0000

    Update mxml to version 2.9.
    Changelog:
    
    CHANGES IN Mini-XML 2.9
    
    	- mxmlLoad* did not correctly load value nodes with MXML_NO_CALLBACK
    	  or MXML_TEXT_CALLBACK (Bug #502)
    
    
    CHANGES IN Mini-XML 2.8
    
    	- Now call docsetutil using xcrun on OS X (Bug #458)
    	- mxmldoc did not escape special HTML characters inside @code foo@
    	  comments.
    	- Fixed a memory leak in mxmlElementDeleteAttr (Bug #452)
    	- Added MXML_MAJOR/MINOR_VERSION definitions to mxml.h (Bug $461)
    	- Fixed a bug reading UTF-16 characters from a file (Bug #454)
    	- Fixed a memory leak when loading invalid XML (Bug #496)
    	- Fixed an XML fragment loading problem (Bug #494)
    
    
    CHANGES IN Mini-XML 2.7
    
    	- Added 64-bit configurations to the VC++ project files (STR #129)
    	- Fixed conformance of mxmldoc's HTML and CSS output.
    	- Added data accessor ("get") functions and made the mxml_node_t and
    	  mxml_index_t structures private but still available in the Mini-XML
    	  header to preserve source compatibility (STR #118)
    	- Updated the source headers to reference the Mini-XML license and its
    	  exceptions to the LGPL2 (STR #108)
    	- Fixed a memory leak when loading a badly-formed XML file (STR #121)
          	- Added a new mxmlFindPath() function to find the value node of a
    	  named element (STR #110)
    	- Building a static version of the library did not work on Windows
    	  (STR #112)
    	- The shared library did not include a destructor for the thread-
    	  specific data key on UNIX-based operating systems (STR #103)
    	- mxmlLoad* did not error out on XML with multiple root nodes (STR #101)
    	- Fixed an issue with the _mxml_vstrdupf function (STR #107)
    	- mxmlSave* no longer write all siblings of the passed node, just that
    	  node and its children (STR #109)

--- textproc/mxml/buildlink3.mk
+++ textproc/mxml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:31 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/03/08 13:50:46 nros Exp $
 
 BUILDLINK_TREE+=	mxml
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	mxml
 MXML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mxml+=	mxml>=2.3
+BUILDLINK_ABI_DEPENDS.mxml+=	mxml>=2.9
 BUILDLINK_PKGSRCDIR.mxml?=	../../textproc/mxml
 .endif # MXML_BUILDLINK3_MK
 
