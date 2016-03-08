fe98e05d30486f66c3b1faa486f3d513c5898691
bsiegert <bsiegert@pkgsrc.org>
2016-03-08 14:24:52 +0000

    Pullup ticket #4944 - requested by taca lang/php55: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.126
    - lang/php55/distinfo                                           1.50
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Mar  5 05:17:33 UTC 2016
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php55: distinfo
    
       Log Message:
       Update php55 to 5.5.33, security fixes.
    
       03 Mar 2016, PHP 5.5.33
    
       - Phar:
         . Fixed bug #71498 (Out-of-Bound Read in phar_parse_zipfile()). (Stas)
    
       - WDDX:
         . Fixed bug #71587 (Use-After-Free / Double-Free in WDDX Deserialize). (Stas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.119.2.6 2016/02/10 20:45:48 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.119.2.7 2016/03/08 14:24:52 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -81,7 +81,7 @@
 PHPVERSION_MK=	defined
 
 # Define each PHP's version.
-PHP55_VERSION=	5.5.32
+PHP55_VERSION=	5.5.33
 PHP56_VERSION=	5.6.18
 PHP70_VERSION=	7.0.3
 
