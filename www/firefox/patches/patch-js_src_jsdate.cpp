cb1e5375b69f3b65cdaf08e58aa87968d88293e7
ryoon <ryoon@pkgsrc.org>
2016-03-08 21:32:52 +0000

    Update to 45.0
    Changelog:
    New
        Instant browser tab sharing through Hello
    
        Tabs synced via Firefox Accounts from other devices are now shown in dropdown area of Awesome Bar when searching
    
        Synced Tabs button in button bar
    
        Introduce a new preference (network.dns.blockDotOnion) to allow blocking .onion at the DNS level
    
        Guarani [gn] locale added
    
    
    Fixed
        URLs containing a Unicode-format Internationalized Domain Name (IDN) are now properly redirected
    
        Various security fixes
    
    
    Fixed in Firefox 45
        2016-37 Font vulnerabilities in the Graphite 2 library
        2016-36 Use-after-free during processing of DER encoded keys in NSS
        2016-35 Buffer overflow during ASN.1 decoding in NSS
        2016-34 Out-of-bounds read in HTML parser following a failed allocation
        2016-33 Use-after-free in GetStaticInstance in WebRTC
        2016-32 WebRTC and LibVPX vulnerabilities found through code inspection
        2016-31 Memory corruption with malicious NPAPI plugin
        2016-30 Buffer overflow in Brotli decompression
        2016-29 Same-origin policy violation using perfomance.getEntries and history navigation with session restore
        2016-28 Addressbar spoofing though history navigation and Location protocol property
        2016-27 Use-after-free during XML transformations
        2016-26 Memory corruption when modifying a file being read by FileReader
        2016-25 Use-after-free when using multiple WebRTC data channels
        2016-24 Use-after-free in SetBody
        2016-23 Use-after-free in HTML5 string parser
        2016-22 Service Worker Manager out-of-bounds read in Service Worker Manager
        2016-21 Displayed page address can be overridden
        2016-20 Memory leak in libstagefright when deleting an array during MP4 processing
        2016-19 Linux video memory DOS with Intel drivers
        2016-18 CSP reports fail to strip location information for embedded iframe pages
        2016-17 Local file overwriting and potential privilege escalation through CSP reports
        2016-16 Miscellaneous memory safety hazards (rv:45.0 / rv:38.7)

--- www/firefox/patches/patch-js_src_jsdate.cpp
+++ www/firefox/patches/patch-js_src_jsdate.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-js_src_jsdate.cpp,v 1.2 2015/02/28 04:30:55 ryoon Exp $
+$NetBSD: patch-js_src_jsdate.cpp,v 1.3 2016/03/08 21:32:52 ryoon Exp $
 
---- js/src/jsdate.cpp.orig	2015-02-17 21:40:42.000000000 +0000
+--- js/src/jsdate.cpp.orig	2016-02-25 23:02:04.000000000 +0000
 +++ js/src/jsdate.cpp
-@@ -2651,11 +2651,11 @@ ToLocaleFormatHelper(JSContext *cx, Hand
+@@ -2672,8 +2672,8 @@ ToLocaleFormatHelper(JSContext* cx, Hand
          if (strcmp(format, "%x") == 0 && result_len >= 6 &&
              /* Format %x means use OS settings, which may have 2-digit yr, so
                 hack end of 3/11/22 or 11.03.22 or 11Mar22 to use 4-digit yr...*/
@@ -11,10 +11,5 @@ $NetBSD: patch-js_src_jsdate.cpp,v 1.2 2015/02/28 04:30:55 ryoon Exp $
 +            !isdigit(((unsigned char)buf[result_len - 3])) &&
 +            isdigit(((unsigned char)buf[result_len - 2])) && isdigit(((unsigned char)buf[result_len - 1])) &&
              /* ...but not if starts with 4-digit year, like 2022/3/11. */
--            !(isdigit(buf[0]) && isdigit(buf[1]) &&
--              isdigit(buf[2]) && isdigit(buf[3]))) {
-+            !(isdigit(((unsigned char)buf[0])) && isdigit(((unsigned char)buf[1])) &&
-+              isdigit(((unsigned char)buf[2])) && isdigit(((unsigned char)buf[3])))) {
-             double localtime = obj->as<DateObject>().cachedLocalTime(&cx->runtime()->dateTimeInfo);
-             int year = IsNaN(localtime) ? 0 : (int) YearFromTime(localtime);
-             JS_snprintf(buf + (result_len - 2), (sizeof buf) - (result_len - 2),
+             !(isdigit(buf[0]) && isdigit(buf[1]) &&
+               isdigit(buf[2]) && isdigit(buf[3]))) {
