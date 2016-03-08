2af44f2533026d912f7b1b629b93d789fd68d9b5
ryoon <ryoon@pkgsrc.org>
2016-03-08 21:32:52 +0000

    Update to 45.0
    Changelog:
    New
        Instant browser tab sharing through Hello
    
        Tabs synced via Firefox Accounts from other devices are now shown in dropdown area of Awesome Bar when searching
    
        Synced Tabs button in button bar
    
        Introduce a new preference (network.dns.blockDotOnion) to allow blocking .onion at the DNS level
    
        Guarani [gn] locale added
    
    
    Fixed
        URLs containing a Unicode-format Internationalized Domain Name (IDN) are now properly redirected
    
        Various security fixes
    
    
    Fixed in Firefox 45
        2016-37 Font vulnerabilities in the Graphite 2 library
        2016-36 Use-after-free during processing of DER encoded keys in NSS
        2016-35 Buffer overflow during ASN.1 decoding in NSS
        2016-34 Out-of-bounds read in HTML parser following a failed allocation
        2016-33 Use-after-free in GetStaticInstance in WebRTC
        2016-32 WebRTC and LibVPX vulnerabilities found through code inspection
        2016-31 Memory corruption with malicious NPAPI plugin
        2016-30 Buffer overflow in Brotli decompression
        2016-29 Same-origin policy violation using perfomance.getEntries and history navigation with session restore
        2016-28 Addressbar spoofing though history navigation and Location protocol property
        2016-27 Use-after-free during XML transformations
        2016-26 Memory corruption when modifying a file being read by FileReader
        2016-25 Use-after-free when using multiple WebRTC data channels
        2016-24 Use-after-free in SetBody
        2016-23 Use-after-free in HTML5 string parser
        2016-22 Service Worker Manager out-of-bounds read in Service Worker Manager
        2016-21 Displayed page address can be overridden
        2016-20 Memory leak in libstagefright when deleting an array during MP4 processing
        2016-19 Linux video memory DOS with Intel drivers
        2016-18 CSP reports fail to strip location information for embedded iframe pages
        2016-17 Local file overwriting and potential privilege escalation through CSP reports
        2016-16 Miscellaneous memory safety hazards (rv:45.0 / rv:38.7)

--- www/firefox/patches/patch-ipc_chromium_src_base_process__util__bsd.cc
+++ www/firefox/patches/patch-ipc_chromium_src_base_process__util__bsd.cc
@@ -1,17 +1,18 @@
-$NetBSD: patch-ipc_chromium_src_base_process__util__bsd.cc,v 1.2 2014/10/15 13:07:07 ryoon Exp $
+$NetBSD: patch-ipc_chromium_src_base_process__util__bsd.cc,v 1.3 2016/03/08 21:32:52 ryoon Exp $
 
 Darwin does not provide kvm_getprocs(3). The code for OS_MACOSX is
 simply copied from process_util_mac.mm
 
---- ipc/chromium/src/base/process_util_bsd.cc.orig	2014-10-11 09:06:28.000000000 +0000
+--- ipc/chromium/src/base/process_util_bsd.cc.orig	2016-02-25 23:01:54.000000000 +0000
 +++ ipc/chromium/src/base/process_util_bsd.cc
-@@ -9,6 +9,9 @@
+@@ -10,6 +10,10 @@
+ #include <spawn.h>
  #include <sys/wait.h>
  
- #include <fcntl.h>
 +#if defined(OS_BSD)
 +#include <kvm.h>
 +#endif
- #include <unistd.h>
- 
++
  #include <string>
+ 
+ #include "base/eintr_wrapper.h"
