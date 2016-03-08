fd6472e43fc27adcc852c0f84a97777f3f4e81ff
nros <nros@pkgsrc.org>
2016-03-08 11:21:27 +0000

    Update libmusicbrainz5 to version 5.1.0.
    Changelog:
    
    Changes for libmusicbrainz 5.1.0
    --------------------------------
    
       - Fix LMB-33 - Handle 'ended' element in 'relation'
       - Fix LMB-34 - Remove non-free XML parser and replace with libxml2
       - Add support for cross-compilation and building out of tree

--- audio/libmusicbrainz5/buildlink3.mk
+++ audio/libmusicbrainz5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2016/03/05 11:27:40 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2016/03/08 11:21:27 nros Exp $
 
 BUILDLINK_TREE+=	libmusicbrainz5
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libmusicbrainz5
 LIBMUSICBRAINZ5_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libmusicbrainz5+=	libmusicbrainz5>=5.0.1
-BUILDLINK_ABI_DEPENDS.libmusicbrainz5?=	libmusicbrainz5>=5.0.1nb3
+BUILDLINK_ABI_DEPENDS.libmusicbrainz5?=	libmusicbrainz5>=5.1.0
 BUILDLINK_PKGSRCDIR.libmusicbrainz5?=	../../audio/libmusicbrainz5
 
 .include "../../www/neon/buildlink3.mk"
