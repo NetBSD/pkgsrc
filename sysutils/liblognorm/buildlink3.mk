8e402bd6f1324bcdfd2b5311f54f3cfa9a334837
fhajny <fhajny@pkgsrc.org>
2016-03-09 20:45:44 +0000

    Update sysutils/liblognorm to 1.1.3.
    - add support for libfastjson
    - make work on Solaris
    - check for runaway rules.
    - fix: process last line if it misses the terminating LF

--- sysutils/liblognorm/buildlink3.mk
+++ sysutils/liblognorm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/02/16 15:17:51 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/03/09 20:45:44 fhajny Exp $
 
 BUILDLINK_TREE+=	liblognorm
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.liblognorm+=	liblognorm>=1.0.0
 BUILDLINK_PKGSRCDIR.liblognorm?=	../../sysutils/liblognorm
 
 .include "../../devel/libestr/buildlink3.mk"
-.include "../../textproc/json-c/buildlink3.mk"
+.include "../../textproc/libfastjson/buildlink3.mk"
 .endif # LIBLOGNORM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-liblognorm
