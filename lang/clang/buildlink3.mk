80a9629db9070d8296a7ed683318f857b6af4fbf
tnn <tnn@pkgsrc.org>
2016-03-10 15:01:52 +0000

    Update to clang-3.8.0. Split out llvm tools to lang/llvm. Packaged in pkgsrc-wip by myself and Kamil. Remove stale pkgsrc/SunOS bread crumbs that were difficult to merge following the build framework switch. Needs to be redone. (Sorry!)
    Summary of significant changes in LLVM:
    - switch build framework from autoconf to CMake
    - llvm-ar now supports thin archives.
    - llvm doesn t produce .data.rel.ro.local or .data.rel sections anymore.
    - opional support for linking clang and the LLVM tools with a single
      libLLVM shared library
    
      pkgsrc note: we have this library in lang/libLLVM, but we don't currently
      use it. We may do so in the future if/when the API matures.
    
    - The optimization to move the prologue and epilogue of functions in colder
      code path (shrink-wrapping) is now enabled by default
    - new target-independent gcc-compatible emulated Thread Local Storage mode
    - various target specific optimizations
    
    Summary of significant changes in Clang:
    
    - new compiler flags for tuning what DWARF information is included
    - better strict alignment handling
    - better support for __builtin_object_size

--- lang/clang/buildlink3.mk
+++ lang/clang/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.3 2015/12/30 15:54:56 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/03/10 15:01:52 tnn Exp $
 
 BUILDLINK_TREE+=	clang
 
 .if !defined(CLANG_BUILDLINK3_MK)
 CLANG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clang+=	clang>=3.7.0
+BUILDLINK_API_DEPENDS.clang+=	clang>=3.8.0
 BUILDLINK_PKGSRCDIR.clang?=	../../lang/clang
 
-#.include "../../textproc/libxml2/buildlink3.mk"
-#.include "../../devel/zlib/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../lang/llvm/buildlink3.mk"
 .endif	# CLANG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-clang
