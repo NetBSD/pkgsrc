42c99a177964191e05d9f84ecef02119dba9123f
tnn <tnn@pkgsrc.org>
2016-03-11 06:50:42 +0000

    resurrect and copy the previous clang patch-ac to here
    jperkin Mon Mar  7 16:27:47 UTC 2016
    Do not use "-z discard-unused" linker flags on SunOS, it is only supported
    by the very latest Oracle Solaris release.

--- lang/llvm/patches/patch-cmake_modules_AddLLVM.cmake
+++ lang/llvm/patches/patch-cmake_modules_AddLLVM.cmake
@@ -1,7 +1,5 @@
-$NetBSD: patch-cmake_modules_AddLLVM.cmake,v 1.2 2016/03/11 06:50:42 tnn Exp $
+$NetBSD: patch-cmake_modules_AddLLVM.cmake,v 1.1 2016/03/11 06:50:42 tnn Exp $
 
-Disable library install rules. Handled manually.
-Make sure llvm-config goes in libexec/libLLVM to avoid conflict.
 Don't use non-portable -z discard-unused on SunOS.
 
 --- cmake/modules/AddLLVM.cmake.orig	2016-02-12 17:52:29.000000000 +0000
@@ -19,25 +17,3 @@ Don't use non-portable -z discard-unused on SunOS.
        elseif(NOT WIN32 AND NOT LLVM_LINKER_IS_GOLD)
          # Object files are compiled with -ffunction-data-sections.
          # Versions of bfd ld < 2.23.1 have a bug in --gc-sections that breaks
-@@ -558,10 +558,12 @@ macro(add_llvm_library name)
-         set(install_type ARCHIVE)
-       endif()
- 
-+      if (false)
-       install(TARGETS ${name}
-             EXPORT LLVMExports
-             ${install_type} DESTINATION ${install_dir}
-             COMPONENT ${name})
-+      endif()
- 
-       if (NOT CMAKE_CONFIGURATION_TYPES)
-         add_custom_target(install-${name}
-@@ -702,7 +704,7 @@ macro(add_llvm_tool name)
-     if( LLVM_BUILD_TOOLS )
-       install(TARGETS ${name}
-               EXPORT LLVMExports
--              RUNTIME DESTINATION bin
-+              RUNTIME DESTINATION libexec/libLLVM
-               COMPONENT ${name})
- 
-       if (NOT CMAKE_CONFIGURATION_TYPES)
