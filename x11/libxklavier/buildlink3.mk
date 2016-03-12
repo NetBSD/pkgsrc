9093142bd6cbf1d0be75f6d8dc5c8bb10cfbd856
ryoon <ryoon@pkgsrc.org>
2016-03-12 11:12:40 +0000

    Update to 5.4
    * Buildlink gobject-introspection
    
    Changelog:
    5.4     A couple of introspection fixes
    5.3     A number of introspection and build fixes
    5.2.1   Fixed GIR params for introspection (broken build)
    5.2     Optional introspection introduced
            Some minor fixes (runtime and build-time)
    5.1     Expanding APIs for GNOME 3 needs (bumped soname):
            - converting ISO codes to translatable names (it was private, now public)
            - search by pattern
            Using to X11 API throgh pkg-config
            One race condition eliminated
            XInput2 version checking: looking for support of DevicePresence

--- x11/libxklavier/buildlink3.mk
+++ x11/libxklavier/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2012/06/14 07:43:23 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2016/03/12 11:12:40 ryoon Exp $
 
 BUILDLINK_TREE+=	libxklavier
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libxklavier
 LIBXKLAVIER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libxklavier+=	libxklavier>=3.0
-BUILDLINK_ABI_DEPENDS.libxklavier+=	libxklavier>=5.0nb4
+BUILDLINK_ABI_DEPENDS.libxklavier+=	libxklavier>=5.4
 BUILDLINK_PKGSRCDIR.libxklavier?=	../../x11/libxklavier
 
 .include "../../textproc/libxml2/buildlink3.mk"
