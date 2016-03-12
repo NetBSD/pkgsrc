abf6f5dd2ad4415799722680aa2054c0d4205461
khorben <khorben@pkgsrc.org>
2016-03-12 02:22:08 +0000

    Package DeforaOS Keyboard 0.3.0
    This release brings:
    - support for the newest API from libDesktop
    - generic keyboard widget for libDesktop
    - improved support for Gtk+ 3
    - early translation to Spanish
    - no more conflicts with x11/gnome-control-center

--- x11/deforaos-keyboard/buildlink3.mk
+++ x11/deforaos-keyboard/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2015/04/25 14:25:03 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2016/03/12 02:22:08 khorben Exp $
 #
 
 BUILDLINK_TREE+=	deforaos-keyboard
@@ -6,11 +6,11 @@ BUILDLINK_TREE+=	deforaos-keyboard
 .if !defined(DEFORAOS_KEYBOARD_BUILDLINK3_MK)
 DEFORAOS_KEYBOARD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-keyboard+=	deforaos-keyboard>=0.0.2
-BUILDLINK_ABI_DEPENDS.deforaos-keyboard?=	deforaos-keyboard>=0.2.1nb10
+BUILDLINK_API_DEPENDS.deforaos-keyboard+=	deforaos-keyboard>=0.3.0
+BUILDLINK_ABI_DEPENDS.deforaos-keyboard?=	deforaos-keyboard>=0.3.0
 BUILDLINK_PKGSRCDIR.deforaos-keyboard?=	../../x11/deforaos-keyboard
 
-.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/deforaos-libdesktop/buildlink3.mk"
 .endif	# DEFORAOS_KEYBOARD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-deforaos-keyboard
