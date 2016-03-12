c8f8d9760304b2c85a7d7225693ed5b6226f5e49
fhajny <fhajny>
2016-03-12 08:33:12 +0000

    Update devel/php-raphf to 1.1.2.
    * Source code documentation available at http://m6w6.github.io/ext-raphf/v1.1.x
    * Travis support
    * Fix package.xml's maximum and minimum PHP version

--- devel/php-raphf/buildlink3.mk
+++ devel/php-raphf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/04/07 23:54:11 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/03/12 08:33:12 fhajny Exp $
 
 .include "../../lang/php/phpversion.mk"
 
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	${PHP_PKG_PREFIX}-raphf
 .if !defined(${PHP_PKG_PREFIX}_RAPHF_BUILDLINK3_MK)
 ${PHP_PKG_PREFIX}_RAPHF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.${PHP_PKG_PREFIX}-raphf+=	${PHP_PKG_PREFIX}-raphf>=1.0.4nb1
+BUILDLINK_API_DEPENDS.${PHP_PKG_PREFIX}-raphf+=	${PHP_PKG_PREFIX}-raphf>=1.0.4nb1<2
 BUILDLINK_PKGSRCDIR.${PHP_PKG_PREFIX}-raphf?=	../../devel/php-raphf
 .endif	# ${PHP_PKG_PREFIX}_RAPHF_BUILDLINK3_MK
 
