b539897b622ed73c7b2f3560ee37299b64ad3c3e
fhajny <fhajny>
2016-03-12 08:34:33 +0000

    Update www/php-propro to 1.0.2.
    * Internals documentation at http://m6w6.github.io/ext-propro/v1.0.x/
    * Travis support
    * Fix package.xml's maximum and minimum PHP version

--- www/php-propro/buildlink3.mk
+++ www/php-propro/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/04/07 23:59:40 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/03/12 08:34:33 fhajny Exp $
 
 .include "../../lang/php/phpversion.mk"
 
@@ -7,8 +7,8 @@ BUILDLINK_TREE+=	${PHP_PKG_PREFIX}-propro
 .if !defined(${PHP_PKG_PREFIX}_PROPRO_BUILDLINK3_MK)
 ${PHP_PKG_PREFIX}_PROPRO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.${PHP_PKG_PREFIX}-propro+=	${PHP_PKG_PREFIX}-propro>=1.0.0
-BUILDLINK_PKGSRCDIR.${PHP_PKG_PREFIX}-propro?=	../../www/php-propro
+BUILDLINK_API_DEPENDS.${PHP_PKG_PREFIX}-propro+=	${PHP_PKG_PREFIX}-propro>=1.0.0<2
+BUILDLINK_PKGSRCDIR.${PHP_PKG_PREFIX}-propro?=		../../www/php-propro
 .endif	# ${PHP_PKG_PREFIX}_PROPRO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-${PHP_PKG_PREFIX}-propro
