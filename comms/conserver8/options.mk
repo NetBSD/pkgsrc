3d4a1472d37593b2ff2ef61d977ed0ab37d47c68
schnoebe <schnoebe@pkgsrc.org>
2016-03-13 18:31:11 +0000

    Upstream: Update to 8.2.1     version 8.2.1 (Jun 2, 2015): 	- added TCP keepalives between client and server - TCP-based consoles 	  already had the code - this was mostly an oversight 	- patch for SEGV and task execution - patch by Artem Savkov 	  <asavkov@redhat.com> 	- expanded break sequences from [1-9] to [1-9a-z] - based on patch by 	  Artem Savkov <asavkov@redhat.com>
    pkgsrc:
        options.mk:
    	add support inet6
    	The way the conserver is coded, you get inet6 or you get uds
        patches/patch-conserver_readcfg.c:
    	new patch, fixing a setproctitle() bug with inet6.
    	This needs to be sent upstream.
        Makefile:
    	install sample configurations
    	Change maintainer to pkgsrc-users@netbsd.org
    	    old maintainer address bounced.
    	drop ``--with-regex'' option, it's no longer supported.

--- comms/conserver8/options.mk
+++ comms/conserver8/options.mk
@@ -1,8 +1,13 @@
-# $NetBSD: options.mk,v 1.1 2009/06/14 07:52:51 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2016/03/13 18:31:11 schnoebe Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.conserver8
-PKG_SUPPORTED_OPTIONS=	pam ssl uds
-PKG_SUGGESTED_OPTIONS=	ssl
+
+PKG_OPTIONS_REQUIRED_GROUPS= connectivity
+PKG_OPTIONS_GROUP.connectivity=inet6 uds
+
+PKG_SUPPORTED_OPTIONS=	pam ssl
+
+PKG_SUGGESTED_OPTIONS=	ssl inet6
 
 .include "../../mk/bsd.options.mk"
 
@@ -13,6 +18,10 @@ CONFIGURE_ARGS+=	--with-pam
 
 .if !empty(PKG_OPTIONS:Muds)
 CONFIGURE_ARGS+=	--with-uds
+.endif
+
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--with-ipv6
 .endif
 
 .if !empty(PKG_OPTIONS:Mssl)
