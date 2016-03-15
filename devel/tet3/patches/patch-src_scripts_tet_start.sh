d146cee3d2bc2e2c565afd4bacbdb7cca68ceece
wiz <wiz@pkgsrc.org>
2016-03-15 12:15:26 +0000

    Remove two weird patches. Add comments to most others.

--- devel/tet3/patches/patch-src_scripts_tet_start.sh
+++ devel/tet3/patches/patch-src_scripts_tet_start.sh
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_scripts_tet_start.sh,v 1.1 2011/12/11 11:57:09 marino Exp $
+$NetBSD: patch-src_scripts_tet_start.sh,v 1.2 2016/03/15 12:15:26 wiz Exp $
+
+Fix shebang.
 
 --- src/scripts/tet_start.sh.orig	2011-12-11 11:02:59.580983000 +0000
 +++ src/scripts/tet_start.sh
