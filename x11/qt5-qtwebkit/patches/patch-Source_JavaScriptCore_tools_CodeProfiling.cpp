c6ccd1f88c62ccd2cafb019e3facc9ab5efb5535
tnn <tnn@pkgsrc.org>
2016-03-15 00:19:00 +0000

    Teach about difference of struct mcontext_t on Linux/i386 vs NetBSD/i386. Fixes build on NetBSD/i386.

--- x11/qt5-qtwebkit/patches/patch-Source_JavaScriptCore_tools_CodeProfiling.cpp
+++ x11/qt5-qtwebkit/patches/patch-Source_JavaScriptCore_tools_CodeProfiling.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-Source_JavaScriptCore_tools_CodeProfiling.cpp,v 1.1 2014/12/30 17:23:47 adam Exp $
+$NetBSD: patch-Source_JavaScriptCore_tools_CodeProfiling.cpp,v 1.2 2016/03/15 00:19:00 tnn Exp $
 
 * Add NetBSD support
 
---- Source/JavaScriptCore/tools/CodeProfiling.cpp.orig	2013-11-27 01:01:49.000000000 +0000
+--- Source/JavaScriptCore/tools/CodeProfiling.cpp.orig	2015-10-13 04:37:12.000000000 +0000
 +++ Source/JavaScriptCore/tools/CodeProfiling.cpp
 @@ -33,7 +33,7 @@
  #include <signal.h>
@@ -22,16 +22,21 @@ $NetBSD: patch-Source_JavaScriptCore_tools_CodeProfiling.cpp,v 1.1 2014/12/30 17
  // Helper function to start & stop the timer.
  // Presently we're using the wall-clock timer, since this seems to give the best results.
  static void setProfileTimer(unsigned usec)
-@@ -73,7 +73,7 @@ static void profilingTimer(int, siginfo_
-     CodeProfiling::sample(reinterpret_cast<void*>(context->__ss.__rip),
-                           reinterpret_cast<void**>(context->__ss.__rbp));
+@@ -80,6 +80,13 @@ static void profilingTimer(int, siginfo_
+     CodeProfiling::sample(reinterpret_cast<void*>(context.gregs[REG_EIP]),
+                           reinterpret_cast<void**>(context.gregs[REG_EBP]));
  }
--#elif OS(LINUX) && CPU(X86) && !OS(ANDROID)
-+#elif (OS(LINUX) || OS(NETBSD)) && CPU(X86) && !OS(ANDROID)
- static void profilingTimer(int, siginfo_t*, void* uap)
- {
-     mcontext_t context = static_cast<ucontext_t*>(uap)->uc_mcontext;
-@@ -143,7 +143,7 @@ void CodeProfiling::begin(const SourceCo
++#elif OS(NETBSD)) && CPU(X86)
++static void profilingTimer(int, siginfo_t*, void* uap)
++{
++    mcontext_t context = static_cast<ucontext_t*>(uap)->uc_mcontext;
++    CodeProfiling::sample(reinterpret_cast<void*>(context.__gregs[_REG_EIP]),
++                          reinterpret_cast<void**>(context.__gregs[_REG_EBP]));
++}
+ #endif
+ 
+ // Callback triggered when the timer is fired.
+@@ -143,7 +150,7 @@ void CodeProfiling::begin(const SourceCo
      if (alreadyProfiling)
          return;
  
