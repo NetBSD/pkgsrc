4fec078402c83e7b648383804c123ba1d3515edb
bsiegert <bsiegert@pkgsrc.org>
2016-03-15 20:54:07 +0000

    Update openssh to 7.2.2 (7.2p2).
    Changes since OpenSSH 7.2p1
    ===========================
    
    This release fixes a security bug:
    
     * sshd(8): sanitise X11 authentication credentials to avoid xauth
       command injection when X11Forwarding is enabled.
    
       Full details of the vulnerability are available at:
       http://www.openssh.com/txt/x11fwd.adv

--- security/openssh/patches/patch-ssh.c
+++ security/openssh/patches/patch-ssh.c
@@ -1,26 +1,15 @@
-$NetBSD: patch-ssh.c,v 1.5 2016/01/18 12:53:26 jperkin Exp $
+$NetBSD: patch-ssh.c,v 1.6 2016/03/15 20:54:07 bsiegert Exp $
 
 Interix support
-Disable roaming
 
---- ssh.c.orig	2015-08-21 04:49:03.000000000 +0000
+--- ssh.c.orig	2016-03-09 18:04:48.000000000 +0000
 +++ ssh.c
-@@ -1084,7 +1084,7 @@ main(int ac, char **av)
- 		    "disabling");
- 		options.update_hostkeys = 0;
+@@ -1097,7 +1097,7 @@ main(int ac, char **av)
  	}
+ 	if (options.connection_attempts <= 0)
+ 		fatal("Invalid number of ConnectionAttempts");
 -#ifndef HAVE_CYGWIN
-+#if defined(HAVE_CYGWIN) || defined(HAVE_INTERIX)
++#if !defined(HAVE_CYGWIN) && !defined(HAVE_INTERIX)
  	if (original_effective_uid != 0)
  		options.use_privileged_port = 0;
  #endif
-@@ -1932,9 +1932,6 @@ ssh_session2(void)
- 			fork_postauth();
- 	}
- 
--	if (options.use_roaming)
--		request_roaming();
--
- 	return client_loop(tty_flag, tty_flag ?
- 	    options.escape_char : SSH_ESCAPECHAR_NONE, id);
- }
