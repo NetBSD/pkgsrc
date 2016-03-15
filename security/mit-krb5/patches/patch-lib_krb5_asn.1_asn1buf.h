564848ba118e4cd2c9d8bac3e6ef8447ec690c59
tez <tez>
2016-03-15 15:16:39 +0000

    Update to 1.14.1 resolving all reported vulnerabilities including: CVE-2015-2695 CVE-2015-2696 CVE-2015-2697 CVE-2015-2698 CVE-2015-8629 CVE-2015-8630 CVE-2015-8631

--- security/mit-krb5/patches/patch-lib_krb5_asn.1_asn1buf.h
+++ security/mit-krb5/patches/patch-lib_krb5_asn.1_asn1buf.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-lib_krb5_asn.1_asn1buf.h,v 1.1 2011/06/01 09:57:23 adam Exp $
+$NetBSD: patch-lib_krb5_asn.1_asn1buf.h,v 1.2 2016/03/15 15:16:39 tez Exp $
 
 Clang does not like this kind of inlines.
 
---- lib/krb5/asn.1/asn1buf.h.orig	2011-06-01 09:47:31.000000000 +0000
+--- lib/krb5/asn.1/asn1buf.h.orig	2016-02-29 19:50:13.000000000 +0000
 +++ lib/krb5/asn.1/asn1buf.h
-@@ -147,7 +147,7 @@ asn1_error_code asn1buf_insert_octet(asn
+@@ -107,7 +107,7 @@ void asn1buf_destroy(asn1buf **buf);
   * effects   Inserts o into the buffer *buf, expanding the buffer if
   *           necessary.  Returns ENOMEM memory is exhausted.
   */
 -#if ((__GNUC__ >= 2) && !defined(ASN1BUF_OMIT_INLINE_FUNCS)) && !defined(CONFIG_SMALL)
 +#if ((__GNUC__ >= 2) && !defined(__clang__) && !defined(ASN1BUF_OMIT_INLINE_FUNCS)) && !defined(CONFIG_SMALL)
- extern __inline__ asn1_error_code asn1buf_insert_octet(asn1buf *buf, const int o)
+ static inline asn1_error_code
+ asn1buf_insert_octet(asn1buf *buf, const int o)
  {
-     asn1_error_code retval;
