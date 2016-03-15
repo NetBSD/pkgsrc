04f4e96d5cdef2ff1df61f5a66ef94955d621f10
szptvlfn <szptvlfn@pkgsrc.org>
2016-03-15 13:45:17 +0000

    Update to 0.13.9
    CHANGES.txt:
    0.13.9
        #50, fix a space leak
        #36, fix the demo examples
        #35, make IsString a superclass of StringLike
        #33, make flattenTree O(n) instead of O(n^2)
    0.13.8
        #30, add parse/render functions directly to the Tree module
    0.13.7
        #32, make sure upper case &#X works in lookupEntity
    0.13.6
        #28, some named entities require a trailing semicolon (e.g. mid)
    0.13.5
        #26, rename the test program to test-tagsoup
    0.13.4
        #24, add isTagComment function
        Update the copyright year
    0.13.3
        Work on GHC 7.9
    0.13.2
        Remove all package upper bounds
        Allow QuickCheck-2.6

--- textproc/hs-tagsoup/buildlink3.mk
+++ textproc/hs-tagsoup/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2016/01/10 11:49:11 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2016/03/15 13:45:17 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-tagsoup
 
 .if !defined(HS_TAGSOUP_BUILDLINK3_MK)
 HS_TAGSOUP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.1
-BUILDLINK_ABI_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.1nb7
+BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.9
+BUILDLINK_ABI_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.9
 BUILDLINK_PKGSRCDIR.hs-tagsoup?=	../../textproc/hs-tagsoup
 
 .include "../../devel/hs-text/buildlink3.mk"
