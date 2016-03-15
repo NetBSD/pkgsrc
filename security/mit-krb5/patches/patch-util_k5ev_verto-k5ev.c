d5761b9254f251f97d6e0eb5a6cb5501540e04f1
tez <tez@pkgsrc.org>
2016-03-15 15:16:39 +0000

    Update to 1.14.1 resolving all reported vulnerabilities including: CVE-2015-2695 CVE-2015-2696 CVE-2015-2697 CVE-2015-2698 CVE-2015-8629 CVE-2015-8630 CVE-2015-8631

--- security/mit-krb5/patches/patch-util_k5ev_verto-k5ev.c
+++ security/mit-krb5/patches/patch-util_k5ev_verto-k5ev.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-util_k5ev_verto-k5ev.c,v 1.2 2013/03/13 12:35:40 adam Exp $
+$NetBSD: patch-util_k5ev_verto-k5ev.c,v 1.3 2016/03/15 15:16:39 tez Exp $
 
 Fix include file path
 
---- util/k5ev/verto-k5ev.c.orig	2013-03-02 01:05:38.000000000 +0000
-+++ util/k5ev/verto-k5ev.c
-@@ -34,7 +34,7 @@
+--- util/verto/verto-k5ev.c.orig	2016-02-29 19:50:13.000000000 +0000
++++ util/verto/verto-k5ev.c
+@@ -35,7 +35,7 @@
  
- #include "verto-k5ev.h"
+ #include <verto.h>
  #include <verto-module.h>
 -#include "rename.h"
 +#include "gssrpc/rename.h"
- #include "autoconf.h"
  #define EV_STANDALONE 1
  /* Avoid using clock_gettime, which would create a dependency on librt. */
+ #define EV_USE_MONOTONIC 0
