80fc2229e8e766192d5930441347ed9c0ae8a5e0
wiz <wiz@pkgsrc.org>
2016-03-18 09:36:26 +0000

    revert ABI/ABI bump for expat.
    Not necessary and cuases problems.

--- textproc/expat/buildlink3.mk
+++ textproc/expat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.20 2016/03/16 19:55:55 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2016/03/18 09:36:26 wiz Exp $
 
 BUILDLINK_TREE+=	expat
 
 .if !defined(EXPAT_BUILDLINK3_MK)
 EXPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.expat+=	expat>=2.1.1
-BUILDLINK_ABI_DEPENDS.expat+=	expat>=2.1.1
+BUILDLINK_API_DEPENDS.expat+=	expat>=1.95.7
+BUILDLINK_ABI_DEPENDS.expat+=	expat>=2.0.0nb1
 BUILDLINK_PKGSRCDIR.expat?=	../../textproc/expat
 .endif # EXPAT_BUILDLINK3_MK
 
