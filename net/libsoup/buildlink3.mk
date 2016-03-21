862db7abe9126efdc77d5fd7fef3028cc4dc5e2c
khorben <khorben@pkgsrc.org>
2016-03-21 22:52:34 +0000

    Relax the requirement on libncurses to any implementation
    As suggested by wiz@. net/gssdp still builds with this change.

--- net/libsoup/buildlink3.mk
+++ net/libsoup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2016/03/21 19:22:14 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2016/03/21 22:52:34 khorben Exp $
 
 BUILDLINK_TREE+=	libsoup
 
@@ -8,9 +8,9 @@ LIBSOUP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libsoup+=	libsoup>=2.50.0
 BUILDLINK_PKGSRCDIR.libsoup?=	../../net/libsoup
 
+.include "../../mk/curses.buildlink3.mk"
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/ncurses/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .endif	# LIBSOUP_BUILDLINK3_MK
 
