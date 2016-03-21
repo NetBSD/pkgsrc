80f91df1534c0f955137103b14e7ed80e6694710
khorben <khorben@pkgsrc.org>
2016-03-21 19:22:14 +0000

    More accurate fix for packages depending on net/libsoup
    Apparently linking with libsoup requires devel/ncurses too now.

--- net/libsoup/buildlink3.mk
+++ net/libsoup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2015/06/08 14:52:33 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2016/03/21 19:22:14 khorben Exp $
 
 BUILDLINK_TREE+=	libsoup
 
@@ -10,6 +10,7 @@ BUILDLINK_PKGSRCDIR.libsoup?=	../../net/libsoup
 
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/ncurses/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .endif	# LIBSOUP_BUILDLINK3_MK
 
