083c77f6b6765ba39c7ffb8ee4e0858d024872f6
joerg <joerg@pkgsrc.org>
2016-03-22 18:55:30 +0000

    Deal with API changes of jpeg-9.

--- graphics/cpia2view/patches/patch-jpeg__decode.c
+++ graphics/cpia2view/patches/patch-jpeg__decode.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-jpeg__decode.c,v 1.1 2014/02/10 11:43:09 obache Exp $
+$NetBSD: patch-jpeg__decode.c,v 1.2 2016/03/22 18:55:30 joerg Exp $
 
-* To match prototype from jpeg>=8 library.
+* To match prototype from jpeg>=9 library.
 
 --- jpeg_decode.c.orig	2014-02-10 11:34:47.000000000 +0000
 +++ jpeg_decode.c
@@ -9,7 +9,7 @@ $NetBSD: patch-jpeg__decode.c,v 1.1 2014/02/10 11:43:09 obache Exp $
  
  GLOBAL(void)
 -jpeg_mem_src (j_decompress_ptr cinfo, unsigned char * source_ptr, size_t source_length)
-+jpeg_mem_src (j_decompress_ptr cinfo, unsigned char * source_ptr, unsigned long source_length)
++jpeg_mem_src (j_decompress_ptr cinfo, const unsigned char * source_ptr, unsigned long source_length)
  {
  	mem_src_ptr src;
     
