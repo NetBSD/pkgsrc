9fc75b34477440305c1b636bed8ab61c84b546b4
gdt <gdt@pkgsrc.org>
2016-03-23 16:36:53 +0000

    Exclude NetBSD 1-5 from using go-pkglint
    (Surely more systems should be using pkgint4, as it's obvious that
    being i386 and no SunOS is not sufficient.  However, this is a
    ~minimal fix for NetBSD 5.)

--- pkgtools/pkglint/select.mk
+++ pkgtools/pkglint/select.mk
@@ -1,4 +1,4 @@
-# $NetBSD: select.mk,v 1.3 2016/02/02 08:00:10 rillig Exp $
+# $NetBSD: select.mk,v 1.4 2016/03/23 16:36:53 gdt Exp $
 #
 # Selects the proper version of pkglint, depending on whether the
 # platform supports the Go programming language.
@@ -7,7 +7,8 @@
 .include "../../mk/bsd.fast.prefs.mk"
 
 # See lang/go/version.mk
-.if ${MACHINE_ARCH:Ni386:Nx86_64:Nevbarm} || ${MACHINE_PLATFORM:MSunOS-*-i386}
+# While it's wrong in the above, go14 does not build on NetBSD 5.
+.if ${MACHINE_ARCH:Ni386:Nx86_64:Nevbarm} || ${MACHINE_PLATFORM:MSunOS-*-i386} || ${MACHINE_PLATFORM:MNetBSD-[1-5]*-*}
 DEPENDS+=	pkglint4>=4.82<5:../../pkgtools/pkglint4
 .else
 DEPENDS+=	pkglint>=5:../../pkgtools/pkglint
