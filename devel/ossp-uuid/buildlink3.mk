52a4a4a30fc348e85af9ea7883365b1c9226e2fc
fhajny <fhajny@pkgsrc.org>
2016-03-31 20:25:23 +0000

    SunOS needs libnsl/libsocket to resolve some symbols in libossp-uuid.

--- devel/ossp-uuid/buildlink3.mk
+++ devel/ossp-uuid/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:24:25 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/03/31 20:25:23 fhajny Exp $
 
 BUILDLINK_TREE+=	ossp-uuid
 
@@ -8,6 +8,13 @@ OSSP_UUID_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.ossp-uuid+=	ossp-uuid>=1.6.0
 BUILDLINK_ABI_DEPENDS.ossp-uuid+=	ossp-uuid>=1.6.2
 BUILDLINK_PKGSRCDIR.ossp-uuid?=		../../devel/ossp-uuid
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} == "SunOS"
+BUILDLINK_LDADD.ossp-uuid+=	-lnsl -lsocket
+BUILDLINK_LDFLAGS.ossp-uuid?=	${BUILDLINK_LDADD.ossp-uuid}
+.endif
 .endif # OSSP_UUID_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ossp-uuid
