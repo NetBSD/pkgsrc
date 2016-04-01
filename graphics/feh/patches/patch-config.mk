861c90e6c079f4eaf61c03d00ea4271818fcc8db
jperkin <jperkin>
2016-04-01 15:34:59 +0000

    Use PKGMANDIR.

--- graphics/feh/patches/patch-config.mk
+++ graphics/feh/patches/patch-config.mk
@@ -1,4 +1,4 @@
-$NetBSD: patch-config.mk,v 1.2 2012/05/01 14:58:47 gls Exp $
+$NetBSD: patch-config.mk,v 1.3 2016/04/01 15:34:59 jperkin Exp $
 
 Fix man pages installation path
 
@@ -9,7 +9,7 @@ Fix man pages installation path
  # Directories for manuals, executables, docs, data, etc.
  main_dir = ${DESTDIR}${PREFIX}
 -man_dir = ${main_dir}/share/man
-+man_dir = ${main_dir}/man
++man_dir = ${main_dir}/${PKGMANDIR}
  bin_dir = ${main_dir}/bin
  doc_dir = ${main_dir}/share/doc/feh
  image_dir = ${main_dir}/share/feh/images
