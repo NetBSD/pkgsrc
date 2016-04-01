ad9d3a2810697d52fc7eda22cb3f3128e25eebe0
jperkin <jperkin@pkgsrc.org>
2016-04-01 11:20:58 +0000

    Support PKGMANDIR.  Add patch comments.

--- mail/safecat/patches/patch-hier.c
+++ mail/safecat/patches/patch-hier.c
@@ -1,14 +1,22 @@
-$NetBSD: patch-hier.c,v 1.1 2012/03/14 23:16:29 joerg Exp $
+$NetBSD: patch-hier.c,v 1.2 2016/04/01 11:20:58 jperkin Exp $
 
---- hier.c.orig	2012-03-13 17:44:57.000000000 +0000
+Use PKGMANDIR.
+
+--- hier.c.orig	2001-04-14 11:09:54.000000000 +0000
 +++ hier.c
-@@ -9,9 +9,6 @@ void hier()
+@@ -7,11 +7,8 @@ void hier()
+   c(auto_home,"bin","safecat",-1,-1,0755);
+   c(auto_home,"bin","maildir",-1,-1,0755);
  
-   d(auto_home,"man",-1,-1,02755);
-   d(auto_home,"man/man1",-1,-1,02755);
+-  d(auto_home,"man",-1,-1,02755);
+-  d(auto_home,"man/man1",-1,-1,02755);
 -  d(auto_home,"man/cat1",-1,-1,02755);
-   c(auto_home,"man/man1","safecat.1",-1,-1,0644);
+-  c(auto_home,"man/man1","safecat.1",-1,-1,0644);
 -  c(auto_home,"man/cat1","safecat.0",-1,-1,0644);
-   c(auto_home,"man/man1","maildir.1",-1,-1,0644);
+-  c(auto_home,"man/man1","maildir.1",-1,-1,0644);
 -  c(auto_home,"man/cat1","maildir.0",-1,-1,0644);
++  d(auto_home,"@PKGMANDIR@",-1,-1,02755);
++  d(auto_home,"@PKGMANDIR@/man1",-1,-1,02755);
++  c(auto_home,"@PKGMANDIR@/man1","safecat.1",-1,-1,0644);
++  c(auto_home,"@PKGMANDIR@/man1","maildir.1",-1,-1,0644);
  }
