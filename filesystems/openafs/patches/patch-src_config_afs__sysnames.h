6ab343fa1790271dbae105987ca75911837ca1b5
jakllsch <jakllsch@pkgsrc.org>
2016-04-04 12:48:29 +0000

    Fix build of OpenAFS userland on at least NetBSD/amd64 7.0. Hopefully NetBSD/x86 -current also works.
    Should merely be a build fix, but bump PKGREVISION anyway.
    
    This commit allocates sysname numbers that have not yet been submitted
    upstream.

---
+++ filesystems/openafs/patches/patch-src_config_afs__sysnames.h
@@ -0,0 +1,17 @@
+$NetBSD: patch-src_config_afs__sysnames.h,v 1.3 2016/04/04 12:48:29 jakllsch Exp $
+
+--- src/config/afs_sysnames.h.orig	2016-03-16 14:31:22.000000000 +0000
++++ src/config/afs_sysnames.h
+@@ -267,6 +267,12 @@
+ #define SYS_NAME_ID_macppc_nbsd50       2541
+ #define SYS_NAME_ID_amd64_nbsd60        2542
+ #define SYS_NAME_ID_i386_nbsd60         2543
++#define SYS_NAME_ID_amd64_nbsd70        2544
++#define SYS_NAME_ID_i386_nbsd70         2545
++#define SYS_NAME_ID_amd64_nbsd80        2546
++#define SYS_NAME_ID_i386_nbsd80         2547
++#define SYS_NAME_ID_amd64_nbsd90        2548
++#define SYS_NAME_ID_i386_nbsd90         2549
+ 
+ #define SYS_NAME_ID_i386_obsd31		2600
+ #define SYS_NAME_ID_i386_obsd32		2601
