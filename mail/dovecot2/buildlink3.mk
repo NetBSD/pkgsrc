d2132f2f5cfa05d07159740f582621d228da8c7c
adam <adam@pkgsrc.org>
2016-04-08 07:55:15 +0000

    Changes 2.2.23: This is a bugfix-only release with various important fixes on top of v2.2.22.  - Various fixes to doveadm. Especially running commands via    doveadm-server was broken.  - director: Fixed user weakness getting stuck in some situations  - director: Fixed a situation where directors keep re-sending    different states to each others and never becoming synced.  - director: Fixed assert-crash related to a slow "user killed" reply  - Fixed assert-crash related to istream-concat, which could have    been triggered at least by a Sieve script.

--- mail/dovecot2/buildlink3.mk
+++ mail/dovecot2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2016/03/05 11:27:50 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2016/04/08 07:55:16 adam Exp $
 
 BUILDLINK_TREE+=	dovecot
 
@@ -7,7 +7,7 @@ DOVECOT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.dovecot+=		dovecot>=2.2.0
 # must match current package version for plugins to load
-BUILDLINK_ABI_DEPENDS.dovecot+=		dovecot>=2.2.21nb1
+BUILDLINK_ABI_DEPENDS.dovecot+=		dovecot>=2.2.23
 BUILDLINK_PKGSRCDIR.dovecot?=		../../mail/dovecot2
 
 pkgbase:=	dovecot
