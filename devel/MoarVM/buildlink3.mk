206b94c6a97875c639171e36c2aff7f54b91d196
bsiegert <bsiegert@pkgsrc.org>
2016-04-08 20:26:46 +0000

    Import MoarVM-2016.03 from wip.
    Short for "Metamodel On A Runtime", MoarVM is a virtual machine built
    especially for Rakudo Perl 6 and the NQP Compiler Toolchain. The primary
    backend target for the Christmas 2015 release of Perl 6, MoarVM stands
    out amongst the various Rakudo and NQP compilation targets by:
    
    - Running the Perl 6 specification test suite in the least time
    - Passing more of the Perl 6 specification tests than Rakudo Perl 6 on
      other backends
    - Having the lowest memory usage
    - Having the best startup time
    - Being fastest to build both NQP and Rakudo - and thus your Perl 6 and
      NQP programs too!
    - Being the only backend to support strings at grapheme level

---
+++ devel/MoarVM/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/04/08 20:26:46 bsiegert Exp $
+
+BUILDLINK_TREE+=	MoarVM
+
+.if !defined(MOARVM_BUILDLINK3_MK)
+MOARVM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.MoarVM+=	MoarVM>=2015.12
+BUILDLINK_PKGSRCDIR.MoarVM?=	../../devel/MoarVM
+
+.include "../../devel/libatomic_ops/buildlink3.mk"
+.include "../../devel/libffi/buildlink3.mk"
+.include "../../devel/libuv/buildlink3.mk"
+.include "../../math/ltm/buildlink3.mk"
+.endif	# MOARVM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-MoarVM
