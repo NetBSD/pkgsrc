1cd762841524af7b51d78f0bda16aec6100dcb05
joerg <joerg@pkgsrc.org>
2016-04-10 15:58:02 +0000

    Remove support for USE_DESTDIR=no.

--- mk/pkgformat/pkg/pkgformat.mk
+++ mk/pkgformat/pkg/pkgformat.mk
@@ -1,19 +1,13 @@
-# $NetBSD: pkgformat.mk,v 1.2 2014/12/30 15:13:20 wiz Exp $
+# $NetBSD: pkgformat.mk,v 1.3 2016/04/10 15:58:03 joerg Exp $
 #
 # This Makefile fragment provides variable and target overrides that are
 # specific to the pkgsrc native package format.
 #
 
-# PKG_FILELIST_CMD outputs the list of files owned by ${PKGNAME} as
-# registered on the system.
+# PKG_FILELIST_CMD outputs the list of files in the package based on
+# _DEPENDS_PLIST.
 #
-# For DESTDIR support, just use _DEPENDS_PLIST instead.
-#
-.if ${_USE_DESTDIR} == "no"
-PKG_FILELIST_CMD=	${PKG_INFO} -qL ${PKGNAME:Q}
-.else
 PKG_FILELIST_CMD=	${SED} -e "/^@/d" -e "s|^|${PREFIX}/|" ${_DEPENDS_PLIST}
-.endif
 
 .include "depends.mk"
 .include "check.mk"
