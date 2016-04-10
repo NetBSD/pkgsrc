603a7ca1f63c9043a81bd8a4e298936a48a883b9
prlw1 <prlw1@pkgsrc.org>
2016-04-10 18:49:42 +0000

    Update vala to 0.32.0
    Vala 0.32.0
    ===========
    
     * Bug fixes and binding updates.
    
    Vala 0.31.1
    ===========
    
     * Introduce [Version] attribute.
     * Require and target GLib >= 2.32.
     * Bug fixes and binding updates.

--- lang/vala/buildlink3.mk
+++ lang/vala/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.13 2016/02/04 12:12:36 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2016/04/10 18:49:42 prlw1 Exp $
 
 BUILDLINK_TREE+=	vala
 
 .if !defined(VALA_BUILDLINK3_MK)
 VALA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vala+=	vala>=0.30
+BUILDLINK_API_DEPENDS.vala+=	vala>=0.32
 BUILDLINK_PKGSRCDIR.vala?=	../../lang/vala
 
-VALAC=${LOCALBASE}/bin/valac-0.30
-VAPIGEN=${LOCALBASE}/bin/vapigen-0.30
+VALAC=${LOCALBASE}/bin/valac-0.32
+VAPIGEN=${LOCALBASE}/bin/vapigen-0.32
 .if defined(GNU_CONFIGURE)
 CONFIGURE_ENV+=	VALAC=${VALAC} VAPIGEN=${VAPIGEN}
 .endif
