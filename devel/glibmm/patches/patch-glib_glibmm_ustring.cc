c0533624d8d37e9841421f337e6c802ffd651845
adam <adam@pkgsrc.org>
2016-04-12 21:27:25 +0000

    2.48.1: Build: * MacOS X: Add alternative #ifdefed code when thread_local   is not supported by the compiler. * Replace most typedefs with the C++11 using keyword.

--- devel/glibmm/patches/patch-glib_glibmm_ustring.cc
+++ devel/glibmm/patches/patch-glib_glibmm_ustring.cc
@@ -1,19 +1,19 @@
-$NetBSD: patch-glib_glibmm_ustring.cc,v 1.1 2015/02/03 11:50:43 adam Exp $
+$NetBSD: patch-glib_glibmm_ustring.cc,v 1.2 2016/04/12 21:27:25 adam Exp $
 
 Work-around NetBSD's native iconv lacking support for WCHAR_T.
 
---- glib/glibmm/ustring.cc.orig	2015-02-03 11:32:45.000000000 +0000
+--- glib/glibmm/ustring.cc.orig	2016-02-29 09:31:32.000000000 +0000
 +++ glib/glibmm/ustring.cc
-@@ -1265,7 +1265,7 @@ ustring ustring::FormatStream::to_string
+@@ -1353,7 +1353,7 @@ ustring::FormatStream::to_string() const
  #ifdef GLIBMM_HAVE_WIDE_STREAM
    const std::wstring str = stream_.str();
  
--# if defined(__STDC_ISO_10646__) && SIZEOF_WCHAR_T == 4
-+# if (defined(__STDC_ISO_10646__) || defined(__NetBSD__)) && SIZEOF_WCHAR_T == 4
+-#if defined(__STDC_ISO_10646__) && SIZEOF_WCHAR_T == 4
++#if (defined(__STDC_ISO_10646__) || defined(__NetBSD__)) && SIZEOF_WCHAR_T == 4
    // Avoid going through iconv if wchar_t always contains UCS-4.
    glong n_bytes = 0;
-   const ScopedPtr<char> buf (g_ucs4_to_utf8(reinterpret_cast<const gunichar*>(str.data()),
-@@ -1349,7 +1349,7 @@ std::wistream& operator>>(std::wistream&
+   const auto buf = make_unique_ptr_gfree(g_ucs4_to_utf8(
+@@ -1441,7 +1441,7 @@ operator>>(std::wistream& is, ustring& u
    std::wstring wstr;
    is >> wstr;
  
@@ -21,13 +21,13 @@ Work-around NetBSD's native iconv lacking support for WCHAR_T.
 +#if (defined(__STDC_ISO_10646__) || defined(__NetBSD__)) && SIZEOF_WCHAR_T == 4
    // Avoid going through iconv if wchar_t always contains UCS-4.
    glong n_bytes = 0;
-   const ScopedPtr<char> buf (g_ucs4_to_utf8(reinterpret_cast<const gunichar*>(wstr.data()),
-@@ -1380,7 +1380,7 @@ std::wostream& operator<<(std::wostream&
+   const auto buf = make_unique_ptr_gfree(g_ucs4_to_utf8(
+@@ -1472,7 +1472,7 @@ operator<<(std::wostream& os, const ustr
  {
-   GError* error = 0;
+   GError* error = nullptr;
  
 -#if defined(__STDC_ISO_10646__) && SIZEOF_WCHAR_T == 4
 +#if (defined(__STDC_ISO_10646__) || defined(__NetBSD__)) && SIZEOF_WCHAR_T == 4
    // Avoid going through iconv if wchar_t always contains UCS-4.
-   const ScopedPtr<gunichar> buf (g_utf8_to_ucs4(utf8_string.raw().data(),
-                                                 utf8_string.raw().size(), 0, 0, &error));
+   const auto buf = make_unique_ptr_gfree(
+     g_utf8_to_ucs4(utf8_string.raw().data(), utf8_string.raw().size(), nullptr, nullptr, &error));
