a694ffef44dd233879cd0596fb877f538b4d1e7e
prlw1 <prlw1@pkgsrc.org>
2016-04-12 16:08:05 +0000

    Add gspell 1.0.1
    gspell is a gnome wrapper for enchant, needed by gedit.

---
+++ textproc/gspell/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/04/12 16:08:05 prlw1 Exp $
+
+BUILDLINK_TREE+=	gspell
+
+.if !defined(GSPELL_BUILDLINK3_MK)
+GSPELL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gspell+=	gspell>=1.0.1
+BUILDLINK_PKGSRCDIR.gspell?=	../../textproc/gspell
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../textproc/enchant/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.endif	# GSPELL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gspell
