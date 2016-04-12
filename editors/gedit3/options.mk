af7e52de802998f14f8545fd9dff01b62585b60e
prlw1 <prlw1@pkgsrc.org>
2016-04-12 16:12:55 +0000

    Update gedit3 to 3.20.1
    - Use a menubar only if gtk-shell-shows-menubar is set to true
    - Spell plugin: adapt code for gspell API changes
    - Save window size properly
    - CSS updates
    - AppData: add project_group and translation tags
    - Rename to "Text Editor" in the .desktop file
    - Use traditional menubar layout on XFCE, Unity, etc
    - require the latest gspell for the spell plugin
    - New Windows WiX installer
    - CSS updates to track GTK+ changes
    - Ship our own app icon
    - Add shortcuts window
    - Adapt code for gspell API changes
    - Rename GeditViewHolder -> GeditViewCentering
    - CloseConfirmationDialog: subclass GtkMessageDialog
    - CSS updates to track GTK+ changes
    - Change default document title to "Untitled Document"
    - Port the spell plugin to gspell
    - Make the Replace dialog resizable
    - Fix close confirmation dialog regression introduced in 3.17.92
    - Spell checking: fix performance problem and re-organize the code
    - Move minimap settings in the preferences dialog
    - CSS improvements
    - Add a snippet generating RPM SPEC %changelog line
    - gtk-mac-integration support fix
    - Add a menu to toggle the source map
    - Fix print margins
    - Fix the --wait options when passing a list of files
    - Improve the Encodings dialog
    - Add setting to display a grid pattern
    - Misc bugfixes
    - Translation updates

--- editors/gedit3/options.mk
+++ editors/gedit3/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2014/12/06 20:41:36 prlw1 Exp $
+# $NetBSD: options.mk,v 1.2 2016/04/12 16:12:55 prlw1 Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gedit3
 PKG_SUPPORTED_OPTIONS=	enchant
@@ -10,11 +10,11 @@ PLIST_VARS=		enchant
 
 ###
 ### Spell-check plugin which uses enchant
+### (gspell is a wrapper for enchant)
 ###
 .if !empty(PKG_OPTIONS:Menchant)
 CONFIGURE_ARGS+=	--enable-spell
-.include "../../textproc/enchant/buildlink3.mk"
-.include "../../textproc/iso-codes/buildlink3.mk"
+.include "../../textproc/gspell/buildlink3.mk"
 PLIST.enchant=		yes
 .else
 CONFIGURE_ARGS+=	--disable-spell
