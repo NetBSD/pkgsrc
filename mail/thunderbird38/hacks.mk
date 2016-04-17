bf7d0e0b8f7dae278a0c0ab38f8f56a8315cc81a
ryoon <ryoon@pkgsrc.org>
2016-04-17 18:22:21 +0000

    Import thunderbird38-38.7.2 as mail/thunderbird38.
    Mozilla Thunderbird is a redesign of the Mozilla mail component. The
    goal is to produce a cross platform stand alone mail application using
    the XUL user interface language. This version uses the gtk2 toolkit.
    
    This package tracks 38 ESR branch.

---
+++ mail/thunderbird38/hacks.mk
@@ -0,0 +1,11 @@
+# $NetBSD: hacks.mk,v 1.1 2016/04/17 18:22:21 ryoon Exp $
+
+# workround for link of thunderbird-bin etc.
+LDFLAGS+=	${COMPILER_RPATH_FLAG}${PREFIX}/lib/${MOZILLA_NAME}
+
+.if ${OPSYS} == "SunOS"
+# workaround for strip problems with libxul.so
+# https://www.illumos.org/issues/4383
+INSTALL_UNSTRIPPED=yes
+.endif
+.include "../../devel/xulrunner17/hacks.mk"
---
+++ mail/thunderbird38/patches/patch-mozilla_xpcom_build_XPCOMInit.cpp
@@ -0,0 +1,28 @@
+$NetBSD: patch-mozilla_xpcom_build_XPCOMInit.cpp,v 1.1 2016/04/17 18:22:21 ryoon Exp $
+
+--- mozilla/xpcom/build/XPCOMInit.cpp.orig	2015-06-08 17:49:33.000000000 +0000
++++ mozilla/xpcom/build/XPCOMInit.cpp
+@@ -141,7 +141,9 @@ extern nsresult nsStringInputStreamConst
+ #include "mozilla/VisualEventTracer.h"
+ #endif
+ 
++#ifndef MOZ_OGG_NO_MEM_REPORTING
+ #include "ogg/ogg.h"
++#endif
+ #if defined(MOZ_VPX) && !defined(MOZ_VPX_NO_MEM_REPORTING)
+ #if defined(HAVE_STDINT_H)
+ // mozilla-config.h defines HAVE_STDINT_H, and then it's defined *again* in
+@@ -669,11 +671,13 @@ NS_InitXPCOM2(nsIServiceManager** aResul
+   // this oddness.
+   mozilla::SetICUMemoryFunctions();
+ 
++#ifndef MOZ_OGG_NO_MEM_REPORTING
+   // Do the same for libogg.
+   ogg_set_mem_functions(OggReporter::CountingMalloc,
+                         OggReporter::CountingCalloc,
+                         OggReporter::CountingRealloc,
+                         OggReporter::CountingFree);
++#endif
+ 
+ #if defined(MOZ_VPX) && !defined(MOZ_VPX_NO_MEM_REPORTING)
+   // And for VPX.
