0809ea269a5a4487eb78311c3483857ed7308d99
jperkin <jperkin@pkgsrc.org>
2016-04-19 13:48:31 +0000

    Handle dts PLIST entries correctly.

--- multimedia/xine-lib/options.mk
+++ multimedia/xine-lib/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2016/04/19 10:41:14 jperkin Exp $
+# $NetBSD: options.mk,v 1.2 2016/04/19 13:48:31 jperkin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xine-lib
 
@@ -7,8 +7,10 @@ PKG_SUPPORTED_OPTIONS=	dts
 .include "../../mk/bsd.options.mk"
 
 # Marked NO_BIN_ON_*.  Only enable if you don't want to distribute packages.
+PLIST_VARS+=		dts
 .if !empty(PKG_OPTIONS:Mdts)
 .include "../../audio/libdca/buildlink3.mk"
+PLIST.dts=		yes
 .else
 CONFIGURE_ARGS+=	--disable-dts
 .endif
