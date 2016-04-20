2f06f6bcc8a144d3382a06b5c9c4e77670ea08d6
leot <leot@pkgsrc.org>
2016-04-20 16:10:14 +0000

    Update www/py-h2 to 2.2.3
    Changes:
    2.2.3 (2016-04-13)
    ------------------
    Bugfixes
    ~~~~~~~~
    - Allowed the 4.X series of hyperframe releases as dependencies.
    
    2.1.4 (2016-04-13)
    ------------------
    Bugfixes
    ~~~~~~~~
    - Allowed the 4.X series of hyperframe releases as dependencies.
    
    2.2.2 (2016-04-05)
    ------------------
    Bugfixes
    ~~~~~~~~
    - Fixed issue where informational responses were erroneously not allowed to be
      sent in the ``HALF_CLOSED_REMOTE`` state.
    - Fixed issue where informational responses were erroneously not allowed to be
      received in the ``HALF_CLOSED_LOCAL`` state.
    - Fixed issue where we allowed information responses to be sent or received
      after final responses.
    
    2.2.1 (2016-03-23)
    ------------------
    Bugfixes
    ~~~~~~~~
    - Fixed issue where users using locales that did not default to UTF-8 were
      unable to install source distributions of the package.
    
    2.2.0 (2016-03-23)
    ------------------
    API Changes (Backward-Compatible)
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    - Added support for sending informational responses (responses with 1XX status)
      codes as part of the standard flow. HTTP/2 allows zero or more informational
      responses with no upper limit: hyper-h2 does too.
    - Added support for receiving informational responses (responses with 1XX
      status) codes as part of the standard flow. HTTP/2 allows zero or more
      informational responses with no upper limit: hyper-h2 does too.
    - Added a new event: ``ReceivedInformationalResponse``. This response is fired
      when informational responses (those with 1XX status codes).
    - Added an ``additional_data`` field to the ``ConnectionTerminated`` event that
      carries any additional data sent on the GOAWAY frame. May be ``None`` if no
      such data was sent.
    - Added the ``initial_values`` optional argument to the ``Settings`` object.
    
    Bugfixes
    ~~~~~~~~
    - Correctly reject all of the connection-specific headers mentioned in RFC 7540
      § 8.1.2.2, not just the ``Connection:`` header.
    - Defaulted the value of ``SETTINGS_MAX_CONCURRENT_STREAMS`` to 100, unless
      explicitly overridden. This is a safe defensive initial value for this
      setting.
    
    2.1.3 (2016-03-16)
    ------------------
    Deprecations
    ~~~~~~~~~~~~
    - Passing dictionaries to ``send_headers`` as the header block is deprecated,
      and will be removed in 3.0.
    
    2.1.2 (2016-02-17)
    ------------------
    Bugfixes
    ~~~~~~~~
    - Reject attempts to push streams on streams that were themselves pushed:
      streams can only be pushed on streams that were initiated by the client.
    - Correctly allow CONTINUATION frames to extend the header block started by a
      PUSH_PROMISE frame.
    - Changed our handling of frames received on streams that were reset by the
      user.
      Previously these would, at best, cause ProtocolErrors to be raised and the
      connection to be torn down (rather defeating the point of resetting streams
      at all) and, at worst, would cause subtle inconsistencies in state between
      hyper-h2 and the remote peer that could lead to header block decoding errors
      or flow control blockages.
      Now when the user resets a stream all further frames received on that stream
      are ignored except where they affect some form of connection-level state,
      where they have their effect and are then ignored.
    - Fixed a bug whereby receiving a PUSH_PROMISE frame on a stream that was
      closed would cause a RST_STREAM frame to be emitted on the closed-stream,
      but not the newly-pushed one. Now this causes a ``ProtocolError``.

--- www/py-h2/patches/patch-setup.py
+++ www/py-h2/patches/patch-setup.py
@@ -1,20 +1,20 @@
-$NetBSD: patch-setup.py,v 1.1 2016/02/16 13:49:56 leot Exp $
+$NetBSD: patch-setup.py,v 1.2 2016/04/20 16:10:14 leot Exp $
 
 Avoid too strict version requirements.
 
---- setup.py.orig	2016-02-05 14:50:35.000000000 +0000
+--- setup.py.orig	2016-04-13 11:17:13.000000000 +0000
 +++ setup.py
-@@ -57,10 +57,10 @@ setup(
+@@ -61,10 +61,10 @@ setup(
          'Programming Language :: Python :: Implementation :: PyPy',
      ],
      install_requires=[
--        'hyperframe>=3.1, <4',
+-        'hyperframe>=3.1, <5, !=4.0.0',
 -        'hpack>=2.0, <3',
 +        'hyperframe>=3.1',
 +        'hpack>=2.0',
      ],
      extras_require={
--        ':python_version == "2.7" or python_version == "3.3"': ['enum34>=1.0.4, <1.1'],
+-        ':python_version == "2.7" or python_version == "3.3"': ['enum34>=1.0.4, <2'],
 +        ':python_version == "2.7" or python_version == "3.3"': ['enum34>=1.0.4'],
      }
  )
