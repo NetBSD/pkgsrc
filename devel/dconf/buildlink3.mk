e2bc008a0b6904e84dbfaa45dfc9e98fe8ba7505
prlw1 <prlw1@pkgsrc.org>
2016-04-20 22:00:55 +0000

    Update dconf to 0.26.0
    XXX is SUBST_CLASSES.Darwin+= libname still needed?
    
    Changes in dconf 0.26.0
    ========================
    
    None.
    
    Changes in dconf 0.25.1
    ========================
    
     - the libdbus-1 backend has been removed.  dconf now always uses GDBus.
    
     - support has been added for system administration frameworks to set up
       the dconf profile via a file placed in the XDG_RUNTIME_DIR or in
       /run/dconf/.  In the case of the file in /run/dconf/, it is not
       possible to modify the proile, even via the DCONF_PROFILE environment
       variable, which makes it slightly more difficult to evade lockdown.
    
     - directory resets are now implemented properly in DConfChangeset which
       means that reading a subkey through a changeset that contains a reset
       for a parent directory of that key will return TRUE with a NULL value
    
     - a new API dconf_client_read_full() has been added which allows
       reading the user value, the default value, or querying what the
       effective value would be if a changeset were to be applied.
    
     - a new API has been added for listing the locks that are in effect:
       dconf_client_list_locks()
    
     - DConfClient has a new "writability-changed" signal
    
     - support for reading default values and listing locks have been added
       to the dconf commandline tool
    
     - support for g_autoptr() has been added for DConfClient and
       DConfChangeset
    
     - the handling of reading of default values via GSettings has been made
       more efficient.  More major changes to the GSettings backend are
       expected in the near future.
    
    Changes in dconf 0.24.0
    ========================
    
    The version number was increased and a new entry was added to the NEWS.
    
    Changes in dconf 0.23.2
    ========================
    
     - remove dconf-editor manpage (accidentally missed during the split)
    
     - fix whitespace issues in 'dconf --help'
    
    Changes in dconf 0.23.1
    ========================
    
     - dconf-editor is now in a separate package
    
     - portability improvements

--- devel/dconf/buildlink3.mk
+++ devel/dconf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2013/09/30 11:17:32 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2016/04/20 22:00:55 prlw1 Exp $
 
 BUILDLINK_TREE+=	dconf
 
@@ -10,7 +10,6 @@ BUILDLINK_ABI_DEPENDS.dconf?=	dconf>=0.14.0
 BUILDLINK_PKGSRCDIR.dconf?=	../../devel/dconf
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../sysutils/dbus/buildlink3.mk"
 .endif	# DCONF_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-dconf
