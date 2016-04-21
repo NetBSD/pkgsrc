717ae1a110a399cded0485778a54e240d1ef953d
prlw1 <prlw1@pkgsrc.org>
2016-04-21 21:32:03 +0000

    Update libsoup to 2.54.0.1
    Add gssapi option (default off)
    
    
    Changes in libsoup from 2.53.92 to 2.54.0.1:
    
    	* (2.54.0.1 fixes a build problem with the 2.54.0 tarball,
              which would not build if you configured with
              "--without-gnome". There are no other changes between 2.54.0
              and 2.54.0.1.)
    
    	* Fixed examples/simple-httpd on Windows [#758759, Chun-wei
              Fan]
    
    Changes in libsoup from 2.53.90 to 2.53.92:
    
    	* libsoup now supports HTTP "Negotiate"/GSSAPI/Kerberos
              authentication. It must be enabled specifically by the
              application and is also subject to certain other
              restrictions, some of which are not yet controllable through
              the API. [#587145, Guido Guenther, Tomas Popela, David
              Woodhouse, Dan Winship]
    
    	* Added support for building under MSVC [#758759, Chun-wei
              Fan]
    
    	* Fixed a problem with the 2.53.90 tarball that caused
              translations to be mis-installed.
    
    	* Updated translations:
    	  Occitan
    
    Changes in libsoup from 2.53.2 to 2.53.90:
    
    	* NUL bytes in headers are now ignored [#760832, Dan Winship]
    
    	* Fixed transfer annotation of soup_form_decode* functions
              [#743966, Lionel Landwerlin]
    
    	* Updated translations:
    	  Bulgarian, Latvian, Norwegian bokmål
    
    Changes in libsoup from 2.53.1 to 2.53.2:
    
    	* Fixed up symbol visibility handling for mingw by copying
              GLib's system [Ignacio Casal Quinteiro, #757146]
    
    	* Finally marked the old SoupSessionAsync and SoupSessionSync
              methods as deprecated [Ignacio Casal Quinteiro, Dan Winship,
              #757146]
    
    	* Added libsoup-2.4.deps for valac [Rico Tzschichholz]
    
    	* Make it possible to build from git without gtk-doc being
              installed [Ignacio Casal Quinteiro]
    
    	* Updated translations:
    	  Norwegian bokmål, Occitan
    
    Changes in libsoup from 2.52.1 to 2.53.1:
    
    	* Really fixed build under MinGW for sure this time [Ignacio
              Casal Quinteiro]
    
            * Fixed SoupServer Web Sockets code so that the
    	  SoupClientContext passed to a SoupServerWebsocketCallback is
    	  fully usable (rather than crashing when you try to do most
    	  things).

--- net/libsoup/buildlink3.mk
+++ net/libsoup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2016/04/11 19:01:37 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2016/04/21 21:32:03 prlw1 Exp $
 
 BUILDLINK_TREE+=	libsoup
 
@@ -6,10 +6,8 @@ BUILDLINK_TREE+=	libsoup
 LIBSOUP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libsoup+=	libsoup>=2.50.0
-BUILDLINK_ABI_DEPENDS.libsoup?=	libsoup>=2.52.2nb1
 BUILDLINK_PKGSRCDIR.libsoup?=	../../net/libsoup
 
-.include "../../mk/curses.buildlink3.mk"
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
