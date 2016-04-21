1dbf1464c38ce47bb9874ab53948adcd5c127971
leot <leot@pkgsrc.org>
2016-04-21 08:13:50 +0000

    Update graphics/py-Pillow to 3.2.0
    pkgsrc changes:
     o Simplify MASTER_SITES
     o Add LICENSE
     o Disable the tests for now (that were also broken for 3.1.2 version).
       All the modules are builded in build/lib.*/PIL/ while selftest.py just
       checks in PIL/. Manually cp(1)-ing all the build/lib.*/PIL/ files in PIL/
       work as a workaround (and all test are passed) although implementing a
       similar (distutils.cmd) logic will be easily buggy.
    
    Changes:
    3.2.0 (2016-04-01)
    ------------------
    - Added install docs for Fedora 23 and FreeBSD #1729, #1739, #1792
      [koobs, zandermartin, wiredfool]
    - Fixed TIFF multiframe load when the frames have different compression types #1782
      [radarhere, geka000]
    - Added __copy__ method to Image #1772
      [radarhere]
    - Updated dates in PIL license in OleFileIO README  #1787
      [radarhere]
    - Corrected Tiff tag names #1786
      [radarhere]
    - Fixed documented name of JPEG property #1783
      [radarhere]
    - Fixed UnboundLocalErrorwhen loading a corrupt jpeg2k file #1780
      [wiredfool]
    - Fixed integer overflow in path.c #1773
      [wiredfool, nedwill]
    - Added debug to command line help text for pilprint #1766
      [radarhere]
    - Expose many more fields in ICC Profiles #1756
      [lambdafu]
    - Documentation changes, URL update, transpose, release checklist
      [radarhere]
    - Fixed saving to nonexistant files specified by pathlib.Path objects, fixes #1747
      [radarhere]
    - Round Image.crop arguments to the nearest integer, fixes #1744
      [hugovk]
    - Fix uninitialized variable warning in _imaging.c:getink, fixes #486
      [wiredfool]
    - Disable multiprocessing install on cygwin, fixes #1690
      [wiredfool]
    - Fix the error reported when libz is not found #1764
      [wiredfool]
    - More general error check to avoid Symbol not found: _PyUnicodeUCS2_AsLatin1String on OS X #1761
      [wiredfool]
    - Added py35 to tox envlist #1724
      [radarhere]
    - Fix EXIF tag name typos #1736
      [zarlant, radarhere]
    - Updated freetype to 2.6.3, Tk/Tcl to 8.6.5 and 8.5.19
      [radarhere]
    - Add a loader for the FTEX format from Independence War 2: Edge of Chaos #1688
      [jleclanche]
    - Improved alpha_composite documentation #1698
      [radarhere]
    - Extend ImageDraw.text method to pass on multiline_text method specific arguments #1647
      [radarhere]
    - Allow ImageSequence to seek to zero #1686
      [radarhere]
    - ImageSequence Iterator is now an iterator #1649
      [radarhere]
    - Updated windows test builds to jpeg9b
      [radarhere]
    - Fixed support for .gbr version 1 images, added support for version 2 in GbrImagePlugin #1653
      [wiredfool]
    - Clarified which YCbCr format is used #1677
      [radarhere]
    - Added TiffTags documentation, Moved windows build documentation to winbuild/ #1667
      [wiredfool]
    - Add tests for OLE file based formats #1678
      [radarhere]
    - Add TIFF IFD test #1671
      [radarhere]
    - Add a basic DDS image plugin with more tests #1654
      [jleclanche, hugovk, wiredfool]
    - Fix incorrect conditional in encode.c #1638
      [manisandro]

--- graphics/py-Pillow/patches/patch-setup.py
+++ graphics/py-Pillow/patches/patch-setup.py
@@ -1,14 +1,15 @@
-$NetBSD: patch-setup.py,v 1.5 2015/10/18 15:47:10 adam Exp $
+$NetBSD: patch-setup.py,v 1.6 2016/04/21 08:13:50 leot Exp $
 
 * Prevent to detect optional tkinter.
 * Disable demo programs.
 
---- setup.py.orig	2015-10-01 15:27:21.000000000 +0000
+--- setup.py.orig	2016-04-01 13:10:57.000000000 +0000
 +++ setup.py
-@@ -82,11 +82,7 @@ def _lib_include(root):
+@@ -80,12 +80,7 @@ def _lib_include(root):
  def _read(file):
      return open(file, 'rb').read()
  
+-
 -try:
 -    import _tkinter
 -except (ImportError, OSError):
@@ -16,13 +17,13 @@ $NetBSD: patch-setup.py,v 1.5 2015/10/18 15:47:10 adam Exp $
 -    _tkinter = None
 +_tkinter = None
  
- 
  NAME = 'Pillow'
-@@ -760,7 +756,6 @@ setup(
-     ext_modules=[Extension("PIL._imaging", ["_imaging.c"])],
-     include_package_data=True,
-     packages=find_packages(),
--    scripts=glob.glob("Scripts/*.py"),
-     test_suite='nose.collector',
-     keywords=["Imaging", ],
-     license='Standard PIL License',
+ PILLOW_VERSION = '3.2.0'
+@@ -760,7 +755,6 @@ setup(name=NAME,
+       ext_modules=[Extension("PIL._imaging", ["_imaging.c"])],
+       include_package_data=True,
+       packages=find_packages(),
+-      scripts=glob.glob("Scripts/*.py"),
+       test_suite='nose.collector',
+       keywords=["Imaging", ],
+       license='Standard PIL License',
