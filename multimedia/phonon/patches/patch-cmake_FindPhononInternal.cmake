f9f97c8caeb886c2ce1ad6530cd4466b5390d4e7
markd <markd@pkgsrc.org>
2016-04-26 08:08:50 +0000

    Update phonon{,-qt5} to 4.9.0
    Changes
    
    * Builsystem helpers are now installed to CMAKE_INSTALL_DATAROOTDIR
    * CMake 2.8.9 is required to use GNUInstallDirs and for the Qt5 code
      branches
    * automoc4 support was removed. Building always use the cmake built-in
      solution now.
    * Qt5 and Qt4 builds use different CMake configurations now
      * Qt4 is as it always has been
      * Qt5 moved away from crudely ported Qt4 configurations to using
        extra-cmake-modules' KDE compiler and cmake flags.
      * Installation paths and so forth are still jointly configured as to
        retain backwards compatibility (i.e. Qt5 build does not follow ECM's
        KDEInstallationPaths)
    
    Bug Fixes
    
    * VolumeSlider has seen async behavior improvements making the slider
      not hop around when changing the volume rapidly and the backend is
      lagging a bit behind. The volume change now occurs upon slider release
      rather than instantly.
    * Fixed a duble encoding issue with local paths that contain percent
      encoded characters being double-encoded
    
    New API
    
    * New AudioOutputInterface49 for backends to implement. This interface
      implements long-existing frontend interfaces for muting, giving the
      backend easier access and control.
      * setMuted(bool) mutes an AudioOuput (without the 4.9 interface this
        is done via setVolume(0.0) on the backend)
      * mutedChanged(bool) signal emitted by the backend to asyncronuously
        notify of the mute application
      * The interface is only used if PulseSupport is not intercepting calls
    * New methods to differntiate states of PulseSupport
      * request(bool) is used by backends to request PulseAudio usage but no
        interception, this essentially enables device listing but lets
        everything else fall through to the backend (the existing isActive()
        method will not return true after request(true), which makes it
        different from enable(true))
      * isRequested() is a getter for request()'s state
      * isUsable() is part of the previouys isActive() behavior, it is true
        iff pulseaudio can be used (daemon running, connected etc)
      * isUsed() is a combination of isRequested() and isUsable() (i.e. active
        but not intercepting)
      * The existing isActive() communicates the same state as before (active
        and intercepting) but now also takes requested into consideration
      * Enabling always implies requsting automatically, so request(false)
        and enable(true) will ultimately still restul in isRequested==true

--- multimedia/phonon/patches/patch-cmake_FindPhononInternal.cmake
+++ multimedia/phonon/patches/patch-cmake_FindPhononInternal.cmake
@@ -1,11 +1,10 @@
-$NetBSD: patch-cmake_FindPhononInternal.cmake,v 1.3 2016/03/01 08:54:42 markd Exp $
+$NetBSD: patch-cmake_FindPhononInternal.cmake,v 1.4 2016/04/26 08:08:50 markd Exp $
 
 Disable Darwin section, creates unusable libraries.
-_include_dirs fix for qt-5.4.2
 
---- cmake/FindPhononInternal.cmake.orig	2014-12-04 09:30:26.000000000 +0000
+--- cmake/FindPhononInternal.cmake.orig	2016-04-18 04:25:17.000000000 +0000
 +++ cmake/FindPhononInternal.cmake
-@@ -199,14 +199,14 @@ set(INSTALL_TARGETS_DEFAULT_ARGS RUNTIME
+@@ -92,14 +92,14 @@ set(INSTALL_TARGETS_DEFAULT_ARGS RUNTIME
                                   ARCHIVE DESTINATION "${LIB_INSTALL_DIR}" COMPONENT Devel)
  
  # on the Mac support an extra install directory for application bundles
@@ -20,34 +19,5 @@ _include_dirs fix for qt-5.4.2
 -endif(APPLE)
 +endif(notAPPLE)
  
- # RPATH Handling
- 
-@@ -324,16 +324,16 @@ if (CMAKE_COMPILER_IS_GNUCXX)
-    # Select flags.
-    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -DNDEBUG -DQT_NO_DEBUG")
-    set(CMAKE_CXX_FLAGS_RELEASE        "-O2 -DNDEBUG -DQT_NO_DEBUG")
--   set(CMAKE_CXX_FLAGS_DEBUG          "-g -O2 -fno-reorder-blocks -fno-schedule-insns -fno-inline")
-+   set(CMAKE_CXX_FLAGS_DEBUG          "-g -O2 -fno-inline")
-    set(CMAKE_CXX_FLAGS_DEBUGFULL      "-g3 -fno-inline")
-    set(CMAKE_CXX_FLAGS_PROFILE        "-g3 -fno-inline -ftest-coverage -fprofile-arcs")
-    set(CMAKE_C_FLAGS_RELWITHDEBINFO   "-O2 -g -DNDEBUG -DQT_NO_DEBUG")
-    set(CMAKE_C_FLAGS_RELEASE          "-O2 -DNDEBUG -DQT_NO_DEBUG")
--   set(CMAKE_C_FLAGS_DEBUG            "-g -O2 -fno-reorder-blocks -fno-schedule-insns -fno-inline")
-+   set(CMAKE_C_FLAGS_DEBUG            "-g -O2 -fno-inline")
-    set(CMAKE_C_FLAGS_DEBUGFULL        "-g3 -fno-inline")
-    set(CMAKE_C_FLAGS_PROFILE          "-g3 -fno-inline -ftest-coverage -fprofile-arcs")
- 
--   set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -Wno-long-long -std=iso9899:1990 -Wundef -Wcast-align -Werror-implicit-function-declaration -Wchar-subscripts -Wall -W -Wpointer-arith -Wwrite-strings -Wformat-security -Wmissing-format-attribute -fno-common")
-+   set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -Wno-long-long -Wundef -Wcast-align -Werror-implicit-function-declaration -Wchar-subscripts -Wall -W -Wpointer-arith -Wwrite-strings -Wformat-security -Wmissing-format-attribute -fno-common")
-    # As of Qt 4.6.x we need to override the new exception macros if we want compile with -fno-exceptions
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wnon-virtual-dtor -Wno-long-long -Wundef -Wcast-align -Wchar-subscripts -Wall -W -Wpointer-arith -Wformat-security -fno-exceptions -DQT_NO_EXCEPTIONS -fno-check-new -fno-common")
- 
-@@ -409,7 +409,7 @@ if (CMAKE_COMPILER_IS_GNUCXX)
-       file(WRITE "${_source_file}" "${_source}")
-       set(_include_dirs "-DINCLUDE_DIRECTORIES:STRING=${QT_INCLUDES}")
- 
--      try_compile(_compile_result ${CMAKE_BINARY_DIR} ${_source_file} CMAKE_FLAGS "${_include_dirs}" OUTPUT_VARIABLE _compile_output_var)
-+      try_compile(_compile_result ${CMAKE_BINARY_DIR} ${_source_file} CMAKE_FLAGS "${CMAKE_CXX_FLAGS}" COMPILE_DEFINITIONS "${_include_dirs}" OUTPUT_VARIABLE _compile_output_var)
- 
-       if(NOT _compile_result)
-          message("${_compile_output_var}")
+ if (NOT PHONON_BUILD_PHONON4QT5) # Qt4
+     include(${phonon_cmake_module_dir}/PhononQt4.cmake)
