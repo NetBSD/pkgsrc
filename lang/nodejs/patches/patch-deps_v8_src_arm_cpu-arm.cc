9d96ea73ad1fe68836b817851359d76a89a237ad
fhajny <fhajny@pkgsrc.org>
2016-04-27 15:03:10 +0000

    Update lang/nodejs to 6.0.0.
    This release will become the new LTS later in 2016.
    
    The following significant changes have been made since the
    previous Node.js v5.0.0 release.
    
    Buffer
    - New Buffer constructors have been added #4682 and #5833.
    - Existing Buffer() and SlowBuffer() constructors have been
      deprecated in docs #4682 and #5833.
    - Previously deprecated Buffer APIs are removed #5048, #4594.
    - Improved error handling #4514.
    - The Buffer.prototype.lastIndexOf() method has been added #4846.
    
    Cluster
    - Worker emitted as first argument in 'message' event #5361.
    - The worker.exitedAfterDisconnect property replaces
      worker.suicide #3743.
    
    Console
    - Calling console.timeEnd() with an unknown label now emits a
      process warning rather than throwing #5901.
    
    Crypto
    - Improved error handling #3100, #5611.
    - Simplified Certificate class bindings #5382.
    - Improved control over FIPS mode #5181.
    - pbkdf2 digest overloading is deprecated #4047.
    
    Dependencies
    - Reintroduce shared c-ares build support #5775.
    - V8 updated to 5.0.71.35 #6372.
    
    DNS
    - Add dns.resolvePtr() API to query plain DNS PTR records #4921.
    
    Domains
    - Clear stack when no error handler #4659.
    
    Events
    - The EventEmitter.prototype._events object no longer inherits
      from Object.prototype #6092.
    - The EventEmitter.prototype.prependListener() and
      EventEmitter.prototype.prependOnceListener() methods have been
      added #6032.
    
    File System
    - The fs.realpath() and fs.realpathSync() methods have been
      updated to use a more efficient libuv-based implementation. This
      change includes the removal of the cache argument and the method
      can throw new errors #3594.
    - FS apis can now accept and return paths as Buffers #5616.
    - Error handling and type checking improvements #5616, #5590,
      #4518, #3917.
    - fs.read's string interface is deprecated #4525.
    
    HTTP
    - 'clientError' can now be used to return custom errors from an
      HTTP server #4557.

--- lang/nodejs/patches/patch-deps_v8_src_arm_cpu-arm.cc
+++ lang/nodejs/patches/patch-deps_v8_src_arm_cpu-arm.cc
@@ -1,6 +1,6 @@
-$NetBSD: patch-deps_v8_src_arm_cpu-arm.cc,v 1.1 2015/10/21 23:46:28 jmcneill Exp $
+$NetBSD: patch-deps_v8_src_arm_cpu-arm.cc,v 1.2 2016/04/27 15:03:10 fhajny Exp $
 
---- deps/v8/src/arm/cpu-arm.cc.orig	2015-10-13 17:20:07.000000000 +0000
+--- deps/v8/src/arm/cpu-arm.cc.orig	2016-04-26 19:50:10.000000000 +0000
 +++ deps/v8/src/arm/cpu-arm.cc
 @@ -7,6 +7,9 @@
  #ifdef __QNXNTO__
@@ -12,14 +12,13 @@ $NetBSD: patch-deps_v8_src_arm_cpu-arm.cc,v 1.1 2015/10/21 23:46:28 jmcneill Exp
  #else
  #include <sys/syscall.h>  // for cache flushing.
  #endif
-@@ -40,6 +43,10 @@ void CpuFeatures::FlushICache(void* star
- #elif V8_OS_QNX
+@@ -24,6 +27,9 @@ void CpuFeatures::FlushICache(void* star
+ #if !defined(USE_SIMULATOR)
+ #if V8_OS_QNX
    msync(start, size, MS_SYNC | MS_INVALIDATE_ICACHE);
- 
 +#elif defined(__NetBSD__)
 +  struct arm_sync_icache_args args = { .addr = (uintptr_t)start, .len = size };
 +  sysarch(ARM_SYNC_ICACHE, (void *)&args);
-+
  #else
    register uint32_t beg asm("r0") = reinterpret_cast<uint32_t>(start);
    register uint32_t end asm("r1") = beg + size;
