1abba5dc7a75408734fc227911abe8124c926405
jperkin <jperkin@pkgsrc.org>
2016-04-28 22:18:58 +0000

    Fix SunOS cpu and network interface detection.  Bump PKGREVISION.

--- sysutils/libgtop/patches/patch-sysdeps_solaris_procopenfiles.c
+++ sysutils/libgtop/patches/patch-sysdeps_solaris_procopenfiles.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-sysdeps_solaris_procopenfiles.c,v 1.1 2013/02/26 15:54:16 jperkin Exp $
+$NetBSD: patch-sysdeps_solaris_procopenfiles.c,v 1.2 2016/04/28 22:18:58 jperkin Exp $
 
 Implement stub functions for Solaris to at least get the package to
 build.  Help filling them out with actual data would be very welcome.
@@ -16,7 +16,7 @@ build.  Help filling them out with actual data would be very welcome.
  #include <sys/types.h>
  #include <fcntl.h>
  #include <sys/stat.h>
-@@ -126,3 +129,46 @@ glibtop_get_proc_open_files_s (glibtop *
+@@ -126,3 +129,67 @@ glibtop_get_proc_open_files_s (glibtop *
  
  	return (glibtop_open_files_entry*)g_array_free(entries, FALSE);
  }
@@ -53,13 +53,34 @@ build.  Help filling them out with actual data would be very welcome.
 +	return NULL;
 +}
 +
++static const unsigned long _glibtop_sysdeps_sysinfo =
++(1L << GLIBTOP_SYSINFO_CPUINFO);
++
 +static glibtop_sysinfo sysinfo = { .flags = 0 };
++
++static void
++init_sysinfo (glibtop *server)
++{
++	guint ncpus = sysconf(_SC_NPROCESSORS_CONF);
++
++	glibtop_init_s (&server, GLIBTOP_SYSDEPS_CPU, 0);
++
++	for (sysinfo.ncpu = 0;
++	     sysinfo.ncpu < GLIBTOP_NCPU && sysinfo.ncpu < ncpus;
++	     sysinfo.ncpu++) {
++		glibtop_entry * const cpuinfo = &sysinfo.cpuinfo[sysinfo.ncpu];
++		cpuinfo->labels = g_ptr_array_new ();
++		cpuinfo->values = g_hash_table_new_full(g_str_hash,
++							g_str_equal,
++							NULL, g_free);
++	}
++
++	sysinfo.flags = _glibtop_sysdeps_sysinfo;
++}
 +
 +const glibtop_sysinfo *
 +glibtop_get_sysinfo_s (glibtop *server)
 +{
-+	server->sysdeps.flags =
-+	    (1L << GLIBTOP_SYSINFO_CPUINFO);
-+	glibtop_init_s (&server, GLIBTOP_SYSDEPS_CPU, 0);
-+	return NULL;
++	init_sysinfo (server);
++	return &sysinfo;
 +}
