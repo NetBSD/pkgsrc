72fd350cbf24e4206d9004d2f870783ffbf6fd73
prlw1 <prlw1@pkgsrc.org>
2016-04-29 15:33:57 +0000

    Update py-psutil to 4.1.0
    4.1.0 - 2016-03-12
    ==================
    
    **Enhancements**
    
    - #777: [Linux] Process.open_files() on Linux return 3 new fields: position,
      mode and flags.
    - #779: Process.cpu_times() returns two new fields, 'children_user' and
      'children_system' (always set to 0 on OSX and Windows).
    - #789: [Windows] psutil.cpu_times() return two new fields: "interrupt" and
      "dpc". Same for psutil.cpu_times_percent().
    - #792: new psutil.cpu_stats() function returning number of CPU ctx switches
      interrupts, soft interrupts and syscalls.
    
    **Bug fixes**
    
    - #774: [FreeBSD] net_io_counters() dropout is no longer set to 0 if the kernel
      provides it.
    - #776: [Linux] Process.cpu_affinity() may erroneously raise NoSuchProcess.
      (patch by wxwright)
    - #780: [OSX] psutil does not compile with some gcc versions.
    - #786: net_if_addrs() may report incomplete MAC addresses.
    - #788: [NetBSD] virtual_memory()'s buffers and shared values were set to 0.
    - #790: [OSX] psutil won't compile on OSX 10.4.
    
    
    4.0.0 - 2016-02-17
    ==================
    
    **Enhancements**
    
    - #523: [Linux, FreeBSD] disk_io_counters() return a new "busy_time" field.
    - #660: [Windows] make.bat is smarter in finding alternative VS install
      locations.  (patch by mpderbec)
    - #732: Process.environ().  (patch by Frank Benkstein)
    - #753: [Linux, OSX, Windows] Process USS and PSS (Linux) "real" memory stats.
      (patch by Eric Rahm)
    - #755: Process.memory_percent() "memtype" parameter.
    - #758: tests now live in psutil namespace.
    - #760: expose OS constants (psutil.LINUX, psutil.OSX, etc.)
    - #756: [Linux] disk_io_counters() return 2 new fields: read_merged_count and
      write_merged_count.
    - #762: new scripts/procsmem.py script.
    
    **Bug fixes**
    
    - #685: [Linux] virtual_memory() provides wrong results on systems with a lot
      of physical memory.
    - #704: [Solaris] psutil does not compile on Solaris sparc.
    - #734: on Python 3 invalid UTF-8 data is not correctly handled for process
      name(), cwd(), exe(), cmdline() and open_files() methods resulting in
      UnicodeDecodeError exceptions. 'surrogateescape' error handler is now
      used as a workaround for replacing the corrupted data.
    - #737: [Windows] when the bitness of psutil and the target process was
      different cmdline() and cwd() could return a wrong result or incorrectly
      report an AccessDenied error.
    - #741: [OpenBSD] psutil does not compile on mips64.
    - #751: [Linux] fixed call to Py_DECREF on possible Null object.
    - #754: [Linux] cmdline() can be wrong in case of zombie process.
    - #759: [Linux] Process.memory_maps() may return paths ending with " (deleted)"
    - #761: [Windows] psutil.boot_time() wraps to 0 after 49 days.
    - #764: [NetBSD] fix compilation on NetBSD-6.x.
    - #766: [Linux] net_connections() can't handle malformed /proc/net/unix file.
    - #767: [Linux] disk_io_counters() may raise ValueError on 2.6 kernels and it's
      broken on 2.4 kernels.
    - #770: [NetBSD] disk_io_counters() metrics didn't update.

--- sysutils/py-psutil/patches/patch-psutil___psutil__bsd.c
+++ sysutils/py-psutil/patches/patch-psutil___psutil__bsd.c
@@ -1,32 +1,29 @@
-$NetBSD: patch-psutil___psutil__bsd.c,v 1.5 2016/02/10 13:20:28 wiz Exp $
+$NetBSD: patch-psutil___psutil__bsd.c,v 1.6 2016/04/29 15:33:57 prlw1 Exp $
 
-1. chunk: Workaround for DTYPE_VNODE not being available in public
-headers, even with _KERNEL set, on NetBSD-6.
+MNT_RELATIME and MNT_EXTATTR are not available on NetBSD-5.
 
-2. chunk: MNT_DISCARD not available on NetBSD-6.
+https://github.com/giampaolo/psutil/pull/812
 
-https://github.com/giampaolo/psutil/pull/764
-
---- psutil/_psutil_bsd.c.orig	2016-01-20 16:26:28.000000000 +0000
+--- psutil/_psutil_bsd.c.orig	2016-03-12 17:12:23.000000000 +0000
 +++ psutil/_psutil_bsd.c
-@@ -100,6 +100,9 @@
- #define _KERNEL
-     #include <uvm/uvm_extern.h>
- #undef _KERNEL
-+    #ifndef DTYPE_VNODE
-+    #define DTYPE_VNODE 1
-+    #endif
- #endif
- 
- 
-@@ -703,8 +706,10 @@ psutil_disk_partitions(PyObject *self, P
+@@ -712,16 +712,20 @@ psutil_disk_partitions(PyObject *self, P
+             strlcat(opts, ",union", sizeof(opts));
+         if (flags & MNT_NOCOREDUMP)
+             strlcat(opts, ",nocoredump", sizeof(opts));
++#if defined(MNT_RELATIME)
+         if (flags & MNT_RELATIME)
              strlcat(opts, ",relatime", sizeof(opts));
++#endif
          if (flags & MNT_IGNORE)
              strlcat(opts, ",ignore", sizeof(opts));
-+#if defined(MNT_DISCARD)
+ #if defined(MNT_DISCARD)
          if (flags & MNT_DISCARD)
              strlcat(opts, ",discard", sizeof(opts));
-+#endif
+ #endif
++#if defined(MNT_EXTATTR)
          if (flags & MNT_EXTATTR)
              strlcat(opts, ",extattr", sizeof(opts));
++#endif
          if (flags & MNT_LOG)
+             strlcat(opts, ",log", sizeof(opts));
+         if (flags & MNT_SYMPERM)
