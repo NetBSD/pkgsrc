5e5759ea5cdb6247fe8e5dc0d0cdb25efca16171
wiz <wiz@pkgsrc.org>
2016-04-29 11:17:34 +0000

    Updated libdrm to 2.4.68.
    Chih-Wei Huang (1):
          android: add virgl to be a valid driver
    
    Daniel Vetter (7):
          headers: Remove _DRM_GEM
          tests/kms-steal-crtc: Use correct includes
          headers: Update drm_i915.h
          headers: Update drm_sarea.h
          headers: Update drm_fourcc.h
          headers: Update drm_mode.h
          headers: Update drm.h
    
    Emil Velikov (1):
          proptest: print signed values correctly
    
    Eric Engestrom (6):
          amdgpu: Fix spelling mistakes
          exynos: Fix spelling mistake
          freedreno: Fix spelling mistakes
          intel: Fix spelling mistakes
          tests: Fix spelling mistakes
          xf86drm: Fix spelling mistakes
    
    Gustavo Padovan (3):
          tests: add virtio_gpu to the driver list
          gitignore: ignore kms-steal-crtc and kms-universal-planes
          modetest: print signed values correctly
    
    Kenneth Graunke (1):
          Bump version for release
    
    Michał Winiarski (1):
          intel/skl: Add missing SKL PCI IDs
    
    Nicolas Dechesne (1):
          freedreno: add dummy fd_bo_from_fbdev implementation when KGSL is
    disabled
    
    Rob Clark (3):
          add libdrm_vc4.pc to .gitignore
          freedreno: update uapi
          freedreno: add support for FD_TIMESTAMP
    
    Rodrigo Vivi (1):
          intel: Adding missing Broxton PCI IDs.
    
    Sonny Jiang (1):
          amdgpu/tests: fix tests for asics with one VCE instance
    
    Thierry Reding (1):
          tegra: Sync with Linux kernel UAPI header
    
    Thomas Hellstrom (1):
          vmwgfx: update uapi

--- x11/libdrm/patches/patch-include_drm_drm.h
+++ x11/libdrm/patches/patch-include_drm_drm.h
@@ -1,13 +1,13 @@
-$NetBSD: patch-include_drm_drm.h,v 1.3 2015/01/03 10:01:40 wiz Exp $
+$NetBSD: patch-include_drm_drm.h,v 1.4 2016/04/29 11:17:34 wiz Exp $
 
 Fix compilation on systems that don't provide O_CLOEXEC.
 
---- include/drm/drm.h.orig	2014-06-14 12:33:26.000000000 +0000
+--- include/drm/drm.h.orig	2016-04-28 00:44:16.000000000 +0000
 +++ include/drm/drm.h
-@@ -641,7 +641,11 @@ struct drm_set_client_cap {
- 	__u64 value;
+@@ -674,7 +674,11 @@ struct drm_set_client_cap {
  };
  
+ #define DRM_RDWR O_RDWR
 +#ifdef O_CLOEXEC
  #define DRM_CLOEXEC O_CLOEXEC
 +#else
