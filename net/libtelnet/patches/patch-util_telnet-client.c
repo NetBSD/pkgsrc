e42e70afa767aa8bf96cb0d9e4406e7cd799a2cd
joerg <joerg@pkgsrc.org>
2016-04-30 20:18:38 +0000

    Fix portability issues due to overstrict namespace macros.

--- net/libtelnet/patches/patch-util_telnet-client.c
+++ net/libtelnet/patches/patch-util_telnet-client.c
@@ -1,10 +1,22 @@
-$NetBSD: patch-util_telnet-client.c,v 1.1 2016/04/11 11:32:22 fhajny Exp $
+$NetBSD: patch-util_telnet-client.c,v 1.2 2016/04/30 20:18:38 joerg Exp $
 
 Provide workaround for a non-POSIX cfmakeraw call.
+Uses getaddrinfo.
 
 --- util/telnet-client.c.orig	2012-03-18 21:07:49.000000000 +0000
 +++ util/telnet-client.c
-@@ -199,7 +199,15 @@ int main(int argc, char **argv) {
+@@ -9,6 +9,10 @@
+  * all present and future rights to this code under copyright law. 
+  */
+ 
++#ifdef __NetBSD__
++#define _NETBSD_SOURCE
++#endif
++#define _XOPEN_SOURCE 520
+ #if !defined(_POSIX_SOURCE)
+ #	define _POSIX_SOURCE
+ #endif
+@@ -199,7 +203,15 @@ int main(int argc, char **argv) {
  	tcgetattr(STDOUT_FILENO, &orig_tios);
  	atexit(_cleanup);
  	tios = orig_tios;
