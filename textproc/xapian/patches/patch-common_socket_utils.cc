5de3b197dae0d85b9bbac2595ad7e86ab6bf8306
schmonz <schmonz@pkgsrc.org>
2016-04-30 14:14:07 +0000

    Update to 1.2.23. From the changelog:
    API:
    
    * PostingSource: Public member variables are now wrapped by methods (mostly
      getters and/or setters, depending on whether they should be readable,
      writable or both).  In 1.3.5, the public members variables have been
      deprecated - we've added the replacement methods in 1.2.23 as well to make
      it easier for people to migrate over.
    
    chert backend:
    
    * xapian-check now performs additional consistency checks for chert. Reported
      by Jean-Francois Dockes and Bob Cargill via xapian-discuss.
    
    documentation:
    
    * Update links to Xapian website and trac to use https, which is now supported,
      thanks to James Aylett.
    
    portability:
    
    * On older Linux kernels, rename() of a file within a directory on NFS can
      sometimes erroneously fail with EXDEV.  This should only happen if you
      try to rename a file across filing systems, so workaround this issue by
      retrying up to 5 times on EXDEV (which should be plenty to avoid this
      bug, and we don't want to risk looping forever).  Fixes #698, reported by
      Mark Dufour.

--- textproc/xapian/patches/patch-common_socket_utils.cc
+++ textproc/xapian/patches/patch-common_socket_utils.cc
@@ -1,4 +1,6 @@
-$NetBSD: patch-common_socket_utils.cc,v 1.1 2015/05/26 12:19:38 jaapb Exp $
+$NetBSD: patch-common_socket_utils.cc,v 1.2 2016/04/30 14:14:07 schmonz Exp $
+
+Fix compilation error on NetBSD-current (as of 2015-05-26).
 
 --- common/socket_utils.cc.orig	2015-05-21 05:57:01.000000000 +0000
 +++ common/socket_utils.cc
