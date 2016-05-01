87afc9453b529d76d9ddc72b8f2c7947a438e45d
dholland <dholland@pkgsrc.org>
2016-05-01 19:35:09 +0000

    Add optional, default-off dependency on afterstep, and disable afterstep support if not enabled to avoid build failure if the configure script finds it. From Yakovetsky Vladimir in PR 36755.

--- x11/aterm/options.mk
+++ x11/aterm/options.mk
@@ -1,9 +1,16 @@
-# $NetBSD: options.mk,v 1.2 2005/10/05 13:29:49 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2016/05/01 19:35:09 dholland Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.aterm
-PKG_SUPPORTED_OPTIONS=	aterm-big5 aterm-greek aterm-kanji aterm-xterm-scroll
+PKG_SUPPORTED_OPTIONS=	aterm-big5 aterm-greek aterm-kanji aterm-xterm-scroll \
+			afterstep
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mafterstep)
+.include "../../wm/afterstep/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=--without-afterimage_config --without-afterstep_config
+.endif
 
 .if !empty(PKG_OPTIONS:Materm-big5)
 CONFIGURE_ARGS+=--enable-big5
