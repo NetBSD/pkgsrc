f444bd1655c9d756cf73f31698880e917683d018
dholland <dholland@pkgsrc.org>
2016-05-01 18:41:01 +0000

    Add bl3 file, to be used by x11/aterm (PR 36755)

---
+++ wm/afterstep/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/01 18:41:01 dholland Exp $
+
+BUILDLINK_TREE+=	afterstep
+
+.if !defined(AFTERSTEP_BUILDLINK3_MK)
+AFTERSTEP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.afterstep+=	afterstep>=2.2.11
+BUILDLINK_PKGSRCDIR.afterstep?=		../../wm/afterstep
+BUILDLINK_DEPMETHOD.afterstep?=		build
+
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXinerama/buildlink3.mk"
+#.include "../../x11/libXt/buildlink3.mk"
+
+.endif	# AFTERSTEP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-afterstep
