afc1895bedad3cab46196d36d5e9db71266633d8
taca <taca@pkgsrc.org>
2016-05-02 13:06:21 +0000

    Update php55 to 5.5.35.
    pkgsrc change: Fix build problem on Linux noted by Matthias Ferdinand on
    pkgsrc-users@.
    
    28 Apr 2016, PHP 5.5.35
    
    - BCMath:
      . Fix bug #72093 (bcpowmod accepts negative scale and corrupts _one_
        definition). (Stas)
    
    - Exif:
      . Fix bug #72094 (Out of bounds heap read access in exif header
        processing). (Stas)
    
    - GD:
      . Fix bug #71912 (libgd: signedness vulnerability). (Stas)
    
    - Intl:
      . Fix bug #72061 (Out-of-bounds reads in zif_grapheme_stripos with negative
        offset). (Stas)
    
    - XML:
      . Fix bug #72099 (xml_parse_into_struct segmentation fault). (Stas)

--- lang/php55/patches/patch-ext_standard_php__dns.h
+++ lang/php55/patches/patch-ext_standard_php__dns.h
@@ -1,15 +1,23 @@
-$NetBSD: patch-ext_standard_php__dns.h,v 1.1 2016/04/02 08:59:24 taca Exp $
+$NetBSD: patch-ext_standard_php__dns.h,v 1.2 2016/05/02 13:06:21 taca Exp $
 
 Fix memory leak.
 
---- ext/standard/php_dns.h.orig	2016-03-29 16:08:54.000000000 +0000
+--- ext/standard/php_dns.h.orig	2016-04-27 12:00:31.000000000 +0000
 +++ ext/standard/php_dns.h
-@@ -33,7 +33,7 @@
+@@ -32,9 +32,15 @@
+ #elif defined(HAVE_RES_NSEARCH)
  #define php_dns_search(res, dname, class, type, answer, anslen) \
  			res_nsearch(res, dname, class, type, answer, anslen);
++#ifdef __GLIBC__
  #define php_dns_free_handle(res) \
 -			res_nclose(res); \
++                        res_nclose(res); \
++                        php_dns_free_res(*res)
++#else
++#define php_dns_free_handle(res) \
 +			res_ndestroy(res); \
  			php_dns_free_res(*res)
++#endif
  
  #elif defined(HAVE_RES_SEARCH)
+ #define php_dns_search(res, dname, class, type, answer, anslen) \
