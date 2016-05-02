c1706ffab66587949cadb04776cd9d3984f43c8e
taca <taca@pkgsrc.org>
2016-05-02 13:08:00 +0000

    Update php56 to 5.6.21.
    pkgsrc change: Fix build problem on Linux noted by Matthias Ferdinand on
    pkgsrc-users@.
    
    28 Apr 2016, PHP 5.6.21
    
    - Core:
      . Fixed bug #69537 (__debugInfo with empty string for key gives error).
        (krakjoe)
      . Fixed bug #71841 (EG(error_zval) is not handled well). (Laruence)
    
    - BCmath:
      . Fixed bug #72093 (bcpowmod accepts negative scale and corrupts
        _one_ definition). (Stas)
    
    - Curl:
      . Fixed bug #71831 (CURLOPT_NOPROXY applied as long instead of string).
        (Michael Sierks)
    
    - Date:
      . Fixed bug #71889 (DateInterval::format Segmentation fault). (Thomas Punt)
    
    - EXIF:
      . Fixed bug #72094 (Out of bounds heap read access in exif header processing). (Stas)
    
    - GD:
      . Fixed bug #71952 (Corruption inside imageaffinematrixget). (Stas)
      . Fixed bug #71912 (libgd: signedness vulnerability). (Stas)
    
    - Intl:
      . Fixed bug #72061 (Out-of-bounds reads in zif_grapheme_stripos with negative
        offset). (Stas)
    
    - OCI8:
      . Fixed bug #71422 (Fix ORA-01438: value larger than specified precision
        allowed for this column). (Chris Jones)
    
    - ODBC:
      . Fixed bug #63171 (Script hangs after max_execution_time). (Remi)
    
    - Opcache:
      . Fixed bug #71843 (null ptr deref ZEND_RETURN_SPEC_CONST_HANDLER).
        (Laruence)
    
    - PDO:
      . Fixed bug #52098 (Own PDOStatement implementation ignore __call()).
        (Daniel Kalaspuffar, Julien)
      . Fixed bug #71447 (Quotes inside comments not properly handled). (Matteo)
    
    - Postgres:
      . Fixed bug #71820 (pg_fetch_object binds parameters before call
        constructor). (Anatol)
    
    - SPL:
      . Fixed bug #67582 (Cloned SplObjectStorage with overwritten getHash fails
        offsetExists()). (Nikita)
    
    - Standard:
      . Fixed bug #71840 (Unserialize accepts wrongly data). (Ryat, Laruence)
      . Fixed bug #67512 (php_crypt() crashes if crypt_r() does not exist or
        _REENTRANT is not defined). (Nikita)
    
    - XML:
      . Fixed bug #72099 (xml_parse_into_struct segmentation fault). (Stas)

--- lang/php56/patches/patch-ext_standard_php__dns.h
+++ lang/php56/patches/patch-ext_standard_php__dns.h
@@ -1,15 +1,23 @@
-$NetBSD: patch-ext_standard_php__dns.h,v 1.1 2016/04/02 09:00:25 taca Exp $
+$NetBSD: patch-ext_standard_php__dns.h,v 1.2 2016/05/02 13:08:00 taca Exp $
 
 Fix memory leak.
 
---- ext/standard/php_dns.h.orig	2016-03-30 23:35:38.000000000 +0000
+--- ext/standard/php_dns.h.orig	2016-04-28 00:33:49.000000000 +0000
 +++ ext/standard/php_dns.h
-@@ -33,7 +33,7 @@
+@@ -32,9 +32,15 @@
+ #elif defined(HAVE_RES_NSEARCH)
  #define php_dns_search(res, dname, class, type, answer, anslen) \
  			res_nsearch(res, dname, class, type, answer, anslen);
++#ifdef __GLIBC__
  #define php_dns_free_handle(res) \
 -			res_nclose(res); \
++                        res_nclose(res); \
++                        php_dns_free_res(*res)
++#else
++#define php_dns_free_handle(res) \
 +			res_ndestroy(res); \
  			php_dns_free_res(*res)
++#endif
  
  #elif defined(HAVE_RES_SEARCH)
+ #define php_dns_search(res, dname, class, type, answer, anslen) \
