080a1c0af4876d64bb06400c36be795f008dfb72
prlw1 <prlw1@pkgsrc.org>
2016-05-03 11:37:39 +0000

    Update libsoup to 2.54.1
    Note: ABI issue
    
    Changes in libsoup from 2.54.0.1 to 2.54.1:
    
            * *** IMPORTANT ***
              Fixed an ABI break in 2.54.0 caused by adding a member to
              SoupAuthClass; 2.54.1 is ABI-compatible with 2.53.92 and
              earlier, but NOT with the anomalous 2.54.0. If you built
              packages against 2.54.0, you will need to rebuild them
              against 2.54.1.
    
            * Fixed NTLM authentication when ntlm_auth from the latest
              version of Samba is present. [#765106, Milan Crha]
    
            * Updates to MSVC build, including for GSS-API support
              [Chun-wei Fan]
    
            * Updated translations:
              Friulian

--- net/libsoup/buildlink3.mk
+++ net/libsoup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2016/04/21 21:32:03 prlw1 Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2016/05/03 11:37:39 prlw1 Exp $
 
 BUILDLINK_TREE+=	libsoup
 
@@ -6,11 +6,21 @@ BUILDLINK_TREE+=	libsoup
 LIBSOUP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libsoup+=	libsoup>=2.50.0
+BUILDLINK_ABI_DEPENDS.libsoup+=	libsoup>=2.54.1
+
 BUILDLINK_PKGSRCDIR.libsoup?=	../../net/libsoup
+
+pkgbase := libsoup
+.include "../../mk/pkg-build-options.mk"
 
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.libsoup:Mgssapi)
+.include "../../mk/krb5.buildlink3.mk"
+.endif
+
 .endif	# LIBSOUP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libsoup
--- net/libsoup/options.mk
+++ net/libsoup/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2016/04/21 21:32:03 prlw1 Exp $
+# $NetBSD: options.mk,v 1.4 2016/05/03 11:37:39 prlw1 Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libsoup
 PKG_SUPPORTED_OPTIONS=	gssapi
@@ -9,7 +9,7 @@ PKG_SUGGESTED_OPTIONS=
 .if !empty(PKG_OPTIONS:Mgssapi)
 .  include "../../mk/krb5.buildlink3.mk"
 CONFIGURE_ARGS+=	--with-gssapi
-CONFIGURE_ARGS+=	--with-krb5-config=${SH_KRB5_CONFIG}
+CONFIGURE_ARGS+=	--with-krb5-config=${KRB5_CONFIG}
 .else
 CONFIGURE_ARGS+=	--without-gssapi
 .endif
