8ddbadd1065fe83f8f0b1e458f7237253072d333
jaapb <jaapb@pkgsrc.org>
2016-05-05 11:45:36 +0000

    Recursive revbump associated with ocaml update.

--- security/ocaml-cryptokit/buildlink3.mk
+++ security/ocaml-cryptokit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2015/08/07 11:20:32 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2016/05/05 11:45:36 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cryptokit
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-cryptokit
 OCAML_CRYPTOKIT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-cryptokit+=	ocaml-cryptokit>=1.5
-BUILDLINK_ABI_DEPENDS.ocaml-cryptokit?=	ocaml-cryptokit>=1.9nb3
+BUILDLINK_ABI_DEPENDS.ocaml-cryptokit?=	ocaml-cryptokit>=1.9nb4
 BUILDLINK_PKGSRCDIR.ocaml-cryptokit?=	../../security/ocaml-cryptokit
 
 .include "../../lang/ocaml/buildlink3.mk"
