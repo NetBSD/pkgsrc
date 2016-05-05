bd74aa57c263ac8f5863d7eb3a78aa018156b1c8
jaapb <jaapb@pkgsrc.org>
2016-05-05 10:28:21 +0000

    Added devel/ocamlbuild, a build system for OCaml. This was split off from the ocaml package starting with 4.03.0.

---
+++ devel/ocamlbuild/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/05 10:28:21 jaapb Exp $
+
+BUILDLINK_TREE+=	ocamlbuild
+
+.if !defined(OCAMLBUILD_BUILDLINK3_MK)
+OCAMLBUILD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocamlbuild+=	ocamlbuild>=0.9.1
+BUILDLINK_PKGSRCDIR.ocamlbuild?=	../../devel/ocamlbuild
+BUILDLINK_DEPMETHOD.ocamlbuild?=	build
+.endif	# OCAMLBUILD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocamlbuild
