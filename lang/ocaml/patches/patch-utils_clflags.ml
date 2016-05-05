2e859f23b1f65fc933230a4a725c65f98d1311f4
jaapb <jaapb@pkgsrc.org>
2016-05-05 08:12:01 +0000

    Updated package to latest version, 4.03.0. Also removed some obsolete and unneeded patches.
    The list of changes is a thousand lines long, so I'm not including it
    here: details can be found in the Changes file in the top directory of the
    package after extraction.

--- lang/ocaml/patches/patch-utils_clflags.ml
+++ lang/ocaml/patches/patch-utils_clflags.ml
@@ -1,16 +1,16 @@
-$NetBSD: patch-utils_clflags.ml,v 1.3 2015/12/29 23:34:53 dholland Exp $
+$NetBSD: patch-utils_clflags.ml,v 1.4 2016/05/05 08:12:01 jaapb Exp $
 
 pkgsrc_runtime flag
 
---- utils/clflags.ml.orig	2015-05-12 15:31:26.000000000 +0000
+--- utils/clflags.ml.orig	2016-04-01 12:53:41.000000000 +0000
 +++ utils/clflags.ml
-@@ -108,7 +108,8 @@ let std_include_dir () =
- let shared = ref false (* -shared *)
- let dlcode = ref true (* not -nodynlink *)
+@@ -148,7 +148,8 @@ let pic_code = ref (match Config.archite
+                      | "amd64" -> true
+                      | _       -> false)
  
 -let runtime_variant = ref "";;      (* -runtime-variant *)
 +let runtime_variant = ref ""        (* -runtime-variant *)
-+let pkgsrc_runtime = ref false;;     (* -pkgsrc-runtime *)
++let pkgsrc_runtime = ref false;;    (* -pkgsrc-runtime *)
  
  let keep_docs = ref false              (* -keep-docs *)
  let keep_locs = ref false              (* -keep-locs *)
