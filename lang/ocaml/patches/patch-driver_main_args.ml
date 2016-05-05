cf8071d759044f857dde71057b136c565c2844d1
jaapb <jaapb@pkgsrc.org>
2016-05-05 08:12:01 +0000

    Updated package to latest version, 4.03.0. Also removed some obsolete and unneeded patches.
    The list of changes is a thousand lines long, so I'm not including it
    here: details can be found in the Changes file in the top directory of the
    package after extraction.

--- lang/ocaml/patches/patch-driver_main_args.ml
+++ lang/ocaml/patches/patch-driver_main_args.ml
@@ -1,10 +1,10 @@
-$NetBSD: patch-driver_main_args.ml,v 1.3 2015/12/29 23:34:53 dholland Exp $
+$NetBSD: patch-driver_main_args.ml,v 1.4 2016/05/05 08:12:01 jaapb Exp $
 
 pkgsrc_runtime flag
 
---- driver/main_args.ml.orig	2015-05-12 15:31:26.000000000 +0000
+--- driver/main_args.ml.orig	2016-04-25 13:36:01.000000000 +0000
 +++ driver/main_args.ml
-@@ -244,6 +244,10 @@ let mk_pack_opt f =
+@@ -376,6 +376,10 @@ let mk_pack_opt f =
    "-pack", Arg.Unit f, " Package the given .cmx files into one .cmx"
  ;;
  
@@ -15,15 +15,15 @@ pkgsrc_runtime flag
  let mk_pp f =
    "-pp", Arg.String f, "<command>  Pipe sources through preprocessor <command>"
  ;;
-@@ -537,6 +541,7 @@ module type Compiler_options = sig
+@@ -767,6 +771,7 @@ module type Compiler_options = sig
    val _output_obj : unit -> unit
    val _output_complete_obj : unit -> unit
    val _pack : unit -> unit
 +  val _pkgsrc_runtime : unit -> unit
    val _pp : string -> unit
    val _principal : unit -> unit
-   val _rectypes : unit -> unit
-@@ -689,6 +694,7 @@ struct
+   val _no_principal : unit -> unit
+@@ -955,6 +960,7 @@ struct
      mk_output_obj F._output_obj;
      mk_output_complete_obj F._output_complete_obj;
      mk_pack_byt F._pack;
@@ -31,7 +31,7 @@ pkgsrc_runtime flag
      mk_pp F._pp;
      mk_ppx F._ppx;
      mk_principal F._principal;
-@@ -807,6 +813,7 @@ struct
+@@ -1108,6 +1114,7 @@ struct
      mk_output_complete_obj F._output_complete_obj;
      mk_p F._p;
      mk_pack_opt F._pack;
