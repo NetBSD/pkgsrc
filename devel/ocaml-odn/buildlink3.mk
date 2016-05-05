3fe6bc44cdc16a83af6333a0b806534fc246b31b
jaapb <jaapb>
2016-05-05 09:36:39 +0000

    Updated package to use findlib-pkginstall-fragment. Revbump was also needed because of ocaml upgrade.

--- devel/ocaml-odn/buildlink3.mk
+++ devel/ocaml-odn/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2015/08/07 11:20:32 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2016/05/05 09:36:39 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-odn
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-odn
 OCAML_ODN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-odn+=	ocaml-odn>=0.0.11
-BUILDLINK_ABI_DEPENDS.ocaml-odn?=	ocaml-odn>=0.0.11nb4
+BUILDLINK_ABI_DEPENDS.ocaml-odn?=	ocaml-odn>=0.0.11nb6
 BUILDLINK_PKGSRCDIR.ocaml-odn?=	../../devel/ocaml-odn
 
 .include "../../lang/ocaml/buildlink3.mk"
