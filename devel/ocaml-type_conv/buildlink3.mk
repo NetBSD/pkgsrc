90a04c01396e6532f4ff51e14cba457d9b474bc8
jaapb <jaapb>
2016-05-05 15:32:17 +0000

    Corrected error in buildlink file

--- devel/ocaml-type_conv/buildlink3.mk
+++ devel/ocaml-type_conv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2016/05/05 11:45:36 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2016/05/05 15:32:47 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-type_conv
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-type_conv
 OCAML_TYPE_CONV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-type_conv+=	ocaml-type_conv>=109.60.01
-BUILDLINK_ABI_DEPENDS.ocaml-type_conv?=	ocaml-type_conv>=113.00.02nb3
+BUILDLINK_ABI_DEPENDS.ocaml-type_conv?=	ocaml-type_conv>=113.00.02nb2
 BUILDLINK_PKGSRCDIR.ocaml-type_conv?=	../../devel/ocaml-type_conv
 
 .include "../../lang/ocaml/buildlink3.mk"
