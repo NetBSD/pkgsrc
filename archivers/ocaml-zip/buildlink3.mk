05de016d5b2fcde322b7ee29f41456d5a4372251
jaapb <jaapb@pkgsrc.org>
2016-05-05 11:27:47 +0000

    Revbump associated with ocaml update. Also added patches to make package compile with newest zlib/ocaml.

--- archivers/ocaml-zip/buildlink3.mk
+++ archivers/ocaml-zip/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/02/03 12:57:14 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/05/05 11:27:47 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-zip
 
 .if !defined(OCAML_ZIP_BUILDLINK3_MK)
 OCAML_ZIP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-zip+=	ocaml-zip>=1.05nb5
+BUILDLINK_API_DEPENDS.ocaml-zip+=	ocaml-zip>=1.05nb6
 BUILDLINK_PKGSRCDIR.ocaml-zip?=	../../archivers/ocaml-zip
 
 .include "../../devel/zlib/buildlink3.mk"
