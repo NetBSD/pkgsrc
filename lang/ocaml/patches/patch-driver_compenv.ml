c19dd1418d23dd750555da0e46a8e1d1e63ed826
jaapb <jaapb>
2016-05-05 08:12:01 +0000

    Updated package to latest version, 4.03.0. Also removed some obsolete and unneeded patches.
    The list of changes is a thousand lines long, so I'm not including it
    here: details can be found in the Changes file in the top directory of the
    package after extraction.

--- lang/ocaml/patches/patch-driver_compenv.ml
+++ lang/ocaml/patches/patch-driver_compenv.ml
@@ -1,14 +1,14 @@
-$NetBSD: patch-driver_compenv.ml,v 1.2 2015/12/29 23:34:53 dholland Exp $
+$NetBSD: patch-driver_compenv.ml,v 1.3 2016/05/05 08:12:01 jaapb Exp $
 
 pkgsrc_runtime flag
 
---- driver/compenv.ml.orig	2014-08-28 16:24:52.000000000 +0000
+--- driver/compenv.ml.orig	2016-04-01 12:53:41.000000000 +0000
 +++ driver/compenv.ml
-@@ -185,6 +185,7 @@ let read_OCAMLPARAM ppf position =
+@@ -217,6 +217,7 @@ let read_one_param ppf position name v =
  
-       | "pp" -> preprocessor := Some v
-       | "runtime-variant" -> runtime_variant := v
-+      | "pkgsrc-runtime" -> set "pkgsrc-runtime" [ pkgsrc_runtime ] v
-       | "cc" -> c_compiler := Some v
+   | "pp" -> preprocessor := Some v
+   | "runtime-variant" -> runtime_variant := v
++  | "pkgsrc-runtime" -> set "pkgsrc-runtime" [ pkgsrc_runtime ] v
+   | "cc" -> c_compiler := Some v
  
-       (* assembly sources *)
+   | "clambda-checks" -> set "clambda-checks" [ clambda_checks ] v
