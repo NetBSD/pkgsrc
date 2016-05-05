994103cf32cc6f8bac417701aa64703d38fb64cf
jaapb <jaapb@pkgsrc.org>
2016-05-05 16:06:51 +0000

    Updated package to version 0.4.6. Changes include:   * (0.4.6) Major changes:    * Create patch functions to mimic the new String.lowercase_ascii,      String.uppercase_ascii, String.capitalize_ascii and      String.uncapitalize_ascii found in OCaml 4.03.0    * Disable tests depending on not yet buildable units with OCaml 4.03.0 (e.g.      expect which depends on batteries, which doesn't yet compile)    * Minimal version of OCaml is now 3.12.1. (Debian Wheezy version)
      * (0.4.5) Major changes:
        * Build and install annotation files.
        * Use builtin bin_annot and annot tags.
        * Tag .mly files on the same basis as .ml and .mli files
          (required by menhir).
        * Remove 'program' constraint from C-dependencies.
          Currently, when a library has C-sources and e.g. an executable
          depends on that library, then changing the C-sources and running
          '-build' does not yield a rebuild of the library.
          By adding these dependencies (rather removing the constraint),
          it seems to work fine.
      * Minor changes:
        * Add tests tag if --enable-tests.
        * Remove ocaml 4.02 warnings.
        * Fix the automatic use of -syntax for OCamlVersion >= 3.12.1.
        * Use the right ocamlfind binary to get all supported packages.
          The name of the ocamlfind binary should be read from the configuration.
          This is especially useful when cross-compiling.
      * Features:
        * no_automatic_syntax (alpha): Disable the automatic inclusion
          of -syntax camlp4o for packages that matches the internal heuristic
          (if a dependency ends with a .syntax or is a well known syntax).
        * compiled_setup_ml (alpha): Fix a bug using multiple arguments
          to the configure script.
      Thanks to Christopher Zimmermann, Jerome Vouillon, Tomohiro Matsuyama
      and Christoph Höger for their pull requests and to help release this version.

--- devel/ocaml-oasis/buildlink3.mk
+++ devel/ocaml-oasis/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2015/08/07 11:20:32 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2016/05/05 16:06:51 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-oasis
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-oasis
 OCAML_OASIS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-oasis+=	ocaml-oasis>=0.4.4
-BUILDLINK_ABI_DEPENDS.ocaml-oasis?=	ocaml-oasis>=0.4.4nb4
+BUILDLINK_ABI_DEPENDS.ocaml-oasis?=	ocaml-oasis>=0.4.5nb1
 BUILDLINK_PKGSRCDIR.ocaml-oasis?=	../../devel/ocaml-oasis
 
 .include "../../lang/ocaml/buildlink3.mk"
