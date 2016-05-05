692b440101fe31ab7f4439cd1598bbdf5f0a27ec
jaapb <jaapb@pkgsrc.org>
2016-05-05 11:45:36 +0000

    Recursive revbump associated with ocaml update.

--- x11/ocaml-graphics/buildlink3.mk
+++ x11/ocaml-graphics/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2015/08/07 11:20:33 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2016/05/05 11:45:37 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-graphics
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-graphics
 OCAML_GRAPHICS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-graphics+=	ocaml-graphics>=3.08.2
-BUILDLINK_ABI_DEPENDS.ocaml-graphics+=	ocaml-graphics>=4.02.3nb4
+BUILDLINK_ABI_DEPENDS.ocaml-graphics+=	ocaml-graphics>=4.03.0nb5
 BUILDLINK_PKGSRCDIR.ocaml-graphics?=	../../x11/ocaml-graphics
 
 .include "../../lang/ocaml/buildlink3.mk"
