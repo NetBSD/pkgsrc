d94c6d790baaa5d8d69f9ab39188943098b7b288
jaapb <jaapb@pkgsrc.org>
2016-05-05 08:12:01 +0000

    Updated package to latest version, 4.03.0. Also removed some obsolete and unneeded patches.
    The list of changes is a thousand lines long, so I'm not including it
    here: details can be found in the Changes file in the top directory of the
    package after extraction.

--- lang/ocaml/patches/patch-driver_main.ml
+++ lang/ocaml/patches/patch-driver_main.ml
@@ -1,12 +1,12 @@
-$NetBSD: patch-driver_main.ml,v 1.2 2015/12/29 23:34:53 dholland Exp $
+$NetBSD: patch-driver_main.ml,v 1.3 2016/05/05 08:12:01 jaapb Exp $
 
 pkgsrc_runtime flag
 
---- driver/main.ml.orig	2014-08-28 16:24:52.000000000 +0000
+--- driver/main.ml.orig	2016-04-25 13:36:01.000000000 +0000
 +++ driver/main.ml
-@@ -112,6 +112,7 @@ module Options = Main_args.Make_bytecomp
-   let _principal = set principal
+@@ -129,6 +129,7 @@ module Options = Main_args.Make_bytecomp
    let _rectypes = set recursive_types
+   let _no_rectypes = unset recursive_types
    let _runtime_variant s = runtime_variant := s
 +  let _pkgsrc_runtime = set pkgsrc_runtime
    let _safe_string = unset unsafe_string
