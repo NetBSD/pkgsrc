027614bf941b69bb78209991f9ea0129a24a3246
jaapb <jaapb@pkgsrc.org>
2016-05-05 15:58:58 +0000

    Updated package to version 0.9.2. Changes include:
    OCamlbuild 0.9.2 is a release to support OCaml 4.03. Martin Neuhäußer
    contributed new flags for flambda-specific optimization options. We
    also extend the scope of the flags -opaque and -for-pack, to align
    with 4.03 best practices. Note that OCamlbuild should still work
    correctly under older OCaml releases.
    
    - 67: Add support for flambda's optimization parameters
      The flags are named according to the flambda options with two exceptions:
        optimization_rounds(int) for -rounds <int>
        optimize({2,3,classic}) for -O{2,3,classic}
      (Martin Neuhäußer)
    - 70 (pratial): fix hygiene complains about the build directory under
      Windows
      (Andreas Hauptmann, Gabriel Scherer)
    - 73: also pass predicates(foo) to ocamldep
      (Jérémie Dimino)
    - extend the scope of the -opaque flag under 4.03
      (Gabriel Scherer)
    - extend the scope of the -for-pack flag to ocamlc,
      as recommend by OCaml compiler maintainers in
      [5996](http://caml.inria.fr/mantis/view.php?id=5995#c13339)
      (Gabriel Scherer)

--- devel/ocamlbuild/buildlink3.mk
+++ devel/ocamlbuild/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/05/05 10:28:21 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/05/05 15:58:58 jaapb Exp $
 
 BUILDLINK_TREE+=	ocamlbuild
 
 .if !defined(OCAMLBUILD_BUILDLINK3_MK)
 OCAMLBUILD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocamlbuild+=	ocamlbuild>=0.9.1
+BUILDLINK_API_DEPENDS.ocamlbuild+=	ocamlbuild>=0.9.2
 BUILDLINK_PKGSRCDIR.ocamlbuild?=	../../devel/ocamlbuild
 BUILDLINK_DEPMETHOD.ocamlbuild?=	build
 .endif	# OCAMLBUILD_BUILDLINK3_MK
