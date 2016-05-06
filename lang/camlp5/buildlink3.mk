0853d901ca572ceb90aa7db59956bdc2a15f0091
jaapb <jaapb@pkgsrc.org>
2016-05-06 10:50:55 +0000

    Updated package to latest version, 6.16. Changes include: * [28 Apr 16] Fixed problems of compatibility with previous versions of   ocaml. * [23 Jan 16] Fixed bug on optional types (ocaml 4.02). * [15 Jan 16] Fixed bug on methods wrongly interpreted as vals. * [05 Jan 16] Updated for ocaml trunk 4.03.0   (git version be2a7e2f830911b331657b8cfa462f5219dfec5b) * [26 Nov 15] Updated again for ocaml trunk current version (4.03.0). * [23 Sep 15] Updated for ocaml trunk current version (4.03.0).

--- lang/camlp5/buildlink3.mk
+++ lang/camlp5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2016/05/05 11:45:36 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2016/05/06 10:50:55 jaapb Exp $
 #
 
 BUILDLINK_TREE+=	camlp5
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	camlp5
 CAMLP5_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.camlp5+=	camlp5>=5.01
-BUILDLINK_ABI_DEPENDS.camlp5+=	camlp5>=6.14nb1
+BUILDLINK_ABI_DEPENDS.camlp5+=	camlp5>=6.16
 BUILDLINK_PKGSRCDIR.camlp5?=	../../lang/camlp5
 
 .include "../../lang/ocaml/buildlink3.mk"
