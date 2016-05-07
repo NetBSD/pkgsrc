dd94bf8c90158c1eb753437d37684d8af626c03a
bsiegert <bsiegert@pkgsrc.org>
2016-05-07 19:25:21 +0000

    Pullup ticket #4969 - requested by joerg x11/qt4-libs: bugfix
    Revisions pulled up:
    - x11/qt4-libs/Makefile                                         1.111
    - x11/qt4-libs/distinfo                                         1.107
    - x11/qt4-libs/patches/patch-src_corelib_thread_qthread__unix.cpp 1.2
    
    ---
       Module Name:	pkgsrc
       Committed By:	joerg
       Date:		Thu May  5 22:04:34 UTC 2016
    
       Modified Files:
       	pkgsrc/x11/qt4-libs: Makefile distinfo
       	pkgsrc/x11/qt4-libs/patches: patch-src_corelib_thread_qthread__unix.cpp
    
       Log Message:
       Fix TLS condition on NetBSD to correctly avoid using pthread_specific.
       Bump revision.

--- x11/qt4-libs/patches/patch-src_corelib_thread_qthread__unix.cpp
+++ x11/qt4-libs/patches/patch-src_corelib_thread_qthread__unix.cpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_corelib_thread_qthread__unix.cpp,v 1.1 2015/06/25 08:02:25 joerg Exp $
+$NetBSD: patch-src_corelib_thread_qthread__unix.cpp,v 1.1.8.1 2016/05/07 19:25:22 bsiegert Exp $
 
 --- src/corelib/thread/qthread_unix.cpp.orig	2015-06-23 15:32:25.000000000 +0000
 +++ src/corelib/thread/qthread_unix.cpp
@@ -8,7 +8,7 @@ $NetBSD: patch-src_corelib_thread_qthread__unix.cpp,v 1.1 2015/06/25 08:02:25 jo
  #endif
 +#if defined(Q_OS_NETBSD)
 +#include <machine/types.h>
-+#if defined(__HAVE_TLS)
++#if defined(__HAVE_TLS_VARIANT_I) || defined(__HAVE_TLS_VARIANT_II)
 +#define HAVE_TLS
 +#endif
 +#endif
