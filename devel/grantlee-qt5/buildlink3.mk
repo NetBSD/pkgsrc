ceb6e6556ee4b0787e2851d00a1d854422e10556
markd <markd@pkgsrc.org>
2016-05-08 11:21:40 +0000

    Add grantlee-qt5 5.1.0
    String template engine in QT5 based on Django template system

---
+++ devel/grantlee-qt5/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/08 11:21:40 markd Exp $
+
+BUILDLINK_TREE+=	grantlee-qt5
+
+.if !defined(GRANTLEE_QT5_BUILDLINK3_MK)
+GRANTLEE_QT5_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.grantlee-qt5+=	grantlee-qt5>=5.1.0
+BUILDLINK_PKGSRCDIR.grantlee-qt5?=	../../devel/grantlee-qt5
+
+.include "../../x11/qt5-qtscript/buildlink3.mk"
+.endif	# GRANTLEE_QT5_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-grantlee-qt5
