a9291acbdaab344dddae70505a078568b8cb433a
nonaka <nonaka@pkgsrc.org>
2016-05-08 11:23:15 +0000

    Updated smartmontools to 6.5.
    Date 2016-05-07
    Summary: smartmontools release 6.5
    -----------------------------------------------------------
    - Experimental support for NVMe devices on FreeBSD, Linux and Windows.
    - smartctl '-i', '-c', '-H' and '-l error': NVMe support.
    - smartctl '-l nvmelog': New option for NVMe.
    - smartd.conf '-H', '-l error' and '-W': NVMe support.
    - Optional NVMe device scanning support on Linux and Windows.
    - configure option '--with-nvme-devicescan' to include NVMe in
      default device scanning result.
    - Device scanning now allows to specify multiple '-d TYPE' options.
    - ATA: Added new POWER MODE values introduced in ATA ACS-2.
    - ATA: SCT commands are no longer issued if ATA Security is locked.
    - SCSI: LB provisioning improvements.
    - SCSI: Fixed GLTSD bit set/cleared info messages.
    - SCSI: Solid State media log page is no longer checked for tapes.
    - SCSI: Improved handling when no tape cartridge in drive.
    - SCSI: Workaround for buggy Seagate firmware.
    - SAT: Improved heuristics to detect bogus sense data from SAT layer.
    - smartd: Fixed crash on missing argument to '-s' directive.
    - update-smart-drivedb: Now uses HTTPS for download by default.
    - update-smart-drivedb: New options to select URL and download tool.
    - update-smart-drivedb: New download tool 'svn'.
    - configure option '--without-update-smart-drivedb' to disable
      update-smart-drivedb script.
    - configure options '--disable-drivedb', '--enable-savestates',
      '--enable-attributelog' and '--with-docdir' are no longer supported.
    - autoconf < 2.60 and automake < 1.10 are no longer supported.
    - Drive database file now also includes the DEFAULT setting
      for each attribute.
    - HDD, SSD and USB additions to drive database.
    - Darwin: New support files for package installer.
      New makefile target 'install-darwin' builds DMG image.
    - Solaris: Auto detection of SATA devices behind SAT layer.
    - Solaris SPARC: Legacy ATA support disabled by default.
      New configure option '--with-solaris-sparc-ata' enables it.
      File os_solaris_ata.s is no longer included in source tarball.
    - Windows: Auto detection of USB devices specified by drive letter.
    - Windows: Device scanning does no longer ignore unknown USB devices.
    - Windows: Prevent drive spin up by '-n standby' check.
    - Windows: New application manifests indicating Win 10 support.
    - Windows smartd: '-m [sys]msgbox' is no longer supported.
    - Windows installer: Defaults to 64-bit version on 64-bit Windows.
    - Various code changes suggested by Clang Static Analyser and Cppcheck.

--- sysutils/smartmontools/patches/patch-os__netbsd.cpp
+++ sysutils/smartmontools/patches/patch-os__netbsd.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-os__netbsd.cpp,v 1.2 2015/04/08 01:43:33 rodent Exp $
+$NetBSD: patch-os__netbsd.cpp,v 1.3 2016/05/08 11:23:15 nonaka Exp $
 
 Use a raw disk device file on NetBSD.
 
---- os_netbsd.cpp.orig	2013-03-29 20:17:03.000000000 +0000
-+++ os_netbsd.cpp
-@@ -57,7 +57,7 @@ printwarning(int msgNo, const char *extr
+--- os_netbsd.cpp.orig	2016-03-26 19:47:47.000000000 +0000
++++ os_netbsd.cpp	2016-05-08 06:45:25.000000000 +0000
+@@ -54,7 +54,7 @@ printwarning(int msgNo, const char *extr
    return;
  }
  
@@ -13,3 +13,12 @@ Use a raw disk device file on NetBSD.
  static const char *net_dev_ata_disk = "wd";
  static const char *net_dev_scsi_disk = "sd";
  static const char *net_dev_scsi_tape = "enrst";
+@@ -128,7 +128,7 @@ get_dev_names(char ***names, const char 
+     n++;
+   }
+ 
+-  void * tmp = (char **)realloc(mp, n * (sizeof(char *)));
++  char ** tmp = (char **)realloc(mp, n * (sizeof(char *)));
+   if (NULL == tmp) {
+     pout("Out of memory constructing scan device list\n");
+     free(mp);
