1b35edf2e1b42640251d7ca828f563b410f36116
bsiegert <bsiegert@pkgsrc.org>
2016-05-08 16:54:10 +0000

    Pullup ticket #4977 - requested by joerg www/aws-demos: build fix
    Revisions pulled up:
    - www/aws-demos/Makefile                                        1.16
    - www/aws/buildlink3.mk                                         1.2
    
    ---
       Module Name:	pkgsrc
       Committed By:	joerg
       Date:		Sat May  7 03:49:21 UTC 2016
    
       Modified Files:
       	pkgsrc/www/aws: buildlink3.mk
       	pkgsrc/www/aws-demos: Makefile
    
       Log Message:
       Use build option framework to not randomly vomit on the console.

--- www/aws/buildlink3.mk
+++ www/aws/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2011/03/02 10:14:31 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.1.42.1 2016/05/08 16:54:10 bsiegert Exp $
 
 BUILDLINK_TREE+=	aws
 
@@ -7,6 +7,9 @@ AWS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.aws+=	aws>=2.10.0.1
 BUILDLINK_PKGSRCDIR.aws?=	../../www/aws
+
+pkgbase := aws
+.include "../../mk/pkg-build-options.mk"
 
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../devel/gnatpython/buildlink3.mk"
