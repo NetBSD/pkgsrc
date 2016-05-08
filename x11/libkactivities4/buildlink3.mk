c234c117f0c5b1c6ed76dc57d3ae07875d76073a
markd <markd@pkgsrc.org>
2016-05-08 10:44:02 +0000

    Split libraries into separate package.

---
+++ x11/libkactivities4/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/08 10:44:02 markd Exp $
+
+BUILDLINK_TREE+=	libkactivities4
+
+.if !defined(LIBKACTIVITIES4_BUILDLINK3_MK)
+LIBKACTIVITIES4_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libkactivities4+=	libkactivities4>=4.7.95
+BUILDLINK_ABI_DEPENDS.libkactivities4+=	libkactivities4>=4.13.3
+BUILDLINK_PKGSRCDIR.libkactivities4?=	../../x11/libkactivities4
+
+.include "../../x11/kdelibs4/buildlink3.mk"
+.endif # LIBKACTIVITIES4_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libkactivities4
