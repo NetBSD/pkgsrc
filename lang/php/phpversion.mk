a7db1491b8e2789fafee5ede5137869312bf9694
bsiegert <bsiegert>
2016-05-08 14:08:45 +0000

    Pullup ticket #4973 - requested by taca lang/php56: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.134
    - lang/php56/distinfo                                           1.26
    - lang/php56/patches/patch-configure                            1.3
    - lang/php56/patches/patch-ext_opcache_config.m4                deleted
    - lang/php56/patches/patch-ext_standard_php__dns.h              1.2
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Mon May  2 13:08:00 UTC 2016
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php56: distinfo
       	pkgsrc/lang/php56/patches: patch-configure
       	    patch-ext_standard_php__dns.h
       Removed Files:
       	pkgsrc/lang/php56/patches: patch-ext_opcache_config.m4
    
       Log Message:
       Update php56 to 5.6.21.
    
       pkgsrc change: Fix build problem on Linux noted by Matthias Ferdinand on
       pkgsrc-users@.
    
       28 Apr 2016, PHP 5.6.21
    
       - Core:
        . Fixed bug #69537 (__debugInfo with empty string for key gives error).
          (krakjoe)
        . Fixed bug #71841 (EG(error_zval) is not handled well). (Laruence)
    
       - BCmath:
        . Fixed bug #72093 (bcpowmod accepts negative scale and corrupts
          _one_ definition). (Stas)
    
       - Curl:
        . Fixed bug #71831 (CURLOPT_NOPROXY applied as long instead of string).
          (Michael Sierks)
    
       - Date:
        . Fixed bug #71889 (DateInterval::format Segmentation fault). (Thomas Punt)
    
       - EXIF:
        . Fixed bug #72094 (Out of bounds heap read access in exif header processing). (Stas)
    
       - GD:
        . Fixed bug #71952 (Corruption inside imageaffinematrixget). (Stas)
        . Fixed bug #71912 (libgd: signedness vulnerability). (Stas)
    
       - Intl:
        . Fixed bug #72061 (Out-of-bounds reads in zif_grapheme_stripos with negative
          offset). (Stas)
    
       - OCI8:
        . Fixed bug #71422 (Fix ORA-01438: value larger than specified precision
          allowed for this column). (Chris Jones)
    
       - ODBC:
        . Fixed bug #63171 (Script hangs after max_execution_time). (Remi)
    
       - Opcache:
        . Fixed bug #71843 (null ptr deref ZEND_RETURN_SPEC_CONST_HANDLER).
          (Laruence)
    
       - PDO:
        . Fixed bug #52098 (Own PDOStatement implementation ignore __call()).
          (Daniel Kalaspuffar, Julien)
        . Fixed bug #71447 (Quotes inside comments not properly handled). (Matteo)
    
       - Postgres:
        . Fixed bug #71820 (pg_fetch_object binds parameters before call
          constructor). (Anatol)
    
       - SPL:
        . Fixed bug #67582 (Cloned SplObjectStorage with overwritten getHash fails
          offsetExists()). (Nikita)
    
       - Standard:
        . Fixed bug #71840 (Unserialize accepts wrongly data). (Ryat, Laruence)
        . Fixed bug #67512 (php_crypt() crashes if crypt_r() does not exist or
          _REENTRANT is not defined). (Nikita)
    
       - XML:
        . Fixed bug #72099 (xml_parse_into_struct segmentation fault). (Stas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.131.2.1 2016/05/08 08:10:06 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.131.2.2 2016/05/08 14:08:45 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -82,7 +82,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP55_VERSION=	5.5.35
-PHP56_VERSION=	5.6.20
+PHP56_VERSION=	5.6.21
 PHP70_VERSION=	7.0.5
 
 # Define initial release of major version.
