db35822a0fe34585ee813b33a73fbda604318cff
fhajny <fhajny@pkgsrc.org>
2016-05-09 12:39:05 +0000

    Update misc/tmux to 2.2. Use release tarballs instead of tagged archives, simplifies patching and rids us of automake dep.
    CHANGES FROM 2.1 to 2.2 10 April 2016
    
    Incompatible Changes
    ====================
    
    * The format strings which referenced time have been removed.
      Instead:
    
      #{t:window_activity}
    
    can be used.
    
    * Support for TMPDIR has been removed.  Use TMUX_TMPDIR instead.
      UTF8 detection how happens automatically if the client supports
      it, hence the:
    
      mouse-utf8
      utf8
    
      options has been removed.
    
    * The:
    
      mouse_utf8_flag
    
      format string has been removed.
    
    * The -I option to show-messages has been removed.  See:
    
      #{t:start_time}
    
      format option instead.
    
    Normal Changes
    ==============
    
    * Panes are unzoomed with selectp -LRUD
    * New formats added:
    
      #{scroll_position}
      #{socket_path}
      #{=10:...} -- limit to N characters (from the start)
      #{=-10:...} -- limit to N characters (from the end)
      #{t:...} -- used to format time-based formats
      #{b:...} -- used to ascertain basename from string
      #{d:...} -- used to ascertain dirname from string
      #{s:...} -- used to perform substitutions on a string
    
    * Job output is run via the format system, so formats work again
    * If display-time is set to 0, then the indicators wait for a key
      to be pressed.
    * list-keys and list-commands can be run without starting the tmux
      server.
    * kill-session learns -C to clear all alerts in all windows of the
      session.
    * Support for hooks (internal for now), but hooks for the
      following have been implemented:
    
      alert-bell
      alert-silence
      alert-activity
      client-attached
      client-detached
      client-resized
      pane-died
      pane-exited
    
    * RGB (24bit) colour support.  The 'Tc' flag must be set in the
      external TERM entry (using terminal-overrides or a custom terminfo entry).

--- misc/tmux/patches/patch-server-client.c
+++ misc/tmux/patches/patch-server-client.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-server-client.c,v 1.3 2015/12/31 07:07:16 ryoon Exp $
+$NetBSD: patch-server-client.c,v 1.4 2016/05/09 12:39:05 fhajny Exp $
 
 * SunOS errno support needs the right include.
 * timersub is missing on Solaris10
 
---- server-client.c.orig	2015-10-18 17:10:43.000000000 +0000
+--- server-client.c.orig	2016-04-04 00:16:53.000000000 +0000
 +++ server-client.c
-@@ -29,6 +29,18 @@
+@@ -30,6 +30,18 @@
  
  #include "tmux.h"
  
@@ -21,6 +21,6 @@ $NetBSD: patch-server-client.c,v 1.3 2015/12/31 07:07:16 ryoon Exp $
 +    } while (0)
 +#endif
 +
- void	server_client_key_table(struct client *, const char *);
- void	server_client_free(int, short, void *);
- void	server_client_check_focus(struct window_pane *);
+ void		server_client_free(int, short, void *);
+ void		server_client_check_focus(struct window_pane *);
+ void		server_client_check_resize(struct window_pane *);
