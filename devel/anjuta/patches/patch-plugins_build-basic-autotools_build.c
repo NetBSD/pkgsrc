ee49b410d1edcc3a3c824acf7751ec7ccd548980
bsiegert <bsiegert@pkgsrc.org>
2016-05-11 15:33:29 +0000

    Pullup ticket #4983 - requested by joerg devel/anjuta: build fix
    Revisions pulled up:
    - devel/anjuta/Makefile                                         1.125
    - devel/anjuta/distinfo                                         1.28
    - devel/anjuta/patches/patch-plugins_build-basic-autotools_build.c 1.3
    - devel/anjuta/patches/patch-plugins_cvs-plugin_cvs-callbacks.c 1.1
    - devel/anjuta/patches/patch-plugins_cvs-plugin_cvs-execute.c   1.1
    
    ---
       Module Name:	pkgsrc
       Committed By:	joerg
       Date:		Sat May  7 09:55:21 UTC 2016
    
       Modified Files:
       	pkgsrc/devel/anjuta: Makefile distinfo
       	pkgsrc/devel/anjuta/patches:
       	    patch-plugins_build-basic-autotools_build.c
       Added Files:
       	pkgsrc/devel/anjuta/patches: patch-plugins_cvs-plugin_cvs-callbacks.c
       	    patch-plugins_cvs-plugin_cvs-execute.c
    
       Log Message:
       Drop extra format string arguments. Disable zero length format string
       warning from GCC.

--- devel/anjuta/patches/patch-plugins_build-basic-autotools_build.c
+++ devel/anjuta/patches/patch-plugins_build-basic-autotools_build.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-plugins_build-basic-autotools_build.c,v 1.2 2013/12/07 23:47:02 dholland Exp $
+$NetBSD: patch-plugins_build-basic-autotools_build.c,v 1.2.20.1 2016/05/11 15:33:29 bsiegert Exp $
 
 --- plugins/build-basic-autotools/build.c.orig	2013-08-09 00:30:17.000000000 +0000
 +++ plugins/build-basic-autotools/build.c
@@ -11,3 +11,12 @@ $NetBSD: patch-plugins_build-basic-autotools_build.c,v 1.2 2013/12/07 23:47:02 d
  {
  	va_list args;
  	gchar *str;
+@@ -524,7 +524,7 @@ build_save_distclean_and_execute_command
+ 
+ 		// Need to run make clean before
+ 		if (!anjuta_util_dialog_boolean_question (GTK_WINDOW (ANJUTA_PLUGIN (bplugin)->shell), FALSE,
+-		    _("Before using this new configuration, the default one needs to be removed. Do you want to do that ?"), NULL))
++		    _("Before using this new configuration, the default one needs to be removed. Do you want to do that ?")))
+ 		{
+ 			if (err)
+ 				*err = g_error_new (ianjuta_builder_error_quark (),
