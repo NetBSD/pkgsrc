deb143db619764de002c4f815d5f20bbbb12e254
adam <adam@pkgsrc.org>
2016-05-13 20:47:32 +0000

    Changes 1.61.0: New Libraries: -------------- Compute:   Parallel/GPU-computing library DLL:   Library for comfortable work with DLL and DSO. Library provides a portable across platforms way to: * load libraries * import any native functions and variables * make alias names for C++ mangled functions and symbols * query libraries for sections and exported symbols * self loading and self querying * getting program and module location by exported symbol Hana:   A modern C++ metaprogramming library. It provides high level algorithms to manipulate heterogeneous sequences, allows writing type-level computations with a natural syntax, provides tools to introspect user-defined types and much more.   Metaparse: A library for generating compile time parsers parsing embedded DSL code as part of the C++ compilation process. The library is similar to Spirit, however while parsers built with Spirit parse at run-time, parsers built with Metaparse parse at compile-time.

--- meta-pkgs/boost/patches/patch-boost_config_stdlib_libcpp.hpp
+++ meta-pkgs/boost/patches/patch-boost_config_stdlib_libcpp.hpp
@@ -1,11 +1,11 @@
-$NetBSD: patch-boost_config_stdlib_libcpp.hpp,v 1.2 2015/08/14 07:54:04 adam Exp $
+$NetBSD: patch-boost_config_stdlib_libcpp.hpp,v 1.3 2016/05/13 20:47:32 adam Exp $
 
---- boost/config/stdlib/libcpp.hpp.orig	2015-08-04 11:44:49.000000000 +0000
+--- boost/config/stdlib/libcpp.hpp.orig	2016-05-05 21:11:02.000000000 +0000
 +++ boost/config/stdlib/libcpp.hpp
-@@ -66,6 +66,8 @@
- 
+@@ -74,6 +74,8 @@
  // libc++ uses a non-standard messages_base
  #define BOOST_NO_STD_MESSAGES
+ #endif
 +// libc++ uses inline namespaces
 +#define BOOST_DETAIL_NO_CONTAINER_FWD
  
