78db8da83eb7ceeff7f11ba7babd36fb1dd1ceea
adam <adam@pkgsrc.org>
2016-05-13 20:47:32 +0000

    Changes 1.61.0: New Libraries: -------------- Compute:   Parallel/GPU-computing library DLL:   Library for comfortable work with DLL and DSO. Library provides a portable across platforms way to: * load libraries * import any native functions and variables * make alias names for C++ mangled functions and symbols * query libraries for sections and exported symbols * self loading and self querying * getting program and module location by exported symbol Hana:   A modern C++ metaprogramming library. It provides high level algorithms to manipulate heterogeneous sequences, allows writing type-level computations with a natural syntax, provides tools to introspect user-defined types and much more.   Metaparse: A library for generating compile time parsers parsing embedded DSL code as part of the C++ compilation process. The library is similar to Spirit, however while parsers built with Spirit parse at run-time, parsers built with Metaparse parse at compile-time.

--- devel/boost-jam/buildlink3.mk
+++ devel/boost-jam/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.27 2015/12/27 12:41:42 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2016/05/13 20:47:32 adam Exp $
 
 BUILDLINK_TREE+=	boost-jam
 
 .if !defined(BOOST_JAM_BUILDLINK3_MK)
 BOOST_JAM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.60.*
+BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.61.*
 BUILDLINK_DEPMETHOD.boost-jam?=		build
 BUILDLINK_PKGSRCDIR.boost-jam?=		../../devel/boost-jam
 .endif # BOOST_JAM_BUILDLINK3_MK
--- devel/py-boost/buildlink3.mk
+++ devel/py-boost/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2015/12/27 12:41:42 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/05/13 20:47:32 adam Exp $
 
 BUILDLINK_TREE+=	py-boost
 
@@ -8,8 +8,8 @@ PY_BOOST_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 # Use a dependency pattern that guarantees the proper ABI.
-BUILDLINK_API_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.60.*
-BUILDLINK_ABI_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.60.*
+BUILDLINK_API_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.61.*
+BUILDLINK_ABI_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.61.*
 BUILDLINK_PKGSRCDIR.py-boost?=	../../devel/py-boost
 
 .include "../../devel/boost-headers/buildlink3.mk"
