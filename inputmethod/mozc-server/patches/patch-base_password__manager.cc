0b5797a4a2e52549d2262d9b5b5332f18ffcfa92
ryoon <ryoon@pkgsrc.org>
2016-05-16 11:51:49 +0000

    Update to 2.17.2313.102
    Changelog:
    2.17.2288.102 - 2.17.2313.102 / 2016-01-03 - 2016-01-10
    
    Summary of changes between 2.17.2288.102 and 2.17.2313.102 as follows.
    
        Third party libraries:
            None.
        Build related changes:
            None.
        Major changes:
            Update system dictionary.
            Status icons for OS X are updated with Noto font.
        Fixed issues:
            #344: Support icon_prop_key entry in ibus-mozc
            #345: Mozc for Android keeps crashing
            #347: Software keyboard is not rendered correctly on Android 6 if non-material theme is selected
            #350: Status icon is not updated when using Windows Store Apps in desktop mode on Windows 10
            #351: Mozc cannot be activated in the search box on the task bar when configured to be the default IME on Windows 10
            Following issues are not completely fixed yet, but at least worked around.
                #348: DirectWrite may fail to render text in certain enviromnents
                #349: Word suggestion can be unexpectedly suppressed on Chromium
        Total commits:
            29 commits.
    
    
    2.17.2285.102 - 2.17.2287.102 / 2016-01-01 - 2016-01-02
    
    Summary of changes between 2.17.2285.102 and 2.17.2287.102 as follows.
    
        Third party libraries:
            None.
        Build related changes:
            None.
        Major changes:
            None.
        Fixed issues:
            None.
        Total commits:
            4 commits.
    
    
    2.17.2241.102 - 2.17.2284.102 / 2015-11-15 - 2015-12-31
    
    Summary of changes between 2.17.2241.102 and 2.17.2284.102 as follows.
    
        Third party libraries:
            fontTools: 5ba7d98 -> 8724513
        Build related changes:
            Reference Dockerfile for Fedora now uses Fedora 23 base image.
            Default SDKROOT for OS X build is switched from macosx10.8 to macosx10.9.
        Major changes:
            CalculatorRewriter is now triggered not only by inputs end with = but also by inputs start with =. For instance, now =1+1 triggers CalculatorRewriter. See the commit message of 5d423b0b and its unittests as for how it works.
            Performance improvements in LOUDS. See commits 3591f5e7 and cac14650 for details.
        Fixed issues:
            #317: session_handler_scenario_test is flaky in Linux build on Travis-CI
            #341: 1d*= should not trigger language-aware rewriter
        Total commits:
            48 commits.

--- inputmethod/mozc-server/patches/patch-base_password__manager.cc
+++ inputmethod/mozc-server/patches/patch-base_password__manager.cc
@@ -1,13 +1,16 @@
-$NetBSD: patch-base_password__manager.cc,v 1.2 2014/01/19 01:18:50 ryoon Exp $
+$NetBSD: patch-base_password__manager.cc,v 1.3 2016/05/16 11:51:49 ryoon Exp $
 
---- base/password_manager.cc.orig	2014-01-06 07:10:31.000000000 +0000
+--- base/password_manager.cc.orig	2016-05-15 08:11:10.000000000 +0000
 +++ base/password_manager.cc
-@@ -264,7 +264,7 @@ bool WinMacPasswordManager::RemovePasswo
+@@ -264,9 +264,9 @@ bool WinMacPasswordManager::RemovePasswo
  // We use plain text file for password storage on Linux. If you port this module
  // to other Linux distro, you might want to implement a new password manager
  // which adopts some secure mechanism such like gnome-keyring.
--#if defined OS_LINUX
-+#if defined(OS_LINUX) || defined(OS_NETBSD)
+-#if defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_NACL)
++#if defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_NACL) || defined(OS_NETBSD)
  typedef PlainPasswordManager DefaultPasswordManager;
- #endif
+-#endif  // OS_LINUX || OS_ANDROID || OS_NACL
++#endif  // OS_LINUX || OS_ANDROID || OS_NACL || OS_NETBSD
  
+ // Windows or Mac
+ #if (defined(OS_WIN) || defined(OS_MACOSX))
