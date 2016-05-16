29c49293629c1c75d7ae22ca7338932c4979404c
ryoon <ryoon@pkgsrc.org>
2016-05-16 11:51:49 +0000

    Update to 2.17.2313.102
    Changelog:
    2.17.2288.102 - 2.17.2313.102 / 2016-01-03 - 2016-01-10
    
    Summary of changes between 2.17.2288.102 and 2.17.2313.102 as follows.
    
        Third party libraries:
            None.
        Build related changes:
            None.
        Major changes:
            Update system dictionary.
            Status icons for OS X are updated with Noto font.
        Fixed issues:
            #344: Support icon_prop_key entry in ibus-mozc
            #345: Mozc for Android keeps crashing
            #347: Software keyboard is not rendered correctly on Android 6 if non-material theme is selected
            #350: Status icon is not updated when using Windows Store Apps in desktop mode on Windows 10
            #351: Mozc cannot be activated in the search box on the task bar when configured to be the default IME on Windows 10
            Following issues are not completely fixed yet, but at least worked around.
                #348: DirectWrite may fail to render text in certain enviromnents
                #349: Word suggestion can be unexpectedly suppressed on Chromium
        Total commits:
            29 commits.
    
    
    2.17.2285.102 - 2.17.2287.102 / 2016-01-01 - 2016-01-02
    
    Summary of changes between 2.17.2285.102 and 2.17.2287.102 as follows.
    
        Third party libraries:
            None.
        Build related changes:
            None.
        Major changes:
            None.
        Fixed issues:
            None.
        Total commits:
            4 commits.
    
    
    2.17.2241.102 - 2.17.2284.102 / 2015-11-15 - 2015-12-31
    
    Summary of changes between 2.17.2241.102 and 2.17.2284.102 as follows.
    
        Third party libraries:
            fontTools: 5ba7d98 -> 8724513
        Build related changes:
            Reference Dockerfile for Fedora now uses Fedora 23 base image.
            Default SDKROOT for OS X build is switched from macosx10.8 to macosx10.9.
        Major changes:
            CalculatorRewriter is now triggered not only by inputs end with = but also by inputs start with =. For instance, now =1+1 triggers CalculatorRewriter. See the commit message of 5d423b0b and its unittests as for how it works.
            Performance improvements in LOUDS. See commits 3591f5e7 and cac14650 for details.
        Fixed issues:
            #317: session_handler_scenario_test is flaky in Linux build on Travis-CI
            #341: 1d*= should not trigger language-aware rewriter
        Total commits:
            48 commits.

--- inputmethod/mozc-server/patches/patch-base_logging.cc
+++ inputmethod/mozc-server/patches/patch-base_logging.cc
@@ -1,10 +1,10 @@
-$NetBSD: patch-base_logging.cc,v 1.3 2013/09/15 12:30:23 joerg Exp $
+$NetBSD: patch-base_logging.cc,v 1.4 2016/05/16 11:51:49 ryoon Exp $
 
---- base/logging.cc.orig	2013-08-28 05:26:13.000000000 +0000
+--- base/logging.cc.orig	2016-05-15 08:11:10.000000000 +0000
 +++ base/logging.cc
 @@ -61,6 +61,10 @@
- #include "base/system_util.h"
- #include "base/util.h"
+ #include "base/mutex.h"
+ #include "base/singleton.h"
  
 +#if defined(OS_NETBSD)
 +#include <lwp.h>
@@ -13,22 +13,22 @@ $NetBSD: patch-base_logging.cc,v 1.3 2013/09/15 12:30:23 joerg Exp $
  DEFINE_bool(colored_log, true, "Enables colored log messages on tty devices");
  DEFINE_bool(logtostderr,
              false,
-@@ -110,7 +114,7 @@ string Logging::GetLogMessageHeader() {
+@@ -102,7 +106,7 @@ string Logging::GetLogMessageHeader() {
             "%4.4d-%2.2d-%2.2d %2.2d:%2.2d:%2.2d %u "
- #if defined(__native_client__)
+ #if defined(OS_NACL)
             "%p",
 -#elif defined(OS_LINUX)
 +#elif defined(OS_LINUX) || defined(OS_NETBSD)
             "%lu",
  #elif defined(OS_MACOSX) && defined(__LP64__)
             "%llu",
-@@ -137,6 +141,9 @@ string Logging::GetLogMessageHeader() {
+@@ -129,6 +133,9 @@ string Logging::GetLogMessageHeader() {
             ::getpid(),
             // pthread_self() returns __nc_basic_thread_data*.
             static_cast<void*>(pthread_self())
 +#elif defined(OS_NETBSD)
-+	   ::getpid(),
-+	   (unsigned long)_lwp_self()
++          ::getpid(),
++          (unsigned long)_lwp_self()
  #else  // = OS_LINUX
             ::getpid(),
             // It returns unsigned long.
