806d26ec4f7e43eefe050b34cba3d9cc282fc89d
ryoon <ryoon@pkgsrc.org>
2016-05-16 11:51:49 +0000

    Update to 2.17.2313.102
    Changelog:
    2.17.2288.102 - 2.17.2313.102 / 2016-01-03 - 2016-01-10
    
    Summary of changes between 2.17.2288.102 and 2.17.2313.102 as follows.
    
        Third party libraries:
            None.
        Build related changes:
            None.
        Major changes:
            Update system dictionary.
            Status icons for OS X are updated with Noto font.
        Fixed issues:
            #344: Support icon_prop_key entry in ibus-mozc
            #345: Mozc for Android keeps crashing
            #347: Software keyboard is not rendered correctly on Android 6 if non-material theme is selected
            #350: Status icon is not updated when using Windows Store Apps in desktop mode on Windows 10
            #351: Mozc cannot be activated in the search box on the task bar when configured to be the default IME on Windows 10
            Following issues are not completely fixed yet, but at least worked around.
                #348: DirectWrite may fail to render text in certain enviromnents
                #349: Word suggestion can be unexpectedly suppressed on Chromium
        Total commits:
            29 commits.
    
    
    2.17.2285.102 - 2.17.2287.102 / 2016-01-01 - 2016-01-02
    
    Summary of changes between 2.17.2285.102 and 2.17.2287.102 as follows.
    
        Third party libraries:
            None.
        Build related changes:
            None.
        Major changes:
            None.
        Fixed issues:
            None.
        Total commits:
            4 commits.
    
    
    2.17.2241.102 - 2.17.2284.102 / 2015-11-15 - 2015-12-31
    
    Summary of changes between 2.17.2241.102 and 2.17.2284.102 as follows.
    
        Third party libraries:
            fontTools: 5ba7d98 -> 8724513
        Build related changes:
            Reference Dockerfile for Fedora now uses Fedora 23 base image.
            Default SDKROOT for OS X build is switched from macosx10.8 to macosx10.9.
        Major changes:
            CalculatorRewriter is now triggered not only by inputs end with = but also by inputs start with =. For instance, now =1+1 triggers CalculatorRewriter. See the commit message of 5d423b0b and its unittests as for how it works.
            Performance improvements in LOUDS. See commits 3591f5e7 and cac14650 for details.
        Fixed issues:
            #317: session_handler_scenario_test is flaky in Linux build on Travis-CI
            #341: 1d*= should not trigger language-aware rewriter
        Total commits:
            48 commits.

--- inputmethod/mozc-server/patches/patch-base_system__util.cc
+++ inputmethod/mozc-server/patches/patch-base_system__util.cc
@@ -1,70 +1,56 @@
-$NetBSD: patch-base_system__util.cc,v 1.3 2013/09/07 18:42:14 ryoon Exp $
+$NetBSD: patch-base_system__util.cc,v 1.4 2016/05/16 11:51:49 ryoon Exp $
 
---- base/system_util.cc.orig	2013-08-28 05:26:13.000000000 +0000
+--- base/system_util.cc.orig	2016-05-15 08:11:10.000000000 +0000
 +++ base/system_util.cc
-@@ -421,14 +421,14 @@ string SystemUtil::GetServerDirectory() 
+@@ -372,11 +372,11 @@ string SystemUtil::GetServerDirectory() 
  #elif defined(OS_MACOSX)
    return MacUtil::GetServerDirectory();
  
--#elif defined(OS_LINUX)
-+#elif defined(OS_LINUX) || defined(OS_NETBSD)
+-#elif defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_NACL)
++#elif defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_NACL) || defined(OS_NETBSD)
  #if defined(MOZC_SERVER_DIRECTORY)
    return MOZC_SERVER_DIRECTORY;
  #else
 -  return "/usr/lib/mozc";
-+  return "@PREFIX@/libexec";
++  return "@PREFIX@/lib/mozc";
  #endif  // MOZC_SERVER_DIRECTORY
  
--#endif  // OS_WIN, OS_MACOSX, OS_LINUX
-+#endif  // OS_WIN, OS_MACOSX, OS_LINUX, OS_NETBSD
- }
- 
- string SystemUtil::GetServerPath() {
-@@ -493,7 +493,7 @@ string SystemUtil::GetUserNameAsString()
+ #endif  // OS_WIN, OS_MACOSX, OS_LINUX, ...
+@@ -450,7 +450,7 @@ string SystemUtil::GetUserNameAsString()
    return ppw->pw_name;
  
  #else  // OS_ANDROID
--  // OS_MACOSX or OS_LINUX
-+  // OS_MACOSX or OS_LINUX, OS_NETBSD
+-  // OS_MACOSX, OS_LINUX or OS_NACL
++  // OS_MACOSX, OS_LINUX, OS_NACL or OS_NETBSD
    struct passwd pw, *ppw;
    char buf[1024];
    CHECK_EQ(0, getpwuid_r(geteuid(), &pw, buf, sizeof(buf), &ppw));
-@@ -653,7 +653,7 @@ string GetSessionIdString() {
+@@ -610,7 +610,7 @@ string GetSessionIdString() {
  #endif  // OS_WIN
  
  string SystemUtil::GetDesktopNameAsString() {
--#ifdef OS_LINUX
-+#if defined(OS_LINUX) || defined(OS_NETBSD)
+-#if defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_NACL)
++#if defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_NACL) || defined(OS_NETBSD)
    const char *display = getenv("DISPLAY");
    if (display == NULL) {
      return "";
-@@ -683,7 +683,7 @@ string SystemUtil::GetDesktopNameAsStrin
-   }
- 
-   return (session_id + "." + window_station_name + "." + desktop_name);
--#endif  // OS_LINUX, OS_MACOSX, OS_WIN
-+#endif  // OS_LINUX, OS_MACOSX, OS_WIN, OS_NETBSD
- }
- 
- #ifdef OS_WIN
-@@ -858,7 +858,7 @@ bool SystemUtil::IsPlatformSupported() {
+@@ -691,7 +691,7 @@ bool SystemUtil::IsPlatformSupported() {
  #if defined(OS_MACOSX)
    // TODO(yukawa): support Mac.
    return true;
--#elif defined(OS_LINUX)
-+#elif defined(OS_LINUX) || defined(OS_NETBSD)
+-#elif defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_NACL)
++#elif defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_NACL) || defined(OS_NETBSD)
    // TODO(yukawa): support Linux.
    return true;
  #elif defined(OS_WIN)
-@@ -1157,10 +1157,13 @@ string SystemUtil::GetOSVersionString() 
- #elif defined(OS_LINUX)
+@@ -930,10 +930,13 @@ string SystemUtil::GetOSVersionString() 
+ #elif defined(OS_LINUX) || defined(OS_NACL)
    const string ret = "Linux";
    return ret;
--#else  // !OS_WIN && !OS_MACOSX && !OS_LINUX
 +#elif defined(OS_NETBSD)
 +  const string ret = "NetBSD";
 +  return ret;
-+#else  // !OS_WIN && !OS_MACOSX && !OS_LINUX && !OS_NETBSD
+ #else  // !OS_WIN && !OS_MACOSX && !OS_LINUX
    const string ret = "Unknown";
    return ret;
 -#endif  // OS_WIN, OS_MACOSX, OS_LINUX
@@ -72,12 +58,21 @@ $NetBSD: patch-base_system__util.cc,v 1.3 2013/09/07 18:42:14 ryoon Exp $
  }
  
  bool SystemUtil::MacOSVersionIsGreaterOrEqual(int32 major,
-@@ -1204,7 +1207,7 @@ uint64 SystemUtil::GetTotalPhysicalMemor
+@@ -977,7 +980,7 @@ uint64 SystemUtil::GetTotalPhysicalMemor
      return 0;
    }
    return total_memory;
--#elif defined(OS_LINUX)
-+#elif defined(OS_LINUX) || defined(OS_NETBSD)
+-#elif defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_NACL)
++#elif defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_NACL) || defined(OS_NETBSD)
  #if defined(_SC_PAGESIZE) && defined(_SC_PHYS_PAGES)
    const long page_size = sysconf(_SC_PAGESIZE);
    const long number_of_phyisical_pages = sysconf(_SC_PHYS_PAGES);
+@@ -992,7 +995,7 @@ uint64 SystemUtil::GetTotalPhysicalMemor
+ #endif  // defined(_SC_PAGESIZE) && defined(_SC_PHYS_PAGES)
+ #else  // !(defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX))
+ #error "unknown platform"
+-#endif  // OS_WIN, OS_MACOSX, OS_LINUX
++#endif  // OS_WIN, OS_MACOSX, OS_LINUX, OS_NETBSD
+ }
+ 
+ bool SystemUtil::IsLittleEndian() {
