e6527e1f821fdbbcd695791a683267aae8c43681
ryoon <ryoon@pkgsrc.org>
2016-05-16 11:51:49 +0000

    Update to 2.17.2313.102
    Changelog:
    2.17.2288.102 - 2.17.2313.102 / 2016-01-03 - 2016-01-10
    
    Summary of changes between 2.17.2288.102 and 2.17.2313.102 as follows.
    
        Third party libraries:
            None.
        Build related changes:
            None.
        Major changes:
            Update system dictionary.
            Status icons for OS X are updated with Noto font.
        Fixed issues:
            #344: Support icon_prop_key entry in ibus-mozc
            #345: Mozc for Android keeps crashing
            #347: Software keyboard is not rendered correctly on Android 6 if non-material theme is selected
            #350: Status icon is not updated when using Windows Store Apps in desktop mode on Windows 10
            #351: Mozc cannot be activated in the search box on the task bar when configured to be the default IME on Windows 10
            Following issues are not completely fixed yet, but at least worked around.
                #348: DirectWrite may fail to render text in certain enviromnents
                #349: Word suggestion can be unexpectedly suppressed on Chromium
        Total commits:
            29 commits.
    
    
    2.17.2285.102 - 2.17.2287.102 / 2016-01-01 - 2016-01-02
    
    Summary of changes between 2.17.2285.102 and 2.17.2287.102 as follows.
    
        Third party libraries:
            None.
        Build related changes:
            None.
        Major changes:
            None.
        Fixed issues:
            None.
        Total commits:
            4 commits.
    
    
    2.17.2241.102 - 2.17.2284.102 / 2015-11-15 - 2015-12-31
    
    Summary of changes between 2.17.2241.102 and 2.17.2284.102 as follows.
    
        Third party libraries:
            fontTools: 5ba7d98 -> 8724513
        Build related changes:
            Reference Dockerfile for Fedora now uses Fedora 23 base image.
            Default SDKROOT for OS X build is switched from macosx10.8 to macosx10.9.
        Major changes:
            CalculatorRewriter is now triggered not only by inputs end with = but also by inputs start with =. For instance, now =1+1 triggers CalculatorRewriter. See the commit message of 5d423b0b and its unittests as for how it works.
            Performance improvements in LOUDS. See commits 3591f5e7 and cac14650 for details.
        Fixed issues:
            #317: session_handler_scenario_test is flaky in Linux build on Travis-CI
            #341: 1d*= should not trigger language-aware rewriter
        Total commits:
            48 commits.

--- inputmethod/mozc-server/patches/patch-build__tools_util.py
+++ inputmethod/mozc-server/patches/patch-build__tools_util.py
@@ -1,14 +1,16 @@
-$NetBSD: patch-build__tools_util.py,v 1.3 2013/09/07 18:42:14 ryoon Exp $
+$NetBSD: patch-build__tools_util.py,v 1.4 2016/05/16 11:51:49 ryoon Exp $
 
---- build_tools/util.py.orig	2013-08-28 05:25:59.000000000 +0000
+--- build_tools/util.py.orig	2016-05-15 08:11:10.000000000 +0000
 +++ build_tools/util.py
-@@ -56,6 +56,9 @@ def IsLinux():
-   """Returns true if the platform is Linux."""
+@@ -59,6 +59,11 @@ def IsLinux():
    return os.name == 'posix' and os.uname()[0] == 'Linux'
+ 
  
 +def IsNetBSD():
 +  """Returns true if the platform is NetBSD."""
 +  return os.name == 'posix' and os.uname()[0] == 'NetBSD'
- 
++
++
  def GetNumberOfProcessors():
    """Returns the number of CPU cores available.
+ 
--- inputmethod/mozc-server/patches/patch-unix_ibus_gen__mozc__xml.py
+++ inputmethod/mozc-server/patches/patch-unix_ibus_gen__mozc__xml.py
@@ -1,53 +1,15 @@
-$NetBSD: patch-unix_ibus_gen__mozc__xml.py,v 1.6 2014/06/15 13:08:34 ryoon Exp $
+$NetBSD: patch-unix_ibus_gen__mozc__xml.py,v 1.7 2016/05/16 11:51:49 ryoon Exp $
 
 * Fix for pkgsrc installation.
 
---- unix/ibus/gen_mozc_xml.py.orig	2014-05-21 10:49:56.000000000 +0000
+--- unix/ibus/gen_mozc_xml.py.orig	2016-05-15 08:11:12.000000000 +0000
 +++ unix/ibus/gen_mozc_xml.py
-@@ -93,6 +93,22 @@ IBUS_ENGINES_PROPS = {
-         'longname': ['%(product_name)s'],
-         'layout': ['default'],
-     },
-+    'NetBSD': {
-+        # DO NOT change the engine name 'mozc-jp'. The names is referenced by
-+        # unix/ibus/mozc_engine.cc.
-+        'name': ['mozc-jp'],
-+        'longname': ['%(product_name)s'],
-+        'layout': ['jp'],
-+    },
-+    # On NetBSD (IBus >= 1.5), we use special label 'default' for the keyboard
-+    # layout.
-+    'NetBSD-IBus1.5': {
-+        # DO NOT change the engine name 'mozc-jp'. The names is referenced by
-+        # unix/ibus/mozc_engine.cc.
-+        'name': ['mozc-jp'],
-+        'longname': ['%(product_name)s'],
-+        'layout': ['default'],
-+    },
- }
- 
- # A dictionary from --branding to a product name which is embedded into the
-@@ -209,7 +225,7 @@ def main():
+@@ -170,7 +170,7 @@ def main():
    (options, unused_args) = parser.parse_args()
  
    setup_arg = []
 -  setup_arg.append(os.path.join(options.server_dir, 'mozc_tool'))
 +  setup_arg.append(os.path.join("@PREFIX@/libexec", 'mozc_tool'))
    setup_arg.append('--mode=config_dialog')
-   if IsIBus15OrGreater(options):
-     # A tentative workaround against IBus 1.5
-@@ -219,6 +235,14 @@ def main():
-     platform = 'Linux'
-     common_props = IBUS_ENGINE_COMMON_PROPS
  
-+  if platform == 'NetBSD':
-+    setup_arg.append(os.path.join("@PREFIX@/libexec", 'mozc_tool'))
-+    setup_arg.append('--mode=config_dialog')
-+    if IsIBus15OrGreater(options):
-+      # A tentative workaround against IBus 1.5
-+      platform = 'NetBSD-IBus1.5'
-+      common_props = IBUS_1_5_ENGINE_COMMON_PROPS
-+
-   param_dict = {'product_name': PRODUCT_NAMES[options.branding],
-                 'ibus_mozc_path': options.ibus_mozc_path,
-                 'ibus_mozc_icon_path': options.ibus_mozc_icon_path}
+   param_dict = {
