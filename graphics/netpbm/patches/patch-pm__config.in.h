a38107024949538d5e8f02356454256ceaca4bc9
richard <richard@pkgsrc.org>
2016-05-16 21:23:31 +0000

    netpbm should use flex tool instead of lex and, while we're here, enable vasprintf for SunOS. bump PKGREVISION

--- graphics/netpbm/patches/patch-pm__config.in.h
+++ graphics/netpbm/patches/patch-pm__config.in.h
@@ -1,13 +1,14 @@
-$NetBSD: patch-pm__config.in.h,v 1.1 2014/11/23 14:38:34 gson Exp $
+$NetBSD: patch-pm__config.in.h,v 1.2 2016/05/16 21:23:31 richard Exp $
 
---- pm_config.in.h.orig	2014-11-23 14:12:37.000000000 +0000
+--- pm_config.in.h.orig	2014-09-08 21:53:09.000000000 +0000
 +++ pm_config.in.h
-@@ -139,7 +139,7 @@
+@@ -139,7 +139,8 @@
  
  /* #define HAVE_SETMODE */
  
 -#if (defined(__GLIBC__) || defined(__GNU_LIBRARY__) || defined(__APPLE__))
-+#if (defined(__GLIBC__) || defined(__GNU_LIBRARY__) || defined(__APPLE__)) || defined(__NetBSD__)
++#if (defined(__GLIBC__) || defined(__GNU_LIBRARY__) || defined(__APPLE__)) \
++	|| defined(__NetBSD__) || (defined(__SVR4) && defined (__sun))
    #define HAVE_VASPRINTF 1
  #else
    #define HAVE_VASPRINTF 0
