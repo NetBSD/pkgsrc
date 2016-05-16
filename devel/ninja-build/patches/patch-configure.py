5637af5ba50f15cb941cee553ef53698a67cbd67
ryoon <ryoon@pkgsrc.org>
2016-05-16 11:56:11 +0000

    Update to 1.7.1
    * Update MASTER_SITES and HOMEPAGE
    
    Changelog:
    I've tagged a new release of Ninja, version v1.7.1
    
    (I've also tagged v1.7.0, but don't use that.)
    
    This is the first release after moving the homepage to https://ninja-build.org
    and the repo to github.com/ninja-build.
    
    == Release notes
    * New feature: Implicit outputs (#989)
      Documented in the manual at https://ninja-build.org/manual.html#ref_outputs
    * Output formatting changes
      + in non-interactive mode, print commands when finished, not when started --
        build output is now always preceded by the command that produced it (#999)
      + as a consequence, the default NINJA_STATUS is now "[%f/%t] " and %p
        is percentage of finished edges instead of percentage of started (#1142)
      + for failing commands, print "FAILED: output name" followed by the
        faiilng command on a new line, instead of "FAILED: command" (#1033)
    
    * Several documentation updates
    
    * Minor changes and bugfixes
      + "./" in paths is no longer an error but instead canonicalizes to nothing
        (#1076)
      + New debugging flag `-d keepdepfile` (#1098)
      + `-w dupbuild=err` now works in subninjas (#1095)
      + Don't loop forever in dry-run mode if the generator rule is dirty (#1069)
      + `msvc_deps_prefix` can now be set per-rule, not just globally (#1043)
      + allow non-ANSI characters in depfiles (#763)
      + `-k 0` now works better with pools (#1017, #1023, #1126)
    
    * `-t browse` changes
      + new --port and --no-browser flags; default to 'all' target (#1112)
      + make tool work if main build file isn't called build.ninja (#1116)
    
    * Windows-specific changes
      + don't crash with deps=msvc if the compiler prints very long include notes
        (#973)
    
    * POSIX-specific changes
      + Clean up build on SIGHUP (#964)
      + subprocesses are now spawned via posix_spawn instead of fork/exec, which
        speeds up builds on some systems (e.g Mac OS X) (#1118)
      + child processes no longer detached from terminal, restoring 1.5.0 behavior
        (#1113)
      + experimentally expose deps=msvc on non-Windows too (#1125)
    
    * Changes to building ninja itself
      + allow bootstrapping ninja itself in an out-of-source build dir (#979)
      + make ninja build on AIX (#1007)
      + ninja's source compiles with MSVC2015 (#1003, #1110, #1130)
      + ninja's source now also compiles with MSVC2008 (!) (#1101)
      + ninja's source builds with libc++ on Linux (#1134)
    
    * Changes to scripts
      + zsh target completion is faster (#1046)
      + bash target completion is faster (#1047)
      + ninja_syntax: don't wrap long words in comments (#1042)
      + ninja_syntax: support implicit outputs

--- devel/ninja-build/patches/patch-configure.py
+++ devel/ninja-build/patches/patch-configure.py
@@ -1,24 +1,24 @@
-$NetBSD: patch-configure.py,v 1.1 2015/08/29 06:32:02 wiz Exp $
+$NetBSD: patch-configure.py,v 1.2 2016/05/16 11:56:11 ryoon Exp $
 
 * Add NetBSD support
 
---- configure.py.orig	2015-06-29 17:21:30.000000000 +0000
+--- configure.py.orig	2016-04-28 14:56:44.000000000 +0000
 +++ configure.py
-@@ -82,6 +82,9 @@ class Platform(object):
-         out, err = popen.communicate()
-         return '/FS ' in str(out)
+@@ -95,7 +95,7 @@ class Platform(object):
+         return self._platform == 'aix'
  
-+    def is_netbsd(self):
-+        return self._platform == 'netbsd'
-+
-     def is_windows(self):
-         return self.is_mingw() or self.is_msvc()
+     def uses_usr_local(self):
+-        return self._platform in ('freebsd', 'openbsd', 'bitrig')
++        return self._platform in ('freebsd', 'openbsd', 'bitrig', 'netbsd')
  
-@@ -330,6 +333,9 @@ else:
+     def supports_ppoll(self):
+         return self._platform in ('linux', 'openbsd', 'bitrig')
+@@ -350,8 +350,8 @@ else:
+         cflags += ['-D_WIN32_WINNT=0x0501']
+     ldflags = ['-L$builddir']
      if platform.uses_usr_local():
-         cflags.append('-I/usr/local/include')
-         ldflags.append('-L/usr/local/lib')
-+    if platform.is_netbsd():
+-        cflags.append('-I/usr/local/include')
+-        ldflags.append('-L/usr/local/lib')
 +        cflags.append('-I@PREFIX@/include')
 +        ldflags.append('-L@PREFIX@/lib')
  
