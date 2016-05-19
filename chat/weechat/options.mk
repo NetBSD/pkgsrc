1bd1ff9958be3eb216428134e701580d0fba80eb
jperkin <jperkin@pkgsrc.org>
2016-05-19 10:51:40 +0000

    Ensure python/ruby are found correctly.  Fixes build on Darwin.

--- chat/weechat/options.mk
+++ chat/weechat/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.14 2016/02/14 12:52:13 ryoon Exp $
+# $NetBSD: options.mk,v 1.15 2016/05/19 10:51:40 jperkin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.weechat
 PKG_SUPPORTED_OPTIONS=	gnutls python lua wide-curses perl ruby
@@ -21,7 +21,8 @@ PLIST_VARS+=		lua plugin python perl ruby
 .if !empty(PKG_OPTIONS:Mpython)
 .include "../../lang/python/extension.mk"
 CMAKE_ARGS+=		-DENABLE_PYTHON:BOOL=ON
-CMAKE_ARGS+=		-DPYTHON_LIBRARY:FILEPATH=${PREFIX}/lib/libpython${PYVERSSUFFIX}.so
+CMAKE_ARGS.Darwin+=	-DPYTHON_LIBRARY:FILEPATH=${PREFIX}/lib/libpython${PYVERSSUFFIX}.dylib
+CMAKE_ARGS.*+=		-DPYTHON_LIBRARY:FILEPATH=${PREFIX}/lib/libpython${PYVERSSUFFIX}.so
 PLIST.python=		yes
 .else
 CMAKE_ARGS+=		-DENABLE_PYTHON:BOOL=OFF
@@ -48,6 +49,8 @@ CMAKE_ARGS+=		-DENABLE_PERL:BOOL=OFF
 .if !empty(PKG_OPTIONS:Mruby)
 .include "../../lang/ruby/buildlink3.mk"
 CMAKE_ARGS+=		-DENABLE_RUBY:BOOL=ON
+CMAKE_ARGS+=		-DRUBY_INCLUDE_DIRS:PATH=${PREFIX}/${RUBY_INC}
+CMAKE_ARGS+=		-DRUBY_LIB:FILEPATH=${PREFIX}/lib/libruby${RUBY_SHLIB}
 PLIST.ruby=		yes
 #BUILDLINK_INCDIRS.${RUBY_BASE}+=	${RUBY_INC}
 #BUILDLINK_INCDIRS.${RUBY_BASE}+=	${RUBY_ARCHINC}
