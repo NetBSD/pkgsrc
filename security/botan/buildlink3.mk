5bda11202aa24381843fdd30763adbdd6626fbe0
joerg <joerg@pkgsrc.org>
2016-05-19 22:10:24 +0000

    Update Botan to 1.10.12, the latest pre-C++11 version. Includes various security fixes.

--- security/botan/buildlink3.mk
+++ security/botan/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2014/02/25 11:51:32 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2016/05/19 22:10:24 joerg Exp $
 
 BUILDLINK_TREE+=	botan
 
 .if !defined(BOTAN_BUILDLINK3_MK)
 BOTAN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.botan+=	botan>=1.8.13<1.9
+BUILDLINK_API_DEPENDS.botan+=	botan>=1.10.12<1.11
 BUILDLINK_PKGSRCDIR.botan?=	../../security/botan
 .endif # BOTAN_BUILDLINK3_MK
 
