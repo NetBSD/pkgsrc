2f84db47fb86a74a501ce90181431babcf1f76b1
youri <youri@pkgsrc.org>
2016-05-20 18:46:10 +0000

    Update include for pkgsrc.

--- security/polkit/buildlink3.mk
+++ security/polkit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/05/20 18:39:33 youri Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/05/20 18:46:10 youri Exp $
 #
 
 BUILDLINK_TREE+=	polkit
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	polkit
 POLKIT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.polkit+=	polkit>=0.103
-BUILDLINK_PKGSRCDIR.polkit?=	../../wip/polkit
+BUILDLINK_PKGSRCDIR.polkit?=	../../security/polkit
 
 #.include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
