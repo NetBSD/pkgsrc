60beeca9bf587a078751d8dac2fc7144c9880d0e
youri <youri>
2016-05-20 18:39:33 +0000

    Import polkit-0.113nb1 as security/polkit.
    PolicyKit is a toolkit for defining and handling authorizations.  It
    is used for allowing unprivileged processes to speak to privileged
    processes.

---
+++ security/polkit/patches/patch-test_mocklib_src_netdb.c
@@ -0,0 +1,30 @@
+$NetBSD: patch-test_mocklib_src_netdb.c,v 1.1 2016/05/20 18:39:33 youri Exp $
+
+Darwin has setnetgrent.
+SunOS has endnetgrent.
+
+--- test/mocklibc/src/netdb.c.orig      2014-01-14 22:42:25.000000000 +0000
++++ test/mocklibc/src/netdb.c
+@@ -36,6 +36,7 @@ static struct netgroup_iter global_iter;
+
+ // REMEMBER: 1 means success, 0 means failure for netgroup methods
+
++#ifndef __APPLE__
+ int setnetgrent(const char *netgroup) {
+   if (!global_netgroup_head)
+     global_netgroup_head = netgroup_parse_all();
+@@ -50,11 +51,14 @@ int setnetgrent(const char *netgroup) {
+   netgroup_iter_init(&global_iter, group);
+   return 1;
+ }
++#endif
+
++#ifndef __sun__
+ void endnetgrent(void) {
+   netgroup_free_all(global_netgroup_head);
+   global_netgroup_head = NULL;
+ }
++#endif
+
+ int getnetgrent(char **host, char **user, char **domain) {
+   if (!global_netgroup_head)
