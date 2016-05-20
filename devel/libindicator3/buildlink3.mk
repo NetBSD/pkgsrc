52a88a30262e7ce861121de41df88af01d6475a3
youri <youri@pkgsrc.org>
2016-05-20 21:33:13 +0000

    Import libindicator3-12.10.1 as devel/libindicator3.
    A set of symbols and convience functions that all indicators would
    like to use.
    
    This package provide GTK3 API.

---
+++ devel/libindicator3/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/20 21:33:13 youri Exp $
+
+BUILDLINK_TREE+=	libindicator3
+
+.if !defined(LIBINDICATOR3_BUILDLINK3_MK)
+LIBINDICATOR3_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libindicator3+=	libindicator3>=12.10.1
+BUILDLINK_PKGSRCDIR.libindicator3?=	../../devel/libindicator3
+
+.include "../../x11/gtk3/buildlink3.mk"
+.endif	# LIBINDICATOR3_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libindicator3
