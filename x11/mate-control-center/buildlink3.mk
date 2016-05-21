3617076522dbc7814f5f83b8f0ab4b75b1aa05cf
youri <youri@pkgsrc.org>
2016-05-21 18:51:11 +0000

    Import mate-control-center-1.14.0 as x11/mate-control-center.
    The control center is MATE's main interface for configuration of various
    aspects of your desktop.

---
+++ x11/mate-control-center/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/21 18:51:11 youri Exp $
+#
+
+BUILDLINK_TREE+=	mate-control-center
+
+.if !defined(MATE_CONTROL_CENTER_BUILDLINK3_MK)
+MATE_CONTROL_CENTER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.mate-control-center+=	mate-control-center>=1.8.3
+BUILDLINK_PKGSRCDIR.mate-control-center?=	../../x11/mate-control-center
+
+.include "../../x11/mate-desktop/buildlink3.mk"
+.include "../../x11/mate-menus/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../graphics/librsvg/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# MATE_CONTROL_CENTER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-mate-control-center
