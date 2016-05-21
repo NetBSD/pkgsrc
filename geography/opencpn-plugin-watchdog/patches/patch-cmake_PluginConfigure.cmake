6397424f6946c23af02bc19787a3cddb09dfd527
bouyer <bouyer@pkgsrc.org>
2016-05-21 10:50:41 +0000

    Make theses build (and run) on linux

--- geography/opencpn-plugin-watchdog/patches/patch-cmake_PluginConfigure.cmake
+++ geography/opencpn-plugin-watchdog/patches/patch-cmake_PluginConfigure.cmake
@@ -1,15 +1,19 @@
-$NetBSD: patch-cmake_PluginConfigure.cmake,v 1.1 2016/04/24 10:23:38 bouyer Exp $
+$NetBSD: patch-cmake_PluginConfigure.cmake,v 1.2 2016/05/21 10:50:41 bouyer Exp $
 Do not overwrite CMAKE_SHARED_LINKER_FLAGS
 
 --- cmake/PluginConfigure.cmake.orig	2016-04-13 23:56:21.000000000 +0200
-+++ cmake/PluginConfigure.cmake	2016-04-15 16:25:56.000000000 +0200
-@@ -40,11 +40,9 @@
++++ cmake/PluginConfigure.cmake	2016-05-21 12:26:00.398634165 +0200
+@@ -40,11 +40,13 @@
   ADD_DEFINITIONS( "-Wall -Wno-unused-result -g -O0 -fexceptions" )
   ENDIF(PROFILING)
  
 - IF(NOT APPLE)
 -  SET(CMAKE_SHARED_LINKER_FLAGS "-Wl,-Bsymbolic")
 - ELSE(NOT APPLE)
++ IF(CMAKE_SYSTEM_NAME MATCHES ".*Linux")
++   SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-Bsymbolic")
++   ADD_DEFINITIONS( "-fPIC")
++ ENDIF(CMAKE_SYSTEM_NAME MATCHES ".*Linux")
 + IF(APPLE)
    SET(CMAKE_SHARED_LINKER_FLAGS "-Wl -undefined dynamic_lookup")
 - ENDIF(NOT APPLE)
