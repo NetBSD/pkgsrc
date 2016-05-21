fcff495d3a27037f9c728fa3c952831f25eb2ba1
youri <youri@pkgsrc.org>
2016-05-21 19:01:59 +0000

    Import mate-panel-1.14.0 as x11/mate-panel.
    mate-panel contains the MATE panel, the libmate-panel-applet library and
    several applets:
    
      + the MATE panel is the area on your desktop from which you can run
        applications and applets, and perform other tasks.
    
      + the libmate-panel-applet library allows to develop small applications
        which may be embedded in the panel. These are called applets.
        Documentation for the API is available with gtk-doc.
    
      + the applets supplied here include the Workspace Switcher, the Window
        List, the Window Selector, the Notification Area, the Clock and the
        infamous 'Wanda the Fish'.

---
+++ x11/mate-panel/options.mk
@@ -0,0 +1,18 @@
+# $NetBSD: options.mk,v 1.1 2016/05/21 19:01:59 youri Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.mate-panel
+PKG_SUPPORTED_OPTIONS=	introspection
+PKG_SUGGESTED_OPTIONS=	introspection
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	introspection
+
+.if !empty(PKG_OPTIONS:Mintrospection)
+BUILDLINK_DEPMETHOD.gobject-introspection+=	build
+.include "../../devel/gobject-introspection/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-introspection
+PLIST.introspection=	yes
+.else
+CONFIGURE_ARGS+=	--disable-introspection
+.endif
