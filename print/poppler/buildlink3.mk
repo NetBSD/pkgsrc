e91e6080fa789e85a7c8cf04ef4a2cdb52ec95ed
wiz <wiz>
2016-05-22 18:20:46 +0000

    Recursive bump for poppler* update to 0.44.0.

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.48 2016/04/15 14:42:51 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.49 2016/05/22 18:20:46 wiz Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.42.0
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.44.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 .include "../../graphics/lcms2/buildlink3.mk"
