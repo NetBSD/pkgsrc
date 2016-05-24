54cc1e280c60deac03b3d28e8e1f11e466caf2c3
wiz <wiz@pkgsrc.org>
2016-05-24 21:08:21 +0000

    Add upstream bug report URLs (from he@).

--- textproc/libxml2/patches/patch-xmlIO.c
+++ textproc/libxml2/patches/patch-xmlIO.c
@@ -2,6 +2,7 @@ $NetBSD$
 
 Since this is built with C90, and %zu isn't supported then, cast
 the size_t argument to unsigned long to match the format.
+https://bugzilla.gnome.org/show_bug.cgi?id=766839
 
 --- xmlIO.c.orig	2016-05-23 07:25:25.000000000 +0000
 +++ xmlIO.c
