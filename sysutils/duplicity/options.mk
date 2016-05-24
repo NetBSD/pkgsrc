1c3658de084a107e43fab390fde4cc25e5865bfd
bsiegert <bsiegert@pkgsrc.org>
2016-05-24 19:10:46 +0000

    Update duplicity to 0.7.07.1. From Frederic Fauberteau in PR pkg/51061.
    New in v0.7.07.1 (2016/04/19)
    ---------------------------
    * Fixed bug #1568677 duplicity fails to use existing S3 bucket in boto backend
      - bug introduced by incomplete fix of bug #1296793
      - simplified setting of bucket locations
    * Fixed bug #1569523 get_bucket unknown keyword location and my_location name error
      - bug introduced in improper fix of bug #1568677
      - gotta love those inconsistent APIs
    * Fixed bug #1571134 incompatible with python-oauth2client version 2.x
      and #1558155 PyDrive backend broken, needs update to oauth2client library
      - used patch from https://bugs.debian.org/820725 but made changes
        to allow the user to continue using the old version
    * Fixed bug #1570293 duplicity is very slow due to excessive fsync
      - removed flush() after write.
      - revert to previous version

--- sysutils/duplicity/options.mk
+++ sysutils/duplicity/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2016/04/18 17:49:20 bsiegert Exp $
+# $NetBSD: options.mk,v 1.2 2016/05/24 19:10:46 bsiegert Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.duplicity
 PKG_SUPPORTED_OPTIONS=	gnupg2
@@ -7,10 +7,8 @@ PKG_SUPPORTED_OPTIONS=	gnupg2
 
 .if !empty(PKG_OPTIONS:Mgnupg2)
 DEPENDS+=	gnupg2-[0-9]*:../../security/gnupg2
-EVAL_PREFIX=	GNUPG2_PREFIX=gnupg2
-GPGPATH=	${GNUPG2_PREFIX}/bin/gpg2
+GPGPATH=	${PREFIX}/bin/gpg2
 .else
 DEPENDS+=	gnupg>=1.0.0:../../security/gnupg
-EVAL_PREFIX=	GNUPG_PREFIX=gnupg
-GPGPATH=	${GNUPG_PREFIX}/bin/gpg
+GPGPATH=	${PREFIX}/bin/gpg
 .endif
