723f930d24ddaea14b3e8c6c6e23bf32cf8776f7
ryoon <ryoon@pkgsrc.org>
2016-05-25 12:41:17 +0000

    Import ImageMagick6-6.9.4.4 as graphics/ImageMagick6.
    ImageMagick TM, is a package for display and interactive manipulation
    of images for the X Window System.  It is written in C and interfaces
    to the X library, and therefore does not require any proprietary
    toolkit in order to compile.  Although the software is copyrighted, it
    is available for free and can be redistributed without fee.
    
    The ImageMagick image display program can display an image on any
    workstation screen running an X server.  It can read and write many of
    the more popular image formats including JPEG, TIFF, PNM, GIF, and
    Photo CD.  In addition you can interactively resize, rotate, sharpen,
    color reduce, or add special effects to an image and save your
    completed work in the same or differing image format.
    
    This package tracks 6 release branch for backward compatibility.

---
+++ graphics/ImageMagick6/buildlink3.mk
@@ -0,0 +1,63 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/25 12:41:17 ryoon Exp $
+
+BUILDLINK_TREE+=	ImageMagick6
+
+.if !defined(IMAGEMAGICK6_BUILDLINK3_MK)
+IMAGEMAGICK6_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ImageMagick6+=	ImageMagick6>=5.5.7.11nb1
+BUILDLINK_ABI_DEPENDS.ImageMagick6+=	ImageMagick6>=6.9.1.7nb2
+BUILDLINK_PKGSRCDIR.ImageMagick6?=	../../graphics/ImageMagick6
+
+pkgbase := ImageMagick6
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.ImageMagick6:Mdjvu)
+.include "../../graphics/djvulibre-lib/buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.ImageMagick6:Mjasper)
+.include "../../graphics/jasper/buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.ImageMagick6:Mopenexr)
+.include "../../graphics/openexr/buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.ImageMagick6:Mwmf)
+.include "../../graphics/libwmf/buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.ImageMagick6:Mx11)
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
+.endif
+
+.PHONY: ImageMagick6-preconfigure
+pre-configure: ImageMagick6-preconfigure
+ImageMagick6-preconfigure:
+	cp ${PREFIX}/lib/pkgconfig/ImageMagick++-6.Q16.pc \
+		${BUILDLINK_DIR}/lib/pkgconfig/ImageMagick++.pc
+	cp ${PREFIX}/lib/pkgconfig/ImageMagick-6.Q16.pc \
+		${BUILDLINK_DIR}/lib/pkgconfig/ImageMagick.pc
+	cp ${PREFIX}/lib/pkgconfig/Magick++-6.Q16.pc \
+		${BUILDLINK_DIR}/lib/pkgconfig/Magick++.pc
+	cp ${PREFIX}/lib/pkgconfig/MagickCore-6.Q16.pc \
+		${BUILDLINK_DIR}/lib/pkgconfig/MagickCore.pc
+	cp ${PREFIX}/lib/pkgconfig/MagickWand-6.Q16.pc \
+		${BUILDLINK_DIR}/lib/pkgconfig/MagickWand.pc
+	cp ${PREFIX}/lib/pkgconfig/Wand-6.Q16.pc \
+		${BUILDLINK_DIR}/lib/pkgconfig/Wand.pc
+
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../archivers/xz/buildlink3.mk"
+.include "../../devel/libltdl/buildlink3.mk"
+.include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/lcms2/buildlink3.mk"
+.include "../../graphics/libwebp/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+.include "../../math/fftw/buildlink3.mk"
+.include "../../print/ghostscript/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.endif # IMAGEMAGICK6_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ImageMagick6
