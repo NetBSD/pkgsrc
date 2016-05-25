c77c857c18c655fa483006063c6c35b70cfe054e
markd <markd@pkgsrc.org>
2016-05-25 23:20:33 +0000

    Add karchive 5.21.0  part of KDE Frameworks 5
    KArchive provides classes for easy reading, creation and manipulation of
    "archive" formats like ZIP and TAR.
    
    It also provides transparent compression and decompression of data, like the
    GZip format, via a subclass of QIODevice.

---
+++ archivers/karchive/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/25 23:20:33 markd Exp $
+
+BUILDLINK_TREE+=	karchive
+
+.if !defined(KARCHIVE_BUILDLINK3_MK)
+KARCHIVE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.karchive+=	karchive>=5.18.0
+BUILDLINK_PKGSRCDIR.karchive?=	../../archivers/karchive
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KARCHIVE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-karchive
