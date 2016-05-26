c281bfa0e6c96fcca885568fcd2f0dfc77aae988
markd <markd@pkgsrc.org>
2016-05-26 08:43:53 +0000

    Add KDE Frameworks 5.21.0 packages: breeze-icons - Breeze icon themes kiconthemes - Support for icon themes kimageformats - Image format plugins for Qt5 kplotting - Lightweight plotting framework

---
+++ graphics/kiconthemes/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/26 08:43:53 markd Exp $
+
+BUILDLINK_TREE+=	kiconthemes
+
+.if !defined(KICONTHEMES_BUILDLINK3_MK)
+KICONTHEMES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kiconthemes+=	kiconthemes>=5.19.0
+BUILDLINK_PKGSRCDIR.kiconthemes?=	../../graphics/kiconthemes
+
+.include "../../x11/kconfigwidgets/buildlink3.mk"
+.include "../../x11/kitemviews/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtsvg/buildlink3.mk"
+.endif	# KICONTHEMES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kiconthemes
