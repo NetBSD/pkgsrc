26e68bb2eb6f6c1f16bfde51deac7461484a8069
markd <markd>
2016-05-26 08:29:04 +0000

    Add KDE Frameworks 5.21.0 packages: kbookmarks - Support for bookmarks and the XBEL format kcmutils - Utilities for interacting with KCModules kconfig - Configuration system kcoreaddons - Addons to QtCore kcrash - Support for application crash analysis and bug report from apps kdeclarative - Provides integration of QML and KDE Frameworks kdoctools - Documentation generation from docbook ki18n - Advanced internationalization framework kidletime - Monitoring user activity kio - Resource and network access abstraction kitemmodels - Models for Qt Model/View system knotifications - Abstraction for system notifications knotifyconfig - Configuration system for KNotify kpackage - Installation and loading of additional content as packages kparts - Document centric plugin system kpeople - Provides access to all contacts and aggregates them by person kpty - Pty abstraction kservice - Advanced plugin and service introspection ktexteditor - Advanced embeddable text editor threadweaver - High-level multithreading framework

---
+++ devel/kcrash/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/26 08:29:04 markd Exp $
+
+BUILDLINK_TREE+=	kcrash
+
+.if !defined(KCRASH_BUILDLINK3_MK)
+KCRASH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kcrash+=	kcrash>=5.18.0
+BUILDLINK_PKGSRCDIR.kcrash?=	../../devel/kcrash
+
+.include "../../devel/kcoreaddons/buildlink3.mk"
+.include "../../x11/kwindowsystem/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KCRASH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kcrash
