e08adefc544a4806f335367b6b1b039e6d6a7234
markd <markd@pkgsrc.org>
2016-05-26 09:31:43 +0000

    Add KDE Frameworks 5.21.0 packages: kdewebkit - KDE Integration for QtWebKit

---
+++ www/kdewebkit/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/26 09:31:43 markd Exp $
+
+BUILDLINK_TREE+=	kdewebkit
+
+.if !defined(KDEWEBKIT_BUILDLINK3_MK)
+KDEWEBKIT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kdewebkit+=	kdewebkit>=5.21.0
+BUILDLINK_PKGSRCDIR.kdewebkit?=	../../www/kdewebkit
+
+.include "../../devel/kparts/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtwebkit/buildlink3.mk"
+.endif	# KDEWEBKIT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kdewebkit
