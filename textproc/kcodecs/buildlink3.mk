8565b2b53cf93c12de99e977f223511d7e778fd2
markd <markd@pkgsrc.org>
2016-05-26 09:26:56 +0000

    Add KDE Frameworks 5.21.0 packages: kapidox - Frameworks API Documentation Tools kcodecs - Provide a collection of methods to manipulate strings using various    encodings kcompletion - Text completion helpers and widgets sonnet - Spelling framework for Qt5

---
+++ textproc/kcodecs/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/26 09:26:56 markd Exp $
+
+BUILDLINK_TREE+=	kcodecs
+
+.if !defined(KCODECS_BUILDLINK3_MK)
+KCODECS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kcodecs+=	kcodecs>=5.18.0
+BUILDLINK_PKGSRCDIR.kcodecs?=	../../textproc/kcodecs
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KCODECS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kcodecs
