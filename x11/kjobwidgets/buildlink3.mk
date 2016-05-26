29618d0142da8602123245c79a29315642971569
markd <markd@pkgsrc.org>
2016-05-26 09:49:48 +0000

    Add KDE Frameworks 5.21.0 packages: frameworkintegration - provide components to allow applications to integrate     with a KDE Workspace kactivities-stats - Core components for the KDE's Activities kactivities5 - Core components for the KDE's Activities kconfigwidgets - Widgets for KConfig kded - Extensible deamon for providing system level services kdesignerplugin - Integration of Frameworks widgets in Qt Designer/Creator kglobalaccel - Add support for global workspace shortcuts kguiaddons - Addons to QtGui kinit - Process launcher to speed up launching KDE applications kitemviews - Widget addons for Qt Model/View kjobwidgets - Widgets for tracking KJob instances ktextwidgets - Advanced text editing widgets kwidgetsaddons - Addons to QtWidgets kwindowsystem - Access to the windowing system kxmlgui - User configurable main windows plasma-framework - Plasma library and runtime components based upon KF5 and     Qt5

---
+++ x11/kjobwidgets/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/26 09:49:49 markd Exp $
+
+BUILDLINK_TREE+=	kjobwidgets
+
+.if !defined(KJOBWIDGETS_BUILDLINK3_MK)
+KJOBWIDGETS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kjobwidgets+=	kjobwidgets>=5.19.0
+BUILDLINK_PKGSRCDIR.kjobwidgets?=	../../x11/kjobwidgets
+
+.include "../../devel/kcoreaddons/buildlink3.mk"
+.include "../../x11/kwidgetsaddons/buildlink3.mk"
+.include "../../x11/qt5-qtx11extras/buildlink3.mk"
+.endif	# KJOBWIDGETS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kjobwidgets
