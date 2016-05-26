41d680c0d39baf8cd0f676cf254e8b6952a91ca8
markd <markd@pkgsrc.org>
2016-05-26 08:51:12 +0000

    Add KDE Frameworks 5.21.0 packages: attica-qt5 - Qt5 library that implements the Open Collaboration Services API kemoticons - Support for emoticons and emoticons themes kunitconversion - Support for unit conversion

---
+++ misc/attica-qt5/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/26 08:51:12 markd Exp $
+
+BUILDLINK_TREE+=	attica-qt5
+
+.if !defined(ATTICA_QT5_BUILDLINK3_MK)
+ATTICA_QT5_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.attica-qt5+=	attica-qt5>=5.18.0
+BUILDLINK_PKGSRCDIR.attica-qt5?=	../../misc/attica-qt5
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# ATTICA_QT5_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-attica-qt5
