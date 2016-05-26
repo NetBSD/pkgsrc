c715cdee8580d30563c5b5f4fe5c493753ecea9b
markd <markd@pkgsrc.org>
2016-05-26 09:01:44 +0000

    Add KDE Frameworks 5.21.0 packages: kdnssd - Abstraction to system DNSSD features knewstuff - Support for downloading application assets from the network kxmlrpcclient - XML-RPC client library for KDE

---
+++ net/kdnssd/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/26 09:01:44 markd Exp $
+
+BUILDLINK_TREE+=	kdnssd
+
+.if !defined(KDNSSD_BUILDLINK3_MK)
+KDNSSD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kdnssd+=	kdnssd>=5.19.0
+BUILDLINK_PKGSRCDIR.kdnssd?=	../../net/kdnssd
+
+.include "../../net/avahi/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KDNSSD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kdnssd
