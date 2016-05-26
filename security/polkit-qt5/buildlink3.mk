53e990d01936a9172445f4bd6ba88c1e66e9b50a
markd <markd@pkgsrc.org>
2016-05-26 06:22:54 +0000

    Add polkit-qt5 0.112.0
    Polkit-Qt is a library that lets developers use the Polkit API through a
    nice Qt-styled API. It is mainly a wrapper around QAction and
    QAbstractButton that lets you integrate those two component easily with
    Polkit.
    
    This package provides Qt5 libraries.

---
+++ security/polkit-qt5/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/26 06:22:54 markd Exp $
+
+BUILDLINK_TREE+=	polkit-qt5
+
+.if !defined(POLKIT_QT5_BUILDLINK3_MK)
+POLKIT_QT5_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.polkit-qt5+=	polkit-qt5>=0.112.0
+BUILDLINK_ABI_DEPENDS.polkit-qt5+=	polkit-qt5>=0.112.0
+BUILDLINK_PKGSRCDIR.polkit-qt5?=	../../security/polkit-qt5
+
+.include "../../security/polkit/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# POLKIT_QT5_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-polkit-qt5
