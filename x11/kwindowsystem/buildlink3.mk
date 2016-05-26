660e2f578ac056df460e35f42b3c6164f33e8a81
markd <markd@pkgsrc.org>
2016-05-26 09:49:48 +0000

    Add KDE Frameworks 5.21.0 packages: frameworkintegration - provide components to allow applications to integrate     with a KDE Workspace kactivities-stats - Core components for the KDE's Activities kactivities5 - Core components for the KDE's Activities kconfigwidgets - Widgets for KConfig kded - Extensible deamon for providing system level services kdesignerplugin - Integration of Frameworks widgets in Qt Designer/Creator kglobalaccel - Add support for global workspace shortcuts kguiaddons - Addons to QtGui kinit - Process launcher to speed up launching KDE applications kitemviews - Widget addons for Qt Model/View kjobwidgets - Widgets for tracking KJob instances ktextwidgets - Advanced text editing widgets kwidgetsaddons - Addons to QtWidgets kwindowsystem - Access to the windowing system kxmlgui - User configurable main windows plasma-framework - Plasma library and runtime components based upon KF5 and     Qt5

---
+++ x11/kwindowsystem/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/05/26 09:49:49 markd Exp $
+
+BUILDLINK_TREE+=	kwindowsystem
+
+.if !defined(KWINDOWSYSTEM_BUILDLINK3_MK)
+KWINDOWSYSTEM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kwindowsystem+=	kwindowsystem>=5.18.0
+BUILDLINK_PKGSRCDIR.kwindowsystem?=	../../x11/kwindowsystem
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtx11extras/buildlink3.mk"
+.endif	# KWINDOWSYSTEM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kwindowsystem
