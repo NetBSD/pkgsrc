fd8e2f003cc3de068b06c420e91f2fe5e19104b0
taca <taca@pkgsrc.org>
2016-05-27 13:29:58 +0000

    Update php70 to 7.0.7 (PHP 7.0.7), including security fix.
    26 May 2016 PHP 7.0.7
    
    - Core:
      . Fixed bug #72162 (use-after-free - error_reporting). (Laruence)
      . Add compiler option to disable special case function calls. (Joe)
      . Fixed bug #72101 (crash on complex code). (Dmitry)
      . Fixed bug #72100 (implode() inserts garbage into resulting string when
        joins very big integer). (Mikhail Galanin)
      . Fixed bug #72057 (PHP Hangs when using custom error handler and typehint).
        (Nikita Nefedov)
      . Fixed bug #72038 (Function calls with values to a by-ref parameter don't
        always throw a notice). (Bob)
      . Fixed bug #71737 (Memory leak in closure with parameter named $this).
        (Nikita)
      . Fixed bug #72059 (?? is not allowed on constant expressions). (Bob, Marcio)
      . Fixed bug #72159 (Imported Class Overrides Local Class Name). (Nikita)
    
    - Curl:
      . Fixed bug #68658 (Define CURLE_SSL_CACERT_BADFILE). (Pierrick)
    
    - DBA:
      . Fixed bug #72157 (use-after-free caused by dba_open). (Shm, Laruence)
    
    - GD:
      . Fixed bug #72227 (imagescale out-of-bounds read). (Stas)
    
    - Intl:
      . Fixed #72241 (get_icu_value_internal out-of-bounds read). (Stas)
    
    - JSON:
      . Fixed bug #72069 (Behavior \JsonSerializable different from json_encode).
        (Laruence)
    
    - Mbstring:
      . Fixed bug #72164 (Null Pointer Dereference - mb_ereg_replace). (Laruence)
    
    - OCI8:
      . Fixed bug #71600 (oci_fetch_all segfaults when selecting more than eight
        columns). (Tian Yang)
    
    - Opcache:
      . Fixed bug #72014 (Including a file with anonymous classes multiple times
        leads to fatal error). (Laruence)
    
    - OpenSSL:
      . Fixed bug #72165 (Null pointer dereference - openssl_csr_new). (Anatol)
    
    - PCNTL:
      . Fixed bug #72154 (pcntl_wait/pcntl_waitpid array internal structure
        overwrite). (Laruence)
    
    - POSIX:
      . Fixed bug #72133 (php_posix_group_to_array crashes if gr_passwd is NULL).
        (esminis at esminis dot lt)
    
    - Postgres:
      . Fixed bug #72028 (pg_query_params(): NULL converts to empty string).
        (Laruence)
      . Fixed bug #71062 (pg_convert() doesn't accept ISO 8601 for datatype
        timestamp). (denver at timothy dot io)
      . Fixed bug #72151 (mysqli_fetch_object changed behaviour). (Anatol)
    
    - Reflection:
      . Fixed bug #72174 (ReflectionProperty#getValue() causes __isset call).
        (Nikita)
    
    - Session:
      . Fixed bug #71972 (Cyclic references causing session_start(): Failed to
        decode session object). (Laruence)
    
    - Sockets:
      . Added socket_export_stream() function for getting a stream compatible
        resource from a socket resource. (Chris Wright, Bob)
    
    - SPL:
      . Fixed bug #72051 (The reference in CallbackFilterIterator doesn't work as
        expected). (Laruence)
    
    - SQLite3:
      . Fixed bug #68849 (bindValue is not using the right data type). (Anatol)
    
    - Standard:
      . Fixed bug #72075 (Referencing socket resources breaks stream_select).
        (Laruence)
      . Fixed bug #72031 (array_column() against an array of objects discards all
        values matching null). (Nikita)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.137 2016/05/27 13:28:07 taca Exp $
+# $NetBSD: phpversion.mk,v 1.138 2016/05/27 13:29:58 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -83,7 +83,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP55_VERSION=	5.5.36
 PHP56_VERSION=	5.6.22
-PHP70_VERSION=	7.0.6
+PHP70_VERSION=	7.0.7
 
 # Define initial release of major version.
 PHP55_RELDATE=	20130620
