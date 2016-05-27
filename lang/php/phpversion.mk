c86414cd3626e4a20de1b5303a5edfcb85afd805
taca <taca@pkgsrc.org>
2016-05-27 13:28:07 +0000

    Update php56 to 5.6.22 (PHP 5.6.22), including security fix.
    26 May 2016, PHP 5.6.22
    
    - Core:
      . Fixed bug #72172 (zend_hex_strtod should not use strlen).
        (bwitz at hotmail dot com )
      . Fixed bug #72114 (Integer underflow / arbitrary null write in
        fread/gzread). (Stas)
      . Fixed bug #72135 (Integer Overflow in php_html_entities). (Stas)
    
    - GD:
      . Fixed bug #72227 (imagescale out-of-bounds read). (Stas)
    
    - Intl
      . Fixed bug #64524 (Add intl.use_exceptions to php.ini-*). (Anatol)
      . Fixed bug #72241 (get_icu_value_internal out-of-bounds read). (Stas)
    
    - Postgres:
      . Fixed bug #72151 (mysqli_fetch_object changed behaviour). (Anatol)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.136 2016/05/27 13:25:44 taca Exp $
+# $NetBSD: phpversion.mk,v 1.137 2016/05/27 13:28:07 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -82,7 +82,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP55_VERSION=	5.5.36
-PHP56_VERSION=	5.6.21
+PHP56_VERSION=	5.6.22
 PHP70_VERSION=	7.0.6
 
 # Define initial release of major version.
