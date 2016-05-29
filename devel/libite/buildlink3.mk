d4401556b8310b23264fba4e4b18e553b039b3df
abs <abs@pkgsrc.org>
2016-05-29 22:25:49 +0000

    No need to have anything less than the initial commited version

--- devel/libite/buildlink3.mk
+++ devel/libite/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2016/05/29 22:24:33 abs Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/05/29 22:25:49 abs Exp $
 
 BUILDLINK_TREE+=	libite
 
 .if !defined(LIBITE_BUILDLINK3_MK)
 LIBITE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libite+=	libite>=0.4.3
+BUILDLINK_API_DEPENDS.libite+=	libite>=1.6.0
 BUILDLINK_PKGSRCDIR.libite?=	../../devel/libite
 .endif # LIBITE_BUILDLINK3_MK
 
