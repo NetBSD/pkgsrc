742221bc470f1b8f78ddd473ca7ac2c69e7bd6b2
abs <abs@pkgsrc.org>
2016-05-29 22:32:11 +0000

    Updated net/inadyn to 1.99.15
    Inadyn v1.99.15
    
    Changes
    
    * Support for new API at https://tunnelbroker.net, fixes issue #83. Use default@tunnelbroker.net to use the DYN.com API to update the IPv4 address for your IPv6-in-IPv4 tunnel. Thanks goes to Horst Venzke @hvenzke for reporting this problem!
    * The old API for the IPv6-in-IPv4 system ipv6tb@he.net is now deprecated. Users should migrate to default@tunnelbroker.net
    * Files generated by the GNU Configure & Build System is now no longer stored in GIT. Instead, users that rely on GIT must run the new ./autogen.sh script to generate the necessary files (configure).
    
    Fixes
    
    * Fix issue #100: regression from 1.99.13 pidfile is no longer created. Inadyn 1.x semantics incompatible with OpenBSD pidfile() that replaced local version in 1.99.14. Problem found by David Schury @daersc.
    * Fix issue #107: If an IP address update fails, e.g. due to temporary connectivity, HTTP transmission problems, etc. then Inadyn now forces an update in the next IP check cycle. (This is the configurable period interval in inadyn.conf.) Reported by Oliver Graute @redbrain17 and audited by @BulldozerBSG, thanks!
    * Fix issue #108: Update README with correct alias syntax for Namecheap, issue reported by @quazar0
    
    Inadyn v1.99.14
    
    Changes
    
    * New setting append-myip which, instead of appending your hostname alias, appends the current IP to the server GET update URL. See the README or the man pages for more details.
    * Prevent Inadyn from bugging out if it cannot write a cache file when the -o, --once flag is given.
    * Inadyn now defaults to a silent build, use make V=1 (like Linux) to get a verbose build. Useful for auto-builders etc.
    * Migrate to libite for functions like pidfile(), strlcpy() etc.
    * Add support for http://GiraDNS.com, thanks to Thorsten Mühlfelder, @thenktor!
    * Add support for https://www.duiadns.net, thanks to Ionut Slaveanu, @svionut!
    * Add Cygwin support for running Inadyn in Windows, thanks to Scott Mann, @sejmann!
    
    Fixes
    
    * Sanitized default logs by placing conditions for debug logs. Thanks to Frank Aurich, @1100101, for this work!
    * Fix issue #82: build failure, no rule to build target CHANGELOG, a regression introduced in 1.99.13.
    * Fix Duck DNS support, thanks to Ismani Nieuweboer, @YourDearestEigenvalue!

--- net/inadyn/patches/patch-include_ddns.h
+++ net/inadyn/patches/patch-include_ddns.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-include_ddns.h,v 1.1 2015/04/04 07:59:34 snj Exp $
+$NetBSD: patch-include_ddns.h,v 1.2 2016/05/29 22:32:12 abs Exp $
 
 /var/run is for non-persistent data, and despite the misleading name,
 RUNTIME_DATA_DIR contains "persistent cache files", so let's use
@@ -7,16 +7,24 @@ RUNTIME_DATA_DIR contains "persistent cache files", so let's use
 With RUNTIME_DATA_DIR set to a more reasonable value, stop using it
 for the default pidfile location.
 
---- include/ddns.h.orig	2015-02-07 23:08:18.000000000 -0800
-+++ include/ddns.h	2015-04-03 22:38:17.000000000 -0700
-@@ -38,8 +38,8 @@
+--- include/ddns.h.orig	2015-07-22 10:01:42.000000000 +0000
++++ include/ddns.h
+@@ -34,14 +34,14 @@
+ #include "plugin.h"
+ #include "libite/lite.h"
+ 
+-#define VERSION_STRING	"Inadyn version " VERSION " -- Dynamic DNS update client."
++#define VERSION_STRING	"Inadyn version " VERSION " -- Dynamic DNS update client."
+ #define AGENT_NAME	"inadyn/" VERSION
+ #define SUPPORT_ADDR	PACKAGE_BUGREPORT
  
  /* Test values */
- #define DEFAULT_CONFIG_FILE	"/etc/inadyn.conf"
--#define RUNTIME_DATA_DIR	"/var/run/inadyn"
--#define DEFAULT_PIDFILE		RUNTIME_DATA_DIR "/inadyn.pid"
-+#define RUNTIME_DATA_DIR	"/var/db/inadyn"
-+#define DEFAULT_PIDFILE		"/var/run/inadyn.pid"
+-#define DEFAULT_CONFIG_FILE	"/etc/inadyn.conf"
+-#define RUNTIME_DATA_DIR        _PATH_VARRUN     "inadyn/"
+-#define DEFAULT_PIDFILE         RUNTIME_DATA_DIR "inadyn.pid"
++#define DEFAULT_CONFIG_FILE     SYSCONFDIR "/inadyn.conf"
++#define RUNTIME_DATA_DIR        _PATH_VARDB  "inadyn/"
++#define DEFAULT_PIDFILE         _PATH_VARRUN "inadyn.pid"
  
  #define DYNDNS_MY_IP_SERVER	"checkip.dyndns.org"
  #define DYNDNS_MY_CHECKIP_URL	"/"
