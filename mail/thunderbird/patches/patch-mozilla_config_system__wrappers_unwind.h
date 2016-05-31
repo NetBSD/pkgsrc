3fe7ce242f994febb2f91732ebcbeaf3fd192ad6
joerg <joerg@pkgsrc.org>
2016-05-31 21:33:50 +0000

    Repeat after me: unwind.h is in already on the generated wrapper list.

--- mail/thunderbird/patches/patch-mozilla_config_system__wrappers_unwind.h
+++
@@ -1,9 +0,0 @@
-$NetBSD: patch-mozilla_config_system__wrappers_unwind.h,v 1.3 2016/04/17 18:33:50 ryoon Exp $
-
---- mozilla/config/system_wrappers/unwind.h.orig	2016-04-17 13:47:46.241139741 +0000
-+++ mozilla/config/system_wrappers/unwind.h
-@@ -0,0 +1,4 @@
-+#pragma GCC system_header
-+#pragma GCC visibility push(default)
-+#include_next <unwind.h>
-+#pragma GCC visibility pop
