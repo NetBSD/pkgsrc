204b8dd55015e00acd5d69926c121512076d800d
leot <leot>
2016-05-31 08:13:45 +0000

    Fix ttyrec "Out of pty's" error.
    Reported by jwodder via PR pkg/43209 (patch similar in spirit but it's a bit
    less intrusive).

--- misc/ttyrec/patches/patch-ttyrec.c
+++ misc/ttyrec/patches/patch-ttyrec.c
@@ -1,10 +1,33 @@
-$NetBSD: patch-ttyrec.c,v 1.1 2015/11/08 21:02:49 dholland Exp $
+$NetBSD: patch-ttyrec.c,v 1.2 2016/05/31 08:13:45 leot Exp $
 
+- add <signal.h> (needed by kill(2))
+- use <util.h> on NetBSD, OpenBSD and Mac OS X
 - remove union wait
 
---- ttyrec.c~	2006-06-11 15:52:50.000000000 +0000
+--- ttyrec.c.orig	2006-06-11 15:52:50.000000000 +0000
 +++ ttyrec.c
-@@ -203,15 +203,11 @@ doinput()
+@@ -49,6 +49,7 @@
+ #include <sys/time.h>
+ #include <sys/file.h>
+ #include <sys/signal.h>
++#include <signal.h>
+ #include <stdio.h>
+ #include <time.h>
+ #include <unistd.h>
+@@ -71,8 +72,12 @@
+ #define _(FOO) FOO
+ 
+ #ifdef HAVE_openpty
++#if defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
++#include <util.h>
++#else
+ #include <libutil.h>
+ #endif
++#endif
+ 
+ #if defined(SVR4) && !defined(CDEL)
+ #if defined(_POSIX_VDISABLE)
+@@ -203,15 +208,11 @@ doinput()
  void
  finish()
  {
