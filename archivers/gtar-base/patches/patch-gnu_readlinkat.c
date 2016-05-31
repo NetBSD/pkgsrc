c82cc680b83799c5176462280347b7cb96f2d5eb
mef <mef@pkgsrc.org>
2016-05-31 15:04:51 +0000

    (pkgsrc) - two patches are removed, upstream change (upstream) - Updated archivers/gtar      to 1.29   Updated archivers/gtar-base to 1.29   Updated archivers/gtar-info to 1.29 ------------------------------------ version 1.29 - Sergey Poznyakoff, 2016-05-16
    * New options: --verbatim-files-from, --no-verbatim-files-from
    
    The --verbatim-files-from option instructs tar to treat each line read
    from a file list as a file name, even if it starts with a dash.
    
    File lists are supplied with the --files-from (-T) option.  By
    default, each line read from a file list is first stripped off the
    leading and trailing whitespace and, if the result begins with a dash,
    it is treated as tar command line option.
    
    Use the --verbatim-files-from option to disable this special handling.
    This facilitates the use of tar with file lists created automatically
    (e.g. by find(1) command).
    
    This option affects all --files-from options that occur after it in
    the command line.  Its effect is reverted by the
    --no-verbatim-files-from option.
    
    * --null option reads file names verbatim
    
    The --null option implies --verbatim-files-from.  I.e. each line
    read from null-delimited file lists is treated as a file name.
    
    This restores the documented behavior, which was broken in version
    1.27.
    
    * New options: --owner-map=FILE and --group-map=FILE
    
    These two options provide fine-grained control over what user/group
    names (or IDs) should be mapped when adding files to archive.
    
    For both options, FILE is a plain text file with user or group
    mappings.  Empty lines are ignored.  Comments are introduced with
    # sign (unless quoted) and extend to the end of the corresponding
    line.  Each non-empty line defines translation for a single UID (GID).
    It must consist of two fields, delimited by any amount of whitespace:
    
         OLDNAME NEWNAME[:NEWID]
    
    OLDNAME is either a valid user (group) name or a ID prefixed with +.  Unless
    NEWID is supplied, NEWNAME must also be either a valid name or a
    +ID.  Otherwise, both NEWNAME and NEWID need not be listed in the
    system user database.
    
    * New option --clamp-mtime
    
    The new --clamp-mtime option changes the behavior of --mtime to only
    use the time specified if the file mtime is newer than the given time.
    The --clamp-mtime option can only be used together with	--mtime.
    
    Typical use case is to make builds reproducible: to loose less
    information, it's better to keep the original date of an archive,
    except for files modified during the build process. In that case, using
    reference (and thus reproducible) timestamps for the latter is good
    enough.
    
    See <https://wiki.debian.org/ReproducibleBuilds> for more information.
    
    * Deprecated --preserve option removed
    
    * Sparse file detection
    
    Tar now uses SEEK_DATA/SEEK_HOLE on systems that support it.  This
    allows for considerable speed-up in sparse-file detection.
    
    New option --hole-detection is provided, that allows the user to
    select the algorithm used for hole detection.  Available arguments
    are:
    
      --hole-detection=seek
         Use lseek(2) SEEK_DATA and SEEK_HOLE "whence" parameters.
    
      --hole-detection=raw
         Scan entire file before storing it to determine where holes
         are located.
    
    The default is to use "seek" whenever possible, and fall back to
    "raw" otherwise.

--- archivers/gtar-base/patches/patch-gnu_readlinkat.c
+++ archivers/gtar-base/patches/patch-gnu_readlinkat.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-gnu_readlinkat.c,v 1.1 2014/03/18 09:03:26 obache Exp $
+$NetBSD: patch-gnu_readlinkat.c,v 1.2 2016/05/31 15:04:51 mef Exp $
 
 * workaround for NetBSD-5.x, readlink is defined as a macro.
 
---- gnu/readlinkat.c.orig	2013-06-29 05:54:25.000000000 +0000
-+++ gnu/readlinkat.c
-@@ -41,6 +41,7 @@
- #define AT_FUNC_POST_FILE_PARAM_DECLS , char *buf, size_t len
- #define AT_FUNC_POST_FILE_ARGS        , buf, len
- #define AT_FUNC_RESULT ssize_t
-+#undef readlink
- #include "at-func.c"
- #undef AT_FUNC_NAME
- #undef AT_FUNC_F1
+--- gnu/readlinkat.c.orig	2015-08-24 19:08:25.000000000 +0900
++++ gnu/readlinkat.c	2016-05-31 23:53:11.000000000 +0900
+@@ -69,6 +69,7 @@ rpl_readlinkat (int fd, char const *file
+ # define AT_FUNC_POST_FILE_PARAM_DECLS , char *buf, size_t len
+ # define AT_FUNC_POST_FILE_ARGS        , buf, len
+ # define AT_FUNC_RESULT ssize_t
++# undef readlink
+ # include "at-func.c"
+ # undef AT_FUNC_NAME
+ # undef AT_FUNC_F1
