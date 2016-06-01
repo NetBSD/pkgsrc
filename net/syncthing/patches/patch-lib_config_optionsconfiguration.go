84bf99022724f799631e3eb200c247cbcdcfd11f
gdt <gdt@pkgsrc.org>
2016-06-01 18:46:00 +0000

    Update to 0.13.4.
    Tested with syncthing-android 0.7.18 (syncthing 0.13.0).
    Packaging changes: increase GOPATH hackery.
    
    Upstream changes: bug and security fixes, plus a major version upgrade
    with an incompatible protocol.
    
    Syncthing v0.13.0 "Copper Cockroach"
    Upgrade Notes
    
      v0.13 is not protocol compatible with v0.12.
    
      The database format has changed. It will be migrated without rescan
      on first startup, but the first startup may take longer than usual.
    
    Major Changes Since v0.12
    
      Devices now announce files that are in progress of being downloaded,
      in addition to those that are already complete. This means a device
      can serve parts of a file to other devices before it has completed
      the download itself.
    
      Folder labels are now distinct from the folder ID. The intention is
      for the label to be human readable and for the ID to be machine
      readable and unique. When upgrading, folder IDs are not
      changed. However when creating new folders Syncthing will suggest a
      random, unique folder ID.
    
      When receiving a connection attempt from an unknown device Syncthing
      will now show the name of that device, not only it's ID.
    
      Syncthing will now manage port mappings on NAT-PMP compliant routers,
      in addition to the existing UPnP support.  Minor Changes and
      Improvements
    
      Ignored files can now be tagged with the prefix (?d) to indicate that
      Syncthing may delete these files if they remain when deleting a
      directory. Ignore patterns use less memory by default.
    
      The address for sync connections, both the listening side and for
      remote devices, can now be made IPv4- or IPv6-only. Use the tcp4:// or
      tcp6:// schemes to specify one or the other. The default tcp:// is
      still dual stack. Additionally, relays are now treated as listen
      addresses and set in the same configuration setting.
    
      The folder settings dialog has been cleaned up.
    
      The environment variable ALL_PROXY_NO_FALLBACK can be set to disable
      falling back to direct connections in the absence of a working proxy.
    
      Many, many bug fixes.
    
    Known Issues
    
      The "Syncing" complete percentage for remote devices may erroneously
      display more than 100% (i.e. 156% or 297%) while a remote device is
      still synchronizing.

--- net/syncthing/patches/patch-lib_config_optionsconfiguration.go
+++ net/syncthing/patches/patch-lib_config_optionsconfiguration.go
@@ -1,4 +1,4 @@
-$NetBSD: patch-lib_config_optionsconfiguration.go,v 1.2 2015/11/26 00:18:56 gdt Exp $
+$NetBSD: patch-lib_config_optionsconfiguration.go,v 1.3 2016/06/01 18:46:01 gdt Exp $
 
 By default, disable automatic upgrade checking, because it is a
 privacy leak, of the sort that would earn a warning on f-droid a la:
@@ -7,14 +7,14 @@ privacy leak, of the sort that would earn a warning on f-droid a la:
 Not reported upstream because they appear to think upgrade
 checks/web-bugs are a feature.
 
---- lib/config/optionsconfiguration.go.orig	2015-11-22 15:05:20.000000000 +0000
+--- lib/config/optionsconfiguration.go.orig	2016-05-26 09:16:08.000000000 +0000
 +++ lib/config/optionsconfiguration.go
-@@ -31,7 +31,7 @@ type OptionsConfiguration struct {
+@@ -29,7 +29,7 @@ type OptionsConfiguration struct {
  	URPostInsecurely        bool     `xml:"urPostInsecurely" json:"urPostInsecurely" default:"false"` // For testing
  	URInitialDelayS         int      `xml:"urInitialDelayS" json:"urInitialDelayS" default:"1800"`
  	RestartOnWakeup         bool     `xml:"restartOnWakeup" json:"restartOnWakeup" default:"true"`
 -	AutoUpgradeIntervalH    int      `xml:"autoUpgradeIntervalH" json:"autoUpgradeIntervalH" default:"12"` // 0 for off
 +	AutoUpgradeIntervalH    int      `xml:"autoUpgradeIntervalH" json:"autoUpgradeIntervalH" default:"0"` // 0 for off
  	KeepTemporariesH        int      `xml:"keepTemporariesH" json:"keepTemporariesH" default:"24"`         // 0 for off
- 	CacheIgnoredFiles       bool     `xml:"cacheIgnoredFiles" json:"cacheIgnoredFiles" default:"true"`
+ 	CacheIgnoredFiles       bool     `xml:"cacheIgnoredFiles" json:"cacheIgnoredFiles" default:"false"`
  	ProgressUpdateIntervalS int      `xml:"progressUpdateIntervalS" json:"progressUpdateIntervalS" default:"5"`
