54356b3cdc6d242627c896842fc67ffd8db7663b
bouyer <bouyer@pkgsrc.org>
2016-06-02 11:41:26 +0000

    Apply the --disable-precomp-headers hack to gcc 5.x too. From John D. Baker, PR pkg/50287

--- x11/wxGTK30/hacks.mk
+++ x11/wxGTK30/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.3 2015/12/14 19:50:55 joerg Exp $
+# $NetBSD: hacks.mk,v 1.4 2016/06/02 11:41:26 bouyer Exp $
 
 ### [Mon Dec 14 16:58:00 MET 2015 : bouyer ]
 ### g++-4.8 on NetBSD/i386 fails with an internal compiler error
@@ -6,7 +6,8 @@
 ### 
 .if ${OPSYS} == "NetBSD" 
 .  include "../../mk/compiler.mk"
-.  if ${PKGSRC_COMPILER} == "gcc" && !empty(CC_VERSION:Mgcc-4.8.*)
+.  if ${PKGSRC_COMPILER} == "gcc" && \
+      (!empty(CC_VERSION:Mgcc-4.8.*) || !empty(CC_VERSION:Mgcc-5.*))
 CONFIGURE_ARGS+=	--disable-precomp-headers
 .  endif
 .endif
