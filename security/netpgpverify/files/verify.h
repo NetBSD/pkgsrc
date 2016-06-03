ce2bd279f6391d482f4e5e03c8f5041d62b6578b
agc <agc@pkgsrc.org>
2016-06-03 00:11:10 +0000

    Update netpgpverify to 20160313
    + minor cosmetic change to bn.h to also define BN_mod_sub, missed in
      previous

--- security/netpgpverify/files/verify.h
+++ security/netpgpverify/files/verify.h
@@ -23,9 +23,9 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #ifndef NETPGP_VERIFY_H_
-#define NETPGP_VERIFY_H_	20160214
+#define NETPGP_VERIFY_H_	20160313
 
-#define NETPGPVERIFY_VERSION	"netpgpverify portable 20160214"
+#define NETPGPVERIFY_VERSION	"netpgpverify portable 20160313"
 
 #include <sys/types.h>
 
