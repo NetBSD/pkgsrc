19d396572ae8eef0d9fee9725ba73e9032573751
fhajny <fhajny@pkgsrc.org>
2016-06-03 11:45:34 +0000

    Update net/powerdns (and modules) to 3.4.9.
    PowerDNS Authoritative Server 3.4.9
    ===================================
    
    This is a minor bugfix and performance release. Two contributions
    by Kees Monshouwer make 3.4.9 fully compatible with the new single
    key ECDSA default that is coming in version 4.0.0.
    
    Changes since 3.4.8:
    - use OpenSSL for ECDSA signing where available (Kees Monshouwer)
    - allow common signing key (Kees Monshouwer)
    - Add a disable-syslog setting
    - fix SOA caching with multiple backends (Kees Monshouwer)
    - whitespace-related zone parsing fixes ticket #3568
    - bindbackend: fix, set domain in list() (Kees Monshouwer)
    
    PowerDNS Authoritative Server 3.4.8
    ===================================
    
    This is a small bugfix release. Additionally, the deb/RPM packages
    on downloads.powerdns.com (those with -static in the name) for
    3.4.8 have been built against Botan 1.10.11 instead of Botan
    1.10.3 like previous packages. Please see the Botan Security page
    for more information on the fixes in Botan 1.10.11. As a PowerDNS
    user, these issues only affect you if you ran our -static packages
    and allowed your users to upload private keys to your
    configuration.
    
    Changes since 3.4.7:
    - Use AC_SEARCH_LIBS (Ruben Kerkhof)
    - Check for inet_aton in libresolv (Ruben Kerkhof)
    - Remove hardcoded -lresolv, -lnsl and -lsocket (Ruben Kerkhof)
    - pdnssec: don't check disabled records (Pieter Lexis)
    - pdnssec: check all records (including disabled ones) only in
      verbose mode (Kees Monshouwer)
    - traling dot in DNAME content (Kees Monshouwer)
    - Fix luabackend compilation on FreeBSD i386 (RvdE)
    - silence g++ 6.0 warnings and error (Kees Monshouwer)
    - add gcc 5.3 and 6.0 support to boost.m4 (Kees Monshouwer)
    
    PowerDNS Authoritative Server 3.4.7
    ===================================
    
    This is a security release fixing Security Advisory 2015-03
    
    Bug fixes:
    - Ignore invalid/empty TKEY and TSIG records (Christian
      Hofstaedtler)
    - Don't reply to truncated queries (Christian Hofstaedtler)
    - don't log out-of-zone ents during AXFR in (Kees Monshouwer)
    - Prevent XSS by escaping user input. Thanks to Pierre Jaury and
      Damien Cauquil at Sysdream for pointing this out.
    - Handle NULL and boolean properly in gPGSql (Aki Tuomi)
    - Improve negative caching (Kees Monshouwer)
    - Do not divide timeout twice (Aki Tuomi)
    - Correctly sort records with a priority.
    
    Improvements:
    - Direct query answers and correct zone-rectification in the GeoIP
      backend (Aki Tuomi)
    - Use token names to identify PKCS#11 keys (Aki Tuomi)
    - Fix typo in an error message (Arjen Zonneveld)
    - limit NSEC3 iterations in bindbackend (Kees Monshouwer)
    - Initialize minbody (Aki Tuomi)
    
    New features:
    - OPENPGPKEY record-type (James Cloos and Kees Monshouwer)
    - add global soa-edit settings (Kees Monshouwer)

--- net/powerdns/patches/patch-pdns_receiver.cc
+++ net/powerdns/patches/patch-pdns_receiver.cc
@@ -1,9 +1,9 @@
-$NetBSD: patch-pdns_receiver.cc,v 1.3 2015/03/27 23:37:52 rodent Exp $
+$NetBSD: patch-pdns_receiver.cc,v 1.4 2016/06/03 11:45:34 fhajny Exp $
 
 Limit inclusion of boost headers to only those necessary to avoid conflicting
 types errors on Solaris.
 
---- pdns/receiver.cc.orig	2015-03-02 13:17:12.000000000 +0000
+--- pdns/receiver.cc.orig	2016-05-13 16:04:50.000000000 +0000
 +++ pdns/receiver.cc
 @@ -42,7 +42,7 @@
  #include <sys/mman.h>
@@ -11,6 +11,6 @@ types errors on Solaris.
  #include <fstream>
 -#include <boost/algorithm/string.hpp>
 +#include <boost/algorithm/string/trim.hpp>
- 
- #include "config.h"
- #include "dns.hh"
+ #ifdef HAVE_OPENSSL
+ #include "opensslsigners.hh"
+ #endif
