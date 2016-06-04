4395ca2b3f0be8aa1e3ea9784175897920e03a6f
bsiegert <bsiegert@pkgsrc.org>
2016-06-04 19:45:18 +0000

    Pullup ticket #5034 - requested by taca lang/php55: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.136
    - lang/php55/distinfo                                           1.53
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Fri May 27 13:25:44 UTC 2016
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php55: distinfo
    
       Log Message:
       Update php55 to 5.5.36 (PHP 5.5.36), including security fix.
    
       26 May 2016, PHP 5.5.36
    
       - Core:
         . Fixed bug #72114 (Integer underflow / arbitrary null write in
           fread/gzread). (Stas)
         . Fixed bug #72135 (Integer Overflow in php_html_entities). (Stas)
    
       - GD:
          . Fixed bug #72227 (imagescale out-of-bounds read). (Stas)
    
       - Intl:
          . Fixed bug #72241 (get_icu_value_internal out-of-bounds read). (Stas)
    
       - Phar:
         . Fixed bug #71331 (Uninitialized pointer in phar_make_dirstream()).
           (CVE-2016-4343) (Stas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.131.2.3 2016/05/08 14:27:23 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.131.2.4 2016/06/04 19:45:18 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -81,7 +81,7 @@
 PHPVERSION_MK=	defined
 
 # Define each PHP's version.
-PHP55_VERSION=	5.5.35
+PHP55_VERSION=	5.5.36
 PHP56_VERSION=	5.6.21
 PHP70_VERSION=	7.0.6
 
