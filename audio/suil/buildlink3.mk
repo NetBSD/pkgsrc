8319cd2aba66fe3f0857d169864941e7597036b1
ryoon <ryoon@pkgsrc.org>
2016-06-05 00:46:25 +0000

    Import suil-0.8.2 as audio/suil.
    iSuil is a lightweight C library for loading and wrapping LV2 plugin
    UIs.
    
    Suil makes it possible to load a UI of any toolkit in a host using
    any other toolkit (assuming the toolkits are both supported by
    Suil). Hosts do not need to build against or link to foreign toolkit
    libraries to use UIs written with that toolkit; all the necessary
    magic is performed by dynamically loaded modules. The API is designed
    such that hosts do not need to explicitly support specific toolkits
    at all - if Suil supports a particular toolkit, then UIs in that
    toolkit will work in all hosts that use Suil automatically.

---
+++ audio/suil/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/06/05 00:46:25 ryoon Exp $
+
+BUILDLINK_TREE+=	suil
+
+.if !defined(SUIL_BUILDLINK3_MK)
+SUIL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.suil+=	suil>=0.8.2
+BUILDLINK_PKGSRCDIR.suil?=	../../audio/suil
+
+.include "../../audio/lv2/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# SUIL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-suil
