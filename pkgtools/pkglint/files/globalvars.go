4e4bec7649044f7cfb9bbb52a5efb9f4996308ce
rillig <rillig@pkgsrc.org>
2016-06-05 11:24:32 +0000

    Updated pkglint to 5.4.0.
    Changes since 5.3.7:
    
    * Replaced the -D... debug options with a single -d
    * Omitted duplicate diagnostics
    * Marked the :Q operator unnecessary for some variables
    * Improved detection of whether bsd.prefs.mk has been included,
      which fixed unwarranted warnings about load time evaluation of
      some variables like ${ECHO} and ${SED}
    * Improved detection for $(VAR) with round parentheses
    * Fixed allowed locations for several variables
    * Improved detection for used variables (still not perfect)
    * Added warning that MASTER_SITES should not be used in HOMEPAGE
    * Fixed warning about manual patches not being in distinfo
    * Added a check for missing MASTER_SITE_* variables
    * Added a check for unfinished url2pkg work
    * Fixed several wrong warnings

--- pkgtools/pkglint/files/globalvars.go
+++ pkgtools/pkglint/files/globalvars.go
@@ -11,18 +11,19 @@ type GlobalVars struct {
 	Pkg        *Package   // The package that is currently checked.
 	Mk         *MkLines   // The Makefile (or fragment) that is currently checked.
 
-	Todo           []string     // The files or directories that still need to be checked.
-	CurrentDir     string       // The currently checked directory, relative to the cwd
-	CurPkgsrcdir   string       // The pkgsrc directory, relative to currentDir
-	Wip            bool         // Is the currently checked directory from pkgsrc-wip?
-	Infrastructure bool         // Is the currently checked item from the pkgsrc infrastructure?
-	TestingData    *TestingData // Is pkglint in self-testing mode (only during development)?
+	Todo           []string // The files or directories that still need to be checked.
+	CurrentDir     string   // The currently checked directory, relative to the cwd
+	CurPkgsrcdir   string   // The pkgsrc directory, relative to currentDir
+	Wip            bool     // Is the currently checked directory from pkgsrc-wip?
+	Infrastructure bool     // Is the currently checked item from the pkgsrc infrastructure?
+	Testing        bool     // Is pkglint in self-testing mode (only during development)?
 
 	Hash         map[string]*Hash // Maps "alg:fname" => hash (inter-package check).
 	UsedLicenses map[string]bool  // Maps "license name" => true (inter-package check).
 
 	errors                int
 	warnings              int
+	logged                map[string]bool
 	explanationsAvailable bool
 	explanationsGiven     map[string]bool
 	autofixAvailable      bool
@@ -52,18 +53,6 @@ type CmdOpts struct {
 	CheckPatches,
 	CheckPlist bool
 
-	DebugInclude,
-	DebugMisc,
-	DebugPatches,
-	DebugQuoting,
-	DebugShell,
-	DebugTools,
-	DebugTrace,
-	DebugUnchecked,
-	DebugUnused,
-	DebugVartypes,
-	DebugVaruse bool
-
 	WarnAbsname,
 	WarnDirectcmd,
 	WarnExtra,
@@ -82,9 +71,11 @@ type CmdOpts struct {
 	PrintHelp,
 	DumpMakefile,
 	Import,
+	LogVerbose,
 	Profiling,
 	Quiet,
 	Recursive,
+	Debug,
 	PrintAutofix,
 	PrintSource,
 	PrintVersion bool
@@ -95,10 +86,6 @@ type CmdOpts struct {
 type Hash struct {
 	hash string
 	line *Line
-}
-
-type TestingData struct {
-	VerifiedBits map[string]bool
 }
 
 var G GlobalVars
