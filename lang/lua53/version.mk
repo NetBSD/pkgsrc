7757c49d65ed3e940ce39e9d2a5e4b711cf79716
alnsn <alnsn@pkgsrc.org>
2016-06-06 22:18:07 +0000

    Update lang/lua53 to version 5.3.3.
    This bugfix version fixes the following problems:
    
    1. Metatable may access its own deallocated field when it has a
       self reference in __newindex.
    2. Label between local definitions can mix-up their initializations.
    3. gmatch iterator fails when called from a coroutine different
       from the one that created it.

--- lang/lua53/version.mk
+++ lang/lua53/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.3 2015/12/05 22:50:26 alnsn Exp $
+# $NetBSD: version.mk,v 1.4 2016/06/06 22:18:07 alnsn Exp $
 
-LUA_VERSION=	5.3.2
+LUA_VERSION=	5.3.3
 
 LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
 LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
