ce4c920adf921d213fe2bca41409ff6259651315
youri <youri@pkgsrc.org>
2016-06-06 13:44:25 +0000

    patch-src_unixsocket.c accepted upstream.

--- x11/lxterminal/patches/patch-src_unixsocket.c
+++ x11/lxterminal/patches/patch-src_unixsocket.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_unixsocket.c,v 1.2 2016/06/06 13:35:35 youri Exp $
+$NetBSD: patch-src_unixsocket.c,v 1.3 2016/06/06 13:44:25 youri Exp $
 
-string.h for memset, submitted upstream.
+string.h for memset, merged upstream.
 
 --- src/unixsocket.c.orig	2015-05-21 08:43:09.000000000 -0700
 +++ src/unixsocket.c	2015-05-21 08:43:20.000000000 -0700
