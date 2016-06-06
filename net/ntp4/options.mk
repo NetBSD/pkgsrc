88bc80e0e79f7c426dc20d8f2fd6ff8a160fb73e
spz <spz@pkgsrc.org>
2016-06-06 18:34:31 +0000

    Pullup ticket #5040 - requested by taca net/ntp4: package build fixes
    Revisions pulled up:
    - net/ntp4/Makefile                                             1.93-1.94
    - net/ntp4/PLIST                                                1.22
    - net/ntp4/options.mk                                           1.3
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	bsiegert
       Date:		Fri May 13 15:50:13 UTC 2016
    
       Modified Files:
       	pkgsrc/net/ntp4: Makefile PLIST
    
       Log Message:
       Fix package installation for Darwin, which installs tickadj and ntpsnmpd.
    
       Not sure what the snmp thing is about; is it picking up a dependency from
       the base system? Why does no other OS build it?
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.92 -r1.93 pkgsrc/net/ntp4/Makefile
       cvs rdiff -u -r1.21 -r1.22 pkgsrc/net/ntp4/PLIST
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	bsiegert
       Date:		Sat May 14 08:13:49 UTC 2016
    
       Modified Files:
       	pkgsrc/net/ntp4: Makefile options.mk
    
       Log Message:
       Do SNMP support properly, as a package option, default disabled.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.93 -r1.94 pkgsrc/net/ntp4/Makefile
       cvs rdiff -u -r1.2 -r1.3 pkgsrc/net/ntp4/options.mk

--- net/ntp4/options.mk
+++ net/ntp4/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2012/06/12 15:46:00 wiz Exp $
+# $NetBSD: options.mk,v 1.2.32.1 2016/06/06 18:34:31 spz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ntp4
-PKG_SUPPORTED_OPTIONS=	inet6
-PKG_SUGGESTED_OPTIONS=	inet6
+PKG_SUPPORTED_OPTIONS=	inet6 snmp
+PKG_SUGGESTED_OPTIONS=
 
 .if empty(MISSING_FEATURES:Minet6)
 PKG_SUGGESTED_OPTIONS+=	inet6
@@ -14,4 +14,13 @@ PKG_SUGGESTED_OPTIONS+=	inet6
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
+.endif
+
+.if !empty(PKG_OPTIONS:Msnmp)
+CONFIGURE_ARGS+=	--with-ntpsnmpd
+PLIST.ntpsnmpd=		yes
+.  include "../../net/net-snmp/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-ntpsnmpd
+CONFIGURE_ARGS+=	--with-net-snmp-config=false
 .endif
