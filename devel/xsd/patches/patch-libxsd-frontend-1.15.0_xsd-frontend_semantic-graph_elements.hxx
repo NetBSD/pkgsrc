d9d10d2976aaef52b17cc34e2580142176df06d0
joerg <joerg@pkgsrc.org>
2016-06-07 17:59:50 +0000

    xsd uses iterator wrappers around the standard types's iterator and const_iterator types, but doesn't provide explicit conversion operators from iterator to const_iterator. This breaks badly when moving from C++03 to C++11 as the erase signature changed. Conditionally the logic appropiately by introducing an intermediate typedef.

--- devel/xsd/patches/patch-libxsd-frontend-1.15.0_xsd-frontend_semantic-graph_elements.hxx
+++ devel/xsd/patches/patch-libxsd-frontend-1.15.0_xsd-frontend_semantic-graph_elements.hxx
@@ -1,4 +1,4 @@
-$NetBSD: patch-libxsd-frontend-1.15.0_xsd-frontend_semantic-graph_elements.hxx,v 1.1 2013/05/21 16:36:22 joerg Exp $
+$NetBSD: patch-libxsd-frontend-1.15.0_xsd-frontend_semantic-graph_elements.hxx,v 1.2 2016/06/07 17:59:51 joerg Exp $
 
 --- libxsd-frontend-1.15.0/xsd-frontend/semantic-graph/elements.hxx.orig	2013-05-14 19:59:19.000000000 +0000
 +++ libxsd-frontend-1.15.0/xsd-frontend/semantic-graph/elements.hxx
@@ -7,7 +7,7 @@ $NetBSD: patch-libxsd-frontend-1.15.0_xsd-frontend_semantic-graph_elements.hxx,v
        remove_edge_left (Names& e)
        {
 -        ListIteratorMap::Iterator i (iterator_map_.find (&e));
-+        ListIteratorMap::ConstIterator i (iterator_map_.find (&e));
++        ListIteratorMap::EraseIterator i (iterator_map_.find (&e));
          assert (i != iterator_map_.end ());
  
          names_.erase (i->second);
