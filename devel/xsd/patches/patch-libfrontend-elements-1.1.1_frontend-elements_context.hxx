c8c07843a1b27fecfa5df865b9e96f852a2dd2c0
joerg <joerg@pkgsrc.org>
2016-06-07 17:59:50 +0000

    xsd uses iterator wrappers around the standard types's iterator and const_iterator types, but doesn't provide explicit conversion operators from iterator to const_iterator. This breaks badly when moving from C++03 to C++11 as the erase signature changed. Conditionally the logic appropiately by introducing an intermediate typedef.

--- devel/xsd/patches/patch-libfrontend-elements-1.1.1_frontend-elements_context.hxx
+++ devel/xsd/patches/patch-libfrontend-elements-1.1.1_frontend-elements_context.hxx
@@ -1,4 +1,4 @@
-$NetBSD: patch-libfrontend-elements-1.1.1_frontend-elements_context.hxx,v 1.1 2013/05/21 16:36:22 joerg Exp $
+$NetBSD: patch-libfrontend-elements-1.1.1_frontend-elements_context.hxx,v 1.2 2016/06/07 17:59:51 joerg Exp $
 
 --- libfrontend-elements-1.1.1/frontend-elements/context.hxx.orig	2013-05-14 19:57:57.000000000 +0000
 +++ libfrontend-elements-1.1.1/frontend-elements/context.hxx
@@ -7,7 +7,7 @@ $NetBSD: patch-libfrontend-elements-1.1.1_frontend-elements_context.hxx,v 1.1 20
      remove (Char const* key)
      {
 -      Map::Iterator i (map_.find (key));
-+      Map::ConstIterator i (map_.find (key));
++      Map::EraseIterator i (map_.find (key));
  
        if (i == map_.end ())
          throw NoEntry ();
