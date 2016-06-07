552b52c85883bf3bc9e43ca27e0f88f16600542a
joerg <joerg@pkgsrc.org>
2016-06-07 17:59:50 +0000

    xsd uses iterator wrappers around the standard types's iterator and const_iterator types, but doesn't provide explicit conversion operators from iterator to const_iterator. This breaks badly when moving from C++03 to C++11 as the erase signature changed. Conditionally the logic appropiately by introducing an intermediate typedef.

--- devel/xsd/patches/patch-libbackend-elements-1.6.1_backend-elements_indentation_cxx.hxx
+++ devel/xsd/patches/patch-libbackend-elements-1.6.1_backend-elements_indentation_cxx.hxx
@@ -1,4 +1,4 @@
-$NetBSD: patch-libbackend-elements-1.6.1_backend-elements_indentation_cxx.hxx,v 1.1 2013/05/21 16:36:22 joerg Exp $
+$NetBSD: patch-libbackend-elements-1.6.1_backend-elements_indentation_cxx.hxx,v 1.2 2016/06/07 17:59:51 joerg Exp $
 
 --- libbackend-elements-1.6.1/backend-elements/indentation/cxx.hxx.orig	2013-05-14 20:12:33.000000000 +0000
 +++ libbackend-elements-1.6.1/backend-elements/indentation/cxx.hxx
@@ -7,7 +7,7 @@ $NetBSD: patch-libbackend-elements-1.6.1_backend-elements_indentation_cxx.hxx,v 
              // Reduce double newline after "}" into a single one.
              //
 -            typename Hold::Iterator i (hold_.end ()), b (hold_.begin ());
-+            typename Hold::ConstIterator i (hold_.end ()), b (hold_.begin ());
++            typename Hold::EraseIterator i (hold_.end ()), b (hold_.begin ());
  
              for (--i; i != b; --i)
              {
