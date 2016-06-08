fb6e65799bbcb30c3bf58bbe52ec134c6d2ec0e6
wiz <wiz@pkgsrc.org>
2016-06-08 19:22:13 +0000

    Bump PKGREVISION for perl-5.24.

--- devel/p5-EV/buildlink3.mk
+++ devel/p5-EV/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/06/12 10:48:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/06/08 19:22:13 wiz Exp $
 
 BUILDLINK_TREE+=	p5-EV
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	p5-EV
 P5_EV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.p5-EV+=	p5-EV>=3.9
-BUILDLINK_ABI_DEPENDS.p5-EV?=	p5-EV>=4.18nb1
+BUILDLINK_ABI_DEPENDS.p5-EV?=	p5-EV>=4.22nb1
 BUILDLINK_PKGSRCDIR.p5-EV?=	../../devel/p5-EV
 
 .include "../../lang/perl5/buildlink3.mk"
