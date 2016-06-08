9e24cadb42e8fc9b53e28104663e8b05f094c38b
joerg <joerg@pkgsrc.org>
2016-06-08 13:03:18 +0000

    Just drop the cast.

--- sysutils/mate-system-monitor/patches/patch-src_lsof.cpp
+++ sysutils/mate-system-monitor/patches/patch-src_lsof.cpp
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_lsof.cpp,v 1.1 2016/06/08 13:00:44 youri Exp $
+$NetBSD: patch-src_lsof.cpp,v 1.2 2016/06/08 13:03:18 joerg Exp $
 
-Fix build on clang.
+Casting temporaries to references is not valid.
 
---- src/lsof.cpp.orig	2016-04-05 16:12:54.000000000 +0000
+--- src/lsof.cpp.orig	2016-06-04 18:57:52.613319665 +0000
 +++ src/lsof.cpp
 @@ -137,7 +137,7 @@ namespace
  
          void update_count(unsigned count)
          {
 -            string s = static_cast<std::ostringstream&>(std::ostringstream() << count).str();
-+            string s = static_cast<const std::ostringstream&>(std::ostringstream() << count).str();
++            string s = (std::ostringstream() << count).str();
              gtk_label_set_text(this->count, s.c_str());
          }
  
