2d7ea8d3d7c4959b17329f9317333915155564e7
wiz <wiz@pkgsrc.org>
2016-06-08 19:22:13 +0000

    Bump PKGREVISION for perl-5.24.

--- devel/p5-B-Hooks-OP-Annotation/buildlink3.mk
+++ devel/p5-B-Hooks-OP-Annotation/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/06/12 10:48:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/06/08 19:22:13 wiz Exp $
 
 BUILDLINK_TREE+=	p5-B-Hooks-OP-Annotation
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	p5-B-Hooks-OP-Annotation
 P5_B_HOOKS_OP_ANNOTATION_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.p5-B-Hooks-OP-Annotation+=	p5-B-Hooks-OP-Annotation>=0.43
-BUILDLINK_ABI_DEPENDS.p5-B-Hooks-OP-Annotation?=	p5-B-Hooks-OP-Annotation>=0.44nb4
+BUILDLINK_ABI_DEPENDS.p5-B-Hooks-OP-Annotation?=	p5-B-Hooks-OP-Annotation>=0.44nb5
 BUILDLINK_PKGSRCDIR.p5-B-Hooks-OP-Annotation?=	../../devel/p5-B-Hooks-OP-Annotation
 BUILDLINK_INCDIRS.p5-B-Hooks-OP-Annotation?=	${PERL5_SUB_INSTALLVENDORARCH}/B/Hooks/OP/Annotation
 BUILDLINK_CONTENTS_FILTER.p5-B-Hooks-OP-Annotation?=	${GREP} '${PERL5_SUB_INSTALLVENDORARCH}/B/Hooks/OP/Annotation'
