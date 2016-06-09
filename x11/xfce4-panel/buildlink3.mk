ae4e3515f338550f1e006e0551bb3c6c027a794b
youri <youri@pkgsrc.org>
2016-06-09 08:36:33 +0000

    xfce4-panel: Enable Gtk3 support.

--- x11/xfce4-panel/buildlink3.mk
+++ x11/xfce4-panel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.48 2015/04/25 14:25:24 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.49 2016/06/09 08:36:33 youri Exp $
 
 BUILDLINK_TREE+=	xfce4-panel
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.xfce4-panel+=	xfce4-panel>=4.12.0
 BUILDLINK_ABI_DEPENDS.xfce4-panel+=	xfce4-panel>=4.12.0nb1
 BUILDLINK_PKGSRCDIR.xfce4-panel?=	../../x11/xfce4-panel
 
+.include "../../x11/gtk3/buildlink3.mk"
 .include "../../x11/libxfce4ui/buildlink3.mk"
 .endif # XFCE4_PANEL_BUILDLINK3_MK
 
