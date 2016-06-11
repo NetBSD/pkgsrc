557e4646719a9e8b09ac35b93ed13dcd66f53645
spz <spz@pkgsrc.org>
2016-06-11 18:31:02 +0000

    Pullup ticket #5039 - requested by taca devel/libthrift: build fix
    Revisions pulled up:
    - devel/libthrift/distinfo                                      1.12-1.14
    - devel/libthrift/options.mk                                    1.8
    - devel/libthrift/patches/patch-lib__rb__setup.rb               1.2
    - devel/libthrift/patches/patch-lib_rb_Makefile.in              1.1
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Mon May 30 09:22:58 UTC 2016
    
       Modified Files:
       	pkgsrc/devel/libthrift: distinfo
       	pkgsrc/devel/libthrift/patches: patch-lib__rb__setup.rb
    
       Log Message:
       Fix build problem with Ruby 2.2 and later.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.11 -r1.12 pkgsrc/devel/libthrift/distinfo
       cvs rdiff -u -r1.1 -r1.2 \
           pkgsrc/devel/libthrift/patches/patch-lib__rb__setup.rb
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Mon May 30 16:03:13 UTC 2016
    
       Modified Files:
       	pkgsrc/devel/libthrift: distinfo options.mk
       Added Files:
       	pkgsrc/devel/libthrift/patches: patch-lib_rb_Makefile.in
    
       Log Message:
       More build fixes:
    
       * Explict specify ruby's path to avoid path in WRKDIR.
       * Do not build again on install stage to fix rpath problem.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.12 -r1.13 pkgsrc/devel/libthrift/distinfo
       cvs rdiff -u -r1.7 -r1.8 pkgsrc/devel/libthrift/options.mk
       cvs rdiff -u -r0 -r1.1 \
           pkgsrc/devel/libthrift/patches/patch-lib_rb_Makefile.in
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Mon May 30 16:09:07 UTC 2016
    
       Modified Files:
       	pkgsrc/devel/libthrift: distinfo
    
       Log Message:
       Update distinfo.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.13 -r1.14 pkgsrc/devel/libthrift/distinfo

--- devel/libthrift/patches/patch-lib__rb__setup.rb
+++ devel/libthrift/patches/patch-lib__rb__setup.rb
@@ -1,7 +1,11 @@
-$NetBSD: patch-lib__rb__setup.rb,v 1.1 2011/06/03 13:39:45 fhajny Exp $
+$NetBSD: patch-lib__rb__setup.rb,v 1.1.40.1 2016/06/11 18:31:02 spz Exp $
+
+* Changes for pkgsrc.
+* Remove duplicate 'bin-dir' key.
+* Do not use obsolete Config::CONFIG but RbConfig::CONFIG.
 
 pkgsrc prefers vendor Ruby dirs.
---- lib/rb/setup.rb.orig	2011-01-31 01:13:14.000000000 +0000
+--- lib/rb/setup.rb.orig	2011-08-11 13:19:12.000000000 +0000
 +++ lib/rb/setup.rb
 @@ -165,9 +165,9 @@ class ConfigTable
        libruby         = "#{c['prefix']}/lib/ruby"
@@ -16,3 +20,20 @@ pkgsrc prefers vendor Ruby dirs.
      elsif newpath_p
        # 1.4.4 <= V <= 1.6.3
        libruby         = "#{c['prefix']}/lib/ruby"
+@@ -281,7 +281,6 @@ class ConfigTable
+     'site-ruby-common' => 'siteruby',     # For backward compatibility
+     'site-ruby'        => 'siterubyver',  # For backward compatibility
+     'bin-dir'          => 'bindir',
+-    'bin-dir'          => 'bindir',
+     'rb-dir'           => 'rbdir',
+     'so-dir'           => 'sodir',
+     'data-dir'         => 'datadir',
+@@ -785,7 +784,7 @@ class ToplevelInstaller
+     else
+       require 'rbconfig'
+     end
+-    ::Config::CONFIG
++    ::RbConfig::CONFIG
+   end
+ 
+   def initialize(ardir_root, config)
