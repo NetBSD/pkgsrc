b9069d318a63396ce5874d523a9ed0c9f48294fb
rumko <rumko@pkgsrc.org>
2016-06-15 18:53:33 +0000

    graphics/liblqr: Remove inlines so digikam and clang are happy.
    Bump PKGREVISION.

--- graphics/liblqr/patches/patch-lqr_lqr__energy.c
+++ graphics/liblqr/patches/patch-lqr_lqr__energy.c
@@ -1,10 +1,34 @@
-$NetBSD: patch-lqr_lqr__energy.c,v 1.1 2012/06/01 21:59:27 wiz Exp $
+$NetBSD: patch-lqr_lqr__energy.c,v 1.2 2016/06/15 18:53:33 rumko Exp $
 
-Needed by gimp-liquid-rescale with clang, otherwise:
+Needed by gimp-liquid-rescale and digikam with clang, otherwise:
 graphics/gimp-liquid-rescale/work/.buildlink/lib/liblqr-1.so: undefined reference to `lqr_pixel_get_rgbcol'
+graphics/digikam/work/.buildlink/lib/liblqr-1.so: undefined reference to `lqr_carver_read_brightness_grey'
+graphics/digikam/work/.buildlink/lib/liblqr-1.so: undefined reference to `lqr_pixel_get_norm'
+graphics/digikam/work/.buildlink/lib/liblqr-1.so: undefined reference to `lqr_carver_read_brightness_std'
+graphics/digikam/work/.buildlink/lib/liblqr-1.so: undefined reference to `lqr_carver_read_brightness_grey'
+
+See https://github.com/carlobaldassi/liblqr/commit/bab7911bce2ea50e1a4db739518fbf7aabfd6888
 
 --- lqr/lqr_energy.c.orig	2009-05-11 00:08:03.000000000 +0000
 +++ lqr/lqr_energy.c
+@@ -44,7 +44,7 @@
+ 
+ /* read normalised pixel value from
+  * rgb buffer at the given index */
+-inline gdouble
++gdouble
+ lqr_pixel_get_norm(void *rgb, gint rgb_ind, LqrColDepth col_depth)
+ {
+     switch (col_depth) {
+@@ -66,7 +66,7 @@ lqr_pixel_get_norm(void *rgb, gint rgb_i
+ 
+ /* write pixel from normalised value
+  * in rgb buffer at the given index */
+-inline void
++void
+ lqr_pixel_set_norm(gdouble val, void *rgb, gint rgb_ind, LqrColDepth col_depth)
+ {
+     switch (col_depth) {
 @@ -90,7 +90,7 @@ lqr_pixel_set_norm(gdouble val, void *rg
      }
  }
@@ -14,3 +38,30 @@ graphics/gimp-liquid-rescale/work/.buildlink/lib/liblqr-1.so: undefined referenc
  lqr_pixel_get_rgbcol(void *rgb, gint rgb_ind, LqrColDepth col_depth, LqrImageType image_type, gint channel)
  {
      gdouble black_fact = 0;
+@@ -115,7 +115,7 @@ lqr_pixel_get_rgbcol(void *rgb, gint rgb
+     }
+ }
+ 
+-inline gdouble
++gdouble
+ lqr_carver_read_brightness_grey(LqrCarver *r, gint x, gint y)
+ {
+     gint now = r->raw[y][x];
+@@ -123,7 +123,7 @@ lqr_carver_read_brightness_grey(LqrCarve
+     return lqr_pixel_get_norm(r->rgb, rgb_ind, r->col_depth);
+ }
+ 
+-inline gdouble
++gdouble
+ lqr_carver_read_brightness_std(LqrCarver *r, gint x, gint y)
+ {
+     gdouble red, green, blue;
+@@ -205,7 +205,7 @@ lqr_carver_read_brightness(LqrCarver *r,
+     return bright * alpha_fact;
+ }
+ 
+-inline gdouble
++gdouble
+ lqr_carver_read_luma_std(LqrCarver *r, gint x, gint y)
+ {
+     gdouble red, green, blue;
--- graphics/liblqr/patches/patch-lqr_lqr__energy__priv.h
+++ graphics/liblqr/patches/patch-lqr_lqr__energy__priv.h
@@ -1,16 +1,35 @@
-$NetBSD: patch-lqr_lqr__energy__priv.h,v 1.1 2012/06/01 21:59:27 wiz Exp $
+$NetBSD: patch-lqr_lqr__energy__priv.h,v 1.2 2016/06/15 18:53:33 rumko Exp $
 
-Needed by gimp-liquid-rescale with clang, otherwise:
+Needed by gimp-liquid-rescale and digikam with clang, otherwise:
 graphics/gimp-liquid-rescale/work/.buildlink/lib/liblqr-1.so: undefined reference to `lqr_pixel_get_rgbcol'
+graphics/digikam/work/.buildlink/lib/liblqr-1.so: undefined reference to `lqr_carver_read_brightness_grey'
+graphics/digikam/work/.buildlink/lib/liblqr-1.so: undefined reference to `lqr_pixel_get_norm'
+graphics/digikam/work/.buildlink/lib/liblqr-1.so: undefined reference to `lqr_carver_read_brightness_std'
+graphics/digikam/work/.buildlink/lib/liblqr-1.so: undefined reference to `lqr_carver_read_brightness_grey'
+
+https://github.com/carlobaldassi/liblqr/commit/bab7911bce2ea50e1a4db739518fbf7aabfd6888
 
 --- lqr/lqr_energy_priv.h.orig	2009-05-11 00:08:03.000000000 +0000
 +++ lqr/lqr_energy_priv.h
-@@ -40,7 +40,7 @@
+@@ -38,15 +38,15 @@
+ #define LQR_SATURATE_(x) (1 / (1 + (1 / (x))))
+ #define LQR_SATURATE(x) ((x) >= 0 ? LQR_SATURATE_(x) : -LQR_SATURATE_(-x))
  
- inline gdouble lqr_pixel_get_norm(void *src, gint src_ind, LqrColDepth col_depth);
- inline void lqr_pixel_set_norm(gdouble val, void *rgb, gint rgb_ind, LqrColDepth col_depth);
+-inline gdouble lqr_pixel_get_norm(void *src, gint src_ind, LqrColDepth col_depth);
+-inline void lqr_pixel_set_norm(gdouble val, void *rgb, gint rgb_ind, LqrColDepth col_depth);
 -inline gdouble lqr_pixel_get_rgbcol(void *rgb, gint rgb_ind, LqrColDepth col_depth, LqrImageType image_type,
++gdouble lqr_pixel_get_norm(void *src, gint src_ind, LqrColDepth col_depth);
++void lqr_pixel_set_norm(gdouble val, void *rgb, gint rgb_ind, LqrColDepth col_depth);
 +gdouble lqr_pixel_get_rgbcol(void *rgb, gint rgb_ind, LqrColDepth col_depth, LqrImageType image_type,
                                      gint channel);
- inline gdouble lqr_carver_read_brightness_grey(LqrCarver *r, gint x, gint y);
- inline gdouble lqr_carver_read_brightness_std(LqrCarver *r, gint x, gint y);
+-inline gdouble lqr_carver_read_brightness_grey(LqrCarver *r, gint x, gint y);
+-inline gdouble lqr_carver_read_brightness_std(LqrCarver *r, gint x, gint y);
++gdouble lqr_carver_read_brightness_grey(LqrCarver *r, gint x, gint y);
++gdouble lqr_carver_read_brightness_std(LqrCarver *r, gint x, gint y);
+ gdouble lqr_carver_read_brightness_custom(LqrCarver *r, gint x, gint y);
+ inline gdouble lqr_carver_read_brightness(LqrCarver *r, gint x, gint y);
+-inline gdouble lqr_carver_read_luma_std(LqrCarver *r, gint x, gint y);
++gdouble lqr_carver_read_luma_std(LqrCarver *r, gint x, gint y);
+ inline gdouble lqr_carver_read_luma(LqrCarver *r, gint x, gint y);
+ inline gdouble lqr_carver_read_rgba(LqrCarver *r, gint x, gint y, gint channel);
+ inline gdouble lqr_carver_read_custom(LqrCarver *r, gint x, gint y, gint channel);
