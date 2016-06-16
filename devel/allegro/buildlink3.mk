c62698f4ca96d3a26fd1c015f5190ece72d9d74e
wiz <wiz@pkgsrc.org>
2016-06-16 06:00:46 +0000

    Add missing includes.
    Used by xalleg.h, noticed when building wip/ags.

--- devel/allegro/buildlink3.mk
+++ devel/allegro/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2012/02/20 15:39:37 reinoud Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2016/06/16 06:00:46 wiz Exp $
 
 BUILDLINK_TREE+=	allegro
 
@@ -8,6 +8,10 @@ ALLEGRO_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.allegro+=	allegro>=4.4.2
 BUILDLINK_PKGSRCDIR.allegro?=	../../devel/allegro
 
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXcursor/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXxf86vm/buildlink3.mk"
 .endif	# ALLEGRO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-allegro
