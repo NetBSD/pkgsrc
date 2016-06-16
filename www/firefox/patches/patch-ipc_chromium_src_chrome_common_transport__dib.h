cfc567ced8c487fe37d2b6de356c7d1e39569058
ryoon <ryoon>
2016-06-16 12:08:21 +0000

    Update to 47.0
    * Remove macOS patches, because I cannot confirm them sadly
    
    Changelog:
    New
        Support for Google’s Widevine CDM on Windows and Mac OS X so streaming services like Amazon Video can switch from Silverlight to encrypted HTML5 video.
        Enable VP9 video codec for users with fast machines
        Embedded YouTube videos now play with HTML5 video if Flash is not installed.
        View and search open tabs from your smartphone or another computer in a sidebar
        Allow no-cache on back/forward navigations for https resources
        Latgalu [ltg] locale added. Wikipedia tells us there are 164,500 daily speakers.
    
    Fixed
        Various security fixes
    
    Changed
        FUEL (Firefox User Extension Library) has been removed. Add-ons relying on it will stop working.
        The browser.sessionstore.restore_on_demand preference has been reset to its default value (true) to avoid e10s performance problems. Because faster is better!
        The Firefox click-to-activate plugin whitelist has been removed.
        XRender is no longer used for rendering web content on Linux as this may cause a regression in remote X performance
    
    Developer
        Web platform changes
        View, start,and debug registered Service Workers in the Service Workers developer tool
        Simulate Push messages in the Service Workers developer tool
        'Start' button for service workers in about:debugging to start registered Service Workers
        Changes that can affect add-on compatibility
        Added support for ChaCha20/Poly1305 cipher suites
        Custom user agents supported in Responsive Design Mode
        Smart multi-line input in the Web Console
    
    Developer Information
    HTML5
        cuechange events are now available on TextTrack objects
        WebCrypto: PBKDF2 supports SHA-2 hash algorithms
        WebCrypto: RSA-PSS signature support
    
    
    Fixed in Firefox 47
        2016-61 Network Security Services (NSS) vulnerabilities
        2016-60 Java applets bypass CSP protections
        2016-59 Information disclosure of disabled plugins through CSS pseudo-classes
        2016-58 Entering fullscreen and persistent pointerlock without user permission
        2016-57 Incorrect icon displayed on permissions notifications
        2016-56 Use-after-free when textures are used in WebGL operations after recycle pool destruction
        2016-55 File overwrite and privilege escalation through Mozilla Windows updater
        2016-54 Partial same-origin-policy through setting location.host through data URI
        2016-53 Out-of-bounds write with WebGL shader
        2016-52 Addressbar spoofing though the SELECT element
        2016-51 Use-after-free deleting tables from a contenteditable document
        2016-50 Buffer overflow parsing HTML5 fragments
        2016-49 Miscellaneous memory safety hazards (rv:47.0 / rv:45.2)

--- www/firefox/patches/patch-ipc_chromium_src_chrome_common_transport__dib.h
+++ www/firefox/patches/patch-ipc_chromium_src_chrome_common_transport__dib.h
@@ -1,4 +1,6 @@
-$NetBSD: patch-ipc_chromium_src_chrome_common_transport__dib.h,v 1.3 2014/02/20 13:19:03 ryoon Exp $
+$NetBSD: patch-ipc_chromium_src_chrome_common_transport__dib.h,v 1.4 2016/06/16 12:08:21 ryoon Exp $
+
+* Support Solaris
 
 --- ipc/chromium/src/chrome/common/transport_dib.h.orig	2013-05-11 19:19:32.000000000 +0000
 +++ ipc/chromium/src/chrome/common/transport_dib.h
--- www/firefox/patches/patch-toolkit_components_protobuf_src_google_protobuf_stubs_platform__macros.h
+++ www/firefox/patches/patch-toolkit_components_protobuf_src_google_protobuf_stubs_platform__macros.h
@@ -1,4 +1,6 @@
-$NetBSD: patch-toolkit_components_protobuf_src_google_protobuf_stubs_platform__macros.h,v 1.1 2015/09/23 06:44:42 ryoon Exp $
+$NetBSD: patch-toolkit_components_protobuf_src_google_protobuf_stubs_platform__macros.h,v 1.2 2016/06/16 12:08:21 ryoon Exp $
+
+* Support Solaris
 
 --- toolkit/components/protobuf/src/google/protobuf/stubs/platform_macros.h.orig	2015-09-11 00:23:02.000000000 +0000
 +++ toolkit/components/protobuf/src/google/protobuf/stubs/platform_macros.h
