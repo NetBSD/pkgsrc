28b924c298e82fc08dd1d650b984565976819efa
jaapb <jaapb@pkgsrc.org>
2016-06-17 11:45:47 +0000

    Updated package to latest version, 1.6.0, and straightened out github variables.
    Changes include:
    
    1.6.0 (30-May-2016)
    
    * Add Re.pp and Re.pp_re
    * Fix ocamldoc syntax
    
    1.5.0 (04-Jan-2016)
    
    * Add Re.exec_opt. Like exec but doesn't raise
    * Add Group module. Old group accessors are deprecated.
    * Add Mark module
    * Improve docs of Re.repn
    * Improve docs of Re_pcre
    * Fix doc of Re_pcre.match
    * Consolidate variants of Re.glob that takes options to modify its behavior
      (?period, ?expand_braces). Old variants are deprecated.
    * New option ?pathname added for Re_glob.glob. Controls how the `/` character
      is matched
    
    1.4.1 (06-Jun-2015)
    
    * Fix 4.00.1 compatibilty with tests.
    
    1.4.0 (12-May-2015)
    
    * Add Re.{mark,marked,mark_set}. Regexps can now be "marked" to query post
      execution if they matched.
    
    1.3.2 (14-Apr-2015)
    
    * Fix replacing 0 length matches
    
    1.3.1 (13-Mar-2015):
    
    * Rename {Cset, Automata} to {Re_cset, Re_automata}
    
    1.3.0 (02-Feb-2015):
    
    * Add Re.split{,_gen,_token,_full,_full_gen}
    * Add Re.replace{,_string}
    * Add Re.all{,_gen}
    * Add posix classes of the form [:xxx:]
    * Add complement suport for posix classes
    * Add Multiline and anchored flag to Re_pcre
    * Add Re_pcre.full_split

--- devel/ocaml-re/buildlink3.mk
+++ devel/ocaml-re/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/10/26 11:42:52 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/06/17 11:45:47 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-re
 
 .if !defined(OCAML_RE_BUILDLINK3_MK)
 OCAML_RE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-re+=	ocaml-re>=1.2.2
+BUILDLINK_API_DEPENDS.ocaml-re+=	ocaml-re>=1.6.0
 BUILDLINK_PKGSRCDIR.ocaml-re?=	../../devel/ocaml-re
 .endif	# OCAML_RE_BUILDLINK3_MK
 
