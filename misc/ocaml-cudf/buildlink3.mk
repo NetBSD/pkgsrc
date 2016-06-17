26a1dabd58c889e0058144459e2bb7d67f27096d
jaapb <jaapb@pkgsrc.org>
2016-06-17 16:15:48 +0000

    Updated package to latest version, 0.8. Changes include:
            * Cudf: performance improvement by allocating larger hashtbl
            * Cudf: expose empty_universe
              (Thanks to Pietro Abate for the above 2 patches)
            * Makefile: inhibit parallel build (thanks to Roberto Di Cosmo)
            * Cudf_parser: prevent in_channel leaks when using from_file

--- misc/ocaml-cudf/buildlink3.mk
+++ misc/ocaml-cudf/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/10/26 14:15:45 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/06/17 16:15:48 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cudf
 
 .if !defined(OCAML_CUDF_BUILDLINK3_MK)
 OCAML_CUDF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-cudf+=	ocaml-cudf>=0.7
+BUILDLINK_API_DEPENDS.ocaml-cudf+=	ocaml-cudf>=0.8
 BUILDLINK_PKGSRCDIR.ocaml-cudf?=	../../misc/ocaml-cudf
 
 .include "../../devel/ocaml-extlib/buildlink3.mk"
