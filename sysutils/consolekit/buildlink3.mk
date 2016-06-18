8c2c978bbeed7d19674b2e4ce448b385419a9a38
youri <youri@pkgsrc.org>
2016-06-18 21:54:43 +0000

    consolekit: Revert buildlink3.mk.

--- sysutils/consolekit/buildlink3.mk
+++ sysutils/consolekit/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2016/06/18 10:23:16 youri Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2016/06/18 21:54:43 youri Exp $
 
 BUILDLINK_TREE+=	consolekit
 
 .if !defined(CONSOLEKIT_BUILDLINK3_MK)
 CONSOLEKIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.consolekit+=	consolekit>=1.0.2
-BUILDLINK_ABI_DEPENDS.consolekit+=	consolekit>=1.0.2
+BUILDLINK_API_DEPENDS.consolekit+=	consolekit>=0.3.0
+BUILDLINK_ABI_DEPENDS.consolekit+=	consolekit>=0.3.0nb9
 BUILDLINK_PKGSRCDIR.consolekit?=	../../sysutils/consolekit
 
 .include "../../sysutils/dbus/buildlink3.mk"
