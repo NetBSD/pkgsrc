eae8498db5e9a2283b2b151a91308880c29e74b7
youri <youri@pkgsrc.org>
2016-06-18 12:16:23 +0000

    polkit: Fix build on FreeBSD.

--- security/polkit/patches/patch-test_mocklib_src_netdb.c
+++ security/polkit/patches/patch-test_mocklib_src_netdb.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-test_mocklib_src_netdb.c,v 1.1 2016/05/20 18:39:33 youri Exp $
+$NetBSD: patch-test_mocklib_src_netdb.c,v 1.2 2016/06/18 12:16:23 youri Exp $
 
 Darwin has setnetgrent.
 SunOS has endnetgrent.
 
---- test/mocklibc/src/netdb.c.orig      2014-01-14 22:42:25.000000000 +0000
+--- test/mocklibc/src/netdb.c.orig	2014-01-14 22:42:25.000000000 +0000
 +++ test/mocklibc/src/netdb.c
 @@ -36,6 +36,7 @@ static struct netgroup_iter global_iter;
-
+ 
  // REMEMBER: 1 means success, 0 means failure for netgroup methods
-
-+#ifndef __APPLE__
+ 
++#if !defined(__APPLE__) && !defined(__FreeBSD__)
  int setnetgrent(const char *netgroup) {
    if (!global_netgroup_head)
      global_netgroup_head = netgroup_parse_all();
@@ -18,13 +18,13 @@ SunOS has endnetgrent.
    return 1;
  }
 +#endif
-
+ 
 +#ifndef __sun__
  void endnetgrent(void) {
    netgroup_free_all(global_netgroup_head);
    global_netgroup_head = NULL;
  }
 +#endif
-
+ 
  int getnetgrent(char **host, char **user, char **domain) {
    if (!global_netgroup_head)
