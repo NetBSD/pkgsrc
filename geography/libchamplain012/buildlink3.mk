1df85145bb94db5316e1ca0316bff2e26659d20c
nros <nros@pkgsrc.org>
2016-06-18 11:38:09 +0000

    Fix usage of libchamplain012 by adding libsoup and sqlite3 to its buildlink3.mk.

--- geography/libchamplain012/buildlink3.mk
+++ geography/libchamplain012/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/08/31 11:50:12 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/06/18 11:38:09 nros Exp $
 
 BUILDLINK_TREE+=	libchamplain012
 
@@ -9,10 +9,12 @@ BUILDLINK_ABI_DEPENDS.libchamplain012+= libchamplain012>=0.12.10
 BUILDLINK_API_DEPENDS.libchamplain012+=	libchamplain012>=0.12.9
 BUILDLINK_PKGSRCDIR.libchamplain012?=	../../geography/libchamplain012
 
+.include "../../databases/sqlite3/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../geography/libmemphis02/buildlink3.mk"
 .include "../../graphics/clutter/buildlink3.mk"
 .include "../../graphics/clutter-gtk/buildlink3.mk"
+.include "../../net/libsoup/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
 .endif	# LIBCHAMPLAIN012_BUILDLINK3_MK
 
