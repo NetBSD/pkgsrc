4103bcffc283eae7d07d2c1fd02b2a55a35b3454
youri <youri@pkgsrc.org>
2016-06-18 10:23:16 +0000

    consolekit: Update to 1.0.2 using ConsoleKit2, the continuation of consolekit.

--- sysutils/consolekit/buildlink3.mk
+++ sysutils/consolekit/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2012/09/15 10:06:21 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2016/06/18 10:23:16 youri Exp $
 
 BUILDLINK_TREE+=	consolekit
 
 .if !defined(CONSOLEKIT_BUILDLINK3_MK)
 CONSOLEKIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.consolekit+=	consolekit>=0.3.0
-BUILDLINK_ABI_DEPENDS.consolekit+=	consolekit>=0.3.0nb9
+BUILDLINK_API_DEPENDS.consolekit+=	consolekit>=1.0.2
+BUILDLINK_ABI_DEPENDS.consolekit+=	consolekit>=1.0.2
 BUILDLINK_PKGSRCDIR.consolekit?=	../../sysutils/consolekit
 
 .include "../../sysutils/dbus/buildlink3.mk"
