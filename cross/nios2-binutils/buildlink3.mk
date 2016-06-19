0fe56f9c9185dff08806669a124451894a6317ef
wiz <wiz@pkgsrc.org>
2016-06-19 16:20:27 +0000

    Move remaining users of USE_CROSSBASE to just install into ${PREFIX}/cross.
    To simplify infrastructure.
    
    Ok joerg@
    
    XXX: nios2-gcc3 loops endlessly during build, before and after change,
    so untested.
    XXX: nios2-gcc does not build on -current, in the same way before
    and after the change, so untested.

--- cross/nios2-binutils/buildlink3.mk
+++ cross/nios2-binutils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/12/06 01:08:27 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/06/19 16:20:28 wiz Exp $
 
 BUILDLINK_TREE+=	nios2-binutils
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.nios2-binutils+=		nios2-binutils>=2.20
 BUILDLINK_PKGSRCDIR.nios2-binutils?=		../../cross/nios2-binutils
 BUILDLINK_FNAME_TRANSFORM.nios2-binutils+=	-e "s,${NIOS2_TARGET}/lib,lib/,g"
 #BUILDLINK_CONTENTS_FILTER.nios2-binutils=	${EGREP} '.*'
-BUILDLINK_CONTENTS_FILTER.nios2-binutils=	${GREP} '${CROSSBASE}'
+BUILDLINK_CONTENTS_FILTER.nios2-binutils=	${GREP} '${PREFIX}/cross'
 
 .endif # NIOS2_BINUTILS_BUILDLINK3_MK
 
