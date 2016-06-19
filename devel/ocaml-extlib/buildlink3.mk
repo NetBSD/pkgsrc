1a4a60a158b5e0404b8a2e73b34d6da7a30c6865
jaapb <jaapb@pkgsrc.org>
2016-06-19 10:12:18 +0000

    Updated buildlink3.mk file for 1.7.0 (API has changed!)

--- devel/ocaml-extlib/buildlink3.mk
+++ devel/ocaml-extlib/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/10/26 10:54:36 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/06/19 10:12:18 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-extlib
 
 .if !defined(OCAML_EXTLIB_BUILDLINK3_MK)
 OCAML_EXTLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-extlib+=	ocaml-extlib>=1.6.1
+BUILDLINK_API_DEPENDS.ocaml-extlib+=	ocaml-extlib>=1.7.0
 BUILDLINK_PKGSRCDIR.ocaml-extlib?=	../../devel/ocaml-extlib
 
 .endif	# OCAML_EXTLIB_BUILDLINK3_MK
