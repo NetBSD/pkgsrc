7ce30ab5af835d19fb80657991da8ac327efcf2c
joerg <joerg@pkgsrc.org>
2016-06-20 17:54:43 +0000

    pbulk-base-0.52: Move the tree iteration logic from the master to the client. This matters primarily when using the additional package list in the top level makefile and ensures that the client configuration is used consistently.

--- pkgtools/pbulk/files/pbulk/pscan/client.c
+++ pkgtools/pbulk/files/pbulk/pscan/client.c
@@ -1,4 +1,4 @@
-/* $NetBSD: client.c,v 1.5 2015/12/07 16:52:40 joerg Exp $ */
+/* $NetBSD: client.c,v 1.6 2016/06/20 17:54:43 joerg Exp $ */
 
 /*-
  * Copyright (c) 2007 Joerg Sonnenberger <joerg@NetBSD.org>.
@@ -48,8 +48,104 @@
 #include "pbulk.h"
 #include "pscan.h"
 
+static void
+find_full_tree_client(int fd, const char *bmake_path)
+{
+	const char * extract_subdir[] = {
+		bmake_path,
+		"show-subdir-var",
+		"VARNAME=SUBDIR",
+		NULL
+	};
+	char **pkgs;
+	size_t i, allocated_pkgs, len_pkgs, len_pkgs_data;
+	char *cat_path;
+	char *buf, *buf_orig, *cat, *cat_orig;
+	size_t buf_len, cat_len;
+	uint32_t net_len_pkgs_data;
+	ssize_t sent_bytes;
+
+	buf = read_from_child(pkgsrc_tree, bmake_path, extract_subdir);
+
+	if (buf == NULL)
+		err(1, "Cannot extract categories");
+
+	cat = cat_orig = buf;
+	allocated_pkgs = len_pkgs = 0;
+	len_pkgs_data = 0;
+	pkgs = NULL;
+	for (;;) {
+		cat += strspn(cat, " \t\n");
+		cat_len = strcspn(cat, " \t\n");
+		if (cat_len == 0)
+			break;
+
+		cat_path = xasprintf("%s/%.*s", pkgsrc_tree, (int)cat_len, cat);
+		buf_orig = buf = read_from_child(cat_path, bmake_path, extract_subdir);
+		free(cat_path);
+		if (buf == NULL) {
+			warnx("Cannot extract subdirectories for %.*s", (int)cat_len, cat);
+			cat += cat_len;
+			continue;
+		}
+
+		for (;;) {
+			buf += strspn(buf, " \t\n");
+			buf_len = strcspn(buf, " \t\n");
+			if (buf_len == 0)
+				break;
+			if (len_pkgs == allocated_pkgs) {
+				if (allocated_pkgs == 0) {
+					allocated_pkgs = 1024;
+					pkgs = xmalloc(sizeof(*pkgs) *
+					               allocated_pkgs);
+				} else {
+					allocated_pkgs *= 2;
+					pkgs = xrealloc(pkgs,
+					    sizeof(*pkgs) * allocated_pkgs);
+				}
+			}
+			pkgs[len_pkgs] = xasprintf("%.*s/%.*s", (int)cat_len,
+			    cat, (int)buf_len, buf);
+			len_pkgs_data += strlen(pkgs[len_pkgs]) + 1;
+			++len_pkgs;
+			buf += buf_len;
+		}
+		free(buf_orig);
+
+		cat += cat_len;
+	}
+
+	free(cat_orig);
+
+	if (len_pkgs_data > 0xfffffffful)
+		errx(1, "Directory list too large");
+	net_len_pkgs_data = ntohl((uint32_t)len_pkgs_data);
+
+	sent_bytes = atomic_write(fd, &net_len_pkgs_data, 4);
+	if (sent_bytes == -1)
+		err(1, "Could not write to socket");
+	if (sent_bytes != 4)
+		errx(1, "Premature end of stream while writing to socket");
+	for (i = 0; i < len_pkgs; ++i) {
+		size_t l = strlen(pkgs[i]);
+		sent_bytes = atomic_write(fd, pkgs[i], l);
+		if (sent_bytes == -1)
+			err(1, "Could not write to socket");
+		if ((size_t)sent_bytes != l)
+			errx(1, "Premature end of stream while writing to socket");
+		sent_bytes = atomic_write(fd, "\n", 1);
+		if (sent_bytes == -1)
+			err(1, "Could not write to socket");
+		if (sent_bytes != 1)
+			errx(1, "Premature end of stream while writing to socket");
+		free(pkgs[i]);
+	}
+	free(pkgs);
+}
+
 void
-client_mode(const char *client_port)
+client_mode(const char *client_port, const char *bmake_path)
 {
 	uint16_t path_len;
 	uint32_t net_output_len;
@@ -82,6 +178,13 @@ loop:
 		err(1, "Could not read from socket");
 	if (recv_bytes != path_len || strlen(path) != path_len)
 		errx(1, "Premature end of stream while reading path from socket");
+
+	if (strcmp(path, "find_full_tree") == 0) {
+		free(path);
+		find_full_tree_client(fd, bmake_path);
+		goto loop;
+	}
+
 	if (path[0] == '/' ||
 	    strchr(path, '/') == NULL ||
 	    strchr(path, '/') != strrchr(path, '/') ||
@@ -104,12 +207,12 @@ loop:
 		errx(1, "Output too large");
 	net_output_len = htonl((uint32_t)output_len);
 
-	sent_bytes = write(fd, &net_output_len, 4);
+	sent_bytes = atomic_write(fd, &net_output_len, 4);
 	if (sent_bytes == -1)
 		err(1, "Could not write to socket");
 	if (sent_bytes != 4)
 		errx(1, "Premature end of stream while writing to socket");
-	sent_bytes = write(fd, output, output_len);
+	sent_bytes = atomic_write(fd, output, output_len);
 	if (sent_bytes == -1)
 		err(1, "Could not write to socket");
 	if ((size_t)sent_bytes != output_len)
