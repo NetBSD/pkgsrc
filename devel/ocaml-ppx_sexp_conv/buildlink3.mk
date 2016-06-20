e50b2cd5ee26d2b8da58ac19cb6ba9f5f3588284
jaapb <jaapb@pkgsrc.org>
2016-06-20 19:10:56 +0000

    Updated package to latest version, 113.33.01. Changes include:
    ## 113.33.01+4.03
    
    Remove generation of empty value bindings.
    
    ## 113.33.00+4.03
    
    Various updates to work with OCaml 4.03.0
    
    ## 113.33.00
    
    - Clean up the documentation for sexplib, modernizing it to include
      `ppx_sexp_conv`, and breaking up the documentation between sexplib and
      `ppx_sexp_conv`.  Also changed the formatting to use org-mode, so it
      will render properly on github.  Markdown doesn't render well by
      default, unless you use quite different conventions about linebeaks.

--- devel/ocaml-ppx_sexp_conv/buildlink3.mk
+++ devel/ocaml-ppx_sexp_conv/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/03/04 15:50:18 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/06/20 19:10:56 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_sexp_conv
 
 .if !defined(OCAML_PPX_SEXP_CONV_BUILDLINK3_MK)
 OCAML_PPX_SEXP_CONV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ppx_sexp_conv+=	ocaml-ppx_sexp_conv>=113.24.00
+BUILDLINK_API_DEPENDS.ocaml-ppx_sexp_conv+=	ocaml-ppx_sexp_conv>=113.33.01
 BUILDLINK_PKGSRCDIR.ocaml-ppx_sexp_conv?=	../../devel/ocaml-ppx_sexp_conv
 
 #.include "../../devel/ocaml-ppx_core/buildlink3.mk"
