37ca649e09904f3c16f345ff49494ef0a1c874fa
kamil <kamil@pkgsrc.org>
2016-06-20 18:02:46 +0000

    Fix BUILDLINK_PKGSRCDIR.liblo

--- audio/liblo/buildlink3.mk
+++ audio/liblo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/06/20 08:09:40 kamil Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/06/20 18:02:46 kamil Exp $
 
 BUILDLINK_TREE+=	liblo
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	liblo
 LIBLO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.liblo+=	liblo>=0.23
-BUILDLINK_PKGSRCDIR.liblo?=	../../wip/liblo
+BUILDLINK_PKGSRCDIR.liblo?=	../../audio/liblo
 
 .include "../../mk/pthread.buildlink3.mk"
 .endif # LIBLO_BUILDLINK3_MK
