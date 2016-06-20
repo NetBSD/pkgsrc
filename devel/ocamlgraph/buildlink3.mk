324d61f168c9c10e57120ee7bf61081163b5b28e
jaapb <jaapb@pkgsrc.org>
2016-06-20 10:36:38 +0000

    Updated buidlink3 for API change, revbump

--- devel/ocamlgraph/buildlink3.mk
+++ devel/ocamlgraph/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/10/26 13:43:48 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/06/20 10:36:38 jaapb Exp $
 
 BUILDLINK_TREE+=	ocamlgraph
 
 .if !defined(OCAMLGRAPH_BUILDLINK3_MK)
 OCAMLGRAPH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocamlgraph+=	ocamlgraph>=1.8.5
+BUILDLINK_API_DEPENDS.ocamlgraph+=	ocamlgraph>=1.8.7
 BUILDLINK_PKGSRCDIR.ocamlgraph?=	../../devel/ocamlgraph
 .endif	# OCAMLGRAPH_BUILDLINK3_MK
 
