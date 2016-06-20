854ac019583ee4dc1d3aaae649c3f62642afd93f
jaapb <jaapb@pkgsrc.org>
2016-06-20 10:43:18 +0000

    Updated buildlink3.mk file to reflect API change, revbump, removed unused patch.

--- devel/ocaml-cmdliner/buildlink3.mk
+++ devel/ocaml-cmdliner/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2016/05/05 11:45:36 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2016/06/20 10:43:18 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cmdliner
 
 .if !defined(OCAML_CMDLINER_BUILDLINK3_MK)
 OCAML_CMDLINER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-cmdliner+=	ocaml-cmdliner>=0.9.4
+BUILDLINK_API_DEPENDS.ocaml-cmdliner+=	ocaml-cmdliner>=0.9.8
 BUILDLINK_ABI_DEPENDS.ocaml-cmdliner?=	ocaml-cmdliner>=0.9.4nb4
 BUILDLINK_PKGSRCDIR.ocaml-cmdliner?=	../../devel/ocaml-cmdliner
 
