b63c4834fe1cb8462eeb03365dac9189eb0f3bd3
jaapb <jaapb@pkgsrc.org>
2016-06-20 15:36:04 +0000

    Updated package to work with new mk/ocaml.mk; no upstream changes. Revbump.

--- net/ocamlnet/buildlink3.mk
+++ net/ocamlnet/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/07/06 09:29:14 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/06/20 15:36:04 jaapb Exp $
 
 BUILDLINK_TREE+=	ocamlnet
 
 .if !defined(OCAMLNET_BUILDLINK3_MK)
 OCAMLNET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocamlnet+=	ocamlnet>=3.5.1
+BUILDLINK_API_DEPENDS.ocamlnet+=	ocamlnet>=4.1.1nb1
 BUILDLINK_PKGSRCDIR.ocamlnet?=	../../net/ocamlnet
 
 .endif	# OCAMLNET_BUILDLINK3_MK
