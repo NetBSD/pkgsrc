1412c8da9b6c491824be818ee4051658aec4b109
prlw1 <prlw1@pkgsrc.org>
2016-06-23 15:05:38 +0000

    Fix previous by going the whole hog and requiring C++11.

--- www/libecap/patches/patch-src_libecap_common_memory.h
+++ www/libecap/patches/patch-src_libecap_common_memory.h
@@ -1,30 +1,24 @@
-$NetBSD: patch-src_libecap_common_memory.h,v 1.1 2016/06/21 17:57:07 joerg Exp $
+$NetBSD: patch-src_libecap_common_memory.h,v 1.2 2016/06/23 15:05:38 prlw1 Exp $
 
---- src/libecap/common/memory.h.orig	2016-06-20 16:15:17.012240492 +0000
+https://bugs.launchpad.net/bugs/1595488
+
+--- src/libecap/common/memory.h.orig	2014-10-02 04:05:24.000000000 +0000
 +++ src/libecap/common/memory.h
-@@ -4,14 +4,24 @@
+@@ -4,14 +4,12 @@
  #define LIBECAP__COMMON_MEMORY_H
  
  #include <libecap/common/libecap.h>
-+#include <ciso646>
-+
-+#if __cplusplus >= 201103L || defined(_LIBCPP_VERSION)
-+#include <functional>
-+#else
- #include <tr1/memory>
-+#endif
- 
- // TODO: add support for boost pointers if std::tr1 is not available
+-#include <tr1/memory>
+-
+-// TODO: add support for boost pointers if std::tr1 is not available
++#include <memory>
  
  namespace libecap {
--
-+#if __cplusplus >= 201103L || defined(_LIBCPP_VERSION)
+ 
+-using std::tr1::weak_ptr;
+-using std::tr1::shared_ptr;
 +using std::weak_ptr;
 +using std::shared_ptr;
-+#else
- using std::tr1::weak_ptr;
- using std::tr1::shared_ptr;
-+#endif
  
  } // namespace libecap
  
