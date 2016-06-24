b52b4f9f1ad10c5f858a5255020f1c05006bf1db
he <he@pkgsrc.org>
2016-06-24 11:33:59 +0000

    Pull in cups instead of cups15, so that it is possible to install this package where other packages are installed which depend on cups. Discussed with wiz@ Bump PKGREVISION.

--- net/freerdp/options.mk
+++ net/freerdp/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2014/08/11 23:09:46 wiz Exp $
+# $NetBSD: options.mk,v 1.6 2016/06/24 11:33:59 he Exp $
 #
 PKG_OPTIONS_VAR=	PKG_OPTIONS.freerdp
 PKG_SUPPORTED_OPTIONS=	alsa cups ffmpeg pcsc pulseaudio debug
@@ -25,7 +25,7 @@ CMAKE_ARGS+=	-DWITH_ALSA=OFF
 ### CUPS support
 ###
 .if !empty(PKG_OPTIONS:Mcups)
-.  include "../../print/cups15/buildlink3.mk"
+.  include "../../print/cups/buildlink3.mk"
 CMAKE_ARGS+=	-DWITH_CUPS=ON
 PLIST.cups=	yes
 .else
