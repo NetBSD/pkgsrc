239b30838a9f20f56008cd93d6f00150badab9d8
wiz <wiz@pkgsrc.org>
2016-06-30 18:05:55 +0000

    Update mame to 0.175.
    Get ready for your vacation and grab MAME 0.175!
    
    We're proud to say MAME now supports a number of previously unemulated
    prototypes, alternate versions of games, and unusual systems.
    Prototypes include the super-rare Konami Kyuukoukabakugekitai, Home
    Data's Mahjong Joshi Pro-wres Give Up 5 Byou Mae, and an early
    Japanese version of E.D.F.: Earth Defense Force. Atari Moto Frenzy,
    previously lacking protection emulation, is now fully playable.
    We've also added a number of gambling games, including some Flaming
    7's variants.
    
    Many more Game Boy peripherals are now supported, including real-time
    clocks, light sensors and tilt sensors. This makes several previously
    unsupported games fully playable.
    
    This release includes improvements to the Sega Master System and
    SG-1000 emulation, including better SG-1000 expansion slot support,
    and drivers with correct clock speeds for South American Master
    System variants.
    
    There's some big news in Sun emulation: all sun3 models will now
    POST, MAME has a SPARCv7 CPU core, and there has been substantial
    progress towards emulating the SPARCstation 1 (sun4c). Using unidasm
    (built with TOOLS=1) you can disassemble SPARCv7 SPARCv7 or SPARCv9
    code, incuding all VIS variants up to VIS-3B.
    
    As usual, there are many emulation improvements, including fixes
    for keyboard controls in some TRS-80 games, and better Seibu COP
    emulation in Legionnaire, Heated Barrel and Godzilla.
    
    In less visible changes, MAME's memory system got a nice cleanup
    exposing a number of existing issues which are now fixed, and the
    netlist-based discrete circuit simulation code has had a major
    overhaul with lots of performance improvements. There are a number
    of improvements to MAME's debugger modules in this release,
    particularly the imgui-based debugger.

---
+++ emulators/mame/patches/patch-3rdparty_bx_include_bx_thread.h
@@ -0,0 +1,14 @@
+$NetBSD: patch-3rdparty_bx_include_bx_thread.h,v 1.4 2016/06/30 18:05:55 wiz Exp $
+
+--- 3rdparty/bx/include/bx/thread.h.orig	2016-06-29 06:34:30.000000000 +0000
++++ 3rdparty/bx/include/bx/thread.h
+@@ -9,7 +9,9 @@
+ #if BX_PLATFORM_POSIX
+ #	include <pthread.h>
+ #	if BX_PLATFORM_BSD
++#          ifdef __FreeBSD__
+ #		include <pthread_np.h>
++#	   endif
+ #	endif
+ #	if defined(__GLIBC__) && !( (__GLIBC__ > 2) || ( (__GLIBC__ == 2) && (__GLIBC_MINOR__ >= 12) ) )
+ #		include <sys/prctl.h>
