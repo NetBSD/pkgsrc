0b34fa267de739576f9e252789db6aff077a2d9a
fhajny <fhajny>
2016-07-01 17:00:15 +0000

    Fix missed krb bl3. Ride previous commit revision.

--- devel/rudiments/buildlink3.mk
+++ devel/rudiments/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2016/07/01 16:02:12 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2016/07/01 17:00:15 fhajny Exp $
 #
 # This Makefile fragment is included by packages that use rudiments.
 #
@@ -15,6 +15,7 @@ BUILDLINK_ABI_DEPENDS.rudiments+=	rudiments>=0.55
 BUILDLINK_PKGSRCDIR.rudiments?=		../../devel/rudiments
 
 .include "../../devel/pcre/buildlink3.mk"
+.include "../../mk/krb5.buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
 .include "../../www/curl/buildlink3.mk"
 .endif # RUDIMENTS_BUILDLINK3_MK
