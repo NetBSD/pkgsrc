566476f429ab5ae9f325bac57d9aa0638dc4abfa
fhajny <fhajny@pkgsrc.org>
2016-07-01 17:02:38 +0000

    Update databases/sqlrelay to 0.65.0
    - SQL Relay now supports Kerberos encryption and authentication
    - SQL Relay now supports TLS/SSL encryption and authentication
    - Python 3 support
    - Node.js 4.x and higher are now correctly detected and supported.
    - Separate "database" and "proxied" Authentication Options
    - Various ODBC-related bugs have been fixed
    - A subtle bug that caused incompatibilities with the popular
      logrotate utility has been fixed
    - A long standing problem that occurred when using a
      listenertimeout with a threaded listener (the default) has been
      fixed.
    - After a graceful restart, Apache workers would get stuck in the
      Closing Connection state after running a PHP script that used
      SQL Relay. This is now fixed.
    
    See full changelog:
    
      http://software.firstworks.com/2016/03/sql-relay-0650-release-announcement.html

--- databases/sqlrelay/patches/patch-src_api_php_sql__relay.cpp
+++ databases/sqlrelay/patches/patch-src_api_php_sql__relay.cpp
@@ -1,13 +1,23 @@
-$NetBSD: patch-src_api_php_sql__relay.cpp,v 1.3 2015/07/05 12:52:39 joerg Exp $
+$NetBSD: patch-src_api_php_sql__relay.cpp,v 1.4 2016/07/01 17:02:39 fhajny Exp $
 
---- src/api/php/sql_relay.cpp.orig	2015-07-05 10:40:12.000000000 +0000
+--- src/api/php/sql_relay.cpp.orig	2016-03-13 19:20:25.000000000 +0000
 +++ src/api/php/sql_relay.cpp
-@@ -16,29 +16,7 @@
+@@ -24,39 +24,15 @@
  	#define ZTS 1
  #endif
  
 -extern "C" {
 -	#ifndef WIN32
+-
+-		// On some platforms (solaris), stdio.h must be included prior
+-		// to including math.h or FILE will get redefined.
+-		#include <stdio.h>
+-
+-		// php.h ultimately includes math.h and on some platforms,
+-		// __cplusplus to be defined when including it.  Manually
+-		// including it prior to including php.h solves this problem.
+-		#include <math.h>
+-
 -		#ifdef __cplusplus
 -			#undef __cplusplus
 -			#define cpluspluswasdefined
@@ -29,6 +39,14 @@ $NetBSD: patch-src_api_php_sql__relay.cpp,v 1.3 2015/07/05 12:52:39 joerg Exp $
 -		#endif
 -	#endif
 -}
++// On some platforms (solaris), stdio.h must be included prior
++// to including math.h or FILE will get redefined.
++#include <stdio.h>
++
++// php.h ultimately includes math.h and on some platforms,
++// __cplusplus to be defined when including it.  Manually
++// including it prior to including php.h solves this problem.
++#include <math.h>
 +#include <php.h>
  
  #include <config.h>
