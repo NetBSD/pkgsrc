dc573d275bb33365ff1f6ffcc291a4683ae2169f
jaapb <jaapb@pkgsrc.org>
2016-07-02 10:11:09 +0000

    Updated package to latest version, 1.5; also updated package to work with ocaml.mk structure and changed upstream to github.
    Changes:
    version 1.5
    * Improve various commands of csvtool avoiding loading the whole CSV file to memory.
    * As a consequence, one avoids some Stack_overflow exceptions for long files.
    
    version 1.4.2
    * Add the possibility to specify an output format to csvtool.
    
    version 1.4.1
    * Fix writing bug when excel_tricks is enabled.
    
    version 1.4
    * Optional discarding of spaces
    * Special handling of files with a header

--- textproc/ocaml-csv/buildlink3.mk
+++ textproc/ocaml-csv/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/07/03 10:53:06 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/07/02 10:11:09 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-csv
 
 .if !defined(OCAML_CSV_BUILDLINK3_MK)
 OCAML_CSV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-csv+=	ocaml-csv>=1.3.0
+BUILDLINK_API_DEPENDS.ocaml-csv+=	ocaml-csv>=1.5
 BUILDLINK_PKGSRCDIR.ocaml-csv?=	../../textproc/ocaml-csv
 
 .endif	# OCAML_CSV_BUILDLINK3_MK
