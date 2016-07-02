beaae91ecf0b8addd9e18e47d213d40bcabaa6fa
wiz <wiz@pkgsrc.org>
2016-07-02 15:16:11 +0000

    Add default-on option for libusb1 support.
    Based on patch by Jonathan Schleifer in PR 51303, with some
    changes by myself.

--- security/gnupg21/options.mk
+++ security/gnupg21/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2015/10/08 13:35:39 fhajny Exp $
+# $NetBSD: options.mk,v 1.2 2016/07/02 15:16:11 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnupg21
-PKG_SUPPORTED_OPTIONS=	ldap
-PKG_SUGGESTED_OPTIONS=	# empty
+PKG_SUPPORTED_OPTIONS=	ldap libusb-1
+PKG_SUGGESTED_OPTIONS=	libusb-1
 
 .include "../../mk/bsd.options.mk"
 
@@ -13,4 +13,13 @@ PLIST.ldap=		yes
 .include "../../databases/openldap-client/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-ldap
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibusb-1)
+# gnupg unfortunately doesn't use pkg-config
+CONFIGURE_ARGS+=	CPPFLAGS="${CPPFLAGS} -I${BUILDLINK_PREFIX.libusb1}/include/libusb-1.0"
+CONFIGURE_ARGS+=	LDFLAGS="${LDFLAGS} ${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.libusb1}/lib -L${BUILDLINK_PREFIX.libusb1}/lib"
+.include "../../devel/libusb1/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-ccid-driver
 .endif
