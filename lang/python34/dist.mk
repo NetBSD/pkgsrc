9fd4e60838ffc7af6b79e17b6670d6f1c849b413
adam <adam@pkgsrc.org>
2016-07-02 15:23:33 +0000

    Changes 3.4.5: Core and Builtins Issue 26478: Fix semantic bugs when using binary operators with dictionary views and tuples. Issue 26171: Fix possible integer overflow and heap corruption in zipimporter.get_data(). Library Issue 26556: Update expat to 2.1.1, fixes CVE-2015-1283. Fix TLS stripping vulnerability in smptlib, CVE-2016-0772. Reported by Team Oststrom Issue 25939: On Windows open the cert store readonly in ssl.enum_certificates. Issue 26012: Don’t traverse into symlinks for ** pattern in pathlib.Path.[r]glob(). Issue 24120: Ignore PermissionError when traversing a tree with pathlib.Path.[r]glob(). Patch by Ulrich Petri. Skip getaddrinfo if host is already resolved. Patch by A. Jesse Jiryu Davis. Add asyncio.timeout() context manager. Issue 26050: Add asyncio.StreamReader.readuntil() method. Patch by Марк Коренберг. Tests Issue 25940: Changed test_ssl to use self-signed.pythontest.net. This avoids relying on svn.python.org, which recently changed root certificate.

--- lang/python34/dist.mk
+++ lang/python34/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.5 2016/05/13 16:35:03 adam Exp $
+# $NetBSD: dist.mk,v 1.6 2016/07/02 15:23:33 adam Exp $
 
-PY_DISTVERSION=	3.4.4
+PY_DISTVERSION=	3.4.5
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python34/distinfo
