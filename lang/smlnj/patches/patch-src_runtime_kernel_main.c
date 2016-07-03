513a791ecb79473aa7e92936c17f2772897db615
dholland <dholland@pkgsrc.org>
2016-07-03 19:35:13 +0000

    Patch up some compiler warnings in the hope of fixing the crashy build. No such luck though...

---
+++ lang/smlnj/patches/patch-src_runtime_kernel_main.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-src_runtime_kernel_main.c,v 1.1 2016/07/03 19:35:13 dholland Exp $
+
+Use standard headers.
+
+--- src/runtime/kernel/main.c~	2005-05-05 16:25:12.000000000 +0000
++++ src/runtime/kernel/main.c
+@@ -6,6 +6,7 @@
+  */
+ 
+ #include <stdio.h>
++#include <string.h>
+ #include <ctype.h>
+ #include "ml-base.h"
+ #include "ml-options.h"
