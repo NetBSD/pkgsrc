f67f5bcec209ce80fea73ce4cbc7e99b7bf9fbd4
adam <adam@pkgsrc.org>
2016-07-07 08:18:15 +0000

    Changes 4.22: - when epoll detects unremovable fds in the fd set, rebuild   only the epoll descriptor, not the signal pipe, to avoid   SIGPIPE in ev_async_send. This doesn't solve it on fork,   so document what needs to be done in ev_loop_fork - remove superfluous sys/timeb.h include on win32 - updated libecb.

--- devel/libev/buildlink3.mk
+++ devel/libev/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2010/12/02 15:47:12 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2016/07/07 08:18:15 adam Exp $
 
 BUILDLINK_TREE+=	libev
 
 .if !defined(LIBEV_BUILDLINK3_MK)
 LIBEV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libev+=libev>=3.9nb1
-BUILDLINK_ABI_DEPENDS.libev+=libev>=4.01
+BUILDLINK_API_DEPENDS.libev+=	libev>=3.9nb1
+BUILDLINK_ABI_DEPENDS.libev+=	libev>=4.01
 BUILDLINK_PKGSRCDIR.libev?=	../../devel/libev
 
 BUILDLINK_LIBDIRS.libev+=	lib/ev
