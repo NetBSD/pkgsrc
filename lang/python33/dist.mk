78655ec2f9986fa3e2faea541378d7e50acbcc7c
wiz <wiz@pkgsrc.org>
2016-07-09 13:04:40 +0000

    Remove python33.

--- lang/python33/dist.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: dist.mk,v 1.6 2015/01/01 21:39:45 he Exp $
-
-PY_DISTVERSION=	3.3.6
-DISTNAME=	Python-${PY_DISTVERSION}
-EXTRACT_SUFX=	.tar.xz
-DISTINFO_FILE=	${.CURDIR}/../../lang/python33/distinfo
-PATCHDIR=	${.CURDIR}/../../lang/python33/patches
-MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION}/
