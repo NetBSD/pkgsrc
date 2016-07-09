31bb79a9fefebf086d070719f5a81252a9dfcc73
agc <agc@pkgsrc.org>
2016-07-09 17:44:44 +0000

    Update netpgpverify and libnetpgpverify to 20160709
    + free resources associated with the cursor when verifying,
    gets rid of 2 more loss records under valgrind.

--- security/netpgpverify/files/verify.h
+++ security/netpgpverify/files/verify.h
@@ -23,9 +23,9 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #ifndef NETPGP_VERIFY_H_
-#define NETPGP_VERIFY_H_	20160708
+#define NETPGP_VERIFY_H_	20160709
 
-#define NETPGPVERIFY_VERSION	"netpgpverify portable 20160708"
+#define NETPGPVERIFY_VERSION	"netpgpverify portable 20160709"
 
 #include <sys/types.h>
 
