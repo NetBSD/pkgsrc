21bd0511f829aa7f655b24e9837c2a5f2b9927d2
rillig <rillig@pkgsrc.org>
2016-07-09 11:39:08 +0000

    Fixed some pkglint warnings.

--- audio/freeswitch-music/options.mk
+++ audio/freeswitch-music/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2015/12/29 04:54:36 dholland Exp $
+# $NetBSD: options.mk,v 1.5 2016/07/09 11:41:57 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.freeswitch
 PKG_SUPPORTED_OPTIONS=	freeswitch-08k freeswitch-16k freeswitch-32k freeswitch-48k
@@ -9,33 +9,33 @@ PKG_SUGGESTED_OPTIONS=	freeswitch-08k freeswitch-16k
 .if !empty(PKG_OPTIONS:Mfreeswitch-08k)
 DISTFILES+=	freeswitch-sounds-music-8000-1.0.8.tar.gz
 INSTALLATION_DIRS+= share/freeswitch/sounds/music/8000
-SOUND_DIRS+= 8000
+SOUND_DIRS+=	8000
 .endif
 
 .if !empty(PKG_OPTIONS:Mfreeswitch-16k)
 DISTFILES+=	freeswitch-sounds-music-16000-1.0.8.tar.gz
-SOUND_DIRS+= 16000
+SOUND_DIRS+=	16000
 INSTALLATION_DIRS+= share/freeswitch/sounds/music/16000
 .endif
 
 .if !empty(PKG_OPTIONS:Mfreeswitch-32k)
 DISTFILES+=	freeswitch-sounds-music-32000-1.0.8.tar.gz
-SOUND_DIRS+= 32000
+SOUND_DIRS+=	32000
 INSTALLATION_DIRS+= share/freeswitch/sounds/music/32000
 .endif
 
 .if !empty(PKG_OPTIONS:Mfreeswitch-48k)
 DISTFILES+=	freeswitch-sounds-music-48000-1.0.8.tar.gz
-SOUND_DIRS+= 48000
+SOUND_DIRS+=	48000
 INSTALLATION_DIRS+= share/freeswitch/sounds/music/48000
 .endif
 
 .for opt in ${PKG_SUPPORTED_OPTIONS}
-.if !empty(PKG_OPTIONS:M${opt})
-PLIST_SUBST+= ${opt}=""
-.else
-PLIST_SUBST+= ${opt}="@comment "
-.endif
+.  if !empty(PKG_OPTIONS:M${opt})
+PLIST_SUBST+=	${opt}=""
+.  else
+PLIST_SUBST+=	${opt}="@comment "
+.  endif
 .endfor
 
 PLIST_VARS+=	${PKG_SUPPORTED_OPTIONS}
