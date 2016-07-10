dc6d005e2f5e07f467a2ffa46b7769b35157f247
mef <mef>
2016-07-10 12:06:21 +0000

    Updated audio/sptk 3.6 to 3.9 ----------------------------- Version 3.9:  * support full covariance for 'gmmp' command.  * add -B, -c1 and -c2 option to 'gmmp' command to specify block size and structure of covariance matrix.  * change temporary file path for csh.  * bug fixes.
    Version 3.8:
     * add 'c2ndps' command for transform from cepstrum to negative derivative of phrase spectrum.
     * add 'mgclsp2sp' command for transform from LSP to spectrum.
     * add 'ndps2c' command for transform from negative derivative of phrase spectrum to cepstrum.
     * add 'wavjoin' command to save stereo wav file.
     * add 'wavsplit' command to load stereo wav file.
     * add -V option to 'dtw' command for loading DTW results.
     * add -e option to calculate inverse matrix stably for 'delta' and 'vc' commands.
     * support MAP estimation in 'gmm' command.
     * replace -i option to -q option for 'lsp2lpc,' 'lspcheck,' and 'mgclsp2mgc' commands.
     * change -c option of 'mlsacheck' command.
     * update swipe and hts_engine API.
     * bug fixes.
    
    Version 3.7:
     * add 'vc' command to perform GMM-based voice conversion.
     * add 'lsp2sp' command to transform LSP coefficients to spectrum.
     * add -R option to 'mlsacheck' command to specify threshold value.
     * add -f option to 'gmmp' command to specify full covariance matrix.
     * add -M, -c1 and -c2 option to 'gmm' command to specify block size and structure of covariance matrix.
     * add -n option to 'delta' command to specify order of regression polynomial.
     * add -magic and -MAGIC option to 'rmse' command to delete and substitute magic number.
     * add -n and -t option to 'rmse' command to specify order and number of vector, respectively.
     * add -POWX, -LOGX option, and new constant (semitone) to 'sopr' command.
     * add -l option to 'decimate' and 'interpolate' command to specify length of vector.
     * add -g and -G option to 'lspcheck' command to check value of gain.
     * add -L option to 'lspcheck' command to specify log gain.
     * add -n option to 'mgc2mgclsp' command to specify split number of unit circle.
     * add -p and -d to 'mgc2mgclsp' command to specify maximum number and end condition of interpolation.
     * add -x 16, 22, 24 and 48 option of 'glogsp' and 'grlogsp' command to specify more x-axis scales.
     * add filter coefficients for 7:4 down-sampling, 3:5 and 7:8 up-sampling.
     * unify option to specify log gain into -L option.
     * fix -c option of 'mlsacheck' command to specify the type of modification of filter coefficients.
     * bug fixes.

--- audio/sptk/patches/patch-bin_pitch_swipe_swipe.c
+++ audio/sptk/patches/patch-bin_pitch_swipe_swipe.c
@@ -1,10 +1,11 @@
-$NetBSD: patch-bin_pitch_swipe_swipe.c,v 1.1 2014/02/22 14:19:36 wiedi Exp $
+$NetBSD: patch-bin_pitch_swipe_swipe.c,v 1.2 2016/07/10 12:06:21 mef Exp $
 
 include values.h for M_PI on SunOS
---- bin/pitch/swipe/swipe.c.orig	2012-12-22 12:25:38.000000000 +0000
-+++ bin/pitch/swipe/swipe.c
-@@ -80,6 +80,9 @@
- #endif
+
+--- bin/pitch/swipe/swipe.c.orig	2015-12-14 14:38:40.000000000 +0900
++++ bin/pitch/swipe/swipe.c	2016-07-10 20:52:54.000000000 +0900
+@@ -70,6 +70,9 @@
+ #define VNUM    1.5 // current version
  
  #include <math.h>
 +#if defined(__sun)
