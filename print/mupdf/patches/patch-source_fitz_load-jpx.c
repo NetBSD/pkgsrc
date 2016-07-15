9d74b48c281c51abfb86dbe781736dd785097c23
leot <leot@pkgsrc.org>
2016-07-15 12:32:33 +0000

    Fix linking with openjpeg-2.1.1
    Restrict OPJ_STATIC definition to Windows (as the comment suggests).
    This avoid linking errors with openjpeg-2.1.1.
    
    Bump PKGREVISION

--- print/mupdf/patches/patch-source_fitz_load-jpx.c
+++ print/mupdf/patches/patch-source_fitz_load-jpx.c
@@ -1,11 +1,22 @@
-$NetBSD: patch-source_fitz_load-jpx.c,v 1.2 2016/02/04 11:50:24 leot Exp $
+$NetBSD: patch-source_fitz_load-jpx.c,v 1.3 2016/07/15 12:32:33 leot Exp $
 
-Fix build with openjpeg-2.1.
-I have misgivings about the code though.
+ o Restrict OPJ_STATIC to Windows (avoid linking errors due to hidden symbols)
+ o Fix build with openjpeg-2.1.
+   I have misgivings about the code though.
 
---- source/fitz/load-jpx.c.orig	2015-11-10 16:19:51.000000000 +0000
+--- source/fitz/load-jpx.c.orig	2016-04-21 11:14:32.000000000 +0000
 +++ source/fitz/load-jpx.c
-@@ -117,7 +117,8 @@ fz_load_jpx(fz_context *ctx, unsigned ch
+@@ -3,7 +3,9 @@
+ /* Without the definition of OPJ_STATIC, compilation fails on windows
+  * due to the use of __stdcall. We believe it is required on some
+  * linux toolchains too. */
++#ifdef _WIN32
+ #define OPJ_STATIC
++#endif
+ #ifndef _MSC_VER
+ #define OPJ_HAVE_STDINT_H
+ #endif
+@@ -117,7 +119,8 @@ fz_load_jpx(fz_context *ctx, unsigned ch
  	opj_stream_set_read_function(stream, fz_opj_stream_read);
  	opj_stream_set_skip_function(stream, fz_opj_stream_skip);
  	opj_stream_set_seek_function(stream, fz_opj_stream_seek);
