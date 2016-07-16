f6860d42de9e5d1f21515c3ada896ee9d9a2adcd
wiz <wiz@pkgsrc.org>
2016-07-16 20:05:57 +0000

    Updated libctl to 3.2.2.
    Switch to guile20.
    
    libctl 3.2.2
    
      * Bug fix to interpolate-uniform for guile 1.8+.

--- devel/libctl/buildlink3.mk
+++ devel/libctl/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/01/23 21:43:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/07/16 20:05:57 wiz Exp $
 
 BUILDLINK_TREE+=	libctl
 
 .if !defined(LIBCTL_BUILDLINK3_MK)
 LIBCTL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libctl+=	libctl>=3.2.1
+BUILDLINK_API_DEPENDS.libctl+=	libctl>=3.2.2
 BUILDLINK_PKGSRCDIR.libctl?=	../../devel/libctl
 
-.include "../../lang/guile/buildlink3.mk"
+.include "../../lang/guile20/buildlink3.mk"
 .endif	# LIBCTL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libctl
