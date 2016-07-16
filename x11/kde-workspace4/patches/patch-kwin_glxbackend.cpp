1716e49a8521927ee245e6ac501055872d617907
markd <markd@pkgsrc.org>
2016-07-16 00:47:00 +0000

    gcc6 fixes

--- x11/kde-workspace4/patches/patch-kwin_glxbackend.cpp
+++ x11/kde-workspace4/patches/patch-kwin_glxbackend.cpp
@@ -1,10 +1,20 @@
-$NetBSD: patch-kwin_glxbackend.cpp,v 1.1 2013/12/12 11:09:28 markd Exp $
+$NetBSD: patch-kwin_glxbackend.cpp,v 1.2 2016/07/16 00:47:00 markd Exp $
 
+fixes for gcc6 from opensuse
 Don't fail build with old MesaLib 
 
---- kwin/glxbackend.cpp.orig	2013-09-26 19:18:26.000000000 +0000
+--- kwin/glxbackend.cpp.orig	2015-08-12 07:03:15.000000000 +0000
 +++ kwin/glxbackend.cpp
-@@ -152,6 +152,7 @@ bool GlxBackend::initRenderingContext()
+@@ -45,7 +45,7 @@ GlxBackend::GlxBackend()
+     , window(None)
+     , fbconfig(NULL)
+     , glxWindow(None)
+-    , ctx(None)
++    , ctx(NULL)
+     , m_bufferAge(0)
+     , haveSwapInterval(false)
+ {
+@@ -164,6 +164,7 @@ bool GlxBackend::initRenderingContext()
  {
      bool direct = options->isGlDirect();
  
@@ -12,7 +22,7 @@ Don't fail build with old MesaLib
      // Use glXCreateContextAttribsARB() when it's available
      if (glXCreateContextAttribsARB) {
          const int attribs_31_core_robustness[] = {
-@@ -197,6 +198,7 @@ bool GlxBackend::initRenderingContext()
+@@ -209,6 +210,7 @@ bool GlxBackend::initRenderingContext()
          if (!ctx)
              ctx = glXCreateContextAttribsARB(display(), fbconfig, 0, direct, attribs_legacy);
      }
