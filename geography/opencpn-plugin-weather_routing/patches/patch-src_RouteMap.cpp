986f67f5697ac9c2931f2a9c7d1f429298bbac89
bouyer <bouyer@pkgsrc.org>
2016-07-18 14:23:46 +0000

    Update to 20160716. Changes since previous: bug fixes, improved error messages, updates for newer draw plugin.

--- geography/opencpn-plugin-weather_routing/patches/patch-src_RouteMap.cpp
+++ geography/opencpn-plugin-weather_routing/patches/patch-src_RouteMap.cpp
@@ -1,16 +1,16 @@
-$NetBSD: patch-src_RouteMap.cpp,v 1.1 2016/06/16 16:03:55 bouyer Exp $
+$NetBSD: patch-src_RouteMap.cpp,v 1.2 2016/07/18 14:23:46 bouyer Exp $
 
---- src/RouteMap.cpp.orig	2016-06-13 18:14:41.000000000 +0200
-+++ src/RouteMap.cpp	2016-06-13 18:17:23.000000000 +0200
-@@ -550,7 +550,7 @@
+--- src/RouteMap.cpp.orig	2016-07-16 16:02:32.000000000 +0200
++++ src/RouteMap.cpp	2016-07-18 15:51:20.000000000 +0200
+@@ -546,7 +546,7 @@
          VB = polar.Speed(H, VW);
  
      /* failed to determine speed.. */
 -    if(isnan(B) || isnan(VB)) {
 +    if(std::isnan(B) || std::isnan(VB)) {
+         // when does this hit??
+         printf("polar failed bad! %f %f %f %f\n", W, VW, B, VB);
          configuration.polar_failed = true;
-         return false; //B = VB = 0;
-     }
 @@ -662,7 +662,7 @@
          B = W + H; /* rotated relative to true wind */
  
@@ -20,7 +20,7 @@ $NetBSD: patch-src_RouteMap.cpp,v 1.1 2016/06/16 16:03:55 bouyer Exp $
              double bearing3 = heading_resolve(B);
              if((bearing1 > bearing2 && bearing3 > bearing2 && bearing3 < bearing1) ||
                 (bearing1 < bearing2 && (bearing3 > bearing2 || bearing3 < bearing1))) {
-@@ -2092,7 +2092,7 @@
+@@ -2089,7 +2089,7 @@
  
          /* did we tack thru the wind? apply penalty */
          bool tacked = false;
@@ -29,7 +29,7 @@ $NetBSD: patch-src_RouteMap.cpp,v 1.1 2016/06/16 16:03:55 bouyer Exp $
              tacked = true;
              dt += configuration.TackingTime;
  #if 0        
-@@ -2101,7 +2101,7 @@
+@@ -2098,7 +2098,7 @@
  #endif
          }
  
