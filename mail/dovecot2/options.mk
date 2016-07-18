f13366c78663b7f43f555581941c746627966d78
fhajny <fhajny@pkgsrc.org>
2016-07-18 15:03:05 +0000

    Split off dovecot2-{gssapi,ldap,mysql,pgsql,sqlite} as separate packages, remove respective options.mk parts.

--- mail/dovecot2/options.mk
+++ mail/dovecot2/options.mk
@@ -1,15 +1,15 @@
-# $NetBSD: options.mk,v 1.9 2016/05/03 12:34:27 taca Exp $
+# $NetBSD: options.mk,v 1.10 2016/07/18 15:03:05 fhajny Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dovecot
-PKG_SUPPORTED_OPTIONS=	gssapi kqueue ldap mysql pam pgsql sqlite tcpwrappers
+PKG_SUPPORTED_OPTIONS=	kqueue pam tcpwrappers
 PKG_OPTIONS_OPTIONAL_GROUPS= ssl
 PKG_OPTIONS_GROUP.ssl=	gnutls ssl
-PKG_SUGGESTED_OPTIONS=	pam sqlite ssl tcpwrappers
+PKG_SUGGESTED_OPTIONS=	pam ssl tcpwrappers
 
 .if defined(PKG_HAVE_KQUEUE)
 PKG_SUGGESTED_OPTIONS+=	kqueue
 .endif
-PLIST_VARS+=		ldap ssl tcpwrappers
+PLIST_VARS+=		ssl tcpwrappers
 
 .include "../../mk/bsd.options.mk"
 
@@ -31,32 +31,6 @@ CONFIGURE_ARGS+=	--without-ssl
 .endif
 
 ###
-### MySQL support
-###
-.if !empty(PKG_OPTIONS:Mmysql)
-CONFIGURE_ARGS+=	--with-mysql
-.  include "../../mk/mysql.buildlink3.mk"
-.endif
-
-###
-### PostgreSQL support
-###
-.if !empty(PKG_OPTIONS:Mpgsql)
-CONFIGURE_ARGS+=	--with-pgsql
-CPPFLAGS+=		-I${BUILDLINK_DIR}/include/pgsql
-.  include "../../mk/pgsql.buildlink3.mk"
-.endif
-
-###
-### LDAP directory support
-###
-.if !empty(PKG_OPTIONS:Mldap)
-CONFIGURE_ARGS+=	--with-ldap
-.  include "../../databases/openldap-client/buildlink3.mk"
-PLIST.ldap=	yes
-.endif
-
-###
 ### PAM support
 ###
 .if !empty(PKG_OPTIONS:Mpam)
@@ -67,14 +41,6 @@ CONFIGURE_ARGS+=	--without-pam
 .endif
 
 ###
-### SQLite support
-###
-.if !empty(PKG_OPTIONS:Msqlite)
-CONFIGURE_ARGS+=	--with-sqlite
-.  include "../../databases/sqlite3/buildlink3.mk"
-.endif
-
-###
 ### kqueue support
 ###
 .if !empty(PKG_OPTIONS:Mkqueue)
@@ -82,16 +48,6 @@ CONFIGURE_ARGS+=	--with-ioloop=kqueue
 CONFIGURE_ARGS+=	--with-notify=kqueue
 .else
 # use the defaults
-.endif
-
-###
-### GSSAPI support
-###
-.if !empty(PKG_OPTIONS:Mgssapi)
-CONFIGURE_ARGS+=	--with-gssapi
-.  include "../../mk/krb5.buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-gssapi
 .endif
 
 ###
