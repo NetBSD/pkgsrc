a612071aea2703c64dcd37c58eb253b392373def
jperkin <jperkin@pkgsrc.org>
2016-07-19 12:46:06 +0000

    Fix build on SunOS.

--- devel/ptlib/patches/patch-src-unix-tlibthrd.cxx
+++ devel/ptlib/patches/patch-src-unix-tlibthrd.cxx
@@ -1,18 +1,19 @@
-$NetBSD: patch-src-unix-tlibthrd.cxx,v 1.2 2012/12/01 15:28:03 darcy Exp $
+$NetBSD: patch-src-unix-tlibthrd.cxx,v 1.3 2016/07/19 12:46:06 jperkin Exp $
 
 - deal with lack of pthread_yield on NetBSD
 - XXX needs other platforms to be checked.
 
---- src/ptlib/unix/tlibthrd.cxx.orig    2012-08-23 02:12:27.000000000 +0000
+--- src/ptlib/unix/tlibthrd.cxx.orig	2012-08-23 02:12:27.000000000 +0000
 +++ src/ptlib/unix/tlibthrd.cxx
-@@ -1531,6 +1531,10 @@ PTimedMutex::~PTimedMutex()
+@@ -1531,7 +1531,11 @@ PTimedMutex::~PTimedMutex()
      for (PINDEX i = 0; i < 100; ++i) {
        if ((result = pthread_mutex_destroy(&m_mutex)) != EBUSY)
          break;
-+#if defined(P_NETBSD)
++#if defined(P_NETBSD) || defined(P_SOLARIS)
 +      sched_yield();
 +#else
        pthread_yield();
 +#endif
      }
    }
+ 
