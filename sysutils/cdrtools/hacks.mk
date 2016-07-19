3968c3b57a0d86450ba2fe422405a1af29aa4532
tron <tron@pkgsrc.org>
2016-07-19 07:33:24 +0000

    Check whether "ABI" is defined just to be sure.

--- sysutils/cdrtools/hacks.mk
+++ sysutils/cdrtools/hacks.mk
@@ -1,8 +1,8 @@
-# $NetBSD: hacks.mk,v 1.1 2016/07/19 07:30:27 tron Exp $
+# $NetBSD: hacks.mk,v 1.2 2016/07/19 07:33:24 tron Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
-.if ${OPSYS} == "Darwin" && ${ABI} == "64"
+.if ${OPSYS} == "Darwin" && defined(ABI) && ${ABI} == "64"
 #
 # PR pkg/51257: "sysutils/cdrtools" fails to build under Mac OS X 64bit
 #               when cwrappers are used
