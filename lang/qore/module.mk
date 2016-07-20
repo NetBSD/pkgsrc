9314f3ea1e6c2e84d3bee1f2b5c717799f97e9af
nros <nros@pkgsrc.org>
2016-07-20 11:58:40 +0000

    Update Qore to version 0.8.12.
    Qore 0.8.12 is a major new release with many new features and hundreds of bug
    fixes as well as packaging fixes. Highlights include:
    
    * The most significant innovation in this release comes in the form of support
      for Prompt Collection
    
    * Support for Bulk DML (large volume SQL operations in one server round-trip)
      in the Mapper, TableMapper, SqlUtil and the new BulkSqlUtil modules
    
    * Standardized function naming convention, many significant design and
      implementation fixes and improvements
    
    * Numerous new functions, methods, constants, operators, and user modules
    
    * Hundreds of bug fixes

--- lang/qore/module.mk
+++ lang/qore/module.mk
@@ -1,4 +1,4 @@
-# $NetBSD: module.mk,v 1.1 2014/12/30 15:39:51 wiz Exp $
+# $NetBSD: module.mk,v 1.2 2016/07/20 11:58:40 nros Exp $
 #
 # This file should be included in the makefile of packages that install
 # Qore modules.
@@ -20,13 +20,20 @@
 #			 modules that uses the latest module api
 #			 version.
 #
+# QORE_USER_MODULE_DIR	 The directory where users modules(qm files) are
+#			 installed relative to ${PREFIX}.
+#
+#
+#
 
 qore-module: post-install .PHONY
 qore-version=qore --short-version | ${SED} -e s/-.*//
 qore-module-api=qore --module-api
 qore-module-dir=qore --module-dir
 qore-latest-module-api=qore --latest-module-api
+qore-user-module-dir=qore --user-module-dir
 PLIST_SUBST+=QORE_VERSION="${qore-version:sh}"
 PLIST_SUBST+=QORE_MODULE_API="${qore-module-api:sh}"
 PLIST_SUBST+=QORE_MODULE_DIR="${qore-module-dir:sh:C,^${PREFIX}/,,W}"
 PLIST_SUBST+=QORE_LATEST_MODULE_API="${qore-latest-module-api:sh}"
+PLIST_SUBST+=QORE_USER_MODULE_DIR="${qore-user-module-dir:sh:C,^${PREFIX}/,,W}"
