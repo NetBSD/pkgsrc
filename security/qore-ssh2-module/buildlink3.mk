f7ef70c54afb76f9ac946aa1236c398fec0e59d9
nros <nros>
2016-07-20 14:54:57 +0000

    Update qore-ssh2-module to version 1.0.
    Patches have been applied upstream.
    
    Release notes:
    
    * fixed crashing bugs handling errors and handle scope in the SFTPClient class
    * added the SftpPoller user module
    * force socket disconnect in case of a timeout error when trying to
      close a file descriptor
    * socket performance instrumentation supported in the SFTPClient class
    * user modules moved to top-level qore module directory from
      version-specific module directory since they are valid for multiple
      versions of qore
    * fixed a bug where a crash would result when attempting a connection and
      libssh2 would not return any user authentication methods
    * implemented an automatic disconnection when timeouts occur to avoid
      dead connections
    * fixed crashing bugs in the SFTPClient class handling disconnect events
      when an sftp handle was open; the handle must be closed before the
      socket connection is closed or a crash will result
    * implemented the SFTPClient::retrieveFile() and SFTPClient::transferFile()
      methods
    * ported the SFTPClient class tests to QUnit and added tests for the
      new methods
    * ported test/sftp-poller.q to QUnit
    * fixed a bug in socket handling related to asyncronous socket event polling
      and select(2) and lack of socket descriptor bounds checking
      (issue 714)
    * requires Qore 0.8.12+ to build (uses the new QoreValue API)

--- security/qore-ssh2-module/buildlink3.mk
+++ security/qore-ssh2-module/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/03/05 11:27:55 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/07/20 14:54:57 nros Exp $
 
 BUILDLINK_TREE+=	qore-ssh2-module
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qore-ssh2-module
 QORE_SSH2_MODULE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qore-ssh2-module+=	qore-ssh2-module>=0.9.9
-BUILDLINK_ABI_DEPENDS.qore-ssh2-module?=	qore-ssh2-module>=0.9.9nb7
+BUILDLINK_ABI_DEPENDS.qore-ssh2-module?=	qore-ssh2-module>=1.0
 BUILDLINK_PKGSRCDIR.qore-ssh2-module?=	../../security/qore-ssh2-module
 
 .include "../../lang/qore/buildlink3.mk"
