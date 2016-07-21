073b2d0f9b85c21a6730eb1910cebf8cc81d2514
wiz <wiz@pkgsrc.org>
2016-07-21 12:49:41 +0000

    Updated libopenmpt to 0.2.6611.
    Add pulseaudio option, default on to follow upstream.
    
    libopenmpt 0.2-beta18 (2016-07-11)
    
    [Change] openmpt123: Add PulseAudio output support. Autotools and
      Makefile build systems now depend on libpulse and libpulse-simple
      by default. Disable with --without-pulseaudio or NO_PULSEAUDIO=1
      respectively. When enabled, PulseAudio will be the default output
      driver,
    [Change] xmp-openmpt: Settings are now stored in xmplay.ini like
      with every other plugin.
    [Regression] openmpt123: Support for FLAC < 1.3.0 has been removed.
      FLAC before 1.3.0 is broken beyond repair as it provides assert.h
      in the include path.
    [Bug] Generated pkg-config file libopenmpt.pc by both Makefile and
      Autotools build systems was totally broken.
    [Bug] libopenmpt no longer uses the non-thread-safe global std::rand()
      function.
    [Bug] Sample loops in GDM modules did not work when using Emscripten.
    [Bug] XM and MO3 loaders could crash due to unaligned memory accesses.
    [Bug] Fixed incorrect handling of custom MPTM tunings on big endian
      platforms.
    [Bug] Fixed various problems found with clang 3.8 static analyzer,
      address sanitizer and undefined behaviour sanitizer.
    [Bug] File header probing functionality was broken for most formats.
    [Bug] With non-seekable streams, the entire file was almost always
      cached even if it was not of any supported module type.
      Seeking in allsubsongs-mode now works correctly.
    openmpt123: Added subsong support.
    Various playback fixes for 669, IT, MT2 and MTM files.
    Some MOD files with more than 128 patterns (e.g. NIETNU.MOD) were
      not loaded correctly.
    A new example libopenmpt_example_c_probe has been added which
      demonstrates the usage and flexibility of openmpt_could_open_propability()
      in the C API under various constraints.

--- audio/libopenmpt/options.mk
+++ audio/libopenmpt/options.mk
@@ -1,13 +1,19 @@
-# $NetBSD: options.mk,v 1.1 2014/08/26 10:44:15 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2016/07/21 12:49:41 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libopenmpt
-PKG_SUPPORTED_OPTIONS=	flac sndfile wavpack
-PKG_SUGGESTED_OPTIONS=	sndfile
+PKG_SUPPORTED_OPTIONS=	flac pulseaudio sndfile wavpack
+PKG_SUGGESTED_OPTIONS=	pulseaudio sndfile
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mflac)
 .include "../../audio/flac/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+.include "../../audio/pulseaudio/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-pulseaudio
 .endif
 
 .if !empty(PKG_OPTIONS:Msndfile)
