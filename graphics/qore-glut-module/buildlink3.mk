e8fd32a1ce38da97bf9cf39a91f7e8527e7e337d
nros <nros>
2016-07-22 13:16:39 +0000

    Add patch to adapt for qore api change. Revbump because of lang/qore version update.

--- graphics/qore-glut-module/buildlink3.mk
+++ graphics/qore-glut-module/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/03/05 11:27:49 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/07/22 13:16:39 nros Exp $
 #
 
 BUILDLINK_TREE+=	qore-glut-module
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	qore-glut-module
 QORE_GLUT_MODULE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qore-glut-module+=	qore-glut-module>=0.0.3
-BUILDLINK_ABI_DEPENDS.qore-glut-module?=	qore-glut-module>=0.0.3nb5
+BUILDLINK_ABI_DEPENDS.qore-glut-module?=	qore-glut-module>=0.0.3nb6
 BUILDLINK_PKGSRCDIR.qore-glut-module?=	../../graphics/qore-glut-module
 
 .include "../../graphics/qore-opengl-module/buildlink3.mk"
