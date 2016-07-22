e92b0af979f47ea340307cef132c41d3adf3a2bc
nros <nros@pkgsrc.org>
2016-07-22 12:39:22 +0000

    Update qore-xml-module to version 0.5.
    Patches have been applied upstream.
    
    release notes:
    This release of the yaml module includes bug fixes and major new features
    such as:
    
    * Support for the DataStream Protocol allowing point-to-point data streaming
      over HTTP using YAML-serialized chunks of data;
      the following user modules support this:
       - DataStreamClient
       - DataStreamRequestHandler
       - DataStreamUtil
    * new function naming convention

--- textproc/qore-yaml-module/buildlink3.mk
+++ textproc/qore-yaml-module/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/03/05 11:27:56 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/07/22 12:39:22 nros Exp $
 #
 
 BUILDLINK_TREE+=	qore-yaml-module
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	qore-yaml-module
 QORE_YAML_MODULE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qore-yaml-module+=	qore-yaml-module>=0.4
-BUILDLINK_ABI_DEPENDS.qore-yaml-module?=	qore-yaml-module>=0.4nb5
+BUILDLINK_ABI_DEPENDS.qore-yaml-module?=	qore-yaml-module>=0.5
 BUILDLINK_PKGSRCDIR.qore-yaml-module?=	../../textproc/qore-yaml-module
 
 
