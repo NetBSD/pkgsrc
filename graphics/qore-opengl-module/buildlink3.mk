84a5f606d1d2eea813563fba14ed7353e6cdbbe0
nros <nros@pkgsrc.org>
2016-07-22 13:04:04 +0000

    Revbump because of lang/qore version update.

--- graphics/qore-opengl-module/buildlink3.mk
+++ graphics/qore-opengl-module/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/03/05 11:27:50 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/07/22 13:12:17 nros Exp $
 #
 
 BUILDLINK_TREE+=	qore-opengl-module
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	qore-opengl-module
 QORE_OPENGL_MODULE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qore-opengl-module+=	qore-opengl-module>=0.0.3
-BUILDLINK_ABI_DEPENDS.qore-opengl-module?=	qore-opengl-module>=0.0.3nb6
+BUILDLINK_ABI_DEPENDS.qore-opengl-module?=	qore-opengl-module>=0.0.3nb7
 BUILDLINK_PKGSRCDIR.qore-opengl-module?=	../../graphics/qore-opengl-module
 
 .include "../../lang/qore/buildlink3.mk"
