693cbd03620f26c10c8eb29a19587faeb223c1ba
nros <nros@pkgsrc.org>
2016-07-22 13:01:33 +0000

    Correct error in buildlink3.mk done in previous commit

--- databases/qore-mysql-module/buildlink3.mk
+++ databases/qore-mysql-module/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2016/07/22 12:58:10 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/07/22 13:01:33 nros Exp $
 #
 
 BUILDLINK_TREE+=	qore-mysql-module
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	qore-mysql-module
 QORE_MYSQL_MODULE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qore-mysql-module+=	qore-mysql-module>=2.0.1
-BUILDLINK_ABI_DEPENDS.qore-mysql-module?=	qore-mysql-module>=2.0.2
+BUILDLINK_ABI_DEPENDS.qore-mysql-module?=	qore-mysql-module>=2.0.1nb6
 BUILDLINK_PKGSRCDIR.qore-mysql-module?=	../../databases/qore-mysql-module
 
 .include "../../lang/qore/buildlink3.mk"
