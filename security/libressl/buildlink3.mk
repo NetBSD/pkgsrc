a8ca8dce715e48d1026c15ee08a29df8ebe10388
wiz <wiz@pkgsrc.org>
2016-07-26 11:53:24 +0000

    Fix BUILDLINK_PASSTHRU_DIRS arguments.

--- security/libressl/buildlink3.mk
+++ security/libressl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/06/26 21:36:06 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/07/26 11:53:24 wiz Exp $
 
 BUILDLINK_TREE+=	libressl
 
@@ -8,8 +8,8 @@ LIBRESSL_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libressl+=	libressl>=2.2.6
 BUILDLINK_PKGSRCDIR.libressl?=	../../security/libressl
 
-BUILDLINK_PASSTHRU_DIRS+=	libressl/include
-BUILDLINK_PASSTHRU_DIRS+=	libressl/lib
+BUILDLINK_PASSTHRU_DIRS+=	${LOCALBASE}/libressl/include
+BUILDLINK_PASSTHRU_DIRS+=	${LOCALBASE}/libressl/lib
 CPPFLAGS+=			-I${PREFIX}/libressl/include
 LDFLAGS+=			${COMPILER_RPATH_FLAG}${PREFIX}/libressl/lib -L${PREFIX}/libressl/lib
 .endif	# LIBRESSL_BUILDLINK3_MK
