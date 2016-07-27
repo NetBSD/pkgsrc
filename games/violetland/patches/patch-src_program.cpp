3ed67165d0b084e6f9689a34d282851aa78637cc
kamil <kamil>
2016-07-27 21:14:36 +0000

    Import violetland-0.5 as games/violetland
    Violetland is an open source cross-platform game similar to Crimsonland.
    
    In this game the player should help a girl by name of Violet to struggle with
    hordes of monsters. For this purpose the various weapon, and also the special
    abilities of the heroine which are opening with experience can be used.

---
+++ games/violetland/patches/patch-src_program.cpp
@@ -0,0 +1,19 @@
+$NetBSD: patch-src_program.cpp,v 1.1 2016/07/27 21:14:36 kamil Exp $
+
+Add NetBSD support.
+
+--- src/program.cpp.orig	2016-07-27 09:04:07.000000000 +0000
++++ src/program.cpp
+@@ -244,8 +244,11 @@ void printVersion() {
+ 	env = "GNU/LINUX";
+ #endif //linux
+ #ifdef __FreeBSD__
+-	env = "BSD";
++	env = "FREEBSD";
+ #endif //__FreeBSD__
++#ifdef __NetBSD__
++	env = "NETBSD";
++#endif //__NetBSD__
+ #ifdef __APPLE__
+ 	env = "MAC";
+ #endif //__APPLE__
