043c944738c727542222693f6a26de07c677b29e
wiz <wiz@pkgsrc.org>
2016-07-27 09:17:41 +0000

    Updated lldb to 3.8.1.
    Mark as not ready for python-3.x.
    
    No special changelog found, but this matches llvm/clang 3.8.1.

--- devel/lldb/patches/patch-scripts_Python_finishSwigPythonLLDB.py
+++ devel/lldb/patches/patch-scripts_Python_finishSwigPythonLLDB.py
@@ -1,12 +1,12 @@
-$NetBSD: patch-scripts_Python_finishSwigPythonLLDB.py,v 1.1 2016/03/28 13:13:55 kamil Exp $
+$NetBSD: patch-scripts_Python_finishSwigPythonLLDB.py,v 1.2 2016/07/27 09:17:42 wiz Exp $
 
 Don't conflict with lang/py-six.
 
---- scripts/Python/finishSwigPythonLLDB.py.orig	2016-02-21 02:08:07.000000000 +0000
+--- scripts/Python/finishSwigPythonLLDB.py.orig	2016-05-19 21:02:17.000000000 +0000
 +++ scripts/Python/finishSwigPythonLLDB.py
-@@ -719,8 +719,8 @@ def main(vDictArgs):
+@@ -749,8 +749,8 @@ def main(vDictArgs):
      if bOk:
-         bOk, strMsg = create_symlinks(vDictArgs, strFrameworkPythonDir)
+         bOk, strMsg = create_symlinks(vDictArgs, strFrameworkPythonDir, strLldbLibDir)
  
 -    if bOk:
 -        bOk, strMsg = copy_six(vDictArgs, strFrameworkPythonDir)
