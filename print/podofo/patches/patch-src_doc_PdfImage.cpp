11b19ca2b126f8eb87d8484768f1c3efe50e983f
adam <adam@pkgsrc.org>
2016-08-01 13:39:34 +0000

    PoDoFo 0.9.4 was released today, after over two years of development and with the help of many new contributors. As there are so many improvements, patches and fixes that made it into this release, we are not able to list them, so please stick with the svn log.

--- print/podofo/patches/patch-src_doc_PdfImage.cpp
+++ print/podofo/patches/patch-src_doc_PdfImage.cpp
@@ -1,16 +1,16 @@
-$NetBSD: patch-src_doc_PdfImage.cpp,v 1.2 2016/06/23 14:15:15 adam Exp $
+$NetBSD: patch-src_doc_PdfImage.cpp,v 1.3 2016/08/01 13:39:34 adam Exp $
 
 Recognize TIFF images with alpha channel when using the photometric
 colour tag.
 
---- src/doc/PdfImage.cpp.orig	2016-06-07 16:48:51.790860153 +0000
+--- src/doc/PdfImage.cpp.orig	2016-05-15 19:06:35.000000000 +0000
 +++ src/doc/PdfImage.cpp
-@@ -550,7 +550,7 @@ void PdfImage::LoadFromTiff( const char*
- 		break;
- 
- 		case PHOTOMETRIC_RGB:
+@@ -589,7 +589,7 @@ void PdfImage::LoadFromTiffHandle(void* 
+             break;
+             
+         case PHOTOMETRIC_RGB:
 -            if ( bitsPixel != 24 )
 +            if ( bitsPixel != 24 && bitsPixel != 32 )
- 			{
- 				TIFFClose(hInfile);
- 				PODOFO_RAISE_ERROR( ePdfError_UnsupportedImageFormat );
+             {
+                 TIFFClose(hInTiffHandle);
+                 PODOFO_RAISE_ERROR( ePdfError_UnsupportedImageFormat );
