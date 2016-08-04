02993043a1ac3e9a02d98754cc43a86ef4e0598b
mef <mef@pkgsrc.org>
2016-08-04 13:08:25 +0000

    Updated security/opensc to 0.16.0 -------------------------------- New in 0.16.0; 2016-05-15 * build     link OpenSSL in static     option: enable PKCS11 thread locking * configuration     use one configuration file for all systems * tools:     package revision as version   ** pkcs11-tool        keygen mechanism in pkcs11 tools        write GOST public key        fix CKA_SENSITIVE attribute of public keys   ** opensc-explorer:        added command find_tags        allow ASN.1 decoding if the file seems incomplete   ** pkcs15-tool:        handle record-based files when doing file caching        option to prine raw data   ** sc-hsm-tool:        status info support for SmartCard-HSM V2.0   ** doc: some missing options are documented, added documentation        for gid tool * minidriver:   support for ECC   Windows x509 enrollment   first implementation of CardDeleteContainer   MD logs controlled by register and environment variable * reader-pcsc   fixed unreleased locks with pcsc-lite   honour PC/SC pt 10 dwMaxAPDUDataSize   added call back for getting vendor/product id   restrict access to card handles after fork   SCardGetAttrib is used to initialize reader's metadata   by default only short APDUs supported * pkcs11   no slot reserved for hot plug   no more slot created 'per-applications'   atomic operation (TODO: expand)   export all C_* symbols   metadata initialized from package info   fix registering pkcs11 mechanisms multiple times   sloppy initialization for C_GetSlotInfo * pkcs15   cache of on-card files extended to application paths   configuration option to enable/disable application   make file cache dir configurable   in key info data type introduced 'auxiliary data' -- container       for the non-pkc15 data. * OpenPGP   support for Gnuk -- USB cryptographic token for GNU Privacy Guard   build without OpenSSL   implemented 'erase card'   additional manufacturers * MyEID   support for 521 bit ECC keys   ATRs for the new cards * sc-hsm   read/write support in minidriver * rtecp   delete keys * GemSafeV1   support for European Patent Office smart card   sign with SHA256 * Gids   first support for Gids smart card * dnie * Feitian PKI card   new ATRs * IsoApplet   (fixes) * starcos   initial support for STARCOS 3.4 (German D-Trust cards) * macosx   install tokend to /Library/Security/ instead /System/Library/Security/   fixed locking issue in pcsc reader * PIV   allow using of cards where default application in not PIV   support for the Yubikey NEO * italian-CNS   italian-cns reg file for minidriver

--- security/opensc/patches/patch-src_libopensc_log.c
+++ security/opensc/patches/patch-src_libopensc_log.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_libopensc_log.c,v 1.2 2015/09/07 19:59:42 gdt Exp $
+$NetBSD: patch-src_libopensc_log.c,v 1.3 2016/08/04 13:08:25 mef Exp $
 
---- src/libopensc/log.c.orig	2015-05-16 20:30:24.000000000 +0000
-+++ src/libopensc/log.c
+--- src/libopensc/log.c.orig	2016-05-31 16:36:09.000000000 +0900
++++ src/libopensc/log.c	2016-08-04 21:52:53.000000000 +0900
 @@ -91,7 +91,13 @@ static void sc_do_log_va(sc_context_t *c
  	gettimeofday (&tv, NULL);
  	tm = localtime (&tv.tv_sec);
  	strftime (time_string, sizeof(time_string), "%H:%M:%S", tm);
--	r = snprintf(p, left, "0x%lx %s.%03ld ", (unsigned long)pthread_self(), time_string, tv.tv_usec / 1000);
+-	r = snprintf(p, left, "0x%lx %s.%03ld ", (unsigned long)pthread_self(), time_string, (long)tv.tv_usec / 1000);
 +	r = snprintf(p, left, "0x%lx %s.%03ld ",
 +#ifdef HAVE_PTHREAD
 +		     (unsigned long)pthread_self(),
