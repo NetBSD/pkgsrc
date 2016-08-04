f30f674979825acec439843165d780be2c7ca2f0
ryoon <ryoon@pkgsrc.org>
2016-08-04 17:03:30 +0000

    Recursive revbump from audio/pulseaudio

--- net/knewstuff/buildlink3.mk
+++ net/knewstuff/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/05/26 09:01:44 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/08/04 17:03:31 ryoon Exp $
 
 BUILDLINK_TREE+=	knewstuff
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	knewstuff
 KNEWSTUFF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.knewstuff+=	knewstuff>=5.21.0
+BUILDLINK_ABI_DEPENDS.knewstuff?=	knewstuff>=5.21.0nb1
 BUILDLINK_PKGSRCDIR.knewstuff?=	../../net/knewstuff
 
 .include "../../devel/kio/buildlink3.mk"
--- x11/kactivities5/buildlink3.mk
+++ x11/kactivities5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/05/26 09:49:48 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/08/04 17:03:32 ryoon Exp $
 
 BUILDLINK_TREE+=	kactivities5
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	kactivities5
 KACTIVITIES5_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kactivities5+=	kactivities5>=5.21.0
+BUILDLINK_ABI_DEPENDS.kactivities5?=	kactivities5>=5.21.0nb1
 BUILDLINK_PKGSRCDIR.kactivities5?=	../../x11/kactivities5
 
 .include "../../devel/kio/buildlink3.mk"
