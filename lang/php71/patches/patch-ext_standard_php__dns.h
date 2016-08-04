7d564ab02037e938cfbb9adb7317e617b26d7a49
jdolecek <jdolecek@pkgsrc.org>
2016-08-04 14:09:03 +0000

    Add php 7.1.0beta1
    PHP 7.1.x builds upon 7.0.x, adding new features (so far)
    
    * Iterable type.
    * HTTP/2 Server Push Support in ext/curl & other ext/curl improvements
    * Fix inconsistent behavior in $this variable
    * Square bracket syntax for array destructuring assignment
    * Create closure from callable
    * More precise float values
    * Asynchronous Signal Handling (without ticks) in ext/pcntl.
    * Additional Context in pcntl_signal Handler
    
    NOTE: this is BETA, not suitable for production environment

---
+++ lang/php71/patches/patch-ext_standard_php__dns.h
@@ -0,0 +1,23 @@
+$NetBSD: patch-ext_standard_php__dns.h,v 1.1 2016/08/04 14:09:03 jdolecek Exp $
+
+Fix memory leak.
+
+--- ext/standard/php_dns.h.orig	2016-04-28 18:13:00.000000000 +0000
++++ ext/standard/php_dns.h
+@@ -32,9 +32,15 @@
+ #elif defined(HAVE_RES_NSEARCH)
+ #define php_dns_search(res, dname, class, type, answer, anslen) \
+ 			res_nsearch(res, dname, class, type, answer, anslen);
++#ifdef __GLIBC__
+ #define php_dns_free_handle(res) \
+-			res_nclose(res); \
++                        res_nclose(res); \
++                        php_dns_free_res(*res)
++#else
++#define php_dns_free_handle(res) \
++			res_ndestroy(res); \
+ 			php_dns_free_res(res)
++#endif
+ 
+ #elif defined(HAVE_RES_SEARCH)
+ #define php_dns_search(res, dname, class, type, answer, anslen) \
