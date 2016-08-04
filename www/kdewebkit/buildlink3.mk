c6d42abaaef5b6eddea81794ae5ae5e6953e94a0
ryoon <ryoon@pkgsrc.org>
2016-08-04 17:03:30 +0000

    Recursive revbump from audio/pulseaudio

--- www/kdewebkit/buildlink3.mk
+++ www/kdewebkit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/05/26 09:31:43 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/08/04 17:03:32 ryoon Exp $
 
 BUILDLINK_TREE+=	kdewebkit
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	kdewebkit
 KDEWEBKIT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kdewebkit+=	kdewebkit>=5.21.0
+BUILDLINK_ABI_DEPENDS.kdewebkit?=	kdewebkit>=5.21.0nb1
 BUILDLINK_PKGSRCDIR.kdewebkit?=	../../www/kdewebkit
 
 .include "../../devel/kparts/buildlink3.mk"
