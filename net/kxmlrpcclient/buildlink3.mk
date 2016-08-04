40baa2d0e384f16005bb456f5a574cb7be00d8a6
ryoon <ryoon@pkgsrc.org>
2016-08-04 17:03:30 +0000

    Recursive revbump from audio/pulseaudio

--- net/kxmlrpcclient/buildlink3.mk
+++ net/kxmlrpcclient/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/05/26 09:01:44 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/08/04 17:03:31 ryoon Exp $
 
 BUILDLINK_TREE+=	kxmlrpcclient
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	kxmlrpcclient
 KXMLRPCCLIENT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kxmlrpcclient+=	kxmlrpcclient>=5.21.0
+BUILDLINK_ABI_DEPENDS.kxmlrpcclient?=	kxmlrpcclient>=5.21.0nb1
 BUILDLINK_PKGSRCDIR.kxmlrpcclient?=	../../net/kxmlrpcclient
 
 .include "../../devel/kio/buildlink3.mk"
