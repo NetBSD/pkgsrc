e37b97fe3c17227bc5752e0562dce22fd81e8a9b
ryoon <ryoon@pkgsrc.org>
2016-08-04 17:03:30 +0000

    Recursive revbump from audio/pulseaudio

--- devel/knotifyconfig/buildlink3.mk
+++ devel/knotifyconfig/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/05/26 08:29:05 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/08/04 17:03:31 ryoon Exp $
 
 BUILDLINK_TREE+=	knotifyconfig
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	knotifyconfig
 KNOTIFYCONFIG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.knotifyconfig+=	knotifyconfig>=5.21.0
+BUILDLINK_ABI_DEPENDS.knotifyconfig?=	knotifyconfig>=5.21.0nb1
 BUILDLINK_PKGSRCDIR.knotifyconfig?=	../../devel/knotifyconfig
 
 .include "../../devel/kio/buildlink3.mk"
