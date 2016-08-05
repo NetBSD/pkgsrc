8478e364ff50a5bda8857ae9b7b521bf25e3b39d
wiz <wiz@pkgsrc.org>
2016-08-05 08:29:01 +0000

    7.1.0beta1 is also smaller than 7.1 -- fix pattern.

--- lang/php70/buildlink3.mk
+++ lang/php70/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/12/06 15:17:31 taca Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/08/05 08:29:01 wiz Exp $
 
 BUILDLINK_TREE+=	php
 
 .if !defined(PHP_BUILDLINK3_MK)
 PHP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.php+=	php>=7.0.0<7.1
-BUILDLINK_ABI_DEPENDS.php+=	php>=7.0.0<7.1
+BUILDLINK_API_DEPENDS.php+=	php>=7.0.0<7.1.0beta1
+BUILDLINK_ABI_DEPENDS.php+=	php>=7.0.0<7.1.0beta1
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php70
 
 .include "../../textproc/libxml2/buildlink3.mk"
