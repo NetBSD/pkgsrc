5cb48e7edbe3b096ebd74afbea5266918b0b308e
prlw1 <prlw1@pkgsrc.org>
2016-08-06 13:03:35 +0000

    Update octave to 4.0.3
    XXX installs lib/charset.alias - is this a problem?
    
    Bugs Fixed in GNU Octave 4.0.3
    
    * doc: clarify differences between atan and atan2 (bug #48178)
    * doc: delete mention of unsupported syntax for looping over structs
      (bug #48064)
    * ver.m: return empty struct for unknown package (bug #48235).
    * Update gnulib subrepo for texinfo formatting fixes (bug #48001)
    * Fix typos in Java conversion of 32 and 64 bit integers (bug #48107)
    * Create valid gnuplot commands even for single-entry colormaps
      (bug #48083).
    * orderfields.m: Remove trailing bracket in docstring (bug #48063).
    * Don't overly restrict options passed to Java jvm (bug #39063).
    * Write integers with correct byte order on big-endian systems
      (bug #47434)
    * doc: Document syntax for specifying color when using Tex
      interpreter (bug #47907).
    * Round quantized pixel values before writing uintN images (bug #47746)
    * Fix popen2 error on Windows when child writes to stderr (bug #43036)
    * it_IT.ts: Correct Italian translation of "col:" (bug #47857).
    * avoid crash in audiowrite argument processing (bug #47875)
    * make __magick_read__ a built-in function (bug #41699)
    * sortrows.m: Improve docstring (bug #47844).
    * doc: Fix typo in exec docstring.
    * Use correct URL for Online Documentation (bug #47835).
    * Fix autoscale affecting legend axes objects (bug #47765).
    * configure.ac: Remove AC_CHECK_FUNC for pipe now that gnulib::pipe used.
    * doc: fix on manual the syntax to empty elements from cell array.
    * octave.texi: Set document encoding to UTF-8.
    * Enable the pipe function on Windows (bug #47614)
    * avoid mulitple definitions of static function-scope vars (bug #47372)
    * Array-sym.cc: Delete obsolete file.
    * Initialize variable to stop unstable results for lgamma (bug #47524).
    * call openmp function at initialization (bug #47372)
    * macros.texi: Colorized links for PDF files with Texinfo 6.x.

--- math/octave/patches/patch-libgui_src_octave-gui.cc
+++ math/octave/patches/patch-libgui_src_octave-gui.cc
@@ -1,8 +1,8 @@
-$NetBSD: patch-libgui_src_octave-gui.cc,v 1.1 2016/02/16 04:21:40 dbj Exp $
+$NetBSD: patch-libgui_src_octave-gui.cc,v 1.2 2016/08/06 13:03:35 prlw1 Exp $
 
 Changes for compiling with Qt5 instead of Qt4
 
---- libgui/src/octave-gui.cc.orig	2015-05-23 14:21:54.000000000 +0000
+--- libgui/src/octave-gui.cc.orig	2016-04-20 18:23:57.000000000 +0000
 +++ libgui/src/octave-gui.cc
 @@ -92,7 +92,7 @@ private:
  // Disable all Qt messages by default.
@@ -22,15 +22,12 @@ Changes for compiling with Qt5 instead of Qt4
  
    install___init_qt___functions ();
  
-@@ -117,11 +117,6 @@ octave_start_gui (int argc, char *argv[]
-   QApplication application (argc, argv);
-   QTranslator gui_tr, qt_tr, qsci_tr;
+@@ -119,7 +119,7 @@ octave_start_gui (int argc, char *argv[]
  
--  // Set the codec for all strings (before wizard)
--#if ! defined (Q_OS_WIN32)
+   // Set the codec for all strings (before wizard)
+ #if ! defined (Q_OS_WIN32)
 -  QTextCodec::setCodecForCStrings (QTextCodec::codecForName ("UTF-8"));
--#endif
--
-   if (start_gui)
-     {
-       // show wizard if this is the first run
++  QTextCodec::setCodecForLocale (QTextCodec::codecForName ("UTF-8"));
+ #endif
+ 
+   // show wizard if this is the first run
