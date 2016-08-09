81eacf74683eaed116b5e341c9e708444207532b
jdolecek <jdolecek@pkgsrc.org>
2016-08-09 21:46:06 +0000

    Update e2fsprogs to 1.43.1, using wip package for version 1.42.12 as base
    Changes too numerous, see following page for details:
    http://e2fsprogs.sourceforge.net/e2fsprogs-release.html#1.43.1
    
    Compile-tested on NetBSD and Mac OS X

--- sysutils/e2fsprogs/patches/patch-lib_ext2fs_bitops.h
+++ sysutils/e2fsprogs/patches/patch-lib_ext2fs_bitops.h
@@ -1,13 +1,15 @@
-$NetBSD: patch-lib_ext2fs_bitops.h,v 1.1 2012/11/19 03:02:14 joerg Exp $
+$NetBSD: patch-lib_ext2fs_bitops.h,v 1.2 2016/08/09 21:46:07 jdolecek Exp $
 
---- lib/ext2fs/bitops.h.orig	2012-11-19 02:21:39.000000000 +0000
-+++ lib/ext2fs/bitops.h
-@@ -135,7 +135,7 @@ extern __u32 ext2fs_get_generic_bitmap_e
- #define _INLINE_ extern
- #else
+Force GNU inline semantic.
+
+--- lib/ext2fs/bitops.h.orig	2014-07-03 23:33:48.000000000 +0900
++++ lib/ext2fs/bitops.h	2014-12-10 21:10:47.000000000 +0900
+@@ -238,7 +238,7 @@ extern errcode_t ext2fs_find_first_set_g
+ #define _INLINE_ inline
+ #else /* not C99 */
  #ifdef __GNUC__
 -#define _INLINE_ extern __inline__
 +#define _INLINE_ extern __attribute__((__gnu_inline__)) __inline__
  #else				/* For Watcom C */
  #define _INLINE_ extern inline
- #endif
+ #endif /* __GNUC__ */
