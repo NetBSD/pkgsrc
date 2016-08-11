a213aabc3b1bab0b546d739c9f8a04db314448d9
is <is@pkgsrc.org>
2016-08-11 22:48:00 +0000

    Fix bus error on little-endian architectures with strict alignment.

--- graphics/netpbm/patches/patch-converter_other_giftopnm.c
+++ graphics/netpbm/patches/patch-converter_other_giftopnm.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-converter_other_giftopnm.c,v 1.1 2015/01/25 15:01:48 jperkin Exp $
+$NetBSD: patch-converter_other_giftopnm.c,v 1.2 2016/08/11 22:48:00 is Exp $
 
 Fix "giftopnm.c:368: error: incompatible types in assignment".
 
@@ -13,3 +13,17 @@ Fix "giftopnm.c:368: error: incompatible types in assignment".
          *eofP = TRUE;
          *lengthP = 0;
      } else {
+@@ -671,10 +671,13 @@ bitsOfLeBuffer(const unsigned char * con
+ 
+     assert(len <= 16);
+ 
++/* should be UNALIGNED_OK, but that'd need fixing, too */
++#if defined(__vax__) || defined(__i486__) || defined(__x86_64__)
+     if (BYTE_ORDER == LITTLE_ENDIAN)
+         /* Fast path */
+         codeBlock = *(uint32_t *) & buf[start/8];
+     else
++#endif
+         /* logic works for little endian too */
+         codeBlock =
+             (buf[start/8+0] <<  0) |
