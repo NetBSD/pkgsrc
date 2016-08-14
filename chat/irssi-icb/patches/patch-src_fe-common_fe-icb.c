ac010ae9e1c433de3d20f8e7ebbc0baba60976fa
leot <leot@pkgsrc.org>
2016-08-14 21:15:12 +0000

    Add support for irssi>=0.8.18 and update distinfo for irssi-0.8.19 update

---
+++ chat/irssi-icb/patches/patch-src_fe-common_fe-icb.c
@@ -0,0 +1,19 @@
+$NetBSD: patch-src_fe-common_fe-icb.c,v 1.1 2016/08/14 21:15:12 leot Exp $
+
+Add `void MODULENAME ## _abicheck(int *version)' function required by
+irssi>=0.8.18.
+
+--- src/fe-common/fe-icb.c.orig	2011-06-13 18:10:41.000000000 +0000
++++ src/fe-common/fe-icb.c
+@@ -531,3 +531,11 @@ void fe_icb_deinit(void)
+ 
+ 	signal_remove("server add fill", (SIGNAL_FUNC) sig_server_add_fill);
+ }
++
++#ifdef IRSSI_ABI_VERSION
++void
++fe_icb_abicheck(int * version)
++{
++	*version = IRSSI_ABI_VERSION;
++}
++#endif
