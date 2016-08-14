d7cda441b4d877da044658bb689f5212d45f92bf
is <is@pkgsrc.org>
2016-08-14 16:56:29 +0000

    Update postgresql94 & related to PostGreSQL 9.4.9

--- databases/postgresql94/patches/patch-src_timezone_localtime.c
+++ databases/postgresql94/patches/patch-src_timezone_localtime.c
@@ -1,31 +1,13 @@
-$NetBSD: patch-src_timezone_localtime.c,v 1.1 2015/01/14 21:01:18 adam Exp $
+$NetBSD: patch-src_timezone_localtime.c,v 1.2 2016/08/14 16:56:29 is Exp $
 
---- src/timezone/localtime.c.orig	2009-06-11 16:49:15.000000000 +0200
+--- src/timezone/localtime.c.orig	2016-08-08 20:29:39.000000000 +0000
 +++ src/timezone/localtime.c
-@@ -81,20 +81,20 @@ static pg_time_t detzcode64(const char *
- static int	differ_by_repeat(pg_time_t t1, pg_time_t t0);
- static const char *getzname(const char *strp);
- static const char *getqzname(const char *strp, int delim);
--static const char *getnum(const char *strp, int *nump, int min, int max);
-+static const char *getnum(const char *strp, int *nump, const int min, const int max);
- static const char *getsecs(const char *strp, long *secsp);
- static const char *getoffset(const char *strp, long *offsetp);
- static const char *getrule(const char *strp, struct rule * rulep);
- static void gmtload(struct state * sp);
--static struct pg_tm *gmtsub(const pg_time_t *timep, long offset,
-+static struct pg_tm *gmtsub(const pg_time_t *timep, const long offset,
- 	   struct pg_tm * tmp);
--static struct pg_tm *localsub(const pg_time_t *timep, long offset,
-+static struct pg_tm *localsub(const pg_time_t *timep, const long offset,
- 		 struct pg_tm * tmp, const pg_tz *tz);
- static int	increment_overflow(int *number, int delta);
--static pg_time_t transtime(pg_time_t janfirst, int year,
--		  const struct rule * rulep, long offset);
-+static pg_time_t transtime(pg_time_t janfirst, const int year,
-+		  const struct rule * rulep, const long offset);
- static int	typesequiv(const struct state * sp, int a, int b);
--static struct pg_tm *timesub(const pg_time_t *timep, long offset,
-+static struct pg_tm *timesub(const pg_time_t *timep, const long offset,
- 		const struct state * sp, struct pg_tm * tmp);
+@@ -84,7 +84,7 @@ struct rule
+  * Prototypes for static functions.
+  */
  
- /* GMT timezone */
+-static struct pg_tm *gmtsub(pg_time_t const *, int32, struct pg_tm *);
++static struct pg_tm *gmtsub(pg_time_t const *, const int32, struct pg_tm *);
+ static bool increment_overflow(int *, int);
+ static bool increment_overflow_time(pg_time_t *, int32);
+ static struct pg_tm *timesub(pg_time_t const *, int32, struct state const *,
