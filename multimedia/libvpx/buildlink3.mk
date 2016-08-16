e40e06ddfe6595241fe2d8835eee513a66f789d3
ryoon <ryoon@pkgsrc.org>
2016-08-16 23:53:25 +0000

    Update to 1.6.0
    Changelog:
    2016-07-20 v1.6.0 "Khaki Campbell Duck"
      This release improves upon the VP9 encoder and speeds up the encoding and
      decoding processes.
    
      - Upgrading:
        This release is ABI incompatible with 1.5.0 due to a new 'color_range' enum
        in vpx_image and some minor changes to the VP8_COMP structure.
    
        The default key frame interval for VP9 has changed from 128 to 9999.
    
      - Enhancement:
        A core focus has been performance for low end Intel processors. SSSE3
        instructions such as 'pshufb' have been avoided and instructions have been
        reordered to better accommodate the more constrained pipelines.
    
        As a result, devices based on Celeron processors have seen substantial
        decoding improvements. From Indian Runner Duck to Javan Whistling Duck,
        decoding speed improved between 10 and 30%. Between Javan Whistling Duck
        and Khaki Campbell Duck, it improved another 10 to 15%.
    
        While Celeron benefited most, Core-i5 also improved 5% and 10% between the
        respective releases.
    
        Realtime performance for WebRTC for both speed and quality has received a
        lot of attention.
    
      - Bug Fixes:
        A number of fuzzing issues, found variously by Mozilla, Chromium and others,
        have been fixed and we strongly recommend updating.

--- multimedia/libvpx/buildlink3.mk
+++ multimedia/libvpx/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2015/11/18 14:09:25 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2016/08/16 23:53:25 ryoon Exp $
 
 BUILDLINK_TREE+=	libvpx
 
 .if !defined(LIBVPX_BUILDLINK3_MK)
 LIBVPX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libvpx+=	libvpx>=1.5.0
-BUILDLINK_ABI_DEPENDS.libvpx+=	libvpx>=1.5.0
+BUILDLINK_API_DEPENDS.libvpx+=	libvpx>=1.6.0
+BUILDLINK_ABI_DEPENDS.libvpx+=	libvpx>=1.6.0
 BUILDLINK_PKGSRCDIR.libvpx?=	../../multimedia/libvpx
 
 .include "../../mk/pthread.buildlink3.mk"
