3663ff179ec2b2c89905205cbdcd89ab6429cd46
ryoon <ryoon@pkgsrc.org>
2016-08-17 00:06:39 +0000

    Recursive revbump from multimedia/libvpx uppdate

--- graphics/osg/buildlink3.mk
+++ graphics/osg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.43 2016/08/03 10:22:15 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.44 2016/08/17 00:06:39 ryoon Exp $
 
 BUILDLINK_TREE+=	osg
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	osg
 OSG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.osg+=	osg>=2.8.3
-BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.0.1nb42
+BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.0.1nb43
 BUILDLINK_PKGSRCDIR.osg?=	../../graphics/osg
 
 .include "../../graphics/cairo/buildlink3.mk"
--- multimedia/xine-lib/buildlink3.mk
+++ multimedia/xine-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.32 2015/11/18 14:19:47 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.33 2016/08/17 00:06:40 ryoon Exp $
 
 BUILDLINK_TREE+=	xine-lib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	xine-lib
 XINE_LIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xine-lib+=	xine-lib>=1rc3c
-BUILDLINK_ABI_DEPENDS.xine-lib+=	xine-lib>=1.2.6nb3
+BUILDLINK_ABI_DEPENDS.xine-lib+=	xine-lib>=1.2.6nb8
 BUILDLINK_PKGSRCDIR.xine-lib?=	../../multimedia/xine-lib
 
 .include "../../multimedia/ffmpeg2/buildlink3.mk"
