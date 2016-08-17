82f67120a8866d2836528ed614170ac0570b44c0
ryoon <ryoon@pkgsrc.org>
2016-08-17 00:06:39 +0000

    Recursive revbump from multimedia/libvpx uppdate

--- audio/chromaprint/buildlink3.mk
+++ audio/chromaprint/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2016/02/11 14:16:03 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2016/08/17 00:06:39 ryoon Exp $
 
 BUILDLINK_TREE+=	chromaprint
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	chromaprint
 CHROMAPRINT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.chromaprint+=	chromaprint>=1.1
-BUILDLINK_ABI_DEPENDS.chromaprint?=	chromaprint>=1.3.1
+BUILDLINK_ABI_DEPENDS.chromaprint?=	chromaprint>=1.3.2nb1
 BUILDLINK_PKGSRCDIR.chromaprint?=	../../audio/chromaprint
 
 .include "../../multimedia/ffmpeg2/buildlink3.mk"
