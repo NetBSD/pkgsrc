1dd512206a00227e7b72752db252df2a0b1fdbed
markd <markd@pkgsrc.org>
2016-08-21 22:20:43 +0000

    Add kdelibs4support 5.25.0
    This framework provides code and utilities to ease the transition from
    kdelibs 4 to KDE Frameworks 5.  This includes CMake macros and C++
    classes whose functionality has been replaced by code in CMake, Qt and
    other frameworks.
    
    Code should aim to port away from this framework eventually.  The API
    documentation of the classes in this framework and the notes at
    <http://community.kde.org/Frameworks/Porting_Notes> should help with
    this.
    
    Note that some of the classes in this framework, especially
    KStandardDirs, may not work correctly unless any libraries and other
    software using the KDELibs 4 Support framework are installed to the same
    location as KDELibs4Support, although it may be sufficient to set the
    KDEDIRS environment variable correctly.

---
+++ x11/kdelibs4support/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/08/21 22:20:43 markd Exp $
+
+BUILDLINK_TREE+=	kdelibs4support
+
+.if !defined(KDELIBS4SUPPORT_BUILDLINK3_MK)
+KDELIBS4SUPPORT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kdelibs4support+=	kdelibs4support>=5.21.0
+BUILDLINK_PKGSRCDIR.kdelibs4support?=	../../x11/kdelibs4support
+
+.include "../../misc/kemoticons/buildlink3.mk"
+.include "../../devel/kitemmodels/buildlink3.mk"
+.include "../../misc/kunitconversion/buildlink3.mk"
+.include "../../devel/kparts/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KDELIBS4SUPPORT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kdelibs4support
