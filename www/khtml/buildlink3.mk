117c6711b1d65d04e95a45d0948eb9307a613ad5
markd <markd@pkgsrc.org>
2016-08-21 22:33:12 +0000

    Add khtml 5.25.0
    KDE Frameworks 5 porting aid
    
    KHTML is a web rendering engine, based on the KParts technology and using
    KJS for JavaScript support.

---
+++ www/khtml/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/08/21 22:33:12 markd Exp $
+
+BUILDLINK_TREE+=	khtml
+
+.if !defined(KHTML_BUILDLINK3_MK)
+KHTML_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.khtml+=	khtml>=5.21.0
+BUILDLINK_PKGSRCDIR.khtml?=	../../www/khtml
+
+.include "../../graphics/giflib/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.include "../../www/kjs/buildlink3.mk"
+.include "../../devel/kparts/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KHTML_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-khtml
