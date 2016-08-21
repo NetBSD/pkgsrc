cb2a9099e7c3c37478e3bb8087966beb085b89d2
markd <markd@pkgsrc.org>
2016-08-21 21:26:01 +0000

    Update KDE Frameworks to 5.25.0
    5.22:
    Sonnet
    * Install parsetrigrams tool for cross compiling
    * hunspell: Load/Store a personal dictionary
    * Support hunspell 1.4
    * configwidget: notify about changed config when ignored words updated
    * settings: don't immediately save the config when updating ignore list
    * configwidget: fix saving when ignore words updated
    * Fix failed to save ignore word issue
    
    5.24:
    Sonnet
    * Mark helper exe as non-gui app
    * Allow nsspellcheck to be compiled on mac per default
    
    5.25:
    Sonnet
    * hunspell: Clean up code for searching for dictionaries, add XDG dirs
    * Try to fix language filter usage of language detection a bit

--- textproc/kapidox/patches/patch-setup.py
+++ textproc/kapidox/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.1 2016/05/26 09:26:56 markd Exp $
+$NetBSD: patch-setup.py,v 1.2 2016/08/21 21:26:01 markd Exp $
 
 Fix man path.
 
---- setup.py.orig	2015-12-12 22:27:05.000000000 +0000
+--- setup.py.orig	2016-08-05 23:24:22.000000000 +0000
 +++ setup.py
-@@ -35,7 +35,7 @@ setup(
+@@ -34,7 +34,7 @@ setup(
              'src/depdiagram-generate-all',
          ],
          data_files= [
--            (os.path.join('share', 'man', 'man1'), ['docs/kgenapidox.1',
-+            (os.path.join('man', 'man1'), ['docs/kgenapidox.1',
-                 'docs/kgenframeworksapidox.1', 'docs/depdiagram-prepare.1',
+-            (os.path.join('share', 'man', 'man1'), ['docs/depdiagram-prepare.1',
++            (os.path.join('man', 'man1'), ['docs/depdiagram-prepare.1',
                  'docs/depdiagram-generate.1',
                  'docs/depdiagram-generate-all.1'])],
+         classifiers = [
