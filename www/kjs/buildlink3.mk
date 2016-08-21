bf5f25be0ef8d5e62b33e23c913a4a4fb8184e9a
markd <markd@pkgsrc.org>
2016-08-21 22:27:17 +0000

    Add kjs 5.25.0
    KDE Framework 5 porting aid
    
    This library provides an ECMAScript compatible interpreter. The ECMA standard
    is based on well known scripting languages such as Netscape's JavaScript and
    Microsoft's JScript.

---
+++ www/kjs/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/08/21 22:27:17 markd Exp $
+
+BUILDLINK_TREE+=	kjs
+
+.if !defined(KJS_BUILDLINK3_MK)
+KJS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kjs+=	kjs>=5.21.0
+BUILDLINK_PKGSRCDIR.kjs?=	../../www/kjs
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KJS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kjs
