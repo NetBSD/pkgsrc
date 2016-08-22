50377fe4e49a0836cdd2359427c9d9057762535f
morr <morr@pkgsrc.org>
2016-08-22 18:11:04 +0000

    pkgsrc changes to package:
    - Add missing php modules
    - Limit work with php-5.6
    - Improve the wordpress.conf
    - Install wp-config-sample.php to WPHOME but not EGDIR
    
    Patch from wen heping.

--- www/wordpress/options.mk
+++ www/wordpress/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2012/04/14 16:25:11 adam Exp $
+# $NetBSD: options.mk,v 1.5 2016/08/22 18:11:04 morr Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.wordpress
 
@@ -10,10 +10,10 @@ PKG_SUGGESTED_OPTIONS=		ap-php
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mphp-cgi)
-DEPENDS+=	php>=5.2.4:${PHPPKGSRCDIR}
+DEPENDS+=	php>=5.6.0:${PHPPKGSRCDIR}
 .endif
 
 .if !empty(PKG_OPTIONS:Map-php)
 .  include "../../mk/apache.mk"
-DEPENDS+=	${APACHE_PKG_PREFIX}-${PHP_PKG_PREFIX}>=4.3.0:../../www/ap-php
+DEPENDS+=	${APACHE_PKG_PREFIX}-${PHP_PKG_PREFIX}>=5.6.0:../../www/ap-php
 .endif
