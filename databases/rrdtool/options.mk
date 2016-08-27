36242278dbbb459c94c0640b5e28add76e28733e
dholland <dholland@pkgsrc.org>
2016-08-27 06:42:43 +0000

    Make options for the python, tcl, and lua bindings. Enable them by default and bump PKGREVISION to 2.
    Also, don't depend on python unless the python option is turned
    on, and make the lua bindings behave themselves on netbsd where
    there's a copy of lua in base.
    
    Note that the ruby bindings are built in a separate package
    (databases/ruby-rrdtool) ...probably the python bindings should be as
    well. Also note that there's another apparently different set of
    python bindings in databases/py-python-rrdtool. I have no idea which
    is preferred.

---
+++ databases/rrdtool/options.mk
@@ -0,0 +1,64 @@
+# $NetBSD: options.mk,v 1.1 2016/08/27 06:42:43 dholland Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.rrdtool
+PKG_SUPPORTED_OPTIONS=	lua python tcl
+PKG_SUGGESTED_OPTIONS=	lua python tcl
+
+# Note that there's another (apparently different) python library
+# in databases/py-python-rrdtool.
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		python tcl lua
+
+
+#
+# XXX: this should include egg.mk, but that doesn't work: it includes
+# extension.mk, which sets do-build and do-install rules, in such a
+# way that we can't override it. Instead we need to cutpaste the
+# definition of EGG_INFODIR and its plist bits. Sigh. Probably the
+# right thing to do is make this a separate package.
+# 
+.if !empty(PKG_OPTIONS:Mpython)
+CONFIGURE_ARGS+=	--enable-python
+
+REPLACE_PYTHON=		examples/stripes.py
+.include "../../lang/python/application.mk"
+
+PLIST.python=		yes
+EGG_NAME=		py_rrdtool-0.2.2
+EGG_INFODIR?=		${EGG_NAME}-py${PYVERSSUFFIX}.egg-info
+PLIST_SUBST+=		PYSITELIB=${PYSITELIB}
+PLIST_SUBST+=		EGG_INFODIR=${EGG_INFODIR}
+#.include "../../lang/python/egg.mk"
+
+.else # python
+CONFIGURE_ARGS+=	--disable-python
+.endif
+
+
+.if !empty(PKG_OPTIONS:Mtcl)
+USE_TOOLS+=		tclsh:run
+CONFIGURE_ARGS+=	--enable-tcl
+CONFIGURE_ARGS+=	--with-tcllib=${BUILDLINK_PREFIX.tcl}/lib
+
+REPLACE_INTERPRETER+=	tclsh
+REPLACE.tclsh.old=	.*tclsh@TCL_VERSION@
+REPLACE.tclsh.new=	${BUILDLINK_PREFIX.tcl}/bin/tclsh
+REPLACE_FILES.tclsh=	bindings/tcl/ifOctets.tcl.in
+
+PLIST.tcl=		yes
+.include "../../lang/tcl/buildlink3.mk"
+
+.else # tcl
+CONFIGURE_ARGS+=	--disable-tcl
+.endif
+
+.if !empty(PKG_OPTIONS:Mlua)
+CONFIGURE_ARGS+=	--enable-lua
+CONFIGURE_ENV+=		ac_cv_path_LUA=${LUA_INTERPRETER}
+PLIST.lua=		yes
+.include "../../lang/lua/module.mk"
+.else
+CONFIGURE_ARGS+=	--disable-lua
+.endif
