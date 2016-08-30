625ccb0984d72b42b629b7ad447cb414cd9bf9aa
ryoon <ryoon@pkgsrc.org>
2016-08-30 12:37:43 +0000

    Update to 1.3.1
    * Remove Apache httpd and php module dependency. This package works fine with
      nginx and php-fpm too
    * Add database option for MySQL and PostgreSQL
    * For MySQL, php-mysqli is recommended. Use it as MySQL option
    
    Changelog:
    Not available.
    1.3.x is new stable branch.

--- devel/mantis/options.mk
+++ devel/mantis/options.mk
@@ -1,11 +1,20 @@
-# $NetBSD: options.mk,v 1.1 2014/09/24 01:06:26 rodent Exp $
+# $NetBSD: options.mk,v 1.2 2016/08/30 12:37:43 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mantis
-PKG_SUPPORTED_OPTIONS=	charts
-PKG_SUGGESTED_OPTIONS+=	# blank
+PKG_OPTIONS_REQUIRED_GROUPS=	db
+PKG_OPTIONS_GROUP.db=		mysql pgsql
+PKG_SUPPORTED_OPTIONS=	charts ldap
+PKG_SUGGESTED_OPTIONS+=	mysql
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mcharts)
 DEPENDS+=	${PHP_PKG_PREFIX}-jpgraph-[0-9]*:../../graphics/php-jpgraph
 .endif
+
+.if !empty(PKG_OPTIONS:Mmysql)
+DEPENDS+=	${PHP_PKG_PREFIX}-mysqli-[0-9]*:../../databases/php-mysqli
+.elif !empty(PKG_OPTIONS:Mpgsql)
+DEPENDS+=	${PHP_PKG_PREFIX}-pgsql-[0-9]*:../../databases/php-pgsql
+.endif
+
