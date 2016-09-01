7e9d091c9c3b353fc99b44f75f85ace23b9f6284
wiz <wiz@pkgsrc.org>
2016-09-01 17:30:21 +0000

    Updated syncthing-gtk to 0.9.2.1.
    0.9.2.1
    
    This release fixes crash-on-statup problem on Windows.
    
    0.9.2
    
    New:
    
        Added black and white icons theme, selectable in UI settings.
        Added Spanish translation (thanks to @buttle)
        New folder ID is pre-filled with auto-generated ID
        Added man page
        Added list of both 5€ patrons to About dialog.
    
    Fixes:
    
        Error message about incompatible nodes is repeated infinitely
        File Pull Order defaults to nothing
        "Override remote changes" option not doing anything
        'tcp://' is prepended to dynamic address in folder editor.
        Added more error details when '404/Not Found' error is generated.
    
    0.9.1
    
    New:
    
        Compatible with Syncthing v0.14.0
        Display notification when .sync-conflict file is created.
        Handle error message about outdated syncthing on remote device
    
    Fixes:
    
        Renamed Chinese localisation directory
    
    
    0.9.0.3
    
    Fixes:
    
        Better cookie handling when no username nor API key is set
        About dialog updated with link to my Patreon page.
    
    0.9.0.2
    
    More fixes:
    
        UI Settings crashing on small screen
        Daemon Settings crashing always
    
    
    0.9.0.1
    
    Quick fix for left half of window missing.
    
    For Syncthing 0.13 and above
    
    Changes:
    
        Added support for folder labels
        Added support for Identification of Connections from Unknown Devices
        Added label that notifies user when changed setting needs ST-GTK restart
        Added 'File Pull Order' and 'Minimum Free Disk Space' fields to folder editor
        Changed formatting in speed limit fields
        Added Chinese, Russian and Lithuanian localization (thanks to @wsxy162, @midyukov-anton and @Moo)
    
    Fixes:
    
        Fixed updater extracting systemd units.
    
    
    
    0.9
    
    never existed

--- net/syncthing-gtk/patches/patch-setup.py
+++ net/syncthing-gtk/patches/patch-setup.py
@@ -1,10 +1,11 @@
-$NetBSD: patch-setup.py,v 1.1 2015/10/03 16:28:56 tnn Exp $
+$NetBSD: patch-setup.py,v 1.2 2016/09/01 17:30:21 wiz Exp $
 
 Avoid calling git.
+Install man page in proper directory for pkgsrc.
 
---- setup.py.orig	2015-02-12 13:40:21.000000000 +0000
+--- setup.py.orig	2016-08-29 04:05:55.000000000 +0000
 +++ setup.py
-@@ -9,13 +9,6 @@ def get_version():
+@@ -12,13 +12,6 @@ def get_version():
  	Returns current package version using git-describe or examining
  	path. If both methods fails, returns 'unknown'.
  	"""
@@ -18,3 +19,12 @@ Avoid calling git.
  	# Git-describe method failed, try to guess from working directory name
  	path = os.getcwd().split(os.path.sep)
  	version = 'unknown'
+@@ -82,7 +75,7 @@ if __name__ == "__main__" : 
+ 				"icons/%s.png" % x for x in (
+ 					'restart', 'settings', 'shutdown', "st-gtk-logo"
+ 			)]),
+-		('share/man/man1', glob.glob("doc/*") ),
++		(os.environ['PKGMANDIR'] + '/man1', glob.glob("doc/*") ),
+ 		('share/icons/hicolor/64x64/emblems', glob.glob("icons/emblem-*.png") ),
+ 		('share/pixmaps', ["icons/syncthing-gtk.png"]),
+ 		('share/applications', ['syncthing-gtk.desktop'] ),
