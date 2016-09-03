a1dff8467af1c944a3acfa374785188857385db6
nros <nros@pkgsrc.org>
2016-09-03 17:56:37 +0000

    Update libetpan to version 1.7.2. patch-src_low-level_smtp_mailsmtp.c has been removed this change is included in this release.
    Changelog:
    1.7.2:
        Fixed so name for Linux
    
    1.7:
    Build
        Fixed expat / curl detection
        Fix build on OpenIndiana
        Support for Berkeley DB 6.0
        Support for Bitcode (Apple)
        Build for Android
        Improved build for Windows
    
    General
        Support for socks proxy
        API for partial base64 parsing
    
    SMTP
        Ability to cancel SMTP operations
        Fixed logging privacy with OAuth2 on smtp
    
    IMAP
        Broken date parsing
        Improved latest TLS/SSL support
        IMAP MOVE implemented
        workaround for XLIST on cyon.ch
        Added ability to send custom IMAP command support
        Workaround for qq.com IMAP server
        Workaround for 163 IMAP server
        Improved SEARCH compatibility

--- mail/libetpan/buildlink3.mk
+++ mail/libetpan/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.29 2016/03/05 11:27:50 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2016/09/03 17:56:37 nros Exp $
 
 BUILDLINK_TREE+=	libetpan
 
@@ -6,10 +6,10 @@ BUILDLINK_TREE+=	libetpan
 LIBETPAN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libetpan+=	libetpan>=1.0
-BUILDLINK_ABI_DEPENDS.libetpan+=	libetpan>=1.6nb1
+BUILDLINK_ABI_DEPENDS.libetpan+=	libetpan>=1.7.2
 BUILDLINK_PKGSRCDIR.libetpan?=	../../mail/libetpan
 
-BDB_ACCEPTED?=	db4 db5
+BDB_ACCEPTED?=	db4 db5 db6
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../security/cyrus-sasl/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
