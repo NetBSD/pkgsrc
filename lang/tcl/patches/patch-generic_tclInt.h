68ec8cf027574a6a7f060419a4f9567516d985fd
adam <adam@pkgsrc.org>
2016-09-03 11:58:01 +0000

    Changes 8.6.6: Bug-fix release.

--- lang/tcl/patches/patch-generic_tclInt.h
+++ lang/tcl/patches/patch-generic_tclInt.h
@@ -1,27 +1,21 @@
-$NetBSD: patch-generic_tclInt.h,v 1.1 2012/08/21 21:31:47 marino Exp $
+$NetBSD: patch-generic_tclInt.h,v 1.2 2016/09/03 11:58:01 adam Exp $
 
---- generic/tclInt.h.orig	2012-07-16 11:57:05.000000000 +0000
+--- generic/tclInt.h.orig	2016-07-11 11:51:23.000000000 +0000
 +++ generic/tclInt.h
-@@ -38,21 +38,9 @@
+@@ -38,16 +38,8 @@
  #include <stdio.h>
  
  #include <ctype.h>
 -#ifdef NO_STDLIB_H
 -#   include "../compat/stdlib.h"
 -#else
--#   include <stdlib.h>
+ #   include <stdlib.h>
 -#endif
 -#ifdef NO_STRING_H
 -#include "../compat/string.h"
 -#else
-+#include <stdlib.h>
  #include <string.h>
 -#endif
--#ifdef STDC_HEADERS
+ #if defined(STDC_HEADERS) || defined(__STDC__) || defined(__C99__FUNC__) \
+      || defined(__cplusplus) || defined(_MSC_VER)
  #include <stddef.h>
--#else
--typedef int ptrdiff_t;
--#endif
- 
- /*
-  * Ensure WORDS_BIGENDIAN is defined correctly:
