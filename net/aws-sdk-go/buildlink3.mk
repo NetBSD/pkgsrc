42b2cd94f1912812ed9f32e9fd8fb9d6f7e1ea79
bsiegert <bsiegert@pkgsrc.org>
2016-09-04 19:29:32 +0000

    Add buildlink file for go-dns. Fix path in aws-sdk-go buildlink file.

--- net/aws-sdk-go/buildlink3.mk
+++ net/aws-sdk-go/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/08/21 19:54:07 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/09/04 19:29:33 bsiegert Exp $
 
 BUILDLINK_TREE+=	aws-sdk-go
 
@@ -9,7 +9,7 @@ BUILDLINK_CONTENTS_FILTER.aws-sdk-go=	${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.aws-sdk-go?=		build
 
 BUILDLINK_API_DEPENDS.aws-sdk-go+=	aws-sdk-go>=1.4.3
-BUILDLINK_PKGSRCDIR.aws-sdk-go?=	../../net/go-aws-sdk
+BUILDLINK_PKGSRCDIR.aws-sdk-go?=	../../net/aws-sdk-go
 
 .endif	# AWS_SDK_GO_BUILDLINK3_MK
 
