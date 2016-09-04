9a7b3d81bee07aa5bbba67627d6aae58d5c099aa
joerg <joerg@pkgsrc.org>
2016-09-04 22:53:27 +0000

    Fix up dependencies to allow Ruby multi-version support to work.

--- x11/ruby-gnome2-gtk3/buildlink3.mk
+++ x11/ruby-gnome2-gtk3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2016/08/03 10:22:25 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2016/09/04 22:53:27 joerg Exp $
 
 BUILDLINK_TREE+=	ruby-gnome2-gtk3
 
@@ -6,8 +6,7 @@ BUILDLINK_TREE+=	ruby-gnome2-gtk3
 RUBY_GNOME2_GTK3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-gtk3+=${RUBY_PKGPREFIX}-gnome2-gtk3>=1.2.0
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk3+=${RUBY_PKGPREFIX}-gnome2-gtk3>=2.2.0nb1
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk3?=	ruby22-gnome2-gtk3>=3.0.8nb1
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk3+=${RUBY_PKGPREFIX}-gnome2-gtk3>=3.0.8nb1
 BUILDLINK_PKGSRCDIR.ruby-gnome2-gtk3?=	../../x11/ruby-gnome2-gtk3
 
 .include "../../devel/ruby-gnome2-glib/buildlink3.mk"
