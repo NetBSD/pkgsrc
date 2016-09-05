0546dc6abb0e953aa13a1f2023512c4e09a70aac
plunky <plunky@pkgsrc.org>
2016-09-05 21:13:21 +0000

    Update to LibreCAD 2.1.1
    There has been a lot of development since the previous version in
    pkgsrc. DWG read/write support and SVG export are major features,
    along with moving to Qt4 and onwards. The QCAD toolbar is no longer
    supported, and the program is faster in operation than the previous
    pkgsrc version.
    
    Apart from the github commit log which is overly detailed, the
    previous release information with some change logs is at
        https://github.com/LibreCAD/LibreCAD/releases

--- cad/librecad/patches/patch-scripts_postprocess-unix.sh
+++ cad/librecad/patches/patch-scripts_postprocess-unix.sh
@@ -1,25 +1,14 @@
-$NetBSD: patch-scripts_postprocess-unix.sh,v 1.2 2012/08/18 06:58:15 ryoon Exp $
+$NetBSD: patch-scripts_postprocess-unix.sh,v 1.3 2016/09/05 21:13:21 plunky Exp $
 
-* Set qt4 tools path
-* Remove GNU find option
+* Set Qt tools path
 
---- scripts/postprocess-unix.sh.orig	2012-04-15 20:54:52.000000000 +0000
+--- scripts/postprocess-unix.sh.orig	2016-06-20 18:43:27.000000000 +0000
 +++ scripts/postprocess-unix.sh
-@@ -7,7 +7,7 @@ DOCDIR="${THISDIR}/support/doc"
- 
- # Generate Help Files
- cd "{$DOCDIR}"
--qcollectiongenerator LibreCADdoc.qhcp
-+@QTDIR@/bin/qcollectiongenerator LibreCADdoc.qhcp
- 
+@@ -8,7 +8,7 @@ APPDATADIR="${THISDIR}/unix/appdata"
+ TSDIRLC="${LCDIR}/ts"
+ TSDIRPI="${PIDIR}/ts"
+ SPTDIR="${LCDIR}/support"
+-LRELEASE="lrelease"
++LRELEASE="@QTDIR@/bin/lrelease"
+
  cd "${THISDIR}"
- 
-@@ -24,7 +24,7 @@ find support/library -type d | sed s/sup
- find support/library -type f -iname *.dxf | sed s/support// | xargs -IFILES  cp support/FILES "${RESOURCEDIR}"/FILES
- 
- # Generate translations
--lrelease librecad.pro
-+@QTDIR@/bin/lrelease librecad.pro
- mkdir -p "${RESOURCEDIR}"/qm
-  
- # Go into translations directory
