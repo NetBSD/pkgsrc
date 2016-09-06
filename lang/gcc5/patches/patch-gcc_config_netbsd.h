39ea60e2d2443487c7c4e994531ae311c121a321
maya <maya>
2016-09-06 05:08:48 +0000

    gcc5: netbsd-only: unconditionally disable libssp, use a saner specfile.
    because we unconditionally disable libssp, we don't need the workaround
    for ssp.h not found, so we can remove that part of that makefile.
    
    bump gcc5 PKGREVISION
    bump gcc5-libs PKGREVISION further than gcc5.

--- lang/gcc5/patches/patch-gcc_config_netbsd.h
+++ lang/gcc5/patches/patch-gcc_config_netbsd.h
@@ -1,6 +1,9 @@
-$NetBSD: patch-gcc_config_netbsd.h,v 1.1 2015/07/16 12:03:36 ryoon Exp $
+$NetBSD: patch-gcc_config_netbsd.h,v 1.2 2016/09/06 05:08:48 maya Exp $
 
---- gcc/config/netbsd.h.orig	2014-01-02 22:23:26.000000000 +0000
+when using shared, link against libc
+another unexplained change
+
+--- gcc/config/netbsd.h.orig	2015-01-05 12:33:28.000000000 +0000
 +++ gcc/config/netbsd.h
 @@ -17,6 +17,9 @@ You should have received a copy of the G
  along with GCC; see the file COPYING3.  If not see
@@ -12,7 +15,23 @@ $NetBSD: patch-gcc_config_netbsd.h,v 1.1 2015/07/16 12:03:36 ryoon Exp $
  /* TARGET_OS_CPP_BUILTINS() common to all NetBSD targets.  */
  #define NETBSD_OS_CPP_BUILTINS_COMMON()		\
    do						\
-@@ -175,3 +178,9 @@ along with GCC; see the file COPYING3.  
+@@ -96,6 +99,7 @@ along with GCC; see the file COPYING3.  
+        %{!pg:-lposix}}		\
+      %{p:-lposix_p}		\
+      %{pg:-lposix_p}}		\
++   %{shared:-lc}		\
+    %{!shared:			\
+      %{!symbolic:		\
+        %{!p:			\
+@@ -109,6 +113,7 @@ along with GCC; see the file COPYING3.  
+        %{!pg:-lposix}}		\
+      %{p:-lposix_p}		\
+      %{pg:-lposix_p}}		\
++   %{shared:-lc}		\
+    %{!shared:			\
+      %{!symbolic:		\
+        %{!p:			\
+@@ -175,3 +180,9 @@ along with GCC; see the file COPYING3.  
  
  #undef WINT_TYPE
  #define WINT_TYPE "int"
