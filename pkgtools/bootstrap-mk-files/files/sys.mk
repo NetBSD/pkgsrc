9a1bbeec75a559e604cc521d1204a11a676787d5
joerg <joerg@pkgsrc.org>
2016-09-08 14:28:12 +0000

    bootstrap-mk-files-20160908: Unless a platform explicitly sets DBG, default to -O2 on all architectures. Most importantly, don't default to unoptimized builds on AMD64.

--- pkgtools/bootstrap-mk-files/files/sys.mk
+++ pkgtools/bootstrap-mk-files/files/sys.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: sys.mk,v 1.1.1.1 2006/07/14 23:13:01 jlam Exp $
+#	$NetBSD: sys.mk,v 1.2 2016/09/08 14:28:12 joerg Exp $
 #	@(#)sys.mk	8.2 (Berkeley) 3/21/94
 
 unix?=		We run Unix
@@ -24,20 +24,7 @@ COMPILE.S?=	${CC} ${AFLAGS} ${CPPFLAGS} -c -traditional-cpp
 LINK.S?=	${CC} ${AFLAGS} ${CPPFLAGS} ${LDFLAGS}
 
 CC?=		cc
-.if ${MACHINE_ARCH} == "alpha" || \
-    ${MACHINE_ARCH} == "arm" || ${MACHINE_ARCH} == "arm26" || \
-		${MACHINE_ARCH} == "arm32" || \
-    ${MACHINE_ARCH} == "i386" || \
-    ${MACHINE_ARCH} == "m68k" || \
-    ${MACHINE_ARCH} == "mipsel" || ${MACHINE_ARCH} == "mipseb" || \
-    ${MACHINE_ARCH} == "sparc" || \
-    ${MACHINE_ARCH} == "vax"
-DBG?=	-O2
-.elif ${MACHINE_ARCH} == "x86_64"
-DBG?=
-.else
-DBG?=	-O
-.endif
+DBG?=		-O2
 CFLAGS?=	${DBG}
 COMPILE.c?=	${CC} ${CFLAGS} ${CPPFLAGS} -c
 LINK.c?=	${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}
