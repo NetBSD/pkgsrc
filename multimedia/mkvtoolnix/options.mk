7034fa734d86bb4151e567286c93d94dc1bc00bd
joerg <joerg@pkgsrc.org>
2016-09-08 16:18:01 +0000

    Update mkvtoolnix to 9.4.0: - restrict GUI option to Qt. - disable compiler optimisation override - new features:   - support for Apple ProRes videos in MOV/MP4 files   - much improves MPEG TS support   - support WebVTT subtitles   - output of Big Endian PCM to WAV supported   - ${TOOL}_OPTIONS can be used for passing additional arguments to     individual programs via environment   - support for core-less DTS streams consisting solely of XLL extension     sub-streams   - time codes computation for AAC, AC-3, DTS, MP3 and TrueHD packets     was rewritten   - support TrueHD tracks inside MPEG TS by keeping (additional) AC-3     audio track   - support MPEG-H p2/HEVC video in MP4 container   - support for AAC in LOAS/LATM multiplex if read from MPEG transport     streams or raw LOAS/LATM AAC files   - support for h.265/HEVC from MPEG TS and elementary streams   - support for teletext subtitles from MPEG TS   - faster format detection for text subtitle formats   - support for DTS audio streams in MP4   - support for VP9 from IVF and WebM files   - support for Opus   - support for reading ALAC from CAF and MP4 files - 5 years of various bug fixes and GUI improvements.

--- multimedia/mkvtoolnix/options.mk
+++ multimedia/mkvtoolnix/options.mk
@@ -1,36 +1,19 @@
-# $NetBSD: options.mk,v 1.1 2011/03/20 13:19:44 adam Exp $
+# $NetBSD: options.mk,v 1.2 2016/09/08 16:18:01 joerg Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mkvtoolnix
-PKG_SUPPORTED_OPTIONS=	# empty
-PKG_OPTIONS_OPTIONAL_GROUPS=	gui
-PKG_OPTIONS_GROUP.gui=	wxwidgets
+PKG_SUPPORTED_OPTIONS=	gui
 PKG_SUGGESTED_OPTIONS=	# empty
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		gui
+PLIST_SRC=		${PLIST_SRC_DFLT}
 
-.if !empty(PKG_OPTIONS:Mqt) || !empty(PKG_OPTIONS:Mwxwidgets)
-PLIST.gui=		yes
-CONFIGURE_ARGS+=	--enable-gui
+.if !empty(PKG_OPTIONS:Mgui)
+PLIST_SRC+=		PLIST.gui
+CONFIGURE_ARGS+=	--enable-qt MOC=${QTDIR}/bin/moc
+CONFIGURE_ARGS+=	UIC=${QTDIR}/bin/uic RCC=${QTDIR}/bin/rcc
+USE_TOOLS+=		pkg-config
+.include "../../x11/qt5-qtbase/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=	--disable-gui
-.endif
-
-#.if !empty(PKG_OPTIONS:Mqt)
-#CONFIGURE_ARGS+=	--enable-qt
-#.  include "../../x11/qt4-libs/buildlink3.mk"
-#.  include "../../x11/qt4-tools/buildlink3.mk"
-#.else
-#CONFIGURE_ARGS+=	--disable-qt
-#.endif
-
-.if !empty(PKG_OPTIONS:Mwxwidgets)
-CONFIGURE_ARGS+=	--enable-wxwidgets
-.  include "../../databases/shared-mime-info/mimedb.mk"
-.  include "../../graphics/hicolor-icon-theme/buildlink3.mk"
-.  include "../../sysutils/desktop-file-utils/desktopdb.mk"
-.  include "../../x11/wxGTK28/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-wxwidgets
+CONFIGURE_ARGS+=	--disable-qt
 .endif
