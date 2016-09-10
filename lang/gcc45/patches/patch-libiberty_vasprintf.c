b592a05dc5c33c4a3175d679d169e1017a3d0c55
maya <maya@pkgsrc.org>
2016-09-10 13:59:31 +0000

    gcc45: use a more reasonable specfile for netbsd fix some build errors I see (using same patches as lang/gcc46)
    build still failing at:
    unwind-dw2.c:1452:5: error: unknown field 'ptm_ceiling' specified in initializer
    unwind-dw2.c:1452:5: warning: missing braces around initializer
    unwind-dw2.c:1452:5: warning: (near initialization for 'once_regsizes.pto_mutex.<anonymous>')

---
+++ lang/gcc45/patches/patch-libiberty_vasprintf.c
@@ -0,0 +1,24 @@
+$NetBSD: patch-libiberty_vasprintf.c,v 1.1 2016/09/10 13:59:31 maya Exp $
+
+use stdarg, not VA_* macros.
+cherry picked necessary parts from
+https://gcc.gnu.org/viewcvs/gcc?view=revision&revision=206881
+
+fixes PR pkg/50042
+
+--- libiberty/vasprintf.c.orig	2011-02-03 07:23:20.000000000 +0000
++++ libiberty/vasprintf.c
+@@ -165,10 +165,10 @@ static void ATTRIBUTE_PRINTF_1
+ checkit (const char *format, ...)
+ {
+   char *result;
+-  VA_OPEN (args, format);
+-  VA_FIXEDARG (args, const char *, format);
++  va_list args;
++  va_start (args, format);
+   vasprintf (&result, format, args);
+-  VA_CLOSE (args);
++  va_end (args);
+ 
+   if (strlen (result) < (size_t) global_total_width)
+     printf ("PASS: ");
