7872b686e89cc38cb406290b314316326523c652
wiz <wiz@pkgsrc.org>
2016-09-11 16:41:16 +0000

    py-reportlab supports python-3.x.

--- print/hplip/options.mk
+++ print/hplip/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.11 2016/07/09 13:04:02 wiz Exp $
+# $NetBSD: options.mk,v 1.12 2016/09/11 16:41:16 wiz Exp $
 #
 # HPLIP dependencies are detailed in the following page:
 # http://hplipopensource.com/hplip-web/install/manual/distros/other.html
@@ -22,7 +22,6 @@ CONFIGURE_ARGS+=	--enable-dbus-build
 EGFILES+=		cups/pstotiff.convs cups/pstotiff.types
 MAKE_DIRS+=		${PKG_SYSCONFDIR}/cups
 DEPENDS+=	${PYPKGPREFIX}-reportlab-[0-9]*:../../print/py-reportlab
-PYTHON_VERSIONS_INCOMPATIBLE=	34 35 # py-reportlab
 .include "../../sysutils/dbus/buildlink3.mk"
 .include "../../sysutils/py-dbus/buildlink3.mk"
 .else
@@ -36,13 +35,7 @@ CONFIGURE_ARGS+=	--enable-scan-build
 MESSAGE_SRC+=		MESSAGE.scan
 MESSAGE_SUBST+=		EGDIR=${EGDIR}
 .include "../../graphics/sane-backends/buildlink3.mk"
-# XXX: a dependency installs py-Pillow, which conflicts
-# .include "../../graphics/py-imaging/buildlink3.mk"
-# DEPENDS+=	${PYPKGPREFIX}-Pillow-[0-9]*:../../print/py-Pillow
-# version depends on if py-reportlab was installed before we called
-# out an imaging library.
-DEPENDS+=	{${PYPKGPREFIX}-imaging-[0-9]*,${PYPKGPREFIX}-Pillow-[0-9]*}:../../graphics/py-imaging
-# PYTHON_VERSIONS_INCOMPATIBLE=	34 35 # py-imaging
+DEPENDS+=	{${PYPKGPREFIX}-Pillow-[0-9]*,${PYPKGPREFIX}-imaging-[0-9]*}:../../graphics/py-Pillow
 .else
 CONFIGURE_ARGS+=	--disable-scan-build
 .endif
