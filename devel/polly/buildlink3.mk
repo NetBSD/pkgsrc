525ff1f606ebafe54b94ec53bee5be08eb77af75
kamil <kamil@pkgsrc.org>
2016-09-12 22:49:23 +0000

    Import LLVM Polly 3.8.1 as devel/polly
    Polly is a high-level loop and data-locality optimizer and optimization
    infrastructure for LLVM. It uses an abstract mathematical representation based
    on integer polyhedra to analyze and optimize the memory access pattern of a
    program. We currently perform classical loop transformations, especially tiling
    and loop fusion to improve data-locality. Polly can also exploit OpenMP level
    parallelism, expose SIMDization opportunities. Work has also be done in the
    area of automatic GPU code generation.
    
    For many users, however, it's not the existing optimizations in Polly that are
    of most interest, but the new analyses and optimizations enabled by the Polly
    infrastructure. At polyhedral.info you can get an idea of what has already been
    done and what is possible in the context of polyhedral compilation.

---
+++ devel/polly/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/09/12 22:49:23 kamil Exp $
+
+BUILDLINK_TREE+=	polly
+
+.if !defined(POLLY_BUILDLINK3_MK)
+POLLY_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.polly+=	polly>=3.8.1
+BUILDLINK_PKGSRCDIR.polly?=	../../devel/polly
+
+.include "../../lang/llvm/buildlink3.mk"
+.endif	# POLLY_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-polly
