d56b4df591e902a569ac612340d313ca539b674b
wiz <wiz@pkgsrc.org>
2016-09-12 09:07:40 +0000

    Add upstream bug report URL.

--- multimedia/gst-plugins1-good/patches/patch-gst_udp_gstudpsrc.c
+++ multimedia/gst-plugins1-good/patches/patch-gst_udp_gstudpsrc.c
@@ -1,4 +1,7 @@
-$NetBSD: patch-gst_udp_gstudpsrc.c,v 1.1 2016/09/12 09:00:10 wiz Exp $
+$NetBSD: patch-gst_udp_gstudpsrc.c,v 1.2 2016/09/12 09:07:40 wiz Exp $
+
+Fix build on NetBSD, member does not exist.
+https://bugzilla.gnome.org/show_bug.cgi?id=771278
 
 --- gst/udp/gstudpsrc.c.orig	2016-07-11 06:19:14.000000000 +0000
 +++ gst/udp/gstudpsrc.c
