e8b418a0d31c3d6550e9f8a88ccd13976693d31b
bsiegert <bsiegert@pkgsrc.org>
2016-09-13 20:50:25 +0000

    Update google-api-go-client to 20160909.
    This adds a bunch of new APIs, many of which now support gRPC (a binary
    protocol over HTTP/2) instead of JSON and HTTP.

--- devel/google-api-go-client/buildlink3.mk
+++ devel/google-api-go-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/12/29 21:47:48 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/09/13 20:50:25 bsiegert Exp $
 
 BUILDLINK_TREE+=	google-api-go-client
 
@@ -10,6 +10,8 @@ BUILDLINK_DEPMETHOD.google-api-go-client?=	build
 BUILDLINK_API_DEPENDS.google-api-go-client+=	google-api-go-client>=20150507
 BUILDLINK_PKGSRCDIR.google-api-go-client?=	../../devel/google-api-go-client
 
+.include "../../misc/go-genproto/buildlink3.mk"
+.include "../../net/go-grpc/buildlink3.mk"
 .include "../../security/go-oauth2/buildlink3.mk"
 .endif	# GOOGLE_API_GO_CLIENT_BUILDLINK3_MK
 
