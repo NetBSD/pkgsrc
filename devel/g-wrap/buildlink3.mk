b0263b546e0e60f012a8749923c42c13f2abe66a
wiz <wiz@pkgsrc.org>
2016-09-15 14:32:39 +0000

    After guile's and guile20's installation prefixes have changed, it's time to bump their dependencies (except those that were bumped in the last 24 hours already).

--- devel/g-wrap/buildlink3.mk
+++ devel/g-wrap/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2016/09/15 00:01:43 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2016/09/15 14:32:39 wiz Exp $
 
 BUILDLINK_TREE+=	g-wrap
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	g-wrap
 G_WRAP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.g-wrap+=	g-wrap>=1.9.5
-BUILDLINK_ABI_DEPENDS.g-wrap+=	g-wrap>=1.9.14nb3
+BUILDLINK_ABI_DEPENDS.g-wrap+=	g-wrap>=1.9.15
 BUILDLINK_PKGSRCDIR.g-wrap?=	../../devel/g-wrap
 
 .include "../../devel/libffi/buildlink3.mk"
