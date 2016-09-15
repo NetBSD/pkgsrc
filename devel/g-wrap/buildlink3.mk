d6aca6a65f49c9d03ec4c288f4501293a3165004
gdt <gdt@pkgsrc.org>
2016-09-15 00:01:43 +0000

    Update to 1.9.15 and switch to guile20
    This package had been held at 1.9.14 because 1.9.15 requires guile
    2.0.
    
    Changes from 1.9.14:
    
      G-Wrap now requires Guile 2.0, allows Guile 2.2;
    
      Requires autoconf >= 2.61, automake >= 1.12, adding /build-aux
    
      We no longer update Changelog, see G-Wrap's git logs instead
    
      No free/mark on WCT objects, and remove use of scm_gc_free
    
      Use scm_module_variable instead of the deprecated scm_sym2var
    
      The documentation has been restructured, updated and fixed to work with texinfo >= 5.2;
    
      Removing doc/version.texi - it is generated/updated

--- devel/g-wrap/buildlink3.mk
+++ devel/g-wrap/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2012/09/15 10:04:08 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2016/09/15 00:01:43 gdt Exp $
 
 BUILDLINK_TREE+=	g-wrap
 
@@ -10,7 +10,7 @@ BUILDLINK_ABI_DEPENDS.g-wrap+=	g-wrap>=1.9.14nb3
 BUILDLINK_PKGSRCDIR.g-wrap?=	../../devel/g-wrap
 
 .include "../../devel/libffi/buildlink3.mk"
-.include "../../lang/guile/buildlink3.mk"
+.include "../../lang/guile20/buildlink3.mk"
 .endif # G_WRAP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-g-wrap
