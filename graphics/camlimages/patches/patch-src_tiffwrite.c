e302ee257d44a626e56ba87f8ce303542a8e5f52
dholland <dholland@pkgsrc.org>
2016-09-18 05:46:31 +0000

    Fix broken build.

--- graphics/camlimages/patches/patch-src_tiffwrite.c
+++ graphics/camlimages/patches/patch-src_tiffwrite.c
@@ -1,10 +1,14 @@
-$NetBSD: patch-src_tiffwrite.c,v 1.1 2012/07/29 12:52:55 marino Exp $
+$NetBSD: patch-src_tiffwrite.c,v 1.2 2016/09/18 05:46:31 dholland Exp $
 
-Both ocaml/caml/config.h and tiff.h define int32, uint32, etc.
+ - Both ocaml/caml/config.h and tiff.h define int32, uint32, etc.
 The workaround of this name polution is to intentionally overwrite the
 first caml definition with a garbage macro before tiff.h is included and
 then unset it after the include.  After tiff version 4.0.1, the same hack
-has to be applied to 64-bit types
+has to be applied to 64-bit types.
+
+ - As of some newer ocaml there's no "uint32", only "uint32_t". The
+first two hunks of this patch are probably no longer necessary.
+
 
 --- src/tiffwrite.c.orig	2011-06-22 18:04:32.000000000 +0000
 +++ src/tiffwrite.c
@@ -26,3 +30,14 @@ has to be applied to 64-bit types
  
  extern value *imglib_error;
  
+@@ -56,8 +60,8 @@ value open_tiff_file_for_write( value fi
+     /* Resolution */
+     /* FillOrder */
+     
+-    TIFFSetField(tif, TIFFTAG_IMAGEWIDTH, (uint32) image_width);
+-    TIFFSetField(tif, TIFFTAG_IMAGELENGTH, (uint32) image_height);
++    TIFFSetField(tif, TIFFTAG_IMAGEWIDTH, (uint32_t) image_width);
++    TIFFSetField(tif, TIFFTAG_IMAGELENGTH, (uint32_t) image_height);
+     TIFFSetField(tif, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);
+     TIFFSetField(tif, TIFFTAG_SAMPLESPERPIXEL, 3);
+     TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 8);
