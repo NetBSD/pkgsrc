3d0b4df4348ab91b6166d03a537a8f72b142749a
kamil <kamil@pkgsrc.org>
2016-09-18 17:15:02 +0000

    Remove pkgsrc-wip reference

--- graphics/nvidia-texture-tools/buildlink3.mk
+++ graphics/nvidia-texture-tools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/09/18 17:08:20 kamil Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/09/18 17:15:02 kamil Exp $
 
 BUILDLINK_TREE+=	nvidia-texture-tools
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	nvidia-texture-tools
 NVIDIA_TEXTURE_TOOLS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.nvidia-texture-tools+=	nvidia-texture-tools>=2.0.8
-BUILDLINK_PKGSRCDIR.nvidia-texture-tools?=	../../wip/nvidia-texture-tools
+BUILDLINK_PKGSRCDIR.nvidia-texture-tools?=	../../graphics/nvidia-texture-tools
 
 .include "../../graphics/openexr/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
