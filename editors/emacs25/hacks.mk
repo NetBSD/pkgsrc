81afe4ee179d663231f027d8e32ad1fbc5cf41cd
ryoon <ryoon@pkgsrc.org>
2016-09-18 13:36:18 +0000

    Import emacs25-25.1 as editors/emacs25.
    GNU Emacs is an extensible, customizable editor textand more.  At its
    core is an interpreter for Emacs Lisp, a dialect of the Lisp
    programming language with extensions to support text editing.  The
    features of GNU Emacs include:
    
    - Content-sensitive editing modes, including syntax coloring, for a
      wide variety of file types including plain text, source code, and
      HTML.
    - Complete built-in documentation, including a tutorial for new users.
    - Support for many languages and their scripts, including all scripts,
      Russian, Greek, Japanese, Chinese, Korean, Thai, Vietnamese, Lao,
      Ethiopian, and some Indian scripts.
    - Highly customizable, using Emacs Lisp code or a graphical
      customization interface.
    - A large number of extensions that add other functionality, including
      a project planner, mail and news reader, debugger interface,
      calendar, and more.  Many of these extensions are distributed with
      GNU Emacs; others are available separately.
    
    This package contains Emacs version 25.

---
+++ editors/emacs25/hacks.mk
@@ -0,0 +1,33 @@
+# $NetBSD: hacks.mk,v 1.1 2016/09/18 13:36:18 ryoon Exp $
+
+.if !defined(EMACS_HACKS_MK)
+EMACS_HACKS_MK=	defined
+
+.include "../../mk/compiler.mk"
+
+### On NetBSD/amd64 7.99.26, gcc optimisation, at least for version 5.x,
+### produces, a "temacs" binary which segfaults.
+###
+.if !empty(MACHINE_PLATFORM:MNetBSD-*)
+.  if !empty(CC_VERSION:Mgcc-5.*)
+PKG_HACKS+=		optimisation
+CFLAGS+=		-fno-optimize-strlen
+.  endif
+.endif
+
+### PaX is enabled, bootstrap-emacs command dumps core with segfault.
+### emacs-24.5/src/Makefile.in does not support NetBSD paxctl(8) syntax.
+### http://debbugs.gnu.org/cgi/bugreport.cgi?bug=23371
+###
+.if !empty(MACHINE_PLATFORM:MNetBSD-*)
+.  if exists(/usr/sbin/paxctl)
+SUBST_CLASSES+=			paxctl
+SUBST_STAGE.paxctl=		pre-configure
+SUBST_MESSAGE.paxctl=		Setting paxctl command
+SUBST_FILES.paxctl+=		src/Makefile.in
+SUBST_SED.paxctl+=		-e 's,test "X$$(PAXCTL)" = X || $$(PAXCTL) -zex,/usr/sbin/paxctl +a,g'
+SUBST_SED.paxctl+=		-e 's,test "X$$(PAXCTL)" = X || $$(PAXCTL) -r,/usr/sbin/paxctl +a,g'
+.  endif
+.endif
+
+.endif  # EMACS_HACKS_MK
