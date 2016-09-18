b7e32dd5663fe4e9cd0f9977d453c4474d553b55
ryoon <ryoon@pkgsrc.org>
2016-09-18 13:36:18 +0000

    Import emacs25-25.1 as editors/emacs25.
    GNU Emacs is an extensible, customizable editor textand more.  At its
    core is an interpreter for Emacs Lisp, a dialect of the Lisp
    programming language with extensions to support text editing.  The
    features of GNU Emacs include:
    
    - Content-sensitive editing modes, including syntax coloring, for a
      wide variety of file types including plain text, source code, and
      HTML.
    - Complete built-in documentation, including a tutorial for new users.
    - Support for many languages and their scripts, including all scripts,
      Russian, Greek, Japanese, Chinese, Korean, Thai, Vietnamese, Lao,
      Ethiopian, and some Indian scripts.
    - Highly customizable, using Emacs Lisp code or a graphical
      customization interface.
    - A large number of extensions that add other functionality, including
      a project planner, mail and news reader, debugger interface,
      calendar, and more.  Many of these extensions are distributed with
      GNU Emacs; others are available separately.
    
    This package contains Emacs version 25.

---
+++ editors/emacs25/files/site-init.el
@@ -0,0 +1,4 @@
+;; $NetBSD: site-init.el,v 1.1 2016/09/18 13:36:18 ryoon Exp $
+
+(autoload 'send-pr "/usr/share/gnats/send-pr.el" 
+  "Command to create and send a problem report." t)
---
+++ editors/emacs25/version.mk
@@ -0,0 +1,7 @@
+# $NetBSD: version.mk,v 1.1 2016/09/18 13:36:18 ryoon Exp $
+
+_EMACS_FLAVOR=	emacs
+_EMACS_REQD=	emacs25>=25.1<26
+
+_EMACS_VERSION_MAJOR=	25
+_EMACS_VERSION_MINOR=	1
