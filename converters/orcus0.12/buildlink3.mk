1977c3cfe4c8757131bf22b4d11c42bcaf6749d2
wiz <wiz@pkgsrc.org>
2016-09-19 14:20:33 +0000

    Rename package to match PKGNAME, and adapt bl3.mk.

--- converters/orcus0.12/buildlink3.mk
+++ converters/orcus0.12/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/09/19 14:10:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/09/19 14:20:33 wiz Exp $
 
-BUILDLINK_TREE+=	orcus
+BUILDLINK_TREE+=	orcus012
 
-.if !defined(ORCUS_BUILDLINK3_MK)
-ORCUS_BUILDLINK3_MK:=
+.if !defined(ORCUS012_BUILDLINK3_MK)
+ORCUS012_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.orcus+=	orcus>=0.9.0
-BUILDLINK_PKGSRCDIR.orcus?=	../../converters/orcus
+BUILDLINK_API_DEPENDS.orcus012+=	orcus0.12>=0.12.0
+BUILDLINK_PKGSRCDIR.orcus012?=	../../converters/orcus0.12
 
 #.include "../../devel/boost-libs/buildlink3.mk"
 #.include "../../devel/zlib/buildlink3.mk"
-.endif	# ORCUS_BUILDLINK3_MK
+.endif	# ORCUS012_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-orcus
+BUILDLINK_TREE+=	-orcus012
