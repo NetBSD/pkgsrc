adecc2afcfbe9efd54b62a94093bfdfca896f9fc
wiz <wiz@pkgsrc.org>
2016-09-20 10:52:53 +0000

    Add libixion dependency. Add missing includes to bl3.mk. Bump PKGREVISION.

--- converters/orcus0.12/buildlink3.mk
+++ converters/orcus0.12/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/09/19 14:20:33 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/09/20 10:52:53 wiz Exp $
 
 BUILDLINK_TREE+=	orcus012
 
@@ -8,8 +8,8 @@ ORCUS012_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.orcus012+=	orcus0.12>=0.12.0
 BUILDLINK_PKGSRCDIR.orcus012?=	../../converters/orcus0.12
 
-#.include "../../devel/boost-libs/buildlink3.mk"
-#.include "../../devel/zlib/buildlink3.mk"
+.include "../../devel/mdds1.2/buildlink3.mk"
+.include "../../math/libixion/buildlink3.mk"
 .endif	# ORCUS012_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-orcus012
