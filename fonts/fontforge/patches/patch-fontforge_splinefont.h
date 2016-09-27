59b2199a1020078184c36caf7131c9dfe3dc8f3c
taca <taca@pkgsrc.org>
2016-09-27 10:24:56 +0000

    Fix build problem on NetBSD 6.

--- fonts/fontforge/patches/patch-fontforge_splinefont.h
+++ fonts/fontforge/patches/patch-fontforge_splinefont.h
@@ -1,15 +1,26 @@
-$NetBSD: patch-fontforge_splinefont.h,v 1.1 2016/02/07 14:09:08 adam Exp $
+$NetBSD: patch-fontforge_splinefont.h,v 1.2 2016/09/27 10:24:57 taca Exp $
 
-NetBSD does not implement uselocale().
+* NetBSD does not implement uselocale().
+* NetBSD prior to 7.0 dose not have locale_t.
 
---- fontforge/splinefont.h.orig	2016-02-07 13:01:04.000000000 +0000
+--- fontforge/splinefont.h.orig	2016-04-05 01:29:03.000000000 +0000
 +++ fontforge/splinefont.h
-@@ -3548,6 +3548,10 @@ char * delimit_null(const char * input, 
+@@ -3549,6 +3549,20 @@ char * delimit_null(const char * input, 
  
  #include "ustring.h"
  
 +#ifdef __NetBSD__
 +#define BAD_LOCALE_HACK
++#ifndef __LOCALE_T_DECLARED
++typedef char* locale_t;
++#define LC_GLOBAL_LOCALE ((locale_t)-1)
++#define LC_ALL_MASK LC_ALL
++#define LC_COLLATE_MASK LC_COLLATE
++#define LC_CTYPE_MASK LC_CTYPE
++#define LC_MONETARY_MASK LC_MONETARY
++#define LC_NUMERIC_MASK LC_NUMERIC
++#define LC_TIME_MASK LC_TIME
++#endif
 +#endif
 +
  #ifdef _WIN32
