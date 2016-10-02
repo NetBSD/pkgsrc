5161d1724dd02b5eb3993f7d5315e6a9c9f8c552
fhajny <fhajny@pkgsrc.org>
2016-10-02 19:21:30 +0000

    Ensure upper limit in mysql{56,57}-client respective bl3.

--- databases/mysql56-client/buildlink3.mk
+++ databases/mysql56-client/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2016/03/05 11:27:43 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/10/02 19:21:30 fhajny Exp $
 
 BUILDLINK_TREE+=	mysql-client
 
 .if !defined(MYSQL_CLIENT_BUILDLINK3_MK)
 MYSQL_CLIENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mysql-client+=	mysql-client>=5.6.10
+BUILDLINK_API_DEPENDS.mysql-client+=	mysql-client>=5.6.10<5.7
 BUILDLINK_ABI_DEPENDS.mysql-client+=	mysql-client>=5.6.29nb1
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql56-client
 BUILDLINK_INCDIRS.mysql-client?=	include/mysql
