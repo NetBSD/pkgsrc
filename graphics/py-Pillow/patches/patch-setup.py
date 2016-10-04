6304fb1cefc9f04a9e135dd483075d8987750656
adam <adam@pkgsrc.org>
2016-10-04 19:30:24 +0000

    3.4.1 (2016-10-04) ------------------ - Allow lists as arguments for Image.new() - Fix fix for map.c overflow (also in 3.3.3)
    3.4.0 (2016-10-03)
    ------------------
    - Removed Image.core.open_ppm, added negative image size checks in Image.py.
    - Windows build: fetch dependencies from pillow-depends
    - Add TIFF save_all writer.
    - Move libtiff fd duplication to _load_libtiff
    - Speed up GIF save optimization step
    - Fix for ImageCms Segfault
    - Make Image.crop an immediate operation, not lazy.
    - Skip empty values in ImageFileDirectory
    - Force reloading palette when using mmap in ImageFile.
    - Fix "invalid escape sequence" warning in Python 3.6
    - Update documentation about drafts
    - Converted documentation parameter format, comments to docstrings
    - Fixed typos
    - Renamed references to OS X to macOS
    - Use truth value when checking for progressive and optimize option on save
    - Convert DPI to ints when saving as JPEG
    - Added append_images parameter to GIF saving
    - Speedup paste with masks up to 80%
    - Rewrite DDS decoders in C, add DXT3 and BC7 decoders
    - Fix PyArg_ParseTuple format in getink()
    - Fix saving originally missing TIFF tags.
    - Allow pathlib.Path in Image.open on Python 2.7
    - Use modern base64 interface over deprecated
    - ImageColor.getrgb hexadecimal RGBA
    - Test fix for bigendian machines
    - Resampling lookups, trailing empty coefficients, precision
    - Add (un)packing between RGBA and BGRa
    - Added return for J2k (and fpx) Load to return a pixel access object
    - Skip failing numpy tests on Pypy <= 5.3.1
    - Show warning when trying to save RGBA image as JPEG
    - Respect pixel centers during transform
    - TOC for supported file formats
    - Fix conversion of bit images to numpy arrays
    - Add ImageOps.scale to expand or contract a PIL image by a factor
    - Flake8 fixes
    - Updated freetype to 2.6.5 on Appveyor builds
    - PCX encoder fixes
    - Docs: Windows console prompts are >
    - Expose Pillow package version as PIL.__version__
    - Add Box and Hamming filters for resampling
    - Retain a reference to core image object in PyAccess

--- graphics/py-Pillow/patches/patch-setup.py
+++ graphics/py-Pillow/patches/patch-setup.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-setup.py,v 1.7 2016/08/13 08:07:22 wen Exp $
+$NetBSD: patch-setup.py,v 1.8 2016/10/04 19:30:24 adam Exp $
 
 * Prevent to detect optional tkinter.
 * Disable demo programs.
 
---- setup.py.orig	2016-08-13 07:58:03.000000000 +0000
+--- setup.py.orig	2016-10-04 14:54:44.000000000 +0000
 +++ setup.py
 @@ -103,11 +103,7 @@ def _read(file):
      return open(file, 'rb').read()
@@ -17,7 +17,7 @@ $NetBSD: patch-setup.py,v 1.7 2016/08/13 08:07:22 wen Exp $
 +_tkinter = None
  
  NAME = 'Pillow'
- PILLOW_VERSION = '3.3.0'
+ PILLOW_VERSION = '3.4.1'
 @@ -746,7 +742,6 @@ setup(name=NAME,
        ext_modules=[Extension("PIL._imaging", ["_imaging.c"])],
        include_package_data=True,
