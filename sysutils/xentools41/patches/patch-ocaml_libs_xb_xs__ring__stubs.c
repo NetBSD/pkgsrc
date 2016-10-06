aee4741fc21bce215c1a0b35ad4edc25a203da4f
bsiegert <bsiegert@pkgsrc.org>
2016-10-06 18:39:27 +0000

    Pullup ticket #5121 - requested by joerg sysutils/xentools41: build fix sysutils/xentools42: build fix
    Revisions pulled up:
    - sysutils/xentools41/distinfo                                  1.42
    - sysutils/xentools41/patches/patch-ocaml_libs_xb_xs__ring__stubs.c 1.1
    - sysutils/xentools42/distinfo                                  1.28
    - sysutils/xentools42/patches/patch-ocaml_libs_xb_xs__ring__stubs.c 1.1
    
    ---
       Module Name:	pkgsrc
       Committed By:	joerg
       Date:		Sat Oct  1 13:07:23 UTC 2016
    
       Modified Files:
       	pkgsrc/sysutils/xentools41: distinfo
       	pkgsrc/sysutils/xentools42: distinfo
       Added Files:
       	pkgsrc/sysutils/xentools41/patches:
       	    patch-ocaml_libs_xb_xs__ring__stubs.c
       	pkgsrc/sysutils/xentools42/patches:
       	    patch-ocaml_libs_xb_xs__ring__stubs.c
    
       Log Message:
       OCAML has switched to using normal uint32_t, so follow here.

---
+++ sysutils/xentools41/patches/patch-ocaml_libs_xb_xs__ring__stubs.c
@@ -0,0 +1,26 @@
+$NetBSD: patch-ocaml_libs_xb_xs__ring__stubs.c,v 1.1.2.2 2016/10/06 18:39:28 bsiegert Exp $
+
+--- ocaml/libs/xb/xs_ring_stubs.c.orig	2016-09-30 12:57:34.000000000 +0000
++++ ocaml/libs/xb/xs_ring_stubs.c
+@@ -46,8 +46,8 @@ static int xs_ring_read(struct mmap_inte
+ 	XENSTORE_RING_IDX cons, prod; /* offsets only */
+ 	int to_read;
+ 
+-	cons = *(volatile uint32*)&intf->req_cons;
+-	prod = *(volatile uint32*)&intf->req_prod;
++	cons = *(volatile uint32_t*)&intf->req_cons;
++	prod = *(volatile uint32_t*)&intf->req_prod;
+ 	xen_mb();
+ 
+ 	if ((prod - cons) > XENSTORE_RING_SIZE)
+@@ -76,8 +76,8 @@ static int xs_ring_write(struct mmap_int
+ 	XENSTORE_RING_IDX cons, prod;
+ 	int can_write;
+ 
+-	cons = *(volatile uint32*)&intf->rsp_cons;
+-	prod = *(volatile uint32*)&intf->rsp_prod;
++	cons = *(volatile uint32_t*)&intf->rsp_cons;
++	prod = *(volatile uint32_t*)&intf->rsp_prod;
+ 	xen_mb();
+ 	if ( (prod - cons) >= XENSTORE_RING_SIZE )
+ 		return 0;
