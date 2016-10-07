339c10bcc9b09ab2d435c02307aa8106dbb216a9
joerg <joerg@pkgsrc.org>
2016-10-07 13:22:10 +0000

    Use sanitized _USE_CWRAPPERS.

--- sysutils/cdrtools/hacks.mk
+++ sysutils/cdrtools/hacks.mk
@@ -1,9 +1,9 @@
-# $NetBSD: hacks.mk,v 1.4 2016/07/21 19:38:31 tron Exp $
+# $NetBSD: hacks.mk,v 1.5 2016/10/07 13:22:10 joerg Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
 .if ${OPSYS} == "Darwin" && defined(ABI) && ${ABI} == "64" && \
-    ${USE_CWRAPPERS:tl} != "no"
+    ${_USE_CWRAPPERS} == "yes"
 #
 # PR pkg/51257: "sysutils/cdrtools" fails to build under Mac OS X 64bit
 #               when cwrappers are used
