72a72e3bdf5011e935ad1ff091d32d5155093f69
adam <adam@pkgsrc.org>
2016-10-07 18:25:29 +0000

    Revbump post boost update

--- converters/libwpd/buildlink3.mk
+++ converters/libwpd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2015/08/15 22:46:39 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2016/10/07 18:25:30 adam Exp $
 
 BUILDLINK_TREE+=	libwpd
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libwpd
 LIBWPD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libwpd+=	libwpd>=0.9.5
-BUILDLINK_ABI_DEPENDS.libwpd+=	libwpd>=0.10.0nb1
+BUILDLINK_ABI_DEPENDS.libwpd+=	libwpd>=0.10.1nb1
 BUILDLINK_PKGSRCDIR.libwpd?=	../../converters/libwpd
 
 .include "../../converters/librevenge/buildlink3.mk"
--- security/liboauth/buildlink3.mk
+++ security/liboauth/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2016/03/05 11:27:54 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2016/10/07 18:25:34 adam Exp $
 
 BUILDLINK_TREE+=	liboauth
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	liboauth
 LIBOAUTH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.liboauth+=	liboauth>=0.8.9
-BUILDLINK_ABI_DEPENDS.liboauth?=	liboauth>=1.0.3nb1
+BUILDLINK_ABI_DEPENDS.liboauth?=	liboauth>=1.0.3nb2
 BUILDLINK_PKGSRCDIR.liboauth?=	../../security/liboauth
 
 .include "../../security/openssl/buildlink3.mk"
