d3bc8d92619a0dc61850b69d96fede7c0f1d58b6
adam <adam@pkgsrc.org>
2016-10-07 18:25:29 +0000

    Revbump post boost update

--- devel/netcdf/buildlink3.mk
+++ devel/netcdf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2016/03/05 11:27:46 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2016/10/07 18:25:31 adam Exp $
 
 BUILDLINK_TREE+=	netcdf
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	netcdf
 NETCDF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.netcdf+=	netcdf>=3.5
-BUILDLINK_ABI_DEPENDS.netcdf+=	netcdf>=4.2.1.1nb3
+BUILDLINK_ABI_DEPENDS.netcdf+=	netcdf>=4.2.1.1nb4
 BUILDLINK_PKGSRCDIR.netcdf?=	../../devel/netcdf
 
 .include "../../devel/hdf5/buildlink3.mk"
