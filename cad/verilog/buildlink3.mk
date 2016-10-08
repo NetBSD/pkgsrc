024409f4db2e705cd3119b9f7aed65fc359d9b72
kamil <kamil@pkgsrc.org>
2016-10-08 22:30:43 +0000

    Update cad/verilog (icarus verilog) from 0.9.7 to 10.1.1
    pkgsrc changes:
     - note GitHub tags (but not use them for now)
     - remove conflict with nonexistent verilog-current
     - install additional documentation in share/doc/ivl (not share/ivl)
     - drop DESTDIR gymnastics - build works without it
     - (re)enable gperf dependency
     - regenerate buildlink3.mk
     - drop patches/patch-lexor_keyword.cc - no longer needed
     - patches/patch-vpi_Makefile partially fixed upstream - rest not needed
    
    upstream changelog
    ==================
    
    Probably the only notes available:
    
    Here are the release notes for Icarus Verilog release branch 10. The 10
    release is a huge improvement over the 0.9 release series, in every
    aspect. Much more of the Verilog and SystemVerilog language is supported,
    many bugs have been fixed, and performance has improved. The changes
    (improvements!) are so numerous that there is no point attempting to
    enumerate them.
    
     -- http://iverilog.wikia.com/wiki/Release_Notes_Icarus_Verilog_10

--- cad/verilog/buildlink3.mk
+++ cad/verilog/buildlink3.mk
@@ -1,18 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.10 2013/07/18 07:59:38 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2016/10/08 22:30:43 kamil Exp $
 
 BUILDLINK_TREE+=	verilog
 
 .if !defined(VERILOG_BUILDLINK3_MK)
 VERILOG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.verilog+=	verilog>=0.8.1nb1
-BUILDLINK_ABI_DEPENDS.verilog+=	verilog>=0.8.7nb1
+BUILDLINK_API_DEPENDS.verilog+=	verilog>=10.1.1
 BUILDLINK_PKGSRCDIR.verilog?=	../../cad/verilog
 
+.include "../../devel/gperf/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../archivers/bzip2/buildlink3.mk"
-.include "../../devel/gperf/buildlink3.mk"
-.include "../../mk/readline.buildlink3.mk"
-.endif # VERILOG_BUILDLINK3_MK
+.endif	# VERILOG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-verilog
