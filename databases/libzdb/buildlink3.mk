729c35ceb7b85564ea9cf65ced8e9e0a150334bd
wiz <wiz@pkgsrc.org>
2016-10-09 21:41:55 +0000

    Recursive bump for all users of pgsql now that the default is 95.

--- databases/libzdb/buildlink3.mk
+++ databases/libzdb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2016/04/11 19:01:34 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/10/09 21:41:56 wiz Exp $
 
 BUILDLINK_TREE+=	libzdb
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libzdb
 LIBZDB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libzdb+=	libzdb>=2.6
-BUILDLINK_ABI_DEPENDS.libzdb?=	libzdb>=3.1nb3
+BUILDLINK_ABI_DEPENDS.libzdb?=	libzdb>=3.1nb4
 BUILDLINK_PKGSRCDIR.libzdb?=	../../databases/libzdb
 
 pkgbase := libzdb
--- geography/gdal-lib/buildlink3.mk
+++ geography/gdal-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.41 2016/10/07 18:25:31 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.42 2016/10/09 21:41:58 wiz Exp $
 
 BUILDLINK_TREE+=	gdal-lib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gdal-lib
 GDAL_LIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gdal-lib+=	gdal-lib>=1.6.1
-BUILDLINK_ABI_DEPENDS.gdal-lib+=	gdal-lib>=1.11.3nb4
+BUILDLINK_ABI_DEPENDS.gdal-lib+=	gdal-lib>=1.11.3nb5
 BUILDLINK_PKGSRCDIR.gdal-lib?=	../../geography/gdal-lib
 
 pkgbase := gdal-lib
