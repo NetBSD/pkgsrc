b6f39dc0d4f2ed5f5dbecf17f5812263e9dea3ee
wiz <wiz@pkgsrc.org>
2016-10-09 21:41:55 +0000

    Recursive bump for all users of pgsql now that the default is 95.

--- databases/postgresql-postgis2/buildlink3.mk
+++ databases/postgresql-postgis2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2015/10/24 21:18:12 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2016/10/09 21:41:57 wiz Exp $
 
 BUILDLINK_TREE+=	postgresql-postgis
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	postgresql-postgis
 POSTGRESQL_POSTGIS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.postgresql-postgis+=	postgresql${PGSQL_VERSION}-postgis>=1.4
-BUILDLINK_ABI_DEPENDS.postgresql-postgis?=	postgresql${PGSQL_VERSION}-postgis>=2.1.4nb2
+BUILDLINK_ABI_DEPENDS.postgresql-postgis?=	postgresql${PGSQL_VERSION}-postgis>=2.2.2nb3
 BUILDLINK_PKGSRCDIR.postgresql-postgis?=	../../databases/postgresql-postgis2
 
 PGSQL_VERSIONS_ACCEPTED=	94 93 92 91
