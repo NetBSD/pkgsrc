f0305d9ed2d8ebbb3ae3bd23ce15aeeb50256af5
tsutsui <tsutsui@pkgsrc.org>
2016-10-10 13:22:22 +0000

    Misc pkgsrc tweaks for the latest jwm-2.3.6:
    - LICENSE has been changed from GPLv2 to MIT since 2.3.6
    - use proper ${PREFIX} in system.jwmrc for default icons
    - add and enable svg option in options.mk for default icons
    - explicitly pass --disable-foo to CONFIGURE_ARGS for disabled options
    
    While here, avoid SUBST on post-patch for future mkpatches ops.
    
    Bump PKGREVISION.

--- wm/jwm/options.mk
+++ wm/jwm/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.5 2014/01/19 15:27:16 tsutsui Exp $
+# $NetBSD: options.mk,v 1.6 2016/10/10 13:22:22 tsutsui Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.jwm
-PKG_SUPPORTED_OPTIONS=	debug fribidi jpeg png
-PKG_SUGGESTED_OPTIONS=	fribidi jpeg png
+PKG_SUPPORTED_OPTIONS=	debug fribidi jpeg png svg
+PKG_SUGGESTED_OPTIONS=	fribidi jpeg png svg
 
 .include "../../mk/bsd.options.mk"
 
@@ -13,14 +13,24 @@ CONFIGURE_ARGS+=	--enable-debug
 .if !empty(PKG_OPTIONS:Mfribidi)
 .include "../../converters/fribidi/buildlink3.mk"
 BUILDLINK_API_DEPENDS.fribidi+=   fribidi>=0.19.2
+.else
+CONFIGURE_ARGS+=	--disable-fribidi
 .endif
 
 .if !empty(PKG_OPTIONS:Mjpeg)
 .include "../../mk/jpeg.buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-jpeg
 .endif
 
 .if !empty(PKG_OPTIONS:Mpng)
 .include "../../graphics/png/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-png
+.endif
+
+.if !empty(PKG_OPTIONS:Msvg)
+.include "../../graphics/librsvg/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-rsvg
 .endif
