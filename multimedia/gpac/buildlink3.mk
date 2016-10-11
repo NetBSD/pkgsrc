3a6d42acdde8594cb15707729c0e25b519241c78
tnn <tnn@pkgsrc.org>
2016-10-11 00:31:28 +0000

    Update to gpac-0.6.1
    0.6.1:
    Fixing packaging bugs in 0.6.0 (wrong version numbers were used).
    
    0.6.0:
    General:
        Improved very high resolutions, high framerate, and high colorspace (10 bits) playback support.
        HEVC top-notch support.
        Improved video playback through OpenGL and OpenGL ES.
        Video 360 navigation support on all platforms.
        Many adaptive streaming improvement (HLS and DASH), see specific section below.
        MP42TS (MPEG2-TS multiplexer) is production grade for adaptive streaming and reference vector streams.
        TEMI extension for fine synchronization and hybrid delivery (mandated by HbbTV 2.0).
        Better language tagging support.
        UTF16 name handling.
        Better multi-channel audio support.
        Improved support for encryption (CENC/PIFF - both AES CTR and CBC are supported).
        Improved generic support for subtitles (METX/METT, STPP TTML, WebVTT, SBTT, STXT, etc.).
        Parallel execution of GPAC tools.
        Better network support for TTL and multi-interface.
        SHVC (scalable) experimental (based on SHM4.1 and old ISOBMFF spec)

--- multimedia/gpac/buildlink3.mk
+++ multimedia/gpac/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2012/12/09 17:15:06 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/10/11 00:31:28 tnn Exp $
 
 BUILDLINK_TREE+=	gpac
 
 .if !defined(GPAC_BUILDLINK3_MK)
 GPAC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gpac+=	gpac>=0.5.0
-BUILDLINK_ABI_DEPENDS.gpac+=	gpac>=0.5.0
+BUILDLINK_API_DEPENDS.gpac+=	gpac>=0.6.0
+BUILDLINK_ABI_DEPENDS.gpac+=	gpac>=0.6.0
 BUILDLINK_PKGSRCDIR.gpac?=	../../multimedia/gpac
 .endif # GPAC_BUILDLINK3_MK
 
