65b01a4819b4a76c8701e9faf8d90181d5066f8e
ryoon <ryoon@pkgsrc.org>
2016-10-12 13:57:13 +0000

    Fix a wrong preprocessor condition and pass initialize variable to kvm_getproc2 From OBATA Akio, thank you. Bump PKGREVISION.

--- sysutils/open-vm-tools/patches/patch-vgauth_common_VGAuthLog.c
+++ sysutils/open-vm-tools/patches/patch-vgauth_common_VGAuthLog.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-vgauth_common_VGAuthLog.c,v 1.1 2016/10/09 03:41:56 ryoon Exp $
+$NetBSD: patch-vgauth_common_VGAuthLog.c,v 1.2 2016/10/12 13:57:13 ryoon Exp $
 
 --- vgauth/common/VGAuthLog.c.orig	2016-02-16 20:06:48.000000000 +0000
 +++ vgauth/common/VGAuthLog.c
@@ -7,7 +7,7 @@ $NetBSD: patch-vgauth_common_VGAuthLog.c,v 1.1 2016/10/09 03:41:56 ryoon Exp $
     buf[sizeof buf - 1] = '\0';
  
 -#ifdef sun
-+#if defined(sun) && defined(__NetBSD__)
++#if defined(sun) || defined(__NetBSD__)
     strerror_r(code, errMsg, sizeof errMsg);
     g_warning("[function %s, file %s, line %d], %s, [errno = %d], %s\n",
               func, file, line, buf, code, errMsg);
