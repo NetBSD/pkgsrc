82d7b915bda3ea5e0320b8089fb5af4c9a2d14b1
adam <adam@pkgsrc.org>
2016-10-14 19:28:28 +0000

    Changes 5.7.16:
    Security Notes
    --------------
    * Incompatible Change: For STANDALONE and WIN builds, the default secure_file_priv value has changed from the empty string to NULL. This is a secure-by-default setting because it disables import and export operations. To permit those operations, set secure_file_priv to the path name of the directory to use for those operations.
    * The linked OpenSSL library for the MySQL Commercial Server has been updated to version 1.0.1u. For a description of issues fixed in this version, see http://www.openssl.org/news/vulnerabilities.html.
    This change does not affect the Oracle-produced MySQL Community build of MySQL Server, which uses the yaSSL library instead.
    
    Functionality Added or Changed
    ------------------------------
    * yaSSL was upgraded to version 2.4.2. This upgrade corrects issues with: Potential AES side channel leaks; DSA padding for unusual sizes; the SSL_CTX_load_verify_locations() OpenSSL compatibility function failing to handle long path directory names.

--- databases/mysql57-client/patches/patch-cmake_boost.cmake
+++ databases/mysql57-client/patches/patch-cmake_boost.cmake
@@ -1,4 +1,4 @@
-$NetBSD: patch-cmake_boost.cmake,v 1.1 2016/09/16 06:49:11 adam Exp $
+$NetBSD: patch-cmake_boost.cmake,v 1.2 2016/10/14 19:28:28 adam Exp $
 
 Fix to use a newer version of Boost.
 
@@ -9,7 +9,7 @@ Fix to use a newer version of Boost.
  # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  
 -# We want boost 1.59.0 in order to build our boost/geometry code.
-+# We want boost 1.61.0 in order to build our boost/geometry code.
++# We want boost 1.62.0 in order to build our boost/geometry code.
  # The boost tarball is fairly big, and takes several minutes
  # to download. So we recommend downloading/unpacking it
  # only once, in a place visible from any bzr sandbox.
@@ -18,11 +18,11 @@ Fix to use a newer version of Boost.
  # is installed on the compile host in the standard location.
  
 -SET(BOOST_PACKAGE_NAME "boost_1_59_0")
-+SET(BOOST_PACKAGE_NAME "boost_1_61_0")
++SET(BOOST_PACKAGE_NAME "boost_1_62_0")
  SET(BOOST_TARBALL "${BOOST_PACKAGE_NAME}.tar.gz")
  SET(BOOST_DOWNLOAD_URL
 -  "http://sourceforge.net/projects/boost/files/boost/1.59.0/${BOOST_TARBALL}"
-+  "http://sourceforge.net/projects/boost/files/boost/1.61.0/${BOOST_TARBALL}"
++  "http://sourceforge.net/projects/boost/files/boost/1.62.0/${BOOST_TARBALL}"
    )
  
  SET(OLD_PACKAGE_NAMES "boost_1_55_0 boost_1_56_0 boost_1_57_0 boost_1_58_0")
@@ -31,7 +31,7 @@ Fix to use a newer version of Boost.
  # //  BOOST_VERSION / 100 % 1000 is the minor version
  # //  BOOST_VERSION / 100000 is the major version
 -# #define BOOST_VERSION 105900
-+# #define BOOST_VERSION 106100
++# #define BOOST_VERSION 106200
  FILE(STRINGS "${BOOST_INCLUDE_DIR}/boost/version.hpp"
    BOOST_VERSION_NUMBER
    REGEX "^#define[\t ]+BOOST_VERSION[\t ][0-9]+.*"
@@ -40,10 +40,10 @@ Fix to use a newer version of Boost.
  ENDIF()
  
 -IF(NOT BOOST_MINOR_VERSION EQUAL 59)
-+IF(NOT BOOST_MINOR_VERSION EQUAL 61)
++IF(NOT BOOST_MINOR_VERSION EQUAL 62)
    MESSAGE(WARNING "Boost minor version found is ${BOOST_MINOR_VERSION} "
 -    "we need 59"
-+    "we need 61"
++    "we need 62"
      )
    COULD_NOT_FIND_BOOST()
  ENDIF()
@@ -52,11 +52,11 @@ Fix to use a newer version of Boost.
  
  # We have a limited set of patches/bugfixes here:
 -SET(BOOST_PATCHES_DIR "${CMAKE_SOURCE_DIR}/include/boost_1_59_0/patches")
-+SET(BOOST_PATCHES_DIR "${CMAKE_SOURCE_DIR}/include/boost_1_61_0/patches")
++SET(BOOST_PATCHES_DIR "${CMAKE_SOURCE_DIR}/include/boost_1_62_0/patches")
  
  # We have a limited set of source files here:
 -SET(BOOST_SOURCES_DIR "${CMAKE_SOURCE_DIR}/include/boost_1_59_0")
-+SET(BOOST_SOURCES_DIR "${CMAKE_SOURCE_DIR}/include/boost_1_61_0")
++SET(BOOST_SOURCES_DIR "${CMAKE_SOURCE_DIR}/include/boost_1_62_0")
  
  # Bug in sqrt(NaN) on 32bit platforms
  IF(SIZEOF_VOIDP EQUAL 4)
