5d8d413391c9632e2f66ee0d0871640a1502adbf
bsiegert <bsiegert>
2016-10-16 10:10:02 +0000

    Fix compilation under macOS. From Jonathan Schleifer in PR pkg/51482.

---
+++ emulators/yabause/patches/patch-src_persdljoy.c
@@ -0,0 +1,17 @@
+$NetBSD: patch-src_persdljoy.c,v 1.1 2016/10/16 10:10:02 bsiegert Exp $
+
+--- src/persdljoy.c.orig	2014-11-12 17:48:47.000000000 +0000
++++ src/persdljoy.c
+@@ -24,11 +24,7 @@
+ */
+ 
+ #ifdef HAVE_LIBSDL
+-#ifdef __APPLE__
+-	#include <SDL/SDL.h>
+-#else
+-	#include "SDL.h"
+-#endif
++#include "SDL.h"
+ 
+ #include "debug.h"
+ #include "persdljoy.h"
