88ab1da339c7d5a1ceb5b01a5ad83d20d95e6372
bsiegert <bsiegert@pkgsrc.org>
2016-10-16 10:10:02 +0000

    Fix compilation under macOS. From Jonathan Schleifer in PR pkg/51482.

---
+++ emulators/yabause/patches/patch-src_yabause.c
@@ -0,0 +1,13 @@
+$NetBSD: patch-src_yabause.c,v 1.1 2016/10/16 10:10:02 bsiegert Exp $
+
+--- src/yabause.c.orig	2014-11-12 17:48:48.000000000 +0000
++++ src/yabause.c
+@@ -48,7 +48,7 @@
+ #include "movie.h"
+ #include "osdcore.h"
+ #ifdef HAVE_LIBSDL
+- #if defined(__APPLE__) || defined(GEKKO)
++ #if defined(GEKKO)
+   #include <SDL/SDL.h>
+  #else
+   #include "SDL.h"
