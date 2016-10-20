682fd23e4570e0436637dcb8bc87e7b1052687bb
fhajny <fhajny@pkgsrc.org>
2016-10-20 10:05:11 +0000

    Update sysutils/rsyslog to 8.22.0.
    Version 8.22.0 [v8-stable] 2016-10-04
    - ompgsql: add template support
      Thanks to Radu Gheorghe for implementing this.
    - generate somewhat better error message on config file syntax error
      a common case (object at invalid location) has received it's own error
      message; for the rest we still rely on the generic flex/bison handler
    - bugfix:omhiredis reconnects after failure
      previously it could loose messages under such conditions.
      Thanks to Bob Gregory for the patch.
    - general cleanup and code improvement
      mostly guided by compiler warnings induced by newer opensuse builbot
      environment
    ------------------------------------------------------------------------------
    Version 8.21.0 [v8-stable] 2016-08-23
    - CHANGE OF BEHAVIOUR:
      by default, internal messages are no longer logged via the internal
      bridge to rsyslog but via the syslog() API call [either directly or
      via liblogging). For the typical single-rsyslogd-instance installation this
      is mostly unnoticable (except for some additional latency). If multiple
      instances are run, only the "main" (the one processing system log messages)
      will see all messages. To return to the old behaviour, do either of those
      two:
      1) add in rsyslog.conf:
         global(processInternalMessages="on")
      2) export the environment variable RSYSLOG_DFLT_LOG_INTERNAL=1
         This will set a new default - the value can still be overwritten via
         rsyslog.conf (method 1). Note that the environment variable must be
         set in your **startup script**.
      For more information, please visit
      http://www.rsyslog.com/rsyslog-error-reporting-improved/
    - slightly improved TLS syslog error messages
    - queue subsystem: improved robustness
      The .qi file is now persisted whenever an existing queue file is fully
      written and a new file is begun. This helps with rsyslog aborts, including
      the common case where the OS issues kill -9 because of insufficiently
      configured termination timout (this is an OS config error, but a frequent
      one). Also, a situation where an orphaned empty file could be left in the
      queue work directory has been fixed. We expect that this change causes
      fewer permanent queue failures.
    - bugfix: build failed on some platforms due to missing include files

--- sysutils/rsyslog/patches/patch-tools_rsyslogd.c
+++ sysutils/rsyslog/patches/patch-tools_rsyslogd.c
@@ -1,18 +1,18 @@
-$NetBSD: patch-tools_rsyslogd.c,v 1.3 2015/01/16 16:58:28 fhajny Exp $
+$NetBSD: patch-tools_rsyslogd.c,v 1.4 2016/10/20 10:05:11 fhajny Exp $
 
 Need errno.h on NetBSD. Default PID file path.
---- tools/rsyslogd.c.orig	2015-01-12 08:49:44.000000000 +0000
+--- tools/rsyslogd.c.orig	2016-08-23 12:15:33.000000000 +0000
 +++ tools/rsyslogd.c
-@@ -29,7 +29,7 @@
- #ifdef HAVE_LIBLOGGING_STDLOG
- #  include <liblogging/stdlog.h>
+@@ -32,7 +32,7 @@
+ #else
+ #  include <syslog.h>
  #endif
 -#ifdef OS_SOLARIS
 +#if defined(OS_SOLARIS) || defined(OS_BSD)
  #	include <errno.h>
  #else
  #	include <sys/errno.h>
-@@ -87,7 +87,7 @@ void rsyslogdDoDie(int sig);
+@@ -89,7 +89,7 @@ void rsyslogdDoDie(int sig);
  
  
  #ifndef PATH_PIDFILE
