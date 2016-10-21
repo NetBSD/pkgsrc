63e6156b27c407ef3e7f5ddada8cc62b672e1eee
fhajny <fhajny@pkgsrc.org>
2016-10-21 14:16:34 +0000

    Update databases/sqlrelay and sub-packages to 0.66.0.
    New features:
    
    - Notification Modules
    - Connection Schedules
    - Query Router Modules
    
    Full changelog:
    
    - fixed mysql set isolation level query
    - updated mysql connection to run commit/rollback at the end of every
      session even if only selects were run, to make sure metadata locks are
      released
    - renamed sqlrclientexitstatus_t to clientsessionexitstatus_t
    - updated all db connection modules to reinit the column count during
      prepare rather than execute
    - fixed bug where server-side errors set by conn/cursor->setError()
      could inadverntantly be overriden during the next call to
      conn/cursor->errorMessage()
    - added several more queries to mysql stmt-api blacklist
    - configure option --disable-odbc-driver actually works now
    - filters can specify an error string and error number now
    - added support for systemd
    - fixed a memory leak by applying and updating a patch from Kasyanov
      Dmitry to deallocate the error message in C++ API destructor
    - legacy_stdio_definitions.lib is now included when building ODBC with
      VS2015
    - added support for PHP7
    - added -wait option to sqlr-start for non-Windows
    - disabled -disable-new-window option to sqlr-start for non-Windows
    - non-ascii character encodings work with the C# API now
    - rudiments prompt class is used instead of using readline directly now
    - renamed "logger-events" to just "events"
    - added notification framework and "events" module
    - added connection schedule framework and "cron" module
    - applied sec/usec patch from samchuck at neowiz to custom_nw logger to
      fix subtle time calculation error
    - applied a patch from samchuck at neowiz to the sqlrclient protocol
      module to deallocate storage for bind variables after logging the
      query to resolve a reference-after-free bug
    - added router module framework with "regex", "userlist" and
      "clientiplist" modules
    - when router detects a possible integrity violation now, it raises an
      event which can be logged or trigger a notification and disables the
      instance
    - router calls begin, commit, and rollback on all connections now, even
      if they're executed as queries rather than using the api methods
    - removed tls_userlist/krb_userlist auth modules and updated the
      userlist auth module to support tls and kerberos auth
    - converted most of the doc source to wikitext
    - fixed ODBC driver bug that could corrupt memory and trick the driver
      manager into thinking that some basic functions are unavailable
    - ODBC SQLTables works more correctly now
    - fixed an ODBC bug that caused the first row to be fetched twice when
      using SQLBindCol

--- databases/sqlrelay/buildlink3.mk
+++ databases/sqlrelay/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2016/10/07 18:25:31 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2016/10/21 14:16:34 fhajny Exp $
 
 BUILDLINK_TREE+=	sqlrelay
 
@@ -6,12 +6,11 @@ BUILDLINK_TREE+=	sqlrelay
 SQLRELAY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.sqlrelay+=	sqlrelay>=0.43
-BUILDLINK_ABI_DEPENDS.sqlrelay+=	sqlrelay>=0.65.0nb2
+BUILDLINK_ABI_DEPENDS.sqlrelay+=	sqlrelay>=0.66.0
 BUILDLINK_PKGSRCDIR.sqlrelay?=		../../databases/sqlrelay
 
 .include "../../devel/rudiments/buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
-.include "../../mk/readline.buildlink3.mk"
 .endif	# SQLRELAY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-sqlrelay
