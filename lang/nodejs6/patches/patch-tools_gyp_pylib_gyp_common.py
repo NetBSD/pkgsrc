875d93c25b0bd24034198750ee11bf722e70902e
fhajny <fhajny@pkgsrc.org>
2016-10-25 19:54:00 +0000

    Import nodejs 6.9.1 (LTS) as lang/nodejs6.
    Node.js is an evented I/O framework for the V8 JavaScript engine. It is
    intended for writing scalable network programs such as web servers.
    
    This package holds the 6.x LTS release.

---
+++ lang/nodejs6/patches/patch-tools_gyp_pylib_gyp_common.py
@@ -0,0 +1,16 @@
+$NetBSD: patch-tools_gyp_pylib_gyp_common.py,v 1.1 2016/10/25 19:54:00 fhajny Exp $
+
+Add support for NetBSD and DragonFly.
+--- tools/gyp/pylib/gyp/common.py.orig	2013-03-28 19:07:52.000000000 +0000
++++ tools/gyp/pylib/gyp/common.py
+@@ -394,6 +394,10 @@ def GetFlavor(params):
+     return 'freebsd'
+   if sys.platform.startswith('openbsd'):
+     return 'openbsd'
++  if sys.platform.startswith('netbsd'):
++    return 'netbsd'
++  if sys.platform.startswith('dragonflybsd'):
++    return 'dragonflybsd'
+   if sys.platform.startswith('aix'):
+     return 'aix'
+ 
