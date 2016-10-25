0fa98d82fb60be91cbbc00bc2be068b9d7f95735
fhajny <fhajny>
2016-10-25 19:50:40 +0000

    Update lang/nodejs to 7.0.0.
    This new major version of Node.js includes: V8 5.4 which brings along with
    98% coverage of ES6 language features, improved reliability and performance,
    and a new experimental URL parser based on the WHATWG URL standard.
    
    As an odd numbered release, in accordance with our Long Term Support plan,
    Node.js v7 will be supported only until about June, 2017, with Node.js v8
    currently scheduled for release in April, 2017.
    
    Notable changes
    
    Buffer
    - Passing invalid input to Buffer.byteLength will now throw an error #8946.
    - Calling Buffer without new is now deprecated and will emit a process
      warning #8169.
    - Passing a negative number to allocUnsafe will now throw an error #7079.
    
    Child Process
    - The fork and execFile methods now have stronger argument validation #7399.
    
    Cluster
    - The worker.suicide method is deprecated and will emit a process warning
      #3747.
    
    Deps
    - V8 has been updated to 5.4.500.36 #8317, #8852, #9253.
    - NODE_MODULE_VERSION has been updated to 51 #8808.
    
    File System
    - A process warning is emitted if a callback is not passed to async file
      system methods #7897.
    
    Intl
    - Intl.v8BreakIterator constructor has been deprecated and will emit a
      process warning #8908.
    
    Promises
    - Unhandled Promise rejections have been deprecated and will emit a process
      warning #8217.
    
    Punycode
    - The punycode module has been deprecated #7941.
    
    URL
    - An Experimental WHATWG URL Parser has been introduced #7448.

--- lang/nodejs/patches/patch-deps_v8_src_base_platform_semaphore.cc
+++ lang/nodejs/patches/patch-deps_v8_src_base_platform_semaphore.cc
@@ -1,8 +1,8 @@
-$NetBSD: patch-deps_v8_src_base_platform_semaphore.cc,v 1.4 2015/09/09 00:04:54 ryoon Exp $
+$NetBSD: patch-deps_v8_src_base_platform_semaphore.cc,v 1.5 2016/10/25 19:50:40 fhajny Exp $
 Work around lack of sem_timedwait(3) in NetBSD < 6.99.4.
 Adapted from d4f11c0cf476dd854eaebec1cbacb1afc7bea18e of the Chromium V8 sources.
 
---- deps/v8/src/base/platform/semaphore.cc.orig	2015-09-08 15:30:38.000000000 +0000
+--- deps/v8/src/base/platform/semaphore.cc.orig	2016-10-25 13:26:03.000000000 +0000
 +++ deps/v8/src/base/platform/semaphore.cc
 @@ -11,6 +11,10 @@
  
@@ -15,12 +15,3 @@ Adapted from d4f11c0cf476dd854eaebec1cbacb1afc7bea18e of the Chromium V8 sources
  #include "src/base/logging.h"
  #include "src/base/platform/elapsed-timer.h"
  #include "src/base/platform/time.h"
-@@ -107,7 +111,7 @@ void Semaphore::Wait() {
- 
- 
- bool Semaphore::WaitFor(const TimeDelta& rel_time) {
--#if V8_OS_NACL
-+#if defined(V8_OS_NACL)  || (defined(V8_OS_NETBSD) && (__NetBSD_Version__ - 0 < 699000400))
-   // PNaCL doesn't support sem_timedwait, do ugly busy waiting.
-   ElapsedTimer timer;
-   timer.Start();
