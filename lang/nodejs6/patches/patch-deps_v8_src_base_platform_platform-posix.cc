ab849d6a65e1e084cc2ce8636446b458ccabf479
fhajny <fhajny>
2016-10-25 19:54:00 +0000

    Import nodejs 6.9.1 (LTS) as lang/nodejs6.
    Node.js is an evented I/O framework for the V8 JavaScript engine. It is
    intended for writing scalable network programs such as web servers.
    
    This package holds the 6.x LTS release.

---
+++ lang/nodejs6/patches/patch-deps_v8_src_base_platform_platform-posix.cc
@@ -0,0 +1,39 @@
+$NetBSD: patch-deps_v8_src_base_platform_platform-posix.cc,v 1.1 2016/10/25 19:54:00 fhajny Exp $
+
+Use sysconf(_SC_THREAD_STACK_MIN) instead of PTHREAD_STACK_MIN.
+Cast explicitly.
+
+--- deps/v8/src/base/platform/platform-posix.cc.orig	2015-09-08 15:30:38.000000000 +0000
++++ deps/v8/src/base/platform/platform-posix.cc
+@@ -329,6 +329,8 @@ int OS::GetCurrentThreadId() {
+   return static_cast<int>(syscall(__NR_gettid));
+ #elif V8_OS_ANDROID
+   return static_cast<int>(gettid());
++#elif V8_OS_NETBSD || V8_OS_FREEBSD
++  return static_cast<int>(reinterpret_cast<intptr_t>(pthread_self()));
+ #elif V8_OS_AIX
+   return static_cast<int>(thread_self());
+ #elif V8_OS_SOLARIS
+@@ -535,8 +537,13 @@ Thread::Thread(const Options& options)
+     : data_(new PlatformData),
+       stack_size_(options.stack_size()),
+       start_semaphore_(NULL) {
++#if defined(__NetBSD__)
++  if (stack_size_ > 0 && static_cast<size_t>(stack_size_) < sysconf(_SC_THREAD_STACK_MIN)) {
++    stack_size_ = sysconf(_SC_THREAD_STACK_MIN);
++#else
+   if (stack_size_ > 0 && static_cast<size_t>(stack_size_) < PTHREAD_STACK_MIN) {
+     stack_size_ = PTHREAD_STACK_MIN;
++#endif
+   }
+   set_name(options.name());
+ }
+@@ -552,7 +559,7 @@ static void SetThreadName(const char* na
+   pthread_set_name_np(pthread_self(), name);
+ #elif V8_OS_NETBSD
+   STATIC_ASSERT(Thread::kMaxThreadNameLength <= PTHREAD_MAX_NAMELEN_NP);
+-  pthread_setname_np(pthread_self(), "%s", name);
++  pthread_setname_np(pthread_self(), "%s", (void *)name);
+ #elif V8_OS_MACOSX
+   // pthread_setname_np is only available in 10.6 or later, so test
+   // for it at runtime.
