8a602f6419b4a9fed17d97b6c04163cf3953e110
fhajny <fhajny@pkgsrc.org>
2016-10-26 13:56:30 +0000

    Update elasticsearch to 2.4.1
    === Enhancements
    
    Allocation::
    - Change DiskThresholdDecider's behavior when factoring in leaving shards
    
    Cluster::
    - Add clusterUUID to RestMainAction output
    
    
    === Bug fixes
    
    Core::
    - Guard against negative result from FileStore.getUsableSpace when pick...
    
    Highlighting::
    - skip GeoPointMultiTermQuery when highlighting
    - Fix highlighting of MultiTermQuery within a FunctionScoreQuery
    
    Index APIs::
    - Fix IndexNotFoundException in multi index search request.
    
    Internal::
    - Don't fail replica if FlushNotAllowedEngineException is thrown
    
    Network::
    - Don't send exception responses twice
    
    Packaging::
    - Fix Windows GC options
    
    Query DSL::
    - Fail parsing when match query contains an array of terms
    
    REST::
    - Correct the description for refresh
    
    Reindex API::
    - Make reindex tests a little less flaky
    
    Scripting::
    - Native scripts should be created once per index, not per segment.
    
    Settings::
    - Validate max thread/merge settings

--- databases/elasticsearch/patches/patch-bin_elasticsearch.in.sh
+++ databases/elasticsearch/patches/patch-bin_elasticsearch.in.sh
@@ -1,4 +1,4 @@
-$NetBSD: patch-bin_elasticsearch.in.sh,v 1.6 2016/09/19 12:28:31 fhajny Exp $
+$NetBSD: patch-bin_elasticsearch.in.sh,v 1.7 2016/10/26 13:56:30 fhajny Exp $
 
 Fix paths.
 
@@ -8,8 +8,8 @@ Fix paths.
      exit 1
  fi
  
--ES_CLASSPATH="$ES_HOME/lib/elasticsearch-2.4.0.jar:$ES_HOME/lib/*"
-+ES_CLASSPATH="@ES_LIBDIR@/elasticsearch-2.4.0.jar:@ES_LIBDIR@/*"
+-ES_CLASSPATH="$ES_HOME/lib/elasticsearch-2.4.1.jar:$ES_HOME/lib/*"
++ES_CLASSPATH="@ES_LIBDIR@/elasticsearch-2.4.1.jar:@ES_LIBDIR@/*"
  
  if [ "x$ES_MIN_MEM" = "x" ]; then
      ES_MIN_MEM=256m
