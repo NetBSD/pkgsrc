4211e6008043390352f9075c748a63d17c7349c7
ryoon <ryoon@pkgsrc.org>
2016-10-27 13:06:42 +0000

    Update to 1.8.112
    * Include a patch from PR pkg/51221
    
    Changelog:
    security-libs/java.security
    SunPKCS11 Provider no longer offering SecureRandom by default
    SecureRandom.PKCS11 from the SunPKCS11 Provider is disabled by default on Solaris because the native PKCS11 implementation has poor performance and is not recommended. If your application requires SecureRandom.PKCS11, you can re-enable it by removing "SecureRandom" from the disabledMechanisms list in conf/security/sunpkcs11-solaris.cfg
    
    Performance improvements have also been made in the java.security.SecureRandom class. Improvements in the JDK implementation have allowed for synchronization to be removed from the java.security.SecureRandom.nextBytes(byte[] bytes) method.
    See JDK-8098581
    
    Fix following security bugs:
    CVE-2016-5556
    CVE-2016-5582
    CVE-2016-5573
    CVE-2016-5597
    CVE-2016-5554
    CVE-2016-5542

--- lang/openjdk8/patches/patch-jdk_src_solaris_native_java_net_NetworkInterface.c
+++ lang/openjdk8/patches/patch-jdk_src_solaris_native_java_net_NetworkInterface.c
@@ -1,18 +1,18 @@
-$NetBSD: patch-jdk_src_solaris_native_java_net_NetworkInterface.c,v 1.2 2015/06/10 11:38:51 tnn Exp $
+$NetBSD: patch-jdk_src_solaris_native_java_net_NetworkInterface.c,v 1.3 2016/10/27 13:06:42 ryoon Exp $
 
 Zones support.
 
---- jdk/src/solaris/native/java/net/NetworkInterface.c.orig	2015-06-09 13:56:55.000000000 +0000
+--- jdk/src/solaris/native/java/net/NetworkInterface.c.orig	2016-10-26 22:56:37.000000000 +0000
 +++ jdk/src/solaris/native/java/net/NetworkInterface.c
-@@ -43,6 +43,7 @@
+@@ -42,6 +42,7 @@
  #include <fcntl.h>
  #include <stropts.h>
  #include <sys/sockio.h>
 +#include <zone.h>
  #endif
  
- #ifdef __linux__
-@@ -67,14 +68,12 @@
+ #if defined(__linux__)
+@@ -62,14 +63,12 @@
  #include <sys/param.h>
  #include <sys/ioctl.h>
  #include <sys/sockio.h>
@@ -29,27 +29,27 @@ Zones support.
  #endif
  #include <net/if_dl.h>
  #include <netinet/in_var.h>
-@@ -1749,7 +1748,8 @@ static short getSubnet(JNIEnv *env, int 
- 
+@@ -1710,7 +1709,8 @@ static short getSubnet(JNIEnv *env, int 
+ }
  
  
 -#define DEV_PREFIX  "/dev/"
-+#define NET_DEV_PREFIX  "/dev/"
-+#define ZONE_NET_DEV_PREFIX  "/dev/net/"
++#define NET_DEV_PREFIX	"/dev/"
++#define ZONE_NET_DEV_PREFIX	"/dev/net/"
  
- /**
+ /*
   * Solaris specific DLPI code to get hardware address from a device.
-@@ -1766,11 +1766,18 @@ static int getMacFromDevice(JNIEnv *env,
+@@ -1728,9 +1728,18 @@ static int getMacFromDevice
+     char buf[128];
      int flags = 0;
  
-    /**
--    * Device is in /dev
+-    // Device is in /dev.  e.g.: /dev/bge0
+-    strcpy(style1dev, DEV_PREFIX);
++   /*
 +    * Device is in /dev in global zone / standalone
-     * e.g.: /dev/bge0
 +    * Device is in /dev/net in a zone
 +    * e.g.: /dev/net/net0
-     */
--    strcpy(style1dev, DEV_PREFIX);
++    */
 +    if (getzoneid()==(zoneid_t)GLOBAL_ZONEID) {
 +        strcpy(style1dev, NET_DEV_PREFIX);
 +    } else {
@@ -58,5 +58,5 @@ Zones support.
      strcat(style1dev, ifname);
 +
      if ((fd = open(style1dev, O_RDWR)) < 0) {
-         /*
-          * Can't open it. We probably are missing the privilege.
+          // Can't open it. We probably are missing the privilege.
+          // We'll have to try something else
