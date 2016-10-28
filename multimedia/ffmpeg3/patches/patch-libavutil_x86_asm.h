5b7ee17ce53347ddf4d82d88f788714c1f7cfbd0
adam <adam@pkgsrc.org>
2016-10-28 19:15:06 +0000

    version 3.2: - libopenmpt demuxer - tee protocol - Changed metadata print option to accept general urls - Alias muxer for Ogg Video (.ogv) - VP8 in Ogg muxing - curves filter doesn't automatically insert points at x=0 and x=1 anymore - 16-bit support in curves filter and selectivecolor filter - OpenH264 decoder wrapper - MediaCodec H.264/HEVC/MPEG-4/VP8/VP9 hwaccel - True Audio (TTA) muxer - crystalizer audio filter - acrusher audio filter - bitplanenoise video filter - floating point support in als decoder - fifo muxer - maskedclamp filter - hysteresis filter - lut2 filter - yuvtestsrc filter - CUDA CUVID H.263/VP8/VP9/10 bit HEVC (Dithered) Decoding - vaguedenoiser filter - added threads option per filter instance - weave filter - gblur filter - avgblur filter - sobel and prewitt filter - MediaCodec HEVC/MPEG-4/VP8/VP9 decoding - Meridian Lossless Packing (MLP) / TrueHD encoder - Non-Local Means (nlmeans) denoising filter - sdl2 output device and ffplay support - sdl1 output device and sdl1 support removed - extended mov edit list support - libfaac encoder removed - Matroska muxer now writes CRC32 elements by default in all Level 1 elements - sidedata video and asidedata audio filter - Changed mapping of rtp MIME type G726 to codec g726le.

--- multimedia/ffmpeg3/patches/patch-libavutil_x86_asm.h
+++ multimedia/ffmpeg3/patches/patch-libavutil_x86_asm.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-libavutil_x86_asm.h,v 1.1 2016/03/05 08:52:23 ryoon Exp $
+$NetBSD: patch-libavutil_x86_asm.h,v 1.2 2016/10/28 19:15:06 adam Exp $
 
-avoid SunOS regset.h definition for REG_SP
+Avoid SunOS regset.h definition for REG_SP
 
---- libavutil/x86/asm.h.orig	2016-01-15 16:58:37.000000000 +0000
+--- libavutil/x86/asm.h.orig	2016-10-28 18:31:25.000000000 +0000
 +++ libavutil/x86/asm.h
 @@ -27,6 +27,11 @@
  typedef struct xmm_reg { uint64_t a, b; } xmm_reg;
@@ -14,5 +14,5 @@ avoid SunOS regset.h definition for REG_SP
 +#endif
 +
  #if ARCH_X86_64
- #    define OPSIZE "q"
- #    define REG_a "rax"
+ #    define FF_OPSIZE "q"
+ #    define FF_REG_a "rax"
