2be081ad09dbbcb6c76d993c483e885d065be59d
maya <maya@pkgsrc.org>
2016-10-30 09:45:32 +0000

    liboggz: bump PKGREVISION for previous. it's a functional change, however small.
    Fix pkglint warnings about whitespace

--- multimedia/liboggz/options.mk
+++ multimedia/liboggz/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2013/05/09 00:40:12 rodent Exp $
+# $NetBSD: options.mk,v 1.2 2016/10/30 09:45:32 maya Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.liboggz
 PKG_SUPPORTED_OPTIONS=	doc valgrind
@@ -8,14 +8,14 @@ PLIST_VARS+=		doc
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdoc)
-BUILD_DEPENDS+=	doxygen-[0-9]*:../../devel/doxygen
+BUILD_DEPENDS+=		doxygen-[0-9]*:../../devel/doxygen
 PLIST.doc=		yes
 .else
 CONFIGURE_ARGS+=	HAVE_DOXYGEN=no
 .endif
 
 .if !empty(PKG_OPTIONS:Mvalgrind)
-BUILD_DEPENDS+=	valgrind-[0-9]*:../../devel/valgrind
+BUILD_DEPENDS+=		valgrind-[0-9]*:../../devel/valgrind
 CONFIGURE_ARGS+=	--enable-valgrind-testing
 .else
 CONFIGURE_ARGS+=	--disable-valgrind-testing
