026f3cb4cda9361e7e93a22d060e0d3a0c49fe70
hauke <hauke@pkgsrc.org>
2016-10-31 14:28:20 +0000

    Re-add editors/xemacs-current/hacks.mk --
    gcc 5 builtins collide with src/gmalloc.c's calloc(), which
    results in 'xemacs -vanilla' busy-looping during the build.
    
    Joerg suggested building with -fno-builtin, see
    <http://mail-index.netbsd.org/tech-toolchain/2016/10/28/msg002820.html> ff.

---
+++ editors/xemacs-current/hacks.mk
@@ -0,0 +1,17 @@
+# $NetBSD: hacks.mk,v 1.1 2016/10/31 14:28:20 hauke Exp $
+
+.if !defined(XEMACS_HACKS_MK)
+XEMACS_HACKS_MK=	defined
+
+.include "../../mk/compiler.mk"
+
+### [Fri Oct 28 10:00:00 UTC 2016 : hauke]
+### gcc 5 builtins collide with src/gmalloc.c's calloc(), which
+### results in 'xemacs -vanilla' busy-looping during the build.
+###
+.if !empty(PKGSRC_COMPILER:Mgcc) && !empty(CC_VERSION:Mgcc-5.[0-9]*)
+PKG_HACKS+=		gcc5-malloc-builtin-conflict
+CFLAGS+=		-fno-builtin
+.endif
+
+.endif  # XEMACS_HACKS_MK
