e84ed58835a5fc20f348327965ffe8fbbc9f4846
adam <adam@pkgsrc.org>
2016-11-06 15:05:30 +0000

    Changes 3.4.2: - Fix Resample coefficient calculation

--- graphics/py-Pillow/patches/patch-setup.py
+++ graphics/py-Pillow/patches/patch-setup.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-setup.py,v 1.8 2016/10/04 19:30:24 adam Exp $
+$NetBSD: patch-setup.py,v 1.9 2016/11/06 15:05:30 adam Exp $
 
 * Prevent to detect optional tkinter.
 * Disable demo programs.
 
---- setup.py.orig	2016-10-04 14:54:44.000000000 +0000
+--- setup.py.orig	2016-10-18 19:12:54.000000000 +0000
 +++ setup.py
 @@ -103,11 +103,7 @@ def _read(file):
      return open(file, 'rb').read()
@@ -17,7 +17,7 @@ $NetBSD: patch-setup.py,v 1.8 2016/10/04 19:30:24 adam Exp $
 +_tkinter = None
  
  NAME = 'Pillow'
- PILLOW_VERSION = '3.4.1'
+ PILLOW_VERSION = '3.4.2'
 @@ -746,7 +742,6 @@ setup(name=NAME,
        ext_modules=[Extension("PIL._imaging", ["_imaging.c"])],
        include_package_data=True,
