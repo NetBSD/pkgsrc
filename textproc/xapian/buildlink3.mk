539c412896288e7f7a5a5d6bcedeb89b9a27ad69
wiz <wiz@pkgsrc.org>
2016-11-07 13:46:45 +0000

    Recursive bump for xapian shlib major bump.

--- textproc/xapian/buildlink3.mk
+++ textproc/xapian/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2016/11/07 13:02:17 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2016/11/07 13:46:45 wiz Exp $
 
 BUILDLINK_TREE+=	xapian
 
 .if !defined(XAPIAN_BUILDLINK3_MK)
 XAPIAN_BUILDLINK3_MK:=
 
-# XXX do these depends change?
 BUILDLINK_API_DEPENDS.xapian+=	xapian>=1.0.7
-BUILDLINK_ABI_DEPENDS.xapian+=	xapian>=1.2.2nb1
+BUILDLINK_ABI_DEPENDS.xapian+=	xapian>=1.4.1
 BUILDLINK_PKGSRCDIR.xapian?=	../../textproc/xapian
 
 .include "../../devel/libuuid/buildlink3.mk"
