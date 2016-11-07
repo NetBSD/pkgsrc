36cbf40ebc3e51ca5b25560bbde02e184127a03a
christos <christos@pkgsrc.org>
2016-11-07 00:41:57 +0000

    Add new patches

---
+++ net/netatalk3/patches/patch-include_atalk_util.h
@@ -0,0 +1,13 @@
+$NetBSD: patch-include_atalk_util.h,v 1.1 2016/11/07 00:41:58 christos Exp $
+
+--- include/atalk/util.h.orig	2014-08-07 07:11:55.000000000 -0400
++++ include/atalk/util.h	2016-11-06 09:33:20.246331563 -0500
+@@ -115,7 +115,7 @@
+  * OpenBSD currently does not use the second arg for dlopen(). For
+  * future compatibility we define DL_LAZY */
+ #ifdef __NetBSD__
+-#define mod_open(a)      dlopen(a, RTLD_LAZY)
++#define mod_open(a)      dlopen(a, RTLD_LAZY|RTLD_GLOBAL)
+ #elif defined(__OpenBSD__)
+ #define mod_open(a)      dlopen(a, DL_LAZY)
+ #else /* ! __NetBSD__ && ! __OpenBSD__ */
