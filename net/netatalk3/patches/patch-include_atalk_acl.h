ce1c02d30292405f4b460bb53cf0d4367483f130
christos <christos@pkgsrc.org>
2016-11-07 12:46:52 +0000

    comment on patches

--- net/netatalk3/patches/patch-include_atalk_acl.h
+++ net/netatalk3/patches/patch-include_atalk_acl.h
@@ -1,4 +1,6 @@
-$NetBSD: patch-include_atalk_acl.h,v 1.1 2016/11/07 00:41:58 christos Exp $
+$NetBSD: patch-include_atalk_acl.h,v 1.2 2016/11/07 12:46:52 christos Exp $
+
+Provide O_IGNORE for the ones with no ACL support
 
 --- include/atalk/acl.h.orig	2014-07-03 01:16:05.000000000 -0400
 +++ include/atalk/acl.h	2016-11-06 11:20:13.660540431 -0500
