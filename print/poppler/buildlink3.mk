e5fa47ef9989254e280387311d0b5fa0879bd653
wiz <wiz@pkgsrc.org>
2016-11-08 10:51:01 +0000

    Updated poppler to 0.48.0.
    Release 0.48.0
            core:
             * Fix crashes and memory leaks in invalid files.
             * Small memory usage improvements.
             * TextOutputDev: Remove null characters from PDF text. Bug #97144
             * TextOutputDev: Break words on all whitespace characters. Bug #97399
             * Fix UTF16 decoding of document outline title. Bug #97156
             * Add functions for named destination name in name-tree/dict
    
            glib:
             * Increase glib requirement to 2.41
    
    Release 0.47.0
            core:
             * Fix abort on documents where the docinfo obj is not a dict. Bug #97134
             * Check for XRefEntry existing before using it. Bug #97005
             * Fix memory leak on PDFDoc::setDocInfoStringEntry() with empty string
             * Don't presume that DocInfo is a dictionary in XRef::createDocInfoIfNoneExists()
    
            build system:
             * configure: Work with non gnu greps

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.50 2016/08/01 14:23:55 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.51 2016/11/08 10:51:01 wiz Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.46.0
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.48.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 .include "../../graphics/lcms2/buildlink3.mk"
