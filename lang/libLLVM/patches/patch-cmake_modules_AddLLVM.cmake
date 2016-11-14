113ae3b6bed4b263230889ffa109a9ec112b24e6
ryoon <ryoon@pkgsrc.org>
2016-11-14 20:15:32 +0000

    Update llvm packages to 3.9.0
    * Drop CppBackend. It is removed.
    
    Changelog:
    * GCC ABI Tag
    * LLVM IR: new intrinsics etc.
    * Change LLVM IPO model
    * Support ThinLTO
    * Improve the ARM targets, ARMv8.2-A support etc.
    * Improve the MIPS targets
    * Improve the PowerPC target, default optim O3 to O2
    * Improve the X86 target, SKylake AVX-512 etc.
    * Improve the AMDGPU, better support for Mesa 12

--- lang/libLLVM/patches/patch-cmake_modules_AddLLVM.cmake
+++ lang/libLLVM/patches/patch-cmake_modules_AddLLVM.cmake
@@ -1,10 +1,10 @@
-$NetBSD: patch-cmake_modules_AddLLVM.cmake,v 1.2 2016/03/11 06:50:42 tnn Exp $
+$NetBSD: patch-cmake_modules_AddLLVM.cmake,v 1.3 2016/11/14 20:15:33 ryoon Exp $
 
 Disable library install rules. Handled manually.
 Make sure llvm-config goes in libexec/libLLVM to avoid conflict.
 Don't use non-portable -z discard-unused on SunOS.
 
---- cmake/modules/AddLLVM.cmake.orig	2016-02-12 17:52:29.000000000 +0000
+--- cmake/modules/AddLLVM.cmake.orig	2016-07-10 02:43:47.000000000 +0000
 +++ cmake/modules/AddLLVM.cmake
 @@ -174,9 +174,9 @@ function(add_link_opts target_name)
          # ld64's implementation of -dead_strip breaks tools that use plugins.
@@ -32,12 +32,34 @@ Don't use non-portable -z discard-unused on SunOS.
  
        if (NOT CMAKE_CONFIGURATION_TYPES)
          add_custom_target(install-${name}
-@@ -702,7 +704,7 @@ macro(add_llvm_tool name)
+@@ -592,10 +594,12 @@ macro(add_llvm_loadable_module name)
+         else()
+           set(dlldir "lib${LLVM_LIBDIR_SUFFIX}")
+         endif()
++        if (false)
+         install(TARGETS ${name}
+           EXPORT LLVMExports
+           LIBRARY DESTINATION ${dlldir}
+           ARCHIVE DESTINATION lib${LLVM_LIBDIR_SUFFIX})
++       endif()
+       endif()
+       set_property(GLOBAL APPEND PROPERTY LLVM_EXPORTS ${name})
+     endif()
+@@ -770,7 +774,7 @@ macro(add_llvm_tool name)
      if( LLVM_BUILD_TOOLS )
        install(TARGETS ${name}
                EXPORT LLVMExports
--              RUNTIME DESTINATION bin
+-              RUNTIME DESTINATION ${LLVM_TOOLS_INSTALL_DIR}
 +              RUNTIME DESTINATION libexec/libLLVM
                COMPONENT ${name})
  
        if (NOT CMAKE_CONFIGURATION_TYPES)
+@@ -811,7 +815,7 @@ macro(add_llvm_utility name)
+   set_target_properties(${name} PROPERTIES FOLDER "Utils")
+   if( LLVM_INSTALL_UTILS AND LLVM_BUILD_UTILS )
+     install (TARGETS ${name}
+-      RUNTIME DESTINATION bin
++      RUNTIME DESTINATION libexec/libLLVM
+       COMPONENT ${name})
+     if (NOT CMAKE_CONFIGURATION_TYPES)
+       add_custom_target(install-${name}
