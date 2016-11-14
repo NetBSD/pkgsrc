53a6436d0512ef9ca5f87aec81d6d31f4f224f43
ryoon <ryoon@pkgsrc.org>
2016-11-14 20:15:32 +0000

    Update llvm packages to 3.9.0
    * Drop CppBackend. It is removed.
    
    Changelog:
    * GCC ABI Tag
    * LLVM IR: new intrinsics etc.
    * Change LLVM IPO model
    * Support ThinLTO
    * Improve the ARM targets, ARMv8.2-A support etc.
    * Improve the MIPS targets
    * Improve the PowerPC target, default optim O3 to O2
    * Improve the X86 target, SKylake AVX-512 etc.
    * Improve the AMDGPU, better support for Mesa 12

--- lang/clang/buildlink3.mk
+++ lang/clang/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2016/03/10 15:01:52 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2016/11/14 20:15:33 ryoon Exp $
 
 BUILDLINK_TREE+=	clang
 
 .if !defined(CLANG_BUILDLINK3_MK)
 CLANG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clang+=	clang>=3.8.0
+BUILDLINK_API_DEPENDS.clang+=	clang>=3.9.0
 BUILDLINK_PKGSRCDIR.clang?=	../../lang/clang
 
 .include "../../textproc/libxml2/buildlink3.mk"
