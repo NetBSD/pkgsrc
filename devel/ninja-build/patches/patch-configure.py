fd437d229dd85c4be7c53eb4115a040e4a978977
wiz <wiz@pkgsrc.org>
2016-11-14 14:38:32 +0000

    Updated ninja-build to 1.7.2.
    This is a minor release after v1.7.1, mostly bugfixes.
    
    == Release notes.
    
    * %e and %r work again in dumb terminals (#1146, #1148)
    * `-t browse` now HTML-escapes rule names (#1157)
    * an edge may now have 0 explicit outputs if it has implicit outputs (#1159)
    * make misc/measure.py python 3-compatible
    * misc/write_fake_manifest.py now optionally writes source files, use it to
      quickly produce large, buildable projects for testing (#1109, #1198)
    * on newer FreeBSDs, use ppoll()

--- devel/ninja-build/patches/patch-configure.py
+++ devel/ninja-build/patches/patch-configure.py
@@ -1,8 +1,8 @@
-$NetBSD: patch-configure.py,v 1.2 2016/05/16 11:56:11 ryoon Exp $
+$NetBSD: patch-configure.py,v 1.3 2016/11/14 14:38:32 wiz Exp $
 
 * Add NetBSD support
 
---- configure.py.orig	2016-04-28 14:56:44.000000000 +0000
+--- configure.py.orig	2016-11-07 18:34:46.000000000 +0000
 +++ configure.py
 @@ -95,7 +95,7 @@ class Platform(object):
          return self._platform == 'aix'
@@ -12,7 +12,7 @@ $NetBSD: patch-configure.py,v 1.2 2016/05/16 11:56:11 ryoon Exp $
 +        return self._platform in ('freebsd', 'openbsd', 'bitrig', 'netbsd')
  
      def supports_ppoll(self):
-         return self._platform in ('linux', 'openbsd', 'bitrig')
+         return self._platform in ('freebsd', 'linux', 'openbsd', 'bitrig')
 @@ -350,8 +350,8 @@ else:
          cflags += ['-D_WIN32_WINNT=0x0501']
      ldflags = ['-L$builddir']
