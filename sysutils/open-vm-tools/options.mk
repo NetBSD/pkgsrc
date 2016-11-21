d96774929f71d5826f8a8501e8cf31660f766542
pho <pho@pkgsrc.org>
2016-11-21 00:14:16 +0000

    Build bin/vmhgfs-fuse and bin/vmware-vmblock-fuse
    Please note that you need -CURRENT to actually use these filesystems. They still build on older systems but fail to run.

--- sysutils/open-vm-tools/options.mk
+++ sysutils/open-vm-tools/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2016/10/09 03:41:56 ryoon Exp $
+# $NetBSD: options.mk,v 1.4 2016/11/21 00:14:16 pho Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.open-vm-tools
 PKG_SUPPORTED_OPTIONS=	x11
@@ -8,6 +8,7 @@ PKG_SUGGESTED_OPTIONS=	x11
 
 .if !empty(PKG_OPTIONS:Mx11)
 PLIST_SRC+=	PLIST.x11
+MESSAGE_SRC+=	MESSAGE.x11
 .  include "../../graphics/gdk-pixbuf2-xlib/buildlink3.mk"
 .  include "../../x11/gtkmm/buildlink3.mk"
 .  include "../../sysutils/desktop-file-utils/desktopdb.mk"
---
+++ sysutils/open-vm-tools/patches/patch-vmhgfs-fuse_config.c
@@ -0,0 +1,91 @@
+$NetBSD: patch-vmhgfs-fuse_config.c,v 1.1 2016/11/21 00:14:16 pho Exp $
+
+* The original SysCompatCheck() only works on Linux.
+* librefuse doesn't support the old style -ho option.
+
+--- vmhgfs-fuse/config.c.orig	2016-02-16 20:06:49.000000000 +0000
++++ vmhgfs-fuse/config.c
+@@ -23,6 +23,7 @@
+ 
+ #include "module.h"
+ #include <sys/utsname.h>
++#include <sys/stat.h>
+ 
+ #ifdef VMX86_DEVEL
+ int LOGLEVEL_THRESHOLD = 4;
+@@ -136,6 +137,7 @@ Usage(char *prog_name)  // IN
+ #define PROC_FILESYSTEMS       "/proc/filesystems"
+ #define FUSER_KERNEL_FS        "fuse"
+ 
++#if defined(__linuX__)
+ /*
+  *----------------------------------------------------------------------
+  *
+@@ -173,8 +175,9 @@ SysCompatFusermountCheck(void)  // IN:
+ 
+    return fusermountExists;
+ }
++#endif
+ 
+-
++#if defined(__linux__)
+ /*
+  *----------------------------------------------------------------------
+  *
+@@ -253,8 +256,9 @@ exit:
+    free(modulesDep);
+    return installedFuse;
+ }
++#endif
+ 
+-
++#if defined(__linux__)
+ /*
+  *----------------------------------------------------------------------
+  *
+@@ -307,7 +311,7 @@ exit:
+    }
+    return registeredFuse;
+ }
+-
++#endif
+ 
+ /*
+  *----------------------------------------------------------------------
+@@ -328,6 +332,7 @@ exit:
+  */
+ static HgfsSystemCompatibility
+ SysCompatCheck(char *prog_name)  // IN
++#if defined(__linux__)
+ {
+    struct utsname buf;
+    HgfsSystemCompatibility sysCompat = HGFS_SYSCOMPAT_OS_NOT_SUPPORTED;
+@@ -381,6 +386,17 @@ exit:
+            HgfsSystemCompatibilityMsg[sysCompat]);
+    return sysCompat;
+ }
++#elif defined(__NetBSD__)
++{
++   HgfsSystemCompatibility sysCompat = HGFS_SYSCOMPAT_FUSE_ENABLED;
++   struct stat st;
++
++   if (stat("/dev/puffs", &st) == -1)
++      sysCompat = HGFS_SYSCOMPAT_OS_NO_FUSE;
++
++   return sysCompat;
++}
++#endif
+ 
+ 
+ /*
+@@ -444,7 +460,9 @@ vmhgfsOptProc(void *data,               
+ 
+    case KEY_HELP:
+       Usage(outargs->argv[0]);
+-      fuse_opt_add_arg(outargs, "-ho");
++      fuse_opt_add_arg(outargs, "--help");
++      free(outargs->argv[0]);
++      outargs->argv[0] = strdup("");
+       fuse_main(outargs->argc, outargs->argv, NULL, NULL);
+       exit(1);
+ 
