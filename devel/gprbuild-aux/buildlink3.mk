dd58b415ef75029d5c6d98cf471e9945cdae9f02
marino <marino@pkgsrc.org>
2016-11-26 18:34:47 +0000

    devel/gprbuild-aux: change buildlink default full=>build
    Normally gprbuild is only used for building, with a notable exception
    of the broken devel/gps.

--- devel/gprbuild-aux/buildlink3.mk
+++ devel/gprbuild-aux/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/04/30 16:28:09 marino Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/11/26 18:34:47 marino Exp $
 
 BUILDLINK_TREE+=	gprbuild-aux
 
@@ -7,6 +7,7 @@ GPRBUILD_AUX_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gprbuild-aux+=	gprbuild-aux>=20130416
 BUILDLINK_PKGSRCDIR.gprbuild-aux?=	../../devel/gprbuild-aux
+BUILDLINK_DEPMETHOD.gprbuild-aux?=	build
 
 .include "../../textproc/xmlada/buildlink3.mk"
 .endif	# GPRBUILD_AUX_BUILDLINK3_MK
