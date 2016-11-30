7e12bc67b5e3edcf8f8e08d65d73738d236f4723
wiz <wiz@pkgsrc.org>
2016-11-30 12:43:35 +0000

    Add upstream bug report URL.

--- audio/hydrogen/patches/patch-src_core_src_logger.cpp
+++ audio/hydrogen/patches/patch-src_core_src_logger.cpp
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_core_src_logger.cpp,v 1.1 2016/10/25 01:23:55 mrg Exp $
+$NetBSD: patch-src_core_src_logger.cpp,v 1.2 2016/11/30 12:43:36 wiz Exp $
 
 usleep(1000000) is illegal.
+https://github.com/hydrogen-music/hydrogen/pull/444
 
 --- src/core/src/logger.cpp.orig	2014-09-09 11:39:33.000000000 -0700
 +++ src/core/src/logger.cpp	2016-10-24 14:40:32.000000000 -0700
