0d2ecbe2255d4ad4350bc98f61d74ed399dbc673
wiedi <wiedi@pkgsrc.org>
2016-12-03 20:14:05 +0000

    fix int types on SunOS

--- audio/alsa-lib/buildlink3.mk
+++ audio/alsa-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:23:52 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/12/03 20:14:05 wiedi Exp $
 
 BUILDLINK_TREE+=	alsa-lib
 
@@ -7,6 +7,8 @@ ALSA_LIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.alsa-lib+=	alsa-lib>=1.0.18
 BUILDLINK_PKGSRCDIR.alsa-lib?=	../../audio/alsa-lib
+
+CPPFLAGS.SunOS+=	-Du_int8_t=uint8_t -Du_int16_t=uint16_t -Du_int32_t=uint32_t -Du_int64_t=uint64_t
 
 .include "../../mk/dlopen.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
