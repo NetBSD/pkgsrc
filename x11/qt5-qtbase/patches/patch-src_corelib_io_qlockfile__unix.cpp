2f3f3e4f2e1dc86ebb8f9ac971751f568bd87a06
marino <marino@pkgsrc.org>
2016-12-04 21:46:54 +0000

    x11/qt5-qtbase: Fix build on FreeBSD and DragonFly
    1. DragonFly needs -lexecinfo LDFLAG
    2. Qlockfile is unsupported on DF, allow to fall through
    3. QStorageInfo only worked for NetBSD.  Take patch from historical
       ports to fix FreeBSD, DragonFly, and OpenBSD.  (It's been fixed
       upstread for QT5 5.6)

--- x11/qt5-qtbase/patches/patch-src_corelib_io_qlockfile__unix.cpp
+++ x11/qt5-qtbase/patches/patch-src_corelib_io_qlockfile__unix.cpp
@@ -1,8 +1,9 @@
-$NetBSD: patch-src_corelib_io_qlockfile__unix.cpp,v 1.1 2015/10/26 19:03:59 adam Exp $
+$NetBSD: patch-src_corelib_io_qlockfile__unix.cpp,v 1.2 2016/12/04 21:46:54 marino Exp $
 
 Add support for NetBSD and OpenBSD.
+Enable fallback to unsupported for DragonFly
 
---- src/corelib/io/qlockfile_unix.cpp.orig	2015-10-25 12:40:42.000000000 +0000
+--- src/corelib/io/qlockfile_unix.cpp.orig	2015-10-13 04:35:31 UTC
 +++ src/corelib/io/qlockfile_unix.cpp
 @@ -54,6 +54,10 @@
  #elif defined(Q_OS_LINUX)
@@ -15,10 +16,11 @@ Add support for NetBSD and OpenBSD.
  #elif defined(Q_OS_BSD4) && !defined(Q_OS_IOS)
  #   include <sys/user.h>
  # if defined(__GLIBC__) && defined(__FreeBSD_kernel__)
-@@ -239,6 +243,19 @@ QString QLockFilePrivate::processNameByP
+@@ -239,7 +243,20 @@ QString QLockFilePrivate::processNameByP
      }
      buf[len] = 0;
      return QFileInfo(QFile::decodeName(buf)).fileName();
+-#elif defined(Q_OS_BSD4) && !defined(Q_OS_IOS)
 +#elif defined(Q_OS_NETBSD) || defined(Q_OS_OPENBSD)
 +    kvm_t *kd = kvm_open(NULL, NULL, NULL, KVM_NO_FILES, NULL);
 +    int cnt;
@@ -32,6 +34,7 @@ Add support for NetBSD and OpenBSD.
 +    }
 +    kvm_close(kd);
 +    return name;
- #elif defined(Q_OS_BSD4) && !defined(Q_OS_IOS)
++#elif defined(Q_OS_BSD4) && !defined(Q_OS_IOS) && !defined(__DragonFly__)
  # if defined(__GLIBC__) && defined(__FreeBSD_kernel__)
      int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PID, pid };
+     size_t len = 0;
