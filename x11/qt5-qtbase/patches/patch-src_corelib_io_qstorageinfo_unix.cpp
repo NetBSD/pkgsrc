216f0d24abfb3c924a0eb371d656e9fc878f2f69
marino <marino@pkgsrc.org>
2016-12-04 21:46:54 +0000

    x11/qt5-qtbase: Fix build on FreeBSD and DragonFly
    1. DragonFly needs -lexecinfo LDFLAG
    2. Qlockfile is unsupported on DF, allow to fall through
    3. QStorageInfo only worked for NetBSD.  Take patch from historical
       ports to fix FreeBSD, DragonFly, and OpenBSD.  (It's been fixed
       upstread for QT5 5.6)

--- x11/qt5-qtbase/patches/patch-src_corelib_io_qstorageinfo_unix.cpp
+++ x11/qt5-qtbase/patches/patch-src_corelib_io_qstorageinfo_unix.cpp
@@ -1,9 +1,10 @@
-$NetBSD: patch-src_corelib_io_qstorageinfo_unix.cpp,v 1.5 2015/10/26 19:03:59 adam Exp $
+$NetBSD: patch-src_corelib_io_qstorageinfo_unix.cpp,v 1.6 2016/12/04 21:46:54 marino Exp $
 
 * make statvfs available on non-NetBSD BSD platforms
+* Use f_bsize instead of f_frsize on non-NetBSD BSD platforms
 * fix build on SunOS
 
---- src/corelib/io/qstorageinfo_unix.cpp.orig	2015-10-13 04:35:31.000000000 +0000
+--- src/corelib/io/qstorageinfo_unix.cpp.orig	2015-10-13 04:35:31 UTC
 +++ src/corelib/io/qstorageinfo_unix.cpp
 @@ -68,8 +68,8 @@
  
@@ -34,3 +35,19 @@ $NetBSD: patch-src_corelib_io_qstorageinfo_unix.cpp,v 1.5 2015/10/26 19:03:59 ad
  #  define QT_STATFSBUF struct statvfs
  #  define QT_STATFS    ::statvfs
  #else
+@@ -506,9 +506,15 @@ void QStorageInfoPrivate::retrieveVolume
+         valid = true;
+         ready = true;
+ 
++#if defined(Q_OS_BSD4) && !defined(Q_OS_NETBSD)
++        bytesTotal = statfs_buf.f_blocks * statfs_buf.f_bsize;
++        bytesFree = statfs_buf.f_bfree * statfs_buf.f_bsize;
++        bytesAvailable = statfs_buf.f_bavail * statfs_buf.f_bsize;
++#else
+         bytesTotal = statfs_buf.f_blocks * statfs_buf.f_frsize;
+         bytesFree = statfs_buf.f_bfree * statfs_buf.f_frsize;
+         bytesAvailable = statfs_buf.f_bavail * statfs_buf.f_frsize;
++#endif
+ #if defined(Q_OS_ANDROID) || defined (Q_OS_BSD4)
+ #if defined(_STATFS_F_FLAGS)
+         readOnly = (statfs_buf.f_flags & ST_RDONLY) != 0;
