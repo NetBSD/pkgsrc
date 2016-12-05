d959069ef34ec11d51e33dd957767f0f317b1b0f
asau <asau>
2016-12-05 20:03:29 +0000

    Update to SBCL 1.3.12
    New in version 1.3.12
    
     * enhancement: on x86-64, compiled functions loaded from fasl
       files can not be moved, but can be freed, by GC.
       Additionally, COMPILE will produce immobile code
       if SB-C::*COMPILE-TO-MEMORY-SPACE* is set to :IMMOBILE.
       (Caution: the flag is experimental and subject to change.)
       The benefits are better physical separation of code from
       data, and potentially easier examination of live images by
       external tools.
     * enhancement: the docstring for SAVE-LISP-AND-DIE has been
       amended to say that the :ROOT-STRUCTURES parameter is not
       meaningless on gencgc, depending on the platform.
     * bug fix: calling a named function (e.g. a DEFUN) concurrently
       with redefining that same function could lead to execution of
       random bytes.
     * bug fix: yes-or-no-p accepts formatter functions (#1639490)
     * bug fix: better handling of exceptions on macOS.
    
    
    New in version 1.3.11
    
     * minor incompatible change: SB-EXT:*INTEXP-MAXIMUM-EXPONENT*
       is removed.
     * enhancement: TRACE ... :REPORT {TRACE,NIL} now work as
       advertised in the documentation string (based on patch by
       Patrick Stein)
     * enhancement: support unboxed signed-word structure slots on
       x86, x86-64 and ARM64. (#377616)
     * optimization: faster logical bit-array operations on
       multidimensional arrays.
     * optimization: better GC performance in the presence of many
       threads. (patch by Ilya Perminov, #1339924)
     * optimization: multiple-value-call is optimized with multiple
       argument forms, not just one. (#753803)
     * bug fix: MAKE-ALIEN-STRING returns the number of allocated
       bytes as a second value as advertised (reported by Johann
       'Myrkraverk' Oskarsson)
     * bug fix: when TO-READTABLE is supplied to COPY-READTABLE, it
       will contain only the macros in FROM-READTABLE and no others.
       (#1631506)
     * enhancement: gencgc has been modified for x86-64 on Linux and
       macOS to a support mark-and-sweep as well as the traditional
       copying strategy. It is conceivable that some applications
       might be adversely affected. Please see ':immobile-space' in
       'base-target-features.lisp-expr' for further details, and
       possible reasons to disable this feature.
     * enhancement: x86-64 supports shrinking the fixed overhead in
       a structure from 2 words to 1 word, reducing memory
       consumption in applications which create many small
       structures.

--- lang/sbcl/patches/patch-src_runtime_gc-common.c
+++ lang/sbcl/patches/patch-src_runtime_gc-common.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_runtime_gc-common.c,v 1.1 2016/10/12 11:35:48 asau Exp $
+$NetBSD: patch-src_runtime_gc-common.c,v 1.2 2016/12/05 20:03:29 asau Exp $
 
---- src/runtime/gc-common.c.orig	2016-09-30 16:41:12.000000000 +0000
+--- src/runtime/gc-common.c.orig	2016-11-29 21:07:36.000000000 +0000
 +++ src/runtime/gc-common.c
-@@ -25,6 +25,10 @@
-  *   <ftp://ftp.cs.utexas.edu/pub/garbage/bigsurv.ps>.
-  */
+@@ -27,6 +27,10 @@
+ 
+ #define _GNU_SOURCE /* for ffsl(3) from string.h */
  
 +#if defined(__NetBSD__)
 +#define _KERNTYPES
