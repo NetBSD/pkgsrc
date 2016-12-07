999e76994be4065999b6cfd04550407ae3fccf2e
fhajny <fhajny>
2016-12-07 17:28:39 +0000

    Update sysutils/collectd to 5.6.2. Most of the NetBSD specific patches were re-applied due to code syntax changes.
    Upstream changes in 5.6.2:
    - collectd: A compile error on AIX has been fixed: "MSG_DONTWAIT" is not
      available on AIX.
    - collectd: The capability checking has been rewritten to be more
      portable.
    - collectd.conf(5): Various typos have been fixed.
    - collectd-tg: Incorrect usage of "nanosleep()" has been fixed which
      caused the tool to sleep (almost) indefinitely.
    - Build system: Detection of the Java environment has been made
      deterministic to allow for reproducible builds.
    - Build system: Detection of the Lua headers has been improved.
    - APC UPS plugin: Unavailable metrics are now skipped instead of
      reported as 0 (zero).
    - Modbus plugin: Build issues on FreeBSD have been fixed.
    - OpenVPN plugin: Gracefully handle empty configurations.
    - PowerDNS plugin: The "recursor command" has been fixed so PowerDNS can
      correctly parse all requested fields.
    - Write Graphite and AMQP plugins: A bug that could lead to a missing
      null-termination has been fixed.
    - Write HTTP plugin: A logic error when checking the correctness of the
      data set has been fixed.
    - Write Kafka plugin: The "Key Random" config option has been fixed.
    - Write Kafka plugin: Support for librdkafka 0.9.0 has been added.
    - Write Riemann plugin: Build issues on Solaris have been fixed.
    - ZFS ARC plugin: A duplicate metric has been removed.

--- sysutils/collectd/patches/patch-src_libcollectclient_network__buffer.c
+++ sysutils/collectd/patches/patch-src_libcollectclient_network__buffer.c
@@ -1,23 +1,23 @@
-$NetBSD: patch-src_libcollectclient_network__buffer.c,v 1.2 2016/10/13 15:17:28 fhajny Exp $
+$NetBSD: patch-src_libcollectclient_network__buffer.c,v 1.3 2016/12/07 17:28:39 fhajny Exp $
 
 Need the workaround on at least SunOS too.
---- src/libcollectdclient/network_buffer.c.orig	2016-09-11 08:10:25.263038721 +0000
+--- src/libcollectdclient/network_buffer.c.orig	2016-11-30 08:52:01.316911197 +0000
 +++ src/libcollectdclient/network_buffer.c
 @@ -36,7 +36,7 @@
  #include <pthread.h>
  
  #if HAVE_LIBGCRYPT
--# if defined __APPLE__
-+# if defined __APPLE__ || defined(__sun)
+-#if defined __APPLE__
++#if defined __APPLE__ || defined(__sun)
  /* default xcode compiler throws warnings even when deprecated functionality
   * is not used. -Werror breaks the build because of erroneous warnings.
   * http://stackoverflow.com/questions/10556299/compiler-warnings-with-libgcrypt-v1-5-0/12830209#12830209
 @@ -49,7 +49,7 @@
   */
- # define GCRYPT_NO_DEPRECATED
- # include <gcrypt.h>
--# if defined __APPLE__
-+# if defined(__APPLE__) || defined(__sun)
+ #define GCRYPT_NO_DEPRECATED
+ #include <gcrypt.h>
+-#if defined __APPLE__
++#if defined __APPLE__ || defined(__sun)
  /* Re enable deprecation warnings */
- #  pragma GCC diagnostic warning "-Wdeprecated-declarations"
- # endif
+ #pragma GCC diagnostic warning "-Wdeprecated-declarations"
+ #endif
