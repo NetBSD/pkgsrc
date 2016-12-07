474477ba30007d32510febc56251bacb86bb8bea
fhajny <fhajny@pkgsrc.org>
2016-12-07 21:34:58 +0000

    Update archivers/lz4 to 1.7.4.2.
    Changes since release 131:
    
    v1.7.4.2
    - fix : Makefile : release build compatible with PIE and customized
      compilation directives provided through environment variables (#274,
      reported by Antoine Martin)
    
    v1.7.4
    - Improved : much better speed in -mx32 mode
    - cli : fix : Large file support in 32-bits mode on Mac OS-X
    - fix : compilation on gcc 4.4 (#272), reported by Antoine Martin
    
    v1.7.3
    - Changed : moved to versioning; package, cli and library have same
      version number
    - Improved: Small decompression speed boost
    - Improved: Small compression speed improvement on 64-bits systems
    - Improved: Small compression ratio and speed improvement on small files
    - Improved: Significant speed boost on ARMv6 and ARMv7
    - Fix : better ratio on 64-bits big-endian targets
    - Improved cmake build script, by Evan Nemerson
    - New liblz4-dll project, by Przemyslaw Skibinki
    - Makefile: Generates object files (*.o) for faster (re)compilation on
      low power systems
    - cli : new : --rm and --help commands
    - cli : new : preserved file attributes, by Przemyslaw Skibinki
    - cli : fix : crash on some invalid inputs
    - cli : fix : -t correctly validates lz4-compressed files, by Nick
      Terrell
    - cli : fix : detects and reports fread() errors, thanks to Hiroshi
      Fujishima report #243
    - cli : bench : new : -r recursive mode
    - lz4cat : can cat multiple files in a single command line (#184)
    - Added : doc/lz4_manual.html, by Przemyslaw Skibinski
    - Added : dictionary compression and frame decompression examples, by
      Nick Terrell
    - Added : Debianization, by Evgeniy Polyakov

--- archivers/lz4/buildlink3.mk
+++ archivers/lz4/buildlink3.mk
@@ -1,12 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.3 2015/06/09 12:04:19 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/12/07 21:34:58 fhajny Exp $
 
 BUILDLINK_TREE+=	lz4
 
 .if !defined(LZ4_BUILDLINK3_MK)
 LZ4_BUILDLINK3_MK:=
 
-BUILDLINK_ABI_DEPENDS.lz4+=	lz4>=130
-BUILDLINK_API_DEPENDS.lz4+=	lz4>=130
+# Set upper limit to force update from the previous version
+# scheme that generated technically higher version numbers
+BUILDLINK_API_DEPENDS.lz4+=	lz4>=1.7.3<100
+BUILDLINK_ABI_DEPENDS.lz4+=	lz4>=1.7.3
 BUILDLINK_PKGSRCDIR.lz4?=	../../archivers/lz4
 .endif # LZ4_BUILDLINK3_MK
 
