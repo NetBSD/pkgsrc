37c2f2da825baa8cc06d8493572cecded8ca1f29
fhajny <fhajny@pkgsrc.org>
2016-12-07 17:28:39 +0000

    Update sysutils/collectd to 5.6.2. Most of the NetBSD specific patches were re-applied due to code syntax changes.
    Upstream changes in 5.6.2:
    - collectd: A compile error on AIX has been fixed: "MSG_DONTWAIT" is not
      available on AIX.
    - collectd: The capability checking has been rewritten to be more
      portable.
    - collectd.conf(5): Various typos have been fixed.
    - collectd-tg: Incorrect usage of "nanosleep()" has been fixed which
      caused the tool to sleep (almost) indefinitely.
    - Build system: Detection of the Java environment has been made
      deterministic to allow for reproducible builds.
    - Build system: Detection of the Lua headers has been improved.
    - APC UPS plugin: Unavailable metrics are now skipped instead of
      reported as 0 (zero).
    - Modbus plugin: Build issues on FreeBSD have been fixed.
    - OpenVPN plugin: Gracefully handle empty configurations.
    - PowerDNS plugin: The "recursor command" has been fixed so PowerDNS can
      correctly parse all requested fields.
    - Write Graphite and AMQP plugins: A bug that could lead to a missing
      null-termination has been fixed.
    - Write HTTP plugin: A logic error when checking the correctness of the
      data set has been fixed.
    - Write Kafka plugin: The "Key Random" config option has been fixed.
    - Write Kafka plugin: Support for librdkafka 0.9.0 has been added.
    - Write Riemann plugin: Build issues on Solaris have been fixed.
    - ZFS ARC plugin: A duplicate metric has been removed.

--- sysutils/collectd/patches/patch-src_irq.c
+++ sysutils/collectd/patches/patch-src_irq.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_irq.c,v 1.3 2016/10/13 15:17:28 fhajny Exp $
+$NetBSD: patch-src_irq.c,v 1.4 2016/12/07 17:28:39 fhajny Exp $
 
 Provide a port to NetBSD.
 
---- src/irq.c.orig	2016-09-11 08:10:25.263038721 +0000
+--- src/irq.c.orig	2016-11-30 08:52:01.316911197 +0000
 +++ src/irq.c
 @@ -27,10 +27,20 @@
  #include "plugin.h"
@@ -10,7 +10,7 @@ Provide a port to NetBSD.
  
 -#if !KERNEL_LINUX
 +#if !KERNEL_LINUX && !KERNEL_NETBSD
- # error "No applicable input method."
+ #error "No applicable input method."
  #endif
  
 +#if KERNEL_NETBSD
@@ -26,17 +26,17 @@ Provide a port to NetBSD.
  /*
   * (Module-)Global variables
   */
-@@ -90,6 +100,7 @@ static void irq_submit (const char *irq_
- 	plugin_dispatch_values (&vl);
+@@ -79,6 +89,7 @@ static void irq_submit(const char *irq_n
+   plugin_dispatch_values(&vl);
  } /* void irq_submit */
  
 +#if KERNEL_LINUX
- static int irq_read (void)
- {
- 	FILE *fh;
-@@ -188,6 +199,64 @@ static int irq_read (void)
+ static int irq_read(void) {
+   FILE *fh;
+   char buffer[1024];
+@@ -171,6 +182,64 @@ static int irq_read(void) {
  
- 	return (0);
+   return (0);
  } /* int irq_read */
 +#endif /* KERNEL_LINUX */
 +
@@ -44,58 +44,58 @@ Provide a port to NetBSD.
 +static int
 +irq_read (void)
 +{
-+	const int mib[4] = {
-+		CTL_KERN,
-+		KERN_EVCNT,
-+		EVCNT_TYPE_INTR,
-+		KERN_EVCNT_COUNT_NONZERO
-+	};
-+	size_t buflen = 0;
-+	void *buf = NULL;
-+	const struct evcnt_sysctl *evs, *last_evs;
++  const int mib[4] = {
++    CTL_KERN,
++    KERN_EVCNT,
++    EVCNT_TYPE_INTR,
++    KERN_EVCNT_COUNT_NONZERO
++  };
++  size_t buflen = 0;
++  void *buf = NULL;
++  const struct evcnt_sysctl *evs, *last_evs;
 +
-+	for (;;) {
-+		size_t newlen;
-+		int error;
++  for (;;) {
++    size_t newlen;
++    int error;
 +
-+		newlen = buflen;
-+		if (buflen)
-+			buf = malloc(buflen);
-+		error = sysctl(mib, __arraycount(mib),
-+			       buf, &newlen, NULL, 0);
-+		if (error) {
-+			ERROR("irq plugin: failed to get event count");
-+			return -1;
-+		}
-+		if (newlen <= buflen) {
-+			buflen = newlen;
-+			break;
-+		}
-+		if (buf)
-+			free(buf);
-+		buflen = newlen;
-+	}
-+	evs = buf;
-+	last_evs = (void*)((char *)buf + buflen);
-+	buflen /= sizeof(uint64_t);
-+	while(evs < last_evs
-+	      && buflen > sizeof(*evs) / sizeof(uint64_t)
-+	      && buflen >= evs->ev_len)
-+	{
-+		char irqname[80];
++    newlen = buflen;
++    if (buflen)
++      buf = malloc(buflen);
++    error = sysctl(mib, __arraycount(mib),
++                   buf, &newlen, NULL, 0);
++    if (error) {
++      ERROR("irq plugin: failed to get event count");
++      return -1;
++    }
++    if (newlen <= buflen) {
++      buflen = newlen;
++      break;
++    }
++    if (buf)
++            free(buf);
++    buflen = newlen;
++  }
++  evs = buf;
++  last_evs = (void*)((char *)buf + buflen);
++  buflen /= sizeof(uint64_t);
++  while(evs < last_evs
++    && buflen > sizeof(*evs) / sizeof(uint64_t)
++    && buflen >= evs->ev_len)
++  {
++    char irqname[80];
 +
-+		snprintf(irqname, 80, "%s-%s", evs->ev_strings,
-+			 evs->ev_strings + evs->ev_grouplen + 1);
++    snprintf(irqname, 80, "%s-%s", evs->ev_strings,
++      evs->ev_strings + evs->ev_grouplen + 1);
 +
-+		irq_submit(irqname, evs->ev_count);
++    irq_submit(irqname, evs->ev_count);
 +
-+		buflen -= evs->ev_len;
-+		evs =(const void*)((const uint64_t *)evs + evs->ev_len);
-+	}
-+	free(buf);
-+	return 0;
++    buflen -= evs->ev_len;
++    evs =(const void*)((const uint64_t *)evs + evs->ev_len);
++  }
++  free(buf);
++  return 0;
 +}
 +#endif /* KERNEL_NETBSD */
  
- void module_register (void)
- {
+ void module_register(void) {
+   plugin_register_config("irq", irq_config, config_keys, config_keys_num);
