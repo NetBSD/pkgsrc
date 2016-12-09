c1b1c7574c522b4e5551d11e4b2df7c128b7e98d
leot <leot@pkgsrc.org>
2016-12-09 08:19:31 +0000

    Update print/mupdf to mupdf-1.10a
    pkgsrc changes:
     - Include more bl3 in buildlink3.mk to reflect current reality
     - Bump BUILDLINK_A[BP]I_DEPENDS.mupdf to 1.10a (packages that depends on
       mupdf seems to not run without being rebuilded despite a bug fix release)
    
    Changes:
    MuPDF 1.10a (2016-11-28)
    ------------------------
    Version 1.10a is a bug fix release.
    
    If you downloaded the windows release for 1.10 you should also
    upgrade, since the zip archive accidentally included the RC2 binaries
    instead of the final version.
    
    A bug with mutool and saving PDF files using the 'ascii' option
    has been fixed.
    
    The iOS build issues have been fixed.

--- print/mupdf/buildlink3.mk
+++ print/mupdf/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2016/11/28 14:40:42 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2016/12/09 08:19:31 leot Exp $
 
 BUILDLINK_TREE+=	mupdf
 
 .if !defined(MUPDF_BUILDLINK3_MK)
 MUPDF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mupdf+=	mupdf>=1.10
-BUILDLINK_ABI_DEPENDS.mupdf?=	mupdf>=1.10
+BUILDLINK_API_DEPENDS.mupdf+=	mupdf>=1.10a
+BUILDLINK_ABI_DEPENDS.mupdf?=	mupdf>=1.10a
 BUILDLINK_PKGSRCDIR.mupdf?=	../../print/mupdf
 
 pkgbase := mupdf
@@ -22,8 +22,12 @@ pkgbase := mupdf
 .include "../../graphics/glfw/buildlink3.mk"
 .endif
 
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../fonts/harfbuzz/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
 .include "../../graphics/jbig2dec/buildlink3.mk"
 .include "../../graphics/openjpeg/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
 .endif	# MUPDF_BUILDLINK3_MK
 
