0e0e8538964d9205589db4b436229a3ebea400b6
leot <leot@pkgsrc.org>
2016-12-09 14:56:34 +0000

    Update comms/gammu to gammu-1.37.91
    Changes:
    20161023 - 1.37.91
    
    [!] * Changed version of the shared library.
    [-] * Improved support for ZTE MF100.
    [-] * Ignore unsolicited +CLCC: reply.
    [-] * Correctly report when some SMSD SQL backend is not compiled in.
    [-] * Fix build of MySQL backend on Linux.
    
    20161018 - 1.37.90
    
    [-] * Improved support Huawei K3770.
    [!] * API changes in some parameter types.
    [-] * Fixed various Windows compilation issues.
    [-] * Fixed several resource leaks.
    [-] * Create outbox SMS atomically in FILES backend.
    [!] * Removed getlocation command as we no longer fit into their usage policy.
    [-] * Fixed call diverts on TP-LINK MA260.
    [+] * Initial support for Oracle database.
    [!] * Removed unused daemons, pbk and pbk_groups tables from the SMSD schema.
    [+] * SMSD outbox entries now can have priority set in the database.
    [+] * Added SIM IMSI to the SMSD status table.
    [+] * Added CheckNetwork directive.
    [+] * SMSD attempts to power on radio if disabled.
    [-] * Fixed processing of AT unsolicited responses in some cases.
    [-] * Fixed parsing USSD responses from some devices.
    
    20160816 - 1.37.4
    
    [-] * Improved support for Huawei E3131.
    [-] * Fixed SMS support for MULTIBAND 900E.
    [-] * Fixed SMS created in text mode.
    
    20160524 - 1.37.3
    
    [-] * Improved support for Huawei E398.
    [-] * Improved support for Huawei/Vodafone K4505.
    [-] * Fixed possible crash if SMSD used in library.
    [-] * Improved support for Huawei E180.
    
    20160413 - 1.37.2
    
    [-] * Fixed compilation of SMSD.
    
    20160413 - 1.37.1
    
    [-] * Properly report errors in HEX encoded strings from SMSD SQL backends.
    [-] * Configurable SMSD table names.
    [-] * Improved support for Huawei E303.
    [-] * Improved support for Vodafone K4511.
    [-] * Improved support for Telit M2M modules.

--- comms/gammu/buildlink3.mk
+++ comms/gammu/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2016/02/07 15:29:34 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/12/09 14:56:34 leot Exp $
 
 BUILDLINK_TREE+=	gammu
 
 .if !defined(GAMMU_BUILDLINK3_MK)
 GAMMU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gammu+=	gammu>=1.37.0
+BUILDLINK_API_DEPENDS.gammu+=	gammu>=1.37.91
 BUILDLINK_PKGSRCDIR.gammu?=	../../comms/gammu
 
 .endif	# GAMMU_BUILDLINK3_MK
