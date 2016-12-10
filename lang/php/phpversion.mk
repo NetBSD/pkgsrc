1f1541da7e7ecddb9b75e068e00c8c683dcaf06b
taca <taca@pkgsrc.org>
2016-12-10 07:10:53 +0000

    Update php71 to 7.1.0 (PHP 7.1.0 official release).
    Changes are too many to write here, please refer NEWS file.

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.167 2016/12/10 07:09:30 taca Exp $
+# $NetBSD: phpversion.mk,v 1.168 2016/12/10 07:10:53 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.29
 PHP70_VERSION=	7.0.14
-PHP71_VERSION=	7.1.0RC6
+PHP71_VERSION=	7.1.0
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
