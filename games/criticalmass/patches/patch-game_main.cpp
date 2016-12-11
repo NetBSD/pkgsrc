0e11ae0ed7230b31f185daf99b031543d779a1de
spz <spz@pkgsrc.org>
2016-12-11 20:47:09 +0000

    Pullup ticket #5172 - requested by dholland games/criticalmass: security fix
    Revisions pulled up:
    - games/criticalmass/Makefile                                   1.38
    - games/criticalmass/distinfo                                   1.14
    - games/criticalmass/patches/patch-Makefile.am                  1.1
    - games/criticalmass/patches/patch-Makefile.in                  1.1
    - games/criticalmass/patches/patch-aa                           1.5
    - games/criticalmass/patches/patch-game_Makefile.am             1.1
    - games/criticalmass/patches/patch-game_Makefile.in             1.1
    - games/criticalmass/patches/patch-game_main.cpp                1.2
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	dholland
       Date:		Sat Dec 10 21:46:52 UTC 2016
    
       Modified Files:
       	pkgsrc/games/criticalmass: Makefile distinfo
       	pkgsrc/games/criticalmass/patches: patch-aa patch-game_main.cpp
       Added Files:
       	pkgsrc/games/criticalmass/patches: patch-Makefile.am patch-Makefile.in
       	    patch-game_Makefile.am patch-game_Makefile.in
    
       Log Message:
       Remove use of ancient and highly insecure curl-7.14.0; use pkgsrc curl.
       Also disable online update by default. (It can be re-enabled in the
       config.)  PKGREVISION -> 8.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.37 -r1.38 pkgsrc/games/criticalmass/Makefile
       cvs rdiff -u -r1.13 -r1.14 pkgsrc/games/criticalmass/distinfo
       cvs rdiff -u -r0 -r1.1 pkgsrc/games/criticalmass/patches/patch-Makefile.am \
           pkgsrc/games/criticalmass/patches/patch-Makefile.in \
           pkgsrc/games/criticalmass/patches/patch-game_Makefile.am \
           pkgsrc/games/criticalmass/patches/patch-game_Makefile.in
       cvs rdiff -u -r1.4 -r1.5 pkgsrc/games/criticalmass/patches/patch-aa
       cvs rdiff -u -r1.1 -r1.2 \
           pkgsrc/games/criticalmass/patches/patch-game_main.cpp

--- games/criticalmass/patches/patch-game_main.cpp
+++ games/criticalmass/patches/patch-game_main.cpp
@@ -1,10 +1,11 @@
-$NetBSD: patch-game_main.cpp,v 1.1 2011/06/20 09:30:03 wiz Exp $
+$NetBSD: patch-game_main.cpp,v 1.1.44.1 2016/12/11 20:47:09 spz Exp $
 
 Add missing include (uncovered by png-1.5).
+Disable online update by default.
 
 --- game/main.cpp.orig	2006-06-30 03:46:11.000000000 +0000
 +++ game/main.cpp
-@@ -24,6 +24,7 @@
+@@ -24,17 +24,18 @@
  #include <ResourceManager.hpp>
  #include <GetDataPath.hpp>
  #include <OnlineUpdate.hpp>
@@ -12,3 +13,16 @@ Add missing include (uncovered by png-1.5).
  
  #include <sys/stat.h>
  #include <sys/types.h>
+ 
+ void migrateConfig( void)
+ {
+-    //if onlineCheck is not set, default it to true
++    //if onlineCheck is not set, default it to false
+     bool dummy;
+     if( ! ConfigS::instance()->getBoolean( "onlineCheck", dummy))
+     {
+-	ConfigS::instance()->updateKeyword( "onlineCheck", "1");
++	ConfigS::instance()->updateKeyword( "onlineCheck", "0");
+     }
+ 
+ #ifdef WIN32
