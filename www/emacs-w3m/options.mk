95e064bd1ea3803baf1e7cf6e5074f3e52d6ea24
hauke <hauke@pkgsrc.org>
2016-12-12 13:53:10 +0000

    Make www/emacs-w3m build and run with xemacs, fixing PR pkg/40685
    Changes:
    
    o accomodate for differing dependencies:
      + graphics/gifsicle as a bug workaround
      + devel/flim (this was an implicite dependency through devel/semi)
      - devel/{apel,semi}, editors/mule-ucs contained in xemacs-packages
    
    o conditional PLIST changes for differing installation paths
    
    Tested with xemacs 21.4 and emacs 22

--- www/emacs-w3m/options.mk
+++ www/emacs-w3m/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2008/04/12 22:43:13 jlam Exp $
+# $NetBSD: options.mk,v 1.3 2016/12/12 13:53:10 hauke Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.emacs-w3m
 PKG_SUPPORTED_OPTIONS=	emacs-w3m-mew
@@ -6,7 +6,11 @@ PKG_SUPPORTED_OPTIONS=	emacs-w3m-mew
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=	mew
-.if !empty(PKG_OPTIONS:Memacs-w3m-mew)
+
+# xemacs-packages contain mew (but not mew-nntp)
+.if (${EMACS_FLAVOR} == "xemacs")
+PLIST.mew =	yes
+.elif !empty(PKG_OPTIONS:Memacs-w3m-mew)
 .include "../../mail/mew/buildlink3.mk"
 PLIST.mew=	yes
 .endif
