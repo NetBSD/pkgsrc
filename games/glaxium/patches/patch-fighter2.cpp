8b361d6bf09f16ec7823037598330e90555ce87f
dholland <dholland@pkgsrc.org>
2016-12-12 06:21:47 +0000

    Fix install to not depend on umask. Avoid undefined behavior to make it run. PKGREVISION -> 11.

---
+++ games/glaxium/patches/patch-fighter2.cpp
@@ -0,0 +1,15 @@
+$NetBSD: patch-fighter2.cpp,v 1.1 2016/12/12 06:21:47 dholland Exp $
+
+Use right array size; avoid running off the end.
+
+--- fighter2.cpp~	2002-10-04 18:05:20.000000000 +0000
++++ fighter2.cpp
+@@ -87,7 +87,7 @@ void Fighter2::initList()
+ 	 }
+    
+     
+-    for (i=0;i<587;i++) {
++    for (i=0;i<578;i++) {
+         fighter2_meshes[i*24+7] = 1 - fighter2_meshes[i*24+7];
+         fighter2_meshes[i*24+7+8] = 1 - fighter2_meshes[i*24+7+8];
+         fighter2_meshes[i*24+7+16] = 1 - fighter2_meshes[i*24+7+16];
