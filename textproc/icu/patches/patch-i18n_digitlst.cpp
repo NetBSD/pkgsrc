1815df53ddb0186fb152e761c91b202fa1a1bfb0
adam <adam@pkgsrc.org>
2016-12-12 17:46:39 +0000

    Updates in ICU 58.2
    Common Changes
    * CLDR 30.0.3
    * Time zone database version 2016j
    * ICU SVN repository structure change. See the note on the Source Code Access page for more information.
    
    ICU4C Fixes
    * 12815 uspoof_getSkeleton sets backwards-incompatible illegal argument exception
    * 12822 digitlist.cpp won't compile on msvc under Node.js
    * 12825 uspoof_check goes into an "infinite loop" when U+30FB is in an input string
    * 12832 GreekUpper::toUpper skips the final character on a non-terminated UTF-8 string
    * 12849 u_strToTitle returns incorrect length if destination is NULL
    * 12868 uprv_convertToPosix() Windows bug

--- textproc/icu/patches/patch-i18n_digitlst.cpp
+++ textproc/icu/patches/patch-i18n_digitlst.cpp
@@ -1,13 +1,13 @@
-$NetBSD: patch-i18n_digitlst.cpp,v 1.1 2016/12/04 04:11:33 ryoon Exp $
+$NetBSD: patch-i18n_digitlst.cpp,v 1.2 2016/12/12 17:46:39 adam Exp $
 
---- i18n/digitlst.cpp.orig	2016-10-17 05:10:38.000000000 +0000
+--- i18n/digitlst.cpp.orig	2016-10-27 01:37:56.000000000 +0000
 +++ i18n/digitlst.cpp
 @@ -61,7 +61,7 @@
  #endif
  
- #if U_USE_STRTOD_L && !U_PLATFORM_USES_ONLY_WIN32_API
--# if U_PLATFORM == U_PF_CYGWIN
-+# if (U_PLATFORM == U_PF_CYGWIN) || defined(__NetBSD__)
+ #if U_USE_STRTOD_L
+-# if U_PLATFORM_USES_ONLY_WIN32_API || U_PLATFORM == U_PF_CYGWIN
++# if U_PLATFORM_USES_ONLY_WIN32_API || U_PLATFORM == U_PF_CYGWIN || defined(__NetBSD__)
  #   include <locale.h>
  # else
  #   include <xlocale.h>
