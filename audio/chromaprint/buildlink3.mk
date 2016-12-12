79ec7202859702c48344f9eba5ff7a322eface1e
wiz <wiz@pkgsrc.org>
2016-12-12 10:15:13 +0000

    Updated chromaprint to 1.4.
    Switch to ffmpeg3.
    
    Version 1.4 -- December 3, 2016
    ===============================
    
     - Fully streaming audio processing without any post-processing step for the fingerprint generation.
     - Many performance optimizations. Especially when processing long streams, operations should be faster and use less memory.
     - C++11 compiler is now required for building the library.
     - The public C API now uses standard fixed-size int types from stdint.h (breaks backwards compatibility).
     - All code written for the library is now distributed under the MIT license.
     - Kiss FFT is now included with the library and automatically used if no other FFT library is found.
     - Added support for using the FFTW3F version of FFTW3.
     - Boost is no longer used.

--- audio/chromaprint/buildlink3.mk
+++ audio/chromaprint/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2016/08/17 00:06:39 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2016/12/12 10:15:13 wiz Exp $
 
 BUILDLINK_TREE+=	chromaprint
 
@@ -6,10 +6,10 @@ BUILDLINK_TREE+=	chromaprint
 CHROMAPRINT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.chromaprint+=	chromaprint>=1.1
-BUILDLINK_ABI_DEPENDS.chromaprint?=	chromaprint>=1.3.2nb1
+BUILDLINK_ABI_DEPENDS.chromaprint?=	chromaprint>=1.4
 BUILDLINK_PKGSRCDIR.chromaprint?=	../../audio/chromaprint
 
-.include "../../multimedia/ffmpeg2/buildlink3.mk"
+.include "../../multimedia/ffmpeg3/buildlink3.mk"
 .endif	# CHROMAPRINT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-chromaprint
