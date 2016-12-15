c1ed0592e7cd34ae1a53c0fe3ce8f6f6e8b1ee5c
joerg <joerg@pkgsrc.org>
2016-12-15 23:45:07 +0000

    Fix C++11 fallout.

---
+++ audio/ardour/patches/patch-gtk2__ardour_editor.cc
@@ -0,0 +1,27 @@
+$NetBSD: patch-gtk2__ardour_editor.cc,v 1.1 2016/12/15 23:45:07 joerg Exp $
+
+Use bool conversion, RefPtr doesn't compare to 0 in C++11 mode.
+
+--- gtk2_ardour/editor.cc.orig	2016-12-15 17:18:50.532467126 +0000
++++ gtk2_ardour/editor.cc
+@@ -714,16 +714,16 @@ Editor::Editor ()
+ 	list<Glib::RefPtr<Gdk::Pixbuf> > window_icons;
+ 	Glib::RefPtr<Gdk::Pixbuf> icon;
+ 
+-	if ((icon = ::get_icon ("ardour_icon_16px")) != 0) {
++	if ((icon = ::get_icon ("ardour_icon_16px"))) {
+ 		window_icons.push_back (icon);
+ 	}
+-	if ((icon = ::get_icon ("ardour_icon_22px")) != 0) {
++	if ((icon = ::get_icon ("ardour_icon_22px"))) {
+ 		window_icons.push_back (icon);
+ 	}
+-	if ((icon = ::get_icon ("ardour_icon_32px")) != 0) {
++	if ((icon = ::get_icon ("ardour_icon_32px"))) {
+ 		window_icons.push_back (icon);
+ 	}
+-	if ((icon = ::get_icon ("ardour_icon_48px")) != 0) {
++	if ((icon = ::get_icon ("ardour_icon_48px"))) {
+ 		window_icons.push_back (icon);
+ 	}
+ 	if (!window_icons.empty()) {
