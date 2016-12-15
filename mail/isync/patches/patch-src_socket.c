e444281d839e6cb78ac64e87e783dbdc780dfd98
schmonz <schmonz@pkgsrc.org>
2016-12-15 14:15:01 +0000

    Update to 1.2.1. From the changelog:
    - The 'isync' compatibility wrapper is now deprecated.
    - An IMAP Path/NAMESPACE rooted in INBOX won't be handled specially any more.
      This means that some Patterns may need adjustment.
    - The default output is a lot less verbose now.
      The meanings of the -V and -D options changed significantly.
    - The SSL/TLS configuration has been re-designed.
      SSL is now explicitly enabled or disabled - "use SSL if available" is gone.
      Notice: Tunnels are assumed to be secure and thus default to no SSL.
    - Support for SASL (flexible authentication) has been added.
    - Support for Windows file systems has been added.
    - Support for compressed data transfer has been added.
    - Folder deletions can be propagated now.

--- mail/isync/patches/patch-src_socket.c
+++ mail/isync/patches/patch-src_socket.c
@@ -1,9 +1,10 @@
-$NetBSD: patch-src_socket.c,v 1.3 2015/07/04 15:52:51 bsiegert Exp $
+$NetBSD: patch-src_socket.c,v 1.4 2016/12/15 14:15:01 schmonz Exp $
+
 Prevent mbsync from crashing, PR pkg/49784.
 
---- src/socket.c.orig	2015-07-04 15:47:16.000000000 +0000
+--- src/socket.c.orig	2015-09-27 10:13:04.000000000 +0000
 +++ src/socket.c
-@@ -477,7 +477,10 @@ static void
+@@ -509,7 +509,10 @@ static void
  socket_connected( conn_t *conn )
  {
  #ifdef HAVE_IPV6
@@ -13,17 +14,5 @@ Prevent mbsync from crashing, PR pkg/49784.
 +		conn->addrs = NULL;
 +	}
  #endif
- 	conf_fd( conn->fd, 0, POLLIN );
+ 	conf_notifier( &conn->notify, 0, POLLIN );
  	conn->state = SCK_READY;
-@@ -488,7 +491,10 @@ static void
- socket_connect_bail( conn_t *conn )
- {
- #ifdef HAVE_IPV6
--	freeaddrinfo( conn->addrs );
-+	if ( conn->addrs ) {
-+		freeaddrinfo( conn->addrs );
-+		conn->addrs = NULL;
-+	}
- #endif
- 	free( conn->name );
- 	conn->name = 0;
