78fbc69d804fd64b95d14303543f45258c2453b0
he <he@pkgsrc.org>
2016-12-16 13:05:58 +0000

    Add a cast so that this builds with the compiler in NetBSD 7.0 as well.

--- graphics/jasper/patches/patch-src_libjasper_jp2_jp2__dec.c
+++ graphics/jasper/patches/patch-src_libjasper_jp2_jp2__dec.c
@@ -1,10 +1,19 @@
-$NetBSD: patch-src_libjasper_jp2_jp2__dec.c,v 1.3 2016/12/16 09:44:44 he Exp $
+$NetBSD: patch-src_libjasper_jp2_jp2__dec.c,v 1.4 2016/12/16 13:05:58 he Exp $
 
 Only output debug info if debuglevel >= 1.
+And .. cast argument to jas_iccprof_createfrombuf() to match.
 
 --- src/libjasper/jp2/jp2_dec.c.orig	2016-11-16 15:03:41.000000000 +0000
 +++ src/libjasper/jp2/jp2_dec.c
-@@ -302,7 +302,9 @@ jas_image_t *jp2_decode(jas_stream_t *in
+@@ -295,14 +295,16 @@ jas_image_t *jp2_decode(jas_stream_t *in
+ 		jas_image_setclrspc(dec->image, jp2_getcs(&dec->colr->data.colr));
+ 		break;
+ 	case JP2_COLR_ICC:
+-		iccprof = jas_iccprof_createfrombuf(dec->colr->data.colr.iccp,
++		iccprof = jas_iccprof_createfrombuf((jas_uchar *)dec->colr->data.colr.iccp,
+ 		  dec->colr->data.colr.iccplen);
+ 		if (!iccprof) {
+ 			jas_eprintf("error: failed to parse ICC profile\n");
  			goto error;
  		}
  		jas_iccprof_gethdr(iccprof, &icchdr);
