f25cd0abeb2cb96c85d9a1856deb65cf4652fb15
he <he@pkgsrc.org>
2016-12-16 09:44:44 +0000

    Upgrade jasper from 1.900.1 to 1.900.29. This integrates most of the patches we had applied in pkgsrc.
    The changes are in ChangeLog, and are not well summarized anywhere
    I can find, sorry...
    
    OK from adam@

--- graphics/jasper/patches/patch-src_libjasper_jp2_jp2__cod.c
+++ graphics/jasper/patches/patch-src_libjasper_jp2_jp2__cod.c
@@ -1,49 +1,10 @@
-$NetBSD: patch-src_libjasper_jp2_jp2__cod.c,v 1.2 2016/05/16 14:03:40 he Exp $
+$NetBSD: patch-src_libjasper_jp2_jp2__cod.c,v 1.3 2016/12/16 09:44:44 he Exp $
 
 Only output debug info if debuglevel >= 1.
 
-Fix CVE-2008-3520, patches from
-https://bugs.gentoo.org/show_bug.cgi?id=222819
-
---- src/libjasper/jp2/jp2_cod.c.old	2016-03-31 14:47:00.000000000 +0200
-+++ src/libjasper/jp2/jp2_cod.c	2016-03-31 14:48:20.000000000 +0200
-@@ -372,7 +372,7 @@
- 	jp2_bpcc_t *bpcc = &box->data.bpcc;
- 	unsigned int i;
- 	bpcc->numcmpts = box->datalen;
--	if (!(bpcc->bpcs = jas_malloc(bpcc->numcmpts * sizeof(uint_fast8_t)))) {
-+	if (!(bpcc->bpcs = jas_alloc2(bpcc->numcmpts, sizeof(uint_fast8_t)))) {
- 		return -1;
- 	}
- 	for (i = 0; i < bpcc->numcmpts; ++i) {
-@@ -416,7 +416,7 @@
- 		break;
- 	case JP2_COLR_ICC:
- 		colr->iccplen = box->datalen - 3;
--		if (!(colr->iccp = jas_malloc(colr->iccplen * sizeof(uint_fast8_t)))) {
-+		if (!(colr->iccp = jas_alloc2(colr->iccplen, sizeof(uint_fast8_t)))) {
- 			return -1;
- 		}
- 		if (jas_stream_read(in, colr->iccp, colr->iccplen) != colr->iccplen) {
-@@ -453,7 +453,7 @@
- 	if (jp2_getuint16(in, &cdef->numchans)) {
- 		return -1;
- 	}
--	if (!(cdef->ents = jas_malloc(cdef->numchans * sizeof(jp2_cdefchan_t)))) {
-+	if (!(cdef->ents = jas_alloc2(cdef->numchans, sizeof(jp2_cdefchan_t)))) {
- 		return -1;
- 	}
- 	for (channo = 0; channo < cdef->numchans; ++channo) {
-@@ -766,7 +766,7 @@
- 	unsigned int i;
- 
- 	cmap->numchans = (box->datalen) / 4;
--	if (!(cmap->ents = jas_malloc(cmap->numchans * sizeof(jp2_cmapent_t)))) {
-+	if (!(cmap->ents = jas_alloc2(cmap->numchans, sizeof(jp2_cmapent_t)))) {
- 		return -1;
- 	}
- 	for (i = 0; i < cmap->numchans; ++i) {
-@@ -795,11 +795,15 @@
+--- src/libjasper/jp2/jp2_cod.c.orig	2016-11-16 15:03:41.000000000 +0000
++++ src/libjasper/jp2/jp2_cod.c
+@@ -808,11 +808,15 @@ static void jp2_cmap_dumpdata(jp2_box_t 
  	jp2_cmap_t *cmap = &box->data.cmap;
  	unsigned int i;
  	jp2_cmapent_t *ent;
@@ -57,21 +18,8 @@ https://bugs.gentoo.org/show_bug.cgi?id=222819
 -		  (int) ent->cmptno, (int) ent->map, (int) ent->pcol);
 +		if (jas_getdbglevel() >= 1) {
 +			fprintf(out, "cmptno=%d; map=%d; pcol=%d\n",
-+			  (int) ent->cmptno, (int) ent->map, (int) ent->pcol);
++				(int) ent->cmptno, (int) ent->map, (int) ent->pcol);
 +		}
  	}
  }
  
-@@ -828,10 +832,10 @@
- 		return -1;
- 	}
- 	lutsize = pclr->numlutents * pclr->numchans;
--	if (!(pclr->lutdata = jas_malloc(lutsize * sizeof(int_fast32_t)))) {
-+	if (!(pclr->lutdata = jas_alloc2(lutsize, sizeof(int_fast32_t)))) {
- 		return -1;
- 	}
--	if (!(pclr->bpc = jas_malloc(pclr->numchans * sizeof(uint_fast8_t)))) {
-+	if (!(pclr->bpc = jas_alloc2(pclr->numchans, sizeof(uint_fast8_t)))) {
- 		return -1;
- 	}
- 	for (i = 0; i < pclr->numchans; ++i) {
--- graphics/jasper/patches/patch-src_libjasper_jp2_jp2__dec.c
+++ graphics/jasper/patches/patch-src_libjasper_jp2_jp2__dec.c
@@ -1,17 +1,12 @@
-$NetBSD: patch-src_libjasper_jp2_jp2__dec.c,v 1.2 2016/05/16 14:03:40 he Exp $
+$NetBSD: patch-src_libjasper_jp2_jp2__dec.c,v 1.3 2016/12/16 09:44:44 he Exp $
 
 Only output debug info if debuglevel >= 1.
-Apply fix for oCERT-2014-012, from
-https://bugzilla.redhat.com/show_bug.cgi?id=1173162
 
-Fix CVE-2008-3520, patches from
-https://bugs.gentoo.org/show_bug.cgi?id=222819
-
---- src/libjasper/jp2/jp2_dec.c.old	2016-03-31 14:47:00.000000000 +0200
-+++ src/libjasper/jp2/jp2_dec.c	2016-03-31 14:48:20.000000000 +0200
-@@ -293,7 +293,9 @@
- 		  dec->colr->data.colr.iccplen);
- 		assert(iccprof);
+--- src/libjasper/jp2/jp2_dec.c.orig	2016-11-16 15:03:41.000000000 +0000
++++ src/libjasper/jp2/jp2_dec.c
+@@ -302,7 +302,9 @@ jas_image_t *jp2_decode(jas_stream_t *in
+ 			goto error;
+ 		}
  		jas_iccprof_gethdr(iccprof, &icchdr);
 -		jas_eprintf("ICC Profile CS %08x\n", icchdr.colorspc);
 +		if (jas_getdbglevel() >= 1) {
@@ -20,35 +15,3 @@ https://bugs.gentoo.org/show_bug.cgi?id=222819
  		jas_image_setclrspc(dec->image, fromiccpcs(icchdr.colorspc));
  		dec->image->cmprof_ = jas_cmprof_createfromiccprof(iccprof);
  		assert(dec->image->cmprof_);
-@@ -336,7 +338,7 @@
- 	}
- 
- 	/* Allocate space for the channel-number to component-number LUT. */
--	if (!(dec->chantocmptlut = jas_malloc(dec->numchans * sizeof(uint_fast16_t)))) {
-+	if (!(dec->chantocmptlut = jas_alloc2(dec->numchans, sizeof(uint_fast16_t)))) {
- 		jas_eprintf("error: no memory\n");
- 		goto error;
- 	}
-@@ -354,7 +356,7 @@
- 			if (cmapent->map == JP2_CMAP_DIRECT) {
- 				dec->chantocmptlut[channo] = channo;
- 			} else if (cmapent->map == JP2_CMAP_PALETTE) {
--				lutents = jas_malloc(pclrd->numlutents * sizeof(int_fast32_t));
-+				lutents = jas_alloc2(pclrd->numlutents, sizeof(int_fast32_t));
- 				for (i = 0; i < pclrd->numlutents; ++i) {
- 					lutents[i] = pclrd->lutdata[cmapent->pcol + i * pclrd->numchans];
- 				}
-@@ -386,6 +388,13 @@
- 	/* Determine the type of each component. */
- 	if (dec->cdef) {
- 		for (i = 0; i < dec->numchans; ++i) {
-+			/* Is the channel number reasonable? */
-+			if (dec->cdef->data.cdef.ents[i].channo >= dec->numchans) {
-+				jas_eprintf("error: invalid channel number in CDEF box\n");
-+
-+				goto error;
-+
-+			}
- 			jas_image_setcmpttype(dec->image,
- 			  dec->chantocmptlut[dec->cdef->data.cdef.ents[i].channo],
- 			  jp2_getct(jas_image_clrspc(dec->image),
