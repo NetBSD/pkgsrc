401d4a69b9d061fc107dbfbbcacc7707d674e79b
joerg <joerg@pkgsrc.org>
2016-12-18 21:39:58 +0000

    Rename hmac to bghmac. Bump revision.

---
+++ devel/bglibs/patches/patch-crypto_hmac-md5.c
@@ -0,0 +1,13 @@
+$NetBSD: patch-crypto_hmac-md5.c,v 1.1 2016/12/18 21:39:58 joerg Exp $
+
+--- crypto/hmac-md5.c.orig	2016-12-18 09:59:13.731995800 +0000
++++ crypto/hmac-md5.c
+@@ -65,7 +65,7 @@ static void HMACTest(const char* key, co
+   const str data_str = { (char*)data, strlen(data), 0 };
+   unsigned char digest[128/8];
+   unsigned i;
+-  hmac(&hmac_md5, &key_str, &data_str, digest);
++  bghmac(&hmac_md5, &key_str, &data_str, digest);
+   printf("HMAC (%d,%d) = ", key_str.len, data_str.len);
+   for (i = 0; i < sizeof digest; ++i) printf("%02x", digest[i]);
+   printf("\n");
