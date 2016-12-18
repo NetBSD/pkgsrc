8046bbb6953477922fdf731f7693af9943efa761
joerg <joerg@pkgsrc.org>
2016-12-18 21:51:31 +0000

    Don't check sign of pointers.

--- misc/kdeadmin3/patches/patch-knetworkconf_knetworkconf_knetworkconf.cpp
+++ misc/kdeadmin3/patches/patch-knetworkconf_knetworkconf_knetworkconf.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-knetworkconf_knetworkconf_knetworkconf.cpp,v 1.1 2012/06/25 11:30:29 joerg Exp $
+$NetBSD: patch-knetworkconf_knetworkconf_knetworkconf.cpp,v 1.2 2016/12/18 21:51:31 joerg Exp $
 
---- knetworkconf/knetworkconf/knetworkconf.cpp.orig	2012-06-18 21:32:02.000000000 +0000
+--- knetworkconf/knetworkconf/knetworkconf.cpp.orig	2006-07-22 08:08:14.000000000 +0000
 +++ knetworkconf/knetworkconf/knetworkconf.cpp
 @@ -29,7 +29,8 @@ KNetworkConf::KNetworkConf(QWidget *pare
    klvProfilesList->setAllColumnsShowFocus(true);
@@ -12,3 +12,21 @@ $NetBSD: patch-knetworkconf_knetworkconf_knetworkconf.cpp,v 1.1 2012/06/25 11:30
    tooltip = new KProfilesListViewToolTip(klvProfilesList);
      
    //Connect signals emmitted by the backend to know when data is ready to be painted.
+@@ -868,7 +869,7 @@ void KNetworkConf::addKnownHostSlot(){
+ 
+   dlg.exec();
+ 
+-  if (!dlg.kleIpAddress->text().isEmpty() && dlg.klbAliases->firstItem() > 0 )
++  if (!dlg.kleIpAddress->text().isEmpty() && dlg.klbAliases->firstItem())
+   {
+     QListViewItem * item = new QListViewItem( klvKnownHosts, 0 );
+ 
+@@ -911,7 +912,7 @@ void KNetworkConf::editKnownHostSlot()
+   dlg.exec();
+ 
+   QString _aliases;
+-  if (!dlg.kleIpAddress->text().isEmpty() && dlg.klbAliases->firstItem() > 0 )
++  if (!dlg.kleIpAddress->text().isEmpty() && dlg.klbAliases->firstItem())
+   {
+     QListViewItem * item = klvKnownHosts->currentItem();
+ 
