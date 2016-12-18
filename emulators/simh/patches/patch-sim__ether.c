be43ed443894b4e0b2b82ec37fa08035bc8b0cca
hans <hans@pkgsrc.org>
2016-12-18 11:11:28 +0000

    There haven't been any simh releases in years, so convert this package to take a snapshot from github. Update to 7964e2b72d9.
    Changes: too many to list them all, full list of changes is available at
    https://github.com/simh/simh/blob/master/README.md
    
    Highlights:
    - many new simulators
    - simulator front panel API
    - VAXstation video display simulation using SDL
    - UDP and NAT packet transports
    - scripting support

---
+++ emulators/simh/patches/patch-sim__ether.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-sim__ether.c,v 1.1 2016/12/18 11:11:28 hans Exp $
+
+SunOS needs sys/ioctl.h for FIONBIO definition.
+
+--- sim_ether.c.orig	2016-11-22 10:24:26.000000000 +0100
++++ sim_ether.c
+@@ -366,6 +366,7 @@
+ */
+ 
+ #include <ctype.h>
++#include <sys/ioctl.h>
+ #include "sim_ether.h"
+ #include "sim_sock.h"
+ #include "sim_timer.h"
