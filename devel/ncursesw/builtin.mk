ae355d3d7da148991d2b39ac2eddb257b9357052
joerg <joerg>
2016-12-18 23:30:34 +0000

    Rename ncurses's libform, libpanel and libmenu to libgnuform, libgnupanel and libgnumenu as they overlap with system libraries on NetBSD, resulting in strange breakage like in devel/cmake's ccmake frontend.

--- devel/ncursesw/builtin.mk
+++ devel/ncursesw/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.11 2013/11/23 11:29:35 obache Exp $
+# $NetBSD: builtin.mk,v 1.12 2016/12/18 23:30:34 joerg Exp $
 
 BUILTIN_PKG:=	ncursesw
 
@@ -117,6 +117,10 @@ BUILDLINK_TRANSFORM+=		l:ncursesw:${BUILTIN_LIBNAME.ncursesw}
 	empty(H_NB_CURSESW:M${LOCALBASE}/*)
 BUILDLINK_CPPFLAGS.ncursesw+=	-DHAVE_WCHAR=1
 .    endif
+.  else
+BUILDLINK_TRANSFORM+=		l:formw:gnuformw
+BUILDLINK_TRANSFORM+=		l:panelw:gnupanelw
+BUILDLINK_TRANSFORM+=		l:menuw:gnumenuw
 .  endif
 
 # According to the ncurses(3) manual page, applications that want to use
