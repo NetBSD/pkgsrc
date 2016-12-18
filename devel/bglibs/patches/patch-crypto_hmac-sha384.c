9fe541440a916c6b1a8aa5d71d4b9d1800a413b5
joerg <joerg@pkgsrc.org>
2016-12-18 21:39:58 +0000

    Rename hmac to bghmac. Bump revision.

---
+++ devel/bglibs/patches/patch-crypto_hmac-sha384.c
@@ -0,0 +1,13 @@
+$NetBSD: patch-crypto_hmac-sha384.c,v 1.1 2016/12/18 21:39:58 joerg Exp $
+
+--- crypto/hmac-sha384.c.orig	2016-12-18 09:59:03.209209425 +0000
++++ crypto/hmac-sha384.c
+@@ -46,7 +46,7 @@ static void test_hmac(const char* key, c
+   const str data_str = { (char*)data, strlen(data), 0 };
+   unsigned char digest[SHA384_DIGEST_LENGTH];
+   unsigned i;
+-  hmac(&hmac_sha384, &key_str, &data_str, digest);
++  bghmac(&hmac_sha384, &key_str, &data_str, digest);
+   for (i = 0; i < sizeof digest; ++i)
+     obuf_putxw(&outbuf, digest[i], 2, '0');
+   obuf_endl(&outbuf);
