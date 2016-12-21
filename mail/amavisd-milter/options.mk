699293781d9642ea96570420b3725e69c1f063c3
kivinen <kivinen@pkgsrc.org>
2016-12-21 12:05:51 +0000

    Sendmail milter for amavisd-new.

---
+++ mail/amavisd-milter/options.mk
@@ -0,0 +1,7 @@
+# $NetBSD: options.mk,v 1.1 2016/12/21 12:05:51 kivinen Exp $
+
+PKG_OPTIONS_VAR=        PKG_OPTIONS.amavisd-milter
+PKG_SUPPORTED_OPTIONS=  # empty
+PKG_SUGGESTED_OPTIONS=  # empty
+
+.include "../../mk/bsd.options.mk"
