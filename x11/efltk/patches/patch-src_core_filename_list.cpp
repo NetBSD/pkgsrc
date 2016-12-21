c2e9fc21849222115cacacb0a96b5b4b8c28ce2b
joerg <joerg@pkgsrc.org>
2016-12-21 13:40:24 +0000

    Newer NetBSD uses POSIX scandir prototype, so adjust.

--- x11/efltk/patches/patch-src_core_filename_list.cpp
+++ x11/efltk/patches/patch-src_core_filename_list.cpp
@@ -1,13 +1,22 @@
-$NetBSD: patch-src_core_filename_list.cpp,v 1.2 2012/01/28 11:59:06 marino Exp $
+$NetBSD: patch-src_core_filename_list.cpp,v 1.3 2016/12/21 13:40:24 joerg Exp $
 
 --- src/core/filename_list.cpp.orig	2003-01-26 01:16:49.000000000 +0000
 +++ src/core/filename_list.cpp
-@@ -46,7 +46,7 @@ int fl_casealphasort(struct dirent **a,
+@@ -41,12 +41,16 @@
+ 						int (*compar)(dirent **, dirent **));
+ #endif
+ 
++#ifdef __NetBSD__
++#include <sys/param.h>
++#endif
++
+ int fl_alphasort(struct dirent **a, struct dirent **b) { return strcmp((*a)->d_name, (*b)->d_name); }
+ int fl_casealphasort(struct dirent **a, struct dirent **b) { return strcasecmp((*a)->d_name, (*b)->d_name); }
  
  int fl_filename_list(const char *dir, dirent ***list, Fl_File_Sort_F *sort) 
  {
 -#if defined(__hpux)
-+#if defined(__hpux) || defined(__DragonFly__)
++#if defined(__hpux) || defined(__DragonFly__) || (defined (__NetBSD__) && __NetBSD_Version__-0 >= 799005100)
      // HP-UX defines the comparison function like this:
      return scandir(dir, list, 0, (int(*)(const dirent **, const dirent **))sort);
  #elif defined(__osf__)
