b7537cba4c682f365e7ccc27b71dbbd329612469
ryoon <ryoon@pkgsrc.org>
2016-12-23 03:53:16 +0000

    Back to 2.0.7 really. Previsous 2.2.0rc3 commit is my mistake

--- graphics/darktable/patches/patch-src_develop_blend.c
+++ graphics/darktable/patches/patch-src_develop_blend.c
@@ -1,22 +1,22 @@
-$NetBSD: patch-src_develop_blend.c,v 1.4 2013/12/04 13:02:08 drochner Exp $
+$NetBSD: patch-src_develop_blend.c,v 1.5 2016/12/23 03:53:16 ryoon Exp $
 
---- src/develop/blend.c.orig	2013-06-24 18:35:28.000000000 +0000
+--- src/develop/blend.c.orig	2016-10-23 15:18:57.000000000 +0000
 +++ src/develop/blend.c
-@@ -1758,7 +1758,7 @@ void dt_develop_blend_process (struct dt
-     if(self->suppress_mask && self->dev->gui_attached && self == self->dev->gui_module && piece->pipe == self->dev->pipe && (d->blendif & (1<<31)))
+@@ -2695,7 +2695,7 @@ void dt_develop_blend_process(struct dt_
+        && (piece->pipe == self->dev->pipe) && (mask_mode & DEVELOP_MASK_BOTH))
      {
  #ifdef _OPENMP
--#if !defined(__SUNOS__)
-+#if !defined(__SUNOS__) && !defined(__NetBSD__)
-       #pragma omp parallel for default(none) shared(roi_out,mask,stderr)
+-#if !defined(__SUNOS__) && !defined(__WIN32__)
++#if !defined(__SUNOS__) && !defined(__WIN32__) && !defined(__NetBSD__)
+ #pragma omp parallel for default(none) shared(roi_out, mask, stderr)
  #else
-       #pragma omp parallel for shared(roi_out,mask)
-@@ -1773,7 +1773,7 @@ void dt_develop_blend_process (struct dt
+ #pragma omp parallel for shared(roi_out, mask)
+@@ -2710,7 +2710,7 @@ void dt_develop_blend_process(struct dt_
  
- 
+ /* now apply blending with per-pixel opacity value as defined in mask */
  #ifdef _OPENMP
--#if !defined(__SUNOS__)
-+#if !defined(__SUNOS__) && !defined(__NetBSD__)
-     #pragma omp parallel for default(none) shared(i,roi_out,o,mask,blend,stderr,ch)
+-#if !defined(__SUNOS__) && !defined(__WIN32__)
++#if !defined(__SUNOS__) && !defined(__WIN32__) && !defined(__NetBSD__)
+ #pragma omp parallel for default(none) shared(i, roi_out, o, mask, blend, stderr)
  #else
-     #pragma omp parallel for shared(i,roi_out,o,mask,blend,ch)
+ #pragma omp parallel for shared(i, roi_out, o, mask, blend)
