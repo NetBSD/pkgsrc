82f52a0568ff00cb717d41428e5d5e85a8833f5f
ryoon <ryoon@pkgsrc.org>
2016-12-23 03:34:41 +0000

    Build shared library and bump PKGREVISION

--- textproc/pugixml/buildlink3.mk
+++ textproc/pugixml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/04/29 11:54:54 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2016/12/23 03:34:41 ryoon Exp $
 
 BUILDLINK_TREE+=	pugixml
 
@@ -7,8 +7,6 @@ PUGIXML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pugixml+=	pugixml>=1.7
 BUILDLINK_PKGSRCDIR.pugixml?=	../../textproc/pugixml
-
-BUILDLINK_DEPMETHOD.pugixml?=	build
 .endif	# PUGIXML_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-pugixml
