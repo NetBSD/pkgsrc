7289a4966415bda99ab4e833456d9db486dad0f6
ryoon <ryoon@pkgsrc.org>
2016-12-23 04:11:03 +0000

    Update to 10.1.0
    Changelog:
    What's New
    
    VMware Tools is a suite of utilities that enhances the performance of the virtual machine's guest operating system and improves management of the virtual machine. Read about the new and enhanced features in this release below:
    
        vmware-namespace-cmd: Added vmware-namespace-cmd command line utility that exposes set/get commands for the namespace database in the VMX.
        gtk3 support: open-vm-tools has been updated to use gtk3 libraries.
        Common Agent Framework (CAF): CAF provides the basic services necessary to simplify secure and efficient management of agents inside virtual machines.
        xmlsec1: Changed guest authentication to xmlsec1.
        FreeBSD: Changes to support open-vm-tools on FreeBSD.
        Automatic Linux Kernel Modules: Automatic rebuilding of kernel modules is enabled by default.
        New sub-command: Added a new sub-command to push updated network information to the host on demand.
        udev-rules: Added udev rules for configuring SCSI timeout in the guest.
        Ubuntu 16.10: Fixes for running on Ubuntu 16.10.
        Quiesced Snapshot: Fix for quiesced snapshot failure leaving guest file system quiesced.
    
    Internationalization
    
    open-vm-tools 10.1.0 supports the following languages:
    
        English
        French
        German
        Spanish
        Italian
        Japanese
        Korean
        Simplified Chinese
        Traditional Chinese
    
    Compatibility
    
    open-vm-tools 10.1.0 is compatible with all supported versions of VMware vSphere, VMware Workstation 12.5 and VMware Fusion 8.5.

--- sysutils/open-vm-tools/patches/patch-libDeployPkg_processPosix.c
+++ sysutils/open-vm-tools/patches/patch-libDeployPkg_processPosix.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-libDeployPkg_processPosix.c,v 1.1 2016/10/09 03:41:56 ryoon Exp $
+$NetBSD: patch-libDeployPkg_processPosix.c,v 1.2 2016/12/23 04:11:03 ryoon Exp $
 
---- libDeployPkg/processPosix.c.orig	2016-02-16 20:06:47.000000000 +0000
+--- libDeployPkg/processPosix.c.orig	2016-09-29 10:05:27.000000000 +0000
 +++ libDeployPkg/processPosix.c
-@@ -22,6 +22,7 @@
+@@ -28,6 +28,7 @@
  #include <unistd.h>
  #include <fcntl.h>
  #include <sys/wait.h>
 +#include <signal.h>
  
- // vmware headers
  #include "util.h"
+ 
