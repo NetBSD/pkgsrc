0b66236954b64b29ba43c2e8c7c0b34a48116b44
joerg <joerg@pkgsrc.org>
2016-12-24 10:16:07 +0000

    Fix ctype.h use.

--- sysutils/collectd/patches/patch-src_utils__format__kairosdb.c
+++ sysutils/collectd/patches/patch-src_utils__format__kairosdb.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-src_utils__format__kairosdb.c,v 1.2 2016/12/07 17:28:39 fhajny Exp $
+$NetBSD: patch-src_utils__format__kairosdb.c,v 1.3 2016/12/24 10:16:07 joerg Exp $
 
---- src/utils_format_kairosdb.c.orig	2016-11-30 08:52:01.332909704 +0000
+--- src/utils_format_kairosdb.c.orig	2016-11-30 08:52:01.000000000 +0000
 +++ src/utils_format_kairosdb.c
 @@ -80,9 +80,9 @@ static int kairosdb_escape_string(char *
    /* authorize -_. and alpha num but also escapes " */
    BUFFER_ADD('"');
    for (size_t src_pos = 0; string[src_pos] != 0; src_pos++) {
 -    if (isalnum(string[src_pos]) || 0x2d == string[src_pos] ||
-+    if ((unsigned char)isalnum(string[src_pos]) || 0x2d == string[src_pos] ||
++    if (isalnum((unsigned char)string[src_pos]) || 0x2d == string[src_pos] ||
          0x2e == string[src_pos] || 0x5f == string[src_pos])
 -      BUFFER_ADD(tolower(string[src_pos]));
 +      BUFFER_ADD(tolower((unsigned char)string[src_pos]));
