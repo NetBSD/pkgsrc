17a4351e4481f98195688a91e38af4ace97d4229
joerg <joerg@pkgsrc.org>
2016-12-25 21:45:09 +0000

    Don't expect pointers to have a sign.

--- ham/linpsk/patches/patch-src_csound.cpp
+++ ham/linpsk/patches/patch-src_csound.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_csound.cpp,v 1.1 2013/03/02 18:06:22 joerg Exp $
+$NetBSD: patch-src_csound.cpp,v 1.2 2016/12/25 21:45:09 joerg Exp $
 
---- src/csound.cpp.orig	2013-02-28 22:13:24.000000000 +0000
+--- src/csound.cpp.orig	2005-02-20 10:05:46.000000000 +0000
 +++ src/csound.cpp
 @@ -81,7 +81,7 @@ return 0; 
    
@@ -11,3 +11,12 @@ $NetBSD: patch-src_csound.cpp,v 1.1 2013/03/02 18:06:22 joerg Exp $
  {
  stream=0;
  audio_buffer.maxbuf=3*BUF_SIZE; // We take 3 buffers
+@@ -92,7 +92,7 @@ output=false;
+ }
+ CSound::~CSound()
+ {
+-if ( audio_buffer.audiobuffer >0)
++if ( audio_buffer.audiobuffer )
+ 	delete audio_buffer.audiobuffer;
+ }
+ 
