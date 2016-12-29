783bb702573fa72d0cbb50a88ce5c2a4ff13d6bb
maya <maya@pkgsrc.org>
2016-12-29 22:46:30 +0000

    Remove gcc45,46,47 and libs as discussed in pkgsrc-users GCC_REQD for these versions now resolves to gcc48 due to a previous commit.
    Please file a bug report if you are having trouble with GCC 4.8.

--- lang/gcc46/hacks.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD: hacks.mk,v 1.2 2013/04/03 19:29:23 jmcneill Exp $
-
-#
-# Avoid -fkeep-inline-functions with NetBSD/arm
-#
-.if ${OPSYS} == "NetBSD" && ${MACHINE_ARCH} == "arm"
-PKG_HACKS+=		netbsd-arm-stage1-fix
-CONFIGURE_ARGS+=	--disable-bootstrap
-.endif
