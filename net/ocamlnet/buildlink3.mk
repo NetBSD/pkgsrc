6ab71103d1c87b380598beedd6a95bee13803308
jaapb <jaapb@pkgsrc.org>
2016-12-30 11:48:41 +0000

    Updated package to latest version, 4.1.2. No substantive changes except for compatibility with 0caml 4.03 (and .04).

--- net/ocamlnet/buildlink3.mk
+++ net/ocamlnet/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2016/06/20 15:36:04 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/12/30 11:48:41 jaapb Exp $
 
 BUILDLINK_TREE+=	ocamlnet
 
 .if !defined(OCAMLNET_BUILDLINK3_MK)
 OCAMLNET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocamlnet+=	ocamlnet>=4.1.1nb1
+BUILDLINK_API_DEPENDS.ocamlnet+=	ocamlnet>=4.1.2
 BUILDLINK_PKGSRCDIR.ocamlnet?=	../../net/ocamlnet
 
 .endif	# OCAMLNET_BUILDLINK3_MK
