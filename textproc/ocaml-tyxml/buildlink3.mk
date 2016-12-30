087f0eb013341c946a32e9fa662f7d1422469214
jaapb <jaapb@pkgsrc.org>
2016-12-30 11:52:28 +0000

    Updated package to latest version, 4.0.1. Changes include:
    * Fix handling of comments in the ppx.
    * Fix printing of utf8 in attributes.
    * Properly flush ppx errors. This bug was causing some blank error messages.
    * Fix handling of whitespaces in <select> in the ppx.

--- textproc/ocaml-tyxml/buildlink3.mk
+++ textproc/ocaml-tyxml/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2016/06/20 15:20:03 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/12/30 11:52:28 jaapb Exp $
 
 BUILDLINK_TREE+=        ocaml-tyxml
 
 .if !defined(OCAML_TYXML_BUILDLINK3_MK)
 OCAML_TYXML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-tyxml+=     ocaml-tyxml>=4.0.0nb1
+BUILDLINK_API_DEPENDS.ocaml-tyxml+=     ocaml-tyxml>=4.0.1
 BUILDLINK_PKGSRCDIR.ocaml-tyxml?=       ../../textproc/ocaml-tyxml
 
 .endif  # OCAML_TYXML_BUILDLINK3_MK
