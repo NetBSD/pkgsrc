6f8e4f63b3fb80a9232df3dd84797d3dd8dcffc8
jaapb <jaapb@pkgsrc.org>
2016-12-30 11:16:56 +0000

    Recursive revbump associated with ocaml update to 4.04.

--- devel/ocaml-cmdliner/buildlink3.mk
+++ devel/ocaml-cmdliner/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2016/06/20 10:43:18 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2016/12/30 11:16:56 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cmdliner
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-cmdliner
 OCAML_CMDLINER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-cmdliner+=	ocaml-cmdliner>=0.9.8
-BUILDLINK_ABI_DEPENDS.ocaml-cmdliner?=	ocaml-cmdliner>=0.9.4nb4
+BUILDLINK_ABI_DEPENDS.ocaml-cmdliner?=	ocaml-cmdliner>=0.9.8nb2
 BUILDLINK_PKGSRCDIR.ocaml-cmdliner?=	../../devel/ocaml-cmdliner
 
 .include "../../devel/ocaml-findlib/buildlink3.mk"
