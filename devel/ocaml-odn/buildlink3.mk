fd00915dbef6086b629950debdf02be9422dfead
jaapb <jaapb@pkgsrc.org>
2016-12-30 11:16:56 +0000

    Recursive revbump associated with ocaml update to 4.04.

--- devel/ocaml-odn/buildlink3.mk
+++ devel/ocaml-odn/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2016/05/05 15:32:17 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2016/12/30 11:16:57 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-odn
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-odn
 OCAML_ODN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-odn+=	ocaml-odn>=0.0.11
-BUILDLINK_ABI_DEPENDS.ocaml-odn?=	ocaml-odn>=0.0.11nb5
+BUILDLINK_ABI_DEPENDS.ocaml-odn?=	ocaml-odn>=0.0.11nb7
 BUILDLINK_PKGSRCDIR.ocaml-odn?=	../../devel/ocaml-odn
 
 .include "../../lang/ocaml/buildlink3.mk"
--- x11/ocaml-graphics/buildlink3.mk
+++ x11/ocaml-graphics/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2016/05/05 11:45:37 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2016/12/30 11:16:57 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-graphics
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-graphics
 OCAML_GRAPHICS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-graphics+=	ocaml-graphics>=3.08.2
-BUILDLINK_ABI_DEPENDS.ocaml-graphics+=	ocaml-graphics>=4.03.0nb5
+BUILDLINK_ABI_DEPENDS.ocaml-graphics+=	ocaml-graphics>=4.04.0nb6
 BUILDLINK_PKGSRCDIR.ocaml-graphics?=	../../x11/ocaml-graphics
 
 .include "../../lang/ocaml/buildlink3.mk"
