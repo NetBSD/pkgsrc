5b473c5eca3a2d157ca9d835ffa950d1ad955711
jaapb <jaapb@pkgsrc.org>
2016-12-30 11:33:50 +0000

    Updated package to latest version, 0.2.1. Changes include:
      * Add ?eq parameter to S.signal

--- devel/ocaml-reactiveData/buildlink3.mk
+++ devel/ocaml-reactiveData/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/02/06 11:23:41 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2016/12/30 11:33:50 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-reactiveData
 
 .if !defined(OCAML_REACTIVEDATA_BUILDLINK3_MK)
 OCAML_REACTIVEDATA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-reactiveData+=	ocaml-reactiveData>=0.2
+BUILDLINK_API_DEPENDS.ocaml-reactiveData+=	ocaml-reactiveData>=0.2.1
 BUILDLINK_PKGSRCDIR.ocaml-reactiveData?=	../../devel/ocaml-reactiveData
 
 .include "../../devel/ocaml-react/buildlink3.mk"
