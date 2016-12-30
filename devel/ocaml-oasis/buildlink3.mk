f6996028a277e62569f487cbf9beb965d8a3958c
jaapb <jaapb@pkgsrc.org>
2016-12-30 11:16:56 +0000

    Recursive revbump associated with ocaml update to 4.04.

--- devel/ocaml-oasis/buildlink3.mk
+++ devel/ocaml-oasis/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2016/05/05 16:06:51 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2016/12/30 11:16:57 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-oasis
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-oasis
 OCAML_OASIS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-oasis+=	ocaml-oasis>=0.4.4
-BUILDLINK_ABI_DEPENDS.ocaml-oasis?=	ocaml-oasis>=0.4.5nb1
+BUILDLINK_ABI_DEPENDS.ocaml-oasis?=	ocaml-oasis>=0.4.6nb2
 BUILDLINK_PKGSRCDIR.ocaml-oasis?=	../../devel/ocaml-oasis
 
 .include "../../lang/ocaml/buildlink3.mk"
