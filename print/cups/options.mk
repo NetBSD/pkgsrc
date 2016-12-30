e11e831cc61b1fb2c56118af5d647333fe07b884
adam <adam>
2016-12-30 07:08:34 +0000

    Added avahi as an option so ippfind can be build; unified PLIST and cleanup for Darwin

--- print/cups/options.mk
+++ print/cups/options.mk
@@ -1,13 +1,23 @@
-# $NetBSD: options.mk,v 1.18 2014/08/23 20:02:11 schnoebe Exp $
+# $NetBSD: options.mk,v 1.19 2016/12/30 07:08:34 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.cups
-PKG_SUPPORTED_OPTIONS=	acl dbus dnssd kerberos pam tcpwrappers
-PKG_SUGGESTED_OPTIONS=	dbus dnssd kerberos
+PKG_SUPPORTED_OPTIONS=	acl dnssd kerberos pam tcpwrappers
+PKG_SUGGESTED_OPTIONS=	dnssd kerberos
 
+PLIST_VARS+=		apple dbus dnssd ippfind libusb pam
+
+.if ${OPSYS} == "Darwin"
+PLIST.apple=		yes
+PLIST.ippfind=		yes
+.else
+# CUPS on Darwin does not support DBus and libusb
+PKG_SUPPORTED_OPTIONS+=	avahi dbus
+PKG_SUGGESTED_OPTIONS+=	dbus
 # Neither DragonFly nor SunOS can build libusb1
-.if ${OPSYS} != "DragonFly" && ${OPSYS} != "SunOS"
-PKG_SUPPORTED_OPTIONS+= libusb
-PKG_SUGGESTED_OPTIONS+= libusb
+.  if ${OPSYS} != "DragonFly" && ${OPSYS} != "SunOS"
+PKG_SUPPORTED_OPTIONS+=	libusb
+PKG_SUGGESTED_OPTIONS+=	libusb
+.  endif
 .endif
 
 .include "../../mk/bsd.options.mk"
@@ -20,9 +30,15 @@ CONFIGURE_ARGS+=	--enable-acl
 CONFIGURE_ARGS+=	--disable-acl
 .endif
 
-PLIST_VARS+=		dbus
+.if !empty(PKG_OPTIONS:Mavahi)
+.include "../../net/avahi/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-avahi
+PLIST.ippfind=		yes
+.else
+CONFIGURE_ARGS+=	--disable-avahi
+.endif
+
 .if !empty(PKG_OPTIONS:Mdbus)
-USE_TOOLS+=		pkg-config
 .  include "../../sysutils/dbus/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-dbus
 PLIST.dbus=		yes
@@ -30,7 +46,6 @@ PLIST.dbus=		yes
 CONFIGURE_ARGS+=	--disable-dbus
 .endif
 
-PLIST_VARS+=		dnssd
 .if !empty(PKG_OPTIONS:Mdnssd)
 .include "../../net/mDNSResponder/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-dnssd
@@ -46,18 +61,15 @@ CONFIGURE_ARGS+=	--enable-gssapi
 CONFIGURE_ARGS+=	--disable-gssapi
 .endif
 
-PLIST_VARS+=		libusb
 .if !empty(PKG_OPTIONS:Mlibusb)
 .include "../../devel/libusb1/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-libusb
 MESSAGE_SRC+=		${PKGDIR}/MESSAGE.libusb
-USE_TOOLS+=		pkg-config
 PLIST.libusb=		yes
 .else
 CONFIGURE_ARGS+=	--disable-libusb
 .endif
 
-PLIST_VARS+=		pam
 .if !empty(PKG_OPTIONS:Mpam)
 .  include "../../mk/pam.buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-pam
