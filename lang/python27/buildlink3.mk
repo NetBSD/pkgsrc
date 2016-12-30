e97f7ce5d6160184ef060f8f62992c7ea1fed14a
adam <adam@pkgsrc.org>
2016-12-30 10:53:21 +0000

    Changes 2.7.13: Core and Builtins ----------------- - Issue 28847: dumbdbm no longer writes the index file in when it is not   changed and supports reading read-only files. - Issue 11145: Fixed miscellaneous issues with C-style formatting of types   with custom __oct__ and __hex__. - Issue 24469: Fixed memory leak caused by int subclasses without overridden   tp_free (e.g. C-inherited Cython classes). - Issue 19398: Extra slash no longer added to sys.path components in case of   empty compile-time PYTHONPATH components. - Issue 21720: Improve exception message when the type of fromlist is unicode.   fromlist parameter of __import__() only accepts str in Python 2 and this   will help to identify the problem especially when the unicode_literals   future import is used. - Issue 26906: Resolving special methods of uninitialized type now causes   implicit initialization of the type instead of a fail. - Issue 18287: PyType_Ready() now checks that tp_name is not NULL.   Original patch by Niklas Koep. - Issue 24098: Fixed possible crash when AST is changed in process of   compiling it. - Issue 28350: String constants with null character no longer interned. - Issue 27942: String constants now interned recursively in tuples and frozensets. - Issue 15578: Correctly incref the parent module while importing. - Issue 26307: The profile-opt build now applies PGO to the built-in modules. - Issue 26020: set literal evaluation order did not match documented behaviour. - Issue 27870: A left shift of zero by a large integer no longer attempts   to allocate large amounts of memory. - Issue 25604: Fix a minor bug in integer true division; this bug could   potentially have caused off-by-one-ulp results on platforms with   unreliable ldexp implementations. - Issue 27473: Fixed possible integer overflow in str, unicode and bytearray   concatenations and repetitions.  Based on patch by Xiang Zhang. - Issue 27507: Add integer overflow check in bytearray.extend().  Patch by   Xiang Zhang. - Issue 27581: Don't rely on wrapping for overflow check in   PySequence_Tuple().  Patch by Xiang Zhang. - Issue 23908: os functions, open() and the io.FileIO constructor now reject   unicode paths with embedded null character on Windows instead of silently   truncating them. - Issue 27514: Make having too many statically nested blocks a SyntaxError   instead of SystemError.

--- lang/python27/buildlink3.mk
+++ lang/python27/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2012/05/07 01:53:42 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2016/12/30 10:53:21 adam Exp $
 
 BUILDLINK_TREE+=	python27
 
@@ -6,16 +6,12 @@ BUILDLINK_TREE+=	python27
 PYTHON27_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.python27+=	python27>=2.7
-BUILDLINK_ABI_DEPENDS.python27+=		python27>=2.7.1nb2
+BUILDLINK_ABI_DEPENDS.python27+=	python27>=2.7.1nb2
 BUILDLINK_PKGSRCDIR.python27?=		../../lang/python27
 
 .if defined(BUILDLINK_DEPMETHOD.python)
 BUILDLINK_DEPMETHOD.python27?=	${BUILDLINK_DEPMETHOD.python}
 .endif
-
-#BUILDLINK_INCDIRS.python27+=	include/python2.7
-#BUILDLINK_LIBDIRS.python27+=	lib/python2.7/config
-#BUILDLINK_TRANSFORM+=		l:python:python2.7
 
 .include "../../mk/dlopen.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
