4f39eebf896504e81e9e2214c724fb16461058f6
wiz <wiz@pkgsrc.org>
2016-12-30 13:33:44 +0000

    Fix options framework abuse by sqsh*.

--- databases/sqsh/options.mk
+++ databases/sqsh/options.mk
@@ -1,38 +1,9 @@
-# $NetBSD: options.mk,v 1.3 2013/01/19 20:27:30 riastradh Exp $
+# $NetBSD: options.mk,v 1.4 2016/12/30 13:33:45 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.sqsh
-PKG_SUPPORTED_OPTIONS+=		sybase-openclient
-PKG_OPTIONS_OPTIONAL_GROUPS+=	x11
-PKG_OPTIONS_GROUP.x11+=		sqsh-motif sqsh-xaw
-PKG_OPTIONS_LEGACY_VARS+=	USE_SYBASE_OPENCLIENT:sybase-openclient
+PKG_SUPPORTED_OPTIONS=		sybase-openclient
 
 .include "../../mk/bsd.options.mk"
-
-###
-### Motif GUI support.
-###
-.if !empty(PKG_OPTIONS:Msqsh-motif)
-.  include "../../mk/motif.buildlink3.mk"
-CONFIGURE_ARGS+=	--with-motif=${MOTIFBASE:Q}
-.endif
-
-###
-### Athena GUI support.
-###
-.if !empty(PKG_OPTIONS:Msqsh-xaw)
-.  include "../../mk/xaw.buildlink3.mk"
-CONFIGURE_ARGS+=	--with-x
-.endif
-
-###
-### Common dependencies for X GUI support.
-###
-.if !empty(PKG_OPTIONS:Msqsh-motif) || !empty(PKG_OPTIONS:Msqsh-xaw)
-.  include "../../x11/libX11/buildlink3.mk"
-.  include "../../x11/libXext/buildlink3.mk"
-.  include "../../x11/libXmu/buildlink3.mk"
-.  include "../../x11/libXt/buildlink3.mk"
-.endif
 
 ###
 ### Support for the native Sybase Open Client.
