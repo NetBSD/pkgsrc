4ad6a01448947f37b47575049ab1e280c686425e
ryoon <ryoon@pkgsrc.org>
2017-01-01 16:14:07 +0000

    Update to 2.46
    Changelog:
    What's New in SeaMonkey 2.46
    
    SeaMonkey 2.46 contains (among other changes) the following major changes relative to SeaMonkey 2.40:
    SeaMonkey-specific changes
    
        HTML5 fullscreen video (e.g. on YouTube) now works fine.

--- www/seamonkey/patches/patch-mozilla_xpcom_components_Module.h
+++ www/seamonkey/patches/patch-mozilla_xpcom_components_Module.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-mozilla_xpcom_components_Module.h,v 1.3 2016/04/14 21:09:07 dholland Exp $
+$NetBSD: patch-mozilla_xpcom_components_Module.h,v 1.4 2017/01/01 16:14:08 ryoon Exp $
 
 * Workaround for binutils/GNU ld 2.26 from NetBSD/amd64 7.99.26
 
---- mozilla/xpcom/components/Module.h.orig	2016-01-23 23:23:51.000000000 +0000
+--- mozilla/xpcom/components/Module.h.orig	2016-12-14 02:10:01.000000000 +0000
 +++ mozilla/xpcom/components/Module.h
 @@ -125,7 +125,7 @@ struct Module
  #    define NSMODULE_SECTION __declspec(allocate(".kPStaticModules$M"), dllexport)
  #  elif defined(__GNUC__)
  #    if defined(__ELF__)
 -#      define NSMODULE_SECTION __attribute__((section(".kPStaticModules"), visibility("protected")))
-+#      define NSMODULE_SECTION __attribute__((section(".kPStaticModules"), visibility("default")))
++#      define NSMODULE_SECTION __attribute__((section(".kPStaticModules"), visibility("hidden")))
  #    elif defined(__MACH__)
  #      define NSMODULE_SECTION __attribute__((section("__DATA, .kPStaticModules"), visibility("default")))
  #    elif defined (_WIN32)
