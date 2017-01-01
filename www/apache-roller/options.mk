50b523ce791c6109ebd3018bbf596f6b1d09e17d
spz <spz@pkgsrc.org>
2017-01-01 18:19:58 +0000

    use jdbc-mysql51 if available

--- www/apache-roller/options.mk
+++ www/apache-roller/options.mk
@@ -1,12 +1,12 @@
-# $NetBSD: options.mk,v 1.1.1.1 2009/02/12 00:26:59 adrianp Exp $
+# $NetBSD: options.mk,v 1.2 2017/01/01 18:19:58 spz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.apache-roller
 
 PKG_OPTIONS_OPTIONAL_GROUPS=	jdbc
-PKG_OPTIONS_GROUP.jdbc=		jdbc-mysql5 jdbc-mysql31
+PKG_OPTIONS_GROUP.jdbc=		jdbc-mysql51 jdbc-mysql5 jdbc-mysql31
 
-PKG_SUPPORTED_OPTIONS=	jdbc-mysql31 jdbc-mysql5
-PKG_SUGGESTED_OPTIONS=	jdbc-mysql31
+PKG_SUPPORTED_OPTIONS=	jdbc-mysql31 jdbc-mysql5 jdbc-mysql51
+PKG_SUGGESTED_OPTIONS=	jdbc-mysql51
 
 .include "../../mk/bsd.options.mk"
 
@@ -19,4 +19,8 @@ DEPENDS+=	jdbc-mysql>=3:../../databases/jdbc-mysql31
 
 .if !empty(PKG_OPTIONS:Mjdbc-mysql5)
 DEPENDS+=	jdbc-mysql>=5:../../databases/jdbc-mysql5
+.endif
+
+.if !empty(PKG_OPTIONS:Mjdbc-mysql51)
+DEPENDS+=	jdbc-mysql>=5.1:../../databases/jdbc-mysql51
 .endif
