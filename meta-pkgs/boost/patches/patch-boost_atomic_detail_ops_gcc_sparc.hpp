115ac23a4cad267e85ace7809dc4ea941079cd46
adam <adam@pkgsrc.org>
2017-01-01 15:32:47 +0000

    Changes 1.63.0:
    Atomic:
    * Added the static constant atomic<T>::is_always_lock_free for conformance with C++17. The constant indicates that the given specialization always provides lock-free implementation of atomic operations.
    * Improved support of Oracle Studio on x86 targets.
    
    Container:
    * Fixed bugs
    
    Context:
    * 11687 Xcode (Apple clang) and thread_local
    * 12114 context does not preserve fpu context on ios arm64 devices (fixed for other architecturs too)
    * 12411 fpu registers not preserved with O2 optimizations in Visual Studio (fixed for other architecturs too)
    * 12551 building boost on arm64 causes error: unknown directive
    * reduced signatur for ontop-functions
    * unit-test for private fcontext-API
    * performance-test for ucontext and fcontext fixed
    
    Fiber:
    * unbounded_channel and bounded_channel marked as deprecated
    * new classes buffered_channel and unbuffered_channel
    * performance improved
    
    Fusion:
    * Fixed fusion::vector buggy constructors (Thanks to Lee Clagett).
    * BOOST_FUSION_ADAPT* and BOOST_FUSION_DEFINE* now allow empty structures. (6592)
    * BOOST_FUSION_DEFINE* allows move constructor and assignment. (10099)
    * Adaptor for std::array (Thanks to Mateusz Łoskot). (8241)
    * Fixed returning temporary variable warnings on MSVC.
    
    Geometry:
    * Intersection: take care that output is valid (work in progres, not yet finished - but more is valid)
    * 12289 Avoid warning/compilation error for std::cout
    * 12412 Invalid result of covered_by() with geographic Points and Boxes
    * 12413 rtree::remove() not working properly with geographic Points
    
    Hash:
    * Fixed some warnings.
    
    Interprocess:
    * Fixed bugs
    
    Intrusive:
    * Fixed bugs
    
    Lexical Cast:
    * Fixed issue found by -fsanitize=undefined 21
    
    Log:
    * Removed the previously deprecated type_info_wrapper.
    * Added a way to access the current file name from the text file sink backend. The file name can be obtained with the get_current_file_name method.
    * Bug fixes
    
    Metaparse:
    * Improved the implementation of BOOST_METAPARSE_STRING
    
    Move:
    * Improved experimental adaptive sort algorithm performance
    * Activate BOOST_MOVE_FORCEINLINE
    
    Optional:
    * Added two new in-place constructors. They work similarly to emplace() functions: they initialize the contained value by perfect-forwarding the obtained arguments. One constructor always initializes the contained value, the other based on a boolean condition.
    * Syntax o = {} now correctly un-initializes an optional, just like in std::optional.
    * Fixed optional<optional<T>&> initialization. (12203)
    * Optional constructors are sfinae-friendly. This fixes interaction with std::tuple. (12563)
    
    Phoenix:
    * Most of modules now support C++11 variadic templates.
    * Fixed type deduction failure with result type.
    * Suppress unused warnings.
    * Fixed too few arguments error on binary math functions.
    * Added workarounds for older compiler.
    
    Python:
    * Added (basic) support for C++11 (std::shared_ptr, std::unique_ptr)
    * Incorporated an extension API to wrap NumPy
    * Removed obsolete bits and pieces
    
    Test:
    * Boost.test v3.4 see the Change log section for more details.
    * New feature: per_element and lexicographic modifiers of BOOST_TEST can also be applied to string comparison
    * Breaking change: colons that appear in test case names are replaced with underscores
    * Bug fixes
    
    TypeIndex:
    * Added runtime_cast to the library as an emulation of dynamic_cast. Thanks to Chris Glover for the implementation. See runtime_cast reference for more info.
    * Internals of the CTTI were made more platform independant, due to change of the std::size_t type to unsigned int.
    
    Units:
    * Fix inspection warnings
    * updated physico-chemical constants to CODATA 2014 values
    * define biot correctly as 10 Ampere
    
    Unordered:
    * Fix an exception safety issue in assignment.
    * Use std::allocator_traits where available, and support modern style rebind (12459).
    * Add some allocator aware constructors.
    * Use the hint iterator in emplace_hint.
    * Fixed some warnings.

--- meta-pkgs/boost/patches/patch-boost_atomic_detail_ops_gcc_sparc.hpp
+++ meta-pkgs/boost/patches/patch-boost_atomic_detail_ops_gcc_sparc.hpp
@@ -1,13 +1,13 @@
-$NetBSD: patch-boost_atomic_detail_ops_gcc_sparc.hpp,v 1.1 2015/08/09 07:59:05 mrg Exp $
+$NetBSD: patch-boost_atomic_detail_ops_gcc_sparc.hpp,v 1.2 2017/01/01 15:32:47 adam Exp $
 
 provide a fence_before_store() for sparc, which fixes the build.
 
---- boost/atomic/detail/ops_gcc_sparc.hpp.orig	2015-08-09 05:20:41.000000000 +1000
-+++ boost/atomic/detail/ops_gcc_sparc.hpp	2015-08-08 17:54:11.000000000 +1000
-@@ -34,6 +34,12 @@
- 
- struct gcc_sparc_cas_base
+--- boost/atomic/detail/ops_gcc_sparc.hpp.orig	2017-01-01 02:18:57.000000000 +0000
++++ boost/atomic/detail/ops_gcc_sparc.hpp
+@@ -36,6 +36,12 @@ struct gcc_sparc_cas_base
  {
+     static BOOST_CONSTEXPR_OR_CONST bool is_always_lock_free = true;
+ 
 +    static BOOST_FORCEINLINE void fence_before_store(memory_order order) BOOST_NOEXCEPT
 +    {
 +        if ((order & memory_order_release) != 0)
@@ -17,7 +17,7 @@ provide a fence_before_store() for sparc, which fixes the build.
      static BOOST_FORCEINLINE void fence_before(memory_order order) BOOST_NOEXCEPT
      {
          if (order == memory_order_seq_cst)
-@@ -55,6 +61,11 @@
+@@ -57,6 +63,11 @@ struct gcc_sparc_cas_base
          if (order == memory_order_seq_cst)
              __asm__ __volatile__ ("membar #Sync" ::: "memory");
      }
