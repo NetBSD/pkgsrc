a102068534b74e77a089766be277bad38608913d
ryoon <ryoon@pkgsrc.org>
2017-01-01 16:14:07 +0000

    Update to 2.46
    Changelog:
    What's New in SeaMonkey 2.46
    
    SeaMonkey 2.46 contains (among other changes) the following major changes relative to SeaMonkey 2.40:
    SeaMonkey-specific changes
    
        HTML5 fullscreen video (e.g. on YouTube) now works fine.

--- www/seamonkey/patches/patch-mozilla_ipc_chromium_src_base_sys__info__posix.cc
+++ www/seamonkey/patches/patch-mozilla_ipc_chromium_src_base_sys__info__posix.cc
@@ -1,23 +1,11 @@
-$NetBSD: patch-mozilla_ipc_chromium_src_base_sys__info__posix.cc,v 1.2 2014/06/22 08:54:39 ryoon Exp $
+$NetBSD: patch-mozilla_ipc_chromium_src_base_sys__info__posix.cc,v 1.3 2017/01/01 16:14:08 ryoon Exp $
 
---- mozilla/ipc/chromium/src/base/sys_info_posix.cc.orig	2014-06-13 00:45:30.000000000 +0000
+--- mozilla/ipc/chromium/src/base/sys_info_posix.cc.orig	2016-12-14 02:07:49.000000000 +0000
 +++ mozilla/ipc/chromium/src/base/sys_info_posix.cc
-@@ -119,7 +119,11 @@ std::wstring SysInfo::GetEnvVar(const wc
+@@ -121,7 +121,11 @@ std::wstring SysInfo::GetEnvVar(const wc
  
  // static
  std::string SysInfo::OperatingSystemName() {
-+#ifdef OS_SOLARIS
-+  struct utsname info;
-+#else
-   utsname info;
-+#endif
-   if (uname(&info) < 0) {
-     NOTREACHED();
-     return "";
-@@ -129,7 +133,11 @@ std::string SysInfo::OperatingSystemName
- 
- // static
- std::string SysInfo::OperatingSystemVersion() {
 +#ifdef OS_SOLARIS
 +  struct utsname info;
 +#else
