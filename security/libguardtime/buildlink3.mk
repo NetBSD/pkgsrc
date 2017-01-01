e0aee5b9970ffeacf210584563df8eece4cff149
adam <adam@pkgsrc.org>
2017-01-01 16:05:55 +0000

    Revbump after boost update

--- security/libguardtime/buildlink3.mk
+++ security/libguardtime/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2016/10/07 18:25:33 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/01/01 16:06:00 adam Exp $
 
 BUILDLINK_TREE+=	libguardtime
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libguardtime
 LIBGUARDTIME_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libguardtime+=	libguardtime>=0.3.11
-BUILDLINK_ABI_DEPENDS.libguardtime+=	libguardtime>=0.3.13nb2
+BUILDLINK_ABI_DEPENDS.libguardtime+=	libguardtime>=0.3.13nb3
 BUILDLINK_PKGSRCDIR.libguardtime?=	../../security/libguardtime
 
 .include "../../security/openssl/buildlink3.mk"
--- www/curl/buildlink3.mk
+++ www/curl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.33 2016/10/07 18:25:35 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2017/01/01 16:06:01 adam Exp $
 
 BUILDLINK_TREE+=	curl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	curl
 CURL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.curl+=	curl>=7.12.3
-BUILDLINK_ABI_DEPENDS.curl+=	curl>=7.50.3nb1
+BUILDLINK_ABI_DEPENDS.curl+=	curl>=7.52.1nb1
 BUILDLINK_PKGSRCDIR.curl?=	../../www/curl
 
 pkgbase:= curl
