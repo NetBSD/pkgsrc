acdd9252bf47edd64e43218be5bdb2dec86a1b3e
wiz <wiz@pkgsrc.org>
2017-01-01 14:43:22 +0000

    Add python-3.6 to incompatible versions.

--- chat/gajim/options.mk
+++ chat/gajim/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2016/07/09 13:03:32 wiz Exp $
+# $NetBSD: options.mk,v 1.8 2017/01/01 14:43:25 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gajim
 PKG_SUPPORTED_OPTIONS=	avahi gnome gnome-keyring ssl
@@ -20,7 +20,7 @@ DEPENDS+=	avahi-[0-9]*:../../net/avahi
 # gnome
 .if !empty(PKG_OPTIONS:Mgnome)
 DEPENDS+=	${PYPKGPREFIX}-gnome2-[0-9]*:../../x11/py-gnome2
-PYTHON_VERSIONS_INCOMPATIBLE=	34 35 # py-ORBit via py-gnome2
+PYTHON_VERSIONS_INCOMPATIBLE=	34 35 36 # py-ORBit via py-gnome2
 .endif
 
 # gnome-keyring
--- net/Radicale/options.mk
+++ net/Radicale/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2016/07/09 13:03:58 wiz Exp $
+# $NetBSD: options.mk,v 1.6 2017/01/01 14:43:49 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.Radicale
 PKG_SUPPORTED_OPTIONS=	git ldap
@@ -10,6 +10,6 @@ DEPENDS+=	${PYPKGPREFIX}-dulwich-[0-9]*:../../devel/py-dulwich
 .endif
 
 .if !empty(PKG_OPTIONS:Mldap)
-PYTHON_VERSIONS_INCOMPATIBLE=	34 35 # py-ldap
+PYTHON_VERSIONS_INCOMPATIBLE=	34 35 36 # py-ldap
 DEPENDS+=	${PYPKGPREFIX}-ldap-[0-9]*:../../databases/py-ldap
 .endif
