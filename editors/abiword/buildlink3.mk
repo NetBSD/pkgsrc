76e13ebd91e95f83a966d84a13ceb0c7383243c6
adam <adam@pkgsrc.org>
2017-01-01 16:05:55 +0000

    Revbump after boost update

--- editors/abiword/buildlink3.mk
+++ editors/abiword/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2016/10/07 18:25:31 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/01/01 16:05:57 adam Exp $
 
 BUILDLINK_TREE+=	abiword
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	abiword
 ABIWORD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.abiword+=	abiword>=3.0.1
-BUILDLINK_ABI_DEPENDS.abiword?=	abiword>=3.0.1nb4
+BUILDLINK_ABI_DEPENDS.abiword?=	abiword>=3.0.1nb5
 BUILDLINK_PKGSRCDIR.abiword?=	../../editors/abiword
 
 .include "../../converters/fribidi/buildlink3.mk"
