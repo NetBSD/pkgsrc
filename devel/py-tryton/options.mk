d9cd52ca4f7d2f437462e137076da25e25647437
wiz <wiz@pkgsrc.org>
2017-01-01 14:43:22 +0000

    Add python-3.6 to incompatible versions.

--- devel/py-tryton/options.mk
+++ devel/py-tryton/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2016/07/09 13:03:44 wiz Exp $
+# $NetBSD: options.mk,v 1.8 2017/01/01 14:43:35 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-tryton
 PKG_SUPPORTED_OPTIONS=	cdecimal goocalendar pytz simplejson
@@ -7,12 +7,12 @@ PKG_SUGGESTED_OPTIONS+=	cdecimal pytz simplejson
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mcdecimal)
-PYTHON_VERSIONS_INCOMPATIBLE+=	34 35 # py-cdecimal not available because obsolete
+PYTHON_VERSIONS_INCOMPATIBLE+=	34 35 36 # py-cdecimal not available because obsolete
 DEPENDS+=		${PYPKGPREFIX}-cdecimal-[0-9]*:../../math/py-cdecimal
 .endif
 
 .if !empty(PKG_OPTIONS:Mgoocalendar)
-PYTHON_VERSIONS_INCOMPATIBLE+=	34 35 # py-gtk2 via py-goocanvas via py-goocalendar
+PYTHON_VERSIONS_INCOMPATIBLE+=	34 35 36 # py-gtk2 via py-goocanvas via py-goocalendar
 DEPENDS+=		${PYPKGPREFIX}-goocalendar-[0-9]*:../../time/py-goocalendar
 .endif
 
