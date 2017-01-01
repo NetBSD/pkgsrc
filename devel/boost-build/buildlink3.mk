07c42ebd5e2b60b654d257e0826eb6d2d6383da6
adam <adam@pkgsrc.org>
2017-01-01 15:32:47 +0000

    Changes 1.63.0:
    Atomic:
    * Added the static constant atomic<T>::is_always_lock_free for conformance with C++17. The constant indicates that the given specialization always provides lock-free implementation of atomic operations.
    * Improved support of Oracle Studio on x86 targets.
    
    Container:
    * Fixed bugs
    
    Context:
    * 11687 Xcode (Apple clang) and thread_local
    * 12114 context does not preserve fpu context on ios arm64 devices (fixed for other architecturs too)
    * 12411 fpu registers not preserved with O2 optimizations in Visual Studio (fixed for other architecturs too)
    * 12551 building boost on arm64 causes error: unknown directive
    * reduced signatur for ontop-functions
    * unit-test for private fcontext-API
    * performance-test for ucontext and fcontext fixed
    
    Fiber:
    * unbounded_channel and bounded_channel marked as deprecated
    * new classes buffered_channel and unbuffered_channel
    * performance improved
    
    Fusion:
    * Fixed fusion::vector buggy constructors (Thanks to Lee Clagett).
    * BOOST_FUSION_ADAPT* and BOOST_FUSION_DEFINE* now allow empty structures. (6592)
    * BOOST_FUSION_DEFINE* allows move constructor and assignment. (10099)
    * Adaptor for std::array (Thanks to Mateusz Łoskot). (8241)
    * Fixed returning temporary variable warnings on MSVC.
    
    Geometry:
    * Intersection: take care that output is valid (work in progres, not yet finished - but more is valid)
    * 12289 Avoid warning/compilation error for std::cout
    * 12412 Invalid result of covered_by() with geographic Points and Boxes
    * 12413 rtree::remove() not working properly with geographic Points
    
    Hash:
    * Fixed some warnings.
    
    Interprocess:
    * Fixed bugs
    
    Intrusive:
    * Fixed bugs
    
    Lexical Cast:
    * Fixed issue found by -fsanitize=undefined 21
    
    Log:
    * Removed the previously deprecated type_info_wrapper.
    * Added a way to access the current file name from the text file sink backend. The file name can be obtained with the get_current_file_name method.
    * Bug fixes
    
    Metaparse:
    * Improved the implementation of BOOST_METAPARSE_STRING
    
    Move:
    * Improved experimental adaptive sort algorithm performance
    * Activate BOOST_MOVE_FORCEINLINE
    
    Optional:
    * Added two new in-place constructors. They work similarly to emplace() functions: they initialize the contained value by perfect-forwarding the obtained arguments. One constructor always initializes the contained value, the other based on a boolean condition.
    * Syntax o = {} now correctly un-initializes an optional, just like in std::optional.
    * Fixed optional<optional<T>&> initialization. (12203)
    * Optional constructors are sfinae-friendly. This fixes interaction with std::tuple. (12563)
    
    Phoenix:
    * Most of modules now support C++11 variadic templates.
    * Fixed type deduction failure with result type.
    * Suppress unused warnings.
    * Fixed too few arguments error on binary math functions.
    * Added workarounds for older compiler.
    
    Python:
    * Added (basic) support for C++11 (std::shared_ptr, std::unique_ptr)
    * Incorporated an extension API to wrap NumPy
    * Removed obsolete bits and pieces
    
    Test:
    * Boost.test v3.4 see the Change log section for more details.
    * New feature: per_element and lexicographic modifiers of BOOST_TEST can also be applied to string comparison
    * Breaking change: colons that appear in test case names are replaced with underscores
    * Bug fixes
    
    TypeIndex:
    * Added runtime_cast to the library as an emulation of dynamic_cast. Thanks to Chris Glover for the implementation. See runtime_cast reference for more info.
    * Internals of the CTTI were made more platform independant, due to change of the std::size_t type to unsigned int.
    
    Units:
    * Fix inspection warnings
    * updated physico-chemical constants to CODATA 2014 values
    * define biot correctly as 10 Ampere
    
    Unordered:
    * Fix an exception safety issue in assignment.
    * Use std::allocator_traits where available, and support modern style rebind (12459).
    * Add some allocator aware constructors.
    * Use the hint iterator in emplace_hint.
    * Fixed some warnings.

--- devel/boost-build/buildlink3.mk
+++ devel/boost-build/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.33 2016/10/07 17:51:11 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2017/01/01 15:32:47 adam Exp $
 
 BUILDLINK_TREE+=	boost-build
 
 .if !defined(BOOST_BUILD_BUILDLINK3_MK)
 BOOST_BUILD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.62.*
+BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.63.*
 BUILDLINK_DEPMETHOD.boost-build?=	build
 BUILDLINK_PKGSRCDIR.boost-build?=	../../devel/boost-build
 .endif # BOOST_BUILD_BUILDLINK3_MK
