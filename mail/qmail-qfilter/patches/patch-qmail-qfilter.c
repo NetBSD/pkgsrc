c8d9c6fa32f6fb96ddeabe909468f546b7d21b5a
schmonz <schmonz@pkgsrc.org>
2017-01-02 02:19:26 +0000

    Adapt to bglibs 2. Bump PKGREVISION.

--- mail/qmail-qfilter/patches/patch-qmail-qfilter.c
+++ mail/qmail-qfilter/patches/patch-qmail-qfilter.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-qmail-qfilter.c,v 1.1 2014/11/17 02:56:04 schmonz Exp $
+$NetBSD: patch-qmail-qfilter.c,v 1.2 2017/01/02 02:19:26 schmonz Exp $
 
 Apply patch (submitted upstream) to set QMAILPPID for filters to
 know grandparent PID.
@@ -7,8 +7,19 @@ With this change, my homegrown badrcptto-alike filter can log
 rejections in the same format as Paul Jarc's realrcptto, thereby
 allowing me to correlate each rejection with a qmail-smtpd session.
 
+Also adapt to bglibs 2.
+
 --- qmail-qfilter.c.orig	2005-08-12 23:40:51.000000000 +0000
 +++ qmail-qfilter.c
+@@ -15,7 +15,7 @@
+  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  */
+ 
+-#include <sysdeps.h>
++#include <bglibs/sysdeps.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
 @@ -332,6 +332,8 @@ int main(int argc, char* argv[])
    if ((qqargv[0] = getenv("QQF_QMAILQUEUE")) == 0)
      qqargv[0] = QMAIL_QUEUE;
