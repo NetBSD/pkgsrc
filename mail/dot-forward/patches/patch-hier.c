b87394b6299ea2397f9b5a0b66ec8b81272aa7d8
schmonz <schmonz@pkgsrc.org>
2017-01-02 03:48:11 +0000

    Support PKGMANDIR and quell pkglint.

--- mail/dot-forward/patches/patch-hier.c
+++ mail/dot-forward/patches/patch-hier.c
@@ -1,15 +1,22 @@
-$NetBSD: patch-hier.c,v 1.1 2012/03/12 21:49:49 joerg Exp $
+$NetBSD: patch-hier.c,v 1.2 2017/01/02 03:48:11 schmonz Exp $
 
---- hier.c.orig	2012-03-12 21:06:14.000000000 +0000
+Install docs following hier(7), and don't install catpages.
+
+--- hier.c.orig	1998-05-19 10:15:50.000000000 +0000
 +++ hier.c
-@@ -7,10 +7,8 @@ void hier()
+@@ -5,12 +5,10 @@ void hier()
+   h(auto_qmail,-1,-1,0755);
+ 
    d(auto_qmail,"bin",-1,-1,0755);
-   d(auto_qmail,"man",-1,-1,0755);
-   d(auto_qmail,"man/man1",-1,-1,0755);
+-  d(auto_qmail,"man",-1,-1,0755);
+-  d(auto_qmail,"man/man1",-1,-1,0755);
 -  d(auto_qmail,"man/cat1",-1,-1,0755);
++  d(auto_qmail,"@PKGMANDIR@",-1,-1,0755);
++  d(auto_qmail,"@PKGMANDIR@/man1",-1,-1,0755);
  
    c(auto_qmail,"bin","dot-forward",-1,-1,0755);
  
-   c(auto_qmail,"man/man1","dot-forward.1",-1,-1,0644);
+-  c(auto_qmail,"man/man1","dot-forward.1",-1,-1,0644);
 -  c(auto_qmail,"man/cat1","dot-forward.0",-1,-1,0644);
++  c(auto_qmail,"@PKGMANDIR@/man1","dot-forward.1",-1,-1,0644);
  }
