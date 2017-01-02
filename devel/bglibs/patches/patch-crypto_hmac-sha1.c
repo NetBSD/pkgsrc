78f4f831909c35376056cc0d27752db12bf955de
schmonz <schmonz@pkgsrc.org>
2017-01-02 02:18:30 +0000

    Update to 2.03. From the changelog:
    - Reverted libbg-cli back to static linkage (for now) to fix problem
      with cli-generate generated code.
    - Fixed header include path in cli-generate.
    - Rewrote iobuf and str number formatting using fmt.
    - Added missing selftest.c file to the dist tarball (oops).
    - Fixed uint##.h headers.
    - Flattened out the installed headers and libraries.
      NOTE: This means this package is not backwards compatible.
    - Added dns library (modified from public domain djbdns-1.05)
    - Added wildcard handling and substitution to bg-installer.
    - Added reversed IPv4 formatting functions (from Jorge Valdes).
    - Added str_end* and str_case_end* functions, parallel to str_start* and
      str_case_start* but for suffixes.
    - Added the ability to use arbitrary format functions in fmt_multi.
    - Added support for enumerate and itemize in cli-generate.
    - Added surfrand_uniform function, protected against bias.

--- devel/bglibs/patches/patch-crypto_hmac-sha1.c
+++ devel/bglibs/patches/patch-crypto_hmac-sha1.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-crypto_hmac-sha1.c,v 1.1 2016/12/18 21:39:58 joerg Exp $
+$NetBSD: patch-crypto_hmac-sha1.c,v 1.2 2017/01/02 02:18:30 schmonz Exp $
+
+Rename hmac to bghmac.
 
 --- crypto/hmac-sha1.c.orig	2016-12-18 09:59:10.547412663 +0000
 +++ crypto/hmac-sha1.c
