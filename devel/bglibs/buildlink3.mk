7f3946f833fc832227b66f30b736c33472bf3dba
schmonz <schmonz@pkgsrc.org>
2017-01-02 02:18:30 +0000

    Update to 2.03. From the changelog:
    - Reverted libbg-cli back to static linkage (for now) to fix problem
      with cli-generate generated code.
    - Fixed header include path in cli-generate.
    - Rewrote iobuf and str number formatting using fmt.
    - Added missing selftest.c file to the dist tarball (oops).
    - Fixed uint##.h headers.
    - Flattened out the installed headers and libraries.
      NOTE: This means this package is not backwards compatible.
    - Added dns library (modified from public domain djbdns-1.05)
    - Added wildcard handling and substitution to bg-installer.
    - Added reversed IPv4 formatting functions (from Jorge Valdes).
    - Added str_end* and str_case_end* functions, parallel to str_start* and
      str_case_start* but for suffixes.
    - Added the ability to use arbitrary format functions in fmt_multi.
    - Added support for enumerate and itemize in cli-generate.
    - Added surfrand_uniform function, protected against bias.

--- devel/bglibs/buildlink3.mk
+++ devel/bglibs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/03/20 19:24:10 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2017/01/02 02:18:30 schmonz Exp $
 
 BUILDLINK_TREE+=	bglibs
 
 .if !defined(BGLIBS_BUILDLINK3_MK)
 BGLIBS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.bglibs+=	bglibs>=1.104
+BUILDLINK_API_DEPENDS.bglibs+=	bglibs>=2.03
 BUILDLINK_PKGSRCDIR.bglibs?=	../../devel/bglibs
 .endif # BGLIBS_BUILDLINK3_MK
 
