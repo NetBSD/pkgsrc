f6b02aff98226cec01bfea3b866c70cd318802fb
rodent <rodent>
2017-01-04 23:49:46 +0000

    Remove --trace argument to rake. Users are not going to GAF about this.

--- devel/ruby-redmine/files/redmine_migrate_db.sh
+++ devel/ruby-redmine/files/redmine_migrate_db.sh
@@ -4,4 +4,4 @@ cd @PREFIX@/share/@RUBY_PKGPREFIX@-redmine/app
 
 GEM_HOME=@PREFIX@/share/@RUBY_PKGPREFIX@-redmine/gems \
   GEM_PATH=@PREFIX@/share/@RUBY_PKGPREFIX@-redmine/gems \
-    @RUBY@ bin/rake --trace db:migrate RAILS_ENV=production
+    @RUBY@ bin/rake db:migrate RAILS_ENV=production
