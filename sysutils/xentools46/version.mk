cc8d096d6cfff699d778d5bd7d6cbc9e13386e0d
sborrill <sborrill@pkgsrc.org>
2017-01-04 16:37:29 +0000

    Move version to version.mk in preparation for updating xenstoretools

---
+++ sysutils/xentools46/version.mk
@@ -0,0 +1,6 @@
+# $NetBSD: version.mk,v 1.1 2017/01/04 16:37:29 sborrill Exp $
+#
+# Version number is used by xentools46 and xenstoretools
+
+VERSION=	4.6.3
+VERSION_IPXE=	9a93db3f0947484e30e753bbd61a10b17336e20e
