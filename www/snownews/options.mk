992edc2948a2ca9710cb561c1038c1204128e286
wiz <wiz>
2017-01-04 16:08:51 +0000

    Fix typo.

--- www/snownews/options.mk
+++ www/snownews/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.6 2017/01/04 15:53:56 roy Exp $
+# $NetBSD: options.mk,v 1.7 2017/01/04 16:08:51 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.snownews
 
 PKG_OPTIONS_REQUIRED_GROUPS=	screen
 PKG_OPTIONS_GROUP.screen=	curses wide-curses
-PLG_OPTIONS_LEGACY_OPTS+=	ncurses:curses ncursesw:wide-curses
+PKG_OPTIONS_LEGACY_OPTS+=	ncurses:curses ncursesw:wide-curses
 
 PKG_SUGGESTED_OPTIONS=	curses
 
