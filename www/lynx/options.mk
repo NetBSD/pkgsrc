0ef612da42c838b8a8baeb268963002bf4a3a328
roy <roy>
2017-01-04 14:49:38 +0000

    Use curses framework.

--- www/lynx/options.mk
+++ www/lynx/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.16 2015/09/30 08:25:37 tnn Exp $
+# $NetBSD: options.mk,v 1.17 2017/01/04 14:49:38 roy Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.lynx
 PKG_SUPPORTED_OPTIONS=	inet6
@@ -7,7 +7,8 @@ PKG_OPTIONS_OPTIONAL_GROUPS=	socksproxy
 PKG_OPTIONS_GROUP.socksproxy=	dante socks4
 
 PKG_OPTIONS_REQUIRED_GROUPS=	screen
-PKG_OPTIONS_GROUP.screen=	ncurses ncursesw slang curses
+PKG_OPTIONS_GROUP.screen=	curses wide-curses slang
+PKG_OPTIONS_LEGACY_OPTS+=	ncurses:curses ncursesw:wide-curses
 
 PKG_SUGGESTED_OPTIONS=	curses inet6
 
@@ -21,38 +22,23 @@ PKG_SUGGESTED_OPTIONS=	curses inet6
 PKG_FAIL_REASON+=	"SOCKS may not be enabled together with the \"slang\"" \
 			"screen library.  If SOCKS support is desired," \
 			"please change ${PKG_OPTIONS_VAR} to include" \
-			"\"curses\" or \"ncurses\" instead."
+			"\"curses\" instead."
 .endif
 
 ###
-### Set the screen library to "slang", "ncurses", or plain "curses".
+### Set the screen library to "curses". "wide-curses" or "slang"
 ###
+.if !empty(PKG_OPTIONS:Mcurses) || !empty(PKG_OPTIONS:Mwide-curses)
+CONFIGURE_ARGS+=	--enable-color-style
+SCREENTYPE=		${CURSES_TYPE}
+.  include "../../mk/curses.buildlink3.mk"
+.endif
 .if !empty(PKG_OPTIONS:Mslang)
 SCREENTYPE=		slang
 .  include "../../devel/libslang/buildlink3.mk"
 post-install:
 	${INSTALL_DATA} ${WRKSRC}/samples/lynx.lss \
 		${DESTDIR}${PREFIX}/share/examples/lynx/lynx.lss
-.endif
-.if !empty(PKG_OPTIONS:Mncurses)
-SCREENTYPE=		ncurses
-USE_NCURSES=		YES # color
-.  include "../../devel/ncurses/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-color-style
-.endif
-.if !empty(PKG_OPTIONS:Mncursesw)
-SCREENTYPE=		ncursesw
-LIBS+=			-lncursesw
-BUILDLINK_ABI_DEPENDS.ncursesw+=	ncursesw>=5.9nb1
-.  include "../../devel/ncursesw/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-color-style
-.endif
-.if !empty(PKG_OPTIONS:Mcurses)
-SCREENTYPE=		curses
-.  include "../../mk/curses.buildlink3.mk"
-.  if !empty(MACHINE_PLATFORM:MNetBSD-1.[56]*-i386)
-CONFIGURE_ARGS+=	--enable-color-style
-.  endif
 .endif
 
 ###
