22bfd66f966618306e0b4ad751fe9ad9a2568752
rodent <rodent@pkgsrc.org>
2017-01-04 23:05:11 +0000

    Make Redmine great again too. General cleanup as well as updating all the package to latest stable. There's a security issue with nokogiri that's resolved with this update. This package was tested extensively not only for build/install but also for functionality. If anyone has problems with it, please let me know.
    Full changelogs (very long) are here:
    
    http://www.redmine.org/projects/redmine/wiki/Changelog_3_3
    http://www.redmine.org/projects/redmine/wiki/Changelog_3_2
    http://www.redmine.org/projects/redmine/wiki/Changelog_3_1

--- devel/ruby-redmine/files/redmine_fix_gems.sh
+++ devel/ruby-redmine/files/redmine_fix_gems.sh
@@ -3,16 +3,18 @@
 GEM_HOME=@PREFIX@/share/@RUBY_PKGPREFIX@-redmine/gems
 GEM_PATH=@PREFIX@/share/@RUBY_PKGPREFIX@-redmine/gems
 PATH=@PREFIX@/sbin:@PREFIX@/bin:${PATH}
+NOKOGIRI_USE_SYSTEM_LIBRARIES=yes
 
-export GEM_HOME GEM_PATH PATH
+export GEM_HOME GEM_PATH PATH NOKOGIRI_USE_SYSTEM_LIBRARIES
 
 cd @PREFIX@/share/@RUBY_PKGPREFIX@-redmine/app
 
-@GEM@ pristine nokogiri --version 1.6.6.4 -- --use-system-libraries --with-xml2-config=@PREFIX@/bin/xml2-config --with-xslt-config=@PREFIX@/bin/xslt-config
+@GEM@ pristine nokogiri --version 1.6.7.2 -- --use-system-libraries --with-xml2-onfig=@PREFIX@/bin/xml2-config --with-libxslt-config=@PREFIX@/bin/xslt-config
+@GEM@ pristine ffi --version 1.9.14
 @GEM@ pristine json --version 1.8.3
-@GEM@ pristine kgio --version 2.10.0
-@GEM@ pristine mysql2 --version 0.3.20
-@GEM@ pristine raindrops --version 0.15.0
-@GEM@ pristine redcarpet --version 3.3.3
-@GEM@ pristine rmagick --version 2.15.4
-@GEM@ pristine unicorn --version 5.0.1
+@GEM@ pristine kgio --version 2.11.0
+@GEM@ pristine mysql2 --version 0.3.21
+@GEM@ pristine raindrops --version 0.17.0
+@GEM@ pristine redcarpet --version 3.3.4
+@GEM@ pristine rmagick --version 2.16.0
+@GEM@ pristine unicorn --version 5.2.0
