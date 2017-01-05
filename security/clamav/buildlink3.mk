5f5ae8da408c4bbcfb089f804db85007ceaa8907
roy <roy@pkgsrc.org>
2017-01-05 11:27:21 +0000

    Use the curses framework. Punt silly buildlink depds.

--- security/clamav/buildlink3.mk
+++ security/clamav/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2016/06/17 15:15:41 prlw1 Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/01/05 11:27:21 roy Exp $
 
 BUILDLINK_TREE+=	clamav
 
@@ -8,14 +8,6 @@ CLAMAV_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.clamav+=	clamav>=0.95.3nb1
 BUILDLINK_ABI_DEPENDS.clamav+=	clamav>=0.98.7nb2
 BUILDLINK_PKGSRCDIR.clamav?=	../../security/clamav
-
-.include "../../archivers/bzip2/buildlink3.mk"
-.include "../../devel/libltdl/buildlink3.mk"
-.include "../../devel/ncurses/buildlink3.mk"
-.include "../../devel/pcre/buildlink3.mk"
-.include "../../devel/zlib/buildlink3.mk"
-.include "../../devel/gmp/buildlink3.mk"
-.include "../../textproc/libxml2/buildlink3.mk"
 .endif	# CLAMAV_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-clamav
