2347bffe215e190a50d6aab8551cdff2a0bb9942
ryoon <ryoon@pkgsrc.org>
2017-01-09 07:55:15 +0000

    Update to 0.9.11
    * Update buildlink3.mk
    * Update HOMEPAGE and MASTER_SITES
    
    Changelog:
    0.9.11:
        Overall changes:
            LibVNCServer/LibVNCClient development now uses continous intregration, provided by TravisCI.
    
        LibVNCClient:
            Now initializes libgcrypt before use if the application did not do it. Fixes a crash when connection to Mac hosts (#45).
            Various fixes that result in more stable handling of malicious or broken servers.
            Removed broken and unmaintained H264 decoding.
            Some documentation fixes.
            Added hooks to WriteToTLS() for optional protection by mutex.
    
        LibVNCServer:
            Stability fixes for the WebSocket implementation.
            Replaced SHA1 implementation with the one from RFC 6234.
            The built-in HTTP server does not allow directory traversals anymore.
            The built-in HTTP now sends correct MIME types for CSS and SVG.
            Added support for systemd socket activation.
            Made it possible to get autoPort behavior with either ipv4 or ipv6 disabled.
            Fixed starting of an onHold-client in threaded mode.
    
    0.9.10:
        Overall changes:
            Moved the whole project from sourceforge to https://libvnc.github.io/.
            Cleaned out the autotools build system which now uses autoreconf.
            Updated noVNC HTML5 client to latest version.
            Split out x11vnc sources into separate repository at https://github.com/LibVNC/x11vnc
            Split out vncterm sources into separate repository at https://github.com/LibVNC/vncterm
            Split out VisualNaCro sources into separate repository at https://github.com/LibVNC/VisualNaCro
            Merged Debian patches.
    
        LibVNCServer/LibVNCClient:
            Fixed some security-related buffer overflow cases.
            Added compatibility headers to make LibVNCServer/LibVNCClient build on native Windows 8.
            Update LZO to version 2.07, fixing CVE-2014-4607.
    
        LibVNCServer:
            Merged patches from KDE/krfb.
            Can now do IPv6 without IPv4.
            Fixed a use-after-free issue in scale.c.

--- net/libvncserver/buildlink3.mk
+++ net/libvncserver/buildlink3.mk
@@ -1,27 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.13 2016/09/19 13:04:18 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2017/01/09 07:55:15 ryoon Exp $
 
 BUILDLINK_TREE+=	libVNCServer
 
 .if !defined(LIBVNCSERVER_BUILDLINK3_MK)
 LIBVNCSERVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libVNCServer+=	libVNCServer>=0.9.1
-BUILDLINK_ABI_DEPENDS.libVNCServer+=	libVNCServer>=0.9.9nb6
+BUILDLINK_API_DEPENDS.libVNCServer+=	libVNCServer>=0.9.11
+BUILDLINK_ABI_DEPENDS.libVNCServer+=	libVNCServer>=0.9.11
 BUILDLINK_PKGSRCDIR.libVNCServer?=	../../net/libvncserver
-
-.include "../../devel/zlib/buildlink3.mk"
-.include "../../mk/jpeg.buildlink3.mk"
-
-pkgbase := libVNCServer
-.include "../../mk/pkg-build-options.mk"
-
-.if !empty(PKG_BUILD_OPTIONS.libVNCServer:Mgnutls)
-.include "../../security/gnutls/buildlink3.mk"
-.endif
-
-.if !empty(PKG_BUILD_OPTIONS.libVNCServer:Mlibgcrypt)
-.include "../../security/libgcrypt/buildlink3.mk"
-.endif
 
 .endif # LIBVNCSERVER_BUILDLINK3_MK
 
