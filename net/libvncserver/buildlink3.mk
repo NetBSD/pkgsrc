a2acf7169dcc9ce2df6e60d738b2ae19ce8e2fad
ryoon <ryoon>
2017-01-11 12:43:17 +0000

    Restore .includes to fix dependent package builds

--- net/libvncserver/buildlink3.mk
+++ net/libvncserver/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2017/01/09 08:14:33 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2017/01/11 12:43:17 ryoon Exp $
 
 BUILDLINK_TREE+=	libVNCServer
 
@@ -8,6 +8,20 @@ LIBVNCSERVER_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libVNCServer+=	libVNCServer>=0.9.11
 BUILDLINK_ABI_DEPENDS.libVNCServer+=	libVNCServer>=0.9.11
 BUILDLINK_PKGSRCDIR.libVNCServer?=	../../net/libvncserver
+
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
+
+pkgbase := libVNCServer
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.libVNCServer:Mgnutls)
+.include "../../security/gnutls/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.libVNCServer:Mlibgcrypt)
+.include "../../security/libgcrypt/buildlink3.mk"
+.endif
 
 .endif # LIBVNCSERVER_BUILDLINK3_MK
 
