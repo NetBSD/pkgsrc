fc76205958bdc01f23ced58236ac9022b841e40f
adam <adam@pkgsrc.org>
2017-01-12 12:24:09 +0000

    GA 1.1.8: - Add connect options MYSQL_OPT_MAX_ALLOWED_PACKET, MYSQL_OPT_NET_BUFFER_LENGTH,   MYSQL_OPT_TLS_VERSION and MYSQL_OPT_SSL_MODE - Fix Visual Studio 2015 build. - Fix segmentation fault when inserting a large string - Fix Compilation Failure with PRE-5.7 C API

--- databases/mysql-connector-c++/patches/patch-driver_mysql__resultbind.cpp
+++ databases/mysql-connector-c++/patches/patch-driver_mysql__resultbind.cpp
@@ -1,16 +1,16 @@
-$NetBSD: patch-driver_mysql__resultbind.cpp,v 1.1 2016/03/08 15:40:02 wiz Exp $
+$NetBSD: patch-driver_mysql__resultbind.cpp,v 1.2 2017/01/12 12:24:09 adam Exp $
 
 https://bugs.mysql.com/bug.php?id=80539
 
---- driver/mysql_resultbind.cpp.orig	2016-01-12 18:42:38.000000000 +0000
+--- driver/mysql_resultbind.cpp.orig	2016-12-14 09:58:54.000000000 +0000
 +++ driver/mysql_resultbind.cpp
-@@ -86,7 +86,9 @@ static struct st_buffer_size_type
-     case MYSQL_TYPE_BLOB:
-     case MYSQL_TYPE_STRING:
+@@ -88,8 +88,8 @@ static struct st_buffer_size_type
      case MYSQL_TYPE_VAR_STRING:
-+#ifdef MYSQL_TYPE_JSON
+ #if LIBMYSQL_VERSION_ID > 50700
      case MYSQL_TYPE_JSON:
-+#endif
-       return st_buffer_size_type(new char[field->max_length + 1], field->max_length + 1, field->type);
+-      return st_buffer_size_type(new char[field->max_length + 1], field->max_length + 1, field->type);
+ #endif //LIBMYSQL_VERSION_ID > 50700
++      return st_buffer_size_type(new char[field->max_length + 1], field->max_length + 1, field->type);
  
      case MYSQL_TYPE_DECIMAL:
+     case MYSQL_TYPE_NEWDECIMAL:
