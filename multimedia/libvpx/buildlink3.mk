f50fd1673452438f0c3b1dc042ddc4be689019c4
wiz <wiz@pkgsrc.org>
2017-01-16 23:45:10 +0000

    Recursive bump for libvpx shlib major change.

--- multimedia/libvpx/buildlink3.mk
+++ multimedia/libvpx/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2016/08/16 23:53:25 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2017/01/16 23:45:10 wiz Exp $
 
 BUILDLINK_TREE+=	libvpx
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libvpx
 LIBVPX_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libvpx+=	libvpx>=1.6.0
-BUILDLINK_ABI_DEPENDS.libvpx+=	libvpx>=1.6.0
+BUILDLINK_ABI_DEPENDS.libvpx+=	libvpx>=1.6.1
 BUILDLINK_PKGSRCDIR.libvpx?=	../../multimedia/libvpx
 
 .include "../../mk/pthread.buildlink3.mk"
