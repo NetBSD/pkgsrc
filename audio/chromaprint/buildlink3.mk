dde688c8bafe2d4ae2474db7e6ade2f991388ce8
wiz <wiz@pkgsrc.org>
2017-01-16 23:45:10 +0000

    Recursive bump for libvpx shlib major change.

--- audio/chromaprint/buildlink3.mk
+++ audio/chromaprint/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2016/12/12 10:15:13 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2017/01/16 23:45:10 wiz Exp $
 
 BUILDLINK_TREE+=	chromaprint
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	chromaprint
 CHROMAPRINT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.chromaprint+=	chromaprint>=1.1
-BUILDLINK_ABI_DEPENDS.chromaprint?=	chromaprint>=1.4
+BUILDLINK_ABI_DEPENDS.chromaprint?=	chromaprint>=1.4.2nb1
 BUILDLINK_PKGSRCDIR.chromaprint?=	../../audio/chromaprint
 
 .include "../../multimedia/ffmpeg3/buildlink3.mk"
