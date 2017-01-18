bda2e61c6c1145991f3460160b71502f9d136532
he <he@pkgsrc.org>
2017-01-18 09:54:51 +0000

    Add two patches so that this at least semi-works when the inet6 option is used:
     * Use correct sockaddr length when doing getnameinfo() for inet6,
       so we avoid an early return with "permanent failure" from getnameinfo()
     * Use temp variables for walking the address lists so that we avoid trying
       freeaddrinfo(NULL) and getting SEGV
    
    This still isn't fully baked and backward compatible: with the
    inet6 option turned on, on NetBSD the conserver process only opens
    an inet6 server socket and no longer serves an inet socket (a
    Linuxism, I suspect), making it troublesome to interoperate with
    older versions of conserver or installations on hosts without IPv6
    connectivity.
    
    PKGREVISION bumped.

---
+++ comms/conserver8/patches/patch-conserver_access.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-conserver_access.c,v 1.1 2017/01/18 09:54:51 he Exp $
+
+Make sure to use correct sockaddr length when doing getnameinfo().
+
+--- conserver/access.c.orig	2017-01-18 09:20:03.000000000 +0000
++++ conserver/access.c
+@@ -150,6 +150,7 @@ AccType(INADDR_STYPE *addr, char **peern
+     so = sizeof(*addr);
+ 
+ #if USE_IPV6
++    so = sizeof(struct sockaddr_in6);
+     error =
+ 	getnameinfo((struct sockaddr *)addr, so, ipaddr, sizeof(ipaddr),
+ 		    NULL, 0, NI_NUMERICHOST);
