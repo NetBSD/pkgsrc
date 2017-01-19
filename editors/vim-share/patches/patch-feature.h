bb56b5c569ff103e605789f260eadd44677129d7
markd <markd>
2017-01-19 09:54:56 +0000

    Allow system-wide vimrc in PKG_SYSCONFDIR

--- editors/vim-share/patches/patch-feature.h
+++ editors/vim-share/patches/patch-feature.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-feature.h,v 1.2 2016/07/03 13:20:59 morr Exp $
+$NetBSD: patch-feature.h,v 1.3 2017/01/19 09:54:56 markd Exp $
 
---- src/feature.h.orig	2012-05-25 09:26:32.000000000 +0000
+--- src/feature.h.orig	2014-12-23 13:34:10.000000000 +0000
 +++ src/feature.h
 @@ -548,7 +548,7 @@
   *			(used only with NO_BUILTIN_TCAPS not defined).
@@ -11,3 +11,12 @@ $NetBSD: patch-feature.h,v 1.2 2016/07/03 13:20:59 morr Exp $
  #endif
  
  #if !defined(NO_BUILTIN_TCAPS)
+@@ -931,7 +931,7 @@
+ /*
+  * SYS_VIMRC_FILE	Name of the system-wide .vimrc file.
+  */
+-/* #define SYS_VIMRC_FILE	"/etc/vimrc" */
++#define SYS_VIMRC_FILE	"@PKG_SYSCONFDIR@/vimrc"
+ 
+ /*
+  * SYS_GVIMRC_FILE	Name of the system-wide .gvimrc file.
