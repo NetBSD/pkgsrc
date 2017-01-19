a16b650423ebac0d6db54146c732a10b971237a4
agc <agc@pkgsrc.org>
2017-01-19 18:52:01 +0000

    Convert all occurrences (353 by my count) of
    	MASTER_SITES= 	site1 \
    			site2
    
    style continuation lines to be simple repeated
    
    	MASTER_SITES+= site1
    	MASTER_SITES+= site2
    
    lines. As previewed on tech-pkg. With thanks to rillig for fixing pkglint
    accordingly.

--- devel/xulrunner17/dist.mk
+++ devel/xulrunner17/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.11 2013/12/16 03:10:12 ryoon Exp $
+# $NetBSD: dist.mk,v 1.12 2017/01/19 18:52:07 agc Exp $
 #
 # used by devel/xulrunner17/Makefile
 # used by www/firefox17/Makefile
@@ -7,8 +7,8 @@ DISTNAME=	firefox-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	17.0.11
 MOZ_BRANCH_MINOR=	esr
-MASTER_SITES=	${MASTER_SITE_MOZILLA_ESR:=firefox/releases/${FIREFOX_VER}/source/} \
-		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
+MASTER_SITES=	${MASTER_SITE_MOZILLA_ESR:=firefox/releases/${FIREFOX_VER}/source/}
+MASTER_SITES+=	${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
 DISTINFO_FILE=	${.CURDIR}/../../devel/xulrunner17/distinfo
