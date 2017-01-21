8bb535792f266557103eb883cd12548a7786592c
ryoon <ryoon@pkgsrc.org>
2017-01-21 20:06:44 +0000

    Recursive revbump from audio/pulseaudio-10.0

--- x11/qt5-qtquick1/buildlink3.mk
+++ x11/qt5-qtquick1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2016/12/04 05:17:16 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2017/01/21 20:06:46 ryoon Exp $
 
 BUILDLINK_TREE+=	qt5-qtquick1
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qt5-qtquick1
 QT5_QTQUICK1_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt5-qtquick1+=	qt5-qtquick1>=5.5.1
-BUILDLINK_ABI_DEPENDS.qt5-qtquick1+=	qt5-qtquick1>=5.5.1nb6
+BUILDLINK_ABI_DEPENDS.qt5-qtquick1+=	qt5-qtquick1>=5.5.1nb7
 BUILDLINK_PKGSRCDIR.qt5-qtquick1?=	../../x11/qt5-qtquick1
 
 BUILDLINK_INCDIRS.qt5-qtquick1+=	qt5/include
