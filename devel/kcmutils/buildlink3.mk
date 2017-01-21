d636552880b23a7e4e27b27f4bf08ccd1d1ea9a5
ryoon <ryoon@pkgsrc.org>
2017-01-21 20:06:44 +0000

    Recursive revbump from audio/pulseaudio-10.0

--- devel/kcmutils/buildlink3.mk
+++ devel/kcmutils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2016/12/04 05:17:05 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2017/01/21 20:06:44 ryoon Exp $
 
 BUILDLINK_TREE+=	kcmutils
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kcmutils
 KCMUTILS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kcmutils+=	kcmutils>=5.21.0
-BUILDLINK_ABI_DEPENDS.kcmutils?=	kcmutils>=5.25.0nb1
+BUILDLINK_ABI_DEPENDS.kcmutils?=	kcmutils>=5.25.0nb2
 BUILDLINK_PKGSRCDIR.kcmutils?=	../../devel/kcmutils
 
 .include "../../devel/kdeclarative/buildlink3.mk"
