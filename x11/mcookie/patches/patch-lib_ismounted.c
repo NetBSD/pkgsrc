9bcd5693b53bb0e3a45d8f8feb39adcfd6bf6d90
bsiegert <bsiegert@pkgsrc.org>
2017-01-24 21:42:26 +0000

    Fix build under macOS (and maybe others).
    The patch below replaces struct statfs with struct statvfs to fix the build
    under NetBSD. Protect it with an #ifdef.

--- x11/mcookie/patches/patch-lib_ismounted.c
+++ x11/mcookie/patches/patch-lib_ismounted.c
@@ -1,15 +1,18 @@
-$NetBSD: patch-lib_ismounted.c,v 1.1 2016/09/20 08:00:51 wiz Exp $
+$NetBSD: patch-lib_ismounted.c,v 1.2 2017/01/24 21:42:26 bsiegert Exp $
 
 Fix build on NetBSD.
 
---- lib/ismounted.c.orig	2016-03-08 13:26:33.000000000 +0000
+--- lib/ismounted.c.orig	2016-11-02 12:57:31.000000000 +0000
 +++ lib/ismounted.c
-@@ -214,7 +214,7 @@ static int check_mntent(const char *file
+@@ -214,7 +214,11 @@ static int check_mntent(const char *file
  static int check_getmntinfo(const char *file, int *mount_flags,
  				  char *mtpt, int mtlen)
  {
--	struct statfs *mp;
++#ifdef __NetBSD__
 +	struct statvfs *mp;
++#else
+ 	struct statfs *mp;
++#endif
          int    len, n;
          const  char   *s1;
  	char	*s2;
