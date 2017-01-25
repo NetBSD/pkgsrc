fd99bd5faddc338df3950571e931c33fade4f54f
maya <maya@pkgsrc.org>
2017-01-25 01:17:16 +0000

    fix previous for unpriv'd installs. from dholland.
    bump PKGREVISION.

--- misc/screen/patches/patch-screen.c
+++ misc/screen/patches/patch-screen.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-screen.c,v 1.2 2017/01/25 00:18:02 maya Exp $
+$NetBSD: patch-screen.c,v 1.3 2017/01/25 01:17:16 maya Exp $
 
 Use standard headers.
 Don't open user-supplied paths as root.
@@ -14,18 +14,20 @@ Don't open user-supplied paths as root.
  #include "screen.h"
  #ifdef HAVE_BRAILLE
  # include "braille.h"
-@@ -674,11 +676,15 @@ int main(int ac, char** av)
+@@ -674,11 +676,17 @@ int main(int ac, char** av)
                if (strlen(screenlogfile) > PATH_MAX)
                  Panic(0, "-L: logfile name too long. (max. %d char)", PATH_MAX);
  
-+	      if (seteuid(getuid()) < 0)
++	      int issetugid = geteuid() == 0;
++
++	      if (issetugid && seteuid(getuid()) < 0)
 +		Panic(0, "setuid 1");
                FILE *w_check;
                if ((w_check = fopen(screenlogfile, "w")) == NULL)
                  Panic(0, "-L: logfile name access problem");
                else
                  fclose(w_check);
-+	      if (seteuid(0) < 0)
++	      if (issetugid && seteuid(0) < 0)
 +		Panic(0, "setuid 2");
              }
              nwin_options.Lflag = 1;
