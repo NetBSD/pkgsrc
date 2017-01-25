01b89a5d9b8838b176fe9376e54f79a52800df05
adam <adam@pkgsrc.org>
2017-01-25 16:09:06 +0000

    Changes 0.25.2: Bugs fixed ---------- * Fixes several issues with C++ template deduction. * Fixes a issue with bound method type inference * Fixes a bug with cascaded tuple assignment * Fixed or silenced many Clang warnings. * Fixes bug with powers of pure real complex numbers

--- devel/py-cython/buildlink3.mk
+++ devel/py-cython/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/08/12 21:08:46 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/01/25 16:09:06 adam Exp $
 
 BUILDLINK_TREE+=	py-cython
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	py-cython
 PY_CYTHON_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.py-cython+=	${PYPKGPREFIX}-cython>=0.12.1
-BUILDLINK_PKGSRCDIR.py-cython?=	../../devel/py-cython
+BUILDLINK_PKGSRCDIR.py-cython?=		../../devel/py-cython
 .endif	# PY_CYTHON_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-py-cython
