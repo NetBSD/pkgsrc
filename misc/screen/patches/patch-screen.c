ba9f73432b414ac118a83f880bcba5fc96668bda
maya <maya@pkgsrc.org>
2017-01-25 00:18:02 +0000

    Add patch fixing root exploit. Reported here: https://lists.gnu.org/archive/html/screen-devel/2017-01/msg00025.html
    from dholland

--- misc/screen/patches/patch-screen.c
+++ misc/screen/patches/patch-screen.c
@@ -1,8 +1,9 @@
-$NetBSD: patch-screen.c,v 1.1 2017/01/23 17:58:34 dholland Exp $
+$NetBSD: patch-screen.c,v 1.2 2017/01/25 00:18:02 maya Exp $
 
 Use standard headers.
+Don't open user-supplied paths as root.
 
---- screen.c~	2017-01-17 19:28:29.000000000 +0000
+--- screen.c.orig	2017-01-17 19:28:29.000000000 +0000
 +++ screen.c
 @@ -88,6 +88,8 @@
  # include <langinfo.h>
@@ -13,3 +14,19 @@ Use standard headers.
  #include "screen.h"
  #ifdef HAVE_BRAILLE
  # include "braille.h"
+@@ -674,11 +676,15 @@ int main(int ac, char** av)
+               if (strlen(screenlogfile) > PATH_MAX)
+                 Panic(0, "-L: logfile name too long. (max. %d char)", PATH_MAX);
+ 
++	      if (seteuid(getuid()) < 0)
++		Panic(0, "setuid 1");
+               FILE *w_check;
+               if ((w_check = fopen(screenlogfile, "w")) == NULL)
+                 Panic(0, "-L: logfile name access problem");
+               else
+                 fclose(w_check);
++	      if (seteuid(0) < 0)
++		Panic(0, "setuid 2");
+             }
+             nwin_options.Lflag = 1;
+             break;
