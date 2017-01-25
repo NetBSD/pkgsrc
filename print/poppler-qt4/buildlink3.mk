d2db1fab00dfdf97777b7b0904198c138e8ec85a
wiz <wiz@pkgsrc.org>
2017-01-25 14:34:55 +0000

    Update poppler-* to 0.50.0:
    Release 0.50.0
            core:
             * PSOutputDev: Fix PS conversion for some files. Bug #63963
             * Fix Outline parsing on broken documents. Bug #98732
             * Fix PDFDoc::saveIncrementalUpdate()'s detection of document being modified. Bug #96561
             * SplashOutputDev: Read softmask into memstrean in case of matte. Bug #97803
             * Bail out if Hints nBitsNumObjects or nBitsDiffGroupLength are greater than 32. Bug #94941
             * CairoOutputDev: initialize CairoOutputDev::antialias. Bug #98983
             * Fix crash when loading some thumbnails. Bug #97870
    
            utils:
             * pdftoppm: Fix -tiff -gray/-mono incorrect output.
             * pdftops: add -passlevel1customcolor. Bug #97193
    
    
            build system:
             * Default to libopenjpeg2 instead of libopenjpeg1
    
            qt:
             * Support OCG state change links
    
            glib:
             * Use g_slice_new0 for PopplerActionLayer. Bug #98786
    
    
    Release 0.49.0
            core:
             * Merge type3 glyph handling from xpdf 3.04. Bug #96667
             * Continue rendering in case of 'Singular matrix in shading pattern fill. Bug #98623
             * Fix memory leak in parametrized gouraudTriangleShadedFill
             * Fix crash on broken files
             * PDFDoc::setDocInfoStringEntry(): treat value consisting of just the unicode marker as an empty string
             * Fix UBSAN warning
             * Misc compile fixes
    
            utils:
             * pdfseparate: remove extra '%' in error message
    
            build system:
             * configure: Fix typo in disable nss help string

--- print/poppler-qt4/buildlink3.mk
+++ print/poppler-qt4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.33 2016/11/08 10:51:27 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2017/01/25 14:34:56 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-qt4
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-qt4
 POPPLER_QT4_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-qt4+=	poppler-qt4>=0.6.1
-BUILDLINK_ABI_DEPENDS.poppler-qt4+=	poppler-qt4>=0.48.0
+BUILDLINK_ABI_DEPENDS.poppler-qt4+=	poppler-qt4>=0.50.0
 BUILDLINK_PKGSRCDIR.poppler-qt4?=	../../print/poppler-qt4
 
 .include "../../print/poppler/buildlink3.mk"
