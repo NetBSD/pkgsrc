498779139edb73b9d16ce78842b97a9a43df3cbf
adam <adam@pkgsrc.org>
2017-01-26 11:03:17 +0000

    Changes 6.2.23:
    New Features/Improvements
    -------------------------
    Added support for global message prefixes.
    
    Added Replication Manager write forwarding, which enables a client to perform simple put and delete operations by forwarding them to the master.
    
    Improved the way we mark a password as consumed.
    
    Added support for IPv6.
    
    Added support for Java 8.
    
    Added support for ARMv7 and above, and for ARM64. ARMv6 and below are no longer supported.
    
    Add a new SMP-optimized data model that is especially valuable for highly multithreaded applications. A sliced Berkeley DB instance contains a collection of related Berkeley DB environments, known as slices, each containing a disjoint subset of records. Transactional consistency is individually maintained within each slice, rather than across multiple slices.
    
    DB now supports the client-server architecture by providing a stand-alone server program and Java client driver APIs. The server program offers remote access to DB features. The client driver APIs provide building blocks for applications that communicate with a database server. Multiple client applications can communicate with a single server simultaneously.
    
    Enabled support of the SQLite user authentication extension.
    
    Added Visual Studio 2012 projects.
    
    Encryption and checksum can now be enabled with external files (formerly known as BLOBs). Note that only the data stored in the database will be encrypted and checksum protected. The data in the external files will not be encrypted or checksum protected.
    
    The BLOB files feature has been renamed external files to reduce confusion with the SQL BLOB datatype. The BLOB related functions have been deprecated, and replaced with new function names; for example, ext_file_threshold has replaced blob_threshold, and blob_threshold is deprecated. See the Upgrading section of the documentation for more details.

--- databases/db6/patches/patch-src_dbinc_atomic.h
+++ databases/db6/patches/patch-src_dbinc_atomic.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_dbinc_atomic.h,v 1.2 2015/09/08 18:51:28 joerg Exp $
+$NetBSD: patch-src_dbinc_atomic.h,v 1.3 2017/01/26 11:03:17 adam Exp $
 
---- src/dbinc/atomic.h.orig	2015-09-02 11:18:22.000000000 +0000
+--- src/dbinc/atomic.h.orig	2016-03-28 19:45:54.000000000 +0000
 +++ src/dbinc/atomic.h
 @@ -70,7 +70,7 @@ typedef struct {
   * These have no memory barriers; the caller must include them when necessary.
@@ -11,12 +11,12 @@ $NetBSD: patch-src_dbinc_atomic.h,v 1.2 2015/09/08 18:51:28 joerg Exp $
  
  #ifdef HAVE_ATOMIC_SUPPORT
  
-@@ -205,7 +205,7 @@ static inline int __atomic_compare_excha
+@@ -225,7 +225,7 @@ static inline int __atomic_compare_excha
  #define	atomic_dec(env, p)	(--(p)->value)
  #define	atomic_compare_exchange(env, p, oldval, newval)		\
  	(DB_ASSERT(env, atomic_read(p) == (oldval)),		\
 -	atomic_init(p, (newval)), 1)
 +	db_atomic_init(p, (newval)), 1)
  #else
- #define atomic_inc(env, p)	__atomic_inc(env, p)
- #define atomic_dec(env, p)	__atomic_dec(env, p)
+ #define	atomic_inc(env, p)	__atomic_inc_int(env, p)
+ #define	atomic_dec(env, p)	__atomic_dec_int(env, p)
