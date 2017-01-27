85076f2e4b732afac2b25f16adbac6ac35a76e52
roy <roy>
2017-01-27 12:11:13 +0000

    Add a whois option.

--- www/awstats/options.mk
+++ www/awstats/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2009/01/15 12:12:04 adam Exp $
+# $NetBSD: options.mk,v 1.3 2017/01/27 12:11:13 roy Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.awstats
+PKG_OPTIONS_VAR=        PKG_OPTIONS.awstats
 
-PKG_SUPPORTED_OPTIONS=	geoip
+PKG_SUPPORTED_OPTIONS=  geoip whois
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,5 +10,12 @@ PKG_SUPPORTED_OPTIONS=	geoip
 ### Use the Geo IP Free module
 ###
 .if !empty(PKG_OPTIONS:Mgeoip)
-DEPENDS+=	p5-Geo-IPfree-[0-9]*:../../net/p5-Geo-IPfree
+DEPENDS+=       p5-Geo-IPfree-[0-9]*:../../net/p5-Geo-IPfree
+.endif
+
+###
+### Optional Whois support
+###
+.if !empty(PKG_OPTIONS:Mwhois)
+DEPENDS+=       p5-Net-XWhois-[0-9]*:../../net/p5-Net-XWhois
 .endif
