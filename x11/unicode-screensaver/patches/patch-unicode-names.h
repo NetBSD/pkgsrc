efe6ab28e84c2e3fb51781b44fefde0911343f5a
mef <mef@pkgsrc.org>
2017-01-29 06:26:51 +0000

    Updated x11/unicode-screensaver to 0.5 -------------------------------------- - ChangeLog unknown. - (pkgsrc-changes) Add patch for correction to yomi Farad

---
+++ x11/unicode-screensaver/patches/patch-unicode-names.h
@@ -0,0 +1,15 @@
+$NetBSD: patch-unicode-names.h,v 1.1 2017/01/29 06:26:52 mef Exp $
+
+Correction of yomi to katakana Farad
+
+--- unicode-names.h~	2015-05-09 16:37:53.000000000 +0900
++++ unicode-names.h	2017-01-29 15:13:03.000000000 +0900
+@@ -19728,7 +19728,7 @@ static const char unicode_names_strings[
+   "SQUARE HOORU\0"
+   "SQUARE HP\0"
+   "SQUARE HPA\0"
+-  "SQUARE HUARADDO\0"
++  "SQUARE FARAD\0"
+   "SQUARE HUIITO\0"
+   "SQUARE HURAN\0"
+   "SQUARE HZ\0"
