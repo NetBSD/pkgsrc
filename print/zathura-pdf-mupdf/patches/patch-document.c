1d1e80919c0c79e615e9b69544408ac5d79088fb
leot <leot@pkgsrc.org>
2017-01-31 10:12:02 +0000

    Update print/zathura-pdf-mupdf to zathura-pdf-mupdf-0.3.1
    Changes:
     - Compatible with mupdf 1.10
     - Added flags for freetype, harfbuzz and zlib
     - Correctly use pdf_save_document

--- print/zathura-pdf-mupdf/patches/patch-document.c
+++ print/zathura-pdf-mupdf/patches/patch-document.c
@@ -1,15 +1,11 @@
-$NetBSD: patch-document.c,v 1.3 2016/11/28 14:41:40 leot Exp $
+$NetBSD: patch-document.c,v 1.4 2017/01/31 10:12:02 leot Exp $
 
-o Correct the glib-2.0 include header file path.
-o Update to mupdf-1.10 API
+Correct the glib-2.0 include header file path.
 
---- document.c.orig	2016-02-14 22:49:46.000000000 +0000
+--- document.c.orig	2017-01-11 21:21:14.000000000 +0000
 +++ document.c
-@@ -3,10 +3,9 @@
- #define _POSIX_C_SOURCE 1
- 
+@@ -5,7 +5,7 @@
  #include <mupdf/fitz.h>
--#include <mupdf/xps.h>
  #include <mupdf/pdf.h>
  
 -#include <glib-2.0/glib.h>
@@ -17,17 +13,3 @@ o Update to mupdf-1.10 API
  
  #include "plugin.h"
  
-@@ -109,12 +108,7 @@ pdf_document_save_as(zathura_document_t*
-   }
- 
-   fz_try (mupdf_document->ctx) {
--    /* fz_write_document claims to accepts NULL as third argument but doesn't.
--     * pdf_write_document does not check if the third arguments is NULL for some
--     * options. */
--
--    fz_write_options opts = { 0 }; /* just use the default options */
--    fz_write_document(mupdf_document->ctx, mupdf_document->document, (char*) path, &opts);
-+    pdf_save_document(mupdf_document->ctx, pdf_specifics(mupdf_document->ctx, mupdf_document->document), (char*) path, NULL);
-   } fz_catch (mupdf_document->ctx) {
-     return ZATHURA_ERROR_UNKNOWN;
-   }
