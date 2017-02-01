6dbfa1d24c2d6ccf06a0238b886ae58f23b0a354
mef <mef>
2017-02-01 13:00:41 +0000

    Updated sysutils/webmin and wbm-* from 1.600 to 1.831 -------------------------------------- ---- Changes since 1.590 ---- The titles for existing clone modules can now be changed on the Module Titles page.
    ---- Changes since 1.610 ----
    - Added a page for viewing and running Webmin scheduled functions.
    - Added a section to the Sending Email page to verify that the
      configured mail server settings work.
    
    ---- Changes since 1.620 ----
    Added a setting to the Web Server Options page to control if redirects
    use just a path, or the full URL.
    
    ---- Changes since 1.640 ----
    Actions on the Webmin Scheduled Functions page can now be clicked on
    to change the time the function runs at.
    
    ---- Changes since 1.650 ----
    Added an SSL option to use only ciphers with perfect forward secrecy.
    Added support for two-factor authentication using Authy or Google Authenticator.
    
    ---- Changes since 1.660 ----
    Updated the UI on several pages to use the standard Webmin library,
    for a more consistent look.
    
    ---- Changes since 1.730 ----
    Added an option to use an SSL connection when Webmin sends email, for
    connecting to remote mail servers like Gmail that don't allow
    unencrypted SMTP.
    
    ---- Changes since 1.770 ----
    An SSL certificate can now be requested from Let's Encrypt using a new
    tab on the SSL Encryption page.
    
    ---- Changes since 1.780 ----
    Added an option for automatically renewing Let's Encrypt certificates.
    If the Let's Encrypt client is not installed, Webmin will use its own
    built-in client code to request a certificate.
    
    ---- Changes since 1.800 ----
    Added an option to the logging page for sending Webmin action log
    messages via email.
    
    ---- Changes since 1.810 ----
    The Let's Encrypt key size can now be customized.
    When Perfect Forward Secrecy ciphers are selected, the required DH
    params file is now created and used by Webmin.

--- sysutils/webmin/wbm.mk
+++ sysutils/webmin/wbm.mk
@@ -1,4 +1,4 @@
-# $NetBSD: wbm.mk,v 1.12 2015/11/25 12:53:29 jperkin Exp $
+# $NetBSD: wbm.mk,v 1.13 2017/02/01 13:00:44 mef Exp $
 #
 # Makefile fragment for Webmin modules
 #
@@ -15,6 +15,7 @@
 #
 # WBM_DEPEND_MODULES	Depending webmin modules.
 #
+LICENSE=	modified-bsd
 
 .include "version.mk"
 
