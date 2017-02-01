36136ca22b220ad2747d1e9efd4a1e3dcf457c26
mef <mef@pkgsrc.org>
2017-02-01 14:17:21 +0000

    Updated sysutils/vifm 0.7.8 to 0.8.2 ------------------------------------ Changes found in NEWS file is too lengthy, just quote the first part of it.
    vifm v0.8.2
    -----------
    This release brings a bit of user-friendliness in form of key suggestions and
    various descriptions provided in the interface.  Matching of files has been
    extended in many ways and become significantly more powerful than before.
    Menu mode also got several enhancements.

--- sysutils/vifm/patches/patch-src_utils_mntent.c
+++ sysutils/vifm/patches/patch-src_utils_mntent.c
@@ -1,19 +1,19 @@
-$NetBSD: patch-src_utils_mntent.c,v 1.1 2015/02/15 12:50:53 ryoon Exp $
+$NetBSD: patch-src_utils_mntent.c,v 1.2 2017/02/01 14:17:21 mef Exp $
 
 * I have no idea how to test this...
 
---- src/utils/mntent.c.orig	2014-10-25 15:09:35.000000000 +0000
-+++ src/utils/mntent.c
-@@ -47,7 +47,7 @@
- #include <stdlib.h>
- #include <string.h>
+--- src/compat/mntent.c.orig	2015-10-09 01:52:00.000000000 +0900
++++ src/compat/mntent.c	2017-02-01 22:41:54.000000000 +0900
+@@ -53,7 +53,7 @@
+ #define f_flags f_flag
+ #endif
  
 -static struct mntent * statfs_to_mntent(struct statfs *mntbuf);
-+static struct mntent * statvfs_to_mntent(struct statvfs *mntbuf);
++static struct mntent * statvfs_to_mntent(struct statfs *mntbuf);
  static char * flags2opts(int flags);
  static char * catopt(char s0[], const char s1[]);
  
-@@ -76,7 +76,7 @@ getmntent(FILE *fp)
+@@ -82,7 +82,7 @@ getmntent(FILE *fp)
  	static int pos = -1;
  	static int mntsize = -1;
  
@@ -22,7 +22,7 @@ $NetBSD: patch-src_utils_mntent.c,v 1.1 2015/02/15 12:50:53 ryoon Exp $
  
  	if(pos == -1 || mntsize == -1)
  	{
-@@ -90,11 +90,11 @@ getmntent(FILE *fp)
+@@ -96,11 +96,11 @@ getmntent(FILE *fp)
  		return NULL;
  	}
  
@@ -36,7 +36,7 @@ $NetBSD: patch-src_utils_mntent.c,v 1.1 2015/02/15 12:50:53 ryoon Exp $
  {
  	static struct mntent _mntent;
  	static char opts_buf[40], *tmp;
-@@ -102,7 +102,7 @@ statfs_to_mntent(struct statfs *mntbuf)
+@@ -108,7 +108,7 @@ statfs_to_mntent(struct statfs *mntbuf)
  	_mntent.mnt_fsname = mntbuf->f_mntfromname;
  	_mntent.mnt_dir = mntbuf->f_mntonname;
  	_mntent.mnt_type = mntbuf->f_fstypename;
@@ -45,12 +45,3 @@ $NetBSD: patch-src_utils_mntent.c,v 1.1 2015/02/15 12:50:53 ryoon Exp $
  	if(tmp != NULL)
  	{
  		opts_buf[sizeof(opts_buf) - 1] = '\0';
-@@ -128,7 +128,7 @@ flags2opts(int flags)
- 	if(flags & MNT_UNION)       res = catopt(res, "union");
- 	if(flags & MNT_ASYNC)       res = catopt(res, "async");
- 	if(flags & MNT_NOATIME)     res = catopt(res, "noatime");
--#ifndef __APPLE__
-+#if !(defined(__APPLE__) || defined(__NetBSD__))
- 	if(flags & MNT_NOCLUSTERR)  res = catopt(res, "noclusterr");
- 	if(flags & MNT_NOCLUSTERW)  res = catopt(res, "noclusterw");
- 	if(flags & MNT_NOSYMFOLLOW) res = catopt(res, "nosymfollow");
