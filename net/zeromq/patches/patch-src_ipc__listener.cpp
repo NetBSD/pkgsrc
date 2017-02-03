76cea31dffaee88f51a90118c6907e2a0974c4b3
maya <maya@pkgsrc.org>
2017-02-03 16:39:15 +0000

    zeromq: fix build on NetBSD. it has SOCK_CLOEXEC, but no accept4. it's expected to use paccept instead. do this.
    tests 75/78 passing

---
+++ net/zeromq/patches/patch-src_ipc__listener.cpp
@@ -0,0 +1,18 @@
+$NetBSD: patch-src_ipc__listener.cpp,v 1.1 2017/02/03 16:39:16 maya Exp $
+
+NetBSD has SOCK_CLOEXEC -- but uses it with paccept, not accept4
+
+--- src/ipc_listener.cpp.orig	2016-12-31 15:25:47.000000000 +0000
++++ src/ipc_listener.cpp
+@@ -391,7 +391,11 @@ zmq::fd_t zmq::ipc_listener_t::accept ()
+     //  resources is considered valid and treated by ignoring the connection.
+     zmq_assert (s != retired_fd);
+ #if defined ZMQ_HAVE_SOCK_CLOEXEC
++#if defined(__NetBSD__)
++    fd_t sock = ::paccept (s, NULL, NULL, NULL, SOCK_CLOEXEC);
++#else
+     fd_t sock = ::accept4 (s, NULL, NULL, SOCK_CLOEXEC);
++#endif
+ #else
+     fd_t sock = ::accept (s, NULL, NULL);
+ #endif
