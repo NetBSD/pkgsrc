6bbe70a4f954d59f76f4b4b6ce2e284fe4bc079b
maya <maya@pkgsrc.org>
2017-02-03 17:10:49 +0000

    rename ptp-suid option to gstreamer-ptp-suid
    requested by wiz

--- multimedia/gstreamer1/options.mk
+++ multimedia/gstreamer1/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.6 2017/01/29 23:24:10 maya Exp $
+# $NetBSD: options.mk,v 1.7 2017/02/03 17:10:49 maya Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gstreamer
-PKG_SUPPORTED_OPTIONS=	gstreamer-gstcheck introspection ptp-suid
+PKG_SUPPORTED_OPTIONS=	gstreamer-gstcheck introspection gstreamer-ptp-suid
 PKG_SUGGESTED_OPTIONS=	introspection
 
 .include "../../mk/bsd.options.mk"
@@ -23,10 +23,7 @@ PLIST.introspection=yes
 CONFIGURE_ARGS+=--disable-introspection
 .endif
 
-# PTP network clock requires setuid root for gst-ptp-helper
-# It's not useful enough to be a default, but left as an option
-# for those interested in it.
-.if !empty(PKG_OPTIONS:Mptp-suid)
+.if !empty(PKG_OPTIONS:Mgstreamer-ptp-suid)
 SPECIAL_PERMS+=		libexec/gstreamer-1.0/gst-ptp-helper ${SETUID_ROOT_PERMS}
 CONFIGURE_ARGS+=	--with-ptp-helper-permissions=setuid-root
 .else
