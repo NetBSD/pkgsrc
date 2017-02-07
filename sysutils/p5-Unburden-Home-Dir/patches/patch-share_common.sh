f2b738f6ff96909e900e4e6aaaa21a0621b49cb3
hauke <hauke@pkgsrc.org>
2017-02-07 14:34:35 +0000

    Add p5-Unburden-Home-Dir package.
    unburden-home-dir allows users to move cache files from browsers,
    etc. off their (nfs or ssd based) home directory, i.e. on a local
    harddisk or tmpfs and replace them with a symbolic link to the new
    location (e.g. on /tmp/ or /scratch/) upon login.
    
    Optionally the contents of these directories and files can be removed
    instead of moved.

---
+++ sysutils/p5-Unburden-Home-Dir/patches/patch-share_common.sh
@@ -0,0 +1,28 @@
+$NetBSD: patch-share_common.sh,v 1.1 2017/02/07 14:34:36 hauke Exp $
+
+Fix absolute paths.
+Normalize prefs filename.
+
+--- share/common.sh.orig	2016-10-17 14:13:06.000000000 +0000
++++ share/common.sh
+@@ -2,16 +2,12 @@
+ 
+ UNBURDEN_BASENAME=${UNBURDEN_BASENAME:-unburden-home-dir}
+ 
+-if [ -e /etc/default/"${UNBURDEN_BASENAME}" ]; then
+-    . /etc/default/"${UNBURDEN_BASENAME}"
++if [ -e @SYSCONFDIR@/"${UNBURDEN_BASENAME}".conf ]; then
++    . @SYSCONFDIR@/"${UNBURDEN_BASENAME}".conf
+ fi
+ 
+-if [ -e /etc/"${UNBURDEN_BASENAME}" ]; then
+-    . /etc/"${UNBURDEN_BASENAME}"
+-fi
+-
+-if [ -e "${HOME}/.${UNBURDEN_BASENAME}" ]; then
+-    . "${HOME}/.${UNBURDEN_BASENAME}"
++if [ -e "${HOME}/.${UNBURDEN_BASENAME}".conf ]; then
++    . "${HOME}/.${UNBURDEN_BASENAME}".conf
+ fi
+ 
+ if [ -e "${XDG_CONFIG_HOME:-$HOME/.config}/${UNBURDEN_BASENAME}/config" ]; then
