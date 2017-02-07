38e467a12690627489779014841da0dcda3141e3
joerg <joerg@pkgsrc.org>
2017-02-07 19:38:48 +0000

    Fix ruby dependency patterns.

--- x11/ruby-gnome2-gtk3/buildlink3.mk
+++ x11/ruby-gnome2-gtk3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2017/02/06 13:54:59 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2017/02/07 19:38:48 joerg Exp $
 
 BUILDLINK_TREE+=	ruby-gnome2-gtk3
 
@@ -6,8 +6,7 @@ BUILDLINK_TREE+=	ruby-gnome2-gtk3
 RUBY_GNOME2_GTK3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-gtk3+=${RUBY_PKGPREFIX}-gnome2-gtk3>=1.2.0
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk3+=${RUBY_PKGPREFIX}-gnome2-gtk3>=3.0.8nb1
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk3?=	ruby22-gnome2-gtk3>=3.1.1nb1
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk3+=${RUBY_PKGPREFIX}-gnome2-gtk3>=3.1.1nb1
 BUILDLINK_PKGSRCDIR.ruby-gnome2-gtk3?=	../../x11/ruby-gnome2-gtk3
 
 .include "../../devel/ruby-gnome2-glib/buildlink3.mk"
