0b60630c654ca70a7c6324b2cdfce8340efb6e32
wiz <wiz@pkgsrc.org>
2017-02-08 21:59:28 +0000

    Updated syncthing to 0.14.23.
    0.14.23
    
    This is a hotfix release recommended for all users.
    
    Resolved issues:
    
        #3970: Syncthing restarts when started manually and auto upgrading
    
    
    0.14.22
    
    This is a regular periodic release withdrawn because of an issue with automatic upgrade.
    
    Resolved issues:
    
        #3935: Leading and trailing spaces are no longer stripped in the GUI password field
        #3908: The GUI shows remaining amount of data to sync per folder.
        #3933: There should no longer be empty entries in the global log
        #3938: Weak hashing is now by default only enabled when it makes sense from a performance point of view.
    
    Also:
    
    This release introduces the the ability to choose between stable and candidate
    releases for auto upgrades.

--- net/syncthing/patches/patch-lib_config_optionsconfiguration.go
+++ net/syncthing/patches/patch-lib_config_optionsconfiguration.go
@@ -1,4 +1,4 @@
-$NetBSD: patch-lib_config_optionsconfiguration.go,v 1.3 2016/06/01 18:46:01 gdt Exp $
+$NetBSD: patch-lib_config_optionsconfiguration.go,v 1.4 2017/02/08 21:59:28 wiz Exp $
 
 By default, disable automatic upgrade checking, because it is a
 privacy leak, of the sort that would earn a warning on f-droid a la:
@@ -7,14 +7,14 @@ privacy leak, of the sort that would earn a warning on f-droid a la:
 Not reported upstream because they appear to think upgrade
 checks/web-bugs are a feature.
 
---- lib/config/optionsconfiguration.go.orig	2016-05-26 09:16:08.000000000 +0000
+--- lib/config/optionsconfiguration.go.orig	2017-02-07 20:25:33.000000000 +0000
 +++ lib/config/optionsconfiguration.go
-@@ -29,7 +29,7 @@ type OptionsConfiguration struct {
- 	URPostInsecurely        bool     `xml:"urPostInsecurely" json:"urPostInsecurely" default:"false"` // For testing
- 	URInitialDelayS         int      `xml:"urInitialDelayS" json:"urInitialDelayS" default:"1800"`
- 	RestartOnWakeup         bool     `xml:"restartOnWakeup" json:"restartOnWakeup" default:"true"`
--	AutoUpgradeIntervalH    int      `xml:"autoUpgradeIntervalH" json:"autoUpgradeIntervalH" default:"12"` // 0 for off
-+	AutoUpgradeIntervalH    int      `xml:"autoUpgradeIntervalH" json:"autoUpgradeIntervalH" default:"0"` // 0 for off
- 	KeepTemporariesH        int      `xml:"keepTemporariesH" json:"keepTemporariesH" default:"24"`         // 0 for off
- 	CacheIgnoredFiles       bool     `xml:"cacheIgnoredFiles" json:"cacheIgnoredFiles" default:"false"`
- 	ProgressUpdateIntervalS int      `xml:"progressUpdateIntervalS" json:"progressUpdateIntervalS" default:"5"`
+@@ -117,7 +117,7 @@ type OptionsConfiguration struct {
+ 	URPostInsecurely        bool                    `xml:"urPostInsecurely" json:"urPostInsecurely" default:"false"` // For testing
+ 	URInitialDelayS         int                     `xml:"urInitialDelayS" json:"urInitialDelayS" default:"1800"`
+ 	RestartOnWakeup         bool                    `xml:"restartOnWakeup" json:"restartOnWakeup" default:"true"`
+-	AutoUpgradeIntervalH    int                     `xml:"autoUpgradeIntervalH" json:"autoUpgradeIntervalH" default:"12"` // 0 for off
++	AutoUpgradeIntervalH    int                     `xml:"autoUpgradeIntervalH" json:"autoUpgradeIntervalH" default:"0"` // 0 for off
+ 	UpgradeToPreReleases    bool                    `xml:"upgradeToPreReleases" json:"upgradeToPreReleases"`              // when auto upgrades are enabled
+ 	KeepTemporariesH        int                     `xml:"keepTemporariesH" json:"keepTemporariesH" default:"24"`         // 0 for off
+ 	CacheIgnoredFiles       bool                    `xml:"cacheIgnoredFiles" json:"cacheIgnoredFiles" default:"false"`
