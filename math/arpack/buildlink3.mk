db3127b73a9a8d26ba5f407f3bdc0165940e3f47
minskim <minskim@pkgsrc.org>
2017-02-09 06:06:25 +0000

    Correct the default value of BUILDLINK_DEPMETHOD.arpack
    Since arpack installs a dynamic library, its BUILDLINK_DEPMETHOD
    shouldn't be set to "build" by default.
    
    Bump PKGREVISION of octave for its runtime dependency change.

--- math/arpack/buildlink3.mk
+++ math/arpack/buildlink3.mk
@@ -1,11 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/04/06 13:46:33 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/02/09 06:06:25 minskim Exp $
 
 BUILDLINK_TREE+=	arpack
 
 .if !defined(ARPACK_BUILDLINK3_MK)
 ARPACK_BUILDLINK3_MK:=
-
-BUILDLINK_DEPMETHOD.arpack?=	build
 
 BUILDLINK_API_DEPENDS.arpack+=	arpack>=96
 BUILDLINK_PKGSRCDIR.arpack?=	../../math/arpack
