a2b737bba506718848180e12561247b3ee4802df
alnsn <alnsn@pkgsrc.org>
2017-02-10 23:01:49 +0000

    Update lang/lua53 to version 5.3.4.
    Lua 5.3.4 fixes all bugs listed in
            http://www.lua.org/bugs.html#5.3.3
    
    1. Expression list with four or more expressions in a 'for' loop can crash
       the interpreter.
       reported by Marco Scho:pl on 17 Jun 2016. existed since 5.2. fixed in 5.3.4.
    
    2. Checking a format for os.date may read pass the format string.
       reported by Nagaev Boris on 10 Jul 2016. existed since 5.3.3. fixed in 5.3.4.
    
    3. Lua can generate wrong code in functions with too many constants.
       reported by Marco Scho:pl on 17 Jul 2016. existed since 5.3.3. fixed in 5.3.4.
    
    4. When a coroutine tries to resume a non-suspended coroutine, it can do some
       mess (and break C assertions) before detecting the error.
       reported by Marco Scho:pl on 20 Jul 2016. fixed in 5.3.4.
    
    Lua 5.3.4 also contains several internal improvements and
    includes a revised reference manual:
    	http://www.lua.org/manual/5.3/

--- lang/lua53/version.mk
+++ lang/lua53/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.4 2016/06/06 22:18:07 alnsn Exp $
+# $NetBSD: version.mk,v 1.5 2017/02/10 23:01:49 alnsn Exp $
 
-LUA_VERSION=	5.3.3
+LUA_VERSION=	5.3.4
 
 LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
 LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
