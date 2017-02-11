fda771f176a6b3a72bd6b1acd064b6b69f4e7a85
adam <adam@pkgsrc.org>
2017-02-11 10:18:51 +0000

    The PostgreSQL Global Development Group has released an update to all supported versions of our database system, including 9.6.2, 9.5.6, 9.4.11, 9.3.16, and 9.2.20. This release includes fixes that prevent data corruption issues in index builds and in certain write-ahead-log replay situations, which are detailed below. It also patches over 75 other bugs reported over the last three months.

--- databases/postgresql94/patches/patch-contrib_uuid-ossp_uuid-ossp.c
+++ databases/postgresql94/patches/patch-contrib_uuid-ossp_uuid-ossp.c
@@ -1,18 +1,18 @@
-$NetBSD: patch-contrib_uuid-ossp_uuid-ossp.c,v 1.1 2015/01/14 21:01:18 adam Exp $
+$NetBSD: patch-contrib_uuid-ossp_uuid-ossp.c,v 1.2 2017/02/11 10:18:52 adam Exp $
 
 Explicitly define HAVE_UUID_H and HAVE_UUID_OSSP.
 
---- contrib/uuid-ossp/uuid-ossp.c.orig	2015-01-14 20:54:38.000000000 +0000
+--- contrib/uuid-ossp/uuid-ossp.c.orig	2017-02-06 21:49:02.000000000 +0000
 +++ contrib/uuid-ossp/uuid-ossp.c
-@@ -26,6 +26,7 @@
+@@ -30,6 +30,7 @@
   */
  #define uuid_hash bsd_uuid_hash
  
 +#define HAVE_UUID_H
- #ifdef HAVE_UUID_H
+ #if defined(HAVE_UUID_H)
  #include <uuid.h>
- #endif
-@@ -42,6 +43,7 @@
+ #elif defined(HAVE_OSSP_UUID_H)
+@@ -46,6 +47,7 @@
   * Some BSD variants offer md5 and sha1 implementations but Linux does not,
   * so we use a copy of the ones from pgcrypto.  Not needed with OSSP, though.
   */
