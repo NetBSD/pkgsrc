cb95a02ff66b5001d8444ae58d47bf5fdb897859
adam <adam@pkgsrc.org>
2017-02-11 22:27:00 +0000

    PoDoFo 0.9.5 was released today, after over 6 months of development and with the help of many new contributors.
    As there are so many improvements, patches and fixes that made it into this release, we are not able to list them, so please stick with the svn log.

--- print/podofo/patches/patch-src_base_PdfMemoryManagement.cpp
+++ print/podofo/patches/patch-src_base_PdfMemoryManagement.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_base_PdfMemoryManagement.cpp,v 1.1 2016/08/02 20:14:44 wiz Exp $
+$NetBSD: patch-src_base_PdfMemoryManagement.cpp,v 1.2 2017/02/11 22:27:00 adam Exp $
 
 Make sure SIZE_MAX is defined.
 
---- src/base/PdfMemoryManagement.cpp.orig	2016-05-15 19:12:45.000000000 +0000
+--- src/base/PdfMemoryManagement.cpp.orig	2017-01-29 12:26:55.000000000 +0000
 +++ src/base/PdfMemoryManagement.cpp
 @@ -31,6 +31,9 @@
   *   files in the program, then also delete it here.                       *
@@ -12,5 +12,5 @@ Make sure SIZE_MAX is defined.
 +#include <stdint.h>
 +
  #include "PdfMemoryManagement.h"
+ #include "PdfDefines.h"
  #include "PdfDefinesPrivate.h"
- 
