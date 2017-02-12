7fed77973bfc8e6739a455cf4bc769619bb10ddb
ryoon <ryoon@pkgsrc.org>
2017-02-12 06:27:04 +0000

    FIx buildlink3.mk to remove graphite2 support

--- fonts/harfbuzz/buildlink3.mk
+++ fonts/harfbuzz/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2017/02/06 12:45:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2017/02/12 06:27:04 ryoon Exp $
 
 BUILDLINK_TREE+=	harfbuzz
 
@@ -6,12 +6,11 @@ BUILDLINK_TREE+=	harfbuzz
 HARFBUZZ_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.harfbuzz+=	harfbuzz>=0.9.4
-BUILDLINK_ABI_DEPENDS.harfbuzz+=	harfbuzz>=1.4.2
+BUILDLINK_ABI_DEPENDS.harfbuzz+=	harfbuzz>=1.4.2nb1
 BUILDLINK_PKGSRCDIR.harfbuzz?=		../../fonts/harfbuzz
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
-.include "../../graphics/graphite2/buildlink3.mk"
 .endif	# HARFBUZZ_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-harfbuzz
