796027db2ec8015f160a379c3d127d34d0d91e45
adam <adam@pkgsrc.org>
2017-02-12 21:50:17 +0000

    Changes 2.0.5: This is a bug fix release, addressing issues in Octave interface, newer compilers and with some HDR file formats.

--- graphics/pfstools/buildlink3.mk
+++ graphics/pfstools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:44 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/02/12 21:50:17 adam Exp $
 
 BUILDLINK_TREE+=	pfstools
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	pfstools
 PFSTOOLS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pfstools+=	pfstools>=1.7.0
-BUILDLINK_PKGSRCDIR.pfstools?=	../../graphics/pfstools
+BUILDLINK_PKGSRCDIR.pfstools?=		../../graphics/pfstools
 .endif # PFSTOOLS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-pfstools
