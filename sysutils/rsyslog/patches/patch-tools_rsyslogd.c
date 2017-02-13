15f1c010d3fc301916dab85cc5224d0e6df9d67a
fhajny <fhajny@pkgsrc.org>
2017-02-13 19:59:36 +0000

    Update sysutils/rsyslog to 8.24.0.
    Changelog (abridged):
    
    - rsyslog now builds on AIX
    - mmdblookup: new maxminddb lookup message modify plugin
    - mmrm1stspace: new module; removes first space in MSG if present
    - KSI signature provider: file permissions can now be specified
    - omzmq: new features
    - change: when the hostname is empty, we now use "localhost-empty-hostname"
    - omelasticsearch: remove "asyncrepl" config parameter
    - omfwd: Add support for bind-to-device (see below on same for imudp)
    - imudp: Add support for bind-to-device
    - imudp: limit rcvbufsize parameter to max 1GiB
    - rainerscript: implement new "call_indirect" statement
    - bugfix imjournal: make state file handling more robust
    - bugfix core: lookup table reload was not properly integrated
    - bugfix core: potential dealock on shutdown
    - bugfix ommongodb: did not work in v8 due to invalid indirection
    - bugfix ommongodb: fix tryResume handling
    - bugfix omfwd: retry processing was not done correctly, could stall
    - bugfix imuxsock: segfault non shutdown when $OmitLocalLogging is on

--- sysutils/rsyslog/patches/patch-tools_rsyslogd.c
+++ sysutils/rsyslog/patches/patch-tools_rsyslogd.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-tools_rsyslogd.c,v 1.4 2016/10/20 10:05:11 fhajny Exp $
+$NetBSD: patch-tools_rsyslogd.c,v 1.5 2017/02/13 19:59:36 fhajny Exp $
 
 Need errno.h on NetBSD. Default PID file path.
---- tools/rsyslogd.c.orig	2016-08-23 12:15:33.000000000 +0000
+--- tools/rsyslogd.c.orig	2017-01-10 09:00:04.000000000 +0000
 +++ tools/rsyslogd.c
-@@ -32,7 +32,7 @@
+@@ -33,7 +33,7 @@
  #else
  #  include <syslog.h>
  #endif
@@ -12,12 +12,12 @@ Need errno.h on NetBSD. Default PID file path.
  #	include <errno.h>
  #else
  #	include <sys/errno.h>
-@@ -89,7 +89,7 @@ void rsyslogdDoDie(int sig);
- 
- 
- #ifndef PATH_PIDFILE
+@@ -146,7 +146,7 @@ void rsyslogdDoDie(int sig);
+ #if defined(_AIX)  /* AIXPORT : Add _AIX */
+ #	define PATH_PIDFILE "/etc/rsyslogd.pid"
+ #else
 -#	define PATH_PIDFILE "/var/run/rsyslogd.pid"
 +#	define PATH_PIDFILE "@RSYSLOG_PIDDIR@/rsyslogd.pid"
+ #endif /*_AIX*/
  #endif
  
- /* global data items */
