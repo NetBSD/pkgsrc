ac93adce98b69d7f02264cd2ac6236b6af1a60b8
fhajny <fhajny@pkgsrc.org>
2017-02-13 19:59:36 +0000

    Update sysutils/rsyslog to 8.24.0.
    Changelog (abridged):
    
    - rsyslog now builds on AIX
    - mmdblookup: new maxminddb lookup message modify plugin
    - mmrm1stspace: new module; removes first space in MSG if present
    - KSI signature provider: file permissions can now be specified
    - omzmq: new features
    - change: when the hostname is empty, we now use "localhost-empty-hostname"
    - omelasticsearch: remove "asyncrepl" config parameter
    - omfwd: Add support for bind-to-device (see below on same for imudp)
    - imudp: Add support for bind-to-device
    - imudp: limit rcvbufsize parameter to max 1GiB
    - rainerscript: implement new "call_indirect" statement
    - bugfix imjournal: make state file handling more robust
    - bugfix core: lookup table reload was not properly integrated
    - bugfix core: potential dealock on shutdown
    - bugfix ommongodb: did not work in v8 due to invalid indirection
    - bugfix ommongodb: fix tryResume handling
    - bugfix omfwd: retry processing was not done correctly, could stall
    - bugfix imuxsock: segfault non shutdown when $OmitLocalLogging is on

--- sysutils/rsyslog/patches/patch-plugins_mmexternal_mmexternal.c
+++ sysutils/rsyslog/patches/patch-plugins_mmexternal_mmexternal.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-plugins_mmexternal_mmexternal.c,v 1.4 2016/04/22 16:52:14 jperkin Exp $
+$NetBSD: patch-plugins_mmexternal_mmexternal.c,v 1.5 2017/02/13 19:59:36 fhajny Exp $
 
 Fix build on BSD.
---- plugins/mmexternal/mmexternal.c.orig	2016-03-02 11:12:44.000000000 +0000
+--- plugins/mmexternal/mmexternal.c.orig	2017-01-10 09:00:04.000000000 +0000
 +++ plugins/mmexternal/mmexternal.c
 @@ -31,7 +31,7 @@
  #include <errno.h>
  #include <unistd.h>
  #include <fcntl.h>
--#if defined(__FreeBSD__)
-+#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFlyBSD__) || defined(__APPLE__)
+-#if defined(_AIX) || defined(__FreeBSD__) 
++#if defined(_AIX) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFlyBSD__) || defined(__APPLE__)
  #include <sys/wait.h>
  #else
  #include <wait.h>
