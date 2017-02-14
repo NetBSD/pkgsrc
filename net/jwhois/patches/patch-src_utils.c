ccf0c12bcff60135a24523b1fef8fe68dfdfdccb
joerg <joerg@pkgsrc.org>
2017-02-14 21:24:49 +0000

    Add missing include.

--- net/jwhois/patches/patch-src_utils.c
+++ net/jwhois/patches/patch-src_utils.c
@@ -1,8 +1,16 @@
-$NetBSD: patch-src_utils.c,v 1.1 2012/10/23 20:49:38 shattered Exp $
+$NetBSD: patch-src_utils.c,v 1.2 2017/02/14 21:25:20 joerg Exp $
 
 --- src/utils.c.orig	2007-06-26 07:00:20.000000000 +0000
 +++ src/utils.c
-@@ -288,7 +288,7 @@ make_connect(const char *host, int port)
+@@ -38,6 +38,7 @@
+ # include <netdb.h>
+ #endif
+ 
++#include <sys/time.h>
+ #include <unistd.h>
+ #include <fcntl.h>
+ #include <errno.h>
+@@ -288,7 +289,7 @@ make_connect(const char *host, int port)
  
        retlen = sizeof(retval);
        error = getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &retval, &retlen);
