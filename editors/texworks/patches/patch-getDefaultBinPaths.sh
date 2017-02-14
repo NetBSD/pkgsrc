6de8ab5cd0ec4fa4750fbf1b9e051443305dc786
markd <markd@pkgsrc.org>
2017-02-14 09:19:43 +0000

    Update texworks to 0.6.1
    Release 0.6.1 (TL'16) [May 2016]
            * Fix "Jump to Source" from PDF context menu
            * Fix fine-sync'ing close to PDF line boundaries
            * Fix loading of Qt-global translations
            * Fix pixel-alignment after zoomFitWindow in continuous mode
            * Fix handling of changing scroll bar visibilities in zoomFitWidth and
              zoomFitWindow
            * Allow horizontal scrolling by Shift+MouseWheel
            * Update translations
    
    Release 0.6.0 (TL'16) [April 2016]
            * Complete redesign of the PDF previewer using QtPDF by Charlie Sharpsteen
            * Fast and responsive preview even at very high magnifications
            * New page modes: continuous scrolling and two-column layout
            * Text selection/copying from the PDF
            * Go back to previous view after clicking on a link using Alt+left arrow
    
            * Fix disappearing spellcheck underline with some fonts/font sizes in Qt 5
            * Update the width of the line number display when changing the editor font
            * Show a note in the "About Scripts" dialog for disabled script languages
            * Explicitly add a "Quit TeXworks" menu item to the global menubar on
              Mac OS X
    
            * Make Qt5 the default framework for building TeXworks
            * Use Travis CI for continuous integration tests and builds for Windows and
              Mac OS X
            * Add support for Lua 5.3
    
    Release 0.4.6 (TL'15) [April 2015]
            * Implement character-level synchronization by using text searching to
              assist SyncTeX
            * Lift hardwrap line length limitation and implement (hard) unwrap
            * Always show console output tab while typesetting to show progress
            * Improve the handling of UTF-8 byte order marks
            * Improve the log parsing script
            * Improve the handling of script errors
    
            * Work around a race condition that causes lines to become invisible,
              subsequently leading to a crash
            * Fix incorrectly accumulating indent
            * Fix SyncTeX initialization with non-ASCII filenames
            * Fix selection of whole lines and when selecting right-to-left
            * Fix hangs and crashes when using "Fit to ..." in the PDF view
            * Fix a crash when running an invalid system command from scripts
            * Fix encoding issues when reading environment variables
            * Fix scrolling when searching for multi-line strings
            * Fix auto-completion when working with RTL languages
            * Improve the handling of Retina displays in Mac OS X
            * Fix a crash when opening PDF files from the Windows Explorer
            * Provide texworks.appdata.xml for *nix platforms
    
            * Fix compilation with Qt 5
            * Improve the CMake build system
            * Add support for Lua 5.2
            * Move development from Google Code to GitHub

--- editors/texworks/patches/patch-getDefaultBinPaths.sh
+++ editors/texworks/patches/patch-getDefaultBinPaths.sh
@@ -1,25 +1,27 @@
-$NetBSD: patch-getDefaultBinPaths.sh,v 1.3 2014/01/14 08:56:08 markd Exp $
+$NetBSD: patch-getDefaultBinPaths.sh,v 1.4 2017/02/14 09:19:43 markd Exp $
 
---- getDefaultBinPaths.sh.orig	2013-04-14 13:45:23.000000000 +0000
+--- getDefaultBinPaths.sh.orig	2016-05-01 08:43:14.000000000 +0000
 +++ getDefaultBinPaths.sh
-@@ -74,13 +74,13 @@ case $PLATFORM in
+@@ -74,14 +74,14 @@ case $PLATFORM in
  	*)			OS=`echo $PLATFORM | sed 's/.*-//'`
  esac
  
+-appendPath "/usr/local/texlive/2017/bin/$ARCH-$OS"
+-appendPath "/usr/local/texlive/2016/bin/$ARCH-$OS"
+-appendPath "/usr/local/texlive/2015/bin/$ARCH-$OS"
+-appendPath "/usr/local/texlive/2014/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2013/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2012/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2011/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2010/bin/$ARCH-$OS"
--appendPath "/usr/local/texlive/2009/bin/$ARCH-$OS"
--appendPath "/usr/local/texlive/2008/bin/$ARCH-$OS"
--appendPath "/usr/local/texlive/2007/bin/$ARCH-$OS"
++#appendPath "/usr/local/texlive/2017/bin/$ARCH-$OS"
++#appendPath "/usr/local/texlive/2016/bin/$ARCH-$OS"
++#appendPath "/usr/local/texlive/2015/bin/$ARCH-$OS"
++#appendPath "/usr/local/texlive/2014/bin/$ARCH-$OS"
 +#appendPath "/usr/local/texlive/2013/bin/$ARCH-$OS"
 +#appendPath "/usr/local/texlive/2012/bin/$ARCH-$OS"
 +#appendPath "/usr/local/texlive/2011/bin/$ARCH-$OS"
 +#appendPath "/usr/local/texlive/2010/bin/$ARCH-$OS"
-+#appendPath "/usr/local/texlive/2009/bin/$ARCH-$OS"
-+#appendPath "/usr/local/texlive/2008/bin/$ARCH-$OS"
-+#appendPath "/usr/local/texlive/2007/bin/$ARCH-$OS"
  
  for TEXLIVEROOT in /usr/local/texlive/* /opt/texlive/*; do
  	# Check if this is really a folder (e.g., /opt/... might not exist)
