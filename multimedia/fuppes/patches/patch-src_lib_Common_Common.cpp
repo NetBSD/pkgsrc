118d1a1b09a449250e108a2b76f379a103cdb9f5
joerg <joerg@pkgsrc.org>
2017-02-14 21:25:50 +0000

    Fix iconv error handling. Bump revision.

--- multimedia/fuppes/patches/patch-src_lib_Common_Common.cpp
+++ multimedia/fuppes/patches/patch-src_lib_Common_Common.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_lib_Common_Common.cpp,v 1.1 2013/04/30 22:34:31 joerg Exp $
+$NetBSD: patch-src_lib_Common_Common.cpp,v 1.2 2017/02/14 21:25:50 joerg Exp $
 
---- src/lib/Common/Common.cpp.orig	2013-04-30 12:37:10.000000000 +0000
+--- src/lib/Common/Common.cpp.orig	2009-12-20 20:26:29.000000000 +0000
 +++ src/lib/Common/Common.cpp
 @@ -38,6 +38,7 @@
  
@@ -10,3 +10,12 @@ $NetBSD: patch-src_lib_Common_Common.cpp,v 1.1 2013/04/30 22:34:31 joerg Exp $
  #endif
  #include <sys/stat.h>
  #include <sys/types.h>
+@@ -466,7 +467,7 @@ std::string ToUTF8(std::string p_sValue,
+ 		icv = iconv_open("UTF-8", p_sEncoding.c_str());
+ 	}
+ 	
+-  if(icv < 0)  
++  if(icv == (iconv_t)-1)  
+     return p_sValue;  
+   
+   size_t nInbytes  = p_sValue.length(); 
