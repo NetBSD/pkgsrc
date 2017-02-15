ae3e20124c59b7186a64a50ca128258fb71584d0
adam <adam@pkgsrc.org>
2017-02-15 17:34:37 +0000

    Libevent 2.1.8-stable, it contains openssl fixes for resetting fd and using bufferevent_openssl_filter_new(). vagrant fixes, some build fixes, increased timeout for some tests (to reduce number of failures due to timing issues), date in RFC1123 format and running tests in parallel.

--- devel/libevent/buildlink3.mk
+++ devel/libevent/buildlink3.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.23 2016/03/05 11:27:45 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2017/02/15 17:34:37 adam Exp $
 
 BUILDLINK_TREE+=	libevent
 
 .if !defined(LIBEVENT_BUILDLINK3_MK)
 LIBEVENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libevent+=libevent>=0.6
-BUILDLINK_ABI_DEPENDS.libevent+=libevent>=2.0.10
-BUILDLINK_ABI_DEPENDS.libevent?=	libevent>=2.0.22nb1
-BUILDLINK_PKGSRCDIR.libevent?=	../../devel/libevent
+BUILDLINK_API_DEPENDS.libevent+=	libevent>=0.6
+BUILDLINK_ABI_DEPENDS.libevent+=	libevent>=2.0.22nb1
+BUILDLINK_PKGSRCDIR.libevent?=		../../devel/libevent
 
 .include "../../security/openssl/buildlink3.mk"
 .endif # LIBEVENT_BUILDLINK3_MK
--- devel/libevent/patches/patch-evutil__rand.c
+++ devel/libevent/patches/patch-evutil__rand.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-evutil__rand.c,v 1.1 2015/10/13 14:21:13 jperkin Exp $
+$NetBSD: patch-evutil__rand.c,v 1.2 2017/02/15 17:34:37 adam Exp $
 
 Native illumos arc4random(3C) imported the latest OpenBSD API which
 does not have arc4random_addrandom().
 
 --- evutil_rand.c.orig	2013-11-01 18:18:57.000000000 +0000
 +++ evutil_rand.c
-@@ -174,7 +174,9 @@ evutil_secure_rng_get_bytes(void *buf, s
+@@ -195,7 +195,9 @@ evutil_secure_rng_get_bytes(void *buf, s
  void
  evutil_secure_rng_add_bytes(const char *buf, size_t n)
  {
