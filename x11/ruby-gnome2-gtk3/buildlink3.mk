64a0b4ab7a0580a40c56ec5d7d50a0a7cd09083f
joerg <joerg@pkgsrc.org>
2017-02-15 00:43:11 +0000

    Fix more fallout from bad recursive bumps. Please check your diffs!

--- x11/ruby-gnome2-gtk3/buildlink3.mk
+++ x11/ruby-gnome2-gtk3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2017/02/12 06:24:59 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2017/02/15 00:43:12 joerg Exp $
 
 BUILDLINK_TREE+=	ruby-gnome2-gtk3
 
@@ -6,8 +6,7 @@ BUILDLINK_TREE+=	ruby-gnome2-gtk3
 RUBY_GNOME2_GTK3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-gtk3+=${RUBY_PKGPREFIX}-gnome2-gtk3>=1.2.0
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk3+=${RUBY_PKGPREFIX}-gnome2-gtk3>=3.1.1nb1
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk3?=	ruby23-gnome2-gtk3>=3.1.1nb2
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk3+=${RUBY_PKGPREFIX}-gnome2-gtk3>=3.1.1nb2
 BUILDLINK_PKGSRCDIR.ruby-gnome2-gtk3?=	../../x11/ruby-gnome2-gtk3
 
 .include "../../devel/ruby-gnome2-glib/buildlink3.mk"
