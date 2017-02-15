7d69b8a1b2fddfc9c6e87446dcd0c61ac1ed4dcb
joerg <joerg>
2017-02-15 00:43:11 +0000

    Fix more fallout from bad recursive bumps. Please check your diffs!

--- devel/ruby-gnome2-pango/buildlink3.mk
+++ devel/ruby-gnome2-pango/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.42 2017/02/12 06:24:42 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.43 2017/02/15 00:43:11 joerg Exp $
 
 BUILDLINK_TREE+=	ruby-gnome2-pango
 
@@ -6,8 +6,7 @@ BUILDLINK_TREE+=	ruby-gnome2-pango
 RUBY_GNOME2_PANGO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-pango+=${RUBY_PKGPREFIX}-gnome2-pango>=0.16.0
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-pango+=${RUBY_PKGPREFIX}-gnome2-pango>=3.1.1nb1
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-pango?=	ruby23-gnome2-pango>=3.1.1nb2
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-pango+=${RUBY_PKGPREFIX}-gnome2-pango>=3.1.1nb2
 BUILDLINK_PKGSRCDIR.ruby-gnome2-pango?=	../../devel/ruby-gnome2-pango
 
 .include "../../devel/ruby-gnome2-glib/buildlink3.mk"
