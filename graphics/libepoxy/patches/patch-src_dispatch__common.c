b7c888ab99882a4a8c1322eaa8b5a9f7304ce749
wiz <wiz@pkgsrc.org>
2017-02-17 15:17:08 +0000

    Updated libepoxy to 1.4.
    This is a new stable release.
    
    Major changes for 1.4 are:
    
        Epoxy can now build with MSVC versions prior to 2013; we still recommend using a recent, C99-compatible compiler, like MSVC 2015 [Chun-wei Fan]
        When used under X11, Epoxy now attempts to handle the cases where the GLX extension is not built or not available [Yaron Cohen-Tal]
        GLX can now be enabled and disabled at configuration time; this allows building Epoxy with GLX on macOS, and allows building Epoxy without GLX on embedded platforms
        Epoxy now exposes API that lets dependent projects safely check if platform API like GLX and EGL is available at run time
        EGL support has been improved on Windows, and made more resilient on other platforms [Yaron Cohen-Tal, Adam Jackson]
        Epoxy supports building with the Meson build system, which has Ninja, Visual Studio, and XCode backends
        Epoxy can generate its API reference using Doxygen (currently only available on Meson builds)
        The GL registry has been updated with the latest version of the API references provided by Khronos; Epoxy now supports the API introduced by OpenGL 4.5

--- graphics/libepoxy/patches/patch-src_dispatch__common.c
+++ graphics/libepoxy/patches/patch-src_dispatch__common.c
@@ -1,11 +1,10 @@
-$NetBSD: patch-src_dispatch__common.c,v 1.4 2015/10/01 13:50:05 tnn Exp $
+$NetBSD: patch-src_dispatch__common.c,v 1.5 2017/02/17 15:17:09 wiz Exp $
 
 Deal with hardcoded libGL locations and versions.
-Function must return a value if errx not declared with attr noreturn.
 
---- src/dispatch_common.c.orig	2015-07-15 23:46:36.000000000 +0000
+--- src/dispatch_common.c.orig	2017-02-06 16:00:20.000000000 +0000
 +++ src/dispatch_common.c
-@@ -103,11 +103,13 @@
+@@ -174,11 +174,13 @@
  #include "dispatch_common.h"
  
  #ifdef __APPLE__
@@ -20,11 +19,3 @@ Function must return a value if errx not declared with attr noreturn.
  #endif
  
  #ifdef ANDROID
-@@ -672,6 +674,7 @@ epoxy_get_proc_address(const char *name)
- #endif
-     }
-     errx(1, "Couldn't find current GLX or EGL context.\n");
-+    return NULL;
- #endif
- }
- 
