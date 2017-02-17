a5eb0d013503f0dab8845ecc5a11dd5d4dc155e3
wiz <wiz@pkgsrc.org>
2017-02-17 15:43:43 +0000

    Updated calibre to 2.79.1.
    2.79.1
    
    New Features
    
        Windows: Make recently opened books show up in the jump lists for the standalone viewer and editor programs
        OS X/Linux: Show an informational popup message when an Android device is plugged in that needs the user to tap Allow for the connection to work.
        Conversion: Log options that are different from the defaults separately, for easy reference
    
    Bug Fixes
    
        2.79.1 fixes a regression in 2.79.0 that broke the file open dialogs on windows vista
        Conversion: Fix regression that broke conversion of some epub files when using the 'Remove first image' option.
        Get Books: Update Kobo plugin for website changes
        Windows MTP driver: Fix error when enumerating device storage not being reported during debug device detection
        Tag Browser: When dragging and dropping a book onto a series increment the series number.
        Fix crash when generating covers with Qt 5.8 on linux
        Fix Get Books result list and jobs list not being populated on Qt 5.8
        Fix importing rules in the coloring/icon rules dialog not enabling the apply button

--- misc/calibre/patches/patch-setup_build__environment.py
+++ misc/calibre/patches/patch-setup_build__environment.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup_build__environment.py,v 1.3 2016/09/29 12:13:43 joerg Exp $
+$NetBSD: patch-setup_build__environment.py,v 1.4 2017/02/17 15:43:43 wiz Exp $
 
 Always build like on Linux.
 
---- setup/build_environment.py.orig	2016-09-29 09:55:14.000000000 +0000
+--- setup/build_environment.py.orig	2017-02-12 02:09:49.000000000 +0000
 +++ setup/build_environment.py
 @@ -97,8 +97,8 @@ def get_sip_dir():
  pyqt['pyqt_sip_dir'] = get_sip_dir()
  pyqt['sip_inc_dir'] = os.environ.get('SIP_INC_DIR', sysconfig.get_path('include'))
  
--glib_flags = subprocess.check_output([PKGCONFIG, '--libs', 'glib-2.0']).strip() if islinux else ''
--fontconfig_flags = subprocess.check_output([PKGCONFIG, '--libs', 'fontconfig']).strip() if islinux else ''
+-glib_flags = subprocess.check_output([PKGCONFIG, '--libs', 'glib-2.0']).strip() if islinux or ishaiku else ''
+-fontconfig_flags = subprocess.check_output([PKGCONFIG, '--libs', 'fontconfig']).strip() if islinux or ishaiku else ''
 +glib_flags = subprocess.check_output([PKGCONFIG, '--libs', 'glib-2.0']).strip()
 +fontconfig_flags = subprocess.check_output([PKGCONFIG, '--libs', 'fontconfig']).strip()
  qt_inc = pyqt['inc']
@@ -19,7 +19,7 @@ Always build like on Linux.
  'WebKit', 'WebKitWidgets', 'Widgets',  'Multimedia', 'MultimediaWidgets', 'Xml',  # 'XmlPatterns',
  )]
  QT_PLUGINS = ('imageformats', 'audio', 'iconengines', 'mediaservice', 'platforms', 'playlistformats', 'printsupport', 'sqldrivers')
--if islinux:
+-if islinux or ishaiku:
 +if True:
      # platformthemes cause crashes in Ubuntu
      QT_PLUGINS += ('platforminputcontexts', 'generic',)
