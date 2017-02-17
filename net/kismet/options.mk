a4dd006fabcc329ee008dba9bb6ca0a1f40bd5a2
joerg <joerg@pkgsrc.org>
2017-02-17 17:10:05 +0000

    Add missing include. Use old ImageMagick for the gps option.

--- net/kismet/options.mk
+++ net/kismet/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2007/02/19 18:22:58 joerg Exp $
+# $NetBSD: options.mk,v 1.3 2017/02/17 17:10:05 joerg Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kismet
@@ -11,7 +11,7 @@ PKG_SUGGESTED_OPTIONS=	gps
 PLIST_SRC+=		PLIST.gps
 DEPENDS+=		wget>=1.9.1:../../net/wget
 .include "../../devel/gmp/buildlink3.mk"
-.include "../../graphics/ImageMagick/buildlink3.mk"
+.include "../../graphics/ImageMagick6/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 .endif
