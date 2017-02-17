433d0b89a2cc0693c9b272fbb0bec94b549b0529
bsiegert <bsiegert>
2017-02-17 19:11:36 +0000

    Pullup ticket #5214 - requested by maya devel/memcached: security fix
    Revisions pulled up:
    - devel/memcached/Makefile                                      1.53
    - devel/memcached/distinfo                                      1.27
    - devel/memcached/patches/patch-Makefile.am                     deleted
    - devel/memcached/patches/patch-aa                              deleted
    - devel/memcached/patches/patch-ab                              deleted
    - devel/memcached/patches/patch-items.c                         1.1
    - devel/memcached/patches/patch-logger.h                        1.1
    
    ---
       Module Name:    pkgsrc
       Committed By:   adam
       Date:           Thu Feb 16 11:10:53 UTC 2017
    
       Modified Files:
              pkgsrc/devel/memcached: Makefile distinfo
       Added Files:
              pkgsrc/devel/memcached/patches: patch-items.c patch-logger.h
       Removed Files:
              pkgsrc/devel/memcached/patches: patch-Makefile.am patch-aa patch-ab
    
       Log Message:
       Changes 1.4.34:
       Add -o modern switches to -h
       metadump: Fix preventing dumping of class 63
       Fix cache_memlimit bug for > 4G values
       metadump: ensure buffer is flushed to client before finishing
       Number of small fixes/additions to new logging
       add logging endpoint for LRU crawler
       evicted_active counter for LRU maintainer
       stop pushing NULL byte into watcher stream
       Scale item hash locks more with more worker threads (minor performance)
       Further increase systemd service hardening
       Missing necessary header for atomic_inc_64_nv() used in logger.c (solaris)
       Fix print format for idle timeout thread
       Improve binary sasl security fixes
       Fix clang compile error
       Widen systemd caps to allow maxconns to increase
       Add -X option to disable cachedump/metadump
       Don't double free in lru_crawler on closed clients
       Fix segfault if metadump client goes away

--- devel/memcached/patches/patch-items.c
+++ devel/memcached/patches/patch-items.c
@@ -1,24 +1,25 @@
-$NetBSD: patch-aa,v 1.9 2015/12/08 17:41:56 adam Exp $
+$NetBSD: patch-items.c,v 1.1.2.2 2017/02/17 19:11:36 bsiegert Exp $
 
 time_t changes
 
---- items.c.orig	2015-11-20 01:18:56.000000000 +0000
+--- items.c.orig	2017-01-08 01:10:24.000000000 +0000
 +++ items.c
-@@ -460,9 +460,9 @@ char *item_cachedump(const unsigned int 
+@@ -489,10 +489,10 @@ char *item_cachedump(const unsigned int 
          /* Copy the key since it may not be null-terminated in the struct */
          strncpy(key_temp, ITEM_key(it), it->nkey);
          key_temp[it->nkey] = 0x00; /* terminate */
--        len = snprintf(temp, sizeof(temp), "ITEM %s [%d b; %lu s]\r\n",
+-        len = snprintf(temp, sizeof(temp), "ITEM %s [%d b; %llu s]\r\n",
 +        len = snprintf(temp, sizeof(temp), "ITEM %s [%d b; %jd s]\r\n",
                         key_temp, it->nbytes - 2,
--                       (unsigned long)it->exptime + process_started);
+                        it->exptime == 0 ? 0 :
+-                       (unsigned long long)it->exptime + process_started);
 +                       (intmax_t)it->exptime + process_started);
          if (bufcurr + len + 6 > memlimit)  /* 6 is END\r\n\0 */
              break;
          memcpy(buffer + bufcurr, temp, len);
-@@ -576,13 +576,13 @@ void item_stats(ADD_STAT add_stats, void
-             if (settings.expirezero_does_not_evict)
+@@ -615,13 +615,13 @@ void item_stats(ADD_STAT add_stats, void
                  APPEND_NUM_FMT_STAT(fmt, n, "number_noexp", "%u", lru_size_map[3]);
+             }
          }
 -        APPEND_NUM_FMT_STAT(fmt, n, "age", "%u", age);
 +        APPEND_NUM_FMT_STAT(fmt, n, "age", "%jd", age);
