57aba32a0c9999c0297fb27a8750816c4a68be54
taca <taca@pkgsrc.org>
2017-02-18 13:17:23 +0000

    API has changed with 0.25.0.

--- devel/libgit2/buildlink3.mk
+++ devel/libgit2/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/09/13 12:21:13 taca Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/02/18 13:17:23 taca Exp $
 
 BUILDLINK_TREE+=	libgit2
 
 .if !defined(LIBGIT2_BUILDLINK3_MK)
 LIBGIT2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgit2+=	libgit2>=0.23.0
+BUILDLINK_API_DEPENDS.libgit2+=	libgit2>=0.25.0
 BUILDLINK_PKGSRCDIR.libgit2?=	../../devel/libgit2
 
 .include "../../devel/zlib/buildlink3.mk"
