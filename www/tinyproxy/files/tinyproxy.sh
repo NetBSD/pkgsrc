3bab993ce856614e33b6687dc66201770ec177e1
nonaka <nonaka@pkgsrc.org>
2017-02-18 05:59:23 +0000

    fixup tinyproxy.conf

--- www/tinyproxy/files/tinyproxy.sh
+++ www/tinyproxy/files/tinyproxy.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: tinyproxy.sh,v 1.2 2011/02/03 19:27:55 shattered Exp $
+# $NetBSD: tinyproxy.sh,v 1.3 2017/02/18 05:59:23 nonaka Exp $
 #
 
 # PROVIDE: tinyproxy
@@ -11,8 +11,8 @@
 name="tinyproxy"
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
-pidfile="/var/run/${name}.pid"
-required_files="@PKG_SYSCONFDIR@/$name.conf"
+pidfile="@VARBASE@/run/tinyproxy/${name}.pid"
+required_files="@PKG_SYSCONFDIR@/${name}.conf"
 
 load_rc_config $name
 run_rc_command "$1"
