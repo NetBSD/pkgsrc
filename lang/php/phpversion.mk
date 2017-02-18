c2067701158defb96e02664bdcfa33004b69a30d
taca <taca@pkgsrc.org>
2017-02-18 13:13:07 +0000

    Update php71 to
    16 Feb 2017, PHP 7.1.2
    
    - Core:
      . Improved GENERATOR_CREATE opcode handler. (Bob, Dmitry)
      . Fixed bug #73877 (readlink() returns garbage for UTF-8 paths). (Anatol)
      . Fixed bug #73876 (Crash when exporting **= in expansion of assign op).
        (Sara)
      . Fixed bug #73962 (bug with symlink related to cyrillic directory). (Anatol)
      . Fixed bug #73969 (segfault in debug_print_backtrace). (andrewnester)
      . Fixed bug #73994 (arginfo incorrect for unpack). (krakjoe)
      . Fixed bug #73973 (assertion error in debug_zval_dump). (andrewnester)
    
    - DOM:
      . Fixed bug #54382 (getAttributeNodeNS doesn't get xmlns* attributes).
        (aboks)
    
    - DTrace:
      . Fixed bug #73965 (DTrace reported as enabled when disabled). (Remi)
    
    - FCGI:
      . Fixed bug #73904 (php-cgi fails to load -c specified php.ini file). (Anatol)
      . Fixed bug #72898 (PHP_FCGI_CHILDREN is not included in phpinfo()). (Anatol)
    
    - FPM:
      . Fixed bug #69865 (php-fpm does not close stderr when using syslog).
        (m6w6)
    
    - GD:
      . Fixed bug #73968 (Premature failing of XBM reading). (cmb)
    
    - GMP:
      . Fixed bug #69993 (test for gmp.h needs to test machine includes).
        (Jordan Gigov)
    
    - Hash:
      . Added hash_hkdf() function. (Andrey Andreev)
      . Fixed bug #73961 (environmental build dependency in hash sha3 source).
        (krakjoe)
    
    - Intl:
      . Fix bug #73956 (Link use CC instead of CXX). (Remi)
    
    - LDAP:
      . Fixed bug #73933 (error/segfault with ldap_mod_replace and opcache).
        (Laruence)
    
    - MySQLi:
      . Fixed bug #73949 (leak in mysqli_fetch_object). (krakjoe)
    
    - Mysqlnd:
      . Fixed bug #69899 (segfault on close() after free_result() with mysqlnd).
        (Richard Fussenegger)
    
    - Opcache:
      . Fixed bug #73983 (crash on finish work with phar in cli + opcache).
        (Anatol)
    
    - OpenSSL:
      . Fixed bug #71519 (add serial hex to return value array). (xrobau)
      . Fixed bug #73692 (Compile ext/openssl with openssl 1.1.0 on Win). (Anatol)
      . Fixed bug #73978 (openssl_decrypt triggers bug in PDO). (Jakub Zelenka)
    
    - PDO_Firebird:
      . Implemented FR #72583 (All data are fetched as strings). (Dorin Marcoci)
    
    - PDO_PgSQL:
      . Fixed bug #73959 (lastInsertId fails to throw an exception for wrong
        sequence name). (andrewnester)
    
    - Phar:
      . Fixed bug #70417 (PharData::compress() doesn't close temp file). (cmb)
    
    - posix:
      . Fixed bug #71219 (configure script incorrectly checks for ttyname_r). (atoh)
    
    - Session:
      . Fixed bug #69582 (session not readable by root in CLI). (EvgeniySpinov)
    
    - SPL:
      . Fixed bug #73896 (spl_autoload() crashes when calls magic _call()). (Dmitry)
    
    - Standard:
      . Fixed bug #69442 (closing of fd incorrect when PTS enabled). (jaytaph)
      . Fixed bug #47021 (SoapClient stumbles over WSDL delivered with
        "Transfer-Encoding: chunked"). (Rowan Collins)
      . Fixed bug #72974 (imap is undefined service on AIX). (matthieu.sarter)
      . Fixed bug #72979 (money_format stores wrong length AIX). (matthieu.sarter)
      . Fixed bug #73374 (intval() with base 0 should detect binary). (Leigh)
      . Fixed bug #69061 (mail.log = syslog contains double information).
        (Tom Sommer)
    
    - ZIP:
      . Fixed bug #70103 (ZipArchive::addGlob ignores remove_all_path option). (cmb,
        Mitch Hagstrand)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.172 2017/02/18 13:09:19 taca Exp $
+# $NetBSD: phpversion.mk,v 1.173 2017/02/18 13:13:07 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.30
 PHP70_VERSION=	7.0.16
-PHP71_VERSION=	7.1.1
+PHP71_VERSION=	7.1.2
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
