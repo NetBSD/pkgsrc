0cc8f57867ad596cbb36bf2364be499156fb9c7b
adam <adam@pkgsrc.org>
2017-02-19 18:54:39 +0000

    Changes 0.25.0: - More cleanups, removal of obsolete stuff, and moves towards py3k   compatibility. - Add support for EC.get_builtin_curves() and use it for testing. - Enable AES CTR mode - Bundle-in six module v. 1.10.0 - add rand_file_name and rand_status - remove all LHASH fiddling - Extend Travis and GitLab CI configuration to test also py3k (with   allowed_failures) and CentOS6 (on GitLab CI). - Add CONTRIBUTORS.rst. Thank you! - Add PEP-484 type hints in comments to all Python files (except for   tests) - Use context managers for file handling wherever possible instead of   leaking open file descriptors. - Improve defaults handling for SSL_CTX_new(). - Fix PGP tests to actually run

--- security/py-m2crypto/patches/patch-setup.py
+++ security/py-m2crypto/patches/patch-setup.py
@@ -1,17 +1,16 @@
-$NetBSD: patch-setup.py,v 1.2 2014/06/01 23:51:25 rodent Exp $
+$NetBSD: patch-setup.py,v 1.3 2017/02/19 18:54:39 adam Exp $
 
 Don't pass "-includeall" to swig.  It causes compiler-dependent
 tests in system include files to break (e.g. "#ifdef __GNUC__"
 doesn't work properly.)
 
---- setup.py.orig	2014-01-22 19:36:53.000000000 +0000
+--- setup.py.orig	2016-07-22 12:43:17.000000000 +0000
 +++ setup.py
-@@ -75,7 +75,7 @@ class _M2CryptoBuildExt(build_ext.build_
+@@ -112,7 +112,6 @@ class _M2CryptoBuildExt(build_ext.build_
+             self.swig_opts.append('-D%s' % arch)
  
-         self.swig_opts = ['-I%s' % i for i in self.include_dirs + \
-                           [opensslIncludeDir, os.path.join(opensslIncludeDir, "openssl")]]
+         self.swig_opts.extend(['-I%s' % i for i in self.include_dirs])
 -        self.swig_opts.append('-includeall')
-+        #self.swig_opts.append('-includeall')
          self.swig_opts.append('-modern')
+         self.swig_opts.append('-builtin')
  
-         # Fedora does hat tricks.
