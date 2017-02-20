c97c719a893342fb9b7d2ffdc438693aa5d32aca
tnn <tnn@pkgsrc.org>
2017-02-20 09:19:27 +0000

    Fix no-x11 option and add option to disable icu. Bump rev.

--- sysutils/open-vm-tools/options.mk
+++ sysutils/open-vm-tools/options.mk
@@ -1,17 +1,24 @@
-# $NetBSD: options.mk,v 1.4 2016/11/21 00:14:16 pho Exp $
+# $NetBSD: options.mk,v 1.5 2017/02/20 09:19:27 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.open-vm-tools
-PKG_SUPPORTED_OPTIONS=	x11
-PKG_SUGGESTED_OPTIONS=	x11
+PKG_SUPPORTED_OPTIONS=	icu x11
+PKG_SUGGESTED_OPTIONS=	icu x11
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Micu)
+.include "../../textproc/icu/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=		--without-icu
+.endif
 
 .if !empty(PKG_OPTIONS:Mx11)
 PLIST_SRC+=	PLIST.x11
 MESSAGE_SRC+=	MESSAGE.x11
 .  include "../../graphics/gdk-pixbuf2-xlib/buildlink3.mk"
-.  include "../../x11/gtkmm/buildlink3.mk"
 .  include "../../sysutils/desktop-file-utils/desktopdb.mk"
+.  include "../../x11/gtkmm/buildlink3.mk"
+.  include "../../x11/libXtst/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=		--without-x
 .endif
