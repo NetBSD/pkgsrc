42b04b1ef321bc72043e5ec2348bded674bc4855
gdt <gdt@pkgsrc.org>
2017-02-21 15:55:28 +0000

    Fix bug in new patch
    Thanks to Matt Wette for code review on guile-devel@gnu.org.

--- lang/guile20/patches/patch-libguile_filesys.c
+++ lang/guile20/patches/patch-libguile_filesys.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-libguile_filesys.c,v 1.2 2017/02/21 02:32:00 gdt Exp $
+$NetBSD: patch-libguile_filesys.c,v 1.3 2017/02/21 15:55:28 gdt Exp $
 
 When using mkostemp (an interface not defined by POSIX), restrict
 flags to the set defined by the documentation of particular operating
@@ -20,7 +20,7 @@ This patch has not yet been sent upstream.
 +#endif
 +#ifdef __NetBSD__
 +  /* Restrict to list of flags documented in man page. */
-+  open_flags = O_APPEND|O_DIRECT|O_SHLOCK|O_EXLOCK|O_SYNC|O_CLOEXEC;
++  open_flags &= O_APPEND|O_DIRECT|O_SHLOCK|O_EXLOCK|O_SYNC|O_CLOEXEC;
 +#endif
 +
    SCM_SYSCALL (rv = mkostemp (c_tmpl, open_flags));
