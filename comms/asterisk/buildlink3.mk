039b49f91b953e3f763a229d743db1d3546d0893
cherry <cherry@pkgsrc.org>
2017-02-21 05:25:13 +0000

    Add an upper API version restriction. The current only user of this buildlink file is asterisk-chan-dongle (which is yet to be committed). With further users, comms/asterisk may need to find a version specific directory as newer versions are imported.

--- comms/asterisk/buildlink3.mk
+++ comms/asterisk/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/02/10 11:01:48 cherry Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/02/21 05:25:13 cherry Exp $
 
 BUILDLINK_TREE+=	asterisk
 
 .if !defined(ASTERISK_BUILDLINK3_MK)
 ASTERISK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.asterisk+=	asterisk>=11.25.1nb1
+# Current user is comms/asterisk-chan-dongle, which is tied to v11.x
+BUILDLINK_API_DEPENDS.asterisk+=	asterisk>=11.25.1nb1<12
 BUILDLINK_PKGSRCDIR.asterisk?=	../../comms/asterisk
 .endif	# ASTERISK_BUILDLINK3_MK
 
