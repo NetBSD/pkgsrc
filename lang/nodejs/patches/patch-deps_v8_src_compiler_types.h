672dab2e3f3d2810ef9ba04bfc75c711adffd723
fhajny <fhajny@pkgsrc.org>
2017-02-22 11:22:43 +0000

    Update lang/nodejs to 7.6.0.
    Notable changes
    
    - deps:
      - update V8 to 5.5
      - upgrade libuv to 1.11.0
      - add node-inspect 1.10.4
      - upgrade zlib to 1.2.11
    - lib: build node inspect into node
    - crypto: Remove expired certs from CNNIC whitelist
    - inspector: add --inspect-brk
    - fs: allow WHATWG URL objects as paths
    - src: support UTF-8 in compiled-in JS source files
    - url: extend url.format to support WHATWG URL

--- lang/nodejs/patches/patch-deps_v8_src_compiler_types.h
+++ lang/nodejs/patches/patch-deps_v8_src_compiler_types.h
@@ -1,12 +1,12 @@
-$NetBSD: patch-deps_v8_src_types.h,v 1.3 2016/06/02 09:57:32 fhajny Exp $
+$NetBSD: patch-deps_v8_src_compiler_types.h,v 1.1 2017/02/22 11:22:43 fhajny Exp $
 
 NetBSD 6 doesn't have nearbyint/nearbyintf.
 
---- deps/v8/src/types.h.orig	2015-10-29 12:22:04.000000000 +0000
-+++ deps/v8/src/types.h
+--- deps/v8/src/compiler/types.h.orig	2017-02-21 22:53:29.000000000 +0000
++++ deps/v8/src/compiler/types.h
 @@ -5,6 +5,14 @@
- #ifndef V8_TYPES_H_
- #define V8_TYPES_H_
+ #ifndef V8_COMPILER_TYPES_H_
+ #define V8_COMPILER_TYPES_H_
  
 +#ifdef __NetBSD__
 +#include <sys/param.h>
