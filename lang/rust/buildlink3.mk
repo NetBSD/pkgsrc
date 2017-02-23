bf3e5c825b9f0c50d0df529c9bfd31251dd62409
jperkin <jperkin@pkgsrc.org>
2017-02-23 09:35:16 +0000

    Update lang/rust to 1.15.1.  Changes since 1.11.0 are too numerous to list, but can be found here:
    	https://github.com/rust-lang/rust/blob/master/RELEASES.md
    
    Add a stage0-bootstrap target to simplify generation of bootstrap kits
    for platforms which are not supported upstream (primarily SmartOS).

--- lang/rust/buildlink3.mk
+++ lang/rust/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/09/06 10:36:49 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/02/23 09:35:16 jperkin Exp $
 
 BUILDLINK_TREE+=	rust
 
 .if !defined(RUST_BUILDLINK3_MK)
 RUST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rust+=	rust>=0.11.0
+BUILDLINK_API_DEPENDS.rust+=	rust>=1.15.1
 BUILDLINK_PKGSRCDIR.rust?=	../../lang/rust
 
 BUILDLINK_PASSTHRU_DIRS+=	${PREFIX}/lib/rustlib
