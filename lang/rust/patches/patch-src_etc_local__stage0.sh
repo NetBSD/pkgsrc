b02498b18ec5ad5cc2834413f8e906e1cc9c188e
jperkin <jperkin@pkgsrc.org>
2017-02-23 09:35:16 +0000

    Update lang/rust to 1.15.1.  Changes since 1.11.0 are too numerous to list, but can be found here:
    	https://github.com/rust-lang/rust/blob/master/RELEASES.md
    
    Add a stage0-bootstrap target to simplify generation of bootstrap kits
    for platforms which are not supported upstream (primarily SmartOS).

--- lang/rust/patches/patch-src_etc_local__stage0.sh
+++ lang/rust/patches/patch-src_etc_local__stage0.sh
@@ -1,31 +1,23 @@
-$NetBSD: patch-src_etc_local__stage0.sh,v 1.3 2016/09/13 12:37:08 ryoon Exp $
+$NetBSD: patch-src_etc_local__stage0.sh,v 1.4 2017/02/23 09:35:16 jperkin Exp $
 
 -Copy additional libarena required on Darwin.
 -Copy GCC support libraries required on SunOS.
 
---- src/etc/local_stage0.sh.orig	2016-08-16 01:54:35.000000000 +0000
+--- src/etc/local_stage0.sh.orig	2016-12-20 23:34:55.000000000 +0000
 +++ src/etc/local_stage0.sh
 @@ -18,7 +18,7 @@ LIB_PREFIX=lib
  
  OS=`uname -s`
  case $OS in
--    ("Linux"|"FreeBSD"|"DragonFly"|"Bitrig"|"OpenBSD"|"SunOS")
-+    ("Linux"|"FreeBSD"|"DragonFly"|"Bitrig"|"OpenBSD"|"SunOS"|"NetBSD")
+-    ("Linux"|"FreeBSD"|"DragonFly"|"Bitrig"|"OpenBSD"|"SunOS"|"Haiku")
++    ("Linux"|"FreeBSD"|"DragonFly"|"Bitrig"|"OpenBSD"|"SunOS"|"Haiku"|"NetBSD")
      BIN_SUF=
      LIB_SUF=.so
      ;;
-@@ -51,6 +51,7 @@ fi
- 
- cp ${PREFIX}/bin/rustc${BIN_SUF} ${TARG_DIR}/stage0/bin/
- cp ${PREFIX}/${LIB_DIR}/${RUSTLIBDIR}/${TARG_DIR}/${LIB_DIR}/* ${TARG_DIR}/stage0/${LIB_DIR}/
-+cp ${PREFIX}/${LIB_DIR}/${LIB_PREFIX}arena*${LIB_SUF} ${TARG_DIR}/stage0/${LIB_DIR}/
- cp ${PREFIX}/${LIB_DIR}/${LIB_PREFIX}extra*${LIB_SUF} ${TARG_DIR}/stage0/${LIB_DIR}/
- cp ${PREFIX}/${LIB_DIR}/${LIB_PREFIX}rust*${LIB_SUF} ${TARG_DIR}/stage0/${LIB_DIR}/
- cp ${PREFIX}/${LIB_DIR}/${LIB_PREFIX}std*${LIB_SUF} ${TARG_DIR}/stage0/${LIB_DIR}/
-@@ -63,6 +64,9 @@ cp ${PREFIX}/${LIB_DIR}/${LIB_PREFIX}log
- cp ${PREFIX}/${LIB_DIR}/${LIB_PREFIX}rbml*${LIB_SUF} ${TARG_DIR}/stage0/${LIB_DIR}/
+@@ -72,6 +72,9 @@ cp ${PREFIX}/${LIB_DIR}/${LIB_PREFIX}rbm
  cp ${PREFIX}/${LIB_DIR}/${LIB_PREFIX}serialize*${LIB_SUF} ${TARG_DIR}/stage0/${LIB_DIR}/
  cp ${PREFIX}/${LIB_DIR}/${LIB_PREFIX}term*${LIB_SUF} ${TARG_DIR}/stage0/${LIB_DIR}/
+ cp ${PREFIX}/${LIB_DIR}/${LIB_PREFIX}proc_macro*${LIB_SUF} ${TARG_DIR}/stage0/${LIB_DIR}/
 +cp ${PREFIX}/${LIB_DIR}/${LIB_PREFIX}gcc_s*${LIB_SUF}* ${TARG_DIR}/stage0/${LIB_DIR}/
 +cp ${PREFIX}/${LIB_DIR}/${LIB_PREFIX}ssp*${LIB_SUF}* ${TARG_DIR}/stage0/${LIB_DIR}/
 +cp ${PREFIX}/${LIB_DIR}/${LIB_PREFIX}stdc++*${LIB_SUF}* ${TARG_DIR}/stage0/${LIB_DIR}/
--- lang/rust/patches/patch-src_rust-installer_install-template.sh
+++ lang/rust/patches/patch-src_rust-installer_install-template.sh
@@ -1,9 +1,9 @@
-$NetBSD: patch-src_rust-installer_install-template.sh,v 1.1 2016/09/06 10:36:49 jperkin Exp $
+$NetBSD: patch-src_rust-installer_install-template.sh,v 1.2 2017/02/23 09:35:16 jperkin Exp $
 
 Support SunOS.
 Fix undefined variable warnings.
 
---- src/rust-installer/install-template.sh.orig	2016-07-04 18:05:18.000000000 +0000
+--- src/rust-installer/install-template.sh.orig	2016-09-27 07:18:21.000000000 +0000
 +++ src/rust-installer/install-template.sh
 @@ -352,6 +352,10 @@ get_host_triple() {
              _ostype=apple-darwin
@@ -16,12 +16,3 @@ Fix undefined variable warnings.
  	MINGW*)
              _ostype=pc-windows-gnu
              ;;
-@@ -381,7 +385,7 @@ get_host_triple() {
-             ;;
- 
- 	*)
--	    err "unknown value from uname -s: $uname_value"
-+	    err "unknown value from uname -s: $_uname_value"
- 	    ;;
-     esac
- 
