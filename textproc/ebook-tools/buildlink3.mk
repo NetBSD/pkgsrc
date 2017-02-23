51ab9347d5c2a3385da3724d835e9a9a82b6e1e9
wiz <wiz@pkgsrc.org>
2017-02-23 07:44:09 +0000

    Recursive bump for libzip shlib major bump.

--- textproc/ebook-tools/buildlink3.mk
+++ textproc/ebook-tools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2015/05/04 21:14:24 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2017/02/23 07:44:09 wiz Exp $
 
 BUILDLINK_TREE+=	ebook-tools
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ebook-tools
 EBOOK_TOOLS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ebook-tools+=	ebook-tools>=0.1.1
-BUILDLINK_ABI_DEPENDS.ebook-tools+=	ebook-tools>=0.1.1nb5
+BUILDLINK_ABI_DEPENDS.ebook-tools+=	ebook-tools>=0.2.2nb1
 BUILDLINK_PKGSRCDIR.ebook-tools?=	../../textproc/ebook-tools
 
 .include "../../archivers/libzip/buildlink3.mk"
