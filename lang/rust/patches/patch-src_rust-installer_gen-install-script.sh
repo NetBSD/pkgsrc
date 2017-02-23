4b5aec7ba8dbd8689188a93b4085e1d42ff579f8
jperkin <jperkin@pkgsrc.org>
2017-02-23 09:35:16 +0000

    Update lang/rust to 1.15.1.  Changes since 1.11.0 are too numerous to list, but can be found here:
    	https://github.com/rust-lang/rust/blob/master/RELEASES.md
    
    Add a stage0-bootstrap target to simplify generation of bootstrap kits
    for platforms which are not supported upstream (primarily SmartOS).

--- lang/rust/patches/patch-src_rust-installer_gen-install-script.sh
+++ lang/rust/patches/patch-src_rust-installer_gen-install-script.sh
@@ -1,15 +1,9 @@
-$NetBSD: patch-src_rust-installer_gen-install-script.sh,v 1.1 2016/09/06 10:36:49 jperkin Exp $
+$NetBSD: patch-src_rust-installer_gen-install-script.sh,v 1.2 2017/02/23 09:35:16 jperkin Exp $
 
 Do not use /bin/echo, not guaranteed to support -n.
 
 --- src/rust-installer/gen-install-script.sh.orig	2016-07-04 18:05:18.000000000 +0000
 +++ src/rust-installer/gen-install-script.sh
-@@ -1,4 +1,4 @@
--#!/bin/sh
-+#!/bin/bash
- # Copyright 2014 The Rust Project Developers. See the COPYRIGHT
- # file at the top-level directory of this distribution and at
- # http://rust-lang.org/COPYRIGHT.
 @@ -11,7 +11,7 @@
  
  set -u
