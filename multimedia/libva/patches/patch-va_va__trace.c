c796536cacd4de642f4e99dd73a7407bffb48d7d
maya <maya@pkgsrc.org>
2017-02-24 16:44:03 +0000

    libva: clean up va_trace.c patch, hard-code gallium, appease libtool.
    have an else case for the va_trace.c else patch, it seems to work
    fine. not sure if the thread id needs to exist for anything, but
    provide it anyway.
    
    if we don't hard-code galilum, in the case of nouveau, it'll attempt
    to use nouveau - which doesn't define __vaDriverInit_0_39. only gallium
    does, so force it.
    this way it works without specifying env LIBVA_DRIVER_NAME.
    
    don't use -ldl in the makefile, it makes libtool angry (restoring
    old patch-ad), although I haven't checked whether it works without
    the patch.
    
    the available.mk file is probably bogus, and I'd really like to only
    rule out platforms if a problem shows up (rather than assume one exists),
    but I won't change the world order.
    
    tested with mpv --vo=vaapi (after adding libva as a dependency),
    nvidia GTX 770 (nouveau) on NetBSD. it claims to be using vaapi and
    plays a video fine.
    
    bump PKGREVISION.

--- multimedia/libva/patches/patch-va_va__trace.c
+++ multimedia/libva/patches/patch-va_va__trace.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-va_va__trace.c,v 1.1 2017/02/24 03:25:36 maya Exp $
+$NetBSD: patch-va_va__trace.c,v 1.2 2017/02/24 16:44:03 maya Exp $
 
 attempt to provide syscall(__NR_gettid) for most
 systems. inspired by lldb Host.cpp
@@ -18,73 +18,70 @@ systems. inspired by lldb Host.cpp
  #include <unistd.h>
  #include <time.h>
  #include <errno.h>
-@@ -294,7 +300,13 @@ static void add_trace_config_info(
+@@ -235,6 +241,21 @@ struct va_trace {
+ 
+ /* Prototype declarations (functions defined in va.c) */
+ 
++pid_t get_tid() {
++#if defined(__linux__)
++    return syscall(__NR_gettid);
++#elif defined(__NetBSD__)
++    return _lwp_self();
++#elif defined(__FreeBSD__) || defined(__DragonFly__)
++    return pthread_getthreadid_np();
++#elif defined(__OpenBSD__)
++    return syscall(SYS_getthrid);
++#else
++#warning "Don't know how to get thread id"
++    return 0;
++#endif
++}
++
+ void va_errorMessage(const char *msg, ...);
+ void va_infoMessage(const char *msg, ...);
+ 
+@@ -294,7 +315,7 @@ static void add_trace_config_info(
  {
      struct trace_config_info *pconfig_info;
      int idx = 0;
-+#if defined(__linux__)
-     pid_t thd_id = syscall(__NR_gettid);
-+#elif defined(__NetBSD__)
-+    pid_t thd_id = _lwp_self();
-+#elif defined(__FreeBSD__) || defined(__DragonFly__)
-+    pid_t thd_id = pthread_getthreadid_np();
-+#endif
+-    pid_t thd_id = syscall(__NR_gettid);
++    pid_t thd_id = get_tid();
  
      LOCK_RESOURCE(pva_trace);
  
-@@ -318,7 +330,13 @@ static void delete_trace_config_info(
+@@ -318,7 +339,7 @@ static void delete_trace_config_info(
  {
      struct trace_config_info *pconfig_info;
      int idx = 0;
-+#if defined(__linux__)
-     pid_t thd_id = syscall(__NR_gettid);
-+#elif defined(__NetBSD__)
-+    pid_t thd_id = _lwp_self();
-+#elif defined(__FreeBSD__) || defined(__DragonFly__)
-+    pid_t thd_id = pthread_getthreadid_np();
-+#endif
+-    pid_t thd_id = syscall(__NR_gettid);
++    pid_t thd_id = get_tid();
  
      LOCK_RESOURCE(pva_trace);
  
-@@ -665,7 +683,13 @@ static struct trace_log_file *start_trac
+@@ -665,7 +686,7 @@ static struct trace_log_file *start_trac
  {
      struct trace_log_files_manager *plog_files_mgr = NULL;
      struct trace_log_file *plog_file = NULL;
-+#if defined(__linux__)
-     pid_t thd_id = syscall(__NR_gettid);
-+#elif defined(__NetBSD__)
-+    pid_t thd_id = _lwp_self();
-+#elif defined(__FreeBSD__) || defined(__DragonFly__)
-+    pid_t thd_id = pthread_getthreadid_np();
-+#endif
+-    pid_t thd_id = syscall(__NR_gettid);
++    pid_t thd_id = get_tid();
      int i = 0;
  
      LOCK_RESOURCE(pva_trace);
-@@ -704,7 +728,13 @@ static void refresh_log_file(
+@@ -704,7 +725,7 @@ static void refresh_log_file(
      struct trace_context *ptra_ctx)
  {
      struct trace_log_file *plog_file = NULL;
-+#if defined(__linux__)
-     pid_t thd_id = syscall(__NR_gettid);
-+#elif defined(__NetBSD__)
-+    pid_t thd_id = _lwp_self();
-+#elif defined(__FreeBSD__) || defined(__DragonFly__)
-+    pid_t thd_id = pthread_getthreadid_np();
-+#endif
+-    pid_t thd_id = syscall(__NR_gettid);
++    pid_t thd_id = get_tid();
      int i = 0;
  
      plog_file = ptra_ctx->plog_file;
-@@ -1226,7 +1256,13 @@ static void internal_TraceUpdateContext 
+@@ -1226,7 +1247,7 @@ static void internal_TraceUpdateContext 
  {
      struct trace_context *trace_ctx = NULL;
      int i = 0, delete = 1;
-+#if defined(__linux__)
-     pid_t thd_id = syscall(__NR_gettid);
-+#elif defined(__NetBSD__)
-+    pid_t thd_id = _lwp_self();
-+#elif defined(__FreeBSD__) || defined(__DragonFly__)
-+    pid_t thd_id = pthread_getthreadid_np();
-+#endif
+-    pid_t thd_id = syscall(__NR_gettid);
++    pid_t thd_id = get_tid();
  
      if(tra_ctx_idx >= MAX_TRACE_CTX_NUM)
          return;
