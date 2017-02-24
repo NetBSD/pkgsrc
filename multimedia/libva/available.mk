60b01b6bd8a1973a6a328a6da870218888e3170a
maya <maya@pkgsrc.org>
2017-02-24 03:25:36 +0000

    libva: put a temp bandaid on the giant mess I created (sorry) a circular dependency of MesaLib - now libva will not depend on it, this breaks stuff but unbreaks the tree ASAP
    add a patch so va_trace.c should compile on freebsd, dragonflybsd, linux
    and netbsd -- excluding it is a bad idea (undefined references, oops)
    
    indirectly put openbsd as a broken platform for now
    
    still fails at runtime with mpv.
    
    bump PKGREVISION.

--- multimedia/libva/available.mk
+++ multimedia/libva/available.mk
@@ -1,4 +1,4 @@
-# $NetBSD: available.mk,v 1.5 2017/02/24 00:10:04 maya Exp $
+# $NetBSD: available.mk,v 1.6 2017/02/24 03:25:36 maya Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
@@ -7,7 +7,11 @@
 # i.e. in mplayer, xine, xbmc etc.  Have a look at buildlink3.mk too.
 
 # The following should be kept up-to-date!
-.if ${OPSYS} != "Darwin"
+.if ${OPSYS} == "FreeBSD"
+VAAPI_AVAILABLE=	yes
+.elif ${OPSYS} == "NetBSD"
+VAAPI_AVAILABLE=	yes
+.elif ${OPSYS} == "Linux"
 VAAPI_AVAILABLE=	yes
 .else
 VAAPI_AVAILABLE=	no
