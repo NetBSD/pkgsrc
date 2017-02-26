e05bb1b52932336cf579222d6ac686adc7440d7f
adam <adam@pkgsrc.org>
2017-02-26 08:45:44 +0000

    * Noteworthy changes in release 4.10: - Updated gnulib - Removed -Werror from default compiler flags - Fixed undefined behavior when negating integers in _asn1_ltostr().   Issue found by oss-fuzz project (via gnutls):   https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=388 - Pass the correct length to _asn1_get_indefinite_length_string in   asn1_get_length_ber. This addresses reading 1-byte past the end   of data. Issue found by oss-fuzz project (via gnutls):   https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=330   https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=33

--- security/libtasn1/buildlink3.mk
+++ security/libtasn1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:25:20 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2017/02/26 08:45:44 adam Exp $
 
 BUILDLINK_TREE+=	libtasn1
 
@@ -7,7 +7,7 @@ LIBTASN1_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libtasn1+=	libtasn1>=0.2.7
 BUILDLINK_ABI_DEPENDS.libtasn1+=	libtasn1>=0.3.0
-BUILDLINK_PKGSRCDIR.libtasn1?=	../../security/libtasn1
+BUILDLINK_PKGSRCDIR.libtasn1?=		../../security/libtasn1
 .endif # LIBTASN1_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libtasn1
