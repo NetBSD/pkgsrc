efd53bb11615f2845875e9ab8f9c41e1db89fad3
adam <adam@pkgsrc.org>
2017-02-26 10:15:52 +0000

    Changes between 1.3.0 and 1.3.1: * Fix bugs in table: EIT * Fix test_dr

--- multimedia/libdvbpsi/buildlink3.mk
+++ multimedia/libdvbpsi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2013/04/23 09:02:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2017/02/26 10:15:52 adam Exp $
 
 BUILDLINK_TREE+=	libdvbpsi
 
@@ -7,7 +7,7 @@ LIBDVBPSI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libdvbpsi+=	libdvbpsi>=0.1.3
 BUILDLINK_ABI_DEPENDS.libdvbpsi+=	libdvbpsi>=1.1.0
-BUILDLINK_PKGSRCDIR.libdvbpsi?=	../../multimedia/libdvbpsi
+BUILDLINK_PKGSRCDIR.libdvbpsi?=		../../multimedia/libdvbpsi
 .endif # LIBDVBPSI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libdvbpsi
