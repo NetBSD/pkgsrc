b3aeec02c75c2dc5d9ac4205df912ec1ac639238
ryoon <ryoon@pkgsrc.org>
2017-02-28 15:19:58 +0000

    Recursive revbump from graphics/libwebp

--- devel/knotifyconfig/buildlink3.mk
+++ devel/knotifyconfig/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2017/02/12 06:24:39 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2017/02/28 15:19:58 ryoon Exp $
 
 BUILDLINK_TREE+=	knotifyconfig
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	knotifyconfig
 KNOTIFYCONFIG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.knotifyconfig+=	knotifyconfig>=5.21.0
-BUILDLINK_ABI_DEPENDS.knotifyconfig?=	knotifyconfig>=5.25.0nb4
+BUILDLINK_ABI_DEPENDS.knotifyconfig?=	knotifyconfig>=5.25.0nb5
 BUILDLINK_PKGSRCDIR.knotifyconfig?=	../../devel/knotifyconfig
 
 .include "../../devel/kio/buildlink3.mk"
