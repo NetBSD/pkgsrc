429e05469d484ae9f5d71a7f1322f2c35607101f
ryoon <ryoon@pkgsrc.org>
2017-02-28 15:19:58 +0000

    Recursive revbump from graphics/libwebp

--- graphics/gdchart/buildlink3.mk
+++ graphics/gdchart/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2016/08/03 10:22:14 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2017/02/28 15:19:59 ryoon Exp $
 
 BUILDLINK_TREE+=	gdchart
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gdchart
 GDCHART_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gdchart+=	gdchart>=0.11.4nb1
-BUILDLINK_ABI_DEPENDS.gdchart+=	gdchart>=0.11.5nb17
+BUILDLINK_ABI_DEPENDS.gdchart+=	gdchart>=0.11.5nb18
 BUILDLINK_PKGSRCDIR.gdchart?=	../../graphics/gdchart
 
 .include "../../graphics/gd/buildlink3.mk"
--- x11/qt5-qtwebkit/buildlink3.mk
+++ x11/qt5-qtwebkit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.27 2017/02/12 06:24:59 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2017/02/28 15:20:01 ryoon Exp $
 
 BUILDLINK_TREE+=	qt5-qtwebkit
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qt5-qtwebkit
 QT5_QTWEBKIT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt5-qtwebkit+=	qt5-qtwebkit>=5.5.1
-BUILDLINK_ABI_DEPENDS.qt5-qtwebkit+=	qt5-qtwebkit>=5.5.1nb9
+BUILDLINK_ABI_DEPENDS.qt5-qtwebkit+=	qt5-qtwebkit>=5.5.1nb10
 BUILDLINK_PKGSRCDIR.qt5-qtwebkit?=	../../x11/qt5-qtwebkit
 
 BUILDLINK_INCDIRS.qt5-qtwebkit+=	qt5/include
