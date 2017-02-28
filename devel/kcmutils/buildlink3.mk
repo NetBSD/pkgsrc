a114b0c6fa6f64b763c05645ff3364f5bfaf5216
ryoon <ryoon>
2017-02-28 15:19:58 +0000

    Recursive revbump from graphics/libwebp

--- devel/kcmutils/buildlink3.mk
+++ devel/kcmutils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2017/02/12 06:24:38 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2017/02/28 15:19:58 ryoon Exp $
 
 BUILDLINK_TREE+=	kcmutils
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kcmutils
 KCMUTILS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kcmutils+=	kcmutils>=5.21.0
-BUILDLINK_ABI_DEPENDS.kcmutils?=	kcmutils>=5.25.0nb4
+BUILDLINK_ABI_DEPENDS.kcmutils?=	kcmutils>=5.25.0nb5
 BUILDLINK_PKGSRCDIR.kcmutils?=	../../devel/kcmutils
 
 .include "../../devel/kdeclarative/buildlink3.mk"
--- www/khtml/buildlink3.mk
+++ www/khtml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2017/02/12 06:24:51 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2017/02/28 15:20:00 ryoon Exp $
 
 BUILDLINK_TREE+=	khtml
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	khtml
 KHTML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.khtml+=	khtml>=5.21.0
-BUILDLINK_ABI_DEPENDS.khtml?=	khtml>=5.25.0nb4
+BUILDLINK_ABI_DEPENDS.khtml?=	khtml>=5.25.0nb5
 BUILDLINK_PKGSRCDIR.khtml?=	../../www/khtml
 
 .include "../../graphics/giflib/buildlink3.mk"
