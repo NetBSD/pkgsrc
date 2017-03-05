4a160daff58f7bb255ac788f1a7cc2c388339f90
wiz <wiz@pkgsrc.org>
2017-03-05 12:49:51 +0000

    Updated jasper to 2.0.12.
        Bumped the version number.
    
        Added some additional checking to prevent a potential integer overflow
        due to conversion in the JPC decoder.
    
        Added numerous more-detailed error messages for the JPC and JP2
        codecs.
        Added a partial verbose capability for the run_test_1 script.
        Moved a test case from the bad category to the good category, as the
        test case had been miscategorized.
    
    
        Added another regression test case.
    
        Fixed some potential double-free problems in the JPC codec.

--- graphics/jasper/patches/patch-src_libjasper_jpc_jpc__enc.c
+++ graphics/jasper/patches/patch-src_libjasper_jpc_jpc__enc.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_libjasper_jpc_jpc__enc.c,v 1.2 2016/12/16 09:44:44 he Exp $
+$NetBSD: patch-src_libjasper_jpc_jpc__enc.c,v 1.3 2017/03/05 12:49:52 wiz Exp $
 
 Replace an sprintf() with snprintf().
 
---- src/libjasper/jpc/jpc_enc.c.orig	2007-01-19 21:43:07.000000000 +0000
+--- src/libjasper/jpc/jpc_enc.c.orig	2017-03-02 18:03:10.000000000 +0000
 +++ src/libjasper/jpc/jpc_enc.c
-@@ -957,7 +957,8 @@ startoff = jas_stream_getrwcount(enc->ou
- 	if (!(enc->mrk = jpc_ms_create(JPC_MS_COM))) {
+@@ -966,7 +966,8 @@ startoff = jas_stream_getrwcount(enc->ou
+ 		jas_eprintf("cannot create COM marker\n");
  		return -1;
  	}
 -	sprintf(buf, "Creator: JasPer Version %s", jas_getversion());
