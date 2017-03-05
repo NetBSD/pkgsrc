682ba1f56ec944442edbf6e64f98ca4d3ecbe5f0
wiz <wiz@pkgsrc.org>
2017-03-05 12:49:51 +0000

    Updated jasper to 2.0.12.
        Bumped the version number.
    
        Added some additional checking to prevent a potential integer overflow
        due to conversion in the JPC decoder.
    
        Added numerous more-detailed error messages for the JPC and JP2
        codecs.
        Added a partial verbose capability for the run_test_1 script.
        Moved a test case from the bad category to the good category, as the
        test case had been miscategorized.
    
    
        Added another regression test case.
    
        Fixed some potential double-free problems in the JPC codec.

--- graphics/jasper/patches/patch-src_libjasper_jp2_jp2__enc.c
+++ graphics/jasper/patches/patch-src_libjasper_jp2_jp2__enc.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_libjasper_jp2_jp2__enc.c,v 1.2 2016/12/16 09:44:44 he Exp $
+$NetBSD: patch-src_libjasper_jp2_jp2__enc.c,v 1.3 2017/03/05 12:49:52 wiz Exp $
 
 Replace an sprintf() with snprintf().
 
---- src/libjasper/jp2/jp2_enc.c.orig	2007-01-19 21:43:05.000000000 +0000
+--- src/libjasper/jp2/jp2_enc.c.orig	2017-03-02 18:03:10.000000000 +0000
 +++ src/libjasper/jp2/jp2_enc.c
-@@ -343,8 +343,9 @@ int jp2_encode(jas_image_t *image, jas_s
+@@ -385,8 +385,9 @@ int jp2_encode(jas_image_t *image, jas_s
  	/* Output the JPEG-2000 code stream. */
  
  	overhead = jas_stream_getrwcount(out);
@@ -15,4 +15,4 @@ Replace an sprintf() with snprintf().
 +		 (unsigned long) overhead);
  
  	if (jpc_encode(image, out, buf)) {
- 		goto error;
+ 		jas_eprintf("jpc_encode failed\n");
