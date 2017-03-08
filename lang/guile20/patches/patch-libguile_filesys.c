19daff45f33a1a6eed04acc1c60433ff02b204b6
jperkin <jperkin>
2017-03-08 14:38:34 +0000

    Pull in upstream fix for mkostemp issue, should fix SunOS and PR#52035.

--- lang/guile20/patches/patch-libguile_filesys.c
+++ lang/guile20/patches/patch-libguile_filesys.c
@@ -1,28 +1,19 @@
-$NetBSD: patch-libguile_filesys.c,v 1.3 2017/02/21 15:55:28 gdt Exp $
+$NetBSD: patch-libguile_filesys.c,v 1.4 2017/03/08 14:38:34 jperkin Exp $
 
-When using mkostemp (an interface not defined by POSIX), restrict
-flags to the set defined by the documentation of particular operating
-systems.
-
-See https://debbugs.gnu.org/cgi/bugreport.cgi?bug=24862#23 for discussion.
-
-This patch has not yet been sent upstream.
+Pull in upstream patch to fix mkostemp flags.
 
 --- libguile/filesys.c.orig	2016-12-15 00:03:33.000000000 +0000
 +++ libguile/filesys.c
-@@ -1486,6 +1486,15 @@ SCM_DEFINE (scm_i_mkstemp, "mkstemp!", 1
+@@ -1483,6 +1483,12 @@ SCM_DEFINE (scm_i_mkstemp, "mkstemp!", 1
+   else
+     {
+       open_flags = scm_i_mode_to_open_flags (mode, &is_binary, FUNC_NAME);
++      /* mkostemp(2) only defines O_APPEND, O_SYNC, and O_CLOEXEC to be
++         useful, as O_RDWR|O_CREAT|O_EXCL are implicitly added.  It also
++         notes that other flags may error on some systems, which turns
++         out to be the case.  Of those flags, O_APPEND is the only one
++         of interest anyway, so limit to that flag.  */
++      open_flags &= O_APPEND;
        mode_bits = scm_i_mode_bits (mode);
      }
  
-+#ifdef __APPLE__
-+  /* https://debbugs.gnu.org/cgi/bugreport.cgi?bug=24862#23 */
-+  open_flags &= O_APPEND|O_SHLOCK|O_EXLOCK|O_CLOEXEC;
-+#endif
-+#ifdef __NetBSD__
-+  /* Restrict to list of flags documented in man page. */
-+  open_flags &= O_APPEND|O_DIRECT|O_SHLOCK|O_EXLOCK|O_SYNC|O_CLOEXEC;
-+#endif
-+
-   SCM_SYSCALL (rv = mkostemp (c_tmpl, open_flags));
-   if (rv == -1)
-     SCM_SYSERROR;
