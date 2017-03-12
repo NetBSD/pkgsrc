ccf8593b08494ca2c8bbb3b20ca383ed10fc0a51
spz <spz>
2017-03-12 19:55:49 +0000

    fix a few warnings This is likely the last change for devel/rt3, consider updating to devel/rt4 eventually

--- devel/rt3/patches/patch-lib_RT.pm
+++ devel/rt3/patches/patch-lib_RT.pm
@@ -1,7 +1,8 @@
-$NetBSD: patch-lib_RT.pm,v 1.2 2015/03/01 22:45:26 spz Exp $
+$NetBSD: patch-lib_RT.pm,v 1.3 2017/03/12 19:55:49 spz Exp $
 
 perl 5.14 qw() in for* fixes
 Fix for CVE-2014-9472 taken from the patch for RT 4.0.0
+Be more defensive about Cwd::realpath($_) actually existing.
 
 --- lib/RT.pm.orig	2013-05-22 19:04:26.000000000 +0000
 +++ lib/RT.pm
@@ -32,3 +33,13 @@ Fix for CVE-2014-9472 taken from the patch for RT 4.0.0
  
      if ( $args{'Heavy'} ) {
          # load scrips' modules
+@@ -641,7 +643,8 @@ sub InitPluginPaths {
+     my @tmp_inc;
+     my $added;
+     for (@INC) {
+-        if ( Cwd::realpath($_) eq $RT::LocalLibPath) {
++        if ( defined Cwd::realpath($_) && Cwd::realpath($_) eq $RT::LocalLibPat
++h) {
+             push @tmp_inc, $_, @lib_dirs;
+             $added = 1;
+         } else {
