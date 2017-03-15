1992040ddb079893644ecfd41c3973a39d9843c3
mef <mef@pkgsrc.org>
2017-03-15 14:07:12 +0000

    Updated ham/gnuradio from 3.7.10.1 to 3.7.11 -------------------------------------------- ChangeLog v3.7.11
    This is a feature release of the 3.7 API series, and incorporates all
    the bug fixes implemented in the 3.7.10.2 maintenance release.
    
    Contributors
        (omitted in this commit log, sorry)
    
    Changes
    
    The GNU Radio project tracks changes via Github pull requests. You can
    get details on each of the below by going to:
    
    https://github.com/gnuradio/gnuradio
    
    Note: Please see the release notes for 3.7.10.2 for details on the bug
    fixes included in this release.
    
    gnuradio-runtime
        #1077 Support dynamically loaded gnuradio installs (Josh Blum)
    
    gnuradio-companion
        #1118 Support vector types in embedded Python blocks (Clayton Smith)
    
    gr-audio
        #1051 Re-implemented defunct Windows audio source (Geof Gnieboer)
        #1052 Implemented block in Windows audio sink (Geof Gnieboer)
    
    gr-blocks
        #896 Added PDU block setters and GRC callbacks (Jacob Gilbert)
        #900 Exposed non-vector multiply const to GRC (Ron Economos)
        #903 Deprecated old-style message queue blocks (Johnathan Corgan)
        #1067 Deprecated blks2 namespace blocks (Johnathan Corgan)
    
    gr-digital
        #910 Deprecated correlateandsync block 3.8 (Johnathan Corgan)
        #912 Deprecated modulation blocks for 3.8 (Sebastian Müller)
        #1069 Improved build memory usage with swig split (Michael Dickens)
        #1097 Deprecated mpskreceivercc block (Johnathan Corgan)
        #1099 Deprecated old-style OFDM receiver blocks (Martin Braun)
    
    gr-dtv
        #875 Added ability to cross-compile gr-dtv (Ron Economos)
        #876 Improved ATSC transmitter performance (Ron Economos)
        #894 Refactored DVB-T RS decoder to use gr-fec (Ron Economos)
        #898 Improved error handling and logging (Ron Economos)
        #900 Improved DVB-T performance (Ron Economos)
        #907 Updated examples to use QT (Ron Economos)
        #1025 Refactor DVB-T2 interleaver (Ron Economos)
    
    gr-filter
        #885 Added set parameter msg port to fractional resampler (Sebastian Müller)
    
    gr-trellis
        #908 Updated examples to use QT (Martin Braun)
    
    gr-uhd
        #872 Added relative phase plots to uhd_fft (Martin Braun)
        #1032 Replace zero-timeout double-recv() with one recv() (Martin Braun)
        #1053 UHD apps may now specify multiple subdevs (Martin Braun)
        #1101 Support TwinRX LO sharing parameters (Andrej Rode)
        #1139 Use UHD internal normalized gain methods (Martin Braun)
    
    gr-utils
        #897 Improved python docstring generation in gr_modtool

--- ham/gnuradio-core/buildlink3.mk
+++ ham/gnuradio-core/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.28 2014/10/17 09:51:56 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.29 2017/03/15 14:07:12 mef Exp $
 
 BUILDLINK_TREE+=	gnuradio-core
 
 .if !defined(GNURADIO_CORE_BUILDLINK3_MK)
 GNURADIO_CORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-core+=	gnuradio-core>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-core+=	gnuradio-core>=3.7.5
+BUILDLINK_API_DEPENDS.gnuradio-core+=	gnuradio-core>=3.7.11
+BUILDLINK_ABI_DEPENDS.gnuradio-core+=	gnuradio-core>=3.7.11
 BUILDLINK_PKGSRCDIR.gnuradio-core?=	../../ham/gnuradio-core
 .endif # GNURADIO_CORE_BUILDLINK3_MK
 
