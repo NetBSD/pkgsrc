3205530fc8c538bbcbb403cfa40ab650689edac2
bsiegert <bsiegert@pkgsrc.org>
2017-03-15 19:38:09 +0000

    Update Go to 1.8.
    The compiler back end introduced in Go 1.7 for 64-bit x86 is now used on all
    architectures, and those architectures should see significant performance
    improvements. For instance, the CPU time required by our benchmark programs was
    reduced by 20-30% on 32-bit ARM systems. There are also some modest performance
    improvements in this release for 64-bit x86 systems. The compiler and linker
    have been made faster. Compile times should be improved by about 15% over Go
    1.7. There is still more work to be done in this area: expect faster
    compilation speeds in future releases.
    
    Garbage collection pauses should be significantly shorter, usually under 100
    microseconds and often as low as 10 microseconds.
    
    The HTTP server adds support for HTTP/2 Push, allowing servers to preemptively
    send responses to a client. This is useful for minimizing network latency by
    eliminating roundtrips. The HTTP server also adds support for graceful
    shutdown, allowing servers to minimize downtime by shutting down only after
    serving all requests that are in flight.
    
    Contexts (added to the standard library in Go 1.7) provide a cancelation and
    timeout mechanism. Go 1.8 adds support for contexts in more parts of the
    standard library, including the database/sql and net packages and
    Server.Shutdown in the net/http package.
    
    Go 1.8 includes many more additions, improvements, and fixes. Find the complete
    set of changes, and more information about the improvements listed above, in
    the Go 1.8 release notes: https://golang.org/doc/go1.8

--- lang/go/patches/patch-lib_time_update.bash
+++ lang/go/patches/patch-lib_time_update.bash
@@ -1,8 +1,8 @@
-$NetBSD: patch-lib_time_update.bash,v 1.3 2016/09/17 15:56:58 bsiegert Exp $
+$NetBSD: patch-lib_time_update.bash,v 1.4 2017/03/15 19:38:09 bsiegert Exp $
 
---- lib/time/update.bash.orig	2016-09-07 19:34:33.000000000 +0000
+--- lib/time/update.bash.orig	2015-12-03 00:52:58.000000000 +0000
 +++ lib/time/update.bash
-@@ -42,7 +42,7 @@ zip -0 -r ../../zoneinfo.zip *
+@@ -41,7 +41,7 @@ zip -0 -r ../../zoneinfo.zip *
  cd ../..
  
  echo
