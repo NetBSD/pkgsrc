4e7ce5271c20993b956bc360f3073b01b9d467f0
maya <maya@pkgsrc.org>
2017-03-15 11:33:03 +0000

    octave: amend wrapper patches to be netbsd-specific. fix sunOS build
    octave is using gnulib for portability, but has an additional wrapper
    around it. unfortunately that means that this type signature is no longer
    portable. netbsd doesn't have struct tm_zone, others do.
    
    I failed to use gnulib to provide struct tm_zone, using an OS-specific macro
    as an ugly hack.
    
    reported by hans

--- math/octave/patches/patch-liboctave_wrappers_strftime-wrapper.h
+++ math/octave/patches/patch-liboctave_wrappers_strftime-wrapper.h
@@ -1,15 +1,19 @@
-$NetBSD: patch-liboctave_wrappers_strftime-wrapper.h,v 1.1 2016/11/19 21:53:12 maya Exp $
+$NetBSD: patch-liboctave_wrappers_strftime-wrapper.h,v 1.2 2017/03/15 11:33:03 maya Exp $
 
-Don't have struct tm_zone (in netbsd), instead use timezone_t as libgnu does
+Octave's wrapper breaks gnulib portability, netbsd doesn't have struct tm_zone,
+use ifdefs to work around it
 
 --- liboctave/wrappers/strftime-wrapper.h.orig	2016-11-13 15:16:10.000000000 +0000
 +++ liboctave/wrappers/strftime-wrapper.h
-@@ -35,7 +35,7 @@ extern "C" {
+@@ -35,7 +35,11 @@ extern "C" {
  
  extern size_t
  octave_strftime_wrapper (char *buf, size_t len, const char *fmt,
--                         const struct tm *t, struct tm_zone *tz, int ns);
++#ifdef __NetBSD__
 +                         const struct tm *t, timezone_t *tz, int ns);
++#else
+                          const struct tm *t, struct tm_zone *tz, int ns);
++#endif
  
  #if defined __cplusplus
  }
