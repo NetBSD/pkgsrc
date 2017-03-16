edcfb99c34dcd5f14eb1ab5149e1bbd7009468ac
wiz <wiz@pkgsrc.org>
2017-03-16 10:02:57 +0000

    Import guile22-2.2.0 as lang/guile22.
    GUILE, GNU's Ubiquitous Intelligent Language for Extension, is a library
    that implements the Scheme language plus various convenient facilities.
    It's designed so that you can link it into an application or utility to
    make it extensible. Our plan is to link this library into all GNU programs
    that call for extensibility.
    
    This is guile 2.2, the current stable version.

---
+++ lang/guile22/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink3.mk,v 1.1 2017/03/16 10:02:57 wiz Exp $
+
+BUILDLINK_TREE+=	guile22
+
+.if !defined(GUILE22_BUILDLINK3_MK)
+GUILE22_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.guile22+=	guile22>=2.2
+BUILDLINK_PKGSRCDIR.guile22?=	../../lang/guile22
+
+BUILDLINK_PASSTHRU_DIRS=		${LOCALBASE}/guile/2.2
+BUILDLINK_FILES.guile22+=		guile/2.2/bin/*
+BUILDLINK_FNAME_TRANSFORM.guile22+=	-e s,guile/2.2/bin,bin,
+BUILDLINK_FNAME_TRANSFORM.guile22+=	-e s,guile/2.2/lib/pkgconfig,lib/pkgconfig,
+
+GUILE22_SUBDIR=				guile/2.2
+
+.include "../../devel/gmp/buildlink3.mk"
+.include "../../devel/libffi/buildlink3.mk"
+.include "../../devel/libltdl/buildlink3.mk"
+.include "../../devel/readline/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../textproc/libunistring/buildlink3.mk"
+.include "../../devel/boehm-gc/buildlink3.mk"
+.endif # GUILE22_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-guile22
