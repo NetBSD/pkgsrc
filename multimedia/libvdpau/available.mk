278bd860d2ccb3667de57d0ce383c179ec56192b
maya <maya@pkgsrc.org>
2017-03-16 19:21:09 +0000

    libvdpau: add an option 'x11' (default enabled) in available.mk, so non-X11 users can avoid all of Xorg being pulled in for a dependency with ease, without needing to know about vdpau, for example in the case of ffmpeg3 which shouldn't require X11.
    Feel free to propose alternatives.
    
    Clean some commented out bits.
    
    PR pkg/52021

--- multimedia/libvdpau/available.mk
+++ multimedia/libvdpau/available.mk
@@ -1,24 +1,16 @@
-# $NetBSD: available.mk,v 1.5 2017/01/23 18:20:59 adam Exp $
+# $NetBSD: available.mk,v 1.6 2017/03/16 19:21:09 maya Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
-# At the moment VDPAU is available only for Linux, FreeBSD and
-# [Open]Solaris.  The following condition is here to NOT list these
-# platforms in multiple places, i.e. in mplayer, xine, xbmc etc.
-# Have a look at buildlink3.mk too.
+PKG_SUPPORTED_OPTIONS+=	x11
+PKG_SUGGESTED_OPTIONS=	x11
 
-#.if ${OPSYS} == "Linux" && (${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64")
-#VDPAU_AVAILABLE=	yes
-#.elif ${OPSYS} == "FreeBSD" && (${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64")
-#VDPAU_AVAILABLE=	yes
-#.elif ${OPSYS} == "SunOS" && (${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64") \
-#  && !empty(OS_VERSION:M5.1[0-9]*)
-#VDPAU_AVAILABLE=	yes
-#.else
+.include "../../mk/bsd.options.mk"
+
 VDPAU_AVAILABLE=	no
-#.endif
 
-# XXX even these restrictions seem arbitrary
+.if !empty(PKG_OPTIONS:Mx11)
 .if ${OPSYS} != "Darwin" && (${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64")
 VDPAU_AVAILABLE=	yes
+.endif
 .endif
