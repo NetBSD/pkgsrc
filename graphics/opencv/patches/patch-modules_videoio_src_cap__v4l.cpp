779d318fd31b545a0f51f176d9296a890b57caa9
prlw1 <prlw1@pkgsrc.org>
2017-03-16 21:59:13 +0000

    Update opencv to 3.2
    Many Darwin library handling patches removed because of commit 912592de4ce
      Remove "INSTALL_NAME_DIR lib" target property
    
    Full changelog at
    
    https://github.com/opencv/opencv/wiki/ChangeLog
    
    Highlights:
    
         * Results from 11 GSoC 2016 projects have been submitted to the
           library, 9 of them have been integrated already, 2 still pending
           (the numbers below are the id's of the Pull Requests in opencv or
           opencv_contrib repository):
              + Ambroise Moreau (Delia Passalacqua) - sinusoidal patterns for
                structured light and phase unwrapping module (711)
              + Alexander Bokov (Maksim Shabunin) - DIS optical flow
                (excellent dense optical flow algorithm that is both
                significantly better and significantly faster than Farneback's
                algorithm - our baseline), and learning-based color constancy
                algorithms implementation (689, 708, 722, 736, 745, 747)
              + Tyan Vladimir (Antonella Cascitelli) - CNN based tracking
                algorithm (GOTURN) (718, 899)
              + Vladislav Samsonov (Ethan Rublee) - PCAFlow and Global Patch
                Collider algorithms implementation (710, 752)
              + Jo o Cartucho (Vincent Rabaud) - Multi-language OpenCV
                Tutorials in Python, C++ and Java (7041)
              + Jiri Horner (Bo Li) - New camera model and parallel processing
                for stitching pipeline (6933)
              + Vitaliy Lyudvichenko (Anatoly Baksheev) - Optimizations and
                improvements of dnn module (707, 750)
              + Iric Wu (Vadim Pisarevsky) - Base64 and JSON support for file
                storage (6697, 6949, 7088). Use names like
                `"myfilestorage.xml?base64"` when writing file storage to
                store big chunks of numerical data in base64-encoded form.
              + Edgar Riba (Manuele Tamburrano, Stefano Fabri) - tiny_dnn
                improvements and integration (720: pending)
              + Yida Wang (Manuele Tamburrano, Stefano Fabri) - Quantization
                and semantic saliency detection with tiny_dnn
              + Anguelos Nicolaou (Lluis Gomez) - Word-spotting CNN based
                algorithm (761: pending)

--- graphics/opencv/patches/patch-modules_videoio_src_cap__v4l.cpp
+++ graphics/opencv/patches/patch-modules_videoio_src_cap__v4l.cpp
@@ -1,6 +1,8 @@
-$NetBSD: patch-modules_videoio_src_cap__v4l.cpp,v 1.1 2016/03/01 20:14:24 joerg Exp $
+$NetBSD: patch-modules_videoio_src_cap__v4l.cpp,v 1.2 2017/03/16 21:59:13 prlw1 Exp $
 
---- modules/videoio/src/cap_v4l.cpp.orig	2015-12-18 15:02:16.000000000 +0000
+Conditionalize settings not available in NetBSD's v4l2 emulation.
+
+--- modules/videoio/src/cap_v4l.cpp.orig	2016-12-23 12:54:44.000000000 +0000
 +++ modules/videoio/src/cap_v4l.cpp
 @@ -216,6 +216,7 @@ make & enjoy!
  #include <fcntl.h>
@@ -10,20 +12,31 @@ $NetBSD: patch-modules_videoio_src_cap__v4l.cpp,v 1.1 2016/03/01 20:14:24 joerg 
  #include <sys/types.h>
  #include <sys/mman.h>
  
-@@ -462,7 +463,9 @@ static int autosetup_capture_mode_v4l2(C
+@@ -469,9 +470,13 @@ static int autosetup_capture_mode_v4l2(C
              V4L2_PIX_FMT_UYVY,
              V4L2_PIX_FMT_SN9C10X,
              V4L2_PIX_FMT_SBGGR8,
 +#ifdef V4L2_PIX_FMT_SGBRG8
              V4L2_PIX_FMT_SGBRG8,
+-            V4L2_PIX_FMT_RGB24,
+-            V4L2_PIX_FMT_Y16
 +#endif
-             V4L2_PIX_FMT_RGB24
++            V4L2_PIX_FMT_RGB24
++#ifdef V4L2_PIX_FMT_Y16
++            ,V4L2_PIX_FMT_Y16
++#endif
      };
  
-@@ -515,9 +518,11 @@ static void v4l2_control_range(CvCapture
-     case V4L2_CID_EXPOSURE:
+     for (size_t i = 0; i < sizeof(try_order) / sizeof(__u32); i++) {
+@@ -520,12 +525,16 @@ static void v4l2_control_range(CvCapture
+     case V4L2_CID_GAIN:
+         cap->gain = range;
+         break;
++#ifdef V4L2_CID_EXPOSURE_ABSOLUTE
+     case V4L2_CID_EXPOSURE_ABSOLUTE:
          cap->exposure = range;
          break;
++#endif
 +#ifdef V4L2_CID_FOCUS_ABSOLUTE
      case V4L2_CID_FOCUS_ABSOLUTE:
          cap->focus = range;
@@ -32,7 +45,7 @@ $NetBSD: patch-modules_videoio_src_cap__v4l.cpp,v 1.1 2016/03/01 20:14:24 joerg 
      }
  }
  
-@@ -539,7 +544,9 @@ static void v4l2_scan_controls(CvCapture
+@@ -547,7 +556,9 @@ static void v4l2_scan_controls(CvCapture
          break;
    }
  
@@ -42,7 +55,32 @@ $NetBSD: patch-modules_videoio_src_cap__v4l.cpp,v 1.1 2016/03/01 20:14:24 joerg 
  }
  
  static int v4l2_set_fps(CvCaptureCAM_V4L* capture) {
-@@ -1523,12 +1530,14 @@ static IplImage* icvRetrieveFrameCAM_V4L
+@@ -563,8 +574,10 @@ static int v4l2_num_channels(__u32 palet
+     case V4L2_PIX_FMT_YVU420:
+     case V4L2_PIX_FMT_MJPEG:
+     case V4L2_PIX_FMT_JPEG:
++#ifdef V4L2_PIX_FMT_Y16
+     case V4L2_PIX_FMT_Y16:
+         return 1;
++#endif
+     case V4L2_PIX_FMT_YUYV:
+     case V4L2_PIX_FMT_UYVY:
+         return 2;
+@@ -592,11 +605,13 @@ static void v4l2_create_frame(CvCaptureC
+         case V4L2_PIX_FMT_YVU420:
+             size.height = size.height * 3 / 2; // "1.5" channels
+             break;
++#ifdef V4L2_PIX_FMT_Y16
+         case V4L2_PIX_FMT_Y16:
+             if(!capture->convert_rgb){
+                 depth = IPL_DEPTH_16U;
+             }
+             break;
++#endif
+         }
+     }
+ 
+@@ -1555,18 +1570,21 @@ static IplImage* icvRetrieveFrameCAM_V4L
                  (unsigned char*)capture->frame.imageData);
          break;
  
@@ -57,10 +95,33 @@ $NetBSD: patch-modules_videoio_src_cap__v4l.cpp,v 1.1 2016/03/01 20:14:24 joerg 
      case V4L2_PIX_FMT_RGB24:
          rgb24_to_rgb24(capture->form.fmt.pix.width,
                  capture->form.fmt.pix.height,
-@@ -1554,10 +1563,14 @@ static inline __u32 capPropertyToV4L2(in
+                 (unsigned char*)capture->buffers[(capture->bufferIndex+1) % capture->req.count].start,
+                 (unsigned char*)capture->frame.imageData);
+         break;
++#ifdef V4L2_PIX_FMT_Y16
+     case V4L2_PIX_FMT_Y16:
+         if(capture->convert_rgb){
+             y16_to_rgb24(capture->form.fmt.pix.width,
+@@ -1579,6 +1597,7 @@ static IplImage* icvRetrieveFrameCAM_V4L
+                    capture->frame.imageSize);
+         }
+         break;
++#endif
+     }
+ 
+     return(&capture->frame);
+@@ -1596,14 +1615,22 @@ static inline __u32 capPropertyToV4L2(in
+         return V4L2_CID_HUE;
+     case CV_CAP_PROP_GAIN:
          return V4L2_CID_GAIN;
++#ifdef V4L2_CID_EXPOSURE_AUTO
+     case CV_CAP_PROP_AUTO_EXPOSURE:
+         return V4L2_CID_EXPOSURE_AUTO;
++#endif
++#ifdef V4L2_CID_EXPOSURE_ABSOLUTE
      case CV_CAP_PROP_EXPOSURE:
-         return V4L2_CID_EXPOSURE;
+         return V4L2_CID_EXPOSURE_ABSOLUTE;
++#endif
 +#ifdef V4L2_CID_FOCUS_AUTO
      case CV_CAP_PROP_AUTOFOCUS:
          return V4L2_CID_FOCUS_AUTO;
@@ -72,3 +133,18 @@ $NetBSD: patch-modules_videoio_src_cap__v4l.cpp,v 1.1 2016/03/01 20:14:24 joerg 
      default:
          return -1;
      }
+@@ -1749,12 +1776,14 @@ static bool icvSetControl (CvCaptureCAM_
+         return false;
+     }
+ 
++#if defined(V4L2_CID_EXPOSURE_AUTO) && defined(V4L2_EXPOSURE_MANUAL) && defined(V4L2_CID_EXPOSURE_ABSOLUTE)
+     if(control.id == V4L2_CID_EXPOSURE_AUTO && control.value == V4L2_EXPOSURE_MANUAL) {
+         // update the control range for expose after disabling autoexposure
+         // as it is not read correctly at startup
+         // TODO check this again as it might be fixed with Linux 4.5
+         v4l2_control_range(capture, V4L2_CID_EXPOSURE_ABSOLUTE);
+     }
++#endif
+ 
+     /* all was OK */
+     return true;
