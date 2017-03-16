a3c69982d1d4c6a77300b5b0008e4164a54f598b
gdt <gdt@pkgsrc.org>
2017-03-16 00:34:46 +0000

    Add gpgme to gimem's bl3
    While arguably the real bug is that gmime's pkg-config file libs
    includes gpgme's libs, this makes libraries visible to depending
    builds that end up on link lines.  Resolves build failure for
    totem-pl-parser.
    
    ok wiz@

--- mail/gmime/buildlink3.mk
+++ mail/gmime/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2013/07/13 19:23:36 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2017/03/16 00:34:46 gdt Exp $
 
 BUILDLINK_TREE+=	gmime
 
@@ -9,6 +9,10 @@ BUILDLINK_API_DEPENDS.gmime+=	gmime>=2.6.15
 BUILDLINK_PKGSRCDIR.gmime?=	../../mail/gmime
 
 .include "../../devel/glib2/buildlink3.mk"
+# gmime-2.6.pc specifies linking with gpgme's libs.  While this is arguably
+# wrong and should perhaps be patched, until then we should include them
+# here.
+.include "../../security/gpgme/buildlink3.mk"
 .endif # GMIME_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gmime
