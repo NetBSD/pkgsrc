07b1458a42ef2cbbeea4b9804146f94028655e58
bsiegert <bsiegert@pkgsrc.org>
2017-03-16 19:33:50 +0000

    Update go-tools to 1.8 release as part of PR pkg/52081.
    No changelog from upstream. There is now a "goyacc" tool to replace
    "go tool yacc" from the main repo.

--- devel/go-tools/buildlink3.mk
+++ devel/go-tools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/12/30 14:07:58 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/03/16 19:33:50 bsiegert Exp $
 
 BUILDLINK_TREE+=	go-tools
 
@@ -8,7 +8,7 @@ GO_TOOLS_BUILDLINK3_MK:=
 BUILDLINK_CONTENTS_FILTER.go-tools=	${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-tools?=		build
 
-BUILDLINK_API_DEPENDS.go-tools+=	go-tools>=1.5
+BUILDLINK_API_DEPENDS.go-tools+=	go-tools>=1.8
 BUILDLINK_PKGSRCDIR.go-tools?=	../../devel/go-tools
 
 .include "../../devel/google-api-go-client/buildlink3.mk"
