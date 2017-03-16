088f6b0f59c732f1e5163debf95fd8f16e827439
maya <maya@pkgsrc.org>
2017-03-16 19:29:39 +0000

    libva: Similar to libvdpau, add an x11 option to make it easier to disable libva dependencies for non-X11 users. feel free to suggest alternatives.
    PR pkg/52021

--- multimedia/libva/available.mk
+++ multimedia/libva/available.mk
@@ -1,11 +1,19 @@
-# $NetBSD: available.mk,v 1.8 2017/02/24 17:07:51 maya Exp $
+# $NetBSD: available.mk,v 1.9 2017/03/16 19:29:39 maya Exp $
 
 .include "../../mk/bsd.prefs.mk"
+
+PKG_SUPPORTED_OPTIONS+=	x11
+PKG_SUGGESTED_OPTIONS=	x11
+
+.include "../../mk/bsd.options.mk"
 
 # At the moment VAAPI is available only for XXX.  The following
 # condition is here to NOT list these platforms in multiple places,
 # i.e. in mplayer, xine, xbmc etc.  Have a look at buildlink3.mk too.
 
+VAAPI_AVAILABLE=	no
+
+.if !empty(PKG_OPTIONS:Mx11)
 # The following should be kept up-to-date!
 .if ${OPSYS} == "DragonFly"
 VAAPI_AVAILABLE=	yes
@@ -15,6 +23,5 @@ VAAPI_AVAILABLE=	yes
 VAAPI_AVAILABLE=	yes
 .elif ${OPSYS} == "Linux"
 VAAPI_AVAILABLE=	yes
-.else
-VAAPI_AVAILABLE=	no
+.endif
 .endif
