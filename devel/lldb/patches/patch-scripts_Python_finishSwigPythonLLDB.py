981e391665372690033370912e856756730515d9
adam <adam@pkgsrc.org>
2017-03-17 22:38:17 +0000

    LLVM 4.0.0: The minimum compiler version required for building LLVM has been raised to 4.8 for GCC and 2015 for Visual Studio. The C API functions LLVMAddFunctionAttr, LLVMGetFunctionAttr, LLVMRemoveFunctionAttr, LLVMAddAttribute, LLVMRemoveAttribute, LLVMGetAttribute, LLVMAddInstrAttribute and LLVMRemoveInstrAttribute have been removed. The C API enum LLVMAttribute has been deleted. The definition and uses of LLVM_ATRIBUTE_UNUSED_RESULT in the LLVM source were replaced with LLVM_NODISCARD, which matches the C++17 [[nodiscard]] semantics rather than gcc’s __attribute__((warn_unused_result)). The Timer related APIs now expect a Name and Description. When upgrading code the previously used names should become descriptions and a short name in the style of a programming language identifier should be added. LLVM now handles invariant.group across different basic blocks, which makes it possible to devirtualize virtual calls inside loops. The aggressive dead code elimination phase (“adce”) now removes branches which do not effect program behavior. Loops are retained by default since they may be infinite but these can also be removed with LLVM option -adce-remove-loops when the loop body otherwise has no live operations. The llvm-cov tool can now export coverage data as json. Its html output mode has also improved.

--- devel/lldb/patches/patch-scripts_Python_finishSwigPythonLLDB.py
+++ devel/lldb/patches/patch-scripts_Python_finishSwigPythonLLDB.py
@@ -1,17 +1,15 @@
-$NetBSD: patch-scripts_Python_finishSwigPythonLLDB.py,v 1.2 2016/07/27 09:17:42 wiz Exp $
+$NetBSD: patch-scripts_Python_finishSwigPythonLLDB.py,v 1.3 2017/03/17 22:38:17 adam Exp $
 
 Don't conflict with lang/py-six.
 
---- scripts/Python/finishSwigPythonLLDB.py.orig	2016-05-19 21:02:17.000000000 +0000
+--- scripts/Python/finishSwigPythonLLDB.py.orig	2016-10-27 22:51:41.000000000 +0000
 +++ scripts/Python/finishSwigPythonLLDB.py
-@@ -749,8 +749,8 @@ def main(vDictArgs):
-     if bOk:
-         bOk, strMsg = create_symlinks(vDictArgs, strFrameworkPythonDir, strLldbLibDir)
+@@ -821,8 +821,6 @@ def main(vDictArgs):
+         bOk, strMsg = create_symlinks(
+             vDictArgs, strFrameworkPythonDir, strLldbLibDir)
  
 -    if bOk:
 -        bOk, strMsg = copy_six(vDictArgs, strFrameworkPythonDir)
-+    # if bOk:
-+    #     bOk, strMsg = copy_six(vDictArgs, strFrameworkPythonDir)
  
      if bOk:
          bOk, strMsg = copy_lldbpy_file_to_lldb_pkg_dir(vDictArgs,
