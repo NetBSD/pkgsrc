f9426a8fbe8f15b645b846d66ae4684409944ea0
adam <adam@pkgsrc.org>
2017-03-17 22:38:17 +0000

    LLVM 4.0.0: The minimum compiler version required for building LLVM has been raised to 4.8 for GCC and 2015 for Visual Studio. The C API functions LLVMAddFunctionAttr, LLVMGetFunctionAttr, LLVMRemoveFunctionAttr, LLVMAddAttribute, LLVMRemoveAttribute, LLVMGetAttribute, LLVMAddInstrAttribute and LLVMRemoveInstrAttribute have been removed. The C API enum LLVMAttribute has been deleted. The definition and uses of LLVM_ATRIBUTE_UNUSED_RESULT in the LLVM source were replaced with LLVM_NODISCARD, which matches the C++17 [[nodiscard]] semantics rather than gcc’s __attribute__((warn_unused_result)). The Timer related APIs now expect a Name and Description. When upgrading code the previously used names should become descriptions and a short name in the style of a programming language identifier should be added. LLVM now handles invariant.group across different basic blocks, which makes it possible to devirtualize virtual calls inside loops. The aggressive dead code elimination phase (“adce”) now removes branches which do not effect program behavior. Loops are retained by default since they may be infinite but these can also be removed with LLVM option -adce-remove-loops when the loop body otherwise has no live operations. The llvm-cov tool can now export coverage data as json. Its html output mode has also improved.

--- lang/llvm/patches/patch-include_llvm_Support_Host.h
+++ lang/llvm/patches/patch-include_llvm_Support_Host.h
@@ -1,19 +1,18 @@
-$NetBSD: patch-include_llvm_Support_Host.h,v 1.2 2016/11/14 20:15:33 ryoon Exp $
+$NetBSD: patch-include_llvm_Support_Host.h,v 1.3 2017/03/17 22:38:17 adam Exp $
 
 Fix lack of machine/endian.h on solaris
 http://permalink.gmane.org/gmane.comp.compilers.llvm.devel/63225
 
---- include/llvm/Support/Host.h.orig	2016-02-26 17:01:45.000000000 +0000
+--- include/llvm/Support/Host.h.orig	2016-10-13 17:43:20.000000000 +0000
 +++ include/llvm/Support/Host.h
-@@ -18,6 +18,20 @@
- 
- #if defined(__linux__) || defined(__GNU__) || defined(__HAIKU__)
+@@ -20,6 +20,18 @@
  #include <endian.h>
+ #elif defined(_AIX)
+ #include <sys/machine.h>
 +#elif defined(__sun) & defined(__SVR4)
 +# ifndef BYTE_ORDER
 +# define LITTLE_ENDIAN 1234
 +# define BIG_ENDIAN 4321
-+# if defined(__sun) & defined(__SVR4)
 +# include <sys/isa_defs.h>
 +# ifdef _LITTLE_ENDIAN
 +# define BYTE_ORDER LITTLE_ENDIAN
@@ -21,7 +20,6 @@ http://permalink.gmane.org/gmane.comp.compilers.llvm.devel/63225
 +# ifdef _BIG_ENDIAN
 +# define BYTE_ORDER BIG_ENDIAN
 +# endif
-+# endif /* sun */
 +# endif /* BYTE_ORDER */
  #else
  #if !defined(BYTE_ORDER) && !defined(LLVM_ON_WIN32)
