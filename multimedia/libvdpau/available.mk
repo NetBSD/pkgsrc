90e9839c3b94721d5c3d7d1aef2dea2f155fcf85
maya <maya@pkgsrc.org>
2017-03-17 09:10:14 +0000

    revert libvdpau/libva changes, this doesn't work as expected.

--- multimedia/libvdpau/available.mk
+++ multimedia/libvdpau/available.mk
@@ -1,17 +1,9 @@
-# $NetBSD: available.mk,v 1.7 2017/03/16 23:07:28 maya Exp $
+# $NetBSD: available.mk,v 1.8 2017/03/17 09:10:14 maya Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.libvdpau
-PKG_SUPPORTED_OPTIONS+=	x11
-PKG_SUGGESTED_OPTIONS=	x11
-
-.include "../../mk/bsd.options.mk"
-
 VDPAU_AVAILABLE=	no
 
-.if !empty(PKG_OPTIONS:Mx11)
 .if ${OPSYS} != "Darwin" && (${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64")
 VDPAU_AVAILABLE=	yes
-.endif
 .endif
