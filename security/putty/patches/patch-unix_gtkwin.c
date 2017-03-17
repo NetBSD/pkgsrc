c7fef1d6be6f632b5b0cd3d8fc6e63f1607a56dd
maya <maya@pkgsrc.org>
2017-03-17 00:11:48 +0000

    putty: update to 0.68
    PuTTY 0.68, released today, supports elliptic-curve cryptography for host
    keys, user authentication keys, and key exchange. Also, for the first time,
    it comes in a 64-bit Windows version.
    
    This update may create a build issue for non-BSD due to ancient functions
    being different on BSD and SYSV. there's always macros if this fails.

--- security/putty/patches/patch-unix_gtkwin.c
+++ security/putty/patches/patch-unix_gtkwin.c
@@ -1,19 +1,22 @@
-$NetBSD: patch-unix_gtkwin.c,v 1.3 2013/08/07 11:06:39 drochner Exp $
+$NetBSD: patch-unix_gtkwin.c,v 1.4 2017/03/17 00:11:48 maya Exp $
 
 Make the home/end keys work on BSD servers as well as Linux ones
 
---- unix/gtkwin.c.orig	2013-07-20 13:15:10.000000000 +0000
+--- unix/gtkwin.c.orig	2017-02-18 17:10:15.000000000 +0000
 +++ unix/gtkwin.c
-@@ -1132,10 +1132,17 @@ gint key_event(GtkWidget *widget, GdkEve
+@@ -1586,13 +1586,21 @@ gint key_event(GtkWidget *widget, GdkEve
  		use_ucsoutput = FALSE;
  		goto done;
  	    }
 -	    if ((code == 1 || code == 4) &&
 -		conf_get_int(inst->conf, CONF_rxvt_homeend)) {
--		end = 1 + sprintf(output+1, code == 1 ? "\x1B[H" : "\x1BOw");
--		use_ucsoutput = FALSE;
 +	    /* Home/End */
 +	    if (code == 1 || code == 4) {
+ #ifdef KEY_EVENT_DIAGNOSTICS
+                 debug((" - rxvt style Home/End"));
+ #endif
+-		end = 1 + sprintf(output+1, code == 1 ? "\x1B[H" : "\x1BOw");
+-		use_ucsoutput = FALSE;
 +		/* Send the correct XTerm or rxvt codes for home/end
 +		 * We used to send ^[1~ and [4~ for Xterm,
 +		 * but those are Linux console */
@@ -23,6 +26,7 @@ Make the home/end keys work on BSD servers as well as Linux ones
 +		else
 +		    he = code == 1 ? "\x1BOH" : "\x1BOF";
 +		end = 1 + sprintf(output+1, "%s", he);
++
  		goto done;
  	    }
  	    if (code) {
