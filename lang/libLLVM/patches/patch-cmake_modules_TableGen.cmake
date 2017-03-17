60b9132584b132a1f93aba2c70bbb4413964d9e9
adam <adam@pkgsrc.org>
2017-03-17 22:38:17 +0000

    LLVM 4.0.0: The minimum compiler version required for building LLVM has been raised to 4.8 for GCC and 2015 for Visual Studio. The C API functions LLVMAddFunctionAttr, LLVMGetFunctionAttr, LLVMRemoveFunctionAttr, LLVMAddAttribute, LLVMRemoveAttribute, LLVMGetAttribute, LLVMAddInstrAttribute and LLVMRemoveInstrAttribute have been removed. The C API enum LLVMAttribute has been deleted. The definition and uses of LLVM_ATRIBUTE_UNUSED_RESULT in the LLVM source were replaced with LLVM_NODISCARD, which matches the C++17 [[nodiscard]] semantics rather than gcc’s __attribute__((warn_unused_result)). The Timer related APIs now expect a Name and Description. When upgrading code the previously used names should become descriptions and a short name in the style of a programming language identifier should be added. LLVM now handles invariant.group across different basic blocks, which makes it possible to devirtualize virtual calls inside loops. The aggressive dead code elimination phase (“adce”) now removes branches which do not effect program behavior. Loops are retained by default since they may be infinite but these can also be removed with LLVM option -adce-remove-loops when the loop body otherwise has no live operations. The llvm-cov tool can now export coverage data as json. Its html output mode has also improved.

--- lang/libLLVM/patches/patch-cmake_modules_TableGen.cmake
+++ lang/libLLVM/patches/patch-cmake_modules_TableGen.cmake
@@ -1,15 +1,15 @@
-$NetBSD: patch-cmake_modules_TableGen.cmake,v 1.2 2016/11/14 20:15:33 ryoon Exp $
+$NetBSD: patch-cmake_modules_TableGen.cmake,v 1.3 2017/03/17 22:38:18 adam Exp $
 
 Disable install of llvm-tblgen, provided by lang/llvm.
 
---- cmake/modules/TableGen.cmake.orig	2016-06-08 21:19:26.000000000 +0000
+--- cmake/modules/TableGen.cmake.orig	2016-12-06 17:09:29.000000000 +0000
 +++ cmake/modules/TableGen.cmake
-@@ -138,7 +138,7 @@ macro(add_tablegen target project)
-       set_target_properties(${target} PROPERTIES LINK_FLAGS -Wl,--stack,16777216)
-     endif(CMAKE_SIZEOF_VOID_P MATCHES "8")
-   endif( MINGW )
+@@ -134,7 +134,7 @@ macro(add_tablegen target project)
+     endif()
+   endif()
+ 
 -  if (${project} STREQUAL LLVM AND NOT LLVM_INSTALL_TOOLCHAIN_ONLY)
 +  if (${project} STREQUAL LLVM AND NOT LLVM_INSTALL_TOOLCHAIN_ONLY AND false)
-     install(TARGETS ${target}
-             EXPORT LLVMExports
-             RUNTIME DESTINATION ${LLVM_TOOLS_INSTALL_DIR})
+     if(${target} IN_LIST LLVM_DISTRIBUTION_COMPONENTS OR
+         NOT LLVM_DISTRIBUTION_COMPONENTS)
+       set(export_to_llvmexports EXPORT LLVMExports)
