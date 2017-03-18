4777e115ec5cdf89626f8fb1319b3e69f69da95f
adam <adam@pkgsrc.org>
2017-03-18 20:18:39 +0000

    Changes 1.4.1: Find the appropriate version of the Python interpreter when building Ensure that the dispatch generation script works with Python 2.x and 3.x Ensure that Epoxy builds correctly under Windows Fix location of the m4 macros in the autotools build

--- graphics/libepoxy/buildlink3.mk
+++ graphics/libepoxy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/04/03 08:48:57 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/03/18 20:18:39 adam Exp $
 
 BUILDLINK_TREE+=	libepoxy
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libepoxy
 LIBEPOXY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libepoxy+=	libepoxy>=1.2
-BUILDLINK_PKGSRCDIR.libepoxy?=	../../graphics/libepoxy
+BUILDLINK_PKGSRCDIR.libepoxy?=		../../graphics/libepoxy
 
 .endif	# LIBEPOXY_BUILDLINK3_MK
 
