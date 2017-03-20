7ef63f35fddea6365ffe96b26218ea04eb3461f2
taca <taca@pkgsrc.org>
2017-03-20 15:54:41 +0000

    Update ruby-pygments.rb to 1.1.1.
    Version 1.1.1 (2016/12/28)
    -----------------------------
    
    * Suppress Ruby 2.4.0's warnings [#172](https://github.com/tmm1/pygments.rb/pull/172)
    * Enable `frozen_string_literal` [#173](https://github.com/tmm1/pygments.rb/pull/173)
    
    Version 1.1.0 (2016/12/24)
    -----------------------------
    
    * Support JRuby [#170](https://github.com/tmm1/pygments.rb/pull/170)
    * Make pygments.rb thread safe [#171](https://github.com/tmm1/pygments.rb/pull/171)
    
    Version 1.0.0 (2016/12/11)
    -----------------------------
    
    * Upgrade bundled pygments to 2.2.0-HEAD [#167](https://github.com/tmm1/pygments.rb/pull/167)
      * This includes **incompatible changes* because of upgrade of pygments.
        See http://pygments.org/ for details.
    * Relax yajl-ruby dependency to "~> 1.2" [#164](https://github.com/tmm1/pygments.rb/pull/164)
    * Python binary can be configured by `PYTMENTS_RB_PYTHON` env [#168](https://github.com/tmm1/pygments.rb/pull/168)
    * Improved error messages when python binary is missing [#158](https://github.com/tmm1/pygments.rb/pull/158)

--- www/ruby-pygments.rb/patches/patch-test_test__pygments.rb
+++ www/ruby-pygments.rb/patches/patch-test_test__pygments.rb
@@ -1,11 +1,12 @@
-$NetBSD: patch-test_test__pygments.rb,v 1.1 2015/11/23 07:23:50 taca Exp $
+$NetBSD: patch-test_test__pygments.rb,v 1.2 2017/03/20 15:54:41 taca Exp $
 
 * run test without install pygments.rb.
 
---- test/test_pygments.rb.orig	2015-11-18 03:08:00.000000000 +0000
+--- test/test_pygments.rb.orig	2017-01-04 15:51:52.000000000 +0000
 +++ test/test_pygments.rb
-@@ -1,7 +1,9 @@
- #coding: utf-8
+@@ -1,8 +1,10 @@
+ # coding: utf-8
+ # frozen_string_literal: true
  
 +$:.push '../lib'
 +
@@ -14,4 +15,4 @@ $NetBSD: patch-test_test__pygments.rb,v 1.1 2015/11/23 07:23:50 taca Exp $
 +require 'pygments'
  ENV['mentos-test'] = "yes"
  
- P = Pygments
+ 
