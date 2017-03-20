f3e3efa5bb4080d8f7976872ad3b751e699edbd6
taca <taca@pkgsrc.org>
2017-03-20 15:54:41 +0000

    Update ruby-pygments.rb to 1.1.1.
    Version 1.1.1 (2016/12/28)
    -----------------------------
    
    * Suppress Ruby 2.4.0's warnings [#172](https://github.com/tmm1/pygments.rb/pull/172)
    * Enable `frozen_string_literal` [#173](https://github.com/tmm1/pygments.rb/pull/173)
    
    Version 1.1.0 (2016/12/24)
    -----------------------------
    
    * Support JRuby [#170](https://github.com/tmm1/pygments.rb/pull/170)
    * Make pygments.rb thread safe [#171](https://github.com/tmm1/pygments.rb/pull/171)
    
    Version 1.0.0 (2016/12/11)
    -----------------------------
    
    * Upgrade bundled pygments to 2.2.0-HEAD [#167](https://github.com/tmm1/pygments.rb/pull/167)
      * This includes **incompatible changes* because of upgrade of pygments.
        See http://pygments.org/ for details.
    * Relax yajl-ruby dependency to "~> 1.2" [#164](https://github.com/tmm1/pygments.rb/pull/164)
    * Python binary can be configured by `PYTMENTS_RB_PYTHON` env [#168](https://github.com/tmm1/pygments.rb/pull/168)
    * Improved error messages when python binary is missing [#158](https://github.com/tmm1/pygments.rb/pull/158)

--- www/ruby-pygments.rb/patches/patch-lib_pygments_popen.rb
+++ www/ruby-pygments.rb/patches/patch-lib_pygments_popen.rb
@@ -1,27 +1,24 @@
-$NetBSD: patch-lib_pygments_popen.rb,v 1.1 2015/11/23 07:23:50 taca Exp $
+$NetBSD: patch-lib_pygments_popen.rb,v 1.2 2017/03/20 15:54:41 taca Exp $
 
 * Fix path
 * Fix python's name under pkgsrc.
 
---- lib/pygments/popen.rb.orig	2015-11-18 02:47:26.000000000 +0000
+--- lib/pygments/popen.rb.orig	2017-01-04 15:51:52.000000000 +0000
 +++ lib/pygments/popen.rb
-@@ -19,7 +19,7 @@ module Pygments
+@@ -24,7 +24,7 @@ module Pygments
      # Get things started by opening a pipe to mentos (the freshmaker), a
      # Python process that talks to the Pygments library. We'll talk back and
      # forth across this pipe.
 -    def start(pygments_path = File.expand_path('../../../vendor/pygments-main/', __FILE__))
 +    def start(pygments_path = File.expand_path('../../vendor/pygments-main/', __FILE__))
-       is_windows = RUBY_PLATFORM =~ /mswin|mingw/
        begin
-         @log = Logger.new(ENV['MENTOS_LOG'] ||= is_windows ? 'NUL:' : '/dev/null')
-@@ -44,10 +44,7 @@ module Pygments
- 
-     # Detect a suitable Python binary to use.
-     def python_binary(is_windows)
--      if is_windows && which('py')
--        return 'py -2'
--      end
--      return which('python2') || 'python'
+         @log = Logger.new(ENV['MENTOS_LOG'] ||= File::NULL)
+         @log.level = Logger::INFO
+@@ -65,7 +65,7 @@ module Pygments
+       elsif windows? && which('py')
+         return 'py -2'
+       end
+-      return which('python2') || which('python')
 +      return '@PYTHON@'
      end
  
