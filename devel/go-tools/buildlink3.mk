ac11e4e4764ba10d4b3190d451ad5d2c11490072
bsiegert <bsiegert@pkgsrc.org>
2017-03-20 22:53:45 +0000

    Unbreak go-crypto and go-net, they no longer depend on one another. Bump go-crypto and go-tools, which now depends on go-crypto-acme.
    Fixes PR pkg/52081 breakage.

--- devel/go-tools/buildlink3.mk
+++ devel/go-tools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/03/16 19:33:50 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/03/20 22:53:45 bsiegert Exp $
 
 BUILDLINK_TREE+=	go-tools
 
@@ -12,6 +12,7 @@ BUILDLINK_API_DEPENDS.go-tools+=	go-tools>=1.8
 BUILDLINK_PKGSRCDIR.go-tools?=	../../devel/go-tools
 
 .include "../../devel/google-api-go-client/buildlink3.mk"
+.include "../../security/go-crypto-acme/buildlink3.mk"
 .endif	# GO_TOOLS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-go-tools
