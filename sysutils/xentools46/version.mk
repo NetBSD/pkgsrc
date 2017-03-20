7a91f7507b9eb7a00984b3e38309e34884f0cacc
bouyer <bouyer@pkgsrc.org>
2017-03-20 18:17:12 +0000

    Update xenkernel46 and xentools46 to 4.6.5. Changes since 4.6.3: various bug fixes. Includes all security patches up to and including XSA-209

--- sysutils/xentools46/version.mk
+++ sysutils/xentools46/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.1 2017/01/04 16:37:29 sborrill Exp $
+# $NetBSD: version.mk,v 1.2 2017/03/20 18:17:13 bouyer Exp $
 #
 # Version number is used by xentools46 and xenstoretools
 
-VERSION=	4.6.3
+VERSION=	4.6.5
 VERSION_IPXE=	9a93db3f0947484e30e753bbd61a10b17336e20e
