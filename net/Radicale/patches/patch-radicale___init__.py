4d4bfd3ee43d84fb5ea7cfcda7f22ad362bbe4d8
sborrill <sborrill@pkgsrc.org>
2017-03-20 10:49:03 +0000

    Fix strip_domain patch to work in anonymous case. Bump PKGREVISION

--- net/Radicale/patches/patch-radicale___init__.py
+++ net/Radicale/patches/patch-radicale___init__.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-radicale___init__.py,v 1.1 2017/03/17 16:56:47 sborrill Exp $
+$NetBSD: patch-radicale___init__.py,v 1.2 2017/03/20 10:49:03 sborrill Exp $
 
 Add support for strip_domain
 Fix reading of can_skip_base_prefix setting
 
---- radicale/__init__.py.orig	2017-03-17 16:04:32.000000000 +0000
-+++ radicale/__init__.py	2017-03-17 16:04:57.000000000 +0000
+--- radicale/__init__.py.orig	2016-01-07 22:29:49.000000000 +0000
++++ radicale/__init__.py	2017-03-20 10:43:49.000000000 +0000
 @@ -249,7 +249,7 @@
          base_prefix = config.get("server", "base_prefix")
          if environ["PATH_INFO"].startswith(base_prefix):
@@ -14,12 +14,13 @@ Fix reading of can_skip_base_prefix setting
              log.LOGGER.debug(
                  "Prefix already stripped from path: %s", environ["PATH_INFO"])
          else:
-@@ -280,6 +280,9 @@
+@@ -280,6 +280,10 @@
              user = environ.get("REMOTE_USER")
              password = None
  
 +        if config.getboolean("auth", "strip_domain"):
-+            user = re.sub("@.*", "", user)
++            if user:
++                user = re.sub("@.*", "", user)
 +
          well_known = WELL_KNOWN_RE.match(path)
          if well_known:
