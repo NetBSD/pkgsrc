13a00acd36c5cbb5b50ad3a55c9408a0dbcfb672
adam <adam@pkgsrc.org>
2017-03-30 11:42:58 +0000

    Python 3.6.1 is the first maintenance release of Python 3.6. The Python 3.6 series contains many new features and optimizations. See the What’s New In Python 3.6 document for more information.
    Major new features of the 3.6 series, compared to 3.5
    Among the new major new features in Python 3.6 are:
    
    PEP 468, Preserving Keyword Argument Order
    PEP 487, Simpler customization of class creation
    PEP 495, Local Time Disambiguation
    PEP 498, Literal String Formatting
    PEP 506, Adding A Secrets Module To The Standard Library
    PEP 509, Add a private version to dict
    PEP 515, Underscores in Numeric Literals
    PEP 519, Adding a file system path protocol
    PEP 520, Preserving Class Attribute Definition Order
    PEP 523, Adding a frame evaluation API to CPython
    PEP 524, Make os.urandom() blocking on Linux (during system startup)
    PEP 525, Asynchronous Generators (provisional)
    PEP 526, Syntax for Variable Annotations (provisional)
    PEP 528, Change Windows console encoding to UTF-8
    PEP 529, Change Windows filesystem encoding to UTF-8
    PEP 530, Asynchronous Comprehensions

--- lang/python36/dist.mk
+++ lang/python36/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.1 2017/01/01 14:34:27 adam Exp $
+# $NetBSD: dist.mk,v 1.2 2017/03/30 11:42:58 adam Exp $
 
-PY_DISTVERSION=	3.6.0
+PY_DISTVERSION=	3.6.1
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python36/distinfo
