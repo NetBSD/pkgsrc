cb5e27bfc2a6965f8ad68a6ee0f0616d2acd05f7
nat <nat@pkgsrc.org>
2017-03-30 18:44:13 +0000

    Fix playback on NetBSD-current as of 20170322.  Ensure that the audioctl device operates on the correct channel.
    Addresses PR kern/52075.
    
    Bump PKGREVISION.

--- audio/timidity/patches/patch-timidity_midi_a__c.c
+++ audio/timidity/patches/patch-timidity_midi_a__c.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-timidity_midi_a__c.c,v 1.1 2012/07/24 19:47:44 jperkin Exp $
+$NetBSD: patch-timidity_midi_a__c.c,v 1.2 2017/03/30 18:44:13 nat Exp $
 
 Some MIDI definitions are missing on some platform's soundcard.h, so just use
 internal definitions everywhere.
 
---- timidity/midi_a.c.orig	2012-07-24 00:44:02.879194950 +0300
-+++ timidity/midi_a.c	2012-07-24 00:44:21.200309747 +0300
+--- timidity/midi_a.c.orig	2009-03-14 15:49:59.000000000 +0000
++++ timidity/midi_a.c
 @@ -49,14 +49,7 @@
  #endif
  #include <fcntl.h>
