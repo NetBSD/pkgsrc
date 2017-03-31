5f7fe16e7d41e39c4030fbbf5b4eea931a17e0a5
wiz <wiz@pkgsrc.org>
2017-03-31 10:32:14 +0000

    Recursive bump for gpgme update which removed a support library.

--- mail/claws-mail-pgpcore/buildlink3.mk
+++ mail/claws-mail-pgpcore/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.30 2017/02/12 06:24:46 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2017/03/31 10:32:15 wiz Exp $
 
 BUILDLINK_TREE+=	claws-mail-pgpcore
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	claws-mail-pgpcore
 CLAWS_MAIL_PGPCORE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.claws-mail-pgpcore+=	claws-mail-pgpcore>=3.7.0
-BUILDLINK_ABI_DEPENDS.claws-mail-pgpcore+=	claws-mail-pgpcore>=3.14.0nb5
+BUILDLINK_ABI_DEPENDS.claws-mail-pgpcore+=	claws-mail-pgpcore>=3.14.1nb1
 BUILDLINK_PKGSRCDIR.claws-mail-pgpcore?=	../../mail/claws-mail-pgpcore
 
 .include "../../x11/gtk2/buildlink3.mk"
--- net/kxmlrpcclient/buildlink3.mk
+++ net/kxmlrpcclient/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2017/02/28 15:20:00 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2017/03/31 10:32:15 wiz Exp $
 
 BUILDLINK_TREE+=	kxmlrpcclient
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kxmlrpcclient
 KXMLRPCCLIENT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kxmlrpcclient+=	kxmlrpcclient>=5.21.0
-BUILDLINK_ABI_DEPENDS.kxmlrpcclient?=	kxmlrpcclient>=5.25.0nb5
+BUILDLINK_ABI_DEPENDS.kxmlrpcclient?=	kxmlrpcclient>=5.25.0nb6
 BUILDLINK_PKGSRCDIR.kxmlrpcclient?=	../../net/kxmlrpcclient
 
 .include "../../devel/kio/buildlink3.mk"
