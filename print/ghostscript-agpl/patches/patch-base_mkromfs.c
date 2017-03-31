6918444d7d88fa675c5cb762871d1861177ee8eb
adam <adam@pkgsrc.org>
2017-03-31 07:26:23 +0000

    Version 9.21 (2017-03-16)
    This is the fifteenth full release in the stable 9.x series.
    
    Highlights in this release include:
    * pdfwrite now preserves annotations from input PDFs (where possible).
    * The GhostXPS interpreter now provides the pdfwrite device with the data it requires to emit a ToUnicode CMap: thus allowing fully searchable PDFs to be created from XPS input (in the vast majority of cases).
    * Ghostscript now allows the default color space for PDF transparency blends.
    * The Ghostscript/GhostPDL configure script now has much better/fuller support for cross compiling.
    * The tiffscaled and tiffscaled4 devices can now use ETS (Even Tone Screening)
    * The toolbin/pdf_info.ps utility can now emit the PDF XML metadata.
    * Ghostscript has a new scan converter available (currently optional, but will become the default in a near future release). It can be enabled by using the command line option: '-dSCANCONVERTERTYPE=2'. This new implementation provides vastly improved performance with large and complex paths.
    * The usual round of bug fixes, compatibility changes, and incremental improvements.

--- print/ghostscript-agpl/patches/patch-base_mkromfs.c
+++ print/ghostscript-agpl/patches/patch-base_mkromfs.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-ai,v 1.3 2015/08/12 22:27:23 wiz Exp $
+$NetBSD: patch-base_mkromfs.c,v 1.1 2017/03/31 07:26:23 adam Exp $
 
 chunk 1:
 fix build with _FORTIFY_SOURCE=2; it seems the arguments sometimes
@@ -8,9 +8,9 @@ chunk 2:
 Replace BSD/Linux specific "qd" printf format specifier by "lld",
 should fix build problem on Solaris reported by Joern Clausen per PR pkg/40664
 
---- base/mkromfs.c.orig	2015-03-30 08:21:24.000000000 +0000
+--- base/mkromfs.c.orig	2017-03-16 10:12:02.000000000 +0000
 +++ base/mkromfs.c
-@@ -1987,7 +1987,7 @@ ws(const byte *str, int len)
+@@ -2014,7 +2014,7 @@ ws(const byte *str, int len)
      if (len >= LINE_SIZE)
          exit(1);
  
@@ -19,12 +19,12 @@ should fix build problem on Solaris reported by Joern Clausen per PR pkg/40664
      flush_line_buf(len);
  }
  
-@@ -2329,7 +2329,7 @@ main(int argc, char *argv[])
- #endif
-     fprintf(out,"\n#include \"stdint_.h\"\n");
-     fprintf(out,"\n#include \"time_.h\"\n\n");
--    fprintf(out,"    time_t gs_romfs_buildtime = %ld;\n\n", time(NULL));
-+    fprintf(out,"    time_t gs_romfs_buildtime = %lld;\n\n", (long long)time(NULL));
+@@ -2368,7 +2368,7 @@ main(int argc, char *argv[])
+     }
+     if (!buildtime)
+         buildtime = time(NULL);
+-    fprintf(out,"    time_t gs_romfs_buildtime = %ld;\n\n", buildtime);
++    fprintf(out,"    time_t gs_romfs_buildtime = %lld;\n\n", (long long)buildtime);
  
      /* process the remaining arguments (options interspersed with paths) */
      for (; atarg < argc; atarg++) {
