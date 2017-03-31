850a92863066e92c4e55912f4ae836e4e32d4d6c
wiz <wiz@pkgsrc.org>
2017-03-31 10:32:14 +0000

    Recursive bump for gpgme update which removed a support library.

--- devel/kdeclarative/buildlink3.mk
+++ devel/kdeclarative/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2017/02/28 15:19:58 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2017/03/31 10:32:14 wiz Exp $
 
 BUILDLINK_TREE+=	kdeclarative
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kdeclarative
 KDECLARATIVE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kdeclarative+=	kdeclarative>=5.21.0
-BUILDLINK_ABI_DEPENDS.kdeclarative?=	kdeclarative>=5.25.0nb5
+BUILDLINK_ABI_DEPENDS.kdeclarative?=	kdeclarative>=5.25.0nb6
 BUILDLINK_PKGSRCDIR.kdeclarative?=	../../devel/kdeclarative
 
 .include "../../graphics/libepoxy/buildlink3.mk"
