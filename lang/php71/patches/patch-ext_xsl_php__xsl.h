34d766688b89c5c182815bd0dad857f53f0baab9
fhajny <fhajny@pkgsrc.org>
2017-04-05 12:28:59 +0000

    Build the dom extension embedded. This enables full functionality in xmlreader and fixes joyent/pkgsrc/issues/477. Bump PKREVISION.

---
+++ lang/php71/patches/patch-ext_xsl_php__xsl.h
@@ -0,0 +1,15 @@
+$NetBSD: patch-ext_xsl_php__xsl.h,v 1.1 2017/04/05 12:28:59 fhajny Exp $
+
+The dom header is now supplied by the main php bl3.
+
+--- ext/xsl/php_xsl.h.orig	2017-03-14 13:17:46.000000000 +0000
++++ ext/xsl/php_xsl.h
+@@ -41,7 +41,7 @@ extern zend_module_entry xsl_module_entr
+ #include <libexslt/exsltconfig.h>
+ #endif
+ 
+-#include "../dom/xml_common.h"
++#include "dom/xml_common.h"
+ #include "xsl_fe.h"
+ 
+ #include <libxslt/extensions.h>
