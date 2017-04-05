01ca6dbd4a2217d3ae39ff75f292ff647ced136c
adam <adam@pkgsrc.org>
2017-04-05 17:34:38 +0000

    version 0.630 * checksum.buf should only be changed after the semaphore wait * Update README * Add documentation for recursive mode * Implement gzip compatible -r recursive option * Add initial argument processing for recursive option * Tidy * Add one more verbose for compat mode * Add support for various combinations in compat mode * models is array of chars. char's signess is implementation specific. It's unsigned on ARMv7. Unsigned char cannot represent negative values. GCC 6 complains about it: * Fix decompression of multiple chunk encrypted archives * Tidy gotos * Show correct lengths during testing on big endian and compressed archives * Update copyright dates * Allow less than maxram to be malloced for checksum to fix Failed to malloc ckbuf in hash_search2 * Base temporary output buffer on maximum mallocable, not maxram * Enable subdir objects for future automake compatibility * Add support for -m option in lrztar * Big endian fix for Solaris Sparc courtesy of joelfredrikson. * Fixed typographical error, changed accomodate to accommodate in README. * A whitespace fix on lrztar. * Add sanity check to prevent trying to malloc more ram than a system/environment is capable of * Cosmetic help change for compat * Add rudimentary manpage for lrz * Fix lrz symbolic linkage * Do not fail if we are unable to write temporary files, giving a warning only that it might fail if we don't have enough ram * Try /tmp/ if none of the temporary environment directories or the current directory are writeable * Set STDOUT correctly in compat mode * Style police * Fix false warning on decompressing from stdin without keep files * Fix false warning on compressing from stdin without keep files * Don't show extra message in compat mode decompress * Show correct appname when called in compat mode * Add support for progress, fast and best flags in compat mode * Add compatibility mode with gzip when called as lrz * Correct adding slash to control->tmpdir. off-by-one error. * Update manpage for long options

--- archivers/lrzip/patches/patch-lrzip.h
+++ archivers/lrzip/patches/patch-lrzip.h
@@ -1,6 +1,7 @@
-$NetBSD: patch-lrzip.h,v 1.1 2013/10/21 11:29:05 fhajny Exp $
+$NetBSD: patch-lrzip.h,v 1.2 2017/04/05 17:34:39 adam Exp $
 
 stdarg needed with modern GCC for va_list.
+
 --- Lrzip.h.orig	2012-05-10 12:45:41.000000000 +0000
 +++ Lrzip.h
 @@ -28,6 +28,8 @@
