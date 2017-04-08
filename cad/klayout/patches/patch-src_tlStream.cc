a1faaddac33b000446b0bdc6f6228ad29f8ed569
mef <mef@pkgsrc.org>
2017-04-08 13:48:52 +0000

    Updated cad/klayout to 0.24.10 ------------------------------ (from: http://klayout.de/development.html) Version 0.24.10 Release date: 2017-04-01 Features:     Enhancements: New method CellView#is_dirty? This method indicates     that a cell view needs saving
        Bugfix: DEF reader was not able to read routing Routed segments
        with vias plus continued wiring were not read. Instead an error
        message was shown saying "Invalid orientation specification"
    
        Bugfix: Setting "technology-data" configuration property did not
        work Reading or writing the "technology-data" configuration
        property is the only way to access technology data from a
        script. Hence it's important that this property can be set. With
        this patch, "technology-data" can be set to an XML string which
        has the same format than the one stored within the configuration
        file.
    
        Bugfix: Reader issues with some RVE files The RVE reader could not
        read files with cell names containing hyphens (like "A-B").
    
        Bugfix: Inplace operators are now working in Python too For
        example "r1 += r2" was not working (r1 and r2 are pya.Region
        objects for example).

--- cad/klayout/patches/patch-src_tlStream.cc
+++ cad/klayout/patches/patch-src_tlStream.cc
@@ -1,4 +1,7 @@
-$NetBSD: patch-src_tlStream.cc,v 1.1 2017/02/14 21:33:08 joerg Exp $
+$NetBSD: patch-src_tlStream.cc,v 1.2 2017/04/08 13:48:52 mef Exp $
+
+date: 2017-02-15 06:33:08 +0900;  author: joerg;  state: Exp;  commitid: BDodrFgUrNgNVXFz;
+Add missing includes.
 
 --- src/tlStream.cc.orig	2017-02-09 18:57:16.170616732 +0000
 +++ src/tlStream.cc
