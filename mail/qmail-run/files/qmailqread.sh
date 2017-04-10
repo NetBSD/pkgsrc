b466633830761451536d6561b88cc03b88960d09
schmonz <schmonz@pkgsrc.org>
2017-04-10 15:04:56 +0000

    Wrap (or shorten) long lines. Parameterize some qmailqread config to match other scripts. Bump version.

--- mail/qmail-run/files/qmailqread.sh
+++ mail/qmail-run/files/qmailqread.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: qmailqread.sh,v 1.9 2017/04/04 07:51:03 schmonz Exp $
+# $NetBSD: qmailqread.sh,v 1.10 2017/04/10 15:04:56 schmonz Exp $
 #
 # @PKGNAME@ script to control a service providing local non-root
 # users access to see the queue. Adapted from a script by Steinar Haug.
@@ -12,6 +12,10 @@
 name="qmailqread"
 
 # User-settable rc.conf variables and their default values:
+: ${qmailqread_postenv:=""}
+: ${qmailqread_tcpflags:="-R1"}
+: ${qmailqread_tcphost:="127.0.0.1"}
+: ${qmailqread_tcpport:="20025"}
 : ${qmailqread_log:="YES"}
 : ${qmailqread_logcmd:="logger -t nb${name} -p mail.info"}
 : ${qmailqread_nologcmd:="@PREFIX@/bin/multilog -*"}
@@ -27,10 +31,17 @@ start_precmd="qmailqread_precmd"
 
 qmailqread_precmd()
 {
-	if [ -f /etc/rc.subr ]; then
-		checkyesno qmailqread_log || qmailqread_logcmd=${qmailqread_nologcmd}
+	if [ -f /etc/rc.subr ] && ! checkyesno qmailqread_log; then
+		qmailqread_logcmd=${qmailqread_nologcmd}
 	fi
-	command="@SETENV@ - @PREFIX@/bin/argv0 @PREFIX@/bin/tcpserver ${name} -R -1 -u `@ID@ -u @QMAIL_SEND_USER@` -g `@ID@ -g @QMAIL_SEND_USER@` 127.0.0.1 20025 @PREFIX@/bin/qmail-qread 2>&1 | @PREFIX@/bin/setuidgid @QMAIL_LOG_USER@ ${qmailqread_logcmd}"
+	command="@SETENV@ - ${qmailqread_postenv}
+@PREFIX@/bin/argv0 @PREFIX@/bin/tcpserver ${name}
+${qmailqread_tcpflags}
+-u `@ID@ -u @QMAIL_SEND_USER@` -g `@ID@ -g @QMAIL_SEND_USER@`
+${qmailqread_tcphost} ${qmailqread_tcpport}
+@PREFIX@/bin/qmail-qread
+2>&1 |
+@PREFIX@/bin/setuidgid @QMAIL_LOG_USER@ ${qmailqread_logcmd}"
 	command_args="&"
 	rc_flags=""
 }
