829b4804d4e39c2a7a297fb7e1cc0895eb3f59cb
schmonz <schmonz@pkgsrc.org>
2017-04-11 14:04:37 +0000

    If qmailqread runs on a custom host and port, have qmail-qread-client find it there. Bump version.

--- mail/qmail-run/files/qmail-qread-client.sh
+++ mail/qmail-run/files/qmail-qread-client.sh
@@ -1,10 +1,18 @@
 #!@SH@
 #
-# $NetBSD: qmail-qread-client.sh,v 1.4 2015/11/25 12:51:30 jperkin Exp $
+# $NetBSD: qmail-qread-client.sh,v 1.5 2017/04/11 14:04:37 schmonz Exp $
 #
 # @PKGNAME@ client program to allow local non-root users to see
 # the queue. Requires that the corresponding server program be running.
 # From Steinar Haug.
 #
 
-exec @PREFIX@/bin/tcpclient -RHl0 -- 127.0.0.1 20025 @SH@ -c 'exec @CAT@ <&6'
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
+	load_rc_config qmailqread
+fi
+
+: ${qmailqread_tcphost:="127.0.0.1"}
+: ${qmailqread_tcpport:="20025"}
+
+exec @PREFIX@/bin/tcpclient -RHl0 -- ${qmailqread_tcphost} ${qmailqread_tcpport} @SH@ -c 'exec @CAT@ <&6'
