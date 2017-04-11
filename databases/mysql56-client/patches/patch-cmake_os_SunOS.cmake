4ef8ee9c1f8d4aa4267a14a7e15949fd4b41830d
adam <adam@pkgsrc.org>
2017-04-11 20:21:02 +0000

    Changes 5.6.36: * Windows builds now use the default runtime libraries (builds use the /MD flag). * CMake support was added for compiling with Developer Studio 12.6. MySQL failed to compile if -DENABLE_DEBUG_SYNC=OFF AND -DWITH_DEBUG=ON were both given. The ENABLE_DEBUG_SYNC option has been removed and enabling WITH_DEBUG enables Debug Sync. * Changes in RPM package structure require a larger set of packages to be removed to install MySQL Server cleanly. * To avoid potential race conditions, Debian packages now use the GNU install utility rather than a combination of mkdir, touch, and chown. * CMake-generated packaging for Debian/Ubuntu packages was refactored for improved maintainability. The change includes updated logic for correctly replacing native distribution packaging in Debian and Ubuntu. * Reminder: MySQL 5.6 requires the Microsoft Visual C++ 2010 Redistributable Package to run on Windows platforms. Users should make sure the package has been installed on the system before starting the server. The package is available at the Microsoft Download Center. * The linked OpenSSL library for the MySQL Commercial Server has been updated to version 1.0.2k. For a description of issues fixed in this version, see http://www.openssl.org/news/vulnerabilities.html. * The mysql_options() C API function now supports a MYSQL_OPT_SSL_MODE option.

--- databases/mysql56-client/patches/patch-cmake_os_SunOS.cmake
+++ databases/mysql56-client/patches/patch-cmake_os_SunOS.cmake
@@ -1,10 +1,10 @@
-$NetBSD: patch-cmake_os_SunOS.cmake,v 1.1 2016/12/16 17:55:48 jperkin Exp $
+$NetBSD: patch-cmake_os_SunOS.cmake,v 1.2 2017/04/11 20:21:02 adam Exp $
 
 Backport section from MySQL 5.7 to fix SunOS/gcc.
 
---- cmake/os/SunOS.cmake.orig	2016-11-28 13:36:22.000000000 +0000
+--- cmake/os/SunOS.cmake.orig	2017-03-18 06:43:12.000000000 +0000
 +++ cmake/os/SunOS.cmake
-@@ -19,7 +19,13 @@ INCLUDE(CheckCSourceCompiles)
+@@ -19,7 +19,13 @@ INCLUDE(CheckCSourceCompiles) 
  
  # We require SunStudio 12u2 (CC 5.11)
  IF(NOT FORCE_UNSUPPORTED_COMPILER)
@@ -17,5 +17,5 @@ Backport section from MySQL 5.7 to fix SunOS/gcc.
 +    ENDIF()
 +  ELSEIF(CMAKE_C_COMPILER_ID MATCHES "SunPro")
      # CC -V yields
+     # CC: Studio 12.6 Sun C++ 5.15 SunOS_sparc Beta 2016/12/19
      # CC: Studio 12.5 Sun C++ 5.14 SunOS_sparc Dodona 2016/04/04
-     # CC: Sun C++ 5.13 SunOS_sparc Beta 2014/03/11
