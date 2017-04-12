b6e40731036e1eab9e629c6f483d80a6bca28388
fhajny <fhajny@pkgsrc.org>
2017-04-12 11:26:51 +0000

    Update lang/nodejs to 7.9.0.
    - util: console is now closer to what is supported in all major browsers

--- lang/nodejs/patches/patch-tools_install.py
+++ lang/nodejs/patches/patch-tools_install.py
@@ -1,12 +1,12 @@
-$NetBSD: patch-tools_install.py,v 1.4 2016/01/21 11:45:30 fhajny Exp $
+$NetBSD: patch-tools_install.py,v 1.5 2017/04/12 11:26:52 fhajny Exp $
 
 Install man pages under the right directory.
 
---- tools/install.py.orig	2016-01-21 01:08:27.000000000 +0000
+--- tools/install.py.orig	2017-04-11 15:48:50.000000000 +0000
 +++ tools/install.py
-@@ -141,10 +141,7 @@ def files(action):
- 
-   action(['deps/v8/tools/gdbinit'], 'share/doc/node/')
+@@ -136,10 +136,7 @@ def files(action):
+   action(['deps/v8/tools/lldbinit'], 'share/doc/node/')
+   action(['deps/v8/tools/lldb_commands.py'], 'share/doc/node/')
  
 -  if 'freebsd' in sys.platform or 'openbsd' in sys.platform:
 -    action(['doc/node.1'], 'man/man1/')
