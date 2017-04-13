b473b08d53ceb75bf2247bc43ae7a08cd40d4070
fhajny <fhajny@pkgsrc.org>
2017-04-13 19:04:53 +0000

    Update chat/ejabberd to 17.04. Make the Redis support unconditional (no extra dependencies).
    Changes in 17.04:
    
    Admin
    - Add more examples on config template
    - Generate ejabberd lib dir when not available in code server
    - Set default prefix to /usr/local
    - Start supervisors after ext_mod
    - Don't log warning on successful ping reply
    - New muc_register_nick command
    
    Core
    - Deprecate jlib.erl in favor of misc.erl
    - Add support for file-based queues
    - ejabberd_sm: Fix routing of headline and groupchat messages
    - Fix c2s connection close on demand
    - Improve overloaded S2S queue processing
    
    Databases
    - Improve Redis related code
    - Add Redis pool support
    - Improve logging of Redis errors
    - Add Redis and SQL as mod_proxy65 RAM backends
    - Add Redis and SQL as mod_carboncopy RAM backends
    - Add Redis and SQL as mod_bosh RAM backends
    - Add Redis and SQL as router RAM backends
    - Add SQL as mod_muc RAM backend
    - Remove obsolete Pubsub mnesia migration calls
    
    Miscellany
    - ejabberd_http: Expand @VERSION@ in custom headers
    - ejabberd_http: Add "custom_headers" option
    - mod_client_state: Queue stanzas of each full JID
    - mod_http_upload: Don't add "Server" header line
    - Pubsub: Refactor pubsub's get_last_items
    - Pubsub: Fix PEP issues

--- chat/ejabberd/options.mk
+++ chat/ejabberd/options.mk
@@ -1,17 +1,10 @@
-# $NetBSD: options.mk,v 1.4 2016/04/25 20:24:14 fhajny Exp $
+# $NetBSD: options.mk,v 1.5 2017/04/13 19:04:53 fhajny Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ejabberd
-PKG_SUPPORTED_OPTIONS=	redis sqlite
+PKG_SUPPORTED_OPTIONS=	sqlite
 PKG_SUGGESTED_OPTIONS=	#
 
 .include "../../mk/bsd.options.mk"
-
-#
-# Add Redis support
-#
-.if !empty(PKG_OPTIONS:Mredis)
-DEPENDS+=		erlang-eredis>=1.0.8.20150910:../../databases/erlang-eredis
-.endif
 
 #
 # Add SQLite 3 support
