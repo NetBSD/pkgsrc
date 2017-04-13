a4d2bbe85ace73bd2a6961242504bef4bd184bf3
khorben <khorben@pkgsrc.org>
2017-04-13 00:41:02 +0000

    Update DeforaOS libSystem to version 0.3.1
    This release brings:
    - build fixes with OBJDIR
    - minor API update in <System/string.h>
    - support for SOURCE_DATE_EPOCH in tests
    - minor fixes and improvements

--- devel/deforaos-libsystem/buildlink3.mk
+++ devel/deforaos-libsystem/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2016/02/19 20:32:04 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/04/13 00:41:02 khorben Exp $
 
 BUILDLINK_TREE+=	deforaos-libsystem
 
 .if !defined(DEFORAOS_LIBSYSTEM_BUILDLINK3_MK)
 DEFORAOS_LIBSYSTEM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-libsystem+=	deforaos-libsystem>=0.3.0
+BUILDLINK_API_DEPENDS.deforaos-libsystem+=	deforaos-libsystem>=0.3.1
 BUILDLINK_PKGSRCDIR.deforaos-libsystem?=	../../devel/deforaos-libsystem
 
 .endif	# DEFORAOS_LIBSYSTEM_BUILDLINK3_MK
