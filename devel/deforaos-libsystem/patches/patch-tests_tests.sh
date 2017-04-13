cfd491db3f890c16c76aabe4050234af5477c67c
khorben <khorben@pkgsrc.org>
2017-04-13 00:41:02 +0000

    Update DeforaOS libSystem to version 0.3.1
    This release brings:
    - build fixes with OBJDIR
    - minor API update in <System/string.h>
    - support for SOURCE_DATE_EPOCH in tests
    - minor fixes and improvements

--- devel/deforaos-libsystem/patches/patch-tests_tests.sh
+++ devel/deforaos-libsystem/patches/patch-tests_tests.sh
@@ -1,25 +1,18 @@
-$NetBSD: patch-tests_tests.sh,v 1.4 2015/10/17 17:57:28 khorben Exp $
+$NetBSD: patch-tests_tests.sh,v 1.5 2017/04/13 00:41:02 khorben Exp $
 
 Avoid building the (experimental) Python binding.
 Use LD_LIBRARY_PATH to find pre-installed libraries.
 
---- tests/tests.sh.orig	2015-10-14 23:30:20.000000000 +0000
+--- tests/tests.sh.orig	2017-04-13 00:29:45.000000000 +0000
 +++ tests/tests.sh
-@@ -42,7 +42,7 @@ _run()
- 	echo -n "$test:" 1>&2
- 	(echo
- 	echo "Testing: $test" "$@"
--	LD_LIBRARY_PATH="$OBJDIR../src" "$OBJDIR$test" "$@") 2>&1
-+	env LD_LIBRARY_PATH="$OBJDIR../src" "$OBJDIR$test" "$@") 2>&1
- 	res=$?
- 	if [ $res -ne 0 ]; then
- 		echo "Test: $test$sep$@: FAIL (error $res)"
-@@ -107,7 +107,7 @@ _test "includes"
- _test "string"
- _test "variable"
- echo "Expected failures:" 1>&2
--_fail "python.sh"
-+#_fail "python.sh"
- if [ -n "$FAILED" ]; then
- 	echo "Failed tests:$FAILED" 1>&2
- 	exit 2
+@@ -114,8 +114,8 @@ failures=
+ 
+ if $PKGCONFIG --exists python-2.7; then
+ 	tests="$tests python.sh"
+-else
+-	failures="$failures python.sh"
++#else
++#	failures="$failures python.sh"
+ fi
+ 
+ $DATE > "$target"
