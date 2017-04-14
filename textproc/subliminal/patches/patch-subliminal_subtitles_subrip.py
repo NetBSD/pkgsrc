50ac19aed0a7876e34b02d3245c7e6995c2a9e66
leot <leot@pkgsrc.org>
2017-04-14 19:10:23 +0000

    Fix indentation and incorrect __future__ import in patch-subliminal_subtitles_subrip.py
    Bump PKGREVISION

--- textproc/subliminal/patches/patch-subliminal_subtitles_subrip.py
+++ textproc/subliminal/patches/patch-subliminal_subtitles_subrip.py
@@ -1,24 +1,25 @@
-$NetBSD: patch-subliminal_subtitles_subrip.py,v 1.1 2017/04/14 14:17:45 leot Exp $
+$NetBSD: patch-subliminal_subtitles_subrip.py,v 1.2 2017/04/14 19:10:23 leot Exp $
 
-Use print() function from the __future__ in order to make the code compatible
-with Python 3.
+- Use print() function from the __future__ in order to make the code compatible
+  with Python 3.
+- Fix an indentation error.
 
 --- subliminal/subtitles/subrip.py.orig	2016-06-24 19:30:34.000000000 +0000
 +++ subliminal/subtitles/subrip.py
-@@ -1,5 +1,6 @@
+@@ -1,4 +1,5 @@
  # -*- coding: utf-8 -*-
- import re
 +from __future__ import print_function
+ import re
  from datetime import time
  
- from subliminal.subtitles import Cue
 @@ -73,10 +74,10 @@ class SubripSubtitle(object):
  
  
  if __name__ == '__main__':
 -    print read_cue('toto')
+-      i = 0
 +    print(read_cue('toto'))
-       i = 0
++    i = 0
      for x in read_cue('toto'):
 -        print x
 +        print(x)
