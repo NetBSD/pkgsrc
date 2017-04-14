8327710ce5079bb1e06fda601ba3e60d8886b3ac
bouyer <bouyer@pkgsrc.org>
2017-04-14 16:28:40 +0000

    Update to 1.3.31. Changes since 1.2.0: Implement Parallel Index Lines (PILs) Use an EBL to generate a perpendicular index line Allow Ctrl-Left Mouse click to select multiple boundaries. Right click   then allows Merge with keep and Merge with delete Add ability to move boundaries and point using left click and drag. Ability  is set in general properties of plugin Add ability to constrain boundary movement Add ability to move a single boundary segment

--- geography/opencpn-plugin-draw/patches/patch-cmake_PluginConfigure.cmake
+++ geography/opencpn-plugin-draw/patches/patch-cmake_PluginConfigure.cmake
@@ -1,23 +1,22 @@
-$NetBSD: patch-cmake_PluginConfigure.cmake,v 1.2 2016/05/21 10:50:41 bouyer Exp $
+$NetBSD: patch-cmake_PluginConfigure.cmake,v 1.3 2017/04/14 16:28:40 bouyer Exp $
 Do not overwrite CMAKE_SHARED_LINKER_FLAGS
 
---- cmake/PluginConfigure.cmake.orig	2016-04-13 08:28:11.000000000 +0200
-+++ cmake/PluginConfigure.cmake	2016-05-21 12:20:47.579936156 +0200
+--- cmake/PluginConfigure.cmake.orig	2017-03-12 09:27:12.000000000 +0100
++++ cmake/PluginConfigure.cmake	2017-04-14 13:29:02.000000000 +0200
 @@ -45,11 +45,13 @@
  #  ADD_DEFINITIONS( "-Wall -Wno-unused-result -g -O2 -fexceptions" )
   ENDIF(PROFILING)
  
 - IF(NOT APPLE)
--  SET(CMAKE_SHARED_LINKER_FLAGS "-Wl,-Bsymbolic")
-- ELSE(NOT APPLE)
 + IF(CMAKE_SYSTEM_NAME MATCHES ".*Linux")
-+   SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-Bsymbolic")
-+   ADD_DEFINITIONS( "-fPIC")
+   SET( CMAKE_SHARED_LINKER_FLAGS "-Wl,-Bsymbolic")
+- ELSE(NOT APPLE)
++  ADD_DEFINITIONS( "-fPIC")
 + ENDIF(CMAKE_SYSTEM_NAME MATCHES ".*Linux")
 + IF(APPLE)
-   SET(CMAKE_SHARED_LINKER_FLAGS "-Wl -undefined dynamic_lookup")
+   SET( CMAKE_SHARED_LINKER_FLAGS "-Wl -undefined dynamic_lookup")
 - ENDIF(NOT APPLE)
 + ENDIF(APPLE)
  
- ENDIF(NOT MSVC)
  
+ ENDIF(NOT MSVC)
