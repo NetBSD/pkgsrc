0a57c8674a1e0d0df5261c516f1c0b1510f2553a
kim <kim@pkgsrc.org>
2017-04-15 22:31:49 +0000

    Update to 4.5.1
    Version 4.5.1 (25/02/2017):
      *Fixes:
      - logfile permissions problem (CVE-2017-5618)
      - SunOS build problem (bug #50089)
      - FreeBSD core dumps (bug #50143)

--- misc/screen/patches/patch-screen.c
+++ misc/screen/patches/patch-screen.c
@@ -1,7 +1,6 @@
-$NetBSD: patch-screen.c,v 1.3 2017/01/25 01:17:16 maya Exp $
+$NetBSD: patch-screen.c,v 1.4 2017/04/15 22:31:49 kim Exp $
 
 Use standard headers.
-Don't open user-supplied paths as root.
 
 --- screen.c.orig	2017-01-17 19:28:29.000000000 +0000
 +++ screen.c
@@ -14,21 +13,3 @@ Don't open user-supplied paths as root.
  #include "screen.h"
  #ifdef HAVE_BRAILLE
  # include "braille.h"
-@@ -674,11 +676,17 @@ int main(int ac, char** av)
-               if (strlen(screenlogfile) > PATH_MAX)
-                 Panic(0, "-L: logfile name too long. (max. %d char)", PATH_MAX);
- 
-+	      int issetugid = geteuid() == 0;
-+
-+	      if (issetugid && seteuid(getuid()) < 0)
-+		Panic(0, "setuid 1");
-               FILE *w_check;
-               if ((w_check = fopen(screenlogfile, "w")) == NULL)
-                 Panic(0, "-L: logfile name access problem");
-               else
-                 fclose(w_check);
-+	      if (issetugid && seteuid(0) < 0)
-+		Panic(0, "setuid 2");
-             }
-             nwin_options.Lflag = 1;
-             break;
