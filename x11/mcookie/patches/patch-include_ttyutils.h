0199bcec75a38cf1c40bff51667553a048ec8f21
bsiegert <bsiegert@pkgsrc.org>
2017-04-16 15:15:00 +0000

    Pullup ticket #5278 - requested by sevan x11/mcookie: build fix
    Revisions pulled up:
    - x11/mcookie/distinfo                                          1.7
    - x11/mcookie/patches/patch-include_ttyutils.h                  1.2
    
    ---
       Module Name:    pkgsrc
       Committed By:   jperkin
       Date:           Thu Apr 13 10:17:25 UTC 2017
    
       Modified Files:
               pkgsrc/x11/mcookie: distinfo
               pkgsrc/x11/mcookie/patches: patch-include_ttyutils.h
    
       Log Message:
       Add a bunch more compatibility defines.  Fixes SunOS.

--- x11/mcookie/patches/patch-include_ttyutils.h
+++ x11/mcookie/patches/patch-include_ttyutils.h
@@ -1,10 +1,29 @@
-$NetBSD: patch-include_ttyutils.h,v 1.1 2016/09/20 08:00:51 wiz Exp $
+$NetBSD: patch-include_ttyutils.h,v 1.1.6.1 2017/04/16 15:15:00 bsiegert Exp $
 
 Define values if missing.
 
---- include/ttyutils.h.orig	2016-01-06 10:50:36.000000000 +0000
+--- include/ttyutils.h.orig	2016-11-02 12:57:31.535167960 +0000
 +++ include/ttyutils.h
-@@ -114,6 +114,13 @@ static inline void reset_virtual_console
+@@ -60,6 +60,18 @@ extern int get_terminal_name(const char
+ static inline void reset_virtual_console(struct termios *tp, int flags)
+ {
+ 	/* Use defaults of <sys/ttydefaults.h> for base settings */
++#ifndef TTYDEF_IFLAG
++#define TTYDEF_IFLAG	(BRKINT | ICRNL | IMAXBEL | IXON | IXANY)
++#endif
++#ifndef TTYDEF_OFLAG
++#define TTYDEF_OFLAG	(OPOST | ONLCR /*| OXTABS*/)
++#endif
++#ifndef TTYDEF_LFLAG
++#define TTYDEF_LFLAG	(ECHO | ICANON | ISIG | IEXTEN | ECHOE|ECHOKE|ECHOCTL)
++#endif
++#ifndef TTYDEF_CFLAG
++#define TTYDEF_CFLAG	(CREAD | CS8 | HUPCL)
++#endif
+ 	tp->c_iflag |= TTYDEF_IFLAG;
+ 	tp->c_oflag |= TTYDEF_OFLAG;
+ 	tp->c_lflag |= TTYDEF_LFLAG;
+@@ -114,6 +126,13 @@ static inline void reset_virtual_console
  # define FFDLY 0
  #endif
  
@@ -18,3 +37,17 @@ Define values if missing.
  	tp->c_iflag |=  (BRKINT | ICRNL | IMAXBEL);
  	tp->c_iflag &= ~(IGNBRK | INLCR | IGNCR | IXOFF | IUCLC | IXANY | ISTRIP);
  	tp->c_oflag |=  (OPOST | ONLCR | NL0 | CR0 | TAB0 | BS0 | VT0 | FF0);
+@@ -158,7 +177,13 @@ static inline void reset_virtual_console
+ 	tp->c_cc[VSTOP]    = CSTOP;
+ 	tp->c_cc[VSUSP]    = CSUSP;
+ 	tp->c_cc[VEOL]     = _POSIX_VDISABLE;
++#ifndef CREPRINT
++#define CREPRINT	('r' & 037)
++#endif
+ 	tp->c_cc[VREPRINT] = CREPRINT;
++#ifndef CDISCARD
++#define CDISCARD	('o' & 037)
++#endif
+ 	tp->c_cc[VDISCARD] = CDISCARD;
+ 	tp->c_cc[VWERASE]  = CWERASE;
+ 	tp->c_cc[VLNEXT]   = CLNEXT;
