de7e1eaef1331bdcbab7e37fe1a65b686635ee8f
bsiegert <bsiegert@pkgsrc.org>
2017-04-17 14:54:45 +0000

    Pullup ticket #5289 - requested by sevan devel/ruby-eet: build fix
    Revisions pulled up:
    - devel/ruby-eet/Makefile                                       1.17
    - devel/ruby-eet/PLIST                                          1.2
    - devel/ruby-eet/distinfo                                       1.5
    - devel/ruby-eet/patches/patch-Rakefile                         1.2
    - devel/ruby-eet/patches/patch-ext_ext.c                        1.1
    - devel/ruby-eet/patches/patch-rake_configuretask.rb            1.1
    - devel/ruby-eet/patches/patch-rake_extensiontask.rb            1.2
    
    ---
       Module Name:    pkgsrc
       Committed By:   minskim
       Date:           Fri Apr 14 21:35:35 UTC 2017
    
       Modified Files:
               pkgsrc/devel/ruby-eet: Makefile PLIST distinfo
               pkgsrc/devel/ruby-eet/patches: patch-Rakefile
                   patch-rake_extensiontask.rb
       Added Files:
               pkgsrc/devel/ruby-eet/patches: patch-ext_ext.c
                   patch-rake_configuretask.rb
    
       Log Message:
       Make ruby-eet build with ruby-2.x
    
       This no longer builds with ruby-1.8.x, which will be removed soon.

--- devel/ruby-eet/patches/patch-rake_extensiontask.rb
+++ devel/ruby-eet/patches/patch-rake_extensiontask.rb
@@ -1,15 +1,34 @@
-$NetBSD: patch-rake_extensiontask.rb,v 1.1 2011/08/12 16:29:04 taca Exp $
+$NetBSD: patch-rake_extensiontask.rb,v 1.1.46.1 2017/04/17 14:54:45 bsiegert Exp $
 
 * Add proper compiler flags.
 
 --- rake/extensiontask.rb.orig	2007-03-23 11:05:12.000000000 +0000
 +++ rake/extensiontask.rb
-@@ -98,7 +98,7 @@ module Rake
+@@ -98,15 +98,15 @@ module Rake
      def define_rules
        for ext in env[:c_exts]
          Rake::Task.create_rule '.'+env[:objext] => '.'+ext do |r|
 -          sh_cmd :cc, :cflags, :cppflags, {'-D' => :defines}, {'-I' => :includedirs}, {'-I' => :topdir},
+-                '-c', '-o', r.name, r.sources
 +          sh_cmd :cc, :cflags, :ccdlflags, :cppflags, {'-D' => :defines}, {'-I' => :includedirs}, {'-I' => :topdir},
-                 '-c', '-o', r.name, r.sources
++                '-c', '-o', r.name, r.sources.join(' ')
          end
        end
+ 
+       for ext in env[:cpp_exts]
+         Rake::Task.create_rule '.'+env[:objext] => '.'+ext do |r|
+           sh_cmd :cxx, :cxxflags, :cppflags, {'-D' => :defines}, {'-I' => :includedirs}, {'-I' => :topdir},
+-                '-o', r.name, '-c', r.sources
++                '-o', r.name, '-c', r.sources.join(' ')
+         end
+       end
+     end
+@@ -121,7 +121,7 @@ module Rake
+         @@DefaultEnv = e
+       end
+ 
+-      Config::CONFIG.merge(ENV).each { |k, v| @@DefaultEnv[k.downcase.to_sym] = v }
++      RbConfig::CONFIG.merge(ENV).each { |k, v| @@DefaultEnv[k.downcase.to_sym] = v }
+       @@DefaultEnv = {
+         :cxx => 'c++',
+         :cxxflags => '',
