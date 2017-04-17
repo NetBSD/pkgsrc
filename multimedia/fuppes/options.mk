1ec0f73facbab6b0c65cd25bc652be0601732f4a
wiz <wiz@pkgsrc.org>
2017-04-17 08:43:02 +0000

    Fix previous: mp4v2 is for multimedia/mp4v2; use libmp4v2 instead.

--- multimedia/fuppes/options.mk
+++ multimedia/fuppes/options.mk
@@ -1,14 +1,14 @@
-# $NetBSD: options.mk,v 1.4 2017/04/17 08:41:30 wiz Exp $
+# $NetBSD: options.mk,v 1.5 2017/04/17 08:43:02 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fuppes
-PKG_SUPPORTED_OPTIONS=	mp4v2 mysql
-PKG_SUGGESTED_OPTIONS=	mp4v2
+PKG_SUPPORTED_OPTIONS=	libmp4v2 mysql
+PKG_SUGGESTED_OPTIONS=	libmp4v2
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=		${PKG_SUPPORTED_OPTIONS}
 
-.if !empty(PKG_OPTIONS:Mmp4v2)
+.if !empty(PKG_OPTIONS:Mlibmp4v2)
 .include "../../multimedia/libmp4v2/buildlink3.mk"
 .endif
 
