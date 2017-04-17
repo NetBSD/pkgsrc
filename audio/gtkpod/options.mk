2e64ca1d729557e4af58fdbebb5a5197f88c2a6d
wiz <wiz@pkgsrc.org>
2017-04-17 08:45:17 +0000

    Fix option name, this is libmp4v2, not mpeg4ip.

--- audio/gtkpod/options.mk
+++ audio/gtkpod/options.mk
@@ -1,12 +1,11 @@
-# $NetBSD: options.mk,v 1.3 2007/02/09 20:32:38 drochner Exp $
+# $NetBSD: options.mk,v 1.4 2017/04/17 08:45:52 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.gtkpod
-PKG_SUPPORTED_OPTIONS=		mpeg4ip
+PKG_SUPPORTED_OPTIONS=		libmp4v2
 PKG_SUGGESTED_OPTIONS=
-PKG_OPTIONS_LEGACY_OPTS+=	faad:mpeg4ip
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mmpeg4ip)
+.if !empty(PKG_OPTIONS:Mlibmp4v2)
 .  include "../../multimedia/libmp4v2/buildlink3.mk"
 .endif
