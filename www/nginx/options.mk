0ff66c36a199be166514b665cdc0f69b29e1c160
bsiegert <bsiegert@pkgsrc.org>
2017-04-17 14:49:23 +0000

    Pullup ticket #5290 - requested by sevan www/nginx: option name change
    Revisions pulled up:
    - www/nginx/options.mk                                          1.37
    
    ---
       Module Name:    pkgsrc
       Committed By:   ryoon
       Date:           Sat Apr 15 05:46:28 UTC 2017
    
       Modified Files:
               pkgsrc/www/nginx: options.mk
    
       Log Message:
       Change v2 option name to http2
    
       http2 is defined in pkgsrc/mk/defaults/options.description

--- www/nginx/options.mk
+++ www/nginx/options.mk
@@ -1,11 +1,11 @@
-# $NetBSD: options.mk,v 1.36 2016/06/15 14:49:11 fhajny Exp $
+# $NetBSD: options.mk,v 1.36.8.1 2017/04/17 14:49:23 bsiegert Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nginx
 PKG_SUPPORTED_OPTIONS=	dav flv gtools inet6 luajit mail-proxy memcache naxsi \
 			pcre push realip ssl sub uwsgi image-filter \
 			debug status nginx-autodetect-cflags echo \
 			set-misc headers-more array-var encrypted-session \
-			form-input perl gzip v2
+			form-input perl gzip http2
 
 PKG_SUGGESTED_OPTIONS=	inet6 pcre ssl
 
@@ -45,7 +45,7 @@ CONFIGURE_ARGS+=	--with-http_dav_module
 CONFIGURE_ARGS+=	--with-http_flv_module
 .endif
 
-.if !empty(PKG_OPTIONS:Mv2)
+.if !empty(PKG_OPTIONS:Mhttp2)
 CONFIGURE_ARGS+=	--with-http_v2_module
 .endif
 
