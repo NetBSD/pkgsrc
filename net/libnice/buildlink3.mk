7caad8028f00ebebc494e43ea5d6a8d0846942cb
wiz <wiz@pkgsrc.org>
2017-04-19 13:28:05 +0000

    Updated libnice to 0.1.14.
    libnice 0.1.14 (2017-04-03)
    ===========================
    Improved RFC compliance
    Split verbose logs into a separate option
    Numerous bug fixes
    Use GnuTLS for hash functions
    Implement NewReno in PseudoTCP
    Requires GLib 2.44 GnuTLS 2.12

--- net/libnice/buildlink3.mk
+++ net/libnice/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.24 2016/12/04 05:17:10 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2017/04/19 13:28:05 wiz Exp $
 
 BUILDLINK_TREE+=	libnice
 
@@ -6,10 +6,11 @@ BUILDLINK_TREE+=	libnice
 LIBNICE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libnice+=	libnice>=0.0.9
-BUILDLINK_ABI_DEPENDS.libnice+=	libnice>=0.1.13nb4
+BUILDLINK_ABI_DEPENDS.libnice+=	libnice>=0.1.14
 BUILDLINK_PKGSRCDIR.libnice?=	../../net/libnice
 
 .include "../../devel/glib2/buildlink3.mk"
+.include "../../security/gnutls/buildlink3.mk"
 .include "../../net/gupnp-igd/buildlink3.mk"
 .endif	# LIBNICE_BUILDLINK3_MK
 
