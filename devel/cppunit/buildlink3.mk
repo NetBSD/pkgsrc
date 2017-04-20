85029ddfaec7c18ed1e0af18a2bf1f0019735952
jperkin <jperkin@pkgsrc.org>
2017-04-20 08:39:03 +0000

    Enable C++11 for all dependencies of cppunit, required as cppunit is now built with it enabled.

--- devel/cppunit/buildlink3.mk
+++ devel/cppunit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2012/05/07 01:53:20 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2017/04/20 08:39:03 jperkin Exp $
 
 BUILDLINK_TREE+=	cppunit
 
@@ -10,5 +10,8 @@ BUILDLINK_ABI_DEPENDS.cppunit+=	cppunit>=1.10.2nb5
 BUILDLINK_PKGSRCDIR.cppunit?=	../../devel/cppunit
 BUILDLINK_CPPFLAGS.cppunit+=	-I${BUILDLINK_PREFIX.cppunit}/include/cppunit
 .endif # CPPUNIT_BUILDLINK3_MK
+
+# cppunit enables C++11 so all dependencies must be built the same way
+CXXFLAGS+=		-std=c++11
 
 BUILDLINK_TREE+=	-cppunit
