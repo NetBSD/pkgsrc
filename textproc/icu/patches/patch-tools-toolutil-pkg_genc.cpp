38eba3b1be8beb440804d6c33d4abc07d92677c9
adam <adam@pkgsrc.org>
2017-04-22 20:11:45 +0000

    Changes 59.1: * Emoji 5.0 data * Includes bidi data files from Unicode 10 beta. * Includes segmentation data files and rules from Unicode 10 beta and CLDR 31.0.1. * Does not yet include the Emoji_Component property. * Otherwise ICU 59 continues to use Unicode 9 data. CLDR 31.0.1 * Including updates for emoji 5.0, for example local names for England, Scotland, and Wales. * GMT and UTC are no longer unified, and CLDR provides distinct UTC display names, avoiding confusion with standard (winter) time in Britain. * See the CLDR download page for other CLDR features and migration issues in CLDR v31. New case mapping API (C++ & Java classes CaseMap) supports styled text.

--- textproc/icu/patches/patch-tools-toolutil-pkg_genc.cpp
+++ textproc/icu/patches/patch-tools-toolutil-pkg_genc.cpp
@@ -1,11 +1,11 @@
-$NetBSD: patch-tools-toolutil-pkg_genc.c,v 1.2 2015/04/06 07:22:54 adam Exp $
+$NetBSD: patch-tools-toolutil-pkg_genc.cpp,v 1.1 2017/04/22 20:11:45 adam Exp $
 
 Older assemblers for Darwin (like the one from Xcode 2.5) do not
 have .balign, but they take .align 4 to mean 2^4 = 16 bytes.
 
---- tools/toolutil/pkg_genc.c.orig	2015-03-27 21:10:56.000000000 +0000
-+++ tools/toolutil/pkg_genc.c
-@@ -137,7 +137,7 @@ static const struct AssemblyType {
+--- tools/toolutil/pkg_genc.cpp.orig	2015-03-27 21:10:56.000000000 +0000
++++ tools/toolutil/pkg_genc.cpp
+@@ -139,7 +139,7 @@ static const struct AssemblyType {
          "#endif\n"
          "\t.data\n"
          "\t.const\n"
