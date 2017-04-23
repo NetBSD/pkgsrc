24f5da1e351ebc4e1a5605f97a47026194f91616
wiz <wiz@pkgsrc.org>
2017-04-23 09:58:31 +0000

    Add -std=c++11 to CXXFLAGS.
    icu uses char16_t in a central header file; this type is added by c++11.

--- textproc/icu/buildlink3.mk
+++ textproc/icu/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.30 2016/12/04 04:11:32 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2017/04/23 09:58:31 wiz Exp $
 
 BUILDLINK_TREE+=	icu
 
@@ -9,5 +9,8 @@ BUILDLINK_API_DEPENDS.icu+=	icu>=3.4
 BUILDLINK_ABI_DEPENDS.icu+=	icu>=58.1
 BUILDLINK_PKGSRCDIR.icu?=	../../textproc/icu
 .endif # ICU_BUILDLINK3_MK
+
+# icu uses char16_t in a central header file, which is added by c++11
+CXXFLAGS+=	-std=c++11
 
 BUILDLINK_TREE+=	-icu
