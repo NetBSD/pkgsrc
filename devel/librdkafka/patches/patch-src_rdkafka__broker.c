eb711a59654fc249f1bbfa80ba3cf8134e952039
fhajny <fhajny@pkgsrc.org>
2017-04-24 12:20:37 +0000

    * Use bash for CONFIG_SHELL * Fix build on NetBSD * Uses python to build * Enable tests

--- devel/librdkafka/patches/patch-src_rdkafka__broker.c
+++ devel/librdkafka/patches/patch-src_rdkafka__broker.c
@@ -1,15 +1,16 @@
-$NetBSD: patch-src_rdkafka__broker.c,v 1.1 2017/04/20 08:01:19 fhajny Exp $
+$NetBSD: patch-src_rdkafka__broker.c,v 1.2 2017/04/24 12:20:37 fhajny Exp $
 
-XPG6 is needed.
+Do not mess with _XOPEN_SOURCE here, it's needed generally and elsewhere.
 
 --- src/rdkafka_broker.c.orig	2017-04-12 08:36:34.000000000 +0000
 +++ src/rdkafka_broker.c
-@@ -31,7 +31,7 @@
+@@ -30,9 +30,6 @@
+ 
  #ifndef _MSC_VER
  #define _GNU_SOURCE
- #ifndef _AIX    /* AIX defines this and the value needs to be set correctly */
+-#ifndef _AIX    /* AIX defines this and the value needs to be set correctly */
 -#define _XOPEN_SOURCE
-+#define _XOPEN_SOURCE 600
- #endif
+-#endif
  #include <signal.h>
  #endif
+ 
