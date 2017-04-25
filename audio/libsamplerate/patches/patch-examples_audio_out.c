339061e9abb934516bf381ec9c3f6bfaffb377f3
bsiegert <bsiegert@pkgsrc.org>
2017-04-25 17:59:54 +0000

    Pullup ticket #5358 - requested by sevan audio/libsamplerate: build fix
    Revisions pulled up:
    - audio/libsamplerate/distinfo                                  1.12
    - audio/libsamplerate/patches/patch-examples_audio__out.h       1.1
    - audio/libsamplerate/patches/patch-examples_audio_out.c        1.2
    
    ---
       Module Name:    pkgsrc
       Committed By:   maya
       Date:           Mon Apr 24 10:08:40 UTC 2017
    
       Modified Files:
               pkgsrc/audio/libsamplerate: distinfo
               pkgsrc/audio/libsamplerate/patches: patch-examples_audio_out.c
       Added Files:
               pkgsrc/audio/libsamplerate/patches: patch-examples_audio__out.h
    
       Log Message:
       libsamplerate: declare AUDIO_OUT_s once and in audio_out.h
       Fixes build failure with GCC 4.5.3 (netbsd 6.0)

--- audio/libsamplerate/patches/patch-examples_audio_out.c
+++ audio/libsamplerate/patches/patch-examples_audio_out.c
@@ -1,10 +1,23 @@
-$NetBSD: patch-examples_audio_out.c,v 1.1 2012/07/04 08:59:23 adam Exp $
+$NetBSD: patch-examples_audio_out.c,v 1.1.38.1 2017/04/25 17:59:54 bsiegert Exp $
 
 Fix building on Mac OS X.
+declare typedef AUDIO_OUT_s once and in audio_out.h
 
---- examples/audio_out.c.orig	2012-06-22 20:14:44.000000000 +0000
+--- examples/audio_out.c.orig	2016-09-13 10:20:20.000000000 +0000
 +++ examples/audio_out.c
-@@ -168,7 +168,6 @@ linux_close (AUDIO_OUT *audio_out)
+@@ -33,11 +33,6 @@
+ #define MAKE_MAGIC(a,b,c,d,e,f,g,h)		\
+ 			((a) + ((b) << 1) + ((c) << 2) + ((d) << 3) + ((e) << 4) + ((f) << 5) + ((g) << 6) + ((h) << 7))
+ 
+-typedef	struct AUDIO_OUT_s
+-{	int magic ;
+-} AUDIO_OUT ;
+-
+-
+ /*------------------------------------------------------------------------------
+ **	Linux (ALSA and OSS) functions for playing a sound.
+ */
+@@ -432,7 +427,6 @@ opensoundsys_close (AUDIO_OUT *audio_out
  
  #if (defined (__MACH__) && defined (__APPLE__)) /* MacOSX */
  
