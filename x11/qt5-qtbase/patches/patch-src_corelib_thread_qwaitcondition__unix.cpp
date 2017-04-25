808d13974a313efd9ccd4a4c86d875896920ce61
bsiegert <bsiegert@pkgsrc.org>
2017-04-25 18:29:40 +0000

    Pullup ticket #5335 - requested by maya x11/qt5-qtbase: build fix
    Revisions pulled up:
    - x11/qt5-qtbase/Makefile                                       1.42
    - x11/qt5-qtbase/distinfo                                       1.32
    - x11/qt5-qtbase/patches/patch-src_corelib_thread_qwaitcondition__unix.cpp 1.1
    
    ---
       Module Name:    pkgsrc
       Committed By:   maya
       Date:           Thu Apr 20 18:38:09 UTC 2017
    
       Modified Files:
               pkgsrc/x11/qt5-qtbase: Makefile
    
       Log Message:
       qt5-qtbase: FreeBSD needs -lutil
    
       PR pkg/52181 from Santhosh Raju
    
    ---
       Module Name:    pkgsrc
       Committed By:   maya
       Date:           Mon Apr 24 12:27:59 UTC 2017
    
       Modified Files:
               pkgsrc/x11/qt5-qtbase: distinfo
       Added Files:
               pkgsrc/x11/qt5-qtbase/patches:
                   patch-src_corelib_thread_qwaitcondition__unix.cpp
    
       Log Message:
       qt5-qtbase: fix build on NetBSD < 6.1 (no pthread_condattr_setclock)
       Invert logic from !defined && to defined || for simplicity and use
       a netbsd version check.

---
+++ x11/qt5-qtbase/patches/patch-src_corelib_thread_qwaitcondition__unix.cpp
@@ -0,0 +1,27 @@
+$NetBSD: patch-src_corelib_thread_qwaitcondition__unix.cpp,v 1.1.2.2 2017/04/25 18:29:40 bsiegert Exp $
+
+don't have pthread_condattr_setclock on NetBSD < 6.1, don't
+use it.
+
+--- src/corelib/thread/qwaitcondition_unix.cpp.orig	2015-10-13 04:35:30.000000000 +0000
++++ src/corelib/thread/qwaitcondition_unix.cpp
+@@ -45,6 +45,7 @@
+ 
+ #include <errno.h>
+ #include <sys/time.h>
++#include <sys/param.h>
+ #include <time.h>
+ 
+ #ifndef QT_NO_THREAD
+@@ -77,7 +78,10 @@ void qt_initialize_pthread_cond(pthread_
+ #if defined(Q_OS_ANDROID)
+     if (local_condattr_setclock && QElapsedTimer::clockType() == QElapsedTimer::MonotonicClock)
+         local_condattr_setclock(&condattr, CLOCK_MONOTONIC);
+-#elif !defined(Q_OS_MAC) && !defined(Q_OS_HAIKU)
++#elif (defined(Q_OS_NETBSD) && (__NetBSD_Version__ < 600010000)) || \
++    defined(Q_OS_MAC) || defined(Q_OS_HAIKU)
++    /* nothing, don't have pthread_condattr_setclock */
++#else
+     if (QElapsedTimer::clockType() == QElapsedTimer::MonotonicClock)
+         pthread_condattr_setclock(&condattr, CLOCK_MONOTONIC);
+ #endif
