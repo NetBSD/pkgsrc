21f56c5fd34b3b01787cff466c0be842939dd12a
schmonz <schmonz@pkgsrc.org>
2017-04-27 20:21:51 +0000

    Wrap long command line. Replace LOCALBASE with PREFIX.

--- security/cvm/files/cvm.sh
+++ security/cvm/files/cvm.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: cvm.sh,v 1.1.1.1 2005/07/20 03:28:42 schmonz Exp $
+# $NetBSD: cvm.sh,v 1.2 2017/04/27 20:21:51 schmonz Exp $
 #
 
 # PROVIDE: cvm
@@ -15,23 +15,27 @@ name="cvm"
 : ${cvm_protocol:="local"}
 : ${cvm_log:="YES"}
 : ${cvm_logcmd:="logger -t nb${name} -p auth.info"}
-: ${cvm_nologcmd:="@LOCALBASE@/bin/multilog -*"}
+: ${cvm_nologcmd:="@PREFIX@/bin/multilog -*"}
 
 if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
 rcvar=${name}
-command="@LOCALBASE@/bin/cvm-${cvm_module}"
+command="@PREFIX@/bin/cvm-${cvm_module}"
 start_precmd="cvm_precmd"
 
 cvm_precmd()
 {
-	if [ -f /etc/rc.subr ]; then
-		checkyesno cvm_log || cvm_logcmd=${cvm_nologcmd}
+	if [ -f /etc/rc.subr ] && ! checkyesno cvm_log; then
+		cvm_logcmd=${cvm_nologcmd}
 	fi
 	umask 0
-	command="@SETENV@ - ${cvm_postenv} @LOCALBASE@/bin/softlimit -m ${cvm_datalimit} @LOCALBASE@/bin/cvm-${cvm_module} cvm-${cvm_protocol}:@VARBASE@/run/cvm-${cvm_module} 2>&1 | @LOCALBASE@/bin/setuidgid cvmlog ${cvm_logcmd}"
+	command="@SETENV@ - ${cvm_postenv}
+@PREFIX@/bin/softlimit -m ${cvm_datalimit} @PREFIX@/bin/cvm-${cvm_module}
+cvm-${cvm_protocol}:@VARBASE@/run/cvm-${cvm_module}
+2>&1 |
+@PREFIX@/bin/setuidgid cvmlog ${cvm_logcmd}"
 	command_args="&"
 	rc_flags=""
 }
