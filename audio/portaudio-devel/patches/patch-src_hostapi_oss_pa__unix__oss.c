a6ceca0bdd2d94f284187c08be9be267b0c4f2dc
nat <nat@pkgsrc.org>
2017-04-27 07:14:01 +0000

    Don't write an endless stream of silence whilst preparing playback. Bump PKGREVISION.
    Addresses PR kern/52196.

---
+++ audio/portaudio-devel/patches/patch-src_hostapi_oss_pa__unix__oss.c
@@ -0,0 +1,22 @@
+$NetBSD: patch-src_hostapi_oss_pa__unix__oss.c,v 1.1 2017/04/27 07:14:01 nat Exp $
+
+Don't write an endless stream of silence when preparing the stream.
+
+--- src/hostapi/oss/pa_unix_oss.c.orig	2016-10-30 01:23:04.000000000 +0000
++++ src/hostapi/oss/pa_unix_oss.c
+@@ -1479,6 +1479,7 @@ static PaError PaOssStream_Prepare( PaOs
+ 
+         /* Looks like we have to turn off blocking before we try this, but if we don't fill the buffer
+          * OSS will complain. */
++#ifndef __NetBSD__
+         PA_ENSURE( ModifyBlocking( stream->playback->fd, 0 ) );
+         while (1)
+         {
+@@ -1486,6 +1487,7 @@ static PaError PaOssStream_Prepare( PaOs
+                 break;
+         }
+         PA_ENSURE( ModifyBlocking( stream->playback->fd, 1 ) );
++#endif
+     }
+ 
+     if( stream->sharedDevice )
