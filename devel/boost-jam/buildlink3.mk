28920f42be336b87729dda1f77187d9ea9ec7216
adam <adam@pkgsrc.org>
2017-04-29 18:31:42 +0000

    Changes 1.64.0: New Libraries Process library by Klemens D. Morgenstern, that provides cross platorm ways to allows you to: * create child processes * setup streams for child processes * communicate with child processes through streams (synchronously or asynchronously) * wait for processes to exit (synchronously or asynchronously) * terminate processes

--- devel/boost-jam/buildlink3.mk
+++ devel/boost-jam/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.30 2017/01/01 15:32:47 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2017/04/29 18:31:42 adam Exp $
 
 BUILDLINK_TREE+=	boost-jam
 
 .if !defined(BOOST_JAM_BUILDLINK3_MK)
 BOOST_JAM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.63.*
+BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.64.*
 BUILDLINK_DEPMETHOD.boost-jam?=		build
 BUILDLINK_PKGSRCDIR.boost-jam?=		../../devel/boost-jam
 .endif # BOOST_JAM_BUILDLINK3_MK
--- devel/py-boost/buildlink3.mk
+++ devel/py-boost/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2017/01/01 16:34:48 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2017/04/29 18:31:42 adam Exp $
 
 BUILDLINK_TREE+=	py-boost
 
@@ -8,8 +8,8 @@ PY_BOOST_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 # Use a dependency pattern that guarantees the proper ABI.
-BUILDLINK_API_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.63.*
-BUILDLINK_ABI_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.63.*
+BUILDLINK_API_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.64.*
+BUILDLINK_ABI_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.64.*
 BUILDLINK_PKGSRCDIR.py-boost?=	../../devel/py-boost
 
 .include "../../devel/boost-headers/buildlink3.mk"
