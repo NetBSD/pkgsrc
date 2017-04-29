2580fcfaf95c499bbfc3cb875d91743cc9bbba67
adam <adam@pkgsrc.org>
2017-04-29 18:18:52 +0000

    Changes in 3.2:
    * Added support for MPI-3.1 features including nonblocking collective I/O,
      address manipulation routines, thread-safety for MPI initialization,
      pre-init functionality, and new MPI_T routines to look up variables
      by name.
    
    * Fortran 2008 bindings are enabled by default and fully supported.
    
    * Added support for the Mellanox MXM InfiniBand interface.  (thanks
      to Mellanox for the code contribution).
    
    * Added support for the Mellanox HCOLL interface for collectives.
      (thanks to Mellanox for the code contribution).
    
    * Significant stability improvements to the MPICH/portals4
      implementation.
    
    * Completely revamped RMA infrastructure including several
      scalability improvements, performance improvements, and bug fixes.
    
    * Added experimental support for Open Fabrics Interfaces (OFI) version 1.0.0.
      https://github.com/ofiwg/libfabric (thanks to Intel for code contribution)
    
    * The Myrinet MX network module, which had a life cyle from 1.1 till
      3.1.2, has now been deleted.
    
    * Several other minor bug fixes, memory leak fixes, and code cleanup.

--- parallel/mpi-ch/options.mk
+++ parallel/mpi-ch/options.mk
@@ -1,12 +1,11 @@
-# $NetBSD: options.mk,v 1.3 2013/05/10 08:24:57 adam Exp $
+# $NetBSD: options.mk,v 1.4 2017/04/29 18:18:52 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mpich
-PKG_SUPPORTED_OPTIONS=	debug f90 mpich-mpd threads
-PKG_SUGGESTED_OPTIONS=	threads
+PKG_SUPPORTED_OPTIONS=	debug f90
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	f90 mpd
+PLIST_VARS+=	f90
 
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-g=debug	# problems
@@ -15,23 +14,10 @@ INSTALL_UNSTRIPPED=	yes
 
 .if !empty(PKG_OPTIONS:Mf90)
 USE_LANGUAGES+=		fortran
+CONFIGURE_ARGS+=	--enable-fortran=yes
 PLIST.f90=		yes
 .else
-USE_LANGUAGES+=		fortran77
-CONFIGURE_ARGS+=	--disable-fc
-.endif
-
-.if !empty(PKG_OPTIONS:Mthreads)
-CONFIGURE_ARGS+=	--with-thread-package=posix # aka pthreads
-.include "../../mk/pthread.buildlink3.mk"
-.endif
-
-# Allow coexistance of MPICH2 and musicpd by disabling MPICH2 parts:
-.if !empty(PKG_OPTIONS:Mmpich-mpd)
-CONFLICTS+=		musicpd-[0-9]*	# installs bin/mpd
-CONFIGURE_ARGS+=	--with-pm=hydra:mpd:gforker
-CONFIGURE_ARGS+=	--with-python=python${PYVERSSUFFIX}
-PLIST.mpd=		yes
-REPLACE_PYTHON+=	src/pm/mpd/*.py src/pm/mpd/test/*.py
-.include "../../lang/python/application.mk"
+#USE_LANGUAGES+=		fortran77
+#CONFIGURE_ARGS+=	--enable-fortran=f77
+CONFIGURE_ARGS+=	--enable-fortran=no
 .endif
