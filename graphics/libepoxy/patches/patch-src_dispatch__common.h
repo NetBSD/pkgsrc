111c1db281aa1bb97a5cd54845dff23829a8bf98
bsiegert <bsiegert@pkgsrc.org>
2017-04-29 18:08:25 +0000

    Pullup ticket #5320 - requested by sevan graphics/libepoxy: build fix
    Revisions pulled up:
    - graphics/libepoxy/distinfo                                    1.14
    - graphics/libepoxy/patches/patch-src_dispatch__common.h        1.7
    
    ---
       Module Name:    pkgsrc
       Committed By:   dsainty
       Date:           Wed Apr 19 07:34:12 UTC 2017
    
       Modified Files:
               pkgsrc/graphics/libepoxy: distinfo
               pkgsrc/graphics/libepoxy/patches: patch-src_dispatch__common.h
    
       Log Message:
       The generic EGL/GLX/WGL defaults are suitable for Darwin as BUILD_EGL
       will be
       correct, and possibly not zero.
    
       No revision bump required - this change fixes the build if the EGL = 0 guess
       was incorrect, and no change if it was correct.

--- graphics/libepoxy/patches/patch-src_dispatch__common.h
+++ graphics/libepoxy/patches/patch-src_dispatch__common.h
@@ -1,8 +1,20 @@
-$NetBSD: patch-src_dispatch__common.h,v 1.6 2017/02/18 14:00:05 joerg Exp $
+$NetBSD: patch-src_dispatch__common.h,v 1.6.2.1 2017/04/29 18:08:25 bsiegert Exp $
+
+The generic defaults are suitable for Darwin as BUILD_EGL will be correct,
+and possibly not zero.
 
 --- src/dispatch_common.h.orig	2017-02-06 16:00:20.000000000 +0000
 +++ src/dispatch_common.h
-@@ -36,7 +36,7 @@
+@@ -27,16 +27,12 @@
+ #define PLATFORM_HAS_EGL 0
+ #define PLATFORM_HAS_GLX ENABLE_GLX
+ #define PLATFORM_HAS_WGL 1
+-#elif defined(__APPLE__)
+-#define PLATFORM_HAS_EGL 0
+-#define PLATFORM_HAS_GLX ENABLE_GLX
+-#define PLATFORM_HAS_WGL 0
+ #elif defined(ANDROID)
+ #define PLATFORM_HAS_EGL 1
  #define PLATFORM_HAS_GLX 0
  #define PLATFORM_HAS_WGL 0
  #else
