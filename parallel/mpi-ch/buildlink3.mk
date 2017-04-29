bb7a4358999a11f5804e278348e333f439418d31
adam <adam@pkgsrc.org>
2017-04-29 18:18:52 +0000

    Changes in 3.2:
    * Added support for MPI-3.1 features including nonblocking collective I/O,
      address manipulation routines, thread-safety for MPI initialization,
      pre-init functionality, and new MPI_T routines to look up variables
      by name.
    
    * Fortran 2008 bindings are enabled by default and fully supported.
    
    * Added support for the Mellanox MXM InfiniBand interface.  (thanks
      to Mellanox for the code contribution).
    
    * Added support for the Mellanox HCOLL interface for collectives.
      (thanks to Mellanox for the code contribution).
    
    * Significant stability improvements to the MPICH/portals4
      implementation.
    
    * Completely revamped RMA infrastructure including several
      scalability improvements, performance improvements, and bug fixes.
    
    * Added experimental support for Open Fabrics Interfaces (OFI) version 1.0.0.
      https://github.com/ofiwg/libfabric (thanks to Intel for code contribution)
    
    * The Myrinet MX network module, which had a life cyle from 1.1 till
      3.1.2, has now been deleted.
    
    * Several other minor bug fixes, memory leak fixes, and code cleanup.

--- parallel/mpi-ch/buildlink3.mk
+++ parallel/mpi-ch/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2015/11/25 12:52:26 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2017/04/29 18:18:52 adam Exp $
 
 BUILDLINK_TREE+=	mpich
 
@@ -8,7 +8,7 @@ MPICH_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.mpich+=	mpich>=3
 BUILDLINK_PKGSRCDIR.mpich?=	../../parallel/mpi-ch
 
-MPI_PREFIX?=	$(BUILDLINK_PREFIX.mpich)
+MPI_PREFIX?=	${BUILDLINK_PREFIX.mpich}
 
 .include "../../parallel/openpa/buildlink3.mk"
 .endif	# MPICH_BUILDLINK3_MK
--- parallel/mpi-ch/patches/patch-src_mpid_ch3_channels_nemesis_include_mpidi_ch3_impl.h
+++ parallel/mpi-ch/patches/patch-src_mpid_ch3_channels_nemesis_include_mpidi_ch3_impl.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_mpid_ch3_channels_nemesis_include_mpidi_ch3_impl.h,v 1.1 2013/04/15 21:40:17 adam Exp $
+$NetBSD: patch-src_mpid_ch3_channels_nemesis_include_mpidi_ch3_impl.h,v 1.2 2017/04/29 18:18:52 adam Exp $
 
 Use pthread_mutexattr_setpshared() only when available.
 
---- src/mpid/ch3/channels/nemesis/include/mpidi_ch3_impl.h.orig	2013-04-15 21:03:05.000000000 +0000
+--- src/mpid/ch3/channels/nemesis/include/mpidi_ch3_impl.h.orig	2015-11-10 02:39:01.000000000 +0000
 +++ src/mpid/ch3/channels/nemesis/include/mpidi_ch3_impl.h
-@@ -99,6 +99,7 @@ int MPIDI_CH3_SHM_Win_free(MPID_Win **wi
+@@ -106,6 +106,7 @@ int MPIDI_CH3_SHM_Win_free(MPID_Win **wi
                               "**pthread_unlock %s", strerror(pt_err));                  \
      } while (0)
  
@@ -12,8 +12,8 @@ Use pthread_mutexattr_setpshared() only when available.
  #define MPIDI_CH3I_SHM_MUTEX_INIT(win_ptr)                                              \
      do {                                                                                \
          int pt_err;                                                                     \
-@@ -117,6 +118,23 @@ int MPIDI_CH3_SHM_Win_free(MPID_Win **wi
-         MPIU_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
+@@ -124,6 +125,23 @@ int MPIDI_CH3_SHM_Win_free(MPID_Win **wi
+         MPIR_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
                               "**pthread_mutex %s", strerror(pt_err));                   \
      } while (0);
 +#else
@@ -23,13 +23,13 @@ Use pthread_mutexattr_setpshared() only when available.
 +        pthread_mutexattr_t attr;                                                       \
 +                                                                                        \
 +        pt_err = pthread_mutexattr_init(&attr);                                         \
-+        MPIU_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
++        MPIR_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
 +                             "**pthread_mutex %s", strerror(pt_err));                   \
 +        pt_err = pthread_mutex_init((win_ptr)->shm_mutex, &attr);                       \
-+        MPIU_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
++        MPIR_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
 +                             "**pthread_mutex %s", strerror(pt_err));                   \
 +        pt_err = pthread_mutexattr_destroy(&attr);                                      \
-+        MPIU_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
++        MPIR_ERR_CHKANDJUMP1(pt_err, mpi_errno, MPI_ERR_OTHER, "**pthread_mutex",       \
 +                             "**pthread_mutex %s", strerror(pt_err));                   \
 +    } while (0);
 +#endif
