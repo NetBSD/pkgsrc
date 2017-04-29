42c7c5223ca12a6b71e9394e0a3b72ad15f5f39f
adam <adam@pkgsrc.org>
2017-04-29 18:31:42 +0000

    Changes 1.64.0: New Libraries Process library by Klemens D. Morgenstern, that provides cross platorm ways to allows you to: * create child processes * setup streams for child processes * communicate with child processes through streams (synchronously or asynchronously) * wait for processes to exit (synchronously or asynchronously) * terminate processes

--- devel/boost-build/buildlink3.mk
+++ devel/boost-build/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.34 2017/01/01 15:32:47 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.35 2017/04/29 18:31:42 adam Exp $
 
 BUILDLINK_TREE+=	boost-build
 
 .if !defined(BOOST_BUILD_BUILDLINK3_MK)
 BOOST_BUILD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.63.*
+BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.64.*
 BUILDLINK_DEPMETHOD.boost-build?=	build
 BUILDLINK_PKGSRCDIR.boost-build?=	../../devel/boost-build
 .endif # BOOST_BUILD_BUILDLINK3_MK
