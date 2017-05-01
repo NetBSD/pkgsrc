b8c17e6e8c77e8cd562a4a82796a25a99998599c
bsiegert <bsiegert@pkgsrc.org>
2017-05-01 09:20:17 +0000

    Pullup ticket #5373 - requested by sevan sysutils/hal: build fix for Linux
    Revisions pulled up:
    - sysutils/hal/distinfo                                         1.28
    - sysutils/hal/patches/patch-hald_linux_addons_addon-storage.c  1.2
    
    ---
       Module Name:    pkgsrc
       Committed By:   maya
       Date:           Wed Apr 26 12:10:43 UTC 2017
    
       Modified Files:
               pkgsrc/sysutils/hal: distinfo
               pkgsrc/sysutils/hal/patches: patch-hald_linux_addons_addon-storage.c
    
       Log Message:
       hal: help RHEL7 builds along by including <stddef.h> for size_t used by
       another header, scsi/sg.h.
    
       Blind fix.

--- sysutils/hal/patches/patch-hald_linux_addons_addon-storage.c
+++ sysutils/hal/patches/patch-hald_linux_addons_addon-storage.c
@@ -1,8 +1,19 @@
-$NetBSD: patch-hald_linux_addons_addon-storage.c,v 1.1 2012/05/14 03:12:01 sbd Exp $
+$NetBSD: patch-hald_linux_addons_addon-storage.c,v 1.1.40.1 2017/05/01 09:20:18 bsiegert Exp $
+
+include stddef.h for size_t used in scsi/sg.h
+glib2 now only allows glib.h to be included
 
 --- hald/linux/addons/addon-storage.c.orig	2009-08-24 12:42:30.000000000 +0000
 +++ hald/linux/addons/addon-storage.c
-@@ -39,7 +39,7 @@
+@@ -32,6 +32,7 @@
+ #include <linux/cdrom.h>
+ #include <linux/fs.h>
+ #include <mntent.h>
++#include <stddef.h>
+ #include <scsi/sg.h>
+ #include <stdint.h>
+ #include <stdio.h>
+@@ -39,7 +40,7 @@
  #include <string.h>
  #include <sys/ioctl.h>
  #include <unistd.h>
