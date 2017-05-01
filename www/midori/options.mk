025e01698f4d62ecd8782e2318add85f8046bef9
bsiegert <bsiegert@pkgsrc.org>
2017-05-01 09:02:16 +0000

    Pullup ticket #5349 - requested by sevan www/midori: build fix
    Revisions pulled up:
    - www/midori/Makefile                                           1.97
    - www/midori/options.mk                                         1.2
    
    ---
       Module Name:    pkgsrc
       Committed By:   maya
       Date:           Sat Apr 22 07:16:19 UTC 2017
    
       Modified Files:
               pkgsrc/www/midori: Makefile options.mk
    
       Log Message:
       Midori binary needs WX memory, use NOT_PAX_MPROTECT_SAFE.
       from macallan@
    
       While here, appease pkglint.
    
       bump PKGREVISION.

--- www/midori/options.mk
+++ www/midori/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2015/10/25 11:53:24 jmcneill Exp $
+# $NetBSD: options.mk,v 1.1.12.1 2017/05/01 09:02:16 bsiegert Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.midori
 
@@ -10,13 +10,13 @@ PKG_SUGGESTED_OPTIONS=		gtk2
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgtk2)
-.include "../../www/webkit24-gtk/buildlink3.mk"  
+.include "../../www/webkit24-gtk/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mgtk3)
 CONFIGURE_ARGS+=	--enable-gtk3
 CMAKE_ARGS+=		-DUSE_GTK3=1
-.include "../../www/webkit24-gtk3/buildlink3.mk"  
+.include "../../www/webkit24-gtk3/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
 .endif
