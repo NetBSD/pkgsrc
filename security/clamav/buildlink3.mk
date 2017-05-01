13c1036c72fa5dbcfbedbb2337d210b3095ba614
bsiegert <bsiegert@pkgsrc.org>
2017-05-01 08:51:40 +0000

    Pullup ticket #5344 - requested by sevan security/clamav: build fix
    Revisions pulled up:
    - security/clamav/buildlink3.mk                                 1.6
    
    ---
       Module Name:    pkgsrc
       Committed By:   jperkin
       Date:           Fri Apr 21 13:40:19 UTC 2017
    
       Modified Files:
               pkgsrc/security/clamav: buildlink3.mk
    
       Log Message:
       Pull in required dependencies.

--- security/clamav/buildlink3.mk
+++ security/clamav/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2017/01/05 11:27:21 roy Exp $
+# $NetBSD: buildlink3.mk,v 1.5.2.1 2017/05/01 08:51:40 bsiegert Exp $
 
 BUILDLINK_TREE+=	clamav
 
@@ -9,5 +9,9 @@ BUILDLINK_API_DEPENDS.clamav+=	clamav>=0.95.3nb1
 BUILDLINK_ABI_DEPENDS.clamav+=	clamav>=0.98.7nb2
 BUILDLINK_PKGSRCDIR.clamav?=	../../security/clamav
 .endif	# CLAMAV_BUILDLINK3_MK
+
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../devel/pcre/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
 
 BUILDLINK_TREE+=	-clamav
