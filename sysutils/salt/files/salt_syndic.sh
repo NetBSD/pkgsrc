2847361d88e9efb636c00b01e1b5adcf1f2f2264
sborrill <sborrill@pkgsrc.org>
2017-05-03 08:24:57 +0000

    Update to 2016.11.4. Changelog: https://docs.saltstack.com/en/latest/topics/releases/2016.11.4.html
    Also ensure PID file paths are correct.

--- sysutils/salt/files/salt_syndic.sh
+++ sysutils/salt/files/salt_syndic.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: salt_syndic.sh,v 1.1 2016/02/16 00:00:08 khorben Exp $
+# $NetBSD: salt_syndic.sh,v 1.2 2017/05/03 08:24:57 sborrill Exp $
 #
 
 # PROVIDE: salt_syndic
@@ -15,6 +15,7 @@ command="@PREFIX@/bin/salt-syndic"
 command_args="-d"
 command_interpreter=@PYTHON@
 required_files="@PKG_SYSCONFDIR@/master @PKG_SYSCONFDIR@/minion"
+pidfile="@VARBASE@/run/salt-syndic.pid"
 
 load_rc_config $name
 run_rc_command "$1"
