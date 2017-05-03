98d6b37e4623c6f5429ba5d7feb3a83671d383ad
maya <maya@pkgsrc.org>
2017-05-03 00:21:03 +0000

    Add GCC 7.1.0
    GCC Java removed, package now uses ISL 0.16.1
    
    Release notes:
    
    We are proud to announce the next, major release of the
    GNU Compiler Collection, 7.1.  This year we celebrated the 30th
    anniversary of the first GCC beta release and this month
    we will celebrate 30 years since the GCC 1.0 release.
    
    GCC 7.1 is a major release containing substantial new
    functionality not available in GCC 6.x or previous GCC releases.
    
    The C++ frontend now has experimental support for all of the current C++17
    draft, with the -std=c++1z and -std=gnu++1z options, and the libstdc++
    library has most of the C++17 draft library features implemented too.
    
    This releases features various improvements in the emitted diagnostics,
    including improved locations, location ranges, suggestions for misspelled
    identifiers, option names, fix-it hints and various new warnings
    have been added.
    
    The optimizers have been improved, with improvements appearing in all of
    intra- and inter-procedural optimizations, link time optimizations and
    various target backends, including, but not limited to, additions of store
    merging pass, code hoisting optimization, loop splitting, and
    shrink wrapping improvements.
    
    The Address Sanitizer can now report uses of variables after leaving their
    scope.  GCC now can be configured for OpenMP 4.5 offloading to NVidia PTX
    GPGPUs.
    
    Some code that compiled successfully with older GCC versions might require
    some code adjustments, see http://gcc.gnu.org/gcc-7/porting_to.html for
    details.
    
    See
    
      https://gcc.gnu.org/gcc-7/changes.html
    
    for more information about changes in GCC 7.1.

---
+++ lang/gcc7/patches/patch-gcc_config_netbsd-protos.h
@@ -0,0 +1,34 @@
+$NetBSD: patch-gcc_config_netbsd-protos.h,v 1.1 2017/05/03 00:21:03 maya Exp $
+
+--- /dev/null	2014-12-15 07:13:57.000000000 +0000
++++ gcc/config/netbsd-protos.h
+@@ -0,0 +1,29 @@
++/* Prototypes.
++   Copyright (C) 2001-2014 Free Software Foundation, Inc.
++
++This file is part of GCC.
++
++GCC is free software; you can redistribute it and/or modify
++it under the terms of the GNU General Public License as published by
++the Free Software Foundation; either version 3, or (at your option)
++any later version.
++
++GCC is distributed in the hope that it will be useful,
++but WITHOUT ANY WARRANTY; without even the implied warranty of
++MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
++GNU General Public License for more details.
++
++You should have received a copy of the GNU General Public License
++along with GCC; see the file COPYING3.  If not see
++<http://www.gnu.org/licenses/>.  */
++
++#ifndef _NETBSD_PROTOS_H_
++#define _NETBSD_PROTOS_H_
++
++double __c99_cabs (double complex);
++float __c99_cabsf (float complex);
++long double __c99_cabsl (long double complex);
++
++void netbsd_patch_builtins (void);
++
++#endif /* _NETBSD_PROTOS_H_ */
