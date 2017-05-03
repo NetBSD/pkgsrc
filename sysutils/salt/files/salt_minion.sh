e947b8d944d5ef169c812d8c08a6060b261e7af0
sborrill <sborrill@pkgsrc.org>
2017-05-03 08:24:57 +0000

    Update to 2016.11.4. Changelog: https://docs.saltstack.com/en/latest/topics/releases/2016.11.4.html
    Also ensure PID file paths are correct.

--- sysutils/salt/files/salt_minion.sh
+++ sysutils/salt/files/salt_minion.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: salt_minion.sh,v 1.2 2013/05/05 12:26:23 imil Exp $
+# $NetBSD: salt_minion.sh,v 1.3 2017/05/03 08:24:57 sborrill Exp $
 #
 
 # PROVIDE: salt_minion
@@ -15,6 +15,7 @@ command="@PREFIX@/bin/salt-minion"
 command_args="-d"
 command_interpreter=@PYTHON@
 required_files="@PKG_SYSCONFDIR@/minion"
+pidfile="@VARBASE@/run/salt-minion.pid"
 
 load_rc_config $name
 run_rc_command "$1"
