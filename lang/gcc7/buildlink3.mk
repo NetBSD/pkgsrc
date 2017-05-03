528deec5be6d284e568f82feeba91c7d702d7a9b
maya <maya@pkgsrc.org>
2017-05-03 00:54:59 +0000

    gcc7: cleanup some accidential wip package and gcc-java leftovers

--- lang/gcc7/buildlink3.mk
+++ lang/gcc7/buildlink3.mk
@@ -1,17 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/05/03 00:21:03 maya Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/05/03 00:54:59 maya Exp $
 
-BUILDLINK_TREE+=	gcc7snapshot
+BUILDLINK_TREE+=	gcc7
 
 .if !defined(GCC7_BUILDLINK3_MK)
 GCC6SNAPSHOT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gcc7snapshot+=	gcc6>=${_GCC_REQD}
-BUILDLINK_ABI_DEPENDS.gcc7snapshot+=	gcc6>=7.0
-BUILDLINK_PKGSRCDIR.gcc7snapshot=	../../wip/gcc7snapshot
-BUILDLINK_DEPMETHOD.gcc7snapshot?=	build
+BUILDLINK_API_DEPENDS.gcc7+=	gcc7>=${_GCC_REQD}
+BUILDLINK_ABI_DEPENDS.gcc7+=	gcc7>=7.0
+BUILDLINK_PKGSRCDIR.gcc7=	../../lang/gcc7
+BUILDLINK_DEPMETHOD.gcc7?=	build
 
-BUILDLINK_FILES.gcc7snapshot=		#empty
-BUILDLINK_AUTO_VARS.gcc7snapshot=	no
+BUILDLINK_FILES.gcc7=		#empty
+BUILDLINK_AUTO_VARS.gcc7=	no
 
 # Packages that link against shared libraries need a full dependency.
 #.if defined(_USE_GCC_SHLIB)
@@ -19,13 +19,9 @@ BUILDLINK_AUTO_VARS.gcc7snapshot=	no
 #ABI_DEPENDS+=	{gcc7,gcc7-libs}>=7.0:../../lang/gcc7-libs
 #.endif
 
-pkgbase := gcc7snapshot
+pkgbase := gcc7
 .include "../../mk/pkg-build-options.mk"
-.if !empty(PKG_BUILD_OPTIONS.gcc7snapshot:Mgcc-java)
-.include "../../devel/zlib/buildlink3.mk"
-.endif
-
 .include "../../mk/dlopen.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
-.endif # GCC7SNAPSHOT_BUILDLINK3_MK
-BUILDLINK_TREE+=	-gcc7snapshot
+.endif # gcc7_BUILDLINK3_MK
+BUILDLINK_TREE+=	-gcc7
