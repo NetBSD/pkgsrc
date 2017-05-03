04ea1fafe4497d51148773316baf0a59024c033e
fhajny <fhajny@pkgsrc.org>
2017-05-03 16:49:54 +0000

    Update databases/mongo-tools to 3.4.4
    3.4.2
    - PreProcessing is failling with "got invalid document size"
    - mongo-replay crashes during playback
    - nil pointer derereference when error occurs
    
    3.4.1
    - Stats collection has large playback performance impact
    - Add option to set capture buffer size to avoid packet loss
    - Playback file contains full reply payload
    - mongoreplay: out of bounds error in "shortenreply" during record
    - Add test for restoring a collection with a default collation
    - nil pointer dereference in mongoreplay when error on new playback
      file creation
    
    3.2.10
    - Don't create intents for system.profile.metadata.json files
    - tools do not respect readPreference=secondary when connecting to a
      mongos
    - Mongodump SSL and GSSAPI authentication
    - No numeric version in -version output
    - Backport to v3.2
    - Issue only one ApplyOps cmd per oplog entry
    
    3.2.9
    - Make -version spit out a bit more information.
    
    3.2.8
    - Mongostat with discover can find the same node twice in a sharded
      cluster
    - mongostat -discover does not alias localhost to the target hostname
    - mongoimport returns exit code 0 even when error prevents records
      from being inserted
    - Wrong error message while using mongoimport
    - add a "-assertExists" option to mongoexport
    - Use v3.2.7 for qa tests
    - mongoimport types jstest fails on functions

--- databases/mongo-tools/patches/patch-vendor_src_github.com_spacemonkeygo_openssl_build.go
+++ databases/mongo-tools/patches/patch-vendor_src_github.com_spacemonkeygo_openssl_build.go
@@ -1,15 +1,17 @@
-$NetBSD: patch-vendor_src_github.com_spacemonkeygo_openssl_build.go,v 1.1 2015/10/21 16:37:22 fhajny Exp $
+$NetBSD: patch-vendor_src_github.com_spacemonkeygo_openssl_build.go,v 1.2 2017/05/03 16:49:54 fhajny Exp $
 
 Simplify lib flags look up via pkg-config.
 
---- vendor/src/github.com/spacemonkeygo/openssl/build.go.orig	2015-09-25 20:02:24.000000000 +0000
+--- vendor/src/github.com/spacemonkeygo/openssl/build.go.orig	2016-12-29 22:33:41.000000000 +0000
 +++ vendor/src/github.com/spacemonkeygo/openssl/build.go
-@@ -16,7 +16,5 @@
+@@ -16,9 +16,5 @@
  
  package openssl
  
 -// #cgo linux pkg-config: openssl
 -// #cgo windows CFLAGS: -DWIN32_LEAN_AND_MEAN
--// #cgo darwin LDFLAGS: -lssl -lcrypto
+-// #cgo windows LDFLAGS: -lcrypt32
+-// #cgo darwin CFLAGS: -Wno-deprecated-declarations
+-// #cgo darwin LDFLAGS: -lssl -lcrypto -framework CoreFoundation -framework Foundation -framework Security
 +// #cgo pkg-config: openssl
  import "C"
