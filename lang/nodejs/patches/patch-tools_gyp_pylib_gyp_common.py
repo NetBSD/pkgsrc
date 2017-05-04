329ee449d731256d98c8882504788411f568e344
fhajny <fhajny@pkgsrc.org>
2017-05-04 16:39:36 +0000

    Update lang/nodejs to 7.10.0.
    - crypto: add randomFill and randomFillSync
    - meta: Added new collaborators
      - add lucamaraschi to collaborators
      - add DavidCai1993 to collaborators
      - add jkrems to collaborators
      - add AnnaMag to collaborators
    - process: fix crash when Promise rejection is a Symbol
    - url: make WHATWG URL more spec compliant
    - v8:
      - fix stack overflow in recursive method
      - fix build errors with g++ 7

--- lang/nodejs/patches/patch-tools_gyp_pylib_gyp_common.py
+++ lang/nodejs/patches/patch-tools_gyp_pylib_gyp_common.py
@@ -1,14 +1,12 @@
-$NetBSD: patch-tools_gyp_pylib_gyp_common.py,v 1.1 2013/05/22 15:17:07 mspo Exp $
+$NetBSD: patch-tools_gyp_pylib_gyp_common.py,v 1.2 2017/05/04 16:39:36 fhajny Exp $
 
 Add support for NetBSD and DragonFly.
---- tools/gyp/pylib/gyp/common.py.orig	2013-03-28 19:07:52.000000000 +0000
+--- tools/gyp/pylib/gyp/common.py.orig	2017-05-03 02:00:16.000000000 +0000
 +++ tools/gyp/pylib/gyp/common.py
-@@ -394,6 +394,10 @@ def GetFlavor(params):
-     return 'freebsd'
-   if sys.platform.startswith('openbsd'):
+@@ -427,6 +427,8 @@ def GetFlavor(params):
      return 'openbsd'
-+  if sys.platform.startswith('netbsd'):
-+    return 'netbsd'
+   if sys.platform.startswith('netbsd'):
+     return 'netbsd'
 +  if sys.platform.startswith('dragonflybsd'):
 +    return 'dragonflybsd'
    if sys.platform.startswith('aix'):
