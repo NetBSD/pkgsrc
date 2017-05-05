86cfb1f454ab1546e43649e17bee45910bd4325b
kamil <kamil@pkgsrc.org>
2017-05-05 23:13:05 +0000

    Update buildlink3.mk with reality in LLVM projects

--- lang/clang/buildlink3.mk
+++ lang/clang/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2016/11/14 20:15:33 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2017/05/05 23:13:05 kamil Exp $
 
 BUILDLINK_TREE+=	clang
 
 .if !defined(CLANG_BUILDLINK3_MK)
 CLANG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clang+=	clang>=3.9.0
+BUILDLINK_API_DEPENDS.clang+=	clang>=4.0.0
 BUILDLINK_PKGSRCDIR.clang?=	../../lang/clang
 
 .include "../../textproc/libxml2/buildlink3.mk"
