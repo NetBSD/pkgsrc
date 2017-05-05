ec3b6ed62e4b0829952cbdf5ee0c798dbe82f0f1
sevan <sevan@pkgsrc.org>
2017-05-05 20:14:05 +0000

    CVE-2017-7596 CVE-2017-7597 CVE-2017-7599 CVE-2017-7600 https://github.com/vadz/libtiff/commit/3144e57770c1e4d26520d8abee750f8ac8b75490 Dependency for applying advisory patch. +http://bugzilla.maptools.org/show_bug.cgi?id=2535 +https://github.com/vadz/libtiff/commit/0abd094b6e5079c4d8be733829240491cb230f3d Bump rev.

--- graphics/tiff/patches/patch-tools_tiffcrop.c
+++ graphics/tiff/patches/patch-tools_tiffcrop.c
@@ -1,8 +1,11 @@
 $NetBSD$
 
-Fix for CVE-2016-10092, ref. http://bugzilla.maptools.org/show_bug.cgi?id=2620
-and
+CVE-2016-10092
+http://bugzilla.maptools.org/show_bug.cgi?id=2620
 https://github.com/vadz/libtiff/commit/9657bbe3cdce4aaa90e07d50c1c70ae52da0ba6a
+Fix double free
+http://bugzilla.maptools.org/show_bug.cgi?id=2535
+https://github.com/vadz/libtiff/commit/0abd094b6e5079c4d8be733829240491cb230f3d
 
 --- tools/tiffcrop.c.orig	2016-11-19 01:45:30.000000000 +0000
 +++ tools/tiffcrop.c
@@ -15,3 +18,11 @@ https://github.com/vadz/libtiff/commit/9657bbe3cdce4aaa90e07d50c1c70ae52da0ba6a
          }
  
          return 1;
+@@ -7986,7 +7986,6 @@ writeCroppedImage(TIFF *in, TIFF *out, s
+   if (!TIFFWriteDirectory(out))
+     {
+     TIFFError("","Failed to write IFD for page number %d", pagenum);
+-    TIFFClose(out);
+     return (-1);
+     }
+ 
