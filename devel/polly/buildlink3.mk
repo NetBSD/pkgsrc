53397f98598d4d01aa8432638d4654c1db97b636
kamil <kamil@pkgsrc.org>
2017-05-05 23:13:05 +0000

    Update buildlink3.mk with reality in LLVM projects

--- devel/polly/buildlink3.mk
+++ devel/polly/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/09/12 22:49:23 kamil Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/05/05 23:13:05 kamil Exp $
 
 BUILDLINK_TREE+=	polly
 
 .if !defined(POLLY_BUILDLINK3_MK)
 POLLY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.polly+=	polly>=3.8.1
+BUILDLINK_API_DEPENDS.polly+=	polly>=4.0.0
 BUILDLINK_PKGSRCDIR.polly?=	../../devel/polly
 
 .include "../../lang/llvm/buildlink3.mk"
