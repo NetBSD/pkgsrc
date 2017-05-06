9a33b2a8830c77962cce2f5855cdbb5413ba650c
bsiegert <bsiegert@pkgsrc.org>
2017-05-06 15:08:52 +0000

    Pullup ticket #5404 - requested by sevan graphics/tiff: security fix
    Revisions pulled up:
    - graphics/tiff/Makefile                                        1.127-1.129
    - graphics/tiff/distinfo                                        1.73-1.75
    - graphics/tiff/patches/patch-libtiff_tif_dir.c                 1.1
    - graphics/tiff/patches/patch-libtiff_tif_dirread.c             1.2
    - graphics/tiff/patches/patch-libtiff_tif_dirwrite.c            1.1
    - graphics/tiff/patches/patch-tools_tiffcp.c                    1.2
    - graphics/tiff/patches/patch-tools_tiffcrop.c                  1.1-1.2
    
    ---
       Module Name:    pkgsrc
       Committed By:   he
       Date:           Fri May  5 19:16:58 UTC 2017
    
       Modified Files:
               pkgsrc/graphics/tiff: Makefile
       Added Files:
               pkgsrc/graphics/tiff/patches: patch-tools_tiffcrop.c
    
       Log Message:
       Apply fix from upstream to fix CVE-2016-10092, ref.
       http://bugzilla.maptools.org/show_bug.cgi?id=2620 and
       https://github.com/vadz/libtiff/commit/9657bbe3cdce4aaa90e07d50c1c70ae52da0ba6a
       Bump PKGREVISION.
    
    ---
       Module Name:    pkgsrc
       Committed By:   he
       Date:           Fri May  5 19:28:23 UTC 2017
    
       Modified Files:
               pkgsrc/graphics/tiff: distinfo
    
       Log Message:
       Forgot "make mps", this one belongs to the previous update, 4.0.7nb3.
    
    ---
       Module Name:    pkgsrc
       Committed By:   he
       Date:           Fri May  5 20:06:03 UTC 2017
    
       Modified Files:
               pkgsrc/graphics/tiff: Makefile distinfo
               pkgsrc/graphics/tiff/patches: patch-tools_tiffcp.c
    
       Log Message:
       Apply fix for CVE-2016-10093
       http://bugzilla.maptools.org/show_bug.cgi?id=2610
       https://github.com/vadz/libtiff/commit/787c0ee906430b772f33ca50b97b8b5ca070faec
       Bump PKGREVISION.
    
    ---
       Module Name:    pkgsrc
       Committed By:   sevan
       Date:           Fri May  5 20:14:05 UTC 2017
    
       Modified Files:
               pkgsrc/graphics/tiff: Makefile distinfo
               pkgsrc/graphics/tiff/patches: patch-libtiff_tif_dirread.c
                   patch-tools_tiffcrop.c
       Added Files:
               pkgsrc/graphics/tiff/patches: patch-libtiff_tif_dir.c
                   patch-libtiff_tif_dirwrite.c
    
       Log Message:
       CVE-2017-7596
       CVE-2017-7597
       CVE-2017-7599
       CVE-2017-7600
       https://github.com/vadz/libtiff/commit/3144e57770c1e4d26520d8abee750f8ac8b75490
       Dependency for applying advisory patch.
       +http://bugzilla.maptools.org/show_bug.cgi?id=2535
       +https://github.com/vadz/libtiff/commit/0abd094b6e5079c4d8be733829240491cb230f3d
       Bump rev.

--- graphics/tiff/patches/patch-libtiff_tif_dirread.c
+++ graphics/tiff/patches/patch-libtiff_tif_dirread.c
@@ -1,11 +1,40 @@
-$NetBSD: patch-libtiff_tif_dirread.c,v 1.2.2.2 2017/05/06 15:01:21 bsiegert Exp $
+$NetBSD: patch-libtiff_tif_dirread.c,v 1.2.2.3 2017/05/06 15:08:52 bsiegert Exp $
 
+CVE-2017-7596
+CVE-2017-7597
 CVE-2017-7598
+CVE-2017-7599
+CVE-2017-7600
 https://github.com/vadz/libtiff/commit/3cfd62d77c2a7e147a05bd678524c345fa9c2bb8
+https://github.com/vadz/libtiff/commit/3144e57770c1e4d26520d8abee750f8ac8b75490
 
---- libtiff/tif_dirread.c.orig	2016-11-18 02:42:46.000000000 +0000
+--- libtiff/tif_dirread.c.orig	2017-05-05 18:56:15.000000000 +0000
 +++ libtiff/tif_dirread.c
-@@ -2872,7 +2872,10 @@ static enum TIFFReadDirEntryErr TIFFRead
+@@ -40,6 +40,7 @@
+  */
+ 
+ #include "tiffiop.h"
++#include <float.h>
+ 
+ #define IGNORE 0          /* tag placeholder used below */
+ #define FAILED_FII    ((uint32) -1)
+@@ -2406,7 +2407,14 @@ static enum TIFFReadDirEntryErr TIFFRead
+ 				ma=(double*)origdata;
+ 				mb=data;
+ 				for (n=0; n<count; n++)
+-					*mb++=(float)(*ma++);
++                                {
++                                    double val = *ma++;
++                                    if( val > FLT_MAX )
++                                        val = FLT_MAX;
++                                    else if( val < -FLT_MAX )
++                                        val = -FLT_MAX;
++                                    *mb++=(float)val;
++                                }
+ 			}
+ 			break;
+ 	}
+@@ -2872,7 +2880,10 @@ static enum TIFFReadDirEntryErr TIFFRead
  		m.l = direntry->tdir_offset.toff_long8;
  	if (tif->tif_flags&TIFF_SWAB)
  		TIFFSwabArrayOfLong(m.i,2);
@@ -17,7 +46,7 @@ https://github.com/vadz/libtiff/commit/3cfd62d77c2a7e147a05bd678524c345fa9c2bb8
  		*value=0.0;
  	else
  		*value=(double)m.i[0]/(double)m.i[1];
-@@ -2900,7 +2903,10 @@ static enum TIFFReadDirEntryErr TIFFRead
+@@ -2900,7 +2911,10 @@ static enum TIFFReadDirEntryErr TIFFRead
  		m.l=direntry->tdir_offset.toff_long8;
  	if (tif->tif_flags&TIFF_SWAB)
  		TIFFSwabArrayOfLong(m.i,2);
--- graphics/tiff/patches/patch-tools_tiffcp.c
+++ graphics/tiff/patches/patch-tools_tiffcp.c
@@ -1,9 +1,15 @@
-$NetBSD: patch-tools_tiffcp.c,v 1.2.2.2 2017/05/06 15:01:21 bsiegert Exp $
+$NetBSD: patch-tools_tiffcp.c,v 1.2.2.3 2017/05/06 15:08:52 bsiegert Exp $
 
 CVE-2017-5225
 http://bugzilla.maptools.org/show_bug.cgi?id=2656
 http://bugzilla.maptools.org/show_bug.cgi?id=2657
 https://github.com/vadz/libtiff/commit/5c080298d59efa53264d7248bbe3a04660db6ef7
+
+and
+
+CVE-2016-10093
+http://bugzilla.maptools.org/show_bug.cgi?id=2610
+https://github.com/vadz/libtiff/commit/787c0ee906430b772f33ca50b97b8b5ca070faec
 
 --- tools/tiffcp.c.orig	2016-10-12 01:45:17.000000000 +0000
 +++ tools/tiffcp.c
@@ -50,6 +56,33 @@ https://github.com/vadz/libtiff/commit/5c080298d59efa53264d7248bbe3a04660db6ef7
  
  	inbuf = _TIFFmalloc(scanlinesizein);
  	outbuf = _TIFFmalloc(scanlinesizeout);
+@@ -1163,7 +1183,7 @@ bad:
+ 
+ static void
+ cpStripToTile(uint8* out, uint8* in,
+-    uint32 rows, uint32 cols, int outskew, int inskew)
++    uint32 rows, uint32 cols, int outskew, int64 inskew)
+ {
+ 	while (rows-- > 0) {
+ 		uint32 j = cols;
+@@ -1320,7 +1340,7 @@ DECLAREreadFunc(readContigTilesIntoBuffe
+ 	tdata_t tilebuf;
+ 	uint32 imagew = TIFFScanlineSize(in);
+ 	uint32 tilew  = TIFFTileRowSize(in);
+-	int iskew = imagew - tilew;
++	int64 iskew = (int64)imagew - (int64)tilew;
+ 	uint8* bufp = (uint8*) buf;
+ 	uint32 tw, tl;
+ 	uint32 row;
+@@ -1348,7 +1368,7 @@ DECLAREreadFunc(readContigTilesIntoBuffe
+ 				status = 0;
+ 				goto done;
+ 			}
+-			if (colb + tilew > imagew) {
++			if (colb > iskew) {
+ 				uint32 width = imagew - colb;
+ 				uint32 oskew = tilew - width;
+ 				cpStripToTile(bufp + colb,
 @@ -1763,7 +1783,7 @@ pickCopyFunc(TIFF* in, TIFF* out, uint16
  	uint32 w, l, tw, tl;
  	int bychunk;
