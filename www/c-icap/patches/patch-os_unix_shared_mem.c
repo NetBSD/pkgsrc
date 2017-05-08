90d84652f702f66ab2e9f83a6b9d69c654a0a436
sborrill <sborrill@pkgsrc.org>
2017-05-08 09:18:31 +0000

    Remove full paths in patches

--- www/c-icap/patches/patch-os_unix_shared_mem.c
+++ www/c-icap/patches/patch-os_unix_shared_mem.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-os_unix_shared_mem.c,v 1.1 2017/04/24 14:35:01 sborrill Exp $
+$NetBSD: patch-os_unix_shared_mem.c,v 1.2 2017/05/08 09:18:31 sborrill Exp $
 
 Support MAP_ANON as a synonym of MAP_ANONYMOUS
 
---- /pkg_comp/obj/pkgsrc/www/c-icap/default/c_icap-0.4.4/os/unix/shared_mem.c.orig	2017-04-24 14:00:54.000000000 +0000
-+++ /pkg_comp/obj/pkgsrc/www/c-icap/default/c_icap-0.4.4/os/unix/shared_mem.c	2017-04-24 14:00:57.000000000 +0000
+--- os/unix/shared_mem.c.orig	2017-04-24 14:00:54.000000000 +0000
++++ os/unix/shared_mem.c	2017-04-24 14:00:57.000000000 +0000
 @@ -29,6 +29,9 @@
  #endif
  #if defined(USE_POSIX_MAPPED_FILES)
