b0a69099449f627a12fee76f2488376d6266311f
adam <adam@pkgsrc.org>
2017-05-09 08:06:54 +0000

    Changes 4.1.1: - Undef PySlice_GetIndicesEx, see https://bugs.python.org/issue29943 - Fix for file with DPI in EXIF but not metadata, and XResolution is an int rather than tuple - Docs: Removed broken download counter badge - Docs: Fixed rst syntax error

--- graphics/py-Pillow/patches/patch-setup.py
+++ graphics/py-Pillow/patches/patch-setup.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-setup.py,v 1.11 2017/04/16 08:11:17 adam Exp $
+$NetBSD: patch-setup.py,v 1.12 2017/05/09 08:06:54 adam Exp $
 
-* Prevent to detect optional tkinter.
-* Disable demo programs.
+Prevent to detect optional tkinter.
+Disable demo programs.
 
---- setup.py.orig	2017-04-04 18:14:25.000000000 +0000
+--- setup.py.orig	2017-04-28 16:48:58.000000000 +0000
 +++ setup.py
 @@ -95,11 +95,7 @@ def _read(file):
          return fp.read()
@@ -17,7 +17,7 @@ $NetBSD: patch-setup.py,v 1.11 2017/04/16 08:11:17 adam Exp $
 +_tkinter = None
  
  NAME = 'Pillow'
- PILLOW_VERSION = '4.1.0'
+ PILLOW_VERSION = '4.1.1'
 @@ -736,7 +732,6 @@ try:
            ext_modules=[Extension("PIL._imaging", ["_imaging.c"])],
            include_package_data=True,
