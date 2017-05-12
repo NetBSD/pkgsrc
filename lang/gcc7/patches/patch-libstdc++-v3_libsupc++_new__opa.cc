a9338113bd3c1a606093c203b79cd597abbcd338
jperkin <jperkin@pkgsrc.org>
2017-05-12 18:10:56 +0000

    Restrict patch to specific circumstances on SunOS.

--- lang/gcc7/patches/patch-libstdc++-v3_libsupc++_new__opa.cc
+++ lang/gcc7/patches/patch-libstdc++-v3_libsupc++_new__opa.cc
@@ -1,15 +1,17 @@
-$NetBSD: patch-libstdc++-v3_libsupc++_new__opa.cc,v 1.1 2017/05/10 15:00:09 jperkin Exp $
+$NetBSD: patch-libstdc++-v3_libsupc++_new__opa.cc,v 1.2 2017/05/12 18:10:56 jperkin Exp $
 
-Ensure we can find std::aligned_alloc if using it.
+Ensure we can find SunOS std::aligned_alloc if using it.
 
 --- libstdc++-v3/libsupc++/new_opa.cc.orig	2017-01-26 14:30:45.000000000 +0000
 +++ libstdc++-v3/libsupc++/new_opa.cc
-@@ -82,6 +82,8 @@ aligned_alloc (std::size_t al, std::size
-   return aligned_ptr;
- }
- #endif
-+#else
-+using std::aligned_alloc;
- #endif
+@@ -31,6 +31,10 @@
+ using std::new_handler;
+ using std::bad_alloc;
  
- _GLIBCXX_WEAK_DEFINITION void *
++#if defined(__sun) && _GLIBCXX_HAVE_ALIGNED_ALLOC
++using std::aligned_alloc;
++#endif
++
+ #if !_GLIBCXX_HAVE_ALIGNED_ALLOC
+ #if _GLIBCXX_HAVE__ALIGNED_MALLOC
+ #define aligned_alloc(al,sz) _aligned_malloc(sz,al)
