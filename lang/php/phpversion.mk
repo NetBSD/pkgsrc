5a4c2abdd98b946a2d1db920257673f67da66c36
taca <taca@pkgsrc.org>
2017-05-12 14:45:36 +0000

    Update php70 to 7.0.19.
    11 May 2017 PHP 7.0.19
    
    - Core:
      . Fixed bug #74188 (Null coalescing operator fails for undeclared static
        class properties). (tpunt)
      . Fixed bug #74408 (Endless loop bypassing execution time limit). (Laruence)
      . Fixed bug #74410 (stream_select() is broken on Windows Nanoserver).
        (Matt Ficken)
      . Fixed bug #74337 (php-cgi.exe crash on facebook callback).
        (Anton Serbulov)
      . Patch for bug #74216 was reverted. (Anatol)
    
    - Date:
      . Fixed bug #74404 (Wrong reflection on DateTimeZone::getTransitions).
        (krakjoe)
      . Fixed bug #74080 (add constant for RFC7231 format datetime). (duncan3dc)
    
    - DOM:
      . Fixed bug #74416 (Wrong reflection on DOMNode::cloneNode).
        (Remi, Fabien Villepinte)
    
    - Fileinfo:
      . Fixed bug #74379 (syntax error compile error in libmagic/apprentice.c).
        (Laruence)
    
    - GD:
      . Fixed bug #74343 (compile fails on solaris 11 with system gd2 library).
        (krakjoe)
    
    - intl:
      . Fixed bug #74433 (wrong reflection for Normalizer methods). (villfa)
      . Fixed bug #74439 (wrong reflection for Locale methods). (villfa)
    
    - MySQLi:
      . Fixed bug #74432 (mysqli_connect adding ":3306" to $host if $port parameter
        not given). (Anatol)
    
    - MySQLnd:
      . Added support for MySQL 8.0 types. (Johannes)
      . Fixed bug #74376 (Invalid free of persistent results on error/connection
        loss). (Yussuf Khalil)
    
    - OpenSSL:
      . Fixed bug #73833 (null character not allowed in openssl_pkey_get_private).
        (Jakub Zelenka)
      . Fixed bug #73711 (Segfault in openssl_pkey_new when generating DSA or DH
        key). (Jakub Zelenka)
      . Fixed bug #74341 (openssl_x509_parse fails to parse ASN.1 UTCTime without
        seconds). (Moritz Fain)
      . Added OpenSSL 1.1.0 support. (Jakub Zelenka)
    
    - phar:
      . Fixed bug #74383 (phar method parameters reflection correction).
        (mhagstrand)
    
    - Standard:
      . Fixed bug #74409 (Reflection information for ini_get_all() is incomplete).
        (Sebastian Bergmann)
      . Fixed bug #72071 (setcookie allows max-age to be negative). (Craig Duncan)
    
    - Streams:
      . Fixed bug #74429 (Remote socket URI with unique persistence identifier
        broken). (Sara)
    
    - SQLite3:
      . Fixed bug #74413 (incorrect reflection for SQLite3::enableExceptions).
        (krakjoe)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.177 2017/04/14 15:43:26 taca Exp $
+# $NetBSD: phpversion.mk,v 1.178 2017/05/12 14:45:36 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -88,7 +88,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP56_VERSION=	5.6.30
-PHP70_VERSION=	7.0.18
+PHP70_VERSION=	7.0.19
 PHP71_VERSION=	7.1.4
 
 # Define initial release of major version.
