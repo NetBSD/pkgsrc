da64df9a9e4dcb4a7e6077c7a245522381dfac59
spz <spz@pkgsrc.org>
2017-05-14 12:55:16 +0000

    updating from version 2.36.4 to 2.36.6 adding patches for: CVE-2017-6311 CVE-2017-6312 CVE-2017-6313 CVE-2017-6314 from bugzilla.gnome.org

---
+++ graphics/gdk-pixbuf2/patches/patch-gdk-pixbuf_io-icns.c
@@ -0,0 +1,16 @@
+$NetBSD: patch-gdk-pixbuf_io-icns.c,v 1.3 2017/05/14 12:55:16 spz Exp $
+
+from hint in https://bugzilla.gnome.org/show_bug.cgi?id=779016
+for CVE-2017-6313
+
+--- gdk-pixbuf/io-icns.c.orig	2016-10-22 03:38:29.000000000 +0000
++++ gdk-pixbuf/io-icns.c
+@@ -95,7 +95,7 @@ load_resources (unsigned size, IN gpoint
+       blocklen = GUINT32_FROM_BE (header->size);
+ 
+       /* Check that blocklen isn't garbage */
+-      if (blocklen > icnslen - (current - bytes))
++      if ((blocklen > icnslen - (current - bytes)) || (blocklen < sizeof (IcnsBlockHeader)))
+         return FALSE;
+ 
+       switch (size)
