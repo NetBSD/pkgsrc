0854bb744db1f74c3049e2c7a792b7278304f6fc
maya <maya@pkgsrc.org>
2017-05-14 02:38:02 +0000

    gcc7: correct typo in patch, from Kai-Uwe Eckhardt Reportedly fixes a bunch of tests. make our cpuinfo patch possibly less harmful to glibc by using the same macro as the symver logic
    bump pkgrevision

--- lang/gcc7/patches/patch-libgcc_config_i386_cpuinfo.h
+++ lang/gcc7/patches/patch-libgcc_config_i386_cpuinfo.h
@@ -1,14 +1,18 @@
-$NetBSD: patch-libgcc_config_i386_cpuinfo.h,v 1.1 2017/05/04 17:38:23 maya Exp $
+$NetBSD: patch-libgcc_config_i386_cpuinfo.h,v 1.2 2017/05/14 02:38:03 maya Exp $
 
-Fix GCC bug #80600
+ifdef out extern for GCC bug #80600, with the same condition as
+cpuinfo.c. removing triggers GCC bug #65612
 
 --- libgcc/config/i386/cpuinfo.h.orig	2017-01-10 12:55:41.000000000 +0000
 +++ libgcc/config/i386/cpuinfo.h
-@@ -108,7 +108,7 @@ enum processor_features
+@@ -108,7 +108,10 @@ enum processor_features
    FEATURE_AVX512VPOPCNTDQ
  };
  
 -extern struct __processor_model
++#if defined SHARED && defined USE_ELF_SYMVER
++extern
++#endif
 +struct __processor_model
  {
    unsigned int __cpu_vendor;
