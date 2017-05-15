752347c42f56acd8271bddc807a8fc32c8034d1e
jperkin <jperkin@pkgsrc.org>
2017-05-15 10:26:19 +0000

    Fix includes.

--- multimedia/vlc/patches/patch-include_vlc_atomic.h
+++ multimedia/vlc/patches/patch-include_vlc_atomic.h
@@ -1,11 +1,11 @@
-$NetBSD: patch-include_vlc_atomic.h,v 1.1 2015/11/01 20:15:06 tnn Exp $
+$NetBSD: patch-include_vlc_atomic.h,v 1.2 2017/05/15 10:26:19 jperkin Exp $
 
 clang ships everything we need, just use <atomic> instead of the
 vlc_atomic.h #ifdef maze.
 
---- include/vlc_atomic.h.orig	2014-08-14 07:20:03.000000000 +0000
+--- include/vlc_atomic.h.orig	2015-02-02 19:42:28.000000000 +0000
 +++ include/vlc_atomic.h
-@@ -32,6 +32,11 @@
+@@ -32,7 +32,14 @@
  /*** Native C11 atomics ***/
  #  include <stdatomic.h>
  
@@ -15,5 +15,8 @@ vlc_atomic.h #ifdef maze.
 +using std::atomic_uintptr_t;
 +using std::atomic_uint;
  # else
++#include <sys/types.h>
++#include <stdint.h>
  
  #  define ATOMIC_FLAG_INIT false
+ 
