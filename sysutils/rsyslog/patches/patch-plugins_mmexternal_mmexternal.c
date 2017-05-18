6bfeaf3b16c3ec0b34bb21f668cb649a54f3aa97
fhajny <fhajny@pkgsrc.org>
2017-05-18 13:27:44 +0000

    Update sysutils/rsyslog to 8.27.0
    Version 8.27.0 [v8-stable] 2017-05-16
    - imkafka: add module
    - imptcp enhancements:
      * optionally emit an error message if incoming messages are truncated
      * optionally emit connection tracking message (on connection create and
        close)
      * add "maxFrameSize" parameter to specify the maximum size permitted
        in octet-counted mode
      * add parameter "discardTruncatedMsg" to permit truncation of
        oversize messages
      * improve octect-counted mode detection: if the octet count is larger
        then the set frame size (or overly large in general), it is now
        assumed that octet-stuffing mode is used. This probably solves a
        number of issues seen in real deployments.
    - imtcp enhancements:
      * add parameter "discardTruncatedMsg" to permit truncation of
        oversize messages
      * add "maxFrameSize" parameter to specify the maximum size permitted
        in octet-counted mode
    - imfile bugfix: "file not found error" repeatedly being reported
      for configured non-existing file.
    - imfile: in inotify mode, add error message if configured file cannot
      be found
    - imfile: add parameter "fileNotFoundError" to optinally disable
      "file not found" error messages
    - core: replaced gethostbyname() with getaddrinfo() call
    - omkafka: add "origin" field to stats output
    - imuxsock: rate-limiting also uses process name
      both for the actual limit procesing as well as warning messages emitted
    - Added new module: KSI log signing ver. 1.2 (lmsig_ksi_ls12)
    - rsylsog base functionality now builds on osx (Mac)
    - build now works on solaris again
    - imfile: fix cross-platform build issue
    - bugfix core: segfault when no parser could parse message
    - bugfix core: rate-limit internal messages when going to external log system
    - bugfix core: when obtaining local hostname, a NULL pointer could be
      accessed.
    - bugfix core: on shutdown, stderr was written to, even if alrady closed
    - bugfix core: perform MainqObj destruction only when not NULL already
    - bugfix core: memory leak when internal messages not processed internally
    - bugfix imptcp: potential overflow in octet count computation
      when a very large octet count was specified, the counter could overflow

--- sysutils/rsyslog/patches/patch-plugins_mmexternal_mmexternal.c
+++ sysutils/rsyslog/patches/patch-plugins_mmexternal_mmexternal.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-plugins_mmexternal_mmexternal.c,v 1.5 2017/02/13 19:59:36 fhajny Exp $
+$NetBSD: patch-plugins_mmexternal_mmexternal.c,v 1.6 2017/05/18 13:27:44 fhajny Exp $
 
 Fix build on BSD.
---- plugins/mmexternal/mmexternal.c.orig	2017-01-10 09:00:04.000000000 +0000
+--- plugins/mmexternal/mmexternal.c.orig	2017-05-15 09:41:19.000000000 +0000
 +++ plugins/mmexternal/mmexternal.c
 @@ -31,7 +31,7 @@
  #include <errno.h>
  #include <unistd.h>
  #include <fcntl.h>
--#if defined(_AIX) || defined(__FreeBSD__) 
-+#if defined(_AIX) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFlyBSD__) || defined(__APPLE__)
+-#if defined(_AIX) || defined(__FreeBSD__) || defined(__APPLE__) 
++#if defined(_AIX) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFlyBSD__) || defined(__APPLE__) 
  #include <sys/wait.h>
  #else
  #include <wait.h>
