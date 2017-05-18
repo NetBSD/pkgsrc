28ae145767a37f9693a4a94fc32d62aaa45a94d1
adam <adam@pkgsrc.org>
2017-05-18 21:20:23 +0000

    Crypto++ 5.6.5
    The 5.6.5 release was mostly a maintenance release. The release included two CVE fixes.
    
    The first, CVE-2016-7420, was a procedural finding due to external build systems failing to define NDEBUG for release builds. The gap was the project's failure to tell users to define NDEBUG. The second, CVE-2016-7544, was a potential memory corruption on Windows platforms when using Microsoft compilers due to use of _malloca and _freea.
    
    Due to CVE-2016-7420 and the possibility for an unwanted assert to egress data, users and distros are encouraged to recompile the library and all dependent programs.

--- security/crypto++/buildlink3.mk
+++ security/crypto++/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2015/03/27 14:14:26 rodent Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2017/05/18 21:20:23 adam Exp $
 
 BUILDLINK_TREE+=	cryptopp
 
@@ -7,7 +7,7 @@ CRYPTOPP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cryptopp+=	cryptopp>=5.6.2
 BUILDLINK_ABI_DEPENDS.cryptopp+=	cryptopp>=5.6.2
-BUILDLINK_PKGSRCDIR.cryptopp?=	../../security/crypto++
+BUILDLINK_PKGSRCDIR.cryptopp?=		../../security/crypto++
 .endif # CRYPTOPP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-cryptopp
