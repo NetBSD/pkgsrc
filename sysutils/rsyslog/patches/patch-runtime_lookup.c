5dc76ef81579663b7d129d45092669090126c540
fhajny <fhajny@pkgsrc.org>
2017-05-18 13:27:44 +0000

    Update sysutils/rsyslog to 8.27.0
    Version 8.27.0 [v8-stable] 2017-05-16
    - imkafka: add module
    - imptcp enhancements:
      * optionally emit an error message if incoming messages are truncated
      * optionally emit connection tracking message (on connection create and
        close)
      * add "maxFrameSize" parameter to specify the maximum size permitted
        in octet-counted mode
      * add parameter "discardTruncatedMsg" to permit truncation of
        oversize messages
      * improve octect-counted mode detection: if the octet count is larger
        then the set frame size (or overly large in general), it is now
        assumed that octet-stuffing mode is used. This probably solves a
        number of issues seen in real deployments.
    - imtcp enhancements:
      * add parameter "discardTruncatedMsg" to permit truncation of
        oversize messages
      * add "maxFrameSize" parameter to specify the maximum size permitted
        in octet-counted mode
    - imfile bugfix: "file not found error" repeatedly being reported
      for configured non-existing file.
    - imfile: in inotify mode, add error message if configured file cannot
      be found
    - imfile: add parameter "fileNotFoundError" to optinally disable
      "file not found" error messages
    - core: replaced gethostbyname() with getaddrinfo() call
    - omkafka: add "origin" field to stats output
    - imuxsock: rate-limiting also uses process name
      both for the actual limit procesing as well as warning messages emitted
    - Added new module: KSI log signing ver. 1.2 (lmsig_ksi_ls12)
    - rsylsog base functionality now builds on osx (Mac)
    - build now works on solaris again
    - imfile: fix cross-platform build issue
    - bugfix core: segfault when no parser could parse message
    - bugfix core: rate-limit internal messages when going to external log system
    - bugfix core: when obtaining local hostname, a NULL pointer could be
      accessed.
    - bugfix core: on shutdown, stderr was written to, even if alrady closed
    - bugfix core: perform MainqObj destruction only when not NULL already
    - bugfix core: memory leak when internal messages not processed internally
    - bugfix imptcp: potential overflow in octet count computation
      when a very large octet count was specified, the counter could overflow

--- sysutils/rsyslog/patches/patch-runtime_lookup.c
+++ sysutils/rsyslog/patches/patch-runtime_lookup.c
@@ -1,21 +1,22 @@
-$NetBSD: patch-runtime_lookup.c,v 1.2 2016/04/22 16:52:14 jperkin Exp $
+$NetBSD: patch-runtime_lookup.c,v 1.3 2017/05/18 13:27:44 fhajny Exp $
 
 Handle Darwin's pthread_setname_np().
 
---- runtime/lookup.c.orig	2016-03-07 06:47:55.000000000 +0000
+--- runtime/lookup.c.orig	2017-05-15 09:41:19.000000000 +0000
 +++ runtime/lookup.c
-@@ -926,8 +926,14 @@ lookupTableDefProcessCnf(struct cnfobj *
+@@ -958,10 +958,12 @@ lookupTableDefProcessCnf(struct cnfobj *
  	strcpy(reloader_thd_name, reloader_prefix);
  	strcpy(reloader_thd_name + strlen(reloader_prefix), (char*) lu->name);
  	reloader_thd_name[thd_name_len - 1] = '\0';
-+#if defined(__NetBSD__)
-+	pthread_setname_np(lu->reloader, "%s", reloader_thd_name);
-+#elif defined(__APPLE__)
-+	pthread_setname_np(reloader_thd_name);
-+#else
- 	pthread_setname_np(lu->reloader, reloader_thd_name);
+-  #ifndef __APPLE__
+-     pthread_setname_np(lu->reloader, reloader_thd_name);
++  #if defined(__NetBSD__)
++     pthread_setname_np(lu->reloader, "%s", reloader_thd_name);
++  #elif defined(__APPLE__)
++     pthread_setname_np(reloader_thd_name); // must check
+   #else
+-     pthread_setname_np(reloader_thd_name); // must check
++     pthread_setname_np(lu->reloader, reloader_thd_name);
+   #endif
  #endif
-+#endif
  	CHKiRet(lookupReadFile(lu->self, lu->name, lu->filename));
- 	DBGPRINTF("lookup table '%s' loaded from file '%s'\n", lu->name, lu->filename);
- 
