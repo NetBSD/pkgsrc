f13d8e1909d78b34dc747bfbabe4ce3894e5811f
joerg <joerg@pkgsrc.org>
2017-05-19 14:59:23 +0000

    Expand terminfo handling to depending packages. Fixes lang/clang.

--- lang/llvm/buildlink3.mk
+++ lang/llvm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2017/05/05 23:13:05 kamil Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2017/05/19 14:59:23 joerg Exp $
 
 BUILDLINK_TREE+=	llvm
 
@@ -9,6 +9,12 @@ BUILDLINK_API_DEPENDS.llvm+=	llvm>=4.0.0
 BUILDLINK_PKGSRCDIR.llvm?=	../../lang/llvm
 
 LLVM_CONFIG_PATH?=		${BUILDLINK_PREFIX.llvm}/bin/llvm-config
+
+pkgbase := llvm
+.include "../../mk/pkg-build-options.mk"
+.  if !empty(PKG_BUILD_OPTIONS.llvm:Mterminfo)
+.include "../../mk/terminfo.buildlink3.mk"
+.  endif
 
 .include "../../devel/zlib/buildlink3.mk"
 .endif	# LLVM_BUILDLINK3_MK
