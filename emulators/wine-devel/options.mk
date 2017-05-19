679e37aafc42bc18118f01c7ee67591955564ee6
prlw1 <prlw1@pkgsrc.org>
2017-05-19 13:18:17 +0000

    Switch all cups15 packages to use cups.

--- emulators/wine-devel/options.mk
+++ emulators/wine-devel/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.9 2016/09/18 17:53:41 maya Exp $
+# $NetBSD: options.mk,v 1.10 2017/05/19 13:18:17 prlw1 Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.wine
 PKG_SUPPORTED_OPTIONS=	cups dbus esound hal ldap opengl sane ssl x11 pulseaudio
@@ -9,7 +9,7 @@ PKG_SUGGESTED_OPTIONS=	opengl ssl x11
 PLIST_VARS+=		esd opengl x11 pulseaudio
 
 .if !empty(PKG_OPTIONS:Mcups)
-.include "../../print/cups15/buildlink3.mk"
+.include "../../print/cups/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-cups
 .endif
