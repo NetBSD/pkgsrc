00b029d0c0943cf2207c45c8ac3d76d648b7c4d8
schmonz <schmonz@pkgsrc.org>
2017-05-20 01:39:12 +0000

    Update to 0.99 (new upstream). From the changelog:
    Included ucspi-ssl-0.70_ucspitls-0.6.patch (STARTTLS support)
    originally designed and provided by Scott Gifford (FEH).
    
    Added Certchain support for sslserver and sslclient (FEH).
    
    Integration and added man-pages (FEH).
    
    Synced with ucspi-tcp6-0.95.
    
    Fixed integration bug in ssl_very.c.
    Included patches from Peter Conrad.
    
    Bug fix in sslserver. Several small
    corrections.
    
    Fix for large X509 serial numbers on x86 (tx. Peter Conrad).
    SAN DNSname has precedence over CN in subject.
    Re-edited man pages and rts tests.
    
    Added IPv6 support (tx. to Felix von Leitner and Brandon Turner).
    UI: Changed sslserver client cert call from '-i/-I' to '-z/-Z'
    for compatibility reasons.
    Added '-4/-6' support for client scripts.
    
    Added output environment variables TCP6* for sslserver.
    sslperl, sslhandle, and sslprint are not IPv6 ready yet.
    
    Added IPv6 capabilities to sslhandle, sslprint, sslperl.
    Changed verification of X.509 certs.
    Removed obsolete socket_4 calls in sslserver.
    
    Streamlined code with ucspi-tcp6-1.00.
    Supplied new certs with customized SAN.
    Make rts working (at least some how).
    
    Added support for personalized client certs.
    New option '-m' in sslserver, complementing '-z'.
    CCAFILE='-' disables client cert request.
    
    Added verbose log output for SSL connection informations.
    
    Fixed wrongly nested CONNECT error code for sslclient.c
    producing wrong warning messages while connecting to
    an IPv4 address.
    Added call of '-ldl' in ssl.lib.
    
    Mitigation of SSL connection hanging during
    coincident change of daylight-saving settings.
    
    Fixed bug in sslserver's dnsip lookup in case of paranoid settings
    and additonal existance of IPv6 AAAA records for incoming IPv4 connection.
    
    Serveral fixes from 'troy@' included to cope with compiler errors and
    to solve a bug in function getbitasaddress in ip4_bit.c (= ucspi-tcp6-1.02).
    Reordered conf-* variables in main dir to allow easier generation of
    packages (i.e. RPM). Fixed script to identify different HW architecture
    and OS. This version works in 32 bit mode on Raspian Linux / RasPi 7.
    
    Added ECDH capabilites (tx to Frank Bergmann for the patches).
    
    Added compatibility with LibreSSL.
    Fixed missing negative return call treatment from 'poll' (tx Frank Bergmann).
    Tentative 'emake' fix for Gentoo build.
    
    Added OpenSSL 1.1 tweaks -- works under Debian (9) 'Stretch'.

--- net/ucspi-ssl/options.mk
+++ net/ucspi-ssl/options.mk
@@ -1,8 +1,7 @@
-# $NetBSD: options.mk,v 1.3 2011/09/17 11:12:26 shattered Exp $
+# $NetBSD: options.mk,v 1.4 2017/05/20 01:39:12 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ucspi-ssl
-PKG_SUPPORTED_OPTIONS+=	perl tls
-PKG_OPTIONS_LEGACY_OPTS+=	ucspi-ssl-perl:perl
+PKG_SUPPORTED_OPTIONS+=	perl
 
 .include "../../mk/bsd.options.mk"
 
@@ -14,11 +13,4 @@ PLIST_SRC+=		${PKGDIR}/PLIST.perl
 DJB_CONFIG_CMDS+=	${ECHO} ${PERL5} > conf-perl;
 .else
 DJB_BUILD_ARGS+=	- sslperl
-.endif
-
-.if !empty(PKG_OPTIONS:Mtls)
-TLS_PATCH=		ucspi-ssl-0.70-ucspitls-0.1.patch
-PATCHFILES+=		${TLS_PATCH}
-SITES.${TLS_PATCH}=	http://www.suspectclass.com/~sgifford/ucspi-tls/files/
-PATCH_DIST_STRIP.${TLS_PATCH}=	-p1
 .endif
