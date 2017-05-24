cfad319ab53d36fcd9413b4dc6fab3342652d7f0
adam <adam@pkgsrc.org>
2017-05-24 19:51:12 +0000

    Removed databases/postgresql91

--- databases/postgresql-postgis2/buildlink3.mk
+++ databases/postgresql-postgis2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2016/10/09 21:41:57 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2017/05/24 19:51:12 adam Exp $
 
 BUILDLINK_TREE+=	postgresql-postgis
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.postgresql-postgis+=	postgresql${PGSQL_VERSION}-postgis>=1
 BUILDLINK_ABI_DEPENDS.postgresql-postgis?=	postgresql${PGSQL_VERSION}-postgis>=2.2.2nb3
 BUILDLINK_PKGSRCDIR.postgresql-postgis?=	../../databases/postgresql-postgis2
 
-PGSQL_VERSIONS_ACCEPTED=	94 93 92 91
+PGSQL_VERSIONS_ACCEPTED=	94 93 92
 
 .include "../../geography/geos/buildlink3.mk"
 .include "../../geography/proj/buildlink3.mk"
