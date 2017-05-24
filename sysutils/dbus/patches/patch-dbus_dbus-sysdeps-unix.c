f56dbcdc6170ff2323ae078c1e1d9cf9022268a9
jperkin <jperkin@pkgsrc.org>
2017-05-24 08:06:28 +0000

    Fix patch, ensure it is limited to SunOS only.

--- sysutils/dbus/patches/patch-dbus_dbus-sysdeps-unix.c
+++ sysutils/dbus/patches/patch-dbus_dbus-sysdeps-unix.c
@@ -1,17 +1,19 @@
-$NetBSD: patch-dbus_dbus-sysdeps-unix.c,v 1.4 2017/05/23 15:12:57 jperkin Exp $
+$NetBSD: patch-dbus_dbus-sysdeps-unix.c,v 1.5 2017/05/24 08:06:28 jperkin Exp $
 
 Support _XOPEN_SOURCE requirements on SunOS with different compiler defaults.
 
 --- dbus/dbus-sysdeps-unix.c.orig	2017-02-16 13:46:23.000000000 +0000
 +++ dbus/dbus-sysdeps-unix.c
-@@ -24,6 +24,12 @@
+@@ -24,6 +24,14 @@
  
  #include <config.h>
  
-+#if defined(__sun) && __STDC_VERSION__ - 0 < 199901L
++#ifdef __sun
++#  if __STDC_VERSION__ - 0 < 199901L
 +#define	_XOPEN_SOURCE	500
-+#else
++#  else
 +#define	_XOPEN_SOURCE	600
++#  endif
 +#endif
 +
  #include "dbus-internals.h"
