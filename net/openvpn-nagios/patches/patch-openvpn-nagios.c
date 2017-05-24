3f16217f43395cdbb9e72af0113628e30c01ce89
adam <adam@pkgsrc.org>
2017-05-24 20:35:12 +0000

    OpenVPN 2.4.2
    Compared to OpenVPN 2.3 this is a major update with a large number of new features, improvements and fixes. Some of the major features are AEAD (GCM) cipher and Elliptic Curve DH key exchange support, improved IPv4/IPv6 dual stack support and more seamless connection migration when client's IP address changes (Peer-ID). Also, the new --tls-crypt feature can be used to increase users' connection privacy.
    
    Compared to OpenVPN 2.4.1 there are several bugfixes and small enhancements. A summary of the changes is available in Changes.rst.

--- net/openvpn-nagios/patches/patch-openvpn-nagios.c
+++ net/openvpn-nagios/patches/patch-openvpn-nagios.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-openvpn-nagios.c,v 1.1 2013/03/26 23:32:50 joerg Exp $
+$NetBSD: patch-openvpn-nagios.c,v 1.2 2017/05/24 20:35:12 adam Exp $
 
---- openvpn-nagios.c.orig	2013-03-25 15:45:11.000000000 +0000
+--- openvpn-nagios.c.orig	2013-02-10 01:33:07.000000000 +0000
 +++ openvpn-nagios.c
 @@ -87,12 +87,11 @@ strmatch(s, match)
  #endif
@@ -16,4 +16,13 @@ $NetBSD: patch-openvpn-nagios.c,v 1.1 2013/03/26 23:32:50 joerg Exp $
 +	(void)strptime((char *)strdate, "%y%m%d%H%M%SZ", &tm);
  	return mktime(&tm);
  }
+ 
+@@ -112,7 +111,7 @@ openvpn_plugin_open_v3(vers, in, out)
+ 	struct ctx *ctx;
+ 	pthread_t thread;
+ 	int ac = 0;
+-	char **av = __UNCONST(in->argv);
++	char **av = (void *)(unsigned long)(const void *)(in->argv);
+ 	int i, ch;
+ 	time_t now;
  
