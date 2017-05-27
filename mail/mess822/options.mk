bba20baa0643091c9ed6e917156b54cc4a055d1b
schmonz <schmonz@pkgsrc.org>
2017-05-27 16:58:42 +0000

    Add "mess822-qmailqueue" option to apply the QMAILQUEUE patch to ofmipd(8) and new-inject(8). The patch is enabled by default for qmail (via "qmail-netqmail"), so do the same here. Bump PKGREVISION.

--- mail/mess822/options.mk
+++ mail/mess822/options.mk
@@ -1,13 +1,21 @@
-# $NetBSD: options.mk,v 1.1 2017/01/14 20:49:07 schmonz Exp $
+# $NetBSD: options.mk,v 1.2 2017/05/27 16:58:42 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mess822
-PKG_SUPPORTED_OPTIONS+=	sasl
+PKG_SUPPORTED_OPTIONS+=	mess822-qmailqueue sasl
+PKG_SUGGESTED_OPTIONS+=	mess822-qmailqueue
 
 .include "../../mk/bsd.options.mk"
 
+.if !empty(PKG_OPTIONS:Mmess822-qmailqueue)
+QMAILQUEUE_PATCH=		mess822-0.58-qmailqueue-20170527.patch
+PATCHFILES+=			${QMAILQUEUE_PATCH}
+SITES.${QMAILQUEUE_PATCH}=	https://schmonz.com/2017/05/27/
+PATCH_DIST_STRIP.${QMAILQUEUE_PATCH}=	-p1
+.endif
+
 .if !empty(PKG_OPTIONS:Msasl)
-SMTPAUTH_PATCH=	mess822-smtp-auth-patch.txt
-PATCHFILES+=		${SMTPAUTH_PATCH}
+SMTPAUTH_PATCH=			mess822-smtp-auth-patch.txt
+PATCHFILES+=			${SMTPAUTH_PATCH}
 SITES.${SMTPAUTH_PATCH}=	http://qmail.gurus.org/
 PATCH_DIST_STRIP.${SMTPAUTH_PATCH}=	-p1
 .endif
