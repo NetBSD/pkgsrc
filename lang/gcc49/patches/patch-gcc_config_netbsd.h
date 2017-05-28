7b4b1df1b8e1b65bacfb49cdf8f3f7f6dbbd36c8
maya <maya@pkgsrc.org>
2017-05-28 01:42:03 +0000

    gcc49: on netbsd, always link against libgcc. from Krister Walfridsson
    bump PKGREVISION

--- lang/gcc49/patches/patch-gcc_config_netbsd.h
+++ lang/gcc49/patches/patch-gcc_config_netbsd.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-gcc_config_netbsd.h,v 1.1 2016/09/05 20:17:01 maya Exp $
+$NetBSD: patch-gcc_config_netbsd.h,v 1.2 2017/05/28 01:42:03 maya Exp $
 
 when using shared, link against libc.
+always link against libgcc
 
---- gcc/config/netbsd.h.orig	2013-01-10 20:38:27.000000000 +0000
 +++ gcc/config/netbsd.h
 @@ -96,6 +96,7 @@ along with GCC; see the file COPYING3.  
         %{!pg:-lposix}}		\
@@ -20,3 +20,12 @@ when using shared, link against libc.
     %{!shared:			\
       %{!symbolic:		\
         %{!p:			\
+@@ -133,7 +135,7 @@ along with GCC; see the file COPYING3.  
+      %{p: -lgcc_p}		\
+      %{pg: -lgcc_p}}"
+ #else
+-#define NETBSD_LIBGCC_SPEC "%{!shared:%{!symbolic: -lgcc}}"
++#define NETBSD_LIBGCC_SPEC "-lgcc"
+ #endif
+ 
+ #undef LIBGCC_SPEC
