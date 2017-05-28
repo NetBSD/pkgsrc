5a5e945a2c7ae53eae9880b2b3609db137974052
maya <maya@pkgsrc.org>
2017-05-28 01:00:20 +0000

    gcc48: always link libgcc on netbsd (GCC r248037, applied to GCC 8 and above - thanks Krister Walfridsson)
    libstdc++: if it thinks vswprintf is broken, don't block the
    entire definition of to_string, just that of to_wstring. backport
    of GCC r223830 already applied to GCC 4.9. this applies to netbsd-6-0.
    
    Doesn't fix any build I wanted to fix, but it does get the build
    of LLVM4.x further on netbsd-6-0. maybe there's an odd package that
    could benefit from it.
    
    bump PKGREVISION.

--- lang/gcc48/patches/patch-gcc_config_netbsd.h
+++ lang/gcc48/patches/patch-gcc_config_netbsd.h
@@ -1,6 +1,7 @@
-$NetBSD: patch-gcc_config_netbsd.h,v 1.1 2016/09/04 18:29:39 maya Exp $
+$NetBSD: patch-gcc_config_netbsd.h,v 1.2 2017/05/28 01:00:20 maya Exp $
 
 when using shared, link against libc.
+always link against libgcc.
 
 --- gcc/config/netbsd.h.orig	2013-01-10 20:38:27.000000000 +0000
 +++ gcc/config/netbsd.h
@@ -20,3 +21,12 @@ when using shared, link against libc.
     %{!shared:			\
       %{!symbolic:		\
         %{!p:			\
+@@ -133,7 +135,7 @@ along with GCC; see the file COPYING3.  
+      %{p: -lgcc_p}		\
+      %{pg: -lgcc_p}}"
+ #else
+-#define NETBSD_LIBGCC_SPEC "%{!shared:%{!symbolic: -lgcc}}"
++#define NETBSD_LIBGCC_SPEC "-lgcc"
+ #endif
+ 
+ #undef LIBGCC_SPEC
