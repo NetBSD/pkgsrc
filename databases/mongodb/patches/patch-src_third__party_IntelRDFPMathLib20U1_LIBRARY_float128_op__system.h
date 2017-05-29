517bb4e046804b81d264f8386f7c8fcf05f1acc2
ryoon <ryoon@pkgsrc.org>
2017-05-29 14:44:46 +0000

    Update to 3.4.4
    * Disable PaX MPROTECT for bin/mongo
    
    Changelog:
    3.4.4 – Apr 21, 2017
    
    Issues fixed:
    
        SERVER-18794: Add an aggregation operator $objectToArray to convert an object to an array of key, value pairs.
        SERVER-23310: Add an aggregation operator $arrayToObject to convert an array of pairs to an object.
        SERVER-22611: ChunkManager refresh can occasionally cause a full reload.
        3.4.4 Changelog
        All JIRA issues closed in 3.4.4
    
    3.4.3 – Mar 28, 2017
    
    Issues fixed:
    
        SERVER-27863: Reschedule firing of early alarms in NetworkInterfaceASIO to avoid mongos crash.
        SERVER-28017: $ne should respect collection’s default collation.
        SERVER-27700: Improve WiredTiger performance on secondary when cache is full.
        3.4.3 Changelog
        All JIRA issues closed in 3.4.3
    
    3.4.2 – Feb 1, 2017
    
    Issues fixed:
    
        SERVER-27125: Arbiters in pv1 should vote no in elections if they can see a healthy primary of equal or greater priority to the candidate.
        SERVER-27584 Add support for filter to listDatabases
        WT-2670 Inefficient I/O when read full DB (poor readahead)
        3.4.2 Changelog
        All JIRA issues closed in 3.4.2
    
    3.4.1 – Dec 20, 2016
    
    Issues fixed:
    
        SERVER-27124: Disallow readConcern: majority reads on replica set protocolVersion 0 (pv0).
        SERVER-27201: $graphLookup triggers null pointer dereference.
        SERVER-27207: Find operation with a sort on a view via mongos may incorrectly return empty result set.
        SERVER-27213: Two $match pipeline stages can combine incorrectly to produce incorrect results.
        3.4.1 Changelog
        All JIRA issues closed in 3.4.1

---
+++ databases/mongodb/patches/patch-src_third__party_IntelRDFPMathLib20U1_LIBRARY_float128_op__system.h
@@ -0,0 +1,120 @@
+$NetBSD: patch-src_third__party_IntelRDFPMathLib20U1_LIBRARY_float128_op__system.h,v 1.1 2017/05/29 14:44:47 ryoon Exp $
+
+--- src/third_party/IntelRDFPMathLib20U1/LIBRARY/float128/op_system.h.orig	2017-04-20 21:43:42.000000000 +0000
++++ src/third_party/IntelRDFPMathLib20U1/LIBRARY/float128/op_system.h
+@@ -44,6 +44,7 @@
+ #       undef  win64 
+ #       undef  darwin 
+ #       undef  interix 
++#       undef  netbsd
+ 
+ #	define dos 1
+ #	define OP_SYSTEM dos
+@@ -62,6 +63,7 @@
+ #       undef   win64
+ #       undef  darwin 
+ #       undef  interix 
++#       undef  netbsd
+ 
+ #	define vms 2
+ #	define OP_SYSTEM vms
+@@ -80,6 +82,7 @@
+ #       undef  win64
+ #       undef  darwin 
+ #       undef  interix 
++#       undef  netbsd
+ 
+ #	define wnt 3
+ #	define OP_SYSTEM wnt
+@@ -98,6 +101,7 @@
+ #       undef  win64
+ #       undef  darwin 
+ #       undef  interix 
++#       undef  netbsd
+ 
+ #	define linux 8
+ #	define OP_SYSTEM linux
+@@ -117,6 +121,7 @@
+ #       undef  win64
+ #       undef  darwin 
+ #       undef  interix 
++#       undef  netbsd
+ 
+ #	define osf 4
+ #	define OP_SYSTEM osf
+@@ -135,6 +140,7 @@
+ #       undef  win64
+ #       undef  darwin 
+ #       undef  interix 
++#       undef  netbsd
+ 
+ #	define hp_ux 5
+ #	define OP_SYSTEM hp_ux
+@@ -153,6 +159,7 @@
+ #       undef  win64
+ #       undef  darwin 
+ #       undef  interix 
++#       undef  netbsd
+ 
+ #	define unicos 6
+ #	define OP_SYSTEM unicos
+@@ -171,6 +178,7 @@
+ #       undef  win64
+ #       undef  darwin 
+ #       undef  interix 
++#       undef  netbsd
+ 
+ #	define ultrix 7
+ #	define OP_SYSTEM ultrix
+@@ -188,6 +196,7 @@
+ #       undef  win64
+ #       undef  darwin 
+ #       undef  interix 
++#       undef  netbsd
+ 
+ #       define win64 9
+ #       define OP_SYSTEM win64
+@@ -205,6 +214,7 @@
+ #       undef  win64
+ #       undef  darwin 
+ #       undef  interix 
++#       undef  netbsd
+ 
+ #       define darwin 10
+ #       define OP_SYSTEM darwin
+@@ -221,9 +231,27 @@
+ #       undef  win64
+ #       undef  darwin 
+ #       undef  interix 
++#       undef  netbsd
+ 
+ #       define interix 11
+ #       define OP_SYSTEM interix
++
++#elif defined(__NetBSD__)
++#       undef  dos
++#       undef  vms
++#       undef  wnt
++#       undef  osf
++#       undef  hp_ux
++#       undef  linux
++#       undef  unicos
++#       undef  ultrix
++#       undef  win64
++#       undef  darwin 
++#       undef  interix 
++#       undef  netbsd
++
++#       define netbsd 12
++#       define OP_SYSTEM netbsd
+ #else
+ 
+ #	error Operating system must be specified.
+@@ -235,6 +263,7 @@
+ 	OP_SYSTEM == linux || \
+ 	OP_SYSTEM == osf || \
+ 	OP_SYSTEM == ultrix || \
++	OP_SYSTEM == netbsd || \
+ 	OP_SYSTEM == unicos \
+ )
+ 
