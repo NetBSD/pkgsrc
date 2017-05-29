b38b76b06c50feb9969e503dd51cb128d9a7deee
wiz <wiz@pkgsrc.org>
2017-05-29 12:20:23 +0000

    Recursive bump for lang/vala

--- lang/vala/buildlink3.mk
+++ lang/vala/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.14 2016/04/10 18:49:42 prlw1 Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2017/05/29 12:20:23 wiz Exp $
 
 BUILDLINK_TREE+=	vala
 
 .if !defined(VALA_BUILDLINK3_MK)
 VALA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vala+=	vala>=0.32
+BUILDLINK_API_DEPENDS.vala+=	vala>=0.36
 BUILDLINK_PKGSRCDIR.vala?=	../../lang/vala
 
-VALAC=${LOCALBASE}/bin/valac-0.32
-VAPIGEN=${LOCALBASE}/bin/vapigen-0.32
+VALAC=${LOCALBASE}/bin/valac-0.36
+VAPIGEN=${LOCALBASE}/bin/vapigen-0.36
 .if defined(GNU_CONFIGURE)
 CONFIGURE_ENV+=	VALAC=${VALAC} VAPIGEN=${VAPIGEN}
 .endif
