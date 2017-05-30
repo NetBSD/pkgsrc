ef30bf0dd08ce1d9a0a30bad85dc55415dc078a5
taca <taca@pkgsrc.org>
2017-05-30 14:19:51 +0000

    * Remove code for ruby18. * Drop setting RUBY_VERSIONS_ACCEPTED since it is now default value.

--- devel/ruby-readline/options.mk
+++ devel/ruby-readline/options.mk
@@ -1,22 +1,9 @@
-# $NetBSD: options.mk,v 1.7 2014/04/12 16:37:46 taca Exp $
+# $NetBSD: options.mk,v 1.8 2017/05/30 14:19:51 taca Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ruby
 PKG_SUPPORTED_OPTIONS=	readline ruby-build-ri-db
-
-#
-# GNU readline supports multi bytes characters.
-#
-PKG_SUGGESTED_OPTIONS=	ruby-build-ri-db
-
-.include "../../lang/ruby/rubyversion.mk"
-
-# Since license of Ruby prior to 1.9.3 is incompatible with newer
-# readline's GPL3, enable it with 1.9.3 and lator.
-#
-.if ${RUBY_VER} != 18
-PKG_SUGGESTED_OPTIONS+=	readline
-.endif
+PKG_SUGGESTED_OPTIONS=	ruby-build-ri-db readline
 
 .include "../../mk/bsd.options.mk"
 
