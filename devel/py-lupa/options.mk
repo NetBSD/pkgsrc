22c0fc2474abb35605ac3ab2f6d9c44e518a01dc
alnsn <alnsn@pkgsrc.org>
2017-05-30 21:59:56 +0000

    Add support for Lua 5.1 and 5.2. Add "luajit" option (off by default).
    Bump PKGREVISION.

--- devel/py-lupa/options.mk
+++ devel/py-lupa/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1.1.1 2015/04/20 19:34:47 alnsn Exp $
+# $NetBSD: options.mk,v 1.2 2017/05/30 21:59:56 alnsn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-lupa
-PKG_SUPPORTED_OPTIONS=	cython
+PKG_SUPPORTED_OPTIONS=	cython luajit
 PKG_SUGGESTED_OPTIONS+=	cython
 
 .include "../../mk/bsd.options.mk"
@@ -9,4 +9,10 @@ PKG_SUGGESTED_OPTIONS+=	cython
 .if !empty(PKG_OPTIONS:Mcython)
 PYSETUPBUILDARGS+=	--with-cython
 .include "../../devel/py-cython/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mluajit)
+.include "../../lang/LuaJIT2/buildlink3.mk"
+.else
+.include "../../lang/lua/buildlink3.mk"
 .endif
