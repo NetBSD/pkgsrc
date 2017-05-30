56a06aa7ae9861d1fa1f20a22e7172bd70ea1c77
bouyer <bouyer@pkgsrc.org>
2017-05-30 14:04:53 +0000

    Add support for NetBSD's socketcan implementation. Tested with the example from http://www.bencz.com/hacks/2016/07/10/python-and-socketcan/ Needs <netcan/can.h> rev 1.3 or newer.

---
+++ lang/python35/patches/patch-Modules_socketmodule.h
@@ -0,0 +1,15 @@
+$NetBSD: patch-Modules_socketmodule.h,v 1.1 2017/05/30 14:04:54 bouyer Exp $
+
+--- Modules/socketmodule.h.orig	2017-03-21 07:32:38.000000000 +0100
++++ Modules/socketmodule.h	2017-05-30 12:36:59.254776235 +0200
+@@ -91,6 +91,10 @@
+ #include <linux/can/bcm.h>
+ #endif
+ 
++#ifdef HAVE_NETCAN_CAN_H
++#include <netcan/can.h>
++#endif
++
+ #ifdef HAVE_SYS_SYS_DOMAIN_H
+ #include <sys/sys_domain.h>
+ #endif
