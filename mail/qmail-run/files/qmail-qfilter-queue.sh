5957fb69c921004526f8feca620d62dc32931448
schmonz <schmonz@pkgsrc.org>
2017-05-31 07:08:04 +0000

    Add dependency on qmail-qfilter. Deprecate qmail-qfilter-queue in favor of qmail-qfilter-{ofmipd,smtpd}-queue, and document how to enable filtering for incoming and submitted messages. Bump version.

--- mail/qmail-run/files/qmail-qfilter-queue.sh
+++ mail/qmail-run/files/qmail-qfilter-queue.sh
@@ -5,31 +5,61 @@
 #
 # Using this wrapper, executable filters:
 # * Must be placed in @PKG_SYSCONFDIR@/filters
-# * Must have filenames matching "qfilter-*"
+# * Must have filenames matching "ofmipd-*" or "smtpd-*"
 # * Will be executed with no arguments
 # * Will be executed in sort(1) order
-#
-# To use this script to filter messages arriving via SMTP, set the
-# QMAILQUEUE environment variable in tcp.smtp like so:
-#
-# :allow,QMAILQUEUE="@PREFIX@/bin/qmail-qfilter-queue"
-#
-# Then rebuild the cdb.
 
-possible_qfilters=`@ECHO@ @PKG_SYSCONFDIR@/filters/qfilter-* | @SORT@`
-qfilters=""
+CAT=@CAT@
+ECHO=@ECHO@
+PKG_SYSCONFDIR=@PKG_SYSCONFDIR@
+PREFIX=@PREFIX@
+SED=@SED@
+SORT=@SORT@
 
-if [ "${possible_qfilters}" = "@PKG_SYSCONFDIR@/filters/qfilter-*" ]; then
-	qfilters="@CAT@"
-else
-	for qfilter in ${possible_qfilters}; do
-		if [ -x "${qfilter}" ]; then
-			qfilters="${qfilters} ${qfilter}"
-		fi
-	done
-	[ -z "${qfilters}" ] && qfilters="@CAT@"
-fi
+warn() {
+	${ECHO} >&2 "$@"
+}
 
-qfilter_cmd="@PREFIX@/bin/qmail-qfilter `@ECHO@ ${qfilters} | @SED@ -e 's| | -- |g'`"
+choose_glob() {
+	local _progname _glob
+	_progname="$1"
+	case "${_progname}" in
+	qmail-qfilter-ofmipd-queue)	_glob='ofmipd-*'	;;
+	qmail-qfilter-smtpd-queue)	_glob='smtpd-*'		;;
+	qmail-qfilter-queue)		_glob='qfilter-*'	;;
+	esac
+	${ECHO} "${_glob}"
+}
 
-exec ${qfilter_cmd}
+choose_filters() {
+	local _glob _filters _possible_filters _filter
+	_glob="$1"
+	_possible_filters=$(${ECHO} ${PKG_SYSCONFDIR}/filters/${_glob} | ${SORT})
+	if [ "${_possible_filters}" = "${PKG_SYSCONFDIR}/filters/${_glob}" ]; then
+		_filters="${CAT}"
+	else
+		for _filter in ${_possible_filters}; do
+			[ -x "${_filter}" ] && _filters="${_filters} ${_filter}"
+		done
+		[ -z "${_filters}" ] && _filters="${CAT}"
+	fi
+	${ECHO} "${_filters}"
+}
+
+qfilter_args() {
+	local _filters
+	_filters="$1"
+	${ECHO} ${_filters} | ${SED} -e 's| | -- |g'
+}
+
+main() {
+	local _progname _glob _filters _args _cmd
+	_progname=$(basename "$0")
+	_glob=$(choose_glob "${_progname}")
+	_filters=$(choose_filters "${_glob}")
+	_args=$(qfilter_args "${_filters}")
+	_cmd="${PREFIX}/bin/qmail-qfilter ${_args}"
+	exec ${_cmd}
+}
+
+main "$@"
