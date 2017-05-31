225e15127450ec290fc0ba76f00d38d9cee9c949
maya <maya@pkgsrc.org>
2017-05-31 02:22:02 +0000

    sudo: workaround deficiencies in netbsd 6,7
    NetBSD 7 doesn't define WCONTINUED or WIFCONTINUED, so provide
    failure fallback definitions.
    
    Thanks nonaka for the heads up.

---
+++ security/sudo/patches/patch-include_sudo__compat.h
@@ -0,0 +1,20 @@
+$NetBSD: patch-include_sudo__compat.h,v 1.1 2017/05/31 02:22:02 maya Exp $
+
+Work around missing WCONTINUED/WIFCONTINUED support in
+NetBSD<8
+
+--- include/sudo_compat.h.orig	2017-05-10 15:38:43.000000000 +0000
++++ include/sudo_compat.h
+@@ -304,6 +304,12 @@ extern int errno;
+ # define SIG2STR_MAX 32
+ #endif
+ 
++/* Deficiencies in NetBSD<8 */
++#ifndef WCONTINUED
++# define WCONTINUED 0
++# define WIFCONTINUED(a) 0
++#endif
++
+ /* WCOREDUMP is not POSIX, this usually works (verified on AIX). */
+ #ifndef WCOREDUMP
+ # define WCOREDUMP(x)	((x) & 0x80)
