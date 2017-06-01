ecd3242deb2a36c2b61ae24a76b5a10c9474e8db
jlam <jlam@pkgsrc.org>
2017-06-01 02:08:04 +0000

    Add "init" framework for management of init scripts for packages.
    ${INIT_SYSTEM} is used to select the type of init system in use on
    the system, and currently can be either "rc.d" for the NetBSD rc.d
    system, or "smf" for the Solaris Service Management Facility
    system.
    
    pkgsrc/mk/init/smf.mk is pkgsrc/mk/smf.mk with some very minor
    modifications to fit into the "init" framework.

---
+++ mk/init/bsd.init-vars.mk
@@ -0,0 +1,44 @@
+# $NetBSD: bsd.init-vars.mk,v 1.1 2017/06/01 02:08:04 jlam Exp $
+#
+# Copyright (c) 2017 The NetBSD Foundation, Inc.
+# All rights reserved.
+#
+# This code is derived from software contributed to The NetBSD Foundation
+# by Johnny C. Lam.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# This Makefile fragment is included by bsd.prefs.mk and defines some
+# variables which must be defined earlier than where bsd.init.mk is
+# included.
+
+# System-settable variables:
+#
+# INIT_SYSTEM
+#	Type of system used to spawn processes as the last stage of
+#	the boot process.
+#
+#	Possible: rc.d, smf
+#	Default: rc.d
+#
+_SYS_VARS.init+=	INIT_SYSTEM
+_BUILD_DEFS+=		INIT_SYSTEM
+INIT_SYSTEM?=		rc.d
