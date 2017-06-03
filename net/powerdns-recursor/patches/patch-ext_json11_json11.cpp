9e499d25df14a9f26e160f525f676fda16c0d8fb
bsiegert <bsiegert@pkgsrc.org>
2017-06-03 18:21:33 +0000

    Pullup ticket #5461 - requested by sevan net/powerdns-recursor: build fix
    Revisions pulled up:
    - net/powerdns-recursor/distinfo                                1.18
    - net/powerdns-recursor/patches/patch-ext_json11_json11.cpp     1.1
    
    ---
       Module Name:    pkgsrc
       Committed By:   joerg
       Date:           Mon May 22 23:41:52 UTC 2017
    
       Modified Files:
               pkgsrc/net/powerdns-recursor: distinfo
       Added Files:
               pkgsrc/net/powerdns-recursor/patches: patch-ext_json11_json11.cpp
    
       Log Message:
       Merge patch from powerdns package to avoid ordering nullptrs.

---
+++ net/powerdns-recursor/patches/patch-ext_json11_json11.cpp
@@ -0,0 +1,30 @@
+$NetBSD: patch-ext_json11_json11.cpp,v 1.1.2.2 2017/06/03 18:21:33 bsiegert Exp $
+
+Clang rejects ordering relations for nullptr, so introduce a template
+indirection.
+
+--- ext/json11/json11.cpp.orig	2017-01-13 08:55:55.000000000 +0000
++++ ext/json11/json11.cpp
+@@ -132,6 +132,13 @@ void Json::dump(string &out) const {
+  * Value wrappers
+  */
+ 
++template <typename T> bool json11_less(T a, T b) {
++  return a < b;
++}
++template <> bool json11_less(const std::nullptr_t a, const std::nullptr_t b) {
++  return false;
++}
++
+ template <Json::Type tag, typename T>
+ class Value : public JsonValue {
+ protected:
+@@ -150,7 +157,7 @@ protected:
+         return m_value == static_cast<const Value<tag, T> *>(other)->m_value;
+     }
+     bool less(const JsonValue * other) const override {
+-        return m_value < static_cast<const Value<tag, T> *>(other)->m_value;
++        return json11_less(m_value, static_cast<const Value<tag, T> *>(other)->m_value);
+     }
+ 
+     const T m_value;
