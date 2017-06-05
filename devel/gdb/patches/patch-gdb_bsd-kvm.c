b85cbac9b3627cf73fa2b964c66866a10de3b921
maya <maya@pkgsrc.org>
2017-06-05 11:38:51 +0000

    gdb: update to 8.0
    GDB 8.0 brings the following fixes and improvements, including:
    
      * C++: Support for rvalue references
    
      * Python scripting enhancements:
        ** New functions to start, stop and access a running btrace recording.
        ** Rvalue reference support in gdb.Type.
    
      * GDB commands interpreter:
        ** User commands now accept an unlimited number of arguments.
        ** The "eval" command now expands user-defined arguments.
    
      * DWARF version 5 support
        (note that its .debug_names index is not supported yet).
    
      * GDB/MI enhancements:
        ** New -file-list-shared-libraries command to list the shared
           libraries in the program.
        ** New -target-flash-erase command, to erase flash memory.
    
      * Support for native FreeBSD/mips (mips*-*-freebsd)
    
      * Support for the following targets:
        ** Synopsys ARC (arc*-*-elf32)
        ** FreeBSD/mips (mips*-*-freebsd)
    
      * Miscellaneous enhancements:
        ** Command-line redirection now supported on MS-Windows hosts.
        ** Support for thread names on MS-Windows.
        ** Support for the PKU register on GNU/Linux.
        ** Support for Target descriptions on sparc32 and sparc64.
        ** New GDB/CLI command to erase flash memory
        ** rdrand and rdseed instructions record/replay support.
    
    Support for the following features have been removed:
    
      * Support for Java programs compiled with gcj
    
      * Support for the following configurations:
        ** FreeBSD/alpha (alpha*-*-freebsd*)
        ** GNU/kFreeBSD/alpha (alpha*-*-kfreebsd*-gnu)
    
    For a complete list and more details on each item, please see the
    gdb/NEWS file.

--- devel/gdb/patches/patch-gdb_bsd-kvm.c
+++ devel/gdb/patches/patch-gdb_bsd-kvm.c
@@ -1,33 +1,15 @@
-$NetBSD: patch-gdb_bsd-kvm.c,v 1.1 2015/03/15 14:56:46 bsiegert Exp $
+$NetBSD: patch-gdb_bsd-kvm.c,v 1.2 2017/06/05 11:38:51 maya Exp $
 
-(1) (unknown yet)
-(2) +#include <sys/signal.h> part:
-NetBSD/i386 5.2 has following problem without sys/signal.h
-from bsd-kvm.c:36:
-/usr/include/sys/signalvar.h:56: error: field 'sd_sigact' has incomplete type
-/usr/include/sys/signalvar.h:59: error: 'NSIG' undeclared here (not in a function)
-gmake[2]: *** [bsd-kvm.o] Error 1
+need _KMEMUSER to use some headers on netbsd, else vaddr_t
+is undefined.
 
---- gdb/bsd-kvm.c.orig	2015-02-19 11:58:07.000000000 +0000
+--- gdb/bsd-kvm.c.orig	2017-06-04 15:51:26.000000000 +0000
 +++ gdb/bsd-kvm.c
-@@ -28,7 +28,12 @@
- #include "gdbthread.h"
+@@ -17,6 +17,7 @@
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
  
- #include <fcntl.h>
-+#include <stddef.h>
- #include <kvm.h>
 +#define _KMEMUSER 1
-+#include <sys/signal.h>
-+#include <lwp.h>
-+#include <sys/lwp.h>
- #ifdef HAVE_NLIST_H
- #include <nlist.h>
- #endif
-@@ -36,6 +41,7 @@
- #include "readline/readline.h"
- #include <sys/param.h>
- #include <sys/proc.h>
-+#include <sys/types.h>
- #include <sys/user.h>
- 
- #include "bsd-kvm.h"
+ #include "defs.h"
+ #include "cli/cli-cmds.h"
+ #include "command.h"
