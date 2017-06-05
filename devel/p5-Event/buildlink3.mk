97c985aa5c583eb3c90d90ca9807ec2c13ba640f
ryoon <ryoon@pkgsrc.org>
2017-06-05 14:22:16 +0000

    Recursive revbump from lang/perl5 5.26.0

--- devel/p5-Event/buildlink3.mk
+++ devel/p5-Event/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2016/06/08 19:22:13 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2017/06/05 14:22:55 ryoon Exp $
 
 BUILDLINK_TREE+=	p5-Event
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	p5-Event
 P5_EVENT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.p5-Event+=	p5-Event>=1.13
-BUILDLINK_ABI_DEPENDS.p5-Event?=	p5-Event>=1.24nb1
+BUILDLINK_ABI_DEPENDS.p5-Event?=	p5-Event>=1.26nb2
 BUILDLINK_PKGSRCDIR.p5-Event?=	../../devel/p5-Event
 
 .include "../../lang/perl5/buildlink3.mk"
