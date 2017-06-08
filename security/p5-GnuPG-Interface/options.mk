81bffc0ba40db6576ee19a25b85eddeb196c7657
wiz <wiz@pkgsrc.org>
2017-06-08 08:29:30 +0000

    I was too hopeful. This does not really support gnupg2 yet, see https://rt.cpan.org/Public/Bug/Display.html?id=102651
    Switch back to gnupg1. Bump PKGREVISION.

--- security/p5-GnuPG-Interface/options.mk
+++ security/p5-GnuPG-Interface/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.1 2017/06/06 20:40:56 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2017/06/08 08:29:30 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.p5_GnuPG_Interface
 PKG_OPTIONS_REQUIRED_GROUPS=	gnupg
-PKG_OPTIONS_GROUP.gnupg=	gnupg1 gnupg2 gnupg21
-PKG_SUGGESTED_OPTIONS=		gnupg2
+PKG_OPTIONS_GROUP.gnupg=	gnupg1 # gnupg2 gnupg21
+PKG_SUGGESTED_OPTIONS=		gnupg1
 
 .include "../../mk/bsd.options.mk"
 
