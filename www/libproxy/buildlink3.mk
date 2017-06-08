d8b26ffcda288c59049d9101da2441b3e184fb3e
adam <adam@pkgsrc.org>
2017-06-08 11:27:05 +0000

    Version 0.4.15 Port to, and require, SpiderMonkey 38. Fix "NetworkManager plugin not being built" Fix "networkmanager plugin not working Fix "Invalid read after free" Fix intermittent unit test failures.

--- www/libproxy/buildlink3.mk
+++ www/libproxy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2013/05/24 18:22:57 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2017/06/08 11:27:05 adam Exp $
 
 BUILDLINK_TREE+=	libproxy
 
@@ -7,7 +7,7 @@ LIBPROXY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libproxy+=	libproxy>=0.2.3
 BUILDLINK_ABI_DEPENDS.libproxy+=	libproxy>=0.4.11
-BUILDLINK_PKGSRCDIR.libproxy?=	../../www/libproxy
+BUILDLINK_PKGSRCDIR.libproxy?=		../../www/libproxy
 .endif # LIBPROXY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libproxy
