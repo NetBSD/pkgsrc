2abea7a7660f9d231409521e09e365f6966d54aa
taca <taca@pkgsrc.org>
2017-06-08 14:52:59 +0000

    Update php70 to 7.0.20.
    8 Jun 2017 PHP 7.0.20
    
    - Core:
      . Fixed bug #74600 (crash (SIGSEGV) in _zend_hash_add_or_update_i).
        (Laruence)
      . Fixed bug #74546 (SIGILL in ZEND_FETCH_CLASS_CONSTANT_SPEC_CONST_CONST).
        (Laruence)
    
    - intl:
      . Fixed bug #74468 (wrong reflection on Collator::sortWithSortKeys). (villfa)
    
    - MySQLi:
      . Fixed bug #74547 (mysqli::change_user() doesn't accept null as $database
        argument w/strict_types). (Anatol)
    
    - Opcache:
      . Fixed bug #74596 (SIGSEGV with opcache.revalidate_path enabled). (Laruence)
    
    - phar:
      . Fixed bug #51918 (Phar::webPhar() does not handle requests sent through PUT
        and DELETE method). (Christian Weiske)
    
    - Standard:
      . Fixed bug #74510 (win32/sendmail.c anchors CC header but not BCC).
        (Damian Wadley, Anatol)
    
    - xmlreader:
      . Fixed bug #74457 (Wrong reflection on XMLReader::expand). (villfa)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.179 2017/05/12 14:46:52 taca Exp $
+# $NetBSD: phpversion.mk,v 1.180 2017/06/08 14:52:59 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -88,7 +88,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP56_VERSION=	5.6.30
-PHP70_VERSION=	7.0.19
+PHP70_VERSION=	7.0.20
 PHP71_VERSION=	7.1.5
 
 # Define initial release of major version.
