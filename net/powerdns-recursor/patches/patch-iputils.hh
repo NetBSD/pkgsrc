54535e45bb7e27a0c00f96d27e1db015c0927bae
fhajny <fhajny@pkgsrc.org>
2017-06-15 07:15:57 +0000

    Update net/powerdns-recursor to 4.0.5.
    Enhancements
    - Add the 2017 DNSSEC root key
    - Add support for RPZ wildcarded target names.
    - Speed up RPZ zone loading and add a zoneSizeHint parameter to
      rpzFile and rpzMaster for faster reloads
    - Make the RPZ summary consistent and log additions/removals at debug
      level, not info
    - Update Ed25519 algorithm number and mnemonic and hook up to the
      Recursor
    - Add use-incoming-edns-subnet option to process and pass along ECS
      and fix some ECS bugs in the process
    - Refuse to start with chroot set in a systemd env
    - Handle exceptions raised by closesocket() to prevent process
      termination
    - Document missing top-pub-queries and top-pub-servfail-queries
      commands for rec_control
    - IPv6 address for g.root-servers.net added
    - Log outgoing queries / incoming responses via protobuf
    
    Bug fixes
    - Correctly lowercase the TSIG algorithm name in hash computation
    - Clear the RPZ NS IP table when clearing the policy, this prevents
      false positives
    - Fix cache-only queries against a forward-zone
    - Only delegate if NSes are below apex in auth-zones
    - Remove hardcoding of port 53 for TCP/IP forwarded zones in recursor
    - Make sure labelsToAdd is not empty in getZoneCuts()
    - Wait until after daemonizing to start the outgoing protobuf thread,
      prevents hangs when the protobuf server is not available
    - Ensure (re)priming the root never fails
    - Don't age the root, fixes a regression from 3.x
    - Fix exception when sending a protobuf message for an empty question
    - LuaWrapper: Allow embedded NULs in strings received from Lua
    - Fix coredumps on illumos/SmartOS
    - StateHolder: Allocate (and copy if needed) before taking the lock
    - SuffixMatchNode: Fix insertion issue for an existing node
    - Fix negative port detection for IPv6 addresses on 32-bit systems

--- net/powerdns-recursor/patches/patch-iputils.hh
+++ net/powerdns-recursor/patches/patch-iputils.hh
@@ -1,10 +1,8 @@
-$NetBSD: patch-iputils.hh,v 1.3 2017/03/31 19:41:13 fhajny Exp $
+$NetBSD: patch-iputils.hh,v 1.4 2017/06/15 07:15:57 fhajny Exp $
 
 - IP_PKTINFO structure different on NetBSD than expected.
-- Backport fix for SunOS segfault problem, see:
-  https://github.com/PowerDNS/pdns/pull/4877
 
---- iputils.hh.orig	2017-01-13 08:55:55.000000000 +0000
+--- iputils.hh.orig	2017-06-13 09:58:51.000000000 +0000
 +++ iputils.hh
 @@ -38,6 +38,10 @@
  #include <boost/tuple/tuple.hpp>
@@ -17,34 +15,3 @@ $NetBSD: patch-iputils.hh,v 1.3 2017/03/31 19:41:13 fhajny Exp $
  #include "namespaces.hh"
  
  #ifdef __APPLE__
-@@ -59,23 +63,22 @@
- #define le64toh(x) OSSwapLittleToHostInt64(x)
- #endif
- 
--// for illumos
--#ifdef BE_64
-+#ifdef __sun
- 
- #define htobe16(x) BE_16(x)
- #define htole16(x) LE_16(x)
--#define be16toh(x) BE_IN16(x)
--#define le16toh(x) LE_IN16(x)
-+#define be16toh(x) BE_IN16(&(x))
-+#define le16toh(x) LE_IN16(&(x))
- 
- #define htobe32(x) BE_32(x)
- #define htole32(x) LE_32(x)
--#define be32toh(x) BE_IN32(x)
--#define le32toh(x) LE_IN32(x)
-+#define be32toh(x) BE_IN32(&(x))
-+#define le32toh(x) LE_IN32(&(x))
- 
- #define htobe64(x) BE_64(x)
- #define htole64(x) LE_64(x)
--#define be64toh(x) BE_IN64(x)
--#define le64toh(x) LE_IN64(x)
-+#define be64toh(x) BE_IN64(&(x))
-+#define le64toh(x) LE_IN64(&(x))
- 
- #endif
- 
