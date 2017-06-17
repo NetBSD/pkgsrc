4b57ace28c268bd04bfd3d293f7cfe6c92a11516
schmonz <schmonz@pkgsrc.org>
2017-06-17 05:58:39 +0000

    Increase default {ofmip,pop3,smtp}d softlimits by about 20 percent, to 180000000 bytes. From Nathan Arthur in private mail.
    Allow path to tcpserver to be overridden in rc.conf (e.g., by
    sslserver from net/ucspi-ssl). From Thomas Lazar in private mail.
    
    Detach processes and their loggers from the controlling terminal
    with pgrphack(8).
    
    Include qmailqread in the services driven by the LWQ-style qmail
    rc.d script.
    
    Unconditionally depend on mail/mess822, now that it's correctly
    marked public-domain. Remove qmail-run-ofmipd option.
    
    Bump version.

--- mail/qmail-run/files/qmailqread.sh
+++ mail/qmail-run/files/qmailqread.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: qmailqread.sh,v 1.10 2017/04/10 15:04:56 schmonz Exp $
+# $NetBSD: qmailqread.sh,v 1.11 2017/06/17 05:58:39 schmonz Exp $
 #
 # @PKGNAME@ script to control a service providing local non-root
 # users access to see the queue. Adapted from a script by Steinar Haug.
@@ -16,6 +16,7 @@ name="qmailqread"
 : ${qmailqread_tcpflags:="-R1"}
 : ${qmailqread_tcphost:="127.0.0.1"}
 : ${qmailqread_tcpport:="20025"}
+: ${qmailqread_tcpserver:="@PREFIX@/bin/tcpserver"}
 : ${qmailqread_log:="YES"}
 : ${qmailqread_logcmd:="logger -t nb${name} -p mail.info"}
 : ${qmailqread_nologcmd:="@PREFIX@/bin/multilog -*"}
@@ -25,7 +26,7 @@ if [ -f /etc/rc.subr ]; then
 fi
 
 rcvar=${name}
-command="@PREFIX@/bin/tcpserver"
+command="${qmailqread_tcpserver}"
 procname=${name}
 start_precmd="qmailqread_precmd"
 
@@ -34,14 +35,17 @@ qmailqread_precmd()
 	if [ -f /etc/rc.subr ] && ! checkyesno qmailqread_log; then
 		qmailqread_logcmd=${qmailqread_nologcmd}
 	fi
-	command="@SETENV@ - ${qmailqread_postenv}
-@PREFIX@/bin/argv0 @PREFIX@/bin/tcpserver ${name}
+	# tcpserver(1) is akin to inetd(8), but runs one service per process.
+	# We want to signal only the tcpserver process responsible for this
+	# service. Use argv0(1) to set procname to "qmailqread".
+	command="@PREFIX@/bin/pgrphack @SETENV@ - ${qmailqread_postenv}
+@PREFIX@/bin/argv0 ${qmailqread_tcpserver} ${name}
 ${qmailqread_tcpflags}
 -u `@ID@ -u @QMAIL_SEND_USER@` -g `@ID@ -g @QMAIL_SEND_USER@`
 ${qmailqread_tcphost} ${qmailqread_tcpport}
 @PREFIX@/bin/qmail-qread
 2>&1 |
-@PREFIX@/bin/setuidgid @QMAIL_LOG_USER@ ${qmailqread_logcmd}"
+@PREFIX@/bin/pgrphack @PREFIX@/bin/setuidgid @QMAIL_LOG_USER@ ${qmailqread_logcmd}"
 	command_args="&"
 	rc_flags=""
 }
