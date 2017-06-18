72794dd28733adce915cb2d659a56b35f07f5b7c
youri <youri@pkgsrc.org>
2017-06-18 22:22:55 +0000

    Update xfce4-conf to 4.13.1:
    4.12.1
    ======
    - Add support for binding to GdkRGBAs (required for migration to gtk3)
    - Translation updates: ast, ca, el, eu, gl, he, hu, lt, nn, oc, ru, sk, sv
    
    Further changelogs not available.

--- devel/xfce4-conf/buildlink3.mk
+++ devel/xfce4-conf/buildlink3.mk
@@ -1,18 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.5 2017/02/12 06:24:42 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2017/06/18 22:22:55 youri Exp $
 
 BUILDLINK_TREE+=	xfce4-conf
 
 .if !defined(XFCE4_CONF_BUILDLINK3_MK)
 XFCE4_CONF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-conf+=	xfce4-conf>=4.12.0
-BUILDLINK_ABI_DEPENDS.xfce4-conf?=	xfce4-conf>=4.12.0nb6
+BUILDLINK_API_DEPENDS.xfce4-conf+=	xfce4-conf>=4.13.1
+BUILDLINK_ABI_DEPENDS.xfce4-conf?=	xfce4-conf>=4.13.1
 BUILDLINK_PKGSRCDIR.xfce4-conf?=	../../devel/xfce4-conf
 
 .include "../../sysutils/dbus/buildlink3.mk"
 .include "../../sysutils/dbus-glib/buildlink3.mk"
 .include "../../x11/libxfce4util/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
 .endif	# XFCE4_CONF_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xfce4-conf
