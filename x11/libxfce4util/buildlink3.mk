932e6ef026d00838bfb0aa7b68da9c3df107e918
youri <youri@pkgsrc.org>
2017-06-18 21:22:08 +0000

    Update libxfce4util to 4.13.0:
    4.13.0
    ======
    [Please note that this is a development release.]
    - Fix abicheck to handle ppc64 architecture
    - Add support for GOBJECT-INTROSPECTION Bug #12159
    - Translation updates: ast bg ca da gl he hr lt sv

--- x11/libxfce4util/buildlink3.mk
+++ x11/libxfce4util/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.34 2017/02/12 06:24:56 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.35 2017/06/18 21:22:08 youri Exp $
 
 BUILDLINK_TREE+=	libxfce4util
 
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.libxfce4util+=	libxfce4util>=4.12.0
 BUILDLINK_ABI_DEPENDS.libxfce4util+=	libxfce4util>=4.12.1nb4
 BUILDLINK_PKGSRCDIR.libxfce4util?=	../../x11/libxfce4util
 
-.include "../../x11/gtk2/buildlink3.mk"
 .endif # LIBXFCE4UTIL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libxfce4util
