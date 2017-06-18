5dc2d0f627bf5fbb4c192dae54d99fa0b84eb8a1
youri <youri@pkgsrc.org>
2017-06-18 20:39:29 +0000

    Update xfce4-exo all the way to 0.11.3:
    0.11.0 (Development Release)
    ======
    - This is the first release with initial GTK+3 support. This initial
      port is thanks to Nick Schermer and Jonas K?mmerlin, who provided
      all necessary components. As this is an initial release, expect to
      find bugs or even wholly incomplete functionality.
    - Translation updates: Asturian, Basque, Bulgarian, Catalan, French,
      German, Greek, Hebrew, Hungarian, Icelandic, Japanese, Korean,
      Lithuanian, Occitan, Panjabi, Portuguese (Brazil), Russian, Slovak,
      Slovenian, Spanish, Swedish, Turkish, Ukrainian
    
    0.11.1 (Development Release)
    ======
    - This release addresses a new bug introduced in 0.11.0 where exo
      did not correctly export the user's environment variables.
    - Translation updates: Chinese (China), Croatian, Swedish
    
    0.11.2 (Development Release)
    ======
    - Several improvements from f2404:
      - Remove unused struct member
      - Check index range before accessing array
      - Do not assign value twice
      - Resolve gdk_cursor_* deprecated warnings
      - Resolve gdk_threads_* deprecated warnings
    - Fixed vertical alignment of options in preferred applications
    - Fixed issue with Thunar detailed view losing selection (Xfce #12916)
    - Translation updates: Asturian, English (Australia), Italian, Kazakh,
      Lithuanian, Malay, Polish, Portuguese, Romanian, Russian, Thai
    
    0.11.3 (Development Release)
    ======
    - New Features:
      - exo-csource: Add --output flag to write output to a file (Xfce #12901)
      - exo-helper: Add --query flag to query the preferred application (Xfce #8579)
    - Build Changes:
      - Requirements updated: GTK 2.24, GTK 3.20, GLib 2.42, libxfce4ui 4.12
      - GTK 3 libraries are no longer optional
      - Default debug for development builds is now "yes" instead of "full"
    - Bug Fixes:
      - Discard preferred application selection if dialog is canceled (Xfce #8802)
      - Do not ship generic category icons, these are standard (Xfce #9992)
      - Do not abort builds due to deprecated declarations (Xfce #11556)
      - Fix crash in Thunar on selection change after directory change (Xfce #13238)
      - Fix crash in exo-helper-1 from GTK 3 migration (Xfce #13374)
      - Fix ExoIconView being unable to decrease its size (Xfce #13402)
    - Documentation Updates:
      - Add missing per-release API indices
      - Resolve undocumented symbols
      - Updated project documentation (HACKING, README, THANKS)
    - Translation Updates: Amharic, Asturian, Catalan, Chinese (Taiwan), Croatian,
      Danish, Dutch, Finnish, Galician, Greek, Indonesian, Kazakh, Korean,
      Lithuanian, Norwegian Bokmal, Norwegian Nynorsk, Occitan,
      Portuguese (Brazil), Russian, Serbian, Slovenian, Spanish, Thai

--- x11/xfce4-exo/buildlink3.mk
+++ x11/xfce4-exo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.31 2017/02/12 06:25:00 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2017/06/18 20:39:29 youri Exp $
 
 BUILDLINK_TREE+=	xfce4-exo
 
@@ -7,7 +7,7 @@ XFCE4_EXO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xfce4-exo+=	xfce4-exo>=0.10.4
 BUILDLINK_ABI_DEPENDS.xfce4-exo?=	xfce4-exo>=0.10.7nb4
-BUILDLINK_PKGSRCDIR.xfce4-exo?=	../../x11/xfce4-exo
+BUILDLINK_PKGSRCDIR.xfce4-exo?=		../../x11/xfce4-exo
 
 .include "../../x11/libxfce4ui/buildlink3.mk"
 .endif	# XFCE4_EXO_BUILDLINK3_MK
