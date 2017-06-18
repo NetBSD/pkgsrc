4ad26fa634afef04061273076969bfcd321eaa19
youri <youri@pkgsrc.org>
2017-06-18 22:48:17 +0000

    Update xfce4-desktop to 4.13.1:
    4.13.1
    ======
    [Please note that this is a development release.]
    
    Development release of xfdesktop targeting for Xfce 4.14.
    Please report all problems at bugzilla.xfce.org.
    
    * Miscellaneous bug fixes:
    - Fix a memory leak reported in Bug #13555
    - Fix garcon-2 dependancy check (continue to use garcon-1)
    
    * Translation Updates:
     ca da de es fr he hr id it ja kk ko ms pl
     pt pt_BR ru th uk zh_CN
    
    4.13.0
    ======
    [Please note that this is a development release.]
    
    Development release of xfdesktop targeting for Xfce 4.14.
    Please report all problems at bugzilla.xfce.org.
    
    * Major changes:
     - Port to Gtk3.
      - The iconview has a CSS name of "XfdesktopIconView" and
        adds the GTK_STYLE_CLASS_VIEW to it's base. It then adds
        GTK_STYLE_CLASS_LABEL when rendering the icon text with
        the states: selected, active, or normal. Additionally it
        uses the GTK_STYLE_CLASS_RUBBERBAND when for the rubberband
        theming.
     - Port to GDBus.
    
    * Miscellaneous bug fixes:
     - Don't redraw the icons on focus change events (Bug #12071)
     - Don't try to divide by 0 (Bug #12115)
     - Settings: Fix loading of wallpaper previews (Bug #11892)
     - Settings: Allow directories to be selected (Bug #12832)
     - Fix Open dialog's title (Bug #12560)
     - Prevent a crash in the backdrop loader (Bug #12339)
     - Use gdk's get static image for animation files
     - Add translation string for Trash desktop icon
     - Retain icon relative position when moving a group

--- x11/xfce4-desktop/patches/patch-common_xfdesktop-common.h
+++ x11/xfce4-desktop/patches/patch-common_xfdesktop-common.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-common_xfdesktop-common.h,v 1.1 2016/06/14 13:09:47 youri Exp $
+$NetBSD: patch-common_xfdesktop-common.h,v 1.2 2017/06/18 22:48:17 youri Exp $
 
 Use the blue background by default.
 
---- common/xfdesktop-common.h.orig	2015-07-19 13:26:58.000000000 +0000
+--- common/xfdesktop-common.h.orig	2017-04-20 15:38:05.000000000 +0000
 +++ common/xfdesktop-common.h
 @@ -35,7 +35,7 @@
  #include <stdarg.h>
@@ -12,4 +12,4 @@ Use the blue background by default.
 +#define DEFAULT_BACKDROP         DATADIR "/backgrounds/xfce/xfce-blue.jpg"
  #define DEFAULT_ICON_FONT_SIZE   12
  #define DEFAULT_ICON_SIZE        48
- #define ITHEME_FLAGS             (GTK_ICON_LOOKUP_USE_BUILTIN \
+ #define ITHEME_FLAGS             (GTK_ICON_LOOKUP_USE_BUILTIN)
