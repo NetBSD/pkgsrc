93175a144cc415c4b0b6d1d00016435245a57bbf
maya <maya@pkgsrc.org>
2017-06-19 13:21:35 +0000

    Apply patch that reportedly fixes the build on DragonflyBSD
    From Aleksej Lebedev (From dragonfly dports) in pkgsrc-users

---
+++ net/transmission/patches/patch-libtransmission_platform-quota.c
@@ -0,0 +1,33 @@
+$NetBSD$
+
+Fix dragonflybsd build
+
+--- libtransmission/platform-quota.c.orig	2017-06-19 12:56:41.129003307 +0000
++++ libtransmission/platform-quota.c
+@@ -18,6 +18,8 @@
+  #include <sys/types.h> /* types needed by quota.h */
+  #if defined(__FreeBSD__) || defined(__OpenBSD__)
+   #include <ufs/ufs/quota.h> /* quotactl() */
++ #elif defined (__DragonFly__)
++  #include <vfs/ufs/quota.h> /* quotactl */
+  #elif defined (__NetBSD__)
+   #include <sys/param.h>
+   #ifndef statfs
+@@ -244,12 +246,16 @@ getquota (const char * device)
+ static int64_t
+ getquota (const char * device)
+ {
++#ifdef __DragonFly__
++  struct ufs_dqblk dq;
++#else
+   struct dqblk dq;
++#endif
+   int64_t limit;
+   int64_t freespace;
+   int64_t spaceused;
+ 
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
++#if defined(__FreeBSD__) || defined(__DragonFly__) || defined(__OpenBSD__) || defined(__APPLE__)
+   if (quotactl(device, QCMD(Q_GETQUOTA, USRQUOTA), getuid(), (caddr_t) &dq) == 0)
+     {
+ #elif defined(__sun)
