152ac95c1d336364640d445f78103136c3bedec9
youri <youri@pkgsrc.org>
2017-06-20 16:11:05 +0000

    Downgrade xfce4-conf to 4.12.1 to make the panel working again.

--- devel/xfce4-conf/buildlink3.mk
+++ devel/xfce4-conf/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2017/06/18 22:22:55 youri Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2017/06/20 16:11:05 youri Exp $
 
 BUILDLINK_TREE+=	xfce4-conf
 
 .if !defined(XFCE4_CONF_BUILDLINK3_MK)
 XFCE4_CONF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-conf+=	xfce4-conf>=4.13.1
-BUILDLINK_ABI_DEPENDS.xfce4-conf?=	xfce4-conf>=4.13.1
+BUILDLINK_API_DEPENDS.xfce4-conf+=	xfce4-conf>=4.12.1
+BUILDLINK_ABI_DEPENDS.xfce4-conf?=	xfce4-conf>=4.12.1
 BUILDLINK_PKGSRCDIR.xfce4-conf?=	../../devel/xfce4-conf
 
 .include "../../sysutils/dbus/buildlink3.mk"
