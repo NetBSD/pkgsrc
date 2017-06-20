6c762f8564eb3181b8f17800988c7f1daae2012b
adam <adam@pkgsrc.org>
2017-06-20 17:00:07 +0000

    Pygame 1.9.3: This is a bugfix release which fixes a couple of key issues which came up with 1.9.2. It also adds wheels for Python 3.6 on Linux, Mac and Windows.

--- devel/py-game/buildlink3.mk
+++ devel/py-game/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.28 2015/04/25 14:20:39 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.29 2017/06/20 17:00:07 adam Exp $
 
-BUILDLINK_TREE+=	${PYPKGPREFIX}-game
+BUILDLINK_TREE+=	py-game
 
 .if !defined(PY_GAME_BUILDLINK3_MK)
 PY_GAME_BUILDLINK3_MK:=
 
 .include "../../lang/python/pyversion.mk"
 
-BUILDLINK_API_DEPENDS.${PYPKGPREFIX}-game+=	${PYPKGPREFIX}-game>=1.9.1
-BUILDLINK_ABI_DEPENDS.${PYPKGPREFIX}-game+=	${PYPKGPREFIX}-game>=1.9.1nb2
-BUILDLINK_PKGSRCDIR.${PYPKGPREFIX}-game?=	../../devel/py-game
+BUILDLINK_API_DEPENDS.py-game+=		py-game>=1.9.1
+BUILDLINK_ABI_DEPENDS.py-game+=		py-game>=1.9.1nb2
+BUILDLINK_PKGSRCDIR.py-game?=		../../devel/py-game
 
 .include "../../audio/SDL_mixer/buildlink3.mk"
 .include "../../devel/SDL/buildlink3.mk"
@@ -17,7 +17,9 @@ BUILDLINK_PKGSRCDIR.${PYPKGPREFIX}-game?=	../../devel/py-game
 .include "../../graphics/SDL_image/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .include "../../multimedia/smpeg/buildlink3.mk"
-.include "../../x11/libX11/buildlink3.mk"
+.if ${OPSYS} != "Darwin"
+.  include "../../x11/libX11/buildlink3.mk"
+.endif
 .endif # PY_GAME_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-${PYPKGPREFIX}-game
+BUILDLINK_TREE+=	-py-game
