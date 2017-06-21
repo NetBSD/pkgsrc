266ac11e92804e1747a59f51e9c47bc33c383d85
tez <tez@pkgsrc.org>
2017-06-21 00:23:23 +0000

    xmlSnprintfElementContent failed to correctly check the available buffer space in two locations. Fixes bug 781333 (CVE-2017-9047) and bug 781701 (CVE-2017-9048). From: https://git.gnome.org/browse/libxml2/commit/?id=932cc9896ab41475d4aa429c27d9afd175959d74
    There were two bugs where parameter-entity references could lead to an
    unexpected change of the input buffer in xmlParseNameComplex and
    xmlDictLookup being called with an invalid pointer.
    
    Percent sign in DTD Names
    =========================
    This fixes bug 766956 initially reported by Wei Lei and independently by
    Chromium's ClusterFuzz, Hanno Böck, and Marco Grassi. Thanks to everyone
    involved.
    
    xmlParseNameComplex with XML_PARSE_OLD10
    ========================================
    This fixes bugs 781205 (CVE-2017-9049) and 781361 (CVE-2017-9050).
    Thanks to Marcel Böhme and Thuan Pham for the report.
    
    Additional hardening
    ====================
    A separate check was added in xmlParseNameComplex to validate the
    buffer size.
    
    From: https://git.gnome.org/browse/libxml2/commit/?id=e26630548e7d138d2c560844c43820b6767251e3

--- textproc/libxml2/patches/patch-valid.c
+++ textproc/libxml2/patches/patch-valid.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-valid.c,v 1.1 2017/06/11 04:40:53 maya Exp $
+$NetBSD: patch-valid.c,v 1.2 2017/06/21 00:23:24 tez Exp $
 
 Upstream commit by Daniel Veillard
 
@@ -7,9 +7,15 @@ Can only be triggered in recovery mode.
 Fixes bug 758422 (CVE-2017-5969).
 
 
---- valid.c.orig	2016-05-23 07:25:25.000000000 +0000
+xmlSnprintfElementContent failed to correctly check the available
+buffer space in two locations.
+Fixes bug 781333 (CVE-2017-9047) and bug 781701 (CVE-2017-9048).
+From: https://git.gnome.org/browse/libxml2/commit/?id=932cc9896ab41475d4aa429c27d9afd175959d74
+
+
+--- valid.c.orig	2017-06-21 00:07:08.204619100 +0000
 +++ valid.c
-@@ -1172,29 +1172,33 @@ xmlDumpElementContent(xmlBufferPtr buf, 
+@@ -1172,29 +1172,33 @@ xmlDumpElementContent(xmlBufferPtr buf,
  	    xmlBufferWriteCHAR(buf, content->name);
  	    break;
  	case XML_ELEMENT_CONTENT_SEQ:
@@ -53,3 +59,44 @@ Fixes bug 758422 (CVE-2017-5969).
  		xmlDumpElementContent(buf, content->c2, 1);
  	    else
  		xmlDumpElementContent(buf, content->c2, 0);
+@@ -1262,22 +1266,23 @@ xmlSnprintfElementContent(char *buf, int
+         case XML_ELEMENT_CONTENT_PCDATA:
+             strcat(buf, "#PCDATA");
+ 	    break;
+-	case XML_ELEMENT_CONTENT_ELEMENT:
++	case XML_ELEMENT_CONTENT_ELEMENT: {
++            int qnameLen = xmlStrlen(content->name);
++
++	    if (content->prefix != NULL)
++                qnameLen += xmlStrlen(content->prefix) + 1;
++	    if (size - len < qnameLen + 10) {
++		strcat(buf, " ...");
++		return;
++	    }
+ 	    if (content->prefix != NULL) {
+-		if (size - len < xmlStrlen(content->prefix) + 10) {
+-		    strcat(buf, " ...");
+-		    return;
+-		}
+ 		strcat(buf, (char *) content->prefix);
+ 		strcat(buf, ":");
+ 	    }
+-	    if (size - len < xmlStrlen(content->name) + 10) {
+-		strcat(buf, " ...");
+-		return;
+-	    }
+ 	    if (content->name != NULL)
+ 		strcat(buf, (char *) content->name);
+ 	    break;
++        }
+ 	case XML_ELEMENT_CONTENT_SEQ:
+ 	    if ((content->c1->type == XML_ELEMENT_CONTENT_OR) ||
+ 	        (content->c1->type == XML_ELEMENT_CONTENT_SEQ))
+@@ -1319,6 +1324,7 @@ xmlSnprintfElementContent(char *buf, int
+ 		xmlSnprintfElementContent(buf, size, content->c2, 0);
+ 	    break;
+     }
++    if (size - strlen(buf) <= 2) return;
+     if (englob)
+         strcat(buf, ")");
+     switch (content->ocur) {
