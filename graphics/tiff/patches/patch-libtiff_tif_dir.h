04d48c59368df05d928c8884d4349f9bbd703410
pgoyette <pgoyette@pkgsrc.org>
2017-06-21 02:38:21 +0000

    Fixup the patches and regenerate distinfo to unbreak the build

--- graphics/tiff/patches/patch-libtiff_tif_dir.h
+++ graphics/tiff/patches/patch-libtiff_tif_dir.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-libtiff_tif_dir.h,v 1.1 2017/06/21 01:08:33 tez Exp $
+$NetBSD: patch-libtiff_tif_dir.h,v 1.2 2017/06/21 02:38:21 pgoyette Exp $
 
 fix CVE-2014-8128, CVE-2016-5318, CVE-2015-7554 & CVE-2016-10095
 per http://bugzilla.maptools.org/show_bug.cgi?id=2580
@@ -16,8 +16,8 @@ diff -w -u -b -r1.54 -r1.55
 --- libtiff/tif_dir.h.orig	18 Feb 2011 20:53:05 -0000	1.54
 +++ libtiff/tif_dir.h	1 Jun 2017 12:44:04 -0000	1.55
 @@ -1,4 +1,4 @@
--/* $Id: patch-libtiff_tif_dir.h,v 1.1 2017/06/21 01:08:33 tez Exp $ */
-+/* $Id: patch-libtiff_tif_dir.h,v 1.1 2017/06/21 01:08:33 tez Exp $ */
+-/* $Id: patch-libtiff_tif_dir.h,v 1.2 2017/06/21 02:38:21 pgoyette Exp $ */
++/* $Id: patch-libtiff_tif_dir.h,v 1.2 2017/06/21 02:38:21 pgoyette Exp $ */
  
  /*
   * Copyright (c) 1988-1997 Sam Leffler
