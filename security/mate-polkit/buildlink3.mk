1f92d24094415a3aa9b6eb706bdcd7de07380174
youri <youri@pkgsrc.org>
2017-06-23 17:25:42 +0000

    Update mate-polkit to 1.18.1.

--- security/mate-polkit/buildlink3.mk
+++ security/mate-polkit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2017/02/12 06:24:49 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/06/23 17:25:42 youri Exp $
 #
 
 BUILDLINK_TREE+=	mate-polkit
@@ -7,11 +7,10 @@ BUILDLINK_TREE+=	mate-polkit
 MATE_POLKIT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mate-polkit+=	mate-polkit>=1.8.0
-BUILDLINK_ABI_DEPENDS.mate-polkit?=	mate-polkit>=1.14.0nb4
+BUILDLINK_ABI_DEPENDS.mate-polkit+=	mate-polkit>=1.14.0nb4
 BUILDLINK_PKGSRCDIR.mate-polkit?=	../../security/mate-polkit
 
 .include "../../security/polkit/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
 .endif	# MATE_POLKIT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mate-polkit
