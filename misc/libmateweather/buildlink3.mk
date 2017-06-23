25a2852443bece2e19e4585d0d9efbc50c406481
youri <youri@pkgsrc.org>
2017-06-23 17:12:57 +0000

    Update libmateweather to 1.18.1.

--- misc/libmateweather/buildlink3.mk
+++ misc/libmateweather/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2017/04/22 21:03:13 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2017/06/23 17:12:57 youri Exp $
 #
 
 BUILDLINK_TREE+=	libmateweather
@@ -7,13 +7,11 @@ BUILDLINK_TREE+=	libmateweather
 LIBMATEWEATHER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libmateweather+=	libmateweather>=1.8.0
-BUILDLINK_ABI_DEPENDS.libmateweather?=	libmateweather>=1.14.0nb5
+BUILDLINK_ABI_DEPENDS.libmateweather+=	libmateweather>=1.14.0nb5
 BUILDLINK_PKGSRCDIR.libmateweather?=	../../misc/libmateweather
 
-.include "../../devel/glib2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../net/libsoup/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
 .endif	# LIBMATEWEATHER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libmateweather
