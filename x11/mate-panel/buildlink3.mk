9c1a530e7c289de3bb8c61672d5dc001748ebfb0
youri <youri@pkgsrc.org>
2017-06-23 17:24:57 +0000

    Update mate-panel to 1.18.3.

--- x11/mate-panel/buildlink3.mk
+++ x11/mate-panel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2017/02/12 06:24:56 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/06/23 17:24:57 youri Exp $
 #
 
 BUILDLINK_TREE+=	mate-panel
@@ -7,17 +7,9 @@ BUILDLINK_TREE+=	mate-panel
 MATE_PANEL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mate-panel+=	mate-panel>=1.8.1
-BUILDLINK_ABI_DEPENDS.mate-panel?=	mate-panel>=1.14.1nb5
+BUILDLINK_ABI_DEPENDS.mate-panel+=	mate-panel>=1.14.1nb5
 BUILDLINK_PKGSRCDIR.mate-panel?=	../../x11/mate-panel
 
-.include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/pango/buildlink3.mk"
-.include "../../graphics/cairo/buildlink3.mk"
-.include "../../graphics/gdk-pixbuf2/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
-.include "../../x11/libXrandr/buildlink3.mk"
-.include "../../x11/libX11/buildlink3.mk"
-.include "../../x11/libXau/buildlink3.mk"
 .endif	# MATE_PANEL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mate-panel
