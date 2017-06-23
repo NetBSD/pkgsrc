dfaba095310c439dc9d7ca2791e15a0703eff7c7
youri <youri@pkgsrc.org>
2017-06-23 17:06:33 +0000

    Update caja to 1.18.3.

--- sysutils/caja/buildlink3.mk
+++ sysutils/caja/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2017/02/12 06:24:49 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/06/23 17:06:33 youri Exp $
 #
 
 BUILDLINK_TREE+=	caja
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	caja
 CAJA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.caja+=	caja>=1.8.2
-BUILDLINK_ABI_DEPENDS.caja?=	caja>=1.14.1nb3
+BUILDLINK_ABI_DEPENDS.caja+=	caja>=1.14.1nb3
 BUILDLINK_PKGSRCDIR.caja?=	../../sysutils/caja
 
 .include "../../x11/mate-desktop/buildlink3.mk"
