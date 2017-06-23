eb969ae98133a068a5ba3d8a2fae3c70365a3ede
youri <youri@pkgsrc.org>
2017-06-23 17:17:09 +0000

    mate-control-center 1.18.2.

--- x11/mate-control-center/buildlink3.mk
+++ x11/mate-control-center/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2017/02/12 06:24:56 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/06/23 17:17:09 youri Exp $
 #
 
 BUILDLINK_TREE+=	mate-control-center
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	mate-control-center
 MATE_CONTROL_CENTER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mate-control-center+=	mate-control-center>=1.8.3
-BUILDLINK_ABI_DEPENDS.mate-control-center?=	mate-control-center>=1.14.0nb3
+BUILDLINK_ABI_DEPENDS.mate-control-center+=	mate-control-center>=1.14.0nb3
 BUILDLINK_PKGSRCDIR.mate-control-center?=	../../x11/mate-control-center
 
 .include "../../x11/mate-desktop/buildlink3.mk"
