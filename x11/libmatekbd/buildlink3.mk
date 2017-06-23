a0f794d8a850bb396cae928d28da04a253579f24
youri <youri@pkgsrc.org>
2017-06-23 17:11:59 +0000

    Update libmatekbd to 1.18.2

--- x11/libmatekbd/buildlink3.mk
+++ x11/libmatekbd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2017/02/12 06:24:55 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/06/23 17:11:59 youri Exp $
 #
 
 BUILDLINK_TREE+=	libmatekbd
@@ -7,13 +7,10 @@ BUILDLINK_TREE+=	libmatekbd
 LIBMATEKBD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libmatekbd+=	libmatekbd>=1.8.0
-BUILDLINK_ABI_DEPENDS.libmatekbd?=	libmatekbd>=1.14.1nb3
+BUILDLINK_ABI_DEPENDS.libmatekbd+=	libmatekbd>=1.14.1nb3
 BUILDLINK_PKGSRCDIR.libmatekbd?=	../../x11/libmatekbd
 
-.include "../../devel/glib2/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
 .include "../../x11/libxklavier/buildlink3.mk"
-.include "../../x11/libX11/buildlink3.mk"
 .endif	# LIBMATEKBD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libmatekbd
