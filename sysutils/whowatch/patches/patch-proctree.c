a7308da9898c3661f9cbcd4bdae6c4a5d36e1eb5
kamil <kamil@pkgsrc.org>
2017-06-23 18:57:13 +0000

    Correct build failure of whowatch on NetBSD 8.99.1
    Address <sys/user.h> removal fallout.

--- sysutils/whowatch/patches/patch-proctree.c
+++ sysutils/whowatch/patches/patch-proctree.c
@@ -1,11 +1,24 @@
-$NetBSD: patch-proctree.c,v 1.1 2012/08/08 18:37:07 marino Exp $
+$NetBSD: patch-proctree.c,v 1.2 2017/06/23 18:57:13 kamil Exp $
 
 Add DragonFly support (kernel proc can return pid of -1, skip)
 kinfo structure is unique on DragonFly
 
+<sys/user.h> is freebsdism in this usage-context, it's now absent in NetBSD 8.99.1
+
 --- proctree.c.orig	2000-06-06 09:03:28.000000000 +0000
 +++ proctree.c
-@@ -170,8 +170,14 @@ int update_tree(void del(void*))
+@@ -16,8 +16,10 @@
+ #ifdef HAVE_PROCESS_SYSCTL
+ #include <sys/param.h>
+ #include <sys/sysctl.h>
++#if defined(__FreeBSD__) || defined(__DragonFly__)
+ #include <sys/user.h>
+ #endif
++#endif
+ 
+ #include "proctree.h"
+ 
+@@ -170,8 +172,14 @@ int update_tree(void del(void*))
  #ifdef HAVE_PROCESS_SYSCTL
  	el = get_all_info(&pi);
  	for(i = 0; i < el; i++) {
