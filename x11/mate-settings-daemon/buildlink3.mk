ec70414bd0b564205a8e3a8268628889fcf323d4
youri <youri@pkgsrc.org>
2017-06-23 17:29:04 +0000

    Update mate-settings-daemon to 1.18.1.

--- x11/mate-settings-daemon/buildlink3.mk
+++ x11/mate-settings-daemon/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2017/02/12 06:24:56 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/06/23 17:29:04 youri Exp $
 #
 
 BUILDLINK_TREE+=	mate-settings-daemon
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	mate-settings-daemon
 MATE_SETTINGS_DAEMON_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mate-settings-daemon+=	mate-settings-daemon>=1.8.1
-BUILDLINK_ABI_DEPENDS.mate-settings-daemon?=	mate-settings-daemon>=1.14.0nb5
+BUILDLINK_ABI_DEPENDS.mate-settings-daemon+=	mate-settings-daemon>=1.14.0nb5
 BUILDLINK_PKGSRCDIR.mate-settings-daemon?=	../../x11/mate-settings-daemon
 
 .include "../../x11/libmatekbd/buildlink3.mk"
