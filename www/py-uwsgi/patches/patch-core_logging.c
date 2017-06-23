3f5352ed7a948d6656d06f21dd72c0a7822cc4f1
joerg <joerg@pkgsrc.org>
2017-06-23 15:56:45 +0000

    Avoid sys/user.h on NetBSD.

--- www/py-uwsgi/patches/patch-core_logging.c
+++ www/py-uwsgi/patches/patch-core_logging.c
@@ -1,15 +1,18 @@
-$NetBSD: patch-core_logging.c,v 1.2 2016/11/04 21:39:00 maya Exp $
+$NetBSD: patch-core_logging.c,v 1.3 2017/06/23 15:56:45 joerg Exp $
 
 Fix build for Bitrig.
 
---- core/logging.c.orig	2016-10-03 08:16:59.000000000 +0000
+--- core/logging.c.orig	2017-03-30 22:11:36.000000000 +0000
 +++ core/logging.c
-@@ -1,7 +1,7 @@
+@@ -1,8 +1,10 @@
  #ifndef __DragonFly__
  #include <uwsgi.h>
  #endif
 -#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__) || defined(__OpenBSD__)
 +#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__) || defined(__OpenBSD__) || defined(__Bitrig__)
++#  if !defined(__NetBSD__)
  #include <sys/user.h>
++#  endif
  #include <sys/sysctl.h>
  #include <kvm.h>
+ #elif defined(__sun__)
