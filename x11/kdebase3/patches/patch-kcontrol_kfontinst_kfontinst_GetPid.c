343c52a6cb54529bb0f39452fe66a70a9300e5f5
kamil <kamil@pkgsrc.org>
2017-06-24 02:00:53 +0000

    Correct build of kde3 base on NetBSD 8.99.1
    Address <sys/user.h> removal fallout.

--- x11/kdebase3/patches/patch-kcontrol_kfontinst_kfontinst_GetPid.c
+++ x11/kdebase3/patches/patch-kcontrol_kfontinst_kfontinst_GetPid.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-kcontrol_kfontinst_kfontinst_GetPid.c,v 1.1 2013/01/15 15:35:25 joerg Exp $
+$NetBSD: patch-kcontrol_kfontinst_kfontinst_GetPid.c,v 1.2 2017/06/24 02:00:53 kamil Exp $
 
---- kcontrol/kfontinst/kfontinst/GetPid.c.orig	2013-01-14 15:01:36.000000000 +0000
+--- kcontrol/kfontinst/kfontinst/GetPid.c.orig	2007-05-14 07:55:38.000000000 +0000
 +++ kcontrol/kfontinst/kfontinst/GetPid.c
 @@ -28,6 +28,10 @@
  ////////////////////////////////////////////////////////////////////////////////
@@ -13,3 +13,13 @@ $NetBSD: patch-kcontrol_kfontinst_kfontinst_GetPid.c,v 1.1 2013/01/15 15:35:25 j
  #include <stdio.h>
  #include <stdlib.h>
  #include <string.h>
+@@ -172,7 +176,9 @@ unsigned int kfi_getPid(const char *proc
+ #endif
+ #include <sys/sysctl.h>
+ #include <sys/time.h>
++#if defined(__FreeBSD__) || defined(__DragonFly__)
+ #include <sys/user.h>
++#endif
+ #include <unistd.h>
+ unsigned int kfi_getPid(const char *proc, unsigned int ppid)
+ {
