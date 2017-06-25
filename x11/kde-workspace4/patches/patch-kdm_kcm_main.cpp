aa0066cc52faf0fd90b81098e0cde379b0df4aae
youri <youri@pkgsrc.org>
2017-06-25 19:58:04 +0000

    Fix NetBSD current build.

--- x11/kde-workspace4/patches/patch-kdm_kcm_main.cpp
+++ x11/kde-workspace4/patches/patch-kdm_kcm_main.cpp
@@ -1,14 +1,14 @@
-$NetBSD: patch-kdm_kcm_main.cpp,v 1.1 2012/03/19 19:44:03 markd Exp $
+$NetBSD: patch-kdm_kcm_main.cpp,v 1.2 2017/06/25 19:58:04 youri Exp $
 
 Allow $PKG_SYSCONFDIR/kdm/kdmrc to override the one under $PREFIX
 
---- kdm/kcm/main.cpp.orig	2010-05-27 17:23:09.000000000 +0000
+--- kdm/kcm/main.cpp.orig	2015-08-12 07:03:15.000000000 +0000
 +++ kdm/kcm/main.cpp
 @@ -281,8 +281,12 @@ KConfig *KDModule::createTempConfig()
      pTempConfigFile->open();
      QString tempConfigName = pTempConfigFile->fileName();
  
-+    QFile confFile (QString::fromLatin1( "@PKG_SYSCONFDIR@" "/kdm/kdmrc" ));
++    QFile confFile (QString::fromLatin1( "/usr/pkg/etc" "/kdm/kdmrc" ));
 +    if ( !confFile.exists() )
 +	confFile.setFileName (QString::fromLatin1( KDE_CONFDIR "/kdm/kdmrc" ));
 +
