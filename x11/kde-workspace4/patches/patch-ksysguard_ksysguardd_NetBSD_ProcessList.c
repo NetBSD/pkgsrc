02fd429608ccb34f93b1bc3b8bf0fc925f059367
youri <youri@pkgsrc.org>
2017-06-25 19:58:04 +0000

    Fix NetBSD current build.

--- x11/kde-workspace4/patches/patch-ksysguard_ksysguardd_NetBSD_ProcessList.c
+++ x11/kde-workspace4/patches/patch-ksysguard_ksysguardd_NetBSD_ProcessList.c
@@ -1,11 +1,12 @@
-$NetBSD: patch-ksysguard_ksysguardd_NetBSD_ProcessList.c,v 1.1 2013/01/17 16:05:53 joerg Exp $
+$NetBSD: patch-ksysguard_ksysguardd_NetBSD_ProcessList.c,v 1.2 2017/06/25 19:58:04 youri Exp $
 
---- ksysguard/ksysguardd/NetBSD/ProcessList.c.orig	2013-01-16 23:02:11.000000000 +0000
+--- ksysguard/ksysguardd/NetBSD/ProcessList.c.orig	2015-08-12 07:03:15.000000000 +0000
 +++ ksysguard/ksysguardd/NetBSD/ProcessList.c
-@@ -35,6 +35,7 @@
+@@ -34,7 +34,7 @@
+ #include <sys/sysctl.h>
  #include <sys/time.h>
  #include <sys/types.h>
- #include <sys/user.h>
+-#include <sys/user.h>
 +#include <sys/resource.h>
  #include <unistd.h>
  #include <signal.h>
