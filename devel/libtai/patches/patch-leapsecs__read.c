b860088764f40bf81cd14429dc62325ea914e129
schmonz <schmonz@pkgsrc.org>
2017-06-26 01:06:07 +0000

    Actually, this code is public-domain. Respect PKG_SYSCONFDIR. Ride previous PKGREVISION bump.

--- devel/libtai/patches/patch-leapsecs__read.c
+++ devel/libtai/patches/patch-leapsecs__read.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-leapsecs__read.c,v 1.1 2017/06/26 00:53:48 schmonz Exp $
+$NetBSD: patch-leapsecs__read.c,v 1.2 2017/06/26 01:06:08 schmonz Exp $
 
 #include <unistd.h> in leapsecs_read.c so we can read leapsecs.dat.
+Respect PKG_SYSCONFDIR.
 
 --- leapsecs_read.c.orig	1998-10-13 16:52:20.000000000 +0000
 +++ leapsecs_read.c
@@ -12,3 +13,12 @@ $NetBSD: patch-leapsecs__read.c,v 1.1 2017/06/26 00:53:48 schmonz Exp $
  extern int errno;
  #include "tai.h"
  #include "leapsecs.h"
+@@ -18,7 +19,7 @@ int leapsecs_read()
+   int i;
+   struct tai u;
+ 
+-  fd = open("/etc/leapsecs.dat",O_RDONLY | O_NDELAY);
++  fd = open("@PKG_SYSCONFDIR@/leapsecs.dat",O_RDONLY | O_NDELAY);
+   if (fd == -1) {
+     if (errno != ENOENT) return -1;
+     if (leapsecs) free(leapsecs);
