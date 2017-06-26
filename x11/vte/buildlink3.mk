613644a351a8623933067acde8acd05fdb33d85a
joerg <joerg@pkgsrc.org>
2017-06-26 11:23:03 +0000

    Do not pick up 0.32+.

--- x11/vte/buildlink3.mk
+++ x11/vte/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.45 2017/02/12 06:24:59 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.46 2017/06/26 11:23:03 joerg Exp $
 
 BUILDLINK_TREE+=	vte
 
 .if !defined(VTE_BUILDLINK3_MK)
 VTE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vte+=	vte>=0.14.0
-BUILDLINK_ABI_DEPENDS.vte+=	vte>=0.28.1nb19
+BUILDLINK_API_DEPENDS.vte+=	vte>=0.14.0<0.32
+BUILDLINK_ABI_DEPENDS.vte+=	vte>=0.28.1nb19<0.32
 BUILDLINK_PKGSRCDIR.vte?=	../../x11/vte
 
 .include "../../devel/glib2/buildlink3.mk"
