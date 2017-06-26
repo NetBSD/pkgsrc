2f48ae4d68a8f42ed70005f296a9986e0d67e0ea
joerg <joerg@pkgsrc.org>
2017-06-26 11:29:46 +0000

    Fix directory.

--- x11/vte3/buildlink3.mk
+++ x11/vte3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/06/25 23:20:45 youri Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/06/26 11:29:46 joerg Exp $
 
 BUILDLINK_TREE+=	vte
 
@@ -7,7 +7,7 @@ VTE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.vte+=	vte>=0.38.0
 BUILDLINK_ABI_DEPENDS.vte+=	vte>=0.38.0
-BUILDLINK_PKGSRCDIR.vte?=	../../x11/vte
+BUILDLINK_PKGSRCDIR.vte?=	../../x11/vte3
 
 .include "../../x11/gtk3/buildlink3.mk"
 .include "../../mk/termcap.buildlink3.mk"
