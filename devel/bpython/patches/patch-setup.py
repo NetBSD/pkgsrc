ac94009a6b4b217b2d682eea6aaba4269965a46f
wiedi <wiedi@pkgsrc.org>
2017-06-27 23:33:08 +0000

    add support for PKGMANDIR

--- devel/bpython/patches/patch-setup.py
+++ devel/bpython/patches/patch-setup.py
@@ -1,8 +1,8 @@
-$NetBSD: patch-setup.py,v 1.1 2017/05/25 06:38:44 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2017/06/27 23:33:08 wiedi Exp $
 
 Use the same man directory across all platforms.
 
---- setup.py.orig	2017-05-25 06:32:08.000000000 +0000
+--- setup.py.orig	2016-10-10 19:21:11.000000000 +0000
 +++ setup.py
 @@ -185,10 +185,7 @@ if using_sphinx:
      build.sub_commands.insert(0, ('build_sphinx_man', None))
@@ -12,7 +12,7 @@ Use the same man directory across all platforms.
 -        man_dir = 'man'
 -    else:
 -        man_dir = 'share/man'
-+    man_dir = 'man'
++    man_dir = os.environ.get('PKGMANDIR', 'man')
  
      # manual pages
      man_pages = [
