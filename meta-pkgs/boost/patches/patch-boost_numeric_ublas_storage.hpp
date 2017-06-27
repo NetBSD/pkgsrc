1dbe04da5d965cc97cdad0ece685d1ef91b13af6
joerg <joerg@pkgsrc.org>
2017-06-27 20:08:59 +0000

    Move workaround for missing include from csound6 to main boost package, it affects others. Bump boost-headers.

---
+++ meta-pkgs/boost/patches/patch-boost_numeric_ublas_storage.hpp
@@ -0,0 +1,13 @@
+$NetBSD: patch-boost_numeric_ublas_storage.hpp,v 1.1 2017/06/27 20:08:59 joerg Exp $
+
+--- boost/numeric/ublas/storage.hpp.orig	2017-06-27 09:07:20.588660202 +0000
++++ boost/numeric/ublas/storage.hpp
+@@ -26,6 +26,8 @@
+ #include <boost/numeric/ublas/traits.hpp>
+ #include <boost/numeric/ublas/detail/iterator.hpp>
+ 
++#include <boost/serialization/array_wrapper.hpp>
++
+ namespace boost { namespace numeric { namespace ublas {
+ 
+ 
