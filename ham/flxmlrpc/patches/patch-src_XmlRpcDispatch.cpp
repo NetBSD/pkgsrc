b0a6ff51e0b14d12b5c871120d974d969d85f3d7
wiedi <wiedi@pkgsrc.org>
2017-06-28 13:47:42 +0000

    everybody wants string.h

--- ham/flxmlrpc/patches/patch-src_XmlRpcDispatch.cpp
+++ ham/flxmlrpc/patches/patch-src_XmlRpcDispatch.cpp
@@ -1,16 +1,14 @@
-$NetBSD: patch-src_XmlRpcDispatch.cpp,v 1.1 2017/06/28 11:07:21 wiedi Exp $
+$NetBSD: patch-src_XmlRpcDispatch.cpp,v 1.2 2017/06/28 13:47:42 wiedi Exp $
 
-add include on SunOS for memset
+add include for memset
 
 --- src/XmlRpcDispatch.cpp.orig	2015-04-19 13:32:14.000000000 +0000
 +++ src/XmlRpcDispatch.cpp
-@@ -32,6 +32,10 @@
+@@ -32,6 +32,8 @@
  #	include <sys/timeb.h>
  #endif // __FreeBSD__
  
-+#if defined(__sun)
 +#include <string.h>
-+#endif
 +
  #if defined(_WINDOWS)
  # include <winsock2.h>
