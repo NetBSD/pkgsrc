ba945f6c6a43dc8b76d6aa251017ae9db67e744a
kamil <kamil@pkgsrc.org>
2017-06-30 00:25:34 +0000

    Adapt fscd for <sys/user.h> removal from NetBSD 8.99.1

--- sysutils/fscd/patches/patch-fscd_fscd.c
+++ sysutils/fscd/patches/patch-fscd_fscd.c
@@ -1,8 +1,18 @@
-$NetBSD: patch-fscd_fscd.c,v 1.1 2013/09/10 14:43:23 joerg Exp $
+$NetBSD: patch-fscd_fscd.c,v 1.2 2017/06/30 00:25:34 kamil Exp $
 
 --- fscd/fscd.c.orig	2012-11-12 00:12:33.000000000 +0000
 +++ fscd/fscd.c
-@@ -1067,6 +1067,10 @@ handle_task(struct fscd_cfg *config, cha
+@@ -45,7 +45,9 @@ __FBSDID("$FreeBSD$");
+ #include <sys/sysctl.h>
+ #include <sys/uio.h>
+ #include <sys/un.h>
++#if defined(__FreeBSD__) || defined(__DragonFly__)
+ #include <sys/user.h>
++#endif
+ #include <sys/time.h>
+ #include <sys/wait.h>
+ #include <ctype.h>
+@@ -1067,6 +1069,10 @@ handle_task(struct fscd_cfg *config, cha
  		print_status(config, sock_fd);
  		pthread_mutex_unlock(&config->service_mtx);
  		return 0;
