38aa61d8775cb07933e028f02464a48b7580526c
prlw1 <prlw1@pkgsrc.org>
2017-07-03 17:42:45 +0000

    Update glib2 to 2.52.3
    Highlights include:
    * OS X implementations of GContentType and GAppInfo have been added
    * Memory leak fixes
    * Minimal support for UUIDs has been added
    * A new file attribute, G_FILE_ATTRIBUTE_RECENT_MODIFIED has been added
      to improve sorting of recent files
    * glib-compile-resources grew a --generate-phony-targets flag
    * GLib now installs a valgrind suppressions file for GLib and GIO
    * glib-genmarshal and glib-mkenums have gained --output options
      for better build system integration
    * New API: g_utf8_make_valid
    
    + Many bug fixes and translation updates. For a full list see:
    https://git.gnome.org/browse/glib/tree/NEWS?h=2.52.3

--- devel/glib2/patches/patch-gio_gdbusauthmechanismsha1.c
+++ devel/glib2/patches/patch-gio_gdbusauthmechanismsha1.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-gio_gdbusauthmechanismsha1.c,v 1.2 2013/12/08 13:08:59 prlw1 Exp $
+$NetBSD: patch-gio_gdbusauthmechanismsha1.c,v 1.3 2017/07/03 17:42:45 prlw1 Exp $
 
 Suppress format warnings that are converted to errors with gcc 4.8
 
---- gio/gdbusauthmechanismsha1.c.orig	2013-08-08 10:00:40.000000000 +0000
+--- gio/gdbusauthmechanismsha1.c.orig	2017-06-22 12:52:49.000000000 +0000
 +++ gio/gdbusauthmechanismsha1.c
-@@ -273,7 +273,7 @@ ensure_keyring_directory (GError **error
+@@ -272,7 +272,7 @@ ensure_keyring_directory (GError **error
                             G_IO_ERROR_FAILED,
-                            _("Permissions on directory '%s' are malformed. Expected mode 0700, got 0%o"),
+                            _("Permissions on directory “%s” are malformed. Expected mode 0700, got 0%o"),
                             path,
 -                           statbuf.st_mode & 0777);
 +                           (guint)statbuf.st_mode & 0777);
