6ee5a304275fba19c99e7b11da54107a4ed63566
adam <adam@pkgsrc.org>
2017-07-03 20:48:03 +0000

    The version 2017.0 is mainly a bug fix release. The biggest fixes regard: * Several fixes for working with HDR images (display in GUI, photometric optimizer, handling of under/over exposured pixels in merging code). * Fixes handling of masks in cpfind when images needs remapping for cp finding. * Sometimes unsaved changes were disregarded without asking the user (e.g. when loading a project via the recently used projects list). Unsaved changes should now always require user confirmation.
    Besides the bug fixes some smaller improvements have been implemented.
    * Optimizer tabs uses now checkboxes instead of bold underlined font for marking of variables to be optimized.
    * Use wxWidgets help windows instead of default browser (Linux&Mac only, this provides table of content, index and full text search in help files).
    * Added special assistant variant for single image projects.
    * Display of final panorama dimensions on stitcher tab.
    * Extended the user defined output sequences:
      - The shipped user defined output sequences are exposed in the GUI as own sub-menu.
      - Added some more user defined output sequences (layered TIFF, cube faces).
      - Added new placeholder %sourceimage% to user defined output sequence.

--- graphics/hugin/patches/patch-src_hugin1_base__wx_StitchingExecutor.cpp
+++ graphics/hugin/patches/patch-src_hugin1_base__wx_StitchingExecutor.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_hugin1_base__wx_StitchingExecutor.cpp,v 1.1 2015/10/18 15:56:56 adam Exp $
+$NetBSD: patch-src_hugin1_base__wx_StitchingExecutor.cpp,v 1.2 2017/07/03 20:48:03 adam Exp $
 
 Re-order includes to fix building.
 
---- src/hugin1/base_wx/StitchingExecutor.cpp.orig	2015-08-24 21:56:49.000000000 +0000
+--- src/hugin1/base_wx/StitchingExecutor.cpp.orig	2017-04-29 16:05:50.000000000 +0000
 +++ src/hugin1/base_wx/StitchingExecutor.cpp
-@@ -37,9 +37,9 @@
+@@ -34,9 +34,9 @@
  #include "hugin_base/panotools/PanoToolsUtils.h"
  #include "hugin_base/panodata/PanoramaOptions.h"
  #include "hugin_base/algorithms/basic/LayerStacks.h"
@@ -14,4 +14,4 @@ Re-order includes to fix building.
 -#include "base_wx/LensTools.h"
  #include "hugin/config_defaults.h"
  
- #if wxCHECK_VERSION(3,0,0)
+ namespace HuginQueue
---
+++ graphics/hugin/patches/patch-src_hugin__script__interface_hsi.i
@@ -0,0 +1,14 @@
+$NetBSD: patch-src_hugin__script__interface_hsi.i,v 1.1 2017/07/03 20:48:04 adam Exp $
+
+Vigra's class VT1 conflicts with '#define VT1' from sys/termios.h.
+
+--- src/hugin_script_interface/hsi.i.orig	2017-06-01 22:41:27.000000000 +0000
++++ src/hugin_script_interface/hsi.i
+@@ -27,6 +27,7 @@
+ 
+ %{
+ #define SWIG_FILE_WITH_INIT
++#undef VT1
+ 
+ #include <hugin_shared.h>
+ #include <appbase/DocumentData.h>
