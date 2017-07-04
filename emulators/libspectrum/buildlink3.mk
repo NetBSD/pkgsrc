b38b56e0415f12c20dcede99d883eca2b800c977
adam <adam@pkgsrc.org>
2017-07-04 09:37:26 +0000

    libspectrum 1.3.5: * Add Multiface One/128/3 support * Add self-inflating buffer for writing binary files * Add class recognition for SCR files * Fix crash when saving CSW tapes * Various minor bug fixes/improvements:   * Add unit test for CSW writing   * Swich to using autoreconf   * Use silent builds by default   * Remove more const qualifiers from libspectrum_buffer API   * Fix GCC warnings about unexpected values in switch statements   * Remove unsused variables from libspectrum_buffer refactoring   * Fix error messages when reading Opus/+D chunks from SZX files   * Distribute standard-tap.tap   * Set logical '1' when reading SZX flags   * Document Multiface snap accessors

--- emulators/libspectrum/buildlink3.mk
+++ emulators/libspectrum/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.19 2017/06/05 04:45:40 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2017/07/04 09:37:26 adam Exp $
 
 BUILDLINK_TREE+=	libspectrum
 
 .if !defined(LIBSPECTRUM_BUILDLINK3_MK)
 LIBSPECTRUM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libspectrum+=	libspectrum>=0.2.2
-BUILDLINK_ABI_DEPENDS.libspectrum+=	libspectrum>=1.1.1nb1
+BUILDLINK_API_DEPENDS.libspectrum+=	libspectrum>=1.3.5
+BUILDLINK_ABI_DEPENDS.libspectrum+=	libspectrum>=1.3.5
 BUILDLINK_PKGSRCDIR.libspectrum?=	../../emulators/libspectrum
 
 .include "../../archivers/bzip2/buildlink3.mk"
