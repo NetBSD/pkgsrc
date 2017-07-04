2b5a331b4c104fc41d10018997a4069d4ce53f8e
fhajny <fhajny@pkgsrc.org>
2017-07-04 13:31:16 +0000

    Update sysutils/rsyslog* to 8.28.0
    - omfwd: add parameter "tcp_frameDelimiter"
    - omkafka: large refactor of kafka subsystem
    - imfile: improved handling of atomically renamed file (w/ wildcards)
    - imfile: add capability to truncate oversize messages or split into
      multiple
    - mmdblookup
      * upgraded from "contrib" to "fully supported" state
      * refactored and simplified code
      * added ability to specify custom names for extracted fields
      * bugfix: fixed multiple memory leaks
    - imptcp: add new parameter "flowControl"
    - imrelp: add "maxDataSize" config parameter
    - multiple modules: gtls: improve error if certificate file can't be
      opened
    - omsnare: allow different tab escapes
    - omelasticsearch: converted to use libfastjson instead of json-c
    - imjournal: _PID fallback
    - multiple modules: add better error messages when regcomp is failing
    - omhiredis: fix build warnings
    - imfile bugfix: files mv-ed in into directory were not handled
    - omprog bugfix: execve() incorrectly called
    - imfile bugfix: multiline timeout did not work if state file exists
    - lmsig_ksi-ls12 bugfix: build problems on some platforms
    - core bugfix: invalid object type assertion
    - regression fix: local hostname was not always detected properly on
      early start (w/o network).
    - bugfix: format security issues in zmq3 modules
    - bugfix build system: add libksi only to those binaries that need it
    - bugfix KSI ls12 components: invalid tree height calculation
    - testbench/CI enhancements

--- sysutils/rsyslog/patches/patch-runtime_lookup.c
+++ sysutils/rsyslog/patches/patch-runtime_lookup.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-runtime_lookup.c,v 1.3 2017/05/18 13:27:44 fhajny Exp $
+$NetBSD: patch-runtime_lookup.c,v 1.4 2017/07/04 13:31:16 fhajny Exp $
 
 Handle Darwin's pthread_setname_np().
 
---- runtime/lookup.c.orig	2017-05-15 09:41:19.000000000 +0000
+--- runtime/lookup.c.orig	2017-06-25 11:46:08.000000000 +0000
 +++ runtime/lookup.c
 @@ -958,10 +958,12 @@ lookupTableDefProcessCnf(struct cnfobj *
  	strcpy(reloader_thd_name, reloader_prefix);
@@ -10,12 +10,12 @@ Handle Darwin's pthread_setname_np().
  	reloader_thd_name[thd_name_len - 1] = '\0';
 -  #ifndef __APPLE__
 -     pthread_setname_np(lu->reloader, reloader_thd_name);
+-  #else
 +  #if defined(__NetBSD__)
 +     pthread_setname_np(lu->reloader, "%s", reloader_thd_name);
 +  #elif defined(__APPLE__)
-+     pthread_setname_np(reloader_thd_name); // must check
-   #else
--     pthread_setname_np(reloader_thd_name); // must check
+      pthread_setname_np(reloader_thd_name); // must check
++  #else
 +     pthread_setname_np(lu->reloader, reloader_thd_name);
    #endif
  #endif
