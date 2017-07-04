e6913984e03eb931f5ca70a90b2010ba4103bc98
khorben <khorben@pkgsrc.org>
2017-07-04 18:29:24 +0000

    Implement a check for RELRO
    This is only performed if PKG_DEVELOPER and RELRO are in use.
    
    After a suggestion during my talk at BSDCan 2017; thanks!
    Also, submitted on tech-pkg@ for review mid-June.
    
    As a next step, it seems this can be extended to libraries, just like the
    check for SHLIBS does (from which this is inspired).

--- mk/check/bsd.check-vars.mk
+++ mk/check/bsd.check-vars.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.check-vars.mk,v 1.8 2015/08/17 17:35:23 jperkin Exp $
+# $NetBSD: bsd.check-vars.mk,v 1.9 2017/07/04 18:29:24 khorben Exp $
 #
 # This Makefile fragment is included separately by bsd.pkg.mk and
 # defines some variables which must be defined earlier than where
@@ -6,7 +6,16 @@
 #
 
 CHECK_FILES_SUPPORTED?=		yes
+CHECK_RELRO_SUPPORTED?=		yes
 CHECK_SHLIBS_SUPPORTED?=	yes
+
+_OPSYS_CAN_CHECK_RELRO?=	${_OPSYS_CAN_CHECK_SHLIBS}
+
+.if ${_OPSYS_CAN_CHECK_RELRO:tl} == "yes"
+_USE_CHECK_RELRO_NATIVE=	yes
+.else
+_USE_CHECK_RELRO_NATIVE=	no
+.endif
 
 .if ${_OPSYS_CAN_CHECK_SHLIBS:tl} == "yes"
 _USE_CHECK_SHLIBS_NATIVE=	yes
