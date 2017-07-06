ea568f6c29ea015d269d8e03627c60ccd14cca38
wiedi <wiedi@pkgsrc.org>
2017-07-06 11:31:16 +0000

    termios.h is not just for SunOS thanks wiz@

--- editors/sandy/patches/patch-sandy.c
+++ editors/sandy/patches/patch-sandy.c
@@ -1,15 +1,13 @@
-$NetBSD: patch-sandy.c,v 1.1 2017/07/06 09:09:02 wiedi Exp $
+$NetBSD: patch-sandy.c,v 1.2 2017/07/06 11:31:16 wiedi Exp $
 
-Needs include for TIOCGWINSZ on SunOS
+Needs include for TIOCGWINSZ
 --- sandy.c.orig	2011-04-01 16:05:58.000000000 +0000
 +++ sandy.c
-@@ -14,6 +14,10 @@
+@@ -14,6 +14,8 @@
  #include <string.h>
  #include <curses.h>
  
-+#if defined(__sun)
-+#include <sys/termios.h>
-+#endif
++#include <termios.h>
 +
  /* Defines */
  #ifndef PIPESIZ /* This is POSIX magic */
