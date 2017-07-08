6c645a211bf19f8f74945eae9f39032099eb4f60
kamil <kamil@pkgsrc.org>
2017-07-08 12:06:56 +0000

    dtpstree: Fix build on NetBSD 8.99.1
    Address <sys/user.h> removal.

--- sysutils/dtpstree/patches/patch-dtpstree.cpp
+++ sysutils/dtpstree/patches/patch-dtpstree.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-dtpstree.cpp,v 1.1 2013/04/30 22:34:31 joerg Exp $
+$NetBSD: patch-dtpstree.cpp,v 1.2 2017/07/08 12:06:56 kamil Exp $
 
---- dtpstree.cpp.orig	2013-04-30 19:54:40.000000000 +0000
+--- dtpstree.cpp.orig	2010-08-05 08:01:47.000000000 +0000
 +++ dtpstree.cpp
 @@ -21,6 +21,7 @@
  
@@ -10,3 +10,13 @@ $NetBSD: patch-dtpstree.cpp,v 1.1 2013/04/30 22:34:31 joerg Exp $
  #include <cstdarg>
  #include <cstdio>
  #include <cstdlib>
+@@ -55,7 +56,9 @@
+ #include <pwd.h>
+ #include <sys/param.h>
+ #include <sys/sysctl.h>
++#if defined(__FreeBSD__) || defined(__DragonFly__)
+ #include <sys/user.h>
++#endif
+ #include <sys/utsname.h>
+ #include <unistd.h>
+ #include <vis.h>
