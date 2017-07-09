8c56f25fab87fcf4c228eed01a975f7d9848f742
schmonz <schmonz@pkgsrc.org>
2017-07-09 16:16:32 +0000

    Fix build with PKGSRC_USE_SSP, and support PKGSRC_USE_FORTIFY. While here, remove reference to LOCALBASE.

--- mail/spamdyke/patches/patch-spamdyke.h
+++ mail/spamdyke/patches/patch-spamdyke.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-aa,v 1.12 2015/05/03 14:22:08 schmonz Exp $
+$NetBSD: patch-spamdyke.h,v 1.1 2017/07/09 16:16:32 schmonz Exp $
 
 Substitute paths.
 
@@ -9,7 +9,7 @@ Substitute paths.
  
  #define DEFAULT_REMOTE_IP               "0.0.0.0"
 -#define DEFAULT_PATH                    "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/var/qmail/bin:/home/vpopmail/bin"
-+#define DEFAULT_PATH                    "/bin:/usr/bin:@LOCALBASE@/bin:@QMAILDIR@/bin:/home/vpopmail/bin"
++#define DEFAULT_PATH                    "/bin:/usr/bin:@PREFIX@/bin:@QMAILDIR@/bin:/home/vpopmail/bin"
  #define DEFAULT_NIHDNS_RESOLVER_FILENAME        "/etc/resolv.conf"
  #define DEFAULT_NIHDNS_PORT             53
  #define DEFAULT_TIMEOUT_NIHDNS_TOTAL_SECS       30
