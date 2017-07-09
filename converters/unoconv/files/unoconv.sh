957bf5e2029f89101534f379ac4914cb3f2bc2c5
manu <manu@pkgsrc.org>
2017-07-09 14:13:53 +0000

    Add converters/unoconv
    unoconv converts any document format that OpenOffice can import, to any
    document format that OpenOffice can export.
    
    unoconv uses the OpenOffice's UNO bindings for non-interactive conversion
    of documents.

---
+++ converters/unoconv/files/unoconv.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+PYTHONBIN=`pkg_info -Q PYTHONBIN libreoffice`
+UNO_PATH=`pkg_info -Q UNO_PATH libreoffice`
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${UNO_PATH}
+
+export UNO_PATH
+export LD_LIBRARY_PATH
+
+${PYTHONBIN} @PREFIX@/libexec/unoconv.py $*
+
