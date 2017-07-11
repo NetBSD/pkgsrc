70cc7cee04ddbc4a8738064382f4490edbcc9711
jaapb <jaapb@pkgsrc.org>
2017-07-11 14:15:10 +0000

    Revbump associated with ocaml-4.04.2, plus correction of versioning scheme and configure flags

--- devel/ocaml-ppx_core/buildlink3.mk
+++ devel/ocaml-ppx_core/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/03/04 15:22:01 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/07/11 14:15:10 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_core
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_core
 OCAML_PPX_CORE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_core+=	ocaml-ppx_core>=113.24.00
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_core+=	ocaml-ppx_core>=114.01.04nb2
 BUILDLINK_PKGSRCDIR.ocaml-ppx_core?=	../../devel/ocaml-ppx_core
 .endif	# OCAML_PPX_CORE_BUILDLINK3_MK
 
