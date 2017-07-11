934b77ad2ce5803c40370165338e58e12538071e
adam <adam@pkgsrc.org>
2017-07-11 19:24:31 +0000

    Changes 2.5.2: Unknown

--- lang/py-jsparser/patches/patch-setup.py
+++ lang/py-jsparser/patches/patch-setup.py
@@ -1,17 +1,20 @@
-$NetBSD: patch-setup.py,v 1.1 2017/02/07 23:48:57 joerg Exp $
+$NetBSD: patch-setup.py,v 1.2 2017/07/11 19:24:31 adam Exp $
 
---- setup.py.orig	2017-02-07 17:06:34.545212473 +0000
+Use setuptools.
+
+--- setup.py.orig	2017-07-09 11:31:42.000000000 +0000
 +++ setup.py
-@@ -1,11 +1,13 @@
+@@ -1,4 +1,7 @@
 -from distutils.core import setup
 +try:
 +    from setuptools import setup
 +except ImportError:
 +    from distutils.core import setup
  
+ # python3 setup.py sdist upload -r pypi
  setup(
-     name='pyjsparser',
-     version='2.4.5',
+@@ -6,7 +9,6 @@ setup(
+     version='2.5.2',
      packages=['pyjsparser'],
      url='https://github.com/PiotrDabkowski/pyjsparser',
 -    install_requires = [],
