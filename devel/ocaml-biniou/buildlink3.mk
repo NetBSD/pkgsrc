6e489f31fe27e7e3cbb7fe57173d576fe0fcbe78
jaapb <jaapb@pkgsrc.org>
2017-07-11 11:27:00 +0000

    Added devel/ocaml-biniou, a library for a new JSON-like (but faster) binary format.

---
+++ devel/ocaml-biniou/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2017/07/11 11:27:00 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-biniou
+
+.if !defined(OCAML_BINIOU_BUILDLINK3_MK)
+OCAML_BINIOU_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-biniou+=	ocaml-biniou>=1.0.13
+BUILDLINK_PKGSRCDIR.ocaml-biniou?=	../../devel/ocaml-biniou
+
+.endif	# OCAML_BINIOU_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-biniou
