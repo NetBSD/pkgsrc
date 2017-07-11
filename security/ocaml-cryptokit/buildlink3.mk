8794117e58050bba2bed0f2ee48c0c3eb5a4d1aa
jaapb <jaapb@pkgsrc.org>
2017-07-11 12:34:22 +0000

    Updated package to latest version, 1.11 (plus distribution change). Changes include:
    Release 1.11:
    - Adapt to "safe string" mode (OCaml 4.02 and later required).
      The API should remain backward-compatible for clients compiled
      in "unsafe string" mode.
    - Update SHA-3 to the official NIST standard (different padding than
      in the Keccak submission).  (Closes: #1528)
    - Fixed bounds checking in "add_substring" methods of hash functions
      and other functions that operate on a substring of a string.
      (Closes: #1480)
    - Use hardware implementation of AES when available on x86 processors.
      (Faster than the software implementation and less sensitive to
       side channel attacks.)
    - Use the Zarith library to implement RSA.
      (Faster than the previous implementation and less sensitive to
       side channel attacks.)
    - Support the hardware random number generator present in recent
      x86 processors.
    - Rebuilt generated files with Oasis 0.4.6 for OCaml 4.03 compatibility.

--- security/ocaml-cryptokit/buildlink3.mk
+++ security/ocaml-cryptokit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2016/12/30 11:16:57 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2017/07/11 12:34:22 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cryptokit
 
@@ -6,9 +6,10 @@ BUILDLINK_TREE+=	ocaml-cryptokit
 OCAML_CRYPTOKIT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-cryptokit+=	ocaml-cryptokit>=1.5
-BUILDLINK_ABI_DEPENDS.ocaml-cryptokit?=	ocaml-cryptokit>=1.10nb3
+BUILDLINK_ABI_DEPENDS.ocaml-cryptokit?=	ocaml-cryptokit>=1.11
 BUILDLINK_PKGSRCDIR.ocaml-cryptokit?=	../../security/ocaml-cryptokit
 
+.include "../../math/ocaml-zarith/buildlink3.mk"
 .include "../../lang/ocaml/buildlink3.mk"
 .endif # OCAML_CRYPTOKIT_BUILDLINK3_MK
 
