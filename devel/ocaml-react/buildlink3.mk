02e7e0d54ac8b372d5fefb8af2da6609b35ebf95
jaapb <jaapb@pkgsrc.org>
2017-07-11 11:02:44 +0000

    Updated package to latest version, 1.2.1. Incorporated license change (BSD3 to ISC) that was not mentioned in changelog. Changes include:
    - Allow signals to be created and `S.stop`ped instantaneously (#18)
      Previously this could lead to failed assertions in updates (e.g.
      `S.bind` trying to switch to an uninitialized signal). Thanks
      to Arthur Wendling for the report.
    - Fix implementation of `S.Bool.flip`, its initial value on creation
      could be wrong in dynamic creation (#17). Thanks to Arthur Wendling
      for the report.
    - Fix bug in `S.Option.value` with `` `Always`` on `S.const None` (#19).
      Thanks to Arthur Wendling for the report.

--- devel/ocaml-react/buildlink3.mk
+++ devel/ocaml-react/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/01/20 15:22:53 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 11:02:44 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-react
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-react
 OCAML_REACT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-react+=	ocaml-react>=1.2.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-react+=	ocaml-react>=1.2.1
 BUILDLINK_PKGSRCDIR.ocaml-react?=	../../devel/ocaml-react
 .endif	# OCAML_REACT_BUILDLINK3_MK
 
