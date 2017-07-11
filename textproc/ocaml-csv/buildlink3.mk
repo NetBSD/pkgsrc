207390b88c9e3f55e3641c2b026a7f898477355e
jaapb <jaapb@pkgsrc.org>
2017-07-11 12:36:21 +0000

    Also changed dependency version in buildlink3.mk

--- textproc/ocaml-csv/buildlink3.mk
+++ textproc/ocaml-csv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/07/02 10:11:09 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 12:36:21 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-csv
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-csv
 OCAML_CSV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-csv+=	ocaml-csv>=1.5
+BUILDLINK_ABI_DEPENDS.ocaml-csv+=	ocaml-csv>=1.7
 BUILDLINK_PKGSRCDIR.ocaml-csv?=	../../textproc/ocaml-csv
 
 .endif	# OCAML_CSV_BUILDLINK3_MK
