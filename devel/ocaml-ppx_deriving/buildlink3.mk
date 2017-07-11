c88ec739f9075652de00687fdbc62a686121e3ae
jaapb <jaapb@pkgsrc.org>
2017-07-11 11:18:16 +0000

    Updated package to latest version, 4.1. Changes include:
    4.1
    
        Fix type error with inheritied polymorphic variant type in [@@deriving map].
        Fix incorrect handling of multi-argument constructors in [@@deriving show].
        Add API hooks for ppx_type_conv.

--- devel/ocaml-ppx_deriving/buildlink3.mk
+++ devel/ocaml-ppx_deriving/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/08/17 16:00:01 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 11:18:16 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_deriving
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_deriving
 OCAML_PPX_DERIVING_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_deriving+=	ocaml-ppx_deriving>=4.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_deriving+=	ocaml-ppx_deriving>=4.1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_deriving?=	../../devel/ocaml-ppx_deriving
 .endif	# OCAML_PPX_DERIVING_BUILDLINK3_MK
 
