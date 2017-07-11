539863707d90c5ab7558edfb9563c6b0fa690cc0
jaapb <jaapb@pkgsrc.org>
2017-07-11 12:31:37 +0000

    Updated package to latest version 7.2.3. Changes include:
    2016-02-25:  Minor version release v7.2.3:
    
                   Fixed callout bug introduced with v7.2.0.
    
                 Thanks to Raman Varabets for the bug report!
    
    2016-02-23:  Fixed linking problem with old versions of PCRE (< 8.20).
    
                 Fixed backward compatibility issue with OCaml <= 3.12.
    
    2016-02-22:  Fixed a subgroup matching bug.
    
    Thanks to Cheng Lou for the bug report!

--- devel/pcre-ocaml/buildlink3.mk
+++ devel/pcre-ocaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2012/05/07 01:53:27 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/07/11 12:31:37 jaapb Exp $
 
 BUILDLINK_TREE+=	pcre-ocaml
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	pcre-ocaml
 PCRE_OCAML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pcre-ocaml+=	pcre-ocaml>=5.10.3nb1
-BUILDLINK_ABI_DEPENDS.pcre-ocaml+=	pcre-ocaml>=5.15.0nb2
+BUILDLINK_ABI_DEPENDS.pcre-ocaml+=	pcre-ocaml>=7.2.3
 BUILDLINK_PKGSRCDIR.pcre-ocaml?=	../../devel/pcre-ocaml
 
 .include "../../devel/pcre/buildlink3.mk"
