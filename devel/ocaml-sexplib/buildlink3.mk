47a444aa8d76160fe5578c12bf38b561340358cf
jaapb <jaapb@pkgsrc.org>
2017-07-11 12:14:13 +0000

    Updated package to latest version, 0.9.1. Attention: this is a new versioning scheme - earlier version was 113.33.03. As per the thread on https://mail-index.netbsd.org/tech-pkg/2016/11/26/msg017548.html and the example given by archivers/lz4, I've just changed the version number without further ado.
    Changes up until 113.43.00 (there seems to be no later changelog) include:
    
        Sexps and EOF are a mess. Try to improve the situation somewhat!
    
    		    In particular, this feature makes improvements to when
    				Parsing_whitespace sexp parser state is reported: it now
    				distinguishes the case Parsing_toplevel_whitespace from
    				Parsing_nested_whitespace and it's only a valid empty parse if eof
    				happens in Parsing_toplevel_whitespace. See test diffs for examples
    				of strings that were previously valid empty parses, but are now
    				incomplete. One of the craziest is probably "(foo #| bar".

--- devel/ocaml-sexplib/buildlink3.mk
+++ devel/ocaml-sexplib/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/06/20 15:10:30 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 12:14:13 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-sexplib
 
 .if !defined(OCAML_SEXPLIB_BUILDLINK3_MK)
 OCAML_SEXPLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=113.33.03
+BUILDLINK_API_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=0.9.0
+BUILDLINK_ABI_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=0.9.1
 BUILDLINK_PKGSRCDIR.ocaml-sexplib?=	../../devel/ocaml-sexplib
 
 .endif	# OCAML_SEXPLIB_BUILDLINK3_MK
