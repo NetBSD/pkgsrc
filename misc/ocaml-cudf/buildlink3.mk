a78125b2ebebdb4b00adae2b8c61c8a5b8ead88e
jaapb <jaapb@pkgsrc.org>
2017-07-11 10:29:58 +0000

    Updated package to latest version, 0.9. Changes include:
    2017-03-21  Stefano Zacchiroli  <zack@upsilon.cc>
    * ALL release 0.9
    * Build: remove spurious dependencies on no longer used camlp4
    * Makefile: fix build issues on Windows

--- misc/ocaml-cudf/buildlink3.mk
+++ misc/ocaml-cudf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/06/17 16:15:48 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 10:29:58 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cudf
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-cudf
 OCAML_CUDF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-cudf+=	ocaml-cudf>=0.8
+BUILDLINK_ABI_DEPENDS.ocaml-cudf+=	ocaml-cudf>=0.9
 BUILDLINK_PKGSRCDIR.ocaml-cudf?=	../../misc/ocaml-cudf
 
 .include "../../devel/ocaml-extlib/buildlink3.mk"
