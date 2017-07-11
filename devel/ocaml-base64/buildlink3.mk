73e46c958b3ea9bda02de96c4a19f87e329b7dfe
jaapb <jaapb@pkgsrc.org>
2017-07-11 11:17:04 +0000

    Updated package to latest version, 2.2.0. Changes include:
    v2.2.0 (2017-06-20)
    
        Switch to jbuilder (#13, @rgrinberg)
    
    v2.1.2 (2016-10-18)
    
        Fix version number (#11, @hannesm)
    
    v2.1.1 (2016-10-03)
    
        Switch build to topkg and obey the odig conventions for installing
        metadata files.
        Add a test suite based on RFC4648 test vectors.
        Improve Travis CI tests to be multidistro.

--- devel/ocaml-base64/buildlink3.mk
+++ devel/ocaml-base64/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/02/06 13:28:07 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/07/11 11:17:04 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-base64
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-base64
 OCAML_BASE64_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-base64+=	ocaml-base64>=2.0.0
+BUILDLINK_API_DEPENDS.ocaml-base64+=	ocaml-base64>=2.2.0
 BUILDLINK_PKGSRCDIR.ocaml-base64?=	../../devel/ocaml-base64
 .endif	# OCAML_BASE64_BUILDLINK3_MK
 
