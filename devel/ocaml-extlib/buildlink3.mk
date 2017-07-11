38a27e384d40a9d5eb01fea9e50c1b77dd4bc501
jaapb <jaapb@pkgsrc.org>
2017-07-11 10:25:39 +0000

    Updated package to latest version, 1.7.2 (also changed distribution site). Changes include:
    1.7.2 (2017-03-29)
     * sync with OCaml 4.05
    
    1.7.1 (2016-11-11)
     * sync with OCaml 4.04
     * add Hashtbl.Make for better compatibility

--- devel/ocaml-extlib/buildlink3.mk
+++ devel/ocaml-extlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/06/19 10:12:18 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 10:25:39 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-extlib
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-extlib
 OCAML_EXTLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-extlib+=	ocaml-extlib>=1.7.0
+BUILDLINK_ABI_DEPENDS.ocaml-extlib+=	ocaml-extlib>=1.7.2
 BUILDLINK_PKGSRCDIR.ocaml-extlib?=	../../devel/ocaml-extlib
 
 .endif	# OCAML_EXTLIB_BUILDLINK3_MK
