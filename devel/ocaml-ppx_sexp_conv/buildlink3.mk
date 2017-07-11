c46338438f82ed109d25890b15e95557d1be59bb
jaapb <jaapb@pkgsrc.org>
2017-07-11 11:40:09 +0000

    Updated package to version 114.01.04. This is an unreleased version; the update is necessary because of changes to other packages which means that the older version does not build.
    Changes include:
    
        Fix generator for polymorphic types where var names clashes with type
    		name: type 't t = ...

--- devel/ocaml-ppx_sexp_conv/buildlink3.mk
+++ devel/ocaml-ppx_sexp_conv/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/06/20 19:10:56 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 11:40:09 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_sexp_conv
 
 .if !defined(OCAML_PPX_SEXP_CONV_BUILDLINK3_MK)
 OCAML_PPX_SEXP_CONV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ppx_sexp_conv+=	ocaml-ppx_sexp_conv>=113.33.01
+BUILDLINK_API_DEPENDS.ocaml-ppx_sexp_conv+=	ocaml-ppx_sexp_conv>=114.01.04
 BUILDLINK_PKGSRCDIR.ocaml-ppx_sexp_conv?=	../../devel/ocaml-ppx_sexp_conv
 
 #.include "../../devel/ocaml-ppx_core/buildlink3.mk"
