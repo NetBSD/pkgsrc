445df9f15cb8ef860a2d8d7a3388cad859efa784
jaapb <jaapb@pkgsrc.org>
2017-07-11 09:52:11 +0000

    Updated package to newest version, 4.04.2. Changes include:
    OCaml 4.04.2 (23 Jun 2017):
    ---------------------------
    
    ### Security fix:
    
    - Local privilege escalation issue with ocaml binaries.
      (Damien Doligez, report by Eric Milliken, review by Xavier Leroy)
    
    OCaml 4.04.1 (14 Apr 2017):
    ---------------------------
    
    ### Code generation and optimizations:
    
    - Consider arrays of length zero as constants
      when using Flambda.
      (Pierre Chambart, review by Mark Shinwell and Leo White)
    
    ### Standard library:
    
    - fix a bug in Set.map as introduced in 4.04.0
      (Gabriel Scherer, report by Thomas Leonard)
    
    ### Tools:
    
    - ocamldoc, avoid nested <pre> tags in module description.
      (Florian Angeletti, report by user 'kosik')
    
    - ocamldoc, wrong Latex output for variant types
      with constructors without arguments.
      (Florian Angeletti, report by Xavier Leroy)
    
    ### Build system:
    
    - New flexlink target in Makefile.nt to bootstrap the
      flexlink binary only, rather than the flexlink binary and the FlexDLL C
      objects.
      (David Allsopp)
    
    ### Bug fixes
    
    - Str.regexp raises "Invalid_argument: index out of bounds"
      (Damien Doligez, report by John Whitington)
    
    - Fix ocamlmklib with bootstrapped FlexDLL. Bootstrapped
      FlexDLL objects are now installed to a subdirectory flexdll of the Standard
      Library which allows the compilers to pick them up explicitly and also
      ocamlmklib to include them without unnecessarily adding the entire Standard
      Library.
      (David Allsopp)
    
    - fix incorrect timestamps returned by Unix.stat on Windows
      when either TZ is set or system date is in DST.
      (David Allsopp, report and initial fix by Nicolás Ojeda Bär, review and
       superior implementation suggestion by Xavier Leroy)
    
    - s390x: Fix address of caml_raise_exn in native dynlink modules
      (Richard Jones, review by Xavier Leroy)
    
    - ensure 16 byte stack alignment inside caml_allocN on x86-64
      for ocaml build with WITH_FRAME_POINTERS defined
      (Christoph Cullmann)
    
    - fix slow compilation on source files containing a lot
      of similar debugging information location entries
      (Mark Shinwell)
    
    - a case of double free in the systhreads library (POSIX implementation)
      (Xavier Leroy, report by Chet Murthy)
    
    - catch uncaught exception when unknown files are passed
      as argument (regression in 4.04.0)
      (Bernhard Schommer, review by Florian Angeletti and Gabriel Scherer,
       report by Stephen Dolan)
    
    - Memory cannot be released after calling
        Bigarray.Genarray.change_layout.
      (Damien Doligez and Xavier Leroy, report by Liang Wang)
    
    - Fix segfault in Unix.create_process on Windows caused by wrong header
      configuration.
      (David Allsopp)
    
    - add dynlink options to ocamlbytecomp.cmxa to allow ocamlopt.opt
      to load plugins. See http://github.com/OCamlPro/ocamlc-plugins for examples.
      (Fabrice Le Fessant, review by David Allsopp)
    
    - caml-types.el: Fix missing format argument, so that it can show kind
      of call at point correctly.
      (Chunhui He)
    
    - Allow Windows CRLF line-endings in ocamlyacc on Unix and Cygwin.
      (David Allsopp, review by Damien Doligez and Xavier Leroy)
    
    - Fix segfault in Sys.runtime_parameters when exception backtraces
      are enabled.
    (Olivier Andrieu)

--- lang/ocaml/buildlink3.mk
+++ lang/ocaml/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.39 2016/12/30 11:15:00 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.40 2017/07/11 09:52:11 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml
 
 .if !defined(OCAML_BUILDLINK3_MK)
 OCAML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml+=	ocaml>=3.08.2
-BUILDLINK_ABI_DEPENDS.ocaml+=	ocaml>=4.04.0
+BUILDLINK_API_DEPENDS.ocaml+=	ocaml>=4.04.0
+BUILDLINK_ABI_DEPENDS.ocaml+=	ocaml>=4.04.2
 BUILDLINK_PKGSRCDIR.ocaml?=	../../lang/ocaml
 #BUILDLINK_DEPMETHOD.ocaml?=	build
 
