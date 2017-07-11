89b1cba5a699bd34830cb82f3479110056781150
jaapb <jaapb@pkgsrc.org>
2017-07-11 13:56:49 +0000

    Updated package to latest version, 133.33.06. There seems to be no changelog.

--- devel/ocaml-js-build-tools/buildlink3.mk
+++ devel/ocaml-js-build-tools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/06/20 14:50:12 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/07/11 13:56:49 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-js-build-tools
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-js-build-tools
 OCAML_JS_BUILD_TOOLS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-js-build-tools+=	ocaml-js-build-tools>=113.33.04
+BUILDLINK_ABI_DEPENDS.ocaml-js-build-tools+=	ocaml-js-build-tools>=113.33.06
 BUILDLINK_PKGSRCDIR.ocaml-js-build-tools?=	../../devel/ocaml-js-build-tools
 
 .endif	# OCAML_JS_BUILD_TOOLS_BUILDLINK3_MK
