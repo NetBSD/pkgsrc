6a337fde3124fda9d5306db7d0529e6af9047d30
jaapb <jaapb@pkgsrc.org>
2017-07-11 11:35:42 +0000

    Bumped PKGREVISION (due to ocaml update) and corrected pkgsrc version number. No internal changes.

--- devel/ocaml-ppx_type_conv/buildlink3.mk
+++ devel/ocaml-ppx_type_conv/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/03/04 15:44:28 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/07/11 11:35:42 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_type_conv
 
 .if !defined(OCAML_PPX_TYPE_CONV_BUILDLINK3_MK)
 OCAML_PPX_TYPE_CONV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ppx_type_conv+=	ocaml-ppx_type_conv>=113.24.00
+BUILDLINK_API_DEPENDS.ocaml-ppx_type_conv+=	ocaml-ppx_type_conv>=114.01.04nb2
 BUILDLINK_PKGSRCDIR.ocaml-ppx_type_conv?=	../../devel/ocaml-ppx_type_conv
 
 #.include "../../devel/ocaml-ppx_driver/buildlink3.mk"
