38fd83756dd6ea430ec2d37db931502f364607bd
jaapb <jaapb@pkgsrc.org>
2017-07-11 10:20:03 +0000

    Updated package to latest version, 1.5.0. Changes include:
    2017-04-24: release 1.5.0
      [+ui] Added the `CAPITALIZE()` function.

--- devel/ocaml-cppo/buildlink3.mk
+++ devel/ocaml-cppo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/02/06 13:34:52 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/07/11 10:20:03 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cppo
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-cppo
 OCAML_CPPO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.3.1
+BUILDLINK_ABI_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.5.0
 BUILDLINK_PKGSRCDIR.ocaml-cppo?=	../../devel/ocaml-cppo
 .endif	# OCAML_CPPO_BUILDLINK3_MK
 
