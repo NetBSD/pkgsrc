b65b2f171edd8b6ed24ea0fd96b81c3aab32160d
jaapb <jaapb@pkgsrc.org>
2017-07-11 14:19:18 +0000

    Revbump associated with ocaml-4.04.2

--- devel/ocaml-reactiveData/buildlink3.mk
+++ devel/ocaml-reactiveData/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2016/12/30 11:33:50 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2017/07/11 14:19:22 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-reactiveData
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-reactiveData
 OCAML_REACTIVEDATA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-reactiveData+=	ocaml-reactiveData>=0.2.1
+BUILDLINK_ABI_DEPENDS.ocaml-reactiveData+=	ocaml-reactiveData>=0.2.1nb1
 BUILDLINK_PKGSRCDIR.ocaml-reactiveData?=	../../devel/ocaml-reactiveData
 
 .include "../../devel/ocaml-react/buildlink3.mk"
--- x11/ocaml-graphics/buildlink3.mk
+++ x11/ocaml-graphics/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2016/12/30 11:16:57 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2017/07/11 14:19:19 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-graphics
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-graphics
 OCAML_GRAPHICS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-graphics+=	ocaml-graphics>=3.08.2
-BUILDLINK_ABI_DEPENDS.ocaml-graphics+=	ocaml-graphics>=4.04.0nb6
+BUILDLINK_ABI_DEPENDS.ocaml-graphics+=	ocaml-graphics>=4.04.2nb7
 BUILDLINK_PKGSRCDIR.ocaml-graphics?=	../../x11/ocaml-graphics
 
 .include "../../lang/ocaml/buildlink3.mk"
