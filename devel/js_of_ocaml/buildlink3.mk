9f68a8fbc7f78613cb2669e4cfe4c5f85c9d5f78
jaapb <jaapb@pkgsrc.org>
2017-07-11 11:32:38 +0000

    Updated package to version 2.8.4. Changes include:
     * Features/Changes
     ** Compiler: new jsoo_link binary to link multiple javascript files
        together and merging sourcemaps.
     ** Toplevel: improve genaration of toplevels, one can now pass a
        an export file containing units to export. (see jsoo_listunits)
     ** Lib: Provide a Js_of_ocaml (namespace) module to ease transition
        with future version of js_of_ocaml
    
     * Bug fixes
     ** Compiler: fix separate compilation when facing empty unit inside a library
     ** Runtime:  fix many javascript stubs
     ** Syntac: fix ppx deriving json

--- devel/js_of_ocaml/buildlink3.mk
+++ devel/js_of_ocaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/12/30 12:04:25 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 11:32:38 jaapb Exp $
 
 BUILDLINK_TREE+=	js_of_ocaml
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	js_of_ocaml
 JS_OF_OCAML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.js_of_ocaml+=	js_of_ocaml>=2.8.3
+BUILDLINK_ABI_DEPENDS.js_of_ocaml+=	js_of_ocaml>=2.8.4
 BUILDLINK_PKGSRCDIR.js_of_ocaml?=	../../devel/js_of_ocaml
 
 .endif	# JS_OF_OCAML_BUILDLINK3_MK
