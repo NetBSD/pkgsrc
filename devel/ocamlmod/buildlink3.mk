30ecbef8b42ead92642f86e7c786cafd465f5e33
jaapb <jaapb@pkgsrc.org>
2017-07-11 14:11:14 +0000

    Updated package to latest version, 0.0.8. Changes include:
    2015-09-02  Sylvain Le Gall <sylvain AT le-gall.net>
    
        * Version 0.0.8
        * Include file test/data/test01.mod (Close: #1238)

--- devel/ocamlmod/buildlink3.mk
+++ devel/ocamlmod/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/01/20 15:59:55 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 14:11:14 jaapb Exp $
 
 BUILDLINK_TREE+=	ocamlmod
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocamlmod
 OCAMLMOD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocamlmod+=	ocamlmod>=0.0.7nb1
+BUILDLINK_ABI_DEPENDS.ocamlmod+=	ocamlmod>=0.0.8
 BUILDLINK_PKGSRCDIR.ocamlmod?=	../../devel/ocamlmod
 .endif	# OCAMLMOD_BUILDLINK3_MK
 
