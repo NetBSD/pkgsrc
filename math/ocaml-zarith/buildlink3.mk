4880831d51795e87cf943b7d8626e2b740c8e53b
jaapb <jaapb@pkgsrc.org>
2017-07-11 14:16:23 +0000

    Revbump associated with ocaml 4.04.2, plus updated distribution method

--- math/ocaml-zarith/buildlink3.mk
+++ math/ocaml-zarith/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/02/07 21:43:03 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/07/11 14:16:23 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-zarith
 
@@ -6,7 +6,10 @@ BUILDLINK_TREE+=	ocaml-zarith
 OCAML_ZARITH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-zarith+=	ocaml-zarith>=1.2.1
+BUILDLINK_ABI_DEPENDS.ocaml-zarith+=	ocaml-zarith>=1.4.1nb6
 BUILDLINK_PKGSRCDIR.ocaml-zarith?=	../../math/ocaml-zarith
+
+.include "../../devel/gmp/buildlink3.mk"
 
 .endif	# OCAML_ZARITH_BUILDLINK3_MK
 
