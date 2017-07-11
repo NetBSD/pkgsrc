d59b77af398e168dc63dea95e98dbf5cdabb0b03
jaapb <jaapb@pkgsrc.org>
2017-07-11 10:00:51 +0000

    Updated package to latest version, 1.07. Changes include:
    - Allocate Zlib data structures outside the OCaml heap for compatibility
      with recent versions of Zlib
      (Github issue #1, pull request #2, report and fix by Einar Lielmanis).
    - Don't pass -L and -I options to the C compiler unless necessary.
    - Compile and install the shared library zip.cmxs.
      (Contributed by E. Millon.)
    - ocamlfind: install under 'zip' and 'camlzip' package names.

--- archivers/ocaml-zip/buildlink3.mk
+++ archivers/ocaml-zip/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/05/05 11:27:47 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 10:00:51 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-zip
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-zip
 OCAML_ZIP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-zip+=	ocaml-zip>=1.05nb6
+BUILDLINK_ABI_DEPENDS.ocaml-zip+=	ocaml-zip>=1.07
 BUILDLINK_PKGSRCDIR.ocaml-zip?=	../../archivers/ocaml-zip
 
 .include "../../devel/zlib/buildlink3.mk"
