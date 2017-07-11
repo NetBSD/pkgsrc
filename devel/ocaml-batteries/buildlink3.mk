627bfa1f024902f52d40bf5dd4e56b337fb1bc7f
jaapb <jaapb@pkgsrc.org>
2017-07-11 11:34:25 +0000

    Updated package to latest version, 2.6.0. Changes include:
    ## v2.6.0 (minor release)
    
    - added Bat{Set,Map,Splay}.any and fixed Bat{Map,Splay}.choose
      #751
      (Cedric Cellier)
    
    - added BatList.favg and faster BatList.fsum
      #746
      (Gabriel Scherer, Francois Berenger)
    
    - install .cmt and .cmti files
      #740
      (Francois Berenger, Gabriel Scherer)
    
    - BatMap: added find_default
      #730
      (Francois Berenger)
    
    - added scripts/test_install.sh
      #743
      (Francois Berenger)
    
    - BatHashtbl: added {to|of}_list, bindings
      #728
      (Francois Berenger, Thibault Suzanne)
    
    - added {BatList|BatArray}.shuffle
      #702, #707
      (Francois Berenger, Gabriel Scherer)
    
    - Clarification and improvements to the documentation
      #682, #685, #693
      (Florian Angeletti, Johannes Kloos, Michael Färber)
    
    - make `LazyList.split_at` lazy:
      `split_at : int -> 'a t -> 'a t * 'a t`
      would previously eagerly force the prefix of the list
      and only be lazy in its second returned value.
      #694
      (Michael Färber, Gabriel Scherer, Thibault Suzanne)
    
    - Add `List.{map2i,iter2i}`
      #696
      (Thibault Suzanne)
    
    - Added `Result.{map,map_both}`
      #705
      (Ifaz Kabir)
    
    - Add {BatSet,BatMap}.{Int,Int32,Int64,Nativeint,Float,Char,String} as
      common instantions of the respective `Make` functor.
      #709, #712
      (Thibault Suzanne, François Bérenger)
    
    - BatString: add `chop : ?l:int -> ?r:int -> string -> string`
      #714, #716
      (Gabriel Scherer, request by François Bérenger)
    
    - BatSet: make `to_array` allocate the resulting array at first
      instead of using Dynarray (faster, uses less memory).
      #724
      (Thibault Suzanne)
    
    - BatList: add `fold_left_map : ('a -> 'b -> 'a * 'c) -> 'a -> 'b list ->
        'a * 'c list`
      #734
      (Thibault Suzanne, review by Gabriel Scherer, request by Oscar Gauthier)
    
    - add ``BatList.frange : float -> [< `To | `Downto ] -> float -> int ->
        float list``
      ``frange 0. `To 1. 3`` is `[0.; 0.5; 1.]`.
      #745
      (François Bérenger)

--- devel/ocaml-batteries/buildlink3.mk
+++ devel/ocaml-batteries/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/07/03 10:40:33 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/07/11 11:34:25 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-batteries
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-batteries
 OCAML_BATTERIES_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-batteries+=	ocaml-batteries>=2.0
+BUILDLINK_ABI_DEPENDS.ocaml-batteries+=	ocaml-batteries>=2.6.0
 BUILDLINK_PKGSRCDIR.ocaml-batteries?=	../../devel/ocaml-batteries
 
 .endif	# OCAML_BATTERIES_BUILDLINK3_MK
