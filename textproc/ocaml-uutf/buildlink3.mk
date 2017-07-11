52ea281025347f3081eb0de0945e2001cc105a61
jaapb <jaapb@pkgsrc.org>
2017-07-11 10:39:47 +0000

    Updated package to latest version, 1.0.1. Changes include:
    v1.0.1 2016-03-07 La Forclaz (VS)
    ---------------------------------
    
    - OCaml 4.05.0 compatibility (removal of `Uchar.dump`).
    
    v1.0.0 2016-11-23 Zagreb
    ------------------------
    
    - `Uutf.String.fold_utf_{8,16be,16le}`, allow substring folding via
      optional arguments. Thanks to Raphaël Proust for the idea and the
      patch.
    - OCaml standard library `Uchar.t` support.
      - Removes and substitutes `type Uutf.uchar = int` by the (abstract)
        `Uchar.t` type. `Uchar.{of,to}_int` allows to recover the previous
        representation.
      - Removes `Uutf.{is_uchar,cp_to_string,pp_cp}`. `Uchar.{is_valid,dump}`
        can be used instead.
    - Safe string support. Manual sources and destinations now work on bytes
      rather than strings.
    - Build depend on topkg.
    - Relicense from BSD3 to ISC.

--- textproc/ocaml-uutf/buildlink3.mk
+++ textproc/ocaml-uutf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/01/20 16:04:57 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 10:39:47 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-uutf
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-uutf
 OCAML_UUTF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-uutf+=	ocaml-uutf>=0.9.3nb1
+BUILDLINK_ABI_DEPENDS.ocaml-uutf+=	ocaml-uutf>=1.0.1
 BUILDLINK_PKGSRCDIR.ocaml-uutf?=	../../textproc/ocaml-uutf
 .endif	# OCAML_UUTF_BUILDLINK3_MK
 
