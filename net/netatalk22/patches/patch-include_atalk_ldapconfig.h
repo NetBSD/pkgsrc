07c92ff740e2a08632d72d1d506d24300984b11b
hauke <hauke@pkgsrc.org>
2017-07-12 13:56:00 +0000

    Update netatalk22 to patch release 2.2.6.
    For changes see
    <https://github.com/Netatalk/Netatalk/commits/branch-netatalk-2-2>
    - the upstream release branch does not have a changelog.
    
    Upstream took a lot of our patches - thanks, guys!
    
    Package changes include untangling ldap support, so that the package
    builds with it. There has been confusion between acl and ldap support
    upstream as well as in the package - see
    patch-include_atalk_ldapconfig.h and
    <https://github.com/Netatalk/Netatalk/commits/branch-netatalk-2-2/include/atalk/ldapconfig.h>.

--- net/netatalk22/patches/patch-include_atalk_ldapconfig.h
+++ net/netatalk22/patches/patch-include_atalk_ldapconfig.h
@@ -1,17 +1,22 @@
-$NetBSD: patch-include_atalk_ldapconfig.h,v 1.1 2014/06/11 11:03:56 hauke Exp $
+$NetBSD: patch-include_atalk_ldapconfig.h,v 1.2 2017/07/12 13:56:00 hauke Exp $
 
---- include/atalk/ldapconfig.h.orig	2012-09-10 09:34:52.000000000 +0000
+Correct an erroneous patch that was unfortunately submitted upstream.
+
+Providing an empty header because we do not support ACLs only serves
+to break ldap support.
+							     
+--- include/atalk/ldapconfig.h.orig	2017-07-09 11:03:31.000000000 +0000
 +++ include/atalk/ldapconfig.h
-@@ -1,4 +1,5 @@
+@@ -1,5 +1,4 @@
  #ifdef HAVE_LDAP
-+#ifdef HAVE_ACLS
+-#ifdef HAVE_ACLS
  
  #ifndef LDAPCONFIG_H
  #define LDAPCONFIG_H
-@@ -39,5 +40,5 @@ extern struct pref_array prefs_array[];
+@@ -40,5 +39,5 @@ extern struct pref_array prefs_array[];
  extern int ldap_config_valid;
  
  #endif /* LDAPCONFIG_H */
--
-+#endif /* HAVE_ACLS */
+-#endif /* HAVE_ACLS */
++
  #endif /* HAVE_LDAP */
