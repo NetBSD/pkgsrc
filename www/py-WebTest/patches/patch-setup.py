35b692fcf28222175b791f647253cba863c1556d
adam <adam@pkgsrc.org>
2017-07-13 06:09:38 +0000

    Fixed patch-setup.py

--- www/py-WebTest/patches/patch-setup.py
+++ www/py-WebTest/patches/patch-setup.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-setup.py,v 1.1 2017/07/11 09:18:01 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2017/07/13 06:09:38 adam Exp $
 
 Allow newer py-nose versions.
 
---- /tmp/pkgsrc/www/py-WebTest/work/WebTest-2.0.27/setup.py.orig	2017-07-11 08:25:23.000000000 +0000
-+++ /tmp/pkgsrc/www/py-WebTest/work/WebTest-2.0.27/setup.py
+--- setup.py.orig	2017-07-11 08:25:23.000000000 +0000
++++ setup.py
 @@ -15,7 +15,7 @@ install_requires = [
  ]
  
