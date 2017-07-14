c160103e9a4e88a125509646d5f626f2d4bb3482
adam <adam@pkgsrc.org>
2017-07-14 08:12:33 +0000

    20170712: x86: AVX-512 pixel_avg_weight_w8 x86: AVX-512 pixel_avg_weight_w16 x86: AVX-512 sub8x16_dct_dc x86: AVX-512 sub8x8_dct_dc x86: AVX-512 add8x8_idct x86: AVX-512 sub16x16_dct x86: AVX-512 sub8x8_dct x86: AVX-512 sub4x4_dct x86: AVX-512 mbtree_propagate_list x86inc: Add aesni cpuflag define

--- multimedia/x264-devel/patches/patch-encoder_analyse.c
+++ multimedia/x264-devel/patches/patch-encoder_analyse.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-encoder_analyse.c,v 1.1 2017/06/16 05:39:23 adam Exp $
+$NetBSD: patch-encoder_analyse.c,v 1.2 2017/07/14 08:12:34 adam Exp $
 
 Use logf() instead of log2f.
 
---- encoder/analyse.c.orig	2017-06-15 20:45:03.000000000 +0000
+--- encoder/analyse.c.orig	2017-07-12 20:45:03.000000000 +0000
 +++ encoder/analyse.c
-@@ -306,7 +306,7 @@ int x264_analyse_init_costs( x264_t *h )
+@@ -309,7 +309,7 @@ int x264_analyse_init_costs( x264_t *h )
  
      logs[0] = 0.718f;
-     for( int i = 1; i <= 2*4*2048; i++ )
+     for( int i = 1; i <= 2*4*mv_range; i++ )
 -        logs[i] = log2f( i+1 ) * 2.0f + 1.718f;
 +        logs[i] = logf(i+1)*2.88539f + 1.718f;
  
