2855b5d3faf9497b68e2e5275df821d2e5c7adb7
joerg <joerg@pkgsrc.org>
2017-07-14 12:55:45 +0000

    Collect patches for xapian in a common subdirectory. Put distinfo for modules into a separate file as well. Don't hard-code -lstdc++ for broken ancient OpenBSD versions of GCC. Sync p5-Xapian PLIST with reality.

--- textproc/xapian/module.mk
+++ textproc/xapian/module.mk
@@ -1,4 +1,4 @@
-# $NetBSD: module.mk,v 1.1 2017/07/10 17:29:58 schmonz Exp $
+# $NetBSD: module.mk,v 1.2 2017/07/14 12:55:45 joerg Exp $
 
 .include "../../textproc/xapian/Makefile.common"
 
@@ -6,6 +6,7 @@ DISTNAME=		xapian-bindings-${VERSION}
 PKGNAME_MODULE=		${DISTNAME:S/-bindings-/-/}
 
 DESCR_SRC=		${PKGDIR}/../../textproc/xapian/DESCR
-DISTINFO_FILE=		${PKGDIR}/../../textproc/xapian/distinfo
+DISTINFO_FILE=		${PKGDIR}/../../textproc/xapian/distinfo-bindings
+PATCHDIR=		${PKGDIR}/../../textproc/xapian/patches-bindings
 
 .include "../../textproc/xapian/buildlink3.mk"
