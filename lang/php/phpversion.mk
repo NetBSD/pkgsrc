5ff1c56dcef94665a8309677e472e76f460ea067
bsiegert <bsiegert@pkgsrc.org>
2017-07-15 19:23:43 +0000

    Pullup ticket #5509 - requested by taca lang/php56: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.184
    - lang/php56/Makefile                                           1.14
    - lang/php56/distinfo                                           1.42
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Fri Jul  7 03:13:48 UTC 2017
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php56: Makefile distinfo
    
       Log Message:
       Update php56 to 5.6.31.
    
       06 Jul 2017, PHP 5.6.31
    
       - Core:
         . Fixed bug #73807 (Performance problem with processing post request over
           2000000 chars). (Nikita)
         . Fixed bug #74111 (Heap buffer overread (READ: 1) finish_nested_data from
           unserialize). (Nikita)
         . Fixed bug #74603 (PHP INI Parsing Stack Buffer Overflow Vulnerability).
           (Stas)
         . Fixed bug #74819 (wddx_deserialize() heap out-of-bound read via
           php_parse_date()). (Derick)
    
       - GD:
         . Fixed bug #74435 (Buffer over-read into uninitialized memory). (cmb)
    
       - mbstring:
         . Add oniguruma upstream fix (CVE-2017-9224, CVE-2017-9226, CVE-2017-9227,
           CVE-2017-9228, CVE-2017-9229) (Remi, Mamoru TASAKA)
    
       - OpenSSL:
         . Fixed bug #74651 (negative-size-param (-1) in memcpy in zif_openssl_seal()).
           (Stas)
    
       - PCRE:
         . Fixed bug #74087 (Segmentation fault in PHP7.1.1(compiled using the bundled PCRE library)).
           (Stas)
    
       - WDDX:
          . Fixed bug #74145 (wddx parsing empty boolean tag leads to SIGSEGV). (Stas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.181.2.2 2017/07/15 19:16:20 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.181.2.3 2017/07/15 19:23:43 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -87,7 +87,7 @@
 PHPVERSION_MK=	defined
 
 # Define each PHP's version.
-PHP56_VERSION=	5.6.30
+PHP56_VERSION=	5.6.31
 PHP70_VERSION=	7.0.21
 PHP71_VERSION=	7.1.7
 
