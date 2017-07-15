282072e9e6135c00038016d5a7822b41a37b1672
wiz <wiz@pkgsrc.org>
2017-07-15 19:10:31 +0000

    Fix pkglint warnings.

--- x11/wxGTK30/hacks.mk
+++ x11/wxGTK30/hacks.mk
@@ -1,10 +1,10 @@
-# $NetBSD: hacks.mk,v 1.5 2016/08/20 13:19:21 gdt Exp $
+# $NetBSD: hacks.mk,v 1.6 2017/07/15 19:10:31 wiz Exp $
 
 ### [Mon Dec 14 16:58:00 MET 2015 : bouyer ]
 ### g++-4.8 on NetBSD/i386 fails with an internal compiler error
 ### when PCH is used. Be conservative and disable on all MACHINE_ARCH
-### 
-.if ${OPSYS} == "NetBSD" 
+###
+.if ${OPSYS} == "NetBSD"
 .  include "../../mk/compiler.mk"
 .  if !empty(PKGSRC_COMPILER:Mgcc) && \
       (!empty(CC_VERSION:Mgcc-4.8.*) || !empty(CC_VERSION:Mgcc-5.*))
