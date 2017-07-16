ece42bd0616ad110128db2c5cfe68ee8e15879b3
christos <christos@pkgsrc.org>
2017-07-16 16:36:09 +0000

    fix paths in patches.

--- emulators/gxemul/patches/patch-src_console_console.cc
+++ emulators/gxemul/patches/patch-src_console_console.cc
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_console_console.cc,v 1.1 2017/07/16 14:10:58 christos Exp $
+$NetBSD: patch-src_console_console.cc,v 1.2 2017/07/16 16:36:09 christos Exp $
 
 Change console_charavail() to return the number of characters available
 in the FIFO instead of 0/1.
 
---- /usr/src/gxemul/gxemul-0.6.0.1/src/console/console.cc	2014-08-17 04:45:15.000000000 -0400
-+++ ./console.cc	2017-07-16 10:04:04.603287788 -0400
+--- src/console/console.cc.orig	2014-08-17 04:45:15.000000000 -0400
++++ src/console/console.cc	2017-07-16 10:04:04.603287788 -0400
 @@ -328,10 +328,19 @@
  }
  
