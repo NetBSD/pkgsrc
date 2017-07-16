787db4932e90a0eff2cab62858c8e89eff212f9d
adam <adam@pkgsrc.org>
2017-07-16 21:01:06 +0000

    2.1.3: Improve build system Add C++17 adaptors std::optional and std::string_view. Improve cross platform configuration Remove some warnings

--- devel/msgpack/buildlink3.mk
+++ devel/msgpack/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/04/06 13:01:30 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/07/16 21:01:06 adam Exp $
 #
 
 BUILDLINK_TREE+=	msgpack
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	msgpack
 .if !defined(MSGPACK_BUILDLINK3_MK)
 MSGPACK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.msgpack+=	msgpack>=0.5.5
+BUILDLINK_API_DEPENDS.msgpack+=	msgpack>=1.2.0
 BUILDLINK_PKGSRCDIR.msgpack?=	../../devel/msgpack
 .endif	# MSGPACK_BUILDLINK3_MK
 
