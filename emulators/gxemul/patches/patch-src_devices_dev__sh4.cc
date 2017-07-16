357e685ba112ddec70b0dde40b07a1474c6a5122
christos <christos@pkgsrc.org>
2017-07-16 16:36:09 +0000

    fix paths in patches.

--- emulators/gxemul/patches/patch-src_devices_dev__sh4.cc
+++ emulators/gxemul/patches/patch-src_devices_dev__sh4.cc
@@ -1,9 +1,9 @@
-$NetBSD: patch-src_devices_dev__sh4.cc,v 1.1 2017/07/16 14:10:58 christos Exp $
+$NetBSD: patch-src_devices_dev__sh4.cc,v 1.2 2017/07/16 16:36:09 christos Exp $
 
 Put the actual number of characters waiting in the FIFO instead of 1 or 0
 
---- /usr/src/gxemul/gxemul-0.6.0.1/src/devices/dev_sh4.cc	2014-08-17 04:45:12.000000000 -0400
-+++ ./dev_sh4.cc	2017-07-16 09:58:19.288954403 -0400
+--- src/devices/dev_sh4.cc.orig	2014-08-17 04:45:12.000000000 -0400
++++ src/devices/dev_sh4.cc	2017-07-16 09:58:19.288954403 -0400
 @@ -1703,8 +1703,8 @@
  
  	case SH4_SCIF_BASE + SCIF_FDR:
