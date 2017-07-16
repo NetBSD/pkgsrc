8b3ff22bc78054cf3aab50dea18abab0371ec75d
leot <leot@pkgsrc.org>
2017-07-16 12:06:25 +0000

    Add stereo/mono fallback logic for `oss' audio output.
    Previously playing files with more audio channels available than the ones
    provided by audio device resulted in muted audio. A possible workaround to that
    was forcing the `--audio-channels=2' or similar.
    
    Thanks to <mrg> for kindly provided a reliable test (file|case) for that!
    
    While here also get rid of a trailing whitespace in options.mk (spotted by
    pkglint!)
    
    Bump PKGREVISION.

---
+++ multimedia/mpv/patches/patch-audio_out_ao__oss.c
@@ -0,0 +1,40 @@
+$NetBSD: patch-audio_out_ao__oss.c,v 1.4 2017/07/16 12:06:25 leot Exp $
+
+ioctl(..., SNDCTL_DSP_CHANNELS, &nchannels) for not supported nchannels does not
+return an error and instead set nchannels to the default value. Instead of
+failing with no audio, fallback to stereo or mono.
+
+Fallback logic inspired by `OSS v3 Programmer's guide', p. 34.
+
+--- audio/out/ao_oss.c.orig	2017-02-12 01:31:16.000000000 +0000
++++ audio/out/ao_oss.c
+@@ -345,13 +345,26 @@ static int reopen_device(struct ao *ao, 
+         // We only use SNDCTL_DSP_CHANNELS for >2 channels, in case some drivers don't have it
+         if (reqchannels > 2) {
+             int nchannels = reqchannels;
+-            if (ioctl(p->audio_fd, SNDCTL_DSP_CHANNELS, &nchannels) == -1 ||
+-                nchannels != reqchannels)
+-            {
++            if (ioctl(p->audio_fd, SNDCTL_DSP_CHANNELS, &nchannels) == -1) {
+                 MP_ERR(ao, "Failed to set audio device to %d channels.\n",
+                        reqchannels);
+                 goto fail;
+             }
++            if (nchannels != reqchannels) {
++                // Fallback to stereo or mono
++                int c;
++                for (nchannels = c = 2; c >= 1; c--, nchannels--) {
++                    if (ioctl(p->audio_fd, SNDCTL_DSP_CHANNELS, &c) == -1) {
++                        MP_ERR(ao, "Failed to set audio device to %d channels.\n", c);
++                        goto fail;
++                    }
++                    if (c == nchannels)
++                        break;
++                }
++                if (!ao_chmap_sel_get_def(ao, &sel, &channels, c))
++                    goto fail;
++                MP_WARN(ao, "using %d channels (requested: %d)\n", channels.num, reqchannels);
++            }
+         } else {
+             int c = reqchannels - 1;
+             if (ioctl(p->audio_fd, SNDCTL_DSP_STEREO, &c) == -1) {
