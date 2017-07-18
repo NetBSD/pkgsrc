5b9a6fb26dac4f11439c4b8211e57cee8d608e4f
adam <adam@pkgsrc.org>
2017-07-18 13:43:39 +0000

    Python 3.6.2 is the second maintenance release of Python 3.6, which was initially released in 2016-12 to great interest.   With the release of 3.6.2, we are now providing the second set of bugfixes and documentation updates to 3.6.  Detailed information about the changes made in 3.6.2 can be found in its change log.

--- lang/python36/dist.mk
+++ lang/python36/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.2 2017/03/30 11:42:58 adam Exp $
+# $NetBSD: dist.mk,v 1.3 2017/07/18 13:43:39 adam Exp $
 
-PY_DISTVERSION=	3.6.1
+PY_DISTVERSION=	3.6.2
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python36/distinfo
