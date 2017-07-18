7f669a9ef3f4d8dd17e03b2ec71c9edb0f539132
kim <kim@pkgsrc.org>
2017-07-18 23:02:18 +0000

    Upgrade misc/screen to version 4.6.1
    Version 4.6.1 (10/07/2017):
      * Fixes:
        - problems with starting session in some cases
        - parallel make install
        - segfault when querying info on nonUTF locale
    
    Version 4.6.0 (28/06/2017):
      * Update Unicode wide tables to 9.0
      * Support more serial speeds
      * Improved namespaces support
      * Migrate from fifos to sockets
      * Start viewing scrollback at first line of output

--- misc/screen/patches/patch-socket.c
+++ misc/screen/patches/patch-socket.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-socket.c,v 1.2 2015/09/01 13:34:47 kim Exp $
+$NetBSD: patch-socket.c,v 1.3 2017/07/18 23:02:18 kim Exp $
 
---- socket.c.orig	2015-06-28 21:22:56.000000000 +0000
-+++ socket.c	2015-09-01 12:42:34.000000000 +0000
-@@ -32,9 +32,7 @@
+--- socket.c.orig	2017-07-10 19:26:25.000000000 +0000
++++ socket.c	2017-07-18 22:35:40.000000000 +0000
+@@ -34,9 +34,7 @@
+ #include <sys/stat.h>
  #include <fcntl.h>
- #if !defined(NAMEDPIPE)
  # include <sys/socket.h>
 -# ifdef _OpenBSD_
 -#  include <sys/uio.h>
 -# endif
 +# include <sys/uio.h>
  # include <sys/un.h>
- #endif
  
+ #ifndef SIGINT
