8f520f92f5654980765121f5b566bc6b36afee0a
leot <leot@pkgsrc.org>
2017-07-20 10:39:25 +0000

    Update x11/dunst to 1.2.0
    pkgsrc changes:
    - dunst now also needs x11/gtk2 and x11/libXrandr
    
    Changes:
    ## 1.2.0 - 2017-07-12
    ### Added
    - `always_run_script` option to run script even if a notification is suppressed
    - Support for more icon file types
    - Support for raw icons
    - `hide_duplicate_count` option to hide the number of duplicate notifications
    - Support for per-urgency frame colours
    - `markup` setting for more fine-grained control over how markup is handled
    - `history_ignore` rule action to exclude a notification from being added to the
      history
    - Support for setting the dpi value dunst will use for font rendering via the
      `Xft.dpi` X resource
    - Experimental support for per-monitor dpi calculation
    - `max_icon_size` option to scale down icons if they exceed a certain size
    - Middle click on notifications can be used to trigger actions
    - Systemd service file, installed by default
    - `%n` format flag for getting progress value without any extra characters
    
    ### Changed
    - Text and icons are now centred vertically
    - Notifications aren't considered duplicate if urgency or icons differ
    - The maximum length of a notification is limited to 5000 characters
    - The frame width and color settings were moved to the global section as
      `frame_width` and `frame_color` respectively
    - Dropped Xinerama in favour of RandR, Xinerama can be enabled with the
      `-force_xinerama` option if needed
    
    ### Deprecated
    - `allow_markup` is deprecated with `markup` as its replacement
    - The urgency specific command line flags have been deprecated with no
      replacement, respond to issue #328 on the bug tracker if you depend on them
    
    ### Fixed
    - Infinite loop if there are 2 configuration file sections with the same name
    - URLs with dashes and underscores in them are now parsed properly
    - Many memory leaks
    - Category based rules were applied without actually matching
    - dmenu command not parsing quoted arguments correctly
    - Icon alignment with dynamic width
    - Issue when loading configuration files with very long lines
    - '\n' is no longer expanded to a newline inside notification text
    - Notification window wasn't redrawn if obscured on systems without a compositor
    - `ignore_newline` now works regardless of the markup setting
    - dmenu process being left as a zombie if no option was selected
    - Crash when opening urls parsed from `<a href="">` tags

--- x11/dunst/patches/patch-config.mk
+++ x11/dunst/patches/patch-config.mk
@@ -1,13 +1,15 @@
-$NetBSD: patch-config.mk,v 1.1 2016/08/26 17:16:09 joerg Exp $
+$NetBSD: patch-config.mk,v 1.2 2017/07/20 10:39:25 leot Exp $
 
---- config.mk.orig	2016-08-26 13:34:57.775528056 +0000
+Do not manually provide an undefined (and not needed) ${X11LIB} to LDFLAGS.
+
+--- config.mk.orig	2017-07-12 08:22:04.000000000 +0000
 +++ config.mk
-@@ -37,7 +37,7 @@ endif
+@@ -42,7 +42,7 @@ endif
  # includes and libs
  INCS := $(shell ${PKG_CONFIG} --cflags ${pkg_config_packs})
  CFLAGS += ${INCS}
--LDFLAGS += -lm -L${X11LIB} -lXss ${XINERAMALIBS} $(shell ${PKG_CONFIG} --libs ${pkg_config_packs})
-+LDFLAGS += -lm -lXss ${XINERAMALIBS} $(shell ${PKG_CONFIG} --libs ${pkg_config_packs})
+-LDFLAGS += -lm -L${X11LIB} -lXss $(shell ${PKG_CONFIG} --libs ${pkg_config_packs})
++LDFLAGS += -lm -lXss $(shell ${PKG_CONFIG} --libs ${pkg_config_packs})
  
  # only make this an fatal error when where not cleaning
  ifneq (clean, $(MAKECMDGOALS))
