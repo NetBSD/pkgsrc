320f4b3c9514fd5fde8127ff9870e0bebb8f3cd4
jaapb <jaapb@pkgsrc.org>
2017-07-21 10:29:06 +0000

    Added package devel/ocaml-migrate-parsetree, a library to migrate parse trees between different versions of OCaml. A prerequisite for an upgrade of devel/ocaml-ppx_tools.

---
+++ devel/ocaml-migrate-parsetree/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1 2017/07/21 10:29:06 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-migrate-parsetree
+
+.if !defined(OCAML_MIGRATE_PARSETREE_BUILDLINK3_MK)
+OCAML_MIGRATE_PARSETREE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-migrate-parsetree+=	ocaml-migrate-parsetree>=1.0.1
+BUILDLINK_PKGSRCDIR.ocaml-migrate-parsetree?=	../../devel/ocaml-migrate-parsetree
+.endif	# OCAML_MIGRATE_PARSETREE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-migrate-parsetree
