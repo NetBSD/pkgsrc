cd8d6b06d02837d9b932aab5955ab6ec5440b3bd
jaapb <jaapb@pkgsrc.org>
2017-07-21 14:08:22 +0000

    Updated package to latest version, 0.9.0 (caution: new versioning scheme!) There does not seem to be an up-to-date changelog, but one change is that the build scheme now uses ocaml-jbuilder.

--- devel/ocaml-ppx_type_conv/buildlink3.mk
+++ devel/ocaml-ppx_type_conv/buildlink3.mk
@@ -1,15 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/07/11 11:35:42 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/21 14:08:22 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_type_conv
 
 .if !defined(OCAML_PPX_TYPE_CONV_BUILDLINK3_MK)
 OCAML_PPX_TYPE_CONV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ppx_type_conv+=	ocaml-ppx_type_conv>=114.01.04nb2
+BUILDLINK_API_DEPENDS.ocaml-ppx_type_conv+=	ocaml-ppx_type_conv>=0.9.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_type_conv?=	../../devel/ocaml-ppx_type_conv
 
-#.include "../../devel/ocaml-ppx_driver/buildlink3.mk"
-.include "../../devel/ocaml-ppx_tools/buildlink3.mk"
 .endif	# OCAML_PPX_TYPE_CONV_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ocaml-ppx_type_conv
