49f14d9c46f47f0472ce636557910400344c0be7
jaapb <jaapb@pkgsrc.org>
2017-07-21 13:27:52 +0000

    Updated package to new version (caution: new versioning scheme!), 0.9.0. There does not seem to be a changelog - one change is in the build system which now uses ocaml-jbuilder.

--- devel/ocaml-ppx_core/buildlink3.mk
+++ devel/ocaml-ppx_core/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/07/11 14:15:10 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/21 13:27:52 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_core
 
 .if !defined(OCAML_PPX_CORE_BUILDLINK3_MK)
 OCAML_PPX_CORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ppx_core+=	ocaml-ppx_core>=113.24.00
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_core+=	ocaml-ppx_core>=114.01.04nb2
+BUILDLINK_API_DEPENDS.ocaml-ppx_core+=	ocaml-ppx_core>=0.9.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_core+=	ocaml-ppx_core>=0.9.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_core?=	../../devel/ocaml-ppx_core
 .endif	# OCAML_PPX_CORE_BUILDLINK3_MK
 
