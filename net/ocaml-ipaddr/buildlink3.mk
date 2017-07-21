3282a93aa42efe41e62d9efc93428f5bcc07c133
jaapb <jaapb@pkgsrc.org>
2017-07-21 14:21:26 +0000

    Updated package to latest version, 2.8.0. Changes include:
    Port to Jbuilder (#65 @vbmithr @avsm). There should be no observable changes,
    except that Ipaddr_unix is now in a separate subdirectory. This means that
    packages that implicitly depended on the module without including the
    ocamlfind ipaddr.unix package may now fail. Just adding the ocamlfind
    dependency will fix it, and is backwards compatible with older Ipaddr
    releases.
    
    Minimum version of OCaml required is now 4.03.0 (formerly was 4.02.2), due to
    the use of recent ppx_sexp_conv with Jbuilder also having that as the minimum
    supported compiler version.

--- net/ocaml-ipaddr/buildlink3.mk
+++ net/ocaml-ipaddr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2017/07/11 12:28:51 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2017/07/21 14:21:26 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ipaddr
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ipaddr
 OCAML_IPADDR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ipaddr+=	ocaml-ipaddr>=2.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-ipaddr?=	ocaml-ipaddr>=2.7.2
+BUILDLINK_ABI_DEPENDS.ocaml-ipaddr?=	ocaml-ipaddr>=2.8.0
 BUILDLINK_PKGSRCDIR.ocaml-ipaddr?=	../../net/ocaml-ipaddr
 BUILDLINK_FILES.ocaml-ipaddr+=	lib/ocaml/site-lib/*
 
