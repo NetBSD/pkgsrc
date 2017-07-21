826383e11b52266c520dc22d20ed47ec8cdc37d9
schmonz <schmonz@pkgsrc.org>
2017-07-21 04:07:01 +0000

    Update to 1.03nb29. pkgsrc changes:
    Remove unneeded options:
    
    - Unconditionally apply netqmail (which includes a local patch; remove it)
    
    - Unconditionally apply bigdns, maildiruniq, outgoingip, rcptcheck, remote
    
    - Unconditionally apply the TLS + SMTP AUTH _patch_ (not the options)
    
    - Record all applied patches (mandatory and optional) in QMAILPATCHES
    
    - Remove badrcptto, qregex, realrcptto, viruscan (moved to rejectutils)
    
    
    
    Simplify packaging:
    
    - Extract a standalone patch <https://schmonz.com/qmail/rejectutils> to
      repackage the mutually conflicting recipient- and content-checking
      patches as separate programs, along with wrappers for running checks
      in sequence
    
    - Extract a standalone patch <https://schmonz.com/qmail/destdir> to
      build to a staging area, as non-root, without hardcoded IDs
    
    - Run the destdir patch's `install-destdir` to make or repair the queue
      and set special file permissions, obviating the need for a dependency
      on mail/queue-fix and handcrafted SPECIAL_PERMS
    
    - While here, run `instcheck` to ensure we've installed just like `make
      setup check` as root would have
    
    - Install INSTALL and SENDMAIL docs under their original names,
      even on Darwin
    
    - Avoid building catpages, since we don't install them, and remove nroff
      from USE_TOOLS
    
    
    Default-enable more useful options:
    
    - "eai" (new) permits UTF-8 almost everywhere in email
    
    - "qmail-rejectutils" (new) adds several tools for selectively
      rejecting messages
    
    - "syncdir" forces synchronous link() and related syscalls
    
    - "tls" and "sasl", instead of causing patch conflicts, cause the TLS
      and SMTP AUTH code to be included (!)

--- mail/qmail/options.mk
+++ mail/qmail/options.mk
@@ -1,181 +1,163 @@
-# $NetBSD: options.mk,v 1.41 2017/05/22 17:18:32 schmonz Exp $
+# $NetBSD: options.mk,v 1.42 2017/07/21 04:07:01 schmonz Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.qmail
-PKG_OPTIONS_OPTIONAL_GROUPS=	rcpt
-PKG_OPTIONS_GROUP.rcpt=	qmail-badrcptto qmail-qregex
-PKG_OPTIONS_GROUP.rcpt+=qmail-rcptcheck qmail-realrcptto
-PKG_SUPPORTED_OPTIONS+=	sasl syncdir tls qmail-bigdns qmail-maildiruniq
-PKG_SUPPORTED_OPTIONS+=	qmail-netqmail qmail-outgoingip qmail-remote
-PKG_SUPPORTED_OPTIONS+=	qmail-srs qmail-viruscan
-PKG_SUGGESTED_OPTIONS+=	qmail-bigdns qmail-netqmail qmail-realrcptto
+PKG_OPTIONS_VAR=		PKG_OPTIONS.qmail
+PKG_SUPPORTED_OPTIONS+=		eai qmail-rejectutils qmail-srs sasl syncdir tls
+PKG_SUGGESTED_OPTIONS+=		eai qmail-rejectutils sasl syncdir tls
+
+# For users migrating from 2017Q2; remove compatibility after 2017Q3 is branched
+QMAIL_PATCHES_ALWAYS_ON=	netqmail bigdns maildiruniq outgoingip rcptcheck remote
+QMAIL_PATCHES_REMOVED=		badrcptto qregex realrcptto viruscan
+PKG_OPTIONS_ALWAYS_ON=		${QMAIL_PATCHES_ALWAYS_ON:S/^/qmail-/}
+PKG_OPTIONS_REMOVED=		${QMAIL_PATCHES_REMOVED:S/^/qmail-/}
+PKG_SUPPORTED_OPTIONS+=		${PKG_OPTIONS_ALWAYS_ON} ${PKG_OPTIONS_REMOVED}
+
+# Formerly optional patches, now unconditionally applied:
+QMAILPATCHES=			netqmail:${DEFAULT_DISTFILES}
+
+QMAILPATCHES+=			tls-smtpauth:${TLSSASL_PATCH}
+TLSSASL_PATCH=			netqmail-1.05-tls-smtpauth-20070417.patch
+PATCHFILES+=			${TLSSASL_PATCH}
+SITES.${TLSSASL_PATCH}=		https://raw.githubusercontent.com/shupp/legacy-qmail-related/master/patches/
+
+QMAILPATCHES+=			bigdns:${BIGDNS_PATCH}
+BIGDNS_PATCH=			qmail-103.patch
+PATCHFILES+=			${BIGDNS_PATCH}
+SITES.${BIGDNS_PATCH}=		http://www.ckdhr.com/ckd/
+SITES.${BIGDNS_PATCH}+=		${MASTER_SITE_LOCAL}
+PATCH_DIST_STRIP.${BIGDNS_PATCH}=-p1
+
+QMAILPATCHES+=			destdir:${DESTDIR_PATCH}
+DESTDIR_PATCH=			netqmail-1.06-destdir-20170716.patch
+PATCHFILES+=			${DESTDIR_PATCH}
+SITES.${DESTDIR_PATCH}=		https://schmonz.com/qmail/destdir/
+
+QMAILPATCHES+=			maildiruniq:${MAILDIRUNIQ_PATCH}
+MAILDIRUNIQ_PATCH=		qmail-1.03-maildir-uniq.patch
+PATCHFILES+=			${MAILDIRUNIQ_PATCH}
+SITES.${MAILDIRUNIQ_PATCH}=	http://www.memoryhole.net/qmail/
+PATCH_DIST_STRIP.${MAILDIRUNIQ_PATCH}=-p1
+
+QMAILPATCHES+=			outgoingip:${OUTGOINGIP_PATCH}
+OUTGOINGIP_PATCH=		outgoingip.patch
+PATCHFILES+=			${OUTGOINGIP_PATCH}
+SITES.${OUTGOINGIP_PATCH}=	http://www.qmail.org/
+
+QMAILPATCHES+=			rcptcheck:${RCPTCHECK_PATCH}
+RCPTCHECK_PATCH=		netqmail-1.06-tls-smtpauth-20070417-rcptcheck-20170716.patch
+PATCHFILES+=			${RCPTCHECK_PATCH}
+SITES.${RCPTCHECK_PATCH}=	https://schmonz.com/qmail/rcptcheck/
+
+QMAILPATCHES+=			remote:${REMOTE_PATCH}
+REMOTE_PATCH=			netqmail-1.06-qmailremote-20170716.patch
+PATCHFILES+=			${REMOTE_PATCH}
+SITES.${REMOTE_PATCH}=		https://schmonz.com/qmail/remote/
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mqmail-badrcptto)
-BADRCPTTO_PATCH=	badrcptto.patch
-PATCHFILES+=		${BADRCPTTO_PATCH}
-SITES.${BADRCPTTO_PATCH}=	http://patch.be/qmail/
-PATCH_DIST_STRIP.${BADRCPTTO_PATCH}=	-p1
+# For users migrating from 2017Q2; remove compatibility after 2017Q3 is branched
+.for i in ${PKG_OPTIONS_ALWAYS_ON}
+.  if !empty(PKG_OPTIONS:M${i})
+PKG_LEGACY_OPTIONS+=		${i}
+PKG_OPTIONS_DEPRECATED_WARNINGS+="WARN: Please unset \"${i}\" option (it's now always enabled)."
+.  endif
+.endfor
+
+# For users migrating from 2017Q2; remove compatibility after 2017Q3 is branched
+.for i in ${PKG_OPTIONS_REMOVED}
+.  if !empty(PKG_OPTIONS:M${i})
+PKG_LEGACY_OPTIONS+=		${i}
+PKG_OPTIONS_DEPRECATED_WARNINGS+="WARN: Please unset \"${i}\" option (it's now always included, as a program)."
+.  endif
+.endfor
+
+.if !empty(PKG_OPTIONS:Meai)
+.  include "../../devel/libidn2/buildlink3.mk"
+QMAILPATCHES+=			eai:${EAI_PATCH}
+EAI_PATCH=			qmail-smtputf8.patch
+PATCHFILES+=			${EAI_PATCH}
+SITES.${EAI_PATCH}=		http://arnt.gulbrandsen.priv.no/qmail/
+PATCH_DIST_STRIP.${EAI_PATCH}=	-p1
 .endif
 
-.if !empty(PKG_OPTIONS:Mqmail-bigdns)
-BIGDNS_PATCH=		qmail-103.patch
-PATCHFILES+=		${BIGDNS_PATCH}
-SITES.${BIGDNS_PATCH}=	http://www.ckdhr.com/ckd/
-SITES.${BIGDNS_PATCH}+=	${MASTER_SITE_LOCAL}
-PATCH_DIST_STRIP.${BIGDNS_PATCH}=	-p1
+PLIST_VARS+=			rejectutils
+.if !empty(PKG_OPTIONS:Mqmail-rejectutils)
+QMAILPATCHES+=			rejectutils:${REJECTUTILS_PATCH}
+REJECTUTILS_PATCH=		netqmail-1.06-rejectutils-20170720.patch
+PATCHFILES+=			${REJECTUTILS_PATCH}
+SITES.${REJECTUTILS_PATCH}=	https://schmonz.com/qmail/rejectutils/
+DJB_BUILD_TARGETS+=		rejectutils
+PLIST.rejectutils=		yes
+REJECTUTILS=			qmail-qfilter-ofmipd-queue	\
+				qmail-qfilter-smtpd-queue	\
+				qmail-qfilter-viruscan		\
+				qmail-rcptcheck			\
+				qmail-rcptcheck-badrcptto	\
+				qmail-rcptcheck-qregex		\
+				qmail-rcptcheck-realrcptto
 .endif
 
-.if !empty(PKG_OPTIONS:Mqmail-maildiruniq)
-MAILDIRUNIQ_PATCH=	qmail-1.03-maildir-uniq.patch
-PATCHFILES+=		${MAILDIRUNIQ_PATCH}
-SITES.${MAILDIRUNIQ_PATCH}=	http://www.memoryhole.net/qmail/
-PATCH_DIST_STRIP.${MAILDIRUNIQ_PATCH}=	-p1
-.endif
-
-.if !empty(PKG_OPTIONS:Mqmail-netqmail)
-DISTNAME=		netqmail-1.06
-.endif
-
-.if !empty(PKG_OPTIONS:Mqmail-outgoingip)
-OUTGOINGIP_PATCH=	outgoingip.patch
-PATCHFILES+=		${OUTGOINGIP_PATCH}
-SITES.${OUTGOINGIP_PATCH}=	http://www.qmail.org/
-.endif
-
-.if !empty(PKG_OPTIONS:Mqmail-remote)
-REMOTE_PATCH=		netqmail-1.06-qmailremote-20170522.patch
-PATCHFILES+=		${REMOTE_PATCH}
-SITES.${REMOTE_PATCH}=	https://schmonz.com/2017/05/22/
-.endif
-
-PLIST_VARS+=		srs
-.PHONY: post-extract-srs post-install-srs
+PLIST_VARS+=			srs
 .if !empty(PKG_OPTIONS:Mqmail-srs)
 .  include "../../mail/libsrs2/buildlink3.mk"
-SRS_PATCH=		qmail-srs-0.8.patch
-PATCHFILES+=		${SRS_PATCH}
-SITES.${SRS_PATCH}=	http://www.mco2.com.br/opensource/download/qmail/
-PATCH_DIST_STRIP.${SRS_PATCH}=	-p1
-.  if !empty(PKG_OPTIONS:Mqmail-netqmail)
-PATCH_DIST_STRIP.${SRS_PATCH}+=	-l
-SUBST_CLASSES+=		srsnetq1 srsnetq2
-SUBST_STAGE.srsnetq1=	pre-patch
-SUBST_STAGE.srsnetq2=	post-patch
-SUBST_FILES.srsnetq1=	Makefile
-SUBST_FILES.srsnetq2=	Makefile
-SUBST_SED.srsnetq1=	-e 's|^auto_split.o env.a$$|auto_split.o|'
-SUBST_SED.srsnetq2=	-e 's|^auto_split.o$$|auto_split.o env.a|'
-SUBST_SED.srsnetq1+=	-e 's|^	substdio.a error.a str.a fs.a auto_qmail.o auto_split.o env.a$$|	substdio.a error.a str.a fs.a auto_qmail.o auto_split.o|'
-SUBST_SED.srsnetq2+=	-e 's|^	substdio.a error.a str.a fs.a auto_qmail.o auto_split.o \\$$|	substdio.a error.a str.a fs.a auto_qmail.o auto_split.o env.a \\|'
-.  endif
-SUBST_CLASSES+=		srsinclude
-SUBST_STAGE.srsinclude=	do-configure
-SUBST_FILES.srsinclude=	srs.c
-SUBST_SED.srsinclude=	-e 's|/usr/local/include/srs2.h|srs2.h|'
-SUBST_CLASSES+=		srsreadme
-SUBST_STAGE.srsreadme=	do-configure
-SUBST_FILES.srsreadme=	README.srs
-SUBST_SED.srsreadme=	-e 's,@PKG_SYSCONFDIR@,${PKG_SYSCONFDIR:Q},g'
-SPECIAL_PERMS+=		${PREFIX}/bin/srsfilter ${QMAIL_ROOT_USER} ${QMAIL_QMAIL_GROUP} 0755
-PLIST.srs=		yes
-MESSAGE_SRC+=		${PKGDIR}/MESSAGE.srs
-post-extract-srs:
-	${CP} ${FILESDIR}/README.srs ${WRKSRC}
-post-install-srs:
-	${INSTALL_DATA} ${WRKSRC}/README.srs ${DESTDIR}${DOCDIR}
-.else
-post-extract-srs:
-	${DO_NADA}
-post-install-srs:
-	${DO_NADA}
-.endif
-
-PLIST_VARS+=		qregex
-.if !empty(PKG_OPTIONS:Mqmail-qregex)
-QREGEX_PATCH=		qregex-20060423.patch
-PATCHFILES+=		${QREGEX_PATCH}
-SITES.${QREGEX_PATCH}=	http://www.arda.homeunix.net/store/qmail/
-# actually http://www.arda.homeunix.net/?ddownload=409
-PATCH_DIST_STRIP.${QREGEX_PATCH}=	-p3
-PLIST.qregex=		yes
-.endif
-
-.if !empty(PKG_OPTIONS:Mqmail-rcptcheck)
-RCPTCHECK_PATCH=	qmail-smtpd.patch
-PATCHFILES+=		${RCPTCHECK_PATCH}
-SITES.${RCPTCHECK_PATCH}=	http://www.soffian.org/downloads/qmail/
-PATCH_DIST_STRIP.${RCPTCHECK_PATCH}=	-p1
-.endif
-
-.if !empty(PKG_OPTIONS:Mqmail-realrcptto)
-REALRCPTTO_PATCH=	qmail-1.03-realrcptto-2006.12.10.patch
-PATCHFILES+=		${REALRCPTTO_PATCH}
-SITES.${REALRCPTTO_PATCH}=	http://code.dogmap.org/qmail/
-PATCH_DIST_STRIP.${REALRCPTTO_PATCH}=	-p1
-.endif
-
-PLIST_VARS+=		tls
-.if !empty(PKG_OPTIONS:Msasl) || !empty(PKG_OPTIONS:Mtls)
-.  if empty(PKG_OPTIONS:Msasl)
-PKG_OPTIONS+=		sasl
-.  endif
-.  include "../../security/openssl/buildlink3.mk"
-TLSSASL_PATCH=		netqmail-1.05-tls-smtpauth-20070417.patch
-PATCHFILES+=		${TLSSASL_PATCH}
-SITES.${TLSSASL_PATCH}=	https://raw.githubusercontent.com/shupp/legacy-qmail-related/master/patches/
-.  if !empty(PKG_OPTIONS:Mtls)
-CFLAGS+=		-DTLS=20070408	# NOTE: match what's _in_ the patch
-USE_TOOLS+=		openssl
-SUBST_CLASSES+=		tmprsadh
-SUBST_STAGE.tmprsadh=	do-configure
-SUBST_FILES.tmprsadh=	update_tmprsadh.sh
-SUBST_SED.tmprsadh=	-e 's|^export PATH=.*||'
-SUBST_SED.tmprsadh+=	-e 's|^openssl |${OPENSSL} |'
-PLIST.tls=		yes
-.  endif
+QMAILPATCHES+=			srs:${SRS_PATCH}
+SRS_PATCH=			qmail-srs-0.8.patch
+PATCHFILES+=			${SRS_PATCH}
+SITES.${SRS_PATCH}=		http://www.mco2.com.br/opensource/download/qmail/
+PATCH_DIST_STRIP.${SRS_PATCH}=	-l
+PATCH_DIST_CAT.${SRS_PATCH}=	${SED} -e 's|binm3 binm3+df|binm3 binm3+df update_tmprsadh|g' < ${SRS_PATCH}
+SUBST_CLASSES+=			srsnetq1 srsnetq2
+SUBST_STAGE.srsnetq1=		pre-patch
+SUBST_STAGE.srsnetq2=		post-patch
+SUBST_FILES.srsnetq1=		Makefile
+SUBST_FILES.srsnetq2=		Makefile
+SUBST_SED.srsnetq1=		-e 's|^auto_split.o env.a$$|auto_split.o|'
+SUBST_SED.srsnetq2=		-e 's|^auto_split.o$$|auto_split.o env.a|'
+SUBST_SED.srsnetq1+=		-e 's|^	substdio.a error.a str.a fs.a auto_qmail.o auto_split.o env.a$$|	substdio.a error.a str.a fs.a auto_qmail.o auto_split.o|'
+SUBST_SED.srsnetq2+=		-e 's|^	substdio.a error.a str.a fs.a auto_qmail.o auto_split.o \\$$|	substdio.a error.a str.a fs.a auto_qmail.o auto_split.o env.a \\|'
+SUBST_CLASSES+=			srsinclude
+SUBST_STAGE.srsinclude=		do-configure
+SUBST_FILES.srsinclude=		srs.c
+SUBST_SED.srsinclude=		-e 's|/usr/local/include/srs2.h|srs2.h|'
+SUBST_CLASSES+=			srsreadme
+SUBST_STAGE.srsreadme=		do-configure
+SUBST_FILES.srsreadme=		README.srs
+SUBST_SED.srsreadme=		-e 's|@PKG_SYSCONFDIR@|${PKG_SYSCONFDIR:Q}|g'
+PLIST.srs=			yes
+MESSAGE_SRC+=			${PKGDIR}/MESSAGE.srs
+READMES+=			README.srs
 .endif
 
 .if !empty(PKG_OPTIONS:Msyncdir)
 .  include "../../devel/syncdir/buildlink3.mk"
-SUBST_CLASSES+=		load
-SUBST_STAGE.load=	do-configure
-SUBST_FILES.load=	make-load.sh
-SUBST_SED.load=		-e '$$s|$$| ${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.syncdir}/lib -L${BUILDLINK_PREFIX.syncdir}/lib \-lsyncdir|'
+SYNCDIR_DISTFILES_cmd=		( cd ../../devel/syncdir && ${MAKE} show-var VARNAME=DISTFILES )
+QMAILPATCHES+=			syncdir:${SYNCDIR_DISTFILES_cmd:sh}
+SUBST_CLASSES+=			load
+SUBST_STAGE.load=		do-configure
+SUBST_FILES.load=		make-load.sh
+SUBST_SED.load=			-e '$$s|$$| ${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.syncdir}/lib -L${BUILDLINK_PREFIX.syncdir}/lib \-lsyncdir|'
 .  if ${OPSYS} == "Darwin"
-SUBST_SED.load+=	-e '$$s|$$| -bind_at_load|'
+SUBST_SED.load+=		-e '$$s|$$| -bind_at_load|'
 .  endif
-SUBST_MESSAGE.load=	Setting linker flags for syncdir.
+SUBST_MESSAGE.load=		Setting linker flags for syncdir.
 .endif
 
-PLIST_VARS+=		viruscan
-.PHONY: post-extract-viruscan post-install-viruscan
-.if !empty(PKG_OPTIONS:Mqmail-viruscan)
-VIRUSCAN_PATCH=		qmail-smtpd-viruscan-1.3.patch
-VIRUSCAN_LOG_PATCH=	qmail-smtpd-viruscan-logging.patch
-PATCHFILES+=		${VIRUSCAN_PATCH} ${VIRUSCAN_LOG_PATCH}
-SITES.${VIRUSCAN_PATCH}=	http://www.qmail.org/
-SITES.${VIRUSCAN_LOG_PATCH}=	${MASTER_SITE_LOCAL}
-PATCH_DIST_STRIP.${VIRUSCAN_PATCH}=	-p1
-PATCH_DIST_STRIP.${VIRUSCAN_LOG_PATCH}=	-p1
-SUBST_CLASSES+=		viruscan
-SUBST_STAGE.viruscan=	do-configure
-SUBST_FILES.viruscan=	qmail-smtpd.c
-SUBST_SED.viruscan=	-e 's|qmail-smtpd: ||g'
-PLIST.viruscan=		yes
-post-extract-viruscan:
-	${SED} -e '1,15d' -e '34,$$d'		\
-		< ${DISTDIR}/${VIRUSCAN_PATCH}	\
-		> ${WRKSRC}/signatures
-	${CHMOD} 644 ${WRKSRC}/signatures
-post-install-viruscan:
-	${INSTALL_DATA} ${WRKSRC}/signatures ${DESTDIR}/${EGDIR}/control
-.  for i in control/signatures
-CONF_FILES+=		${EGDIR}/${i} ${PKG_SYSCONFDIR}/${i}
-.  endfor
+PLIST_VARS+=			tls
+.if !empty(PKG_OPTIONS:Msasl) || !empty(PKG_OPTIONS:Mtls)
+.  if empty(PKG_OPTIONS:Msasl)
+PKG_OPTIONS+=			sasl
+.  endif
+.  include "../../security/openssl/buildlink3.mk"
+.  if !empty(PKG_OPTIONS:Mtls)
+CFLAGS+=			-DTLS=20070408	# NOTE: match what's _in_ the patch
+USE_TOOLS+=			openssl
+SUBST_CLASSES+=			tmprsadh
+SUBST_STAGE.tmprsadh=		do-configure
+SUBST_FILES.tmprsadh=		update_tmprsadh.sh
+SUBST_SED.tmprsadh=		-e 's|^export PATH=.*||'
+SUBST_SED.tmprsadh+=		-e 's|^openssl |${OPENSSL} |'
+PLIST.tls=			yes
+.  endif
 .else
-post-extract-viruscan:
-	${DO_NADA}
-post-install-viruscan:
-	${DO_NADA}
+BUILDLINK_TRANSFORM+=		rm:-lssl
+BUILDLINK_TRANSFORM+=		rm:-lcrypto
 .endif
