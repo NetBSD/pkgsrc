74646589d204ae775eeb0517291f7ec89b0fb1d4
jaapb <jaapb@pkgsrc.org>
2017-07-21 13:45:42 +0000

    Updated package to latest version, 0.9.1 (caution: new versioning scheme!) There does not seem to be a changelog, but the build system now uses ocaml-jbuilder.

--- devel/ocaml-ppx_driver/buildlink3.mk
+++ devel/ocaml-ppx_driver/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/07/11 14:19:22 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/21 13:45:42 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_driver
 
 .if !defined(OCAML_PPX_DRIVER_BUILDLINK3_MK)
 OCAML_PPX_DRIVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ppx_driver+=	ocaml-ppx_driver>=113.24.00
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_driver+=	ocaml-ppx_driver>=113.45.68nb2
+BUILDLINK_API_DEPENDS.ocaml-ppx_driver+=	ocaml-ppx_driver>=0.9.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_driver+=	ocaml-ppx_driver>=0.9.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_driver?=	../../devel/ocaml-ppx_driver
 
 #.include "../../devel/ocaml-ppx_optcomp/buildlink3.mk"
