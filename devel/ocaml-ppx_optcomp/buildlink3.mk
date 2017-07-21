5e34ff19db2a0c36cd287954dfbc0124f1384385
jaapb <jaapb@pkgsrc.org>
2017-07-21 13:34:18 +0000

    Updated devel/ppx_optcomp to latest version, 0.9.0 (caution: new versioning scheme!). There does not seem to be a changelog, but one change is in the build system: ocaml-jbuilder is used now.

--- devel/ocaml-ppx_optcomp/buildlink3.mk
+++ devel/ocaml-ppx_optcomp/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/07/11 14:15:50 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/21 13:34:18 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_optcomp
 
 .if !defined(OCAML_PPX_OPTCOMP_BUILDLINK3_MK)
 OCAML_PPX_OPTCOMP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ppx_optcomp+=	ocaml-ppx_optcomp>=113.24.00
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_optcomp+=	ocaml-ppx_optcomp>=114.08.09nb2
+BUILDLINK_API_DEPENDS.ocaml-ppx_optcomp+=	ocaml-ppx_optcomp>=0.9.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_optcomp+=	ocaml-ppx_optcomp>=0.9.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_optcomp?=	../../devel/ocaml-ppx_optcomp
 
 .include "../../devel/ocaml-ppx_core/buildlink3.mk"
