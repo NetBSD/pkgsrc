9010ec0cabbbb763341fcf4dd1f7c22e73327e06
wiz <wiz@pkgsrc.org>
2017-07-22 19:25:38 +0000

    Fix previous: allow newer versions of some dependencies.

--- devel/py-requests/patches/patch-setup.py
+++ devel/py-requests/patches/patch-setup.py
@@ -1,10 +1,19 @@
-$NetBSD: patch-setup.py,v 1.1 2017/05/29 10:43:42 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2017/07/22 19:25:38 wiz Exp $
 
-Allow newer pytest-httpbin (used only for testing).
+Allow newer versions of dependencies.
 
---- setup.py.orig	2017-05-29 09:48:14.000000000 +0000
+--- setup.py.orig	2017-06-14 17:44:15.000000000 +0000
 +++ setup.py
-@@ -44,7 +44,7 @@ requires = [
+@@ -42,13 +42,13 @@ if sys.argv[-1] == 'publish':
+ packages = ['requests']
+ 
+ requires = [
+-    'chardet>=3.0.2,<3.1.0',
+-    'idna>=2.5,<2.6',
+-    'urllib3>=1.21.1,<1.22',
++    'chardet>=3.0.2',
++    'idna>=2.5',
++    'urllib3>=1.21.1',
      'certifi>=2017.4.17'
  
  ]
