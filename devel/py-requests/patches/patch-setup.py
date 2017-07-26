32b5d5cd7e08541037bdeaafa011b8cc8209acdf
adam <adam@pkgsrc.org>
2017-07-26 16:43:39 +0000

    2.18.2:
    Bugfixes
    * requests.help no longer fails on Python 2.6 due to the absence of ssl.OPENSSL_VERSION_NUMBER.
    
    Dependencies
    * We now support urllib3 v1.22.

--- devel/py-requests/patches/patch-setup.py
+++ devel/py-requests/patches/patch-setup.py
@@ -1,8 +1,8 @@
-$NetBSD: patch-setup.py,v 1.2 2017/07/22 19:25:38 wiz Exp $
+$NetBSD: patch-setup.py,v 1.3 2017/07/26 16:43:39 adam Exp $
 
 Allow newer versions of dependencies.
 
---- setup.py.orig	2017-06-14 17:44:15.000000000 +0000
+--- setup.py.orig	2017-07-25 15:15:36.000000000 +0000
 +++ setup.py
 @@ -42,13 +42,13 @@ if sys.argv[-1] == 'publish':
  packages = ['requests']
@@ -10,7 +10,7 @@ Allow newer versions of dependencies.
  requires = [
 -    'chardet>=3.0.2,<3.1.0',
 -    'idna>=2.5,<2.6',
--    'urllib3>=1.21.1,<1.22',
+-    'urllib3>=1.21.1,<1.23',
 +    'chardet>=3.0.2',
 +    'idna>=2.5',
 +    'urllib3>=1.21.1',
