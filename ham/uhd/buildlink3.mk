974e20ac17e9ed07095e81da8689fdc6cdeded87
wiz <wiz@pkgsrc.org>
2017-07-31 21:09:27 +0000

    Use tabs consistently. Fixes a pkglint warning.

--- ham/uhd/buildlink3.mk
+++ ham/uhd/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/01/27 06:38:49 dbj Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/07/31 21:09:27 wiz Exp $
 
-BUILDLINK_TREE+=        uhd
+BUILDLINK_TREE+=	uhd
 
 .if !defined(UHD_BUILDLINK3_MK)
 UHD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.uhd+=    uhd>=2.22
-BUILDLINK_PKGSRCDIR.uhd?=              ../../ham/uhd
+BUILDLINK_API_DEPENDS.uhd+=	uhd>=2.22
+BUILDLINK_PKGSRCDIR.uhd?=	../../ham/uhd
 
 # gnuradio-uhd FindUHD.cmake uses this env var as a hint for finding uhd
 CONFIGURE_ENV+=		UHD_DIR=${BUILDLINK_DIR:Q}
 
 .endif # UHD_BUILDLINK3_MK
 
-BUILDLINK_TREE+=        -uhd
+BUILDLINK_TREE+=	-uhd
