d4b42bb855636f1195304b6761acbe1c4835de4f
kleink <kleink@pkgsrc.org>
2017-08-01 15:36:26 +0000

    Update py-weasyprint to 0.39.
    Version 0.39
    ------------
    
    Released on 2017-06-24.
    
    Bug fixes:
    
    * Fix the use of WeasyPrint's URL fetcher with CairoSVG.
    
    
    Version 0.38
    ------------
    
    Released on 2017-06-16.
    
    Bug fixes:
    
    * `#477 <https://github.com/Kozea/WeasyPrint/issues/477>`_:
      Don't crash on font-face's src attributes with local functions.
    
    
    Version 0.37
    ------------
    
    Released on 2017-06-15.
    
    WeasyPrint now depends on tinycss2 instead of tinycss.
    
    New features:
    
    * `#437 <https://github.com/Kozea/WeasyPrint/issues/437>`_:
      Support local links in generated PDFs.
    
    Bug fixes:
    
    * `#412 <https://github.com/Kozea/WeasyPrint/issues/412>`_:
      Use a NullHandler log handler when WeasyPrint is used as a library.
    * `#417 <https://github.com/Kozea/WeasyPrint/issues/417>`_,
      `#472 <https://github.com/Kozea/WeasyPrint/issues/472>`_:
      Don't crash on some line breaks.
    * `#327 <https://github.com/Kozea/WeasyPrint/issues/327>`_:
      Don't crash with replaced elements with height set in percentages.
    * `#467 <https://github.com/Kozea/WeasyPrint/issues/467>`_:
      Remove incorrect line breaks.
    * `#446 <https://github.com/Kozea/WeasyPrint/pull/446>`_:
      Let the logging module do the string interpolation.

--- print/py-weasyprint/patches/patch-setup.py
+++ print/py-weasyprint/patches/patch-setup.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-setup.py,v 1.1 2015/08/14 17:05:04 kleink Exp $
+$NetBSD: patch-setup.py,v 1.2 2017/08/01 15:36:26 kleink Exp $
 
 Adjust script path to allow multiple Python version installs.
 
---- setup.py.orig	2014-05-05 12:33:27.000000000 +0200
-+++ setup.py	2015-07-16 19:14:03.000000000 +0200
-@@ -75,7 +75,7 @@
-     test_suite='weasyprint.tests',
+--- setup.py.orig	2017-06-22 16:31:14.000000000 +0000
++++ setup.py
+@@ -87,7 +87,7 @@ setup(
+         "pytest-runner", "pytest-cov", "pytest-flake8", "pytest-isort"]},
      entry_points={
          'console_scripts': [
 -            'weasyprint = weasyprint.__main__:main',
