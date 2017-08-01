4228042408878cb99e0948d7a9b15839d726cad9
ryoon <ryoon@pkgsrc.org>
2017-08-01 11:48:20 +0000

    Enable C++ binding that is required by misc/libreoffice 5.4.0.3
    * Use C and C++ laguages
    * Bump PKGREVISION

--- security/gpgme/buildlink3.mk
+++ security/gpgme/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2017/03/31 10:31:57 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2017/08/01 11:48:20 ryoon Exp $
 
 BUILDLINK_TREE+=	gpgme
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gpgme
 GPGME_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gpgme+=	gpgme>=0.9.0
-BUILDLINK_ABI_DEPENDS.gpgme+=	gpgme>=1.8.0
+BUILDLINK_ABI_DEPENDS.gpgme+=	gpgme>=1.8.0nb1
 BUILDLINK_PKGSRCDIR.gpgme?=	../../security/gpgme
 
 .include "../../devel/librfuncs/buildlink3.mk"
