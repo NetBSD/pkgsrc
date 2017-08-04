c68776f1a8fa599af3ddea980983a66b03598e7d
taca <taca@pkgsrc.org>
2017-08-04 23:08:47 +0000

    Update php71 to 7.1.8.
    * pkgsrc change: enable readline PKG_OPTIONS default.
    
    
    03 Aug 2017, PHP 7.1.8
    
    - Core:
      . Fixed bug #74832 (Loading PHP extension with already registered function
        name leads to a crash). (jpauli)
      . Fixed bug #74780 (parse_url() broken when query string contains colon).
        (jhdxr)
      . Fixed bug #74761 (Unary operator expected error on some systems). (petk)
      . Fixed bug #73900 (Use After Free in unserialize() SplFixedArray). (nikic)
      . Fixed bug #74923 (Crash when crawling through network share). (Anatol)
      . Fixed bug #74913 (fixed incorrect poll.h include). (petk)
      . Fixed bug #74906 (fixed incorrect errno.h include). (petk)
    
    - Date:
      . Fixed bug #74852 (property_exists returns true on unknown DateInterval
        property). (jhdxr)
    
    - OCI8:
      . Fixed bug #74625 (Integer overflow in oci_bind_array_by_name). (Ingmar Runge)
    
    - Opcache:
      . Fixed bug #74623 (Infinite loop in type inference when using HTMLPurifier).
        (nikic)
    
    - OpenSSL:
     . Fixed bug #74798 (pkcs7_en/decrypt does not work if \x0a is used in content).
       (Anatol)
     . Added OPENSSL_DONT_ZERO_PAD_KEY constant to prevent key padding and fix bug
       #71917 (openssl_open() returns junk on envelope < 16 bytes) and bug #72362
       (OpenSSL Blowfish encryption is incorrect for short keys). (Jakub Zelenka)
    
    - PDO:
      . Fixed bug #69356 (PDOStatement::debugDumpParams() truncates query). (Adam
        Baratz)
    
    - SPL:
      . Fixed bug #73471 (PHP freezes with AppendIterator). (jhdxr)
    
    - SQLite3:
      . Fixed bug #74883 (SQLite3::__construct() produces "out of memory" exception
        with invalid flags). (Anatol)
    
    - Wddx:
      . Fixed bug #73173 (huge memleak when wddx_unserialize).
        (tloi at fortinet dot com)
    
    - zlib:
      . Fixed bug #73944 (dictionary option of inflate_init() does not work).
        (wapmorgan)

--- lang/php71/Makefile.php
+++ lang/php71/Makefile.php
@@ -1,4 +1,4 @@
-# $NetBSD: Makefile.php,v 1.5 2017/07/12 09:11:36 manu Exp $
+# $NetBSD: Makefile.php,v 1.6 2017/08/04 23:08:47 taca Exp $
 # used by lang/php71/Makefile
 # used by www/ap-php/Makefile
 # used by www/php-fpm/Makefile
@@ -46,7 +46,7 @@ CONFIGURE_ARGS+=	--with-libxml-dir=${PREFIX}
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.${PHP_PKG_PREFIX}
 PKG_SUPPORTED_OPTIONS+=	inet6 ssl maintainer-zts readline
-PKG_SUGGESTED_OPTIONS+=	inet6 ssl
+PKG_SUGGESTED_OPTIONS+=	inet6 ssl readline
 
 .if ${OPSYS} == "SunOS" || ${OPSYS} == "Darwin" || ${OPSYS} == "FreeBSD"
 PKG_SUPPORTED_OPTIONS+=	dtrace
