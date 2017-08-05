60467de384da2c4773bf88153122646fecf50093
schmonz <schmonz@pkgsrc.org>
2017-08-05 15:16:05 +0000

    Catch up to qmail-run:
    - Collapse redundant code for invoking service-specific rc.d scripts.
    - Don't try to run a service's rc.d script if it isn't enabled in rc.conf.
    - Prefix "nb" to procnames.
    
    Bump version.

--- net/djbdns-run/files/djbdns.sh
+++ net/djbdns-run/files/djbdns.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: djbdns.sh,v 1.1 2005/04/15 05:15:55 schmonz Exp $
+# $NetBSD: djbdns.sh,v 1.2 2017/08/05 15:16:05 schmonz Exp $
 #
 # @PKGNAME@ master script for administrators to control djbdns
 # services.
@@ -22,32 +22,31 @@ rcd_dir=`@DIRNAME@ $0`
 #
 forward_commands()
 {
-	# Backward compat with NetBSD <1.6:
-	[ -z "$rc_arg" ] && rc_arg=$_arg
-
-	for file in $COMMAND_LIST; do
-		$rcd_dir/$file $rc_arg
-	done
+	djbdnsrcd $COMMAND_LIST
 }
 
 reverse_commands()
 {
-	# Backward compat with NetBSD <1.6:
-	[ -z "$rc_arg" ] && rc_arg=$_arg
-
 	REVCOMMAND_LIST=
 	for file in $COMMAND_LIST; do
 		REVCOMMAND_LIST="$file $REVCOMMAND_LIST"
 	done
-	for file in $REVCOMMAND_LIST; do
-		$rcd_dir/$file $rc_arg
-	done
+
+	djbdnsrcd $REVCOMMAND_LIST
 }
 
 djbdnsrcd()
 {
-	for service in $@; do
-		$rcd_dir/${service} $rc_arg
+	# Backward compat with NetBSD <1.6:
+	[ -z "$rc_arg" ] && rc_arg=$_arg
+
+	for service in "$@"; do
+		if [ -f /etc/rc.subr ]; then
+			load_rc_config $service
+			checkyesno $service && $rcd_dir/${service} $rc_arg
+		else
+			$rcd_dir/${service} $rc_arg
+		fi
 	done
 }
 
