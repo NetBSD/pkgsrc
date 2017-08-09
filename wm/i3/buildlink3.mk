72ac570a0c1a2af01b84b6c7187867c8c7b854fb
nros <nros@pkgsrc.org>
2017-08-09 10:27:44 +0000

    fix typo in buildlink3 file

--- wm/i3/buildlink3.mk
+++ wm/i3/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/04/05 17:04:59 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/08/09 10:27:44 nros Exp $
 
 BUILDLINK_TREE+=	i3
 
 .if !defined(I3_BUILDLINK3_MK)
 I3_BUILDLINK3_MK:=
 
-BULDLINK_DEPMETHOD.i3?=		build
+BUILDLINK_DEPMETHOD.i3?=		build
 BUILDLINK_API_DEPENDS.i3+=	i3>=4.10.1
 BUILDLINK_PKGSRCDIR.i3?=	../../wm/i3
 
