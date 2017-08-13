956773f614c3100d60b5dbd964ed5fca992f0eee
taca <taca@pkgsrc.org>
2017-08-13 14:21:03 +0000

    Update isc-dhcp4 to 4.3.6.
    This is mainly bug fix release and changes from 4.3.4 are too many to write
    here, please refer RELNOTES in detail.

--- net/isc-dhcp4/buildlink3.mk
+++ net/isc-dhcp4/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:25:09 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2017/08/13 14:21:03 taca Exp $
 
 BUILDLINK_TREE+=	isc-dhcp
 
 .if !defined(ISC_DHCP_BUILDLINK3_MK)
 ISC_DHCP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.isc-dhcp+=	isc-dhcp>=4.1.0
+BUILDLINK_API_DEPENDS.isc-dhcp+=	isc-dhcp>=4.3.0
 BUILDLINK_PKGSRCDIR.isc-dhcp?=	../../net/isc-dhcp4
 .endif # ISC_DHCP_BUILDLINK3_MK
 
