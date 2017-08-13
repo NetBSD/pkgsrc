953dacd01a4226f1e824e423e2b128d2d93b8ebd
bsiegert <bsiegert@pkgsrc.org>
2017-08-13 19:07:26 +0000

    Pullup ticket #5532 - requested by taca lang/php71: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.186
    - lang/php71/Makefile.php                                       1.6
    - lang/php71/distinfo                                           1.26
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Fri Aug  4 23:08:47 UTC 2017
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php71: Makefile.php distinfo
    
       Log Message:
       Update php71 to 7.1.8.
    
       * pkgsrc change: enable readline PKG_OPTIONS default.
    
       03 Aug 2017, PHP 7.1.8
    
       - Core:
         . Fixed bug #74832 (Loading PHP extension with already registered function
           name leads to a crash). (jpauli)
         . Fixed bug #74780 (parse_url() broken when query string contains colon).
           (jhdxr)
         . Fixed bug #74761 (Unary operator expected error on some systems). (petk)
         . Fixed bug #73900 (Use After Free in unserialize() SplFixedArray). (nikic)
         . Fixed bug #74923 (Crash when crawling through network share). (Anatol)
         . Fixed bug #74913 (fixed incorrect poll.h include). (petk)
         . Fixed bug #74906 (fixed incorrect errno.h include). (petk)
    
       - Date:
         . Fixed bug #74852 (property_exists returns true on unknown DateInterval
           property). (jhdxr)
    
       - OCI8:
         . Fixed bug #74625 (Integer overflow in oci_bind_array_by_name). (Ingmar Runge)
    
       - Opcache:
         . Fixed bug #74623 (Infinite loop in type inference when using HTMLPurifier).
           (nikic)
    
       - OpenSSL:
        . Fixed bug #74798 (pkcs7_en/decrypt does not work if \x0a is used in content).
          (Anatol)
        . Added OPENSSL_DONT_ZERO_PAD_KEY constant to prevent key padding and fix bug
          #71917 (openssl_open() returns junk on envelope < 16 bytes) and bug #72362
          (OpenSSL Blowfish encryption is incorrect for short keys). (Jakub Zelenka)
    
       - PDO:
         . Fixed bug #69356 (PDOStatement::debugDumpParams() truncates query). (Adam
           Baratz)
    
       - SPL:
         . Fixed bug #73471 (PHP freezes with AppendIterator). (jhdxr)
    
       - SQLite3:
         . Fixed bug #74883 (SQLite3::__construct() produces "out of memory" exception
           with invalid flags). (Anatol)
    
       - Wddx:
         . Fixed bug #73173 (huge memleak when wddx_unserialize).
           (tloi at fortinet dot com)
    
       - zlib:
         . Fixed bug #73944 (dictionary option of inflate_init() does not work).
           (wapmorgan)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.181.2.4 2017/08/13 19:01:25 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.181.2.5 2017/08/13 19:07:26 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.31
 PHP70_VERSION=	7.0.22
-PHP71_VERSION=	7.1.7
+PHP71_VERSION=	7.1.8
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
