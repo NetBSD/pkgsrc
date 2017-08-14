3fe45d8c86986f2bc50ca8d27dd1ef5ff6c186f3
ryoon <ryoon@pkgsrc.org>
2017-08-14 00:02:16 +0000

    Update to 1.7.141
    * Support build and run under NetBSD PaX MPROTECT environment
    
    Changelog:
    Many bugfixes and security bugfixes

--- lang/openjdk7/patches/patch-jdk_src_solaris_native_java_net_net__util__md.h
+++ lang/openjdk7/patches/patch-jdk_src_solaris_native_java_net_net__util__md.h
@@ -1,13 +1,13 @@
-$NetBSD: patch-jdk_src_solaris_native_java_net_net__util__md.h,v 1.1 2014/07/25 20:35:14 ryoon Exp $
+$NetBSD: patch-jdk_src_solaris_native_java_net_net__util__md.h,v 1.2 2017/08/14 00:02:16 ryoon Exp $
 
---- jdk/src/solaris/native/java/net/net_util_md.h.orig	2014-06-14 20:38:31.000000000 +0000
+--- jdk/src/solaris/native/java/net/net_util_md.h.orig	2017-08-13 05:55:21.000000000 +0000
 +++ jdk/src/solaris/native/java/net/net_util_md.h
-@@ -37,7 +37,7 @@
- #endif
- 
- 
--#if defined(__linux__) || defined(__FreeBSD__) || defined(MACOSX)
-+#if defined(__linux__) || defined(__FreeBSD__) || defined(MACOSX) || defined(__DragonFly__)
+@@ -46,7 +46,7 @@
+    close subroutine does not return until the select call returns.
+    ...
+ */
+-#if defined(__linux__) || defined(__FreeBSD__) || defined(MACOSX) || defined (_AIX)
++#if defined(__linux__) || defined(__FreeBSD__) || defined(MACOSX) || defined (_AIX) || defined(__DragonFly__)
  extern int NET_Timeout(int s, long timeout);
  extern int NET_Read(int s, void* buf, size_t len);
  extern int NET_RecvFrom(int s, void *buf, int len, unsigned int flags,
