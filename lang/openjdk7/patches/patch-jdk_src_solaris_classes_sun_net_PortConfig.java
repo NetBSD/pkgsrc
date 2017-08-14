720eae0be4bb7dd510479eadae1ce25a13c768e9
ryoon <ryoon@pkgsrc.org>
2017-08-14 00:02:16 +0000

    Update to 1.7.141
    * Support build and run under NetBSD PaX MPROTECT environment
    
    Changelog:
    Many bugfixes and security bugfixes

--- lang/openjdk7/patches/patch-jdk_src_solaris_classes_sun_net_PortConfig.java
+++ lang/openjdk7/patches/patch-jdk_src_solaris_classes_sun_net_PortConfig.java
@@ -1,9 +1,9 @@
-$NetBSD: patch-jdk_src_solaris_classes_sun_net_PortConfig.java,v 1.2 2014/05/02 10:48:13 ryoon Exp $
+$NetBSD: patch-jdk_src_solaris_classes_sun_net_PortConfig.java,v 1.3 2017/08/14 00:02:16 ryoon Exp $
 
---- jdk/src/solaris/classes/sun/net/PortConfig.java.orig	2014-05-01 13:11:40.000000000 +0000
+--- jdk/src/solaris/classes/sun/net/PortConfig.java.orig	2017-08-13 05:55:21.000000000 +0000
 +++ jdk/src/solaris/classes/sun/net/PortConfig.java
-@@ -52,7 +52,7 @@ public final class PortConfig {
-                     } else if (os.startsWith("SunOS")) {
+@@ -59,7 +59,7 @@ public final class PortConfig {
+                     } else if (os.startsWith("AIX")) {
                          defaultLower = 32768;
                          defaultUpper = 65535;
 -                    } else if (os.contains("OS X") || os.endsWith("BSD")) {
