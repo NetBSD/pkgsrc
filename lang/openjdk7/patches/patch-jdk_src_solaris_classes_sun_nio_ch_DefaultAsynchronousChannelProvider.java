21995d030d6d8c5a15acbf5936ecc6d17fc17a0a
ryoon <ryoon@pkgsrc.org>
2017-08-14 00:02:16 +0000

    Update to 1.7.141
    * Support build and run under NetBSD PaX MPROTECT environment
    
    Changelog:
    Many bugfixes and security bugfixes

--- lang/openjdk7/patches/patch-jdk_src_solaris_classes_sun_nio_ch_DefaultAsynchronousChannelProvider.java
+++ lang/openjdk7/patches/patch-jdk_src_solaris_classes_sun_nio_ch_DefaultAsynchronousChannelProvider.java
@@ -1,9 +1,9 @@
-$NetBSD: patch-jdk_src_solaris_classes_sun_nio_ch_DefaultAsynchronousChannelProvider.java,v 1.2 2014/05/02 10:48:13 ryoon Exp $
+$NetBSD: patch-jdk_src_solaris_classes_sun_nio_ch_DefaultAsynchronousChannelProvider.java,v 1.3 2017/08/14 00:02:16 ryoon Exp $
 
 * Fix apache-tomcat7's error, "java.lang.InternalError: platform not recognized"
   patch is from http://www.freebsd.org/cgi/query-pr.cgi?pr=183389
 
---- jdk/src/solaris/classes/sun/nio/ch/DefaultAsynchronousChannelProvider.java.orig	2014-05-01 13:11:40.000000000 +0000
+--- jdk/src/solaris/classes/sun/nio/ch/DefaultAsynchronousChannelProvider.java.orig	2017-08-13 05:55:21.000000000 +0000
 +++ jdk/src/solaris/classes/sun/nio/ch/DefaultAsynchronousChannelProvider.java
 @@ -50,7 +50,7 @@ public class DefaultAsynchronousChannelP
              return new SolarisAsynchronousChannelProvider();
@@ -12,5 +12,5 @@ $NetBSD: patch-jdk_src_solaris_classes_sun_nio_ch_DefaultAsynchronousChannelProv
 -        if (osname.contains("OS X") || osname.endsWith("BSD"))
 +        if (osname.contains("OS X") || osname.endsWith("BSD") || osname.contains("DragonFly"))
              return new BsdAsynchronousChannelProvider();
-         throw new InternalError("platform not recognized");
-     }
+         if (osname.equals("AIX"))
+             return new AixAsynchronousChannelProvider();
