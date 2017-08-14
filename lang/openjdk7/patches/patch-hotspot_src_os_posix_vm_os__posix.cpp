e74819d8265a2f3e1d1da85f3b6ac16e1365961c
ryoon <ryoon@pkgsrc.org>
2017-08-14 00:02:16 +0000

    Update to 1.7.141
    * Support build and run under NetBSD PaX MPROTECT environment
    
    Changelog:
    Many bugfixes and security bugfixes

--- lang/openjdk7/patches/patch-hotspot_src_os_posix_vm_os__posix.cpp
+++ lang/openjdk7/patches/patch-hotspot_src_os_posix_vm_os__posix.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-hotspot_src_os_posix_vm_os__posix.cpp,v 1.1 2013/06/15 09:31:05 jperkin Exp $
+$NetBSD: patch-hotspot_src_os_posix_vm_os__posix.cpp,v 1.2 2017/08/14 00:02:16 ryoon Exp $
 
 rlim_t fixes.
 
---- hotspot/src/os/posix/vm/os_posix.cpp.orig	2013-06-11 09:37:03.648327363 +0000
+--- hotspot/src/os/posix/vm/os_posix.cpp.orig	2017-08-13 05:55:27.000000000 +0000
 +++ hotspot/src/os/posix/vm/os_posix.cpp
-@@ -130,12 +130,12 @@ void os::Posix::print_rlimit_info(output
+@@ -162,12 +162,12 @@ void os::Posix::print_rlimit_info(output
  
    st->print(" STACK ");
    getrlimit(RLIMIT_STACK, &rlim);
@@ -18,8 +18,8 @@ rlim_t fixes.
 +  if (rlim.rlim_cur == (rlim_t)RLIM_INFINITY) st->print("infinity");
    else st->print("%uk", rlim.rlim_cur >> 10);
  
-   //Isn't there on solaris
-@@ -148,7 +148,7 @@ void os::Posix::print_rlimit_info(output
+   // Isn't there on solaris and aix.
+@@ -180,7 +180,7 @@ void os::Posix::print_rlimit_info(output
  
    st->print(", NOFILE ");
    getrlimit(RLIMIT_NOFILE, &rlim);
@@ -28,7 +28,7 @@ rlim_t fixes.
    else st->print("%d", rlim.rlim_cur);
  
  #ifdef __OpenBSD__
-@@ -160,7 +160,7 @@ void os::Posix::print_rlimit_info(output
+@@ -192,7 +192,7 @@ void os::Posix::print_rlimit_info(output
  #else
    st->print(", AS ");
    getrlimit(RLIMIT_AS, &rlim);
