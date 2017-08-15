ad79537d6cfcace8bf6ebd383a43890e55177040
triaxx <triaxx@pkgsrc.org>
2017-08-15 10:57:35 +0000

    Update wm/awesome to 4.2
    Reviewed by maya@
    
    Changes by packager:
    * Add options handling
    * Add DBus support through options.mk
    * Add API doc generation through options.mk
    * Move libexecinfo dependency to options.mk
    
    Changes from Changelog:
    https://awesomewm.org/changelogs/short/v4.2.txt

---
+++ wm/awesome/options.mk
@@ -0,0 +1,34 @@
+PKG_OPTIONS_VAR=		PKG_OPTIONS.awesome
+PKG_SUPPORTED_OPTIONS=		dbus debug doc
+PLIST_VARS+=			doc
+
+.include "../../mk/bsd.prefs.mk"
+.include "../../mk/bsd.options.mk"
+
+###
+### DBus support
+###
+.if !empty(PKG_OPTIONS:Mdbus)
+CMAKE_ARGS+=	-DWITH_DBUS=ON
+.  include "../../sysutils/dbus/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DWITH_DBUS=OFF
+.endif
+
+###
+### Debug support
+###
+.if !empty(PKG_OPTIONS:Mdebug)
+.  include "../../devel/libexecinfo/buildlink3.mk"
+.endif
+
+###
+### Doc support
+###
+.if !empty(PKG_OPTIONS:Mdoc)
+CMAKE_ARGS+=	-DGENERATE_DOC=ON
+BUILD_DEPENDS+=	${LUA_PKGPREFIX}-doc-[0-9]*:../../devel/lua-doc
+PLIST.doc=	yes
+.else
+CMAKE_ARGS+=	-DGENERATE_DOC=OFF
+.endif
--- wm/awesome/patches/patch-build-utils_lgi-check.sh
+++ wm/awesome/patches/patch-build-utils_lgi-check.sh
@@ -1,17 +1,26 @@
-$NetBSD: patch-build-utils_lgi-check.sh,v 1.1 2013/12/08 00:18:21 dholland Exp $
+$NetBSD: patch-build-utils_lgi-check.sh,v 1.2 2017/08/15 10:57:35 triaxx Exp $
 
-For some reason (probably cmake's doing) setting AWESOME_IGNORE_LGI=1
-from pkgsrc does not prevent this from failing the build, so do it
-forcibly.
+Use pkgsrc lua interpreter instead of builtin one.
 
---- build-utils/lgi-check.sh~	2013-04-01 11:44:46.000000000 +0000
+--- build-utils/lgi-check.sh.orig	2017-07-15 13:53:13.000000000 +0000
 +++ build-utils/lgi-check.sh
-@@ -14,7 +14,7 @@ die()
- 	echo " Add AWESOME_IGNORE_LGI=1 to your environment to continue."
- 	echo
- 	echo
--	if [ "x$AWESOME_IGNORE_LGI" = "x1" ]
-+	if true;
- 	then
- 		exit 0
- 	fi
+@@ -22,16 +22,16 @@ die()
+ }
+ 
+ # Check if we have lgi
+-lua -e 'require("lgi")' || die
++@LUA_INTERPRETER@ -e 'require("lgi")' || die
+ 
+ # Check the version number.
+ # Keep this in sync with lib/gears/surface.lua and .travis.yml (LGIVER)!
+-lua -e '_, _, major_minor, patch = string.find(require("lgi.version"), "^(%d%.%d)%.(%d)");
++@LUA_INTERPRETER@ -e '_, _, major_minor, patch = string.find(require("lgi.version"), "^(%d%.%d)%.(%d)");
+ 	if tonumber(major_minor) < 0.8 or (tonumber(major_minor) == 0.8 and tonumber(patch) < 0) then
+ 		error(string.format("lgi is too old, need at least version %s, got %s.",
+ 		                    "0.8.0", require("lgi.version"))) end' || die
+ 
+ # Check for the needed gi files
+-lua -e 'l = require("lgi") assert(l.cairo, l.Pango, l.PangoCairo, l.GLib, l.Gio)' || die
++@LUA_INTERPRETER@ -e 'l = require("lgi") assert(l.cairo, l.Pango, l.PangoCairo, l.GLib, l.Gio)' || die
+ 
+ # vim: filetype=sh:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
