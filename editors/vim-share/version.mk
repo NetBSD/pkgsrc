37f56e58cea51c218278bfb8ecf37b81efba5bcf
wiz <wiz@pkgsrc.org>
2017-08-15 21:00:11 +0000

    Updated vim-share to 8.0.0921.
    The usual bugfixes.
    
    Add PRINT_PLIST_AWK line to make updates easier.

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,5 +1,7 @@
-# $NetBSD: version.mk,v 1.105 2017/07/09 23:58:24 wen Exp $
+# $NetBSD: version.mk,v 1.106 2017/08/15 21:00:11 wiz Exp $
 
 VIM_VERSION=		8.0
-VIM_PATCHLEVEL=		0702
+VIM_PATCHLEVEL=		0921
 VIM_SUBDIR=		vim80
+
+PRINT_PLIST_AWK+=	{ gsub(/${VIM_SUBDIR}/, "$${VIM_SUBDIR}"); print; next; }
