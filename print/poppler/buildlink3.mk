3f0bb75093711c893489f848e0c65d96a25b1028
nros <nros@pkgsrc.org>
2017-08-15 10:44:58 +0000

    blbump due to update of poppler to version 0.57.0.

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.53 2017/05/02 13:37:30 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.54 2017/08/15 10:44:58 nros Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.54.0
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.57.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 .include "../../graphics/lcms2/buildlink3.mk"
