f3ad0fa2b266912a76c9adafb88347acf2160724
adam <adam@pkgsrc.org>
2017-08-16 07:17:58 +0000

    2.18.4: Improvements * Error messages for invalid headers now include the header name for easier debugging
    Dependencies
    * We now support idna v2.6.

--- devel/py-requests/patches/patch-setup.py
+++ devel/py-requests/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.3 2017/07/26 16:43:39 adam Exp $
+$NetBSD: patch-setup.py,v 1.4 2017/08/16 07:17:58 adam Exp $
 
 Allow newer versions of dependencies.
 
---- setup.py.orig	2017-07-25 15:15:36.000000000 +0000
+--- setup.py.orig	2017-08-15 13:19:39.000000000 +0000
 +++ setup.py
 @@ -42,13 +42,13 @@ if sys.argv[-1] == 'publish':
  packages = ['requests']
  
  requires = [
 -    'chardet>=3.0.2,<3.1.0',
--    'idna>=2.5,<2.6',
+-    'idna>=2.5,<2.7',
 -    'urllib3>=1.21.1,<1.23',
 +    'chardet>=3.0.2',
 +    'idna>=2.5',
