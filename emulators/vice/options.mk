ba814cf78223828ee913b012f64b1d7cf0ef80ea
adam <adam@pkgsrc.org>
2017-08-16 17:54:09 +0000

    Changes in VICE 3.1
    General
    - New support for double sided 1571 g64 and p64 images.
    - SID filter improvements.
    
    C64(SC)/SCPU64/C64DTV/C128 changes
    - New joyport script64 dongle emulation.
    - New joyport vizawrite64 dongle emulation.
    - Fixed joyport paperclip64 dongle emulation.
    - Fixed StarDOS emulation.
    
    XVIC
    - Fixed VIA emulation.
    
    C1541
    - Fixed commands and added new commands.
    
    BeOS/Haiku changes
    - New drag & drop support: dragging a file from Tracker into the window
      now autostarts it, and dragging text into the window pastes it.

--- emulators/vice/options.mk
+++ emulators/vice/options.mk
@@ -1,73 +1,66 @@
-# $NetBSD: options.mk,v 1.7 2012/12/12 09:58:03 wiz Exp $
+# $NetBSD: options.mk,v 1.8 2017/08/16 17:54:09 adam Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.vice
-PKG_SUPPORTED_OPTIONS=	ffmpeg lame vice-fullscreen vice-hwscaling vice-ethernet
-PKG_OPTIONS_REQUIRED_GROUPS = gui
-PKG_OPTIONS_GROUP.gui = xaw gnome sdl
-PKG_SUGGESTED_OPTIONS=	ffmpeg lame vice-fullscreen vice-hwscaling vice-ethernet
-PKG_SUGGESTED_OPTIONS+=	xaw
+PKG_OPTIONS_VAR=		PKG_OPTIONS.vice
+PKG_SUPPORTED_OPTIONS=		ffmpeg vte
+PKG_OPTIONS_REQUIRED_GROUPS=	gui
+PKG_OPTIONS_GROUP.gui=		gtk2 gtk3 sdl xaw
+PKG_SUGGESTED_OPTIONS=		gtk3
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mxaw)
-.  include "../../x11/libXt/buildlink3.mk"
-.  include "../../x11/libXmu/buildlink3.mk"
-.  include "../../x11/libXaw/buildlink3.mk"
-.  include "../../x11/libXpm/buildlink3.mk"
-PLIST_SRC += PLIST.x11
+PLIST_VARS+=	pcf sdl x11
+
+# Cocoa user interface works only in application bundle.
+#.if !empty(PKG_OPTIONS:Mcocoa)
+#CONFIGURE_ARGS+=	--disable-bundle
+#CONFIGURE_ARGS+=	--with-cocoa
+#PLIST.x11=		yes
+#INSTALL_TARGET=		bindist
+#.else
+USE_TOOLS+=		bdftopcf
+#.endif
+
+.if !empty(PKG_OPTIONS:Mgtk2)
+CONFIGURE_ARGS+=	--enable-gnomeui
+PLIST.pcf=		yes
+PLIST.x11=		yes
+.  include "../../x11/gtk2/buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mgnome)
-CONFIGURE_ARGS+=	--enable-gnomeui
-PLIST_SRC+=		PLIST.x11
-.  include "../../x11/gtk2/buildlink3.mk"
-.  include "../../x11/vte/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mgtk3)
+CONFIGURE_ARGS+=	--enable-gnomeui3
+PLIST.pcf=		yes
+PLIST.x11=		yes
+.  include "../../x11/gtk3/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Msdl)
-CONFIGURE_ARGS+=	--enable-sdlui
-PLIST_SRC+=		PLIST.sdl
-.  include "../../devel/SDL/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mvice-fullscreen)
-CONFIGURE_ARGS+=	--enable-fullscreen
-.  if !empty(PKG_OPTIONS:Mxaw)
-.    include "../../x11/libXrandr/buildlink3.mk"
-.    include "../../x11/libXxf86vm/buildlink3.mk"
-.  endif
-.  if !empty(PKG_OPTIONS:Mgnome)
-.    include "../../x11/libXrandr/buildlink3.mk"
-.    include "../../x11/libXxf86vm/buildlink3.mk"
-.  endif
-.  if !empty(PKG_OPTIONS:Msdl)
-.  endif
-.endif
-
-.if !empty(PKG_OPTIONS:Mvice-hwscaling)
-.  if !empty(PKG_OPTIONS:Mxaw)
-.    include "../../x11/libXv/buildlink3.mk"
-.  endif
-.  if !empty(PKG_OPTIONS:Mgnome)
-.    include "../../graphics/gtkglext/buildlink3.mk"
-.  endif
-.  if !empty(PKG_OPTIONS:Msdl)
-.  endif
-.endif
-
-.if !empty(PKG_OPTIONS:Mvice-ethernet)
-CONFIGURE_ARGS+=	--enable-ethernet
-.  include "../../net/libpcap/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-sdlui2
+PLIST.pcf=		yes
+PLIST.sdl=		yes
+.  include "../../devel/SDL2/buildlink3.mk"
 .endif
 
 # If desired, ffmpeg and lame can be build-time-only dependencies,
 # since they are loaded dynamically only.
 .if !empty(PKG_OPTIONS:Mffmpeg)
 #BUILDLINK_DEPMETHOD.ffmpeg?=	build
-CONFIGURE_ARGS+=	--enable-ffmpeg
-.  include "../../multimedia/ffmpeg010/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-external-ffmpeg
+.  include "../../multimedia/ffmpeg3/buildlink3.mk"
 .endif
-.if !empty(PKG_OPTIONS:Mlame)
-#BUILDLINK_DEPMETHOD.lame?=	build
-.  include "../../audio/lame/buildlink3.mk"
+
+.if !empty(PKG_OPTIONS:Mvte)
+.  include "../../x11/vte/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mxaw)
+PLIST.pcf=		yes
+PLIST.x11=		yes
+.  include "../../x11/libXt/buildlink3.mk"
+.  include "../../x11/libXmu/buildlink3.mk"
+.  include "../../x11/libXaw/buildlink3.mk"
+.  include "../../x11/libXpm/buildlink3.mk"
+.  include "../../x11/libXrandr/buildlink3.mk"
+.  include "../../x11/libXv/buildlink3.mk"
+.  include "../../x11/libXxf86vm/buildlink3.mk"
 .endif
