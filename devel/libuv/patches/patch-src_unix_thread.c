19f3acb1719a1717bdbab8c42c1c6ca5f204f0fb
adam <adam@pkgsrc.org>
2017-08-17 20:06:47 +0000

    Version 1.14.0: * unix: check for NULL in uv_os_unsetenv for parameter name (André Klitzing) * doc: add thread safety warning for process title (Matthew Taylor) * unix: always copy process title into local buffer (Matthew Taylor) * poll: add support for OOB TCP and GPIO interrupts (CurlyMoo) * win,build: fix appveyor properly (Refael Ackermann) * win: include filename in dlopen error message (Ben Noordhuis) * aix: add netmask, mac address into net interfaces (Gireesh Punathil) * unix, windows: map EREMOTEIO errno (Ben Noordhuis) * unix: fix wrong MAC of uv_interface_address (XadillaX) * win,build: fix building from Windows SDK or VS console (Saúl Ibarra Corretgé) * github: fix link to help repo in issue template (Ben Noordhuis) * zos: remove nonexistent include from autotools build (Saúl Ibarra Corretgé) * misc: remove reference to pthread-fixes.h from LICENSE (Saúl Ibarra Corretgé) * docs: fix guide source code example paths (Anticrisis) * android: fix compilation with new NDK versions (Saúl Ibarra Corretgé) * misc: add android-toolchain to .gitignore (Saúl Ibarra Corretgé) * win, fs: support unusual reparse points (Bartosz Sosnowski) * android: fix detection of pthread_condattr_setclock (Saúl Ibarra Corretgé) * android: remove no longer needed check (Saúl Ibarra Corretgé) * doc: update instructions for building on Android (Saúl Ibarra Corretgé) * win, process: support semicolons in PATH variable (Bartosz Sosnowski) * doc: document uv_async_(init|send) return values (Ben Noordhuis) * doc: add Android as a tier 3 supported platform (Saúl Ibarra Corretgé) * unix: add missing semicolon (jBarz) * win, test: fix double close in test runner (Bartosz Sosnowski) * doc: update supported windows version baseline (Ben Noordhuis) * test,zos: skip chown root test (jBarz) * test,zos: use gid=-1 to test spawn_setgid_fails (jBarz) * zos: fix hr timer resolution (jBarz) * android: fix blocking recvmsg due to netlink bug (Jacob Segal) * zos: read more accurate rss info from RSM (jBarz) * win: allow bound/connected socket in uv_tcp_open() (Maciej Szeptuch   (Neverous)) * doc: differentiate SmartOS and SunOS support (cjihrig) * unix: make uv_poll_stop() remove fd from pollset (Ben Noordhuis) * unix, windows: add basic uv_fs_copyfile() (cjihrig)

--- devel/libuv/patches/patch-src_unix_thread.c
+++ devel/libuv/patches/patch-src_unix_thread.c
@@ -1,16 +1,15 @@
-$NetBSD: patch-src_unix_thread.c,v 1.2 2017/03/28 18:44:49 maya Exp $
+$NetBSD: patch-src_unix_thread.c,v 1.3 2017/08/17 20:06:47 adam Exp $
 
 Use feature test for pthread_condattr_setclock, absent in netbsd-6-0
 
---- src/unix/thread.c.orig	2017-02-01 00:38:56.000000000 +0000
+--- src/unix/thread.c.orig	2017-08-16 14:11:33.000000000 +0000
 +++ src/unix/thread.c
-@@ -424,7 +424,8 @@ int uv_cond_init(uv_cond_t* cond) {
+@@ -528,7 +528,7 @@ int uv_cond_init(uv_cond_t* cond) {
    if (err)
      return -err;
  
--#if !(defined(__ANDROID__) && defined(HAVE_PTHREAD_COND_TIMEDWAIT_MONOTONIC))
-+#if defined(HAVE_PTHREAD_CONDATTR_SETCLOCK) && \
-+   (!defined(HAVE_PTHREAD_COND_TIMEDWAIT_MONOTONIC))
+-#if !(defined(__ANDROID_API__) && __ANDROID_API__ < 21)
++#if defined(HAVE_PTHREAD_CONDATTR_SETCLOCK)
    err = pthread_condattr_setclock(&attr, CLOCK_MONOTONIC);
    if (err)
      goto error2;
