4b1fe427941275450dc91006d7bab1620e85a1fc
gavan <gavan@pkgsrc.org>
2017-08-17 01:13:32 +0000

    Add go-afero package

---
+++ devel/go-afero/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1 2017/08/17 01:13:32 gavan Exp $
+
+BUILDLINK_TREE+=	go-afero
+
+.if !defined(GO_AFERO_BUILDLINK3_MK)
+GO_AFERO_BUILDLINK3_MK:=
+
+BUILDLINK_CONTENTS_FILTER.go-afero=	${EGREP} gopkg/
+BUILDLINK_DEPMETHOD.go-afero?=		build
+
+BUILDLINK_API_DEPENDS.go-afero+=	go-afero>=0.0
+BUILDLINK_PKGSRCDIR.go-afero?=		../../devel/go-afero
+
+.include "../../textproc/go-text/buildlink3.mk"
+.include "../../security/go-sftp/buildlink3.mk"
+.include "../../devel/go-fs/buildlink3.mk"
+.include "../../devel/go-errors/buildlink3.mk"
+.include "../../security/go-crypto/buildlink3.mk"
+.endif  # GO_AFERO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-go-afero
+
