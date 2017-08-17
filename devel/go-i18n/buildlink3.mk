76e4377a0f426d7d51f40a2efa7bdd0ef3bbccc6
gavan <gavan@pkgsrc.org>
2017-08-17 01:29:44 +0000

    Add go-i18n package

---
+++ devel/go-i18n/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2017/08/17 01:29:44 gavan Exp $
+
+BUILDLINK_TREE+=	go-i18n
+
+.if !defined(GO_I18N_BUILDLINK3_MK)
+GO_I18N_BUILDLINK3_MK:=
+
+BUILDLINK_CONTENTS_FILTER.go-i18n=	${EGREP} gopkg/
+BUILDLINK_DEPMETHOD.go-i18n?=		build
+
+BUILDLINK_API_DEPENDS.go-i18n+=		go-i18n>=1.8.1
+BUILDLINK_PKGSRCDIR.go-i18n?=		../../devel/go-i18n
+
+.include "../../www/go-toml-pelletier/buildlink3.mk"
+.endif  # GO_I18N_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-go-i18n
+
