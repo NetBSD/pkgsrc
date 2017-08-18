05264c3c73ad32214b43898d3636fda45f2d34bf
adam <adam@pkgsrc.org>
2017-08-18 21:41:19 +0000

    Fix crash on macOS 10.13 (Darwin 7).

--- devel/scmcvs/patches/patch-lib_vasnprintf.c
+++ devel/scmcvs/patches/patch-lib_vasnprintf.c
@@ -1,16 +1,19 @@
-$NetBSD: patch-lib_vasnprintf.c,v 1.1 2015/04/12 07:43:42 dsainty Exp $
+$NetBSD: patch-lib_vasnprintf.c,v 1.2 2017/08/18 21:41:19 adam Exp $
 
 Avoid CVS crashing on systems using _FORTIFY_SOURCE by default.
-
 http://git.savannah.gnu.org/cgit/gnulib.git/commit/lib/vasnprintf.c?id=913c09becd9df89dbd9b9f386e7f35c240d5efe8
 
---- lib/vasnprintf.c.orig	2005-05-24 05:44:33.000000000 +1200
-+++ lib/vasnprintf.c	2015-04-12 18:56:46.352971555 +1200
-@@ -558,9 +558,21 @@
+Fix crash on macOS 10.13 (Darwin 7).
+http://git.savannah.gnu.org/cgit/gnulib.git/commit/lib/vasnprintf.c?id=7df04f9b8a0adb1575ca0555775ec10860143cbf
+
+--- lib/vasnprintf.c.orig	2005-05-23 17:44:33.000000000 +0000
++++ lib/vasnprintf.c
+@@ -558,9 +558,29 @@ VASNPRINTF (CHAR_T *resultbuf, size_t *l
  		  }
  		*p = dp->conversion;
  #if USE_SNPRINTF
-+# if !(__GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 3))
++# if !(__GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 3) \
++	|| (defined __APPLE__ && defined __MACH__))
  		p[1] = '%';
  		p[2] = 'n';
  		p[3] = '\0';
@@ -23,6 +26,13 @@ http://git.savannah.gnu.org/cgit/gnulib.git/commit/lib/vasnprintf.c?id=913c09bec
 +		   in format strings in writable memory may crash the program
 +		   (if compiled with _FORTIFY_SOURCE=2), so we should avoid it
 +		   in this situation.  */
++		/* On Mac OS X 10.3 or newer, we know that snprintf's return
++		   value conforms to ISO C 99: the tests gl_SNPRINTF_RETVAL_C99
++		   and gl_SNPRINTF_TRUNCATION_C99 pass.
++		   Therefore we can avoid using %n in this situation.
++		   On Mac OS X 10.13 or newer, the use of %n in format strings
++		   in writable memory by default crashes the program, so we
++		   should avoid it in this situation.  */
 +		p[1] = '\0';
 +# endif
  #else
