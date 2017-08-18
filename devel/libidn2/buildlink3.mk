01813790ead7f24c992f676781d6052e183590cf
adam <adam@pkgsrc.org>
2017-08-18 21:04:51 +0000

    Version 2.0.3 (released 2017-07-24) [beta]
    ** %IDN2_USE_STD3_ASCII_RULES disabled by default.
     Previously we were eliminating non-STD3 characters from domain strings
     such as _443._tcp.example.com, or IPs 1.2.3.4/24 provided to libidn2
     functions. That was an unexpected regression for applications switching
     from libidn and thus it is no longer applied by default.
     Use %IDN2_USE_STD3_ASCII_RULES to enable that behavior again.
    
    ** Fix several documentation issues
    
    ** Fix build issues
    
    ** Modernize gtk-doc build infrastructure.

--- devel/libidn2/buildlink3.mk
+++ devel/libidn2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/08/17 13:49:15 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/08/18 21:04:51 adam Exp $
 
 BUILDLINK_TREE+=	libidn2
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.libidn2+=	libidn2>=2.0.0
 BUILDLINK_PKGSRCDIR.libidn2?=	../../devel/libidn2
 
 .include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../textproc/libunistring/buildlink3.mk"
 .endif	# LIBIDN2_BUILDLINK3_MK
 
