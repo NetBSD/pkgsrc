cce471812722e593bbd0785305cef2bbb1f32952
triaxx <triaxx@pkgsrc.org>
2017-08-18 13:26:37 +0000

    Fix MASTER_SITES that did not used MASTER_SITE_SOURCEFORGE. Remove useless patches using buildlink framework (thanks to wip/grip).

--- audio/grip/options.mk
+++ audio/grip/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2017/08/18 12:21:29 triaxx Exp $
+# $NetBSD: options.mk,v 1.2 2017/08/18 13:26:37 triaxx Exp $
 #
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.grip
@@ -12,6 +12,7 @@ PKG_SUGGESTED_OPTIONS=		cdparanoia id3
 ### Cdparanoia support
 ###
 .if !empty(PKG_OPTIONS:Mcdparanoia)
+BUILDLINK_FNAME_TRANSFORM.cdparanoia+=	-e s,/cdparanoia/,/,g
 .  include "../../audio/cdparanoia/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-cdpar
