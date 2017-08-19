cafce63107df999bc4a8c21075f55a2358661303
jlam <jlam@pkgsrc.org>
2017-08-19 00:30:07 +0000

    Update pkgtools/pkgtasks to 1.14.
    Use TASK_VERBOSE environment variable to select which tasks should
    emit output when invoked.

--- pkgtools/pkgtasks/files/t/t_preremove.sh
+++ pkgtools/pkgtasks/files/t/t_preremove.sh
@@ -40,25 +40,64 @@ test_setup()
 	TASK_INFO_FILES_SUCCESS="yes"
 }
 
-# Mock actions whose return values are ignored.
+# Mock actions that return the truthiness of environment variables.
+mock_pkgtask()
+{
+	# required parameters
+	local name="$1"; shift
+	local varname="$1"; shift
+
+	# pkgtask parameters
+	local arg
+	local silent=
+	local OPTIND=1
+	while getopts ":s" arg "$@"; do
+		case $arg in
+		s)	silent="yes" ;;
+		*)	return 127 ;;
+		esac
+	done
+	shift $(( ${OPTIND} - 1 ))
+	local action="$1"; shift
+	local stage="$1"; shift
+
+	local value symbol result
+	eval value="\${$varname}"
+	if [ "$value" = "yes" ]; then
+		symbol=">"
+		result=0
+	else
+		symbol="!"
+		result=1
+	fi
+
+	# Skip output if doing a "check" action.
+	case $action in
+	check-*)
+		: "do nothing" ;;
+	*)	[ -n "$silent" ] || echo "$symbol $name" ;;
+	esac
+	return $result
+}
+
 task_files()
 {
-	[ "${TASK_FILES_SUCCESS}" = "yes" ]
+	mock_pkgtask "files" "TASK_FILES_SUCCESS" "$@"
 }
 
 task_function()
 {
-	[ "${TASK_FUNCTION_SUCCESS}" = "yes" ]
+	mock_pkgtask "function" "TASK_FUNCTION_SUCCESS" "$@"
 }
 
 task_icon_themes()
 {
-	[ "${TASK_ICON_THEMES_SUCCESS}" = "yes" ]
+	mock_pkgtask "icon_themes" "TASK_ICON_THEMES_SUCCESS" "$@"
 }
 
 task_info_files()
 {
-	[ "${TASK_INFO_FILES_SUCCESS}" = "yes" ]
+	mock_pkgtask "info_files" "TASK_INFO_FILES_SUCCESS" "$@"
 }
 
 # Always succeed except if "function" task fails.
@@ -117,6 +156,41 @@ test5()
 	else
 		return 1
 	fi
+	return 0
+}
+
+test6()
+{
+	: ${CAT:=cat}
+
+	describe="\${TASK_VERBOSE} = none"
+	TASK_VERBOSE=none
+	task_preremove "$datafile" > output
+	${CAT} output
+	if [ -s "$output" ]; then
+		# no output expected
+		return 1
+	fi
+	return 0
+}
+
+test7()
+{
+	: ${CAT:=cat}
+	: ${GREP:=grep}
+
+	describe="\${TASK_VERBOSE} = all"
+	TASK_VERBOSE=all
+	task_preremove "$datafile" > output
+	${CAT} output
+	for task in files function icon_themes info_files; do
+		if ${GREP} -q "$task" output; then
+			: "success"
+		else
+			describe="$describe: '$task' missing!"
+			return 1
+		fi
+	done
 	return 0
 }
 
