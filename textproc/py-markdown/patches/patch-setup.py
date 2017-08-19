e95fff54e01ac497c8832cd7c0026c6c78c31ec2
adam <adam@pkgsrc.org>
2017-08-19 14:25:14 +0000

    version 2.6.9: a bug-fix release

--- textproc/py-markdown/patches/patch-setup.py
+++ textproc/py-markdown/patches/patch-setup.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-setup.py,v 1.1 2015/11/04 09:45:05 wiz Exp $
+$NetBSD: patch-setup.py,v 1.2 2017/08/19 14:25:15 adam Exp $
 
 Avoid conflicts with other python versions of this package.
 
---- setup.py.orig	2015-08-28 03:42:04.000000000 +0000
+--- setup.py.orig	2017-08-18 00:07:03.000000000 +0000
 +++ setup.py
-@@ -244,7 +244,7 @@ setup(
-     maintainer_email='waylan.limberg [at] icloud.com',
+@@ -246,7 +246,7 @@ setup(
+     maintainer_email='waylan.limberg@icloud.com',
      license='BSD License',
      packages=['markdown', 'markdown.extensions'],
 -    scripts=['bin/%s' % SCRIPT_NAME],
