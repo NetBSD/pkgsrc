006782c08894dc519cbf578fb248035e8f0018a4
fhajny <fhajny@pkgsrc.org>
2017-08-22 17:00:35 +0000

    Update net/dnsdist to 1.2.0.
    - fix for CVE-2016-7069 and CVE-2017-7557.
    - applying rules on cache hits
    - addition of runtime changeable rules that matches IP address for a
      certain time: TimedIPSetRule
    - SNMP support, exporting statistics and sending traps
    - preventing the packet cache from ageing responses when deployed in
      front of authoritative servers
    - TTL alteration capabilities
    - consistent hash results over multiple deployments
    - exporting CNAME records over protobuf
    - tuning the size of the ringbuffers used to keep track of recent
      queries and responses
    - various DNSCrypt-related fixes and improvements, including automatic
      key rotation
    
    Full changelog:
    
      https://dnsdist.org/changelog.html

--- net/dnsdist/patches/patch-dnsdist-console.cc
+++ net/dnsdist/patches/patch-dnsdist-console.cc
@@ -1,12 +1,12 @@
-$NetBSD: patch-dnsdist-console.cc,v 1.1 2017/03/31 20:49:51 fhajny Exp $
+$NetBSD: patch-dnsdist-console.cc,v 1.2 2017/08/22 17:00:35 fhajny Exp $
 
 Add NetBSD support.
 
---- dnsdist-console.cc.orig	2016-12-29 12:45:46.000000000 +0000
+--- dnsdist-console.cc.orig	2017-08-21 12:52:09.000000000 +0000
 +++ dnsdist-console.cc
-@@ -22,7 +22,7 @@
- #include "dnsdist.hh"
+@@ -23,7 +23,7 @@
  #include "sodcrypto.hh"
+ #include "pwd.h"
  
 -#if defined (__OpenBSD__)
 +#if defined (__OpenBSD__) || defined (__NetBSD__)
