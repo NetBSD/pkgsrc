dcede495564bf8a5217500a3e28a0509ef6cab3f
fhajny <fhajny@pkgsrc.org>
2017-08-22 17:00:35 +0000

    Update net/dnsdist to 1.2.0.
    - fix for CVE-2016-7069 and CVE-2017-7557.
    - applying rules on cache hits
    - addition of runtime changeable rules that matches IP address for a
      certain time: TimedIPSetRule
    - SNMP support, exporting statistics and sending traps
    - preventing the packet cache from ageing responses when deployed in
      front of authoritative servers
    - TTL alteration capabilities
    - consistent hash results over multiple deployments
    - exporting CNAME records over protobuf
    - tuning the size of the ringbuffers used to keep track of recent
      queries and responses
    - various DNSCrypt-related fixes and improvements, including automatic
      key rotation
    
    Full changelog:
    
      https://dnsdist.org/changelog.html

--- net/dnsdist/patches/patch-iputils.hh
+++ net/dnsdist/patches/patch-iputils.hh
@@ -1,4 +1,4 @@
-$NetBSD: patch-iputils.hh,v 1.1 2017/03/31 20:49:51 fhajny Exp $
+$NetBSD: patch-iputils.hh,v 1.2 2017/08/22 17:00:35 fhajny Exp $
 
 - Do not use IP_PKTINFO on NetBSD, the structure is not as expected.
 - Backport fix for SunOS segfault problem, see:
@@ -16,35 +16,4 @@ $NetBSD: patch-iputils.hh,v 1.1 2017/03/31 20:49:51 fhajny Exp $
 +
  #ifdef __APPLE__
  #include <libkern/OSByteOrder.h>
- 
-@@ -59,23 +63,22 @@
- #define le64toh(x) OSSwapLittleToHostInt64(x)
- #endif
- 
--// for illumos
--#ifdef BE_64
-+#ifdef __sun
- 
- #define htobe16(x) BE_16(x)
- #define htole16(x) LE_16(x)
--#define be16toh(x) BE_IN16(x)
--#define le16toh(x) LE_IN16(x)
-+#define be16toh(x) BE_IN16(&(x))
-+#define le16toh(x) LE_IN16(&(x))
- 
- #define htobe32(x) BE_32(x)
- #define htole32(x) LE_32(x)
--#define be32toh(x) BE_IN32(x)
--#define le32toh(x) LE_IN32(x)
-+#define be32toh(x) BE_IN32(&(x))
-+#define le32toh(x) LE_IN32(&(x))
- 
- #define htobe64(x) BE_64(x)
- #define htole64(x) LE_64(x)
--#define be64toh(x) BE_IN64(x)
--#define le64toh(x) LE_IN64(x)
-+#define be64toh(x) BE_IN64(&(x))
-+#define le64toh(x) LE_IN64(&(x))
- 
- #endif
  
