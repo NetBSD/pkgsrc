339c9224dda7066c4b2175394f4115b58ba7d5bd
bouyer <bouyer@pkgsrc.org>
2017-08-24 13:01:32 +0000

    We need -Wl,-Bsymbolic in the linker flags for NetBSD too, not only linux. The reason is that a plugin may include its private version of some code that opencpn also has in its source. Without -Bsymbolic the plugin may use the objects from opencpn instead of its version, and which do not match the includes it was compiled with, and bad things happens. bump PKGREVISIONs

--- geography/opencpn-plugin-statusbar/patches/patch-cmake_PluginConfigure.cmake
+++ geography/opencpn-plugin-statusbar/patches/patch-cmake_PluginConfigure.cmake
@@ -1,22 +1,17 @@
-$NetBSD: patch-cmake_PluginConfigure.cmake,v 1.1 2017/04/14 16:38:54 bouyer Exp $
+$NetBSD: patch-cmake_PluginConfigure.cmake,v 1.2 2017/08/24 13:01:33 bouyer Exp $
 Do not overwrite CMAKE_SHARED_LINKER_FLAGS
 
 --- cmake/PluginConfigure.cmake.orig	2016-05-09 14:08:57.000000000 +0200
-+++ cmake/PluginConfigure.cmake	2017-04-14 17:00:15.000000000 +0200
-@@ -34,11 +34,13 @@
-  ADD_DEFINITIONS( "-Wall -Wno-unused-result -g -O2 -fexceptions" )
++++ cmake/PluginConfigure.cmake	2017-08-24 11:50:45.935411441 +0200
+@@ -35,9 +35,9 @@
   ENDIF(PROFILING)
  
-- IF(NOT APPLE)
-+ IF(CMAKE_SYSTEM_NAME MATCHES ".*Linux")
-   SET(CMAKE_SHARED_LINKER_FLAGS "-Wl,-Bsymbolic")
-- ELSE(NOT APPLE)
-+  ADD_DEFINITIONS( "-fPIC")
-+ ENDIF(CMAKE_SYSTEM_NAME MATCHES ".*Linux")
-+ if(APPLE)
-   SET(CMAKE_SHARED_LINKER_FLAGS "-Wl -undefined dynamic_lookup")
-- ENDIF(NOT APPLE)
-+ ENDIF(APPLE)
+  IF(NOT APPLE)
+-  SET(CMAKE_SHARED_LINKER_FLAGS "-Wl,-Bsymbolic")
++  SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-Bsymbolic")
+  ELSE(NOT APPLE)
+-  SET(CMAKE_SHARED_LINKER_FLAGS "-Wl -undefined dynamic_lookup")
++  SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl -undefined dynamic_lookup")
+  ENDIF(NOT APPLE)
  
  ENDIF(NOT MSVC)
- 
