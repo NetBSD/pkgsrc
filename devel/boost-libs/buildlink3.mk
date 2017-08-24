7d3c9ee7c75a0b8b5a8b93414f55e5fbd2a6ad92
adam <adam@pkgsrc.org>
2017-08-24 19:31:31 +0000

    Version 1.65.0 New Libraries: PolyCollection, Stacktrace. Updated Libraries: Asio, Context, Config, Core, Coroutine2, DLL, Fiber, Fusion, Geometry, Hash, Log, Phoenix, Predef, Program Options, Python, Smart Pointers, Test, TypeIndex, Unordered, Utility, Variant.

--- devel/boost-libs/buildlink3.mk
+++ devel/boost-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.43 2017/04/29 18:31:42 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.44 2017/08/24 19:31:32 adam Exp $
 
 BUILDLINK_TREE+=	boost-libs
 
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	boost-libs
 BOOST_LIBS_BUILDLINK3_MK:=
 
 # Use a dependency pattern that guarantees the proper ABI.
-BUILDLINK_API_DEPENDS.boost-libs+=	boost-libs-1.64.*
-BUILDLINK_ABI_DEPENDS.boost-libs?=	boost-libs>=1.64.0
+BUILDLINK_API_DEPENDS.boost-libs+=	boost-libs-1.65.*
+BUILDLINK_ABI_DEPENDS.boost-libs?=	boost-libs>=1.65.0
 BUILDLINK_PKGSRCDIR.boost-libs?=	../../devel/boost-libs
 
 .include "../../mk/bsd.fast.prefs.mk"
---
+++ devel/boost-mpi/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2017/08/24 19:31:32 adam Exp $
+
+BUILDLINK_TREE+=	boost-mpi
+
+.if !defined(BOOST_MPI_BUILDLINK3_MK)
+BOOST_MPI_BUILDLINK3_MK:=
+
+# Use a dependency pattern that guarantees the proper ABI.
+BUILDLINK_API_DEPENDS.boost-mpi+=	boost-mpi-1.65.*
+BUILDLINK_ABI_DEPENDS.boost-mpi+=	boost-mpi-1.65.*
+BUILDLINK_PKGSRCDIR.boost-mpi?=		../../devel/boost-mpi
+
+.include "../../devel/boost-libs/buildlink3.mk"
+.include "../../mk/mpi.buildlink3.mk"
+.endif # BOOST_MPI_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-boost-mpi
