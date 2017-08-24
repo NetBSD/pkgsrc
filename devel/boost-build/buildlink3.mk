3752d737c8a71f5ad750fdb2a789a9af3918112e
adam <adam@pkgsrc.org>
2017-08-24 19:31:31 +0000

    Version 1.65.0 New Libraries: PolyCollection, Stacktrace. Updated Libraries: Asio, Context, Config, Core, Coroutine2, DLL, Fiber, Fusion, Geometry, Hash, Log, Phoenix, Predef, Program Options, Python, Smart Pointers, Test, TypeIndex, Unordered, Utility, Variant.

--- devel/boost-build/buildlink3.mk
+++ devel/boost-build/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.35 2017/04/29 18:31:42 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2017/08/24 19:31:31 adam Exp $
 
 BUILDLINK_TREE+=	boost-build
 
 .if !defined(BOOST_BUILD_BUILDLINK3_MK)
 BOOST_BUILD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.64.*
+BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.65.*
 BUILDLINK_DEPMETHOD.boost-build?=	build
 BUILDLINK_PKGSRCDIR.boost-build?=	../../devel/boost-build
 .endif # BOOST_BUILD_BUILDLINK3_MK
