8bdc8840f8730dcfadeb94f17ef4d4cd2bf38422
adam <adam@pkgsrc.org>
2017-08-25 05:28:53 +0000

    Release 1.4.18: * Diagnose --word-regexp as unsupported if it was not configured. * Preliminary support for OS/2. * A number of portability improvements inherited from gnulib.

--- devel/m4/patches/patch-lib_stdio.in.h
+++ devel/m4/patches/patch-lib_stdio.in.h
@@ -1,16 +1,16 @@
-$NetBSD: patch-lib_stdio.in.h,v 1.3 2013/10/04 21:02:11 wiz Exp $
+$NetBSD: patch-lib_stdio.in.h,v 1.4 2017/08/25 05:28:53 adam Exp $
 
 glibc 2.16's /usr/include/stdio.h does not provide gets(3) anymore,
 and this line causes breakage.
 From: http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/sys-devel/m4/files/m4-1.4.16-no-gets.patch
 
---- lib/stdio.in.h.orig	2013-09-22 06:22:58.000000000 +0000
+--- lib/stdio.in.h.orig	2016-12-31 13:54:42.000000000 +0000
 +++ lib/stdio.in.h
-@@ -719,7 +719,6 @@ _GL_WARN_ON_USE (getline, "getline is un
+@@ -744,7 +744,6 @@ _GL_WARN_ON_USE (getline, "getline is un
     removed it.  */
  #undef gets
- #if HAVE_RAW_DECL_GETS
+ #if HAVE_RAW_DECL_GETS && !defined __cplusplus
 -_GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
  #endif
  
- 
+ #if @GNULIB_OBSTACK_PRINTF@ || @GNULIB_OBSTACK_PRINTF_POSIX@
