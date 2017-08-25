8cbb3118955f88a1d296be2792ec27c429f5be21
adam <adam@pkgsrc.org>
2017-08-25 05:16:35 +0000

    Bump depenency

--- devel/py-boost/buildlink3.mk
+++ devel/py-boost/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2017/04/29 18:31:42 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2017/08/25 05:16:35 adam Exp $
 
 BUILDLINK_TREE+=	py-boost
 
@@ -8,8 +8,8 @@ PY_BOOST_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 # Use a dependency pattern that guarantees the proper ABI.
-BUILDLINK_API_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.64.*
-BUILDLINK_ABI_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.64.*
+BUILDLINK_API_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.65.*
+BUILDLINK_ABI_DEPENDS.py-boost+=	${PYPKGPREFIX}-boost-1.65.*
 BUILDLINK_PKGSRCDIR.py-boost?=	../../devel/py-boost
 
 .include "../../devel/boost-headers/buildlink3.mk"
