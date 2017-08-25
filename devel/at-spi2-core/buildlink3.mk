285c16cf58f19a7da5368a8849e55f6f7c6cf9c9
prlw1 <prlw1@pkgsrc.org>
2017-08-25 12:17:00 +0000

    Update at-spi2-core to 2.25.91
    This has several fixes which aren't in the "stable" 2.24.1
    
    
    What's new in at-spi2-core 2.25.91:
    
    * Meson builds should now be usable.
    
    What's new in at-spi2-core 2.25.90:
    
    * Fix a couple of introspection issues (bgo#784481).
    * atspi_get_a11_bus: don't leak the DBusConnection.
    * Meson fixes.
    
    What's new in at-spi2-core 2.25.4:
    
    * Fix gir generation with autotools (bgo#783994).
    
    What's new in at-spi2-core 2.25.3:
    
    * Fix -Wmisleading-indentation warnings.
    * Fix memory leak of at-spi-bus-launcher.
    * Add error-message, error-for, details, and details-for relation types.
    * Poll direct dbus connections in the main loop--fixes processes
      being marked hung and the hung flag never being removed.
    * Add Meson build system (thanks to Emmanuele Bassi).
    * Various build fixes (thanks to Emmanuele Bassi).
    
    What's new in at-spi2-core 2.25.2:
    
    * attempt to fix an occasional crash when an application disappears
      (bgo#767074).
    * Add some missing roles to correspond with atk (description list,
      description term, description value, and footnote).
    
    What's new in at-spi2-core 2.25.1:
    
    * atspi_table_cell_get_position: don't crash on error.
    
    What's new in at-spi2-core 2.23.92:
    
    * Table cell API fixes (bgo#779835).
    
    What's new in at-spi2-core 2.23.90:
    
    * Fix an occasional crash when an application is closed (bgo#767074).
    
    What's new in at-spi2-core 2.23.4:
    
    * Don't pull in X headers if x11 is disabled (bgo#773710).
    * at-spi-bus-launcher: session management fixes (bgo#774441).
    * events: add recently added page changed event (bgo#719898).
    * roles: EXTENDED roles are deprecated (bgo#W720123).

--- devel/at-spi2-core/buildlink3.mk
+++ devel/at-spi2-core/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2012/10/08 20:23:48 prlw1 Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/08/25 12:17:00 prlw1 Exp $
 
 BUILDLINK_TREE+=	at-spi2-core
 
@@ -11,6 +11,9 @@ BUILDLINK_PKGSRCDIR.at-spi2-core?=	../../devel/at-spi2-core
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../sysutils/dbus/buildlink3.mk"
+.include "../../x11/libXi/buildlink3.mk"
+.include "../../x11/libXtst/buildlink3.mk"
+.include "../../x11/libxkbcommon/buildlink3.mk"
 .endif # AT_SPI2_CORE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-at-spi2-core
