ef0f69bc6f21049997e70743e67ac8560ad9ca2d
jperkin <jperkin@pkgsrc.org>
2017-08-25 16:49:11 +0000

    Remove trailing empty lines.

--- devel/go-blackfriday/buildlink3.mk
+++ devel/go-blackfriday/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/08/17 01:17:16 gavan Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/08/25 16:49:12 jperkin Exp $
 
 BUILDLINK_TREE+=	go-blackfriday
 
@@ -15,4 +15,3 @@ BUILDLINK_PKGSRCDIR.go-blackfriday?=		../../devel/go-blackfriday
 .endif  # GO_BLACKFRIDAY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-go-blackfriday
-
