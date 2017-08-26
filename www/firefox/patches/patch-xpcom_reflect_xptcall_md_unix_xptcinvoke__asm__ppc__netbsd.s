9d76898928de88991a44963dbe2cd82555f9aa04
he <he@pkgsrc.org>
2017-08-26 10:36:01 +0000

    Add some patches to get us closer to building on NetBSD/powerpc:  * moz.build: CONFIG['OS_TEST'] is apparently PCU, not MACHINE, so use    'powerpc' instead of the longish list of powerpc ports.  * xptcinvoke_asm_ppc_netbsd.s: adapt to use of NS_InvokeByIndex()  * xptcinvoke_ppc_netbsd.cpp: adapt to use of NS_InvokeByIndex()  * xptcstubs_ppc_netbsd.cpp: adapt in the direction of xptcstubs_ppc_linux.cpp;    this has apparently not been build-tested in a while. The current stumbling block is the lack of 64-bit atomic operations. No PKGREVISION bump as this is a partial build fix only for NetBSD/powerpc.

--- www/firefox/patches/patch-xpcom_reflect_xptcall_md_unix_xptcinvoke__asm__ppc__netbsd.s
+++ www/firefox/patches/patch-xpcom_reflect_xptcall_md_unix_xptcinvoke__asm__ppc__netbsd.s
@@ -1,4 +1,6 @@
-$NetBSD: patch-xpcom_reflect_xptcall_md_unix_xptcinvoke__asm__ppc__netbsd.s,v 1.1 2014/10/15 13:43:32 ryoon Exp $
+$NetBSD: patch-xpcom_reflect_xptcall_md_unix_xptcinvoke__asm__ppc__netbsd.s,v 1.2 2017/08/26 10:36:01 he Exp $
+
+Adapt to the use of NS_InvokeByIndex instead of XPTC_InvokeByIndex.
 
 --- xpcom/reflect/xptcall/md/unix/xptcinvoke_asm_ppc_netbsd.s.orig	2014-10-11 09:06:50.000000000 +0000
 +++ xpcom/reflect/xptcall/md/unix/xptcinvoke_asm_ppc_netbsd.s
@@ -8,18 +10,18 @@ $NetBSD: patch-xpcom_reflect_xptcall_md_unix_xptcinvoke__asm__ppc__netbsd.s,v 1.
  	.align 2
 -	.globl XPTC_InvokeByIndex
 -	.type  XPTC_InvokeByIndex,@function
-+	.globl NS_InvokeByIndex_P
-+	.type  NS_InvokeByIndex_P,@function
++	.globl NS_InvokeByIndex
++	.type  NS_InvokeByIndex,@function
  
  #
 -# XPTC_InvokeByIndex(nsISupports* that, uint32_t methodIndex,
 -#                    uint32_t paramCount, nsXPTCVariant* params)
-+# NS_InvokeByIndex_P(nsISupports* that, PRUint32 methodIndex,
++# NS_InvokeByIndex(nsISupports* that, PRUint32 methodIndex,
 +#                    PRUint32 paramCount, nsXPTCVariant* params)
  #
  
 -XPTC_InvokeByIndex:
-+NS_InvokeByIndex_P:
++NS_InvokeByIndex:
  	stwu    sp,-32(sp)			# setup standard stack frame
  	mflr    r0				# save LR
  	stw     r3,8(sp)			# r3 <= that
