a7e2f6beff0bce146981d5a49657cface1c811bd
fhajny <fhajny@pkgsrc.org>
2017-08-26 10:07:27 +0000

    Update net/freeradius to 3.0.15.
    Based on a PR from @coyhile
    (https://github.com/joyent/pkgsrc/issues/18). Splits modules with
    external dependencies into separate packages.
    
    The 1.1.x branch was EOL'd in 2008. No upgrade guide from 1.1.x to 3.0.x
    seem to exist.
    
    Summary of improvements in 3.x:
    
    - Moved configuration entries in radiusd.conf to make more sense.
    - Added the "integer64" and "ipv4prefix" data types.
    - Added RADIUS over TLS (i.e. RadSec). See raddb/sites-available/tls.
    - Updated internal API to support new attributes and formats.
    - Added code to send SNMP Traps. See raddb/trigger.conf.
    - Added preliminary support for Apple's Grand Central Dispatch.
    - Added provisions for raddb/dictionary.local, for local changes See
      raddb/dictionary for more details.
    - Added packet/s tracking. See max_pps in the "listen" section.
    - The %{} expansions and "unlang" conditions are now parsed at server
      start. Descriptive errors are produced for syntax and format errors.
    - Casting is now supported for "unlang" comparisons. See "man unlang"
      e.g. <ipaddr>127.0.0.1 == Framed-IP-Address.
    - Direct comparison of attribute references is now supported e.g. &Foo
      == &Bar. This avoids stringification of the attributes.
    - Direct assignment of attributes is now supported e.g. Foo := &Bar. It
      also works for "octets" data types.
    - Comparisons of IPv4 and IPv6 prefixes are now supported The "<"
      operator means "within the prefix" for comparisons.
    - New sha1 xlat expansion (thanks to Alan Buxey).
    - Colourised log messages when logging to stdout. Look for yellow
      warnings and red errors. Doing this will save you a LOT of grief.
    - If the PCRE library is available, use it (insted of the POSIX
      functions) to process regular expressions (thanks to Phil Mayers).
    - -xv now displays all the features the server was built with, and the
      versions of the core libraries (libtalloc, libssl).
    
    Summary of improvements in 2.x:
    
    - simple policy language (see "man unlang")
    - virtual servers ("raddb/sites-available/README")
    - IPv6 support
    - better proxy support ("raddb/proxy.conf")
    - More EAP types
    - Debugging output should be <em>much</em> easier to understand
    - VMPS support
    - More modules have been moved to "stable" status (python, etc.)
    - SQL configuration has been cleaned up (see "raddb/sql/*")
    - limited support for HUP.  (The configuration for some modules is
      re-loaded on HUP.  Nothing else is reloaded.)
    - check configuration and exit ("radiusd -C")
    - Server core is now event based (simpler, more powerful)

---
+++ net/freeradius/patches/patch-main_util.c
@@ -0,0 +1,20 @@
+$NetBSD: patch-main_util.c,v 1.1 2017/08/26 10:07:28 fhajny Exp $
+O_DIRECTORY to open(2) is a Linuxism.
+
+--- src/main/util.c.orig	2016-09-29 15:19:48.000000000 +0000
++++ src/main/util.c
+@@ -31,6 +31,14 @@ RCSID("$Id: 22299f8c8d6bc98616fa025ee3da
+ #include <fcntl.h>
+ 
+ /*
++ *      Not all libc implmenetations have O_DIRECTORY to pass to
++ *      open(2)
++ */
++#ifndef O_DIRECTORY
++#define O_DIRECTORY 0
++#endif
++
++/*
+  *	The signal() function in Solaris 2.5.1 sets SA_NODEFER in
+  *	sa_flags, which causes grief if signal() is called in the
+  *	handler before the cause of the signal has been cleared.
