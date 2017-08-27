b58f4313b1a478a6cdac137eb4e5cd8c8a4a9978
adam <adam@pkgsrc.org>
2017-08-27 12:12:25 +0000

    5.0.1:
    This bugfix release fixes several issues, most of which are obscure enough that typical applications are not impacted.
    
    Bug fixes:
    Update decay->nunpurged before purging, in order to avoid potential update races and subsequent incorrect purging volume.
    Only abort on dlsym(3) error if the failure impacts an enabled feature (lazy locking and/or background threads). This mitigates an initialization failure bug for which we still do not have a clear reproduction test case.
    Modify tsd management so that it neither crashes nor leaks if a thread's only allocation activity is to call free() after TLS destructors have been executed. This behavior was observed when operating with GNU libc, and is unlikely to be an issue with other libc implementations.
    Mask signals during background thread creation. This prevents signals from being inadvertently delivered to background threads.
    Avoid inactivity checks within background threads, in order to prevent recursive mutex acquisition.
    Fix extent_grow_retained() to use the specified hooks when the arena.<i>.extent_hooks mallctl is used to override the default hooks.
    Add missing reentrancy support for custom extent hooks which allocate.
    Post-fork(2), re-initialize the list of tcaches associated with each arena to contain no tcaches except the forking thread's.
    Add missing post-fork(2) mutex reinitialization for extent_grow_mtx. This fixes potential deadlocks after fork(2).
    Enforce minimum autoconf version (currently 2.68), since 2.63 is known to generate corrupt configure scripts.
    Ensure that the configured page size (--with-lg-page) is no larger than the configured huge page size (--with-lg-hugepage).

--- devel/jemalloc/patches/patch-src_pages.c
+++ devel/jemalloc/patches/patch-src_pages.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_pages.c,v 1.1 2017/05/17 16:36:46 gson Exp $
+$NetBSD: patch-src_pages.c,v 1.2 2017/08/27 12:12:25 adam Exp $
 
 Set os_overcommits to true on NetBSD since NetBSD does overcommit.
 This also has the benefit of not triggering the issue reported in
 https://github.com/jemalloc/jemalloc/issues/837 .
 
---- src/pages.c.orig	2017-03-01 03:10:36.000000000 +0000
+--- src/pages.c.orig	2017-07-02 00:44:25.000000000 +0000
 +++ src/pages.c
-@@ -296,6 +296,8 @@ pages_boot(void)
- 	if (os_overcommits)
+@@ -414,6 +414,8 @@ pages_boot(void) {
  		mmap_flags |= MAP_NORESERVE;
+ 	}
  #  endif
 +#elif defined __NetBSD__
 +	os_overcommits = true;
