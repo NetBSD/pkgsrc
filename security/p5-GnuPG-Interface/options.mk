45b3ddbb6a6638e0040fa2f0880d39759ac1122c
wiz <wiz@pkgsrc.org>
2017-08-29 12:11:36 +0000

    Remove references to gnupg21 in preparation for its removal.

--- security/p5-GnuPG-Interface/options.mk
+++ security/p5-GnuPG-Interface/options.mk
@@ -1,15 +1,13 @@
-# $NetBSD: options.mk,v 1.2 2017/06/08 08:29:30 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2017/08/29 12:11:36 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.p5_GnuPG_Interface
 PKG_OPTIONS_REQUIRED_GROUPS=	gnupg
-PKG_OPTIONS_GROUP.gnupg=	gnupg1 # gnupg2 gnupg21
+PKG_OPTIONS_GROUP.gnupg=	gnupg1 # gnupg2
 PKG_SUGGESTED_OPTIONS=		gnupg1
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mgnupg21)
-DEPENDS+=		gnupg21>=2.1:../../security/gnupg21
-.elif !empty(PKG_OPTIONS:Mgnupg2)
+.if !empty(PKG_OPTIONS:Mgnupg2)
 DEPENDS+=		gnupg2>=2.0<2.1:../../security/gnupg2
 .else
 DEPENDS+=		gnupg>=1.4.2:../../security/gnupg
