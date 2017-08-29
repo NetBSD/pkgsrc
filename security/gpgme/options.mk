6d4f290b5ca9bcbdaa73445f67bcb1154dcb58c0
wiz <wiz@pkgsrc.org>
2017-08-29 12:20:25 +0000

    Fix pattern.

--- security/gpgme/options.mk
+++ security/gpgme/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2017/08/29 12:11:35 wiz Exp $
+# $NetBSD: options.mk,v 1.7 2017/08/29 12:20:25 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.gpgme
 PKG_OPTIONS_REQUIRED_GROUPS=	gnupg
@@ -8,7 +8,7 @@ PKG_SUGGESTED_OPTIONS=		gnupg2
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgnupg2)
-DEPENDS+=		gnupg2>=2.0<2.1:../../security/gnupg2
+DEPENDS+=		gnupg2-[0-9]*:../../security/gnupg2
 REPLACE_SH+=		tests/gpg/pinentry
 GPG_DEFAULT=		${LOCALBASE}/bin/gpg2
 MAKE_FLAGS+=		GPG=${GPG_DEFAULT}
