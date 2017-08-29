b8069f2e87193a32416bf456896e3c204d17e9de
wiz <wiz@pkgsrc.org>
2017-08-29 12:23:24 +0000

    Fix gnupg2 dependency pattern.

--- security/p5-GnuPG-Interface/options.mk
+++ security/p5-GnuPG-Interface/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2017/08/29 12:11:36 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2017/08/29 12:23:24 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.p5_GnuPG_Interface
 PKG_OPTIONS_REQUIRED_GROUPS=	gnupg
@@ -8,7 +8,7 @@ PKG_SUGGESTED_OPTIONS=		gnupg1
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgnupg2)
-DEPENDS+=		gnupg2>=2.0<2.1:../../security/gnupg2
+DEPENDS+=		gnupg2-[0-9]*:../../security/gnupg2
 .else
 DEPENDS+=		gnupg>=1.4.2:../../security/gnupg
 .endif
