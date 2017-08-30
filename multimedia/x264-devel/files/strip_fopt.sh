638830d9e2897c7712cd8594d5029a121b421f53
wiz <wiz@pkgsrc.org>
2017-08-30 19:47:08 +0000

    Strip -pie from nasm/yasm flags. Fixes PIE build.

--- multimedia/x264-devel/files/strip_fopt.sh
+++ multimedia/x264-devel/files/strip_fopt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: strip_fopt.sh,v 1.3 2011/01/19 12:45:12 drochner Exp $
+# $NetBSD: strip_fopt.sh,v 1.4 2017/08/30 19:47:08 wiz Exp $
 #
 # libtool assumes that the compiler can handle the -fPIC flag and the
 # -fno-common. This isn't always true, for example yasm can't handle it.
@@ -8,7 +8,7 @@ command="$1"
 shift
 while [ $# -gt 0 ]; do
 	case "$1" in
-	-fPIC|-fno-common)
+	-fPIC|-fno-common|-pie)
 		;;
 	*)
 		command="$command $1"
