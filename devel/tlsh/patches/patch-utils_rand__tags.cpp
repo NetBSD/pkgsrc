5fd299c57a269ba5b38f73599aaa0f763a0feb37
wiz <wiz@pkgsrc.org>
2017-09-01 07:52:02 +0000

    Replace patch with correct upstream fix.
    Bump PKGREVISION.

--- devel/tlsh/patches/patch-utils_rand__tags.cpp
+++ devel/tlsh/patches/patch-utils_rand__tags.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-utils_rand__tags.cpp,v 1.1 2016/12/17 14:18:32 joerg Exp $
+$NetBSD: patch-utils_rand__tags.cpp,v 1.2 2017/09/01 07:52:02 wiz Exp $
 
-Checking for negative pointers doesn't make sense.
+https://github.com/trendmicro/tlsh/commit/fb076b0f62b620e0de4cf95a5ccb81f21d4abc24#diff-02ad04ab1753a2119772311c29f24d23
 
 --- utils/rand_tags.cpp.orig	2016-12-16 17:30:02.495417195 +0000
 +++ utils/rand_tags.cpp
@@ -9,7 +9,7 @@ Checking for negative pointers doesn't make sense.
  static void rhtml_contents(std::string &htmls, int *ntags, int *ndistinct_tags)
  {
 -	if ((*ntags <= 0) && (ndistinct_tags <= 0))
-+	if ((*ntags <= 0) && (ndistinct_tags == 0))
++	if ((*ntags <= 0) && (*ndistinct_tags <= 0))
  		return; 
  	if (random() % 10 == 1) {
  		anchor(htmls);
