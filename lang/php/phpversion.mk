e0ed81e8d869f7e0be5a202b9086aac55bc97acb
taca <taca@pkgsrc.org>
2017-09-01 10:50:38 +0000

    Update php71 to 7.1.9 (PHP 7.1.9).
    31 Aug 2017, PHP 7.1.9
    
    - Core:
      . Fixed bug #74947 (Segfault in scanner on INF number). (Laruence)
      . Fixed bug #74954 (null deref and segfault in zend_generator_resume()). (Bob)
      . Fixed bug #74725 (html_errors=1 breaks unhandled exceptions). (Andrea)
      . Fixed bug #75063 (Main CWD initialized with wrong codepage). (Anatol)
    
    - cURL:
      . Fixed bug #74125 (Fixed finding CURL on systems with multiarch support).
        (cebe)
    
    - Date:
      . Fixed bug #75002 (Null Pointer Dereference in timelib_time_clone). (Derick)
    
    - Intl:
      . Fixed bug #74993 (Wrong reflection on some locale_* functions). (Sara)
    
    - Mbstring:
      . Fixed bug #71606 (Segmentation fault mb_strcut with HTML-ENTITIES encoding).
        (cmb)
      . Fixed bug #62934 (mb_convert_kana() does not convert iteration marks).
        (Nikita)
      . Fixed bug #75001 (Wrong reflection on mb_eregi_replace). (Fabien
        Villepinte)
    
    - MySQLi:
      . Fixed bug #74968 (PHP crashes when calling mysqli_result::fetch_object with
        an abstract class). (Anatol)
    
    - OCI8:
      . Expose oci_unregister_taf_callback() (Tianfang Yang)
    
    - Opcache:
      . Fixed bug #74980 (Narrowing occurred during type inference). (Laruence)
    
    - phar:
      . Fixed bug #74991 (include_path has a 4096 char limit in some cases).
        (bwbroersma)
    
    - Reflection:
      . Fixed bug #74949 (null pointer dereference in _function_string). (Laruence)
    
    - Session:
      . Fixed bug #74892 (Url Rewriting (trans_sid) not working on urls that start
        with "#"). (Andrew Nester)
      . Fixed bug #74833 (SID constant created with wrong module number). (Anatol)
    
    - SimpleXML:
      . Fixed bug #74950 (nullpointer deref in simplexml_element_getDocNamespaces).
        (Laruence)
    
    - SPL:
      . Fixed bug #75049 (spl_autoload_unregister can't handle
        spl_autoload_functions results). (Laruence)
      . Fixed bug #74669 (Unserialize ArrayIterator broken). (Andrew Nester)
      . Fixed bug #74977 (Appending AppendIterator leads to segfault).
        (Andrew Nester)
      . Fixed bug #75015 (Crash in recursive iterator destructors). (Julien)
    
    - Standard:
      . Fixed bug #75075 (unpack with X* causes infinity loop). (Laruence)
      . Fixed bug #74103 (heap-use-after-free when unserializing invalid array
        size). (Nikita)
      . Fixed bug #75054 (A Denial of Service Vulnerability was found when
        performing deserialization). (Nikita)
    
    - WDDX:
      . Fixed bug #73793 (WDDX uses wrong decimal seperator). (cmb)
    
    - XMLRPC:
      . Fixed bug #74975 (Incorrect xmlrpc serialization for classes with declared
        properties). (blar)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.187 2017/09/01 10:49:14 taca Exp $
+# $NetBSD: phpversion.mk,v 1.188 2017/09/01 10:50:38 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.31
 PHP70_VERSION=	7.0.23
-PHP71_VERSION=	7.1.8
+PHP71_VERSION=	7.1.9
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
