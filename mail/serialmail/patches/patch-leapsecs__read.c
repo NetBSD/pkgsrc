b47cbd021b80740d8674431db84768aa66f2ad81
schmonz <schmonz@pkgsrc.org>
2017-09-01 03:46:36 +0000

    Modernize patch filenames.

--- mail/serialmail/patches/patch-leapsecs__read.c
+++ mail/serialmail/patches/patch-leapsecs__read.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-ab,v 1.2 2012/04/15 00:20:49 schmonz Exp $
+$NetBSD: patch-leapsecs__read.c,v 1.1 2017/09/01 03:46:36 schmonz Exp $
 
 Fix to the included libtai, brought over from devel/libtai:
 
 #include <unistd.h> in leapsecs_read.c so we can read leapsecs.dat.
 
---- leapsecs_read.c.orig	1998-09-04 22:33:37.000000000 -0400
+--- leapsecs_read.c.orig	1998-11-15 02:28:23.000000000 +0000
 +++ leapsecs_read.c
 @@ -2,6 +2,7 @@
  #include <sys/stat.h>
