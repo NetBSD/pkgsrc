13cd98d591415ad974d3f2dfa22acc1940cb7201
tron <tron@pkgsrc.org>
2017-09-02 19:17:37 +0000

    Update "x11-links" package to version 1.06: Add macOS "ibxcb-xkb" shared libraries filenames to fix the build of (at least) the "libxkbcommon" package

--- pkgtools/x11-links/buildlink3.mk
+++ pkgtools/x11-links/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.69 2017/01/25 02:51:35 mrg Exp $
+# $NetBSD: buildlink3.mk,v 1.70 2017/09/02 19:17:37 tron Exp $
 #
 # Don't include this file manually!  It will be included as necessary
 # by bsd.buildlink3.mk.
@@ -12,7 +12,7 @@ BUILDLINK_TREE+=	x11-links
 .if !defined(X11_LINKS_BUILDLINK3_MK)
 X11_LINKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=1.04
+BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=1.06
 BUILDLINK_PKGSRCDIR.x11-links?=	../../pkgtools/x11-links
 BUILDLINK_DEPMETHOD.x11-links?=	build
 
