6a3f704a36e3fcd8af4affa0a40bdc20d3be43b4
leot <leot@pkgsrc.org>
2017-09-03 11:29:10 +0000

    Whitespaces vs tabs police (suggested by pkglint, NFCI)

--- print/cups-filters/buildlink3.mk
+++ print/cups-filters/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/06/14 21:19:58 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/09/03 11:29:10 leot Exp $
 
-BUILDLINK_TREE+=        cups-filters
+BUILDLINK_TREE+=	cups-filters
 
 .if !defined(CUPS_FILTERS_BUILDLINK3_MK)
 CUPS_FILTERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cups-filters+=    cups-filters>=1.8.2
-BUILDLINK_PKGSRCDIR.cups-filters?=      ../../print/cups-filters
+BUILDLINK_API_DEPENDS.cups-filters+=	cups-filters>=1.8.2
+BUILDLINK_PKGSRCDIR.cups-filters?=	../../print/cups-filters
 
 .include "../../print/cups/buildlink3.mk"
 .endif # CUPS_FILTERS_BUILDLINK3_MK
