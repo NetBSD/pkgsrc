41ccad6aacb7fcaaf4b78440432a9ff537243dc2
bsiegert <bsiegert@pkgsrc.org>
2017-09-03 07:12:07 +0000

    Update Go to 1.9.
    The latest Go release, version 1.9, arrives six months after Go 1.8 and
    is the tenth release in the Go 1.x series. There are two changes to the
    language: adding support for type aliases and defining when
    implementations may fuse floating point operations. Most of the changes
    are in the implementation of the toolchain, runtime, and libraries. As
    always, the release maintains the Go 1 promise of compatibility. We
    expect almost all Go programs to continue to compile and run as before.
    
    The release adds transparent monotonic time support, parallelizes
    compilation of functions within a package, better supports test helper
    functions, includes a new bit manipulation package, and has a new
    concurrent map type.
    
    
    There are some instabilities on FreeBSD that are known but not
    understood. These can lead to program crashes in rare cases. See issue
    15658. Any help in solving this FreeBSD-specific issue would be
    appreciated.
    
    Go stopped running NetBSD builders during the Go 1.9 development cycle
    due to NetBSD kernel crashes, up to and including NetBSD 7.1. As Go 1.9
    is being released, NetBSD 7.1.1 is being released with a fix. However,
    at this time we have no NetBSD builders passing our test suite. Any help
    investigating the various NetBSD issues would be appreciated.

--- lang/go/patches/patch-src_crypto_x509_root__bsd.go
+++ lang/go/patches/patch-src_crypto_x509_root__bsd.go
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_crypto_x509_root__bsd.go,v 1.1 2016/01/31 10:17:27 bsiegert Exp $
+$NetBSD: patch-src_crypto_x509_root__bsd.go,v 1.2 2017/09/03 07:12:07 bsiegert Exp $
 
---- src/crypto/x509/root_bsd.go.orig	2016-01-13 19:47:00.000000000 +0000
+--- src/crypto/x509/root_bsd.go.orig	2017-07-24 19:38:15.000000000 +0000
 +++ src/crypto/x509/root_bsd.go
-@@ -11,4 +11,5 @@ var certFiles = []string{
- 	"/usr/local/share/certs/ca-root-nss.crt", // FreeBSD/DragonFly
+@@ -12,4 +12,5 @@ var certFiles = []string{
  	"/etc/ssl/cert.pem",                      // OpenBSD
+ 	"/usr/local/share/certs/ca-root-nss.crt", // DragonFly
  	"/etc/openssl/certs/ca-certificates.crt", // NetBSD
 +	"/etc/ssl/certs/ca-certificates.crt",     // NetBSD
  }
