b4f0418c46ba5903343acada1376700764e28276
jaapb <jaapb@pkgsrc.org>
2017-09-03 11:59:15 +0000

    Updated package to latest version, 2.7.0. Changes include:
    s minor release is the first to support OCaml 4.05.0. As with
    previous OCaml versions, we backported new 4.05.0 convenience function
    from the compiler stdlib, allowing Batteries user to use them with
    older OCaml versions, and thus write backward-compatible code. In
    particular, the new *_opt functions returning option values instead of
    exceptions are all backported.
    
    - BatNum: fix of_float_string to handle negative numbers properly
      #780
      (Anton Yabchinskiy)
    
    - added BatArray.min_max
      #757
      (Francois Berenger)
    
    - added a Label module to BatVect
      #763
      (Varun Gandhi, review by Francois Berenger, Gabriel Scherer, Thibault Suzanne)
    
    - fix documentation of BatVect.insert to match (correct) implementation
      #766, #767
      (Gabriel Scherer, report by Varun Gandhi)
    
    - avoid using exceptions for internal control-flow
      #768, #769
        This purely internal change should improve performances when using
        js_of_ocaml, which generates much slower code for local exceptions
        raising/catching than the native OCaml backend.
        Internal exceptions (trough the BatReturn label) have been removed
        from the modules BatString, BatSubstring and BatVect.
      (Gabriel Scherer, request and review by Clément Pit-Claudel)
    
    - added `BatVect.find_opt : ('a -> bool) -> 'a t -> 'a option`
      and BatVect.Make.find_opt
      #769
      (Gabriel Scherer)
    
    - Documents exceptions for List.(min, max)
      #770
      (Varun Gandhi)
    
    - BatText: bugfixes in `rindex{,_from}` and `rcontains_from`
      #775
      (Gabriel Scherer)
    
    - Support for the new OCaml release 4.05
      the `*_opt` functions and List.compare_lengths, compare_length_with
      are also backported to older OCaml releases, so code using them from
      Batteries should be backwards-compatible
      #777, #779
      (Tej Chajed, Gabriel Scherer)

--- devel/ocaml-batteries/buildlink3.mk
+++ devel/ocaml-batteries/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/07/11 11:34:25 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/09/03 11:59:15 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-batteries
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-batteries
 OCAML_BATTERIES_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-batteries+=	ocaml-batteries>=2.0
-BUILDLINK_ABI_DEPENDS.ocaml-batteries+=	ocaml-batteries>=2.6.0
+BUILDLINK_ABI_DEPENDS.ocaml-batteries+=	ocaml-batteries>=2.7.0
 BUILDLINK_PKGSRCDIR.ocaml-batteries?=	../../devel/ocaml-batteries
 
 .endif	# OCAML_BATTERIES_BUILDLINK3_MK
