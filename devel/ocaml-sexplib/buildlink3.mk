358cb9e1d54b7b39c1774e9a4e2dea4cb54ae43a
jaapb <jaapb@pkgsrc.org>
2017-09-03 12:56:01 +0000

    Updated package to latest version, 0.9.2. I cannot find a changelog (or at least not one that is up to date).

--- devel/ocaml-sexplib/buildlink3.mk
+++ devel/ocaml-sexplib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 12:14:13 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2017/09/03 12:56:01 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-sexplib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-sexplib
 OCAML_SEXPLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=0.9.1
+BUILDLINK_ABI_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=0.9.2
 BUILDLINK_PKGSRCDIR.ocaml-sexplib?=	../../devel/ocaml-sexplib
 
 .endif	# OCAML_SEXPLIB_BUILDLINK3_MK
