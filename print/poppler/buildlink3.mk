5fc0fef9364252df49837b8a96d1ab8a19e93287
wiz <wiz@pkgsrc.org>
2017-09-04 15:08:29 +0000

    Update poppler* to 0.58.0:
    Release 0.58.0
            core:
             * CairoOutputDev: cairo 1.14 now has high quality downscaling
             * Signature related improvements. Bug #99271
             * Tweak which cmap we use. Bug #101855
             * Memory leak fixes
             * Substantial rework of the internals
             * win32: call ANSI functions directly. Bug #100312
             * Add some documentation
    
            qt5:
             * Expose signature information.
             * ArthurOutputDev: initialize the image with the paper color. Bug #102129
             * Fix copy'n'paste bugs: Qt4 -> Qt5
             * ArthurOutputDev: Properly set the QPainter transformation
             * ArthurOutputDev: Use Qt::SvgMiterJoin instead of Qt::MiterJoin. Bug #102356
    
            utils:
             * pdfinfo: add -dests option to print named destinations. Bug #97262
             * pdftocairo: add -jpegopt for setting jpeg compression parameters. Bug #45727
             * pdftoppm: add -jpegopt for setting jpeg compression parameters. Bug #45727
             * pdfimages: support listing/extracting inline images. Bug #25625
    
            build system:
             * cmake: Various Windows fixes
             * cmake: Use -std=c++11 instead of -std=gnu++11
    
            cpp:
             * Fix page.text() not taking page orientation into account. Bug #94517

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.54 2017/08/15 10:44:58 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.55 2017/09/04 15:08:29 wiz Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.57.0
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.58.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 .include "../../graphics/lcms2/buildlink3.mk"
