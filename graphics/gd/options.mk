badc12d1e2e89bc4b5a8a9a246babcefaafd9bce
adam <adam@pkgsrc.org>
2017-09-04 06:20:45 +0000

    Changes 2.2.5:
    Security
    * Double-free in gdImagePngPtr(). (CVE-2017-6362)
    * Buffer over-read into uninitialized memory. (CVE-2017-7890)
    
    Fixed
    * Fix 109: XBM reading fails with printed error
    * Fix 338: Fatal and normal libjpeg/ibpng errors not distinguishable
    * Fix 357: 2.2.4: Segfault in test suite
    * Fix 386: gdImageGrayScale() may produce colors
    * Fix 406: webpng -i removes the transparent color
    * Fix Coverity 155475: Failure to restore alphaBlendingFlag
    * Fix Coverity 155476: potential resource leak
    * Fix several build issues and test failures
    * Fix and reenable optimized support for reading 1 bps TIFFs
    
    Added
    * The native MSVC buildchain now supports libtiff and most executables

--- graphics/gd/options.mk
+++ graphics/gd/options.mk
@@ -1,17 +1,9 @@
-# $NetBSD: options.mk,v 1.6 2017/04/15 15:50:42 kim Exp $
+# $NetBSD: options.mk,v 1.7 2017/09/04 06:20:45 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gd
-PKG_SUPPORTED_OPTIONS=	tiff x11
-PKG_SUGGESTED_OPTIONS=	tiff
+PKG_SUPPORTED_OPTIONS=	x11
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Mtiff)
-.include "../../graphics/tiff/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-tiff=${BUILDLINK_PREFIX.tiff}
-.else
-CONFIGURE_ARGS+=	--without-tiff
-.endif
 
 .if !empty(PKG_OPTIONS:Mx11)
 .include "../../x11/libXpm/buildlink3.mk"
