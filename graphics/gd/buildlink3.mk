3d8c18a8409c758f4df91f0f29fd44ecd9427954
adam <adam@pkgsrc.org>
2017-09-04 06:20:45 +0000

    Changes 2.2.5:
    Security
    * Double-free in gdImagePngPtr(). (CVE-2017-6362)
    * Buffer over-read into uninitialized memory. (CVE-2017-7890)
    
    Fixed
    * Fix 109: XBM reading fails with printed error
    * Fix 338: Fatal and normal libjpeg/ibpng errors not distinguishable
    * Fix 357: 2.2.4: Segfault in test suite
    * Fix 386: gdImageGrayScale() may produce colors
    * Fix 406: webpng -i removes the transparent color
    * Fix Coverity 155475: Failure to restore alphaBlendingFlag
    * Fix Coverity 155476: potential resource leak
    * Fix several build issues and test failures
    * Fix and reenable optimized support for reading 1 bps TIFFs
    
    Added
    * The native MSVC buildchain now supports libtiff and most executables

--- graphics/gd/buildlink3.mk
+++ graphics/gd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.41 2017/04/15 15:50:42 kim Exp $
+# $NetBSD: buildlink3.mk,v 1.42 2017/09/04 06:20:45 adam Exp $
 
 BUILDLINK_TREE+=	gd
 
@@ -14,10 +14,6 @@ BUILDLINK_PKGSRCDIR.gd?=	../../graphics/gd
 pkgbase := gd
 .include "../../mk/pkg-build-options.mk"
 
-.if !empty(PKG_BUILD_OPTIONS.gd:Mtiff)
-.include "../../graphics/tiff/buildlink3.mk"
-.endif
-
 .if !empty(PKG_BUILD_OPTIONS.gd:Mx11)
 .include "../../x11/libXpm/buildlink3.mk"
 .endif
@@ -25,8 +21,10 @@ pkgbase := gd
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/libimagequant/buildlink3.mk"
 .include "../../graphics/libwebp/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 .endif # GD_BUILDLINK3_MK
