12d49759aa31f095445b3172963b99256e4ebba2
joerg <joerg@pkgsrc.org>
2017-09-04 15:56:33 +0000

    Use ASCII.

---
+++ converters/py-cairosvg/patches/patch-cairosvg_____init____.py
@@ -0,0 +1,11 @@
+$NetBSD: patch-cairosvg_____init____.py,v 1.1 2017/09/04 15:56:33 joerg Exp $
+
+--- cairosvg/__init__.py.orig	2017-08-27 00:23:26.754329800 +0000
++++ cairosvg/__init__.py
+@@ -1,5 +1,5 @@
+ # This file is part of CairoSVG
+-# Copyright © 2010-2015 Kozea
++# Copyright (C) 2010-2015 Kozea
+ #
+ # This library is free software: you can redistribute it and/or modify it under
+ # the terms of the GNU Lesser General Public License as published by the Free
