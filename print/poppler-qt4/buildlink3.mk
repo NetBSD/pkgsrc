9c1cf47f809ccbedb4467647432de3f0356b55b3
ryoon <ryoon@pkgsrc.org>
2017-09-04 19:57:18 +0000

    Update to 0.59.0
    Changelog:
    Release 0.59.0
            core:
             * Fix infinite recursion in NameTree parsing in broken files
    
            utils:
             * pdfunite: Fix API porting error that caused abort in some cases
             * pdfinfo: Fix crashes and memory leaks when using -dests
             * pdfinfo: use GooString.append instead of sprintf/strcat
             * pdfimages: Fix warning when compiling with cygwin
    
            build system:
             * Fix cygwin 32-bit compile
             * cmake tweaks

--- print/poppler-qt4/buildlink3.mk
+++ print/poppler-qt4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.37 2017/09/04 15:08:29 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.38 2017/09/04 19:57:18 ryoon Exp $
 
 BUILDLINK_TREE+=	poppler-qt4
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-qt4
 POPPLER_QT4_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-qt4+=	poppler-qt4>=0.6.1
-BUILDLINK_ABI_DEPENDS.poppler-qt4+=	poppler-qt4>=0.58.0
+BUILDLINK_ABI_DEPENDS.poppler-qt4+=	poppler-qt4>=0.59.0
 BUILDLINK_PKGSRCDIR.poppler-qt4?=	../../print/poppler-qt4
 
 .include "../../print/poppler/buildlink3.mk"
