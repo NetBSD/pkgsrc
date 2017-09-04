1685aecb46a43cad419c9f6b539e1bd5f7ca80f2
ryoon <ryoon@pkgsrc.org>
2017-09-04 19:57:18 +0000

    Update to 0.59.0
    Changelog:
    Release 0.59.0
            core:
             * Fix infinite recursion in NameTree parsing in broken files
    
            utils:
             * pdfunite: Fix API porting error that caused abort in some cases
             * pdfinfo: Fix crashes and memory leaks when using -dests
             * pdfinfo: use GooString.append instead of sprintf/strcat
             * pdfimages: Fix warning when compiling with cygwin
    
            build system:
             * Fix cygwin 32-bit compile
             * cmake tweaks

--- print/poppler-glib/buildlink3.mk
+++ print/poppler-glib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.56 2017/09/04 15:08:29 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.57 2017/09/04 19:57:18 ryoon Exp $
 
 BUILDLINK_TREE+=	poppler-glib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-glib
 POPPLER_GLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-glib+=	poppler-glib>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler-glib+=	poppler-glib>=0.58.0
+BUILDLINK_ABI_DEPENDS.poppler-glib+=	poppler-glib>=0.59.0
 BUILDLINK_PKGSRCDIR.poppler-glib?=	../../print/poppler-glib
 
 .include "../../devel/glib2/buildlink3.mk"
