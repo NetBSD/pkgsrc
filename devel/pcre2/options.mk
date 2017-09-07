ed5a8e4499af0e4da027f3b4f816aac2ec2bcbe9
wiz <wiz@pkgsrc.org>
2017-09-07 13:47:21 +0000

    Make JIT an option. Enable it by default on the list of platforms martin@ extracted from the source, see PR 52524.
    The pattern matching and/or the list itself may need further fixes.
    
    Retest on NetBSD 8 shows that the binaries are now mprotect-safe,
    remove variable.
    
    Bump PKGREVISION.

---
+++ devel/pcre2/options.mk
@@ -0,0 +1,19 @@
+# $NetBSD: options.mk,v 1.1 2017/09/07 13:47:21 wiz Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.pcre2
+PKG_SUPPORTED_OPTIONS=	pcre2-jit
+
+.if (${MACHINE_ARCH} == "i386" || \
+    ${MACHINE_ARCH} == "x86_64" || \
+    ${MACHINE_ARCH} == "mips" || \
+    ${MACHINE_ARCH} == "powerpc" || \
+    ${MACHINE_ARCH} == "sparc" || \
+    (!empty(MACHINE_ARCH:Marm) && empty(MACHINE_ARCH:Marmv4)))
+PKG_SUGGESTED_OPTIONS=	pcre2-jit
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mpcre2-jit)
+CONFIGURE_ARGS+=	--enable-jit
+.endif
