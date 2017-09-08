00d8158bf2df3818fa3b3513a024921a55405d99
jaapb <jaapb@pkgsrc.org>
2017-09-08 09:51:18 +0000

    Recursive revbump associated with update of ocaml to 4.05

--- devel/ocaml-type_conv/buildlink3.mk
+++ devel/ocaml-type_conv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2017/07/11 14:19:19 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2017/09/08 09:51:22 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-type_conv
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-type_conv
 OCAML_TYPE_CONV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-type_conv+=	ocaml-type_conv>=109.60.01
-BUILDLINK_ABI_DEPENDS.ocaml-type_conv?=	ocaml-type_conv>=113.00.02nb5
+BUILDLINK_ABI_DEPENDS.ocaml-type_conv?=	ocaml-type_conv>=113.00.02nb6
 BUILDLINK_PKGSRCDIR.ocaml-type_conv?=	../../devel/ocaml-type_conv
 
 .include "../../lang/ocaml/buildlink3.mk"
