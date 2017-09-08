6644ac1a7f6c31bf9ef827fa06ecefdbd92b6244
jaapb <jaapb@pkgsrc.org>
2017-09-08 16:55:19 +0000

    Stubs library installed in right location

--- devel/js_of_ocaml/buildlink3.mk
+++ devel/js_of_ocaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2017/09/08 13:19:34 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/09/08 16:55:19 jaapb Exp $
 
 BUILDLINK_TREE+=	js_of_ocaml
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	js_of_ocaml
 JS_OF_OCAML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.js_of_ocaml+=	js_of_ocaml>=3.0.0
-#BUILDLINK_ABI_DEPENDS.js_of_ocaml+=	js_of_ocaml>=3.0.0
+BUILDLINK_ABI_DEPENDS.js_of_ocaml+=	js_of_ocaml>=3.0.0nb1
 BUILDLINK_PKGSRCDIR.js_of_ocaml?=	../../devel/js_of_ocaml
 
 .endif	# JS_OF_OCAML_BUILDLINK3_MK
