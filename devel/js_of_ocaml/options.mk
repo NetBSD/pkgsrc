29e97d8ba1b32894e6bf34e741a4784f387db626
jaapb <jaapb@pkgsrc.org>
2017-09-08 13:19:34 +0000

    Updated package to latest version, 3.0.0. Changes include:
    Features/Changes
    
        Misc: switch to ppx instead of camlp4
        Misc: always install deriving_json
        Misc: remove support for async_kernel, bin_prot
        Misc: remove support for ppx_driver
        Misc: split js_of_ocaml in multiple packages
        Misc: switch to jbuilder
        Misc: new pseudo filesystem support with better node support
        Compiler: sourcemap support is now optional
        Compiler: no more strong dep on yojson, b64, menhir, ocamllex
        Compiler: improve separate compilation workflow
        Runtime: add support for weakdef
        Syntaxes: add expect tests
        Syntaxes: switch to ocaml-migrate-parsetree and ppx_tools_versioned
        Lib: Add primitives in [Js.Unsafe]
        Lib: remove keycode module, replaced by Dom_html.Keyboard_code
        Lib: XmlHttpRequest, API change for perform functions.
    
    Bug fixes:
    
        Runtime: fix many stubs
        Lib: fix many signatures
        Compiler: fix performance of compilation passes (simpl,clean)
        Compiler: fix compat with OCaml 4.06
        Toplevel: flush channels after execution.
        Toplevel: fix toplevel generation when using -export-unit

--- devel/js_of_ocaml/options.mk
+++ devel/js_of_ocaml/options.mk
@@ -1,22 +1,34 @@
-# $NetBSD: options.mk,v 1.1 2015/01/20 16:09:15 jaapb Exp $
+# $NetBSD: options.mk,v 1.2 2017/09/08 13:19:34 jaapb Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.js_of_ocaml
-PKG_SUPPORTED_OPTIONS=	ocaml-tyxml
+PKG_SUPPORTED_OPTIONS=	ocaml-tyxml camlp4
 PKG_SUGGESTED_OPTIONS=	ocaml-tyxml
 
 .include "../../mk/bsd.prefs.mk"
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	tyxml
+PLIST_VARS+=	tyxml camlp4
+
 ###
 ### TyXML support
 ###
 .if !empty(PKG_OPTIONS:Mocaml-tyxml)
 .include "../../textproc/ocaml-tyxml/buildlink3.mk"
 .include "../../devel/ocaml-reactiveData/buildlink3.mk"
-MAKE_ENV+=	USE_TYXML="YES" USE_REACT="YES"
 PLIST.tyxml=	yes
+JBUILDER_BUILD_PACKAGES+=	js_of_ocaml-tyxml
+OPAM_INSTALL_FILES+=	js_of_ocaml-tyxml
 .else
-MAKE_ENV+=	USE_TYXML="NO" USE_REACT="NO"
+.endif
+
+###
+### Camlp4 support
+###
+.if !empty(PKG_OPTIONS:Mcamlp4)
+.include "../../lang/camlp4/buildlink3.mk"
+PLIST.camlp4=	yes
+JBUILDER_BUILD_PACKAGES+=	js_of_ocaml-camlp4
+OPAM_INSTALL_FILES+=	js_of_ocaml-camlp4
+.else
 .endif
