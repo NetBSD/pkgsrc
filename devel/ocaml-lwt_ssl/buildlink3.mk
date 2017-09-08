a6b51bed450a627cc39a865504dbc340371d24db
jaapb <jaapb@pkgsrc.org>
2017-09-08 13:02:53 +0000

    Added package ocaml-lwt_ssl, a split-off from ocaml-lwt.

---
+++ devel/ocaml-lwt_ssl/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2017/09/08 13:02:53 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-lwt_ssl
+
+.if !defined(OCAML_LWT_SSL_BUILDLINK3_MK)
+OCAML_LWT_SSL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-lwt_ssl+=	ocaml-lwt_ssl>=1.1.0
+BUILDLINK_ABI_DEPENDS.ocaml-lwt_ssl?=	ocaml-lwt_ssl>=1.1.0nb1
+BUILDLINK_PKGSRCDIR.ocaml-lwt_ssl?=	../../devel/ocaml-lwt_ssl
+
+.include "../../devel/ocaml-lwt/buildlink3.mk"
+.include "../../security/ocaml-ssl/buildlink3.mk"
+.endif	# OCAML_LWT_SSL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-lwt_ssl
