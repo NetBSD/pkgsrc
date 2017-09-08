d2b42f916ff36f1415c04a0be5ecb7b89c468107
jaapb <jaapb@pkgsrc.org>
2017-09-08 16:50:46 +0000

    Updated package to latest version, 2.0.0beta4. Changes include:
    * Building with OCaml < 4.02.3 is no longer supported
    * Support compilation with a built-in mccs solver (removing run-time dependency
      to aspcud). Integrated half-working "heuristics" dropped.
    * Remove jsonm and transitive uutf dependency.
    * Switch build systems to `jbuilder`, including `lib-ext` support
    * Allow repeated and or'd arguments to the `list` command
    * Many code and build system portability fixes (Windows, BSD)
    * Add `switch export --full` to include package definitions
    * Add `announce:` and `stamp:` fields to repositories (`repo` files)
    * Add a global cache of git objects (greatly speeding up multiple cloning)
    * Allow `opam pin URL` without a package name
    * Many error handling, messages and corner case fixes
    * Updated the versions of dependencies
    * Removed the unused `features:` field, and proposal for a new syntax
    * More informative exit codes, and documentation thereof

--- misc/ocaml-opam/buildlink3.mk
+++ misc/ocaml-opam/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 10:46:23 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2017/09/08 16:50:46 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-opam
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-opam
 OCAML_OPAM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-opam+=	ocaml-opam>=1.2.2
-BUILDLINK_ABI_DEPENDS.ocaml-opam+=	ocaml-opam>=2.0.0_beta3
+BUILDLINK_ABI_DEPENDS.ocaml-opam+=	ocaml-opam>=2.0.0_beta4
 BUILDLINK_PKGSRCDIR.ocaml-opam?=	../../misc/ocaml-opam
 
 .endif	# OCAML_OPAM_BUILDLINK3_MK
