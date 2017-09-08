bf046efc87c764aa506cdbf3e2ef6f9cdd27fc0c
jaapb <jaapb@pkgsrc.org>
2017-09-08 12:58:06 +0000

    Added package ocaml-lwt_react. This is a split from ocaml-lwt.

---
+++ devel/ocaml-lwt_react/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2017/09/08 12:58:06 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-lwt_react
+
+.if !defined(OCAML_LWT_REACT_BUILDLINK3_MK)
+OCAML_LWT_REACT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-lwt_react+=	ocaml-lwt_react>=1.1.0
+BUILDLINK_PKGSRCDIR.ocaml-lwt_react?=	../../devel/ocaml-lwt_react
+
+.include "../../devel/ocaml-lwt/buildlink3.mk"
+.include "../../devel/ocaml-react/buildlink3.mk"
+.endif	# OCAML_LWT_REACT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-lwt_react
