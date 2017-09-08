3d9bc971d85d4eb1216d0e1130e86b2b80219117
jaapb <jaapb@pkgsrc.org>
2017-09-08 12:45:33 +0000

    Updated package to latest version, 3.1.0, which includes some infrastructure changes as well (packages for glib, react and ssl support have been split off). Upstream changes include:
    ===== 3.1.0 (2017-07-19) =====
    
    ====== Additions ======
    
      * Port to Jbuilder (#374, Andrew Ray).
      * Lwt_io.establish_server_with_client_address (#346, Rudi Grinberg).
      * Lwt_unix.getcwd (#403, Rapha?l Proust).
    
    ====== Planned to break in 4.0.0 ======
    
      * Delete lwt.simple-top (#371).
      * Delete Lwt_chan (#441).
    
    ====== Fixes ======
    
      * Make Lwt_log functions tail-recursive (#348, Jan Doms).
      * Make more of Lwt_list tail-recursive (#347, Jan Doms).
      * Improve string messages in exceptions (#368, #382, Jan Doms, Rapha?l
        Proust).
      * Don't call Unix.set_nonblock or Unix.clear_nonblock unnecessarily on
        some fds (#356, David Sheets).
      * Lwt_unix.sleep and Lwt_unix.timeout returning too early when using
        libev (#433, Stijn Devriendt).
      * Lwt_sequence.fold_r iterating the wrong way in some cases (#405,
        Stijn Devriendt).
      * Build conflicts in some cases due to duplicate cst_to_constr
        function (#362, J?r?mie Dimino).
      * Don't use deprecated readdir_r system call (#430, Rapha?l Proust).
    
    ====== Miscellaneous ======
    
      * The Lwt core, lwt.ml, has been thoroughly refactored and commented
        (#354, reviewed Gabriel Radanne, Edwin T?r?k, Rapha?l Proust, Jan
        Doms, Fabian Hemmer, Sebastien Mondet, Simon Cruanes, Anil
        Madhavapeddy, Pierre Chambart, and many others).
      * Lots of tests for most of the Lwt core (#339, #389, #392, #440,
        #448, #450, Joseph Thomas, Ryan Slade).
      * Documentation fixes (including by Joseph Thomas, Rapha?l Proust,
        Richard Degenne, Stavros Polymenis).
      * Contributing documentation (#379).
      * Massively adjust whitespace for legibility (#400, #409, #416,
        Richard Degenne).
      * Improvements to CI (Etienne Millon, Raphael Rafatpanah, Zack Coker,
        Yotam Barnoy).
      * The additional packages lwt_ssl, lwt_react, lwt_glib get new minor
        releases, the change being new Jbuilder build systems (#374, Andrew
        Ray).
    
    ===== 3.0.0 (2017-04-10) =====
    
    ====== Breaking ======
    
      * These changes were originally announced in release 2.7.0 (#308).
      * Lwt_engine.libev now has an optional argument for selecting the libev back
        end (#269, #294, Jeremy Yallop).
      * Lwt_io.establish_server has been changed to make it more difficult to leak
        file descriptors (#258, #260).
      * Lwt_io.shutdown_server now evaluates to a promise, which completes when the
        listening socket's close(2) operation completes (#259).
      * Lwt_unix.bind now evaluates to a promise, because the bind(2) system call
        can block for Unix domain sockets (#296, requested David Sheets).
      * ocamlfind packages lwt.react, lwt.ssl, lwt.glib are replaced by lwt_react,
        lwt_ssl, lwt_glib. These have been separate OPAM packages, under those
    names, since 2.7.0 (#301).

--- devel/ocaml-lwt/buildlink3.mk
+++ devel/ocaml-lwt/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.4 2017/07/11 11:06:54 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/09/08 12:45:33 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt
 
 .if !defined(OCAML_LWT_BUILDLINK3_MK)
 OCAML_LWT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-lwt+=	ocaml-lwt>=2.4.2
-BUILDLINK_ABI_DEPENDS.ocaml-lwt+=	ocaml-lwt>=2.7.1
+BUILDLINK_API_DEPENDS.ocaml-lwt+=	ocaml-lwt>=3.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-lwt+=	ocaml-lwt>=3.1.0
 BUILDLINK_PKGSRCDIR.ocaml-lwt?=	../../devel/ocaml-lwt
 
-.include "../../devel/libev/buildlink3.mk"
+#.include "../../devel/libev/buildlink3.mk"
 .endif	# OCAMLLWT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ocaml-lwt
