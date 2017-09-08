815c2e1e5af6c188f4da185e932a4feb97b4208a
jaapb <jaapb@pkgsrc.org>
2017-09-08 11:51:36 +0000

    Updated package to latest version, 1.0+beta13. Changes include:
    1.0+beta13 (05/09/2017)
    
        Generate toplevel html index for documentation (#224, Thomas Gazagnaire)
        Fix recompilation of native artifacts. Regression introduced in the last version (1.0+beta12) when digests replaces timestamps for checking staleness (#238, David Allsopp)
    
    1.0+beta12 (18/08/2017)
    
        Fix the quoting of FLG lines in generated .merlin files (#200, Marcello Seri)
        Use the full path of archive files when linking. Before jbuilder would do: -I <path> file.cmxa, now it does -I <path> <path>/file.cmxa. Fixes #118 and #177
        Use an absolute path for ppx drivers in .merlin files. Merlin <3.0.0 used to run ppx commands from the directory where the .merlin was present but this is no longer the case
        Allow to use jbuilder install in contexts other than opam; if ocamlfind is present in the PATH and the user didn't pass --prefix or --libdir explicitly, use the output of ocamlfind printconf destdir as destination directory for library files (#179, Francois Bobot)
        Allow (:include ...) forms in all *flags fields (#153, David Allsopp)
        Add a utop subsommand. Running jbuilder utop in a directory builds and executes a custom utop toplevel with all libraries defined in the current directory (#183, Rudi Grinberg)
        Do not accept per_file anymore in preprocess field. per_file was renamed per_module and it is planned to reuse per_file for another purpose
        Warn when a file is both present in the source tree and generated by a rule. Before, jbuilder would silently ignore the rule. One now has to add a field (fallback) to custom rules to keep the current behavior (#218)
        Get rid of the deprecated-ppx-method findlib package for ppx rewriters (#222, fixes #163)
        Use digests (MD5) of files contents to detect changes rather than just looking at the timestamps. We still use timestamps to avoid recomputing digests. The performance difference is negligible and we avoid more useless recompilations, especially when switching branches for instance (#209, fixes #158)
    
    1.0+beta11 (21/07/2017)
    
        Fix the error message when there are more than one <package>.opam file for a given pacakge
        Report an error when in a wrapped library, a module that is not the toplevel module depends on the toplevel module. This doesn't make as such a module would in theory be inaccessible from the outside
        Add ${SCOPE_ROOT} pointing to the root of the current scope, to fix some misuses of ${ROOT}
        Fix useless hint when all missing dependencies are optional (#137)
        Fix a bug preventing one from generating META.pkg.template with a custom rule (#190)
        Fix compilation of reason projects: .rei files where ignored and caused the build to fail (#184)

--- devel/ocaml-jbuilder/buildlink3.mk
+++ devel/ocaml-jbuilder/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/07/11 11:14:58 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/09/08 11:51:36 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-jbuilder
 
 .if !defined(OCAML_JBUILDER_BUILDLINK3_MK)
 OCAML_JBUILDER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-jbuilder+=	ocaml-jbuilder>=1.0_beta10
+BUILDLINK_API_DEPENDS.ocaml-jbuilder+=	ocaml-jbuilder>=1.0_beta12
 BUILDLINK_PKGSRCDIR.ocaml-jbuilder?=	../../devel/ocaml-jbuilder
 .endif	# OCAML_JBUILDER_BUILDLINK3_MK
 
