d37c53889fa3c4599ffe1c35972dbbcdb0e37473
jaapb <jaapb@pkgsrc.org>
2017-09-08 11:31:01 +0000

    Updated package to latest version, 4.05. A changelog is not available - this is a minor release that fixes compatibility with OCaml 4.05, though.

--- lang/camlp4/buildlink3.mk
+++ lang/camlp4/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2017/07/11 14:19:22 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/09/08 11:31:01 jaapb Exp $
 
 BUILDLINK_TREE+=	camlp4
 
 .if !defined(CAMLP4_BUILDLINK3_MK)
 CAMLP4_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.camlp4+=	camlp4>=4.04
-BUILDLINK_ABI_DEPENDS.camlp4+=	camlp4>=4.04nb1
+BUILDLINK_API_DEPENDS.camlp4+=	camlp4>=4.05
+BUILDLINK_ABI_DEPENDS.camlp4+=	camlp4>=4.05
 BUILDLINK_PKGSRCDIR.camlp4?=	../../lang/camlp4
 .endif	# CAMLP4_BUILDLINK3_MK
 
