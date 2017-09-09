17025aa77424a7b8a41af07bd79bc6615c130c4e
adam <adam@pkgsrc.org>
2017-09-09 07:10:34 +0000

    Rocks'n'Diamonds 4.0.0.2:
    This release is mainly a bugfix release, but also contains the following changes and additions:
    * fixed several bugs and problems with level template handling in the level editor
    * fixed several problems with the undo/redo buttons under certain conditions
    * fixed various other small and medium bugs (see commit history for details)
    * improved menu screens navigation and level selection using the keyboard
    * added option to use pre-defined level template for newly created levels
    * added level template settings to editor/level configuration screen in editor
    * added setup option to show/hide classic elements in the editor palette
    * added setup option to enable/disable auto-incrementing solved levels
    * added headless mode (no window) for automated tape tests using ‘autotest’
    * improved progress and summary output for automatic tape replay / testing

---
+++ games/rocksndiamonds/patches/patch-src_libgame_joystick.c
@@ -0,0 +1,22 @@
+$NetBSD: patch-src_libgame_joystick.c,v 1.1 2017/09/09 07:10:34 adam Exp $
+
+Allow joystick on NetBSD.
+
+--- src/libgame/joystick.c.orig	2015-10-20 21:58:08.000000000 +0000
++++ src/libgame/joystick.c
+@@ -9,12 +9,13 @@
+ // joystick.c
+ // ============================================================================
+ 
+-#if defined(PLATFORM_FREEBSD)
++#include "misc.h"
++
++#if defined(PLATFORM_FREEBSD) || (defined(PLATFORM_NETBSD) && defined(HAVE_JOYSTICK))
+ #include <machine/joystick.h>
+ #endif
+ 
+ #include "joystick.h"
+-#include "misc.h"
+ 
+ 
+ /* ========================================================================= */
