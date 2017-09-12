bfb1f97aa2d280f82ef7ca47d63158f0708227a7
ryoon <ryoon@pkgsrc.org>
2017-09-12 02:32:17 +0000

    Fix packaging when pgp5 is no installed etc.
    * Add pgp5 or gpg configure args explicitly
    * Conditional PLIST
    
    Reported by pgoyette@. Thank you.

--- security/pinepgp/options.mk
+++ security/pinepgp/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2017/09/09 11:43:08 ryoon Exp $
+# $NetBSD: options.mk,v 1.2 2017/09/12 02:32:17 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pinepgp
 
@@ -10,8 +10,16 @@ PLIST_VARS=		gpg pgp5
 
 .if !empty(PKG_OPTIONS:Mgpg)
 DEPENDS+=	gnupg>=1.2.3:../../security/gnupg
+CONFIGURE_ARGS+=	--with-gpg
+PLIST.gpg=yes
+.else
+CONFIGURE_ARGS+=	--without-gpg
 .endif
 
 .if !empty(PKG_OPTIONS:Mpgp5)
 DEPENDS+=	pgp5>=5.0:../../security/pgp5
+CONFIGURE_ARGS+=	--with-pgp5
+PLIST.pgp5=yes
+.else
+CONFIGURE_ARGS+=	--without-pgp5
 .endif
