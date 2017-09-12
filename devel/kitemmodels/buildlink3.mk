3beb235ecaadaa531ac68c2a7ed6af276e469572
wiz <wiz@pkgsrc.org>
2017-09-12 13:51:59 +0000

    recursive bump for qt5-qtwebkit dependency change

--- devel/kitemmodels/buildlink3.mk
+++ devel/kitemmodels/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2017/04/22 21:03:10 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2017/09/12 13:51:59 wiz Exp $
 
 BUILDLINK_TREE+=	kitemmodels
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kitemmodels
 KITEMMODELS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kitemmodels+=	kitemmodels>=5.18.0
-BUILDLINK_ABI_DEPENDS.kitemmodels?=	kitemmodels>=5.25.0nb6
+BUILDLINK_ABI_DEPENDS.kitemmodels?=	kitemmodels>=5.25.0nb8
 BUILDLINK_PKGSRCDIR.kitemmodels?=	../../devel/kitemmodels
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
