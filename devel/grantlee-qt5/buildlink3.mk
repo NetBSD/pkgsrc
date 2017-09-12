69f254a8c4f208473483c5fde93e777bd9ef6b38
wiz <wiz@pkgsrc.org>
2017-09-12 13:51:59 +0000

    recursive bump for qt5-qtwebkit dependency change

--- devel/grantlee-qt5/buildlink3.mk
+++ devel/grantlee-qt5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2017/04/22 21:03:09 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2017/09/12 13:51:59 wiz Exp $
 
 BUILDLINK_TREE+=	grantlee-qt5
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	grantlee-qt5
 GRANTLEE_QT5_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.grantlee-qt5+=	grantlee-qt5>=5.1.0
-BUILDLINK_ABI_DEPENDS.grantlee-qt5?=	grantlee-qt5>=5.1.0nb7
+BUILDLINK_ABI_DEPENDS.grantlee-qt5?=	grantlee-qt5>=5.1.0nb9
 BUILDLINK_PKGSRCDIR.grantlee-qt5?=	../../devel/grantlee-qt5
 
 .include "../../x11/qt5-qtscript/buildlink3.mk"
