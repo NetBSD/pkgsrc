db82261e1b0e5deb3a328bd61f406f26a6a780a0
wiz <wiz@pkgsrc.org>
2017-09-12 13:51:59 +0000

    recursive bump for qt5-qtwebkit dependency change

--- x11/qt5-qtquick1/buildlink3.mk
+++ x11/qt5-qtquick1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2017/04/22 21:03:22 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2017/09/12 13:52:00 wiz Exp $
 
 BUILDLINK_TREE+=	qt5-qtquick1
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qt5-qtquick1
 QT5_QTQUICK1_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt5-qtquick1+=	qt5-qtquick1>=5.5.1
-BUILDLINK_ABI_DEPENDS.qt5-qtquick1+=	qt5-qtquick1>=5.5.1nb11
+BUILDLINK_ABI_DEPENDS.qt5-qtquick1+=	qt5-qtquick1>=5.5.1nb13
 BUILDLINK_PKGSRCDIR.qt5-qtquick1?=	../../x11/qt5-qtquick1
 
 BUILDLINK_INCDIRS.qt5-qtquick1+=	qt5/include
