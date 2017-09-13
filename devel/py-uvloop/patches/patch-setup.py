3c65700662c96aec03c1038bd34525e82e563e3f
adam <adam@pkgsrc.org>
2017-09-13 06:21:01 +0000

    py-uvloop: update to 0.8.1
    v0.8.1:
    * Fix create_datagram_endpoint to work with AF_INET6 and no local_addr

--- devel/py-uvloop/patches/patch-setup.py
+++ devel/py-uvloop/patches/patch-setup.py
@@ -1,12 +1,12 @@
-$NetBSD: patch-setup.py,v 1.1 2017/06/24 06:47:59 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2017/09/13 06:21:02 adam Exp $
 
 Use external libuv.
 
---- setup.py.orig	2017-06-24 06:13:59.000000000 +0000
+--- setup.py.orig	2017-09-12 20:04:48.000000000 +0000
 +++ setup.py
-@@ -81,7 +81,7 @@ class uvloop_build_ext(build_ext):
+@@ -80,7 +80,7 @@ class uvloop_build_ext(build_ext):
+             return
  
-     def initialize_options(self):
          super().initialize_options()
 -        self.use_system_libuv = False
 +        self.use_system_libuv = True
