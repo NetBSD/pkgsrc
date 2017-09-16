df74b31f3dac7825584ac7e81321ee94ac589ce3
spz <spz@pkgsrc.org>
2017-09-16 15:39:07 +0000

    Pullup ticket #5548 - requested by he devel/SDL2: build fix for macppc
    Revisions pulled up:
    - devel/SDL2/distinfo                                           1.25
    - devel/SDL2/patches/patch-src_video_SDL__blit__N.c             1.1
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	he
       Date:		Tue Sep  5 09:02:18 UTC 2017
    
       Modified Files:
       	pkgsrc/devel/SDL2: distinfo
       Added Files:
       	pkgsrc/devel/SDL2/patches: patch-src_video_SDL__blit__N.c
    
       Log Message:
       Add a patch to move active code after declarations in the altivec case.
       No PKGREVISION bump, since this is a build fix only.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.24 -r1.25 pkgsrc/devel/SDL2/distinfo
       cvs rdiff -u -r0 -r1.1 \
           pkgsrc/devel/SDL2/patches/patch-src_video_SDL__blit__N.c

---
+++ devel/SDL2/patches/patch-src_video_SDL__blit__N.c
@@ -0,0 +1,33 @@
+$NetBSD: patch-src_video_SDL__blit__N.c,v 1.1.2.2 2017/09/16 15:39:07 spz Exp $
+
+Put code after declarations.
+
+--- src/video/SDL_blit_N.c.orig	2016-10-20 03:56:26.000000000 +0000
++++ src/video/SDL_blit_N.c
+@@ -118,12 +118,6 @@ calc_swizzle32(const SDL_PixelFormat * s
+         16, 8, 0, 24,
+         0, NULL
+     };
+-    if (!srcfmt) {
+-        srcfmt = &default_pixel_format;
+-    }
+-    if (!dstfmt) {
+-        dstfmt = &default_pixel_format;
+-    }
+     const vector unsigned char plus = VECUINT8_LITERAL(0x00, 0x00, 0x00, 0x00,
+                                                        0x04, 0x04, 0x04, 0x04,
+                                                        0x08, 0x08, 0x08, 0x08,
+@@ -136,6 +130,13 @@ calc_swizzle32(const SDL_PixelFormat * s
+     Uint32 gmask = RESHIFT(srcfmt->Gshift) << (dstfmt->Gshift);
+     Uint32 bmask = RESHIFT(srcfmt->Bshift) << (dstfmt->Bshift);
+     Uint32 amask;
++
++    if (!srcfmt) {
++        srcfmt = &default_pixel_format;
++    }
++    if (!dstfmt) {
++        dstfmt = &default_pixel_format;
++    }
+     /* Use zero for alpha if either surface doesn't have alpha */
+     if (dstfmt->Amask) {
+         amask =
