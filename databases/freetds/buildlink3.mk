2f2f42d00292947e709eddbdffd115fd21de02f8
taca <taca@pkgsrc.org>
2017-09-17 14:57:59 +0000

    databases/freetds: update to 1.00.55.
    Summary of Changes in release 1.0
    --------------------------------------------
    User visible (not in a particular order):
    - Removed "8.0" from protocol version string accepted. Please
      update configuration files;
    - Default protocol version is now auto. This could slow down
      connection but make user experience less painful;
    - Sybase encrypted login. Set encryption to get it;
    - Support protocol version 7.4;
    - Add intent support to specify we don't want to change data;
    - Allow to attach database file during the login (MS SQL Server);
    - Support for Sybase time/date/bigdate/bigdatetime;
    - Pool is working again;
    - ODBC BCP (not complete);
    - Improved dbconvert and dbconvert_ps (more compatible);
    - Fixed dbspid;
    - Improved ODBC type information;
    - Better certificate verification;
    - AppVeyor is used for every build;
    - Try all IPs from DNS. This allows SQL Cluster connection
      to secondary servers.
    
    Implementation:
    - Removed Nmake support;
    - Type conversions simplified;
    - Better type handle code.

--- databases/freetds/buildlink3.mk
+++ databases/freetds/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.21 2016/09/19 13:04:18 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2017/09/17 14:57:59 taca Exp $
 
 BUILDLINK_TREE+=	freetds
 
 .if !defined(FREETDS_BUILDLINK3_MK)
 FREETDS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.freetds+=	freetds>=0.82
-BUILDLINK_ABI_DEPENDS.freetds+=	freetds>=0.91nb6
+BUILDLINK_API_DEPENDS.freetds+=	freetds>=1.00
+BUILDLINK_ABI_DEPENDS.freetds+=	freetds>=1.00
 BUILDLINK_PKGSRCDIR.freetds?=	../../databases/freetds
 
 .include "../../converters/libiconv/buildlink3.mk"
