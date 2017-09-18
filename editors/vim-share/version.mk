2e8000613d22bd0cdbf1755569b2a82fb2523ab5
wiz <wiz@pkgsrc.org>
2017-09-18 23:27:01 +0000

    vim-share: add comment where changelog can be found
    From morr@

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,8 @@
-# $NetBSD: version.mk,v 1.107 2017/09/11 21:05:00 wiz Exp $
+# $NetBSD: version.mk,v 1.108 2017/09/18 23:27:01 wiz Exp $
 
 VIM_VERSION=		8.0
 VIM_PATCHLEVEL=		1096
+# Changelog: see http://ftp.vim.org/pub/vim/patches/8.0/
 VIM_SUBDIR=		vim80
 
 PRINT_PLIST_AWK+=	{ gsub(/${VIM_SUBDIR}/, "$${VIM_SUBDIR}"); print; next; }
