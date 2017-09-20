75d5c5d2bb119bc4fabc6bf5c51619858dc9700a
adam <adam@pkgsrc.org>
2017-09-20 06:47:56 +0000

    rtmpdump: clean-up
    Clean-up:
    Removed double USE_LIBTOOL=yes.
    Pass external CFLAGS and LDFLAGS.
    Put PREFIX in patches, removing SUBST.
    Do not override optimisation with OPT=-O2.

--- net/rtmpdump/buildlink3.mk
+++ net/rtmpdump/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2016/03/05 11:27:53 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/09/20 06:47:56 adam Exp $
 
 BUILDLINK_TREE+=	rtmpdump
 
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	rtmpdump
 RTMPDUMP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.rtmpdump+=	rtmpdump>=2.3
-BUILDLINK_ABI_DEPENDS.rtmpdump?=	rtmpdump>=2.4nb3
-BUILDLINK_PKGSRCDIR.rtmpdump?=	../../net/rtmpdump
+BUILDLINK_ABI_DEPENDS.rtmpdump+=	rtmpdump>=2.4nb3
+BUILDLINK_PKGSRCDIR.rtmpdump?=		../../net/rtmpdump
 
 .include "../../security/openssl/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
