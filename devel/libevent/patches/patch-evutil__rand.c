f2f52c4cab42cc30ec5d370fd8b02dbde7185dfd
maya <maya@pkgsrc.org>
2017-09-22 02:58:50 +0000

    libevent: remove redundant include, define __EXT_POSIX2 on QNX.
    Should help QNX build the package, but still missing a fallback definition
    for SA_RESTART.
    
    Reported by Jan Danielsson on pkgsrc-users.

--- devel/libevent/patches/patch-evutil__rand.c
+++ devel/libevent/patches/patch-evutil__rand.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-evutil__rand.c,v 1.3 2017/03/08 13:37:55 jperkin Exp $
+$NetBSD: patch-evutil__rand.c,v 1.4 2017/09/22 02:58:50 maya Exp $
 
 Native illumos arc4random(3C) imported the latest OpenBSD API which
 does not have arc4random_addrandom().
 
---- evutil_rand.c.orig	2013-11-01 18:18:57.000000000 +0000
+--- evutil_rand.c.orig	2016-10-04 19:55:31.000000000 +0000
 +++ evutil_rand.c
-@@ -195,7 +195,9 @@ evutil_secure_rng_get_bytes(void *buf, s
+@@ -195,8 +195,10 @@ evutil_secure_rng_get_bytes(void *buf, s
  void
  evutil_secure_rng_add_bytes(const char *buf, size_t n)
  {
@@ -15,3 +15,4 @@ does not have arc4random_addrandom().
 +#endif
  }
  
+ void
