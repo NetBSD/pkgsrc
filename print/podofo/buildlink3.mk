81628137d8e2d4cd4cfa3512538d17eff1db8780
joerg <joerg@pkgsrc.org>
2017-09-25 22:26:27 +0000

    Apply upstream fix for providing the same ABI for C++03 and C++11. Bump revision. Belatedly fix b3.mk.

--- print/podofo/buildlink3.mk
+++ print/podofo/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.8 2013/07/05 21:12:45 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2017/09/25 22:26:27 joerg Exp $
 
 BUILDLINK_TREE+=	podofo
 
 .if !defined(PODOFO_BUILDLINK3_MK)
 PODOFO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.podofo+=	podofo>=0.9.2
+BUILDLINK_API_DEPENDS.podofo+=	podofo>=0.9.5nb1
 BUILDLINK_PKGSRCDIR.podofo?=	../../print/podofo
 
 .include "../../devel/zlib/buildlink3.mk"
