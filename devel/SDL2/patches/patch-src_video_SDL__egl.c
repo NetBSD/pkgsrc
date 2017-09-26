ed1285657d334ce05b901092a85d838a752db6a9
adam <adam@pkgsrc.org>
2017-09-26 13:10:56 +0000

    SDL2: update to 2.0.6
    Changes 2.0.6:
    
    General:
    * Added cross-platform Vulkan graphics support in SDL_vulkan.h
            SDL_Vulkan_LoadLibrary()
            SDL_Vulkan_GetVkGetInstanceProcAddr()
            SDL_Vulkan_GetInstanceExtensions()
            SDL_Vulkan_CreateSurface()
            SDL_Vulkan_GetDrawableSize()
            SDL_Vulkan_UnloadLibrary()
      This is all the platform-specific code you need to bring up Vulkan on all SDL platforms. You can look at an example in test/testvulkan.c
    * Added SDL_ComposeCustomBlendMode() to create custom blend modes for 2D rendering
    * Added SDL_HasNEON() which returns whether the CPU has NEON instruction support
    * Added support for many game controllers, including the Nintendo Switch Pro Controller
    * Added support for inverted axes and separate axis directions in game controller mappings
    * Added functions to return information about a joystick before it's opened:
            SDL_JoystickGetDeviceVendor()
            SDL_JoystickGetDeviceProduct()
            SDL_JoystickGetDeviceProductVersion()
            SDL_JoystickGetDeviceType()
            SDL_JoystickGetDeviceInstanceID()
    * Added functions to return information about an open joystick:
            SDL_JoystickGetVendor()
            SDL_JoystickGetProduct()
            SDL_JoystickGetProductVersion()
            SDL_JoystickGetType()
            SDL_JoystickGetAxisInitialState()
    * Added functions to return information about an open game controller:
            SDL_GameControllerGetVendor()
            SDL_GameControllerGetProduct()
            SDL_GameControllerGetProductVersion()
    * Added SDL_GameControllerNumMappings() and SDL_GameControllerMappingForIndex() to be able to enumerate the built-in game controller mappings
    * Added SDL_LoadFile() and SDL_LoadFile_RW() to load a file into memory
    * Added SDL_DuplicateSurface() to make a copy of a surface
    * Added an experimental JACK audio driver
    * Implemented non-power-of-two audio resampling, optionally using libsamplerate to perform the resampling
    * Added the hint SDL_HINT_AUDIO_RESAMPLING_MODE to control the quality of resampling
    * Added the hint SDL_HINT_RENDER_LOGICAL_SIZE_MODE to control the scaling policy for SDL_RenderSetLogicalSize():
            "0" or "letterbox" - Uses letterbox/sidebars to fit the entire rendering on screen (the default)
            "1" or "overscan"  - Will zoom the rendering so it fills the entire screen, allowing edges to be drawn offscreen
    * Added the hints SDL_HINT_MOUSE_NORMAL_SPEED_SCALE and SDL_HINT_MOUSE_RELATIVE_SPEED_SCALE to scale the mouse speed when being read from raw mouse input
    * Added the hint SDL_HINT_TOUCH_MOUSE_EVENTS to control whether SDL will synthesize mouse events from touch events

--- devel/SDL2/patches/patch-src_video_SDL__egl.c
+++ devel/SDL2/patches/patch-src_video_SDL__egl.c
@@ -1,19 +1,23 @@
-$NetBSD: patch-src_video_SDL__egl.c,v 1.4 2017/01/26 03:46:20 nat Exp $
+$NetBSD: patch-src_video_SDL__egl.c,v 1.5 2017/09/26 13:10:56 adam Exp $
 
---- src/video/SDL_egl.c.orig	2016-10-20 03:56:26.000000000 +0000
+--- src/video/SDL_egl.c.orig	2017-09-22 18:51:01.000000000 +0000
 +++ src/video/SDL_egl.c
-@@ -40,10 +40,10 @@
+@@ -44,12 +44,12 @@
  
  #if SDL_VIDEO_DRIVER_RPI
  /* Raspbian places the OpenGL ES/EGL binaries in a non standard path */
--#define DEFAULT_EGL "/opt/vc/lib/libEGL.so"
--#define DEFAULT_OGL_ES2 "/opt/vc/lib/libGLESv2.so"
+-#define DEFAULT_EGL "/opt/vc/lib/libbrcmEGL.so"
+-#define DEFAULT_OGL_ES2 "/opt/vc/lib/libbrcmGLESv2.so"
+-#define ALT_EGL "/opt/vc/lib/libEGL.so"
+-#define ALT_OGL_ES2 "/opt/vc/lib/libGLESv2.so"
 -#define DEFAULT_OGL_ES_PVR "/opt/vc/lib/libGLES_CM.so"
 -#define DEFAULT_OGL_ES "/opt/vc/lib/libGLESv1_CM.so"
-+#define DEFAULT_EGL PREFIX "/lib/libEGL.so"
-+#define DEFAULT_OGL_ES2 PREFIX "/lib/libGLESv2.so"
-+#define DEFAULT_OGL_ES_PVR PREFIX "/lib/libGLESv2.so"
-+#define DEFAULT_OGL_ES PREFIX "/lib/libGLESv2.so"
++#define DEFAULT_EGL "/lib/libbrcmEGL.so"
++#define DEFAULT_OGL_ES2 "/lib/libbrcmGLESv2.so"
++#define ALT_EGL "/lib/libEGL.so"
++#define ALT_OGL_ES2 "/lib/libGLESv2.so"
++#define DEFAULT_OGL_ES_PVR "/lib/libGLES_CM.so"
++#define DEFAULT_OGL_ES "/lib/libGLESv1_CM.so"
  
  #elif SDL_VIDEO_DRIVER_ANDROID || SDL_VIDEO_DRIVER_VIVANTE
  /* Android */
