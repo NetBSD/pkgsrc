74761ee97fe524b92b08c8ec56472523f039659f
roy <roy@pkgsrc.org>
2017-09-26 14:02:32 +0000

    Remove beta from dependency check as it causes issues.

--- lang/php70/buildlink3.mk
+++ lang/php70/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/08/05 08:29:01 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/09/26 14:02:32 roy Exp $
 
 BUILDLINK_TREE+=	php
 
 .if !defined(PHP_BUILDLINK3_MK)
 PHP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.php+=	php>=7.0.0<7.1.0beta1
-BUILDLINK_ABI_DEPENDS.php+=	php>=7.0.0<7.1.0beta1
+BUILDLINK_API_DEPENDS.php+=	php>=7.0.0<7.1.0
+BUILDLINK_ABI_DEPENDS.php+=	php>=7.0.0<7.1.0
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php70
 
 .include "../../textproc/libxml2/buildlink3.mk"
