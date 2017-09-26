f8ea3f837a45931b8e42cade9a008c903cbcf2e0
mspo <mspo@pkgsrc.org>
2017-09-26 17:21:08 +0000

    lang/nqp becomes the same as wip/nqp, upgrading to 2017.09

--- lang/nqp/buildlink3.mk
+++ lang/nqp/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.9 2017/09/18 09:53:01 maya Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2017/09/26 17:21:08 mspo Exp $
 
 BUILDLINK_TREE+=	nqp
 
 .if !defined(NQP_BUILDLINK3_MK)
 NQP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nqp+=		nqp>=2013.08
-BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2013.08nb12
+BUILDLINK_API_DEPENDS.nqp+=		nqp>=2015.11
+BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2015.11
 BUILDLINK_PKGSRCDIR.nqp?=		../../lang/nqp
 
-.include "../../lang/parrot/buildlink3.mk"
+.include "../../devel/MoarVM/buildlink3.mk"
 .endif # NQP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-nqp
