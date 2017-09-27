f8df49edf0d8c2e1bfe1ab21b88cb12b7d0a96ea
wiz <wiz@pkgsrc.org>
2017-09-27 13:45:53 +0000

    esound: remove default-off arts option
    arts was removed from pkgsrc

--- audio/esound/options.mk
+++ audio/esound/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.3 2012/09/08 18:58:43 cheusov Exp $
+# $NetBSD: options.mk,v 1.4 2017/09/27 13:45:53 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.esound
-PKG_SUPPORTED_OPTIONS=	inet6 arts
+PKG_SUPPORTED_OPTIONS=	inet6
 PKG_SUGGESTED_OPTIONS=	inet6
 
 .include "../../mk/bsd.options.mk"
@@ -10,10 +10,4 @@ PKG_SUGGESTED_OPTIONS=	inet6
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
-.endif
-
-.if !empty(PKG_OPTIONS:Marts)
-.include "../../audio/arts/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-arts --disable-artstest
 .endif
