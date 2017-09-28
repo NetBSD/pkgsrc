c8531900b682b0247246583fbc52e853b20a7553
jaapb <jaapb@pkgsrc.org>
2017-09-28 09:15:24 +0000

    Updated devel/ocaml-re package to install documentation

--- devel/ocaml-re/buildlink3.mk
+++ devel/ocaml-re/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 10:13:14 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2017/09/28 09:15:24 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-re
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-re
 OCAML_RE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-re+=	ocaml-re>=1.6.0
-BUILDLINK_ABI_DEPENDS.ocaml-re+=	ocaml-re>=1.7.1
+BUILDLINK_ABI_DEPENDS.ocaml-re+=	ocaml-re>=1.7.1nb2
 BUILDLINK_PKGSRCDIR.ocaml-re?=	../../devel/ocaml-re
 .endif	# OCAML_RE_BUILDLINK3_MK
 
