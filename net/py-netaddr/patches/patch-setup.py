d634cec4953272142f3e1e72cb939569d9fce8c2
adam <adam@pkgsrc.org>
2017-09-28 12:25:11 +0000

    py-netaddr: update to 0.7.19
    Release: 0.7.19:
    
    * added a new SubnetSplitter class for those looking to divide up subnets.
      Thanks alanwill and RyPeck and those on (Stack Overflow discussion).
    
    * removed bundled pytest dependency code for "python setup.py test".
    
    * setup.py now uses setuptools only (no more distutils) and setup_egg.py removed.
    
    * cleaned up INSTALL docs so they accurately reflect current Python packaging.
    
    * fixed broken parsing, generating and reading of IEEE index files when switching
      between Python 2.x and 3.x.

--- net/py-netaddr/patches/patch-setup.py
+++ net/py-netaddr/patches/patch-setup.py
@@ -1,25 +1,12 @@
-$NetBSD: patch-setup.py,v 1.2 2016/03/06 15:07:07 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2017/09/28 12:25:11 adam Exp $
 
-Prefer setup from setuptools, the older one may not
-support install_requires or setup_requires
+Do not overwrite script's interpreter.
 
---- setup.py.orig	2015-08-25 21:54:04.000000000 +0000
+--- setup.py.orig	2017-01-11 23:42:52.000000000 +0000
 +++ setup.py
-@@ -8,7 +8,10 @@ A distutils Python setup file. For setup
- import os
- import sys
- 
--from distutils.core import setup, Command
-+try:
-+    from setuptools import setup, Command
-+except ImportError:
-+    from distutils.core import setup, Command
- 
- if os.path.exists('MANIFEST'):
-     os.remove('MANIFEST')
-@@ -64,7 +67,6 @@ def main():
-         version          = release.version,
-         options = {
+@@ -176,7 +176,6 @@ def main():
+         version=netaddr.__version__,
+         options={
              'build_scripts': {
 -                'executable': '/usr/bin/env python',
              },
