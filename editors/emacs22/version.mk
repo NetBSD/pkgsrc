71ab2b730aebc749188964d211f0b3a14975e0f4
wiz <wiz@pkgsrc.org>
2017-09-29 12:40:26 +0000

    emacs22*, emacs23*, emacs24*: remove unmaintained emacs versions
    As announced in
    https://mail-index.netbsd.org/pkgsrc-users/2017/09/11/msg025563.html
    
    This still leaves emacs20, emacs21, and the current version, emacs25.

--- editors/emacs22/version.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD: version.mk,v 1.4 2013/02/17 00:39:53 dholland Exp $
-
-_EMACS_FLAVOR=	emacs
-_EMACS_REQD=	emacs22>=22.3<23
-_EMACS_PKGDEP.base=
-_EMACS_PKGDEP.leim=
-
-_EMACS_VERSION_MAJOR=	22
-_EMACS_VERSION_MINOR=	3
