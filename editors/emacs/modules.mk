ec6e8dc000e5c9fdc80624a60db18a5bc7d0c386
wiz <wiz@pkgsrc.org>
2017-09-29 12:40:26 +0000

    emacs22*, emacs23*, emacs24*: remove unmaintained emacs versions
    As announced in
    https://mail-index.netbsd.org/pkgsrc-users/2017/09/11/msg025563.html
    
    This still leaves emacs20, emacs21, and the current version, emacs25.

--- editors/emacs/modules.mk
+++ editors/emacs/modules.mk
@@ -1,4 +1,4 @@
-# $NetBSD: modules.mk,v 1.21 2017/01/01 02:48:43 dholland Exp $
+# $NetBSD: modules.mk,v 1.22 2017/09/29 12:40:27 wiz Exp $
 #
 # This Makefile fragment handles Emacs Lisp Packages (== ELPs).
 #
@@ -41,9 +41,7 @@
 #			The user's favourite Emacs version.  The default
 #			value is set in mk/defaults/mk.conf.
 #		Possible values:
-#			emacs25, emacs24, emacs24nox,
-#			emacs23, emacs23nox, emacs22, emacs22nox,
-#			emacs21, emacs21nox, emacs20,
+#			emacs25, emacs21, emacs21nox, emacs20,
 #			xemacs215, xemacs215nox, xemacs214, xemacs214nox
 #		Default value:
 #			emacs25
@@ -63,14 +61,10 @@
 #		Description:
 #			Versions the ELP accepts (supports).
 #		Possible values:
-#			emacs25, emacs24, emacs24nox,
-#			emacs23, emacs23nox, emacs22, emacs22nox,
-#			emacs21, emacs21nox, emacs20,
+#			emacs25, emacs21, emacs21nox, emacs20,
 #			xemacs215, xemacs215nox, xemacs214, xemacs214nox
 #		Default value:
-#			emacs25, emacs24, emacs24nox,
-#			emacs23, emacs23nox, emacs22, emacs22nox,
-#			emacs21, emacs21nox, emacs20,
+#			emacs25, emacs21, emacs21nox, emacs20,
 #			xemacs215, xemacs215nox, xemacs214, xemacs214nox
 #
 #	EMACS_BUILDLINK
@@ -180,10 +174,10 @@
 #		Possible values:
 #			XXX
 #
-#	FOR_{emacs25,emacs24,emacs24nox,emacs23,emacs23nox,emacs22,emacs22nox,emacs21,emacs21nox,emacs20,xemacs215,xemacs215nox,xemacs214,xemacs214nox}
+#	FOR_{emacs25,emacs21,emacs21nox,emacs20,xemacs215,xemacs215nox,xemacs214,xemacs214nox}
 #	FOR_{emacs,xemacs}
 #	FOR_{emacs_x,emacs_nox}
-#	NOTFOR_{emacs25,emacs24,emacs24nox,emacs23,emacs23nox,emacs22,emacs22nox,emacs21,emacs21nox,emacs20,xemacs215,xemacs215nox,xemacs214,xemacs214nox}
+#	NOTFOR_{emacs25,emacs21,emacs21nox,emacs20,xemacs215,xemacs215nox,xemacs214,xemacs214nox}
 #	NOTFOR_{emacs,xemacs}
 #	NOTFOR_{emacs_x,emacs_nox}
 #		Description:
@@ -227,20 +221,13 @@ BUILD_DEFS_EFFECTS+=	${_SYS_VARS.emacs}
 #
 
 _EMACS_VERSIONS_ALL= \
-	emacs20 emacs21 emacs21nox emacs22 emacs22nox emacs23 emacs23nox \
-	emacs24 emacs24nox emacs25 emacs25nox \
+	emacs20 emacs21 emacs21nox emacs25 emacs25nox \
 	xemacs214 xemacs214nox xemacs215 xemacs215nox
 
 _EMACS_PKGDIR_MAP= \
 	emacs20@../../editors/emacs20 \
 	emacs21@../../editors/emacs21 \
 	emacs21nox@../../editors/emacs21-nox11 \
-	emacs22@../../editors/emacs22 \
-	emacs22nox@../../editors/emacs22-nox11 \
-	emacs23@../../editors/emacs23 \
-	emacs23nox@../../editors/emacs23-nox11 \
-	emacs24@../../editors/emacs24 \
-	emacs24nox@../../editors/emacs24-nox11 \
 	emacs25@../../editors/emacs25 \
 	emacs25nox@../../editors/emacs25-nox11 \
 	xemacs214@../../editors/xemacs \
