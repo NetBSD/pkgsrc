794bc715b821a4b3574b2fee78b7b64bb437aa44
joerg <joerg@pkgsrc.org>
2017-09-30 20:58:00 +0000

    Redo Python 3.x support.

---
+++ devel/py-txgithub/patches/patch-txgithub_scripts_gist.py
@@ -0,0 +1,25 @@
+$NetBSD: patch-txgithub_scripts_gist.py,v 1.1 2017/09/30 20:58:00 joerg Exp $
+
+--- txgithub/scripts/gist.py.orig	2017-09-30 20:47:17.806190554 +0000
++++ txgithub/scripts/gist.py
+@@ -30,16 +30,16 @@ def postGist(reactor, token, files):
+         gistFiles['gistfile1'] = {"content": sys.stdin.read()}
+ 
+     response = yield api.gists.create(files=gistFiles)
+-    print response['html_url']
++    print(response['html_url'])
+ 
+ 
+ def run(reactor, *argv):
+     config = Options()
+     try:
+         config.parseOptions(argv[1:]) # When given no argument, parses sys.argv[1:]
+-    except usage.UsageError, errortext:
+-        print '%s: %s' % (argv[0], errortext)
+-        print '%s: Try --help for usage details.' % (argv[0])
++    except usage.UsageError as errortext:
++        print('%s: %s' % (argv[0], errortext))
++        print('%s: Try --help for usage details.' % (argv[0]))
+         sys.exit(1)
+ 
+     return postGist(reactor, **config)
