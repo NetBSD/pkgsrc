7f675f7fa2dd009b4163943888f6cb56c4748005
wiz <wiz@pkgsrc.org>
2017-10-03 15:12:42 +0000

    transfig: remove, replaced by print/fig2dev

--- textproc/dblatex/options.mk
+++ textproc/dblatex/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2014/12/10 12:27:27 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2017/10/03 15:12:42 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dblatex
 PKG_SUPPORTED_OPTIONS=	dblatex-fig
@@ -7,5 +7,5 @@ PKG_SUGGESTED_OPTIONS=	dblatex-fig
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdblatex-fig)
-DEPENDS+=	transfig-[0-9]*:../../print/transfig
+DEPENDS+=	fig2dev-[0-9]*:../../print/fig2dev
 .endif
