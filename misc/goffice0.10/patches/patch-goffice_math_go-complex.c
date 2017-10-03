6bf59d9d0a35f11f91ff3a5fa4472a2b69b51a27
wiz <wiz@pkgsrc.org>
2017-10-03 12:44:05 +0000

    goffice0.10: update to 0.10.35.
    goffice 0.10.35:
    
    Morten:
    	* Fix quad-precision overflow problem.
    
    --------------------------------------------------------------------------
    goffice 0.10.34:
    
    Morten:
    	* Fix rich-text format problem.
    
    --------------------------------------------------------------------------
    goffice 0.10.33:
    
    Jean:
    	* Make box plots look like bar plots in legends. [#773825]
    	* Fix conditional compilation for long double. Patch from Ludovic
    	Rousseau). [#774439]
    	* Reimplement major grids for discrete axes. [#775624].
    	* Fix add child menu in graph guru. [#777336]
    	* Fix evaluation of regression curves for area plots. [#777334]
    
    Morten:
    	* Introspection fixes.
    
    --------------------------------------------------------------------------
    goffice 0.10.32:
    
    Morten:
    	* Plug leak.
    	* Avoid gnome-common dependency.
    
    --------------------------------------------------------------------------
    goffice 0.10.31:
    
    --------------------------------------------------------------------------
    goffice 0.10.30:
    
    Jean:
    	* Fix color scale reference to its axis. [#766829]
    
    --------------------------------------------------------------------------
    goffice 0.10.29:
    
    Jean:
    	* Do not load invalid data labels in XYcolor plots. [#765155]
    	* Fix an infinite loop condition in 3D plots. [#765480]
    	* Implement line width and fill pattern support in themes. [#765970]
    
    Morten:
    	* Improve go_complex_sqrt.
    	* Improve go_complex_div.
    
    --------------------------------------------------------------------------
    goffice 0.10.28:
    
    Jean:
    	* Fix histograms with missing values. [#763446]
    
    Morten:
    	* Configuration fixes.
    	* Introspection improvements.
    
    --------------------------------------------------------------------------
    goffice 0.10.27:
    
    Andreas:
    	* Support new foreign element in ODF export of time styles
    
    Jean:
    	* Add title to axis lines. [#760675]
    Morten:
    	* Fix go_complex_powl accuracy for real case.
    	* Fix go_complex_div for very small divisor.
    
    --------------------------------------------------------------------------
    goffice 0.10.26:
    
    --------------------------------------------------------------------------
    goffice 0.10.25:
    
    Morten:
    	* Plug leaks.
    	* Fix axis editing bug.  [#684886]
    	* Make builds more reproducible.
    
    --------------------------------------------------------------------------
    goffice 0.10.24:
    
    Jean:
    	* Fix bounds and distance for rectangle and paths in RTL mode. [#753963]
    	* Don't access NULL X or Y values in YXZ matrix plots. [#754590]

--- misc/goffice0.10/patches/patch-goffice_math_go-complex.c
+++ misc/goffice0.10/patches/patch-goffice_math_go-complex.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-goffice_math_go-complex.c,v 1.1 2015/08/10 19:30:06 joerg Exp $
+$NetBSD: patch-goffice_math_go-complex.c,v 1.2 2017/10/03 12:44:05 wiz Exp $
 
 Format string is passed in as argument.
 
---- goffice/math/go-complex.c.orig	2015-08-10 16:25:07.000000000 +0000
+--- goffice/math/go-complex.c.orig	2016-03-24 21:19:04.000000000 +0000
 +++ goffice/math/go-complex.c
-@@ -58,6 +58,8 @@
- #define COMPLEX SUFFIX(GOComplex)
+@@ -87,6 +87,8 @@ MAKE_BOXED_TYPE(go_complex_get_type, "GO
+ #endif
  
  /* ------------------------------------------------------------------------- */
 +#pragma GCC diagnostic push
@@ -13,7 +13,7 @@ Format string is passed in as argument.
  
  char *
  SUFFIX(go_complex_to_string) (COMPLEX const *src, char const *reformat,
-@@ -103,6 +105,7 @@ SUFFIX(go_complex_to_string) (COMPLEX co
+@@ -132,6 +134,7 @@ SUFFIX(go_complex_to_string) (COMPLEX co
  
  	return res;
  }
