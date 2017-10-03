4a5aab7673eae6682ed27c01b1ca94550934399e
wiz <wiz@pkgsrc.org>
2017-10-03 12:36:52 +0000

    *: Recursive PKGREVISION bump for vala 0.38 update.

--- x11/libdesktop-agnostic/buildlink3.mk
+++ x11/libdesktop-agnostic/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2017/05/29 12:20:23 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2017/10/03 12:36:52 wiz Exp $
 
 BUILDLINK_TREE+=	libdesktop-agnostic
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libdesktop-agnostic
 LIBDESKTOP_AGNOSTIC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libdesktop-agnostic+=	libdesktop-agnostic>=0.3.92
-BUILDLINK_ABI_DEPENDS.libdesktop-agnostic+=	libdesktop-agnostic>=0.3.92nb23
+BUILDLINK_ABI_DEPENDS.libdesktop-agnostic+=	libdesktop-agnostic>=0.3.92nb24
 BUILDLINK_PKGSRCDIR.libdesktop-agnostic?=	../../x11/libdesktop-agnostic
 
 .include "../../lang/vala/buildlink3.mk"
