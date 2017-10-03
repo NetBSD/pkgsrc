1605dd1b436c8932901b3befff340be19c490a64
jperkin <jperkin@pkgsrc.org>
2017-10-03 09:43:06 +0000

    mk: Add support for SSP checks.

--- mk/check/bsd.check-vars.mk
+++ mk/check/bsd.check-vars.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.check-vars.mk,v 1.9 2017/07/04 18:29:24 khorben Exp $
+# $NetBSD: bsd.check-vars.mk,v 1.10 2017/10/03 09:43:06 jperkin Exp $
 #
 # This Makefile fragment is included separately by bsd.pkg.mk and
 # defines some variables which must be defined earlier than where
@@ -8,8 +8,10 @@
 CHECK_FILES_SUPPORTED?=		yes
 CHECK_RELRO_SUPPORTED?=		yes
 CHECK_SHLIBS_SUPPORTED?=	yes
+CHECK_SSP_SUPPORTED?=		yes
 
 _OPSYS_CAN_CHECK_RELRO?=	${_OPSYS_CAN_CHECK_SHLIBS}
+_OPSYS_CAN_CHECK_SSP?=		${_OPSYS_CAN_CHECK_SHLIBS}
 
 .if ${_OPSYS_CAN_CHECK_RELRO:tl} == "yes"
 _USE_CHECK_RELRO_NATIVE=	yes
@@ -21,6 +23,12 @@ _USE_CHECK_RELRO_NATIVE=	no
 _USE_CHECK_SHLIBS_NATIVE=	yes
 .else
 _USE_CHECK_SHLIBS_NATIVE=	no
+.endif
+
+.if ${_OPSYS_CAN_CHECK_SSP:tl} == "yes"
+_USE_CHECK_SSP_NATIVE=		yes
+.else
+_USE_CHECK_SSP_NATIVE=		no
 .endif
 
 USE_TOOLS+=	awk cat cmp diff echo find grep rm sed test touch true
