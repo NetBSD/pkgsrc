284c91b3c02c51a306caef71f5551d8185e086b3
wiz <wiz@pkgsrc.org>
2017-10-04 11:58:45 +0000

    xfig: Add upstream bug report.

--- graphics/xfig/patches/patch-src_w__keyboard.c
+++ graphics/xfig/patches/patch-src_w__keyboard.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_w__keyboard.c,v 1.1 2017/10/03 15:09:36 wiz Exp $
+$NetBSD: patch-src_w__keyboard.c,v 1.2 2017/10/04 11:58:45 wiz Exp $
 
 Remove unportable header.
+https://sourceforge.net/p/mcj/tickets/16/
 
 --- src/w_keyboard.c.orig	2016-07-23 21:10:28.000000000 +0000
 +++ src/w_keyboard.c
