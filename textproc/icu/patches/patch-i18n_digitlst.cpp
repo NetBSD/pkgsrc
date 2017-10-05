4ac4bb71ee551bceee24766bbbc3c2ce05e3faea
maya <maya@pkgsrc.org>
2017-10-05 22:50:30 +0000

    icu: never include xlocale.h, always use locale.h
    This was a glibc header, whereas locale.h is a POSIX one.
    glibc went ahead and removed it in the new version.
    
    change suggested by Thomas Orgis on tech-pkg but probably not applied
    exactly.

--- textproc/icu/patches/patch-i18n_digitlst.cpp
+++ textproc/icu/patches/patch-i18n_digitlst.cpp
@@ -1,13 +1,20 @@
-$NetBSD: patch-i18n_digitlst.cpp,v 1.2 2016/12/12 17:46:39 adam Exp $
+$NetBSD: patch-i18n_digitlst.cpp,v 1.3 2017/10/05 22:50:30 maya Exp $
 
---- i18n/digitlst.cpp.orig	2016-10-27 01:37:56.000000000 +0000
+Never include xlocale.h - not portable header, also removed in current
+glibc.
+
+--- i18n/digitlst.cpp.orig	2017-01-20 00:20:31.000000000 +0000
 +++ i18n/digitlst.cpp
-@@ -61,7 +61,7 @@
+@@ -61,11 +61,7 @@
  #endif
  
  #if U_USE_STRTOD_L
 -# if U_PLATFORM_USES_ONLY_WIN32_API || U_PLATFORM == U_PF_CYGWIN
-+# if U_PLATFORM_USES_ONLY_WIN32_API || U_PLATFORM == U_PF_CYGWIN || defined(__NetBSD__)
- #   include <locale.h>
- # else
- #   include <xlocale.h>
+-#   include <locale.h>
+-# else
+-#   include <xlocale.h>
+-# endif
++# include <locale.h>
+ #endif
+ 
+ // ***************************************************************************
