b3c179bc603a4cd5a9f8bc5f93d45ee5bd72166c
hauke <hauke@pkgsrc.org>
2017-10-06 14:16:00 +0000

    Upgrade security/libfprint from 0.0.6 to 0.7.0
    Changes are too numerous to list.
    Install cli test tools.

--- security/libfprint/buildlink3.mk
+++ security/libfprint/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:18 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/10/06 14:16:00 hauke Exp $
 
 BUILDLINK_TREE+=	libfprint
 
 .if !defined(LIBFPRINT_BUILDLINK3_MK)
 LIBFPRINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libfprint+=	libfprint>=0.0.6
-BUILDLINK_ABI_DEPENDS.libfprint+=	libfprint>=0.0.6
+BUILDLINK_API_DEPENDS.libfprint+=	libfprint>=0.7.0
+BUILDLINK_ABI_DEPENDS.libfprint+=	libfprint>=0.7.0
 BUILDLINK_PKGSRCDIR.libfprint?=		../../security/libfprint
 .endif # LIBFPRINT_BUILDLINK3_MK
 
