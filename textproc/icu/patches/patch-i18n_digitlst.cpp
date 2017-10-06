e5709ddbbba949be71e6101f1132ab815303c386
adam <adam@pkgsrc.org>
2017-10-06 05:42:01 +0000

    Fix building on Darwin

--- textproc/icu/patches/patch-i18n_digitlst.cpp
+++ textproc/icu/patches/patch-i18n_digitlst.cpp
@@ -1,20 +1,21 @@
-$NetBSD: patch-i18n_digitlst.cpp,v 1.3 2017/10/05 22:50:30 maya Exp $
+$NetBSD: patch-i18n_digitlst.cpp,v 1.4 2017/10/06 05:42:01 adam Exp $
 
-Never include xlocale.h - not portable header, also removed in current
-glibc.
+Do not include xlocale.h, except for Darwin - not portable header,
+also removed in current glibc.
 
 --- i18n/digitlst.cpp.orig	2017-01-20 00:20:31.000000000 +0000
 +++ i18n/digitlst.cpp
-@@ -61,11 +61,7 @@
+@@ -61,10 +61,10 @@
  #endif
  
  #if U_USE_STRTOD_L
 -# if U_PLATFORM_USES_ONLY_WIN32_API || U_PLATFORM == U_PF_CYGWIN
 -#   include <locale.h>
 -# else
--#   include <xlocale.h>
--# endif
-+# include <locale.h>
++# ifdef __APPLE__
+ #   include <xlocale.h>
++# else
++#   include <locale.h>
+ # endif
  #endif
  
- // ***************************************************************************
