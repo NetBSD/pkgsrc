ea26d1a6d771726d63168610d5245b9bbc5146e9
sevan <sevan@pkgsrc.org>
2017-10-08 02:41:39 +0000

    - Add support for running as a server on NetBSD/evbarm, currently confined to ARMv6 (Pi). Support for further family revisions in NetBSD is controlled by the presence of src/config/param.armv$_nbsd$$.h to set the CPU family version and major OS version number.
    - Add description to most patches

--- filesystems/openafs/patches/patch-src_config_afs__sysnames.h
+++ filesystems/openafs/patches/patch-src_config_afs__sysnames.h
@@ -1,17 +1,20 @@
-$NetBSD: patch-src_config_afs__sysnames.h,v 1.3 2016/04/04 12:48:29 jakllsch Exp $
+$NetBSD: patch-src_config_afs__sysnames.h,v 1.4 2017/10/08 02:41:39 sevan Exp $
 
---- src/config/afs_sysnames.h.orig	2016-03-16 14:31:22.000000000 +0000
+- Add SYS_NAME_IDs for NetBSD 7 to 9 on arm/i386/amd64
+
+--- src/config/afs_sysnames.h.orig	2017-07-04 09:38:22.000000000 +0000
 +++ src/config/afs_sysnames.h
-@@ -267,6 +267,12 @@
+@@ -275,6 +275,13 @@
  #define SYS_NAME_ID_macppc_nbsd50       2541
  #define SYS_NAME_ID_amd64_nbsd60        2542
  #define SYS_NAME_ID_i386_nbsd60         2543
 +#define SYS_NAME_ID_amd64_nbsd70        2544
 +#define SYS_NAME_ID_i386_nbsd70         2545
 +#define SYS_NAME_ID_amd64_nbsd80        2546
-+#define SYS_NAME_ID_i386_nbsd80         2547
-+#define SYS_NAME_ID_amd64_nbsd90        2548
-+#define SYS_NAME_ID_i386_nbsd90         2549
++#define SYS_NAME_ID_arm32_nbsd80        2547
++#define SYS_NAME_ID_i386_nbsd80         2548
++#define SYS_NAME_ID_amd64_nbsd90        2549
++#define SYS_NAME_ID_i386_nbsd90         2550
  
  #define SYS_NAME_ID_i386_obsd31		2600
  #define SYS_NAME_ID_i386_obsd32		2601
