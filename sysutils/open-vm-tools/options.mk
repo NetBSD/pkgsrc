a6423e86ed9ee964b1f77f7aa6ab565dca97c051
adam <adam@pkgsrc.org>
2017-10-08 16:01:30 +0000

    open-vm-tools: fix distfile retrieval, building, and clean-ups
    PkgSrc-only changes:
    - Used proper MASTER_SITE
    - Distfile now can be downloaded correctly
    - Unified PLISTs
    - Clean up

--- sysutils/open-vm-tools/options.mk
+++ sysutils/open-vm-tools/options.mk
@@ -1,10 +1,12 @@
-# $NetBSD: options.mk,v 1.5 2017/02/20 09:19:27 tnn Exp $
+# $NetBSD: options.mk,v 1.6 2017/10/08 16:01:30 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.open-vm-tools
 PKG_SUPPORTED_OPTIONS=	icu x11
 PKG_SUGGESTED_OPTIONS=	icu x11
 
 .include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	x11
 
 .if !empty(PKG_OPTIONS:Micu)
 .include "../../textproc/icu/buildlink3.mk"
@@ -13,7 +15,7 @@ CONFIGURE_ARGS+=		--without-icu
 .endif
 
 .if !empty(PKG_OPTIONS:Mx11)
-PLIST_SRC+=	PLIST.x11
+PLIST.x11=	yes
 MESSAGE_SRC+=	MESSAGE.x11
 .  include "../../graphics/gdk-pixbuf2-xlib/buildlink3.mk"
 .  include "../../sysutils/desktop-file-utils/desktopdb.mk"
