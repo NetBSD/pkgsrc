9d00433babc77cfdc8b5f81948af2d2c001bde83
gdt <gdt@pkgsrc.org>
2017-10-08 15:25:45 +0000

    quagga: Update to 1.2.2
    Upstream changes:
    
    Quagga 1.2.2
    
      Upstream did not publsh NEWS -- basically bug fixes
    
    Quagga 1.2.1
    
      bug fixes and minor improvements
    
    Quagga 1.2.0
    
      This is a feature release, for testing or more adventurous
      users. More conservative users may wish to stay with an older
      release. This release contains:
    
        Next-Hop Resolution Protocol support from Timo Teräs
        BGP Large Community support, thanks to Job Snijders and Keyur Patel
        BGP session establishment reworked, for speed and reliability
        BGP route-advertisement timer interval default lowered, to 3s for eBGP and 1s for iBGP.
        BGP Connect retries made more aggressive, with lower retry timer and a slower-ramping backoff.
    
    Quagga 1.1.1
    
      bug and security fixes
      There is a known regression with IPv6 BGP sessions, see Bugzilla #870.
    
    Quagga 1.1.0
    
      This is a release with a number of new features, and many bug fixes.
      Notably:
    
        Greatly improved nexthop resolution for recursive routes. (Cumulus)
        Event driven nexthop resolution for BGP (Cumulus)
        Route tags support (Piotr Chytła, Packet Consulting)
        Transport of TE related metrics over OSPF, IS-IS (Olivier Dugeon, Orange)
        IPv6 Multipath for zebra and BGP (Ayan Banerjee, Cumulus)
    
      This release also changed the default of 'link-detect' state,
      controlling whether zebra will respond to link-state events and
      consider an interface to be down when link is down. To retain
      current the behavior save your config before updating, otherwise
      remove the 'link-detect' flag from your config prior to
      updating. There is also a new global 'default link-detect (on|off)'
      flag to configure the global default.
    
    Quagga 1.0.20161017
    
      zebra IPv6 RA and BGP MRT dump security fixes

--- net/quagga/patches/patch-zebra_kernel__socket.c
+++ net/quagga/patches/patch-zebra_kernel__socket.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-zebra_kernel__socket.c,v 1.1 2016/06/19 23:05:51 joerg Exp $
+$NetBSD: patch-zebra_kernel__socket.c,v 1.2 2017/10/08 15:25:45 gdt Exp $
 
---- zebra/kernel_socket.c.orig	2016-06-19 18:59:02.266180373 +0000
+--- zebra/kernel_socket.c.orig	2017-10-03 14:57:10.000000000 +0000
 +++ zebra/kernel_socket.c
 @@ -208,7 +208,9 @@ const struct message rtm_type_str[] =
  #ifdef RTM_OLDDEL
@@ -12,13 +12,3 @@ $NetBSD: patch-zebra_kernel__socket.c,v 1.1 2016/06/19 23:05:51 joerg Exp $
    {RTM_NEWADDR,  "RTM_NEWADDR"},
    {RTM_DELADDR,  "RTM_DELADDR"},
    {RTM_IFINFO,   "RTM_IFINFO"},
-@@ -242,7 +244,9 @@ static const struct message rtm_flag_str
- #ifdef RTF_CLONING
-   {RTF_CLONING,   "CLONING"},
- #endif /* RTF_CLONING */
-+#ifdef RTF_XRESOLVE
-   {RTF_XRESOLVE,  "XRESOLVE"},
-+#endif /* RTF_XRESOLVE */
- #ifdef RTF_LLINFO
-   {RTF_LLINFO,    "LLINFO"},
- #endif /* RTF_LLINFO */
