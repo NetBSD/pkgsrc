b3e9d6509a9c4c5ef26df5ffc3a25ce94b2ef510
spz <spz@pkgsrc.org>
2017-10-09 12:23:07 +0000

    Pullup ticket #5564 - requested by sevan math/py-numpy: security update
    Revisions pulled up:
    - math/py-numpy/Makefile                                        1.44
    - math/py-numpy/distinfo                                        1.25
    - math/py-numpy/patches/patch-numpy_distutils_fcompiler_gnu.py  1.13
    
    -------------------------------------------------------------------
       Module Name:    pkgsrc
       Committed By:   adam
       Date:           Thu Oct  5 08:21:27 UTC 2017
    
       Modified Files:
               pkgsrc/math/py-numpy: Makefile distinfo
               pkgsrc/math/py-numpy/patches: patch-numpy_distutils_fcompiler_gnu.py
    
       Log Message:
       py-numpy: update to 1.13.3
    
       NumPy 1.13.3:
       This is a bugfix release for some problems found since 1.13.1. The most
       important fixes are for CVE-2017-12852 and temporary elision. Users of
       earlier
       versions of 1.13 should upgrade.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.43 -r1.44 pkgsrc/math/py-numpy/Makefile
       cvs rdiff -u -r1.24 -r1.25 pkgsrc/math/py-numpy/distinfo
       cvs rdiff -u -r1.12 -r1.13 \
           pkgsrc/math/py-numpy/patches/patch-numpy_distutils_fcompiler_gnu.py

--- math/py-numpy/patches/patch-numpy_distutils_fcompiler_gnu.py
+++ math/py-numpy/patches/patch-numpy_distutils_fcompiler_gnu.py
@@ -1,4 +1,4 @@
-$NetBSD: patch-numpy_distutils_fcompiler_gnu.py,v 1.12 2017/06/15 07:02:53 adam Exp $
+$NetBSD: patch-numpy_distutils_fcompiler_gnu.py,v 1.12.6.1 2017/10/09 12:23:07 spz Exp $
 
 Linker needs -shared explictly (at least with GCC 4.7 on SunOS), plus
 any ABI flags as appropriate.
@@ -6,7 +6,7 @@ On OS X, do not use '-bundle' and 'dynamic_lookup' (to avoid Python.framework).
 Do not use -funroll-loops compiler flag.
 Do not run a shell command when it is "None".
 
---- numpy/distutils/fcompiler/gnu.py.orig	2017-06-07 15:26:35.000000000 +0000
+--- numpy/distutils/fcompiler/gnu.py.orig	2017-09-29 20:10:10.000000000 +0000
 +++ numpy/distutils/fcompiler/gnu.py
 @@ -57,8 +57,10 @@ class GnuFCompiler(FCompiler):
                      return ('gfortran', m.group(1))
@@ -18,9 +18,9 @@ Do not run a shell command when it is "None".
 +                if m.group(1).split(".") < ["4", "2"]:
 +                    self.g2c = "f95"
                  return ('gfortran', m.group(1))
-             m = re.search(r'GNU Fortran.*?\-?([0-9-.]+)', version_string)
-             if m:
-@@ -87,7 +89,7 @@ class GnuFCompiler(FCompiler):
+             m = re.search(
+                 r'GNU Fortran.*?\-?([0-9-.]+\.[0-9-.]+)', version_string)
+@@ -88,7 +90,7 @@ class GnuFCompiler(FCompiler):
          'compiler_f77' : [None, "-g", "-Wall", "-fno-second-underscore"],
          'compiler_f90' : None,  # Use --fcompiler=gnu95 for f90 codes
          'compiler_fix' : None,
@@ -29,7 +29,7 @@ Do not run a shell command when it is "None".
          'archiver'     : ["ar", "-cr"],
          'ranlib'       : ["ranlib"],
          'linker_exe'   : [None, "-g", "-Wall"]
-@@ -139,7 +141,7 @@ class GnuFCompiler(FCompiler):
+@@ -140,7 +142,7 @@ class GnuFCompiler(FCompiler):
                      s = 'Env. variable MACOSX_DEPLOYMENT_TARGET set to 10.3'
                      warnings.warn(s, stacklevel=2)
  
@@ -38,7 +38,7 @@ Do not run a shell command when it is "None".
          else:
              opt.append("-shared")
          if sys.platform.startswith('sunos'):
-@@ -215,7 +217,6 @@ class GnuFCompiler(FCompiler):
+@@ -216,7 +218,6 @@ class GnuFCompiler(FCompiler):
              opt = ['-O2']
          else:
              opt = ['-O3']
@@ -46,7 +46,7 @@ Do not run a shell command when it is "None".
          return opt
  
      def _c_arch_flags(self):
-@@ -270,7 +271,7 @@ class Gnu95FCompiler(GnuFCompiler):
+@@ -271,7 +272,7 @@ class Gnu95FCompiler(GnuFCompiler):
                            "-fno-second-underscore"] + _EXTRAFLAGS,
          'compiler_fix' : [None, "-Wall",  "-g","-ffixed-form",
                            "-fno-second-underscore"] + _EXTRAFLAGS,
@@ -55,7 +55,7 @@ Do not run a shell command when it is "None".
          'archiver'     : ["ar", "-cr"],
          'ranlib'       : ["ranlib"],
          'linker_exe'   : [None, "-Wall"]
-@@ -283,7 +284,7 @@ class Gnu95FCompiler(GnuFCompiler):
+@@ -284,7 +285,7 @@ class Gnu95FCompiler(GnuFCompiler):
  
      def _universal_flags(self, cmd):
          """Return a list of -arch flags for every supported architecture."""
