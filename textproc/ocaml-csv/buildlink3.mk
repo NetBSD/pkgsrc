3e872f927afb288154b307c2d2ef5b8a6705925a
jaapb <jaapb@pkgsrc.org>
2017-10-10 09:47:25 +0000

    Updated package textproc/ocaml-csv to latest version (2.0).
    Changes: Lwt support has been added (I'll add an option for that later)
    and the package now uses jbuilder to build.

--- textproc/ocaml-csv/buildlink3.mk
+++ textproc/ocaml-csv/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 12:36:21 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2017/10/10 09:47:25 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-csv
 
 .if !defined(OCAML_CSV_BUILDLINK3_MK)
 OCAML_CSV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-csv+=	ocaml-csv>=1.5
-BUILDLINK_ABI_DEPENDS.ocaml-csv+=	ocaml-csv>=1.7
+BUILDLINK_API_DEPENDS.ocaml-csv+=	ocaml-csv>=2.0
+BUILDLINK_ABI_DEPENDS.ocaml-csv+=	ocaml-csv>=2.0
 BUILDLINK_PKGSRCDIR.ocaml-csv?=	../../textproc/ocaml-csv
 
 .endif	# OCAML_CSV_BUILDLINK3_MK
