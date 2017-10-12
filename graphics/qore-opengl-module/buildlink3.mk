7347d95313f61b591e93b060673f242213c08536
nros <nros@pkgsrc.org>
2017-10-12 18:21:23 +0000

    Revbump and use of c++11 beacuse of lang/qore update.

--- graphics/qore-opengl-module/buildlink3.mk
+++ graphics/qore-opengl-module/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2016/07/22 13:12:17 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2017/10/12 18:21:24 nros Exp $
 #
 
 BUILDLINK_TREE+=	qore-opengl-module
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	qore-opengl-module
 QORE_OPENGL_MODULE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qore-opengl-module+=	qore-opengl-module>=0.0.3
-BUILDLINK_ABI_DEPENDS.qore-opengl-module?=	qore-opengl-module>=0.0.3nb7
+BUILDLINK_ABI_DEPENDS.qore-opengl-module?=	qore-opengl-module>=0.0.3nb8
 BUILDLINK_PKGSRCDIR.qore-opengl-module?=	../../graphics/qore-opengl-module
 
 .include "../../lang/qore/buildlink3.mk"
