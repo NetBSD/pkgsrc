7132e73f356c52c884089882d7df24b93edb9a0b
christos <christos@pkgsrc.org>
2017-10-13 17:24:05 +0000

    Sync with latest.

--- net/xl2tpd/patches/patch-l2tp.h
+++ net/xl2tpd/patches/patch-l2tp.h
@@ -1,11 +1,10 @@
-$NetBSD: patch-l2tp.h,v 1.2 2013/07/12 19:06:31 christos Exp $
+$NetBSD: patch-l2tp.h,v 1.3 2017/10/13 17:24:05 christos Exp $
 
 Include common.h first since it typedefs things other headers need
-Conditionalize in_pktinfo
 
---- l2tp.h.orig	2013-06-17 06:17:24.000000000 -0400
-+++ l2tp.h	2013-07-12 15:00:53.000000000 -0400
-@@ -28,13 +28,13 @@
+--- l2tp.h.orig	2017-08-02 13:46:06.000000000 -0400
++++ l2tp.h	2017-10-13 12:46:33.946695069 -0400
+@@ -30,13 +30,13 @@
  #endif
  #include "osport.h"
  #include "scheduler.h"
@@ -20,13 +19,3 @@ Conditionalize in_pktinfo
  #include "ipsecmast.h"
  
  #define CONTROL_PIPE "/var/run/xl2tpd/l2tp-control"
-@@ -172,7 +172,9 @@
-     struct call *self;
-     struct lns *lns;            /* LNS that owns us */
-     struct lac *lac;            /* LAC that owns us */
-+#ifdef IP_PKTINFO
-     struct in_pktinfo my_addr;  /* Address of my endpoint */
-+#endif
- };
- 
- struct tunnel_list
