ac1f092f94ecddfd07a32f7b23b2ab9a6e93bc6d
jperkin <jperkin@pkgsrc.org>
2017-10-13 14:51:16 +0000

    py-meinheld: Support SunOS, based on PR#52613

--- www/py-meinheld/patches/patch-setup.py
+++ www/py-meinheld/patches/patch-setup.py
@@ -1,15 +1,26 @@
-$NetBSD: patch-setup.py,v 1.1 2017/09/30 12:51:57 wiz Exp $
+$NetBSD: patch-setup.py,v 1.2 2017/10/13 14:51:16 jperkin Exp $
 
 Add NetBSD support.
 
 --- setup.py.orig	2016-11-01 23:46:07.000000000 +0000
 +++ setup.py
-@@ -41,6 +41,8 @@ def get_picoev_file():
+@@ -41,6 +41,10 @@ def get_picoev_file():
          poller_file = 'meinheld/server/picoev_kqueue.c'
      elif "FreeBSD" == platform.system():
          poller_file = 'meinheld/server/picoev_kqueue.c'
 +    elif "NetBSD" == platform.system():
 +        poller_file = 'meinheld/server/picoev_kqueue.c'
++    elif "SunOS" == platform.system():
++        poller_file = 'meinheld/server/picoev_select.c'
      else:
          print("Sorry, not support .")
          sys.exit(1)
+@@ -90,7 +94,7 @@ setup(name='meinheld',
+     author_email='yutaka.matsubara@gmail.com',
+     url='http://meinheld.org',
+     license='BSD',
+-    platforms='Linux, BSD, Darwin',
++    platforms='Linux, BSD, Darwin, SunOS',
+     packages= ['meinheld'],
+     install_requires=install_requires,
+ 
