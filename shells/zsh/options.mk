8e71604672c7752698b704debde5fb52ac38630e
jperkin <jperkin@pkgsrc.org>
2017-10-13 09:35:19 +0000

    zsh: Upgrade to 5.4.2
    pkgsrc changes:
     - Use PRINT_PLIST_AWK to aid future upgrades.
     - Support the "static" option on Darwin and SunOS.
    
    Upstream changes from 5.3.1 to 5.4:
    
    The 'exec' and 'command' precommand modifiers, and options to them, are
    now parsed after parameter expansion.  Previously, both the modifier and
    any options to it were parsed between alias expansion and parameter
    expansion (see zshexpn(1)), so they could neither be quoted nor be the
    result of parameter expansion.  Examples: 's=command; $s -V ls' and
    '\command -V ls' now work as expected.
    
    Functions executed by ZLE widgets no longer have their standard input
    closed, but redirected from /dev/null instead. That still guards
    against user defined widgets inadvertently reading from the tty device.
    
    There is an option WARN_NESTED_VAR, a companion to the existing
    WARN_CREATE_GLOBAL that causes a warning if a function updates a
    variable from an enclosing scope without using typeset -g.  It can be
    turned on for an individual function with "functions -W".
    
    zmodload now has an option -s to be silent on a failure to find a module
    but still print other errors.

--- shells/zsh/options.mk
+++ shells/zsh/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2017/05/11 13:54:27 jperkin Exp $
+# $NetBSD: options.mk,v 1.2 2017/10/13 09:35:19 jperkin Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.zsh
 PKG_SUPPORTED_OPTIONS=		static
@@ -8,7 +8,14 @@ PKG_SUPPORTED_OPTIONS=		static
 PLIST_VARS+=		dynamic
 .if !empty(PKG_OPTIONS:Mstatic)
 CONFIGURE_ARGS+=	--disable-dynamic
+.  if ${OPSYS} != "Darwin" && ${OPSYS} != "SunOS"
 LDFLAGS+=		-static
+.  endif
 .else
 PLIST.dynamic=		yes
 .endif
+PRINT_PLIST_AWK+=	{if ($$0 ~ /\.so$$/) {$$0 = "$${PLIST.dynamic}" $$0;}}
+PRINT_PLIST_AWK+=	{if ($$0 ~ /functions\/tcp/) {$$0 = "$${PLIST.dynamic}" $$0;}}
+PRINT_PLIST_AWK+=	{if ($$0 ~ /functions\/zf/) {$$0 = "$${PLIST.dynamic}" $$0;}}
+PRINT_PLIST_AWK+=	{if ($$0 ~ /functions\/zsh-newuser/) {$$0 = "$${PLIST.dynamic}" $$0;}}
+PRINT_PLIST_AWK+=	{if ($$0 ~ /scripts\/newuser/) {$$0 = "$${PLIST.dynamic}" $$0;}}
