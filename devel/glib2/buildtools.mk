4e54cc3c6ee4c5fe3f5abcafb70b389373376b47
prlw1 <prlw1@pkgsrc.org>
2017-10-13 15:49:55 +0000

    glib2/buildtools.mk: don't override PYTHON_FOR_BUILD_ONLY if already set.

--- devel/glib2/buildtools.mk
+++ devel/glib2/buildtools.mk
@@ -1,5 +1,5 @@
-# $NetBSD: buildtools.mk,v 1.1 2017/10/13 15:40:12 prlw1 Exp $
+# $NetBSD: buildtools.mk,v 1.2 2017/10/13 15:49:55 prlw1 Exp $
 
 TOOL_DEPENDS+=	${PYPKGPREFIX}-glib2-tools-[0-9]*:../../devel/py-glib2-tools
-PYTHON_FOR_BUILD_ONLY=yes
+PYTHON_FOR_BUILD_ONLY?=yes
 .include "../../lang/python/pyversion.mk"
