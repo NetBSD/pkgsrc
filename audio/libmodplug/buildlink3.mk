3af8c590da35184e48fcf49b78ef352ff8010f94
adam <adam@pkgsrc.org>
2017-10-13 16:00:59 +0000

    libmodplug: update to 0.8.9.0
    Version 0.8.9.0:
      OOB Write and Read fixes + a number of divide by zero fixes.
             (ABC, PAT, AMF, MDL, PSM, XM, IT, MMCMP, MID)
     There were some patches 2010-2016 which were recorded here.

--- audio/libmodplug/buildlink3.mk
+++ audio/libmodplug/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2011/05/05 08:52:43 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2017/10/13 16:00:59 adam Exp $
 
 BUILDLINK_TREE+=	libmodplug
 
 .if !defined(LIBMODPLUG_BUILDLINK3_MK)
 LIBMODPLUG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libmodplug+=		libmodplug>=0.7
+BUILDLINK_API_DEPENDS.libmodplug+=	libmodplug>=0.7
 BUILDLINK_ABI_DEPENDS.libmodplug+=	libmodplug>=0.8.8.1
 BUILDLINK_PKGSRCDIR.libmodplug?=	../../audio/libmodplug
 .endif # LIBMODPLUG_BUILDLINK3_MK
