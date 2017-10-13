8d7582fa69658bf5bc7f05b8d81da78878169927
adam <adam@pkgsrc.org>
2017-10-13 16:08:35 +0000

    rocksndiamonds: update to 4.0.1.0
    Rocks'n'Diamonds 4.0.1.0:
    This new release mainly adds improved support for game controllers and joysticks. While old-style joysticks and game pads were supported since a long time ago in Rocks’n’Diamonds, modern game controllers like the Xbox game controller were not supported very well until now.

--- games/rocksndiamonds/patches/patch-src_libgame_joystick.h
+++ games/rocksndiamonds/patches/patch-src_libgame_joystick.h
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_libgame_joystick.h,v 1.1 2017/09/09 07:10:34 adam Exp $
+$NetBSD: patch-src_libgame_joystick.h,v 1.2 2017/10/13 16:08:35 adam Exp $
 
 Allow joystick on NetBSD.
 
---- src/libgame/joystick.h.orig	2015-10-20 21:58:08.000000000 +0000
+--- src/libgame/joystick.h.orig	2017-09-12 20:17:10.000000000 +0000
 +++ src/libgame/joystick.h
-@@ -19,7 +19,7 @@
- #define	JOYSTICK_ACTIVE		(1 << 1)
- #define JOYSTICK_ACTIVATED	(JOYSTICK_AVAILABLE | JOYSTICK_ACTIVE)
+@@ -23,7 +23,7 @@
+ 
+ #define MAX_JOYSTICK_NAME_LEN	40
  
 -#if defined(PLATFORM_FREEBSD)
 +#if defined(PLATFORM_FREEBSD) || (defined(PLATFORM_NETBSD) && defined(HAVE_JOYSTICK))
