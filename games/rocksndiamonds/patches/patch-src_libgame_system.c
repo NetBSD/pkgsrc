b23b18cdcf2fb276248f7b13316bce98926b8c47
adam <adam@pkgsrc.org>
2017-10-13 16:08:35 +0000

    rocksndiamonds: update to 4.0.1.0
    Rocks'n'Diamonds 4.0.1.0:
    This new release mainly adds improved support for game controllers and joysticks. While old-style joysticks and game pads were supported since a long time ago in Rocks’n’Diamonds, modern game controllers like the Xbox game controller were not supported very well until now.

--- games/rocksndiamonds/patches/patch-src_libgame_system.c
+++ games/rocksndiamonds/patches/patch-src_libgame_system.c
@@ -1,18 +1,17 @@
-$NetBSD: patch-src_libgame_system.c,v 1.1 2017/09/09 07:10:34 adam Exp $
+$NetBSD: patch-src_libgame_system.c,v 1.2 2017/10/13 16:08:35 adam Exp $
 
 InitJoystick will be called more than once, so reset the state
 variable 'joystick' only if there's no joystick available (yet).
 
---- src/libgame/system.c.orig	2017-03-15 20:15:06.000000000 +0000
+--- src/libgame/system.c.orig	2017-09-12 20:17:10.000000000 +0000
 +++ src/libgame/system.c
-@@ -1603,9 +1603,10 @@ void InitJoysticks()
- #endif
+@@ -1630,8 +1630,10 @@ void InitJoysticks()
  
    /* always start with reliable default values */
--  joystick.status = JOYSTICK_NOT_AVAILABLE;
+   joystick.status = JOYSTICK_NOT_AVAILABLE;
 +  if (joystick.status == JOYSTICK_NOT_AVAILABLE) {
    for (i = 0; i < MAX_PLAYERS; i++)
-     joystick.fd[i] = -1;		/* joystick device closed */
+     joystick.nr[i] = -1;		/* no joystick configured */
 +  }
  
    SDLInitJoysticks();
