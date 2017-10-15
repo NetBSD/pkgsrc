f7397834107d9c23b8e911c35a87dbfc590689c4
spz <spz@pkgsrc.org>
2017-10-15 11:49:07 +0000

    Pullup ticket #5568 - requested by he lang/ocaml: fix for NetBSD/powerpc
    Revisions pulled up:
    - lang/ocaml/Makefile                                           1.115
    - lang/ocaml/PLIST                                              1.44
    - lang/ocaml/distinfo                                           1.111
    - lang/ocaml/patches/patch-asmrun_signals__osdep.h              1.2
    - lang/ocaml/patches/patch-configure                            1.25
    - lang/ocaml/patches/patch-testsuite_tests_lib-dynlink-native_Makefile 1.1
    - lang/ocaml/patches/patch-testsuite_tests_tool-ocamldoc-man_Makefile 1.3
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	he
       Date:		Tue Oct 10 12:10:05 UTC 2017
    
       Modified Files:
       	pkgsrc/lang/ocaml: Makefile PLIST distinfo
       	pkgsrc/lang/ocaml/patches: patch-asmrun_signals__osdep.h
       	    patch-configure
       Added Files:
       	pkgsrc/lang/ocaml/patches:
       	    patch-testsuite_tests_lib-dynlink-native_Makefile
       	    patch-testsuite_tests_tool-ocamldoc-man_Makefile
    
       Log Message:
       Apply the required fixes to make this work again on NetBSD/powerpc.
    
       A similar set of changes has been sent upstream.
       OK by jaapb@
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.114 -r1.115 pkgsrc/lang/ocaml/Makefile
       cvs rdiff -u -r1.43 -r1.44 pkgsrc/lang/ocaml/PLIST
       cvs rdiff -u -r1.110 -r1.111 pkgsrc/lang/ocaml/distinfo
       cvs rdiff -u -r1.1 -r1.2 \
           pkgsrc/lang/ocaml/patches/patch-asmrun_signals__osdep.h
       cvs rdiff -u -r1.24 -r1.25 pkgsrc/lang/ocaml/patches/patch-configure
       cvs rdiff -u -r0 -r1.1 \
           pkgsrc/lang/ocaml/patches/patch-testsuite_tests_lib-dynlink-native_Makefile
       cvs rdiff -u -r0 -r1.3 \
           pkgsrc/lang/ocaml/patches/patch-testsuite_tests_tool-ocamldoc-man_Makefile

--- lang/ocaml/patches/patch-asmrun_signals__osdep.h
+++ lang/ocaml/patches/patch-asmrun_signals__osdep.h
@@ -1,21 +1,34 @@
-$NetBSD: patch-asmrun_signals__osdep.h,v 1.1 2017/07/11 17:15:02 he Exp $
+$NetBSD: patch-asmrun_signals__osdep.h,v 1.1.6.1 2017/10/15 11:49:07 spz Exp $
 
 Provide necessary glue to do this on NetBSD/powerpc.
 
---- asmrun/signals_osdep.h.orig	2016-11-04 16:08:24.000000000 +0000
+--- asmrun/signals_osdep.h.orig	2017-07-13 08:56:44.000000000 +0000
 +++ asmrun/signals_osdep.h
-@@ -154,6 +154,26 @@
-  #define CONTEXT_YOUNG_PTR (context->uc_mcontext.gregs[REG_R15])
-  #define CONTEXT_FAULTING_ADDRESS ((char *) info->si_addr)
+@@ -303,27 +303,30 @@
+   #define CONTEXT_YOUNG_PTR (context->regs->gpr[31])
+   #define CONTEXT_SP (context->regs->gpr[1])
  
+-/****************** s390x, ELF (Linux) */
+-#elif defined(TARGET_s390x) && defined(SYS_elf)
 +/****************** PowerPC, NetBSD */
-+
-+#elif defined(TARGET_power) && defined (__NetBSD__)
-+
+ 
+-  #define DECLARE_SIGNAL_HANDLER(name) \
+-    static void name(int sig, struct sigcontext * context)
++#elif defined(TARGET_power) && defined (SYS_netbsd)
+ 
+-  #define SET_SIGACT(sigact,name) \
+-     sigact.sa_handler = (void (*)(int)) (name); \
+-     sigact.sa_flags = 0
 + #include <ucontext.h>
 + #define DECLARE_SIGNAL_HANDLER(name) \
 + static void name(int sig, siginfo_t * info, ucontext_t * context)
-+
+ 
+-  typedef unsigned long context_reg;
+-  #define CONTEXT_PC (context->sregs->regs.psw.addr)
+-  #define CONTEXT_EXCEPTION_POINTER (context->sregs->regs.gprs[13])
+-  #define CONTEXT_YOUNG_LIMIT (context->sregs->regs.gprs[10])
+-  #define CONTEXT_YOUNG_PTR (context->sregs->regs.gprs[11])
+-  #define CONTEXT_SP (context->sregs->regs.gprs[15])
 + #define SET_SIGACT(sigact,name) \
 + sigact.sa_sigaction = (void (*)(int,siginfo_t *,void *)) (name); \
 + sigact.sa_flags = SA_SIGINFO
@@ -25,18 +38,39 @@ Provide necessary glue to do this on NetBSD/powerpc.
 + #define CONTEXT_YOUNG_PTR (context->uc_mcontext.__gregs[_REG_R31])
 + #define CONTEXT_SP (_UC_MACHINE_SP(context))
 + #define CONTEXT_FAULTING_ADDRESS ((char *) info->si_addr)
-+
+ 
+-/****************** PowerPC, BSD */
 +/* #define CONTEXT_YOUNG_LIMIT (CONTEXT_STATE.CONTEXT_REG(r30)) */
 +
- /****************** I386, Linux */
++/****************** PowerPC, other BSDs */
  
- #elif defined(TARGET_i386) && defined(SYS_linux_elf)
-@@ -287,7 +307,7 @@
- 
- /****************** PowerPC, ELF (Linux) */
- 
--#elif defined(TARGET_power) && defined(SYS_elf)
-+#elif defined(TARGET_power) && defined(SYS_elf) && !defined(__NetBSD__)
+ #elif defined(TARGET_power) && \
+-    (defined(SYS_bsd) || defined(SYS_bsd_elf) || defined(SYS_netbsd))
++    (defined(SYS_bsd) || defined(SYS_bsd_elf))
  
    #define DECLARE_SIGNAL_HANDLER(name) \
-     static void name(int sig, struct sigcontext * context)
+     static void name(int sig, int code, struct sigcontext * context)
+@@ -339,6 +342,23 @@
+   #define CONTEXT_YOUNG_PTR (context->sc_frame.fixreg[31])
+   #define CONTEXT_SP (context->sc_frame.fixreg[1])
+ 
++/****************** s390x, ELF (Linux) */
++#elif defined(TARGET_s390x) && defined(SYS_elf)
++
++  #define DECLARE_SIGNAL_HANDLER(name) \
++    static void name(int sig, struct sigcontext * context)
++
++  #define SET_SIGACT(sigact,name) \
++     sigact.sa_handler = (void (*)(int)) (name); \
++     sigact.sa_flags = 0
++
++  typedef unsigned long context_reg;
++  #define CONTEXT_PC (context->sregs->regs.psw.addr)
++  #define CONTEXT_EXCEPTION_POINTER (context->sregs->regs.gprs[13])
++  #define CONTEXT_YOUNG_LIMIT (context->sregs->regs.gprs[10])
++  #define CONTEXT_YOUNG_PTR (context->sregs->regs.gprs[11])
++  #define CONTEXT_SP (context->sregs->regs.gprs[15])
++
+ /****************** SPARC, Solaris */
+ 
+ #elif defined(TARGET_sparc) && defined(SYS_solaris)
