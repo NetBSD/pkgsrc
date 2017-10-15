4190bf1b775b0125e2b028850062dfc6eaf15ffa
prlw1 <prlw1@pkgsrc.org>
2017-10-15 23:19:10 +0000

    Revert buildtool.mk additions for glib-{mkenums,genmarshal} using packages Add TOOL_DEPENDS+=glib2-tools to glib2/buildlink3.mk

--- devel/glib2/buildlink3.mk
+++ devel/glib2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2017/07/03 17:42:45 prlw1 Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2017/10/15 23:19:11 prlw1 Exp $
 
 BUILDLINK_TREE+=	glib2
 
@@ -8,6 +8,8 @@ GLIB2_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.glib2+=	glib2>=2.4.0
 BUILDLINK_ABI_DEPENDS.glib2+=	glib2>=2.34.0
 BUILDLINK_PKGSRCDIR.glib2?=	../../devel/glib2
+
+TOOL_DEPENDS+=	glib2-tools-[0-9]*:../../devel/glib2-tools
 
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
