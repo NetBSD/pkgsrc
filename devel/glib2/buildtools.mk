f76b8906c51fd87c916bdc34ca6232199d1568e0
prlw1 <prlw1@pkgsrc.org>
2017-10-15 22:31:01 +0000

    Put gdbus-codgen back into operation
    - Revert "Move gdbus-codegen users to py-glib2-tools by including
      glib2/buildtools.mk" 1f764df
    - while here change to TOOL_DEPENDS
    - switch from py-glib2-tools to glib2-tools

--- devel/glib2/buildtools.mk
+++ devel/glib2/buildtools.mk
@@ -1,5 +1,3 @@
-# $NetBSD: buildtools.mk,v 1.2 2017/10/13 15:49:55 prlw1 Exp $
+# $NetBSD: buildtools.mk,v 1.3 2017/10/15 22:31:02 prlw1 Exp $
 
-TOOL_DEPENDS+=	${PYPKGPREFIX}-glib2-tools-[0-9]*:../../devel/py-glib2-tools
-PYTHON_FOR_BUILD_ONLY?=yes
-.include "../../lang/python/pyversion.mk"
+TOOL_DEPENDS+=	glib2-tools-[0-9]*:../../devel/glib2-tools
