0d8de42e29bdbf3296cc17b0947ae772e482916f
adam <adam@pkgsrc.org>
2017-10-16 18:10:41 +0000

    Added devel/py-ipython5 version 5.5.0

---
+++ devel/py-ipython5/patches/patch-setupbase.py
@@ -0,0 +1,15 @@
+$NetBSD: patch-setupbase.py,v 1.1 2017/10/16 18:10:41 adam Exp $
+
+Honor PKGMANDIR
+
+--- setupbase.py.orig	2014-04-02 05:31:02.000000000 +0000
++++ setupbase.py
+@@ -272,7 +272,7 @@ def find_data_files():
+     Just man pages at this point.
+     """
+ 
+-    manpagebase = pjoin('share', 'man', 'man1')
++    manpagebase = pjoin(os.environ.get('PKGMANDIR'), 'man1')
+ 
+     # Simple file lists can be made by hand
+     manpages = [f for f in glob(pjoin('docs','man','*.1.gz')) if isfile(f)]
