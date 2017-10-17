ea1bf76ad785f965b59fcbf0cb9741beffe34c9d
bouyer <bouyer@pkgsrc.org>
2017-10-17 10:57:34 +0000

    Update xen*46 to 4.6.6, including fixes up to XSA244. changes since Xen 4.6.5: mostly bug fixes, including security fixes for XSA206, XSA211 to XSA244. PKGREVISION set to 1 to account for the fact that it's not a stock Xen 4.6.6.
    Note that, unlike upstream, pv-linear-pt defaults to true, so that
    NetBSD PV guests (including dom0) will continue to boot without changes
    to boot.cfg

--- sysutils/xentools46/version.mk
+++ sysutils/xentools46/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.2 2017/03/20 18:17:13 bouyer Exp $
+# $NetBSD: version.mk,v 1.3 2017/10/17 10:57:34 bouyer Exp $
 #
 # Version number is used by xentools46 and xenstoretools
 
-VERSION=	4.6.5
+VERSION=	4.6.6
 VERSION_IPXE=	9a93db3f0947484e30e753bbd61a10b17336e20e
