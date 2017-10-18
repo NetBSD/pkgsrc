7ecee98e70979cb92c546e2843d1766d3bcc7454
joerg <joerg@pkgsrc.org>
2017-10-18 11:15:39 +0000

    Explicitly depend on the extraction tools here as SKIP_DEPENDS doesn't make the dependency visible. It also helps bulk builds on platforms without native bzcat.

--- devel/gmp/inplace.mk
+++ devel/gmp/inplace.mk
@@ -1,4 +1,4 @@
-# $NetBSD: inplace.mk,v 1.6 2015/09/15 20:56:33 joerg Exp $
+# $NetBSD: inplace.mk,v 1.7 2017/10/18 11:15:39 joerg Exp $
 #
 # Include this file to extract devel/gmp source into the WRKSRC of
 # another package. This is to be used by GCC packages to avoid the
@@ -16,3 +16,5 @@ extract-inplace-gmp:
 	(cd ../../devel/gmp && ${MAKE} WRKDIR=${WRKSRC}/.devel.gmp EXTRACT_DIR=${WRKSRC} \
 		WRKSRC='$${EXTRACT_DIR}/$${DISTNAME:C/a$$//}' SKIP_DEPENDS=YES patch)
 	${MV} ${WRKSRC}/gmp-* ${WRKSRC}/gmp
+
+USE_TOOLS+=	bzcat tar
