6515c9b03de968bd5f8c7bb77b0d1c561615b892
jaapb <jaapb@pkgsrc.org>
2017-10-19 23:05:43 +0000

    Made all input files in devel/libgnomeui UTF8
    This solves a compile error, glib-mkenums expects all files to be UTF8.
    Patch lifted from:
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=870729

--- devel/libgnomeui/buildlink3.mk
+++ devel/libgnomeui/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.62 2017/02/12 06:24:40 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.63 2017/10/19 23:05:43 jaapb Exp $
 
 BUILDLINK_TREE+=	libgnomeui
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libgnomeui
 LIBGNOMEUI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libgnomeui+=		libgnomeui>=2.8.0
-BUILDLINK_ABI_DEPENDS.libgnomeui+=	libgnomeui>=2.24.5nb34
+BUILDLINK_ABI_DEPENDS.libgnomeui+=	libgnomeui>=2.24.5nb35
 BUILDLINK_PKGSRCDIR.libgnomeui?=	../../devel/libgnomeui
 
 .include "../../devel/GConf/buildlink3.mk"
