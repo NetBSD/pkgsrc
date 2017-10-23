7450bf9fbf4ef139947cacbbef7993894b18a433
jaapb <jaapb@pkgsrc.org>
2017-10-23 13:43:31 +0000

    Updated graph/ocamlgraph to version 1.8.8.
    Changes include compatibility with safe-string (needed for ocaml 4.06
    compatibility), installation fixes and some bugfixes and enhancements.

--- devel/ocamlgraph/buildlink3.mk
+++ devel/ocamlgraph/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 14:19:20 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2017/10/23 13:43:31 jaapb Exp $
 
 BUILDLINK_TREE+=	ocamlgraph
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocamlgraph
 OCAMLGRAPH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocamlgraph+=	ocamlgraph>=1.8.7
-BUILDLINK_ABI_DEPENDS.ocamlgraph+=	ocamlgraph>=1.8.7nb8
+BUILDLINK_ABI_DEPENDS.ocamlgraph+=	ocamlgraph>=1.8.8
 BUILDLINK_PKGSRCDIR.ocamlgraph?=	../../devel/ocamlgraph
 .endif	# OCAMLGRAPH_BUILDLINK3_MK
 
