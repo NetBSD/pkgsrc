c1bacae5208918eff18631cf38b0e58e41ddbb2d
morr <morr@pkgsrc.org>
2017-10-24 11:04:03 +0000

    Update to patchset 1214.
    Changes:
    8.0.1177  in a terminal window the popup menu is not cleared
    8.0.1178  using old compiler on MS-Windows
    8.0.1179  Test_popup_and_window_resize() does not always pass
    8.0.1180  MS-Windows testclean target deletes the color script
    8.0.1181  tests using Vim command fail on MS-Windows
    8.0.1182  cannot see or change mzscheme dll name
    8.0.1183  MS-Windows build instructions are outdated
    8.0.1184  the :marks command is not tested
    8.0.1185  Ruby library includes minor version number
    8.0.1186  still quite a few old style tests
    8.0.1187  building with lua fails for OSX on Travis
    8.0.1188  autocmd test fails on MS-Windows
    8.0.1189  E172 is not actually useful
    8.0.1190  unusable after opening new window in BufWritePre event
    8.0.1191  MS-Windows: missing 32 and 64 bit files in installer
    8.0.1192  MS-Windows: terminal feature not enabled by default
    8.0.1193  crash when wiping out a buffer after using getbufinfo()
    8.0.1194  actual fg and bg colors of terminal are unknown
    8.0.1195  can't build on MS-Windows
    8.0.1196  crash when t_RF is not set
    8.0.1197  MS-Windows build instructions are not up to date
    8.0.1198  older compilers don't know uint8_t
    8.0.1199  when 'clipboard' is "autoselectplus" star register is set
    8.0.1200  tests switch the bell off twice
    8.0.1201  "yL" is affected by 'scrolloff'
    8.0.1202  :wall gives an errof for a terminal window
    8.0.1203  terminal window mistreats composing characters
    8.0.1204  a QuitPre autocommand may get the wrong file name
    8.0.1205  it is possible to unload a changed buffer
    8.0.1206  no autocmd for entering or leaving the command line
    8.0.1207  profiling skips the first and last script line
    8.0.1208  'statusline' drops empty group with highlight change
    8.0.1209  still too many old style tests
    8.0.1210  CTRL-G and CTRL-T are ignored with typeahead
    8.0.1211  cannot reorder tab pages with drag & drop
    8.0.1212  MS-Windows: tear-off menu does not work on 64 bit
    8.0.1213  setting 'mzschemedll' has no effect
    8.0.1214  accessing freed memory when EXITFREE is set

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.109 2017/10/07 12:32:23 morr Exp $
+# $NetBSD: version.mk,v 1.110 2017/10/24 11:04:03 morr Exp $
 
 VIM_VERSION=		8.0
-VIM_PATCHLEVEL=		1176
+VIM_PATCHLEVEL=		1214
 # Changelog: see http://ftp.vim.org/pub/vim/patches/8.0/
 VIM_SUBDIR=		vim80
 
