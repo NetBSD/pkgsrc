8015d2b9fe3b9991aaae444ceb26b82bb4fc12eb
taca <taca@pkgsrc.org>
2017-10-27 08:47:49 +0000

    lang/php71: Update to 7.1.11
    * pkgsrc change: remove post-extract which is not required any more.
    * including securiy fixes.
    
    26 Oct 2017, PHP 7.1.11
    
    - Core:
      . Fixed bug #75241 (Null pointer dereference in zend_mm_alloc_small()).
        (Laruence)
      . Fixed bug #75236 (infinite loop when printing an error-message). (Andrea)
      . Fixed bug #75252 (Incorrect token formatting on two parse errors in one
        request). (Nikita)
      . Fixed bug #75220 (Segfault when calling is_callable on parent).
        (andrewnester)
      . Fixed bug #75290 (debug info of Closures of internal functions contain
        garbage argument names). (Andrea)
    
    - Date:
      . Fixed bug #75055 (Out-Of-Bounds Read in timelib_meridian()). (Derick)
    
    - Apache2Handler:
      . Fixed bug #75311 (error: 'zend_hash_key' has no member named 'arKey' in
        apache2handler). (mcarbonneaux)
    
    - Hash:
      . Fixed bug #75303 (sha3 hangs on bigendian). (Remi)
    
    - Intl:
      . Fixed bug #75318 (The parameter of UConverter::getAliases() is not
        optional). (cmb)
    
    - litespeed:
      . Fixed bug #75248 (Binary directory doesn't get created when building
        only litespeed SAPI). (petk)
      . Fixed bug #75251 (Missing program prefix and suffix). (petk)
    
    - mcrypt:
      . Fixed bug #72535 (arcfour encryption stream filter crashes php). (Leigh)
    
    - MySQLi:
      . Fixed bug #75018 (Data corruption when reading fields of bit type). (Anatol)
    
    - OCI8:
      . Fixed incorrect reference counting. (Dmitry, Tianfang Yang)
    
    - Opcache
      . Fixed bug #75255 (Request hangs and not finish). (Dmitry)
    
    - PCRE:
      . Fixed bug #75207 (applied upstream patch for CVE-2016-1283). (Anatol)
    
    - PDO_mysql:
      . Fixed bug #75177 (Type 'bit' is fetched as unexpected string). (Anatol)
    
    - SPL:
      . Fixed bug #73629 (SplDoublyLinkedList::setIteratorMode masks intern flags).
        (J. Jeising, cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.192 2017/10/27 08:46:49 taca Exp $
+# $NetBSD: phpversion.mk,v 1.193 2017/10/27 08:47:49 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.32
 PHP70_VERSION=	7.0.25
-PHP71_VERSION=	7.1.10
+PHP71_VERSION=	7.1.11
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
