9431eae6bf5135b150a997f297561019f318e438
bsiegert <bsiegert@pkgsrc.org>
2017-10-28 18:20:14 +0000

    Update Go to 1.9.2.
    This release includes fixes to the compiler, linker, runtime, documentation,
    go command, and the crypto/x509, database/sql, log, and net/smtp packages. It
    includes a fix to a bug introduced in Go 1.9.1 that broke "go get"
    of non-Git repositories under certain conditions.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,8 +1,8 @@
-# $NetBSD: version.mk,v 1.29 2017/10/06 18:38:25 bsiegert Exp $
+# $NetBSD: version.mk,v 1.30 2017/10/28 18:20:14 bsiegert Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
-GO_VERSION=	1.9.1
+GO_VERSION=	1.9.2
 GO14_VERSION=	1.4.3
 
 ONLY_FOR_PLATFORM=	*-*-i386 *-*-x86_64 *-*-*arm*
