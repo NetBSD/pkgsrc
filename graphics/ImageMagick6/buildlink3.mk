42acbdf084ef6d5337c2386a4b2c49203349bfe3
fhajny <fhajny@pkgsrc.org>
2017-10-30 14:44:05 +0000

    graphics/ImageMagick*: Replace (optional) dependency on jasper, removed by upstream in as early as 6.8, and replace with openjpeg - for the purposes of JPEG 2000 (JP2) support. Bump PKGREVISION.

--- graphics/ImageMagick6/buildlink3.mk
+++ graphics/ImageMagick6/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/02/28 15:19:59 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/10/30 14:44:05 fhajny Exp $
 
 BUILDLINK_TREE+=	ImageMagick6
 
@@ -15,8 +15,8 @@ pkgbase := ImageMagick6
 .if !empty(PKG_BUILD_OPTIONS.ImageMagick6:Mdjvu)
 .include "../../graphics/djvulibre-lib/buildlink3.mk"
 .endif
-.if !empty(PKG_BUILD_OPTIONS.ImageMagick6:Mjasper)
-.include "../../graphics/jasper/buildlink3.mk"
+.if !empty(PKG_BUILD_OPTIONS.ImageMagick6:Mjp2)
+.include "../../graphics/openjpeg/buildlink3.mk"
 .endif
 .if !empty(PKG_BUILD_OPTIONS.ImageMagick6:Mopenexr)
 .include "../../graphics/openexr/buildlink3.mk"
