0550aae46cb21b0de0dc50f51577dc38597e7b2f
fhajny <fhajny@pkgsrc.org>
2017-11-01 12:07:31 +0000

    Import nodejs-8.9.0 (LTS) as lang/nodejs8.
    Changes since nodejs 8.8.1:
    
    - doc: add Gibson Fahnestock to Release team
    - deps: update npm to 5.5.1
    - http2: The exposed http2 socket is no longer manipulatable
    - module: support custom paths to require.resolve()
    - util: util.TextEncoder and util.TextDecoder are no longer experimental.
      There will no longer be a warning when they are used

---
+++ lang/nodejs8/patches/patch-tools_install.py
@@ -0,0 +1,18 @@
+$NetBSD: patch-tools_install.py,v 1.1 2017/11/01 12:07:31 fhajny Exp $
+
+Install man pages under the right directory.
+
+--- tools/install.py.orig	2017-04-11 15:48:50.000000000 +0000
++++ tools/install.py
+@@ -136,10 +136,7 @@ def files(action):
+   action(['deps/v8/tools/lldbinit'], 'share/doc/node/')
+   action(['deps/v8/tools/lldb_commands.py'], 'share/doc/node/')
+ 
+-  if 'freebsd' in sys.platform or 'openbsd' in sys.platform:
+-    action(['doc/node.1'], 'man/man1/')
+-  else:
+-    action(['doc/node.1'], 'share/man/man1/')
++  action(['doc/node.1'], os.environ.get('PKGMANDIR') + '/man1/')
+ 
+   if 'true' == variables.get('node_install_npm'): npm_files(action)
+ 
