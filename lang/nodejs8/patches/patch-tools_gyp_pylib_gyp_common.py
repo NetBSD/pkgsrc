c5ae394511d903150b119e771ff5ee6714bd28a1
fhajny <fhajny@pkgsrc.org>
2017-11-01 12:07:31 +0000

    Import nodejs-8.9.0 (LTS) as lang/nodejs8.
    Changes since nodejs 8.8.1:
    
    - doc: add Gibson Fahnestock to Release team
    - deps: update npm to 5.5.1
    - http2: The exposed http2 socket is no longer manipulatable
    - module: support custom paths to require.resolve()
    - util: util.TextEncoder and util.TextDecoder are no longer experimental.
      There will no longer be a warning when they are used

---
+++ lang/nodejs8/patches/patch-tools_gyp_pylib_gyp_common.py
@@ -0,0 +1,14 @@
+$NetBSD: patch-tools_gyp_pylib_gyp_common.py,v 1.1 2017/11/01 12:07:31 fhajny Exp $
+
+Add support for NetBSD and DragonFly.
+--- tools/gyp/pylib/gyp/common.py.orig	2017-05-03 02:00:16.000000000 +0000
++++ tools/gyp/pylib/gyp/common.py
+@@ -427,6 +427,8 @@ def GetFlavor(params):
+     return 'openbsd'
+   if sys.platform.startswith('netbsd'):
+     return 'netbsd'
++  if sys.platform.startswith('dragonflybsd'):
++    return 'dragonflybsd'
+   if sys.platform.startswith('aix'):
+     return 'aix'
+ 
