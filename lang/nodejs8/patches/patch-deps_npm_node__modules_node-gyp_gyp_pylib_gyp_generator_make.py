1cdefa69a168eff82294c6d3fa5f8c7cf577c47c
fhajny <fhajny@pkgsrc.org>
2017-11-01 12:07:31 +0000

    Import nodejs-8.9.0 (LTS) as lang/nodejs8.
    Changes since nodejs 8.8.1:
    
    - doc: add Gibson Fahnestock to Release team
    - deps: update npm to 5.5.1
    - http2: The exposed http2 socket is no longer manipulatable
    - module: support custom paths to require.resolve()
    - util: util.TextEncoder and util.TextDecoder are no longer experimental.
      There will no longer be a warning when they are used

---
+++ lang/nodejs8/patches/patch-deps_npm_node__modules_node-gyp_gyp_pylib_gyp_generator_make.py
@@ -0,0 +1,15 @@
+$NetBSD: patch-deps_npm_node__modules_node-gyp_gyp_pylib_gyp_generator_make.py,v 1.1 2017/11/01 12:07:31 fhajny Exp $
+
+Ensure we use the system libtool on OSX.
+
+--- deps/npm/node_modules/node-gyp/gyp/pylib/gyp/generator/make.py.orig	2013-06-13 17:35:30.000000000 +0000
++++ deps/npm/node_modules/node-gyp/gyp/pylib/gyp/generator/make.py
+@@ -161,7 +161,7 @@ cmd_solink_module = $(LINK.$(TOOLSET)) -
+ 
+ LINK_COMMANDS_MAC = """\
+ quiet_cmd_alink = LIBTOOL-STATIC $@
+-cmd_alink = rm -f $@ && ./gyp-mac-tool filter-libtool libtool $(GYP_LIBTOOLFLAGS) -static -o $@ $(filter %.o,$^)
++cmd_alink = rm -f $@ && ./gyp-mac-tool filter-libtool /usr/bin/libtool $(GYP_LIBTOOLFLAGS) -static -o $@ $(filter %.o,$^)
+ 
+ quiet_cmd_link = LINK($(TOOLSET)) $@
+ cmd_link = $(LINK.$(TOOLSET)) $(GYP_LDFLAGS) $(LDFLAGS.$(TOOLSET)) -o "$@" $(LD_INPUTS) $(LIBS)
