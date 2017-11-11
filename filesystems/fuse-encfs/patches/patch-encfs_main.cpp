b2c96c97bc43574ade012f7e07d5d1c406506bfc
gdt <gdt@pkgsrc.org>
2017-11-11 03:18:16 +0000

    Update to 1.9.2:
      - fix a use-after-free bug that was introduced in v1.9-rc1 (#214)
      - cast booleans to int before writing the XML config (#343)
      - support reading the config file from pipes (#253)
      - add "-t" option to set syslog tag
      - allow read/write in standard reverse mode (#301)
      - reject empty passwords
      - support building with openssl 1.1

--- filesystems/fuse-encfs/patches/patch-encfs_main.cpp
+++ filesystems/fuse-encfs/patches/patch-encfs_main.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-encfs_main.cpp,v 1.1 2017/06/19 18:41:39 maya Exp $
+$NetBSD: patch-encfs_main.cpp,v 1.2 2017/11/11 03:18:16 gdt Exp $
 
---- encfs/main.cpp.orig	2016-09-18 20:16:04.000000000 +0000
+--- encfs/main.cpp.orig	2017-07-25 18:26:45.000000000 +0000
 +++ encfs/main.cpp
 @@ -488,8 +488,10 @@ static void *idleMonitor(void *);
  void *encfs_init(fuse_conn_info *conn) {
@@ -11,5 +11,5 @@ $NetBSD: patch-encfs_main.cpp,v 1.1 2017/06/19 18:41:39 maya Exp $
    conn->async_read = true;
 +#endif
  
-   if (ctx->args->isDaemon) {
-     // Switch to using syslog.
+   // if an idle timeout is specified, then setup a thread to monitor the
+   // filesystem.
