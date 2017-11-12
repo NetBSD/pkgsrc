a19b529371687006ce1ac3cf4c735e565057ed51
khorben <khorben@pkgsrc.org>
2017-11-12 15:36:57 +0000

    Update dependencies on print/cups to print/cups-base
    This is with the notable exception of meta-pkgs/desktop-gnome, which I
    believe implies a fully functional cups.
    
    This is still missing revision bumps - I'll be right there (first time I
    am doing this on so many packages at a time).
    
    As discussed on tech-pkg@

--- print/epdfview/options.mk
+++ print/epdfview/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2017/05/19 13:18:18 prlw1 Exp $
+# $NetBSD: options.mk,v 1.4 2017/11/12 15:36:58 khorben Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.epdfview
 PKG_SUPPORTED_OPTIONS=	cups
@@ -6,7 +6,7 @@ PKG_SUPPORTED_OPTIONS=	cups
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mcups)
-.include "../../print/cups/buildlink3.mk"
+.include "../../print/cups-base/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-cups
 .else
 CONFIGURE_ARGS+=	--without-cups
