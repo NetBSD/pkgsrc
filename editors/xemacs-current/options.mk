3068e0f357ed6e3b2848c68e719dd0bd0dd60fab
hauke <hauke@pkgsrc.org>
2017-11-15 14:46:31 +0000

    Upgrade xemacs-current to 21.5.34
    Since the non-{X11,lucid} toolkits have bitrotted, remove those
    options, and rework options.mk like the editors/xemacs one.
    
    Switching to the portable dumper and using system malloc obsoletes the
    hacks.
    
    Like XEmacs 21.4 and the emacsen, we need to build with -DTERMINFO for
    proper console display.
    
    Since Makefile content is minimal for xemacs-current{,-nox11}, keep
    the pkgsrc nb revision centrally in Makefile.common.
    
    Take maintainership.

--- editors/xemacs-current/options.mk
+++ editors/xemacs-current/options.mk
@@ -1,38 +1,69 @@
-# $NetBSD: options.mk,v 1.14 2016/12/30 23:18:53 wiz Exp $
+# $NetBSD: options.mk,v 1.15 2017/11/15 14:46:31 hauke Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.xemacs
-PKG_SUPPORTED_OPTIONS+=		ldap xface canna debug
-#PKG_SUPPORTED_OPTIONS+=	esound
+PKG_SUPPORTED_OPTIONS+=		ldap canna debug
 
 .include "../../mk/bsd.options.mk"
 
-###
-### Any of the "toolkit" options or "xft" implies "x11".
-###
-.if !empty(PKG_OPTIONS:Mgtk) || !empty(PKG_OPTIONS:Mlucid) || \
-    !empty(PKG_OPTIONS:Mmotif) || !empty(PKG_OPTIONS:Mxft)
-.  if empty(PKG_OPTIONS:Mx11)
+# Option "xft" implies "x11".
+.if  empty(PKG_OPTIONS:Mx11) && !empty(PKG_OPTIONS:Mxft)
 PKG_OPTIONS+=		x11
-.  endif
 .endif
 
 ###
-### Default to using the Lucid X11 toolkit if none is specified.
+### Support drawing pretty X11 Lucid widgets
 ###
+PLIST_VARS+=		x11
 .if !empty(PKG_OPTIONS:Mx11)
-.  if empty(PKG_OPTIONS:Mgtk) && empty(PKG_OPTIONS:Mlucid) && \
-      empty(PKG_OPTIONS:Mmotif)
-PKG_OPTIONS+=		lucid
-.  endif
+.  include "../../mk/jpeg.buildlink3.mk"
+.  include "../../graphics/png/buildlink3.mk"
+.  include "../../graphics/tiff/buildlink3.mk"
+.  include "../../x11/libXpm/buildlink3.mk"
+.  include "../../mk/xaw.buildlink3.mk"
+.  include "../../x11/xbitmaps/buildlink3.mk"
+PLIST.x11=		yes
+CONFIGURE_ARGS+=	--with-x
+CONFIGURE_ARGS+=	--with-jpeg
+CONFIGURE_ARGS+=	--with-png
+CONFIGURE_ARGS+=	--with-tiff
+CONFIGURE_ARGS+=	--with-xpm
+CONFIGURE_ARGS+=	--with-site-includes=${PREFIX}/include:${X11BASE}/include
+CONFIGURE_ARGS+=	--with-site-libraries=${PREFIX}/lib:${X11BASE}/lib
+CONFIGURE_ARGS+=	--with-site-runtime-libraries=${PREFIX}/lib:${X11BASE}/lib
+# Lucid widgets
+CONFIGURE_ARGS+=	--with-toolbars=yes
+CONFIGURE_ARGS+=	--with-menubars=yes
+CONFIGURE_ARGS+=	--with-scrollbars=lucid
+CONFIGURE_ARGS+=	--with-dialogs=lucid
+CONFIGURE_ARGS+=	--with-widgets=lucid
+CONFIGURE_ARGS+=	--with-athena=xaw
+CONFIGURE_ARGS+=	--with-xim=xlib
+.else
+CONFIGURE_ARGS+=	--without-x
+CONFIGURE_ARGS+=	--without-jpeg
+CONFIGURE_ARGS+=	--without-png
+CONFIGURE_ARGS+=	--without-tiff
+CONFIGURE_ARGS+=	--without-xpm
+CONFIGURE_ARGS+=	--with-site-includes=${PREFIX}/include
+CONFIGURE_ARGS+=	--with-site-libraries=${PREFIX}/lib
+CONFIGURE_ARGS+=	--with-site-runtime-libraries=${PREFIX}/lib
 .endif
 
-PLIST_VARS+=		ldap
 .if !empty(PKG_OPTIONS:Mldap)
-PLIST.ldap=		yes
 CONFIGURE_ARGS+=	--with-ldap
 .  include "../../databases/openldap-client/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-ldap
+.endif
+
+.if !empty(PKG_OPTIONS:Mxft)
+.  include "../../fonts/fontconfig/buildlink3.mk"
+.  include "../../graphics/freetype2/buildlink3.mk"
+.  include "../../x11/libXft/buildlink3.mk"
+.  include "../../x11/libXrender/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-xft=emacs,tabs,menubars,gauges
+.else
+CONFIGURE_ARGS+=	--with-xft=no
 .endif
 
 .if !empty(PKG_OPTIONS:Mxface)
@@ -42,79 +73,18 @@ CONFIGURE_ARGS+=	--with-xface
 CONFIGURE_ARGS+=	--without-xface
 .endif
 
+PLIST_VARS+=		canna
 .if !empty(PKG_OPTIONS:Mcanna)
 .  include "../../inputmethod/canna-lib/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-canna
+PLIST.canna=		yes
 .else
 CONFIGURE_ARGS+=	--without-canna
 .endif
 
-.if !empty(PKG_OPTIONS:Mesound)
-.  include "../../audio/esound/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--with-sound=none
-.endif
-
+PLIST_VARS+=            debug
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-debug=yes --with-debug
-.endif
-
-###
-### Support drawing pretty X11 widgets.
-###
-.if !empty(PKG_OPTIONS:Mx11)
-.  include "../../mk/jpeg.buildlink3.mk"
-.  include "../../graphics/png/buildlink3.mk"
-.  include "../../graphics/tiff/buildlink3.mk"
-.  include "../../x11/xbitmaps/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-x
-.  if ${X11_TYPE} != "modular"
-CONFIGURE_ARGS+=	--with-site-prefixes=${PREFIX}:${X11BASE}
-CONFIGURE_ARGS+=	--with-site-runtime-libraries=${PREFIX}/lib:${X11BASE}/lib
-.  else
-CONFIGURE_ARGS+=	--with-site-prefixes=${PREFIX}
-CONFIGURE_ARGS+=	--with-site-runtime-libraries=${PREFIX}/lib
-.  endif
-.else
-CONFIGURE_ARGS+=	--without-x
-CONFIGURE_ARGS+=	--without-jpeg
-CONFIGURE_ARGS+=	--without-png
-CONFIGURE_ARGS+=	--without-tiff
-CONFIGURE_ARGS+=	--without-xpm
-CONFIGURE_ARGS+=	--with-site-prefixes=${PREFIX}
-CONFIGURE_ARGS+=	--with-site-runtime-libraries=${PREFIX}/lib
-.endif
-
-###
-### Support using Xft for rendering TrueType fonts.
-###
-.if !empty(PKG_OPTIONS:Mxft)
-.  include "../../fonts/fontconfig/buildlink3.mk"
-.  include "../../graphics/freetype2/buildlink3.mk"
-.  include "../../x11/libXft/buildlink3.mk"
-.  include "../../x11/libXrender/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-xft=emacs,tabs,menubars,gauges
-.endif
-
-###
-### Support using GTK X11 widgets.
-###
-.if !empty(PKG_OPTIONS:Mgtk)
-.  include "../../x11/gtk/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-gtk
-.endif
-
-###
-### Support using Motif X11 widgets.
-###
-.if !empty(PKG_OPTIONS:Mmotif)
-.  include "../../mk/motif.buildlink3.mk"
-.endif
-
-###
-### Support using Lucid X11 widgets.
-###
-.if !empty(PKG_OPTIONS:Mlucid)
-.  include "../../mk/xaw.buildlink3.mk"
-CONFIGURE_ARGS+=	--with-widgets=lucid
+CFLAGS+=                -g3
+INSTALL_UNSTRIPPED=     yes
 .endif
