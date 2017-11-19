2927acb929f8599533a9cdfad32917bbc143facc
jmcneill <jmcneill@pkgsrc.org>
2017-11-19 16:50:35 +0000

    Update to 2017.09

--- sysutils/u-boot-pinebook/patches/patch-lib_bch.c
+++ sysutils/u-boot-pinebook/patches/patch-lib_bch.c
@@ -1,13 +1,26 @@
-$NetBSD: patch-lib_bch.c,v 1.1 2017/09/10 17:00:41 jmcneill Exp $
+$NetBSD: patch-lib_bch.c,v 1.2 2017/11/19 16:50:35 jmcneill Exp $
 
---- lib/bch.c.orig	2017-07-10 17:07:38.000000000 +0000
+--- lib/bch.c.orig	2017-09-21 06:16:57.000000000 +0000
 +++ lib/bch.c
-@@ -61,7 +61,7 @@
+@@ -61,8 +61,11 @@
  #include <linux/bitops.h>
  #else
  #include <errno.h>
 -#if defined(__FreeBSD__)
 +#if defined(__FreeBSD__) || defined(__NetBSD__)
  #include <sys/endian.h>
++#elif defined(__APPLE__)
++#include <machine/endian.h>
++#define htobe32 htonl
  #else
  #include <endian.h>
+ #endif
+@@ -117,7 +120,7 @@ struct gf_poly_deg1 {
+ };
+ 
+ #ifdef USE_HOSTCC
+-#if !defined(__DragonFly__) && !defined(__FreeBSD__)
++#if !defined(__DragonFly__) && !defined(__FreeBSD__) && !defined(__APPLE__)
+ static int fls(int x)
+ {
+ 	int r = 32;
