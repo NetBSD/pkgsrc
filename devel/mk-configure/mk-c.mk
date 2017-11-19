8bfd5cc754c23ec9266c0a354c6b1c80d11c05cd
cheusov <cheusov@pkgsrc.org>
2017-11-19 15:08:25 +0000

    Update mk-configure to 0.29.2
      Fix MKC_FEATURES "fgenln"
      mkc_check_common.sh is installed to libexec directory

--- devel/mk-configure/mk-c.mk
+++ devel/mk-configure/mk-c.mk
@@ -1,16 +1,17 @@
-# $NetBSD: mk-c.mk,v 1.5 2014/09/14 13:11:39 cheusov Exp $
+# $NetBSD: mk-c.mk,v 1.6 2017/11/19 15:08:25 cheusov Exp $
 
 TOOL_DEPENDS+=	${BUILDLINK_API_DEPENDS.mk-c}:${BUILDLINK_PKGSRCDIR.mk-c}
 
 MAKE_PROGRAM=	mkcmake
 MAKE_ENV+=	MANDIR=${PREFIX}/${PKGMANDIR} INSTALL=${INSTALL:Q}
 MAKE_ENV+=	SYSCONFDIR=${PKG_SYSCONFDIR}
+CONFIGURE_TARGET?=	errorcheck
 
 do-configure:
 	set -e; cd ${WRKSRC}; \
 	env ${MAKE_ENV} \
 	    ${MAKE_PROGRAM} ${MAKE_FLAGS} ${BUILD_MAKE_FLAGS} \
-		-f ${MAKE_FILE} errorcheck
+		-f ${MAKE_FILE} ${CONFIGURE_TARGET}
 
 ###
 BUILDLINK_TREE+=	mk-c
