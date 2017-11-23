3a2f00f636c590f2c18e6122504b59afd472684a
wiz <wiz@pkgsrc.org>
2017-11-23 17:19:40 +0000

    recursive bump for libxkbcommon removal from at-spi2-core

--- devel/libgnomeuimm/buildlink3.mk
+++ devel/libgnomeuimm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.50 2017/02/12 06:24:40 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.51 2017/11/23 17:19:41 wiz Exp $
 
 BUILDLINK_TREE+=	libgnomeuimm
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libgnomeuimm
 LIBGNOMEUIMM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libgnomeuimm+=	libgnomeuimm>=2.10.0
-BUILDLINK_ABI_DEPENDS.libgnomeuimm+=	libgnomeuimm>=2.28.0nb35
+BUILDLINK_ABI_DEPENDS.libgnomeuimm+=	libgnomeuimm>=2.28.0nb36
 BUILDLINK_PKGSRCDIR.libgnomeuimm?=	../../devel/libgnomeuimm
 
 .include "../../devel/gconfmm/buildlink3.mk"
--- x11/vte3/buildlink3.mk
+++ x11/vte3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/06/26 11:29:46 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2017/11/23 17:19:46 wiz Exp $
 
 BUILDLINK_TREE+=	vte
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	vte
 VTE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.vte+=	vte>=0.38.0
-BUILDLINK_ABI_DEPENDS.vte+=	vte>=0.38.0
+BUILDLINK_ABI_DEPENDS.vte+=	vte>=0.38.4nb1
 BUILDLINK_PKGSRCDIR.vte?=	../../x11/vte3
 
 .include "../../x11/gtk3/buildlink3.mk"
