7848ae22b3e3fd888852e683d35b6adb0dac0346
taca <taca@pkgsrc.org>
2017-11-25 13:16:08 +0000

    lang/php71: Update to 7.1.12
    23 Nov 2017, PHP 7.1.12
    
    - Core:
      . Fixed bug #75420 (Crash when modifing property name in __isset for
        BP_VAR_IS). (Laruence)
      . Fixed bug #75368 (mmap/munmap trashing on unlucky allocations). (Nikita,
        Dmitry)
    
    - CLI:
      . Fixed bug #75287 (Builtin webserver crash after chdir in a shutdown
        function). (Laruence)
    
    - Enchant:
      . Fixed bug #53070 (enchant_broker_get_path crashes if no path is set). (jelle
        van der Waa, cmb)
      . Fixed bug #75365 (Enchant still reports version 1.1.0). (cmb)
    
    - Exif:
      . Fixed bug #75301 (Exif extension has built in revision version). (Peter
        Kokot)
    
    - GD:
      . Fixed bug #65148 (imagerotate may alter image dimensions). (cmb)
      . Fixed bug #75437 (Wrong reflection on imagewebp). (Fabien Villepinte)
    
    - intl:
      . Fixed bug #75317 (UConverter::setDestinationEncoding changes source instead
        of destination). (andrewnester)
    
    - interbase:
      . Fixed bug #75453 (Incorrect reflection for ibase_[p]connect). (villfa)
    
    - Mysqli:
      . Fixed bug #75434 (Wrong reflection for mysqli_fetch_all function). (Fabien
        Villepinte)
    
    - OCI8:
      . Fixed valgrind issue. (Tianfang Yang)
    
    - OpenSSL:
      . Fixed bug #75363 (openssl_x509_parse leaks memory). (Bob, Jakub Zelenka)
      . Fixed bug #75307 (Wrong reflection for openssl_open function). (villfa)
    
    - Opcache:
      . Fixed bug #75373 (Warning Internal error: wrong size calculation). (Laruence, Dmitry)
    
    - PGSQL:
      . Fixed bug #75419 (Default link incorrectly cleared/linked by pg_close()). (Sara)
    
    - SOAP:
      . Fixed bug #75464 (Wrong reflection on SoapClient::__setSoapHeaders). (villfa)
    
    - Zlib:
      . Fixed bug #75299 (Wrong reflection on inflate_init and inflate_add). (Fabien
        Villepinte)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.195 2017/11/25 13:07:40 taca Exp $
+# $NetBSD: phpversion.mk,v 1.196 2017/11/25 13:16:08 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.32
 PHP70_VERSION=	7.0.26
-PHP71_VERSION=	7.1.11
+PHP71_VERSION=	7.1.12
 PHP72_VERSION=	7.2.0RC6
 
 # Define initial release of major version.
