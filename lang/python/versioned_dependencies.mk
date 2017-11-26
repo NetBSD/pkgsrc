8550d000b8ca6ae30c693561ebe725819175294e
wiz <wiz@pkgsrc.org>
2017-11-26 09:43:33 +0000

    versioned_dependencies: remove gobject support
    unused, and doesn't work as-is

--- lang/python/versioned_dependencies.mk
+++ lang/python/versioned_dependencies.mk
@@ -1,4 +1,4 @@
-# $NetBSD: versioned_dependencies.mk,v 1.29 2017/11/24 22:16:34 wiz Exp $
+# $NetBSD: versioned_dependencies.mk,v 1.30 2017/11/26 09:43:33 wiz Exp $
 #
 # This file determines which separate distribution of a Python
 # package is used as dependency, depending on the Python version
@@ -9,7 +9,7 @@
 # PYTHON_VERSIONED_DEPENDENCIES
 #       The Python package which should be added as a dependency.
 #
-#       Possible values: Pmw X dialog ephem flup gobject ipython jsonlib python-digest
+#       Possible values: Pmw X dialog ephem flup ipython jsonlib python-digest
 #       Default: (nothing)
 #
 
@@ -21,7 +21,6 @@ _SUPPORTED_PACKAGES+=	X textproc/py-X2 textproc/py-X
 _SUPPORTED_PACKAGES+=	dialog devel/py-dialog2 devel/py-dialog
 _SUPPORTED_PACKAGES+=	ephem math/py-ephem math/py-ephem3
 _SUPPORTED_PACKAGES+=	flup www/py-flup www/py-flup3
-_SUPPORTED_PACKAGES+=	gobject devel/py-gobject devel/py-gobject3
 _SUPPORTED_PACKAGES+=	ipython devel/py-ipython5 devel/py-ipython
 _SUPPORTED_PACKAGES+=	jsonlib textproc/py-jsonlib textproc/py-jsonlib3
 _SUPPORTED_PACKAGES+=	python-digest www/py-python-digest www/py-python3-digest
