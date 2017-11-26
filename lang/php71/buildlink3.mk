c2cf64f06f42f610367e43393ed9af4bdf91640e
wiz <wiz@pkgsrc.org>
2017-11-26 19:14:34 +0000

    php71: improve pattern
    Since we have php72 release candidates in pkgsrc, "<7.2" is not sufficient
    since this will pull in "7.2rc6". Change pattern to <7.1.99 to stop this.

--- lang/php71/buildlink3.mk
+++ lang/php71/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2017/09/26 14:02:53 roy Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2017/11/26 19:14:34 wiz Exp $
 
 BUILDLINK_TREE+=	php
 
 .if !defined(PHP_BUILDLINK3_MK)
 PHP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.php+=	php>=7.1.0<7.2
-BUILDLINK_ABI_DEPENDS.php+=	php>=7.1.0<7.2
+BUILDLINK_API_DEPENDS.php+=	php>=7.1.0<7.1.99
+BUILDLINK_ABI_DEPENDS.php+=	php>=7.1.0<7.1.99
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php71
 
 .include "../../textproc/libxml2/buildlink3.mk"
