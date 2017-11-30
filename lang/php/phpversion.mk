5ec29480646be2fe1dfd2a5385d1c42399296a55
jdolecek <jdolecek@pkgsrc.org>
2017-11-30 20:45:46 +0000

    Updated php72 from RC6 to final 7.2.0
    Changes versus 7.1.x as listed in release announcement:
    * Convert numeric keys in object/array casts
    * Counting of non-countable objects
    * Object typehint
    * HashContext as Object
    * Argon2 in password hash
    * Improve TLS constants to sane values
    * Mcrypt extension removed
    * New sodium extension

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.196 2017/11/25 13:16:08 taca Exp $
+# $NetBSD: phpversion.mk,v 1.197 2017/11/30 20:45:46 jdolecek Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.32
 PHP70_VERSION=	7.0.26
 PHP71_VERSION=	7.1.12
-PHP72_VERSION=	7.2.0RC6
+PHP72_VERSION=	7.2.0
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
