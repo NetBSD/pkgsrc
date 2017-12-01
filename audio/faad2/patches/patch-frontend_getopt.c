36a45d20d2145450a18bc1c3f3aa64d54323074c
adam <adam@pkgsrc.org>
2017-12-01 11:17:51 +0000

    faad2: updated to 2.8.6
    2.8.6:
    2.8.5:
            - another package fix
    2.8.4:
            - minor fix to released packages
    2.8.3
            - better autotools support
    2.8.2
            - PNS bug fixed
            - New MP4 input module
            - NeAACDecGetVersion:
                    new api function to get version and copyright strings

--- audio/faad2/patches/patch-frontend_getopt.c
+++ audio/faad2/patches/patch-frontend_getopt.c
@@ -1,15 +1,14 @@
-$NetBSD: patch-frontend_getopt.c,v 1.1 2017/07/23 16:09:17 adam Exp $
+$NetBSD: patch-frontend_getopt.c,v 1.2 2017/12/01 11:17:51 adam Exp $
 
 Avoid conflicting definitions.
 
---- frontend/getopt.c.orig	2017-07-23 13:10:42.000000000 +0000
+--- frontend/getopt.c.orig	2017-09-06 08:37:30.000000000 +0000
 +++ frontend/getopt.c
-@@ -171,8 +171,6 @@ static enum
+@@ -171,7 +171,6 @@ static enum
  
  #if __STDC__ || defined(PROTO)
  extern char *getenv(const char *name);
 -extern int  strcmp (const char *s1, const char *s2);
--extern int  strncmp(const char *s1, const char *s2, unsigned int n);
  
  static int my_strlen(const char *s);
  static char *my_index (const char *str, int chr);
