3474477165aec8c2f37d23ab0a40edaf30b14259
maya <maya@pkgsrc.org>
2017-12-08 08:52:56 +0000

    bump for print/poppler major bump
    note: script detailed in `man revbump` insisted on bumping pkgrevision of
    print/poppler, it's probably wrong, but not committing this change is wronger.

--- print/poppler-cpp/buildlink3.mk
+++ print/poppler-cpp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.31 2017/09/04 19:57:18 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2017/12/08 08:52:56 maya Exp $
 
 BUILDLINK_TREE+=	poppler-cpp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-cpp
 POPPLER_CPP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-cpp+=	poppler-cpp>=0.16.7
-BUILDLINK_ABI_DEPENDS.poppler-cpp+=	poppler-cpp>=0.59.0
+BUILDLINK_ABI_DEPENDS.poppler-cpp+=	poppler-cpp>=0.61.1nb1
 BUILDLINK_PKGSRCDIR.poppler-cpp?=	../../print/poppler-cpp
 
 .include "../../print/poppler/buildlink3.mk"
--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.57 2017/12/08 07:48:11 spz Exp $
+# $NetBSD: buildlink3.mk,v 1.58 2017/12/08 08:52:56 maya Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.61.0
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.61.1nb1
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 .include "../../graphics/lcms2/buildlink3.mk"
