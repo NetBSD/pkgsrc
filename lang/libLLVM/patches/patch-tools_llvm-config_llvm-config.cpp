bb1753b3bd0c6156d361c2a1240f39930ccc2c44
gdt <gdt@pkgsrc.org>
2017-12-08 02:14:44 +0000

    libLLVM: Revert update to 5.0.0
    The update broke MesaLib, which blocks a vast number of packages.  In
    the interest of stability heading towards 2017Q4, revert it, leaving
    the issue of how to handle updates to this package (how much testing
    is required, keeping multiple versions, etc.) to after the branch.

--- lang/libLLVM/patches/patch-tools_llvm-config_llvm-config.cpp
+++ lang/libLLVM/patches/patch-tools_llvm-config_llvm-config.cpp
@@ -1,26 +1,24 @@
-$NetBSD: patch-tools_llvm-config_llvm-config.cpp,v 1.4 2017/12/01 19:22:13 adam Exp $
+$NetBSD: patch-tools_llvm-config_llvm-config.cpp,v 1.5 2017/12/08 02:14:44 gdt Exp $
 
 Avoid conflict with lang/clang.
 Use -lLLVM-3.8 instead of -lLLVM-3.8.0 so we don't break unnecessarily
 on patch updates.
 
---- tools/llvm-config/llvm-config.cpp.orig	2017-06-01 20:51:55.000000000 +0000
+--- tools/llvm-config/llvm-config.cpp.orig	2017-01-12 19:47:38.000000000 +0000
 +++ tools/llvm-config/llvm-config.cpp
-@@ -332,11 +332,11 @@ int main(int argc, char **argv) {
+@@ -332,9 +332,9 @@ int main(int argc, char **argv) {
          ("-I" + ActiveIncludeDir + " " + "-I" + ActiveObjRoot + "/include");
    } else {
      ActivePrefix = CurrentExecPrefix;
 -    ActiveIncludeDir = ActivePrefix + "/include";
 +    ActiveIncludeDir = "@PREFIX@/include/libLLVM";
-     SmallString<256> path(StringRef(LLVM_TOOLS_INSTALL_DIR));
-     sys::fs::make_absolute(ActivePrefix, path);
-     ActiveBinDir = path.str();
+     ActiveBinDir = ActivePrefix + "/bin";
 -    ActiveLibDir = ActivePrefix + "/lib" + LLVM_LIBDIR_SUFFIX;
 +    ActiveLibDir = "@PREFIX@/lib/libLLVM";
      ActiveCMakeDir = ActiveLibDir + "/cmake/llvm";
      ActiveIncludeOption = "-I" + ActiveIncludeDir;
    }
-@@ -372,14 +372,14 @@ int main(int argc, char **argv) {
+@@ -370,14 +370,14 @@ int main(int argc, char **argv) {
      StaticDir = ActiveLibDir;
    } else if (HostTriple.isOSDarwin()) {
      SharedExt = "dylib";
@@ -37,7 +35,7 @@ on patch updates.
      StaticExt = "a";
      StaticDir = SharedDir = ActiveLibDir;
      StaticPrefix = SharedPrefix = "lib";
-@@ -392,7 +392,7 @@ int main(int argc, char **argv) {
+@@ -390,7 +390,7 @@ int main(int argc, char **argv) {
  
    bool DyLibExists = false;
    const std::string DyLibName =
@@ -46,7 +44,7 @@ on patch updates.
  
    // If LLVM_LINK_DYLIB is ON, the single shared library will be returned
    // for "--libs", etc, if they exist. This behaviour can be overridden with
-@@ -493,7 +493,7 @@ int main(int argc, char **argv) {
+@@ -491,7 +491,7 @@ int main(int argc, char **argv) {
          OS << ActiveIncludeOption << ' ' << LLVM_CXXFLAGS << '\n';
        } else if (Arg == "--ldflags") {
          OS << ((HostTriple.isWindowsMSVCEnvironment()) ? "-LIBPATH:" : "-L")
