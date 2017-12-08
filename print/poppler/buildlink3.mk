b632d5c0c4838f4d495bf45e1df63d565e984ead
spz <spz@pkgsrc.org>
2017-12-08 07:48:11 +0000

    Update poppler packages from 0.59.0 to 0.61.1 fixing CVE-2017-14517 CVE-2017-14518 CVE-2017-14519 CVE-2017-14520 CVE-2017-15565
    upstream notable changes:
    Release 0.61.1
            core:
             * CairoOutputDev: don't overflow y * stride when accessing image data
    
            cpp:
             * Fix for corrupted image files on Windows. Bug #102494
    
            build system:
             * Fix incorrect paths in .pc files. Bug #103578
             * add the custom buildtests target only once. Bug #103003
    
    Release 0.61.0
            core:
             * Fix crashes in broken files
             * Cleanup unused functions from GlobalParams
             * Tweak LZWStream::processNextCode error handling. Bug #103174
             * Warning fixes
             * Remove t1lib code
    
            qt5:
             * Clean up the remaining Splash code in Arthur backend. Bug #103117
             * ArthurOutputDev: Properly implement saveState/restoreState. Bug #103118
             * Fix leak in ArthurOutputDev::updateFont. Bug #103508
    
            build system:
             * Use GNUInstallDirs. Bug #103211
             * mingw: Install pkg-config files
             * mingw: change library names to include the soversion. Bug #103157
             * Fix installing a .cc file as header
             * Use -pthread flag instead of -lpthread
    
    Release 0.60.1
            qt5:
             * ArthurOutputDev: Add missing 'return' in error paths
    
            build system:
             * FindLIBOPENJPEG.cmake: Add CheckCXXSourceCompiles
    
    Release 0.60.0
            core:
             * Enable libcurl support by default
             * PSOutputDev: Fix wrong text generation. Bug #102760
             * Added methods to get and set the font size of text fields. Bug #101692
             * CairoOutputDev: Do not extend the pattern in drawImageMaskRegular
             * CairoOutputDev: do not use the custom downscaling for rendering images when using cairo >= 1.14
             * Fix build with old clang
             * Fix various crashes in broken files
             * Fix some warnings
             * Add some constness to the basic classes
             * Remove unused functions from GlobalParams
    
            qt5:
             * Added methods to get and set the font size of text fields. Bug #101692
             * Add whether renderToImage shows annotations
             * ArthurOutputDev: Replace Splash font rendering by Qt font rendering
             * ArthurOutputDev: Implement the drawSoftMaskedImage method
             * ArthurOutputDev: Fix several small bugs related to dash pattern handling
             * Fix two minor typos
    
            build system:
             * cmake is now the default build system
             * autotools based build system has been removed
    
             utils:
             * pdfinfo: don't truncate dest name

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.56 2017/09/04 19:57:18 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.57 2017/12/08 07:48:11 spz Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.59.0
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.61.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 .include "../../graphics/lcms2/buildlink3.mk"
