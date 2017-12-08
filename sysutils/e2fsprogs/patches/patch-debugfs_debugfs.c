9f9a4b07758c1785bfd4d697c3d6721a170248da
adam <adam@pkgsrc.org>
2017-12-08 08:12:58 +0000

    e2fsprogs: updated to 1.43.7
    PkgSrc:
    * Depend on devel/libuuid and devel/libblkid
    * Do not install uuidgen; now it is provided by devel/libuuid
    * Fix PLIST/fsck on Linux.
    
    
    E2fsprogs 1.43.7:
    
    Fix the error handling code in debugfs, fuse2fs and tune2fs so errors
    trying to replay the journal won't accidentally result in a seg fault
    if reopening the file system after the journal replay fails for some
    reason.
    
    Fix resize2fs to avoid it from either failing or corrupting the file
    system image in certain corner cases when doing an off-line resize.
    
    Fix many spelling mistakes in various man pages, code comments, and
    program messages.
    
    Fix e2fsck and debugfs so that maliciously corrupted file systems will
    not cause buffer overflows.
    
    Update German translation.
    
    Fixed various Debian Packaging Issues.

--- sysutils/e2fsprogs/patches/patch-debugfs_debugfs.c
+++ sysutils/e2fsprogs/patches/patch-debugfs_debugfs.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-debugfs_debugfs.c,v 1.1 2017/09/21 09:48:56 jperkin Exp $
+$NetBSD: patch-debugfs_debugfs.c,v 1.2 2017/12/08 08:12:58 adam Exp $
 
 Fix build on SunOS.
 
---- debugfs/debugfs.c.orig	2016-09-02 04:17:32.000000000 +0000
+--- debugfs/debugfs.c.orig	2017-10-16 05:45:45.000000000 +0000
 +++ debugfs/debugfs.c
-@@ -26,6 +26,9 @@ extern char *optarg;
- #include <errno.h>
+@@ -29,6 +29,9 @@ extern char *optarg;
+ #ifdef HAVE_SYS_SYSMACROS_H
+ #include <sys/sysmacros.h>
  #endif
- #include <fcntl.h>
 +#ifdef __sun
 +#include <sys/mkdev.h>
 +#endif
--- sysutils/e2fsprogs/patches/patch-misc_create__inode.c
+++ sysutils/e2fsprogs/patches/patch-misc_create__inode.c
@@ -1,16 +1,16 @@
-$NetBSD: patch-misc_create__inode.c,v 1.1 2017/09/21 09:48:56 jperkin Exp $
+$NetBSD: patch-misc_create__inode.c,v 1.2 2017/12/08 08:12:58 adam Exp $
 
 Fix build on SunOS.
 
---- misc/create_inode.c.orig	2017-01-31 06:08:02.000000000 +0000
+--- misc/create_inode.c.orig	2017-10-16 05:45:45.000000000 +0000
 +++ misc/create_inode.c
-@@ -22,6 +22,9 @@
- #include <attr/xattr.h>
+@@ -25,6 +25,9 @@
+ #ifdef HAVE_SYS_SYSMACROS_H
+ #include <sys/sysmacros.h>
  #endif
- #include <sys/ioctl.h>
 +#ifdef __sun
 +#include <sys/mkdev.h>
 +#endif
+ 
  #include <ext2fs/ext2fs.h>
  #include <ext2fs/ext2_types.h>
- #include <ext2fs/fiemap.h>
