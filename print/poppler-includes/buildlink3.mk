3cfc205ecfe2ef8e6bd5687694f5d94918529be8
maya <maya@pkgsrc.org>
2017-12-08 08:52:56 +0000

    bump for print/poppler major bump
    note: script detailed in `man revbump` insisted on bumping pkgrevision of
    print/poppler, it's probably wrong, but not committing this change is wronger.

--- print/poppler-includes/buildlink3.mk
+++ print/poppler-includes/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.27 2017/09/04 19:57:18 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2017/12/08 08:52:56 maya Exp $
 
 BUILDLINK_TREE+=	poppler-includes
 
@@ -8,7 +8,7 @@ POPPLER_INCLUDES_BUILDLINK3_MK:=
 BUILDLINK_DEPMETHOD.poppler-includes?=	build
 
 BUILDLINK_API_DEPENDS.poppler-includes+=	poppler-includes>=0.22.2
-BUILDLINK_ABI_DEPENDS.poppler-includes?=	poppler-includes>=0.59.0
+BUILDLINK_ABI_DEPENDS.poppler-includes?=	poppler-includes>=0.61.1nb1
 BUILDLINK_PKGSRCDIR.poppler-includes?=	../../print/poppler-includes
 .endif # POPPLER_INCLUDES_BUILDLINK3_MK
 
