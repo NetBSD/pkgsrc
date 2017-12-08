d0b367cb0f94b0cdaefe74ada41a3926d9e5ef8e
adam <adam@pkgsrc.org>
2017-12-08 08:03:35 +0000

    libuuid, libblkid, mcookie: updated to 2.31
    util-linux v2.31:
    blkid: improve man page wording
    blkid: more info about ambivalent results to the man page
    blkid: cleanup man page info about lsblk
    
    pkgsrc:
    unified libuuid, libblkid, and mcookie
    libuuid: also installs uuidgen

--- devel/libuuid/patches/patch-lib_randutils.c
+++ devel/libuuid/patches/patch-lib_randutils.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-lib_randutils.c,v 1.4 2015/11/27 14:57:36 jperkin Exp $
+$NetBSD: patch-lib_randutils.c,v 1.5 2017/12/08 08:03:35 adam Exp $
 
 O_CLOEXEC is not available on every platform (e.g. MacOS X < 10.7). It
 was introduced in POSIX 2008.
 
---- lib/randutils.c.orig	2015-10-05 14:18:43.000000000 +0000
+--- lib/randutils.c.orig	2017-10-02 09:30:08.000000000 +0000
 +++ lib/randutils.c
-@@ -30,6 +30,10 @@
- THREAD_LOCAL unsigned short ul_jrand_seed[3];
- #endif
+@@ -72,6 +72,10 @@ static void crank_random(void)
+ 		rand();
+ }
  
 +#if !defined(O_CLOEXEC)
 +#define O_CLOEXEC 0
