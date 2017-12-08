03b468e0081cd9fde8ac9d84aa7ff294ebb6663b
maya <maya@pkgsrc.org>
2017-12-08 08:52:56 +0000

    bump for print/poppler major bump
    note: script detailed in `man revbump` insisted on bumping pkgrevision of
    print/poppler, it's probably wrong, but not committing this change is wronger.

--- print/poppler-qt4/buildlink3.mk
+++ print/poppler-qt4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.38 2017/09/04 19:57:18 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.39 2017/12/08 08:52:56 maya Exp $
 
 BUILDLINK_TREE+=	poppler-qt4
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-qt4
 POPPLER_QT4_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-qt4+=	poppler-qt4>=0.6.1
-BUILDLINK_ABI_DEPENDS.poppler-qt4+=	poppler-qt4>=0.59.0
+BUILDLINK_ABI_DEPENDS.poppler-qt4+=	poppler-qt4>=0.61.1nb1
 BUILDLINK_PKGSRCDIR.poppler-qt4?=	../../print/poppler-qt4
 
 .include "../../print/poppler/buildlink3.mk"
