6d36d27e02f988a095f7d27ed7edd52136fa68a3
taca <taca@pkgsrc.org>
2017-12-09 02:28:19 +0000

    devel/php-raphf2: fix BUILDLINK_PKGSRCDIR
    Fix BUILDLINK_PKGSRCDI to here, devel/php-raphf2 not devel/php-raphf.

--- devel/php-raphf2/buildlink3.mk
+++ devel/php-raphf2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/03/12 09:11:01 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/12/09 02:28:19 taca Exp $
 
 .include "../../lang/php/phpversion.mk"
 
@@ -8,7 +8,7 @@ BUILDLINK_TREE+=	${PHP_PKG_PREFIX}-raphf
 ${PHP_PKG_PREFIX}_RAPHF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.${PHP_PKG_PREFIX}-raphf+=	${PHP_PKG_PREFIX}-raphf>=2.0.0
-BUILDLINK_PKGSRCDIR.${PHP_PKG_PREFIX}-raphf?=	../../devel/php-raphf
+BUILDLINK_PKGSRCDIR.${PHP_PKG_PREFIX}-raphf?=	../../devel/php-raphf2
 .endif	# ${PHP_PKG_PREFIX}_RAPHF_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-${PHP_PKG_PREFIX}-raphf
