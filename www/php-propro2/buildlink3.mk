5827e2bd9242472d69057820080e1f963d3c3b7e
taca <taca@pkgsrc.org>
2017-12-09 02:40:22 +0000

    www/php-propro2: fix dependency in buildlink3.mk
    Fix BUILDLINK_PKGSRCDI to here, www/php-propro2 not www/php-propro.

--- www/php-propro2/buildlink3.mk
+++ www/php-propro2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/03/12 09:12:52 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2017/12/09 02:40:22 taca Exp $
 
 .include "../../lang/php/phpversion.mk"
 
@@ -8,7 +8,7 @@ BUILDLINK_TREE+=	${PHP_PKG_PREFIX}-propro
 ${PHP_PKG_PREFIX}_PROPRO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.${PHP_PKG_PREFIX}-propro+=	${PHP_PKG_PREFIX}-propro>=2.0.0
-BUILDLINK_PKGSRCDIR.${PHP_PKG_PREFIX}-propro?=		../../www/php-propro
+BUILDLINK_PKGSRCDIR.${PHP_PKG_PREFIX}-propro?=		../../www/php-propro2
 .endif	# ${PHP_PKG_PREFIX}_PROPRO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-${PHP_PKG_PREFIX}-propro
