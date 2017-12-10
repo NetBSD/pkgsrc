1293c2c4bc867603dcc8a1e1d6a8718d42988236
adam <adam@pkgsrc.org>
2017-12-10 08:53:16 +0000

    py-markdown: updated to 2.6.10
    2.6.10:
    This version was released to force PyPI to point to the new docs location.
    Closes 601. The old PyPI hosted docs can be deleted after this.

--- textproc/py-markdown/patches/patch-setup.py
+++ textproc/py-markdown/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.2 2017/08/19 14:25:15 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2017/12/10 08:53:16 adam Exp $
 
 Avoid conflicts with other python versions of this package.
 
---- setup.py.orig	2017-08-18 00:07:03.000000000 +0000
+--- setup.py.orig	2017-12-07 18:12:06.000000000 +0000
 +++ setup.py
-@@ -246,7 +246,7 @@ setup(
+@@ -98,7 +98,7 @@ setup(
      maintainer_email='waylan.limberg@icloud.com',
      license='BSD License',
      packages=['markdown', 'markdown.extensions'],
 -    scripts=['bin/%s' % SCRIPT_NAME],
 +    scripts=['bin/%s' % SCRIPT_NAME+sys.version[0:3]],
      cmdclass={
-         'install_scripts': md_install_scripts,
-         'build_docs': build_docs,
+         'install_scripts': md_install_scripts
+     },
