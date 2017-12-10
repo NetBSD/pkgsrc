833834ba5595d56ca81907ca84eef08ae6882b2d
bsiegert <bsiegert@pkgsrc.org>
2017-12-10 16:02:59 +0000

    Fix poppler-glib build.
    The gtkdoc.py patch used a mixture of tabs and spaces, which was rejected
    by the Python interpreter. Also (with Python 3.6 as default), there was an
    uncaught TypeError, which I added to the existing except clause.

--- print/poppler/patches/patch-gtkdoc.py
+++ print/poppler/patches/patch-gtkdoc.py
@@ -1,14 +1,29 @@
-$NetBSD: patch-gtkdoc.py,v 1.1 2017/12/08 07:48:11 spz Exp $
+$NetBSD: patch-gtkdoc.py,v 1.2 2017/12/10 16:02:59 bsiegert Exp $
 
 --- gtkdoc.py.orig	2017-11-12 18:14:32.000000000 +0000
 +++ gtkdoc.py
+@@ -196,12 +196,12 @@ class GTKDoc(object):
+             if stdout:
+                 try:
+                     sys.stdout.write(stdout.encode("utf-8"))
+-                except UnicodeDecodeError:
++                except (TypeError, UnicodeDecodeError):
+                     sys.stdout.write(stdout)
+             if stderr:
+                 try:
+                     sys.stderr.write(stderr.encode("utf-8"))
+-                except UnicodeDecodeError:
++                except (TypeError, UnicodeDecodeError):
+                     sys.stderr.write(stderr)
+ 
+         if process.returncode != 0:
 @@ -223,6 +223,9 @@ class GTKDoc(object):
              return
  
          def copy_file_replacing_existing(src, dest):
 +            if src == dest:
-+		self.logger.debug('copy_file_replacing_existing not copying to itself: %s', src)
-+		return
++               self.logger.debug('copy_file_replacing_existing not copying to itself: %s', src)
++               return
              if os.path.isdir(src):
                  self.logger.debug('skipped directory %s',  src)
                  return
