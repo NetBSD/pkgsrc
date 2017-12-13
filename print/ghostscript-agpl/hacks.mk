2d75d9b35ccdd2251a31a8d0c87c048642712219
gdt <gdt@pkgsrc.org>
2017-12-13 13:02:06 +0000

    ghostscript-agpl: Adopt freetype2 hack from fontfonge
    This package also needs the missing freetype2 header file on NetBSD 7,
    and the same faked-up version seems to work fine.

--- print/ghostscript-agpl/hacks.mk
+++ print/ghostscript-agpl/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2013/03/16 22:25:21 dholland Exp $
+# $NetBSD: hacks.mk,v 1.2 2017/12/13 13:02:06 gdt Exp $
 
 ### [Tue Jan  9 15:57:05 EST 2007 : tv]
 ### Interix has u_int64_t, but not uint64_t
@@ -19,4 +19,24 @@ post-patch:
 	${SED} -e 's/^#Darwin#//' ${WRKSRC}/base/unix-dll.mak > \
 		${WRKSRC}/base/unix-darwin.mak
 	${MV} ${WRKSRC}/base/unix-darwin.mak ${WRKSRC}/base/unix-dll.mak
+.endif
+
+## Copied from ../../fonts/fontforge/hacks.mk.  See
+## https://gnats.netbsd.org/50902
+
+# On NetBSD 7.x for a while a version of freetype2 that was supposed
+# to have ftttdrv.h was shipped without actually installing that
+# header.
+
+.include "../../mk/bsd.prefs.mk"
+
+.if ${X11_TYPE} == "native" && !empty(MACHINE_PLATFORM:MNetBSD-7.*) && \
+	!exists(/usr/X11R7/include/freetype2/ftttdrv.h)
+BUILDLINK_TARGETS+=	buildlink-freetype2-ftttdrv.h
+buildlink-freetype2-ftttdrv.h:
+	${RUN}								\
+	dest=${BUILDLINK_DIR}"/include/freetype2/ftttdrv.h";		\
+	${ECHO_BUILDLINK_MSG} "Hacking around missing ftttdrv.h";	\
+	${ECHO} '#define TT_INTERPRETER_VERSION_35  35' >> "$$dest";	\
+	${ECHO} '#define TT_INTERPRETER_VERSION_38  38' >> "$$dest"
 .endif
