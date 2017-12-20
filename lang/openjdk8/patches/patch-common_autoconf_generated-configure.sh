0a48636e33ee3eeb80ea52044394c09254c28200
spz <spz@pkgsrc.org>
2017-12-20 16:54:59 +0000

    Pullup ticket #5654 - requested by bsiegert lang/openjdk8: security update
    Revisions pulled up:
    - lang/openjdk8/Makefile                                        1.55
    - lang/openjdk8/PLIST                                           1.8
    - lang/openjdk8/distinfo                                        1.51
    - lang/openjdk8/patches/patch-common_autoconf_generated-configure.sh 1.13
    - lang/openjdk8/patches/patch-jdk_make_CompileLaunchers.gmk     1.5
    - lang/openjdk8/patches/patch-jdk_make_lib_CoreLibraries.gmk    1.4
    - lang/openjdk8/patches/patch-jdk_src_solaris_native_java_net_NetworkInterface.c 1.4
    
    -------------------------------------------------------------------
       Module Name:    pkgsrc
       Committed By:   ryoon
       Date:           Tue Nov 28 05:07:25 UTC 2017
    
       Modified Files:
               pkgsrc/lang/openjdk8: Makefile PLIST distinfo
               pkgsrc/lang/openjdk8/patches:
                   patch-common_autoconf_generated-configure.sh
                   patch-jdk_make_CompileLaunchers.gmk
                   patch-jdk_make_lib_CoreLibraries.gmk
                   patch-jdk_src_solaris_native_java_net_NetworkInterface.c
    
       Log Message:
       Update to 1.8.152
    
       Changelog:
       Security bug fixes:
       CVE-2017-10285
       CVE-2017-10388
       CVE-2017-10309
       CVE-2017-10274
       CVE-2017-10356
       CVE-2017-10293
       CVE-2017-10342
       CVE-2017-10350
       CVE-2017-10349
       CVE-2017-10348
       CVE-2017-10357
       CVE-2016-9841
       CVE-2016-10165
       CVE-2017-10355
       CVE-2017-10281
       CVE-2017-10347
       CVE-2017-10386
       CVE-2017-10380
       CVE-2017-10295
       CVE-2017-10341
       CVE-2017-10345
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.54 -r1.55 pkgsrc/lang/openjdk8/Makefile
       cvs rdiff -u -r1.7 -r1.8 pkgsrc/lang/openjdk8/PLIST
       cvs rdiff -u -r1.50 -r1.51 pkgsrc/lang/openjdk8/distinfo
       cvs rdiff -u -r1.12 -r1.13 \
           pkgsrc/lang/openjdk8/patches/patch-common_autoconf_generated-configure.sh
       cvs rdiff -u -r1.4 -r1.5 \
           pkgsrc/lang/openjdk8/patches/patch-jdk_make_CompileLaunchers.gmk
       cvs rdiff -u -r1.3 -r1.4 \
           pkgsrc/lang/openjdk8/patches/patch-jdk_make_lib_CoreLibraries.gmk \
           pkgsrc/lang/openjdk8/patches/patch-jdk_src_solaris_native_java_net_NetworkInterface.c

--- lang/openjdk8/patches/patch-common_autoconf_generated-configure.sh
+++ lang/openjdk8/patches/patch-common_autoconf_generated-configure.sh
@@ -1,10 +1,10 @@
-$NetBSD: patch-common_autoconf_generated-configure.sh,v 1.12 2017/05/10 13:59:57 ryoon Exp $
+$NetBSD: patch-common_autoconf_generated-configure.sh,v 1.12.6.1 2017/12/20 16:54:59 spz Exp $
 
 BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
 
---- common/autoconf/generated-configure.sh.orig	2016-10-26 22:56:42.000000000 +0000
+--- common/autoconf/generated-configure.sh.orig	2017-11-28 00:13:34.000000000 +0000
 +++ common/autoconf/generated-configure.sh
-@@ -8454,9 +8454,9 @@ done
+@@ -8486,9 +8486,9 @@ done
    # We need to find a recent version of GNU make. Especially on Solaris, this can be tricky.
    if test "x$MAKE" != x; then
      # User has supplied a make, test it.
@@ -17,7 +17,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
  
    MAKE_CANDIDATE=""$MAKE""
    DESCRIPTION="user supplied MAKE=$MAKE"
-@@ -11755,7 +11755,7 @@ $as_echo "$as_me: Potential Boot JDK fou
+@@ -11787,7 +11787,7 @@ $as_echo "$as_me: Potential Boot JDK fou
              BOOT_JDK_FOUND=no
            else
              # Oh, this is looking good! We probably have found a proper JDK. Is it the correct version?
@@ -26,7 +26,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
  
              # Extra M4 quote needed to protect [] in grep expression.
              FOUND_VERSION_78=`echo $BOOT_JDK_VERSION | grep  '\"1\.[78]\.'`
-@@ -12539,7 +12539,7 @@ fi
+@@ -12571,7 +12571,7 @@ fi
        # Resolve file symlinks
        while test $COUNTER -lt 20; do
          ISLINK=`$LS -l $sym_link_dir/$sym_link_file | $GREP '\->' | $SED -e 's/.*-> \(.*\)/\1/'`
@@ -35,7 +35,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
            # This is not a symbolic link! We are done!
            break
          fi
-@@ -16225,16 +16225,15 @@ $as_echo_n "checking flags for boot jdk 
+@@ -16257,16 +16257,15 @@ $as_echo_n "checking flags for boot jdk 
    # Maximum amount of heap memory.
    # Maximum stack size.
    if test "x$BOOT_JDK_BITS" = x32; then
@@ -53,7 +53,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
  
    $ECHO "Check if jvm arg is ok: -Xmx$JVM_MAX_HEAP" >&5
    $ECHO "Command: $JAVA -Xmx$JVM_MAX_HEAP -version" >&5
-@@ -19454,7 +19453,7 @@ $as_echo "(none, will use system headers
+@@ -19486,7 +19485,7 @@ $as_echo "(none, will use system headers
    elif test "x$OPENJDK_TARGET_OS" = "xwindows"; then
      COMPILER_CHECK_LIST="cl"
    elif test "x$OPENJDK_TARGET_OS" = "xsolaris"; then
@@ -62,7 +62,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
    elif test "x$OPENJDK_TARGET_OS" = "xaix"; then
      # Do not probe for cc on AIX.
      COMPILER_CHECK_LIST="xlc_r"
-@@ -19910,7 +19909,7 @@ $as_echo_n "checking resolved symbolic l
+@@ -19942,7 +19941,7 @@ $as_echo_n "checking resolved symbolic l
        # Resolve file symlinks
        while test $COUNTER -lt 20; do
          ISLINK=`$LS -l $sym_link_dir/$sym_link_file | $GREP '\->' | $SED -e 's/.*-> \(.*\)/\1/'`
@@ -71,7 +71,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
            # This is not a symbolic link! We are done!
            break
          fi
-@@ -20348,7 +20347,7 @@ $as_echo_n "checking for resolved symbol
+@@ -20380,7 +20379,7 @@ $as_echo_n "checking for resolved symbol
        # Resolve file symlinks
        while test $COUNTER -lt 20; do
          ISLINK=`$LS -l $sym_link_dir/$sym_link_file | $GREP '\->' | $SED -e 's/.*-> \(.*\)/\1/'`
@@ -80,7 +80,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
            # This is not a symbolic link! We are done!
            break
          fi
-@@ -20376,7 +20375,7 @@ $as_echo "no, keeping CC" >&6; }
+@@ -20408,7 +20407,7 @@ $as_echo "no, keeping CC" >&6; }
    COMPILER=$CC
    COMPILER_NAME=$COMPILER_NAME
  
@@ -89,7 +89,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
      # Make sure we use the Sun Studio compiler and not gcc on Solaris, which won't work
      COMPILER_VERSION_TEST=`$COMPILER -V 2>&1 | $HEAD -n 1`
      $ECHO $COMPILER_VERSION_TEST | $GREP "^.*: Sun $COMPILER_NAME" > /dev/null
-@@ -21511,7 +21510,7 @@ $as_echo_n "checking resolved symbolic l
+@@ -21544,7 +21543,7 @@ $as_echo_n "checking resolved symbolic l
        # Resolve file symlinks
        while test $COUNTER -lt 20; do
          ISLINK=`$LS -l $sym_link_dir/$sym_link_file | $GREP '\->' | $SED -e 's/.*-> \(.*\)/\1/'`
@@ -98,7 +98,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
            # This is not a symbolic link! We are done!
            break
          fi
-@@ -21949,7 +21948,7 @@ $as_echo_n "checking for resolved symbol
+@@ -21982,7 +21981,7 @@ $as_echo_n "checking for resolved symbol
        # Resolve file symlinks
        while test $COUNTER -lt 20; do
          ISLINK=`$LS -l $sym_link_dir/$sym_link_file | $GREP '\->' | $SED -e 's/.*-> \(.*\)/\1/'`
@@ -107,7 +107,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
            # This is not a symbolic link! We are done!
            break
          fi
-@@ -21977,7 +21976,7 @@ $as_echo "no, keeping CXX" >&6; }
+@@ -22010,7 +22009,7 @@ $as_echo "no, keeping CXX" >&6; }
    COMPILER=$CXX
    COMPILER_NAME=$COMPILER_NAME
  
@@ -116,7 +116,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
      # Make sure we use the Sun Studio compiler and not gcc on Solaris, which won't work
      COMPILER_VERSION_TEST=`$COMPILER -V 2>&1 | $HEAD -n 1`
      $ECHO $COMPILER_VERSION_TEST | $GREP "^.*: Sun $COMPILER_NAME" > /dev/null
-@@ -29390,6 +29389,15 @@ $as_echo "$ac_cv_c_bigendian" >&6; }
+@@ -29451,6 +29450,15 @@ $as_echo "$ac_cv_c_bigendian" >&6; }
      if test "x$OPENJDK_TARGET_OS" = xbsd || test "x$OPENJDK_TARGET_OS" = xmacosx; then
        SET_EXECUTABLE_ORIGIN="$SET_SHARED_LIBRARY_ORIGIN"
      fi
@@ -132,7 +132,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
    else
      if test "x$OPENJDK_TARGET_OS" = xsolaris; then
        # If it is not gcc, then assume it is the Oracle Solaris Studio Compiler
-@@ -29531,6 +29539,13 @@ rm -f core conftest.err conftest.$ac_obj
+@@ -29592,6 +29600,13 @@ rm -f core conftest.err conftest.$ac_obj
                C_O_FLAG_NORM="-Os"
                C_O_FLAG_NONE=""
                ;;
@@ -146,16 +146,16 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
              *)
                C_O_FLAG_HI="-O3"
                C_O_FLAG_NORM="-O2"
-@@ -29732,7 +29747,7 @@ fi
+@@ -29796,7 +29811,7 @@ fi
    #
    case $COMPILER_NAME in
      gcc )
--      CCXXFLAGS_JDK="$CCXXFLAGS $CCXXFLAGS_JDK -W -Wall -Wno-unused -Wno-parentheses -Wno-sign-compare \
-+      CCXXFLAGS_JDK="$CCXXFLAGS $CCXXFLAGS_JDK -W -Wall -Wno-unused -Wno-parentheses -Wno-sign-compare -Wno-unused-parameter \
+-      COMMON_CCXXFLAGS_JDK="$COMMON_CCXXFLAGS $COMMON_CCXXFLAGS_JDK -W -Wall -Wno-unused -Wno-parentheses -Wno-sign-compare \
++      COMMON_CCXXFLAGS_JDK="$COMMON_CCXXFLAGS $COMMON_CCXXFLAGS_JDK -W -Wall -Wno-unused -Wno-parentheses -Wno-sign-compare -Wno-unused-parameter \
        -pipe \
        -D_GNU_SOURCE -D_REENTRANT -D_LARGEFILE64_SOURCE"
-       case $OPENJDK_TARGET_CPU_ARCH in
-@@ -30343,7 +30358,8 @@ $as_echo "alsa pulse x11" >&6; }
+       CXXSTD_CXXFLAG="-std=gnu++98"
+@@ -30753,7 +30768,8 @@ $as_echo "alsa pulse x11" >&6; }
    if test "x$OPENJDK_TARGET_OS" = xbsd; then
      { $as_echo "$as_me:${as_lineno-$LINENO}: checking what is not needed on BSD?" >&5
  $as_echo_n "checking what is not needed on BSD?... " >&6; }
@@ -165,7 +165,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
        ALSA_NOT_NEEDED=yes
        PULSE_NOT_NEEDED=yes
        { $as_echo "$as_me:${as_lineno-$LINENO}: result: alsa pulse" >&5
-@@ -31555,7 +31571,11 @@ $as_echo "$as_me: WARNING: freetype not 
+@@ -31965,7 +31981,11 @@ $as_echo "$as_me: WARNING: freetype not 
  
        # Allow --with-freetype-lib and --with-freetype-include to override
        if test "x$with_freetype_include" != x; then
@@ -178,7 +178,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
        fi
        if test "x$with_freetype_lib" != x; then
          POTENTIAL_FREETYPE_LIB_PATH="$with_freetype_lib"
-@@ -34373,7 +34393,7 @@ $as_echo "$as_me: The path of FREETYPE_I
+@@ -34783,7 +34803,7 @@ $as_echo "$as_me: The path of FREETYPE_I
      FREETYPE_INCLUDE_PATH="`cd "$path"; $THEPWDCMD -L`"
    fi
  
@@ -187,7 +187,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
          FREETYPE_CFLAGS="-I$FREETYPE_INCLUDE_PATH/freetype2 -I$FREETYPE_INCLUDE_PATH"
        else
          FREETYPE_CFLAGS="-I$FREETYPE_INCLUDE_PATH"
-@@ -34506,7 +34526,7 @@ $as_echo "$as_me: The path of FREETYPE_L
+@@ -34916,7 +34936,7 @@ $as_echo "$as_me: The path of FREETYPE_L
        if test "x$OPENJDK_TARGET_OS" = xwindows; then
          FREETYPE_LIBS="$FREETYPE_LIB_PATH/freetype.lib"
        else
@@ -196,7 +196,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
        fi
      fi
  
-@@ -35732,9 +35752,6 @@ fi
+@@ -36142,9 +36162,6 @@ fi
  
  
  
@@ -206,7 +206,7 @@ BOOT_JDK_VERSION part: pkg/51221 (Build error with OpenJDK8 and i386)
  
      llvm_components="jit mcjit engine nativecodegen native"
      unset LLVM_CFLAGS
-@@ -35777,7 +35794,7 @@ fi
+@@ -36187,7 +36204,7 @@ fi
    fi
  
    # libCrun is the c++ runtime-library with SunStudio (roughly the equivalent of gcc's libstdc++.so)
