ac7b996a7223de3337b8fb9539a90712ab4a3856
adam <adam@pkgsrc.org>
2017-12-21 09:44:14 +0000

    libassuan2: updated to 2.5.1
    Noteworthy changes in version 2.5.1:
     * Fix c+p error in the previous usleep fix.
    
    Noteworthy changes in version 2.5.0:
     * New function to change the system hooks for the socket
       interface.
     * Fix the use of the internal usleep in the nPth implementation.
     * Interface changes relative to the 2.4.0 release:
     assuan_sock_set_system_hooks   NEW.

--- security/libassuan2/buildlink3.mk
+++ security/libassuan2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2015/12/14 14:07:42 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2017/12/21 09:44:14 adam Exp $
 
 BUILDLINK_TREE+=	libassuan
 
@@ -7,7 +7,7 @@ LIBASSUAN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libassuan+=	libassuan>=2.4.2
 BUILDLINK_ABI_DEPENDS.libassuan+=	libassuan>=2.4.2
-BUILDLINK_PKGSRCDIR.libassuan?=	../../security/libassuan2
+BUILDLINK_PKGSRCDIR.libassuan?=		../../security/libassuan2
 
 .include "../../security/libgpg-error/buildlink3.mk"
 .endif # LIBASSUAN_BUILDLINK3_MK
