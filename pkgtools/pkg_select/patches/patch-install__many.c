6c5789b15b3eadcff0072032883d5c712ecf8861
plunky <plunky@pkgsrc.org>
2017-12-29 11:59:13 +0000

    fix crashes due to SSP being enabled

--- pkgtools/pkg_select/patches/patch-install__many.c
+++ pkgtools/pkg_select/patches/patch-install__many.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-install__many.c,v 1.1 2013/11/29 23:13:56 dholland Exp $
+$NetBSD: patch-install__many.c,v 1.2 2017/12/29 11:59:13 plunky Exp $
 
 Fix const correctness.
 
---- install_many.c~	2009-03-08 14:25:53.000000000 +0000
+--- install_many.c.orig	2009-03-08 14:25:53.000000000 +0000
 +++ install_many.c
 @@ -199,7 +199,7 @@ process_many(int action)
  		if ((list = build_list_from_slist()) == NULL)
