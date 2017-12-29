d41a3c0d72d1527dce3ad0edd6a714672f359772
gdt <gdt@pkgsrc.org>
2017-12-29 23:06:14 +0000

    glibmm: Add kludge to use gcc 4.9 for depending packages
    glibmm seems to need gcc 4.9 to build; therefore set GCC_REQD.
    
    This means that depending packages using other versions (specifically
    4.8 on NetBSD 7) will fail to link.  As a kludge to make more things
    build for the upcoming branch, force GCC_REQD to 4.9 in glibmm's
    buildlink3.mk.  Now, at least some packages that depend on glibmm
    build.  While icky, it's my judgement that having more packages
    building is better.
    
    This will very likely be reverted either after the branch or as part
    of the upcoming compiler selection logic changes.
    
    As proposed on tech-pkg.

--- devel/glibmm/buildlink3.mk
+++ devel/glibmm/buildlink3.mk
@@ -1,9 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.15 2012/09/15 10:04:10 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2017/12/29 23:06:14 gdt Exp $
 
 BUILDLINK_TREE+=	glibmm
 
 .if !defined(GLIBMM_BUILDLINK3_MK)
 GLIBMM_BUILDLINK3_MK:=
+
+# It is necessary to use the same compiler to link against libmm as it
+# was built with.  This is hacky, but enables at least
+# gnome-system-monitor to build.
+GCC_REQD+=		4.9
 
 BUILDLINK_API_DEPENDS.glibmm+=	glibmm>=2.24.2
 BUILDLINK_ABI_DEPENDS.glibmm+=	glibmm>=2.32.1nb1
