680e212d0896b91d049a25882f8cdd98c2b195ec
adam <adam@pkgsrc.org>
2017-12-30 10:39:24 +0000

    llvm: updated to 5.0.1
    LLVM 5.0.1:
    This release contains bug-fixes for the LLVM 5.0.0 release. This
    release is API and ABI compatible with 5.0.0.

--- devel/lldb/buildlink3.mk
+++ devel/lldb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 19:47:37 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2017/12/30 10:39:24 adam Exp $
 
 BUILDLINK_TREE+=	lldb
 
@@ -8,11 +8,10 @@ LLDB_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.lldb+=	lldb>=4.0.1
 BUILDLINK_PKGSRCDIR.lldb?=	../../devel/lldb
 
-.include "../../textproc/libxml2/buildlink3.mk"
-.include "../../lang/llvm/buildlink3.mk"
-.include "../../lang/clang/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
-.include "../../devel/swig/buildlink3.mk"
+.include "../../lang/clang/buildlink3.mk"
+.include "../../lang/llvm/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
 .endif	# LLDB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-lldb
