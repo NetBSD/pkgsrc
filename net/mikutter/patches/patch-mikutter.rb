0e19eebb4c14fcb94b7ed976c7c065ae991946ba
tsutsui <tsutsui@pkgsrc.org>
2017-12-30 05:05:14 +0000

    mikutter: update to 3.6.0.
    Upstream change:
    
    mikutter 3.6.0
    
    * World
     * Twitter functions are now implemented as a plugin
     * Service is no longer available, so the World plugin provides an equivalent
       of Service
    * Spell
    * Form DSL
     * generalized modules for settings etc. so make it possible to use it
       in Dialog etc.
     * photo select
      * select images
     * label
     * link (Model viewer)
    * Dialog DSL/dialog method
     * universal interface for plugins to show a dialog box
     * no dependencies on Gtk
    * Photo Model
     * preserve image URLs for multiple sizes and use a proper larger size
       one for requested image
    * new setting in config for the maxinum number of tweets in timelines
    * settings
     * tree structure in side menu by "settings" block
     * improve drawing speed for settings with many items
    * appearance settings for PostBox and World Shifter
    * requires Ruby 2.3 and later
    * gems for mikutter project
     * use Diva gem
      * new gem derived from mikutter
      * replacement of Retriever, mikutter still provides Retriever compatible
        interfaces
     * Delayer Deferred 2
      * async/await

--- net/mikutter/patches/patch-mikutter.rb
+++ net/mikutter/patches/patch-mikutter.rb
@@ -1,9 +1,7 @@
-$NetBSD: patch-mikutter.rb,v 1.4 2017/06/10 09:45:33 tsutsui Exp $
+$NetBSD: patch-mikutter.rb,v 1.5 2017/12/30 05:05:14 tsutsui Exp $
 
 - pkgsrc can handle location of ruby binary
   https://dev.mikutter.hachune.net/issues/889
-- pull fix to use delayer-deferred 2.x for upstream ticket #995
-  https://dev.mikutter.hachune.net/issues/995
 
 --- mikutter.rb.orig	2017-06-10 01:11:51.000000000 +0000
 +++ mikutter.rb
@@ -16,32 +14,3 @@ $NetBSD: patch-mikutter.rb,v 1.4 2017/06/10 09:45:33 tsutsui Exp $
  =begin rdoc
  = mikutter - the moest twitter client
  Copyright (C) 2009-2017 Toshiaki Asai
-@@ -68,12 +66,12 @@ def boot!(profile)
-   rescue => exception
-     into_debug_mode(exception)
-     notice "catch exception `#{exception.class}'"
--    raise exception
-+    raise
-   rescue Exception => exception
-     notice "catch exception `#{exception.class}'"
-     exception = Mainloop.exception_filter(exception)
-     notice "=> `#{exception.class}'"
--    raise exception end
-+    raise end
-   exception = Mainloop.exception_filter(nil)
-   if exception
-     notice "raise mainloop exception `#{exception.class}'"
-@@ -82,6 +80,13 @@ def boot!(profile)
- 
- def error_handling!(exception)
-   notice "catch #{exception.class}"
-+  if Mopt.debug && exception.respond_to?(:deferred) && exception.deferred
-+    if command_exist?('dot')
-+      notice "[[#{exception.deferred.graph_draw}]]"
-+    else
-+      notice exception.deferred.graph
-+    end
-+  end
-   File.open(File.expand_path(File.join(Environment::TMPDIR, 'crashed_exception')), 'w'){ |io| Marshal.dump(exception, io) }
-   raise exception end
- 
