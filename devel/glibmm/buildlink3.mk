ed6ddf7ed2b9b2457607b0fe707098ae4fb51142
gdt <gdt@pkgsrc.org>
2017-12-30 17:12:51 +0000

    glibmm: Drop GCC_REQD from buildlink3.mk
    As a hack for 2017Q4, I added GCC_REQD+=4.9 to glibmm's buildlink3.mk,
    to make some fraction of glibmm-depending packages build on the
    branch.  This commit reverts the bl3 addition (but leaves
    GCC_REQD+=4.9 for glibmm itself).
    
    The result will be that someone building pkgsrc with a compiler older
    than gcc 4.9 will be able to build glibmm (via forcing 4.9), and
    depending packages will fail.  This is much like the situation before
    the previous commit, except that glibmm will build.

--- devel/glibmm/buildlink3.mk
+++ devel/glibmm/buildlink3.mk
@@ -1,14 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.16 2017/12/29 23:06:14 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2017/12/30 17:12:51 gdt Exp $
 
 BUILDLINK_TREE+=	glibmm
 
 .if !defined(GLIBMM_BUILDLINK3_MK)
 GLIBMM_BUILDLINK3_MK:=
-
-# It is necessary to use the same compiler to link against libmm as it
-# was built with.  This is hacky, but enables at least
-# gnome-system-monitor to build.
-GCC_REQD+=		4.9
 
 BUILDLINK_API_DEPENDS.glibmm+=	glibmm>=2.24.2
 BUILDLINK_ABI_DEPENDS.glibmm+=	glibmm>=2.32.1nb1
