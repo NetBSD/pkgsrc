cb5273574e0c3b47f9e509a8e4caa1139875f9f9
rillig <rillig@pkgsrc.org>
2018-01-01 18:16:35 +0000

    Replaced $(ROUND) with ${CURLY} variable references.
    This has been a pkglint warning for several years now, and pkglint can even
    fix it automatically. And it did for this commit.
    
    Only in lang/mercury, two passes of autofixing were necessary because there
    were nested variables.

--- lang/openjdk7/builtin.mk
+++ lang/openjdk7/builtin.mk
@@ -1,3 +1,5 @@
+# $NetBSD: builtin.mk,v 1.2 2018/01/01 18:16:37 rillig Exp $
+
 BUILTIN_PKG:=	openjdk7
 
 BUILTIN_FIND_FILES_VAR:=	OJDK7
@@ -26,7 +28,7 @@ MAKEVARS+=	IS_BUILTIN.openjdk7
 .if !defined(BUILTIN_PKG.openjdk7) && \
     !empty(IS_BUILTIN.openjdk7:M[yY][eE][sS])
 BUILTIN_VERSION.openjdk7!=	${OJDK7} -version 2>&1 | ${SED} -Ee 's:^[^0-9]*([0-9._]+)$:\1:' -e 's/_/./g' -e 's/([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)/\1.\2.\4/'
-BUILTIN_PKG.openjdk7=	openjdk7-$(BUILTIN_VERSION.openjdk7)
+BUILTIN_PKG.openjdk7=	openjdk7-${BUILTIN_VERSION.openjdk7}
 .endif
 MAKEVARS+=	BUILTIN_PKG.openjdk7
 
--- print/tex-blindtext/options.mk
+++ print/tex-blindtext/options.mk
@@ -1,15 +1,15 @@
-# $NetBSD: options.mk,v 1.1 2017/01/20 21:43:58 markd Exp $
+# $NetBSD: options.mk,v 1.2 2018/01/01 18:16:38 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tex-blindtext
 BLINDTEXT_LANGUAGES=	catalan english french german latin
-PKG_SUPPORTED_OPTIONS+=	$(BLINDTEXT_LANGUAGES) polyglossia
+PKG_SUPPORTED_OPTIONS+=	${BLINDTEXT_LANGUAGES} polyglossia
 PKG_SUGGESTED_OPTIONS+=	english
 
 .include "../../mk/bsd.options.mk"
 
 .for l in $(BLINDTEXT_LANGUAGES)
 .if !empty(PKG_OPTIONS:M$(l))
-DEPENDS+=	tex-babel-$(l)-[0-9]*:../../print/tex-babel-$(l)
+DEPENDS+=	tex-babel-${l}-[0-9]*:../../print/tex-babel-${l}
 .endif
 .endfor
 
