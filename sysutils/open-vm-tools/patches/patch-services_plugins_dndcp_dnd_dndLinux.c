b62fef397b0c3f7bdf9502b76bdb021014a33213
ryoon <ryoon@pkgsrc.org>
2018-01-01 06:56:01 +0000

    Update to 10.2.0
    * Use x11/gtkmm3
    
    Changelog:
    Compatibility
      * open-vm-tools 10.2.0 is compatible with all supported versions
        of VMware vSphere ESXi 5.5 and later, VMware Workstation 14.0 and
        VMware Fusion 10.0. See VMware Compatibility Guide for more information.
    
      * Starting with VMware Tools version 10.2.0, Perl script based VMware
        Tools installation for FreeBSD has been discontinued. Going forward,
        FreeBSD systems are supported only through the open-vm-tools packages
        directly available from FreeBSD package repositories. FreeBSD packages
        for open-vm-tools 10.1.0 and later are available from FreeBSD package
        repositories.
    
    Resolved Issues
      * Summary page of the VM does not list the IP address of the VMs in
        the right order
    
      * Guest authentication fails with a SystemError fault when the requested
        password is expired
    
      * The free space reported in vim.vm.GuestInfo.DiskInfo for a Linux guest
        does not match with df command in the guest
    
      * VMware user process might not restart after upgrades of open-vm-tools

--- sysutils/open-vm-tools/patches/patch-services_plugins_dndcp_dnd_dndLinux.c
+++ sysutils/open-vm-tools/patches/patch-services_plugins_dndcp_dnd_dndLinux.c
@@ -1,22 +1,22 @@
-$NetBSD: patch-services_plugins_dndcp_dnd_dndLinux.c,v 1.1 2016/10/09 03:41:56 ryoon Exp $
+$NetBSD: patch-services_plugins_dndcp_dnd_dndLinux.c,v 1.2 2018/01/01 06:56:01 ryoon Exp $
 
---- services/plugins/dndcp/dnd/dndLinux.c.orig	2016-02-16 20:06:48.000000000 +0000
+--- services/plugins/dndcp/dnd/dndLinux.c.orig	2017-12-15 19:09:51.000000000 +0000
 +++ services/plugins/dndcp/dnd/dndLinux.c
-@@ -39,7 +39,7 @@
+@@ -40,7 +40,7 @@
  #include "util.h"
  #include "escape.h"
  #include "su.h"
--#if defined(linux) || defined(sun) || defined(__FreeBSD__)
-+#if defined(linux) || defined(sun) || defined(__FreeBSD__) || defined(__NetBSD__)
+-#if defined(__linux__) || defined(sun) || defined(__FreeBSD__)
++#if defined(__linux__) || defined(sun) || defined(__FreeBSD__) || defined(__NetBSD__)
  #include "vmblock_user.h"
  #include "mntinfo.h"
  #endif
-@@ -276,7 +276,7 @@ DnD_UriIsNonFileSchemes(const char *uri)
+@@ -277,7 +277,7 @@ DnD_UriIsNonFileSchemes(const char *uri)
  
  
  /* We need to make this suck less. */
--#if defined(linux) || defined(sun) || defined(__FreeBSD__)
-+#if defined(linux) || defined(sun) || defined(__FreeBSD__) || defined(__NetBSD__)
+-#if defined(__linux__) || defined(sun) || defined(__FreeBSD__)
++#if defined(__linux__) || defined(sun) || defined(__FreeBSD__) || defined(__NetBSD__)
  
  /*
   *----------------------------------------------------------------------------
