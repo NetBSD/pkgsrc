8073bbf190d2f8dda3585f3ec570d7ca59b39a9c
adam <adam@pkgsrc.org>
2018-01-01 20:36:09 +0000

    boost: updated to 1.66.0
    1.66.0:
    New Libraries
    Beast:
    Portable HTTP, WebSocket, and network operations using only C++11 and Boost.Asio, from Vinnie Falco.
    
    CallableTraits:
    A spiritual successor to Boost.FunctionTypes, Boost.CallableTraits is a header-only C++11 library for the compile-time inspection and manipulation of all 'callable' types. Additional support for C++17 features, from Barrett Adair.
    
    Mp11:
    A C++11 metaprogramming library, from Peter Dimov.

--- devel/boost-jam/buildlink3.mk
+++ devel/boost-jam/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.32 2017/08/24 19:31:32 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.33 2018/01/01 20:36:09 adam Exp $
 
 BUILDLINK_TREE+=	boost-jam
 
 .if !defined(BOOST_JAM_BUILDLINK3_MK)
 BOOST_JAM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.65.*
+BUILDLINK_API_DEPENDS.boost-jam+=	boost-jam-1.66.*
 BUILDLINK_DEPMETHOD.boost-jam?=		build
 BUILDLINK_PKGSRCDIR.boost-jam?=		../../devel/boost-jam
 .endif # BOOST_JAM_BUILDLINK3_MK
