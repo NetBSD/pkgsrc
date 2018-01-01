b49607def45d4d36bd5ceace15e20c83fbdd2f80
wiz <wiz@pkgsrc.org>
2018-01-01 10:23:04 +0000

    apache22: remove, it was eol'd in June 2017
    Remove packages that only work with apache22.
    Remove apache22 references.

--- www/ap2-auth-ntlm/patches/patch-ntlmssp.inc.c
+++
@@ -1,7 +0,0 @@
-$NetBSD: patch-ntlmssp.inc.c,v 1.2 2009/02/09 18:26:46 joerg Exp $
-
---- ntlmssp.inc.c.orig	2004-02-19 15:21:06.000000000 +0000
-+++ ntlmssp.inc.c	2008-10-17 19:33:51.000000000 +0100
-@@ -1,1 +1,2 @@
-+#include "httpd/httpd.h"
- /*
