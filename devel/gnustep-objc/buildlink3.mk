58442720c5a924f134dd4de122d9cdc5cd2d5538
spz <spz@pkgsrc.org>
2018-01-01 13:41:50 +0000

    Pullup ticket #5664 - requested by he devel/gnustep-objc: build fix
    Revisions pulled up:
    - devel/gnustep-objc/Makefile                                   1.31
    - devel/gnustep-objc/buildlink3.mk                              1.11
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	he
       Date:		Sun Dec 31 11:20:03 UTC 2017
    
       Modified Files:
       	pkgsrc/devel/gnustep-objc: Makefile buildlink3.mk
    
       Log Message:
       At least on NetBSD, gnustep-objc's libobjc.so ends up with various
       _Unwind_* symbols undefined, and apparently they can come from -lgcc_s.
       Being cautious, link in -lgcc_s on NetBSD only so far.
       Bump PKGREVISION.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.30 -r1.31 pkgsrc/devel/gnustep-objc/Makefile
       cvs rdiff -u -r1.10 -r1.11 pkgsrc/devel/gnustep-objc/buildlink3.mk

--- devel/gnustep-objc/buildlink3.mk
+++ devel/gnustep-objc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2012/05/07 01:53:22 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.10.48.1 2018/01/01 13:41:50 spz Exp $
 
 BUILDLINK_TREE+=	gnustep-objc
 
@@ -10,6 +10,10 @@ BUILDLINK_ABI_DEPENDS.gnustep-objc+=	gnustep-objc>=1.3.0nb1
 BUILDLINK_PKGSRCDIR.gnustep-objc?=	../../devel/gnustep-objc
 
 BUILDLINK_LDFLAGS.gnustep-objc+=	-lobjc -lpthread
+.if ${OPSYS} == "NetBSD"
+# Missing _Unwind_* symbols in gnustep objc.so, needs to come from somewhere...
+BUILDLINK_LDFLAGS.gnustep-objc+=	-lgcc_s
+.endif
 
 .include "../../devel/gnustep-make/buildlink3.mk"
 .endif # GNUSTEP_OBJC_BUILDLINK3_MK
