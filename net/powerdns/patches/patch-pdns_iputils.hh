11f83eb19eaf8ca913d227ba3404591d41821b09
fhajny <fhajny@pkgsrc.org>
2018-01-02 12:18:15 +0000

    Update net/powerdns* to 4.1.0.
    PowerDNS Authoritative Server 4.1.0
    ===========================================================
    
    - Improved performance: 400% speedup in some scenarios
    - Crypto API: DNSSEC fully configurable via RESTful API
    - Improved documentation
    - Database related improvements
    - Enhanced tooling
    - Support for TCP Fast Open
    - Support for non-local bind
    - Support for Botan 2.x (and removal of support for Botan 1.10)
    - Our packages now ship with PKCS #11 support.
    - Recursor passthrough removal
    
    Full changelog:
    
      https://doc.powerdns.com/authoritative/changelog/4.1.html
    
    
    
    PowerDNS Authoritative Server 4.0.5
    ===========================================================
    
    Fixes
    - Fix for missing check on API operations (CVE-2017-15091)
    - Bindbackend: do not corrupt data supplied by other backends in
      getAllDomains
    - API: prevent sending nameservers list and zone-level NS in rrsets
    - gpgsql: make statement names actually unique
    - Fix remotebackend params
    - Fix godbc query logging
    - For create-slave-zone, actually add all slaves, and not only first n
      times
    - Fix a regression in axfr-rectify + test
    - When making a netmask from a comboaddress, we neglected to zero the
      port
    - Fix libatomic detection on ppc64
    - Catch DNSName exception in the Zoneparser
    - Publish inactive KSK/CSK as CDNSKEY/CDS
    - Handle AFSDB record separately due to record structure.
    - Treat requestor's payload size lower than 512 as equal to 512
    - Correctly purge entries from the caches after a transfer
    - Handle a signing pipe worker dying with work still pending
    - Ignore SOA-EDIT for PRESIGNED zones.
    - Check return value for all getTSIGKey calls.
    
    Improvements
    - Fix ldap-strict autoptr feature, including a test
    - mydnsbackend: Add getAllDomains
    - Stubresolver: Use only recursor setting if given
    - LuaWrapper: Allow embedded NULs in strings received from Lua
    - sdig: Clarify that the ednssubnet option takes "subnet/mask"
    - Tests: Ensure all required tools are available
    - PowerDNS sdig does not truncate trailing bits of EDNS Client Subnet
      mask
    - LuaJIT 2.1: Lua fallback functionality no longer uses Lua namespace
    - Add support for Botan 2.x
    - Ship ldapbackend schema files in tarball
    - Collection of schema changes
    - Fix typo in two log messages
    - Add help text on autodetecting systemd support
    - Use a unique pointer for bind backend's d_of
    - Fix some of the issues found by @jpmens

--- net/powerdns/patches/patch-pdns_iputils.hh
+++ net/powerdns/patches/patch-pdns_iputils.hh
@@ -1,4 +1,4 @@
-$NetBSD: patch-pdns_iputils.hh,v 1.2 2017/03/29 11:46:03 fhajny Exp $
+$NetBSD: patch-pdns_iputils.hh,v 1.3 2018/01/02 12:18:15 fhajny Exp $
 
 - Do not use IP_PKTINFO on NetBSD, the structure is not as expected.
 - Backport fix for SunOS segfault problem, see:
@@ -16,35 +16,4 @@ $NetBSD: patch-pdns_iputils.hh,v 1.2 2017/03/29 11:46:03 fhajny Exp $
 +
  #ifdef __APPLE__
  #include <libkern/OSByteOrder.h>
- 
-@@ -59,23 +63,22 @@
- #define le64toh(x) OSSwapLittleToHostInt64(x)
- #endif
- 
--// for illumos
--#ifdef BE_64
-+#ifdef __sun
- 
- #define htobe16(x) BE_16(x)
- #define htole16(x) LE_16(x)
--#define be16toh(x) BE_IN16(x)
--#define le16toh(x) LE_IN16(x)
-+#define be16toh(x) BE_IN16(&(x))
-+#define le16toh(x) LE_IN16(&(x))
- 
- #define htobe32(x) BE_32(x)
- #define htole32(x) LE_32(x)
--#define be32toh(x) BE_IN32(x)
--#define le32toh(x) LE_IN32(x)
-+#define be32toh(x) BE_IN32(&(x))
-+#define le32toh(x) LE_IN32(&(x))
- 
- #define htobe64(x) BE_64(x)
- #define htole64(x) LE_64(x)
--#define be64toh(x) BE_IN64(x)
--#define le64toh(x) LE_IN64(x)
-+#define be64toh(x) BE_IN64(&(x))
-+#define le64toh(x) LE_IN64(&(x))
- 
- #endif
  
