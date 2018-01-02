800b3d2ca2530ca78dbd603c8f4ea9647971eec1
fhajny <fhajny@pkgsrc.org>
2018-01-02 15:52:44 +0000

    Update mail/dovecot2* to 2.3.0.
    Some of the larger changes:
    
    * Various setting changes, see https://wiki2.dovecot.org/Upgrading/2.3
    * Logging rewrite started: Logging is now based on hierarchical events.
      This makes it possible to do various things, like: 1) giving
      consistent log prefixes, 2) enabling debug logging with finer
      granularity, 3) provide logs in more machine readable formats
      (e.g. json). Everything isn't finished yet, especially a lot of the
      old logging code still needs to be translated to the new way.
    * Statistics rewrite started: Stats are now based on (log) events.
      It's possible to gather statistics about any event that is logged.
      See http://wiki2.dovecot.org/Statistics for details
    * ssl_dh setting replaces the old generated ssl-parameters.dat
    * IMAP: When BINARY FETCH finds a broken mails, send [PARSE] error
      instead of [UNKNOWNCTE]
    * Linux: core dumping via PR_SET_DUMPABLE is no longer enabled by
      default due to potential security reasons (found by cPanel Security
      Team).
    + Added support for SMTP submission proxy server, which includes
      support for BURL and CHUNKING extension.
    + LMTP rewrite. Supports now CHUNKING extension and mixing of
      local/proxy recipients.
    + auth: Support libsodium to add support for ARGON2I and ARGON2ID
      password schemes.
    + auth: Support BLF-CRYPT password scheme in all platforms
    + auth: Added LUA scripting support for passdb/userdb.
      See https://wiki2.dovecot.org/AuthDatabase/Lua
    - Input streams are more reliable now when there are errors or when
      the maximum buffer size is reached. Previously in some situations
      this could have caused Dovecot to try to read already freed memory.
    - Output streams weren't previously handling failures when writing a
      trailer at the end of the stream. This mainly affected encrypt and
      zlib compress ostreams, which could have silently written truncated
      files if the last write happened to fail (which shouldn't normally
      have ever happened).
    - virtual plugin: Fixed panic when fetching mails from virtual
      mailboxes with IMAP BINARY extension.
    - doveadm-server: Fix potential hangs with SSL connections
    - doveadm proxy: Reading commands' output from v2.2.33+ servers could
      have caused the output to be corrupted or caused a crash.
    - Many other smaller fixes

--- mail/dovecot2/patches/patch-src_plugins_quota_quota-fs.c
+++ mail/dovecot2/patches/patch-src_plugins_quota_quota-fs.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_plugins_quota_quota-fs.c,v 1.6 2016/04/08 07:55:16 adam Exp $
+$NetBSD: patch-src_plugins_quota_quota-fs.c,v 1.7 2018/01/02 15:52:44 fhajny Exp $
 
 Fix for missing declaration.
 
---- src/plugins/quota/quota-fs.c.orig	2016-04-08 07:46:25.000000000 +0000
+--- src/plugins/quota/quota-fs.c.orig	2017-12-22 13:53:41.000000000 +0000
 +++ src/plugins/quota/quota-fs.c
-@@ -680,7 +680,7 @@ fs_quota_get_netbsd(struct fs_quota_root
+@@ -700,7 +700,7 @@ fs_quota_get_netbsd(struct fs_quota_root
  	struct quotakey qk;
  	struct quotaval qv;
  	struct quotahandle *qh;
@@ -12,4 +12,4 @@ Fix for missing declaration.
 +	int i, ret;
  
  	if ((qh = quota_open(root->mount->mount_path)) == NULL) {
- 		i_error("cannot open quota for %s: %m",
+ 		*error_r = t_strdup_printf("cannot open quota for %s: %m",
