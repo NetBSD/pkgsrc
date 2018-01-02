3e1abb5da959353cffca22dd63a2a9a03d16c9a5
wiz <wiz@pkgsrc.org>
2018-01-02 10:37:29 +0000

    kbuild: update to SVN 3127
    Provided by ITOU (Sunagawa) Keiki in PR 52833.
    
    Changes: only SVN commit list found, see
    http://trac.netlabs.org/kbuild/timeline but this version is needed
    to build Virtualbox.

--- devel/kbuild/patches/patch-src_kmk_kmkbuiltin_osdep.c
+++ devel/kbuild/patches/patch-src_kmk_kmkbuiltin_osdep.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_kmk_kmkbuiltin_osdep.c,v 1.1 2016/07/08 09:42:54 kamil Exp $
+$NetBSD: patch-src_kmk_kmkbuiltin_osdep.c,v 1.2 2018/01/02 10:37:29 wiz Exp $
 
 Add NetBSD support.
 
---- src/kmk/kmkbuiltin/osdep.c.orig	2015-06-21 21:21:29.000000000 +0000
-+++ src/kmk/kmkbuiltin/osdep.c
+--- src/kmk/kmkbuiltin/osdep.c.orig	2017-11-26 23:29:40.350768372 +0900
++++ src/kmk/kmkbuiltin/osdep.c	2017-12-02 00:41:40.030220196 +0900
 @@ -41,8 +41,10 @@
  #elif defined(__OpenBSD__)
  # include "openbsd.c"
  
 +#elif defined(__NetBSD__)
-+# include "strtofflags.c"
++# include <util.h>
 +
  #elif defined(__HAIKU__)
  # include "haikufakes.c"
