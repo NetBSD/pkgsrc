f59d02085d5b4bfc230afe8923d57c2b1d680537
adam <adam@pkgsrc.org>
2018-01-05 11:35:06 +0000

    py-weasyprint: updated to 0.42
    Version 0.42
    New features:
    * Support relative file URIs when using CLI.
    
    Bug fixes:
    * Fix slow performance for pre-formatted boxes with a lot of children.
    * Don't crash when rendering some tables.
    * Fix rendering of floats in inlines.
    * Split lines carefully.
    * Fix root when frozen with Pyinstaller.
    * Handle SVGs containing images embedded as data URIs.
    * Fix border-radius rendering problem with some PDF readers.
    * Fix pipenv support.
    * Smartly handle replaced boxes with percentage width in auto-width parents.
    * Don't ignore CSS @page rules that are imported by an @import rule.

--- print/py-weasyprint/patches/patch-setup.py
+++ print/py-weasyprint/patches/patch-setup.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-setup.py,v 1.2 2017/08/01 15:36:26 kleink Exp $
+$NetBSD: patch-setup.py,v 1.3 2018/01/05 11:35:06 adam Exp $
 
 Adjust script path to allow multiple Python version installs.
 
---- setup.py.orig	2017-06-22 16:31:14.000000000 +0000
+--- setup.py.orig	2017-12-22 23:26:45.000000000 +0000
 +++ setup.py
-@@ -87,7 +87,7 @@ setup(
-         "pytest-runner", "pytest-cov", "pytest-flake8", "pytest-isort"]},
+@@ -83,7 +83,7 @@ setup(
+         ':python_version >= "3.0"': ['CairoSVG >= 1.0.20']},
      entry_points={
          'console_scripts': [
 -            'weasyprint = weasyprint.__main__:main',
