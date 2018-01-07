4183e1fe3271ea62db2ace43889ab923f722e0db
leot <leot@pkgsrc.org>
2018-01-07 12:57:03 +0000

    girara: Update graphics/girara to 0.2.8
    pkgsrc changes:
     - Bump BUILDLINK_API_DEPENDS.girara due to API and ABI changes and delete
       BUILDLINK_ABI_DEPENDS.girara that is no more needed.
    
    Changes:
    0.2.8
    -----
    * Adopt CSS to newer GTK+ 3 releases
    * Remove obsolete features
    * Various fixes and improvments
    * Update translations

--- graphics/girara/buildlink3.mk
+++ graphics/girara/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2017/11/23 17:19:42 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/01/07 12:57:03 leot Exp $
 
 BUILDLINK_TREE+=	girara
 
 .if !defined(GIRARA_BUILDLINK3_MK)
 GIRARA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.girara+=	girara>=0.2.4
-BUILDLINK_ABI_DEPENDS.girara+=	girara>=0.2.7nb3
+BUILDLINK_API_DEPENDS.girara+=	girara>=0.2.8
 BUILDLINK_PKGSRCDIR.girara?=	../../graphics/girara
 
 .include "../../x11/gtk3/buildlink3.mk"
