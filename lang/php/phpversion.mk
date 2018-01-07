6c9e101dd9c240d7d707c8d7de667d0c2ae63f3d
bsiegert <bsiegert@pkgsrc.org>
2018-01-07 17:30:26 +0000

    Pullup ticket #5673 - requested by taca lang/php56: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.198
    - lang/php56/distinfo                                           1.44
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Fri Jan  5 03:08:36 UTC 2018
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php56: distinfo
    
       Log Message:
       lang/php56: update to 5.6.33
    
       04 Jan 2017, PHP 5.6.33
    
       - GD:
         . Fixed bug #75571 (Potential infinite loop in gdImageCreateFromGifCtx). (cmb)
    
       - Phar:
         . Fixed bug #74782 (Reflected XSS in .phar 404 page). (Stas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.197 2017/11/30 20:45:46 jdolecek Exp $
+# $NetBSD: phpversion.mk,v 1.197.2.1 2018/01/07 17:30:26 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -87,7 +87,7 @@
 PHPVERSION_MK=	defined
 
 # Define each PHP's version.
-PHP56_VERSION=	5.6.32
+PHP56_VERSION=	5.6.33
 PHP70_VERSION=	7.0.26
 PHP71_VERSION=	7.1.12
 PHP72_VERSION=	7.2.0
