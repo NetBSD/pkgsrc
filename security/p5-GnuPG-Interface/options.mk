bfa27f23b359016cdc3b8ba0e42a84c60e37094a
wiz <wiz@pkgsrc.org>
2018-01-09 10:22:47 +0000

    p5-GnuPG-Interface: Add comment about gnupg2 support patch.

--- security/p5-GnuPG-Interface/options.mk
+++ security/p5-GnuPG-Interface/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2017/08/29 12:23:24 wiz Exp $
+# $NetBSD: options.mk,v 1.5 2018/01/09 10:22:47 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.p5_GnuPG_Interface
 PKG_OPTIONS_REQUIRED_GROUPS=	gnupg
@@ -8,6 +8,8 @@ PKG_SUGGESTED_OPTIONS=		gnupg1
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgnupg2)
+# patch available at
+# https://github.com/bestpractical/gnupg-interface/pull/1
 DEPENDS+=		gnupg2-[0-9]*:../../security/gnupg2
 .else
 DEPENDS+=		gnupg>=1.4.2:../../security/gnupg
