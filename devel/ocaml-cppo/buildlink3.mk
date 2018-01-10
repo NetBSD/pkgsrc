e73c7d8861abb824c55e4c9ea667f6d9558c6244
jaapb <jaapb@pkgsrc.org>
2018-01-10 14:48:49 +0000

    Updated package devel/ocaml-cppo to version 1.6.0.
    Improvements are minor, most notably the change to support safe-string.

--- devel/ocaml-cppo/buildlink3.mk
+++ devel/ocaml-cppo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/07/11 10:20:03 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/01/10 14:48:49 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cppo
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-cppo
 OCAML_CPPO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.3.1
-BUILDLINK_ABI_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.5.0
+BUILDLINK_ABI_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.6.0
 BUILDLINK_PKGSRCDIR.ocaml-cppo?=	../../devel/ocaml-cppo
 .endif	# OCAML_CPPO_BUILDLINK3_MK
 
