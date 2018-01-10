b3ed03518698be83b356ff2972fa17a4061cc424
jaapb <jaapb@pkgsrc.org>
2018-01-10 15:54:02 +0000

    Updated package devel/ocaml-ppx_ast to version 0.9.2.
    Changelog is not available.

--- devel/ocaml-ppx_ast/buildlink3.mk
+++ devel/ocaml-ppx_ast/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/07/21 10:32:25 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/01/10 15:54:02 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_ast
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_ast
 OCAML_PPX_AST_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_ast+=	ocaml-ppx_ast>=0.9.1
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_ast+=	ocaml-ppx_ast>=0.9.2
 BUILDLINK_PKGSRCDIR.ocaml-ppx_ast?=	../../devel/ocaml-ppx_ast
 .endif	# OCAML_PPX_AST_BUILDLINK3_MK
 
