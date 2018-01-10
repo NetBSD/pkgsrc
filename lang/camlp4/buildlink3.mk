0eeba5ac706ccbbbdee8e15c7b78069a9ff4cf36
jaapb <jaapb@pkgsrc.org>
2018-01-10 16:22:41 +0000

    Updated package lang/camlp4 to version 4.06+1.
    This is due to the upgrade of lang/ocaml to 4.06 - camlp4 has to follow
    suit.

--- lang/camlp4/buildlink3.mk
+++ lang/camlp4/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2017/09/08 11:31:01 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/01/10 16:22:41 jaapb Exp $
 
 BUILDLINK_TREE+=	camlp4
 
 .if !defined(CAMLP4_BUILDLINK3_MK)
 CAMLP4_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.camlp4+=	camlp4>=4.05
-BUILDLINK_ABI_DEPENDS.camlp4+=	camlp4>=4.05
+BUILDLINK_API_DEPENDS.camlp4+=	camlp4>=4.06
+BUILDLINK_ABI_DEPENDS.camlp4+=	camlp4>=4.06
 BUILDLINK_PKGSRCDIR.camlp4?=	../../lang/camlp4
 .endif	# CAMLP4_BUILDLINK3_MK
 
