9372ea98eb0272d45797de639d97ec7a9a6d5908
jaapb <jaapb@pkgsrc.org>
2018-01-10 16:33:23 +0000

    Updated package misc/ocaml-opam to version 2.0.0beta5.
    This version includes several bugfixes; see the CHANGES file.

--- misc/ocaml-opam/buildlink3.mk
+++ misc/ocaml-opam/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2017/09/08 16:50:46 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/01/10 16:33:23 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-opam
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-opam
 OCAML_OPAM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-opam+=	ocaml-opam>=1.2.2
-BUILDLINK_ABI_DEPENDS.ocaml-opam+=	ocaml-opam>=2.0.0_beta4
+BUILDLINK_ABI_DEPENDS.ocaml-opam+=	ocaml-opam>=2.0.0_beta5
 BUILDLINK_PKGSRCDIR.ocaml-opam?=	../../misc/ocaml-opam
 
 .endif	# OCAML_OPAM_BUILDLINK3_MK
