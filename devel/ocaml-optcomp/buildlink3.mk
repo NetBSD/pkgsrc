700b7617d459dab9e68d2fd570813054430ff83a
jaapb <jaapb@pkgsrc.org>
2018-01-10 15:47:30 +0000

    Corrected PLIST for devel/ocaml-optcomp

--- devel/ocaml-optcomp/buildlink3.mk
+++ devel/ocaml-optcomp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2017/09/08 09:51:21 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/01/10 15:47:30 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-optcomp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-optcomp
 OCAML_OPTCOMP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-optcomp+=	ocaml-optcomp>=1.6nb1
-BUILDLINK_ABI_DEPENDS.ocaml-optcomp+=	ocaml-optcomp>=1.6nb6
+BUILDLINK_ABI_DEPENDS.ocaml-optcomp+=	ocaml-optcomp>=1.6nb8
 BUILDLINK_PKGSRCDIR.ocaml-optcomp?=	../../devel/ocaml-optcomp
 
 .include "../../lang/camlp4/buildlink3.mk"
