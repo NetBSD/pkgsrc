023ec8797c09550fbbd001612f74dd6b615a5a89
jaapb <jaapb@pkgsrc.org>
2018-01-10 16:34:50 +0000

    Updated package misc/ocaml-topkg to version 0.9.1.
    This package contains mostly minor fixes and enhancements; see the
    CHANGES.md file.

--- misc/ocaml-topkg/buildlink3.mk
+++ misc/ocaml-topkg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/07/11 09:58:59 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/01/10 16:34:50 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-topkg
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-topkg
 OCAML_TOPKG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-topkg+=	ocaml-topkg>=0.9.0
+BUILDLINK_ABI_DEPENDS.ocaml-topkg+=	ocaml-topkg>=0.9.1
 BUILDLINK_PKGSRCDIR.ocaml-topkg?=	../../misc/ocaml-topkg
 
 .endif	# OCAML_TOPKG_BUILDLINK3_MK
