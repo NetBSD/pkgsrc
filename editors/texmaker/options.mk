f82e2bfc3f37788a7a878df149329014e2a6973b
wiz <wiz@pkgsrc.org>
2018-01-10 22:09:02 +0000

    texmaker: default to qt5 now that poppler-qt4 is gone.

--- editors/texmaker/options.mk
+++ editors/texmaker/options.mk
@@ -1,20 +1,13 @@
-# $NetBSD: options.mk,v 1.2 2014/12/02 12:16:29 mef Exp $
-#
+# $NetBSD: options.mk,v 1.3 2018/01/10 22:09:02 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.texmaker
 PKG_OPTIONS_REQUIRED_GROUPS= qt
-PKG_OPTIONS_GROUP.qt = qt4 qt5
-PKG_SUGGESTED_OPTIONS=	qt4
+PKG_OPTIONS_GROUP.qt = qt5
+PKG_SUGGESTED_OPTIONS=	qt5
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mqt5)
 .include "../../x11/qt5-qtscript/buildlink3.mk"
 .include "../../x11/qt5-qtwebkit/buildlink3.mk"
 .include "../../print/poppler-qt5/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mqt4)
-.include "../../x11/qt4-libs/buildlink3.mk"
-.include "../../x11/qt4-tools/buildlink3.mk"
-.include "../../print/poppler-qt4/buildlink3.mk"
 .endif
