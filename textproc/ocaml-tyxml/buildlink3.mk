a977b17bb9bd5794813cc009dcff22745b31072e
jaapb <jaapb@pkgsrc.org>
2018-01-10 16:48:21 +0000

    Revbump for textproc/ocaml-tyxml.
    This revision also adds a patch to make the program compile with
    safe-string and therefore with OCaml 4.06.

--- textproc/ocaml-tyxml/buildlink3.mk
+++ textproc/ocaml-tyxml/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.6 2018/01/07 13:04:35 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2018/01/10 16:48:21 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-tyxml
 
 .if !defined(OCAML_TYXML_BUILDLINK3_MK)
 OCAML_TYXML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-tyxml+=	ocaml-tyxml>=4.0.1
-BUILDLINK_ABI_DEPENDS.ocaml-tyxml+=	ocaml-tyxml>=4.1.0
-BUILDLINK_PKGSRCDIR.ocaml-tyxml?=	../../textproc/ocaml-tyxml
+BUILDLINK_API_DEPENDS.ocaml-tyxml+=     ocaml-tyxml>=4.0.1
+BUILDLINK_ABI_DEPENDS.ocaml-tyxml+=     ocaml-tyxml>=4.1.0nb2
+BUILDLINK_PKGSRCDIR.ocaml-tyxml?=       ../../textproc/ocaml-tyxml
 
 .endif  # OCAML_TYXML_BUILDLINK3_MK
 
