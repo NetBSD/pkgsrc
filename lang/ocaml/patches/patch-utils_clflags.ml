fcf1059b4126a9a824ea64dde82fadb19da36d90
jaapb <jaapb@pkgsrc.org>
2018-01-10 14:30:34 +0000

    Updated package lang/ocaml to version 4.06.
    Changes are many and can be found at https://ocaml.org/releases/4.06.html
    
    One change that breaks a lot of packages is that the safe-string compiler
    option is now enabled by default, which means that all modules need to use
    immutable strings (or explicitly use the bytes type).

--- lang/ocaml/patches/patch-utils_clflags.ml
+++ lang/ocaml/patches/patch-utils_clflags.ml
@@ -1,16 +1,16 @@
-$NetBSD: patch-utils_clflags.ml,v 1.4 2016/05/05 08:12:01 jaapb Exp $
+$NetBSD: patch-utils_clflags.ml,v 1.5 2018/01/10 14:30:35 jaapb Exp $
 
 pkgsrc_runtime flag
 
---- utils/clflags.ml.orig	2016-04-01 12:53:41.000000000 +0000
+--- utils/clflags.ml.orig	2017-10-03 08:35:25.000000000 +0000
 +++ utils/clflags.ml
-@@ -148,7 +148,8 @@ let pic_code = ref (match Config.archite
+@@ -154,7 +154,8 @@ let pic_code = ref (match Config.archite
                       | "amd64" -> true
                       | _       -> false)
  
 -let runtime_variant = ref "";;      (* -runtime-variant *)
-+let runtime_variant = ref ""        (* -runtime-variant *)
-+let pkgsrc_runtime = ref false;;    (* -pkgsrc-runtime *)
++let runtime_variant = ref ""           (* -runtime-variant *)
++let pkgsrc_runtime = ref false;;       (* -pkgsrc-runtime *)
  
  let keep_docs = ref false              (* -keep-docs *)
- let keep_locs = ref false              (* -keep-locs *)
+ let keep_locs = ref true               (* -keep-locs *)
