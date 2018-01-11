84e699822e9efeebd836ab6d1988bc8208837fb0
markd <markd@pkgsrc.org>
2018-01-11 09:32:01 +0000

    texworks: update to 0.6.2 and switch to qt5
    Release 0.6.2 (TL'17) [April 2017]
    * Implement handling of links to external files in the PDF preview
    * Implement a simple screen calibration widget to allow adapting the PDF
      preview to the screen's resolution
    * Improve responsiveness when opening large documents
    * Use old content instead of rendering placeholers after a PDF was reloaded
      to facilitate seeing changes
    * Improve the usability of the "Remove Aux Files..." dialog by letting the
      user toogle the selection by clicking anywhere on the row
    * Add support for the bibliography program "biber" by default
    * Allow custom zoom levels in the PDF status bar context menu
    * Add a Windows manifest so TeXworks looks more "native" on modern versions
      of Windows
    
    * Fix PDF zoom speed for high-resolution mouse-wheels
    * Fix a deadlock causing TeXworks to hang when loading PDF annotations such
      as links
    * Fix crash when changing files outside TeXworks
    * Fix crash when syncing from an if-block in the TeX file
    * Fix crash when reloading a PDF while text was selected
    * Fix enabling of the first/previous/next/last page toolbar buttons in the
      PDF preview
    * Fix the wrong appearance of the PDF magnifier in some cases
    * Fix truncation issues and misreported Windows versions in "Email to
      mailing list"
    
    * Update translations

--- editors/texworks/patches/patch-getDefaultBinPaths.sh
+++ editors/texworks/patches/patch-getDefaultBinPaths.sh
@@ -1,11 +1,12 @@
-$NetBSD: patch-getDefaultBinPaths.sh,v 1.4 2017/02/14 09:19:43 markd Exp $
+$NetBSD: patch-getDefaultBinPaths.sh,v 1.5 2018/01/11 09:32:01 markd Exp $
 
---- getDefaultBinPaths.sh.orig	2016-05-01 08:43:14.000000000 +0000
+--- getDefaultBinPaths.sh.orig	2017-04-29 16:23:51.000000000 +0000
 +++ getDefaultBinPaths.sh
-@@ -74,14 +74,14 @@ case $PLATFORM in
+@@ -74,15 +74,15 @@ case $PLATFORM in
  	*)			OS=`echo $PLATFORM | sed 's/.*-//'`
  esac
  
+-appendPath "/usr/local/texlive/2018/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2017/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2016/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2015/bin/$ARCH-$OS"
@@ -14,6 +15,7 @@ $NetBSD: patch-getDefaultBinPaths.sh,v 1.4 2017/02/14 09:19:43 markd Exp $
 -appendPath "/usr/local/texlive/2012/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2011/bin/$ARCH-$OS"
 -appendPath "/usr/local/texlive/2010/bin/$ARCH-$OS"
++#appendPath "/usr/local/texlive/2018/bin/$ARCH-$OS"
 +#appendPath "/usr/local/texlive/2017/bin/$ARCH-$OS"
 +#appendPath "/usr/local/texlive/2016/bin/$ARCH-$OS"
 +#appendPath "/usr/local/texlive/2015/bin/$ARCH-$OS"
