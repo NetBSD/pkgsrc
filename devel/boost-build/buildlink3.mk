76d107c3c402eb3fb907f2553ebb270b6597c7c5
jperkin <jperkin@pkgsrc.org>
2018-01-13 16:09:59 +0000

    boost-build: Fix API depends pattern.

--- devel/boost-build/buildlink3.mk
+++ devel/boost-build/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.36 2017/08/24 19:31:31 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.37 2018/01/13 16:09:59 jperkin Exp $
 
 BUILDLINK_TREE+=	boost-build
 
 .if !defined(BOOST_BUILD_BUILDLINK3_MK)
 BOOST_BUILD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.65.*
+BUILDLINK_API_DEPENDS.boost-build+=	boost-build-1.66.*
 BUILDLINK_DEPMETHOD.boost-build?=	build
 BUILDLINK_PKGSRCDIR.boost-build?=	../../devel/boost-build
 .endif # BOOST_BUILD_BUILDLINK3_MK
