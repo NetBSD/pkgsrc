6fc95f39310518b807431ff9d3ed022299a3f6a5
tsutsui <tsutsui@pkgsrc.org>
2018-01-14 05:15:05 +0000

    mikutter: update to 3.6.1.
    Upstream changes:
    
    mikutter 3.6.1
    
    * happy new year
    * honor iPhone X
    * require Diva gem 0.3.1 or later
     * JSON exported Time was not in iso8601 format
    * Spell to update user profile (thanks shibafu528)
    * fix typo (thanks akkiesoft)
    * twitter: crash when using World other than Twitter due to wrong assumption
               that User#idname was always defined
    * openimg: sporadic crash when opening images

--- net/mikutter/patches/patch-mikutter.rb
+++ net/mikutter/patches/patch-mikutter.rb
@@ -1,9 +1,9 @@
-$NetBSD: patch-mikutter.rb,v 1.5 2017/12/30 05:05:14 tsutsui Exp $
+$NetBSD: patch-mikutter.rb,v 1.6 2018/01/14 05:15:05 tsutsui Exp $
 
 - pkgsrc can handle location of ruby binary
   https://dev.mikutter.hachune.net/issues/889
 
---- mikutter.rb.orig	2017-06-10 01:11:51.000000000 +0000
+--- mikutter.rb.orig	2018-01-13 17:07:11.000000000 +0000
 +++ mikutter.rb
 @@ -1,7 +1,5 @@
 -#!/bin/sh
@@ -13,4 +13,4 @@ $NetBSD: patch-mikutter.rb,v 1.5 2017/12/30 05:05:14 tsutsui Exp $
 -#!ruby
  =begin rdoc
  = mikutter - the moest twitter client
- Copyright (C) 2009-2017 Toshiaki Asai
+ Copyright (C) 2009-2018 Toshiaki Asai
