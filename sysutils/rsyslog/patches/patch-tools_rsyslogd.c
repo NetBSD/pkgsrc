51b0ed3bf6dcf9c74fc91f53ba85e7f79f1483cc
fhajny <fhajny@pkgsrc.org>
2018-01-15 11:01:16 +0000

    Update sysutils/rsyslog* to 8.32.0.
    - rsyslogd: add capability to specify that no pid file shall be
      written
    - ompgsql: considerable enhancements
    - build system: removed --enable-rtinst configure option
    - pmrfc3164: support for headerless messages
    - omhiredis: add option to use RPUSH instead of LPUSH
    - mmexternal improvements
    - omprog: refactored, code shared with mmexternal moved to common
      object
    - logctl tool: refactor to support newer rsyslog standards
    - imfile: added support for Solaris File Event notification (FEN)
    - core/action: new parameter "action.errorfile"
    - imfile: added new module parameter "sortFiles"
    - imuxsock: improved status reporting: socket name received from
      systemd
    - build system: added new testbench configure switches
    - mmpstrucdata: new parameter "sd_name.lowercase"
    - omfile: add module-global option "dynafile.donotsuspend"
    - testbench: add a capability to turn off libfaketime tests via
      configure
    - testbench: name valgrind tests consistently
    - RainerScript: add function parse_json()
    - RainerScript: add function substring()
    - RainerScript: add function http_request()
    - RainerScript: add function previous_is_suspended()
    - Patches from BSD projects have been imported
    - script bugfix: invalid function names were silently ignored
    - rainerscript: add int2hex() function
    - rainerscript: add is_time() function
    - RainerScript: add function script_error() and error-reporting
      support
    - testbench: fixed build problem of testbench tools under Alpine Linux
    - added --enable-libsystemd configure option to enforce use of
      libsystemd
    - core/glbl: remove long-unused option $optimizeforuniprocessor
    - core/queue: emit better status messages at rsyslog shutdown
    - fixed a couple of build issues with gcc-7 (in less frequently used
      modules)
    - fixed a couple of build issues on the arm platform (actually
      raspbian)
    - impstats: fix invalid counter definitions for getrusage() reporting
    - imudp bugfix: potential segfault in ratelimiting
    - imptcp bugfix: access to free'ed memory
    - mmanon bugfix: fix wrong ipv6 embedded recognition
    - imfile bugfix: not detecting files in directory when wildcards are
      used.
    - script bugfix: improper string-to-number conversion for negative
      numbers
    - core/action bugfix: 100% CPU utilization on suspension of output
      module
    - core/variables bugfix: bare $! cannot be used in set statement
    - core bugfix: auto commit of actions improperly handled
    - core bugfix: filename length limitation of 199 bytes
    - core bugfix: undefined behavior due to integer overflow
    - core bugfix: race on LocalHostIP property during startup
    - bugfix: potential segfault on startup
    - omhiredis bugfix: rsyslog segfault on startup if no template is
      specified
    - omprog bugfix: argv[0] not set when using binary without arguments
    - core: refactoring of rsyslog's cstr "class"
    - parent directory creation function refactored
    - mmsnmptrapd bugfix: potential misadressing
    - imkafka: fix potential small ressource leak
    - imkafka bugfix: do not emit error message on regular state
    - omkafka: expose operational status to user where useful
    - omkafka bugfix: potential message duplication
    - omkafka: fix multithreading
    - omkafka bugfix: potential misadressing
    - omkafka bugfix: build fails with older versions of librdkafka
    - omgssapi bugfix: fix compiler warnings with gcc-7
    - dnscache bugfix: entries were cached based on IP AND port number
    - omkafka bugfix: fixed memory leak
    - mmdblookup bugfix: replace thread-unsafe strtok() by thread-safe
      counterpart
    - pmnormalize bugfix: remove unsave "strcat" implementation
    - rainerscript bugfix: ltrim() and rtrim function misadressing
    - imklog bugfix: local host IP was hardcoded to 127.0.0.1
    - cleanup: remove obsolete pre-KSI GuardTime signature interface
    - cleanup: obsolete defintion SOL_TCP replaced by newer IPPROTO_TCP
    - lookup tables: fixed undefined behavior detected by UBSan

--- sysutils/rsyslog/patches/patch-tools_rsyslogd.c
+++ sysutils/rsyslog/patches/patch-tools_rsyslogd.c
@@ -1,18 +1,10 @@
-$NetBSD: patch-tools_rsyslogd.c,v 1.5 2017/02/13 19:59:36 fhajny Exp $
+$NetBSD: patch-tools_rsyslogd.c,v 1.6 2018/01/15 11:01:16 fhajny Exp $
 
-Need errno.h on NetBSD. Default PID file path.
---- tools/rsyslogd.c.orig	2017-01-10 09:00:04.000000000 +0000
+Default PID file path.
+
+--- tools/rsyslogd.c.orig	2018-01-09 08:40:30.000000000 +0000
 +++ tools/rsyslogd.c
-@@ -33,7 +33,7 @@
- #else
- #  include <syslog.h>
- #endif
--#ifdef OS_SOLARIS
-+#if defined(OS_SOLARIS) || defined(OS_BSD)
- #	include <errno.h>
- #else
- #	include <sys/errno.h>
-@@ -146,7 +146,7 @@ void rsyslogdDoDie(int sig);
+@@ -148,7 +148,7 @@ void rsyslogdDoDie(int sig);
  #if defined(_AIX)  /* AIXPORT : Add _AIX */
  #	define PATH_PIDFILE "/etc/rsyslogd.pid"
  #else
