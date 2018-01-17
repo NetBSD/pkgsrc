853c1fcd58c3f211f8b7ac231058077c0e85d372
markd <markd@pkgsrc.org>
2018-01-17 20:00:20 +0000

    kf5: update to 5.41.0
    16 months worth of updates. requires qt5>=5.7

--- sysutils/baloo5/patches/patch-src_file_extractor_iohandler.h
+++ sysutils/baloo5/patches/patch-src_file_extractor_iohandler.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_file_extractor_iohandler.h,v 1.1 2016/05/26 09:17:14 markd Exp $
+$NetBSD: patch-src_file_extractor_iohandler.h,v 1.2 2018/01/17 20:09:33 markd Exp $
 
-Don't clash with stdio defines
+Don't use "stdin"
 
---- src/file/extractor/iohandler.h.orig	2016-04-03 20:42:08.000000000 +0000
+--- src/file/extractor/iohandler.h.orig	2017-12-02 20:01:27.000000000 +0000
 +++ src/file/extractor/iohandler.h
 @@ -31,7 +31,7 @@ namespace Baloo {
  class IOHandler
  {
  public:
--    IOHandler(int stdin, int stdout);
-+    IOHandler(int StdIn, int StdOut);
+-    IOHandler(int stdin);
++    IOHandler(int stdIn);
      quint64 nextId();
      bool atEnd() const;
  
