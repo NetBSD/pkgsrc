5166dddd0a062d7aefe148f9d3874aca17a049d4
markd <markd@pkgsrc.org>
2018-01-17 20:13:38 +0000

    kf5: update to 5.41.0
    16 months worth of updates. requires qt5>=5.7

---
+++ x11/kwindowsystem/hacks.mk
@@ -0,0 +1,7 @@
+# $NetBSD: hacks.mk,v 1.1 2018/01/17 20:16:58 markd Exp $
+
+# linking -lQt5Widgets on NetBSD with gcc produces
+# ld: BFD (NetBSD Binutils nb1) 2.27 assertion fail
+# /usr/src/external/gpl3/binutils/lib/libbfd/../../dist/bfd/elflink.c:2694
+# unless -lexecinfo preceeds -lQt5Widgets
+BUILDLINK_TRANSFORM.NetBSD+=l:Qt5Widgets:execinfo:Qt5Widgets
