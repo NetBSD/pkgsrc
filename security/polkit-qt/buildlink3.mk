4690eb2d371138ded3b514c9648f6024a830f89f
markd <markd@pkgsrc.org>
2018-01-17 18:48:26 +0000

    polkit-qt: update to 0.112.0
    matching the polkit-qt5 version, and switching to polkit.

--- security/polkit-qt/buildlink3.mk
+++ security/polkit-qt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2018/01/07 13:04:30 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2018/01/17 18:48:26 markd Exp $
 
 BUILDLINK_TREE+=	polkit-qt
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.polkit-qt+=	polkit-qt>=0.9.2
 BUILDLINK_ABI_DEPENDS.polkit-qt+=	polkit-qt>=0.9.4nb19
 BUILDLINK_PKGSRCDIR.polkit-qt?=		../../security/polkit-qt
 
-.include "../../security/policykit/buildlink3.mk"
+.include "../../security/polkit/buildlink3.mk"
 .include "../../x11/qt4-libs/buildlink3.mk"
 .endif	# POLKIT_QT_BUILDLINK3_MK
 
