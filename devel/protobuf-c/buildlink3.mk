9c7baa456cc03d31df2291d122645cd323c81211
jperkin <jperkin@pkgsrc.org>
2018-01-17 12:10:37 +0000

    Belated PKGREVISION bump for devel/protobuf update.
    Fixes at least joyent/pkgsrc#60.

--- devel/protobuf-c/buildlink3.mk
+++ devel/protobuf-c/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/06/10 01:39:33 wiedi Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/01/17 12:10:37 jperkin Exp $
 
 BUILDLINK_TREE+=	protobuf-c
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	protobuf-c
 PROTOBUF_C_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.protobuf-c+=	protobuf-c>=1.1.1
+BUILDLINK_ABI_DEPENDS.protobuf-c?=	protobuf-c>=1.3.0nb1
 BUILDLINK_PKGSRCDIR.protobuf-c?=	../../devel/protobuf-c
 
 .include "../../devel/protobuf/buildlink3.mk"
