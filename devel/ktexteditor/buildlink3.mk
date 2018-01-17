d35570f050d2d1833a766cf8634fbfd005afa017
markd <markd@pkgsrc.org>
2018-01-17 20:00:20 +0000

    kf5: update to 5.41.0
    16 months worth of updates. requires qt5>=5.7

--- devel/ktexteditor/buildlink3.mk
+++ devel/ktexteditor/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2017/11/30 16:45:03 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2018/01/17 20:00:22 markd Exp $
 
 BUILDLINK_TREE+=	ktexteditor
 
@@ -10,6 +10,7 @@ BUILDLINK_ABI_DEPENDS.ktexteditor?=	ktexteditor>=5.25.0nb12
 BUILDLINK_PKGSRCDIR.ktexteditor?=	../../devel/ktexteditor
 
 .include "../../devel/kparts/buildlink3.mk"
+.include "../../textproc/syntax-highlighting/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# KTEXTEDITOR_BUILDLINK3_MK
 
