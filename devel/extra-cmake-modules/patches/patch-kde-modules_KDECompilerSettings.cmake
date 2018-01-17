0dc008fee5e4726eb876f0f0a44c8081c89c3f61
markd <markd@pkgsrc.org>
2018-01-17 19:56:18 +0000

    extra-cmake-modules: update tp 5.41.0
    16 months worth of updates. requires qt5>=5.7

--- devel/extra-cmake-modules/patches/patch-kde-modules_KDECompilerSettings.cmake
+++ devel/extra-cmake-modules/patches/patch-kde-modules_KDECompilerSettings.cmake
@@ -1,17 +1,17 @@
-$NetBSD: patch-kde-modules_KDECompilerSettings.cmake,v 1.1 2016/06/07 17:54:59 joerg Exp $
+$NetBSD: patch-kde-modules_KDECompilerSettings.cmake,v 1.2 2018/01/17 19:56:18 markd Exp $
 
 Do not hard-code --enable-new-dtags. It is completely broken on systems like
 NetBSD implementing the sane rpath semantic and just have a new enough binutils
 version. It is up to the system compiler to know what the correct platfor
 choice is.
 
---- kde-modules/KDECompilerSettings.cmake.orig	2016-06-05 23:16:56.900586724 +0000
+--- kde-modules/KDECompilerSettings.cmake.orig	2017-11-30 07:42:14.000000000 +0000
 +++ kde-modules/KDECompilerSettings.cmake
-@@ -208,14 +208,6 @@ if (POLICY CMP0063)
+@@ -229,14 +229,6 @@ if (POLICY CMP0063)
      cmake_policy(SET CMP0063 NEW)
  endif()
  
--if (UNIX AND NOT APPLE)
+-if (UNIX AND NOT APPLE AND NOT CYGWIN)
 -    # Enable adding DT_RUNPATH, which means that LD_LIBRARY_PATH takes precedence
 -    # over the built-in rPath
 -    set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--enable-new-dtags ${CMAKE_SHARED_LINKER_FLAGS}")
