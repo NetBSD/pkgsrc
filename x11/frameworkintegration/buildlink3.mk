f451daf0aa3c2dbe0934f5c88f9e09416532c6a8
markd <markd@pkgsrc.org>
2018-01-17 20:13:38 +0000

    kf5: update to 5.41.0
    16 months worth of updates. requires qt5>=5.7

--- x11/frameworkintegration/buildlink3.mk
+++ x11/frameworkintegration/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2017/11/30 16:45:10 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2018/01/17 20:16:57 markd Exp $
 
 BUILDLINK_TREE+=	frameworkintegration
 
@@ -9,7 +9,8 @@ BUILDLINK_API_DEPENDS.frameworkintegration+=	frameworkintegration>=5.21.0
 BUILDLINK_ABI_DEPENDS.frameworkintegration?=	frameworkintegration>=5.25.0nb11
 BUILDLINK_PKGSRCDIR.frameworkintegration?=	../../x11/frameworkintegration
 
-.include "../../devel/kio/buildlink3.mk"
+.include "../../devel/kpackage/buildlink3.mk"
+.include "../../net/knewstuff/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# FRAMEWORKINTEGRATION_BUILDLINK3_MK
 
