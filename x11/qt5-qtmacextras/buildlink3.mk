f6d531cfa2ac019e101b89db2f61c1c489e45cb1
markd <markd@pkgsrc.org>
2018-01-18 11:35:48 +0000

    qt5-qtmacextras: update to 5.10.0

--- x11/qt5-qtmacextras/buildlink3.mk
+++ x11/qt5-qtmacextras/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2017/11/30 16:45:12 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2018/01/18 11:35:48 markd Exp $
 
 BUILDLINK_TREE+=	qt5-qtmacextras
 
 .if !defined(QT5_QTMACEXTRAS_BUILDLINK3_MK)
 QT5_QTMACEXTRAS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qt5-qtmacextras+=	qt5-qtmacextras>=5.5.1
-BUILDLINK_ABI_DEPENDS.qt5-qtmacextras+=	qt5-qtmacextras>=5.5.1nb9
+BUILDLINK_API_DEPENDS.qt5-qtmacextras+=	qt5-qtmacextras>=5.9.1
+BUILDLINK_ABI_DEPENDS.qt5-qtmacextras+=	qt5-qtmacextras>=5.9.1
 BUILDLINK_PKGSRCDIR.qt5-qtmacextras?=	../../x11/qt5-qtmacextras
 
 BUILDLINK_INCDIRS.qt5-qtmacextras+=	qt5/include
