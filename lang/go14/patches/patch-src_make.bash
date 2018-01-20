906bacbb176ad771b76aff22a5c971332dbe450b
christos <christos@pkgsrc.org>
2018-01-20 20:44:13 +0000

    don't substitute bash here

--- lang/go14/patches/patch-src_make.bash
+++ lang/go14/patches/patch-src_make.bash
@@ -1,15 +1,9 @@
-$NetBSD: patch-src_make.bash,v 1.1 2018/01/20 16:55:25 christos Exp $
+$NetBSD: patch-src_make.bash,v 1.2 2018/01/20 20:44:13 christos Exp $
 
  * Workaround for Warninig armv5 instruction in GCC4.8 generic arm compiler 
 
 --- src/make.bash.orig	2015-09-23 04:37:37.000000000 +0000
 +++ src/make.bash
-@@ -1,4 +1,4 @@
--#!/usr/bin/env bash
-+#!/usr/pkg/bin/bash
- # Copyright 2009 The Go Authors. All rights reserved.
- # Use of this source code is governed by a BSD-style
- # license that can be found in the LICENSE file.
 @@ -121,6 +121,15 @@ case "$GOHOSTARCH" in
  386) mflag=-m32;;
  amd64) mflag=-m64;;
