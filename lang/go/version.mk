18de1bf911abb47ad6a1ecb8b801fb0968e6997d
christos <christos@pkgsrc.org>
2018-01-20 16:55:05 +0000

    Add Yasushi Oshima patches for arm from:     https://github.com/oshimaya/pkgsrc/tree/master/lang/go

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,11 +1,11 @@
-# $NetBSD: version.mk,v 1.30 2017/10/28 18:20:14 bsiegert Exp $
+# $NetBSD: version.mk,v 1.31 2018/01/20 16:55:05 christos Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
 GO_VERSION=	1.9.2
 GO14_VERSION=	1.4.3
 
-ONLY_FOR_PLATFORM=	*-*-i386 *-*-x86_64 *-*-*arm*
+ONLY_FOR_PLATFORM=	*-*-i386 *-*-x86_64 *-*-earmv[67]hf
 NOT_FOR_PLATFORM=	SunOS-*-i386
 .if ${MACHINE_ARCH} == "i386"
 GOARCH=		386
@@ -13,9 +13,14 @@ GOCHAR=		8
 .elif ${MACHINE_ARCH} == "x86_64"
 GOARCH=		amd64
 GOCHAR=		6
-.elif !empty(MACHINE_ARCH:M*arm*)
+.elif ${MACHINE_ARCH} == "earmv6hf" || ${MACHINE_ARCH} == "earmv7hf"
 GOARCH=		arm
 GOCHAR=		5
+.endif
+.if ${MACHINE_ARCH} == "earmv6hf"
+GOOPT=		GOARM=6
+.elif ${MACHINE_ARCH} == "earmv7hf"
+GOOPT=		GOARM=7
 .endif
 PLIST_SUBST+=	GO_PLATFORM=${LOWER_OPSYS:Q}_${GOARCH:Q} GOARCH=${GOARCH:Q}
 PLIST_SUBST+=	GOCHAR=${GOCHAR:Q}
