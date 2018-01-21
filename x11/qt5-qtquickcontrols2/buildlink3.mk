a5fefcaeba477e01ac1063894c9e156eddd5a01b
markd <markd@pkgsrc.org>
2018-01-21 04:17:28 +0000

    qt5-qtquickcontrols2: add version 5.10.0
    Next generation user interface controls based on Qt Quick

---
+++ x11/qt5-qtquickcontrols2/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2018/01/21 04:17:28 markd Exp $
+
+BUILDLINK_TREE+=	qt5-qtquickcontrols2
+
+.if !defined(QT5_QTQUICKCONTROLS2_BUILDLINK3_MK)
+QT5_QTQUICKCONTROLS2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt5-qtquickcontrols2+=	qt5-qtquickcontrols2>=5.10.0
+BUILDLINK_PKGSRCDIR.qt5-qtquickcontrols2?=	../../wip/qt5-qtquickcontrols2
+
+.include "../../wip/qt5-qtdeclarative/buildlink3.mk"
+.endif	# QT5_QTQUICKCONTROLS2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt5-qtquickcontrols2
