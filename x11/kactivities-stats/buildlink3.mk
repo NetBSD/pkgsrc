92bb91c1769a352aead3b09175ef9ec191437501
markd <markd@pkgsrc.org>
2018-01-21 20:17:34 +0000

    kactivities-stats: add buildlink3.mk

---
+++ x11/kactivities-stats/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2018/01/21 20:17:34 markd Exp $
+
+BUILDLINK_TREE+=	kactivities-stats
+
+.if !defined(KACTIVITIES_STATS_BUILDLINK3_MK)
+KACTIVITIES_STATS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kactivities-stats+=	kactivities-stats>=5.41.0
+BUILDLINK_PKGSRCDIR.kactivities-stats?=	../../x11/kactivities-stats
+
+.include "../../x11/kactivities5/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KACTIVITIES_STATS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kactivities-stats
