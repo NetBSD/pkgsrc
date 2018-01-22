965d5f8c2c8d81174757c334fd8df1fb77f769df
jaapb <jaapb@pkgsrc.org>
2018-01-22 11:58:02 +0000

    Updated package devel/ocaml-lwt-ssl to version 1.1.1.
    This fixes compatibility with OCaml 4.06.

--- devel/ocaml-lwt_ssl/buildlink3.mk
+++ devel/ocaml-lwt_ssl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/01/10 16:53:09 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/01/22 11:58:02 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt_ssl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-lwt_ssl
 OCAML_LWT_SSL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-lwt_ssl+=	ocaml-lwt_ssl>=1.1.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt_ssl?=	ocaml-lwt_ssl>=1.1.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-lwt_ssl?=	ocaml-lwt_ssl>=1.1.1
 BUILDLINK_PKGSRCDIR.ocaml-lwt_ssl?=	../../devel/ocaml-lwt_ssl
 
 .include "../../devel/ocaml-lwt/buildlink3.mk"
