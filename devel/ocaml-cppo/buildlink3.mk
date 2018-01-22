fde4882a09eb8a4d2abc52669cac902d9d093fae
jaapb <jaapb@pkgsrc.org>
2018-01-22 12:04:04 +0000

    Added missing dependency to devel/ocaml-cppo

--- devel/ocaml-cppo/buildlink3.mk
+++ devel/ocaml-cppo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/01/10 14:48:49 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/01/22 12:04:04 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cppo
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-cppo
 OCAML_CPPO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.3.1
-BUILDLINK_ABI_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.6.0
+BUILDLINK_ABI_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.6.0nb1
 BUILDLINK_PKGSRCDIR.ocaml-cppo?=	../../devel/ocaml-cppo
 .endif	# OCAML_CPPO_BUILDLINK3_MK
 
