d36d00a6fd3ac7054f9758ff69c6ff16a31fe755
markd <markd@pkgsrc.org>
2018-01-22 01:02:28 +0000

    kirigami2: add version 5.41.0
    Kirigami is a set of QtQuick components at the moment targeted for
    mobile use (in the future desktop as well) targeting both Plasma
    Mobile and Android. It's not a whole set of components, all the
    "Primitive" ones like buttons and textboxes are a job for
    QtQuickControls (soon QtQuickControls2) but it's a set of high level
    components to make the creation of applications that look and feel
    great on mobile as well as desktop devices and follow the Kirigami
    Human Interface Guidelines.

---
+++ x11/kirigami2/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2018/01/22 01:02:28 markd Exp $
+
+BUILDLINK_TREE+=	kirigami2
+
+.if !defined(KIRIGAMI2_BUILDLINK3_MK)
+KIRIGAMI2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kirigami2+=	kirigami2>=5.41.0
+BUILDLINK_PKGSRCDIR.kirigami2?=	../../x11/kirigami2
+
+.include "../../x11/qt5-qtquickcontrols2/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KIRIGAMI2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kirigami2
