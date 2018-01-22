e5e2aba08bccf240334b2aa28bbdac7254bd9d72
wiz <wiz@pkgsrc.org>
2018-01-22 11:36:31 +0000

    qt5-qtquickcontrols2: wip -> x11

--- x11/qt5-qtquickcontrols2/buildlink3.mk
+++ x11/qt5-qtquickcontrols2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/01/21 04:17:28 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/01/22 11:36:31 wiz Exp $
 
 BUILDLINK_TREE+=	qt5-qtquickcontrols2
 
@@ -6,9 +6,9 @@ BUILDLINK_TREE+=	qt5-qtquickcontrols2
 QT5_QTQUICKCONTROLS2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt5-qtquickcontrols2+=	qt5-qtquickcontrols2>=5.10.0
-BUILDLINK_PKGSRCDIR.qt5-qtquickcontrols2?=	../../wip/qt5-qtquickcontrols2
+BUILDLINK_PKGSRCDIR.qt5-qtquickcontrols2?=	../../x11/qt5-qtquickcontrols2
 
-.include "../../wip/qt5-qtdeclarative/buildlink3.mk"
+.include "../../x11/qt5-qtdeclarative/buildlink3.mk"
 .endif	# QT5_QTQUICKCONTROLS2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-qt5-qtquickcontrols2
