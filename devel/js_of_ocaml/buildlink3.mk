f3f1affd66f4c2a850e213942d5ca6f0c89c7f46
jaapb <jaapb@pkgsrc.org>
2018-01-22 11:50:24 +0000

    Updated package devel/js_of_ocaml to version 3.0.2.
    Changes include compatibility fixes for OCaml 4.05 and 4.06, plus
    some other minor features and bugfixes (see the CHANGES file for details)

--- devel/js_of_ocaml/buildlink3.mk
+++ devel/js_of_ocaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2017/09/08 16:55:19 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/01/22 11:50:24 jaapb Exp $
 
 BUILDLINK_TREE+=	js_of_ocaml
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	js_of_ocaml
 JS_OF_OCAML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.js_of_ocaml+=	js_of_ocaml>=3.0.0
-BUILDLINK_ABI_DEPENDS.js_of_ocaml+=	js_of_ocaml>=3.0.0nb1
+BUILDLINK_ABI_DEPENDS.js_of_ocaml+=	js_of_ocaml>=3.0.2
 BUILDLINK_PKGSRCDIR.js_of_ocaml?=	../../devel/js_of_ocaml
 
 .endif	# JS_OF_OCAML_BUILDLINK3_MK
