3acf7e3257be314c815858d47ba36d90bf6c2ca3
jaapb <jaapb@pkgsrc.org>
2018-01-22 11:53:24 +0000

    Updated package devel/ocaml-lwt to version 3.2.0.
    This release includes several bugfixes and additions. For more details see
    the CHANGES file.

--- devel/ocaml-lwt/buildlink3.mk
+++ devel/ocaml-lwt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2018/01/07 13:04:08 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2018/01/22 11:53:24 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-lwt
 OCAML_LWT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-lwt+=	ocaml-lwt>=3.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt+=	ocaml-lwt>=3.1.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-lwt+=	ocaml-lwt>=3.2.0
 BUILDLINK_PKGSRCDIR.ocaml-lwt?=		../../devel/ocaml-lwt
 
 #.include "../../devel/libev/buildlink3.mk"
