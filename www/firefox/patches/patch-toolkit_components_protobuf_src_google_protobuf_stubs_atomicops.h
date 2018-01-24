7b249aa4f9f333501306f640e6ca12f429a4c89a
ryoon <ryoon@pkgsrc.org>
2018-01-24 16:52:08 +0000

    Update to 58.0
    Changelog:
    New
        Performance improvements, including:
            Rendering graphics for Windows users by using Off-Main-Threa
               Painting (OMTP)
            Loading pages faster by changing how Firefox caches and retrieves
               JavaScript
    
        Improvements to Firefox Screenshots:
            Copy and paste screenshots directly to your clipboard
            Firefox Screenshots now works in Private Browsing mode
    
        Added Nepali (ne-NP) locale
    
        In case you missed it--57 Release privacy and performance feature:
          Users can enable Tracking Protection at all times. Learn how to turn
          Tracking Protection on.
    
    Fixed
        Fonts installed in non-standard directories will no longer appear
          blank for Linux users
    
        Various security fixes
    
    Changed
        User profiles created in Firefox 58 (and in future releases) are not
        supported in previous versions of Firefox. Users who downgrade to
        a previous version should create a new profile for that version.
        Learn about alternatives to downgrading on our support site.
    
        Added a warning to alert users and site owners of planned security
        changes to sites affected by the gradual distrust plan for
        the Symantec certificate authority
    
    #CVE-2018-5091: Use-after-free with DTMF timers
    #CVE-2018-5092: Use-after-free in Web Workers
    #CVE-2018-5093: Buffer overflow in WebAssembly during Memory/Table resizing
    #CVE-2018-5094: Buffer overflow in WebAssembly with garbage collection on
     uninitialized memory
    #CVE-2018-5095: Integer overflow in Skia library during edge builder allocation
    #CVE-2018-5097: Use-after-free when source document is manipulated during XSLT
    #CVE-2018-5098: Use-after-free while manipulating form input elements
    #CVE-2018-5099: Use-after-free with widget listener
    #CVE-2018-5100: Use-after-free when IsPotentiallyScrollable arguments are
     freed from memory
    #CVE-2018-5101: Use-after-free with floating first-letter style elements
    #CVE-2018-5102: Use-after-free in HTML media elements
    #CVE-2018-5103: Use-after-free during mouse event handling
    #CVE-2018-5104: Use-after-free during font face manipulation
    #CVE-2018-5105: WebExtensions can save and execute files on local file
     system without user prompts
    #CVE-2018-5106: Developer Tools can expose style editor information
     cross-origin through service worker
    #CVE-2018-5107: Printing process will follow symlinks for local file access
    #CVE-2018-5108: Manually entered blob URL can be accessed by subsequent
     private browsing tabs
    #CVE-2018-5109: Audio capture prompts and starts with incorrect origin
     attribution
    #CVE-2018-5110: Cursor can be made invisible on OS X
    #CVE-2018-5117: URL spoofing with right-to-left text aligned left-to-right
    #CVE-2018-5118: Activity Stream images can attempt to load local content
     through file:
    #CVE-2018-5119: Reader view will load cross-origin content in violation
     of CORS headers
    #CVE-2018-5121: OS X Tibetan characters render incompletely in the addressbar
    #CVE-2018-5122: Potential integer overflow in DoCrypt
    #CVE-2018-5090: Memory safety bugs fixed in Firefox 58
    #CVE-2018-5089: Memory safety bugs fixed in Firefox 58 and Firefox ESR 52.6

--- www/firefox/patches/patch-toolkit_components_protobuf_src_google_protobuf_stubs_atomicops.h
+++ www/firefox/patches/patch-toolkit_components_protobuf_src_google_protobuf_stubs_atomicops.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-toolkit_components_protobuf_src_google_protobuf_stubs_atomicops.h,v 1.3 2016/08/06 08:46:59 ryoon Exp $
+$NetBSD: patch-toolkit_components_protobuf_src_google_protobuf_stubs_atomicops.h,v 1.4 2018/01/24 16:52:08 ryoon Exp $
 
---- toolkit/components/protobuf/src/google/protobuf/stubs/atomicops.h.orig	2016-07-25 20:22:06.000000000 +0000
+--- toolkit/components/protobuf/src/google/protobuf/stubs/atomicops.h.orig	2018-01-11 20:17:03.000000000 +0000
 +++ toolkit/components/protobuf/src/google/protobuf/stubs/atomicops.h
-@@ -76,6 +76,11 @@ typedef intptr_t Atomic64;
+@@ -87,6 +87,11 @@ typedef intptr_t Atomic64;
  #endif
  #endif
  
@@ -13,8 +13,8 @@ $NetBSD: patch-toolkit_components_protobuf_src_google_protobuf_stubs_atomicops.h
 +#else
  // Use AtomicWord for a machine-sized pointer.  It will use the Atomic32 or
  // Atomic64 routines below, depending on your architecture.
- #if defined(__OpenBSD__) && !defined(GOOGLE_PROTOBUF_ARCH_64_BIT)
-@@ -83,6 +88,7 @@ typedef Atomic32 AtomicWord;
+ #if defined(__OpenBSD__) && !defined(GOOGLE_PROTOBUF_ARCH_64_BIT) /* See Bug 1192556 */
+@@ -94,6 +99,7 @@ typedef Atomic32 AtomicWord;
  #else
  typedef intptr_t AtomicWord;
  #endif
