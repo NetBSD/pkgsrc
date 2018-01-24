29a73284b204cc8d7ba2133206268ec34dbae534
adam <adam@pkgsrc.org>
2018-01-24 10:20:32 +0000

    py-raven: updated to 6.5.0
    6.5.0:
    [Core] Fixed missing deprecation on processors.SanitizePasswordsProcessor
    [Core] Improve exception handling in Serializer.transform
    [Core] Fixed celery.register_logger_signal ignoring subclasses
    [Core] Fixed sanitizer skipping byte instances
    [Lambda] Fixed AttributeError when requestContext not present

--- www/py-raven/patches/patch-setup.py
+++ www/py-raven/patches/patch-setup.py
@@ -1,8 +1,8 @@
-$NetBSD: patch-setup.py,v 1.2 2017/12/14 13:18:07 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2018/01/24 10:20:32 adam Exp $
 
 Relax requirements.
 
---- setup.py.orig	2017-12-11 18:59:44.000000000 +0000
+--- setup.py.orig	2018-01-17 21:06:54.000000000 +0000
 +++ setup.py
 @@ -60,19 +60,19 @@ tests_require = [
      'bottle',
@@ -15,18 +15,19 @@ Relax requirements.
      'nose',
      'pycodestyle',
      'pytz',
-     'pytest>=3.2.0,<3.3.0',
+-    'pytest>=3.2.0,<3.3.0',
 -    'pytest-timeout==1.2.0',
 -    'pytest-xdist==1.18.2',
 -    'pytest-pythonpath==0.7.1',
--    'pytest-sugar==0.8',
+-    'pytest-sugar==0.9.0',
++    'pytest>=3.2.0',
 +    'pytest-timeout>=1.2.0',
 +    'pytest-xdist>=1.18.2',
 +    'pytest-pythonpath>=0.7.1',
-+    'pytest-sugar>=0.8',
++    'pytest-sugar>=0.9.0',
      'pytest-cov',
--    'pytest-flake8==0.9',
-+    'pytest-flake8>=0.9',
+-    'pytest-flake8==0.9.1',
++    'pytest-flake8>=0.9.1',
      'requests',
      'tornado>=4.1',
      'webob',
