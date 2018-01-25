9ba9741ac32f926987c1c9ff8b3187c6fa7bfeda
tnn <tnn@pkgsrc.org>
2018-01-25 14:13:22 +0000

    synergy: update to 2.0.0
    changes unknown

--- net/synergy/patches/patch-tools_gtest-1.6.0_include_gtest_internal_gtest-port.h
+++ net/synergy/patches/patch-tools_gtest-1.6.0_include_gtest_internal_gtest-port.h
@@ -1,7 +1,7 @@
-$NetBSD: patch-tools_gtest-1.6.0_include_gtest_internal_gtest-port.h,v 1.2 2015/02/03 18:49:34 tnn Exp $
+$NetBSD: patch-tools_gtest-1.6.0_include_gtest_internal_gtest-port.h,v 1.3 2018/01/25 14:13:22 tnn Exp $
 
---- ext/gtest-1.6.0/include/gtest/internal/gtest-port.h.orig	2011-05-07 02:25:30.000000000 +0000
-+++ ext/gtest-1.6.0/include/gtest/internal/gtest-port.h
+--- ext/gtest/include/gtest/internal/gtest-port.h.orig	2011-05-07 02:25:30.000000000 +0000
++++ ext/gtest/include/gtest/internal/gtest-port.h
 @@ -481,6 +481,16 @@
  #  define BOOST_TR1_DETAIL_CONFIG_HPP_INCLUDED
  #  include <tuple>
