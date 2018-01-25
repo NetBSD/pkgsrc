312842c6ec9df72f3fd1f61ddf0b94e3cb360c38
maya <maya@pkgsrc.org>
2018-01-25 23:27:30 +0000

    gcc7: update to 7.3.0
    Local changes:
    Remove patch hunks merged upstream (non-c99 compatibility cabs workaround,
    linking to libc by default on netbsd).
    
    GCC changes:
    Added code generation options to mitigate Spectre variant 2 (CVE-2017-5715)
    for the powerpc and x86 targets.
    
    RTEMS: Support has been added for Epiphany target.
    
    List of bugs fixed:
    https://gcc.gnu.org/bugzilla/buglist.cgi?bug_status=RESOLVED&list_id=199567&resolution=FIXED&target_milestone=7.3

--- lang/gcc7/patches/patch-gcc_config_netbsd.h
+++ lang/gcc7/patches/patch-gcc_config_netbsd.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-gcc_config_netbsd.h,v 1.1 2017/05/03 00:21:03 maya Exp $
+$NetBSD: patch-gcc_config_netbsd.h,v 1.2 2018/01/25 23:27:30 maya Exp $
 
-when using shared, link against libc
-another unexplained change`
+Handle netbsd's compatibility non-C99 cabs (causes link
+failure with gfortran)
 
---- gcc/config/netbsd.h.orig	2014-01-02 22:23:26.000000000 +0000
+--- gcc/config/netbsd.h.orig	2017-09-29 21:34:00.000000000 +0000
 +++ gcc/config/netbsd.h
 @@ -17,6 +17,9 @@ You should have received a copy of the G
  along with GCC; see the file COPYING3.  If not see
@@ -15,29 +15,16 @@ another unexplained change`
  /* TARGET_OS_CPP_BUILTINS() common to all NetBSD targets.  */
  #define NETBSD_OS_CPP_BUILTINS_COMMON()		\
    do						\
-@@ -96,6 +99,7 @@ along with GCC; see the file COPYING3.  
-        %{!pg:-lposix}}		\
-      %{p:-lposix_p}		\
-      %{pg:-lposix_p}}		\
-+   %{shared:-lc}		\
-    %{!shared:			\
-      %{!symbolic:		\
-        %{!p:			\
-@@ -109,6 +113,7 @@ along with GCC; see the file COPYING3.  
-        %{!pg:-lposix}}		\
-      %{p:-lposix_p}		\
-      %{pg:-lposix_p}}		\
-+   %{shared:-lc}		\
-    %{!shared:			\
-      %{!symbolic:		\
-        %{!p:			\
-@@ -175,3 +180,9 @@ along with GCC; see the file COPYING3.  
- 
+@@ -181,6 +184,12 @@ along with GCC; see the file COPYING3.  
  #undef WINT_TYPE
  #define WINT_TYPE "int"
-+
+ 
 +#undef SUBTARGET_INIT_BUILTINS
 +#define SUBTARGET_INIT_BUILTINS			\
 +do {						\
 +  netbsd_patch_builtins ();			\
 +} while(0)
++
+ #undef  SUBTARGET_INIT_BUILTINS
+ #define SUBTARGET_INIT_BUILTINS						\
+   do {									\
