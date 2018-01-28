c1638964b0d35852c813aa1b6994c177af680bbb
wiz <wiz@pkgsrc.org>
2018-01-28 16:24:10 +0000

    recursive bump after mpfr shlib major bump

--- graphics/qore-glut-module/buildlink3.mk
+++ graphics/qore-glut-module/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/01/07 13:04:16 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/01/28 16:24:10 wiz Exp $
 #
 
 BUILDLINK_TREE+=	qore-glut-module
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	qore-glut-module
 QORE_GLUT_MODULE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qore-glut-module+=	qore-glut-module>=0.0.3
-BUILDLINK_ABI_DEPENDS.qore-glut-module?=	qore-glut-module>=0.0.3nb7
+BUILDLINK_ABI_DEPENDS.qore-glut-module?=	qore-glut-module>=0.0.3nb8
 BUILDLINK_PKGSRCDIR.qore-glut-module?=		../../graphics/qore-glut-module
 
 .include "../../graphics/qore-opengl-module/buildlink3.mk"
