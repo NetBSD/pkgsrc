551f77918def0faaf873b7ce2747ccd1f21344c2
markd <markd@pkgsrc.org>
2018-02-01 18:49:59 +0000

    prison: Add version 5.41.0
    A barcode API to produce QRCode barcodes and DataMatrix barcodes

---
+++ graphics/prison/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2018/02/01 18:49:59 markd Exp $
+
+BUILDLINK_TREE+=	prison
+
+.if !defined(PRISON_BUILDLINK3_MK)
+PRISON_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.prison+=	prison>=5.41.0
+BUILDLINK_PKGSRCDIR.prison?=	../../graphics/prison
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# PRISON_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-prison
