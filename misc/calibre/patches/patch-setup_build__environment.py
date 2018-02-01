c5663586358999213678e7c3c133d22b8e9b6381
wiz <wiz@pkgsrc.org>
2018-02-01 16:05:56 +0000

    calibre: update to 3.16.0.
    Lots of changes since 2.x, too many to list here.
    (2.x does not build against current qt5...)

--- misc/calibre/patches/patch-setup_build__environment.py
+++ misc/calibre/patches/patch-setup_build__environment.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-setup_build__environment.py,v 1.4 2017/02/17 15:43:43 wiz Exp $
+$NetBSD: patch-setup_build__environment.py,v 1.5 2018/02/01 16:05:56 wiz Exp $
 
 Always build like on Linux.
 
---- setup/build_environment.py.orig	2017-02-12 02:09:49.000000000 +0000
+--- setup/build_environment.py.orig	2017-08-25 03:47:17.000000000 +0000
 +++ setup/build_environment.py
-@@ -97,8 +97,8 @@ def get_sip_dir():
+@@ -93,8 +93,8 @@ def get_sip_dir():
  pyqt['pyqt_sip_dir'] = get_sip_dir()
  pyqt['sip_inc_dir'] = os.environ.get('SIP_INC_DIR', sysconfig.get_path('include'))
  
@@ -15,12 +15,3 @@ Always build like on Linux.
  qt_inc = pyqt['inc']
  qt_lib = pyqt['lib']
  ft_lib_dirs = []
-@@ -121,7 +121,7 @@ QT_DLLS = ['Qt5' + x for x in (
- 'WebKit', 'WebKitWidgets', 'Widgets',  'Multimedia', 'MultimediaWidgets', 'Xml',  # 'XmlPatterns',
- )]
- QT_PLUGINS = ('imageformats', 'audio', 'iconengines', 'mediaservice', 'platforms', 'playlistformats', 'printsupport', 'sqldrivers')
--if islinux or ishaiku:
-+if True:
-     # platformthemes cause crashes in Ubuntu
-     QT_PLUGINS += ('platforminputcontexts', 'generic',)
- 
