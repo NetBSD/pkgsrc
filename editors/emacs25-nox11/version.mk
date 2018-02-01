a3639f8c2dafe120a8c4b3048153d3e82361df27
jperkin <jperkin@pkgsrc.org>
2018-02-01 11:18:16 +0000

    emacs25-nox11: Fix _EMACS_VERSION_MINOR.

--- editors/emacs25-nox11/version.mk
+++ editors/emacs25-nox11/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.4 2017/04/29 07:11:56 wiz Exp $
+# $NetBSD: version.mk,v 1.5 2018/02/01 11:18:16 jperkin Exp $
 
 _EMACS_FLAVOR=	emacs
 _EMACS_REQD=	emacs25-nox11>=25.1<26
 
 _EMACS_VERSION_MAJOR=	25
-_EMACS_VERSION_MINOR=	2
+_EMACS_VERSION_MINOR=	3
