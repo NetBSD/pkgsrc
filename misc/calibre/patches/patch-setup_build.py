bc61760f539e12683672ba8e16cdc6c50897fa9e
wiz <wiz@pkgsrc.org>
2018-02-01 16:05:56 +0000

    calibre: update to 3.16.0.
    Lots of changes since 2.x, too many to list here.
    (2.x does not build against current qt5...)

--- misc/calibre/patches/patch-setup_build.py
+++ misc/calibre/patches/patch-setup_build.py
@@ -1,6 +1,5 @@
-$NetBSD: patch-setup_build.py,v 1.2 2016/09/29 12:13:43 joerg Exp $
+$NetBSD: patch-setup_build.py,v 1.3 2018/02/01 16:05:56 wiz Exp $
 
-Find libtool output correctly.
 Fix linking against native X.
 
 --- setup/build.py.orig	2016-09-29 10:28:31.000000000 +0000
@@ -21,12 +20,3 @@ Fix linking against native X.
          win32 {{
              LIBS += {py_lib}
              TARGET_EXT = .dll
-@@ -456,7 +458,7 @@ class Build(Command):
-         if iswindows:
-             qmc += ['-spec', qmakespec]
-         fext = 'dll' if iswindows else 'dylib' if isosx else 'so'
--        name = '%s%s.%s' % ('release/' if iswindows else 'lib', sip['target'], fext)
-+        name = '%s%s.%s' % ('release/' if iswindows else '.libs/lib', sip['target'], fext)
-         try:
-             os.chdir(src_dir)
-             if self.newer(dest, sip['headers'] + sip['sources'] + ext.sources + ext.headers):
