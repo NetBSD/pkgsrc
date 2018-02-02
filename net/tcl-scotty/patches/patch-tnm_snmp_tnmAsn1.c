9f5c1713b35f2a3c3b892862c615ac4cc4f4dd84
he <he@pkgsrc.org>
2018-02-02 13:55:28 +0000

    Add a patch which does minimal validation when decoding OIDs. Bump PKGREVISION.

--- net/tcl-scotty/patches/patch-tnm_snmp_tnmAsn1.c
+++ net/tcl-scotty/patches/patch-tnm_snmp_tnmAsn1.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-tnm_snmp_tnmAsn1.c,v 1.1 2014/03/05 13:52:29 he Exp $
+$NetBSD: patch-tnm_snmp_tnmAsn1.c,v 1.2 2018/02/02 13:55:29 he Exp $
 
 Constify.
+Provide minimal robustness against mis-coded OIDs.
 
 --- tnm/snmp/tnmAsn1.c.orig	1996-07-29 21:33:44.000000000 +0000
 +++ tnm/snmp/tnmAsn1.c
@@ -48,3 +49,15 @@ Constify.
      int convert = 0;
  
      if (! str) return NULL;
+@@ -914,6 +915,11 @@ Tnm_BerDecOID(packet, packetlen, oid, oi
+ 	strcpy(error, "OBJECT IDENTIFIER of length 0");
+ 	return NULL;
+     }
++    if (asnlen > TNM_OIDMAXLEN) {
++	sprintf(error, "OBJECT IDENTIFIER too long: %d, max is %d",
++		asnlen, TNM_OIDMAXLEN);
++	return NULL;
++    }
+     
+     if (asnlen == 1 && (*packet % 40 == *packet)) {
+ 	*oid       = *packet++;
