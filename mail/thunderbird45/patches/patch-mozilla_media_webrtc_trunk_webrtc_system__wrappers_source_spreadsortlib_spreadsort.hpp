1b2b291145ca965dc6cdf1814bf848beb0e653a1
rillig <rillig@pkgsrc.org>
2018-02-02 00:20:44 +0000

    mail/*: fix pkglint warnings

--- mail/thunderbird45/patches/patch-mozilla_media_webrtc_trunk_webrtc_system__wrappers_source_spreadsortlib_spreadsort.hpp
+++ mail/thunderbird45/patches/patch-mozilla_media_webrtc_trunk_webrtc_system__wrappers_source_spreadsortlib_spreadsort.hpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-mozilla_media_webrtc_trunk_webrtc_system__wrappers_source_spreadsortlib_spreadsort.hpp,v 1.1 2017/04/27 13:38:19 ryoon Exp $
+$NetBSD: patch-mozilla_media_webrtc_trunk_webrtc_system__wrappers_source_spreadsortlib_spreadsort.hpp,v 1.2 2018/02/02 00:20:45 rillig Exp $
 
 --- mozilla/media/webrtc/trunk/webrtc/system_wrappers/source/spreadsortlib/spreadsort.hpp.orig	2016-04-07 21:33:24.000000000 +0000
 +++ mozilla/media/webrtc/trunk/webrtc/system_wrappers/source/spreadsortlib/spreadsort.hpp
