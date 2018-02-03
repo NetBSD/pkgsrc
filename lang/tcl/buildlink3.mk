e508088e3be0d1ee6f458a2258dd536d701911a2
adam <adam@pkgsrc.org>
2018-02-03 17:51:19 +0000

    tcl: updated to 8.6.8
    8.6.8:
    [array names -regexp] supports backrefs
    Fix gcc build failures due to #pragma placement
    (bug)[b50fb2] exec redir append stdout and stderr to file
    (bug)[2a9465] http state 100 continue handling broken
    (bug)[0e4d88] replace command, delete trace kills namespace
    (bug)[1a5655] [info * methods] includes mixins
    tzdata updated to Olson's tzdata2017c
    (bug)[fc1409] segfault in method cloning, oo-15.15
    (bug)[6f2f83] More robust [load] for ReactOS
    (bug)[3298012] Stop crash when hash tables overflow 32 bits
    (bug)[5d6de6] Close failing case of [package prefer stable]
    (bug)[fab924] Fix misleading [load] message on Windows
    (bug)[4f6a1e] Crash when ensemble map and list are same
    (bug)[ce3a21] file normalize failure when tail is empty
    (new)[TIP 477] nmake build system reform
    (bug)[586e71] EvalObjv exception handling at level 0

--- lang/tcl/buildlink3.mk
+++ lang/tcl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.29 2016/10/21 11:45:44 kamil Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2018/02/03 17:51:19 adam Exp $
 
 BUILDLINK_TREE+=	tcl
 
@@ -22,8 +22,8 @@ _TOOLS_USE_PKGSRC.tclsh=	yes
 TCLCONFIG_SH?=		${BUILDLINK_PREFIX.tcl}/lib/tclConfig.sh
 TCLSH=			${BUILDLINK_PREFIX.tcl}/bin/tclsh
 # Bundled library
-ITCL_VERSION=		4.0.5
-TDBC_VERSION=		1.0.4
+ITCL_VERSION=		4.1.1
+TDBC_VERSION=		1.0.6
 
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../mk/dlopen.buildlink3.mk"
--- lang/tcl/patches/patch-unix_tclUnixPort.h
+++ lang/tcl/patches/patch-unix_tclUnixPort.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-unix_tclUnixPort.h,v 1.4 2016/03/06 11:47:49 adam Exp $
+$NetBSD: patch-unix_tclUnixPort.h,v 1.5 2018/02/03 17:51:19 adam Exp $
 
---- unix/tclUnixPort.h.orig	2016-02-25 20:12:38.000000000 +0000
+--- unix/tclUnixPort.h.orig	2017-10-13 13:41:43.000000000 +0000
 +++ unix/tclUnixPort.h
 @@ -145,11 +145,7 @@ typedef off_t		Tcl_SeekOffset;
- #if HAVE_STDINT_H
+ #ifdef HAVE_STDINT_H
  #   include <stdint.h>
  #endif
 -#ifdef HAVE_UNISTD_H
