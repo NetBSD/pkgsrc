58e3b6d06dec718bb415e72d3a12170239ec9f00
ryoon <ryoon@pkgsrc.org>
2018-02-03 00:06:29 +0000

    converters/libqxp: import libqxp-0.0.1
    libqxp is a library and a set of tools for reading and converting
    QuarkXPress file format. It supports versions 3.1-4.1 currently.

---
+++ converters/libqxp/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2018/02/03 00:06:29 ryoon Exp $
+
+BUILDLINK_TREE+=	libqxp
+
+.if !defined(LIBQXP_BUILDLINK3_MK)
+LIBQXP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libqxp+=	libqxp>=0.0.1
+BUILDLINK_PKGSRCDIR.libqxp?=	../../converters/libqxp
+
+.include "../../converters/librevenge/buildlink3.mk"
+.endif	# LIBQXP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libqxp
