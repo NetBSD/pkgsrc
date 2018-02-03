28523672e44be08e7cffc1cd2c39cde31e770faa
ryoon <ryoon@pkgsrc.org>
2018-02-03 00:39:50 +0000

    Update to 6.0.0.3
    Changelog:
    Many improvements including:
    In Writer:
    * Improved input fields
    * Support images rotation
    * As mail merge source
    * New table styles
    * "Grammar by" spell checking
    
    In Calc:
    * Improvement of pivot table
    * Export cell range to PNG or JPEG
    * Pasting unformatted text
    * Protection of cells and sheets
    * Some new spreadsheet functions
    
    In Impress and Draw
    * Add new Impress templates
    * New layers in draw operations

--- misc/libreoffice/patches/patch-sdext_source_pdfimport_xpdfwrapper_pdfioutdev__gpl.cxx
+++ misc/libreoffice/patches/patch-sdext_source_pdfimport_xpdfwrapper_pdfioutdev__gpl.cxx
@@ -1,19 +1,19 @@
-$NetBSD: patch-sdext_source_pdfimport_xpdfwrapper_pdfioutdev__gpl.cxx,v 1.1 2018/01/11 12:52:05 ryoon Exp $
+$NetBSD: patch-sdext_source_pdfimport_xpdfwrapper_pdfioutdev__gpl.cxx,v 1.2 2018/02/03 00:39:50 ryoon Exp $
 
 * Fix build with poppler-0.62.0
 
---- sdext/source/pdfimport/xpdfwrapper/pdfioutdev_gpl.cxx.orig	2017-12-12 17:45:07.000000000 +0000
+--- sdext/source/pdfimport/xpdfwrapper/pdfioutdev_gpl.cxx.orig	2018-01-24 20:31:03.000000000 +0000
 +++ sdext/source/pdfimport/xpdfwrapper/pdfioutdev_gpl.cxx
 @@ -36,7 +36,7 @@
  // FIXME: we can't use #if POPPLER_CHECK_VERSION(0, 21, 0) && !POPPLER_CHECK_VERSION(0, 21, 1)
  //        because the internal poppler does not provide poppler-version.h and the macro always returns 0
  #if POPPLER_CHECK_VERSION(0, 21, 1)
--#include "UTF8.h"
-+#include "UnicodeMapFuncs.h"
+-#include <UTF8.h>
++#include <UnicodeMapFuncs.h>
  #elif POPPLER_CHECK_VERSION(0, 21, 0)
  #include "UTF.h"
  #else
-@@ -918,7 +918,7 @@ void PDFOutDev::drawChar(GfxState *state
+@@ -913,7 +913,7 @@ void PDFOutDev::drawChar(GfxState *state
              );
  
      // silence spurious warning
