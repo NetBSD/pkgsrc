a3f631d92de671bbd305e969aa3193d47c1f3f70
ryoon <ryoon@pkgsrc.org>
2018-02-03 00:39:50 +0000

    Update to 6.0.0.3
    Changelog:
    Many improvements including:
    In Writer:
    * Improved input fields
    * Support images rotation
    * As mail merge source
    * New table styles
    * "Grammar by" spell checking
    
    In Calc:
    * Improvement of pivot table
    * Export cell range to PNG or JPEG
    * Pasting unformatted text
    * Protection of cells and sheets
    * Some new spreadsheet functions
    
    In Impress and Draw
    * Add new Impress templates
    * New layers in draw operations

--- misc/libreoffice/patches/patch-mysqlc_Library__mysqlc.mk
+++ misc/libreoffice/patches/patch-mysqlc_Library__mysqlc.mk
@@ -1,10 +1,10 @@
-$NetBSD: patch-mysqlc_Library__mysqlc.mk,v 1.1 2016/12/06 15:21:00 ryoon Exp $
+$NetBSD: patch-mysqlc_Library__mysqlc.mk,v 1.2 2018/02/03 00:39:50 ryoon Exp $
 
---- mysqlc/Library_mysqlc.mk.orig	2016-07-28 22:11:19.000000000 +0000
+--- mysqlc/Library_mysqlc.mk.orig	2018-01-24 20:31:03.000000000 +0000
 +++ mysqlc/Library_mysqlc.mk
 @@ -16,7 +16,7 @@ $(eval $(call gb_Library_use_externals,m
  
- ifeq ($(SYSTEM_MYSQL_CPPCONN),)
+ ifeq ($(SYSTEM_MYSQL_CONNECTOR_CPP),)
  $(eval $(call gb_Library_add_libs,mysqlc,\
 -	$(if $(filter-out WNT,$(OS)),$(if $(filter MACOSX SOLARIS,$(OS)),-lz -lm,\
 +	$(if $(filter-out WNT,$(OS)),$(if $(filter NETBSD MACOSX SOLARIS,$(OS)),-lz -lm,\
