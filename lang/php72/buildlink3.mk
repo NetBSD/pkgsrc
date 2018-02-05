065ea9cc27f003669bd4f34539f61c7796f4426e
jdolecek <jdolecek@pkgsrc.org>
2018-02-05 08:51:25 +0000

    Switch lang/php72 to use pkgsrc pcre library instead of the bundled one
    PHP bundles it's own copy of pcre 8.41 with the distribution, which
    is used when build doesn't find system one. Switch to the pkgsrc
    pcre, so that we don't need to maintain local PHP JIT patch here.
    Upcoming PHP 7.3 already switched to pcre2, so no value maintaining
    private PCRE patch any more (if there ever was).
    
    XXX devel/pcre doesn't seem to enable PCRE JIT, might be good to do so

--- lang/php72/buildlink3.mk
+++ lang/php72/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/11/15 08:56:12 jdolecek Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/02/05 08:51:25 jdolecek Exp $
 
 BUILDLINK_TREE+=	php
 
 .if !defined(PHP_BUILDLINK3_MK)
 PHP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.php+=	php>=7.2.0rc1<7.3
-BUILDLINK_ABI_DEPENDS.php+=	php>=7.2.0rc1<7.3
+BUILDLINK_API_DEPENDS.php+=	php>=7.2.2nb2<7.3
+BUILDLINK_ABI_DEPENDS.php+=	php>=7.2.2nb2<7.3
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php72
 
 .include "../../textproc/libxml2/buildlink3.mk"
