a45918056888810111c79c8dbaa8ff0ac6cd3a5c
jperkin <jperkin@pkgsrc.org>
2018-02-05 11:21:56 +0000

    php7*: Standardise on major.minor.99 usage in buildlink3.mk
    With the introduction of beta and rc releases of php7* into pkgsrc the pattern
    matching is often incorrect (for example the current version of php-7.1.0rc6
    breaks both <7.1.0 and >=7.1.0).  Using .99 is not ideal but does at least
    avoid the confusion developers seem to be having with the pmatch ordering.

--- lang/php70/buildlink3.mk
+++ lang/php70/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2017/09/26 14:02:32 roy Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/02/05 11:21:56 jperkin Exp $
 
 BUILDLINK_TREE+=	php
 
 .if !defined(PHP_BUILDLINK3_MK)
 PHP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.php+=	php>=7.0.0<7.1.0
-BUILDLINK_ABI_DEPENDS.php+=	php>=7.0.0<7.1.0
+BUILDLINK_API_DEPENDS.php+=	php>=7.0.0<7.0.99
+BUILDLINK_ABI_DEPENDS.php+=	php>=7.0.0<7.0.99
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php70
 
 .include "../../textproc/libxml2/buildlink3.mk"
--- lang/php71/buildlink3.mk
+++ lang/php71/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2017/11/26 19:14:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/02/05 11:21:56 jperkin Exp $
 
 BUILDLINK_TREE+=	php
 
 .if !defined(PHP_BUILDLINK3_MK)
 PHP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.php+=	php>=7.1.0<7.1.99
-BUILDLINK_ABI_DEPENDS.php+=	php>=7.1.0<7.1.99
+BUILDLINK_API_DEPENDS.php+=	php>=7.0.99<7.1.99
+BUILDLINK_ABI_DEPENDS.php+=	php>=7.0.99<7.1.99
 BUILDLINK_PKGSRCDIR.php?=	../../lang/php71
 
 .include "../../textproc/libxml2/buildlink3.mk"
