329952f9b9f3dea124d06970bb36a315478b866d
joerg <joerg@pkgsrc.org>
2018-02-07 12:10:21 +0000

    Belatedly bump requirement. 4.0 and 5.0 are not API (nor ABI) compatible.

--- lang/clang/buildlink3.mk
+++ lang/clang/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2017/07/11 19:47:37 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2018/02/07 12:10:21 joerg Exp $
 
 BUILDLINK_TREE+=	clang
 
 .if !defined(CLANG_BUILDLINK3_MK)
 CLANG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clang+=	clang>=4.0.1
+BUILDLINK_API_DEPENDS.clang+=	clang>=5.0
 BUILDLINK_PKGSRCDIR.clang?=	../../lang/clang
 
 .include "../../textproc/libxml2/buildlink3.mk"
