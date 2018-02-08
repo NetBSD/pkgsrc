e49add34e85191fdbb9fefd9b23d38dd7ed2f41c
mrg <mrg@pkgsrc.org>
2018-02-08 10:50:44 +0000

    instead of removing "struct tm;", include <ctime>.  also, the file is called date_object.h.  fixes GCC 6 build.

--- x11/kdelibs4/patches/patch-kjs_date_object.h
+++ x11/kdelibs4/patches/patch-kjs_date_object.h
@@ -1,15 +1,16 @@
-$NetBSD: patch-kjs_date__object.h,v 1.1 2018/01/31 11:33:22 jperkin Exp $
+$NetBSD: patch-kjs_date_object.h,v 1.1 2018/02/08 10:50:44 mrg Exp $
 
-Remove bogus forward declaration.
+Replace bogus forward declaration with <ctime> include.
 
 --- kjs/date_object.h.orig	2017-11-05 01:51:22.000000000 +0000
 +++ kjs/date_object.h
-@@ -24,8 +24,6 @@
+@@ -24,8 +24,8 @@
  #include "function.h"
  #include "JSWrapperObject.h"
  
 -struct tm;
--
++#include <ctime>
+ 
  namespace KJS {
  
      class FunctionPrototype;
