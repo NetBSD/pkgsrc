923aad507688a95b4df9e41df144577c9b28ecc0
adam <adam@pkgsrc.org>
2018-02-09 08:44:50 +0000

    py-m2crypto: updated to 0.28.2
    0.28.0:
    - Mainly port to Python 3 (supporting 2.6, 2.7, 3.3, 3.4, 3.5, 3.6)
    - Some lame efforts to make setup.py build --openssl work better (needs
      more real testing on Mac OS X)
    - Fix licence: it is MIT, not BSD
    - Fix and add tests for SWIG/_aes.i module
    - Improve somehow situation on Mac OS X (some testing, improve setup.py,
      testsuite should fully pass)
    - Bundle-in unittest2 for Python 2.6 (dealing with the need for
      specific version of unittest2 package was too complicated)
    - Remove all PGP modules

--- security/py-m2crypto/patches/patch-setup.py
+++ security/py-m2crypto/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.3 2017/02/19 18:54:39 adam Exp $
+$NetBSD: patch-setup.py,v 1.4 2018/02/09 08:44:50 adam Exp $
 
 Don't pass "-includeall" to swig.  It causes compiler-dependent
 tests in system include files to break (e.g. "#ifdef __GNUC__"
 doesn't work properly.)
 
---- setup.py.orig	2016-07-22 12:43:17.000000000 +0000
+--- setup.py.orig	2018-02-08 18:54:48.000000000 +0000
 +++ setup.py
-@@ -112,7 +112,6 @@ class _M2CryptoBuildExt(build_ext.build_
-             self.swig_opts.append('-D%s' % arch)
+@@ -192,7 +192,6 @@ class _M2CryptoBuildExt(build_ext.build_
  
-         self.swig_opts.extend(['-I%s' % i for i in self.include_dirs])
+         # swig seems to need the default header file directories
+         self.swig_opts.extend(['-I%s' % i for i in _get_additional_includes()])
 -        self.swig_opts.append('-includeall')
          self.swig_opts.append('-modern')
          self.swig_opts.append('-builtin')
