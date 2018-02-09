f9f6af31ad889e43bd9597106d58ccb6b3392108
adam <adam@pkgsrc.org>
2018-02-09 08:44:50 +0000

    py-m2crypto: updated to 0.28.2
    0.28.0:
    - Mainly port to Python 3 (supporting 2.6, 2.7, 3.3, 3.4, 3.5, 3.6)
    - Some lame efforts to make setup.py build --openssl work better (needs
      more real testing on Mac OS X)
    - Fix licence: it is MIT, not BSD
    - Fix and add tests for SWIG/_aes.i module
    - Improve somehow situation on Mac OS X (some testing, improve setup.py,
      testsuite should fully pass)
    - Bundle-in unittest2 for Python 2.6 (dealing with the need for
      specific version of unittest2 package was too complicated)
    - Remove all PGP modules

--- security/py-m2crypto/patches/patch-SWIG___m2crypto__wrap.c
+++ security/py-m2crypto/patches/patch-SWIG___m2crypto__wrap.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-SWIG___m2crypto__wrap.c,v 1.1 2017/02/19 18:54:39 adam Exp $
+$NetBSD: patch-SWIG___m2crypto__wrap.c,v 1.2 2018/02/09 08:44:50 adam Exp $
 
 Avoid conflict with hmac() definition from stdlib.h (NetBSD).
 
---- SWIG/_m2crypto_wrap.c.orig	2017-02-19 18:35:59.000000000 +0000
+--- SWIG/_m2crypto_wrap.c.orig	2018-02-08 18:54:48.000000000 +0000
 +++ SWIG/_m2crypto_wrap.c
-@@ -5050,7 +5050,7 @@ PyObject *hmac_final(HMAC_CTX *ctx) {
+@@ -5949,7 +5949,7 @@ PyObject *hmac_final(HMAC_CTX *ctx) {
      return ret;
  }
  
@@ -12,8 +12,8 @@ Avoid conflict with hmac() definition from stdlib.h (NetBSD).
 +PyObject *hmac_func(PyObject *key, PyObject *data, const EVP_MD *md) {
      const void *kbuf, *dbuf;
      void *blob;
-     int klen;
-@@ -12676,7 +12676,7 @@ SWIGINTERN PyObject *_wrap_hmac(PyObject
+     int klen = 0;
+@@ -13610,7 +13610,7 @@ SWIGINTERN PyObject *_wrap_hmac(PyObject
        SWIG_exception(SWIG_ValueError,"Received a NULL pointer.");
      }
    }
