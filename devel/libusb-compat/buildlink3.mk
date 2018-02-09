6a42f7d47f3eb379b049101367956a9aaed31c48
khorben <khorben@pkgsrc.org>
2018-02-09 23:54:53 +0000

    Register missing dependency on devel/libusb1

--- devel/libusb-compat/buildlink3.mk
+++ devel/libusb-compat/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/01/23 14:31:45 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/02/09 23:54:53 khorben Exp $
 
 BUILDLINK_TREE+=	libusb-compat
 
@@ -6,8 +6,10 @@ BUILDLINK_TREE+=	libusb-compat
 LIBUSB_COMPAT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libusb-compat+=	libusb-compat>=0.1.6rc2
+BUILDLINK_ABI_DEPENDS.libusb-compat+=	libusb-compat>=0.1.6rc2
 BUILDLINK_PKGSRCDIR.libusb-compat?=	../../devel/libusb-compat
 
+.include "../../devel/libusb1/buildlink3.mk"
 .endif	# LIBUSB_COMPAT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libusb-compat
