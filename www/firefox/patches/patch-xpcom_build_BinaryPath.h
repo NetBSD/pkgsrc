e90892ceb3590a2bfc6c8bcee7653b059fa48817
ryoon <ryoon@pkgsrc.org>
2018-02-10 07:02:47 +0000

    Update to 58.0.2
    * Fix segfault on netbsd-7
    
    Changelog:
    Fix
        Avoid a signature validation issue during update on macOS
    
        Blocklisted graphics drivers related to off main thread painting crashes
    
        Tab crash during printing
    
        Fix clicking links and scrolling emails on Microsoft Hotmail and Outlook
          (OWA) webmail

--- www/firefox/patches/patch-xpcom_build_BinaryPath.h
+++ www/firefox/patches/patch-xpcom_build_BinaryPath.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-xpcom_build_BinaryPath.h,v 1.3 2018/01/31 14:02:18 ryoon Exp $
+$NetBSD: patch-xpcom_build_BinaryPath.h,v 1.4 2018/02/10 07:02:47 ryoon Exp $
 
 * Fix build under netbsd-7, PR pkg/52956
 
@@ -24,13 +24,17 @@ $NetBSD: patch-xpcom_build_BinaryPath.h,v 1.3 2018/01/31 14:02:18 ryoon Exp $
    static nsresult Get(char aResult[MAXPATHLEN])
    {
      int mib[4];
-@@ -195,7 +197,8 @@ private:
-     return NS_OK;
+@@ -257,6 +259,13 @@ private:
+     return NS_ERROR_FAILURE;
    }
  
--#elif defined(__OpenBSD__)
-+#elif defined(__OpenBSD__) || \
-+      (defined(__NetBSD__) && !defined(KERN_PROC_PATHNAME))
-   static nsresult Get(char aResult[MAXPATHLEN])
-   {
-     int mib[4];
++#elif (defined(__NetBSD__) && !defined(KERN_PROC_PATHNAME))
++  static nsresult Get(char aResult[MAXPATHLEN])
++  {
++    char path[] = "@PREFIX@/lib/firefox/firefox";
++    sprintf(aResult, "%s", path);
++    return NS_OK;
++  }
+ #else
+ #error Oops, you need platform-specific code here
+ #endif
