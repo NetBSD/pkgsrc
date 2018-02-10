8be8819156643b4539320837d436d225812cb9b3
khorben <khorben@pkgsrc.org>
2018-02-10 13:53:46 +0000

    Import global switch for libusb's implementation [2/2]
    This switch is meant to be used by packages requiring an implementation of the
    former libusb (as in devel/libusb). The original implementation can be
    chosen by setting LIBUSB_TYPE to "native".
    
    The alternative implementation libusb-compat (as in devel/libusb-compat) wraps
    libusb1 (in devel/libusb1). This implementation can be chosen by setting
    LIBUSB_TYPE to "compat". On NetBSD, it has the advantage of not requiring root
    privileges to locate and use USB devices without a kernel driver.
    
    This second part switches packages using libusb to this framework. It does not
    change compilation options or dependencies at this point.
    
    Compile-tested on most packages affected and available on NetBSD/amd64.

--- comms/obexftp/buildlink3.mk
+++ comms/obexftp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/03/15 08:18:43 plunky Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/02/10 13:53:47 khorben Exp $
 
 BUILDLINK_TREE+=	obexftp
 
@@ -10,7 +10,7 @@ BUILDLINK_PKGSRCDIR.obexftp?=	../../comms/obexftp
 
 .include "../../comms/openobex/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
-.include "../../devel/libusb/buildlink3.mk"
+.include "../../mk/libusb.buildlink3.mk"
 .endif # OBEXFTP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-obexftp
