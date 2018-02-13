8437aba8a27b9a3fff990bdc2b2098bc49183f8a
adam <adam@pkgsrc.org>
2018-02-13 08:34:03 +0000

    py-uwsgi: updated to 2.0.16
    uWSGI 2.0.16
    Security
    [CVE-2018-6758] Stack-based buffer overflow in core/utils.c:uwsgi_expand_path()
    
    Changes
    Backported early_post_jail plugin hook (Bjørnar Ness)
    Fixed ipv6 suupport for http-socket (James Brown)
    Enable execinfo on DragonFly BSD (Aaron LI)
    Fix inet_ntop buffer size (Orivej Desh)
    Add worker running time metrics (Serge/yasek)
    Backported safe-pidfile, safe-pidfile2 (Nate Coraor)
    Stop using libxml2 by default on osx
    Fixed uwsgi_kvlist_parse signature
    Backport http range fixes from master (Curtis Maloney, Sokolov Yura)
    relicensed mod_proxy_uwsgi to Apache 2.0
    logging: Add ${millis} support to json encode
    plugins/router_xmldir: fixup invalid locale check (Riccardo Magliocchetti)
    Add ssl-verify-depth flag to set the max Client CA chain length (Paul Tagliamonte)
    Allow to override build date (Bernhard M. Wiedemann)
    Python 3 plugin: improved thread names handling (Jyrki Muukkonen, Mark Meyer)
    Added uwsgi_resolve_ip for redis host (ahmbas)
    plugins/gevent: Fix signal handlers (Maslov Alexander)
    Write x509 DER to the uwsgi buffer (Paul Tagliamonte)
    plugin/http: Fix compilation (Melvyn Sopacua)
    Fixed emperor throttling system (Jeremy Hiatt)
    Fix application loading without Plack after excluding “.” from @INC in new Perl versions (Anton Petrusevich)
    Fix MULE MSG QUEUE IS FULL message hint (Eugene Tataurov)
    Build System: support k_minor has a _xxx suffix (TOGO Li)
    Fixed drop-after-* options (Robert DeRose)
    Add mule_send_msg success indicator (Josh Tiras)
    Properly check item size in uwsgi_queue_push (Josh Tiras)
    FastRouter / HTTP Router can now have a ‘fallback’ key configured
    HTTP Router now supports post-buffer, just like FastRouter
    Fix handling of env in embedded dict in Python plugin (could cause segfaults in single thread mode)
    Add support for Brotli (.br) with –static-gzip
    Back-ported HTTP/1.1 support (–http11-socket) from 2.1

--- www/py-uwsgi/options.mk
+++ www/py-uwsgi/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2014/01/23 14:47:05 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2018/02/13 08:34:03 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-uwsgi
-PKG_SUPPORTED_OPTIONS=	debug openssl pcre uuid yaml zlib
-PKG_SUGGESTED_OPTIONS+=	libxml2 openssl zlib
+PKG_SUPPORTED_OPTIONS=	debug uuid yaml
+PKG_SUGGESTED_OPTIONS+=	libxml2
 
 PKG_OPTIONS_OPTIONAL_GROUPS+=	json xml
 PKG_OPTIONS_GROUP.json=		jansson yajl
@@ -10,56 +10,40 @@ PKG_OPTIONS_GROUP.xml=		expat libxml2
 
 .include "../../mk/bsd.options.mk"
 
-UWSGI_DEBUG=			debug=false;
+UWSGI_DEBUG=			debug=false
 .if !empty(PKG_OPTIONS:Mdebug)
-UWSGI_DEBUG=			debug=true;
+UWSGI_DEBUG=			debug=true
 .endif
 
-UWSGI_XML=			xml=auto;
+UWSGI_XML=			xml=auto
 .if !empty(PKG_OPTIONS:Mexpat)
 .include "../../textproc/expat/buildlink3.mk"
-UWSGI_XML=			xml=expat;
+UWSGI_XML=			xml=expat
 .elif !empty(PKG_OPTIONS:Mlibxml2)
 .include "../../textproc/libxml2/buildlink3.mk"
-UWSGI_XML=			xml=libxml2;
+UWSGI_XML=			xml=libxml2
 .else
-UWSGI_XML=			xml=false;
+UWSGI_XML=			xml=false
 .endif
 
-UWSGI_JSON=			json=auto;
+UWSGI_JSON=			json=auto
 .if !empty(PKG_OPTIONS:Mjansson)
 .include "../../textproc/jansson/buildlink3.mk"
-UWSGI_JSON=			json=jansson;
+UWSGI_JSON=			json=jansson
 .elif !empty(PKG_OPTIONS:Myajl)
 .include "../../devel/yajl/buildlink3.mk"
-UWSGI_JSON=			json=yajl;
+UWSGI_JSON=			json=yajl
 BROKEN=		The yajl option requires a yajl.pc file which that package doesn't have.
 .else
-UWSGI_JSON=			json=false;
+UWSGI_JSON=			json=false
 .endif
 
-UWSGI_SSL=			ssl=auto;
-.if !empty(PKG_OPTIONS:Mopenssl)
-.include "../../security/openssl/buildlink3.mk"
-UWSGI_SSL=			ssl=true;
-.else
-UWSGI_SSL=			ssl=false;
-.endif
-
-UWSGI_PCRE=			pcre=auto;
-.if !empty(PKG_OPTIONS:Mpcre)
-.include "../../devel/pcre/buildlink3.mk"
-UWSGI_PCRE=			pcre=true;
-.else
-UWSGI_PCRE=			pcre=false;
-.endif
-
-UWSGI_UUID=			uuid=auto;
+UWSGI_UUID=			uuid=auto
 .if !empty(PKG_OPTIONS:Muuid)
 .include "../../devel/libuuid/buildlink3.mk"
-UWSGI_UUID=			uuid=true;
+UWSGI_UUID=			uuid=true
 .else
-UWSGI_UUID=			uuid=false;
+UWSGI_UUID=			uuid=false
 .endif
 
 UWSGI_YAML=			yaml=auto
@@ -68,8 +52,4 @@ UWSGI_YAML=			yaml=auto
 UWSGI_YAML=			yaml=true
 .else
 UWSGI_YAML=			yaml=false
-.endif
-
-.if !empty(PKG_OPTIONS:Mzlib)
-.include "../../devel/zlib/buildlink3.mk"
 .endif
