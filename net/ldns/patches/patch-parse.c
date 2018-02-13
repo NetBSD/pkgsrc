80a6c715fcdfc0bdb83894f3112f500968183b51
he <he@pkgsrc.org>
2018-02-13 14:59:14 +0000

    Apply fix for CVE-2017-10002, via https://git.nlnetlabs.nl/ldns/commit/?id=c8391790 Bump PKGREVISION.

---
+++ net/ldns/patches/patch-parse.c
@@ -0,0 +1,18 @@
+$NetBSD: patch-parse.c,v 1.1 2018/02/13 14:59:15 he Exp $
+
+Apply fix for CVE-2017-10002, via
+https://git.nlnetlabs.nl/ldns/commit/?id=c8391790
+
+--- parse.c.orig	2014-01-10 21:04:41.000000000 +0000
++++ parse.c
+@@ -118,6 +118,10 @@ ldns_fget_token_l(FILE *f, char *token, 
+ 			if (line_nr) {
+ 				*line_nr = *line_nr + 1;
+ 			}
++			if (limit > 0 && (i >= limit || (size_t)(t-token) >= limit)) {
++				*t = '\0';
++				return -1;
++			}
+ 			*t++ = ' ';
+ 			prev_c = c;
+ 			continue;
