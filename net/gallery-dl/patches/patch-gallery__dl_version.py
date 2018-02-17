f12eb96c8c771099c6a923429e89f9fadc65e3d0
leot <leot@pkgsrc.org>
2018-02-17 15:52:43 +0000

    gallery-dl: Update net/gallery-dl to 1.2.0
    Changes:
    1.2.0
    -----
     * Added support for:
       - `paheal` (#69)
       - `komikcast` (#70)
       - `subapics` (#70)
     * Added `--download-archive` to record downloaded files in an archive file
     * Added `--write-log` to write logging output to a file
     * Added a filetype check on download completion to fix incorrectly assigned filename extensions (#63)
     * Added the `tumblr:...` pseudo URI scheme to support custom domains for Tumblr blogs (#71)
     * Added fallback URLs for `tumblr` images (#64)
     * Added support for `reddit`-hosted images (#68)
     * Improved the input file format by allowing comments and per-URL options
     * Fixed OAuth 1.0 signature generation for Python 3.3 and 3.4 (#75)
     * Fixed smaller issues for `luscious`, `hentai2read`, `hentaihere` and `imgur`
     * Removed the `batoto` module

--- net/gallery-dl/patches/patch-gallery__dl_version.py
+++ net/gallery-dl/patches/patch-gallery__dl_version.py
@@ -1,14 +1,14 @@
-$NetBSD: patch-gallery__dl_version.py,v 1.1 2017/09/04 09:35:26 leot Exp $
+$NetBSD: patch-gallery__dl_version.py,v 1.2 2018/02/17 15:52:43 leot Exp $
 
 Avoid a UnicodeDecodeError.
 
---- gallery_dl/version.py.orig	2017-05-01 11:24:12.000000000 +0000
+--- gallery_dl/version.py.orig	2018-02-16 21:28:15.000000000 +0000
 +++ gallery_dl/version.py
 @@ -1,6 +1,6 @@
  # -*- coding: utf-8 -*-
  
--# Copyright 2016-2017 Mike Fährmann
-+# Copyright 2016-2017 Mike Fahrmann
+-# Copyright 2016-2018 Mike Fährmann
++# Copyright 2016-2018 Mike Fahrmann
  #
  # This program is free software; you can redistribute it and/or modify
  # it under the terms of the GNU General Public License version 2 as
