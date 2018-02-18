fe5e172e09914eb785cf7539b61fcdf0192d233b
jmcneill <jmcneill@pkgsrc.org>
2018-02-18 12:27:20 +0000

    u-boot-pinebook: update to 2018.01

--- sysutils/u-boot-pinebook/patches/patch-include_configs_sunxi-common.h
+++ sysutils/u-boot-pinebook/patches/patch-include_configs_sunxi-common.h
@@ -1,19 +1,15 @@
-$NetBSD: patch-include_configs_sunxi-common.h,v 1.1 2017/09/10 17:00:41 jmcneill Exp $
+$NetBSD: patch-include_configs_sunxi-common.h,v 1.2 2018/02/18 12:27:20 jmcneill Exp $
 
-sunxi: setup simplefb for Allwinner DE2
-
-Obtained from https://patchwork.ozlabs.org/patch/759922/
-
---- include/configs/sunxi-common.h.orig	2017-07-10 17:07:38.000000000 +0000
+--- include/configs/sunxi-common.h.orig	2018-01-23 06:50:34.000000000 +0000
 +++ include/configs/sunxi-common.h
-@@ -293,6 +293,10 @@ extern int soft_i2c_gpio_scl;
+@@ -284,6 +284,10 @@ extern int soft_i2c_gpio_scl;
  
- #endif /* CONFIG_VIDEO */
+ #endif /* CONFIG_VIDEO_SUNXI */
  
 +#ifdef CONFIG_VIDEO_DE2
 +#define CONFIG_VIDEO_DT_SIMPLEFB
 +#endif
 +
  /* Ethernet support */
- #ifdef CONFIG_SUNXI_EMAC
+ #ifdef CONFIG_SUN4I_EMAC
  #define CONFIG_PHY_ADDR		1
