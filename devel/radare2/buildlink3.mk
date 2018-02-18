a5ae52779e52eda58151800159325c1d9799e7b6
kamil <kamil@pkgsrc.org>
2018-02-18 23:17:54 +0000

    radare2: Update buildlink3.mk
    Update API and ABI requirement to 2.3.0.
    Require capstone, file, libzip.

--- devel/radare2/buildlink3.mk
+++ devel/radare2/buildlink3.mk
@@ -1,14 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/07/19 16:02:51 kamil Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/02/18 23:17:54 kamil Exp $
 
 BUILDLINK_TREE+=	radare2
 
 .if !defined(RADARE2_BUILDLINK3_MK)
 RADARE2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.radare2+=	radare2>=0.9.9
-BUILDLINK_ABI_DEPENDS.radare2?=	radare2>=0.9.9nb1
+BUILDLINK_API_DEPENDS.radare2+=	radare2>=2.3.0
+BUILDLINK_ABI_DEPENDS.radare2?=	radare2>=2.3.0
 BUILDLINK_PKGSRCDIR.radare2?=	../../devel/radare2
 
+.include "../../devel/capstone/buildlink3.mk"
+.include "../../sysutils/file/buildlink3.mk"
+.include "../../archivers/libzip/buildlink3.mk"
 .endif	# RADARE2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-radare2
