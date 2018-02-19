ff6560bff9910672e2db352ab01a7d9c7dda523a
kamil <kamil@pkgsrc.org>
2018-02-19 02:54:59 +0000

    radare2: Simplify the package
    Remove Makefile.common as it is no longer used and merge its content with
    Makefile.
    
    Appease pkglint in buildlink3.mk.

--- devel/radare2/buildlink3.mk
+++ devel/radare2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/02/18 23:17:54 kamil Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/02/19 02:54:59 kamil Exp $
 
 BUILDLINK_TREE+=	radare2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	radare2
 RADARE2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.radare2+=	radare2>=2.3.0
-BUILDLINK_ABI_DEPENDS.radare2?=	radare2>=2.3.0
+BUILDLINK_ABI_DEPENDS.radare2+=	radare2>=2.3.0
 BUILDLINK_PKGSRCDIR.radare2?=	../../devel/radare2
 
 .include "../../devel/capstone/buildlink3.mk"
