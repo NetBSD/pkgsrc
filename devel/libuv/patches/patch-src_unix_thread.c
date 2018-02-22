a9f9ad901f7b26cd4c561a97b70b2d37e7d0755d
adam <adam@pkgsrc.org>
2018-02-22 08:44:14 +0000

    libuv: updated to 1.19.2
    Version 1.19.2
    
    Changes since version 1.19.1:
    * test: fix incorrect asserts
    * test: fix a typo in test-fork.c
    * build: remove long-obsolete gyp workarounds
    * build: split off tests into separate gyp file
    * test: check uv_cond_timedwait more carefully
    * include,src: introduce UV__ERR() macro
    * build: add url field to libuv.pc
    * doc: mark IBM i as Tier 3 support
    * win,build: correct C2059 errors
    * zos: fix timeout for condition variable
    * win: CREATE_NO_WINDOW when stdio is not inherited
    * build: fix commmon.gypi comment
    * doc: document uv_timer_start() on an active timer
    * doc: add note about handle movability
    * doc: fix syntax error in loop documentation
    * osx,stream: retry sending handle on EMSGSIZE error
    * unix: delay fs req register until after validation
    * test: add tests for bad inputs
    * unix,win: ensure req->bufs is freed
    * test: add additional fs memory management checks

--- devel/libuv/patches/patch-src_unix_thread.c
+++ devel/libuv/patches/patch-src_unix_thread.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_unix_thread.c,v 1.3 2017/08/17 20:06:47 adam Exp $
+$NetBSD: patch-src_unix_thread.c,v 1.4 2018/02/22 08:44:15 adam Exp $
 
 Use feature test for pthread_condattr_setclock, absent in netbsd-6-0
 
---- src/unix/thread.c.orig	2017-08-16 14:11:33.000000000 +0000
+--- src/unix/thread.c.orig	2018-02-21 20:25:38.000000000 +0000
 +++ src/unix/thread.c
-@@ -528,7 +528,7 @@ int uv_cond_init(uv_cond_t* cond) {
+@@ -568,7 +568,7 @@ int uv_cond_init(uv_cond_t* cond) {
    if (err)
-     return -err;
+     return UV__ERR(err);
  
 -#if !(defined(__ANDROID_API__) && __ANDROID_API__ < 21)
 +#if defined(HAVE_PTHREAD_CONDATTR_SETCLOCK)
