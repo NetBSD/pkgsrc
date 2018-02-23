87d132375d3162723643eed43fab4eea0a1eca0b
khorben <khorben@pkgsrc.org>
2018-02-23 17:04:43 +0000

    Add support for DESTDIR
    Fix building math/z3 in privileged mode when not building as root:
    ocamlfind: Cannot mkdir /usr/pkg/lib/ocaml/site-lib/Z3: Permission denied
    
    This now uses the option -destdir when calling ocamlfind(1). It also
    includes a workaround for what might be a bug in ocamlfind(1), where it
    also wants to update ld.conf in spite of specifying -destdir.
    
    Tested on NetBSD/amd64.
    
    ok dholland@

--- math/z3/patches/patch-scripts_mk__util.py
+++ math/z3/patches/patch-scripts_mk__util.py
@@ -1,11 +1,12 @@
-$NetBSD: patch-scripts_mk__util.py,v 1.1 2015/11/24 05:45:58 dholland Exp $
+$NetBSD: patch-scripts_mk__util.py,v 1.2 2018/02/23 17:04:43 khorben Exp $
 
 For pkgsrc, use site-packages rather than dist-packages.
+Add support for DESTDIR.
 
 diff -r 59247c69b92d scripts/mk_util.py
---- scripts/mk_util.py	Mon Nov 23 19:08:36 2015 -0500
-+++ scripts/mk_util.py	Mon Nov 23 19:50:45 2015 -0500
-@@ -637,7 +637,7 @@
+--- scripts/mk_util.py.orig	2018-02-22 23:06:23.490379838 +0000
++++ scripts/mk_util.py
+@@ -637,7 +637,7 @@ def parse_options():
              SLOW_OPTIMIZE = True
          elif not IS_WINDOWS and opt in ('-p', '--prefix'):
              PREFIX = arg
@@ -14,3 +15,14 @@ diff -r 59247c69b92d scripts/mk_util.py
              mk_dir(DESTDIR + PYTHON_PACKAGE_DIR)
              if sys.version >= "3":
                  mk_dir(os.path.join(DESTDIR + PYTHON_PACKAGE_DIR, '__pycache__'))
+@@ -1506,8 +1506,8 @@ class MLComponent(Component):
+                         get_component(Z3_DLL_COMPONENT).dll_name + '$(SO_EXT)',
+                         os.path.join(sub_dir, 'z3ml.cmxa'),
+                         os.path.join(sub_dir, 'META')))
+-                out.write('\t%s remove Z3\n' % (OCAMLFIND))
+-                out.write('\t%s install Z3 %s' % (OCAMLFIND, (os.path.join(sub_dir, 'META'))))
++                out.write('\t%s remove -destdir $(DESTDIR)/ -ldconf ignore Z3\n' % (OCAMLFIND))
++                out.write('\t%s install -destdir $(DESTDIR)/ -ldconf ignore Z3 %s' % (OCAMLFIND, (os.path.join(sub_dir, 'META'))))
+                 for m in modules:
+                     out.write(' %s.cma' % (os.path.join(sub_dir, m)))
+                     out.write(' %s.cmx' % (os.path.join(sub_dir, m)))
