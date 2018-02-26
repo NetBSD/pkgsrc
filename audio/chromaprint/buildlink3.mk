b6638a544c3d415f9c71cc0824182f5285034afb
adam <adam@pkgsrc.org>
2018-02-26 08:19:25 +0000

    revbump after x264-devel update

--- audio/chromaprint/buildlink3.mk
+++ audio/chromaprint/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2017/01/16 23:45:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2018/02/26 08:19:25 adam Exp $
 
 BUILDLINK_TREE+=	chromaprint
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	chromaprint
 CHROMAPRINT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.chromaprint+=	chromaprint>=1.1
-BUILDLINK_ABI_DEPENDS.chromaprint?=	chromaprint>=1.4.2nb1
+BUILDLINK_ABI_DEPENDS.chromaprint?=	chromaprint>=1.4.3nb1
 BUILDLINK_PKGSRCDIR.chromaprint?=	../../audio/chromaprint
 
 .include "../../multimedia/ffmpeg3/buildlink3.mk"
