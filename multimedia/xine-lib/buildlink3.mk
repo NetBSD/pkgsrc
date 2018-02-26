1b8112071ab80e2b243256c5ea04e7c5fd13df2a
adam <adam@pkgsrc.org>
2018-02-26 08:19:25 +0000

    revbump after x264-devel update

--- multimedia/xine-lib/buildlink3.mk
+++ multimedia/xine-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.35 2018/01/07 13:04:25 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2018/02/26 08:19:26 adam Exp $
 
 BUILDLINK_TREE+=	xine-lib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	xine-lib
 XINE_LIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xine-lib+=	xine-lib>=1rc3c
-BUILDLINK_ABI_DEPENDS.xine-lib+=	xine-lib>=1.2.6nb9
+BUILDLINK_ABI_DEPENDS.xine-lib+=	xine-lib>=1.2.6nb10
 BUILDLINK_PKGSRCDIR.xine-lib?=		../../multimedia/xine-lib
 
 .include "../../multimedia/ffmpeg2/buildlink3.mk"
