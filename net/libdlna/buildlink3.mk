56e383ec2ebd5d84ec47a0d3ad500743bac51082
adam <adam@pkgsrc.org>
2018-02-26 08:19:25 +0000

    revbump after x264-devel update

--- net/libdlna/buildlink3.mk
+++ net/libdlna/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2017/01/16 23:45:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2018/02/26 08:19:26 adam Exp $
 #
 
 BUILDLINK_TREE+=	libdlna
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	libdlna
 LIBDLNA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libdlna+=	libdlna>=0.2.3
-BUILDLINK_ABI_DEPENDS.libdlna+=	libdlna>=0.2.4nb7
+BUILDLINK_ABI_DEPENDS.libdlna+=	libdlna>=0.2.4nb8
 BUILDLINK_PKGSRCDIR.libdlna?=	../../net/libdlna
 
 .include "../../multimedia/ffmpeg1/buildlink3.mk"
