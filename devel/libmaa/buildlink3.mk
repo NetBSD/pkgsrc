414ba47dadec031d5dfb031e5f05edec5abc88b1
cheusov <cheusov@pkgsrc.org>
2018-02-27 19:36:06 +0000

    libmaa was updated to version 1.4.0   License has been changed from LGPL to MIT, permitted by Rik Faith,   original author of this library.
      Build system changed from autoshit to mk-configure.
    
      Library version/soname was bumped from 3 to 4. I cannot
      guarantee that mk-configure and libtool build shared library the
      same way.
    
      Tests were added for stk_*, mem_* and prm_* functions.
    
      stk_isempty function was added.
    
      stk_* and other was reimplemented without obstack functions (glibc
      extension).

--- devel/libmaa/buildlink3.mk
+++ devel/libmaa/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2011/05/07 10:06:02 cheusov Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/02/27 19:36:06 cheusov Exp $
 
 BUILDLINK_TREE+=	libmaa
 
 .if !defined(LIBMAA_BUILDLINK3_MK)
 LIBMAA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libmaa+=	libmaa>=1.3.0
+BUILDLINK_API_DEPENDS.libmaa+=	libmaa>=1.4.0
 BUILDLINK_PKGSRCDIR.libmaa?=	../../devel/libmaa
 .endif # LIBMAA_BUILDLINK3_MK
 
