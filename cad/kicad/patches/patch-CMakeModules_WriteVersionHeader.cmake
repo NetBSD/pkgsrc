8a6e280761d1cd4903b79d9d16178a854095f36f
mef <mef@pkgsrc.org>
2018-03-01 13:59:11 +0000

    Updated cad/kicad{,-footprints,-doc,-i18n,-lib} to 4.0.7, provided the patch by @tristelo,
    the location of the patch is here:
       https://gist.github.com/steleto/7d03ea70a3b1d6dbd3ad9cc9424b0112#file-pkgsrc_kicad-4-0-7-patch
    Hopefull to fix PR/52996, but not checked on the paticular NetBSD release.
    Only checked on 8.99.12 so far.
    -----------------------------------------------------------
    KiCad 4.0.7 Stable Release
    2017-08-28
    The KiCad project is proud to announce the latest stable
    release of KiCad. The 4.0.7 stable version contains critical
    bug fixes and other minor improvements since the last
    release. It also includes improved footprint, symbol, and 3D
    model libraries, translations, and documentation. The stable
    release version 4.0.7 is made from the stable 4.0 branch with
    bug fixes cherry picked from the development branch of
    KiCad. KiCad binaries are available or will be available in
    the near future for download for Windows, OSX and Linux, see
    the download page for guidance.
      http://kicad-pcb.org/post/release-4.0.7/
    
    KiCad 4.0.6 Stable Release
    2017-03-07
    The KiCad project is proud to announce the latest stable
    release of KiCad. The 4.0.6 stable version contains critical
    bug fixes and other minor improvements since the last
    release. One other notable change was the replacement of the
    Avhttp library with the libcurl library when building the
    GitHub plugin. This should allow Linux distributions that are
    not compatible with the OpenSSL library license to distribute
    KiCad with the GitHub plugin enabled.
      http://kicad-pcb.org/post/release-4.0.6/
    
    KiCad 4.0.5 Stable Release
    2016-12-18
    The KiCad project is proud to announce a new stable release of
    KiCad. The 4.0.5 stable version contains critical bug fixes
    and version string improvements since the last release. The
    stable release version 4.0.5 is made from the stable 4.0
    branch with bug fixes cherry picked from the development
    branch of KiCad. KiCad binaries are available or will be
    available in the near future for download for Windows, OSX and
    Linux, see the download page for guidance.
      http://kicad-pcb.org/post/release-4.0.5/

--- cad/kicad/patches/patch-CMakeModules_WriteVersionHeader.cmake
+++ cad/kicad/patches/patch-CMakeModules_WriteVersionHeader.cmake
@@ -1,22 +1,24 @@
-$NetBSD: patch-CMakeModules_WriteVersionHeader.cmake,v 1.2 2016/09/08 09:32:28 bouyer Exp $
+$NetBSD: patch-CMakeModules_WriteVersionHeader.cmake,v 1.3 2018/03/01 13:59:11 mef Exp $
 
 Don't use ${CMAKE_MODULE_PATH} directly as it will be changed
 and containt multiple directories
 
---- CMakeModules/WriteVersionHeader.cmake.orig	2015-12-14 20:13:21.000000000 +0100
-+++ CMakeModules/WriteVersionHeader.cmake	2015-12-14 20:14:58.000000000 +0100
-@@ -32,12 +32,12 @@
-     # Detect the appropiate VCS and set the version string.
-     if( EXISTS "${SRC_PATH}/.bzr" )
-         message( STATUS "Using Bazaar to determine build version string." )
--        include( ${CMAKE_MODULE_PATH}/CreateBzrVersionHeader.cmake )
-+        include( ${PROJECT_SOURCE_DIR}/CMakeModules/CreateBzrVersionHeader.cmake )
-         create_bzr_version_header( ${SRC_PATH} )
-         set( _wvh_version_str ${KICAD_BUILD_VERSION} )
-     elseif( EXISTS "${SRC_PATH}/.git" )
-         message( STATUS "Using Git to determine build version string." )
--        include( ${CMAKE_MODULE_PATH}/CreateGitVersionHeader.cmake )
-+        include( ${PROJECT_SOURCE_DIR}/CMakeModules/CreateGitVersionHeader.cmake )
-         create_git_version_header( ${SRC_PATH} )
-         set( _wvh_version_str ${KICAD_BUILD_VERSION} )
-     endif()
+--- CMakeModules/WriteVersionHeader.cmake.orig	2017-03-06 23:58:56.000000000 +0000
++++ CMakeModules/WriteVersionHeader.cmake
+@@ -26,14 +26,14 @@
+ # not defined during the build configuration.  If
+ # CreateGitVersionHeader cannot determine the current repo version, a
+ # version.h file is still created with KICAD_VERSION set to "no-vcs-found".
+-include( ${CMAKE_MODULE_PATH}/KiCadVersion.cmake )
++include( ${CMAKE_CURRENT_LIST_DIR}/KiCadVersion.cmake )
+ 
+ # Attempt to detect if we have a git repo and set the version string if
+ # the version wasn't set to something other than the default value in
+ # KiCadVersion.cmake.
+ if( KICAD_VERSION STREQUAL "no-vcs-found" AND EXISTS "${SRC_PATH}/.git" )
+     message( STATUS "Using Git to determine build version string." )
+-    include( ${CMAKE_MODULE_PATH}/CreateGitVersionHeader.cmake )
++    include( ${PROJECT_SOURCE_DIR}/CMakeModules/CreateGitVersionHeader.cmake )
+     create_git_version_header( ${SRC_PATH} )
+ endif()
+ 
