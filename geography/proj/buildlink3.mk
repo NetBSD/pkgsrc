186a67990a5cc11bb96150e75800aab8d68756a2
gdt <gdt@pkgsrc.org>
2018-03-02 01:13:10 +0000

    Bump required versions to 5.0.0
    There is an ABI break, and the API is changing enough that it's too
    hard to figure out if not bumping API is safe.  This is unfortunately
    a big change and all of the geospatial world is going to have to cope.

--- geography/proj/buildlink3.mk
+++ geography/proj/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2015/03/14 18:39:05 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/03/02 01:13:10 gdt Exp $
 
 BUILDLINK_TREE+=	proj
 
 .if !defined(PROJ_BUILDLINK3_MK)
 PROJ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.proj+=	proj>=4.9.1
-BUILDLINK_ABI_DEPENDS.proj+=	proj>=4.9.1
+BUILDLINK_API_DEPENDS.proj+=	proj>=5.0.0
+BUILDLINK_ABI_DEPENDS.proj+=	proj>=5.0.0
 BUILDLINK_PKGSRCDIR.proj?=	../../geography/proj
 .endif # PROJ_BUILDLINK3_MK
 
