b4e8cde92767173f5e785ab85dabb0595a744683
wiz <wiz@pkgsrc.org>
2018-03-02 15:15:50 +0000

    fontcacheproto: remove obsolete package

--- x11/fontcacheproto/builtin.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: builtin.mk,v 1.3 2014/03/10 11:05:51 jperkin Exp $
-
-BUILTIN_PKG:=	fontcacheproto
-PKGCONFIG_FILE.fontcacheproto=	${X11BASE}/lib/pkgconfig/fontcacheproto.pc
-PKGCONFIG_FILE.fontcacheproto+=	${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/fontcacheproto.pc
-
-.include "../../mk/buildlink3/pkgconfig-builtin.mk"
-.include "../../mk/x11.builtin.mk"
