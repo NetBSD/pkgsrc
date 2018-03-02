df5b32d5cb71a4442813a8ebbfff2892febbf68b
taca <taca@pkgsrc.org>
2018-03-02 02:11:24 +0000

    lang/php72: update to 7.2.3
    01 Mar 2018, PHP 7.2.3
    
    - Core:
      . Fixed bug #75864 ("stream_isatty" returns wrong value on s390x). (Sam Ding)
    
    - Apache2Handler:
      . Fixed bug #75882 (a simple way for segfaults in threadsafe php just with
        configuration). (Anatol)
    
    - Date:
      . Fixed bug #75857 (Timezone gets truncated when formatted). (carusogabriel)
      . Fixed bug #75928 (Argument 2 for `DateTimeZone::listIdentifiers()` should
        accept `null`). (Pedro Lacerda)
      . Fixed bug #68406 (calling var_dump on a DateTimeZone object modifies it).
        (jhdxr)
    
    - LDAP:
      . Fixed bug #49876 (Fix LDAP path lookup on 64-bit distros). (dzuelke)
    
    - libxml2:
      . Fixed bug #75871 (use pkg-config where available). (pmmaga)
    
    - PGSQL:
      . Fixed bug #75838 (Memory leak in pg_escape_bytea()). (ard_1 at mail dot ru)
    
    - Phar:
      . Fixed bug #54289 (Phar::extractTo() does not accept specific directories to
        be extracted). (bishop)
      . Fixed bug #65414 (deal with leading slash while adding files correctly).
        (bishopb)
      . Fixed bug #65414 (deal with leading slash when adding files correctly).
        (bishopb)
    
    - ODBC:
      . Fixed bug #73725 (Unable to retrieve value of varchar(max) type). (Anatol)
    
    - Opcache:
      . Fixed bug #75729 (opcache segfault when installing Bitrix). (Nikita)
      . Fixed bug #75893 (file_get_contents $http_response_header variable bugged
        with opcache). (Nikita)
      . Fixed bug #75938 (Modulus value not stored in variable). (Nikita)
    
    - SPL:
      . Fixed bug #74519 (strange behavior of AppendIterator). (jhdxr)
    
    - Standard:
      . Fixed bug #75916 (DNS_CAA record results contain garbage). (Mike,
        Philip Sharp)
      . Fixed bug #75981 (Prevent reading beyond buffer start in http wrapper).
        (Stas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.206 2018/03/02 02:09:48 taca Exp $
+# $NetBSD: phpversion.mk,v 1.207 2018/03/02 02:11:24 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.33
 PHP70_VERSION=	7.0.27
 PHP71_VERSION=	7.1.15
-PHP72_VERSION=	7.2.2
+PHP72_VERSION=	7.2.3
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
