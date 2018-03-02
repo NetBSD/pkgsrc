28f0a62ef4b1f37b0bd86b98d03c160f2a975357
taca <taca@pkgsrc.org>
2018-03-02 02:13:44 +0000

    lang/php56: update to 5.6.34
    01 Mar 2018, PHP 5.6.34
    
    - Standard:
      . Fixed bug #75981 (stack-buffer-overflow while parsing HTTP response). (Stas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.208 2018/03/02 02:12:26 taca Exp $
+# $NetBSD: phpversion.mk,v 1.209 2018/03/02 02:13:44 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -87,7 +87,7 @@
 PHPVERSION_MK=	defined
 
 # Define each PHP's version.
-PHP56_VERSION=	5.6.33
+PHP56_VERSION=	5.6.34
 PHP70_VERSION=	7.0.28
 PHP71_VERSION=	7.1.15
 PHP72_VERSION=	7.2.3
