05363dab82871d26656a7004934de4319d94df90
triaxx <triaxx@pkgsrc.org>
2018-03-02 05:59:18 +0000

    Enable FreeBSD support

--- lang/rust/patches/patch-src_bootstrap_lib.rs
+++ lang/rust/patches/patch-src_bootstrap_lib.rs
@@ -1,10 +1,11 @@
-$NetBSD: patch-src_bootstrap_lib.rs,v 1.2 2018/01/08 09:33:58 ryoon Exp $
+$NetBSD: patch-src_bootstrap_lib.rs,v 1.3 2018/03/02 05:59:18 triaxx Exp $
 
 Don't filter out optimization flags.
+FreeBSD has a particular C++ runtime library name
 
---- src/bootstrap/lib.rs.orig	2018-01-01 21:50:13.000000000 +0000
+--- src/bootstrap/lib.rs.orig	2018-02-12 18:51:18.000000000 +0000
 +++ src/bootstrap/lib.rs
-@@ -636,7 +636,6 @@ impl Build {
+@@ -632,7 +632,6 @@ impl Build {
          // cc-rs because the build scripts will determine that for themselves.
          let mut base = self.cc[&target].args().iter()
                             .map(|s| s.to_string_lossy().into_owned())
@@ -12,3 +13,15 @@ Don't filter out optimization flags.
                             .collect::<Vec<_>>();
  
          // If we're compiling on macOS then we add a few unconditional flags
+@@ -643,6 +642,11 @@ impl Build {
+             base.push("-stdlib=libc++".into());
+         }
+ 
++        // FreeBSD (from 10.2) also uses libc++.
++        if target.contains("freebsd") {
++            base.push("-stdlib=libc++".into());
++        }
++
+         // Work around an apparently bad MinGW / GCC optimization,
+         // See: http://lists.llvm.org/pipermail/cfe-dev/2016-December/051980.html
+         // See: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=78936
