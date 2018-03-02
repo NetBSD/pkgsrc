6172de8d594c1da059883ac5b7f72dedf73c0c6d
taca <taca@pkgsrc.org>
2018-03-02 02:09:48 +0000

    lang/php71: update to 7.1.15
    01 Mar 2018, PHP 7.1.15
    
    - Apache2Handler:
      . Fixed bug #75882 (a simple way for segfaults in threadsafe php just with
        configuration). (Anatol)
    
    - Date:
      . Fixed bug #75857 (Timezone gets truncated when formatted). (carusogabriel)
      . Fixed bug #75928 (Argument 2 for `DateTimeZone::listIdentifiers()` should
        accept `null`). (Pedro Lacerda)
      . Fixed bug #68406 (calling var_dump on a DateTimeZone object modifies it).
        (jhdxr)
    
    - PGSQL:
      . Fixed #75838 (Memory leak in pg_escape_bytea()). (ard_1 at mail dot ru)
    
    - ODBC:
      . Fixed bug #73725 (Unable to retrieve value of varchar(max) type). (Anatol)
    
    - LDAP:
      . Fixed bug #49876 (Fix LDAP path lookup on 64-bit distros). (dzuelke)
    
    - libxml2:
      . Fixed bug #75871 (use pkg-config where available). (pmmaga)
    
    - Phar:
      . Fixed bug #65414 (deal with leading slash when adding files correctly).
        (bishopb)
    
    - SPL:
      . Fixed bug #74519 (strange behavior of AppendIterator). (jhdxr)
    
    - Standard:
      . Fixed bug #75916 (DNS_CAA record results contain garbage). (Mike,
        Philip Sharp)
      . Fixed bug #75981 (stack-buffer-overflow while parsing HTTP response). (Stas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.205 2018/02/23 23:37:38 christos Exp $
+# $NetBSD: phpversion.mk,v 1.206 2018/03/02 02:09:48 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.33
 PHP70_VERSION=	7.0.27
-PHP71_VERSION=	7.1.14
+PHP71_VERSION=	7.1.15
 PHP72_VERSION=	7.2.2
 
 # Define initial release of major version.
