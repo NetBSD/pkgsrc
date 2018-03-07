f14ce2b61a0e4adcf6f44d0d4ff2acc8ff82e89d
wiz <wiz@pkgsrc.org>
2018-03-07 12:00:13 +0000

    xorgproto: make it a build dependency by default.

--- x11/xorgproto/buildlink3.mk
+++ x11/xorgproto/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/02/11 20:13:08 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/03/07 12:00:13 wiz Exp $
 
 BUILDLINK_TREE+=	xorgproto
 
@@ -6,7 +6,8 @@ BUILDLINK_TREE+=	xorgproto
 XORGPROTO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xorgproto+=	xorgproto>=2018.1
-BUILDLINK_PKGSRCDIR.xorgproto?=	../../x11/xorgproto
+BUILDLINK_PKGSRCDIR.xorgproto?=		../../x11/xorgproto
+BUILDLINK_DEPMETHOD.xorgproto?=		build
 .endif	# XORGPROTO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xorgproto
