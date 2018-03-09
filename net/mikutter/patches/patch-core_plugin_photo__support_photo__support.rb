a2e0aef68b62dd7fc7386b12aa23e09dd8795e56
tsutsui <tsutsui@pkgsrc.org>
2018-03-09 14:37:02 +0000

    mikutter: update to 3.6.5.
    pkgsrc changes:
    * explicitly depends on json_pure gem as per Gemfile
    * remove patches that has been integrated in upstream
    
    Upstream changes:
    
    mikutter 3.6.5
    
    * world: on serializing World Model, change format to JSON to use
      functions of Diva
    * show an active account name in tooltip when mouse hovers on
      World Shifter icon
    * logo image in Form DSL about dialog widget was not shown in some case
    * avoid warning caused by use of a deprecated constant
    * follows and followers are not taken properly so that notifications
      of follow and remove didn't work
    * form DLS options were not in order
    * Twitter: time of direct messages in the Activity tab was shown in UTC
    * clear search timeline if blank is specified in the search box
    * update description of requirements in README by assuming use of bundler
    * twitpic images were not shown in some case

--- net/mikutter/patches/patch-core_plugin_photo__support_photo__support.rb
+++ net/mikutter/patches/patch-core_plugin_photo__support_photo__support.rb
@@ -1,8 +1,6 @@
-$NetBSD: patch-core_plugin_photo__support_photo__support.rb,v 1.4 2017/06/10 09:45:33 tsutsui Exp $
+$NetBSD: patch-core_plugin_photo__support_photo__support.rb,v 1.5 2018/03/09 14:37:02 tsutsui Exp $
 
 - temporary workaround to skip totorijp gems (sorry, @toshi_a)
-- pull fixes for upstream ticket #1031
-  https://dev.mikutter.hachune.net/issues/1031
 
 --- core/plugin/photo_support/photo_support.rb.orig	2017-06-10 01:11:51.000000000 +0000
 +++ core/plugin/photo_support/photo_support.rb
@@ -15,18 +13,7 @@ $NetBSD: patch-core_plugin_photo__support_photo__support.rb,v 1.4 2017/06/10 09:
  
  module Plugin::PhotoSupport
    INSTAGRAM_PATTERN = %r{\Ahttps?://(?:instagr\.am|(?:www\.)?instagram\.com)/p/([a-zA-Z0-9_\-]+)/}
-@@ -25,8 +25,9 @@ end
- 
- Plugin.create :photo_support do
-   # twitpic
--  defimageopener('twitpic', %r<^http://twitpic\.com/[a-zA-Z0-9]+>) do |display_url|
-+  defimageopener('twitpic', %r<^https?://twitpic\.com/[a-zA-Z0-9]+>) do |display_url|
-     connection = HTTPClient.new
-+    connection.transparent_gzip_decompression = true
-     page = connection.get_content(display_url)
-     next nil if page.empty?
-     doc = Nokogiri::HTML(page)
-@@ -181,9 +182,14 @@ Plugin.create :photo_support do
+@@ -181,9 +181,14 @@ Plugin.create :photo_support do
  
    # totori.dip.jp
    defimageopener('totori.dip.jp', %r#\Ahttp://totori\.dip\.jp/?\Z#) do |display_url|
