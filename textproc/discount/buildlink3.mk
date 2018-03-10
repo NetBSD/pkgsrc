b89fa5361be1d8e6580ebdb36f5b1f7350d269f8
schmonz <schmonz@pkgsrc.org>
2018-03-10 05:37:44 +0000

    Build and install shared library. Bump PKGREVISION.

--- textproc/discount/buildlink3.mk
+++ textproc/discount/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/04/04 09:40:23 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/03/10 05:37:44 schmonz Exp $
 
 BUILDLINK_TREE+=	discount
 
 .if !defined(DISCOUNT_BUILDLINK3_MK)
 DISCOUNT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.discount+=	discount>=2.1.6
+BUILDLINK_API_DEPENDS.discount+=	discount>=2.2.3anb1
 BUILDLINK_PKGSRCDIR.discount?=		../../textproc/discount
 .endif	# DISCOUNT_BUILDLINK3_MK
 
