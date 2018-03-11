781157d0213a2a1e835d20131c5db3fa5b2fe882
bsiegert <bsiegert@pkgsrc.org>
2018-03-11 20:45:19 +0000

    Depend on go-crypto instead of go-crypto-acme.
    Bump revision.

--- devel/go-tools/buildlink3.mk
+++ devel/go-tools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/01/07 13:04:03 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/03/11 20:45:19 bsiegert Exp $
 
 BUILDLINK_TREE+=	go-tools
 
@@ -12,7 +12,7 @@ BUILDLINK_API_DEPENDS.go-tools+=	go-tools>=1.8
 BUILDLINK_PKGSRCDIR.go-tools?=		../../devel/go-tools
 
 .include "../../devel/google-api-go-client/buildlink3.mk"
-.include "../../security/go-crypto-acme/buildlink3.mk"
+.include "../../security/go-crypto/buildlink3.mk"
 .endif	# GO_TOOLS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-go-tools
