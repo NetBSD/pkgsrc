81e5d97ad5dc9835dd95e0ddb46698e7fec6ff0b
bsiegert <bsiegert@pkgsrc.org>
2018-03-11 20:38:25 +0000

    Update go-sys to 0.0.20180318. No changelog from upstream.

--- devel/go-sys/buildlink3.mk
+++ devel/go-sys/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/05/02 17:00:53 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/03/11 20:38:25 bsiegert Exp $
 
 BUILDLINK_TREE+=	go-sys
 
@@ -8,7 +8,7 @@ GO_SYS_BUILDLINK3_MK:=
 BUILDLINK_CONTENTS_FILTER.go-sys= ${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-sys?=	build
 
-BUILDLINK_API_DEPENDS.go-sys+=	go-sys>=0.0.0
+BUILDLINK_API_DEPENDS.go-sys+=	go-sys>=0.0.20180101
 BUILDLINK_PKGSRCDIR.go-sys?=	../../devel/go-sys
 .endif	# GO_SYS_BUILDLINK3_MK
 
