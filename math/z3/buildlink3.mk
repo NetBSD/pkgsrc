6970c37f1c14e7fbe481d5c03dd0a258bf956402
khorben <khorben@pkgsrc.org>
2018-03-13 00:34:02 +0000

    Remove the references to wip

--- math/z3/buildlink3.mk
+++ math/z3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/03/13 00:31:16 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/03/13 00:34:02 khorben Exp $
 
 BUILDLINK_TREE+=	z3
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	z3
 Z3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.z3+=	z3>=4.5.0
-BUILDLINK_PKGSRCDIR.z3?=	../../wip/z3
+BUILDLINK_PKGSRCDIR.z3?=	../../math/z3
 
 .endif  # Z3_BUILDLINK3_MK
 
