fe5b1e2b4ac6feda3739c27b964e1107b179a4ed
taca <taca@pkgsrc.org>
2018-03-13 17:49:48 +0000

    www/ruby-jekyll: update to 3.7.3
    Changes from 3.5.2 is too many to write here, please refer release page:
    <https://github.com/jekyll/jekyll/releases> or history:
    <https://github.com/jekyll/jekyll/blob/master/History.markdown>.

--- www/ruby-jekyll/patches/patch-lib_jekyll_commands_new.rb
+++ www/ruby-jekyll/patches/patch-lib_jekyll_commands_new.rb
@@ -1,10 +1,10 @@
-$NetBSD: patch-lib_jekyll_commands_new.rb,v 1.1 2017/04/22 17:13:48 taca Exp $
+$NetBSD: patch-lib_jekyll_commands_new.rb,v 1.2 2018/03/13 17:49:49 taca Exp $
 
 Do not use bundle unless specify --execute-bundle option.
 
---- lib/jekyll/commands/new.rb.orig	2017-04-22 08:36:18.000000000 +0000
+--- lib/jekyll/commands/new.rb.orig	2017-10-15 15:52:10.000000000 +0000
 +++ lib/jekyll/commands/new.rb
-@@ -13,6 +13,8 @@ module Jekyll
+@@ -15,6 +15,8 @@ module Jekyll
              c.option "blank", "--blank", "Creates scaffolding but with empty files"
              c.option "skip-bundle", "--skip-bundle", "Skip 'bundle install'"
  
@@ -13,19 +13,22 @@ Do not use bundle unless specify --execute-bundle option.
              c.action do |args, options|
                Jekyll::Commands::New.process(args, options)
              end
-@@ -125,12 +127,13 @@ RUBY
+@@ -127,7 +129,8 @@ RUBY
          # unless the user opts to generate a blank blog or skip 'bundle install'.
  
          def after_install(path, options = {})
 -          unless options["blank"] || options["skip-bundle"]
 +          execute_bundle = options["execute-bundle"]
 +          unless options["blank"] || options["skip-bundle"] || (not execute_bundle)
-             bundle_install path
+             begin
+               require "bundler"
+               bundle_install path
+@@ -137,7 +140,7 @@ RUBY
            end
  
            Jekyll.logger.info "New jekyll site installed in #{path.cyan}."
 -          Jekyll.logger.info "Bundle install skipped." if options["skip-bundle"]
-+          Jekyll.logger.info "Bundle install skipped." if options["skip-bundle"] or (not execute_bundle)
++          Jekyll.logger.info "Bundle install skipped." if options["skip-bundle"] || (not execute_bundle)
          end
  
          def bundle_install(path)
