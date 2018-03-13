699cc6526eb6d2ff217e1135eb2d298bc901d637
khorben <khorben@pkgsrc.org>
2018-03-13 00:31:16 +0000

    Update math/z3 to version 4.5.0
    From the release notes:
    
        New features:
        - New theories of strings and sequences.
        - Incremental consequence finder for finite domains.
        - CMake build system (thanks @delcypher).
        - Updated and improved OCaml API (thanks @martin-neuhaeusser).
        - Updated and improved Java API (thanks @cheshire).
        - New resource limit facilities to avoid non-deterministic timeout behaviour.
        - New bit-vector simplification and ackermannization tactics (thanks @MikolasJanota, @nunoplopes).
        - QSAT: a new solver for quantified arithmetic problems. See:
          Bjorner, Janota: Playing with Quantified Satisfaction, LPAR 2016.
    
        A multitude of bugs has been fixed.
    
    I am about to commit a separate package for the Python bindings.
    
    Coordinated with dholland@

---
+++ math/z3/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2018/03/13 00:31:16 khorben Exp $
+
+BUILDLINK_TREE+=	z3
+
+.if !defined(Z3_BUILDLINK3_MK)
+Z3_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.z3+=	z3>=4.5.0
+BUILDLINK_PKGSRCDIR.z3?=	../../wip/z3
+
+.endif  # Z3_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-z3
