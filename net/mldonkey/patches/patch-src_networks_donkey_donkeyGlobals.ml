d0f7d58cdfee3f5e6b8a465be4eee1f702f95852
dholland <dholland@pkgsrc.org>
2018-03-14 14:05:37 +0000

    Partial build fixes for ocaml 4.06.
    not at all clear this thing is worth the trouble...

--- net/mldonkey/patches/patch-src_networks_donkey_donkeyGlobals.ml
+++ net/mldonkey/patches/patch-src_networks_donkey_donkeyGlobals.ml
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_networks_donkey_donkeyGlobals.ml,v 1.1 2016/08/15 15:48:19 jaapb Exp $
+$NetBSD: patch-src_networks_donkey_donkeyGlobals.ml,v 1.2 2018/03/14 14:05:37 dholland Exp $
 
 Compile with OCaml 4.03
+
 --- src/networks/donkey/donkeyGlobals.ml.orig	2011-10-25 17:33:21.000000000 +0000
 +++ src/networks/donkey/donkeyGlobals.ml
 @@ -781,7 +781,6 @@ let set_client_name c name md4 =
