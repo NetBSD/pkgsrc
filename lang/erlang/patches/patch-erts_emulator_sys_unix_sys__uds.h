fd73ea96b4ce9237bf158756b31ea3017c4a5ca7
fhajny <fhajny@pkgsrc.org>
2018-03-16 11:45:29 +0000

    lang/erlang: Update to 20.3.
    ssl:
    - Added new API functions to facilitate cipher suite
      handling
    
    erts, observer:
    - More crash dump info such as: process binary virtual
      heap stats, full info for process causing out-of-mem
      during GC, more port related info, and dirty scheduler
      info.
    
    inets:
    - Add support for unix domain sockets in the http client.

--- lang/erlang/patches/patch-erts_emulator_sys_unix_sys__uds.h
+++ lang/erlang/patches/patch-erts_emulator_sys_unix_sys__uds.h
@@ -1,14 +1,14 @@
-$NetBSD: patch-erts_emulator_sys_unix_sys__uds.h,v 1.1 2018/01/29 16:54:38 jperkin Exp $
+$NetBSD: patch-erts_emulator_sys_unix_sys__uds.h,v 1.2 2018/03/16 11:45:29 fhajny Exp $
 
 Fix SunOS/C99.
 
---- erts/emulator/sys/unix/sys_uds.h.orig	2017-12-11 20:10:59.000000000 +0000
+--- erts/emulator/sys/unix/sys_uds.h.orig	2018-03-13 20:11:05.000000000 +0000
 +++ erts/emulator/sys/unix/sys_uds.h
 @@ -26,7 +26,11 @@
  #endif
  
  #if defined(__sun__) && !defined(_XOPEN_SOURCE)
-+#if (__STDC_VERSION__-0 < 199901L)
++#if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L
  #define _XOPEN_SOURCE 500
 +#else
 +#define _XOPEN_SOURCE 600
