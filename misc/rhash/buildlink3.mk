8e1f2abe7b40c509c103309577bcfe4d309b213b
adam <adam@pkgsrc.org>
2018-03-17 08:02:14 +0000

    rhash: updated to 1.3.6
    RHash v1.3.6:
    * support --file-list option
    * librhash: speedup sha3
    * Bugfix: fix --follow option
    * better MSYS and CygWin support
    * configuration script
    * Bugfix: broken binary hash output
    * Bugfix: fix running on WinXP

--- misc/rhash/buildlink3.mk
+++ misc/rhash/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/04/18 21:21:30 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/03/17 08:02:14 adam Exp $
 
 BUILDLINK_TREE+=	rhash
 
@@ -9,6 +9,8 @@ BUILDLINK_API_DEPENDS.rhash+=	rhash>=1.3.1
 BUILDLINK_PKGSRCDIR.rhash?=	../../misc/rhash
 
 BUILDLINK_DEPMETHOD.rhash?=	build
+
+.include "../../security/openssl/buildlink3.mk"
 .endif	# RHASH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-rhash
