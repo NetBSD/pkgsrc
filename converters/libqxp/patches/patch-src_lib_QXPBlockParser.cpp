daa1d591238c0a5e9d69e5d6359a1379721e15b5
tnn <tnn@pkgsrc.org>
2018-03-17 09:22:06 +0000

    libqxp: add a missing #include for std::abs

--- converters/libqxp/patches/patch-src_lib_QXPBlockParser.cpp
+++ converters/libqxp/patches/patch-src_lib_QXPBlockParser.cpp
@@ -1,10 +1,18 @@
-$NetBSD: patch-src_lib_QXPBlockParser.cpp,v 1.1 2018/03/12 01:29:44 dholland Exp $
+$NetBSD: patch-src_lib_QXPBlockParser.cpp,v 1.2 2018/03/17 09:22:06 tnn Exp $
 
 Fix clang build. (this doesn't fail on gcc; don't ask me why)
 
---- src/lib/QXPBlockParser.cpp~	2017-12-31 15:11:37.000000000 +0000
+--- src/lib/QXPBlockParser.cpp.orig	2017-12-31 15:11:37.000000000 +0000
 +++ src/lib/QXPBlockParser.cpp
-@@ -108,7 +108,7 @@ std::shared_ptr<RVNGInputStream> QXPBloc
+@@ -16,6 +16,7 @@
+ #include <set>
+ #include <vector>
+ #include <iterator>
++#include <cstdlib>
+ 
+ #include "QXPHeader.h"
+ #include "QXPMemoryStream.h"
+@@ -108,7 +109,7 @@ std::shared_ptr<RVNGInputStream> QXPBloc
  
        const int32_t nextVal = bigIdx ? readS32(m_input, be) : readS16(m_input, be);
        isBig = nextVal < 0;
