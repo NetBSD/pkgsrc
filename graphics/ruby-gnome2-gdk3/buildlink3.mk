c433214e641dacbfd4fe4b3dbd324d8c7b9a5c04
taca <taca@pkgsrc.org>
2018-03-20 17:41:43 +0000

    devel/ruby-gnome2-pango: use ${RUBY_PKGPREFIX} instead of harcoded ruby24

--- graphics/ruby-gnome2-gdk3/buildlink3.mk
+++ graphics/ruby-gnome2-gdk3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2018/03/12 11:15:37 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2018/03/20 17:42:36 taca Exp $
 
 BUILDLINK_TREE+=	ruby-gnome2-gdk3
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ruby-gnome2-gdk3
 RUBY_GNOME2_GDK3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-gdk3+=	${RUBY_PKGPREFIX}-gnome2-gtk3>=1.2
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gdk3+=	ruby24-gnome2-gdk3>=3.2.1nb2
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-gdk3+=	${RUBY_PKGPREFIX}-gnome2-gdk3>=3.2.1nb2
 BUILDLINK_PKGSRCDIR.ruby-gnome2-gdk3?=		../../graphics/ruby-gnome2-gdk3
 
 .include "../../x11/gtk3/buildlink3.mk"
