2b171393636bf48cd4d58be3b17806cb8e28d6fc
gdt <gdt@pkgsrc.org>
2018-03-21 22:38:57 +0000

    encfs: Update to 1.9.4
    Upstream changes:
    
    v1.9.4 / 2018-01-28
    ===================
    	* Fix packaging error leading to wrong version output
    	* Fix shared lib compilation (cmake -DBUILD_SHARED_LIBS=ON ..)
    
    v1.9.3 / 2018-01-28
    ===================
    	* Fix compilation with "-std=c++11"
    	* Fix a gid permission issue with allow_other
    	* Fix operations on symlinks
    	* Fix a race in idle unmount
    	* Add `--reversewrite` cli option
    	* Add PID to syslog logging
    	* Deep code sanity check
    	* Modernize coding style
    	* Make codebase warnings & clang-tidy clean
    	* Bump FUSE_USE_VERSION to 29
    	* Ensure compatibility with LibreSSL
    	* Switch to vendored lib tinyxml2 & easylogging
    	* Reorganize man page and add missing options
    	* Add gtest and micro benchmarks

--- filesystems/fuse-encfs/patches/patch-encfs_DirNode.cpp
+++ filesystems/fuse-encfs/patches/patch-encfs_DirNode.cpp
@@ -1,13 +1,13 @@
-$NetBSD: patch-encfs_DirNode.cpp,v 1.1 2017/06/19 18:41:39 maya Exp $
+$NetBSD: patch-encfs_DirNode.cpp,v 1.2 2018/03/21 22:38:57 gdt Exp $
 
 Define _DIRENT_HAVE_D_TYPE at the top for all the OSes that support
 it. This is an untested functional change for FreeBSD and APPLE.
 
---- encfs/DirNode.cpp.orig	2016-09-18 20:16:04.000000000 +0000
+--- encfs/DirNode.cpp.orig	2018-01-28 21:07:41.000000000 +0000
 +++ encfs/DirNode.cpp
 @@ -42,6 +42,10 @@
- #include "Error.h"
- #include "Mutex.h"
+ #include "NameIO.h"
+ #include "easylogging++.h"
  
 +#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__APPLE__)
 +#define _DIRENT_HAVE_D_TYPE
@@ -16,12 +16,3 @@ it. This is an untested functional change for FreeBSD and APPLE.
  using namespace std;
  
  namespace encfs {
-@@ -78,7 +82,7 @@ static bool _nextName(struct dirent *&de
- 
-   if (de) {
-     if (fileType) {
--#if defined(_DIRENT_HAVE_D_TYPE) || defined(__FreeBSD__) || defined(__APPLE__)
-+#if defined(_DIRENT_HAVE_D_TYPE)
-       *fileType = de->d_type;
- #else
- #warning "struct dirent.d_type not supported"
