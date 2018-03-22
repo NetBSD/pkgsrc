6bef73a243f5addc349fa7a009e0c799b75cdea3
triaxx <triaxx@pkgsrc.org>
2018-03-22 13:29:11 +0000

    grip: update to 3.7.0
    * Add the GConf missing dependency
    * Patch the default cdrom device to /dev/rcd0d for NetBSD
    
    Changes since 3.6.1:
    v3.7.0
    
    	* Sync Brazilian Portuguese from Translation Project
    	* Patch by Phil Stracchino: extended genres to genre list
    
    v3.6.3
    
    	* Sync ukranian and hungarian translations from Translation Project
    	* Patch from Adrian Reber: fix compiler warning with gcc 8.0
    	* Update the FSF postal address in source files
    
    v3.6.2
    
    	* Translation updates: German, Italian, Brazilian Portuguese, Serbian

--- audio/grip/patches/patch-src_grip.c
+++ audio/grip/patches/patch-src_grip.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_grip.c,v 1.1 2017/08/18 12:21:29 triaxx Exp $
+$NetBSD: patch-src_grip.c,v 1.2 2018/03/22 13:29:11 triaxx Exp $
 
 Add system dependent headers.
 
---- src/grip.c.orig	2017-07-31 21:04:12.000000000 +0000
+--- src/grip.c.orig	2018-01-31 15:06:36.000000000 +0000
 +++ src/grip.c
 @@ -26,6 +26,9 @@
  #include <stdio.h>
@@ -14,3 +14,12 @@ Add system dependent headers.
  #include <gdk/gdkx.h>
  #include <X11/Xlib.h>
  #include <time.h>
+@@ -773,7 +776,7 @@ static void DoLoadConfig(GripInfo *ginfo
+ 
+   *ginfo->version='\0';
+ 
+-  strcpy(ginfo->cd_device,"/dev/cdrom");
++  strcpy(ginfo->cd_device,CD_DEVICE);
+   *ginfo->force_scsi='\0';
+ 
+   ginfo->local_mode=FALSE;
