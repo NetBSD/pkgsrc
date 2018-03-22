9c254f36b3c6e11b9144d0fba04e3dfe617d01f0
adam <adam@pkgsrc.org>
2018-03-22 08:05:25 +0000

    oniguruma: updated to 6.8.1
    6.8.1:
    Update shared library version to 5.0.0 for API incompatible changes from 6.7.1

--- textproc/oniguruma/buildlink3.mk
+++ textproc/oniguruma/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/01/07 13:04:35 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/03/22 08:05:25 adam Exp $
 
 BUILDLINK_TREE+=	oniguruma
 
 .if !defined(ONIGURUMA_BUILDLINK3_MK)
 ONIGURUMA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.oniguruma+=	oniguruma>=5.9.1
+BUILDLINK_API_DEPENDS.oniguruma+=	oniguruma>=6.8.1
 BUILDLINK_PKGSRCDIR.oniguruma?=		../../textproc/oniguruma
 .endif # ONIGURUMA_BUILDLINK3_MK
 
