cbd323c0ea6a62a4bee8ac942c7a81ebc15dbe4f
fhajny <fhajny@pkgsrc.org>
2018-03-23 12:08:50 +0000

    mail/rspamd: Update to 1.7.1.
    - [CritFix] Fix lowercase comparison
    - [CritFix] Timezone defines seconds WEST UTC not East
    - [Feature] Add filename to log format
    - [Feature] Add lua rules squeezing
    - [Feature] Add related symbols analysis to rspamd_stats
    - [Feature] Remove upstream `X-Spam: Yes` header by default
    - [Feature] rspamd_stats: Output progress info on STDERR
    - [Feature] Whitelist for emails module
    - [Fix] Do not allow dependencies on self
    - [Fix] Do not cache metric result
    - [Fix] Do not trust all issuers as a client certificate
    - [Fix] Fix dependencies in lua squeeze
    - [Fix] Fix enabling/disabling squeezed rules
    - [Fix] Fix enabling/disabling symbols
    - [Fix] Fix external dependencies
    - [Fix] Fix processing of a single compressed file
    - [Fix] Fix some typos
    - [Fix] Fix various modules in case of empty message
    - [Fix] Handle callbacks that returns table of options
    - [Fix] Improve cached action interaction
    - [Fix] Make dynamic conf more NaN aware
    - [Fix] Never hide actions from WebUI `configuration` tab
    - [Project] Implementation of Lua rules squeezing

--- mail/rspamd/patches/patch-src_libutil_util.c
+++ mail/rspamd/patches/patch-src_libutil_util.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_libutil_util.c,v 1.1 2018/03/13 15:09:15 fhajny Exp $
+$NetBSD: patch-src_libutil_util.c,v 1.2 2018/03/23 12:08:50 fhajny Exp $
 
 Fix SunOS workaround.
 
---- src/libutil/util.c.orig	2018-03-12 16:46:32.000000000 +0000
+--- src/libutil/util.c.orig	2018-03-20 12:26:55.000000000 +0000
 +++ src/libutil/util.c
 @@ -2766,8 +2766,8 @@ rspamd_gmtime (gint64 ts, struct tm *des
  	dest->tm_sec = remsecs % 60;
@@ -16,11 +16,11 @@ Fix SunOS workaround.
  #ifdef HAVE_SANE_TZSET
 @@ -2787,8 +2787,8 @@ void rspamd_localtime (gint64 ts, struct
  
- 	ts += timezone;
+ 	ts -= timezone;
  	rspamd_gmtime (ts, dest);
 -	dest->tm_zone = daylight ? (tzname[1] ? tzname[1] : tzname[0]) : tzname[0];
  #if !defined(__sun)
 +	dest->tm_zone = daylight ? (tzname[1] ? tzname[1] : tzname[0]) : tzname[0];
- 	dest->tm_gmtoff = timezone;
+ 	dest->tm_gmtoff = -timezone;
  #endif
  }
