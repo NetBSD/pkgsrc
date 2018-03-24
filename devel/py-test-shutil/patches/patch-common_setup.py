3fb3d380ae34b7bb3f836ff08cd8a1b62879e508
adam <adam@pkgsrc.org>
2018-03-24 07:14:59 +0000

    Do not depend on setuptools-git.

---
+++ devel/py-test-shutil/patches/patch-common_setup.py
@@ -0,0 +1,13 @@
+$NetBSD: patch-common_setup.py,v 1.1 2018/03/24 07:14:59 adam Exp $
+
+Do not depend on setuptools-git.
+
+--- common_setup.py.orig	2018-03-24 07:10:19.000000000 +0000
++++ common_setup.py
+@@ -72,6 +72,5 @@ def common_setup(src_dir):
+             license='MIT license',
+             platforms=['unix', 'linux'],
+             cmdclass={'test': PyTest, 'egg_info': EggInfo},
+-            setup_requires=['setuptools-git'],
+             include_package_data=True
+             )
