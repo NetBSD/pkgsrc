884ca4854817b6103362d644cb949159c1879939
mrg <mrg@pkgsrc.org>
2018-03-26 02:33:41 +0000

    add support for netbsd-6 and netbsd-7.  sort of works.  the failures i see are more generic and not x11 at this point.

--- x11/xorgproto/buildlink3.mk
+++ x11/xorgproto/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/03/07 12:00:13 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/03/26 02:33:41 mrg Exp $
 
 BUILDLINK_TREE+=	xorgproto
 
 .if !defined(XORGPROTO_BUILDLINK3_MK)
 XORGPROTO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xorgproto+=	xorgproto>=2018.1
+BUILDLINK_API_DEPENDS.xorgproto+=	xorgproto>=2016.1
 BUILDLINK_PKGSRCDIR.xorgproto?=		../../x11/xorgproto
 BUILDLINK_DEPMETHOD.xorgproto?=		build
 .endif	# XORGPROTO_BUILDLINK3_MK
