5d5890fecae67601b41ead8953e1c62c46928e7d
maya <maya@pkgsrc.org>
2018-03-26 22:24:45 +0000

    firefox: update to 59.0.2
    CVE-2018-5148: Use-after-free in compositor
    
    Invalid page rendering with hardware acceleration enabled (Bug 1435472)
    
    Windows 7 users with touch screens or certain 3rd party desktop applications which interact with Firefox through accessibility services may experience random browser crashes. Known 3rd party applicatioins with issues: StickyPassword, Windows 7 touch screen. (Bug 1424505)
    
    Browser keyboard shortcuts (eg copy Ctrl+C) don't work on sites that use those keys with resistFingerprinting enabled (Bug 1433592)
    
    High CPU / memory churn caused by third-party software on some computers (Bug 1446280)
    
    Users who have configured an "automatic proxy configuration URL" and want to reload their proxy settings from the URL will find the Reload button disabled in the Connection Settings dialog when they select Preferences/Options > Network Proxy > Settings... (Bug 1445991)
    
    URL Fragment Identifiers Break Service Worker Responses (Bug 1443850)
    
    User's trying to cancel a print around the time it completes will continue to get intermittent crashes (Bug 1441598)
    
    Broken getUserMedia (audio) on DragonFly, FreeBSD, NetBSD, OpenBSD. Video chat apps either wouldn't work or be always muted (Bug 1444074)

--- www/firefox/patches/patch-modules_libpref_init_all.js
+++ www/firefox/patches/patch-modules_libpref_init_all.js
@@ -1,36 +1,22 @@
-$NetBSD: patch-modules_libpref_init_all.js,v 1.7 2018/03/17 00:59:03 ryoon Exp $
+$NetBSD: patch-modules_libpref_init_all.js,v 1.8 2018/03/26 22:24:45 maya Exp $
+
+Else case same as linux for duplex audio, capture_delay, and disabling
+custom styling to workaround a GTK bug
 
 --- modules/libpref/init/all.js.orig	2018-03-10 02:54:17.000000000 +0000
 +++ modules/libpref/init/all.js
-@@ -522,25 +522,18 @@ pref("media.getusermedia.agc", 3); // kA
- // capture_delay: Adjustments for OS-specific input delay (lower bound)
- // playout_delay: Adjustments for OS-specific AudioStream+cubeb+output delay (lower bound)
- // full_duplex: enable cubeb full-duplex capture/playback
-+pref("media.navigator.audio.full_duplex", true);
- #if defined(XP_MACOSX)
- pref("media.peerconnection.capture_delay", 50);
--pref("media.navigator.audio.full_duplex", true);
- #elif defined(XP_WIN)
- pref("media.peerconnection.capture_delay", 50);
--pref("media.navigator.audio.full_duplex", true);
- #elif defined(ANDROID)
- pref("media.peerconnection.capture_delay", 100);
--pref("media.navigator.audio.full_duplex", true);
+@@ -532,7 +532,10 @@ pref("media.peerconnection.capture_delay
  pref("media.navigator.hardware.vp8_encode.acceleration_enabled", true);
  pref("media.navigator.hardware.vp8_encode.acceleration_remote_enabled", true);
  pref("media.navigator.hardware.vp8_decode.acceleration_enabled", false);
--#elif defined(XP_LINUX) || defined(MOZ_SNDIO)
--pref("media.peerconnection.capture_delay", 70);
--pref("media.navigator.audio.full_duplex", true);
- #else
--// *BSD, others - merely a guess for now
--pref("media.peerconnection.capture_delay", 50);
--pref("media.navigator.audio.full_duplex", false);
++#elif defined(XP_LINUX) || defined(MOZ_SNDIO)
 +pref("media.peerconnection.capture_delay", 70);
+ #else
++// *BSD, others - merely a guess for now
+ pref("media.peerconnection.capture_delay", 70);
  #endif
  // Use MediaDataDecoder API for WebRTC, this includes hardware acceleration for
- // decoding.
-@@ -1338,8 +1331,8 @@ pref("dom.forms.autocomplete.formautofil
+@@ -1331,8 +1334,8 @@ pref("dom.forms.autocomplete.formautofil
  // Enable search in <select> dropdowns (more than 40 options)
  pref("dom.forms.selectSearch", false);
  // Allow for webpages to provide custom styling for <select>
