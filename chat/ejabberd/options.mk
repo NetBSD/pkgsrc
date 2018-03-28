22ff55f87e6ab060e750cef62e239d48a9c2014c
fhajny <fhajny@pkgsrc.org>
2018-03-28 21:51:09 +0000

    chat/ejabberd: Update to 18.03.
    Admin
    - Avoid logging IP addresses in mod_register when it's not desired
    - Command 'reload-config' allows to reload certificates
    - Get rid of 'fs' package dependency
    - Improve log message when module startup has failed
    - mod_muc_admin: New command get_room_affiliation
    - prosody2ejabberd: Report meaningful error when luerl is not
      available
    
    Configure
    - Accept atoms in api_permission command lists and commands with
      numbers in them
    - Validate additional listen opts: inet, inet6, backlog
    - Remove 'iqdisc' option
    - New option –enable-group=xxx
    - New option 'negotiation_timeout'
    - New option 'new_sql_schema'
    - New option 'validate_stream'
    - ejabberd_service: New option 'global_routes' for
    - mod_avatar: New 'rate_limit' option
    - mod_block_strangers: New 'access' option
    - mod_block_strangers: New 'captcha' option
    - mod_pubsub: New option 'force_node_config'
    
    Miscelanea
    - Simplify ejabberd_sup code
    - New gen_mod mod_options/1 callback to provide known options and
      defaults
    - Replace ?MYLANG with connection's language wherever possible
    - sql/*: Add username to peer indexes
    - cyrsasl: Simplify code for splitting auth string in cyrsasl
    - ejabberd_auth: Cache 'isuser' queries to external auth program
    - ejabberd_web_admin: Hardcode required ACL rules
    - mod_admin_extra: Command check_password_hash supports all hash
      methods
    - mod_admin_extra: Fix srg_get_info command with @all@ and @online@
    - mod_avatar: Fulfill all requirements of XEP-0398 v0.2.0
    - mod_avatar: Improve validation of 'convert' option
    - mod_block_strangers: Bounce groupchat to bare JID
    - mod_block_strangers: Fix a typo in call to create_captcha()
    - mod_caps: Only store CAPS if contact is subscribed
    - mod_carboncopy: Copy outgoing MUC PMs
    - mod_mam: Really run use_cache/1 and cache_nodes/1 callbacks
    - mod_pubsub: Remove items of unregistered user
    - mod_push_keepalive: Preserve timeout on resumption
    - mod_shared_roster: Try to fix ejabberd_c2s:process_info: got
      unexpected info
    - mod_shared_roster_ldap: Fix processing of ldap_memberattr_format_re
      option
    - mod_stream_mgmt: Abort connection on count error
    - mod_stream_mgmt: Clean up on timed out resumption

--- chat/ejabberd/options.mk
+++ chat/ejabberd/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2017/04/13 19:04:53 fhajny Exp $
+# $NetBSD: options.mk,v 1.6 2018/03/28 21:51:09 fhajny Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ejabberd
 PKG_SUPPORTED_OPTIONS=	sqlite
@@ -10,5 +10,5 @@ PKG_SUGGESTED_OPTIONS=	#
 # Add SQLite 3 support
 #
 .if !empty(PKG_OPTIONS:Msqlite)
-DEPENDS+=		erlang-sqlite3>=1.1.5:../../databases/erlang-sqlite3
+DEPENDS+=		erlang-sqlite3>=1.1.6:../../databases/erlang-sqlite3
 .endif
