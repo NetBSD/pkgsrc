c93e83b5f5947909c15623a4a8b9833230c4f08f
bsiegert <bsiegert@pkgsrc.org>
2018-03-30 11:52:18 +0000

    Update Go to 1.10.1.
    This fixes a security issue (https://github.com/golang/go/issues/23867).
    Also:
    These releases include fixes to the compiler, runtime, go command, and the
    archive/zip, crypto/tls, crypto/x509, encoding/json, net, net/http, and
    net/http/pprof packages.
    
    ok wiz@ for committing during freeze

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,10 +1,10 @@
-# $NetBSD: version.mk,v 1.35 2018/03/04 12:54:06 bsiegert Exp $
+# $NetBSD: version.mk,v 1.36 2018/03/30 11:52:18 bsiegert Exp $
 
 SSP_SUPPORTED=	no
 
 .include "../../mk/bsd.prefs.mk"
 
-GO_VERSION=	1.10
+GO_VERSION=	1.10.1
 GO14_VERSION=	1.4.3
 
 ONLY_FOR_PLATFORM=	*-*-i386 *-*-x86_64 *-*-earmv[67]hf
