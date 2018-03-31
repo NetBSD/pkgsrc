200845da1864934f35f8211aee44cccdd4fcfe0f
adam <adam@pkgsrc.org>
2018-03-31 16:22:10 +0000

    py-Pillow: disabled mp_compile hack; it has problems with native parallel building

--- graphics/py-Pillow/patches/patch-setup.py
+++ graphics/py-Pillow/patches/patch-setup.py
@@ -1,9 +1,18 @@
-$NetBSD: patch-setup.py,v 1.14 2018/01/04 20:59:50 adam Exp $
+$NetBSD: patch-setup.py,v 1.15 2018/03/31 16:22:11 adam Exp $
 
 Prevent to detect optional tkinter.
+Disable mp_compile hack; it has problems with native parallel building.
 
---- setup.py.orig	2018-01-04 20:47:56.000000000 +0000
+--- setup.py.orig	2018-01-01 21:04:32.000000000 +0000
 +++ setup.py
+@@ -23,7 +23,6 @@ from setuptools import Extension, setup
+ 
+ # monkey patch import hook. Even though flake8 says it's not used, it is.
+ # comment this out to disable multi threaded builds.
+-import mp_compile
+ 
+ 
+ if sys.platform == "win32" and sys.version_info >= (3, 7):
 @@ -128,11 +128,7 @@ def get_version():
      return locals()['__version__']
  
