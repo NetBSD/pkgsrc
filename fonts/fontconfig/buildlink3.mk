1788e6754c85ba955299ec46a377b8f9ab157fd8
schmonz <schmonz@pkgsrc.org>
2018-03-31 04:30:33 +0000

    Since fontconfig depends on devel/gettext-lib (as of 2.13.0nb1), anything depending on fontconfig does too. Add it to buildlink3.mk. This is a high-impact change to commit during the freeze, but seems both correct and necessary, and has wiz@'s OK.

--- fonts/fontconfig/buildlink3.mk
+++ fonts/fontconfig/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.34 2018/03/12 11:15:32 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.35 2018/03/31 04:30:33 schmonz Exp $
 
 BUILDLINK_TREE+=	fontconfig
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.fontconfig+=	fontconfig>=2.10.93
 BUILDLINK_ABI_DEPENDS.fontconfig+=	fontconfig>=2.13.0
 BUILDLINK_PKGSRCDIR.fontconfig?=	../../fonts/fontconfig
 
+.include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../devel/libuuid/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
