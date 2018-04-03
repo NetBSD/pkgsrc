8a1ec151541a3cf43cf7a6c2bd524b7500374339
adam <adam@pkgsrc.org>
2018-04-03 14:34:00 +0000

    nmap: updated to 7.70
    7.70:
    We're excited to make our first Nmap release of 2018--version 7.70!  It
    includes hundreds of new OS and service fingerprints, 9 new NSE scripts
    (for a total of 588), a much-improved version of our Npcap windows packet
    capturing library/driver, and service detection improvements to make -sV
    faster and more accurate.

--- net/nmap/patches/patch-zenmap_test_run__tests.py
+++ net/nmap/patches/patch-zenmap_test_run__tests.py
@@ -1,15 +1,12 @@
-$NetBSD: patch-zenmap_test_run__tests.py,v 1.2 2016/10/14 15:11:16 maya Exp $
+$NetBSD: patch-zenmap_test_run__tests.py,v 1.3 2018/04/03 14:34:00 adam Exp $
 
---- zenmap/test/run_tests.py.orig	2016-06-26 04:39:29.000000000 +0000
+--- zenmap/test/run_tests.py.orig	2017-08-07 16:14:51.000000000 +0000
 +++ zenmap/test/run_tests.py
-@@ -11,8 +11,9 @@ if __name__ == "__main__":
+@@ -11,6 +11,7 @@ if __name__ == "__main__":
          sys.exit(0)
  
      os.chdir("..")
-+    sys.path.insert(1,"build/lib")
++    sys.path.insert(1, "build/lib")
      suite = unittest.defaultTestLoader.discover(
--        start_dir=glob.glob("build/lib.*")[0],
-+        start_dir=glob.glob("build/lib/*")[0],
+         start_dir=glob.glob("build/lib*")[0],
          pattern="*.py"
-         )
-     unittest.TextTestRunner().run(suite)
