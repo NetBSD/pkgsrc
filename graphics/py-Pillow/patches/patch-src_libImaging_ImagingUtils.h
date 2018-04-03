8150febe827806cc49cf174140d3edf631fca2af
adam <adam@pkgsrc.org>
2018-04-03 09:24:19 +0000

    py-Pillow: updated to 5.1.0
    5.1.0:
    Close fp before return in ImagingSavePPM
    Added documentation for ICNS append_images
    Docs: Move intro text below its header
    CI: Rename appveyor.yml as .appveyor.yml
    Fix TypeError for JPEG2000 parser feed
    Certain corrupted jpegs can result in no data read
    Add support for BLP file format
    Simplify version checks
    Fix "invalid escape sequence" warning on Python 3.6+
    Allow append_images to set .icns scaled images
    Support appending to existing PDFs
    Fix and improve efficient saving of ICNS on macOS
    Build: Enable pip cache in AppVeyor build
    Trim trailing whitespace
    Docs: Correct reference to Image.new method
    Rearrange ImageFilter classes into alphabetical order
    Test: Remove duplicate line
    Build: Update AppVeyor PyPy version
    Tiff: Open 8 bit Tiffs with 5 or 6 channels, discarding extra channels
    Readme: Added Twitter badge
    Removed __main__ code from ImageCms
    Test: Changed assert statements to unittest calls
    Depends: Update libimagequant to 2.11.10, raqm to 0.5.0, freetype to 2.9
    Remove _imaging.crc32 in favor of builtin Python crc32 implementation
    Move Tk directory to src directory
    Enable pip cache in Travis CI
    Remove unused and duplicate imports
    Docs: Changed documentation references to 2.x to 2.7
    Fix memory leak when opening webp files
    Setup: Fix "TypeError: 'NoneType' object is not iterable" for PPC and CRUX
    Setup: Add libdirs for ppc64le and armv7l

--- graphics/py-Pillow/patches/patch-src_libImaging_ImagingUtils.h
+++ graphics/py-Pillow/patches/patch-src_libImaging_ImagingUtils.h
@@ -1,23 +1,15 @@
-$NetBSD: patch-src_libImaging_ImagingUtils.h,v 1.1 2018/01/12 19:50:52 adam Exp $
+$NetBSD: patch-src_libImaging_ImagingUtils.h,v 1.2 2018/04/03 09:24:20 adam Exp $
 
-Disable the work around; it does not really work.
+Fix building with GCC 4.x; https://github.com/python-pillow/Pillow/issues/2956
 
---- src/libImaging/ImagingUtils.h.orig	2018-01-12 19:46:54.000000000 +0000
+--- src/libImaging/ImagingUtils.h.orig	2018-01-01 21:04:32.000000000 +0000
 +++ src/libImaging/ImagingUtils.h
-@@ -30,16 +30,4 @@
-     (MULDIV255(in1, (255 - mask), tmp1) + in2)
- 
- 
--/* This is to work around a bug in GCC prior 4.9 in 64 bit mode.
--   GCC generates code with partial dependency which is 3 times slower.
--   See: http://stackoverflow.com/a/26588074/253146 */
--#if defined(__x86_64__) && defined(__SSE__) &&  ! defined(__NO_INLINE__) && \
--    ! defined(__clang__) && defined(GCC_VERSION) && (GCC_VERSION < 40900)
--static float __attribute__((always_inline)) inline _i2f(int v) {
--    float x;
+@@ -37,7 +37,7 @@
+     ! defined(__clang__) && defined(GCC_VERSION) && (GCC_VERSION < 40900)
+ static float __attribute__((always_inline)) inline _i2f(int v) {
+     float x;
 -    __asm__("xorps %0, %0; cvtsi2ss %1, %0" : "=X"(x) : "r"(v) );
--    return x;
--}
--#else
- static float inline _i2f(int v) { return (float) v; }
--#endif
++    __asm__("xorps %0, %0; cvtsi2ss %1, %0" : "=x"(x) : "r"(v) );
+     return x;
+ }
+ #else
