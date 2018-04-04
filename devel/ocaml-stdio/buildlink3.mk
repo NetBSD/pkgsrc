9d9a4a17ae77c7b47000d6f4dee7223d8a04f914
jaapb <jaapb@pkgsrc.org>
2018-04-04 16:26:25 +0000

    Revbump for devel/ocaml-stdio.
    No upstream changes; this just deals with some new files caused to be
    installed by the new version of jbuilder.

--- devel/ocaml-stdio/buildlink3.mk
+++ devel/ocaml-stdio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/01/10 16:10:33 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/04/04 16:26:25 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-stdio
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-stdio
 OCAML_STDIO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-stdio+=	ocaml-stdio>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-stdio+=	ocaml-stdio>=0.10.0
+BUILDLINK_ABI_DEPENDS.ocaml-stdio+=	ocaml-stdio>=0.10.0nb2
 BUILDLINK_PKGSRCDIR.ocaml-stdio?=	../../devel/ocaml-stdio
 .endif	# OCAML_STDIO_BUILDLINK3_MK
 
