2cc58d87bb1da13ef45b0051c84eff2cc413593e
jaapb <jaapb@pkgsrc.org>
2018-04-04 19:36:53 +0000

    Revbump for devel/ocaml-ppx_type_conv.
    No upstream changes, just dealing with new files caused to be installed
    by the new version of jbuilder.

--- devel/ocaml-ppx_type_conv/buildlink3.mk
+++ devel/ocaml-ppx_type_conv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2017/07/21 14:08:22 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/04/04 19:36:53 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_type_conv
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_type_conv
 OCAML_PPX_TYPE_CONV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_type_conv+=	ocaml-ppx_type_conv>=0.9.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_type_conv+=	ocaml-ppx_type_conv>=0.9.0nb3
 BUILDLINK_PKGSRCDIR.ocaml-ppx_type_conv?=	../../devel/ocaml-ppx_type_conv
 
 .endif	# OCAML_PPX_TYPE_CONV_BUILDLINK3_MK
