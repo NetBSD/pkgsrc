ab93cc6a71b3d4af3f211e5a03f4396370c594c8
jaapb <jaapb@pkgsrc.org>
2018-04-04 16:20:15 +0000

    New package revision for devel/ocaml-sexplib.
    No upstream changes; this deals with some extra files caused to be
    installed by the new version of jbuilder.

--- devel/ocaml-sexplib/buildlink3.mk
+++ devel/ocaml-sexplib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/01/10 16:09:33 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/04/04 16:20:15 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-sexplib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-sexplib
 OCAML_SEXPLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=0.10.0
+BUILDLINK_ABI_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=0.10.0nb1
 BUILDLINK_PKGSRCDIR.ocaml-sexplib?=	../../devel/ocaml-sexplib
 
 .endif	# OCAML_SEXPLIB_BUILDLINK3_MK
