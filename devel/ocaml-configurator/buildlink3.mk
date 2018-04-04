cf8b72563ec5448cc9d933fe86840c45a2b102eb
jaapb <jaapb@pkgsrc.org>
2018-04-04 16:23:36 +0000

    Revbump for devel/ocaml-configurator.
    No upstream changes; this just deals with some new files caused to be
    installed by the new version of jbuilder.

--- devel/ocaml-configurator/buildlink3.mk
+++ devel/ocaml-configurator/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/10/16 08:38:27 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/04/04 16:23:36 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-configurator
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-configurator
 OCAML_CONFIGURATOR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-configurator+=	ocaml-configurator>=0.9.1
+BUILDLINK_ABI_DEPENDS.ocaml-configurator+=	ocaml-configurator>=0.9.1nb2
 BUILDLINK_PKGSRCDIR.ocaml-configurator?=	../../devel/ocaml-configurator
 
 .endif	# OCAML_CONFIGURATOR_BUILDLINK3_MK
