d5347bc2cfb2db9b6d178ac5885b416c5f266236
jaapb <jaapb@pkgsrc.org>
2018-04-04 19:15:07 +0000

    Revbump for devel/ocaml-ppx_ast.
    No upstream changes, just dealing with new files caused to be installed by
    the new version of jbuilder.

--- devel/ocaml-ppx_ast/buildlink3.mk
+++ devel/ocaml-ppx_ast/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/01/10 15:54:02 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/04/04 19:15:07 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_ast
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_ast
 OCAML_PPX_AST_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_ast+=	ocaml-ppx_ast>=0.9.1
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_ast+=	ocaml-ppx_ast>=0.9.2
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_ast+=	ocaml-ppx_ast>=0.9.2nb1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_ast?=	../../devel/ocaml-ppx_ast
 .endif	# OCAML_PPX_AST_BUILDLINK3_MK
 
