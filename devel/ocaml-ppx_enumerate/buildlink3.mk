c19aa1cf0e2443647f6e4a8112a22fd94f7c2c7e
jaapb <jaapb@pkgsrc.org>
2018-04-04 19:39:43 +0000

    Revbump for devel/ocaml-ppx_enumerate.
    No upstream changes, just dealing with new files caused to be installed
    by the new version of jbuilder.

--- devel/ocaml-ppx_enumerate/buildlink3.mk
+++ devel/ocaml-ppx_enumerate/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/10/11 13:57:15 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/04/04 19:39:43 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_enumerate
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_enumerate
 OCAML_PPX_ENUMERATE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_enumerate+=	ocaml-ppx_enumerate>=0.9.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_enumerate+=	ocaml-ppx_enumerate>=0.9.0nb2
 BUILDLINK_PKGSRCDIR.ocaml-ppx_enumerate?=	../../devel/ocaml-ppx_enumerate
 .endif	# OCAML_PPX_ENUMERATE_BUILDLINK3_MK
 
