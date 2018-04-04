c2f3147d19a8888b4bc34e65383fcf342a770e55
jaapb <jaapb@pkgsrc.org>
2018-04-04 09:34:46 +0000

    Updated lang/ocaml to version 4.06.1.
    This is a bugfix release that contains no breaking changes.

--- lang/ocaml/buildlink3.mk
+++ lang/ocaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.44 2018/01/10 16:14:20 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.45 2018/04/04 09:34:46 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml
 OCAML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml+=	ocaml>=4.06.0
-BUILDLINK_ABI_DEPENDS.ocaml+=	ocaml>=4.06.0
+BUILDLINK_ABI_DEPENDS.ocaml+=	ocaml>=4.06.1
 BUILDLINK_PKGSRCDIR.ocaml?=	../../lang/ocaml
 #BUILDLINK_DEPMETHOD.ocaml?=	build
 
