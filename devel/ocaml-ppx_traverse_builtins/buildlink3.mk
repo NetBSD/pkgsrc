3fe14bf607910e575f79b7656bd8ff04a1435204
jaapb <jaapb@pkgsrc.org>
2018-04-04 19:17:36 +0000

    Revbump for devel/ocaml-ppx_traverse_builtins.
    No upstream changes, just dealing with files caused to be installed by
    the new version of jbuilder.

--- devel/ocaml-ppx_traverse_builtins/buildlink3.mk
+++ devel/ocaml-ppx_traverse_builtins/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/07/19 12:30:02 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/04/04 19:17:36 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_traverse_builtins
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_traverse_builtins
 OCAML_PPX_TRAVERSE_BUILTINS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_traverse_builtins+=	ocaml-ppx_traverse_builtins>=0.9.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_traverse_builtins+=	ocaml-ppx_traverse_builtins>=0.9.0nb3
 BUILDLINK_PKGSRCDIR.ocaml-ppx_traverse_builtins?=	../../devel/ocaml-ppx_traverse_builtins
 .endif	# OCAML_PPX_TRAVERSE_BUILTINS_BUILDLINK3_MK
 
