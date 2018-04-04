31bcbcff2a71d8f31ae3d74a7d6b97e731c7ecd9
jaapb <jaapb@pkgsrc.org>
2018-04-04 19:30:52 +0000

    Revbump for devel/ocaml-ppx_metaquot.
    No upstream changes, just dealing with new files caused to be installed
    by the new version of jbuilder.

--- devel/ocaml-ppx_metaquot/buildlink3.mk
+++ devel/ocaml-ppx_metaquot/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/07/21 14:04:17 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/04/04 19:30:52 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_metaquot
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_metaquot
 OCAML_PPX_METAQUOT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_metaquot+=	ocaml-ppx_metaquot>=0.9.0
+BUILDLINK_API_DEPENDS.ocaml-ppx_metaquot+=	ocaml-ppx_metaquot>=0.9.0nb3
 BUILDLINK_PKGSRCDIR.ocaml-ppx_metaquot?=	../../devel/ocaml-ppx_metaquot
 
 .endif	# OCAML_PPX_METAQUOT_BUILDLINK3_MK
