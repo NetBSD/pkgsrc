bf795dc87b1340ce0193407162d9e894341c4874
jaapb <jaapb@pkgsrc.org>
2018-04-04 19:26:12 +0000

    Revbump for devel/ocaml-ppx_optcomp.
    No upstream changes, just dealing with the files caused to be installed
    by the new version of jbuilder.

--- devel/ocaml-ppx_optcomp/buildlink3.mk
+++ devel/ocaml-ppx_optcomp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/01/07 13:04:08 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/04/04 19:26:12 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_optcomp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_optcomp
 OCAML_PPX_OPTCOMP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_optcomp+=	ocaml-ppx_optcomp>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_optcomp+=	ocaml-ppx_optcomp>=0.9.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_optcomp+=	ocaml-ppx_optcomp>=0.9.0nb3
 BUILDLINK_PKGSRCDIR.ocaml-ppx_optcomp?=		../../devel/ocaml-ppx_optcomp
 
 .include "../../devel/ocaml-ppx_core/buildlink3.mk"
