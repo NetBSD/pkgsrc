31cade06bd905bf853e4ae3361615e7bb6a20ee9
jaapb <jaapb@pkgsrc.org>
2018-04-04 19:27:57 +0000

    Revbump for devel/ocaml-ppx_driver.
    No upstream changes, just dealing with new files caused to be installed
    by the new version of jbuilder.

--- devel/ocaml-ppx_driver/buildlink3.mk
+++ devel/ocaml-ppx_driver/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/01/10 16:06:00 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/04/04 19:27:57 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_driver
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_driver
 OCAML_PPX_DRIVER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_driver+=	ocaml-ppx_driver>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_driver+=	ocaml-ppx_driver>=0.9.2
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_driver+=	ocaml-ppx_driver>=0.9.2nb1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_driver?=	../../devel/ocaml-ppx_driver
 
 #.include "../../devel/ocaml-ppx_optcomp/buildlink3.mk"
