7774ad13cf23f231b30969a4089aa98b4ba9b7f3
jaapb <jaapb@pkgsrc.org>
2018-04-04 19:44:19 +0000

    Revbump for devel/ocaml-ppx_base.
    No upstream changes, just dealing with new files caused to be installed
    by the new version of jbuilder.

--- devel/ocaml-ppx_base/buildlink3.mk
+++ devel/ocaml-ppx_base/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/10/14 10:17:52 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/04/04 19:44:19 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_base
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_base
 OCAML_PPX_BASE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_base+=	ocaml-ppx_base>=0.9.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_base+=	ocaml-ppx_base>=0.9.0nb2
 BUILDLINK_PKGSRCDIR.ocaml-ppx_base?=	../../devel/ocaml-ppx_base
 .endif	# OCAML_PPX_BASE_BUILDLINK3_MK
 
