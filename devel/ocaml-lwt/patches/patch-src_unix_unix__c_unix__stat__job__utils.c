f07442b9eb6f3de49b7baffbe204a3c125e6eb62
jaapb <jaapb@pkgsrc.org>
2018-04-05 11:04:39 +0000

    Updated devel/ocaml-lwt to version 3.3.0.
    This includes minor bugfixes and improvements, but nothing that breaks
    backwards compatibility.

--- devel/ocaml-lwt/patches/patch-src_unix_unix__c_unix__stat__job__utils.c
+++ devel/ocaml-lwt/patches/patch-src_unix_unix__c_unix__stat__job__utils.c
@@ -1,19 +1,19 @@
-$NetBSD: patch-src_unix_lwt__unix__unix.h,v 1.1 2018/02/01 09:40:13 jperkin Exp $
+$NetBSD: patch-src_unix_unix__c_unix__stat__job__utils.c,v 1.1 2018/04/05 11:04:40 jaapb Exp $
 
 Need limits.h for IOV_MAX.
 Avoid system NANOSEC definition.
 
---- src/unix/lwt_unix_unix.h.orig	2017-12-19 21:28:38.000000000 +0000
-+++ src/unix/lwt_unix_unix.h
-@@ -33,6 +33,7 @@
- #include <caml/unixsupport.h>
- #include <caml/version.h>
- #include <dirent.h>
+--- src/unix/unix_c/unix_stat_job_utils.c.orig	2018-01-11 16:40:12.000000000 +0000
++++ src/unix/unix_c/unix_stat_job_utils.c
+@@ -28,6 +28,7 @@
+ #include <caml/alloc.h>
+ #include <caml/memory.h>
+ #include <errno.h>
 +#include <limits.h>
- #include <poll.h>
- #include <sys/resource.h>
- #include <sys/time.h>
-@@ -1288,11 +1289,11 @@ static value copy_stat(int use_64, struc
+ 
+ #include "unix_stat_job_utils.h"
+ 
+@@ -37,11 +38,11 @@ value copy_stat(int use_64, struct stat 
      CAMLlocal5(atime, mtime, ctime, offset, v);
  
      atime = caml_copy_double((double)buf->st_atime +
