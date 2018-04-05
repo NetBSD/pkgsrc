853f57e67c2d0864857169d2e2e7d6ab46bc8cd1
bouyer <bouyer@pkgsrc.org>
2018-04-05 15:56:25 +0000

    Update to 1.5_20180405. Changes since 1.4: Add ability to check for the first line crossing as well as the closest line crossing. Import csv file to define new boundary and boundary points Allow right click in Points and Layers tabs of Pathmanager to Hide/Show items
    And lots of UI and bug fixes, speedup, cleanup.

--- geography/opencpn-plugin-draw/patches/patch-cmake_PluginConfigure.cmake
+++ geography/opencpn-plugin-draw/patches/patch-cmake_PluginConfigure.cmake
@@ -1,9 +1,9 @@
-$NetBSD: patch-cmake_PluginConfigure.cmake,v 1.4 2017/08/24 13:01:33 bouyer Exp $
+$NetBSD: patch-cmake_PluginConfigure.cmake,v 1.5 2018/04/05 15:56:25 bouyer Exp $
 Do not overwrite CMAKE_SHARED_LINKER_FLAGS
 
---- cmake/PluginConfigure.cmake.orig	2017-07-07 00:58:37.000000000 +0200
-+++ cmake/PluginConfigure.cmake	2017-08-24 11:45:09.253375656 +0200
-@@ -46,9 +46,9 @@
+--- cmake/PluginConfigure.cmake.orig	2018-03-02 08:09:23.000000000 +0100
++++ cmake/PluginConfigure.cmake	2018-04-05 17:46:39.601675541 +0200
+@@ -40,9 +40,9 @@
   ENDIF(PROFILING)
  
   IF(NOT APPLE)
@@ -14,4 +14,4 @@ Do not overwrite CMAKE_SHARED_LINKER_FLAGS
 +  SET( CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl -undefined dynamic_lookup")
   ENDIF(NOT APPLE)
  
- 
+ ENDIF(NOT MSVC)
