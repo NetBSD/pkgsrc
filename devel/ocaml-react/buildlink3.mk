2d751ec892755a5e745867334efa4fe59943bdaa
jaapb <jaapb@pkgsrc.org>
2018-04-07 10:38:59 +0000

    Revbump for devel/ocaml-react.
    No upstream changes, just dealing with new files installed by the new
    version of jbuilder.

--- devel/ocaml-react/buildlink3.mk
+++ devel/ocaml-react/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2017/07/11 11:02:44 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/04/07 10:38:59 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-react
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-react
 OCAML_REACT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-react+=	ocaml-react>=1.2.0nb1
-BUILDLINK_ABI_DEPENDS.ocaml-react+=	ocaml-react>=1.2.1
+BUILDLINK_ABI_DEPENDS.ocaml-react+=	ocaml-react>=1.2.1nb3
 BUILDLINK_PKGSRCDIR.ocaml-react?=	../../devel/ocaml-react
 .endif	# OCAML_REACT_BUILDLINK3_MK
 
