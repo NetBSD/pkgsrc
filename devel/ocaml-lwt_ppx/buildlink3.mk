87f7961fca09dbf711b6bdadbc082978a2e14d8b
jaapb <jaapb@pkgsrc.org>
2018-04-09 08:58:47 +0000

    Added package devel/ocaml-lwt_ppx at version 1.1.0.
    This was previously part of devel/ocaml-lwt and has now been spun off into
    its own package.

---
+++ devel/ocaml-lwt_ppx/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2018/04/09 08:58:47 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-lwt_ppx
+
+.if !defined(OCAML_LWT_PPX_BUILDLINK3_MK)
+OCAML_LWT_PPX_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-lwt_ppx+=	ocaml-lwt_ppx>=1.1.0
+BUILDLINK_ABI_DEPENDS.ocaml-lwt_ppx+=	ocaml-lwt_ppx>=1.1.0nb3
+BUILDLINK_PKGSRCDIR.ocaml-lwt_ppx?=	../../devel/ocaml-lwt_ppx
+
+.include "../../devel/ocaml-lwt/buildlink3.mk"
+.endif	# OCAML_LWT_PPX_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-lwt_ppx
