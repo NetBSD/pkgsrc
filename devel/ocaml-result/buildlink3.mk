7cc6382948dbe4763c770642ca2ba196c0b7634a
jaapb <jaapb@pkgsrc.org>
2018-04-09 09:36:08 +0000

    Updated package devel/ocaml-result to version 1.3.
    This switches the build system to jbuilder.

--- devel/ocaml-result/buildlink3.mk
+++ devel/ocaml-result/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/01/10 16:53:10 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/04/09 09:36:08 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-result
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-result
 OCAML_RESULT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-result+=	ocaml-result>=1.2
-BUILDLINK_ABI_DEPENDS.ocaml-result+=	ocaml-result>=1.2nb4
+BUILDLINK_ABI_DEPENDS.ocaml-result+=	ocaml-result>=1.3
 BUILDLINK_PKGSRCDIR.ocaml-result?=	../../devel/ocaml-result
 .endif	# OCAML_RESULT_BUILDLINK3_MK
 
