ded41762a606c0a5d671c46a30a75854d26afbf8
jaapb <jaapb@pkgsrc.org>
2018-04-13 13:55:27 +0000

    Revbump associated with the upgrade of lang/ocaml
    (this is the upgrade from 4.06 to 4.06.1)

--- security/ocaml-cryptokit/buildlink3.mk
+++ security/ocaml-cryptokit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2018/01/10 16:53:12 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2018/04/13 13:55:28 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cryptokit
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-cryptokit
 OCAML_CRYPTOKIT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-cryptokit+=	ocaml-cryptokit>=1.5
-BUILDLINK_ABI_DEPENDS.ocaml-cryptokit?=	ocaml-cryptokit>=1.11nb2
+BUILDLINK_ABI_DEPENDS.ocaml-cryptokit?=	ocaml-cryptokit>=1.11nb3
 BUILDLINK_PKGSRCDIR.ocaml-cryptokit?=	../../security/ocaml-cryptokit
 
 .include "../../math/ocaml-zarith/buildlink3.mk"
