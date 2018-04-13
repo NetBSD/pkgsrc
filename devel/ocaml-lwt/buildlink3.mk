260fe2493492f58528e12617a2c4d0a0a81e235d
jaapb <jaapb@pkgsrc.org>
2018-04-13 12:52:00 +0000

    Updated devel/ocaml-lwt to version 4.0.1.
    4.0.0 is a major release that breaks quite a few interfaces, such as:
    
    * Delete package lwt.ppx. The PPX syntax is in package lwt_ppx since
      Lwt 3.2.0.
    * Remove >> syntax from the PPX.
    * Delete modules Lwt_log, Lwt_daemon, Lwt_log_core, and package lwt.log.
      These are in package lwt_log since Lwt 3.2.0, but it is recommended to
      use Logs_lwt from the logs library instead (initiated Hannes Mehnert).
    * Delete package lwt.preemptive. It is an alias for lwt.unix since
      Lwt 3.2.0.
    * Delete package lwt.syntax. The Camlp4 syntax is in package lwt_camlp4
      since Lwt 3.2.0.
    * Delete module Lwt_chan, a predecessor of Lwt_io.
    * Delete package lwt.simple-top, a predecessor of utop.
    * Make resolvers (Lwt.u) contravariant.
    
    (for an upgrade path see the upstream Github site)
    
    4.0.1 is a bugfix release.

--- devel/ocaml-lwt/buildlink3.mk
+++ devel/ocaml-lwt/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2018/04/05 11:04:39 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2018/04/13 12:52:00 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt
 
 .if !defined(OCAML_LWT_BUILDLINK3_MK)
 OCAML_LWT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-lwt+=	ocaml-lwt>=3.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt+=	ocaml-lwt>=3.3.0
+BUILDLINK_API_DEPENDS.ocaml-lwt+=	ocaml-lwt>=4.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-lwt+=	ocaml-lwt>=4.0.1
 BUILDLINK_PKGSRCDIR.ocaml-lwt?=		../../devel/ocaml-lwt
 
 #.include "../../devel/libev/buildlink3.mk"
