fba07b701a0ecb74525c981f82856b7f443a7832
jaapb <jaapb@pkgsrc.org>
2018-04-13 12:54:03 +0000

    Updated devel/ocaml-lwt_react to version 1.1.1.
    This upgrade was introduced with Lwt 4.0.0. It fixes a bug with
    Lwt_react.E.limit and Lwt_react.S.limit.

--- devel/ocaml-lwt_react/buildlink3.mk
+++ devel/ocaml-lwt_react/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/01/10 16:53:09 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/04/13 12:54:03 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt_react
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-lwt_react
 OCAML_LWT_REACT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-lwt_react+=	ocaml-lwt_react>=1.1.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt_react+=	ocaml-lwt_react>=1.1.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-lwt_react+=	ocaml-lwt_react>=1.1.1
 BUILDLINK_PKGSRCDIR.ocaml-lwt_react?=	../../devel/ocaml-lwt_react
 
 .include "../../devel/ocaml-lwt/buildlink3.mk"
