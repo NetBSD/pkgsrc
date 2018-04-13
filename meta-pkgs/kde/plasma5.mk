f1769d27e16d79169f9b3efafe8fdd9a697b9230
markd <markd@pkgsrc.org>
2018-04-13 22:02:39 +0000

    kde: dont need to extract with gtar any more

--- meta-pkgs/kde/plasma5.mk
+++ meta-pkgs/kde/plasma5.mk
@@ -1,10 +1,10 @@
-# $NetBSD: plasma5.mk,v 1.1 2018/04/11 11:34:57 markd Exp $
+# $NetBSD: plasma5.mk,v 1.2 2018/04/13 22:02:39 markd Exp $
 
 PLASMAVER?=	5.12.4
 CATEGORIES+=	kde
 MASTER_SITES=	${MASTER_SITE_KDE:=plasma/${PLASMAVER}/}
 EXTRACT_SUFX=	.tar.xz
 
-EXTRACT_USING=	gtar
+#EXTRACT_USING=	gtar
 
 .include "../../meta-pkgs/kde/Makefile.common"
