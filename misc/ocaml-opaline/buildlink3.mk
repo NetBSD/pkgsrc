ab12420483f6bacf6c4c85fa82423b3a033a7090
jaapb <jaapb@pkgsrc.org>
2018-04-13 12:27:02 +0000

    Updated misc/ocaml-opaline to version 0.3.0.
    Now supports optional files.

--- misc/ocaml-opaline/buildlink3.mk
+++ misc/ocaml-opaline/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/04/07 10:33:39 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/04/13 12:27:02 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-opaline
 
 .if !defined(OCAML_OPALINE_BUILDLINK3_MK)
 OCAML_OPALINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-opaline+=	ocaml-opaline>=0.2.2
+BUILDLINK_API_DEPENDS.ocaml-opaline+=	ocaml-opaline>=0.3.0
 BUILDLINK_PKGSRCDIR.ocaml-opaline?=	../../misc/ocaml-opaline
 
 # We only need this to install
