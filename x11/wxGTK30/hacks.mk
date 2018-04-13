270c017d08eb70635c084c483c17191b60b4e5d8
bouyer <bouyer@pkgsrc.org>
2018-04-13 11:43:55 +0000

    gcc 6.x also has problems with precompiled headers (found on earmv7hf).

--- x11/wxGTK30/hacks.mk
+++ x11/wxGTK30/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.6 2017/07/15 19:10:31 wiz Exp $
+# $NetBSD: hacks.mk,v 1.7 2018/04/13 11:43:55 bouyer Exp $
 
 ### [Mon Dec 14 16:58:00 MET 2015 : bouyer ]
 ### g++-4.8 on NetBSD/i386 fails with an internal compiler error
@@ -7,7 +7,7 @@
 .if ${OPSYS} == "NetBSD"
 .  include "../../mk/compiler.mk"
 .  if !empty(PKGSRC_COMPILER:Mgcc) && \
-      (!empty(CC_VERSION:Mgcc-4.8.*) || !empty(CC_VERSION:Mgcc-5.*))
+      (!empty(CC_VERSION:Mgcc-4.8.*) || !empty(CC_VERSION:Mgcc-5.*) || !empty(CC_VERSION:Mgcc-6.*))
 CONFIGURE_ARGS+=	--disable-precomp-headers
 .  endif
 .endif
