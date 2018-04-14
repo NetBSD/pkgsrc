2a53c9f5ff52b84a9b77a08caa71a1293abf561c
markd <markd@pkgsrc.org>
2018-04-14 03:33:19 +0000

    kde: default applications version to 18.04.0

--- meta-pkgs/kde/applications.mk
+++ meta-pkgs/kde/applications.mk
@@ -1,6 +1,6 @@
-# $NetBSD: applications.mk,v 1.1 2018/04/11 11:34:57 markd Exp $
+# $NetBSD: applications.mk,v 1.2 2018/04/14 03:33:19 markd Exp $
 
-KAPPSVER?=	17.12.3
+KAPPSVER?=	18.04.0
 CATEGORIES+=	kde
 MASTER_SITES?=	${MASTER_SITE_KDE:=applications/${KAPPSVER}/src/}
 EXTRACT_SUFX?=	.tar.xz
