582b8d4c3fd4f46ebf1ae00ef8229abb6c20456e
wiz <wiz@pkgsrc.org>
2018-04-22 22:11:47 +0000

    py-qt4-qscintilla: fix buildlink3.mk depends pattern

--- x11/py-qt4-qscintilla/buildlink3.mk
+++ x11/py-qt4-qscintilla/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.28 2018/03/12 11:15:55 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.29 2018/04/22 22:11:47 wiz Exp $
 
 BUILDLINK_TREE+=	py-qt4-qscintilla
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	py-qt4-qscintilla
 PY_QT4_QSCINTILLA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.py-qt4-qscintilla+=	${PYPKGPREFIX}-qt4-qscintilla>=2.4.3
-BUILDLINK_ABI_DEPENDS.py-qt4-qscintilla+=	py36-qt4-qscintilla>=2.9.1nb2
+BUILDLINK_ABI_DEPENDS.py-qt4-qscintilla+=	${PYPKGPREFIX}-qt4-qscintilla>=2.9.1nb2
 BUILDLINK_PKGSRCDIR.py-qt4-qscintilla?=		../../x11/py-qt4-qscintilla
 
 .include "../../x11/py-sip/buildlink3.mk"
