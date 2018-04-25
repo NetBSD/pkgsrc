75e571256d1e9b3436103c5a55722b391d6fe5d2
markd <markd@pkgsrc.org>
2018-04-25 05:53:53 +0000

    abiword: build with libical-3

--- editors/abiword/patches/patch-src_text_ptbl_xp_pd__DocumentRDF.cpp
+++ editors/abiword/patches/patch-src_text_ptbl_xp_pd__DocumentRDF.cpp
@@ -1,10 +1,12 @@
-$NetBSD: patch-src_text_ptbl_xp_pd__DocumentRDF.cpp,v 1.1 2015/09/13 21:32:13 nros Exp $
+$NetBSD: patch-src_text_ptbl_xp_pd__DocumentRDF.cpp,v 1.2 2018/04/25 05:53:53 markd Exp $
 
 * fixes build on FreeBSD 10 with clang. From FreeBSD ports mailing list.
   Applied upstream.
   https://lists.freebsd.org/pipermail/freebsd-ports/2015-March/098621.html
 
---- src/text/ptbl/xp/pd_DocumentRDF.cpp.orig	2015-04-04 22:33:15.000000000 +0000
+* fix using modern libical
+
+--- src/text/ptbl/xp/pd_DocumentRDF.cpp.orig	2014-02-27 05:11:32.000000000 +0000
 +++ src/text/ptbl/xp/pd_DocumentRDF.cpp
 @@ -570,6 +570,11 @@ bool PD_URI::operator==(const std::strin
      return m_value == b;
@@ -30,3 +32,14 @@ $NetBSD: patch-src_text_ptbl_xp_pd__DocumentRDF.cpp,v 1.1 2015/09/13 21:32:13 nr
  bool operator<( std::pair< PD_URI, PD_URI > a, PD_URI b)
  {
      return a.first.toString() < b.toString();
+@@ -2269,8 +2269,8 @@ PD_RDFEvent::exportToFile( const std::st
+         icalcomponent_set_uid( c,         m_uid.c_str() );
+         icalcomponent_set_location( c,    m_location.c_str() );
+         icalcomponent_set_description( c, m_desc.c_str() );
+-        icalcomponent_set_dtstart( c,     icaltime_from_timet( m_dtstart, 0 ) );
+-        icalcomponent_set_dtend( c,       icaltime_from_timet( m_dtend, 0 ) );
++        icalcomponent_set_dtstart( c,     icaltime_from_timet_with_zone( m_dtstart, 0, 0 ) );
++        icalcomponent_set_dtend( c,       icaltime_from_timet_with_zone( m_dtend, 0, 0 ) );
+ 
+         char* data = icalcomponent_as_ical_string( c );
+         std::ofstream oss( filename.c_str() );
