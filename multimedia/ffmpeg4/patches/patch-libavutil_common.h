5822f37ede2ed308a43366210f3f905b302cf208
adam <adam@pkgsrc.org>
2018-04-26 07:56:57 +0000

    ffmpeg4/ffplay4: added version 4.0
    version 4.0:
    - Bitstream filters for editing metadata in H.264, HEVC and MPEG-2 streams
    - Dropped support for OpenJPEG versions 2.0 and below. Using OpenJPEG now
      requires 2.1 (or later) and pkg-config.
    - VDA dropped (use VideoToolbox instead)
    - MagicYUV encoder
    - Raw AMR-NB and AMR-WB demuxers
    - TiVo ty/ty+ demuxer
    - Intel QSV-accelerated MJPEG encoding
    - PCE support for extended channel layouts in the AAC encoder
    - native aptX and aptX HD encoder and decoder
    - Raw aptX and aptX HD muxer and demuxer
    - NVIDIA NVDEC-accelerated H.264, HEVC, MJPEG, MPEG-1/2/4, VC1, VP8/9 hwaccel decoding
    - Intel QSV-accelerated overlay filter
    - mcompand audio filter
    - acontrast audio filter
    - OpenCL overlay filter
    - video mix filter
    - video normalize filter
    - audio lv2 wrapper filter
    - VAAPI MJPEG and VP8 decoding
    - AMD AMF H.264 and HEVC encoders
    - video fillborders filter
    - video setrange filter
    - nsp demuxer
    - support LibreSSL (via libtls)
    - AVX-512/ZMM support added
    - Dropped support for building for Windows XP. The minimum supported Windows
      version is Windows Vista.
    - deconvolve video filter
    - entropy video filter
    - hilbert audio filter source
    - aiir audio filter
    - aiff: add support for CD-ROM XA ADPCM
    - Removed the ffserver program
    - Removed the ffmenc and ffmdec muxer and demuxer
    - VideoToolbox HEVC encoder and hwaccel
    - VAAPI-accelerated ProcAmp (color balance), denoise and sharpness filters
    - Add android_camera indev
    - codec2 en/decoding via libcodec2
    - muxer/demuxer for raw codec2 files and .c2 files
    - Moved nvidia codec headers into an external repository.
      They can be found at http://git.videolan.org/?p=ffmpeg/nv-codec-headers.git
    - native SBC encoder and decoder
    - drmeter audio filter
    - hapqa_extract bitstream filter
    - filter_units bitstream filter
    - AV1 Support through libaom
    - E-AC-3 dependent frames support
    - bitstream filter for extracting E-AC-3 core
    - Haivision SRT protocol via libsrt
    - segafilm muxer
    - vfrdet filter

---
+++ multimedia/ffmpeg4/patches/patch-libavutil_common.h
@@ -0,0 +1,29 @@
+$NetBSD: patch-libavutil_common.h,v 1.1 2018/04/26 07:56:57 adam Exp $
+
+Add required int includes on various opensource platforms.
+
+--- libavutil/common.h.orig	2016-01-15 16:58:36.000000000 +0000
++++ libavutil/common.h
+@@ -43,6 +43,22 @@
+ #include "version.h"
+ #include "libavutil/avconfig.h"
+ 
++#if defined(__cplusplus)
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__linux__)
++#undef _STDINT_H_
++#undef _SYS_STDINT_H_
++#undef _STDINT_H
++#undef _GCC_WRAP_STDINT_H
++#ifndef __STDC_CONSTANT_MACROS
++#define __STDC_CONSTANT_MACROS
++#endif
++#include <stdint.h>
++#endif /* FreeBSD | OpenBSD | linux */
++#if defined(__DragonFly__) || defined(__NetBSD__)
++#include <machine/int_const.h>
++#endif /* DragonFly | NetBSD */
++#endif /* __cplusplus */
++
+ #if AV_HAVE_BIGENDIAN
+ #   define AV_NE(be, le) (be)
+ #else
