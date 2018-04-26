7754051b24197ccbf609957b3e118edf41e5e912
adam <adam@pkgsrc.org>
2018-04-26 07:56:57 +0000

    ffmpeg4/ffplay4: added version 4.0
    version 4.0:
    - Bitstream filters for editing metadata in H.264, HEVC and MPEG-2 streams
    - Dropped support for OpenJPEG versions 2.0 and below. Using OpenJPEG now
      requires 2.1 (or later) and pkg-config.
    - VDA dropped (use VideoToolbox instead)
    - MagicYUV encoder
    - Raw AMR-NB and AMR-WB demuxers
    - TiVo ty/ty+ demuxer
    - Intel QSV-accelerated MJPEG encoding
    - PCE support for extended channel layouts in the AAC encoder
    - native aptX and aptX HD encoder and decoder
    - Raw aptX and aptX HD muxer and demuxer
    - NVIDIA NVDEC-accelerated H.264, HEVC, MJPEG, MPEG-1/2/4, VC1, VP8/9 hwaccel decoding
    - Intel QSV-accelerated overlay filter
    - mcompand audio filter
    - acontrast audio filter
    - OpenCL overlay filter
    - video mix filter
    - video normalize filter
    - audio lv2 wrapper filter
    - VAAPI MJPEG and VP8 decoding
    - AMD AMF H.264 and HEVC encoders
    - video fillborders filter
    - video setrange filter
    - nsp demuxer
    - support LibreSSL (via libtls)
    - AVX-512/ZMM support added
    - Dropped support for building for Windows XP. The minimum supported Windows
      version is Windows Vista.
    - deconvolve video filter
    - entropy video filter
    - hilbert audio filter source
    - aiir audio filter
    - aiff: add support for CD-ROM XA ADPCM
    - Removed the ffserver program
    - Removed the ffmenc and ffmdec muxer and demuxer
    - VideoToolbox HEVC encoder and hwaccel
    - VAAPI-accelerated ProcAmp (color balance), denoise and sharpness filters
    - Add android_camera indev
    - codec2 en/decoding via libcodec2
    - muxer/demuxer for raw codec2 files and .c2 files
    - Moved nvidia codec headers into an external repository.
      They can be found at http://git.videolan.org/?p=ffmpeg/nv-codec-headers.git
    - native SBC encoder and decoder
    - drmeter audio filter
    - hapqa_extract bitstream filter
    - filter_units bitstream filter
    - AV1 Support through libaom
    - E-AC-3 dependent frames support
    - bitstream filter for extracting E-AC-3 core
    - Haivision SRT protocol via libsrt
    - segafilm muxer
    - vfrdet filter

---
+++ multimedia/ffmpeg4/buildlink3.mk
@@ -0,0 +1,59 @@
+# $NetBSD: buildlink3.mk,v 1.1 2018/04/26 07:56:57 adam Exp $
+
+BUILDLINK_TREE+=	ffmpeg4
+
+.if !defined(FFMPEG4_BUILDLINK3_MK)
+FFMPEG4_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ffmpeg4+=	ffmpeg4>=4.0
+BUILDLINK_ABI_DEPENDS.ffmpeg4+=	ffmpeg4>=4.0
+BUILDLINK_PKGSRCDIR.ffmpeg4?=	../../multimedia/ffmpeg4
+
+pkgbase := ffmpeg4
+.include "../../mk/pkg-build-options.mk"
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg4:Mfreetype)
+.  include "../../graphics/freetype2/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg4:Mfontconfig)
+.  include "../../fonts/fontconfig/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg4:Mtheora)
+.  include "../../multimedia/libtheora/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg4:Mxvid)
+.  include "../../multimedia/xvidcore/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg4:Mx264)
+.  include "../../multimedia/x264-devel/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg4:Mlibvpx)
+.  include "../../multimedia/libvpx/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg4:Mvaapi)
+.  include "../../multimedia/libva/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ffmpeg4:Mvdpau)
+.  include "../../multimedia/libvdpau/buildlink3.mk"
+.endif
+
+BUILDLINK_INCDIRS.ffmpeg4+=		include/ffmpeg4
+BUILDLINK_LIBDIRS.ffmpeg4+=		lib/ffmpeg4
+BUILDLINK_FNAME_TRANSFORM.ffmpeg4+=	-e 's|lib/ffmpeg4/pkgconfig/|lib/pkgconfig/|'
+
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../archivers/xz/buildlink3.mk"
+.include "../../devel/libgetopt/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.endif # FFMPEG4_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ffmpeg4
