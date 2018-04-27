ec0424fb5ccde14e0d1cfc48345a9410cb6bea52
adam <adam@pkgsrc.org>
2018-04-27 07:45:09 +0000

    py-raven: updated to 6.7.0
    6.7.0:
    [Sanic] Added support for sanic.
    [Core] Disabled dill logger by default
    [Core] Added SENTRY_NAME, SENTRY_ENVIRONMENT and SENTRY_RELEASE environment variables
    [Core] DSN secret is now optional
    [Core] Added fix for cases with exceptions in repr
    [core] Fixed bug with mutating record.data

--- www/py-raven/patches/patch-setup.py
+++ www/py-raven/patches/patch-setup.py
@@ -1,33 +1,37 @@
-$NetBSD: patch-setup.py,v 1.4 2018/03/05 09:08:22 adam Exp $
+$NetBSD: patch-setup.py,v 1.5 2018/04/27 07:45:09 adam Exp $
 
 Relax requirements.
 
---- setup.py.orig	2018-02-03 03:03:31.000000000 +0000
+--- setup.py.orig	2018-04-18 09:28:51.000000000 +0000
 +++ setup.py
-@@ -61,19 +61,19 @@ tests_require = [
+@@ -67,21 +67,21 @@ if sys.version_info >= (3, 5):
+ tests_require = [
+     'bottle',
      'celery>=2.5',
-     'coverage<4',
+-    'coverage<4',
++    'coverage',
      'exam>=0.5.2',
 -    'flake8==3.5.0',
 +    'flake8>=3.5.0',
      'logbook',
      'mock',
      'nose',
-     'pycodestyle',
      'pytz',
 -    'pytest>=3.2.0,<3.3.0',
--    'pytest-timeout==1.2.0',
+-    'pytest-timeout==1.2.1',
 -    'pytest-xdist==1.18.2',
--    'pytest-pythonpath==0.7.1',
--    'pytest-sugar==0.9.0',
+-    'pytest-pythonpath==0.7.2',
+-    'pytest-cov==2.5.1',
+-    'pytest-flake8==1.0.0',
 +    'pytest>=3.2.0',
-+    'pytest-timeout>=1.2.0',
++    'pytest-timeout>=1.2.1',
 +    'pytest-xdist>=1.18.2',
 +    'pytest-pythonpath>=0.7.1',
-+    'pytest-sugar>=0.9.0',
-     'pytest-cov',
--    'pytest-flake8==0.9.1',
-+    'pytest-flake8>=0.9.1',
++    'pytest-cov>=2.5.1',
++    'pytest-flake8>=1.0.0',
      'requests',
-     'tornado>=4.1',
+-    'tornado>=4.1,<5.0',
++    'tornado>=4.1',
      'tox',
+     'webob',
+     'webtest',
