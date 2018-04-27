93af52542247d63df46e0e18a3ceb55fee96d3fb
minskim <minskim@pkgsrc.org>
2018-04-27 17:47:27 +0000

    net/grpc: Update to 1.11.0
    Notable changes since 1.7.2:
    - Requires protobuf>=3.5.0
    - Exec_ctx has been made a thread_local, and is no longer to be passed
      as a function parameter.
    - LB policies request re-resolution without shutting down
    - On server, include receiving HTTP/2 settings in handshake timeout
    - C++ headers are moved from include/grpc++ to include/grpcpp. Headers
      in include/grpc++ are deprecated
    - Experimental gRPC-C++ Cocoapods podspec
    - Several features of core have been removed from the surface or GPR
      API: grpc_alarm, gpr_join_host_port, gpr_cmdline, gpr_subprocess,
      gpr_tls, gpr_avl, and gpr_thd
    - Add core underpinnings for TLS session ticket support
    - Experimental support for configurable	retries

--- net/grpc/buildlink3.mk
+++ net/grpc/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/10/23 15:48:04 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/04/27 17:47:27 minskim Exp $
 
 BUILDLINK_TREE+=	grpc
 
 .if !defined(GRPC_BUILDLINK3_MK)
 GRPC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.grpc+=	grpc>=1.6.7
+BUILDLINK_API_DEPENDS.grpc+=	grpc>=1.11.0
 BUILDLINK_PKGSRCDIR.grpc?=	../../net/grpc
 
+.include "../../devel/protobuf/buildlink3.mk"
 .endif	# GRPC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-grpc
