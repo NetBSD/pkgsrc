7a0f9f7345a24d30b27e1b49168240b81091b62d
wiz <wiz@pkgsrc.org>
2018-04-28 19:28:20 +0000

    ffmpegthumbnailer: update to 2.2.0.
    FFmpegThumbnailer 2.2.0
    - New option to prefer embedded image metadata over video content (command line argument: -m)
    - Fixed 'Resource temporarily unavailable' errors when using ffmpeg < 3.0
    
    FFmpegThumbnailer 2.1.2
    - Restored deinterlace functionality using the libavfilter library (additional ffmpeg dependency)
    - Additional video mime types added to the thumbnailer file
    - Take rotation metadata into account when generating thumbnails
    
    FFmpegThumbnailer 2.1.1
    - Buildable agains latest ffmpeg (currently breaks deinterlacing)
    - Fallback when smart frame detection fails (thanks to johnnydeez)
    - Add MPEG-TS (MTS) support (thanks to marcinn)
    
    FFmpegThumbnailer 2.1.0
    - The library is now completely silent on std out and err by default
    - A callback can be registered to listen to logging messages
    - Fixed cmake installation directories (thanks to Vallimar)
    - Fixed dimension calculation for anamorphic streams (Thanks to Richard Zurad)
    - Fixed static library filename
    
    FFmpegThumbnailer 2.0.10
    - Migrated to github
    - Use cmake instead of autotools
    - Remove stderr logging
    
    FFmpegThumbnailer 2.0.9
    - Fixed gio library loading issue
    - Support udp sources (thanks to tchristensen)
    - No longer support older ffmpeg versions

--- multimedia/ffmpegthumbnailer/buildlink3.mk
+++ multimedia/ffmpegthumbnailer/buildlink3.mk
@@ -1,16 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/01/07 13:04:24 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/04/28 19:28:20 wiz Exp $
 
 BUILDLINK_TREE+=	ffmpegthumbnailer
 
 .if !defined(FFMPEGTHUMBNAILER_BUILDLINK3_MK)
 FFMPEGTHUMBNAILER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ffmpegthumbnailer+=	ffmpegthumbnailer>=2.0.6nb1
-BUILDLINK_ABI_DEPENDS.ffmpegthumbnailer?=	ffmpegthumbnailer>=2.0.8nb1
+BUILDLINK_API_DEPENDS.ffmpegthumbnailer+=	ffmpegthumbnailer>=2.2.0
 BUILDLINK_PKGSRCDIR.ffmpegthumbnailer?=		../../multimedia/ffmpegthumbnailer
 
 #.include "../../graphics/png/buildlink3.mk"
-#.include "../../multimedia/ffmpeg1/buildlink3.mk"
+#.include "../../multimedia/ffmpeg3/buildlink3.mk"
 .endif	# FFMPEGTHUMBNAILER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ffmpegthumbnailer
