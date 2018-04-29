de0dead0b9f496ddbdb2ee01b41d5cc001a1afb2
wiz <wiz@pkgsrc.org>
2018-04-29 05:50:39 +0000

    vim*: update to 8.0.1772
    Latest bugfixes.

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.114 2018/04/16 10:19:37 morr Exp $
+# $NetBSD: version.mk,v 1.115 2018/04/29 05:50:39 wiz Exp $
 
 VIM_VERSION=		8.0
-VIM_PATCHLEVEL=		1722
+VIM_PATCHLEVEL=		1772
 # Changelog: see http://ftp.vim.org/pub/vim/patches/8.0/
 VIM_SUBDIR=		vim80
 
