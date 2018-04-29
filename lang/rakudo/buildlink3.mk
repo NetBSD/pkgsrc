34ac2b25fd8996ed29c1bb27fdb55f9ffb96e051
adam <adam@pkgsrc.org>
2018-04-29 20:53:39 +0000

    rakudo: removed references to wip

--- lang/rakudo/buildlink3.mk
+++ lang/rakudo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/09/26 17:23:05 mspo Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/04/29 20:53:39 adam Exp $
 
 BUILDLINK_TREE+=	rakudo
 
@@ -7,9 +7,9 @@ RAKUDO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.rakudo+=		rakudo>=2017.09
 BUILDLINK_ABI_DEPENDS.rakudo+=		rakudo>=2017.09
-BUILDLINK_PKGSRCDIR.rakudo?=		../../wip/rakudo
+BUILDLINK_PKGSRCDIR.rakudo?=		../../lang/rakudo
 
-.include "../../wip/nqp/buildlink3.mk"
+.include "../../lang/nqp/buildlink3.mk"
 .endif # RAKUDO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-rakudo
