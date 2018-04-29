232d98b74d5b2a1448855a8abc752d15224af5e1
adam <adam@pkgsrc.org>
2018-04-29 21:00:04 +0000

    ncbi-blast+: removed references to wip

--- biology/ncbi-blast+/buildlink3.mk
+++ biology/ncbi-blast+/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/04/27 20:28:28 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/04/29 21:00:04 adam Exp $
 
 BUILDLINK_TREE+=	ncbi-blast+
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ncbi-blast+
 NCBI_BLAST+_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ncbi-blast++=	ncbi-blast+>=2.7.1
-BUILDLINK_PKGSRCDIR.ncbi-blast+?=	../../wip/ncbi-blast+
+BUILDLINK_PKGSRCDIR.ncbi-blast+?=	../../biology/ncbi-blast+
 
 .endif	# NCBI_BLAST+_BUILDLINK3_MK
 
