35aa3efc12adc770206abbe3193f13f971afe0c0
adam <adam@pkgsrc.org>
2018-04-29 21:31:17 +0000

    revbump for boost-libs update

--- converters/librevenge/buildlink3.mk
+++ converters/librevenge/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2018/01/01 21:18:07 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2018/04/29 21:31:19 adam Exp $
 
 BUILDLINK_TREE+=	librevenge
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	librevenge
 LIBREVENGE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.librevenge+=	librevenge>=0.0.1
-BUILDLINK_ABI_DEPENDS.librevenge+=	librevenge>=0.0.4nb5
+BUILDLINK_ABI_DEPENDS.librevenge+=	librevenge>=0.0.4nb6
 BUILDLINK_PKGSRCDIR.librevenge?=	../../converters/librevenge
 
 .include "../../devel/boost-libs/buildlink3.mk"
--- mail/libetpan/buildlink3.mk
+++ mail/libetpan/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.36 2018/01/07 13:04:21 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.37 2018/04/29 21:31:21 adam Exp $
 
 BUILDLINK_TREE+=	libetpan
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libetpan
 LIBETPAN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libetpan+=	libetpan>=1.0
-BUILDLINK_ABI_DEPENDS.libetpan+=	libetpan>=1.8nb2
+BUILDLINK_ABI_DEPENDS.libetpan+=	libetpan>=1.8nb3
 BUILDLINK_PKGSRCDIR.libetpan?=		../../mail/libetpan
 
 BDB_ACCEPTED?=	db4 db5 db6
