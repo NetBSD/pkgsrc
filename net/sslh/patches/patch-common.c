5114308b44197388730a44f2e1fc29a51812157f
wiz <wiz@pkgsrc.org>
2018-04-29 09:41:15 +0000

    sslh: update to 1.19c.
    v1.19: 20JAN2018
    	Added 'syslog_facility' configuration option to
    	specify where to log.
    
    	TLS now supports SNI and ALPN (Travis Burtrum),
    	including support for Let's Encrypt challenges
    	(Jonathan McCrohan)
    
    	ADB probe. (Mike Frysinger)
    
    	Added per-protocol 'fork' option. (Oleg Oshmyan)
    
    	Added chroot option. (Mike Frysinger)
    
    	A truckload of bug fixes and documentation
    	improvements (Various contributors)

--- net/sslh/patches/patch-common.c
+++ net/sslh/patches/patch-common.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-common.c,v 1.1 2017/08/15 13:13:36 jperkin Exp $
+$NetBSD: patch-common.c,v 1.2 2018/04/29 09:41:16 wiz Exp $
 
 Avoid queue variable conflict.
 
---- common.c.orig	2016-03-29 19:19:05.000000000 +0000
+--- common.c.orig	2018-02-11 17:42:21.000000000 +0000
 +++ common.c
-@@ -237,7 +237,7 @@ int connect_addr(struct connection *cnx,
+@@ -287,7 +287,7 @@ int connect_addr(struct connection *cnx,
  }
  
  /* Store some data to write to the queue later */
@@ -12,8 +12,8 @@ Avoid queue variable conflict.
 +int defer_write(struct sslhqueue *q, void* data, int data_size)
  {
      char *p;
-     if (verbose)
-@@ -261,7 +261,7 @@ int defer_write(struct queue *q, void* d
+     ptrdiff_t data_offset = q->deferred_data - q->begin_deferred_data;
+@@ -313,7 +313,7 @@ int defer_write(struct queue *q, void* d
   * Upon success, the number of bytes written is returned.
   * Upon failure, -1 returned (e.g. connexion closed)
   * */
@@ -22,7 +22,7 @@ Avoid queue variable conflict.
  {
      int n;
  
-@@ -313,7 +313,7 @@ void dump_connection(struct connection *
+@@ -365,7 +365,7 @@ void dump_connection(struct connection *
   * returns FD_STALLED if data was read, could not be written, and has been
   * stored in temporary buffer.
   */
@@ -31,16 +31,16 @@ Avoid queue variable conflict.
  {
     char buffer[BUFSIZ];
     int target, from, size_r, size_w;
-@@ -594,7 +594,7 @@ void setup_syslog(const char* bin_name)
-     int res;
+@@ -658,7 +658,7 @@ void setup_syslog(const char* bin_name) 
+     int res, fn;
  
      name1 = strdup(bin_name);
 -    res = asprintf(&name2, "%s[%d]", basename(name1), getpid());
 +    res = asprintf(&name2, "%s[%d]", basename(name1), (int)getpid());
      CHECK_RES_DIE(res, "asprintf");
-     openlog(name2, LOG_CONS, LOG_AUTH);
-     free(name1);
-@@ -694,7 +694,7 @@ void write_pid_file(const char* pidfile)
+ 
+     for (fn = 0; facilitynames[fn].c_val != -1; fn++)
+@@ -781,7 +781,7 @@ void write_pid_file(const char* pidfile)
          exit(3);
      }
  
