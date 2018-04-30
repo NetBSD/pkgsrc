e9dd4cbd2453e08e017c8bc4ee019289f2ddf599
wiz <wiz@pkgsrc.org>
2018-04-30 10:23:49 +0000

    unison2.51: add upstream commit as comment.

--- net/unison2.51/patches/patch-src_uigtk2.ml
+++ net/unison2.51/patches/patch-src_uigtk2.ml
@@ -1,6 +1,8 @@
-$NetBSD: patch-src_uigtk2.ml,v 1.1 2018/04/30 10:14:29 wiz Exp $
+$NetBSD: patch-src_uigtk2.ml,v 1.2 2018/04/30 10:23:49 wiz Exp $
 
 Lablgtk now has safe_string
+https://github.com/bcpierce00/unison/commit/2e7ea9481c6c3ff2ec513c39f73cfe15c0763c06
+
 --- src/uigtk2.ml.orig	2015-10-05 18:14:23.000000000 +0000
 +++ src/uigtk2.ml
 @@ -94,7 +94,7 @@ let icon =
