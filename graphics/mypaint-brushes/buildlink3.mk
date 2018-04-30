589dd7e9d3a3281a6a552180d830f36ee15702a8
wiz <wiz@pkgsrc.org>
2018-04-30 20:57:04 +0000

    mypaint-brushes: fix package name in bl3.mk
    While here, shorten names of variables.

--- graphics/mypaint-brushes/buildlink3.mk
+++ graphics/mypaint-brushes/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/04/30 04:56:17 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/04/30 20:57:04 wiz Exp $
 
-BUILDLINK_TREE+=	mypaint-brushes-mypaint-brushes
+BUILDLINK_TREE+=	mypaint-brushes
 
-.if !defined(MYPAINT_BRUSHES_MYPAINT_BRUSHES_BUILDLINK3_MK)
-MYPAINT_BRUSHES_MYPAINT_BRUSHES_BUILDLINK3_MK:=
+.if !defined(MYPAINT_BRUSHES_BUILDLINK3_MK)
+MYPAINT_BRUSHES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mypaint-brushes-mypaint-brushes+=	mypaint-brushes-mypaint-brushes>=1.3.0
-BUILDLINK_PKGSRCDIR.mypaint-brushes-mypaint-brushes?=	../../graphics/mypaint-brushes
-.endif	# MYPAINT_BRUSHES_MYPAINT_BRUSHES_BUILDLINK3_MK
+BUILDLINK_API_DEPENDS.mypaint-brushes+=	mypaint-brushes>=1.3.0
+BUILDLINK_PKGSRCDIR.mypaint-brushes?=	../../graphics/mypaint-brushes
+.endif	# MYPAINT_BRUSHES_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-mypaint-brushes-mypaint-brushes
+BUILDLINK_TREE+=	-mypaint-brushes
