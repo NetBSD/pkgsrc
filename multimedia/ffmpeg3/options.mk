de709a132e135fd66f22d5c40eb439d1fd47d595
adam <adam@pkgsrc.org>
2018-04-30 09:00:45 +0000

    ffmpeg[34]: added tesseract option

--- multimedia/ffmpeg3/options.mk
+++ multimedia/ffmpeg3/options.mk
@@ -1,11 +1,11 @@
-# $NetBSD: options.mk,v 1.11 2018/03/05 21:17:07 rjs Exp $
+# $NetBSD: options.mk,v 1.12 2018/04/30 09:00:45 adam Exp $
 
 # Global and legacy options
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ffmpeg3
 PKG_SUPPORTED_OPTIONS=	ass bluray doc fdk-aac fontconfig freetype gnutls \
-			lame libvpx opencore-amr openssl opus rpi \
-			rtmp theora vorbis x11 x264 x265 xcb xvid
+			lame libvpx opencore-amr openssl opus rpi rtmp \
+			tesseract theora vorbis x11 x264 x265 xcb xvid
 PKG_SUGGESTED_OPTIONS=	lame ass bluray freetype fontconfig libvpx openssl \
 			theora vorbis x11 x264 xvid
 
@@ -111,6 +111,14 @@ CONFIGURE_ARGS+=	--disable-openssl
 .if !empty(PKG_OPTIONS:Mrtmp)
 CONFIGURE_ARGS+=	--enable-librtmp
 .include "../../net/rtmpdump/buildlink3.mk"
+.endif
+
+# OCR filter using Tesseract
+.if !empty(PKG_OPTIONS:Mtesseract)
+CONFIGURE_ARGS+=	--enable-libtesseract
+.include "../../graphics/tesseract/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-libtesseract
 .endif
 
 # OGG Theora support
@@ -219,4 +227,3 @@ CONFIGURE_ARGS+=	--enable-libbluray
 .else
 CONFIGURE_ARGS+=	--disable-libbluray
 .endif
-
