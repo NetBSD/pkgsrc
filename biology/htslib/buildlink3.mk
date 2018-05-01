c428bbd88b583c71727fb97b9784b9f45cedf976
bacon <bacon@pkgsrc.org>
2018-05-01 13:20:44 +0000

    biology/htslib: Fix category in bl3

--- biology/htslib/buildlink3.mk
+++ biology/htslib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/04/30 16:51:54 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/05/01 13:20:44 bacon Exp $
 
 BUILDLINK_TREE+=	htslib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	htslib
 HTSLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.8
-BUILDLINK_PKGSRCDIR.htslib?=	../../wip/htslib
+BUILDLINK_PKGSRCDIR.htslib?=	../../biology/htslib
 
 .endif	# HTSLIB_BUILDLINK3_MK
 
