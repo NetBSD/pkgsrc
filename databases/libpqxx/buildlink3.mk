40d0c75b72a43afa57a7d6a176031f9aa86e8367
prlw1 <prlw1@pkgsrc.org>
2018-05-03 12:58:24 +0000

    Update libpqxx to 6.2.2
    patch-ab no longer needed just since 15 Jan 2018(!)
    
    5 years worth of changes including:
    - C++11 is now required. Your compiler must have shared_ptr, noexcept, etc.
    - Removed pqxx::items. Use the new C++11 initialiser syntax.
    - Removed maketemporary. We weren't using it.
    - Can now be built outside the source tree.
    - New, simpler, lambda-friendly transactor framework.
    - New, simpler, prepared statements and parameterised statements.
    - Result rows can be passed around independently.
    - New exec0(): perform query, expect zero rows of data.
    - New exec1(): perform query, expect (and return) a single row of data.
    - New exec_n(): perform query, expect exactly n rows of data.
    - No longer defines Visual Studio's NOMINMAX in headers.
    - Much faster configure script.
    - Most configuration items are gone.
    - Retired all existing capability flags.
    - Documentation on readthedocs.org, thanks Tim Sheerman-Chase.
    - Expose SQLSTATE error codes in sql_error exceptions.
    - Adds a first-generation parser for SQL arrays.

--- databases/libpqxx/buildlink3.mk
+++ databases/libpqxx/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.8 2012/05/07 01:53:18 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2018/05/03 12:58:24 prlw1 Exp $
 
 BUILDLINK_TREE+=	libpqxx
 
 .if !defined(LIBPQXX_BUILDLINK3_MK)
 LIBPQXX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libpqxx+=	libpqxx>=2.5.0
-BUILDLINK_ABI_DEPENDS.libpqxx+=	libpqxx>=2.5.4nb1
+BUILDLINK_API_DEPENDS.libpqxx+=	libpqxx>=6.2.2
 BUILDLINK_PKGSRCDIR.libpqxx?=	../../databases/libpqxx
 .endif # LIBPQXX_BUILDLINK3_MK
 
