11583caf6b0662cc4f993e6db0a6ae87020a3d3f
tsutsui <tsutsui@pkgsrc.org>
2018-05-03 12:04:02 +0000

    ruby-gnome2: update to 3.2.5.
    Upstream changes (from NEWS):
    
    == Ruby-GNOME2 3.2.5: 2018-05-02
    
    This is a bug fix release of 3.2.4.
    
    === Changes
    
    ==== Document
    
      * Fixes
    
        * Fixed typos.
          [GitHub#1158][Patch by kojix2]
          [GitHub#1160][Patch by kojix2]
    
    ==== Ruby/GLib2
    
      * Fixes
    
        * Fixed a GC related crash bug.
          [GitHub#1162][Reported by Izumi Tsutsui]
    
    ==== Ruby/GObjectIntrospection
    
      * Improvements
    
        * Disabled NULL check for GObject Introspection < 1.42. Because
          GObject Introspection < 1.42 doesn't support "(nullable)"
          annotation yet.
    
    ==== Ruby/GdkPixbuf2
    
      * Improvements
    
        * (({GdkPixbuf::Pixbuf#composite})): Suppressed wrong warning.
          [GitHub#1156][Reported by Chaistrin]
          [GitHub#1157][Patch by cedlemo]
    
      * Fixes
    
        * Added a missing white space into message.
          [GitHub#1155][Reported by Robert A. Heiler]
    
        * (({GdkPixbuf::Pixbuf#composite})): Fixed a bug that width and
          height are ignored.
          [Patch by cedlemo]
    
    ==== Ruby/GTK3
    
      * Improvements
    
        * (({Gtk::TextBuffer#initialize})): Accepted "property-name" form.
          [GitHub#1161][Reported by kojix2]
    
    ==== Ruby/Poppler
    
      * Improvements
    
        * Added a workaround for poppler-glib 0.63 bug.
          [GitHub#1159][Reported by HIGUCHI Daisuke]
    
    === Thanks
    
      * Robert A. Heiler
    
      * Chaistrin
    
      * cedlemo
    
      * kojix2
    
      * Izumi Tsutsui
    
      * HIGUCHI Daisuke

--- x11/ruby-gnome2-gtk3/buildlink3.mk
+++ x11/ruby-gnome2-gtk3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2018/04/16 14:34:02 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2018/05/03 12:04:04 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gnome2-gtk3
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ruby-gnome2-gtk3
 RUBY_GNOME2_GTK3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-gtk3+=	${RUBY_PKGPREFIX}-gnome2-gtk3>=1.2.0
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk3+=	${RUBY_PKGPREFIX}-gnome2-gtk3>=3.2.4nb1
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk3+=	${RUBY_PKGPREFIX}-gnome2-gtk3>=3.2.5
 BUILDLINK_PKGSRCDIR.ruby-gnome2-gtk3?=		../../x11/ruby-gnome2-gtk3
 
 .include "../../devel/ruby-gnome2-glib/buildlink3.mk"
