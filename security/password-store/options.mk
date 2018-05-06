bd6dba1a921ab2ac604cdd08c886a15db2290ef2
leot <leot@pkgsrc.org>
2018-05-06 11:56:58 +0000

    password-store: Update security/password-store to 1.7.1
    pkgsrc changes:
     - Rename `xclip' PKG_OPTION to `x11' (and add a PKG_OPTIONS_LEGACY_OPTS
       accordingly) and also depends on converters/base64 (needed by the `--clip'
       option, like xclip) and qrencode (needed by the new `--qrcode' option).
     - Add support for `test' target and adjust the part of the test suite for
       gnupg>=2.2.5 via patches/patch-tests_t0300-reencryption.sh.
     - sysutils/pwgen is no more needed, remove it from DEPENDS
       (now `tr -dc '<characters>' < /dev/urandom' is used instead)
     - Add patches/patch-contrib_dmenu_passmenu to fix `passmenu --type'
       (at least xdotool-2.20110530.1 does not support any `--file' option used by
       passmenu)
     - Adjust PAX invocations in `do-install' target to ignore possible `*.orig'
       and `.gitignore' files.
    
    Changes
    1.7.1
    -----
    == Bug Fixes ==
    * Fix test suite on OS X
    * Add compatibility with GnuPG 2.2.19
    * Uniformly use the $GPG variable
    * Do the correct thing with subkeys when reencrypting
    
    1.7
    ---
    == New Features ==
    * Extensions: pass can now load user-defined extensions from a system
      directory or a user directory. There's already a nice ecosystem of
      extensions being built, even at this early stage. See the pass man page for
      more information.
    * Signatures: there is now an option to enforce signatures of the .gpg-id file
      and extensions using an environment variable.
    * QRCodes: generate and show have now learned the --qrcode/-q switch. Note to
      package maintainers: this adds a dependency on the popular qrencode package.
    * Password generation: rather than use pwgen, we now use /dev/urandom more
      directly, which results in more assured password security, as well as
      customizable character sets, via an environment variable. See the pass man
      page for more information on this customization. Package maintainers: you
      may now drop the dependency on pwgen.
    * Importers: there now are several more importers. More and more folks are
      moving to pass!
    * Selectable clipping: you can now specify which line you wish to copy to the
      clipboard or display with a qrcode when using -c or -q.
    * Git discovery: The PASSWORD_STORE_GIT environment variable has been removed,
      and instead pass will automatically choose the git repository closest to the
      file being modified (but not out of the actual password store itself). This
      should help people who like to nest git repos for different organizations.
    * Bug fixes: too many to count.
    
    == Note To Distros ==
    * Drop the dependency of pwgen.
    * Add the dependency of qrencode.
    * The Makefile now does the right thing with DESTDIR, so you might want to
      double check that your package recipe does the right thing.
    * The semantics for auto-detection of bash completion has changed, with new
      environment variables for such things. See INSTALL for details.

--- security/password-store/options.mk
+++ security/password-store/options.mk
@@ -1,13 +1,16 @@
-# $NetBSD: options.mk,v 1.1 2015/01/05 11:32:49 imil Exp $
+# $NetBSD: options.mk,v 1.2 2018/05/06 11:56:58 leot Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pass
 
-PKG_SUPPORTED_OPTIONS=	git xclip
-PKG_SUGGESTED_OPTIONS=	git
+PKG_SUPPORTED_OPTIONS=		git x11
+PKG_SUGGESTED_OPTIONS=		git
+PKG_OPTIONS_LEGACY_OPTS+=	xclip:x11
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mxclip)
+.if !empty(PKG_OPTIONS:Mx11)
+DEPENDS+=	base64-[0-9]*:../../converters/base64
+DEPENDS+=	qrencode-[0-9]*:../../converters/qrencode
 DEPENDS+=	xclip>=0.12:../../x11/xclip
 .endif
 
