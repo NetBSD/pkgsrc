30924fe9475b4adb44df73966c3df829dc845844
jaapb <jaapb@pkgsrc.org>
2018-05-09 14:44:12 +0000

    Updated devel/ocaml-stdio to version 0.11.0.
    Changes are minor.

--- devel/ocaml-stdio/buildlink3.mk
+++ devel/ocaml-stdio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/04/04 19:21:21 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/05/09 14:44:12 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-stdio
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-stdio
 OCAML_STDIO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-stdio+=	ocaml-stdio>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-stdio+=	ocaml-stdio>=0.10.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-stdio+=	ocaml-stdio>=0.11.0
 BUILDLINK_PKGSRCDIR.ocaml-stdio?=	../../devel/ocaml-stdio
 .endif	# OCAML_STDIO_BUILDLINK3_MK
 
