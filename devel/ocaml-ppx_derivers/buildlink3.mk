e932cb19c6ce3becb773bb32f4283ad5cd62a3ad
jaapb <jaapb@pkgsrc.org>
2018-05-09 14:37:14 +0000

    Updated devel/ocaml-ppx_derivers to version 1.2.
    No changes are recorded from 1.1 to 1.2; the only change from 1.0 to 1.1
    seems to be the addition of a LICENSE.md file.

--- devel/ocaml-ppx_derivers/buildlink3.mk
+++ devel/ocaml-ppx_derivers/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/04/04 19:34:44 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/05/09 14:37:14 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_derivers
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_derivers
 OCAML_PPX_DERIVERS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_derivers+=	ocaml-ppx_derivers>=1.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_derivers+=	ocaml-ppx_derivers>=1.0nb2
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_derivers+=	ocaml-ppx_derivers>=1.2
 BUILDLINK_PKGSRCDIR.ocaml-ppx_derivers?=	../../devel/ocaml-ppx_derivers
 .endif	# OCAML_PPX_DERIVERS_BUILDLINK3_MK
 
