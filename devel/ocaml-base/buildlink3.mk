d0101d58f447fa68a3d2962d817966752a900ce0
jaapb <jaapb@pkgsrc.org>
2018-05-09 14:34:07 +0000

    Updated devel/ocaml-base to version 0.11.0.
    This introduces quite a few (minor) improvements and bugfixes, all of
    which can be found in more detail in the CHANGES.md file.

--- devel/ocaml-base/buildlink3.mk
+++ devel/ocaml-base/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/04/04 16:21:29 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/05/09 14:34:07 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-base
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-base
 OCAML_BASE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-base+=	ocaml-base>=0.9.3
-BUILDLINK_ABI_DEPENDS.ocaml-base+=	ocaml-base>=0.10.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-base+=	ocaml-base>=0.11.0
 BUILDLINK_PKGSRCDIR.ocaml-base?=	../../devel/ocaml-base
 .endif	# OCAML_BASE_BUILDLINK3_MK
 
