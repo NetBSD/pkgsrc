d8e765aae4ae73404c089f293e8b19d5ea9b3063
jaapb <jaapb@pkgsrc.org>
2018-05-09 15:26:02 +0000

    Updated buildlink3.mk for net/ocamlnet.

--- net/ocamlnet/buildlink3.mk
+++ net/ocamlnet/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2018/01/10 16:37:41 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2018/05/09 15:26:02 jaapb Exp $
 
 BUILDLINK_TREE+=	ocamlnet
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocamlnet
 OCAMLNET_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocamlnet+=	ocamlnet>=4.1.2
-BUILDLINK_ABI_DEPENDS.ocamlnet+=	ocamlnet>=4.1.5
+BUILDLINK_ABI_DEPENDS.ocamlnet+=	ocamlnet>=4.1.6
 BUILDLINK_PKGSRCDIR.ocamlnet?=	../../net/ocamlnet
 
 .endif	# OCAMLNET_BUILDLINK3_MK
