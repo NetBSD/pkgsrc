074079f8d168e64cfae80a321ccf06a90c106495
jaapb <jaapb@pkgsrc.org>
2018-05-09 14:54:54 +0000

    Updated devel/ocaml-ppx_compare to version 0.11.1.
    The package now depends on devel/ocaml-ppxlib instead of on its
    constituent parts. There have also been some minor improvements from
    version 0.9 to 0.10.

--- devel/ocaml-ppx_compare/buildlink3.mk
+++ devel/ocaml-ppx_compare/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/04/04 19:38:22 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/05/09 14:54:54 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_compare
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_compare
 OCAML_PPX_COMPARE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_compare+=	ocaml-ppx_compare>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_compare+=	ocaml-ppx_compare>=0.9.0nb2
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_compare+=	ocaml-ppx_compare>=0.11.1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_compare?=		../../devel/ocaml-ppx_compare
 .endif	# OCAML_PPX_COMPARE_BUILDLINK3_MK
 
