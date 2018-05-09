f54734bfdeb960a4e9863155a1bdb090de3b36e1
jaapb <jaapb@pkgsrc.org>
2018-05-09 14:30:46 +0000

    Updated package devel/ocaml-sexplib to version 0.11.0.
    There are dependency changes (use ocaml-parsexp, drop ocaml-base) and
    a minor compatibility bugfix.

--- devel/ocaml-sexplib/buildlink3.mk
+++ devel/ocaml-sexplib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2018/04/04 16:20:15 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2018/05/09 14:30:46 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-sexplib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-sexplib
 OCAML_SEXPLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=0.10.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=0.11.0
 BUILDLINK_PKGSRCDIR.ocaml-sexplib?=	../../devel/ocaml-sexplib
 
 .endif	# OCAML_SEXPLIB_BUILDLINK3_MK
