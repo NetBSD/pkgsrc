b0566a81ee054b9021085ccc62f9dea9e1fa4043
jaapb <jaapb@pkgsrc.org>
2018-05-09 14:46:59 +0000

    Added new package devel/ocaml-ppxlib.
    This is a package that subsumes several other packages (see DESCR for
    details). All dependencies on these packages have already been removed
    (or will be soon, as soon as I finish committing this batch of updates)
    and the packages themselves will be removed from pkgsrc.

---
+++ devel/ocaml-ppxlib/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2018/05/09 14:46:59 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-ppxlib
+
+.if !defined(OCAML_PPXLIB_BUILDLINK3_MK)
+OCAML_PPXLIB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.3.0
+BUILDLINK_PKGSRCDIR.ocaml-ppxlib?=	../../devel/ocaml-ppxlib
+
+.endif	# OCAML_PPXLIB_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-ppxlib
