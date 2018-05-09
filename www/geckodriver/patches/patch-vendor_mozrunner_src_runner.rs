ca7bb5eecfb390350e270184ae2169e648c6c364
maya <maya@pkgsrc.org>
2018-05-09 23:41:39 +0000

    geckodriver: update to 0.20.1. Fixes build with rust 1.24.1.
    Fixes PR pkg/53263, tested by Mayuresh.
    
    0.20.1:
    
    Fixed
    
        Avoid attempting to kill Firefox process that has stopped.
    
        With the change to allow Firefox enough time to shut down in
        0.20.0, geckodriver started unconditionally killing the process
        to reap its exit status. This caused geckodriver to inaccurately
        report a successful Firefox shutdown as a failure.
    
        The regression should not have caused any functional problems, but
        the termination cause and the exit status are now reported correctly.
    
    0.20.0:
    
    Added
    
        New --jsdebugger flag to open the Browser Toolbox when Firefox
        launches. This is useful for debugging Marionette internals.
    
        Introduced the temporary, boolean capability
        moz:useNonSpecCompliantPointerOrigin to disable the WebDriver
        conforming behavior of calculating the Pointer Origin.
    
    Changed
    
        HTTP status code for the StaleElementReference error changed
        from 400 (Bad Request) to 404 (Not Found).
    
        Backtraces from geckodriver no longer substitute for missing
        Marionette stacktraces.
    
        webdriver crate upgraded to 0.35.0.
    
    Fixed
    
        The Firefox process is now given ample time to shut down, allowing
        enough time for the Firefox shutdown hang monitor to kick in.
    
        Firefox has an integrated background monitor that observes
        long-running threads during shutdown. These threads will be
        killed after 63 seconds in the event of a hang. To allow Firefox
        to shut down these threads on its own, geckodriver has to wait
        that time and some additional seconds.
    
        Grapheme clusters are now accepted as input for keyboard input
        to actions.
    
        Input to the value field of the keyDown and keyUp action
        primitives used to only accept single characters, which means
        geckodriver would error when a valid grapheme cluster was sent in,
        for example with the tamil nadu character U+0BA8 U+0BBF.
    
        Thanks to Greg Fraley for fixing this bug.
    
        Improved error messages for malformed capability values.

--- www/geckodriver/patches/patch-vendor_mozrunner_src_runner.rs
+++ www/geckodriver/patches/patch-vendor_mozrunner_src_runner.rs
@@ -1,12 +1,36 @@
-$NetBSD: patch-vendor_mozrunner_src_runner.rs,v 1.1 2017/11/11 20:25:56 tnn Exp $
+$NetBSD: patch-vendor_mozrunner_src_runner.rs,v 1.2 2018/05/09 23:41:39 maya Exp $
 
-Make the fallback method for finding the default browser behave like Linux.
+Make the linux method the fallback method.
 
---- ../vendor/mozrunner-0.5.0/src/runner.rs.orig	2017-09-11 14:07:59.000000000 +0000
-+++ ../vendor/mozrunner-0.5.0/src/runner.rs
-@@ -318,10 +318,11 @@ pub mod platform {
+--- ../vendor/mozrunner-0.6.1/src/runner.rs.orig	2017-09-11 14:07:59.000000000 +0000
++++ ../vendor/mozrunner-0.6.1/src/runner.rs
+@@ -351,20 +351,6 @@ fn find_binary(name: &str) -> Option<Pat
+     })
+ }
  
- #[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
+-#[cfg(target_os = "linux")]
+-pub mod platform {
+-    use super::find_binary;
+-    use std::path::PathBuf;
+-
+-    pub fn firefox_default_path() -> Option<PathBuf> {
+-        find_binary("firefox")
+-    }
+-
+-    pub fn arg_prefix_char(c: char) -> bool {
+-        c == '-'
+-    }
+-}
+-
+ #[cfg(target_os = "macos")]
+ pub mod platform {
+     use super::find_binary;
+@@ -455,12 +441,13 @@ pub mod platform {
+     }
+ }
+ 
+-#[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
++#[cfg(not(any(target_os = "macos", target_os = "windows")))]
  pub mod platform {
 +    use super::find_binary;
      use std::path::PathBuf;
