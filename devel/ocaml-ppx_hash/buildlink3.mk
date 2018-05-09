a08cc5ed3e75e989975bd90800c0d899a7194c10
jaapb <jaapb@pkgsrc.org>
2018-05-09 14:56:43 +0000

    Updated devel/ocaml-ppx_hash to version 0.11.1.
    This version depends on ppxlib rather than its constituent parts.
    There have also been minor improvements from version 0.9 to version 0.10.

--- devel/ocaml-ppx_hash/buildlink3.mk
+++ devel/ocaml-ppx_hash/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/04/04 19:42:52 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/05/09 14:56:43 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_hash
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_hash
 OCAML_PPX_HASH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_hash+=	ocaml-ppx_hash>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_hash+=	ocaml-ppx_hash>=0.9.0nb2
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_hash+=	ocaml-ppx_hash>=0.11.1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_hash?=	../../devel/ocaml-ppx_hash
 
 .endif	# OCAML_PPX_HASH_BUILDLINK3_MK
