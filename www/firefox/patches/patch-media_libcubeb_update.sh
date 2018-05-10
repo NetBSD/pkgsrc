8b90e55959ff79f24f42bffa7955acba760ffc39
ryoon <ryoon@pkgsrc.org>
2018-05-10 20:01:53 +0000

    Update to 60.0
    * Remove untested patches including NetBSD/earm support
    
    Changelog:
    New
        Added a policy engine that allows customized Firefox deployments in
          enterprise environments, using Windows Group Policy or a cross-platform
          JSON file
    
        Enhancements to New Tab / Firefox Home
            Responsive layout that shows more content for users with wide-screen
              displays
            Highlights section includes web sites saved to Pocket
            More options to reorder sections and content on the page
            Pocket Sponsored Stories will appear for a percentage of users in
              the US. Read about our privacy-conscious approach to sponsored content
    
        Redesigned Cookies and Site Storage section in Preferences for greater
          clarity and control of first- and third-party cookies
    
        Applied Quantum CSS to render browser UI
    
        Added support for Web Authentication API, which allows USB tokens for
          website authentication
    
        Enhanced camera privacy indicators: Firefox now turns off your camera
          and the camera's light when you disable video recording, and turns
          the camera and light on when you resume recording
    
        Added an option for Linux users to show or hide page titles in a bar
          at the top of the browser. You'll find the Title Bar option in the
          Customize panel available from the main browser menu.
    
        Improved WebRTC audio performance and playback for Linux users
    
        Locale added: Occitan (oc)
    
    Fixed
        Various security fixes
    
    Changed
    #CVE-2018-5154: Use-after-free with SVG animations and clip paths
    #CVE-2018-5155: Use-after-free with SVG animations and text paths
    #CVE-2018-5157: Same-origin bypass of PDF Viewer to view protected PDF files
    #CVE-2018-5158: Malicious PDF can inject JavaScript into PDF Viewer
    #CVE-2018-5159: Integer overflow and out-of-bounds write in Skia
    #CVE-2018-5160: Uninitialized memory use by WebRTC encoder
    #CVE-2018-5152: WebExtensions information leak through webRequest API
    #CVE-2018-5153: Out-of-bounds read in mixed content websocket messages
    #CVE-2018-5163: Replacing cached data in JavaScript Start-up Bytecode Cache
    #CVE-2018-5164: CSP not applied to all multipart content sent with
                    multipart/x-mixed-replace
    #CVE-2018-5166: WebExtension host permission bypass through filterReponseData
    #CVE-2018-5167: Improper linkification of chrome: and javascript: content
                    in web console and JavaScript debugger
    #CVE-2018-5168: Lightweight themes can be installed without user interaction
    #CVE-2018-5169: Dragging and dropping link text onto home button can set home
                    page to include chrome pages
    #CVE-2018-5172: Pasted script from clipboard can run in the Live Bookmarks
                    page or PDF viewer
    #CVE-2018-5173: File name spoofing of Downloads panel with Unicode characters
    #CVE-2018-5174: Windows Defender SmartScreen UI runs with less secure behavior
                    for downloaded files in Windows 10 April 2018 Update
    #CVE-2018-5175: Universal CSP bypass on sites using strict-dynamic in
                    their policies
    #CVE-2018-5176: JSON Viewer script injection
    #CVE-2018-5177: Buffer overflow in XSLT during number formatting
    #CVE-2018-5165: Checkbox for enabling Flash protected mode is inverted in
                    32-bit Firefox
    #CVE-2018-5180: heap-use-after-free in mozilla::WebGLContext::DrawElementsInstanced
    #CVE-2018-5181: Local file can be displayed in noopener tab through drag and
                    drop of hyperlink
    #CVE-2018-5182: Local file can be displayed from hyperlink dragged and dropped
                    on addressbar
    #CVE-2018-5151: Memory safety bugs fixed in Firefox 60
    #CVE-2018-5150: Memory safety bugs fixed in Firefox 60 and Firefox ESR 52.8

--- www/firefox/patches/patch-media_libcubeb_update.sh
+++ www/firefox/patches/patch-media_libcubeb_update.sh
@@ -1,12 +1,12 @@
-$NetBSD: patch-media_libcubeb_update.sh,v 1.3 2017/08/10 14:46:15 ryoon Exp $
+$NetBSD: patch-media_libcubeb_update.sh,v 1.4 2018/05/10 20:01:53 ryoon Exp $
 
---- media/libcubeb/update.sh.orig	2017-07-31 16:20:49.000000000 +0000
+--- media/libcubeb/update.sh.orig	2018-05-03 16:58:30.000000000 +0000
 +++ media/libcubeb/update.sh
 @@ -20,6 +20,7 @@ cp $1/src/cubeb_log.h src
  cp $1/src/cubeb_mixer.cpp src
  cp $1/src/cubeb_mixer.h src
  cp $1/src/cubeb_opensl.c src
 +cp $1/src/cubeb_oss.c src
- cp $1/src/cubeb_osx_run_loop.h src
- cp $1/src/cubeb_panner.cpp src
- cp $1/src/cubeb_panner.h src
+ cp $1/src/cubeb-jni.cpp src
+ cp $1/src/cubeb-jni.h src
+ cp $1/src/android/cubeb-output-latency.h src/android
