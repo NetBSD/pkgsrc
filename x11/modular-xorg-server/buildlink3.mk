042a9a6261cea41ae39cc411df32f332a086fd96
wiz <wiz@pkgsrc.org>
2018-05-11 13:47:35 +0000

    modular-xorg-server: update to 1.20.0.
    1.20.0
    
    Lots of Present, DRI3 modifier, and buildsystem fixes. Thanks to
    everyone who contributed to this release!
    
    
    1.19.99.905
    
    More bugfixes, and streams support for Xwayland. This will almost
    certainly be the last RC.
    
    1.19.99.904
    
    Another batch of cleanups and fixes, mostly in glamor and DRI3.
    
    At this point the only outstanding major feature for 1.20 that hasn't
    landed yet is EGLStreams support for Xwayland. The patches require some
    minor rebasing to account for per-window flips in Present, I'll post
    that momentarily I suppose. I'm honestly of two minds about merging it,
    the deafening silence of review feedback has not been encouraging.
    
    1.19.99.903
    
    Fixes for meson, modesetting, and a crash I introduced right before RC2.
    
    1.19.99.902
    
    This merges the DRI3 1.2 support (with an asterisk) and per-window
    flips for Xwayland, along with the usual collection of miscellaneous
    fixes. In particular the meson build is much closer to production-
    ready. The asterisk for DRI3 is that the modesetting driver has some
    known issues still, so the code to enable planes and modifiers is
    hidden behind an xorg.conf option:
    
    Section "ServerFlags"
        Option "Debug" "dmabuf_capable"
    EndSection
    
    Thanks to all for testing and debugging.

--- x11/modular-xorg-server/buildlink3.mk
+++ x11/modular-xorg-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2018/03/07 11:57:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2018/05/11 13:47:35 wiz Exp $
 
 BUILDLINK_TREE+=	modular-xorg-server
 
@@ -21,6 +21,7 @@ BUILDLINK_DEPMETHOD.libpciaccess=	full
 .include "../../sysutils/libpciaccess/buildlink3.mk"
 .include "../../x11/pixman/buildlink3.mk"
 
+.include "../../x11/xorgproto/buildlink3.mk"
 .if !empty(USE_OLD_MODULAR_XORG_SERVER:M[yY][eE][sS])
 .include "../../graphics/MesaLib7/buildlink3.mk" # XXX should not be needed for non-dri build
 .include "../../x11/libXfont/buildlink3.mk"
@@ -28,7 +29,6 @@ BUILDLINK_DEPMETHOD.libpciaccess=	full
 .include "../../x11/libXfont2/buildlink3.mk"
 .include "../../graphics/MesaLib/buildlink3.mk" # XXX should not be needed for non-dri build
 .endif
-.include "../../x11/xorgproto/buildlink3.mk"
 .endif # MODULAR_XORG_SERVER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-modular-xorg-server
