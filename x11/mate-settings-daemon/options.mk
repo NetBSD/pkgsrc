ee6822838539964443eb083264ff474d2db719ff
youri <youri@pkgsrc.org>
2018-05-14 13:17:15 +0000

    Update to 1.20.1:
    HiDPI: Load xsettings during initialization phase
    Add HiDPI configuration in XSettings
    Add support for HiDPI displays
    plugins: G_GNUC_UNUSED in unused but needed variables
    xsettings: drop CanChangeAccels which won’t work with GTK+3
    msd-datetime-mechanism.c: Fix build warnings:
    msd-mpris-manager.c: Fix build warning:
    configure.ac: drop GLIB_VERSION_MAX_ALLOWED
    Translations update

--- x11/mate-settings-daemon/options.mk
+++ x11/mate-settings-daemon/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2016/05/21 19:15:32 youri Exp $
+# $NetBSD: options.mk,v 1.2 2018/05/14 13:17:15 youri Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.mate-settings-daemon
 PKG_SUPPORTED_OPTIONS=		nss polkit
@@ -23,14 +23,6 @@ CONFIGURE_ARGS+=	--disable-smartcard-support
 CONFIGURE_ARGS+=	--enable-pulse
 .else
 CONFIGURE_ARGS+=	--disable-pulse
-.endif
-
-.if !empty(PKG_OPTIONS:Mgstreamer)
-.include "../../multimedia/gstreamer0.10/buildlink3.mk"
-.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-gstreamer
-.else
-CONFIGURE_ARGS+=	--disable-gstreamer
 .endif
 
 .if !empty(PKG_OPTIONS:Mpolkit)
