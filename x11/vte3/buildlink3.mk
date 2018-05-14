cc997a2c654275123d8c8b4bbc2835c89136bee9
youri <youri@pkgsrc.org>
2018-05-14 12:09:22 +0000

    Update vte3 to 0.50.3: Full changelog unavailable.
    vte3 now depends on pcre2 and gnutls.

--- x11/vte3/buildlink3.mk
+++ x11/vte3/buildlink3.mk
@@ -1,14 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/04/16 14:34:02 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/05/14 12:09:22 youri Exp $
 
 BUILDLINK_TREE+=	vte
 
 .if !defined(VTE_BUILDLINK3_MK)
 VTE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vte+=	vte>=0.38.0
-BUILDLINK_ABI_DEPENDS.vte+=	vte>=0.38.4nb3
+BUILDLINK_API_DEPENDS.vte+=	vte>=0.48.3
+BUILDLINK_ABI_DEPENDS.vte+=	vte>=0.48.3
 BUILDLINK_PKGSRCDIR.vte?=	../../x11/vte3
 
+.include "../../devel/pcre2/buildlink3.mk"
+.include "../../security/gnutls/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
 .include "../../mk/termcap.buildlink3.mk"
 .endif # VTE_BUILDLINK3_MK
