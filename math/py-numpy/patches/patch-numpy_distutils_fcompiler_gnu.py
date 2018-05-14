23d11e04828d62a762fb6d2804fd5c9b01dd0f86
adam <adam@pkgsrc.org>
2018-05-14 06:36:17 +0000

    py-numpy: Do not generate debug symbols

--- math/py-numpy/patches/patch-numpy_distutils_fcompiler_gnu.py
+++ math/py-numpy/patches/patch-numpy_distutils_fcompiler_gnu.py
@@ -1,14 +1,15 @@
-$NetBSD: patch-numpy_distutils_fcompiler_gnu.py,v 1.13 2017/10/05 08:21:27 adam Exp $
+$NetBSD: patch-numpy_distutils_fcompiler_gnu.py,v 1.14 2018/05/14 06:36:17 adam Exp $
 
 Linker needs -shared explictly (at least with GCC 4.7 on SunOS), plus
 any ABI flags as appropriate.
+Do not generate debug symbols (remove '-g').
 On OS X, do not use '-bundle' and 'dynamic_lookup' (to avoid Python.framework).
 Do not use -funroll-loops compiler flag.
 Do not run a shell command when it is "None".
 
---- numpy/distutils/fcompiler/gnu.py.orig	2017-09-29 20:10:10.000000000 +0000
+--- numpy/distutils/fcompiler/gnu.py.orig	2018-04-23 16:28:56.000000000 +0000
 +++ numpy/distutils/fcompiler/gnu.py
-@@ -57,8 +57,10 @@ class GnuFCompiler(FCompiler):
+@@ -63,8 +63,10 @@ class GnuFCompiler(FCompiler):
                      return ('gfortran', m.group(1))
          else:
              # Output probably from --version, try harder:
@@ -20,16 +21,24 @@ Do not run a shell command when it is "None".
                  return ('gfortran', m.group(1))
              m = re.search(
                  r'GNU Fortran.*?\-?([0-9-.]+\.[0-9-.]+)', version_string)
-@@ -88,7 +90,7 @@ class GnuFCompiler(FCompiler):
-         'compiler_f77' : [None, "-g", "-Wall", "-fno-second-underscore"],
+@@ -91,13 +93,13 @@ class GnuFCompiler(FCompiler):
+     possible_executables = ['g77', 'f77']
+     executables = {
+         'version_cmd'  : [None, "-dumpversion"],
+-        'compiler_f77' : [None, "-g", "-Wall", "-fno-second-underscore"],
++        'compiler_f77' : [None, "-Wall", "-fno-second-underscore"],
          'compiler_f90' : None,  # Use --fcompiler=gnu95 for f90 codes
          'compiler_fix' : None,
 -        'linker_so'    : [None, "-g", "-Wall"],
-+        'linker_so'    : [None, "-g", "-Wall", "-shared", ""],
++        'linker_so'    : [None, "-Wall", "-shared", ""],
          'archiver'     : ["ar", "-cr"],
          'ranlib'       : ["ranlib"],
-         'linker_exe'   : [None, "-g", "-Wall"]
-@@ -140,7 +142,7 @@ class GnuFCompiler(FCompiler):
+-        'linker_exe'   : [None, "-g", "-Wall"]
++        'linker_exe'   : [None, "-Wall"]
+     }
+     module_dir_switch = None
+     module_include_switch = None
+@@ -146,7 +148,7 @@ class GnuFCompiler(FCompiler):
                      s = 'Env. variable MACOSX_DEPLOYMENT_TARGET set to 10.3'
                      warnings.warn(s, stacklevel=2)
  
@@ -38,7 +47,7 @@ Do not run a shell command when it is "None".
          else:
              opt.append("-shared")
          if sys.platform.startswith('sunos'):
-@@ -216,7 +218,6 @@ class GnuFCompiler(FCompiler):
+@@ -237,7 +239,6 @@ class GnuFCompiler(FCompiler):
              opt = ['-O2']
          else:
              opt = ['-O3']
@@ -46,16 +55,25 @@ Do not run a shell command when it is "None".
          return opt
  
      def _c_arch_flags(self):
-@@ -271,7 +272,7 @@ class Gnu95FCompiler(GnuFCompiler):
+@@ -288,13 +289,13 @@ class Gnu95FCompiler(GnuFCompiler):
+     possible_executables = ['gfortran', 'f95']
+     executables = {
+         'version_cmd'  : ["<F90>", "-dumpversion"],
+-        'compiler_f77' : [None, "-Wall", "-g", "-ffixed-form",
++        'compiler_f77' : [None, "-Wall", "-ffixed-form",
                            "-fno-second-underscore"] + _EXTRAFLAGS,
-         'compiler_fix' : [None, "-Wall",  "-g","-ffixed-form",
+-        'compiler_f90' : [None, "-Wall", "-g",
++        'compiler_f90' : [None, "-Wall",
+                           "-fno-second-underscore"] + _EXTRAFLAGS,
+-        'compiler_fix' : [None, "-Wall",  "-g","-ffixed-form",
++        'compiler_fix' : [None, "-Wall",  "-ffixed-form",
                            "-fno-second-underscore"] + _EXTRAFLAGS,
 -        'linker_so'    : ["<F90>", "-Wall", "-g"],
-+        'linker_so'    : ["<F90>", "-Wall", "-g", "-shared", ""],
++        'linker_so'    : ["<F90>", "-Wall", "-shared", ""],
          'archiver'     : ["ar", "-cr"],
          'ranlib'       : ["ranlib"],
          'linker_exe'   : [None, "-Wall"]
-@@ -284,7 +285,7 @@ class Gnu95FCompiler(GnuFCompiler):
+@@ -307,7 +308,7 @@ class Gnu95FCompiler(GnuFCompiler):
  
      def _universal_flags(self, cmd):
          """Return a list of -arch flags for every supported architecture."""
