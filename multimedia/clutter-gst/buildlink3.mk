533a7cd3d233bba7223b57f5784f0461ba90d45e
jperkin <jperkin@pkgsrc.org>
2018-05-15 15:49:41 +0000

    clutter-gst: Update to 3.0.26.
    This fixes compilation problems with preprocessor pasting, but also
    renames the library, thus requiring an ABI bump.
    
    List of changes:
    
     o Fix compilation error with -Werror=format-nonliteral
    
     o Rename plugin to avoid dash in the name

--- multimedia/clutter-gst/buildlink3.mk
+++ multimedia/clutter-gst/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.35 2018/04/16 14:33:53 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2018/05/15 15:49:41 jperkin Exp $
 #
 
 BUILDLINK_TREE+=	clutter-gst
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	clutter-gst
 CLUTTER_GST_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.clutter-gst+=	clutter-gst>=2.0.0
-BUILDLINK_ABI_DEPENDS.clutter-gst?=	clutter-gst>=3.0.24nb2
+BUILDLINK_ABI_DEPENDS.clutter-gst?=	clutter-gst>=3.0.26
 BUILDLINK_PKGSRCDIR.clutter-gst?=	../../multimedia/clutter-gst
 
 .include "../../graphics/clutter/buildlink3.mk"
