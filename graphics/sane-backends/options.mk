630790cc10eb505bebe672baf2267806e45d90b7
jperkin <jperkin@pkgsrc.org>
2018-05-15 14:58:08 +0000

    sane-backends: Handle nls correctly.

--- graphics/sane-backends/options.mk
+++ graphics/sane-backends/options.mk
@@ -1,17 +1,24 @@
-# $NetBSD: options.mk,v 1.9 2012/06/12 15:45:56 wiz Exp $
+# $NetBSD: options.mk,v 1.10 2018/05/15 14:58:08 jperkin Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.sane-backends
-PKG_SUPPORTED_OPTIONS=		inet6 snmp
+PKG_SUPPORTED_OPTIONS=		inet6 nls snmp
 PKG_SUGGESTED_OPTIONS=		inet6
 
 .include "../../mk/bsd.options.mk"
 
-# Package-specific option-handling
+PLIST_VARS+=	nls
 
 .if !empty(PKG_OPTIONS:Minet6)
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
+.endif
+
+.if !empty(PKG_OPTIONS:Mnls)
+CONFIGURE_ARGS+=	--enable-nls
+PLIST.nls=		yes
+.else
+CONFIGURE_ARGS+=	--disable-nls
 .endif
 
 .if !empty(PKG_OPTIONS:Msnmp)
