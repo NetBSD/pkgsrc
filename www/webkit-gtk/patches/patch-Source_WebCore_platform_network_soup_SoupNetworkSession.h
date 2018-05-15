e36eb37f8b476556d4d4a8013ce2af119d107fed
wiz <wiz@pkgsrc.org>
2018-05-15 08:32:37 +0000

    webkit-gtk: update patch after upstream review.

--- www/webkit-gtk/patches/patch-Source_WebCore_platform_network_soup_SoupNetworkSession.h
+++ www/webkit-gtk/patches/patch-Source_WebCore_platform_network_soup_SoupNetworkSession.h
@@ -1,14 +1,14 @@
-$NetBSD: patch-Source_WebCore_platform_network_soup_SoupNetworkSession.h,v 1.1 2018/05/14 21:22:34 wiz Exp $
+$NetBSD: patch-Source_WebCore_platform_network_soup_SoupNetworkSession.h,v 1.2 2018/05/15 08:32:37 wiz Exp $
 
 Add missing header for URL type.
 
 --- Source/WebCore/platform/network/soup/SoupNetworkSession.h.orig	2018-05-07 08:56:05.000000000 +0000
 +++ Source/WebCore/platform/network/soup/SoupNetworkSession.h
-@@ -32,6 +32,7 @@
+@@ -28,6 +28,7 @@
+ 
+ #include <gio/gio.h>
+ #include <pal/SessionID.h>
++#include <URL.h>
+ #include <wtf/Function.h>
  #include <wtf/Noncopyable.h>
  #include <wtf/glib/GRefPtr.h>
- #include <wtf/text/WTFString.h>
-+#include <URL.h>
- 
- typedef struct _SoupCache SoupCache;
- typedef struct _SoupCookieJar SoupCookieJar;
