54f00124a9cad8e752484c9a35fae3a71f8822ad
wiz <wiz@pkgsrc.org>
2018-05-15 15:21:04 +0000

    webkit-gtk: reorder per upstream style checker.

--- www/webkit-gtk/patches/patch-Source_WebCore_platform_network_soup_SoupNetworkSession.h
+++ www/webkit-gtk/patches/patch-Source_WebCore_platform_network_soup_SoupNetworkSession.h
@@ -1,14 +1,14 @@
-$NetBSD: patch-Source_WebCore_platform_network_soup_SoupNetworkSession.h,v 1.2 2018/05/15 08:32:37 wiz Exp $
+$NetBSD: patch-Source_WebCore_platform_network_soup_SoupNetworkSession.h,v 1.3 2018/05/15 15:21:04 wiz Exp $
 
 Add missing header for URL type.
 
 --- Source/WebCore/platform/network/soup/SoupNetworkSession.h.orig	2018-05-07 08:56:05.000000000 +0000
 +++ Source/WebCore/platform/network/soup/SoupNetworkSession.h
-@@ -28,6 +28,7 @@
+@@ -26,6 +26,7 @@
+ #ifndef SoupNetworkSession_h
+ #define SoupNetworkSession_h
  
++#include <URL.h>
  #include <gio/gio.h>
  #include <pal/SessionID.h>
-+#include <URL.h>
  #include <wtf/Function.h>
- #include <wtf/Noncopyable.h>
- #include <wtf/glib/GRefPtr.h>
