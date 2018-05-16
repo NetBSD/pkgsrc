1f70f17ddb2cb2f04fdf832ea4a0544fa95a2aa4
youri <youri@pkgsrc.org>
2018-05-16 11:30:36 +0000

    Update to 1.2.1:
    Brings fixes for many platforms.
    
    Full changelog:
    
    1.2.1:
    
    Stable release of ConsoleKit2
    
    Changes since 1.1.2
    New Features:
    
    Add wscons headers for OpenBSD
    Bug Fixes:
    
    Allow changing to VTs we don't have a session for
    Correct introspection prefixes
    Change configure.ac to enable the udev backend with devattr
    Use KDSKBMODE if defined
    Use #ifdef HEADER_H instead of #ifdef OS
    FreeBSD session activation
    Place sessions with a VT on the static Seat1
    clean up clang warnings
    correct uninitialized variable warning
    Explicitly check for needed symbols
    Correct build errors in OpenBSD
    fix compiling on FreeBSD
    Misc Changes:
    
    update gitignore
    more autotools cleanup work
    update readme with new deps
    
    1.2.0:
    Stable release of ConsoleKit2
    
    Changes since 1.1.2
    New Features:
    
    Add wscons headers for OpenBSD
    Bug Fixes:
    
    Allow changing to VTs we don't have a session for
    Correct introspection prefixes
    Change configure.ac to enable the udev backend with devattr
    Use KDSKBMODE if defined
    Use #ifdef HEADER_H instead of #ifdef OS
    FreeBSD session activation
    Place sessions with a VT on the static Seat1
    clean up clang warnings
    correct uninitialized variable warning
    Explicitly check for needed symbols
    Correct build errors in OpenBSD
    fix compiling on FreeBSD
    Misc Changes:
    
    update gitignore
    more autotools cleanup work
    update readme with new deps
    
    1.1.2:
    
    [Please note that this is a development release.]
    
    New Features:
    
    add a session-state property and method
    Add session-class dbus property and get method
    introduce libconsolekit
    Add dbus method GetVTNr to Session
    add dbus LockSession/UnlockSession
    add dbus ActivateSession/ActivateSessionOnSeat
    Bug Fixes:
    
    add session class + state to ck-list-sessions
    Allow session-class property to be set via OpenSessionWithParameters
    wrong lib linked in libconsolekit.pc.in
    remove the rest of the gcc warnings
    remove warnings about signed/unsigned comparisons
    remove warning about redeclaration
    remove warnings about redundant function declarations
    fix --enable-libudev configure flag
    coverity: 180820 remove dead code
    Misc Changes:
    
    add CONSOLEKIT_DEBUGGING support back in
    update ck-list-sessions man page
    update tests for new methods added
    simplify configure.ac
    document session types allowed
    
    
    1.1.1:
    
    [Please note that this is a development release.]
    
    Changes:
    
    Don't try to run "chmod +x" on 90-consolekit, it is not a executable.
    Use INSTALL_SCRIPT instead of INSTALL_PROGRAM for 90-consolekit.
    Remove the root restriction for XDG_RUNTIME_DIR.
    cgroups: Attempt to use 'cpuaccount' if 'all' cgroup fails on pid move
    ck-remove-directory: return exit code 0 on success
    ck-remove-directory: remove unused dest parameter on become_user
    ck-remove-directory: remove dest dir as real user
    Add support for selinux
    Freebsd 10.3: Fix session activation
    Fix a reversed condition for invalid device on NetBSD
    cgroups: Use UID of logged in user instead UID of session leader process
    Fix memory leaks and a memory corruption issue.
    OpenBSD: adapt to recent kvm(3) restrictions
    Fix pam-ck-connector on musl
    Fix for OpenBSD and FreeBSD VT device naming scheme.
    Add a few more display manager users to filter out.
    Update udev rules.
    Handle XDG_RUNTIME_DIR not being created/set.
    Add support for server managed FDs.
    Prevent a use-after-free when emitting signal changes.
    Fix coverity and gcc warnings.
    Add a LockedHint property.

--- sysutils/consolekit/patches/patch-src_ck-sysdeps-netbsd.c
+++ sysutils/consolekit/patches/patch-src_ck-sysdeps-netbsd.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_ck-sysdeps-netbsd.c,v 1.1 2017/06/22 22:09:17 kamil Exp $
+$NetBSD: patch-src_ck-sysdeps-netbsd.c,v 1.2 2018/05/16 11:30:36 youri Exp $
 
-Don't include <sys/user.h> on NetBSD.
+Fix NetBSD build.
 
---- src/ck-sysdeps-netbsd.c.orig	2016-03-19 15:14:24.000000000 +0000
+--- src/ck-sysdeps-netbsd.c.orig	2017-06-02 01:31:44.000000000 +0000
 +++ src/ck-sysdeps-netbsd.c
 @@ -36,7 +36,6 @@
  #include <sys/types.h>
@@ -11,4 +11,16 @@ Don't include <sys/user.h> on NetBSD.
 -#include <sys/user.h>
  #include <sys/ioctl.h>
  
- #include <dev/wscons/wsdisplay_usl_io.h>
+ #ifdef HAVE_SYS_MOUNT_H
+@@ -494,7 +493,11 @@ ck_make_tmpfs (guint uid, guint gid, con
+ 
+         opts = g_strdup_printf ("mode=0700,uid=%d", uid);
+ 
++#ifdef __NetBSD__
++	result = mount("tmpfs", dest, 0, opts, strlen(opts));
++#else
+         result = mount("tmpfs", dest, 0, opts);
++#endif
+ 
+         g_free (opts);
+ 
