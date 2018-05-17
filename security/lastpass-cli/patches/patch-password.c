7dbc62f3e8cea881a0d5c7bec96ae4f28bd79abf
jperkin <jperkin@pkgsrc.org>
2018-05-17 14:06:58 +0000

    lastpass-cli: Update to 1.3.1.
    # Version 1.3.1
     * Revert "pins: remove GlobalSign R1/R3 pins" from Robert Copeland
     * Readme update from Wesley Schwengle
     * Add Dockerfile to create a clean build environment from Wesley Schwengle
     * Missing dependencies in readme
     * Added CLion project files to ignore list
    
    # Version 1.3.0
     * `lpass show` now supports `--json` format
     * `lpass show` now supports `--quiet` flag to suppress prompts,
       from Pau Sanchez
     * `lpass import` has `--keep-dupes` flag which will preserve duplicate
       accounts on import
     * `LPASS_PINENTRY` environment variable may now be used to set custom
       path to pinentry, from Martynas Mickevičius
     * Build fix for aarch64 and others from Natanael Copa
     * New fish completions from Israel Chauca Fuentes
     * Zsh completions from Richard Hillmann
     * Brew build instructions updates from Roger D. Winans
     * bugfix: site notes now show up in notes textarea instead of fields
     * spelling fixes from Josh Soref
    
    # Version 1.2.2
     * `lpass ls --format` now supports "%al" to show URL, from Yikai Zhao
     * $VISUAL can be used in preference to $EDITOR, from Wesley Schwengle
     * `lpass edit` can now directly edit multiline ssh keys
     * fields are now preserved when edited with `lpass edit`
     * Bugfix: use-after-free in http.c fixed, from Björn Ketelaars
     * Bugfix: command-line completion now works for names with spaces
     * Bugfix: loading attachments from shared folders now works, from Spencer
       Whyte
     * Debian packing updates from Hannes Hörl
     * Documentation updates from Darragh Grealish and Steven Liekens

--- security/lastpass-cli/patches/patch-password.c
+++ security/lastpass-cli/patches/patch-password.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-password.c,v 1.1 2017/08/16 15:18:24 jperkin Exp $
+$NetBSD: patch-password.c,v 1.2 2018/05/17 14:06:58 jperkin Exp $
 
 Avoid "Missing sentinel in function call" warnings.
 
---- password.c.orig	2017-06-28 12:50:49.000000000 +0000
+--- password.c.orig	2018-05-17 09:42:27.000000000 +0000
 +++ password.c
 @@ -73,7 +73,7 @@ static char *password_prompt_askpass(con
  		close(read_fds[1]);
@@ -13,12 +13,12 @@ Avoid "Missing sentinel in function call" warnings.
  		_exit(76);
  	}
  	close(read_fds[1]);
-@@ -266,7 +266,7 @@ char *password_prompt(const char *prompt
+@@ -273,7 +273,7 @@ char *password_prompt(const char *prompt
  		close(read_fds[1]);
  		close(write_fds[0]);
  		close(write_fds[1]);
--		execlp("pinentry", "pinentry", NULL);
-+		execlp("pinentry", "pinentry", (char *)0);
+-		execlp(pinentry, pinentry, NULL);
++		execlp(pinentry, pinentry, (char *)0);
  		_exit(76);
  	}
  	close(read_fds[1]);
