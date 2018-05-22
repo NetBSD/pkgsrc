1e6a61a1138c272776144102e53e64337155cc20
youri <youri@pkgsrc.org>
2018-05-22 16:03:00 +0000

    Update to 1.1.0:
    Changes in 1.1.0 since 1.0.2:
    
    * Fixed crash with invalid <Name> tag in a menu.
    
    * Added new API menu_cache_app_get_generic_name() to get generic
        name for application.
    
    * Fixed potential access violation, use runtime user dir instead of tmp dir.
        It limits libmenu-cache compatibility to menu-cached >= 0.7.0.
    
    * Directory $XDG_DATA_HOME/applications will be created if it does not
        exist so it will be monitored in any case.
    
    * Fixed issue when subdirectories added would be skipped in monitoring.
    
    * Fixed potential file descriptors leak.
    
    * Reduced inactivity timer to 6 seconds (from 600 seconds).
    
    * Fixed an issue with multiple daemons started: test if daemon is already
        running on socket before killing old socket file.
    
    * Fixed 100% CPU load by menu-cached due to invalid dup2() call.
    
    
    Changes in 1.0.2 since 1.0.1:
    
    * Fixed crash in menu-cached if cache regeneration fails.
    
    * Fixed 100% CPU load by menu-cached in some rare conditions.
    
    * Invalid empty <DefaultLayout> should be ignored, see specification.
    
    * Fixed crash in menu-cache-gen on <MergeFile type="parent"/> if no file
        to merge found.
    
    * Fixed showing empty Other menu in some cases.
    
    * The option --disable-debug is now default, instead of --enable-debug.

--- sysutils/menu-cache/patches/patch-menu-cache-daemon_menu-cached.c
+++ sysutils/menu-cache/patches/patch-menu-cache-daemon_menu-cached.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-menu-cache-daemon_menu-cached.c,v 1.1 2016/04/28 14:57:46 youri Exp $
+$NetBSD: patch-menu-cache-daemon_menu-cached.c,v 1.2 2018/05/22 16:03:00 youri Exp $
 
 Fix SunOS build.
 
---- menu-cache-daemon/menu-cached.c.orig	2012-11-14 08:07:36.000000000 +0000
+--- menu-cache-daemon/menu-cached.c.orig	2017-11-02 18:57:27.000000000 +0000
 +++ menu-cache-daemon/menu-cached.c
-@@ -28,6 +28,7 @@
+@@ -33,6 +33,7 @@
  
  #include <stdio.h>
  #include <stdlib.h>
 +#include <fcntl.h>
  #include <glib.h>
+ #include <glib/gstdio.h>
  #include <gio/gio.h>
- #include <sys/types.h>
