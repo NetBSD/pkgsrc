321a98c61f0f178b0ea524cee1df396a1419b96b
leot <leot@pkgsrc.org>
2018-05-23 18:44:52 +0000

    bitlbee: Always create the ${pidfile} directory and improve rc.d script
    - At least on NetBSD every files on /var/run are removed at every
      boot by mountcritlocal rc.d script. Add a bitlbee_precmd() function
      to always check that a directory for the ${pidfile} exists and
      create it if needed.
    - Check if /etc/rc.subr exists before source-ing it.
    - Introduce BITLBEE_USER and BITLBEE_GROUP variables and reuse them to avoid
      hardcoding `bitlbee' and `nobody' (NFCI)
    
    Discussed with and suggestions from <tonio>, thanks!

--- chat/bitlbee/files/bitlbee.sh
+++ chat/bitlbee/files/bitlbee.sh
@@ -3,13 +3,25 @@
 # PROVIDE: bitlbee
 # REQUIRE: DAEMON
 
-. /etc/rc.subr
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
+fi
 
 name="bitlbee"
 rcvar=$name
 command="@PREFIX@/sbin/bitlbee"
 pidfile="@VARBASE@/run/${name}/pid"
 command_args="-D"	# run as a daemon
+start_precmd="bitlbee_precmd"
+
+bitlbee_precmd()
+{
+	if [ ! -d "@VARBASE@/run/${name}" ]; then
+		@MKDIR@ "@VARBASE@/run/${name}"
+		@CHMOD@ 0700 "@VARBASE@/run/${name}"
+		@CHOWN@ @BITLBEE_USER@:@BITLBEE_GROUP@ "@VARBASE@/run/${name}"
+	fi
+}
 
 if [ -f /etc/rc.subr ]; then
 	load_rc_config $name
