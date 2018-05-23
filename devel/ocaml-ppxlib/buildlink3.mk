f302ff04886db348e8fa3a03121289be8a915115
jaapb <jaapb@pkgsrc.org>
2018-05-23 08:11:55 +0000

    Added SUPERSEDES to devel/ocaml-ppxlib, revbump
    This package replaces several other ocaml PPX-related packages; this is
    now formally stated with a SUPERSEDES variable.

--- devel/ocaml-ppxlib/buildlink3.mk
+++ devel/ocaml-ppxlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/05/09 14:46:59 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/05/23 08:11:55 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppxlib
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppxlib
 OCAML_PPXLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.3.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.3.0nb1
 BUILDLINK_PKGSRCDIR.ocaml-ppxlib?=	../../devel/ocaml-ppxlib
 
 .endif	# OCAML_PPXLIB_BUILDLINK3_MK
