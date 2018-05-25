5ec5fe717076b6ac773e2a07f2521c70e8a6f5ec
jmcneill <jmcneill@pkgsrc.org>
2018-05-25 10:45:22 +0000

    Update U-Boot to 2018.05.

--- sysutils/u-boot/u-boot-version.mk
+++ sysutils/u-boot/u-boot-version.mk
@@ -1,3 +1,3 @@
-# $NetBSD: u-boot-version.mk,v 1.1 2018/05/24 22:35:56 jmcneill Exp $
+# $NetBSD: u-boot-version.mk,v 1.2 2018/05/25 10:45:22 jmcneill Exp $
 
-UBOOT_VERSION?=	2017.11
+UBOOT_VERSION?=	2018.05
