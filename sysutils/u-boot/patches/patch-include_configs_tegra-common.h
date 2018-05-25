de8243a079d197bcea91f95d980eb25cfe8d1c5b
jmcneill <jmcneill@pkgsrc.org>
2018-05-25 10:45:22 +0000

    Update U-Boot to 2018.05.

--- sysutils/u-boot/patches/patch-include_configs_tegra-common.h
+++ sysutils/u-boot/patches/patch-include_configs_tegra-common.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-include_configs_tegra-common.h,v 1.1 2017/09/23 17:33:12 jmcneill Exp $
+$NetBSD: patch-include_configs_tegra-common.h,v 1.2 2018/05/25 10:45:22 jmcneill Exp $
 
---- include/configs/tegra-common.h.orig	2017-07-10 17:07:38.000000000 +0000
+--- include/configs/tegra-common.h.orig	2018-05-07 15:32:36.000000000 +0000
 +++ include/configs/tegra-common.h
-@@ -101,4 +101,7 @@
- #include <config_distro_defaults.h>
- #endif
+@@ -92,4 +92,7 @@
+ /* Misc utility code */
+ #define CONFIG_BOUNCE_BUFFER
  
 +/* Override default uimg size limit */
 +#define	CONFIG_SYS_BOOTM_LEN		0x1000000
