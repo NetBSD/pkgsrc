3d803cbf5b188f743727febf2d7a6a02a0cb0c05
youri <youri@pkgsrc.org>
2018-05-28 23:41:52 +0000

    Update to fix free space calculation on a hard drive on NetBSD.

--- sysutils/xfce4-fsguard-plugin/patches/patch-panel-plugin_fsguard.c
+++ sysutils/xfce4-fsguard-plugin/patches/patch-panel-plugin_fsguard.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-panel-plugin_fsguard.c,v 1.2 2017/06/20 21:49:03 youri Exp $
+$NetBSD: patch-panel-plugin_fsguard.c,v 1.3 2018/05/28 23:41:53 youri Exp $
 
 Fix NetBSD support.
 Add SunOS, OS X and DragonFlyBSD support.
 
---- panel-plugin/fsguard.c.orig	2012-06-30 09:07:55.000000000 +0000
+--- panel-plugin/fsguard.c.orig	2016-10-29 06:43:10.000000000 +0000
 +++ panel-plugin/fsguard.c
 @@ -38,13 +38,18 @@
  #include <string.h>
@@ -25,3 +25,15 @@ Add SunOS, OS X and DragonFlyBSD support.
  #include <gtk/gtk.h>
  #include <libxfce4util/libxfce4util.h>
  #include <libxfce4ui/libxfce4ui.h>
+@@ -277,7 +282,11 @@ fsguard_check_fs (FsGuard *fsguard)
+     err = statfs (fsguard->path, &fsd);
+ 
+     if (err != -1) {
++#ifdef __NetBSD__
++        blocksize       = fsd.f_frsize;
++#else
+         blocksize       = fsd.f_bsize;
++#endif
+         freeblocks      = fsd.f_bavail;
+         totalblocks     = fsd.f_blocks;
+         freespace       = (freeblocks * blocksize) / 1048576;
