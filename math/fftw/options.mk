f81e7f5520dbab1157a48929d9d6fb852887bccb
adam <adam@pkgsrc.org>
2018-05-30 06:45:58 +0000

    fftw: updated to 3.3.8
    FFTW 3.3.8:
    
    * Fixed AVX, AVX2 for gcc-8.
    
      By default, FFTW 3.3.7 was broken with gcc-8.  AVX and AVX2 code
      assumed that the compiler honors the distinction between +0 and -0,
      but gcc-8 -ffast-math does not.  The default CFLAGS included -ffast-math.
      This release ensures that FFTW works with gcc-8 -ffast-math, and
      removes -ffast-math from the default CFLAGS for good measure.

--- math/fftw/options.mk
+++ math/fftw/options.mk
@@ -1,15 +1,14 @@
-# $NetBSD: options.mk,v 1.2 2013/07/09 15:46:45 tsutsui Exp $
+# $NetBSD: options.mk,v 1.3 2018/05/30 06:45:58 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fftw
 PKG_SUPPORTED_OPTIONS=	fftw-fortran
-PKG_SUGGESTED_OPTIONS=
 # Enable fortran support by default on platforms supported by lang/g95.
 .if (!empty(MACHINE_ARCH:Mi386) || !empty(MACHINE_ARCH:Mx86_64) || \
 	!empty(MACHINE_ARCH:Mia64) || !empty(MACHINE_ARCH:Mpowerpc*) || \
 	!empty(MACHINE_ARCH:Mhppa) || !empty(MACHINE_ARCH:Msparc*) || \
 	!empty(MACHINE_ARCH:Malpha) || !empty(MACHINE_ARCH:Mmips*))
 # ...but disable it until lang/g95 issue is resolved.
-#PKG_SUGGESTED_OPTIONS+=	fftw-fortran
+#PKG_SUGGESTED_OPTIONS=	fftw-fortran
 .endif
 
 .include "../../mk/bsd.options.mk"
