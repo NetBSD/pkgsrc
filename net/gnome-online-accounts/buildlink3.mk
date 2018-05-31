6b847d307a6d471f12b8eb9d4e68d8f421fb152f
youri <youri@pkgsrc.org>
2018-05-31 14:44:30 +0000

    Update gnome-online-accounts to 3.29.1:
    Too many changes to list, see
    https://github.com/GNOME/gnome-online-accounts/blob/master/NEWS.

--- net/gnome-online-accounts/buildlink3.mk
+++ net/gnome-online-accounts/buildlink3.mk
@@ -1,13 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/02/04 12:19:23 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/05/31 14:44:30 youri Exp $
 
 BUILDLINK_TREE+=	gnome-online-accounts
 
 .if !defined(GNOME_ONLINE_ACCOUNTS_BUILDLINK3_MK)
 GNOME_ONLINE_ACCOUNTS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnome-online-accounts+=	gnome-online-accounts>=3.16.3nb2
+BUILDLINK_API_DEPENDS.gnome-online-accounts+=	gnome-online-accounts>=3.29.1
 BUILDLINK_PKGSRCDIR.gnome-online-accounts?=	../../net/gnome-online-accounts
 
+.include "../../security/libgcrypt/buildlink3.mk"
+.include "../../security/libsecret/buildlink3.mk"
+.include "../../textproc/json-glib/buildlink3.mk"
+.include "../../www/librest07/buildlink3.mk"
+.include "../../www/webkit-gtk/buildlink3.mk"
 .endif	# GNOME_ONLINE_ACCOUNTS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnome-online-accounts
