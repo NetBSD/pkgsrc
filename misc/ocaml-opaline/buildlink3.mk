5a6d5f36a63c055f01b358988aa1fcbce84b8164
jaapb <jaapb@pkgsrc.org>
2018-05-31 11:22:04 +0000

    Updated misc/ocaml-opaline to version 0.3.1.
    Several improvements to enhance compatibility with opam-installer.

--- misc/ocaml-opaline/buildlink3.mk
+++ misc/ocaml-opaline/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/04/13 12:27:02 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/05/31 11:22:04 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-opaline
 
 .if !defined(OCAML_OPALINE_BUILDLINK3_MK)
 OCAML_OPALINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-opaline+=	ocaml-opaline>=0.3.0
+BUILDLINK_API_DEPENDS.ocaml-opaline+=	ocaml-opaline>=0.3.1
 BUILDLINK_PKGSRCDIR.ocaml-opaline?=	../../misc/ocaml-opaline
 
 # We only need this to install
