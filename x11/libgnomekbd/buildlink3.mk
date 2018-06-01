0e6941dad3f6fd3d011f52855140082d9c77795e
youri <youri@pkgsrc.org>
2018-06-01 17:04:18 +0000

    - Move libgnomekbd to libgnomekbd2 for older Gnome2 packages - Import libgnomekbd 3.6.0 for Cinnamon and Gnome3.

--- x11/libgnomekbd/buildlink3.mk
+++ x11/libgnomekbd/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2016/03/12 11:28:37 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2018/06/01 17:04:18 youri Exp $
 
 BUILDLINK_TREE+=	libgnomekbd
 
 .if !defined(LIBGNOMEKBD_BUILDLINK3_MK)
 LIBGNOMEKBD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgnomekbd+=	libgnomekbd>=2.18.0
-BUILDLINK_ABI_DEPENDS.libgnomekbd+=	libgnomekbd>=2.32.0nb19
+BUILDLINK_API_DEPENDS.libgnomekbd+=	libgnomekbd>=3.6.0
+BUILDLINK_ABI_DEPENDS.libgnomekbd+=	libgnomekbd>=3.6.0
 BUILDLINK_PKGSRCDIR.libgnomekbd?=	../../x11/libgnomekbd
 
 .include "../../x11/libxklavier/buildlink3.mk"
