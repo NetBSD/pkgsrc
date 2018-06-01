2b95caa91aa5c10c1372a7f797a47448f1718cb5
adam <adam@pkgsrc.org>
2018-06-01 07:55:43 +0000

    msgpack: updated to 3.0.1
    version 3.0.1
    Add fuzz directory to release tar ball
    Add include file checking for X-Code
    
    
    version 3.0.0
    
    breaking changes
    ----------------
    Change offset parameter updating rule. If parse error is happend, offset is updated to the error position.
    
    other updates
    -------------
    Improve cross platform configuration
    Improve build system
    Improve user class adaptor
    Improve msgpack::object visitation logic
    Remove some warnings
    Add -> and * operators to object_handle
    Improve CI environment
    Improve documents
    Refactoring
    Add OSS-Fuzz support

--- devel/msgpack/patches/patch-include_msgpack_v1_vrefbuffer.hpp
+++ devel/msgpack/patches/patch-include_msgpack_v1_vrefbuffer.hpp
@@ -1,4 +1,6 @@
-$NetBSD: patch-include_msgpack_v1_vrefbuffer.hpp,v 1.1 2017/09/04 15:58:05 joerg Exp $
+$NetBSD: patch-include_msgpack_v1_vrefbuffer.hpp,v 1.2 2018/06/01 07:55:43 adam Exp $
+
+NetBSD and FreeBSD fix.
 
 --- include/msgpack/v1/vrefbuffer.hpp.orig	2017-09-03 19:45:31.891730579 +0000
 +++ include/msgpack/v1/vrefbuffer.hpp
