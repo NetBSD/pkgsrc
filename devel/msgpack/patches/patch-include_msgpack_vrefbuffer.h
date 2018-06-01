c82dbb1ea0cc0165a5d209f72154a748551edeae
adam <adam@pkgsrc.org>
2018-06-01 07:55:43 +0000

    msgpack: updated to 3.0.1
    version 3.0.1
    Add fuzz directory to release tar ball
    Add include file checking for X-Code
    
    
    version 3.0.0
    
    breaking changes
    ----------------
    Change offset parameter updating rule. If parse error is happend, offset is updated to the error position.
    
    other updates
    -------------
    Improve cross platform configuration
    Improve build system
    Improve user class adaptor
    Improve msgpack::object visitation logic
    Remove some warnings
    Add -> and * operators to object_handle
    Improve CI environment
    Improve documents
    Refactoring
    Add OSS-Fuzz support

--- devel/msgpack/patches/patch-include_msgpack_vrefbuffer.h
+++ devel/msgpack/patches/patch-include_msgpack_vrefbuffer.h
@@ -1,13 +1,15 @@
-$NetBSD: patch-include_msgpack_vrefbuffer.h,v 1.1 2017/09/04 15:58:05 joerg Exp $
+$NetBSD: patch-include_msgpack_vrefbuffer.h,v 1.2 2018/06/01 07:55:43 adam Exp $
 
---- include/msgpack/vrefbuffer.h.orig	2017-09-03 19:45:34.429664800 +0000
+FreeBSD fix.
+
+--- include/msgpack/vrefbuffer.h.orig	2018-04-09 13:56:00.000000000 +0000
 +++ include/msgpack/vrefbuffer.h
 @@ -13,7 +13,7 @@
  #include "zone.h"
  #include <stdlib.h>
  
--#if defined(unix) || defined(__unix) || defined(__APPLE__) || defined(__OpenBSD__) || defined(__QNX__) || defined(__QNXTO__)
-+#if defined(unix) || defined(__unix) || defined(__APPLE__) || defined(__OpenBSD__) || defined(__QNX__) || defined(__QNXTO__) || defined(__NetBSD__) || defined(__FreeBSD__)
+-#if defined(unix) || defined(__unix) || defined(__APPLE__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__QNX__) || defined(__QNXTO__)
++#if defined(unix) || defined(__unix) || defined(__APPLE__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__QNX__) || defined(__QNXTO__) || defined(__FreeBSD__)
  #include <sys/uio.h>
  #else
  struct iovec {
