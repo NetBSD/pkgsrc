32297ae2e0eb07afeb29d38fe4cb10ac6f6fb2be
schmonz <schmonz@pkgsrc.org>
2018-06-02 00:10:48 +0000

    Update to 2.20. From the changelog:
    - Added support for STARTTLS directly in the SMTP protocol.
      Adapted from contribution by John R. Levine.
    
    - Added support for "final ok" rules in mailrules plugin.
    
    - Added hook for debugging plugin invocation (set $MSG_DEBUG=1).

--- mail/mailfront/options.mk
+++ mail/mailfront/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2017/07/31 20:40:19 schmonz Exp $
+# $NetBSD: options.mk,v 1.2 2018/06/02 00:10:48 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mailfront
-PKG_SUPPORTED_OPTIONS+=	lua
-PKG_SUGGESTED_OPTIONS+=	lua
+PKG_SUPPORTED_OPTIONS+=	lua gnutls
+#PKG_SUGGESTED_OPTIONS+=	lua gnutls
 
 .include "../../mk/bsd.options.mk"
 
@@ -12,4 +12,12 @@ LUA_VERSIONS_ACCEPTED=	51
 .  include "../../lang/lua/buildlink3.mk"
 BUILD_TARGET+=		lua
 PLIST.lua=		yes
+.endif
+
+.if !empty(PKG_OPTIONS:Mgnutls)
+BUILDLINK_API_DEPENDS.gnutls+=  gnutls>=3.5.0
+.  include "../../security/gnutls/buildlink3.mk"
+DJB_CONFIG_CMDS+=	${ECHO} gnutls > conf-tls;
+.else
+DJB_CONFIG_CMDS+=	${ECHO} none > conf-tls;
 .endif
