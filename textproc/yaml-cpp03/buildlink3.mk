f2870beccf33c50bad9748c9e6817ba6f9bb40dc
joerg <joerg@pkgsrc.org>
2018-06-02 19:04:32 +0000

    Pick up the correct yaml-cpp version.

--- textproc/yaml-cpp03/buildlink3.mk
+++ textproc/yaml-cpp03/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/04/06 03:41:07 maya Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/06/02 19:04:32 joerg Exp $
 
 BUILDLINK_TREE+=	yaml-cpp
 
 .if !defined(YAML_CPP_BUILDLINK3_MK)
 YAML_CPP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.yaml-cpp+=	yaml-cpp>=0.3.0
+BUILDLINK_API_DEPENDS.yaml-cpp+=	yaml-cpp>=0.3.0<0.4
 BUILDLINK_PKGSRCDIR.yaml-cpp?=		../../textproc/yaml-cpp03
 
 .endif	# YAML_CPP_BUILDLINK3_MK
