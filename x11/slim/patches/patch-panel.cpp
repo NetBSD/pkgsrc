6e46baf6771e5e88a12227b6e65da8b23bdadb9a
joerg <joerg@pkgsrc.org>
2018-06-02 21:00:03 +0000

    Fix patch to actually correct the real error here.

--- x11/slim/patches/patch-panel.cpp
+++ x11/slim/patches/patch-panel.cpp
@@ -1,4 +1,6 @@
-$NetBSD: patch-panel.cpp,v 1.1 2018/06/02 16:46:50 jmcneill Exp $
+$NetBSD: patch-panel.cpp,v 1.2 2018/06/02 21:00:03 joerg Exp $
+
+XCreateGC returns a null pointer on failure, not a negative value.
 
 --- panel.cpp.orig	2013-10-01 22:38:05.000000000 +0000
 +++ panel.cpp
@@ -7,7 +9,7 @@ $NetBSD: patch-panel.cpp,v 1.1 2018/06/02 16:46:50 jmcneill Exp $
  		gcv.graphics_exposures = False;
  		WinGC = XCreateGC(Dpy, Win, gcm, &gcv);
 -		if (WinGC < 0) {
-+		if ((intptr_t)WinGC < 0) {
++		if (!WinGC) {
  			cerr << APPNAME
  				<< ": failed to create pixmap\n.";
  			exit(ERR_EXIT);
