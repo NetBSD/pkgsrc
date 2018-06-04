8826aece1f842738851ca6091ac12e87fc496329
youri <youri@pkgsrc.org>
2018-06-04 01:01:23 +0000

    Fix NetBSD.

--- sysutils/consolekit/patches/patch-src_ck-sysdeps-netbsd.c
+++ sysutils/consolekit/patches/patch-src_ck-sysdeps-netbsd.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_ck-sysdeps-netbsd.c,v 1.2 2018/05/16 11:30:36 youri Exp $
+$NetBSD: patch-src_ck-sysdeps-netbsd.c,v 1.3 2018/06/04 01:01:23 youri Exp $
 
 Fix NetBSD build.
 
@@ -12,6 +12,24 @@ Fix NetBSD build.
  #include <sys/ioctl.h>
  
  #ifdef HAVE_SYS_MOUNT_H
+@@ -121,7 +120,7 @@ ck_process_stat_get_tty (CkProcessStat *
+ {
+         g_return_val_if_fail (stat != NULL, NULL);
+ 
+-        if (stat->tty == NODEV){
++        if (stat->tty == ENODEV){
+                 return NULL;
+         }
+ 
+@@ -164,7 +163,7 @@ stat2proc (pid_t        pid,
+         }
+ 
+         num = KI_MAXCOMLEN;
+-        if (num >= sizeof(P->cmd)) {
++        if ((unsigned)num >= sizeof(P->cmd)) {
+                 num = sizeof(P->cmd) - 1;
+         }
+ 
 @@ -494,7 +493,11 @@ ck_make_tmpfs (guint uid, guint gid, con
  
          opts = g_strdup_printf ("mode=0700,uid=%d", uid);
