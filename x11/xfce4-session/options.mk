588e08b0eac9c420ae5c6cc3a9b557b5e0b01ab4
youri <youri@pkgsrc.org>
2018-06-04 10:51:56 +0000

    Optionalize mate-polkit.

--- x11/xfce4-session/options.mk
+++ x11/xfce4-session/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2018/06/01 14:15:47 youri Exp $
+# $NetBSD: options.mk,v 1.3 2018/06/04 10:51:56 youri Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xfce4-session
 PKG_SUPPORTED_OPTIONS=	polkit
@@ -9,6 +9,6 @@ PKG_SUGGESTED_OPTIONS=	polkit
 PLIST_VARS=	polkit
 
 .if !empty(PKG_OPTIONS:Mpolkit)
-.include "../../security/polkit/buildlink3.mk"
+.include "../../security/mate-polkit/buildlink3.mk"
 PLIST.polkit=	yes
 .endif
