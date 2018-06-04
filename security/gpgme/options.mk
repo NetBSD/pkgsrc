6fab62b34868ea80f7a0887084e0854b01c91875
leot <leot@pkgsrc.org>
2018-06-04 07:44:35 +0000

    gpgme: Improve style and comment in options.mk (NFCI)
    - Move the CONFIGURE_ARGS in the `gnupg1' logic after DEPENDS to make the
      two branches symmetric
    - Adjust the comment, the tests needs gpgsm, not gpg-agent
    
    Suggested via a private email by <uwe>, thanks!

--- security/gpgme/options.mk
+++ security/gpgme/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.10 2018/06/03 21:18:34 leot Exp $
+# $NetBSD: options.mk,v 1.11 2018/06/04 07:44:35 leot Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.gpgme
 PKG_OPTIONS_REQUIRED_GROUPS=	gnupg
@@ -13,6 +13,6 @@ REPLACE_SH+=		tests/gpg/pinentry
 GPG_DEFAULT=		${LOCALBASE}/bin/gpg2
 MAKE_FLAGS+=		GPG=${GPG_DEFAULT}
 .else
-CONFIGURE_ARGS+=	--disable-gpgsm-test	# XXX: needs gpg-agent
 DEPENDS+=		gnupg>=1.4.2:../../security/gnupg
+CONFIGURE_ARGS+=	--disable-gpgsm-test	# XXX: needs gpgsm
 .endif
