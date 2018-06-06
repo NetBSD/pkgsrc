9e2d5368ab60bd42d060559e660f6bf3d6a8ae9f
wiz <wiz@pkgsrc.org>
2018-06-06 21:58:28 +0000

    u-boot: include bsd.pkg.mk later
    Ideally, this should happen in the main Makefiles and not in
    an included file like this one, but the change for that is too big
    for me for now and this fixes u-boot-pine64.

--- sysutils/u-boot/u-boot-arm64.mk
+++ sysutils/u-boot/u-boot-arm64.mk
@@ -1,9 +1,11 @@
-# $NetBSD: u-boot-arm64.mk,v 1.2 2017/09/07 15:30:31 jmcneill Exp $
+# $NetBSD: u-boot-arm64.mk,v 1.3 2018/06/06 21:58:28 wiz Exp $
 
 MAINTAINER=	port-arm@NetBSD.org
 BUILD_DEPENDS+=	cross-aarch64-none-elf-gcc-[0-9]*:../../cross/aarch64-none-elf-gcc
 
 UBOOT_ENV+=	CROSS_COMPILE=${PREFIX}/cross-aarch64-none-elf/bin/aarch64-none-elf-
+
+.include "u-boot.mk"
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.u-boot-${UBOOT_TARGET}
 PKG_SUPPORTED_OPTIONS=	el1
@@ -14,4 +16,4 @@ PKG_SUPPORTED_OPTIONS=	el1
 UBOOT_OPTIONS+=	CONFIG_ARMV8_SWITCH_TO_EL1=y
 .endif
 
-.include "u-boot.mk"
+.include "../../mk/bsd.pkg.mk"
