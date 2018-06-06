e8b346cd374be325b7de39600245eb45237a0feb
gdt <gdt@pkgsrc.org>
2018-06-06 22:45:58 +0000

    hamlib: update to 3.2
    Version 3.2
            2018-03-31
            * New models, IC-7850/IC-7851 in IC-785x.  Mike, W9MDB
            * Fix ft991_get_mode, Mike, W9MDB
            * New model, FT-891.  Mike, W9MDB
            * New Barrett 2050 backend, Mike, W9MDB
            * New Flrig backend, Mike, W9MDB
            * New model, IC-M710, Mike, W9MDB
            * Build instructions and test script for Python3
            * Rename autogen.sh to bootsrap and don't call configure
            * micro-ham support. Christoph, DL1YCF
            * New rotator, SPID MD-01/02 in ROT2 mode, Norvald Ryeng, LA6YKA
            * New model, Flex 6400/6600, Mike, W9MDB
            * New model, IC-7610, Mike, W9MDB
            * Add support for CM108B PTT, Max Lock
            * New model, Rohde & Schwarz XK2100, Mike, W9MDB
            * Many other bug fixes and enhancements

--- ham/hamlib/buildlink3.mk
+++ ham/hamlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:24:46 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2018/06/06 22:45:58 gdt Exp $
 
 BUILDLINK_TREE+=	hamlib
 
@@ -9,5 +9,7 @@ BUILDLINK_API_DEPENDS.hamlib+=	hamlib>=1.1.4
 BUILDLINK_ABI_DEPENDS.hamlib+=	hamlib>=1.2.5
 BUILDLINK_PKGSRCDIR.hamlib?=	../../ham/hamlib
 .endif # HAMLIB_BUILDLINK3_MK
+
+.include "../../devel/libusb1/buildlink3.mk"
 
 BUILDLINK_TREE+=	-hamlib
