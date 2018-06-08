09d143aa16628f82284533a4ed00d1e2c690eb03
nros <nros@pkgsrc.org>
2018-06-08 19:25:39 +0000

    Updated databases/qore-mysql-module to version 2.0.2.1 .
    patch-configure and patch-src_Makefile.in removed since
    they have applied upstream.
    
    Changes from release notes:
    * updated SQLStatement::fetchColumns() to return an empty
      hash when no data is available
    * fixed a bug where DBI-SELECT-ROW-ERROR exceptions were
      being raised as MYSQL-SELECT-ROW-ERROR exceptions
    * fixed formatting bugs in invalid value specification
      error messages
    * updated configure to find and compile with MariaDB and
      Percona includes and libs
    * placeholders and quotes in SQL comments are ignored
    * fixed time column retrieval; the date component is now
      set to 1970-01-01
    * fixed binary and varbinary column handling
    * fixed the "optimal-numbers" option with decimal values
      with decimal points
    * fixed returning result sets with duplicate column names

--- databases/qore-mysql-module/buildlink3.mk
+++ databases/qore-mysql-module/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2018/01/28 16:24:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2018/06/08 19:25:39 nros Exp $
 #
 
 BUILDLINK_TREE+=	qore-mysql-module
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	qore-mysql-module
 QORE_MYSQL_MODULE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qore-mysql-module+=	qore-mysql-module>=2.0.1
-BUILDLINK_ABI_DEPENDS.qore-mysql-module?=	qore-mysql-module>=2.0.1nb8
+BUILDLINK_ABI_DEPENDS.qore-mysql-module?=	qore-mysql-module>=2.0.2.1
 BUILDLINK_PKGSRCDIR.qore-mysql-module?=		../../databases/qore-mysql-module
 
 .include "../../lang/qore/buildlink3.mk"
