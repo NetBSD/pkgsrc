cf962478a092a2b6e27a550825f9b9b212a2d5b4
minskim <minskim@pkgsrc.org>
2018-06-11 18:15:31 +0000

    devel/go-i18n: Add a missing entry to buildlink3.mk

--- devel/go-i18n/buildlink3.mk
+++ devel/go-i18n/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/08/25 16:49:13 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/06/11 18:15:31 minskim Exp $
 
 BUILDLINK_TREE+=	go-i18n
 
@@ -11,6 +11,7 @@ BUILDLINK_DEPMETHOD.go-i18n?=		build
 BUILDLINK_API_DEPENDS.go-i18n+=		go-i18n>=1.8.1
 BUILDLINK_PKGSRCDIR.go-i18n?=		../../devel/go-i18n
 
+.include "../../devel/go-yaml/buildlink3.mk"
 .include "../../www/go-toml-pelletier/buildlink3.mk"
 .endif  # GO_I18N_BUILDLINK3_MK
 
