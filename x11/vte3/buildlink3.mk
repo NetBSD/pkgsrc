5a1c93966b37e41e462b0d9450ca099b78381767
youri <youri@pkgsrc.org>
2018-06-12 23:00:34 +0000

    Update to 52.2, no changelog available.

--- x11/vte3/buildlink3.mk
+++ x11/vte3/buildlink3.mk
@@ -1,18 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.7 2018/05/22 12:21:32 youri Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2018/06/12 23:00:34 youri Exp $
 
-BUILDLINK_TREE+=	vte
+BUILDLINK_TREE+=	vte3
 
-.if !defined(VTE_BUILDLINK3_MK)
-VTE_BUILDLINK3_MK:=
+.if !defined(VTE3_BUILDLINK3_MK)
+VTE3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vte+=	vte3>=0.48.3
-BUILDLINK_ABI_DEPENDS.vte+=	vte3>=0.48.3
-BUILDLINK_PKGSRCDIR.vte?=	../../x11/vte3
+BUILDLINK_API_DEPENDS.vte3+=	vte3>=0.52.2
+BUILDLINK_ABI_DEPENDS.vte3+=	vte3>=0.52.2
+BUILDLINK_PKGSRCDIR.vte3?=	../../x11/vte3
 
 .include "../../devel/pcre2/buildlink3.mk"
 .include "../../security/gnutls/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
 .include "../../mk/termcap.buildlink3.mk"
-.endif # VTE_BUILDLINK3_MK
+.endif # VTE3_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-vte
+BUILDLINK_TREE+=	-vte3
