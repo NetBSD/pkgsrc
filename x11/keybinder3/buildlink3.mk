ef5a0ce80ccde106d36636e5d90da108db3adcc3
youri <youri@pkgsrc.org>
2018-06-12 00:18:31 +0000

    x11/keybinder3: import keybinder3-0.3.2
    The Keybinder package contains a library for registering global keyboard
    shortcuts.
    Keybinder works with GTK-based applications using the X Window System.
    
    The library contains:
    - A C library, libkeybinder
    - Python bindings, python-keybinder
    - An examples directory with programs in C, Lua, Python and Vala.
    
    There are: Python (py-keybinder) and Lua (lua-keybinder) bindings.

---
+++ x11/keybinder3/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2018/06/12 00:18:31 youri Exp $
+
+BUILDLINK_TREE+=	keybinder3
+
+.if !defined(KEYBINDER3_BUILDLINK3_MK)
+KEYBINDER3_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.keybinder3+=	keybinder3>=0.3.0
+BUILDLINK_ABI_DEPENDS.keybinder3+=	keybinder3>=0.3.0nb5
+BUILDLINK_PKGSRCDIR.keybinder3?=	../../x11/keybinder3
+
+.include "../../devel/gobject-introspection/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.endif	# KEYBINDER3_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-keybinder3
