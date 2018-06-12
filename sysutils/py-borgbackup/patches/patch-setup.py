d346fa114656dfc241e5923d0b5806794ed52ed5
bsiegert <bsiegert@pkgsrc.org>
2018-06-12 14:22:25 +0000

    Update py-borgbackup to 1.1.6.
    Version 1.1.6 (2018-06-11)
    --------------------------
    
    Compatibility notes:
    
    - When upgrading from borg 1.0.x to 1.1.x, please note:
    
      - read all the compatibility notes for 1.1.0*, starting from 1.1.0b1.
      - borg upgrade: you do not need to and you also should not run it.
      - borg might ask some security-related questions once after upgrading.
        You can answer them either manually or via environment variable.
        One known case is if you use unencrypted repositories, then it will ask
        about a unknown unencrypted repository one time.
      - your first backup with 1.1.x might be significantly slower (it might
        completely read, chunk, hash a lot files) - this is due to the
        --files-cache mode change (and happens every time you change mode).
        You can avoid the one-time slowdown by using the pre-1.1.0rc4-compatible
        mode (but that is less safe for detecting changed files than the default).
        See the --files-cache docs for details.
    - 1.1.6 changes:
    
      - also allow msgpack-python 0.5.6.
    
    Fixes:
    
    - fix borg exception handling on ENOSPC error with xattrs, #3808
    - prune: fix/improve overall progress display
    - borg config repo ... does not need cache/manifest/key, #3802
    - debug dump-repo-objs should not depend on a manifest obj
    - pypi package:
    
      - include .coveragerc, needed by tox.ini
      - fix package long description, #3854
    
    New features:
    
    - mount: add uid, gid, umask mount options
    - delete:
    
      - only commit once, #3823
      - implement --dry-run, #3822
    - check:
    
      - show progress while rebuilding missing manifest, #3787
      - more --repair output
    - borg config --list <repo>, #3612
    
    Other changes:
    
    - update msgpack requirement, #3753
    - update bundled zstd to 1.3.4, #3745
    - update bundled lz4 code to 1.8.2, #3870
    - docs:
    
      - describe what BORG_LIBZSTD_PREFIX does
      - fix and deduplicate encryption quickstart docs, #3776
    - vagrant:
    
      - FUSE for macOS: upgrade 3.7.1 to 3.8.0
      - exclude macOS High Sierra upgrade on the darwin64 machine
      - remove borgbackup.egg-info dir in fs_init (after rsync)
      - use pyenv-based build/test on jessie32/62
      - use local 32 and 64bit debian jessie boxes
      - use "vagrant" as username for new xenial box
    - travis OS X: use xcode 8.3 (not broken)

--- sysutils/py-borgbackup/patches/patch-setup.py
+++ sysutils/py-borgbackup/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.3 2018/04/21 12:37:46 wiz Exp $
+$NetBSD: patch-setup.py,v 1.4 2018/06/12 14:22:25 bsiegert Exp $
 
 Use latest msgpack version
 
---- setup.py.orig	2018-03-30 07:08:16.000000000 +0000
+--- setup.py.orig	2018-06-11 10:55:36.000000000 +0000
 +++ setup.py
 @@ -39,7 +39,7 @@ install_requires = [
-     # msgpack pure python data corruption was fixed in 0.4.6.
-     # msgpack 0.5.0 was a bit of a troublemaker.
-     # also, msgpack dropped py34 support at 0.5.0.
--    'msgpack-python>=0.4.6,<0.5.0',
-+    'msgpack',
- ]
- 
- # note for package maintainers: if you package borgbackup for distribution,
+     # we are rather picky about msgpack versions, because a good working msgpack is
+     # very important for borg, see https://github.com/borgbackup/borg/issues/3753
+     # best versions seem to be 0.4.6, 0.4.7, 0.4.8 and 0.5.6:
+-    'msgpack-python >=0.4.6, <=0.5.6, !=0.5.0, !=0.5.1, !=0.5.2, !=0.5.3, !=0.5.4, !=0.5.5',
++    'msgpack >=0.4.6, <=0.5.6, !=0.5.0, !=0.5.1, !=0.5.2, !=0.5.3, !=0.5.4, !=0.5.5',
+     # if you can't satisfy the above requirement, these are versions that might
+     # also work ok, IF you make sure to use the COMPILED version of msgpack-python,
+     # NOT the PURE PYTHON fallback implementation: ==0.5.1, ==0.5.4
