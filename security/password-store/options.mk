c9f91af52cf7315f6de22edf9c1a8372d1a5d297
leot <leot@pkgsrc.org>
2018-06-14 16:08:39 +0000

    password-store: Update security/password-store to 1.7.2
    pkgsrc changes:
     - Deprecate PKG_OPTIONS.pass in favour of PKG_OPTIONS.password-store
     - Remove patches/patch-tests_t0300-reencryption.sh, fixed similarly upstream
    
    Changes:
    1.7.2
    -----
    Pass 1.7.2 has a number of important changes and bug fixes, including
    a fix for CVE-2018-12356. Everybody must update immediately.

--- security/password-store/options.mk
+++ security/password-store/options.mk
@@ -1,10 +1,18 @@
-# $NetBSD: options.mk,v 1.2 2018/05/06 11:56:58 leot Exp $
+# $NetBSD: options.mk,v 1.3 2018/06/14 16:08:39 leot Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.pass
+PKG_OPTIONS_VAR=	PKG_OPTIONS.password-store
 
 PKG_SUPPORTED_OPTIONS=		git x11
 PKG_SUGGESTED_OPTIONS=		git
 PKG_OPTIONS_LEGACY_OPTS+=	xclip:x11
+
+.include "../../mk/bsd.prefs.mk"
+
+.if defined(PKG_OPTIONS.pass)
+PKG_LEGACY_OPTIONS+=		${PKG_OPTIONS.pass}
+PKG_OPTIONS_DEPRECATED_WARNINGS+= \
+	"Deprecated variable PKG_OPTIONS.pass used, use ${PKG_OPTIONS_VAR} instead."
+.endif
 
 .include "../../mk/bsd.options.mk"
 
