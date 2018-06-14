94c57894ce5c3ebd4950054d35af4030c560e442
youri <youri@pkgsrc.org>
2018-06-14 12:22:21 +0000

    Fix buildlink3.mk.

--- graphics/colord/buildlink3.mk
+++ graphics/colord/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/06/13 15:49:40 youri Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/06/14 12:22:21 youri Exp $
 
 BUILDLINK_TREE+=	colord
 
@@ -6,23 +6,8 @@ BUILDLINK_TREE+=	colord
 COLORD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.colord+=	colord>=1.2.12
-BUILDLINK_PKGSRCDIR.colord?=	../../cinnamon/colord
+BUILDLINK_PKGSRCDIR.colord?=	../../graphics/colord
 
-# XXX
-# XXX Uncomment and keep only the buildlink3 lines below which are directly
-# XXX needed for dependencies to compile, link, and run.  If this package
-# XXX provides a wrappered API or otherwise does not expose the APIs of the
-# XXX buildlink3 lines below to dependencies, remove them.
-# XXX
-#.include "../../print/cups-base/buildlink3.mk"
-#.include "../../wip/geocode-glib/buildlink3.mk"
-#.include "../../devel/nss/buildlink3.mk"
-.include "../../graphics/lcms2/buildlink3.mk"
-#.include "../../databases/sqlite3/buildlink3.mk"
-#.include "../../security/polkit/buildlink3.mk"
-#.include "../../devel/libdbusmenu-qt/buildlink3.mk"
-#.include "../../sysutils/dbus/buildlink3.mk"
-#.include "../../devel/gobject-introspection/buildlink3.mk"
 .endif	# COLORD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-colord
