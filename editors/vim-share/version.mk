b28ae7bfbbb82bc84ff8f7d36bc7d025a89358b1
morr <morr@pkgsrc.org>
2018-06-16 22:46:43 +0000

    Update to newest patchset.
    Changes:
    
    8.1.0032  BS in prompt buffer starts new line
    8.1.0033  keys to stop Vim in terminal are wrong
    8.1.0034  cursor not restored with ":edit #"
    8.1.0035  not easy to switch between prompt buffer and other windows
    8.1.0036  not restoring Insert mode if leaving prompt buffer with mouse
    8.1.0037  cannot easily append lines to another buffer
    8.1.0038  popup test causes Vim to exit
    8.1.0039  cannot easily delete lines in another buffer
    8.1.0040  warnings from 64-bit compiler
    8.1.0041  attribute "width" missing from python window attribute list
    8.1.0042  if omni completion opens a window Insert mode is stopped
    8.1.0043  ++bad argument of :edit does not work properly
    8.1.0044  if a test function exists Vim this may go unnoticed
    8.1.0045  popup test isn't run completely
    8.1.0046  loading a session file fails if 'winheight' is big
    8.1.0047  no completion for :unlet $VAR
    8.1.0048  vim_str2nr() does not handle numbers close to the maximum
    8.1.0049  shell cannot tell running in a terminal window
    8.1.0050  $VIM_TERMINAL is also set when not in a terminal window
    8.1.0051  MS-Windows: missing #endif
    8.1.0052  when mapping to &lt;Nop&gt; times out the next mapping is skipped
    8.1.0053  first argument of 'completefunc' has inconsistent type
    8.1.0054  compiler warning for using %ld for "long long"
    8.1.0055  complete test has wrong order of arguments
    8.1.0056  crash when using :hardcopy with illegal byte
    8.1.0057  popup menu displayed wrong when using autocmd
    8.1.0058  display problem with margins and scrolling
    8.1.0059  displayed digraph for "ga" wrong with 'encoding' "cp1251"
    8.1.0060  crash when autocommands delete the current buffer
    8.1.0061  window title is wrong after resetting and setting 'title'

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.116 2018/06/04 09:05:00 morr Exp $
+# $NetBSD: version.mk,v 1.117 2018/06/16 22:46:43 morr Exp $
 
 VIM_VERSION=		8.1
-VIM_PATCHLEVEL=		0031
+VIM_PATCHLEVEL=		0061
 # Changelog: see http://ftp.vim.org/pub/vim/patches/8.0/
 VIM_SUBDIR=		vim81
 
