59997483136230a174e59b163938e5e63ebc0f32
adam <adam@pkgsrc.org>
2018-06-16 13:08:32 +0000

    py-requests: updated to 2.19.1
    2.19.1:
    Bugfixes
    - Fixed issue where status_codes.py's init function failed trying to append to
      a __doc__ value of None.
    
    2.19.0:
    Improvements
    - Warn user about possible slowdown when using cryptography version < 1.3.4
    - Check for invalid host in proxy URL, before forwarding request to adapter.
    - Fragments are now properly maintained across redirects. (RFC7231 7.1.2)
    - Removed use of cgi module to expedite library load time.
    - Added support for SHA-256 and SHA-512 digest auth algorithms.
    - Minor performance improvement to Request.content.
    - Migrate to using collections.abc for 3.7 compatibility.
    
    Bugfixes
    - Parsing empty Link headers with parse_header_links() no longer return one bogus entry.
    - Fixed issue where loading the default certificate bundle from a zip archive
      would raise an IOError.
    - Fixed issue with unexpected ImportError on windows system which do not support winreg module.
    - DNS resolution in proxy bypass no longer includes the username and password in
      the request. This also fixes the issue of DNS queries failing on macOS.
    - Properly normalize adapter prefixes for url comparison.
    - Passing None as a file pointer to the files param no longer raises an exception.
    - Calling copy on a RequestsCookieJar will now preserve the cookie policy correctly.

--- devel/py-requests/patches/patch-requests_____init____.py
+++ devel/py-requests/patches/patch-requests_____init____.py
@@ -1,33 +1,35 @@
-$NetBSD: patch-requests_____init____.py,v 1.2 2017/05/29 10:43:42 adam Exp $
+$NetBSD: patch-requests_____init____.py,v 1.3 2018/06/16 13:08:32 adam Exp $
 
+* Allow newer py-urllib3 and py-chardet.
 * Add workaround for segfault of security/py-certbot on NetBSD/amd64
   7.0.2 and 7.99.53. It seems that the real root cause is in
   security/py-OpenSSL or OpenSSL. However I do not find how to fix it.
 
   This is introduced in https://github.com/shazow/urllib3
 
-commit 770105ebda5a3a495c944baba51a61fdba1814c7
-Author: Cory Benfield <lukasaoz@gmail.com>
-Date:   Tue Jul 19 13:00:31 2016 +0100
-
-    Clean up some bugs.
-
-via https://github.com/kennethreitz/requests
-
-t 99fa7becf263473c7bfc1998b41c2c6c80a0f499
-Author: Cory Benfield <lukasaoz@gmail.com>
-Date:   Tue Nov 15 10:09:11 2016 +0000
-
-    Update urllib3 to 1.19
-
---- requests/__init__.py.orig	2017-05-27 23:15:49.000000000 +0000
+--- requests/__init__.py.orig	2018-06-12 14:39:23.000000000 +0000
 +++ requests/__init__.py
-@@ -76,7 +76,7 @@ except AssertionError:
+@@ -60,15 +60,12 @@ def check_compatibility(urllib3_version,
+     # urllib3 >= 1.21.1, <= 1.23
+     assert major == 1
+     assert minor >= 21
+-    assert minor <= 23
+ 
+     # Check chardet for compatibility.
+     major, minor, patch = chardet_version.split('.')[:3]
+     major, minor, patch = int(major), int(minor), int(patch)
+     # chardet >= 3.0.2, < 3.1.0
+     assert major == 3
+-    assert minor < 1
+-    assert patch >= 2
+ 
+ 
+ def _check_cryptography(cryptography_version):
+@@ -93,7 +90,6 @@ except (AssertionError, ValueError):
  # Attempt to enable urllib3's SNI support, if possible
  try:
      from urllib3.contrib import pyopenssl
 -    pyopenssl.inject_into_urllib3()
-+    #pyopenssl.inject_into_urllib3()
- except ImportError:
-     pass
  
+     # Check cryptography version
+     from cryptography import __version__ as cryptography_version
