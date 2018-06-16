755a76b454c76816e0fdf96318a8a482fdbce3c1
leot <leot@pkgsrc.org>
2018-06-16 14:43:21 +0000

    mpg321: Do not unlock unititialized main_lock
    Bump PKGREVISION
    
    Problem spotted by <maya>, fixes PR pkg/53369 by <sevan>.

--- audio/mpg321/patches/patch-mpg321.c
+++ audio/mpg321/patches/patch-mpg321.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-mpg321.c,v 1.1 2016/06/02 09:18:19 jperkin Exp $
+$NetBSD: patch-mpg321.c,v 1.2 2018/06/16 14:43:21 leot Exp $
 
-Ensure structs are zero'd before use.
+- Ensure structs are zero'd before use.
+- Do not unlock uninitialized main_lock
 
 --- mpg321.c.orig	2012-03-25 12:27:49.000000000 +0000
 +++ mpg321.c
@@ -12,3 +13,12 @@ Ensure structs are zero'd before use.
      playbuf.pl = pl = new_playlist();
  
      if (!pl)
+@@ -750,8 +751,6 @@ int main(int argc, char *argv[])
+ 	    	    if (tcgetattr(0, &terminal_settings) < 0)
+ 	    		    perror("tcgetattr()");
+ 	    	    memcpy(&old_terminal_settings, &terminal_settings, sizeof(struct termios));
+-		    /* Early thread start */
+-		    sem_post(&main_lock);
+ 	    }
+     }
+     /* Play the mpeg files or zip it! */
