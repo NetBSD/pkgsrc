269e3ca9d9ca7ed20ac5b42dcf4563ca7d6bbd7b
wiz <wiz@pkgsrc.org>
2018-06-17 14:37:00 +0000

    *: bump PKGREVISION for poppler 0.65.0 shlib version bump

--- print/poppler-glib/buildlink3.mk
+++ print/poppler-glib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.62 2018/04/28 20:19:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.63 2018/06/17 14:37:00 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-glib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-glib
 POPPLER_GLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-glib+=	poppler-glib>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler-glib+=	poppler-glib>=0.64.0
+BUILDLINK_ABI_DEPENDS.poppler-glib+=	poppler-glib>=0.65.0
 BUILDLINK_PKGSRCDIR.poppler-glib?=	../../print/poppler-glib
 
 .include "../../devel/glib2/buildlink3.mk"
