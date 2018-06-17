8660ab3e6e534c01c0597967dc72033579df7d3e
wiz <wiz@pkgsrc.org>
2018-06-17 14:37:00 +0000

    *: bump PKGREVISION for poppler 0.65.0 shlib version bump

--- print/poppler-data/buildlink3.mk
+++ print/poppler-data/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/04/29 21:22:55 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/06/17 14:37:00 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-data
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	poppler-data
 POPPLER_DATA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-data+=	poppler-data>=0.4.8
+BUILDLINK_ABI_DEPENDS.poppler-data?=	poppler-data>=0.4.9
 BUILDLINK_PKGSRCDIR.poppler-data?=	../../print/poppler-data
 .endif	# POPPLER_DATA_BUILDLINK3_MK
 
--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.62 2018/04/28 20:19:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.63 2018/06/17 14:37:00 wiz Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.64.0
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.65.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 .include "../../graphics/lcms2/buildlink3.mk"
