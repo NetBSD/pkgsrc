0b4e54293c3012ebefbf52467e63314d1d92fce0
schmonz <schmonz@pkgsrc.org>
2018-06-18 11:00:21 +0000

    Update to 0.99d. From the changelog:
    - Added `correct` pid display in error log.
    - Fixed cosmetic bug in sslserver displaying parent and not child pid in log.
      Tx Bruce Guenter.
    
    pkgsrc changes:
    
    - Adjust path to OpenSSL certs
    - Install the provided example DH params
    - Look for /etc/dnsrewrite under PKG_SYSCONFBASE
    - Create a user and group for privilege separation
    - Install manpages and more documentation

--- net/ucspi-ssl/options.mk
+++ net/ucspi-ssl/options.mk
@@ -1,16 +1,20 @@
-# $NetBSD: options.mk,v 1.4 2017/05/20 01:39:12 schmonz Exp $
+# $NetBSD: options.mk,v 1.5 2018/06/18 11:00:21 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ucspi-ssl
 PKG_SUPPORTED_OPTIONS+=	perl
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=		perl
 .if !empty(PKG_OPTIONS:Mperl)
 .include "../../lang/perl5/buildlink3.mk"
 USE_TOOLS+=		perl:run
-SSL_PROGRAMS+=		sslperl
-PLIST_SRC+=		${PKGDIR}/PLIST.perl
 DJB_CONFIG_CMDS+=	${ECHO} ${PERL5} > conf-perl;
+PLIST.perl=		yes
+do-install-sslperl:
+	${INSTALL_PROGRAM} ${WRKSRC}/command/sslperl ${DESTDIR}${PREFIX}/bin
 .else
 DJB_BUILD_ARGS+=	- sslperl
+do-install-sslperl:
+	${DO_NADA}
 .endif
