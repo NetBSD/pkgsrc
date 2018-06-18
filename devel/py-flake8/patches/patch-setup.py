c347d68774b1825f8f6dbd99aa0802dde3958a8c
adam <adam@pkgsrc.org>
2018-06-18 11:17:54 +0000

    py-flake8: fix for py-flakes 2.0.0; revbump

--- devel/py-flake8/patches/patch-setup.py
+++ devel/py-flake8/patches/patch-setup.py
@@ -1,17 +1,20 @@
-$NetBSD: patch-setup.py,v 1.3 2018/05/14 07:39:55 leot Exp $
+$NetBSD: patch-setup.py,v 1.4 2018/06/18 11:17:54 adam Exp $
 
+Allow newer py-flakes.
 Add support for pycodestyle-2.4.0.
 
 From upstream commits 0273ca56, 85c503de, 68782675, e61d9d24, ef1d5cee.
 
 --- setup.py.orig	2017-08-08 00:31:43.000000000 +0000
 +++ setup.py
-@@ -22,7 +22,7 @@ requires = [
+@@ -21,8 +21,8 @@ requires = [
+     # http://flake8.pycqa.org/en/latest/faq.html#why-does-flake8-use-ranges-for-its-dependencies
      # And in which releases we will update those ranges here:
      # http://flake8.pycqa.org/en/latest/internal/releases.html#releasing-flake8
-     "pyflakes >= 1.5.0, < 1.7.0",
+-    "pyflakes >= 1.5.0, < 1.7.0",
 -    "pycodestyle >= 2.0.0, < 2.4.0",
-+    "pycodestyle >= 2.4.0, < 2.5.0",
++    "pyflakes >= 1.5.0",
++    "pycodestyle >= 2.4.0",
      "mccabe >= 0.6.0, < 0.7.0",
      "setuptools >= 30",
  ]
