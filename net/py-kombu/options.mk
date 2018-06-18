79305988d98e464c25f39f2e8fc91771542c1cae
adam <adam@pkgsrc.org>
2018-06-18 07:11:11 +0000

    py-kombu: updated to 4.2.1
    4.2.1:
    The 4.2.0 release contained remains of the async module by accident.
    This is now fixed.
    - Handle librabbitmq fileno raising a ValueError when socket is not connected.
    
    4.2.0:
    - Now passing max_retries, interval_start, interval_step,
      interval_max parameters from broker transport_options to
      :meth:~kombu.Connection.ensure_connection when returning
      :meth:~kombu.Connection.default_connection
    - Qpid: messages are now durable by default
    - Kombu now requires version 2.10.4 or greater of the redis library,
      in line with Celery
    - Fixed ImportError in some environments with outdated simplejson
    - MongoDB: fixed failure on MongoDB versions with an "-rc" tag
    - Ensure periodic polling frequency does not exceed timeout in
      virtual transport
    - Fixed string handling when using python-future module
    - Replaced "async" with "asynchronous" in preparation for Python 3.7
    - Allow removing pool size limit when in use

--- net/py-kombu/options.mk
+++ net/py-kombu/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2014/06/09 00:51:47 rodent Exp $
+# $NetBSD: options.mk,v 1.3 2018/06/18 07:11:11 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-kombu
 PKG_SUPPORTED_OPTIONS=	beanstalk couchdb mongo msgpack pyro redis slmq
@@ -10,54 +10,55 @@ PKG_SUGGESTED_OPTIONS+=	# blank
 .include "../../lang/python/pyversion.mk"
 
 .if !empty(PKG_OPTIONS:Mbeanstalk)
-DEPENDS+=		${PYPKGPREFIX}-beanstalkc-[0-9]*:../../net/py-beanstalkc
+DEPENDS+=	${PYPKGPREFIX}-beanstalkc-[0-9]*:../../net/py-beanstalkc
 .endif
 
 .if !empty(PKG_OPTIONS:Mcouchdb)
-DEPENDS+=		${PYPKGPREFIX}-couchdb-[0-9]*:../../databases/py-couchdb
+DEPENDS+=	${PYPKGPREFIX}-couchdb-[0-9]*:../../databases/py-couchdb
 .endif
 
 # XXX: Not packaged yet. Needs some work.
 #.if !empty(PKG_OPTIONS:Mlibrabbitmq)
-#DEPENDS+=		${PYPKGPREFIX}-librabbitmq>=1.5.2:../../net/py-librabbitmq
+#DEPENDS+=	${PYPKGPREFIX}-librabbitmq>=1.5.2:../../net/py-librabbitmq
 #.endif
 
 .if !empty(PKG_OPTIONS:Mmongo)
-DEPENDS+=		${PYPKGPREFIX}-mongo>=2.6.2:../../databases/py-mongo
+DEPENDS+=	${PYPKGPREFIX}-mongo>=2.6.2:../../databases/py-mongo
 .endif
 
 .if !empty(PKG_OPTIONS:Mmsgpack)
-DEPENDS+=		${PYPKGPREFIX}-msgpack>=0.3.0:../../devel/py-msgpack
+DEPENDS+=	${PYPKGPREFIX}-msgpack>=0.3.0:../../devel/py-msgpack
 .endif
 
 .if !empty(PKG_OPTIONS:Mpyro)
-DEPENDS+=		${PYPKGPREFIX}-Pyro4-[0-9]*:../../devel/py-Pyro
+DEPENDS+=	${PYPKGPREFIX}-Pyro4-[0-9]*:../../devel/py-Pyro
 .endif
 
 .if !empty(PKG_OPTIONS:Mredis)
-DEPENDS+=		redis>2.8.0:../../databases/redis
+DEPENDS+=	${PYPKGPREFIX}-redis>=2.10.5:../../databases/py-redis
 .endif
 
 .if !empty(PKG_OPTIONS:Mslmq)
-DEPENDS+=		${PYPKGPREFIX}-softlayer_messaging>=1.0.3:../../net/py-softlayer_messaging
+DEPENDS+=	${PYPKGPREFIX}-softlayer_messaging>=1.0.3:../../net/py-softlayer_messaging
 .endif
 
 .if !empty(PKG_OPTIONS:Msqlalchemy)
-DEPENDS+=		${PYPKGPREFIX}-sqlalchemy-[0-9]*:../../databases/py-sqlalchemy
+DEPENDS+=	${PYPKGPREFIX}-sqlalchemy-[0-9]*:../../databases/py-sqlalchemy
 .endif
 
 .if !empty(PKG_OPTIONS:Msqs)
-DEPENDS+=		${PYPKGPREFIX}-boto>=2.13.3:../../net/py-boto
+DEPENDS+=	${PYPKGPREFIX}-boto3>=1.4.4:../../net/py-boto3
+DEPENDS+=	${PYPKGPREFIX}-curl-[0-9]*:../../www/py-curl
 .endif
 
 .if !empty(PKG_OPTIONS:Myaml)
-DEPENDS+=		${PYPKGPREFIX}-yaml>=3.10:../../textproc/py-yaml
+DEPENDS+=	${PYPKGPREFIX}-yaml>=3.10:../../textproc/py-yaml
 .endif
 
 .if !empty(PKG_OPTIONS:Mzeromq)
-DEPENDS+=		${PYPKGPREFIX}-zmq>=13.1.0:../../net/py-zmq
+DEPENDS+=	${PYPKGPREFIX}-zmq>=13.1.0:../../net/py-zmq
 .endif
 
 .if !empty(PKG_OPTIONS:Mzookeeper)
-DEPENDS+=		${PYPKGPREFIX}-kazoo>=1.3.1:../../sysutils/py-kazoo
+DEPENDS+=	${PYPKGPREFIX}-kazoo>=1.3.1:../../sysutils/py-kazoo
 .endif
