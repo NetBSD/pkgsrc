f9a1a86bd4caf427c2c4b4aef2528b5a37f0805b
adam <adam@pkgsrc.org>
2018-06-19 07:21:23 +0000

    qt5: updated to 5.11.0
    Qt 5.11 comes with quite a few new features as well as many bug fixes to existing functionality.

--- x11/qt5-qtbase/patches/patch-src_corelib_global_qconfig-bootstrapped.h
+++ x11/qt5-qtbase/patches/patch-src_corelib_global_qconfig-bootstrapped.h
@@ -1,13 +1,13 @@
-$NetBSD: patch-src_corelib_global_qconfig-bootstrapped.h,v 1.1 2018/01/31 12:36:57 jperkin Exp $
+$NetBSD: patch-src_corelib_global_qconfig-bootstrapped.h,v 1.2 2018/06/19 07:21:23 adam Exp $
 
 SunOS does not have getauxval.
 
---- src/corelib/global/qconfig-bootstrapped.h.orig	2017-11-30 13:49:46.000000000 +0000
+--- src/corelib/global/qconfig-bootstrapped.h.orig	2018-05-14 04:46:28.000000000 +0000
 +++ src/corelib/global/qconfig-bootstrapped.h
-@@ -78,7 +78,11 @@
- #define QT_FEATURE_cxx11_random (QT_HAS_INCLUDE(<random>) ? 1 : -1)
+@@ -79,7 +79,11 @@
  #define QT_NO_DATASTREAM
  #define QT_FEATURE_datetimeparser -1
+ #define QT_FEATURE_etw -1
 +#ifdef __sun
 +# define QT_FEATURE_getauxval -1
 +#else
