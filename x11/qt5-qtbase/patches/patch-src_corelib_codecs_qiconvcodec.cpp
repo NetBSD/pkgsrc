dd90c05a7517e3333d74996e73885380b248b89e
adam <adam@pkgsrc.org>
2018-06-19 07:21:23 +0000

    qt5: updated to 5.11.0
    Qt 5.11 comes with quite a few new features as well as many bug fixes to existing functionality.

--- x11/qt5-qtbase/patches/patch-src_corelib_codecs_qiconvcodec.cpp
+++ x11/qt5-qtbase/patches/patch-src_corelib_codecs_qiconvcodec.cpp
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_corelib_codecs_qiconvcodec.cpp,v 1.2 2018/01/17 19:30:47 markd Exp $
+$NetBSD: patch-src_corelib_codecs_qiconvcodec.cpp,v 1.3 2018/06/19 07:21:23 adam Exp $
 
-* Add DragonFly support
-* Add NetBSD support
+Add DragonFly support
+Add NetBSD support
 
---- src/corelib/codecs/qiconvcodec.cpp.orig	2017-01-18 14:20:58.000000000 +0000
+--- src/corelib/codecs/qiconvcodec.cpp.orig	2018-05-14 04:46:28.000000000 +0000
 +++ src/corelib/codecs/qiconvcodec.cpp
-@@ -52,9 +52,14 @@ QT_REQUIRE_CONFIG(iconv);
+@@ -51,9 +51,14 @@ QT_REQUIRE_CONFIG(iconv);
  #include <stdio.h>
  #include <dlfcn.h>
  
@@ -16,12 +16,12 @@ $NetBSD: patch-src_corelib_codecs_qiconvcodec.cpp,v 1.2 2018/01/17 19:30:47 mark
 +
  // unistd.h is needed for the _XOPEN_UNIX macro
  #include <unistd.h>
--#if defined(_XOPEN_UNIX) && !defined(Q_OS_QNX) && !defined(Q_OS_OSF)
-+#if defined(_XOPEN_UNIX) && !defined(Q_OS_QNX) && !defined(Q_OS_OSF) || defined(Q_OS_NETBSD) || defined(__DragonFly__)
+-#if defined(_XOPEN_UNIX) && !defined(Q_OS_QNX)
++#if defined(_XOPEN_UNIX) && !defined(Q_OS_QNX) || defined(Q_OS_NETBSD) || defined(__DragonFly__)
  #  include <langinfo.h>
  #endif
  
-@@ -71,6 +76,9 @@ QT_REQUIRE_CONFIG(iconv);
+@@ -70,6 +75,9 @@ QT_REQUIRE_CONFIG(iconv);
  #  else
  #    define UTF16 "UTF-16LE"
  #  endif
@@ -31,18 +31,12 @@ $NetBSD: patch-src_corelib_codecs_qiconvcodec.cpp,v 1.2 2018/01/17 19:30:47 mark
  #else
  #  define UTF16 "UTF-16"
  #endif
-@@ -444,11 +452,13 @@ iconv_t QIconvCodec::createIconv_t(const
- #endif
-     const char *codeset = empty_codeset;
-     cd = iconv_open(to ? to : codeset, from ? from : codeset);
-+#elif defined(__NetBSD)
-+    const char *codeset = 0;
- #else
+@@ -447,7 +455,7 @@ iconv_t QIconvCodec::createIconv_t(const
      char *codeset = 0;
  #endif
  
--#if defined(_XOPEN_UNIX) && !defined(Q_OS_QNX) && !defined(Q_OS_OSF)
-+#if (defined(_XOPEN_UNIX) && !defined(Q_OS_QNX) && !defined(Q_OS_OSF)) || defined(Q_OS_NETBSD) || defined(__DragonFly__)
+-#if defined(_XOPEN_UNIX) && !defined(Q_OS_QNX)
++#if defined(_XOPEN_UNIX) && !defined(Q_OS_QNX) || defined(Q_OS_NETBSD) || defined(__DragonFly__)
      if (cd == (iconv_t) -1) {
          codeset = nl_langinfo(CODESET);
          if (codeset)
