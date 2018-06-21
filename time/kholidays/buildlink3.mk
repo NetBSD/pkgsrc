9958da0cdbb9bd585995ab52f8d2f9ad3d71590e
markd <markd@pkgsrc.org>
2018-06-21 08:15:59 +0000

    kholidays: Add version 5.47.0
    This library provides a C++ API that determines holiday and other
    special events for a geographical region.

---
+++ time/kholidays/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2018/06/21 08:15:59 markd Exp $
+
+BUILDLINK_TREE+=	kholidays
+
+.if !defined(KHOLIDAYS_BUILDLINK3_MK)
+KHOLIDAYS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kholidays+=	kholidays>=5.44.0
+BUILDLINK_PKGSRCDIR.kholidays?=	../../time/kholidays
+
+.include "../../x11/qt5-qtdeclarative/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KHOLIDAYS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kholidays
