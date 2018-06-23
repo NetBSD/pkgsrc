364e828d305722fb916220d960311919f069ceba
youri <youri@pkgsrc.org>
2018-06-23 20:27:38 +0000

    Update thunar to 1.8.1:
    - drop gtk2 backend and libxfce4util dependency
    - completely revised taskbar
    - (optional) gobject-introspection support for thunarx
    - other minor improvements...
    
    https://andreldm.com/2018/06/06/thunar-1.8.0-release.html

--- sysutils/xfce4-thunar/buildlink3.mk
+++ sysutils/xfce4-thunar/buildlink3.mk
@@ -1,18 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.42 2018/05/15 22:25:31 youri Exp $
+# $NetBSD: buildlink3.mk,v 1.43 2018/06/23 20:27:38 youri Exp $
 
 BUILDLINK_TREE+=	xfce4-thunar
 
 .if !defined(XFCE4_THUNAR_BUILDLINK3_MK)
 XFCE4_THUNAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-thunar+=	xfce4-thunar>=1.6.6
-BUILDLINK_ABI_DEPENDS.xfce4-thunar+=	xfce4-thunar>=1.6.11nb3
+BUILDLINK_API_DEPENDS.xfce4-thunar+=	xfce4-thunar>=1.8.0
+BUILDLINK_ABI_DEPENDS.xfce4-thunar+=	xfce4-thunar>=1.8.0
 BUILDLINK_PKGSRCDIR.xfce4-thunar?=	../../sysutils/xfce4-thunar
 
 .include "../../graphics/libexif/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .include "../../sysutils/desktop-file-utils/buildlink3.mk"
-.include "../../x11/libxfce4util/buildlink3.mk"
 .include "../../x11/xfce4-exo/buildlink3.mk"
 .include "../../x11/xfce4-panel/buildlink3.mk"
 .endif	# XFCE4_THUNAR_BUILDLINK3_MK
