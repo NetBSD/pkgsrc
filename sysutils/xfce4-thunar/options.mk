2b1f85954e21524a42d0c57f5edaf95a77acc96c
youri <youri@pkgsrc.org>
2018-06-23 20:27:38 +0000

    Update thunar to 1.8.1:
    - drop gtk2 backend and libxfce4util dependency
    - completely revised taskbar
    - (optional) gobject-introspection support for thunarx
    - other minor improvements...
    
    https://andreldm.com/2018/06/06/thunar-1.8.0-release.html

--- sysutils/xfce4-thunar/options.mk
+++ sysutils/xfce4-thunar/options.mk
@@ -1,12 +1,20 @@
-# $NetBSD: options.mk,v 1.4 2017/06/18 21:40:45 youri Exp $
+# $NetBSD: options.mk,v 1.5 2018/06/23 20:27:38 youri Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xfce4-thunar
-PKG_SUPPORTED_OPTIONS=	gvfs
-PKG_SUGGESTED_OPTIONS=	gvfs
+PKG_SUPPORTED_OPTIONS=	gvfs introspection
+PKG_SUGGESTED_OPTIONS=	gvfs introspection
+
+PLIST_VARS+=	introspection
 
 .include "../../mk/bsd.options.mk"
 
 # for trash support, and remote filesystems
 .if !empty(PKG_OPTIONS:Mgvfs)
 DEPENDS+=	gvfs-[0-9]*:../../sysutils/gvfs
+.endif
+
+# for GObject introspection support in thunarx
+.if !empty(PKG_OPTIONS:Mintrospection)
+PLIST.introspection=	yes
+.include "../../devel/gobject-introspection/buildlink3.mk"
 .endif
