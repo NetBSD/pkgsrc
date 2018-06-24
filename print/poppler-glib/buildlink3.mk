1fac9d61bbf4cb8cde17eee5195268bc1ee3ce8b
wiz <wiz@pkgsrc.org>
2018-06-24 11:16:10 +0000

    *: recursive bump for poppler-0.66.0 update

--- print/poppler-glib/buildlink3.mk
+++ print/poppler-glib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.63 2018/06/17 14:37:00 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.64 2018/06/24 11:16:10 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-glib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-glib
 POPPLER_GLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-glib+=	poppler-glib>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler-glib+=	poppler-glib>=0.65.0
+BUILDLINK_ABI_DEPENDS.poppler-glib+=	poppler-glib>=0.66.0
 BUILDLINK_PKGSRCDIR.poppler-glib?=	../../print/poppler-glib
 
 .include "../../devel/glib2/buildlink3.mk"
--- print/poppler-includes/buildlink3.mk
+++ print/poppler-includes/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.33 2018/06/17 14:37:00 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2018/06/24 11:16:10 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-includes
 
@@ -8,7 +8,7 @@ POPPLER_INCLUDES_BUILDLINK3_MK:=
 BUILDLINK_DEPMETHOD.poppler-includes?=	build
 
 BUILDLINK_API_DEPENDS.poppler-includes+=	poppler-includes>=0.22.2
-BUILDLINK_ABI_DEPENDS.poppler-includes?=	poppler-includes>=0.65.0
+BUILDLINK_ABI_DEPENDS.poppler-includes?=	poppler-includes>=0.66.0
 BUILDLINK_PKGSRCDIR.poppler-includes?=		../../print/poppler-includes
 .endif # POPPLER_INCLUDES_BUILDLINK3_MK
 
