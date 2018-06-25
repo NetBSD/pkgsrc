eb553afd9f56203e752629d6f7e065800175b9ff
bouyer <bouyer@pkgsrc.org>
2018-06-25 14:01:45 +0000

    update gputils to 1.5.0. Changes since 1.4.2:
        Several pre-defined constant and variable in the gpasm.
        Extension of error and messg directives: These directives - inside of the parameter string - recognize and execute the #v() macro.
        New directives: __badrom, assume, elif, elifdef, elifndef
        New gplink feature: Remove the unnecessary banksel and pagesel directives.
        The errorlevel directive accept number ranges. E.g. +303-310 or -303-310
        The gpvo utility use the disassembler to decode the program memory sections.
        The gplink save the local RAM symbols to COD file.
        The ".direct" directive - inside of the parameter string - recognize and execute the #v() macro.
        The length of source file names in COD file, now 256 bytes long instead of 64 bytes. The gpvc utility thereafter also able to correctly detect the shorter names.
        The inc and lkr files are synced with MPLABX 3.35

--- devel/gputils/patches/patch-libgputils_gpreadobj.c
+++ devel/gputils/patches/patch-libgputils_gpreadobj.c
@@ -1,23 +1,22 @@
-$NetBSD: patch-libgputils_gpreadobj.c,v 1.1 2016/09/14 14:10:50 dholland Exp $
+$NetBSD: patch-libgputils_gpreadobj.c,v 1.2 2018/06/25 14:01:45 bouyer Exp $
 
 Be more paranoid about input, pursuant to a build failure in sdcc3
 that seems to involve gplib trying to allocate gigs of memory.
 
---- libgputils/gpreadobj.c~	2015-11-23 18:17:01.000000000 +0000
-+++ libgputils/gpreadobj.c
-@@ -109,6 +109,15 @@ gp_read_file(const char *filename)
+--- ./libgputils/gpreadobj.c.orig	2016-08-06 11:44:18.000000000 +0200
++++ ./libgputils/gpreadobj.c	2018-06-25 14:41:31.504470855 +0200
+@@ -712,6 +712,14 @@
+   /* determine the size of the file */
    fstat(fileno(infile), &statbuf);
    file->size = statbuf.st_size;
- 
 +  /* just in case */
 +  if (file->size < 0) {
-+    gp_error("File \"%s\" size is negative.", filename);
++    gp_error("File \"%s\" size is negative.", File_name);
 +  }
 +  if (file->size > 100*1024*1024) {
-+    gp_error("File \"%s\" size %ld is unreasonably large.", filename,
-+	     file->size);
++    gp_error("File \"%s\" size %ld is unreasonably large.", File_name,
++            file->size);
 +  }
-+
+ 
    /* read the object file into memory */
-   file->file = (unsigned char *)GP_Malloc(file->size);
-   n = fread(file->file, 1, file->size, infile);
+   file->file = (uint8_t *)GP_Malloc(file->size);
