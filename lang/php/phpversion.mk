425e99da6dd7561b2ce3cf16dcbc22ede9823e74
taca <taca@pkgsrc.org>
2018-06-25 15:19:22 +0000

    lang/php71: update to 7.1.19
    22 Jun 2018, PHP 7.1.19
    
    - CLI Server:
      . Fixed bug #76333 (PHP built-in server does not find files if root path
        contains special characters). (Anatol)
    
    - OpenSSL:
      . Fixed bug #76296 (openssl_pkey_get_public does not respect open_basedir).
        (Erik Lax, Jakub Zelenka)
      . Fixed bug #76174 (openssl extension fails to build with LibreSSL 2.7).
        (Jakub Zelenka)
    
    - SPL:
      . Fixed bug #76367 (NoRewindIterator segfault 11). (Laruence)
    
    - Standard:
      . Fixed bug #76335 ("link(): Bad file descriptor" with non-ASCII path).
        (Anatol)
      . Fixed bug #76383 (array_map on $GLOBALS returns IS_INDIRECT). (Bob)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.220 2018/06/24 10:34:47 taca Exp $
+# $NetBSD: phpversion.mk,v 1.221 2018/06/25 15:19:22 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.36
 PHP70_VERSION=	7.0.30
-PHP71_VERSION=	7.1.18
+PHP71_VERSION=	7.1.19
 PHP72_VERSION=	7.2.7
 
 # Define initial release of major version.
