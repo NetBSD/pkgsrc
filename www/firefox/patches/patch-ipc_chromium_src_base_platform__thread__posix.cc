56a8f3ad14fdfefb538096194cd9385234008d2c
jperkin <jperkin@pkgsrc.org>
2018-06-26 09:37:09 +0000

    firefox: Add SunOS support.
    With stock pkgsrc this still fails to build due to lang/clang, but using
    joyent/clang6 produces a working build that appears to function well.

--- www/firefox/patches/patch-ipc_chromium_src_base_platform__thread__posix.cc
+++ www/firefox/patches/patch-ipc_chromium_src_base_platform__thread__posix.cc
@@ -1,9 +1,9 @@
-$NetBSD: patch-ipc_chromium_src_base_platform__thread__posix.cc,v 1.9 2017/08/10 14:46:15 ryoon Exp $
+$NetBSD: patch-ipc_chromium_src_base_platform__thread__posix.cc,v 1.10 2018/06/26 09:37:10 jperkin Exp $
 
 * Support NetBSD
-* Support Solaris
+* Support Solaris (we can't rely on pthread_setname_np so ignore it).
 
---- ipc/chromium/src/base/platform_thread_posix.cc.orig	2017-07-31 16:20:47.000000000 +0000
+--- ipc/chromium/src/base/platform_thread_posix.cc.orig	2018-06-05 19:47:29.000000000 +0000
 +++ ipc/chromium/src/base/platform_thread_posix.cc
 @@ -12,7 +12,9 @@
  #if defined(OS_MACOSX)
@@ -15,3 +15,12 @@ $NetBSD: patch-ipc_chromium_src_base_platform__thread__posix.cc,v 1.9 2017/08/10
  #elif defined(OS_LINUX)
  #include <sys/syscall.h>
  #include <sys/prctl.h>
+@@ -103,7 +105,7 @@ void PlatformThread::SetName(const char*
+   pthread_setname_np(pthread_self(), "%s", (void *)name);
+ #elif defined(OS_BSD) && !defined(__GLIBC__)
+   pthread_set_name_np(pthread_self(), name);
+-#elif defined(OS_SOLARIS)
++#elif defined(notOS_SOLARIS)
+   pthread_setname_np(pthread_self(), name);
+ #else
+ #endif
