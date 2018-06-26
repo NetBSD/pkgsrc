bd1a04ad81b7f8cbfa1426cd7f11e78e14397dca
jperkin <jperkin@pkgsrc.org>
2018-06-26 09:37:09 +0000

    firefox: Add SunOS support.
    With stock pkgsrc this still fails to build due to lang/clang, but using
    joyent/clang6 produces a working build that appears to function well.

---
+++ www/firefox/patches/patch-js_src_util_NativeStack.cpp
@@ -0,0 +1,23 @@
+$NetBSD: patch-js_src_util_NativeStack.cpp,v 1.1 2018/06/26 09:37:10 jperkin Exp $
+
+Support SunOS.
+
+--- js/src/util/NativeStack.cpp.orig	2018-06-05 19:47:32.000000000 +0000
++++ js/src/util/NativeStack.cpp
+@@ -32,7 +32,7 @@ js::GetNativeStackBaseImpl()
+     return static_cast<void*>(pTib->StackBase);
+ }
+ 
+-#elif defined(SOLARIS)
++#elif defined(__sun)
+ 
+ #include <ucontext.h>
+ 
+@@ -78,6 +78,7 @@ js::GetNativeStackBaseImpl()
+ #  elif defined(PTHREAD_NP_H) || defined(_PTHREAD_NP_H_) || defined(NETBSD)
+     /* e.g. on FreeBSD 4.8 or newer, neundorf@kde.org */
+     pthread_attr_get_np(thread, &sattr);
++#  elif defined(__sun)
+ #  else
+     /*
+      * FIXME: this function is non-portable;
