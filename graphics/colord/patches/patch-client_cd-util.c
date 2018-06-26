d15420378760a213928b9d427b74b0a808a83f91
jperkin <jperkin@pkgsrc.org>
2018-06-26 10:16:20 +0000

    colord: Fix SunOS build.

--- graphics/colord/patches/patch-client_cd-util.c
+++ graphics/colord/patches/patch-client_cd-util.c
@@ -1,10 +1,21 @@
-$NetBSD: patch-client_cd-util.c,v 1.1 2018/06/13 15:49:40 youri Exp $
+$NetBSD: patch-client_cd-util.c,v 1.2 2018/06/26 10:16:20 jperkin Exp $
 
 Fix db path.
+SunOS requires sys/termios.h
 
 --- client/cd-util.c.orig	2016-07-27 13:49:36.000000000 +0000
 +++ client/cd-util.c
-@@ -814,7 +814,7 @@ cd_util_dump (CdUtilPrivate *priv, gchar
+@@ -27,6 +27,9 @@
+ #ifdef HAVE_PWD_H
+ #include <pwd.h>
+ #endif
++#ifdef __sun
++#include <sys/termios.h>
++#endif
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <sys/ioctl.h>
+@@ -814,7 +817,7 @@ cd_util_dump (CdUtilPrivate *priv, gchar
  {
  	CdDevice *device;
  	CdProfile *profile;
