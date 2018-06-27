78d4f95854fa1ab573bcaa3815c8d2e2b9227789
jaapb <jaapb@pkgsrc.org>
2018-06-27 19:29:43 +0000

    Updated package devel/ocaml-cppo to version 1.6.4.
    This fixes build with the upcoming OCaml 4.07 release, and also has some
    fixes in the tests.

--- devel/ocaml-cppo/buildlink3.mk
+++ devel/ocaml-cppo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/01/22 12:04:04 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/06/27 19:29:43 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cppo
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-cppo
 OCAML_CPPO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.3.1
-BUILDLINK_ABI_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.6.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.6.4
 BUILDLINK_PKGSRCDIR.ocaml-cppo?=	../../devel/ocaml-cppo
 .endif	# OCAML_CPPO_BUILDLINK3_MK
 
