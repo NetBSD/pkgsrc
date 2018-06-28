c774585d30a5ea38d75357718e9b9943f42a7de1
ryoon <ryoon@pkgsrc.org>
2018-06-28 13:52:37 +0000

    Update to 61.0
    Changelog:
    New
        Enhanced performance:
            Faster page rendering with Quantum CSS improvements and the new
              retained display list feature
            Faster switching between tabs on Windows and Linux
            WebExtensions now run in their own process on MacOS
    
        Convenient access to more search engines: You can now add search engines
          to the address bar "Search with" tool from the page action menu when on
          a webpage that provides an OpenSearch plugin
    
        Share links from Firefox for MacOS more easily: You can now share the URL
          of an active tab from the page actions menu in the address bar
    
        Improved security:
            On-by-default support for the latest draft of the TLS 1.3 specification
            Access to FTP subresources inside http(s) pages has been blocked
    
        A more consistent user experience: Improvements for dark theme support
          across the entire Firefox user interface
    
        More customization for tab management: added support to allow WebExtensions
          to hide tabs
    
        Improved bookmark syncing
    
    Fixed
        Various security fixes
    
    Changed
        The settings for customizing your homepage and new tab page in Firefox
          have been added to a new Preferences section that can be accessed from
          Firefox at about:preferences#home. The settings can also be accessed via
          the gear icon on the New Tab page.
    
    Security fixes:
    #CVE-2018-12359: Buffer overflow using computed size of canvas element
    #CVE-2018-12360: Use-after-free when using focus()
    #CVE-2018-12361: Integer overflow in SwizzleData
    #CVE-2018-12358: Same-origin bypass using service worker and redirection
    #CVE-2018-12362: Integer overflow in SSSE3 scaler
    #CVE-2018-5156: Media recorder segmentation fault when track type is changed during capture
    #CVE-2018-12363: Use-after-free when appending DOM nodes
    #CVE-2018-12364: CSRF attacks through 307 redirects and NPAPI plugins
    #CVE-2018-12365: Compromised IPC child process can list local filenames
    #CVE-2018-12371: Integer overflow in Skia library during edge builder allocation
    #CVE-2018-12366: Invalid data handling during QCMS transformations
    #CVE-2018-12367: Timing attack mitigation of PerformanceNavigationTiming
    #CVE-2018-12368: No warning when opening executable SettingContent-ms files
    #CVE-2018-12369: WebExtension security permission checks bypassed by embedded experiments
    #CVE-2018-12370: SameSite cookie protections bypassed when exiting Reader View
    #CVE-2018-5186: Memory safety bugs fixed in Firefox 61
    #CVE-2018-5187: Memory safety bugs fixed in Firefox 60 and Firefox ESR 60.1
    #CVE-2018-5188: Memory safety bugs fixed in Firefox 60, Firefox ESR 60.1, and Firefox ESR 52.9

--- www/firefox/patches/patch-browser_app_profile_firefox.js
+++ www/firefox/patches/patch-browser_app_profile_firefox.js
@@ -1,14 +1,17 @@
-$NetBSD: patch-browser_app_profile_firefox.js,v 1.7 2018/05/18 12:50:09 ryoon Exp $
+$NetBSD: patch-browser_app_profile_firefox.js,v 1.8 2018/06/28 13:52:37 ryoon Exp $
 
---- browser/app/profile/firefox.js.orig	2018-05-16 05:38:23.000000000 +0000
+--- browser/app/profile/firefox.js.orig	2018-06-21 20:03:42.000000000 +0000
 +++ browser/app/profile/firefox.js
-@@ -1747,3 +1747,9 @@ pref("app.shield.optoutstudies.enabled",
- #else
+@@ -1737,6 +1737,12 @@ pref("app.shield.optoutstudies.enabled",
  pref("app.shield.optoutstudies.enabled", false);
  #endif
-+
+ 
 +// Select UI locale from LANG/LC_MESSAGE environmental variables
 +pref("intl.locale.requested", "");
 +
 +// Disable multiprocess window support. Workaround for PR 53273.
 +pref("browser.tabs.remote.autostart", false);
++
+ // Savant Shield study preferences
+ pref("shield.savant.enabled", false);
+ pref("shield.savant.loglevel", "warn");
