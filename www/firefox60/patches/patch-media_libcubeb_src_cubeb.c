4f98c2df3d759b05ece1bb61923256c4da24408b
ryoon <ryoon@pkgsrc.org>
2018-06-28 14:04:10 +0000

    www/firefox60: import firefox60-60.1.0
    Mozilla Firefox is a free, open-source and cross-platform web browser
    for Windows, Linux, MacOS X and many other operating systems.
    
    It is fast and easy to use, and offers many advantages over other web
    browsers, such as tabbed browsing and the ability to block pop-up
    windows.
    
    Firefox also offers excellent bookmark and history management, and it
    can be extended by developers using industry standards such as XML,
    CSS, JavaScript, C++, etc. Many extensions are available.
    
    This package provides Firefox 60 ESR.
    
    Securty fixes:
    #CVE-2018-12359: Buffer overflow using computed size of canvas element
    #CVE-2018-12360: Use-after-free when using focus()
    #CVE-2018-12361: Integer overflow in SwizzleData
    #CVE-2018-12362: Integer overflow in SSSE3 scaler
    #CVE-2018-5156: Media recorder segmentation fault when track type is changed during capture
    #CVE-2018-12363: Use-after-free when appending DOM nodes
    #CVE-2018-12364: CSRF attacks through 307 redirects and NPAPI plugins
    #CVE-2018-12365: Compromised IPC child process can list local filenames
    #CVE-2018-12371: Integer overflow in Skia library during edge builder allocation
    #CVE-2018-12366: Invalid data handling during QCMS transformations
    #CVE-2018-12367: Timing attack mitigation of PerformanceNavigationTiming
    #CVE-2018-12368: No warning when opening executable SettingContent-ms files
    #CVE-2018-12369: WebExtension security permission checks bypassed by embedded experiments
    #CVE-2018-5187: Memory safety bugs fixed in Firefox 60 and Firefox ESR 60.1
    #CVE-2018-5188: Memory safety bugs fixed in Firefox 60, Firefox ESR 60.1, and Firefox ESR 52.9

---
+++ www/firefox60/patches/patch-media_libcubeb_src_cubeb.c
@@ -0,0 +1,37 @@
+$NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.1 2018/06/28 14:04:10 ryoon Exp $
+
+* Support OSS audio
+
+--- media/libcubeb/src/cubeb.c.orig	2017-07-31 16:20:49.000000000 +0000
++++ media/libcubeb/src/cubeb.c
+@@ -55,6 +55,9 @@ int audiotrack_init(cubeb ** context, ch
+ #if defined(USE_KAI)
+ int kai_init(cubeb ** context, char const * context_name);
+ #endif
++#if defined(USE_OSS)
++int oss_init(cubeb ** context, char const * context_name);
++#endif
+ 
+ static int
+ validate_stream_params(cubeb_stream_params * input_stream_params,
+@@ -155,6 +158,10 @@ cubeb_init(cubeb ** context, char const 
+ #if defined(USE_KAI)
+       init_oneshot = kai_init;
+ #endif
++    } else if (!strcmp(backend_name, "oss")) {
++#if defined(USE_OSS)
++      init_oneshot = oss_init;
++#endif
+     } else {
+       /* Already set */
+     }
+@@ -199,6 +206,9 @@ cubeb_init(cubeb ** context, char const 
+ #if defined(USE_KAI)
+     kai_init,
+ #endif
++#if defined(USE_OSS)
++    oss_init,
++#endif
+   };
+   int i;
+ 
