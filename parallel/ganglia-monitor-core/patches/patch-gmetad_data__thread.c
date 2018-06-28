fd8c0985dfacd32530fe2f3a20f66033783c3660
bacon <bacon@pkgsrc.org>
2018-06-28 01:05:01 +0000

    parallel/ganglia-monitor-core: Unbreak linux build, add Linux rc files
    Add MESSAGE.Linux with instructions for init and systemd.
    
    Take ownership from pkgsrc-users.

--- parallel/ganglia-monitor-core/patches/patch-gmetad_data__thread.c
+++ parallel/ganglia-monitor-core/patches/patch-gmetad_data__thread.c
@@ -1,14 +1,16 @@
-$NetBSD: patch-gmetad_data__thread.c,v 1.1 2016/02/16 12:48:18 fhajny Exp $
+$NetBSD: patch-gmetad_data__thread.c,v 1.2 2018/06/28 01:05:01 bacon Exp $
 
-Portability.
+# Portability
 
 --- gmetad/data_thread.c.orig	2015-07-01 19:49:35.000000000 +0000
 +++ gmetad/data_thread.c
-@@ -1,6 +1,7 @@
+@@ -1,6 +1,9 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <unistd.h>
++#ifndef __linux__
 +#include <sys/filio.h>
++#endif
  #include <sys/ioctl.h>
  #include <sys/poll.h>
  #include <sys/time.h>
