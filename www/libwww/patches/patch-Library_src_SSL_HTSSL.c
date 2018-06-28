3804ce3216dc32efd077993dbac9a44b8660a282
jperkin <jperkin@pkgsrc.org>
2018-06-28 15:45:46 +0000

    libwww: Unbreak OpenSSL 1.1 patch.  Fix SunOS MD5 section.

--- www/libwww/patches/patch-Library_src_SSL_HTSSL.c
+++ www/libwww/patches/patch-Library_src_SSL_HTSSL.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-Library_src_SSL_HTSSL.c,v 1.1 2018/06/24 16:54:59 adam Exp $
+$NetBSD: patch-Library_src_SSL_HTSSL.c,v 1.2 2018/06/28 15:45:46 jperkin Exp $
 
 Fix for OpenSSL 1.1.x.
 
---- Library/src/SSL/HTSSL.c.orig	2018-06-24 13:38:32.023842509 +0000
+--- Library/src/SSL/HTSSL.c.orig	2017-06-24 05:51:38.000000000 +0000
 +++ Library/src/SSL/HTSSL.c
 @@ -98,6 +98,7 @@ PRIVATE void apps_ssl_info_callback (SSL
  PRIVATE int verify_callback (int ok, X509_STORE_CTX * ctx)
@@ -12,7 +12,7 @@ Fix for OpenSSL 1.1.x.
      X509 * err_cert = X509_STORE_CTX_get_current_cert(ctx);
      int err = X509_STORE_CTX_get_error(ctx);
      int depth = X509_STORE_CTX_get_error_depth(ctx);
-@@ -115,24 +116,24 @@ PRIVATE int verify_callback (int ok, X50
+@@ -115,24 +116,28 @@ PRIVATE int verify_callback (int ok, X50
  	    verify_error=X509_V_ERR_CERT_CHAIN_TOO_LONG;
  	}
      }
@@ -20,8 +20,11 @@ Fix for OpenSSL 1.1.x.
 +    switch (ctx_error = X509_STORE_CTX_get_error(ctx)) {
  
      case X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT:
--	X509_NAME_oneline(X509_get_issuer_name(ctx->current_cert), buf, 256);
++#if OPENSSL_VERSION_NUMBER >= 0x10100000
 +	X509_NAME_oneline(X509_get_issuer_name(X509_STORE_CTX_get0_cert(ctx)), buf, 256);
++#else
+ 	X509_NAME_oneline(X509_get_issuer_name(ctx->current_cert), buf, 256);
++#endif
  	HTTRACE(PROT_TRACE, "issuer= %s\n" _ buf);
  	break;
  
@@ -41,7 +44,7 @@ Fix for OpenSSL 1.1.x.
  	HTTRACE(PROT_TRACE, "\n");
  	break;
      }
-@@ -214,12 +215,6 @@ PUBLIC BOOL HTSSL_init (void)
+@@ -214,12 +219,6 @@ PUBLIC BOOL HTSSL_init (void)
  
  	/* select the protocol method */
  	switch (ssl_prot_method) {
