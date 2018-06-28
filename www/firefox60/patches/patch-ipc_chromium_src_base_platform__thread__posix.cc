6eebd430cee4d707c1f3f84392bede07aa9640fd
ryoon <ryoon@pkgsrc.org>
2018-06-28 14:04:10 +0000

    www/firefox60: import firefox60-60.1.0
    Mozilla Firefox is a free, open-source and cross-platform web browser
    for Windows, Linux, MacOS X and many other operating systems.
    
    It is fast and easy to use, and offers many advantages over other web
    browsers, such as tabbed browsing and the ability to block pop-up
    windows.
    
    Firefox also offers excellent bookmark and history management, and it
    can be extended by developers using industry standards such as XML,
    CSS, JavaScript, C++, etc. Many extensions are available.
    
    This package provides Firefox 60 ESR.
    
    Securty fixes:
    #CVE-2018-12359: Buffer overflow using computed size of canvas element
    #CVE-2018-12360: Use-after-free when using focus()
    #CVE-2018-12361: Integer overflow in SwizzleData
    #CVE-2018-12362: Integer overflow in SSSE3 scaler
    #CVE-2018-5156: Media recorder segmentation fault when track type is changed during capture
    #CVE-2018-12363: Use-after-free when appending DOM nodes
    #CVE-2018-12364: CSRF attacks through 307 redirects and NPAPI plugins
    #CVE-2018-12365: Compromised IPC child process can list local filenames
    #CVE-2018-12371: Integer overflow in Skia library during edge builder allocation
    #CVE-2018-12366: Invalid data handling during QCMS transformations
    #CVE-2018-12367: Timing attack mitigation of PerformanceNavigationTiming
    #CVE-2018-12368: No warning when opening executable SettingContent-ms files
    #CVE-2018-12369: WebExtension security permission checks bypassed by embedded experiments
    #CVE-2018-5187: Memory safety bugs fixed in Firefox 60 and Firefox ESR 60.1
    #CVE-2018-5188: Memory safety bugs fixed in Firefox 60, Firefox ESR 60.1, and Firefox ESR 52.9

---
+++ www/firefox60/patches/patch-ipc_chromium_src_base_platform__thread__posix.cc
@@ -0,0 +1,26 @@
+$NetBSD: patch-ipc_chromium_src_base_platform__thread__posix.cc,v 1.1 2018/06/28 14:04:10 ryoon Exp $
+
+* Support NetBSD
+* Support Solaris (we can't rely on pthread_setname_np so ignore it).
+
+--- ipc/chromium/src/base/platform_thread_posix.cc.orig	2018-06-05 19:47:29.000000000 +0000
++++ ipc/chromium/src/base/platform_thread_posix.cc
+@@ -12,7 +12,9 @@
+ #if defined(OS_MACOSX)
+ #include <mach/mach.h>
+ #elif defined(OS_NETBSD)
++_Pragma("GCC visibility push(default)")
+ #include <lwp.h>
++_Pragma("GCC visibility pop")
+ #elif defined(OS_LINUX)
+ #include <sys/syscall.h>
+ #include <sys/prctl.h>
+@@ -103,7 +105,7 @@ void PlatformThread::SetName(const char*
+   pthread_setname_np(pthread_self(), "%s", (void *)name);
+ #elif defined(OS_BSD) && !defined(__GLIBC__)
+   pthread_set_name_np(pthread_self(), name);
+-#elif defined(OS_SOLARIS)
++#elif defined(notOS_SOLARIS)
+   pthread_setname_np(pthread_self(), name);
+ #else
+ #endif
