8240d91d5c03243fc9487856708febd16e917b3a
adam <adam@pkgsrc.org>
2018-07-03 07:44:00 +0000

    py-Pillow: updated to 5.2.0
    5.2.0:
    Fixed saving a multiframe image as a single frame PDF
    If a Qt version is already imported, attempt to use it first
    Fix transform fill color for alpha images
    TGA: Add support for writing RLE data
    TGA: Read and write LA data
    QuantOctree.c: Remove erroneous attempt to average over an empty range
    Changed ICNS format tests to pass on OS X 10.11
    Fixed bug in ImageDraw.multiline_textsize()
    Added getsize_multiline support for PIL.ImageFont
    Added ImageFile get_format_mimetype method
    Changed mmap file pointer to use context manager
    Changed ellipse point calculations to be more evenly distributed
    Only extract first Exif segment
    Tests: Test ImageDraw2, WalImageFile
    Remove unnecessary '#if 0' code
    Tests: Added GD tests
    Fix collections ABCs DeprecationWarning in Python 3.7
    unpack_from is faster than unpack of slice
    Docs: Add coordinate system links and file handling links in documentation
    Tests: TestFilePng: Fix test_save_l_transparency()
    Docs: Correct argument name
    Docs: Update CMake download URL
    Docs: Improve Image.transform documentation
    Fix transform fillcolor argument when image mode is RGBA or LA
    Tests: More specific Exception testing
    Add getrgb HSB/HSV color strings
    Allow float values in getrgb HSL color string
    AppVeyor: Upgrade to Python 2.7.15 and 3.4.4
    AppVeyor: Upgrade to PyPy 6.0.0
    Deprecate PILLOW_VERSION and VERSION
    Support Python 3.7
    Depends: Update freetype to 2.9.1, libjpeg to 9c, libwebp to 1.0.0
    Build macOS wheels with Xcode 6.4, supporting older macOS versions
    Fix _i2f compilation on some GCC versions
    Changed encoderinfo to have priority over info when saving GIF images
    Rename PIL.version to PIL._version and remove it from module
    Enable background colour parameter on rotate
    Remove unnecessary #if 1 directive
    Remove unused Python class, Path
    Fix dereferencing type-punned pointer will break strict-aliasing

--- graphics/py-Pillow/patches/patch-setup.py
+++ graphics/py-Pillow/patches/patch-setup.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-setup.py,v 1.15 2018/03/31 16:22:11 adam Exp $
+$NetBSD: patch-setup.py,v 1.16 2018/07/03 07:44:00 adam Exp $
 
-Prevent to detect optional tkinter.
 Disable mp_compile hack; it has problems with native parallel building.
+Prevent to detect optional tkinter.
 
---- setup.py.orig	2018-01-01 21:04:32.000000000 +0000
+--- setup.py.orig	2018-07-01 19:02:58.000000000 +0000
 +++ setup.py
 @@ -23,7 +23,6 @@ from setuptools import Extension, setup
  
@@ -12,8 +12,8 @@ Disable mp_compile hack; it has problems with native parallel building.
 -import mp_compile
  
  
- if sys.platform == "win32" and sys.version_info >= (3, 7):
-@@ -128,11 +128,7 @@ def get_version():
+ if sys.platform == "win32" and sys.version_info >= (3, 8):
+@@ -128,11 +127,7 @@ def get_version():
      return locals()['__version__']
  
  
