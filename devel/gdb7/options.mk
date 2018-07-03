b4c4cc2e4aa4e9e53d4af662bf41facc4bf4183d
adam <adam@pkgsrc.org>
2018-07-03 05:03:01 +0000

    extend PYTHON_VERSIONS_ for Python 3.7

--- devel/gdb7/options.mk
+++ devel/gdb7/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2017/06/05 10:41:28 maya Exp $
+# $NetBSD: options.mk,v 1.2 2018/07/03 05:03:09 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gdb
 PKG_SUPPORTED_OPTIONS=	python
@@ -8,6 +8,6 @@ PKG_SUGGESTED_OPTIONS=	python
 
 .if !empty(PKG_OPTIONS:Mpython)
 CONFIGURE_ARGS+=	--with-python=${PYTHONBIN}
-PYTHON_VERSIONS_INCOMPATIBLE=	34 35 36
+PYTHON_VERSIONS_INCOMPATIBLE=	34 35 36 37
 .include "../../lang/python/application.mk"
 .endif
