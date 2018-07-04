c32f0aa320ea99777016da25e84206e6ab87b8c6
adam <adam@pkgsrc.org>
2018-07-04 12:46:29 +0000

    gpgme: updated to 1.11.1
    version 1.11.1:
    * Fixed build problems in the 1.11.0 release.
    * Added C++ interfaces which were planned for 1.11.0.
    
    version 1.11.0:
    * New encryption API to support direct key specification including
      hidden recipients option and taking keys from a file.  This also
      allows to enforce the use of a subkey.
    * New encryption flag for the new API to enforce the use of plain
      mail addresses (addr-spec).
    * The import API can now tell whether v3 keys are skipped.  These old
      and basically broken keys are not anymore supported by GnuPG 2.1.
    * The decrypt and verify API will now return the MIME flag as
      specified by RFC-4880bis.
    * The offline mode now has an effect on gpg by disabling all network
      access.
    * A failed OpenPGP verification how returns the fingerprint of the
      intended key if a recent gpg version was used for signature
      creation.
    * New tool gpgme-json as native messaging server for web browsers.
      As of now public key encryption and decryption is supported.
      Requires Libgpg-error 1.29.
    * New context flag "request-origin" which has an effect when used
      with GnuPG 2.2.6 or later.
    * New context flag "no-symkey-cache" which has an effect when used
      with GnuPG 2.2.7 or later.
    * New convenience constant GPGME_KEYLIST_MODE_LOCATE.
    * Improved the Python documentation.
    * Fixed a potential regression with GnuPG 2.2.6 or later.
    * Fixed a crash in the Python bindings on 32 bit platforms.
    * Various minor fixes.

--- security/gpgme/options.mk
+++ security/gpgme/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.11 2018/06/04 07:44:35 leot Exp $
+# $NetBSD: options.mk,v 1.12 2018/07/04 12:46:29 adam Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.gpgme
 PKG_OPTIONS_REQUIRED_GROUPS=	gnupg
@@ -10,7 +10,7 @@ PKG_SUGGESTED_OPTIONS=		gnupg2
 .if !empty(PKG_OPTIONS:Mgnupg2)
 DEPENDS+=		gnupg2>=2.2.0:../../security/gnupg2
 REPLACE_SH+=		tests/gpg/pinentry
-GPG_DEFAULT=		${LOCALBASE}/bin/gpg2
+GPG_DEFAULT=		${PREFIX}/bin/gpg2
 MAKE_FLAGS+=		GPG=${GPG_DEFAULT}
 .else
 DEPENDS+=		gnupg>=1.4.2:../../security/gnupg
