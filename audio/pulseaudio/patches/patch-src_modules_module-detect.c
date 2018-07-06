109821d36ea2f4fb01c7807512961a84e4f71d40
ryoon <ryoon@pkgsrc.org>
2018-07-06 14:10:15 +0000

    Update to 12.0
    Changelog:
    PulseAudio 12.0 release notes
    
    Changes at a glance
      * Better latency reporting (and hence better A/V sync) with the A2DP
        bluetooth profile
      * Much more accurate latency reporting for AirPlay devices
      * Fixed a crash or high CPU use problem with Intel HDMI LPE
      * module-switch-on-connect now ignores virtual devices
      * When using passthrough for compressed audio, set the "non-audio" bit
      * Prioritize HDMI output over S/PDIF output
      * HSP support for more bluetooth headsets
      * Choose the A2DP bluetooth profile by default instead of HSP
      * New "sink_input_properties" module argument for module-ladspa-sink
      * New "use_system_clock_for_timing" module argument for module-pipe-sink
      * module-pipe-sink can now use an existing pipe
      * Steelseries Arctis 7 USB headset stereo output support
      * Dell Thunderbolt Dock TB16 speaker jack support
      * Fixed digital input support for some USB sound cards
      * Fixed Native Instruments Traktor Audio 6 detection
      * Ability to disable input or output on macOS
      * New "dereverb" option for the Speex echo canceller
      * New module: module-always-source
      * State files not any more readable by all users in the system mode
      * module-augment-properties now uses XDG_DATA_DIRS to find .desktop files
      * Updates for the Vala bindings
      * The GConf dependency can now be avoided
      * qpaeq license changed from AGPL to LGPL
      * qpaeq ported to Qt 5
      * Compatibility with glibc 2.27
      * The esdcompat tool isn't any more installed if esound support is disabled

--- audio/pulseaudio/patches/patch-src_modules_module-detect.c
+++ audio/pulseaudio/patches/patch-src_modules_module-detect.c
@@ -1,12 +1,14 @@
-$NetBSD: patch-src_modules_module-detect.c,v 1.4 2016/01/22 19:29:11 ryoon Exp $
+$NetBSD: patch-src_modules_module-detect.c,v 1.5 2018/07/06 14:10:15 ryoon Exp $
 
 Hack to set proper sound device on NetBSD.
 
---- src/modules/module-detect.c.orig	2014-01-23 18:57:55.000000000 +0000
+--- src/modules/module-detect.c.orig	2018-05-11 11:43:30.000000000 +0000
 +++ src/modules/module-detect.c
-@@ -122,6 +122,17 @@ static int detect_oss(pa_core *c, int ju
+@@ -121,7 +121,19 @@ static int detect_alsa(pa_core *c, int j
+ static int detect_oss(pa_core *c, int just_one) {
      FILE *f;
      int n = 0, b = 0;
++    pa_module *m = NULL;
  
 +#if defined(__NetBSD__)
 +/*
@@ -15,14 +17,22 @@ Hack to set proper sound device on NetBSD.
 + */
 +    char args[64];
 +    pa_snprintf(args, sizeof(args), "mmap=0 device=/dev/audio");
-+    if (!pa_module_load(c, "module-oss", args))
-+	exit(1);
++    if (pa_module_load(&m, c, "module-oss", args) < 0)
++	return 0;
 +    n = 1;
 +#else /* __linux__ or __FreeBSD__ */
      if (!(f = pa_fopen_cloexec("/dev/sndstat", "r")) &&
          !(f = pa_fopen_cloexec("/proc/sndstat", "r")) &&
          !(f = pa_fopen_cloexec("/proc/asound/oss/sndstat", "r"))) {
-@@ -173,6 +184,7 @@ static int detect_oss(pa_core *c, int ju
+@@ -135,7 +147,6 @@ static int detect_oss(pa_core *c, int ju
+     while (!feof(f)) {
+         char line[256], args[64];
+         unsigned device;
+-        pa_module *m = NULL;
+ 
+         if (!fgets(line, sizeof(line), f))
+             break;
+@@ -174,6 +185,7 @@ static int detect_oss(pa_core *c, int ju
      }
  
      fclose(f);
