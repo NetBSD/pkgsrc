57fa664a94a69a6e73b26f12650632c806aa47c3
youri <youri@pkgsrc.org>
2018-07-09 13:53:34 +0000

    clock_gettime definition for macOS <= Sierra.

--- print/cups-base/patches/patch-cups_ipp.c
+++ print/cups-base/patches/patch-cups_ipp.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-cups_ipp.c,v 1.1 2018/06/22 14:12:45 bouyer Exp $
+$NetBSD: patch-cups_ipp.c,v 1.2 2018/07/09 13:53:34 youri Exp $
 From upstream: https://github.com/apple/cups/commit/18545a5e7ab478b187a0d7136ee19496ae32fc9b
  Fix regressions in ippValidateAttribute (Issue #5322, Issue #5330) 
 
---- cups/ipp.c.orig
+--- cups/ipp.c.orig	2018-06-05 16:06:54.000000000 +0000
 +++ cups/ipp.c
-@@ -5101,16 +5101,19 @@ ippValidateAttribute(
+@@ -5097,16 +5097,19 @@ ippValidateAttribute(
  	      break;
  	  }
  
@@ -34,18 +34,18 @@ From upstream: https://github.com/apple/cups/commit/18545a5e7ab478b187a0d7136ee1
  
  	  if ((ptr - attr->values[i].string.text) > (IPP_MAX_TEXT - 1))
  	  {
-@@ -5163,16 +5166,19 @@ ippValidateAttribute(
+@@ -5159,16 +5162,19 @@ ippValidateAttribute(
  	      break;
  	  }
  
 -	  if (*ptr < ' ' || *ptr == 0x7f)
--	  {
++	  if (*ptr)
+ 	  {
 -	    ipp_set_error(IPP_STATUS_ERROR_BAD_REQUEST, _("\"%s\": Bad name value \"%s\" - bad control character (PWG 5100.14 section 8.1)."), attr->name, attr->values[i].string.text);
 -	    return (0);
 -	  }
 -	  else if (*ptr)
-+	  if (*ptr)
- 	  {
+-	  {
 -	    ipp_set_error(IPP_STATUS_ERROR_BAD_REQUEST, _("\"%s\": Bad name value \"%s\" - bad UTF-8 sequence (RFC 8011 section 5.1.3)."), attr->name, attr->values[i].string.text);
 -	    return (0);
 -	  }
