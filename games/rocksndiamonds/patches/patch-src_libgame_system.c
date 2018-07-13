cd0cb1dfff0bd6dd46791083e227dca303a77fdd
adam <adam@pkgsrc.org>
2018-07-13 17:27:32 +0000

    rocksndiamonds: updated to 4.1.0.0
    4.1.0.0:
    added various additional customization options to create your own games
    added showing/hiding game element sections in the level editor depending on the selected game engine
    added doing ‘redo’ by pressing ‘undo’ button with Ctrl or Shift key in the level editor
    added new automatically saved config file for the selected zoom tile size in the level editor
    moved level property ‘initial wind direction’ to balloon elements in editor
    fixed various bugs (and probably added some new ones)

--- games/rocksndiamonds/patches/patch-src_libgame_system.c
+++ games/rocksndiamonds/patches/patch-src_libgame_system.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_libgame_system.c,v 1.2 2017/10/13 16:08:35 adam Exp $
+$NetBSD: patch-src_libgame_system.c,v 1.3 2018/07/13 17:27:32 adam Exp $
 
 InitJoystick will be called more than once, so reset the state
 variable 'joystick' only if there's no joystick available (yet).
 
---- src/libgame/system.c.orig	2017-09-12 20:17:10.000000000 +0000
+--- src/libgame/system.c.orig	2018-04-10 19:44:08.000000000 +0000
 +++ src/libgame/system.c
-@@ -1630,8 +1630,10 @@ void InitJoysticks()
+@@ -1744,8 +1744,10 @@ void InitJoysticks()
  
    /* always start with reliable default values */
    joystick.status = JOYSTICK_NOT_AVAILABLE;
