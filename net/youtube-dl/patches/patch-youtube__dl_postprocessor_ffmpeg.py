31c004be18b03788abf2cb82da5e986c4dc67043
leot <leot@pkgsrc.org>
2018-07-14 11:27:05 +0000

    youtube-dl: Update net/youtube-dl to 20180710
    Changes:
    version 2018.07.10
    
    Core
    * [utils] Share JSON-LD regular expression
    * [downloader/dash] Improve error handling (#16927)
    
    Extractors
    + [nrktv] Add support for new season and serie URL schema
    + [nrktv] Add support for new episode URL schema (#16909)
    + [frontendmasters] Add support for frontendmasters.com (#3661, #16328)
    * [funk] Fix extraction (#16918)
    * [watchbox] Fix extraction (#16904)
    * [dplayit] Sort formats
    * [dplayit] Fix extraction (#16901)
    * [youtube] Improve login error handling (#13822)
    
    
    version 2018.07.04
    
    Core
    * [extractor/common] Properly escape % in MPD templates (#16867)
    * [extractor/common] Use source URL as Referer for HTML5 entries (16849)
    * Prefer ffmpeg over avconv by default (#8622)
    
    Extractors
    * [pluralsight] Switch to graphql (#16889, #16895, #16896, #16899)
    * [lynda] Simplify login and improve error capturing (#16891)
    + [go90] Add support for embed URLs (#16873)
    * [go90] Detect geo restriction error and pass geo verification headers
      (#16874)
    * [vlive] Fix live streams extraction (#16871)
    * [npo] Fix typo (#16872)
    + [mediaset] Add support for new videos and extract all formats (#16568)
    * [dctptv] Restore extraction based on REST API (#16850)
    * [svt] Improve extraction and add support for pages (#16802)
    * [porncom] Fix extraction (#16808)
    
    
    version 2018.06.25
    
    Extractors
    * [joj] Relax URL regular expression (#16771)
    * [brightcove] Workaround sonyliv DRM protected videos (#16807)
    * [motherless] Fix extraction (#16786)
    * [itv] Make SOAP request non fatal and extract metadata from webpage (#16780)
    - [foxnews:insider] Remove extractor (#15810)
    + [foxnews] Add support for iframe embeds (#15810, #16711)
    
    
    version 2018.06.19
    
    Core
    + [extractor/common] Introduce expected_status in _download_* methods
      for convenient accept of HTTP requests failed with non 2xx status codes
    + [compat] Introduce compat_integer_types
    
    Extractors
    * [peertube] Improve generic support (#16733)
    + [6play] Use geo verification headers
    * [rtbf] Fix extraction for python 3.2
    * [vgtv] Improve HLS formats extraction
    + [vgtv] Add support for www.aftonbladet.se/tv URLs
    * [bbccouk] Use expected_status
    * [markiza] Expect 500 HTTP status code
    * [tvnow] Try all clear manifest URLs (#15361)

--- net/youtube-dl/patches/patch-youtube__dl_postprocessor_ffmpeg.py
+++ net/youtube-dl/patches/patch-youtube__dl_postprocessor_ffmpeg.py
@@ -1,21 +1,21 @@
-$NetBSD: patch-youtube__dl_postprocessor_ffmpeg.py,v 1.3 2018/05/06 10:22:59 leot Exp $
+$NetBSD: patch-youtube__dl_postprocessor_ffmpeg.py,v 1.4 2018/07/14 11:27:05 leot Exp $
 
 Also look and use ffmpeg[234]/ffproble[234] if possible, preferring
 the unversioned one (i.e. selected via alternatives framework)
 or the newest one.
 
---- youtube_dl/postprocessor/ffmpeg.py.orig	2016-02-09 11:57:41.000000000 +0000
+--- youtube_dl/postprocessor/ffmpeg.py.orig	2018-07-03 22:29:01.000000000 +0000
 +++ youtube_dl/postprocessor/ffmpeg.py
-@@ -51,7 +51,7 @@ class FFmpegPostProcessor(PostProcessor)
+@@ -76,7 +76,7 @@ class FFmpegPostProcessor(PostProcessor)
          return FFmpegPostProcessor(downloader)._versions
  
      def _determine_executables(self):
 -        programs = ['avprobe', 'avconv', 'ffmpeg', 'ffprobe']
 +        programs = ['avprobe', 'avconv', 'ffmpeg', 'ffmpeg4', 'ffmpeg3', 'ffmpeg2', 'ffprobe', 'ffprobe4', 'ffprobe3', 'ffprobe2']
-         prefer_ffmpeg = False
+         prefer_ffmpeg = True
  
          self.basename = None
-@@ -79,7 +79,7 @@ class FFmpegPostProcessor(PostProcessor)
+@@ -104,7 +104,7 @@ class FFmpegPostProcessor(PostProcessor)
                          self._versions = {}
                          return None
                      location = os.path.dirname(os.path.abspath(location))
@@ -24,26 +24,26 @@ or the newest one.
                          prefer_ffmpeg = True
  
                  self._paths = dict(
-@@ -93,18 +93,18 @@ class FFmpegPostProcessor(PostProcessor)
+@@ -118,18 +118,18 @@ class FFmpegPostProcessor(PostProcessor)
              self._paths = dict((p, p) for p in programs)
  
-         if prefer_ffmpeg:
--            prefs = ('ffmpeg', 'avconv')
-+            prefs = ('ffmpeg', 'ffmpeg4', 'ffmpeg3', 'ffmpeg2', 'avconv')
-         else:
+         if prefer_ffmpeg is False:
 -            prefs = ('avconv', 'ffmpeg')
 +            prefs = ('avconv', 'ffmpeg', 'ffmpeg4', 'ffmpeg3', 'ffmpeg2')
+         else:
+-            prefs = ('ffmpeg', 'avconv')
++            prefs = ('ffmpeg', 'ffmpeg4', 'ffmpeg3', 'ffmpeg2', 'avconv')
          for p in prefs:
              if self._versions[p]:
                  self.basename = p
                  break
  
-         if prefer_ffmpeg:
--            prefs = ('ffprobe', 'avprobe')
-+            prefs = ('ffprobe', 'ffprobe4', 'ffprobe3', 'ffprobe2', 'avprobe')
-         else:
+         if prefer_ffmpeg is False:
 -            prefs = ('avprobe', 'ffprobe')
 +            prefs = ('avprobe', 'ffprobe', 'ffprobe4', 'ffprobe3', 'ffprobe2')
+         else:
+-            prefs = ('ffprobe', 'avprobe')
++            prefs = ('ffprobe', 'ffprobe4', 'ffprobe3', 'ffprobe2', 'avprobe')
          for p in prefs:
              if self._versions[p]:
                  self.probe_basename = p
