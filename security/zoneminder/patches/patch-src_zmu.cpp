7a1e8dcb18cc8828cfeaeb5a5a28b13db53266b2
gdt <gdt@pkgsrc.org>
2018-07-14 15:03:57 +0000

    zoneminder: adjust fix for printf/time_t
    Rather than casting to long, cast to intmax_t and print with %jd, as
    suggested by joerg@.

--- security/zoneminder/patches/patch-src_zmu.cpp
+++ security/zoneminder/patches/patch-src_zmu.cpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_zmu.cpp,v 1.1 2018/07/13 02:05:11 gdt Exp $
+$NetBSD: patch-src_zmu.cpp,v 1.2 2018/07/14 15:03:57 gdt Exp $
 
 --- src/zmu.cpp.orig	2015-02-05 02:52:37.000000000 +0000
 +++ src/zmu.cpp
@@ -7,25 +7,37 @@ $NetBSD: patch-src_zmu.cpp,v 1.1 2018/07/13 02:05:11 gdt Exp $
  				{
  					if ( have_output ) printf( "%c", separator );
 -					printf( "%ld.%02ld", timestamp.tv_sec, timestamp.tv_usec/10000 );
-+					printf( "%ld.%02ld", (long) timestamp.tv_sec, timestamp.tv_usec/10000 );
++					printf( "%jd.%02ld", (intmax_t) timestamp.tv_sec, timestamp.tv_usec/10000 );
  					have_output = true;
  				}
  			}
-@@ -729,7 +729,7 @@ int main( int argc, char *argv[] )
+@@ -724,12 +724,12 @@ int main( int argc, char *argv[] )
+ 						if ( monitor && monitor->connect() )
+ 						{
+ 							struct timeval tv = monitor->GetTimestamp();
+-							printf( "%4d%5d%6d%9d%11ld.%02ld%6d%6d%8d%8.2f\n",
++							printf( "%4d%5d%6d%9d%11jd.%02ld%6d%6d%8d%8.2f\n",
+ 								monitor->Id(),
  								function,
  								monitor->GetState(),
  								monitor->GetTriggerState(),
 -								tv.tv_sec, tv.tv_usec/10000,
-+								(long) tv.tv_sec, tv.tv_usec/10000,
++								(intmax_t) tv.tv_sec, tv.tv_usec/10000,
  								monitor->GetLastReadIndex(),
  								monitor->GetLastWriteIndex(),
  								monitor->GetLastEvent(),
-@@ -746,7 +746,7 @@ int main( int argc, char *argv[] )
+@@ -741,12 +741,12 @@ int main( int argc, char *argv[] )
+ 					else
+ 					{
+ 						struct timeval tv = { 0, 0 };
+-						printf( "%4d%5d%6d%9d%11ld.%02ld%6d%6d%8d%8.2f\n",
++						printf( "%4d%5d%6d%9d%11jd.%02ld%6d%6d%8d%8.2f\n",
+ 							mon_id,
  							function,
  							0,
  							0,
 -							tv.tv_sec, tv.tv_usec/10000,
-+							(long) tv.tv_sec, tv.tv_usec/10000,
++							(intmax_t) tv.tv_sec, tv.tv_usec/10000,
  							0,
  							0,
  							0,
