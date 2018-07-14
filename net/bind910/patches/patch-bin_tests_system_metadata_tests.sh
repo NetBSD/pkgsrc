7d6ebb9ed6a89d2022929171300c94056c2be5a0
spz <spz@pkgsrc.org>
2018-07-14 18:36:25 +0000

    Pullup ticket #5788 - requested by taca net/bind910: security update
    Revisions pulled up:
    - net/bind910/Makefile                                          1.41
    - net/bind910/distinfo                                          1.33-1.34
    - net/bind910/patches/patch-bin_tests_system_metadata_tests.sh  1.2
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jul 14 03:53:42 UTC 2018
    
       Modified Files:
       	pkgsrc/net/bind910: Makefile distinfo
       	pkgsrc/net/bind910/patches: patch-bin_tests_system_metadata_tests.sh
    
       Log Message:
       net/bind910: update to 9.10.8
    
       This release contains security fix for CVE-2018-5738 and several bug fixes.
       For more detail, please refer CHANGES file.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.40 -r1.41 pkgsrc/net/bind910/Makefile
       cvs rdiff -u -r1.32 -r1.33 pkgsrc/net/bind910/distinfo
       cvs rdiff -u -r1.1 -r1.2 \
           pkgsrc/net/bind910/patches/patch-bin_tests_system_metadata_tests.sh
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jul 14 03:54:59 UTC 2018
    
       Modified Files:
       	pkgsrc/net/bind910: distinfo
    
       Log Message:
       net/bind910: remove local trial patch.
    
       Remove local trial patch info.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.33 -r1.34 pkgsrc/net/bind910/distinfo

--- net/bind910/patches/patch-bin_tests_system_metadata_tests.sh
+++ net/bind910/patches/patch-bin_tests_system_metadata_tests.sh
@@ -1,15 +1,15 @@
-$NetBSD: patch-bin_tests_system_metadata_tests.sh,v 1.1 2018/03/24 15:02:32 taca Exp $
+$NetBSD: patch-bin_tests_system_metadata_tests.sh,v 1.1.4.1 2018/07/14 18:36:25 spz Exp $
 
 * Avoid using "==" for argument of test(1).
 
---- bin/tests/system/metadata/tests.sh.orig	2018-03-08 20:55:52.000000000 +0000
+--- bin/tests/system/metadata/tests.sh.orig	2018-07-03 06:44:00.000000000 +0000
 +++ bin/tests/system/metadata/tests.sh
-@@ -146,7 +146,7 @@ status=`expr $status + $ret`
+@@ -144,7 +144,7 @@ status=`expr $status + $ret`
  
- echo "I:checking warning about permissions change on key with dnssec-settime ($n)"
+ echo_i "checking warning about permissions change on key with dnssec-settime ($n)"
  uname=`uname -o 2> /dev/null`
 -if [ Cygwin == "$uname"  ]; then
 +if [ Cygwin = "$uname"  ]; then
- 	echo "I: Cygwin detected, skipping"
+ 	echo_i "Cygwin detected, skipping"
  else
  	ret=0
