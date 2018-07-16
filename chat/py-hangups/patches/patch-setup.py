4f57abc3f23aa9117da239fe54aaad77ab9fbdca
leot <leot@pkgsrc.org>
2018-07-16 17:43:24 +0000

    py-hangups: Update chat/py-hangups to 0.4.5
    Changes:
    0.4.5
    -----
     * raise minimum Python version to 3.5.3
     * add support for Python 3.7
     * improve markup parsing
     * fix crash on event for unloaded conversion
     * add compatibility with aiohttp 3

--- chat/py-hangups/patches/patch-setup.py
+++ chat/py-hangups/patches/patch-setup.py
@@ -1,15 +1,16 @@
-$NetBSD: patch-setup.py,v 1.1 2017/09/04 13:58:24 leot Exp $
+$NetBSD: patch-setup.py,v 1.2 2018/07/16 17:43:25 leot Exp $
 
 Avoid too strict version requirements.
 
---- setup.py.orig	2017-07-30 23:31:42.000000000 +0000
+--- setup.py.orig	2018-07-15 22:09:11.000000000 +0000
 +++ setup.py
-@@ -23,22 +23,22 @@ with open('README.rst') as f:
+@@ -25,16 +25,16 @@ with open('README.rst') as f:
  # hangups from breaking when new versions of dependencies are released,
  # especially for end-users (non-developers) who use pip to install hangups.
  install_requires = [
 -    'ConfigArgParse==0.11.0',
--    'aiohttp>=1.3,<3',
+-    'aiohttp>=1.3,<4',
+-    'async-timeout>=2,<4',
 -    'appdirs>=1.4,<1.5',
 -    'readlike==0.1.2',
 -    'requests>=2.6.0,<3',  # uses semantic versioning (after 2.6)
@@ -19,6 +20,7 @@ Avoid too strict version requirements.
 -    'MechanicalSoup==0.6.0',
 +    'ConfigArgParse>=0.11.0',
 +    'aiohttp>=1.3',
++    'async-timeout>=2',
 +    'appdirs>=1.4',
 +    'readlike>=0.1.2',
 +    'requests>=2.6.0',  # uses semantic versioning (after 2.6)
@@ -29,11 +31,3 @@ Avoid too strict version requirements.
  ]
  
  
- if sys.version_info < (3, 4, 3):
-     # For Python earlier than 3.4.3, use a backported asyncio that fixes an
-     # issue with an exception being logged on exit.
--    install_requires.append('asyncio==3.4.3')
-+    install_requires.append('asyncio>=3.4.3')
- 
- 
- setup(
