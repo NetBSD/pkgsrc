2f3da49beff43d3cbd2878c0ae1766f74683fb35
minskim <minskim@pkgsrc.org>
2018-07-17 03:51:22 +0000

    devel/go-i18n: Update to 1.10.0
    Changes since 1.8.1:
    - Export language.RegisterPluralFunc
    - Export language.Operand
    - Update to CLDR 31.0.1
    - Fix panic in goi18n command
    - Export language.GetPluralSpec

--- devel/go-i18n/buildlink3.mk
+++ devel/go-i18n/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/06/11 18:15:31 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/07/17 03:51:22 minskim Exp $
 
 BUILDLINK_TREE+=	go-i18n
 
@@ -8,7 +8,7 @@ GO_I18N_BUILDLINK3_MK:=
 BUILDLINK_CONTENTS_FILTER.go-i18n=	${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-i18n?=		build
 
-BUILDLINK_API_DEPENDS.go-i18n+=		go-i18n>=1.8.1
+BUILDLINK_API_DEPENDS.go-i18n+=		go-i18n>=1.10.0
 BUILDLINK_PKGSRCDIR.go-i18n?=		../../devel/go-i18n
 
 .include "../../devel/go-yaml/buildlink3.mk"
