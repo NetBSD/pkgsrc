ef32207668c7259ab9c935f481bb1e1c4172adeb
alnsn <alnsn@pkgsrc.org>
2018-07-17 21:52:25 +0000

    Update lang/lua53 to version 5.3.5.
    Lua 5.3.5 fixes all bugs listed in http://www.lua.org/bugs.html#5.3.4:
    
    1. Wrong code generated for a 'goto' followed by a label inside an 'if'.
    2. Lua crashes when building sequences with more than 2^30 elements.
    3. Table length computation overflows for sequences larger than 2^31 elements.
    4. Lua does not check GC when creating error messages.
    5. Dead keys with nil values can stay in weak tables.
    6. lua_pushcclosure should not call the garbage collector when n is zero.
    7. Memory-allocation error when resizing a table can leave it in an
       inconsistent state..

--- lang/lua53/version.mk
+++ lang/lua53/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.5 2017/02/10 23:01:49 alnsn Exp $
+# $NetBSD: version.mk,v 1.6 2018/07/17 21:52:25 alnsn Exp $
 
-LUA_VERSION=	5.3.4
+LUA_VERSION=	5.3.5
 
 LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
 LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
