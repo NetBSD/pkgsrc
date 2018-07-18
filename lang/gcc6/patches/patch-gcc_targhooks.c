f4d8613caebc1591cb6982cbf1d058ea5a2c9467
maya <maya@pkgsrc.org>
2018-07-18 22:35:44 +0000

    gcc6: don't try to make references to stack_chk_fail_local on netbsd/sun sun was added as joyent is patching for the same.
    this code is only reached if compiling for i386 (or some variation of ppc)
    netbsd's stack_chk_fail_local won't work for this.
    
    PR pkg/53436.

---
+++ lang/gcc6/patches/patch-gcc_targhooks.c
@@ -0,0 +1,17 @@
+$NetBSD: patch-gcc_targhooks.c,v 1.1 2018/07/18 22:35:44 maya Exp $
+
+PR pkg/53436
+netbsd can't handle hidden reference to stack_chk_fail_local.
+(note: this code is only happening if targeting i386)
+
+--- gcc/targhooks.c.orig	2017-06-22 07:22:13.000000000 +0000
++++ gcc/targhooks.c
+@@ -755,7 +755,7 @@ default_external_stack_protect_fail (voi
+ tree
+ default_hidden_stack_protect_fail (void)
+ {
+-#ifndef HAVE_GAS_HIDDEN
++#if !defined(HAVE_GAS_HIDDEN) || defined(__NetBSD__) || defined(__sun)
+   return default_external_stack_protect_fail ();
+ #else
+   tree t = stack_chk_fail_decl;
