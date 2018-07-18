595ae7827da6f24bd9c6e93925dd958c51a2d118
maya <maya@pkgsrc.org>
2018-07-18 23:45:14 +0000

    libblkid: all users of this require libuuid.
    see pkgconfig file:
    Requires.private: uuid
    
    prompted by
    https://us-east.manta.joyent.com/pkgsrc/public/reports/Linux/ubuntu16.04/trunk/x86_64/20180717.0004/hal-0.5.14nb20/configure.log

--- devel/libblkid/buildlink3.mk
+++ devel/libblkid/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/12/08 08:03:35 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/07/18 23:45:14 maya Exp $
 
 BUILDLINK_TREE+=	libblkid
 
@@ -8,6 +8,7 @@ LIBBLKID_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libblkid+=	libblkid>=2.18
 BUILDLINK_PKGSRCDIR.libblkid?=		../../devel/libblkid
 
+.include "../../devel/libuuid/buildlink3.mk"
 .endif	# LIBBLKID_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libblkid
