54b786790c485419304ba3a36c2e7e6121a82276
jaapb <jaapb@pkgsrc.org>
2018-07-19 15:15:20 +0000

    Recursive revbump associated with the update of lang/ocaml to 4.07.

--- misc/ocaml-opam-file-format/buildlink3.mk
+++ misc/ocaml-opam-file-format/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/07/11 10:34:30 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/07/19 15:15:27 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-opam-file-format
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-opam-file-format
 OCAML_OPAM_FILE_FORMAT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-opam-file-format+=	ocaml-opam-file-format>=2.0.0beta
-BUILDLINK_ABI_DEPENDS.ocaml-opam-file-format+=	ocaml-opam-file-format>=2.0.0beta3
+BUILDLINK_ABI_DEPENDS.ocaml-opam-file-format+=	ocaml-opam-file-format>=2.0.0beta3nb4
 BUILDLINK_PKGSRCDIR.ocaml-opam-file-format?=	../../misc/ocaml-opam-file-format
 .endif	# OCAML_OPAM_FILE_FORMAT_BUILDLINK3_MK
 
--- misc/ocaml-topkg/buildlink3.mk
+++ misc/ocaml-topkg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/01/10 16:34:50 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/07/19 15:15:27 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-topkg
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-topkg
 OCAML_TOPKG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-topkg+=	ocaml-topkg>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-topkg+=	ocaml-topkg>=0.9.1
+BUILDLINK_ABI_DEPENDS.ocaml-topkg+=	ocaml-topkg>=0.9.1nb2
 BUILDLINK_PKGSRCDIR.ocaml-topkg?=	../../misc/ocaml-topkg
 
 .endif	# OCAML_TOPKG_BUILDLINK3_MK
