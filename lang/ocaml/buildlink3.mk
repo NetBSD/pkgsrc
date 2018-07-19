0305a76c63b3830b1789f873a7185c01f411e1a0
jaapb <jaapb@pkgsrc.org>
2018-07-19 12:20:57 +0000

    Corrected buildlink file for lang/ocaml

--- lang/ocaml/buildlink3.mk
+++ lang/ocaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.46 2018/07/19 12:16:38 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.47 2018/07/19 12:20:57 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml
 OCAML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml+=	ocaml>=4.07.0
-BUILDLINK_ABI_DEPENDS.ocaml+=	ocaml>=4.07.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml+=	ocaml>=4.07.0
 BUILDLINK_PKGSRCDIR.ocaml?=	../../lang/ocaml
 #BUILDLINK_DEPMETHOD.ocaml?=	build
 
