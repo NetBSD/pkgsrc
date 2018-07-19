63d90d8c2cb55992eb0e2f8583b63a149d7114a0
jaapb <jaapb@pkgsrc.org>
2018-07-19 15:15:20 +0000

    Recursive revbump associated with the update of lang/ocaml to 4.07.

--- devel/ocamlify/buildlink3.mk
+++ devel/ocamlify/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/01/10 16:17:05 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/07/19 15:15:26 jaapb Exp $
 
 BUILDLINK_TREE+=	ocamlify
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocamlify
 OCAMLIFY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocamlify+=	ocamlify>=0.0.2nb1
-BUILDLINK_ABI_DEPENDS.ocamlify+=	ocamlify>=0.0.2nb7
+BUILDLINK_ABI_DEPENDS.ocamlify+=	ocamlify>=0.0.2nb9
 BUILDLINK_PKGSRCDIR.ocamlify?=	../../devel/ocamlify
 .endif	# OCAMLIFY_BUILDLINK3_MK
 
