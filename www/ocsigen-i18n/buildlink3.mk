c3250378a6fa49d4961e1f6c5a310dc01c76ba72
jaapb <jaapb@pkgsrc.org>
2018-07-19 14:24:10 +0000

    Corrected install procedure for www/ocsigen-i18n and revbump
    This package uses ocamlfind to build, but not to install. This has now
    been incorporated into the Makefile.

--- www/ocsigen-i18n/buildlink3.mk
+++ www/ocsigen-i18n/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/06/05 16:02:40 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/07/19 14:24:10 jaapb Exp $
 
 BUILDLINK_TREE+=	ocsigen-i18n
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocsigen-i18n
 OCSIGEN_I18N_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocsigen-i18n+=	ocsigen-i18n>=3.1.0
+BUILDLINK_ABI_DEPENDS.ocsigen-i18n+=	ocsigen-i18n>=3.1.0nb1
 BUILDLINK_PKGSRCDIR.ocsigen-i18n?=	../../www/ocsigen-i18n
 .endif	# OCSIGEN_I18N_BUILDLINK3_MK
 
