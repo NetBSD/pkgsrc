98b283cef6b9b8f8f9a77dd6018493ef02e9f4fe
jaapb <jaapb@pkgsrc.org>
2018-07-19 12:18:03 +0000

    Updated lang/camlp4 to version 4.07+1.
    This is a maintenance release, only meant for compatibility with OCaml
    4.07.

--- lang/camlp4/buildlink3.mk
+++ lang/camlp4/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2018/01/10 16:22:41 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2018/07/19 12:18:03 jaapb Exp $
 
 BUILDLINK_TREE+=	camlp4
 
 .if !defined(CAMLP4_BUILDLINK3_MK)
 CAMLP4_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.camlp4+=	camlp4>=4.06
-BUILDLINK_ABI_DEPENDS.camlp4+=	camlp4>=4.06
+BUILDLINK_API_DEPENDS.camlp4+=	camlp4>=4.07
+BUILDLINK_ABI_DEPENDS.camlp4+=	camlp4>=4.07
 BUILDLINK_PKGSRCDIR.camlp4?=	../../lang/camlp4
 .endif	# CAMLP4_BUILDLINK3_MK
 
