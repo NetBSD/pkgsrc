b7ac24061b9de6af2dba75cbfbb3b84fdd21b9c1
taca <taca@pkgsrc.org>
2018-07-20 13:28:48 +0000

    lang/php56: update to 5.6.37
    19 Jul 2018, PHP 5.6.37
    
    - Exif:
      . Fixed bug #76423 (Int Overflow lead to Heap OverFlow in
        exif_thumbnail_extract of exif.c). (Stas)
      . Fixed bug #76557 (heap-buffer-overflow (READ of size 48) while reading exif
        data). (Stas)
    
    - Win32:
      . Fixed bug #76459 (windows linkinfo lacks openbasedir check). (Anatol)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.224 2018/07/20 13:27:28 taca Exp $
+# $NetBSD: phpversion.mk,v 1.225 2018/07/20 13:28:48 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -87,7 +87,7 @@
 PHPVERSION_MK=	defined
 
 # Define each PHP's version.
-PHP56_VERSION=	5.6.36
+PHP56_VERSION=	5.6.37
 PHP70_VERSION=	7.0.31
 PHP71_VERSION=	7.1.20
 PHP72_VERSION=	7.2.8
