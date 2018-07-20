d8023bd50ec885ce4d74ae7b250881c4a80d34ed
taca <taca@pkgsrc.org>
2018-07-20 13:25:26 +0000

    lang/php72: update to 7.2.8
    19 Jul 2018, PHP 7.2.8
    
    - Core:
      . Fixed bug #76534 (PHP hangs on 'illegal string offset on string references
        with an error handler). (Laruence)
      . Fixed bug #76520 (Object creation leaks memory when executed over HTTP).
        (Nikita)
      . Fixed bug #76502 (Chain of mixed exceptions and errors does not serialize
        properly). (Nikita)
    
    - Date:
      . Fixed bug #76462 (Undefined property: DateInterval::$f). (Anatol)
    
    - EXIF:
      . Fixed bug #76409 (heap use after free in _php_stream_free). (cmb)
      . Fixed bug #76423 (Int Overflow lead to Heap OverFlow in
        exif_thumbnail_extract of exif.c). (Stas)
      . Fixed bug #76557 (heap-buffer-overflow (READ of size 48) while reading exif
        data). (Stas)
    
    - FPM:
      . Fixed bug #73342 (Vulnerability in php-fpm by changing stdin to
        non-blocking). (Nikita)
    
    - GMP:
      . Fixed bug #74670 (Integer Underflow when unserializing GMP and possible
        other classes). (Nikita)
    
    - intl:
      . Fixed bug #76556 (get_debug_info handler for BreakIterator shows wrong
        type). (cmb)
    
    - mbstring:
      . Fixed bug #76532 (Integer overflow and excessive memory usage
        in mb_strimwidth). (MarcusSchwarz)
    
    - Opcache:
      . Fixed bug #76477 (Opcache causes empty return value).
        (Nikita, Laruence)
    
    - PGSQL:
      . Fixed bug #76548 (pg_fetch_result did not fetch the next row). (Anatol)
    
    - phpdbg:
      . Fix arginfo wrt. optional/required parameters. (cmb)
    
    - Reflection:
      . Fixed bug #76536 (PHP crashes with core dump when throwing exception in
        error handler). (Laruence)
      . Fixed bug #75231 (ReflectionProperty#getValue() incorrectly works with
        inherited classes). (Nikita)
    
    - Standard:
      . Fixed bug #76505 (array_merge_recursive() is duplicating sub-array keys).
        (Laruence)
      . Fixed bug #71848 (getimagesize with $imageinfo returns false). (cmb)
    
    - Win32:
      . Fixed bug #76459 (windows linkinfo lacks openbasedir check). (Anatol)
    
    - ZIP:
      . Fixed bug #76461 (OPSYS_Z_CPM defined instead of OPSYS_CPM).
        (Dennis Birkholz, Remi)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.222 2018/07/20 13:23:46 taca Exp $
+# $NetBSD: phpversion.mk,v 1.223 2018/07/20 13:25:26 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.36
 PHP70_VERSION=	7.0.30
 PHP71_VERSION=	7.1.20
-PHP72_VERSION=	7.2.7
+PHP72_VERSION=	7.2.8
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
