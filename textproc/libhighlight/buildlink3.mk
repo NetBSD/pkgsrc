4cb2630979f3497d2a64fcd475ea92a3cce220a5
schmonz <schmonz@pkgsrc.org>
2018-07-23 05:16:43 +0000

    Update to 3.44. From the changelog:
    - fixed gcc 8 compilation warnings
    - fixed Ruby string interpolation
      (https://github.com/andre-simon/highlight/issues/70)
    - added support for ALAN IF (thanks to Tristano Ajmone)
    - added 107 Base16 themes [https://github.com/chriskempson/base16]
      (thanks to Tristano Ajmone)
    - updated Rust and Java reserved words lists
    - revised documentation
    - moved extras/css-themes into extras/themes-resources
    - added extras/themes-resources/base16 (thanks to Tristano Ajmone)
    - GUI: added Base16 theme selection checkbox
    - CLI: added --base16 option to enable the new themes
    - CLI: accept - as argument to read from stdin
      (https://github.com/andre-simon/highlight/issues/73)

--- textproc/libhighlight/buildlink3.mk
+++ textproc/libhighlight/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/05/03 13:13:51 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/07/23 05:16:43 schmonz Exp $
 
 BUILDLINK_TREE+=	libhighlight
 
 .if !defined(LIBHIGHLIGHT_BUILDLINK3_MK)
 LIBHIGHLIGHT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libhighlight+=	libhighlight>=3.42
+BUILDLINK_API_DEPENDS.libhighlight+=	libhighlight>=3.44
 BUILDLINK_PKGSRCDIR.libhighlight?=	../../textproc/libhighlight
 .endif	# LIBHIGHLIGHT_BUILDLINK3_MK
 
