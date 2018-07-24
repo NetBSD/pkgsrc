39b4d3519a58fabb24079519fd78b11c8d397b43
jperkin <jperkin@pkgsrc.org>
2018-07-24 16:07:24 +0000

    openjdk7: Fixes for SunOS and C99.

--- lang/openjdk7/patches/patch-jdk_src_share_native_sun_security_ec_impl_ecc__impl.h
+++ lang/openjdk7/patches/patch-jdk_src_share_native_sun_security_ec_impl_ecc__impl.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-jdk_src_share_native_sun_security_ec_impl_ecc__impl.h,v 1.1 2013/06/15 09:31:06 jperkin Exp $
+$NetBSD: patch-jdk_src_share_native_sun_security_ec_impl_ecc__impl.h,v 1.2 2018/07/24 16:07:25 jperkin Exp $
 
 GCC compatability.
 
---- jdk/src/share/native/sun/security/ec/impl/ecc_impl.h.orig	2013-02-20 17:07:30.000000000 +0000
+--- jdk/src/share/native/sun/security/ec/impl/ecc_impl.h.orig	2017-08-13 05:55:20.000000000 +0000
 +++ jdk/src/share/native/sun/security/ec/impl/ecc_impl.h
-@@ -38,12 +38,13 @@
+@@ -39,12 +39,13 @@
  
  #ifndef _ECC_IMPL_H
  #define _ECC_IMPL_H
@@ -19,11 +19,11 @@ GCC compatability.
  #include "ecl-exp.h"
  
  /*
-@@ -57,6 +58,11 @@ typedef unsigned long ulong_t;
+@@ -58,6 +59,11 @@ typedef unsigned long ulong_t;
  typedef enum { B_FALSE, B_TRUE } boolean_t;
  #endif /* __linux__ */
  
-+#if defined(__solaris__) && defined(_GNU_SOURCE)
++#if defined(__solaris__) && defined(__XOPEN_OR_POSIX)
 +#define B_FALSE _B_FALSE
 +#define B_TRUE _B_TRUE
 +#endif /* __solaris__ && _GNU_SOURCE */
