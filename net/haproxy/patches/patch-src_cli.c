b6e8c7fb4d7aef40bba5b6ad6af3f59eb2778f3a
jperkin <jperkin@pkgsrc.org>
2018-07-25 13:51:18 +0000

    haproxy: Fix SunOS C99 handling.

--- net/haproxy/patches/patch-src_cli.c
+++ net/haproxy/patches/patch-src_cli.c
@@ -1,15 +1,19 @@
-$NetBSD: patch-src_cli.c,v 1.1 2018/04/05 09:18:39 jperkin Exp $
+$NetBSD: patch-src_cli.c,v 1.2 2018/07/25 13:51:18 jperkin Exp $
 
 Special handling for XPG4_2 on SunOS.
 
---- src/cli.c.orig	2018-02-08 13:05:15.000000000 +0000
+--- src/cli.c.orig	2018-06-27 14:27:32.000000000 +0000
 +++ src/cli.c
-@@ -11,6 +11,9 @@
+@@ -11,6 +11,13 @@
   *
   */
  
-+#if defined(__sun) && !defined(_XOPEN_SOURCE) && (__STDC_VERSION__-0 < 199901L)
-+#define _XOPEN_SOURCE
++#if defined(__sun) && !defined(_XOPEN_SOURCE)
++#  if (__STDC_VERSION__-0 < 199901L)
++#define _XOPEN_SOURCE 500
++#  else
++#define _XOPEN_SOURCE 600
++#  endif
 +#endif
  #include <ctype.h>
  #include <errno.h>
