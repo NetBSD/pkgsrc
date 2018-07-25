e7d54e802e917c3c65ccb40d2aa8ee8ae8bb1476
jperkin <jperkin@pkgsrc.org>
2018-07-25 13:51:18 +0000

    haproxy: Fix SunOS C99 handling.

--- net/haproxy/patches/patch-src_haproxy.c
+++ net/haproxy/patches/patch-src_haproxy.c
@@ -1,15 +1,19 @@
-$NetBSD: patch-src_haproxy.c,v 1.1 2018/04/05 09:18:39 jperkin Exp $
+$NetBSD: patch-src_haproxy.c,v 1.2 2018/07/25 13:51:18 jperkin Exp $
 
 Special handling for XPG4_2 on SunOS.
 
---- src/haproxy.c.orig	2018-02-08 13:05:15.000000000 +0000
+--- src/haproxy.c.orig	2018-06-27 14:27:32.000000000 +0000
 +++ src/haproxy.c
-@@ -26,6 +26,9 @@
+@@ -26,6 +26,13 @@
   */
  
  #define _GNU_SOURCE
-+#if defined(__sun) && !defined(_XOPEN_SOURCE) && (__STDC_VERSION__-0 < 199901L)
-+#define _XOPEN_SOURCE
++#if defined(__sun) && !defined(_XOPEN_SOURCE)
++#  if (__STDC_VERSION__-0 < 199901L)
++#define _XOPEN_SOURCE 500
++#  else
++#define _XOPEN_SOURCE 600
++#  endif
 +#endif
  #include <stdio.h>
  #include <stdlib.h>
