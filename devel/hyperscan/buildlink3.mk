c657999f84431187683060c847ce1bfa0802caa6
minskim <minskim@pkgsrc.org>
2018-07-25 00:26:30 +0000

    devel/hyperscan: Update to 5.0.0
    New features:
    
    - A hybrid library of Hyperscan and PCRE called Chimera
    - Logical combination matching
    - Experimental support for Windows

--- devel/hyperscan/buildlink3.mk
+++ devel/hyperscan/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/05/07 22:18:53 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/07/25 00:26:30 minskim Exp $
 
 BUILDLINK_TREE+=	hyperscan
 
 .if !defined(HYPERSCAN_BUILDLINK3_MK)
 HYPERSCAN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hyperscan+=	hyperscan>=4.5.1
+BUILDLINK_API_DEPENDS.hyperscan+=	hyperscan>=5.0.0
 BUILDLINK_PKGSRCDIR.hyperscan?=		../../devel/hyperscan
 
 .endif	# HYPERSCAN_BUILDLINK3_MK
