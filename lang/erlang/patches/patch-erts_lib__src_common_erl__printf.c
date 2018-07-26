fde954766795e26eb1523499b1eaacc24d686cdf
jperkin <jperkin@pkgsrc.org>
2018-07-26 16:01:45 +0000

    erlang: Fix _XOPEN_SOURCE configuration.

--- lang/erlang/patches/patch-erts_lib__src_common_erl__printf.c
+++ lang/erlang/patches/patch-erts_lib__src_common_erl__printf.c
@@ -1,15 +1,19 @@
-$NetBSD: patch-erts_lib__src_common_erl__printf.c,v 1.1 2018/07/03 11:01:21 fhajny Exp $
+$NetBSD: patch-erts_lib__src_common_erl__printf.c,v 1.2 2018/07/26 16:01:45 jperkin Exp $
 
-Unlock putc_unlocked in stdio.h.
+Set _XOPEN_SOURCE correctly on SunOS.
 
 --- erts/lib_src/common/erl_printf.c.orig	2018-06-19 07:07:55.000000000 +0000
 +++ erts/lib_src/common/erl_printf.c
-@@ -27,6 +27,10 @@
+@@ -27,6 +27,14 @@
  #include "config.h"
  #endif
  
 +#ifdef __sun
++#if __STDC_VERSION__-0 < 199901L
 +#define _XOPEN_SOURCE 500
++#else
++#define _XOPEN_SOURCE 600
++#endif
 +#endif
 +
  #include <string.h>
