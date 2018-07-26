54e04307a6c1857032c182ae034ea052c53f22bf
abs <abs@pkgsrc.org>
2018-07-26 22:37:48 +0000

    Rather than playing "reserved define whack-a-mole" on NetBSD by trying to define _ISOC99_SOURCE to mitigate the damage of defining _XOPEN_SOURCE, just don't define _XOPEN_SOURCE - nothing in the source defines it and it breaks at least gcc 6.4 on NetBSD-8.0
    No PKGREVISION bump as should not affect any building systems

--- textproc/icu/patches/patch-common_uposixdefs.h
+++ textproc/icu/patches/patch-common_uposixdefs.h
@@ -1,14 +1,15 @@
-$NetBSD: patch-common_uposixdefs.h,v 1.5 2018/01/16 13:28:12 jperkin Exp $
+$NetBSD: patch-common_uposixdefs.h,v 1.6 2018/07/26 22:37:48 abs Exp $
 
 C99 and related define overrides.
+Do not play _XOPEN_SOURCE games on NetBSD, as it breaks at least gcc 6.4 on NetBSD-8.0
 
---- common/uposixdefs.h.orig	2017-03-14 21:01:57.000000000 +0000
+--- common/uposixdefs.h.orig	2018-06-21 09:39:23.000000000 +0000
 +++ common/uposixdefs.h
 @@ -44,8 +44,10 @@
       * and define _XOPEN_SOURCE to different values depending on __STDC_VERSION__.
       * In C++ source code (e.g., putil.cpp), __STDC_VERSION__ is not defined at all.
       */
-+#if !defined(_SCO_DS)
++#if !defined(_SCO_DS) && !defined(__NetBSD__)
  #   define _XOPEN_SOURCE 600
  #endif
 +#endif
@@ -27,11 +28,3 @@ C99 and related define overrides.
  
  /**
   * Solaris says:
-@@ -68,4 +72,7 @@
- #   define _STDC_C99
- #endif
- 
-+#if defined(__cplusplus) && defined(__NetBSD__)
-+#define _ISOC99_SOURCE
-+#endif
- #endif  /* __UPOSIXDEFS_H__ */
