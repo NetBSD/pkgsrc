43adb6de53cdc225c02c877eeccbdbac7bf8a37f
schmonz <schmonz@pkgsrc.org>
2018-07-27 07:39:12 +0000

    Fix textproc/highlight build with 'x11' option.

--- textproc/highlight/options.mk
+++ textproc/highlight/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2018/05/03 13:13:50 schmonz Exp $
+# $NetBSD: options.mk,v 1.3 2018/07/27 07:39:12 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.highlight
 PKG_SUPPORTED_OPTIONS=	x11
@@ -7,8 +7,10 @@ PKG_SUPPORTED_OPTIONS=	x11
 
 PLIST_VARS+=		x11
 .if !empty(PKG_OPTIONS:Mx11)
-.include "../../x11/qt5-qttools/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
 BUILD_TARGET+=		gui
 INSTALL_TARGET+=	install-gui
 PLIST.x11=		yes
+pre-configure:
+	${LN} -sf ${QTDIR}/bin/qmake ${TOOLS_DIR}/bin
 .endif
