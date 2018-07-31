6475bfe8d50c79749252fdcf76103da590cd9e56
maya <maya@pkgsrc.org>
2018-07-31 01:17:56 +0000

    move --disable-gcc-global-regs to Makefile.php. Seems to make a previously segfaulting netbsd-8/i386's build not segfault. ap-php runs PHP's configure and builds some of its code, so it needs the same flag.
    Now we can stop requiring an arbitrary GCC version. The test case in the
    GCC bugzilla fails on all GCC versions I tested, but magically some
    versions of GCC manage to build a working PHP.

--- lang/php72/Makefile.php
+++ lang/php72/Makefile.php
@@ -1,12 +1,13 @@
-# $NetBSD: Makefile.php,v 1.7 2018/07/30 07:17:16 manu Exp $
+# $NetBSD: Makefile.php,v 1.8 2018/07/31 01:17:56 maya Exp $
 # used by lang/php72/Makefile
 # used by www/ap-php/Makefile
 # used by www/php-fpm/Makefile
 
-# With --disable-gcc-global-regs, works around 
+# segfaults when buidling with many compilers
 # https://bugs.php.net/bug.php?id=74527
+# https://gcc.gnu.org/bugzilla/show_bug.cgi?id=86236
 .if ${MACHINE_ARCH} == "i386"
-GCC_REQD+=		6.4
+CONFIGURE_ARGS+=	--disable-gcc-global-regs
 .endif
 
 # the binary actually needs full dep on PCRE
