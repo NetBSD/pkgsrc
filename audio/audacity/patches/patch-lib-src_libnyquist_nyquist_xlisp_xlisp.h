44c3bd72cd377871386439f54b1ac9ea6c0cc564
jperkin <jperkin@pkgsrc.org>
2018-07-31 21:10:29 +0000

    audacity: SunOS build fixes.

--- audio/audacity/patches/patch-lib-src_libnyquist_nyquist_xlisp_xlisp.h
+++ audio/audacity/patches/patch-lib-src_libnyquist_nyquist_xlisp_xlisp.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-lib-src_libnyquist_nyquist_xlisp_xlisp.h,v 1.1 2015/04/21 01:33:39 rodent Exp $
+$NetBSD: patch-lib-src_libnyquist_nyquist_xlisp_xlisp.h,v 1.2 2018/07/31 21:10:29 jperkin Exp $
 
 Define endianness for {Free,Open,Net}BSD and Bitrig.
 
---- lib-src/libnyquist/nyquist/xlisp/xlisp.h.orig	2015-03-02 01:07:22.000000000 +0000
+--- lib-src/libnyquist/nyquist/xlisp/xlisp.h.orig	2016-01-13 14:31:18.000000000 +0000
 +++ lib-src/libnyquist/nyquist/xlisp/xlisp.h
-@@ -166,6 +166,20 @@ extern long ptrtoabs();
+@@ -151,6 +151,27 @@ extern long ptrtoabs();
  #else
  #define XL_BIG_ENDIAN
  #endif
@@ -21,6 +21,13 @@ Define endianness for {Free,Open,Net}BSD and Bitrig.
 +  #define XL_LITTLE_ENDIAN
 + #else
 +  #define XL_BIG_ENDIAN
++ #endif
++#elif defined(__sun)
++ #include <sys/byteorder.h>
++ #ifdef _BIG_ENDIAN
++  #define XL_BIG_ENDIAN
++ #else
++  #define XL_LITTLE_ENDIAN
 + #endif
  #endif
  
