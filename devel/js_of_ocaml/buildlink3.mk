25f5600d8297d137335f05b3bcac7b78339f2b71
jaapb <jaapb@pkgsrc.org>
2018-08-03 09:19:56 +0000

    Added dependency to camlp4 option for devel/js_of_ocaml
    Said package needs devel/ocaml-deriving-ocsigen to compile the entirety
    of its camlp4 stuff.

--- devel/js_of_ocaml/buildlink3.mk
+++ devel/js_of_ocaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2018/07/19 14:05:57 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2018/08/03 09:19:56 jaapb Exp $
 
 BUILDLINK_TREE+=	js_of_ocaml
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	js_of_ocaml
 JS_OF_OCAML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.js_of_ocaml+=	js_of_ocaml>=3.0.0
-BUILDLINK_ABI_DEPENDS.js_of_ocaml+=	js_of_ocaml>=3.2.0
+BUILDLINK_ABI_DEPENDS.js_of_ocaml+=	js_of_ocaml>=3.2.0nb1
 BUILDLINK_PKGSRCDIR.js_of_ocaml?=	../../devel/js_of_ocaml
 
 .endif	# JS_OF_OCAML_BUILDLINK3_MK
