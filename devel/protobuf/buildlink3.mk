b9fb78e13528615478223975ae1d9231c9486eb6
minskim <minskim@pkgsrc.org>
2018-08-04 21:19:39 +0000

    devel/protobuf: Update to 3.6.0
    Major changes:
    - Starting from this release, we now require C++11.
    - Moved to C++11 types like std::atomic and std::unique_ptr.

--- devel/protobuf/buildlink3.mk
+++ devel/protobuf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/01/17 12:10:37 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/08/04 21:19:39 minskim Exp $
 
 BUILDLINK_TREE+=	protobuf
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	protobuf
 PROTOBUF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.protobuf+=	protobuf>=3.0.0
-BUILDLINK_ABI_DEPENDS.protobuf?=		protobuf>=3.5.1nb1
+BUILDLINK_ABI_DEPENDS.protobuf?=	protobuf>=3.6.0
 BUILDLINK_PKGSRCDIR.protobuf?=		../../devel/protobuf
 
 .include "../../devel/zlib/buildlink3.mk"
