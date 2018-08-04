c1377ca5fdf514adebbbfa0a4dd07c199caa1e14
nia <nia@pkgsrc.org>
2018-08-04 04:04:28 +0000

    games/simgear: Update to simgear-2018.2.2.

--- games/simgear/buildlink3.mk
+++ games/simgear/buildlink3.mk
@@ -1,18 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.12 2015/04/25 14:23:03 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2018/08/04 04:04:28 nia Exp $
 
 BUILDLINK_TREE+=	simgear
 
 .if !defined(SIMGEAR_BUILDLINK3_MK)
 SIMGEAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.simgear+=	simgear>=2.0.0nb6
-BUILDLINK_ABI_DEPENDS.simgear+=	simgear>=2.8.0nb23
+BUILDLINK_API_DEPENDS.simgear+=	simgear>=2018.2.2
 BUILDLINK_PKGSRCDIR.simgear?=	../../games/simgear
-BUILDLINK_DEPMETHOD.simgear?=	build
 
+.include "../../audio/openal-soft/buildlink3.mk"
+.include "../../devel/boost-headers/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
-.include "../../games/plib/buildlink3.mk"
-.include "../../mk/jpeg.buildlink3.mk"
-.endif # SIMGEAR_BUILDLINK3_MK
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../graphics/osg/buildlink3.mk"
+.endif	# SIMGEAR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-simgear
