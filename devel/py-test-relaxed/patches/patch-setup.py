f338eca85a92ec9fa0828e1b5eee0b4f92d57227
adam <adam@pkgsrc.org>
2018-08-05 10:30:12 +0000

    py-test-relaxed: updated to 1.1.4
    1.1.4:
    - :support:- backported Add missing universal wheel indicator in setup
      metadata.
    
    1.1.3:
    - :bug:- Fix the @raises helper decorator so it actually raises an
      exception when the requested exception is not raised by the decorated
      function. That's definitely not a confusing sentence.

--- devel/py-test-relaxed/patches/patch-setup.py
+++ devel/py-test-relaxed/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.2 2018/04/18 07:18:21 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2018/08/05 10:30:12 adam Exp $
 
 Allow newer pytest.
 
---- setup.py.orig	2018-04-18 07:14:04.000000000 +0000
+--- setup.py.orig	2018-07-25 00:03:25.000000000 +0000
 +++ setup.py
-@@ -29,7 +29,7 @@ setup(
- 
+@@ -26,7 +26,7 @@ setup(
+     },
      install_requires=[
          # NOTE: pytest 3.3 broke something, not sure what yet
--        'pytest>=3,<3.3',
-+        'pytest>=3.4',
+-        "pytest>=3,<3.3",
++        "pytest>=3.4",
          # TODO: ditto; six is so widely used it's prob worth having a broad pin
-         'six>=1,<2',
+         "six>=1,<2",
          # TODO: ditto again!
