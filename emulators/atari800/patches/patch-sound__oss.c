6a21922003f236173513c3a2c986f17ad7760c1d
adam <adam@pkgsrc.org>
2018-08-05 21:27:04 +0000

    atari800: updated to 4.0.0
    Version 4.0.0:
    
    This release brings four years of hard work of fellow Atari800 developers
    on improvements and bugfixes of our favorite Atari emulator.
    
    Ports to Atari ST/TT/Falcon, Android, Raspberry Pi and Sega Dreamcast
    have been improved (some of them greatly).
    
    CPU, SIO, ANTIC, GTIA, POKEY and PIA emulation has been corrected.
    
    Atari800 now includes Altirra BIOS so it is now possible to run *some*
    programs even without installing the original ROM files.
    
    The total number of changes is so huge that major version bump was necessary.
    
    Contrary to usual NEWS entries here follows a very incomplete list. It might
    be updated in later releases. In the meantime please read DOC/ChangeLog
    and the commit history in git for complete list of changes.
    
    This release contains breaking changes in color handling. Users updating from
    an earlier version should reset their color settings, or else the display
    might be unreadable. To reset the color settings, do one of these:
     * Select one of the presets available in the menu option "Display settings"->
       "Color preset"; or
     * Run atari800 with the -color-preset command-line option, e.g.:
       atari800 -colors-preset standard; or
     * Delete the emulator's config file (.atari800.cfg). Caution: you'll lose
       all Atari800 settings!
    
    New features:
    -------------
     * Support for loading of CAS images with "fsk" chunks - images of
       copy-protected tapes can now be loaded, with SIO patch being disabled.
     * Bit3 Full View 80 Column card emulation.
     * New cartridge type 68: "Atrax 128 KB cartridge".
       Previously existing cartridge type 17: "Atrax 128 KB cartridge" was based
       on a misconception - real Atrax cartridges have their address and data
       lines intermixed, so type 17 could not be used with actual ROM dumps. So,
       type 17 has been renamed to "Decoded Atrax 128 KB cartridge", and new type
       68 has been added, whcih now can be used for Atrax ROM chip dumps.
     * New cartridge types supported:
       - aDawliah 32 KB cartridge
       - aDawliah 64 KB cartridge
    
     * new command line switch "-volume" (for 16bit sound output) that can set
       the output volume of the Atari 800 emulator with value from 0 to 100.
    
    Changes:
    --------
     * The Sound Settings option "Fragment size" has been renamed to less cryptic
       "Hardware buffer size".
     * Total emulator volume is lower now because the output has been shifted
       in order to fix a possible annoying humming sound in silence on some
       receivers (TV sets).
    
    Fixes:
    ------
     * Fixed computation of gamma adjustment - now it is applied to each of the
       three RGB channels separately.
     * On systems that support synchronized sound: Fixed the emulator crashing
       when Dual POKEY was enabled while High Fidelity POKEY was turned off.
     * Improve screen update routines in the Dreamcast port. They don't use
       DIRTYRECT anymore but are faster than the old routines when the whole
       screen is dirty.

--- emulators/atari800/patches/patch-sound__oss.c
+++ emulators/atari800/patches/patch-sound__oss.c
@@ -1,14 +1,18 @@
-$NetBSD: patch-sound__oss.c,v 1.1 2017/12/14 22:12:19 adam Exp $
+$NetBSD: patch-sound__oss.c,v 1.2 2018/08/05 21:27:04 adam Exp $
 
 Do not hard-code sound device.
 
---- sound_oss.c.orig	2013-12-15 23:17:45.000000000 +0000
+--- sound_oss.c.orig	2018-04-21 17:02:14.000000000 +0000
 +++ sound_oss.c
-@@ -36,7 +36,7 @@
+@@ -36,11 +36,7 @@
  
  #define DEBUG 0
  
+-#if defined (__NetBSD__) || defined (__OpenBSD__)
+-static const char * const dspname = "/dev/audio";
+-#else
 -static const char * const dspname = "/dev/dsp";
+-#endif
 +static const char * const dspname = DEVOSSAUDIO;
  
  static int dsp_fd;
