b128fceda9a4639145c38fe6d540a74148f34761
jperkin <jperkin@pkgsrc.org>
2018-08-06 11:12:27 +0000

    osg: SunOS fixes and remove obsolete patches.

--- graphics/osg/patches/patch-src_osgPlugins_osc_osc_OscHostEndianness.h
+++ graphics/osg/patches/patch-src_osgPlugins_osc_osc_OscHostEndianness.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_osgPlugins_osc_osc_OscHostEndianness.h,v 1.1 2018/08/04 01:05:10 nia Exp $
+$NetBSD: patch-src_osgPlugins_osc_osc_OscHostEndianness.h,v 1.2 2018/08/06 11:12:27 jperkin Exp $
 
 Configure endianness for osc plug-in.
 
---- src/osgPlugins/osc/osc/OscHostEndianness.h.orig	2015-07-23 11:14:00.000000000 +0000
+--- src/osgPlugins/osc/osc/OscHostEndianness.h.orig	2017-08-28 16:50:49.000000000 +0000
 +++ src/osgPlugins/osc/osc/OscHostEndianness.h
 @@ -64,7 +64,7 @@
          #else
@@ -13,3 +13,21 @@ Configure endianness for osc plug-in.
          #include <sys/endian.h>
          #if (_BYTE_ORDER == _LITTLE_ENDIAN)
              #ifndef __LITTLE_ENDIAN__
+@@ -77,6 +77,17 @@
+         #else
+             #error Unknown machine endianness detected.
+         #endif
++    #elif defined(__sun)
++        #include <sys/byteorder.h>
++        #if defined(_BIG_ENDIAN)
++            #ifndef __BIG_ENDIAN__
++                #define __BIG_ENDIAN__
++            #endif
++        #else
++            #ifndef __LITTLE_ENDIAN__
++                #define __LITTLE_ENDIAN__
++            #endif
++        #endif
+     #endif
+ 
+     #if   defined(__LITTLE_ENDIAN__)
