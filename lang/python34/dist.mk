d8f96fba3203d006d7468e40c338857f217fd9f4
adam <adam@pkgsrc.org>
2018-08-07 08:44:41 +0000

    python34: updated to 3.4.9
    3.4.9:
    Bug fix release for Python 3.4.

--- lang/python34/dist.mk
+++ lang/python34/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.9 2018/02/05 17:35:12 adam Exp $
+# $NetBSD: dist.mk,v 1.10 2018/08/07 08:44:41 adam Exp $
 
-PY_DISTVERSION=	3.4.8
+PY_DISTVERSION=	3.4.9
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python34/distinfo
