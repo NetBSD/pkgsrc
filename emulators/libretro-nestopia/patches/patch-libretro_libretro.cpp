f85e5276a2793b3ac683e0ac27c38c770cf3d62b
nia <nia@pkgsrc.org>
2018-08-11 20:26:12 +0000

    libretro-nestopia: update to 1.49.
    Changes:
    
    ----------------------------------------------------------------
    1.49
    ----------------------------------------------------------------
    libretro:
    
     Additions:
      - Add support for multiline cheats and raw cheats (iLag)
      - Add adapter autoselect using NstDatabase.xml (hunterk)
      - Famicom Mic support
      - Cheevos ram access support (meepingsnesroms)
      - Add .wav sample loading
    
     Fixes:
      - Fixed heap corruption bug with crosshair (Arto Vainiolehto)
      - Fixed black screen when non-existent custom palette is selected
      - Fixed crosshair and overscan with blargg filter
    
    Core:
    
     Additions:
      - Added support for overclocking (meepingsnesroms)
    
     Fixes:
      - Fixed NSF and FDS in Dendy mode (Eugene.S)
      - Fixed coding mistake in PPU (zeromus)
      - Modified submappers for VRC2/VRC4 games (GeneralFailer)
      - Fixed compilation error in SetRamPowerState (Arto Vainiolehto)
      - Fix for McAcc games (joepogo)
    
    ----------------------------------------------------------------
    1.48
    ----------------------------------------------------------------
    libretro:
    
     Additions:
      - Added ability to load custom palettes
      - Added many palettes (Monroe88)
      - Added monochrome blargg NTSC filter (dalter)
      - Added Turbo A/B buttons
    
    Core:
    
     Additions:
      - RAM Power-on State setting
      - Support for mapper 23.10
    
     Fixes:
      - VRC2 Mirroring bug (koitsu, lidnariq)
      - Dendy timing and audio fixes (FHorse, Eugene.S)
      - Removed unused code in APU
      - Properly initialize RAM - fixes F-1 Race (koitsu)
      - Silenced many build warnings (orbea)
    
    ----------------------------------------------------------------
    1.47
    ----------------------------------------------------------------
    libretro:
    
     Additions:
      - Game Genie Sound Distortion option
      - Region selection core option
    
     Changes:
      - Default to "consumer" palette
      - Vertical and Horizontal overscan options separated
    
     Fixes:
      - Fixed FDS save path
    
    Core:
    
     Additions:
      - Added support for MC-ACC (perilsensitive)
      - Added NstDatabase.xml entries (clobber)
      - Added support for NES 2.0 submappers
      - Added support for mapper 4.1
      - Added support for mapper 4.3
      - Added support for mapper 23.15
      - Added support for mapper 25.15
      - Added support for mapper 32.1
      - Added support for mapper 68.1
      - Added support for mapper 71.1
      - Added support for mapper 78.1
      - Added support for mapper 78.3
    
     Fixes:
      - Reverted fix for Mapper 79
      - Fixed Burai Fighter status area (perilsensitive)
      - FME-7 IRQ Fix (lidnariq)
      - Disabled buggy audio sync
    
    "fine with me" snj

--- emulators/libretro-nestopia/patches/patch-libretro_libretro.cpp
+++ emulators/libretro-nestopia/patches/patch-libretro_libretro.cpp
@@ -1,17 +1,17 @@
-$NetBSD: patch-libretro_libretro.cpp,v 1.1 2015/04/10 03:16:18 snj Exp $
+$NetBSD: patch-libretro_libretro.cpp,v 1.2 2018/08/11 20:26:12 nia Exp $
 
 retroarch normally expects to find the NstDatabase.xml file in
 ~/.config/retroarch/bios, but there's no real reason to be editing it.
 Rather than have users manually copy the file into place, we install it
 system-wide and look for it there instead.
 
---- libretro/libretro.cpp.orig	2015-03-19 19:54:18.000000000 -0700
-+++ libretro/libretro.cpp	2015-03-29 23:31:42.000000000 -0700
-@@ -624,7 +624,7 @@ bool retro_load_game(const struct retro_
-    if (!environ_cb(RETRO_ENVIRONMENT_GET_OVERSCAN, &use_overscan))
-       use_overscan = true;
- 
--   snprintf(db_path, sizeof(db_path), "%s%cNstDatabase.xml", dir, slash);
+--- libretro/libretro.cpp.orig	2018-06-24 23:26:58.000000000 +0000
++++ libretro/libretro.cpp
+@@ -1073,7 +1073,7 @@ bool retro_load_game(const struct retro_
+    }
+    delete custompalette;
+    
+-   sprintf(db_path, "%s%cNstDatabase.xml", dir, slash);
 +   snprintf(db_path, sizeof(db_path), "%s/share/libretro-nestopia/NstDatabase.xml", PREFIX);
  
     if (log_cb)
