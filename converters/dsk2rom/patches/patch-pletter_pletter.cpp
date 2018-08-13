bbf40287006f9816a43923790e24ef8b314423a2
nia <nia@pkgsrc.org>
2018-08-13 15:53:49 +0000

    dsk2rom: add version 0.80.
    DSK2ROM is a utility for converting MSX .dsk images to MSX .rom images.

---
+++ converters/dsk2rom/patches/patch-pletter_pletter.cpp
@@ -0,0 +1,16 @@
+$NetBSD: patch-pletter_pletter.cpp,v 1.1 2018/08/13 15:53:49 nia Exp $
+
+Add missing includes.
+
+--- pletter/pletter.cpp.orig	2007-05-27 11:18:42.000000000 +0000
++++ pletter/pletter.cpp
+@@ -9,6 +9,9 @@
+ 
+ */
+ 
++#include <cstdlib>
++#include <cstring>
++#include <cstdio>
+ #include <string>
+ using namespace std;
+ 
