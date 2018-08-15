06d88b96f25a6ab6c7f855c93873e488617235a7
fhajny <fhajny@pkgsrc.org>
2018-08-15 13:08:21 +0000

    sysutils/syslog-ng: Update to 3.17.2.
    3.17.2
    ======
    
    ## Bugfixes
    
    - Fix a bug in flow-control
    - Fix template function evaluation in debugger
    
    3.17.1
    ======
    
    ## Features
    
    - Client side failback mode
    - New linux-audit() source as SCL
    - Decorating generated configuration
    - Introduce ewmm() source
    - Add parsing of Cisco unified call manager
    - Mandatory parameters for cfg-block (SCL)
    
    ## Bugfixes
    
    - dqtool cat print backlog items
    - Rewind backlog in case of stateless LogProtoClient
    - Filter out incorrectly parsed sudo logs
    - Minor fixes related to client-lib-dir, loggen and eventlog
    - Minor stats-query fixes and refactor
    - Reliable disk buffer non-empty backlog
    - Fix pip package versions on older distro releases (dbld)
    - Fix a groupset/groupunset and map-value-pairs() crash
    - Make g_atomic_counter_set() atomic and update minimum glib version
      to 2.26
    - Aligning java related SCLs with mandatory parameters
    - Loggen minor fixes
    - grab-logging should be installed as a header
    - Fix possible underflow of memory_usage (afsql, logqueue-fifo)
    - Fix SELinux module version inconsistency
    - Fix CMake unit test compilation (no-pie)
    - Fix possible crash in syslog-parser()
    - Disable ack for mark mode
    - Fixing a Telegram destination bug with bot id
    - All drivers should support inner destination or source plugins
    - Fix default file and directory creation ownership
    - Fix global "center;;received" counter when systemd-journal() is used
    - Link everything to libsecret-storage
    - Inform about the right dns-cache() configuration (warning message
      typo)
    - Adjusting window size for internal mark mode
    - Fix memory leaks in disk-buffer()
    - Fix undefined behavior in log multiplexer
    - Fix static linking mode (autotools)
    - Fix internal mark mode infinite loop with old ivykis
    - Fix missing normalize flags
    - Keep JVM running on reload if once configured
    - Fix a race condition (suspend) in LogSource
    - Add `@requires json-plugin` to the cim() parser
    - Added exclude_kmsg option to system source
    - Fix padding template function
    - Leak & invalid memory access
    - FreeBSD 11.2 builderror SOCK_STREAM
    - Add ref-counted TLSVerifier struct (use after free fix)
    
    ## Other changes
    
    - Improve loggen's file message parser
    - syslog-ng-debun improvements
    - Goodbye "goto relex" (refactor)
    - Refactor the callback registration mechanism of WildcardFileReader
    - Extended Linux capabilities detection (pkg-config)
    - Add atomic gssize
    - Lower the message level of `@requires` to debug
    - macOS warning elimination
    - Remove a misleading rewrite-related debug message
    - Minor updates to SELinux policy installer script
    - More robust GLib detection (CMake)
    - Logthreaded nonfunctional changes
    - Confgen and pragma improvements
    - Flush before stopping syslog-ng (functional tests)
    - Port unit tests into criterion (test_filters_netmask6, test_findeom,
      csv_parser, patternDB)
    - Libtest refactors
    - Add missing files to the source tarball
    - Better python binary detection

--- sysutils/syslog-ng/patches/patch-modules_system-source_system-source.c
+++ sysutils/syslog-ng/patches/patch-modules_system-source_system-source.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-modules_system-source_system-source.c,v 1.1 2015/10/29 16:34:13 fhajny Exp $
+$NetBSD: patch-modules_system-source_system-source.c,v 1.2 2018/08/15 13:08:21 fhajny Exp $
 
 Add basic NetBSD system() definition.
 
---- modules/system-source/system-source.c.orig	2015-10-27 08:08:53.000000000 +0000
+--- modules/system-source/system-source.c.orig	2018-08-10 10:28:08.000000000 +0000
 +++ modules/system-source/system-source.c
-@@ -294,6 +294,10 @@ system_generate_system_transports(GStrin
-       system_sysblock_add_unix_dgram(sysblock, "/var/run/log", NULL, NULL);
-       system_sysblock_add_freebsd_klog(sysblock, u.release);
+@@ -295,6 +295,10 @@ system_generate_system_transports(GStrin
+       if (!exclude_kernel_messages)
+         system_sysblock_add_freebsd_klog(sysblock, u.release);
      }
 +  else if (strcmp(u.sysname, "NetBSD") == 0)
 +    {
