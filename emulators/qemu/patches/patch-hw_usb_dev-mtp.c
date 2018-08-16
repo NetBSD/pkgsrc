be92d6e1fc23dfd156641d57e82a6beb201b769b
adam <adam@pkgsrc.org>
2018-08-16 10:15:09 +0000

    qemu: updated to 3.0.0
    3.0.0:
    
    Incompatible changes
    Option "-no-kvm-pit-reinjection" has been removed. Use "-global kvm-pit.lost_tick_policy=discard" instead.
    Option "-no-kvm-irqchip" has been removed. Use "-machine kernel_irqchip=off" instead.
    The "vlan" parameter of the "-net" option has been removed. Use "-netdev" with IDs or the "hubport" netdev instead.
    The deprecated -drive options "geometry", "addr" and "serial" have been removed. Use "-drive if=none" and pass the options to "-device" instead.
    The "xlnx-ep108" machine has been replaced by the "xlnx-zcu102" machine.
    The qemu-img "convert -s snapshot_id_or_name" argument has been removed. Use "convert -l snapshot_param" instead.
    PPC support for explicitly allocated RMAs has been removed (kernel support removed in 2015).
    The default for VFIO's "display" option has been changed from "auto" to "off", because "auto" requires an OpenGL-compatible configuration for vGPUs that require GL support.
    The deprecated xlnx-ep108 machine was removed.
    Some 32-bit guests on the "virt" machine may need "-machine highmem=off" if they are unable to support LPAE and are confused by the PCI ECAM now being above the 4GB mark in the address space
    
    New deprecated options and features
    The "arch" output member of the "query-cpus-fast" command is deprecated. Use the "target" output member instead.
    Option "-virtioconsole" is deprecated, use "-device virtconsole" instead.
    The non-functional "-clock" option will be removed soon.
    The "pc-0.10" and "pc-0.11" machines are now deprecated. Use newer machine types instead.
    Option "-enable-hax" is deprecated. Use "-accel hax" instead.
    The "arch" memeber in the reply to query-cpus-fast is deprecated, use "target" instead.
    The "file" driver for drives is no longer appropriate for character or host devices and will only accept regular files (S_IFREG). Use host_cdrom or host_device instead.
    Consult the "Deprecated Features" appendix for the full list of historically deprecated features/options.
    
    Future incompatible changes
    Three options are using different names on the command line and in configuration file. In particular:
    The "acpi" configuration file section matches command-line option "acpitable";
    The "boot-opts" configuration file section matches command-line option "boot";
    The "smp-opts" configuration file section matches command-line option "smp".
    -readconfig will standardize on the name for the command line option.
    Behavior of automatic calculation of SMP topology when some SMP topology options for -smp are omitted (sockets, cores, threads) will change in the future. If guest ABI needs to be preserved on upgrades while using the SMP topology options, users should either set set all options explicitly (sockets, cores, threads), or omit all of them.
    Devices "allwinner-a10", "pc87312", "ssi-sd" will be configured with explicit properties instead of implicitly. This is unlikely to affect users.
    For x86, specifying a CPUID feature with both "+feature/-feature" and "feature=on/off" will cause a warning. The current behavior for this combination ("+feature/-feature" wins over "feature=on/off") will be changed so that "+feature" and "-feature" will be synonyms for "feature=on" and "feature=off" respectively).
    The read-only block drivers "bochs", "cloop" and "dmg" as well as "rbd" and "vvfat" in certain read-only configurations will no longer enable read-only mode automatically. It will be necessary to specify "read-only=on" explicitly on the command line and in QMP commands for the setup to keep working; the default "read-only=off" setting will result in an error.
    On s390x, using KVM with a Linux host kernel version < 3.15 has been broken since QEMU version 2.10. This will not be fixed unless a need is communicated (otherwise the code will be removed in the near future, so that you need at least Linux kernel version 3.15 on the host to run KVM on System z)

--- emulators/qemu/patches/patch-hw_usb_dev-mtp.c
+++ emulators/qemu/patches/patch-hw_usb_dev-mtp.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-hw_usb_dev-mtp.c,v 1.1 2017/08/25 12:39:56 jperkin Exp $
+$NetBSD: patch-hw_usb_dev-mtp.c,v 1.2 2018/08/16 10:15:09 adam Exp $
 
 Support NAME_MAX.
 
---- hw/usb/dev-mtp.c.orig	2017-04-20 14:57:00.000000000 +0000
+--- hw/usb/dev-mtp.c.orig	2018-08-14 19:10:34.000000000 +0000
 +++ hw/usb/dev-mtp.c
 @@ -26,6 +26,10 @@
  #include "hw/usb.h"
- #include "hw/usb/desc.h"
+ #include "desc.h"
  
 +#ifndef NAME_MAX
 +#define NAME_MAX 255
