e58b44c7d0e3e6a5ff2790346f2c790a8be19b1e
nia <nia@pkgsrc.org>
2018-08-16 13:25:27 +0000

    Remove 'dynarec' option and enable it by default instead.
    Discussed with wiz.

--- emulators/libretro-mupen64plus/options.mk
+++ emulators/libretro-mupen64plus/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2018/08/14 13:08:57 nia Exp $
+# $NetBSD: options.mk,v 1.8 2018/08/16 13:25:27 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.libretro-mupen64plus
 PKG_OPTIONS_REQUIRED_GROUPS=	graphics
@@ -8,17 +8,10 @@ PKG_OPTIONS_GROUP.graphics=	opengl
 
 .if !empty(MACHINE_ARCH:M*arm*)
 PKG_OPTIONS_GROUP.graphics+=	rpi
-PKG_SUPPORTED_OPTIONS+=		dynarec
-PKG_SUGGESTED_OPTIONS+=		rpi dynarec
-MUPEN64_DYNAREC_ARCH=		arm
-.elif ${MACHINE_ARCH} == "i386"
-PKG_SUPPORTED_OPTIONS+=		dynarec
-PKG_SUGGESTED_OPTIONS+=		dynarec opengl
-MUPEN64_DYNAREC_ARCH=		x86
-.elif ${MACHINE_ARCH} == "x86_64"
-PKG_SUPPORTED_OPTIONS+=		dynarec
-PKG_SUGGESTED_OPTIONS+=		dynarec opengl
-MUPEN64_DYNAREC_ARCH=		x86_64
+.endif
+
+.if !empty(MACHINE_ARCH:Mearm*) && ${OPSYS} == "NetBSD"
+PKG_SUGGESTED_OPTIONS+=		rpi
 .else
 PKG_SUGGESTED_OPTIONS+=		opengl
 .endif
@@ -27,15 +20,7 @@ PKG_SUGGESTED_OPTIONS+=		opengl
 
 .if !empty(PKG_OPTIONS:Mopengl)
 .include "../../graphics/MesaLib/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mrpi)
+.elif !empty(PKG_OPTIONS:Mrpi)
 MAKE_ENV+=	platform=rpi
 .include "../../misc/raspberrypi-userland/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mdynarec)
-MAKE_ENV+=	WITH_DYNAREC=${MUPEN64_DYNAREC_ARCH}
-.else
-MAKE_ENV+=	WITH_DYNAREC=
 .endif
