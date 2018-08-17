e02ed0aa3a3c658d8942b79130df57e0ca086f7d
wiz <wiz@pkgsrc.org>
2018-08-17 12:36:05 +0000

    tigervnc: update to 1.9.0.
    (Still using xorg-server 1.17, up to 1.20 is supported.)
    
    The new TigerVNC 1.9.0 is now available. Lots of changes have been made
    since the last release, but the highlights are:
    
       - Alternative, "raw" keyboard mode in the native client and all servers
       - CapsLock/NumLock/ScrollLock synchronisation in the native client and all servers
       - Automatic "repair" of JPEG artefacts on screen in all servers
       - Support for UNIX sockets in the native client and in the UNIX servers
       - Both clients now warn when sending the password over a possibly insecure channel
       - Performance improvements in the Java client
       - The Java client now requires Java 7
       - Improved high latency handling in all servers
       - Better keyboard handling in the native client on Windows
       - Slightly better keyboard handling in x0vncserver
       - x0vncserver now supports cursors and screen resize
       - Xorg 1.20 can now be used as a base for Xvnc/libvnc.so

--- net/tigervnc/patches/patch-unix_xserver_hw_vnc_vncModule.c
+++ net/tigervnc/patches/patch-unix_xserver_hw_vnc_vncModule.c
@@ -1,17 +1,17 @@
-$NetBSD: patch-unix_xserver_hw_vnc_vncModule.c,v 1.1 2016/01/17 22:01:20 joerg Exp $
+$NetBSD: patch-unix_xserver_hw_vnc_vncModule.c,v 1.2 2018/08/17 12:36:05 wiz Exp $
 
---- unix/xserver/hw/vnc/vncModule.c.orig	2016-01-17 00:14:36.000000000 +0000
+--- unix/xserver/hw/vnc/vncModule.c.orig	2018-07-16 14:08:55.000000000 +0000
 +++ unix/xserver/hw/vnc/vncModule.c
-@@ -40,7 +40,7 @@ typedef pointer XF86OptionPtr;
- #include "RFBGlue.h"
+@@ -39,7 +39,7 @@ typedef pointer XF86OptionPtr;
  #include "XorgGlue.h"
+ #include "RandrGlue.h"
  
 -static void vncModuleInit(INITARGS);
 +static void vncModuleInit(void);
  
  static MODULESETUPPROTO(vncSetup);
  
-@@ -82,7 +82,7 @@ vncSetup(void * module, void * opts, int
+@@ -81,7 +81,7 @@ vncSetup(void * module, void * opts, int
      return (void *)1;
  }
  
