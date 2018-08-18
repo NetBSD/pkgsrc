215ec91e2daf627e3e9f36cc3472a69ee79e7442
ryoon <ryoon@pkgsrc.org>
2018-08-18 00:11:32 +0000

    Update to 6.1.0.3
    Changelog:
    Many new features including:
    * EPUB export of Writer
    * vlookup performance improvement of Calc
    * New drawing style of Impress and Draw
    * Embedded HSQLDB deprecation of Base

--- misc/libreoffice/patches/patch-external_harfbuzz_UnpackedTarball__harfbuzz.mk
+++ misc/libreoffice/patches/patch-external_harfbuzz_UnpackedTarball__harfbuzz.mk
@@ -1,15 +1,14 @@
-$NetBSD: patch-external_harfbuzz_UnpackedTarball__harfbuzz.mk,v 1.3 2018/06/25 13:34:48 ryoon Exp $
+$NetBSD: patch-external_harfbuzz_UnpackedTarball__harfbuzz.mk,v 1.4 2018/08/18 00:11:32 ryoon Exp $
 
---- external/harfbuzz/UnpackedTarball_harfbuzz.mk.orig	2018-06-19 22:27:30.000000000 +0000
+--- external/harfbuzz/UnpackedTarball_harfbuzz.mk.orig	2018-08-02 19:54:54.000000000 +0000
 +++ external/harfbuzz/UnpackedTarball_harfbuzz.mk
-@@ -15,12 +15,6 @@ $(eval $(call gb_UnpackedTarball_update_
+@@ -15,11 +15,6 @@ $(eval $(call gb_UnpackedTarball_update_
  
  $(eval $(call gb_UnpackedTarball_set_patchlevel,harfbuzz,0))
  
 -$(eval $(call gb_UnpackedTarball_add_patches,harfbuzz, \
 -    external/harfbuzz/clang-cl.patch \
 -    external/harfbuzz/ubsan.patch \
--	external/harfbuzz/revert-make-inert.patch \
 -))
 -
  ifneq ($(ENABLE_RUNTIME_OPTIMIZATIONS),TRUE)
--- misc/libreoffice/patches/patch-mysqlc_Library__mysqlc.mk
+++ misc/libreoffice/patches/patch-mysqlc_Library__mysqlc.mk
@@ -1,6 +1,6 @@
-$NetBSD: patch-mysqlc_Library__mysqlc.mk,v 1.2 2018/02/03 00:39:50 ryoon Exp $
+$NetBSD: patch-mysqlc_Library__mysqlc.mk,v 1.3 2018/08/18 00:11:32 ryoon Exp $
 
---- mysqlc/Library_mysqlc.mk.orig	2018-01-24 20:31:03.000000000 +0000
+--- mysqlc/Library_mysqlc.mk.orig	2018-08-02 19:54:54.000000000 +0000
 +++ mysqlc/Library_mysqlc.mk
 @@ -16,7 +16,7 @@ $(eval $(call gb_Library_use_externals,m
  
@@ -8,6 +8,6 @@ $NetBSD: patch-mysqlc_Library__mysqlc.mk,v 1.2 2018/02/03 00:39:50 ryoon Exp $
  $(eval $(call gb_Library_add_libs,mysqlc,\
 -	$(if $(filter-out WNT,$(OS)),$(if $(filter MACOSX SOLARIS,$(OS)),-lz -lm,\
 +	$(if $(filter-out WNT,$(OS)),$(if $(filter NETBSD MACOSX SOLARIS,$(OS)),-lz -lm,\
- 	-rdynamic -lz -lcrypt -lnsl -lm)) \
+ 	-rdynamic -lz -lcrypt -lm)) \
  ))
  endif
