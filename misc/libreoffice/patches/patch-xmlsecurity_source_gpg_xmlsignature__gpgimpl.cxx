cb38763b27fc36704f2c5455d365d74cf48f33c6
ryoon <ryoon@pkgsrc.org>
2018-08-18 00:11:32 +0000

    Update to 6.1.0.3
    Changelog:
    Many new features including:
    * EPUB export of Writer
    * vlookup performance improvement of Calc
    * New drawing style of Impress and Draw
    * Embedded HSQLDB deprecation of Base

--- misc/libreoffice/patches/patch-xmlsecurity_source_gpg_xmlsignature__gpgimpl.cxx
+++ misc/libreoffice/patches/patch-xmlsecurity_source_gpg_xmlsignature__gpgimpl.cxx
@@ -1,12 +1,12 @@
-$NetBSD: patch-xmlsecurity_source_gpg_xmlsignature__gpgimpl.cxx,v 1.2 2018/02/26 18:04:31 ryoon Exp $
+$NetBSD: patch-xmlsecurity_source_gpg_xmlsignature__gpgimpl.cxx,v 1.3 2018/08/18 00:11:32 ryoon Exp $
 
 * Do not confuse with key.h from gpgmepp and nss
 
---- xmlsecurity/source/gpg/xmlsignature_gpgimpl.cxx.orig	2017-07-25 20:26:39.000000000 +0000
+--- xmlsecurity/source/gpg/xmlsignature_gpgimpl.cxx.orig	2018-08-02 19:54:54.000000000 +0000
 +++ xmlsecurity/source/gpg/xmlsignature_gpgimpl.cxx
-@@ -23,7 +23,7 @@
- 
- #include <gpgme.h>
+@@ -33,7 +33,7 @@
+ #pragma clang diagnostic pop
+ #endif
  #include <context.h>
 -#include <key.h>
 +#include <gpgme++/key.h>
