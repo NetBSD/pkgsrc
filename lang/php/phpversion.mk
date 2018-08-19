54face64c4f0282b1db2e5ad18fc540310c0f874
taca <taca@pkgsrc.org>
2018-08-19 13:56:59 +0000

    lang/php72: update to 7.2.9
    16 Aug 2018, PHP 7.2.9
    
    - Calendar:
      . Fixed bug #52974 (jewish.c: compile error under Windows with GBK charset).
        (cmb)
    
    - Filter:
      . Fixed bug #76366 (References in sub-array for filtering breaks the filter).
        (ZiHang Gao)
    
    - PDO_Firebird:
      . Fixed bug #76488 (Memory leak when fetching a BLOB field). (Simonov Denis)
    
    - PDO_PgSQL:
      . Fixed bug #75402 (Possible Memory Leak using PDO::CURSOR_SCROLL option).
        (Anatol)
    
    - SQLite3:
      . Fixed #76665 (SQLite3Stmt::bindValue() with SQLITE3_FLOAT doesn't juggle).
        (cmb)
    
    - Standard:
      . Fixed bug #73817 (Incorrect entries in get_html_translation_table). (cmb)
      . Fixed bug #68553 (array_column: null values in $index_key become incrementing
        keys in result). (Laruence)
      . Fixed bug #76643 (Segmentation fault when using `output_add_rewrite_var`).
        (cmb)
    
    - Zip:
      . Fixed bug #76524 (ZipArchive memory leak (OVERWRITE flag and empty archive)).
        (Timur Ibragimov)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.226 2018/08/19 13:55:24 taca Exp $
+# $NetBSD: phpversion.mk,v 1.227 2018/08/19 13:56:59 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.37
 PHP70_VERSION=	7.0.31
 PHP71_VERSION=	7.1.21
-PHP72_VERSION=	7.2.8
+PHP72_VERSION=	7.2.9
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
