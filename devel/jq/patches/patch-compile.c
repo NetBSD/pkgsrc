55581dce9b100339391678e55074f33d060573e6
leot <leot@pkgsrc.org>
2018-08-19 18:08:05 +0000

    jq: Avoid SIGSEGVs when using date builtins
    Apply patch written by <soda> and shared with upstream as part of
    <https://github.com/stedolan/jq/pull/1458> in order to fix crashes of jq when
    using date builtin functions.
    
    Also add a SUBST class intended to avoid invoking git as part of pre-configure
    phase when invoking autoreconf.
    
    While here take the MAINTAINERship and reformat a bit the Makefile
    (NFCI).
    
    Thanks a lot to <soda>, Takehiko NOZAKI and <nonaka> for investigating
    the issue, writing the patches, sharing them with upstream and as
    part of PR pkg/52460!
    
    Bump PKGREVISION

---
+++ devel/jq/patches/patch-compile.c
@@ -0,0 +1,22 @@
+$NetBSD: patch-compile.c,v 1.1 2018/08/19 18:08:05 leot Exp $
+
+Defining _GNU_SOURCE, _BSD_SOURCE etc. in C sources is problematic,
+because the result of the configure command may be inconsistent with it.
+to be consistent, such macros have to be defined in the early stage of
+the configure command, and the AC_USE_SYSTEM_EXTENSIONS macro does the job.
+
+Part of pull request 1458, commit id `df9a0963f8fa6fca773b059dce22c598152f3edb':
+
+ <https://github.com/stedolan/jq/pull/1458>
+
+Also shared via PR pkg/52460.
+
+--- compile.c.orig	2015-08-18 04:25:04.000000000 +0000
++++ compile.c
+@@ -1,6 +1,3 @@
+-#ifndef _GNU_SOURCE
+-#define _GNU_SOURCE // for strdup
+-#endif
+ #include <assert.h>
+ #include <math.h>
+ #include <string.h>
