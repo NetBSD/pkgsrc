bfeed3aba163b410c53b17e028ca485ff942cbd1
adam <adam@pkgsrc.org>
2018-08-20 07:31:51 +0000

    py-curl: only use distutils (used to import setuptools when wheels were installed)

--- www/py-curl/patches/patch-setup.py
+++ www/py-curl/patches/patch-setup.py
@@ -1,10 +1,25 @@
-$NetBSD: patch-setup.py,v 1.1 2017/04/05 17:56:18 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2018/08/20 07:31:51 adam Exp $
 
+Stick to distutils.
 Add multi Python variant support.
 
---- setup.py.orig	2016-02-04 04:39:06.000000000 +0000
+--- setup.py.orig	2018-06-02 04:59:02.000000000 +0000
 +++ setup.py
-@@ -520,9 +520,9 @@ def get_data_files():
+@@ -10,12 +10,7 @@ VERSION = "7.43.0.2"
+ 
+ import glob, os, re, sys, subprocess
+ import distutils
+-try:
+-    import wheel
+-    if wheel:
+-        from setuptools import setup
+-except ImportError:
+-    from distutils.core import setup
++from distutils.core import setup
+ from distutils.extension import Extension
+ from distutils.util import split_quoted
+ from distutils.version import LooseVersion
+@@ -605,9 +600,9 @@ def get_data_files():
      # a list of tuples with (path to install to, a list of local files)
      data_files = []
      if sys.platform == "win32":
