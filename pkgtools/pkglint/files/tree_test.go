6df0430c2d822e022aecfaa97da12155c5da50c1
rillig <rillig@pkgsrc.org>
2018-08-21 19:21:41 +0000

    pkgtools/pkglint: update source files to latest state

--- pkgtools/pkglint/files/tree_test.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "gopkg.in/check.v1"
-
-func (s *Suite) Test_Tree_String(c *check.C) {
-	c.Check(NewTree("not", NewTree("empty", "varname")).String(), equals, "(not (empty \"varname\"))")
-}
