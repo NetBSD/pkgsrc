794998d73b80bb3a0a5b5b054a52c3f51dd833d3
nia <nia@pkgsrc.org>
2018-08-21 17:34:48 +0000

    chat/tootstream: Update to 0.3.5.
    Changes:
    
    Added
    
        Updated to Mastodon.py 1.3 (no additional features yet)
    
    Fixed
    
        List renames did not work

--- chat/tootstream/patches/patch-src_tootstream_toot.py
+++ chat/tootstream/patches/patch-src_tootstream_toot.py
@@ -1,24 +1,9 @@
-$NetBSD: patch-src_tootstream_toot.py,v 1.1 2018/07/31 16:15:33 nia Exp $
+$NetBSD: patch-src_tootstream_toot.py,v 1.2 2018/08/21 17:34:48 nia Exp $
 
 'async' is a reserved keyword in Python 3.7.
 
---- src/tootstream/toot.py.orig	2018-05-31 00:06:11.000000000 +0000
+--- src/tootstream/toot.py.orig	2018-08-09 00:55:08.000000000 +0000
 +++ src/tootstream/toot.py
-@@ -1279,11 +1279,11 @@ def stream(mastodon, rest):
- 
-     try:
-         if rest == "home" or rest == "":
--            handle = mastodon.stream_user(toot_listener, async=True)
-+            handle = mastodon.stream_user(toot_listener, run_async=True)
-         elif rest == "fed" or rest == "public":
--            handle = mastodon.stream_public(toot_listener, async=True)
-+            handle = mastodon.stream_public(toot_listener, run_async=True)
-         elif rest == "local":
--            handle = mastodon.stream_local(toot_listener, async=True)
-+            handle = mastodon.stream_local(toot_listener, run_async=True)
-         elif rest.startswith('list'):
-             # Remove list from the rest string
-             items = rest.split('list ')
 @@ -1295,10 +1295,10 @@ def stream(mastodon, rest):
                  cprint("List {} is not found".format(items[-1]), fg('red'))
                  return
