b90095f7a6807d88c6b0482108244c2e3fc7f986
wiz <wiz@pkgsrc.org>
2018-08-22 09:42:51 +0000

    Recursive bump for perl5-5.28.0

--- databases/p5-DBI/buildlink3.mk
+++ databases/p5-DBI/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2018/01/07 13:03:59 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2018/08/22 09:42:52 wiz Exp $
 
 BUILDLINK_TREE+=	p5-DBI
 
@@ -8,7 +8,7 @@ P5_DBI_BUILDLINK3_MK:=
 .include "../../mk/bsd.fast.prefs.mk"
 
 BUILDLINK_API_DEPENDS.p5-DBI+=	p5-DBI>=1.612
-BUILDLINK_ABI_DEPENDS.p5-DBI?=	p5-DBI>=1.636nb3
+BUILDLINK_ABI_DEPENDS.p5-DBI?=	p5-DBI>=1.641nb1
 BUILDLINK_PKGSRCDIR.p5-DBI?=	../../databases/p5-DBI
 BUILDLINK_INCDIRS.p5-DBI?=	${PERL5_SUB_INSTALLVENDORARCH}/auto/DBI
 
--- devel/p5-Event/buildlink3.mk
+++ devel/p5-Event/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/01/07 13:04:09 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/08/22 09:42:52 wiz Exp $
 
 BUILDLINK_TREE+=	p5-Event
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	p5-Event
 P5_EVENT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.p5-Event+=	p5-Event>=1.13
-BUILDLINK_ABI_DEPENDS.p5-Event?=	p5-Event>=1.26nb2
+BUILDLINK_ABI_DEPENDS.p5-Event?=	p5-Event>=1.26nb3
 BUILDLINK_PKGSRCDIR.p5-Event?=		../../devel/p5-Event
 
 .include "../../lang/perl5/buildlink3.mk"
