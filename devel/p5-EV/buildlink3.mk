9306aa749e9c33c81b74e37c40b63aefa63fa54b
wiz <wiz@pkgsrc.org>
2018-08-22 09:42:51 +0000

    Recursive bump for perl5-5.28.0

--- devel/p5-EV/buildlink3.mk
+++ devel/p5-EV/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2017/06/05 14:22:54 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/08/22 09:42:52 wiz Exp $
 
 BUILDLINK_TREE+=	p5-EV
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	p5-EV
 P5_EV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.p5-EV+=	p5-EV>=3.9
-BUILDLINK_ABI_DEPENDS.p5-EV?=	p5-EV>=4.22nb3
+BUILDLINK_ABI_DEPENDS.p5-EV?=	p5-EV>=4.22nb4
 BUILDLINK_PKGSRCDIR.p5-EV?=	../../devel/p5-EV
 
 .include "../../lang/perl5/buildlink3.mk"
