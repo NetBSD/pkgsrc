467e9989a7e9eff7de852290c1db3fa904bc6956
maya <maya@pkgsrc.org>
2018-08-22 20:48:36 +0000

    Remove clauses 3,4 from TNF-only copyright blocks.
    This is based on the decision The NetBSD Foundation made in 2008 to
    do so, which was already applied to src.
    
    This change has been applied to code which is likely not in other
    repositories.
    
    ok board@, reviewed by riastradh@

--- pkgtools/url2pkg/files/url2pkg.pl
+++ pkgtools/url2pkg/files/url2pkg.pl
@@ -1,5 +1,5 @@
 #! @PERL@
-# $NetBSD: url2pkg.pl,v 1.37 2018/05/28 20:18:01 rillig Exp $
+# $NetBSD: url2pkg.pl,v 1.38 2018/08/22 20:48:38 maya Exp $
 #
 
 # Copyright (c) 2010 The NetBSD Foundation, Inc.
@@ -16,13 +16,6 @@
 # 2. Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
-# 3. All advertising materials mentioning features or use of this software
-#    must display the following acknowledgement:
-# This product includes software developed by the NetBSD
-# Foundation, Inc. and its contributors.
-# 4. Neither the name of The NetBSD Foundation nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
 # ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
