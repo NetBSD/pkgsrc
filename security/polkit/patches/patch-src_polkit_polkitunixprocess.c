7d28f8624bd60b9a7011773eafaeddfe7f4ab989
bsiegert <bsiegert@pkgsrc.org>
2018-08-24 19:18:27 +0000

    Pullup ticket #5814 - requested by wiz security/polkit: security fix
    Revisions pulled up:
    - security/polkit/Makefile                                      1.12
    - security/polkit/distinfo                                      1.8
    - security/polkit/patches/patch-src_polkit_polkitunixprocess.c  1.6
    
    ---
       Module Name:	pkgsrc
       Committed By:	wiz
       Date:		Thu Aug 16 12:30:43 UTC 2018
    
       Modified Files:
       	pkgsrc/security/polkit: Makefile distinfo
       	pkgsrc/security/polkit/patches: patch-src_polkit_polkitunixprocess.c
    
       Log Message:
       polkit: update to 0.115.
    
       This is polkit 0.115.
    
       Highlights:
        Fixes CVE-2018-1116, a local information disclosure and denial of service
        caused by trusting client-submitted UIDs when referencing processes.
        Thanks to Matthias Gerstner of the SUSE security team for reporting
        this issue.
    
       Changes since polkit 0.114:
    
       Miloslav Trmač (1):
             Fix CVE-2018-1116: Trusting client-supplied UID
    
       Ray Strode (3):
             Post-release version bump to 0.115
             jsauthority: pass "%s" format string to remaining report function
             NEWS: fix date from 2017 to 2018 for 0.114 entry

--- security/polkit/patches/patch-src_polkit_polkitunixprocess.c
+++ security/polkit/patches/patch-src_polkit_polkitunixprocess.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-src_polkit_polkitunixprocess.c,v 1.5 2018/05/15 09:09:22 jperkin Exp $
+$NetBSD: patch-src_polkit_polkitunixprocess.c,v 1.5.2.1 2018/08/24 19:18:27 bsiegert Exp $
 
 Fix SunOS includes.
 Fix pid_t type.
 
---- src/polkit/polkitunixprocess.c.orig	2018-03-23 15:09:30.000000000 +0000
+--- src/polkit/polkitunixprocess.c.orig	2018-06-25 13:55:45.000000000 +0000
 +++ src/polkit/polkitunixprocess.c
 @@ -24,6 +24,11 @@
  #endif
@@ -17,7 +17,7 @@ Fix pid_t type.
  #ifdef HAVE_FREEBSD
  #include <sys/param.h>
  #include <sys/sysctl.h>
-@@ -87,7 +92,7 @@ enum
+@@ -95,7 +100,7 @@ enum
  
  static void subject_iface_init (PolkitSubjectIface *subject_iface);
  
@@ -25,4 +25,4 @@ Fix pid_t type.
 +static guint64 get_start_time_for_pid (pid_t pid,
                                         GError **error);
  
- static gint _polkit_unix_process_get_owner (PolkitUnixProcess  *process,
+ #if defined(HAVE_FREEBSD) || defined(HAVE_NETBSD) || defined(HAVE_OPENBSD)
