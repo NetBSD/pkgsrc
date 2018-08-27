df0d982c3ef7674bf2518bd98e0c06989982b492
bouyer <bouyer@pkgsrc.org>
2018-08-27 15:54:09 +0000

    Update to 20180827. Changes since 20180405: Implement Create Boundary JSON interface Implement CreateTextPoint ODAPI New 'createtextpoint' api Bug fixes

--- geography/opencpn-plugin-draw/patches/patch-cmake_PluginConfigure.cmake
+++ geography/opencpn-plugin-draw/patches/patch-cmake_PluginConfigure.cmake
@@ -1,17 +1,17 @@
-$NetBSD: patch-cmake_PluginConfigure.cmake,v 1.5 2018/04/05 15:56:25 bouyer Exp $
+$NetBSD: patch-cmake_PluginConfigure.cmake,v 1.6 2018/08/27 15:54:09 bouyer Exp $
 Do not overwrite CMAKE_SHARED_LINKER_FLAGS
 
---- cmake/PluginConfigure.cmake.orig	2018-03-02 08:09:23.000000000 +0100
-+++ cmake/PluginConfigure.cmake	2018-04-05 17:46:39.601675541 +0200
-@@ -40,9 +40,9 @@
-  ENDIF(PROFILING)
+--- cmake/PluginConfigure.cmake.orig	2018-07-18 10:14:19.000000000 +0200
++++ cmake/PluginConfigure.cmake	2018-08-27 15:38:00.730541824 +0200
+@@ -69,9 +69,9 @@
+   ADD_DEFINITIONS(-D_CRT_NONSTDC_NO_DEPRECATE -D_CRT_SECURE_NO_DEPRECATE)
+ ELSE(MSVC)
+   IF(NOT APPLE)
+-    SET( CMAKE_SHARED_LINKER_FLAGS "-Wl,-Bsymbolic")
++    SET( CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-Bsymbolic")
+   ELSE(NOT APPLE)
+-    SET( CMAKE_SHARED_LINKER_FLAGS "-Wl -undefined dynamic_lookup")
++    SET( CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl -undefined dynamic_lookup")
+   ENDIF(NOT APPLE) 
+ ENDIF(MSVC)
  
-  IF(NOT APPLE)
--  SET( CMAKE_SHARED_LINKER_FLAGS "-Wl,-Bsymbolic")
-+  SET( CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-Bsymbolic")
-  ELSE(NOT APPLE)
--  SET( CMAKE_SHARED_LINKER_FLAGS "-Wl -undefined dynamic_lookup")
-+  SET( CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl -undefined dynamic_lookup")
-  ENDIF(NOT APPLE)
- 
- ENDIF(NOT MSVC)
