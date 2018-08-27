20dbeef572fb72d38a0c64f108585f5f44dbdb71
bsiegert <bsiegert@pkgsrc.org>
2018-08-27 15:01:09 +0000

    Update Go to 1.10.4.
    go1.10.4 (released 2018/08/24) includes fixes to the go command, linker, and
    the net/http, mime/multipart, ld/macho, bytes, and strings packages. See the
    Go 1.10.4 milestone on our issue tracker for details.
    
    pkgsrc note: After the import of Go 1.11, this package will be renamed to
    lang/go110.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,10 +1,10 @@
-# $NetBSD: version.mk,v 1.41 2018/07/08 18:40:40 bsiegert Exp $
+# $NetBSD: version.mk,v 1.42 2018/08/27 15:01:09 bsiegert Exp $
 
 SSP_SUPPORTED=	no
 
 .include "../../mk/bsd.prefs.mk"
 
-GO_VERSION=	1.10.3
+GO_VERSION=	1.10.4
 GO19_VERSION=	1.9.7
 GO14_VERSION=	1.4.3
 
