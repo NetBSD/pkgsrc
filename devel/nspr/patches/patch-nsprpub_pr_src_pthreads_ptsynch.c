da17c2ddcf6fd568557a396893acf8da1964ce8e
ryoon <ryoon@pkgsrc.org>
2018-08-30 12:21:44 +0000

    Update to 4.20
    Changelog:
    NSPR 4.20 contains the following changes:
    - Added support for the RISC-V 64-bit target.
    - Added support for aarch64 Windows.
    - Added support for abstract socket addresses on Linux and Android.
    - Fixed a FreeBSD build issue.
    - ASAN poisoning functions on Windows no longer use the dllimport
      attribute.

--- devel/nspr/patches/patch-nsprpub_pr_src_pthreads_ptsynch.c
+++ devel/nspr/patches/patch-nsprpub_pr_src_pthreads_ptsynch.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-nsprpub_pr_src_pthreads_ptsynch.c,v 1.3 2013/07/20 08:57:20 ryoon Exp $
+$NetBSD: patch-nsprpub_pr_src_pthreads_ptsynch.c,v 1.4 2018/08/30 12:21:44 ryoon Exp $
 
---- nsprpub/pr/src/pthreads/ptsynch.c.orig	2012-03-06 13:14:21.000000000 +0000
+--- nspr/pr/src/pthreads/ptsynch.c.orig	2018-08-28 12:42:28.000000000 +0000
 +++ nspr/pr/src/pthreads/ptsynch.c
-@@ -810,7 +810,7 @@ PR_IMPLEMENT(PRStatus) PR_DeleteSemaphor
-  */
+@@ -913,7 +913,7 @@ PR_IMPLEMENT(PRStatus) PR_DeleteSemaphor
  #if (defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)) \
-     || defined(FREEBSD) || defined(OPENBSD) || defined(BSDI) \
+     || (defined(FREEBSD) && __FreeBSD_version < 1200059) \
+     || defined(OPENBSD) || defined(BSDI) \
 -    || defined(DARWIN) || defined(SYMBIAN)
 +    || defined(DARWIN) || defined(SYMBIAN) || defined(__DragonFly__)
  /* union semun is defined by including <sys/sem.h> */
