e49fef2ad8f6b86bea4cf9be4f6a77b7cd9a4cbf
tnn <tnn@pkgsrc.org>
2018-08-31 09:06:07 +0000

    fontconfig: needs libbz2

--- fonts/fontconfig/buildlink3.mk
+++ fonts/fontconfig/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.35 2018/03/31 04:30:33 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2018/08/31 09:06:07 tnn Exp $
 
 BUILDLINK_TREE+=	fontconfig
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.fontconfig+=	fontconfig>=2.10.93
 BUILDLINK_ABI_DEPENDS.fontconfig+=	fontconfig>=2.13.0
 BUILDLINK_PKGSRCDIR.fontconfig?=	../../fonts/fontconfig
 
+.include "../../archivers/bzip2/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../devel/libuuid/buildlink3.mk"
