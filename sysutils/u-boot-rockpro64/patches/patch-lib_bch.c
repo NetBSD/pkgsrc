bb51c988325341e1bd22784ec5530714626f7396
tnn <tnn@pkgsrc.org>
2018-09-01 10:38:19 +0000

    sysutils/u-boot-rockpro64: import u-boot-rockpro64-2017.09
    U-Boot is a bootloader for embedded boards based on PowerPC, ARM, MIPS and
    several other processors, which can be installed in a boot ROM and used to
    initialize and test the hardware or to download and run application code.
    
    This package provides U-Boot for the rk3389-based RockPRO64 from PINE64.

---
+++ sysutils/u-boot-rockpro64/patches/patch-lib_bch.c
@@ -0,0 +1,39 @@
+$NetBSD: patch-lib_bch.c,v 1.1 2018/09/01 10:38:19 tnn Exp $
+
+u-boot: fix build on macos
+
+--- lib/bch.c.orig	2018-08-06 22:28:00.000000000 +0000
++++ lib/bch.c
+@@ -61,8 +61,10 @@
+ #include <linux/bitops.h>
+ #else
+ #include <errno.h>
+-#if defined(__FreeBSD__)
++#if defined(__FreeBSD__) || defined(__NetBSD__)
+ #include <sys/endian.h>
++#elif defined(__APPLE__)
++#include <machine/endian.h>
+ #else
+ #include <endian.h>
+ #endif
+@@ -71,7 +73,11 @@
+ #include <string.h>
+ 
+ #undef cpu_to_be32
++#if defined(__APPLE__)
++#define cpu_to_be32 htonl
++#else
+ #define cpu_to_be32 htobe32
++#endif
+ #define DIV_ROUND_UP(n,d) (((n) + (d) - 1) / (d))
+ #define kmalloc(size, flags)	malloc(size)
+ #define kzalloc(size, flags)	calloc(1, size)
+@@ -117,7 +123,7 @@ struct gf_poly_deg1 {
+ };
+ 
+ #ifdef USE_HOSTCC
+-#if !defined(__DragonFly__) && !defined(__FreeBSD__)
++#if !defined(__DragonFly__) && !defined(__FreeBSD__) && !defined(__APPLE__)
+ static int fls(int x)
+ {
+ 	int r = 32;
