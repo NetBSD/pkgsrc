838197094bb2077cc9ff25dd6aff593b156364c5
maya <maya@pkgsrc.org>
2018-09-03 17:40:32 +0000

    erlang: disable PGO on netbsd. gcov doesn't work on netbsd-8 at least.
    From Michael Taylor in PR pkg/53567

--- lang/erlang/hacks.mk
+++ lang/erlang/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.2 2015/04/28 14:34:20 fhajny Exp $
+# $NetBSD: hacks.mk,v 1.3 2018/09/03 17:40:32 maya Exp $
 
 .if !defined(ERLANG_HACKS_MK)
 ERLANG_HACKS_MK=	# empty
@@ -21,5 +21,10 @@ CFLAGS+=	-DMMAP_MAP_FIXED
 .endif
 
 CFLAGS.NetBSD+=	-D_NETBSD_SOURCE
+
+# gcov miscompiled PR 53567
+.if (${OPSYS} == "NetBSD")
+CONFIGURE_ARGS+=	--disable-pgo
+.endif
 
 .endif
