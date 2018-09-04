b9e3f7fc4eb4e59fa34a87d3fc24349de342ede5
minskim <minskim@pkgsrc.org>
2018-09-04 03:07:47 +0000

    devel/go-afero: Update to 1.1.1
    API changes:
    - Move sftpfs to subpackage

--- devel/go-afero/buildlink3.mk
+++ devel/go-afero/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/08/25 16:49:11 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/09/04 03:07:47 minskim Exp $
 
 BUILDLINK_TREE+=	go-afero
 
@@ -8,7 +8,7 @@ GO_AFERO_BUILDLINK3_MK:=
 BUILDLINK_CONTENTS_FILTER.go-afero=	${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-afero?=		build
 
-BUILDLINK_API_DEPENDS.go-afero+=	go-afero>=0.0
+BUILDLINK_API_DEPENDS.go-afero+=	go-afero>=1.1.1
 BUILDLINK_PKGSRCDIR.go-afero?=		../../devel/go-afero
 
 .include "../../textproc/go-text/buildlink3.mk"
