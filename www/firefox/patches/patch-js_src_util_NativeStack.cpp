a605603a09b9d2a22a68bad523fa7ee881a8038e
ryoon <ryoon@pkgsrc.org>
2018-09-05 15:29:58 +0000

    Update to 62.0
    Changelog:
    New
        Firefox Home (the default New Tab) now allows users to display up to
          4 rows of top sites, Pocket stories, and highlights
    
        "Reopen in Container" tab menu option appears for users with Containers
          that lets them choose to reopen a tab in a different container
    
        In advance of removing all trust for Symantec-issued certificates in
          Firefox 63, a preference was added that allows users to distrust
          certificates issued by Symantec. To use this preference, go to
          about:config in the address bar and set the preference
          "security.pki.distrust_ca_policy" to 2.
    
        Added FreeBSD support for WebAuthn
    
        Improved graphics rendering for Windows users without accelerated hardware
          using Parallel-Off-Main-Thread Painting
    
        Support for CSS Shapes, allowing for richer web page layouts. This goes
          hand in hand with a brand new Shape Path Editor in the CSS inspector.
    
        CSS Variable Fonts (OpenType Font Variations) support, which makes it
          possible to create beautiful typography with a single font file
    
        Updates for enterprise environments:
            AutoConfig is sandboxed to the documented API by default. You
            can disable the sandbox by setting the preference
            general.config.sandbox_enabled to false. Our long term plan is to
            remove the ability to turn off the sandboxing. If you need to
            continue to use more complex AutoConfig scripts, you will need to use
            Firefox Extended Support Release (ESR).
    
        Added Canadian English (en-CA) locale
    
    Changed
        Removed the description field for bookmarks. Users who have stored
          descriptions using the field may wish to export these descriptions
          as html or json files, as they will be removed in a future release.
    
        Dark theme is automatically enabled in macOS 10.14 dark mode
    
        Changed the default setting to Enforce (3) for the
          security.pki.name_matching_mode preference
    
        Adobe Flash applets now run in a more secure mode using process
          sandboxing on macOS. Learn how this may affect features here.
    
        Users disconnecting from Sync are now offered the option to wipe
          their Firefox profile data (including bookmarks, passwords, history,
          cookies, and site data) from their desktop computer
    
        Changed how WebRTC handles screen sharing: When screen-sharing a window,
          the window will be brought to front
    
    Developer
        Three-pane Inspector in Developer Tools separates the rules into its own
           panel

--- www/firefox/patches/patch-js_src_util_NativeStack.cpp
+++ www/firefox/patches/patch-js_src_util_NativeStack.cpp
@@ -1,19 +1,19 @@
-$NetBSD: patch-js_src_util_NativeStack.cpp,v 1.1 2018/06/26 09:37:10 jperkin Exp $
+$NetBSD: patch-js_src_util_NativeStack.cpp,v 1.2 2018/09/05 15:29:58 ryoon Exp $
 
 Support SunOS.
 
---- js/src/util/NativeStack.cpp.orig	2018-06-05 19:47:32.000000000 +0000
+--- js/src/util/NativeStack.cpp.orig	2018-08-30 16:44:22.000000000 +0000
 +++ js/src/util/NativeStack.cpp
-@@ -32,7 +32,7 @@ js::GetNativeStackBaseImpl()
+@@ -46,7 +46,7 @@ js::GetNativeStackBaseImpl()
      return static_cast<void*>(pTib->StackBase);
  }
  
 -#elif defined(SOLARIS)
 +#elif defined(__sun)
  
- #include <ucontext.h>
+ JS_STATIC_ASSERT(JS_STACK_GROWTH_DIRECTION < 0);
  
-@@ -78,6 +78,7 @@ js::GetNativeStackBaseImpl()
+@@ -134,6 +134,7 @@ js::GetNativeStackBaseImpl()
  #  elif defined(PTHREAD_NP_H) || defined(_PTHREAD_NP_H_) || defined(NETBSD)
      /* e.g. on FreeBSD 4.8 or newer, neundorf@kde.org */
      pthread_attr_get_np(thread, &sattr);
