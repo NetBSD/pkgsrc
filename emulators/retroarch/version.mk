8fff5c83a1d8f8edbee186b62bf490b7321201f6
nia <nia@pkgsrc.org>
2018-09-06 07:50:20 +0000

    emulators/retroarch: Update to 1.7.4.
    Changes:
    
        CHEEVOS: Fix crash when scrolling Achievement List while Unofficial Achievements enabled (#6732).
        CHEEVOS: Added hitcounts support for PauseIf/ResetIf (#6817).
        COMMON: Automatically hide "Configuration Override options" in Quick Menu.
        COMMON: Small Bugfix to not trigger savestate code when pressing Reset.
        COMMON: Added libsixel video driver.
        EMSCRIPTEN: Fix Game Focus Toggle.
        HID/OSX: Fix to set hid device registration deterministic (#6497), to address issue #6640 re-adding dynamic device registration.
        LOCALIZATION: Update Italian translation.
        LOCALIZATION: Update Japanese translation.
        LOCALIZATION: Update Polish translation.
        LOCALIZATION: Update Portuguese / Brazilian translation.
        LOCALIZATION: Update Russian translation.
        LOCALIZATION: Update Spanish translation.
        MIDI: Add MIDI support to the libretro API. Dosbox is the first proof of concept core implementing libretro MIDI.
        MIDI: Add a Windows driver for MIDI, based on winmm.
        MENU/QT/WIMP: Qt QSlider styling for Dark Theme.
        MENU/QT/WIMP: Remove button ghostly inside highlighting.
        MENU/QT/WIMP: Initial grid view.
        MENU/QT/WIMP: Drag&drop to add new playlist items, add option to add/edit/delete playlists.
        MENU/QT/WIMP: Add menu option to update RetroArch (Windows only for now).
        MENU/QT/WIMP: Add menu option to manage shaders.
        MENU/QT/WIMP: Add menu option to manage core options.
        MENU/XMB: Add new icons for the settings
        MENU/XMB: Add an option to show the desktop ui
        METAL: Initial work-in-progress video driver for Metal. macOS-only right now, and currently requires macOS 10.13.
        METAL: Supports XMB/MaterialUI, has a menu display driver. Has a font rendering driver.
        METAL/SLANG: Slang shaders should be compatible with Metal video driver.
        NETWORK: Enable SSL/TLS support by default for desktop platforms.
        QNX: Fix Game Focus Toggle.
        REMAPS: Fix the way offsets are calculated for keyboard remapping.
        RUNAHEAD: Fix full-screen mode change breaking Secondary Core's environment variables.
        VULKAN: Fix two validation errors.
        VULKAN: Try to avoid creating swapchains redundantly. Should fix black screen and having to alt tab out of window again to get display working on Nvidia GPUs (Windows).
        VULKAN/OSX: Initial MoltenVK support. Not enabled yet, several MoltenVK bugs should be fixed first before we can have it fully working.
        X11: Fix Game Focus Toggle.

--- emulators/retroarch/version.mk
+++ emulators/retroarch/version.mk
@@ -1,5 +1,10 @@
-# $NetBSD: version.mk,v 1.1 2018/08/07 16:44:26 nia Exp $
+# $NetBSD: version.mk,v 1.2 2018/09/06 07:50:20 nia Exp $
+#
+# used by emulators/libretro-core-info/Makefile
+# used by emulators/libretro-database/Makefile
+# used by emulators/libretro-glsl-shaders/Makefile
+# used by emulators/retroarch-assets/Makefile
 
 .if !defined(RETROARCH_VERSION)
-RETROARCH_VERSION=	1.7.3
+RETROARCH_VERSION=	1.7.4
 .endif
