802b9c5aa9db9a21891f6f5790ab65b853043ff7
khorben <khorben@pkgsrc.org>
2018-09-08 15:14:56 +0000

    Update devel/radare2 to version 2.4.0
    Unfortunately I could not test this with devel/radare2-cutter: the
    version currently packaged in pkgsrc did not build for me (issue
    with Qt). It should certainly be updated as well to match this
    version (eg to 1.4) but it now seems to require qt5-qtwebengine,
    which is only in wip.
    
    Also note that this is not the latest version of radare2.
    
    Tested on NetBSD/amd64 (7.1).
    
    From the release notes for this version:
    
    Date: 2018-03-05
    Codename: Chusy Chabber
    Downloads: https://radare.mikelloc.com/get/2.4.0/
    
    Build
    
        Find _main in MSVC-compiled MZ binaries
        Meson: Added sdb building/installation rules
        Install Headers and .pc Files with Meson
        Use meson pkgconfig for .pc files
        Initial sys/purge.sh distro to remove r2 distro packages
        Fix MSVC dynamic linking
        Upgrade to latest acr to honor AWK env var
        Added v810 and tms320 to meson
        Added x86_udis to meson
    
    Debugger/IO
    
        Use io.pcache instead of io.cache, improve speed
        Enable io.cache for afta too under aaaa
        Added asm.bbinfo for debugging op_pos
        Implemented wao for arm64
        Support RIOBind on RBuffer for fcpy_at
    
    Assembly
    
        Added an alternative and colored view for varsum
        Improvements in bin.strfilter and asm.emu
        Hexagon QDSP6 v6 support - LGPLv3
    
    Bin API
    
        More code cleanup in RBin api for file and obj
        Refactor RBin for file and obj, mach0 headers to stdout
        izz/izzj now works with -n without using rabin2
        Make idpi independent of bin loading
    
    New Commands/Eval
    
        Implemented @@iS RBin.Section foreach operator
        Implemented @% suffix operator
        Refactored asm.cmt* and scr.*
        Implemented @B: to temporary seek to the Nth BB
        Implemented cp. and fix r_sys_copy on mac/ios
        Fix wao ret* for arm64
        Added command to edit ~/.radare2rc
        Improveed aae to support cstr and pstr like asm.emu.str
        Implemented support for cstr and pstr for asm.emu.str in disasm
        Added asm.emu.stroff: always show offset when printing asm.emu string
        Initial support for dbg.symstore
        Change all asm.emu* to asm.emu.*
    
    Better support for AVR:
    
        Implemented opcode masking for improved zignatures
        Added missing opcode info (val, ptr)
        added pseudo code for avr instructions movw and st*
    
    Better graphs and visual panel:
    
        VC rotates scr.color instead of toggling
        Implement axgj
        Added < command and make dietline reuse rcons.readChar
        Improve RCons.readPush and add .readFlush() to make V* happy
        Implemented r_cons_readpush() new api and fix the V... automation
        e graph.format=? and missing help for @@iS
        Intial implementation of function call argument display
        Experimental Cons_INVERT in strings in disasm
        Added VbC to browse comments
        Added VbF to browse functions
        Implement VV) - rotate asm.emu.str in visual graph
        Added ) key in visual to toggle asm.emu.str
        Honor scr.color in Vbf
        Added Visual browser for process threads

--- devel/radare2/patches/patch-libr_socket_run.c
+++ devel/radare2/patches/patch-libr_socket_run.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-libr_socket_run.c,v 1.2 2018/02/18 17:36:58 kamil Exp $
+$NetBSD: patch-libr_socket_run.c,v 1.3 2018/09/08 15:14:56 khorben Exp $
 
 Support SunOS.
 
---- libr/socket/run.c.orig	2018-01-29 10:36:59.000000000 +0000
+--- libr/socket/run.c.orig	2018-03-05 17:12:35.000000000 +0000
 +++ libr/socket/run.c
-@@ -278,7 +278,7 @@ static void restore_saved_fd (int saved,
+@@ -278,7 +278,7 @@ static void restore_saved_fd(int saved, 
  }
  
- static int handle_redirection_proc (const char *cmd, bool in, bool out, bool err) {
+ static int handle_redirection_proc(const char *cmd, bool in, bool out, bool err) {
 -#if HAVE_PTY
 +#if HAVE_PTY && !defined(__sun)
  	// use PTY to redirect I/O because pipes can be problematic in
