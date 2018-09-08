9264f59e3507d06482ad9972b9bfffdcb504a170
adam <adam@pkgsrc.org>
2018-09-08 06:55:51 +0000

    cmake: updated to 3.12.2
    Changes in 3.12.2 since 3.12.1:
    * Android: Add support for NDK r18
    * CheckIPOSupported: Simplify result reporting logic
    * CheckIPOSupported: Tolerate backslashes in output of failed checks
    * VS: Restore CMakeLists.txt references in each target
    * EXPORT_PROPERTIES: Add test for an undefined property
    * EXPORT_PROPERTIES: Prevent null dereference for undefined property
    * Help: Fix typo in clang-tidy example -checks option
    * FindBoost: Fix context discovery for 1.60 and below
    * FindCUDA: Do not find cublas_device on CUDA >= 9.2
    * FindMatlab: Remove erroneous duplicate code
    * CUDA: Avoid using deprecated cublas_device to identify device lib dirs
    * cmState: Clear GlobVerificationManager state on Reset

--- devel/cmake/patches/patch-Source_Checks_Curses_CheckCurses.c
+++ devel/cmake/patches/patch-Source_Checks_Curses_CheckCurses.c
@@ -1,8 +1,20 @@
-$NetBSD: patch-Source_Checks_Curses_CheckCurses.c,v 1.1 2018/07/24 12:14:38 ryoon Exp $
+$NetBSD: patch-Source_Checks_Curses_CheckCurses.c,v 1.2 2018/09/08 06:55:51 adam Exp $
 
---- Source/Checks/Curses/CheckCurses.c.orig	2018-07-17 11:29:00.000000000 +0000
+Use curses.h provided by buildlink.
+
+--- Source/Checks/Curses/CheckCurses.c.orig	2018-08-09 12:14:07.000000000 +0000
 +++ Source/Checks/Curses/CheckCurses.c
-@@ -10,6 +10,6 @@
+@@ -1,15 +1,7 @@
+-#if defined(CURSES_HAVE_NCURSES_H)
+-#  include <ncurses.h>
+-#elif defined(CURSES_HAVE_NCURSES_NCURSES_H)
+-#  include <ncurses/ncurses.h>
+-#elif defined(CURSES_HAVE_NCURSES_CURSES_H)
+-#  include <ncurses/curses.h>
+-#else
+-#  include <curses.h>
+-#endif
++#include <curses.h>
  
  int main()
  {
