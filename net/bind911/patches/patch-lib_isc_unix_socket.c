3148a5500452d538789fbb70df25af9791bc5cc7
taca <taca@pkgsrc.org>
2018-09-09 13:11:38 +0000

    net/bind911 Add BIND 9.11 package
    Add bind9.11.4pl1 (BIND 9.11.4-P1) package.
    
    Note: named(8) requires writable permission to current directory when
    start up or the directory specified by "directory" in options statement.
    
    
    BIND, the Berkeley Internet Name Daemon, version 9 is a major rewrite
    of nearly all aspects of the underlying BIND architecture.  Some
    of the important features of BIND-9 are:
    
    	- DNS Security
    	- IP version 6
    	- DNS Protocol Enhancements
    	- Views
    	- Multiprocessor Support
    	- Improved Portability Architecture
    	- Full NSEC3 support
    	- Automatic zone re-signing
    	- New update-policy methods tcp-self and 6to4-self
    
    This package contains the BIND 9.11 release.
    
    	- Catalog Zones, a new method for provisioning servers
    	- "dnstap", a fast and flexible method of capturing and logging
    	  DNS traffic.
    	- "dyndb", a new API for loading zone data from an external database
    	- dnssec-keymgr, a new key mainenance utility
    	- mdig, an alternate version of dig utility
    	- And more...

---
+++ net/bind911/patches/patch-lib_isc_unix_socket.c
@@ -0,0 +1,33 @@
+$NetBSD: patch-lib_isc_unix_socket.c,v 1.1 2018/09/09 13:11:38 taca Exp $
+
+Make ENOBUFS a soft error
+https://gitlab.isc.org/isc-projects/bind9/issues/462
+
+--- lib/isc/unix/socket.c.orig	2018-07-24 17:24:11.000000000 +0000
++++ lib/isc/unix/socket.c
+@@ -257,6 +257,7 @@ typedef enum { poll_idle, poll_active, p
+ #define SOFT_ERROR(e)	((e) == EAGAIN || \
+ 			 (e) == EWOULDBLOCK || \
+ 			 (e) == EINTR || \
++			 (e) == ENOBUFS || \
+ 			 (e) == 0)
+ 
+ #define DLVL(x) ISC_LOGCATEGORY_GENERAL, ISC_LOGMODULE_SOCKET, ISC_LOG_DEBUG(x)
+@@ -1952,7 +1953,7 @@ doio_recv(isc__socket_t *sock, isc_socke
+ 		SOFT_OR_HARD(EHOSTDOWN, ISC_R_HOSTDOWN);
+ 		/* HPUX 11.11 can return EADDRNOTAVAIL. */
+ 		SOFT_OR_HARD(EADDRNOTAVAIL, ISC_R_ADDRNOTAVAIL);
+-		ALWAYS_HARD(ENOBUFS, ISC_R_NORESOURCES);
++		SOFT_OR_HARD(ENOBUFS, ISC_R_NORESOURCES);
+ 		/* Should never get this one but it was seen. */
+ #ifdef ENOPROTOOPT
+ 		SOFT_OR_HARD(ENOPROTOOPT, ISC_R_HOSTUNREACH);
+@@ -2149,7 +2150,7 @@ doio_send(isc__socket_t *sock, isc_socke
+ 		ALWAYS_HARD(EHOSTDOWN, ISC_R_HOSTUNREACH);
+ #endif
+ 		ALWAYS_HARD(ENETUNREACH, ISC_R_NETUNREACH);
+-		ALWAYS_HARD(ENOBUFS, ISC_R_NORESOURCES);
++		SOFT_OR_HARD(ENOBUFS, ISC_R_NORESOURCES);
+ 		ALWAYS_HARD(EPERM, ISC_R_HOSTUNREACH);
+ 		ALWAYS_HARD(EPIPE, ISC_R_NOTCONNECTED);
+ 		ALWAYS_HARD(ECONNRESET, ISC_R_CONNECTIONRESET);
