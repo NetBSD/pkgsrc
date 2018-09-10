bea8cee918f6410b52fd3dcd7fb78ee821497e5b
kim <kim@pkgsrc.org>
2018-09-10 13:34:16 +0000

    Add upstream patch to address CVE-2018-1000222. Restore the tiff option, so libtiff can be avoided. Ok by adam@.

--- graphics/gd/options.mk
+++ graphics/gd/options.mk
@@ -1,9 +1,17 @@
-# $NetBSD: options.mk,v 1.7 2017/09/04 06:20:45 adam Exp $
+# $NetBSD: options.mk,v 1.8 2018/09/10 13:34:16 kim Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gd
-PKG_SUPPORTED_OPTIONS=	x11
+PKG_SUPPORTED_OPTIONS=	tiff x11
+PKG_SUGGESTED_OPTIONS=	tiff
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mtiff)
+.include "../../graphics/tiff/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-tiff=${BUILDLINK_PREFIX.tiff}
+.else
+CONFIGURE_ARGS+=	--without-tiff
+.endif
 
 .if !empty(PKG_OPTIONS:Mx11)
 .include "../../x11/libXpm/buildlink3.mk"
