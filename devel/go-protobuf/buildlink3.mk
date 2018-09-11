2906d68f46ff9b338985ae4709a73c89fbee3127
minskim <minskim@pkgsrc.org>
2018-09-11 18:27:08 +0000

    devel/go-protobuf: Update to 1.2.0
    Notable changes:
    
    - add DiscardUnknown function
    - The serialization logic (for both Marshal and Unmarshal) has been
      optimized. Testing inside Google demonstrate that the new
      implementation is about 1.3x to 2.1x faster.
    - Unknown fields in proto3 are now preserved.
    - JSON marshal and unmarshal now check for proto2 required field.
    - GetExtension returns the raw bytes if the ExtensionDesc does not
      have type information.
    - Invalid UTF-8 in string types are rejected during marshal and
      unmarshal.
    - Fields of bad wire types are treated as unknown fields instead of an
      error.
    - The protoc-gen-go generator supports the paths=source_relative
      option.
    - Unknown fields in map entries are now ignored instead of causing an
      unmarshal error.
    - The "import public" feature is now implemented using Go type
      aliases.
    - jsonpb.Unmarshal correctly handles escaped strings for some of the
      well-known types
    - jsonpb.Unmarshal correctly returns an error when trying to marshal a
      scalar enum into repeated enums.
    - jsonpb.Unmarshal correctly handles string versions of numeric
      primitives
    - jsonpb.Unmarshal correctly handles enum values in maps
    - fixes a performance regression where nested maps led to quadratic
      behavior
    - reverts strict UTF-8 validation for proto2 string fields
    - makes UTF-8 validation errors for proto3 string fields non-fatal

--- devel/go-protobuf/buildlink3.mk
+++ devel/go-protobuf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/01/07 13:04:02 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/09/11 18:27:08 minskim Exp $
 
 BUILDLINK_TREE+=	go-protobuf
 
@@ -8,7 +8,7 @@ GO_PROTOBUF_BUILDLINK3_MK:=
 BUILDLINK_CONTENTS_FILTER.go-protobuf=	${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-protobuf?=	build
 
-BUILDLINK_API_DEPENDS.go-protobuf+=	go-protobuf>=20160829
+BUILDLINK_API_DEPENDS.go-protobuf+=	go-protobuf>=1.2.0
 BUILDLINK_PKGSRCDIR.go-protobuf?=	../../devel/go-protobuf
 .endif	# GO_PROTOBUF_BUILDLINK3_MK
 
