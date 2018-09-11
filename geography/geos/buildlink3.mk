65b8e0e79cdb4f3338b17d7b5d46300daffb6656
gdt <gdt@pkgsrc.org>
2018-09-11 16:13:29 +0000

    Update to 3.7.0:
    pkgsrc changes: revise comments about geos C vs C++ library linking in
    light of unexpected libtool behavior (which defeats geos's scheme)
    
    Upstream NEWS:
    
    3.7.0 changes
    2018-09-10
    - New things:
      - CAPI: GEOSDistanceIndexed (#795, Dan Baston)
      - CAPI: GEOSCoordSeq_isCCW (#870, Dan Baston)
      - CAPI: GEOSGeom_getXMin, GEOSGeom_getXMax,
              GEOSGeom_getYMin, GEOSGeom_getYMax (#871, Dan Baston)
      - CAPI: GEOSFrechetDistance (#797, Shinichi SUGIYAMA)
      - CAPI: GEOSReverse (#872, Dan Baston)
      - CAPI: GEOSGeomGetZ (#581, J Smith)
    
    - Improvements
      - Interruptible snap operation (Paul Ramsey)
      - Numerous packaging, doc, and build changes
        (Debian group: Bas Couwenberg, Francesco Paolo Lovergine)
        (NetBSD: Greg Troxel)
      - Allow static library with C API for CMake builds (#878, Dakota Hawkins)
    
    - C++ API changes:
      - Require defining USE_UNSTABLE_GEOS_CPP_API for use without
        warnings.
      - Make C++11 required (Mateusz Loskot)
      - Use C++11 unique_ptr, nullptr, and override constructs
        (Mateusz Loskot)
      - C++11 standard delete on noncopyable
        (#851, Vicky Vergara)
      - Fix CommonBits::getBit to correctly handle i >= 32
        (#834, Kurt Schwehr)

--- geography/geos/buildlink3.mk
+++ geography/geos/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.11 2012/06/02 16:27:30 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2018/09/11 16:13:29 gdt Exp $
 
 BUILDLINK_TREE+=	geos
 
 .if !defined(GEOS_BUILDLINK3_MK)
 GEOS_BUILDLINK3_MK:=
+
+#USE_LANGUAGES+=	c++11
 
 BUILDLINK_API_DEPENDS.geos+=	geos>=2.2.3
 BUILDLINK_ABI_DEPENDS.geos+=	geos>=3.3.4
