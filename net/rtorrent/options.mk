09532fb4bd89f9f6ccaa80f3ba302c1cdad0a15d
maya <maya@pkgsrc.org>
2018-09-14 21:06:42 +0000

    rtorrent: disable xmlrpc option by default, it is too insecure.
    bump PKGREVISION.

--- net/rtorrent/options.mk
+++ net/rtorrent/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2018/06/24 16:59:52 adam Exp $
+# $NetBSD: options.mk,v 1.3 2018/09/14 21:06:42 maya Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.rtorrent
 PKG_SUPPORTED_OPTIONS=	xmlrpc
-PKG_SUGGESTED_OPTIONS=	xmlrpc
+PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
 
