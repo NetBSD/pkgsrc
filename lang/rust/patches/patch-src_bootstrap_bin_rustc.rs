77de38b1906b3be43ab41eeed506929ca00ea579
jperkin <jperkin@pkgsrc.org>
2018-09-14 10:04:43 +0000

    rust: Update to 1.29.0.
    Version 1.29.0 (2018-09-13)
    ==========================
    
    Compiler
    --------
    - [Bumped minimum LLVM version to 5.0.][51899]
    - [Added `powerpc64le-unknown-linux-musl` target.][51619]
    - [Added `aarch64-unknown-hermit` and `x86_64-unknown-hermit` targets.][52861]
    
    Libraries
    ---------
    - [`Once::call_once` now no longer requires `Once` to be `'static`.][52239]
    - [`BuildHasherDefault` now implements `PartialEq` and `Eq`.][52402]
    - [`Box<CStr>`, `Box<OsStr>`, and `Box<Path>` now implement `Clone`.][51912]
    - [Implemented `PartialEq<&str>` for `OsString` and `PartialEq<OsString>`
      for `&str`.][51178]
    - [`Cell<T>` now allows `T` to be unsized.][50494]
    - [`SocketAddr` is now stable on Redox.][52656]
    
    Stabilized APIs
    ---------------
    - [`Arc::downcast`]
    - [`Iterator::flatten`]
    - [`Rc::downcast`]
    
    Cargo
    -----
    - [Cargo can silently fix some bad lockfiles ][cargo/5831] You can use
      `--locked` to disable this behaviour.
    - [`cargo-install` will now allow you to cross compile an install
      using `--target`][cargo/5614]
    - [Added the `cargo-fix` subcommand to automatically move project code from
      2015 edition to 2018.][cargo/5723]
    
    Misc
    ----
    - [`rustdoc` now has the `--cap-lints` option which demotes all lints above
      the specified level to that level.][52354] For example `--cap-lints warn`
      will demote `deny` and `forbid` lints to `warn`.
    - [`rustc` and `rustdoc` will now have the exit code of `1` if compilation
      fails, and `101` if there is a panic.][52197]
    - [A preview of clippy has been made available through rustup.][51122]
      You can install the preview with `rustup component add clippy-preview`
    
    Compatibility Notes
    -------------------
    - [`str::{slice_unchecked, slice_unchecked_mut}` are now deprecated.][51807]
      Use `str::get_unchecked(begin..end)` instead.
    - [`std::env::home_dir` is now deprecated for its unintuitive behaviour.][51656]
      Consider using the `home_dir` function from
      https://crates.io/crates/dirs instead.
    - [`rustc` will no longer silently ignore invalid data in target spec.][52330]
    
    [52861]: https://github.com/rust-lang/rust/pull/52861/
    [52656]: https://github.com/rust-lang/rust/pull/52656/
    [52239]: https://github.com/rust-lang/rust/pull/52239/
    [52330]: https://github.com/rust-lang/rust/pull/52330/
    [52354]: https://github.com/rust-lang/rust/pull/52354/
    [52402]: https://github.com/rust-lang/rust/pull/52402/
    [52103]: https://github.com/rust-lang/rust/pull/52103/
    [52197]: https://github.com/rust-lang/rust/pull/52197/
    [51807]: https://github.com/rust-lang/rust/pull/51807/
    [51899]: https://github.com/rust-lang/rust/pull/51899/
    [51912]: https://github.com/rust-lang/rust/pull/51912/
    [51511]: https://github.com/rust-lang/rust/pull/51511/
    [51619]: https://github.com/rust-lang/rust/pull/51619/
    [51656]: https://github.com/rust-lang/rust/pull/51656/
    [51178]: https://github.com/rust-lang/rust/pull/51178/
    [51122]: https://github.com/rust-lang/rust/pull/51122
    [50494]: https://github.com/rust-lang/rust/pull/50494/
    [cargo/5614]: https://github.com/rust-lang/cargo/pull/5614/
    [cargo/5723]: https://github.com/rust-lang/cargo/pull/5723/
    [cargo/5831]: https://github.com/rust-lang/cargo/pull/5831/
    [`Arc::downcast`]: https://doc.rust-lang.org/std/sync/struct.Arc.html#method.downcast
    [`Iterator::flatten`]: https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.flatten
    [`Rc::downcast`]: https://doc.rust-lang.org/std/rc/struct.Rc.html#method.downcast

--- lang/rust/patches/patch-src_bootstrap_bin_rustc.rs
+++ lang/rust/patches/patch-src_bootstrap_bin_rustc.rs
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_bootstrap_bin_rustc.rs,v 1.3 2018/05/18 12:45:52 ryoon Exp $
+$NetBSD: patch-src_bootstrap_bin_rustc.rs,v 1.4 2018/09/14 10:04:43 jperkin Exp $
 
 * Set RPATH for pkgsrc wrapper
 
---- src/bootstrap/bin/rustc.rs.orig	2018-05-07 17:31:28.000000000 +0000
+--- src/bootstrap/bin/rustc.rs.orig	2018-09-11 03:37:38.000000000 +0000
 +++ src/bootstrap/bin/rustc.rs
-@@ -226,7 +226,7 @@ fn main() {
+@@ -233,7 +233,7 @@ fn main() {
                  cmd.arg("-Z").arg("osx-rpath-install-name");
                  Some("-Wl,-rpath,@loader_path/../lib")
              } else if !target.contains("windows") && !target.contains("wasm32") {
--- lang/rust/patches/patch-src_liblibc_src_unix_solaris_mod.rs
+++ lang/rust/patches/patch-src_liblibc_src_unix_solaris_mod.rs
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_liblibc_src_unix_solaris_mod.rs,v 1.3 2018/08/09 17:04:03 jperkin Exp $
+$NetBSD: patch-src_liblibc_src_unix_solaris_mod.rs,v 1.4 2018/09/14 10:04:43 jperkin Exp $
 
 Add temporary upstream fixes for SunOS.
 
---- src/liblibc/src/unix/solaris/mod.rs.orig	2018-07-30 22:16:16.000000000 +0000
+--- src/liblibc/src/unix/solaris/mod.rs.orig	2018-09-11 03:38:01.000000000 +0000
 +++ src/liblibc/src/unix/solaris/mod.rs
 @@ -692,6 +692,8 @@ pub const EOWNERDEAD: ::c_int = 58;
  pub const ENOTRECOVERABLE: ::c_int = 59;
@@ -13,12 +13,3 @@ Add temporary upstream fixes for SunOS.
  pub const ETIME: ::c_int = 62;
  pub const ENOSR: ::c_int = 63;
  pub const ENONET: ::c_int = 64;
-@@ -1208,7 +1210,7 @@ pub const EPOLLET: ::c_int = 0x80000000;
- pub const EPOLLRDHUP: ::c_int = 0x2000;
- pub const EPOLLEXCLUSIVE: ::c_int = 0x10000000;
- pub const EPOLLONESHOT: ::c_int = 0x40000000;
--pub const EPOLL_CLOEXEC: ::c_int = 0x02000000;
-+pub const EPOLL_CLOEXEC: ::c_int = 0x80000;
- pub const EPOLL_CTL_ADD: ::c_int = 1;
- pub const EPOLL_CTL_MOD: ::c_int = 3;
- pub const EPOLL_CTL_DEL: ::c_int = 2;
