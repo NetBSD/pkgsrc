98c490e9224c278b3d2ad7d65fb06e359a76f8d7
bsiegert <bsiegert@pkgsrc.org>
2018-09-15 12:36:40 +0000

    Update vim to 8.1.0390.
    Among other minor changes, this correctly detects the argument types for
    select, fixing PR port-sparc64/53296.

--- editors/vim-share/version.mk
+++ editors/vim-share/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.118 2018/07/28 08:27:35 morr Exp $
+# $NetBSD: version.mk,v 1.119 2018/09/15 12:36:41 bsiegert Exp $
 
 VIM_VERSION=		8.1
-VIM_PATCHLEVEL=		0216
+VIM_PATCHLEVEL=		0390
 # Changelog: see http://ftp.vim.org/pub/vim/patches/8.1/
 VIM_SUBDIR=		vim81
 
