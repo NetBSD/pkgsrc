39c909e9f73a6d7ef024c2739cc63dbc50ed74e7
maya <maya@pkgsrc.org>
2018-09-17 07:30:31 +0000

    suil: appease pkglint by appending instead of assigning to this variable.

--- audio/suil/buildlink3.mk
+++ audio/suil/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2018/04/16 14:33:45 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2018/09/17 07:30:31 maya Exp $
 
 BUILDLINK_TREE+=	suil
 
 .if !defined(SUIL_BUILDLINK3_MK)
 SUIL_BUILDLINK3_MK:=
 
+BUILDLINK_ABI_DEPENDS.suil+=	suil>=0.8.2nb5
 BUILDLINK_API_DEPENDS.suil+=	suil>=0.8.2
-BUILDLINK_ABI_DEPENDS.suil?=	suil>=0.8.2nb5
 BUILDLINK_PKGSRCDIR.suil?=	../../audio/suil
 
 .include "../../audio/lv2/buildlink3.mk"
