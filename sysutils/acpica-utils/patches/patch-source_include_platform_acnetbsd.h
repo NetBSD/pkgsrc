395ab4d6abfc77738cba2e35febed81a324b7a82
maya <maya@pkgsrc.org>
2018-09-17 13:11:59 +0000

    acpica-utils: build fix for netbsd<7
    from Edgar Fuß in PR pkg/52654

---
+++ sysutils/acpica-utils/patches/patch-source_include_platform_acnetbsd.h
@@ -0,0 +1,13 @@
+--- source/include/platform/acnetbsd.h.orig	2015-04-10 17:27:17.000000000 +0200
++++ source/include/platform/acnetbsd.h	2015-12-10 14:15:22.000000000 +0100
+@@ -112,4 +112,10 @@
+ #define ACPI_USE_SYSTEM_CLIBRARY
+ #define ACPI_USE_NATIVE_DIVIDE
+ 
++/* NetBSD versions prior to 7 lack sem_timedwait() in librt */
++#include <sys/param.h>
++#if (__NetBSD_Version__ < 700000000)       /* NetBSD 7 */
++#define ACPI_USE_ALTERNATE_TIMEOUT
++#endif
++
+ #endif /* __ACNETBSD_H__ */
