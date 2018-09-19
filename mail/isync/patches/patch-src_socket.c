bf114174dc670b61245560d6fff21dc67e97ee85
schmonz <schmonz@pkgsrc.org>
2018-09-19 13:08:14 +0000

    Update to 1.3.0. From the changelog:
    - Network timeout handling has been added.
    
    - Support for proper Maildir++ and a Maildir sub-folder naming style
      without extra dots have been added.
    
    - Support for TLS client certificates was added.
    
    - Support for recovering from baseless UID validity changes was added.
    
    - The get-cert script was renamed to mbsync-get-cert.

--- mail/isync/patches/patch-src_socket.c
+++ mail/isync/patches/patch-src_socket.c
@@ -1,18 +1,19 @@
-$NetBSD: patch-src_socket.c,v 1.4 2016/12/15 14:15:01 schmonz Exp $
+$NetBSD: patch-src_socket.c,v 1.5 2018/09/19 13:08:14 schmonz Exp $
 
 Prevent mbsync from crashing, PR pkg/49784.
 
---- src/socket.c.orig	2015-09-27 10:13:04.000000000 +0000
+--- src/socket.c.orig	2017-10-01 08:42:35.000000000 +0000
 +++ src/socket.c
-@@ -509,7 +509,10 @@ static void
+@@ -542,8 +542,10 @@ static void
  socket_connected( conn_t *conn )
  {
  #ifdef HAVE_IPV6
 -	freeaddrinfo( conn->addrs );
-+	if ( conn->addrs ) {
+-	conn->addrs = 0;
++	if (conn->addrs) {
 +		freeaddrinfo( conn->addrs );
-+		conn->addrs = NULL;
++		conn->addrs = 0;
 +	}
  #endif
  	conf_notifier( &conn->notify, 0, POLLIN );
- 	conn->state = SCK_READY;
+ 	socket_expect_read( conn, 0 );
