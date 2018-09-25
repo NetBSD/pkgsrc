94f5f86de01b8257ffc53fe8a026c3d10f769858
jperkin <jperkin@pkgsrc.org>
2018-09-25 12:55:53 +0000

    firefox: Extend the SunOS fix to another section.

--- www/firefox/patches/patch-js_src_util_NativeStack.cpp
+++ www/firefox/patches/patch-js_src_util_NativeStack.cpp
@@ -1,9 +1,18 @@
-$NetBSD: patch-js_src_util_NativeStack.cpp,v 1.2 2018/09/05 15:29:58 ryoon Exp $
+$NetBSD: patch-js_src_util_NativeStack.cpp,v 1.3 2018/09/25 12:55:53 jperkin Exp $
 
 Support SunOS.
 
 --- js/src/util/NativeStack.cpp.orig	2018-08-30 16:44:22.000000000 +0000
 +++ js/src/util/NativeStack.cpp
+@@ -13,7 +13,7 @@
+ # if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+ #  include <pthread_np.h>
+ # endif
+-# if defined(SOLARIS) || defined(AIX)
++# if defined(__sun) || defined(AIX)
+ #  include <ucontext.h>
+ # endif
+ # if defined(ANDROID) && !defined(__aarch64__)
 @@ -46,7 +46,7 @@ js::GetNativeStackBaseImpl()
      return static_cast<void*>(pTib->StackBase);
  }
