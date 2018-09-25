0891623c7362a9c88d47a231aeda04ba61cbbf88
wiz <wiz@pkgsrc.org>
2018-09-25 09:50:59 +0000

    libbluray: add some missing dependencies to bl3.mk
    (listed in pkg-config file)

--- multimedia/libbluray/buildlink3.mk
+++ multimedia/libbluray/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/03/18 18:18:45 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/09/25 09:50:59 wiz Exp $
 
 BUILDLINK_TREE+=	libbluray
 
@@ -8,6 +8,9 @@ LIBBLURAY_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libbluray+=	libbluray>=0.8.1
 BUILDLINK_PKGSRCDIR.libbluray?=		../../multimedia/libbluray
 
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
 .endif	# LIBBLURAY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libbluray
