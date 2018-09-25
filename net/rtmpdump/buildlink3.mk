939ac7bf345d2a2c41b7823183468ace92223e8b
wiz <wiz@pkgsrc.org>
2018-09-25 09:25:45 +0000

    rtmpdump: add dependencies to bl3.mk
    they were hiding in the pkgconfig file

--- net/rtmpdump/buildlink3.mk
+++ net/rtmpdump/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2018/09/23 21:09:32 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2018/09/25 09:25:45 wiz Exp $
 
 BUILDLINK_TREE+=	rtmpdump
 
@@ -6,9 +6,13 @@ BUILDLINK_TREE+=	rtmpdump
 RTMPDUMP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.rtmpdump+=	rtmpdump>=2.3
-BUILDLINK_ABI_DEPENDS.rtmpdump+=	rtmpdump>=2.4nb3
+BUILDLINK_ABI_DEPENDS.rtmpdump+=	rtmpdump>=2.4.0.0
 BUILDLINK_PKGSRCDIR.rtmpdump?=		../../net/rtmpdump
 
+.include "../../devel/gmp/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../security/nettle/buildlink3.mk"
+.include "../../security/gnutls/buildlink3.mk"
 .endif	# RTMPDUMP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-rtmpdump
