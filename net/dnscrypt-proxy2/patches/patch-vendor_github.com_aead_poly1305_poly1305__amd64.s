46d2db7f0167a23fb394b1857956b1cb50ac87db
nia <nia@pkgsrc.org>
2018-09-30 19:56:47 +0000

    dnscrypt-proxy2: resolve "relocation target runtime.support_avx2 not defined"

---
+++ net/dnscrypt-proxy2/patches/patch-vendor_github.com_aead_poly1305_poly1305__amd64.s
@@ -0,0 +1,14 @@
+$NetBSD: patch-vendor_github.com_aead_poly1305_poly1305__amd64.s,v 1.1 2018/09/30 19:56:47 nia Exp $
+
+"poly1305.supportsAVX2: relocation target runtime.support_avx2 not defined"
+
+--- vendor/github.com/aead/poly1305/poly1305_amd64.s.orig	2018-07-09 16:19:45.000000000 +0000
++++ vendor/github.com/aead/poly1305/poly1305_amd64.s
+@@ -150,6 +150,6 @@ TEXT ·finalize(SB), $0-16
+ 
+ // func supportsAVX2() bool
+ TEXT ·supportsAVX2(SB), 4, $0-1
+-	MOVQ runtime·support_avx2(SB), AX
++	MOVQ $0X0, AX
+ 	MOVB AX, ret+0(FP)
+ 	RET
