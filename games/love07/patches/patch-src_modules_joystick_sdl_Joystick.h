df373ad3871dc95b9676ac26c99231f0c3367826
maya <maya@pkgsrc.org>
2018-09-30 19:10:10 +0000

    Add love07, LOVE version 0.7.2
    LOVE is a framework for making 2D games in the Lua programming
    language. LOVE is totally free, and can be used in anything from
    friendly open-source hobby projects, to evil, closed-source commercial
    ones.
    
    We need multiple versions as games were written for the older versions.

---
+++ games/love07/patches/patch-src_modules_joystick_sdl_Joystick.h
@@ -0,0 +1,15 @@
+$NetBSD: patch-src_modules_joystick_sdl_Joystick.h,v 1.1 2018/09/30 19:10:11 maya Exp $
+
+Match include path for SDL<2
+
+--- src/modules/joystick/sdl/Joystick.h.orig	2012-04-06 18:15:02.000000000 +0000
++++ src/modules/joystick/sdl/Joystick.h
+@@ -26,7 +26,7 @@
+ #include <common/EnumMap.h>
+ 
+ // SDL
+-#include <SDL.h>
++#include <SDL/SDL.h>
+ 
+ namespace love
+ {
