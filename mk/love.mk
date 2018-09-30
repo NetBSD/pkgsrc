e84963502c7a443dee0d6e45118ffdef34f37018
maya <maya@pkgsrc.org>
2018-09-30 19:22:18 +0000

    Add love.mk, helps generate wrappers to execute LOVE2D games with the correct LOVE version.

---
+++ mk/love.mk
@@ -0,0 +1,23 @@
+# $NetBSD: love.mk,v 1.1 2018/09/30 19:22:18 maya Exp $
+#
+# Wrapper for LOVE2D games.
+# Creates launchers for the required version.
+#
+# The following system variables must be defined before:
+#	LOVE_GAME			Name of the executable created
+#	LOVE_DATA			.love filename
+#	LOVE_VERSION			LOVE version to be used. 0.8, 0.9, ..
+
+do-build:
+	${ECHO} '#!/bin/sh' > ${WRKSRC}/${LOVE_GAME}
+	${ECHO} '${PREFIX}/love${LOVE_VERSION}/bin/love ${PREFIX}/share/games/${LOVE_DATA}' \
+		>> ${WRKSRC}/${LOVE_GAME}
+
+LOVE_VERSION_NODOT=${LOVE_VERSION:S/.//}
+
+INSTALLATION_DIRS+=	bin share/games
+do-install:
+	${INSTALL_DATA} ${WRKSRC}/${LOVE_DATA} ${DESTDIR}${PREFIX}/share/games/${LOVE_DATA}
+	${INSTALL_SCRIPT} ${WRKSRC}/${LOVE_GAME} ${DESTDIR}${PREFIX}/bin/${LOVE_GAME}
+
+DEPENDS+=	love${LOVE_VERSION_NODOT}-[0-9]*:../../wip/love${LOVE_VERSION_NODOT}
