1d06f74438c3b32362b1db233eb0fe50933e528f
maya <maya@pkgsrc.org>
2018-09-30 19:10:10 +0000

    Add love07, LOVE version 0.7.2
    LOVE is a framework for making 2D games in the Lua programming
    language. LOVE is totally free, and can be used in anything from
    friendly open-source hobby projects, to evil, closed-source commercial
    ones.
    
    We need multiple versions as games were written for the older versions.

---
+++ games/love07/patches/patch-src_modules_keyboard_sdl_wrap__Keyboard.h
@@ -0,0 +1,15 @@
+$NetBSD: patch-src_modules_keyboard_sdl_wrap__Keyboard.h,v 1.1 2018/09/30 19:10:11 maya Exp $
+
+Match SDL1 include paths
+
+--- src/modules/keyboard/sdl/wrap_Keyboard.h.orig	2011-05-04 16:00:10.000000000 +0000
++++ src/modules/keyboard/sdl/wrap_Keyboard.h
+@@ -26,7 +26,7 @@
+ #include "Keyboard.h"
+ 
+ // SDL
+-#include <SDL.h>
++#include <SDL/SDL.h>
+ 
+ namespace love
+ {
