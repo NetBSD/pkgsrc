2d3fbcc90907075510b1e85709854545c29b913a
sevan <sevan@pkgsrc.org>
2018-10-01 00:11:29 +0000

    Unbreak build on DragonFly BSD. Add OpenBSD to the exclusion list in chartype.h as with upstream version.

---
+++ databases/mysql57-client/patches/patch-cmd-line-utils_libedit_chartype.h
@@ -0,0 +1,16 @@
+$NetBSD: patch-cmd-line-utils_libedit_chartype.h,v 1.1 2018/10/01 00:11:29 sevan Exp $
+
+Exclude DragonFly & OpenBSD
+
+--- cmd-line-utils/libedit/chartype.h.orig	2018-09-30 16:10:45.673504000 -0700
++++ cmd-line-utils/libedit/chartype.h	2018-09-30 16:12:19.519385000 -0700
+@@ -49,7 +49,8 @@
+   TODO : Verify if FreeBSD & AIX stores ISO 10646 in wchar_t. */
+ #if !defined(__NetBSD__) && !defined(__sun) \
+   && !(defined(__APPLE__) && defined(__MACH__)) \
+-  && !defined(__FreeBSD__) && !defined(_AIX)
++  && !defined(__FreeBSD__) && !defined(_AIX) \
++  && !defined(__DragonFly__) && !defined(__OpenBSD__)
+ #ifndef __STDC_ISO_10646__
+ /* In many places it is assumed that the first 127 code points are ASCII
+  * compatible, so ensure wchar_t indeed does ISO 10646 and not some other
