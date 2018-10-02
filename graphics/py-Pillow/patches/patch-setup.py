625fd303ab873e977a127957047e2b5b76fcee78
adam <adam@pkgsrc.org>
2018-10-02 09:10:46 +0000

    py-Pillow: updated to 5.3.0
    5.3.0:
    Changed Image size property to be read-only by default
    Add warnings if image file identification fails due to lack of WebP support
    Hide the Ghostscript progress dialog popup on Windows
    Adding support to reading tiled and YcbCr jpeg tiffs through libtiff
    Fixed None as TIFF compression argument
    Changed GIF seek to remove previous info items
    Improved PDF document info
    Add line width parameter to rectangle and ellipse-based shapes
    Fixed decompression bomb check in _crop
    Added support to ImageDraw.floodfill for non-RGB colors
    Tests: Avoid catching unexpected exceptions in tests
    Use TextIOWrapper.detach() instead of NoCloseStream
    Added transparency to matrix conversion
    Added ImageOps pad method
    Give correct extrema for I;16 format images
    Added PySide2
    Corrected TIFF tags
    CI: Install CFFI and pycparser without any PYTHONOPTIMIZE
    Read/Save RGB webp as RGB (instead of RGBX)
    ImageDraw: Add line joints
    Improved performance of ImageDraw floodfill method
    Fix builds with --parallel
    Add more raw Tiff modes (RGBaX, RGBaXX, RGBAX, RGBAXX)
    Close existing WebP fp before setting new fp
    Add orientation, compression and id_section as TGA save keyword arguments
    Convert int values of RATIONAL TIFF tags to floats
    Fix code for PYTHONOPTIMIZE
    Changed ImageFilter.Kernel to subclass ImageFilter.BuiltinFilter, instead of the other way around
    Remove unused draw.draw_line, draw.draw_point and font.getabc methods
    Tests: Added ImageFilter tests
    Tests: Added ImageChops tests
    AppVeyor: Download lib if not present in pillow-depends
    Travis CI: Add Python 3.7 and Xenial
    Docs: Added documentation for NumPy conversion
    Depends: Update libimagequant to 2.12.1
    Add three-color support to ImageOps.colorize
    Tests: Add LA to TGA test modes
    Skip outline if the draw operation fills with the same colour
    Flake8 fixes
    Avoid deprecated 'U' mode when opening files

--- graphics/py-Pillow/patches/patch-setup.py
+++ graphics/py-Pillow/patches/patch-setup.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-setup.py,v 1.16 2018/07/03 07:44:00 adam Exp $
+$NetBSD: patch-setup.py,v 1.17 2018/10/02 09:10:46 adam Exp $
 
 Disable mp_compile hack; it has problems with native parallel building.
 Prevent to detect optional tkinter.
 
---- setup.py.orig	2018-07-01 19:02:58.000000000 +0000
+--- setup.py.orig	2018-10-01 05:51:22.000000000 +0000
 +++ setup.py
 @@ -23,7 +23,6 @@ from setuptools import Extension, setup
  
@@ -26,3 +26,16 @@ Prevent to detect optional tkinter.
  
  NAME = 'Pillow'
  PILLOW_VERSION = get_version()
+@@ -205,12 +200,6 @@ class pil_build_ext(build_ext):
+         if self.debug:
+             global DEBUG
+             DEBUG = True
+-        if sys.version_info >= (3, 5) and not self.parallel:
+-            # For Python < 3.5, we monkeypatch distutils to have parallel
+-            # builds. If --parallel (or -j) wasn't specified, we want to
+-            # reproduce the same behavior as before, that is, auto-detect the
+-            # number of jobs.
+-            self.parallel = mp_compile.MAX_PROCS
+         for x in self.feature:
+             if getattr(self, 'disable_%s' % x):
+                 setattr(self.feature, x, False)
