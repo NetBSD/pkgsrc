b4342a83ee7df97a9b477b628ebe2584f8fee77c
nia <nia@pkgsrc.org>
2018-10-03 22:27:05 +0000

    net/miniupnpc: update to version 2.1
    Changes:
    
    VERSION 2.1 : released 2018/05/07
    
    2018/05/07:
      CMake Modernize and cleanup CMakeLists.txt
      Update MS Visual Studio projects
    
    2018/04/30:
      listdevices: show devices sorted by XML desc URL
    
    2018/04/26:
      Small fix in miniupnpcmodule.c (python module)
      Support cross compiling in Makefile.mingw
    
    2018/04/06:
      Use SOCKET type instead of int (for Win64 compilation)
      Increments API_VERSION to 17
    
    2018/02/22:
      Disable usage of MiniSSDPd when using -m option
    
    2017/12/11:
      Fix buffer over run in minixml.c
      Fix uninitialized variable access in upnpreplyparse.c
    
    2017/05/05:
      Fix CVE-2017-8798   Thanks to tin/Team OSTStrom
    
    2016/11/11:
      check strlen before memcmp in XML parsing portlistingparse.c
      fix build under SOLARIS and CYGWIN
    
    2016/10/11:
      Add python 3 compatibility to IGD test

--- net/miniupnpc/patches/patch-minissdpc.c
+++ net/miniupnpc/patches/patch-minissdpc.c
@@ -1,14 +1,15 @@
-$NetBSD: patch-minissdpc.c,v 1.1 2017/07/03 11:53:57 wiedi Exp $
+$NetBSD: patch-minissdpc.c,v 1.2 2018/10/03 22:27:05 nia Exp $
 
 need sockio.h for SIOCGIFADDR on SunOS
---- minissdpc.c.orig	2016-01-19 10:26:58.000000000 +0000
+
+--- minissdpc.c.orig	2018-04-06 10:53:30.000000000 +0000
 +++ minissdpc.c
-@@ -11,6 +11,9 @@
- #include <string.h>
- #include <stdlib.h>
- #include <sys/types.h>
+@@ -14,6 +14,9 @@
+ #if defined (__NetBSD__)
+ #include <net/if.h>
+ #endif
 +#if defined(__sun)
-+#include <sys/sockio.h> 
++#include <sys/sockio.h>
 +#endif
  #if defined(_WIN32) || defined(__amigaos__) || defined(__amigaos4__)
  #ifdef _WIN32
