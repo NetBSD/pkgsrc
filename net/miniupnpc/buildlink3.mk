4fc75e4e5957612aacee9a0107467a525342c148
nia <nia@pkgsrc.org>
2018-10-03 22:27:05 +0000

    net/miniupnpc: update to version 2.1
    Changes:
    
    VERSION 2.1 : released 2018/05/07
    
    2018/05/07:
      CMake Modernize and cleanup CMakeLists.txt
      Update MS Visual Studio projects
    
    2018/04/30:
      listdevices: show devices sorted by XML desc URL
    
    2018/04/26:
      Small fix in miniupnpcmodule.c (python module)
      Support cross compiling in Makefile.mingw
    
    2018/04/06:
      Use SOCKET type instead of int (for Win64 compilation)
      Increments API_VERSION to 17
    
    2018/02/22:
      Disable usage of MiniSSDPd when using -m option
    
    2017/12/11:
      Fix buffer over run in minixml.c
      Fix uninitialized variable access in upnpreplyparse.c
    
    2017/05/05:
      Fix CVE-2017-8798   Thanks to tin/Team OSTStrom
    
    2016/11/11:
      check strlen before memcmp in XML parsing portlistingparse.c
      fix build under SOLARIS and CYGWIN
    
    2016/10/11:
      Add python 3 compatibility to IGD test

--- net/miniupnpc/buildlink3.mk
+++ net/miniupnpc/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/12/01 22:28:29 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/10/03 22:27:05 nia Exp $
 
 BUILDLINK_TREE+=	miniupnpc
 
 .if !defined(MINIUPNPC_BUILDLINK3_MK)
 MINIUPNPC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.miniupnpc+=	miniupnpc>=2.0
+BUILDLINK_API_DEPENDS.miniupnpc+=	miniupnpc>=2.1
 BUILDLINK_PKGSRCDIR.miniupnpc?=		../../net/miniupnpc
 .endif	# MINIUPNPC_BUILDLINK3_MK
 
