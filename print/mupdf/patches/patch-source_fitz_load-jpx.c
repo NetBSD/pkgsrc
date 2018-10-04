ab9c8ecf30c02e8f2422ed0fbb25305b1463826b
leot <leot@pkgsrc.org>
2018-10-04 19:02:33 +0000

    mupdf: Update print/mupdf to 1.14.0
    pkgsrc changes:
     - Pass USE_SYSTEM_LIBS=yes via MAKE_ENV in order to honor system (pkgsrc)
       packages {C,LD}FLAGS
     - Rewrite the logic to disable lcms2 in patch-ab and inject -DNO_ICC
       directly via CFLAGS
    
    Changes:
    MuPDF 1.14.0
    ------------
    * New features:
        * Added "Source Han Serif" CJK fallback font.
        * Added more scripts to the Noto fallback fonts.
        * Multi-page PNM support.
        * "mutool show" now supports a path syntax for selecting objects to show.
    
    * Build system simplifications:
        * Auto-generated CMap, ICC, and JS source files are checked in to git.
        * Embedded CMap resources are now generated by a python script.
        * Embedded font resources are linked directly if using GNU ld or windows.
        * Namegen tool replaced by use of C macros.
        * Simplified Makefile.
    
    * Annotation editing:
        * New annotation editing mode in mupdf-gl.
        * Can create, edit, and delete most annotation types.
        * Can create appearance streams for most annotation types.
        * Can create appearance streams for Tx form fields.
        * Can create appearance streams for Ch form fields.
    
    * Form filling in mupdf-gl:
        * Can click buttons, checkboxes, and radioboxes.
        * Can fill out text fields using dialog box.
        * Can select choice options using dialog box.
        * Can verify and sign digital signatures.
    
    * Improved UI for mupdf-gl:
        * Password dialog.
        * Error dialog.
        * Open/save file dialog.
        * Snap selection to words or lines by holding control or control+shift.
        * Save and restore current location, bookmarks, and navigation history.
    
    * Bug fixes:
        * Improved CJK character fallback handling in EPUB.
    
    * API changes:
        * Pass rectangle and matrix structs by value.
        * Replaced PDF_NAME_Xxx macros with PDF_NAME(Xxx).
        * Added PDF_TRUE, PDF_FALSE, and PDF_NULL constant pdf_obj* macros.
        * Added helper functions: pdf_dict_get_int, etc.
        * Removed 'doc' argument in pdf_new_int, etc.
        * Quads instead of rects when highlighting and searching text.
        * mutool run: Pass arguments to script in scriptArgs global.

--- print/mupdf/patches/patch-source_fitz_load-jpx.c
+++ print/mupdf/patches/patch-source_fitz_load-jpx.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-source_fitz_load-jpx.c,v 1.8 2018/05/06 13:19:10 leot Exp $
+$NetBSD: patch-source_fitz_load-jpx.c,v 1.9 2018/10/04 19:02:33 leot Exp $
 
 - MuPDF does some locking around its allocation calls; it overrides openjpeg's
   allocators to do this locking too. However mupdf tries to manually align things
@@ -13,27 +13,30 @@ $NetBSD: patch-source_fitz_load-jpx.c,v 1.8 2018/05/06 13:19:10 leot Exp $
 
   From OpenBSD ports textproc/mupdf/patch-source_fitz_load-jpx_c,v 1.5.
 
---- source/fitz/load-jpx.c.orig	2018-04-12 13:14:06.000000000 +0000
+--- source/fitz/load-jpx.c.orig	2018-09-25 12:39:17.000000000 +0000
 +++ source/fitz/load-jpx.c
-@@ -548,30 +548,14 @@ void opj_free(void *ptr)
+@@ -577,19 +577,7 @@ void opj_free(void *ptr)
  
- void * opj_aligned_malloc(size_t size)
+ static void * opj_aligned_malloc_n(size_t alignment, size_t size)
  {
 -	uint8_t *ptr;
 -	int off;
- 
+-
 -	if (size == 0)
 -		return NULL;
 -
--	size += 16 + sizeof(uint8_t);
+-	size += alignment + sizeof(uint8_t);
 -	ptr = opj_malloc(size);
 -	if (ptr == NULL)
 -		return NULL;
--	off = 16-(((int)(intptr_t)ptr) & 15);
+-	off = alignment-(((int)(intptr_t)ptr) & (alignment - 1));
 -	ptr[off-1] = off;
 -	return ptr + off;
 +	return opj_malloc(size);
  }
+ 
+ void * opj_aligned_malloc(size_t size)
+@@ -604,13 +592,7 @@ void * opj_aligned_32_malloc(size_t size
  
  void opj_aligned_free(void* ptr_)
  {
@@ -44,7 +47,6 @@ $NetBSD: patch-source_fitz_load-jpx.c,v 1.8 2018/05/06 13:19:10 leot Exp $
 -
 -	off = ptr[-1];
 -	opj_free((void *)(((unsigned char *)ptr) - off));
-+	
 +	opj_free(ptr_);
  }
  
