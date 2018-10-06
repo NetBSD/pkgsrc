33c0183c7d31aed1f781f0fe5cd11f77fa5e342c
adam <adam@pkgsrc.org>
2018-10-06 22:41:22 +0000

    Added x11/qt5-qtcharts, x11/qt5-qtnetworkauth

---
+++ x11/qt5-qtcharts/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2018/10/06 22:41:22 adam Exp $
+
+BUILDLINK_TREE+=	qt5-qtcharts
+
+.if !defined(QT5_QTCHARTS_BUILDLINK3_MK)
+QT5_QTCHARTS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt5-qtcharts+=	qt5-qtcharts>=5.11.2
+BUILDLINK_ABI_DEPENDS.qt5-qtcharts+=	qt5-qtcharts>=5.11.2
+BUILDLINK_PKGSRCDIR.qt5-qtcharts?=	../../x11/qt5-qtcharts
+
+BUILDLINK_INCDIRS.qt5-qtcharts+=	qt5/include
+BUILDLINK_LIBDIRS.qt5-qtcharts+=	qt5/lib
+BUILDLINK_LIBDIRS.qt5-qtcharts+=	qt5/plugins
+
+.include "../../x11/qt5-qtdeclarative/buildlink3.mk"
+.endif	# QT5_QTCHARTS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt5-qtcharts
