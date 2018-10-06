75c4443b052045bca6f420e095af418ca7564d6a
spz <spz@pkgsrc.org>
2018-10-06 16:14:20 +0000

    Pullup ticket #5837 - requested by nia net/dnscrypt-proxy2: build fix
    Revisions pulled up:
    - net/dnscrypt-proxy2/distinfo                                  1.3
    - net/dnscrypt-proxy2/patches/patch-vendor_github.com_aead_poly1305_poly1305__amd64.s 1.1
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	nia
       Date:		Sun Sep 30 19:56:47 UTC 2018
    
       Modified Files:
       	pkgsrc/net/dnscrypt-proxy2: distinfo
       Added Files:
       	pkgsrc/net/dnscrypt-proxy2/patches:
       	    patch-vendor_github.com_aead_poly1305_poly1305__amd64.s
    
       Log Message:
       dnscrypt-proxy2: resolve "relocation target runtime.support_avx2 not defined"
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.2 -r1.3 pkgsrc/net/dnscrypt-proxy2/distinfo
       cvs rdiff -u -r0 -r1.1 \
           pkgsrc/net/dnscrypt-proxy2/patches/patch-vendor_github.com_aead_poly1305_poly1305__amd64.s

---
+++ net/dnscrypt-proxy2/patches/patch-vendor_github.com_aead_poly1305_poly1305__amd64.s
@@ -0,0 +1,14 @@
+$NetBSD: patch-vendor_github.com_aead_poly1305_poly1305__amd64.s,v 1.2.2.2 2018/10/06 16:14:20 spz Exp $
+
+"poly1305.supportsAVX2: relocation target runtime.support_avx2 not defined"
+
+--- vendor/github.com/aead/poly1305/poly1305_amd64.s.orig	2018-07-09 16:19:45.000000000 +0000
++++ vendor/github.com/aead/poly1305/poly1305_amd64.s
+@@ -150,6 +150,6 @@ TEXT ·finalize(SB), $0-16
+ 
+ // func supportsAVX2() bool
+ TEXT ·supportsAVX2(SB), 4, $0-1
+-	MOVQ runtime·support_avx2(SB), AX
++	MOVQ $0X0, AX
+ 	MOVB AX, ret+0(FP)
+ 	RET
