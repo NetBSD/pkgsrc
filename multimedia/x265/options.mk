a26c8f227120ca314a06b41440b7faebb2abd638
wiz <wiz@pkgsrc.org>
2018-10-06 15:41:56 +0000

    x265: rename 'main10' option to 'x265-main10' since it's very package-specific

--- multimedia/x265/options.mk
+++ multimedia/x265/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.1 2018/10/06 12:12:48 adam Exp $
+# $NetBSD: options.mk,v 1.2 2018/10/06 15:41:56 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.x265
-PKG_SUPPORTED_OPTIONS=	main10
+PKG_SUPPORTED_OPTIONS=	x265-main10
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mmain10)
+.if !empty(PKG_OPTIONS:Mx265-main10)
 CMAKE_ARGS+=	-DHIGH_BIT_DEPTH=ON
 .endif
