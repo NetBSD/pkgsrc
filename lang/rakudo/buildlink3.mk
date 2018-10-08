a2209b0b5f48d350406a76c552d3eab46fd804c9
maya <maya@pkgsrc.org>
2018-10-08 16:54:28 +0000

    Update rakudo, nqp, MoarVM to 2018.06
    Bumping buildlink3.mk required because the reporter mentions having
    to do this because of a time-consuming incompatibility in p6-zip.
    Unclear if the packages are independent, so updating together.
    
    No changelog found.
    
    From tomasz motyl in PR pkg/53663

--- lang/rakudo/buildlink3.mk
+++ lang/rakudo/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/04/29 20:53:39 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/10/08 16:54:28 maya Exp $
 
 BUILDLINK_TREE+=	rakudo
 
 .if !defined(RAKUDO_BUILDLINK3_MK)
 RAKUDO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rakudo+=		rakudo>=2017.09
-BUILDLINK_ABI_DEPENDS.rakudo+=		rakudo>=2017.09
+BUILDLINK_API_DEPENDS.rakudo+=		rakudo>=2018.06
+BUILDLINK_ABI_DEPENDS.rakudo+=		rakudo>=2018.06
 BUILDLINK_PKGSRCDIR.rakudo?=		../../lang/rakudo
 
 .include "../../lang/nqp/buildlink3.mk"
