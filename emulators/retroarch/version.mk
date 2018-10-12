d1e7b8b6270d98e08393238340b3de8d73183be5
nia <nia@pkgsrc.org>
2018-10-12 23:43:46 +0000

    retroarch: update to version 1.7.5.
    Changes:
    
        CAMERA: Fix Video4Linux2 driver that broke years ago.
        CONFIG: Add 'Reset To Defaults' setting in Configurations. Thi will reset your config file to defaults.
        CHEATS: Add support for Rumble when increase or decrease by the rumble value.
        CHEATS: Add cheat variables to allow for updating large portions of memory.
        CHEEVOS: Prevent loading states before achievements are fully loaded.
        CRT: New porches and interlaced bug fix.
        CRT: New functionality, ability to switch between 15KHz and 31KHz, etc.
        COMMON: Support for "OEM-102" key (usually '' on Euro keyboards).
        INPUT: Add new menu toggle combos 'L3 + R' and 'L + R' (useful for Switch).
        LOCALIZATION: Update Portuguese / Brazilian translation.
        LOCALIZATION: Update Japanese translation.
        LOCALIZATION: Update Polish translation.
        LOCALIZATION: Update Spanish translation.
        MENU: Add dropdown lists for many settings.
        MENU: Fix crash that could happen when changing core's options on Android.
        MENU/QT/WIMP: Add option to rename playlists.
        MENU/QT/WIMP: Add option to filter extensions inside archives when adding to a playlist.
        MENU/QT/WIMP: Rename playlist entries with 2 single clicks.
        MENU/QT/WIMP: Fix shader parameter checkboxes not working
        METAL: Add screenshot support.
        NETPLAY: Save lobby details received back from server after first announcement.
        OPENGL/GLX: Implement Adaptive VSync - GLX_EXT_swap_control_tear.
        OPENGL/WGL: Implement Adaptive VSync - WGL_EXT_swap_control_tear.
        RUNAHEAD: Fix performance degradation that could happen over time (after approx. 30 mins). Fixed input IDs outside of range 0-35 causing slow performance in runahead.
        VULKAN: Fix race condition in threaded mailbox emulation.
        VULKAN: Maintenance fixes.

--- emulators/retroarch/version.mk
+++ emulators/retroarch/version.mk
@@ -1,8 +1,8 @@
-# $NetBSD: version.mk,v 1.3 2018/09/06 09:33:38 nia Exp $
+# $NetBSD: version.mk,v 1.4 2018/10/12 23:43:47 nia Exp $
 #
 # used by emulators/libretro-core-info/Makefile
 # used by emulators/retroarch/Makefile
 
 .if !defined(RETROARCH_VERSION)
-RETROARCH_VERSION=	1.7.4
+RETROARCH_VERSION=	1.7.5
 .endif
