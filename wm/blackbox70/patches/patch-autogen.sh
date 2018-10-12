c3992f1bf8549772a4b4bd4514564cd7cf64f15a
he <he@pkgsrc.org>
2018-10-12 18:00:55 +0000

    Switch upstream for blackbox70, and upgrade to version 0.72.
    The old upstream last had a release in November 2005, and the new
    upstream in May this year.  In recent rebuilds, the old version
    has developed "focus issues" when run with "sloppy focus", as is
    my preference. The new upstream is a fork of blackbox but where
    this among other issues have been fixed.
    
    The patches have also been re-done and partly renamed, and some of
    the old patches to build with SunPro have not been carried forward,
    as I don't have a suitable system to verify that these are still
    required.

---
+++ wm/blackbox70/patches/patch-autogen.sh
@@ -0,0 +1,10 @@
+$NetBSD: patch-autogen.sh,v 1.1 2018/10/12 18:00:55 he Exp $
+
+--- autogen.sh.orig	2016-07-10 16:31:38.000000000 +0000
++++ autogen.sh
+@@ -1,4 +1,4 @@
+-#!/bin/bash
++#!/bin/sh
+ 
+ cd "$(dirname "$0")"
+ 
