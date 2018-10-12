fcc9dca5ad6b3586869408b0a20e2bbb37ab4ed2
leot <leot@pkgsrc.org>
2018-10-12 23:17:34 +0000

    robobrowser: Fix patch-robobrowser_helpers.py for non-3.7 Python-s

--- www/py-robobrowser/patches/patch-robobrowser_helpers.py
+++ www/py-robobrowser/patches/patch-robobrowser_helpers.py
@@ -1,14 +1,17 @@
-$NetBSD: patch-robobrowser_helpers.py,v 1.1 2018/10/12 23:08:10 leot Exp $
+$NetBSD: patch-robobrowser_helpers.py,v 1.2 2018/10/12 23:17:34 leot Exp $
 
 Add support for Python 3.7.
 
 --- robobrowser/helpers.py.orig	2015-06-07 19:27:41.000000000 +0000
 +++ robobrowser/helpers.py
-@@ -3,6 +3,7 @@ Miscellaneous helper functions
+@@ -3,6 +3,10 @@ Miscellaneous helper functions
  """
  
  import re
-+re._pattern_type = getattr(re, '_pattern_type', re.Pattern)
++try:
++    re._pattern_type
++except AttributeError:
++    re._pattern_type = re.Pattern
  
  from bs4 import BeautifulSoup
  from bs4.element import Tag
