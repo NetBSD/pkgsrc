16dc57f29ea562821189738ddbc4baa69128064f
nia <nia@pkgsrc.org>
2018-10-13 17:02:16 +0000

    libretro-reicast: import version 20181013
    Libretro is a simple but powerful development interface that allows for the
    easy creation of emulators, games and multimedia applications that can plug
    straight into any libretro-compatible frontend. This development interface is
    open to others so that they can run these pluggable emulator and game cores
    also in their own programs or devices.
    
    Reicast is a multi-platform Sega Dreamcast emulator.

---
+++ emulators/libretro-reicast/options.mk
@@ -0,0 +1,28 @@
+# $NetBSD: options.mk,v 1.1 2018/10/13 17:02:16 nia Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.libretro-reicast
+PKG_OPTIONS_REQUIRED_GROUPS=	graphics
+PKG_OPTIONS_GROUP.graphics=	opengl
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if !empty(MACHINE_ARCH:M*arm*)
+PKG_OPTIONS_GROUP.graphics+=	rpi
+.endif
+
+.if !empty(MACHINE_ARCH:Mearm*) && ${OPSYS} == "NetBSD"
+PKG_SUGGESTED_OPTIONS+=		rpi
+.else
+PKG_SUGGESTED_OPTIONS+=		opengl
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mopengl)
+MAKE_FLAGS+=	HAVE_OIT=1
+.include "../../graphics/MesaLib/buildlink3.mk"
+.elif !empty(PKG_OPTIONS:Mrpi)
+MAKE_FLAGS+=	GLES=1
+MAKE_FLAGS+=	GL_LIB="-lbrcmGLESv2"
+.include "../../misc/raspberrypi-userland/buildlink3.mk"
+.endif
