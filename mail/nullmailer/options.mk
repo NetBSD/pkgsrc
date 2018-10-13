42245cb43fa250f054882a734c44d434acf55f1e
schmonz <schmonz@pkgsrc.org>
2018-10-13 14:38:54 +0000

    Update to 2.2. From the changelog:
    - nullmailer-send no longer generates bounces for rejected bounces.
      Thanks Fejes József
    
    - Fixed compile error in sendmail on GCC older than 4.9.
    
    - Fixed treating authentication failure as message rejection.
      Thanks Fejes József
    
    - nullmailer-inject now sets the full name of the sender to the user
      name as a fallback. This helps distinguish system sent messages when
      the MTA rewrites the address (as does GMail, for example).
    
    - Fixed compatibility issue with gnutls 3.6 (and possibly others).

--- mail/nullmailer/options.mk
+++ mail/nullmailer/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2017/10/28 04:51:06 schmonz Exp $
+# $NetBSD: options.mk,v 1.3 2018/10/13 14:38:54 schmonz Exp $
 
 PKG_OPTIONS_VAR=        PKG_OPTIONS.nullmailer
 PKG_SUPPORTED_OPTIONS=  gnutls
@@ -11,4 +11,4 @@ PKG_SUGGESTED_OPTIONS=  gnutls
 CONFIGURE_ARGS+=	--enable-tls
 .else
 CONFIGURE_ARGS+=	--disable-tls
-.endif # option gnutls
+.endif
