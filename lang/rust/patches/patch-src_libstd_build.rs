22dd4a9d1fabd70d6dba72e313bae166c4cae950
he <he@pkgsrc.org>
2018-10-13 10:03:36 +0000

    Add the bits required to build rust on NetBSD/powerpc ports, and point to the bootstrap kit for NetBSD/powerpc I'm hosting at the moment.
    Also add the bits I used when cross-building the NetBSD/powerpc
    rust on amd64, commented out, as well as the gcc / c++ wrapper
    script I used in the process.
    
    The changes affecting other ports are:
     * We now add LD_LIBRARY_PATH in the make environment, so that
       if the bootstrap kit binaries and shared libraries don't have
       the $ORIGIN-style RPATH entries, it will still work
     * The bootstrap.py script has been changed to turn off the
       generation of debuginfo in "RUSTFLAGS"; for some so far unknown
       reason, the NetBSD/powerpc rust will not build if you ask for
       debug info.  This could perhaps have been made OS-variant dependent,
       but isn't at the moment.
    
    So .. bump PKGREVISION.

--- lang/rust/patches/patch-src_libstd_build.rs
+++ lang/rust/patches/patch-src_libstd_build.rs
@@ -1,12 +1,20 @@
-$NetBSD: patch-src_libstd_build.rs,v 1.1 2017/11/16 19:48:24 jperkin Exp $
+$NetBSD: patch-src_libstd_build.rs,v 1.2 2018/10/13 10:03:37 he Exp $
 
 SunOS support:
  - Support PKGSRC_USE_SSP (ugly for now).
  - Add libumem support.
+ - Add libexecinfo for backtrace().
 
 --- src/libstd/build.rs.orig	2017-10-09 20:04:51.000000000 +0000
 +++ src/libstd/build.rs
-@@ -46,6 +46,9 @@ fn main() {
+@@ -43,12 +43,16 @@ fn main() {
+         println!("cargo:rustc-link-lib=pthread");
+     } else if target.contains("dragonfly") || target.contains("bitrig") ||
+               target.contains("netbsd") || target.contains("openbsd") {
++        println!("cargo:rustc-link-lib=execinfo");
+         println!("cargo:rustc-link-lib=pthread");
+     } else if target.contains("solaris") {
+         println!("cargo:rustc-link-lib=socket");
          println!("cargo:rustc-link-lib=posix4");
          println!("cargo:rustc-link-lib=pthread");
          println!("cargo:rustc-link-lib=resolv");
