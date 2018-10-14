0bdf7c233a2d6ff2e1f6e4666d3fe6365c200fad
schmonz <schmonz@pkgsrc.org>
2018-10-14 12:17:50 +0000

    Update to 3.47. From the changelog:
    - fixed xterm256 and truecolor whitespace output #2
      (https://gitlab.com/saalen/highlight/issues/90)
    - fixed LaTeX, TeX, SVG and ODT whitespace output (regression of 3.45)
    - added darkplus theme (https://gitlab.com/saalen/highlight/merge_requests/84)
    - converted ChangeLog to AsciiDoc
    - allowed state test indicators to match both whitespace (ws) and the
      enclosing state (others)
    - CLI: default output changed to xterm256 or truecolor if run in a
      terminal with color support and only a single file is outputted
    - GUI: added checkbox in the clipboard tab to output selected lines only

--- textproc/libhighlight/patches/patch-src_core_platform__fs.cpp
+++ textproc/libhighlight/patches/patch-src_core_platform__fs.cpp
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_core_platform__fs.cpp,v 1.1 2018/05/03 13:13:51 schmonz Exp $
+$NetBSD: patch-src_core_platform__fs.cpp,v 1.2 2018/10/14 12:17:50 schmonz Exp $
 
 Fix build on (at least) SmartOS.
 
---- src/core/platform_fs.cpp.orig	2018-01-20 20:42:17.000000000 +0000
+--- src/core/platform_fs.cpp.orig	2018-10-13 19:26:45.000000000 +0000
 +++ src/core/platform_fs.cpp
-@@ -72,13 +72,13 @@ std::string getHomePath()
- #include <pwd.h>
+@@ -79,13 +79,13 @@ int isColorEscCapable() {
+ 
  const char pathSeparator = '/';
  
 -std::string getAppPath()
