b481a3b74945d8fc015228ab07f7b616feb556cc
nia <nia@pkgsrc.org>
2018-10-14 03:35:44 +0000

    libretro-reicast: stick to gl3 for now, considering the version of mesalib.

--- emulators/libretro-reicast/options.mk
+++ emulators/libretro-reicast/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2018/10/13 17:02:16 nia Exp $
+# $NetBSD: options.mk,v 1.2 2018/10/14 03:35:44 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.libretro-reicast
 PKG_OPTIONS_REQUIRED_GROUPS=	graphics
@@ -19,7 +19,7 @@ PKG_SUGGESTED_OPTIONS+=		opengl
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mopengl)
-MAKE_FLAGS+=	HAVE_OIT=1
+MAKE_FLAGS+=	HAVE_GL3=1
 .include "../../graphics/MesaLib/buildlink3.mk"
 .elif !empty(PKG_OPTIONS:Mrpi)
 MAKE_FLAGS+=	GLES=1
