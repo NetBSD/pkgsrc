9c3186aa2c1dc672c403c32898a20481ceb3776b
jaapb <jaapb@pkgsrc.org>
2018-10-15 11:26:59 +0000

    Updated security/ocaml-safepass to version 3.0.
    Changes include:
    - use jbuilder for building
    - allow picking different versions of Bcrypt hashes
    - use unbuffered IO to read only required number of bytes from /dev/urandom

--- security/ocaml-safepass/buildlink3.mk
+++ security/ocaml-safepass/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/07/19 15:15:28 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/10/15 11:26:59 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-safepass
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-safepass
 OCAML_SAFEPASS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-safepass+=	ocaml-safepass>=2.0
-BUILDLINK_ABI_DEPENDS.ocaml-safepass+=	ocaml-safepass>=2.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-safepass+=	ocaml-safepass>=3.0
 BUILDLINK_PKGSRCDIR.ocaml-safepass?=	../../security/ocaml-safepass
 .endif	# OCAML_SAFEPASS_BUILDLINK3_MK
 
