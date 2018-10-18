10b6a60a01310f01e044e558a0e7e9726e07ba63
bsiegert <bsiegert@pkgsrc.org>
2018-10-18 19:54:32 +0000

    Update spamassassin to 3.4.2.
    From Attila Fueloep in pull request NetBSD/pkgsrc#32.
    
    Apache SpamAssassin 3.4.2 contains numerous tweaks and bug fixes over the
    past three and 1/2 years.  As we release 3.4.2, we are preparing 4.0.0 which
    will move us into a full UTF-8 environment.  We expect one final 3.4.3
    release.
    
    As with any release there are a number of functional patches, improvements as
    well as security reasons to upgrade to 3.4.2.  In this case we have over 3
    years of issues being resolved at once.  And we are laying thr groundwork for
    version 4.0 which is is designed to more natively handle UTF-8.
    
    However, there is one specific pressing reason to upgrade.  Specifically, we
    will stop producing SHA-1 signatures for rule updates.  This means that while
    we produce rule updates with the focus on them working for any release from
    v3.3.2 forward, they will start failing SHA-1 validation for sa-update.
    
    *** If you do not update to 3.4.2, you will be stuck at the last ruleset
        with SHA-1 signatures in the near future. ***
    
    Full release notes at http://svn.apache.org/repos/asf/spamassassin/branches/3.4/build/announcements/3.4.2.txt.

--- mail/spamassassin/patches/patch-spamc_libspamc.c
+++ mail/spamassassin/patches/patch-spamc_libspamc.c
@@ -1,28 +1,24 @@
-$NetBSD: patch-spamc_libspamc.c,v 1.1 2018/03/17 09:14:35 tnn Exp $
+$NetBSD: patch-spamc_libspamc.c,v 1.2 2018/10/18 19:54:32 bsiegert Exp $
 
 Set the client protocol to TLS instead of SSLv3.
 Fixes build with current openssl.
 
 --- spamc/libspamc.c.orig	2015-04-28 19:56:59.000000000 +0000
 +++ spamc/libspamc.c
-@@ -1213,11 +1213,7 @@ int message_filter(struct transport *tp,
+@@ -1214,7 +1214,7 @@
      if (flags & SPAMC_USE_SSL) {
  #ifdef SPAMC_SSL
  	SSLeay_add_ssl_algorithms();
--	if (flags & SPAMC_TLSV1) {
--	    meth = TLSv1_client_method();
--	} else {
--	    meth = SSLv3_client_method(); /* default */
--	}
+-	meth = SSLv23_client_method();
 +	meth = TLSv1_client_method();
  	SSL_load_error_strings();
  	ctx = SSL_CTX_new(meth);
  #else
-@@ -1604,7 +1600,7 @@ int message_tell(struct transport *tp, c
+@@ -1601,7 +1601,7 @@
      if (flags & SPAMC_USE_SSL) {
  #ifdef SPAMC_SSL
  	SSLeay_add_ssl_algorithms();
--	meth = SSLv3_client_method();
+-	meth = SSLv23_client_method();
 +	meth = TLSv1_client_method();
  	SSL_load_error_strings();
  	ctx = SSL_CTX_new(meth);
