a48a04fd9b7cb8b787880702f4d968aa716e96a8
adam <adam@pkgsrc.org>
2018-10-18 16:25:40 +0000

    ntopng: updated to 3.6.1
    3.6.1 Stable
    Brew formula fixes
    
    3.6 Stable
    
    New features
    ------------
    New pro charts
    Ability to compare data with the past (time shift)
    Trend lines based on ASAP
    Average and percentile lines overlayed on the graph and animated
    New color scheme that uses pastel colors for better visualization
    https://www.ntop.org/ntopng/ntopng-and-time-series-from-rrd-to-influxdb-new-charts-with-time-shift/
    New timeseries API with support for RRD and InfluxDB
    Abstracts and handles multiple sources transparently
    https://www.ntop.org/guides/ntopng/api/lua/timeseries/index.html
    Streaming pcap captures with BPF support
    Download live packet captures right from the browser
    New SNMP devices caching
    Periodically cache information of all the SNMP device configured
    Calculate and visualize interfaces throughput
    
    Improvements
    ------------
    Security
    Access to the web user interface is controlled with ACLs
    Secure ntopng cookies with SameSite and HttpOnly
    HTTP cookie authentication
    Improved random session id generation
    Various SNMP improvemenets
    Caching
    Interfaces status change alerts
    Device interfaces page
    Devices and interfaces added to flows
    Fixed several library memory leaks
    Improved device and interface charts
    Interfaces throughput calculation and visualization
    Ability to delete all SNMP devices at once
    Improved active devices discovery
    OS detection via HTTP User-Agent
    Alerts
    Crypto miners alerts toggle
    Detection and alerting of anomalous terminations
    Module for sending telegram.org alerts
    Slack
    Configurable Slack channel names
    Added Slack test button
    Charts
    Active flows vs local hosts chart
    Active flows vs interface traffic chart
    Ubuntu 18.04 support
    Support for ElasticSearch 6 export
    Added support for custom categories lists
    Added ability to use the non-JIT Lua interpreter
    Improved ntopng startup and shutdown time
    Support for capturing from interface pairs with PF_RING ZC
    Support for variable PPP header lenght
    Migrated geolocation to GeoLite2 and libmaxminddb
    Configuration backup and restore
    Improved IE browser support
    Using client SSL certificate for protocol detection
    Optimized host/flows purging

--- net/ntopng/patches/patch-src_Redis.cpp
+++ net/ntopng/patches/patch-src_Redis.cpp
@@ -1,20 +1,44 @@
-$NetBSD: patch-src_Redis.cpp,v 1.1 2016/04/08 16:59:07 adam Exp $
+$NetBSD: patch-src_Redis.cpp,v 1.2 2018/10/18 16:25:40 adam Exp $
 
-Allow Redis connection through unix socket.
+Allow Redis connection through a socket.
 
---- src/Redis.cpp.orig	2016-04-06 11:41:42.000000000 +0000
+--- src/Redis.cpp.orig	2018-09-19 15:35:27.000000000 +0000
 +++ src/Redis.cpp
-@@ -75,7 +75,12 @@ void Redis::reconnectRedis() {
+@@ -35,9 +35,7 @@ Redis::Redis(const char *_redis_host, co
+   redis_host = _redis_host ? strdup(_redis_host) : NULL;
+   redis_password = _redis_password ? strdup(_redis_password) : NULL;
+   redis_port = _redis_port, redis_db_id = _redis_db_id;
+-#ifdef __linux__
+   is_socket_connection = false;
+-#endif
+ 
+   num_requests = num_reconnections = 0;
+   redis = NULL, operational = false;
+@@ -75,13 +73,11 @@ void Redis::reconnectRedis() {
+     ntop->getTrace()->traceEvent(TRACE_NORMAL, "Redis has disconnected: reconnecting...");
      redisFree(redis);
    }
+-#ifdef __linux__
+   struct stat buf;
  
--  redis = redisConnectWithTimeout(redis_host, redis_port, timeout);
-+  if (redis_host[0] == '/') {
-+    redis = redisConnectUnixWithTimeout(redis_host, timeout);
-+  }
-+  else {
-+    redis = redisConnectWithTimeout(redis_host, redis_port, timeout);
-+  }
+   if(!stat(redis_host, &buf) && S_ISSOCK(buf.st_mode))
+     redis = redisConnectUnixWithTimeout(redis_host, timeout), is_socket_connection = true;
+   else
+-#endif
+     redis = redisConnectWithTimeout(redis_host, redis_port, timeout);
  
-   while(num_attemps > 0) {
-     if(redis) reply = (redisReply*)redisCommand(redis, "PING"); else reply = NULL;
+   if(redis_password) {
+@@ -122,13 +118,11 @@ void Redis::reconnectRedis() {
+       goto redis_error_handler;
+     } else {
+       freeReplyObject(reply);
+-#ifdef __linux__
+       if(!is_socket_connection)
+ 	ntop->getTrace()->traceEvent(TRACE_NORMAL,
+ 				     "Successfully connected to redis %s:%u@%u",
+ 				     redis_host, redis_port, redis_db_id);
+       else
+-#endif
+ 	ntop->getTrace()->traceEvent(TRACE_NORMAL,
+ 				     "Successfully connected to redis %s@%u",
+ 				     redis_host, redis_db_id);
