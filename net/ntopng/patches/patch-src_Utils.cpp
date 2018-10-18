dcc7561739f442a1dcb856cf35c7903254390616
adam <adam@pkgsrc.org>
2018-10-18 16:25:40 +0000

    ntopng: updated to 3.6.1
    3.6.1 Stable
    Brew formula fixes
    
    3.6 Stable
    
    New features
    ------------
    New pro charts
    Ability to compare data with the past (time shift)
    Trend lines based on ASAP
    Average and percentile lines overlayed on the graph and animated
    New color scheme that uses pastel colors for better visualization
    https://www.ntop.org/ntopng/ntopng-and-time-series-from-rrd-to-influxdb-new-charts-with-time-shift/
    New timeseries API with support for RRD and InfluxDB
    Abstracts and handles multiple sources transparently
    https://www.ntop.org/guides/ntopng/api/lua/timeseries/index.html
    Streaming pcap captures with BPF support
    Download live packet captures right from the browser
    New SNMP devices caching
    Periodically cache information of all the SNMP device configured
    Calculate and visualize interfaces throughput
    
    Improvements
    ------------
    Security
    Access to the web user interface is controlled with ACLs
    Secure ntopng cookies with SameSite and HttpOnly
    HTTP cookie authentication
    Improved random session id generation
    Various SNMP improvemenets
    Caching
    Interfaces status change alerts
    Device interfaces page
    Devices and interfaces added to flows
    Fixed several library memory leaks
    Improved device and interface charts
    Interfaces throughput calculation and visualization
    Ability to delete all SNMP devices at once
    Improved active devices discovery
    OS detection via HTTP User-Agent
    Alerts
    Crypto miners alerts toggle
    Detection and alerting of anomalous terminations
    Module for sending telegram.org alerts
    Slack
    Configurable Slack channel names
    Added Slack test button
    Charts
    Active flows vs local hosts chart
    Active flows vs interface traffic chart
    Ubuntu 18.04 support
    Support for ElasticSearch 6 export
    Added support for custom categories lists
    Added ability to use the non-JIT Lua interpreter
    Improved ntopng startup and shutdown time
    Support for capturing from interface pairs with PF_RING ZC
    Support for variable PPP header lenght
    Migrated geolocation to GeoLite2 and libmaxminddb
    Configuration backup and restore
    Improved IE browser support
    Using client SSL certificate for protocol detection
    Optimized host/flows purging

--- net/ntopng/patches/patch-src_Utils.cpp
+++ net/ntopng/patches/patch-src_Utils.cpp
@@ -1,42 +1,72 @@
-$NetBSD: patch-src_Utils.cpp,v 1.1 2017/06/15 14:54:45 fhajny Exp $
+$NetBSD: patch-src_Utils.cpp,v 1.2 2018/10/18 16:25:40 adam Exp $
 
+Fix building on NetBSD.
 d_type is not POSIX, provide workaround for SunOS.
 
---- src/Utils.cpp.orig	2016-06-27 19:31:18.000000000 +0000
+--- src/Utils.cpp.orig	2018-09-19 15:35:27.000000000 +0000
 +++ src/Utils.cpp
-@@ -23,6 +23,9 @@
+@@ -21,10 +21,13 @@
  
- #include <curl/curl.h>
- #include <string.h>
+ #include "ntop_includes.h"
+ 
+-#if defined(__OpenBSD__) || defined(__APPLE__)
++#if defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
+ #include <net/if_dl.h>
+ #include <ifaddrs.h>
+ #endif
 +#ifdef __sun
 +struct stat s;
 +#endif
  
  // A simple struct for strings.
  typedef struct {
-@@ -1232,7 +1235,12 @@ bool scan_dir(const char * dir_name, lis
-     entry = readdir (d);
+@@ -1747,7 +1750,12 @@ static bool scan_dir(const char * dir_na
      if(!entry) break;
      d_name = entry->d_name;
-+#ifdef __sun
-+    stat(entry->d_name, &s);
-+    if (!(s.st_mode & S_IFDIR)) {
-+#else
-     if(!(entry->d_type & DT_DIR)) {
-+#endif
-       if(!stat(entry->d_name, &file_stats)) {
-         struct dirent *temp = (struct dirent *)malloc(sizeof(struct dirent));
-         memcpy(temp, entry, sizeof(struct dirent));
-@@ -1241,7 +1249,12 @@ bool scan_dir(const char * dir_name, lis
-       }
-     }
  
 +#ifdef __sun
 +    stat(entry->d_name, &s);
-+    if (s.st_mode & S_IFDIR) {
++    if (s.st_mode & S_IFREG) {
 +#else
-     if(entry->d_type & DT_DIR) {
+     if(entry->d_type & DT_REG) {
++#endif
+       snprintf(path, sizeof(path), "%s/%s", dir_name, entry->d_name);
+       if(!stat(path, &file_stats)) {
+         struct dirent *temp = (struct dirent *)malloc(sizeof(struct dirent));
+@@ -1757,7 +1765,11 @@ static bool scan_dir(const char * dir_na
+ 	  *total += file_stats.st_size;
+       }
+ 
++#ifdef __sun
++    } else if (s.st_mode & S_IFDIR) {
++#else
+     } else if(entry->d_type & DT_DIR) {
 +#endif
        if(strncmp (d_name, "..", 2) != 0 &&
            strncmp (d_name, ".", 1) != 0) {
-         int path_length;
+         path_length = snprintf (path, MAX_PATH,
+@@ -1849,7 +1861,12 @@ bool ntop_delete_old_files(const char *d
+   if(!d) return false;
+ 
+   while((result = readdir(d)) != NULL) {
++#ifdef __sun
++    stat(entry->d_name, &s);
++    if (s.st_mode & S_IFREG) {
++#else
+     if(result->d_type & DT_REG) {
++#endif
+       if((path_length = snprintf(path, MAX_PATH, "%s/%s", dir_name, result->d_name)) <= MAX_PATH) {
+ 	ntop->fixPath(path);
+ 
+@@ -1858,7 +1875,11 @@ bool ntop_delete_old_files(const char *d
+ 	    unlink(path);
+ 	}
+       }
++#ifdef __sun
++    } else if (s.st_mode & S_IFDIR) {
++#else
+     } else if(result->d_type & DT_DIR) {
++#endif
+       if(strncmp(result->d_name, "..", 2) && strncmp(result->d_name, ".", 1)) {
+         if((path_length = snprintf(path, MAX_PATH, "%s/%s", dir_name, result->d_name)) <= MAX_PATH) {
+ 	  ntop->fixPath(path);
