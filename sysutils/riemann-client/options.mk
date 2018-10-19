3d21ed63330358a99fe7bb6ea70d50661a99edf7
jperkin <jperkin@pkgsrc.org>
2018-10-19 15:53:41 +0000

    riemann-client: Update to 1.10.3.
    riemann-c-client 1.10.3
    
    Dependencies on the generated protobuf C sources have been fixed in the Makefile.
    A few harmless compile-time warnings have been silenced.
    Fixed a few typos in riemann-client(1).
    
    riemann-c-client 1.10.2
    
    A number of formatting issues in the API docs have been fixed.
    
    riemann-c-client 1.10.1
    
    The library compiles on OSX/Darwin again, after fixing a symbol aliasing issue.
    Reported by @ilovezfs.
    
    riemann-c-client 1.10.0
    
    Added support for micro-second timestamps (available since Riemann 0.2.13),
    implemented by Mathieu Corbin, @mcorbin.  Miscellaneous changes
    
    The parts of the test suite that require a running Riemann server are now
    disabled by default, and require setting the RCC_NETWORK_TESTS environment
    variable to a non-zero value, before running the test suite.
    
    Fixes an issue on systems that had something else listening on the port, or a
    Riemann with a different config than the one the test suite expected.
    
    Reported by Dave Cottlehuber, @dch.

--- sysutils/riemann-client/options.mk
+++ sysutils/riemann-client/options.mk
@@ -1,12 +1,10 @@
-# $NetBSD: options.mk,v 1.1 2016/10/13 11:03:13 fhajny Exp $
+# $NetBSD: options.mk,v 1.2 2018/10/19 15:53:41 jperkin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.riemann-client
 PKG_SUPPORTED_OPTIONS=	tls
 PKG_SUGGESTED_OPTIONS+=	tls
 
 .include "../../mk/bsd.options.mk"
-
-PLIST_VARS+=		tls
 
 .if !empty(PKG_OPTIONS:Mtls)
 CONFIGURE_ARGS+=	--enable-tls
