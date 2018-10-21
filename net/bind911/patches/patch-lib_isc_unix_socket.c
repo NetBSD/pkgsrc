40c99cd8774ba3276112407cd4a3f23560d51d67
taca <taca@pkgsrc.org>
2018-10-21 15:51:14 +0000

    net/bind911: update to 9.11.5
    	--- 9.11.5 released ---
    
    	--- 9.11.5rc1 released ---
    
    5038.	[bug]		Chaosnet addresses were compared incorrectly.
    			[GL #562]
    
    5034.	[bug]		A race between threads could prevent zone maintenance
    			scheduled immediately after zone load from being
    			performed. [GL #542]
    
    5033.	[bug]		When adding NTAs to multiple views using "rndc nta",
    			the text returned via rndc was incorrectly terminated
    			after the first line, making it look as if only one
    			NTA had been added. Also, it was not possible to
    			differentiate between views with the same name but
    			different classes; this has been corrected with the
    			addition of a "-class" option. [GL #105]
    
    5032.	[func]		Add krb5-selfsub and ms-selfsub update policy rules.
    			[GL #511]
    
    5030.	[bug]		Align CMSG buffers to a 64-bit boundary, fixes crash
    			on architectures with strict alignment. [GL #521]
    
    5028.	[bug]		Spread the initial RRSIG expiration times over the
    			entire working sig-validity-interval when signing a
    			zone in named to even out re-signing and transfer
    			loads. [GL #418]
    
    5026.	[bug]		rndc reconfig should not touch already loaded zones.
    			[GL #276]
    
    5022.	[doc]		Update ms-self, ms-subdomain, krb5-self, and
    			krb5-subdomain documentation. [GL !708]
    
    5021.	[bug]		dig returned a non-zero exit code when it received a
    			reply over TCP after a retry. [GL #487]
    
    5019.	[cleanup]	A message is now logged when ixfr-from-differences is
    			set at zone level for an inline-signed zone. [GL #470]
    
    5018.	[bug]		Fix incorrect sizeof arguments in lib/isc/pk11.c.
    			[GL !588]
    
    5017.	[bug]		lib/isc/pk11.c failed to unlink the session before
    			releasing the lock which is unsafe. [GL !589]
    
    5016.	[bug]		Named could assert with overlapping filter-aaaa and
    			dns64 acls. [GL #445]
    
    5015.	[bug]		Reloading all zones caused zone maintenance to cease
    			for inline-signed zones. [GL #435]
    
    5014.	[bug]		Signatures loaded from the journal for the signed
    			version of an inline-signed zone were not scheduled for
    			refresh. [GL #482]
    
    5012.	[bug]		Fix lock order reversal in pk11_initialize. [GL !590]
    
    5009.	[bug]		Upon an OpenSSL failure, the first error in the OpenSSL
    			error queue was not logged. [GL #476]
    
    5008.	[bug]		"rndc signing -nsec3param ..." requests were silently
    			ignored for zones which were not yet loaded or
    			transferred. [GL #468]
    
    5007.	[cleanup]	Replace custom ISC boolean and integer data types
    			with C99 stdint.h and stdbool.h types. [GL #9]
    
    5005.	[bug]		dnssec-verify, and dnssec-signzone at the verification
    			step, failed on some validly signed zones. [GL #442]
    
    5004.	[bug]		'rndc reconfig' could cause inline zones to stop
    			re-signing. [GL #439]
    
    5003.	[bug]		dns_acl_isinsecure did not handle geoip elements.
    			[GL #406]
    
    5002.	[bug]		mdig: Handle malformed +ednsopt option, support 100
    			+ednsopt options per query rather than 100 total and
    			address memory leaks if +ednsopt was specified.
    			[GL #410]
    
    5001.	[bug]		Fix refcount errors on error paths. [GL !563]
    
    4996.	[bug]		dig: Handle malformed +ednsopt option. [GL #403]
    
    4995.	[test]		Add tests for "tcp-self" update policy. [GL !282]
    
    4994.	[bug]		Trust anchor telemetry queries were not being sent
    			upstream for locally served zones. [GL #392]
    
    4992.	[bug]		The wrong address was being logged for trust anchor
    			telemetry queries. [GL #379]
    
    4990.	[bug]		Prevent a possible NULL reference in pkcs11-keygen.
    			[GL #401]

--- net/bind911/patches/patch-lib_isc_unix_socket.c
+++ net/bind911/patches/patch-lib_isc_unix_socket.c
@@ -1,33 +1,24 @@
-$NetBSD: patch-lib_isc_unix_socket.c,v 1.1 2018/09/09 13:11:38 taca Exp $
+$NetBSD: patch-lib_isc_unix_socket.c,v 1.2 2018/10/21 15:51:14 taca Exp $
 
-Make ENOBUFS a soft error
-https://gitlab.isc.org/isc-projects/bind9/issues/462
+Apply fix from NetBSD revision 1.24.
 
---- lib/isc/unix/socket.c.orig	2018-07-24 17:24:11.000000000 +0000
+--- lib/isc/unix/socket.c.orig	2018-10-06 01:36:17.000000000 +0000
 +++ lib/isc/unix/socket.c
-@@ -257,6 +257,7 @@ typedef enum { poll_idle, poll_active, p
- #define SOFT_ERROR(e)	((e) == EAGAIN || \
+@@ -258,6 +258,7 @@ typedef enum { poll_idle, poll_active, p
  			 (e) == EWOULDBLOCK || \
+ 			 (e) == ENOBUFS || \
  			 (e) == EINTR || \
 +			 (e) == ENOBUFS || \
  			 (e) == 0)
  
  #define DLVL(x) ISC_LOGCATEGORY_GENERAL, ISC_LOGMODULE_SOCKET, ISC_LOG_DEBUG(x)
-@@ -1952,7 +1953,7 @@ doio_recv(isc__socket_t *sock, isc_socke
- 		SOFT_OR_HARD(EHOSTDOWN, ISC_R_HOSTDOWN);
- 		/* HPUX 11.11 can return EADDRNOTAVAIL. */
- 		SOFT_OR_HARD(EADDRNOTAVAIL, ISC_R_ADDRNOTAVAIL);
--		ALWAYS_HARD(ENOBUFS, ISC_R_NORESOURCES);
-+		SOFT_OR_HARD(ENOBUFS, ISC_R_NORESOURCES);
- 		/* Should never get this one but it was seen. */
- #ifdef ENOPROTOOPT
- 		SOFT_OR_HARD(ENOPROTOOPT, ISC_R_HOSTUNREACH);
-@@ -2149,7 +2150,7 @@ doio_send(isc__socket_t *sock, isc_socke
- 		ALWAYS_HARD(EHOSTDOWN, ISC_R_HOSTUNREACH);
- #endif
- 		ALWAYS_HARD(ENETUNREACH, ISC_R_NETUNREACH);
--		ALWAYS_HARD(ENOBUFS, ISC_R_NORESOURCES);
-+		SOFT_OR_HARD(ENOBUFS, ISC_R_NORESOURCES);
- 		ALWAYS_HARD(EPERM, ISC_R_HOSTUNREACH);
- 		ALWAYS_HARD(EPIPE, ISC_R_NOTCONNECTED);
- 		ALWAYS_HARD(ECONNRESET, ISC_R_CONNECTIONRESET);
+@@ -1575,7 +1576,8 @@ build_msghdr_send(isc__socket_t *sock, c
+ 
+ #if defined(IPV6_USE_MIN_MTU)
+ 	if ((sock->type == isc_sockettype_udp) &&
+-	    ((dev->attributes & ISC_SOCKEVENTATTR_USEMINMTU) != 0))
++	    ((dev->attributes & ISC_SOCKEVENTATTR_USEMINMTU) != 0) &&
++	    (sock->pf == AF_INET6))
+ 	{
+ 		int use_min_mtu = 1;	/* -1, 0, 1 */
+ 
