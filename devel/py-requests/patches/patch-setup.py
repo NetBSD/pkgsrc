5d477f32e3ea5c0d12dd354f8d0703020623b2a2
adam <adam@pkgsrc.org>
2018-10-22 07:55:36 +0000

    py-requests: updated to 2.20.0
    2.20.0:
    Bugfixes
    Content-Type header parsing is now case-insensitive (e.g. charset=utf8 v Charset=utf8).
    Fixed exception leak where certain redirect urls would raise uncaught urllib3 exceptions.
    Requests removes Authorization header from requests redirected from https to http on the same hostname. (CVE-2018-18074)
    should_bypass_proxies now handles URIs without hostnames (e.g. files).
    
    Dependencies
    Requests now supports urllib3 v1.24.
    
    Deprecations
    Requests has officially stopped support for Python 2.6.

--- devel/py-requests/patches/patch-setup.py
+++ devel/py-requests/patches/patch-setup.py
@@ -1,24 +1,25 @@
-$NetBSD: patch-setup.py,v 1.5 2018/06/16 13:08:32 adam Exp $
+$NetBSD: patch-setup.py,v 1.6 2018/10/22 07:55:36 adam Exp $
 
 Allow newer versions of dependencies.
 
---- setup.py.orig	2018-06-12 14:39:23.000000000 +0000
+--- setup.py.orig	2018-10-18 15:41:46.000000000 +0000
 +++ setup.py
-@@ -49,13 +49,13 @@ else:
+@@ -42,14 +42,14 @@ if sys.argv[-1] == 'publish':
  packages = ['requests']
  
  requires = [
 -    'chardet>=3.0.2,<3.1.0',
 -    'idna>=2.5,<2.8',
--    'urllib3>=1.21.1,<1.24',
+-    'urllib3>=1.21.1,<1.25',
 +    'chardet>=3.0.2',
 +    'idna>=2.5',
 +    'urllib3>=1.21.1',
      'certifi>=2017.4.17'
  
  ]
--test_requirements = ['pytest-httpbin==0.0.7', 'pytest-cov', 'pytest-mock', 'pytest-xdist', 'PySocks>=1.5.6, !=1.5.7', 'pytest>=2.8.0']
-+test_requirements = ['pytest-httpbin>=0.0.7', 'pytest-cov', 'pytest-mock', 'pytest-xdist', 'PySocks>=1.5.6, !=1.5.7', 'pytest>=2.8.0']
- 
- about = {}
- with open(os.path.join(here, 'requests', '__version__.py'), 'r', 'utf-8') as f:
+ test_requirements = [
+-    'pytest-httpbin==0.0.7',
++    'pytest-httpbin>=0.0.7',
+     'pytest-cov',
+     'pytest-mock',
+     'pytest-xdist',
