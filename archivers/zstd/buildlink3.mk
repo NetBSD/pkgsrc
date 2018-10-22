2a4226c153a677cce396fe4e25c23dcccf54bac8
adam <adam@pkgsrc.org>
2018-10-22 15:28:02 +0000

    zstd: updated to 1.3.7
    Zstandard v1.3.7
    perf: slightly better decompression speed on clang (depending on hardware target)
    fix: ratio for dictionary compression at levels 9 and 10, reported by @indygreg
    build: no longer build backtrace by default in release mode; restrict further automatic mode
    build: control backtrace support through build macro BACKTRACE
    misc: added man pages for zstdless and zstdgrep, by @samrussell

--- archivers/zstd/buildlink3.mk
+++ archivers/zstd/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/03/08 16:51:36 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/10/22 15:28:02 adam Exp $
 
 BUILDLINK_TREE+=	zstd
 
 .if !defined(ZSTD_BUILDLINK3_MK)
 ZSTD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.zstd+=	zstd>=1.1.3
+BUILDLINK_API_DEPENDS.zstd+=	zstd>=1.3.6
 BUILDLINK_PKGSRCDIR.zstd?=	../../archivers/zstd
 .endif	# ZSTD_BUILDLINK3_MK
 
