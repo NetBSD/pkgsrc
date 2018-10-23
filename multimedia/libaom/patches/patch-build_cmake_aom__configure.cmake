a50a51aae0306c45b53b8cf8f81432183de2d3ee
jperkin <jperkin@pkgsrc.org>
2018-10-23 17:30:21 +0000

    libaom: Fix build on SunOS.

--- multimedia/libaom/patches/patch-build_cmake_aom__configure.cmake
+++ multimedia/libaom/patches/patch-build_cmake_aom__configure.cmake
@@ -1,10 +1,24 @@
-$NetBSD: patch-build_cmake_aom__configure.cmake,v 1.1 2018/09/30 18:58:16 maya Exp $
+$NetBSD: patch-build_cmake_aom__configure.cmake,v 1.2 2018/10/23 17:30:21 jperkin Exp $
 
 Don't disable fortify just because it's a release build.
+Set CPU correctly on SunOS.
 
---- build/cmake/aom_configure.cmake.orig	2018-09-28 20:57:38.000000000 +0000
+--- build/cmake/aom_configure.cmake.orig	2018-10-01 03:20:05.000000000 +0000
 +++ build/cmake/aom_configure.cmake
-@@ -290,9 +290,6 @@ else()
+@@ -65,7 +65,11 @@ if(NOT AOM_TARGET_CPU)
+     endif()
+   elseif("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "i386" OR
+          "${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "x86")
+-    set(AOM_TARGET_CPU "x86")
++    if(${CMAKE_SIZEOF_VOID_P} EQUAL 4)
++      set(AOM_TARGET_CPU "x86")
++    elseif(${CMAKE_SIZEOF_VOID_P} EQUAL 8)
++      set(AOM_TARGET_CPU "x86_64")
++    endif()
+   elseif("${CMAKE_SYSTEM_PROCESSOR}" MATCHES "^arm" OR
+          "${CMAKE_SYSTEM_PROCESSOR}" MATCHES "^mips")
+     set(AOM_TARGET_CPU "${CMAKE_SYSTEM_PROCESSOR}")
+@@ -290,9 +294,6 @@ else()
      add_compiler_flag_if_supported("-Werror")
    endif()
  
