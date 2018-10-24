1c13df01122f7d8ba5b2ded30b881c872d838ab7
leot <leot@pkgsrc.org>
2018-10-24 18:31:07 +0000

    webkit-gtk: Update www/webkit-gtk to 2.22.2
    pkgsrc changes:
     - Bump GCC_REQD to 6 (now gcc 6.0.0 or newer is needed)
     - Add NetBSD support for JavaScript JIT on x86_64, i386, arm,
       aarch64 and mips
     - Add WebKitWebProcess and jsc to NOT_PAX_MPROTECT_SAFE.
       At least on NetBSD/amd64, running SunSpider 1.0.2 JavaScript Benchmark
       (<https://webkit.org/perf/sunspider/sunspider.html>) with MiniBrowser
       before `paxctl +m'-ing them needed:
        Total:                 1006.9ms +/- 0.7%
       ...while after `paxctl +m'-ing them:
        Total:                 322.3ms +/- 3.0%
       (Probably EACCESS due PaX MPROTECT are handled gracefully and
       silently instead of failing hard at runtime.)
       Please also note that webkit-gtk browsers should not need any
       NOT_PAX_MPROTECT_SAFE because WebKitWebProcess is used and already
       have that.
     - Improve handling of `webkit-jit' by introducing a
       WEBKIT_JIT_MACHINE_PLATFORMS list that contain all MACHINE_PLATFORMs
       triplets that have `webkit-jit' option as suggested one.
     - Always use OS(...) and BOS(...) macros instead of __Os__ macros for
       consistency with webkit code.
     - Add definition for BOS(SOLARIS) and OS(SOLARIS) and add it to the
       OS(UNIX) OSes list.
     - Limit patch-Source_JavaScriptCore_jit_ExecutableAllocator.cpp to
       OpenBSD. It is no longer present in FreeBSD ports and it is not
       problematic in NetBSD.
     - Remove no longer needed patch-Source_WTF_wtf_ThreadSpecific.h:
       NetBSD 5.x was already part of NOT_FOR_PLATFORM.
     - Sync patch-CMakeLists.txt with FreeBSD ports.
       Please note that this also removes WTF_CPU_SPARC64 definition that was
       unused.
     - madvise(2) on {Free,DragonFly,Net,Open}BSD supports MADV_FREE and
       MADV_DONTNEED flags. Define the corresponding HAVE_* via
       patch-Source_WTF_wtf_Platform.h.
     - Use globbing for REPLACE_{PERL,PYTHON} where possible.
    
    Changes:
    WebKitGTK+ 2.22.2
    =================
      - Several fixes for video playback with media source extensions (MSE).
        This allows using WebM support for YouTube, which no longer works through
        regular video source. Note that MSE is still disabled by default and
        webkit_settings_set_enable_mediasource() has to be used to enable the
        feature.
      - Fix the build when only Wayland support is enabled and X11 headers are
        not available.
    
    WebKitGTK+ 2.22.1
    =================
      - Fix printing in landscape.
      - Fix the build in several platforms: s390x, ppc64le, armv7hl.
      - Fix the build with a11y disabled.
      - Fix the build with video disabled.
      - Fix several crashes and rendering issues.
    
    WebKitGTK+ 2.22.0
    ==================
      - Add warn_unused_result attribute to some JavaScriptCore GLib APIs.
      - Make pinch to zoom scale the page without changing the layout.
      - Fix the build in mips64.

--- www/webkit-gtk/patches/patch-Source_WebKit_NetworkProcess_cache_NetworkCacheFileSystem.cpp
+++ www/webkit-gtk/patches/patch-Source_WebKit_NetworkProcess_cache_NetworkCacheFileSystem.cpp
@@ -1,13 +1,13 @@
-$NetBSD: patch-Source_WebKit_NetworkProcess_cache_NetworkCacheFileSystem.cpp,v 1.1 2018/05/16 18:55:45 jperkin Exp $
+$NetBSD: patch-Source_WebKit_NetworkProcess_cache_NetworkCacheFileSystem.cpp,v 1.2 2018/10/24 18:31:07 leot Exp $
 
 SunOS does not support DT_{DIR,REG}
 
---- Source/WebKit/NetworkProcess/cache/NetworkCacheFileSystem.cpp.orig	2018-02-19 07:45:32.000000000 +0000
+--- Source/WebKit/NetworkProcess/cache/NetworkCacheFileSystem.cpp.orig	2018-07-25 11:56:32.000000000 +0000
 +++ Source/WebKit/NetworkProcess/cache/NetworkCacheFileSystem.cpp
-@@ -50,6 +50,12 @@ namespace NetworkCache {
- 
+@@ -54,6 +54,12 @@ namespace NetworkCache {
  static DirectoryEntryType directoryEntryType(uint8_t dtype)
  {
+ #if !OS(WINDOWS)
 +#ifndef DT_DIR
 +#define DT_DIR	S_IFDIR
 +#endif
@@ -17,11 +17,11 @@ SunOS does not support DT_{DIR,REG}
      switch (dtype) {
      case DT_DIR:
          return DirectoryEntryType::Directory;
-@@ -63,12 +69,20 @@ static DirectoryEntryType directoryEntry
- 
+@@ -71,12 +77,20 @@ static DirectoryEntryType directoryEntry
  void traverseDirectory(const String& path, const Function<void (const String&, DirectoryEntryType)>& function)
  {
-+#ifdef __sun
+ #if !OS(WINDOWS)
++#if OS(SOLARIS)
 +    struct stat s;
 +#endif
      DIR* dir = opendir(WebCore::FileSystem::fileSystemRepresentation(path).data());
@@ -29,7 +29,7 @@ SunOS does not support DT_{DIR,REG}
          return;
      dirent* dp;
      while ((dp = readdir(dir))) {
-+#ifdef __sun
++#if OS(SOLARIS)
 +        stat(dp->d_name, &s);
 +        if (s.st_mode != S_IFDIR && s.st_mode != S_IFREG)
 +#else
@@ -38,15 +38,15 @@ SunOS does not support DT_{DIR,REG}
              continue;
          const char* name = dp->d_name;
          if (!strcmp(name, ".") || !strcmp(name, ".."))
-@@ -76,7 +90,11 @@ void traverseDirectory(const String& pat
+@@ -84,7 +98,11 @@ void traverseDirectory(const String& pat
          auto nameString = String::fromUTF8(name);
          if (nameString.isNull())
              continue;
-+#ifdef __sun
++#if OS(SOLARIS)
 +        function(nameString, directoryEntryType(s.st_mode));
 +#else
          function(nameString, directoryEntryType(dp->d_type));
 +#endif
      }
      closedir(dir);
- }
+ #else
