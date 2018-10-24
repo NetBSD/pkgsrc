7f7915487e16cd5daae4073b17e72d9693eafd5b
leot <leot@pkgsrc.org>
2018-10-24 21:11:45 +0000

    *: (belatedly) revbump for net/libsoup update
    Thanks to <wiz>!

--- www/webkit24-gtk/buildlink3.mk
+++ www/webkit24-gtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2018/07/20 03:33:58 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2018/10/24 21:11:48 leot Exp $
 
 BUILDLINK_TREE+=	webkit24-gtk
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	webkit24-gtk
 WEBKIT24_GTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.webkit24-gtk+=	webkit24-gtk>=2.0
-BUILDLINK_ABI_DEPENDS.webkit24-gtk?=	webkit24-gtk>=2.4.11nb15
+BUILDLINK_ABI_DEPENDS.webkit24-gtk?=	webkit24-gtk>=2.4.11nb18
 BUILDLINK_PKGSRCDIR.webkit24-gtk?=	../../www/webkit24-gtk
 
 # Linking with this library may require newer libstdc++ features than are
