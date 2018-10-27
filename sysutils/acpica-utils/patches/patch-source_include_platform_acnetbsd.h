8c94db67dcf569d8789a16d5d54700944261e0a7
abs <abs@pkgsrc.org>
2018-10-27 21:44:53 +0000

    Avoid some sprintf buffer overflows reported by gcc 8. Bump PKGREVISION

--- sysutils/acpica-utils/patches/patch-source_include_platform_acnetbsd.h
+++ sysutils/acpica-utils/patches/patch-source_include_platform_acnetbsd.h
@@ -1,3 +1,7 @@
+$NetBSD: patch-source_include_platform_acnetbsd.h,v 1.2 2018/10/27 21:44:53 abs Exp $
+
+NetBSD versions prior to 7 lack sem_timedwait() in librt
+
 --- source/include/platform/acnetbsd.h.orig	2015-04-10 17:27:17.000000000 +0200
 +++ source/include/platform/acnetbsd.h	2015-12-10 14:15:22.000000000 +0100
 @@ -112,4 +112,10 @@
