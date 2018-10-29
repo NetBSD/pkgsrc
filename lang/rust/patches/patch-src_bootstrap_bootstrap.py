d2b01358fe79c04f97c6fd44d5910d066e58bc52
he <he@pkgsrc.org>
2018-10-29 22:24:11 +0000

    Upgrade rust to version 1.30.0.
    Upstream changes:
    
    Language
     * Procedural macros are now available. These kinds of macros allow
       for more powerful code generation. There is a new chapter available
       in the Rust Programming Language book that goes further in depth.
     * You can now use keywords as identifiers using the raw identifiers
       syntax (r#), e.g. let r#for = true;
     * Using anonymous parameters in traits is now deprecated with a
       warning and will be a hard error in the 2018 edition.
     * You can now use crate in paths. This allows you to refer to the
       crate root in the path, e.g. use crate::foo; refers to foo in
       src/lib.rs.
     * Using a external crate no longer requires being prefixed with
       ::. Previously, using a external crate in a module without a
       use statement required let json = ::serde_json::from_str(foo);
       but can now be written as let json = serde_json::from_str(foo);.
     * You can now apply the #[used] attribute to static items to
       prevent the compiler from optimising them away, even if they
       appear to be unused, e.g. #[used] static FOO: u32 = 1;
     * You can now import and reexport macros from other crates with
       the use syntax. Macros exported with #[macro_export] are now
       placed into the root module of the crate. If your macro relies
       on calling other local macros, it is recommended to export with
       the #[macro_export(local_inner_macros)] attribute so users won't
       have to import those macros.
     * You can now catch visibility keywords (e.g. pub, pub(crate)) in
       macros using the vis specifier.
     * Non-macro attributes now allow all forms of literals, not just
       strings. Previously, you would write #[attr("true")], and you
       can now write #[attr(true)].
     * You can now specify a function to handle a panic in the Rust
       runtime with the #[panic_handler] attribute.
    
    Compiler
     * Added the riscv32imc-unknown-none-elf target.
     * Added the aarch64-unknown-netbsd target
    
    Libraries
     * ManuallyDrop now allows the inner type to be unsized.
    
    Stabilized APIs
     * Ipv4Addr::BROADCAST
     * Ipv4Addr::LOCALHOST
     * Ipv4Addr::UNSPECIFIED
     * Ipv6Addr::LOCALHOST
     * Ipv6Addr::UNSPECIFIED
     * Iterator::find_map
     * The following methods are replacement methods for trim_left,
       trim_right, trim_left_matches, and trim_right_matches, which
       will be deprecated in 1.33.0:
     * str::trim_end_matches
     * str::trim_end
     * str::trim_start_matches
     * str::trim_start
    
    Cargo
     * cargo run doesn't require specifying a package in workspaces.
     * cargo doc now supports --message-format=json. This is equivalent
       to calling rustdoc --error-format=json.
     * Cargo will now provide a progress bar for builds.
    
    Misc
     * rustdoc allows you to specify what edition to treat your code
       as with the --edition option.
     * rustdoc now has the --color (specify whether to output color)
       and --error-format (specify error format, e.g. json) options.
     * We now distribute a rust-gdbgui script that invokes gdbgui with
       Rust debug symbols.
     * Attributes from Rust tools such as rustfmt or clippy are now
       available, e.g. #[rustfmt::skip] will skip formatting the next
       item.
    
    
    Pkgsrc changest:
     * Explicitly list bootstrap kit version number for each kit we carry,
       so that one entry's version doesn't "bleed into" following kits.
     * Tweak for handling "earmv7hf" CPU type for NetBSD in the bootstrap.py
       script
     * Add two patches from Debian for sparc64; rust would generate code
       generating unaligned accesses, causing SIGBUS on sparc64
     * Update most of the bootstrap kits to version 1.29.2; need minimum
       1.29.0 to build 1.30.0.
     * Rust regrettably doesn't build for powerpc or earmv7hf in this version,
       most probably due to "char" being "unsigned char" on these platforms.
       Ref. https://github.com/rust-lang/rust/issues/55465

--- lang/rust/patches/patch-src_bootstrap_bootstrap.py
+++ lang/rust/patches/patch-src_bootstrap_bootstrap.py
@@ -1,12 +1,13 @@
-$NetBSD: patch-src_bootstrap_bootstrap.py,v 1.1 2018/10/13 10:03:37 he Exp $
+$NetBSD: patch-src_bootstrap_bootstrap.py,v 1.2 2018/10/29 22:24:11 he Exp $
 
 Use `uname -p` on NetBSD, as that's reliable & sensible there.
 Also force debuginfo to 0, otherwise the build doesn't work on
 NetBSD/macppc (powerpc).
+Also, handle earmv7hf for NetBSD.
 
 --- src/bootstrap/bootstrap.py.orig	2018-09-20 17:28:03.000000000 +0000
 +++ src/bootstrap/bootstrap.py
-@@ -193,6 +193,11 @@ def default_build_triple():
+@@ -196,6 +196,11 @@ def default_build_triple():
          'OpenBSD': 'unknown-openbsd'
      }
  
@@ -18,12 +19,17 @@ NetBSD/macppc (powerpc).
      # Consider the direct transformation first and then the special cases
      if ostype in ostype_mapper:
          ostype = ostype_mapper[ostype]
-@@ -619,7 +624,7 @@ class RustBuild(object):
-         env["LIBRARY_PATH"] = os.path.join(self.bin_root(), "lib") + \
-             (os.pathsep + env["LIBRARY_PATH"]) \
-             if "LIBRARY_PATH" in env else ""
--        env["RUSTFLAGS"] = "-Cdebuginfo=2 "
-+        env["RUSTFLAGS"] = "-Cdebuginfo=0 "
- 
-         build_section = "target.{}".format(self.build_triple())
-         target_features = []
+@@ -275,10 +280,12 @@ def default_build_triple():
+             ostype = 'linux-androideabi'
+         else:
+             ostype += 'eabihf'
+-    elif cputype in {'armv7l', 'armv8l'}:
++    elif cputype in {'armv7l', 'armv8l', 'earmv7hf'}:
+         cputype = 'armv7'
+         if ostype == 'linux-android':
+             ostype = 'linux-androideabi'
++        elif ostype == 'unknown-netbsd':
++            ostype += '-eabihf'
+         else:
+             ostype += 'eabihf'
+     elif cputype == 'mips':
