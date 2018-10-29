86dee5516f3440f14d89607bff1f2d13484988a2
spz <spz@pkgsrc.org>
2018-10-29 19:29:11 +0000

    Pullup ticket #5860 - requested by taca lang/php: security update lang/php72: security update
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.233
    - lang/php72/distinfo                                           1.31
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Oct 13 06:18:37 UTC 2018
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php72: distinfo
    
       Log Message:
       lang/php72: update to 7.2.11
    
       11 Oct 2018, PHP 7.2.11
    
       - Core:
         . Fixed bug #76800 (foreach inconsistent if array modified during loop).
           (Dmitry)
         . Fixed bug #76901 (method_exists on SPL iterator passthrough method corrupts
           memory). (Nikita)
    
       - CURL:
         . Fixed bug #76480 (Use curl_multi_wait() so that timeouts are respected).
           (Pierrick)
    
       - iconv:
         . Fixed bug #66828 (iconv_mime_encode Q-encoding longer than it should be).
           (cmb)
    
       - Opcache:
         . Fixed bug #76832 (ZendOPcache.MemoryBase periodically deleted by the OS).
           (Anatol)
         . Fixed bug #76796 (Compile-time evaluation of disabled function in opcache
           causes segfault). (Nikita)
    
       - POSIX:
         . Fixed bug #75696 (posix_getgrnam fails to print details of group). (cmb)
    
       - Reflection:
         . Fixed bug #74454 (Wrong exception being thrown when using ReflectionMethod).
           (cmb)
    
       - Standard:
         . Fixed bug #73457 (Wrong error message when fopen FTP wrapped fails to open
           data connection). (Ville Hukkamäki)
         . Fixed bug #74764 (Bindto IPv6 works with file_get_contents but fails with
           stream_socket_client). (Ville Hukkamäki)
         . Fixed bug #75533 (array_reduce is slow when $carry is large array).
           (Manabu Matsui)
    
       - XMLRPC:
         . Fixed bug #76886 (Can't build xmlrpc with expat). (Thomas Petazzoni, cmb)
    
       - Zlib:
         . Fixed bug #75273 (php_zlib_inflate_filter() may not update bytes_consumed).
           (Martin Burke, cmb)
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.232 -r1.233 pkgsrc/lang/php/phpversion.mk
       cvs rdiff -u -r1.30 -r1.31 pkgsrc/lang/php72/distinfo

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.231.2.1 2018/10/29 19:18:28 spz Exp $
+# $NetBSD: phpversion.mk,v 1.231.2.2 2018/10/29 19:29:11 spz Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.38
 PHP70_VERSION=	7.0.32
 PHP71_VERSION=	7.1.23
-PHP72_VERSION=	7.2.10
+PHP72_VERSION=	7.2.11
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
