ad9d7bb7bb29ec9e6e238247cff049b410b00d9a
leot <leot@pkgsrc.org>
2018-10-30 17:49:37 +0000

    gtk3: Add reference to upstream issue regarding freetype headers include-s

--- x11/gtk3/patches/patch-gtk_gtkfontchooserwidget.c
+++ x11/gtk3/patches/patch-gtk_gtkfontchooserwidget.c
@@ -1,7 +1,11 @@
-$NetBSD: patch-gtk_gtkfontchooserwidget.c,v 1.1 2018/10/29 21:35:17 leot Exp $
+$NetBSD: patch-gtk_gtkfontchooserwidget.c,v 1.2 2018/10/30 17:49:37 leot Exp $
 
 Correctly include freetype2 headers (<ft2build.h> and then macros
 should be used instead of directly including the headers).
+
+Issue opened upstream as:
+
+ <https://gitlab.gnome.org/GNOME/gtk/issues/1432> 
 
 --- gtk/gtkfontchooserwidget.c.orig	2018-07-18 11:04:31.000000000 +0000
 +++ gtk/gtkfontchooserwidget.c
