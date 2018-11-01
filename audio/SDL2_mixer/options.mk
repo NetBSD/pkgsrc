d3251942262252d49bb4edfb09cb93516ed97100
adam <adam@pkgsrc.org>
2018-11-01 21:12:40 +0000

    SDL2_mixer: updated to 2.0.4
    2.0.4:
    * Removed smpeg support for mp3 music, now that it's replaced by libmpg123
    * Fixed mp3 mad decoder to skip tags, which otherwise would lead to crashes
    * Added support for Opus music playback using opusfile library
    
    2.0.3:
    * Fixed regression where Mix_Init() would return 0 for available music formats

--- audio/SDL2_mixer/options.mk
+++ audio/SDL2_mixer/options.mk
@@ -1,42 +1,13 @@
-# $NetBSD: options.mk,v 1.2 2018/06/24 10:28:45 adam Exp $
+# $NetBSD: options.mk,v 1.3 2018/11/01 21:12:40 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.SDL2_mixer
-PKG_SUPPORTED_OPTIONS=	flac fluidsynth modplug mpg123 vorbis
-PKG_SUGGESTED_OPTIONS+=	flac modplug mpg123 vorbis
+PKG_SUPPORTED_OPTIONS=	fluidsynth
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Mflac)
-.include "../../audio/flac/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-music-flac=yes
-.else
-CONFIGURE_ARGS+=	--enable-music-flac=no
-.endif
 
 .if !empty(PKG_OPTIONS:Mfluidsynth)
 .include "../../audio/fluidsynth/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-music-midi-fluidsynth=yes
 .else
 CONFIGURE_ARGS+=	--enable-music-midi-fluidsynth=no
-.endif
-
-.if !empty(PKG_OPTIONS:Mmodplug)
-.include "../../audio/libmodplug/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-music-mod-modplug=yes
-.else
-CONFIGURE_ARGS+=	--enable-music-mod-modplug=no
-.endif
-
-.if !empty(PKG_OPTIONS:Mmpg123)
-.include "../../audio/mpg123/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-music-mp3-mpg123=yes
-.else
-CONFIGURE_ARGS+=	--enable-music-mp3-mpg123=no
-.endif
-
-.if !empty(PKG_OPTIONS:Mvorbis)
-.include "../../audio/libvorbis/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-music-ogg=yes
-.else
-CONFIGURE_ARGS+=	--enable-music-ogg=no
 .endif
