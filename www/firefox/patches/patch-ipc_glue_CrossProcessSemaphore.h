7b07c69302c3f83454e9e61b3e874e59f0837209
ryoon <ryoon@pkgsrc.org>
2018-11-04 00:38:44 +0000

    Update to 63.0.1
    * Minimize pkgsrc specific patches.
    * A build system written in Rust lang does not find a C++ header files
      from pkgsrc (non-base) GCC, this version is not buildable on NetBSD 7.
      I will investigate this problem again.
    
    Changelog:
    63.0.1
    Fixed
    
        Snippets are not loaded due to missing element (bug 1503047)
    
        Print preview always shows 30% scale when it is actually Shrink To Fit
        (bug 1501952)
    
        Dialog displayed when closing multiple windows shows unreplaced %1$S
        placeholder in Japanese and potentially other locales (bug 1500823)
    
    
    63.0
    New
    
        Performance and visual improvements for Windows users
    
        Performance improvements for macOS users
    
        Added content blocking, a collection of Firefox settings that offer
        users greater control over technology that can track them around the
        web. In 63, users can opt to block third-party tracking cookies or
        block all trackers and create exceptions for trusted sites that don't
        work correctly with content blocking enabled.
    
        WebExtensions now run in their own process on Linux
    
        Firefox now warns about having multiple windows and tabs open
        when quitting from the main menu. The Save and Quit feature has been
        removed. You can restore your session by ticking the box for Restore
        previous session in the General->Startup options or by using Restore
        Previous Session in the main menu.
    
        Firefox now recognizes the operating system accessibility setting for
        reducing animation
    
        Added search shortcuts for Top Sites: Amazon and Google appear as Top
        Sites tiles on the Firefox Home (New Tab) page. When selected these
        tiles will change focus to the address bar to initiate a search.
        Currently in US only.
    
    
    Fixed
    
        Resolved an issue that prevented the address bar from autofilling
        bookmarked URLs in certain cases
    
        Various security fixes
    
    
    Changed
    
        In the Library, the Open in Sidebar feature for individual bookmarks
        was removed
    
        The option to Never check for updates was removed from about:preferences.
        You can use the DisableAppUpdate enterprise policy as a substitute.
    
        The Ctrl+Tab shortcut now displays thumbnail previews of your tabs and
        cycles through tabs in recently used order. This new default behavior
        is activated only in new profiles and can be changed in preferences.
    
    
    #CVE-2018-12391: HTTP Live Stream audio data is accessible cross-origin
    #CVE-2018-12392: Crash with nested event loops
    #CVE-2018-12393: Integer overflow during Unicode conversion while loading JavaScript
    #CVE-2018-12395: WebExtension bypass of domain restrictions through header rewriting
    #CVE-2018-12396: WebExtension content scripts can execute in disallowed contexts
    #CVE-2018-12397: Missing warning prompt when WebExtension requests local file access
    #CVE-2018-12398: CSP bypass through stylesheet injection in resource URIs
    #CVE-2018-12399: Spoofing of protocol registration notification bar
    #CVE-2018-12400: Favicons are cached in private browsing mode on Firefox for Android
    #CVE-2018-12401: DOS attack through special resource URI parsing
    #CVE-2018-12402: SameSite cookies leak when pages are explicitly saved
    #CVE-2018-12403: Mixed content warning is not displayed when HTTPS page loads a favicon over HTTP
    #CVE-2018-12388: Memory safety bugs fixed in Firefox 63
    #CVE-2018-12390: Memory safety bugs fixed in Firefox 63 and Firefox ESR 60.3

--- www/firefox/patches/patch-ipc_glue_CrossProcessSemaphore.h
+++ www/firefox/patches/patch-ipc_glue_CrossProcessSemaphore.h
@@ -1,11 +1,8 @@
-$NetBSD: patch-ipc_glue_CrossProcessSemaphore.h,v 1.1 2017/06/29 08:07:59 martin Exp $
+$NetBSD: patch-ipc_glue_CrossProcessSemaphore.h,v 1.2 2018/11/04 00:38:44 ryoon Exp $
 
-- avoid use of sem_t on NetBSD
-  http://mail-index.netbsd.org/pkgsrc-bugs/2017/06/23/msg062225.html
-
---- ipc/glue/CrossProcessSemaphore.h.orig	2017-06-05 20:45:20.000000000 +0000
+--- ipc/glue/CrossProcessSemaphore.h.orig	2018-10-18 20:06:06.000000000 +0000
 +++ ipc/glue/CrossProcessSemaphore.h
-@@ -91,7 +91,13 @@ private:
+@@ -93,7 +93,13 @@ private:
    HANDLE mSemaphore;
  #elif !defined(OS_MACOSX)
    RefPtr<mozilla::ipc::SharedMemoryBasic> mSharedBuffer;
