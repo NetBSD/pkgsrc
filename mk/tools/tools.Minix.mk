a521007a946b3bce3f459c8335448d0e8e007f8d
sevan <sevan@pkgsrc.org>
2018-11-05 13:42:24 +0000

    Reduce whitespace noise when compared to NetBSD config. NFC

--- mk/tools/tools.Minix.mk
+++ mk/tools/tools.Minix.mk
@@ -1,4 +1,4 @@
-# $NetBSD: tools.Minix.mk,v 1.10 2018/11/05 13:32:53 sevan Exp $
+# $NetBSD: tools.Minix.mk,v 1.11 2018/11/05 13:42:24 sevan Exp $
 #
 # System-supplied tools for the Minix operating system.
 
@@ -7,7 +7,7 @@ TOOLS_PLATFORM.[?=		/bin/[
 TOOLS_PLATFORM.awk?=		/usr/bin/awk
 TOOLS_PLATFORM.basename?=	/usr/bin/basename
 TOOLS_PLATFORM.bsdtar?=		/bin/tar
-TOOLS_PLATFORM.byacc?= 		/usr/bin/yacc
+TOOLS_PLATFORM.byacc?=		/usr/bin/yacc
 TOOLS_PLATFORM.bzcat?=		/usr/bin/bzcat
 TOOLS_PLATFORM.bzip2?=		/usr/bin/bzip2
 TOOLS_PLATFORM.cat?=		/bin/cat
@@ -15,7 +15,7 @@ TOOLS_PLATFORM.chgrp?=		/bin/chgrp
 TOOLS_PLATFORM.chmod?=		/bin/chmod
 TOOLS_PLATFORM.chown?=		/sbin/chown
 TOOLS_PLATFORM.cmp?=		/usr/bin/cmp
-TOOLS_PLATFORM.cp?=		/bin/cp 
+TOOLS_PLATFORM.cp?=		/bin/cp
 TOOLS_PLATFORM.cut?=		/usr/bin/cut
 TOOLS_PLATFORM.date?=		/bin/date
 TOOLS_PLATFORM.diff?=		/usr/bin/diff
@@ -31,9 +31,9 @@ TOOLS_PLATFORM.file?=		/usr/bin/file
 TOOLS_PLATFORM.find?=		/usr/bin/find
 TOOLS_PLATFORM.flex?=		/usr/bin/flex
 TOOLS_PLATFORM.ftp?=		/usr/bin/ftp
-TOOLS_PLATFORM.gegrep?= 	/usr/bin/egrep
-TOOLS_PLATFORM.gfgrep?= 	/usr/bin/fgrep
-TOOLS_PLATFORM.ggrep?= 		/usr/bin/grep
+TOOLS_PLATFORM.gegrep?=		/usr/bin/egrep
+TOOLS_PLATFORM.gfgrep?=		/usr/bin/fgrep
+TOOLS_PLATFORM.ggrep?=		/usr/bin/grep
 TOOLS_PLATFORM.grep?=		/usr/bin/grep
 TOOLS_PLATFORM.gsoelim?=	${TOOLS_PLATFORM.soelim}	# GNUish
 TOOLS_PLATFORM.gunzip?=		/usr/bin/gunzip -f
@@ -47,21 +47,21 @@ TOOLS_PLATFORM.install?=	/usr/bin/install
 .else
 TOOLS_PLATFORM.install?=	${TOOLDIR}/bin/${MACHINE_GNU_PLATFORM}-install
 .endif
-TOOLS_PLATFORM.install-info?= 	/usr/bin/install-info
+TOOLS_PLATFORM.install-info?=	/usr/bin/install-info
 TOOLS_PLATFORM.ksh?=		/bin/ksh
 TOOLS_PLATFORM.lex?=		/usr/bin/lex
 TOOLS_PLATFORM.ln?=		/bin/ln
 TOOLS_PLATFORM.ls?=		/bin/ls
 TOOLS_PLATFORM.m4?=		/usr/bin/m4
 TOOLS_PLATFORM.mail?=		/usr/bin/mail
-TOOLS_PLATFORM.makeinfo?= 	/usr/bin/makeinfo
+TOOLS_PLATFORM.makeinfo?=	/usr/bin/makeinfo
 TOOLS_PLATFORM.mkdir?=		/bin/mkdir -p
 TOOLS_PLATFORM.mktemp?=		/usr/bin/mktemp
 TOOLS_PLATFORM.mtree?=		/usr/sbin/mtree
 TOOLS_PLATFORM.mv?=		/bin/mv
 TOOLS_PLATFORM.nice?=		/usr/bin/nice
 TOOLS_PLATFORM.nroff?=		/usr/bin/nroff
-TOOLS_PLATFORM.openssl?= 	/usr/bin/openssl
+TOOLS_PLATFORM.openssl?=	/usr/bin/openssl
 TOOLS_PLATFORM.patch?=		/usr/bin/patch
 TOOLS_PLATFORM.pax?=		/bin/pax
 TOOLS_PLATFORM.printf?=		/usr/bin/printf
@@ -97,8 +97,8 @@ TOOLS_PLATFORM.tsort?=		/usr/bin/tsort
 TOOLS_PLATFORM.uniq?=		/usr/bin/uniq
 TOOLS_PLATFORM.wc?=		/usr/bin/wc
 TOOLS_PLATFORM.xargs?=		/usr/bin/xargs
-TOOLS_PLATFORM.xz?= 		/usr/bin/xz
-TOOLS_PLATFORM.xzcat?= 		/usr/bin/xzcat
+TOOLS_PLATFORM.xz?=		/usr/bin/xz
+TOOLS_PLATFORM.xzcat?=		/usr/bin/xzcat
 TOOLS_PLATFORM.yacc?=		/usr/bin/yacc
 
 .if !empty(USE_CROSS_COMPILE:M[yY][eE][sS])
