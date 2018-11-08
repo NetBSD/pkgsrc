6fb46f3a47ad7ad387d4ecc6b87e62c668d33432
schmonz <schmonz@pkgsrc.org>
2018-11-08 20:57:28 +0000

    Bump acceptutils dependency to get STARTTLS support in fixsmtpio(8) (obviating the need for qmail-smtpd(8) to be patched to link OpenSSL).
    Make TLS configurable for submission, POP3, and now also incoming SMTP:
    
    - "yes"  (startup will fail if cert or DH params are missing)
    - "no"   (even if they're present, don't offer TLS)
    - "auto" (the default: offer TLS iff they're present)
    
    Mention TLS setup in MESSAGE.
    
    Delay SMTP greeting by 2 seconds. Enable zen.spamhaus.org RBL.
    
    Bump version.

--- mail/qmail-run/files/qmailsmtpd.sh
+++ mail/qmail-run/files/qmailsmtpd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: qmailsmtpd.sh,v 1.19 2018/11/03 17:08:26 schmonz Exp $
+# $NetBSD: qmailsmtpd.sh,v 1.20 2018/11/08 20:57:28 schmonz Exp $
 #
 # @PKGNAME@ script to control qmail-smtpd (SMTP service).
 #
@@ -11,28 +11,32 @@
 name="qmailsmtpd"
 
 # User-settable rc.conf variables and their default values:
-: ${qmailsmtpd_postenv:=""}
-: ${qmailsmtpd_tcpflags:="-vRl0"}
-: ${qmailsmtpd_tcphost:="0"}
+: ${qmailsmtpd_postenv:="SSL_UID=$(@ID@ -u @UCSPI_SSL_USER@) SSL_GID=$(@ID@ -g @UCSPI_SSL_GROUP@)"}
+: ${qmailsmtpd_tcpflags:="-ne -vRl0"}
+: ${qmailsmtpd_tcphost:="0.0.0.0"}
 : ${qmailsmtpd_tcpport:="25"}
-: ${qmailsmtpd_datalimit:="180000000"}
+: ${qmailsmtpd_datalimit:="360000000"}
 : ${qmailsmtpd_pretcpserver:=""}
-: ${qmailsmtpd_tcpserver:="@PREFIX@/bin/tcpserver"}
-: ${qmailsmtpd_presmtpd:=""}
+: ${qmailsmtpd_tcpserver:="@PREFIX@/bin/sslserver"}
+: ${qmailsmtpd_presmtpd:="@PREFIX@/bin/greetdelay @PREFIX@/bin/rblsmtpd -r zen.spamhaus.org @PREFIX@/bin/fixsmtpio"}
 : ${qmailsmtpd_smtpdcmd:="@PREFIX@/bin/qmail-smtpd"}
 : ${qmailsmtpd_postsmtpd:=""}
 : ${qmailsmtpd_log:="YES"}
 : ${qmailsmtpd_logcmd:="logger -t nbqmail/smtpd -p mail.info"}
 : ${qmailsmtpd_nologcmd:="@PREFIX@/bin/multilog -*"}
+: ${qmailsmtpd_tls:="auto"}
+: ${qmailsmtpd_tls_dhparams:="@PKG_SYSCONFDIR@/control/dh2048.pem"}
+: ${qmailsmtpd_tls_cert:="@PKG_SYSCONFDIR@/control/servercert.pem"}
 
 if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
 rcvar=${name}
-required_files="@PKG_SYSCONFDIR@/control/concurrencyincoming"
-required_files="${required_files} @PKG_SYSCONFDIR@/tcp.smtp.cdb"
+required_files="@PKG_SYSCONFDIR@/control/me"
+required_files="${required_files} @PKG_SYSCONFDIR@/control/concurrencyincoming"
 required_files="${required_files} @PKG_SYSCONFDIR@/control/rcpthosts"
+required_files="${required_files} @PKG_SYSCONFDIR@/tcp.smtp.cdb"
 command="${qmailsmtpd_tcpserver}"
 procname=nb${name}
 start_precmd="qmailsmtpd_precmd"
@@ -43,11 +47,35 @@ cont_cmd="qmailsmtpd_cont"
 cdb_cmd="qmailsmtpd_cdb"
 reload_cmd=${cdb_cmd}
 
+qmailsmtpd_configure_tls() {
+	if [ "auto" = "${qmailsmtpd_tls}" ]; then
+		if [ -f "${qmailsmtpd_tls_dhparams}" ] && [ -f "${qmailsmtpd_tls_cert}" ]; then
+			qmailsmtpd_enable_tls
+		else
+			qmailsmtpd_disable_tls
+		fi
+	elif [ -f /etc/rc.subr ] && checkyesno qmailsmtpd_tls; then
+		qmailsmtpd_enable_tls
+	else
+		qmailsmtpd_disable_tls
+	fi
+}
+
+qmailsmtpd_disable_tls() {
+	qmailsmtpd_postenv="${qmailsmtpd_postenv} DISABLETLS=1"
+}
+
+qmailsmtpd_enable_tls() {
+	qmailsmtpd_postenv="${qmailsmtpd_postenv} DHFILE=${qmailsmtpd_tls_dhparams}"
+	qmailsmtpd_postenv="${qmailsmtpd_postenv} CERTFILE=${qmailsmtpd_tls_cert}"
+}
+
 qmailsmtpd_precmd()
 {
 	if [ -f /etc/rc.subr ] && ! checkyesno qmailsmtpd_log; then
 		qmailsmtpd_logcmd=${qmailsmtpd_nologcmd}
 	fi
+	qmailsmtpd_configure_tls
 	# tcpserver(1) is akin to inetd(8), but runs one service per process.
 	# We want to signal only the tcpserver process responsible for this
 	# service. Use argv0(1) to set procname to "nbqmailsmtpd".
