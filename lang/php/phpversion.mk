20cd568a93c9050a0f52dd39c913d78fa5c4c72b
taca <taca@pkgsrc.org>
2018-11-08 13:59:11 +0000

    lang/php71: update to 7.1.24
    08 Nov 2018, PHP 7.1.24
    
    - Core:
      . Fixed bug #76946 (Cyclic reference in generator not detected). (Nikita)
      . Fixed bug #77035 (The phpize and ./configure create redundant .deps file).
        (Peter Kokot)
      . Fixed bug #77041 (buildconf should output error messages to stderr)
        (Mizunashi Mana)
    
    - Date:
      . Fixed bug #75851 (Year component overflow with date formats "c", "o", "r"
        and "y"). (Adam Saponara)
    
    - FCGI:
      . Fixed bug #76948 (Failed shutdown/reboot or end session in Windows).
        (Anatol)
      . Fixed bug #76954 (apache_response_headers removes last character from header
        name). (stodorovic)
    
    - FTP:
      . Fixed bug #76972 (Data truncation due to forceful ssl socket shutdown).
        (Manuel Mausz)
    
    - intl:
      . Fixed bug #76942 (U_ARGUMENT_TYPE_MISMATCH). (anthrax at unixuser dot org)
    
    - Standard:
      . Fixed bug #76965 (INI_SCANNER_RAW doesn't strip trailing whitespace).
        (Pierrick)
    
    - Tidy:
      . Fixed bug #77027 (tidy::getOptDoc() not available on Windows). (cmb)
    
    - XML:
      . Fixed bug #30875 (xml_parse_into_struct() does not resolve entities). (cmb)
      . Add support for getting SKIP_TAGSTART and SKIP_WHITE options. (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.233 2018/10/13 06:18:37 taca Exp $
+# $NetBSD: phpversion.mk,v 1.234 2018/11/08 13:59:11 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.38
 PHP70_VERSION=	7.0.32
-PHP71_VERSION=	7.1.23
+PHP71_VERSION=	7.1.24
 PHP72_VERSION=	7.2.11
 
 # Define initial release of major version.
