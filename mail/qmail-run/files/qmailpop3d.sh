5928e59ee36943db538150bcadb52daa0aebf9bf
schmonz <schmonz@pkgsrc.org>
2018-11-08 20:57:28 +0000

    Bump acceptutils dependency to get STARTTLS support in fixsmtpio(8) (obviating the need for qmail-smtpd(8) to be patched to link OpenSSL).
    Make TLS configurable for submission, POP3, and now also incoming SMTP:
    
    - "yes"  (startup will fail if cert or DH params are missing)
    - "no"   (even if they're present, don't offer TLS)
    - "auto" (the default: offer TLS iff they're present)
    
    Mention TLS setup in MESSAGE.
    
    Delay SMTP greeting by 2 seconds. Enable zen.spamhaus.org RBL.
    
    Bump version.

--- mail/qmail-run/files/qmailpop3d.sh
+++ mail/qmail-run/files/qmailpop3d.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: qmailpop3d.sh,v 1.23 2018/11/03 17:08:26 schmonz Exp $
+# $NetBSD: qmailpop3d.sh,v 1.24 2018/11/08 20:57:28 schmonz Exp $
 #
 # @PKGNAME@ script to control qmail-pop3d (POP3 server for Maildirs).
 #
@@ -10,7 +10,7 @@
 name="qmailpop3d"
 
 # User-settable rc.conf variables and their default values:
-: ${qmailpop3d_postenv:="SSL_UID=$(@ID@ -u @UCSPI_SSL_USER@) SSL_GID=$(@ID@ -g @UCSPI_SSL_GROUP@) DHFILE=@PKG_SYSCONFDIR@/control/dh2048.pem CERTFILE=@PKG_SYSCONFDIR@/control/servercert.pem"}
+: ${qmailpop3d_postenv:="SSL_UID=$(@ID@ -u @UCSPI_SSL_USER@) SSL_GID=$(@ID@ -g @UCSPI_SSL_GROUP@)"}
 : ${qmailpop3d_tcpflags:="-ne -vRl0"}
 : ${qmailpop3d_tcphost:="0.0.0.0"}
 : ${qmailpop3d_tcpport:="110"}
@@ -24,15 +24,16 @@ name="qmailpop3d"
 : ${qmailpop3d_log:="YES"}
 : ${qmailpop3d_logcmd:="logger -t nbqmail/pop3d -p mail.info"}
 : ${qmailpop3d_nologcmd:="@PREFIX@/bin/multilog -*"}
+: ${qmailpop3d_tls:="auto"}
+: ${qmailpop3pd_tls_dhparams:="@PKG_SYSCONFDIR@/control/dh2048.pem"}
+: ${qmailpop3pd_tls_cert:="@PKG_SYSCONFDIR@/control/servercert.pem"}
 
 if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
 rcvar=${name}
-required_files="@PKG_SYSCONFDIR@/control/dh2048.pem"
-required_files="${required_files} @PKG_SYSCONFDIR@/control/servercert.pem"
-required_files="${required_files} @PKG_SYSCONFDIR@/control/me"
+required_files="@PKG_SYSCONFDIR@/control/me"
 required_files="${required_files} @PKG_SYSCONFDIR@/control/concurrencypop3"
 required_files="${required_files} @PKG_SYSCONFDIR@/control/pop3capabilities"
 required_files="${required_files} @PKG_SYSCONFDIR@/tcp.pop3.cdb"
@@ -46,11 +47,35 @@ cont_cmd="qmailpop3d_cont"
 cdb_cmd="qmailpop3d_cdb"
 reload_cmd=${cdb_cmd}
 
+qmailpop3d_configure_tls() {
+	if [ "auto" = "${qmailpop3d_tls}" ]; then
+		if [ -f "${qmailpop3d_tls_dhparams}" ] && [ -f "${qmailpop3d_tls_cert}" ]; then
+			qmailpop3d_enable_tls
+		else
+			qmailpop3d_disable_tls
+		fi
+	elif [ -f /etc/rc.subr ] && checkyesno qmailpop3d_tls; then
+		qmailpop3d_enable_tls
+	else
+		qmailpop3d_disable_tls
+	fi
+}
+
+qmailpop3d_disable_tls() {
+	qmailpop3d_postenv="${qmailpop3d_postenv} DISABLETLS=1"
+}
+
+qmailpop3d_enable_tls() {
+	qmailpop3d_postenv="${qmailpop3d_postenv} DHFILE=${qmailpop3d_tls_dhparams}"
+	qmailpop3d_postenv="${qmailpop3d_postenv} CERTFILE=${qmailpop3d_tls_cert}"
+}
+
 qmailpop3d_precmd()
 {
 	if [ -f /etc/rc.subr ] && ! checkyesno qmailpop3d_log; then
 		qmailpop3d_logcmd=${qmailpop3d_nologcmd}
 	fi
+	qmailpop3d_configure_tls
 	# tcpserver(1) is akin to inetd(8), but runs one service per process.
 	# We want to signal only the tcpserver process responsible for this
 	# service. Use argv0(1) to set procname to "nbqmailpop3d".
