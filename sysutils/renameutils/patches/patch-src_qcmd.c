bf3fd0947e2468fe843d0d87816f2a85d790b2cd
bsiegert <bsiegert@pkgsrc.org>
2018-11-09 12:50:49 +0000

    Import renameutils-0.12.0 from pkgsrc-wip.
    The file renaming utilities consists of five programs designed to make renaming
    of files faster and less cumbersome:
    - qmv ("quick move") allows a bunch of file names to be edited in a text editor;
    - imv ("interactive move") allows a single file name to be edited in the
      terminal using the GNU Readline library
    - qcp and icp are similar to qmv and imv but copy files instead of moving them;
    - deurlname removes URL encoded characters (such as %20 representing space) from
      file names.
    
    PR pkg/52546, which I neglected for a year.

---
+++ sysutils/renameutils/patches/patch-src_qcmd.c
@@ -0,0 +1,14 @@
+$NetBSD: patch-src_qcmd.c,v 1.1 2018/11/09 12:50:49 bsiegert Exp $
+
+Use ls from GNU coreutils.
+--- src/qcmd.c.orig	2011-08-21 17:15:51 UTC
++++ src/qcmd.c
+@@ -239,7 +239,7 @@
+     editor_program = xstrdup(editor_program);
+ 
+     if (ls_program == NULL)
+-        ls_program = xstrdup("ls");
++        ls_program = xstrdup("gls");
+ 
+     /* Parse format options */
+     if (format_options != NULL && !format->parse_options(format_options))
