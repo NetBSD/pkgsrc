abc4285676e7584d54b29fee6840c2a8f57c080b
mrg <mrg@pkgsrc.org>
2018-11-10 11:45:27 +0000

    port the arm64 support to GCC 8.

---
+++ lang/gcc8/patches/patch-gcc_config_aarch64_aarch64-builtins.c
@@ -0,0 +1,16 @@
+$NetBSD: patch-gcc_config_aarch64_aarch64-builtins.c,v 1.1 2018/11/10 11:45:27 mrg Exp $
+
+initialise subtarget builtins so cabs*() are renamed.
+
+--- gcc/config/aarch64/aarch64-builtins.c.orig	2017-01-20 21:03:41.000000000 +0000
++++ gcc/config/aarch64/aarch64-builtins.c	2018-11-10 00:44:41.905576216 +0000
+@@ -991,6 +991,9 @@
+      register them.  */
+   if (!TARGET_ILP32)
+     aarch64_init_pauth_hint_builtins ();
++#ifdef SUBTARGET_INIT_BUILTINS
++  SUBTARGET_INIT_BUILTINS;
++#endif
+ }
+ 
+ tree
