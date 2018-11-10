29636b7cb23f25ba4ddcfb05bba73b7e3a5f8273
leot <leot@pkgsrc.org>
2018-11-10 19:51:12 +0000

    cups{,-base}: Update print/cups{,-base} to 2.2.9
    pkgsrc changes:
    - Remove patches/patch-cups_ipp.c, no more needed (applied)
    - Remove patches/patch-ppdc_Makefile, libcupsppdc.la is no more installed
      (also libcupscgi.la, libcupsmime.la are no longer installed, unfortunately no
      rationale seems present in the changelog about that)
    
    Changes:
    2.2.9
    -----
    - Localization changes (Issue #5348, Issue #5362, Issue #5408)
    - Documentation updates (Issue #5369)
    - The lpadmin command would create a non-working printer in some error cases
      (Issue #5305)
    - The scheduler would crash if an empty `AccessLog` directive was specified
      (Issue #5309)
    - Fixed a regression in the changes to ippValidateAttribute (Issue #5322,
      Issue #5330)
    - Fixed a crash bug in the Epson dot matrix driver (Issue #5323)
    - Automatic debug logging of job errors did not work with systemd (Issue #5337)
    - The web interface did not list the IPP Everywhere "driver" (Issue #5338)
    - The IPP Everywhere "driver" now properly supports face-up printers
      (Issue #5345)
    - Fixed some typos in the label printer drivers (Issue #5350)
    - Multi-file jobs could get stuck if the backend failed (Issue #5359,
      Issue #5413)
    - The IPP Everywhere "driver" no longer does local filtering when printing to
      a shared CUPS printer (Issue #5361)
    - The lpadmin command now correctly reports IPP errors when configuring an
      IPP Everywhere printer (Issue #5370)
    - Fixed some memory leaks discovered by Coverity (Issue #5375)
    - The PPD compiler incorrectly terminated JCL options (Issue #5379)
    - The cupstestppd utility did not generate errors for missing/mismatched
      CloseUI/JCLCloseUI keywords (Issue #5381)
    - The scheduler now reports the actual location of the log file (Issue #5398)
    - Added a USB quirk rule (Issue #5420)
    - The scheduler was being backgrounded on macOS, causing applications to spin
      (rdar://40436080)
    - The scheduler did not validate that required initial request attributes were
      in the operation group (rdar://41098178)
    - Authentication in the web interface did not work on macOS (rdar://41444473)
    - Fixed an issue with HTTP Digest authentication (rdar://41709086)
    - The scheduler could crash when job history was purged (rdar://42198057)
    - Dropped non-working RSS subscriptions UI from web interface templates.
    - Fixed a memory leak for some IPP (extension) syntaxes.

--- print/cups-base/patches/patch-test_ippfind.c
+++ print/cups-base/patches/patch-test_ippfind.c
@@ -1,16 +1,16 @@
-$NetBSD: patch-test_ippfind.c,v 1.1 2017/11/12 14:10:15 khorben Exp $
+$NetBSD: patch-test_ippfind.c,v 1.2 2018/11/10 19:51:12 leot Exp $
 
---- test/ippfind.c.orig	2015-05-19 02:36:15.000000000 +0000
+--- test/ippfind.c.orig	2018-11-08 13:49:36.000000000 +0000
 +++ test/ippfind.c
-@@ -1719,11 +1719,6 @@ dnssd_error_string(int error)		/* I - Er
+@@ -1765,11 +1765,6 @@ dnssd_error_string(int error)		/* I - Er
  
      case kDNSServiceErr_PollingMode :
          return ("Service polling mode error.");
 -
--#ifndef WIN32
+-#ifndef _WIN32
 -    case kDNSServiceErr_Timeout :
 -        return ("Service timeout.");
--#endif /* !WIN32 */
+-#endif /* !_WIN32 */
    }
  
  #  elif defined(HAVE_AVAHI)
