2ad8674be0a5e8f14e8ec4ede788c796b0329665
jperkin <jperkin@pkgsrc.org>
2018-11-12 14:22:58 +0000

    mk: Add CTF infrastructure support.
    Platform support is determined by _OPSYS_SUPPORTS_CTF from mk/platform, the
    user enables support by setting PKGSRC_USE_CTF=yes, and packages can
    explicitly disable support with CTF_SUPPORTED=no or skip certain files with
    CTF_FILES_SKIP.
    
    The path to ctfconvert is configured via TOOLS_PLATFORM.ctfconvert.
    
    If all of the requisite variables are enabled, a compiler-specific debug flag
    is passed via the wrappers to ensure we have DWARF information to convert,
    _INSTALL_UNSTRIPPED is explicitly defined to avoid binaries being stripped
    prior to conversion, and the conversion is performed during the install stage.
    
    It is recommended that users who enable the feature also set STRIP_DEBUG=yes
    to reduce the final binary size once the conversion has been performed.
    
    This has been used for the past year in Joyent SmartOS builds.  FreeBSD is
    marked as supported but is untested.

--- mk/platform/FreeBSD.mk
+++ mk/platform/FreeBSD.mk
@@ -1,4 +1,4 @@
-# $NetBSD: FreeBSD.mk,v 1.36 2017/10/03 13:18:00 jperkin Exp $
+# $NetBSD: FreeBSD.mk,v 1.37 2018/11/12 14:22:58 jperkin Exp $
 #
 # Variable definitions for the FreeBSD operating system.
 
@@ -77,6 +77,7 @@ PKG_HAVE_KQUEUE=	# defined
 _OPSYS_SUPPORTS_SSP=	yes
 .endif
 
+_OPSYS_SUPPORTS_CTF=		yes # Compact Type Format conversion.
 _OPSYS_SUPPORTS_CWRAPPERS=	yes
 _OPSYS_CAN_CHECK_SHLIBS=	yes # use readelf in check/bsd.check-vars.mk
 _OPSYS_CAN_CHECK_SSP=		no  # only supports libssp at this time
