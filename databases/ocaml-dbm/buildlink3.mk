8f3e30e298cdd955cbd82265d3af7255c00aeb61
jaapb <jaapb@pkgsrc.org>
2018-11-12 16:10:16 +0000

    Revbumps associated with update of lang/ocaml.

--- databases/ocaml-dbm/buildlink3.mk
+++ databases/ocaml-dbm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2018/07/19 15:15:21 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2018/11/12 16:10:16 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-dbm
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-dbm
 OCAML_DBM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-dbm+=	ocaml-dbm>=1.0
-BUILDLINK_ABI_DEPENDS.ocaml-dbm?=	ocaml-dbm>=1.0nb16
+BUILDLINK_ABI_DEPENDS.ocaml-dbm?=	ocaml-dbm>=1.0nb17
 BUILDLINK_PKGSRCDIR.ocaml-dbm?=		../../databases/ocaml-dbm
 
 .include "../../databases/gdbm/buildlink3.mk"
