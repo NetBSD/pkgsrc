3d163ffd82b0e76752223363a9ff1e826a9cfb8f
jaapb <jaapb@pkgsrc.org>
2018-11-12 16:10:51 +0000

    Updated lang/camlp5 to version 7.07.
    This includes support for ocaml 4.07.1.

--- lang/camlp5/buildlink3.mk
+++ lang/camlp5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2018/07/19 14:19:46 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2018/11/12 16:10:51 jaapb Exp $
 #
 
 BUILDLINK_TREE+=	camlp5
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	camlp5
 .if !defined(CAMLP5_BUILDLINK3_MK)
 CAMLP5_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.camlp5+=	camlp5>=5.01
-BUILDLINK_ABI_DEPENDS.camlp5+=	camlp5>=7.06
+BUILDLINK_API_DEPENDS.camlp5+=	camlp5>=7.00
+BUILDLINK_ABI_DEPENDS.camlp5+=	camlp5>=7.07
 BUILDLINK_PKGSRCDIR.camlp5?=	../../lang/camlp5
 
 .include "../../lang/ocaml/buildlink3.mk"
