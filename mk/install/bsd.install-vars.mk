71e098497c0c36d42c339d6a55e29bdcd566dd8a
jperkin <jperkin@pkgsrc.org>
2018-11-12 14:22:58 +0000

    mk: Add CTF infrastructure support.
    Platform support is determined by _OPSYS_SUPPORTS_CTF from mk/platform, the
    user enables support by setting PKGSRC_USE_CTF=yes, and packages can
    explicitly disable support with CTF_SUPPORTED=no or skip certain files with
    CTF_FILES_SKIP.
    
    The path to ctfconvert is configured via TOOLS_PLATFORM.ctfconvert.
    
    If all of the requisite variables are enabled, a compiler-specific debug flag
    is passed via the wrappers to ensure we have DWARF information to convert,
    _INSTALL_UNSTRIPPED is explicitly defined to avoid binaries being stripped
    prior to conversion, and the conversion is performed during the install stage.
    
    It is recommended that users who enable the feature also set STRIP_DEBUG=yes
    to reduce the final binary size once the conversion has been performed.
    
    This has been used for the past year in Joyent SmartOS builds.  FreeBSD is
    marked as supported but is untested.

--- mk/install/bsd.install-vars.mk
+++ mk/install/bsd.install-vars.mk
@@ -1,10 +1,19 @@
-# $NetBSD: bsd.install-vars.mk,v 1.8 2009/09/02 14:40:40 joerg Exp $
+# $NetBSD: bsd.install-vars.mk,v 1.9 2018/11/12 14:22:58 jperkin Exp $
 #
 # This Makefile fragment is included separately by bsd.pkg.mk and
 # defines some variables which must be defined earlier than where
 # bsd.install.mk is included.
 #
 # Package-settable variables:
+#
+# CTF_SUPPORTED
+#	If set to "no", CTF conversion will not be attempted and any
+#	necessary debug CFLAGS will not be added.
+#
+# CTF_FILES_SKIP
+#	A list of shell patterns (like lib/*) relative to ${PREFIX} that
+#	should be excluded from CTF conversion.  Note that a * in a pattern
+#	also matches a slash in a pathname.
 #
 # INSTALLATION_DIRS_FROM_PLIST
 #	If set to "yes", the static PLIST files of the package will
@@ -45,6 +54,17 @@ _MANCOMPRESSED!=							\
 .endif
 _MANZ=		${MANZ:Dyes:Uno}
 MAKEVARS+=	_MANCOMPRESSED _MANZ
+
+# The logic for setting _PKGSRC_USE_CTF is in bsd.prefs.mk, at this point we
+# are only concerned if we should set up the tools and variables or not.
+#
+.if ${_PKGSRC_USE_CTF} == "yes"
+TOOLS_CREATE+=		ctfconvert
+TOOLS_PATH.ctfconvert=	${TOOLS_PLATFORM.ctfconvert}
+TOOLS_ARGS.ctfconvert?=	-i
+CTFCONVERT?=		ctfconvert
+CTF_FILES_SKIP?=	# none
+.endif
 
 STRIP_DEBUG?=	no
 
