dfc2c8a21682282f813c4288c73ad7fe9d503fe7
markd <markd@pkgsrc.org>
2018-11-13 11:10:41 +0000

    xine-lib: update to 1.2.9
    2018-01-11: Release
    xine-lib 1.2.9
     Changes include new video single step feature, keyframe index and x32
     support. Optimizations in xine engine core functionality, resulting in
     ex. faster seeking and smaller CPU usage. Small fixes and optimizations
     all around the codebase.
    
    2017-02-21: Release
    xine-lib 1.2.8
     Lot of changes since 1.2.6. Support for modern codecs, hardware and
     software. Small fixes and optimizations all around the codebase.
    
     OpenGL 2.0, Xv and VAAPI fixes. H.265 hardware decoding support (VAAPI).
     Hardware overlay support with Raspbery Pi.
    
     Support for recent ffmpeg versions. Deep color support (9/10bit) with
     ffmpeg. Multithreaded decoding fixes.
    
     Audio channel mixing and gain control improvements (ffmpeg, faad), AAC
     LATM support with faad, DVB AAC compatibility fixes. "Time Domain Audio
     Analyzer" Visualization Post Plugin.
    
     Auto recover from temporary DVB signal loss. Improved Matroska
     compatibility (TrueHD and PCM sound, HDMV/Text subtitles, compressed
     PGS subtitles). Qt fixes, support for low framerate mp4 and H.265 in mp4.
     mp3 compatibility fixes.

--- multimedia/xine-lib/patches/patch-src-post-planar-eq.c
+++ multimedia/xine-lib/patches/patch-src-post-planar-eq.c
@@ -1,21 +1,21 @@
-$NetBSD: patch-src-post-planar-eq.c,v 1.3 2014/06/27 12:17:22 jperkin Exp $
+$NetBSD: patch-src-post-planar-eq.c,v 1.4 2018/11/13 11:10:41 markd Exp $
 
 https://bugs.xine-project.org/show_bug.cgi?id=524
 
 Disable MMX sections on SunOS.
 
---- src/post/planar/eq.c.orig	2013-09-18 10:04:54.000000000 +0000
+--- src/post/planar/eq.c.orig	2018-01-11 12:49:47.000000000 +0000
 +++ src/post/planar/eq.c
 @@ -31,7 +31,7 @@
  #include <pthread.h>
  
  
--#if defined(ARCH_X86) || defined(ARCH_X86_64)
-+#if (defined(ARCH_X86) || defined(ARCH_X86_64)) && !defined(__sun)
- static void process_MMX(unsigned char *dest, int dstride, unsigned char *src, int sstride,
- 		    int w, int h, int brightness, int contrast)
- {
-@@ -138,9 +138,11 @@ typedef struct eq_parameters_s {
+-#if defined(ARCH_X86)
++#if defined(ARCH_X86) && !defined(__sun)
+ 
+ #if defined(ARCH_X86_64)
+ #  define MEM1(reg) "(%"reg")"
+@@ -157,9 +157,11 @@ typedef struct eq_parameters_s {
   * description of params struct
   */
  START_PARAM_DESCR( eq_parameters_t )
@@ -29,12 +29,12 @@ Disable MMX sections on SunOS.
              "contrast" )
  END_PARAM_DESCR( param_descr )
  
-@@ -259,7 +261,7 @@ static post_plugin_t *eq_open_plugin(pos
+@@ -278,7 +280,7 @@ static post_plugin_t *eq_open_plugin(pos
    }
  
    process = process_C;
--#if defined(ARCH_X86) || defined(ARCH_X86_64)
-+#if (defined(ARCH_X86) || defined(ARCH_X86_64)) && !defined(__sun)
+-#if defined(ARCH_X86)
++#if defined(ARCH_X86) && !defined(__sun)
    if( xine_mm_accel() & MM_ACCEL_X86_MMX )
      process = process_MMX;
  #endif
