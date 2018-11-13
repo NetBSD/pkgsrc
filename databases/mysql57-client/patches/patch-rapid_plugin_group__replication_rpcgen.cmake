1ba770079579de4c469ae03242b07107a0b11752
markd <markd@pkgsrc.org>
2018-11-13 11:24:38 +0000

    mysql57-client: tweak patch to work on modern linux copy tirpc handling from the else clause for linux systems that no longer have sun rpc in glibc.

--- databases/mysql57-client/patches/patch-rapid_plugin_group__replication_rpcgen.cmake
+++ databases/mysql57-client/patches/patch-rapid_plugin_group__replication_rpcgen.cmake
@@ -1,15 +1,20 @@
-$NetBSD: patch-rapid_plugin_group__replication_rpcgen.cmake,v 1.1 2017/01/12 09:39:47 jperkin Exp $
+$NetBSD: patch-rapid_plugin_group__replication_rpcgen.cmake,v 1.2 2018/11/13 11:24:38 markd Exp $
 
 Don't depend on rpcgen, just use the pre-generated headers.
 
---- rapid/plugin/group_replication/rpcgen.cmake.orig	2016-11-28 13:32:52.000000000 +0000
+--- rapid/plugin/group_replication/rpcgen.cmake.orig	2018-06-08 09:56:29.000000000 +0000
 +++ rapid/plugin/group_replication/rpcgen.cmake
-@@ -64,7 +64,7 @@ FOREACH(X xcom_vp)
+@@ -64,7 +64,12 @@ FOREACH(X xcom_vp)
    SET (x_vanilla_h      ${XCOM_BASEDIR}/${X}.h.gen)
    SET (x_vanilla_c      ${XCOM_BASEDIR}/${X}_xdr.c.gen)
  
 -  IF(WIN32)
 +  IF(1)
++    IF(RPC_INCLUDE_DIR STREQUAL "/usr/include/tirpc")
++      INCLUDE_DIRECTORIES(SYSTEM /usr/include/tirpc)
++      ADD_DEFINITIONS(-DHAVE_TIRPC)
++      SET(TIRPC_LIBRARY tirpc)
++    ENDIF()
      # on windows system's there is no rpcgen, thence copy
      # the files in the source directory
      ADD_CUSTOM_COMMAND(OUTPUT ${x_gen_h} ${x_gen_c} ${x_tmp_plat_h}
