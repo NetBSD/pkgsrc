daf765ed4af83400bf5ba2f8f4660fa664e9049c
markd <markd@pkgsrc.org>
2018-11-13 11:10:41 +0000

    xine-lib: update to 1.2.9
    2018-01-11: Release
    xine-lib 1.2.9
     Changes include new video single step feature, keyframe index and x32
     support. Optimizations in xine engine core functionality, resulting in
     ex. faster seeking and smaller CPU usage. Small fixes and optimizations
     all around the codebase.
    
    2017-02-21: Release
    xine-lib 1.2.8
     Lot of changes since 1.2.6. Support for modern codecs, hardware and
     software. Small fixes and optimizations all around the codebase.
    
     OpenGL 2.0, Xv and VAAPI fixes. H.265 hardware decoding support (VAAPI).
     Hardware overlay support with Raspbery Pi.
    
     Support for recent ffmpeg versions. Deep color support (9/10bit) with
     ffmpeg. Multithreaded decoding fixes.
    
     Audio channel mixing and gain control improvements (ffmpeg, faad), AAC
     LATM support with faad, DVB AAC compatibility fixes. "Time Domain Audio
     Analyzer" Visualization Post Plugin.
    
     Auto recover from temporary DVB signal loss. Improved Matroska
     compatibility (TrueHD and PCM sound, HDMV/Text subtitles, compressed
     PGS subtitles). Qt fixes, support for low framerate mp4 and H.265 in mp4.
     mp3 compatibility fixes.

--- multimedia/xine-lib/patches/patch-src-post-deinterlace-xine_plugin.c
+++ multimedia/xine-lib/patches/patch-src-post-deinterlace-xine_plugin.c
@@ -1,48 +1,48 @@
-$NetBSD: patch-src-post-deinterlace-xine_plugin.c,v 1.2 2014/05/18 15:38:18 wiz Exp $
+$NetBSD: patch-src-post-deinterlace-xine_plugin.c,v 1.3 2018/11/13 11:10:41 markd Exp $
 
 https://bugs.xine-project.org/show_bug.cgi?id=524
 
---- src/post/deinterlace/xine_plugin.c.orig	2011-08-30 13:13:35.000000000 +0000
+--- src/post/deinterlace/xine_plugin.c.orig	2018-01-11 12:49:47.000000000 +0000
 +++ src/post/deinterlace/xine_plugin.c
-@@ -86,23 +86,32 @@ typedef struct deinterlace_parameters_s 
+@@ -86,23 +86,32 @@ typedef struct deinterlace_parameters_s
   * description of params struct
   */
  START_PARAM_DESCR( deinterlace_parameters_t )
--PARAM_ITEM( POST_PARAM_TYPE_INT, method, enum_methods, 0, 0, 0,
+-PARAM_ITEM( POST_PARAM_TYPE_INT, method, (char **)enum_methods, 0, 0, 0,
 +PARAM_ITEM( deinterlace_parameters_t,
-+	    POST_PARAM_TYPE_INT, method, enum_methods, 0, 0, 0,
++            POST_PARAM_TYPE_INT, method, (char **)enum_methods, 0, 0, 0,
              "deinterlace method" )
 -PARAM_ITEM( POST_PARAM_TYPE_BOOL, enabled, NULL, 0, 1, 0,
 +PARAM_ITEM( deinterlace_parameters_t,
-+	    POST_PARAM_TYPE_BOOL, enabled, NULL, 0, 1, 0,
++            POST_PARAM_TYPE_BOOL, enabled, NULL, 0, 1, 0,
              "enable/disable" )
--PARAM_ITEM( POST_PARAM_TYPE_INT, pulldown, enum_pulldown, 0, 0, 0,
+-PARAM_ITEM( POST_PARAM_TYPE_INT, pulldown, (char **)enum_pulldown, 0, 0, 0,
 +PARAM_ITEM( deinterlace_parameters_t,
-+	    POST_PARAM_TYPE_INT, pulldown, enum_pulldown, 0, 0, 0,
++            POST_PARAM_TYPE_INT, pulldown, (char **)enum_pulldown, 0, 0, 0,
              "pulldown algorithm" )
 -PARAM_ITEM( POST_PARAM_TYPE_INT, pulldown_error_wait, NULL, 0, 0, 0,
 +PARAM_ITEM( deinterlace_parameters_t,
-+	    POST_PARAM_TYPE_INT, pulldown_error_wait, NULL, 0, 0, 0,
++            POST_PARAM_TYPE_INT, pulldown_error_wait, NULL, 0, 0, 0,
              "number of frames of telecine pattern sync required before mode change" )
--PARAM_ITEM( POST_PARAM_TYPE_INT, framerate_mode, enum_framerate, 0, 0, 0,
+-PARAM_ITEM( POST_PARAM_TYPE_INT, framerate_mode, (char **)enum_framerate, 0, 0, 0,
 +PARAM_ITEM( deinterlace_parameters_t,
-+	    POST_PARAM_TYPE_INT, framerate_mode, enum_framerate, 0, 0, 0,
++            POST_PARAM_TYPE_INT, framerate_mode, (char **)enum_framerate, 0, 0, 0,
              "framerate output mode" )
 -PARAM_ITEM( POST_PARAM_TYPE_BOOL, judder_correction, NULL, 0, 1, 0,
 +PARAM_ITEM( deinterlace_parameters_t,
-+	    POST_PARAM_TYPE_BOOL, judder_correction, NULL, 0, 1, 0,
++            POST_PARAM_TYPE_BOOL, judder_correction, NULL, 0, 1, 0,
              "make frames evenly spaced for film mode (24 fps)" )
 -PARAM_ITEM( POST_PARAM_TYPE_BOOL, use_progressive_frame_flag, NULL, 0, 1, 0,
 +PARAM_ITEM( deinterlace_parameters_t,
-+	    POST_PARAM_TYPE_BOOL, use_progressive_frame_flag, NULL, 0, 1, 0,
++            POST_PARAM_TYPE_BOOL, use_progressive_frame_flag, NULL, 0, 1, 0,
              "disable deinterlacing when progressive_frame flag is set" )
 -PARAM_ITEM( POST_PARAM_TYPE_BOOL, chroma_filter, NULL, 0, 1, 0,
 +PARAM_ITEM( deinterlace_parameters_t,
-+	    POST_PARAM_TYPE_BOOL, chroma_filter, NULL, 0, 1, 0,
++            POST_PARAM_TYPE_BOOL, chroma_filter, NULL, 0, 1, 0,
              "apply chroma filter after deinterlacing" )
 -PARAM_ITEM( POST_PARAM_TYPE_BOOL, cheap_mode, NULL, 0, 1, 0,
 +PARAM_ITEM( deinterlace_parameters_t,
-+	    POST_PARAM_TYPE_BOOL, cheap_mode, NULL, 0, 1, 0,
++            POST_PARAM_TYPE_BOOL, cheap_mode, NULL, 0, 1, 0,
              "skip image format conversion - cheaper but not 100% correct" )
  END_PARAM_DESCR( param_descr )
  
--- multimedia/xine-lib/patches/patch-src-post-planar-noise.c
+++ multimedia/xine-lib/patches/patch-src-post-planar-noise.c
@@ -1,30 +1,21 @@
-$NetBSD: patch-src-post-planar-noise.c,v 1.3 2014/06/27 12:17:22 jperkin Exp $
+$NetBSD: patch-src-post-planar-noise.c,v 1.4 2018/11/13 11:10:41 markd Exp $
 
 https://bugs.xine-project.org/show_bug.cgi?id=524
 
 Disable MMX sections on SunOS.
 
---- src/post/planar/noise.c.orig	2013-09-18 10:04:54.000000000 +0000
+--- src/post/planar/noise.c.orig	2018-01-11 12:49:47.000000000 +0000
 +++ src/post/planar/noise.c
-@@ -155,7 +155,7 @@ static inline void lineNoise_C(uint8_t *
-     }
- }
- 
--#ifdef ARCH_X86
-+#if defined(ARCH_X86) && !defined(__sun)
- static inline void lineNoise_MMX(uint8_t *dst, uint8_t *src, int8_t *noise, int len, int shift){
-     intarch_t mmx_len= len&(~7);
-     noise+=shift;
-@@ -224,7 +224,7 @@ static inline void lineNoiseAvg_C(uint8_
+@@ -141,7 +141,7 @@ static inline void lineNoise_C(uint8_t *
      }
  }
  
 -#ifdef ARCH_X86
 +#if defined(ARCH_X86) && !defined(__sun)
  
- static inline void lineNoiseAvg_MMX(uint8_t *dst, uint8_t *src, int len, int8_t **shift){
-     intarch_t mmx_len= len&(~7);
-@@ -333,15 +333,20 @@ static const char *const enum_quality[]
+ #if defined(ARCH_X86_64)
+ #  define TYPEA int64_t
+@@ -340,15 +340,20 @@ static const char *const enum_quality[]
   * description of params struct
   */
  START_PARAM_DESCR( noise_parameters_t )
@@ -33,24 +24,24 @@ Disable MMX sections on SunOS.
 +    POST_PARAM_TYPE_INT, luma_strength, NULL, 0, 100, 0,
      "Amount of noise to add to luma channel" )
 -PARAM_ITEM( POST_PARAM_TYPE_INT, chroma_strength, NULL, 0, 100, 0,
-+PARAM_ITEM( noise_parameters_t, 
++PARAM_ITEM( noise_parameters_t,
 +    POST_PARAM_TYPE_INT, chroma_strength, NULL, 0, 100, 0,
      "Amount of noise to add to chroma channel" )
--PARAM_ITEM( POST_PARAM_TYPE_INT, quality, enum_quality, 0, 0, 0,
-+PARAM_ITEM( noise_parameters_t, 
-+    POST_PARAM_TYPE_INT, quality, enum_quality, 0, 0, 0,
+-PARAM_ITEM( POST_PARAM_TYPE_INT, quality, (char **)enum_quality, 0, 0, 0,
++PARAM_ITEM( noise_parameters_t,
++    POST_PARAM_TYPE_INT, quality, (char **)enum_quality, 0, 0, 0,
      "Quality level of noise" )
--PARAM_ITEM( POST_PARAM_TYPE_INT, type, enum_types, 0, 0, 0,
-+PARAM_ITEM( noise_parameters_t, 
-+    POST_PARAM_TYPE_INT, type, enum_types, 0, 0, 0,
+-PARAM_ITEM( POST_PARAM_TYPE_INT, type, (char **)enum_types, 0, 0, 0,
++PARAM_ITEM( noise_parameters_t,
++    POST_PARAM_TYPE_INT, type, (char **)enum_types, 0, 0, 0,
      "Type of noise" )
 -PARAM_ITEM( POST_PARAM_TYPE_BOOL, pattern, NULL, 0, 1, 0,
-+PARAM_ITEM( noise_parameters_t, 
++PARAM_ITEM( noise_parameters_t,
 +    POST_PARAM_TYPE_BOOL, pattern, NULL, 0, 1, 0,
      "Mix random noise with a (semi)regular pattern" )
  END_PARAM_DESCR( param_descr )
  
-@@ -460,7 +465,7 @@ void *noise_init_plugin(xine_t *xine, vo
+@@ -467,7 +472,7 @@ void *noise_init_plugin(xine_t *xine, vo
      class->description     = N_("Adds noise");
      class->dispose         = default_post_class_dispose;
  
@@ -59,12 +50,12 @@ Disable MMX sections on SunOS.
      if (xine_mm_accel() & MM_ACCEL_X86_MMX) {
          lineNoise = lineNoise_MMX;
          lineNoiseAvg = lineNoiseAvg_MMX;
-@@ -579,7 +584,7 @@ static int noise_draw(vo_frame_t *frame,
+@@ -587,7 +592,7 @@ static int noise_draw(vo_frame_t *frame,
                frame->width * 2, frame->height, &this->params[0]);
      }
  
 -#ifdef ARCH_X86
 +#if defined(ARCH_X86) && !defined(__sun)
      if (xine_mm_accel() & MM_ACCEL_X86_MMX)
-         asm volatile ("emms\n\t");
+         __asm__ __volatile__ ("emms\n\t");
      if (xine_mm_accel() & MM_ACCEL_X86_MMXEXT)
