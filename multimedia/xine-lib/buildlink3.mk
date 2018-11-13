4086a60b5e4d46b166933b0b8041d8d3b9b0ee5a
markd <markd@pkgsrc.org>
2018-11-13 11:10:41 +0000

    xine-lib: update to 1.2.9
    2018-01-11: Release
    xine-lib 1.2.9
     Changes include new video single step feature, keyframe index and x32
     support. Optimizations in xine engine core functionality, resulting in
     ex. faster seeking and smaller CPU usage. Small fixes and optimizations
     all around the codebase.
    
    2017-02-21: Release
    xine-lib 1.2.8
     Lot of changes since 1.2.6. Support for modern codecs, hardware and
     software. Small fixes and optimizations all around the codebase.
    
     OpenGL 2.0, Xv and VAAPI fixes. H.265 hardware decoding support (VAAPI).
     Hardware overlay support with Raspbery Pi.
    
     Support for recent ffmpeg versions. Deep color support (9/10bit) with
     ffmpeg. Multithreaded decoding fixes.
    
     Audio channel mixing and gain control improvements (ffmpeg, faad), AAC
     LATM support with faad, DVB AAC compatibility fixes. "Time Domain Audio
     Analyzer" Visualization Post Plugin.
    
     Auto recover from temporary DVB signal loss. Improved Matroska
     compatibility (TrueHD and PCM sound, HDMV/Text subtitles, compressed
     PGS subtitles). Qt fixes, support for low framerate mp4 and H.265 in mp4.
     mp3 compatibility fixes.

--- multimedia/xine-lib/buildlink3.mk
+++ multimedia/xine-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.37 2018/03/12 11:15:41 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.38 2018/11/13 11:10:41 markd Exp $
 
 BUILDLINK_TREE+=	xine-lib
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.xine-lib+=	xine-lib>=1rc3c
 BUILDLINK_ABI_DEPENDS.xine-lib+=	xine-lib>=1.2.6nb11
 BUILDLINK_PKGSRCDIR.xine-lib?=		../../multimedia/xine-lib
 
-.include "../../multimedia/ffmpeg2/buildlink3.mk"
+.include "../../multimedia/ffmpeg3/buildlink3.mk"
 .endif # XINE_LIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xine-lib
