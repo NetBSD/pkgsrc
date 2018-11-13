3efb5b7f5b8c8b91be3f19e4b461a38ff00335a1
markd <markd@pkgsrc.org>
2018-11-13 11:10:41 +0000

    xine-lib: update to 1.2.9
    2018-01-11: Release
    xine-lib 1.2.9
     Changes include new video single step feature, keyframe index and x32
     support. Optimizations in xine engine core functionality, resulting in
     ex. faster seeking and smaller CPU usage. Small fixes and optimizations
     all around the codebase.
    
    2017-02-21: Release
    xine-lib 1.2.8
     Lot of changes since 1.2.6. Support for modern codecs, hardware and
     software. Small fixes and optimizations all around the codebase.
    
     OpenGL 2.0, Xv and VAAPI fixes. H.265 hardware decoding support (VAAPI).
     Hardware overlay support with Raspbery Pi.
    
     Support for recent ffmpeg versions. Deep color support (9/10bit) with
     ffmpeg. Multithreaded decoding fixes.
    
     Audio channel mixing and gain control improvements (ffmpeg, faad), AAC
     LATM support with faad, DVB AAC compatibility fixes. "Time Domain Audio
     Analyzer" Visualization Post Plugin.
    
     Auto recover from temporary DVB signal loss. Improved Matroska
     compatibility (TrueHD and PCM sound, HDMV/Text subtitles, compressed
     PGS subtitles). Qt fixes, support for low framerate mp4 and H.265 in mp4.
     mp3 compatibility fixes.

--- multimedia/xine-lib/patches/patch-contrib_libxdg-basedir_basedir.c
+++ multimedia/xine-lib/patches/patch-contrib_libxdg-basedir_basedir.c
@@ -1,31 +1,15 @@
-$NetBSD: patch-contrib_libxdg-basedir_basedir.c,v 1.1 2016/06/19 23:05:17 joerg Exp $
+$NetBSD: patch-contrib_libxdg-basedir_basedir.c,v 1.2 2018/11/13 11:10:41 markd Exp $
 
 Fix empty body problems.
 
---- contrib/libxdg-basedir/basedir.c.orig	2016-06-19 19:28:26.879811053 +0000
+--- contrib/libxdg-basedir/basedir.c.orig	2018-01-11 12:49:47.000000000 +0000
 +++ contrib/libxdg-basedir/basedir.c
-@@ -143,14 +143,14 @@ static void xdgFreeStringList(char** lis
- /** Free all data in the cache and set pointers to null. */
- static void xdgFreeData(xdgCachedData *cache)
- {
--	if (cache->dataHome);
-+	if (cache->dataHome)
- 	{
- 		/* the first element of the directory lists is usually the home directory */
- 		if (cache->searchableDataDirectories[0] != cache->dataHome)
- 			free(cache->dataHome);
- 		cache->dataHome = 0;
- 	}
--	if (cache->configHome);
-+	if (cache->configHome)
- 	{
- 		if (cache->searchableConfigDirectories[0] != cache->configHome)
- 			free(cache->configHome);
-@@ -280,9 +280,9 @@ static char** xdgGetPathListEnv(const ch
+@@ -282,10 +282,9 @@ static char** xdgGetPathListEnv(const ch
  	else
  	{
  		if (!strings) return NULL;
--		for (size = 0; strings[size]; ++size) ; ++size;
+-		for (size = 0; strings[size]; ++size) ;
+-                ++size;
 -		if (!(itemlist = (char**)malloc(sizeof(char*)*size))) return NULL;
 -		xdgZeroMemory(itemlist, sizeof(char*)*(size));
 +		for (size = 0; strings[size++];)
