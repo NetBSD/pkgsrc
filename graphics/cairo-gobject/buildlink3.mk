be500265f7ac72d01ba3ac3cbd066a5a54b362ec
kleink <kleink@pkgsrc.org>
2018-11-14 19:01:48 +0000

    cairo-gobject: Update BUILDLINK_ABI_DEPENDS for 1.16.0.
    Revbump for cairo update about to proceed.

--- graphics/cairo-gobject/buildlink3.mk
+++ graphics/cairo-gobject/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2018/03/12 11:15:33 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2018/11/14 19:01:48 kleink Exp $
 
 BUILDLINK_TREE+=	cairo-gobject
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	cairo-gobject
 CAIRO_GOBJECT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cairo-gobject+=	cairo-gobject>=1.10.2
-BUILDLINK_ABI_DEPENDS.cairo-gobject+=	cairo-gobject>=1.14.12nb1
+BUILDLINK_ABI_DEPENDS.cairo-gobject+=	cairo-gobject>=1.16.0
 BUILDLINK_PKGSRCDIR.cairo-gobject?=	../../graphics/cairo-gobject
 
 .include "../../devel/glib2/buildlink3.mk"
