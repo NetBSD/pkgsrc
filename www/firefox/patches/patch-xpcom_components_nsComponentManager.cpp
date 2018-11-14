d8974bbcaa12fe3fc505ee36a081c159468f0717
wiz <wiz@pkgsrc.org>
2018-11-14 18:51:40 +0000

    firefox: fix build on NetBSD
    Tip from jperkin, thanks!

--- www/firefox/patches/patch-xpcom_components_nsComponentManager.cpp
+++ www/firefox/patches/patch-xpcom_components_nsComponentManager.cpp
@@ -1,17 +1,20 @@
-$NetBSD: patch-xpcom_components_nsComponentManager.cpp,v 1.1 2018/11/12 12:50:52 jperkin Exp $
+$NetBSD: patch-xpcom_components_nsComponentManager.cpp,v 1.2 2018/11/14 18:51:40 wiz Exp $
 
 Ensure symbols aren't pruned from being unused.
 
 --- xpcom/components/nsComponentManager.cpp.orig	2018-10-31 00:08:15.000000000 +0000
 +++ xpcom/components/nsComponentManager.cpp
-@@ -298,8 +298,8 @@ extern mozilla::Module const* const __st
+@@ -298,8 +298,13 @@ extern mozilla::Module const* const __st
  
  #  if defined(__ELF__) || (defined(_WIN32) && defined(__GNUC__))
  
--extern "C" mozilla::Module const* const __start_kPStaticModules;
--extern "C" mozilla::Module const* const __stop_kPStaticModules;
++#ifdef __sun
 +extern "C" mozilla::Module const* const __start_kPStaticModules = nullptr;
 +extern "C" mozilla::Module const* const __stop_kPStaticModules = nullptr;
++#else
+ extern "C" mozilla::Module const* const __start_kPStaticModules;
+ extern "C" mozilla::Module const* const __stop_kPStaticModules;
++#endif
  
  #  elif defined(__MACH__)
  
