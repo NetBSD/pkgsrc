946955efcdbf5a9bf3df39a5a249d7191b27ad39
markd <markd@pkgsrc.org>
2018-11-15 01:13:01 +0000

    {cdrdao,gcdmaster}: update to 1.2.4
    This is a maintenance release containing only minor bug, warning and
    compilation fixes.
    
    * Fixed autocon warnings (Denis Leroy)
    * Ported to lame 3.100 (from Fedora)
    * G++ compile fixes (Nick Bailey)
    * Renamed xdao folder to gcdmaster (Denis Leroy)
    * pccts format security patch (from Fedora)
    * Fixed compile issues with glibc >= 2.12
    * Gcdmaster segfault fix (Adrian Knoth)
    * Prevent a FTBFS on kfreebsd (Christoph Egger)
    * Also look in /etc/default/cdrdao config (Andrew Suffield)
    * Fix printf format security issues (Frantisek Kluknavsky Fedora)
    * Add missing options to man page (Honza Horák)
    * CD_TEXT fix for HL-DT-ST (Kees Cook)
    * Man page hyphen fixes (Markus Koschany)
    * Some updates to the old scsilib smake files (Ubuntu patches)

--- sysutils/cdrdao/patches/patch-xdao_SampleDisplay.cc
+++ sysutils/cdrdao/patches/patch-xdao_SampleDisplay.cc
@@ -1,25 +1,7 @@
-$NetBSD: patch-xdao_SampleDisplay.cc,v 1.1 2017/02/14 21:22:43 joerg Exp $
+$NetBSD: patch-xdao_SampleDisplay.cc,v 1.2 2018/11/15 01:13:02 markd Exp $
 
---- xdao/SampleDisplay.cc.orig	2017-02-09 17:06:36.365008002 +0000
-+++ xdao/SampleDisplay.cc
-@@ -768,7 +768,7 @@ bool SampleDisplay::handleLeaveEvent(Gdk
- void SampleDisplay::redraw(gint x, gint y, gint width, gint height,
- 			   int drawMask)
- {
--  if (pixmap_ == 0)
-+  if (!pixmap_)
-     return;
- 
-   get_window()->draw_drawable(drawGc_, pixmap_, x, y, x, y, width, height);
-@@ -820,7 +820,7 @@ void SampleDisplay::updateSamples()
- 
-   Toc *toc = tocEdit_->toc();
- 
--  if (pixmap_ == 0)
-+  if (!pixmap_)
-     return;
- 
-   gint halfHeight = chanHeight_ / 2;
+--- gcdmaster/SampleDisplay.cc.orig	2017-02-09 17:06:36.365008002 +0000
++++ gcdmaster/SampleDisplay.cc
 @@ -1025,9 +1025,6 @@ void SampleDisplay::updateSamples()
  
  	}
@@ -30,21 +12,3 @@ $NetBSD: patch-xdao_SampleDisplay.cc,v 1.1 2017/02/14 21:22:43 joerg Exp $
  	if (0 && (gint)di < sampleEndX_) {
  	  pos = sampleBuf[len -1].left() * halfHeight;
  	  pos /= SHRT_MAX;
-@@ -1084,7 +1081,7 @@ void SampleDisplay::updateSamples()
- 
- void SampleDisplay::drawCursor(gint x)
- {
--  if (pixmap_ == 0)
-+  if (!pixmap_)
-     return;
- 
-   if (x < sampleStartX_ || x > sampleEndX_)
-@@ -1118,7 +1115,7 @@ void SampleDisplay::drawTimeTick(gint x,
- {
-   char buf[50];
- 
--  if (pixmap_ == 0)
-+  if (!pixmap_)
-     return;
- 
-   unsigned long min = sample / (60 * 44100);
