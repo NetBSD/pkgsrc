d257ac6c0de429ab047cc330c0d9b89feef71845
schmonz <schmonz@pkgsrc.org>
2018-11-15 13:46:07 +0000

    Remove 'inet6' option (fefe's patch). For IPv6 support, see net/ucspi-tcp6. Bump PKGREVISION.

--- net/ucspi-tcp/Makefile
+++ net/ucspi-tcp/Makefile
@@ -1,7 +1,7 @@
-# $NetBSD: Makefile,v 1.43 2018/07/25 16:19:16 schmonz Exp $
+# $NetBSD: Makefile,v 1.44 2018/11/15 13:46:07 schmonz Exp $
 
 DISTNAME=		ucspi-tcp-0.88
-PKGREVISION=		5
+PKGREVISION=		6
 CATEGORIES=		sysutils net
 MASTER_SITES=		http://cr.yp.to/ucspi-tcp/ ftp://cr.yp.to/ucspi-tcp/
 DISTFILES=		${DISTNAME}.tar.gz ${MANPAGES}
@@ -10,6 +10,10 @@ MAINTAINER=		schmonz@NetBSD.org
 HOMEPAGE=		http://cr.yp.to/ucspi-tcp.html
 COMMENT=		Command-line tools for building TCP client-server applications
 LICENSE=		public-domain
+
+DESTDIR_PATCH=		ucspi-tcp-0.88-destdir-20180725.patch
+PATCHFILES+=		${DESTDIR_PATCH}
+SITES.${DESTDIR_PATCH}=	https://schmonz.com/qmail/ucspitcpdestdir/
 
 MANPAGES=		${DISTNAME}-man-20020317.tar.gz
 SITES.${MANPAGES}=	http://smarden.org/pape/djb/manpages/
--- net/ucspi-tcp/options.mk
+++ net/ucspi-tcp/options.mk
@@ -1,29 +1,10 @@
-# $NetBSD: options.mk,v 1.7 2018/07/25 16:19:16 schmonz Exp $
+# $NetBSD: options.mk,v 1.8 2018/11/15 13:46:07 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ucspi-tcp
-PKG_SUPPORTED_OPTIONS+=	inet6 ucspi-tcp-nodefaultrbl
-PKG_SUGGESTED_OPTIONS+=	inet6 ucspi-tcp-nodefaultrbl
+PKG_SUPPORTED_OPTIONS+=	ucspi-tcp-nodefaultrbl
+PKG_SUGGESTED_OPTIONS+=	ucspi-tcp-nodefaultrbl
 
 .include "../../mk/bsd.options.mk"
-
-DESTDIR_PATCH=			ucspi-tcp-0.88-destdir-20180725.patch
-PATCHFILES+=			${DESTDIR_PATCH}
-SITES.${DESTDIR_PATCH}=		https://schmonz.com/qmail/ucspitcpdestdir/
-
-PLIST_VARS+=			inet6
-.if !empty(PKG_OPTIONS:Minet6)
-IPV6_PATCH=			ucspi-tcp-0.88-ipv6.diff20.bz2
-PATCHFILES+=			${IPV6_PATCH}
-SITES.${IPV6_PATCH}=		http://www.fefe.de/ucspi/
-PATCH_DIST_STRIP.${IPV6_PATCH}=	-p1
-PLIST.inet6=			yes
-
-SUBST_CLASSES+=		hier
-SUBST_STAGE.hier=	pre-configure
-SUBST_MESSAGE.hier=	Fixing manpage path.
-SUBST_FILES.hier=	hier.c
-SUBST_SED.hier=		-e 's,man,${PKGMANDIR},'
-.endif
 
 .if !empty(PKG_OPTIONS:Mucspi-tcp-nodefaultrbl)
 NODEFAULTRBL_PATCH=		rblsmtpd-nodefaultrbl.patch
