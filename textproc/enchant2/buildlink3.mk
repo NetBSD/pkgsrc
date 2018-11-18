f505b31a509ec83dffd7ac4ee1f05c0c4c60ec8e
bsiegert <bsiegert@pkgsrc.org>
2018-11-18 13:08:19 +0000

    Make enchant2 bl3 create enchant.pc.
    I have enough of these manual hacks in configure scripts that check for
    'enchant => someversion' instead of 'enchant-2 >= someversion'. Instead,
    let's give them the former.
    
    Caveat: this makes it impossible to have enchant and enchant-2 buildlinked
    into the same build, but you do not want that.

--- textproc/enchant2/buildlink3.mk
+++ textproc/enchant2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/11/01 09:36:47 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/11/18 13:08:19 bsiegert Exp $
 
 BUILDLINK_TREE+=	enchant2
 
@@ -8,6 +8,13 @@ ENCHANT2_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.enchant2+=	enchant2>=2
 BUILDLINK_ABI_DEPENDS.enchant2+=	enchant2>=2.2.3
 BUILDLINK_PKGSRCDIR.enchant2?=	../../textproc/enchant2
+
+# Lots of older software looks for enchant.pc instead of enchant-2.pc.
+${BUILDLINK_DIR}/lib/pkgconfig/enchant.pc:
+	${MKDIR} ${BUILDLINK_DIR}/lib/pkgconfig
+	cd ${BUILDLINK_DIR}/lib/pkgconfig && ${LN} -sf enchant-2.pc enchant.pc
+
+buildlink-enchant2-cookie: ${BUILDLINK_DIR}/lib/pkgconfig/enchant.pc
 
 # spelling libraries are loaded dynamically and do not need
 # to be included here
