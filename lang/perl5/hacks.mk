109740973c59fa213a4b7dbac6526ebd78884859
maya <maya@pkgsrc.org>
2018-11-20 09:07:06 +0000

    perl5: remove workarounds for very old versions of netbsd+GCC

--- lang/perl5/hacks.mk
+++ lang/perl5/hacks.mk
@@ -1,41 +1,9 @@
-# $NetBSD: hacks.mk,v 1.19 2016/10/28 15:55:50 dholland Exp $
+# $NetBSD: hacks.mk,v 1.20 2018/11/20 09:07:06 maya Exp $
 
 .if !defined(PERL5_HACKS_MK)
 PERL5_HACKS_MK=	defined
 
 .include "../../mk/compiler.mk"
-
-### [Thu Jun 7 04:25:34 UTC 2001 : jlam]
-### Fix brokenness when using an older toolchain (gcc<3.3) on
-### NetBSD/sparc64.  Pass -g and -DDEBUGGING to the compiler to
-### circumvent some code-generation bugs.
-###
-.if !empty(MACHINE_PLATFORM:MNetBSD-*-sparc64)
-.  if !empty(CC_VERSION:Mgcc*)
-.    if !defined(_GCC_IS_TOO_OLD)
-_GCC_IS_TOO_OLD!=	\
-	if ${PKG_ADMIN} pmatch 'gcc<3.3' ${CC_VERSION}; then		\
-		${ECHO} "yes";						\
-	else								\
-		${ECHO} "no";						\
-	fi
-MAKEFLAGS+=	_GCC_IS_TOO_OLD=${_GCC_IS_TOO_OLD:Q}
-.    endif
-.    if !empty(_GCC_IS_TOO_OLD:M[yY][eE][sS])
-PKG_HACKS+=	sparc64-codegen
-CFLAGS+=	-DDEBUGGING -g -msoft-quad-float -O2
-.    endif
-.  endif
-.endif
-
-### [Mon May 9 15:35:44 UTC 2005 : jlam]
-### On NetBSD/arm, skipping one part of the optimization pass empirically
-### "fixes" the build of perl using gcc-3.x.
-###
-.if !empty(CC_VERSION:Mgcc-3.*) && !empty(MACHINE_ARCH:Marm*)
-PKG_HACKS+=	arm-codegen
-CFLAGS+=	-fno-cse-skip-blocks
-.endif
 
 ### [ Fri Oct 11 10:00:00 UTC 2011 : hauke ]
 ###
