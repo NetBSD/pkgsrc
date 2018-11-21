2a4085a6566f0bd61b3d565b6783574d6872780c
adam <adam@pkgsrc.org>
2018-11-21 16:58:52 +0000

    cmake: updated to 3.13.0
    CMake 3.13.0:
    The Visual Studio Generators for VS 2010 and above learned to
    support the “INTERPROCEDURAL_OPTIMIZATION” target property and
    supporting “CheckIPOSupported” module.
    The “Green Hills MULTI” generator has been updated to include
    support for platform, architecture, and toolset selection.
    The “cmake” command gained the “-S <source_dir>” command line
    option to specify the location of the source directory. This option
    can be used independently of “-B”.
    The “cmake” command gained the “-B <build_dir>” command line
    option to specify the location of the build directory. This option
    can be used independently of “-S”.
    The “cmake” “-E create_symlink” command can now be used on
    Windows.
    The “target_link_directories()” command was created to specify
    link directories for targets and their dependents.
    The “target_link_options()” command was created to specify link
    options for targets and their dependents.
    The “target_link_libraries()” command may now be called to modify
    targets created outside the current directory. See policy “CMP0079”.
    The “install(TARGETS)” command learned to install targets created
    outside the current directory.
    A “VS_DEBUGGER_COMMAND_ARGUMENTS” target property was created to
    set the debugging command line arguments with Visual Studio
    Generators for VS 2010 and above.
    A “VS_DEBUGGER_ENVIRONMENT” target property was created to set the
    debugging environment with Visual Studio Generators for VS 2010 and
    above.
    The “option()” command now honors an existing normal variable of
    the same name and does nothing instead of possibly creating a cache
    entry (or setting its type) and removing the normal variable. See
    policy “CMP0077”.
    The “target_sources()” command now interprets relative source file
    paths as relative to the current source directory. This simplifies
    incrementally building up a target’s sources from subdirectories.
    The “CMP0076” policy was added to provide backward compatibility
    with the old behavior where required.

--- devel/cmake/patches/patch-Source_Checks_cm__cxx17__check.cpp
+++ devel/cmake/patches/patch-Source_Checks_cm__cxx17__check.cpp
@@ -1,12 +1,13 @@
-$NetBSD: patch-Source_Checks_cm__cxx17__check.cpp,v 1.1 2018/05/11 13:39:57 jperkin Exp $
+$NetBSD: patch-Source_Checks_cm__cxx17__check.cpp,v 1.2 2018/11/21 16:58:52 adam Exp $
 
 Test for an issue in the illumos headers related to C++17.
 
---- Source/Checks/cm_cxx17_check.cpp.orig	2018-04-17 12:28:52.000000000 +0000
+--- Source/Checks/cm_cxx17_check.cpp.orig	2018-11-20 14:49:09.000000000 +0000
 +++ Source/Checks/cm_cxx17_check.cpp
-@@ -1,5 +1,7 @@
-+#include <cassert>
+@@ -1,6 +1,8 @@
  #include <cstdio>
++#include <cassert>
+ #include <memory>
  #include <unordered_map>
 +#include <type_traits>
  
