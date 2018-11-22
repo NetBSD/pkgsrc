ff189a8cae9f4dbeb52ad1bb6a5c727992af6472
jperkin <jperkin@pkgsrc.org>
2018-11-22 17:23:08 +0000

    ruby-{cairo,gnome}: Unbreak bad recursive revision bumps.
    Some part of the revbump script obviously did the wrong thing here and
    replaced ${RUBY_PKGPREFIX} with whatever it expands to on the developer's
    local machine.

--- graphics/ruby-cairo/buildlink3.mk
+++ graphics/ruby-cairo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/11/14 22:21:45 kleink Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/11/22 17:23:08 jperkin Exp $
 
 BUILDLINK_TREE+=	ruby-cairo
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ruby-cairo
 RUBY_CAIRO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-cairo+=	${RUBY_PKGPREFIX}-cairo>=1.6.0
-BUILDLINK_ABI_DEPENDS.ruby-cairo+=	ruby24-cairo>=1.16.2nb1
+BUILDLINK_ABI_DEPENDS.ruby-cairo+=	${RUBY_PKGPREFIX}-cairo>=1.16.2nb1
 BUILDLINK_PKGSRCDIR.ruby-cairo?=	../../graphics/ruby-cairo
 
 .include "../../graphics/cairo/buildlink3.mk"
