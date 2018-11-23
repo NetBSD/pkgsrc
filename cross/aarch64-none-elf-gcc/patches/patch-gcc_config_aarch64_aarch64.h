b845f06bbeb80d983a33bb895652a287f62f242b
ryoon <ryoon@pkgsrc.org>
2018-11-23 05:30:45 +0000

    Update to 8.2.0
    Build and work sysutils/u-boot-pine64 fine.

--- cross/aarch64-none-elf-gcc/patches/patch-gcc_config_aarch64_aarch64.h
+++ cross/aarch64-none-elf-gcc/patches/patch-gcc_config_aarch64_aarch64.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-gcc_config_aarch64_aarch64.h,v 1.1 2018/11/08 23:28:28 mrg Exp $
+$NetBSD: patch-gcc_config_aarch64_aarch64.h,v 1.2 2018/11/23 05:30:45 ryoon Exp $
 
 driver-aarch64.o is not included for NetBSD and it is responsible for
 the definition of host_detect_local_cpu.
@@ -7,9 +7,9 @@ Without it, we get an undefined reference.
 
 Similar to GCC bugzilla #77800 for arm.
 
---- gcc/config/aarch64/aarch64.h.orig	2017-01-19 15:51:49.000000000 -0800
-+++ gcc/config/aarch64/aarch64.h	2018-11-08 14:31:59.315619048 -0800
-@@ -930,7 +930,8 @@
+--- gcc/config/aarch64/aarch64.h.orig	2018-02-21 14:05:45.000000000 +0000
++++ gcc/config/aarch64/aarch64.h
+@@ -1000,7 +1000,8 @@ extern const char *aarch64_rewrite_mcpu 
  #define MCPU_TO_MARCH_SPEC_FUNCTIONS \
    { "rewrite_mcpu", aarch64_rewrite_mcpu },
  
@@ -17,5 +17,5 @@ Similar to GCC bugzilla #77800 for arm.
 +/* NetBSD does not export necessary info via /proc/cpuinfo. */
 +#if defined(__aarch64__) && !defined(__NetBSD__)
  extern const char *host_detect_local_cpu (int argc, const char **argv);
+ #define HAVE_LOCAL_CPU_DETECT
  # define EXTRA_SPEC_FUNCTIONS						\
-   { "local_cpu_detect", host_detect_local_cpu },			\
