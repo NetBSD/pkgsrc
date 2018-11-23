f1b968dd47968f03bd100980b8d44b57ec16abfd
ryoon <ryoon@pkgsrc.org>
2018-11-23 08:06:28 +0000

    Recursive revbump from multimedia/libva

--- games/simgear/buildlink3.mk
+++ games/simgear/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2018/11/14 22:21:32 kleink Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2018/11/23 08:06:28 ryoon Exp $
 
 BUILDLINK_TREE+=	simgear
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	simgear
 SIMGEAR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.simgear+=	simgear>=2018.2.2
-BUILDLINK_ABI_DEPENDS.simgear?=	simgear>=2018.2.2nb5
+BUILDLINK_ABI_DEPENDS.simgear?=	simgear>=2018.2.2nb6
 BUILDLINK_PKGSRCDIR.simgear?=	../../games/simgear
 
 .include "../../audio/openal-soft/buildlink3.mk"
