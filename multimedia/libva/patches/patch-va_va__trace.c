1981f85bc3056e1aef5c7888bbdc6f6405d1cc1f
ryoon <ryoon@pkgsrc.org>
2018-11-23 08:05:04 +0000

    Update to 2.3.0
    Changelog:
    version 2.3.0 - 30.Sep.2018
    * Bump VA-API version to 1.3.0 and libva to 2.3.0
    * Add max frame size parameters for multiple pass case in legacy mode
    * Add new BRC mode AVBR
    * Add new interface for High Dynamic Range tone mapping
    * Add missing enum to string conversions
    * Add hevc subsets parameters structure
    * Add Customized Noise Reduction (HVS) interfaces
    * Add new BRC mode definition QVBR
    * Add more complete colour properties for use in VPP
    
    Version 2.2.0 - DD.July.2018
    * Bump VA-API version to 1.2.0 and libva to 2.2.0
    * Add support for hevc range extension decoding
    * Add support for fast intra prediction in HEVC FEI
    * Add 10/12-bit YUV render target formats
    * Add fourcc code for Y210/Y216/Y410/Y416/RGB565/BGR565
    * Add VA_STATUS_ERROR_NOT_ENOUGH_BUFFER
    * Add VA_SURFACE_ATTRIB_USAGE_HINT_EXPORT
    * Improve documentation
    
    Version 2.1.0 - 12.Feb.2018
    * Bump VA-API version to 1.1.0 and libva to 2.1.0
    * Add API for multi-frame processing
    * Add entrypoint VAEntrypointStats for Statistics
    * Add data structures for HEVC FEI support
    * Add new attributes for decoding/encoding/video processing
    * Add new VPP filter for Total Color Correction
    * Add blending interface in VPP
    * Add rotation interface in VPP
    * Add mirroring interface in VPP
    * Add Chroma siting flags in VPP
    * Add new color standard definitions
    * Add new interface for exporting surface
    * Add message callbacks for drivers to use
    
    Version 2.0.0 - 21.Oct.2017
    * Bump VA-API version to 1.0.0 and libva to 2.0.0
    * Add new API for H264 FEI support
    * Add definition of VA_FOURCC_I420
    * Add functions for converting common enums to strings
    * Deprecate H.264 baseline profile and FMO support
    * Deprecate packed misc packed header flag
    * Delete libva-tpi and libva-egl backends
    * Refine VASliceParameterBufferHEVC, VAEncMiscParameterBuffer
    * Fix errors in VAConfigAttribValEncROI, VAEncMacroblockParameterBufferH264
    * Fix race condition in wayland support
    * Rename vaMessageCallback to VAMessageCallback
    * Make logging callbacks library-safe

--- multimedia/libva/patches/patch-va_va__trace.c
+++ multimedia/libva/patches/patch-va_va__trace.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-va_va__trace.c,v 1.2 2017/02/24 16:44:03 maya Exp $
+$NetBSD: patch-va_va__trace.c,v 1.3 2018/11/23 08:05:04 ryoon Exp $
 
 attempt to provide syscall(__NR_gettid) for most
 systems. inspired by lldb Host.cpp
 
---- va/va_trace.c.orig	2016-11-10 00:34:35.000000000 +0000
+--- va/va_trace.c.orig	2018-10-23 13:50:59.000000000 +0000
 +++ va/va_trace.c
-@@ -48,6 +48,12 @@
+@@ -50,10 +50,31 @@
  #include <sys/stat.h>
  #include <sys/syscall.h>
  #include <pthread.h>
@@ -18,9 +18,6 @@ systems. inspired by lldb Host.cpp
  #include <unistd.h>
  #include <time.h>
  #include <errno.h>
-@@ -235,6 +241,21 @@ struct va_trace {
- 
- /* Prototype declarations (functions defined in va.c) */
  
 +pid_t get_tid() {
 +#if defined(__linux__)
@@ -37,10 +34,10 @@ systems. inspired by lldb Host.cpp
 +#endif
 +}
 +
- void va_errorMessage(const char *msg, ...);
- void va_infoMessage(const char *msg, ...);
- 
-@@ -294,7 +315,7 @@ static void add_trace_config_info(
+ /*
+  * Env. to debug some issue, e.g. the decode/encode issue in a video conference scenerio:
+  * .LIBVA_TRACE=log_file: general VA parameters saved into log_file
+@@ -290,7 +311,7 @@ static void add_trace_config_info(
  {
      struct trace_config_info *pconfig_info;
      int idx = 0;
@@ -49,16 +46,7 @@ systems. inspired by lldb Host.cpp
  
      LOCK_RESOURCE(pva_trace);
  
-@@ -318,7 +339,7 @@ static void delete_trace_config_info(
- {
-     struct trace_config_info *pconfig_info;
-     int idx = 0;
--    pid_t thd_id = syscall(__NR_gettid);
-+    pid_t thd_id = get_tid();
- 
-     LOCK_RESOURCE(pva_trace);
- 
-@@ -665,7 +686,7 @@ static struct trace_log_file *start_trac
+@@ -668,7 +689,7 @@ static struct trace_log_file *start_trac
  {
      struct trace_log_files_manager *plog_files_mgr = NULL;
      struct trace_log_file *plog_file = NULL;
@@ -67,7 +55,7 @@ systems. inspired by lldb Host.cpp
      int i = 0;
  
      LOCK_RESOURCE(pva_trace);
-@@ -704,7 +725,7 @@ static void refresh_log_file(
+@@ -707,7 +728,7 @@ static void refresh_log_file(
      struct trace_context *ptra_ctx)
  {
      struct trace_log_file *plog_file = NULL;
@@ -76,7 +64,7 @@ systems. inspired by lldb Host.cpp
      int i = 0;
  
      plog_file = ptra_ctx->plog_file;
-@@ -1226,7 +1247,7 @@ static void internal_TraceUpdateContext 
+@@ -1230,7 +1251,7 @@ static void internal_TraceUpdateContext 
  {
      struct trace_context *trace_ctx = NULL;
      int i = 0, delete = 1;
