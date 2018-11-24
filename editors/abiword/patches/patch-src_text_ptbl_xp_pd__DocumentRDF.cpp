34d2c5245e3696f196b326135a892e4788ccdafc
nia <nia@pkgsrc.org>
2018-11-24 19:14:09 +0000

    editors/abiword: Update to 3.0.2
    Changes:
    
    Fixes in the core (all platforms)
    
        Various build fixes: Bug 13697, bug 13650, bug 13726, Bug 13753, Bug 13569
        Various crash fixes: Bug 13716, Bug 13752, Bug 13807, Bug 13766
        Various memory leaks fixes.
        Bug 13709 - Fix import of very long paragraphs.
    
    Gtk
    
    Fixes specific to the Gtk+ version
    
        Bug 13533 - Fix printing of SVG images.
        Bug 13722 - Fix scrolling in the Insert Symbol dialog.
        Bug 13729 - Make the available languages list expandable.
        Bug 13731 - Fix text rendering after an SVG image
        Bug 13662 - Never change an image resolution on scaling.
        Bug 13734 - Do not execute radio buttons events when unchecked.
        Bug 13736 - Properly iterate through the plugin arguments
        Fix SVG icon.)
        Fixed bug in the font chooser.
        Bug 13773 - Fix the ruler and background page theming on Gtk3. Ruler knobs no longer do "3D" on Gtk3. Bug 13762
        Bug 13787 - Fix the table widget drawing
        AbiWord works better on Wayland: Bug 13766, Bug 13767
    
    Import/Export
    
    Fixes in the OpenDocument importer / exporter
    
        Bug 13707 - Fixed paragraph export to Open Document.
        Bug 13706 - If needed apply default style for headers.
        OpenDocument: Fix a buffer overrun
    
    Plugins
    
    Fixes in the collaboration plugin
    
        Fix various build issues. Bug 13712, Bug 13754 and Bug 13742

--- editors/abiword/patches/patch-src_text_ptbl_xp_pd__DocumentRDF.cpp
+++ editors/abiword/patches/patch-src_text_ptbl_xp_pd__DocumentRDF.cpp
@@ -1,37 +1,9 @@
-$NetBSD: patch-src_text_ptbl_xp_pd__DocumentRDF.cpp,v 1.2 2018/04/25 05:53:53 markd Exp $
+$NetBSD: patch-src_text_ptbl_xp_pd__DocumentRDF.cpp,v 1.3 2018/11/24 19:14:09 nia Exp $
 
-* fixes build on FreeBSD 10 with clang. From FreeBSD ports mailing list.
-  Applied upstream.
-  https://lists.freebsd.org/pipermail/freebsd-ports/2015-March/098621.html
+fix using modern libical
 
-* fix using modern libical
-
---- src/text/ptbl/xp/pd_DocumentRDF.cpp.orig	2014-02-27 05:11:32.000000000 +0000
+--- src/text/ptbl/xp/pd_DocumentRDF.cpp.orig	2015-04-02 03:09:20.000000000 +0000
 +++ src/text/ptbl/xp/pd_DocumentRDF.cpp
-@@ -570,6 +570,11 @@ bool PD_URI::operator==(const std::strin
-     return m_value == b;
- }
- 
-+bool PD_URI::operator<(const PD_URI& b) const
-+{
-+return m_value < b.m_value;
-+}
-+
- PD_URI
- PD_URI::prefixedToURI( PD_RDFModelHandle model ) const
- {
-@@ -611,11 +616,6 @@ bool PD_URI::write( std::ostream& ss ) c
-     return true;
- }
- 
--bool operator<( PD_URI a, PD_URI b)
--{
--    return a.toString() < b.toString();
--}
--
- bool operator<( std::pair< PD_URI, PD_URI > a, PD_URI b)
- {
-     return a.first.toString() < b.toString();
 @@ -2269,8 +2269,8 @@ PD_RDFEvent::exportToFile( const std::st
          icalcomponent_set_uid( c,         m_uid.c_str() );
          icalcomponent_set_location( c,    m_location.c_str() );
