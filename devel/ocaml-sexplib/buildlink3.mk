c37fe0138e29ee95e272db728a6eb934bda56e24
jaapb <jaapb@pkgsrc.org>
2018-11-27 15:24:09 +0000

    Updated devel/ocaml-sexplib to work with dune

--- devel/ocaml-sexplib/buildlink3.mk
+++ devel/ocaml-sexplib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2018/07/19 15:15:25 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2018/11/27 15:24:09 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-sexplib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-sexplib
 OCAML_SEXPLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=0.11.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=0.11.0nb3
 BUILDLINK_PKGSRCDIR.ocaml-sexplib?=	../../devel/ocaml-sexplib
 
 .endif	# OCAML_SEXPLIB_BUILDLINK3_MK
