05b08ca98ee48b339e62ff97cee36b27b7e3970a
jaapb <jaapb@pkgsrc.org>
2018-11-27 17:09:01 +0000

    Updated devel/ocaml-ppx_tools_versioned to version 5.2.1
    Just a minor change here in versions of dependencies. The package also
    uses dune now.

--- devel/ocaml-ppx_tools_versioned/buildlink3.mk
+++ devel/ocaml-ppx_tools_versioned/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/07/19 13:01:34 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/11/27 17:09:01 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_tools_versioned
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_tools_versioned
 OCAML_PPX_TOOLS_VERSIONED_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_tools_versioned+=	ocaml-ppx_tools_versioned>=5.0.1
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_tools_versioned+=	ocaml-ppx_tools_versioned>=5.2
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_tools_versioned+=	ocaml-ppx_tools_versioned>=5.2.1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_tools_versioned?=		../../devel/ocaml-ppx_tools_versioned
 .endif	# OCAML_PPX_TOOLS_VERSIONED_BUILDLINK3_MK
 
