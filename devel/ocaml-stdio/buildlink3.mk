72c7ebee3a904274198b35c30f144552404300ba
jaapb <jaapb@pkgsrc.org>
2018-11-27 15:25:39 +0000

    Updated devel/ocaml-stdio to work with dune

--- devel/ocaml-stdio/buildlink3.mk
+++ devel/ocaml-stdio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2018/07/19 15:15:25 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2018/11/27 15:25:39 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-stdio
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-stdio
 OCAML_STDIO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-stdio+=	ocaml-stdio>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-stdio+=	ocaml-stdio>=0.11.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-stdio+=	ocaml-stdio>=0.11.0nb3
 BUILDLINK_PKGSRCDIR.ocaml-stdio?=	../../devel/ocaml-stdio
 .endif	# OCAML_STDIO_BUILDLINK3_MK
 
