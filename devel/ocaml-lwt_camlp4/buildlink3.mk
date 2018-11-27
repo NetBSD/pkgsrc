82ccc1e1b4902b73bd58dec2421e5ba009a2e59b
jaapb <jaapb@pkgsrc.org>
2018-11-27 23:04:03 +0000

    Updated package devel/ocaml-lwt_camlp4 to use dune

--- devel/ocaml-lwt_camlp4/buildlink3.mk
+++ devel/ocaml-lwt_camlp4/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/06/05 15:48:36 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/11/27 23:04:03 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt_camlp4
 
 .if !defined(OCAML_LWT_CAMLP4_BUILDLINK3_MK)
 OCAML_LWT_CAMLP4_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-lwt_camlp4+=	ocaml-lwt_camlp4>=20180325
+BUILDLINK_API_DEPENDS.ocaml-lwt_camlp4+=	ocaml-lwt_camlp4>=20180325nb3
 BUILDLINK_PKGSRCDIR.ocaml-lwt_camlp4?=	../../devel/ocaml-lwt_camlp4
 
 .include "../../devel/ocaml-lwt/buildlink3.mk"
