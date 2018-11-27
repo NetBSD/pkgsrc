76735a77f708386bf6d2f5c7e27489a5c26407ef
bsiegert <bsiegert@pkgsrc.org>
2018-11-27 20:32:06 +0000

    Port gtkhtml314 to enchant2, bump revision.

--- www/gtkhtml314/buildlink3.mk
+++ www/gtkhtml314/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2012/09/15 10:06:46 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2018/11/27 20:32:06 bsiegert Exp $
 
 BUILDLINK_TREE+=	gtkhtml314
 
@@ -10,7 +10,7 @@ BUILDLINK_ABI_DEPENDS.gtkhtml314+=	gtkhtml314>=3.32.2nb9
 BUILDLINK_PKGSRCDIR.gtkhtml314?=	../../www/gtkhtml314
 
 .include "../../devel/GConf/buildlink3.mk"
-.include "../../textproc/enchant/buildlink3.mk"
+.include "../../textproc/enchant2/buildlink3.mk"
 .include "../../textproc/iso-codes/buildlink3.mk"
 .endif # GTKHTML314_BUILDLINK3_MK
 
