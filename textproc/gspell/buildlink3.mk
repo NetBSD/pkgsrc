4809ec64dc3f26634159797d3a4401ec4680e4f2
bsiegert <bsiegert@pkgsrc.org>
2018-11-27 14:40:25 +0000

    Port gspell to enchant2, bump revision.

--- textproc/gspell/buildlink3.mk
+++ textproc/gspell/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2018/11/14 22:22:31 kleink Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2018/11/27 14:40:25 bsiegert Exp $
 
 BUILDLINK_TREE+=	gspell
 
@@ -6,11 +6,11 @@ BUILDLINK_TREE+=	gspell
 GSPELL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gspell+=	gspell>=1.0.1
-BUILDLINK_ABI_DEPENDS.gspell?=	gspell>=1.4.2nb8
+BUILDLINK_ABI_DEPENDS.gspell?=	gspell>=1.4.2nb9
 BUILDLINK_PKGSRCDIR.gspell?=	../../textproc/gspell
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../textproc/enchant/buildlink3.mk"
+.include "../../textproc/enchant2/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
 .endif	# GSPELL_BUILDLINK3_MK
 
