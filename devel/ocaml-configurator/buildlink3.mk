57e1dea7ad8f27825e24c1e781a1de537ccfc50d
jaapb <jaapb@pkgsrc.org>
2018-11-27 15:27:18 +0000

    Updated devel/ocaml-configurator to work with dune

--- devel/ocaml-configurator/buildlink3.mk
+++ devel/ocaml-configurator/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/07/19 15:15:22 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/11/27 15:27:18 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-configurator
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-configurator
 OCAML_CONFIGURATOR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-configurator+=	ocaml-configurator>=0.9.1
-BUILDLINK_ABI_DEPENDS.ocaml-configurator+=	ocaml-configurator>=0.11.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-configurator+=	ocaml-configurator>=0.11.0nb3
 BUILDLINK_PKGSRCDIR.ocaml-configurator?=	../../devel/ocaml-configurator
 
 .endif	# OCAML_CONFIGURATOR_BUILDLINK3_MK
