ee90f143b0635598cd681bf83f43d3cc13420847
jaapb <jaapb@pkgsrc.org>
2018-11-27 16:43:05 +0000

    Updated devel/ocaml-topkg to version 1.0.0.
    This version includes several improvements in opam support, and
    bugfixes.

--- misc/ocaml-topkg/buildlink3.mk
+++ misc/ocaml-topkg/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/07/19 15:15:27 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/11/27 16:43:05 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-topkg
 
 .if !defined(OCAML_TOPKG_BUILDLINK3_MK)
 OCAML_TOPKG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-topkg+=	ocaml-topkg>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-topkg+=	ocaml-topkg>=0.9.1nb2
+BUILDLINK_API_DEPENDS.ocaml-topkg+=	ocaml-topkg>=1.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-topkg+=	ocaml-topkg>=1.0.0
 BUILDLINK_PKGSRCDIR.ocaml-topkg?=	../../misc/ocaml-topkg
 
 .endif	# OCAML_TOPKG_BUILDLINK3_MK
