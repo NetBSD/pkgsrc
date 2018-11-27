ac80923515ccb50e464d0e4bda66784918d055c1
jaapb <jaapb@pkgsrc.org>
2018-11-27 15:59:54 +0000

    Updated package devel/ocaml-cppo to version 1.6.5.
    One small bugfix. The package now also uses dune.

--- devel/ocaml-cppo/buildlink3.mk
+++ devel/ocaml-cppo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2018/07/19 15:15:22 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2018/11/27 15:59:54 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cppo
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-cppo
 OCAML_CPPO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.3.1
-BUILDLINK_ABI_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.6.4nb1
+BUILDLINK_ABI_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.6.5
 BUILDLINK_PKGSRCDIR.ocaml-cppo?=	../../devel/ocaml-cppo
 .endif	# OCAML_CPPO_BUILDLINK3_MK
 
