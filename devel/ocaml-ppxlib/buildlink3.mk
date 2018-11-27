285f5f885e7757d488c91d0e1330211f34bccbd3
jaapb <jaapb@pkgsrc.org>
2018-11-27 18:57:00 +0000

    Updated package devel/ocaml-ppxlib to version 0.3.1.
    Mostly minor changes upstream. Package also updated to use dune.

--- devel/ocaml-ppxlib/buildlink3.mk
+++ devel/ocaml-ppxlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/07/19 15:15:25 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/11/27 18:57:00 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppxlib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppxlib
 OCAML_PPXLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.3.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.3.0nb2
+BUILDLINK_ABI_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.3.1
 BUILDLINK_PKGSRCDIR.ocaml-ppxlib?=	../../devel/ocaml-ppxlib
 
 .endif	# OCAML_PPXLIB_BUILDLINK3_MK
