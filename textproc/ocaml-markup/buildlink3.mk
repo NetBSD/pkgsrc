9fd0a0a489e63e9634cc458cd1f202837bbf0382
jaapb <jaapb@pkgsrc.org>
2018-11-27 17:20:09 +0000

    Updated textproc/ocaml-markup to version 0.8.0.
    This package does away with the configure script and spins the lwt
    parts off into a new markup-lwt library (which is now optional).
    
    Some upstream bugfixes as well.

--- textproc/ocaml-markup/buildlink3.mk
+++ textproc/ocaml-markup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/07/19 15:15:29 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/11/27 17:20:09 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-markup
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-markup
 OCAML_MARKUP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-markup+=	ocaml-markup>=0.7.2
-BUILDLINK_ABI_DEPENDS.ocaml-markup+=	ocaml-markup>=0.7.6nb1
+BUILDLINK_ABI_DEPENDS.ocaml-markup+=	ocaml-markup>=0.8.0
 BUILDLINK_PKGSRCDIR.ocaml-markup?=	../../textproc/ocaml-markup
 
 .endif	# OCAML_MARKUP_BUILDLINK3_MK
