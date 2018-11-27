ea03d5042b6a68de78d16e9df159b48f84f096ec
jaapb <jaapb@pkgsrc.org>
2018-11-27 18:50:12 +0000

    Updated package devel/ocaml-lwt_ppx to use dune

--- devel/ocaml-lwt_ppx/buildlink3.mk
+++ devel/ocaml-lwt_ppx/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/07/19 14:15:23 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/11/27 18:50:12 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt_ppx
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-lwt_ppx
 OCAML_LWT_PPX_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-lwt_ppx+=	ocaml-lwt_ppx>=1.1.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt_ppx+=	ocaml-lwt_ppx>=1.2.1
+BUILDLINK_ABI_DEPENDS.ocaml-lwt_ppx+=	ocaml-lwt_ppx>=1.2.1nb2
 BUILDLINK_PKGSRCDIR.ocaml-lwt_ppx?=	../../devel/ocaml-lwt_ppx
 
 .include "../../devel/ocaml-lwt/buildlink3.mk"
