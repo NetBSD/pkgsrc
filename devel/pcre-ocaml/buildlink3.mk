94b1b55f78aad84587396505251396cdd1f7a8c9
jaapb <jaapb@pkgsrc.org>
2018-11-27 19:08:14 +0000

    Updated package devel/pcre-ocaml to version 7.3.5.
    This changes the package build system to dune.

--- devel/pcre-ocaml/buildlink3.mk
+++ devel/pcre-ocaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2018/07/19 15:15:26 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2018/11/27 19:08:14 jaapb Exp $
 
 BUILDLINK_TREE+=	pcre-ocaml
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	pcre-ocaml
 PCRE_OCAML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pcre-ocaml+=	pcre-ocaml>=5.10.3nb1
-BUILDLINK_ABI_DEPENDS.pcre-ocaml+=	pcre-ocaml>=7.3.4nb3
+BUILDLINK_ABI_DEPENDS.pcre-ocaml+=	pcre-ocaml>=7.3.5
 BUILDLINK_PKGSRCDIR.pcre-ocaml?=	../../devel/pcre-ocaml
 
 .include "../../devel/pcre/buildlink3.mk"
