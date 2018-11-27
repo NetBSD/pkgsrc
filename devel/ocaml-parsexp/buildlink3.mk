a82b161b2b363a79e33c5435ef7ba6c72a3df1c2
jaapb <jaapb@pkgsrc.org>
2018-11-27 15:21:18 +0000

    Updated devel/ocaml-parsexp to work with dune

--- devel/ocaml-parsexp/buildlink3.mk
+++ devel/ocaml-parsexp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/07/19 15:15:23 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/11/27 15:21:18 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-parsexp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-parsexp
 OCAML_PARSEXP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-parsexp+=	ocaml-parsexp>=0.11.0
-BUILDLINK_ABI_DEPENDS.ocaml-parsexp+=	ocaml-parsexp>=0.11.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-parsexp+=	ocaml-parsexp>=0.11.0nb3
 BUILDLINK_PKGSRCDIR.ocaml-parsexp?=	../../devel/ocaml-parsexp
 
 .endif	# OCAML_PARSEXP_BUILDLINK3_MK
