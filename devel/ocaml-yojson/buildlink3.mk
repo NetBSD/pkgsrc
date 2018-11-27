65766fe302b4a8510edcb8d06649d7bdc3a91a1e
jaapb <jaapb@pkgsrc.org>
2018-11-27 23:33:40 +0000

    Updated package devel/ocaml-yojson to use dune.

--- devel/ocaml-yojson/buildlink3.mk
+++ devel/ocaml-yojson/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/07/19 14:18:09 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/11/27 23:33:40 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-yojson
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-yojson
 OCAML_YOJSON_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-yojson+=	ocaml-yojson>=1.3.3
-BUILDLINK_ABI_DEPENDS.ocaml-yojson+=	ocaml-yojson>=1.4.1nb2
+BUILDLINK_ABI_DEPENDS.ocaml-yojson+=	ocaml-yojson>=1.4.1nb4
 BUILDLINK_PKGSRCDIR.ocaml-yojson?=	../../devel/ocaml-yojson
 
 .endif	# OCAML_YOJSON_BUILDLINK3_MK
