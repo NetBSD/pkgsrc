2face65f5fc0ca8b8d77123715c6321997912ed0
jaapb <jaapb@pkgsrc.org>
2018-11-27 22:45:00 +0000

    Updated textproc/ocaml-easy-format to version 1.3.1.
    Can't find a changelog, but mostly this seems to deal with changing the
    build system to dune.

--- textproc/ocaml-easy-format/buildlink3.mk
+++ textproc/ocaml-easy-format/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/07/19 15:15:29 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/11/27 22:45:00 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-easy-format
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-easy-format
 OCAML_EASY_FORMAT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-easy-format+=	ocaml-easy-format>=1.2.0
-BUILDLINK_ABI_DEPENDS.ocaml-easy-format+=	ocaml-easy-format>=1.2.0nb4
+BUILDLINK_ABI_DEPENDS.ocaml-easy-format+=	ocaml-easy-format>=1.3.1
 BUILDLINK_PKGSRCDIR.ocaml-easy-format?=		../../textproc/ocaml-easy-format
 .endif	# OCAML_EASY_FORMAT_BUILDLINK3_MK
 
