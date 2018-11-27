25291745d04249c7786c49abd21d988dd731b926
jaapb <jaapb@pkgsrc.org>
2018-11-27 14:03:45 +0000

    Updated misc/ocaml-opam-file-format to version 2.0.0.
    No changes from the previous version as far as I can see, but the project
    is out of beta upstream.

--- misc/ocaml-opam-file-format/buildlink3.mk
+++ misc/ocaml-opam-file-format/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/07/19 15:15:27 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2018/11/27 14:03:45 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-opam-file-format
 
 .if !defined(OCAML_OPAM_FILE_FORMAT_BUILDLINK3_MK)
 OCAML_OPAM_FILE_FORMAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-opam-file-format+=	ocaml-opam-file-format>=2.0.0beta
-BUILDLINK_ABI_DEPENDS.ocaml-opam-file-format+=	ocaml-opam-file-format>=2.0.0beta3nb4
+BUILDLINK_API_DEPENDS.ocaml-opam-file-format+=	ocaml-opam-file-format>=2.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-opam-file-format+=	ocaml-opam-file-format>=2.0.0
 BUILDLINK_PKGSRCDIR.ocaml-opam-file-format?=	../../misc/ocaml-opam-file-format
 .endif	# OCAML_OPAM_FILE_FORMAT_BUILDLINK3_MK
 
