90b349a627e936ada62570d169034e9ee3bfc9ea
jaapb <jaapb@pkgsrc.org>
2018-11-27 15:37:45 +0000

    Changed devel/ocaml-result to use dune (and revbump)

--- devel/ocaml-result/buildlink3.mk
+++ devel/ocaml-result/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/07/19 15:15:25 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/11/27 15:37:45 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-result
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-result
 OCAML_RESULT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-result+=	ocaml-result>=1.2
-BUILDLINK_ABI_DEPENDS.ocaml-result+=	ocaml-result>=1.3nb1
+BUILDLINK_ABI_DEPENDS.ocaml-result+=	ocaml-result>=1.3nb3
 BUILDLINK_PKGSRCDIR.ocaml-result?=	../../devel/ocaml-result
 .endif	# OCAML_RESULT_BUILDLINK3_MK
 
