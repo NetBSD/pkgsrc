048214940fdb6edb6dc662104a6ab0b6b32f3482
jaapb <jaapb@pkgsrc.org>
2018-11-27 23:21:57 +0000

    Updated package devel/ocaml-ppx_compare to use dune. Revbump.

--- devel/ocaml-ppx_compare/buildlink3.mk
+++ devel/ocaml-ppx_compare/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/07/19 15:15:24 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2018/11/27 23:21:57 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_compare
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_compare
 OCAML_PPX_COMPARE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_compare+=	ocaml-ppx_compare>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_compare+=	ocaml-ppx_compare>=0.11.1nb1
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_compare+=	ocaml-ppx_compare>=0.11.1nb3
 BUILDLINK_PKGSRCDIR.ocaml-ppx_compare?=		../../devel/ocaml-ppx_compare
 .endif	# OCAML_PPX_COMPARE_BUILDLINK3_MK
 
