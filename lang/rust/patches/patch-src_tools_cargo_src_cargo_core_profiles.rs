7f2b7167c2eb3d8d6e0cce77ab11d9a81cc55f31
adam <adam@pkgsrc.org>
2018-11-27 15:45:23 +0000

    rust: speed-up building; clean-ups
    - use 'build' target for building, not 'dist'
    - set jobs also for install target
    - do not generate tarballs; we don't need them, but they take a lot of disk-space
    - do not install 'src'
    - do not generate 'install.log' nor 'uninstall.sh'
    - on Darwin, use headerpad_max_install_names to be able to fix all dylibs
    - make optimized bootstrap
    - pkglint fixes
    - get ready to depend on lang/llvm and devel/jemalloc

--- lang/rust/patches/patch-src_tools_cargo_src_cargo_core_profiles.rs
+++ lang/rust/patches/patch-src_tools_cargo_src_cargo_core_profiles.rs
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_tools_cargo_src_cargo_core_profiles.rs,v 1.1 2018/10/29 22:24:11 he Exp $
+$NetBSD: patch-src_tools_cargo_src_cargo_core_profiles.rs,v 1.2 2018/11/27 15:45:23 adam Exp $
 
 Turn off incremental builds for sparc64, ref.
 https://sources.debian.org/patches/cargo/0.29.0-1/2007_sparc64_disable_incremental_build.patch/
 
---- ./src/tools/cargo/src/cargo/core/profiles.rs.orig	2018-10-24 20:01:28.000000000 +0000
-+++ ./src/tools/cargo/src/cargo/core/profiles.rs
+--- src/tools/cargo/src/cargo/core/profiles.rs.orig	2018-10-24 20:01:28.000000000 +0000
++++ src/tools/cargo/src/cargo/core/profiles.rs
 @@ -458,6 +458,9 @@ impl Profile {
              debuginfo: Some(2),
              debug_assertions: true,
