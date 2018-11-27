1eddf63f13930d6577bd831689f8648a8191cd42
jaapb <jaapb@pkgsrc.org>
2018-11-27 17:36:10 +0000

    Updated package devel/ocaml-ppx_derivers to use dune

--- devel/ocaml-ppx_derivers/buildlink3.mk
+++ devel/ocaml-ppx_derivers/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/07/19 15:15:24 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/11/27 17:36:10 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_derivers
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_derivers
 OCAML_PPX_DERIVERS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_derivers+=	ocaml-ppx_derivers>=1.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_derivers+=	ocaml-ppx_derivers>=1.2nb1
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_derivers+=	ocaml-ppx_derivers>=1.2nb3
 BUILDLINK_PKGSRCDIR.ocaml-ppx_derivers?=	../../devel/ocaml-ppx_derivers
 .endif	# OCAML_PPX_DERIVERS_BUILDLINK3_MK
 
