3623e0aabb28806f0b78bee0d14729506eaa1332
jaapb <jaapb@pkgsrc.org>
2018-11-27 23:25:16 +0000

    Updated package devel/ocaml-ppx_hash to use dune. Revbump.

--- devel/ocaml-ppx_hash/buildlink3.mk
+++ devel/ocaml-ppx_hash/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/07/19 15:15:24 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/11/27 23:25:16 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_hash
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_hash
 OCAML_PPX_HASH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_hash+=	ocaml-ppx_hash>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_hash+=	ocaml-ppx_hash>=0.11.1nb1
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_hash+=	ocaml-ppx_hash>=0.11.1nb3
 BUILDLINK_PKGSRCDIR.ocaml-ppx_hash?=	../../devel/ocaml-ppx_hash
 
 .endif	# OCAML_PPX_HASH_BUILDLINK3_MK
