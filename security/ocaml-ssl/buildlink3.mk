3e76c0a43ace2aef8610960ea3232168bd45b15f
jaapb <jaapb@pkgsrc.org>
2018-11-27 19:03:09 +0000

    Updated package security/ocaml-ssl to version 0.5.7.
    Changes include minor bugfixes and a change of the build system to dune.

--- security/ocaml-ssl/buildlink3.mk
+++ security/ocaml-ssl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2018/11/12 16:10:16 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2018/11/27 19:03:09 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ssl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ssl
 OCAML_SSL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.4.0
-BUILDLINK_ABI_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.5.5nb3
+BUILDLINK_ABI_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.5.7
 BUILDLINK_PKGSRCDIR.ocaml-ssl?=		../../security/ocaml-ssl
 
 .include "../../lang/ocaml/buildlink3.mk"
