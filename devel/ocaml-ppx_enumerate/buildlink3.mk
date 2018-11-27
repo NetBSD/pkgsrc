39b7a8f3db3d492782d4140fa6ed894005d8827a
jaapb <jaapb@pkgsrc.org>
2018-11-27 23:23:23 +0000

    Updated package devel/ocaml-ppx_enumerate to use dune. Revbump.

--- devel/ocaml-ppx_enumerate/buildlink3.mk
+++ devel/ocaml-ppx_enumerate/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/07/19 15:15:24 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/11/27 23:23:23 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_enumerate
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_enumerate
 OCAML_PPX_ENUMERATE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_enumerate+=	ocaml-ppx_enumerate>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_enumerate+=	ocaml-ppx_enumerate>=0.11.1nb1
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_enumerate+=	ocaml-ppx_enumerate>=0.11.1nb3
 BUILDLINK_PKGSRCDIR.ocaml-ppx_enumerate?=	../../devel/ocaml-ppx_enumerate
 .endif	# OCAML_PPX_ENUMERATE_BUILDLINK3_MK
 
