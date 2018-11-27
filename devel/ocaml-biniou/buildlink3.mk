18ba9beb97d46ab56291f394b5e3417049d7a71b
jaapb <jaapb@pkgsrc.org>
2018-11-27 22:49:35 +0000

    Updated devel/ocaml-biniou package to use dune

--- devel/ocaml-biniou/buildlink3.mk
+++ devel/ocaml-biniou/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/07/19 15:15:22 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2018/11/27 22:49:35 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-biniou
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-biniou
 OCAML_BINIOU_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-biniou+=	ocaml-biniou>=1.0.13
-BUILDLINK_ABI_DEPENDS.ocaml-biniou+=	ocaml-biniou>=1.2.0nb2
+BUILDLINK_ABI_DEPENDS.ocaml-biniou+=	ocaml-biniou>=1.2.0nb4
 BUILDLINK_PKGSRCDIR.ocaml-biniou?=	../../devel/ocaml-biniou
 
 .endif	# OCAML_BINIOU_BUILDLINK3_MK
