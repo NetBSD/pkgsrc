4a62fa3255cf8710132935ec6b3c9236a449427b
jaapb <jaapb@pkgsrc.org>
2018-11-28 12:00:16 +0000

    Updated package devel/ocaml-lwt_react to use dune

--- devel/ocaml-lwt_react/buildlink3.mk
+++ devel/ocaml-lwt_react/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/07/19 15:15:23 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/11/28 12:00:16 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt_react
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-lwt_react
 OCAML_LWT_REACT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-lwt_react+=	ocaml-lwt_react>=1.1.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt_react+=	ocaml-lwt_react>=1.1.1nb1
+BUILDLINK_ABI_DEPENDS.ocaml-lwt_react+=	ocaml-lwt_react>=1.1.1nb3
 BUILDLINK_PKGSRCDIR.ocaml-lwt_react?=	../../devel/ocaml-lwt_react
 
 .include "../../devel/ocaml-lwt/buildlink3.mk"
