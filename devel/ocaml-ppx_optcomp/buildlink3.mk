53e933076e415e790e0397920004e38c21c849eb
jaapb <jaapb@pkgsrc.org>
2018-11-29 11:56:54 +0000

    Updated package devel/ocaml-ppx_optcomp to use dune

--- devel/ocaml-ppx_optcomp/buildlink3.mk
+++ devel/ocaml-ppx_optcomp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2018/07/19 15:15:24 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2018/11/29 11:56:54 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_optcomp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_optcomp
 OCAML_PPX_OPTCOMP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_optcomp+=	ocaml-ppx_optcomp>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_optcomp+=	ocaml-ppx_optcomp>=0.11.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_optcomp+=	ocaml-ppx_optcomp>=0.11.0nb3
 BUILDLINK_PKGSRCDIR.ocaml-ppx_optcomp?=		../../devel/ocaml-ppx_optcomp
 
 .endif	# OCAML_PPX_OPTCOMP_BUILDLINK3_MK
