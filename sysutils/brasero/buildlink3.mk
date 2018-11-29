dc077ce8fd34d3b06803ea5a2932234cfd113d1d
prlw1 <prlw1@pkgsrc.org>
2018-11-29 11:21:45 +0000

    Revbump for libcanberra gstreamer change.

--- sysutils/brasero/buildlink3.mk
+++ sysutils/brasero/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.56 2018/11/14 22:22:22 kleink Exp $
+# $NetBSD: buildlink3.mk,v 1.57 2018/11/29 11:21:46 prlw1 Exp $
 
 BUILDLINK_TREE+=	brasero
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	brasero
 BRASERO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.brasero+=	brasero>=2.26.0
-BUILDLINK_ABI_DEPENDS.brasero+=	brasero>=2.32.1nb49
+BUILDLINK_ABI_DEPENDS.brasero+=	brasero>=2.32.1nb50
 BUILDLINK_PKGSRCDIR.brasero?=	../../sysutils/brasero
 
 .include "../../x11/gtk2/buildlink3.mk"
