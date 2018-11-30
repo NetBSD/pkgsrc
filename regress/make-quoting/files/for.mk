fa9415d238597e7ecfa803385ecdf9f875033f8c
rillig <rillig@pkgsrc.org>
2018-11-30 19:06:32 +0000

    regress/make-quoting: adjust to bmake change from 2015

--- regress/make-quoting/files/for.mk
+++ regress/make-quoting/files/for.mk
@@ -1,7 +1,10 @@
-# $NetBSD: for.mk,v 1.1.1.1 2005/05/15 21:10:16 rillig Exp $
+# $NetBSD: for.mk,v 1.2 2018/11/30 19:06:33 rillig Exp $
 #
-# .for loops don't interpret variables in sh(1) mode, but split them at
-# whitespace.
+# Up to 2015, .for loops didn't interpret variables in sh(1) mode, but
+# split them at whitespace.
+#
+# Since 2015-05-20, the .for loops behave like the rest of Make,
+# splitting at the quoting.
 
 LIST_1=		1 2 "3     3"  '4 ' 5'5'5"5"5
 
