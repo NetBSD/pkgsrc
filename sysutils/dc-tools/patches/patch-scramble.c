06ddc283bafab2690deb94f5002410993e98839d
thorpej <thorpej@pkgsrc.org>
2018-11-30 16:33:37 +0000

    Make this build cleanly with clang on a macOS host.

---
+++ sysutils/dc-tools/patches/patch-scramble.c
@@ -0,0 +1,9 @@
+--- scramble.c.orig	2018-11-30 08:21:21.000000000 -0800
++++ scramble.c	2018-11-30 08:21:35.000000000 -0800
+@@ -1,5 +1,6 @@
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ 
+ #define MAXCHUNK (2048*1024)
+ 
