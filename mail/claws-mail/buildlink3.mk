09c63120090c8cccfb8d20412f0a5b59c7a7efcf
bsiegert <bsiegert@pkgsrc.org>
2018-12-01 17:31:06 +0000

    Port claws-mail to use enchant2.
    Bump revision of claws-mail itself and of all the plugins.

--- mail/claws-mail/buildlink3.mk
+++ mail/claws-mail/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2018/01/07 13:04:21 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2018/12/01 17:31:06 bsiegert Exp $
 
 BUILDLINK_TREE+=	claws-mail
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.claws-mail+=	claws-mail>=3.7.0
 BUILDLINK_ABI_DEPENDS.claws-mail+=	claws-mail>=3.11.1
 BUILDLINK_PKGSRCDIR.claws-mail?=	../../mail/claws-mail
 
-.include "../../textproc/enchant/buildlink3.mk"
+.include "../../textproc/enchant2/buildlink3.mk"
 .endif # CLAWS_MAIL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-claws-mail
