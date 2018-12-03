62b54fac434899f09782c90e021ee3514f4f43dc
wiz <wiz@pkgsrc.org>
2018-12-03 08:35:28 +0000

    *: recursive bump for poppler-0.71.0 shlib bump

--- games/simgear/buildlink3.mk
+++ games/simgear/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2018/11/23 08:06:28 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2018/12/03 08:35:28 wiz Exp $
 
 BUILDLINK_TREE+=	simgear
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	simgear
 SIMGEAR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.simgear+=	simgear>=2018.2.2
-BUILDLINK_ABI_DEPENDS.simgear?=	simgear>=2018.2.2nb6
+BUILDLINK_ABI_DEPENDS.simgear?=	simgear>=2018.2.2nb7
 BUILDLINK_PKGSRCDIR.simgear?=	../../games/simgear
 
 .include "../../audio/openal-soft/buildlink3.mk"
