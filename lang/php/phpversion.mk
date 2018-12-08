6f7ed56313162b6efea76922dca66e789729dfd7
taca <taca@pkgsrc.org>
2018-12-08 06:58:36 +0000

    lang/php: fix php71's version
    Oops, fix PHP71_VERSION.

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.239 2018/12/07 17:14:58 taca Exp $
+# $NetBSD: phpversion.mk,v 1.240 2018/12/08 06:58:36 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.39
 PHP70_VERSION=	7.0.33
-PHP71_VERSION=	7.1.26
+PHP71_VERSION=	7.1.25
 PHP72_VERSION=	7.2.13
 
 # Define initial release of major version.
