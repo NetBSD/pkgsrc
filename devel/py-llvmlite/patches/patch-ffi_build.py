8dfb1f8cb767c43f9c2572f139d9dd75c68efe33
adam <adam@pkgsrc.org>
2018-12-09 20:22:55 +0000

    py-llvmlite: updated to 0.26.0
    v0.26.0
    The primary new features in this release is support for generation of Intel JIT events, which makes profiling of JIT compiled code in Intel VTune possible. This release also contains some minor build improvements for ARMv7, and some small fixes.
    
    LLVM 7 support was originally slated for this release, but had to be delayed after some issues arose in testing. LLVM 6 is still required for llvmlite.
    
    * Use native cmake on armv7l
    * Throttle thread count for llvm build on armv7l.
    * Add shutdown detection to ObjectRef __del__ method.
    * conda recipe: add make as build dep
    * Add get_element_offset to TargetData
    * Fix gep method call on Constant objects
    * Fix typo in irbuilder documentation
    * Enable IntelJIT events for LLVM for VTune support

--- devel/py-llvmlite/patches/patch-ffi_build.py
+++ devel/py-llvmlite/patches/patch-ffi_build.py
@@ -1,10 +1,20 @@
-$NetBSD: patch-ffi_build.py,v 1.2 2018/08/07 10:46:42 adam Exp $
+$NetBSD: patch-ffi_build.py,v 1.3 2018/12/09 20:22:55 adam Exp $
 
-Add NetBSD support
+Allow newer LLVM.
+Add NetBSD support.
 
 --- ffi/build.py.orig	2018-07-10 14:46:02.000000000 +0000
 +++ ffi/build.py
-@@ -143,6 +143,8 @@ def main():
+@@ -109,7 +109,7 @@ def main_posix(kind, library_ext):
+ 
+     out = out.decode('latin1')
+     print(out)
+-    if not out.startswith('6.0.'):
++    if False:
+         msg = (
+             "Building llvmlite requires LLVM 6.0.x. Be sure to "
+             "set LLVM_CONFIG to the right executable path.\n"
+@@ -157,6 +157,8 @@ def main():
          main_posix('linux', '.so')
      elif sys.platform.startswith(('freebsd','openbsd')):
          main_posix('freebsd', '.so')
