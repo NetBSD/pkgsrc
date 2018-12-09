36532f9a02a103dbe379e41aa41db86515b7d422
adam <adam@pkgsrc.org>
2018-12-09 20:04:38 +0000

    llvm: updated to 7.0.0
    LLVM 7.0.0 Release
    
    The release contains the work on trunk up to SVN revision 338536 plus
    work on the release branch. It is the result of the community's work
    over the past six months, including: function multiversioning in Clang
    with the 'target' attribute for ELF-based x86/x86_64 targets, improved
    PCH support in clang-cl, preliminary DWARF v5 support, basic support
    for OpenMP 4.5 offloading to NVPTX, OpenCL C++ support, MSan, X-Ray
    and libFuzzer support for FreeBSD, early UBSan, X-Ray and libFuzzer
    support for OpenBSD, UBSan checks for implicit conversions, many
    long-tail compatibility issues fixed in lld which is now production
    ready for ELF, COFF and MinGW, new tools llvm-exegesis, llvm-mca and
    diagtool. And as usual, many optimizations, improved diagnostics, and
    bug fixes.
    
    For more details, see the release notes:
    https://llvm.org/releases/7.0.0/docs/ReleaseNotes.html
    https://llvm.org/releases/7.0.0/tools/clang/docs/ReleaseNotes.html
    https://llvm.org/releases/7.0.0/tools/clang/tools/extra/docs/ReleaseNotes.html
    https://llvm.org/releases/7.0.0/tools/lld/docs/ReleaseNotes.html

--- lang/clang/patches/patch-lib_Frontend_InitHeaderSearch.cpp
+++ lang/clang/patches/patch-lib_Frontend_InitHeaderSearch.cpp
@@ -1,13 +1,13 @@
-$NetBSD: patch-lib_Frontend_InitHeaderSearch.cpp,v 1.1 2018/08/09 14:56:41 jperkin Exp $
+$NetBSD: patch-lib_Frontend_InitHeaderSearch.cpp,v 1.2 2018/12/09 20:04:38 adam Exp $
 
 Don't add /usr/local/include by default on Solaris.
 
---- lib/Frontend/InitHeaderSearch.cpp.orig	2018-01-04 07:43:41.000000000 +0000
+--- lib/Frontend/InitHeaderSearch.cpp.orig	2018-06-28 23:23:45.000000000 +0000
 +++ lib/Frontend/InitHeaderSearch.cpp
-@@ -216,6 +216,7 @@ void InitHeaderSearch::AddDefaultCInclud
-     case llvm::Triple::NaCl:
+@@ -221,6 +221,7 @@ void InitHeaderSearch::AddDefaultCInclud
      case llvm::Triple::PS4:
      case llvm::Triple::ELFIAMCU:
+     case llvm::Triple::Fuchsia:
 +    case llvm::Triple::Solaris:
        break;
      case llvm::Triple::Win32:
