c859e7b19dcd558c211972c47c585a4ef506a382
adam <adam@pkgsrc.org>
2018-12-09 20:04:38 +0000

    llvm: updated to 7.0.0
    LLVM 7.0.0 Release
    
    The release contains the work on trunk up to SVN revision 338536 plus
    work on the release branch. It is the result of the community's work
    over the past six months, including: function multiversioning in Clang
    with the 'target' attribute for ELF-based x86/x86_64 targets, improved
    PCH support in clang-cl, preliminary DWARF v5 support, basic support
    for OpenMP 4.5 offloading to NVPTX, OpenCL C++ support, MSan, X-Ray
    and libFuzzer support for FreeBSD, early UBSan, X-Ray and libFuzzer
    support for OpenBSD, UBSan checks for implicit conversions, many
    long-tail compatibility issues fixed in lld which is now production
    ready for ELF, COFF and MinGW, new tools llvm-exegesis, llvm-mca and
    diagtool. And as usual, many optimizations, improved diagnostics, and
    bug fixes.
    
    For more details, see the release notes:
    https://llvm.org/releases/7.0.0/docs/ReleaseNotes.html
    https://llvm.org/releases/7.0.0/tools/clang/docs/ReleaseNotes.html
    https://llvm.org/releases/7.0.0/tools/clang/tools/extra/docs/ReleaseNotes.html
    https://llvm.org/releases/7.0.0/tools/lld/docs/ReleaseNotes.html

--- devel/lldb/buildlink3.mk
+++ devel/lldb/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2017/12/30 10:39:24 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2018/12/09 20:04:39 adam Exp $
 
 BUILDLINK_TREE+=	lldb
 
 .if !defined(LLDB_BUILDLINK3_MK)
 LLDB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lldb+=	lldb>=4.0.1
+BUILDLINK_API_DEPENDS.lldb+=	lldb>=7.0.0
 BUILDLINK_PKGSRCDIR.lldb?=	../../devel/lldb
 
 .include "../../devel/zlib/buildlink3.mk"
