cdf76dedaa95df38186785a4e77d7e407a9fa10e
adam <adam@pkgsrc.org>
2018-12-09 18:33:55 +0000

    tcl: updated to 8.6.9
    8.6.9:
    * (enhance) stop blocking conversion of object to/from class
    * (enhance) NR-enable [package require]
    * (bug)[9fd5c6] crash in object deletion, test oo-11.5
    * (bug)[3c32a3] crash deleting object with class mixed in
    * (platform) stop using -lieee, removed from glibc-2.27
    ***POTENTIAL INCOMPATIBILITY for math programs that embed Tcl***
    * (bug)[8e6a9a] bad binary [string match], test string-11.55
    * (bug)[1873ea] repair multi-thread std channel init
    * (bug)[db36fa] broken bytecode for index values
    * (bug) broken compiled [string replace], test string-14.19
    * (bug) [string trim*] engine crashed on invalid UTF
    * (bug) missing trace in compiled [array set], test var-20.11
    * (bug)[46a241] crash in unset array with search, var-13.[23]
    * (bug)[27b682] race made [file delete] raise "no such file"
    * (bug)[925643] 32/64 cleanup of filesystem DIR operations
    * (bug) leaks in TclSetEnv and env cache
    * (bug)[3592747] [yieldto] dying namespace, tailcall-14.1
    * (bug)[270f78] race in [file mkdir]
    * (bug)[3f7af0] [file delete] raised "permission denied"
    * (bug)[d051b7] overflow crash in [format]
    * revised quoting of [exec] args in generated command line
    ***POTENTIAL INCOMPATIBILITY***
    * HTTP Keep-Alive with pipelined requests
    => http 2.9.0
    * (new)[TIP 505] [lreplace] accepts all out of range indices
    * (bug) Prevent crash from NULL keyName
    => registry 1.3.3
    * (enhance) advance dde version
    => dde 1.4.1
    * tzdata updated to Olson's tzdata2018g
    * Update tcltest package for Travis support
    => tcltest 2.5.0
    * (bug)[35a8f1] overlong string length of some lists
    * (bug)[00d04c] Repair [binary encode base64]

--- lang/tcl/buildlink3.mk
+++ lang/tcl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.30 2018/02/03 17:51:19 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2018/12/09 18:33:55 adam Exp $
 
 BUILDLINK_TREE+=	tcl
 
@@ -22,8 +22,8 @@ _TOOLS_USE_PKGSRC.tclsh=	yes
 TCLCONFIG_SH?=		${BUILDLINK_PREFIX.tcl}/lib/tclConfig.sh
 TCLSH=			${BUILDLINK_PREFIX.tcl}/bin/tclsh
 # Bundled library
-ITCL_VERSION=		4.1.1
-TDBC_VERSION=		1.0.6
+ITCL_VERSION=		4.1.2
+TDBC_VERSION=		1.1.0
 
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../mk/dlopen.buildlink3.mk"
