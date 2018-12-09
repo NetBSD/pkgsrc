b035ae40fc13c13db0a6ad8754e31e4a1fca1167
adam <adam@pkgsrc.org>
2018-12-09 20:04:38 +0000

    llvm: updated to 7.0.0
    LLVM 7.0.0 Release
    
    The release contains the work on trunk up to SVN revision 338536 plus
    work on the release branch. It is the result of the community's work
    over the past six months, including: function multiversioning in Clang
    with the 'target' attribute for ELF-based x86/x86_64 targets, improved
    PCH support in clang-cl, preliminary DWARF v5 support, basic support
    for OpenMP 4.5 offloading to NVPTX, OpenCL C++ support, MSan, X-Ray
    and libFuzzer support for FreeBSD, early UBSan, X-Ray and libFuzzer
    support for OpenBSD, UBSan checks for implicit conversions, many
    long-tail compatibility issues fixed in lld which is now production
    ready for ELF, COFF and MinGW, new tools llvm-exegesis, llvm-mca and
    diagtool. And as usual, many optimizations, improved diagnostics, and
    bug fixes.
    
    For more details, see the release notes:
    https://llvm.org/releases/7.0.0/docs/ReleaseNotes.html
    https://llvm.org/releases/7.0.0/tools/clang/docs/ReleaseNotes.html
    https://llvm.org/releases/7.0.0/tools/clang/tools/extra/docs/ReleaseNotes.html
    https://llvm.org/releases/7.0.0/tools/lld/docs/ReleaseNotes.html

--- lang/llvm/patches/patch-cmake_modules_AddLLVM.cmake
+++ lang/llvm/patches/patch-cmake_modules_AddLLVM.cmake
@@ -1,21 +1,31 @@
-$NetBSD: patch-cmake_modules_AddLLVM.cmake,v 1.3 2017/03/19 19:01:48 adam Exp $
+$NetBSD: patch-cmake_modules_AddLLVM.cmake,v 1.4 2018/12/09 20:04:38 adam Exp $
 
 Don't use non-portable -z discard-unused on SunOS.
+On Darwin, create symbolic links to shared libraries.
 On Darwin, use correct install-name for shared libraries.
 
---- cmake/modules/AddLLVM.cmake.orig	2017-01-17 21:47:58.000000000 +0000
+--- cmake/modules/AddLLVM.cmake.orig	2018-08-01 07:51:55.000000000 +0000
 +++ cmake/modules/AddLLVM.cmake
-@@ -182,9 +182,6 @@ function(add_link_opts target_name)
+@@ -218,9 +218,6 @@ function(add_link_opts target_name)
          # ld64's implementation of -dead_strip breaks tools that use plugins.
          set_property(TARGET ${target_name} APPEND_STRING PROPERTY
                       LINK_FLAGS " -Wl,-dead_strip")
 -      elseif(${CMAKE_SYSTEM_NAME} MATCHES "SunOS")
 -        set_property(TARGET ${target_name} APPEND_STRING PROPERTY
 -                     LINK_FLAGS " -Wl,-z -Wl,discard-unused=sections")
-       elseif(NOT WIN32 AND NOT LLVM_LINKER_IS_GOLD)
+       elseif(NOT WIN32 AND NOT LLVM_LINKER_IS_GOLD AND NOT ${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD")
          # Object files are compiled with -ffunction-data-sections.
          # Versions of bfd ld < 2.23.1 have a bug in --gc-sections that breaks
-@@ -1419,7 +1416,7 @@ function(llvm_setup_rpath name)
+@@ -517,7 +514,7 @@ function(llvm_add_library name)
+   endif()
+ 
+   if(ARG_SHARED AND UNIX)
+-    if(NOT APPLE AND ARG_SONAME)
++    if(ARG_SONAME)
+       get_target_property(output_name ${name} OUTPUT_NAME)
+       if(${output_name} STREQUAL "output_name-NOTFOUND")
+         set(output_name ${name})
+@@ -1632,7 +1629,7 @@ function(llvm_setup_rpath name)
    endif()
  
    if (APPLE)
