cabbad8da30e49a2efdec43509659d88be00a68d
adam <adam@pkgsrc.org>
2018-12-09 20:04:38 +0000

    llvm: updated to 7.0.0
    LLVM 7.0.0 Release
    
    The release contains the work on trunk up to SVN revision 338536 plus
    work on the release branch. It is the result of the community's work
    over the past six months, including: function multiversioning in Clang
    with the 'target' attribute for ELF-based x86/x86_64 targets, improved
    PCH support in clang-cl, preliminary DWARF v5 support, basic support
    for OpenMP 4.5 offloading to NVPTX, OpenCL C++ support, MSan, X-Ray
    and libFuzzer support for FreeBSD, early UBSan, X-Ray and libFuzzer
    support for OpenBSD, UBSan checks for implicit conversions, many
    long-tail compatibility issues fixed in lld which is now production
    ready for ELF, COFF and MinGW, new tools llvm-exegesis, llvm-mca and
    diagtool. And as usual, many optimizations, improved diagnostics, and
    bug fixes.
    
    For more details, see the release notes:
    https://llvm.org/releases/7.0.0/docs/ReleaseNotes.html
    https://llvm.org/releases/7.0.0/tools/clang/docs/ReleaseNotes.html
    https://llvm.org/releases/7.0.0/tools/clang/tools/extra/docs/ReleaseNotes.html
    https://llvm.org/releases/7.0.0/tools/lld/docs/ReleaseNotes.html

--- lang/clang/patches/patch-lib_Driver_ToolChains_Solaris.h
+++ lang/clang/patches/patch-lib_Driver_ToolChains_Solaris.h
@@ -1,16 +1,16 @@
-$NetBSD: patch-lib_Driver_ToolChains_Solaris.h,v 1.1 2018/08/09 14:56:41 jperkin Exp $
+$NetBSD: patch-lib_Driver_ToolChains_Solaris.h,v 1.2 2018/12/09 20:04:38 adam Exp $
 
 Override AddCXXStdlibLibArgs().
 
---- lib/Driver/ToolChains/Solaris.h.orig	2018-01-04 07:43:41.000000000 +0000
+--- lib/Driver/ToolChains/Solaris.h.orig	2018-02-05 23:59:13.000000000 +0000
 +++ lib/Driver/ToolChains/Solaris.h
-@@ -60,6 +60,9 @@ public:
-   void AddClangCXXStdlibIncludeArgs(
-       const llvm::opt::ArgList &DriverArgs,
-       llvm::opt::ArgStringList &CC1Args) const override;
-+  void AddCXXStdlibLibArgs(
-+      const llvm::opt::ArgList &Args,
-+      llvm::opt::ArgStringList &CmdArgs) const override;
+@@ -67,6 +67,9 @@ public:
  
+   SanitizerMask getSupportedSanitizers() const override;
    unsigned GetDefaultDwarfVersion() const override { return 2; }
++  void AddCXXStdlibLibArgs(
++     const llvm::opt::ArgList &Args,
++     llvm::opt::ArgStringList &CmdArgs) const override;
  
+ protected:
+   Tool *buildAssembler() const override;
