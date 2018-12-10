8d1ced81d89767724bb024cd0e0a830843514e7c
leot <leot@pkgsrc.org>
2018-12-10 14:59:50 +0000

    swi-prolog*: Update lang/swi-prolog* to 7.6.4
    pkgsrc changes:
     - Add patches/patch-src_os_pl-files.c to avoid overlapping `src'
       and `dst' in srtcpy(3).  It is an undefined behaviour and it leads to build
       issue in swi-prolog-packages on platforms that uses SSP.
     - readline dependency is no longer needed for swi-prolog-lite.
       The readline functionality is now provided as a package via
       swi-prolog-packages.
     - Inject CFLAGS via COFLAGS and pass -fPIC via CMFLAGS so that
       swi-prolog-jpl will link
     - Add patches/patch-src_pl-rsort.c to use FreeBSD qsort_r(3)
       instead of using nested functions. This is probably more portable
       and has less problems (e.g. does not need for PaX MPROTECT).
    
    Changes:
    7.6.x
    -----
    Major release. Multi-argument indexing, better multi-threaded
    performance, threaded global GC (atoms and clauses), mode-directed
    tabling, restored same expand and directive semantics for initial
    load and reloading files, cleaner initializatin and command line
    handling.
    
    7.4.x
    -----
    Major release. More robust and better scalable support for multi-core
    hardware, tabling, engines. Many enhancements to the RDF, HTTP and
    SSL libraries. This is the first release licensed under the Simplified
    BSD (BSD-2) license.
    
    7.2.x
    -----
    Major release, providing native key-value support by means of dicts,
    native strings using "hello world" syntax, web-accessible Prolog
    engines (Pengines) and a high level SQL library called CQL.
    
    
    Please note that this is just a summary of the most important changes,
    for more information please give a look to:
    
     <http://www.swi-prolog.org/ChangeLog?branch=stable>

--- lang/swi-prolog-lite/buildlink3.mk
+++ lang/swi-prolog-lite/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2015/08/18 07:31:01 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2018/12/10 14:59:50 leot Exp $
 
 BUILDLINK_TREE+=	swi-prolog-lite
 
@@ -9,9 +9,6 @@ BUILDLINK_API_DEPENDS.swi-prolog-lite+=	swi-prolog-lite>=5.2.9
 BUILDLINK_ABI_DEPENDS.swi-prolog-lite+=	swi-prolog-lite>=6.6.6nb2
 BUILDLINK_PKGSRCDIR.swi-prolog-lite?=	../../lang/swi-prolog-lite
 
-.include "../../devel/ncurses/buildlink3.mk"
-.include "../../devel/readline/buildlink3.mk"
-#.include "../../devel/gmp/buildlink3.mk" ?
 .endif	# SWI_PROLOG_LITE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-swi-prolog-lite
