137bdc202af3168d800a7c7f6241437c24100686
wiz <wiz@pkgsrc.org>
2018-12-11 10:37:20 +0000

    musicpd: update to 0.21.3.
    ver 0.21.3 (2018/11/16)
    * output
      - alsa: fix crash bug
      - alsa: fix stuttering at start of playback
      - alsa: fix discarded samples at end of song
      - alsa: clear error after reopening device
    * log: default to journal if MPD was started as systemd service
    
    ver 0.21.2 (2018/11/12)
    * protocol
      - operator "=~" matches a regular expression
      - operator "contains" matches substrings
    * decoder
      - ffmpeg: require FFmpeg 3.1 or later
      - ffmpeg: fix broken sound with certain codecs
    * output
      - alsa: fix high CPU usage with dmix
      - httpd: fix three crash bugs
    * mixer
      - alsa: fix more rounding errors
    * fix zlib support
    
    ver 0.21.1 (2018/11/04)
    * protocol
      - allow escaping quotes in filter expressions
      - operator "==" never searches substrings in filter expressions
    * decoder
      - ffmpeg: fix build failure with non-standard FFmpeg installation path
      - flac: fix linker failure when building without FLAC support
    * encoder
      - vorbis: fix linker failure when building without Vorbis decoder
    * fix build failure on Linux-PowerPC
    * fix build failure on FreeBSD
    * eliminate DLL dependencies on Windows
    * add warning about buggy Boost version 1.67
    * require Meson 0.47.2 because a Meson 0.47.1 bug breaks our build
    
    ver 0.21 (2018/10/31)
    * configuration
      - add "include" directive, allows including config files
      - incremental "metadata_to_use" setting
    * protocol
      - "tagtypes" can be used to hide tags
      - "find" and "search" can sort
      - "outputs" prints the plugin name
      - "outputset" sets runtime attributes
      - close connection when client sends HTTP request
      - new filter syntax for "find"/"search" etc. with negation
    * database
      - simple: scan audio formats
      - proxy: require libmpdclient 2.9
      - proxy: forward `sort` and `window` to server
    * player
      - hard-code "buffer_before_play" to 1 second, independent of audio format
      - "one-shot" single mode
    * input
      - curl: download to buffer instead of throttling transfer
      - qobuz: new plugin to play Qobuz streams
      - tidal: new plugin to play Tidal streams
    * tags
      - new tags "OriginalDate", "MUSICBRAINZ_WORKID"
    * decoder
      - ffmpeg: require at least version 11.12
      - gme: try loading m3u sidecar files
      - hybrid_dsd: new decoder plugin
      - mad: move "gapless_mp3_playback" setting to "decoder" block
      - mikmod: require at least version 3.2
      - pcm: support audio/L24 (RFC 3190)
      - sidplay: support basic and kernal rom (libsidplayfp)
    * resampler
      - soxr: flush resampler at end of song
    * output
      - alsa: non-blocking mode
      - alsa: change "dop" and "allowed_formats" settings at runtime
      - ao: fix crash bug due to partial frames
      - shout: support the Shine encoder plugin
      - sndio: remove support for the broken RoarAudio sndio emulation
      - osx: initial support for DSD over PCM
      - roar: removed
      - httpd_output: support for unix sockets
    * mixer
      - sndio: new mixer plugin
    * encoder
      - opus: support for sending metadata using ogg stream chaining
    * listen on $XDG_RUNTIME_DIR/mpd/socket by default
    * append hostname to Zeroconf service name
    * systemd watchdog support
    * require GCC 6
    * build with Meson instead of autotools
    * use GTest instead of cppunit
    
    ver 0.20.23 (2018/10/29)
    * protocol
      - emit "player" idle event when restarting the current song
    * fix broken float to s32 conversion
    * new clang crash bug workaround
    
    ver 0.20.22 (2018/10/23)
    * protocol
      - add tag fallbacks for AlbumArtistSort, ArtistSort
      - fix empty string filter on fallback tags
      - "count group ..." can print an empty group
      - fix broken command "list ... group"
    * storage
      - curl: URL-encode paths
    * decoder
      - fluidsynth: adapt to API change in version 2.0
    * Android
      - now runs as a service
      - add button to start/stop MPD
      - add option to auto-start on boot
    * work around clang bug leading to crash
    * install the SVG icon

--- audio/musicpd/options.mk
+++ audio/musicpd/options.mk
@@ -1,74 +1,46 @@
-# $NetBSD: options.mk,v 1.30 2018/04/29 09:58:21 wiz Exp $
+# $NetBSD: options.mk,v 1.31 2018/12/11 10:37:20 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.musicpd
-PKG_SUPPORTED_OPTIONS=	adplug audiofile avahi bzip2 curl faad ffmpeg flac fluidsynth id3 inet6 libao lame jack libmms libmpdclient libwildmidi mikmod modplug mpg123 musepack musicpd-soundcloud openal opus pulseaudio shout sqlite3 tremor twolame vorbis wavpack zziplib
+PKG_SUPPORTED_OPTIONS=	adplug audiofile avahi bzip2 curl faad ffmpeg flac fluidsynth id3 inet6 libao lame jack libmms libmpdclient libsoxr libwildmidi mikmod modplug mpg123 musepack musicpd-soundcloud openal opus pulseaudio shine shout sqlite3 tremor twolame vorbis wavpack zziplib
 PKG_SUGGESTED_OPTIONS=	audiofile curl faad flac id3 inet6 libao musepack vorbis
-
-# TODO:
-# shine
-# libsoxr
 
 PKG_OPTIONS_OPTIONAL_GROUPS=	vorbis
 PKG_OPTIONS_GROUP.vorbis=	tremor vorbis
-
-PKG_OPTIONS_LEGACY_OPTS=	ogg:vorbis
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Madplug)
 .  include "../../audio/adplug/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-adplug
-.else
-CONFIGURE_ARGS+=	--disable-adplug
 .endif
 
 .if !empty(PKG_OPTIONS:Maudiofile)
 BUILDLINK_API_DEPENDS.libaudiofile+=	libaudiofile>=0.3
 .  include "../../audio/libaudiofile/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-audiofile
-.else
-CONFIGURE_ARGS+=	--disable-audiofile
 .endif
 
 .if !empty(PKG_OPTIONS:Mavahi)
 .  include "../../net/avahi/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-zeroconf=avahi
 .endif
 
 .if !empty(PKG_OPTIONS:Mbzip2)
 .  include "../../archivers/bzip2/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-bzip2
-.else
-CONFIGURE_ARGS+=	--disable-bzip2
 .endif
 
 # wants a different version of the library than we have in pkgsrc
 #.if !empty(PKG_OPTIONS:Mcdparanoia)
 #.  include "../../audio/cdparanoia/buildlink3.mk"
-#CONFIGURE_ARGS+=	--enable-cdio-paranoia
-#.else
-#CONFIGURE_ARGS+=	--enable-cdio-paranoia
 #.endif
 
 .if !empty(PKG_OPTIONS:Mcurl)
 .  include "../../www/curl/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-curl
-.else
-CONFIGURE_ARGS+=	--disable-curl
 .endif
 
 .if !empty(PKG_OPTIONS:Mfaad)
 .  include "../../audio/faad2/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-aac
-.else
-CONFIGURE_ARGS+=	--disable-aac
 .endif
 
 .if !empty(PKG_OPTIONS:Mffmpeg)
 .  include "../../multimedia/ffmpeg4/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-ffmpeg
-.else
-CONFIGURE_ARGS+=	--disable-ffmpeg
 .endif
 
 .if !empty(PKG_OPTIONS:Mflac)
@@ -77,201 +49,124 @@ BUILDLINK_ABI_DEPENDS.flac+=	flac>=1.2
 # XXX whole album flac files can appearently be parsed without libcue,
 # so I've yet to find out what libcue is good for
 #.  include "../../audio/libcue/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-flac
-.else
-CONFIGURE_ARGS+=	--disable-flac
 .endif
 
 .if !empty(PKG_OPTIONS:Mfluidsynth)
 .  include "../../audio/fluidsynth/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-fluidsynth
-.else
-CONFIGURE_ARGS+=	--disable-fluidsynth
 .endif
 
 # needs to be imported from wip first
 #.if !empty(PKG_OPTIONS:Mgme)
 #.  include "../../wip/libgme/buildlink3.mk"
-#CONFIGURE_ARGS+=	--enable-gme
-#.else
-#CONFIGURE_ARGS+=	--disable-gme
 #.endif
 
 .if !empty(PKG_OPTIONS:Mid3)
 .  include "../../audio/libid3tag/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-id3
-.else
-CONFIGURE_ARGS+=	--disable-id3
 .endif
 
 .if !empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGS+=	--enable-ipv6
-.else
-CONFIGURE_ARGS+=	--disable-ipv6
 .endif
 
 .if !empty(PKG_OPTIONS:Mjack)
 .  include "../../audio/jack/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-jack
-.else
-CONFIGURE_ARGS+=	--disable-jack
 .endif
 
 .if !empty(PKG_OPTIONS:Mlame)
 .  include "../../audio/lame/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-lame-encoder
-.else
-CONFIGURE_ARGS+=	--disable-lame-encoder
 .endif
 
 .if !empty(PKG_OPTIONS:Mlibao)
 .  include "../../audio/libao/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-ao
-.else
-CONFIGURE_ARGS+=	--disable-ao
 .endif
 
 .if !empty(PKG_OPTIONS:Mlibmms)
 .  include "../../net/libmms/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-mms
-.else
-CONFIGURE_ARGS+=	--disable-mms
 .endif
 
 .if !empty(PKG_OPTIONS:Mlibmpdclient)
 .  include "../../audio/libmpdclient/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-libmpdclient
-.else
-CONFIGURE_ARGS+=	--disable-libmpdclient
 .endif
 
 # does not find libresid-builder
 #.if !empty(PKG_OPTIONS:Mlibsidplay2)
 #.  include "../../audio/libsidplay2/buildlink3.mk"
-#CONFIGURE_ARGS+=	--enable-sidplay
-#.else
-#CONFIGURE_ARGS+=	--disable-sidplay
 #.endif
+
+.if !empty(PKG_OPTIONS:Mlibsoxr)
+.  include "../../audio/libsoxr/buildlink3.mk"
+.endif
 
 .if !empty(PKG_OPTIONS:Mlibwildmidi)
 .  include "../../audio/libwildmidi/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-wildmidi
-.else
-CONFIGURE_ARGS+=	--disable-wildmidi
 .endif
 
 .if !empty(PKG_OPTIONS:Mmikmod)
 .  include "../../audio/libmikmod/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-mikmod
-.else
-CONFIGURE_ARGS+=	--disable-mikmod
 .endif
 
 .if !empty(PKG_OPTIONS:Mmodplug)
 .  include "../../audio/libmodplug/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-modplug
-.else
-CONFIGURE_ARGS+=	--disable-modplug
 .endif
 
 .if !empty(PKG_OPTIONS:Mmpg123)
 .  include "../../audio/mpg123/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-mpg123
-.else
-CONFIGURE_ARGS+=	--disable-mpg123
 .endif
 
 .if !empty(PKG_OPTIONS:Mmusepack)
 .  include "../../audio/musepack/buildlink3.mk"
-CONFIGURE_ENV+=		mpcdec_prefix=${BUILDLINK_PREFIX.musepack}
-CONFIGURE_ARGS+=	--enable-mpc
-.else
-CONFIGURE_ARGS+=	--disable-mpc
 .endif
 
 .if !empty(PKG_OPTIONS:Mmusicpd-soundcloud)
 BUILDLINK_API_DEPENDS.yajl+=	yajl>=2.1
 .  include "../../devel/yajl/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-soundcloud
-.else
-CONFIGURE_ARGS+=	--disable-soundcloud
 .endif
 
 .if !empty(PKG_OPTIONS:Mopenal)
 .  include "../../audio/openal-soft/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-openal
-.else
-CONFIGURE_ARGS+=	--disable-openal
 .endif
 
 .if !empty(PKG_OPTIONS:Mopus)
 .  include "../../audio/libopus/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-opus
-.else
-CONFIGURE_ARGS+=	--disable-opus
 .endif
 
 .if !empty(PKG_OPTIONS:Mpulseaudio)
 .  include "../../audio/pulseaudio/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-pulse
-.else
-CONFIGURE_ARGS+=	--disable-pulse
+.endif
+
+.if !empty(PKG_OPTIONS:Mshine)
+.  include "../../audio/shine/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mshout)
 .  include "../../audio/libshout/buildlink3.mk"
 .  include "../../audio/lame/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-shout
-.else
-CONFIGURE_ARGS+=	--disable-shout
 .endif
 
 # needs libresid-builder or libsidutils, both not packaged yet
 #.if !empty(PKG_OPTIONS:Msidplay)
 #.  include "../../audio/libsidplay2/buildlink3.mk"
-#CONFIGURE_ARGS+=	--enable-sidplay
-#.else
-#CONFIGURE_ARGS+=	--disable-sidplay
 #.endif
 
 .if !empty(PKG_OPTIONS:Msqlite3)
 .  include "../../databases/sqlite3/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-sqlite
-.else
-CONFIGURE_ARGS+=	--disable-sqlite
 .endif
 
 .if !empty(PKG_OPTIONS:Mtremor)
 .  include "../../audio/tremor/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-tremor=${BUILDLINK_PREFIX.tremor}
-.else
-CONFIGURE_ARGS+=	--without-tremor
 .endif
 
 .if !empty(PKG_OPTIONS:Mtwolame)
 .  include "../../audio/twolame/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-twolame-encoder
-.else
-CONFIGURE_ARGS+=	--disable-twolame-encoder
 .endif
 
 .if !empty(PKG_OPTIONS:Mvorbis)
 .  include "../../audio/libvorbis/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-vorbis
-.else
-CONFIGURE_ARGS+=	--disable-vorbis
 .endif
 
 .if !empty(PKG_OPTIONS:Mwavpack)
 .  include "../../audio/wavpack/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-wavpack
-.else
-CONFIGURE_ARGS+=	--disable-wavpack
 .endif
 
 .if !empty(PKG_OPTIONS:Mzziplib)
 .  include "../../archivers/zziplib/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-zzip
-.else
-CONFIGURE_ARGS+=	--disable-zzip
 .endif
