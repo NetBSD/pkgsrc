162e15e54d085a320ee4b0ee99616b4d6e8d5418
adam <adam@pkgsrc.org>
2018-12-11 09:11:08 +0000

    py-requests: updated to 2.21.0
    2.21.0:
    **Dependencies**
    - Requests now supports idna v2.8.

--- devel/py-requests/patches/patch-setup.py
+++ devel/py-requests/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.6 2018/10/22 07:55:36 adam Exp $
+$NetBSD: patch-setup.py,v 1.7 2018/12/11 09:11:08 adam Exp $
 
 Allow newer versions of dependencies.
 
---- setup.py.orig	2018-10-18 15:41:46.000000000 +0000
+--- setup.py.orig	2018-12-10 05:59:26.000000000 +0000
 +++ setup.py
 @@ -42,14 +42,14 @@ if sys.argv[-1] == 'publish':
  packages = ['requests']
  
  requires = [
 -    'chardet>=3.0.2,<3.1.0',
--    'idna>=2.5,<2.8',
+-    'idna>=2.5,<2.9',
 -    'urllib3>=1.21.1,<1.25',
 +    'chardet>=3.0.2',
 +    'idna>=2.5',
