57904dd9d90d038c8dd86f7b35fdeeed22ecf802
bsiegert <bsiegert@pkgsrc.org>
2018-12-11 12:36:28 +0000

    Import gcr-3.28.0nb1 from wip.
    GCR is a library for displaying certificates, and crypto UI, accessing
    key stores. It also provides the viewer for crypto files on the GNOME
    desktop.
    
    GCK is a library for accessing PKCS#11 modules like smart cards, in a
    (G)object oriented way.

---
+++ security/gcr/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2018/12/11 12:36:28 bsiegert Exp $
+
+BUILDLINK_TREE+=	gcr
+
+.if !defined(GCR_BUILDLINK3_MK)
+GCR_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gcr+=	gcr>=3.18.0
+BUILDLINK_PKGSRCDIR.gcr?=	../../wip/gcr
+
+BUILDLINK_API_DEPENDS.glib2+=	glib2>=2.38
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/gobject-introspection/buildlink3.mk"
+.include "../../security/libgcrypt/buildlink3.mk"
+.include "../../security/p11-kit/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.endif	# GCR_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gcr
