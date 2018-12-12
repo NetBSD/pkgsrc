4471239a8145f77ff1ff257fa13b067496ff53ac
adam <adam@pkgsrc.org>
2018-12-12 16:45:52 +0000

    fuse-emulator: updated to 1.5.7
    1.5.7
    GTK+ 3: Make the offset entry wider in memory browser
    GTK+: Remove duplicated surface creation code
    GTK+: Remove use of deprecated gtk_device_(un)grab functions
    GTK+/Win32: Add *.bin as an allowed filetype to file selectors and amend .fmf filetype
    Improve Fuse's Kempston mouse handling under GTK+ 3.x
    "Save binary" command can now save 65536 bytes again (regression introduced in 1.5.6)
    Various minor bugfixes

--- emulators/fuse-emulator/patches/patch-ui_gtk_gtkmouse.c
+++ emulators/fuse-emulator/patches/patch-ui_gtk_gtkmouse.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-ui_gtk_gtkmouse.c,v 1.1 2017/05/26 19:35:48 adam Exp $
+$NetBSD: patch-ui_gtk_gtkmouse.c,v 1.2 2018/12/12 16:45:52 adam Exp $
 
 Fix for a case when GTK has been compiled with Quartz support.
 
---- ui/gtk/gtkmouse.c.orig	2017-05-26 17:42:26.000000000 +0000
+--- ui/gtk/gtkmouse.c.orig	2018-12-09 12:06:11.000000000 +0000
 +++ ui/gtk/gtkmouse.c
-@@ -31,8 +31,10 @@
- #include "ui/ui.h"
+@@ -35,8 +35,10 @@
+ #endif
  
  /* For XWarpPointer *only* - see below */
 +#ifndef GDK_WINDOWING_QUARTZ
@@ -15,14 +15,14 @@ Fix for a case when GTK has been compiled with Quartz support.
  
  static GdkCursor *nullpointer = NULL;
  
-@@ -48,8 +50,10 @@ gtkmouse_reset_pointer( void )
-    */
-   GdkWindow *window = gtk_widget_get_window( gtkui_drawing_area );
+@@ -64,8 +66,10 @@ gtkmouse_reset_pointer( void )
+ #endif                /* #ifdef GDK_WINDOWING_WAYLAND */
  
+   window = gtk_widget_get_window( mouse_widget );
 +#ifndef GDK_WINDOWING_QUARTZ
    XWarpPointer( GDK_WINDOW_XDISPLAY( window ), None, 
                  GDK_WINDOW_XID( window ), 0, 0, 0, 0, 128, 128 );
 +#endif
  }
  
- gboolean
+ static gboolean
