937b0b137a8fdeeb61f6e91761f66b327ab6b2c2
abs <abs@pkgsrc.org>
2018-12-13 16:34:10 +0000

    Copy harfbuzz patch to avoid playing _POSIX_C_SOURCE games on NetBSD - causes issues with some pkgsrc gcc versions

--- misc/libreoffice/patches/patch-external_harfbuzz_UnpackedTarball__harfbuzz.mk
+++ misc/libreoffice/patches/patch-external_harfbuzz_UnpackedTarball__harfbuzz.mk
@@ -1,16 +1,14 @@
-$NetBSD: patch-external_harfbuzz_UnpackedTarball__harfbuzz.mk,v 1.4 2018/08/18 00:11:32 ryoon Exp $
+$NetBSD: patch-external_harfbuzz_UnpackedTarball__harfbuzz.mk,v 1.5 2018/12/13 16:34:10 abs Exp $
 
---- external/harfbuzz/UnpackedTarball_harfbuzz.mk.orig	2018-08-02 19:54:54.000000000 +0000
+--- external/harfbuzz/UnpackedTarball_harfbuzz.mk.orig	2018-10-29 19:55:29.000000000 +0000
 +++ external/harfbuzz/UnpackedTarball_harfbuzz.mk
-@@ -15,11 +15,6 @@ $(eval $(call gb_UnpackedTarball_update_
- 
+@@ -16,8 +16,7 @@ $(eval $(call gb_UnpackedTarball_update_
  $(eval $(call gb_UnpackedTarball_set_patchlevel,harfbuzz,0))
  
--$(eval $(call gb_UnpackedTarball_add_patches,harfbuzz, \
+ $(eval $(call gb_UnpackedTarball_add_patches,harfbuzz, \
 -    external/harfbuzz/clang-cl.patch \
 -    external/harfbuzz/ubsan.patch \
--))
--
++    external/harfbuzz/src-hb.patch \
+ ))
+ 
  ifneq ($(ENABLE_RUNTIME_OPTIMIZATIONS),TRUE)
- $(eval $(call gb_UnpackedTarball_add_patches,harfbuzz, \
-     external/harfbuzz/harfbuzz-rtti.patch \
