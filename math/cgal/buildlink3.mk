24fb56a3bae321bc1ee06fdbb193019c64801a06
adam <adam@pkgsrc.org>
2018-12-13 19:51:31 +0000

    revbump for boost 1.69.0

--- math/cgal/buildlink3.mk
+++ math/cgal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.33 2018/08/16 18:54:30 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2018/12/13 19:51:35 adam Exp $
 
 BUILDLINK_TREE+=	cgal
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	cgal
 CGAL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cgal+=	cgal>=3.9
-BUILDLINK_ABI_DEPENDS.cgal+=	cgal>=3.9nb35
+BUILDLINK_ABI_DEPENDS.cgal+=	cgal>=3.9nb36
 BUILDLINK_PKGSRCDIR.cgal?=	../../math/cgal
 
 .include "../../devel/boost-headers/buildlink3.mk"
--- security/liboauth/buildlink3.mk
+++ security/liboauth/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2018/08/16 18:54:31 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2018/12/13 19:51:36 adam Exp $
 
 BUILDLINK_TREE+=	liboauth
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	liboauth
 LIBOAUTH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.liboauth+=	liboauth>=0.8.9
-BUILDLINK_ABI_DEPENDS.liboauth?=	liboauth>=1.0.3nb9
+BUILDLINK_ABI_DEPENDS.liboauth?=	liboauth>=1.0.3nb10
 BUILDLINK_PKGSRCDIR.liboauth?=		../../security/liboauth
 
 .include "../../security/openssl/buildlink3.mk"
