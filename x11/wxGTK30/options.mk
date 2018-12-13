f7d1cded49a7a50d8981b508bfcadc6bd929f2af
adam <adam@pkgsrc.org>
2018-12-13 21:21:55 +0000

    wxGTK30: allow building on Darwin

--- x11/wxGTK30/options.mk
+++ x11/wxGTK30/options.mk
@@ -1,27 +1,18 @@
-# $NetBSD: options.mk,v 1.1 2018/12/08 10:47:17 bouyer Exp $
+# $NetBSD: options.mk,v 1.2 2018/12/13 21:21:55 adam Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.wxGTK30
-.if ${OPSYS} != "Darwin"
-PKG_OPTIONS_REQUIRED_GROUPS= gtk
-PKG_OPTIONS_GROUP.gtk= gtk2 gtk3
-PKG_SUGGESTED_OPTIONS= gtk3
-.endif
+PKG_OPTIONS_VAR=		PKG_OPTIONS.wxGTK30
+PKG_OPTIONS_REQUIRED_GROUPS=	gtk
+PKG_OPTIONS_GROUP.gtk=		gtk2 gtk3
+PKG_SUGGESTED_OPTIONS=		gtk3
 
 .include "../../mk/bsd.options.mk"
 
-.if ${OPSYS} == "Darwin"
-CONFIGURE_ARGS+=	--with-osx_cocoa
-CONFIGURE_ARGS+=	--without-macosx-sdk
-CONFIGURE_ARGS+=	--without-macosx-version-min
-CPPFLAGS+=		-D__ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES=1
-.else
-.  if !empty(PKG_OPTIONS:Mgtk2)
+.if !empty(PKG_OPTIONS:Mgtk2)
 PLIST_SRC+=		PLIST.gtk2
 CONFIGURE_ARGS+=	--with-gtk=2
 .include "../../x11/gtk2/buildlink3.mk"
-. else
+.else
 PLIST_SRC+=		PLIST.gtk3
 CONFIGURE_ARGS+=	--with-gtk=3
 .include "../../x11/gtk3/buildlink3.mk"
-. endif
 .endif
