dc7bbcabb798b6c302118608a2f45a5189ce307f
adam <adam@pkgsrc.org>
2018-12-13 21:21:55 +0000

    wxGTK30: allow building on Darwin

--- x11/wxGTK30/buildlink3.mk
+++ x11/wxGTK30/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2018/12/08 10:47:17 bouyer Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2018/12/13 21:21:55 adam Exp $
 
 BUILDLINK_TREE+=	wxGTK30
 
@@ -17,7 +17,6 @@ CONFIGURE_ARGS+=		--with-wx-config=${PREFIX}/libexec/wx-3.0/wx-config
 pkgbase := wxGTK30
 .include "../../mk/pkg-build-options.mk"
 
-
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
@@ -26,11 +25,11 @@ pkgbase := wxGTK30
 .if ${OPSYS} != "Darwin"
 .include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../graphics/glu/buildlink3.mk"
-.if !empty(PKG_BUILD_OPTIONS.wxGTK30:Mgtk2)
+.  if !empty(PKG_BUILD_OPTIONS.wxGTK30:Mgtk2)
 .include "../../x11/gtk2/buildlink3.mk"
-.else
+.  else
 .include "../../x11/gtk3/buildlink3.mk"
-.endif
+.  endif
 .include "../../x11/libSM/buildlink3.mk"
 .include "../../x11/libXxf86vm/buildlink3.mk"
 .endif
