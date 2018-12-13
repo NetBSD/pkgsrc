a84d37141fc3f70ddf28ba17d1b13845752edd28
bsiegert <bsiegert@pkgsrc.org>
2018-12-13 14:03:02 +0000

    correct directory, oops

--- security/gcr/buildlink3.mk
+++ security/gcr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/12/11 12:36:28 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/12/13 14:03:02 bsiegert Exp $
 
 BUILDLINK_TREE+=	gcr
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gcr
 GCR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gcr+=	gcr>=3.18.0
-BUILDLINK_PKGSRCDIR.gcr?=	../../wip/gcr
+BUILDLINK_PKGSRCDIR.gcr?=	../../security/gcr
 
 BUILDLINK_API_DEPENDS.glib2+=	glib2>=2.38
 .include "../../devel/glib2/buildlink3.mk"
