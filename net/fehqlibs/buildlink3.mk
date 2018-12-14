bd8cca7ba6213adaf0dee9be9ea5c7b405f1c424
schmonz <schmonz@pkgsrc.org>
2018-12-14 22:51:32 +0000

    Update to fehQlibs-10 (versioned here as "0.9.10" to compare newer than the previous 0.9). From the changelog:
    - Added IPV6_V6ONLY setsockoptions call.
    - Improved poll handling in iopause.c considering poll RC.
    - Added building of shared libraries + updated docs.
    
    pkgsrc does not yet build shared libraries.

--- net/fehqlibs/buildlink3.mk
+++ net/fehqlibs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/10/07 12:53:54 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2018/12/14 22:51:32 schmonz Exp $
 
 BUILDLINK_TREE+=	fehqlibs
 
 .if !defined(FEHQLIBS_BUILDLINK3_MK)
 FEHQLIBS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fehqlibs+=fehqlibs>=0.9
+BUILDLINK_API_DEPENDS.fehqlibs+=fehqlibs>=0.9.10
 BUILDLINK_PKGSRCDIR.fehqlibs?=	../../net/fehqlibs
 BUILDLINK_DEPMETHOD.fehqlibs?=	build
 .endif	# FEHQLIBS_BUILDLINK3_MK
