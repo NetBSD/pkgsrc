b8a739523164ff079349a9d9d8d8f83777d66e71
schmonz <schmonz@pkgsrc.org>
2018-12-16 07:02:07 +0000

    #include <sys/types.h> for pid_t. Fixes FreeBSD build.

--- devel/cpputest/patches/patch-include_CppUTest_PlatformSpecificFunctions.h
+++ devel/cpputest/patches/patch-include_CppUTest_PlatformSpecificFunctions.h
@@ -1,10 +1,19 @@
-$NetBSD: patch-include_CppUTest_PlatformSpecificFunctions.h,v 1.1 2018/06/22 15:56:56 schmonz Exp $
+$NetBSD: patch-include_CppUTest_PlatformSpecificFunctions.h,v 1.2 2018/12/16 07:02:08 schmonz Exp $
 
 Expect pid_t from fork() and waitpid().
 
 --- include/CppUTest/PlatformSpecificFunctions.h.orig	2016-05-25 05:41:47.000000000 +0000
 +++ include/CppUTest/PlatformSpecificFunctions.h
-@@ -33,8 +33,8 @@ TestOutput::WorkingEnvironment PlatformS
+@@ -28,13 +28,17 @@
+ #ifndef PLATFORMSPECIFICFUNCTIONS_H_
+ #define PLATFORMSPECIFICFUNCTIONS_H_
+ 
++#ifdef CPPUTEST_HAVE_SYS_TYPES_H
++#include <sys/types.h>
++#endif
++
+ #include "CppUTest/TestOutput.h"
+ TestOutput::WorkingEnvironment PlatformSpecificGetWorkingEnvironment();
  
  class TestPlugin;
  extern void (*PlatformSpecificRunTestInASeperateProcess)(UtestShell* shell, TestPlugin* plugin, TestResult* result);
