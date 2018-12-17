f1f39c1edbad361eae29d1b738f0576bc2b8f47d
schmonz <schmonz@pkgsrc.org>
2018-12-17 19:24:44 +0000

    Add proper autoconfery for res_close(), from maya@ (thanks!).

--- mail/libspf2/patches/patch-src_libspf2_spf__dns__resolv.c
+++ mail/libspf2/patches/patch-src_libspf2_spf__dns__resolv.c
@@ -1,16 +1,15 @@
-$NetBSD: patch-src_libspf2_spf__dns__resolv.c,v 1.1 2018/12/17 18:32:42 schmonz Exp $
+$NetBSD: patch-src_libspf2_spf__dns__resolv.c,v 1.2 2018/12/17 19:24:44 schmonz Exp $
 
 OpenBSD's libc resolver provides res_init() but not res_close().
 
---- src/libspf2/spf_dns_resolv.c.orig	Sat Jan 28 08:24:47 2012
+--- src/libspf2/spf_dns_resolv.c.orig	2012-01-28 08:24:47.000000000 +0000
 +++ src/libspf2/spf_dns_resolv.c
-@@ -607,7 +607,9 @@ SPF_dns_resolv_free(SPF_dns_server_t *spf_dns_server)
+@@ -606,7 +606,7 @@ SPF_dns_resolv_free(SPF_dns_server_t *sp
+ {
  	SPF_ASSERT_NOTNULL(spf_dns_server);
  
- #if ! HAVE_DECL_RES_NINIT
-+#  ifndef __OpenBSD__
+-#if ! HAVE_DECL_RES_NINIT
++#if HAVE_DECL_RES_CLOSE
  	res_close();
-+#  endif
  #endif
  
- 	free(spf_dns_server);
