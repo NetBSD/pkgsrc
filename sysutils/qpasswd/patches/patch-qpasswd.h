fc66e1d42dca9021221ff31825d3222dd6bdb450
schmonz <schmonz@pkgsrc.org>
2018-12-17 16:32:18 +0000

    Handle _XOPEN_SOURCE on SunOS. Should fix SmartOS build.

--- sysutils/qpasswd/patches/patch-qpasswd.h
+++ sysutils/qpasswd/patches/patch-qpasswd.h
@@ -1,10 +1,25 @@
-$NetBSD: patch-qpasswd.h,v 1.1 2018/08/05 15:00:46 schmonz Exp $
+$NetBSD: patch-qpasswd.h,v 1.2 2018/12/17 16:32:19 schmonz Exp $
 
-Avoid Linuxisms.
+Avoid Linuxisms. Handle _XOPEN_SOURCE on SunOS.
 
 --- qpasswd.h.orig	2004-01-13 16:10:18.000000000 +0000
 +++ qpasswd.h
-@@ -8,14 +8,13 @@
+@@ -1,21 +1,28 @@
+ #ifndef QP_QPASSWD_H
+ #define QP_QPASSWD_H
+ 
++#if defined(__sun) && !defined(_XOPEN_SOURCE)
++#  if (__STDC_VERSION__-0 < 199901L)
++#define _XOPEN_SOURCE 500
++#  else
++#define _XOPEN_SOURCE 600
++#  endif
++#else
+ #define _XOPEN_SOURCE  /* needed for crypt(3) */
++#endif
+ #define _BSD_SOURCE    /* needed by initgroups(3) + fchdir(2) */
+ 
+ #include <sys/types.h>
  #include <sys/time.h>
  #include <sys/stat.h>
  #include <sys/mman.h>
