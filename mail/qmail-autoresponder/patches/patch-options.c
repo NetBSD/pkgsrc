684c3c981fbaf50e6456c3fe1f2334c396c0f695
schmonz <schmonz@pkgsrc.org>
2018-12-17 16:15:28 +0000

    Handle _XOPEN_SOURCE on SunOS. Should fix SmartOS build.

--- mail/qmail-autoresponder/patches/patch-options.c
+++ mail/qmail-autoresponder/patches/patch-options.c
@@ -1,12 +1,20 @@
-$NetBSD: patch-options.c,v 1.1 2017/09/28 17:27:34 schmonz Exp $
+$NetBSD: patch-options.c,v 1.2 2018/12/17 16:15:29 schmonz Exp $
 
-Remove unneeded include.
+Remove unneeded include. Handle _XOPEN_SOURCE on SunOS.
 
---- options.c.orig	2017-08-22 23:38:24.000000000 +0000
+--- options.c.orig	2018-04-13 16:17:51.000000000 +0000
 +++ options.c
-@@ -1,5 +1,4 @@
+@@ -1,5 +1,12 @@
++#if defined(__sun) && !defined(_XOPEN_SOURCE)
++#  if (__STDC_VERSION__-0 < 199901L)
++#define _XOPEN_SOURCE 500
++#  else
++#define _XOPEN_SOURCE 600
++#  endif
++#else
  #define _XOPEN_SOURCE
 -#include <mysql/mysql.h>
++#endif
  #include <stdlib.h>
  #include <string.h>
  #include <time.h>
