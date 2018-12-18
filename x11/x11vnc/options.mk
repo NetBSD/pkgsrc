0e4aaacfd7fda58676c659e2d2d9dc0c7afb1d7f
tnn <tnn@pkgsrc.org>
2018-12-18 15:53:46 +0000

    x11vnc: update to 0.9.15
    x11vnc 0.9.15 2018-02-04
      Misc. buffer overflow and memleak fixes.
      Support for OpenSSL 1.1.0.
      Fix for Debian bug #672435.
    
    x11vnc 0.9.14 2015-11-14
      Added support for the X Composite Extension.
      Added XInput 2 multi-pointer support
      Separated x11vnc sources from LibVNCServer.
      The project is now hosted at GitHub.

--- x11/x11vnc/options.mk
+++ x11/x11vnc/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.5 2012/11/08 12:56:21 sborrill Exp $
+# $NetBSD: options.mk,v 1.6 2018/12/18 15:53:46 tnn Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.x11vnc
-PKG_SUPPORTED_OPTIONS=	avahi inet6 x11
-PKG_SUGGESTED_OPTIONS=	inet6 x11
+PKG_SUPPORTED_OPTIONS=	avahi x11
+PKG_SUGGESTED_OPTIONS=	x11
 
 .include "../../mk/bsd.options.mk"
 
@@ -14,15 +14,12 @@ CONFIGURE_ARGS+=	--with-avahi=${BUILDLINK_PREFIX.avahi}
 CONFIGURE_ARGS+=	--without-avahi
 .endif
 
-.if empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGS+=	--without-ipv6
-CPPFLAGS+=		-DX11VNC_IPV6=0
-.endif
-
 .if !empty(PKG_OPTIONS:Mx11)
 CONFIGURE_ARGS+=		--with-x
 BUILDLINK_DEPMETHOD.libXt?=	build
 .include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXcomposite/buildlink3.mk"
+.include "../../x11/libXcursor/buildlink3.mk"
 .include "../../x11/libXdamage/buildlink3.mk"
 .include "../../x11/libXext/buildlink3.mk"
 .include "../../x11/libXfixes/buildlink3.mk"
