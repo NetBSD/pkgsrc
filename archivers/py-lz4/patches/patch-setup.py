cbe195ee74e054d54fd967455f00d87a51c9db73
adam <adam@pkgsrc.org>
2018-12-18 12:02:34 +0000

    py-lz4: updated to 2.1.5
    2.1.5
    This release contains no functional changes other than changes to the Appveyor configuration for publishing wheels.
    
    2.1.4
    This release contains no functional changes other than changes to the Travis configuration for publishing wheels.
    
    2.1.3
    A simplification of the tox.ini file
    More robust checking for pkgconfig availability
    Integration of cibuildwheel into travis builds so as to build and publish binary wheels for Linux and OSX
    Only require pytest-runner if pytest/test is being called
    Blacklists version 3.3.0 of pytest which has a bug that can cause the tests to fail.

--- archivers/py-lz4/patches/patch-setup.py
+++ archivers/py-lz4/patches/patch-setup.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-setup.py,v 1.1 2017/12/31 18:48:57 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2018/12/18 12:02:34 adam Exp $
 
 Prefer external compiler optimizations.
 
---- setup.py.orig	2017-12-31 18:34:27.000000000 +0000
+--- setup.py.orig	2018-12-18 02:40:14.000000000 +0000
 +++ setup.py
-@@ -91,7 +91,6 @@ elif compiler in ('unix', 'mingw32'):
-             extra_compile_args.append(pkgconfig.cflags('liblz4'))
+@@ -100,7 +100,6 @@ elif compiler in ('unix', 'mingw32'):
+             extra_compile_args.append(pkgconfig_cflags('liblz4'))
      else:
          extra_compile_args = [
 -            '-O3',
