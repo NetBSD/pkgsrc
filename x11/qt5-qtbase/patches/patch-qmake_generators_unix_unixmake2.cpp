8435bcddf70f8e4e8c56066722994ebe9b210fec
adam <adam@pkgsrc.org>
2018-12-18 09:57:17 +0000

    qt5: updated to 5.12.0
    Just in time for the end of the year, we have released Qt 5.12 LTS today. This is a long-term-supported (LTS) release that we will support for 3 years to come. We have had a strong focus on quality and fixed more than 2000 bugs since the last Qt LTS version, Qt 5.9.7 – make that over 5000 bugfixes since Qt 5.6.3. Of course, this is only the start, and we will work hard on continuously improving the quality of Qt 5.12 in upcoming patches

--- x11/qt5-qtbase/patches/patch-qmake_generators_unix_unixmake2.cpp
+++ x11/qt5-qtbase/patches/patch-qmake_generators_unix_unixmake2.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-qmake_generators_unix_unixmake2.cpp,v 1.3 2018/01/17 19:30:47 markd Exp $
+$NetBSD: patch-qmake_generators_unix_unixmake2.cpp,v 1.4 2018/12/18 09:57:18 adam Exp $
 
 Append external variables.
 
---- qmake/generators/unix/unixmake2.cpp.orig	2017-05-26 12:43:31.000000000 +0000
+--- qmake/generators/unix/unixmake2.cpp.orig	2018-12-03 11:15:26.000000000 +0000
 +++ qmake/generators/unix/unixmake2.cpp
-@@ -169,12 +169,12 @@ UnixMakefileGenerator::writeMakeParts(QT
+@@ -183,12 +183,12 @@ UnixMakefileGenerator::writeMakeParts(QT
      t << "####### Compiler, tools and options\n\n";
      t << "CC            = " << var("QMAKE_CC") << endl;
      t << "CXX           = " << var("QMAKE_CXX") << endl;
@@ -21,14 +21,14 @@ Append external variables.
      {
          QString isystem = var("QMAKE_CFLAGS_ISYSTEM");
          const ProStringList &incs = project->values("INCLUDEPATH");
-@@ -198,8 +198,8 @@ UnixMakefileGenerator::writeMakeParts(QT
+@@ -212,8 +212,8 @@ UnixMakefileGenerator::writeMakeParts(QT
  
      if(!project->isActiveConfig("staticlib")) {
          t << "LINK          = " << var("QMAKE_LINK") << endl;
 -        t << "LFLAGS        = " << var("QMAKE_LFLAGS") << endl;
--        t << "LIBS          = $(SUBLIBS) " << fixLibFlags("QMAKE_LIBS").join(' ') << ' '
+-        t << "LIBS          = $(SUBLIBS) " << fixLibFlags("LIBS").join(' ') << ' '
 +        t << "LFLAGS       += " << var("QMAKE_LFLAGS") << endl;
-+        t << "LIBS         += $(SUBLIBS) " << fixLibFlags("QMAKE_LIBS").join(' ') << ' '
++        t << "LIBS         += $(SUBLIBS) " << fixLibFlags("LIBS").join(' ') << ' '
+                                            << fixLibFlags("LIBS_PRIVATE").join(' ') << ' '
+                                            << fixLibFlags("QMAKE_LIBS").join(' ') << ' '
                                             << fixLibFlags("QMAKE_LIBS_PRIVATE").join(' ') << endl;
-     }
- 
