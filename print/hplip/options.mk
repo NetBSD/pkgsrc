99d4998092ce02960ec4e13be49439865ebd8e89
nia <nia@pkgsrc.org>
2018-12-19 22:22:59 +0000

    hplip: Set PYTHON_VERSIONS_INCOMPATIBLE before including py-dbus's buildlink3.

--- print/hplip/options.mk
+++ print/hplip/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.14 2018/07/03 05:03:30 adam Exp $
+# $NetBSD: options.mk,v 1.15 2018/12/19 22:22:59 nia Exp $
 #
 # HPLIP dependencies are detailed in the following page:
 # http://hplipopensource.com/hplip-web/install/manual/distros/other.html
@@ -14,6 +14,32 @@ PKG_OPTIONS_LEGACY_OPTS+=	gui:qt4
 PKG_OPTIONS_LEGACY_OPTS+=	qt:qt4
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mqt4) || !empty(PKG_OPTIONS:Mqt5)
+PYTHON_VERSIONS_INCOMPATIBLE=	34 35 36 37 # py-notify
+CONFIGURE_ARGS+=	--enable-gui-build
+CONFIGURE_ARGS+=	--enable-policykit
+EGFILES+=		dbus-1/system.d/com.hp.hplip.conf
+MAKE_DIRS+=		${PKG_SYSCONFDIR}/dbus-1/system.d
+.include "../../security/policykit/buildlink3.mk"
+.include "../../sysutils/desktop-file-utils/desktopdb.mk"
+DEPENDS+=	${PYPKGPREFIX}-notify-[0-9]*:../../sysutils/py-notify
+. if !empty(PKG_OPTIONS:Mqt4)
+PLIST_SRC+=		PLIST.qt4
+CONFIGURE_ARGS+=	--enable-qt4
+.  include "../../x11/py-qt4/buildlink3.mk"
+. endif
+. if !empty(PKG_OPTIONS:Mqt5)
+PLIST_SRC+=		PLIST.qt5
+CONFIGURE_ARGS+=	--enable-qt5
+.  include "../../x11/py-qt5/buildlink3.mk"
+. endif
+.else
+CONFIGURE_ARGS+=	--disable-policykit
+CONFIGURE_ARGS+=	--disable-qt4
+CONFIGURE_ARGS+=	--disable-qt5
+CONFIGURE_ARGS+=	--disable-gui-build
+.endif
 
 .if !empty(PKG_OPTIONS:Mfax)
 PLIST_SRC+=		PLIST.fax
@@ -38,30 +64,4 @@ MESSAGE_SUBST+=		EGDIR=${EGDIR}
 DEPENDS+=	{${PYPKGPREFIX}-Pillow-[0-9]*,${PYPKGPREFIX}-imaging-[0-9]*}:../../graphics/py-Pillow
 .else
 CONFIGURE_ARGS+=	--disable-scan-build
-.endif
-
-.if !empty(PKG_OPTIONS:Mqt4) || !empty(PKG_OPTIONS:Mqt5)
-CONFIGURE_ARGS+=	--enable-gui-build
-CONFIGURE_ARGS+=	--enable-policykit
-EGFILES+=		dbus-1/system.d/com.hp.hplip.conf
-MAKE_DIRS+=		${PKG_SYSCONFDIR}/dbus-1/system.d
-.include "../../security/policykit/buildlink3.mk"
-.include "../../sysutils/desktop-file-utils/desktopdb.mk"
-DEPENDS+=	${PYPKGPREFIX}-notify-[0-9]*:../../sysutils/py-notify
-PYTHON_VERSIONS_INCOMPATIBLE=	34 35 36 37 # py-notify
-. if !empty(PKG_OPTIONS:Mqt4)
-PLIST_SRC+=		PLIST.qt4
-CONFIGURE_ARGS+=	--enable-qt4
-.  include "../../x11/py-qt4/buildlink3.mk"
-. endif
-. if !empty(PKG_OPTIONS:Mqt5)
-PLIST_SRC+=		PLIST.qt5
-CONFIGURE_ARGS+=	--enable-qt5
-.  include "../../x11/py-qt5/buildlink3.mk"
-. endif
-.else
-CONFIGURE_ARGS+=	--disable-policykit
-CONFIGURE_ARGS+=	--disable-qt4
-CONFIGURE_ARGS+=	--disable-qt5
-CONFIGURE_ARGS+=	--disable-gui-build
 .endif
