6d3fe60e3515e0878d9d356892ed56f97d998b04
wiz <wiz@pkgsrc.org>
2018-12-21 08:46:16 +0000

    inkscape: get this closer to building with poppler-0.72
    More work to do.

--- graphics/inkscape/patches/patch-src_extension_internal_pdfinput_pdf-parser.h
+++ graphics/inkscape/patches/patch-src_extension_internal_pdfinput_pdf-parser.h
@@ -1,15 +1,27 @@
-$NetBSD: patch-src_extension_internal_pdfinput_pdf-parser.h,v 1.2 2015/02/03 13:55:12 adam Exp $
+$NetBSD: patch-src_extension_internal_pdfinput_pdf-parser.h,v 1.3 2018/12/21 08:46:16 wiz Exp $
 
 Explicitly include poppler/Object.h; otherwise Python's Object.h gets included.
 
---- src/extension/internal/pdfinput/pdf-parser.h.orig	2012-11-18 14:42:04.000000000 +0000
+--- src/extension/internal/pdfinput/pdf-parser.h.orig	2017-08-06 20:44:00.000000000 +0000
 +++ src/extension/internal/pdfinput/pdf-parser.h
-@@ -26,7 +26,7 @@ namespace Inkscape {
+@@ -25,8 +25,18 @@ namespace Inkscape {
+ // TODO clean up and remove using:
  using Inkscape::Extension::Internal::SvgBuilder;
  
- #include "goo/gtypes.h"
+-#include "goo/gtypes.h"
 -#include "Object.h"
 +#include "poppler/Object.h"
++
++// poppler 0.72
++#define getCString c_str
++
++// poppler 0.71
++typedef bool GBool;
++#define gTrue true
++#define gFalse false  
++
++// poppler 0.70
++#define _POPPLER_CONST const
  
  class GooString;
  class XRef;
