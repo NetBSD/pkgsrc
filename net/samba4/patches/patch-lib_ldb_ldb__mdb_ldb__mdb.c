d9e2b6b8e4b57c71094693cd3f57a268cbf88463
adam <adam@pkgsrc.org>
2018-12-22 01:13:52 +0000

    samba4: buidling fixes
    PkgSrc changes:
    * fix building on Darwin and probably other systems as well
    * install manpages
    * use correct install_name on Darwin
    * does not collide with p5-Parse-Yapp anymore
    * use cmocka and libgcrypt
    * clean-ups

--- net/samba4/patches/patch-lib_ldb_ldb__mdb_ldb__mdb.c
+++ net/samba4/patches/patch-lib_ldb_ldb__mdb_ldb__mdb.c
@@ -1,4 +1,6 @@
-$NetBSD: patch-lib_ldb_ldb__mdb_ldb__mdb.c,v 1.1 2018/11/23 07:30:02 ryoon Exp $
+$NetBSD: patch-lib_ldb_ldb__mdb_ldb__mdb.c,v 1.2 2018/12/22 01:13:52 adam Exp $
+
+Some systems do not define EBADE.
 
 --- lib/ldb/ldb_mdb/ldb_mdb.c.orig	2018-07-12 08:23:36.000000000 +0000
 +++ lib/ldb/ldb_mdb/ldb_mdb.c
@@ -6,7 +8,7 @@ $NetBSD: patch-lib_ldb_ldb__mdb_ldb__mdb.c,v 1.1 2018/11/23 07:30:02 ryoon Exp $
  		return LDB_SUCCESS;
  	case EIO:
  		return LDB_ERR_OPERATIONS_ERROR;
-+#if !defined(__NetBSD__)
++#ifdef EBADE
  	case EBADE:
 +#endif
  	case MDB_INCOMPATIBLE:
