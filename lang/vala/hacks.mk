6a51ee7e3a267851be1398f11ac512b906a015bc
tsutsui <tsutsui@pkgsrc.org>
2018-12-24 14:45:50 +0000

    vala: apply the same hack as PR pkg/53743 for gcc 6.5.0 for NetBSD/evbarm.
    Reported by bouyer@:
     http://mail-index.netbsd.org/pkgsrc-changes/2018/12/24/msg185017.html
    Bump PKGREVISION again.

--- lang/vala/hacks.mk
+++ lang/vala/hacks.mk
@@ -1,15 +1,16 @@
-# $NetBSD: hacks.mk,v 1.1 2018/12/24 00:10:09 tsutsui Exp $
+# $NetBSD: hacks.mk,v 1.2 2018/12/24 14:45:50 tsutsui Exp $
 
 .if !defined(VALA_HACKS_MK)
 VALA_HACKS_MK=	defined
 
 .include "../../mk/compiler.mk"
 
-### On NetBSD/earmv6hf 8.0 with gcc 5.5.0, vala binaries compiled with the
-### default optiomization options produce segmentation faulting.
+### On NetBSD/earmv6hf 8.0 with gcc 5.5.0 and 8.99.25 with gcc 6.5.0,
+### vala binaries compiled with the default optiomization options produce
+### segmentation faulting.
 ### Compiling with `-fno-optimize-sibling-calls' works around.
 ###
-.if !empty(MACHINE_PLATFORM:MNetBSD-*-*arm*) && !empty(CC_VERSION:Mgcc-5.*)
+.if !empty(MACHINE_PLATFORM:MNetBSD-*-*arm*) && !empty(CC_VERSION:Mgcc-[56].*)
 PKG_HACKS+=	optimisation
 CFLAGS+=	-fno-optimize-sibling-calls
 .endif
