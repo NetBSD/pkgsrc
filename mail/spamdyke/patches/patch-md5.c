517f5a9b864761d1772ad7b993b7af4b24188f4c
schmonz <schmonz@pkgsrc.org>
2018-12-25 16:24:26 +0000

    Sprinkle static on an inline. Fixes SmartOS and Ubuntu bulk builds (that somehow don't have any -On in CFLAGS).

--- mail/spamdyke/patches/patch-md5.c
+++ mail/spamdyke/patches/patch-md5.c
@@ -1,9 +1,18 @@
-$NetBSD: patch-md5.c,v 1.1 2017/07/09 16:16:32 schmonz Exp $
+$NetBSD: patch-md5.c,v 1.2 2018/12/25 16:24:26 schmonz Exp $
 
 Support PKGSRC_USE_SSP.
 
 --- md5.c.orig	2015-04-30 00:53:36.000000000 +0000
 +++ md5.c
+@@ -128,7 +128,7 @@ unsigned char *htole(unsigned char *dest
+  * Return value:
+  *   X circular left-shifted n positions
+  */
+-inline uint32_t lshift(uint32_t X, int n)
++static inline uint32_t lshift(uint32_t X, int n)
+   {
+   return((n > 0) ? ((X << n) | (X >> (32 - n))) : X);
+   }
 @@ -165,7 +165,7 @@ unsigned char *md5(unsigned char destina
    if (pad_len <= 0)
      pad_len += 64;
