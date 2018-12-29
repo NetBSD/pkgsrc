6a24fde8aa0d01488ea6ad23693e5fb3cdff5c72
triaxx <triaxx@pkgsrc.org>
2018-12-29 13:38:53 +0000

    MesaLib: add headers for major()/minor() on Linux
    Bump revision

--- graphics/MesaLib/patches/patch-src_gbm_main_gbm.c
+++ graphics/MesaLib/patches/patch-src_gbm_main_gbm.c
@@ -1,15 +1,18 @@
-$NetBSD: patch-src_gbm_main_gbm.c,v 1.1 2015/04/25 11:19:18 tnn Exp $
+$NetBSD: patch-src_gbm_main_gbm.c,v 1.2 2018/12/29 13:38:53 triaxx Exp $
 
-Solaris needs sys/mkdev.h for major(2).
+* Solaris needs sys/mkdev.h for major(2).
+* Linux needs sys/sysmacros.h for major(2).
 
---- src/gbm/main/gbm.c.orig	2015-03-21 00:51:18.000000000 +0000
+--- src/gbm/main/gbm.c.orig	2016-05-01 12:48:59.000000000 +0000
 +++ src/gbm/main/gbm.c
-@@ -35,6 +35,9 @@
+@@ -35,6 +35,11 @@
  #include <stdint.h>
  
  #include <sys/types.h>
 +#ifdef __sun
 +#include <sys/mkdev.h> /* for major(2) */
++#elif defined(__linux__)
++#include <sys/sysmacros.h>
 +#endif
  #include <sys/stat.h>
  #include <unistd.h>
