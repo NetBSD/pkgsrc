f2d8ada7c909edb387236c904e703bfd49ff40b0
ryoon <ryoon@pkgsrc.org>
2018-12-31 13:11:12 +0000

    Update to 1.0.4
    * Use FreeBSD patches to fix build and startup crash
    
    Changelog:
    I could not find it.

--- graphics/cinepaint/patches/patch-app_layers__dialog.c
+++ graphics/cinepaint/patches/patch-app_layers__dialog.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-app_layers__dialog.c,v 1.1 2012/07/03 18:08:21 joerg Exp $
+$NetBSD: patch-app_layers__dialog.c,v 1.2 2018/12/31 13:11:12 ryoon Exp $
 
---- app/layers_dialog.c.orig	2012-07-03 13:55:55.000000000 +0000
+--- app/layers_dialog.c.orig	2007-11-09 15:24:27.000000000 +0000
 +++ app/layers_dialog.c
-@@ -2769,7 +2769,7 @@ layer_widget_preview_redraw (LayerWidget
+@@ -2773,7 +2773,7 @@ layer_widget_preview_redraw (LayerWidget
  # ifdef DEBUG
      printf("%s:%d %s() wrong data or widget\n", __FILE__,__LINE__,__func__);
  # endif
