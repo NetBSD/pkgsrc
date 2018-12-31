3319e9e8e3ccbf0f08856434bc0104c4dfd9a750
rhialto <rhialto@pkgsrc.org>
2018-12-31 15:38:55 +0000

    emulators/vice: update to 3.3.
    * Changes in VICE 3.3
    =====================
    
    This release stabilizes and introduces the GTK3 UI as the primary user interface
    for all major ports. For "lesser" platforms we still have the SDL1/SDL2 based
    interface. All other user interfaces have been removed. RIP
    
    ** General
    ----------
    
    - Old, unmaintained, ports removed:
      * MacOS Cocoa: use SDL or Gtk3
      * WinVice: use SDL or Gtk3
      * BeOS/Haiku: use SDL
      * AmigaOS: use SDL
      * Unix Xaw: use SDL or Gtk3
      * Unix Gtk2: use SDL or Gtk3
      * OS/2: use SDL
    
    - Fix building against external ffmpeg >= 4.0
    
    - Fix errors in multi-SID code
    
    - added single SNES pad emulation (compatible to Ninjas adapter/TrapThem64)
    
    - Make VICE much faster by using -O3 vs -O2
    
    - add a slot number before the literal name of a game-controller, which is less
      confusing when more than one controller of the same type is used
    
    - fix parsing of the commandline with -config first
    
    - added dutch to list of keyboard mappings
    
    (more details in the included share/doc/vice/NEWS file)

--- emulators/vice/options.mk
+++ emulators/vice/options.mk
@@ -1,41 +1,30 @@
-# $NetBSD: options.mk,v 1.9 2018/07/14 16:05:19 maya Exp $
+# $NetBSD: options.mk,v 1.10 2018/12/31 15:38:55 rhialto Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.vice
-PKG_SUPPORTED_OPTIONS=		ffmpeg vte
+PKG_SUPPORTED_OPTIONS=		ffmpeg
 PKG_OPTIONS_REQUIRED_GROUPS=	gui
-PKG_OPTIONS_GROUP.gui=		gtk2 gtk3wip sdl xaw
-PKG_SUGGESTED_OPTIONS=		gtk2
+PKG_OPTIONS_GROUP.gui=		gtk3 sdl sdl2
+PKG_SUGGESTED_OPTIONS=		gtk3 ffmpeg
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	mo pcf sdl x11
+PLIST_VARS+=	pcf sdl x11
 
-# Cocoa user interface works only in application bundle.
-#.if !empty(PKG_OPTIONS:Mcocoa)
-#CONFIGURE_ARGS+=	--disable-bundle
-#CONFIGURE_ARGS+=	--with-cocoa
-#PLIST.x11=		yes
-#INSTALL_TARGET=		bindist
-#.else
-USE_TOOLS+=		bdftopcf
-#.endif
-
-.if !empty(PKG_OPTIONS:Mgtk2)
-CONFIGURE_ARGS+=	--enable-gnomeui
-PLIST.mo=		yes
-PLIST.pcf=		yes
-PLIST.x11=		yes
-.  include "../../x11/gtk2/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mgtk3wip)
+.if !empty(PKG_OPTIONS:Mgtk3)
 CONFIGURE_ARGS+=	--enable-native-gtk3ui
 PLIST.pcf=		yes
 PLIST.x11=		yes
 .  include "../../x11/gtk3/buildlink3.mk"
+TOOL_DEPENDS+=		glib2-tools>=2.56:../../devel/glib2-tools
 .endif
 
 .if !empty(PKG_OPTIONS:Msdl)
+CONFIGURE_ARGS+=	--enable-sdlui
+PLIST.sdl=		yes
+.  include "../../devel/SDL/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Msdl2)
 CONFIGURE_ARGS+=	--enable-sdlui2
 PLIST.sdl=		yes
 .  include "../../devel/SDL2/buildlink3.mk"
@@ -47,25 +36,4 @@ PLIST.sdl=		yes
 #BUILDLINK_DEPMETHOD.ffmpeg?=	build
 CONFIGURE_ARGS+=	--enable-external-ffmpeg
 .  include "../../multimedia/ffmpeg3/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mvte)
-.  if !empty(PKG_OPTIONS:Mgtk2) || !empty(PKG_OPTIONS:Mgtk3wip)
-.    include "../../x11/vte/buildlink3.mk"
-.  else
-#    vte only makes sense with gtk*
-.  endif
-.endif
-
-.if !empty(PKG_OPTIONS:Mxaw)
-PLIST.mo=		yes
-PLIST.pcf=		yes
-PLIST.x11=		yes
-.  include "../../x11/libXt/buildlink3.mk"
-.  include "../../x11/libXmu/buildlink3.mk"
-.  include "../../x11/libXaw/buildlink3.mk"
-.  include "../../x11/libXpm/buildlink3.mk"
-.  include "../../x11/libXrandr/buildlink3.mk"
-.  include "../../x11/libXv/buildlink3.mk"
-.  include "../../x11/libXxf86vm/buildlink3.mk"
 .endif
