6b90bd232995f41f755bd9424b07b58b3162df06
nia <nia@pkgsrc.org>
2019-01-02 13:24:28 +0000

    emulators/libretro-mame2010: Import version libretro-mame2010-20180417
    Libretro is a simple but powerful development interface that allows for
    the easy creation of emulators, games and multimedia applications that
    can plug straight into any libretro-compatible frontend. This development
    interface is open to others so that they can run these pluggable emulator
    and game cores also in their own programs or devices.
    
    MAME is the Multiple Arcade Machine Emulator. This is a port of MAME
    0.139 to libretro with backported improvements and is compatible with
    MAME 0.139 romsets.

---
+++ emulators/libretro-mame2010/patches/patch-src_emu_emucore.h
@@ -0,0 +1,22 @@
+$NetBSD: patch-src_emu_emucore.h,v 1.1 2019/01/02 13:24:29 nia Exp $
+
+Avoid colliding with libc symbols on NetBSD.
+
+--- src/emu/emucore.h.orig	2018-04-17 17:16:44.000000000 +0000
++++ src/emu/emucore.h
+@@ -502,6 +502,7 @@ inline void fatalerror_exitcode(running_
+ //**************************************************************************
+ 
+ // population count
++#ifndef __NetBSD__
+ inline int popcount(UINT32 val)
+ {
+ 	int count;
+@@ -510,6 +511,7 @@ inline int popcount(UINT32 val)
+ 		val &= val - 1;
+ 	return count;
+ }
++#endif
+ 
+ 
+ // convert a series of 32 bits into a float
