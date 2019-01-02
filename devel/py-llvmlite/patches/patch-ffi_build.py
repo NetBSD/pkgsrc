626ef62cd267b2fe673777f26cc3a09170b877a1
adam <adam@pkgsrc.org>
2019-01-02 15:42:17 +0000

    py-llvmlite: updated to 0.27.0
    v0.27.0
    
    This release updates llvmlite to LLVM 7. Note that LLVM 7.0.0 contains a critical bug that is resolved with a patch included in the llvmdev conda package recipe. The final release of LLVM 7.0.1 may also resolve the issue.
    
    Add another thread for RPi builds.
    llvm lld integration
    Build LLD as part of the llvmdev package
    Set up CI with Azure Pipelines
    LLVM 7 support

--- devel/py-llvmlite/patches/patch-ffi_build.py
+++ devel/py-llvmlite/patches/patch-ffi_build.py
@@ -1,19 +1,9 @@
-$NetBSD: patch-ffi_build.py,v 1.3 2018/12/09 20:22:55 adam Exp $
+$NetBSD: patch-ffi_build.py,v 1.4 2019/01/02 15:42:17 adam Exp $
 
-Allow newer LLVM.
 Add NetBSD support.
 
 --- ffi/build.py.orig	2018-07-10 14:46:02.000000000 +0000
 +++ ffi/build.py
-@@ -109,7 +109,7 @@ def main_posix(kind, library_ext):
- 
-     out = out.decode('latin1')
-     print(out)
--    if not out.startswith('6.0.'):
-+    if False:
-         msg = (
-             "Building llvmlite requires LLVM 6.0.x. Be sure to "
-             "set LLVM_CONFIG to the right executable path.\n"
 @@ -157,6 +157,8 @@ def main():
          main_posix('linux', '.so')
      elif sys.platform.startswith(('freebsd','openbsd')):
