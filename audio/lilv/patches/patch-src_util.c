bf9d04bd4556902b298b2e6247b2880498109c0e
bsiegert <bsiegert@pkgsrc.org>
2019-01-02 20:31:48 +0000

    Update lilv to 0.24.4.
    Lilv 0.24.4
    
      * Fix saving state when broken links are encountered
      * Don't attempt to load remote or non-Turtle files
      * lv2apply: Activate plugin before running
      * lv2apply: Use default values when they are not nan
      * lv2bench: Improve support for plugins with sequence ports
      * lv2bench: Support running a single plugin given on the command line
      * Gracefully handle plugins with missing binary URIs
      * Remove use of deprecated readdir_r
      * Install Python bindings when configured without tests (thanks Clement Skau)
    
    Lilv 0.24.2
    
      * Fix saving state to paths that contain URI delimiters (#, ?, etc)
      * Fix comparison of restored states with paths
    
    Lilv 0.24.0
    
      * Add new hand-crafted Pythonic bindings with full test coverage
      * Add lv2apply utility for applying plugins to audio files
      * Add lilv_world_get_symbol()
      * Add lilv_state_set_metadata() for adding state banks/comments/etc (based on
        patch from Hanspeter Portner)
      * Fix crash when state contains non-POD properties
      * Fix crash when NULL predicate is passed to lilv_world_find_nodes()
      * Fix state file versioning
      * Unload contained resources when bundle is unloaded
      * Do not instantiate plugin when data fails to parse
      * Support re-loading plugins
      * Replace bundles if bundle with newer plugin version is loaded (based on
        patch from Robin Gareus)
      * Fix loading dyn-manifest from bundles with spaces in their path
      * Check lv2:binary predicate for UIs
      * Add LILV_URI_ATOM_PORT and LILV_URI_CV_PORT defines
      * Fix documentation installation
      * Fix outdated comment references to lilv_uri_to_path()
    
    Lilv 0.22.0
    
      * Fix loading files with spaces in their path
      * Add lilv_file_uri_parse() for correct URI to path conversion
      * Add lilv_node_get_path() for convenient file URI path access
      * Add lilv_state_emit_port_values() for special port value handling
      * Add lilv_state_get_uri()
      * Add lilv_state_delete() for deleting user saved presets
      * Add lilv_free() for systems picky about such things
      * Fix lilv_world_ask() to work with wildcards
      * Fix creation of duplicate manifest entries when saving state
      * Fix bindings for Python 3
      * Load discovered owl ontologies as specifications
      * Expose lilv_world_load_specifications() and lilv_world_load_plugin_classes
        ()
      * Tolerate passing NULL to lilv_state_restore()
      * Preserve absolute paths in state if no link directory is given
      * Fix a few minor/unlikely memory errors
      * Configure based on compiler target OS for cross-compilation
      * Fix lilv_realpath() on pre-POSIX-2008 systems
      * Fix directory walking on some systems (thanks Matt Fischer)
      * Windows fixes (thanks John Emmas)
      * Minor documentation improvements
      * Upgrade to waf 1.8.14

--- audio/lilv/patches/patch-src_util.c
+++ audio/lilv/patches/patch-src_util.c
@@ -1,16 +1,16 @@
-$NetBSD: patch-src_util.c,v 1.1 2018/07/31 15:35:38 jperkin Exp $
+$NetBSD: patch-src_util.c,v 1.2 2019/01/02 20:31:48 bsiegert Exp $
 
 Set _POSIX_C_SOURCE correctly.
 
---- src/util.c.orig	2014-08-08 14:40:03.000000000 +0000
+--- src/util.c.orig	2018-07-21 08:10:18.000000000 +0000
 +++ src/util.c
 @@ -14,7 +14,9 @@
    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
  
 +#if !defined(__sun) || (__STDC_VERSION__-0 < 199901L)
- #define _POSIX_C_SOURCE 1  /* for fileno */
+ #define _POSIX_C_SOURCE 200809L  /* for fileno */
 +#endif
- #define _BSD_SOURCE     1  /* for realpath, symlink */
+ #define _BSD_SOURCE     1        /* for realpath, symlink */
+ #define _DEFAULT_SOURCE 1        /* for realpath, symlink */
  
- #ifdef __APPLE__
