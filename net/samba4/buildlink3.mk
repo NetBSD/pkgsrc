7ef3bb494b8bcac9dd327f816bba99a76592222e
adam <adam@pkgsrc.org>
2019-01-03 19:36:45 +0000

    samba4: use external tevent and tdb; bump revision

--- net/samba4/buildlink3.mk
+++ net/samba4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/12/22 01:13:52 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/01/03 19:36:45 adam Exp $
 
 BUILDLINK_TREE+=	samba
 
@@ -11,11 +11,13 @@ BUILDLINK_PKGSRCDIR.samba?=	../../net/samba4
 .include "../../archivers/libarchive/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../databases/lmdb/buildlink3.mk"
+.include "../../databases/tdb/buildlink3.mk"
 .include "../../devel/cmocka/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/popt/buildlink3.mk"
 .include "../../devel/readline/buildlink3.mk"
 .include "../../devel/talloc/buildlink3.mk"
+.include "../../devel/tevent/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../security/gnutls/buildlink3.mk"
 .include "../../security/libgcrypt/buildlink3.mk"
