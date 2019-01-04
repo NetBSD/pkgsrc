f4678f3185040f7ec8891cefea368bd16453807b
triaxx <triaxx@pkgsrc.org>
2019-01-04 17:47:12 +0000

    Fix PR pkg/53816.
    * Fix from upstream: https://gitlab.kitware.com/cmake/cmake/issues/17614
    * Patch comment to appease pkglint
    * Bump revision

--- devel/cmake/patches/patch-Modules_FindCurses.cmake
+++ devel/cmake/patches/patch-Modules_FindCurses.cmake
@@ -1,4 +1,8 @@
-$NetBSD: patch-Modules_FindCurses.cmake,v 1.1 2017/01/05 22:34:25 joerg Exp $
+$NetBSD: patch-Modules_FindCurses.cmake,v 1.2 2019/01/04 17:47:12 triaxx Exp $
+
+Recognize libgnuform as valid implementation of a form library to match
+devel/ncurses. This is necessary due to cmake's insistance of scanning
+PREFIX/lib directly.
 
 --- Modules/FindCurses.cmake.orig	2017-01-05 22:24:12.384738412 +0000
 +++ Modules/FindCurses.cmake
