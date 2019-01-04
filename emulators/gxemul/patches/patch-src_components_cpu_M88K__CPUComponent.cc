4c29b56ea5af894c84499f7804b8464deba8355e
ryoon <ryoon@pkgsrc.org>
2019-01-04 13:35:16 +0000

    Update to 0.6.1
    * Update MASTER_SITES and HOMEPAGE
    
    Changelog:
    * Support OpenBSD/sgi 6.4
    * Add luna-88k and luna-88k2 emulations
    * Add i960 emulation stub

--- emulators/gxemul/patches/patch-src_components_cpu_M88K__CPUComponent.cc
+++ emulators/gxemul/patches/patch-src_components_cpu_M88K__CPUComponent.cc
@@ -1,13 +1,15 @@
-$NetBSD: patch-src_components_cpu_M88K__CPUComponent.cc,v 1.1 2012/11/23 12:33:22 joerg Exp $
+$NetBSD: patch-src_components_cpu_M88K__CPUComponent.cc,v 1.2 2019/01/04 13:35:16 ryoon Exp $
 
---- src/components/cpu/M88K_CPUComponent.cc.orig	2010-02-14 09:33:54.000000000 +0000
+Don't define functions returning functions.
+
+--- src/components/cpu/M88K_CPUComponent.cc.orig	2018-12-07 06:29:22.000000000 +0000
 +++ src/components/cpu/M88K_CPUComponent.cc
 @@ -337,7 +337,7 @@ int M88K_CPUComponent::GetDyntransICshif
  }
  
  
--void (*M88K_CPUComponent::GetDyntransToBeTranslated())(CPUDyntransComponent*, DyntransIC*) const
-+DyntransIC_t M88K_CPUComponent::GetDyntransToBeTranslated() const
+-void (*M88K_CPUComponent::GetDyntransToBeTranslated())(CPUDyntransComponent*, DyntransIC*)
++DyntransIC_t M88K_CPUComponent::GetDyntransToBeTranslated()
  {
  	return instr_ToBeTranslated;
  }
