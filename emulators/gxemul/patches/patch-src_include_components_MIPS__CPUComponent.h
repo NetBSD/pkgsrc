58f626b68d53a07801b082b2e453e9faacda035b
ryoon <ryoon@pkgsrc.org>
2019-01-04 13:35:16 +0000

    Update to 0.6.1
    * Update MASTER_SITES and HOMEPAGE
    
    Changelog:
    * Support OpenBSD/sgi 6.4
    * Add luna-88k and luna-88k2 emulations
    * Add i960 emulation stub

--- emulators/gxemul/patches/patch-src_include_components_MIPS__CPUComponent.h
+++ emulators/gxemul/patches/patch-src_include_components_MIPS__CPUComponent.h
@@ -1,13 +1,15 @@
-$NetBSD: patch-src_include_components_MIPS__CPUComponent.h,v 1.1 2012/11/23 12:33:22 joerg Exp $
+$NetBSD: patch-src_include_components_MIPS__CPUComponent.h,v 1.2 2019/01/04 13:35:16 ryoon Exp $
 
---- src/include/components/MIPS_CPUComponent.h.orig	2012-11-19 20:10:20.000000000 +0000
+Don't define functions returning functions.
+
+--- src/include/components/MIPS_CPUComponent.h.orig	2018-12-07 06:29:22.000000000 +0000
 +++ src/include/components/MIPS_CPUComponent.h
 @@ -196,7 +196,7 @@ protected:
  	virtual bool FunctionTraceReturnImpl(int64_t& retval);
  
  	virtual int GetDyntransICshift() const;
--	virtual void (*GetDyntransToBeTranslated())(CPUDyntransComponent*, DyntransIC*) const;
-+	virtual DyntransIC_t GetDyntransToBeTranslated() const;
+-	virtual void (*GetDyntransToBeTranslated())(CPUDyntransComponent*, DyntransIC*);
++	virtual DyntransIC_t GetDyntransToBeTranslated();
  
  	virtual void ShowRegisters(GXemul* gxemul, const vector<string>& arguments) const;
  
