e338d40041534ee988d3a14a9af503001b3f3217
ryoon <ryoon@pkgsrc.org>
2019-01-04 13:35:16 +0000

    Update to 0.6.1
    * Update MASTER_SITES and HOMEPAGE
    
    Changelog:
    * Support OpenBSD/sgi 6.4
    * Add luna-88k and luna-88k2 emulations
    * Add i960 emulation stub

--- emulators/gxemul/patches/patch-src_include_refcount__ptr.h
+++ emulators/gxemul/patches/patch-src_include_refcount__ptr.h
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_include_refcount__ptr.h,v 1.2 2014/12/08 20:55:19 ryoon Exp $
+$NetBSD: patch-src_include_refcount__ptr.h,v 1.3 2019/01/04 13:35:16 ryoon Exp $
+
+Fix compilation on Linux.
 
 --- src/include/refcount_ptr.h.orig	2014-08-17 08:45:13.000000000 +0000
 +++ src/include/refcount_ptr.h
