89aef6b699884a06c9b3d0b77799f4020887c17f
tnn <tnn@pkgsrc.org>
2019-01-05 23:29:40 +0000

    rust: add some kludges to better support NetBSD HEAD-llvm
    1) bootstrap rustc adds -lgcc_s when linking
    -> Dropped with a BUILDLINK_TRANSFORM
    
    2) bootstrap rustc has shared linkage to libgcc_s.so.1
    -> Until upstream changes this to static linkage, we look for
       libgcc_s.so.1 in ${FILESDIR} where the user must place it manually.
    
    3) newly built rustc adds -lstdc++ instead of -lc++ when linking llvm
    -> fixed with patch-src_librustc__llvm_build.rs
    
    4) newly built rustc adds -lgcc_s when linking
    -> fixed with patch-src_libunwind_build.rs

---
+++ lang/rust/patches/patch-src_libunwind_build.rs
@@ -0,0 +1,17 @@
+$NetBSD: patch-src_libunwind_build.rs,v 1.1 2019/01/05 23:29:40 tnn Exp $
+
+fix build on NetBSD HEAD-llvm. XXX there is probably a better way to do this.
+
+--- src/libunwind/build.rs.orig	2018-12-18 23:11:17.000000000 +0000
++++ src/libunwind/build.rs
+@@ -25,7 +25,9 @@ fn main() {
+     } else if target.contains("rumprun") {
+         println!("cargo:rustc-link-lib=unwind");
+     } else if target.contains("netbsd") {
+-        println!("cargo:rustc-link-lib=gcc_s");
++	if !env::var_os("PKGSRC_HAVE_LIBCPP").is_some() {
++            println!("cargo:rustc-link-lib=gcc_s");
++        }
+     } else if target.contains("openbsd") {
+         println!("cargo:rustc-link-lib=c++abi");
+     } else if target.contains("solaris") {
