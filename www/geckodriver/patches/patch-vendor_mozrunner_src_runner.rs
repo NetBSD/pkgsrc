e26d3cfec18195bad72eccf00285f11775b43b80
tnn <tnn@pkgsrc.org>
2019-01-05 03:01:30 +0000

    geckodriver: update to 0.23.0
    v0.23.0
    
    Removed:
    - The POST /session/{session id}/element/{element id}/tap endpoint
      was removed, thanks to Kerem Kat.
    Changed:
    - webdriver crate upgraded to 0.38.0.
    Fixed:
    - desiredCapabilities and requiredCapabilities are again
        recognised on session creation
    - duration field made optional on pause actions
    - Log level formatted to expected Marionette input
    - temporary field on addon installation made optional
    - SHA1s in version information uses limited number of characters

--- www/geckodriver/patches/patch-vendor_mozrunner_src_runner.rs
+++ www/geckodriver/patches/patch-vendor_mozrunner_src_runner.rs
@@ -1,18 +1,19 @@
-$NetBSD: patch-vendor_mozrunner_src_runner.rs,v 1.2 2018/05/09 23:41:39 maya Exp $
+$NetBSD: patch-vendor_mozrunner_src_runner.rs,v 1.3 2019/01/05 03:01:30 tnn Exp $
 
 Make the linux method the fallback method.
 
---- ../vendor/mozrunner-0.6.1/src/runner.rs.orig	2017-09-11 14:07:59.000000000 +0000
-+++ ../vendor/mozrunner-0.6.1/src/runner.rs
-@@ -351,20 +351,6 @@ fn find_binary(name: &str) -> Option<Pat
-     })
+--- ../vendor/mozrunner-0.8.0/src/runner.rs.orig	2018-09-11 12:26:34.000000000 +0000
++++ ../vendor/mozrunner-0.8.0/src/runner.rs
+@@ -293,21 +293,6 @@ impl Runner for FirefoxRunner {
+     }
  }
  
 -#[cfg(target_os = "linux")]
 -pub mod platform {
--    use super::find_binary;
+-    use path::find_binary;
 -    use std::path::PathBuf;
 -
+-    /// Searches the system path for `firefox`.
 -    pub fn firefox_default_path() -> Option<PathBuf> {
 -        find_binary("firefox")
 -    }
@@ -24,17 +25,17 @@ Make the linux method the fallback method.
 -
  #[cfg(target_os = "macos")]
  pub mod platform {
-     use super::find_binary;
-@@ -455,12 +441,13 @@ pub mod platform {
-     }
- }
+     use path::{find_binary, is_binary};
+@@ -410,12 +395,12 @@ pub mod platform {
  
--#[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
-+#[cfg(not(any(target_os = "macos", target_os = "windows")))]
+ #[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
  pub mod platform {
-+    use super::find_binary;
++    use path::find_binary;
      use std::path::PathBuf;
  
+-    /// Returns `None` for all other operating systems than Linux, macOS, and
+-    /// Windows.
++    /// Searches the system path for `firefox`.
      pub fn firefox_default_path() -> Option<PathBuf> {
 -        None
 +        find_binary("firefox")
