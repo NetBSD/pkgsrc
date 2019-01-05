511b7711ef0bc1a8d0cb2cee237749259742f4bf
schmonz <schmonz@pkgsrc.org>
2019-01-05 06:18:11 +0000

    Remove commented-out 'inet6' option. Add CONFLICTS with djbdnscurve6.

--- net/djbdns/options.mk
+++ net/djbdns/options.mk
@@ -1,24 +1,11 @@
-# $NetBSD: options.mk,v 1.21 2018/09/30 22:10:51 schmonz Exp $
+# $NetBSD: options.mk,v 1.22 2019/01/05 06:18:11 schmonz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.djbdns
-PKG_SUPPORTED_OPTIONS+=		# inet6
 PKG_SUPPORTED_OPTIONS+=		djbdns-cachestats djbdns-ignoreip2
 PKG_SUPPORTED_OPTIONS+=		djbdns-mergequeries djbdns-tinydns64
 PKG_SUGGESTED_OPTIONS+=		djbdns-mergequeries djbdns-tinydns64
 
 .include "../../mk/bsd.options.mk"
-
-PLIST_VARS+=			inet6
-#.if !empty(PKG_OPTIONS:Minet6)
-#IPV6_PATCH=			djbdns-1.05-test28.diff.xz
-#PATCHFILES+=			${IPV6_PATCH}
-#SITES.${IPV6_PATCH}=		http://www.fefe.de/dns/
-#USE_TOOLS+=			xzcat
-#PATCH_DIST_CAT.${IPV6_PATCH}=	xzcat < ${IPV6_PATCH}
-#PATCH_DIST_STRIP.${IPV6_PATCH}=	-p1
-#MAKE_JOBS_SAFE=			no
-#PLIST.inet6=			yes
-#.endif
 
 .if !empty(PKG_OPTIONS:Mdjbdns-cachestats)
 CACHESTATS_PATCH=		djbdns-cachestats.patch
