d054ed7c59307c68adae76b584013b142114b4d0
gutteridge <gutteridge@pkgsrc.org>
2019-01-06 05:45:29 +0000

    devel/m4: add patches to fix building with glibc >= 2.27
    Addresses PR pkg/53826, further details are provided there. This is a
    temporary fix, until the next m4 release. No PKGREVISION, because
    there should be no change to existing packages, it addresses build
    failures only.

--- devel/m4/patches/patch-lib_stdio-impl.h
+++ devel/m4/patches/patch-lib_stdio-impl.h
@@ -1,10 +1,26 @@
-$NetBSD: patch-lib_stdio-impl.h,v 1.1 2018/10/28 20:53:44 sevan Exp $
+$NetBSD: patch-lib_stdio-impl.h,v 1.2 2019/01/06 05:45:30 gutteridge Exp $
 
 Treat Minix 3 same as NetBSD
 
---- lib/stdio-impl.h.orig	2018-10-28 20:27:54.933015000 +0000
+Work around glibc changes that broke dependent gnulib functionality.
+Patch from: http://git.savannah.gnu.org/gitweb/?p=gnulib.git;a=commitdiff;h=4af4a4a71827c0bc5e0ec67af23edef4f15cee8e
+
+--- lib/stdio-impl.h.orig	2016-12-31 13:54:42.000000000 +0000
 +++ lib/stdio-impl.h
-@@ -58,7 +58,7 @@
+@@ -18,6 +18,12 @@
+    the same implementation of stdio extension API, except that some fields
+    have different naming conventions, or their access requires some casts.  */
+ 
++/* Glibc 2.28 made _IO_IN_BACKUP private.  For now, work around this
++   problem by defining it ourselves.  FIXME: Do not rely on glibc
++   internals.  */
++#if !defined _IO_IN_BACKUP && defined _IO_EOF_SEEN
++# define _IO_IN_BACKUP 0x100
++#endif
+ 
+ /* BSD stdio derived implementations.  */
+ 
+@@ -58,7 +64,7 @@
  #  define fp_ fp
  # endif
  
