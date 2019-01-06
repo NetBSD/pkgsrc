9833f3b012390c5cfd2027cc3269a7c982e8f286
markd <markd@pkgsrc.org>
2019-01-06 08:41:01 +0000

    inkscape: update to 0.92.3
    Also post 0.92.3 updates from upstream for poppler-0.72 by way of
    linuxfromscratch.
    http://www.linuxfromscratch.org/patches/blfs/svn/inkscape-0.92.3-consolidated_fixes-1.patch
    
    Inkscape 0.92.3 is a maintenance release resolving numerous bugs and making
    this the most stable release of the series. Among the most reported bugs
    addressed were:
    
    * Blank pages being output when attempting to print multiple copies of a
      document
    * The inability to cancel during the export of large files
    * Application crashes caused by attempting to drag a path at a cap or line
      join
    * Some issues with keyboard shortcut functionality on non-Latin keyboard
      layouts
    
    A few performance improvements and usability enhancements that made it into
    the release, include:
    
    * New SVG export options from the command line
    * Support for right-to-left text
    * Improved ellipse controls
    * Multi-line text support for the PDF+LaTeX export
    * New render tile preferences for performance tuning
    * Better startup performance for many Windows users

--- graphics/inkscape/patches/patch-CMakeScripts_DefineDependsandFlags.cmake
+++ graphics/inkscape/patches/patch-CMakeScripts_DefineDependsandFlags.cmake
@@ -1,18 +1,25 @@
-$NetBSD: patch-CMakeScripts_DefineDependsandFlags.cmake,v 1.1 2017/09/09 21:48:56 prlw1 Exp $
+$NetBSD: patch-CMakeScripts_DefineDependsandFlags.cmake,v 1.2 2019/01/06 08:41:01 markd Exp $
 
-Support poppler 0.58
-https://gitlab.com/inkscape/inkscape/commit/9418824967eb4c53371ef8588243fed4cab496e0
+support for poppler 0.72 from upstream by way of linuxfromscratch 
 
---- CMakeScripts/DefineDependsandFlags.cmake.orig	2017-02-13 23:46:57.000000000 +0000
+--- CMakeScripts/DefineDependsandFlags.cmake.orig	2018-03-11 20:38:09.000000000 +0000
 +++ CMakeScripts/DefineDependsandFlags.cmake
-@@ -126,6 +126,10 @@ if(ENABLE_POPPLER)
- 		POPPLER_VERSION VERSION_EQUAL   "0.29.0")
- 	    set(POPPLER_EVEN_NEWER_NEW_COLOR_SPACE_API ON)
+@@ -116,18 +116,6 @@ if(ENABLE_POPPLER)
+ 		set(HAVE_POPPLER_GLIB ON)
+ 	    endif()
  	endif()
-+	if(POPPLER_VERSION VERSION_GREATER "0.58.0" OR
-+		POPPLER_VERSION VERSION_EQUAL   "0.58.0")
-+            set(POPPLER_NEW_OBJECT_API ON)
-+	endif()
+-	if(POPPLER_VERSION VERSION_GREATER "0.26.0" OR
+-		POPPLER_VERSION VERSION_EQUAL   "0.26.0")
+-	    set(POPPLER_EVEN_NEWER_COLOR_SPACE_API ON)
+-	endif()
+-	if(POPPLER_VERSION VERSION_GREATER "0.29.0" OR
+-		POPPLER_VERSION VERSION_EQUAL   "0.29.0")
+-	    set(POPPLER_EVEN_NEWER_NEW_COLOR_SPACE_API ON)
+-	endif()
+-	if(POPPLER_VERSION VERSION_GREATER "0.58.0" OR
+-		POPPLER_VERSION VERSION_EQUAL   "0.58.0")
+-            set(POPPLER_NEW_OBJECT_API ON)
+-	endif()
      else()
  	set(ENABLE_POPPLER_CAIRO OFF)
      endif()
--- graphics/inkscape/patches/patch-src_extension_internal_pdfinput_pdf-parser.h
+++ graphics/inkscape/patches/patch-src_extension_internal_pdfinput_pdf-parser.h
@@ -1,27 +1,74 @@
-$NetBSD: patch-src_extension_internal_pdfinput_pdf-parser.h,v 1.3 2018/12/21 08:46:16 wiz Exp $
+$NetBSD: patch-src_extension_internal_pdfinput_pdf-parser.h,v 1.4 2019/01/06 08:41:01 markd Exp $
 
 Explicitly include poppler/Object.h; otherwise Python's Object.h gets included.
+Support for poppler 0.72 from upstream by way of linuxfromscratch 
 
---- src/extension/internal/pdfinput/pdf-parser.h.orig	2017-08-06 20:44:00.000000000 +0000
+--- src/extension/internal/pdfinput/pdf-parser.h.orig	2018-03-11 20:38:09.000000000 +0000
 +++ src/extension/internal/pdfinput/pdf-parser.h
-@@ -25,8 +25,18 @@ namespace Inkscape {
+@@ -9,6 +9,7 @@
+ #define PDF_PARSER_H
+ 
+ #ifdef HAVE_POPPLER
++#include "poppler-transition-api.h"
+ 
+ #ifdef USE_GCC_PRAGMAS
+ #pragma interface
+@@ -25,8 +26,9 @@ namespace Inkscape {
  // TODO clean up and remove using:
  using Inkscape::Extension::Internal::SvgBuilder;
  
--#include "goo/gtypes.h"
++#include "glib/poppler-features.h"
+ #include "goo/gtypes.h"
 -#include "Object.h"
 +#include "poppler/Object.h"
-+
-+// poppler 0.72
-+#define getCString c_str
-+
-+// poppler 0.71
-+typedef bool GBool;
-+#define gTrue true
-+#define gFalse false  
-+
-+// poppler 0.70
-+#define _POPPLER_CONST const
  
  class GooString;
  class XRef;
+@@ -127,11 +129,14 @@ public:
+ 
+   // Constructor for regular output.
+   PdfParser(XRef *xrefA, SvgBuilder *builderA, int pageNum, int rotate,
+-            Dict *resDict, PDFRectangle *box, PDFRectangle *cropBox);
++            Dict *resDict,
++            _POPPLER_CONST PDFRectangle *box,
++            _POPPLER_CONST PDFRectangle *cropBox);
+ 
+   // Constructor for a sub-page object.
+   PdfParser(XRef *xrefA, Inkscape::Extension::Internal::SvgBuilder *builderA,
+-            Dict *resDict, PDFRectangle *box);
++            Dict *resDict,
++            _POPPLER_CONST PDFRectangle *box);
+ 
+   virtual ~PdfParser();
+ 
+@@ -185,7 +190,7 @@ private:
+ 
+   void go(GBool topLevel);
+   void execOp(Object *cmd, Object args[], int numArgs);
+-  PdfOperator *findOp(char *name);
++  PdfOperator *findOp(const char *name);
+   GBool checkArg(Object *arg, TchkType type);
+   int getPos();
+ 
+@@ -256,7 +261,7 @@ private:
+ 			   double x2, double y2, GfxColor *color2,
+ 			   int nComps, int depth);
+   void doPatchMeshShFill(GfxPatchMeshShading *shading);
+-  void fillPatch(GfxPatch *patch, int nComps, int depth);
++  void fillPatch(_POPPLER_CONST GfxPatch *patch, int nComps, int depth);
+   void doEndPath();
+ 
+   // path clipping operators
+@@ -287,7 +292,12 @@ private:
+   void opMoveShowText(Object args[], int numArgs);
+   void opMoveSetShowText(Object args[], int numArgs);
+   void opShowSpaceText(Object args[], int numArgs);
++#if POPPLER_CHECK_VERSION(0,64,0)
++  void doShowText(const GooString *s);
++#else
+   void doShowText(GooString *s);
++#endif
++  
+ 
+   // XObject operators
+   void opXObject(Object args[], int numArgs);
