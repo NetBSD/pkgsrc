50f77e879592b1164c18726ff8ee75b3577c616a
markd <markd@pkgsrc.org>
2019-01-06 11:21:06 +0000

    {py-,}protobuf: update to 3.6.1
    minor changes.
    patches for python3.7
    build py-protobuf with "--cpp_implementation" to get _message extension

---
+++ devel/protobuf/patches/patch-python_google_protobuf_pyext_extension_dict.cc
@@ -0,0 +1,15 @@
+$NetBSD: patch-python_google_protobuf_pyext_extension_dict.cc,v 1.1 2019/01/06 11:21:06 markd Exp $
+
+python3.7 fixes 
+
+--- python/google/protobuf/pyext/extension_dict.cc.orig	2018-07-30 22:16:10.000000000 +0000
++++ python/google/protobuf/pyext/extension_dict.cc
+@@ -53,7 +53,7 @@
+   #endif
+   #define PyString_AsStringAndSize(ob, charpp, sizep) \
+     (PyUnicode_Check(ob)? \
+-       ((*(charpp) = PyUnicode_AsUTF8AndSize(ob, (sizep))) == NULL? -1: 0): \
++       ((*(charpp) = const_cast<char*>(PyUnicode_AsUTF8AndSize(ob, (sizep)))) == NULL? -1: 0): \
+        PyBytes_AsStringAndSize(ob, (charpp), (sizep)))
+ #endif
+ 
