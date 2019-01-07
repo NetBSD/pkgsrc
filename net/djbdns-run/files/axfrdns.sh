2d523eb68f604ae302c03de19d6e76af2b2f8726
schmonz <schmonz@pkgsrc.org>
2019-01-07 14:01:55 +0000

    With the 'inet6' option, depend on djbdnscurve6 instead of djbdns.
    Extract most of MESSAGE to README.pkgsrc.
    
    Remove vestigial Red Hat chkconfig stanza from main control script (we
    have pkgtools/rc.d-boot now).
    
    Use CONF_FILES where applicable.
    
    rc.d script defaults:
    - Tag log entries like qmail-run's; for instance, "nbtinydns" and
      "nbdnscache" become "nbdjbdns/tiny" and "nbdjbdns/cache".
    - Have tinydns listen on the network.
    - Auto-rebuild CDBs as needed on service start.
    
    Bump version.

--- net/djbdns-run/files/axfrdns.sh
+++ net/djbdns-run/files/axfrdns.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: axfrdns.sh,v 1.9 2017/08/05 15:16:05 schmonz Exp $
+# $NetBSD: axfrdns.sh,v 1.10 2019/01/07 14:01:55 schmonz Exp $
 #
 # @PKGNAME@ script to control axfrdns (DNS zone-transfer and TCP service).
 #
@@ -13,13 +13,16 @@ name="axfrdns"
 
 # User-settable rc.conf variables and their default values:
 : ${axfrdns_postenv:=""}
-: ${axfrdns_tcpflags:="-vDRHl0"}
-: ${axfrdns_tcpport:="53"}
+: ${tinydns_ip:="@TINYDNS_LISTENIP@"}
 : ${axfrdns_datalimit:="300000"}
 : ${axfrdns_pretcpserver:=""}
 : ${axfrdns_tcpserver:="@PREFIX@/bin/tcpserver"}
+: ${axfrdns_tcpflags:="-vDRHl0"}
+: ${axfrdns_tcprules:="@PKG_SYSCONFDIR@/axfrdns/tcp"}
+: ${axfrdns_autocdb:="YES"}
+: ${axfrdns_tcpport:="53"}
 : ${axfrdns_log:="YES"}
-: ${axfrdns_logcmd:="logger -t nb${name} -p daemon.info"}
+: ${axfrdns_logcmd:="logger -t nbdjbdns/axfr -p daemon.info"}
 : ${axfrdns_nologcmd:="@PREFIX@/bin/multilog -*"}
 
 if [ -f /etc/rc.subr ]; then
@@ -27,7 +30,7 @@ if [ -f /etc/rc.subr ]; then
 fi
 
 rcvar=${name}
-required_files="@PKG_SYSCONFDIR@/${name}/tcp.cdb"
+required_files="${axfrdns_tcprules}"
 command="${axfrdns_tcpserver}"
 procname=nb${name}
 start_precmd="axfrdns_precmd"
@@ -35,34 +38,42 @@ extra_commands="cdb reload"
 cdb_cmd="axfrdns_cdb"
 reload_cmd=${cdb_cmd}
 
-axfrdns_precmd()
-{
+axfrdns_precmd() {
 	if [ -f /etc/rc.subr ] && ! checkyesno axfrdns_log; then
 		axfrdns_logcmd=${axfrdns_nologcmd}
+	fi
+	if [ -f /etc/rc.subr ] && checkyesno axfrdns_autocdb; then
+		axfrdns_needcdb && axfrdns_cdb
 	fi
 	# tcpserver(1) is akin to inetd(8), but runs one service per process.
 	# We want to signal only the tcpserver process responsible for this
 	# service. Use argv0(1) to set procname to "axfrdns".
-	command="@PREFIX@/bin/pgrphack @SETENV@ - ${axfrdns_postenv}
-ROOT=@PKG_SYSCONFDIR@/tinydns IP=${tinydns_ip}
-@PREFIX@/bin/envuidgid @DJBDNS_AXFR_USER@
-@PREFIX@/bin/softlimit -d ${axfrdns_datalimit} ${axfrdns_pretcpserver}
-@PREFIX@/bin/argv0 ${axfrdns_tcpserver} ${procname}
-${axfrdns_tcpflags} -x @PKG_SYSCONFDIR@/${name}/tcp.cdb
--- ${tinydns_ip} ${axfrdns_tcpport}
-@PREFIX@/bin/${name}
-</dev/null 2>&1 |
+	command="@PREFIX@/bin/pgrphack @SETENV@ - ${axfrdns_postenv} \
+ROOT=@PKG_SYSCONFDIR@/tinydns \
+IP=${tinydns_ip} \
+@PREFIX@/bin/envuidgid @DJBDNS_AXFR_USER@ \
+@PREFIX@/bin/softlimit -d ${axfrdns_datalimit} ${axfrdns_pretcpserver} \
+@PREFIX@/bin/argv0 ${axfrdns_tcpserver} ${procname} \
+${axfrdns_tcpflags} -x ${axfrdns_tcprules}.cdb \
+-- \
+${tinydns_ip} ${axfrdns_tcpport} \
+@PREFIX@/bin/${name} \
+</dev/null 2>&1 | \
 @PREFIX@/bin/pgrphack @PREFIX@/bin/setuidgid @DJBDNS_LOG_USER@ ${axfrdns_logcmd}"
 	command_args="&"
 	rc_flags=""
 }
 
-axfrdns_cdb()
-{
-	@ECHO@ "Reloading @PKG_SYSCONFDIR@/axfrdns/tcp."
-	cd @PKG_SYSCONFDIR@/${name}
-	@PREFIX@/bin/tcprules tcp.cdb tcp.tmp < tcp
-	@CHMOD@ 644 tcp.cdb
+axfrdns_needcdb() {
+	_src=${axfrdns_tcprules}
+	_dst=${axfrdns_tcprules}.cdb
+	[ -f "${_src}" -a "${_src}" -nt "${_dst}" ] || [ ! -f "${_dst}" ]
+}
+
+axfrdns_cdb() {
+	@ECHO@ "Reloading ${axfrdns_tcprules}."
+	@PREFIX@/bin/tcprules ${axfrdns_tcprules}.cdb ${axfrdns_tcprules}.tmp < ${axfrdns_tcprules}
+	@CHMOD@ 644 ${axfrdns_tcprules}.cdb
 }
 
 if [ -f /etc/rc.subr ]; then
--- net/djbdns-run/files/rbldns.sh
+++ net/djbdns-run/files/rbldns.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: rbldns.sh,v 1.7 2017/08/05 15:16:05 schmonz Exp $
+# $NetBSD: rbldns.sh,v 1.8 2019/01/07 14:01:55 schmonz Exp $
 #
 # @PKGNAME@ script to control rbldns (local RBL service).
 #
@@ -16,16 +16,19 @@ name="rbldns"
 : ${rbldns_ip:="127.0.0.3"}
 : ${rbldns_base:="rbl"}
 : ${rbldns_datalimit:="250000"}
+: ${rbldns_autocdb:="YES"}
 : ${rbldns_log:="YES"}
-: ${rbldns_logcmd:="logger -t nb${name} -p daemon.info"}
+: ${rbldns_logcmd:="logger -t nbdjbdns/rbl -p daemon.info"}
 : ${rbldns_nologcmd:="@PREFIX@/bin/multilog -*"}
+
+rbldns_data="@PKG_SYSCONFDIR@/${name}/data"
 
 if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
 rcvar=${name}
-required_files="@PKG_SYSCONFDIR@/${name}/data.cdb"
+required_files="${rbldns_data}"
 command="@PREFIX@/bin/${name}"
 procname=nb${name}
 start_precmd="rbldns_precmd"
@@ -33,28 +36,36 @@ extra_commands="cdb reload"
 cdb_cmd="rbldns_cdb"
 reload_cmd=${cdb_cmd}
 
-rbldns_precmd()
-{
+rbldns_precmd() {
 	if [ -f /etc/rc.subr ] && ! checkyesno rbldns_log; then
 		rbldns_logcmd=${rbldns_nologcmd}
 	fi
-	command="@PREFIX@/bin/pgrphack @SETENV@ - ${rbldns_postenv}
-ROOT=@PKG_SYSCONFDIR@/${name} IP=${rbldns_ip} BASE=${rbldns_base}
-@PREFIX@/bin/envuidgid @DJBDNS_RBL_USER@
-@PREFIX@/bin/softlimit -d ${rbldns_datalimit}
-@PREFIX@/bin/argv0 @PREFIX@/bin/${name} ${procname}
-2>&1 |
+	if [ -f /etc/rc.subr ] && checkyesno rbldns_autocdb; then
+		rbldns_needcdb && rbldns_cdb
+	fi
+	command="@PREFIX@/bin/pgrphack @SETENV@ - ${rbldns_postenv} \
+ROOT=@PKG_SYSCONFDIR@/${name} \
+IP=${rbldns_ip} \
+BASE=${rbldns_base} \
+@PREFIX@/bin/envuidgid @DJBDNS_RBL_USER@ \
+@PREFIX@/bin/softlimit -d ${rbldns_datalimit} \
+@PREFIX@/bin/argv0 @PREFIX@/bin/${name} ${procname} \
+2>&1 | \
 @PREFIX@/bin/pgrphack @PREFIX@/bin/setuidgid @DJBDNS_LOG_USER@ ${rbldns_logcmd}"
 	command_args="&"
 	rc_flags=""
 }
 
-rbldns_cdb()
-{
-	@ECHO@ "Reloading @PKG_SYSCONFDIR@/${name}/data."
-	cd @PKG_SYSCONFDIR@/${name}
-	@PREFIX@/bin/rbldns-data
-	@CHMOD@ 644 data.cdb
+rbldns_needcdb() {
+	_src=${axfrdns_tcprules}
+	_dst=${axfrdns_tcprules}.cdb
+	[ -f "${_src}" -a "${_src}" -nt "${_dst}" ] || [ ! -f "${_dst}" ]
+}
+
+rbldns_cdb() {
+	@ECHO@ "Reloading ${rbldns_data}."
+	cd @PKG_SYSCONFDIR@/${name} && @PREFIX@/bin/rbldns-data
+	@CHMOD@ 644 ${rbldns_data}.cdb
 }
 
 if [ -f /etc/rc.subr ]; then
