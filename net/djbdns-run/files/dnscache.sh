7e9aadb23cca416cbfc0a1b0b945af4558234590
schmonz <schmonz@pkgsrc.org>
2019-01-07 14:01:55 +0000

    With the 'inet6' option, depend on djbdnscurve6 instead of djbdns.
    Extract most of MESSAGE to README.pkgsrc.
    
    Remove vestigial Red Hat chkconfig stanza from main control script (we
    have pkgtools/rc.d-boot now).
    
    Use CONF_FILES where applicable.
    
    rc.d script defaults:
    - Tag log entries like qmail-run's; for instance, "nbtinydns" and
      "nbdnscache" become "nbdjbdns/tiny" and "nbdjbdns/cache".
    - Have tinydns listen on the network.
    - Auto-rebuild CDBs as needed on service start.
    
    Bump version.

--- net/djbdns-run/files/dnscache.sh
+++ net/djbdns-run/files/dnscache.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: dnscache.sh,v 1.10 2017/08/05 15:16:05 schmonz Exp $
+# $NetBSD: dnscache.sh,v 1.11 2019/01/07 14:01:55 schmonz Exp $
 #
 # @PKGNAME@ script to control dnscache (caching DNS resolver).
 #
@@ -13,12 +13,12 @@ name="dnscache"
 
 # User-settable rc.conf variables and their default values:
 : ${dnscache_postenv:=""}
-: ${dnscache_ip:="127.0.0.1"}
-: ${dnscache_ipsend:="0.0.0.0"}
+: ${dnscache_ip:="@DNSCACHE_LISTENIP@"}
+: ${dnscache_ipsend:="@DNSCACHE_SENDIP@"}
 : ${dnscache_size:="1000000"}
 : ${dnscache_datalimit:="3000000"}
 : ${dnscache_log:="YES"}
-: ${dnscache_logcmd:="logger -t nb${name} -p daemon.info"}
+: ${dnscache_logcmd:="logger -t nbdjbdns/cache -p daemon.info"}
 : ${dnscache_nologcmd:="@PREFIX@/bin/multilog -*"}
 
 if [ -f /etc/rc.subr ]; then
@@ -32,18 +32,19 @@ command="@PREFIX@/bin/${name}"
 procname=nb${name}
 start_precmd="dnscache_precmd"
 
-dnscache_precmd()
-{
+dnscache_precmd() {
 	if [ -f /etc/rc.subr ] && ! checkyesno dnscache_log; then
 		dnscache_logcmd=${dnscache_nologcmd}
 	fi
-	command="@PREFIX@/bin/pgrphack @SETENV@ - ${dnscache_postenv}
-ROOT=@PKG_SYSCONFDIR@/${name} IP=${dnscache_ip}
-IPSEND=${dnscache_ipsend} CACHESIZE=${dnscache_size}
-@PREFIX@/bin/envuidgid @DJBDNS_CACHE_USER@
-@PREFIX@/bin/softlimit -o250 -d ${dnscache_datalimit}
-@PREFIX@/bin/argv0 @PREFIX@/bin/${name} ${procname}
-</dev/urandom 2>&1 |
+	command="@PREFIX@/bin/pgrphack @SETENV@ - ${dnscache_postenv} \
+ROOT=@PKG_SYSCONFDIR@/${name} \
+IP=${dnscache_ip} \
+IPSEND=${dnscache_ipsend} \
+CACHESIZE=${dnscache_size} \
+@PREFIX@/bin/envuidgid @DJBDNS_CACHE_USER@ \
+@PREFIX@/bin/softlimit -o250 -d ${dnscache_datalimit} \
+@PREFIX@/bin/argv0 @PREFIX@/bin/${name} ${procname} \
+</dev/urandom 2>&1 | \
 @PREFIX@/bin/pgrphack @PREFIX@/bin/setuidgid @DJBDNS_LOG_USER@ ${dnscache_logcmd}"
 	command_args="&"
 	rc_flags=""
