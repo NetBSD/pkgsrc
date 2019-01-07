54ff807922010c32b41d582c71270fab21ee1247
schmonz <schmonz@pkgsrc.org>
2019-01-07 14:01:55 +0000

    With the 'inet6' option, depend on djbdnscurve6 instead of djbdns.
    Extract most of MESSAGE to README.pkgsrc.
    
    Remove vestigial Red Hat chkconfig stanza from main control script (we
    have pkgtools/rc.d-boot now).
    
    Use CONF_FILES where applicable.
    
    rc.d script defaults:
    - Tag log entries like qmail-run's; for instance, "nbtinydns" and
      "nbdnscache" become "nbdjbdns/tiny" and "nbdjbdns/cache".
    - Have tinydns listen on the network.
    - Auto-rebuild CDBs as needed on service start.
    
    Bump version.

--- net/djbdns-run/options.mk
+++ net/djbdns-run/options.mk
@@ -1,12 +1,28 @@
-# $NetBSD: options.mk,v 1.1 2018/07/29 23:59:26 schmonz Exp $
+# $NetBSD: options.mk,v 1.2 2019/01/07 14:01:55 schmonz Exp $
 
-PKG_OPTIONS_VAR=		PKG_OPTIONS.djbdns-run
-PKG_SUPPORTED_OPTIONS+=		inet6
+PKG_OPTIONS_VAR=	PKG_OPTIONS.djbdns-run
+PKG_SUPPORTED_OPTIONS+=	inet6
 
 .include "../../mk/bsd.options.mk"
 
+FILES_SUBST+=		TINYDNS_LISTENIP=${TINYDNS_LISTENIP:Q}
+FILES_SUBST+=		DNSCACHE_LISTENIP=${DNSCACHE_LISTENIP:Q}
+FILES_SUBST+=		DNSCACHE_SENDIP=${DNSCACHE_SENDIP:Q}
+
 .if !empty(PKG_OPTIONS:Minet6)
-DEPENDS+=			ucspi-tcp6-[0-9]*:../../net/ucspi-tcp6
+DEPENDS_DJBDNS=		djbdnscurve6-[0-9]*:../../net/djbdnscurve6
+DEPENDS+=		ucspi-tcp6-[0-9]*:../../net/ucspi-tcp6
+TINYDNS_LISTENIP=	::
+DNSCACHE_LISTENIP=	::1
+DNSCACHE_SENDIP=	::
+CONF_FILES+=		${PREFIX}/share/examples/${PKGBASE}/dnscache-ip \
+			${PKG_SYSCONFDIR}/dnscache/ip/::1
 .else
-DEPENDS+=			ucspi-tcp-[0-9]*:../../net/ucspi-tcp
+DEPENDS_DJBDNS=		djbdns>=1.05nb5:../../net/djbdns
+DEPENDS+=		ucspi-tcp-[0-9]*:../../net/ucspi-tcp
+TINYDNS_LISTENIP=	0.0.0.0
+DNSCACHE_LISTENIP=	127.0.0.1
+DNSCACHE_SENDIP=	0.0.0.0
+CONF_FILES+=		${PREFIX}/share/examples/${PKGBASE}/dnscache-ip \
+			${PKG_SYSCONFDIR}/dnscache/ip/127.0.0.1
 .endif
