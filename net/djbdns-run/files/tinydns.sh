31940d97a1863de0c9a96ce65a8154a622082cc3
schmonz <schmonz@pkgsrc.org>
2019-01-07 14:01:55 +0000

    With the 'inet6' option, depend on djbdnscurve6 instead of djbdns.
    Extract most of MESSAGE to README.pkgsrc.
    
    Remove vestigial Red Hat chkconfig stanza from main control script (we
    have pkgtools/rc.d-boot now).
    
    Use CONF_FILES where applicable.
    
    rc.d script defaults:
    - Tag log entries like qmail-run's; for instance, "nbtinydns" and
      "nbdnscache" become "nbdjbdns/tiny" and "nbdjbdns/cache".
    - Have tinydns listen on the network.
    - Auto-rebuild CDBs as needed on service start.
    
    Bump version.

--- net/djbdns-run/files/tinydns.sh
+++ net/djbdns-run/files/tinydns.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: tinydns.sh,v 1.9 2017/08/05 15:16:05 schmonz Exp $
+# $NetBSD: tinydns.sh,v 1.10 2019/01/07 14:01:55 schmonz Exp $
 #
 # @PKGNAME@ script to control tinydns (authoritative DNS service).
 #
@@ -13,18 +13,21 @@ name="tinydns"
 
 # User-settable rc.conf variables and their default values:
 : ${tinydns_postenv:=""}
-: ${tinydns_ip:="127.0.0.2"}
+: ${tinydns_ip:="@TINYDNS_LISTENIP@"}
 : ${tinydns_datalimit:="300000"}
+: ${tinydns_autocdb:="YES"}
 : ${tinydns_log:="YES"}
-: ${tinydns_logcmd:="logger -t nb${name} -p daemon.info"}
+: ${tinydns_logcmd:="logger -t nbdjbdns/tiny -p daemon.info"}
 : ${tinydns_nologcmd:="@PREFIX@/bin/multilog -*"}
+
+tinydns_data="@PKG_SYSCONFDIR@/${name}/data"
 
 if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
 rcvar=${name}
-required_files="@PKG_SYSCONFDIR@/${name}/data.cdb"
+required_files="${tinydns_data}"
 command="@PREFIX@/bin/${name}"
 procname=nb${name}
 start_precmd="tinydns_precmd"
@@ -32,28 +35,35 @@ extra_commands="cdb reload"
 cdb_cmd="tinydns_cdb"
 reload_cmd=${cdb_cmd}
 
-tinydns_precmd()
-{
+tinydns_precmd() {
 	if [ -f /etc/rc.subr ] && ! checkyesno tinydns_log; then
 		tinydns_logcmd=${tinydns_nologcmd}
 	fi
-	command="@PREFIX@/bin/pgrphack @SETENV@ - ${tinydns_postenv}
-ROOT=@PKG_SYSCONFDIR@/${name} IP=${tinydns_ip}
-@PREFIX@/bin/envuidgid @DJBDNS_TINY_USER@
-@PREFIX@/bin/softlimit -d ${tinydns_datalimit}
-@PREFIX@/bin/argv0 @PREFIX@/bin/${name} ${procname}
-</dev/null 2>&1 |
+	if [ -f /etc/rc.subr ] && checkyesno tinydns_autocdb; then
+		tinydns_needcdb && tinydns_cdb
+	fi
+	command="@PREFIX@/bin/pgrphack @SETENV@ - ${tinydns_postenv} \
+ROOT=@PKG_SYSCONFDIR@/${name} \
+IP=${tinydns_ip} \
+@PREFIX@/bin/envuidgid @DJBDNS_TINY_USER@ \
+@PREFIX@/bin/softlimit -d ${tinydns_datalimit} \
+@PREFIX@/bin/argv0 @PREFIX@/bin/${name} ${procname} \
+</dev/null 2>&1 | \
 @PREFIX@/bin/pgrphack @PREFIX@/bin/setuidgid @DJBDNS_LOG_USER@ ${tinydns_logcmd}"
 	command_args="&"
 	rc_flags=""
 }
 
-tinydns_cdb()
-{
-	@ECHO@ "Reloading @PKG_SYSCONFDIR@/${name}/data."
-	cd @PKG_SYSCONFDIR@/${name}
-	@PREFIX@/bin/tinydns-data
-	@CHMOD@ 644 data.cdb
+tinydns_needcdb() {
+	_src=${tinydns_data}
+	_dst=${tinydns_data}.cdb
+	[ -f "${_src}" -a "${_src}" -nt "${_dst}" ] || [ ! -f "${_dst}" ]
+}
+
+tinydns_cdb() {
+	@ECHO@ "Reloading ${tinydns_data}."
+	cd @PKG_SYSCONFDIR@/${name} && @PREFIX@/bin/tinydns-data
+	@CHMOD@ 644 ${tinydns_data}.cdb
 }
 
 if [ -f /etc/rc.subr ]; then
