a7e6f8d57bb331211615713b82501c6b0076db8d
schmonz <schmonz@pkgsrc.org>
2019-01-07 19:45:48 +0000

    Get a copy of dnsroots.global from djbdns and use the copy as CONF_FILES. Fixes build on NetBSD that somehow wasn't broken on OS X.

--- net/djbdns-run/options.mk
+++ net/djbdns-run/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2019/01/07 14:01:55 schmonz Exp $
+# $NetBSD: options.mk,v 1.3 2019/01/07 19:45:48 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.djbdns-run
 PKG_SUPPORTED_OPTIONS+=	inet6
@@ -12,6 +12,7 @@ FILES_SUBST+=		DNSCACHE_SENDIP=${DNSCACHE_SENDIP:Q}
 .if !empty(PKG_OPTIONS:Minet6)
 DEPENDS_DJBDNS=		djbdnscurve6-[0-9]*:../../net/djbdnscurve6
 DEPENDS+=		ucspi-tcp6-[0-9]*:../../net/ucspi-tcp6
+DNSROOTS_GLOBAL=	share/examples/djbdnscurve6/dnsroots.global
 TINYDNS_LISTENIP=	::
 DNSCACHE_LISTENIP=	::1
 DNSCACHE_SENDIP=	::
@@ -20,6 +21,7 @@ CONF_FILES+=		${PREFIX}/share/examples/${PKGBASE}/dnscache-ip \
 .else
 DEPENDS_DJBDNS=		djbdns>=1.05nb5:../../net/djbdns
 DEPENDS+=		ucspi-tcp-[0-9]*:../../net/ucspi-tcp
+DNSROOTS_GLOBAL=	share/examples/djbdns/dnsroots.global
 TINYDNS_LISTENIP=	0.0.0.0
 DNSCACHE_LISTENIP=	127.0.0.1
 DNSCACHE_SENDIP=	0.0.0.0
