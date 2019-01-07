27c1f5cb890f2bd94f6499fd19ed6961426d53a4
bacon <bacon@pkgsrc.org>
2019-01-07 15:00:10 +0000

    ncbi-blast+: Upgrade to 2.8.1
    Support for new BLAST database format
    Increased makeblastdb output file size limit to 4GB
    Other minor bug fixes and enhancements
    
    OK wiz@

--- biology/ncbi-blast+/patches/patch-src_algo_blast_core_blast__kappa.c
+++ biology/ncbi-blast+/patches/patch-src_algo_blast_core_blast__kappa.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_algo_blast_core_blast__kappa.c,v 1.1 2018/04/27 20:28:28 bacon Exp $
+$NetBSD: patch-src_algo_blast_core_blast__kappa.c,v 1.2 2019/01/07 15:00:10 bacon Exp $
 
 # Work around NetBSD stderr def choking macro
 
---- src/algo/blast/core/blast_kappa.c.orig	2018-03-26 21:55:44.000000000 +0000
+--- src/algo/blast/core/blast_kappa.c.orig	2019-01-02 04:00:17.000000000 +0000
 +++ src/algo/blast/core/blast_kappa.c
 @@ -32,6 +32,7 @@
   * system for each match in blastpgp
@@ -12,7 +12,16 @@ $NetBSD: patch-src_algo_blast_core_blast__kappa.c,v 1.1 2018/04/27 20:28:28 baco
  #include <float.h>
  #include <algo/blast/core/ncbi_math.h>
  #include <algo/blast/core/blast_hits.h>
-@@ -3404,13 +3405,15 @@ Blast_RedoAlignmentCore_MT(EBlastProgram
+@@ -57,7 +58,7 @@
+  * shared(...) list. */
+ #    define STDERR_COMMA
+ #  else
+-#    define STDERR_COMMA stderr,
++#    define STDERR_COMMA my_stderr,
+ #  endif
+ #endif
+ 
+@@ -3412,6 +3413,8 @@ Blast_RedoAlignmentCore_MT(EBlastProgram
      }
  
      Boolean interrupt = FALSE;
@@ -21,11 +30,3 @@ $NetBSD: patch-src_algo_blast_core_blast__kappa.c,v 1.1 2018/04/27 20:28:28 baco
  #pragma omp parallel \
      default(none) num_threads(actual_num_threads) \
      if(actual_num_threads>1) \
-     shared(interrupt, seqsrc_tld, score_params_tld, hit_params_tld, \
-     gap_align_tld, results_tld, \
-     redoneMatches_tld, \
--    stderr, \
-+    my_stderr, \
-     numQueries, numMatches, theseMatches, \
-     numFrames, program_number, subjectBlk_tld, positionBased, \
-     default_db_genetic_code, localScalingFactor, queryInfo, \
