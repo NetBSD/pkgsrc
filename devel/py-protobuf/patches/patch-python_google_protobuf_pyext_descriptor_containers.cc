d8b89579b66f3e22464eec1b75b4c9e7f191c056
markd <markd@pkgsrc.org>
2019-01-08 05:56:19 +0000

    {py-,}protobuf: don't share the patches directory some patches only apply to py-protobuf.

--- devel/py-protobuf/patches/patch-python_google_protobuf_pyext_descriptor_containers.cc
+++ devel/py-protobuf/patches/patch-python_google_protobuf_pyext_descriptor_containers.cc
@@ -1,10 +1,10 @@
-$NetBSD: patch-python_google_protobuf_pyext_extension_dict.cc,v 1.1 2019/01/06 11:21:06 markd Exp $
+$NetBSD: patch-python_google_protobuf_pyext_descriptor_containers.cc,v 1.1 2019/01/08 05:56:19 markd Exp $
 
 python3.7 fixes 
 
---- python/google/protobuf/pyext/extension_dict.cc.orig	2018-07-30 22:16:10.000000000 +0000
-+++ python/google/protobuf/pyext/extension_dict.cc
-@@ -53,7 +53,7 @@
+--- python/google/protobuf/pyext/descriptor_containers.cc.orig	2018-07-30 22:16:10.000000000 +0000
++++ python/google/protobuf/pyext/descriptor_containers.cc
+@@ -66,7 +66,7 @@
    #endif
    #define PyString_AsStringAndSize(ob, charpp, sizep) \
      (PyUnicode_Check(ob)? \
