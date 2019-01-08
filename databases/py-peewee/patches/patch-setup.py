19730a4a4df8218b46cbe487532d011b0577bea7
adam <adam@pkgsrc.org>
2019-01-08 08:37:59 +0000

    py-peewee: updated to 3.8.1
    3.8.1
    
    New features
    Sqlite SearchField now supports the match() operator, allowing full-text search to be performed on a single column (as opposed to the whole table).
    
    Changes
    Remove minimum passphrase restrictions in SQLCipher integration.
    
    Bugfixes
    Support inheritance of ManyToManyField instances.
    Ensure operator overloads are invoked when generating filter expressions.
    Fix incorrect scoring in Sqlite BM25, BM25f and Lucene ranking algorithms.
    Support string field-names in data dictionary when performing an ON CONFLICT ... UPDATE query, which allows field-specific conversions to be applied.

--- databases/py-peewee/patches/patch-setup.py
+++ databases/py-peewee/patches/patch-setup.py
@@ -1,12 +1,15 @@
-$NetBSD: patch-setup.py,v 1.1 2018/10/26 11:57:01 jperkin Exp $
+$NetBSD: patch-setup.py,v 1.2 2019/01/08 08:37:59 adam Exp $
 
 Find libsqlite3.
 
---- setup.py.orig	2018-08-30 19:16:58.000000000 +0000
+--- setup.py.orig	2018-08-16 18:40:47.000000000 +0000
 +++ setup.py
-@@ -75,6 +75,7 @@ def _have_sqlite_extension_support():
+@@ -73,8 +73,9 @@ def _have_sqlite_extension_support():
+     success = False
+     try:
          compiler.link_executable(
-             compiler.compile([src_file], output_dir=tmp_dir),
+-            compiler.compile([src_file], output_dir=tmp_dir),
++            compiler.compile([src_file], output_dir=tmp_dir, include_dirs=['@BUILDLINK_PREFIX.sqlite3@/include']),
              bin_file,
 +            library_dirs=['@BUILDLINK_PREFIX.sqlite3@/lib'],
              libraries=['sqlite3'])
