bfb5ff91dbb4afeaca46576f5edca1051c3188d0
schmonz <schmonz@pkgsrc.org>
2019-01-10 13:23:16 +0000

    Let installed ucspi-tcp6 satisfy ucspi-tcp dependency.

--- net/publicfile/options.mk
+++ net/publicfile/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2018/07/29 23:46:21 schmonz Exp $
+# $NetBSD: options.mk,v 1.2 2019/01/10 13:23:16 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.publicfile
 PKG_SUPPORTED_OPTIONS+=	inet6
@@ -8,5 +8,5 @@ PKG_SUPPORTED_OPTIONS+=	inet6
 .if !empty(PKG_OPTIONS:Minet6)
 DEPENDS+=		ucspi-tcp6-[0-9]*:../../net/ucspi-tcp6
 .else
-DEPENDS+=		ucspi-tcp-[0-9]*:../../net/ucspi-tcp
+DEPENDS+=		{ucspi-tcp6-[0-9]*,ucspi-tcp-[0-9]*}:../../net/ucspi-tcp
 .endif
