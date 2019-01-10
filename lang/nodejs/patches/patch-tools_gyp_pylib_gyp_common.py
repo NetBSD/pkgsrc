9b052bca05912a4f400ca812594f80907cb7afce
ryoon <ryoon@pkgsrc.org>
2019-01-10 13:02:21 +0000

    Fix comment

--- lang/nodejs/patches/patch-tools_gyp_pylib_gyp_common.py
+++ lang/nodejs/patches/patch-tools_gyp_pylib_gyp_common.py
@@ -1,6 +1,6 @@
-$NetBSD: patch-tools_gyp_pylib_gyp_common.py,v 1.2 2017/05/04 16:39:36 fhajny Exp $
+$NetBSD: patch-tools_gyp_pylib_gyp_common.py,v 1.3 2019/01/10 13:02:21 ryoon Exp $
 
-Add support for NetBSD and DragonFly.
+Add support for DragonFly.
 --- tools/gyp/pylib/gyp/common.py.orig	2017-05-03 02:00:16.000000000 +0000
 +++ tools/gyp/pylib/gyp/common.py
 @@ -427,6 +427,8 @@ def GetFlavor(params):
