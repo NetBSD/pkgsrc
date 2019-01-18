5e66bc1171075cb1d9a466d84dc195156377e138
youri <youri@pkgsrc.org>
2019-01-18 17:53:27 +0000

    xfce4-battery-plugin: update to 1.1.2:
    1.1.2 (2019/01/13):
    ===================
    * Listen for UPower events for power source changes (Bug #14978, thanks to Alexander Butenko)
    * Reset averages when a battery is removed/added (Bug #14930, thanks to John Lindgren)
    * Re-detect battery presence every 30 seconds (Bug #14930, thanks to John Lindgren)
    * Fix null pointer dereference with no battery present (Bug #14930, thanks to John Lindgren)
    * Remove deprecated /proc/acpi support in favor of sysfs. (Thanks to John Lindgren)
    * Remove APM support on FreeBSD as well, it doesn't even build. (Thanks to John Lindgren)
    * Remove historical APM support for Linux, support ACPI only. (Thanks to John Lindgren)
    * Initially show/hide widgets according to user config (Bug #9594, thanks to John Lindgren)
    
    1.1.1 (2018/09/22):
    ===================
    * Avoid memory leaks
    * Bump to minimal Gtk to 3.16
    * Only set progressbar width or height depending on the orientation (Bug #12942)
    * Create a single GtkCssProvider in setup_battmon() (Bug #12975)
    * Improved spacing and layout in dialogs (Bug #14659)

--- sysutils/xfce4-battery-plugin/patches/patch-panel-plugin_battery.c
+++ sysutils/xfce4-battery-plugin/patches/patch-panel-plugin_battery.c
@@ -1,78 +1,44 @@
-$NetBSD: patch-panel-plugin_battery.c,v 1.3 2018/11/25 20:55:42 martin Exp $
+$NetBSD: patch-panel-plugin_battery.c,v 1.4 2019/01/18 17:53:27 youri Exp $
 
 Fix for NetBSD.
 
---- panel-plugin/battery.c.orig	2016-10-29 08:37:38.000000000 +0200
-+++ panel-plugin/battery.c	2018-11-25 21:49:59.741230700 +0100
-@@ -26,13 +26,28 @@
+--- panel-plugin/battery.c.orig	2019-01-18 17:56:56.433182652 +0000
++++ panel-plugin/battery.c
+@@ -26,13 +26,32 @@
  #include <config.h>
  #endif
  
--#if (defined(__FreeBSD__) || defined(__FreeBSD_kernel__)) && (defined(i386) || defined(__i386__))
-+#ifdef __NetBSD__
-+#include <sys/param.h>
-+#include <sys/ioctl.h>
-+#include <dev/apm/apmbios.h>
-+#include <dev/apm/apmio.h>
-+#define APMDEVICE "/dev/apm"
-+#define _ACPI_APM_BATT_UNKNOWN	0xffff /* from sys/dev/acpi/acpi_apm.c */
-+#elif (defined(__FreeBSD__) || defined(__FreeBSD_kernel__)) && (defined(i386) || defined(__i386__))
- #include <machine/apm_bios.h>
--#elif (defined(__OpenBSD__) || defined(__NetBSD__))
-+#elif (defined(__OpenBSD__)
- #include <sys/param.h>
- #include <sys/ioctl.h>
-+#if defined(__sparc64__)
-+#include <sparc/apmvar.h>
-+#elif defined(__x86_64__)
-+#include <dev/apm/apmbios.h>
-+#include <dev/apm/apmio.h>
-+#else
- #include <machine/apmvar.h>
-+#endif
- #define APMDEVICE "/dev/apm"
-+#define _ACPI_APM_BATT_UNKNOWN	0xffff /* from sys/dev/acpi/acpi_apm.c */
- #elif __linux__
- #include <libapm.h>
- #endif
-@@ -162,7 +177,7 @@ init_options(t_battmon_options *options)
- gboolean
- detect_battery_info(t_battmon *battmon)
- {
--#ifdef __FreeBSD__
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
-   /* This is how I read the information from the APM subsystem under
-      FreeBSD.  Each time this functions is called (once every second)
-      the APM device is opened, read from and then closed.
-@@ -401,7 +416,7 @@ update_apm_status(t_battmon *battmon)
-         acline = apm.ac_line_status ? TRUE : FALSE;
- 
-     }
--#elif __FreeBSD__
-+#elif defined(__FreeBSD__) || defined(__DragonFly__)
-     else {
-  /* This is how I read the information from the APM subsystem under
-      FreeBSD.  Each time this functions is called (once every second)
-@@ -500,6 +515,11 @@ battmon.c:241: for each function it appe
-     if(battmon->options.display_percentage && charge > 0 && !(battmon->options.hide_when_full && acline && charge >= 99)){
-         gtk_widget_show(GTK_WIDGET(battmon->charge));
-         gtk_widget_show(GTK_WIDGET(battmon->timechargealignment));
-+#if defined(__NetBSD__) || defined(__OpenBSD__)
-+	if (apm.battery_state == APM_BATT_ABSENT)
-+		g_snprintf(buffer, sizeof(buffer), "--%%");
-+	else
-+#endif
-         g_snprintf(buffer, sizeof(buffer),"%d%% ", charge);
-         gtk_label_set_text(battmon->charge,buffer);
-     } else {
-@@ -509,6 +529,10 @@ battmon.c:241: for each function it appe
-     if (battmon->options.display_time && time_remaining > 0 && !(battmon->options.hide_when_full && acline && charge >= 99 )){
-         gtk_widget_show(GTK_WIDGET(battmon->rtime));
-         gtk_widget_show(GTK_WIDGET(battmon->timechargealignment));
+-#if (defined(__OpenBSD__) || defined(__NetBSD__))
+-#include <sys/param.h>
+-#include <sys/ioctl.h>
+-#include <machine/apmvar.h>
+-#define APMDEVICE "/dev/apm"
 +#if defined(__NetBSD__)
-+	if (acline || time_remaining == _ACPI_APM_BATT_UNKNOWN)
-+		g_snprintf(buffer, sizeof(buffer), "--:--");
-+#endif
-         g_snprintf(buffer, sizeof(buffer),"%02d:%02d ",time_remaining/60,time_remaining%60);
-         gtk_label_set_text(battmon->rtime,buffer);
++	#include <sys/param.h>
++	#include <sys/ioctl.h>
++	#include <dev/apm/apmbios.h>
++	#include <dev/apm/apmio.h>
++	#define APMDEVICE "dev/apm"
++	#define _ACPI_APM_BATT_UNKNOWN 0xffff /* from sys/dev/apci/acpi_apm.c */
++#elif (defined(__FreeBSD__) || defined(__FreeBSD_kernel__)) && \
++				(defined(i386) || defined(__i386__))
++	#include <machine/apm_bios.h>
++#elif defined(__OpenBSD__)
++	#include <sys/param.h>
++	#include <sys/ioctl.h>
++	#if defined(__sparc64__)
++		#include <sparc/apmvar.h>
++	#elif defined(__x86_64__)
++		#include <dev/apm/apmbios.h>
++		#include <dev/apm/apmio.h>
++	#else
++		#include <machine/apmvar.h>
++	#endif
++	#define APMDEVICE "/dev/apm"
++	#define _ACPI_APM_BATT_UNKNOWN 0xffff /* from sys/dev/apci/acpi_apm.c */
+ #endif
  
++
+ #include <sys/stat.h>
+ #include <sys/types.h>
+ #include <fcntl.h>
