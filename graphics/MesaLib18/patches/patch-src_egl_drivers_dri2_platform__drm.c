775f0bb8cabb5a2a4b4a5900d592d736e9862726
tnn <tnn@pkgsrc.org>
2019-01-20 09:50:28 +0000

    MesaLib18: more patch cleanup
    patch-configure.ac: configure test for HAVE_OPEN_MEMSTREAM
    patch-src_amd_common_ac__debug.c: ditto
    patch-src_gallium_drivers_radeonsi_si__state__shaders.c: ditto
    patch-src_gallium_auxiliary_util_u__helpers.c: add comment
    patch-src_util_u__thread.h: add comment
    patch-src_egl_drivers_dri2_platform__drm.c: fix incomplete #ifdef
    patch-src_mesa_tnl_t__pipeline.c: delete obsolete patch

--- graphics/MesaLib18/patches/patch-src_egl_drivers_dri2_platform__drm.c
+++ graphics/MesaLib18/patches/patch-src_egl_drivers_dri2_platform__drm.c
@@ -1,15 +1,17 @@
-$NetBSD: patch-src_egl_drivers_dri2_platform__drm.c,v 1.1 2018/10/07 23:49:31 ryoon Exp $
+$NetBSD: patch-src_egl_drivers_dri2_platform__drm.c,v 1.2 2019/01/20 09:50:28 tnn Exp $
 
 netbsd-5 build fix
 
---- src/egl/drivers/dri2/platform_drm.c.orig	2017-10-19 12:23:53.000000000 +0000
+--- src/egl/drivers/dri2/platform_drm.c.orig	2019-01-17 11:26:22.000000000 +0000
 +++ src/egl/drivers/dri2/platform_drm.c
-@@ -688,7 +688,9 @@ dri2_initialize_drm(_EGLDriver *drv, _EG
+@@ -725,7 +725,11 @@ dri2_initialize_drm(_EGLDriver *drv, _EG
        }
        dri2_dpy->own_device = true;
     } else {
 +#ifdef F_DUPFD_CLOEXEC
        dri2_dpy->fd = fcntl(gbm_device_get_fd(gbm), F_DUPFD_CLOEXEC, 3);
++#else
++      dri2_dpy->fd = -1;
 +#endif
        if (dri2_dpy->fd < 0) {
           err = "DRI2: failed to fcntl() existing gbm device";
