4e92b53a7462d5f1fac32ca17b746ff004a0bdac
tnn <tnn@pkgsrc.org>
2019-01-20 09:50:28 +0000

    MesaLib18: more patch cleanup
    patch-configure.ac: configure test for HAVE_OPEN_MEMSTREAM
    patch-src_amd_common_ac__debug.c: ditto
    patch-src_gallium_drivers_radeonsi_si__state__shaders.c: ditto
    patch-src_gallium_auxiliary_util_u__helpers.c: add comment
    patch-src_util_u__thread.h: add comment
    patch-src_egl_drivers_dri2_platform__drm.c: fix incomplete #ifdef
    patch-src_mesa_tnl_t__pipeline.c: delete obsolete patch

--- graphics/MesaLib18/patches/patch-src_amd_common_ac__debug.c
+++ graphics/MesaLib18/patches/patch-src_amd_common_ac__debug.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_amd_common_ac__debug.c,v 1.1 2019/01/19 17:47:51 tnn Exp $
+$NetBSD: patch-src_amd_common_ac__debug.c,v 1.2 2019/01/20 09:50:28 tnn Exp $
 
 #if'out some debug code on SunOS. There is no open_memstream(3)
 
@@ -8,7 +8,7 @@ $NetBSD: patch-src_amd_common_ac__debug.c,v 1.1 2019/01/19 17:47:51 tnn Exp $
  
  	char *out;
  	size_t outsize;
-+#if !defined(__sun)
++#if defined(HAVE_OPEN_MEMSTREAM)
  	FILE *memf = open_memstream(&out, &outsize);
  	ib.f = memf;
  	ac_do_parse_ib(memf, &ib);
