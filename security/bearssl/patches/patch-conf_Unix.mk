f7940d8aadaf5b3ef134b7bcf732f07a66045f9b
agc <agc@pkgsrc.org>
2019-01-21 06:59:58 +0000

    Update security/bearssl from 0.5 to 0.6
    Changes since previous version:
    
    + Added general-purpose implementations of EAX and CCM modes (including
    shared precomputation support for EAX).
    + Added general-purpose RSA/OAEP implementation.
    + Added general-purpose HKDF implementation.
    + Added support for CCM and CCM_8 TLS cipher suites (RFC 6655 and RFC 7251).
    + Added RSA and EC key generation.
    + Added private key encoding support ("raw" and PKCS#8 formats, both
    in DER and PEM, for RSA and EC key pairs).
    + Made Base64 encoding/decoding constant-time (with regards to the
    encoded data bytes).
    + Added a generic API for random seed providers.
    + Added an extra DRBG based on AES/CTR + Hirose construction for reseeding.
    + Some cosmetic fixes to avoid warnings with picky compilers.
    + Makefile fix to achieve compatibility with OpenBSD.
    + Fixed a bug in bit length computation for big integers (this was
    breaking RSA signatures with some specific implementations and key lengths).
    + Made SSL/TLS client stricter in cipher suite selection (to align with
    server behaviour).

--- security/bearssl/patches/patch-conf_Unix.mk
+++ security/bearssl/patches/patch-conf_Unix.mk
@@ -1,12 +1,13 @@
-$NetBSD: patch-conf_Unix.mk,v 1.1 2017/09/22 22:00:57 wiedi Exp $
+$NetBSD: patch-conf_Unix.mk,v 1.2 2019/01/21 06:59:58 agc Exp $
 
 Don't overwrite LDFLAGS
---- conf/Unix.mk.orig	2017-04-03 19:42:14.000000000 +0000
-+++ conf/Unix.mk
-@@ -53,7 +53,7 @@ LDDLLOUT = -o
+
+--- conf/Unix.mk.orig	2018-08-14 13:41:54.000000000 -0700
++++ conf/Unix.mk	2019-01-20 22:50:36.191161113 -0800
+@@ -53,7 +53,7 @@
  
  # Static linker.
- LD = gcc
+ LD = cc
 -LDFLAGS = 
 +#LDFLAGS = 
  LDOUT = -o 
