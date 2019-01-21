a7fbfb70dff89445192f2a2ede5179d2c20203f2
markd <markd@pkgsrc.org>
2019-01-21 10:12:12 +0000

    texstudio: update to 2.12.14
    TeXstudio 2.12.14
    ----------------
    - fix bug that not all available GUI languages are shown
    - tweak fuzzy completer
    - fix template presentation
    
    TeXstudio 2.12.12
    ----------------
    - improve macro handling, provide gui that directly downloads macros from
      github.com/texstudio-org/texstudio-macro
    - add fuzzy completion mode
    - add cwls
    - fix rare bug that the character appear reverse order
    - bug fixes
    
    TeXstudio 2.12.10
    ----------------
    - improvement recognized latex commands and packages for completion/syntax
      checking
    - complete package names when they contain minus
    - do not clean .git subfolder with auxiliary files
    - pdf viewer: improved previous page jumping, fix embedded movie boxes (when
      compiled with phonon)
    - option to use qt file dialog rather than system file dialog
    - fix structure view with Qt >5.10
    - bug fixes
    
    TeXstudio 2.12.8
    ----------------
    - improved tokenizer for syntax checking and completion
    - pressing Ctrl+F while the editor search has focus opens the extended
      search, i.e., you'll get extended search by pressing Ctrl+F twice
    - automatically insert math delimiters ($) when inserting a math command
      outside of a math env (can be turned off, default on)
    - change copy behavior of multiple cursors from selection order to line
      order
    - file list is now a transparent popup window: closes when clicked outside
    - combine file conflict and show diff dialog
    - fix file watcher not recognizing multiple changes to a file in quick
      succession
    - improved detection of package names when opening texdoc
    - new symbol panel layout
    - some touch scrolling on certain panels
    - updated synctex and poppler libraries for pdf viewer
    - fix undo when a snippet is inserted and a selection is inserted as well
    - fix some parts of the editor background may not be drawn in the correct
      color
    - build for OSX High Sierra
    - various other fixes

--- editors/texstudio/patches/patch-pdfviewer_PDFDocument.cpp
+++ editors/texstudio/patches/patch-pdfviewer_PDFDocument.cpp
@@ -1,9 +1,9 @@
-$NetBSD: patch-pdfviewer_PDFDocument.cpp,v 1.1 2018/10/31 12:11:03 jperkin Exp $
+$NetBSD: patch-pdfviewer_PDFDocument.cpp,v 1.2 2019/01/21 10:12:13 markd Exp $
 
 Avoid ambiguous function call.
 
---- pdfviewer/PDFDocument.cpp.orig	2017-08-12 12:00:03.000000000 +0000
-+++ pdfviewer/PDFDocument.cpp
+--- src/pdfviewer/PDFDocument.cpp.orig	2017-08-12 12:00:03.000000000 +0000
++++ src/pdfviewer/PDFDocument.cpp
 @@ -3095,7 +3095,7 @@ retryNow:
  			pdfWidget->setFocus();
  
