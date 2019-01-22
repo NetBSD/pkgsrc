861f0a50cffef4ab7e9d5bdf22c71ca291d4dda0
wiz <wiz@pkgsrc.org>
2019-01-22 21:50:47 +0000

    libdrm: update to 2.4.97.
    Alex Deucher (1):
          amdgpu: update to latest marketing names from 18.50
    
    Andrey Grodzovsky (3):
          amdgpu/test: Add illegal register and memory access test v2
          amdgpu/test: Disable deadlock tests for all non gfx8/9 ASICs.
          amdgpu/test: Enable deadlock test for CI family (gfx7)
    
    Christian König (1):
          amdgpu: add VM test to exercise max/min address space
    
    Daniel Vetter (1):
          doc: Rename README&CONTRIBUTING to .rst
    
    Eric Anholt (2):
          Avoid hardcoded strlens in drmParseSubsystemType().
          drm: Attempt to parse SPI devices as platform bus devices.
    
    Eric Engestrom (6):
          xf86drmHash: remove unused loop variable
          meson: fix typo in compiler flag
          tests: skip drmdevice test if the machine doesn't have any drm device
          freedreno: remove always-defined #ifdef
          xf86atomic: #undef internal define
          README: reflow the project description to improve readability
    
    François Tigeot (2):
          xf86drm: implement drmParseSubsystemType for DragonFly
          libdrm: Use DRM_IOCTL_GET_PCIINFO on DragonFly
    
    Leo Liu (1):
          tests/amdgpu/vcn: fix the nop command in IBs
    
    Lucas De Marchi (2):
          gitignore: sort file
          gitignore: add _build
    
    Marek Olšák (3):
          amdgpu: update amdgpu_drm.h
          amdgpu: add a faster BO list API
          Bump the version to 2.4.97
    
    Mauro Rossi (1):
          android: Fix 32-bit app crashing in 64-bit Android

--- x11/libdrm/patches/patch-libsync.h
+++ x11/libdrm/patches/patch-libsync.h
@@ -1,11 +1,11 @@
-$NetBSD: patch-libsync.h,v 1.2 2018/12/25 01:13:57 sevan Exp $
+$NetBSD: patch-libsync.h,v 1.3 2019/01/22 21:50:47 wiz Exp $
 
 Fix public header on SunOS.
 If ETIME is not defined, use ETIMEDOUT.
 
---- libsync.h.orig	2018-10-16 14:49:03.000000000 +0000
+--- libsync.h.orig	2019-01-22 16:32:41.000000000 +0000
 +++ libsync.h
-@@ -33,8 +33,15 @@
+@@ -33,9 +33,16 @@
  #include <stdint.h>
  #include <string.h>
  #include <sys/ioctl.h>
@@ -14,10 +14,11 @@ If ETIME is not defined, use ETIMEDOUT.
 +#endif
  #include <sys/poll.h>
  #include <unistd.h>
-+
+ 
 +#ifndef ETIME
 +#define ETIME ETIMEDOUT
 +#endif
- 
++
  #if defined(__cplusplus)
  extern "C" {
+ #endif
