0c6296e4e7e3b13ef9e47be3aa22585c6d0d4b29
gdt <gdt@pkgsrc.org>
2019-01-22 15:28:45 +0000

    viking: Update to 1.7
    packaging changes:
    
      - remove patch fragment applied upstream
      - add a request to clarify and upstream the sun patch
      - update comments about why things are or are not included.  Add a
        note to consider gtk3.
      - disable geoclue, oauth, and zip, pending decision on defaults and
        options (magic and mapnik remain disabled)
      - remove perl dependencies, apparently no longer preesent upstream
      - depend on libbzip2
      - depend on png (was already indirect)
      - depend on nettle (MD5 for thumbnail cache)
    
    upstream changes:
    
    == New features since 1.6.2 ==
    * KMZ Map File Support
    * Apply Button for Layer Properties
    * Support More GPX Fields
    * Updated Default Map: Mapbox Outdoors
    * GeoClue Support for Automatic Location
    * Faster GPX File Loading
    * Waypoint / EXIF GPS Image Direction Support
    * Support More Garmin Waypoint Symbols
    * Support Garmin Device GPX Point Limits
    * Windows GPSD Client
    * Support 'External' GPX File Layers in .vik Files
    * Usage of HTTPS addresses
    * Support OAuth for OSM
    * Support High Resolution Displays
    * Track+Route Split+Join Tools
    * Calculation of Eddington Number
    * Calendar Display
    * Can Select a Result from the Online Search Results
    * Reproducible Builds
    * 64bit Windows Build only
    * Travis Build + CI
    * Consistent Licensing of GPL2+
    * Many Internal Improvements + Fixes
    
    (bug fixes)

--- geography/viking/patches/patch-src_gpspoint.c
+++ geography/viking/patches/patch-src_gpspoint.c
@@ -1,12 +1,14 @@
-$NetBSD: patch-aa,v 1.3 2013/04/16 01:08:47 gdt Exp $
+$NetBSD: patch-src_gpspoint.c,v 1.1 2019/01/22 15:28:45 gdt Exp $
 
 Work around a bug in NetBSD.  See PR standards/40695.  Bug is fixed in
 -current but not -5; revisit if the workaround causes problems.
+While 5 is very old, keep the patch because somebody might be using it
+and the pach is not problematic.
 (Not filed upstream because this is a NetBSD bug.)
 
---- src/gpspoint.c.orig	2010-12-13 01:55:55.000000000 +0000
+--- src/gpspoint.c.orig	2018-12-23 10:48:06.000000000 +0000
 +++ src/gpspoint.c
-@@ -81,8 +81,25 @@ static gdouble line_altitude = VIK_DEFAU
+@@ -94,8 +94,25 @@ static gdouble line_altitude = VIK_DEFAU
  static gboolean line_visible = TRUE;
  
  static gboolean line_extended = FALSE;
@@ -24,11 +26,11 @@ Work around a bug in NetBSD.  See PR standards/40695.  Bug is fixed in
 + */
 +static gdouble line_speed = NAN;
 +static gdouble line_course = NAN;
-+#endif
++#endif /* _GNUC_PREREQ__(3,3) */
 +#else /* NetBSD */
  static gdouble line_speed = NAN;
  static gdouble line_course = NAN;
 +#endif /* NetBSD */
  static gint line_sat = 0;
  static gint line_fix = 0;
- /* other possible properties go here */
+ static gdouble line_hdop = VIK_DEFAULT_DOP;
