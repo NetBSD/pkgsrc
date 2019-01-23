60bf84fce6de9bc5dd1a86ed3366fff27edd1eec
jperkin <jperkin@pkgsrc.org>
2019-01-23 15:44:34 +0000

    clang: Fix SunOS include paths.  Bump PKGREVISION.

--- lang/clang/patches/patch-lib_Driver_ToolChains_Solaris.h
+++ lang/clang/patches/patch-lib_Driver_ToolChains_Solaris.h
@@ -1,10 +1,18 @@
-$NetBSD: patch-lib_Driver_ToolChains_Solaris.h,v 1.2 2018/12/09 20:04:38 adam Exp $
+$NetBSD: patch-lib_Driver_ToolChains_Solaris.h,v 1.3 2019/01/23 15:44:34 jperkin Exp $
 
 Override AddCXXStdlibLibArgs().
 
 --- lib/Driver/ToolChains/Solaris.h.orig	2018-02-05 23:59:13.000000000 +0000
 +++ lib/Driver/ToolChains/Solaris.h
-@@ -67,6 +67,9 @@ public:
+@@ -62,11 +62,17 @@ public:
+                             llvm::opt::ArgStringList &CC1Args) const override;
+ 
+   void
++  addLibCxxIncludePaths(const llvm::opt::ArgList &DriverArgs,
++                        llvm::opt::ArgStringList &CC1Args) const override;
++  void
+   addLibStdCxxIncludePaths(const llvm::opt::ArgList &DriverArgs,
+                            llvm::opt::ArgStringList &CC1Args) const override;
  
    SanitizerMask getSupportedSanitizers() const override;
    unsigned GetDefaultDwarfVersion() const override { return 2; }
