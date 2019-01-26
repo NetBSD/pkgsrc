0ca7ef31e03bd8f1fd1df948e4ec5199826e0cfb
tnn <tnn@pkgsrc.org>
2019-01-26 21:17:20 +0000

    libLLVM: update to 7.0.1, adapted from lang/llvm

--- lang/libLLVM/patches/patch-tools_llvm-config_llvm-config.cpp
+++ lang/libLLVM/patches/patch-tools_llvm-config_llvm-config.cpp
@@ -1,24 +1,26 @@
-$NetBSD: patch-tools_llvm-config_llvm-config.cpp,v 1.5 2017/12/08 02:14:44 gdt Exp $
+$NetBSD: patch-tools_llvm-config_llvm-config.cpp,v 1.6 2019/01/26 21:17:20 tnn Exp $
 
 Avoid conflict with lang/clang.
 Use -lLLVM-3.8 instead of -lLLVM-3.8.0 so we don't break unnecessarily
 on patch updates.
 
---- tools/llvm-config/llvm-config.cpp.orig	2017-01-12 19:47:38.000000000 +0000
+--- tools/llvm-config/llvm-config.cpp.orig	2018-06-23 16:50:09.000000000 +0000
 +++ tools/llvm-config/llvm-config.cpp
-@@ -332,9 +332,9 @@ int main(int argc, char **argv) {
+@@ -333,11 +333,11 @@ int main(int argc, char **argv) {
          ("-I" + ActiveIncludeDir + " " + "-I" + ActiveObjRoot + "/include");
    } else {
      ActivePrefix = CurrentExecPrefix;
 -    ActiveIncludeDir = ActivePrefix + "/include";
 +    ActiveIncludeDir = "@PREFIX@/include/libLLVM";
-     ActiveBinDir = ActivePrefix + "/bin";
+     SmallString<256> path(StringRef(LLVM_TOOLS_INSTALL_DIR));
+     sys::fs::make_absolute(ActivePrefix, path);
+     ActiveBinDir = path.str();
 -    ActiveLibDir = ActivePrefix + "/lib" + LLVM_LIBDIR_SUFFIX;
 +    ActiveLibDir = "@PREFIX@/lib/libLLVM";
      ActiveCMakeDir = ActiveLibDir + "/cmake/llvm";
      ActiveIncludeOption = "-I" + ActiveIncludeDir;
    }
-@@ -370,14 +370,14 @@ int main(int argc, char **argv) {
+@@ -373,14 +373,14 @@ int main(int argc, char **argv) {
      StaticDir = ActiveLibDir;
    } else if (HostTriple.isOSDarwin()) {
      SharedExt = "dylib";
@@ -35,7 +37,7 @@ on patch updates.
      StaticExt = "a";
      StaticDir = SharedDir = ActiveLibDir;
      StaticPrefix = SharedPrefix = "lib";
-@@ -390,7 +390,7 @@ int main(int argc, char **argv) {
+@@ -393,7 +393,7 @@ int main(int argc, char **argv) {
  
    bool DyLibExists = false;
    const std::string DyLibName =
@@ -44,7 +46,7 @@ on patch updates.
  
    // If LLVM_LINK_DYLIB is ON, the single shared library will be returned
    // for "--libs", etc, if they exist. This behaviour can be overridden with
-@@ -491,7 +491,7 @@ int main(int argc, char **argv) {
+@@ -494,7 +494,7 @@ int main(int argc, char **argv) {
          OS << ActiveIncludeOption << ' ' << LLVM_CXXFLAGS << '\n';
        } else if (Arg == "--ldflags") {
          OS << ((HostTriple.isWindowsMSVCEnvironment()) ? "-LIBPATH:" : "-L")
