fd0e6186e14ee1b0f9eeb0c66cb4f980bdfdbb2e
jaapb <jaapb@pkgsrc.org>
2019-01-29 12:43:53 +0000

    Updated devel/js_of_ocaml to 3.3.0.
    Changes include:
    - version works with tyxml 4.3.0
    - remove support for camlp4
    and other improvements and bugfixes.

--- devel/js_of_ocaml/options.mk
+++ devel/js_of_ocaml/options.mk
@@ -1,14 +1,14 @@
-# $NetBSD: options.mk,v 1.6 2018/11/29 11:59:21 jaapb Exp $
+# $NetBSD: options.mk,v 1.7 2019/01/29 12:43:53 jaapb Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.js_of_ocaml
-PKG_SUPPORTED_OPTIONS=	ocaml-tyxml camlp4
+PKG_SUPPORTED_OPTIONS=	ocaml-tyxml
 PKG_SUGGESTED_OPTIONS=	ocaml-tyxml
 
 .include "../../mk/bsd.prefs.mk"
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	tyxml camlp4
+PLIST_VARS+=	tyxml
 
 ###
 ### TyXML support
@@ -19,17 +19,5 @@ PLIST_VARS+=	tyxml camlp4
 PLIST.tyxml=	yes
 DUNE_BUILD_PACKAGES+=	js_of_ocaml-tyxml
 OPAM_INSTALL_FILES+=	js_of_ocaml-tyxml
-.else
-.endif
-
-###
-### Camlp4 support
-###
-.if !empty(PKG_OPTIONS:Mcamlp4)
-.include "../../lang/camlp4/buildlink3.mk"
-DEPENDS+=	ocaml-deriving-ocsigen>=0.8.1:../../devel/ocaml-deriving-ocsigen
-PLIST.camlp4=	yes
-DUNE_BUILD_PACKAGES+=	js_of_ocaml-camlp4
-OPAM_INSTALL_FILES+=	js_of_ocaml-camlp4
 .else
 .endif
