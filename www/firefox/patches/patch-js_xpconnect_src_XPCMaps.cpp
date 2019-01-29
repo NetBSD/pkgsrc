c2e566dfca0a5e4123cb815d45575f1337b37253
ryoon <ryoon@pkgsrc.org>
2019-01-29 16:28:22 +0000

    Updatet to 65.0
    Changelog:
    New
    
        Enhanced tracking protection: Simplified content blocking settings give users standard, strict, and custom options to control online trackers. A redesigned content blocking section in the site information panel (viewed by expanding the small “i” icon in the address bar) shows what Firefox detects and blocks on each website you visit. To learn more about content blocking, visit the Mozilla Blog.
    
        A better experience for multilingual users: An updated Language section in Preferences allows users to install multiple language packs and order language preferences for Firefox and websites, without having to download locale-specific versions.
    
        Support for Handoff on macOS: Continue browsing across devices. Pick up where you left off with iOS (via Firefox or Safari) on Firefox on Mac.
    
        A better video streaming experience for Windows users: Firefox now supports the next-generation, royalty-free video compression technology called AV1. Read about Mozilla’s contribution to this new open standard.
    
        Improved performance and web compatibility, with support for the WebP image format: WebP brings the same image quality as existing formats at smaller file sizes, which saves bandwidth and speeds up page load.
    
    Fixed
    
        Various security fixes.
    
    Changed
    
        Enhanced security for macOS, Linux, and Android users via stronger stack smashing protection which is now enabled by default for all platforms. "Stack smashing" is a common security attack in which malicious actors corrupt or take control of a vulnerable program.
    
        Firefox will now warn you when closing a window (regardless of whether you have automatic session restore enabled for restart).
    
        Easier performance management: The revamped Task Manager page found at about:performance now reports memory usage for tabs and add-ons.
    
        Improved the pop-up blocker to prevent multiple pop-up windows from being opened by websites at the same time.
    
    Security fixes:
    Not available yet.

--- www/firefox/patches/patch-js_xpconnect_src_XPCMaps.cpp
+++ www/firefox/patches/patch-js_xpconnect_src_XPCMaps.cpp
@@ -1,21 +1,21 @@
-$NetBSD: patch-js_xpconnect_src_XPCMaps.cpp,v 1.1 2018/11/07 12:55:11 martin Exp $
+$NetBSD: patch-js_xpconnect_src_XPCMaps.cpp,v 1.2 2019/01/29 16:28:22 ryoon Exp $
 
 Avoid alignement issues, see bugzilla
 see https://bugzilla.mozilla.org/show_bug.cgi?id=1434726
 
---- js/xpconnect/src/XPCMaps.cpp.orig	2018-10-31 01:08:09.000000000 +0100
-+++ js/xpconnect/src/XPCMaps.cpp	2018-11-07 13:03:15.754241064 +0100
-@@ -23,7 +23,13 @@ using namespace mozilla;
- static PLDHashNumber
- HashIIDPtrKey(const void* key)
- {
+--- js/xpconnect/src/XPCMaps.cpp.orig	2019-01-18 00:20:42.000000000 +0000
++++ js/xpconnect/src/XPCMaps.cpp
+@@ -21,7 +21,13 @@ using namespace mozilla;
+ // nsID, not the hash of the pointer to the nsID.
+ 
+ static PLDHashNumber HashIIDPtrKey(const void* key) {
 +#ifdef __sparc__
-+    uintptr_t v;
-+    memcpy(&v, key, sizeof(v));
-+    return HashGeneric(&v);
++ uintptr_t v;
++ memcpy(&v, key, sizeof(v));
++ return HashGeneric(&v);
 +#else
-     return HashGeneric(*((uintptr_t*)key));
+   return HashGeneric(*((uintptr_t*)key));
 +#endif
  }
  
- static bool
+ static bool MatchIIDPtrKey(const PLDHashEntryHdr* entry, const void* key) {
--- www/firefox/patches/patch-toolkit_xre_glxtest.cpp
+++ www/firefox/patches/patch-toolkit_xre_glxtest.cpp
@@ -1,16 +1,16 @@
-$NetBSD: patch-toolkit_xre_glxtest.cpp,v 1.1 2015/07/05 11:55:06 martin Exp $
+$NetBSD: patch-toolkit_xre_glxtest.cpp,v 1.2 2019/01/29 16:28:22 ryoon Exp $
 
 Fix libGL filename on NetBSD,
 see https://bugzilla.mozilla.org/show_bug.cgi?id=1180498
 
---- toolkit/xre/glxtest.cpp.orig	2015-07-04 20:26:15.000000000 +0200
-+++ toolkit/xre/glxtest.cpp	2015-07-04 20:26:15.000000000 +0200
-@@ -124,7 +124,7 @@ void glxtest()
-     fatal_error("The MOZ_AVOID_OPENGL_ALTOGETHER environment variable is defined");
+--- toolkit/xre/glxtest.cpp.orig	2019-01-18 00:21:31.000000000 +0000
++++ toolkit/xre/glxtest.cpp
+@@ -116,7 +116,7 @@ void glxtest() {
+         "The MOZ_AVOID_OPENGL_ALTOGETHER environment variable is defined");
  
-   ///// Open libGL and load needed symbols /////
+     ///// Open libGL and load needed symbols /////
 -#ifdef __OpenBSD__
 +#if defined(__OpenBSD__) || defined(__NetBSD__)
-   #define LIBGL_FILENAME "libGL.so"
+ #define LIBGL_FILENAME "libGL.so"
  #else
-   #define LIBGL_FILENAME "libGL.so.1"
+ #define LIBGL_FILENAME "libGL.so.1"
