599bbd0fe2be8ada161348c22c0c23353c34444c
ryoon <ryoon@pkgsrc.org>
2019-01-29 16:28:22 +0000

    Updatet to 65.0
    Changelog:
    New
    
        Enhanced tracking protection: Simplified content blocking settings give users standard, strict, and custom options to control online trackers. A redesigned content blocking section in the site information panel (viewed by expanding the small “i” icon in the address bar) shows what Firefox detects and blocks on each website you visit. To learn more about content blocking, visit the Mozilla Blog.
    
        A better experience for multilingual users: An updated Language section in Preferences allows users to install multiple language packs and order language preferences for Firefox and websites, without having to download locale-specific versions.
    
        Support for Handoff on macOS: Continue browsing across devices. Pick up where you left off with iOS (via Firefox or Safari) on Firefox on Mac.
    
        A better video streaming experience for Windows users: Firefox now supports the next-generation, royalty-free video compression technology called AV1. Read about Mozilla’s contribution to this new open standard.
    
        Improved performance and web compatibility, with support for the WebP image format: WebP brings the same image quality as existing formats at smaller file sizes, which saves bandwidth and speeds up page load.
    
    Fixed
    
        Various security fixes.
    
    Changed
    
        Enhanced security for macOS, Linux, and Android users via stronger stack smashing protection which is now enabled by default for all platforms. "Stack smashing" is a common security attack in which malicious actors corrupt or take control of a vulnerable program.
    
        Firefox will now warn you when closing a window (regardless of whether you have automatic session restore enabled for restart).
    
        Easier performance management: The revamped Task Manager page found at about:performance now reports memory usage for tabs and add-ons.
    
        Improved the pop-up blocker to prevent multiple pop-up windows from being opened by websites at the same time.
    
    Security fixes:
    Not available yet.

--- www/firefox/patches/patch-xpcom_components_nsComponentManager.cpp
+++ www/firefox/patches/patch-xpcom_components_nsComponentManager.cpp
@@ -1,12 +1,12 @@
-$NetBSD: patch-xpcom_components_nsComponentManager.cpp,v 1.2 2018/11/14 18:51:40 wiz Exp $
+$NetBSD: patch-xpcom_components_nsComponentManager.cpp,v 1.3 2019/01/29 16:28:22 ryoon Exp $
 
 Ensure symbols aren't pruned from being unused.
 
---- xpcom/components/nsComponentManager.cpp.orig	2018-10-31 00:08:15.000000000 +0000
+--- xpcom/components/nsComponentManager.cpp.orig	2019-01-18 00:21:31.000000000 +0000
 +++ xpcom/components/nsComponentManager.cpp
-@@ -298,8 +298,13 @@ extern mozilla::Module const* const __st
+@@ -267,8 +267,13 @@ NSMODULE_ASAN_BLACKLIST __declspec(alloc
  
- #  if defined(__ELF__) || (defined(_WIN32) && defined(__GNUC__))
+ #if defined(__ELF__) || (defined(_WIN32) && defined(__GNUC__))
  
 +#ifdef __sun
 +extern "C" mozilla::Module const* const __start_kPStaticModules = nullptr;
@@ -16,5 +16,5 @@ Ensure symbols aren't pruned from being unused.
  extern "C" mozilla::Module const* const __stop_kPStaticModules;
 +#endif
  
- #  elif defined(__MACH__)
+ #elif defined(__MACH__)
  
