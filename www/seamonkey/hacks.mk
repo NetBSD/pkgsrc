0a244190f71dd0079724496a1dd510a7ea5e52be
tnn <tnn@pkgsrc.org>
2019-01-29 22:33:57 +0000

    remove obsolete hacks.mk & reduce diffs between mozilla derivative packages

--- www/seamonkey/hacks.mk
+++
@@ -1,6 +0,0 @@
-# $NetBSD: hacks.mk,v 1.5 2013/05/23 13:25:30 ryoon Exp $
-
-# workround for link of seamonkey-bin etc.
-LDFLAGS+=	${COMPILER_RPATH_FLAG}${PREFIX}/lib/seamonkey
-
-.include "../../www/firefox/hacks.mk"
