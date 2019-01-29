149f60720535d2fdc41bed8a10342b1407ebf82e
bsiegert <bsiegert@pkgsrc.org>
2019-01-29 18:54:03 +0000

    Update harfbuzz to 2.3.0.
    -   Fix regression on big-endian architectures. Ouch!
    -   Misc bug and build fixes.
    -   Fix subsetting of simple GSUB/GDEF.
    -   Merge CFF / CFF2 support contributed by Adobe. This mostly involves
        the subsetter, but also get_glyph_extents on CFF fonts.

--- fonts/harfbuzz/patches/patch-src_hb.hh
+++ fonts/harfbuzz/patches/patch-src_hb.hh
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_hb.hh,v 1.1 2018/11/23 09:50:55 abs Exp $
+$NetBSD: patch-src_hb.hh,v 1.2 2019/01/29 18:54:03 bsiegert Exp $
 
 Do not play _POSIX_C_SOURCE games on NetBSD - causes issues with some pkgsrc gcc versions
 
---- src/hb.hh.orig	2018-11-04 01:41:46.000000000 +0000
+--- src/hb.hh.orig	2018-12-18 03:36:12.000000000 +0000
 +++ src/hb.hh
-@@ -35,7 +35,7 @@
- #include "config.h"
+@@ -59,7 +59,7 @@
+ # define __EXTENSIONS__ 1
  #endif
  
 -#ifndef _POSIX_C_SOURCE
-+#if !defined(_POSIX_C_SOURCE) && !defined(__NetBSD__)
++#if !defined (_POSIX_C_SOURCE) && !defined (__NetBSD__)
  #define _POSIX_C_SOURCE 200809L
  #endif
  
