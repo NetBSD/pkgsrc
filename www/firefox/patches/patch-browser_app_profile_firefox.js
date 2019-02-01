e7af42fc557cf0e0bf8a5f10ee1c3d0776209d02
ryoon <ryoon@pkgsrc.org>
2019-02-01 16:47:59 +0000

    Bump PKGREVISION
    * Use graphics/libwebp
    * Bump devel/cbindgen requirement (PR pkg/53925)
    * Enable system's addons, for example langpacks from www/firefox-l10n
    * Fix potential configure error from clock_gettime(CLOCK_MONOTONIC, ...)

--- www/firefox/patches/patch-browser_app_profile_firefox.js
+++ www/firefox/patches/patch-browser_app_profile_firefox.js
@@ -1,13 +1,16 @@
-$NetBSD: patch-browser_app_profile_firefox.js,v 1.11 2019/01/29 16:28:22 ryoon Exp $
+$NetBSD: patch-browser_app_profile_firefox.js,v 1.12 2019/02/01 16:47:59 ryoon Exp $
 
---- browser/app/profile/firefox.js.orig	2019-01-18 00:20:22.000000000 +0000
+--- browser/app/profile/firefox.js.orig	2019-01-24 18:48:38.000000000 +0000
 +++ browser/app/profile/firefox.js
-@@ -1770,6 +1770,12 @@ pref("toolkit.coverage.endpoint.base", "
+@@ -1770,6 +1770,15 @@ pref("toolkit.coverage.endpoint.base", "
  pref("prio.enabled", true);
  #endif
  
 +// Select UI locale from LANG/LC_MESSAGE environmental variables
 +pref("intl.locale.requested", "");
++
++// Enable system addons, for example langpacks from www/firefox-l10n
++pref("extensions.autoDisableScopes", 11);
 +
 +// Disable multiprocess window support. Workaround for PR 53273.
 +pref("browser.tabs.remote.autostart", false);
