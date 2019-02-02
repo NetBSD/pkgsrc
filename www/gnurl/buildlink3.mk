32ed9550395429b8ad65e450846b1623f1406efb
leot <leot@pkgsrc.org>
2019-02-02 09:00:56 +0000

    gnurl: Include all needed bl3
    (No PKGREVISION bump since there are no gnurl bl3 users at the moment.)

--- www/gnurl/buildlink3.mk
+++ www/gnurl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/02/01 15:21:29 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/02/02 09:00:56 leot Exp $
 
 BUILDLINK_TREE+=	gnurl
 
@@ -12,7 +12,9 @@ pkgbase:= gnurl
 .include "../../mk/pkg-build-options.mk"
 
 .include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/libidn2/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
+.include "../../security/gnutls/buildlink3.mk"
 .endif # GNURL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnurl
