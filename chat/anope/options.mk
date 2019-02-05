40bb1ee1e6ed24724290af11918dbc9d65fbfdf1
nia <nia@pkgsrc.org>
2019-02-05 19:10:50 +0000

    chat/anope: Update to 2.0.6
    [10 years of updates, no changelog. Based on work in pkgsrc-wip.]

--- chat/anope/options.mk
+++ chat/anope/options.mk
@@ -1,22 +1,45 @@
-# $NetBSD: options.mk,v 1.1.1.1 2006/10/20 22:40:24 adrianp Exp $
+# $NetBSD: options.mk,v 1.2 2019/02/05 19:10:50 nia Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.anope
-PKG_SUPPORTED_OPTIONS=	mysql anope-dbencryption
+PKG_OPTIONS_VAR=		PKG_OPTIONS.anope
+PKG_SUPPORTED_OPTIONS=		gnutls openssl mysql ldap pcre sqlite3
+PKG_SUGGESTED_OPTIONS=		openssl
+PLIST_VARS+=			gnutls openssl mysql ldap pcre sqlite3
 
 .include "../../mk/bsd.options.mk"
 
-###
-### Use MySQL for storing data
-###
-.if !empty(PKG_OPTIONS:Mmysql)
-.	include "../../mk/mysql.buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-mysql
+.if !empty(PKG_OPTIONS:Mgnutls)
+PLIST.gnutls=	yes
+ANOPE_EXTRAS+=	m_ssl_gnutls.cpp
+.include "../../security/gnutls/buildlink3.mk"
 .endif
 
-###
-### Encrypt sensitive data in the database (only with mysql)
-###
-.if !empty(PKG_OPTIONS:Mmysql) && !empty(PKG_OPTIONS:Manope-dbencryption)
-CONFIGURE_ARGS+=	--with-encryption
+.if !empty(PKG_OPTIONS:Mopenssl)
+PLIST.openssl=	yes
+ANOPE_EXTRAS+=	m_ssl_openssl.cpp
+.include "../../security/openssl/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mmysql)
+PLIST.mysql=	yes
+ANOPE_EXTRAS+=	m_mysql.cpp
+.include "../../mk/mysql.buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mldap)
+PLIST.ldap=	yes
+ANOPE_EXTRAS+=	m_ldap.cpp
+ANOPE_EXTRAS+=	m_ldap_authentication.cpp
+.include "../../databases/openldap-client/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mpcre)
+PLIST.pcre=	yes
+ANOPE_EXTRAS+=	m_regex_pcre.cpp
+.include "../../devel/pcre/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Msqlite3)
+PLIST.sqlite3=	yes
+ANOPE_EXTRAS+=	m_sqlite.cpp
+.include "../../databases/sqlite3/buildlink3.mk"
 .endif
