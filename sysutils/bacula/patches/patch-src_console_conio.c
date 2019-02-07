c3c827ab00570d96610e3734e1ac50fea459440a
markd <markd@pkgsrc.org>
2019-02-07 11:29:31 +0000

    bacula{,-qt*-console,-tray-monitor*}: update to 9.4.1
    7 years worth of updates.

--- sysutils/bacula/patches/patch-src_console_conio.c
+++ sysutils/bacula/patches/patch-src_console_conio.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_console_conio.c,v 1.3 2014/01/22 10:45:21 jperkin Exp $
+$NetBSD: patch-src_console_conio.c,v 1.4 2019/02/07 11:29:31 markd Exp $
 
 - need termios.h, our term.h does not include it
 - t_insert_line and t_delete_line are macros in term.h; undef them
 - spell HAVE_TERM_H correctly
 
---- src/console/conio.c.orig	2013-02-19 19:21:35.000000000 +0000
+--- src/console/conio.c.orig	2018-12-21 08:40:51.000000000 +0000
 +++ src/console/conio.c
-@@ -64,11 +64,12 @@
+@@ -54,11 +54,12 @@
  
  #ifdef HAVE_CONIO
  
@@ -14,13 +14,13 @@ $NetBSD: patch-src_console_conio.c,v 1.3 2014/01/22 10:45:21 jperkin Exp $
  #include <curses.h>
  #include <term.h>
  
- #if defined(HAVE_SUN_OS)
--#if !defined(_TERM_H)
-+#if !defined(HAVE_TERM_H)
+ #ifdef HAVE_SUN_OS
+-#ifndef _TERM_H
++#ifndef HAVE_TERM_H
  extern "C" int tgetent(void *, const char *);
  extern "C" int tgetnum(const char *);
  extern "C" char *tgetstr (const char*, char**);
-@@ -250,7 +251,9 @@ static char * getnext(void);
+@@ -235,7 +236,9 @@ static char * getnext(void);
  static char * getprev(void);
  static void putline(char *newl, int newlen);
  static void t_honk_horn(void);
