84e8c6b88f00cc34760a52cf8249c7aaeb932b72
adam <adam@pkgsrc.org>
2019-02-08 10:48:38 +0000

    py-openpyxl: updated to 2.6.0
    2.6.0:
    Bugfixes
    * Exception on tables with names containing spaces.
    * Cannot save files with existing images.
    
    2.6.-b1:
    Bugfixes
    * Cannot use read-only mode with stream
    * Hyperlinks always set on A1
    * Internal row counter not initialised when reading files
    * Exception raised on out of bounds date
    
    2.6-a1:
    Major changes
    Implement robust for merged cells so that these can be formatted the way Excel does without confusion. Thanks to Magnus Schieder.
    
    Minor changes
    Add support for worksheet scenarios
    Add read support for chartsheets
    Add method for moving ranges of cells on a worksheet
    Last version to support Python 2.7
    
    Deprecations
    Type inference and coercion for cell values

--- textproc/py-openpyxl/patches/patch-setup.py
+++ textproc/py-openpyxl/patches/patch-setup.py
@@ -1,12 +1,12 @@
-$NetBSD: patch-setup.py,v 1.1 2018/11/23 11:42:17 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2019/02/08 10:48:38 adam Exp $
 
 Prefer lxml to et_xmlfile.
 
---- setup.py.orig	2018-11-23 11:39:16.000000000 +0000
+--- setup.py.orig	2019-02-06 14:12:51.000000000 +0000
 +++ setup.py
 @@ -59,7 +59,7 @@ setup(
      license=__license__,
-     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
      install_requires=[
 -        'jdcal', 'et_xmlfile',
 +        'jdcal', 'lxml',
