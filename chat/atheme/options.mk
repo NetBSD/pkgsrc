626dd06b1b9b3e62c7e681a066ba910a19eb14a0
nia <nia@pkgsrc.org>
2019-02-09 20:13:02 +0000

    chat/atheme: Add ldap option, fix build without builtin ldap...

--- chat/atheme/options.mk
+++ chat/atheme/options.mk
@@ -1,9 +1,11 @@
-# $NetBSD: options.mk,v 1.1.1.1 2008/06/16 10:38:27 adrianp Exp $
+# $NetBSD: options.mk,v 1.2 2019/02/09 20:13:02 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.atheme
 
-PKG_SUPPORTED_OPTIONS=	ssl atheme-large-net
+PKG_SUPPORTED_OPTIONS=	ldap ssl atheme-large-net
 PKG_SUGGESTED_OPTIONS=	ssl
+
+PLIST_VARS+=		ldap
 
 .include "../../mk/bsd.options.mk"
 
@@ -15,6 +17,13 @@ PKG_SUGGESTED_OPTIONS=	ssl
 CONFIGURE_ARGS+=	--enable-ssl
 .else
 CONFIGURE_ARGS+=	--disable-ssl
+.endif
+
+.if !empty(PKG_OPTIONS:Mldap)
+.  include "../../databases/openldap-client/buildlink3.mk"
+PLIST.ldap=		yes
+.else
+CONFIGURE_ARGS+=	--without-ldap
 .endif
 
 ###
