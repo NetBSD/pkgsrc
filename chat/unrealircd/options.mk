b34d10adcf93d0a05c9c2c867385c93592292cd9
nia <nia@pkgsrc.org>
2019-02-09 15:23:15 +0000

    chat/unrealircd: Update to 4.2.1.1
    Many changes, coming from an unsupported version, but at least
    fixes CVE-2016-7144.

--- chat/unrealircd/options.mk
+++ chat/unrealircd/options.mk
@@ -1,86 +1,22 @@
-# $NetBSD: options.mk,v 1.16 2015/11/26 16:36:00 jperkin Exp $
+# $NetBSD: options.mk,v 1.17 2019/02/09 15:23:15 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.unrealircd
 
-PKG_SUPPORTED_OPTIONS=	inet6 unrealircd-nospoof unrealircd-ziplinks
-PKG_SUPPORTED_OPTIONS+= unrealircd-remoteinc ssl unrealircd-chroot
-PKG_SUPPORTED_OPTIONS+=	unrealircd-prefixaq unrealircd-showlistmodes
-PKG_SUPPORTED_OPTIONS+= unrealircd-topicisnuhost unrealircd-shunnotices
-PKG_SUPPORTED_OPTIONS+= unrealircd-operoverride-verify
+PKG_SUPPORTED_OPTIONS+=	unrealircd-remoteinc
+PKG_SUPPORTED_OPTIONS+=	unrealircd-showlistmodes
+PKG_SUPPORTED_OPTIONS+=	unrealircd-topicisnuhost unrealircd-shunnotices
+PKG_SUPPORTED_OPTIONS+=	unrealircd-operoverride-verify
 PKG_SUPPORTED_OPTIONS+=	unrealircd-no-operoverride unrealircd-disableusermod
-PKG_SUGGESTED_OPTIONS=	unrealircd-showlistmodes unrealircd-prefixaq
+PKG_SUGGESTED_OPTIONS=	unrealircd-showlistmodes
 
 .include "../../mk/bsd.options.mk"
-
-###
-### Enable IPv6 support
-###
-.if !empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGS+=	--enable-inet6
-.else
-CONFIGURE_ARGS+=	--disable-inet6
-CONFIGURE_ENV+=		ac_cv_ip6=no
-.endif
-
-###
-### Enable SSL support
-###
-.if !empty(PKG_OPTIONS:Mssl)
-.	include "../../security/openssl/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-ssl=${SSLBASE}
-.endif
-
-###
-### Enable Anti-Spoofing protection for older OS's with an insecure IPv4 stack
-###
-.if !empty(PKG_OPTIONS:Munrealircd-nospoof)
-CONFIGURE_ARGS+=	--enable-nospoof
-.endif
-
-###
-### Compile in support for ziplinks.  This compresses data sent from
-### server <-> server with zlib.
-###
-.if !empty(PKG_OPTIONS:Munrealircd-ziplinks)
-CONFIGURE_ARGS+=		--enable-ziplinks=${BUILDLINK_PREFIX.zlib}
-.	include "../../devel/zlib/buildlink3.mk"
-.endif
 
 ###
 ### Compile in support for remote include files.
 ###
 .if !empty(PKG_OPTIONS:Munrealircd-remoteinc)
 CONFIGURE_ARGS+=		--enable-libcurl=${PREFIX}
-.	include "../../www/curl/buildlink3.mk"
-.	include "../../net/libcares/buildlink3.mk"
-.endif
-
-###
-### Compile in CHROOTDIR support.
-### As far as pkgsrc goes this is just an expirement.  You should read the
-### unrealircd docs/source for further information on this.  By default
-### CHROOTDIR is defined as ${IRCD_HOME}.
-###
-.if !empty(PKG_OPTIONS:Munrealircd-chroot)
-CFLAGS+=		-DCHROOTDIR
-CFLAGS+=		-DIRC_USER=\"${UIRCD_USER}\"
-CFLAGS+=		-DIRC_GROUP=\"${UIRCD_GROUP}\"
-CONFIGURE_ARGS+=	--disable-dynamic-linking
-.else
-CONFIGURE_ARGS+=	--enable-dynamic-linking
-.endif
-
-###
-### Enable prefixes for chanadmin and chanowner.
-### This will give +a the & prefix and ~ for +q (just like +o is @)
-### Supported by the major clients:
-###	mIRC, xchat, epic, eggdrop, Klient, PJIRC, etc.
-### with the notable exceptions of:
-###	irssi, KVIrc and CGI:IRC.
-### This feature should be enabled/disabled network-wide.
-###
-.if !empty(PKG_OPTIONS:Munrealircd-prefixaq)
-CONFIGURE_ARGS+=		--enable-prefixaq
+.  include "../../www/curl/buildlink3.mk"
 .endif
 
 ###
