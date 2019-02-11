a72d3e401f728443036a9a7c3811b2f897b47592
adam <adam@pkgsrc.org>
2019-02-11 10:46:56 +0000

    libuv: updated to 1.26.0
    Changes 1.26.0:
    * doc: fix uv_get_free_memory doc
    * unix: fix epoll cpu 100% issue
    * openbsd,tcp: special handling of EINVAL on connect
    * win: simplify registry closing in uv_cpu_info()
    * src,include: define UV_MAXHOSTNAMESIZE
    * win: return product name in uv_os_uname() version
    * thread: allow specifying stack size for new thread
    * win: fix duplicate tty vt100 fn key
    * unix: don't attempt to invalidate invalid fd

--- devel/libuv/patches/patch-src_unix_thread.c
+++ devel/libuv/patches/patch-src_unix_thread.c
@@ -1,10 +1,23 @@
-$NetBSD: patch-src_unix_thread.c,v 1.4 2018/02/22 08:44:15 adam Exp $
+$NetBSD: patch-src_unix_thread.c,v 1.5 2019/02/11 10:46:56 adam Exp $
 
+Fix for systems without PTHREAD_STACK_MIN.
 Use feature test for pthread_condattr_setclock, absent in netbsd-6-0
 
---- src/unix/thread.c.orig	2018-02-21 20:25:38.000000000 +0000
+--- src/unix/thread.c.orig	2019-02-10 16:44:47.000000000 +0000
 +++ src/unix/thread.c
-@@ -568,7 +568,7 @@ int uv_cond_init(uv_cond_t* cond) {
+@@ -219,9 +219,11 @@ int uv_thread_create_ex(uv_thread_t* tid
+     pagesize = (size_t)getpagesize();
+     /* Round up to the nearest page boundary. */
+     stack_size = (stack_size + pagesize - 1) &~ (pagesize - 1);
++#ifdef PTHREAD_STACK_MIN
+     if (stack_size < PTHREAD_STACK_MIN)
+       stack_size = PTHREAD_STACK_MIN;
++#endif
+   }
+ 
+   if (stack_size > 0) {
+     attr = &attr_storage;
+@@ -685,7 +687,7 @@ int uv_cond_init(uv_cond_t* cond) {
    if (err)
      return UV__ERR(err);
  
