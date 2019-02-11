594d5a69a19bdcecc35513c135cc750cf05a4450
leot <leot@pkgsrc.org>
2019-02-11 22:01:18 +0000

    sc-im: Add xls option and add it to suggested option
    Bump PKGREVISION.
    
    Patch provided by Sijmen J. Mulder via tech-pkg@, thanks!

--- math/sc-im/options.mk
+++ math/sc-im/options.mk
@@ -1,13 +1,19 @@
-# $NetBSD: options.mk,v 1.1 2019/01/30 12:58:24 leot Exp $
+# $NetBSD: options.mk,v 1.2 2019/02/11 22:01:18 leot Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sc-im
-PKG_SUPPORTED_OPTIONS+=	color xlsx lua
-PKG_SUGGESTED_OPTIONS+=	color xlsx lua
+PKG_SUPPORTED_OPTIONS+=	color xls xlsx lua
+PKG_SUGGESTED_OPTIONS+=	color xls xlsx lua
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mcolor)
 CFLAGS+=	-DUSECOLORS
+.endif
+
+.if !empty(PKG_OPTIONS:Mxls)
+CFLAGS+=	-DXLS
+LDFLAGS+=	-lxlsreader
+.include "../../textproc/libxls/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mxlsx)
