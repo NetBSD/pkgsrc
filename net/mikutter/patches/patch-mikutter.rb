57d1543a03746e69fd2bc9db71ee2a9afb56d15f
tsutsui <tsutsui@pkgsrc.org>
2019-02-12 17:21:18 +0000

    mikutter: update to 3.8.5.
    Upstream changes:
    
    mikutter 3.8.5
    
    * update URLs of mikutter Web
    * [photo-support] reddit
     * thanks cob odo
    * possible crash on receiving notifications
     * thanks ncaq net
    * happy new year
    * use oEmbed API to get Gyazo images
     * thanks Shibuya Rin

--- net/mikutter/patches/patch-mikutter.rb
+++ net/mikutter/patches/patch-mikutter.rb
@@ -1,9 +1,9 @@
-$NetBSD: patch-mikutter.rb,v 1.6 2018/01/14 05:15:05 tsutsui Exp $
+$NetBSD: patch-mikutter.rb,v 1.7 2019/02/12 17:21:18 tsutsui Exp $
 
 - pkgsrc can handle location of ruby binary
   https://dev.mikutter.hachune.net/issues/889
 
---- mikutter.rb.orig	2018-01-13 17:07:11.000000000 +0000
+--- mikutter.rb.orig	2019-02-11 07:32:53.000000000 +0000
 +++ mikutter.rb
 @@ -1,7 +1,5 @@
 -#!/bin/sh
@@ -13,4 +13,4 @@ $NetBSD: patch-mikutter.rb,v 1.6 2018/01/14 05:15:05 tsutsui Exp $
 -#!ruby
  =begin rdoc
  = mikutter - the moest twitter client
- Copyright (C) 2009-2018 Toshiaki Asai
+ Copyright (C) 2009-2019 Toshiaki Asai
