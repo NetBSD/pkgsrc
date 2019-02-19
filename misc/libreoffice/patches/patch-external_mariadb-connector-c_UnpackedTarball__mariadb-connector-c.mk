231404489a68b5e15f41af39296f873f871dc2bf
ryoon <ryoon@pkgsrc.org>
2019-02-19 16:28:03 +0000

    Update to 6.2.0.3
    Changelog:
    Many bugfixes and improvements including:
    * New menu GUI
    * Fix copy and paste of table in Writer
    * REGEX function in Calc
    * Animation improvements in Impress and Draw
    * Drop MySQL C++ Connector in Base
    * Improve EMF+ graphics import
    * Improve MS PowerPoint PPT/PPTX file import/export
    * New icons and themes

--- misc/libreoffice/patches/patch-external_mariadb-connector-c_UnpackedTarball__mariadb-connector-c.mk
+++ misc/libreoffice/patches/patch-external_mariadb-connector-c_UnpackedTarball__mariadb-connector-c.mk
@@ -1,11 +1,11 @@
-$NetBSD: patch-external_mariadb-connector-c_UnpackedTarball__mariadb-connector-c.mk,v 1.1 2018/02/03 00:39:50 ryoon Exp $
+$NetBSD: patch-external_mariadb-connector-c_UnpackedTarball__mariadb-connector-c.mk,v 1.2 2019/02/19 16:28:03 ryoon Exp $
 
---- external/mariadb-connector-c/UnpackedTarball_mariadb-connector-c.mk.orig	2018-01-24 20:31:03.000000000 +0000
+--- external/mariadb-connector-c/UnpackedTarball_mariadb-connector-c.mk.orig	2019-01-23 19:35:25.000000000 +0000
 +++ external/mariadb-connector-c/UnpackedTarball_mariadb-connector-c.mk
-@@ -31,6 +31,7 @@ $(eval $(call gb_UnpackedTarball_add_pat
-     $(if $(filter MSC,$(COM)),external/mariadb-connector-c/mariadb-msvc.patch.1) \
+@@ -32,6 +32,7 @@ $(eval $(call gb_UnpackedTarball_add_pat
      external/mariadb-connector-c/mariadb-inline.patch.1 \
      external/mariadb-connector-c/mariadb-CONC-104.patch.1 \
+     external/mariadb-connector-c/clang-cl.patch.0 \
 +    external/mariadb-connector-c/netbsd.patch \
  ))
  
