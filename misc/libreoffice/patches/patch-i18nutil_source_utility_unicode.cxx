83e8b0487eceb125f72cf47349fae33e34b164d4
ryoon <ryoon@pkgsrc.org>
2019-02-19 16:28:03 +0000

    Update to 6.2.0.3
    Changelog:
    Many bugfixes and improvements including:
    * New menu GUI
    * Fix copy and paste of table in Writer
    * REGEX function in Calc
    * Animation improvements in Impress and Draw
    * Drop MySQL C++ Connector in Base
    * Improve EMF+ graphics import
    * Improve MS PowerPoint PPT/PPTX file import/export
    * New icons and themes

--- misc/libreoffice/patches/patch-i18nutil_source_utility_unicode.cxx
+++ misc/libreoffice/patches/patch-i18nutil_source_utility_unicode.cxx
@@ -1,9 +1,9 @@
-$NetBSD: patch-i18nutil_source_utility_unicode.cxx,v 1.3 2018/04/19 18:07:03 mrg Exp $
+$NetBSD: patch-i18nutil_source_utility_unicode.cxx,v 1.4 2019/02/19 16:28:03 ryoon Exp $
 
 add missing ICU namespace needed for icu 61.
 
---- i18nutil/source/utility/unicode.cxx.orig	2018-03-29 08:04:09.000000000 -0700
-+++ i18nutil/source/utility/unicode.cxx	2018-04-17 18:01:19.268575308 -0700
+--- i18nutil/source/utility/unicode.cxx.orig	2019-01-23 19:35:25.000000000 +0000
++++ i18nutil/source/utility/unicode.cxx
 @@ -25,6 +25,7 @@
  #include <i18nutil/unicode.hxx>
  #include <sal/log.hxx>
@@ -19,4 +19,4 @@ add missing ICU namespace needed for icu 61.
 +using namespace U_ICU_NAMESPACE;
  
  template<class L, typename T>
- T getScriptType( const sal_Unicode ch, const L* typeList, T unknownType ) {
+ static T getScriptType( const sal_Unicode ch, const L* typeList, T unknownType ) {
