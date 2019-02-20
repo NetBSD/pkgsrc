c31177ba3b62b33faca48bb9e99a6b779c539dfa
martin <martin@pkgsrc.org>
2019-02-20 18:04:28 +0000

    Bump minimal version here as well

--- sysutils/arm-trusted-firmware-sun50iw1p1/buildlink3.mk
+++ sysutils/arm-trusted-firmware-sun50iw1p1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/05/26 17:29:44 jmcneill Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/02/20 18:04:28 martin Exp $
 
 BUILDLINK_DEPMETHOD.arm-trusted-firmware-sun50iw1p1?=	build
 
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	arm-trusted-firmware-sun50iw1p1
 .if !defined(ARM_TRUSTED_FIRMWARE_SUN50IW1P1_BUILDLINK3_MK)
 ARM_TRUSTED_FIRMWARE_SUN50IW1P1_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.arm-trusted-firmware-sun50iw1p1+=	arm-trusted-firmware-sun50iw1p1>=20180210
+BUILDLINK_API_DEPENDS.arm-trusted-firmware-sun50iw1p1+=	arm-trusted-firmware-sun50iw1p1>=20180210nb1
 BUILDLINK_PKGSRCDIR.arm-trusted-firmware-sun50iw1p1?=	../../sysutils/arm-trusted-firmware-sun50iw1p1
 .endif	# ARM_TRUSTED_FIRMWARE_SUN50IW1P1_BUILDLINK3_MK
 
