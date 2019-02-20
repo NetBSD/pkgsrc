8732010910abdcafc608d012c4df8026d9b4700b
ryoon <ryoon@pkgsrc.org>
2019-02-20 12:21:34 +0000

    Fix build when security/cyrus-sasl is installed
    Introduce sasl option (disabled by default) to inconsistent
    detection of cyrus-sasl between CMAKE and CC.
    Reported by NISHIMURA Takeshi.

--- net/libvncserver/options.mk
+++ net/libvncserver/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.2 2019/02/01 12:45:21 tnn Exp $
+# $NetBSD: options.mk,v 1.3 2019/02/20 12:21:34 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libVNCServer
-PKG_SUPPORTED_OPTIONS=	gnutls inet6 libgcrypt openssl
+PKG_SUPPORTED_OPTIONS=	gnutls inet6 libgcrypt openssl sasl
 PKG_SUGGESTED_OPTIONS=	gnutls inet6 libgcrypt
 PKG_OPTIONS_OPTIONAL_GROUPS+=	ssl
 PKG_OPTIONS_GROUP.ssl=		gnutls openssl
@@ -33,4 +33,11 @@ CMAKE_ARGS+=		-DWITH_GCRYPT=OFF
 CMAKE_ARGS+=	-DWITH_OPENSSL=ON
 .else
 CMAKE_ARGS+=	-DWITH_OPENSSL=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Msasl)
+.include "../../security/cyrus-sasl/buildlink3.mk"
+CMAKE_ARGS+=	-DWITH_SASL=ON
+.else
+CMAKE_ARGS+=	-DWITH_SASL=OFF
 .endif
