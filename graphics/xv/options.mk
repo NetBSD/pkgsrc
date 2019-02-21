5cde92f82cf52e78238235ce50b91bbc3ea81bea
tsutsui <tsutsui@pkgsrc.org>
2019-02-21 06:00:22 +0000

    xv: add a missed options.mk file in the previous commit.
    > xv: make ghostscript support optional and disable it by default.

---
+++ graphics/xv/options.mk
@@ -0,0 +1,15 @@
+# $NetBSD: options.mk,v 1.1 2019/02/21 06:00:22 tsutsui Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.xv
+PKG_SUPPORTED_OPTIONS=	ghostscript
+PKG_SUGGESTED_OPTIONS=	
+
+.include "../../mk/bsd.options.mk"
+
+# Postscript support using installed ghostscript commands
+.if !empty(PKG_OPTIONS:Mghostscript)
+USE_TOOLS+=	gs:run
+IMAKEOPTS+=	-DUseInstalledGhostScript
+.else
+IMAKEOPTS+=	-UUseInstalledGhostScript
+.endif
