9ce542f5f244a6f495a92152d76b2dbc6c2c51dc
gutteridge <gutteridge@pkgsrc.org>
2019-02-22 06:21:26 +0000

    orcus: bump dependency version in buildlink3.mk
    libreoffice, the sole package that presently depends on orcus, requires a
    minimum version of 0.14 now. (And, for that matter, the orcus change log
    indicates that there have been incompatible API changes.) Bump
    accordingly here. (If an older version of orcus was present, the
    current version of libreoffice would simply have failed to build.)

--- converters/orcus/buildlink3.mk
+++ converters/orcus/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2018/02/03 00:04:10 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2019/02/22 06:21:26 gutteridge Exp $
 
 BUILDLINK_TREE+=	orcus
 
 .if !defined(ORCUS_BUILDLINK3_MK)
 ORCUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.orcus+=	orcus>=0.13.2
+BUILDLINK_API_DEPENDS.orcus+=	orcus>=0.14.0
 BUILDLINK_PKGSRCDIR.orcus?=	../../converters/orcus
 
 .endif	# ORCUS_BUILDLINK3_MK
