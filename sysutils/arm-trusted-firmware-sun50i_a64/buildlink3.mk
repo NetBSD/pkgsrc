6efc888cfda59b5ade220161f326563bc90be345
martin <martin@pkgsrc.org>
2019-02-23 06:20:12 +0000

    ARM trusted firmware, mainline version, build for the sun50i_a64 platform (e.g. Pinebook).

---
+++ sysutils/arm-trusted-firmware-sun50i_a64/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/02/23 06:20:12 martin Exp $
+
+BUILDLINK_DEPMETHOD.arm-trusted-firmware-sun50i_a64?=	build
+
+BUILDLINK_TREE+=	arm-trusted-firmware-sun50i_a64
+
+.if !defined(ARM_TRUSTED_FIRMWARE_SUN50I_A64_BUILDLINK3_MK)
+ARM_TRUSTED_FIRMWARE_SUN50I_A64_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.arm-trusted-firmware-sun50i_a64+=	arm-trusted-firmware-sun50i_a64>=2.0
+BUILDLINK_PKGSRCDIR.arm-trusted-firmware-sun50i_a64?=	../../sysutils/arm-trusted-firmware-sun50i_a64
+.endif	# ARM_TRUSTED_FIRMWARE_SUN50I_A64_BUILDLINK3_MK
+
