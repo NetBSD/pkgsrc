5fdee0b2cb5d046865787688f155d5c8aaf904d7
martin <martin@pkgsrc.org>
2019-02-23 06:39:27 +0000

    Ballance buildlink tree

--- sysutils/arm-trusted-firmware-sun50i_a64/buildlink3.mk
+++ sysutils/arm-trusted-firmware-sun50i_a64/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/02/23 06:20:12 martin Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/02/23 06:39:27 martin Exp $
 
 BUILDLINK_DEPMETHOD.arm-trusted-firmware-sun50i_a64?=	build
 
@@ -10,4 +10,6 @@ ARM_TRUSTED_FIRMWARE_SUN50I_A64_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.arm-trusted-firmware-sun50i_a64+=	arm-trusted-firmware-sun50i_a64>=2.0
 BUILDLINK_PKGSRCDIR.arm-trusted-firmware-sun50i_a64?=	../../sysutils/arm-trusted-firmware-sun50i_a64
 .endif	# ARM_TRUSTED_FIRMWARE_SUN50I_A64_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-arm-trusted-firmware-sun50i_a64
 
