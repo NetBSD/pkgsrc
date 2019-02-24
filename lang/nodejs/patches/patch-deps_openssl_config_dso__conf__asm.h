c1394f3c016685bb64502c03ba749541e75e9c4c
rin <rin@pkgsrc.org>
2019-02-24 12:18:55 +0000

    - Fix OpenSSL support for NetBSD/amd64 (https works fine now) - Fix NetBSD/i386 support (hopefully also works for other ILP32 archs) - Add NetBSD/aarch64 support - Bump revision

---
+++ lang/nodejs/patches/patch-deps_openssl_config_dso__conf__asm.h
@@ -0,0 +1,32 @@
+$NetBSD: patch-deps_openssl_config_dso__conf__asm.h,v 1.1 2019/02/24 12:18:55 rin Exp $
+
+Support NetBSD/arm,aarch64,i386,amd64 (and hopefully other ILP32 archs)
+
+--- deps/openssl/config/dso_conf_asm.h.orig	2019-02-24 10:37:55.875191472 +0900
++++ deps/openssl/config/dso_conf_asm.h	2019-02-24 10:24:24.895671320 +0900
+@@ -9,9 +9,9 @@
+ # include "./archs/linux-x32/asm/crypto/include/internal/dso_conf.h"
+ #elif defined(OPENSSL_LINUX) && defined(__x86_64__)
+ # include "./archs/linux-x86_64/asm/crypto/include/internal/dso_conf.h"
+-#elif defined(OPENSSL_LINUX) && defined(__arm__)
++#elif (defined(OPENSSL_LINUX) || defined(__NetBSD__)) && defined(__arm__)
+ # include "./archs/linux-armv4/asm/crypto/include/internal/dso_conf.h"
+-#elif defined(OPENSSL_LINUX) && defined(__aarch64__)
++#elif (defined(OPENSSL_LINUX) || defined(__NetBSD__)) && defined(__aarch64__)
+ # include "./archs/linux-aarch64/asm/crypto/include/internal/dso_conf.h"
+ #elif defined(__APPLE__) && defined(__MACH__) && defined(__i386__)
+ # include "./archs/darwin-i386-cc/asm/crypto/include/internal/dso_conf.h"
+@@ -21,9 +21,10 @@
+ # include "./archs/VC-WIN32/asm/crypto/include/internal/dso_conf.h"
+ #elif defined(_WIN32) && defined(_M_X64)
+ # include "./archs/VC-WIN64A/asm/crypto/include/internal/dso_conf.h"
+-#elif (defined(__FreeBSD__) || defined(__OpenBSD__)) && defined(__i386__)
+-# include "./archs/BSD-x86/asm/crypto/include/internal/dso_conf.h"
+-#elif (defined(__FreeBSD__) || defined(__OpenBSD__)) && defined(__x86_64__)
++// XXX missing
++//#elif (defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)) && defined(__i386__)
++//# include "./archs/BSD-x86/asm/crypto/include/internal/dso_conf.h"
++#elif (defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)) && defined(__x86_64__)
+ # include "./archs/BSD-x86_64/asm/crypto/include/internal/dso_conf.h"
+ #elif defined(__sun) && defined(__i386__)
+ # include "./archs/solaris-x86-gcc/asm/crypto/include/internal/dso_conf.h"
--- lang/nodejs/patches/patch-deps_openssl_openssl__no__asm.gypi
+++ lang/nodejs/patches/patch-deps_openssl_openssl__no__asm.gypi
@@ -1,17 +1,30 @@
-$NetBSD: patch-deps_openssl_openssl__no__asm.gypi,v 1.2 2019/01/10 13:02:41 ryoon Exp $
+$NetBSD: patch-deps_openssl_openssl__no__asm.gypi,v 1.3 2019/02/24 12:18:55 rin Exp $
 
-Add support for NetBSD.
+Support NetBSD/arm,aarch64,i386,amd64 (and hopefully other ILP32 archs)
 
---- deps/openssl/openssl_no_asm.gypi.orig	2018-12-10 21:19:04.000000000 +0000
-+++ deps/openssl/openssl_no_asm.gypi
-@@ -32,6 +32,10 @@
+XXX Change default to linux-elf instead of linux-x86_64 (from FreeBSD ports)
+
+--- deps/openssl/openssl_no_asm.gypi.orig	2019-01-29 16:20:46.000000000 +0900
++++ deps/openssl/openssl_no_asm.gypi	2019-02-24 10:29:47.246812244 +0900
+@@ -32,6 +32,12 @@
        'includes': ['config/archs/linux-elf/no-asm/openssl.gypi'],
      }, 'target_arch=="x64" and OS=="freebsd"', {
        'includes': ['config/archs/BSD-x86_64/no-asm/openssl.gypi'],
 +    }, 'target_arch=="arm" and OS=="netbsd"', {
 +      'includes': ['config/archs/linux-armv4/no-asm/openssl.gypi'],
++    }, 'target_arch=="arm64" and OS=="netbsd"', {
++      'includes': ['config/archs/linux-aarch64/no-asm/openssl.gypi'],
 +    }, 'target_arch=="x64" and OS=="netbsd"', {
 +      'includes': ['config/archs/BSD-x86_64/no-asm/openssl.gypi'],
      }, 'target_arch=="x64" and OS=="mac"', {
        'includes': ['config/archs/darwin64-x86_64-cc/no-asm/openssl.gypi'],
      }, 'target_arch=="x64" and OS=="solaris"', {
+@@ -42,7 +48,7 @@
+       'includes': ['config/archs/linux-x86_64/no-asm/openssl.gypi'],
+     }, {
+       # Other architectures don't use assembly
+-      'includes': ['config/archs/linux-x86_64/no-asm/openssl.gypi'],
++      'includes': ['config/archs/linux-elf/no-asm/openssl.gypi'],
+     }],
+   ],
+ }
