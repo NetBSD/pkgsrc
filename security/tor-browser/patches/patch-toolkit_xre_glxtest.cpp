02b745f7659f007ce69e86bdf9c094760e8c9ee5
wiz <wiz@pkgsrc.org>
2019-02-25 15:32:23 +0000

    tor-browser: update to 8.5.
    This is based on a git checkout from a couple days ago; not completely
    sure about the version number.
    
    The Makefile now contains a short how-to for updating this package.
    
    Many thanks for the www/firefox60 patches!
    
    Use at your own risk!
    Survives basic browsing and check.torproject.org claims it connects via tor.
    
    Changes: too many to document.

--- security/tor-browser/patches/patch-toolkit_xre_glxtest.cpp
+++ security/tor-browser/patches/patch-toolkit_xre_glxtest.cpp
@@ -1,16 +1,16 @@
-$NetBSD: patch-toolkit_xre_glxtest.cpp,v 1.1 2017/01/22 12:27:22 ryoon Exp $
+$NetBSD: patch-toolkit_xre_glxtest.cpp,v 1.2 2019/02/25 15:32:24 wiz Exp $
 
 Fix libGL filename on NetBSD,
 see https://bugzilla.mozilla.org/show_bug.cgi?id=1180498
 
---- toolkit/xre/glxtest.cpp.orig	2015-07-04 20:26:15.000000000 +0200
-+++ toolkit/xre/glxtest.cpp	2015-07-04 20:26:15.000000000 +0200
-@@ -124,7 +124,7 @@ void glxtest()
-     fatal_error("The MOZ_AVOID_OPENGL_ALTOGETHER environment variable is defined");
+--- toolkit/xre/glxtest.cpp.orig	2019-02-23 20:00:59.000000000 +0000
++++ toolkit/xre/glxtest.cpp
+@@ -116,7 +116,7 @@ void glxtest() {
+         "The MOZ_AVOID_OPENGL_ALTOGETHER environment variable is defined");
  
-   ///// Open libGL and load needed symbols /////
+     ///// Open libGL and load needed symbols /////
 -#ifdef __OpenBSD__
 +#if defined(__OpenBSD__) || defined(__NetBSD__)
-   #define LIBGL_FILENAME "libGL.so"
+ #define LIBGL_FILENAME "libGL.so"
  #else
-   #define LIBGL_FILENAME "libGL.so.1"
+ #define LIBGL_FILENAME "libGL.so.1"
