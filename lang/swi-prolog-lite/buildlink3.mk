15e0e81fbce7e9cac992b2de6c27c63c4ca5160c
leot <leot@pkgsrc.org>
2019-02-25 15:20:44 +0000

    swi-prolog*: Update to 8.0.1
    pkgsrc changes:
     - Rename swi-prolog to swipl to follow upstream nomenclature
     - Add all main packages (except X11) to swi-prolog-lite.
       After CMake migration in order to generate the documentation and being able
       to use it (e.g. via help/2) it is needed to add basic, archive, ssl and term
       packages.
       All X11 packages are provided by swi-prolog-packages.
     - Adjust pkgsrc Makefile-s logic to upstream CMake migration:
        o Uncoditionally disable not wanted packages in Makefile.common (to avoid
          possible PLIST mismatches; please note that this will probably disable
          tipc package on Linux!).
          All other installed packages are enabled/disabled via
          swi-prolog-{lite,packages,jpl} Makefile.
        o Add libarchive, ossp-uuid and openssl build dependency to
          swi-prolog-packages.  Despite these are provided by swi-prolog-lite
          they are needed as part of the build of swi-prolog-packages too.
        o Remove no more needed logic to check and eventually start X server to
          build documentation.  Should address PR pkg/42047.
     - Remove an unconditional CHECK_WRKREF_SKIP, this was needed on FreeBSD
       but unfortunately it is not clear why.  If this is still needed please let
       me know in order to try to investigate further and address that.
     - Bump API requirements to 8.0.1 in buildlink3.mk to be on the safe side
       now that shared libraries are provided on all platforms.
    
    Changes:
    8.0.1
    -----
    Indexing on multiple arguments together, indexing inside compounds,
    Mode-directed tabling, saved states using ZIP files, many deployment
    enhancements. Moved build environment to CMake and removed most of
    the build tool dependencies. Builds documentation along with the
    binary.
    
    Please note that this is just a short summary. Unfortunately the
    complete changelog is very long, full changelog can be found at:
    
     <http://www.swi-prolog.org/ChangeLog?branch=stable&from=7.6.4&to=8.0.1>

--- lang/swi-prolog-lite/buildlink3.mk
+++ lang/swi-prolog-lite/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.13 2018/12/10 14:59:50 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2019/02/25 15:20:44 leot Exp $
 
 BUILDLINK_TREE+=	swi-prolog-lite
 
 .if !defined(SWI_PROLOG_LITE_BUILDLINK3_MK)
 SWI_PROLOG_LITE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.swi-prolog-lite+=	swi-prolog-lite>=5.2.9
-BUILDLINK_ABI_DEPENDS.swi-prolog-lite+=	swi-prolog-lite>=6.6.6nb2
+BUILDLINK_API_DEPENDS.swi-prolog-lite+=	swi-prolog-lite>=8.0.1
 BUILDLINK_PKGSRCDIR.swi-prolog-lite?=	../../lang/swi-prolog-lite
 
 .endif	# SWI_PROLOG_LITE_BUILDLINK3_MK
