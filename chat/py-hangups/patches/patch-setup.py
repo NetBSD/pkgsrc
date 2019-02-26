035f91336018fc73ceb73700c3fe38fd1667f630
leot <leot@pkgsrc.org>
2019-02-26 18:31:46 +0000

    py-hangups: Update to 0.4.9
    Changes:
    0.4.9
    -----
     - fix loading events by event id with Conversation.get_events
    
    0.4.8
    -----
     - set device name to "hangups" when authenticating
     - fix snap package build
    
    0.4.7
    -----
     - add manual login option
     - publish tests in package
     - add participant watermarks to conversation objects and UI
     - improve hyperlink detection
     - fix issue with loading conversion history
     - bump aiohttp requirement to >=3.3

--- chat/py-hangups/patches/patch-setup.py
+++ chat/py-hangups/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.3 2018/10/15 08:55:49 leot Exp $
+$NetBSD: patch-setup.py,v 1.4 2019/02/26 18:31:47 leot Exp $
 
 Avoid too strict version requirements.
 
---- setup.py.orig	2018-10-14 20:15:25.000000000 +0000
+--- setup.py.orig	2019-01-30 04:55:18.000000000 +0000
 +++ setup.py
 @@ -25,16 +25,16 @@ with open('README.rst') as f:
  # hangups from breaking when new versions of dependencies are released,
  # especially for end-users (non-developers) who use pip to install hangups.
  install_requires = [
 -    'ConfigArgParse==0.11.0',
--    'aiohttp>=1.3,<4',
+-    'aiohttp>=3.3,<4',
 -    'async-timeout>=2,<4',
 -    'appdirs>=1.4,<1.5',
 -    'readlike==0.1.2',
@@ -19,7 +19,7 @@ Avoid too strict version requirements.
 -    'urwid==1.3.1',
 -    'MechanicalSoup==0.6.0',
 +    'ConfigArgParse>=0.11.0',
-+    'aiohttp>=1.3',
++    'aiohttp>=3.3',
 +    'async-timeout>=2',
 +    'appdirs>=1.4',
 +    'readlike>=0.1.2',
