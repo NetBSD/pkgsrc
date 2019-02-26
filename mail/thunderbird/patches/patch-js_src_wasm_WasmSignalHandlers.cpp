407c1351f0d3ca5ba3ddc692d9bd017024472d0c
ryoon <ryoon@pkgsrc.org>
2019-02-26 11:32:12 +0000

    Update to 60.5.1
    Changelog:
    60.5.1
    Fixed
    CalDav access to some servers not working
    
    #CVE-2018-18500: Use-after-free parsing HTML5 stream
    #CVE-2018-18505: Privilege escalation through IPC channel messages
    #CVE-2016-5824: DoS (use-after-free) via a crafted ics file
    #CVE-2018-18501: Memory safety bugs fixed in Firefox 65, Firefox ESR 60.5, and Thunderbird 60.5
    
    60.5.0
    New
    FileLink provider WeTransfer to upload large attachments
    
    Thunderbird now allows the addition of OpenSearch search engines from a local XML file using a minimal user inferface: [+] button to select a file an add, [-] to remove.
    
    More search engines: Google and DuckDuckGo available by default in some locales
    
    During account creation, Thunderbird will now detect servers using the Microsoft Exchange protocol. It will offer the installation of a 3rd party add-on (Owl) which supports that protocol.
    
    Fixed
    Thunderbird now compatible with other WebExtension-based FileLink add-ons like the Dropbox add-on
    
    Crash when using custom sound for new email notification
    
    WebExtension-based dictionaries from addons.mozilla.org not working in Thunderbird
    
    Calendar: Printing of calendars not working
    
    #CVE-2018-18356: Use-after-free in Skia
    #CVE-2019-5785: Integer overflow in Skia
    #CVE-2018-18335: Buffer overflow in Skia with accelerated Canvas 2D
    #CVE-2018-18509: S/MIME signature spoofing

--- mail/thunderbird/patches/patch-js_src_wasm_WasmSignalHandlers.cpp
+++ mail/thunderbird/patches/patch-js_src_wasm_WasmSignalHandlers.cpp
@@ -1,12 +1,12 @@
-$NetBSD: patch-js_src_wasm_WasmSignalHandlers.cpp,v 1.1 2018/12/16 08:12:16 ryoon Exp $
+$NetBSD: patch-js_src_wasm_WasmSignalHandlers.cpp,v 1.2 2019/02/26 11:32:13 ryoon Exp $
 
---- js/src/wasm/WasmSignalHandlers.cpp.orig	2018-12-04 23:11:52.000000000 +0000
+--- js/src/wasm/WasmSignalHandlers.cpp.orig	2019-02-13 14:19:40.000000000 +0000
 +++ js/src/wasm/WasmSignalHandlers.cpp
-@@ -135,6 +135,7 @@ struct AutoSignalHandler
- #  define EBP_sig(p) ((p)->uc_mcontext.gregs[REG_EBP])
- #  define ESP_sig(p) ((p)->uc_mcontext.gregs[REG_ESP])
- # else
-+#  include <sys/regset.h>
- #  define XMM_sig(p,i) ((p)->uc_mcontext.fpregs.fp_reg_set.fpchip_state.xmm[i])
- #  define EIP_sig(p) ((p)->uc_mcontext.gregs[REG_PC])
- #  define EBP_sig(p) ((p)->uc_mcontext.gregs[REG_EBP])
+@@ -133,6 +133,7 @@ struct AutoSignalHandler {
+ #define EBP_sig(p) ((p)->uc_mcontext.gregs[REG_EBP])
+ #define ESP_sig(p) ((p)->uc_mcontext.gregs[REG_ESP])
+ #else
++#include <sys/regset.h>
+ #define XMM_sig(p, i) ((p)->uc_mcontext.fpregs.fp_reg_set.fpchip_state.xmm[i])
+ #define EIP_sig(p) ((p)->uc_mcontext.gregs[REG_PC])
+ #define EBP_sig(p) ((p)->uc_mcontext.gregs[REG_EBP])
--- mail/thunderbird/patches/patch-toolkit_xre_glxtest.cpp
+++ mail/thunderbird/patches/patch-toolkit_xre_glxtest.cpp
@@ -1,13 +1,13 @@
-$NetBSD: patch-toolkit_xre_glxtest.cpp,v 1.1 2018/12/16 08:12:16 ryoon Exp $
+$NetBSD: patch-toolkit_xre_glxtest.cpp,v 1.2 2019/02/26 11:32:13 ryoon Exp $
 
---- toolkit/xre/glxtest.cpp.orig	2018-12-04 23:12:02.000000000 +0000
+--- toolkit/xre/glxtest.cpp.orig	2019-02-13 14:19:45.000000000 +0000
 +++ toolkit/xre/glxtest.cpp
-@@ -124,7 +124,7 @@ void glxtest()
-     fatal_error("The MOZ_AVOID_OPENGL_ALTOGETHER environment variable is defined");
+@@ -116,7 +116,7 @@ void glxtest() {
+         "The MOZ_AVOID_OPENGL_ALTOGETHER environment variable is defined");
  
-   ///// Open libGL and load needed symbols /////
+     ///// Open libGL and load needed symbols /////
 -#ifdef __OpenBSD__
 +#if defined(__OpenBSD__) || defined(__NetBSD__)
-   #define LIBGL_FILENAME "libGL.so"
+ #define LIBGL_FILENAME "libGL.so"
  #else
-   #define LIBGL_FILENAME "libGL.so.1"
+ #define LIBGL_FILENAME "libGL.so.1"
