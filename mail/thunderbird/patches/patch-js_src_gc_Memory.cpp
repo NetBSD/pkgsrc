e3a35ebfe3cf7d68315b0a5b8a09bcc9ad02232b
ryoon <ryoon@pkgsrc.org>
2019-02-26 11:32:12 +0000

    Update to 60.5.1
    Changelog:
    60.5.1
    Fixed
    CalDav access to some servers not working
    
    #CVE-2018-18500: Use-after-free parsing HTML5 stream
    #CVE-2018-18505: Privilege escalation through IPC channel messages
    #CVE-2016-5824: DoS (use-after-free) via a crafted ics file
    #CVE-2018-18501: Memory safety bugs fixed in Firefox 65, Firefox ESR 60.5, and Thunderbird 60.5
    
    60.5.0
    New
    FileLink provider WeTransfer to upload large attachments
    
    Thunderbird now allows the addition of OpenSearch search engines from a local XML file using a minimal user inferface: [+] button to select a file an add, [-] to remove.
    
    More search engines: Google and DuckDuckGo available by default in some locales
    
    During account creation, Thunderbird will now detect servers using the Microsoft Exchange protocol. It will offer the installation of a 3rd party add-on (Owl) which supports that protocol.
    
    Fixed
    Thunderbird now compatible with other WebExtension-based FileLink add-ons like the Dropbox add-on
    
    Crash when using custom sound for new email notification
    
    WebExtension-based dictionaries from addons.mozilla.org not working in Thunderbird
    
    Calendar: Printing of calendars not working
    
    #CVE-2018-18356: Use-after-free in Skia
    #CVE-2019-5785: Integer overflow in Skia
    #CVE-2018-18335: Buffer overflow in Skia with accelerated Canvas 2D
    #CVE-2018-18509: S/MIME signature spoofing

--- mail/thunderbird/patches/patch-js_src_gc_Memory.cpp
+++ mail/thunderbird/patches/patch-js_src_gc_Memory.cpp
@@ -1,21 +1,22 @@
-$NetBSD: patch-js_src_gc_Memory.cpp,v 1.1 2018/12/16 08:12:16 ryoon Exp $
+$NetBSD: patch-js_src_gc_Memory.cpp,v 1.2 2019/02/26 11:32:13 ryoon Exp $
 
---- js/src/gc/Memory.cpp.orig	2018-12-04 23:11:48.000000000 +0000
+--- js/src/gc/Memory.cpp.orig	2019-02-13 14:19:38.000000000 +0000
 +++ js/src/gc/Memory.cpp
-@@ -504,6 +504,7 @@ MapMemoryAt(void* desired, size_t length
- {
+@@ -419,6 +419,7 @@ static inline void* MapMemoryAt(void* de
+                                 off_t offset = 0) {
  
- #if defined(__ia64__) || defined(__aarch64__) || \
+ #if defined(__ia64__) || defined(__aarch64__) ||  \
 +    (defined(__sun) && defined(__x86_64__)) || \
-     (defined(__sparc__) && defined(__arch64__) && (defined(__NetBSD__) || defined(__linux__)))
-     MOZ_ASSERT((0xffff800000000000ULL & (uintptr_t(desired) + length - 1)) == 0);
- #endif
-@@ -554,7 +555,7 @@ MapMemory(size_t length, int prot = PROT
-         return nullptr;
-     }
-     return region;
--#elif defined(__aarch64__) || (defined(__sparc__) && defined(__arch64__) && defined(__linux__))
-+#elif defined(__aarch64__) || (defined(__sparc__) && defined(__arch64__) && defined(__linux__)) || (defined(__sun) && defined(__x86_64__))
-    /*
-     * There might be similar virtual address issue on arm64 which depends on
-     * hardware and kernel configurations. But the work around is slightly
+     (defined(__sparc__) && defined(__arch64__) && \
+      (defined(__NetBSD__) || defined(__linux__)))
+   MOZ_ASSERT((0xffff800000000000ULL & (uintptr_t(desired) + length - 1)) == 0);
+@@ -468,7 +469,8 @@ static inline void* MapMemory(size_t len
+   }
+   return region;
+ #elif defined(__aarch64__) || \
+-    (defined(__sparc__) && defined(__arch64__) && defined(__linux__))
++    (defined(__sparc__) && defined(__arch64__) && defined(__linux__)) || \
++    (defined(__sun) && defined(__x86_64__))
+   /*
+    * There might be similar virtual address issue on arm64 which depends on
+    * hardware and kernel configurations. But the work around is slightly
