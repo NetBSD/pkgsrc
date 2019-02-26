e822ea8d668d357e5bdf9472d2c8aba11bea1291
ryoon <ryoon@pkgsrc.org>
2019-02-26 11:32:12 +0000

    Update to 60.5.1
    Changelog:
    60.5.1
    Fixed
    CalDav access to some servers not working
    
    #CVE-2018-18500: Use-after-free parsing HTML5 stream
    #CVE-2018-18505: Privilege escalation through IPC channel messages
    #CVE-2016-5824: DoS (use-after-free) via a crafted ics file
    #CVE-2018-18501: Memory safety bugs fixed in Firefox 65, Firefox ESR 60.5, and Thunderbird 60.5
    
    60.5.0
    New
    FileLink provider WeTransfer to upload large attachments
    
    Thunderbird now allows the addition of OpenSearch search engines from a local XML file using a minimal user inferface: [+] button to select a file an add, [-] to remove.
    
    More search engines: Google and DuckDuckGo available by default in some locales
    
    During account creation, Thunderbird will now detect servers using the Microsoft Exchange protocol. It will offer the installation of a 3rd party add-on (Owl) which supports that protocol.
    
    Fixed
    Thunderbird now compatible with other WebExtension-based FileLink add-ons like the Dropbox add-on
    
    Crash when using custom sound for new email notification
    
    WebExtension-based dictionaries from addons.mozilla.org not working in Thunderbird
    
    Calendar: Printing of calendars not working
    
    #CVE-2018-18356: Use-after-free in Skia
    #CVE-2019-5785: Integer overflow in Skia
    #CVE-2018-18335: Buffer overflow in Skia with accelerated Canvas 2D
    #CVE-2018-18509: S/MIME signature spoofing

--- mail/thunderbird/patches/patch-js_src_util_NativeStack.cpp
+++ mail/thunderbird/patches/patch-js_src_util_NativeStack.cpp
@@ -1,9 +1,9 @@
-$NetBSD: patch-js_src_util_NativeStack.cpp,v 1.1 2018/12/16 08:12:16 ryoon Exp $
+$NetBSD: patch-js_src_util_NativeStack.cpp,v 1.2 2019/02/26 11:32:13 ryoon Exp $
 
---- js/src/util/NativeStack.cpp.orig	2018-12-04 23:11:52.000000000 +0000
+--- js/src/util/NativeStack.cpp.orig	2019-02-13 14:19:39.000000000 +0000
 +++ js/src/util/NativeStack.cpp
-@@ -32,7 +32,7 @@ js::GetNativeStackBaseImpl()
-     return static_cast<void*>(pTib->StackBase);
+@@ -30,7 +30,7 @@ void* js::GetNativeStackBaseImpl() {
+   return static_cast<void*>(pTib->StackBase);
  }
  
 -#elif defined(SOLARIS)
@@ -11,11 +11,11 @@ $NetBSD: patch-js_src_util_NativeStack.cpp,v 1.1 2018/12/16 08:12:16 ryoon Exp $
  
  #include <ucontext.h>
  
-@@ -78,6 +78,7 @@ js::GetNativeStackBaseImpl()
- #  elif defined(PTHREAD_NP_H) || defined(_PTHREAD_NP_H_) || defined(NETBSD)
-     /* e.g. on FreeBSD 4.8 or newer, neundorf@kde.org */
-     pthread_attr_get_np(thread, &sattr);
-+#  elif defined(__sun)
- #  else
-     /*
-      * FIXME: this function is non-portable;
+@@ -69,6 +69,7 @@ void* js::GetNativeStackBaseImpl() {
+ #elif defined(PTHREAD_NP_H) || defined(_PTHREAD_NP_H_) || defined(NETBSD)
+   /* e.g. on FreeBSD 4.8 or newer, neundorf@kde.org */
+   pthread_attr_get_np(thread, &sattr);
++#elif defined(__sun)
+ #else
+   /*
+    * FIXME: this function is non-portable;
