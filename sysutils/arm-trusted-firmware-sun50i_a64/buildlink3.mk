2c5f00b6f730391490c74cb057fa843856b76f1a
martin <martin@pkgsrc.org>
2019-02-27 06:23:39 +0000

    Switch this over to a snapshot of the MASTER branch post-2.2 release, to get proper support for Pinebook.

--- sysutils/arm-trusted-firmware-sun50i_a64/buildlink3.mk
+++ sysutils/arm-trusted-firmware-sun50i_a64/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/02/23 06:39:27 martin Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/02/27 06:23:39 martin Exp $
 
 BUILDLINK_DEPMETHOD.arm-trusted-firmware-sun50i_a64?=	build
 
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	arm-trusted-firmware-sun50i_a64
 .if !defined(ARM_TRUSTED_FIRMWARE_SUN50I_A64_BUILDLINK3_MK)
 ARM_TRUSTED_FIRMWARE_SUN50I_A64_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.arm-trusted-firmware-sun50i_a64+=	arm-trusted-firmware-sun50i_a64>=2.0
+BUILDLINK_API_DEPENDS.arm-trusted-firmware-sun50i_a64+=	arm-trusted-firmware-sun50i_a64>2.0
 BUILDLINK_PKGSRCDIR.arm-trusted-firmware-sun50i_a64?=	../../sysutils/arm-trusted-firmware-sun50i_a64
 .endif	# ARM_TRUSTED_FIRMWARE_SUN50I_A64_BUILDLINK3_MK
 
