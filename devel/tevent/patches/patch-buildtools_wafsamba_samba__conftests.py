82e173eb3373560b6a0d51d62dd9f0aee5d05c2e
adam <adam@pkgsrc.org>
2019-03-02 14:06:03 +0000

    tevent: updated to 0.9.39
    0.9.39:
    Unknown changes

--- devel/tevent/patches/patch-buildtools_wafsamba_samba__conftests.py
+++ devel/tevent/patches/patch-buildtools_wafsamba_samba__conftests.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-buildtools_wafsamba_samba__conftests.py,v 1.1 2019/02/23 21:28:33 jperkin Exp $
+$NetBSD: patch-buildtools_wafsamba_samba__conftests.py,v 1.2 2019/03/02 14:06:03 adam Exp $
 
 Ensure defines are strings to avoid assertion failure, some
 returned values are unicode.
 
---- buildtools/wafsamba/samba_conftests.py.orig	2019-01-14 22:24:44.000000000 +0000
+--- buildtools/wafsamba/samba_conftests.py.orig	2019-02-26 06:44:21.000000000 +0000
 +++ buildtools/wafsamba/samba_conftests.py
 @@ -97,9 +97,9 @@ def CHECK_LARGEFILE(conf, define='HAVE_L
                  if flag[:2] == "-D":
@@ -15,5 +15,5 @@ returned values are unicode.
 -                        conf.DEFINE(flag_split[0], flag_split[1])
 +                        conf.DEFINE(str(flag_split[0]), str(flag_split[1]))
  
-     if conf.CHECK_CODE('return !(sizeof(off_t) >= 8)',
+     if conf.CHECK_CODE('if (sizeof(off_t) < 8) return 1',
                         define,
