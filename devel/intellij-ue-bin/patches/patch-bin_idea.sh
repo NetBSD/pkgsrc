e279c702d30ec17145bfd75cdb09bbb374c7a980
youri <youri@pkgsrc.org>
2019-03-03 17:11:12 +0000

    devel/intellij-ue-bin: import intellij-ue-bin-2018.3.5
    This is a binary package for Intellij Ultimate Edition.
    
    IntelliJ IDEA Ultimate Edition is the open source version of IntelliJ
    IDEA, a premier IDE (Integrated Development Environment) for Java,
    Groovy and other programming languages such as Scala or Clojure.
    
    The Ultimate Edition includes:
    
        An Intelligent code editor that has all the smarts for
        understanding Java, XML and Groovy code
    
        Refactorings, code inspections and intentions, super-fast
        navigation and search
    
        Testing frameworks integration: JUnit and TestNG
    
        Build tools support: Ant and Maven
    
        Popular version control systems integration: CVS, Subversion
        and Git
    
        Swing UI designer

---
+++ devel/intellij-ue-bin/patches/patch-bin_idea.sh
@@ -0,0 +1,24 @@
+$NetBSD: patch-bin_idea.sh,v 1.1 2019/03/03 17:11:13 youri Exp $
+
+Add a few JVM options to make it work in a common case.
+
+--- bin/idea.sh.orig	2019-02-26 12:34:54.000000000 +0000
++++ bin/idea.sh
+@@ -21,6 +21,7 @@ message()
+   fi
+ }
+ 
++IDEA_JDK=@PKG_JAVA_HOME@
+ UNAME=`which uname`
+ GREP=`which egrep`
+ GREP_OPTIONS=""
+@@ -202,6 +203,9 @@ fi
+ # ---------------------------------------------------------------------
+ IFS="$(printf '\n\t')"
+ "$JAVA_BIN" \
++  -Xmx4096 -Xms4096 \
++  -XX:-UseCompressedClassPointers \
++  -XX:-UseCompressedOops \
+   -classpath "$CLASSPATH" \
+   ${VM_OPTIONS} \
+   "-XX:ErrorFile=$HOME/java_error_in_IDEA_%p.log" \
