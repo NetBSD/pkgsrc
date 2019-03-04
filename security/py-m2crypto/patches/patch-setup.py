6413394f0d3b688390c0aad141c01a678d98951e
adam <adam@pkgsrc.org>
2019-03-04 18:41:48 +0000

    py-m2crypto: updated to 0.32.0
    0.32.0:
    - setup.py: use ${CPP} as path to cpp
    - Bump pipeline OpenSSL from 1.1.0i to 1.1.0j
    - Stub wchar_t helpers and ignore unused WCHAR defs
    - Add type comment to setup.py

--- security/py-m2crypto/patches/patch-setup.py
+++ security/py-m2crypto/patches/patch-setup.py
@@ -1,12 +1,23 @@
-$NetBSD: patch-setup.py,v 1.4 2018/02/09 08:44:50 adam Exp $
+$NetBSD: patch-setup.py,v 1.5 2019/03/04 18:41:48 adam Exp $
+
+CPP may contain spaces.
 
 Don't pass "-includeall" to swig.  It causes compiler-dependent
 tests in system include files to break (e.g. "#ifdef __GNUC__"
 doesn't work properly.)
 
---- setup.py.orig	2018-02-08 18:54:48.000000000 +0000
+--- setup.py.orig	2019-03-04 17:27:03.000000000 +0000
 +++ setup.py
-@@ -192,7 +192,6 @@ class _M2CryptoBuildExt(build_ext.build_
+@@ -50,7 +50,7 @@ def _get_additional_includes():
+                                 '*Visual*', 'VC', 'include')
+         err = glob.glob(globmask)
+     else:
+-        pid = subprocess.Popen([os.environ.get('CPP', 'cpp'), '-Wp,-v', '-'],
++        pid = subprocess.Popen(os.environ.get('CPP', 'cpp').split() + ['-Wp,-v', '-'],
+                                stdin=open(os.devnull, 'r'),
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+@@ -197,7 +197,6 @@ class _M2CryptoBuildExt(build_ext.build_
  
          # swig seems to need the default header file directories
          self.swig_opts.extend(['-I%s' % i for i in _get_additional_includes()])
