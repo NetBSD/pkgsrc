fa5db9c3d0a7d7b1337b2b7d8b6beac382a1cc8f
adam <adam@pkgsrc.org>
2019-03-05 12:43:40 +0000

    python27: updated to 2.7.16
    Python 2.7.16 is a bugfix release in the Python 2.7 series.

--- lang/python27/dist.mk
+++ lang/python27/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.15 2018/05/19 06:54:55 spz Exp $
+# $NetBSD: dist.mk,v 1.16 2019/03/05 12:43:40 adam Exp $
 
-PY_DISTVERSION=	2.7.15
+PY_DISTVERSION=	2.7.16
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python27/distinfo
