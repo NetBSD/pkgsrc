31aa60a4587c8475a6f4d8c473e4b009c63891fb
jaapb <jaapb@pkgsrc.org>
2019-03-05 16:13:26 +0000

    Updated devel/ocaml-sexplib0 to version 0.12.0
    Package now compatible with dune 1.7.
    
    There doesn't seem to be a changes file upstream, but changes are minor
    only.

--- devel/ocaml-sexplib0/buildlink3.mk
+++ devel/ocaml-sexplib0/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/11/27 14:45:05 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2019/03/05 16:13:26 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-sexplib0
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-sexplib0
 OCAML_SEXPLIB0_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-sexplib0+=	ocaml-sexplib0>=0.11.0
-BUILDLINK_ABI_DEPENDS.ocaml-sexplib0+=	ocaml-sexplib0>=0.11.0nb3
+BUILDLINK_ABI_DEPENDS.ocaml-sexplib0+=	ocaml-sexplib0>=0.12.0
 BUILDLINK_PKGSRCDIR.ocaml-sexplib0?=	../../devel/ocaml-sexplib0
 
 .endif	# OCAML_SEXPLIB0_BUILDLINK3_MK
