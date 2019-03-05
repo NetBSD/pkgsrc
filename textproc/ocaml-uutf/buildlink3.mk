400699a99ef5aaedae7620ca470af94a32af4dc8
jaapb <jaapb@pkgsrc.org>
2019-03-05 17:06:52 +0000

    Updated package textproc/ocaml-uutf to version 1.0.2.
    Minor bugfix (substring folding).

--- textproc/ocaml-uutf/buildlink3.mk
+++ textproc/ocaml-uutf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/07/19 15:15:30 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2019/03/05 17:06:52 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-uutf
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-uutf
 OCAML_UUTF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-uutf+=	ocaml-uutf>=0.9.3nb1
-BUILDLINK_ABI_DEPENDS.ocaml-uutf+=	ocaml-uutf>=1.0.1nb4
+BUILDLINK_ABI_DEPENDS.ocaml-uutf+=	ocaml-uutf>=1.0.2
 BUILDLINK_PKGSRCDIR.ocaml-uutf?=	../../textproc/ocaml-uutf
 .endif	# OCAML_UUTF_BUILDLINK3_MK
 
