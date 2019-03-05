60b6862461effd28bd39c85943b3de6a53e1ce8b
jaapb <jaapb@pkgsrc.org>
2019-03-05 17:38:30 +0000

    Updated devel/ocaml-compiler-libs for dune compatibility.
    Package now compatible with dune 1.7; revbump.

--- devel/ocaml-compiler-libs/buildlink3.mk
+++ devel/ocaml-compiler-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/11/27 18:54:05 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2019/03/05 17:38:30 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-compiler-libs
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-compiler-libs
 OCAML_COMPILER_LIBS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-compiler-libs+=	ocaml-compiler-libs>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-compiler-libs+=	ocaml-compiler-libs>=0.11.0nb3
+BUILDLINK_ABI_DEPENDS.ocaml-compiler-libs+=	ocaml-compiler-libs>=0.11.0nb4
 BUILDLINK_PKGSRCDIR.ocaml-compiler-libs?=	../../devel/ocaml-compiler-libs
 .endif	# OCAML_COMPILER_LIBS_BUILDLINK3_MK
 
