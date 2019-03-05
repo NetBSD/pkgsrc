39662183907187f5211222c3b2702f5e01b66816
jaapb <jaapb@pkgsrc.org>
2019-03-05 17:59:40 +0000

    Updated devel/ocaml-ppx_compare to version 0.12.0.
    Package is now compatible with dune 1.7.
    
    The CHANGES.md file is not up to date, but other changes are minor.

--- devel/ocaml-ppx_compare/buildlink3.mk
+++ devel/ocaml-ppx_compare/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2018/11/27 23:21:57 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2019/03/05 17:59:40 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_compare
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_compare
 OCAML_PPX_COMPARE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_compare+=	ocaml-ppx_compare>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_compare+=	ocaml-ppx_compare>=0.11.1nb3
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_compare+=	ocaml-ppx_compare>=0.12.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_compare?=		../../devel/ocaml-ppx_compare
 .endif	# OCAML_PPX_COMPARE_BUILDLINK3_MK
 
