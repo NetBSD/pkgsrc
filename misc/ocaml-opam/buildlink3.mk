a8c9445c5e86f8502c5fbd72e633cf66fa05963a
jaapb <jaapb@pkgsrc.org>
2019-03-05 18:36:24 +0000

    Updated misc/ocaml-opam to version 2.0.3.
    Package is now compatible with dune 1.7.
    
    Other upstream changes are minor; see the CHANGES file.

--- misc/ocaml-opam/buildlink3.mk
+++ misc/ocaml-opam/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2018/11/27 16:53:53 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2019/03/05 18:36:24 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-opam
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-opam
 OCAML_OPAM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-opam+=	ocaml-opam>=2.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-opam+=	ocaml-opam>=2.0.1
+BUILDLINK_ABI_DEPENDS.ocaml-opam+=	ocaml-opam>=2.0.3
 BUILDLINK_PKGSRCDIR.ocaml-opam?=	../../misc/ocaml-opam
 
 .endif	# OCAML_OPAM_BUILDLINK3_MK
