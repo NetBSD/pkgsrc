a77766407c0ca08fb527973a9057daa9644f2c3b
jaapb <jaapb@pkgsrc.org>
2019-03-05 18:53:15 +0000

    Updated net/ocaml-ipaddr to version 3.1.0.
    Package is now compatible with dune 1.7.
    
    Several backwards incompatible changes in version 3.0.0:
    - sexp serialisers removed from main interface;
    - macaddr module now in separate opam package;
    - replace of_string/bytes functions with rresult types;
    - use sexplib0 instead of the full sexp library;
    - changes to function signatures.
    
    Full details in the CHANGES.md file; there are also several minor changes.

--- net/ocaml-ipaddr/buildlink3.mk
+++ net/ocaml-ipaddr/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.17 2018/11/27 18:59:50 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2019/03/05 18:53:15 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ipaddr
 
 .if !defined(OCAML_IPADDR_BUILDLINK3_MK)
 OCAML_IPADDR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ipaddr+=	ocaml-ipaddr>=2.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-ipaddr?=	ocaml-ipaddr>=2.8.0nb7
+BUILDLINK_API_DEPENDS.ocaml-ipaddr+=	ocaml-ipaddr>=3.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-ipaddr?=	ocaml-ipaddr>=3.1.0
 BUILDLINK_PKGSRCDIR.ocaml-ipaddr?=	../../net/ocaml-ipaddr
 BUILDLINK_FILES.ocaml-ipaddr+=		lib/ocaml/site-lib/*
 
