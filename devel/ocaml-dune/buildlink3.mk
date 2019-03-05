18e9fae94ecc2f1467061a309a58acbe08e825c7
jaapb <jaapb@pkgsrc.org>
2019-03-05 16:11:03 +0000

    Updated devel/ocaml-dune to version 1.7.3.
    The biggest change (introduced in 1.7.0) is that dune-package files are
    generated instead of .dune files. This means that every package that is
    built with dune needs an update, of the PLIST if nothing else (I'll be
    committing those ASAP).
    
    There are several other changes, improvments and bugfixes that are
    listed in the CHANGES.md file in the distfile.

--- devel/ocaml-dune/buildlink3.mk
+++ devel/ocaml-dune/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/11/27 14:27:39 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/03/05 16:11:03 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-dune
 
 .if !defined(OCAML_DUNE_BUILDLINK3_MK)
 OCAML_DUNE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-dune+=	ocaml-dune>=1.5.1
-BUILDLINK_ABI_DEPENDS.ocaml-dune+=	ocaml-dune>=1.5.1
+BUILDLINK_API_DEPENDS.ocaml-dune+=	ocaml-dune>=1.7.0
+BUILDLINK_ABI_DEPENDS.ocaml-dune+=	ocaml-dune>=1.7.3
 BUILDLINK_PKGSRCDIR.ocaml-dune?=	../../devel/ocaml-dune
 .endif	# OCAML_DUNE_BUILDLINK3_MK
 
