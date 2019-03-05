328f65c01ee7d5b0857d221a4eb434ae57daadd0
ryoon <ryoon@pkgsrc.org>
2019-03-05 11:11:12 +0000

    Update to 5.1.0
    Changelog:
    # General improvements
    
        The Octave plotting system now supports high resolution screens, i.e., those with greater than 96 DPI which are referred to as HiDPI/Retina monitors.
    
        Unicode character support for files and folders in Windows.
    
        A new core function movfun will apply a function to a sliding window of arbitrary size on a dataset and accumulate the results. Many common cases have been implemented using the naming scheme movXXX where XXX is the function that will be applied. For example, the moving average over a dataset is movmean. New moving window functions:
    
        movfun movslice movmad movmax movmean movmedian movmin movprod movstd movsum movvar
    
        The fsolve function has been tweaked to use larger step sizes when calculating the Jacobian of a function with finite differences. This leads to faster convergence.
    
        The ranks function has been recoded for performance and is now 25X faster. In addition, it now supports a third argument that specifies how to resolve the ranking of tie values.
    
        The function randi has been recoded to produce an unbiased (all results are equally likely) sample of integers. This may produce different results in existing code. If it is necessary to reproduce the exact random integer sequence as in previous versions use
    
        ri = imin + floor ((imax - imin + 1) * rand ());
    
        The function isdefinite now returns true or false rather than -1, 0, or 1. To test for a positive semi-definite matrix (old output of 0) check whether the following two conditions hold:
    
        isdefinite (A) => 0 and isdefinite (A + 5*TOL, TOL) => 1
    
        The intmax, intmin, and flintmax functions now accept a variable as input. Existing code to query the range of an existing variable can be simplified by removing the call to class that was previously required. For example defining the variable x = int8 (3) in the workspace, calls like
    
        range = [ intmin(class(x)), intmax(class(x)) ]
    
        can in Octave 5 be simplified to range = [ intmin(x), intmax(x) ].
    
        The path handling functions no longer perform variable or brace expansion on path elements and Octave’s load-path is no longer subject to these expansions.
    
        A new printing device is available, "-ddumb", which produces ASCII art for plots. This device is only available with the gnuplot toolkit.
    
    # Dependencies
    
        The GUI requires Qt libraries. The minimum Qt4 version supported is Qt4.8. Qt5 of any version is preferred.
    
        The OSMesa library is no longer used. To print invisible figures when using OpenGL graphics, the Qt QOFFSCREENSURFACE feature must be available and you must use the qt graphics toolkit.
    
        The FFTW library is now required to perform FFT calculations. The FFTPACK sources have been removed from Octave.
    
    Matlab compatibility
    * Many improvements.

--- math/octave/patches/patch-libinterp_dldfcn_module.mk
+++ math/octave/patches/patch-libinterp_dldfcn_module.mk
@@ -1,15 +1,15 @@
-$NetBSD: patch-libinterp_dldfcn_module.mk,v 1.3 2018/08/12 08:33:55 maya Exp $
+$NetBSD: patch-libinterp_dldfcn_module.mk,v 1.4 2019/03/05 11:11:12 ryoon Exp $
 
 install .oct loadable modules with INSTALL_LIB to avoid stripping them
 
---- libinterp/dldfcn/module.mk.orig	2018-08-09 18:20:32.000000000 +0000
+--- libinterp/dldfcn/module.mk.orig	2019-02-23 17:33:37.000000000 +0000
 +++ libinterp/dldfcn/module.mk
-@@ -40,7 +40,7 @@ octlib_LTLIBRARIES += $(DLDFCN_LIBS)
+@@ -37,7 +37,7 @@ octlib_LTLIBRARIES += $(DLDFCN_LIBS)
  ## of symbolic links
  
  %.oct : %.la
 -	$(AM_V_GEN)$(INSTALL_PROGRAM) %reldir%/.libs/$(shell $(SED) -n -e "s/dlname='\([^']*\)'/\1/p" < $<) $@
 +	$(AM_V_GEN)$(INSTALL_LIB) %reldir%/.libs/$(shell $(SED) -n -e "s/dlname='\([^']*\)'/\1/p" < $<) $@
  
- else
- 
+ %canon_reldir%___delaunayn___la_SOURCES = %reldir%/__delaunayn__.cc
+ %canon_reldir%___delaunayn___la_CPPFLAGS = $(libinterp_liboctinterp_la_CPPFLAGS) $(QHULL_CPPFLAGS)
