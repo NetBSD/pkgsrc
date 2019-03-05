7da43b418d3fb7a1578a3f0b3fac912bad92c329
prlw1 <prlw1@pkgsrc.org>
2019-03-05 16:30:18 +0000

    Update py-meson to 0.49.2
    No release notes provided.
    
    In brief:
    - meson still strips rpath on install
    - glib2 requires meson >= 0.48.0

--- devel/py-meson/patches/patch-mesonbuild_environment.py
+++ devel/py-meson/patches/patch-mesonbuild_environment.py
@@ -1,32 +1,24 @@
-$NetBSD: patch-mesonbuild_environment.py,v 1.2 2018/05/15 09:04:37 jperkin Exp $
+$NetBSD: patch-mesonbuild_environment.py,v 1.3 2019/03/05 16:30:18 prlw1 Exp $
 
 Support SunOS ar and SunOS-specific GCC behaviour.
 
---- mesonbuild/environment.py.orig	2018-02-20 21:48:57.000000000 +0000
+--- mesonbuild/environment.py.orig	2019-01-23 16:46:09.000000000 +0000
 +++ mesonbuild/environment.py
-@@ -29,6 +29,7 @@ from .compilers import (
-     GCC_CYGWIN,
-     GCC_MINGW,
-     GCC_OSX,
-+    GCC_SUNOS,
-     GCC_STANDARD,
-     ICC_STANDARD,
-     is_assembly,
-@@ -409,6 +410,8 @@ class Environment:
-         # Detect GCC type (Apple, MinGW, Cygwin, Unix)
-         if '__APPLE__' in defines:
-             return GCC_OSX
-+        elif '__sun' in defines:
-+            return GCC_SUNOS
-         elif '__MINGW32__' in defines or '__MINGW64__' in defines:
-             return GCC_MINGW
+@@ -528,6 +528,8 @@ class Environment:
+             return CompilerType.GCC_MINGW
          elif '__CYGWIN__' in defines:
-@@ -779,6 +782,8 @@ class Environment:
+             return CompilerType.GCC_CYGWIN
++        elif '__sun' in defines:
++            return CompilerType.GCC_SUNOS
+         return CompilerType.GCC_STANDARD
+ 
+     def _get_compilers(self, lang, want_cross):
+@@ -1019,6 +1021,8 @@ class Environment:
                  return ArLinker(linker)
              if p.returncode == 1 and err.startswith('usage'): # OSX
                  return ArLinker(linker)
-+            if p.returncode == 1 and err.startswith('ar: bad option'): # SunOS
++            if p.returncode == 1 and err.startswith('ar: bad option'): # SUNOS
 +                return ArLinker(linker)
+             if p.returncode == 1 and err.startswith('Usage'): # AIX
+                 return ArLinker(linker)
          self._handle_exceptions(popen_exceptions, linkers, 'linker')
-         raise EnvironmentException('Unknown static linker "%s"' % ' '.join(linkers))
- 
