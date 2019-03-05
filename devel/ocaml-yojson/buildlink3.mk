d225a9b108384f833d37d37964b3347bc3de5068
jaapb <jaapb@pkgsrc.org>
2019-03-05 18:21:04 +0000

    Updated devel/ocaml-yojson to version 1.7.0.
    Package is now compatible with dune 1.7.
    
    Main change is that json types are now replaced by their t aliases;
    there are some other minor bugfixes and changes as well. Full details in
    the CHANGES.md file.

--- devel/ocaml-yojson/buildlink3.mk
+++ devel/ocaml-yojson/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/11/27 23:33:40 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2019/03/05 18:21:04 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-yojson
 
 .if !defined(OCAML_YOJSON_BUILDLINK3_MK)
 OCAML_YOJSON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-yojson+=	ocaml-yojson>=1.3.3
-BUILDLINK_ABI_DEPENDS.ocaml-yojson+=	ocaml-yojson>=1.4.1nb4
+BUILDLINK_API_DEPENDS.ocaml-yojson+=	ocaml-yojson>=1.7.0
+BUILDLINK_ABI_DEPENDS.ocaml-yojson+=	ocaml-yojson>=1.7.0
 BUILDLINK_PKGSRCDIR.ocaml-yojson?=	../../devel/ocaml-yojson
 
 .endif	# OCAML_YOJSON_BUILDLINK3_MK
