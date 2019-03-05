71b9a5e7ee275badf992227b3300cdfc1dd2b6aa
jaapb <jaapb@pkgsrc.org>
2019-03-05 17:34:16 +0000

    Updated devel/ocaml-base64 to version 3.1.0.
    The package is now compatible with dune 1.7.
    
    Several API-breaking changes in version 3.0.0: module renames and
    changes in function signatures.
    
    Other, more minor bugfixes and additions.

--- devel/ocaml-base64/buildlink3.mk
+++ devel/ocaml-base64/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/07/19 15:15:21 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2019/03/05 17:34:16 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-base64
 
 .if !defined(OCAML_BASE64_BUILDLINK3_MK)
 OCAML_BASE64_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-base64+=	ocaml-base64>=2.0.0
-BUILDLINK_API_DEPENDS.ocaml-base64+=	ocaml-base64>=2.2.0nb4
+BUILDLINK_API_DEPENDS.ocaml-base64+=	ocaml-base64>=3.0.0
+BUILDLINK_API_DEPENDS.ocaml-base64+=	ocaml-base64>=3.1.0
 BUILDLINK_PKGSRCDIR.ocaml-base64?=	../../devel/ocaml-base64
 .endif	# OCAML_BASE64_BUILDLINK3_MK
 
