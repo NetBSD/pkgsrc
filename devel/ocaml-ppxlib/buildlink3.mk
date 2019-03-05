2eb3c5bb88bb892654e7d84d11b1db8e6400b6f3
jaapb <jaapb@pkgsrc.org>
2019-03-05 17:57:36 +0000

    Updated devel/ocaml-ppxlib to version 0.5.0.
    Package is now compatible with dune 1.7.
    
    Other minor changes, bugfixes and additions, see CHANGES.md in the
    distfile.

--- devel/ocaml-ppxlib/buildlink3.mk
+++ devel/ocaml-ppxlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/11/27 18:57:00 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2019/03/05 17:57:36 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppxlib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppxlib
 OCAML_PPXLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.3.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.3.1
+BUILDLINK_ABI_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.5.0
 BUILDLINK_PKGSRCDIR.ocaml-ppxlib?=	../../devel/ocaml-ppxlib
 
 .endif	# OCAML_PPXLIB_BUILDLINK3_MK
