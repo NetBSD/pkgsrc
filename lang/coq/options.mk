544309c2ef3cb6c2d000192398856a9a22c8d98b
jaapb <jaapb@pkgsrc.org>
2019-03-06 09:28:23 +0000

    Updated lang/coq to version 8.9.0.
    Many improvements and fixes, but none that appear to break compatibility.
    For more details see the CHANGES file.

--- lang/coq/options.mk
+++ lang/coq/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2018/08/02 12:57:03 jaapb Exp $
+# $NetBSD: options.mk,v 1.5 2019/03/06 09:28:23 jaapb Exp $
 
 PKG_OPTIONS_VAR= PKG_OPTIONS.coq
 PKG_SUPPORTED_OPTIONS= doc coqide
@@ -7,33 +7,14 @@ PKG_SUGGESTED_OPTIONS= coqide
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdoc)
-BUILD_DEPENDS+= hevea>=1.10:../../textproc/hevea
+PYTHON_VERSIONS_ACCEPTED=	37 36 35 34
 CONFIGURE_ARGS+=	-with-doc yes
 PLIST.doc=		yes
-#BUILD_DEPENDS+=		tex-latex-bin-[0-9]*:../../print/tex-latex-bin
-#BUILD_DEPENDS+=		makeindexk-[0-9]*:../../textproc/makeindexk
-#BUILD_DEPENDS+=		dvipsk-[0-9]*:../../print/dvipsk
-#BUILD_DEPENDS+=		tex-babel-[0-9]*:../../print/tex-babel
-#BUILD_DEPENDS+=		tex-babel-english-[0-9]*:../../print/tex-babel-english
-#BUILD_DEPENDS+=		tex-bibtex-[0-9]*:../../print/tex-bibtex
-#BUILD_DEPENDS+=		tex-cm-super-[0-9]*:../../fonts/tex-cm-super
-#BUILD_DEPENDS+=		tex-comment-[0-9]*:../../print/tex-comment
-#BUILD_DEPENDS+=		tex-ec-[0-9]*:../../fonts/tex-ec
-#BUILD_DEPENDS+=		tex-eepic-[0-9]*:../../graphics/tex-eepic
-#BUILD_DEPENDS+=		tex-fancyhdr-[0-9]*:../../print/tex-fancyhdr
-#BUILD_DEPENDS+=		tex-float-[0-9]*:../../print/tex-float
-#BUILD_DEPENDS+=		tex-index-[0-9]*:../../print/tex-index
-#BUILD_DEPENDS+=		tex-listings-[0-9]*:../../print/tex-listings
+BUILD_DEPENDS+=		tex-latex-bin-[0-9]*:../../print/tex-latex-bin
+BUILD_DEPENDS+= hevea>=1.10:../../textproc/hevea
 BUILD_DEPENDS+=		tex-moreverb-[0-9]*:../../print/tex-moreverb
-#BUILD_DEPENDS+=		tex-multirow-[0-9]*:../../print/tex-multirow
 BUILD_DEPENDS+=		tex-preprint-[0-9]*:../../print/tex-preprint
-#BUILD_DEPENDS+=		tex-pslatex-[0-9]*:../../print/tex-pslatex
-#BUILD_DEPENDS+=		tex-psnfss-[0-9]*:../../fonts/tex-psnfss
-#BUILD_DEPENDS+=		tex-stmaryrd-[0-9]*:../../fonts/tex-stmaryrd
 BUILD_DEPENDS+=		tex-ucs-[0-9]*:../../print/tex-ucs
-#BUILD_DEPENDS+=		tex-xcolor-[0-9]*:../../print/tex-xcolor
-#BUILD_DEPENDS+=		fig2dev-[0-9]*:../../print/fig2dev
-#.include "../../graphics/ImageMagick/buildlink3.mk"
 BUILD_DEPENDS+=		py[0-9]*-sphinx-[0-9]*:../../textproc/py-sphinx
 BUILD_DEPENDS+=		py[0-9]*-sphinx-rtd-theme-[0-9]*:../../textproc/py-sphinx-rtd-theme
 BUILD_DEPENDS+=		py[0-9]*-sphinxcontrib-bibtex-[0-9]*:../../textproc/py-sphinxcontrib-bibtex
@@ -42,6 +23,29 @@ BUILD_DEPENDS+=		py[0-9]*-pybtex-docutils-[0-9]*:../../textproc/py-pybtex-docuti
 BUILD_DEPENDS+=		py[0-9]*-pexpect-[0-9]*:../../devel/py-pexpect
 BUILD_DEPENDS+=		py[0-9]*-antlr4-[0-9]*:../../textproc/py-antlr4
 BUILD_DEPENDS+=		py[0-9]*-beautifulsoup4-[0-9]*:../../www/py-beautifulsoup4
+BUILD_DEPENDS+=		latexmk-[0-9]*:../../print/latexmk
+BUILD_DEPENDS+=		tex-xetex-[0-9]*:../../print/tex-xetex
+BUILD_DEPENDS+=		tex-polyglossia-[0-9]*:../../print/tex-polyglossia
+BUILD_DEPENDS+=		tex-fncychap-[0-9]*:../../print/tex-fncychap
+BUILD_DEPENDS+=		tex-tabulary-[0-9]*:../../print/tex-tabulary
+BUILD_DEPENDS+=		tex-varwidth-[0-9]*:../../print/tex-varwidth
+BUILD_DEPENDS+=		tex-parskip-[0-9]*:../../print/tex-parskip
+BUILD_DEPENDS+=		tex-upquote-[0-9]*:../../print/tex-upquote
+BUILD_DEPENDS+=		tex-capt-of-[0-9]*:../../print/tex-capt-of
+BUILD_DEPENDS+=		tex-needspace-[0-9]*:../../print/tex-needspace
+BUILD_DEPENDS+=		tex-unicode-math-[0-9]*:../../math/tex-unicode-math
+BUILD_DEPENDS+=		tex-microtype-[0-9]*:../../print/tex-microtype
+BUILD_DEPENDS+=		tex-adjustbox-[0-9]*:../../print/tex-adjustbox
+BUILD_DEPENDS+=		tex-xindy-[0-9]*:../../textproc/tex-xindy
+BUILD_DEPENDS+=		tex-ec-[0-9]*:../../fonts/tex-ec
+BUILD_DEPENDS+=		tex-xcolor-[0-9]*:../../print/tex-xcolor
+BUILD_DEPENDS+=		tex-xkeyval-[0-9]*:../../print/tex-xkeyval
+BUILD_DEPENDS+=		tex-titlesec-[0-9]*:../../print/tex-titlesec
+BUILD_DEPENDS+=		tex-framed-[0-9]*:../../print/tex-framed
+BUILD_DEPENDS+=		tex-float-[0-9]*:../../print/tex-float
+BUILD_DEPENDS+=		tex-wrapfig-[0-9]*:../../print/tex-wrapfig
+BUILD_DEPENDS+=		tex-lm-math-[0-9]*:../../fonts/tex-lm-math
+BUILD_DEPENDS+=		dvipsk-[0-9]*:../../print/dvipsk
 .else
 CONFIGURE_ARGS+= -with-doc no
 .endif
