13bb68118567cde3ffb76b7d420671895a4dcebd
jperkin <jperkin@pkgsrc.org>
2019-03-07 20:19:11 +0000

    rust: Fix another instance of the rand crate for SunOS.
    While here convert some tabs->spaces that broke the test suite.

--- lang/rust/patches/patch-src_tools_cargo_src_cargo_core_profiles.rs
+++ lang/rust/patches/patch-src_tools_cargo_src_cargo_core_profiles.rs
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_tools_cargo_src_cargo_core_profiles.rs,v 1.2 2018/11/27 15:45:23 adam Exp $
+$NetBSD: patch-src_tools_cargo_src_cargo_core_profiles.rs,v 1.3 2019/03/07 20:19:11 jperkin Exp $
 
 Turn off incremental builds for sparc64, ref.
 https://sources.debian.org/patches/cargo/0.29.0-1/2007_sparc64_disable_incremental_build.patch/
@@ -9,9 +9,9 @@ https://sources.debian.org/patches/cargo/0.29.0-1/2007_sparc64_disable_increment
              debuginfo: Some(2),
              debug_assertions: true,
              overflow_checks: true,
-+	    #[cfg(target_arch = "sparc64")]
-+	    incremental: false,
-+	    #[cfg(not(target_arch = "sparc64"))]
++            #[cfg(target_arch = "sparc64")]
++            incremental: false,
++            #[cfg(not(target_arch = "sparc64"))]
              incremental: true,
              ..Profile::default()
          }
