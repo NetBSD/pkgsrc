494da6ce7f4c4a51dce30bdf2ee4745164cbedaa
jaapb <jaapb@pkgsrc.org>
2019-03-12 18:44:33 +0000

    Added devel/ocaml-ppx_inline_test, a ppx rewriter for in-line tests

---
+++ devel/ocaml-ppx_inline_test/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/03/12 18:44:33 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-ppx_inline_test
+
+.if !defined(OCAML_PPX_INLINE_TEST_BUILDLINK3_MK)
+OCAML_PPX_INLINE_TEST_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-ppx_inline_test+=	ocaml-ppx_inline_test>=0.12.0
+BUILDLINK_PKGSRCDIR.ocaml-ppx_inline_test?=	../../devel/ocaml-ppx_inline_test
+
+.endif	# OCAML_PPX_INLINE_TEST_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-ppx_inline_test
