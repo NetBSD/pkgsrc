e48787621b4973a2770d16e3da8986475c924d88
taca <taca@pkgsrc.org>
2019-03-12 04:13:42 +0000

    lang/php72: update to 7.2.16
    Update php72 to 7.2.16.
    
    07 Mar 2019, PHP 7.2.16
    
    - Core:
      . Fixed bug #77589 (Core dump using parse_ini_string with numeric sections).
        (Laruence)
      . Fixed bug #77630 (rename() across the device may allow unwanted access
        during processing). (Stas)
    
    - COM:
      . Fixed bug #77621 (Already defined constants are not properly reported).
        (cmb)
    
    - EXIF:
      . Fixed bug #77509 (Uninitialized read in exif_process_IFD_in_TIFF). (Stas)
      . Fixed bug #77540 (Invalid Read on exif_process_SOFn). (Stas)
      . Fixed bug #77563 (Uninitialized read in exif_process_IFD_in_MAKERNOTE). (Stas)
      . Fixed bug #77659 (Uninitialized read in exif_process_IFD_in_MAKERNOTE). (Stas)
    
    - PDO_OCI:
      . Support Oracle Database tracing attributes ACTION, MODULE,
        CLIENT_INFO, and CLIENT_IDENTIFIER. (Cameron Porter)
    
    - PHAR:
      . Fixed bug #77396 (Null Pointer Dereference in phar_create_or_parse_filename).
        (bishop)
    
    - SPL:
      . Fixed bug #51068 (DirectoryIterator glob:// don't support current path
        relative queries). (Ahmed Abdou)
      . Fixed bug #77431 (openFile() silently truncates after a null byte). (cmb)
    
    - Standard:
      . Fixed bug #77552 (Unintialized php_stream_statbuf in stat functions).
        (John Stevenson)
    
    - MySQL
      . Disabled LOCAL INFILE by default, can be enabled using php.ini directive
        mysqli.allow_local_infile for mysqli, or PDO::MYSQL_ATTR_LOCAL_INFILE
        attribute for pdo_mysql. (Darek Slusarczyk)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.249 2019/03/12 04:12:16 taca Exp $
+# $NetBSD: phpversion.mk,v 1.250 2019/03/12 04:13:42 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP71_VERSION=	7.1.27
-PHP72_VERSION=	7.2.15
+PHP72_VERSION=	7.2.16
 PHP73_VERSION=	7.3.2
 
 # Define initial release of major version.
