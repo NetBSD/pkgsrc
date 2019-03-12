0d7557d6b1278ed8e47293dd7f25f38360546cff
taca <taca@pkgsrc.org>
2019-03-12 04:15:03 +0000

    lang/php73: update to 7.3.3
    Update php73 to 7.3.3.
    
    07 Mar 2019, PHP 7.3.3
    
    - Core:
      . Fixed bug #77589 (Core dump using parse_ini_string with numeric sections).
        (Laruence)
      . Fixed bug #77329 (Buffer Overflow via overly long Error Messages).
        (Dmitry)
      . Fixed bug #77494 (Disabling class causes segfault on member access).
        (Dmitry)
      . Fixed bug #77498 (Custom extension Segmentation fault when declare static
        property). (Nikita)
      . Fixed bug #77530 (PHP crashes when parsing `(2)::class`). (Ekin)
      . Fixed bug #77546 (iptcembed broken function). (gdegoulet)
      . Fixed bug #77630 (rename() across the device may allow unwanted access
        during processing). (Stas)
    
    - COM:
      . Fixed bug #77621 (Already defined constants are not properly reported).
        (cmb)
      . Fixed bug #77626 (Persistence confusion in php_com_import_typelib()). (cmb)
    
    - EXIF:
      . Fixed bug #77509 (Uninitialized read in exif_process_IFD_in_TIFF). (Stas)
      . Fixed bug #77540 (Invalid Read on exif_process_SOFn). (Stas)
      . Fixed bug #77563 (Uninitialized read in exif_process_IFD_in_MAKERNOTE). (Stas)
      . Fixed bug #77659 (Uninitialized read in exif_process_IFD_in_MAKERNOTE). (Stas)
    
    - Mbstring:
      . Fixed bug #77514 (mb_ereg_replace() with trailing backslash adds null byte).
        (Nikita)
    
    - MySQL
      . Disabled LOCAL INFILE by default, can be enabled using php.ini directive
        mysqli.allow_local_infile for mysqli, or PDO::MYSQL_ATTR_LOCAL_INFILE
        attribute for pdo_mysql. (Darek Slusarczyk)
    
    - OpenSSL:
      . Fixed bug #77390 (feof might hang on TLS streams in case of fragmented TLS
        records). (Abyl Valg, Jakub Zelenka)
    
    - PDO_OCI:
      . Support Oracle Database tracing attributes ACTION, MODULE,
        CLIENT_INFO, and CLIENT_IDENTIFIER. (Cameron Porter)
    
    - PHAR:
      . Fixed bug #77396 (Null Pointer Dereference in phar_create_or_parse_filename).
        (bishop)
      . Fixed bug #77586 (phar_tar_writeheaders_int() buffer overflow). (bishop)
    
    - phpdbg:
      . Fixed bug #76596 (phpdbg support for display_errors=stderr). (kabel)
    
    - SPL:
      . Fixed bug #51068 (DirectoryIterator glob:// don't support current path
        relative queries). (Ahmed Abdou)
      . Fixed bug #77431 (openFile() silently truncates after a null byte). (cmb)
    
    - Standard:
      . Fixed bug #77552 (Unintialized php_stream_statbuf in stat functions).
        (John Stevenson)
      . Fixed bug #77612 (setcookie() sets incorrect SameSite header if all of its
        options filled). (Nikita)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.250 2019/03/12 04:13:42 taca Exp $
+# $NetBSD: phpversion.mk,v 1.251 2019/03/12 04:15:03 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP71_VERSION=	7.1.27
 PHP72_VERSION=	7.2.16
-PHP73_VERSION=	7.3.2
+PHP73_VERSION=	7.3.3
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
