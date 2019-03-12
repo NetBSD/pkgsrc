82cb5a9af42fc96e6afd3564e4b77aabf3b763a9
jaapb <jaapb@pkgsrc.org>
2019-03-12 18:34:12 +0000

    Added devel/ocaml-ppx_here, a ppx rewriter for source position

---
+++ devel/ocaml-ppx_here/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/03/12 18:34:12 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-ppx_here
+
+.if !defined(OCAML_PPX_HERE_BUILDLINK3_MK)
+OCAML_PPX_HERE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-ppx_here+=	ocaml-ppx_here>=0.12.0
+BUILDLINK_PKGSRCDIR.ocaml-ppx_here?=	../../devel/ocaml-ppx_here
+
+.endif	# OCAML_PPX_HERE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-ppx_here
