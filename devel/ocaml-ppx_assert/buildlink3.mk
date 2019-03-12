76ea93d72887958e041b27ee6d1ea7e2590e08e2
jaapb <jaapb@pkgsrc.org>
2019-03-12 18:42:12 +0000

    Added devel/ocaml-ppx_assert, a ppx rewriter for assertions

---
+++ devel/ocaml-ppx_assert/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/03/12 18:42:12 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-ppx_assert
+
+.if !defined(OCAML_PPX_ASSERT_BUILDLINK3_MK)
+OCAML_PPX_ASSERT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-ppx_assert+=	ocaml-ppx_assert>=0.12.0
+BUILDLINK_PKGSRCDIR.ocaml-ppx_assert?=	../../devel/ocaml-ppx_assert
+
+.endif	# OCAML_PPX_ASSERT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-ppx_assert
