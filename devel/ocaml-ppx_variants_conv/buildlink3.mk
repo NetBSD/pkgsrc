402f36aa804028b6bdd72950886a47a78443a581
jaapb <jaapb@pkgsrc.org>
2019-03-13 11:03:16 +0000

    Added devel/ocaml-ppx_variants_conv, ppx rewriter for variant types

---
+++ devel/ocaml-ppx_variants_conv/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/03/13 11:03:16 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-ppx_variants_conv
+
+.if !defined(OCAML_PPX_VARIANTS_CONV_BUILDLINK3_MK)
+OCAML_PPX_VARIANTS_CONV_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-ppx_variants_conv+=	ocaml-ppx_variants_conv>=0.12.0
+BUILDLINK_PKGSRCDIR.ocaml-ppx_variants_conv?=	../../devel/ocaml-ppx_variants_conv
+
+.endif	# OCAML_PPX_VARIANTS_CONV_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-ppx_variants_conv
