20a7c8c6a4eb1d02cafcc657845c4ee5f772510b
leot <leot@pkgsrc.org>
2019-03-13 20:49:40 +0000

    webkit-gtk: Update to 2.24.0
    pkgsrc changes:
     - Remove patch-Source_JavaScriptCore_assembler_MacroAssemblerARM.cpp: logic
       changed upstream and there is no longer a function that check for a VFP.
    
    Changes:
    2.24.0
    ------
     - Added support fot content filtering.
     - Variation fonts support.
     - Fully emoji rendering support.
     - Added navigation and pinch zoom gestures for touchpads.
     - Support for JPEG2000 images (please note that in pkgsrc at the
       moment it is disabled to avoid a dependency on openjpeg)
     - Script dialogs are now modal to the current web view only.
     - New API to convert URI to format for display.

---
+++ www/webkit-gtk/patches/patch-Source_WebKit_Scripts_generate-unified-sources.sh
@@ -0,0 +1,15 @@
+$NetBSD: patch-Source_WebKit_Scripts_generate-unified-sources.sh,v 1.1 2019/03/13 20:49:41 leot Exp $
+
+Avoid unportable `==' test(1) operator.
+
+--- Source/WebKit/Scripts/generate-unified-sources.sh.orig	2019-02-12 11:21:18.000000000 +0000
++++ Source/WebKit/Scripts/generate-unified-sources.sh
+@@ -7,7 +7,7 @@ ARGS=("$@")
+ cd $SRCROOT
+ 
+ if [ -z "${BUILD_SCRIPTS_DIR}" ]; then
+-    if [ "${DEPLOYMENT_LOCATION}" == "YES" ]; then
++    if [ "${DEPLOYMENT_LOCATION}" = "YES" ]; then
+         BUILD_SCRIPTS_DIR="${SDKROOT}${WK_ALTERNATE_WEBKIT_SDK_PATH}/usr/local/include/wtf/Scripts"
+     else
+         BUILD_SCRIPTS_DIR="${BUILT_PRODUCTS_DIR}/usr/local/include/wtf/Scripts"
