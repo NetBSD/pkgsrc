dfc32ce5f2c4d314d0588de50a9a75953c9958d2
wiz <wiz@pkgsrc.org>
2019-03-13 21:16:15 +0000

    notion: fix an incorrect libX11 call.
    Causes crashes with stricter jemalloc on -current.
    
    Bump PKGREVISION.

---
+++ wm/notion/patches/patch-ioncore_group.c
@@ -0,0 +1,23 @@
+$NetBSD: patch-ioncore_group.c,v 1.1 2019/03/13 21:16:15 wiz Exp $
+
+Xutf8TextListToTextProperty needs a NULL terminated list of pointers.
+
+--- ioncore/group.c.orig	2017-05-05 14:55:07.000000000 +0000
++++ ioncore/group.c
+@@ -349,7 +349,7 @@ void group_managed_notify(WGroup *ws, WR
+ 
+ bool group_init(WGroup *ws, WWindow *par, const WFitParams *fp, const char *name)
+ {
+-    const char *p[1];
++    const char *p[2];
+ 
+     ws->current_managed=NULL;
+     ws->managed_stdisp=NULL;
+@@ -365,6 +365,7 @@ bool group_init(WGroup *ws, WWindow *par
+         return FALSE;
+ 
+     p[0] = name;
++    p[1] = NULL;
+     xwindow_set_text_property(ws->dummywin, XA_WM_NAME, p, 1);
+ 
+     region_init(&ws->reg, par, fp);
