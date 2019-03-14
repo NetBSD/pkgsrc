2d55312fa30c4d7f661af103c975e1ebd0a3ee61
wiz <wiz@pkgsrc.org>
2019-03-14 11:07:17 +0000

    i3: update to 4.16.1.
    This is i3 v4.16.1. This version is considered stable. All users of i3 are
    strongly encouraged to upgrade.
    
    This is a bugfix release for v4.16.
    
    Bugfixes
    
      • Truncate wm_name utf8 strings to first zero byte
        (fixes window title corruption)
      • Apply title_align to non-leaf containers
        Additionally, marks will now display for non-leaf containers.
      • attach_to_workspace: set new parent before tree_render
        (fixes a heap-use-after-free)
      • Use ipc queue for all messages
        (fixes an i3bar crash)
      • Fix crash with popups when fullscreen is non-leaf
      • Fix: render_con shows floating containers on wrong workspace

--- wm/i3/patches/patch-src_main.c
+++ wm/i3/patches/patch-src_main.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_main.c,v 1.4 2018/10/19 15:21:54 jperkin Exp $
+$NetBSD: patch-src_main.c,v 1.5 2019/03/14 11:07:18 wiz Exp $
 * NetBSD versions lower than 7 does not have shm_open and shm_unlink
   see patch-src_log.c 
---- src/main.c.orig	2018-03-10 17:29:14.000000000 +0000
+--- src/main.c.orig	2019-01-27 15:45:19.000000000 +0000
 +++ src/main.c
-@@ -169,11 +169,13 @@ static void i3_exit(void) {
-     ev_loop_destroy(main_loop);
- #endif
- 
+@@ -160,11 +160,13 @@ void main_set_x11_cb(bool enable) {
+  *
+  */
+ static void i3_exit(void) {
 +#if !defined(__NetBSD__)
      if (*shmlogname != '\0') {
          fprintf(stderr, "Closing SHM log \"%s\"\n", shmlogname);
@@ -16,8 +16,8 @@ $NetBSD: patch-src_main.c,v 1.4 2018/10/19 15:21:54 jperkin Exp $
 +#endif
      ipc_shutdown(SHUTDOWN_REASON_EXIT);
      unlink(config.ipc_socket_path);
- }
-@@ -185,9 +187,11 @@ static void i3_exit(void) {
+     xcb_disconnect(conn);
+@@ -188,9 +190,11 @@ static void i3_exit(void) {
   *
   */
  static void handle_core_signal(int sig, siginfo_t *info, void *data) {
