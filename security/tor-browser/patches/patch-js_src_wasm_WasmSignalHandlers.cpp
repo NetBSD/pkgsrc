cb16f159c5fda3704551ea24da2c94e9f5fd738f
wiz <wiz@pkgsrc.org>
2019-03-15 11:51:26 +0000

    tor-browser: update for recent changes to firefox60 and rust.
    Reduce diffs to firefox60 even more while here.

--- security/tor-browser/patches/patch-js_src_wasm_WasmSignalHandlers.cpp
+++ security/tor-browser/patches/patch-js_src_wasm_WasmSignalHandlers.cpp
@@ -1,14 +1,14 @@
-$NetBSD: patch-js_src_wasm_WasmSignalHandlers.cpp,v 1.1 2019/02/25 15:32:24 wiz Exp $
+$NetBSD: patch-js_src_wasm_WasmSignalHandlers.cpp,v 1.2 2019/03/15 11:51:26 wiz Exp $
 
 Support SunOS.
 
---- js/src/wasm/WasmSignalHandlers.cpp.orig	2019-02-23 20:00:48.000000000 +0000
+--- js/src/wasm/WasmSignalHandlers.cpp.orig	2019-02-13 14:19:40.000000000 +0000
 +++ js/src/wasm/WasmSignalHandlers.cpp
-@@ -135,6 +135,7 @@ struct AutoSignalHandler {
- #else
- #define XMM_sig(p, i) ((p)->uc_mcontext.fpregs.fp_reg_set.fpchip_state.xmm[i])
- #define EIP_sig(p) ((p)->uc_mcontext.gregs[REG_PC])
-+#  include <sys/regset.h>
+@@ -133,6 +133,7 @@ struct AutoSignalHandler {
  #define EBP_sig(p) ((p)->uc_mcontext.gregs[REG_EBP])
  #define ESP_sig(p) ((p)->uc_mcontext.gregs[REG_ESP])
- #endif
+ #else
++#include <sys/regset.h>
+ #define XMM_sig(p, i) ((p)->uc_mcontext.fpregs.fp_reg_set.fpchip_state.xmm[i])
+ #define EIP_sig(p) ((p)->uc_mcontext.gregs[REG_PC])
+ #define EBP_sig(p) ((p)->uc_mcontext.gregs[REG_EBP])
