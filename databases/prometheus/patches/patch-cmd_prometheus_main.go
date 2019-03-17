8c6050f112889e4ce3181387c2cad1951530d1d2
tm <tm@pkgsrc.org>
2019-03-17 12:55:13 +0000

    prometheus: import version 2.8.0
    The Prometheus monitoring system and time series database.
    
    Prometheus, a Cloud Native Computing Foundation project, is a systems and
    service monitoring system. It collects metrics from configured targets at
    given intervals, evaluates rule expressions, displays the results, and
    can trigger alerts if some condition is observed to be true.

---
+++ databases/prometheus/patches/patch-cmd_prometheus_main.go
@@ -0,0 +1,35 @@
+$NetBSD: patch-cmd_prometheus_main.go,v 1.1 2019/03/17 12:55:13 tm Exp $
+
+Add prefix for SYSCONFDIR and VARBASE to store configuration file and metrics data at the correct location.
+
+--- cmd/prometheus/main.go.orig	2019-03-12 04:09:20.000000000 +0000
++++ cmd/prometheus/main.go
+@@ -133,7 +133,7 @@ func main() {
+ 	a.HelpFlag.Short('h')
+ 
+ 	a.Flag("config.file", "Prometheus configuration file path.").
+-		Default("prometheus.yml").StringVar(&cfg.configFile)
++		Default("@PKG_SYSCONFDIR@/prometheus.yml").StringVar(&cfg.configFile)
+ 
+ 	a.Flag("web.listen-address", "Address to listen on for UI, API, and telemetry.").
+ 		Default("0.0.0.0:9090").StringVar(&cfg.web.ListenAddress)
+@@ -163,16 +163,16 @@ func main() {
+ 		Default("false").BoolVar(&cfg.web.EnableAdminAPI)
+ 
+ 	a.Flag("web.console.templates", "Path to the console template directory, available at /consoles.").
+-		Default("consoles").StringVar(&cfg.web.ConsoleTemplatesPath)
++		Default("@PKG_SYSCONFDIR@/consoles").StringVar(&cfg.web.ConsoleTemplatesPath)
+ 
+ 	a.Flag("web.console.libraries", "Path to the console library directory.").
+-		Default("console_libraries").StringVar(&cfg.web.ConsoleLibrariesPath)
++		Default("@PKG_SYSCONFDIR@/console_libraries").StringVar(&cfg.web.ConsoleLibrariesPath)
+ 
+ 	a.Flag("web.page-title", "Document title of Prometheus instance.").
+ 		Default("Prometheus Time Series Collection and Processing Server").StringVar(&cfg.web.PageTitle)
+ 
+ 	a.Flag("storage.tsdb.path", "Base path for metrics storage.").
+-		Default("data/").StringVar(&cfg.localStoragePath)
++		Default("@VARBASE@/lib/prometheus/data/").StringVar(&cfg.localStoragePath)
+ 
+ 	a.Flag("storage.tsdb.min-block-duration", "Minimum duration of a data block before being persisted. For use in testing.").
+ 		Hidden().Default("2h").SetValue(&cfg.tsdb.MinBlockDuration)
