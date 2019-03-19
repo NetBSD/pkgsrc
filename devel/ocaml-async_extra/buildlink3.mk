06ca53516e43b9cf117cd5d366777304eec60667
jaapb <jaapb@pkgsrc.org>
2019-03-19 10:51:00 +0000

    Added devel/ocaml-async_extra, extra stuff for ocaml-async

---
+++ devel/ocaml-async_extra/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/03/19 10:51:00 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-async_extra
+
+.if !defined(OCAML_ASYNC_EXTRA_BUILDLINK3_MK)
+OCAML_ASYNC_EXTRA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-async_extra+=	ocaml-async_extra>=0.12.0
+BUILDLINK_PKGSRCDIR.ocaml-async_extra?=	../../devel/ocaml-async_extra
+
+.endif	# OCAML_ASYNC_EXTRA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-async_extra
