e079a1517873fb81e6a440a3bec8ce95712a9e2d
jaapb <jaapb@pkgsrc.org>
2019-03-19 11:19:17 +0000

    Added devel/ocaml-fpath, file path library for OCaml

---
+++ devel/ocaml-fpath/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/03/19 11:19:17 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-fpath
+
+.if !defined(OCAML_FPATH_BUILDLINK3_MK)
+OCAML_FPATH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-fpath+=	ocaml-fpath>=0.7.2
+BUILDLINK_PKGSRCDIR.ocaml-fpath?=	../../devel/ocaml-fpath
+
+.endif	# OCAML_FPATH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-fpath
