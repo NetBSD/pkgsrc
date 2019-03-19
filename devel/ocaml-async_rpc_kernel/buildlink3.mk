795bb07bebcf0bb0e83c3e9051dabc176edeaedb
jaapb <jaapb@pkgsrc.org>
2019-03-19 10:43:19 +0000

    Added devel/ocaml-async_rpc_kernel, platform-independent RPC code for async

---
+++ devel/ocaml-async_rpc_kernel/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/03/19 10:43:19 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-async_rpc_kernel
+
+.if !defined(OCAML_ASYNC_RPC_KERNEL_BUILDLINK3_MK)
+OCAML_ASYNC_RPC_KERNEL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-async_rpc_kernel+=	ocaml-async_rpc_kernel>=0.12.0
+BUILDLINK_PKGSRCDIR.ocaml-async_rpc_kernel?=	../../devel/ocaml-async_rpc_kernel
+
+.endif	# OCAML_ASYNC_RPC_KERNEL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-async_rpc_kernel
