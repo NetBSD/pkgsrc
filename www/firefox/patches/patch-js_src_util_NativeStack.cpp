7b54652e279731af2dc1619db6ce291b849d4894
ryoon <ryoon@pkgsrc.org>
2019-03-19 16:11:27 +0000

    Update to 66.0
    Changelog:
    New
        Firefox now prevents websites from automatically playing sound. You can add individual sites to an exceptions list or turn blocking off. To learn more about block autoplay, which will be rolled out gradually to all users, visit the Mozilla blog.
    
        Improved search experience:
            Find a specific webpage faster when you have a lot of tabs open: You can now search within all of your open tabs from the tab overflow menu
            Easier search via a redesigned new tab in Private Windows
    
        Smoother scrolling: Scroll anchoring keeps content from jumping as images and ads load at the top of the page
    
        Improved performance and better user experience for extensions:
            Extensions now store their settings in a Firefox database, rather than individual JSON files, making every site you visit faster
            A redesigned keyboard shortcuts section in about:addons makes it easier to view and adjust default shortcuts
    
        Redesigned certificate error pages help you better understand and resolve issues, including identification of certificate issuers for anti-virus software
    
        Added basic support for macOS Touch Bar
    
        Experimenting with an improved Pocket experience in New Tab with different layouts and more topical content
    
        Improved performance and reduced crash rates by [doubling web content loading processes from 4 to 8 [1]
    
        Easier, passwordless security: Added support for Windows Hello on Windows 10, allowing you to use your face, fingerprint, or external security keys for website authentication
    
    Fixed
        The Dark and Light Firefox themes now override the system setting for title bar accent color on Windows 10
    
        Linux users: Resolved an issue that caused Firefox to freeze when downloading files
    
        Various security fixes
    
    Changed
    
        System title bar is hidden by default to match Gnome guideline for Linux users
    
    Developer
    
        DevTools Inspector is now fully usable when the Debugger is paused
    
        Lowered priority of setTimeout and setInterval during page load to improve overall page load performance
    
        Fixed: <button> element is no longer special cased in event dispatch, per latest specifications
    
    Security fixes:
    Not available yet.

--- www/firefox/patches/patch-js_src_util_NativeStack.cpp
+++ www/firefox/patches/patch-js_src_util_NativeStack.cpp
@@ -1,18 +1,18 @@
-$NetBSD: patch-js_src_util_NativeStack.cpp,v 1.4 2019/01/29 16:28:22 ryoon Exp $
+$NetBSD: patch-js_src_util_NativeStack.cpp,v 1.5 2019/03/19 16:11:28 ryoon Exp $
 
 Support SunOS.
 
---- js/src/util/NativeStack.cpp.orig	2019-01-18 00:20:37.000000000 +0000
+--- js/src/util/NativeStack.cpp.orig	2019-03-07 16:53:39.000000000 +0000
 +++ js/src/util/NativeStack.cpp
 @@ -13,7 +13,7 @@
- #if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
- #include <pthread_np.h>
- #endif
--#if defined(SOLARIS) || defined(AIX)
-+#if defined(__sun) || defined(AIX)
- #include <ucontext.h>
- #endif
- #if defined(ANDROID) && !defined(__aarch64__)
+ #  if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+ #    include <pthread_np.h>
+ #  endif
+-#  if defined(SOLARIS) || defined(AIX)
++#  if defined(__sun) || defined(AIX)
+ #    include <ucontext.h>
+ #  endif
+ #  if defined(ANDROID) && !defined(__aarch64__)
 @@ -40,7 +40,7 @@ void* js::GetNativeStackBaseImpl() {
    return static_cast<void*>(pTib->StackBase);
  }
@@ -23,10 +23,10 @@ Support SunOS.
  JS_STATIC_ASSERT(JS_STACK_GROWTH_DIRECTION < 0);
  
 @@ -128,6 +128,7 @@ void* js::GetNativeStackBaseImpl() {
- #elif defined(PTHREAD_NP_H) || defined(_PTHREAD_NP_H_) || defined(NETBSD)
+ #    elif defined(PTHREAD_NP_H) || defined(_PTHREAD_NP_H_) || defined(NETBSD)
    /* e.g. on FreeBSD 4.8 or newer, neundorf@kde.org */
    pthread_attr_get_np(thread, &sattr);
-+#elif defined(__sun)
- #else
++#    elif defined(__sun)
+ #    else
    /*
     * FIXME: this function is non-portable;
