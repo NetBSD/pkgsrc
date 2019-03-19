47945ae00ccb33601a19293b6e5c96d8385a4c2a
jaapb <jaapb@pkgsrc.org>
2019-03-19 10:48:15 +0000

    Added devel/ocaml-async_unix, unix hooks for ocaml-async

---
+++ devel/ocaml-async_unix/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/03/19 10:48:15 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-async_unix
+
+.if !defined(OCAML_ASYNC_UNIX_BUILDLINK3_MK)
+OCAML_ASYNC_UNIX_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-async_unix+=	ocaml-async_unix>=0.12.0
+BUILDLINK_PKGSRCDIR.ocaml-async_unix?=	../../devel/ocaml-async_unix
+
+.endif	# OCAML_ASYNC_UNIX_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-async_unix
