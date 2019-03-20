40b307f9d81886c21795ec745e4089fb66cb6daf
jperkin <jperkin@pkgsrc.org>
2019-03-20 16:02:10 +0000

    libtelnet: SunOS/C99 build fixes.

--- net/libtelnet/patches/patch-util_telnet-client.c
+++ net/libtelnet/patches/patch-util_telnet-client.c
@@ -1,22 +1,27 @@
-$NetBSD: patch-util_telnet-client.c,v 1.2 2016/04/30 20:18:38 joerg Exp $
+$NetBSD: patch-util_telnet-client.c,v 1.3 2019/03/20 16:02:10 jperkin Exp $
 
 Provide workaround for a non-POSIX cfmakeraw call.
 Uses getaddrinfo.
 
 --- util/telnet-client.c.orig	2012-03-18 21:07:49.000000000 +0000
 +++ util/telnet-client.c
-@@ -9,6 +9,10 @@
+@@ -9,9 +9,15 @@
   * all present and future rights to this code under copyright law. 
   */
  
 +#ifdef __NetBSD__
 +#define _NETBSD_SOURCE
 +#endif
++#if !defined(__sun) || __STDC_VERSION__-0 < 199901L
 +#define _XOPEN_SOURCE 520
  #if !defined(_POSIX_SOURCE)
  #	define _POSIX_SOURCE
  #endif
-@@ -199,7 +203,15 @@ int main(int argc, char **argv) {
++#endif
+ #if !defined(_BSD_SOURCE)
+ #	define _BSD_SOURCE
+ #endif
+@@ -199,7 +205,15 @@ int main(int argc, char **argv) {
  	tcgetattr(STDOUT_FILENO, &orig_tios);
  	atexit(_cleanup);
  	tios = orig_tios;
