630ea75f1da7b4e3fc86629dd2bd1509b9d01c74
wiz <wiz@pkgsrc.org>
2019-03-20 12:13:21 +0000

    ecore: remove libXp support.
    Bump PKGREVISION, and recursively bump all affected packages.
    
    Untested since eet, an ecore dependency, does not build for me.

--- graphics/edje/buildlink3.mk
+++ graphics/edje/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2018/12/13 19:51:34 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2019/03/20 12:13:21 wiz Exp $
 
 BUILDLINK_TREE+=	edje
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	edje
 EDJE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.edje+=	edje>=1.7.7
-BUILDLINK_ABI_DEPENDS.edje?=	edje>=1.7.7nb15
+BUILDLINK_ABI_DEPENDS.edje?=	edje>=1.7.7nb16
 BUILDLINK_PKGSRCDIR.edje?=	../../graphics/edje
 
 .include	"../../devel/ecore/buildlink3.mk"
