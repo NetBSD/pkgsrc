d28f45892595554c72ef9df3e98961251f985ebf
jperkin <jperkin@pkgsrc.org>
2019-03-20 16:02:10 +0000

    libtelnet: SunOS/C99 build fixes.

--- net/libtelnet/patches/patch-util_telnet-chatd.c
+++ net/libtelnet/patches/patch-util_telnet-chatd.c
@@ -1,14 +1,19 @@
-$NetBSD: patch-util_telnet-chatd.c,v 1.1 2016/04/30 20:18:38 joerg Exp $
+$NetBSD: patch-util_telnet-chatd.c,v 1.2 2019/03/20 16:02:10 jperkin Exp $
 
 Uses strdup.
 
---- util/telnet-chatd.c.orig	2016-04-27 08:59:27.691493917 +0000
+--- util/telnet-chatd.c.orig	2012-03-18 21:07:49.000000000 +0000
 +++ util/telnet-chatd.c
-@@ -10,6 +10,7 @@
+@@ -10,9 +10,12 @@
   */
  
  #if !defined(_WIN32)
++#  if !defined(__sun) || __STDC_VERSION__-0 < 199901L
 +#	define _XOPEN_SOURCE 520
  #	if !defined(_POSIX_SOURCE)
  #		define _POSIX_SOURCE
+ #	endif
++#  endif
+ #	if !defined(_BSD_SOURCE)
+ #		define _BSD_SOURCE
  #	endif
