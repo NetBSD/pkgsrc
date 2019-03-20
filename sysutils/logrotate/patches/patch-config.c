f02f919897d8e9f8d1dca43b0eca7f5c85168099
he <he@pkgsrc.org>
2019-03-20 10:34:10 +0000

    Update logrotate to version 3.15.0.
    Pkgsrc changes:
     * Track rename of logrotate-default to logrotate.conf
     * Add a fix so that the log file name is actually passed
       to the various script hooks(!)
       https://github.com/logrotate/logrotate/pull/245
    
    Upstream changes:
    
    3.15.0
    ======
     * timer unit: change trigger fuzz from 12h to 1h (#230)
     * service unit: only run if /var/log is mounted (#230)
     * preserve fractional part of timestamps when compressing (#226)
     * re-indent source code using spaces only (#188)
     * minage: avoid rounding issue while comparing the amount of seconds (#36)
     * never remove old log files if rotate -1 is specified (#202)
     * return non-zero exit status if a config file contains an error (#199)
     * make copytruncate work with rotate 0 (#191)
     * warn user if both size and the time interval options are used (#192)
     * pass rotated log file name as the 2nd argument of the postrotate
       script when sharedscript is not enabled (#193)
     * rename logrotate-default to logrotate.conf (#187)
    
    3.14.0
    ======
     * make configure show support status for SELinux and ACL at the end (#179)
     * make logrotate build again on FreeBSD (#178)
     * move wtmp and btmp definitions from logrotate.conf to
     * separate configuration files in logrotate.d (#168)
     * print a warning about logrotate doing nothing when -d is used (#165)
     * do not reject executable config files (#166)
     * add hardening options to logrotate.service in examples (#143)
     * fix spurious compressor failure when using su and compress (#169)
     * keep logrotate version in .tarball-version in release tarballs (#156)
     * introduce the hourago configuration directive (#159)
     * ignore empty patterns in tabooext to avoid exclusion of everything (#160)
     * properly report skipped test cases instead of pretending success
    
    3.13.0
    ======
     * make distribution tarballs report logrotate version properly (RHBZ#1500264)
     * make (un)compress work even if stdin and/or stdout are closed (#154)
     * remove -s from DEFAULT_MAIL_COMMAND and improve its documenation (#152)
     * uncompress logs before mailing them even if delaycompress is enabled (#151)
     * handle unlink of a non-existing log file as a warning only (#144)
     * include compile-time options in the output of logrotate --version (#145)
     * make logrotate --version print to stdout instead of stderr (#145)
     * flush write buffers before syncing state file (#148)
     * specify (un)compress utility explicitly in tests (#137)
     * enable running tests in parallel (#132)
     * explicitly map root UID/GID to 0 on Cygwin (#133)
     * add .dpkg-bak and .dpkg-del to default tabooext list (#134)
    
    3.12.3
    ======
     * copy and copytruncate directives now work together again
     * unlink() is no longer preceded by open() unless shred is enabled (#124)
     * compress and uncompress now take commands from $PATH, too (#122)
    
    3.12.2
    ======
     * build fixes related to -Werror (#119) and -Werror=format= (#108)
     * configure --enable-werror now controls use of the -Werror flag (#123)
    
    3.12.1
    ======
     * Included forgotten build-aux directory in release tarballs.
    
    3.12.0
    ======
     * Fixed accident removal of rotated files with dateext. (#118)
     * Line comments inside globs in config files are now skipped. (#109)
     * logrotate now recovers from a corrupted state file. (#45)
     * Makefile.legacy has been removed. (#103)
     * config.h is now generated by autotools. (#102 and #103)
     * createolddir now creates old directory as unprivileged user. (#114)
     * weekly rotations are now predictable and configurable. (#93)
     * Errors in config files are no longer treated as fatal errors. (#81)
     * configure --with-default-mail-command specifies default mail command. (#100)
     * Fixed heap buffer overflow when parsing crafted config file. (#33)

--- sysutils/logrotate/patches/patch-config.c
+++ sysutils/logrotate/patches/patch-config.c
@@ -1,16 +1,17 @@
-$NetBSD: patch-config.c,v 1.5 2017/01/26 20:12:32 nils Exp $
+$NetBSD: patch-config.c,v 1.6 2019/03/20 10:34:10 he Exp $
 
 Fix NetBSD compilation.
 
 --- config.c.orig	2016-12-02 13:12:33.000000000 +0000
 +++ config.c
-@@ -3,6 +3,9 @@
- #ifndef __NetBSD__
- #include <alloca.h>
- #endif
+@@ -23,7 +23,9 @@
+ #include <wctype.h>
+ #include <fnmatch.h>
+ #include <sys/mman.h>
+-#include <libgen.h>
 +#ifndef _LIBGEN_H_
 +#  include <libgen.h>
 +#endif
- #include <limits.h>
- #include <ctype.h>
- #include <dirent.h>
+ 
+ #if !defined(PATH_MAX) && defined(__FreeBSD__)
+ #include <sys/param.h>
