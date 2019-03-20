25b4085ee0b0851ede5aaa897a6f8279acb129ab
he <he@pkgsrc.org>
2019-03-20 10:34:10 +0000

    Update logrotate to version 3.15.0.
    Pkgsrc changes:
     * Track rename of logrotate-default to logrotate.conf
     * Add a fix so that the log file name is actually passed
       to the various script hooks(!)
       https://github.com/logrotate/logrotate/pull/245
    
    Upstream changes:
    
    3.15.0
    ======
     * timer unit: change trigger fuzz from 12h to 1h (#230)
     * service unit: only run if /var/log is mounted (#230)
     * preserve fractional part of timestamps when compressing (#226)
     * re-indent source code using spaces only (#188)
     * minage: avoid rounding issue while comparing the amount of seconds (#36)
     * never remove old log files if rotate -1 is specified (#202)
     * return non-zero exit status if a config file contains an error (#199)
     * make copytruncate work with rotate 0 (#191)
     * warn user if both size and the time interval options are used (#192)
     * pass rotated log file name as the 2nd argument of the postrotate
       script when sharedscript is not enabled (#193)
     * rename logrotate-default to logrotate.conf (#187)
    
    3.14.0
    ======
     * make configure show support status for SELinux and ACL at the end (#179)
     * make logrotate build again on FreeBSD (#178)
     * move wtmp and btmp definitions from logrotate.conf to
     * separate configuration files in logrotate.d (#168)
     * print a warning about logrotate doing nothing when -d is used (#165)
     * do not reject executable config files (#166)
     * add hardening options to logrotate.service in examples (#143)
     * fix spurious compressor failure when using su and compress (#169)
     * keep logrotate version in .tarball-version in release tarballs (#156)
     * introduce the hourago configuration directive (#159)
     * ignore empty patterns in tabooext to avoid exclusion of everything (#160)
     * properly report skipped test cases instead of pretending success
    
    3.13.0
    ======
     * make distribution tarballs report logrotate version properly (RHBZ#1500264)
     * make (un)compress work even if stdin and/or stdout are closed (#154)
     * remove -s from DEFAULT_MAIL_COMMAND and improve its documenation (#152)
     * uncompress logs before mailing them even if delaycompress is enabled (#151)
     * handle unlink of a non-existing log file as a warning only (#144)
     * include compile-time options in the output of logrotate --version (#145)
     * make logrotate --version print to stdout instead of stderr (#145)
     * flush write buffers before syncing state file (#148)
     * specify (un)compress utility explicitly in tests (#137)
     * enable running tests in parallel (#132)
     * explicitly map root UID/GID to 0 on Cygwin (#133)
     * add .dpkg-bak and .dpkg-del to default tabooext list (#134)
    
    3.12.3
    ======
     * copy and copytruncate directives now work together again
     * unlink() is no longer preceded by open() unless shred is enabled (#124)
     * compress and uncompress now take commands from $PATH, too (#122)
    
    3.12.2
    ======
     * build fixes related to -Werror (#119) and -Werror=format= (#108)
     * configure --enable-werror now controls use of the -Werror flag (#123)
    
    3.12.1
    ======
     * Included forgotten build-aux directory in release tarballs.
    
    3.12.0
    ======
     * Fixed accident removal of rotated files with dateext. (#118)
     * Line comments inside globs in config files are now skipped. (#109)
     * logrotate now recovers from a corrupted state file. (#45)
     * Makefile.legacy has been removed. (#103)
     * config.h is now generated by autotools. (#102 and #103)
     * createolddir now creates old directory as unprivileged user. (#114)
     * weekly rotations are now predictable and configurable. (#93)
     * Errors in config files are no longer treated as fatal errors. (#81)
     * configure --with-default-mail-command specifies default mail command. (#100)
     * Fixed heap buffer overflow when parsing crafted config file. (#33)

--- sysutils/logrotate/patches/patch-logrotate.c
+++ sysutils/logrotate/patches/patch-logrotate.c
@@ -1,25 +1,19 @@
-$NetBSD: patch-logrotate.c,v 1.3 2017/03/06 12:59:53 fhajny Exp $
+$NetBSD: patch-logrotate.c,v 1.4 2019/03/20 10:34:10 he Exp $
 
-Fixes GCC error:
-"error: missing sentinel in function call [-Werror=format=]"
+Fix the running of scripts:
+ * the "-c" option to "sh" causes subsequent args to not be passed
+   to the script (oops!)
+ * the "logrotate_script" arg ends up as arg1, not arg0 of the script,
+   and is not documented, so get rid of it
 
---- logrotate.c.orig	2016-11-30 13:07:15.000000000 +0000
+--- logrotate.c.orig	2018-10-05 15:01:21.000000000 +0000
 +++ logrotate.c
-@@ -440,7 +440,7 @@ static int runScript(struct logInfo *log
- 				DOEXIT(1);
- 			}
- 		}
--		execl("/bin/sh", "sh", "-c", script, "logrotate_script", logfn, NULL);
-+		execl("/bin/sh", "sh", "-c", script, "logrotate_script", logfn, (char *)NULL);
- 		DOEXIT(1);
- 	}
- 
-@@ -800,7 +800,7 @@ static int mailLog(struct logInfo *log,
- 				DOEXIT(1);
- 			}
- 
--			execlp(uncompressCommand, uncompressCommand, NULL);
-+			execlp(uncompressCommand, uncompressCommand, (char *)NULL);
- 			DOEXIT(1);
- 		}
+@@ -451,7 +451,7 @@ static int runScript(struct logInfo *log
+                 exit(1);
+             }
+         }
+-        execl("/bin/sh", "sh", "-c", script, "logrotate_script", logfn, logrotfn, (char *) NULL);
++        execl("/bin/sh", "sh", script, logfn, logrotfn, (char *) NULL);
+         exit(1);
+     }
  
