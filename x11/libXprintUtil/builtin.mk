50a6fca81e310b9f8168194865919852e45685bb
wiz <wiz@pkgsrc.org>
2019-03-20 12:18:47 +0000

    *: remove libXprint packages
    Long unsupported upstream.

--- x11/libXprintUtil/builtin.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: builtin.mk,v 1.3 2014/03/10 11:05:52 jperkin Exp $
-
-BUILTIN_PKG:=	libXprintUtil
-PKGCONFIG_FILE.libXprintUtil=	${X11BASE}/lib/pkgconfig/xprintutil.pc
-PKGCONFIG_FILE.libXprintUtil+=	${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/xprintutil.pc
-
-.include "../../mk/buildlink3/pkgconfig-builtin.mk"
-.include "../../mk/x11.builtin.mk"
