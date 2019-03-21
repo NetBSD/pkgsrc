29145470eb037272595ae3fbcf7131f2c49078bb
fox <fox@pkgsrc.org>
2019-03-21 10:16:40 +0000

    fasttracker2: update to b142
    Changes since b141:
    
    Beta #142 - 18.03.2019
    - XM loader: fixed a bug where XMs with stereo samples would display a
      warning message box for every stereo sample loaded, instead of just one.

--- audio/fasttracker2/patches/patch-src_ft2__diskop.c
+++ audio/fasttracker2/patches/patch-src_ft2__diskop.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-src_ft2__diskop.c,v 1.1 2018/12/01 18:20:09 fox Exp $
+$NetBSD: patch-src_ft2__diskop.c,v 1.2 2019/03/21 10:16:40 fox Exp $
 
 Added <sys/types.h> / <sys/stat.h> to prevent "unknown type name"
 (dev_t, ino_t and nlink_t) error from the included <fts.h>.
 
---- src/ft2_diskop.c.orig	2018-11-23 13:36:02.000000000 +0000
+--- src/ft2_diskop.c.orig	2019-03-18 16:59:48.000000000 +0000
 +++ src/ft2_diskop.c
 @@ -12,6 +12,8 @@
  #include <windows.h>
@@ -11,6 +11,6 @@ Added <sys/types.h> / <sys/stat.h> to prevent "unknown type name"
  #else
 +#include <sys/types.h>
 +#include <sys/stat.h>
- #include <fts.h> /* for fts_open() and stuff in recursiveDelete() */
+ #include <fts.h> // for fts_open() and stuff in recursiveDelete()
  #include <unistd.h>
  #include <dirent.h>
