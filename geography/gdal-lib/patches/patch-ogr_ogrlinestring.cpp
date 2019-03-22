d7f5bf2da49bf9833dfd9ce03a8720f298a1e290
gdt <gdt@pkgsrc.org>
2019-03-22 15:31:11 +0000

    geography/gdal-lib: Replace std::fabs workaround with fix from upstream

--- geography/gdal-lib/patches/patch-ogr_ogrlinestring.cpp
+++ geography/gdal-lib/patches/patch-ogr_ogrlinestring.cpp
@@ -1,15 +1,14 @@
-$NetBSD: patch-ogr_ogrlinestring.cpp,v 1.1 2019/03/22 14:00:49 gdt Exp $
+$NetBSD: patch-ogr_ogrlinestring.cpp,v 1.2 2019/03/22 15:31:11 gdt Exp $
 
-fabs is not in std.   Sent upstream via mail
+Include required header for std::fabs.  From upstream.
 
 --- ogr/ogrlinestring.cpp.orig	2019-03-15 12:35:19.000000000 +0000
 +++ ogr/ogrlinestring.cpp
-@@ -2551,7 +2551,7 @@ void OGRSimpleCurve::segmentize( double 
-         const double dfX = paoPoints[i+1].x - paoPoints[i].x;
-         const double dfY = paoPoints[i+1].y - paoPoints[i].y;
-         const double dfSquareDist = dfX * dfX + dfY * dfY;
--        if( std::fabs(dfSquareDist - dfSquareMaxLength) > 1e-5 * dfSquareMaxLength )
-+        if( fabs(dfSquareDist - dfSquareMaxLength) > 1e-5 * dfSquareMaxLength )
-         {
-             const double dfIntermediatePoints =
-                 floor(sqrt(dfSquareDist / dfSquareMaxLength) - 1e-2);
+@@ -31,6 +31,7 @@
+ #include "ogr_geos.h"
+ #include "ogr_p.h"
+ 
++#include <cmath>
+ #include <cstdlib>
+ #include <algorithm>
+ #include <limits>
