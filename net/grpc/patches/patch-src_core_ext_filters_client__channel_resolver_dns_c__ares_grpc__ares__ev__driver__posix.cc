f5ad61256b5caa75aed279fc718afd0b1dda5c39
adam <adam@pkgsrc.org>
2019-03-22 08:25:51 +0000

    grpc: updated to 1.19.1
    Release v1.19.1
    Core
    Backport 18201 to fix "symbol not found: _ares_library_init" error in C++ Bazel build on MacOS.
    
    Python
    Backport "Add the missing grpc_cfstream dependency" to v1.19.x.
    
    
    Release v1.19.0
    Core
    Fix c-ares on Windows "DNS resolution failure" triggered by logging.
    Disable c-ares on Android.
    Ignore reserved bit in WINDOW_UPDATE frame.
    Set c-ares as the default resolver.
    Add period at end of metadata.google.internal to prevent unnecessary DNS lookups.
    Decrease verbosity of ALTS platform check to avoid a spam log message.
    Fix windows localhost address sorting bug.
    Re-enable c-ares as the default resolver; but don't turn on SRV queries.
    Remove filters from subchannel args.
    
    C++
    Register for cq avalanching when interceptors are going to be run.
    Add a caching interceptor to the keyvaluestore example.
    Enable per-channel subchannel pool.
    Fix build with bazel 0.21.
    Switch the default DNS resolver from native to c-ares.
    Modifying semantics for GetSendMessage and GetSerializedSendMessage. Also adding ModifySendMessage.
    Add interceptor methods to fail recv msg for hijacked rpcs and set recv message to nullptr on failure.
    Add interceptor method to fail hijacked send messages and get status on POST_SEND_MESSAGE.
    New Experimental Interception API - GetSendMessage and GetSerializedSendMessage.
    
    C#
    Upgrade System.Interactive.Async to 3.2.0.
    Refactor ServerServiceDefinition and move it to Grpc.Core.Api nuget.
    Allow passing null implementation to generated BindService overload using ServiceBinderBase.
    Move public types needed for server implementation to Grpc.Core.Api.
    
    Objective-C
    Disable c-ares on iOS.
    Added support for tvOS.
    Fixing a few thread safety issues in gRPC Objective-C library.
    Rolling out new API for gRPC Objective-C library.
    
    Python
    grpc_prefork(): check grpc_is_initialized before creating execctx.
    [gRPC] Enable Python 3 for Bazel to Run Tests.
    Escalate the failure of protoc execution.
    Remove dependency of grpc.framework.foundation.callable_util.
    
    Ruby
    Disable service config resolution with c-ares by default, for now.
    Ruby: refactor init/shutdown logic to avoid using atexit; fix windows.
    Ruby tooling: respect user toolchain overrides.

--- net/grpc/patches/patch-src_core_ext_filters_client__channel_resolver_dns_c__ares_grpc__ares__ev__driver__posix.cc
+++ net/grpc/patches/patch-src_core_ext_filters_client__channel_resolver_dns_c__ares_grpc__ares__ev__driver__posix.cc
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_core_ext_filters_client__channel_resolver_dns_c__ares_grpc__ares__ev__driver__posix.cc,v 1.1 2018/05/15 15:20:18 jperkin Exp $
+$NetBSD: patch-src_core_ext_filters_client__channel_resolver_dns_c__ares_grpc__ares__ev__driver__posix.cc,v 1.2 2019/03/22 08:25:51 adam Exp $
 
 Add SunOS support.
 
---- src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_ev_driver_posix.cc.orig	2018-04-13 18:08:11.000000000 +0000
+--- src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_ev_driver_posix.cc.orig	2019-03-08 20:07:26.000000000 +0000
 +++ src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_ev_driver_posix.cc
-@@ -22,6 +22,11 @@
- 
+@@ -23,6 +23,11 @@
  #include <ares.h>
+ #include <string.h>
  #include <sys/ioctl.h>
 +#ifdef __sun
 +#include <sys/filio.h>
