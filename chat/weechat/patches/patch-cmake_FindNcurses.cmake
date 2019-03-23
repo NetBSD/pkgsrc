1bf09b7efb5ea1d43f88ef8c35812bb5a7ca3c63
nia <nia@pkgsrc.org>
2019-03-23 14:54:13 +0000

    chat/weechat: Search for libcurses when attempting to find a value for NCURSES_LIBRARY. Seems to help problems when configuring against netbsd curses.

--- chat/weechat/patches/patch-cmake_FindNcurses.cmake
+++ chat/weechat/patches/patch-cmake_FindNcurses.cmake
@@ -1,8 +1,8 @@
-$NetBSD: patch-cmake_FindNcurses.cmake,v 1.2 2018/07/23 22:57:22 maya Exp $
+$NetBSD: patch-cmake_FindNcurses.cmake,v 1.3 2019/03/23 14:54:13 nia Exp $
 
 Don't try to look for ncurses.h, so FAKE_NCURSES works for netbsd curses.
 
---- cmake/FindNcurses.cmake.orig	2018-03-18 06:41:40.000000000 +0000
+--- cmake/FindNcurses.cmake.orig	2019-02-17 07:20:07.000000000 +0000
 +++ cmake/FindNcurses.cmake
 @@ -22,7 +22,7 @@ if(NCURSES_FOUND)
  endif()
@@ -13,3 +13,12 @@ Don't try to look for ncurses.h, so FAKE_NCURSES works for netbsd curses.
    PATHS /usr/include/ncursesw /usr/include/ncurses /usr/include
    /usr/local/include/ncursesw /usr/local/include/ncurses /usr/local/include
    /usr/pkg/include/ncursesw /usr/pkg/include/ncurses /usr/pkg/include
+@@ -42,7 +42,7 @@ if(NCURSESW_LIBRARY)
+   set(NCURSES_LIBRARY ${NCURSESW_LIBRARY})
+ else()
+   find_library(NCURSES_LIBRARY
+-    NAMES ncurses
++    NAMES curses ncurses
+     PATHS /lib /usr/lib /usr/local/lib /usr/pkg/lib
+   )
+   find_package(PkgConfig QUIET)
