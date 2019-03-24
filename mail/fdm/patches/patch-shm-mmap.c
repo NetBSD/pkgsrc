0bec18f4225ed12745d9abda0c7c6afddd00c78e
leot <leot@pkgsrc.org>
2019-03-24 14:41:05 +0000

    fdm: Add a comment about patch-shm-mmap.c

--- mail/fdm/patches/patch-shm-mmap.c
+++ mail/fdm/patches/patch-shm-mmap.c
@@ -1,6 +1,15 @@
-$NetBSD: patch-shm-mmap.c,v 1.1 2019/03/18 10:53:41 leot Exp $
+$NetBSD: patch-shm-mmap.c,v 1.2 2019/03/24 14:41:05 leot Exp $
 
 Adjust mremap(2) usage for NetBSD.
+
+Patch shared upstream via:
+
+ <https://github.com/nicm/fdm/pull/59>
+
+...but rejected.
+
+configure check was changed to only check and enable mremap(2) for
+platforms that also define MREMAP_MAYMOVE.
 
 --- shm-mmap.c.orig	2019-02-12 22:08:26.000000000 +0000
 +++ shm-mmap.c
