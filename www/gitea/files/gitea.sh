52c34bc73f049c1b987359309071961e33d02d93
nia <nia@pkgsrc.org>
2019-03-24 21:34:23 +0000

    www/gitea: Add version 1.7.4
    Mostly based on work by tuxillo in pkgsrc-wip with some changes and additions
    by me.
    
    Gitea is a community managed fork of Gogs, lightweight code hosting solution
    written in Go and published under the MIT license.

---
+++ www/gitea/files/gitea.sh
@@ -0,0 +1,25 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: gitea.sh,v 1.1 2019/03/24 21:34:23 nia Exp $
+#
+# PROVIDE: gitea
+# REQUIRE: DAEMON
+
+. /etc/rc.subr
+
+name="gitea"
+rcvar=${name}
+required_files="@PKG_SYSCONFDIR@/conf/app.ini"
+command="@PREFIX@/sbin/gitea"
+command_args="--config @PKG_SYSCONFDIR@/app.ini web &"
+
+gitea_env="GITEA_WORK_DIR=@GITEA_SHARE_DIR@"
+gitea_env="${gitea_env} GITEA_CUSTOM=@PKG_SYSCONFDIR@"
+gitea_env="${gitea_env} HOME=@GITEA_USER_HOME@"
+gitea_env="${gitea_env} USER=@GITEA_USER@"
+
+gitea_user="@GITEA_USER@"
+gitea_group="@GITEA_GROUP@"
+
+load_rc_config $name
+run_rc_command "$1"
