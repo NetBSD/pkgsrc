54ad6c893ec0b54b4ebeabe40b053639e8ae9f73
sno <sno@pkgsrc.org>
2019-03-24 09:18:53 +0000

    Updating package for Log4CPlus in devel/log4cplus from 2.0.2 to 2.0.3
    Because of the fixed new API changes, bump the API version in buildlink3.mk
    to 2.0.3 - since 1.x API will likely disappear (it's already deprecated)
    
    # log4cplus 2.0.3
    
      - Fix compilation on systems without `O_CLOEXEC`. This affects, e.g.,
        Solaris 10. GitHub issue #326. (Patch by Jiangrz).
    
      - Provide `log4cplus::deinitialize()` to make the API symmetric
        (`log4cplus::initialize()` already exists) and to allow
        de-/initialization without `log4cplus::Initializer` instance. GitHub
        issue #340.
    
      - Deal with `register` keyword being generated in SWIG based bindings. The
        keyword is unused and reserved in C++17. Remove use of
        `std::unary_function`, it was removed in C++17.
    
      - Add ability to define new log levels using C API. Add reconfiguration
        API. (Jens Rehsack)
    
      - Add `NDCMatchFilter` and `MDCMatchFilter`. (Franck)

--- devel/log4cplus/buildlink3.mk
+++ devel/log4cplus/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/01/07 13:04:07 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2019/03/24 09:18:53 sno Exp $
 
 BUILDLINK_TREE+=	log4cplus
 
 .if !defined(LOG4CPLUS_BUILDLINK3_MK)
 LOG4CPLUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.log4cplus+=	log4cplus>=1.1.0
+BUILDLINK_API_DEPENDS.log4cplus+=	log4cplus>=2.0.3
 BUILDLINK_PKGSRCDIR.log4cplus?=		../../devel/log4cplus
 .endif	# LOG4CPLUS_BUILDLINK3_MK
 
