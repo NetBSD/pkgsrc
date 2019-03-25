78dab66649ed7c66981bd355d6aa24af48b0545d
wiz <wiz@pkgsrc.org>
2019-03-25 14:20:37 +0000

    openal-soft: update to 1.19.1.
    openal-soft-1.19.1:
    
        Implemented capture support for the SoundIO backend.
    
        Fixed source buffer queues potentially not playing properly when a queue
        entry completes.
    
        Fixed possible unexpected failures when generating auxiliary effect slots.
    
        Fixed a crash with certain reverb or device settings.
    
        Fixed OpenSL capture.
    
        Improved output limiter response, better ensuring the sample amplitude is
        clamped for output.
    
    openal-soft-1.19.0:
    
        Implemented the ALC_SOFT_device_clock extension.
    
        Implemented the Pitch Shifter, Frequency Shifter, and Autowah effects.
    
        Fixed compiling on FreeBSD systems that use freebsd-lib 9.1.
    
        Fixed compiling on NetBSD.
    
        Fixed the reverb effect's density scale and panning parameters.
    
        Fixed use of the WASAPI backend with certain games, which caused odd COM
        initialization errors.
    
        Increased the number of virtual channels for decoding Ambisonics to HRTF
        output.
    
        Changed 32-bit x86 builds to use SSE2 math by default for performance.
        Build-time options are available to use just SSE1 or x87 instead.
    
        Replaced the 4-point Sinc resampler with a more efficient cubic resampler.
    
        Renamed the MMDevAPI backend to WASAPI.
    
        Added support for 24-bit, dual-ear HRTF data sets. The built-in data set
        has been updated to 24-bit.
    
        Added a 24- to 48-point band-limited Sinc resampler.
    
        Added an SDL2 playback backend. Disabled by default to avoid a dependency
        on SDL2.
    
        Improved the performance and quality of the Chorus and Flanger effects.
    
        Improved the efficiency of the band-limited Sinc resampler.
    
        Improved the Sinc resampler's transition band to avoid over-attenuating
        higher frequencies.
    
        Improved the performance of some filter operations.
    
        Improved the efficiency of object ID lookups.
    
        Improved the efficienty of internal voice/source synchronization.
    
        Improved AL call error logging with contextualized messages.
    
        Removed the reverb effect's modulation stage. Due to the lack of reference
        for its intended behavior and strength.

--- audio/openal-soft/patches/patch-Alc_alconfig.c
+++ audio/openal-soft/patches/patch-Alc_alconfig.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-alc_alcconfig.c,v 1.2 2017/09/13 08:08:22 wiz Exp $
+$NetBSD: patch-Alc_alconfig.c,v 1.1 2019/03/25 14:20:37 wiz Exp $
 
 Allow the Makefile to specify the default config file path.
 
---- Alc/alcConfig.c.orig	2017-07-30 05:09:21.000000000 +0000
-+++ Alc/alcConfig.c
-@@ -424,7 +424,7 @@ void ReadALConfig(void)
-     al_string ppath;
+--- Alc/alconfig.c.orig	2018-10-11 22:05:31.000000000 +0000
++++ Alc/alconfig.c
+@@ -428,7 +428,7 @@ void ReadALConfig(void)
+     const char *str;
      FILE *f;
  
 -    str = "/etc/openal/alsoft.conf";
