e7ffb9782ae2c5e6c30094f059df9af6605f0d18
jaapb <jaapb@pkgsrc.org>
2019-03-25 09:19:32 +0000

    Added net/ocaml-protocol_version_header, protocol-aware version negotation

---
+++ net/ocaml-protocol_version_header/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/03/25 09:19:32 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-protocol_version_header
+
+.if !defined(OCAML_PROTOCOL_VERSION_HEADER_BUILDLINK3_MK)
+OCAML_PROTOCOL_VERSION_HEADER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-protocol_version_header+=	ocaml-protocol_version_header>=0.12.0
+BUILDLINK_PKGSRCDIR.ocaml-protocol_version_header?=	../../net/ocaml-protocol_version_header
+
+.endif	# OCAML_PROTOCOL_VERSION_HEADER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-protocol_version_header
