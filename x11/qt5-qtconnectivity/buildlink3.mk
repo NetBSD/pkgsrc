34e2a36295b617caf1aaa44f81dcbced9cc3ef8d
adam <adam@pkgsrc.org>
2019-03-26 11:14:16 +0000

    qt5: updated to 5.12.2
    5.12.2:
    While not adding new features, the Qt 5.12.2 release provides a number of bug
    fixes and other improvements.
    
    Compared to Qt 5.12.1, the new Qt 5.12.2 contains more than 250 bug fixes. For
    details of the most important changes, please check the Change files of Qt
    5.12.2.
    
    With Qt 5.12.2 we bring back widely asked MinGW 32 bit prebuild binaries in
    addition to 64 bit ones.
    
    Qt 5.12 LTS will receive many more patch releases throughout the coming years
    and we recommend all active developed projects to migrate to Qt 5.12 LTS. Qt
    5.9 LTS is currently in ‘Strict’ phase and receives only the selected important
    bug and security fixes, while Qt 5.12 LTS is currently receiving all the bug
    fixes. With Qt 5.6 Support ending in March 2019 all active projects still using
    Qt 5.6 LTS should now migrate to a later version of Qt.

---
+++ x11/qt5-qtconnectivity/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/03/26 11:14:17 adam Exp $
+
+BUILDLINK_TREE+=	qt5-qtconnectivity
+
+.if !defined(QT5_QTCONNECTIVITY_BUILDLINK3_MK)
+QT5_QTCONNECTIVITY_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt5-qtconnectivity+=	qt5-qtconnectivity>=5.12.2
+BUILDLINK_PKGSRCDIR.qt5-qtconnectivity?=	../../x11/qt5-qtconnectivity
+
+.include "../../x11/qt5-qtdeclarative/buildlink3.mk"
+.endif	# QT5_QTCONNECTIVITY_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt5-qtconnectivity
