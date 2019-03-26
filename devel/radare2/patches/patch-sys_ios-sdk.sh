e320e4af811746ebebd7ab29bf8670c0c4904211
ryoon <ryoon@pkgsrc.org>
2019-03-26 19:29:52 +0000

    Update to 3.3.0
    Changelog:
    Many improvements and bugfixes including:
    * Improve x86, PPC, iOS, Android, ARM, m68k, wasm, and msp430 etc. support
    * Fix build under GNU Hurd etc.
    * Add .NET PE binary
    * Improve GDB support

--- devel/radare2/patches/patch-sys_ios-sdk.sh
+++ devel/radare2/patches/patch-sys_ios-sdk.sh
@@ -1,13 +1,15 @@
-$NetBSD: patch-sys_ios-sdk.sh,v 1.1 2018/02/18 17:40:18 kamil Exp $
+$NetBSD: patch-sys_ios-sdk.sh,v 1.2 2019/03/26 19:29:53 ryoon Exp $
 
---- sys/ios-sdk.sh.orig	2018-01-29 10:36:59.000000000 +0000
+* POSIX shell portability
+
+--- sys/ios-sdk.sh.orig	2019-02-19 12:35:24.000000000 +0000
 +++ sys/ios-sdk.sh
-@@ -131,7 +131,7 @@ while test $# -gt 0; do
- 		-archs)
- 			shift
- 			if test $# -gt 0; then
--				if [ "$1" == "all" ]; then
-+				if [ "$1" = "all" ]; then
- 					ARCHS="armv7+armv7s+arm64"
- 				else
- 					ARCHS=$1
+@@ -152,7 +152,7 @@ while test $# -gt 0; do
+ 	-archs|-a|--archs)
+ 		shift
+ 		if test $# -gt 0; then
+-			if [ "$1" == "all" ]; then
++			if [ "$1" = "all" ]; then
+ 				ARCHS="armv7+armv7s+arm64"
+ 			else
+ 				ARCHS=$1
