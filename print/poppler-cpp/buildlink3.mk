9abcea0140f29181d8fd07b51ff4a14fe09901b9
wiz <wiz@pkgsrc.org>
2019-03-27 11:18:38 +0000

    poppler: revert update to 0.75.0
    Of course, it breaks everything again, and I don't want to fix it
    now in the freeze.

--- print/poppler-cpp/buildlink3.mk
+++ print/poppler-cpp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.44 2019/03/26 07:12:07 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.45 2019/03/27 11:18:38 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-cpp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-cpp
 POPPLER_CPP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-cpp+=	poppler-cpp>=0.16.7
-BUILDLINK_ABI_DEPENDS.poppler-cpp+=	poppler-cpp>=0.75.0
+BUILDLINK_ABI_DEPENDS.poppler-cpp+=	poppler-cpp>=0.74.0
 BUILDLINK_PKGSRCDIR.poppler-cpp?=	../../print/poppler-cpp
 
 .include "../../print/poppler/buildlink3.mk"
--- print/poppler-includes/buildlink3.mk
+++ print/poppler-includes/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.40 2019/03/26 07:12:07 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.41 2019/03/27 11:18:38 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-includes
 
@@ -8,7 +8,7 @@ POPPLER_INCLUDES_BUILDLINK3_MK:=
 BUILDLINK_DEPMETHOD.poppler-includes?=	build
 
 BUILDLINK_API_DEPENDS.poppler-includes+=	poppler-includes>=0.22.2
-BUILDLINK_ABI_DEPENDS.poppler-includes?=	poppler-includes>=0.75.0
+BUILDLINK_ABI_DEPENDS.poppler-includes?=	poppler-includes>=0.74.0
 BUILDLINK_PKGSRCDIR.poppler-includes?=		../../print/poppler-includes
 .endif # POPPLER_INCLUDES_BUILDLINK3_MK
 
