16a329e668356a828f4973f4b412f4725cbeb319
wiz <wiz@pkgsrc.org>
2019-03-27 11:18:38 +0000

    poppler: revert update to 0.75.0
    Of course, it breaks everything again, and I don't want to fix it
    now in the freeze.

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.71 2019/03/26 07:12:07 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.72 2019/03/27 11:18:38 wiz Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.75.0
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.74.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 GCC_REQD+=	4.9
