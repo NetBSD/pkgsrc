a63c784433985adb47aecd27cdf54056bee4dd21
jperkin <jperkin@pkgsrc.org>
2019-03-27 10:50:30 +0000

    webkit-gtk: Naive implementation of computeRAMSize for SunOS.

--- www/webkit-gtk/patches/patch-Source_WTF_wtf_RAMSize.cpp
+++ www/webkit-gtk/patches/patch-Source_WTF_wtf_RAMSize.cpp
@@ -1,13 +1,16 @@
-$NetBSD: patch-Source_WTF_wtf_RAMSize.cpp,v 1.2 2019/03/13 20:49:40 leot Exp $
+$NetBSD: patch-Source_WTF_wtf_RAMSize.cpp,v 1.3 2019/03/27 10:50:30 jperkin Exp $
 
-Add support for NetBSD.
+Add support for NetBSD and SunOS.
 
 --- Source/WTF/wtf/RAMSize.cpp.orig	2019-02-12 11:21:17.000000000 +0000
 +++ Source/WTF/wtf/RAMSize.cpp
-@@ -35,13 +35,16 @@
+@@ -35,13 +35,19 @@
  #if OS(LINUX)
  #include <sys/sysinfo.h>
  #endif // OS(LINUX)
++#if OS(SOLARIS)
++#include <unistd.h>
++#endif
 +#if OS(NETBSD)
 +#include <sys/sysctl.h>
 +#endif // OS(NETBSD)
@@ -22,10 +25,12 @@ Add support for NetBSD.
  static const size_t ramSizeGuess = 512 * MB;
  #endif
  
-@@ -59,9 +62,18 @@ static size_t computeRAMSize()
+@@ -59,9 +65,20 @@ static size_t computeRAMSize()
      struct sysinfo si;
      sysinfo(&si);
      return si.totalram * si.mem_unit;
++#elif OS(SOLARIS)
++    return (sysconf(_SC_PHYS_PAGES) * sysconf(_SC_PAGESIZE));
 +#elif OS(NETBSD)
 +    int mib[2];
 +    size_t len, totalram;
