c003973d7f0997ab79993a52faf06847a21b71d8
adam <adam@pkgsrc.org>
2019-04-03 08:05:44 +0000

    py-Pillow: updated to 6.0.0
    6.0.0:
    Python 2.7 support will be removed in Pillow 7.0.0
    Add EXIF class
    Add ImageOps exif_transpose method
    Added warnings to deprecated CMSProfile attributes
    Documented reading TIFF multiframe images
    Improved speed of opening an MPO file
    Update palette in quantize
    Improvements to TIFF is_animated and n_frames
    Fixed incompatible pointer type warnings
    Improvements to PA and LA conversion and palette operations
    Consistent DPI rounding
    Change size of MPO image to match frame
    Read Photoshop resolution data
    Ensure image is mutable before saving
    Correct remap_palette documentation
    Promote P images to PA in putalpha
    Allow RGB and RGBA values for new P images
    Fixed TIFF bug when seeking backwards and then forwards
    Cache EXIF information
    Added transparency for all PNG greyscale modes
    Fix deprecation warnings in Python 3.8
    Fixed GIF bug when rewinding to a non-zero frame
    Only close original fp in __del__ and __exit__ if original fp is exclusive
    Fix BytesWarning in Tests/test_numpy.py
    Add missing MIME types and extensions
    Add I;16 PNG save
    Add support for BMP RGBA bitfield compression
    Added ability to set language for text rendering
    Only close exclusive fp on Image __exit__
    Changed EPS subprocess stdout from devnull to None
    Add reading old-JPEG compressed TIFFs
    Add EXIF support for PNG
    Add option to set dither param on quantize
    Add reading of DDS uncompressed RGB data
    Correct length of Tiff BYTE tags
    Add DIB saving and loading through Image open
    Removed deprecated VERSION
    Fix 'BytesWarning: Comparison between bytes and string' in PdfDict
    Do not resize in Image.thumbnail if already the destination size
    Replace .seek() magic numbers with io.SEEK_* constants
    Make ContainerIO.isatty() return a bool, not int
    Add support to all transpose operations for I;16 modes
    Deprecate support for PyQt4 and PySide
    Add TIFF compression codecs: LZMA, Zstd, WebP
    Fixed pickling of iTXt class with protocol > 1
    _util.isPath returns True for pathlib.Path objects
    Remove unnecessary unittest.main() boilerplate from test files
    Exif: Seek to IFD offset
    Deprecate PIL.*ImagePlugin.__version__ attributes
    Docs: Add note about ImageDraw operations that exceed image bounds
    Allow for unknown PNG chunks after image data
    Changed EPS subprocess stdin from devnull to None
    Fix possible integer overflow
    Catch BaseException for resource cleanup handlers
    Improve pytest configuration to allow specific tests as CLI args
    Drop support for Python 3.4
    Remove deprecated PIL.OleFileIO
    Remove deprecated ImageOps undocumented functions
    Depends: Update libwebp to 1.0.2
    Detect MIME types

--- graphics/py-Pillow/patches/patch-setup.py
+++ graphics/py-Pillow/patches/patch-setup.py
@@ -1,11 +1,10 @@
-$NetBSD: patch-setup.py,v 1.17 2018/10/02 09:10:46 adam Exp $
+$NetBSD: patch-setup.py,v 1.18 2019/04/03 08:05:44 adam Exp $
 
 Disable mp_compile hack; it has problems with native parallel building.
-Prevent to detect optional tkinter.
 
---- setup.py.orig	2018-10-01 05:51:22.000000000 +0000
+--- setup.py.orig	2019-04-02 04:19:42.000000000 +0000
 +++ setup.py
-@@ -23,7 +23,6 @@ from setuptools import Extension, setup
+@@ -22,7 +22,6 @@ from setuptools import Extension, setup
  
  # monkey patch import hook. Even though flake8 says it's not used, it is.
  # comment this out to disable multi threaded builds.
@@ -13,25 +12,12 @@ Prevent to detect optional tkinter.
  
  
  if sys.platform == "win32" and sys.version_info >= (3, 8):
-@@ -128,11 +127,7 @@ def get_version():
-     return locals()['__version__']
- 
- 
--try:
--    import _tkinter
--except (ImportError, OSError):
--    # pypy emits an oserror
--    _tkinter = None
-+_tkinter = None
- 
- NAME = 'Pillow'
- PILLOW_VERSION = get_version()
-@@ -205,12 +200,6 @@ class pil_build_ext(build_ext):
+@@ -265,12 +264,6 @@ class pil_build_ext(build_ext):
          if self.debug:
              global DEBUG
              DEBUG = True
--        if sys.version_info >= (3, 5) and not self.parallel:
--            # For Python < 3.5, we monkeypatch distutils to have parallel
+-        if sys.version_info.major >= 3 and not self.parallel:
+-            # For Python 2.7, we monkeypatch distutils to have parallel
 -            # builds. If --parallel (or -j) wasn't specified, we want to
 -            # reproduce the same behavior as before, that is, auto-detect the
 -            # number of jobs.
