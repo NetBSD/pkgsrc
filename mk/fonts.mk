a1802884b19de8a8d1ac26a65d30833575af8ca0
maya <maya@pkgsrc.org>
2019-04-04 07:34:11 +0000

    Add a generic fonts.mk makefile fragment.
    Reviewed by rillig in
    http://mail-index.netbsd.org/tech-pkg/2019/03/05/msg020895.html
    
    I am only adding TTF and OTF handling because I don't know enough about
    the other font formats. :)

---
+++ mk/fonts.mk
@@ -0,0 +1,27 @@
+# $NetBSD: fonts.mk,v 1.12 2019/04/04 07:34:11 maya Exp $
+#
+# Install .otf and .ttf font files.
+#
+# The following variables may be used to tweak the installation path:
+#
+# TTF_FONTS_DIR is the install directory for .ttf files
+#
+# OTF_FONTS_DIR is the install directory for .otf files
+
+NO_CONFIGURE?=	yes
+NO_BUILD?=	yes
+
+TTF_FONTS_DIR?=		${PREFIX}/share/fonts/X11/TTF
+OTF_FONTS_DIR?=		${PREFIX}/share/fonts/X11/OTF
+
+INSTALLATION_DIRS+=	${TTF_FONTS_DIR} ${OTF_FONTS_DIR}
+
+do-install: install-fonts
+
+install-fonts:
+	${STEP_MSG} "Installing all .otf and .ttf files from " ${WRKSRC}
+	find ${WRKSRC} -name '*.ttf' \
+	    -exec ${INSTALL_DATA} "{}" ${DESTDIR}${TTF_FONTS_DIR} ";"
+	find ${WRKSRC} -name '*.otf' \
+	    -exec ${INSTALL_DATA} "{}" ${DESTDIR}${OTF_FONTS_DIR} ";"
+
