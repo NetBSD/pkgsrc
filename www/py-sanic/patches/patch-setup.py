07c29da696b0ff5ec588a68b0e6761c12841d97d
adam <adam@pkgsrc.org>
2019-04-04 12:25:38 +0000

    py-sanic: updated to 19.3.1
    19.3.1
    Changes:
    * Add support for zero-length and RFC 5987 encoded filename for multipart/form-data requests.
    * The type of expires attribute of sanic.cookies.Cookie is now enforced to be of type datetime.
    * Add support for the stream parameter of sanic.Sanic.add_route() available to sanic.Blueprint.add_route().
    * Accept negative values for route parameters with type int or number.
    * Deprecated the use of sanic.request.Request.raw_args - it has a fundamental flaw in which is drops repeated query string parameters. Added sanic.request.Request.query_args as a replacement for the original use-case.
    * Remove an unwanted None check in Request class repr implementation. This changes the default repr of a Request from <Request> to <Request: None />
    * Added 2 new parameters to sanic.app.Sanic.create_server:
    return_asyncio_server - whether to return an asyncio.Server.
    asyncio_server_kwargs - kwargs to pass to loop.create_server for the event loop that sanic is using.
    This is a breaking change.
    * Added a set of test cases that test and benchmark route resolution.
    * The type of the "max-age" value in a sanic.cookies.Cookie is now enforced to be an integer. Non-integer values are replaced with 0.
    * Added the endpoint attribute to an incoming request, containing the name of the handler function.
    * Improved request streaming. request.stream is now a bounded-size buffer instead of an unbounded queue. Callers must now call await request.stream.read() instead of await request.stream.get() to read each portion of the body.
    This is a breaking change.
    
    Fixes:
    * Sanic was prefetching time.time() and updating it once per second to avoid excessive time.time() calls. The implementation was observed to cause memory leaks in some cases. The benefit of the prefetch appeared to negligible, so this has been removed. Fixes
    * Fix a bug in the auto-reloader when the process was launched as a module i.e. python -m init0.mod1 where the sanic server is started in init0/mod1.py with debug enabled and imports another module in init0.
    * Allow sanic test client to bind to a random port by specifying port=None when constructing a SanicTestClient
    * Added the ability to specify middleware on a blueprint group, so that all routes produced from the blueprints in the group have the middleware applied.
    * Allow the the use the SANIC_ACCESS_LOG environment variable to enable/disable the access log when not explicitly passed to app.run(). This allows the access log to be disabled for example when running via gunicorn.
    
    Developer infrastructure:
    * Update project PyPI credentials
    * fix linter issue causing travis build failures
    * Fix python version in doc build
    * Upgrade setuptools version and use native docutils in doc build
    * Upgrade pytest, and fix caplog unit tests
    
    Typos and Documentation:
    * Fix typo at the exception documentation
    * fix typo in Asyncio example
    * Documentation typo
    * Fix grammar in README.md
    * Added "databases" to the extensions list
    * Add sanic-zipkin to extensions list
    * Removed link to deleted repo, Sanic-OAuth, from the extensions list
    * 18.12 changelog
    * Add example of amending request object
    * Update README
    * Update README
    * Update README, including new logo
    * fix minor type and pip install instruction mismatch
    * Documentation Enhancements

--- www/py-sanic/patches/patch-setup.py
+++ www/py-sanic/patches/patch-setup.py
@@ -1,17 +1,17 @@
-$NetBSD: patch-setup.py,v 1.2 2019/01/18 08:15:07 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2019/04/04 12:25:38 adam Exp $
 
 Allow newer versions.
 
---- setup.py.orig	2019-01-18 08:10:59.000000000 +0000
+--- setup.py.orig	2019-03-23 02:52:58.000000000 +0000
 +++ setup.py
-@@ -61,8 +61,8 @@ requirements = [
+@@ -81,8 +81,8 @@ requirements = [
      uvloop,
      ujson,
-     'aiofiles>=0.3.0',
--    'websockets>=6.0,<7.0',
--    'multidict>=4.0,<5.0',
-+    'websockets>=6.0',
-+    'multidict>=4.0',
+     "aiofiles>=0.3.0",
+-    "websockets>=6.0,<7.0",
+-    "multidict>=4.0,<5.0",
++    "websockets>=6.0",
++    "multidict>=4.0",
  ]
- if strtobool(os.environ.get("SANIC_NO_UJSON", "no")):
-     print("Installing without uJSON")
+ 
+ tests_require = [
