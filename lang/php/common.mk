2b6e6d52467be86bb15a1b4fc42a342e61db2eb1
taca <taca@pkgsrc.org>
2019-04-07 05:03:41 +0000

    lang/php: swtch to use https sites
    Switch to use https sites.

--- lang/php/common.mk
+++ lang/php/common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: common.mk,v 1.5 2016/09/11 17:14:46 taca Exp $
+# $NetBSD: common.mk,v 1.6 2019/04/07 05:03:41 taca Exp $
 # used by lang/php56/Makefile.common
 # used by lang/php70/Makefile.common
 # used by lang/php71/Makefile.common
@@ -9,14 +9,14 @@ PHPCOMMON_MK=	defined
 DISTNAME?=		php-${PHP_VERSION}
 
 .if !defined(PECL_VERSION)
-MASTER_SITES?=		http://www.php.net/distributions/ \
-			http://uk.php.net/distributions/ \
-			http://de.php.net/distributions/ \
-			http://us.php.net/distributions/
+MASTER_SITES?=		https://www.php.net/distributions/ \
+			https://uk.php.net/distributions/ \
+			https://de.php.net/distributions/ \
+			https://us.php.net/distributions/
 EXTRACT_SUFX?=		.tar.bz2
 .endif
 
-HOMEPAGE?=		http://www.php.net/
+HOMEPAGE?=		https://www.php.net/
 
 PLIST_SUBST+=		PHP_EXTENSION_DIR=${PHP_EXTENSION_DIR}
 PKG_SYSCONFVAR?=	php
