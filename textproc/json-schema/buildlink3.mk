59f97121de117285592c205fb267dd5b75bb4e2e
wiz <wiz@pkgsrc.org>
2019-04-08 22:07:42 +0000

    json-schema: add pcrecpp to bl3.mk

--- textproc/json-schema/buildlink3.mk
+++ textproc/json-schema/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/12/23 17:22:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/04/08 22:07:42 wiz Exp $
 
 BUILDLINK_TREE+=	json-schema
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.json-schema+=	json-schema>=1.0
 BUILDLINK_PKGSRCDIR.json-schema?=	../../textproc/json-schema
 BUILDLINK_DEPMETHOD.json-schema?=	build
 
+.include "../../devel/pcre/buildlink3.mk"
 .include "../../textproc/jsoncpp/buildlink3.mk"
 .endif	# JSON_SCHEMA_BUILDLINK3_MK
 
