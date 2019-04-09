881e04ef0924eb642d8258f398a90328b25eff22
wiz <wiz@pkgsrc.org>
2019-04-09 07:08:18 +0000

    jsoncpp: update to 1.8.4nb2.
    Give static library the same name as the shared one.
    
    Bump dependency in bl3.mk to a version with shared libraries.

--- textproc/jsoncpp/buildlink3.mk
+++ textproc/jsoncpp/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/04/09 06:48:18 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/04/09 07:08:18 wiz Exp $
 
 BUILDLINK_TREE+=	jsoncpp
 
 .if !defined(JSONCPP_BUILDLINK3_MK)
 JSONCPP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.jsoncpp+=	jsoncpp>=0.6.0rc2svn276
+BUILDLINK_API_DEPENDS.jsoncpp+=	jsoncpp>=1.8.4nb1
 BUILDLINK_PKGSRCDIR.jsoncpp?=	../../textproc/jsoncpp
 .endif	# JSONCPP_BUILDLINK3_MK
 
