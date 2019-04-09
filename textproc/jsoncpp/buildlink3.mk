a3597c6f4c4927a4952da8e3c1271ca8b3782408
wiz <wiz@pkgsrc.org>
2019-04-09 06:48:18 +0000

    jsoncpp: build shared library
    Bump PKGREVISION.

--- textproc/jsoncpp/buildlink3.mk
+++ textproc/jsoncpp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/04/18 22:26:08 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/04/09 06:48:18 wiz Exp $
 
 BUILDLINK_TREE+=	jsoncpp
 
@@ -7,7 +7,6 @@ JSONCPP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.jsoncpp+=	jsoncpp>=0.6.0rc2svn276
 BUILDLINK_PKGSRCDIR.jsoncpp?=	../../textproc/jsoncpp
-BUILDLINK_DEPMETHOD.jsoncpp?=	build
 .endif	# JSONCPP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-jsoncpp
