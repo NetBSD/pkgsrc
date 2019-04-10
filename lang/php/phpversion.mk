60d77de592a6bbe9a52fe09127b3b0b30a4a5e1e
bsiegert <bsiegert@pkgsrc.org>
2019-04-10 11:21:14 +0000

    Pullup ticket #5938 - requested by taca lang/php73: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.254
    - lang/php73/distinfo                                           1.6
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sun Apr  7 16:32:47 UTC 2019
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php73: distinfo
    
       Log Message:
       lang/php73: update to 7.3.4
    
       04 April 2019, PHP 7.3.4
    
       - Core:
         . Fixed bug #77738 (Nullptr deref in zend_compile_expr). (Laruence)
         . Fixed bug #77660 (Segmentation fault on break 2147483648). (Laruence)
         . Fixed bug #77652 (Anonymous classes can lose their interface information).
           (Nikita)
         . Fixed bug #77345 (Stack Overflow caused by circular reference in garbage
           collection). (Alexandru Patranescu, Nikita, Dmitry)
         . Fixed bug #76956 (Wrong value for 'syslog.filter' documented in php.ini).
           (cmb)
    
       - Apache2Handler:
         . Fixed bug #77648 (BOM in sapi/apache2handler/php_functions.c). (cmb)
    
       - Bcmath:
         . Fixed bug #77742 (bcpow() implementation related to gcc compiler
           optimization). (Nikita)
    
       - CLI Server:
         . Fixed bug #77722 (Incorrect IP set to $_SERVER['REMOTE_ADDR'] on the
           localhost). (Nikita)
    
       - COM:
         . Fixed bug #77578 (Crash when php unload). (cmb)
    
       - EXIF:
         . Fixed bug #77753 (Heap-buffer-overflow in php_ifd_get32s). (Stas)
         . Fixed bug #77831 (Heap-buffer-overflow in exif_iif_add_value). (Stas)
    
       - FPM:
         . Fixed bug #77677 (FPM fails to build on AIX due to missing WCOREDUMP).
           (Kevin Adler)
    
       - GD:
         . Fixed bug #77700 (Writing truecolor images as GIF ignores interlace flag).
           (cmb)
    
       - MySQLi:
         . Fixed bug #77597 (mysqli_fetch_field hangs scripts). (Nikita)
    
       - Opcache:
         . Fixed bug #77743 (Incorrect pi node insertion for jmpznz with identical
           successors). (Nikita)
    
       - Phar:
         . Fxied bug #77697 (Crash on Big_Endian platform). (Laruence)
    
       - phpdbg:
         . Fixed bug #77767 (phpdbg break cmd aliases listed in help do not match
           actual aliases). (Miriam Lauter)
    
       - sodium:
         . Fixed bug #77646 (sign_detached() strings not terminated). (Frank)
    
       - SQLite3:
         . Added sqlite3.defensive INI directive. (BohwaZ)
    
       - Standard:
         . Fixed bug #77664 (Segmentation fault when using undefined constant in
           custom wrapper). (Laruence)
         . Fixed bug #77669 (Crash in extract() when overwriting extracted array).
           (Nikita)
         . Fixed bug #76717 (var_export() does not create a parsable value for
           PHP_INT_MIN). (Nikita)
         . Fixed bug #77765 (FTP stream wrapper should set the directory as
           executable). (Vlad Temian)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.251.2.2 2019/04/10 11:17:13 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.251.2.3 2019/04/10 11:21:14 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP71_VERSION=	7.1.28
 PHP72_VERSION=	7.2.17
-PHP73_VERSION=	7.3.3
+PHP73_VERSION=	7.3.4
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
