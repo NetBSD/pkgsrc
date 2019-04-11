efa29a75e3b73016ee168dadc71214d58313addb
nia <nia@pkgsrc.org>
2019-04-11 20:56:34 +0000

    games/etlegacy: Add version 2.76.
    Enemy Territory: Legacy is an open source project that aims to
    create a fully compatible client and server for the popular online
    FPS game Wolfenstein: Enemy Territory.

---
+++ games/etlegacy/options.mk
@@ -0,0 +1,56 @@
+# $NetBSD: options.mk,v 1.1 2019/04/11 20:56:34 nia Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.etlegacy
+PKG_SUPPORTED_OPTIONS=		curl lua ogg theora freetype sqlite3 openssl
+PKG_SUGGESTED_OPTIONS=		curl lua ogg theora freetype sqlite3 openssl
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mcurl)
+CMAKE_ARGS+=	-DFEATURE_CURL=ON
+.include "../../www/curl/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DFEATURE_CURL=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mlua)
+CMAKE_ARGS+=	-DFEATURE_LUA=ON
+.include "../../lang/lua53/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DFEATURE_LUA=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mogg)
+CMAKE_ARGS+=	-DFEATURE_OGG_VORBIS=ON
+.include "../../audio/libvorbis/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DFEATURE_OGG_VORBIS=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mtheora)
+CMAKE_ARGS+=	-DFEATURE_THEORA=ON
+.include "../../multimedia/libtheora/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DFEATURE_THEORA=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mfreetype)
+CMAKE_ARGS+=	-DFEATURE_FREETYPE=ON
+.include "../../graphics/freetype2/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DFEATURE_FREETYPE=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Msqlite3)
+CMAKE_ARGS+=	-DFEATURE_DBMS=ON
+.include "../../databases/sqlite3/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DFEATURE_DBMS=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mopenssl)
+CMAKE_ARGS+=	-DFEATURE_OPENSSL=ON
+.include "../../security/openssl/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DFEATURE_OPENSSL=OFF
+.endif
