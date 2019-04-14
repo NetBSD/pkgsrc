f3eba5fbbf01a3b3779ae35a03e850acee080778
he <he@pkgsrc.org>
2019-04-14 12:49:17 +0000

    Sorry, messed up one patch; corrected version here.

--- lang/rust/patches/patch-src_librustc__llvm_build.rs
+++ lang/rust/patches/patch-src_librustc__llvm_build.rs
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_librustc__llvm_build.rs,v 1.4 2019/04/14 12:42:03 he Exp $
+$NetBSD: patch-src_librustc__llvm_build.rs,v 1.5 2019/04/14 12:49:17 he Exp $
 
 fix build on NetBSD HEAD-llvm. XXX there is probably a better way to do this.
 
@@ -9,14 +9,13 @@ fix build on NetBSD HEAD-llvm. XXX there is probably a better way to do this.
      } else if target.contains("netbsd") && llvm_static_stdcpp.is_some() {
          // NetBSD uses a separate library when relocation is required
 -        "stdc++_pic"
-+        // NetBSD uses a separate library when relocation is required
-+        if env::var_os("PKGSRC_HAVE_LIBCPP").is_some() {
-+            "c++_pic"
-+        } else {
++	if env::var_os("PKGSRC_HAVE_LIBCPP").is_some() {
++	    "c++_pic"
++	} else {
 +            "stdc++_pic"
-+        }
++	}
 +    } else if env::var_os("PKGSRC_HAVE_LIBCPP").is_some() {
-+        "c++"
++	"c++"
      } else if llvm_use_libcxx.is_some() {
          "c++"
      } else {
