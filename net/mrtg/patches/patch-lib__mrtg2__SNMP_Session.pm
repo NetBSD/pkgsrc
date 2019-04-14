60f45b356bbcba41fc33eab1410a3b49dcc27855
jnemeth <jnemeth@pkgsrc.org>
2019-04-14 21:50:36 +0000

    Update to MRTG 2.17.7:
    Changes 2.17.7, 2018-07-12
    --------------------------
    From: Martin Sechny, shenk.sk
    * update to HTML 5 template in bin/mrtg, checked by HTML+CSS W3C validator
    * corrected style in HTML template: color with background-color
      (workaround for missing text color property in mrtg.cfg)
    
    Changes 2.17.6, 2014-12-02
    --------------------------
    From: tobi
    * add --descr=ifname option to indexmaker
    * fix parsing of global option in cfgmaker
    * add dcn id to cfgmaker (baed on input from Pawel Golaszeswski)
    
    From: Steve Alligood <steve@betterlinux.com>
    * make sure threshval does not get clobberd
    
    Changes 2.17.5, 2012-04-26
    --------------------------
    From: Pavel Gulchouck <gul@gul.kiev.ua>
    * fix for kmg option coredump

--- net/mrtg/patches/patch-lib__mrtg2__SNMP_Session.pm
+++ net/mrtg/patches/patch-lib__mrtg2__SNMP_Session.pm
@@ -1,24 +1,24 @@
-$NetBSD: patch-lib__mrtg2__SNMP_Session.pm,v 1.1 2013/08/03 17:05:17 tron Exp $
+$NetBSD: patch-lib__mrtg2__SNMP_Session.pm,v 1.2 2019/04/14 21:50:36 jnemeth Exp $
 
 Fix run-time warnings under Perl 5.16 and newer caused by the IPv6 support
 in the "Socket" module.
 
---- lib/mrtg2/SNMP_Session.pm.orig	2012-01-12 10:29:00.000000000 +0000
-+++ lib/mrtg2/SNMP_Session.pm	2013-08-03 17:56:14.000000000 +0100
-@@ -146,7 +146,7 @@
+--- lib/mrtg2/SNMP_Session.pm.orig	2018-07-13 06:09:56.000000000 +0000
++++ lib/mrtg2/SNMP_Session.pm
+@@ -146,7 +146,7 @@ BEGIN {
  
      if (eval {local $SIG{__DIE__};require Socket6;} &&
         eval {local $SIG{__DIE__};require IO::Socket::INET6; IO::Socket::INET6->VERSION("1.26");}) {
--       Socket6->import(qw(pack_sockaddr_in6 inet_pton getaddrinfo unpack_sockaddr_in6));
+-       Socket6->import(qw(pack_sockaddr_in6 inet_pton getaddrinfo));
 +       Socket6->import(qw(inet_pton getaddrinfo));
  	$ipv6_addr_len = length(pack_sockaddr_in6(161, inet_pton(AF_INET6(), "::1")));
  	$SNMP_Session::ipv6available = 1;
      }
-@@ -601,7 +601,7 @@
+@@ -601,7 +601,7 @@ use Carp;
  BEGIN {
      if($SNMP_Session::ipv6available) {
  	    import IO::Socket::INET6;
--        Socket6->import(qw(pack_sockaddr_in6 inet_pton getaddrinfo unpack_sockaddr_in6));
+-        Socket6->import(qw(pack_sockaddr_in6 inet_pton getaddrinfo));
 +        Socket6->import(qw(inet_pton getaddrinfo));
      }
  }
