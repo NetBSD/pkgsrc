f60473e41a2372473e18550859327faa92acb8d2
adam <adam@pkgsrc.org>
2019-04-17 08:27:20 +0000

    jbig2dec: updated to 0.16
    Version 0.16:
    * API change allowing for library users to get the custom allocator
      so it may be freed if necessary. This is useful if the allocator
      is extended with e.g. a custom pointer needed by the allocator
      callbacks.

--- graphics/jbig2dec/buildlink3.mk
+++ graphics/jbig2dec/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/01/07 13:04:14 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/04/17 08:27:20 adam Exp $
 
 BUILDLINK_TREE+=	jbig2dec
 
 .if !defined(JBIG2DEC_BUILDLINK3_MK)
 JBIG2DEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.jbig2dec+=	jbig2dec>=0.11
+BUILDLINK_API_DEPENDS.jbig2dec+=	jbig2dec>=0.16
 BUILDLINK_PKGSRCDIR.jbig2dec?=		../../graphics/jbig2dec
 .endif	# JBIG2DEC_BUILDLINK3_MK
 
