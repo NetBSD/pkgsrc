1aaefb82958dc2e7cb79f02132c0b39398dc767e
adam <adam@pkgsrc.org>
2019-04-18 07:47:33 +0000

    libuv: updated to 1.28.0
    Version 1.28.0
    * unix,win: add uv_gettimeofday()
    * unix,win: add uv_fs_{open,read,close}dir()
    * unix: fix uv_interface_addresses()
    * fs: remove macOS-specific copyfile(3)
    * fs: add test for copyfile() respecting permissions
    * build: partially revert 5234b1c43a
    * zos: fix setsockopt error when using AF_UNIX
    * unix: suppress EINTR/EINPROGRESS in uv_fs_close()
    * build: use cmake APPLE variable to detect platform
    * distcheck: remove duplicate test/ entry
    * unix: remove unused cmpxchgl() function
    * unix: support sockaddr_un in uv_udp_send()
    * unix: guard use of PTHREAD_STACK_MIN
    * unix,win: introduce uv_timeval64_t
    * doc: document uv_timeval_t and uv_timeval64_t

--- devel/libuv/patches/patch-src_unix_thread.c
+++ devel/libuv/patches/patch-src_unix_thread.c
@@ -1,23 +1,10 @@
-$NetBSD: patch-src_unix_thread.c,v 1.5 2019/02/11 10:46:56 adam Exp $
+$NetBSD: patch-src_unix_thread.c,v 1.6 2019/04/18 07:47:33 adam Exp $
 
-Fix for systems without PTHREAD_STACK_MIN.
 Use feature test for pthread_condattr_setclock, absent in netbsd-6-0
 
 --- src/unix/thread.c.orig	2019-02-10 16:44:47.000000000 +0000
 +++ src/unix/thread.c
-@@ -219,9 +219,11 @@ int uv_thread_create_ex(uv_thread_t* tid
-     pagesize = (size_t)getpagesize();
-     /* Round up to the nearest page boundary. */
-     stack_size = (stack_size + pagesize - 1) &~ (pagesize - 1);
-+#ifdef PTHREAD_STACK_MIN
-     if (stack_size < PTHREAD_STACK_MIN)
-       stack_size = PTHREAD_STACK_MIN;
-+#endif
-   }
- 
-   if (stack_size > 0) {
-     attr = &attr_storage;
-@@ -685,7 +687,7 @@ int uv_cond_init(uv_cond_t* cond) {
+@@ -687,7 +689,7 @@ int uv_cond_init(uv_cond_t* cond) {
    if (err)
      return UV__ERR(err);
  
