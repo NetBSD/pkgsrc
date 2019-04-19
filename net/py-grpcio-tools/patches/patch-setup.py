0c24aacfc324528133ed702ab2b2f58f7849626f
adam <adam@pkgsrc.org>
2019-04-19 09:58:50 +0000

    py-grpcio-tools: updated to 1.20.0
    1.20.0:
    Sync with grpc 1.20.0

--- net/py-grpcio-tools/patches/patch-setup.py
+++ net/py-grpcio-tools/patches/patch-setup.py
@@ -1,10 +1,11 @@
-$NetBSD: patch-setup.py,v 1.1 2018/04/28 13:57:05 minskim Exp $
+$NetBSD: patch-setup.py,v 1.2 2019/04/19 09:58:50 adam Exp $
 
 Use pthread on FreeBSD.
+Use external protobuf.
 
---- setup.py.orig	2018-04-13 19:56:09.000000000 +0000
+--- setup.py.orig	2019-04-15 23:12:52.000000000 +0000
 +++ setup.py
-@@ -85,7 +85,7 @@ if EXTRA_ENV_COMPILE_ARGS is None:
+@@ -88,7 +88,7 @@ if EXTRA_ENV_COMPILE_ARGS is None:
          EXTRA_ENV_COMPILE_ARGS += ' -fno-wrapv -frtti'
  if EXTRA_ENV_LINK_ARGS is None:
      EXTRA_ENV_LINK_ARGS = ''
@@ -13,7 +14,19 @@ Use pthread on FreeBSD.
          EXTRA_ENV_LINK_ARGS += ' -lpthread'
      elif "win32" in sys.platform and sys.version_info < (3, 5):
          msvcr = cygwinccompiler.get_msvcr()[0]
-@@ -113,7 +113,7 @@ if "win32" in sys.platform:
+@@ -101,11 +101,9 @@ if EXTRA_ENV_LINK_ARGS is None:
+ EXTRA_COMPILE_ARGS = shlex.split(EXTRA_ENV_COMPILE_ARGS)
+ EXTRA_LINK_ARGS = shlex.split(EXTRA_ENV_LINK_ARGS)
+ 
+-CC_FILES = [os.path.normpath(cc_file) for cc_file in protoc_lib_deps.CC_FILES]
+ PROTO_FILES = [
+     os.path.normpath(proto_file) for proto_file in protoc_lib_deps.PROTO_FILES
+ ]
+-CC_INCLUDE = os.path.normpath(protoc_lib_deps.CC_INCLUDE)
+ PROTO_INCLUDE = os.path.normpath(protoc_lib_deps.PROTO_INCLUDE)
+ 
+ GRPC_PYTHON_TOOLS_PACKAGE = 'grpc_tools'
+@@ -116,7 +114,7 @@ if "win32" in sys.platform:
      DEFINE_MACROS += (('WIN32_LEAN_AND_MEAN', 1),)
      if '64bit' in platform.architecture()[0]:
          DEFINE_MACROS += (('MS_WIN64', 1),)
@@ -22,3 +35,20 @@ Use pthread on FreeBSD.
      DEFINE_MACROS += (('HAVE_PTHREAD', 1),)
  
  # By default, Python3 distutils enforces compatibility of
+@@ -163,7 +161,7 @@ def extension_modules():
+     plugin_sources += [
+         os.path.join('grpc_tools', 'main.cc'),
+         os.path.join('grpc_root', 'src', 'compiler', 'python_generator.cc')
+-    ] + [os.path.join(CC_INCLUDE, cc_file) for cc_file in CC_FILES]
++    ]
+ 
+     plugin_ext = extension.Extension(
+         name='grpc_tools._protoc_compiler',
+@@ -172,7 +170,6 @@ def extension_modules():
+             '.',
+             'grpc_root',
+             os.path.join('grpc_root', 'include'),
+-            CC_INCLUDE,
+         ],
+         language='c++',
+         define_macros=list(DEFINE_MACROS),
