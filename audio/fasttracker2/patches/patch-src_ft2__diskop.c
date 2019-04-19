870155bd04875e27551fc4596d48101084f1b92a
fox <fox@pkgsrc.org>
2019-04-19 02:05:14 +0000

    fasttracker2: update to b149
    Changes since b148:
    
    Beta #149 - 18.04.2019 (updated the same day)
    - Bug fix: If the window is minimized while you double-click on an associated
      module in your OS, maximize the window before asking if you want to discard
      unsaved changes.
    >> Updates:
    - Updated rtmidi to version 4.0
    - Windows: Updated SDL to version 2.0.9 (finally found a way to prevent a
      serious bug with 2.0.9, hence why I have been using 2.0.8 for a long time).
    - Increased "volume toolbox" (Smp. Ed.) range from -200..200% to -500..500%
    - Windows: Default Disk Op. directory is now "\Users\username"

--- audio/fasttracker2/patches/patch-src_ft2__diskop.c
+++ audio/fasttracker2/patches/patch-src_ft2__diskop.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-src_ft2__diskop.c,v 1.2 2019/03/21 10:16:40 fox Exp $
+$NetBSD: patch-src_ft2__diskop.c,v 1.3 2019/04/19 02:05:14 fox Exp $
 
 Added <sys/types.h> / <sys/stat.h> to prevent "unknown type name"
 (dev_t, ino_t and nlink_t) error from the included <fts.h>.
 
---- src/ft2_diskop.c.orig	2019-03-18 16:59:48.000000000 +0000
+--- src/ft2_diskop.c.orig	2019-04-19 01:53:39.359713817 +0000
 +++ src/ft2_diskop.c
-@@ -12,6 +12,8 @@
- #include <windows.h>
+@@ -13,6 +13,8 @@
  #include <direct.h>
+ #include <shlobj.h> // SHGetFolderPathW()
  #else
 +#include <sys/types.h>
 +#include <sys/stat.h>
