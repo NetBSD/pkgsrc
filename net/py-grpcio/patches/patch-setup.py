94626331fdee395a038dee72303d0d6a16f68b40
adam <adam@pkgsrc.org>
2019-04-19 07:31:47 +0000

    py-grpcio: updated to 1.20.0
    Release v1.20.0
    Add Python example for error handling.
    python docs: details are UTF-8 encodable, not just ASCII.
    Use C-Core API to perform time conversion.
    Use correct C integer types in Cython.
    Add the missing grpc_cfstream dependency.
    Strip Python wheel binary.
    Simplify batch operations event interpretation.
    add enter_graceful_shutdown() to health service.

--- net/py-grpcio/patches/patch-setup.py
+++ net/py-grpcio/patches/patch-setup.py
@@ -1,13 +1,14 @@
-$NetBSD: patch-setup.py,v 1.3 2019/03/28 17:35:14 leot Exp $
+$NetBSD: patch-setup.py,v 1.4 2019/04/19 07:31:47 adam Exp $
 
 Use dependencies in pkgsrc.
 
---- setup.py.orig	2019-02-26 17:31:26.000000000 +0000
+--- setup.py.orig	2019-04-15 23:12:51.000000000 +0000
 +++ setup.py
-@@ -207,18 +207,19 @@ EXTENSION_INCLUDE_DIRECTORIES = (
+@@ -206,19 +206,7 @@ EXTENSION_INCLUDE_DIRECTORIES = (
+     (PYTHON_STEM,) + CORE_INCLUDE + SSL_INCLUDE + ZLIB_INCLUDE +
      NANOPB_INCLUDE + CARES_INCLUDE + ADDRESS_SORTING_INCLUDE)
  
- EXTENSION_LIBRARIES = ()
+-EXTENSION_LIBRARIES = ()
 -if "linux" in sys.platform:
 -  EXTENSION_LIBRARIES += ('rt',)
 -if not "win32" in sys.platform:
@@ -20,32 +21,20 @@ Use dependencies in pkgsrc.
 -  EXTENSION_LIBRARIES += ('z',)
 -if BUILD_WITH_SYSTEM_CARES:
 -  EXTENSION_LIBRARIES += ('cares',)
-+#if "linux" in sys.platform:
-+#  EXTENSION_LIBRARIES += ('rt',)
-+#if not "win32" in sys.platform:
-+#  EXTENSION_LIBRARIES += ('m',)
-+#if "win32" in sys.platform:
-+#  EXTENSION_LIBRARIES += ('advapi32', 'ws2_32',)
-+#if BUILD_WITH_SYSTEM_OPENSSL:
-+#  EXTENSION_LIBRARIES += ('ssl', 'crypto',)
-+#if BUILD_WITH_SYSTEM_ZLIB:
-+#  EXTENSION_LIBRARIES += ('z',)
-+#if BUILD_WITH_SYSTEM_CARES:
-+#  EXTENSION_LIBRARIES += ('cares',)
-+EXTENSION_LIBRARIES += ('grpc',)
++EXTENSION_LIBRARIES = ('grpc',)
  
  DEFINE_MACROS = (('OPENSSL_NO_ASM', 1), ('_WIN32_WINNT', 0x600))
  if not DISABLE_LIBC_COMPATIBILITY:
-@@ -265,7 +266,7 @@ def cython_extensions_and_necessity():
+@@ -265,7 +253,7 @@ def cython_extensions_and_necessity():
                    for name in CYTHON_EXTENSION_MODULE_NAMES]
    config = os.environ.get('CONFIG', 'opt')
    prefix = 'libs/' + config + '/'
--  if "darwin" in sys.platform or USE_PREBUILT_GRPC_CORE:
-+  if False and "darwin" in sys.platform or USE_PREBUILT_GRPC_CORE:
+-  if USE_PREBUILT_GRPC_CORE:
++  if False:
      extra_objects = [prefix + 'libares.a',
                       prefix + 'libboringssl.a',
                       prefix + 'libgpr.a',
-@@ -277,7 +278,7 @@ def cython_extensions_and_necessity():
+@@ -277,7 +265,7 @@ def cython_extensions_and_necessity():
    extensions = [
        _extension.Extension(
            name=module_name,
