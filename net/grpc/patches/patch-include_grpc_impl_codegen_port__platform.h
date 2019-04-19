ae1c4906b9415ea708e36a58e4eb3a0d11b7a38f
adam <adam@pkgsrc.org>
2019-04-19 07:15:36 +0000

    grpc: updated to 1.20.0
    Release v1.20.0
    This release contains refinements, improvements, and bug fixes, with highlights listed below.
    
    Core
    Fix bug in CFStream endpoint.
    Fix a NULL deref in tcp_client_windows.cc.
    Avoid using grpc_core::Executor when the background poller is available.
    Update the c-ares submodule to the 1.15 release.
    Remove unnecessary hack which causes data races.
    Fix Potentially Dangerous Typo - locked function being run outside of combiner.
    Trace log the status of every c-ares lookup; cleanup error handling.
    Add SPIFFE security stack to gRPC .
    Nuking the poll-cv polling engine.
    Backport 18201 to fix "symbol not found: _ares_library_init" error in Bazel on MacOS build.
    Run run_after_write closures in h2 once write action is done.
    Transition into state CONNECTING when we start name resolution.
    Support "darwin_x86_64" CPU in cares.BUILD.
    Move grpc_shutdown internals to a detached thread.
    Fix c-ares on Windows "DNS resolution failure" triggered by logging.
    Disable c-ares on Android.
    
    C++
    Global Interceptor Registration allowed only once.
    cmake fix: builds disabled by CODEGEN=OFF should not be installed.
    cmake: when cross-compiling, the host grpc_cpp_plugin should be used.

--- net/grpc/patches/patch-include_grpc_impl_codegen_port__platform.h
+++ net/grpc/patches/patch-include_grpc_impl_codegen_port__platform.h
@@ -1,11 +1,10 @@
-$NetBSD: patch-include_grpc_impl_codegen_port__platform.h,v 1.2 2018/12/31 23:00:08 leot Exp $
+$NetBSD: patch-include_grpc_impl_codegen_port__platform.h,v 1.3 2019/04/19 07:15:36 adam Exp $
 
 Add NetBSD support.
-Add SunOS support.
 
---- include/grpc/impl/codegen/port_platform.h.orig	2018-07-12 14:40:09.000000000 +0000
+--- include/grpc/impl/codegen/port_platform.h.orig	2019-04-15 22:38:24.000000000 +0000
 +++ include/grpc/impl/codegen/port_platform.h
-@@ -282,6 +282,29 @@
+@@ -331,6 +331,29 @@
  #else /* _LP64 */
  #define GPR_ARCH_32 1
  #endif /* _LP64 */
@@ -35,30 +34,3 @@ Add SunOS support.
  #elif defined(__native_client__)
  #define GPR_PLATFORM_STRING "nacl"
  #ifndef _BSD_SOURCE
-@@ -310,6 +333,26 @@
- #else /* _LP64 */
- #define GPR_ARCH_32 1
- #endif /* _LP64 */
-+#elif defined(__sun)
-+#define GPR_PLATFORM_STRING "sunos"
-+#define GPR_SUNOS 1
-+#define GPR_CPU_POSIX 1
-+#define GPR_GCC_ATOMIC 1
-+#define GPR_GCC_TLS 1
-+#define GPR_POSIX_LOG 1
-+#define GPR_POSIX_ENV 1
-+#define GPR_POSIX_TMPFILE 1
-+#define GPR_POSIX_STRING 1
-+#define GPR_POSIX_SUBPROCESS 1
-+#define GPR_POSIX_SYNC 1
-+#define GPR_POSIX_TIME 1
-+#define GPR_GETPID_IN_UNISTD_H 1
-+#define GPR_SUPPORT_CHANNELS_FROM_FD 1
-+#ifdef _LP64
-+#define GPR_ARCH_64 1
-+#else /* _LP64 */
-+#define GPR_ARCH_32 1
-+#endif /* _LP64 */
- #else
- #error "Could not auto-detect platform"
- #endif
