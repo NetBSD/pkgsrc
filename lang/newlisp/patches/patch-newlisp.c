1f1fd1358d8240a45abbb85fc094f8ac75f894f4
hauke <hauke@pkgsrc.org>
2019-04-24 13:09:45 +0000

    Upgrade lang/newlisp from 10.6 to 10.7.1
    Changes too numerous to include, see
    <http://www.newlisp.org/downloads/newLISP-10.7.1-Release.html>
    <http://www.newlisp.org/downloads/previous-release-notes/>

--- lang/newlisp/patches/patch-newlisp.c
+++ lang/newlisp/patches/patch-newlisp.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-newlisp.c,v 1.3 2015/12/29 23:34:52 dholland Exp $
+$NetBSD: patch-newlisp.c,v 1.4 2019/04/24 13:09:45 hauke Exp $
 
 Fix build with recent readline.
 
---- newlisp.c.orig	2014-04-08 14:02:27.000000000 +0000
+--- newlisp.c.orig	2017-01-25 16:42:21.000000000 +0000
 +++ newlisp.c
 @@ -138,7 +138,7 @@ char preLoad[] = 
      "(define (Class:Class) (cons (context) (args)))";
@@ -13,18 +13,16 @@ Fix build with recent readline.
  #endif
  /* --------------------- globals -------------------------------------- */
  
-@@ -904,8 +904,8 @@ if(errorReg && !isNil((CELL*)errorEvent-
- 
+@@ -931,7 +931,7 @@ if(errorReg && !isNil((CELL*)errorEvent-
  #ifdef READLINE
  rl_readline_name = "newlisp";
--rl_attempted_completion_function = (CPPFunction *)newlisp_completion;
--#if defined(LINUX) || defined(_BSD)
-+rl_attempted_completion_function = newlisp_completion;
-+#if defined(LINUX)
+ rl_attempted_completion_function = (char ** (*) (const char *, int, int))newlisp_completion;
+-#if defined(LINUX) || defined(_BSD) || defined(KFREEBSD)
++#if defined(LINUX) || defined(KFREEBSD)
  /* in Bash .inputrc put 'set blink-matching-paren on' */
  rl_set_paren_blink_timeout(300000); /* 300 ms */
  #endif
-@@ -959,7 +959,7 @@ return 0;
+@@ -985,7 +985,7 @@ return 0;
  #endif /* not LIBRARY */
  
  #ifdef READLINE
@@ -33,21 +31,12 @@ Fix build with recent readline.
  {
  static int list_index, len, clen;
  char * name;
-@@ -984,15 +984,9 @@ while((name = primitive[list_index].name
- return ((char *)NULL);
- }
+@@ -1012,7 +1012,7 @@ return ((char *)NULL);
  
--#ifdef _BSD
--extern char **completion_matches PARAMS((char *, rl_compentry_func_t *));
--#else
--char ** completion_matches(const char * text, CPFunction commands);
--#endif
--
+ char ** completion_matches(const char * text,  char * (*commands)(const char *, int)); 
+ 
 -char ** newlisp_completion (char * text, int start, int end)
 +char ** newlisp_completion (const char * text, int start, int end)
  {
--return(completion_matches(text, (CPFunction *)command_generator));
-+return(completion_matches(text, command_generator));
+ return(completion_matches(text,  (char * (*) (const char *, int) )command_generator));
  }
- #endif /* READLINE */
- 
