6b2d188ab1d2922c6113dd8610fb94ce0ea93db5
leot <leot@pkgsrc.org>
2019-04-25 17:44:21 +0000

    python3*: improve the comment in patch-Python_pylifecycle.c
    The PR have (and probably will have further) relevant information,
    reference it too.

--- lang/python36/patches/patch-Python_pylifecycle.c
+++ lang/python36/patches/patch-Python_pylifecycle.c
@@ -1,7 +1,9 @@
-$NetBSD: patch-Python_pylifecycle.c,v 1.1 2019/04/09 18:47:24 adam Exp $
+$NetBSD: patch-Python_pylifecycle.c,v 1.2 2019/04/25 17:44:21 leot Exp $
 
-NetBSD is also affected by bpo-30225.
-See bpo-33384: https://bugs.python.org/issue33384
+NetBSD is also affected by bpo-30225 (<https://bugs.python.org/issue30225>),
+use fstat(2) on NetBSD too.  Fixes PR pkg/53229.
+
+Reported upstream via bpo-33384: <https://bugs.python.org/issue33384>
 
 --- Python/pylifecycle.c.orig	2019-04-09 17:51:55.000000000 +0000
 +++ Python/pylifecycle.c
