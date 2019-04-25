a66b9021a8a0b4ddee8cfc5441ae19b7f7bc12ef
markd <markd@pkgsrc.org>
2019-04-25 21:00:43 +0000

    kde: update KDE Frameworks to 5.57.0

--- security/kwallet/patches/patch-src_runtime_kwalletd_backend_blowfish.h
+++ security/kwallet/patches/patch-src_runtime_kwalletd_backend_blowfish.h
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_runtime_kwalletd_backend_blowfish.h,v 1.1 2018/02/08 13:49:04 jperkin Exp $
+$NetBSD: patch-src_runtime_kwalletd_backend_blowfish.h,v 1.2 2019/04/25 21:00:43 markd Exp $
 
 Avoid macro conflicts.
 
---- src/runtime/kwalletd/backend/blowfish.h.orig	2017-12-02 20:09:03.000000000 +0000
+--- src/runtime/kwalletd/backend/blowfish.h.orig	2019-04-07 07:33:14.000000000 +0000
 +++ src/runtime/kwalletd/backend/blowfish.h
 @@ -54,6 +54,8 @@ public:
-     virtual int decrypt(void *block, int len);
+     int decrypt(void *block, int len) override;
  
  private:
 +#undef _S
