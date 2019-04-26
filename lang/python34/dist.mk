7a898f86a5693c761b618ede410b14dd8cd3ed03
roy <roy@pkgsrc.org>
2019-04-26 09:40:15 +0000

    Wave bye-bye to python34 and python35.
    Discussed on pkgsrc-users@

--- lang/python34/dist.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: dist.mk,v 1.11 2019/03/20 19:29:11 adam Exp $
-
-PY_DISTVERSION=	3.4.10
-DISTNAME=	Python-${PY_DISTVERSION}
-EXTRACT_SUFX=	.tar.xz
-DISTINFO_FILE=	${.CURDIR}/../../lang/python34/distinfo
-PATCHDIR=	${.CURDIR}/../../lang/python34/patches
-MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION}/
