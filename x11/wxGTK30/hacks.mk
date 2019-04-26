964a0ac7984dca8bcaecdf6e373b23a6a553a60b
bouyer <bouyer@pkgsrc.org>
2019-04-26 16:53:20 +0000

    Disable precompiled headers for gcc-7.x too, it dies the same way as previous versions.

--- x11/wxGTK30/hacks.mk
+++ x11/wxGTK30/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.7 2018/04/13 11:43:55 bouyer Exp $
+# $NetBSD: hacks.mk,v 1.8 2019/04/26 16:53:20 bouyer Exp $
 
 ### [Mon Dec 14 16:58:00 MET 2015 : bouyer ]
 ### g++-4.8 on NetBSD/i386 fails with an internal compiler error
@@ -7,7 +7,7 @@
 .if ${OPSYS} == "NetBSD"
 .  include "../../mk/compiler.mk"
 .  if !empty(PKGSRC_COMPILER:Mgcc) && \
-      (!empty(CC_VERSION:Mgcc-4.8.*) || !empty(CC_VERSION:Mgcc-5.*) || !empty(CC_VERSION:Mgcc-6.*))
+      (!empty(CC_VERSION:Mgcc-4.8.*) || !empty(CC_VERSION:Mgcc-5.*) || !empty(CC_VERSION:Mgcc-6.*) || !empty(CC_VERSION:Mgcc-7.*))
 CONFIGURE_ARGS+=	--disable-precomp-headers
 .  endif
 .endif
