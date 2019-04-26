f186ee7c4d1b56dae5f8ea618c0069d8ecad14f8
maya <maya@pkgsrc.org>
2019-04-26 13:13:41 +0000

    Omit mentions of python 34 and 35, after those were removed.
    - Includes some whitespace changes, to be handled in a separate commit.

--- audio/streamtuner/options.mk
+++ audio/streamtuner/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.8 2018/07/03 05:03:03 adam Exp $
+# $NetBSD: options.mk,v 1.9 2019/04/26 13:13:43 maya Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.streamtuner
@@ -28,7 +28,7 @@ CONFIGURE_ARGS+=	--disable-xiph
 .if !empty(PKG_OPTIONS:Mpython)
 PLIST.python=		yes
 CONFIGURE_ARGS+=	--enable-python
-PYTHON_VERSIONS_INCOMPATIBLE=	34 35 36 37 # py-gtk2
+PYTHON_VERSIONS_INCOMPATIBLE=	 36 37 # py-gtk2
 .  include "../../x11/py-gtk2/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-python
