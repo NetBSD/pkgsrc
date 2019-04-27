3d7fc412bc24acf6290f58e6c04fc421aa1cafa4
wiz <wiz@pkgsrc.org>
2019-04-27 13:47:54 +0000

    compat51: whitespace fix

--- emulators/compat51/options.mk
+++ emulators/compat51/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2013/02/17 17:24:08 spz Exp $
+# $NetBSD: options.mk,v 1.2 2019/04/27 13:47:54 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.compat51
 PKG_SUPPORTED_OPTIONS=	x11
@@ -7,5 +7,5 @@ PKG_SUGGESTED_OPTIONS=	x11
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mx11)
-  DEPENDS+=	compat51-x11>=5.1:../../emulators/compat51-x11
+DEPENDS+=	compat51-x11>=5.1:../../emulators/compat51-x11
 .endif
