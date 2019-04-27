1f430241c5a925b9f7e7801c98b650eaad3a135a
wiz <wiz@pkgsrc.org>
2019-04-27 13:34:24 +0000

    compat50: fix whitespace

--- emulators/compat50/options.mk
+++ emulators/compat50/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2013/02/17 17:24:07 spz Exp $
+# $NetBSD: options.mk,v 1.2 2019/04/27 13:34:24 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.compat50
 PKG_SUPPORTED_OPTIONS=	x11
@@ -8,6 +8,6 @@ PKG_SUGGESTED_OPTIONS=	x11
 
 .if !empty(PKG_OPTIONS:Mx11)
 .  if ${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "sparc64" || ${MACHINE_ARCH} == "x86_64"
-     DEPENDS+=	compat50-x11>=5.0:../../emulators/compat50-x11
+DEPENDS+=	compat50-x11>=5.0:../../emulators/compat50-x11
 .  endif
 .endif
