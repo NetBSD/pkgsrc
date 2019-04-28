d64c835c3978e1e99f74d66283bb3f45748cb50a
wiz <wiz@pkgsrc.org>
2019-04-28 07:06:06 +0000

    glade: replace with glade3, nothing uses this.

---
+++ devel/glade/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/04/28 07:06:49 wiz Exp $
+
+BUILDLINK_TREE+=	glade
+
+.if !defined(GLADE_BUILDLINK3_MK)
+GLADE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.glade+=	glade>=3.22.0
+BUILDLINK_ABI_DEPENDS.glade+=	glade>=3.22.0
+BUILDLINK_PKGSRCDIR.glade?=	../../devel/glade
+
+.include "../../x11/gtk3/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.endif # GLADE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-glade
