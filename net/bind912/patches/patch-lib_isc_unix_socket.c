e03c03b122c7d66ce5f060b476a3cdf52ab8d77a
taca <taca@pkgsrc.org>
2019-04-30 02:46:16 +0000

    net/bind912: update to 9.12.4pl1
    Update bind912 to 9.12.4pl1 (BIND 9.12.4-P1).
    
    Fix security problem CVE-2018-5743 and CVE-2019-6467 and overhaul pkgsrc.
    Now no need to change namedb is permission under NetBSD.
    
    pkgsrc changes:
    
    * Simplify DESCR.
    * Update note about required directories.
    * Drop pkg-config from USE_TOOLS.
    * Drop none existing configure arguments and PKG_OPTIONS:
    	- fetchlimit
    	- sit
    * Sort PLIST.
    
    Please refer CHANGES file in detail before 9.12.4 release:
    
    	--- 9.12.4-P1 released ---
    
    5200.	[security]	tcp-clients settings could be exceeded in some cases,
    			which could lead to exhaustion of file descriptors.
    			(CVE-2018-5743) [GL #615]
    
    5199.	[security]	In certain configurations, named could crash
    			if nxdomain-redirect was in use and a redirected
    			query resulted in an NXDOMAIN from the cache.
    			(CVE-2019-6467) [GL #880]
    
    5167.	[bug]		nxdomain-redirect could sometimes lookup the wrong
    			redirect name. [GL #892]

--- net/bind912/patches/patch-lib_isc_unix_socket.c
+++ net/bind912/patches/patch-lib_isc_unix_socket.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-lib_isc_unix_socket.c,v 1.2 2018/10/21 15:51:46 taca Exp $
+$NetBSD: patch-lib_isc_unix_socket.c,v 1.3 2019/04/30 02:46:16 taca Exp $
 
 Apply fix from NetBSD revision 1.24.
 
---- lib/isc/unix/socket.c.orig	2018-10-06 05:51:22.000000000 +0000
+--- lib/isc/unix/socket.c.orig	2019-02-27 23:42:04.000000000 +0000
 +++ lib/isc/unix/socket.c
 @@ -257,6 +257,7 @@ typedef enum { poll_idle, poll_active, p
  			 (e) == EWOULDBLOCK || \
@@ -12,13 +12,3 @@ Apply fix from NetBSD revision 1.24.
  			 (e) == 0)
  
  #define DLVL(x) ISC_LOGCATEGORY_GENERAL, ISC_LOGMODULE_SOCKET, ISC_LOG_DEBUG(x)
-@@ -1574,7 +1575,8 @@ build_msghdr_send(isc__socket_t *sock, c
- 
- #if defined(IPV6_USE_MIN_MTU)
- 	if ((sock->type == isc_sockettype_udp) &&
--	    ((dev->attributes & ISC_SOCKEVENTATTR_USEMINMTU) != 0))
-+	    ((dev->attributes & ISC_SOCKEVENTATTR_USEMINMTU) != 0) &&
-+	    (sock->pf == AF_INET6))
- 	{
- 		int use_min_mtu = 1;	/* -1, 0, 1 */
- 
