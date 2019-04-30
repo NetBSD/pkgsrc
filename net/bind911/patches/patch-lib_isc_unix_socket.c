00b05b358f3dc9cdaeeeba92ca35a29fda75f9e3
taca <taca@pkgsrc.org>
2019-04-30 02:51:38 +0000

    net/bind911: update to 9.11.6pl1
    Update bind911 to 9.11.5pl4 (BIND 9.11.5-P4).
    
    Fix security problem CVE-2018-5743 and overhaul pkgsrc.  Now no need
    to change namedb is permission under NetBSD.
    
    * Update note about required directories.
    * Drop pkg-config from USE_TOOLS.
    * Drop none existing configure arguments and PKG_OPTIONS:
    	- fetchlimit
    	- sit
    
    	--- 9.11.6-P1 released ---
    
    5200.	[security]	tcp-clients settings could be exceeded in some cases,
    			which could lead to exhaustion of file descriptors.
    			(CVE-2018-5743) [GL #615]

--- net/bind911/patches/patch-lib_isc_unix_socket.c
+++ net/bind911/patches/patch-lib_isc_unix_socket.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-lib_isc_unix_socket.c,v 1.2 2018/10/21 15:51:14 taca Exp $
+$NetBSD: patch-lib_isc_unix_socket.c,v 1.3 2019/04/30 02:51:38 taca Exp $
 
 Apply fix from NetBSD revision 1.24.
 
---- lib/isc/unix/socket.c.orig	2018-10-06 01:36:17.000000000 +0000
+--- lib/isc/unix/socket.c.orig	2019-02-27 23:28:15.000000000 +0000
 +++ lib/isc/unix/socket.c
 @@ -258,6 +258,7 @@ typedef enum { poll_idle, poll_active, p
  			 (e) == EWOULDBLOCK || \
@@ -12,13 +12,3 @@ Apply fix from NetBSD revision 1.24.
  			 (e) == 0)
  
  #define DLVL(x) ISC_LOGCATEGORY_GENERAL, ISC_LOGMODULE_SOCKET, ISC_LOG_DEBUG(x)
-@@ -1575,7 +1576,8 @@ build_msghdr_send(isc__socket_t *sock, c
- 
- #if defined(IPV6_USE_MIN_MTU)
- 	if ((sock->type == isc_sockettype_udp) &&
--	    ((dev->attributes & ISC_SOCKEVENTATTR_USEMINMTU) != 0))
-+	    ((dev->attributes & ISC_SOCKEVENTATTR_USEMINMTU) != 0) &&
-+	    (sock->pf == AF_INET6))
- 	{
- 		int use_min_mtu = 1;	/* -1, 0, 1 */
- 
