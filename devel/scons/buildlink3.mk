bde0fffd6ac5958e4cb597c61bb895230952e18f
gdt <gdt@pkgsrc.org>
2019-04-30 22:14:57 +0000

    devel/scons: Explain about common woes

--- devel/scons/buildlink3.mk
+++ devel/scons/buildlink3.mk
@@ -1,4 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/01/02 17:18:47 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/04/30 22:14:57 gdt Exp $
+
+# Note that while scons is a build tool, it requires running a python
+# program on the host that uses the scons library.  Currently, the
+# python version on the host and the version being used on the target
+# for the package being built are linked.
+# \todo Consider if this can be BUILD_DEPENDS (or TOOL), instead of buildlink.
+
+# scons scripts (SConstruct) have a number of common problems,
+# including not passing environment variables set by pkgsrc to
+# invocations of the compiler.  See the
+# /usr/pkgsrc/*/*/patch-SConstruct for typical remediations.
 
 SCONSBIN=	${PREFIX}/bin/scons
 
