d216a09ca112d0f257f526488e2b4e05cc6d8b7b
hauke <hauke@pkgsrc.org>
2019-04-30 09:22:10 +0000

    Update net/cacti-spine to v1.2.2
    For the many changes, see <https://cacti.net/spine_changelog.php>.
    
    v1.2.3 is available, but does not sport a configure script. An
    autotools dance showered me in error messages, so I didn't bother,
    since v1.2.x versions are expected to be compatible.

--- net/cacti-spine/patches/patch-spine.h
+++ net/cacti-spine/patches/patch-spine.h
@@ -1,11 +1,13 @@
-$NetBSD: patch-spine.h,v 1.1 2016/06/02 16:00:02 hauke Exp $
+$NetBSD: patch-spine.h,v 1.2 2019/04/30 09:22:10 hauke Exp $
 
 Despite the autoconf --sysconfdir, spine uses a hardcoded path to
 look for its config file. Patch that for pkgsrc.
 
---- spine.h.orig	2016-05-08 18:57:41.000000000 +0000
+While here, patch in the net/cacti logfile path.
+
+--- spine.h.orig	2019-02-24 14:53:23.000000000 +0000
 +++ spine.h
-@@ -136,7 +136,7 @@
+@@ -145,7 +145,7 @@
  
  /* locations to search for the config file */
  #define CONFIG_PATHS 4
@@ -14,3 +16,12 @@ look for its config file. Patch that for pkgsrc.
  #define CONFIG_PATH_2 "/etc/"
  #define CONFIG_PATH_3 "/etc/cacti/"
  #define CONFIG_PATH_4 "../etc/"
+@@ -159,7 +159,7 @@
+ #define DEFAULT_DB_PASS "cactiuser"
+ #define DEFAULT_DB_PORT 3306
+ #define DEFAULT_DB_PREG 0
+-#define DEFAULT_LOGFILE "/var/www/html/cacti/log/cacti.log"
++#define DEFAULT_LOGFILE "@CACTI_LOGDIR@/cacti.log"
+ #define DEFAULT_TIMEOUT 294000000
+ 
+ /* threads constants */
