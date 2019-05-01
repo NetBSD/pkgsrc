8cffd47993b1c369dedd0d066998688e016ac4ed
gutteridge <gutteridge@pkgsrc.org>
2019-05-01 02:51:49 +0000

    xscreensaver: don't install with SETUID if PAM is enabled
    If the (non-default) "pam" option is selected, then don't install this
    SETUID, it's not necessary. (Or, to put it differently, do what gnome-
    screensaver and mate-screensaver are already doing, which is the same.)

--- x11/xscreensaver/options.mk
+++ x11/xscreensaver/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.11 2018/09/02 20:26:59 wiz Exp $
+# $NetBSD: options.mk,v 1.12 2019/05/01 02:51:49 gutteridge Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xscreensaver
 PKG_SUPPORTED_OPTIONS=	pam webcollage opengl
@@ -18,6 +18,7 @@ DEPENDS+=	pam-pwauth_suid-[0-9]*:../../security/pam-pwauth_suid
 CONFIGURE_ARGS+=	--without-pam
 # configure should figure out
 #CONFIGURE_ARGS+=	--without-shadow
+SPECIAL_PERMS+=		bin/xscreensaver ${SETUID_ROOT_PERMS}
 .endif
 
 PLIST_SRC+=	PLIST
