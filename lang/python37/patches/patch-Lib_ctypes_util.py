f79cb6ccdf23155f5cb3707e82a23a461a1f1992
wiedi <wiedi@pkgsrc.org>
2019-05-02 12:50:42 +0000

    python37: fix find_library() on SunOS
    Help find_library find pkgsrc libs also on SunOS.
    While here pull in http://bugs.python.org/issue23287 like Oracle Solaris and
    OpenIndiana already do.

--- lang/python37/patches/patch-Lib_ctypes_util.py
+++ lang/python37/patches/patch-Lib_ctypes_util.py
@@ -1,11 +1,13 @@
-$NetBSD: patch-Lib_ctypes_util.py,v 1.1 2019/03/02 13:23:36 adam Exp $
+$NetBSD: patch-Lib_ctypes_util.py,v 1.2 2019/05/02 12:50:42 wiedi Exp $
 
 Fallback to clang.
 
 Look for shared libraries in PkgSrc prefix.
 Note: /usr/local will get replaced by SUBST.
 
---- Lib/ctypes/util.py.orig	2018-12-23 21:37:36.000000000 +0000
+Pull in patch for http://bugs.python.org/issue23287 for SunOS
+
+--- Lib/ctypes/util.py.orig	2019-03-25 20:21:05.000000000 +0000
 +++ Lib/ctypes/util.py
 @@ -102,6 +102,8 @@ elif os.name == "posix":
  
@@ -16,7 +18,54 @@ Note: /usr/local will get replaced by SUBST.
              c_compiler = shutil.which('cc')
          if not c_compiler:
              # No C compiler available, give up
-@@ -287,7 +289,7 @@ elif os.name == "posix":
+@@ -213,34 +215,15 @@ elif os.name == "posix":
+ 
+     elif sys.platform == "sunos5":
+ 
+-        def _findLib_crle(name, is64):
+-            if not os.path.exists('/usr/bin/crle'):
+-                return None
++        def _findLib_path(name, is64):
+ 
+             env = dict(os.environ)
+             env['LC_ALL'] = 'C'
+ 
+             if is64:
+-                args = ('/usr/bin/crle', '-64')
++                paths = "/lib/64:/usr/lib/64:/usr/local/lib"
+             else:
+-                args = ('/usr/bin/crle',)
+-
+-            paths = None
+-            try:
+-                proc = subprocess.Popen(args,
+-                                        stdout=subprocess.PIPE,
+-                                        stderr=subprocess.DEVNULL,
+-                                        env=env)
+-            except OSError:  # E.g. bad executable
+-                return None
+-            with proc:
+-                for line in proc.stdout:
+-                    line = line.strip()
+-                    if line.startswith(b'Default Library Path (ELF):'):
+-                        paths = os.fsdecode(line).split()[4]
+-
+-            if not paths:
+-                return None
++                paths = "/lib:/usr/lib:/usr/local/lib"
+ 
+             for dir in paths.split(":"):
+                 libfile = os.path.join(dir, "lib%s.so" % name)
+@@ -250,7 +233,7 @@ elif os.name == "posix":
+             return None
+ 
+         def find_library(name, is64 = False):
+-            return _get_soname(_findLib_crle(name, is64) or _findLib_gcc(name))
++            return _get_soname(_findLib_path(name, is64) or _findLib_gcc(name))
+ 
+     else:
+ 
+@@ -287,7 +270,7 @@ elif os.name == "posix":
          def _findLib_ld(name):
              # See issue #9998 for why this is needed
              expr = r'[^\(\)\s]*lib%s\.[^\(\)\s]*' % re.escape(name)
