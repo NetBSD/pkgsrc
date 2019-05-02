0c02172d89bd92233fd9bc9fe7a81abaca65fd04
wiz <wiz@pkgsrc.org>
2019-05-02 08:53:00 +0000

    uvscan: remove
    Distfile does not exist and was not redistributable.
    Package was marked BROKEN for this reason for some time.
    Newer version available, package could be re-added if someone is interested.
    (Last update was 2007.)

--- security/uvscan/files/uvscan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# $NetBSD: uvscan.sh,v 1.4 2007/07/29 05:19:51 jlam Exp $
-#
-# Wrapper script to execute McAfee(R) VirusScan
-
-LD_LIBRARY_PATH=@PREFIX@/libexec/uvscan @PREFIX@/libexec/uvscan/uvscan "$@"
