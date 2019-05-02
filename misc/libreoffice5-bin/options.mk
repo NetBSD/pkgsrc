aa7eff226519a09948b056b00140d660d9221c5f
wiz <wiz@pkgsrc.org>
2019-05-02 08:36:09 +0000

    sun-jdk6, sun-jre6: remove
    Last update in 2013, remove sun-jdk7/sun-jre7 instead

--- misc/libreoffice5-bin/options.mk
+++ misc/libreoffice5-bin/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2016/01/08 23:53:13 ryoon Exp $
+# $NetBSD: options.mk,v 1.2 2019/05/02 08:36:10 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libreoffice-bin
 PKG_SUPPORTED_OPTIONS=	java
@@ -6,7 +6,7 @@ PKG_SUPPORTED_OPTIONS=	java
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mjava)
-PKG_JVMS_ACCEPTED=	jdk15 sun-jdk6
+PKG_JVMS_ACCEPTED=	jdk15
 USE_JAVA=		run
 .include "../../mk/java-vm.mk"
 .endif
