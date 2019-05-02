13c6e3c842a7bf25b69e53ed50c6eda3287cd697
taca <taca@pkgsrc.org>
2019-05-02 13:29:53 +0000

    net/bind914: fix an error when reloading configuration
    Fix an error when reloading configuration.  There is on more check to
    "directory" in option statement is writable.
    
    Bump PKGREVISION.

--- net/bind914/patches/patch-bin_named_server.c
+++ net/bind914/patches/patch-bin_named_server.c
@@ -1,11 +1,27 @@
-$NetBSD: patch-bin_named_server.c,v 1.1 2019/04/30 03:34:34 taca Exp $
+$NetBSD: patch-bin_named_server.c,v 1.2 2019/05/02 13:29:53 taca Exp $
 
 * Disable checking working directory is writable as BIND_USER in NetBSD
   base system.
 
 --- bin/named/server.c.orig	2019-04-06 20:09:59.000000000 +0000
 +++ bin/named/server.c
-@@ -8864,6 +8864,7 @@ load_configuration(const char *filename,
+@@ -6464,6 +6464,7 @@ directory_callback(const char *clausenam
+ 			    "option 'directory' contains relative path '%s'",
+ 			    directory);
+ 
++#if 0
+ 	if (!isc_file_isdirwritable(directory)) {
+ 		isc_log_write(named_g_lctx, NAMED_LOGCATEGORY_GENERAL,
+ 			      NAMED_LOGMODULE_SERVER, ISC_LOG_ERROR,
+@@ -6471,6 +6472,7 @@ directory_callback(const char *clausenam
+ 			      directory);
+ 		return (ISC_R_NOPERM);
+ 	}
++#endif
+ 
+ 	result = isc_dir_chdir(directory);
+ 	if (result != ISC_R_SUCCESS) {
+@@ -8864,6 +8866,7 @@ load_configuration(const char *filename,
  		named_os_changeuser();
  	}
  
@@ -13,7 +29,7 @@ $NetBSD: patch-bin_named_server.c,v 1.1 2019/04/30 03:34:34 taca Exp $
  	/*
  	 * Check that the working directory is writable.
  	 */
-@@ -8874,6 +8875,7 @@ load_configuration(const char *filename,
+@@ -8874,6 +8877,7 @@ load_configuration(const char *filename,
  		result = ISC_R_NOPERM;
  		goto cleanup;
  	}
