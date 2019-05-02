a973313c9f34546f41ec7857519849a941f01f23
taca <taca@pkgsrc.org>
2019-05-02 13:43:38 +0000

    lang/php72: update to 7.2.18
    Update php72 to update to 7.2.18.
    
    02 May 2019, PHP 7.2.18
    
    - CLI:
      . Fixed bug #77794 (Incorrect Date header format in built-in server).
        (kelunik)
    
    - EXIF
      . Fixed bug #77950 (Heap-buffer-overflow in _estrndup via exif_process_IFD_TAG).
        (CVE-2019-11036) (Stas)
    
    - Interbase:
      . Fixed bug #72175 (Impossibility of creating multiple connections to
        Interbase with php 7.x). (Nikita)
    
    - Intl:
      . Fixed bug #77895 (IntlDateFormatter::create fails in strict mode if $locale
        = null). (Nikita)
    
    - litespeed:
      . LiteSpeed SAPI 7.3.1, better process management, new API function
        litespeed_finish_request(). (George Wang)
    
    - Mail
      . Fixed bug #77821 (Potential heap corruption in TSendMail()). (cmb)
    
    - PCRE:
      . Fixed bug #77827 (preg_match does not ignore \r in regex flags). (requinix,
        cmb)
    
    - PDO:
      . Fixed bug #77849 (Disable cloning of PDO handle/connection objects).
        (camporter)
    
    - phpdbg:
      . Fixed bug #76801 (too many open files). (alekitto)
      . Fixed bug #77800 (phpdbg segfaults on listing some conditional breakpoints).
        (krakjoe)
      . Fixed bug #77805 (phpdbg build fails when readline is shared). (krakjoe)
    
    - Reflection:
      . Fixed bug #77772 (ReflectionClass::getMethods(null) doesn't work). (Nikita)
      . Fixed bug #77882 (Different behavior: always calls destructor). (Nikita)
    
    - Standard:
      . Fixed bug #77680 (recursive mkdir on ftp stream wrapper is incorrect).
        (Vlad Temian)
      . Fixed bug #77844 (Crash due to null pointer in parse_ini_string with
        INI_SCANNER_TYPED). (Nikita)
      . Fixed bug #77853 (Inconsistent substr_compare behaviour with empty
        haystack). (Nikita)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.254 2019/04/07 16:32:47 taca Exp $
+# $NetBSD: phpversion.mk,v 1.255 2019/05/02 13:43:38 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP71_VERSION=	7.1.28
-PHP72_VERSION=	7.2.17
+PHP72_VERSION=	7.2.18
 PHP73_VERSION=	7.3.4
 
 # Define initial release of major version.
