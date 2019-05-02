4b38a046e9ca7873e76bde65121be528841a7eee
taca <taca@pkgsrc.org>
2019-05-02 13:45:28 +0000

    lang/php73: update to 7.3.5
    Update php73 to 7.3.5.
    
    02 May 2019, PHP 7.3.5
    
    - Core:
      . Fixed bug #77903 (ArrayIterator stops iterating after offsetSet call).
        (Nikita)
    
    - CLI:
      . Fixed bug #77794 (Incorrect Date header format in built-in server).
        (kelunik)
    
    - EXIF
      . Fixed bug #77950 (Heap-buffer-overflow in _estrndup via exif_process_IFD_TAG).
        (CVE-2019-11036) (Stas)
    
    - Interbase:
      . Fixed bug #72175 (Impossibility of creating multiple connections to
        Interbase with php 7.x). (Nikita)
    
    - Intl:
      . Fixed bug #77895 (IntlDateFormatter::create fails in strict mode if $locale
        = null). (Nikita)
    
    - litespeed:
      . LiteSpeed SAPI 7.3.1, better process management, new API function
        litespeed_finish_request(). (George Wang)
    
    - LDAP:
      . Fixed bug #77869 (Core dump when using server controls) (mcmic)
    
    - Mail
      . Fixed bug #77821 (Potential heap corruption in TSendMail()). (cmb)
    
    - mbstring:
      . Implemented FR #72777 (Implement regex stack limits for mbregex functions).
        (Yasuo Ohgaki, Stas)
    
    - MySQLi:
      . Fixed bug #77773 (Unbuffered queries leak memory - MySQLi / mysqlnd).
        (Nikita)
    
    - PCRE:
      . Fixed bug #77827 (preg_match does not ignore \r in regex flags). (requinix,
        cmb)
    
    - PDO:
      . Fixed bug #77849 (Disable cloning of PDO handle/connection objects).
        (camporter)
    
    - phpdbg:
      . Fixed bug #76801 (too many open files). (alekitto)
      . Fixed bug #77800 (phpdbg segfaults on listing some conditional breakpoints).
        (krakjoe)
      . Fixed bug #77805 (phpdbg build fails when readline is shared). (krakjoe)
    
    - Reflection:
      . Fixed bug #77772 (ReflectionClass::getMethods(null) doesn't work). (Nikita)
      . Fixed bug #77882 (Different behavior: always calls destructor). (Nikita)
    
    - Standard:
      . Fixed bug #77793 (Segmentation fault in extract() when overwriting
        reference with itself). (Nikita)
      . Fixed bug #77844 (Crash due to null pointer in parse_ini_string with
        INI_SCANNER_TYPED). (Nikita)
      . Fixed bug #77853 (Inconsistent substr_compare behaviour with empty
        haystack). (Nikita)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.255 2019/05/02 13:43:38 taca Exp $
+# $NetBSD: phpversion.mk,v 1.256 2019/05/02 13:45:28 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP71_VERSION=	7.1.28
 PHP72_VERSION=	7.2.18
-PHP73_VERSION=	7.3.4
+PHP73_VERSION=	7.3.5
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
