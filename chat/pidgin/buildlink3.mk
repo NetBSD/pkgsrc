3507099e11c6126c6c04e1ae45273631dc00e44f
ryoon <ryoon@pkgsrc.org>
2019-05-05 22:49:45 +0000

    Recursive rebvump from devel/nss

--- chat/pidgin/buildlink3.mk
+++ chat/pidgin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.36 2013/02/16 11:19:24 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.37 2019/05/05 22:49:46 ryoon Exp $
 
 BUILDLINK_TREE+=	pidgin
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	pidgin
 PIDGIN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pidgin+=	pidgin>=2.5.1
-BUILDLINK_ABI_DEPENDS.pidgin+=	pidgin>=2.10.6nb7
+BUILDLINK_ABI_DEPENDS.pidgin+=	pidgin>=2.13.0nb4
 BUILDLINK_PKGSRCDIR.pidgin?=	../../chat/pidgin
 
 .include "../../chat/libpurple/buildlink3.mk"
