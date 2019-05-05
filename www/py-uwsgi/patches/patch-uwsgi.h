2176cf68336ec614be20ce436bde8b41aa3d3011
wiedi <wiedi@pkgsrc.org>
2019-05-05 17:55:34 +0000

    py-uwsgi: fix build on SunOS

--- www/py-uwsgi/patches/patch-uwsgi.h
+++ www/py-uwsgi/patches/patch-uwsgi.h
@@ -1,10 +1,11 @@
-$NetBSD: patch-uwsgi.h,v 1.6 2015/04/06 02:35:54 rodent Exp $
+$NetBSD: patch-uwsgi.h,v 1.7 2019/05/05 17:55:34 wiedi Exp $
 
 Avoid clash with sys/user.h inclusion in logging.c on DragonFly BSD.
+Avoid using "sun" which breaks on SunOS
 
---- uwsgi.h.orig	2015-03-17 07:34:34.000000000 +0000
+--- uwsgi.h.orig	2019-02-09 14:48:07.000000000 +0000
 +++ uwsgi.h
-@@ -327,6 +327,10 @@ extern int pivot_root(const char *new_ro
+@@ -322,6 +322,10 @@ extern int pivot_root(const char *new_ro
  #elif defined(__CYGWIN__)
  #elif defined(__HURD__)
  #else
@@ -14,4 +15,13 @@ Avoid clash with sys/user.h inclusion in logging.c on DragonFly BSD.
 +#endif /* __DragonFly__ */
  #include <sys/event.h>
  #endif
+ 
+@@ -1629,7 +1633,7 @@ struct wsgi_request {
+ 	union address {
+ 		struct sockaddr_in sin;
+ 		struct sockaddr_in6 sin6;
+-		struct sockaddr_un sun;
++		struct sockaddr_un so_sun;
+ 	} client_addr;
+ };
  
