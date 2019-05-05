2043dc0eaa667e5fa6fea78cb7c02dd1dd289c2f
bsiegert <bsiegert@pkgsrc.org>
2019-05-05 14:26:11 +0000

    Pullup ticket #5943 - requested by leot www/webkit-gtk: build fix
    Revisions pulled up:
    - www/webkit-gtk/options.mk                                     1.15-1.16
    
    ---
       Module Name:    pkgsrc
       Committed By:   leot
       Date:           Tue Apr  9 15:34:04 UTC 2019
    
       Modified Files:
               pkgsrc/www/webkit-gtk: options.mk
    
       Log Message:
       webkit-gtk: Add references about WEBKIT_JIT_MACHINE_PLATFORMS
    
    ---
       Module Name:    pkgsrc
       Committed By:   leot
       Date:           Thu Apr 11 17:48:39 UTC 2019
    
       Modified Files:
               pkgsrc/www/webkit-gtk: options.mk
    
       Log Message:
       webkit-gtk: Adjust WEBKIT_JIT_MACHINE_PLATFORMS per-upstream
    
       Sync WEBKIT_JIT_MACHINE_PLATFORMS (platforms where `webkit-jit'
       option is suggested) is enabled by default with
       Source/cmake/WebKitFeatures.cmake.
    
       While here also disable ENABLE_C_LOOP when ENABLE_JIT is enabled
       (the two options are incompatible).
    
       Should workaround PR pkg/54109.
    
       (No PKGREVISION bump since it should only fix platforms where since
       update of webkit-gtk 2.24.0 were broken.)

--- www/webkit-gtk/options.mk
+++ www/webkit-gtk/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.14 2018/11/05 16:50:48 bsiegert Exp $
+# $NetBSD: options.mk,v 1.14.4.1 2019/05/05 14:26:11 bsiegert Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.webkit-gtk
@@ -12,14 +12,16 @@ PLIST_VARS=	introspection
 #
 # Platforms that support the webkit-jit option
 #
+# Please see:
+#  Source/cmake/WebKitFeatures.cmake
+#  Source/WTF/wtf/Platform.h
+#
 WEBKIT_JIT_MACHINE_PLATFORMS+=	Darwin-*-*
 WEBKIT_JIT_MACHINE_PLATFORMS+=	DragonFly-*-*
-WEBKIT_JIT_MACHINE_PLATFORMS+=	FreeBSD-*-x86_64 FreeBSD-*-i386 FreeBSD-*-arm*
-WEBKIT_JIT_MACHINE_PLATFORMS+=	FreeBSD-*-aarch64 FreeBSD-*-mips*
-WEBKIT_JIT_MACHINE_PLATFORMS+=	Linux-*-x86_64 Linux-*-i386 Linux-*-arm*
+WEBKIT_JIT_MACHINE_PLATFORMS+=	FreeBSD-*-x86_64 FreeBSD-*-aarch64
+WEBKIT_JIT_MACHINE_PLATFORMS+=	Linux-*-x86_64 Linux-*-arm*
 WEBKIT_JIT_MACHINE_PLATFORMS+=	Linux-*-aarch64 Linux-*-mips*
-WEBKIT_JIT_MACHINE_PLATFORMS+=	NetBSD-*-x86_64 NetBSD-*-i386 NetBSD-*-arm*
-WEBKIT_JIT_MACHINE_PLATFORMS+=	NetBSD-*-aarch64 NetBSD-*-mips*
+WEBKIT_JIT_MACHINE_PLATFORMS+=	NetBSD-*-x86_64 NetBSD-*-aarch64
 
 .if !empty(WEBKIT_JIT_MACHINE_PLATFORMS:@.PLAT.@${MACHINE_PLATFORM:M${.PLAT.}}@)
 PKG_SUGGESTED_OPTIONS+=	webkit-jit
@@ -32,6 +34,7 @@ PKG_SUGGESTED_OPTIONS+=	webkit-jit
 #
 .if !empty(PKG_OPTIONS:Mwebkit-jit)
 CMAKE_ARGS+=	-DENABLE_JIT=ON
+CMAKE_ARGS+=	-DENABLE_C_LOOP=OFF
 .else
 CMAKE_ARGS+=	-DENABLE_JIT=OFF
 .endif
