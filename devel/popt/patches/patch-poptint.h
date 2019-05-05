6a71e2baf98ace67d98aa98d4b14aa8d07ad8d14
rjs <rjs@pkgsrc.org>
2019-05-05 23:16:06 +0000

    Fix compat symbol link warning.

--- devel/popt/patches/patch-poptint.h
+++ devel/popt/patches/patch-poptint.h
@@ -1,15 +1,19 @@
-$NetBSD: patch-poptint.h,v 1.1 2011/04/01 21:28:32 tez Exp $
+$NetBSD: patch-poptint.h,v 1.2 2019/05/05 23:16:06 rjs Exp $
 
 Don't include stdint.h if it does not exist.  Fixes PR#44770
 
---- poptint.h.orig	2011-04-01 21:07:17.593937200 +0000
+--- poptint.h.orig	2010-01-19 00:39:10.000000000 +0000
 +++ poptint.h
-@@ -9,7 +9,9 @@
+@@ -9,7 +9,13 @@
  #ifndef H_POPTINT
  #define H_POPTINT
  
 +#ifdef HAVE_STDINT_H
  #include <stdint.h>
++#endif
++
++#ifdef HAVE_STDLIB_H
++#include <stdlib.h>
 +#endif
  
  /**
