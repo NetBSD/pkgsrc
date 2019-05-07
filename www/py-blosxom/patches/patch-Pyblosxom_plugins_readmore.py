4c159fea2b0c578d2731e21e41e11dda414a72ba
hauke <hauke@pkgsrc.org>
2019-05-07 14:26:06 +0000

    Make sure the readmore plugin produces valid html - browsers are not as tolerant as they used to be.
    The current version does not yet support python3, so mandate 2.7.

--- www/py-blosxom/patches/patch-Pyblosxom_plugins_readmore.py
+++ www/py-blosxom/patches/patch-Pyblosxom_plugins_readmore.py
@@ -1,10 +1,30 @@
-$NetBSD: patch-Pyblosxom_plugins_readmore.py,v 1.1 2015/05/22 14:01:16 hauke Exp $
+$NetBSD: patch-Pyblosxom_plugins_readmore.py,v 1.2 2019/05/07 14:26:06 hauke Exp $
+
+Try to make sure the truncated text is valid HTML
 
 Use the current flavour before the default one, if available.
 
 --- Pyblosxom/plugins/readmore.py.orig	2013-07-31 00:53:58.000000000 +0000
 +++ Pyblosxom/plugins/readmore.py
-@@ -209,7 +209,11 @@ def cb_story(args):
+@@ -194,8 +194,15 @@ def cb_story(args):
+     breakpoint = config.get("readmore_breakpoint", READMORE_BREAKPOINT)
+     template = config.get("readmore_template", READMORE_TEMPLATE)
+ 
+-    # check to see if the breakpoint is in the body.
+-    match = re.search(breakpoint, entry["body"])
++    """
++    Check to see if the breakpoint is in the body.
++
++    Since it might have been wrapped in html tags by a markdown
++    plugin, grab everything from the end of breakpoint up to, but
++    excluding, either the first opening tag, or newline.
++    """
++    match = re.search('(' + breakpoint + ')(.*?)(<[ ]*?[^/].+|[\n])',
++                      entry["body"])
+ 
+     # if not, return because we don't have to do anything
+     if not match:
+@@ -209,7 +216,11 @@ def cb_story(args):
      # otherwise we replace the breakpoint with the template
      base_url = config["base_url"]
      file_path = entry["file_path"]
@@ -17,3 +37,9 @@ Use the current flavour before the default one, if available.
      url = '%s/%s.%s' % (base_url, file_path, flavour)
  
      link = (template % {"url": url,
+@@ -218,4 +229,4 @@ def cb_story(args):
+                         "flavour": flavour})
+ 
+     entry["just_summary"] = 1
+-    entry["body"] = entry["body"][:match.start()] + link
++    entry["body"] = entry["body"][:match.start(1)] + link + str(match.group(2))
