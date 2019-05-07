7eb08574c3a22b54370af4b61c2638dbd81bcbaa
jaapb <jaapb@pkgsrc.org>
2019-05-07 11:29:34 +0000

    Updated devel/ocaml-dune to version 1.9.3.
    Many mostly minor changes and bugfixes, nothing that breaks backward
    compatibility. A full list is available in the CHANGES file.
    
    The package now also builds the release version, which results in a lot
    fewer warnings, and actually is necessary for building with OCaml 4.08.

--- devel/ocaml-dune/buildlink3.mk
+++ devel/ocaml-dune/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/03/05 16:11:03 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/05/07 11:29:34 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-dune
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-dune
 OCAML_DUNE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-dune+=	ocaml-dune>=1.7.0
-BUILDLINK_ABI_DEPENDS.ocaml-dune+=	ocaml-dune>=1.7.3
+BUILDLINK_ABI_DEPENDS.ocaml-dune+=	ocaml-dune>=1.9.3
 BUILDLINK_PKGSRCDIR.ocaml-dune?=	../../devel/ocaml-dune
 .endif	# OCAML_DUNE_BUILDLINK3_MK
 
