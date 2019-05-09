8f65381b8f6ce72db055c3d2ad46885076eeecd4
adam <adam@pkgsrc.org>
2019-05-09 09:47:34 +0000

    texlive: updated to 20190410
    20190410:
    
    Kpathsea: More consistent brace expansion and path splitting; new variable TEXMFDOTDIR instead of hard-coded . in paths allows for easily searching additional or sub-directories (see comments in texmf.cnf).
    
    epTEX, eupTEX: New primitives \readpapersizespecial and \expanded.
    
    LuaTEX: Lua 5.3 now used, with concomitant arithmetic and interface changes. The homegrown library pplib is used to read pdf files, thus eliminating the dependency on poppler (and the need for C++); Lua interface changed accordingly.
    
    MetaPost: r-mpost command name recognized as an alias for invocation with the –restricted option, and added to the list of restricted commands available by default. Minimum precision now 2 for decimal and binary mode. Binary mode no longer available in MPlib but still available in standalone MetaPost.
    
    pdfTEX: New primitive \expanded; if new primitive parameter \pdfomitcharset is set to 1, the /CharSet string omitted from the PDF output, since it cannot feasibly be guaranteed correct, as required by PDF/A-2 and PDF/A-3.
    
    XeTEX: New primitives \expanded, \creationdate, \elapsedtime, \filedump, \filemoddate, \filesize, \resettimer, \normaldeviate, \uniformdeviate, \randomseed; extend \Ucharcat to produce active characters.
    
    tlmgr: Support curl as a download program; use lz4 and gzip before xz for local backups, if available; prefer system-provided binaries over binaries provided with TEX Live for compressor and download programs, unless the environment variable TEXLIVE_PREFER_OWN is set.
    
    install-tl: New option -gui (with no argument) is the default on Windows and Macs, and invokes a new Tcl/TK GUI (see sections 1.3 and 3.1.6).
    
    Utilities:
    
    cwebbin (https://ctan.org/pkg/cwebbin) is now the CWEB implementation in TEX Live, with support for more language dialects, and including the ctwill program to make mini-indexes.
    chkdvifont: report font information from DVI files, also from tfm/ofm, vf, gf, pk.
    dvispc: make a DVI file page-independent with respect to specials.
    MacTEX: x86_64-darwin now supports 10.12 and higher (Sierra, High Sierra, Mojave); x86_64-darwinlegacy still supports 10.6 and newer. The spell checker Excalibur is no longer included, since it requires 32-bit support.
    
    Platforms: removed sparc-solaris.

--- print/luatex/patches/patch-libs_lua53_lua53-src_src_luaconf.h
+++ print/luatex/patches/patch-libs_lua53_lua53-src_src_luaconf.h
@@ -1,26 +1,25 @@
-$NetBSD: patch-ac,v 1.7 2016/06/16 04:06:19 markd Exp $
+$NetBSD: patch-libs_lua53_lua53-src_src_luaconf.h,v 1.1 2019/05/09 09:47:36 adam Exp $
 
 Prepare @PREFIX@ for substituting the correct one in from the pkgsrc Makefile.
-
 Solaris portability fix.
 
---- libs/lua52/lua52-src/src/luaconf.h.orig	2013-04-05 10:58:37.000000000 +0000
-+++ libs/lua52/lua52-src/src/luaconf.h
-@@ -102,7 +102,7 @@
+--- libs/lua53/lua53-src/src/luaconf.h.orig	2018-09-05 21:30:41.000000000 +0000
++++ libs/lua53/lua53-src/src/luaconf.h
+@@ -200,7 +200,7 @@
+ 
  #else			/* }{ */
  
- #define LUA_VDIR	LUA_VERSION_MAJOR "." LUA_VERSION_MINOR "/"
 -#define LUA_ROOT	"/usr/local/"
 +#define LUA_ROOT	"@PREFIX@"
- #define LUA_LDIR	LUA_ROOT "share/lua/" LUA_VDIR
- #define LUA_CDIR	LUA_ROOT "lib/lua/" LUA_VDIR
+ #define LUA_LDIR	LUA_ROOT "share/lua/" LUA_VDIR "/"
+ #define LUA_CDIR	LUA_ROOT "lib/lua/" LUA_VDIR "/"
  #define LUA_PATH_DEFAULT  \
-@@ -177,7 +177,7 @@
+@@ -276,7 +276,7 @@
  ** default definition.
  */
  #if defined(__GNUC__) && ((__GNUC__*100 + __GNUC_MINOR__) >= 302) && \
 -    defined(__ELF__)		/* { */
 +    defined(__ELF__) && !defined(__sun)		/* { */
  #define LUAI_FUNC	__attribute__((visibility("hidden"))) extern
- #define LUAI_DDEC	LUAI_FUNC
- #define LUAI_DDEF	/* empty */
+ #else				/* }{ */
+ #define LUAI_FUNC	extern
