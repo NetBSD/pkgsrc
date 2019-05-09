3c9eb1f4b43e37cd7d99945c2078f36f96d72ebc
adam <adam@pkgsrc.org>
2019-05-09 09:28:22 +0000

    poppler: updated to 0.76.1
    poppler-0.76.1:
    
    core:
     * Make the mul tables be calculated at compile time with constexpr.
     * splash: Fix compile with SPLASH_CMYK enabled
     * Some typo fixing in error messages
    
    qt5:
     * Fix regression in annotation handling
    
    build system:
     * Fix some typos in build system output and comments
    
    
    poppler-0.76.0:
    
    core:
     * Fix regression on case-insensitive search.
     * Remove GooList, use std::vector instead
     * Fix radiobutton reporting wrong state.
     * Handle UTF16-LE strings
     * Don't error out if there's no DA in FreeText annotation
     * cairo: Compute correct coverage values for box filter.
     * cairo: Constrain number of cycles in rescale filter.
     * Read more fields from ViewerPreferences
     * Introduce and use Ref::INVALID
     * Fix crashes in broken files
     * Fix mismatched free/delete
     * Add missing include guards
    
    utils:
     * pdftohtml: Properly initialize HtmlOutputDev::page to avoid SIGSEGV upon error exit.
    
    
    poppler-0.75.0:
    
    core:
     * Fix rendering of some annotations
     * Fix crashes in broken files
     * Small internal code improvements
    
    cpp:
     * Improve documentation
     * tests: Add showing version information to poppler-dump
    
    utils:
     * pdfattach: new util
     * pdftohtml: add -dataurls parameter
     * pdftoppm: add -sep and -forcenum parameters
     * pdftohtml: make singleHtml and stout not mutually exclusive
     * pdfsig: fix use after free

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.72 2019/03/27 11:18:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.73 2019/05/09 09:28:22 adam Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler
 POPPLER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
-BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.74.0
+BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.76.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 GCC_REQD+=	4.9
