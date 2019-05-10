72ea4f5c07f67d3ead6cc82a11ffd0ae80691af2
bacon <bacon@pkgsrc.org>
2019-05-10 16:19:22 +0000

    biology/vsearch: import vsearch-2.13.3
    VSEARCH supports de novo and reference based chimera detection,
    clustering, full-length and prefix dereplication, rereplication,
    reverse complementation, masking, all-vs-all pairwise global
    alignment, exact and global alignment searching, shuffling,
    subsampling and sorting. It also supports FASTQ file analysis,
    filtering, conversion and merging of paired-end reads.
    
    The aim of this project is to create an alternative to the USEARCH
    tool developed by Robert C. Edgar (2010).

---
+++ biology/vsearch/patches/patch-src_city.cc
@@ -0,0 +1,20 @@
+$NetBSD: patch-src_city.cc,v 1.1 2019/05/10 16:19:22 bacon Exp $
+
+# Portability
+
+--- src/city.cc.orig	2019-04-30 11:57:32 UTC
++++ src/city.cc
+@@ -60,6 +60,13 @@ static uint32 UNALIGNED_LOAD32(const char *p) {
+ #define bswap_32(x) OSSwapInt32(x)
+ #define bswap_64(x) OSSwapInt64(x)
+ 
++#elif defined(__FreeBSD__)
++
++#include <sys/endian.h>
++
++#define bswap_32(x) bswap32(x)
++#define bswap_64(x) bswap64(x)
++
+ #elif defined(__NetBSD__)
+ 
+ #include <sys/types.h>
