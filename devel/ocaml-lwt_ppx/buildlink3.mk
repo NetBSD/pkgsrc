130ae342658f3bca23af66e458aab54799d682f0
jaapb <jaapb@pkgsrc.org>
2019-05-13 11:41:42 +0000

    Updated devel/ocaml-lwt_ppx to version 1.2.2.
    Update associated with the update of ocaml-lwt to version 4.2.1.

--- devel/ocaml-lwt_ppx/buildlink3.mk
+++ devel/ocaml-lwt_ppx/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2019/03/05 17:42:36 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2019/05/13 11:41:42 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt_ppx
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-lwt_ppx
 OCAML_LWT_PPX_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-lwt_ppx+=	ocaml-lwt_ppx>=1.1.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt_ppx+=	ocaml-lwt_ppx>=1.2.1nb3
+BUILDLINK_ABI_DEPENDS.ocaml-lwt_ppx+=	ocaml-lwt_ppx>=1.2.2
 BUILDLINK_PKGSRCDIR.ocaml-lwt_ppx?=	../../devel/ocaml-lwt_ppx
 
 .include "../../devel/ocaml-lwt/buildlink3.mk"
