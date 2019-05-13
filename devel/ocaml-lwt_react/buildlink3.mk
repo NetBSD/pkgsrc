40ceb8a540ee4890e1cc773fd98bf4da24bf881c
jaapb <jaapb@pkgsrc.org>
2019-05-13 11:39:40 +0000

    Updated devel/ocaml-lwt_react to version 1.1.2.
    Update associated with the update of ocaml-lwt to version 4.2.1

--- devel/ocaml-lwt_react/buildlink3.mk
+++ devel/ocaml-lwt_react/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2019/03/05 17:43:19 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2019/05/13 11:39:40 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt_react
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-lwt_react
 OCAML_LWT_REACT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-lwt_react+=	ocaml-lwt_react>=1.1.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt_react+=	ocaml-lwt_react>=1.1.1nb4
+BUILDLINK_ABI_DEPENDS.ocaml-lwt_react+=	ocaml-lwt_react>=1.1.2
 BUILDLINK_PKGSRCDIR.ocaml-lwt_react?=	../../devel/ocaml-lwt_react
 
 .include "../../devel/ocaml-lwt/buildlink3.mk"
