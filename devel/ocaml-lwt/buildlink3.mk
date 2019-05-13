8629cbbf94f96f409ac16e69809a12879400bdc2
jaapb <jaapb@pkgsrc.org>
2019-05-13 11:38:45 +0000

    Updated devel/ocaml-lwt to version 4.2.1.
    Minor changes, the most important of which is introducing ocaml 4.08
    compatibility. The configure script has also been moved and is now called
    during compilation.

--- devel/ocaml-lwt/buildlink3.mk
+++ devel/ocaml-lwt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2019/03/05 16:56:16 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2019/05/13 11:38:45 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-lwt
 OCAML_LWT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-lwt+=	ocaml-lwt>=4.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt+=	ocaml-lwt>=4.1.0nb3
+BUILDLINK_ABI_DEPENDS.ocaml-lwt+=	ocaml-lwt>=4.2.1
 BUILDLINK_PKGSRCDIR.ocaml-lwt?=		../../devel/ocaml-lwt
 
 #.include "../../devel/libev/buildlink3.mk"
