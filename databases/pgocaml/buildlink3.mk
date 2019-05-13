9d73b8693cecaa190e05c3e8a653853837f4fde4
jaapb <jaapb@pkgsrc.org>
2019-05-13 12:12:33 +0000

    Updated databases/pgocaml to version 3.2.
    This includes a general modernisation of the package, PPX support and
    a lot of minor changes.

--- databases/pgocaml/buildlink3.mk
+++ databases/pgocaml/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/07/19 13:47:36 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2019/05/13 12:12:33 jaapb Exp $
 
 BUILDLINK_TREE+=	pgocaml
 
 .if !defined(PGOCAML_BUILDLINK3_MK)
 PGOCAML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.pgocaml+=	pgocaml>=2.3
-BUILDLINK_ABI_DEPENDS.pgocaml+=	pgocaml>=2.3nb7
+BUILDLINK_API_DEPENDS.pgocaml+=	pgocaml>=3.0
+BUILDLINK_ABI_DEPENDS.pgocaml+=	pgocaml>=3.2
 BUILDLINK_PKGSRCDIR.pgocaml?=	../../databases/pgocaml
 
 .include "../../devel/ocaml-re/buildlink3.mk"
