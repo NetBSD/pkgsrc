eee89bc0f85388a8e30db941c48d87725ddb75d7
jaapb <jaapb@pkgsrc.org>
2019-05-14 16:01:17 +0000

    "Updated" package x11/ocaml-graphics to version 2.0.0.
    The package itself hasn't changed, it is just distributed differently;
    as a separate package rather than with ocaml itself. The version has
    thus regressed from OCaml's 4.07 to an independent 2.0.0, which means
    that people will have to manually "upgrade" the package.

--- x11/ocaml-graphics/buildlink3.mk
+++ x11/ocaml-graphics/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.24 2018/11/12 16:10:17 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2019/05/14 16:01:17 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-graphics
 
 .if !defined(OCAML_GRAPHICS_BUILDLINK3_MK)
 OCAML_GRAPHICS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-graphics+=	ocaml-graphics>=3.08.2
-BUILDLINK_ABI_DEPENDS.ocaml-graphics+=	ocaml-graphics>=4.07.1nb13
+BUILDLINK_API_DEPENDS.ocaml-graphics+=	ocaml-graphics>=2.0.0
 BUILDLINK_PKGSRCDIR.ocaml-graphics?=	../../x11/ocaml-graphics
 
 .include "../../lang/ocaml/buildlink3.mk"
-.endif # OCAML_GRAPHICS_BUILDLINK3_MK
+.include "../../x11/libX11/buildlink3.mk"
+.endif	# OCAML_GRAPHICS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ocaml-graphics
