fc4dcb28b7553070dda707bf3477e1b6c268d329
nia <nia@pkgsrc.org>
2019-05-17 09:59:18 +0000

    inspircd3: Update to 3.1.0
    Changes:
    
    * Added a configure option, --disable-auto-extras, which disables
    automatically enabling extras for which the dependencies are
    available.
    * Added support for disabling a STS policy for users in specific
    connect classes.
    * Added support for the IRCv3 Message IDs specification.
    * Fixed a crash in the silence module on some older versions of
    GCC.
    * Fixed linking against v2 servers running the ASCII case mapping
    module from inspircd-extras.
    * Fixed an inverted condition in the commonchans module which made
    the module not work.
    * Fixed configure not failing when invalid options were passed to
    it.
    * Fixed pending X-lines only being applied to a single user.
    * Fixed servers not specifying whether they are hidden. If no
    visibility is specified then servers default to the visibility of
    their parent server.

--- chat/inspircd3/options.mk
+++ chat/inspircd3/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2019/05/03 17:14:27 nia Exp $
+# $NetBSD: options.mk,v 1.2 2019/05/17 09:59:18 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.inspircd
 PKG_SUPPORTED_OPTIONS=		gnutls geoip openssl mysql mbedtls ldap pcre pgsql sqlite3
@@ -9,72 +9,54 @@ PLIST_VARS+=			gnutls geoip openssl mysql mbedtls ldap pcre pgsql sqlite3
 
 .if !empty(PKG_OPTIONS:Mgeoip)
 PLIST.geoip=		yes
-INSPIRCD_EXTRAS_ON+=	m_geo_maxmind.cpp
+INSPIRCD_EXTRAS+=	m_geo_maxmind.cpp
 .include "../../geography/libmaxminddb/buildlink3.mk"
-.else
-INSPIRCD_EXTRAS_OFF+=	m_geo_maxmind.cpp
 .endif
 
 .if !empty(PKG_OPTIONS:Mgnutls)
 PLIST.gnutls=		yes
-INSPIRCD_EXTRAS_ON+=	m_ssl_gnutls.cpp
+INSPIRCD_EXTRAS+=	m_ssl_gnutls.cpp
 .include "../../security/gnutls/buildlink3.mk"
-.else
-INSPIRCD_EXTRAS_OFF+=	m_ssl_gnutls.cpp
 .endif
 
 .if !empty(PKG_OPTIONS:Mopenssl)
 PLIST.openssl=		yes
-INSPIRCD_EXTRAS_ON+=	m_ssl_openssl.cpp
+INSPIRCD_EXTRAS+=	m_ssl_openssl.cpp
 .include "../../security/openssl/buildlink3.mk"
-.else
-INSPIRCD_EXTRAS_OFF+=	m_ssl_openssl.cpp
 .endif
 
 .if !empty(PKG_OPTIONS:Mmbedtls)
 PLIST.mbedtls=		yes
-INSPIRCD_EXTRAS_ON+=	m_ssl_mbedtls.cpp
+INSPIRCD_EXTRAS+=	m_ssl_mbedtls.cpp
 .include "../../security/mbedtls/buildlink3.mk"
-.else
-INSPIRCD_EXTRAS_OFF+=	m_ssl_mbedtls.cpp
 .endif
 
 .if !empty(PKG_OPTIONS:Mmysql)
 PLIST.mysql=		yes
-INSPIRCD_EXTRAS_ON+=	m_mysql.cpp
+INSPIRCD_EXTRAS+=	m_mysql.cpp
 .include "../../mk/mysql.buildlink3.mk"
-.else
-INSPIRCD_EXTRAS_OFF+=	m_mysql.cpp
 .endif
 
 .if !empty(PKG_OPTIONS:Mldap)
 PLIST.ldap=		yes
-INSPIRCD_EXTRAS_ON+=	m_mysql.cpp
+INSPIRCD_EXTRAS+=	m_ldap.cpp
 .include "../../databases/openldap-client/buildlink3.mk"
-.else
-INSPIRCD_EXTRAS_OFF+=	m_mysql.cpp
 .endif
 
 .if !empty(PKG_OPTIONS:Mpcre)
 PLIST.pcre=		yes
-INSPIRCD_EXTRAS_ON+=	m_regex_pcre.cpp
+INSPIRCD_EXTRAS+=	m_regex_pcre.cpp
 .include "../../devel/pcre/buildlink3.mk"
-.else
-INSPIRCD_EXTRAS_OFF+=	m_regex_pcre.cpp
 .endif
 
 .if !empty(PKG_OPTIONS:Mpgsql)
 PLIST.pgsql=		yes
-INSPIRCD_EXTRAS_ON+=	m_pgsql.cpp
+INSPIRCD_EXTRAS+=	m_pgsql.cpp
 .include "../../mk/pgsql.buildlink3.mk"
-.else
-INSPIRCD_EXTRAS_OFF+=	m_pgsql.cpp
 .endif
 
 .if !empty(PKG_OPTIONS:Msqlite3)
 PLIST.sqlite3=		yes
-INSPIRCD_EXTRAS_ON+=	m_sqlite3.cpp
+INSPIRCD_EXTRAS+=	m_sqlite3.cpp
 .include "../../databases/sqlite3/buildlink3.mk"
-.else
-INSPIRCD_EXTRAS_OFF+=	m_sqlite3.cpp
 .endif
