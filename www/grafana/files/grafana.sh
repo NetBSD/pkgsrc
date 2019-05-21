9cb6afd5f24ade680ab921012ab9333e344f2e00
adam <adam@pkgsrc.org>
2019-05-21 08:32:04 +0000

    grafana: updated to 6.1.6
    6.1.6:
    Features / Enhancements
    Security: Bump jQuery to 3.4.0
    Bug Fixes
    Playlist: Fix loading dashboards by tag.
    
    6.1.5:
    Security: Urgent security patch release.
    
    6.1.4:
    Bug Fixes
    DataPanel: Added missing built-in interval variables to scopedVars.
    Explore: Adds maxDataPoints to data source query options .
    Explore: Fixes so intervals are recalculated on run query.
    Heatmap: Fix for empty graph when panel is too narrow.
    Heatmap: Fixed auto decimals when bucket name is not number.
    QueryInspector: Now shows error responses again.
    
    6.1.3:
    Bug Fixes
    Graph: Fixed auto decimals in legend values for some units like ms and s.
    Graph: Fixed png rendering with legend to the right.
    Singlestat: Use decimals when manually specified.
    UI Switch: Fix broken UI switches. Fixes Default Data Source switch, Explore Logs switches, Gauge option switches.
    
    6.1.2:
    Bug Fixes
    Graph: Fixed series legend color for hidden series.
    Graph: Fixed tooltip highlight on white theme.
    Styles: Fixed menu hover highlight border.
    Singlestat Panel: Correctly use the override decimals.
    
    6.1.1:
    Bug Fixes
    Alerting: Notification channel http api fixes.
    Graphite: Editing graphite query function now works again.
    Playlist: Kiosk & auto fit panels modes are working normally again .
    QueryEditors: Toggle edit mode now always work on slower computers.
    
    6.1.0:
    Bug Fixes
    CloudWatch: Fix for dimension value list when changing dimension key.
    Graphite: Editing function arguments now works again.
    InfluxDB: Fix tag names with periods in alert evaluation.
    PngRendering: Fix for panel height & title centering .
    Templating: Fix for editing query variables.

--- www/grafana/files/grafana.sh
+++ www/grafana/files/grafana.sh
@@ -6,7 +6,7 @@
 
 if [ -f /etc/rc.subr ]
 then
-        . /etc/rc.subr
+	. /etc/rc.subr
 fi
 
 name="grafana"
@@ -16,7 +16,7 @@ grafana_group="@GRAFANA_GROUP@"
 grafana_chdir="@PREFIX@/share/grafana"
 pidfile="@VARBASE@/lib/grafana/data/grafana.pid"
 command="@PREFIX@/bin/grafana-server"
-command_args="-homepath ${grafana_chdir} -config /usr/pkg/etc/grafana.conf -pidfile ${pidfile} < /dev/null > /dev/null 2>&1 &"
+command_args="-homepath ${grafana_chdir} -config @PKG_SYSCONFDIR@/grafana.conf -pidfile ${pidfile} < /dev/null > /dev/null 2>&1 &"
 
 load_rc_config $name
 run_rc_command "$1"
