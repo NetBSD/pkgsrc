122d791da23e7d4c430991deb605259dc9ee02c9
rillig <rillig@pkgsrc.org>
2019-05-22 20:47:05 +0000

    mk/tools: in the wrapper log, quote arguments containing = naturally

--- mk/tools/shquote.sh
+++ mk/tools/shquote.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# $NetBSD: shquote.sh,v 1.1 2019/03/24 11:29:19 rillig Exp $
+# $NetBSD: shquote.sh,v 1.2 2019/05/22 20:47:05 rillig Exp $
 
 # Quotes all shell meta characters from $1 and writes the result to $shquoted.
 shquote()
@@ -7,13 +7,27 @@ shquote()
 	shquoted=$1
 	case $shquoted in
 	*\'*)
-		shquoted=`$tools_wrapper_sed -e 's,'\'','\''\\\\'\'''\'',g' <<EOF
+		# replace each ' with '\''
+		shquoted=`$tools_wrapper_sed -e "s,','\\\\\\\\'',g" <<EOF
 $shquoted
 EOF`
 	esac
 
 	case $shquoted in
-	*[!!%+,\-./0-9:=@A-Z_a-z]*|'')
+	(*[!!%+,\-./0-9:=@A-Z_a-z]*|'')
+
+		# Move the single quote after the first equals sign, so that
+		# macro definitions look like -DMACRO='"value"' instead of
+		# the less common '-DMACRO="value"'.
+		case $shquoted in
+		(*=*)	lhs=${shquoted%%=*}
+			case $lhs in
+			(*[!!%+,\-./0-9:=@A-Z_a-z]*|'') ;;
+			(*)	shquoted="$lhs='${shquoted#*=}'"
+				return
+			esac
+		esac
+
 		shquoted="'$shquoted'"
 	esac
 }
