7df0c70635cd498182dcf255dac3ed542e47d78d
hauke <hauke@pkgsrc.org>
2019-05-24 08:38:28 +0000

    Upgrade mail/courier-unicode to v2.1.
    There are currently no pkgsrc packages that depend on this one, so
    we don't bother with keeping around the old version.
    
    From the changelog:
    
    2018-07-13  Sam Varshavchik  <mrsam@courier-mta.com>
    
            * unicode.c: Fix error and validation of valid modified-utf7
            encoding.
    
    2018-07-11  Sam Varshavchik  <mrsam@courier-mta.com>
    
            * unicode.c: Implement unicode_x_smap_modutf8 pseudo-encoding.
    
    2018-04-27  Sam Varshavchik  <mrsam@courier-mta.com>
    
            * gcc 8 update, fix assertions. libtool and toolchain updates.
    
    2.0
    
    2017-03-06  Sam Varshavchik  <mrsam@courier-mta.com>
    
            * Migrate to C++11: remove unicode_char, use char32_t. Replace
            std::vector<unicode_char> with std::u32string.

--- mail/courier-unicode/buildlink3.mk
+++ mail/courier-unicode/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/02/12 02:30:07 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/05/24 08:38:28 hauke Exp $
 
 BUILDLINK_TREE+=	courier-unicode
 
 .if !defined(COURIER_UNICODE_BUILDLINK3_MK)
 COURIER_UNICODE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.courier-unicode+=		courier-unicode>=1.2
-BUILDLINK_ABI_DEPENDS.courier-unicode+=		courier-unicode>=1.2
-BUILDLINK_PKGSRCDIR.courier-unicode?=		../../mail/courier-unicode
+BUILDLINK_API_DEPENDS.courier-unicode+=	courier-unicode>=2.0
+BUILDLINK_ABI_DEPENDS.courier-unicode+=	courier-unicode>=2.1
+BUILDLINK_PKGSRCDIR.courier-unicode?=	../../mail/courier-unicode
 
 .endif # COURIER_UNICODE_BUILDLINK3_MK
 
