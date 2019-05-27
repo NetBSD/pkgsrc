cf5db0bc6c2f91f8a4770def65e450513c290c8c
bsiegert <bsiegert@pkgsrc.org>
2019-05-27 14:33:10 +0000

    Update go111 to 1.11.10.
    This release includes fixes to the compiler, the linker, the go command, the
    runtime, and the os package.
    
    While here, remove pkg/bootstrap from the package, as it is only used
    for bootstrapping.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.61 2019/04/16 18:30:27 bsiegert Exp $
+# $NetBSD: version.mk,v 1.62 2019/05/27 14:33:10 bsiegert Exp $
 
 CTF_SUPPORTED=		no
 SSP_SUPPORTED=		no
@@ -7,7 +7,7 @@ STRIP_DEBUG_SUPPORTED=	no
 .include "../../mk/bsd.prefs.mk"
 
 GO112_VERSION=	1.12.4
-GO111_VERSION=	1.11.9
+GO111_VERSION=	1.11.10
 GO110_VERSION=	1.10.8
 GO19_VERSION=	1.9.7
 GO14_VERSION=	1.4.3
