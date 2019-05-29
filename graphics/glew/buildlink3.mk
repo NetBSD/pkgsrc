a8c37c7e0416c4ba2f2e678c8a34c919c48c0c5f
adam <adam@pkgsrc.org>
2019-05-29 19:52:48 +0000

    Revbump after updating graphics/glew

--- graphics/glew/buildlink3.mk
+++ graphics/glew/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2015/04/25 14:23:11 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2019/05/29 19:52:48 adam Exp $
 
 BUILDLINK_TREE+=	glew
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	glew
 GLEW_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.glew+=	glew>=1.3.4
-BUILDLINK_ABI_DEPENDS.glew+=	glew>=1.11.0nb1
+BUILDLINK_ABI_DEPENDS.glew+=	glew>=2.1.0
 BUILDLINK_PKGSRCDIR.glew?=	../../graphics/glew
 
 .include "../../mk/bsd.fast.prefs.mk"
