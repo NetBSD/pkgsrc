64417d7f7b0ee8fc3794e92f99881ca5f563167c
adam <adam@pkgsrc.org>
2019-05-29 19:52:48 +0000

    Revbump after updating graphics/glew

--- x11/slop/buildlink3.mk
+++ x11/slop/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/07/28 15:19:59 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/05/29 19:52:48 adam Exp $
 
 BUILDLINK_TREE+=	slop
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	slop
 SLOP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.slop+=	slop>=7.4
+BUILDLINK_ABI_DEPENDS.slop?=	slop>=7.4nb3
 BUILDLINK_PKGSRCDIR.slop?=	../../x11/slop
 
 .include "../../graphics/glew/buildlink3.mk"
