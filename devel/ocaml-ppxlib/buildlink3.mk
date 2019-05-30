708248608c3c40ba011c08d8ba2c6ba4c7fa3aab
jaapb <jaapb@pkgsrc.org>
2019-05-30 09:29:23 +0000

    Updated devel/ocaml-ppxlib to version 0.8.0.
    Minor changes, most notably addition of support for OCaml 4.08.

--- devel/ocaml-ppxlib/buildlink3.mk
+++ devel/ocaml-ppxlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2019/03/05 17:57:36 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2019/05/30 09:29:23 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppxlib
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppxlib
 OCAML_PPXLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.3.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.5.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppxlib+=	ocaml-ppxlib>=0.8.0
 BUILDLINK_PKGSRCDIR.ocaml-ppxlib?=	../../devel/ocaml-ppxlib
 
 .endif	# OCAML_PPXLIB_BUILDLINK3_MK
