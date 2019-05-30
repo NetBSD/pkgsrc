81b223411ec92d5b49ad4ad8a83a4d19ce41a310
jaapb <jaapb@pkgsrc.org>
2019-05-30 10:00:07 +0000

    Added dependency to devel/ocaml-rresult. Revbump.
    Dependency in question is ocamlbuild, as per PR pkg/54248.

--- devel/ocaml-rresult/buildlink3.mk
+++ devel/ocaml-rresult/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/12 17:49:59 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/05/30 10:00:07 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-rresult
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-rresult
 OCAML_RRESULT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-rresult+=	ocaml-rresult>=0.6.0
+BUILDLINK_ABI_DEPENDS.ocaml-rresult+=	ocaml-rresult>=0.6.0nb1
 BUILDLINK_PKGSRCDIR.ocaml-rresult?=	../../devel/ocaml-rresult
 
 .endif	# OCAML_RRESULT_BUILDLINK3_MK
