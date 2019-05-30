491c16075c1d44bf8b2c4ee866f45773731949c5
jaapb <jaapb@pkgsrc.org>
2019-05-30 09:37:20 +0000

    Added missing dependency to devel/ocaml-ppx_fields_conv
    Dependency is on devel/ocaml-ppxlib. This solves PR pkg/54242.

--- devel/ocaml-ppx_fields_conv/buildlink3.mk
+++ devel/ocaml-ppx_fields_conv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/12 18:18:40 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/05/30 09:37:20 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_fields_conv
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_fields_conv
 OCAML_PPX_FIELDS_CONV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_fields_conv+=	ocaml-ppx_fields_conv>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_fields_conv+=	ocaml-ppx_fields_conv>=0.12.0nb1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_fields_conv?=	../../devel/ocaml-ppx_fields_conv
 
 .include "../../devel/ocaml-fieldslib/buildlink3.mk"
