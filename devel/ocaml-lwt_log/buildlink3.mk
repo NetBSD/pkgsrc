f8e389ce330fae687dd9de1bc3a3e13b551af57d
jaapb <jaapb@pkgsrc.org>
2019-05-30 10:26:03 +0000

    Changed upstream address for devel/ocaml-lwt_log. Revbump.
    Distribution is in the ocsigen GitHub repository again. This version
    also seems to install more libraries.

--- devel/ocaml-lwt_log/buildlink3.mk
+++ devel/ocaml-lwt_log/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2019/03/05 17:16:32 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2019/05/30 10:26:03 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt_log
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-lwt_log
 OCAML_LWT_LOG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-lwt_log+=	ocaml-lwt_log>=1.1.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt_log?=	ocaml-lwt_log>=1.1.0nb4
+BUILDLINK_ABI_DEPENDS.ocaml-lwt_log?=	ocaml-lwt_log>=1.1.0nb5
 BUILDLINK_PKGSRCDIR.ocaml-lwt_log?=	../../devel/ocaml-lwt_log
 
 .include "../../devel/ocaml-lwt/buildlink3.mk"
