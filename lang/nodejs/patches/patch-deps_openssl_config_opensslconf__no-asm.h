10db2b8bc0ece814c64cabb613a1c379cb7f9314
adam <adam@pkgsrc.org>
2019-05-31 20:52:00 +0000

    nodejs: updated to 10.16.0
    Version 10.16.0 'Dubnium' (LTS)
    
    Notable Changes
    deps:
    update ICU to 64.2
    upgrade npm to 6.9.0
    upgrade openssl sources to 1.1.1b
    upgrade to libuv 1.28.0
    events: add once method to use promises with EventEmitter
    n-api: mark thread-safe function as stable
    repl: support top-level for-await-of
    zlib: add brotli support

--- lang/nodejs/patches/patch-deps_openssl_config_opensslconf__no-asm.h
+++ lang/nodejs/patches/patch-deps_openssl_config_opensslconf__no-asm.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-deps_openssl_config_opensslconf__no-asm.h,v 1.3 2019/02/24 12:18:55 rin Exp $
+$NetBSD: patch-deps_openssl_config_opensslconf__no-asm.h,v 1.4 2019/05/31 20:52:00 adam Exp $
 
 Support NetBSD/arm,aarch64,i386,amd64 (and hopefully other ILP32 archs)
 
---- deps/openssl/config/opensslconf_no-asm.h.orig	2019-01-29 16:20:45.000000000 +0900
-+++ deps/openssl/config/opensslconf_no-asm.h	2019-02-24 10:26:51.159213732 +0900
+--- deps/openssl/config/opensslconf_no-asm.h.orig	2019-05-28 21:32:16.000000000 +0000
++++ deps/openssl/config/opensslconf_no-asm.h
 @@ -4,9 +4,9 @@
  # include "./archs/linux-x32/no-asm/include/openssl/opensslconf.h"
  #elif defined(OPENSSL_LINUX) && defined(__x86_64__)
@@ -16,16 +16,14 @@ Support NetBSD/arm,aarch64,i386,amd64 (and hopefully other ILP32 archs)
  # include "./archs/linux-aarch64/no-asm/include/openssl/opensslconf.h"
  #elif defined(__APPLE__) && defined(__MACH__) && defined(__i386__)
  # include "./archs/darwin-i386-cc/no-asm/include/openssl/opensslconf.h"
-@@ -16,9 +16,10 @@
- # include "./archs/VC-WIN32/no-asm/include/openssl/opensslconf.h"
- #elif defined(_WIN32) && defined(_M_X64)
+@@ -18,9 +18,9 @@
  # include "./archs/VC-WIN64A/no-asm/include/openssl/opensslconf.h"
+ #elif defined(_WIN32) && defined(_M_ARM64)
+ # include "./archs/VC-WIN64-ARM/no-asm/include/openssl/opensslconf.h"
 -#elif (defined(__FreeBSD__) || defined(__OpenBSD__)) && defined(__i386__)
--# include "./archs/BSD-x86/no-asm/include/openssl/opensslconf.h"
++#elif (defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)) && defined(__i386__)
+ # include "./archs/BSD-x86/no-asm/include/openssl/opensslconf.h"
 -#elif (defined(__FreeBSD__) || defined(__OpenBSD__)) && defined(__x86_64__)
-+// XXX missing
-+//#elif (defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)) && defined(__i386__)
-+//# include "./archs/BSD-x86/no-asm/include/openssl/opensslconf.h"
 +#elif (defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)) && defined(__x86_64__)
  # include "./archs/BSD-x86_64/no-asm/include/openssl/opensslconf.h"
  #elif defined(__sun) && defined(__i386__)
