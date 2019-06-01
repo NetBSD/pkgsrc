aae59c130aff2d54824bed27de0ec6a41c975aa2
leot <leot@pkgsrc.org>
2019-06-01 17:39:05 +0000

    cups-filters: Update to 1.23.0
    pkgsrc changes:
     - Remove outdated comment regarding opvp: upstream issue was solved
       but they are now also disabled by default by upstream
    
    Changes:
    1.23.0
    ------
     - pdftops, mupdftoraster: Let pdftops call mutool directly and
       so that it directly outputs PostScript, eliminating the need
       to call the mupdftoraster and rastertops filters.
     - mupdftoraster: Reduced the use of temporary files from 3 to
       just one.
     - imagetopdf, imagetoraster, pdftopdf: Add support for
       print-scaling option (Issue #108, Pull request #118).
    
    1.22.6
    ------
     - rastertops: Fixed PageSize settings in the PostScript output
       (Must be in points not in pixels).
     - pdftops, mupdftoraster: Produce actual grayscale/monochrome
       PostScript (and not only instructions to print grayscale/
       monochrome) for jobs to be printed in grayscale/monochrome
       (Issue #96, Pull request #115).
     - mupdftoraster: Fixed filter not producing output at all.
     - Build system: ENABLE_DRIVERLESS got only defined with CUPS
       1.6 and newer, not with older CUPS versions (Issue #111).
     - pdftopdf, imagetopdf, imagetoraster: Silenced compiler
       warnings.
     - cups-browsed, driverless: Replaced httpConnect() calls by
       httpConnect2() calls as the former CUPS library function is
       deprecated.
     - Build system: Compile everything using the CUPS libraries
       with '-D_PPD_DEPRECATED=""' for the time being until the
       deprecated PPD API calls get replaced, to stop the flooding
       with PPD API deprecation warnings making more important
       warnings being overlooked.
     - cups-browsed: When removing a local queue on shutdown or
       when DNS-SD reports the printer as disappeared check whether
       the local queue got overwritten by an external process as
       sometimes the shutdown or disappearing event comes too close
       for cups-browsed receiving a printer-modified notification
       from CUPS before (Ubuntu bug #1731417).

--- print/cups-filters/patches/patch-filter_mupdftoraster.c
+++ print/cups-filters/patches/patch-filter_mupdftoraster.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-filter_mupdftoraster.c,v 1.1 2016/11/06 11:21:19 leot Exp $
+$NetBSD: patch-filter_mupdftoraster.c,v 1.2 2019/06/01 17:39:05 leot Exp $
 
 execvpe(3) appeared only on NetBSD 8.0, use execve(3) instead (like
 cups-filters-1.11.4).
 
---- filter/mupdftoraster.c.orig	2016-10-27 22:50:52.000000000 +0000
+--- filter/mupdftoraster.c.orig	2019-05-16 13:02:29.000000000 +0000
 +++ filter/mupdftoraster.c
-@@ -215,7 +215,7 @@ mutool_spawn (const char *filename,
+@@ -202,7 +202,7 @@ mutool_spawn (const char *filename,
  
    if ((pid = fork()) == 0) {
-     /* Execute Mutool command line ... */
+     /* Execute mutool command line ... */
 -    execvpe(filename, mutoolargv, envp);
 +    execve(filename, mutoolargv, envp);
      perror(filename);
