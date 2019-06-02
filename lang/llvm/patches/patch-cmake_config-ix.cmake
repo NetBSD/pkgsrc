f65bab12ecb4daeb9330d5d0203e7865e8e4d4ec
adam <adam@pkgsrc.org>
2019-06-02 08:35:55 +0000

    llvm: updated to 8.0.0
    8.0.0:
    Non-comprehensive list of changes in this release
    * The llvm-cov tool can now export lcov trace files using the -format=lcov option of the export command.
    * The add_llvm_loadable_module CMake macro has been removed. The add_llvm_library macro with the MODULE argument now provides the same functionality. See Writing an LLVM Pass.
    * For MinGW, references to data variables that might need to be imported from a dll are accessed via a stub, to allow the linker to convert it to a dllimport if needed.
    * Added support for labels as offsets in .reloc directive.
    * Support for precise identification of X86 instructions with memory operands, by using debug information. This supports profile-driven cache prefetching. It is enabled with the -x86-discriminate-memops LLVM Flag.
    * Support for profile-driven software cache prefetching on X86. This is part of a larger system, consisting of: an offline cache prefetches recommender, AutoFDO tooling, and LLVM. In this system, a binary compiled with -x86-discriminate-memops is run under the observation of the recommender. The recommender identifies certain memory access instructions by their binary file address, and recommends a prefetch of a specific type (NTA, T0, etc) be performed at a specified fixed offset from such an instruction’s memory operand. Next, this information needs to be converted to the AutoFDO syntax and the resulting profile may be passed back to the compiler with the LLVM flag -prefetch-hints-file, together with the exact same set of compilation parameters used for the original binary. More information is available in the RFC.
    * Windows support for libFuzzer (x86_64).

--- lang/llvm/patches/patch-cmake_config-ix.cmake
+++ lang/llvm/patches/patch-cmake_config-ix.cmake
@@ -1,14 +1,14 @@
-$NetBSD: patch-cmake_config-ix.cmake,v 1.3 2018/08/07 10:44:50 adam Exp $
+$NetBSD: patch-cmake_config-ix.cmake,v 1.4 2019/06/02 08:35:55 adam Exp $
 
 Do not generate invalid llvm-config in pkgsrc.
 
---- cmake/config-ix.cmake.orig	2018-01-02 17:53:08.000000000 +0000
+--- cmake/config-ix.cmake.orig	2018-11-26 00:03:39.000000000 +0000
 +++ cmake/config-ix.cmake
-@@ -153,7 +153,7 @@ if(NOT LLVM_USE_SANITIZER MATCHES "Memor
+@@ -129,7 +129,7 @@ if(NOT LLVM_USE_SANITIZER MATCHES "Memor
      endif()
      if(LLVM_ENABLE_TERMINFO)
        set(HAVE_TERMINFO 0)
--      foreach(library tinfo terminfo curses ncurses ncursesw)
+-      foreach(library terminfo tinfo curses ncurses ncursesw)
 +      foreach(library terminfo curses ncurses ncursesw tinfo)
          string(TOUPPER ${library} library_suffix)
          check_library_exists(${library} setupterm "" HAVE_TERMINFO_${library_suffix})
