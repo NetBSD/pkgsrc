b91b41499d9db908b7b9a75bba8abf28fdc58bc8
maya <maya@pkgsrc.org>
2019-06-03 23:09:29 +0000

    glib2: spell mnt_mntopts correctly. bump pkgrevision.
    While here, mention upstream merge requests for the things I've already
    submitted.

--- devel/glib2/patches/patch-gio_gunixmounts.c
+++ devel/glib2/patches/patch-gio_gunixmounts.c
@@ -1,7 +1,8 @@
-$NetBSD: patch-gio_gunixmounts.c,v 1.3 2019/06/03 21:24:04 maya Exp $
+$NetBSD: patch-gio_gunixmounts.c,v 1.4 2019/06/03 23:09:29 maya Exp $
 
 SunOS has sys/mntent.h but no mnt_opts.
 XXX who else uses the sys/mntent.h case?
+https://gitlab.gnome.org/GNOME/glib/merge_requests/890
 
 --- gio/gunixmounts.c.orig	2019-05-03 13:43:28.000000000 +0000
 +++ gio/gunixmounts.c
@@ -10,7 +11,7 @@ XXX who else uses the sys/mntent.h case?
                                               NULL,
                                               mntent.mnt_fstype,
 -                                             mntent.mnt_opts,
-+                                             NULL/*mntent.mnt_opts*/,
++                                             mntent.mnt_mntopts,
                                               is_read_only);
  
        return_list = g_list_prepend (return_list, mount_entry);
