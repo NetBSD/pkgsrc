03ef89494affbf94a34fbaebc2b53f19a4bc34e9
prlw1 <prlw1@pkgsrc.org>
2019-06-03 09:53:50 +0000

    Update glib2 to 2.60.2
    GLib's move to meson from autotools means we are effectively starting
    from scratch with non-linux build fixes. Support for python 2 has
    also been dropped (given that meson requires python 3). I replaced
    sysutils/gio-fam with a build option.
    
    The full lengthy list of changes is available from:
    
      https://gitlab.gnome.org/GNOME/glib/tags/2.60.2
    
    Some highlights:
    * Improve network status detection with NetworkManager (!781)
    * Add async GIO API: g_file_query_default_handler_async(), g_app_info_launch_uris_async() (#1249, #1347)
    * Add overlay support to g_resources_get_info(). (#1445)
    * Add writev() and writev_all() APIs to GOutputStream and GPollableOutputStream,
      and provide implementations of them for many subclasses. (#1431)
    * Hide bind mounts from GIO mount listings. (#1271)
    * Automatically realign data passed to `g_variant_new_from_bytes()` or
      `g_variant_new_from_data()` if it is not correctly aligned. This prevents
      misaligned accesses on architectures which don’t support them. Callers should
      still aim to correctly align data to get higher performance. (#1342)
    * Support `ld -b binary` (on platforms which support it; i.e. Linux) to provide
      large pre-compiled `GResource` resources with a fast compilation time. (#1489+* Drop Python 2 support and require Python 3.4+. See discussion on
      https://mail.gnome.org/archives/desktop-devel-list/2018-July/msg00004.html.
      (!196)
    * Various fixes to eliminate thread races, found by thread sanitizer (tsan).

--- devel/gdbus-codegen/hacks.mk
+++
@@ -1,3 +0,0 @@
-# $NetBSD: hacks.mk,v 1.4 2013/08/24 09:36:43 obache Exp $
-
-.sinclude "../../devel/glib2/hacks.mk"
--- devel/glib2/buildlink3.mk
+++ devel/glib2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.29 2018/04/23 08:26:50 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2019/06/03 09:53:50 prlw1 Exp $
 
 BUILDLINK_TREE+=	glib2
 
@@ -8,8 +8,8 @@ GLIB2_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.glib2+=	glib2>=2.4.0
 BUILDLINK_ABI_DEPENDS.glib2+=	glib2>=2.34.0
 BUILDLINK_PKGSRCDIR.glib2?=	../../devel/glib2
-BUILDLINK_INCDIRS.glib2+=	include/glib/glib-2.0
-BUILDLINK_INCDIRS.glib2+=	include/glib/gio-unix-2.0
+BUILDLINK_INCDIRS.glib2+=	include/glib-2.0
+BUILDLINK_INCDIRS.glib2+=	include/gio-unix-2.0
 BUILDLINK_INCDIRS.glib2+=	lib/glib-2.0/include
 
 TOOL_DEPENDS+=	glib2-tools-[0-9]*:../../devel/glib2-tools
@@ -19,9 +19,7 @@ TOOL_DEPENDS+=	glib2-tools-[0-9]*:../../devel/glib2-tools
 .include "../../devel/pcre/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../devel/libffi/buildlink3.mk"
-.if empty(MACHINE_PLATFORM:MIRIX-5*)
 .include "../../mk/pthread.buildlink3.mk"
-.endif
 .endif # GLIB2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-glib2
