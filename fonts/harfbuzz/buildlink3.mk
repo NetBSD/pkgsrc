91a311c28c7bdb23f694ed87c1e3315f971af652
ryoon <ryoon@pkgsrc.org>
2019-06-03 12:27:48 +0000

    Include mk file earlier to enable rust conditional

--- fonts/harfbuzz/buildlink3.mk
+++ fonts/harfbuzz/buildlink3.mk
@@ -1,9 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2018/11/12 03:49:09 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2019/06/03 12:27:48 ryoon Exp $
 
 BUILDLINK_TREE+=	harfbuzz
 
 .if !defined(HARFBUZZ_BUILDLINK3_MK)
 HARFBUZZ_BUILDLINK3_MK:=
+
+.include "../../lang/rust/platform.mk"
 
 BUILDLINK_API_DEPENDS.harfbuzz+=	harfbuzz>=2.1.1
 BUILDLINK_ABI_DEPENDS.harfbuzz+=	harfbuzz>=2.1.1
@@ -11,6 +13,9 @@ BUILDLINK_PKGSRCDIR.harfbuzz?=		../../fonts/harfbuzz
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
+.if !empty(PLATFORM_SUPPORTS_RUST:M[Yy][Ee][Ss])
+.include "../../graphics/graphite2/buildlink3.mk"
+.endif
 .endif	# HARFBUZZ_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-harfbuzz
