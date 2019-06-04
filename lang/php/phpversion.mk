1d271ce65efb7f0ca47bf94b9e30b8dbb7185d46
spz <spz@pkgsrc.org>
2019-06-04 20:47:08 +0000

    Pullup ticket #5975 - requested by taca lang/php73: security update
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.260
    - lang/php73/distinfo                                           1.8
    
    -------------------------------------------------------------------
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jun  1 15:36:02 UTC 2019
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php73: distinfo
    
       Log Message:
       lang/php73: update to 7.3.6
    
       Update to php73 to 7.3.6.
    
       30 May 2019, PHP 7.3.6
    
       - cURL:
         . Implemented FR #72189 (Add missing CURL_VERSION_* constants). (Javier
           Spagnoletti)
    
       - EXIF:
         . Fixed bug #77988 (heap-buffer-overflow on php_jpg_get16).
           (CVE-2019-11040) (Stas)
    
       - FPM:
         . Fixed bug #77934 (php-fpm kill -USR2 not working). (Jakub Zelenka)
         . Fixed bug #77921 (static.php.net doesn't work anymore). (Peter Kokot)
    
       - GD:
         . Fixed bug #77943 (imageantialias($image, false); does not work). (cmb)
         . Fixed bug #77973 (Uninitialized read in gdImageCreateFromXbm).
           (CVE-2019-11038) (cmb)
    
       - Iconv:
         . Fixed bug #78069 (Out-of-bounds read in iconv.c:_php_iconv_mime_decode()
           due to integer overflow). (CVE-2019-11039). (maris dot adam)
    
       - JSON:
         . Fixed bug #77843 (Use after free with json serializer). (Nikita)
    
       - Opcache:
         . Fixed possible crashes, because of inconsistent PCRE cache and opcache
           SHM reset. (Alexey Kalinin, Dmitry)
    
       - PDO_MySQL:
         . Fixed bug #77944 (Wrong meta pdo_type for bigint on LLP64). (cmb)
    
       - Reflection:
         . Fixed bug #75186 (Inconsistent reflection of Closure:::__invoke()). (Nikita)
    
       - Session:
         . Fixed bug #77911 (Wrong warning for session.sid_bits_per_character). (cmb)
    
       - SOAP:
         . Fixed bug #77945 (Segmentation fault when constructing SoapClient with
           WSDL_CACHE_BOTH). (Nikita)
    
       - SPL:
         . Fixed bug #77024 (SplFileObject::__toString() may return array). (Craig
           Duncan)
    
       - SQLite:
         . Fixed bug #77967 (Bypassing open_basedir restrictions via file uris). (Stas)
    
       - Standard:
         . Fixed bug #77931 (Warning for array_map mentions wrong type). (Nikita)
         . Fixed bug #78003 (strip_tags output change since PHP 7.3). (cmb)
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.259 -r1.260 pkgsrc/lang/php/phpversion.mk
       cvs rdiff -u -r1.7 -r1.8 pkgsrc/lang/php73/distinfo

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.251.2.8 2019/06/04 19:39:07 spz Exp $
+# $NetBSD: phpversion.mk,v 1.251.2.9 2019/06/04 20:47:08 spz Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP71_VERSION=	7.1.30
 PHP72_VERSION=	7.2.19
-PHP73_VERSION=	7.3.5
+PHP73_VERSION=	7.3.6
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
