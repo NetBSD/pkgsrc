16388934df0ca6f9335738cf2fa668287ce5f6cb
nia <nia@pkgsrc.org>
2019-06-05 06:07:27 +0000

    faad2: Update to 2.8.8
    * Add new files to Makefile.am
    * Include "utils" in source package
    * Some improvements to "unicode_support.h" + include this file on ALL
    platforms.
    * Fixed possible buffer overflow.

--- audio/faad2/patches/patch-frontend_mp4read.c
+++ audio/faad2/patches/patch-frontend_mp4read.c
@@ -1,20 +1,20 @@
-$NetBSD: patch-frontend_mp4read.c,v 1.1 2017/12/01 11:17:51 adam Exp $
+$NetBSD: patch-frontend_mp4read.c,v 1.2 2019/06/05 06:07:27 nia Exp $
 
 Do not re-define bswap32() and bswap16().
 
---- frontend/mp4read.c.orig	2017-12-01 11:02:01.786551844 +0000
+--- frontend/mp4read.c.orig	2017-12-17 11:18:43.000000000 +0000
 +++ frontend/mp4read.c
-@@ -45,6 +45,8 @@ mp4config_t mp4config = { 0 };
+@@ -46,6 +46,8 @@ mp4config_t mp4config = { 0 };
  
  static FILE *g_fin = NULL;
  
 +#include "config.h"
 +#ifndef HAVE_SYS_ENDIAN_H
- static inline uint32_t bswap32(uint32_t u32)
+ static inline uint32_t bswap32(const uint32_t u32)
  {
  #ifndef WORDS_BIGENDIAN
-@@ -58,6 +60,7 @@ static inline uint16_t bswap16(uint16_t 
-     return __builtin_bswap16(u16);
+@@ -71,6 +73,7 @@ static inline uint16_t bswap16(const uin
+ 	return u16;
  #endif
  }
 +#endif
