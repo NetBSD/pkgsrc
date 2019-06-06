17be40a07f3ebff49885d32e56049a7a54039ad1
adam <adam@pkgsrc.org>
2019-06-06 11:05:12 +0000

    glib2: updated to 2.60.3
    Overview of changes in GLib 2.60.3
    
    * Various fixes to small key/value support in GHashTable
    
    * Bugs fixed:
     - Critical in g_socket_client_async_connect_complete
     - New GHashTable implementation confuses valgrind
     - test_month_names: assertion failed
     - GNetworkAddressAddressEnumerator unsafely modifies cache in GNetworkAddress
     - Leaks in gsocketclient.c connection code
     - glib/date test fails
     - GDB pretty-printer for GHashTable no longer works
     - !815 Merge branch 'wip/tingping/socketclient-cancel-2' into 'master'
     - !816 Backport !814 “gschema.dtd: Add target attribute to alias” to glib-2-60
     - !826 Backport !824 “gsocketclient: Fix a leak in the connection code” to glib-2-60
     - !829 Backport !828 “build: Fix a typo in the test whether _NL_ABALTMON_n is supported” to glib-2-60
     - !834 Backport !823 "gnetworkaddress: Fix parallel enumerations interfering with eachother" to glib-2-60
     - !838 Backport !835 “Fix typo in German translation” to glib-2-60
     - !841 Backport !839 “tests: Update month name check for Greek locale” to glib-2-60
     - !844 Backport !840 “ghash: Disable small-arrays under valgrind” to glib-2-60
     - !846 Backport !845 “Fixing g_format_size_full() on Windows-x64” to glib-2-60
     - !855 Backport !848 (more GHashTable fixes) to glib-2-60
     - !858 Backport !852 “Update gdb pretty-printer for GHashTable” to glib-2-60
    
    * Translation updates:
     - German

--- devel/glib2/patches/patch-gio_giomodule.c
+++ devel/glib2/patches/patch-gio_giomodule.c
@@ -1,33 +1,9 @@
-$NetBSD: patch-gio_giomodule.c,v 1.4 2019/06/05 08:57:15 jperkin Exp $
+$NetBSD: patch-gio_giomodule.c,v 1.5 2019/06/06 11:05:12 adam Exp $
 
-Re-enable gdesktopappinfo on Darwin.
 Disable inotify on SunOS.
 
 --- gio/giomodule.c.orig	2019-05-03 13:43:28.000000000 +0000
 +++ gio/giomodule.c
-@@ -43,12 +43,9 @@
- #endif
- #include <glib/gstdio.h>
- 
--#if defined(G_OS_UNIX) && !defined(HAVE_COCOA)
-+#if defined(G_OS_UNIX)
- #include "gdesktopappinfo.h"
- #endif
--#ifdef HAVE_COCOA
--#include "gosxappinfo.h"
--#endif
- 
- #ifdef HAVE_COCOA
- #include <AvailabilityMacros.h>
-@@ -1084,7 +1081,7 @@ _g_io_modules_ensure_extension_points_re
-     {
-       registered_extensions = TRUE;
-       
--#if defined(G_OS_UNIX) && !defined(HAVE_COCOA)
-+#if defined(G_OS_UNIX)
- #if !GLIB_CHECK_VERSION (3, 0, 0)
-       ep = g_io_extension_point_register (G_DESKTOP_APP_INFO_LOOKUP_EXTENSION_POINT_NAME);
-       G_GNUC_BEGIN_IGNORE_DEPRECATIONS
 @@ -1209,7 +1206,7 @@ _g_io_modules_ensure_loaded (void)
        /* Initialize types from built-in "modules" */
        g_type_ensure (g_null_settings_backend_get_type ());
@@ -37,11 +13,3 @@ Disable inotify on SunOS.
        g_type_ensure (g_inotify_file_monitor_get_type ());
  #endif
  #if defined(HAVE_KQUEUE)
-@@ -1225,7 +1222,6 @@ _g_io_modules_ensure_loaded (void)
- #endif
- #ifdef HAVE_COCOA
-       g_type_ensure (g_nextstep_settings_backend_get_type ());
--      g_type_ensure (g_osx_app_info_get_type ());
- #endif
- #ifdef G_OS_UNIX
-       g_type_ensure (_g_unix_volume_monitor_get_type ());
