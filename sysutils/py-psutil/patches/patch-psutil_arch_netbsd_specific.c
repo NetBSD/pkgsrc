030e5b7c0d24f4cb13ec31d9b0be6cb12d6cd8d1
leot <leot@pkgsrc.org>
2019-06-07 19:09:06 +0000

    py-psutil: Fix psutil.Popen.as_dict() MemoryError on NetBSD
    Previously for kern.argmax a size_t was used in sysctl(3) and the
    uninitialized data lead to a too big malloc() call that failed.
    
    Bump PKGREVISION

--- sysutils/py-psutil/patches/patch-psutil_arch_netbsd_specific.c
+++ sysutils/py-psutil/patches/patch-psutil_arch_netbsd_specific.c
@@ -1,8 +1,11 @@
-$NetBSD: patch-psutil_arch_netbsd_specific.c,v 1.2 2019/06/01 01:11:57 kamil Exp $
+$NetBSD: patch-psutil_arch_netbsd_specific.c,v 1.3 2019/06/07 19:09:06 leot Exp $
 
 Don't include <sys/user.h> on NetBSD. Include <sys/iostat.h> instead.
 
 Use proc_cwd on NetBSD >= 8.99.42.
+
+Use an `int' to store `kern.argmax' to avoid possible read of
+uninitialized memory.
 
 --- psutil/arch/netbsd/specific.c.orig	2019-03-02 20:46:28.000000000 +0000
 +++ psutil/arch/netbsd/specific.c
@@ -40,3 +43,31 @@ Use proc_cwd on NetBSD >= 8.99.42.
  
  // XXX: This is no longer used as per
  // https://github.com/giampaolo/psutil/pull/557#issuecomment-171912820
+@@ -313,8 +331,8 @@ psutil_get_proc_list(kinfo_proc **procLi
+ char *
+ psutil_get_cmd_args(pid_t pid, size_t *argsize) {
+     int mib[4];
++    int argmax;
+     ssize_t st;
+-    size_t argmax;
+     size_t size;
+     char *procargs = NULL;
+ 
+@@ -339,14 +357,15 @@ psutil_get_cmd_args(pid_t pid, size_t *a
+     mib[2] = pid;
+     mib[3] = KERN_PROC_ARGV;
+ 
+-    st = sysctl(mib, 4, procargs, &argmax, NULL, 0);
++    size = argmax;
++    st = sysctl(mib, 4, procargs, &size, NULL, 0);
+     if (st == -1) {
+         free(procargs);
+         PyErr_SetFromErrno(PyExc_OSError);
+         return NULL;
+     }
+ 
+-    *argsize = argmax;
++    *argsize = size;
+     return procargs;
+ }
+ 
