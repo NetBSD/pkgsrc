25604dfef2aca9cc0801ea93880d69dd24ba2d88
leot <leot@pkgsrc.org>
2019-06-08 19:53:26 +0000

    *: use LIBRSVG_TYPE instead of PLATFORM_SUPPORTS_RUST

--- fonts/harfbuzz/buildlink3.mk
+++ fonts/harfbuzz/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2019/06/03 12:27:48 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2019/06/08 19:53:26 leot Exp $
 
 BUILDLINK_TREE+=	harfbuzz
 
 .if !defined(HARFBUZZ_BUILDLINK3_MK)
 HARFBUZZ_BUILDLINK3_MK:=
 
-.include "../../lang/rust/platform.mk"
+.include "../../graphics/librsvg/available.mk"
 
 BUILDLINK_API_DEPENDS.harfbuzz+=	harfbuzz>=2.1.1
 BUILDLINK_ABI_DEPENDS.harfbuzz+=	harfbuzz>=2.1.1
@@ -13,7 +13,7 @@ BUILDLINK_PKGSRCDIR.harfbuzz?=		../../fonts/harfbuzz
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
-.if !empty(PLATFORM_SUPPORTS_RUST:M[Yy][Ee][Ss])
+.if ${LIBRSVG_TYPE} == "rust"
 .include "../../graphics/graphite2/buildlink3.mk"
 .endif
 .endif	# HARFBUZZ_BUILDLINK3_MK
