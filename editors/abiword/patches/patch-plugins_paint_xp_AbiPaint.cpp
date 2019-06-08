da7926cedfcd3a80a37f3a5db3b7ef9601211bcb
rillig <rillig@pkgsrc.org>
2019-06-08 10:40:52 +0000

    editors/*: let pkglint fix indentation and variable alignment
    pkglint -Wall -F -r editors
    
    Manually fixed the changes in editors/nedit since the "fixed" indentation
    was wrong.

--- editors/abiword/patches/patch-plugins_paint_xp_AbiPaint.cpp
+++ editors/abiword/patches/patch-plugins_paint_xp_AbiPaint.cpp
@@ -1,5 +1,7 @@
-$NetBSD: patch-plugins_paint_xp_AbiPaint.cpp,v 1.1 2015/09/13 21:32:13 nros Exp $
+$NetBSD: patch-plugins_paint_xp_AbiPaint.cpp,v 1.2 2019/06/08 10:40:53 rillig Exp $
+
 * Fix seg fault.
+
 --- plugins/paint/xp/AbiPaint.cpp.orig	2013-04-07 13:53:03.000000000 +0000
 +++ plugins/paint/xp/AbiPaint.cpp
 @@ -177,6 +177,9 @@ static DECLARE_ABI_PLUGIN_METHOD(specify
