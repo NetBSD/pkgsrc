952f69508c984aed84a7e5144822d3a18f1f8ab1
leot <leot@pkgsrc.org>
2019-06-09 13:57:27 +0000

    py-rt: Backport upstream patch to fix search() Format='s' and Format='i'
    At least on RT 4 blank lines can be present at the end of the search response,
    backport upstream commit bb07009c4f62dd2ac393aab82fded6424eead82f to address
    that.
    
    Bump PKGREVISION

---
+++ net/py-rt/patches/patch-rt.py
@@ -0,0 +1,29 @@
+$NetBSD: patch-rt.py,v 1.1 2019/06/09 13:57:27 leot Exp $
+
+Backport upstream commit bb07009c4f62dd2ac393aab82fded6424eead82f
+to ignore ignore possible empty lines.
+
+--- rt.py.orig	2018-07-16 12:47:27.000000000 +0000
++++ rt.py
+@@ -518,15 +518,19 @@ class Rt:
+             return items
+         elif Format == 's':
+             items = []
+-            msgs = msg.splitlines()[2:]
++            msgs = lines[2:]
+             for msg in msgs:
++                if "" == msg: # Ignore blank line at the end
++                   continue
+                 ticket_id, subject = msg.split(': ', 1)
+                 items.append({'id': 'ticket/' + ticket_id, 'Subject': subject})
+             return items
+         elif Format == 'i':
+             items = []
+-            msgs = msg.splitlines()[2:]
++            msgs = lines[2:]
+             for msg in msgs:
++                if "" == msg: # Ignore blank line at the end
++                   continue
+                 _, ticket_id = msg.split('/', 1)
+                 items.append({'id': 'ticket/' + ticket_id})
+             return items
