1bcc9f147a5005a5822d40dde9f4aa380a0052e5
sjmulder <sjmulder@pkgsrc.org>
2019-06-10 10:40:52 +0000

    math/sc-im: use LDLIBS to fix Ubuntu build
    The build on Ubuntu 14.04 and 18.04 failed on undefined references to
    libxls functions caused by having the -l at the beginning of the
    argument list, injected into LDFLAGS, rather than at the end, where
    LDLIBS ends up.

--- math/sc-im/options.mk
+++ math/sc-im/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2019/02/11 22:01:18 leot Exp $
+# $NetBSD: options.mk,v 1.3 2019/06/10 10:40:52 sjmulder Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sc-im
 PKG_SUPPORTED_OPTIONS+=	color xls xlsx lua
@@ -12,13 +12,13 @@ CFLAGS+=	-DUSECOLORS
 
 .if !empty(PKG_OPTIONS:Mxls)
 CFLAGS+=	-DXLS
-LDFLAGS+=	-lxlsreader
+LDLIBS+=	-lxlsreader
 .include "../../textproc/libxls/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mxlsx)
 CFLAGS+=	-DXLSX -DXLSX_EXPORT
-LDFLAGS+=	-lxlsxwriter
+LDLIBS+=	-lxlsxwriter
 .include "../../archivers/libzip/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../textproc/libxlsxwriter/buildlink3.mk"
