79de0b3865643ac0032739471c7b3cec7683fb80
jaapb <jaapb@pkgsrc.org>
2019-06-11 09:06:33 +0000

    Updated devel/ocaml-core_kernel to version 0.12.2.
    This adds support for OCaml 4.08.
    I have also added bash to USE_TOOLS to fix PR pkg/54285.

--- devel/ocaml-core_kernel/buildlink3.mk
+++ devel/ocaml-core_kernel/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/19 10:07:47 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/06/11 09:06:33 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-core_kernel
 
 .if !defined(OCAML_CORE_KERNEL_BUILDLINK3_MK)
 OCAML_CORE_KERNEL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-core_kernel+=	ocaml-core_kernel>=0.12.0
+BUILDLINK_API_DEPENDS.ocaml-core_kernel+=	ocaml-core_kernel>=0.12.2
 BUILDLINK_PKGSRCDIR.ocaml-core_kernel?=	../../devel/ocaml-core_kernel
 
 .endif	# OCAML_CORE_KERNEL_BUILDLINK3_MK
