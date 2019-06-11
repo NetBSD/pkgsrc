1613c2b4dfaec85be3a88e75d2df082e90655a08
leot <leot@pkgsrc.org>
2019-06-11 22:55:04 +0000

    py-psutil: Update to 5.6.3
    pkgsrc changes:
     - Remove no longer needed patch hunks (applied upstream)
    
    Changes:
    5.6.3
    =====
    2019-06-11
    Enhancements
    ------------
     - 1494: [AIX] added support for Process.environ().  (patch by Arnon Yaari)
    
    Bug fixes
    ---------
     - 1276: [AIX] can't get whole cmdline().  (patch by Arnon Yaari)
     - 1501: [Windows] Process cmdline() and exe() raise unhandled "WinError 1168
       element not found" exceptions for "Registry" and "Memory Compression" psuedo
       processes on Windows 10.
     - 1526: [NetBSD] process cmdline() could raise MemoryError.  (patch by
       Kamil Rytarowski)

--- sysutils/py-psutil/patches/patch-psutil___psutil__bsd.c
+++ sysutils/py-psutil/patches/patch-psutil___psutil__bsd.c
@@ -1,22 +1,9 @@
-$NetBSD: patch-psutil___psutil__bsd.c,v 1.9 2019/06/01 01:11:57 kamil Exp $
-
-Don't include <sys/user.h> on every BSD.
-It's absent on NetBSD and unused on OpenBSD.
+$NetBSD: patch-psutil___psutil__bsd.c,v 1.10 2019/06/11 22:55:04 leot Exp $
 
 Define proc_cwd on NetBSD >= 8.99.42.
 
 --- psutil/_psutil_bsd.c.orig	2019-04-11 21:10:12.000000000 +0000
 +++ psutil/_psutil_bsd.c
-@@ -30,7 +30,9 @@
- #include <sys/types.h>
- #include <sys/param.h>
- #include <sys/sysctl.h>
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
- #include <sys/user.h>
-+#endif
- #include <sys/proc.h>
- #include <sys/file.h>
- #include <sys/socket.h>
 @@ -919,6 +921,8 @@ PsutilMethods[] = {
  #if defined(PSUTIL_FREEBSD) || defined(PSUTIL_OPENBSD)
      {"proc_connections", psutil_proc_connections, METH_VARARGS,
