e9f5788be8ac9da5126d8bb507a738bcb726f926
jaapb <jaapb@pkgsrc.org>
2019-06-11 08:44:31 +0000

    Updated devel/ocaml-ppx_bin_prot to 0.12.1.
    This version adds support for OCaml 4.08.

--- devel/ocaml-ppx_bin_prot/buildlink3.mk
+++ devel/ocaml-ppx_bin_prot/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/13 11:15:45 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/06/11 08:44:31 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_bin_prot
 
 .if !defined(OCAML_PPX_BIN_PROT_BUILDLINK3_MK)
 OCAML_PPX_BIN_PROT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ppx_bin_prot+=	ocaml-ppx_bin_prot>=0.12.0
+BUILDLINK_API_DEPENDS.ocaml-ppx_bin_prot+=	ocaml-ppx_bin_prot>=0.12.1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_bin_prot?=	../../devel/ocaml-ppx_bin_prot
 
 .endif	# OCAML_PPX_BIN_PROT_BUILDLINK3_MK
