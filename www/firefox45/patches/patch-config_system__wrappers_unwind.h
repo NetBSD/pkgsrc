a5af70d068708c4eb7fdbc89b32571ce5f5e48ed
wiz <wiz@pkgsrc.org>
2019-06-12 15:25:01 +0000

    firefox{36,45}: remove old ESR releases of firefox
    This still leaves the current ESR release, firefox60, the unsupported
    one before that, firefox52, which does not depend on rust.

--- www/firefox45/patches/patch-config_system__wrappers_unwind.h
+++
@@ -1,9 +0,0 @@
-$NetBSD: patch-config_system__wrappers_unwind.h,v 1.1 2016/04/27 16:36:50 ryoon Exp $
-
---- config/system_wrappers/unwind.h.orig	2013-05-13 19:56:18.000000000 +0000
-+++ config/system_wrappers/unwind.h
-@@ -0,0 +1,4 @@
-+#pragma GCC system_header
-+#pragma GCC visibility push(default)
-+#include_next <unwind.h>
-+#pragma GCC visibility pop
