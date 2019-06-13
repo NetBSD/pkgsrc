6f34d031b8aff868f9474d84b19da2b243f810aa
nia <nia@pkgsrc.org>
2019-06-13 22:05:24 +0000

    firefox: Add sun audio backend and make it a default where supported.
    This replaces the OSS backend with something that passes the unit tests,
    supports additional channels, and supports recording. It will be included
    with future versions of Firefox.
    
    Tested with:
    
    * YouTube audio-video sync test
    * about:support device detection
    * WebRTC microphone recording (using an USB microphone)
    
    Note: you can select an audio backend using the about:config variable
    media.cubeb.backend. This can be set to options such as sun/pulse/oss.
    
    Let me know if you still need to use the oss backend. It's very
    incomplete, buggy, and FreeBSD has already removed it - ideally we
    should eventually.
    
    Bump PKGREVISION.

--- www/firefox/patches/patch-media_libcubeb_update.sh
+++ www/firefox/patches/patch-media_libcubeb_update.sh
@@ -1,12 +1,13 @@
-$NetBSD: patch-media_libcubeb_update.sh,v 1.5 2018/11/04 00:38:45 ryoon Exp $
+$NetBSD: patch-media_libcubeb_update.sh,v 1.6 2019/06/13 22:05:24 nia Exp $
 
---- media/libcubeb/update.sh.orig	2018-10-18 20:06:09.000000000 +0000
+--- media/libcubeb/update.sh.orig	2019-05-29 15:39:11.000000000 +0000
 +++ media/libcubeb/update.sh
-@@ -20,6 +20,7 @@ cp $1/src/cubeb_log.h src
+@@ -25,6 +25,8 @@ cp $1/src/cubeb_log.h src
  cp $1/src/cubeb_mixer.cpp src
  cp $1/src/cubeb_mixer.h src
  cp $1/src/cubeb_opensl.c src
 +cp $1/src/cubeb_oss.c src
++cp $1/src/cubeb_sun.c src
  cp $1/src/cubeb-jni.cpp src
  cp $1/src/cubeb-jni.h src
  cp $1/src/android/cubeb-output-latency.h src/android
