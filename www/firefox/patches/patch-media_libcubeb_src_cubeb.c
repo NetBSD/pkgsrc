13e867555d2b56b679c28aa03e167b7d9fb8cb4a
nia <nia@pkgsrc.org>
2019-06-13 22:05:24 +0000

    firefox: Add sun audio backend and make it a default where supported.
    This replaces the OSS backend with something that passes the unit tests,
    supports additional channels, and supports recording. It will be included
    with future versions of Firefox.
    
    Tested with:
    
    * YouTube audio-video sync test
    * about:support device detection
    * WebRTC microphone recording (using an USB microphone)
    
    Note: you can select an audio backend using the about:config variable
    media.cubeb.backend. This can be set to options such as sun/pulse/oss.
    
    Let me know if you still need to use the oss backend. It's very
    incomplete, buggy, and FreeBSD has already removed it - ideally we
    should eventually.
    
    Bump PKGREVISION.

--- www/firefox/patches/patch-media_libcubeb_src_cubeb.c
+++ www/firefox/patches/patch-media_libcubeb_src_cubeb.c
@@ -1,10 +1,21 @@
-$NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.9 2018/11/04 04:46:57 ryoon Exp $
+$NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.10 2019/06/13 22:05:24 nia Exp $
 
 * Support OSS audio
+* Support Sun audio
 
---- media/libcubeb/src/cubeb.c.orig	2018-10-18 20:06:09.000000000 +0000
+--- media/libcubeb/src/cubeb.c.orig	2019-06-09 08:15:58.000000000 +0000
 +++ media/libcubeb/src/cubeb.c
-@@ -60,6 +60,9 @@ int audiotrack_init(cubeb ** context, ch
+@@ -51,6 +51,9 @@ int wasapi_init(cubeb ** context, char c
+ #if defined(USE_SNDIO)
+ int sndio_init(cubeb ** context, char const * context_name);
+ #endif
++#if defined(USE_SUN)
++int sun_init(cubeb ** context, char const * context_name);
++#endif
+ #if defined(USE_OPENSL)
+ int opensl_init(cubeb ** context, char const * context_name);
+ #endif
+@@ -60,6 +63,9 @@ int audiotrack_init(cubeb ** context, ch
  #if defined(USE_KAI)
  int kai_init(cubeb ** context, char const * context_name);
  #endif
@@ -14,7 +25,7 @@ $NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.9 2018/11/04 04:46:57 ryoon Exp $
  
  static int
  validate_stream_params(cubeb_stream_params * input_stream_params,
-@@ -160,6 +163,10 @@ cubeb_init(cubeb ** context, char const 
+@@ -160,6 +166,10 @@ cubeb_init(cubeb ** context, char const 
  #if defined(USE_KAI)
        init_oneshot = kai_init;
  #endif
@@ -25,7 +36,17 @@ $NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.9 2018/11/04 04:46:57 ryoon Exp $
      } else {
        /* Already set */
      }
-@@ -204,6 +211,9 @@ cubeb_init(cubeb ** context, char const 
+@@ -195,6 +205,9 @@ cubeb_init(cubeb ** context, char const 
+ #if defined(USE_SNDIO)
+     sndio_init,
+ #endif
++#if defined(USE_SUN)
++    sun_init,
++#endif
+ #if defined(USE_OPENSL)
+     opensl_init,
+ #endif
+@@ -204,6 +217,9 @@ cubeb_init(cubeb ** context, char const 
  #if defined(USE_KAI)
      kai_init,
  #endif
