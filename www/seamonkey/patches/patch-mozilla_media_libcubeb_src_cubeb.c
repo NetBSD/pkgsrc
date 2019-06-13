2ee16d6a4abd07ce808e13cea68e5b5ab79ce449
nia <nia@pkgsrc.org>
2019-06-13 10:53:11 +0000

    seamonkey: sun audio support for NetBSD/SunOS, use gtk3 by default
    cubeb_sun replaces cubeb_oss, adding support for additional channels
    on NetBSD, passing tests, and recording support (more useful on firefox
    where WebRTC works)
    
    upstream's official builds use gtk3 over gtk2 and doing so enables
    support for hidpi displays.
    
    me and several others have been using this for the past ~week, see
    https://github.com/kinetiknz/cubeb/pull/510
    
    bump PKGREVISION.

--- www/seamonkey/patches/patch-mozilla_media_libcubeb_src_cubeb.c
+++ www/seamonkey/patches/patch-mozilla_media_libcubeb_src_cubeb.c
@@ -1,8 +1,18 @@
-$NetBSD: patch-mozilla_media_libcubeb_src_cubeb.c,v 1.3 2017/11/10 22:45:27 ryoon Exp $
+$NetBSD: patch-mozilla_media_libcubeb_src_cubeb.c,v 1.4 2019/06/13 10:53:11 nia Exp $
 
---- mozilla/media/libcubeb/src/cubeb.c.orig	2017-10-16 07:19:00.000000000 +0000
+--- mozilla/media/libcubeb/src/cubeb.c.orig	2018-07-12 02:17:30.000000000 +0000
 +++ mozilla/media/libcubeb/src/cubeb.c
-@@ -54,6 +54,9 @@ int audiotrack_init(cubeb ** context, ch
+@@ -45,6 +45,9 @@ int wasapi_init(cubeb ** context, char c
+ #if defined(USE_SNDIO)
+ int sndio_init(cubeb ** context, char const * context_name);
+ #endif
++#if defined(USE_SUN)
++int sun_init(cubeb ** context, char const * context_name);
++#endif
+ #if defined(USE_OPENSL)
+ int opensl_init(cubeb ** context, char const * context_name);
+ #endif
+@@ -54,6 +57,9 @@ int audiotrack_init(cubeb ** context, ch
  #if defined(USE_KAI)
  int kai_init(cubeb ** context, char const * context_name);
  #endif
@@ -12,7 +22,17 @@ $NetBSD: patch-mozilla_media_libcubeb_src_cubeb.c,v 1.3 2017/11/10 22:45:27 ryoo
  
  
  static int
-@@ -141,6 +144,9 @@ cubeb_init(cubeb ** context, char const 
+@@ -132,6 +138,9 @@ cubeb_init(cubeb ** context, char const 
+ #if defined(USE_SNDIO)
+     sndio_init,
+ #endif
++#if defined(USE_SUN)
++    sun_init,
++#endif
+ #if defined(USE_OPENSL)
+     opensl_init,
+ #endif
+@@ -141,6 +150,9 @@ cubeb_init(cubeb ** context, char const 
  #if defined(USE_KAI)
      kai_init,
  #endif
