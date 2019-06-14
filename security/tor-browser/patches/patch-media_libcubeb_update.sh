d368b4b3d96b99f9d47aec1b9622083bd81c2b87
nia <nia@pkgsrc.org>
2019-06-14 17:22:21 +0000

    tor-browser: add sun audio backend. requested by wiz.

--- security/tor-browser/patches/patch-media_libcubeb_update.sh
+++ security/tor-browser/patches/patch-media_libcubeb_update.sh
@@ -1,12 +1,13 @@
-$NetBSD: patch-media_libcubeb_update.sh,v 1.1 2019/02/25 15:32:24 wiz Exp $
+$NetBSD: patch-media_libcubeb_update.sh,v 1.2 2019/06/14 17:22:21 nia Exp $
 
---- media/libcubeb/update.sh.orig	2018-05-03 16:58:30.000000000 +0000
+--- media/libcubeb/update.sh.orig	2019-05-15 06:47:08.000000000 +0000
 +++ media/libcubeb/update.sh
-@@ -20,6 +20,7 @@ cp $1/src/cubeb_log.h src
+@@ -20,6 +20,8 @@ cp $1/src/cubeb_log.h src
  cp $1/src/cubeb_mixer.cpp src
  cp $1/src/cubeb_mixer.h src
  cp $1/src/cubeb_opensl.c src
 +cp $1/src/cubeb_oss.c src
++cp $1/src/cubeb_sun.c src
  cp $1/src/cubeb-jni.cpp src
  cp $1/src/cubeb-jni.h src
  cp $1/src/android/cubeb-output-latency.h src/android
