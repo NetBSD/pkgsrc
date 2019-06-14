7ef684aa96979d7ca943d13ec6a7a6e6dd9ccd58
nia <nia@pkgsrc.org>
2019-06-14 17:22:21 +0000

    tor-browser: add sun audio backend. requested by wiz.

--- security/tor-browser/patches/patch-media_libcubeb_src_cubeb.c
+++ security/tor-browser/patches/patch-media_libcubeb_src_cubeb.c
@@ -1,10 +1,21 @@
-$NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.3 2019/02/25 15:32:24 wiz Exp $
+$NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.4 2019/06/14 17:22:21 nia Exp $
 
 * Support OSS audio
+* Support Sun audio
 
---- media/libcubeb/src/cubeb.c.orig	2017-07-31 16:20:49.000000000 +0000
+--- media/libcubeb/src/cubeb.c.orig	2019-05-15 06:47:08.000000000 +0000
 +++ media/libcubeb/src/cubeb.c
-@@ -55,6 +55,9 @@ int audiotrack_init(cubeb ** context, ch
+@@ -51,6 +51,9 @@ int wasapi_init(cubeb ** context, char c
+ #if defined(USE_SNDIO)
+ int sndio_init(cubeb ** context, char const * context_name);
+ #endif
++#if defined(USE_SUN)
++int sun_init(cubeb ** context, char const * context_name);
++#endif
+ #if defined(USE_OPENSL)
+ int opensl_init(cubeb ** context, char const * context_name);
+ #endif
+@@ -60,6 +63,9 @@ int audiotrack_init(cubeb ** context, ch
  #if defined(USE_KAI)
  int kai_init(cubeb ** context, char const * context_name);
  #endif
@@ -14,7 +25,18 @@ $NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.3 2019/02/25 15:32:24 wiz Exp $
  
  static int
  validate_stream_params(cubeb_stream_params * input_stream_params,
-@@ -155,6 +158,10 @@ cubeb_init(cubeb ** context, char const 
+@@ -148,6 +154,10 @@ cubeb_init(cubeb ** context, char const 
+ #if defined(USE_SNDIO)
+       init_oneshot = sndio_init;
+ #endif
++    } else if (!strcmp(backend_name, "sun")) {
++#if defined(USE_SUN)
++      init_oneshot = sun_init;
++#endif
+     } else if (!strcmp(backend_name, "opensl")) {
+ #if defined(USE_OPENSL)
+       init_oneshot = opensl_init;
+@@ -160,6 +170,10 @@ cubeb_init(cubeb ** context, char const 
  #if defined(USE_KAI)
        init_oneshot = kai_init;
  #endif
@@ -25,7 +47,17 @@ $NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.3 2019/02/25 15:32:24 wiz Exp $
      } else {
        /* Already set */
      }
-@@ -199,6 +206,9 @@ cubeb_init(cubeb ** context, char const 
+@@ -195,6 +209,9 @@ cubeb_init(cubeb ** context, char const 
+ #if defined(USE_SNDIO)
+     sndio_init,
+ #endif
++#if defined(USE_SUN)
++    sun_init,
++#endif
+ #if defined(USE_OPENSL)
+     opensl_init,
+ #endif
+@@ -204,6 +221,9 @@ cubeb_init(cubeb ** context, char const 
  #if defined(USE_KAI)
      kai_init,
  #endif
