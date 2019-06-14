8b3c2329af310e734a7f069216889cc0c0266479
nia <nia@pkgsrc.org>
2019-06-14 08:11:34 +0000

    firefox52: This gets a sun audio too.
    Like seamonkey, this uses an older API for it, so device detection for
    microphones (etc) is currently disabled.

--- www/firefox52/patches/patch-media_libcubeb_update.sh
+++ www/firefox52/patches/patch-media_libcubeb_update.sh
@@ -1,12 +1,13 @@
-$NetBSD: patch-media_libcubeb_update.sh,v 1.1 2017/04/27 01:55:57 ryoon Exp $
+$NetBSD: patch-media_libcubeb_update.sh,v 1.2 2019/06/14 08:11:35 nia Exp $
 
---- media/libcubeb/update.sh.orig	2016-10-31 20:15:39.000000000 +0000
+--- media/libcubeb/update.sh.orig	2017-07-10 16:18:12.000000000 +0000
 +++ media/libcubeb/update.sh
-@@ -16,6 +16,7 @@ cp $1/src/cubeb_audiounit.cpp src
+@@ -17,6 +17,8 @@ cp $1/src/cubeb_audiounit.cpp src
  cp $1/src/cubeb_osx_run_loop.h src
  cp $1/src/cubeb_jack.cpp src
  cp $1/src/cubeb_opensl.c src
 +cp $1/src/cubeb_oss.c src
++cp $1/src/cubeb_sun.c src
  cp $1/src/cubeb_panner.cpp src
  cp $1/src/cubeb_panner.h src
  cp $1/src/cubeb_pulse.c src
