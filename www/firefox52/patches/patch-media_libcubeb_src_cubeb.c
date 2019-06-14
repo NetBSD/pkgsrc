ff3a81209190b0b755accc49b61f7a7ef4f8bde2
nia <nia@pkgsrc.org>
2019-06-14 08:11:34 +0000

    firefox52: This gets a sun audio too.
    Like seamonkey, this uses an older API for it, so device detection for
    microphones (etc) is currently disabled.

--- www/firefox52/patches/patch-media_libcubeb_src_cubeb.c
+++ www/firefox52/patches/patch-media_libcubeb_src_cubeb.c
@@ -1,10 +1,20 @@
-$NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.1 2017/04/27 01:55:57 ryoon Exp $
+$NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.2 2019/06/14 08:11:35 nia Exp $
 
 * Support OSS audio
 
---- media/libcubeb/src/cubeb.c.orig	2017-01-23 16:13:54.000000000 +0000
+--- media/libcubeb/src/cubeb.c.orig	2017-04-11 02:13:12.000000000 +0000
 +++ media/libcubeb/src/cubeb.c
-@@ -54,6 +54,9 @@ int audiotrack_init(cubeb ** context, ch
+@@ -45,6 +45,9 @@ int wasapi_init(cubeb ** context, char c
+ #if defined(USE_SNDIO)
+ int sndio_init(cubeb ** context, char const * context_name);
+ #endif
++#if defined(USE_SUN)
++int sun_init(cubeb ** context, char const * context_name);
++#endif
+ #if defined(USE_OPENSL)
+ int opensl_init(cubeb ** context, char const * context_name);
+ #endif
+@@ -54,6 +57,9 @@ int audiotrack_init(cubeb ** context, ch
  #if defined(USE_KAI)
  int kai_init(cubeb ** context, char const * context_name);
  #endif
@@ -14,7 +24,17 @@ $NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.1 2017/04/27 01:55:57 ryoon Exp $
  
  
  static int
-@@ -141,6 +144,9 @@ cubeb_init(cubeb ** context, char const 
+@@ -132,6 +138,9 @@ cubeb_init(cubeb ** context, char const 
+ #if defined(USE_SNDIO)
+     sndio_init,
+ #endif
++#if defined(USE_SUN)
++    sun_init,
++#endif
+ #if defined(USE_OPENSL)
+     opensl_init,
+ #endif
+@@ -141,6 +150,9 @@ cubeb_init(cubeb ** context, char const 
  #if defined(USE_KAI)
      kai_init,
  #endif
