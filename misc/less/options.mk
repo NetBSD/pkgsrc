93f962b06756e16cd9792ebb7891836060cef75b
leot <leot@pkgsrc.org>
2019-06-15 10:03:38 +0000

    less: Update to 551
    pkgsrc changes:
     - Adjust regexp options to better match the underlaying configure semantic
       by introducing a `regex' group and having `pcre' and `regexp' as possible
       options.
     - Update LICENSE to reflect the licenses really used
     - Take MAINTAINERship
    
    Changes:
    Major changes between "less" versions 530 and 551
     * Add --mouse option.
     * Add --wheel-lines option.
     * Add --no-histdups option.
     * Add --save-marks option.
     * Support PCRE2 regular expression library.
     * Redraw screen on SIGWINCH even if screen size doesn't change.
     * Shell-escape filenames in history so they can be used again.
     * Ring bell if user enters invalid long option name.
     * Use PCRE_UTF8 flag for pcre regular expressions when in UTF-8 mode.
     * Windows: use wide-char string to set console title.
     * Don't count lines in initial screen if using -X with -F.
     * Support mingw build system.
     * Fix bug in v command on empty file.
     * Fix bug in v command when filename contains shell metacharacters.

--- misc/less/options.mk
+++ misc/less/options.mk
@@ -1,11 +1,15 @@
-# $NetBSD: options.mk,v 1.1 2008/03/02 15:35:33 bjs Exp $
+# $NetBSD: options.mk,v 1.2 2019/06/15 10:03:38 leot Exp $
 
 PKG_OPTIONS_VAR=	 	PKG_OPTIONS.less
-PKG_SUPPORTED_OPTIONS=	 	pcre
+PKG_OPTIONS_REQUIRED_GROUPS=	regex
+PKG_OPTIONS_GROUP.regex=	pcre regexp
+PKG_SUGGESTED_OPTIONS=	 	regexp
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mpcre)
 CONFIGURE_ARGS+= 	--with-regex=pcre
 .  include "../../devel/pcre/buildlink3.mk"
+.elif !empty(PKG_OPTIONS:Mregexp)
+CONFIGURE_ARGS+= 	--with-regex=posix
 .endif
