3c82fe997c8572a11d3384db9e90a9f4181ad18b
adam <adam@pkgsrc.org>
2019-06-15 08:21:32 +0000

    jack: updated to 0.125.0
    0.125.0:
    Bug fixes

--- audio/jack/buildlink3.mk
+++ audio/jack/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2016/02/14 09:53:10 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2019/06/15 08:21:32 adam Exp $
 
 BUILDLINK_TREE+=	jack
 
@@ -10,7 +10,7 @@ BUILDLINK_PKGSRCDIR.jack?=	../../audio/jack
 
 .include "../../audio/libsamplerate/buildlink3.mk"
 .include "../../devel/libuuid/buildlink3.mk"
-.include "../../databases/db4/buildlink3.mk"
+.include "../../mk/bdb.buildlink3.mk"
 # for DLOPEN_REQUIRE_PTHREADS
 .include "../../mk/dlopen.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
