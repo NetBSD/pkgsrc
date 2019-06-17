091498c4173cc229e649affa31e5e58cc38c58ef
jaapb <jaapb@pkgsrc.org>
2019-06-17 09:19:57 +0000

    Fixed bug in dependency specification for devel/ocaml-fmt.

--- devel/ocaml-fmt/buildlink3.mk
+++ devel/ocaml-fmt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/12 17:41:27 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/06/17 09:19:57 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-fmt
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-fmt
 OCAML_FMT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-fmt+=	ocaml-fmt>=0.8.5
+BUILDLINK_ABI_DEPENDS.ocaml-fmt+=	ocaml-fmt>=0.8.5nb1
 BUILDLINK_PKGSRCDIR.ocaml-fmt?=	../../devel/ocaml-fmt
 
 .endif	# OCAML_FMT_BUILDLINK3_MK
