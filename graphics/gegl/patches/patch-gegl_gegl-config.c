d742fd19ee9e339f17de910e3e80c26393b7fede
ryoon <ryoon@pkgsrc.org>
2019-06-18 14:26:59 +0000

    Update to 0.4.16
    Changelog:
    GEGL-0.4.16
    Not available
    
    GEGL-0.4.15
    
    Build
    
    Added gitlab continuous integration.
    
    Core
    
    Platform specific build fixes for both win32 and OSX.
    
    Avoid in-place processing for cached nodes.
    
    Indentation consitency improvements.
    
    Permit more detailed specifying/overriding of OpenCL device through the
    GEGL_USE_OPENCL environment variable.
    
    New call gegl_update_anim_time for applying the animation curves of
    properties.
    
    Buffer
    
    Swapped behavior of cubic interpolation from cubic to mithcell
    coefficients, leading to crisper results in all uses of the cubic
    sampler.
    
    Permit tiles to be unreffed after GEGL shut-down without crash, which
    is likely when using bindings to dynamic languages.
    
    Use malloc_trim when compiled with glibc to possibly decrease the
    resident size of progress when buffers are destroyed.
    
    Improved deadlock prevention in gegl_buffer_copy() when using copy on
    write.
    
    Operations
    
    Use indirect buffer inputs on main thread during multi-processed
    processing for point-filters/composers and tranform base classes.
    
    Smaller required/invalided ROI for map-absolute/relative when using
    nearest neighbor.
    
    tiff-save and jpg-save fixed dimensions when buffer saved has non-zero
    origin
    
    Added missing descriptions of properties for cubism, mosaic,
    tile-paper, color, mantiuk06 and vignette operations.
    
    ff-load fix some of the deprecation warnings.
    
    ff-save avoid crashing if audio or video codec fails to initialize.
    
    cache, parent class changed - it is no longer a point operations, also
    speed up processing and permit copy on write handling of tiles.
    
    opacity and invert - crash for 8bpc buffers, reimplemented logic for
    (non-)associated alpha OpenCL kernel selection.
    
    Tools
    
    Drop direct dependency on exiv2 - we now only transiently depend on it
    through the gexiv2 abstraction.
    
    commandline tool/gui
    
    Store per image editing chains in .gegl folder instead of sidecars.
    
    Set window title, permit interaction with opened .lui documents, fixed
    playback of audio for opened videos - it now works; if the video is
    decoding, scaling and display is fast enough to leave spare cycles.
    
    Support for nearest neighbor scaling of imagry; aiding video playback
    without dropping audio frames.
    
    Support for custom collection order and per-file key-value metadata.

--- graphics/gegl/patches/patch-gegl_gegl-config.c
+++ graphics/gegl/patches/patch-gegl_gegl-config.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-gegl_gegl-config.c,v 1.1 2019/04/09 13:09:03 ryoon Exp $
+$NetBSD: patch-gegl_gegl-config.c,v 1.2 2019/06/18 14:26:59 ryoon Exp $
 
---- gegl/gegl-config.c.orig	2019-02-25 19:00:08.000000000 +0000
+--- gegl/gegl-config.c.orig	2019-05-07 20:32:27.000000000 +0000
 +++ gegl/gegl-config.c
-@@ -244,7 +244,11 @@ gegl_config_class_init (GeglConfigClass 
- 
+@@ -280,7 +280,11 @@ gegl_config_class_init (GeglConfigClass 
+     }
  #else
      mem_total = sysconf (_SC_PHYS_PAGES) * sysconf (_SC_PAGESIZE);
 +#if defined(_SC_AVPHYS_PAGES)
