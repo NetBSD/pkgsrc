7f860ad3fe8dfe6e4e021e01ae1f3df5eee17170
taca <taca@pkgsrc.org>
2019-06-20 02:16:53 +0000

    net/bind914: update to 9.14.3
    Update bind914 to 9.14.3 (BIND 9.14.3).
    
    
    	--- 9.14.3 released ---
    
    5244.	[security]	Fixed a race condition in dns_dispatch_getnext()
    			that could cause an assertion failure if a
    			significant number of incoming packets were
    			rejected. (CVE-2019-6471) [GL #942]
    
    5243.	[bug]		Fix a possible race between dispatcher and socket
    			code in a high-load cold-cache resolver scenario.
    			[GL #943]
    
    5242.	[bug]		In relaxed qname minimizatiom mode, fall back to
    			normal resolution when encountering a lame
    			delegation, and use _.domain/A queries rather
    			than domain/NS. [GL #1055]
    
    5241.	[bug]		Fix Ed448 private and public key ASN.1 prefix blobs.
    			[GL #225]
    
    5240.	[bug]		Remove key id calculation for RSAMD5. [GL #996]
    
    5238.	[bug]		Fix a possible deadlock in TCP code. [GL #1046]
    
    5237.	[bug]		Recurse to find the root server list with 'dig +trace'.
    			[GL #1028]
    
    5234.	[port]		arm: just use the compiler's default support for
    			yield. [GL #981]

--- net/bind914/patches/patch-lib_isc_rwlock.c
+++ net/bind914/patches/patch-lib_isc_rwlock.c
@@ -1,21 +1,16 @@
-$NetBSD: patch-lib_isc_rwlock.c,v 1.1 2019/04/30 03:34:34 taca Exp $
+$NetBSD: patch-lib_isc_rwlock.c,v 1.2 2019/06/20 02:16:53 taca Exp $
 
 * Platform change from NetBSD base.
 
---- lib/isc/rwlock.c.orig	2019-04-06 20:09:59.000000000 +0000
+--- lib/isc/rwlock.c.orig	2019-06-04 15:20:04.000000000 +0000
 +++ lib/isc/rwlock.c
-@@ -54,11 +54,12 @@
- # define isc_rwlock_pause() __asm__ __volatile__ ("rep; nop")
- #elif defined(__ia64__)
- # define isc_rwlock_pause() __asm__ __volatile__ ("hint @pause")
--#elif defined(__arm__)
-+#elif defined(__arm__) && defined(_ARM_ARCH_6)
+@@ -58,7 +58,8 @@
  # define isc_rwlock_pause() __asm__ __volatile__ ("yield")
  #elif defined(sun) && (defined(__sparc) || defined(__sparc__))
  # define isc_rwlock_pause() smt_pause()
 -#elif defined(__sparc) || defined(__sparc__)
 +/* Disable pause, only works on v9 */
-+#elif (defined(__sparc) || defined(__sparc__)) && defined(notdef)
++#elif defined(__sparc) || defined(__sparc__) && defined(notdef)
  # define isc_rwlock_pause() __asm__ __volatile__ ("pause")
  #elif defined(__ppc__) || defined(_ARCH_PPC)  ||			\
  	defined(_ARCH_PWR) || defined(_ARCH_PWR2) || defined(_POWER)
