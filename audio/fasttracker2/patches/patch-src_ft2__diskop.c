e093843fdd7f233091067a35ec1e4f38e35910e6
fox <fox@pkgsrc.org>
2019-06-20 11:39:06 +0000

    fasttracker2: update to b160
    Changes since b159
    
    Beta #160 - 19.06.2019
    - Bugfix: Certain instrument properties could be modified even if the current
      instrument was zero. This is a special ins. number for pattern editing, it's
      not a real instrument. (You can set ins. #0 by pressing 0 on the numpad).
    - Bugfix: It was impossible to select an instrument higher than 0x60 (100) when
      using the numpad keys.
    - Bugfix: "Srnk." (shrink pattern) would erase the last row after its operation
    - Bugfix: When loading a floating point .wav, it would only load silence unless
      the amplitude peaked at max (1.0).
    - Bugfix: 16-bit mono .aiff files would not load correctly at all!
    - When loading 24-bit .wav/.aiff files, properly normalize amplitude before
      converting to 16-bit. This now takes up more CPU time and RAM, but it's worth
      it.
    - Default instrument envelopes were always read from preset slot #1. I don't
      like this at all, as it could mess things up when loading sample-only songs.
      It has now been changed to always use the stock envelope values when loading
      such files or when a new project is started (or instruments zapped).
    - Properly reject .it (Impulse Tracker) modules when attempted to be loaded
    - Code cleanup. Proper space alignment for special tab indents, removed
      extreme use of parenthesis, spaces etc.

--- audio/fasttracker2/patches/patch-src_ft2__diskop.c
+++ audio/fasttracker2/patches/patch-src_ft2__diskop.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_ft2__diskop.c,v 1.3 2019/04/19 02:05:14 fox Exp $
+$NetBSD: patch-src_ft2__diskop.c,v 1.4 2019/06/20 11:39:06 fox Exp $
 
 Added <sys/types.h> / <sys/stat.h> to prevent "unknown type name"
 (dev_t, ino_t and nlink_t) error from the included <fts.h>.
 
 --- src/ft2_diskop.c.orig	2019-04-19 01:53:39.359713817 +0000
 +++ src/ft2_diskop.c
-@@ -13,6 +13,8 @@
+@@ -15,6 +15,8 @@
  #include <direct.h>
  #include <shlobj.h> // SHGetFolderPathW()
  #else
--- audio/fasttracker2/patches/patch-src_ft2__unicode.c
+++ audio/fasttracker2/patches/patch-src_ft2__unicode.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_ft2__unicode.c,v 1.2 2019/03/21 10:16:40 fox Exp $
+$NetBSD: patch-src_ft2__unicode.c,v 1.3 2019/06/20 11:39:06 fox Exp $
 
 1. Added a type cast to iconv(3) calls to match the prototypes defined
 in NetBSD's iconv.h.
@@ -6,10 +6,10 @@ in NetBSD's iconv.h.
 2. Added a NetBSD specific iconv_open() call to prevent failures due
 to mismatches iconv_open() parameters present in GNU iconv(3).
 
---- src/ft2_unicode.c.orig	2019-03-16 22:34:24.000000000 +0000
+--- src/ft2_unicode.c.orig	2019-06-18 19:45:46.000000000 +0000
 +++ src/ft2_unicode.c
-@@ -277,7 +277,11 @@ char *cp437ToUtf8(char *src)
- 	inLen  = srcLen;
+@@ -271,7 +271,11 @@ char *cp437ToUtf8(char *src)
+ 	inLen = srcLen;
  	outPtr = outBuf;
  
 +#if defined(__NetBSD__)
@@ -20,24 +20,24 @@ to mismatches iconv_open() parameters present in GNU iconv(3).
  	iconv(cd, NULL, NULL, &outPtr, &outLen); // flush
  	iconv_close(cd);
  
-@@ -307,6 +311,8 @@ char *utf8ToCp437(char *src, bool remove
+@@ -301,6 +305,8 @@ char *utf8ToCp437(char *src, bool remove
  
  #ifdef __APPLE__
  	cd = iconv_open("437//TRANSLIT//IGNORE", "UTF-8-MAC");
 +#elif defined(__NetBSD__)
-+	cd = iconv_open("437", "UTF-8");    	
++	cd = iconv_open("437", "UTF-8");
  #else
  	cd = iconv_open("437//TRANSLIT//IGNORE", "UTF-8");
  #endif
-@@ -323,7 +329,11 @@ char *utf8ToCp437(char *src, bool remove
- 	inLen  = srcLen;
+@@ -317,7 +323,11 @@ char *utf8ToCp437(char *src, bool remove
+ 	inLen = srcLen;
  	outPtr = outBuf;
  
 +#if defined(__NetBSD__)
 +	rc = iconv(cd, (const char **)&inPtr, &inLen, &outPtr, &outLen);
 +#else
  	rc = iconv(cd, &inPtr, &inLen, &outPtr, &outLen);
-+#endif	
++#endif
  	iconv(cd, NULL, NULL, &outPtr, &outLen); // flush
  	iconv_close(cd);
  
