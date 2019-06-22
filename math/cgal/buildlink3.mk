e480def488f4aa129909697fc896c15cb0e69b2e
mef <mef@pkgsrc.org>
2019-06-22 05:54:27 +0000

    (math/cgal) Updated 3.9 to 4.14  (Re: PR pkg/54278)
    CHANGES.md from 3.9 to 4.14 will be of 2.8k lines.
    Here is the URL for Release information:
    
    CGAL 4.14 released
      https://www.cgal.org/2019/03/29/cgal414/
    
    CGAL 4.13.1 released
      https://www.cgal.org/2019/04/05/cgal4131
    
    CGAL 4.12.2 released
      https://www.cgal.org/2019/04/05/cgal4122
    
    Other old Release
      https://www.cgal.org/releases.html

--- math/cgal/buildlink3.mk
+++ math/cgal/buildlink3.mk
@@ -1,18 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.34 2018/12/13 19:51:35 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.35 2019/06/22 05:54:27 mef Exp $
 
 BUILDLINK_TREE+=	cgal
 
 .if !defined(CGAL_BUILDLINK3_MK)
 CGAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cgal+=	cgal>=3.9
-BUILDLINK_ABI_DEPENDS.cgal+=	cgal>=3.9nb36
+BUILDLINK_API_DEPENDS.cgal+=	cgal>=4.11
+BUILDLINK_ABI_DEPENDS.cgal+=	cgal>=4.11
 BUILDLINK_PKGSRCDIR.cgal?=	../../math/cgal
 
 .include "../../devel/boost-headers/buildlink3.mk"
 .include "../../devel/boost-libs/buildlink3.mk"
 .include "../../math/mpfr/buildlink3.mk"
-.include "../../x11/qt4-libs/buildlink3.mk"
 
 .endif	# CGAL_BUILDLINK3_MK
 
