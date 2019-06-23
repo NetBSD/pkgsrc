2e251a86dd3407eee48496dc819705276d42b977
nia <nia@pkgsrc.org>
2019-06-23 09:21:30 +0000

    Remove amaya.
    It's unmaintained by upstream for most of this decade (even then, this
    is an old version), and broken in bulk builds since at least last year.
    
    Discussed on pkgsrc-users@.

--- www/amaya/hacks.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: hacks.mk,v 1.2 2013/04/08 11:17:24 rodent Exp $
-
-.include "../../mk/bsd.prefs.mk"
-
-# Workaround for PR 45266
-.if ${OPSYS} == "NetBSD"
-BUILDLINK_INCDIRS.heimdal=	# empty
-.endif
