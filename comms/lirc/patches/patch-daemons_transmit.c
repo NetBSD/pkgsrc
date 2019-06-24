904d75c92dec73044d7c3455d8cc64cd7fdd5a2d
triaxx <triaxx@pkgsrc.org>
2019-06-24 19:01:51 +0000

    lirc: fix build on Arch Linux
    * Remove inlining for send_data()
    * Add Linux specific missing include for major()
    * Bump revision

--- comms/lirc/patches/patch-daemons_transmit.c
+++ comms/lirc/patches/patch-daemons_transmit.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-daemons_transmit.c,v 1.1 2016/05/02 02:32:14 dholland Exp $
+$NetBSD: patch-daemons_transmit.c,v 1.2 2019/06/24 19:01:51 triaxx Exp $
 
 Patch up gcc inline mess.
 
---- daemons/transmit.c~	2011-03-25 22:28:18.000000000 +0000
+--- daemons/transmit.c.orig	2011-03-25 22:28:18.000000000 +0000
 +++ daemons/transmit.c
 @@ -27,7 +27,7 @@ struct sbuf send_buffer;
  static void send_signals(lirc_t * signals, int n);
@@ -13,3 +13,12 @@ Patch up gcc inline mess.
  {
  	(*code) &= ~((((ir_code) 1) << bit));
  	(*code) |= ((ir_code) (data ? 1 : 0) << bit);
+@@ -179,7 +179,7 @@ inline void send_trail(struct ir_remote
+ 	}
+ }
+ 
+-inline void send_data(struct ir_remote *remote, ir_code data, int bits, int done)
++void send_data(struct ir_remote *remote, ir_code data, int bits, int done)
+ {
+ 	int i;
+ 	int all_bits = bit_count(remote);
