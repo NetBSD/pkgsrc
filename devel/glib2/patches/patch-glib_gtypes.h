7f3bb9f2420e42ff05c03564726ad0732f2a2ec0
schmonz <schmonz@pkgsrc.org>
2019-06-25 15:11:36 +0000

    #include <sys/types.h> for glib/gtypes.h to find ssize_t on OS X.

--- devel/glib2/patches/patch-glib_gtypes.h
+++ devel/glib2/patches/patch-glib_gtypes.h
@@ -1,11 +1,19 @@
-$NetBSD: patch-glib_gtypes.h,v 1.1 2019/06/25 10:57:28 prlw1 Exp $
+$NetBSD: patch-glib_gtypes.h,v 1.2 2019/06/25 15:11:36 schmonz Exp $
 
 We insist on C99, so size_t exists => use it.
 PR pkg/54298
 
 --- glib/gtypes.h.orig	2019-06-10 17:47:20.000000000 +0000
 +++ glib/gtypes.h
-@@ -57,6 +57,9 @@ typedef unsigned int    guint;
+@@ -32,6 +32,7 @@
+ #include <glibconfig.h>
+ #include <glib/gmacros.h>
+ #include <glib/gversionmacros.h>
++#include <sys/types.h>
+ #include <time.h>
+ 
+ G_BEGIN_DECLS
+@@ -57,6 +58,9 @@ typedef unsigned int    guint;
  typedef float   gfloat;
  typedef double  gdouble;
  
