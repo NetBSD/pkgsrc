62739c7daf8f2726250304b3e323d9bf1110ec14
schmonz <schmonz@pkgsrc.org>
2019-06-26 01:16:10 +0000

    <sys/types.h> is preferred, via gdt@.

--- audio/mp3splt-gtk/patches/patch-src_player__window.c
+++ audio/mp3splt-gtk/patches/patch-src_player__window.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_player__window.c,v 1.1 2019/06/25 21:25:24 schmonz Exp $
+$NetBSD: patch-src_player__window.c,v 1.2 2019/06/26 01:16:11 schmonz Exp $
 
-#include <sys/time.h> for time_t definition on at least NetBSD.
+#include <sys/types.h> for time_t definition on at least NetBSD.
 
 --- src/player_window.c.orig	2014-05-10 14:48:57.000000000 +0000
 +++ src/player_window.c
@@ -8,7 +8,7 @@ $NetBSD: patch-src_player__window.c,v 1.1 2019/06/25 21:25:24 schmonz Exp $
   * this file is used for the player control tab
   **********************************************************/
  
-+#include <sys/time.h>
++#include <sys/types.h>
  #include <sys/timeb.h>
  
  #include "player_window.h"
