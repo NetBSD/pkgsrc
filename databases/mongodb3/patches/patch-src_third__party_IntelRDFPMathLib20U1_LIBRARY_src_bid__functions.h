0ad2a819133c76e6efbb1800815eebc8e2b0588b
gdt <gdt@pkgsrc.org>
2019-06-28 16:30:56 +0000

    databases/mongodb3: Add, to ameliorate problems with mongodb 4
    This is a copy of mongodb before the update to 4, with annotations to
    explain why it exists.  Reasons include:
    
      - mongodb 4 is licensed under the Server Side Public License, to
        which some have objected
    
      - mongodb 4 requires c++17 (and hence gcc 8)
    
    (ok to add during freeze by wiz@)

---
+++ databases/mongodb3/patches/patch-src_third__party_IntelRDFPMathLib20U1_LIBRARY_src_bid__functions.h
@@ -0,0 +1,13 @@
+$NetBSD: patch-src_third__party_IntelRDFPMathLib20U1_LIBRARY_src_bid__functions.h,v 1.1 2019/06/28 16:30:56 gdt Exp $
+
+--- src/third_party/IntelRDFPMathLib20U1/LIBRARY/src/bid_functions.h.orig	2017-04-20 21:43:42.000000000 +0000
++++ src/third_party/IntelRDFPMathLib20U1/LIBRARY/src/bid_functions.h
+@@ -159,7 +159,7 @@ typedef unsigned    long fexcept_t;
+ typedef unsigned bid__int64 fexcept_t;
+ #endif
+ #else
+-#ifdef __QNX__
++#if defined(__QNX__) || defined(__NetBSD__)
+ #include <fenv.h>
+ #else
+ typedef unsigned short int fexcept_t;
