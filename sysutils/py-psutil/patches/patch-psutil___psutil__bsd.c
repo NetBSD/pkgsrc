b3a6513725ef06478df323ecbdea717ffa121fbc
wiz <wiz@pkgsrc.org>
2019-06-29 18:00:49 +0000

    py-psutil: fix build on -current, which does not define LSDEAD any longer.
    Bump PKGREVISION.

--- sysutils/py-psutil/patches/patch-psutil___psutil__bsd.c
+++ sysutils/py-psutil/patches/patch-psutil___psutil__bsd.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-psutil___psutil__bsd.c,v 1.10 2019/06/11 22:55:04 leot Exp $
+$NetBSD: patch-psutil___psutil__bsd.c,v 1.11 2019/06/29 18:00:49 wiz Exp $
 
 Define proc_cwd on NetBSD >= 8.99.42.
 
---- psutil/_psutil_bsd.c.orig	2019-04-11 21:10:12.000000000 +0000
+--- psutil/_psutil_bsd.c.orig	2019-06-11 04:04:44.000000000 +0000
 +++ psutil/_psutil_bsd.c
-@@ -919,6 +921,8 @@ PsutilMethods[] = {
+@@ -921,6 +921,8 @@ PsutilMethods[] = {
  #if defined(PSUTIL_FREEBSD) || defined(PSUTIL_OPENBSD)
      {"proc_connections", psutil_proc_connections, METH_VARARGS,
       "Return connections opened by process"},
@@ -13,3 +13,13 @@ Define proc_cwd on NetBSD >= 8.99.42.
      {"proc_cwd", psutil_proc_cwd, METH_VARARGS,
       "Return process current working directory."},
  #endif
+@@ -1071,7 +1073,9 @@ void init_psutil_bsd(void)
+     PyModule_AddIntConstant(module, "SSLEEP", LSSLEEP);
+     PyModule_AddIntConstant(module, "SSTOP", LSSTOP);
+     PyModule_AddIntConstant(module, "SZOMB", LSZOMB);
++#if defined(LSDEAD)
+     PyModule_AddIntConstant(module, "SDEAD", LSDEAD);
++#endif
+     PyModule_AddIntConstant(module, "SONPROC", LSONPROC);
+     // unique to NetBSD
+     PyModule_AddIntConstant(module, "SSUSPENDED", LSSUSPENDED);
