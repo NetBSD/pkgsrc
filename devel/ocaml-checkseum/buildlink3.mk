b829f0596c561e801dcd2d6cc2074dc18f43981a
jaapb <jaapb@pkgsrc.org>
2019-07-01 12:32:56 +0000

    Updated devel/ocaml-checkseum to version 0.1.0.
    This adds several minor upgrades.

--- devel/ocaml-checkseum/buildlink3.mk
+++ devel/ocaml-checkseum/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/12 17:57:08 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/07/01 12:32:56 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-checkseum
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-checkseum
 OCAML_CHECKSEUM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-checkseum+=	ocaml-checkseum>=0.0.3
+BUILDLINK_ABI_DEPENDS.ocaml-checkseum+=	ocaml-checkseum>=0.1.0
 BUILDLINK_PKGSRCDIR.ocaml-checkseum?=	../../devel/ocaml-checkseum
 
 .endif	# OCAML_CHECKSEUM_BUILDLINK3_MK
