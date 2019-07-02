94de2f59ef9635bb9654281fb4d697290232f6e2
gdt <gdt@pkgsrc.org>
2019-07-02 19:50:08 +0000

    geography/gpsd: Update ABI in buildlink3.mk
    3.19 was an ABI break, so force depending packages to use it.

--- geography/gpsd/buildlink3.mk
+++ geography/gpsd/buildlink3.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.5 2012/05/07 01:53:31 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2019/07/02 19:50:08 gdt Exp $
 
 BUILDLINK_TREE+=	gpsd
 
 .if !defined(GPSD_BUILDLINK3_MK)
 GPSD_BUILDLINK3_MK:=
 
-# 2.90 is the beginning of withdrawing of interfaces.  Plus
-# it's an ABI.
-BUILDLINK_API_DEPENDS.gpsd+=	gpsd>=2.90
-BUILDLINK_ABI_DEPENDS.gpsd+=	gpsd>=2.96
+# 3.19 changed structures and is thus an ABI break.
+BUILDLINK_API_DEPENDS.gpsd+=	gpsd>=3.19
+BUILDLINK_ABI_DEPENDS.gpsd+=	gpsd>=3.19
 BUILDLINK_PKGSRCDIR.gpsd?=	../../geography/gpsd
 .endif # GPSD_BUILDLINK3_MK
 
