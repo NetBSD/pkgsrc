39eb804a7d79b0cdfc7c705b675a4ecbc2784e9c
leot <leot@pkgsrc.org>
2019-07-02 10:48:08 +0000

    webkit-gtk: Update to 2.24.3
    pkgsrc changes:
     - Remove not needed dependency to gnutls and add missing dependency to
       libtasn1 (previously indirectly picked up via gnutls)
     - Remove patch-Source_WebCore_platform_graphics_gstreamer_MediaPlayerPrivateGStreamerBase.cpp,
       fix is now present in 2.24.3.
     - Remove a no more needed hunk in
       patch-Source_JavaScriptCore_assembler_ARM64Assembler.h.
    
    Changes:
    2.24.3
    ======
     - Deprecate WebSQL APIs.
     - Make Previous/Next gesture work in RTL mode.
     - Fix content disappearing when using CSS transforms.
     - Fix rendering artifacts in youtube volume button.
     - Fix trapezoid artifact in github comment box.
     - Fix video pause that sometimes caused to skip to finish.
     - Fix volume level changes when playing a video.
     - Fix HLS streams being slow to start.
     - Fix some radio streams that could not be played.
     - Fix the build with older versions of GStreamer.
     - Fix the build with video and audio disabled.
     - Fix several crashes and rendering issues.
     - Translation updates: Brazilian Portuguese.

--- www/webkit-gtk/patches/patch-Source_JavaScriptCore_assembler_ARM64Assembler.h
+++ www/webkit-gtk/patches/patch-Source_JavaScriptCore_assembler_ARM64Assembler.h
@@ -1,21 +1,11 @@
-$NetBSD: patch-Source_JavaScriptCore_assembler_ARM64Assembler.h,v 1.3 2019/03/13 20:49:40 leot Exp $
+$NetBSD: patch-Source_JavaScriptCore_assembler_ARM64Assembler.h,v 1.4 2019/07/02 10:48:08 leot Exp $
 
-- Bump m_compareRegister to hold values of JSC::ARM64Assembler::RegisterID
 - Add support for FreeBSD and NetBSD.
   Based on patch from FreeBSD ports but using __builtin___clear_cache()
   instead of __clear_cache() (the former is also available in gcc).
 
 --- Source/JavaScriptCore/assembler/ARM64Assembler.h.orig	2019-02-12 11:21:03.000000000 +0000
 +++ Source/JavaScriptCore/assembler/ARM64Assembler.h
-@@ -462,7 +462,7 @@ public:
-                 JumpLinkType m_linkType : 8;
-                 Condition m_condition : 4;
-                 unsigned m_bitNumber : 6;
--                RegisterID m_compareRegister : 6;
-+                RegisterID m_compareRegister : 8;
-                 bool m_is64Bit : 1;
-             } realTypes;
-             struct CopyTypes {
 @@ -2874,6 +2874,8 @@ public:
      {
  #if OS(IOS_FAMILY)
