072f773e32bae8d7f2188f52dd974391ff2a55cc
nia <nia@pkgsrc.org>
2019-07-02 13:37:34 +0000

    quakeforge: The existing workaround is causing duplicate symbol errors at link-time so just cheat by using "static inline".
    Also, don't hardcode the libGL version number, take advantage of symlinks
    instead to make libGL load on NetBSD-current with native X.
    
    Bump PKGREVISION.
    
    I still can't really get this to run for more than a few seconds (except
    qw-client, which I can't test ingame), it may be borked.

--- games/quakeforge/patches/patch-include_QF_mathlib.h
+++ games/quakeforge/patches/patch-include_QF_mathlib.h
@@ -1,44 +1,41 @@
-$NetBSD: patch-include_QF_mathlib.h,v 1.3 2013/06/23 19:54:42 dholland Exp $
+$NetBSD: patch-include_QF_mathlib.h,v 1.4 2019/07/02 13:37:34 nia Exp $
 
---- include/QF/mathlib.h.orig	2013-03-28 15:01:01.000000000 +0000
+--- include/QF/mathlib.h.orig	2004-04-07 18:01:45.000000000 +0000
 +++ include/QF/mathlib.h
-@@ -255,11 +255,12 @@ void QuatMult (const quat_t v1, const qu
+@@ -255,12 +255,8 @@ void QuatMult (const quat_t v1, const qu
  	(PlaneDist (point, plane) - (plane)->dist)
  
  extern mplane_t * const frustum;
 -extern inline qboolean R_CullBox (const vec3_t mins, const vec3_t maxs);
 -extern inline qboolean R_CullSphere (const vec3_t origin, const float radius);
 -extern inline float VectorNormalize (vec3_t v);	// returns vector length
- #ifndef IMPLEMENT_R_Cull
-+#if defined(__clang__) || !defined(__GNUC__) || defined(__GNC_STDC_INLINE__)
-+inline
-+#else
- extern inline
-+#endif
- #endif
+-#ifndef IMPLEMENT_R_Cull
+-extern inline
+-#endif
++
++static inline
  qboolean
  R_CullBox (const vec3_t mins, const vec3_t maxs)
-@@ -273,7 +274,11 @@ R_CullBox (const vec3_t mins, const vec3
+ {
+@@ -272,9 +268,7 @@ R_CullBox (const vec3_t mins, const vec3
+ 	return false;
  }
  
- #ifndef IMPLEMENT_R_Cull
-+#if defined(__clang__) || !defined(__GNUC__) || defined(__GNC_STDC_INLINE__)
-+inline
-+#else
- extern inline
-+#endif
- #endif
+-#ifndef IMPLEMENT_R_Cull
+-extern inline
+-#endif
++static inline
  qboolean
  R_CullSphere (const vec3_t origin, const float radius)
-@@ -291,7 +296,11 @@ R_CullSphere (const vec3_t origin, const
+ {
+@@ -290,9 +284,7 @@ R_CullSphere (const vec3_t origin, const
+ 	return false;
  }
  
- #ifndef IMPLEMENT_VectorNormalize
-+#if defined(__clang__) || !defined(__GNUC__) || defined(__GNC_STDC_INLINE__)
-+inline
-+#else
- extern inline
-+#endif
- #endif
+-#ifndef IMPLEMENT_VectorNormalize
+-extern inline
+-#endif
++static inline
  float
  VectorNormalize (vec3_t v)
+ {
