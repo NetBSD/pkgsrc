7915dddcf507a0bda1443b76565749f2b10b9541
bsiegert <bsiegert@pkgsrc.org>
2019-07-02 13:15:50 +0000

    Pullup ticket #5986 - requested by nia news/newsbeuter: build fix
    Revisions pulled up:
    - news/newsbeuter/Makefile                                      1.23
    - news/newsbeuter/distinfo                                      1.3
    - news/newsbeuter/patches/patch-src_feedhq__api.cpp             1.1
    - news/newsbeuter/patches/patch-src_oldreader__api.cpp          1.1
    - news/newsbeuter/patches/patch-src_ttrss__api.cpp              1.1
    
    ---
       Module Name:	pkgsrc
       Committed By:	nia
       Date:		Tue Jul  2 07:37:54 UTC 2019
    
       Modified Files:
       	pkgsrc/news/newsbeuter: Makefile distinfo
       Added Files:
       	pkgsrc/news/newsbeuter/patches: patch-src_feedhq__api.cpp
       	    patch-src_oldreader__api.cpp patch-src_ttrss__api.cpp
    
       Log Message:
       newsbeuter: Fix build with new json-c.
    
       https://github.com/json-c/json-c/issues/304
    
       XXX pullup?

---
+++ news/newsbeuter/patches/patch-src_feedhq__api.cpp
@@ -0,0 +1,17 @@
+$NetBSD: patch-src_feedhq__api.cpp,v 1.1.2.2 2019/07/02 13:15:50 bsiegert Exp $
+
+Update for newer json-c API.
+
+https://github.com/json-c/json-c/issues/304
+
+--- src/feedhq_api.cpp.orig	2015-02-19 10:56:59.000000000 +0000
++++ src/feedhq_api.cpp
+@@ -132,7 +132,7 @@ std::vector<tagged_feedurl> feedhq_api::
+ 	// TODO: parse result
+ 
+ 	struct json_object * reply = json_tokener_parse(result.c_str());
+-	if (is_error(reply)) {
++	if (reply == NULL) {
+ 		LOG(LOG_ERROR, "feedhq_api::get_subscribed_urls: failed to parse response as JSON.");
+ 		return urls;
+ 	}
---
+++ news/newsbeuter/patches/patch-src_ttrss__api.cpp
@@ -0,0 +1,31 @@
+$NetBSD: patch-src_ttrss__api.cpp,v 1.1.2.2 2019/07/02 13:15:50 bsiegert Exp $
+
+Update for newer json-c API.
+
+https://github.com/json-c/json-c/issues/304
+
+--- src/ttrss_api.cpp.orig	2015-02-19 10:56:59.000000000 +0000
++++ src/ttrss_api.cpp
+@@ -113,19 +113,19 @@ struct json_object * ttrss_api::run_op(c
+ 	LOG(LOG_DEBUG, "ttrss_api::run_op(%s,...): post=%s reply = %s", op.c_str(), req_data.c_str(), result.c_str());
+ 
+ 	struct json_object * reply = json_tokener_parse(result.c_str());
+-	if (is_error(reply)) {
++	if (reply == NULL) {
+ 		LOG(LOG_ERROR, "ttrss_api::run_op: reply failed to parse: %s", result.c_str());
+ 		return NULL;
+ 	}
+ 
+ 	struct json_object * status = json_object_object_get(reply, "status");
+-	if (is_error(status)) {
++	if (status == NULL) {
+ 		LOG(LOG_ERROR, "ttrss_api::run_op: no status code");
+ 		return NULL;
+ 	}
+ 
+ 	struct json_object * content = json_object_object_get(reply, "content");
+-	if (is_error(content)) {
++	if (content == NULL) {
+ 		LOG(LOG_ERROR, "ttrss_api::run_op: no content part in answer from server");
+ 		return NULL;
+ 	}
