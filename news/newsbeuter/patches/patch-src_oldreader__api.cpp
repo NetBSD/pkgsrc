e19e15cb138d612350d83e080924429fbfa2eb07
nia <nia@pkgsrc.org>
2019-07-02 07:37:54 +0000

    newsbeuter: Fix build with new json-c.
    https://github.com/json-c/json-c/issues/304
    
    XXX pullup?

---
+++ news/newsbeuter/patches/patch-src_oldreader__api.cpp
@@ -0,0 +1,17 @@
+$NetBSD: patch-src_oldreader__api.cpp,v 1.1 2019/07/02 07:37:54 nia Exp $
+
+Update for newer json-c API.
+
+https://github.com/json-c/json-c/issues/304
+
+--- src/oldreader_api.cpp.orig	2015-02-19 10:56:59.000000000 +0000
++++ src/oldreader_api.cpp
+@@ -134,7 +134,7 @@ std::vector<tagged_feedurl> oldreader_ap
+ 	// TODO: parse result
+ 
+ 	struct json_object * reply = json_tokener_parse(result.c_str());
+-	if (is_error(reply)) {
++	if (reply == NULL) {
+ 		LOG(LOG_ERROR, "oldreader_api::get_subscribed_urls: failed to parse response as JSON.");
+ 		return urls;
+ 	}
