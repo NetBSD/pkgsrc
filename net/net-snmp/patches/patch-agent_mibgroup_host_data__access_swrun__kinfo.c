f424f6af70ff21ee708af7537a642dac3a6b7492
bsiegert <bsiegert@pkgsrc.org>
2019-07-02 09:40:31 +0000

    Pullup ticket #5983 - requested by kamil net/net-snmp: build fix for PR pkg/54333
    Revisions pulled up:
    - net/net-snmp/distinfo                                         1.94
    - net/net-snmp/patches/patch-agent_mibgroup_host_data__access_swrun__kinfo.c 1.4
    
    ---
       Module Name:    pkgsrc
       Committed By:   kamil
       Date:           Sun Jun 30 21:43:25 UTC 2019
    
       Modified Files:
               pkgsrc/net/net-snmp: distinfo
               pkgsrc/net/net-snmp/patches:
                   patch-agent_mibgroup_host_data__access_swrun__kinfo.c
    
       Log Message:
       net-snmp: Fix build on NetBSD 8.99.49
    
       Make usage of LSDEAD conditional.

--- net/net-snmp/patches/patch-agent_mibgroup_host_data__access_swrun__kinfo.c
+++ net/net-snmp/patches/patch-agent_mibgroup_host_data__access_swrun__kinfo.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-agent_mibgroup_host_data__access_swrun__kinfo.c,v 1.3 2014/12/31 10:06:57 adam Exp $
+$NetBSD: patch-agent_mibgroup_host_data__access_swrun__kinfo.c,v 1.3.38.1 2019/07/02 09:40:31 bsiegert Exp $
 
 --- agent/mibgroup/host/data_access/swrun_kinfo.c.orig	2014-12-08 20:23:22.000000000 +0000
 +++ agent/mibgroup/host/data_access/swrun_kinfo.c
@@ -13,3 +13,13 @@ $NetBSD: patch-agent_mibgroup_host_data__access_swrun__kinfo.c,v 1.3 2014/12/31 
  #include <net-snmp/net-snmp-config.h>
  
  #include <stdio.h>
+@@ -286,7 +290,9 @@ netsnmp_arch_swrun_container_load( netsn
+ 	case LSSUSPENDED:
+         case LSSTOP:  entry->hrSWRunStatus = HRSWRUNSTATUS_NOTRUNNABLE;
+                       break;
++#ifdef LSDEAD
+ 	case LSDEAD:
++#endif
+         case LSZOMB:  entry->hrSWRunStatus = HRSWRUNSTATUS_INVALID;
+ 		      break;
+         default:   
