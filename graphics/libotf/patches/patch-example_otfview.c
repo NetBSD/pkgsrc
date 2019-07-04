ffd08f5e35aa17a34e6a50ae370327b7f5bfdafe
ryoon <ryoon@pkgsrc.org>
2019-07-04 13:26:59 +0000

    Bump PKGREVISION
    * Use 1024 instead of NAME_MAX. NAME_MAX is not defined OmniOS-derived OS
      and MAXNAMELEN is not portable too, _POSIX_NAME_MAX is too short.
      Discussed with tron@.
    * Simplyfy my previous code to fix overrun

--- graphics/libotf/patches/patch-example_otfview.c
+++ graphics/libotf/patches/patch-example_otfview.c
@@ -1,37 +1,40 @@
-$NetBSD: patch-example_otfview.c,v 1.2 2019/05/29 12:54:10 ryoon Exp $
+$NetBSD: patch-example_otfview.c,v 1.3 2019/07/04 13:26:59 ryoon Exp $
 
 * Fix a runtime crash on NetBSD.
 
 --- example/otfview.c.orig	2015-09-02 13:39:25.000000000 +0000
 +++ example/otfview.c
-@@ -28,6 +28,7 @@ write to the Free Software Foundation, I
- #include <sys/stat.h>
- #include <unistd.h>
- #include <libgen.h>
-+#include <limits.h>
+@@ -51,6 +51,8 @@ write to the Free Software Foundation, I
  
- #include "config.h"
- #ifdef HAVE_ALLOCA_H
-@@ -1603,7 +1604,10 @@ ExposeProc (Widget w, XEvent *event, Str
+ #include <otf.h>
+ 
++#define MY_NAME_MAX 1024
++
+ #define CAST_FROM_XTPOINTER(TYPE, DATA, VAR)	\
+   do {						\
+     long TYPE temp = (long TYPE) (DATA);	\
+@@ -1603,7 +1605,9 @@ ExposeProc (Widget w, XEvent *event, Str
  {
    XTextProperty text_prop;
    char *pname = "otfview";
 -  char *fname = basename (filename);
 +  char *tmpfname = basename (filename);
-+  size_t len = strlen(tmpfname);
-+  char fname[NAME_MAX];
-+  snprintf(fname, len + 1, "%s", tmpfname);
++  char fname[MY_NAME_MAX];
++  snprintf(fname, sizeof(fname), "%s", tmpfname);
    char *name = alloca (strlen (fname) + 3 + strlen (pname) + 1);
  
    sprintf (name, "%s - %s", pname, fname);
-@@ -1735,7 +1739,9 @@ main (int argc, char **argv)
+@@ -1735,9 +1739,11 @@ main (int argc, char **argv)
      char title[256];
      Arg arg[1];
  
 -    filename = basename (filename);
 +    char *tmpfilename = basename (filename);
-+    size_t len = strlen(tmpfilename);
-+    snprintf(filename, len + 1, "%s", tmpfilename);
++    char fname[MY_NAME_MAX];
++    snprintf(fname, sizeof(fname), "%s", tmpfilename);
      sprintf (title, "%s family:%s style:%s",
- 	     filename, face->family_name, face->style_name);
+-	     filename, face->family_name, face->style_name);
++	     fname, face->family_name, face->style_name);
      XtSetArg (arg[0], XtNtitle, title);
+     XtSetValues (shell, arg, 1);
+   }
