d681c08e4441bfac29ac38a4d883c012872acda0
jaapb <jaapb@pkgsrc.org>
2019-07-04 09:18:37 +0000

    Removed camlp4 dependency from databases/pgocaml and revbump

--- databases/pgocaml/buildlink3.mk
+++ databases/pgocaml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2019/05/13 12:12:33 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2019/07/04 09:18:37 jaapb Exp $
 
 BUILDLINK_TREE+=	pgocaml
 
@@ -6,11 +6,10 @@ BUILDLINK_TREE+=	pgocaml
 PGOCAML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pgocaml+=	pgocaml>=3.0
-BUILDLINK_ABI_DEPENDS.pgocaml+=	pgocaml>=3.2
+BUILDLINK_ABI_DEPENDS.pgocaml+=	pgocaml>=3.2nb1
 BUILDLINK_PKGSRCDIR.pgocaml?=	../../databases/pgocaml
 
 .include "../../devel/ocaml-re/buildlink3.mk"
-.include "../../lang/camlp4/buildlink3.mk"
 .include "../../textproc/ocaml-csv/buildlink3.mk"
 .include "../../time/ocaml-calendar/buildlink3.mk"
 .endif	# PGOCAML_BUILDLINK3_MK
