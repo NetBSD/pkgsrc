9ed99ed810543041b3e6bbcc5b45773acd5c9560
ryoon <ryoon@pkgsrc.org>
2019-07-04 13:46:46 +0000

    Update to 1.2.8
    Changelog:
    Not available

--- lang/scala-sbt/patches/patch-bin_sbt-launch-lib.bash
+++ lang/scala-sbt/patches/patch-bin_sbt-launch-lib.bash
@@ -1,8 +1,8 @@
-$NetBSD: patch-bin_sbt-launch-lib.bash,v 1.1 2018/12/16 09:54:41 ryoon Exp $
+$NetBSD: patch-bin_sbt-launch-lib.bash,v 1.2 2019/07/04 13:46:47 ryoon Exp $
 
 * Use pkgsrc Java
 
---- bin/sbt-launch-lib.bash.orig	2018-11-30 20:39:42.000000000 +0000
+--- bin/sbt-launch-lib.bash.orig	2018-12-30 07:45:19.000000000 +0000
 +++ bin/sbt-launch-lib.bash
 @@ -10,7 +10,7 @@ declare -a residual_args
  declare -a java_args
@@ -11,7 +11,7 @@ $NetBSD: patch-bin_sbt-launch-lib.bash,v 1.1 2018/12/16 09:54:41 ryoon Exp $
 -declare java_cmd=java
 +declare java_cmd=@JAVA_BINPREFIX@-java
  declare java_version
- declare init_sbt_version="1.2.7"
+ declare init_sbt_version="1.2.8"
  declare sbt_default_mem=1024
 @@ -27,7 +27,7 @@ while [ -h "$SCRIPT" ] ; do
    fi
