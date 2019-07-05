3be3d3db0a4b44b39e7cd4ba6b624c6bc3bd7c99
nia <nia@pkgsrc.org>
2019-07-05 15:21:18 +0000

    audacious-plugins: Disable options / dependencies that were removed.
    Should fix the musepack conflicts with VLC, since I can't find musepack
    support existing in audacious any more (maybe through ffmpeg).

--- audio/audacious-plugins/options.mk
+++ audio/audacious-plugins/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.8 2017/09/27 13:33:07 wiz Exp $
+# $NetBSD: options.mk,v 1.9 2019/07/05 15:21:18 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.audacious-plugins
-PKG_SUPPORTED_OPTIONS=	esound inet6 jack midi pulseaudio sid
-PKG_SUGGESTED_OPTIONS+=	inet6 pulseaudio jack
+PKG_SUPPORTED_OPTIONS=	jack pulseaudio sid
+PKG_SUGGESTED_OPTIONS+=	pulseaudio jack
 
 .include "../../mk/bsd.options.mk"
 
@@ -14,32 +14,12 @@ PLIST.sid=	yes
 CONFIGURE_ARGS+=	--disable-sid
 .endif
 
-PLIST_VARS+=	esd
-.if !empty(PKG_OPTIONS:Mesound)
-.  include "../../audio/esound/buildlink3.mk"
-PLIST.esd=	yes
-.else
-CONFIGURE_ARGS+=	--disable-esd
-.endif
-
-.if !empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGS+=	--enable-ipv6
-.endif
-
 PLIST_VARS+=	jack
 .if !empty(PKG_OPTIONS:Mjack)
 .  include "../../audio/jack/buildlink3.mk"
 PLIST.jack=	yes
 .else
 CONFIGURE_ARGS+=	--disable-jack
-.endif
-
-PLIST_VARS+=	midi
-.if !empty(PKG_OPTIONS:Mmidi)
-BUILD_DEPENDS+=		timidity>=0:../../audio/timidity
-PLIST.midi=	yes
-.else
-CONFIGURE_ARGS+=	--disable-timidity
 .endif
 
 PLIST_VARS+=	pulse
