2556d902e323c11e1a8844ff331bc71ae9def61c
nia <nia@pkgsrc.org>
2019-07-06 13:17:00 +0000

    audacious: Update to 3.10.0
    Upstream decided to drop GTK3 support.
    GTK3 was previously the default UI in pkgsrc.
    
    I've changed the deafult to Qt5 to retain HiDPI support.
    GTK2 is also available for those who want it.
    
    New plugins: sox-resampler, silence-removal, ffaudio (used for formats
    not handled by other plugins like opus), alsa

--- audio/audacious-plugins/options.mk
+++ audio/audacious-plugins/options.mk
@@ -1,17 +1,19 @@
-# $NetBSD: options.mk,v 1.9 2019/07/05 15:21:18 nia Exp $
+# $NetBSD: options.mk,v 1.10 2019/07/06 13:17:01 nia Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.audacious-plugins
-PKG_SUPPORTED_OPTIONS=	jack pulseaudio sid
-PKG_SUGGESTED_OPTIONS+=	pulseaudio jack
+PKG_OPTIONS_VAR=		PKG_OPTIONS.audacious-plugins
+PKG_SUPPORTED_OPTIONS+=		alsa jack pulseaudio lirc
+PKG_SUPPORTED_OPTIONS+=		adplug sidplay fluidsynth libgme modplug
+PKG_SUGGESTED_OPTIONS.Linux+=	alsa lirc
+PKG_OPTIONS_LEGACY_OPTS+=	sid:sidplay
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	sid
-.if !empty(PKG_OPTIONS:Msid)
-.  include "../../audio/libsidplay/buildlink3.mk"
-PLIST.sid=	yes
+PLIST_VARS+=	alsa
+.if !empty(PKG_OPTIONS:Malsa)
+.  include "../../audio/alsa-lib/buildlink3.mk"
+PLIST.alsa=	yes
 .else
-CONFIGURE_ARGS+=	--disable-sid
+CONFIGURE_ARGS+=	--disable-alsa
 .endif
 
 PLIST_VARS+=	jack
@@ -28,4 +30,52 @@ PLIST_VARS+=	pulse
 PLIST.pulse=	yes
 .else
 CONFIGURE_ARGS+=	--disable-pulse
+.endif
+
+PLIST_VARS+=	lirc
+.if !empty(PKG_OPTIONS:Mlirc)
+.  include "../../comms/lirc/buildlink3.mk"
+PLIST.lirc=	yes
+.else
+CONFIGURE_ARGS+=	--disable-lirc
+.endif
+
+PLIST_VARS+=	adplug
+.if !empty(PKG_OPTIONS:Madplug)
+.  include "../../audio/adplug/buildlink3.mk"
+PLIST.adplug=	yes
+.else
+CONFIGURE_ARGS+=	--disable-adplug
+.endif
+
+PLIST_VARS+=	sid
+.if !empty(PKG_OPTIONS:Msidplay)
+.  include "../../audio/libsidplay/buildlink3.mk"
+PLIST.sid=	yes
+.else
+CONFIGURE_ARGS+=	--disable-sid
+.endif
+
+PLIST_VARS+=	fluidsynth
+.if !empty(PKG_OPTIONS:Mfluidsynth)
+.  include "../../audio/fluidsynth/buildlink3.mk"
+PLIST.fluidsynth=	yes
+.else
+CONFIGURE_ARGS+=	--disable-amidiplug
+.endif
+
+PLIST_VARS+=	libgme
+.if !empty(PKG_OPTIONS:Mlibgme)
+.  include "../../audio/game-music-emu/buildlink3.mk"
+PLIST.libgme=	yes
+.else
+CONFIGURE_ARGS+=	--disable-console
+.endif
+
+PLIST_VARS+=	modplug
+.if !empty(PKG_OPTIONS:Mmodplug)
+.  include "../../audio/libmodplug/buildlink3.mk"
+PLIST.modplug=	yes
+.else
+CONFIGURE_ARGS+=	--disable-modplug
 .endif
