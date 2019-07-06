e8166c02bd9853fe29edb4051367c5a98c57bd8a
nia <nia@pkgsrc.org>
2019-07-06 13:17:00 +0000

    audacious: Update to 3.10.0
    Upstream decided to drop GTK3 support.
    GTK3 was previously the default UI in pkgsrc.
    
    I've changed the deafult to Qt5 to retain HiDPI support.
    GTK2 is also available for those who want it.
    
    New plugins: sox-resampler, silence-removal, ffaudio (used for formats
    not handled by other plugins like opus), alsa

--- audio/audacious/buildlink3.mk
+++ audio/audacious/buildlink3.mk
@@ -1,22 +1,31 @@
-# $NetBSD: buildlink3.mk,v 1.39 2018/11/14 22:20:59 kleink Exp $
+# $NetBSD: buildlink3.mk,v 1.40 2019/07/06 13:17:00 nia Exp $
 
 BUILDLINK_TREE+=	audacious
 
 .if !defined(AUDACIOUS_BUILDLINK3_MK)
 AUDACIOUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.audacious+=	audacious>=3.5.1
-BUILDLINK_ABI_DEPENDS.audacious+=	audacious>=3.5.2nb10
+BUILDLINK_API_DEPENDS.audacious+=	audacious>=3.10.1
+BUILDLINK_ABI_DEPENDS.audacious+=	audacious>=3.10.1
 BUILDLINK_PKGSRCDIR.audacious?=		../../audio/audacious
 
-.include "../../devel/atk/buildlink3.mk"
+.include "../../mk/bsd.fast.prefs.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/pango/buildlink3.mk"
+.include "../../devel/gobject-introspection/buildlink3.mk"
+
+pkgbase := audacious
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.audacious:Mdbus)
 .include "../../sysutils/dbus/buildlink3.mk"
-.include "../../sysutils/dbus-glib/buildlink3.mk"
-.include "../../textproc/libxml2/buildlink3.mk"
-.include "../../x11/gtk3/buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.audacious:Mgtk2)
+.include "../../x11/gtk2/buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.audacious:Mqt5)
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif
 .endif # AUDACIOUS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-audacious
