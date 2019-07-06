462392151b8be4887d83a93d46a01b9703714f57
adam <adam@pkgsrc.org>
2019-07-06 22:42:25 +0000

    py-meson: updated to 0.51.0
    0.51.0:
    (C) Preprocessor flag handling
    Sanity checking compilers with user flags
    New sourceset module
    n_debug=if-release and buildtype=plain means no asserts
    target_type in build_targets accepts the value 'shared_module'
    New modules kwarg for python.find_installation
    Support for the Intel Compiler on Windows (ICL)
    Added basic support for the Xtensa CPU toolchain
    Dependency objects now have a get_variable method
    CMake prefix path overrides
    Tests that should fail but did not are now errors
    New target keyword argument: link_language
    New module to parse kconfig output files
    Add new meson subprojects foreach command
    Added c17 and c18 as c_std values for recent GCC and Clang Versions
    gpgme dependency now supports gpgme-config
    Can link against custom targets
    Removed the deprecated --target-files API
    Generators have a new depends keyword argument
    Specifying options per mer machine
    subproject.get_variable() now accepts a fallback argument
    Add keyword static to find_library
    Fortran include statements recursively parsed
    CMake subprojects
    Multipe cross files can be specified

--- devel/py-meson/patches/patch-mesonbuild_compilers_compilers.py
+++ devel/py-meson/patches/patch-mesonbuild_compilers_compilers.py
@@ -1,11 +1,11 @@
-$NetBSD: patch-mesonbuild_compilers_compilers.py,v 1.6 2019/03/11 12:08:19 jperkin Exp $
+$NetBSD: patch-mesonbuild_compilers_compilers.py,v 1.7 2019/07/06 22:42:25 adam Exp $
 
 Support SunOS-specific GCC behaviour.
 Limit GNU ld options correctly.
 
---- mesonbuild/compilers/compilers.py.orig	2019-01-23 16:46:09.000000000 +0000
+--- mesonbuild/compilers/compilers.py.orig	2019-06-16 18:54:18.000000000 +0000
 +++ mesonbuild/compilers/compilers.py
-@@ -174,6 +174,14 @@ apple_buildtype_linker_args = {'plain': 
+@@ -198,6 +198,14 @@ apple_buildtype_linker_args = {'plain': 
                                 'custom': [],
                                 }
  
@@ -20,16 +20,16 @@ Limit GNU ld options correctly.
  gnulike_buildtype_linker_args = {'plain': [],
                                   'debug': [],
                                   'debugoptimized': [],
-@@ -1210,7 +1218,7 @@ class Compiler:
-                     paths = paths + ':' + padding
-             args.append('-Wl,-rpath,' + paths)
+@@ -1304,7 +1312,7 @@ class Compiler:
+         if mesonlib.is_sunos():
+             return args
  
 -        if get_compiler_is_linuxlike(self):
 +        if get_compiler_uses_gnuld(self):
              # Rpaths to use while linking must be absolute. These are not
              # written to the binary. Needed only with GNU ld:
              # https://sourceware.org/bugzilla/show_bug.cgi?id=16936
-@@ -1295,6 +1303,7 @@ class CompilerType(enum.Enum):
+@@ -1395,6 +1403,7 @@ class CompilerType(enum.Enum):
      GCC_OSX = 1
      GCC_MINGW = 2
      GCC_CYGWIN = 3
@@ -37,8 +37,8 @@ Limit GNU ld options correctly.
  
      CLANG_STANDARD = 10
      CLANG_OSX = 11
-@@ -1318,6 +1327,10 @@ class CompilerType(enum.Enum):
-         return self.name in ('GCC_OSX', 'CLANG_OSX', 'ICC_OSX')
+@@ -1422,6 +1431,10 @@ class CompilerType(enum.Enum):
+         return self.name in ('GCC_OSX', 'CLANG_OSX', 'ICC_OSX', 'PGI_OSX')
  
      @property
 +    def is_sunos_compiler(self):
@@ -46,9 +46,9 @@ Limit GNU ld options correctly.
 +
 +    @property
      def is_windows_compiler(self):
-         return self.name in ('GCC_MINGW', 'GCC_CYGWIN', 'CLANG_MINGW', 'ICC_WIN', 'ARM_WIN', 'CCRX_WIN')
+         return self.name in ('GCC_MINGW', 'GCC_CYGWIN', 'CLANG_MINGW', 'ICC_WIN', 'ARM_WIN', 'CCRX_WIN', 'PGI_WIN')
  
-@@ -1330,7 +1343,7 @@ def get_macos_dylib_install_name(prefix,
+@@ -1434,7 +1447,7 @@ def get_macos_dylib_install_name(prefix,
      return '@rpath/' + install_name
  
  def get_gcc_soname_args(compiler_type, prefix, shlib_name, suffix, soversion, darwin_versions, is_shared_module):
@@ -57,7 +57,7 @@ Limit GNU ld options correctly.
          sostr = '' if soversion is None else '.' + soversion
          return ['-Wl,-soname,%s%s.%s%s' % (prefix, shlib_name, suffix, sostr)]
      elif compiler_type.is_windows_compiler:
-@@ -1474,6 +1487,8 @@ class GnuLikeCompiler(abc.ABC):
+@@ -1912,6 +1925,8 @@ class GnuLikeCompiler(abc.ABC):
      def get_buildtype_linker_args(self, buildtype):
          if self.compiler_type.is_osx_compiler:
              return apple_buildtype_linker_args[buildtype]
@@ -66,7 +66,7 @@ Limit GNU ld options correctly.
          return gnulike_buildtype_linker_args[buildtype]
  
      @abc.abstractmethod
-@@ -1538,6 +1553,8 @@ class GnuLikeCompiler(abc.ABC):
+@@ -1976,6 +1991,8 @@ class GnuLikeCompiler(abc.ABC):
          if self.compiler_type.is_osx_compiler:
              # Apple ld
              return ['-Wl,-undefined,dynamic_lookup']
