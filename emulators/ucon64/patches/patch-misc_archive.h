cd64e07525793ed8d3f1a18432f6364f068aaa98
wiz <wiz@pkgsrc.org>
2019-07-07 22:47:57 +0000

    ucon64: update to 2.2.0.
    With much help from upstream to get this compiling nicely on NetBSD.
    
    2.2.0
    - added (GB) --gp2bmp convert Game Boy Printer data (produced by --xmccl) to
    BMP (by dbjh; based on version of gblnk (by Chris McCormick) improved by
    Difegue. Many thanks to Difegue for providing me with Game Boy Printer data.
    Thanks to Martin Eyre for documenting his findings about Game Boy Printer
    commands)
    - added (SNES) --sminis=ID convert *.srm (SRAM) file to SNES/Super Famicom
    Classic Mini (hakchi2) format for game with identifier ID (by dbjh)
    - added (SNES) --smini2srm convert SNES/Super Famicom Classic Mini (hakchi2)
    SRAM data to *.srm (SRAM) file (by dbjh)
    - added support for NetBSD (by dbjh; thanks to Thomas Klausner for his help)
    - updated support for OpenBSD (64-bit) (by dbjh)
    - updated (all) made -o also work for --rrom, --r83 and --rjoliet, just like
    the other rename options (--rdat, --rl and --ru) (by dbjh)
    - updated (SNES) improved reliability of GDSF communication for PCs with a
    parallel port that supports ECP (built-in or PCIe parallel port card). At last.
    It is essential to correctly specify ecr_offset for this to work (by dbjh; many
    thanks to Mystic_Merlin for testing)
    - updated (SNES) improved --ufosd by adding specific support for 10 and 12 Mbit
    HiROMs and implementing a generic way to set the SRAM mapping bytes (by dbjh;
    thanks to Revenant for the information and Trenton_net for testing)
    - added (SNES) --xufosd send ROM to Super UFO Pro 8 SD. Be sure to use a tool
    like Zadig to add libusb-0.1 (libusb-win32) support (by dbjh; thanks to
    Revenant for the information and Trenton_net and Revenant for testing)
    - updated (SNES) added NTSC/PAL fix for Soul Blazer F/G and Super Metroid E (by
    dbjh; thanks to Mystic_Merlin)
    - added (SNES) work-around for using the Game Doctor SF6 protocol for parallel
    ports that do not support reading from the Control register. It is good enough
    to at least allow dumping saver data and SRAM. To enable the new functionality
    assign a value other than 0 to the variable gd6_send_byte_delay. See the FAQ
    question 55 for details (by dbjh; many thanks to NorQue and Mystic_Merlin for
    testing)
    - updated (SNES) made --gd3 specify a pass-through mapping for LoROM DSP games
    (by dbjh)
    - updated (SNES) added proper recognition of Pro Fighter headers of LoROM DSP
    games with 2, 8 or 32 kB SRAM (by dbjh)
    - updated (SNES) added interpretation of remaining part of GD3 header (DRAM and
    SRAM mapping) to --dbuh (by dbjh)
    - updated (SNES & Genesis) fixed split file detection for formats other than
    (M)GD and MGH which I broke in 2.1.0 (by dbjh)
    - added (all) --split=N split ROM in parts of N Bytes (including possible
    header). This is a generic file splitting option. Use -s, optionally in
    combination with --ssize=SIZE to split files for use with a specific console or
    backup unit (by dbjh)
    - added (SNES) support for Super Magicom/SMC IC2 file format. This format is
    used by several ancient backup units, including the Twin Supercom, Future
    Supercom Pro.9 and UFO Super Drive PRO 6 HYPER VERSION in order to run HiROM
    games up to 16 Mbit. Use --ic2 to convert a file to Super Magicom/SMC IC2
    format (by dbjh; thanks to Mystic_Merlin for info and testing)
    - updated (SNES) fixed bug in --ufo that caused it to produce incorrect headers
    for games that do not use SRAM (by dbjh; thanks to Mystic_Merlin for feedback
    and testing)
    - updated (SNES) fixed bug in -j for files in UFO format smaller than or equal
    to 8 Mbit (by dbjh)

---
+++ emulators/ucon64/patches/patch-misc_archive.h
@@ -0,0 +1,23 @@
+$NetBSD: patch-misc_archive.h,v 1.1 2019/07/07 22:47:57 wiz Exp $
+
+Patch from upstream to improve build on NetBSD.
+
+--- misc/archive.h.orig	2017-05-05 09:40:40.000000000 +0000
++++ misc/archive.h
+@@ -1,7 +1,7 @@
+ /*
+ archive.h - g(un)zip and unzip support
+ 
+-Copyright (c) 2001 - 2004, 2015 - 2017 dbjh
++Copyright (c) 2001 - 2004, 2015 - 2017, 2019 dbjh
+ 
+ 
+ This program is free software; you can redistribute it and/or modify
+@@ -58,6 +58,7 @@ extern off_t fsizeof2 (const char *filen
+ #define fseek(FILE, OFFSET, MODE) fseek2(FILE, OFFSET, MODE)
+ #define fread(BUF, SIZE, NUM, FILE) fread2(BUF, SIZE, NUM, FILE)
+ #define fgetc(FILE) fgetc2(FILE)
++#undef  fgets                                   // necessary on (at least) NetBSD
+ #define fgets(BUF, MAXLEN, FILE) fgets2(BUF, MAXLEN, FILE)
+ #undef  feof                                    // necessary on (at least) Cygwin
+ #define feof(FILE) feof2(FILE)
