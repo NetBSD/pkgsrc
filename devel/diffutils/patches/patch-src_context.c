0f3d9ffaa7e5e3194bda82c932119144d83a4afc
schwarz <schwarz@pkgsrc.org>
2019-07-07 18:02:43 +0000

    Updated devel/diffutils to 3.7
    Changes: bug fixes, diff --color now produces output compatible with less -R.
    (cf. https://savannah.gnu.org/forum/forum.php?forum_id=9341)

--- devel/diffutils/patches/patch-src_context.c
+++ devel/diffutils/patches/patch-src_context.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-src_context.c,v 1.1 2017/09/08 09:42:34 adam Exp $
+$NetBSD: patch-src_context.c,v 1.2 2019/07/07 18:02:44 schwarz Exp $
 
 date: 2009-03-10 23:06:06 +0900;  author: apb;  state: Exp;
 Fix a build failure on platforms where time_t is larger than long, by
 using long long instead.  (Not using time_t, because there's no easy and
 portable printf format for time_t.)
 
---- src/context.c.orig	2012-04-07 01:25:21.000000000 +0200
-+++ src/context.c	2012-04-07 01:27:52.000000000 +0200
-@@ -57,8 +57,8 @@
+--- src/context.c.orig	2018-12-31 07:22:10.000000000 +0100
++++ src/context.c	2019-07-07 00:38:26.000000000 +0200
+@@ -58,8 +58,8 @@
  	  if (LONG_MIN <= TYPE_MINIMUM (time_t)
  	      && TYPE_MAXIMUM (time_t) <= LONG_MAX)
  	    {
