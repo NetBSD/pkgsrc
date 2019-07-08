392592bcf47c402bc0cede33b9c5c433554fd27e
taca <taca@pkgsrc.org>
2019-07-08 13:18:52 +0000

    lang/php72: update to 7.2.20
    Update php72 to 7.2.20.
    
    04 Jul 2019, PHP 7.2.20
    
    - Core:
      . Fixed bug #76980 (Interface gets skipped if autoloader throws an exception).
        (Nikita)
    
    - DOM:
      . Fixed bug #78025 (segfault when accessing properties of DOMDocumentType).
        (cmb)
    
    - MySQLi:
      . Fixed bug #77956 (When mysqli.allow_local_infile = Off, use a meaningful
        error message). (Sjon Hortensius)
      . Fixed bug #38546 (bindParam incorrect processing of bool types).
        (camporter)
    
    - Opcache:
      . Fixed bug #78106 (Path resolution fails if opcache disabled during request).
        (Nikita)
      . Fixed bug #78185 (File cache no longer works). (Dmitry)
    
    - OpenSSL:
      . Fixed bug #78079 (openssl_encrypt_ccm.phpt fails with OpenSSL 1.1.1c).
        (Jakub Zelenka)
    
    - Sockets:
      . Fixed bug #78038 (Socket_select fails when resource array contains
        references). (Nikita)
    
    - Standard:
      . Fixed bug #77135 (Extract with EXTR_SKIP should skip $this).
        (Craig Duncan, Dmitry)
      . Fixed bug ##77937	(preg_match failed). (cmb, Anatol)
    
    - Zip:
      . Fixed bug #76345 (zip.h not found). (Michael Maroszek)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.260 2019/06/01 15:36:02 taca Exp $
+# $NetBSD: phpversion.mk,v 1.261 2019/07/08 13:18:52 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP71_VERSION=	7.1.30
-PHP72_VERSION=	7.2.19
+PHP72_VERSION=	7.2.20
 PHP73_VERSION=	7.3.6
 
 # Define initial release of major version.
