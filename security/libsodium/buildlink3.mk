788bb3aaef19ec8922603e8f763cdb2612f65e32
schmonz <schmonz@pkgsrc.org>
2019-07-10 12:56:50 +0000

    Add include/sodium to BUILDLINK_INCDIRS.libsodium.

--- security/libsodium/buildlink3.mk
+++ security/libsodium/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2016/02/07 10:59:18 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/07/10 12:56:50 schmonz Exp $
 
 BUILDLINK_TREE+=	libsodium
 
@@ -7,6 +7,8 @@ LIBSODIUM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libsodium+=	libsodium>=0.3
 BUILDLINK_PKGSRCDIR.libsodium?=		../../security/libsodium
+BUILDLINK_INCDIRS.libsodium+=		include
+BUILDLINK_INCDIRS.libsodium+=		include/sodium
 .endif	# LIBSODIUM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libsodium
