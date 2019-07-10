75fd6e47f8d629315f19df9f22f461c0c96a849d
schmonz <schmonz@pkgsrc.org>
2019-07-10 11:20:02 +0000

    Build and install shared libs. Bump BUILDLINK_API_DEPENDS and PKGREVISION.

--- net/fehqlibs/buildlink3.mk
+++ net/fehqlibs/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/12/14 22:51:32 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/07/10 11:20:02 schmonz Exp $
 
 BUILDLINK_TREE+=	fehqlibs
 
 .if !defined(FEHQLIBS_BUILDLINK3_MK)
 FEHQLIBS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fehqlibs+=fehqlibs>=0.9.10
+BUILDLINK_API_DEPENDS.fehqlibs+=fehqlibs>=0.9.12.10
 BUILDLINK_PKGSRCDIR.fehqlibs?=	../../net/fehqlibs
-BUILDLINK_DEPMETHOD.fehqlibs?=	build
 .endif	# FEHQLIBS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-fehqlibs
