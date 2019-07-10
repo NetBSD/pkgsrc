20a36febeb65f27cb74a68508476d86147c00723
schmonz <schmonz@pkgsrc.org>
2019-07-10 12:59:53 +0000

    Specify include/qlibs and lib/qlibs paths once, in fehqlibs/buildlink3.mk.

--- net/fehqlibs/buildlink3.mk
+++ net/fehqlibs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2019/07/10 11:20:02 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2019/07/10 12:59:54 schmonz Exp $
 
 BUILDLINK_TREE+=	fehqlibs
 
@@ -7,6 +7,8 @@ FEHQLIBS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.fehqlibs+=fehqlibs>=0.9.12.10
 BUILDLINK_PKGSRCDIR.fehqlibs?=	../../net/fehqlibs
+BUILDLINK_INCDIRS.fehqlibs+=	include/qlibs
+BUILDLINK_LIBDIRS.fehqlibs+=	lib/qlibs
 .endif	# FEHQLIBS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-fehqlibs
