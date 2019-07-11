1209447f8c05bdc788e4f86c7f7f3f957dae9615
ryoon <ryoon@pkgsrc.org>
2019-07-11 11:32:40 +0000

    Update to 68.0
    Changelog:
    
    New
        Dark mode in reader view expands so that windows are also dark on the controls, sidebars and toolbars.
    
        Improved extension security and discovery:
            New reporting feature in about:addons allows you to report security and performance issues with extensions and themes.
            Redesigned extensions dashboard in about:addons provides easy access to information about your extensions, including data and settings access required by each extension.
            Find high quality, secure extensions via the Recommended Extensions program in about:addons, which now displays user count and ratings for each extension. "Recommended” badges for these extensions also appear on AMO. More extensions will be added over time.
    
        Cryptomining and fingerprinting protections are added to strict content blocking settings in Privacy & Security preferences.
    
        WebRender will roll out to Windows 10 users with AMD graphics cards.
    
        Windows Background Intelligent Transfer Service (BITS) update download support, which allows Firefox update downloads to continue when Firefox is closed.
    
    Fixed
    
        Various security fixes
    
        Local files can no longer access other files in the same directory.
    
    Security fixes:
    #CVE-2019-9811: Sandbox escape via installation of malicious language pack
    #CVE-2019-11711: Script injection within domain through inner window reuse
    #CVE-2019-11712: Cross-origin POST requests can be made with NPAPI plugins by following 308 redirects
    #CVE-2019-11713: Use-after-free with HTTP/2 cached stream
    #CVE-2019-11714: NeckoChild can trigger crash when accessed off of main thread
    #CVE-2019-11729: Empty or malformed p256-ECDH public keys may trigger a segmentation fault
    #CVE-2019-11715: HTML parsing error can contribute to content XSS
    #CVE-2019-11716: globalThis not enumerable until accessed
    #CVE-2019-11717: Caret character improperly escaped in origins
    #CVE-2019-11718: Activity Stream writes unsanitized content to innerHTML
    #CVE-2019-11719: Out-of-bounds read when importing curve25519 private key
    #CVE-2019-11720: Character encoding XSS vulnerability
    #CVE-2019-11721: Domain spoofing through unicode latin 'kra' character
    #CVE-2019-11730: Same-origin policy treats all files in a directory as having the same-origin
    #CVE-2019-11723: Cookie leakage during add-on fetching across private browsing boundaries
    #CVE-2019-11724: Retired site input.mozilla.org has remote troubleshooting permissions
    #CVE-2019-11725: Websocket resources bypass safebrowsing protections
    #CVE-2019-11727: PKCS#1 v1.5 signatures can be used for TLS 1.3
    #CVE-2019-11728: Port scanning through Alt-Svc header
    #CVE-2019-11710: Memory safety bugs fixed in Firefox 68
    #CVE-2019-11709: Memory safety bugs fixed in Firefox 68 and Firefox ESR 60.8

--- www/firefox/patches/patch-toolkit_modules_subprocess_subprocess__shared__unix.js
+++ www/firefox/patches/patch-toolkit_modules_subprocess_subprocess__shared__unix.js
@@ -1,16 +1,16 @@
-$NetBSD: patch-toolkit_modules_subprocess_subprocess__shared__unix.js,v 1.1 2019/04/15 08:31:10 pho Exp $
+$NetBSD: patch-toolkit_modules_subprocess_subprocess__shared__unix.js,v 1.2 2019/07/11 11:32:40 ryoon Exp $
 
 Fix broken native messaging on NetBSD and possibly other BSDs too:
 https://bugzilla.mozilla.org/show_bug.cgi?id=1543602
 
 Please remove this patch when the upstream issue is resolved.
 
---- toolkit/modules/subprocess/subprocess_shared_unix.js.orig	2019-04-11 04:52:37.712288660 +0000
+--- toolkit/modules/subprocess/subprocess_shared_unix.js.orig	2019-07-06 01:49:01.000000000 +0000
 +++ toolkit/modules/subprocess/subprocess_shared_unix.js
 @@ -12,7 +12,15 @@
-
+ 
  const LIBC = OS.Constants.libc;
-
+ 
 -const LIBC_CHOICES = ["libc.so", "libSystem.B.dylib", "a.out"];
 +/* libc.so isn't meant to be dlopen'ed. On Linux it's usually an ld
 + * script so one cannot dlopen it. On NetBSD (and possibly other
@@ -21,6 +21,6 @@ Please remove this patch when the upstream issue is resolved.
 + * the NULL handle, i.e. the one which NSPR calls "a.out".
 + */
 +const LIBC_CHOICES = ["a.out"];
-
+ 
  const unix = {
    pid_t: ctypes.int32_t,
