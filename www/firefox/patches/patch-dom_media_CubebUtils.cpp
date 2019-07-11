1947ebe94ccfeb11c3a0862c5dc45f463590c8b5
ryoon <ryoon@pkgsrc.org>
2019-07-11 11:32:40 +0000

    Update to 68.0
    Changelog:
    
    New
        Dark mode in reader view expands so that windows are also dark on the controls, sidebars and toolbars.
    
        Improved extension security and discovery:
            New reporting feature in about:addons allows you to report security and performance issues with extensions and themes.
            Redesigned extensions dashboard in about:addons provides easy access to information about your extensions, including data and settings access required by each extension.
            Find high quality, secure extensions via the Recommended Extensions program in about:addons, which now displays user count and ratings for each extension. "Recommended” badges for these extensions also appear on AMO. More extensions will be added over time.
    
        Cryptomining and fingerprinting protections are added to strict content blocking settings in Privacy & Security preferences.
    
        WebRender will roll out to Windows 10 users with AMD graphics cards.
    
        Windows Background Intelligent Transfer Service (BITS) update download support, which allows Firefox update downloads to continue when Firefox is closed.
    
    Fixed
    
        Various security fixes
    
        Local files can no longer access other files in the same directory.
    
    Security fixes:
    #CVE-2019-9811: Sandbox escape via installation of malicious language pack
    #CVE-2019-11711: Script injection within domain through inner window reuse
    #CVE-2019-11712: Cross-origin POST requests can be made with NPAPI plugins by following 308 redirects
    #CVE-2019-11713: Use-after-free with HTTP/2 cached stream
    #CVE-2019-11714: NeckoChild can trigger crash when accessed off of main thread
    #CVE-2019-11729: Empty or malformed p256-ECDH public keys may trigger a segmentation fault
    #CVE-2019-11715: HTML parsing error can contribute to content XSS
    #CVE-2019-11716: globalThis not enumerable until accessed
    #CVE-2019-11717: Caret character improperly escaped in origins
    #CVE-2019-11718: Activity Stream writes unsanitized content to innerHTML
    #CVE-2019-11719: Out-of-bounds read when importing curve25519 private key
    #CVE-2019-11720: Character encoding XSS vulnerability
    #CVE-2019-11721: Domain spoofing through unicode latin 'kra' character
    #CVE-2019-11730: Same-origin policy treats all files in a directory as having the same-origin
    #CVE-2019-11723: Cookie leakage during add-on fetching across private browsing boundaries
    #CVE-2019-11724: Retired site input.mozilla.org has remote troubleshooting permissions
    #CVE-2019-11725: Websocket resources bypass safebrowsing protections
    #CVE-2019-11727: PKCS#1 v1.5 signatures can be used for TLS 1.3
    #CVE-2019-11728: Port scanning through Alt-Svc header
    #CVE-2019-11710: Memory safety bugs fixed in Firefox 68
    #CVE-2019-11709: Memory safety bugs fixed in Firefox 68 and Firefox ESR 60.8

--- www/firefox/patches/patch-dom_media_CubebUtils.cpp
+++ www/firefox/patches/patch-dom_media_CubebUtils.cpp
@@ -1,13 +1,13 @@
-$NetBSD: patch-dom_media_CubebUtils.cpp,v 1.3 2019/01/29 16:28:22 ryoon Exp $
+$NetBSD: patch-dom_media_CubebUtils.cpp,v 1.4 2019/07/11 11:32:40 ryoon Exp $
 
---- dom/media/CubebUtils.cpp.orig	2019-01-18 00:20:24.000000000 +0000
+--- dom/media/CubebUtils.cpp.orig	2019-07-06 01:48:31.000000000 +0000
 +++ dom/media/CubebUtils.cpp
-@@ -151,7 +151,7 @@ const char kBrandBundleURL[] = "chrome:/
+@@ -163,7 +163,7 @@ const char kBrandBundleURL[] = "chrome:/
  
  const char* AUDIOSTREAM_BACKEND_ID_STR[] = {
      "jack",  "pulse",       "alsa",  "audiounit", "audioqueue", "wasapi",
 -    "winmm", "directsound", "sndio", "opensl",    "audiotrack", "kai"};
-+    "winmm", "directsound", "sndio", "opensl",    "audiotrack", "kai", "oss"};
++    "winmm", "directsound", "sndio", "opensl",    "audiotrack", "kai", "oss", "sun"};
  /* Index for failures to create an audio stream the first time. */
  const int CUBEB_BACKEND_INIT_FAILURE_FIRST =
      ArrayLength(AUDIOSTREAM_BACKEND_ID_STR);
