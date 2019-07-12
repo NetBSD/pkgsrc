ea04b792f47c4ece8964af3fe6d67c95c049dfb2
schmonz <schmonz@pkgsrc.org>
2019-07-12 15:34:54 +0000

    LDFLAGS.SunOS+=-lnsl -lsocket to fix SmartOS.

--- net/fehqlibs/buildlink3.mk
+++ net/fehqlibs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2019/07/10 12:59:54 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2019/07/12 15:34:54 schmonz Exp $
 
 BUILDLINK_TREE+=	fehqlibs
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.fehqlibs+=fehqlibs>=0.9.12.10
 BUILDLINK_PKGSRCDIR.fehqlibs?=	../../net/fehqlibs
 BUILDLINK_INCDIRS.fehqlibs+=	include/qlibs
 BUILDLINK_LIBDIRS.fehqlibs+=	lib/qlibs
+LDFLAGS.SunOS+=			-lnsl -lsocket
 .endif	# FEHQLIBS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-fehqlibs
