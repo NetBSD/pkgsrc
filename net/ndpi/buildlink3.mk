2d2ceaeba049a7f36cdd47d1899126dfe7e07f17
adam <adam@pkgsrc.org>
2019-07-12 09:30:34 +0000

    ndpi: updated to 2.8
    2.8 Stable
    
    New Supported Protocols and Services
    * Added Modbus over TCP dissector
    
    Improvements
    * Wireshark Lua plugin compatibility with Wireshark 3
    * Improved MDNS dissection
    * Improved HTTP response code handling
    * Full dissection of HTTP responses
    
    Fixes
    * Fixed false positive mining detection
    * Fixed invalid TCP DNS dissection
    * Releasing buffers upon realloc failures
    * ndpiReader: Prevents references after free
    * Endianness fixes
    * Fixed IPv6 HTTP traffic dissection
    * Fixed H.323 detection
    
    Other
    * Disabled ookla statistics which need to be improved
    * Support for custom protocol files of arbitrary length
    * Update radius.c to RFC2865

--- net/ndpi/buildlink3.mk
+++ net/ndpi/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/04/08 16:51:23 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/07/12 09:30:34 adam Exp $
 
 BUILDLINK_TREE+=	ndpi
 
 .if !defined(NDPI_BUILDLINK3_MK)
 NDPI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ndpi+=	ndpi>=1.7
+BUILDLINK_API_DEPENDS.ndpi+=	ndpi>=2.8
 BUILDLINK_PKGSRCDIR.ndpi?=	../../net/ndpi
 
 .include "../../textproc/json-c/buildlink3.mk"
