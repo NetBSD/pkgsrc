ccb65c099e8dca240b461b227f21740d211f677c
gutteridge <gutteridge@pkgsrc.org>
2019-07-13 00:40:21 +0000

    xfce4-cpugraph-plugin: update to 1.1.0 (2/7/2019)
    Change log:
    
    1.1.0 (2/7/2019)
    =====
    - Updated translations
    
    1.0.91 (22/6/2019)
    =====
    - Add option to disable the graph (Bug #15163)
    - Use css to change bar colors (Bug #15186)
    - Fix bars in horizontal mode
    - Updated translations

--- sysutils/xfce4-cpugraph-plugin/patches/patch-panel-plugin_os.c
+++ sysutils/xfce4-cpugraph-plugin/patches/patch-panel-plugin_os.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-panel-plugin_os.c,v 1.2 2017/06/23 16:47:23 youri Exp $
+$NetBSD: patch-panel-plugin_os.c,v 1.3 2019/07/13 00:40:22 gutteridge Exp $
 
 Fix NetBSD support.
 
---- panel-plugin/os.c.orig	2012-04-18 21:27:33.000000000 +0000
+--- panel-plugin/os.c.orig	2019-07-02 23:18:51.000000000 +0000
 +++ panel-plugin/os.c
-@@ -211,7 +211,7 @@ gboolean read_cpu_data( CpuData *data, g
- 	data[0].load = 0;
- 	for( i = 1 ; i <= nb_cpu ; i++ )
- 	{
--		cp_time1 = cp_time + CPUSTATE * (i - 1);
-+		cp_time1 = cp_time + CPUSTATES * (i - 1);
- 		used = cp_time1[CP_USER] + cp_time1[CP_NICE] + cp_time1[CP_SYS] + cp_time1[CP_INTR];
- 		total = used + cp_time1[CP_IDLE];
+@@ -226,7 +226,7 @@ read_cpu_data (CpuData *data, guint nb_c
+     data[0].load = 0;
+     for (i = 1; i <= nb_cpu; i++)
+     {
+-        cp_time1 = cp_time + CPUSTATE * (i - 1);
++        cp_time1 = cp_time + CPUSTATES * (i - 1);
+         used = cp_time1[CP_USER] + cp_time1[CP_NICE] + cp_time1[CP_SYS] + cp_time1[CP_INTR];
+         total = used + cp_time1[CP_IDLE];
  
