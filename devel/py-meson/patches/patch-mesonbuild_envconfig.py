3c97e3be0d03acdc5794f3091d668ae1d680a8ee
prlw1 <prlw1@pkgsrc.org>
2019-07-15 15:11:43 +0000

    Apply patches from /usr/pkg/lib/python3.7/site-packages/mesonbuild/envconfig.py to fix e.g.
      File "/usr/pkg/lib/python3.7/site-packages/mesonbuild/dependencies/base.py", line 1966, in from_entry
        return ExternalProgram(name, command=command, silent=True)
      File "/usr/pkg/lib/python3.7/site-packages/mesonbuild/dependencies/base.py", line 1929, in __init__
        self.path = self.command[-1]
    IndexError: list index out of range
    *** Error code 2
    
    Stop.
    make[1]: stopped in /usr/pkgsrc/devel/at-spi2-atk
    *** Error code 1

---
+++ devel/py-meson/patches/patch-mesonbuild_envconfig.py
@@ -0,0 +1,18 @@
+$NetBSD: patch-mesonbuild_envconfig.py,v 1.1 2019/07/15 15:11:44 prlw1 Exp $
+
+https://github.com/mesonbuild/meson/issues/5451
+
+--- mesonbuild/envconfig.py.orig	2019-07-09 16:34:42.000000000 +0000
++++ mesonbuild/envconfig.py
+@@ -340,6 +340,11 @@ This is probably wrong, it should always
+             command = os.environ.get(evar)
+             if command is not None:
+                 command = shlex.split(command)
++
++        # Do not return empty or blank string entries
++        if command is not None and (len(command) == 0 or len(command[0].strip()) == 0):
++            return None
++
+         return command
+ 
+ class Directories:
