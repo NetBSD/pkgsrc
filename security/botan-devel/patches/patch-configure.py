7d3d45586e5d20f2bb7675e02fa6de3fc25c2b3e
bsiegert <bsiegert@pkgsrc.org>
2019-07-15 18:31:29 +0000

    Update botan-devel to 2.11.0.
    There are too many changes to put them in this message.

--- security/botan-devel/patches/patch-configure.py
+++ security/botan-devel/patches/patch-configure.py
@@ -1,15 +1,16 @@
-$NetBSD: patch-configure.py,v 1.1 2018/02/02 13:56:35 jperkin Exp $
+$NetBSD: patch-configure.py,v 1.2 2019/07/15 18:31:29 bsiegert Exp $
 
 Leave pkgsrc to handle security features.
 
---- configure.py.orig	2017-04-05 01:09:22.000000000 +0000
+--- configure.py.orig	2019-07-01 10:23:16.000000000 +0000
 +++ configure.py
-@@ -1060,7 +1060,7 @@ class CompilerInfo(InfoObject): # pylint
-             if flag != None and flag != '' and flag not in abi_link:
-                 abi_link.append(flag)
+@@ -1266,9 +1266,6 @@ class CompilerInfo(InfoObject): # pylint
+         if options.msvc_runtime:
+             abi_link.add("/" + options.msvc_runtime)
  
 -        if options.with_stack_protector and self.stack_protector_flags != '':
-+        if options.with_stack_protector and self.stack_protector_flags != '' and False:
-             abi_link.append(self.stack_protector_flags)
- 
+-            abi_link.add(self.stack_protector_flags)
+-
          if options.with_coverage_info:
+             if self.coverage_flags == '':
+                 raise UserError('No coverage handling for %s' % (self.basename))
