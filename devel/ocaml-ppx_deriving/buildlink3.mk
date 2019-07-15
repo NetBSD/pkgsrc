f0873c493a435bde912b6793bc09fd9c796e9f0d
jaapb <jaapb@pkgsrc.org>
2019-07-15 12:57:48 +0000

    Updated devel/ocaml-ppx_deriving to version 4.4.
    This adds support for dune and OCaml 4.08, and has some other minor
    improvements and bugfixes as well.

--- devel/ocaml-ppx_deriving/buildlink3.mk
+++ devel/ocaml-ppx_deriving/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2018/07/19 15:15:24 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2019/07/15 12:57:48 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_deriving
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_deriving
 OCAML_PPX_DERIVING_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_deriving+=	ocaml-ppx_deriving>=4.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_deriving+=	ocaml-ppx_deriving>=4.2.1nb2
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_deriving+=	ocaml-ppx_deriving>=4.4
 BUILDLINK_PKGSRCDIR.ocaml-ppx_deriving?=	../../devel/ocaml-ppx_deriving
 .endif	# OCAML_PPX_DERIVING_BUILDLINK3_MK
 
