952bf36bea5352ee8995781f3627aa86ee3cec1b
nia <nia@pkgsrc.org>
2019-07-16 15:36:02 +0000

    opencv: Disable jp2 support by default
    Re-enable it with the 'jasper' option.
    
    Part of the process of trying to minimize the potential impact of a
    vulnerable jasper.
    
    Bump PKGREVISION.

--- graphics/opencv/options.mk
+++ graphics/opencv/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.7 2019/04/24 19:34:44 adam Exp $
+# $NetBSD: options.mk,v 1.8 2019/07/16 15:36:02 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.opencv
-PKG_SUPPORTED_OPTIONS=	ffmpeg
+PKG_SUPPORTED_OPTIONS=	ffmpeg jasper
 
 .include "../../mk/bsd.options.mk"
 
@@ -12,6 +12,15 @@ CMAKE_ARGS+=	-DFFMPEG_LIB_DIR=${PREFIX}/lib/ffmpeg2
 .include "../../multimedia/ffmpeg2/buildlink3.mk"
 .else
 CMAKE_ARGS+=	-DWITH_FFMPEG=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mjasper)
+CMAKE_ARGS+=	-DWITH_JASPER=ON
+# jasper uses SIZE_MAX and friends in its headers.
+CXXFLAGS+=	-D__STDC_LIMIT_MACROS
+.include "../../graphics/jasper/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DWITH_JASPER=OFF
 .endif
 
 # FIXME: should be option.mk'ed instead
