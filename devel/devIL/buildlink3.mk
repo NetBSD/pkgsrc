e3d13b7cdbf4f245c1815035eb748d855c0355f5
nia <nia@pkgsrc.org>
2019-07-16 22:45:42 +0000

    devIL: Disable jp2 support by default
    Re-enable it with the 'jasper' option.
    
    Part of the process of trying to minimize the potential impact of a
    vulnerable jasper.
    
    While here, define LICENSE.
    
    Bump PKGREVISION

--- devel/devIL/buildlink3.mk
+++ devel/devIL/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.30 2015/04/25 14:20:33 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.31 2019/07/16 22:45:42 nia Exp $
 
 BUILDLINK_TREE+=	devIL
 
@@ -9,9 +9,12 @@ BUILDLINK_API_DEPENDS.devIL+=	devIL>=1.7.8
 BUILDLINK_ABI_DEPENDS.devIL+=	devIL>=1.7.8nb13
 BUILDLINK_PKGSRCDIR.devIL?=	../../devel/devIL
 
+.include "../../mk/pkg-build-options.mk"
 .include "../../devel/SDL/buildlink3.mk"
 .include "../../graphics/glu/buildlink3.mk"
-.include "../../graphics/jasper/buildlink3.mk"
+.if !empty(PKG_BUILD_OPTIONS.opencv:Mjasper)
+.  include "../../graphics/jasper/buildlink3.mk"
+.endif
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../graphics/giflib/buildlink3.mk"
 .include "../../graphics/mng/buildlink3.mk"
