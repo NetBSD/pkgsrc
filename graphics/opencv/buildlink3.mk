f7b88350252b7e884c165273a3edaa26a1c236e0
nia <nia@pkgsrc.org>
2019-07-16 15:39:35 +0000

    opencv: Conditionally include jasper in buildlink3

--- graphics/opencv/buildlink3.mk
+++ graphics/opencv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2019/04/24 19:34:44 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2019/07/16 15:39:35 nia Exp $
 
 BUILDLINK_TREE+=	opencv
 
@@ -8,9 +8,12 @@ OPENCV_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.opencv+=	opencv>=3.0.0
 BUILDLINK_PKGSRCDIR.opencv?=	../../graphics/opencv
 
+.include "../../mk/pkg-build-options.mk"
 .include "../../devel/protobuf/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
-.include "../../graphics/jasper/buildlink3.mk"
+.if !empty(PKG_BUILD_OPTIONS.opencv:Mjasper)
+.  include "../../graphics/jasper/buildlink3.mk"
+.endif
 .include "../../graphics/libwebp/buildlink3.mk"
 .include "../../graphics/openexr/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
