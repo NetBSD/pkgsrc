8fc395cf68dfdd5657b152ed6d4aa2dc1c6cca9d
nia <nia@pkgsrc.org>
2019-07-16 15:34:25 +0000

    gegl: Disable jp2 support by default
    Re-enable it with the 'jasper' option.
    
    Part of the process of trying to minimize the potential impact of a
    vulnerable jasper.
    
    Bump PKGREVISION

--- graphics/gegl/options.mk
+++ graphics/gegl/options.mk
@@ -1,14 +1,21 @@
-# $NetBSD: options.mk,v 1.1 2009/02/03 06:21:40 adam Exp $
+# $NetBSD: options.mk,v 1.2 2019/07/16 15:34:25 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gegl
-PKG_SUPPORTED_OPTIONS=	svg
+PKG_SUPPORTED_OPTIONS=	svg jasper
 PKG_SUGGESTED_OPTIONS=	svg
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=	svg
-
 .if !empty(PKG_OPTIONS:Msvg)
 .include "../../graphics/librsvg/buildlink3.mk"
 PLIST.svg=	yes
+.endif
+
+PLIST_VARS+=	jasper
+.if !empty(PKG_OPTIONS:Mjasper)
+.include "../../graphics/jasper/buildlink3.mk"
+PLIST.jasper=	yes
+.else
+CONFIGURE_ARGS+=	--without-jasper
 .endif
