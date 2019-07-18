a3425cf2caae9d0b58448af546970d7b12795cd7
taca <taca@pkgsrc.org>
2019-07-18 03:02:02 +0000

    net/bind914: update to 9.14.4
    Update bind914 to 9.14.4.
    
    	--- 9.14.4 released ---
    
    5260.	[bug]		dnstap-read was producing malformed output for large
    			packets. [GL #1093]
    
    5258.	[func]		Added support for the GeoIP2 API from MaxMind,
    			when BIND is compiled using "configure --with-geoip2".
    			The legacy GeoIP API can be enabled by using
    			"configure --with-geoip" instead. These options
    			cannot be used together.
    
    			Certain geoip ACL settings that were available with
    			legacy GeoIP are not available when using GeoIP2.
    			See the ARM for details. [GL #182]
    
    5257.	[bug]		Some statistics data was not being displayed.
    			Add shading to the zone tables. [GL #1030]
    
    5256.	[bug]		Ensure that glue records are included in root
    			priming responses if "minimal-responses" is not
    			set to "yes". [GL #1092]
    
    5255.	[bug]		Errors encountered while reloading inline-signing
    			zones could be ignored, causing the zone content to
    			be left in an incompletely updated state rather than
    			reverted. [GL #1109]
    
    5254.	[func]		Collect metrics to report to the statistics-channel
    			DNSSEC signing operations (dnssec-sign) and refresh
    			operations (dnssec-refresh) per zone and per keytag.
    			[GL #513]
    
    5253.	[port]		Support platforms that don't define ULLONG_MAX.
    			[GL #1098]
    
    5251.	[bug]		Statistics were broken in x86 Windows builds.
    			[GL #1081]
    
    5249.	[bug]		Fix a possible underflow in recursion clients
    			statistics when hitting recursive clients
    			soft quota. [GL #1067]

--- net/bind914/patches/patch-lib_isc_stats.c
+++ net/bind914/patches/patch-lib_isc_stats.c
@@ -1,18 +1,15 @@
-$NetBSD: patch-lib_isc_stats.c,v 1.2 2019/05/20 16:31:08 taca Exp $
+$NetBSD: patch-lib_isc_stats.c,v 1.3 2019/07/18 03:02:02 taca Exp $
 
 * Platform fixes from NetBSD base system.
 
---- lib/isc/stats.c.orig	2019-05-10 04:51:34.000000000 +0000
+--- lib/isc/stats.c.orig	2019-07-09 18:15:48.000000000 +0000
 +++ lib/isc/stats.c
-@@ -30,7 +30,11 @@
+@@ -30,7 +30,7 @@
  #define ISC_STATS_MAGIC			ISC_MAGIC('S', 't', 'a', 't')
  #define ISC_STATS_VALID(x)		ISC_MAGIC_VALID(x, ISC_STATS_MAGIC)
  
-+#ifndef _LP64
-+typedef atomic_int_fast32_t isc_stat_t;
-+#else
+-#if defined(_WIN32) && !defined(_WIN64)
++#if (defined(_WIN32) && !defined(_WIN64)) || !defined(_LP64)
+ typedef atomic_int_fast32_t isc_stat_t;
+ #else
  typedef atomic_int_fast64_t isc_stat_t;
-+#endif
- 
- struct isc_stats {
- 	unsigned int		magic;
