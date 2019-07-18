02f6d156a4678353afd5263b96c14c8b380d1222
nia <nia@pkgsrc.org>
2019-07-18 12:15:04 +0000

    grafx2: Update to 2.6
    Changes:
    
    - SDL2/X11/win32 GDI support in addition to SDL1.2.x
    - Many more "8bits" computer constraints modes
    - Support for more C64 pictures formats
    - Support for Thomson file formats
    - Open URL when clicking them in oneline help
    - Brush and picture copy/paste support

--- graphics/grafx2/patches/patch-misc.c
+++ graphics/grafx2/patches/patch-misc.c
@@ -1,16 +1,16 @@
-$NetBSD: patch-misc.c,v 1.1 2018/07/17 13:07:50 ryoon Exp $
+$NetBSD: patch-misc.c,v 1.2 2019/07/18 12:15:04 nia Exp $
 
 * Fix sysctl build failure
 
---- misc.c.orig	2018-05-15 16:51:04.000000000 +0000
+--- misc.c.orig	2019-01-10 08:40:57.000000000 +0000
 +++ misc.c
-@@ -24,6 +24,9 @@
+@@ -25,6 +25,9 @@
  #if defined(WIN32)
  #define _WIN32_WINNT 0x0500
  #endif
 +#if defined(__NetBSD__)
 +#define _NETBSD_SOURCE
 +#endif
- #include <SDL.h>
  #include <string.h>
  #ifndef _MSC_VER
+ #include <strings.h>
