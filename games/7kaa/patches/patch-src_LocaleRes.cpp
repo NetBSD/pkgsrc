2e3f42e16bdda79578f20d64016772003bbb966c
nia <nia@pkgsrc.org>
2019-07-19 09:13:50 +0000

    7kaa: Also apply the iconv patch to __sun.

--- games/7kaa/patches/patch-src_LocaleRes.cpp
+++ games/7kaa/patches/patch-src_LocaleRes.cpp
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_LocaleRes.cpp,v 1.1 2019/06/30 21:49:42 nia Exp $
+$NetBSD: patch-src_LocaleRes.cpp,v 1.2 2019/07/19 09:13:51 nia Exp $
 
-Support NetBSD iconv being special.
+Support NetBSD and SunOS iconv being special.
 
 --- src/LocaleRes.cpp.orig	2019-06-22 22:22:26.000000000 +0000
 +++ src/LocaleRes.cpp
@@ -8,7 +8,7 @@ Support NetBSD iconv being special.
  	size_t c;
  	while( in_left>0 )
  	{
-+#ifdef __NetBSD__
++#if defined(__NetBSD__) || defined(__sun)
 +		c = iconv(cd, (const char **)&p1, &in_left, &p2, &out_left);
 +#else
  		c = iconv(cd, &p1, &in_left, &p2, &out_left);
