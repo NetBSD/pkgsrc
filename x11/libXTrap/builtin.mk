baf1ad3ab72be77a362dea908a075d5dd03849f8
wiz <wiz@pkgsrc.org>
2019-07-19 21:13:27 +0000

    libXTrap, xtrap: remove
    Not useful according to upstream (deprecated since ~1994).

--- x11/libXTrap/builtin.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: builtin.mk,v 1.3 2014/03/10 11:05:52 jperkin Exp $
-
-BUILTIN_PKG:=	libXTrap
-PKGCONFIG_FILE.libXTrap=	${X11BASE}/lib/pkgconfig/xtrap.pc
-PKGCONFIG_FILE.libXTrap+=	${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/xtrap.pc
-
-.include "../../mk/buildlink3/pkgconfig-builtin.mk"
-.include "../../mk/x11.builtin.mk"
