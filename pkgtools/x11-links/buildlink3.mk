45ba05d20a6abf879c809a7042aa1c6217ce29ac
wiz <wiz@pkgsrc.org>
2019-07-20 21:11:08 +0000

    x11-links: bump bl3.mk dependency.

--- pkgtools/x11-links/buildlink3.mk
+++ pkgtools/x11-links/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.82 2019/07/10 22:36:49 rjs Exp $
+# $NetBSD: buildlink3.mk,v 1.83 2019/07/20 21:11:08 wiz Exp $
 #
 # Don't include this file manually!  It will be included as necessary
 # by bsd.buildlink3.mk.
@@ -12,7 +12,7 @@ BUILDLINK_TREE+=	x11-links
 .if !defined(X11_LINKS_BUILDLINK3_MK)
 X11_LINKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=1.20
+BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=1.21
 BUILDLINK_PKGSRCDIR.x11-links?=		../../pkgtools/x11-links
 BUILDLINK_DEPMETHOD.x11-links?=		build
 
