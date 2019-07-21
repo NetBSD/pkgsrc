5739f13dd8530b4593e4bfc601e9eefefd1c0ba0
nia <nia@pkgsrc.org>
2019-07-21 11:14:37 +0000

    SDL: Backport fixes from upstream's hg for the following CVEs:
    CVE-2019-7572 - buffer-overflow
    CVE-2019-7573 - heap-overflow
    CVE-2019-7574 - heap-overflow
    CVE-2019-7575 - heap-overflow
    CVE-2019-7576 - heap-overflow
    CVE-2019-7577 - buffer-overflow
    CVE-2019-7578 - heap-overflow
    CVE-2019-7635 - heap-overflow
    CVE-2019-7636 - heap-overflow
    CVE-2019-7637 - heap-overflow
    CVE-2019-7638 - heap-overflow
    
    Bump PKGREVISION.

--- devel/SDL/patches/patch-src_video_x11_SDL_x11video.c
+++ devel/SDL/patches/patch-src_video_x11_SDL_x11video.c
@@ -1,6 +1,11 @@
-$NetBSD: patch-src_video_x11_SDL_x11video.c,v 1.1 2012/02/20 14:55:59 jdc Exp $
+$NetBSD: patch-src_video_x11_SDL_x11video.c,v 1.2 2019/07/21 11:14:38 nia Exp $
 
+Hunk 1:
 We need to call XChangeProperty(..., 32, ...) with an object aligned as long.
+
+Hunk 2:
+CVE-2019-7637: Fix in integer overflow in SDL_CalculatePitch
+From https://hg.libsdl.org/SDL/rev/9b0e5c555c0f
 
 --- src/video/x11/SDL_x11video.c.orig	2012-02-14 22:26:01.000000000 +0000
 +++ src/video/x11/SDL_x11video.c	2012-02-14 22:53:45.000000000 +0000
@@ -29,3 +34,14 @@ We need to call XChangeProperty(..., 32, ...) with an object aligned as long.
  			XChangeProperty(SDL_Display, WMwindow, WM_CLIENT_MACHINE, XA_STRING, 8,
  					PropModeReplace, (unsigned char *)hostname, SDL_strlen(hostname));
  		}
+@@ -1225,6 +1225,10 @@ SDL_Surface *X11_SetVideoMode(_THIS, SDL
+ 		current->w = width;
+ 		current->h = height;
+ 		current->pitch = SDL_CalculatePitch(current);
++		if (!current->pitch) {
++			current = NULL;
++			goto done;
++		}
+ 		if (X11_ResizeImage(this, current, flags) < 0) {
+ 			current = NULL;
+ 			goto done;
