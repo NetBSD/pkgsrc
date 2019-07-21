532e72c1f877777266ac522e1711d26251ff3bb0
wiz <wiz@pkgsrc.org>
2019-07-21 20:33:02 +0000

    gdk-pixbuf2 now depends on shared-mime-info. Bump ABI depends.

--- graphics/gdk-pixbuf2/buildlink3.mk
+++ graphics/gdk-pixbuf2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2016/08/03 10:22:14 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2019/07/21 20:33:02 wiz Exp $
 
 BUILDLINK_TREE+=	gdk-pixbuf2
 
@@ -6,9 +6,10 @@ BUILDLINK_TREE+=	gdk-pixbuf2
 GDK_PIXBUF2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gdk-pixbuf2+=	gdk-pixbuf2>=2.22.0
-BUILDLINK_ABI_DEPENDS.gdk-pixbuf2+=	gdk-pixbuf2>=2.34.0nb1
+BUILDLINK_ABI_DEPENDS.gdk-pixbuf2+=	gdk-pixbuf2>=2.38.1
 BUILDLINK_PKGSRCDIR.gdk-pixbuf2?=	../../graphics/gdk-pixbuf2
 
+.include "../../databases/shared-mime-info/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .endif	# GDK_PIXBUF2_BUILDLINK3_MK
