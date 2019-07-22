21afb1029796448b056afb57b895d2e9a1ee1048
tnn <tnn@pkgsrc.org>
2019-07-22 23:48:31 +0000

    geckodriver: update to 0.24.0
    Added
        Introduces strictFileInteractability capability
        Added new endpoint GET /session/{session id}/moz/screenshot/full
        Added new --marionette-host <HOSTNAME> flag
        Added new endpoint POST /session/{session_id}/window/new
    Changed
        Allow file uploads to hidden <input type=file> elements
        Allow use of an indefinite script timeout for the Set Timeouts
        command, thanks to reimu.
    Fixed
        Corrected Content-Type of response header to utf-8 to fix
        an HTTP/1.1 compatibility bug.
        Relaxed the deserialization of timeouts parameters to allow unknown
        fields for the Set Timeouts command.
        Fixed a regression in the Take Element Screenshot to not screenshot
        the viewport, but the requested element.

--- www/geckodriver/patches/patch-vendor_mozrunner_src_runner.rs
+++ www/geckodriver/patches/patch-vendor_mozrunner_src_runner.rs
@@ -1,32 +1,10 @@
-$NetBSD: patch-vendor_mozrunner_src_runner.rs,v 1.3 2019/01/05 03:01:30 tnn Exp $
+$NetBSD: patch-vendor_mozrunner_src_runner.rs,v 1.4 2019/07/22 23:48:31 tnn Exp $
 
 Make the linux method the fallback method.
 
---- ../vendor/mozrunner-0.8.0/src/runner.rs.orig	2018-09-11 12:26:34.000000000 +0000
-+++ ../vendor/mozrunner-0.8.0/src/runner.rs
-@@ -293,21 +293,6 @@ impl Runner for FirefoxRunner {
-     }
- }
- 
--#[cfg(target_os = "linux")]
--pub mod platform {
--    use path::find_binary;
--    use std::path::PathBuf;
--
--    /// Searches the system path for `firefox`.
--    pub fn firefox_default_path() -> Option<PathBuf> {
--        find_binary("firefox")
--    }
--
--    pub fn arg_prefix_char(c: char) -> bool {
--        c == '-'
--    }
--}
--
- #[cfg(target_os = "macos")]
- pub mod platform {
-     use path::{find_binary, is_binary};
-@@ -410,12 +395,12 @@ pub mod platform {
+--- ../vendor/mozrunner-0.9.0/src/runner.rs.orig	2019-01-03 11:24:34.000000000 +0000
++++ ../vendor/mozrunner-0.9.0/src/runner.rs
+@@ -410,12 +410,12 @@ pub mod platform {
  
  #[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
  pub mod platform {
