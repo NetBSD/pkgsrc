6ddb87c295a831fc2ae114eade095a4559f517e5
wiz <wiz@pkgsrc.org>
2019-07-23 17:19:56 +0000

    *: recursive bump for poppler 0.79

--- graphics/osg/buildlink3.mk
+++ graphics/osg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.74 2019/07/21 22:24:06 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.75 2019/07/23 17:19:57 wiz Exp $
 
 BUILDLINK_TREE+=	osg
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	osg
 OSG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.osg+=	osg>=2.8.3
-BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.4.1nb16
+BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.4.1nb17
 BUILDLINK_PKGSRCDIR.osg?=	../../graphics/osg
 
 .include "../../mk/bsd.fast.prefs.mk"
