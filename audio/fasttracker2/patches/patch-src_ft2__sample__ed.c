986b7d2c3d0ac66c433d2d223158eda4ef4ae908
bsiegert <bsiegert@pkgsrc.org>
2019-07-23 13:05:45 +0000

    Pullup ticket #6011 - requested by fox audio/fasttracker2: update, i386 build fix
    Revisions pulled up:
    - audio/fasttracker2/Makefile                                   1.28-1.30
    - audio/fasttracker2/distinfo                                   1.27-1.29
    - audio/fasttracker2/patches/patch-src_ft2__header.h            1.1
    - audio/fasttracker2/patches/patch-src_ft2__sample__ed.c        1.1
    
    ---
       Module Name:    pkgsrc
       Committed By:   fox
       Date:           Sat Jul  6 16:11:35 UTC 2019
    
       Modified Files:
               pkgsrc/audio/fasttracker2: Makefile distinfo
    
       Log Message:
       fasttracker2: update to b161
    
       Changes since b160
    
       Beta #161 - 04.07.2019
       - Bugfix: If you held down a sample data loop pin while a system request was
         about to be shown, the loop pins would act strange lateron.
       - Bugfix: Fixed a MIDI-related memory leak when pressing "Load config." in the
         Config screen.
       - Bugfix: The palette editor scrollbar thumbs would not draw properly when
         dragging them.
       - Bugfix: Attempting to sample in stereo (Smp. Ed.) at the last sample slot
         would result in a crash.
       - Bugfix: Using keyboard shortcuts to interact with a system request/message
         could trigger an unwanted "note key up" event after the box closed. (déjà vu)
       - The palette editor is now rewritten to behave exactly like original FT2
       - Code cleanup
       - Updated src\gfxdata\bmp\readme.txt
    
    ---
       Module Name:    pkgsrc
       Committed By:   fox
       Date:           Sat Jul 13 21:39:35 UTC 2019
    
       Modified Files:
               pkgsrc/audio/fasttracker2: Makefile distinfo
    
       Log Message:
       fasttracker2: update to b162
    
       Changes since b161
    
       Beta #162 - 12.07.2019
       - Added a "Hardware mouse" option in Config -> Layout
       - Changed the help text a little ("Problems/FAQ" and "Known bugs")
    
    ---
       Module Name:    pkgsrc
       Committed By:   fox
       Date:           Mon Jul 22 23:38:47 UTC 2019
    
       Modified Files:
               pkgsrc/audio/fasttracker2: Makefile distinfo
       Added Files:
               pkgsrc/audio/fasttracker2/patches: patch-src_ft2__header.h
                   patch-src_ft2__sample__ed.c
    
       Log Message:
       fasttracker2: Fixes build failures.
    
       Fixes build failures in i386 platforms where SSE option is not available.
    
       Fix suggested by leot@NetBSD.org

---
+++ audio/fasttracker2/patches/patch-src_ft2__sample__ed.c
@@ -0,0 +1,27 @@
+$NetBSD: patch-src_ft2__sample__ed.c,v 1.2.2.2 2019/07/23 13:05:46 bsiegert Exp $
+
+Add the HAS_SSE macro so that build failures do not occur in i386
+systems that lack SSE support.
+
+Suggestion by leot@
+
+--- src/ft2_sample_ed.c.orig	2019-07-22 16:27:23.010951973 +0000
++++ src/ft2_sample_ed.c
+@@ -689,7 +689,7 @@ static void sampleLine(int16_t x1, int16
+ 
+ static void getMinMax16(const void *p, uint32_t scanLen, int16_t *min16, int16_t *max16)
+ {
+-#if defined __APPLE__ || defined _WIN32 || defined __i386__ || defined __amd64__
++#if defined __APPLE__ || (defined HAS_SSE && (defined _WIN32 || defined __i386)) || defined __amd64__
+ 	if (cpu.hasSSE2)
+ 	{
+ 		/* Taken with permission from the OpenMPT project (and slightly modified).
+@@ -775,7 +775,7 @@ static void getMinMax16(const void *p, u
+ 
+ static void getMinMax8(const void *p, uint32_t scanLen, int8_t *min8, int8_t *max8)
+ {
+-#if defined __APPLE__ || defined _WIN32 || defined __i386__ || defined __amd64__
++#if defined __APPLE__ || (defined HAS_SSE && (defined _WIN32 || defined __i386)) || defined __amd64__
+ 	if (cpu.hasSSE2)
+ 	{
+ 		/* Taken with permission from the OpenMPT project (and slightly modified).
