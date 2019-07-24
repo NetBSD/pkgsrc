4368958b26088ab1d5f90e8a90eb3a7231bb1fd0
nia <nia@pkgsrc.org>
2019-07-24 19:44:55 +0000

    qt5-qtbase: Fix package creation with gtk3 option.

--- x11/qt5-qtbase/options.mk
+++ x11/qt5-qtbase/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.12 2019/01/26 22:58:04 tnn Exp $
+# $NetBSD: options.mk,v 1.13 2019/07/24 19:44:55 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.qt5
 PKG_SUPPORTED_OPTIONS+=	cups eglfs gtk3
@@ -6,7 +6,6 @@ PKG_SUPPORTED_OPTIONS+=	cups eglfs gtk3
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=		cups
-
 .if !empty(PKG_OPTIONS:Mcups)
 .  include "../../print/cups-base/buildlink3.mk"
 CONFIGURE_ARGS+=	-cups
@@ -21,9 +20,11 @@ CONFIGURE_ARGS+=	-no-cups
 #CONFIGURE_ARGS+=	-no-eglfs
 #.endif
 
+PLIST_VARS+=		gtk3
 .if !empty(PKG_OPTIONS:Mgtk3)
 .  include "../../x11/gtk3/buildlink3.mk"
 CONFIGURE_ARGS+=	-gtk
+PLIST.gtk3=		yes
 .else
 CONFIGURE_ARGS+=	-no-gtk
 .endif
