7665eb2bb83f933f64508cebb61a54088f56d5eb
rin <rin@pkgsrc.org>
2019-07-24 02:39:57 +0000

    Various fixes for wscons framebuffer:
    - Fix support for fbi_fboffset != 0
    - Fix SEGV when using 8-bpp display on LP64 machine
    - Add better handling for 24-bpp display
    
    The problems have already been reported upstream:
    
    https://bitbucket.org/arakiken/mlterm/pull-requests/5/fix-for-wscons-framebuffer/diff
    
    Bump PKGREVISION.

---
+++ x11/mlterm/patches/patch-uitoolkit_fb_ui.h
@@ -0,0 +1,17 @@
+$ NetBSD $
+
+- Fix support for fbi_fboffset != 0
+
+Reported upstream:
+https://bitbucket.org/arakiken/mlterm/pull-requests/5/fix-for-wscons-framebuffer/diff
+
+--- uitoolkit/fb/ui.h	2019-07-24 10:51:04.187713603 +0900
++++ ../mlterm.patched/uitoolkit/fb/ui.h	2019-07-24 09:59:19.091595937 +0900
+@@ -44,6 +44,7 @@ typedef struct {
+ 
+   int fb_fd;
+   unsigned char *fb;
++  unsigned char *fb_base;
+   size_t smem_len;
+   unsigned int line_length;
+   unsigned int xoffset;
