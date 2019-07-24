2d45aac63e6e8fff8a946a4a150517198e17245b
micha <micha@pkgsrc.org>
2019-07-24 14:08:23 +0000

    Fixed patch-src_video_SDL__pixels.c for NetBSD 7
    Declaration of local variable 'byte' moved out of for loop because this syntax
    requires C99 (breaks e.g. NetBSD 7 because LANGUAGES= c99 is not defined by
    package).

--- devel/SDL/patches/patch-src_video_SDL__pixels.c
+++ devel/SDL/patches/patch-src_video_SDL__pixels.c
@@ -1,11 +1,15 @@
-$NetBSD: patch-src_video_SDL__pixels.c,v 1.1 2019/07/21 11:14:38 nia Exp $
+$NetBSD: patch-src_video_SDL__pixels.c,v 1.2 2019/07/24 14:08:23 micha Exp $
 
 CVE-2019-7637: Fix in integer overflow in SDL_CalculatePitch
 From https://hg.libsdl.org/SDL/rev/9b0e5c555c0f
 
+Local variable 'byte':
+Declaration moved out of for loop because this syntax requires C99
+(breaks e.g. NetBSD 7 because LANGUAGES= c99 is not defined by package)
+
 --- src/video/SDL_pixels.c.orig	2012-01-19 06:30:06.000000000 +0000
 +++ src/video/SDL_pixels.c
-@@ -286,26 +286,53 @@ void SDL_DitherColors(SDL_Color *colors,
+@@ -286,26 +286,54 @@ void SDL_DitherColors(SDL_Color *colors,
  	}
  }
  /* 
@@ -17,11 +21,12 @@ From https://hg.libsdl.org/SDL/rev/9b0e5c555c0f
  {
 -	Uint16 pitch;
 +	unsigned int pitch = 0;
++	Uint8 byte;  // Requires C99 if defined inside for loop
  
  	/* Surface should be 4-byte aligned for speed */
 -	pitch = surface->w*surface->format->BytesPerPixel;
 +	/* The code tries to prevent from an Uint16 overflow. */;
-+	for (Uint8 byte = surface->format->BytesPerPixel; byte; byte--) {
++	for (byte = surface->format->BytesPerPixel; byte; byte--) {
 +		pitch += (unsigned int)surface->w;
 +		if (pitch < surface->w) {
 +			SDL_SetError("A scanline is too wide");
