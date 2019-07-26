5393bb9156ec5ff9c10a8c06e2cf9c1e932fe124
rin <rin@pkgsrc.org>
2019-07-26 23:28:43 +0000

    Revert partially the previous: "- Add better handling for 24-bpp display"
    NetBSD does not set fbi_subtype.fbi_rgbmasks.alpha_size correctly.
    
    This also has been reported upstream.
    
    Rebump PKGREVISION.

--- x11/mlterm/patches/patch-uitoolkit_fb_ui__display__wscons.c
+++ x11/mlterm/patches/patch-uitoolkit_fb_ui__display__wscons.c
@@ -2,13 +2,12 @@ $ NetBSD $
 
 - Fix support for fbi_fboffset != 0
 - Fix SEGV when using 8-bpp display on LP64 machine
-- Add better handling for 24-bpp display
 
 Reported upstream:
 https://bitbucket.org/arakiken/mlterm/pull-requests/5/fix-for-wscons-framebuffer/diff
 
---- uitoolkit/fb/ui_display_wscons.c	2019-07-24 10:51:04.210666890 +0900
-+++ ../mlterm.patched/uitoolkit/fb/ui_display_wscons.c	2019-07-24 10:31:06.733563761 +0900
+--- uitoolkit/fb/ui_display_wscons.c.orig	2019-03-31 22:10:47.000000000 +0900
++++ uitoolkit/fb/ui_display_wscons.c	2019-07-26 21:54:53.421920873 +0900
 @@ -260,6 +260,7 @@ static int open_display(u_int depth /* u
  #ifdef WSDISPLAYIO_GET_FBINFO
    struct wsdisplayio_fbinfo vinfo2;
@@ -17,17 +16,8 @@ https://bitbucket.org/arakiken/mlterm/pull-requests/5/fix-for-wscons-framebuffer
    int mode;
    int wstype;
    struct rgb_info rgbinfos[] = {
-@@ -323,15 +324,26 @@ static int open_display(u_int depth /* u
-   if (ioctl(_display.fb_fd, WSDISPLAYIO_GET_FBINFO, &vinfo2) == 0) {
-     vinfo.width = vinfo2.fbi_width;
-     vinfo.height = vinfo2.fbi_height;
--    vinfo.depth = vinfo2.fbi_bitsperpixel;
-+    if (vinfo2.fbi_bitsperpixel == 32 &&
-+        vinfo2.fbi_subtype.fbi_rgbmasks.alpha_size == 0) {
-+      vinfo.depth = 24;
-+    } else {
-+      vinfo.depth = vinfo2.fbi_bitsperpixel;
-+    }
+@@ -326,12 +327,18 @@ static int open_display(u_int depth /* u
+     vinfo.depth = vinfo2.fbi_bitsperpixel;
      vinfo.cmsize = vinfo2.fbi_subtype.fbi_cmapinfo.cmap_entries;
  
 -    /* XXX fbi_fboffset is regarded as multiple of fbi_stride */
@@ -49,7 +39,7 @@ https://bitbucket.org/arakiken/mlterm/pull-requests/5/fix-for-wscons-framebuffer
    } else {
  #ifdef DEBUG
      bl_debug_printf(BL_DEBUG_TAG " WSDISPLAYIO_GTYPE and WSDISPLAYIO_GET_FBINFO failed.\n");
-@@ -341,6 +353,7 @@ static int open_display(u_int depth /* u
+@@ -341,6 +348,7 @@ static int open_display(u_int depth /* u
    }
  
    _display.xoffset = 0;
@@ -57,7 +47,7 @@ https://bitbucket.org/arakiken/mlterm/pull-requests/5/fix-for-wscons-framebuffer
  
    _display.width = _disp.width = vinfo.width;
    _display.height = _disp.height = vinfo.height;
-@@ -403,7 +416,7 @@ static int open_display(u_int depth /* u
+@@ -403,7 +411,7 @@ static int open_display(u_int depth /* u
    if (wstype == WSDISPLAY_TYPE_LUNA && (_disp.depth == 4 || _disp.depth == 8)) {
      u_int plane;
  
@@ -66,7 +56,7 @@ https://bitbucket.org/arakiken/mlterm/pull-requests/5/fix-for-wscons-framebuffer
  
      for (plane = 0; plane < _disp.depth; plane++) {
        _display.plane_offset[plane] = 0x40000 * plane;
-@@ -411,7 +424,7 @@ static int open_display(u_int depth /* u
+@@ -411,7 +419,7 @@ static int open_display(u_int depth /* u
    } else
  #endif
    {
@@ -75,7 +65,7 @@ https://bitbucket.org/arakiken/mlterm/pull-requests/5/fix-for-wscons-framebuffer
    }
  
    if ((_display.fb = mmap(NULL, _display.smem_len, PROT_WRITE | PROT_READ, MAP_SHARED,
-@@ -421,11 +434,7 @@ static int open_display(u_int depth /* u
+@@ -421,11 +429,7 @@ static int open_display(u_int depth /* u
      goto error;
    }
  
@@ -88,7 +78,7 @@ https://bitbucket.org/arakiken/mlterm/pull-requests/5/fix-for-wscons-framebuffer
  
    if (_disp.depth < 15) {
      if (vinfo.depth >= 2 && _disp.depth == 1) {
-@@ -630,7 +639,7 @@ static int open_display(u_int depth /* u
+@@ -630,7 +634,7 @@ static int open_display(u_int depth /* u
  error:
    if (_display.fb) {
      munmap(_display.fb, _display.smem_len);
