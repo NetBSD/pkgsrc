9fe8ae5035b9384321ae100ea62638ee2d207eae
fox <fox@pkgsrc.org>
2019-07-31 01:35:27 +0000

    cliqz: update to 1.28.0
    Changes since 1.27.4:
    
    Merge with Firefox up to 68.0.1

--- www/cliqz/patches/patch-mozilla-release_gfx_angle_checkout_src_common_third__party_smhasher_src_PMurHash.cpp
+++ www/cliqz/patches/patch-mozilla-release_gfx_angle_checkout_src_common_third__party_smhasher_src_PMurHash.cpp
@@ -1,17 +1,19 @@
-$NetBSD: patch-mozilla-release_gfx_angle_checkout_src_common_third__party_smhasher_src_PMurHash.cpp,v 1.1 2019/04/19 14:02:03 fox Exp $
+$NetBSD: patch-mozilla-release_gfx_angle_checkout_src_common_third__party_smhasher_src_PMurHash.cpp,v 1.2 2019/07/31 01:35:27 fox Exp $
 
 Fix build where _LITTLE_ENDIAN is not an integer.
 
 Original patch from ryoon, imported from www/firefox
 
---- mozilla-release/gfx/angle/checkout/src/common/third_party/smhasher/src/PMurHash.cpp.orig	2018-11-16 08:40:07.000000000 +0000
+--- mozilla-release/gfx/angle/checkout/src/common/third_party/smhasher/src/PMurHash.cpp.orig	2019-07-06 01:48:31.000000000 +0000
 +++ mozilla-release/gfx/angle/checkout/src/common/third_party/smhasher/src/PMurHash.cpp
-@@ -92,7 +92,7 @@ on big endian machines, or a byte-by-byt
+@@ -91,8 +91,8 @@ on big endian machines, or a byte-by-byt
  /* gcc 'may' define __LITTLE_ENDIAN__ or __BIG_ENDIAN__ to 1 (Note the trailing __),
   * or even _LITTLE_ENDIAN or _BIG_ENDIAN (Note the single _ prefix) */
  #if !defined(__BYTE_ORDER)
--  #if defined(__LITTLE_ENDIAN__) && __LITTLE_ENDIAN__==1 || defined(_LITTLE_ENDIAN) && _LITTLE_ENDIAN==1
-+  #if defined(__LITTLE_ENDIAN__) && __LITTLE_ENDIAN__-0==1 || defined(_LITTLE_ENDIAN) && _LITTLE_ENDIAN-0==1
-     #define __BYTE_ORDER __LITTLE_ENDIAN
-   #elif defined(__BIG_ENDIAN__) && __BIG_ENDIAN__==1 || defined(_BIG_ENDIAN) && _BIG_ENDIAN==1
-     #define __BYTE_ORDER __BIG_ENDIAN
+-#    if defined(__LITTLE_ENDIAN__) && __LITTLE_ENDIAN__ == 1 || \
+-        defined(_LITTLE_ENDIAN) && _LITTLE_ENDIAN == 1
++#    if defined(__LITTLE_ENDIAN__) && __LITTLE_ENDIAN__-0 == 1 || \
++        defined(_LITTLE_ENDIAN) && _LITTLE_ENDIAN-0 == 1
+ #        define __BYTE_ORDER __LITTLE_ENDIAN
+ #    elif defined(__BIG_ENDIAN__) && __BIG_ENDIAN__ == 1 || defined(_BIG_ENDIAN) && _BIG_ENDIAN == 1
+ #        define __BYTE_ORDER __BIG_ENDIAN
