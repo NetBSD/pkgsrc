1f46411a0ab4060c4b9ab39a1b584e1d88500930
jaapb <jaapb@pkgsrc.org>
2019-07-31 09:27:29 +0000

    Updated devel/ocaml-ppx_custom_printf to version 0.12.1.
    This adds support for OCaml 4.08.

--- devel/ocaml-ppx_custom_printf/buildlink3.mk
+++ devel/ocaml-ppx_custom_printf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/13 10:40:28 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/07/31 09:27:29 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_custom_printf
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_custom_printf
 OCAML_PPX_CUSTOM_PRINTF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_custom_printf+=	ocaml-ppx_custom_printf>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_custom_printf+=	ocaml-ppx_custom_printf>=0.12.1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_custom_printf?=	../../devel/ocaml-ppx_custom_printf
 
 .endif	# OCAML_PPX_CUSTOM_PRINTF_BUILDLINK3_MK
