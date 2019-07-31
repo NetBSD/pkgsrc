328b112df41e5124345bc0b56f8c70ea3c860e67
brook <brook@pkgsrc.org>
2019-07-31 18:56:18 +0000

    R-bitops: update to canonical form of an R package.
    Update to the canonical form of an R package and fix the LICENSE
    field.

--- math/R-bitops/buildlink3.mk
+++ math/R-bitops/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2011/06/14 21:28:50 alnsn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/07/31 18:56:18 brook Exp $
 
 BUILDLINK_TREE+=	Rbitops
 
 .if !defined(RBITOPS_BUILDLINK3_MK)
 RBITOPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.Rbitops+=	R-bitops>=1.0.4.1
-BUILDLINK_ABI_DEPENDS.Rbitops+=	R-bitops>=1.0.4.1
+BUILDLINK_API_DEPENDS.Rbitops+=	R-bitops>=1.0.6
 BUILDLINK_PKGSRCDIR.Rbitops?=	../../math/R-bitops
 .endif # RBITOPS_BUILDLINK3_MK
 
