39b30ae5a597b5c1ee4d6fe3c9724b776bb277d6
brook <brook@pkgsrc.org>
2019-07-31 18:39:23 +0000

    R-RcppArmadillo: update to version 0.9.600.4.0.
    Update to the canonical form of an R package and fix the LICENSE
    field.

--- math/R-RcppArmadillo/buildlink3.mk
+++ math/R-RcppArmadillo/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/03/22 15:36:04 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/07/31 18:39:23 brook Exp $
 
 BUILDLINK_TREE+=	R-RcppArmadillo
 
 .if !defined(R_RCPPARMADILLO_BUILDLINK3_MK)
 R_RCPPARMADILLO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.R-RcppArmadillo+=	R-RcppArmadillo>=0.8.300.1.0
+BUILDLINK_API_DEPENDS.R-RcppArmadillo+=	R-RcppArmadillo>=0.9.600.4.0
 BUILDLINK_PKGSRCDIR.R-RcppArmadillo?=	../../math/R-RcppArmadillo
 
 .include "../../devel/R-Rcpp/buildlink3.mk"
