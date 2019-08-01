a15b9e787d4ee3518fcfe089f7f769f4b61a65fc
taca <taca@pkgsrc.org>
2019-08-01 14:14:04 +0000

    lang/php73: update to 7.3.8
    01 Aug 2019, PHP 7.3.8
    
    - Core:
      . Added syslog.filter=raw option. (Erik Lundin)
      . Fixed bug #78212 (Segfault in built-in webserver). (cmb)
    
    - Date:
      . Fixed bug #69044 (discrepency between time and microtime). (krakjoe)
      . Updated timelib to 2018.02. (Derick)
    
    - EXIF:
      . Fixed bug #78256 (heap-buffer-overflow on exif_process_user_comment).
        (CVE-2019-11042) (Stas)
      . Fixed bug #78222 (heap-buffer-overflow on exif_scan_thumbnail).
        (CVE-2019-11041) (Stas)
    
    - FTP:
      . Fixed bug #78039 (FTP with SSL memory leak). (Nikita)
    
    - Libxml:
      . Fixed bug #78279 (libxml_disable_entity_loader settings is shared between
        requests (cgi-fcgi)). (Nikita)
    
    - LiteSpeed:
      . Updated to LiteSpeed SAPI V7.4.3 (increased response header count limit from
        100 to 1000, added crash handler to cleanly shutdown PHP request, added
        CloudLinux mod_lsapi mode). (George Wang)
      . Fixed bug #76058 (After "POST data can't be buffered", using php://input
        makes huge tmp files). (George Wang)
    
    - Openssl:
      . Fixed bug #78231 (Segmentation fault upon stream_socket_accept of exported
        socket-to-stream). (Nikita)
    
    - Opcache:
      . Fixed bug #78341 (Failure to detect smart branch in DFA pass). (Nikita)
      . Fixed bug #78189 (file cache strips last character of uname hash). (cmb)
      . Fixed bug #78202 (Opcache stats for cache hits are capped at 32bit NUM).
        (cmb)
      . Fixed bug #78271 (Invalid result of if-else). (Nikita)
      . Fixed bug #78291 (opcache_get_configuration doesn't list all directives).
        (Andrew Collington)
    
    - PCRE:
      . Fixed bug #78338 (Array cross-border reading in PCRE). (cmb)
      . Fixed bug #78197 (PCRE2 version check in configure fails for "##.##-xxx"
        version strings). (pgnet, Peter Kokot)
    
    - PDO_Sqlite:
      . Fixed bug #78192 (SegFault when reuse statement after schema has changed).
        (Vincent Quatrevieux)
    
    - Phar:
      . Fixed bug #77919 (Potential UAF in Phar RSHUTDOWN). (cmb)
    
    - Phpdbg:
      . Fixed bug #78297 (Include unexistent file memory leak). (Nikita)
    
    - SQLite:
      . Upgraded to SQLite 3.28.0. (cmb)
    
    - Standard:
      . Fixed bug #78241 (touch() does not handle dates after 2038 in PHP 64-bit). (cmb)
      . Fixed bug #78269 (password_hash uses weak options for argon2). (Remi)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.262 2019/07/08 13:20:29 taca Exp $
+# $NetBSD: phpversion.mk,v 1.263 2019/08/01 14:14:04 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP71_VERSION=	7.1.30
 PHP72_VERSION=	7.2.20
-PHP73_VERSION=	7.3.7
+PHP73_VERSION=	7.3.8
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
