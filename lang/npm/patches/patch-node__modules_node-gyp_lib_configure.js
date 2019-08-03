93e4f7f9f1c6e526315b438e6b9ecb2be6b476f1
adam <adam@pkgsrc.org>
2019-08-03 07:32:55 +0000

    npm: updated to 6.10.2
    v6.10.2:
    
    tl;dr - Fixes several issues with the cache when npm is run as sudo on Unix systems.
    
    TESTING
    check test cache for root-owned files
    run sudo tests on Travis-CI
    set --no-esm tap flag
    add script to run tests and leave fixtures for inspection and debugging
    
    BUGFIXES
    add a util for writing arbitrary files to cache This prevents metrics timing and debug logs from becoming root-owned.
    infer cache owner from parent dir in correct-mkdir util
    ensure correct owner on cached all-packages metadata
    report server error on failure
    Fix npm ci with file: dependencies.

--- lang/npm/patches/patch-node__modules_node-gyp_lib_configure.js
+++ lang/npm/patches/patch-node__modules_node-gyp_lib_configure.js
@@ -1,22 +1,22 @@
-$NetBSD: patch-node__modules_node-gyp_lib_configure.js,v 1.1 2018/05/04 13:23:53 fhajny Exp $
+$NetBSD: patch-node__modules_node-gyp_lib_configure.js,v 1.2 2019/08/03 07:32:55 adam Exp $
 
 Skip Python detection, version 2.7 is required and we know where it is.
 
---- node_modules/node-gyp/lib/configure.js.orig	2018-04-24 05:41:37.000000000 +0000
+--- node_modules/node-gyp/lib/configure.js.orig	2019-07-23 16:20:30.000000000 +0000
 +++ node_modules/node-gyp/lib/configure.js
-@@ -29,21 +29,14 @@ exports.usage = 'Generates ' + (win ? 'M
+@@ -15,21 +15,14 @@ if (win) {
+ }
  
  function configure (gyp, argv, callback) {
- 
--  var python = gyp.opts.python || process.env.PYTHON || 'python2'
+-  var python
 +  var python = '@PYTHONBIN@'
-     , buildDir = path.resolve('build')
-     , configNames = [ 'config.gypi', 'common.gypi' ]
-     , configs = []
-     , nodeDir
-     , release = processRelease(argv, gyp, process.version, process.release)
+   var buildDir = path.resolve('build')
+   var configNames = [ 'config.gypi', 'common.gypi' ]
+   var configs = []
+   var nodeDir
+   var release = processRelease(argv, gyp, process.version, process.release)
  
--  findPython(python, function (err, found) {
+-  findPython(gyp.opts.python, function (err, found) {
 -    if (err) {
 -      callback(err)
 -    } else {
@@ -27,4 +27,4 @@ Skip Python detection, version 2.7 is required and we know where it is.
 +  getNodeDir()
  
    function getNodeDir () {
- 
+     // 'python' should be set by now
