40dfd57057da1621898ab8f7f87c06a4b4ce113a
morr <morr@pkgsrc.org>
2019-08-03 20:37:19 +0000

    Update to version 2.0.3.
    ChangeLog too big to list here, please take a look at
    http://www.haproxy.org/download/2.0/src/CHANGELOG

--- net/haproxy/options.mk
+++ net/haproxy/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.6 2017/07/04 07:04:17 adam Exp $
+# $NetBSD: options.mk,v 1.7 2019/08/03 20:37:19 morr Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.haproxy
-PKG_SUPPORTED_OPTIONS=	pcre ssl deviceatlas lua
+PKG_SUPPORTED_OPTIONS=	pcre ssl deviceatlas lua prometheus
 PKG_SUGGESTED_OPTIONS=	pcre ssl
 
 .include "../../mk/bsd.options.mk"
@@ -24,6 +24,14 @@ BUILD_MAKE_FLAGS+=	LUA_VERSION_ACCEPTED=53
 BUILD_MAKE_FLAGS+=	LUA_INC=${PREFIX}/${LUA_INCDIR}
 .  include "../../lang/lua/buildlink3.mk"
 .endif
+
+###
+### Use Prometheus
+###
+.if !empty(PKG_OPTIONS:Mprometheus)
+BUILD_MAKE_FLAGS+=	EXTRA_OBJS="contrib/prometheus-exporter/service-prometheus.o"
+.endif
+
 
 ###
 ### Support DeviceAtlas detection.
