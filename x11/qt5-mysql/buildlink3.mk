95327cbeadfff4ce950f4e8df2dde588aebd0aca
adam <adam@pkgsrc.org>
2019-08-09 17:41:38 +0000

    qt5: updated to 5.13.0
    New in Qt 5.13 and Tools Update
    
    Get ready for a round of great new features and development tools! Highlights include
    
    * Improvements to Qt Quick, Qt Quick Controls 2, Qt 3D, Qt WebEngine, and more
    * Full support for Qt for WebAssembly and Qt KNX
    * Sketch support for Qt Design Studio and other upgrades to our UI design tools

--- x11/qt5-mysql/buildlink3.mk
+++ x11/qt5-mysql/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2019/04/03 00:33:17 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2019/08/09 17:41:38 adam Exp $
 
 BUILDLINK_TREE+=	qt5-mysql
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qt5-mysql
 QT5_MYSQL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt5-mysql+=	qt5-mysql>=5.5.1
-BUILDLINK_ABI_DEPENDS.qt5-mysql+=	qt5-mysql>=5.12.2nb1
+BUILDLINK_ABI_DEPENDS.qt5-mysql+=	qt5-mysql>=5.13.0
 BUILDLINK_PKGSRCDIR.qt5-mysql?=		../../x11/qt5-mysql
 
 BUILDLINK_LIBDIRS.qt5-mysql+=		qt5/plugins
