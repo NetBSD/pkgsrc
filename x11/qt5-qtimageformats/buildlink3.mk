f2044771c1e687731f2e6e4f3001dcd1f01c3ed5
adam <adam@pkgsrc.org>
2019-08-09 17:41:38 +0000

    qt5: updated to 5.13.0
    New in Qt 5.13 and Tools Update
    
    Get ready for a round of great new features and development tools! Highlights include
    
    * Improvements to Qt Quick, Qt Quick Controls 2, Qt 3D, Qt WebEngine, and more
    * Full support for Qt for WebAssembly and Qt KNX
    * Sketch support for Qt Design Studio and other upgrades to our UI design tools

--- x11/qt5-qtimageformats/buildlink3.mk
+++ x11/qt5-qtimageformats/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2019/04/03 00:33:18 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2019/08/09 17:41:40 adam Exp $
 
 BUILDLINK_TREE+=	qt5-qtimageformats
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qt5-qtimageformats
 QT5_QTIMAGEFORMATS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt5-qtimageformats+=	qt5-qtimageformats>=5.9.1
-BUILDLINK_ABI_DEPENDS.qt5-qtimageformats+=	qt5-qtimageformats>=5.12.2nb1
+BUILDLINK_ABI_DEPENDS.qt5-qtimageformats+=	qt5-qtimageformats>=5.13.0
 BUILDLINK_PKGSRCDIR.qt5-qtimageformats?=	../../x11/qt5-qtimageformats
 
 BUILDLINK_INCDIRS.qt5-qtimageformats+=	qt5/include
