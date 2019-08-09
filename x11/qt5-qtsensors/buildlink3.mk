bdcebbf44770572b41ae5128044f08a2b2fab990
adam <adam@pkgsrc.org>
2019-08-09 17:41:38 +0000

    qt5: updated to 5.13.0
    New in Qt 5.13 and Tools Update
    
    Get ready for a round of great new features and development tools! Highlights include
    
    * Improvements to Qt Quick, Qt Quick Controls 2, Qt 3D, Qt WebEngine, and more
    * Full support for Qt for WebAssembly and Qt KNX
    * Sketch support for Qt Design Studio and other upgrades to our UI design tools

--- x11/qt5-qtsensors/buildlink3.mk
+++ x11/qt5-qtsensors/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2019/04/03 00:33:19 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2019/08/09 17:41:41 adam Exp $
 
 BUILDLINK_TREE+=	qt5-qtsensors
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qt5-qtsensors
 QT5_QTSENSORS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt5-qtsensors+=	qt5-qtsensors>=5.9.1
-BUILDLINK_ABI_DEPENDS.qt5-qtsensors+=	qt5-qtsensors>=5.12.2nb1
+BUILDLINK_ABI_DEPENDS.qt5-qtsensors+=	qt5-qtsensors>=5.13.0
 BUILDLINK_PKGSRCDIR.qt5-qtsensors?=	../../x11/qt5-qtsensors
 
 BUILDLINK_INCDIRS.qt5-qtsensors+=	qt5/include
