51f4d0404504cded7bf0cb0545a87ed7ab926465
ryoon <ryoon@pkgsrc.org>
2019-08-10 11:41:29 +0000

    Update to 6.3.0.4
    * qt4 option is no longer provided
    
    Changelog:
    See: https://wiki.documentfoundation.org/ReleaseNotes/6.3 in detail
    
    * Improve performances
    * Improve file load an save performances in Writer
    * Improve MS Office file supoort in Calc
    * Use tabcompact UI by default
    * Use context single mode for Writer and Draw
    * Support PDF/A-2 export
    * And many improvements and bugfixes

--- misc/libreoffice/patches/patch-external_postgresql_ExternalProject__postgresql.mk
+++ misc/libreoffice/patches/patch-external_postgresql_ExternalProject__postgresql.mk
@@ -1,15 +1,15 @@
-$NetBSD: patch-external_postgresql_ExternalProject__postgresql.mk,v 1.1 2016/12/06 15:21:00 ryoon Exp $
+$NetBSD: patch-external_postgresql_ExternalProject__postgresql.mk,v 1.2 2019/08/10 11:41:29 ryoon Exp $
 
 * Do not try to link NSPR/NSS libraries. They are not required really.
 
---- external/postgresql/ExternalProject_postgresql.mk.orig	2016-07-28 22:11:19.000000000 +0000
+--- external/postgresql/ExternalProject_postgresql.mk.orig	2019-08-06 17:26:35.000000000 +0000
 +++ external/postgresql/ExternalProject_postgresql.mk
-@@ -60,7 +60,7 @@ $(call gb_ExternalProject_get_state_targ
- 				$(if $(WITH_GSSAPI),--with-gssapi)) \
+@@ -67,7 +67,7 @@ $(call gb_ExternalProject_get_state_targ
+ 				$(if $(ENABLE_LDAP),,--with-ldap=no) \
  			CPPFLAGS="$(postgresql_CPPFLAGS)" \
  			LDFLAGS="$(postgresql_LDFLAGS)" \
--			EXTRA_LDAP_LIBS="-llber -lssl3 -lsmime3 -lnss3 -lnssutil3 -lplds4 -lplc4 -lnspr4" \
-+			EXTRA_LDAP_LIBS="-llber" \
+-			$(if $(ENABLE_LDAP),EXTRA_LDAP_LIBS="-llber -lssl3 -lsmime3 -lnss3 -lnssutil3 -lplds4 -lplc4 -lnspr4") \
++			$(if $(ENABLE_LDAP),EXTRA_LDAP_LIBS="-llber") \
  		&& cd src/interfaces/libpq \
  		&& MAKEFLAGS= && $(MAKE) all-static-lib)
  
