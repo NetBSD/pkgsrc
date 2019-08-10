dc66f6f04b43aa9c316b4f7124ba19693e8c0694
bsiegert <bsiegert@pkgsrc.org>
2019-08-10 12:11:27 +0000

    Pullup ticket #6021 - requested by taca lang/php71: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.265
    - lang/php71/distinfo                                           1.53
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Thu Aug  1 14:20:58 UTC 2019
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php71: distinfo
    
       Log Message:
       lang/php71: update to 7.1.31
    
       01 Aug 2019, PHP 7.1.31
    
       - SQLite:
         . Upgraded to SQLite 3.28.0. (cmb)
    
       - EXIF:
         . Fixed bug #78256 (heap-buffer-overflow on exif_process_user_comment).
         (CVE-2019-11042) (Stas)
         . Fixed bug #78222 (heap-buffer-overflow on exif_scan_thumbnail).
         (CVE-2019-11041) (Stas)
    
       - Phar:
         . Fixed bug #77919 (Potential UAF in Phar RSHUTDOWN). (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.260.2.2 2019/08/10 12:07:22 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.260.2.3 2019/08/10 12:11:27 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -88,7 +88,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
-PHP71_VERSION=	7.1.30
+PHP71_VERSION=	7.1.31
 PHP72_VERSION=	7.2.21
 PHP73_VERSION=	7.3.8
 
