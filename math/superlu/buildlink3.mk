84e123ddd2649426dd22c35a2835afe9b7a227e5
wiz <wiz@pkgsrc.org>
2019-08-11 13:17:48 +0000

    Bump PKGREVISIONs for perl 5.30.0

--- math/superlu/buildlink3.mk
+++ math/superlu/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2018/08/22 09:42:52 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2019/08/11 13:17:49 wiz Exp $
 
 BUILDLINK_TREE+=	superlu
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	superlu
 SUPERLU_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.superlu+=	superlu>=2.0nb2
-BUILDLINK_ABI_DEPENDS.superlu?=	superlu>=3.0.1nb6
+BUILDLINK_ABI_DEPENDS.superlu?=	superlu>=3.0.1nb7
 BUILDLINK_PKGSRCDIR.superlu?=	../../math/superlu
 BUILDLINK_DEPMETHOD.superlu?=	build
 .endif # SUPERLU_BUILDLINK3_MK
