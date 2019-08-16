4d9ba4bd550ac0ca3f3561c57d0b29f7792ee368
wiz <wiz@pkgsrc.org>
2019-08-16 10:08:04 +0000

    libnotify: update to 0.7.8.
    Switch to meson build system. Remove introspection option.
    
    New in 0.7.8
    ============
    * Support meson build system [Marco; !3]
    * notify-send: Support full URLs as hint values [Marco; !4]
    * Mic. bug fixes [Ting-Wei, Florian; #760438, !8]
    
    Contributors:
      Emmanuele Bassi, Abderrahim Kitouni, Ting-Wei Lan, Iain Lane, Florian Müllner,
      Jan Tojnar, Marco Trevisan (Treviño), Marco Trevisan

--- sysutils/libnotify/options.mk
+++ sysutils/libnotify/options.mk
@@ -1,9 +1,8 @@
-# $NetBSD: options.mk,v 1.3 2016/11/08 10:24:43 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2019/08/16 10:08:04 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libnotify
-PKG_SUPPORTED_OPTIONS=	doc introspection
-PKG_SUGGESTED_OPTIONS+=	introspection
-PLIST_VARS+=		doc introspection
+PKG_SUPPORTED_OPTIONS=	doc
+PLIST_VARS+=		doc
 
 .include "../../mk/bsd.options.mk"
 
@@ -13,13 +12,4 @@ CONFIGURE_ARGS+=	--enable-docbook-docs
 PLIST.doc=		yes
 .else
 CONFIGURE_ARGS+=	--disable-docbook-docs
-.endif
-
-.if !empty(PKG_OPTIONS:Mintrospection)
-BUILDLINK_API_DEPENDS.gobject-introspection+=	gobject-introspection>=0.9.12
-.include "../../devel/gobject-introspection/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-introspection=yes
-PLIST.introspection=	yes
-.else
-CONFIGURE_ARGS+=	--enable-introspection=no
 .endif
