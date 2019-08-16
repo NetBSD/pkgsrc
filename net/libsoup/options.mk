97910db34ece270ef123dda8759eb09257673f15
wiz <wiz@pkgsrc.org>
2019-08-16 13:08:58 +0000

    libsoup: update to 2.66.2.
    Switch to meson since autotools build system was removed.
    
    Changes in libsoup from 2.66.1 to 2.66.2:
    
    	* Make gettext optional (might not be available in Windows)
    	  [Seungha Yang]
    
    	* MSVC: set encoding to UTF-8 to avoid errors [Seungha Yang]
    
    	* MinGW tests build fix [Tomas Popela]
    
    	* Check for TLS support only when external glib dependency is available
    	  [Seungha Yang]
    
    Changes in libsoup from 2.66.0 to 2.66.1:
    
            * Fix dylib versioning in MacOS [Tom Schoonjans]
    
            * Visual Studio build fixes [Chun-wei Fan]
    
            * MinGW build fixes [Руслан Ижбулатов]
    
            * Meson build system improvements [Claudio Saavedra, Jakub Adam]
    
            * Fix random CI failures due to parallel apache tests [Claudio Saavedra]
    
            * Code cleanups [Claudio Saavedra]
    
    Changes in libsoup from 2.65.92 to 2.66.0:
    
            * Fix lookup for libxml2 on Visual Studio builds [Chun-wei Fan]
    
    Changes in libsoup from 2.65.91 to 2.65.92:
    
            * SoupConnection: Fix check for remote disconnectin idle state
              [Michael Catanzaro, #134]
    
            * Code cleanups and warnings fixed [Michael Catanzaro]
    
            * Remove stale .pc.in files [Claudio Saavedra, #137]
    
            * Updated translations: Dutch
    
    Changes in libsoup from 2.65.90 to 2.65.91:
    
    	* Remove RFCs from sources [Tomas Popela, #133]
    
    Changes in libsoup from 2.65.2 to 2.65.90:
    
            * Avoid using threads in SoupRequestFile asynchronous API
              [Ignacio Casal Quinteiro]
    
    Changes in libsoup from 2.65.1 to 2.65.2:
    
    	* Remove autotools build system and change to meson by default
              [#12, Tomas Popela]
    
    	* Many fixes to Meson [Claudio Saavedra, Tomas Popela, Xavier
              Claessens]
    
    	* Improvements to the continuous integration infrastructure
              [Xavier Claessens]
    
    	* Make sure SoupSession features are not disposed until all
              pending messages are processed [#130, Claudio Saavedra]
    
    	* Updated translations: Greek, Slovak, French.
    
    Changes in libsoup from 2.64.0 to 2.65.1:
    
    	* Added MTLMv2 responses support [Michal Hrubý]
    	* Many fixes to the meson build system [Tomas Popela]
    	* Remove Visual Studio project files [Chun-wei Fan]
    	* Enable VAPI by default [#119, Jeremy Bicha]
    	* New strict header field parameter parsing API [Claudio Saavedra]
    	* Code cleanups [Claudio Saavedra]
    	* Updated translations: Belarusian, Serbian

--- net/libsoup/options.mk
+++ net/libsoup/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2016/05/03 11:37:39 prlw1 Exp $
+# $NetBSD: options.mk,v 1.5 2019/08/16 13:08:58 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libsoup
 PKG_SUPPORTED_OPTIONS=	gssapi
@@ -8,8 +8,4 @@ PKG_SUGGESTED_OPTIONS=
 
 .if !empty(PKG_OPTIONS:Mgssapi)
 .  include "../../mk/krb5.buildlink3.mk"
-CONFIGURE_ARGS+=	--with-gssapi
-CONFIGURE_ARGS+=	--with-krb5-config=${KRB5_CONFIG}
-.else
-CONFIGURE_ARGS+=	--without-gssapi
 .endif
