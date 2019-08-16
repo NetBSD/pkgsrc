cea76e6d13803bdc931af0b75f1d0e8e2dac0220
wiz <wiz@pkgsrc.org>
2019-08-16 10:43:24 +0000

    pango: update to 1.44.3.
    Switch to meson, autoconf build support was removed.
    
    Overview of changes in 1.44.3
    =============================
    - Install pango-ot headers
    - Make subpixel positioning optional
    - fc: Ignore fonts with unsupported formats
    
    Overview of changes in 1.44.2
    =============================
    - Disable ligatures when letterspacing
    - Set design coords on hb_font_t
    - Expose more font options in pango-view
    - OS X: Make 'system-ui' font work
    - Keep deprecated pango-fc apis in headers
    - Make hex boxes work, always
    - introspection: Various build fixes
    - introspection: Add PangoPT, PangoFT2 namespaces
    - layout: Make the new line-spacing opt-in
    
    Overview of changes in 1.44.1
    =============================
    - Fix a crash with allow_break attributes
    - Fix Emoji spacing
    - Fix up includes and pkg-config requires
    - Correct some cases for hyphen insertion
    
    Overview of changes in 1.44.0
    =============================
    - Use harfbuzz for shaping on all platforms
    - Stop using freetype for font loading; this
        drops support for type1 and bitmap fonts
    - Add a getter for hb_font_t
    - Make PangoCoverage a GObject
    - Add a pango_tailor_break api
    - font metrics: Add line height
    - layout: Support line spacing
    - layout: Draw hyphens for line breaks
    - Add an attribute to suppress line breaking
    - cairo: Don't render hex boxes for space
    - Add an attribute to show invisible characters
    - Stop quantizing glyph positions
    - Add tests for itemization and line breaking
    - Remove language and shape engine remnants
    - Rename meson options: gtk_doc, introspection
    - Require GLib 2.59.2
    - Require Harfbuzz 2.0
    
    Overview of changes in 1.43.0
    =============================
    - Drop autotools
    - Drop Visual Studio build
    - Build with meson everywhere
    - Update Emoji tables for Unicode 11
    - Update test data for Unicode 11
    - Fix a crash with Thai breaking
    - Fix a crash with font variations
    - Deprecate bidi apis in favor of fribidi
    - Add a variable font family api
    - Improve font fallback handling on win32

--- devel/pango/patches/patch-pango_pangocoretext.c
+++ devel/pango/patches/patch-pango_pangocoretext.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-pango_pangocoretext.c,v 1.1 2018/11/20 08:34:43 maya Exp $
+$NetBSD: patch-pango_pangocoretext.c,v 1.2 2019/08/16 10:43:24 wiz Exp $
 From 60df2b006e5d4553abc7bb5fe9a99539c91b0022 Mon Sep 17 00:00:00 2001
 From: Tom Schoonjans <Tom.Schoonjans%diamond.ac.uk@localhost>
 Date: Fri, 20 Jul 2018 12:30:34 +0100
@@ -6,9 +6,9 @@ Subject: [PATCH] Revert "Restore scaling of CoreText fonts."
 
 This reverts commit f661cdbf8cfbdb2ba7241d8dfa4a579c6dfe0a94.
 
---- pango/pangocoretext.c.orig	2018-07-16 00:50:43.000000000 +0000
+--- pango/pangocoretext.c.orig	2019-08-04 01:59:02.000000000 +0000
 +++ pango/pangocoretext.c
-@@ -225,7 +225,10 @@ pango_core_text_font_class_init (PangoCo
+@@ -204,7 +204,10 @@ pango_core_text_font_class_init (PangoCo
    object_class->finalize = pango_core_text_font_finalize;
  
    font_class->describe = pango_core_text_font_describe;
@@ -18,5 +18,5 @@ This reverts commit f661cdbf8cfbdb2ba7241d8dfa4a579c6dfe0a94.
 +   */
 +  font_class->describe_absolute = pango_core_text_font_describe;
    font_class->get_coverage = pango_core_text_font_get_coverage;
-   font_class->find_shaper = pango_core_text_font_find_shaper;
    font_class->get_font_map = pango_core_text_font_get_font_map;
+   font_class->create_hb_font = pango_core_text_font_create_hb_font;
