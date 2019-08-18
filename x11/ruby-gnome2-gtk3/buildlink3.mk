137059aabe62bde863d5813ded2ec414db91b990
tsutsui <tsutsui@pkgsrc.org>
2019-08-18 03:13:21 +0000

    ruby-gnome2: update to 3.3.7.
    Upstream changes (from NEWS):
    
    == Ruby-GNOME2 3.3.7: 2019-08-17
    
    This is the bug fix release of 3.3.6.
    
    === Changes
    
    ==== Ruby/Pango
    
      * Improvements
    
        * (({Pango::AttrList#each})): Added.
    
        * (({Pango::AttrType})): Added support for (({PANGO_ATTR_FONT_DESC})).
    
    ==== Ruby/GObjectIntrospection
    
      * Improvements
    
        * Added support for (({GHashTable<utf8, enum>})).
    
        * Added support for GObject Introspection 1.60.0.
    
    ==== Ruby/Pango
    
      * Improvements
    
        * Added support for Pango 1.44.
          [GitHub#1288][Reported by Toshiaki Asai]
    
    ==== Ruby/GTK3
    
      * Fixes
    
        * Fixed document markup.
          [GitHub#1280][GitHub#1281][GitHub#1282][Patch by İsmail Arılık]
    
    ==== Ruby/WNCK3
    
      * Added.
        [GitHub#1284][Reported by Christopher L. Ramsey]
    
    ==== Ruby/libsecret
    
      * Added.
    
    === Thanks
    
      * İsmail Arılık
    
      * Christopher L. Ramsey
    
      * Toshiaki Asai

--- x11/ruby-gnome2-gtk3/buildlink3.mk
+++ x11/ruby-gnome2-gtk3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.27 2019/07/21 22:24:20 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2019/08/18 03:13:23 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gnome2-gtk3
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ruby-gnome2-gtk3
 RUBY_GNOME2_GTK3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-gtk3+=	${RUBY_PKGPREFIX}-gnome2-gtk3>=1.2.0
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk3+=	${RUBY_PKGPREFIX}-gnome2-gtk3>=3.3.6nb1
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-gtk3+=	${RUBY_PKGPREFIX}-gnome2-gtk3>=3.3.7
 BUILDLINK_PKGSRCDIR.ruby-gnome2-gtk3?=		../../x11/ruby-gnome2-gtk3
 
 .include "../../devel/ruby-gnome2-glib/buildlink3.mk"
