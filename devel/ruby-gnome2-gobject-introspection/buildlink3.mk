be632172b8716d5da3f8849b3e5a55a4b76c0b75
tsutsui <tsutsui@pkgsrc.org>
2019-08-18 03:13:21 +0000

    ruby-gnome2: update to 3.3.7.
    Upstream changes (from NEWS):
    
    == Ruby-GNOME2 3.3.7: 2019-08-17
    
    This is the bug fix release of 3.3.6.
    
    === Changes
    
    ==== Ruby/Pango
    
      * Improvements
    
        * (({Pango::AttrList#each})): Added.
    
        * (({Pango::AttrType})): Added support for (({PANGO_ATTR_FONT_DESC})).
    
    ==== Ruby/GObjectIntrospection
    
      * Improvements
    
        * Added support for (({GHashTable<utf8, enum>})).
    
        * Added support for GObject Introspection 1.60.0.
    
    ==== Ruby/Pango
    
      * Improvements
    
        * Added support for Pango 1.44.
          [GitHub#1288][Reported by Toshiaki Asai]
    
    ==== Ruby/GTK3
    
      * Fixes
    
        * Fixed document markup.
          [GitHub#1280][GitHub#1281][GitHub#1282][Patch by İsmail Arılık]
    
    ==== Ruby/WNCK3
    
      * Added.
        [GitHub#1284][Reported by Christopher L. Ramsey]
    
    ==== Ruby/libsecret
    
      * Added.
    
    === Thanks
    
      * İsmail Arılık
    
      * Christopher L. Ramsey
    
      * Toshiaki Asai

--- devel/ruby-gnome2-gobject-introspection/buildlink3.mk
+++ devel/ruby-gnome2-gobject-introspection/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/06/09 03:36:07 tsutsui Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2019/08/18 03:13:21 tsutsui Exp $
 
 BUILDLINK_TREE+=	ruby-gnome2-gobject-introspection
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ruby-gnome2-gobject-introspection
 RUBY_GNOME2_GOBJECT_INTROSPECTION_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-gobject-introspection+=	${RUBY_PKGPREFIX}-gnome2-gobject-introspection>=1.2.0
-BUILDLINK_ABI_DEPENDS.ruby-gnome2-gobject-introspection+=	${RUBY_PKGPREFIX}-gnome2-gobject-introspection>=3.2.7
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-gobject-introspection+=	${RUBY_PKGPREFIX}-gnome2-gobject-introspection>=3.3.7
 BUILDLINK_PKGSRCDIR.ruby-gnome2-gobject-introspection?=		../../devel/ruby-gnome2-gobject-introspection
 
 .include "../../devel/gobject-introspection/buildlink3.mk"
