14483b59f04b08ba46b917292310b10fa26ac600
wiz <wiz@pkgsrc.org>
2019-08-20 14:06:46 +0000

    glib2: update to 2.60.6
    Overview of changes in GLib 2.60.6
    ==================================
    
    * Fix various bugs with use of the `GKeyfileSettingsBackend` within flatpaks (!984, !985, #1825)
    
    * Bugs fixed:
     - !993 Backport !984, !985 keyfile/portal fixes to glib-2-60
    
    
    Overview of changes in GLib 2.60.5
    ==================================
    
    * Fix implicit use of the `GKeyfileSettingsBackend` (#1822)
    
    * Fix opening a URI using the ‘Open URI’ portal (!968)
    
    * Bugs fixed:
     - !910 Backport !909 “D-Bus auth mechanism improvements” to glib-2-60
     - !949 Backport !945 “Avoid overrunning stack at the end of the varargs.” to glib-2-60
     - !956 Backport !954 “Fix the ISO 15924 code for Manichaean” to glib-2-60
     - !958 gthread: fix minor errno problem in GCond
     - !969 Backport !968 ”Fix typo in request handle” to glib-2-60
     - !977 Backport !974 “Ensure that the keyfile settings backend exists” to glib-2-60

--- devel/glib2/patches/patch-gio_giomodule.c
+++ devel/glib2/patches/patch-gio_giomodule.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-gio_giomodule.c,v 1.5 2019/06/06 11:05:12 adam Exp $
+$NetBSD: patch-gio_giomodule.c,v 1.6 2019/08/20 14:06:46 wiz Exp $
 
 Disable inotify on SunOS.
 
---- gio/giomodule.c.orig	2019-05-03 13:43:28.000000000 +0000
+--- gio/giomodule.c.orig	2019-07-24 10:52:04.000000000 +0000
 +++ gio/giomodule.c
-@@ -1209,7 +1206,7 @@ _g_io_modules_ensure_loaded (void)
-       /* Initialize types from built-in "modules" */
+@@ -1210,7 +1210,7 @@ _g_io_modules_ensure_loaded (void)
        g_type_ensure (g_null_settings_backend_get_type ());
        g_type_ensure (g_memory_settings_backend_get_type ());
+       g_type_ensure (g_keyfile_settings_backend_get_type ());
 -#if defined(HAVE_INOTIFY_INIT1)
 +#if defined(HAVE_INOTIFY_INIT1) && !defined(__sun)
        g_type_ensure (g_inotify_file_monitor_get_type ());
