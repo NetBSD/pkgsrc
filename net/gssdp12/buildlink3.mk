f9bdde991ec98d927d7942696caa50aeddc0f825
wiz <wiz@pkgsrc.org>
2019-08-20 11:26:13 +0000

    net/gssdp12: import gssdp12-1.2.1
    A GObject-based API for handling resource discovery and announcement
    over SSDP (Simple Service Discovery Protocol). This package also
    provides a GTK-based device sniffer.
    
    This package contains the 1.2 branch.

---
+++ net/gssdp12/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/08/20 11:26:13 wiz Exp $
+
+BUILDLINK_TREE+=	gssdp12
+
+.if !defined(GSSDP12_BUILDLINK3_MK)
+GSSDP12_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gssdp12+=	gssdp12>=1.2
+BUILDLINK_PKGSRCDIR.gssdp12?=	../../net/gssdp12
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../net/libsoup/buildlink3.mk"
+.endif	# GSSDP12_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gssdp12
