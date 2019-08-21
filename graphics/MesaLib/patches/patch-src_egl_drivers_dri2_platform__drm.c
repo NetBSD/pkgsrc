473a172c9ad5330173cd016ba9f434d29462296e
nia <nia@pkgsrc.org>
2019-08-21 13:35:27 +0000

    Move graphics/MesaLib18 to graphics/MesaLib.
    Move graphics/MesaLib to graphics/MesaLib11.

--- graphics/MesaLib/patches/patch-src_egl_drivers_dri2_platform__drm.c
+++ graphics/MesaLib/patches/patch-src_egl_drivers_dri2_platform__drm.c
@@ -1,16 +1,18 @@
-$NetBSD: patch-src_egl_drivers_dri2_platform__drm.c,v 1.2 2016/01/05 13:02:57 tnn Exp $
+$NetBSD: patch-src_egl_drivers_dri2_platform__drm.c,v 1.3 2019/08/21 13:35:28 nia Exp $
 
 netbsd-5 build fix
 
---- src/egl/drivers/dri2/platform_drm.c.orig	2015-12-09 16:10:13.000000000 +0000
+--- src/egl/drivers/dri2/platform_drm.c.orig	2019-01-17 11:26:22.000000000 +0000
 +++ src/egl/drivers/dri2/platform_drm.c
-@@ -626,7 +626,9 @@ dri2_initialize_drm(_EGLDriver *drv, _EG
-       if (gbm == NULL)
-          goto cleanup;
+@@ -725,7 +725,11 @@ dri2_initialize_drm(_EGLDriver *drv, _EG
+       }
+       dri2_dpy->own_device = true;
     } else {
 +#ifdef F_DUPFD_CLOEXEC
-       fd = fcntl(gbm_device_get_fd(gbm), F_DUPFD_CLOEXEC, 3);
+       dri2_dpy->fd = fcntl(gbm_device_get_fd(gbm), F_DUPFD_CLOEXEC, 3);
++#else
++      dri2_dpy->fd = -1;
 +#endif
-       if (fd < 0)
+       if (dri2_dpy->fd < 0) {
+          err = "DRI2: failed to fcntl() existing gbm device";
           goto cleanup;
-    }
---
+++ graphics/MesaLib11/patches/patch-src_mesa_drivers_dri_i965_intel__screen.c
@@ -0,0 +1,39 @@
+$NetBSD: patch-src_mesa_drivers_dri_i965_intel__screen.c,v 1.1 2019/08/21 13:35:29 nia Exp $
+
+Fix build on older NetBSD w/o _SC_PHYS_PAGES.
+
+--- src/mesa/drivers/dri/i965/intel_screen.c.orig	2015-06-07 10:06:53.000000000 +0000
++++ src/mesa/drivers/dri/i965/intel_screen.c
+@@ -42,6 +42,10 @@
+ #include "utils.h"
+ #include "xmlpool.h"
+ 
++#ifdef HAVE_SYS_SYSCTL_H
++# include <sys/sysctl.h>
++#endif
++
+ static const __DRIconfigOptionsExtension brw_config_options = {
+    .base = { __DRI_CONFIG_OPTIONS, 1 },
+    .xml =
+@@ -829,6 +833,13 @@ brw_query_renderer_integer(__DRIscreen *
+       const unsigned gpu_mappable_megabytes =
+          (aper_size / (1024 * 1024)) * 3 / 4;
+ 
++#if defined(HW_PHYSMEM64)
++      int mib[2] = { CTL_HW, HW_PHYSMEM64 };
++      const uint64_t system_memory_bytes;
++      size_t len = sizeof(system_memory_bytes);
++      if (sysctl(mib, 2, &system_memory_bytes, &len, NULL, 0) != 0)
++         return -1;
++#else
+       const long system_memory_pages = sysconf(_SC_PHYS_PAGES);
+       const long system_page_size = sysconf(_SC_PAGE_SIZE);
+ 
+@@ -837,6 +848,7 @@ brw_query_renderer_integer(__DRIscreen *
+ 
+       const uint64_t system_memory_bytes = (uint64_t) system_memory_pages
+          * (uint64_t) system_page_size;
++#endif
+ 
+       const unsigned system_memory_megabytes =
+          (unsigned) (system_memory_bytes / (1024 * 1024));
