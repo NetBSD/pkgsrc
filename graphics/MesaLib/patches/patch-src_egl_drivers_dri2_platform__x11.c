3fa7dd9ddf3faafea70460a17be09005f1a49fe7
nia <nia@pkgsrc.org>
2019-08-21 13:35:27 +0000

    Move graphics/MesaLib18 to graphics/MesaLib.
    Move graphics/MesaLib to graphics/MesaLib11.

--- graphics/MesaLib/patches/patch-src_egl_drivers_dri2_platform__x11.c
+++ graphics/MesaLib/patches/patch-src_egl_drivers_dri2_platform__x11.c
@@ -1,10 +1,23 @@
-$NetBSD: patch-src_egl_drivers_dri2_platform__x11.c,v 1.2 2016/05/18 12:55:28 wiz Exp $
+$NetBSD: patch-src_egl_drivers_dri2_platform__x11.c,v 1.3 2019/08/21 13:35:28 nia Exp $
 
 Provide compat strndup for older Darwin.
 
---- src/egl/drivers/dri2/platform_x11.c.orig	2016-05-05 12:59:22.000000000 +0000
+* From FreeBSD ports 18.0.0:
+work-around for https://bugs.freedesktop.org/show_bug.cgi?id=100627
+
+Mon Apr 10 19:14:48 2017 UTC
+DRI3 remains enabled at compile time, but it is now disabled at
+runtime for stock FreeBSD.  Set LIBGL_DRI3_ENABLE in the
+environment to enable DRI3.
+This change was made to work around a problem in libEGL,
+which fails to fall back to using DRI2 when run on a system
+without DRI3 support.
+
+* Added logging statement to note dri3 initialization being invoked.
+
+--- src/egl/drivers/dri2/platform_x11.c.orig	2018-02-09 02:17:57.000000000 +0000
 +++ src/egl/drivers/dri2/platform_x11.c
-@@ -591,6 +591,23 @@ dri2_x11_local_authenticate(struct dri2_
+@@ -608,6 +608,23 @@ dri2_x11_local_authenticate(struct dri2_
     return EGL_TRUE;
  }
  
@@ -28,3 +41,20 @@ Provide compat strndup for older Darwin.
  static EGLBoolean
  dri2_x11_connect(struct dri2_egl_display *dri2_dpy)
  {
+@@ -1466,8 +1483,15 @@ dri2_initialize_x11(_EGLDriver *drv, _EG
+ 
+    if (!disp->Options.ForceSoftware) {
+ #ifdef HAVE_DRI3
+-      if (!env_var_as_boolean("LIBGL_DRI3_DISABLE", false))
++#if ((defined(__FreeBSD__) || defined(__FreeBSD_kernel__)) && !defined(__DRM_NEXT__)) || defined(__DragonFly__) || defined(__NetBSD__)
++      if (env_var_as_boolean("LIBGL_DRI3_ENABLE", false))
++#endif
++      if (!env_var_as_boolean("LIBGL_DRI3_DISABLE", false)) {
++         _eglLog(_EGL_INFO, "platform_x11.c: calling dri2_initialize_x11_dri3\n");
+          initialized = dri2_initialize_x11_dri3(drv, disp);
++         if (initialized)
++            _eglLog(_EGL_INFO, "platform_x11.c: initialized by dri2_initialize_x11_dri3\n");
++      }
+ #endif
+ 
+       if (!initialized)
--- graphics/MesaLib/patches/patch-src_mesa_drivers_dri_swrast_swrast.c
+++ graphics/MesaLib/patches/patch-src_mesa_drivers_dri_swrast_swrast.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_mesa_drivers_dri_swrast_swrast.c,v 1.2 2015/06/07 15:48:12 tnn Exp $
+$NetBSD: patch-src_mesa_drivers_dri_swrast_swrast.c,v 1.3 2019/08/21 13:35:28 nia Exp $
 
 Fix build on older NetBSD w/o _SC_PHYS_PAGES.
 
---- src/mesa/drivers/dri/swrast/swrast.c.orig	2015-06-07 10:06:53.000000000 +0000
+--- src/mesa/drivers/dri/swrast/swrast.c.orig	2017-10-19 12:23:53.000000000 +0000
 +++ src/mesa/drivers/dri/swrast/swrast.c
-@@ -150,6 +150,12 @@ swrast_query_renderer_integer(__DRIscree
+@@ -151,6 +151,12 @@ swrast_query_renderer_integer(__DRIscree
          size_t len = sizeof(system_memory_bytes);
          if (sysctl(mib, 2, &system_memory_bytes, &len, NULL, 0) != 0)
              return -1;
-+#elif defined(HW_PHYSMEM64)
++#elif defined(HW_PHYSMEM64) && defined(__NetBSD__)
 +        int mib[2] = { CTL_HW, HW_PHYSMEM64 };
 +        uint64_t system_memory_bytes;
 +        size_t len = sizeof(system_memory_bytes);
