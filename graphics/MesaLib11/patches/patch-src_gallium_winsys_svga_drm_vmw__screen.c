d6e2c62d914e8f3c259bcabfb38327b2da6fe9b4
nia <nia@pkgsrc.org>
2019-08-21 13:35:27 +0000

    Move graphics/MesaLib18 to graphics/MesaLib.
    Move graphics/MesaLib to graphics/MesaLib11.

---
+++ graphics/MesaLib11/patches/patch-src_gallium_winsys_svga_drm_vmw__screen.c
@@ -0,0 +1,19 @@
+$NetBSD: patch-src_gallium_winsys_svga_drm_vmw__screen.c,v 1.1 2019/08/21 13:35:29 nia Exp $
+
+* Solaris needs sys/mkdev.h for major(2).
+* Linux needs sys/sysmacros.h for major(2).
+
+--- src/gallium/winsys/svga/drm/vmw_screen.c.orig	2016-01-22 15:44:31.000000000 +0000
++++ src/gallium/winsys/svga/drm/vmw_screen.c
+@@ -32,6 +32,11 @@
+ #include "pipe/p_compiler.h"
+ #include "util/u_hash_table.h"
+ #include <sys/types.h>
++#ifdef __sun
++#include <sys/mkdev.h> /* for major(2) */
++#elif defined(__linux__)
++#include <sys/sysmacros.h>
++#endif
+ #include <sys/stat.h>
+ #include <unistd.h>
+ 
---
+++ graphics/MesaLib11/patches/patch-src_mapi_entry__x86__tls.h
@@ -0,0 +1,48 @@
+$NetBSD: patch-src_mapi_entry__x86__tls.h,v 1.1 2019/08/21 13:35:29 nia Exp $
+
+NetBSD only supports zero-initialized initial-exec tls variables in conjuction
+with dlopen(3) at the moment.
+
+Fix --enable-glx-tls with clang. From FreeBSD.
+
+--- src/mapi/entry_x86_tls.h.orig	2015-09-02 17:06:23.000000000 +0000
++++ src/mapi/entry_x86_tls.h
+@@ -50,10 +50,27 @@ __asm__(".balign 16\n"
+    ".balign 16\n"                \
+    func ":"
+ 
++#ifdef __NetBSD__
++extern const mapi_func table_noop_array[];
+ #define STUB_ASM_CODE(slot)      \
+    "call x86_current_tls\n\t"    \
+    "movl %gs:(%eax), %eax\n\t"   \
++   "testl %eax, %eax\n\t"        \
++   "je 1f\n\t"                   \
++   "jmp *(4 * " slot ")(%eax)\n\t" \
++   "1:\n\t"                      \
++   "call 2f\n"                   \
++   "2:\n\t"                      \
++   "popl %eax\n\t"               \
++   "addl $_GLOBAL_OFFSET_TABLE_+[.-1b], %eax\n\t" \
++   "movl table_noop_array@GOT(%eax), %eax\n\t" \
+    "jmp *(4 * " slot ")(%eax)"
++#else
++#define STUB_ASM_CODE(slot)      \
++   "call x86_current_tls\n\t"    \
++   "movl %gs:(%eax), %eax\n\t"   \
++   "jmp *(4 * " slot ")(%eax)"
++#endif
+ 
+ #define MAPI_TMP_STUB_ASM_GCC
+ #include "mapi_tmp.h"
+@@ -71,8 +88,8 @@ __asm__(".text");
+ extern unsigned long
+ x86_current_tls();
+ 
+-static char x86_entry_start[];
+-static char x86_entry_end[];
++extern char x86_entry_start[] __attribute__((visibility("hidden")));
++extern char x86_entry_end[] __attribute__((visibility("hidden")));
+ 
+ void
+ entry_patch_public(void)
