5cc92f35084d62437cb67e0128156ca0d3c9922f
ryoon <ryoon@pkgsrc.org>
2019-08-22 12:22:48 +0000

    Recursive revbump from boost-1.71.0

--- www/libmediawiki/buildlink3.mk
+++ www/libmediawiki/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.32 2019/07/01 04:07:53 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.33 2019/08/22 12:23:53 ryoon Exp $
 
 BUILDLINK_TREE+=	libmediawiki
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libmediawiki
 LIBMEDIAWIKI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libmediawiki+=	libmediawiki>=2.5.0
-BUILDLINK_ABI_DEPENDS.libmediawiki?=	libmediawiki>=4.13.0nb11
+BUILDLINK_ABI_DEPENDS.libmediawiki?=	libmediawiki>=4.13.0nb12
 BUILDLINK_PKGSRCDIR.libmediawiki?=	../../www/libmediawiki
 
 .include "../../x11/kdelibs4/buildlink3.mk"
