c080c4645c24e81fa7739313abe6d8fc2476df18
adam <adam@pkgsrc.org>
2019-08-22 18:35:22 +0000

    fs-uae: updated to 3.0.0
    Changes in FS-UAE 3.0 (compared to 2.8):
    * Fixed JIT direct access on modern Linux distributions.
    * FS-UAE default is now to show a 692×540 image, not stretched to fill screen.
    * Built-in configurations for many more game controllers.
    * New option whdload_quit_key to gracefully quit slaves.
    * Added more shaders (compiled/ported/written by guest.r and rsn8887).
    * Updated emulation core from WinUAE 3300b2.
    * Several new configuration options (see Changelog for details).
    * Many more smaller fixes and updates (see Changelog for details).
    
    Changes in FS-UAE Launcher 3.0 (compared to 2.8):
    * Plugins can provide files for offline use (WHDLoad, JST, SKick, etc).
    * Added support for .gz/.adz/.roz/.xz-compressed files.
    * Support WHDLoad versions 18.4 and 18.5 (game database).
    * Revert fullscreen_mode to default when upgrading from older versions.
    * Use https for all more fs-uae.net and openretro.org communication.
    * Support hd_requirements = xpkmaster.library.
    * Fixed problem on Windows where devices were sometimes not detected.
    * Make configurations list wider when window width is quite large.
    * Try to use config from game database when launching with a WHDLoad archive.
    * Extract WHDLoad arguments from .info file when loading a WHDLoad archive.
    * Sync “unpublished” games from the online game database, option to show.
    * Many bugfixes and smaller improvements (see Changelog for details).
    
    Build-related:
    * Only x86-64 (64-bit) official builds for Windows and macOS and Linux.
    * Building for x86 (32-bit) still works on all platforms.
    * 32-bit packages for Ubuntu, Debian, Fedora and openSUSE are still available.
    * Builds for macOS require 10.9+. The source can be built for older versions.

--- emulators/fs-uae-arcade/patches/patch-setup.py
+++ emulators/fs-uae-arcade/patches/patch-setup.py
@@ -1,16 +1,16 @@
-$NetBSD: patch-setup.py,v 1.1 2018/02/08 20:58:50 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2019/08/22 18:35:22 adam Exp $
 
 Fix installation on Darwin.
 
 --- setup.py.orig	2015-10-27 20:54:58.000000000 +0000
 +++ setup.py
-@@ -172,10 +172,6 @@ if sys.argv[1] == "build_exe":
+@@ -178,10 +178,6 @@ if sys.argv[1] == "build_exe":
  if sys.platform == "win32" and False:
      setup_kwargs["windows"] = scripts
  
 -if sys.platform == "darwin":
 -    setup_kwargs["name"] = title
--    setup_kwargs["version"] = "2.8.3"
+-    setup_kwargs["version"] = "3.0.0"
 -else:
 -    setup_kwargs["scripts"] = scripts
 +setup_kwargs["scripts"] = scripts
