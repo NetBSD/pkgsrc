852d38cdafaf69940919f5513560e216bb5be3ca
tnn <tnn@pkgsrc.org>
2019-08-23 07:05:18 +0000

    MesaLib: fix wrong type passed to sysctl(3) arg2

--- graphics/MesaLib/patches/patch-src_mesa_drivers_dri_i915_intel__screen.c
+++ graphics/MesaLib/patches/patch-src_mesa_drivers_dri_i915_intel__screen.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_mesa_drivers_dri_i915_intel__screen.c,v 1.3 2019/08/21 13:35:28 nia Exp $
+$NetBSD: patch-src_mesa_drivers_dri_i915_intel__screen.c,v 1.4 2019/08/23 07:05:18 tnn Exp $
 
 Move sys/sysctl.h include because on NetBSD, sysctl.h includes sys/param.h,
 and sys/param.h defines a one argument ALIGN.  Allows mesa to redefine
@@ -6,7 +6,7 @@ to a two argument ALIGN.
 
 Fix build on older NetBSD w/o _SC_PHYS_PAGES.
 
---- src/mesa/drivers/dri/i915/intel_screen.c.orig	2017-11-10 23:18:56.000000000 +0000
+--- src/mesa/drivers/dri/i915/intel_screen.c.orig	2019-08-07 16:39:17.000000000 +0000
 +++ src/mesa/drivers/dri/i915/intel_screen.c
 @@ -28,6 +28,11 @@
  #include <errno.h>
@@ -20,7 +20,7 @@ Fix build on older NetBSD w/o _SC_PHYS_PAGES.
  #include "main/glheader.h"
  #include "main/context.h"
  #include "main/framebuffer.h"
-@@ -739,6 +744,13 @@ i915_query_renderer_integer(__DRIscreen
+@@ -733,6 +738,13 @@ i915_query_renderer_integer(__DRIscreen 
        const unsigned gpu_mappable_megabytes =
           (aper_size / (1024 * 1024)) * 3 / 4;
  
@@ -28,13 +28,13 @@ Fix build on older NetBSD w/o _SC_PHYS_PAGES.
 +      int mib[2] = { CTL_HW, HW_PHYSMEM64 };
 +      uint64_t system_memory_bytes;
 +      size_t len = sizeof(system_memory_bytes);
-+      if (sysctl(mib, 2, &system_memory_bytes, &len, NULL, 0) != 0)
++      if (sysctl(mib, 2, (void*)&system_memory_bytes, &len, NULL, 0) != 0)
 +         return -1;
 +#else
        const long system_memory_pages = sysconf(_SC_PHYS_PAGES);
        const long system_page_size = sysconf(_SC_PAGE_SIZE);
  
-@@ -747,6 +759,7 @@ i915_query_renderer_integer(__DRIscreen
+@@ -741,6 +753,7 @@ i915_query_renderer_integer(__DRIscreen 
  
        const uint64_t system_memory_bytes = (uint64_t) system_memory_pages
           * (uint64_t) system_page_size;
