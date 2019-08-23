0fe0195090adb1cdedb0d1a0a3ee2d8adc41c5b2
leot <leot@pkgsrc.org>
2019-08-23 18:24:39 +0000

    libnotify: Move the meson argument to disable doc to `doc' option
    Disabling the docbook_docs belong to `doc' option, move MESON_ARGS directly
    there. While here also make xmlto a tool dependency. (NFCI)

--- sysutils/libnotify/options.mk
+++ sysutils/libnotify/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2019/08/16 10:09:37 wiz Exp $
+# $NetBSD: options.mk,v 1.6 2019/08/23 18:24:39 leot Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libnotify
 PKG_SUPPORTED_OPTIONS=	doc
@@ -7,6 +7,9 @@ PLIST_VARS+=		doc
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdoc)
-BUILD_DEPENDS+=	xmlto-[0-9]*:../../textproc/xmlto
+TOOL_DEPENDS+=		xmlto-[0-9]*:../../textproc/xmlto
 PLIST.doc=		yes
+MESON_ARGS+=		-Ddocbook_docs=enabled
+.else
+MESON_ARGS+=		-Ddocbook_docs=disabled
 .endif
