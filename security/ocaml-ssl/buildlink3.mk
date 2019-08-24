02bed7fdc937bc78e35d4dbab59586e33e4f192d
jaapb <jaapb@pkgsrc.org>
2019-08-24 11:09:39 +0000

    Updated security/ocaml-ssl to version 0.5.9.
    This includes API changes to add support for hostname validation and
    APLN support, as well as some minor changes.

--- security/ocaml-ssl/buildlink3.mk
+++ security/ocaml-ssl/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.27 2019/03/05 17:44:50 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2019/08/24 11:09:39 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ssl
 
 .if !defined(OCAML_SSL_BUILDLINK3_MK)
 OCAML_SSL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.4.0
-BUILDLINK_ABI_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.5.7nb1
+BUILDLINK_API_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.5.8
+BUILDLINK_ABI_DEPENDS.ocaml-ssl+=	ocaml-ssl>=0.5.9
 BUILDLINK_PKGSRCDIR.ocaml-ssl?=		../../security/ocaml-ssl
 
 .include "../../lang/ocaml/buildlink3.mk"
