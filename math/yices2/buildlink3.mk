e624d5b83038859bd74d695eff75e562dacee4d0
alnsn <alnsn@pkgsrc.org>
2019-08-24 22:14:13 +0000

    Make pkglint happy.

--- math/yices2/buildlink3.mk
+++ math/yices2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2019/08/24 22:09:16 alnsn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/08/24 22:14:13 alnsn Exp $
 
 BUILDLINK_TREE+=	yices2
 
@@ -8,8 +8,8 @@ YICES2_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.yices2+=	yices2>=2.6.1
 BUILDLINK_ABI_DEPENDS.yices2+=	yices2>=2.6.1
 BUILDLINK_PKGSRCDIR.yices2?=	../../math/yices2
-.endif # YICES2_BUILDLINK3_MK
 
 .include "../../devel/gmp/buildlink3.mk"
+.endif # YICES2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-yices2
