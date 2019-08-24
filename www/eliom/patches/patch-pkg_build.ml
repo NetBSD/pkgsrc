d7f3a78f26439d40fa34437ef9253f054d11cfa6
jaapb <jaapb@pkgsrc.org>
2019-08-24 11:07:34 +0000

    Changes to www/eliom for ocaml 4.08
    This includes patches already upstream (but not yet released), as well as
    removing the dependency on ocaml-deriving-ocsigen.

--- www/eliom/patches/patch-pkg_build.ml
+++ www/eliom/patches/patch-pkg_build.ml
@@ -1,9 +1,23 @@
-$NetBSD: patch-pkg_build.ml,v 1.2 2016/02/06 14:50:22 jaapb Exp $
+$NetBSD: patch-pkg_build.ml,v 1.3 2019/08/24 11:07:34 jaapb Exp $
 
 Do not install mime types
---- pkg/build.ml.orig	2016-01-28 18:22:25.000000000 +0000
+Do not build syntax package (patch from upstream)
+--- pkg/build.ml.orig	2019-02-08 11:31:12.000000000 +0000
 +++ pkg/build.ml
-@@ -107,8 +107,7 @@ let () =
+@@ -75,12 +75,6 @@ let () =
+     Pkg.bin ~auto:true ~dst:"eliom-distillery" "src/tools/distillery";
+     Pkg.bin ~auto:true "src/ocamlbuild/eliombuild";
+ 
+-    (* SYNTAXES *)
+-    Pkg.lib ~exts:exts_syntax ~dst:"syntax/pa_eliom_seed" "src/syntax/pa_eliom_seed";
+-    Pkg.lib ~exts:exts_syntax ~dst:"syntax/pa_eliom_client_client" "src/syntax/pa_eliom_client_client";
+-    Pkg.lib ~exts:exts_syntax ~dst:"syntax/pa_eliom_client_server" "src/syntax/pa_eliom_client_server";
+-    Pkg.lib ~exts:exts_syntax ~dst:"syntax/pa_eliom_type_filter" "src/syntax/pa_eliom_type_filter";
+-
+     Pkg.lib ~exts:exts_modlib ~dst:"ocamlbuild/ocamlbuild_eliom" "src/ocamlbuild/ocamlbuild_eliom";
+ 
+     (* PPX *)
+@@ -109,8 +103,7 @@ let () =
      (* MISC *)
  
      Pkg.doc "README.md";
