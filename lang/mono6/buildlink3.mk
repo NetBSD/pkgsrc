b4fd3bab1c945eb5eaf19fd70b666f64b6486901
maya <maya@pkgsrc.org>
2019-08-25 16:37:01 +0000

    Add mono 6.0.0.313.
    The Mono Project is an open development initiative sponsored by Ximian
    that is working to develop an open source, Unix version of the Microsoft
    .NET development platform.  Its objective is to enable Unix developers to
    build and deploy cross-platform .NET Applications.  The project will
    implement various technologies developed by Microsoft that have now been
    submitted to the ECMA for standardization.

---
+++ lang/mono6/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/08/25 16:37:01 maya Exp $
+
+BUILDLINK_TREE+=	mono
+
+.if !defined(MONO_BUILDLINK3_MK)
+MONO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.mono+=	mono>=4
+BUILDLINK_ABI_DEPENDS.mono?=	mono>=4.0.1
+BUILDLINK_PKGSRCDIR.mono?=	../../lang/mono
+ALL_ENV+=			MONO_SHARED_DIR=${WRKDIR}
+BUILDLINK_CONTENTS_FILTER.mono=	${EGREP} '(^include/|^lib/)'
+
+.include "../../textproc/icu/buildlink3.mk"
+.endif # MONO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-mono
