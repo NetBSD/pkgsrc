030749ad5d586ca7345c52759df6f93bd5f8818e
leot <leot@pkgsrc.org>
2019-08-25 11:54:34 +0000

    libnotify: Readd `introspection' option and install gtk-doc for `doc' option
    - Readd introspection option (enabled by default) like it was present in
      previous libnotify version.
    - gtk-doc is probably a "bigger" tool dependency than xmlto, make it part of
      the `doc' option (this also make gtk-doc no longer a tool dependency by
      default).
    
    Bump PKGREVISION
    
    Discussed and thanks to <wiz>!

--- sysutils/libnotify/options.mk
+++ sysutils/libnotify/options.mk
@@ -1,15 +1,28 @@
-# $NetBSD: options.mk,v 1.6 2019/08/23 18:24:39 leot Exp $
+# $NetBSD: options.mk,v 1.7 2019/08/25 11:54:34 leot Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libnotify
-PKG_SUPPORTED_OPTIONS=	doc
-PLIST_VARS+=		doc
+PKG_SUPPORTED_OPTIONS=	doc introspection
+PKG_SUGGESTED_OPTIONS=	introspection
+PLIST_VARS+=		doc introspection
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdoc)
+TOOL_DEPENDS+=		gtk-doc-[0-9]*:../../textproc/gtk-doc
 TOOL_DEPENDS+=		xmlto-[0-9]*:../../textproc/xmlto
 PLIST.doc=		yes
 MESON_ARGS+=		-Ddocbook_docs=enabled
+MESON_ARGS+=		-Dgtk_doc=true
 .else
 MESON_ARGS+=		-Ddocbook_docs=disabled
+MESON_ARGS+=		-Dgtk_doc=false
+.endif
+
+.if !empty(PKG_OPTIONS:Mintrospection)
+BUILDLINK_DEPMETHOD.gobject-introspection=	build
+.include "../../devel/gobject-introspection/buildlink3.mk"
+PLIST.introspection=	yes
+MESON_ARGS+=		-Dintrospection=enabled
+.else
+MESON_ARGS+=		-Dintrospection=disabled
 .endif
