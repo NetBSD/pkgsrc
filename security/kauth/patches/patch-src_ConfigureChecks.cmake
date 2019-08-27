5cd1deb33ceb155dd7f605cb940acf6fcca4f12e
markd <markd@pkgsrc.org>
2019-08-27 20:11:28 +0000

    kauth: update to 5.61.0
    5.59.0
      Don't hardcode dbus policy install dir
    5.58.0
      Force KAuth helpers to have UTF-8 support

--- security/kauth/patches/patch-src_ConfigureChecks.cmake
+++ security/kauth/patches/patch-src_ConfigureChecks.cmake
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_ConfigureChecks.cmake,v 1.1 2016/05/26 09:09:24 markd Exp $
+$NetBSD: patch-src_ConfigureChecks.cmake,v 1.2 2019/08/27 20:11:28 markd Exp $
 
 Install in example dir
 
---- src/ConfigureChecks.cmake.orig	2016-02-06 13:36:29.000000000 +0000
+--- src/ConfigureChecks.cmake.orig	2019-08-03 19:51:11.000000000 +0000
 +++ src/ConfigureChecks.cmake
-@@ -222,7 +222,7 @@ if(KAUTH_HELPER_BACKEND_NAME STREQUAL "D
+@@ -224,7 +224,7 @@ if(KAUTH_HELPER_BACKEND_NAME STREQUAL "D
  
      # Install some files as well
      install( FILES backends/dbus/org.kde.kf5auth.conf
--             DESTINATION ${SYSCONF_INSTALL_DIR}/dbus-1/system.d )
+-             DESTINATION ${KDE_INSTALL_DBUSDIR}/system.d )
 +             DESTINATION ${KDE_INSTALL_DATAROOTDIR}/examples/kauth )
  
      install( FILES backends/dbus/dbus_policy.stub
