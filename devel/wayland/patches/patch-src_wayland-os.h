edce8c3c038e5716f1a9632fe3cca1fb2a31244e
nia <nia@pkgsrc.org>
2019-08-28 10:35:40 +0000

    wayland: declare wl_os_epoll_create_cloexec properly

--- devel/wayland/patches/patch-src_wayland-os.h
+++ devel/wayland/patches/patch-src_wayland-os.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_wayland-os.h,v 1.1 2019/08/18 16:05:12 nia Exp $
+$NetBSD: patch-src_wayland-os.h,v 1.2 2019/08/28 10:35:40 nia Exp $
 
 BSD support from FreeBSD
 
@@ -6,7 +6,7 @@ https://lists.freedesktop.org/archives/wayland-devel/2019-February/040024.html
 
 --- src/wayland-os.h.orig	2019-03-21 00:55:25.000000000 +0000
 +++ src/wayland-os.h
-@@ -26,17 +26,23 @@
+@@ -26,17 +26,29 @@
  #ifndef WAYLAND_OS_H
  #define WAYLAND_OS_H
  
@@ -24,10 +24,15 @@ https://lists.freedesktop.org/archives/wayland-devel/2019-February/040024.html
  ssize_t
  wl_os_recvmsg_cloexec(int sockfd, struct msghdr *msg, int flags);
  
-+
++#ifdef HAVE_SYS_EPOLL_H
  int
--wl_os_epoll_create_cloexec(void);
+ wl_os_epoll_create_cloexec(void);
++#endif
++
++#ifdef HAVE_SYS_EVENT_H
++int
 +wl_os_queue_create_cloexec(void);
++#endif
  
  int
  wl_os_accept_cloexec(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
