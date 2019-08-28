74d4ecd55c7d26916b3ba6bb309dbdf92d9942b9
schmonz <schmonz@pkgsrc.org>
2019-08-28 14:38:42 +0000

    Extend patch to a new function, re-fixing the Illumos build.

--- textproc/libhighlight/patches/patch-src_core_platform__fs.cpp
+++ textproc/libhighlight/patches/patch-src_core_platform__fs.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_core_platform__fs.cpp,v 1.2 2018/10/14 12:17:50 schmonz Exp $
+$NetBSD: patch-src_core_platform__fs.cpp,v 1.3 2019/08/28 14:38:42 schmonz Exp $
 
 Fix build on (at least) SmartOS.
 
---- src/core/platform_fs.cpp.orig	2018-10-13 19:26:45.000000000 +0000
+--- src/core/platform_fs.cpp.orig	2019-07-31 21:48:56.000000000 +0000
 +++ src/core/platform_fs.cpp
-@@ -79,13 +79,13 @@ int isColorEscCapable() {
+@@ -91,13 +91,13 @@ std::string getTempFilePath(){
  
  const char pathSeparator = '/';
  
@@ -20,3 +20,14 @@ Fix build on (at least) SmartOS.
  {
      struct passwd *pw = getpwuid(getuid());
      return string(pw->pw_dir);
+@@ -127,8 +127,8 @@ int isColorEscCapable() {
+     return 0;
+ }
+ 
+-std::string getTempFilePath(){
+-    std::string path("/tmp");
++::std::string getTempFilePath(){
++    ::std::string path("/tmp");
+     
+     char* tempOption=getenv("TEMP");
+     if (tempOption) path=string(tempOption);
