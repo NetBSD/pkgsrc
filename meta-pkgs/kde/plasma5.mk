42664e10a989851a2f7e808163a7e76877c2d5e6
markd <markd@pkgsrc.org>
2019-08-28 19:36:53 +0000

    kde: set default plasma version to 5.16.4

--- meta-pkgs/kde/plasma5.mk
+++ meta-pkgs/kde/plasma5.mk
@@ -1,6 +1,6 @@
-# $NetBSD: plasma5.mk,v 1.2 2018/04/13 22:02:39 markd Exp $
+# $NetBSD: plasma5.mk,v 1.3 2019/08/28 19:36:53 markd Exp $
 
-PLASMAVER?=	5.12.4
+PLASMAVER?=	5.16.4
 CATEGORIES+=	kde
 MASTER_SITES=	${MASTER_SITE_KDE:=plasma/${PLASMAVER}/}
 EXTRACT_SUFX=	.tar.xz
