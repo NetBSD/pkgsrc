f8d693e3e00d1cc5379f2ad8e1ba765e386b5aff
bouyer <bouyer@pkgsrc.org>
2019-08-29 15:36:39 +0000

    Update kicad packages to 5.1.4. Changes since 5.1.2:
    The KiCad project is proud to announce the latest series 5 stable release of
    KiCad. The 5.1.4 stable version contains critical bug fixes and other minor
    improvements since the 5.1.2 and 5.1.3 releases. It also includes improved
    footprint, symbol, and 3D model libraries, translations, and documentation.
    A list of all of the fixed bugs since the 5.1.2 and 5.1.3 releases can be
    found on the KiCad 5.1.3 milestone page and the KiCad 5.1.4 milestone page.
    This release contains several critical bug fixes so please consider upgrading
    as soon as possible.

--- cad/kicad/patches/patch-CMakeModules_Functions.cmake
+++ cad/kicad/patches/patch-CMakeModules_Functions.cmake
@@ -1,19 +1,25 @@
-$NetBSD: patch-CMakeModules_Functions.cmake,v 1.3 2016/09/08 09:32:28 bouyer Exp $
+$NetBSD: patch-CMakeModules_Functions.cmake,v 1.4 2019/08/29 15:36:40 bouyer Exp $
 
 Don't use ${CMAKE_MODULE_PATH} directly as it will be changed
 and containt multiple directories
 
---- CMakeModules/Functions.cmake.orig	2016-09-03 22:47:43.000000000 +0200
-+++ CMakeModules/Functions.cmake	2016-09-07 18:48:08.000000000 +0200
-@@ -36,9 +36,9 @@
+--- CMakeModules/Functions.cmake.orig	2019-08-04 18:36:25.000000000 +0200
++++ CMakeModules/Functions.cmake	2019-08-29 13:23:28.260311176 +0200
+@@ -40,7 +40,7 @@
              -DinputFile=${inputFile}
              -DoutHeaderFile=${outHeaderFile}
              -DoutCppFile=${outCppFile}
 -            -P ${CMAKE_MODULE_PATH}/TokenList2DsnLexer.cmake
 +            -P ${PROJECT_SOURCE_DIR}/CMakeModules/TokenList2DsnLexer.cmake
-         DEPENDS ${inputFile}
--                ${CMAKE_MODULE_PATH}/TokenList2DsnLexer.cmake
-+                ${PROJECT_SOURCE_DIR}/CMakeModules/TokenList2DsnLexer.cmake
          COMMENT "TokenList2DsnLexer.cmake creating:
             ${outHeaderFile} and
             ${outCppFile} from
+@@ -49,7 +49,7 @@
+ 
+     add_custom_target( ${outputTarget}
+         DEPENDS ${outHeaderFile} ${outCppFile}
+-                ${CMAKE_MODULE_PATH}/TokenList2DsnLexer.cmake
++                ${PROJECT_SOURCE_DIR}/CMakeModules/TokenList2DsnLexer.cmake
+         )
+ 
+     # extra_args, if any, are treated as source files (typically headers) which
