99d7d5492287ddc45961bd74a12c818e6efe6a77
nia <nia@pkgsrc.org>
2019-08-30 18:55:07 +0000

    lib3ds: Update buildlink3

--- graphics/lib3ds/buildlink3.mk
+++ graphics/lib3ds/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2015/04/25 14:23:15 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2019/08/30 18:55:07 nia Exp $
 
 BUILDLINK_TREE+=	lib3ds
 
@@ -9,7 +9,9 @@ BUILDLINK_API_DEPENDS.lib3ds+=	lib3ds>=1.2.0nb2
 BUILDLINK_ABI_DEPENDS.lib3ds+=	lib3ds>=1.2.0nb7
 BUILDLINK_PKGSRCDIR.lib3ds?=	../../graphics/lib3ds
 
-.include "../../graphics/Mesa/buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../graphics/freeglut/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
 .endif # LIB3DS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-lib3ds
