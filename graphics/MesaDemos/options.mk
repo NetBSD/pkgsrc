482a0448457215f350929e703ffd43a15f7a7979
nia <nia@pkgsrc.org>
2019-08-30 17:12:19 +0000

    MesaDemos: Update to 8.4.0nb2
    - Use freeglut instead of glut
    - Support wayland
    - Support EGL
    - Support GLESv2
    - Support OSMesa

--- graphics/MesaDemos/options.mk
+++ graphics/MesaDemos/options.mk
@@ -1,16 +1,33 @@
-# $NetBSD: options.mk,v 1.1 2019/01/04 01:43:20 tnn Exp $
+# $NetBSD: options.mk,v 1.2 2019/08/30 17:12:19 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.MesaDemos
-PKG_SUPPORTED_OPTIONS=		egl
-PKG_SUGGESTED_OPTIONS=
+PKG_SUPPORTED_OPTIONS=		libdrm wayland x11
+PKG_SUGGESTED_OPTIONS=		libdrm x11
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	egl
+PLIST_VARS+=	wayland x11
 
-.if !empty(PKG_OPTIONS:Megl)
-CONFIGURE_ARGS+=	--enable-egl
-PLIST.egl=		yes
+.if !empty(PKG_OPTIONS:Mlibdrm)
+CONFIGURE_ARGS+=	--enable-libdrm
+.include "../../x11/libdrm/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=	--disable-egl
+CONFIGURE_ARGS+=	--disable-libdrm
+.endif
+
+.if !empty(PKG_OPTIONS:Mwayland)
+PLIST.wayland=		yes
+CONFIGURE_ARGS+=	--enable-wayland
+.include "../../devel/wayland/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-wayland
+.endif
+
+.if !empty(PKG_OPTIONS:Mx11)
+PLIST.x11=		yes
+CONFIGURE_ARGS+=	--enable-x11
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-x11
 .endif
