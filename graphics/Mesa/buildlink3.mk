4bca8164803194d9072fc5a7e75339716fa32818
nia <nia@pkgsrc.org>
2019-08-30 19:06:34 +0000

    Remove graphics/Mesa
    This metapackage isn't particularly useful for modern applications and is
    potentially misleading.
    
    Not everything needs glu for OpenGL support, and glut has increasingly
    fell out of favour, to the extent of being dropped from Mesa. It's
    mostly been replaced by e.g. SDL.
    
    Hopefully I've narrowed down everything pulling in the metapackage
    and switched everything to using only the individual libraries it needs.

--- graphics/Mesa/buildlink3.mk
+++
@@ -1,7 +0,0 @@
-# $NetBSD: buildlink3.mk,v 1.6 2019/08/30 17:01:33 nia Exp $
-
-BUILDLINK_PREFIX.Mesa=	${BUILDLINK_PREFIX.MesaLib}
-
-.include "../../graphics/MesaLib/buildlink3.mk"
-.include "../../graphics/glu/buildlink3.mk"
-.include "../../graphics/freeglut/buildlink3.mk"
