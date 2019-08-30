af0e51caf0d2fc753eeac805a83b8e5e352756a8
nia <nia@pkgsrc.org>
2019-08-30 13:00:56 +0000

    MesaLib: various changes to how this is built
    - Try to appease the Darwin build.
    - Simplify and reduce the number of options to make testing easier.
    - Silence lots of pkglint warnings.
    - Enable the surfaceless platform as recommended by Mesa.
    - Switch to gallium swrast as recommended by Mesa.
    - Switch to gallium osmesa as recommended by Mesa.
    - Support building without X11.
    
    todo:
    - glvnd support (needs testing to figure out which platforms it builds on)
    - make AMD drivers optional on platforms like aarch64
    
    Bump PKGREVISION.

--- graphics/MesaLib/buildlink3.mk
+++ graphics/MesaLib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.58 2019/08/21 13:35:28 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.59 2019/08/30 13:00:56 nia Exp $
 
 BUILDLINK_TREE+=	MesaLib
 
@@ -11,38 +11,43 @@ BUILDLINK_PKGSRCDIR.MesaLib?=	../../graphics/MesaLib
 
 .include "../../mk/bsd.fast.prefs.mk"
 
-.if ${X11_TYPE} == "modular"
-BUILDLINK_ABI_DEPENDS.MesaLib+=	MesaLib>=10.5.3
-.  if defined(USE_LANGUAGES) && !empty(USE_LANGUAGES:Mc++)
-# This is needed to avoid linking conflicting libstdc++ versions
-GCC_REQD+=			4.2
-.  endif
-.endif
-
 # See <http://developer.apple.com/qa/qa2007/qa1567.html>.
 .if ${X11_TYPE} == "native" && !empty(MACHINE_PLATFORM:MDarwin-[9].*-*)
 BUILDLINK_LDFLAGS.MesaLib+=	-Wl,-dylib_file,/System/Library/Frameworks/OpenGL.framework/Versions/A/Libraries/libGL.dylib:/System/Library/Frameworks/OpenGL.framework/Versions/A/Libraries/libGL.dylib
 .endif
 
 pkgbase:= MesaLib
-.include "../../mk/pkg-build-options.mk"
 
-.if ${X11_TYPE} == "native" && ${OPSYS} != "Cygwin" && exists(${X11BASE}/lib/pkgconfig/dri.pc)
-PKG_BUILD_OPTIONS.MesaLib+=	dri
-.endif
-
-.if !empty(PKG_BUILD_OPTIONS.MesaLib:Mdri)
-.  include "../../graphics/MesaLib/dri.mk"
-.endif
-
-.if	${X11_TYPE} == "modular" && !empty(PKG_BUILD_OPTIONS.MesaLib:Mdri) ||	\
+.if	${X11_TYPE} == "modular" && (${OPSYS} != "Darwin" && ${OPSYS} != "Cygwin") ||	\
 	${X11_TYPE} == "native"  && exists(${X11BASE}/include/EGL/egl.h)
 MESALIB_SUPPORTS_EGL=	yes
 .else
 MESALIB_SUPPORTS_EGL=	no
 .endif
 
-.include "../../x11/libXext/buildlink3.mk"
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.MesaLib:Mwayland)
+.  include "../../devel/wayland/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.MesaLib:Mx11)
+.  include "../../x11/libX11/buildlink3.mk"
+.  include "../../x11/libXdamage/buildlink3.mk"
+.  include "../../x11/libXext/buildlink3.mk"
+.  include "../../x11/libXfixes/buildlink3.mk"
+.  include "../../x11/libXrandr/buildlink3.mk"
+.  include "../../x11/libXxf86vm/buildlink3.mk"
+.  include "../../x11/libxcb/buildlink3.mk"
+.  include "../../x11/libxshmfence/buildlink3.mk"
+.  include "../../x11/xcb-proto/buildlink3.mk"
+.  include "../../x11/xorgproto/buildlink3.mk"
+.endif
+
+.if ${OPSYS} != "Darwin" && ${OPSYS} != "Cygwin"
+.  include "../../x11/libdrm/buildlink3.mk"
+.endif
+
 .include "../../mk/pthread.buildlink3.mk"
 .endif # MESALIB_BUILDLINK3_MK
 
