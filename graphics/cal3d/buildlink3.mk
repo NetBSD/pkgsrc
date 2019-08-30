35052c6818f6c60c5a9f84912b3fe30ea2441229
nia <nia@pkgsrc.org>
2019-08-30 18:33:56 +0000

    cal3d: does not seem to actually need any OpenGL bits

--- graphics/cal3d/buildlink3.mk
+++ graphics/cal3d/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2015/04/25 14:23:07 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2019/08/30 18:34:30 nia Exp $
 
 BUILDLINK_TREE+=	cal3d
 
@@ -8,8 +8,6 @@ CAL3D_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.cal3d+=	cal3d>=0.9.1
 BUILDLINK_ABI_DEPENDS.cal3d+=	cal3d>=0.9.1nb3
 BUILDLINK_PKGSRCDIR.cal3d?=	../../graphics/cal3d
-
-.include "../../graphics/Mesa/buildlink3.mk"
 .endif # CAL3D_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-cal3d
