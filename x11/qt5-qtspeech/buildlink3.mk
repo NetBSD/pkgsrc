44ecbac790145a270330af3853645589cec3aa62
markd <markd@pkgsrc.org>
2019-08-30 06:30:00 +0000

    qt5-qtspeech: Add version 5.13.0
    Qt5 module to make text to speech and speech recognition easy

---
+++ x11/qt5-qtspeech/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/08/30 06:30:00 markd Exp $
+
+BUILDLINK_TREE+=	qt5-qtspeech
+
+.if !defined(QT5_QTSPEECH_BUILDLINK3_MK)
+QT5_QTSPEECH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt5-qtspeech+=	qt5-qtspeech>=5.13.0
+BUILDLINK_PKGSRCDIR.qt5-qtspeech?=	../../wip/qt5-qtspeech
+
+BUILDLINK_INCDIRS.qt5-qtspeech+=  qt5/include
+BUILDLINK_LIBDIRS.qt5-qtspeech+=  qt5/lib
+BUILDLINK_LIBDIRS.qt5-qtspeech+=  qt5/plugins
+
+.include "../../x11/qt5-qtmultimedia/buildlink3.mk"
+.endif	# QT5_QTSPEECH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt5-qtspeech
