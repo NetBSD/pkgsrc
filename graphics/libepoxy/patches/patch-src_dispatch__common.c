80c2a93ae2cc7ac8066cdc8acf0ac891af81a4e0
nia <nia@pkgsrc.org>
2019-08-31 13:50:09 +0000

    libepoxy: Update to 1.5.3
    Changes since 1.5.2:
    
        Disable EGL on macOS
        Fix build on macOS [#176, Emmanuele Bassi]
        Add macOS tests
        Fix build with uClibc [#181, Emmanuele Bassi]
        Fix GLES3 symbol lookup [Adam Jackson]
        Fix conditional compilation [#184, Zhao Zhili]
        Require Meson 0.47
        Fix Autotools build with EGL-X11 support [#188, Emmanuele Bassi]
    
    Changes since 1.5.1:
    
        Fix the detection of the -z,relro linker flag
        Query the EGL context version when bootstrapping on GLES [Adam Jackson]
        Avoid inadvertedly loading libraries when probing for them [Adam Jackson]
        Issue #169: Fix build on FreeBSD [Ting-Wei Lan]
        Consistently use abort() instead of exit() for internal state checks
        Issue #171: Fix a performance regression in the global function pointer
        trampolines introduced by using -Bsymbolic-functions
        Improve performance when using GL function pointers like glAlphaFunc [Adam Jackson]
    
    Changes since 1.5.0:
    
        Do no add pkg-config dependencies on gl on systems that do not use
        pkg-config, like macOS and Windows [Tom Schoonjans, #156]
        Generalise checks for dlvsym [Ross Burton, #158]
        Add an option for disabling building the test suite [Ross Burton]
        Typo fixes in the comments and documentation [luz.paz, #159]
        Simplify the Meson configuration logic for EGL and GLX [Eric
        Engestrom, #162]
        Use assert when no context is found [Adam Jackson, #166]
        Remove a test superceded by GLVND [#165]
        Avoid Meson warnings when testing for linker arguments
    
    
    Changes from Epoxy 1.4.3
    
        Bump the Meson dependency to 0.44.1
        Include Xlib.h in the tests that use X11 API
        Update the GL registry to OpenGL 4.6
        Add gl and egl private dependencies in the pkg-config file
        Allow building Epoxy without X11 support
        Rename the Meson configuration options to be more idiomatic
        New API:
            epoxy_set_resolver_failure_handler()
            epoxy_glsl_version()
            epoxy_extension_in_string()
    
    Issues fixed
    
        #128 - Fix macOS linker flags [Tom Schoonjans]
        #129 - Use GLVND if available [Adam Jackson]
        #134 - Add fallback definition for EGL_CAST [Daniel Stone]
        #133 - Try even harder to not load GLX [Adam Jackson]
        #138 - Fix the libOpenGL soname [Adam Jackson]
        #137 - Update differences with GLEW [Nigel Stewart]
        #131 - Add epoxy_set_resolver_failure_handler() [Adam Jackson]
        #140 - Fix pointer mismatch on Windows 10 [danem]
        #141 - Define visibility flags for static builds [Dylan Baker]
        #136 - Expose epoxy_extension_in_string() [Lyude Paul]
        #151 - Use correct guard for Android builds [Robert Bragg]
        #154 - Fix dlwrap for glvnd [Adam Jackson]
        #155 - Respect DLOPEN_LIBS [Michał Górny]
        #143 - Fix printf family usage [Ikey Doherty]
        #152 - Do not use OPENGL_LIB on Android
        #145 - Add epoxy_glsl_version()

--- graphics/libepoxy/patches/patch-src_dispatch__common.c
+++ graphics/libepoxy/patches/patch-src_dispatch__common.c
@@ -1,30 +1,35 @@
-$NetBSD: patch-src_dispatch__common.c,v 1.6 2019/05/27 17:04:04 nia Exp $
+$NetBSD: patch-src_dispatch__common.c,v 1.7 2019/08/31 13:50:09 nia Exp $
 
 Deal with hardcoded libGL locations and versions.
 
---- src/dispatch_common.c.orig	2017-06-06 09:55:43.000000000 +0000
+--- src/dispatch_common.c.orig	2018-10-04 23:40:48.000000000 +0000
 +++ src/dispatch_common.c
-@@ -174,11 +174,13 @@
+@@ -174,7 +174,9 @@
  #include "dispatch_common.h"
  
- #ifdef __APPLE__
+ #if defined(__APPLE__)
 +#ifndef GLX_LIB
  #define GLX_LIB "/opt/X11/lib/libGL.1.dylib"
 +#endif
- #elif defined(ANDROID)
- #define GLX_LIB "libGLESv2.so"
+ #define OPENGL_LIB "/System/Library/Frameworks/OpenGL.framework/Versions/Current/OpenGL"
+ #define GLES1_LIB "libGLESv1_CM.so"
+ #define GLES2_LIB "libGLESv2.so"
+@@ -189,12 +191,12 @@
+ #define GLES2_LIB "libGLESv2.dll"
+ #define OPENGL_LIB "OPENGL32"
  #else
+-#define GLVND_GLX_LIB "libGLX.so.1"
 -#define GLX_LIB "libGL.so.1"
+-#define EGL_LIB "libEGL.so.1"
+-#define GLES1_LIB "libGLESv1_CM.so.1"
+-#define GLES2_LIB "libGLESv2.so.2"
+-#define OPENGL_LIB "libOpenGL.so.0"
++#define GLVND_GLX_LIB "libGLX.so"
 +#define GLX_LIB "libGL.so"
++#define EGL_LIB "libEGL.so"
++#define GLES1_LIB "libGLESv1_CM.so"
++#define GLES2_LIB "libGLESv2.so"
++#define OPENGL_LIB "libOpenGL.so"
  #endif
  
- #ifdef ANDROID
-@@ -190,7 +192,7 @@
- #define GLES1_LIB "libGLES_CM.dll"
- #define GLES2_LIB "libGLESv2.dll"
- #else
--#define EGL_LIB "libEGL.so.1"
-+#define EGL_LIB "libEGL.so"
- #define GLES1_LIB "libGLESv1_CM.so.1"
- #define GLES2_LIB "libGLESv2.so.2"
- #endif
+ #ifdef __GNUC__
