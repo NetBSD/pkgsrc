8509d6f4c5f8d3d14d9b629135af1e45c453d5b0
taca <taca@pkgsrc.org>
2019-09-01 13:02:05 +0000

    lang/php72: update to 7.2.22
    29 Aug 2019, PHP 7.2.22
    
    - Core:
      . Fixed bug #78363 (Buffer overflow in zendparse). (Nikita)
      . Fixed bug #78379 (Cast to object confuses GC, causes crash). (Dmitry)
    
    - Curl:
      . Fixed bug #77946 (Bad cURL resources returned by curl_multi_info_read()).
        (Abyr Valg)
    
    - Exif:
      . Fixed bug #78333 (Exif crash (bus error) due to wrong alignment and
        invalid cast). (Nikita)
    
    - Iconv:
      . Fixed bug #78342 (Bus error in configure test for iconv //IGNORE). (Rainer
        Jung)
    
    - LiteSpeed:
      . Updated to LiteSpeed SAPI V7.5 (Fixed clean shutdown). (George Wang)
    
    - MySQLnd:
      . Fixed bug #78179 (MariaDB server version incorrectly detected). (cmb)
    
    - Opcache:
      . Fixed bug #77191 (Assertion failure in dce_live_ranges() when silencing is
        used). (Nikita)
    
    - Standard:
      . Fixed bug #69100 (Bus error from stream_copy_to_stream (file -> SSL stream)
        with invalid length). (Nikita)
      . Fixed bug #78282 (atime and mtime mismatch). (cmb)
      . Fixed bug #78326 (improper memory deallocation on stream_get_contents()
        with fixed length buffer). (Albert Casademont)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.265 2019/08/01 14:20:58 taca Exp $
+# $NetBSD: phpversion.mk,v 1.266 2019/09/01 13:02:05 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP71_VERSION=	7.1.31
-PHP72_VERSION=	7.2.21
+PHP72_VERSION=	7.2.22
 PHP73_VERSION=	7.3.8
 
 # Define initial release of major version.
