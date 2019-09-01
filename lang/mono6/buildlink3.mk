b563c1fef8721357fbaada3fcbc76c07d45b9fe7
nia <nia@pkgsrc.org>
2019-09-01 12:04:38 +0000

    mono6: Give up and add cmake to USE_TOOLS.
    While here, silcence some pkglint warnings.

--- lang/mono6/buildlink3.mk
+++ lang/mono6/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/08/25 16:37:01 maya Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/09/01 12:04:38 nia Exp $
 
 BUILDLINK_TREE+=	mono
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	mono
 MONO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mono+=	mono>=4
-BUILDLINK_ABI_DEPENDS.mono?=	mono>=4.0.1
+BUILDLINK_ABI_DEPENDS.mono+=	mono>=4.0.1
 BUILDLINK_PKGSRCDIR.mono?=	../../lang/mono
 ALL_ENV+=			MONO_SHARED_DIR=${WRKDIR}
 BUILDLINK_CONTENTS_FILTER.mono=	${EGREP} '(^include/|^lib/)'
