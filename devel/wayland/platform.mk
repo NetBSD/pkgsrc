d9fbae70c3e3f7fac12e03dfc8f71a05d4c94081
nia <nia@pkgsrc.org>
2019-09-01 09:35:05 +0000

    wayland: Add Linux as supported platform

--- devel/wayland/platform.mk
+++ devel/wayland/platform.mk
@@ -1,10 +1,10 @@
-# $NetBSD: platform.mk,v 1.1 2019/08/29 12:04:38 nia Exp $
+# $NetBSD: platform.mk,v 1.2 2019/09/01 09:35:05 nia Exp $
 
 .if !defined(PLATFORM_SUPPORTS_WAYLAND)
 
 # Please only add operating systems here after verifying that both
 # devel/wayland and devel/wayland-protocols build.
-.for _wayland_os in NetBSD
+.for _wayland_os in NetBSD Linux
 WAYLAND_PLATFORMS+=		${_wayland_os}-*-*
 .endfor
 
