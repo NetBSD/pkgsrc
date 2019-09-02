a49744986f802ad04d55d885bacf5d879b3b975b
nia <nia@pkgsrc.org>
2019-09-02 14:45:37 +0000

    fontforge: Update to 20190801
    Along with the usual bugfixes, there have been a couple of new features worth calling out:
    
        Added Croatian translation
        Added user decompositions
        New graphic for the splash/about screen
        Images embedded in SFDs are now serialised as PNGs
            This is enabled by default, but may be turned off with the 'WritePNGInSFD' option.
            A new tag is used to identify this mode; 'Image2', instead of 'Image'
            This requires FontForge to be compiled with libpng support. If not compiled with libpng, FontForge will revert to the old method of serialising RLE encoded raw images.
    
    As part of an ongoing effort to clean up the code base, there have additionally been multiple build system changes:
    
        Python 2 support is deprecated. It is strongly recommended to build with Python 3 support. Python 2 support will be removed in a future release.
        Both the Windows and Mac builds are now built with Python 3 instead of Python 2.
        The minimum supported version for the Mac build is now MacOS Sierra (10.12)
        FontForge no longer uses gnulib
        collab support has been removed
        The build system now expects libuninameslist to be present, and will fail if it is not found. Building without libuninameslist must be explcititly specified using --without-libuninameslist

--- fonts/fontforge/patches/patch-fontforge_splinefont.h
+++ fonts/fontforge/patches/patch-fontforge_splinefont.h
@@ -1,28 +1,41 @@
-$NetBSD: patch-fontforge_splinefont.h,v 1.2 2016/09/27 10:24:57 taca Exp $
+$NetBSD: patch-fontforge_splinefont.h,v 1.3 2019/09/02 14:45:37 nia Exp $
 
-* NetBSD does not implement uselocale().
-* NetBSD prior to 7.0 dose not have locale_t.
+NetBSD does not implement uselocale().
 
---- fontforge/splinefont.h.orig	2016-04-05 01:29:03.000000000 +0000
+--- fontforge/splinefont.h.orig	2019-07-22 10:10:01.551001000 +0000
 +++ fontforge/splinefont.h
-@@ -3549,6 +3549,20 @@ char * delimit_null(const char * input, 
+@@ -2625,17 +2625,33 @@ extern bool isSplinePointPartOfGuide( Sp
  
- #include "ustring.h"
+ extern void debug_printHint( StemInfo *h, char* msg );
  
-+#ifdef __NetBSD__
-+#define BAD_LOCALE_HACK
-+#ifndef __LOCALE_T_DECLARED
-+typedef char* locale_t;
-+#define LC_GLOBAL_LOCALE ((locale_t)-1)
-+#define LC_ALL_MASK LC_ALL
-+#define LC_COLLATE_MASK LC_COLLATE
-+#define LC_CTYPE_MASK LC_CTYPE
-+#define LC_MONETARY_MASK LC_MONETARY
-+#define LC_NUMERIC_MASK LC_NUMERIC
-+#define LC_TIME_MASK LC_TIME
-+#endif
-+#endif
-+
- #ifdef _WIN32
+-#if defined(_WIN32) || defined(__HAIKU__)
++#if defined(_WIN32) || defined(__HAIKU__) || defined(__NetBSD__)
  #define BAD_LOCALE_HACK
++#ifndef __NetBSD__
  typedef char* locale_t;
++#endif
++#ifndef LC_GLOBAL_LOCALE
+ #define LC_GLOBAL_LOCALE ((locale_t)-1)
++#endif
++#ifndef LC_ALL_MASK
+ #define LC_ALL_MASK LC_ALL
++#endif
++#ifndef LC_COLLATE_MASK
+ #define LC_COLLATE_MASK LC_COLLATE
++#endif
++#ifndef LC_CTYPE_MASK
+ #define LC_CTYPE_MASK LC_CTYPE
++#endif
++#ifndef LC_MONETARY_MASK
+ #define LC_MONETARY_MASK LC_MONETARY
++#endif
++#ifndef LC_NUMERIC_MASK
+ #define LC_NUMERIC_MASK LC_NUMERIC
++#endif
++#ifndef LC_TIME_MASK
+ #define LC_TIME_MASK LC_TIME
+ #endif
++#endif
+ 
+ static inline void switch_to_c_locale(locale_t * tmplocale_p, locale_t * oldlocale_p) {
+ #ifndef BAD_LOCALE_HACK
