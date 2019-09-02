435af01a8b259b6d0f7b30a1ac3a49eabe39d32f
adam <adam@pkgsrc.org>
2019-09-02 13:19:35 +0000

    Changed PYTHON_VERSIONS_INCOMPATIBLE to PYTHON_VERSIONS_ACCEPTED; needed for future Python 3.8

--- net/Radicale/options.mk
+++ net/Radicale/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.8 2019/04/26 13:14:06 maya Exp $
+# $NetBSD: options.mk,v 1.9 2019/09/02 13:19:49 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.Radicale
 PKG_SUPPORTED_OPTIONS=	git ldap
@@ -10,6 +10,6 @@ DEPENDS+=	${PYPKGPREFIX}-dulwich-[0-9]*:../../devel/py-dulwich
 .endif
 
 .if !empty(PKG_OPTIONS:Mldap)
-PYTHON_VERSIONS_INCOMPATIBLE=	 36 37 # py-ldap
+PYTHON_VERSIONS_ACCEPTED=	 27 # py-ldap
 DEPENDS+=	${PYPKGPREFIX}-ldap-[0-9]*:../../databases/py-ldap
 .endif
--- www/py-django14/options.mk
+++ www/py-django14/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.9 2019/04/26 13:14:20 maya Exp $
+# $NetBSD: options.mk,v 1.10 2019/09/02 13:20:04 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.django
 PKG_SUPPORTED_OPTIONS=	mysql oracle pgsql sqlite
@@ -10,7 +10,7 @@ PLIST_VARS+=	${PKG_SUPPORTED_OPTIONS}
 
 .if !empty(PKG_OPTIONS:Mmysql)
 DEPENDS+=	${PYPKGPREFIX}-mysqldb-[0-9]*:../../databases/py-mysqldb
-PYTHON_VERSIONS_INCOMPATIBLE=	 36 37 # py-mysqldb
+PYTHON_VERSIONS_ACCEPTED=	 27 # py-mysqldb
 PLIST.mysql=	yes
 .endif
 
@@ -26,6 +26,6 @@ PLIST.pgsql=	yes
 
 .if !empty(PKG_OPTIONS:Msqlite)
 DEPENDS+=	${PYPKGPREFIX}-sqlite2-[0-9]*:../../databases/py-sqlite2
-PYTHON_VERSIONS_INCOMPATIBLE=	 36 37 # py-sqlite2
+PYTHON_VERSIONS_ACCEPTED=	 27 # py-sqlite2
 PLIST.sqlite=	yes
 .endif
