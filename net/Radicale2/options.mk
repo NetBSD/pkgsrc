8c2f2605fbea1dcc7feb331414fd3cc24e3df9b1
adam <adam@pkgsrc.org>
2019-09-02 13:19:35 +0000

    Changed PYTHON_VERSIONS_INCOMPATIBLE to PYTHON_VERSIONS_ACCEPTED; needed for future Python 3.8

--- net/Radicale2/options.mk
+++ net/Radicale2/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2019/04/26 13:14:07 maya Exp $
+# $NetBSD: options.mk,v 1.5 2019/09/02 13:19:49 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.Radicale
 
@@ -8,6 +8,6 @@ PKG_OPTIONS_VAR=	PKG_OPTIONS.Radicale
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mldap)
-PYTHON_VERSIONS_INCOMPATIBLE=	 36 37 # py-ldap
+PYTHON_VERSIONS_ACCEPTED=	 27 # py-ldap
 DEPENDS+=	${PYPKGPREFIX}-ldap-[0-9]*:../../databases/py-ldap
 .endif
