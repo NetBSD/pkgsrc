f8ee0771d757394038d78ceda2261befdaa86fe9
nia <nia@pkgsrc.org>
2019-09-04 16:38:06 +0000

    libepoxy: add mesa to buildlink3

--- graphics/libepoxy/buildlink3.mk
+++ graphics/libepoxy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/03/18 20:18:39 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/09/04 16:38:06 nia Exp $
 
 BUILDLINK_TREE+=	libepoxy
 
@@ -8,6 +8,7 @@ LIBEPOXY_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libepoxy+=	libepoxy>=1.2
 BUILDLINK_PKGSRCDIR.libepoxy?=		../../graphics/libepoxy
 
+.include "../../graphics/MesaLib/buildlink3.mk"
 .endif	# LIBEPOXY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libepoxy
