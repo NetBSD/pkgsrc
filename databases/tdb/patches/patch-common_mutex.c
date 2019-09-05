63bd2b5dfc0f14e0602862b59e11f52c82cbe5ba
hauke <hauke@pkgsrc.org>
2019-09-05 12:39:56 +0000

    Solarish pthreads expect mutex datastructures to be properly zeroed out, unlike the Linux implementation, which upstream mainly targets. This leads to "tdb_open_ex: tdb_mutex_init failed for /var/samba/lock/gencache.tdb: Device busy" errors, described in <https://illumos.topicbox.com/groups/developer/T28dcabf2f69cac04>.
    Illumos kernel code attempted to mitigate the problem as of
    <https://www.illumos.org/issues/9959>, but the changes have yet to
    show up in releases.
    
    The patches originate from
    <https://github.com/oposs/pkg.oetiker.ch-build/blob/master/build/samba4/patches/pthread_mutex_init>,
    modified appropriately.
    
    Tested on omniosce r151030.

---
+++ databases/tdb/patches/patch-common_mutex.c
@@ -0,0 +1,63 @@
+$NetBSD: patch-common_mutex.c,v 1.1 2019/09/05 12:39:56 hauke Exp $
+
+On SunOS (OpenSolaris), pthread_mutex_init() expects a zeroed-out
+mutex data structure
+
+--- common/mutex.c.orig	2017-04-28 08:57:26.000000000 +0000
++++ common/mutex.c
+@@ -567,6 +567,7 @@ int tdb_mutex_init(struct tdb_context *t
+ 	}
+ 	m = tdb->mutexes;
+ 
++        memset(&ma, 0, sizeof(pthread_mutexattr_t));
+ 	ret = pthread_mutexattr_init(&ma);
+ 	if (ret != 0) {
+ 		goto fail_munmap;
+@@ -587,6 +588,7 @@ int tdb_mutex_init(struct tdb_context *t
+ 	for (i=0; i<tdb->hash_size+1; i++) {
+ 		pthread_mutex_t *chain = &m->hashchains[i];
+ 
++                memset(chain, 0, sizeof(pthread_mutex_t));
+ 		ret = pthread_mutex_init(chain, &ma);
+ 		if (ret != 0) {
+ 			goto fail;
+@@ -595,6 +597,7 @@ int tdb_mutex_init(struct tdb_context *t
+ 
+ 	m->allrecord_lock = F_UNLCK;
+ 
++        memset(&m->allrecord_mutex, 0, sizeof(pthread_mutex_t));
+ 	ret = pthread_mutex_init(&m->allrecord_mutex, &ma);
+ 	if (ret != 0) {
+ 		goto fail;
+@@ -672,6 +675,7 @@ static bool tdb_mutex_locking_supported(
+ 
+ 	initialized = true;
+ 
++	memset(&ma, 0, sizeof(pthread_mutexattr_t));
+ 	ret = pthread_mutexattr_init(&ma);
+ 	if (ret != 0) {
+ 		return false;
+@@ -688,6 +692,7 @@ static bool tdb_mutex_locking_supported(
+ 	if (ret != 0) {
+ 		goto cleanup_ma;
+ 	}
++        memset(&m, 0, sizeof(pthread_mutex_t));
+ 	ret = pthread_mutex_init(&m, &ma);
+ 	if (ret != 0) {
+ 		goto cleanup_ma;
+@@ -868,6 +873,7 @@ _PUBLIC_ bool tdb_runtime_check_for_robu
+ 		goto cleanup;
+ 	}
+ 
++        memset(&ma, 0, sizeof(pthread_mutexattr_t));
+ 	ret = pthread_mutexattr_init(&ma);
+ 	if (ret != 0) {
+ 		goto cleanup;
+@@ -885,6 +891,7 @@ _PUBLIC_ bool tdb_runtime_check_for_robu
+ 	if (ret != 0) {
+ 		goto cleanup;
+ 	}
++        memset(ptr, 0, sizeof(pthread_mutex_t));
+ 	ret = pthread_mutex_init(ptr, &ma);
+ 	if (ret != 0) {
+ 		goto cleanup;
