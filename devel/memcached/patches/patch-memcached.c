119e1bf107c307a6f8c92d852c3092af2a268bca
adam <adam@pkgsrc.org>
2019-09-05 09:34:51 +0000

    memcached: updated to 1.5.17
    1.5.17:
    Fixes
    fix strncpy call in stats conns to avoid ASAN violation
    extstore: fix indentation
    add error handling when calling dup function
    add unlock when item_cachedump malloc failed
    extstore: emulate pread(v) for macOS
    fix off-by-one in logger to allow CAS commands to be logged.
    use strdup for explicitly configured slab sizes
    move mem_requested from slabs.c to items.c (internal cleanup)
    
    New Features
    add server address to the "stats conns" output
    log client connection id with fetchers and mutations
    Add a handler for seccomp crashes

--- devel/memcached/patches/patch-memcached.c
+++ devel/memcached/patches/patch-memcached.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-memcached.c,v 1.2 2015/05/21 22:21:08 rodent Exp $
+$NetBSD: patch-memcached.c,v 1.3 2019/09/05 09:34:51 adam Exp $
 
-Fix this the right way thanks to joerg@.
+time_t changes
 
---- memcached.c.orig	2015-01-01 07:50:52.000000000 +0000
+--- memcached.c.orig	2019-08-29 22:13:13.000000000 +0000
 +++ memcached.c
-@@ -2764,8 +2764,8 @@ static void process_stats_conns(ADD_STAT
-                 APPEND_NUM_STAT(i, "addr", "%s", conn_name);
+@@ -3593,8 +3593,8 @@ static void process_stats_conns(ADD_STAT
+                 }
                  APPEND_NUM_STAT(i, "state", "%s",
                          state_text(conns[i]->state));
 -                APPEND_NUM_STAT(i, "secs_since_last_cmd", "%d",
