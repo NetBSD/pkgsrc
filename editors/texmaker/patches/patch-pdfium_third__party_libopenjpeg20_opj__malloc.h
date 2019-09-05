91a5f2f5ccd935ff65b67d68bedba907b245ca82
nros <nros@pkgsrc.org>
2019-09-05 08:07:25 +0000

    Update texmaker to version 5.0.3 .
    Removed poppler-qt5 dependecy since it uses pdfium instead.
    
    version 5.0.2 -> 5.0.3 :
    - the bug about the bad highlighting on misspelled text before a \cite{} or
      \label{} command has been fixed
    - if no text is selected the "(un)comment" command will (un)comment the
      current line
    - the default size of the configuration dialog has been reduced (users request)
    - Fold/Unfold/Fold All/Unfold All menu options has been added
      (thanks to Robin Watts)
    - a bug about completion when citing references that contain numbers has
      been fixed
    - a trouble in the "recent files" menu with names including an ampersand
      has been fixed
    - a typo in the documentation has been fixed
    - a typo in the de translation has been fixed (thanks to Stéphane YVON)
    version 5.0.1 -> 5.0.2 :
    - a memory leak has been fixed in the internal pdf viewer
    - the trouble about the "open terminal" command on windows has been fixed
    version 5.0 -> 5.0.1 :
    - a trouble with the MikTex's ps2pdf command has been fixed
    - the "-dpiscale x" option has been added to the Texmaker command line : with
      this option the environment variable QT_SCALE_FACTOR will be set to x before
      building the GUI (same behavior than adding "export QT_SCALE_FACTOR=x" to
      the environment variables). Can be used to adjust the interface if the
      monitor dpi is not well detected.
    - the cs translation has been updated
    version 4.5 -> 5.0 :
    - new and more modern interface look
    - highdpi is now supported on windows and linux
    - a spellcheck annoying trouble on kde5 has been fixed
    - new pdf engine
    - synctex updated to 1.18
    - Qt4 support has been removed : Qt5 (>=5.7) is now required
    - de, es, hu, pl, and zh_CN translations have been updated
    note : Texmaker depends now only from the Qt toolkit to be compiled

---
+++ editors/texmaker/patches/patch-pdfium_third__party_libopenjpeg20_opj__malloc.h
@@ -0,0 +1,13 @@
+$NetBSD: patch-pdfium_third__party_libopenjpeg20_opj__malloc.h,v 1.1 2019/09/05 08:07:25 nros Exp $
+* use posix memalign on BSD systems
+--- pdfium/third_party/libopenjpeg20/opj_malloc.h.orig	2019-05-04 19:18:14.758209492 +0000
++++ pdfium/third_party/libopenjpeg20/opj_malloc.h
+@@ -98,7 +98,7 @@ Allocate memory aligned to a 16 byte bou
+ #else /* Not _WIN32 */
+ 	#if defined(__sun)
+ 		#define HAVE_MEMALIGN
+-  #elif defined(__FreeBSD__)
++  #elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(DragonFly)
+     #define HAVE_POSIX_MEMALIGN
+ 	/* Linux x86_64 and OSX always align allocations to 16 bytes */
+ 	#elif !defined(__amd64__) && !defined(__APPLE__) && !defined(_AIX)
