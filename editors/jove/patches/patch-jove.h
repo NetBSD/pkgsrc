48224bc1e78913e0cac6dc0835a8285a4819f93a
christos <christos@pkgsrc.org>
2019-09-06 15:53:42 +0000

    simplify...

--- editors/jove/patches/patch-jove.h
+++ editors/jove/patches/patch-jove.h
@@ -1,14 +1,16 @@
-$NetBSD: patch-jove.h,v 1.1 2019/09/02 08:53:32 christos Exp $
+$NetBSD: patch-jove.h,v 1.2 2019/09/06 15:53:42 christos Exp $
 
 - modern os's need time.h for time(3)
 
 --- jove.h.orig	2015-10-15 14:21:55.000000000 -0400
-+++ jove.h	2019-09-02 04:39:50.417825060 -0400
-@@ -16,6 +16,7 @@
- 
- #ifndef MAC
++++ jove.h	2019-09-06 11:49:09.552268238 -0400
+@@ -18,8 +18,8 @@
  # include <sys/types.h>
-+# include <time.h>
  #else
  # include <types.h>
- # include <time.h>	/* for time_t */
+-# include <time.h>	/* for time_t */
+ #endif
++#include <time.h>	/* for time_t */
+ 
+ /* proto: macro to allow us to prototype any function declaration
+  * without upsetting old compilers.
