aab621414188ae406339aef3296388fdc0f39e36
tsutsui <tsutsui@pkgsrc.org>
2019-09-07 00:36:34 +0000

    emulators/xm8: import xm8-1.70.
    XM8 is "eXcellent Multi-platform emulator type 8", an emulator of
    NEC PC-8801 series, based on "ePC-8801MA".

---
+++ emulators/xm8/patches/patch-Source_UI_emu.cpp
@@ -0,0 +1,14 @@
+$NetBSD: patch-Source_UI_emu.cpp,v 1.1 2019/09/07 00:36:34 tsutsui Exp $
+
+- change rom file directory to ~/.local/share/retro_pc_pi/xm8/rom
+
+--- Source/UI/emu.cpp.orig	2015-04-20 05:10:30.000000000 +0000
++++ Source/UI/emu.cpp
+@@ -8,6 +8,7 @@
+ */
+ 
+ #include "common.h"
++#include "classes.h"
+ #include "emu.h"
+ #include "emu_sdl.h"
+ 
---
+++ emulators/xm8/patches/patch-Source_UI_emu__sdl.h
@@ -0,0 +1,24 @@
+$NetBSD: patch-Source_UI_emu__sdl.h,v 1.1 2019/09/07 00:36:34 tsutsui Exp $
+
+- change rom file directory to ~/.local/share/retro_pc_pi/xm8/rom
+
+--- Source/UI/emu_sdl.h.orig	2015-05-15 21:05:04.000000000 +0000
++++ Source/UI/emu_sdl.h
+@@ -20,7 +20,7 @@ class Video;
+ class EMU_SDL
+ {
+ public:
+-	EMU_SDL(Video *v);
++	EMU_SDL(App *a, Video *v);
+ 										// constructor
+ 	virtual ~EMU_SDL();
+ 										// destructor
+@@ -47,6 +47,8 @@ private:
+ 										// full path buffer for get_bios_path()
+ 	Video *video;
+ 										// video driver
++	Setting *setting;
++										// setting driver
+ };
+ 
+ #endif // EMU_SDL_H
