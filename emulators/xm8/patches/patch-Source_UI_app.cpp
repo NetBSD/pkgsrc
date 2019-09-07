b4eaa297c1607010b99ac4b024e7b7745721d56e
tsutsui <tsutsui@pkgsrc.org>
2019-09-07 00:36:34 +0000

    emulators/xm8: import xm8-1.70.
    XM8 is "eXcellent Multi-platform emulator type 8", an emulator of
    NEC PC-8801 series, based on "ePC-8801MA".

---
+++ emulators/xm8/patches/patch-Source_UI_app.cpp
@@ -0,0 +1,15 @@
+$NetBSD: patch-Source_UI_app.cpp,v 1.1 2019/09/07 00:36:34 tsutsui Exp $
+
+- change rom file directory to ~/.local/share/retro_pc_pi/xm8/rom
+
+--- Source/UI/app.cpp.orig	2018-01-20 05:37:58.000000000 +0000
++++ Source/UI/app.cpp
+@@ -251,7 +251,7 @@ bool App::Init()
+ 	}
+ 
+ 	// emulator i/f wrapper
+-	wrapper = new EMU_SDL(video);
++	wrapper = new EMU_SDL(this, video);
+ 
+ 	// emulator i/f
+ 	emu = new EMU(wrapper);
