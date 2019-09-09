2c5056658b787f2d38508878f14f79dcdc01bbbb
nia <nia@pkgsrc.org>
2019-09-09 10:08:54 +0000

    libglvnd: Don't use --version-script on SunOS
    Adjust whitespace

--- graphics/libglvnd/buildlink3.mk
+++ graphics/libglvnd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/08/30 14:39:53 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/09/09 10:08:54 nia Exp $
 
 BUILDLINK_TREE+=	libglvnd
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libglvnd
 LIBGLVND_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libglvnd+=	libglvnd>=1.1.1
-BUILDLINK_PKGSRCDIR.libglvnd?=	../../graphics/libglvnd
+BUILDLINK_PKGSRCDIR.libglvnd?=		../../graphics/libglvnd
 .endif	# LIBGLVND_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libglvnd
