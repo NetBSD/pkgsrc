583e7a8acbb9e5e17886bb50d44e54637aaa0680
wiz <wiz@pkgsrc.org>
2019-09-09 12:40:50 +0000

    freetype2: remove ABI depends that's lower than API depends
    It will have no effect.

--- graphics/freetype2/buildlink3.mk
+++ graphics/freetype2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.48 2019/09/09 12:26:04 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.49 2019/09/09 12:40:50 wiz Exp $
 
 BUILDLINK_TREE+=	freetype2
 
@@ -6,7 +6,6 @@ BUILDLINK_TREE+=	freetype2
 FREETYPE2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.freetype2+=	freetype2>=2.8.1
-BUILDLINK_ABI_DEPENDS.freetype2+=	freetype2>=2.4.11
 BUILDLINK_PKGSRCDIR.freetype2?=		../../graphics/freetype2
 BUILDLINK_INCDIRS.freetype2?=		include/freetype2
 
