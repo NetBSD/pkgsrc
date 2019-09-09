68cdce420a1af8d49f4fa09d96da32b722987bca
nia <nia@pkgsrc.org>
2019-09-09 12:59:06 +0000

    libappindicator: Make this the gtk3 version.
    Nothing is using the gtk2 version.
    
    Bump PKGREVISIONs.

--- x11/mate-control-center/options.mk
+++ x11/mate-control-center/options.mk
@@ -1,13 +1,15 @@
-# $NetBSD: options.mk,v 1.1 2019/09/07 04:02:58 gutteridge Exp $
+# $NetBSD: options.mk,v 1.2 2019/09/09 12:59:06 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mate-control-center
-PKG_SUPPORTED_OPTIONS=	libappindicator3
-PKG_SUGGESTED_OPTIONS=	libappindicator3
+PKG_SUPPORTED_OPTIONS=	libappindicator
+PKG_SUGGESTED_OPTIONS=	libappindicator
+
+PKG_OPTIONS_LEGACY_OPTS+=	libappindicator3:libappindicator
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mlibappindicator3)
-.include "../../devel/libappindicator3/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mlibappindicator)
+.include "../../devel/libappindicator/buildlink3.mk"
 # configure automatically enables this option if it finds it in the
 # environment. There are no additional files generated.
 .endif
