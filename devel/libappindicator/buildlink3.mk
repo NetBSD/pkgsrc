1ef1796be9194055a97d4cb9968979b8bceb6132
nia <nia@pkgsrc.org>
2019-09-09 13:25:46 +0000

    libindicator: Make this the GTK3 version.
    Nothing is using the GTK2 version.
    
    Bump PKGREVISIONs

--- devel/libappindicator/buildlink3.mk
+++ devel/libappindicator/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2019/09/09 12:59:06 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2019/09/09 13:25:47 nia Exp $
 
 BUILDLINK_TREE+=	libappindicator
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.libappindicator+=	libappindicator>=12.10.0
 BUILDLINK_ABI_DEPENDS.libappindicator+=	libappindicator>=12.10.0nb1
 BUILDLINK_PKGSRCDIR.libappindicator?=	../../devel/libappindicator
 
-.include "../../devel/libindicator3/buildlink3.mk"
+.include "../../devel/libindicator/buildlink3.mk"
 .include "../../devel/libdbusmenu-gtk3/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
 .endif	# LIBAPPINDICATOR_BUILDLINK3_MK
