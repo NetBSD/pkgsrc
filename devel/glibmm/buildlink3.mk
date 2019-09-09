c743b06ad0435c0bfcdafe5d19d2dedddfc78504
maya <maya@pkgsrc.org>
2019-09-09 11:40:16 +0000

    glibmm: sync GCC_REQD between bl3, Makefile.
    Fixes atkmm builds with GCC < 4.9

--- devel/glibmm/buildlink3.mk
+++ devel/glibmm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2017/12/30 17:12:51 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2019/09/09 11:40:16 maya Exp $
 
 BUILDLINK_TREE+=	glibmm
 
@@ -8,6 +8,8 @@ GLIBMM_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.glibmm+=	glibmm>=2.24.2
 BUILDLINK_ABI_DEPENDS.glibmm+=	glibmm>=2.32.1nb1
 BUILDLINK_PKGSRCDIR.glibmm?=	../../devel/glibmm
+
+GCC_REQD+=	4.9
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/libsigc++/buildlink3.mk"
