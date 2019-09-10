ce73546b3a02a1f75b285c230c3c60787df1f7ef
leot <leot@pkgsrc.org>
2019-09-10 22:10:04 +0000

    webkit-gtk: Update to 2.26.0
    pkgsrc changes:
     - When built with gcc, 7.3.0 or newer is now needed
    
    Changes:
    WebKitGTK 2.26.0
    ================
     - Disable process swap on navigation.
     - Do not enable the sandbox in Snap.
     - Fix sandbox parsing DISPLAY on X11.
     - Translation updates: Brazilian Portuguese, German.
    
    WebKitGTK 2.25.92
    =================
     - Add WEBKIT_USE_SINGLE_WEB_PROCESS environment variable to force single
       process model in all WebKitWebContext.
       This is a temporary solution for applications still depending on the
       single process mode behavior. It will be only available in 2.26 series.
     - Add new API to remove a filter from an user content manager given its
       identifier.
     - Add support for HSTS.
     - Several improvements and bug fixes in MSE media player.
     - Fix building without unified sources.
     - Fix several crashes and rendering issues.
     - Translation updates: Polish, Ukrainian.
    
    WebKitGTK 2.25.4
    ================
     - Switch to use libsoup WebSockets API.
     - Add support for permessage-deflate WebSocket extension.
     - Add support for datalist element in text input fields.
     - Fix a crash with empty video source.
     - Fix several crashes and rendering issues.
    
    WebKitGTK 2.25.3
    ================
     - Remove support for GTK2 NPAPI plugins.
     - Fix web view updates after swapping web process if accelerated
       compositing mode is forced.
     - Make kinetic scrolling work again.
     - Fix position of emoji chooser when page is scrolled.
     - Fix web process deadlock when scrolling twitter timeline which contains
       HLS videos.
     - Make navigation gesture use dark fallback background color color on dark
       themes.
     - Make Previous/Next gesture work in RTL mode.
     - Support cancelling touchscreen back/forward gesture.
     - Add user agent quirk to make github work in FreeBSD.
     - Fix content disappearing when using CSS transforms.
     - Fix some radio streams that could not be played.
     - Fix video pause that sometimes caused to skip to finish.
     - Fix volume level changes when playing a video.
     - Fix several crashes and rendering issues.
    
    WebKitGTK 2.25.2
    ================
     - Enable process switch on cross site navigation.
     - Use libwpe with fdo backend to implement accelerated compositing under
       wayland.
     - Fix rendering artifacts in youtube volume button.
     - Fix trapezoid artifact in github comment box.
     - Ensure web extensions directory is readable when sandbox is enabled.
     - Fix the executable name of WebDriver process, renamed by mistake in
       2.25.1.
     - Enable hyperlink auditing setting by default.
     - Remove the option to build without using the redirected XComposite window.
     - Fix HLS streams being slow to start.
     - Make accessibility work when sandbox is enabled.
     - Fix several crashes and rendering issues.
    
    WebKitGTK 2.25.1
    ================
     - Add support for subprocess sandboxing.
     - Add API to get the web process unique identifier of a WebKitFrame.
     - Add WebKitWebPage::did-associate-form-controls-for-frame signal and
       deprecate did-associate-form-controls.
     - Implement AtkComponentIface scroll_to methods.
     - Improve rendering of form controls when GTK theme is dark and enable
       prefers-color-scheme media query.
     - Show the emoji chooser popover for editable content.
     - Fix touch capabilities detection for websites checking touch events
       properties present in window or pointer media queries.
     - Fix several crashes and rendering issues.

--- www/webkit-gtk/patches/patch-Source_JavaScriptCore_Scripts_check-xcfilelists.sh
+++ www/webkit-gtk/patches/patch-Source_JavaScriptCore_Scripts_check-xcfilelists.sh
@@ -1,8 +1,8 @@
-$NetBSD: patch-Source_JavaScriptCore_Scripts_check-xcfilelists.sh,v 1.1 2019/03/13 20:49:40 leot Exp $
+$NetBSD: patch-Source_JavaScriptCore_Scripts_check-xcfilelists.sh,v 1.2 2019/09/10 22:10:04 leot Exp $
 
 Avoid unportable `==' test(1) operator.
 
---- Source/JavaScriptCore/Scripts/check-xcfilelists.sh.orig	2019-02-12 11:21:03.000000000 +0000
+--- Source/JavaScriptCore/Scripts/check-xcfilelists.sh.orig	2019-08-28 11:46:27.000000000 +0000
 +++ Source/JavaScriptCore/Scripts/check-xcfilelists.sh
 @@ -1,8 +1,8 @@
  #!/bin/sh
@@ -15,4 +15,4 @@ Avoid unportable `==' test(1) operator.
 +[ "${USE_INTERNAL_SDK}" = "YES" ] || { echo "### Not running because USE_INTERNAL_SDK is not YES"; exit 0; }
  
  SCRIPT="${BUILD_SCRIPTS_DIR}/generate-xcfilelists"
- [ -f "${SCRIPT}" ] || SCRIPT="${PROJECT_DIR}/../../Tools/Scripts/generate-xcfilelists"
+ [ -f "${SCRIPT}" ] || SCRIPT="${PROJECT_DIR}/../../../${WK_ADDITIONAL_SCRIPTS_DIR}/generate-xcfilelists"
--- www/webkit-gtk/patches/patch-Source_WTF_wtf_FileSystem.cpp
+++ www/webkit-gtk/patches/patch-Source_WTF_wtf_FileSystem.cpp
@@ -1,16 +1,16 @@
-$NetBSD: patch-Source_WTF_wtf_FileSystem.cpp,v 1.1 2019/03/13 20:49:40 leot Exp $
+$NetBSD: patch-Source_WTF_wtf_FileSystem.cpp,v 1.2 2019/09/10 22:10:04 leot Exp $
 
 Support MAP_FILE if unavailable.
 
---- Source/WTF/wtf/FileSystem.cpp.orig	2019-02-12 11:21:17.000000000 +0000
+--- Source/WTF/wtf/FileSystem.cpp.orig	2019-08-28 11:46:29.000000000 +0000
 +++ Source/WTF/wtf/FileSystem.cpp
-@@ -316,6 +316,9 @@ MappedFileData::MappedFileData(const Str
-         return;
+@@ -324,6 +324,9 @@ bool MappedFileData::mapFileHandle(Platf
+         return true;
      }
  
 +#ifndef MAP_FILE
 +#define MAP_FILE	0
 +#endif
-     void* data = mmap(0, size, PROT_READ, MAP_FILE | MAP_SHARED, fd, 0);
-     close(fd);
+     void* data = mmap(0, size, PROT_READ, MAP_FILE | (mode == MappedFileMode::Shared ? MAP_SHARED : MAP_PRIVATE), fd, 0);
  
+     if (data == MAP_FAILED) {
