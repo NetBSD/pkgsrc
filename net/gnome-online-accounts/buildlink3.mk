05cd0a44f60c45a58084ebcd7ba6b6c118ac4c17
leot <leot@pkgsrc.org>
2019-09-10 22:53:54 +0000

    *: recursive bump for webkit-gtk-2.26.0

--- net/gnome-online-accounts/buildlink3.mk
+++ net/gnome-online-accounts/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2019/07/21 22:24:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2019/09/10 22:53:54 leot Exp $
 
 BUILDLINK_TREE+=	gnome-online-accounts
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gnome-online-accounts
 GNOME_ONLINE_ACCOUNTS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gnome-online-accounts+=	gnome-online-accounts>=3.29.1
-BUILDLINK_ABI_DEPENDS.gnome-online-accounts?=	gnome-online-accounts>=3.28.0nb9
+BUILDLINK_ABI_DEPENDS.gnome-online-accounts?=	gnome-online-accounts>=3.28.0nb10
 BUILDLINK_PKGSRCDIR.gnome-online-accounts?=	../../net/gnome-online-accounts
 
 .include "../../security/libgcrypt/buildlink3.mk"
