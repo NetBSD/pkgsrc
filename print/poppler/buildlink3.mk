fdc35c70d00752f4f6e660a6838d8e1c6e12446b
bsiegert <bsiegert@pkgsrc.org>
2019-09-10 18:20:51 +0000

    Pullup ticket #6054 - requested by maya print/poppler: build fix
    Revisions pulled up:
    - print/poppler/buildlink3.mk                                   1.75
    
    ---
       Module Name:	pkgsrc
       Committed By:	maya
       Date:		Mon Sep  9 11:05:00 UTC 2019
    
       Modified Files:
       	pkgsrc/print/poppler: buildlink3.mk
    
       Log Message:
       poppler: sync GCC_REQD between bl3 and Makefile.

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.73 2019/05/09 09:28:22 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.73.2.1 2019/09/10 18:20:51 bsiegert Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.poppler+=	poppler>=0.5.1
 BUILDLINK_ABI_DEPENDS.poppler+=	poppler>=0.76.0
 BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
-GCC_REQD+=	4.9
+GCC_REQD+=	5
 
 .include "../../graphics/lcms2/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
