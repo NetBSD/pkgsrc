5dda65c70ed272b2f39ac65d35a197148ac710ef
bsiegert <bsiegert@pkgsrc.org>
2019-09-10 17:56:00 +0000

    Pullup ticket #6044 - requested by taca lang/php73: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.267
    - lang/php73/distinfo                                           1.11
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sun Sep  1 13:03:17 UTC 2019
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php73: distinfo
    
       Log Message:
       lag/php73: update to 7.3.9
    
       29 Aug 2019, PHP 7.3.9
    
       - Core:
         . Fixed bug #78363 (Buffer overflow in zendparse). (Nikita)
         . Fixed bug #78379 (Cast to object confuses GC, causes crash). (Dmitry)
         . Fixed bug #78412 (Generator incorrectly reports non-releasable $this as GC
           child). (Nikita)
    
       - Curl:
         . Fixed bug #77946 (Bad cURL resources returned by curl_multi_info_read()).
           (Abyr Valg)
    
       - Exif:
         . Fixed bug #78333 (Exif crash (bus error) due to wrong alignment and
           invalid cast). (Nikita)
    
       - FPM:
         . Fixed bug #77185 (Use-after-free in FPM master event handling).
           (Maksim Nikulin)
    
       - Iconv:
         . Fixed bug #78342 (Bus error in configure test for iconv //IGNORE). (Rainer
           Jung)
    
       - LiteSpeed:
         . Updated to LiteSpeed SAPI V7.5 (Fixed clean shutdown). (George Wang)
    
       - MBString:
         . Fixed bug #78380 (Oniguruma 6.9.3 fixes CVEs). (CVE-2019-13224) (Stas)
    
       - MySQLnd:
         . Fixed bug #78179 (MariaDB server version incorrectly detected). (cmb)
         . Fixed bug #78213 (Empty row pocket). (cmb)
    
       - Opcache:
         . Fixed bug #77191 (Assertion failure in dce_live_ranges() when silencing is
           used). (Nikita)
    
       - Standard:
         . Fixed bug #69100 (Bus error from stream_copy_to_stream (file -> SSL stream)
           with invalid length). (Nikita)
         . Fixed bug #78282 (atime and mtime mismatch). (cmb)
         . Fixed bug #78326 (improper memory deallocation on stream_get_contents()
           with fixed length buffer). (Albert Casademont)
         . Fixed bug #78346 (strip_tags no longer handling nested php tags). (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.260.2.4 2019/09/08 19:24:31 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.260.2.5 2019/09/10 17:56:00 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP71_VERSION=	7.1.31
 PHP72_VERSION=	7.2.22
-PHP73_VERSION=	7.3.8
+PHP73_VERSION=	7.3.9
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
