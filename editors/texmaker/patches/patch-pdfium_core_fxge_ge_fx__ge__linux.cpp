f00fd90eeb6fcfade3c430561212164fc3a7a471
nros <nros@pkgsrc.org>
2019-09-10 16:17:13 +0000

    Set X11 fonts more correctly.
    Use X11BASE to set X11 fonts and add
    the TrueType directory used on solaris.

--- editors/texmaker/patches/patch-pdfium_core_fxge_ge_fx__ge__linux.cpp
+++ editors/texmaker/patches/patch-pdfium_core_fxge_ge_fx__ge__linux.cpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-pdfium_core_fxge_ge_fx__ge__linux.cpp,v 1.1 2019/09/05 08:07:25 nros Exp $
+$NetBSD: patch-pdfium_core_fxge_ge_fx__ge__linux.cpp,v 1.2 2019/09/10 16:17:13 nros Exp $
 * Add X11 and pkgsrc font paths
 --- pdfium/core/fxge/ge/fx_ge_linux.cpp.orig	2017-04-16 14:53:52.000000000 +0000
 +++ pdfium/core/fxge/ge/fx_ge_linux.cpp
@@ -7,14 +7,11 @@ $NetBSD: patch-pdfium_core_fxge_ge_fx__ge__linux.cpp,v 1.1 2019/09/05 08:07:25 n
  // Copyright 2014 PDFium Authors. All rights reserved.
  // Use of this source code is governed by a BSD-style license that can be
  // found in the LICENSE file.
-@@ -132,6 +133,11 @@ std::unique_ptr<IFX_SystemFontInfo> IFX_
+@@ -132,6 +133,8 @@ std::unique_ptr<IFX_SystemFontInfo> IFX_
      pInfo->AddPath("/usr/share/X11/fonts/Type1");
      pInfo->AddPath("/usr/share/X11/fonts/TTF");
      pInfo->AddPath("/usr/local/share/fonts");
-+    pInfo->AddPath("/usr/X11R7/lib/X11/fonts/Type1");
-+    pInfo->AddPath("/usr/X11R7/lib/X11/fonts/TTF");
-+    pInfo->AddPath("/usr/X11R6/lib/X11/fonts/Type1");
-+    pInfo->AddPath("/usr/X11R6/lib/X11/fonts/TTF");
++#include "../fxge_x11fonts.inc"
 +#include "../fxge_pkgsrcfonts.inc"
    }
    return std::unique_ptr<IFX_SystemFontInfo>(pInfo);
