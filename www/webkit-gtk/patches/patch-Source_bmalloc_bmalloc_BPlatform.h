eebbbec083cdb6f40e40aceabc4e2528b2f51ef1
leot <leot@pkgsrc.org>
2019-09-10 22:10:04 +0000

    webkit-gtk: Update to 2.26.0
    pkgsrc changes:
     - When built with gcc, 7.3.0 or newer is now needed
    
    Changes:
    WebKitGTK 2.26.0
    ================
     - Disable process swap on navigation.
     - Do not enable the sandbox in Snap.
     - Fix sandbox parsing DISPLAY on X11.
     - Translation updates: Brazilian Portuguese, German.
    
    WebKitGTK 2.25.92
    =================
     - Add WEBKIT_USE_SINGLE_WEB_PROCESS environment variable to force single
       process model in all WebKitWebContext.
       This is a temporary solution for applications still depending on the
       single process mode behavior. It will be only available in 2.26 series.
     - Add new API to remove a filter from an user content manager given its
       identifier.
     - Add support for HSTS.
     - Several improvements and bug fixes in MSE media player.
     - Fix building without unified sources.
     - Fix several crashes and rendering issues.
     - Translation updates: Polish, Ukrainian.
    
    WebKitGTK 2.25.4
    ================
     - Switch to use libsoup WebSockets API.
     - Add support for permessage-deflate WebSocket extension.
     - Add support for datalist element in text input fields.
     - Fix a crash with empty video source.
     - Fix several crashes and rendering issues.
    
    WebKitGTK 2.25.3
    ================
     - Remove support for GTK2 NPAPI plugins.
     - Fix web view updates after swapping web process if accelerated
       compositing mode is forced.
     - Make kinetic scrolling work again.
     - Fix position of emoji chooser when page is scrolled.
     - Fix web process deadlock when scrolling twitter timeline which contains
       HLS videos.
     - Make navigation gesture use dark fallback background color color on dark
       themes.
     - Make Previous/Next gesture work in RTL mode.
     - Support cancelling touchscreen back/forward gesture.
     - Add user agent quirk to make github work in FreeBSD.
     - Fix content disappearing when using CSS transforms.
     - Fix some radio streams that could not be played.
     - Fix video pause that sometimes caused to skip to finish.
     - Fix volume level changes when playing a video.
     - Fix several crashes and rendering issues.
    
    WebKitGTK 2.25.2
    ================
     - Enable process switch on cross site navigation.
     - Use libwpe with fdo backend to implement accelerated compositing under
       wayland.
     - Fix rendering artifacts in youtube volume button.
     - Fix trapezoid artifact in github comment box.
     - Ensure web extensions directory is readable when sandbox is enabled.
     - Fix the executable name of WebDriver process, renamed by mistake in
       2.25.1.
     - Enable hyperlink auditing setting by default.
     - Remove the option to build without using the redirected XComposite window.
     - Fix HLS streams being slow to start.
     - Make accessibility work when sandbox is enabled.
     - Fix several crashes and rendering issues.
    
    WebKitGTK 2.25.1
    ================
     - Add support for subprocess sandboxing.
     - Add API to get the web process unique identifier of a WebKitFrame.
     - Add WebKitWebPage::did-associate-form-controls-for-frame signal and
       deprecate did-associate-form-controls.
     - Implement AtkComponentIface scroll_to methods.
     - Improve rendering of form controls when GTK theme is dark and enable
       prefers-color-scheme media query.
     - Show the emoji chooser popover for editable content.
     - Fix touch capabilities detection for websites checking touch events
       properties present in window or pointer media queries.
     - Fix several crashes and rendering issues.

--- www/webkit-gtk/patches/patch-Source_bmalloc_bmalloc_BPlatform.h
+++ www/webkit-gtk/patches/patch-Source_bmalloc_bmalloc_BPlatform.h
@@ -1,15 +1,23 @@
-$NetBSD: patch-Source_bmalloc_bmalloc_BPlatform.h,v 1.1 2018/10/24 18:31:07 leot Exp $
+$NetBSD: patch-Source_bmalloc_bmalloc_BPlatform.h,v 1.2 2019/09/10 22:10:04 leot Exp $
 
-Add BOS(SOLARIS) definition.
+Add BOS(SOLARIS) and BOS(NETBSD) definitions.
 
---- Source/bmalloc/bmalloc/BPlatform.h.orig	2018-07-25 11:56:32.000000000 +0000
+--- Source/bmalloc/bmalloc/BPlatform.h.orig	2019-08-28 11:46:33.000000000 +0000
 +++ Source/bmalloc/bmalloc/BPlatform.h
-@@ -48,6 +48,10 @@
+@@ -48,10 +48,18 @@
  #define BOS_LINUX 1
  #endif
  
 +#ifdef __sun
 +#define BOS_SOLARIS 1
++#endif
++
+ #if defined(__FreeBSD__) || defined(__DragonFly__) || defined(__FreeBSD_kernel__)
+ #define BOS_FREEBSD 1
+ #endif
+ 
++#if defined(__NetBSD__)
++#define BOS_NETBSD 1
 +#endif
 +
  #if defined(WIN32) || defined(_WIN32)
