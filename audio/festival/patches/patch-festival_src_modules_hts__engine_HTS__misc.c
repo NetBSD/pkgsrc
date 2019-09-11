fe6e093fae64cf36234cd78d620c676fcf1cb4b0
nia <nia@pkgsrc.org>
2019-09-11 10:03:17 +0000

    festival: Commit a missing patch.

---
+++ audio/festival/patches/patch-festival_src_modules_hts__engine_HTS__misc.c
@@ -0,0 +1,18 @@
+$NetBSD: patch-festival_src_modules_hts__engine_HTS__misc.c,v 1.1 2019/09/11 10:03:17 nia Exp $
+
+Support NetBSD and FreeBSD.
+
+--- festival/src/modules/hts_engine/HTS_misc.c.orig	2017-09-04 15:54:08.000000000 +0000
++++ festival/src/modules/hts_engine/HTS_misc.c
+@@ -245,8 +245,10 @@ size_t HTS_ftell(HTS_File * fp)
+    } else if (fp->type == HTS_FILE) {
+       fpos_t pos;
+       fgetpos((FILE *) fp->pointer, &pos);
+-#if defined(_WIN32) || defined(__CYGWIN__) || defined(__APPLE__)
++#if defined(_WIN32) || defined(__CYGWIN__) || defined(__APPLE__) || defined(__FreeBSD__)
+       return (size_t) pos;
++#elif defined(__NetBSD__)
++      return (size_t) pos._pos;
+ #else
+       return (size_t) pos.__pos;
+ #endif                          /* _WIN32 || __APPLE__ */
