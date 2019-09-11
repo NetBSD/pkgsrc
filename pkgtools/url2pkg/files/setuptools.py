00aaf8cc3a97b351941aac20b3213976579b0e31
rillig <rillig@pkgsrc.org>
2019-09-11 05:25:55 +0000

    pkgtools/url2pkg: update to 2.34
    Changes since 2.33:
    
    * code cleanup in MakeMaker.pm
    * don't create a PLIST for Perl packages
    
    * allow Python dependencies from setup.py to use spaces around >=
    * properly indented Python code according to PEP 8
    * added mock for setuptools.find_package
    * only try to migrate a package from GitHub to PyPI if it is really
      a Python package on GitHub

--- pkgtools/url2pkg/files/setuptools.py
+++ pkgtools/url2pkg/files/setuptools.py
@@ -1,15 +1,15 @@
-# $NetBSD: setuptools.py,v 1.1 2019/08/18 11:26:33 rillig Exp $
+# $NetBSD: setuptools.py,v 1.2 2019/09/11 05:25:55 rillig Exp $
 
 def url2pkg_print_depends(keyword, depends):
-	for dep in depends:
-		if '>' not in dep:
-			dep = dep + '>=0'
-		print('%s\t%s' % (keyword, dep))
+    for dep in depends:
+        print('%s\t%s%s' % (keyword, dep.replace(' ', ''), '' if '>' in dep else '>=0'))
 
 def setup(**kwargs):
+    if 'install_requires' in kwargs:
+        url2pkg_print_depends('DEPENDS', kwargs['install_requires'])
 
-	if 'install_requires' in kwargs:
-		url2pkg_print_depends('DEPENDS', kwargs['install_requires'])
+    if 'tests_require' in kwargs:
+        url2pkg_print_depends('TEST_DEPENDS', kwargs['tests_require'])
 
-	if 'tests_require' in kwargs:
-		url2pkg_print_depends('TEST_DEPENDS', kwargs['tests_require'])
+def find_packages(where='.', exclude=(), include=('*',)):
+    return []
