e86477cd1679bc1886bc57fa07118a4cd8787e71
ryoon <ryoon@pkgsrc.org>
2019-09-11 14:19:08 +0000

    Remove GCC related things
    Reported by David H. Gutteridge, thank you.

--- www/firefox/mozilla-common.mk
+++ www/firefox/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.137 2019/09/06 03:00:23 ryoon Exp $
+# $NetBSD: mozilla-common.mk,v 1.138 2019/09/11 14:19:08 ryoon Exp $
 #
 # common Makefile fragment for mozilla packages based on gecko 2.0.
 #
@@ -50,11 +50,6 @@ test:
 .if ${OPSYS} == "OpenBSD"
 TOOLS_PLATFORM.tar=	${TOOLS_PATH.bsdtar}
 USE_TOOLS+=		bsdtar
-.endif
-GCC_REQD+=		6.1
-.if !empty(MACHINE_PLATFORM:MNetBSD-[0-7]**-*) || \
-	!empty(MACHINE_PLATFORM:MNetBSD-8.[0-8]*-*)
-USE_PKGSRC_GCC_RUNTIME=	yes
 .endif
 .if ${MACHINE_ARCH} == "i386"
 # Fix for PR pkg/48152.
