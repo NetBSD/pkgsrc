927b479e5f32cf4967442b618d908317b801bd2c
bouyer <bouyer@pkgsrc.org>
2019-09-12 16:54:34 +0000

    Update to 1.6.8. Changes: - updated translations - UI bugfixes - Added DeleteBoundary and DeleteBoundaryPoint to API and json messages

--- geography/opencpn-plugin-draw/patches/patch-cmake_PluginConfigure.cmake
+++ geography/opencpn-plugin-draw/patches/patch-cmake_PluginConfigure.cmake
@@ -1,10 +1,10 @@
-$NetBSD: patch-cmake_PluginConfigure.cmake,v 1.6 2018/08/27 15:54:09 bouyer Exp $
+$NetBSD: patch-cmake_PluginConfigure.cmake,v 1.7 2019/09/12 16:54:34 bouyer Exp $
 Do not overwrite CMAKE_SHARED_LINKER_FLAGS
 
---- cmake/PluginConfigure.cmake.orig	2018-07-18 10:14:19.000000000 +0200
-+++ cmake/PluginConfigure.cmake	2018-08-27 15:38:00.730541824 +0200
-@@ -69,9 +69,9 @@
-   ADD_DEFINITIONS(-D_CRT_NONSTDC_NO_DEPRECATE -D_CRT_SECURE_NO_DEPRECATE)
+--- cmake/PluginConfigure.cmake.orig	2019-07-18 09:04:32.000000000 +0200
++++ cmake/PluginConfigure.cmake	2019-09-12 18:28:14.915554652 +0200
+@@ -70,9 +70,9 @@
+   ADD_DEFINITIONS(-D HAVE_SNPRINTF)
  ELSE(MSVC)
    IF(NOT APPLE)
 -    SET( CMAKE_SHARED_LINKER_FLAGS "-Wl,-Bsymbolic")
