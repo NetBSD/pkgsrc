1abf2a3383be5f17a7f6686dc43cc51367faa127
tsutsui <tsutsui@pkgsrc.org>
2019-09-14 18:11:34 +0000

    ruby-gnome: Update to 3.3.8, and rename package names to match gems.
    Also reorganize several dependencies in Makefile and buildlink3.mk.
    See the following post for details:
     https://mail-index.netbsd.org/pkgsrc-users/2019/09/07/msg029327.html
     https://mail-index.netbsd.org/pkgsrc-users/2019/09/13/msg029356.html
    
    Upstream changes (from NEWS):
    
    == Ruby-GNOME 3.3.8: 2019-09-10
    
    This is a partially GLib 2.62.0 support release.
    
    === Changes
    
    ==== All
    
      * Improvements
    
        * Changed our project name to Ruby-GNOME from Ruby-GNOME2.
          [GitHub#1277][Suggested by kojix2]
          [GitHub#1291][Patch by kojix2]
    
        * Stopped to release (({.tar.gz})) because they are no longer
          used.
    
    ==== Ruby/GLib2
    
      * Improvements
    
        * (({GLib.convert})): Changed to set correct encoding.
    
        * (({GLib::FILENAME_ENCODING})): Added.
    
        * Changed to use the same enum object for the same enum value.
    
        * (({GLib::Enum.find})): Added.
    
        * (({GLib::Bytes#initialize})): Changed to reuse (({String})) data
          even if the given (({String})) isn't frozen.
    
        * (({GLib::Bytes.try_convert})): Added.
    
        * (({GLib::Enum.try_convert})): Added.
    
        * (({GLib::Flags.try_convert})): Added.
    
        * (({GLib::Type.try_convert})): Added.
    
        * (({GLib::MkEnums.create})): Added support for flags to enum
          definition.
          [GitHub#1295][Patch by Mamoru TASAKA]
    
    ==== Ruby/GIO2
    
      * Fixes
    
        * Renamed to (({Gio::Icon#hash})) from (({Gio::Icon.hash})).
          [GitHub#1293][Reported by Erik Czumadewski]
    
    ==== Ruby/GObjectIntrospection
    
      * Improvements
    
        * Introduced (({try_convert})) protocol.
    
    ==== Ruby/CairoGObject
    
      * Improvements
    
        * (({Cairo::Context.try_convert})): Added.
    
        * (({Cairo::Device.try_convert})): Added.
    
        * (({Cairo::Pattern.try_convert})): Added.
    
        * (({Cairo::Surface.try_convert})): Added.
    
        * (({Cairo::ScaledFont.try_convert})): Added.
    
        * (({Cairo::FontFace.try_convert})): Added.
    
        * (({Cairo::FontOptions.try_convert})): Added.
    
        * (({Cairo::Region.try_convert})): Added.
    
    === Thanks
    
      * kojix2
    
      * Erik Czumadewski
    
      * Mamoru TASAKA

---
+++ devel/ruby-gio2/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/09/14 18:11:37 tsutsui Exp $
+
+BUILDLINK_TREE+=	ruby-gio2
+
+.if !defined(RUBY_GIO2_BUILDLINK3_MK)
+RUBY_GIO2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ruby-gio2+=	${RUBY_PKGPREFIX}-gio2>=3.3.8
+BUILDLINK_ABI_DEPENDS.ruby-gio2+=	${RUBY_PKGPREFIX}-gio2>=3.3.8
+BUILDLINK_PKGSRCDIR.ruby-gio2?=		../../devel/ruby-gio2
+
+.include "../../devel/ruby-gobject-introspection/buildlink3.mk"
+.include "../../lang/ruby/buildlink3.mk"
+.endif # RUBY_GIO2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ruby-gio2
