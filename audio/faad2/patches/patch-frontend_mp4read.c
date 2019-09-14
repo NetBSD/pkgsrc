c707823a0e2329139bf41b4eeeb49ef9f83e5369
nia <nia@pkgsrc.org>
2019-09-14 13:34:06 +0000

    faad2: Update to 2.9.0
    Changes:
    
    [ Krzysztof Nikiel ]
    
        Build system fixes and code clean-up
    
    [ LoRd_MuldeR ]
    
        Fix compiler warnings and code indentation
        Fix compilation with GCC <= 4.7.3
        MSVC solution file clean-up
    
    [ Cameron Cawley ]
    
        Fix compilation with GCC 4.7.4
        Fix compilation with MinGW
    
    [ Michael Fink ]
    
        MSVC 2017 project file update
    
    [ Hugo Lefeuvre ]
    
        Fix crash with unsupported MP4 files (NULL pointer dereference,
        division by zero)
        CVE-2019-6956: ps_dec: sanitize iid_index before mixing
        CVE-2018-20196: sbr_fbt: sanitize sbr->M (should not exceed MAX_M)
        CVE-2018-20199, CVE-2018-20360: specrec: better handle unexpected
        parametric stereo (PS)
        CVE-2018-20362, CVE-2018-19504, CVE-2018-20195, CVE-2018-20198,
        CVE-2018-20358: syntax.c: check for syntax element inconsistencies
        CVE-2018-20194, CVE-2018-19503, CVE-2018-20197, CVE-2018-20357,
        CVE-2018-20359, CVE-2018-20361: sbr_hfadj: sanitize frequency band
        borders
    
    [ Hugo Beauzée-Luyssen ]
    
        CVE-2019-15296, CVE-2018-19502: Fix a couple buffer overflows
    
    [ Filip Roséen ]
    
        Prevent crash on SCE followed by CPE
    
    [ Gianfranco Costamagna ]
    
        Fix linking with GCC 9 and "-Wl,--as-needed"
    
    [ Fabian Greffrath ]
    
        Enable the frontend to be built reproducibly

--- audio/faad2/patches/patch-frontend_mp4read.c
+++ audio/faad2/patches/patch-frontend_mp4read.c
@@ -1,20 +1,19 @@
-$NetBSD: patch-frontend_mp4read.c,v 1.2 2019/06/05 06:07:27 nia Exp $
+$NetBSD: patch-frontend_mp4read.c,v 1.3 2019/09/14 13:34:06 nia Exp $
 
-Do not re-define bswap32() and bswap16().
+Avoid conflicting with NetBSD libc.
 
---- frontend/mp4read.c.orig	2017-12-17 11:18:43.000000000 +0000
+--- frontend/mp4read.c.orig	2019-09-09 10:28:33.000000000 +0000
 +++ frontend/mp4read.c
-@@ -46,6 +46,8 @@ mp4config_t mp4config = { 0 };
+@@ -46,6 +46,7 @@ mp4config_t mp4config = { 0 };
  
  static FILE *g_fin = NULL;
  
-+#include "config.h"
-+#ifndef HAVE_SYS_ENDIAN_H
++#ifndef __NetBSD__
  static inline uint32_t bswap32(const uint32_t u32)
  {
  #ifndef WORDS_BIGENDIAN
-@@ -71,6 +73,7 @@ static inline uint16_t bswap16(const uin
- 	return u16;
+@@ -75,6 +76,7 @@ static inline uint16_t bswap16(const uin
+     return u16;
  #endif
  }
 +#endif
