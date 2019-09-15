b200f24a8d406434ff5b7b2a1dcb93970676b8fb
nia <nia@pkgsrc.org>
2019-09-15 17:55:09 +0000

    anjuta: Update to 3.34
    Switch to GTK+3 and remove the dependency on GNOME 2's gnome-terminal.

--- devel/anjuta/patches/patch-plugins_build-basic-autotools_build.c
+++ devel/anjuta/patches/patch-plugins_build-basic-autotools_build.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-plugins_build-basic-autotools_build.c,v 1.3 2016/05/07 09:55:21 joerg Exp $
+$NetBSD: patch-plugins_build-basic-autotools_build.c,v 1.4 2019/09/15 17:55:09 nia Exp $
 
---- plugins/build-basic-autotools/build.c.orig	2013-08-09 00:30:17.000000000 +0000
+--- plugins/build-basic-autotools/build.c.orig	2018-10-22 19:25:42.000000000 +0000
 +++ plugins/build-basic-autotools/build.c
-@@ -216,7 +216,7 @@ directory_has_file (GFile *dir, const gc
+@@ -218,7 +218,7 @@ directory_has_file (GFile *dir, const gc
  }
  
  static gchar*
@@ -11,12 +11,12 @@ $NetBSD: patch-plugins_build-basic-autotools_build.c,v 1.3 2016/05/07 09:55:21 j
  {
  	va_list args;
  	gchar *str;
-@@ -524,7 +524,7 @@ build_save_distclean_and_execute_command
+@@ -575,7 +575,7 @@ build_save_distclean_and_execute_command
  
  		// Need to run make clean before
  		if (!anjuta_util_dialog_boolean_question (GTK_WINDOW (ANJUTA_PLUGIN (bplugin)->shell), FALSE,
--		    _("Before using this new configuration, the default one needs to be removed. Do you want to do that ?"), NULL))
-+		    _("Before using this new configuration, the default one needs to be removed. Do you want to do that ?")))
+-		    _("Before using this new configuration, the default one needs to be removed. Do you want to do that?"), NULL))
++		    _("Before using this new configuration, the default one needs to be removed. Do you want to do that?")))
  		{
  			if (err)
  				*err = g_error_new (ianjuta_builder_error_quark (),
