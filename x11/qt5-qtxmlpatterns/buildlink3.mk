1accb3ef0001b93da506fc898e4a67edaa0da8fe
adam <adam@pkgsrc.org>
2019-09-16 19:24:52 +0000

    qt5: updated to 5.13.1
    As a patch release, Qt 5.13.1 does not add any new functionality but provides many bug fixes and other improvements

--- x11/qt5-qtxmlpatterns/buildlink3.mk
+++ x11/qt5-qtxmlpatterns/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2019/08/09 17:41:42 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2019/09/16 19:24:56 adam Exp $
 
 BUILDLINK_TREE+=	qt5-qtxmlpatterns
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qt5-qtxmlpatterns
 QT5_QTXMLPATTERNS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt5-qtxmlpatterns+=	qt5-qtxmlpatterns>=5.9.1
-BUILDLINK_ABI_DEPENDS.qt5-qtxmlpatterns+=	qt5-qtxmlpatterns>=5.13.0
+BUILDLINK_ABI_DEPENDS.qt5-qtxmlpatterns+=	qt5-qtxmlpatterns>=5.13.1
 BUILDLINK_PKGSRCDIR.qt5-qtxmlpatterns?=		../../x11/qt5-qtxmlpatterns
 
 BUILDLINK_INCDIRS.qt5-qtxmlpatterns+=	qt5/include
