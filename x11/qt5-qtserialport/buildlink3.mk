5b34e9d74d5fb7107f8ea25178e6651e5ddb32e2
adam <adam@pkgsrc.org>
2019-09-16 19:24:52 +0000

    qt5: updated to 5.13.1
    As a patch release, Qt 5.13.1 does not add any new functionality but provides many bug fixes and other improvements

--- x11/qt5-qtserialport/buildlink3.mk
+++ x11/qt5-qtserialport/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2019/08/09 17:41:41 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2019/09/16 19:24:55 adam Exp $
 
 BUILDLINK_TREE+=	qt5-qtserialport
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qt5-qtserialport
 QT5_QTSERIALPORT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt5-qtserialport+=	qt5-qtserialport>=5.9.1
-BUILDLINK_ABI_DEPENDS.qt5-qtserialport+=	qt5-qtserialport>=5.13.0
+BUILDLINK_ABI_DEPENDS.qt5-qtserialport+=	qt5-qtserialport>=5.13.1
 BUILDLINK_PKGSRCDIR.qt5-qtserialport?=	../../x11/qt5-qtserialport
 
 BUILDLINK_INCDIRS.qt5-qtserialport+=	qt5/include
