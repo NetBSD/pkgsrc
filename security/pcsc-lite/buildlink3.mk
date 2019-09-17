890a29db5ab48711dfe92ec53b1939d196304315
perseant <perseant@pkgsrc.org>
2019-09-17 23:09:42 +0000

    Update ccid to version 1.4.27, including link requirement in pcsc-lite. Closes PR security/54556.

--- security/pcsc-lite/buildlink3.mk
+++ security/pcsc-lite/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.8 2018/01/07 13:04:30 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2019/09/17 23:09:42 perseant Exp $
 
 BUILDLINK_TREE+=	pcsc-lite
 
 .if !defined(PCSC_LITE_BUILDLINK3_MK)
 PCSC_LITE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.pcsc-lite+=	pcsc-lite>=1.6.5
+BUILDLINK_API_DEPENDS.pcsc-lite+=	pcsc-lite>=1.8.3
 BUILDLINK_PKGSRCDIR.pcsc-lite?=		../../security/pcsc-lite
 
 .endif # PCSC_LITE_BUILDLINK3_MK
