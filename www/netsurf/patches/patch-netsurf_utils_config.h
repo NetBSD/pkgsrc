e3fec949c2f02b4244d65767964472a570144736
nros <nros@pkgsrc.org>
2019-09-18 20:32:47 +0000

    Remove compilation messages about redundant declaration of strchrnul
    NetBSD has strchrnul so tell netsurf this.

--- www/netsurf/patches/patch-netsurf_utils_config.h
+++ www/netsurf/patches/patch-netsurf_utils_config.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-netsurf_utils_config.h,v 1.1 2015/09/03 20:56:10 leot Exp $
+$NetBSD: patch-netsurf_utils_config.h,v 1.2 2019/09/18 20:32:47 nros Exp $
 
-NetBSD has strcasestr(3).
+NetBSD has strcasestr(3) and strchrnul(3).
 
---- netsurf/utils/config.h.orig	2015-03-11 07:34:53.000000000 +0000
+--- netsurf/utils/config.h.orig	2019-07-17 15:37:00.000000000 +0000
 +++ netsurf/utils/config.h
-@@ -31,7 +31,7 @@
+@@ -30,7 +30,7 @@
  char *strndup(const char *s, size_t n);
  #endif
  
@@ -13,3 +13,12 @@ NetBSD has strcasestr(3).
  #define HAVE_STRCASESTR
  #else
  #undef HAVE_STRCASESTR
+@@ -51,7 +51,7 @@ char *strcasestr(const char *haystack, c
+ /* For some reason, UnixLib defines this unconditionally. Assume we're using
+  *  UnixLib if building for RISC OS.
+  */
+-#if ((defined(_GNU_SOURCE) && !defined(__APPLE__)) || defined(__riscos__))
++#if ((defined(_GNU_SOURCE) && !defined(__APPLE__)) || defined(__riscos__) || defined(__NetBSD__))
+ #define HAVE_STRCHRNUL
+ #else
+ #undef HAVE_STRCHRNUL
