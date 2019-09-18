0d4fa94ee8e435a7d118ae57643f3bcc139fb0f4
nia <nia@pkgsrc.org>
2019-09-18 22:30:49 +0000

    opencolorio: Update to 1.1.1
    Changes:
    
        Added optional compatibility for building apps with OpenImageIO 1.9+
        Added USE_SSE checks to fix Linux build failure
        getDisplays() result ordering now matches the active_displays config
        definition or OCIO_ACTIVE_DISPLAYS env var override.
        Fixed incorrect getDefaultDisplay()/getDefaultView() result when
        OCIO_ACTIVE_DISPLAYS or OCIO_ACTIVE_VIEWS env vars are unset.
        Fixed Windows-specific GetEnv() bug
        Fixed Windows and MacOS CI failure cases
        libc++ build fixes
        Added support for YAML > 5.0.1
        YAML and TinyXML patch fixes
        Clang visibility fix
        Added write support for Truelight LUTs
        Improved OCIOYaml
        Python string corruption fix
        Added support for CDL
        Updated documentation
        Added args/kwargs support to Python MatrixTransform
        Added description field to Look objects
        Improved Python 3 compatibility
        CSP file read fix
        Added Linux, MacOS, and Windows continuos integration
        Improved 1D LUT extrapolation
        Improved 1D LUT negative handling
        Improved Windows build system
        Improved cross-platform build system
        Undefined role crash fix
        After Effects plugin updated
        Added reference Photoshop plugin
        Added reference Docker image

--- graphics/opencolorio/buildlink3.mk
+++ graphics/opencolorio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/04/06 03:42:19 maya Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/09/18 22:30:49 nia Exp $
 
 BUILDLINK_TREE+=	OpenColorIO
 
@@ -8,9 +8,9 @@ OPENCOLORIO_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.OpenColorIO+=	OpenColorIO>=1.0.9
 BUILDLINK_PKGSRCDIR.OpenColorIO?=	../../graphics/opencolorio
 
-#.include "../../graphics/lcms2/buildlink3.mk"
-#.include "../../textproc/tinyxml/buildlink3.mk"
-#.include "../../wip/yaml-cpp03/buildlink3.mk"
+.include "../../graphics/lcms2/buildlink3.mk"
+.include "../../textproc/tinyxml/buildlink3.mk"
+.include "../../textproc/yaml-cpp/buildlink3.mk"
 .endif	# OPENCOLORIO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-OpenColorIO
