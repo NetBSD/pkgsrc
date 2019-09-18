227109edc8b50c274522c1eeb630b0b2ebcb8d1d
nia <nia@pkgsrc.org>
2019-09-18 23:40:17 +0000

    blender: Update to 2.80
    Release notes:
    https://wiki.blender.org/wiki/Reference/Release_Notes/2.80

--- graphics/blender/patches/patch-intern_guardedalloc_intern_mallocn__intern.h
+++ graphics/blender/patches/patch-intern_guardedalloc_intern_mallocn__intern.h
@@ -1,13 +1,15 @@
-$NetBSD: patch-intern_guardedalloc_intern_mallocn__intern.h,v 1.2 2015/12/19 20:52:12 markd Exp $
+$NetBSD: patch-intern_guardedalloc_intern_mallocn__intern.h,v 1.3 2019/09/18 23:40:17 nia Exp $
 
---- intern/guardedalloc/intern/mallocn_intern.h.orig	2015-10-07 00:09:33.000000000 +0000
+Support NetBSD.
+
+--- intern/guardedalloc/intern/mallocn_intern.h.orig	2019-07-24 07:41:39.000000000 +0000
 +++ intern/guardedalloc/intern/mallocn_intern.h
-@@ -63,6 +63,8 @@
- #elif defined(WIN32)
+@@ -55,6 +55,8 @@
+ #elif defined(__HAIKU__)
  #  include <malloc.h>
- #  define malloc_usable_size _msize
+ size_t malloc_usable_size(void *ptr);
 +#elif defined(__NetBSD__)
-+#  define malloc_usable_size(x) 0
++#  define malloc_usable_size(p) (0)
  #else
  #  pragma message "We don't know how to use malloc_usable_size on your platform"
  #  undef USE_MALLOC_USABLE_SIZE
