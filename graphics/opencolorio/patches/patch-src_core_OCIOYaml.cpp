5d2e34758b362b2b4b59ba6343d737b8f18c28a6
nia <nia@pkgsrc.org>
2019-09-18 22:30:49 +0000

    opencolorio: Update to 1.1.1
    Changes:
    
        Added optional compatibility for building apps with OpenImageIO 1.9+
        Added USE_SSE checks to fix Linux build failure
        getDisplays() result ordering now matches the active_displays config
        definition or OCIO_ACTIVE_DISPLAYS env var override.
        Fixed incorrect getDefaultDisplay()/getDefaultView() result when
        OCIO_ACTIVE_DISPLAYS or OCIO_ACTIVE_VIEWS env vars are unset.
        Fixed Windows-specific GetEnv() bug
        Fixed Windows and MacOS CI failure cases
        libc++ build fixes
        Added support for YAML > 5.0.1
        YAML and TinyXML patch fixes
        Clang visibility fix
        Added write support for Truelight LUTs
        Improved OCIOYaml
        Python string corruption fix
        Added support for CDL
        Updated documentation
        Added args/kwargs support to Python MatrixTransform
        Added description field to Look objects
        Improved Python 3 compatibility
        CSP file read fix
        Added Linux, MacOS, and Windows continuos integration
        Improved 1D LUT extrapolation
        Improved 1D LUT negative handling
        Improved Windows build system
        Improved cross-platform build system
        Undefined role crash fix
        After Effects plugin updated
        Added reference Photoshop plugin
        Added reference Docker image

---
+++ graphics/opencolorio/patches/patch-src_core_OCIOYaml.cpp
@@ -0,0 +1,60 @@
+$NetBSD: patch-src_core_OCIOYaml.cpp,v 1.1 2019/09/18 22:30:49 nia Exp $
+
+Description: Fix build with yaml-cpp 0.6
+ One of the changes in yaml-cpp 0.6 was the replacement of some inline
+ virtual functions with out-of-line virtual functions, to ensure a
+ single definition of the relevant vtable.
+ .
+ Unfortunately, OpenColorIO forward-declares these classes with a different
+ GCC visibility attribute. Now that the key function for the vtables is
+ not inlined into code using yaml-cpp.h that breaks link-time resolution of
+ the vtables.
+Author: Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
+Bug: https://github.com/imageworks/OpenColorIO/issues/517
+
+--- src/core/OCIOYaml.cpp.orig	2019-03-28 05:12:57.000000000 +0000
++++ src/core/OCIOYaml.cpp
+@@ -30,43 +30,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ 
+ #include <OpenColorIO/OpenColorIO.h>
+ 
+-#ifndef WIN32
+-
+-// fwd declare yaml-cpp visibility
+-#pragma GCC visibility push(hidden)
+-namespace YAML {
+-    class Exception;
+-    class BadDereference;
+-    class RepresentationException;
+-    class EmitterException;
+-    class ParserException;
+-    class InvalidScalar;
+-    class KeyNotFound;
+-    template <typename T> class TypedKeyNotFound;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::ColorSpace>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::Config>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::Exception>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::GpuShaderDesc>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::ImageDesc>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::Look>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::Processor>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::Transform>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::AllocationTransform>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::CDLTransform>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::ColorSpaceTransform>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::DisplayTransform>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::ExponentTransform>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::FileTransform>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::GroupTransform>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::LogTransform>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::LookTransform>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::MatrixTransform>;
+-    template <> class TypedKeyNotFound<OCIO_NAMESPACE::TruelightTransform>;
+-}
+-#pragma GCC visibility pop
+-
+-#endif
+-
+ #ifdef WIN32
+ #pragma warning( push )
+ #pragma warning( disable: 4146 )
