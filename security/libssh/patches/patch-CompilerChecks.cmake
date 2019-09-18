1cc09dcc03b4ce0578db5a104beb6a0c9c12534f
nia <nia@pkgsrc.org>
2019-09-18 19:35:08 +0000

    libssh: update patch

--- security/libssh/patches/patch-CompilerChecks.cmake
+++ security/libssh/patches/patch-CompilerChecks.cmake
@@ -1,27 +1,36 @@
-$NetBSD: patch-CompilerChecks.cmake,v 1.1 2018/12/03 15:19:51 adam Exp $
+$NetBSD: patch-CompilerChecks.cmake,v 1.2 2019/09/18 19:35:08 nia Exp $
 
 Let PkgSrc handle security features.
 
---- CompilerChecks.cmake.orig	2018-12-03 09:27:44.000000000 +0000
+--- CompilerChecks.cmake.orig	2019-06-27 08:34:16.000000000 +0000
 +++ CompilerChecks.cmake
-@@ -62,20 +62,7 @@ if (UNIX)
+@@ -66,29 +66,6 @@ if (UNIX)
          endif()
      endif()
  
 -    check_c_compiler_flag_ssp("-fstack-protector-strong" WITH_STACK_PROTECTOR_STRONG)
 -    if (WITH_STACK_PROTECTOR_STRONG)
 -        list(APPEND SUPPORTED_COMPILER_FLAGS "-fstack-protector-strong")
+-        # This is needed as Solaris has a seperate libssp
+-        if (SOLARIS)
+-            list(APPEND SUPPORTED_LINKER_FLAGS "-fstack-protector-strong")
+-        endif()
 -    else (WITH_STACK_PROTECTOR_STRONG)
 -        check_c_compiler_flag_ssp("-fstack-protector" WITH_STACK_PROTECTOR)
 -        if (WITH_STACK_PROTECTOR)
 -            list(APPEND SUPPORTED_COMPILER_FLAGS "-fstack-protector")
+-            # This is needed as Solaris has a seperate libssp
+-            if (SOLARIS)
+-                list(APPEND SUPPORTED_LINKER_FLAGS "-fstack-protector")
+-            endif()
 -        endif()
 -    endif (WITH_STACK_PROTECTOR_STRONG)
- 
+-
 -    check_c_compiler_flag_ssp("-fstack-clash-protection" WITH_STACK_CLASH_PROTECTION)
 -    if (WITH_STACK_CLASH_PROTECTION)
 -        list(APPEND SUPPORTED_COMPILER_FLAGS "-fstack-clash-protection")
 -    endif()
- 
+-
      if (PICKY_DEVELOPER)
          add_c_compiler_flag("-Wno-error=deprecated-declarations" SUPPORTED_COMPILER_FLAGS)
+         add_c_compiler_flag("-Wno-error=tautological-compare" SUPPORTED_COMPILER_FLAGS)
