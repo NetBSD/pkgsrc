80d0900a33c5f52897b22b0df52e4c22a4aa4b33
adam <adam@pkgsrc.org>
2019-09-19 16:50:57 +0000

    py-zstandard: updated to 0.12.0
    0.12.0:
    
    Backwards Compatibility Notes
    * Support for Python 3.4 has been dropped since Python 3.4 is no longer
      a supported Python version upstream. (But it will likely continue to
      work until Python 2.7 support is dropped and we port to Python 3.5+
      APIs.)
    
    Bug Fixes
    * Fix ``ZstdDecompressor.__init__`` on 64-bit big-endian systems.
    * Fix memory leak in ``ZstdDecompressionReader.seek()``.
    
    Changes
    * CI transitioned to Azure Pipelines (from AppVeyor and Travis CI).
    * Switched to ``pytest`` for running tests (from ``nose``).
    * Bundled zstandard library upgraded from 1.3.8 to 1.4.3.

--- archivers/py-zstandard/patches/patch-zstd.c
+++ archivers/py-zstandard/patches/patch-zstd.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-zstd.c,v 1.2 2019/02/26 06:42:27 adam Exp $
+$NetBSD: patch-zstd.c,v 1.3 2019/09/19 16:50:57 adam Exp $
 
 Allow newer zstd.
 
---- zstd.c.orig	2019-02-26 00:17:23.000000000 +0000
+--- zstd.c.orig	2019-09-16 01:27:27.000000000 +0000
 +++ zstd.c
 @@ -210,10 +210,6 @@ void zstd_module_init(PyObject* m) {
  	   We detect this mismatch here and refuse to load the module if this
  	   scenario is detected.
  	*/
--	if (ZSTD_VERSION_NUMBER != 10308 || ZSTD_versionNumber() != 10308) {
+-	if (ZSTD_VERSION_NUMBER != 10403 || ZSTD_versionNumber() != 10403) {
 -		PyErr_SetString(PyExc_ImportError, "zstd C API mismatch; Python bindings not compiled against expected zstd version");
 -		return;
 -	}
