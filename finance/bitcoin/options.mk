55b8a9f27bd6e966731ea377962156288198b996
adam <adam@pkgsrc.org>
2019-09-20 16:57:28 +0000

    bitcoin: updated to 0.18.1
    0.18.1
    
    P2P protocol and network code
    - Add tests and documentation for blocksonly
    - Avoid logging transaction decode errors to stderr
    - fix: tor: Call `event_base_loopbreak` from the event's callback
    - Make poll in InterruptibleRecv only filter for POLLIN events
    
    Wallet
    - Add -ignorepartialspends to list of ignored wallet options
    
    RPC and other APIs
    - Bugfix: fix pruneblockchain returned prune height
    - Document iswitness flag and fix bug in converttopsbt
    - Ensure that uncompressed public keys in a multisig always returns a legacy address
    - Disallow extended encoding for non-witness transactions
    - add 2nd arg to signrawtransactionwithkey examples
    - signrawtransactionwithkey: report error when missing redeemScript/witnessScript
    
    GUI
    - fix the bug of OPEN CONFIGURATION FILE on Mac
    - Show "No wallets available" in open menu instead of nothing
    - Enable open wallet menu on setWalletController
    - Set progressDialog to nullptr
    - Fix open wallet menu initialization order
    - Set `AA_EnableHighDpiScaling` attribute early
    - Enable console line edit on setClientModel
    - Assert QMetaObject::invokeMethod result
    
    Build system
    - Add test for GCC bug 90348
    - Install bitcoin-wallet manpage
    - build with -fstack-reuse=none
    
    Tests and QA
    - Pure python EC
    - Add test for superfluous witness record in deserialization
    - Bugfix: test/functional/rpc_psbt: Remove check for specific error message that depends on uncertain assumptions
    - Add test that addmultisigaddress fails for watchonly addresses
    
    Documentation
    - Remove text about txes always relayed from -whitelist
    
    Miscellaneous
    - Catch by reference not value in wallettool
    - Replace fprintf with tfm::format

--- finance/bitcoin/options.mk
+++ finance/bitcoin/options.mk
@@ -1,24 +1,29 @@
-# $NetBSD: options.mk,v 1.2 2019/05/13 08:32:56 khorben Exp $
+# $NetBSD: options.mk,v 1.3 2019/09/20 16:57:28 adam Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.bitcoin
-PKG_SUPPORTED_OPTIONS+=		qt5 miniupnpc
+PKG_SUPPORTED_OPTIONS+=		qt5 upnp
 # qt5 is off because it doubles the footprint of the package.
 # Please do not enable it by default; instead, create a split
 # package.
-PKG_SUGGESTED_OPTIONS+=
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mqt5)
-PLIST.qt=	yes
+PLIST_VARS+=	qt
 
+.if !empty(PKG_OPTIONS:Mqt5)
+CONFIGURE_ARGS+=	--with-gui=qt5
+CONFIGURE_ARGS+=	--with-qt-bindir=${QTDIR}/bin
+PLIST.qt=	yes
 .include "../../converters/qrencode/buildlink3.mk"
 .include "../../devel/protobuf/buildlink3.mk"
 .include "../../x11/qt5-qttools/buildlink3.mk"
-
-CONFIGURE_ARGS+=	--with-qt-bindir=${QTDIR}/bin
+.else
+CONFIGURE_ARGS+=	--with-gui=no
 .endif
 
-.if !empty(PKG_OPTIONS:Mminiupnpc)
+.if !empty(PKG_OPTIONS:Mupnp)
+CONFIGURE_ARGS+=	--with-miniupnpc
 .include "../../net/miniupnpc/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-miniupnpc
 .endif
