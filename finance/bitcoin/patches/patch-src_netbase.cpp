9f5abf01ea10c4ce0d783983f97273d2f98ccc73
adam <adam@pkgsrc.org>
2019-09-20 16:57:28 +0000

    bitcoin: updated to 0.18.1
    0.18.1
    
    P2P protocol and network code
    - Add tests and documentation for blocksonly
    - Avoid logging transaction decode errors to stderr
    - fix: tor: Call `event_base_loopbreak` from the event's callback
    - Make poll in InterruptibleRecv only filter for POLLIN events
    
    Wallet
    - Add -ignorepartialspends to list of ignored wallet options
    
    RPC and other APIs
    - Bugfix: fix pruneblockchain returned prune height
    - Document iswitness flag and fix bug in converttopsbt
    - Ensure that uncompressed public keys in a multisig always returns a legacy address
    - Disallow extended encoding for non-witness transactions
    - add 2nd arg to signrawtransactionwithkey examples
    - signrawtransactionwithkey: report error when missing redeemScript/witnessScript
    
    GUI
    - fix the bug of OPEN CONFIGURATION FILE on Mac
    - Show "No wallets available" in open menu instead of nothing
    - Enable open wallet menu on setWalletController
    - Set progressDialog to nullptr
    - Fix open wallet menu initialization order
    - Set `AA_EnableHighDpiScaling` attribute early
    - Enable console line edit on setClientModel
    - Assert QMetaObject::invokeMethod result
    
    Build system
    - Add test for GCC bug 90348
    - Install bitcoin-wallet manpage
    - build with -fstack-reuse=none
    
    Tests and QA
    - Pure python EC
    - Add test for superfluous witness record in deserialization
    - Bugfix: test/functional/rpc_psbt: Remove check for specific error message that depends on uncertain assumptions
    - Add test that addmultisigaddress fails for watchonly addresses
    
    Documentation
    - Remove text about txes always relayed from -whitelist
    
    Miscellaneous
    - Catch by reference not value in wallettool
    - Replace fprintf with tfm::format

--- finance/bitcoin/patches/patch-src_netbase.cpp
+++ finance/bitcoin/patches/patch-src_netbase.cpp
@@ -1,17 +1,20 @@
-$NetBSD: patch-src_netbase.cpp,v 1.1 2018/07/08 11:13:46 khorben Exp $
+$NetBSD: patch-src_netbase.cpp,v 1.2 2019/09/20 16:57:28 adam Exp $
 
 It is unclear why this patch exists.  NetBSD 6 and 7 have AI_ADDRCONFIG,
 although NetBSD 5 does not.  Arguably upstream should have an autoconf
 test instead.  This is not known to be reported upstream.
 
---- src/netbase.cpp.orig	2017-04-20 09:28:25.000000000 +0000
+--- src/netbase.cpp.orig	2019-09-20 11:57:52.000000000 +0000
 +++ src/netbase.cpp
-@@ -95,7 +95,7 @@ bool static LookupIntern(const char *psz
+@@ -80,7 +80,11 @@ bool static LookupIntern(const char *psz
      aiHint.ai_socktype = SOCK_STREAM;
      aiHint.ai_protocol = IPPROTO_TCP;
      aiHint.ai_family = AF_UNSPEC;
--#ifdef WIN32
-+#ifndef AI_ADDRCONFIG
-     aiHint.ai_flags = fAllowLookup ? 0 : AI_NUMERICHOST;
- #else
++#ifdef AI_ADDRCONFIG
      aiHint.ai_flags = fAllowLookup ? AI_ADDRCONFIG : AI_NUMERICHOST;
++#else
++    aiHint.ai_flags = fAllowLookup ? 0 : AI_NUMERICHOST;
++#endif
+     struct addrinfo *aiRes = nullptr;
+     int nErr = getaddrinfo(pszName, nullptr, &aiHint, &aiRes);
+     if (nErr)
