3709b10d493dbea6f02ebcfedab4494f02057dc0
bsiegert <bsiegert@pkgsrc.org>
2019-09-20 19:23:33 +0000

    Pullup ticket #6056 - requested by maya devel/glibmm: build fix
    Revisions pulled up:
    - devel/glibmm/buildlink3.mk                                    1.18
    
    ---
       Module Name:	pkgsrc
       Committed By:	maya
       Date:		Mon Sep  9 11:40:16 UTC 2019
    
       Modified Files:
       	pkgsrc/devel/glibmm: buildlink3.mk
    
       Log Message:
       glibmm: sync GCC_REQD between bl3, Makefile.
    
       Fixes atkmm builds with GCC < 4.9

--- devel/glibmm/buildlink3.mk
+++ devel/glibmm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2017/12/30 17:12:51 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.17.12.1 2019/09/20 19:23:33 bsiegert Exp $
 
 BUILDLINK_TREE+=	glibmm
 
@@ -8,6 +8,8 @@ GLIBMM_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.glibmm+=	glibmm>=2.24.2
 BUILDLINK_ABI_DEPENDS.glibmm+=	glibmm>=2.32.1nb1
 BUILDLINK_PKGSRCDIR.glibmm?=	../../devel/glibmm
+
+GCC_REQD+=	4.9
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/libsigc++/buildlink3.mk"
