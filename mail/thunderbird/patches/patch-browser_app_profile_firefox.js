4ec2ba02501295c0650fa2f6cafa4593b5bff8b0
ryoon <ryoon@pkgsrc.org>
2019-09-21 10:55:16 +0000

    Update to 68.1.0
    Changelog:
        new
        Offer to configure Exchange accounts for Office365. A third-party add-on is required for this account type. IMAP still exists as alternative.
    
        fixed
        Edit tag not working
    
        fixed
        Write window: "Insert > Characters and Symbols" not working
    
        fixed
        Moving/dragging messages from "Search Messages" result dialog not working
    
        fixed
        Command line -compose "attachment=" not working
    
        fixed
        Custom views not working
    
        fixed
        Issues with list of content types/actions for incoming attachments
    
        fixed
        "Learn More" links in Error Console not working
    
        fixed
        Visual glitches: Quick Filter Bar tag buttons too tall, missing scroll
          bar on Connection Setting subdialog, LDAP server selection after "New",
          "Edit" and "Delete"
    
        fixed
        Calendar: Parts of CalDAV dialog not working
    
        fixed
        Various security fixes
    
    Security fixes:
    CVE-2019-11739: Covert Content Attack on S/MIME encryption using a crafted multipart/alternative message
    #CVE-2019-11746: Use-after-free while manipulating video
    #CVE-2019-11744: XSS by breaking out of title and textarea elements using innerHTML
    #CVE-2019-11742: Same-origin policy violation with SVG filters and canvas to steal cross-origin images
    #CVE-2019-11752: Use-after-free while extracting a key value in IndexedDB
    #CVE-2019-11743: Cross-origin access to unload event attributes
    #CVE-2019-11740: Memory safety bugs fixed in Firefox 69, Firefox ESR 68.1, Firefox ESR 60.9, Thunderbird 68.1, and Thunderbird 60.9

--- mail/thunderbird/patches/patch-browser_app_profile_firefox.js
+++ mail/thunderbird/patches/patch-browser_app_profile_firefox.js
@@ -1,14 +1,20 @@
-$NetBSD: patch-browser_app_profile_firefox.js,v 1.1 2018/12/16 08:12:15 ryoon Exp $
+$NetBSD: patch-browser_app_profile_firefox.js,v 1.2 2019/09/21 10:55:17 ryoon Exp $
 
---- browser/app/profile/firefox.js.orig	2018-12-04 23:11:44.000000000 +0000
+--- browser/app/profile/firefox.js.orig	2019-09-09 23:43:23.000000000 +0000
 +++ browser/app/profile/firefox.js
-@@ -1748,3 +1748,9 @@ pref("app.shield.optoutstudies.enabled",
- #else
- pref("app.shield.optoutstudies.enabled", false);
- #endif
-+
+@@ -1862,6 +1862,15 @@ pref("prio.publicKeyB", "26E6674E65425B8
+ pref("toolkit.coverage.enabled", false);
+ pref("toolkit.coverage.endpoint.base", "https://coverage.mozilla.org");
+ 
 +// Select UI locale from LANG/LC_MESSAGE environmental variables
 +pref("intl.locale.requested", "");
 +
++// Enable system addons, for example langpacks from www/firefox-l10n
++pref("extensions.autoDisableScopes", 11);
++
 +// Disable multiprocess window support. Workaround for PR 53273.
 +pref("browser.tabs.remote.autostart", false);
++
+ // Discovery prefs
+ pref("browser.discovery.enabled", true);
+ pref("browser.discovery.containers.enabled", true);
--- mail/thunderbird/patches/patch-xpcom_build_BinaryPath.h
+++ mail/thunderbird/patches/patch-xpcom_build_BinaryPath.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-xpcom_build_BinaryPath.h,v 1.2 2019/02/26 11:32:13 ryoon Exp $
+$NetBSD: patch-xpcom_build_BinaryPath.h,v 1.3 2019/09/21 10:55:17 ryoon Exp $
 
---- xpcom/build/BinaryPath.h.orig	2019-02-13 14:19:45.000000000 +0000
+--- xpcom/build/BinaryPath.h.orig	2019-09-09 23:43:45.000000000 +0000
 +++ xpcom/build/BinaryPath.h
 @@ -21,7 +21,8 @@
      defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__OpenBSD__)
- #include <sys/sysctl.h>
+ #  include <sys/sysctl.h>
  #endif
 -#if defined(__OpenBSD__)
 +#if defined(__OpenBSD__) || \
 +    (defined(__NetBSD__) && !defined(KERN_PROC_PATHNAME))
- #include <sys/stat.h>
+ #  include <sys/stat.h>
  #endif
  #include "mozilla/UniquePtr.h"
 @@ -164,7 +165,8 @@ class BinaryPath {
@@ -26,13 +26,13 @@ $NetBSD: patch-xpcom_build_BinaryPath.h,v 1.2 2019/02/26 11:32:13 ryoon Exp $
      return NS_ERROR_FAILURE;
    }
  
-++#elif (defined(__NetBSD__) && !defined(KERN_PROC_PATHNAME))
++#elif (defined(__NetBSD__) && !defined(KERN_PROC_PATHNAME))
 +  static nsresult Get(char aResult[MAXPATHLEN])
 +  {
-+    char path[] = "@PREFIX@/lib/firefox60/firefox60";
++    char path[] = "@PREFIX@/lib/firefox/firefox";
 +    sprintf(aResult, "%s", path);
 +    return NS_OK;
 +  }
  #else
- #error Oops, you need platform-specific code here
+ #  error Oops, you need platform-specific code here
  #endif
