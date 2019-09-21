2ddea93e3e105bfe904b92442341f77acc180e80
ryoon <ryoon@pkgsrc.org>
2019-09-21 10:55:16 +0000

    Update to 68.1.0
    Changelog:
        new
        Offer to configure Exchange accounts for Office365. A third-party add-on is required for this account type. IMAP still exists as alternative.
    
        fixed
        Edit tag not working
    
        fixed
        Write window: "Insert > Characters and Symbols" not working
    
        fixed
        Moving/dragging messages from "Search Messages" result dialog not working
    
        fixed
        Command line -compose "attachment=" not working
    
        fixed
        Custom views not working
    
        fixed
        Issues with list of content types/actions for incoming attachments
    
        fixed
        "Learn More" links in Error Console not working
    
        fixed
        Visual glitches: Quick Filter Bar tag buttons too tall, missing scroll
          bar on Connection Setting subdialog, LDAP server selection after "New",
          "Edit" and "Delete"
    
        fixed
        Calendar: Parts of CalDAV dialog not working
    
        fixed
        Various security fixes
    
    Security fixes:
    CVE-2019-11739: Covert Content Attack on S/MIME encryption using a crafted multipart/alternative message
    #CVE-2019-11746: Use-after-free while manipulating video
    #CVE-2019-11744: XSS by breaking out of title and textarea elements using innerHTML
    #CVE-2019-11742: Same-origin policy violation with SVG filters and canvas to steal cross-origin images
    #CVE-2019-11752: Use-after-free while extracting a key value in IndexedDB
    #CVE-2019-11743: Cross-origin access to unload event attributes
    #CVE-2019-11740: Memory safety bugs fixed in Firefox 69, Firefox ESR 68.1, Firefox ESR 60.9, Thunderbird 68.1, and Thunderbird 60.9

--- mail/thunderbird/patches/patch-toolkit_xre_glxtest.cpp
+++ mail/thunderbird/patches/patch-toolkit_xre_glxtest.cpp
@@ -1,13 +1,13 @@
-$NetBSD: patch-toolkit_xre_glxtest.cpp,v 1.2 2019/02/26 11:32:13 ryoon Exp $
+$NetBSD: patch-toolkit_xre_glxtest.cpp,v 1.3 2019/09/21 10:55:17 ryoon Exp $
 
---- toolkit/xre/glxtest.cpp.orig	2019-02-13 14:19:45.000000000 +0000
+--- toolkit/xre/glxtest.cpp.orig	2019-09-09 23:43:45.000000000 +0000
 +++ toolkit/xre/glxtest.cpp
-@@ -116,7 +116,7 @@ void glxtest() {
+@@ -132,7 +132,7 @@ void glxtest() {
          "The MOZ_AVOID_OPENGL_ALTOGETHER environment variable is defined");
  
      ///// Open libGL and load needed symbols /////
 -#ifdef __OpenBSD__
 +#if defined(__OpenBSD__) || defined(__NetBSD__)
- #define LIBGL_FILENAME "libGL.so"
+ #  define LIBGL_FILENAME "libGL.so"
  #else
- #define LIBGL_FILENAME "libGL.so.1"
+ #  define LIBGL_FILENAME "libGL.so.1"
