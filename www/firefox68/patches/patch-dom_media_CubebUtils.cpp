16c994bacf98f78c5c6505be3779448e6278f33e
ryoon <ryoon@pkgsrc.org>
2019-09-21 07:31:43 +0000

    www/firefox68: import firefox68-68.1.0
    Mozilla Firefox is a free, open-source and cross-platform web browser
    for Windows, Linux, MacOS X and many other operating systems.
    
    It is fast and easy to use, and offers many advantages over other web
    browsers, such as tabbed browsing and the ability to block pop-up
    windows.
    
    Firefox also offers excellent bookmark and history management, and it
    can be extended by developers using industry standards such as XML,
    CSS, JavaScript, C++, etc. Many extensions are available.
    
    This package provides Firefox 68 ESR.

---
+++ www/firefox68/patches/patch-dom_media_CubebUtils.cpp
@@ -0,0 +1,13 @@
+$NetBSD: patch-dom_media_CubebUtils.cpp,v 1.1 2019/09/21 07:31:44 ryoon Exp $
+
+--- dom/media/CubebUtils.cpp.orig	2019-07-06 01:48:31.000000000 +0000
++++ dom/media/CubebUtils.cpp
+@@ -163,7 +163,7 @@ const char kBrandBundleURL[] = "chrome:/
+ 
+ const char* AUDIOSTREAM_BACKEND_ID_STR[] = {
+     "jack",  "pulse",       "alsa",  "audiounit", "audioqueue", "wasapi",
+-    "winmm", "directsound", "sndio", "opensl",    "audiotrack", "kai"};
++    "winmm", "directsound", "sndio", "opensl",    "audiotrack", "kai", "oss", "sun"};
+ /* Index for failures to create an audio stream the first time. */
+ const int CUBEB_BACKEND_INIT_FAILURE_FIRST =
+     ArrayLength(AUDIOSTREAM_BACKEND_ID_STR);
