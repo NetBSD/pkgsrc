a350e65b3cceb1f92018812e2018ea5efad091e4
ryoon <ryoon@pkgsrc.org>
2019-09-21 10:55:16 +0000

    Update to 68.1.0
    Changelog:
        new
        Offer to configure Exchange accounts for Office365. A third-party add-on is required for this account type. IMAP still exists as alternative.
    
        fixed
        Edit tag not working
    
        fixed
        Write window: "Insert > Characters and Symbols" not working
    
        fixed
        Moving/dragging messages from "Search Messages" result dialog not working
    
        fixed
        Command line -compose "attachment=" not working
    
        fixed
        Custom views not working
    
        fixed
        Issues with list of content types/actions for incoming attachments
    
        fixed
        "Learn More" links in Error Console not working
    
        fixed
        Visual glitches: Quick Filter Bar tag buttons too tall, missing scroll
          bar on Connection Setting subdialog, LDAP server selection after "New",
          "Edit" and "Delete"
    
        fixed
        Calendar: Parts of CalDAV dialog not working
    
        fixed
        Various security fixes
    
    Security fixes:
    CVE-2019-11739: Covert Content Attack on S/MIME encryption using a crafted multipart/alternative message
    #CVE-2019-11746: Use-after-free while manipulating video
    #CVE-2019-11744: XSS by breaking out of title and textarea elements using innerHTML
    #CVE-2019-11742: Same-origin policy violation with SVG filters and canvas to steal cross-origin images
    #CVE-2019-11752: Use-after-free while extracting a key value in IndexedDB
    #CVE-2019-11743: Cross-origin access to unload event attributes
    #CVE-2019-11740: Memory safety bugs fixed in Firefox 69, Firefox ESR 68.1, Firefox ESR 60.9, Thunderbird 68.1, and Thunderbird 60.9

--- mail/thunderbird/patches/patch-media_libcubeb_update.sh
+++ mail/thunderbird/patches/patch-media_libcubeb_update.sh
@@ -1,12 +1,13 @@
-$NetBSD: patch-media_libcubeb_update.sh,v 1.1 2018/12/16 08:12:16 ryoon Exp $
+$NetBSD: patch-media_libcubeb_update.sh,v 1.2 2019/09/21 10:55:17 ryoon Exp $
 
---- media/libcubeb/update.sh.orig	2018-12-04 23:11:53.000000000 +0000
+--- media/libcubeb/update.sh.orig	2019-09-09 23:43:34.000000000 +0000
 +++ media/libcubeb/update.sh
-@@ -20,6 +20,7 @@ cp $1/src/cubeb_log.h src
+@@ -25,6 +25,8 @@ cp $1/src/cubeb_log.h src
  cp $1/src/cubeb_mixer.cpp src
  cp $1/src/cubeb_mixer.h src
  cp $1/src/cubeb_opensl.c src
 +cp $1/src/cubeb_oss.c src
++cp $1/src/cubeb_sun.c src
  cp $1/src/cubeb-jni.cpp src
  cp $1/src/cubeb-jni.h src
  cp $1/src/android/cubeb-output-latency.h src/android
