f019c8c05120ad7f3602c0f06c51017d5e67c3ce
ryoon <ryoon@pkgsrc.org>
2019-09-21 07:31:43 +0000

    www/firefox68: import firefox68-68.1.0
    Mozilla Firefox is a free, open-source and cross-platform web browser
    for Windows, Linux, MacOS X and many other operating systems.
    
    It is fast and easy to use, and offers many advantages over other web
    browsers, such as tabbed browsing and the ability to block pop-up
    windows.
    
    Firefox also offers excellent bookmark and history management, and it
    can be extended by developers using industry standards such as XML,
    CSS, JavaScript, C++, etc. Many extensions are available.
    
    This package provides Firefox 68 ESR.

---
+++ www/firefox68/patches/patch-browser_app_profile_firefox.js
@@ -0,0 +1,20 @@
+$NetBSD: patch-browser_app_profile_firefox.js,v 1.1 2019/09/21 07:31:44 ryoon Exp $
+
+--- browser/app/profile/firefox.js.orig	2019-07-06 01:48:29.000000000 +0000
++++ browser/app/profile/firefox.js
+@@ -1865,6 +1865,15 @@ pref("prio.publicKeyB", "26E6674E65425B8
+ pref("toolkit.coverage.enabled", false);
+ pref("toolkit.coverage.endpoint.base", "https://coverage.mozilla.org");
+ 
++// Select UI locale from LANG/LC_MESSAGE environmental variables
++pref("intl.locale.requested", "");
++
++// Enable system addons, for example langpacks from www/firefox-l10n
++pref("extensions.autoDisableScopes", 11);
++
++// Disable multiprocess window support. Workaround for PR 53273.
++pref("browser.tabs.remote.autostart", false);
++
+ // Discovery prefs
+ pref("browser.discovery.enabled", true);
+ pref("browser.discovery.containers.enabled", true);
