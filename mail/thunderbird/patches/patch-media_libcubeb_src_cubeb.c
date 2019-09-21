fb6104efff43a0e24ceede844ad632ae2e54e227
ryoon <ryoon@pkgsrc.org>
2019-09-21 10:55:16 +0000

    Update to 68.1.0
    Changelog:
        new
        Offer to configure Exchange accounts for Office365. A third-party add-on is required for this account type. IMAP still exists as alternative.
    
        fixed
        Edit tag not working
    
        fixed
        Write window: "Insert > Characters and Symbols" not working
    
        fixed
        Moving/dragging messages from "Search Messages" result dialog not working
    
        fixed
        Command line -compose "attachment=" not working
    
        fixed
        Custom views not working
    
        fixed
        Issues with list of content types/actions for incoming attachments
    
        fixed
        "Learn More" links in Error Console not working
    
        fixed
        Visual glitches: Quick Filter Bar tag buttons too tall, missing scroll
          bar on Connection Setting subdialog, LDAP server selection after "New",
          "Edit" and "Delete"
    
        fixed
        Calendar: Parts of CalDAV dialog not working
    
        fixed
        Various security fixes
    
    Security fixes:
    CVE-2019-11739: Covert Content Attack on S/MIME encryption using a crafted multipart/alternative message
    #CVE-2019-11746: Use-after-free while manipulating video
    #CVE-2019-11744: XSS by breaking out of title and textarea elements using innerHTML
    #CVE-2019-11742: Same-origin policy violation with SVG filters and canvas to steal cross-origin images
    #CVE-2019-11752: Use-after-free while extracting a key value in IndexedDB
    #CVE-2019-11743: Cross-origin access to unload event attributes
    #CVE-2019-11740: Memory safety bugs fixed in Firefox 69, Firefox ESR 68.1, Firefox ESR 60.9, Thunderbird 68.1, and Thunderbird 60.9

--- mail/thunderbird/patches/patch-media_libcubeb_src_cubeb.c
+++ mail/thunderbird/patches/patch-media_libcubeb_src_cubeb.c
@@ -1,8 +1,18 @@
-$NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.1 2018/12/16 08:12:16 ryoon Exp $
+$NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.2 2019/09/21 10:55:17 ryoon Exp $
 
---- media/libcubeb/src/cubeb.c.orig	2018-12-04 23:11:52.000000000 +0000
+--- media/libcubeb/src/cubeb.c.orig	2019-09-09 23:43:34.000000000 +0000
 +++ media/libcubeb/src/cubeb.c
-@@ -60,6 +60,9 @@ int audiotrack_init(cubeb ** context, ch
+@@ -51,6 +51,9 @@ int wasapi_init(cubeb ** context, char c
+ #if defined(USE_SNDIO)
+ int sndio_init(cubeb ** context, char const * context_name);
+ #endif
++#if defined(USE_SUN)
++int sun_init(cubeb ** context, char const * context_name);
++#endif
+ #if defined(USE_OPENSL)
+ int opensl_init(cubeb ** context, char const * context_name);
+ #endif
+@@ -60,6 +63,9 @@ int audiotrack_init(cubeb ** context, ch
  #if defined(USE_KAI)
  int kai_init(cubeb ** context, char const * context_name);
  #endif
@@ -12,7 +22,7 @@ $NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.1 2018/12/16 08:12:16 ryoon Exp $
  
  static int
  validate_stream_params(cubeb_stream_params * input_stream_params,
-@@ -160,6 +163,10 @@ cubeb_init(cubeb ** context, char const 
+@@ -160,6 +166,10 @@ cubeb_init(cubeb ** context, char const 
  #if defined(USE_KAI)
        init_oneshot = kai_init;
  #endif
@@ -23,7 +33,17 @@ $NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.1 2018/12/16 08:12:16 ryoon Exp $
      } else {
        /* Already set */
      }
-@@ -204,6 +211,9 @@ cubeb_init(cubeb ** context, char const 
+@@ -195,6 +205,9 @@ cubeb_init(cubeb ** context, char const 
+ #if defined(USE_SNDIO)
+     sndio_init,
+ #endif
++#if defined(USE_SUN)
++    sun_init,
++#endif
+ #if defined(USE_OPENSL)
+     opensl_init,
+ #endif
+@@ -204,6 +217,9 @@ cubeb_init(cubeb ** context, char const 
  #if defined(USE_KAI)
      kai_init,
  #endif
