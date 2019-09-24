80c43d94e25e7e1786651c24c58ac26943bb1ce1
adam <adam@pkgsrc.org>
2019-09-24 09:04:08 +0000

    py-pyinotify: allow building on platform other than Linux, FreeBSD, and NetBSD; clean-up

--- sysutils/py-pyinotify/patches/patch-setup.py
+++ sysutils/py-pyinotify/patches/patch-setup.py
@@ -1,19 +1,21 @@
-$NetBSD: patch-setup.py,v 1.1 2018/06/12 01:00:56 youri Exp $
+$NetBSD: patch-setup.py,v 1.2 2019/09/24 09:04:08 adam Exp $
 
 Add NetBSD.
 
 --- setup.py.orig	2015-06-04 15:28:48.000000000 +0000
 +++ setup.py
-@@ -25,7 +25,7 @@ if sys.version_info < (2, 4):
+@@ -24,10 +24,6 @@ if sys.version_info < (2, 4):
+     sys.stderr.write('This module requires at least Python 2.4\n')
      sys.exit(1)
  
- # check linux platform
+-# check linux platform
 -if not platform.startswith('linux') and not platform.startswith('freebsd'):
-+if not platform.startswith('linux') and not platform.startswith('freebsd') and not platform.startswith('netbsd'):
-     sys.stderr.write("inotify is not available on %s\n" % platform)
-     sys.exit(1)
+-    sys.stderr.write("inotify is not available on %s\n" % platform)
+-    sys.exit(1)
  
-@@ -71,7 +71,7 @@ def should_compile_ext_mod():
+ 
+ classif = [
+@@ -71,7 +67,7 @@ def should_compile_ext_mod():
          return True
  
      try_libc_name = 'c'
