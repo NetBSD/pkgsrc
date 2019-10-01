685463b9ea85f8e677705c45da04bfc0d610c574
nia <nia@pkgsrc.org>
2019-10-01 12:43:04 +0000

    Add audio/qmmp.
    qmmp is an audio player. The default user interface is similar to Winamp/XMMP.
    An alternative user interface based on a standard widget set is also available.

---
+++ audio/qmmp/options.mk
@@ -0,0 +1,57 @@
+# $NetBSD: options.mk,v 1.1 2019/10/01 12:43:04 nia Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.qmmp
+PKG_SUPPORTED_OPTIONS+=		alsa jack pulseaudio
+PKG_SUPPORTED_OPTIONS+=		ffmpeg musepack wavpack
+PKG_SUGGESTED_OPTIONS+=		ffmpeg
+PKG_SUGGESTED_OPTIONS.Linux+=	alsa
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	alsa
+.if !empty(PKG_OPTIONS:Malsa)
+PLIST.alsa=	yes
+.  include "../../audio/alsa-lib/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DUSE_ALSA=OFF
+.endif
+
+PLIST_VARS+=	pulse
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+PLIST.pulse=	yes
+.  include "../../audio/pulseaudio/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DUSE_PULSE=OFF
+.endif
+
+PLIST_VARS+=	jack
+.if !empty(PKG_OPTIONS:Mjack)
+PLIST.jack=	yes
+.  include "../../audio/jack/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DUSE_JACK=OFF
+.endif
+
+PLIST_VARS+=	ffmpeg
+.if !empty(PKG_OPTIONS:Mffmpeg)
+PLIST.ffmpeg=	yes
+.  include "../../multimedia/ffmpeg4/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DUSE_FFMPEG=OFF
+.endif
+
+PLIST_VARS+=	mpc
+.if !empty(PKG_OPTIONS:Mmusepack)
+PLIST.mpc=	yes
+.  include "../../audio/musepack/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DUSE_MPC=OFF
+.endif
+
+PLIST_VARS+=	wv
+.if !empty(PKG_OPTIONS:Mwavpack)
+PLIST.wv=	yes
+.  include "../../audio/wavpack/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DUSE_WAVPACK=OFF
+.endif
