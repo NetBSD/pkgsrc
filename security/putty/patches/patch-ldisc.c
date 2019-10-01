846f075510112f5aa941d2fb0472bb42a19dcccc
ryoon <ryoon@pkgsrc.org>
2019-10-01 15:22:41 +0000

    Update to 0.73
    Changelog:
    Vulnerabilities fixed in this release include:
    
     - On Windows, the listening sockets used for local port forwarding
       were opened in a mode that did not prevent other processes from
       also listening on the same ports and stealing some of the incoming
       connections.
    
     - In the PuTTY terminal, bracketed paste mode was broken in 0.72, in
       a way that made the pasted data look like manual keyboard input. So
       any application relying on the bracketing sequences to protect
       against malicious clipboard contents would have been misled.
    
     - An SSH-1 server could trigger an access to freed memory by sending
       the SSH1_MSG_DISCONNECT message. Not known to be exploitable.
    
    Other bug fixes include:
    
     - Windows Plink no longer crashes on startup when it tries to tell
       you it's reusing an existing SSH connection.
    
     - Windows PuTTY now updates its terminal window size correctly if the
       screen resolution changes while it's maximised.
    
     - If you display the coloured error messages from gcc in the PuTTY
       terminal, there is no longer a missing character if a colour change
       happens exactly at the end of a line.
    
     - If you use the 'Clear Scrollback' menu option or escape sequence
       while text in the scrollback is selected, it no longer causes an
       assertion failure.

--- security/putty/patches/patch-ldisc.c
+++ security/putty/patches/patch-ldisc.c
@@ -1,33 +1,33 @@
-$NetBSD: patch-ldisc.c,v 1.2 2019/04/01 12:10:43 ryoon Exp $
+$NetBSD: patch-ldisc.c,v 1.3 2019/10/01 15:22:41 ryoon Exp $
 
 pwrite is a standard system call
 
---- ldisc.c.orig	2019-03-16 12:26:34.000000000 +0000
+--- ldisc.c.orig	2019-09-22 09:14:48.000000000 +0000
 +++ ldisc.c
-@@ -42,7 +42,7 @@ static int plen(Ldisc *ldisc, unsigned c
- 	return 4;		       /* <XY> hex representation */
+@@ -40,7 +40,7 @@ static int plen(Ldisc *ldisc, unsigned c
+         return 4;                      /* <XY> hex representation */
  }
  
 -static void pwrite(Ldisc *ldisc, unsigned char c)
 +static void pwrite_(Ldisc *ldisc, unsigned char c)
  {
      if ((c >= 32 && c <= 126) ||
- 	(!in_utf(ldisc->term) && c >= 0xA0) ||
-@@ -229,7 +229,7 @@ void ldisc_send(Ldisc *ldisc, const void
- 		    int i;
- 		    c_write(ldisc, "^R\r\n", 4);
- 		    for (i = 0; i < ldisc->buflen; i++)
--			pwrite(ldisc, ldisc->buf[i]);
-+			pwrite_(ldisc, ldisc->buf[i]);
- 		}
- 		break;
- 	      case CTRL('V'):	       /* quote next char */
-@@ -294,7 +294,7 @@ void ldisc_send(Ldisc *ldisc, const void
+         (!in_utf(ldisc->term) && c >= 0xA0) ||
+@@ -227,7 +227,7 @@ void ldisc_send(Ldisc *ldisc, const void
+                     int i;
+                     c_write(ldisc, "^R\r\n", 4);
+                     for (i = 0; i < ldisc->buflen; i++)
+-                        pwrite(ldisc, ldisc->buf[i]);
++                        pwrite_(ldisc, ldisc->buf[i]);
+                 }
+                 break;
+               case CTRL('V'):          /* quote next char */
+@@ -292,7 +292,7 @@ void ldisc_send(Ldisc *ldisc, const void
                  sgrowarray(ldisc->buf, ldisc->bufsiz, ldisc->buflen);
- 		ldisc->buf[ldisc->buflen++] = c;
- 		if (ECHOING)
--		    pwrite(ldisc, (unsigned char) c);
-+		    pwrite_(ldisc, (unsigned char) c);
- 		ldisc->quotenext = false;
- 		break;
- 	    }
+                 ldisc->buf[ldisc->buflen++] = c;
+                 if (ECHOING)
+-                    pwrite(ldisc, (unsigned char) c);
++                    pwrite_(ldisc, (unsigned char) c);
+                 ldisc->quotenext = false;
+                 break;
+             }
--- security/putty/patches/patch-timing.c
+++ security/putty/patches/patch-timing.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-timing.c,v 1.2 2013/08/07 11:06:39 drochner Exp $
+$NetBSD: patch-timing.c,v 1.3 2019/10/01 15:22:41 ryoon Exp $
 
---- timing.c.orig	2012-09-19 22:12:00.000000000 +0000
+--- timing.c.orig	2019-09-22 09:14:51.000000000 +0000
 +++ timing.c
 @@ -60,19 +60,10 @@ static int compare_timers(void *av, void
       * Failing that, compare on the other two fields, just so that
@@ -9,18 +9,18 @@ $NetBSD: patch-timing.c,v 1.2 2013/08/07 11:06:39 drochner Exp $
 -#if defined(__LCC__) || defined(__clang__)
 -    /* lcc won't let us compare function pointers. Legal, but annoying. */
 -    {
--	int c = memcmp(&a->fn, &b->fn, sizeof(a->fn));
--	if (c)
--	    return c;
+-        int c = memcmp(&a->fn, &b->fn, sizeof(a->fn));
+-        if (c)
+-            return c;
 -    }
--#else    
+-#else
 -    if (a->fn < b->fn)
 +    if ((uintptr_t)a->fn < (uintptr_t)b->fn)
- 	return -1;
+         return -1;
 -    else if (a->fn > b->fn)
 +    else if ((uintptr_t)a->fn > (uintptr_t)b->fn)
- 	return +1;
+         return +1;
 -#endif
  
      if (a->ctx < b->ctx)
- 	return -1;
+         return -1;
