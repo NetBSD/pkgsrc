82c523180dc8ca1fa1180e781a853ddf7fc0ae39
ryoon <ryoon@pkgsrc.org>
2019-10-01 15:22:41 +0000

    Update to 0.73
    Changelog:
    Vulnerabilities fixed in this release include:
    
     - On Windows, the listening sockets used for local port forwarding
       were opened in a mode that did not prevent other processes from
       also listening on the same ports and stealing some of the incoming
       connections.
    
     - In the PuTTY terminal, bracketed paste mode was broken in 0.72, in
       a way that made the pasted data look like manual keyboard input. So
       any application relying on the bracketing sequences to protect
       against malicious clipboard contents would have been misled.
    
     - An SSH-1 server could trigger an access to freed memory by sending
       the SSH1_MSG_DISCONNECT message. Not known to be exploitable.
    
    Other bug fixes include:
    
     - Windows Plink no longer crashes on startup when it tries to tell
       you it's reusing an existing SSH connection.
    
     - Windows PuTTY now updates its terminal window size correctly if the
       screen resolution changes while it's maximised.
    
     - If you display the coloured error messages from gcc in the PuTTY
       terminal, there is no longer a missing character if a colour change
       happens exactly at the end of a line.
    
     - If you use the 'Clear Scrollback' menu option or escape sequence
       while text in the scrollback is selected, it no longer causes an
       assertion failure.

--- security/putty/patches/patch-unix_uxucs.c
+++ security/putty/patches/patch-unix_uxucs.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-unix_uxucs.c,v 1.2 2013/08/07 11:06:39 drochner Exp $
+$NetBSD: patch-unix_uxucs.c,v 1.3 2019/10/01 15:22:41 ryoon Exp $
 
---- unix/uxucs.c.orig	2013-07-22 07:12:05.000000000 +0000
+--- unix/uxucs.c.orig	2019-09-22 09:14:52.000000000 +0000
 +++ unix/uxucs.c
-@@ -72,7 +72,7 @@ int wc_to_mb(int codepage, int flags, co
- 	memset(&state, 0, sizeof state);
+@@ -68,7 +68,7 @@ int wc_to_mb(int codepage, int flags, co
+         memset(&state, 0, sizeof state);
  
- 	while (wclen > 0) {
--	    int i = wcrtomb(output, wcstr[0], &state);
-+	    size_t i = wcrtomb(output, wcstr[0], &state);
- 	    if (i == (size_t)-1 || i > n - mblen)
- 		break;
- 	    memcpy(mbstr+n, output, i);
+         while (wclen > 0) {
+-            int i = wcrtomb(output, wcstr[0], &state);
++            size_t i = wcrtomb(output, wcstr[0], &state);
+             if (i == (size_t)-1 || i > n - mblen)
+                 break;
+             memcpy(mbstr+n, output, i);
