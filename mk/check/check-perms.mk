4e5092a845c078ad1a408056608bd1b0761893cb
rillig <rillig@pkgsrc.org>
2019-10-01 21:56:11 +0000

    mk/check/check-perms.mk: complete the _VARGROUPS section

--- mk/check/check-perms.mk
+++ mk/check/check-perms.mk
@@ -1,4 +1,4 @@
-# $NetBSD: check-perms.mk,v 1.20 2019/10/01 21:48:30 rillig Exp $
+# $NetBSD: check-perms.mk,v 1.21 2019/10/01 21:56:11 rillig Exp $
 #
 # This file checks that after installation of a package, all files and
 # directories of that package have sensible permissions set.
@@ -30,6 +30,9 @@
 _VARGROUPS+=		check-perms
 _USER_VARS.check-perms=	CHECK_PERMS
 _PKG_VARS.check-perms=	CHECK_PERMS_SKIP CHECK_PERMS_AUTOFIX
+_DEF_VARS.check-perms=	TOOL_DEPENDS
+_USE_VARS.check-perms=	PKG_DEVELOPER MACHINE_PLATFORM DESTDIR PREFIX
+_IGN_VARS.check-perms=	_* LOCALBASE PKGNAME HOST_PKG_INFO
 
 .if ${PKG_DEVELOPER:Uno} != "no"
 CHECK_PERMS?=		yes
