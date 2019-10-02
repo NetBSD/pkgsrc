6e448ee99a9ca51ff658694326eaa9009ec76ae4
taca <taca@pkgsrc.org>
2019-10-02 14:08:05 +0000

    lang/php73: update to 7.3.10
    Update lang/php73 to 7.3.10.
    
    pkgsrc changes
    
    * Clean two pkglint's warnings.
    
    26 Sep 2019, PHP 7.3.10
    
    - Core:
      . Fixed bug #78220 (Can't access OneDrive folder). (cmb, ab)
      . Fixed bug #77922 (Double release of doc comment on inherited shadow
        property). (Nikita)
      . Fixed bug #78441 (Parse error due to heredoc identifier followed by digit).
        (cmb)
      . Fixed bug #77812 (Interactive mode does not support PHP 7.3-style heredoc).
        (cmb, Nikita)
    
    - FastCGI:
      . Fixed bug #78469 (FastCGI on_accept hook is not called when using named
        pipes on Windows). (Sergei Turchanov)
    
    - FPM:
      . Fixed bug #78334 (fpm log prefix message includes wrong stdout/stderr
        notation). (Tsuyoshi Sadakata)
    
    - Intl:
      . Ensure IDNA2003 rules are used with idn_to_ascii() and idn_to_utf8()
        when requested. (Sara)
    
    - MBString:
      . Fixed bug #78559 (Heap buffer overflow in mb_eregi). (cmb)
    
    - MySQLnd:
      . Fixed connect_attr issues and added the _server_host connection attribute.
        (Qianqian Bu)
    
    - ODBC:
      . Fixed bug #78473 (odbc_close() closes arbitrary resources). (cmb)
    
    - PDO_MySQL:
      . Fixed bug #41997 (SP call yields additional empty result set). (cmb)
    
    - sodium:
      . Fixed bug #78510 (Partially uninitialized buffer returned by
        sodium_crypto_generichash_init()). (Frank Denis, cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.269 2019/10/02 14:05:22 taca Exp $
+# $NetBSD: phpversion.mk,v 1.270 2019/10/02 14:08:05 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP71_VERSION=	7.1.32
 PHP72_VERSION=	7.2.23
-PHP73_VERSION=	7.3.9
+PHP73_VERSION=	7.3.10
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
