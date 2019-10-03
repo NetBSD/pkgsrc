6ed88bed02099ffb81be23f48e20b5a4ab11af3e
agc <agc@pkgsrc.org>
2019-10-03 20:18:12 +0000

    Bump netpgpverify and libnetpgpverify to 20191003 - fix problem pointed out by mrg and gcc8:
    + don't read an automatic array element we haven't already written

--- security/netpgpverify/files/verify.h
+++ security/netpgpverify/files/verify.h
@@ -23,9 +23,9 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #ifndef NETPGP_VERIFY_H_
-#define NETPGP_VERIFY_H_	20190117
+#define NETPGP_VERIFY_H_	20191003
 
-#define NETPGPVERIFY_VERSION	"netpgpverify portable 20190117"
+#define NETPGPVERIFY_VERSION	"netpgpverify portable 20191003"
 
 #include <sys/types.h>
 
