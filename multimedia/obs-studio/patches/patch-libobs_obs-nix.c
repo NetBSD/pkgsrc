b3e6d1e10767596e20b060183bcc6c3fceb828ee
nia <nia@pkgsrc.org>
2019-10-03 20:33:15 +0000

    obs-studio: Update to 24.0.1
    24.0.1 Hotfix
    
        Browser sources will now have their pre-24.0 audio behavior by default; to use OBS to control the browser source audio, users must now use the "Control Audio via OBS" option in the browser source properties. This change was made as a safety precaution because it changed user configurations compared with how the browser audio used to function, which confused some users.
        Fixed an issue where certain cameras (particularly C920 cameras) would output with a lot of delay depending on the drivers installed
        Fixed a crash on startup
        Fixed a crash that would happen when closing the Mixer chat window
    
    24.0 New Features
    
        Added the ability to pause while recording to allow seamless on-the-fly removal of video segments. This can only be used when not sharing an encoder with the stream. [Jim]
        Added an option to automatically adjust bitrate when congestion occurs to advanced settings, which is an alternative to dropping frames (available in advanced settings). Note that this currently only works with RTMP, and severe congestion may cause increased delay to viewers. [Jim/pkv]
        Added the ability to select multiple sources on the preview by box selection [Dillon]
        Added the ability to create custom browser docks in the View -> Docks menu. This allows you to open extra dockable webpages whenever OBS opens [Jim]
        Browser sources can now have their volume adjusted via the audio mixer, or have filters applied to them. Additionally, you can now change whether the browser source outputs to speakers or to stream only via the audio monitoring settings in advanced audio properties. [Osiris/pkv/Jim]
        Added a script to pause recording when a specific scene is active [cg2121]
        Added a "Hotkey Focus Behavior" option to advanced settings, which allows you to set whether hotkeys are disabled depending on whether you have the main window in focus or not [jb-alvarado/Jim]
        Added an option to general settings to allow users to confirm when clicking the "Stop Recording" button [glikely]
        Added channels widget for restream.io service integration [SoftwareArchitector]
        Added the "Area" shader as a downscale shader in video settings [jpark37]
        Added an "Enable Preview" button to the main window when the preview is disabled [cg2121]
        Added (or rather fixed) hardware acceleration support for decoding media files when using the media source where applicable [Jim]
    
    24.0 Tweaks/Fixes
    
        Updated all dependencies on Windows (such as FFmpeg, x264, CEF) to their latest versions [Jim]
        Made a number of optimizations and performance improvements [jpark37]
        Made a number of performance improvements to QSV [brittneysclark]
        Changed default recording format to mkv instead of flv [WizardCM]
        Recording now automatically stop if there is less than 50 megabytes left of disk space available [cg2121]
        Fixed a number of issues with Linux window capture [kkartaltepe]
        Fixed the tray icon showing up on startup even when it was turned off [Jim]
        Fixed a bug where encoders could lock up when an encode call fails [Jim]
        Fixed an issue where projectors would have a gray background rather than a black background [Jim]
        Fixed a bug where secondary Twitch/Mixer browser panels would stop appearing [Jim]
        Fixed a freeze that could happen under rare circumstances when shutting down [Jim]
        If no tracks are selected when recording in advanced output mode, it will now default to the same track being used for streaming [cg2121]

--- multimedia/obs-studio/patches/patch-libobs_obs-nix.c
+++ multimedia/obs-studio/patches/patch-libobs_obs-nix.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-libobs_obs-nix.c,v 1.1 2019/06/30 21:01:24 nia Exp $
+$NetBSD: patch-libobs_obs-nix.c,v 1.2 2019/10/03 20:33:15 nia Exp $
 
 Support NetBSD.
 
---- libobs/obs-nix.c.orig	2019-06-14 05:28:10.000000000 +0000
+--- libobs/obs-nix.c.orig	2019-09-20 15:14:16.000000000 +0000
 +++ libobs/obs-nix.c
 @@ -26,7 +26,9 @@
  #if defined(__FreeBSD__)
@@ -14,7 +14,7 @@ Support NetBSD.
  #include <sys/utsname.h>
  #include <xcb/xcb.h>
  #if USE_XINPUT
-@@ -218,6 +220,7 @@ static void log_processor_info(void)
+@@ -216,6 +218,7 @@ static void log_processor_info(void)
  
  static void log_memory_info(void)
  {
@@ -22,10 +22,10 @@ Support NetBSD.
  	struct sysinfo info;
  	if (sysinfo(&info) < 0)
  		return;
-@@ -226,6 +229,7 @@ static void log_memory_info(void)
- 			(uint64_t)info.totalram * info.mem_unit / 1024 / 1024,
- 			((uint64_t)info.freeram + (uint64_t)info.bufferram) *
- 			info.mem_unit / 1024 / 1024);
+@@ -225,6 +228,7 @@ static void log_memory_info(void)
+ 	     (uint64_t)info.totalram * info.mem_unit / 1024 / 1024,
+ 	     ((uint64_t)info.freeram + (uint64_t)info.bufferram) *
+ 		     info.mem_unit / 1024 / 1024);
 +#endif
  }
  
