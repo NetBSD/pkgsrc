d244c2d5a7e7f3e05257ddbb050058b8c96ea74b
sborrill <sborrill@pkgsrc.org>
2019-10-03 16:34:59 +0000

    c-icap: update to 0.5.5
    c-icap-0.5.5 changes
    - c-icap may crash with a SIGBUS while using mmap to map files to memory.
    - Fix multiple brotli decoding bugs
    - c-icap-client does not send the ";ieof" preview termination sequence when sends zero sized files
    
    c-icap-0.5.4 changes
    - Bug fix: IPv6 address can not be used on Port configuration parameter
    - Mark as deprecated the tls-method TlsPort option
    - Bug fix: c-icap fails to decompress zero-sized files compressed with brotli
    
    c-icap-0.5.2 changes
    - Document the forceUnload=off option for Service/Module configuration parameter
    - Bug fix: c-icap crashes when converting ci_simple_file_t to a memory object
    - ci_headers_value* functions should remove spaces at the beginning of the returned value
    
    c-icap-0.5.1 changes
    The 0.5.1 release has the following new major features :
    * TLS/SSL support. This feature sponsored by Ergon Informatik AG.
    * A non-blocking ICAP client API. This feature sponsored by Ergon Informatik AG.
    * Allow 204 response on preview handler even if the ICAP client does not support preview.
    * New API functions.
    
    Major bugs fixed:
    * c-icap crashes on shutdown or on reconfigure, because of unloaded c++ dynamic libraries. The new release accepts the forceUnload=off parameter to Module and Service configuration parameters to force c-icap to not actually unload dynamic libraries on reconfigure, or shutdown.
    
    New configuration parameters:
    * FakeAllow204
    * TlsPort
    * TlsPassphrase

--- www/c-icap/patches/patch-cfg__param.c
+++ www/c-icap/patches/patch-cfg__param.c
@@ -1,50 +1,47 @@
-$NetBSD: patch-cfg__param.c,v 1.3 2017/02/06 14:14:48 adam Exp $
+$NetBSD: patch-cfg__param.c,v 1.4 2019/10/03 16:34:59 sborrill Exp $
 
 Fix compilation.
 
---- cfg_param.c.orig	2015-08-27 09:01:51.000000000 +0000
-+++ cfg_param.c
-@@ -699,14 +699,12 @@ int fread_line(FILE * f_conf, char *line
- struct ci_conf_entry *find_action(char *str, char **arg)
+--- cfg_param.c.orig	2018-09-13 15:15:05.000000000 +0100
++++ cfg_param.c	2019-10-03 13:26:05.000000000 +0100
+@@ -780,11 +780,11 @@
  {
-      char *end, *table, *s;
--     int size;
-      end = str;
--     while (*end != '\0' && !isspace(*end))
-+     while (*end != '\0' && !isspace((unsigned char)*end))
-           end++;
--     size = end - str;
-      *end = '\0';               /*Mark the end of Variable...... */
-      end++;                     /*... and continue.... */
--     while (*end != '\0' && isspace(*end))      /*Find the start of arguments ...... */
-+     while (*end != '\0' && isspace((unsigned char)*end))      /*Find the start of arguments ...... */
-           end++;
-      *arg = end;
-      if ((s = strchr(str, '.')) != NULL) {
-@@ -744,7 +742,7 @@ char **split_args(char *args)
-           else {
-               /*Support arguments in the form arg{a, b...}*/
-               brkt = 0;
--              while (*end != '\0' && (!isspace(*end) || brkt)) {
-+              while (*end != '\0' && (!isspace((unsigned char)*end) || brkt)) {
-                     if (*end == '{') brkt = 1;
-                     else if (brkt && *end == '}') brkt = 0;
-                     end++;
-@@ -763,7 +761,7 @@ char **split_args(char *args)
+     char *end, *table, *s;
+     end = str;
+-    while (*end != '\0' && !isspace(*end))
++    while (*end != '\0' && !isspace((unsigned char)*end))
+         end++;
+     *end = '\0';               /*Mark the end of Variable...... */
+     end++;                     /*... and continue.... */
+-    while (*end != '\0' && isspace(*end))      /*Find the start of arguments ...... */
++    while (*end != '\0' && isspace((unsigned char)*end))      /*Find the start of arguments ...... */
+         end++;
+     *arg = end;
+     if ((s = strchr(str, '.')) != NULL) {
+@@ -820,7 +820,7 @@
+         } else {
+             /*Support arguments in the form arg{a, b...}*/
+             brkt = 0;
+-            while (*end != '\0' && (!isspace(*end) || brkt)) {
++            while (*end != '\0' && (!isspace((unsigned char)*end) || brkt)) {
+                 if (*end == '{') brkt = 1;
+                 else if (brkt && *end == '}') brkt = 0;
+                 end++;
+@@ -839,7 +839,7 @@
  
-           if (*end == '"')
-                end++;
--          while (*end != '\0' && isspace(*end))
-+          while (*end != '\0' && isspace((unsigned char)*end))
-                end++;
+         if (*end == '"')
+             end++;
+-        while (*end != '\0' && isspace(*end))
++        while (*end != '\0' && isspace((unsigned char)*end))
+             end++;
  
-      } while (*end != '\0');
-@@ -795,7 +793,7 @@ int process_line(char *orig_line)
-      line[LINESIZE-1] = '\0';
+     } while (*end != '\0');
+@@ -871,7 +871,7 @@
+     line[LINESIZE-1] = '\0';
  
-      str = line;
--     while (*str != '\0' && isspace(*str))      /*Eat the spaces in the begging */
-+     while (*str != '\0' && isspace((unsigned char)*str))      /*Eat the spaces in the begging */
-           str++;
-      if (*str == '\0' || *str == '#')   /*Empty line or comment */
-           return 1;
+     str = line;
+-    while (*str != '\0' && isspace(*str))      /*Eat the spaces in the begging */
++    while (*str != '\0' && isspace((unsigned char)*str))      /*Eat the spaces in the begging */
+         str++;
+     if (*str == '\0' || *str == '#')   /*Empty line or comment */
+         return 1;
