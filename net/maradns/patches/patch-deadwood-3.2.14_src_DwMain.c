6bbf14ae70f90f5329bd3dd780f7827d5a9eb63f
nia <nia@pkgsrc.org>
2019-10-06 12:52:20 +0000

    maradns: Update to 2.0.17
    maradns-2.0.17:
    This is the stable release of MaraDNS. No security updates were made.
    
            Deadwood updated to 3.2.14
            Default max_mem value doubled as discussed in GitHub issue #52.

--- net/maradns/patches/patch-deadwood-3.2.14_src_DwMain.c
+++ net/maradns/patches/patch-deadwood-3.2.14_src_DwMain.c
@@ -1,7 +1,9 @@
-$NetBSD: patch-an,v 1.3 2019/01/18 22:14:14 bsiegert Exp $
+$NetBSD: patch-deadwood-3.2.14_src_DwMain.c,v 1.1 2019/10/06 12:52:20 nia Exp $
 
---- deadwood-3.2.12/src/DwMain.c.orig	2015-09-26 15:26:04.000000000 +0000
-+++ deadwood-3.2.12/src/DwMain.c
+Support PKG_SYSCONFDIR.
+
+--- deadwood-3.2.14/src/DwMain.c.orig	2019-01-20 16:22:47.000000000 +0000
++++ deadwood-3.2.14/src/DwMain.c
 @@ -66,7 +66,7 @@ void dw_parse_args(int argc, char **argv
                  process_mararc(argv[2]);
          } else {
