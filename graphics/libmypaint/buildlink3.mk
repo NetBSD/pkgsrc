ace3d2cc1ebdd13accf86e65ff7229bc95e05cde
nia <nia@pkgsrc.org>
2019-10-06 12:23:03 +0000

    libmypaint: Update to 1.4.0
    This release backports a few new features and bug fixes from the 2.0 alpha
    branch.

--- graphics/libmypaint/buildlink3.mk
+++ graphics/libmypaint/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/04/30 04:53:25 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/10/06 12:23:03 nia Exp $
 
 BUILDLINK_TREE+=	libmypaint
 
 .if !defined(LIBMYPAINT_BUILDLINK3_MK)
 LIBMYPAINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libmypaint+=	libmypaint>=1.3.0
+BUILDLINK_API_DEPENDS.libmypaint+=	libmypaint>=1.4.0
 BUILDLINK_PKGSRCDIR.libmypaint?=	../../graphics/libmypaint
 
 .include "../../textproc/json-c/buildlink3.mk"
