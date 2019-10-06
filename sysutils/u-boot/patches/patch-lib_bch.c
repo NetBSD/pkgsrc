8f08344ba08e1ae635df74ccc192f6bdf12b14af
bad <bad@pkgsrc.org>
2019-10-06 17:02:10 +0000

    fix build of u-boot using pylibfdt on macOS.
    u-boot.mk:
      pass PYTHON2, PYTHONCONFIG and PYTHONLIBPATH in ALL_ENV
    patch-Makefile:
      make PYTHON2 overridable.  Add PYTHONCONFIG and BYTHONLIBPATH.
    patch-lib_bch.c:
      #include <libkern/OSByteOrder.h> and use OSSwapHostToBigInt32
      as in u-boot master ab8fc41302 "fix compile error on macOS Mojave".
    patch-scripts_dtc_pylibfdt_Makefile:
      determine PYTHONCLFAGS and PYTHONLIBS by asking PYTHONCONFIG.
      build PYTHONLDFLAGS from PYTHONLIBPATH and PYTHONLIBS
      add PYTHON{LD.C}FLAGS to LDFLAGS and CPPFLAGS in cmd_pymod.
    
    bump PKGREVISION
    
    tested with u-boot-nanopi-mi.

--- sysutils/u-boot/patches/patch-lib_bch.c
+++ sysutils/u-boot/patches/patch-lib_bch.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-lib_bch.c,v 1.3 2017/10/08 12:09:43 jmcneill Exp $
+$NetBSD: patch-lib_bch.c,v 1.4 2019/10/06 17:02:10 bad Exp $
 
---- lib/bch.c.orig	2017-09-11 18:10:40.000000000 +0000
+--- lib/bch.c.orig	2018-11-14 16:10:06.000000000 +0000
 +++ lib/bch.c
-@@ -61,8 +61,10 @@
+@@ -60,8 +60,11 @@
  #include <linux/bitops.h>
  #else
  #include <errno.h>
@@ -11,22 +11,23 @@ $NetBSD: patch-lib_bch.c,v 1.3 2017/10/08 12:09:43 jmcneill Exp $
  #include <sys/endian.h>
 +#elif defined(__APPLE__)
 +#include <machine/endian.h>
++#include <libkern/OSByteOrder.h>
  #else
  #include <endian.h>
  #endif
-@@ -71,7 +73,11 @@
+@@ -70,7 +73,11 @@
  #include <string.h>
  
  #undef cpu_to_be32
 +#if defined(__APPLE__)
-+#define cpu_to_be32 htonl
++#define cpu_to_be32 OSSwapHostToBigInt32
 +#else
  #define cpu_to_be32 htobe32
 +#endif
  #define DIV_ROUND_UP(n,d) (((n) + (d) - 1) / (d))
  #define kmalloc(size, flags)	malloc(size)
  #define kzalloc(size, flags)	calloc(1, size)
-@@ -117,7 +123,7 @@ struct gf_poly_deg1 {
+@@ -116,7 +123,7 @@ struct gf_poly_deg1 {
  };
  
  #ifdef USE_HOSTCC
