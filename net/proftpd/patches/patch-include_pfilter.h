7f41541d35d9922dfb4e94a607dfa4afc2e421d9
christos <christos@pkgsrc.org>
2019-10-07 19:29:47 +0000

    - update to 1.3.6 - add blacklistd support.

---
+++ net/proftpd/patches/patch-include_pfilter.h
@@ -0,0 +1,6 @@
+--- /dev/null	2016-01-22 17:30:55.000000000 -0500
++++ include/pfilter.h	2016-01-22 16:18:33.000000000 -0500
+@@ -0,0 +1,3 @@
++
++void pfilter_notify(int);
++void pfilter_init(void);
