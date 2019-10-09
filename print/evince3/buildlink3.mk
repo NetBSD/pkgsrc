07dcb10686dc4ccdd954e4efff241cb3c8241713
gdt <gdt@pkgsrc.org>
2019-10-09 16:27:30 +0000

    print/evince3: Use print/evince3 in bl3
    print/evince is no more, so stop referencing it.  (This package needs
    and update and I'm not sure what else; I am simply blinding adjusting
    stale pointers to more sensible values.)

--- print/evince3/buildlink3.mk
+++ print/evince3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2019/07/21 22:24:09 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2019/10/09 16:27:30 gdt Exp $
 
 BUILDLINK_TREE+=	evince
 
@@ -7,7 +7,7 @@ EVINCE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.evince+=	evince>=3.0
 BUILDLINK_ABI_DEPENDS.evince+=	evince>=3.28.2nb15
-BUILDLINK_PKGSRCDIR.evince?=	../../print/evince
+BUILDLINK_PKGSRCDIR.evince?=	../../print/evince3
 
 .include "../../x11/gtk3/buildlink3.mk"
 .endif # EVINCE_BUILDLINK3_MK
