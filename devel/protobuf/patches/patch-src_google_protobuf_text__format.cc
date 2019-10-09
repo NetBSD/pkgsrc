e11ac628a8c561362922d8a8f38cfc0711abf7f6
otis <otis@pkgsrc.org>
2019-10-09 08:46:22 +0000

    protobuf: Add comment into patch file (cmath and pull request)

--- devel/protobuf/patches/patch-src_google_protobuf_text__format.cc
+++ devel/protobuf/patches/patch-src_google_protobuf_text__format.cc
@@ -1,4 +1,9 @@
-$NetBSD: patch-src_google_protobuf_text__format.cc,v 1.1 2019/10/08 22:11:37 otis Exp $
+$NetBSD: patch-src_google_protobuf_text__format.cc,v 1.2 2019/10/09 08:46:22 otis Exp $
+
+Fix build on NetBSD 8.1 with gcc 5.5.0.
+
+Also, pull request with upstream has been opened at:
+https://github.com/protocolbuffers/protobuf/pull/6747
 
 --- src/google/protobuf/text_format.cc.orig	2019-10-03 00:08:05.000000000 +0000
 +++ src/google/protobuf/text_format.cc
