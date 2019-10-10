bde2afe7cdfcf6b811b92c202d11c162d57c4fcc
wiz <wiz@pkgsrc.org>
2019-10-10 14:15:10 +0000

    py-mercurial: update to 5.1.2.
    1. Mercurial 5.1.2 (2019-10-02)
    
    Regularly scheduled bug fix release.
    
    1.1. extensions
    
      • phabricator: don't abort if property writing fails during amending
    
    1.2. tests
    
      • test: allow different result for zstd compression (issue6188)
    
    2. Mercurial 5.1.1 (2019-09-05)
    
    Regularly scheduled bug fix release.
    
    2.1. commands
    
      • bookmarks: actual fix for race condition deleting bookmark
    
    2.2. core
    
      • exchange: abort on pushing bookmarks pointing to secret changesets (issue6159)
      • python-zstandard: apply big-endian fix (issue6188)
      • fncache: make debugrebuildfncache not fail on broken fncache
    
    2.3. tests
    
      • various test runner fixes
      • fixed use of tr --delete for tr(1) portability

--- devel/py-mercurial/version.mk
+++ devel/py-mercurial/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.5 2019/09/02 13:19:40 adam Exp $
+# $NetBSD: version.mk,v 1.6 2019/10/10 14:15:10 wiz Exp $
 
-VERSION=	5.1
+VERSION=	5.1.2
 
 PYTHON_VERSIONS_ACCEPTED=	27 # 5.0 has "beta" support for python 3
 # see https://www.mercurial-scm.org/wiki/Python3
