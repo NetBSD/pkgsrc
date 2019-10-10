07d1262c9ae048fb4ec7421179c913c57bb6912e
prlw1 <prlw1@pkgsrc.org>
2019-10-10 15:41:29 +0000

    Update clamav to 0.102.0
    * The On-Access Scanning feature has been migrated out of clamd and
      into a brand new utility named clamonacc, which is disabled in this
      package as it is for Linux only.
    * The freshclam database update utility has undergone a significant
      update. This includes:
         + Added support for HTTPS.
         + Support for database mirrors hosted on ports other than 80.
         + Removal of the mirror management feature (mirrors.dat).
         + An all new libfreshclam library API.
    * Added support for extracting ESTsoft .egg archives. This feature is
      new code developed from scratch using ESTsoft's Egg-archive
      specification and without referencing the UnEgg library provided by
      ESTsoft. This was necessary because the UnEgg library's license
      includes restrictions limiting the commercial use of the UnEgg library.
    
    Full release notes available at:
    https://github.com/Cisco-Talos/clamav-devel/blob/rel/0.102/NEWS.md

--- security/clamav/patches/patch-libclamav_fmap.h
+++ security/clamav/patches/patch-libclamav_fmap.h
@@ -1,23 +1,23 @@
-$NetBSD: patch-libclamav_fmap.h,v 1.1 2017/07/24 09:39:42 maya Exp $
+$NetBSD: patch-libclamav_fmap.h,v 1.2 2019/10/10 15:41:30 prlw1 Exp $
 
 rename gets to my_gets to avoid conflict with fortify/ssp - they use
 macros to override libc functions
 
---- libclamav/fmap.h.orig	2016-04-22 15:02:19.000000000 +0000
+--- libclamav/fmap.h.orig	2019-10-01 17:24:09.000000000 +0000
 +++ libclamav/fmap.h
-@@ -74,7 +74,7 @@ struct cl_fmap {
-     void        (*unmap)(fmap_t*);
-     const void* (*need)(fmap_t*, size_t at, size_t len, int lock);
-     const void* (*need_offstr)(fmap_t*, size_t at, size_t len_hint);
--    const void* (*gets)(fmap_t*, char *dst, size_t *at, size_t max_len);
-+    const void* (*my_gets)(fmap_t*, char *dst, size_t *at, size_t max_len);
-     void        (*unneed_off)(fmap_t*, size_t at, size_t len);
+@@ -75,7 +75,7 @@ struct cl_fmap {
+     void (*unmap)(fmap_t *);
+     const void *(*need)(fmap_t *, size_t at, size_t len, int lock);
+     const void *(*need_offstr)(fmap_t *, size_t at, size_t len_hint);
+-    const void *(*gets)(fmap_t *, char *dst, size_t *at, size_t max_len);
++    const void *(*my_gets)(fmap_t *, char *dst, size_t *at, size_t max_len);
+     void (*unneed_off)(fmap_t *, size_t at, size_t len);
  #ifdef _WIN32
      HANDLE fh;
-@@ -156,7 +156,7 @@ static inline const void *fmap_need_offs
- }
+@@ -169,7 +169,7 @@ static inline const void *fmap_need_offs
  
- static inline const void *fmap_gets(fmap_t *m, char *dst, size_t *at, size_t max_len) {
+ static inline const void *fmap_gets(fmap_t *m, char *dst, size_t *at, size_t max_len)
+ {
 -    return m->gets(m, dst, at, max_len);
 +    return m->my_gets(m, dst, at, max_len);
  }
