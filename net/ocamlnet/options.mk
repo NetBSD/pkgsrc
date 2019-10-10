43211db70250c5ebc4ddab64d983647bc2200819
wiz <wiz@pkgsrc.org>
2019-10-10 14:16:33 +0000

    ocamlnet: remove optional gtk1 support

--- net/ocamlnet/options.mk
+++ net/ocamlnet/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.9 2018/01/22 11:17:52 jaapb Exp $
+# $NetBSD: options.mk,v 1.10 2019/10/10 14:16:33 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ocamlnet
-PKG_SUPPORTED_OPTIONS=	gtk gtk2 gnutls
+PKG_SUPPORTED_OPTIONS=	gtk2 gnutls
 PKG_SUGGESTED_OPTIONS=	gnutls
 
 PLIST_VARS+=		gnutls cmxs
@@ -11,17 +11,7 @@ PLIST_VARS+=		gnutls cmxs
 .include "../../mk/bsd.options.mk"
 
 ###
-### GTK(1) support
-###
-.if !empty(PKG_OPTIONS:Mgtk)
-.  include  "../../x11/lablgtk1/buildlink3.mk"
-CONFIGURE_ARGS+=	-enable-gtk
-.else
-CONFIGURE_ARGS+=	-disable-gtk
-.endif
-
-###
-### GTK(2) support
+### GTK2 support
 ###
 .if !empty(PKG_OPTIONS:Mgtk2)
 .  include  "../../x11/ocaml-lablgtk/buildlink3.mk"
