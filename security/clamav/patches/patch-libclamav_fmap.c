aaea0259c3b7c1d056900754523ca0a875f0e236
prlw1 <prlw1@pkgsrc.org>
2019-10-10 15:41:29 +0000

    Update clamav to 0.102.0
    * The On-Access Scanning feature has been migrated out of clamd and
      into a brand new utility named clamonacc, which is disabled in this
      package as it is for Linux only.
    * The freshclam database update utility has undergone a significant
      update. This includes:
         + Added support for HTTPS.
         + Support for database mirrors hosted on ports other than 80.
         + Removal of the mirror management feature (mirrors.dat).
         + An all new libfreshclam library API.
    * Added support for extracting ESTsoft .egg archives. This feature is
      new code developed from scratch using ESTsoft's Egg-archive
      specification and without referencing the UnEgg library provided by
      ESTsoft. This was necessary because the UnEgg library's license
      includes restrictions limiting the commercial use of the UnEgg library.
    
    Full release notes available at:
    https://github.com/Cisco-Talos/clamav-devel/blob/rel/0.102/NEWS.md

--- security/clamav/patches/patch-libclamav_fmap.c
+++ security/clamav/patches/patch-libclamav_fmap.c
@@ -1,25 +1,25 @@
-$NetBSD: patch-libclamav_fmap.c,v 1.1 2017/07/24 09:39:42 maya Exp $
+$NetBSD: patch-libclamav_fmap.c,v 1.2 2019/10/10 15:41:30 prlw1 Exp $
 
 rename gets to my_gets to avoid conflict with fortify/ssp - they use
 macros to override libc functions
 
---- libclamav/fmap.c.orig	2016-04-22 15:02:19.000000000 +0000
+--- libclamav/fmap.c.orig	2019-10-01 17:24:09.000000000 +0000
 +++ libclamav/fmap.c
-@@ -274,7 +274,7 @@ extern cl_fmap_t *cl_fmap_open_handle(vo
-     m->unmap = use_aging ? unmap_mmap : unmap_malloc;
-     m->need = handle_need;
-     m->need_offstr = handle_need_offstr;
--    m->gets = handle_gets;
-+    m->my_gets = handle_gets;
-     m->unneed_off = handle_unneed_off;
+@@ -278,7 +278,7 @@ extern cl_fmap_t *cl_fmap_open_handle(vo
+     m->unmap           = use_aging ? unmap_mmap : unmap_malloc;
+     m->need            = handle_need;
+     m->need_offstr     = handle_need_offstr;
+-    m->gets            = handle_gets;
++    m->my_gets         = handle_gets;
+     m->unneed_off      = handle_unneed_off;
      return m;
  }
-@@ -664,7 +664,7 @@ extern cl_fmap_t *cl_fmap_open_memory(co
-     m->unmap = unmap_malloc;
-     m->need = mem_need;
+@@ -674,7 +674,7 @@ extern cl_fmap_t *cl_fmap_open_memory(co
+     m->unmap       = unmap_malloc;
+     m->need        = mem_need;
      m->need_offstr = mem_need_offstr;
--    m->gets = mem_gets;
-+    m->my_gets = mem_gets;
-     m->unneed_off = mem_unneed_off;
+-    m->gets        = mem_gets;
++    m->my_gets     = mem_gets;
+     m->unneed_off  = mem_unneed_off;
      return m;
  }
