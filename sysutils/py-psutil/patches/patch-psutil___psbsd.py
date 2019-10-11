5ccde2ded8cd176fbf755e646bc534591b4caf0e
kamil <kamil@pkgsrc.org>
2019-10-11 20:01:50 +0000

    py-psutil: Stop using SDEAD on NetBSD
    Fixes usage on recent NetBSD.

--- sysutils/py-psutil/patches/patch-psutil___psbsd.py
+++ sysutils/py-psutil/patches/patch-psutil___psbsd.py
@@ -1,9 +1,20 @@
-$NetBSD: patch-psutil___psbsd.py,v 1.3 2019/06/01 01:11:57 kamil Exp $
+$NetBSD: patch-psutil___psbsd.py,v 1.4 2019/10/11 20:01:51 kamil Exp $
 
 Use proc_cwd on NetBSD >= 8.99.42.
 
+Stop using SDEAD as it is no longer available.
+
 --- psutil/_psbsd.py.orig	2019-04-11 21:10:12.000000000 +0000
 +++ psutil/_psbsd.py
+@@ -57,7 +57,7 @@ elif OPENBSD or NETBSD:
+         # equivalent. Also it appears there's no equivalent of
+         # psutil.STATUS_DEAD. SDEAD really means STATUS_ZOMBIE.
+         # cext.SZOMB: _common.STATUS_ZOMBIE,
+-        cext.SDEAD: _common.STATUS_ZOMBIE,
++        # cext.SDEAD: _common.STATUS_ZOMBIE,
+         cext.SZOMB: _common.STATUS_ZOMBIE,
+         # From http://www.eecs.harvard.edu/~margo/cs161/videos/proc.h.txt
+         # OpenBSD has SRUN and SONPROC: SRUN indicates that a process
 @@ -105,6 +105,7 @@ HAS_PER_CPU_TIMES = hasattr(cext, "per_c
  HAS_PROC_NUM_THREADS = hasattr(cext, "proc_num_threads")
  HAS_PROC_OPEN_FILES = hasattr(cext, 'proc_open_files')
