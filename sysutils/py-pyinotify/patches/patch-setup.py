ea3d3c5beeaad719d71b6a06b6837ba5ab63795a
wiz <wiz@pkgsrc.org>
2019-10-11 14:38:56 +0000

    py-pyinotify: unconditionally build inotify syscalls so.
    Fixes build on NetBSD and probably others.

--- sysutils/py-pyinotify/patches/patch-setup.py
+++ sysutils/py-pyinotify/patches/patch-setup.py
@@ -1,9 +1,18 @@
-$NetBSD: patch-setup.py,v 1.2 2019/09/24 09:04:08 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2019/10/11 14:38:56 wiz Exp $
 
 Add NetBSD.
 
 --- setup.py.orig	2015-06-04 15:28:48.000000000 +0000
 +++ setup.py
+@@ -3,7 +3,7 @@
+ # Set True to force compile native C-coded extension providing direct access
+ # to inotify's syscalls. If set to False this extension will only be compiled
+ # if no inotify interface from ctypes is found.
+-compile_ext_mod = False
++compile_ext_mod = True
+ 
+ # import statements
+ import os
 @@ -24,10 +24,6 @@ if sys.version_info < (2, 4):
      sys.stderr.write('This module requires at least Python 2.4\n')
      sys.exit(1)
