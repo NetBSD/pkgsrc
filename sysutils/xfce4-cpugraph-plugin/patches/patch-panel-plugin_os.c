5dff3be97291fe8fda9ce7c162e5af85a9f4c21e
gutteridge <gutteridge@pkgsrc.org>
2019-10-12 10:25:59 +0000

    xfce4-cpugraph-plugin: note upstream bug report in patch (NFC)

--- sysutils/xfce4-cpugraph-plugin/patches/patch-panel-plugin_os.c
+++ sysutils/xfce4-cpugraph-plugin/patches/patch-panel-plugin_os.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-panel-plugin_os.c,v 1.3 2019/07/13 00:40:22 gutteridge Exp $
+$NetBSD: patch-panel-plugin_os.c,v 1.4 2019/10/12 10:25:59 gutteridge Exp $
 
-Fix NetBSD support.
+Fix NetBSD support. https://bugzilla.xfce.org/show_bug.cgi?id=15794
 
 --- panel-plugin/os.c.orig	2019-07-02 23:18:51.000000000 +0000
 +++ panel-plugin/os.c
