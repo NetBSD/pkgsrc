95e3b614be3b33834dab8e56491b114afb9f78b0
maya <maya@pkgsrc.org>
2019-10-13 19:52:47 +0000

    proftpd: add a configure check for blacklist, instead of assuming it always exists. helps non-netbsd/non-freebsd.

--- net/proftpd/patches/patch-include_pfilter.h
+++ net/proftpd/patches/patch-include_pfilter.h
@@ -1,5 +1,7 @@
 --- /dev/null	2016-01-22 17:30:55.000000000 -0500
 +++ include/pfilter.h	2016-01-22 16:18:33.000000000 -0500
+--- include/pfilter.h.orig	2019-10-13 19:47:58.412725364 +0000
++++ include/pfilter.h
 @@ -0,0 +1,3 @@
 +
 +void pfilter_notify(int);
