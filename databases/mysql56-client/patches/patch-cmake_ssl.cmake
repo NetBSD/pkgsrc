2e542b881bb1fc3168f8d3fcc4829d65747f1c81
adam <adam@pkgsrc.org>
2019-10-14 19:44:46 +0000

    mysql56: updated to 5.6.46
    Changes in MySQL 5.6.46
    
    Configuration Notes
    
    It is now possible to compile MySQL 5.6 using OpenSSL 1.1.1, enabling compilation support for MySQL 5.6 against OpenSSL even when OpenSSL 1.0.2 reaches End of Life status at the end of 2019.
    
    In addition, MySQL 5.6 now supports TLSv1.1 and TLSv1.2 protocols for encrypted connections. This applies to MySQL Server, MySQL clients such as mysql and mysqldump, and master/slave replication. Previously, MySQL 5.6 supported only TLSv1, so TLSv1.1/TLSv1.2 support enables use of more secure TLS protocols. It also enables connecting from MySQL 5.6 clients and replication slaves to MySQL 5.7 and higher servers that have TLSv1 disabled, which previously was not possible. It also means that MySQL 5.7 and higher clients that do not want to use TLSv1 can connect to MySQL 5.6 servers.
    
    All MySQL 5.6 builds now use OpenSSL. MySQL no longer supports using yaSSL as the SSL library, and source distributions no longer include yaSSL.
    
    The WITH_SSL CMake option no longer permits bundled (use yaSSL) as a valid value, and the default option value has changed from bundled to system (use the version of OpenSSL installed on the host system).
    
    
    Bugs Fixed
    
    Improper locking during storage engine initialization could cause a server exit.
    
    A query with a WHERE clause whose predicate contained a numeric value in scientific notation was not handled correctly.
    
    VS2019 produced compilation errors with debug compilation selected due to use of the /ZI flag. Now /Z7 is used instead.
    
    Password masking was incomplete for SHOW PROCESSLIST and some INFORMATION_SCHEMA and Performance Schema tables.
    
    The -DWITH_EXAMPLE_STORAGE_ENGINE=1 CMake option was ignored but should not have been. If -DWITH_EXAMPLE_STORAGE_ENGINE=0 is given, the EXAMPLE storage engine is built as a plugin.

--- databases/mysql56-client/patches/patch-cmake_ssl.cmake
+++ databases/mysql56-client/patches/patch-cmake_ssl.cmake
@@ -1,11 +1,11 @@
-$NetBSD: patch-cmake_ssl.cmake,v 1.2 2013/04/20 08:06:01 adam Exp $
+$NetBSD: patch-cmake_ssl.cmake,v 1.3 2019/10/14 19:44:47 adam Exp $
 
 Allow OpenSSH versions below 1.0.
 Do not prefer static libraries (or linking will fail).
 
---- cmake/ssl.cmake.orig	2013-04-05 12:27:18.000000000 +0000
+--- cmake/ssl.cmake.orig	2019-09-27 07:04:56.000000000 +0000
 +++ cmake/ssl.cmake
-@@ -143,9 +143,7 @@ MACRO (MYSQL_CHECK_SSL)
+@@ -152,9 +152,7 @@ MACRO (MYSQL_CHECK_SSL)
      ENDIF()
  
      # On mac this list is <.dylib;.so;.a>
@@ -14,9 +14,9 @@ Do not prefer static libraries (or linking will fail).
 -      LIST(REVERSE CMAKE_FIND_LIBRARY_SUFFIXES)
        MESSAGE(STATUS "suffixes <${CMAKE_FIND_LIBRARY_SUFFIXES}>")
      ENDIF()
-     FIND_LIBRARY(OPENSSL_LIBRARY
-@@ -172,8 +170,7 @@ MACRO (MYSQL_CHECK_SSL)
  
+@@ -194,8 +192,7 @@ MACRO (MYSQL_CHECK_SSL)
+     ENDIF()
      IF(OPENSSL_INCLUDE_DIR AND
         OPENSSL_LIBRARY   AND
 -       CRYPTO_LIBRARY      AND
