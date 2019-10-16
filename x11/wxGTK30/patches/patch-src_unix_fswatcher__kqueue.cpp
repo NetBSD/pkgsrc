34df1388cb49a314a8bbf137e5ff2d6b69719601
wiz <wiz@pkgsrc.org>
2019-10-16 20:43:14 +0000

    wxGTK30: fix build on NetBSD 9.99.14+

--- x11/wxGTK30/patches/patch-src_unix_fswatcher__kqueue.cpp
+++ x11/wxGTK30/patches/patch-src_unix_fswatcher__kqueue.cpp
@@ -1,15 +1,26 @@
-$NetBSD: patch-src_unix_fswatcher__kqueue.cpp,v 1.2 2018/08/16 11:46:25 wiz Exp $
+$NetBSD: patch-src_unix_fswatcher__kqueue.cpp,v 1.3 2019/10/16 20:43:14 wiz Exp $
 
 Fix compilation error on NetBSD
 https://trac.wxwidgets.org/ticket/18199
 
---- src/unix/fswatcher_kqueue.cpp.orig	2014-08-13 21:20:42.000000000 +0000
+--- src/unix/fswatcher_kqueue.cpp.orig	2018-03-07 16:55:38.000000000 +0000
 +++ src/unix/fswatcher_kqueue.cpp
-@@ -125,8 +125,13 @@ public:
+@@ -14,6 +14,10 @@
+     #pragma hdrstop
+ #endif
+ 
++#ifdef __NetBSD__
++#include <sys/param.h>
++#endif
++
+ #if wxUSE_FSWATCHER
+ 
+ #include "wx/fswatcher.h"
+@@ -125,8 +129,13 @@ public:
          struct kevent event;
          int action = EV_ADD | EV_ENABLE | EV_CLEAR | EV_ERROR;
          int flags = Watcher2NativeFlags(watch->GetFlags());
-+#ifdef __NetBSD__
++#if defined(__NetBSD__) && (__NetBSD_Version__ <= 999001400)
 +        EV_SET( &event, watch->GetFileDescriptor(), EVFILT_VNODE, action,
 +                flags, 0, (intptr_t)watch.get() );
 +#else
@@ -19,7 +30,7 @@ https://trac.wxwidgets.org/ticket/18199
  
          // TODO more error conditions according to man
          // TODO best deal with the error here
-@@ -281,10 +286,11 @@ protected:
+@@ -281,10 +290,11 @@ protected:
  
          wxLogTrace(wxTRACE_FSWATCHER, "Event: ident=%d, filter=%d, flags=%u, "
                     "fflags=%u, data=%d, user_data=%p",
