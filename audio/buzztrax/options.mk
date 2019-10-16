66220014369c495cffe755d9268074e50471e7f2
nia <nia@pkgsrc.org>
2019-10-16 12:47:20 +0000

    buzztrax: Make the alsa option work.

--- audio/buzztrax/options.mk
+++ audio/buzztrax/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2019/10/16 11:18:11 nia Exp $
+# $NetBSD: options.mk,v 1.2 2019/10/16 12:47:20 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.buzztrax
 PKG_SUPPORTED_OPTIONS=		alsa
@@ -7,5 +7,6 @@ PKG_SUGGESTED_OPTIONS.Linux=	alsa
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Malsa)
+PLIST_SRC+=	PLIST.alsa
 .include "../../audio/alsa-lib/buildlink3.mk"
 .endif
