da1fd9a5ed20a305b12639a24f8a1776d1c9ca11
nia <nia@pkgsrc.org>
2019-10-16 12:50:27 +0000

    portaudio-devel: Support C++ bindings, ALSA, JACK.
    Bump PKGREVISION.

--- audio/portaudio-devel/buildlink3.mk
+++ audio/portaudio-devel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:23:58 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2019/10/16 12:50:27 nia Exp $
 
 BUILDLINK_TREE+=	portaudio-devel
 
@@ -8,8 +8,8 @@ PORTAUDIO_DEVEL_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.portaudio-devel+=	portaudio-devel>=20060521
 BUILDLINK_PKGSRCDIR.portaudio-devel?=	../../audio/portaudio-devel
 BUILDLINK_INCDIRS.portaudio-devel?=	include/portaudio2
-BUILDLINK_LIBDIRS.portaudio-devel?=	lib/portaudio2
-BUILDLINK_CPPFLAGS.portaudio-devel?=	-I${BUILDLINK_PREFIX.portaudio-devel}/include/portaudio2
+BUILDLINK_LIBDIRS.portaudio-devel+=	lib/portaudio2
+BUILDLINK_CPPFLAGS.portaudio-devel+=	-I${BUILDLINK_PREFIX.portaudio-devel}/include/portaudio2
 .endif # PORTAUDIO_DEVEL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-portaudio-devel
