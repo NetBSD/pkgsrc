f91b6d46f7e8d33d28b71990d931921cc9bb35eb
nia <nia@pkgsrc.org>
2019-10-16 12:50:27 +0000

    portaudio-devel: Support C++ bindings, ALSA, JACK.
    Bump PKGREVISION.

---
+++ audio/portaudio-devel/options.mk
@@ -0,0 +1,23 @@
+# $NetBSD: options.mk,v 1.1 2019/10/16 12:50:27 nia Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.portaudio
+PKG_SUPPORTED_OPTIONS=		alsa jack
+PKG_SUGGESTED_OPTIONS.Linux=	alsa
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		alsa
+.if !empty(PKG_OPTIONS:Malsa)
+PLIST.alsa=		yes
+CONFIGURE_ARGS+=	--with-alsa
+.include "../../audio/alsa-lib/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-alsa
+.endif
+
+.if !empty(PKG_OPTIONS:Mjack)
+CONFIGURE_ARGS+=	--with-jack
+.include "../../audio/jack/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-jack
+.endif
---
+++ audio/portaudio-devel/patches/patch-src_hostapi_alsa_pa__linux__alsa.c
@@ -0,0 +1,17 @@
+$NetBSD: patch-src_hostapi_alsa_pa__linux__alsa.c,v 1.1 2019/10/16 12:50:27 nia Exp $
+
+Support building on non-Linux systems.
+
+--- src/hostapi/alsa/pa_linux_alsa.c.orig	2016-10-30 01:23:04.000000000 +0000
++++ src/hostapi/alsa/pa_linux_alsa.c
+@@ -79,6 +79,10 @@
+ 
+ #include "pa_linux_alsa.h"
+ 
++#ifndef ESTRPIPE
++    #define ESTRPIPE EPIPE
++#endif
++
+ /* Add missing define (for compatibility with older ALSA versions) */
+ #ifndef SND_PCM_TSTAMP_ENABLE
+     #define SND_PCM_TSTAMP_ENABLE SND_PCM_TSTAMP_MMAP
