8c71a4d646bae9cd203110306a0e780e923439a5
nia <nia@pkgsrc.org>
2019-10-16 12:00:51 +0000

    devilutionx: Update to 0.5.0
    Features
    
        Sound is now accurate to the original
        All in-game issues fixed
        Delete hero, inline dialogs and scrollbars are now implemented
        Screenshots now have different names
        Multiple simultaneous dialogs fixed
        All builds are now 64bit (except for Windows and Raspberry Pi)
        Memory leaks and crashes fixed
        All keys are now mapped
        UI text now has correct shadows
        Much lower CPU usage
    
    Known issues
    
        Error dialogs not implemented in main UI
        The game must restart after hosting multiplayer

---
+++ games/devilutionx/patches/patch-3rdParty_StormLib_src_StormPort.h
@@ -0,0 +1,14 @@
+$NetBSD: patch-3rdParty_StormLib_src_StormPort.h,v 1.1 2019/10/16 12:00:51 nia Exp $
+
+strings.h for strncasecmp.
+
+--- 3rdParty/StormLib/src/StormPort.h.orig	2019-10-09 22:34:02.000000000 +0000
++++ 3rdParty/StormLib/src/StormPort.h
+@@ -162,6 +162,7 @@
+   #include <stdio.h>
+   #include <stdarg.h>
+   #include <string.h>
++  #include <strings.h>
+   #include <ctype.h>
+   #include <assert.h>
+   #include <errno.h>
