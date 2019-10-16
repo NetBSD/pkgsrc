141a53e31f9429beaa03b8bcec21d96e320429c6
nia <nia@pkgsrc.org>
2019-10-16 19:25:44 +0000

    fluidsynth: Optionally support more audio backends.

---
+++ audio/fluidsynth/options.mk
@@ -0,0 +1,35 @@
+# $NetBSD: options.mk,v 1.1 2019/10/16 19:25:44 nia Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.fluidsynth
+PKG_SUPPORTED_OPTIONS=		alsa jack portaudio pulseaudio
+PKG_SUGGESTED_OPTIONS.Linux=	alsa
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Malsa)
+CMAKE_ARGS+=	-Denable-alsa=ON
+.include "../../audio/alsa-lib/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-Denable-alsa=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mjack)
+CMAKE_ARGS+=	-Denable-jack=ON
+.include "../../audio/jack/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-Denable-jack=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mportaudio)
+CMAKE_ARGS+=	-Denable-portaudio=ON
+.include "../../audio/portaudio-devel/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-Denable-portaudio=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+CMAKE_ARGS+=	-Denable-pulseaudio=ON
+.include "../../audio/pulseaudio/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-Denable-pulseaudio=OFF
+.endif
