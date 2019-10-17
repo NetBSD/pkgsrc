c19a2fdab003f0ecb0b648188310b29081a06f67
wiz <wiz@pkgsrc.org>
2019-10-17 13:55:44 +0000

    mame: add denied upstream pull request URL

--- emulators/mame/patches/patch-3rdparty_bgfx_examples_common_font_font__manager.cpp
+++ emulators/mame/patches/patch-3rdparty_bgfx_examples_common_font_font__manager.cpp
@@ -1,6 +1,7 @@
-$NetBSD: patch-3rdparty_bgfx_examples_common_font_font__manager.cpp,v 1.1 2016/01/14 23:12:01 wiz Exp $
+$NetBSD: patch-3rdparty_bgfx_examples_common_font_font__manager.cpp,v 1.2 2019/10/17 13:55:45 wiz Exp $
 
 Compatibility with freetype2-2.6.2.
+https://github.com/bkaradzic/bgfx/pull/782
 
 --- 3rdparty/bgfx/examples/common/font/font_manager.cpp.orig	2015-12-30 07:18:51.000000000 +0000
 +++ 3rdparty/bgfx/examples/common/font/font_manager.cpp
