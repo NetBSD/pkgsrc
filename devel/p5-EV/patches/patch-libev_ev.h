1293acebbb9cdfb56730f8c81570d7b55a87ea4f
mef <mef@pkgsrc.org>
2019-10-17 13:59:08 +0000

    (devel/p5-EV) Updated 4.22 to 4.27
    4.27 Thu Jun 27 09:39:58 CEST 2019
            - (libev) completely rewritten linuxaio backend, maybe
              usable as a general-use backend.
            - (libev) use more aggressive assertions to catch
              more usage errors.
            - allow users to re-enable assert() in case it is disabled
              by perl (which is typically the case).
    
    4.26 Mon Jun 24 23:39:40 CEST 2019
            - (libev) included experimental linux aio backend.
            - allow the linux aio backend to be used by default
              only when explicitly configured during Makefile.PL time.
    
    4.25 Fri Dec 21 08:04:26 CET 2018
            - (libev) updated to libev 4.25, minor fixes and enhancements.
            - document the requirement to "use EV" when using EV::MakeMaker.
            - only use __register_atfork with glibc and uclibc, as musl
              defines __linux__, but doesn't implement the linux standard
              base ABI, nor makes itself detectable via a macro, both apparently
              by design, winning the "broken by design 2016 award" - well done.
            - correct EV::periodic example 24h after midnight -> one day after
              midnight (reported by Felix Ostmann).

--- devel/p5-EV/patches/patch-libev_ev.h
+++ devel/p5-EV/patches/patch-libev_ev.h
@@ -1,12 +1,12 @@
-$NetBSD: patch-libev_ev.h,v 1.3 2015/07/27 14:30:34 mef Exp $
+$NetBSD: patch-libev_ev.h,v 1.4 2019/10/17 13:59:08 mef Exp $
 
 Don't define a variable as both static and extern.
 
---- libev/ev.h.orig	2013-03-01 11:05:29.000000000 +0000
-+++ libev/ev.h
-@@ -557,8 +557,6 @@ EV_API_DECL struct ev_loop *ev_default_l
+--- libev/ev.h.orig	2019-06-26 14:15:30.000000000 +0900
++++ libev/ev.h	2019-10-17 22:55:20.971868889 +0900
+@@ -565,8 +565,6 @@ EV_API_DECL struct ev_loop *ev_default_l
  EV_INLINE struct ev_loop *
- ev_default_loop_uc_ (void) EV_THROW
+ ev_default_loop_uc_ (void) EV_NOEXCEPT
  {
 -  extern struct ev_loop *ev_default_loop_ptr;
 -
