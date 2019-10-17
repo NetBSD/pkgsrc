284e05edb25b6321ff73d4dcb04f47fcd880bc67
micha <micha@pkgsrc.org>
2019-10-17 10:16:28 +0000

    x11/fltk13: Backport handling for option "opengl" from wip/fltk14-devel
    Only include MesaLib and glu if option is enabled.

--- x11/fltk13/buildlink3.mk
+++ x11/fltk13/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2018/03/12 11:15:49 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2019/10/17 10:16:28 micha Exp $
 
 BUILDLINK_TREE+=	fltk
 
@@ -10,13 +10,22 @@ BUILDLINK_ABI_DEPENDS.fltk?=	fltk>=1.3.3nb4
 BUILDLINK_PKGSRCDIR.fltk?=	../../x11/fltk13
 BUILDLINK_FILES.fltk+=		include/Fl/*
 
+pkgbase := fltk
 .include "../../mk/bsd.fast.prefs.mk"
+.include "../../mk/pkg-build-options.mk"
+
+# For "opengl" option
+.if !empty(PKG_BUILD_OPTIONS.fltk:Mopengl)
+.  if ${OPSYS} != "Darwin"
+.     include "../../graphics/MesaLib/buildlink3.mk"
+.     include "../../graphics/glu/buildlink3.mk"
+.  endif
+.endif
+
 .if ${OPSYS} != "Darwin"
-.include "../../graphics/MesaLib/buildlink3.mk"
-.include "../../graphics/glu/buildlink3.mk"
-.include "../../x11/libXext/buildlink3.mk"
-.include "../../x11/libXft/buildlink3.mk"
-.include "../../x11/libXinerama/buildlink3.mk"
+.  include "../../x11/libXext/buildlink3.mk"
+.  include "../../x11/libXft/buildlink3.mk"
+.  include "../../x11/libXinerama/buildlink3.mk"
 .endif
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
