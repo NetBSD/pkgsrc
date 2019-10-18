3780744b7dbfaf04057679adffd5d77df6f32aa5
nia <nia@pkgsrc.org>
2019-10-18 09:59:46 +0000

    meson: Update to 0.52.0
    Changes:
    
    * Gettext targets are ignored if gettext is not installed
    * Support taking environment values from a dictionary
    * alias_target
    * Enhancements to the pkg_config_path argument
    * The meson test program now accepts an additional "--gdb-path" argument to specify the GDB binary
    * Better support for illumos and Solaris
    * Splitting of Compiler.get_function_attribute('visibility')
    * Clang-tidy target
    * Add blocks dependency
    * Meson's builtin b_lundef is now supported on macOS
    * Compiler and dynamic linker representation split
    * Add depth option to wrap-git
    * Enhancements to the source_set module
    * added --only test(s) option to run_project_tests.py
    * Experimental Webassembly support via Emscripten
    * Version check in find_program()
    * Added vs_module_defs to shared_module()
    * Improved support for static libraries
    * Enhancements to the kconfig module
    * Added include_type kwarg to dependency
    * Enhancements to configure_file()
    * Projects args can be set separately for build and host machines (potentially breaking change)
    * Allow checking if a variable is a disabler
    * gtkdoc-check support
    * gnome.gtkdoc() returns target object
    * Dist is now a top level command

--- devel/meson/patches/patch-mesonbuild_dependencies_dev.py
+++ devel/meson/patches/patch-mesonbuild_dependencies_dev.py
@@ -1,46 +1,18 @@
-$NetBSD: patch-mesonbuild_dependencies_dev.py,v 1.1 2019/10/04 14:06:19 prlw1 Exp $
+$NetBSD: patch-mesonbuild_dependencies_dev.py,v 1.2 2019/10/18 09:59:46 nia Exp $
 
 Allow handling different versions of LLVM in pkgsrc using LLVM_CONFIG_PATH.
 
---- mesonbuild/dependencies/dev.py.orig	2019-07-09 16:34:42.000000000 +0000
+--- mesonbuild/dependencies/dev.py.orig	2019-10-06 17:01:35.000000000 +0000
 +++ mesonbuild/dependencies/dev.py
-@@ -212,21 +212,24 @@ class LLVMDependencyConfigTool(ConfigToo
-         # newest back to oldest (3.5 is arbitrary), and finally the devel version.
-         # Please note that llvm-config-6.0 is a development snapshot and it should
-         # not be moved to the beginning of the list.
--        self.tools = [
--            'llvm-config', # base
--            'llvm-config-8',   'llvm-config80',
--            'llvm-config-7',   'llvm-config70',
--            'llvm-config-6.0', 'llvm-config60',
--            'llvm-config-5.0', 'llvm-config50',
--            'llvm-config-4.0', 'llvm-config40',
--            'llvm-config-3.9', 'llvm-config39',
--            'llvm-config-3.8', 'llvm-config38',
--            'llvm-config-3.7', 'llvm-config37',
--            'llvm-config-3.6', 'llvm-config36',
--            'llvm-config-3.5', 'llvm-config35',
--            'llvm-config-9',     # Debian development snapshot
--            'llvm-config-devel', # FreeBSD development snapshot
--        ]
+@@ -209,7 +209,10 @@ class LLVMDependencyConfigTool(ConfigToo
+         # before `super().__init__` is called.
+         HasNativeKwarg.__init__(self, kwargs)
+ 
+-        self.tools = get_llvm_tool_names('llvm-config')
 +        if 'LLVM_CONFIG_PATH' in os.environ:
 +            self.tools = [ os.environ['LLVM_CONFIG_PATH'] ]
 +        else:
-+            self.tools = [
-+                'llvm-config', # base
-+                'llvm-config-8',   'llvm-config80',
-+                'llvm-config-7',   'llvm-config70',
-+                'llvm-config-6.0', 'llvm-config60',
-+                'llvm-config-5.0', 'llvm-config50',
-+                'llvm-config-4.0', 'llvm-config40',
-+                'llvm-config-3.9', 'llvm-config39',
-+                'llvm-config-3.8', 'llvm-config38',
-+                'llvm-config-3.7', 'llvm-config37',
-+                'llvm-config-3.6', 'llvm-config36',
-+                'llvm-config-3.5', 'llvm-config35',
-+                'llvm-config-9',     # Debian development snapshot
-+                'llvm-config-devel', # FreeBSD development snapshot
-+            ]
++            self.tools = get_llvm_tool_names('llvm-config')
  
          # Fedora starting with Fedora 30 adds a suffix of the number
          # of bits in the isa that llvm targets, for example, on x86_64
