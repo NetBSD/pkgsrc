97f10cbfe55fd0b2655bdaf17dd7a6cf3f75fe9b
nia <nia@pkgsrc.org>
2019-10-18 09:04:16 +0000

    libdrm: Update to 2.4.100
    No changelog, needed for updating mesa due to newer amdgpu bits...

--- x11/libdrm/builtin.mk
+++ x11/libdrm/builtin.mk
@@ -1,6 +1,7 @@
-# $NetBSD: builtin.mk,v 1.4 2018/06/01 15:05:43 maya Exp $
+# $NetBSD: builtin.mk,v 1.5 2019/10/18 09:04:16 nia Exp $
 
 BUILTIN_PKG:=	libdrm
+
 .for pcfile in libdrm libdrm_amdgpu libdrm_intel libdrm_nouveau libdrm_radeon
 PKGCONFIG_FILE.libdrm+=	${X11BASE}/lib/pkgconfig/${pcfile}.pc
 PKGCONFIG_FILE.libdrm+=	${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/${pcfile}.pc
