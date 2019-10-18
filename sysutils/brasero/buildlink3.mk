2a735ce4ab956c5eba6a3905b14a89ff28dcf4be
nia <nia@pkgsrc.org>
2019-10-18 12:31:02 +0000

    brasero: Update to 3.12.2.
    Switch to gtk3 and away from gstreamer0.10.

--- sysutils/brasero/buildlink3.mk
+++ sysutils/brasero/buildlink3.mk
@@ -1,19 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.60 2019/07/21 22:24:11 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.61 2019/10/18 12:31:02 nia Exp $
 
 BUILDLINK_TREE+=	brasero
 
 .if !defined(BRASERO_BUILDLINK3_MK)
 BRASERO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.brasero+=	brasero>=2.26.0
-BUILDLINK_ABI_DEPENDS.brasero+=	brasero>=2.32.1nb53
+BUILDLINK_API_DEPENDS.brasero+=	brasero>=3.12.2
 BUILDLINK_PKGSRCDIR.brasero?=	../../sysutils/brasero
 
-.include "../../x11/gtk2/buildlink3.mk"
-.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
-.include "../../audio/libcanberra/buildlink3.mk"
-.include "../../multimedia/totem-pl-parser/buildlink3.mk"
-
-.endif # BRASERO_BUILDLINK3_MK
+.include "../../x11/gtk3/buildlink3.mk"
+.endif	# BRASERO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-brasero
