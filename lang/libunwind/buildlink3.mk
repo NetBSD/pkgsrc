d425afb7788160bd72fe74a34a0c7da523a44f8d
adam <adam@pkgsrc.org>
2019-10-19 13:59:06 +0000

    libcxx: updated to 9.0.0
    Libc++ 9.0.0
    
    Fixes
    
    Minor fixes to std::chrono operators.
    libc++ now correctly handles Objective-C++ ARC qualifiers in std::is_pointer.
    std::span general updates and fixes.
    Updates to the std::abs implementation.
    std::to_chars now adds leading zeros.
    Ensure std::tuple is trivially constructible.
    std::aligned_union now works in C++03.
    Output of nullptr to std::basic_ostream is formatted properly.
    
    Features
    
    Implemented P0608: sane variant converting constructor.
    Added ssize function.
    Added front and back methods in std::span.
    std::is_unbounded_array and std::is_bounded_array added to type traits.
    std::atomic now includes many new features and specialization including improved Freestanding support.
    Added std::midpoint and std::lerp math functions.
    Added the function std::is_constant_evaluated.
    Erase-like algorithms now return size type.
    Added contains method to container types.
    std::swap is now a constant expression.
    
    Updates
    
    libc++ dropped support for GCC 4.9; we now support GCC 5.1 and above.
    libc++ added explicit support for WebAssembly System Interface (WASI).
    Progress towards full support of rvalues and variadics in C++03 mode. std::move and std::forward now both work in C++03 mode.

--- lang/libunwind/buildlink3.mk
+++ lang/libunwind/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2019/06/02 08:35:56 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2019/10/19 13:59:06 adam Exp $
 
 BUILDLINK_TREE+=	libunwind
 
 .if !defined(LIBUNWIND_BUILDLINK3_MK)
 LIBUNWIND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libunwind+=	libunwind>=8.0.0
+BUILDLINK_API_DEPENDS.libunwind+=	libunwind>=9.0.0
 BUILDLINK_PKGSRCDIR.libunwind?=		../../lang/libunwind
 
 .include "../../lang/llvm/buildlink3.mk"
