66073b3abce2561d645544e0a1953291dbb364ea
nia <nia@pkgsrc.org>
2019-10-19 12:37:04 +0000

    poppler: Update to 0.81.0
    pkgsrc changes:
    
    to avoid adding a new dependency on boost, poppler now uses cairo as
    the default graphics backend. the qt5 package seems to require splash (?)
    so that one gets a new boost dependency.
    
    we could also avoid it by forcing cmake detection to fail, but in this
    case it prints a warning about performance losses.
    
    Release 0.81.0:
            core:
             * Splash: Always enable support for CMYK rasterization
             * CairoOutputDev: Check scaled dimensions for 0. Issue #737
             * BaseCryptStream: Fix potential uninitialized memory read
             * SplashBitmap: Fix wrong width condition for splashModeDeviceN8
             * Fix crashes in malformed files
    
    Release 0.80.0:
            core:
             * Annotations: Implement support for setting a different text in the appearance stream than the real text
             * Splash: Optionally use small_vector from boost
             * Fix memory leaks on broken files
             * Fix abort on broken files
             * Small code simplifications
             * Remove USE_FIXEDPOINT support. Issue #821
    
            qt5:
             * Fix MSVC build
             * Add subsitute-font information
             * Fix since marker of some functions
             * Fix leak when aborting text extraction
             * Small code simplifications
    
            glib:
             * Make print scaling getter visible
             * Make Duplex/NumCopies/PrintPageRange preference available in API
             * Implement Movie API
    
            utils:
             * pdftotext: Add -nodiag flag to remove diagonal text on output
    
            build system:
             * Mark external lib include dirs as SYSTEM

--- print/poppler-cpp/buildlink3.mk
+++ print/poppler-cpp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.47 2019/07/23 17:19:57 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.48 2019/10/19 12:37:05 nia Exp $
 
 BUILDLINK_TREE+=	poppler-cpp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-cpp
 POPPLER_CPP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-cpp+=	poppler-cpp>=0.16.7
-BUILDLINK_ABI_DEPENDS.poppler-cpp+=	poppler-cpp>=0.79.0
+BUILDLINK_ABI_DEPENDS.poppler-cpp+=	poppler-cpp>=0.81.0
 BUILDLINK_PKGSRCDIR.poppler-cpp?=	../../print/poppler-cpp
 
 .include "../../print/poppler/buildlink3.mk"
