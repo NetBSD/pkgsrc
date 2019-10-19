6e47f4e6d93b474bc0812ca70ebf50fa21ce4e16
adam <adam@pkgsrc.org>
2019-10-19 13:52:40 +0000

    llvm: updated to 9.0.0
    9.0.0:
    Non-comprehensive list of changes in this release:
    * Two new extension points, namely EP_FullLinkTimeOptimizationEarly and EP_FullLinkTimeOptimizationLast are available for plugins to specialize the legacy pass manager full LTO pipeline.
    * llvm-objcopy/llvm-strip got support for COFF object files/executables, supporting the most common copying/stripping options.
    * The CMake parameter CLANG_ANALYZER_ENABLE_Z3_SOLVER has been replaced by LLVM_ENABLE_Z3_SOLVER.
    * The RISCV target is no longer “experimental” (see Changes to the RISCV Target below for more details).
    * The ORCv1 JIT API has been deprecated. Please see Transitioning from ORCv1 to ORCv2.
    * Support for target-independent hardware loops in IR has been added, with PowerPC and Arm implementations.

--- lang/llvm/patches/patch-cmake_modules_AddLLVM.cmake
+++ lang/llvm/patches/patch-cmake_modules_AddLLVM.cmake
@@ -1,22 +1,22 @@
-$NetBSD: patch-cmake_modules_AddLLVM.cmake,v 1.4 2018/12/09 20:04:38 adam Exp $
+$NetBSD: patch-cmake_modules_AddLLVM.cmake,v 1.5 2019/10/19 13:52:40 adam Exp $
 
 Don't use non-portable -z discard-unused on SunOS.
 On Darwin, create symbolic links to shared libraries.
 On Darwin, use correct install-name for shared libraries.
 
---- cmake/modules/AddLLVM.cmake.orig	2018-08-01 07:51:55.000000000 +0000
+--- cmake/modules/AddLLVM.cmake.orig	2019-07-12 14:40:18.000000000 +0000
 +++ cmake/modules/AddLLVM.cmake
-@@ -218,9 +218,6 @@ function(add_link_opts target_name)
+@@ -222,9 +222,6 @@ function(add_link_opts target_name)
          # ld64's implementation of -dead_strip breaks tools that use plugins.
          set_property(TARGET ${target_name} APPEND_STRING PROPERTY
                       LINK_FLAGS " -Wl,-dead_strip")
 -      elseif(${CMAKE_SYSTEM_NAME} MATCHES "SunOS")
 -        set_property(TARGET ${target_name} APPEND_STRING PROPERTY
 -                     LINK_FLAGS " -Wl,-z -Wl,discard-unused=sections")
-       elseif(NOT WIN32 AND NOT LLVM_LINKER_IS_GOLD AND NOT ${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD")
+       elseif(NOT WIN32 AND NOT LLVM_LINKER_IS_GOLD AND
+              NOT ${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD|AIX")
          # Object files are compiled with -ffunction-data-sections.
-         # Versions of bfd ld < 2.23.1 have a bug in --gc-sections that breaks
-@@ -517,7 +514,7 @@ function(llvm_add_library name)
+@@ -536,7 +533,7 @@ function(llvm_add_library name)
    endif()
  
    if(ARG_SHARED AND UNIX)
@@ -25,7 +25,7 @@ On Darwin, use correct install-name for shared libraries.
        get_target_property(output_name ${name} OUTPUT_NAME)
        if(${output_name} STREQUAL "output_name-NOTFOUND")
          set(output_name ${name})
-@@ -1632,7 +1629,7 @@ function(llvm_setup_rpath name)
+@@ -1727,7 +1724,7 @@ function(llvm_setup_rpath name)
    endif()
  
    if (APPLE)
