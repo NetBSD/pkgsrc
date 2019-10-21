b95879a646a99f4eaeffc85a80e12525a716598f
wiz <wiz@pkgsrc.org>
2019-10-21 16:21:44 +0000

    heimdal: fix build on OpenSSL 1.1 systems by disabling OpenSSL.
    heimdal includes a copy of the relevant functions itself.
    
    Add a comment that the dependency should be re-enabled when updating
    this package.
    
    Bump PKGREVISION.

--- security/heimdal/buildlink3.mk
+++ security/heimdal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.58 2019/04/03 00:33:04 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.59 2019/10/21 16:21:44 wiz Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -18,7 +18,6 @@ pkgbase := heimdal
 .if !empty(PKG_BUILD_OPTIONS.heimdal:Mldap)
 .  include "../../databases/openldap-client/buildlink3.mk"
 .endif
-.include "../../security/openssl/buildlink3.mk"
 
 CHECK_BUILTIN.heimdal:=	yes
 .include "../../security/heimdal/builtin.mk"
