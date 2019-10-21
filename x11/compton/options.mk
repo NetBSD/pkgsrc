d4bdcc84a3ade36b1743a8e49d5e6822bcef65aa
pho <pho@pkgsrc.org>
2019-10-21 15:56:53 +0000

    compton has switched from gmake to meson. Update Makefile so options actually take effect

--- x11/compton/options.mk
+++ x11/compton/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2018/12/01 13:23:51 nia Exp $
+# $NetBSD: options.mk,v 1.2 2019/10/21 15:56:53 pho Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.compton
 PKG_SUPPORTED_OPTIONS=	dbus libdrm opengl pcre
@@ -9,23 +9,23 @@ PKG_SUGGESTED_OPTIONS=	dbus libdrm opengl pcre
 .if !empty(PKG_OPTIONS:Mdbus)
 .include "../../sysutils/dbus/buildlink3.mk"
 .else
-MAKE_FLAGS+=	NO_DBUS=1
+MESON_ARGS+=	-Ddbus=false
 .endif
 
 .if !empty(PKG_OPTIONS:Mlibdrm)
 .include "../../x11/libdrm/buildlink3.mk"
 .else
-MAKE_FLAGS+=	NO_VSYNC_DRM=1
+MESON_ARGS+=	-Dvsync_drm=false
 .endif
 
 .if !empty(PKG_OPTIONS:Mopengl)
 .include "../../graphics/MesaLib/buildlink3.mk"
 .else
-MAKE_FLAGS+=	NO_OPENGL=1
+MESON_ARGS+=	-Dopengl=false
 .endif
 
 .if !empty(PKG_OPTIONS:Mpcre)
 .include "../../devel/pcre/buildlink3.mk"
 .else
-MAKE_FLAGS+=	NO_REGEX_PCRE=1
+MESON_ARGS+=	-Dregex=false
 .endif
