2dd5bdc96dc01d800f583c069992342b982a12a0
mef <mef@pkgsrc.org>
2019-10-21 23:47:07 +0000

    (www/emacs-w3m-snapshot) Updated from 1.4.625+0.20180518 to 1.4.632+0.20190920.1116
    (upstream)
      ChangeLog has too many lines, sorry to omit
    (pkgsrc)
      - Some VARIABLES introduced to ease further version update
      - following two patches dropped
        (patch-w3mhack.el)
           Compile mew-shimbun.el and mew-w3m.el when
           emacs-w3m-mew option is set.
        (patch-aclocal.m4)
           Don't quote ${EGREP}, it may be set to "grep -E".
       - (pkglint) LOCALBASE -> PREFIX

--- www/emacs-w3m-snapshot/buildlink3.mk
+++ www/emacs-w3m-snapshot/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/08/05 22:17:58 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/10/21 23:47:07 mef Exp $
 #
 
 BUILDLINK_TREE+=	emacs-w3m
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	emacs-w3m
 .if !defined(EMACS_W3M_BUILDLINK3_MK)
 EMACS_W3M_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.emacs-w3m+=	${EMACS_PKGNAME_PREFIX}emacs-w3m>=1.4.364
+BUILDLINK_API_DEPENDS.emacs-w3m+=	${EMACS_PKGNAME_PREFIX}emacs-w3m>=1.4.50
 BUILDLINK_PKGSRCDIR.emacs-w3m?=		../../www/emacs-w3m-snapshot
 
 BUILDLINK_CONTENTS_FILTER.emacs-w3m=	${EGREP} '.*\.el$$|.*\.elc$$'
