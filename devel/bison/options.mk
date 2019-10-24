52b9826d6fbda3e15dd530144d840655fc5e8a7c
jperkin <jperkin@pkgsrc.org>
2019-10-24 10:25:39 +0000

    bison: Spell CONFIGURE_ARGS correctly.

--- devel/bison/options.mk
+++ devel/bison/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2019/10/23 11:58:01 triaxx Exp $
+# $NetBSD: options.mk,v 1.2 2019/10/24 10:25:39 jperkin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.bison
 PKG_SUPPORTED_OPTIONS=	nls
@@ -13,10 +13,10 @@ PLIST_VARS+=		nls
 .if !empty(PKG_OPTIONS:Mnls)
 USE_PKGLOCALEDIR=	yes
 USE_TOOLS+=		msgfmt
-CONFIGURE_ARG+=		--enable-nls
+CONFIGURE_ARGS+=	--enable-nls
 CONFIGURE_ENV+=		gt_cv_func_gnugettext1_libintl=yes
 .include "../../devel/gettext-lib/buildlink3.mk"
 PLIST.nls=		yes
 .else
-CONFIGURE_ARG+=		--disable-nls
+CONFIGURE_ARGS+=	--disable-nls
 .endif
