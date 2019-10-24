1857ecd83146ae9f66c404fda57325c4efbf297a
kamil <kamil@pkgsrc.org>
2019-10-24 22:28:46 +0000

    ocsync: Fix build on NetBSD 9.99.17
    Adapt the local patch for the POSIX iconv(3) API change in new NetBSD.

--- net/ocsync/patches/patch-src_std_c__string.c
+++ net/ocsync/patches/patch-src_std_c__string.c
@@ -1,17 +1,33 @@
-$NetBSD: patch-src_std_c__string.c,v 1.1 2013/11/22 12:01:13 ryoon Exp $
+$NetBSD: patch-src_std_c__string.c,v 1.2 2019/10/24 22:28:46 kamil Exp $
 
 * For NetBSD iconv(3), this should be fixed for GNU iconv and NetBSD case.
 
 --- src/std/c_string.c.orig	2013-10-18 13:11:09.000000000 +0000
 +++ src/std/c_string.c
-@@ -83,7 +83,11 @@ enum iconv_direction { iconv_from_native
+@@ -40,6 +40,15 @@
+ #include <windows.h>
+ #endif
+ 
++#if defined(__NetBSD__)
++#include <sys/param.h>
++#if __NetBSD_Prereq__(9,99,17)
++#define NETBSD_POSIX_ICONV 1
++#else
++#define NETBSD_POSIX_ICONV 0
++#endif
++#endif
++
+ #ifdef WITH_ICONV
+ #include <iconv.h>
+ 
+@@ -83,7 +92,11 @@ enum iconv_direction { iconv_from_native
  
  static char *c_iconv(const char* str, enum iconv_direction dir)
  {
-+#if !defined(__NetBSD__)
-   char *in = (char*)str;
-+#else
++#if defined(__NetBSD__) && !NETBSD_POSIX_ICONV
 +  const char *in = (char*)str;
++#else
+   char *in = (char*)str;
 +#endif
    size_t size;
    size_t outsize;
