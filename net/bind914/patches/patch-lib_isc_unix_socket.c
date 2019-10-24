e1f4799326c231ae6e30b95d16ced3be6d0c52a0
otis <otis@pkgsrc.org>
2019-10-24 12:50:36 +0000

    bind914: Fix build on SmartOS
    SmartOS requires _XOPEN_SOURCE for various macros and functions (CMSG_DATA() et
    al.)

--- net/bind914/patches/patch-lib_isc_unix_socket.c
+++ net/bind914/patches/patch-lib_isc_unix_socket.c
@@ -1,10 +1,28 @@
-$NetBSD: patch-lib_isc_unix_socket.c,v 1.2 2019/06/14 16:14:05 taca Exp $
+$NetBSD: patch-lib_isc_unix_socket.c,v 1.3 2019/10/24 12:50:36 otis Exp $
 
 * Apply fixes from NetBSD base system.
+* Fix build on SmartOS. In this special case, _XOPEN_SOURCE has to be only
+  defined on SmartOS.
 
---- lib/isc/unix/socket.c.orig	2019-04-06 20:09:59.000000000 +0000
+--- lib/isc/unix/socket.c.orig	2019-10-02 06:24:10.000000000 +0000
 +++ lib/isc/unix/socket.c
-@@ -225,6 +225,7 @@ typedef enum { poll_idle, poll_active, p
+@@ -11,6 +11,15 @@
+ 
+ /*! \file */
+ 
++/* needed for CMSG_DATA */
++#if defined(__sun)
++#if (__STDC_VERSION__ - 0 < 199901L)
++#define _XOPEN_SOURCE 500
++#else
++#define _XOPEN_SOURCE 600
++#endif
++#endif
++
+ #include <config.h>
+ 
+ #include <inttypes.h>
+@@ -225,6 +234,7 @@ typedef enum { poll_idle, poll_active, p
  			 (e) == EWOULDBLOCK || \
  			 (e) == ENOBUFS || \
  			 (e) == EINTR || \
