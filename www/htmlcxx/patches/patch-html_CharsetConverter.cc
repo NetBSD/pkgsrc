d0f4e2f4c221fd467a515d89a65e02279388055b
kamil <kamil@pkgsrc.org>
2019-10-25 10:29:04 +0000

    htmlcxx: Fix build on NetBSD 9.99.17
    Adapt the local patch for the POSIX iconv(3) API change in new NetBSD.

--- www/htmlcxx/patches/patch-html_CharsetConverter.cc
+++ www/htmlcxx/patches/patch-html_CharsetConverter.cc
@@ -1,15 +1,34 @@
-$NetBSD: patch-html_CharsetConverter.cc,v 1.1 2014/02/16 22:58:51 wiz Exp $
+$NetBSD: patch-html_CharsetConverter.cc,v 1.2 2019/10/25 10:29:04 kamil Exp $
 
 Fix build on NetBSD.
 
 --- html/CharsetConverter.cc.orig	2011-05-15 17:32:10.000000000 +0000
 +++ html/CharsetConverter.cc
-@@ -37,7 +37,7 @@ string CharsetConverter::convert(const s
+@@ -4,6 +4,15 @@
+ #include <cerrno>
+ #include "CharsetConverter.h"
+ 
++#if defined(__NetBSD__)
++#include <sys/param.h>
++#if __NetBSD_Prereq__(9,99,17)
++#define NETBSD_POSIX_ICONV 1
++#else
++#define NETBSD_POSIX_ICONV 0
++#endif
++#endif
++
+ using namespace std;
+ using namespace htmlcxx;
+ 
+@@ -37,7 +46,11 @@ string CharsetConverter::convert(const s
  
  	size_t ret;
  	while (1) {
--		ret = iconv(mIconvDescriptor, const_cast<char**>(&inbuf), &inbytesleft, &outbuf, &outbytesleft);
++#if (defined(__NetBSD__) && !NETBSD_POSIX_ICONV)
 +		ret = iconv(mIconvDescriptor, (const char**)(&inbuf), &inbytesleft, &outbuf, &outbytesleft);
++#else
+ 		ret = iconv(mIconvDescriptor, const_cast<char**>(&inbuf), &inbytesleft, &outbuf, &outbytesleft);
++#endif
  		if (ret == 0) break;
  		if (ret == (size_t)-1 && errno == E2BIG) return string();
  
