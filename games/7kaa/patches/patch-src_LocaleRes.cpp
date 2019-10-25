13481b4c42e6d846dbecaea8e987fb6f7a4c1fb6
kamil <kamil@pkgsrc.org>
2019-10-25 10:00:58 +0000

    7kaa: Fix build on NetBSD 9.99.17
    Adapt the local patch for the POSIX iconv(3) API change in new NetBSD.

--- games/7kaa/patches/patch-src_LocaleRes.cpp
+++ games/7kaa/patches/patch-src_LocaleRes.cpp
@@ -1,14 +1,30 @@
-$NetBSD: patch-src_LocaleRes.cpp,v 1.2 2019/07/19 09:13:51 nia Exp $
+$NetBSD: patch-src_LocaleRes.cpp,v 1.3 2019/10/25 10:00:58 kamil Exp $
 
 Support NetBSD and SunOS iconv being special.
 
 --- src/LocaleRes.cpp.orig	2019-06-22 22:22:26.000000000 +0000
 +++ src/LocaleRes.cpp
-@@ -169,7 +169,11 @@ const char *LocaleRes::conv_str(iconv_t 
+@@ -30,6 +30,15 @@
+ #include <ODB.h>
+ #include <LocaleRes.h>
+ 
++#if defined(__NetBSD__)
++#include <sys/param.h>
++#if __NetBSD_Prereq__(9,99,17)
++#define NETBSD_POSIX_ICONV 1
++#else
++#define NETBSD_POSIX_ICONV 0
++#endif
++#endif
++
+ //------------- End of function Constructor -------//
+ //
+ LocaleRes::LocaleRes()
+@@ -169,7 +178,11 @@ const char *LocaleRes::conv_str(iconv_t 
  	size_t c;
  	while( in_left>0 )
  	{
-+#if defined(__NetBSD__) || defined(__sun)
++#if (defined(__NetBSD__) && !NETBSD_POSIX_ICONV) || defined(__sun)
 +		c = iconv(cd, (const char **)&p1, &in_left, &p2, &out_left);
 +#else
  		c = iconv(cd, &p1, &in_left, &p2, &out_left);
