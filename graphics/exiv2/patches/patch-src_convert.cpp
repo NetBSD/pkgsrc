20d15b2d45b58c4a665d5b9b439d0cfbf8433f9d
kamil <kamil@pkgsrc.org>
2019-10-25 09:44:26 +0000

    exiv2: Fix build on NetBSD 9.99.17
    Adapt the local patch for the POSIX iconv(3) API change in new NetBSD.

--- graphics/exiv2/patches/patch-src_convert.cpp
+++ graphics/exiv2/patches/patch-src_convert.cpp
@@ -1,14 +1,14 @@
-$NetBSD: patch-src_convert.cpp,v 1.1 2019/07/15 22:07:08 nia Exp $
+$NetBSD: patch-src_convert.cpp,v 1.2 2019/10/25 09:44:26 kamil Exp $
 
 Support NetBSD and SunOS iconv being special.
 
---- src/convert.cpp.orig	2019-04-26 09:31:16.000000000 +0000
+--- src/convert.cpp.orig	2019-07-29 06:33:07.000000000 +0000
 +++ src/convert.cpp
 @@ -1545,11 +1545,19 @@ namespace {
              char outbuf[256];
              char* outptr = outbuf;
              size_t outbytesleft = sizeof(outbuf);
-+#if defined(__NetBSD__) || defined(__sun)
++#if (defined(__NetBSD__) && !NETBSD_POSIX_ICONV) || defined(__sun)
 +            size_t rc = iconv(cd,
 +                              const_cast<const char **>(&inptr),
 +                              &inbytesleft,
