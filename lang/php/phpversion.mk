db57fb49abe2505db2ae57405263f4a486a8615a
taca <taca@pkgsrc.org>
2019-10-25 02:51:18 +0000

    lang/php72: update to 7.2.24
    Update php72 to 7.2.24.
    
    
    24 Oct 2019, PHP 7.2.24
    
    - Core:
      . Fixed bug #78535 (auto_detect_line_endings value not parsed as bool).
        (bugreportuser)
      . Fixed bug #78620 (Out of memory error). (cmb, Nikita)
    
    - Exif:
      . Fixed bug #78442 ('Illegal component' on exif_read_data since PHP7)
    	(Kalle)
    
    - FPM:
      . Fixed bug #78599 (env_path_info underflow in fpm_main.c can lead to RCE).
        (CVE-2019-11043) (Jakub Zelenka)
    
    - MBString:
      . Fixed bug #78579 (mb_decode_numericentity: args number inconsistency).
        (cmb)
      . Fixed bug #78609 (mb_check_encoding() no longer supports stringable
        objects). (cmb)
    
    - MySQLi:
      . Fixed bug #76809 (SSL settings aren't respected when persistent connections
        are used). (fabiomsouto)
    
    - PDO_MySQL:
      . Fixed bug #78623 (Regression caused by "SP call yields additional empty
        result set"). (cmb)
    
    - Session:
      . Fixed bug #78624 (session_gc return value for user defined session
        handlers). (bshaffer)
    
    - Standard:
      . Fixed bug #76342 (file_get_contents waits twice specified timeout).
        (Thomas Calvet)
      . Fixed bug #78612 (strtr leaks memory when integer keys are used and the
        subject string shorter). (Nikita)
      . Fixed bug #76859 (stream_get_line skips data if used with data-generating
        filter). (kkopachev)
    
    - Zip:
      . Fixed bug #78641 (addGlob can modify given remove_path value). (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.270 2019/10/02 14:08:05 taca Exp $
+# $NetBSD: phpversion.mk,v 1.271 2019/10/25 02:51:18 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP71_VERSION=	7.1.32
-PHP72_VERSION=	7.2.23
+PHP72_VERSION=	7.2.24
 PHP73_VERSION=	7.3.10
 
 # Define initial release of major version.
