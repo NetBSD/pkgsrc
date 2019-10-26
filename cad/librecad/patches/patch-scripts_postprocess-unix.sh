189fbd1d7caaa9384ba84359082bf109de37d9b0
kamil <kamil@pkgsrc.org>
2019-10-26 12:23:01 +0000

    librecad: Upgrade to 2.2.0 rc1
    Exact changelog is unknown, but there is Qt4 -> Qt5 switch.
    
    Upstream changelog
    ==================
    2.2.0 Release Candidate
    There were more than 250 commits since 2.2.0-alpha, thus it was overdue to get rid of the alpha in the tag now.
    With some more improvements in the last couple weeks, we switch directly to Release Candidate as the response from community is, that the master branch is stable.
    There are only some minor changes in the line before the final 2.2.0 release.

--- cad/librecad/patches/patch-scripts_postprocess-unix.sh
+++ cad/librecad/patches/patch-scripts_postprocess-unix.sh
@@ -1,8 +1,8 @@
-$NetBSD: patch-scripts_postprocess-unix.sh,v 1.3 2016/09/05 21:13:21 plunky Exp $
+$NetBSD: patch-scripts_postprocess-unix.sh,v 1.4 2019/10/26 12:23:01 kamil Exp $
 
-* Set Qt tools path
+Fix QTDIR.
 
---- scripts/postprocess-unix.sh.orig	2016-06-20 18:43:27.000000000 +0000
+--- scripts/postprocess-unix.sh.orig	2018-02-15 20:54:45.000000000 +0000
 +++ scripts/postprocess-unix.sh
 @@ -8,7 +8,7 @@ APPDATADIR="${THISDIR}/unix/appdata"
  TSDIRLC="${LCDIR}/ts"
@@ -10,5 +10,6 @@ $NetBSD: patch-scripts_postprocess-unix.sh,v 1.3 2016/09/05 21:13:21 plunky Exp 
  SPTDIR="${LCDIR}/support"
 -LRELEASE="lrelease"
 +LRELEASE="@QTDIR@/bin/lrelease"
-
+ 
  cd "${THISDIR}"
+ 
