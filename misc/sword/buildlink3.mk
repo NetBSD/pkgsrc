ae84021a73a2fb2670924de36311c03ab01d3a45
kamil <kamil@pkgsrc.org>
2019-10-27 16:59:45 +0000

    sword: Upgrade to 1.8.1
    Full changelog is unknown.
    
    CHANGELOG
    =========
    1.8.0
    
    I would like to take this chance to announce the immediate availability of
    SWORD release 1.8.0. I know this release has been a long time in coming,
    but the long time comes with lots of benefits for users, developers, and
    maintainers. The benefits to users and developers are mentioned elsewhere,
    throughout the code and other places. The main benefit to maintainers is
    that, now, there are automated tests in place and the release process is
    now automated. This means that future releases on that 1.8 branch can be
    easily executed whenever needed. Have a Merry Christmas, everyone! And keep
    your eyes open for a 1.8.1 in the not too distant future to fix up
    buildings in the binding code. Otherwise, you can get the code you're
    looking for below:
    MD5: 095dbd723738c2a232c85685a11827a8 sword-1.8.0.tar.gz
    SHA512: c45f3135255322a77e955297997db2529f31b397c42cc4b9474dc6ec8d329b2233b292078979de5fbf33cad4a1a607aabb66f86501072a729d68e9fc840c8c8e
    sword-1.8.0.tar.gz
    URL: sword-1.8.0.tar.gz
    --Greg

--- misc/sword/buildlink3.mk
+++ misc/sword/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.24 2019/08/22 12:23:35 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2019/10/27 16:59:45 kamil Exp $
 
 BUILDLINK_TREE+=	sword
 
 .if !defined(SWORD_BUILDLINK3_MK)
 SWORD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.sword+=	sword>=1.7.4
-BUILDLINK_ABI_DEPENDS.sword?=	sword>=1.7.4nb24
+BUILDLINK_API_DEPENDS.sword+=	sword>=1.8.1
+BUILDLINK_ABI_DEPENDS.sword?=	sword>=1.8.1
 BUILDLINK_PKGSRCDIR.sword?=	../../misc/sword
 
 .include "../../textproc/icu/buildlink3.mk"
