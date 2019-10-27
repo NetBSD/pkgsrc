89fca259b9ea314f81ad93a887065da23b6ebd92
kamil <kamil@pkgsrc.org>
2019-10-27 14:51:51 +0000

    qjackctl: Switch from qt4 to qt5
    Remove old qt4 options.

--- audio/qjackctl/options.mk
+++ audio/qjackctl/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.2 2016/07/09 11:44:23 rillig Exp $
+# $NetBSD: options.mk,v 1.3 2019/10/27 14:51:51 kamil Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.qjackctl
 PKG_OPTIONS_REQUIRED_GROUPS= qt
-PKG_OPTIONS_GROUP.qt= 	qt4 qt5
-PKG_SUGGESTED_OPTIONS=	qt4
+PKG_OPTIONS_GROUP.qt= 	qt5
+PKG_SUGGESTED_OPTIONS=	qt5
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mqt5)
@@ -13,11 +13,4 @@ CONFIGURE_ARGS+=        --with-qt5=${QTDIR}
 .  if ${OPSYS} != "Darwin"
 .	include "../../x11/qt5-qtx11extras/buildlink3.mk"
 .  endif
-.endif
-
-.if !empty(PKG_OPTIONS:Mqt4)
-CONFIGURE_ARGS+=        --with-qt4=${QTDIR}
-.include "../../x11/qt4-libs/buildlink3.mk"
-.include "../../x11/qt4-tools/buildlink3.mk"
-.include "../../x11/qt4-qdbus/buildlink3.mk"
 .endif
