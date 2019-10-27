39955c2d780283e1540b71bf69140358052da191
he <he@pkgsrc.org>
2019-10-27 18:34:27 +0000

    Bump bootstrap version to 1.38.0 for NetBSD/aarch64 (9.0), NetBSD/amd64 (8.0) and NetBSD/i386 (8.0).  These bootstraps do not use the pkgsrc llvm, i.e. are built with the rust-llvm option.
    Disable warnings as fatal errors; 1.38.0 building 1.38.0 produces
    warnings which would otherwise be fatal, ref.
      https://github.com/rust-lang/rust/issues/65722
    
    Also, revert back to BUILD_TARGET=build which I mistakenly changed
    to "dist" previously.
    
    Bump PKGVERSION.

--- lang/rust/patches/patch-src_bootstrap_lib.rs
+++ lang/rust/patches/patch-src_bootstrap_lib.rs
@@ -1,11 +1,23 @@
-$NetBSD: patch-src_bootstrap_lib.rs,v 1.4 2019/01/19 12:44:08 ryoon Exp $
+$NetBSD: patch-src_bootstrap_lib.rs,v 1.5 2019/10/27 18:34:27 he Exp $
 
 Don't filter out optimization flags.
 FreeBSD has a particular C++ runtime library name
+Also, don't make warnings fatal -- 1.38.0 warns about a construct
+when building 1.38.0, ref.
+  https://github.com/rust-lang/rust/issues/65722
 
 --- src/bootstrap/lib.rs.orig	2019-01-16 09:30:27.000000000 +0000
 +++ src/bootstrap/lib.rs
-@@ -779,7 +779,6 @@ impl Build {
+@@ -104,7 +104,7 @@
+ //! also check out the `src/bootstrap/README.md` file for more information.
+ 
+ // NO-RUSTC-WRAPPER
+-#![deny(warnings, rust_2018_idioms, unused_lifetimes)]
++#![deny(rust_2018_idioms, unused_lifetimes)]
+ 
+ #![feature(core_intrinsics)]
+ #![feature(drain_filter)]
+@@ -768,7 +768,6 @@ impl Build {
          // cc-rs because the build scripts will determine that for themselves.
          let mut base = self.cc[&target].args().iter()
                             .map(|s| s.to_string_lossy().into_owned())
@@ -13,7 +25,7 @@ FreeBSD has a particular C++ runtime library name
                             .collect::<Vec<String>>();
  
          // If we're compiling on macOS then we add a few unconditional flags
-@@ -790,6 +789,11 @@ impl Build {
+@@ -779,6 +778,11 @@ impl Build {
              base.push("-stdlib=libc++".into());
          }
  
