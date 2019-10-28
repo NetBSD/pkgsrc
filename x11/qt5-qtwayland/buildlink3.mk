dec636515ed4e13535ff416a990693cd67f3299e
nia <nia@pkgsrc.org>
2019-10-28 21:35:43 +0000

    Add x11/qt5-qtwayland.
    Qt5 module for Wayland support
    
    Note: while it does seem silly to have this in the X11 category, it's for
    consistency with other core qt5 packages.

---
+++ x11/qt5-qtwayland/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/10/28 21:35:43 nia Exp $
+
+BUILDLINK_TREE+=	qt5-qtwayland
+
+.if !defined(QT5_QTWAYLAND_BUILDLINK3_MK)
+QT5_QTWAYLAND_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt5-qtwayland+=	qt5-qtwayland>=5.9.1
+BUILDLINK_ABI_DEPENDS.qt5-qtwayland+=	qt5-qtwayland>=5.13.1
+BUILDLINK_PKGSRCDIR.qt5-qtwayland?=	../../x11/qt5-qtwayland
+
+BUILDLINK_INCDIRS.qt5-qtwayland+=	qt5/include
+BUILDLINK_LIBDIRS.qt5-qtwayland+=	qt5/lib
+BUILDLINK_LIBDIRS.qt5-qtwayland+=	qt5/plugins
+
+.include "../../devel/wayland/buildlink3.mk"
+.include "../../devel/wayland-protocols/buildlink3.mk"
+
+.endif	# QT5_QTWAYLAND_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt5-qtwayland
