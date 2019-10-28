1ada978ffdb81b17123b57d8f202251bc6243bae
kamil <kamil@pkgsrc.org>
2019-10-28 09:33:29 +0000

    yabause: Upgrade to 0.9.15
    Switch from qt4 to qt5.
    
    CHANGELOG
    =========
    Yabause 0.9.15
    
    August 24, 2016 - guillaume.duhamel - Release
    
    Following our strict release schedule, here's a new Yabause version.
    This new version includes so many new stuff, I cannot list them all.
    (If you really want the full list, it's on our repository)
    
    Arguably, the most notable features of this release are:
    
    - Low-level CD Block emulation
    - CloneCD file format support
    - High resolution for the software renderer
    
    We also included code from other great projects:
    
    - Musashi 68K core (code by Karl Stenerud)
    - SSF sound format playing (code by R. Belmont, Richard Bannister, Neil Corlett)
    
    As many of you should already be aware, Yabause was forked into a new project,
    uoYabause, by devmiyax. We included some of his fixes back into Yabause.
    
    Due to lack of maintainer, this release will be the last to include a gtk port.
    
    Since our last release, we also improved our tools:
    
    - Automated builds for linux, mac and windows
    - Development builds pushed after each commit
    - Translations on Transifex
    - Compatibility reports on our wiki
    
    And finally, we had to move from Paypal to Patreon

---
+++ emulators/yabause/patches/patch-src_qt_ui_UICheatRaw.cpp
@@ -0,0 +1,15 @@
+$NetBSD: patch-src_qt_ui_UICheatRaw.cpp,v 1.1 2019/10/28 09:33:29 kamil Exp $
+
+Fix Qt 5.11.
+
+--- src/qt/ui/UICheatRaw.cpp.orig	2016-08-19 15:31:13.000000000 +0000
++++ src/qt/ui/UICheatRaw.cpp
+@@ -20,8 +20,6 @@
+ #include "UIHexInput.h"
+ #include "../QtYabause.h"
+ 
+-#include <QButtonGroup>
+-
+ UICheatRaw::UICheatRaw( QWidget* p )
+ 	: QDialog( p )
+ {
