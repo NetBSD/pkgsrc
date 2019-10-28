147239745cd1b089c7ecedf22eda75203974e008
maya <maya@pkgsrc.org>
2019-10-28 21:49:22 +0000

    atril: don't enable dvi support by default. bump pkgrevision.
    This option is pulling in t1lib. t1lib is an enormous security risk.
    It hasn't seen maintenance since 2011 and we have local patches for
    security issues from 2011.
    
    Given the lack of attention, it's likely there are more security
    issues lurking.
    
    Documents are usually obtained from untrusted sources, and thus are
    considered a remote attack vector
    Documents may embed their own fonts. If one embeds a T1 font, it might
    be parsed by this unmaintained library.
    
    To avoid this risk, rip out the t1lib dependency.

--- print/atril/options.mk
+++ print/atril/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.3 2019/03/06 05:48:55 gutteridge Exp $
+# $NetBSD: options.mk,v 1.4 2019/10/28 21:49:22 maya Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.atril
 PKG_SUPPORTED_OPTIONS=	dbus pdf ps xps dvi djvu tiff gdk-pixbuf keyring introspection
-PKG_SUGGESTED_OPTIONS=	dbus pdf ps xps dvi djvu tiff gdk-pixbuf keyring
+PKG_SUGGESTED_OPTIONS=	dbus pdf ps xps djvu tiff gdk-pixbuf keyring
 
 .include "../../mk/bsd.options.mk"
 
