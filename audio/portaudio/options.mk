dabe1c99dd226cc8d103ddbdc6f9aa73a676fc87
nia <nia@pkgsrc.org>
2019-10-28 15:32:24 +0000

    portaudio: Add missing patch + debug option.

--- audio/portaudio/options.mk
+++ audio/portaudio/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1 2019/10/20 11:10:49 nia Exp $
+# $NetBSD: options.mk,v 1.2 2019/10/28 15:32:24 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.portaudio
-PKG_SUPPORTED_OPTIONS=		alsa jack
+PKG_SUPPORTED_OPTIONS=		alsa jack debug
 PKG_SUGGESTED_OPTIONS.Linux=	alsa
 
 .include "../../mk/bsd.options.mk"
@@ -20,4 +20,8 @@ CONFIGURE_ARGS+=	--with-jack
 .include "../../audio/jack/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-jack
+.endif
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug-output
 .endif
