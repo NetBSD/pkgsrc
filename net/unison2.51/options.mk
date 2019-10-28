eeff45badbfeee762d0f63efb4ca9d3b96f88329
gdt <gdt@pkgsrc.org>
2019-10-28 20:02:29 +0000

    net/unison2.51: Clean up BUILDLINK_API_DEPENDS
    Drop ocaml declarations as they are ancient (3.x!).
    
    Move ocaml-lablgtk decl to options.mk where the bl3 is included, for
    tidyness and readability.
    
    No functional change intended.

--- net/unison2.51/options.mk
+++ net/unison2.51/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2018/04/30 10:14:29 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2019/10/28 20:02:29 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.unison
 PKG_SUPPORTED_OPTIONS=	lablgtk
@@ -7,7 +7,10 @@ PKG_SUPPORTED_OPTIONS=	lablgtk
 
 .if !empty(PKG_OPTIONS:Mlablgtk)
 MAKE_FLAGS+=	UISTYLE=gtk2
+BUILDLINK_API_DEPENDS.ocaml-lablgtk+= ocaml-lablgtk>=2.16.0
+
 DEPENDS+=	font-schumacher-misc>=1.0:../../fonts/font-schumacher-misc
+
 .include "../../x11/ocaml-lablgtk/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .else
