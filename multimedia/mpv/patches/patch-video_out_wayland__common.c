8d8c725aaf8e99655eef488626e34afe645dca03
nia <nia@pkgsrc.org>
2019-10-29 22:46:39 +0000

    *: fix manual definition of evdev mouse button ids

--- multimedia/mpv/patches/patch-video_out_wayland__common.c
+++ multimedia/mpv/patches/patch-video_out_wayland__common.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-video_out_wayland__common.c,v 1.2 2019/10/25 14:35:56 leot Exp $
+$NetBSD: patch-video_out_wayland__common.c,v 1.3 2019/10/29 22:46:39 nia Exp $
 
 Allow building on systems without evdev.
 
@@ -8,12 +8,12 @@ Allow building on systems without evdev.
  #include <limits.h>
  #include <poll.h>
  #include <unistd.h>
-+#if defined(__linux) || defined(__FreeBSD__)
++#if defined(__linux__) || defined(__FreeBSD__)
  #include <linux/input.h>
 +#else
-+#define BTN_LEFT	(0)
-+#define BTN_MIDDLE	(1)
-+#define BTN_RIGHT	(2)
++#define BTN_LEFT	(0x110)
++#define BTN_RIGHT	(0x111)
++#define BTN_MIDDLE	(0x112)
 +#endif
  #include <time.h>
  #include "common/msg.h"
