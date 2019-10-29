891348c02969ec2e5a45c9ed0f5c24c7e6b3395f
kamil <kamil@pkgsrc.org>
2019-10-29 17:46:36 +0000

    x11/py-qt5-qscintilla: import py37-qt5-qscintilla-2.11.2
    These are the python-bindings for QScintilla2.
    
    Qt(TM) is a GUI software toolkit. Qt simplifies the task of writing and
    maintaining GUI (graphical user interface) applications.
    
    Qt is written in C++ and is fully object-oriented. It has everything you need
    to create professional GUI applications. And it enables you to create them
    quickly.
    
    Qt is a multi-platform toolkit. When developing software with Qt, you can run
    it on the X Window System (Unix/X11) or Microsoft Windows NT and Windows 95/98.
    Simply recompile your source code on the platform you want.
    
    Qt cuts down the complexity in implementing large and complex systems. Its
    ingenious signal-slot technology enables true component programming.

---
+++ x11/py-qt5-qscintilla/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/10/29 17:46:36 kamil Exp $
+
+BUILDLINK_TREE+=	py-qt5-qscintilla
+
+.if !defined(PY_QT5_QSCINTILLA_BUILDLINK3_MK)
+PY_QT5_QSCINTILLA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.py-qt5-qscintilla+=	${PYPKGPREFIX}-qt5-qscintilla>=2.11.2
+BUILDLINK_ABI_DEPENDS.py-qt5-qscintilla+=	${PYPKGPREFIX}-qt5-qscintilla>=2.11.2
+BUILDLINK_PKGSRCDIR.py-qt5-qscintilla?=		../../x11/py-qt5-qscintilla
+
+.include "../../x11/py-sip/buildlink3.mk"
+.include "../../x11/py-qt5/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qscintilla/buildlink3.mk"
+.endif	# PY_QT5_QSCINTILLA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-py-qt5-qscintilla
