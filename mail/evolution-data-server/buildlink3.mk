d98d5263db7267875571b21858cd972ef45aadf8
nia <nia@pkgsrc.org>
2019-10-30 17:26:49 +0000

    evolution: Update to 3.34.1
    Switched to GTK3, etc.

--- mail/evolution-data-server/buildlink3.mk
+++ mail/evolution-data-server/buildlink3.mk
@@ -1,25 +1,31 @@
-# $NetBSD: buildlink3.mk,v 1.69 2019/05/05 22:49:49 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.70 2019/10/30 17:26:50 nia Exp $
+# XXX
+# XXX This file was created automatically using createbuildlink-3.17.
+# XXX After this file has been verified as correct, the comment lines
+# XXX beginning with "XXX" should be removed.  Please do not commit
+# XXX unverified buildlink3.mk files.
+# XXX
+# XXX Packages that only install static libraries or headers should
+# XXX include the following line:
+# XXX
+# XXX	BUILDLINK_DEPMETHOD.evolution-data-server?=	build
 
 BUILDLINK_TREE+=	evolution-data-server
 
 .if !defined(EVOLUTION_DATA_SERVER_BUILDLINK3_MK)
 EVOLUTION_DATA_SERVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.evolution-data-server+=	evolution-data-server>=1.8.0
-BUILDLINK_ABI_DEPENDS.evolution-data-server+=	evolution-data-server>=2.32.3nb60
-BUILDLINK_PKGSRCDIR.evolution-data-server?=	../../mail/evolution-data-server
+BUILDLINK_API_DEPENDS.evolution-data-server+=	evolution-data-server>=3.35.1
+BUILDLINK_PKGSRCDIR.evolution-data-server?=	../../mywip/evolution-data-server
 
-BDB_ACCEPTED?=		db4 db5
 .include "../../databases/sqlite3/buildlink3.mk"
-.include "../../devel/GConf/buildlink3.mk"
+.include "../../devel/nspr/buildlink3.mk"
 .include "../../devel/nss/buildlink3.mk"
 .include "../../net/libsoup/buildlink3.mk"
-.include "../../net/ORBit2/buildlink3.mk"
-.include "../../net/libgdata0.6/buildlink3.mk"
-.include "../../time/libical/buildlink3.mk"
-.include "../../security/libgnome-keyring/buildlink3.mk"
+.include "../../textproc/icu/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
-.include "../../mk/bdb.buildlink3.mk"
-.endif # EVOLUTION_DATA_SERVER_BUILDLINK3_MK
+.include "../../time/libical/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.endif	# EVOLUTION_DATA_SERVER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-evolution-data-server
