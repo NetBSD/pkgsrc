2af5d5f41c85f37c4a124456cfbcd92cb1284ed8
jperkin <jperkin@pkgsrc.org>
2019-10-31 15:05:52 +0000

    gmake: Unbreak PREFER_PKGSRC circular dependencies.
    Just because a builtin is available does not mean a user has configured their
    system to use it.  Check for USE_BUILTIN instead of IS_BUILTIN to ensure both
    the builtin is available, and the user has allowed pkgsrc to use it, before
    enabling.

--- devel/gmake/options.mk
+++ devel/gmake/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2019/10/28 10:03:18 triaxx Exp $
+# $NetBSD: options.mk,v 1.8 2019/10/31 15:05:52 jperkin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gmake
 PKG_SUPPORTED_OPTIONS=	nls
@@ -12,10 +12,9 @@ PLIST_VARS+=		nls
 # to avoid a circular dependency (gmake->gettext-tools->ncurses->gmake).
 .include "../../devel/gettext-lib/builtin.mk"
 .if !empty(PKG_OPTIONS:Mnls) && \
-    !empty(IS_BUILTIN.gettext:M[yY][eE][sS])
+    !empty(USE_BUILTIN.gettext:M[yY][eE][sS])
 USE_PKGLOCALEDIR=	yes
 USE_TOOLS+=		msgfmt
-USE_BUILTIN.gettext=	yes
 .  include "../../devel/gettext-lib/buildlink3.mk"
 PLIST.nls=		yes
 .else
