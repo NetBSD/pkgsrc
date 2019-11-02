50235f4d157af22f8e67e94a7e5f8f34b1c30a70
rillig <rillig@pkgsrc.org>
2019-11-02 16:16:18 +0000

    math: align variable assignments
    pkglint -Wall -F --only aligned -r
    
    Manual correction in R/Makefile.extension for the MASTER_SITES
    continuation line.

--- math/mpcomplex/builtin.mk
+++ math/mpcomplex/builtin.mk
@@ -1,11 +1,11 @@
-# $NetBSD: builtin.mk,v 1.1 2011/07/08 09:40:57 drochner Exp $
+# $NetBSD: builtin.mk,v 1.2 2019/11/02 16:16:20 rillig Exp $
 
 BUILTIN_PKG:=	mpcomplex
 
 PKGCONFIG_FILE.mpcomplex=	/usr/include/mpc.h
 PKGCONFIG_BASE.mpcomplex=	/usr
 
-BUILTIN_VERSION_SCRIPT.mpcomplex= ${AWK} \
+BUILTIN_VERSION_SCRIPT.mpcomplex=	${AWK} \
 	'/\#define[ \t]*MPC_VERSION_STRING[ \t]/ { \
 	v = substr($$3, 2, length($$3)-2); } \
 	END { print v; }'
