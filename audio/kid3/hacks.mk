68fa863d3b4cfbb66a7d4a4ce705fb6015e29106
rillig <rillig@pkgsrc.org>
2019-11-02 21:09:12 +0000

    audio: align variable assignments and directives
    pkglint -Wall -F --only aligned --only indent -r
    
    No manual corrections.

--- audio/kid3/hacks.mk
+++ audio/kid3/hacks.mk
@@ -1,7 +1,7 @@
-# $NetBSD: hacks.mk,v 1.1 2018/01/21 04:10:44 markd Exp $
+# $NetBSD: hacks.mk,v 1.2 2019/11/02 21:09:14 rillig Exp $
 
 # linking -lQt5Widgets on NetBSD with gcc produces
 # ld: BFD (NetBSD Binutils nb1) 2.27 assertion fail
 # /usr/src/external/gpl3/binutils/lib/libbfd/../../dist/bfd/elflink.c:2694
 # unless -lexecinfo preceeds -lQt5Widgets
-BUILDLINK_TRANSFORM.NetBSD+=l:Qt5Widgets:execinfo:Qt5Widgets
+BUILDLINK_TRANSFORM.NetBSD+=	l:Qt5Widgets:execinfo:Qt5Widgets
