b7391c5bf54ef13078939beb4b89155d01381c58
mrg <mrg@pkgsrc.org>
2019-11-02 22:25:46 +0000

    update csound6 to 6.13.  changes since 6.03.02 include:
    - new opcodes and generators:
      pinker, exciter, vactrol, hdf5 family, many new array-operations,
      lenarray, getseed, tabifd, websocket, framebuffer, olabuffer,
      compress2, directory, ftsamplebank, mvclpf1, mvclpf2, mvclpf3,
      mvclpf4, mvchpf, cepsinv, moogladder2, paulstretch, mp3scal,
      filescal, dct, getftargs, mfb, quadbezier, select, midiarp,
      hilbert2, ableton link, pvstrace, ceil, floor, round, int, frac,
      powoftwo, abs, log2, log10, log, exp, sqrt, cos, sin, tan, acos,
      asin, atan, sinh, cosh, tanh, cbrt, limit1, atan2, pow,hypot,
      fmod, fmax, fmin, limit, tvconv, liveconv, bpf, xyscale, ntom,
      mton, OSCsend, OSCraw, sorta, sortd, dot, zdf_1pole_mode,
      zdf_2pole_mode, zdf_ladder, zdf_1pole and zdf_2pole.xml,
      diode_ladder, z35_hpf, K35_lpf, product, sc_phasor, sc_lag,
      sc_lagud, sc_trig, midiout_i, chngetks, chnsetks, squinewave,
      gen53, loscilphs, loscil3phs, balance2, fluidinfo, ftaudio,
      OSCcount, faustplay, faustdsp, OSCbundle, beosc, beadsyn,
      printarray, reshapearray, trim, trim_i, string2array, nstrstr,
      ntof, ampmidicurve
    - deprecated opcodes:
      stackops, sndload
    - fixes and feature extensions to many opcodes
    - new while loop construct
    - ?: now does strings as well as numbers
    - better mp3 support
    - many, many bugs fixed
    - many internal artificial limits removed
    - better debugging and instrumentation
    - src_conv utility improved
    - floating point tempo
    - higher precision p2 and p3
    - html5 frontend removed
    - better server support

--- audio/csound6/patches/patch-Top_one_file.c
+++ audio/csound6/patches/patch-Top_one_file.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-Top_one_file.c,v 1.3 2014/08/06 06:17:07 mrg Exp $
+$NetBSD: patch-Top_one_file.c,v 1.4 2019/11/02 22:25:46 mrg Exp $
 
 Add NetBSD and DragonFlyBSD support.
 
---- Top/one_file.c.orig	2014-05-04 04:49:41.000000000 -0700
-+++ Top/one_file.c	2014-08-04 22:21:26.000000000 -0700
+--- Top/one_file.c.orig	2019-07-12 14:54:19.000000000 -0700
++++ Top/one_file.c	2019-10-29 13:56:09.162659487 -0700
 @@ -29,7 +29,7 @@
  #include <stdlib.h>
  #include "corfile.h"
@@ -13,16 +13,7 @@ Add NetBSD and DragonFlyBSD support.
  #  include <sys/types.h>
  #  include <sys/stat.h>
  #endif
-@@ -62,7 +62,7 @@
- {
- #define   nBytes (256)
-     char lbuf[256];
--#if defined(LINUX) || defined(__MACH__)
-+#if defined(LINUX) || defined(__MACH__) || defined(__NetBSD__) || defined(__DragonFly__)
-     struct stat tmp;
- #elif defined(WIN32)
-     struct _stat tmp;
-@@ -94,7 +94,7 @@
+@@ -96,7 +96,7 @@
        }
  #endif
        if (ext != NULL && ext[0] != (char) 0) {
@@ -31,12 +22,3 @@ Add NetBSD and DragonFlyBSD support.
          char  *p;
          /* remove original extension (does not work on OS X */
          /* and may be a bad idea) */
-@@ -114,7 +114,7 @@
-           } while (lbuf[i] != '\0');
-       }
- #endif
--#if defined(LINUX) || defined(__MACH__)
-+#if defined(LINUX) || defined(__MACH__) || defined(__NetBSD__) || defined(__DragonFly__)
-       /* if the file already exists, try again */
-     } while (stat(lbuf, &tmp) == 0);
- #elif defined(WIN32)
