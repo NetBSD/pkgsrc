e14c58829fc71528340cf088e298a474563a7c4e
rillig <rillig@pkgsrc.org>
2019-11-02 15:37:59 +0000

    databases: align variable assignments
    pkglint -Wall -F --only aligned -r
    
    No manual corrections.

--- databases/qore-freetds-module/options.mk
+++ databases/qore-freetds-module/options.mk
@@ -1,11 +1,11 @@
-# $NetBSD: options.mk,v 1.1 2014/12/30 15:41:23 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2019/11/02 15:38:10 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.qore
 PKG_SUPPORTED_OPTIONS=	debug
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdebug)
-CONFIGURE_ARGS+=        --enable-debug
+CONFIGURE_ARGS+=	--enable-debug
 .else
-CONFIGURE_ARGS+=        --disable-debug
+CONFIGURE_ARGS+=	--disable-debug
 .endif
