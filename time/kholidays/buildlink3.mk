b69e96e9a06a0b98473a0a38f3c99931034864be
rillig <rillig@pkgsrc.org>
2019-11-02 16:28:37 +0000

    time: align variable assignments
    pkglint -Wall -F --only aligned -r
    
    No manual corrections.

--- time/kholidays/buildlink3.mk
+++ time/kholidays/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2019/04/03 00:33:09 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2019/11/02 16:28:38 rillig Exp $
 
 BUILDLINK_TREE+=	kholidays
 
@@ -7,7 +7,7 @@ KHOLIDAYS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kholidays+=	kholidays>=5.44.0
 BUILDLINK_ABI_DEPENDS.kholidays?=	kholidays>=5.47.0nb4
-BUILDLINK_PKGSRCDIR.kholidays?=	../../time/kholidays
+BUILDLINK_PKGSRCDIR.kholidays?=		../../time/kholidays
 
 .include "../../x11/qt5-qtdeclarative/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
