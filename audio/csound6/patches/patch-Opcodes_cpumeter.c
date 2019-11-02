ce01847d45600408d54ab1e8d2cc9a6982d30b9e
mrg <mrg@pkgsrc.org>
2019-11-02 22:25:46 +0000

    update csound6 to 6.13.  changes since 6.03.02 include:
    - new opcodes and generators:
      pinker, exciter, vactrol, hdf5 family, many new array-operations,
      lenarray, getseed, tabifd, websocket, framebuffer, olabuffer,
      compress2, directory, ftsamplebank, mvclpf1, mvclpf2, mvclpf3,
      mvclpf4, mvchpf, cepsinv, moogladder2, paulstretch, mp3scal,
      filescal, dct, getftargs, mfb, quadbezier, select, midiarp,
      hilbert2, ableton link, pvstrace, ceil, floor, round, int, frac,
      powoftwo, abs, log2, log10, log, exp, sqrt, cos, sin, tan, acos,
      asin, atan, sinh, cosh, tanh, cbrt, limit1, atan2, pow,hypot,
      fmod, fmax, fmin, limit, tvconv, liveconv, bpf, xyscale, ntom,
      mton, OSCsend, OSCraw, sorta, sortd, dot, zdf_1pole_mode,
      zdf_2pole_mode, zdf_ladder, zdf_1pole and zdf_2pole.xml,
      diode_ladder, z35_hpf, K35_lpf, product, sc_phasor, sc_lag,
      sc_lagud, sc_trig, midiout_i, chngetks, chnsetks, squinewave,
      gen53, loscilphs, loscil3phs, balance2, fluidinfo, ftaudio,
      OSCcount, faustplay, faustdsp, OSCbundle, beosc, beadsyn,
      printarray, reshapearray, trim, trim_i, string2array, nstrstr,
      ntof, ampmidicurve
    - deprecated opcodes:
      stackops, sndload
    - fixes and feature extensions to many opcodes
    - new while loop construct
    - ?: now does strings as well as numbers
    - better mp3 support
    - many, many bugs fixed
    - many internal artificial limits removed
    - better debugging and instrumentation
    - src_conv utility improved
    - floating point tempo
    - higher precision p2 and p3
    - html5 frontend removed
    - better server support

--- audio/csound6/patches/patch-Opcodes_cpumeter.c
+++ audio/csound6/patches/patch-Opcodes_cpumeter.c
@@ -1,17 +1,17 @@
-$NetBSD: patch-Opcodes_cpumeter.c,v 1.3 2014/08/06 06:17:07 mrg Exp $
+$NetBSD: patch-Opcodes_cpumeter.c,v 1.4 2019/11/02 22:25:46 mrg Exp $
 
 Add NetBSD and DragonFlyBSD support.
 
 be best to patch this to use eg, getloadavg() or something not /proc.
 
---- Opcodes/cpumeter.c.orig	2014-05-04 04:49:41.000000000 -0700
-+++ Opcodes/cpumeter.c	2014-08-04 22:19:34.000000000 -0700
-@@ -17,7 +17,7 @@
-  */
+--- Opcodes/cpumeter.c.orig	2019-07-12 14:54:19.000000000 -0700
++++ Opcodes/cpumeter.c	2019-10-29 13:44:20.487656938 -0700
+@@ -36,7 +36,7 @@
+ #include <float.h>
  
  // only available on Linux (no /proc/stat on OSX)
 -#if defined(LINUX)
 +#if defined(LINUX) || defined(__NetBSD__) || defined(__DragonFly__)
- 
- #include "csoundCore.h"
- #include <sys/resource.h>
+ /*######  Miscellaneous global stuff  ####################################*/
+ #define SMLBUFSIZ (512)
+ #define TEST (0)
--- audio/csound6/patches/patch-Top_csound.c
+++ audio/csound6/patches/patch-Top_csound.c
@@ -1,20 +1,20 @@
-$NetBSD: patch-Top_csound.c,v 1.2 2014/08/05 05:12:38 mrg Exp $
+$NetBSD: patch-Top_csound.c,v 1.3 2019/11/02 22:25:46 mrg Exp $
 
 Add NetBSD and DragonFlyBSD support.
 
---- Top/csound.c.orig	2014-01-07 08:54:20.000000000 -0800
-+++ Top/csound.c	2014-02-12 23:44:07.000000000 -0800
-@@ -900,7 +900,8 @@
+--- Top/csound.c.orig	2019-07-12 14:54:19.000000000 -0700
++++ Top/csound.c	2019-10-29 13:50:02.540610805 -0700
+@@ -1027,7 +1027,8 @@
  
  #if defined(ANDROID) || (!defined(LINUX) && !defined(SGI) && \
                           !defined(__HAIKU__) && !defined(__BEOS__) && \
--                         !defined(__MACH__))
-+                         !defined(__MACH__) && !defined(__NetBSD__) && \
-+			 !defined(__DragonFly__))
+-                         !defined(__MACH__) && !defined(__EMSCRIPTEN__))
++                         !defined(__MACH__) && !defined(__EMSCRIPTEN__) && \
++                         !defined(__NetBSD__) && defined(__DragonFly__))
+ 
  static char *signal_to_string(int sig)
  {
-     switch(sig) {
-@@ -1053,7 +1054,7 @@
+@@ -1226,7 +1227,7 @@
  }
  
  static const int sigs[] = {
