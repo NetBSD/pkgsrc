2e0da2817eabd376570060fde5256c6cb8b930dd
rillig <rillig@pkgsrc.org>
2019-11-02 12:54:38 +0000

    www/php-apcu: fix pkglint warnings and configure options

--- www/php-apcu/buildlink3.mk
+++ www/php-apcu/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2017/12/13 11:34:47 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/11/02 12:54:38 rillig Exp $
 
 BUILDLINK_TREE+=	php-apcu
 
 .if !defined(PHP_APCU_BUILDLINK3_MK)
 PHP_APCU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.php-apcu+=    ${PHP_PKG_PREFIX}-apcu>=5.0.0
-BUILDLINK_PKGSRCDIR.php-apcu?=	../../www/php-apcu
+BUILDLINK_API_DEPENDS.php-apcu+=	${PHP_PKG_PREFIX}-apcu>=5.0.0
+BUILDLINK_PKGSRCDIR.php-apcu?=		../../www/php-apcu
 .endif # PHP_APCU_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-php-apcu
