01c6083cc1cfc4d628473258f3e809d7e87c50e1
rillig <rillig@pkgsrc.org>
2019-11-02 21:09:12 +0000

    audio: align variable assignments and directives
    pkglint -Wall -F --only aligned --only indent -r
    
    No manual corrections.

--- audio/timidity/options.mk
+++ audio/timidity/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2012/07/24 19:47:43 jperkin Exp $
+# $NetBSD: options.mk,v 1.3 2019/11/02 21:09:17 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.timidity
 PKG_SUPPORTED_OPTIONS=	x11 gtk
@@ -18,17 +18,17 @@ PLIST.x11=	yes
 
 .include "../../mk/bsd.prefs.mk"
 
-.if !defined(XAW_TYPE)
-WITH_XAWLIB=--with-xawlib=xaw,xaw3d,neXtaw
-.elif ${XAW_TYPE} == "standard"
-WITH_XAWLIB=--with-xawlib=xaw,xaw3d,neXtaw
-.elif ${XAW_TYPE} == "3d"
-WITH_XAWLIB=--with-xawlib=xaw3d
-.elif ${XAW_TYPE} == "xpm"
-WITH_XAWLIB=--with-xawlib=xaw3d
-.elif ${XAW_TYPE} == "neXtaw"
-WITH_XAWLIB=--with-xawlib=neXtaw
-.endif
+.  if !defined(XAW_TYPE)
+WITH_XAWLIB=	--with-xawlib=xaw,xaw3d,neXtaw
+.  elif ${XAW_TYPE} == "standard"
+WITH_XAWLIB=	--with-xawlib=xaw,xaw3d,neXtaw
+.  elif ${XAW_TYPE} == "3d"
+WITH_XAWLIB=	--with-xawlib=xaw3d
+.  elif ${XAW_TYPE} == "xpm"
+WITH_XAWLIB=	--with-xawlib=xaw3d
+.  elif ${XAW_TYPE} == "neXtaw"
+WITH_XAWLIB=	--with-xawlib=neXtaw
+.  endif
 
 CONFIGURE_ARGS+=	--enable-xaw --enable-xdnd
 CONFIGURE_ARGS+=	--with-xaw-resource-prefix="${PREFIX}/lib/X11/"
@@ -41,10 +41,10 @@ CONFIGURE_ARGS+=	${WITH_XAWLIB}
 .include "../../mk/xaw.buildlink3.mk"
 #.include "../../x11/libXft/buildlink3.mk"
 
-.if !empty(PKG_OPTIONS:Mgtk)
+.  if !empty(PKG_OPTIONS:Mgtk)
 CONFIGURE_ARGS+=	--enable-gtk
 .include "../../x11/gtk2/buildlink3.mk"
-.endif
+.  endif
 
 .else
 CONFIGURE_ARGS+=	--without-x
