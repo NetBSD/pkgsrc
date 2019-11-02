546ef83c02a7de3e5e2ea73691b11ad5f8351086
rillig <rillig@pkgsrc.org>
2019-11-02 22:54:25 +0000

    archivers: align variable assignments
    pkglint -Wall -F --only aligned --only indent -r
    
    No manual corrections.

--- archivers/ocaml-decompress/buildlink3.mk
+++ archivers/ocaml-decompress/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/12 18:00:02 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/11/02 22:54:27 rillig Exp $
 
 BUILDLINK_TREE+=	ocaml-decompress
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-decompress
 OCAML_DECOMPRESS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-decompress+=	ocaml-decompress>=0.8.1
-BUILDLINK_PKGSRCDIR.ocaml-decompress?=	../../archivers/ocaml-decompress
+BUILDLINK_PKGSRCDIR.ocaml-decompress?=		../../archivers/ocaml-decompress
 
 .endif	# OCAML_DECOMPRESS_BUILDLINK3_MK
 
