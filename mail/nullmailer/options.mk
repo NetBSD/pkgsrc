60aec349b707a539098584e50c752326b0ecfb02
rillig <rillig@pkgsrc.org>
2019-11-02 16:25:17 +0000

    mail: align variable assignments
    pkglint -Wall -F --only aligned -r
    
    No manual corrections.

--- mail/nullmailer/options.mk
+++ mail/nullmailer/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.3 2018/10/13 14:38:54 schmonz Exp $
+# $NetBSD: options.mk,v 1.4 2019/11/02 16:25:24 rillig Exp $
 
-PKG_OPTIONS_VAR=        PKG_OPTIONS.nullmailer
-PKG_SUPPORTED_OPTIONS=  gnutls
-PKG_SUGGESTED_OPTIONS=  gnutls
+PKG_OPTIONS_VAR=	PKG_OPTIONS.nullmailer
+PKG_SUPPORTED_OPTIONS=	gnutls
+PKG_SUGGESTED_OPTIONS=	gnutls
 
 .include "../../mk/bsd.options.mk"
 
