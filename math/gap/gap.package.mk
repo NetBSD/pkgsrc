a0d277b95b73790d22a4a67b0f823888ba0022f5
rillig <rillig@pkgsrc.org>
2019-11-02 16:16:18 +0000

    math: align variable assignments
    pkglint -Wall -F --only aligned -r
    
    Manual correction in R/Makefile.extension for the MASTER_SITES
    continuation line.

--- math/gap/gap.package.mk
+++ math/gap/gap.package.mk
@@ -1,14 +1,14 @@
-# $NetBSD: gap.package.mk,v 1.14 2007/02/22 19:26:47 wiz Exp $
+# $NetBSD: gap.package.mk,v 1.15 2019/11/02 16:16:19 rillig Exp $
 #
 # Set GAP_PKG_NAME in your Makefile. More may appear here later.
 
 # Note that between 4.3nb1 == bug fix 3 and 4.3nb2 == bug fix 4 there are
 # some interface changes
 
-DEPENDS+=	gap>=4.3.5:../../math/gap
-GAP_DIRECTORY=	${LOCALBASE}/lib/gap4.3
+DEPENDS+=		gap>=4.3.5:../../math/gap
+GAP_DIRECTORY=		${LOCALBASE}/lib/gap4.3
 GAP_PKG_ROOT=		${GAP_DIRECTORY}/pkg
 GAP_PKG_DIRECTORY=	${GAP_PKG_ROOT}/${GAP_PKG_NAME}
-GAP_PLATFORM=	${MACHINE_GNU_PLATFORM}-${CC}
+GAP_PLATFORM=		${MACHINE_GNU_PLATFORM}-${CC}
 
 PLIST_SUBST+=		GAP_PLATFORM=${GAP_PLATFORM:Q}
