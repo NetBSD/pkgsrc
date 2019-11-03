acc4f6d306a4ab721849fd3ff89dd87d2685e060
rillig <rillig@pkgsrc.org>
2019-11-03 19:24:31 +0000

    emulators: align variable assignments
    pkglint -Wall -F --only aligned --only indent -r
    
    No manual corrections.

--- emulators/unicorn/buildlink3.mk
+++ emulators/unicorn/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/02/26 02:25:28 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/11/03 19:24:34 rillig Exp $
 
 BUILDLINK_TREE+=	unicorn
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	unicorn
 UNICORN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.unicorn+=	unicorn>=1.0.1
-BUILDLINK_PKGSRCDIR.unicorn?=		../../emulators/unicorn
+BUILDLINK_PKGSRCDIR.unicorn?=	../../emulators/unicorn
 .endif	# UNICORN_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-unicorn
