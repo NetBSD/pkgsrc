46409c4882711453d7aea1d81be253f635aa38d1
kamil <kamil@pkgsrc.org>
2019-11-03 12:10:29 +0000

    lld: Sync local patches with merged and pending patches upstream
    Cherry-pick upstream patch commit 2a0fcae3d4d1fd85d6ae8378d7c6f12430c0087d
    [lld] [ELF] Add '-z nognustack' opt to suppress emitting PT_GNU_STAC dummy option for an AArch64 erratum.
    
    [LLD] Add NetBSD support as a new flavor of LLD (nb.lld)
    https://reviews.llvm.org/D69755
    
    Clang >=clang-9.0.0nb3 is required.

--- devel/lld/patches/patch-test_ELF_gnustack.s
+++ devel/lld/patches/patch-test_ELF_gnustack.s
@@ -1,7 +1,7 @@
-$NetBSD: patch-test_ELF_gnustack.s,v 1.3 2019/10/21 22:07:58 rjs Exp $
+$NetBSD: patch-test_ELF_gnustack.s,v 1.4 2019/11/03 12:10:29 kamil Exp $
 
-Add '-z nognustack' option to disable emitting PT_GNU_STACK.
-https://reviews.llvm.org/D56554
+Cherry-pick upstream patch commit 2a0fcae3d4d1fd85d6ae8378d7c6f12430c0087d
+[lld] [ELF] Add '-z nognustack' opt to suppress emitting PT_GNU_STAC dummy option for an AArch64 erratum.
 
 --- test/ELF/gnustack.s.orig	2019-05-01 05:49:01.000000000 +0000
 +++ test/ELF/gnustack.s
@@ -9,8 +9,8 @@ https://reviews.llvm.org/D56554
  # RUN: ld.lld %t1 -o %t -z noexecstack
  # RUN: llvm-readobj --program-headers -S %t | FileCheck --check-prefix=RW %s
  
-+# RUN: ld.lld %t1 -o %t -z noexecstack
-+# RUN: llvm-readobj --program-headers -S %t | FileCheck --check-prefix=NOGNUSTACK %s
++# RUN: ld.lld %t1 -o %t -z nognustack
++# RUN: llvm-readobj --program-headers -s %t | FileCheck --check-prefix=NOGNUSTACK %s
 +
  # RW:      Type: PT_GNU_STACK
  # RW-NEXT: Offset: 0x0
