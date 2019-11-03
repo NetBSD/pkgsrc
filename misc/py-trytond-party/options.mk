20669916996e6b645f338c462d0d16eae7b11435
rillig <rillig@pkgsrc.org>
2019-11-03 17:04:19 +0000

    misc: align variable assignments
    pkglint -Wall -F --only aligned --only indent -r
    
    No manual corrections.

--- misc/py-trytond-party/options.mk
+++ misc/py-trytond-party/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2019/09/02 13:19:44 adam Exp $
+# $NetBSD: options.mk,v 1.7 2019/11/03 17:04:26 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-trytond-party
 PKG_SUPPORTED_OPTIONS=	vatnumber
@@ -7,6 +7,6 @@ PKG_SUGGESTED_OPTIONS+=	vatnumber
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mvatnumber)
-PYTHON_VERSIONS_ACCEPTED=	 27 # py-simplesoap via py-stdnum and py-vatnumber
+PYTHON_VERSIONS_ACCEPTED= 27 # py-simplesoap via py-stdnum and py-vatnumber
 DEPENDS+=	${PYPKGPREFIX}-vatnumber-[0-9]*:../../finance/py-vatnumber
 .endif
