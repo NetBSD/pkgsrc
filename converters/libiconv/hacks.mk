75a107a2c25c0862b63a8ebff749d0a79b271037
rillig <rillig@pkgsrc.org>
2019-11-03 17:14:24 +0000

    converters: align variable assignments
    pkglint -Wall -F --only aligned --only indent -r
    
    No manual corrections.

--- converters/libiconv/hacks.mk
+++ converters/libiconv/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.3 2010/11/05 16:43:55 shattered Exp $
+# $NetBSD: hacks.mk,v 1.4 2019/11/03 17:14:25 rillig Exp $
 
 .if !defined(LIBICONV_HACKS_MK)
 LIBICONV_HACKS_MK=	# defined
@@ -10,7 +10,7 @@ LIBICONV_HACKS_MK=	# defined
 .if !empty(MACHINE_PLATFORM:MIRIX-5.3-*)
 PKG_HACKS+=		sys_types_h-inttypes_h-conflict
 post-wrapper:
-		${MKDIR} ${BUILDLINK_DIR}/include/sys && ${GREP} -v '^typedef.*[^u_]int[12368]*_t;' /usr/include/sys/types.h > ${BUILDLINK_DIR}/include/sys/types.h
+	${MKDIR} ${BUILDLINK_DIR}/include/sys && ${GREP} -v '^typedef.*[^u_]int[12368]*_t;' /usr/include/sys/types.h > ${BUILDLINK_DIR}/include/sys/types.h
 # should match int8_t, int16_t, and int32_t (only)
 .endif
 
--- converters/qrencode/options.mk
+++ converters/qrencode/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2013/06/09 13:48:50 rodent Exp $
+# $NetBSD: options.mk,v 1.2 2019/11/03 17:14:26 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.qrencode
 PKG_SUPPORTED_OPTIONS=	static
@@ -9,5 +9,5 @@ PKG_SUGGESTED_OPTIONS+=	# blank
 .if !empty(PKG_OPTIONS:Mstatic)
 CONFIGURE_ARGS+=	--enable-static=yes
 .else
-CONFIGURE_ARGS+=        --enable-static=no
+CONFIGURE_ARGS+=	--enable-static=no
 .endif
