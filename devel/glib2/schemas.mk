b0f11e3a0a54414979ed62cbd44c796bbd075df3
rillig <rillig@pkgsrc.org>
2019-11-03 10:39:04 +0000

    devel: align variable assignments
    pkglint -Wall -F --only aligned --only indent -r
    
    No manual corrections.

--- devel/glib2/schemas.mk
+++ devel/glib2/schemas.mk
@@ -1,4 +1,4 @@
-# $NetBSD: schemas.mk,v 1.2 2011/02/20 15:07:40 jmmv Exp $
+# $NetBSD: schemas.mk,v 1.3 2019/11/03 10:39:12 rillig Exp $
 #
 # This Makefile fragment is intended to be included by packages that install
 # GSettings schemas.  It takes care of rebuilding the corresponding databases
@@ -12,7 +12,7 @@ GLIB_COMPILE_SCHEMAS=	${BUILDLINK_PREFIX.glib2}/bin/glib-compile-schemas
 
 FILES_SUBST+=		GLIB_SCHEMAS_DIR=${GLIB_SCHEMAS_DIR:Q}
 FILES_SUBST+=		GLIB_COMPILE_SCHEMAS=${GLIB_COMPILE_SCHEMAS:Q}
-GLIB_SCHEMAS_DIR=${BUILDLINK_PREFIX.glib2}/share/glib-2.0/schemas
+GLIB_SCHEMAS_DIR=	${BUILDLINK_PREFIX.glib2}/share/glib-2.0/schemas
 INSTALL_TEMPLATES+=	../../devel/glib2/files/schemas.tmpl
 DEINSTALL_TEMPLATES+=	../../devel/glib2/files/schemas.tmpl
 
--- devel/ocaml-bos/buildlink3.mk
+++ devel/ocaml-bos/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/19 11:29:00 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/11/03 10:39:22 rillig Exp $
 
 BUILDLINK_TREE+=	ocaml-bos
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-bos
 OCAML_BOS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-bos+=	ocaml-bos>=0.2.0
-BUILDLINK_PKGSRCDIR.ocaml-bos?=	../../devel/ocaml-bos
+BUILDLINK_PKGSRCDIR.ocaml-bos?=		../../devel/ocaml-bos
 
 .endif	# OCAML_BOS_BUILDLINK3_MK
 
