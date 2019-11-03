eb3c2b436e5440b4f5442b4b14f127e2dfeb9ace
rillig <rillig@pkgsrc.org>
2019-11-03 11:45:28 +0000

    net: align variable assignments
    pkglint -Wall -F --only aligned --only indent -r
    
    No manual corrections.

--- net/libvncserver/options.mk
+++ net/libvncserver/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.3 2019/02/20 12:21:34 ryoon Exp $
+# $NetBSD: options.mk,v 1.4 2019/11/03 11:45:40 rillig Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.libVNCServer
-PKG_SUPPORTED_OPTIONS=	gnutls inet6 libgcrypt openssl sasl
-PKG_SUGGESTED_OPTIONS=	gnutls inet6 libgcrypt
+PKG_OPTIONS_VAR=		PKG_OPTIONS.libVNCServer
+PKG_SUPPORTED_OPTIONS=		gnutls inet6 libgcrypt openssl sasl
+PKG_SUGGESTED_OPTIONS=		gnutls inet6 libgcrypt
 PKG_OPTIONS_OPTIONAL_GROUPS+=	ssl
 PKG_OPTIONS_GROUP.ssl=		gnutls openssl
 
