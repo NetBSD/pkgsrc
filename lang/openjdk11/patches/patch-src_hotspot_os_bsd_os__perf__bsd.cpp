2d5126dca6aa90ba1cfbc981fabb80a7339a08d2
tnn <tnn@pkgsrc.org>
2019-11-03 00:37:28 +0000

    openjdk11: update to 11.0.5 GA and delete patches accepted by upstream

--- lang/openjdk11/patches/patch-src_hotspot_os_bsd_os__perf__bsd.cpp
+++ lang/openjdk11/patches/patch-src_hotspot_os_bsd_os__perf__bsd.cpp
@@ -1,21 +1,10 @@
-$NetBSD: patch-src_hotspot_os_bsd_os__perf__bsd.cpp,v 1.1 2019/10/01 12:36:34 tnn Exp $
+$NetBSD: patch-src_hotspot_os_bsd_os__perf__bsd.cpp,v 1.2 2019/11/03 00:37:28 tnn Exp $
 
-sys/user.h was removed from NetBSD.
 KERN_PROC_PATHNAME is available from NetBSD>=7.99.23
 
---- src/hotspot/os/bsd/os_perf_bsd.cpp.orig	2019-09-19 12:22:38.000000000 +0000
+--- src/hotspot/os/bsd/os_perf_bsd.cpp.orig	2019-10-16 18:31:09.000000000 +0000
 +++ src/hotspot/os/bsd/os_perf_bsd.cpp
-@@ -34,7 +34,9 @@
-   #include <mach/mach.h>
-   #include <mach/task_info.h>
- #else
-+#ifndef __NetBSD__
-   #include <sys/user.h>
-+#endif
-   #include <sys/sched.h>
-   #include <sys/resource.h>
-   #define NET_RT_IFLIST2 NET_RT_IFLIST
-@@ -857,6 +859,9 @@ int SystemProcessInterface::SystemProces
+@@ -859,6 +859,9 @@ int SystemProcessInterface::SystemProces
  
    for (int i = 0; i < pid_count; i++) {
      // Executable path
