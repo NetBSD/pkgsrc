52541126d5004b6db6e5f04c072ef32f59839d9d
kamil <kamil@pkgsrc.org>
2019-11-03 12:40:40 +0000

    lld: Disable gnustack on NetBSD
    Fix corrupted text in description of cherry-picked patch for gnustack.

--- devel/lld/patches/patch-ELF_Writer.cpp
+++ devel/lld/patches/patch-ELF_Writer.cpp
@@ -1,7 +1,7 @@
-$NetBSD: patch-ELF_Writer.cpp,v 1.4 2019/11/03 12:10:29 kamil Exp $
+$NetBSD: patch-ELF_Writer.cpp,v 1.5 2019/11/03 12:40:40 kamil Exp $
 
 Cherry-pick upstream patch commit 2a0fcae3d4d1fd85d6ae8378d7c6f12430c0087d
-[lld] [ELF] Add '-z nognustack' opt to suppress emitting PT_GNU_STAC dummy option for an AArch64 erratum.
+[lld] [ELF] Add '-z nognustack' opt to suppress emitting PT_GNU_STACK
 
 --- ELF/Writer.cpp.orig	2019-09-06 11:20:15.000000000 +0000
 +++ ELF/Writer.cpp
