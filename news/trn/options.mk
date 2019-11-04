cfd9da61036529ae43e4a658ff14559aac7e9ee1
rillig <rillig@pkgsrc.org>
2019-11-04 19:17:10 +0000

    news: align variable assignments
    pkglint -Wall -F --only aligned --only indent -r
    
    No manual corrections.

--- news/trn/options.mk
+++ news/trn/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.4 2011/01/14 16:58:26 asau Exp $
+# $NetBSD: options.mk,v 1.5 2019/11/04 19:17:11 rillig Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.trn
+PKG_OPTIONS_VAR=		PKG_OPTIONS.trn
 PKG_OPTIONS_REQUIRED_GROUPS=	nntpclient
 PKG_OPTIONS_GROUP.nntpclient=	trn-inn trn-nntpclnt
-PKG_SUGGESTED_OPTIONS+=	trn-nntpclnt
+PKG_SUGGESTED_OPTIONS+=		trn-nntpclnt
 
 .include "../../mk/bsd.options.mk"
 
