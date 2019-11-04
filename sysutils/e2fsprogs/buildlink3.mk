c307028987801dbd4027f152f1cbf0dcb69b10a6
rillig <rillig@pkgsrc.org>
2019-11-04 21:28:40 +0000

    sysutils: align variable assignments
    pkglint -Wall -F --only aligned --only indent -r
    
    Manually excluded consolekit and dc-tools since pkglint didn't get the
    formatting correct.

--- sysutils/e2fsprogs/buildlink3.mk
+++ sysutils/e2fsprogs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/10/04 12:21:33 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/11/04 21:28:44 rillig Exp $
 
 BUILDLINK_TREE+=	e2fsprogs
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	e2fsprogs
 E2FSPROGS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.e2fsprogs+=	e2fsprogs>=1.43.7nb1
-BUILDLINK_PKGSRCDIR.e2fsprogs?=	../../sysutils/e2fsprogs
+BUILDLINK_PKGSRCDIR.e2fsprogs?=		../../sysutils/e2fsprogs
 
 .endif	# E2FSPROGS_BUILDLINK3_MK
 
