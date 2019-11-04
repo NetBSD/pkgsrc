958301530deef310ce0ed6d1d2ee52556a1752af
rillig <rillig@pkgsrc.org>
2019-11-04 19:57:58 +0000

    print: align variable assignments
    pkglint -Wall -F --only aligned --only indent -r
    
    No manual corrections.

--- print/tex-pdftools/options.mk
+++ print/tex-pdftools/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2012/04/24 20:27:18 minskim Exp $
+# $NetBSD: options.mk,v 1.2 2019/11/04 19:58:07 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tex-pdftools
-PKG_SUPPORTED_OPTIONS+= x11
-PKG_SUGGESTED_OPTIONS+= x11
+PKG_SUPPORTED_OPTIONS+=	x11
+PKG_SUGGESTED_OPTIONS+=	x11
 
 .include "../../mk/bsd.options.mk"
 
