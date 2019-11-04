78871ca78674e5c107eac7dc873d658edcf1d2e6
gutteridge <gutteridge@pkgsrc.org>
2019-11-04 05:48:26 +0000

    scapy: update to 2.4.3
    This latest version of scapy has improved BSD and SunOS support, among
    other changes. I've continued our DragonFly support, since we were
    already carrying patches for it. (These should be submitted upstream.)
    
    (This also addresses PR pkg/54550, submitted by Gabriel Potter of
    scapy. Thanks for the reminder, and for all your work on your project!)
    
    I have tested a pkgsrc build and scapy regression suite in the
    following environments:
    NetBSD 8.1_STABLE with Python 3.6.9
    NetBSD 9.99.17 with Python 3.7.5
    DragonFly BSD 5.6.2 with Python 3.6.9
    OpenIndiana Hipster 2019.04 with Python 3.7.5
    Fedora Linux 30 with Python 3.7.5
    
    Significant details from the upstream change summaries:
    
    2.4.3
    
    Main Changes
    Core
    
        364 commits since v2.4.2
        better native support for FreeBSD, NetBSD, OpenBSD
        Windows: native RAW sockets support, load interfaces/routes using C calls, ...
        Solaris: fixed support
        latency improvements
        sniff() can be used to test BPF fiters on pcap files
        more unit tests and Python3 compatibility
        asynchronous sniffing
        UTScapy vim syntax highlighting
        drop distutils for setuptools
        Console / IPython integration improvements
    
    Layers
    Major changes
    New
    
        HTTP (from the deprecated scapy-http module), TLS 1.3, ATA over Ethernet, OVD, IEC 60870-5-104, enip, ...
    
    Improved
    
        NetflowV9, ISOTP, Zigbee, RTR, BLE, PPI, DNS, LLDP, ...
        Bluetooth/BTLE rework
        PPI / 802.11 improvements
    
    2.4.2
    
    Main changes
    
        Gabriel Potter is officially part of the Scapy maintainers team
        PEP08 compliance (see #1277)
        Speed improvements (see #642)
    
    Core
    
        253 merged pull requests since v2.4.0
        Python 3.7 support
        Enhanced Windows support
        unit testing is now 100% tox based
    
    Layers
    Major changes
    
        Many automotive related layers added (ISO-TP...)
    
    New
    
        EtherCat
        OPCDA
        SOCKS
        USBpcap
        RPKI
    
    Improved
    
        MACsec, MQTT, MPLS, DNS, ARP, Dot15d4, Zigbee, Bluetooth4LE, RadioTap ...
        Enhanced monitor mode support
    
    Other
    
        addresses a v2.4.0 vulnerability
    
    2.4.0
    
    Main changes
    
        Python3 support
        85% code coverage
    
    Core
    
        Pcap/PcapNg improvements
        enhanced Windows support
        OpenBSD improvements
        OSX 802.11 monitor mode
        Krack AP module
        iPython support
        automatically tested on Linux, OSX & Windows
        ...
    
    Layers
    Major changes
    
    TLS (including TLS1.3), X.509 ...
    New
    
    HTTP/2, EAP-TTLS, TACACS, MQTT ...
    Improved
    
    IPv6, SCTP, NTP, PPTP, CDP, BGP, ISIS ...

--- net/scapy/options.mk
+++ net/scapy/options.mk
@@ -1,16 +1,26 @@
-# $NetBSD: options.mk,v 1.11 2019/11/03 11:45:52 rillig Exp $
+# $NetBSD: options.mk,v 1.12 2019/11/04 05:48:26 gutteridge Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scapy
 
-PKG_SUPPORTED_OPTIONS=	scapy-crypto gnuplot scapy-pyx
+PKG_SUPPORTED_OPTIONS=	libpcap scapy-crypto gnuplot scapy-pyx
+PKG_SUGGESTED_OPTIONS=	libpcap
 
 .include "../../mk/bsd.options.mk"
+
+###
+### Ensure libpcap is available in the environment. (In many cases, it
+### should be part of a base installation. It is most essential for
+### SunOS support, where it's depended upon more than for other OSes.)
+###
+.if !empty(PKG_OPTIONS:Mlibpcap)
+.include "../../net/libpcap/buildlink3.mk"
+.endif
 
 ###
 ### Add in crypto support for WEP operations
 ###
 .if !empty(PKG_OPTIONS:Mscapy-crypto)
-DEPENDS+=	${PYPKGPREFIX}-amkCrypto-[0-9]*:../../security/py-amkCrypto
+DEPENDS+=	${PYPKGPREFIX}-cryptography-[0-9]*:../../security/py-cryptography
 .endif
 
 ###
