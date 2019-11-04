4ca67a8f5144846dfb3f6229810481825ae157bb
rillig <rillig@pkgsrc.org>
2019-11-04 21:50:15 +0000

    wm: align variable assignments
    pkglint -Wall -F --only aligned --only indent -r
    
    No manual corrections.

--- wm/xmonad-contrib/options.mk
+++ wm/xmonad-contrib/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2018/07/04 13:40:40 jperkin Exp $
+# $NetBSD: options.mk,v 1.3 2019/11/04 21:50:16 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xmonad-contrib
-PKG_SUPPORTED_OPTIONS= xft2
-PKG_SUGGESTED_OPTIONS= xft2
+PKG_SUPPORTED_OPTIONS=	xft2
+PKG_SUGGESTED_OPTIONS=	xft2
 
 .include "../../mk/bsd.options.mk"
 
@@ -14,8 +14,8 @@ SUBST_STAGE.1=	pre-patch
 SUBST_FILES.1=	xmonad-contrib.cabal
 SUBST_SED.1=	-e "s|render text|render text+  default: False|"
 
-SUBST_CLASSES+=	2
-SUBST_STAGE.2=	pre-configure
-SUBST_FILES.2=	xmonad-contrib.cabal
+SUBST_CLASSES+=		2
+SUBST_STAGE.2=		pre-configure
+SUBST_FILES.2=		xmonad-contrib.cabal
 SUBST_FILTER_CMD.2=	${TR} '+' \\n
 .endif
