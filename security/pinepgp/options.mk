29e0f8c5910d1dddbce1ebb958215fa60ae4bd9e
rillig <rillig@pkgsrc.org>
2019-11-04 21:12:51 +0000

    security: align variable assignments
    pkglint -Wall -F --only aligned --only indent -r
    
    No manual corrections.

--- security/pinepgp/options.mk
+++ security/pinepgp/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2017/09/12 02:32:17 ryoon Exp $
+# $NetBSD: options.mk,v 1.3 2019/11/04 21:12:58 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pinepgp
 
@@ -9,17 +9,17 @@ PLIST_VARS=		gpg pgp5
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgpg)
-DEPENDS+=	gnupg>=1.2.3:../../security/gnupg
+DEPENDS+=		gnupg>=1.2.3:../../security/gnupg
 CONFIGURE_ARGS+=	--with-gpg
-PLIST.gpg=yes
+PLIST.gpg=		yes
 .else
 CONFIGURE_ARGS+=	--without-gpg
 .endif
 
 .if !empty(PKG_OPTIONS:Mpgp5)
-DEPENDS+=	pgp5>=5.0:../../security/pgp5
+DEPENDS+=		pgp5>=5.0:../../security/pgp5
 CONFIGURE_ARGS+=	--with-pgp5
-PLIST.pgp5=yes
+PLIST.pgp5=		yes
 .else
 CONFIGURE_ARGS+=	--without-pgp5
 .endif
