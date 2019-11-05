44f5659666f63d6091e99d80174e9801448ae9d1
adam <adam@pkgsrc.org>
2019-11-05 14:58:43 +0000

    qt5: updated to 5.13.2
    Qt 5.13.2 Released
    
    We have released Qt 5.13.2 today. As a patch release, Qt 5.13.2 does not add any new functionality but provides many bug fixes and other improvements.
    
    Compared to Qt 5.13.1, the new Qt 5.13.2 contains more than 200 bug fixes. For details of the most important changes, please check the Change files of Qt 5.13.2.

---
+++ x11/qt5-qtscxml/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/11/05 14:58:45 adam Exp $
+
+BUILDLINK_TREE+=	qt5-qtscxml
+
+.if !defined(QT5_QTSCXML_BUILDLINK3_MK)
+QT5_QTSCXML_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt5-qtscxml+=	qt5-qtscxml>=5.13.2
+BUILDLINK_PKGSRCDIR.qt5-qtscxml?=	../../x11/qt5-qtscxml
+
+.include "../../x11/qt5-qtdeclarative/buildlink3.mk"
+.endif	# QT5_QTSCXML_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt5-qtscxml
