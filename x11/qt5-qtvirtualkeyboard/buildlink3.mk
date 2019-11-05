562360b95247fb85be8d74516f12d467e4ddb92f
adam <adam@pkgsrc.org>
2019-11-05 14:58:43 +0000

    qt5: updated to 5.13.2
    Qt 5.13.2 Released
    
    We have released Qt 5.13.2 today. As a patch release, Qt 5.13.2 does not add any new functionality but provides many bug fixes and other improvements.
    
    Compared to Qt 5.13.1, the new Qt 5.13.2 contains more than 200 bug fixes. For details of the most important changes, please check the Change files of Qt 5.13.2.

---
+++ x11/qt5-qtvirtualkeyboard/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/11/05 14:58:46 adam Exp $
+
+BUILDLINK_TREE+=	qt5-qtvirtualkeyboard
+
+.if !defined(QT5_QTVIRTUALKEYBOARD_BUILDLINK3_MK)
+QT5_QTVIRTUALKEYBOARD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt5-qtvirtualkeyboard+=	qt5-qtvirtualkeyboard>=5.13.2
+BUILDLINK_PKGSRCDIR.qt5-qtvirtualkeyboard?=	../../x11/qt5-qtvirtualkeyboard
+
+.include "../../x11/qt5-qtdeclarative/buildlink3.mk"
+.endif	# QT5_QTVIRTUALKEYBOARD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt5-qtvirtualkeyboard
