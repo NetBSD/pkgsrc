f11df1f2b7e1e9646801e13ec0f8cfc914737dfe
adam <adam@pkgsrc.org>
2019-11-06 15:48:23 +0000

    py-psutil: updated to 5.6.5
    5.6.5
    **Bug fixes**
    - remove pyproject.toml as it was causing installation issues.
    
    5.6.4
    **Enhancements**
    - [Linux] added Process.cpu_times().iowait counter, which is the time
      spent waiting for blocking I/O to complete.
    - add PEP 517/8 build backend and requirements specification for better
      pip integration.
    
    **Bug fixes**
    - [Windows] Process' cmdline(), environ() or cwd() may occasionally fail
      with ERROR_PARTIAL_COPY which now gets translated to AccessDenied.
    - [Linux] cpu_affinity() segfaults on CentOS 5 / manylinux.
      cpu_affinity() support for CentOS 5 was removed.
    - [AIX] compilation error on AIX 7.2 due to 32 vs 64 bit differences.
    - 'type' and 'family' fields returned by net_connections() are not
      always turned into enums.
    - [NetBSD] process cmdline() erroneously raise ZombieProcess error if
      cmdline has non encodable chars.
    - usage percent may be rounded to 0 on Python 2.
    - [Windows] getloadavg() math for calculating 5 and 15 mins values is
      incorrect.
    - [Linux] use CC compiler env var if defined.
    - [Windows] `NtWow64*` syscalls fail to raise the proper error code
    - [OSX] calling close() (in C) on possible negative integers.
    - [SunOS] compilation fails on SunOS 5.10.

--- sysutils/py-psutil/patches/patch-psutil___psbsd.py
+++ sysutils/py-psutil/patches/patch-psutil___psbsd.py
@@ -1,12 +1,10 @@
-$NetBSD: patch-psutil___psbsd.py,v 1.4 2019/10/11 20:01:51 kamil Exp $
-
-Use proc_cwd on NetBSD >= 8.99.42.
+$NetBSD: patch-psutil___psbsd.py,v 1.5 2019/11/06 15:48:23 adam Exp $
 
 Stop using SDEAD as it is no longer available.
 
---- psutil/_psbsd.py.orig	2019-04-11 21:10:12.000000000 +0000
+--- psutil/_psbsd.py.orig	2019-06-28 13:13:13.000000000 +0000
 +++ psutil/_psbsd.py
-@@ -57,7 +57,7 @@ elif OPENBSD or NETBSD:
+@@ -58,7 +58,7 @@ elif OPENBSD or NETBSD:
          # equivalent. Also it appears there's no equivalent of
          # psutil.STATUS_DEAD. SDEAD really means STATUS_ZOMBIE.
          # cext.SZOMB: _common.STATUS_ZOMBIE,
@@ -15,20 +13,3 @@ Stop using SDEAD as it is no longer available.
          cext.SZOMB: _common.STATUS_ZOMBIE,
          # From http://www.eecs.harvard.edu/~margo/cs161/videos/proc.h.txt
          # OpenBSD has SRUN and SONPROC: SRUN indicates that a process
-@@ -105,6 +105,7 @@ HAS_PER_CPU_TIMES = hasattr(cext, "per_c
- HAS_PROC_NUM_THREADS = hasattr(cext, "proc_num_threads")
- HAS_PROC_OPEN_FILES = hasattr(cext, 'proc_open_files')
- HAS_PROC_NUM_FDS = hasattr(cext, 'proc_num_fds')
-+HAS_PROC_CWD = hasattr(cext, 'proc_cwd')
- 
- kinfo_proc_map = dict(
-     ppid=0,
-@@ -845,6 +846,8 @@ class Process(object):
-         if OPENBSD and self.pid == 0:
-             return None  # ...else it would raise EINVAL
-         elif NETBSD:
-+            if HAS_PROC_CWD:
-+                return cext.proc_cwd(self.pid) or None
-             with wrap_exceptions_procfs(self):
-                 return os.readlink("/proc/%s/cwd" % self.pid)
-         elif HAS_PROC_OPEN_FILES:
