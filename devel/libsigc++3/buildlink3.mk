2230155f670ce1cfa83c436938911c638ef24348
wiz <wiz@pkgsrc.org>
2019-11-07 13:17:42 +0000

    devel/libsigc++3: import libsigc++3-3.0.0
    libsigc++ implements a full callback system for use in widget
    libraries, abstract interfaces, and general programming. Originally
    part of the Gtk-- widget set, libsigc++ is now a separate library
    to provide for more general use. It is the most complete library
    of its kind with the ability to connect an abstract callback to a
    class method, function, or function object. It contains adaptor
    classes for connection of dissimilar callbacks and has an ease of
    use unmatched by other C++ callback libraries.
    
    This is major version 3 of the library.

---
+++ devel/libsigc++3/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/11/07 13:17:42 wiz Exp $
+
+BUILDLINK_TREE+=	libsigcpp3
+
+.if !defined(LIBSIGCPP3_BUILDLINK3_MK)
+LIBSIGCPP3_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libsigcpp3+=	libsigc++3>=3.0.0
+BUILDLINK_PKGSRCDIR.libsigcpp3?=		../../devel/libsigc++3
+
+# needs C++11
+GCC_REQD+=	4.8
+
+.endif # LIBSIGCPP3_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libsigcpp3
