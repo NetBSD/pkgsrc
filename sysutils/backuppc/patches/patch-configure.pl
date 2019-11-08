3a12672b5da0997d66bf2759da4d330182a17c1d
tnn <tnn@pkgsrc.org>
2019-11-08 17:11:12 +0000

    backuppc: update to 4.3.1
    BackupPC 4.0.0 is a significant improvement over 3.x in terms of
    performance and storage efficiency. It is backward compatible with
    3.x pool storage, so it can be used to upgrade an existing installation
    as well as for brand new installs.

--- sysutils/backuppc/patches/patch-configure.pl
+++ sysutils/backuppc/patches/patch-configure.pl
@@ -1,15 +1,15 @@
-$NetBSD: patch-configure.pl,v 1.1 2015/02/13 17:57:36 tnn Exp $
+$NetBSD: patch-configure.pl,v 1.2 2019/11/08 17:11:12 tnn Exp $
 
 Prevent previous installation from altering build results.
 
---- configure.pl.orig	2015-01-12 00:19:53.000000000 +0000
+--- configure.pl.orig	2018-05-07 17:14:29.000000000 +0000
 +++ configure.pl
-@@ -158,7 +158,7 @@ EOF
- # config file to get all the defaults.
+@@ -266,7 +266,7 @@ EOF
  #
+ my $ConfigDir = $opts{"config-dir"} || "/etc/BackupPC";
  my $ConfigPath = "";
 -my $ConfigFileOK = 1;
 +my $ConfigFileOK = 0;
  while ( 1 ) {
-     if ( $ConfigFileOK && -f "/etc/BackupPC/config.pl"
+     if ( $ConfigFileOK && -f "$DestDir$ConfigDir/config.pl"
              && (!defined($opts{fhs}) || $opts{fhs}) && !defined($opts{"config-path"}) ) {
