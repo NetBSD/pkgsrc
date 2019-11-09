dcd6b084c47c8bc095a4b490be41e8b0468d461f
nia <nia@pkgsrc.org>
2019-11-09 11:22:20 +0000

    ncmpc: Update to 0.36
    ncmpc 0.36 - (2019-11-05)
    * screen_keydef: show "Add new key" only if there is room for more keys
    * support the Alt modifier in hotkeys
    * reduce network transfer

--- audio/ncmpc/patches/patch-src_ncu.cxx
+++ audio/ncmpc/patches/patch-src_ncu.cxx
@@ -1,18 +1,18 @@
-$NetBSD: patch-src_ncu.cxx,v 1.1 2019/09/14 12:19:40 nia Exp $
+$NetBSD: patch-src_ncu.cxx,v 1.2 2019/11/09 11:22:20 nia Exp $
 
 (from the commit log)
 date: 2010-02-25 03:08:12 +0900;  author: drochner;  state: Exp;
  Set the keyboard mode to raw() explicitely. ncurses seems to do
  this in some magical way, didn't find a hint in documentation.
 
---- src/ncu.cxx.orig	2019-04-10 09:08:34.000000000 +0000
+--- src/ncu.cxx.orig	2019-11-05 11:06:41.000000000 +0000
 +++ src/ncu.cxx
-@@ -55,6 +55,8 @@ ncu_init()
+@@ -59,6 +59,8 @@ ncu_init()
  	/* enable extra keys */
  	keypad(stdscr, true);
  
 +	raw();
 +
- 	/* initialize mouse support */
- #ifdef HAVE_GETMOUSE
- 	if (options.enable_mouse)
+ #ifdef NCURSES_VERSION
+ 	/* define Alt-* keys which for some reasons aren't defined by
+ 	   default (tested with ncurses 6.1 on Linux) */
