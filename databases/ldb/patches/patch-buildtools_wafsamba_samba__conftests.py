a2f173f9b4198bd5318b92d9e7eacad83e61a4f5
adam <adam@pkgsrc.org>
2019-11-10 16:58:06 +0000

    ldb: updated to 2.0.7
    2.0.7:
    Unknown changes

--- databases/ldb/patches/patch-buildtools_wafsamba_samba__conftests.py
+++ databases/ldb/patches/patch-buildtools_wafsamba_samba__conftests.py
@@ -1,9 +1,9 @@
-$NetBSD: patch-buildtools_wafsamba_samba__conftests.py,v 1.1 2019/03/20 19:06:17 adam Exp $
+$NetBSD: patch-buildtools_wafsamba_samba__conftests.py,v 1.2 2019/11/10 16:58:06 adam Exp $
 
 Ensure defines are strings to avoid assertion failure, some
 returned values are unicode.
 
---- buildtools/wafsamba/samba_conftests.py.orig	2019-02-26 15:12:23.000000000 +0000
+--- buildtools/wafsamba/samba_conftests.py.orig	2019-03-21 10:12:32.000000000 +0000
 +++ buildtools/wafsamba/samba_conftests.py
 @@ -97,9 +97,9 @@ def CHECK_LARGEFILE(conf, define='HAVE_L
                  if flag[:2] == "-D":
@@ -15,5 +15,5 @@ returned values are unicode.
 -                        conf.DEFINE(flag_split[0], flag_split[1])
 +                        conf.DEFINE(str(flag_split[0]), str(flag_split[1]))
  
-     if conf.CHECK_CODE('return !(sizeof(off_t) >= 8)',
+     if conf.CHECK_CODE('if (sizeof(off_t) < 8) return 1',
                         define,
