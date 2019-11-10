55683234562f7a4afeaea848a3fd38eed22cc580
adam <adam@pkgsrc.org>
2019-11-10 16:58:06 +0000

    ldb: updated to 2.0.7
    2.0.7:
    Unknown changes

--- databases/ldb/buildlink3.mk
+++ databases/ldb/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/08/03 06:52:32 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/11/10 16:58:06 adam Exp $
 
 BUILDLINK_TREE+=	ldb
 
 .if !defined(LDB_BUILDLINK3_MK)
 LDB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ldb+=	ldb>=1.5.5
+BUILDLINK_API_DEPENDS.ldb+=	ldb>=2.0.7
 BUILDLINK_PKGSRCDIR.ldb?=	../../databases/ldb
 
 .include "../../converters/libiconv/buildlink3.mk"
