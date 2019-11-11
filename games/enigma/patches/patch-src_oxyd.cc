d973df08569fc77fbcf398a1e9175473c97fedb6
maya <maya@pkgsrc.org>
2019-11-11 22:03:01 +0000

    enigma: update to 1.21. C++11 compatibility patches from amdmi3@freebsd. Intended to address PR pkg/54597: games/enigma distfile contains broken PNG images
    Changes in Version 1.21
    =======================
    
    User-Visible changes
    --------------------
    
        - New translations: Croatian, Czech, Danish, Scottish-Gaelic, and Slovak
        - updated remaining translations
        - fixed many English typos
        - Russian reference manual
        - rollover for options and rating buttons
        - 51 new Enigma levels (now a total of 984 main-pack levels, plus 21 tutorials)
    
    Internal changes
    ----------------
    
        - further rewrites of old-API levels to new API (only 212 old-API left)
        - many bug fixes
    
    Changes in Version 1.20
    =======================
    
    User-Visible changes
    --------------------
    
        - added ingame music by Taztunes (deactivated by default)
        - removed "over par" notification by general request
        - 78 new Enigma levels (now a total of 956 levels, plus 21 tutorials)
        - replaced old Sokoban levels by 1077 imported Sokoball levels
        - 95 new Pentomino levels
    
    Internal changes
    ----------------
    
        - many bug fixes
    
    Changes in Version 1.10  (Released only for level authors and testers)
    =======================
    
    User-Visible changes
    --------------------
    
        - new objects: wires, st_barrier, opal oxyds, drunken thiefs,
            st_yinyang, st_dispenser_extralife, fl_scales, framed floors,
            it_meditation_volcano, it_meditation_caldera, puzzle singletons,
            mirror sheets, bigbricks in wood and blue-sand decor, cracks
            with water beneath, black pearls, and st_rawglass_quad
        - vastly extended st_window, hollow puzzles, it_strip
        - color coding for keys and locks
        - four new oxyd colors and shuffle oxyds
        - many improved images for higher resolutions
        - new images for coins
        - extra lifes now look like batteries to not confuse them with ac_pearl
        - tutorial level for st_chess
    
    Internal changes
    ----------------
    
        - new API, involving a reengineering of many objects, a consistent
            naming scheme for all objects, and a rewrite of almost every level
        - new libraries:
            liblua, libmath, libmap to ease programming of complex levels
            libpuzzle, libtrain to ease programming of large puzzles and trains
            libimport, libpento, libsoko to generate or import complete levels
              from different formats
        - internal maze generation
        - internal flood generation
        - improved it_rubberband, st_surprise and it_surprise
        - itemfreeze stone now is an own object st_ice

--- games/enigma/patches/patch-src_oxyd.cc
+++ games/enigma/patches/patch-src_oxyd.cc
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_oxyd.cc,v 1.1 2011/02/23 10:20:26 adam Exp $
+$NetBSD: patch-src_oxyd.cc,v 1.2 2019/11/11 22:03:01 maya Exp $
 
 Fix name conflict.
 
---- src/oxyd.cc.orig	2011-02-22 13:24:27.000000000 +0000
+--- src/oxyd.cc.orig	2014-12-19 21:24:38.000000000 +0000
 +++ src/oxyd.cc
-@@ -130,7 +130,7 @@ namespace 
+@@ -125,7 +125,7 @@ namespace 
          }
      }
  
@@ -13,14 +13,14 @@ Fix name conflict.
      {
          assert (a.getGridType() >= GridType_First &&
                  a.getGridType() <= GridType_Last);
-@@ -644,8 +644,8 @@ void OxydLoader::connect_signals () 
+@@ -652,8 +652,8 @@ void OxydLoader::connect_signals () 
          int nrec = level.getNumRecipients(sender);
          for (int irec=0; irec<nrec; ++irec) {
              SignalLocation recipient = level.getRecipient(sender, irec);
--            GridLoc src = to_gridloc(sender);
--            GridLoc dst = to_gridloc(recipient);
-+            GridLoc src = to_gridloc2(sender);
-+            GridLoc dst = to_gridloc2(recipient);
-             world::AddSignal (src, dst, "signal");
+-            GridLoc src = ::to_gridloc(sender);
+-            GridLoc dst = ::to_gridloc(recipient);
++            GridLoc src = ::to_gridloc2(sender);
++            GridLoc dst = ::to_gridloc2(recipient);
+             AddSignal (src, dst, "signal");
          }
      }
