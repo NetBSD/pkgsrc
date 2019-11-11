f148bcb8c8142e08af2af6a9b9c23970de8fb0dd
markd <markd@pkgsrc.org>
2019-11-11 06:38:05 +0000

    extra-cmake-modules: update to 5.64.0
    5.62.0
      ECMAddQtDesignerPlugin: pass code sample indirectly via variable name arg
      Keep 'lib' as default LIBDIR on Arch Linux based systems
      Enable autorcc by default
      Define install location for JAR/AAR files for Android
      Add ECMAddQtDesignerPlugin
    
    5.63.0
      new module ECMSourceVersionControl
      Fix FindEGL when using Emscripten
      ECMAddQch: add INCLUDE_DIRS argument
    
    5.64.0
      Don't set C/C++ standards if already set
      Use modern way to set the C/CXX standad
      Raise CMake requirements to 3.5
      ECMAddQch: support PREDEFINED_MACROS/BLANK_MACROS with blanks & quotes

--- devel/extra-cmake-modules/patches/patch-kde-modules_KDECompilerSettings.cmake
+++ devel/extra-cmake-modules/patches/patch-kde-modules_KDECompilerSettings.cmake
@@ -1,26 +1,12 @@
-$NetBSD: patch-kde-modules_KDECompilerSettings.cmake,v 1.3 2018/03/24 20:13:40 joerg Exp $
+$NetBSD: patch-kde-modules_KDECompilerSettings.cmake,v 1.4 2019/11/11 06:38:05 markd Exp $
 
 Do not hard-code --enable-new-dtags. It is completely broken on systems like
 NetBSD implementing the sane rpath semantic and just have a new enough binutils
 version. It is up to the system compiler to know what the correct platfor
 choice is.
 
-Do not force C90 mode. It is not a reasonable default.
-
 --- kde-modules/KDECompilerSettings.cmake.orig	2017-11-30 07:42:14.000000000 +0000
 +++ kde-modules/KDECompilerSettings.cmake
-@@ -189,11 +189,6 @@ endif()
- # Pick sensible versions of the C and C++ standards.
- # Note that MSVC does not have equivalent flags; the features are either
- # supported or they are not.
--if (CMAKE_C_COMPILER_ID STREQUAL "GNU" OR CMAKE_C_COMPILER_ID MATCHES "Clang")
--    # We use the C89 standard because that is what is common to all our
--    # compilers (in particular, MSVC 2010 does not support C99)
--    set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -std=iso9899:1990")
--endif()
- if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
- elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Intel" AND NOT WIN32)
 @@ -229,14 +224,6 @@ if (POLICY CMP0063)
      cmake_policy(SET CMP0063 NEW)
  endif()
