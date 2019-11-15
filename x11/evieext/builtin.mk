b0b25698053988ce813e620a85731cd22fec85ec
wiz <wiz@pkgsrc.org>
2019-11-15 09:18:16 +0000

    evieext: remove, it's part of xorgproto now
    Noted by taca.

--- x11/evieext/builtin.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD: builtin.mk,v 1.5 2019/11/03 09:14:07 rillig Exp $
-
-BUILTIN_PKG:=			evieext
-PKGCONFIG_FILE.evieext=		${X11BASE}/lib/pkgconfig/evieproto.pc
-PKGCONFIG_FILE.evieext+=	${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/evieproto.pc
-PKGCONFIG_FILE.evieext+=	${X11BASE}/share/pkgconfig/evieproto.pc
-
-.include "../../mk/buildlink3/pkgconfig-builtin.mk"
-.include "../../mk/x11.builtin.mk"
