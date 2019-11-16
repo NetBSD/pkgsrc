662fe657862906d8a17ca680a49b2a823bff6758
ng0 <ng0@pkgsrc.org>
2019-11-16 17:36:28 +0000

    xplanet: Update to version 1.3.1
    * Add 2017 leap second patch from upstream as well as
      fix output bug when num_times is 2 patch.
    * Fix build with clang
    
    Upstream Changelog from SVN:
    
    Don't outline symbol for markers where outlined=false
    fix marker bug when range is a small value
    update gif code to giflib-4.2.0
    update gif code to 5.0.0
    fix arc bug at close ranges, update default night image
    update version number
    use distance from limb plane instead of planet center when deciding when to draw annotations
    reapply marker fix
    fix limb distance bug
    Add -label_altitude option
    fix radius bug
    Fix label bug
    use GetTempPath() for windows
    add windows directory
    add string.h to png.c
    use JPL's leapsecond kernel definitions for ET-UTC
    add deltaT expression for pre 1838
    fix UTC bug
    Update MS Windows desktop size
    update comments in config file
    updates for cygwin
    set default desktop file type to PNG
    add 2015 leap second
    release 1.3.1

--- x11/xplanet/patches/patch-src_libannotate_addMarkers.cpp
+++ x11/xplanet/patches/patch-src_libannotate_addMarkers.cpp
@@ -1,43 +1,47 @@
-$NetBSD: patch-src_libannotate_addMarkers.cpp,v 1.1 2018/02/13 12:45:02 ryoon Exp $
+$NetBSD: patch-src_libannotate_addMarkers.cpp,v 1.2 2019/11/16 17:36:28 ng0 Exp $
+
+Multiple inFile.getline() calls within xplanet test the return
+against NULL in a read loop, but g++ 6 doesn't like this. Replace
+with checks for inFile.eof() and inFile.fail() instead.
 
 --- src/libannotate/addMarkers.cpp.orig	2011-11-15 01:44:24.000000000 +0000
 +++ src/libannotate/addMarkers.cpp
-@@ -423,13 +423,16 @@ addMarkers(PlanetProperties *planetPrope
+@@ -429,12 +429,16 @@ addMarkers(PlanetProperties *planetProperties, Planet *planet,
          {
              ifstream inFile(markerFile.c_str());
              char *line = new char[MAX_LINE_LENGTH];
 -            while (inFile.getline (line, MAX_LINE_LENGTH, '\n') != NULL)
-+	    while (1)
++            while (1)
              {
                  unsigned char color[3];
                  memcpy(color, planetProperties->MarkerColor(), 3);
                  string font(planetProperties->MarkerFont());
                  int fontSize(planetProperties->MarkerFontSize());
++
++                inFile.getline (line, MAX_LINE_LENGTH, '\n');
++                if (inFile.eof() || inFile.fail())
++                    break;
                  
-+		inFile.getline (line, MAX_LINE_LENGTH, '\n');
-+ 		if (inFile.eof() || inFile.fail())
-+		    break;
-                 readMarkerFile(line, planet, pixel_radius, X, Y, Z, 
+                 readMarkerFile(line, planet, pixel_radius, 
                                 view, projection, width, height, 
-                                color, font, fontSize, 
-@@ -469,13 +472,18 @@ addMarkers(View *view, const int width, 
+@@ -475,13 +479,18 @@ addMarkers(View *view, const int width, const int height,
          {
              ifstream inFile(markerFile.c_str());
              char *line = new char[MAX_LINE_LENGTH];
 -            while (inFile.getline (line, MAX_LINE_LENGTH, '\n') != NULL)
--            {
-+	    while (1)
-+	    {
-+		inFile.getline (line, MAX_LINE_LENGTH, '\n');
++            while (1)
+             {
++                inFile.getline (line, MAX_LINE_LENGTH, '\n');
 +
                  unsigned char color[3];
                  memcpy(color, options->Color(), 3);
                  string font(options->Font());
                  int fontSize(options->FontSize());
-                 
-+ 		if (inFile.eof() || inFile.fail())
-+		    break;
+-                
 +
-                 readMarkerFile(line, NULL, 0, 0, 0, 0,
++                if (inFile.eof() || inFile.fail())
++                    break;
++
+                 readMarkerFile(line, NULL, 0, 
                                 view, NULL, width, height, 
                                 color, font, fontSize, 1.0, 
