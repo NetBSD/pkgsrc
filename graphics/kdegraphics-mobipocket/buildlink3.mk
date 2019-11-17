cb3381d812067ded7f0a89767c7617034952f99f
markd <markd@pkgsrc.org>
2019-11-17 03:07:33 +0000

    kdegraphics-mobipocket: update to 19.08.3  qt5/kf5 version

--- graphics/kdegraphics-mobipocket/buildlink3.mk
+++ graphics/kdegraphics-mobipocket/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.16 2019/08/22 12:23:18 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2019/11/17 03:07:33 markd Exp $
 
 BUILDLINK_TREE+=	kdegraphics-mobipocket
 
 .if !defined(KDEGRAPHICS_MOBIPOCKET_BUILDLINK3_MK)
 KDEGRAPHICS_MOBIPOCKET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kdegraphics-mobipocket+=	kdegraphics-mobipocket>=4.14.3
-BUILDLINK_ABI_DEPENDS.kdegraphics-mobipocket?=	kdegraphics-mobipocket>=16.08.0nb9
+BUILDLINK_API_DEPENDS.kdegraphics-mobipocket+=	kdegraphics-mobipocket>=17.12.1
 BUILDLINK_PKGSRCDIR.kdegraphics-mobipocket?=	../../graphics/kdegraphics-mobipocket
 
-.include "../../x11/kdelibs4/buildlink3.mk"
+.include "../../devel/kio/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# KDEGRAPHICS_MOBIPOCKET_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kdegraphics-mobipocket
