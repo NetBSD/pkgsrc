fab8ed388b61967df5d8712691d774ff8d3b387f
markd <markd@pkgsrc.org>
2019-11-17 05:40:26 +0000

    ksmtp: add version 19.08.3
    Job-based library to send email through an SMTP server.

---
+++ mail/ksmtp/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/11/17 05:40:26 markd Exp $
+
+BUILDLINK_TREE+=	ksmtp
+
+.if !defined(KSMTP_BUILDLINK3_MK)
+KSMTP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ksmtp+=	ksmtp>=17.12.1
+BUILDLINK_PKGSRCDIR.ksmtp?=	../../mail/ksmtp
+
+.include "../../devel/kio/buildlink3.mk"
+.include "../../mail/kmime/buildlink3.mk"
+.include "../../security/cyrus-sasl/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KSMTP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ksmtp
