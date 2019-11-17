fc351c06782621f279fa13a22e4b8f060db1286b
markd <markd@pkgsrc.org>
2019-11-17 05:38:04 +0000

    kmbox: add version 19.08.3
    KMBox provides API to access emails in storages in the MBox format

---
+++ mail/kmbox/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/11/17 05:38:04 markd Exp $
+
+BUILDLINK_TREE+=	kmbox
+
+.if !defined(KMBOX_BUILDLINK3_MK)
+KMBOX_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kmbox+=	kmbox>=17.12.1
+BUILDLINK_PKGSRCDIR.kmbox?=	../../mail/kmbox
+
+.include "../../mail/kmime/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KMBOX_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kmbox
