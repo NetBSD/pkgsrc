3942eadb43bb3614986fc730b2e372d2096ae9af
markd <markd@pkgsrc.org>
2019-11-17 06:01:46 +0000

    kpimtextedit: add version 19.08.3
    MTextedit provides a textedit with PIM-specific features.
    
    KPIMTextedit is a library that provides an enchanced text edit class,
    see TextEdit.  This text is highlighted with a EMailQuoteHighlighter.

---
+++ textproc/kpimtextedit/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/11/17 06:01:46 markd Exp $
+
+BUILDLINK_TREE+=	kpimtextedit
+
+.if !defined(KPIMTEXTEDIT_BUILDLINK3_MK)
+KPIMTEXTEDIT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kpimtextedit+=	kpimtextedit>=17.12.1
+BUILDLINK_PKGSRCDIR.kpimtextedit?=	../../textproc/kpimtextedit
+
+.include "../../devel/grantlee-qt5/buildlink3.mk"
+.include "../../devel/kio/buildlink3.mk"
+.include "../../misc/kemoticons/buildlink3.mk"
+.include "../../textproc/syntax-highlighting/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtspeech/buildlink3.mk"
+.endif	# KPIMTEXTEDIT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kpimtextedit
