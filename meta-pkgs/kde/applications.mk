8f29be9d405738422ea9e28565cd8935410e5132
markd <markd@pkgsrc.org>
2019-11-17 02:23:38 +0000

    kde: set default KAPPSVER to 19.08.3

--- meta-pkgs/kde/applications.mk
+++ meta-pkgs/kde/applications.mk
@@ -1,6 +1,6 @@
-# $NetBSD: applications.mk,v 1.3 2019/08/28 03:10:23 markd Exp $
+# $NetBSD: applications.mk,v 1.4 2019/11/17 02:23:38 markd Exp $
 
-KAPPSVER?=	19.08.0
+KAPPSVER?=	19.08.3
 CATEGORIES+=	kde
 MASTER_SITES?=	${MASTER_SITE_KDE:=applications/${KAPPSVER}/src/}
 EXTRACT_SUFX?=	.tar.xz
