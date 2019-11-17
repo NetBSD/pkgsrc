70eb9c512f7114f002f2360203270c913c22253b
markd <markd@pkgsrc.org>
2019-11-17 06:22:26 +0000

    kcalutils: add version 19.08.3
    This library provides a set of utility functions that help applications
    access and use calendar data via the KCalendarCore library.

---
+++ time/kcalutils/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/11/17 06:22:26 markd Exp $
+
+BUILDLINK_TREE+=	kcalutils
+
+.if !defined(KCALUTILS_BUILDLINK3_MK)
+KCALUTILS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kcalutils+=	kcalutils>=17.12.1
+BUILDLINK_PKGSRCDIR.kcalutils?=		../../time/kcalutils
+
+.include "../../misc/kidentitymanagement/buildlink3.mk"
+.include "../../time/kcalendarcore/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KCALUTILS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kcalutils
