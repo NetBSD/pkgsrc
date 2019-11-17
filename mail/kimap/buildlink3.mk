deaf5c3f08972d14ed226b7277cc03bd89bb8115
markd <markd@pkgsrc.org>
2019-11-17 05:34:51 +0000

    kimap: add version 19.08.3
    Job-based API for interacting with IMAP server

---
+++ mail/kimap/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/11/17 05:34:51 markd Exp $
+
+BUILDLINK_TREE+=	kimap
+
+.if !defined(KIMAP_BUILDLINK3_MK)
+KIMAP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kimap+=	kimap>=17.12.1
+BUILDLINK_PKGSRCDIR.kimap?=	../../mail/kimap
+
+.include "../../devel/kio/buildlink3.mk"
+.include "../../mail/kmime/buildlink3.mk"
+.include "../../security/cyrus-sasl/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KIMAP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kimap
