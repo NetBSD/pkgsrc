fec89bc73e00def0f29da5dfcbb2b0be8aa5d011
markd <markd@pkgsrc.org>
2019-11-17 02:41:04 +0000

    libkexiv2: fix buildlink3.mk

--- graphics/libkexiv2/buildlink3.mk
+++ graphics/libkexiv2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2019/11/17 02:36:54 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2019/11/17 02:41:04 markd Exp $
 
 BUILDLINK_TREE+=	libkexiv2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libkexiv2
 LIBKEXIV2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libkexiv2+=	libkexiv2>=17.12.1
-BUILDLINK_PKGSRCDIR.libkexiv2?=		../../wip/libkexiv2
+BUILDLINK_PKGSRCDIR.libkexiv2?=		../../graphics/libkexiv2
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# LIBKEXIV2_BUILDLINK3_MK
