4b2ff82071c813de0bbc30719b5979f2cd3327ea
markd <markd@pkgsrc.org>
2019-11-17 02:52:23 +0000

    libkdcraw: update to 19.08.3   qt5/kf5 version

--- graphics/libkdcraw/buildlink3.mk
+++ graphics/libkdcraw/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.51 2019/08/22 12:23:20 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.52 2019/11/17 02:52:23 markd Exp $
 
 BUILDLINK_TREE+=	libkdcraw
 
 .if !defined(LIBKDCRAW_BUILDLINK3_MK)
 LIBKDCRAW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkdcraw+=	libkdcraw>=4.8.0
-BUILDLINK_ABI_DEPENDS.libkdcraw?=	libkdcraw>=15.08.3nb12
+BUILDLINK_API_DEPENDS.libkdcraw+=	libkdcraw>=17.12.1
 BUILDLINK_PKGSRCDIR.libkdcraw?=		../../graphics/libkdcraw
 
-.include "../../x11/kdelibs4/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# LIBKDCRAW_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkdcraw
