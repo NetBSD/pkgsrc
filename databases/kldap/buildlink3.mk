983f556c020ec26d7c40b8cb3eb782f4d09ff364
markd <markd@pkgsrc.org>
2019-11-17 05:47:49 +0000

    kldap: add version 19.08.3
    LDAP access API for KDE

---
+++ databases/kldap/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/11/17 05:47:49 markd Exp $
+
+BUILDLINK_TREE+=	kldap
+
+.if !defined(KLDAP_BUILDLINK3_MK)
+KLDAP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kldap+=	kldap>=17.12.1
+BUILDLINK_PKGSRCDIR.kldap?=	../../databases/kldap
+
+.include "../../textproc/kcompletion/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KLDAP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kldap
