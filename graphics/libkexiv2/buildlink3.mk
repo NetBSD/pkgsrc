c335832f6497567e92bcf2143782addb40994a22
markd <markd@pkgsrc.org>
2019-11-17 02:36:54 +0000

    libkexiv2: update to 19.08.3  qt5/kf5 version

--- graphics/libkexiv2/buildlink3.mk
+++ graphics/libkexiv2/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.20 2018/01/07 13:04:15 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2019/11/17 02:36:54 markd Exp $
 
 BUILDLINK_TREE+=	libkexiv2
 
 .if !defined(LIBKEXIV2_BUILDLINK3_MK)
 LIBKEXIV2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkexiv2+=	libkexiv2>=4.8.0
-BUILDLINK_PKGSRCDIR.libkexiv2?=		../../graphics/libkexiv2
+BUILDLINK_API_DEPENDS.libkexiv2+=	libkexiv2>=17.12.1
+BUILDLINK_PKGSRCDIR.libkexiv2?=		../../wip/libkexiv2
 
+.include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# LIBKEXIV2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkexiv2
