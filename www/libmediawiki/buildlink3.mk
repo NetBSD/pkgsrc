760452745e3b6fe4865e92e6f628184c09d92a95
markd <markd@pkgsrc.org>
2019-11-17 08:53:25 +0000

    libmediawiki: update to 5.37.0    qt5/kf5 version

--- www/libmediawiki/buildlink3.mk
+++ www/libmediawiki/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.33 2019/08/22 12:23:53 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.34 2019/11/17 08:53:25 markd Exp $
 
 BUILDLINK_TREE+=	libmediawiki
 
 .if !defined(LIBMEDIAWIKI_BUILDLINK3_MK)
 LIBMEDIAWIKI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libmediawiki+=	libmediawiki>=2.5.0
-BUILDLINK_ABI_DEPENDS.libmediawiki?=	libmediawiki>=4.13.0nb12
+BUILDLINK_API_DEPENDS.libmediawiki+=	libmediawiki>=5.37.0
 BUILDLINK_PKGSRCDIR.libmediawiki?=	../../www/libmediawiki
 
-.include "../../x11/kdelibs4/buildlink3.mk"
+.include "../../devel/kcoreaddons/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# LIBMEDIAWIKI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libmediawiki
