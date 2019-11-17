1e452c32ee1a2c262ee91ad05caf1d64fa58936d
markd <markd@pkgsrc.org>
2019-11-17 08:59:53 +0000

    kipi-plugins: update to 19.08.3   qt5/kf5 version

--- graphics/kipi-plugins/buildlink3.mk
+++ graphics/kipi-plugins/buildlink3.mk
@@ -1,12 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.3 2012/03/31 23:45:58 dholland Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2019/11/17 08:59:53 markd Exp $
 
 BUILDLINK_TREE+=	kipi-plugins
 
 .if !defined(KIPI_PLUGINS_BUILDLINK3_MK)
 KIPI_PLUGINS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kipi-plugins+=	kipi-plugins>=1.1.0
+BUILDLINK_API_DEPENDS.kipi-plugins+=	kipi-plugins>=19.08.3
 BUILDLINK_PKGSRCDIR.kipi-plugins?=	../../graphics/kipi-plugins
-.endif # KIPI_PLUGINS_BUILDLINK3_MK
+
+.include "../../graphics/libkipi/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KIPI_PLUGINS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kipi-plugins
