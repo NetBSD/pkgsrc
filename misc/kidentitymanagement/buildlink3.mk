f8801af36cfa28612035a95655c0a8e28688bc01
markd <markd@pkgsrc.org>
2019-11-17 06:16:24 +0000

    kidentitymanagement: add version 19.08.3
    Library for managing identitites.

---
+++ misc/kidentitymanagement/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/11/17 06:16:24 markd Exp $
+
+BUILDLINK_TREE+=	kidentitymanagement
+
+.if !defined(KIDENTITYMANAGEMENT_BUILDLINK3_MK)
+KIDENTITYMANAGEMENT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kidentitymanagement+=	kidentitymanagement>=17.12.1
+BUILDLINK_PKGSRCDIR.kidentitymanagement?=	../../misc/kidentitymanagement
+
+.include "../../textproc/kpimtextedit/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KIDENTITYMANAGEMENT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kidentitymanagement
