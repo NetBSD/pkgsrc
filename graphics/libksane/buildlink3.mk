47fc4e7281c946d87450834e88f4dea8918542ff
markd <markd@pkgsrc.org>
2019-11-17 02:59:09 +0000

    libksane: update to 19.08.3  qt5/kf5 version

--- graphics/libksane/buildlink3.mk
+++ graphics/libksane/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.35 2019/08/22 12:23:20 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2019/11/17 02:59:09 markd Exp $
 
 BUILDLINK_TREE+=	libksane
 
 .if !defined(LIBKSANE_BUILDLINK3_MK)
 LIBKSANE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libksane+=	libksane>=4.8.0
-BUILDLINK_ABI_DEPENDS.libksane?=	libksane>=15.08.3nb12
+BUILDLINK_API_DEPENDS.libksane+=	libksane>=17.12.1
 BUILDLINK_PKGSRCDIR.libksane?=		../../graphics/libksane
 
-.include "../../x11/kdelibs4/buildlink3.mk"
+.include "../../security/kwallet/buildlink3.mk"
+.include "../../x11/ktextwidgets/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# LIBKSANE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libksane
