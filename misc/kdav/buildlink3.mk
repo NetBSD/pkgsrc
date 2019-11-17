04a8af0123f7349f2ccf206562b2fe2c5ce1af78
markd <markd@pkgsrc.org>
2019-11-17 05:52:26 +0000

    kdav: add version 19.08.3
    This is an DAV protocol implemention with KJobs.
    
    Calendars and todos are supported, using either GroupDAV
    or CalDAV, and contacts are supported using GroupDAV or
    CardDAV.

---
+++ misc/kdav/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/11/17 05:52:26 markd Exp $
+
+BUILDLINK_TREE+=	kdav
+
+.if !defined(KDAV_BUILDLINK3_MK)
+KDAV_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kdav+=	kdav>=17.12.1
+BUILDLINK_PKGSRCDIR.kdav?=	../../misc/kdav
+
+.include "../../devel/kio/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KDAV_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kdav
