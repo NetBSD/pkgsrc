aede7b187757fb568bc405fe0940ea9a41e4ba60
markd <markd@pkgsrc.org>
2019-11-18 09:16:45 +0000

    libkmahjongg: update to 19.08.3   qt5/kf5 version

--- games/libkmahjongg/buildlink3.mk
+++ games/libkmahjongg/buildlink3.mk
@@ -1,13 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/04/03 12:03:29 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/11/18 09:16:45 markd Exp $
 
 BUILDLINK_TREE+=	libkmahjongg
 
 .if !defined(LIBKMAHJONGG_BUILDLINK3_MK)
 LIBKMAHJONGG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkmahjongg+=	libkmahjongg>=4.10.2
+BUILDLINK_API_DEPENDS.libkmahjongg+=	libkmahjongg>=19.08.3
 BUILDLINK_PKGSRCDIR.libkmahjongg?=	../../games/libkmahjongg
 
+.include "../../textproc/kcompletion/buildlink3.mk"
+.include "../../x11/kconfigwidgets/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtsvg/buildlink3.mk"
 .endif	# LIBKMAHJONGG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkmahjongg
