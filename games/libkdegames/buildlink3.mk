6066e3fa99bb79c09ac000214b5f8ddde6a6c662
markd <markd@pkgsrc.org>
2019-11-18 09:11:49 +0000

    libkdegames: update to 19.08.3    qt5/kf5 version

--- games/libkdegames/buildlink3.mk
+++ games/libkdegames/buildlink3.mk
@@ -1,13 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/04/03 12:03:29 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/11/18 09:11:49 markd Exp $
 
 BUILDLINK_TREE+=	libkdegames
 
 .if !defined(LIBKDEGAMES_BUILDLINK3_MK)
 LIBKDEGAMES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkdegames+=	libkdegames>=4.10.2
+BUILDLINK_API_DEPENDS.libkdegames+=	libkdegames>=19.08.3
 BUILDLINK_PKGSRCDIR.libkdegames?=	../../games/libkdegames
 
+.include "../../devel/kdeclarative/buildlink3.mk"
+.include "../../net/kdnssd/buildlink3.mk"
+.include "../../net/knewstuff/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# LIBKDEGAMES_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkdegames
