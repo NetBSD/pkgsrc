02cb8e3b780f97f9ec5984d67243e1bf874da97e
markd <markd@pkgsrc.org>
2019-11-20 22:04:14 +0000

    analitza: update to 19.08.3   qt5/kf5 version

--- math/analitza/buildlink3.mk
+++ math/analitza/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.35 2019/08/22 12:23:29 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2019/11/20 22:04:14 markd Exp $
 
 BUILDLINK_TREE+=	analitza
 
 .if !defined(ANALITZA_BUILDLINK3_MK)
 ANALITZA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.analitza+=	analitza>=4.8.0
-BUILDLINK_ABI_DEPENDS.analitza?=	analitza>=4.14.3nb17
-BUILDLINK_PKGSRCDIR.analitza?=		../../math/analitza
+BUILDLINK_API_DEPENDS.analitza+=	analitza>=19.08.3
+BUILDLINK_PKGSRCDIR.analitza?=	../../math/analitza
 
-.include "../../x11/kdelibs4/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtsvg/buildlink3.mk"
+.include "../../x11/qt5-qtdeclarative/buildlink3.mk"
 .endif	# ANALITZA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-analitza
