58d6d46a004a40fec6f197ec52532586473631be
nia <nia@pkgsrc.org>
2019-11-21 23:48:27 +0000

    libxmp: Update to 4.4.1
    4.4.1 (20161012):
    	Fix issues reported by Saga Musix:
    	- fix MDL c5spd to preserve base periods
    	- fix MDL sample decoder loop with corrupted data
    	- fix MASI loader OPLH and PPAN subchunks parsing
    	Other changes:
    	- fix MacOS Tiger build issues (reported by Misty De Meo)
    	- fix sample loop corner case (reported by knight-ryu12)
    	- fix set pan effect in multichannel MODs (reported by Leilei)
    	- fix global volume on module loop (reported by Travis Evans)
    	- fix IT pan right value (by NoSuck)
    	- fix MASI effects based on OpenMPT PSM loader
    	- fix memory leak in XMs with 256 patterns
    	- fix anticlick when rendering only one sample
    	- fix anticlick in His Master's Noise instruments
    	- fix anticlick in MED synth instruments
    
    4.4.0 (20160719):
    	Fix bugs caught in the OpenMPT test cases:
    	- fix XM arpeggio in FastTracker 2 compatible mode
    	- fix IT bidirectional loop sample length
    	- fix MOD vibrato and tremolo in Protracker compatible mode
    	Fix multichannel MOD issues reported by Leilei:
    	- fix XM replayer note delay and retrig quirk
    	- fix XM replayer channel pan
    	- fix MOD loader period to note conversion
    	Fix issues reported by Lionel Debroux:
    	- fix virtual channel deallocation error handling
    	- fix S3M global volume effect
    	- fix IT envelope reset on tone portamento
    	- fix IT voice leak caused by disabled envelope
    	- fix IT volume column tone portamento
    	- fix XM envelope position setting
    	- fix FT2 arpeggio+portamento quirk with finetunes
    	- fix mixer anticlick routines
    	- accept S3M modules with invalid effects
    	Fix issues reported by Saga Musix:
    	- fix 669 effects when no instrument number is specified
    	- fix 669 effects to be frequency-based
    	- fix 669 initial tempo
    	Other changes:
    	- fix S3M channel reset on sample end (reported by Alexander Null)
    	- fix Noisetracker MOD speed setting (reported by Tero Auvinen)
    	- fix IT loader DCA sanity check (reported by Paul Gomez Givera)
    	- fix IT envelope reset after offset with portamento
    	- fix bidirectional sample interpolation
    	- fix mixer resampling and tuning issues
    	- add Antti Lankila's Amiga 500 modeling mixer
    	- add support to filter effect E0 in Amiga mods
    	- add flags to configure player mode
    	- add option to set the maximum number of virtual channels
    	- add frequency-based "period" mode
    	- add support to IT sample sustain loop
    	- limit Oktalyzer modules to MOD note range
    	- remove broken synth chip and Adlib emulation suport
    	- code refactoring and cleanup
    
    4.3.13 (20160417):
    	Fix bugs caught in the OpenMPT test cases:
    	- fix IT volume column fine volume slide with row delay
    	Other changes:
    	- fix MOD vs XM set finetune effect
    	- fix IT old instrument volume
    	- fix IT panbrello speed
    	- fix IT random pan variation left bias
    	- fix IT default pan in sample mode (reported by Hai Shalom)
    	- fix S3M set pan effect (reported by Hai Shalom and Johannes Schultz)
    	- code refactoring and cleanup
    
    4.3.12 (20160305):
    	Fix bugs caught in the OpenMPT test cases:
    	- fix IT note off with instrument
    	- fix IT note recover after cut
    	- fix IT instrument without note after note cut event
    	- fix IT pan reset on new note instead of new instrument
    	- fix IT volume swing problems
    	- fix XM glissando effect
    	- fix Scream Tracker 3 period limits
    	- fix Scream Tracker 3 tremolo memory
    	Other changes:
    	- fix IT pattern break in hexadecimal (reported by StarFox008)
    	- fix S3M subsong detection (reported by knight-ryu12)
    	- fix S3M/IT special marker handling (reported by knight-ryu12)
    	- fix Galaxy Music System 4.0 song length (reported by AntonZab)
    	- fix tone portamento memory without note (reported by NoSuck)
    	- fix IT pan swing limits
    	- Add TrackerPacker v1 format converter
    	- Add TrackerPacker v2 format converter
    	- Add ProPacker 1.0 format converter

--- audio/libxmp/patches/patch-src_common.h
+++ audio/libxmp/patches/patch-src_common.h
@@ -1,13 +1,13 @@
-$NetBSD: patch-src_common.h,v 1.2 2015/02/10 03:30:03 rxg Exp $
+$NetBSD: patch-src_common.h,v 1.3 2019/11/21 23:48:27 nia Exp $
 
---- src/common.h.orig	2015-02-07 11:28:38.000000000 +0000
+--- src/common.h.orig	2016-10-12 10:30:03.000000000 +0000
 +++ src/common.h
 @@ -10,7 +10,7 @@
  #include "xmp.h"
  
  #if defined(__GNUC__) || defined(__clang__)
--#if !defined(WIN32) && !defined(ANDROID) && !defined(__APPLE__) && !defined(__AMIGA__) && !defined(B_BEOS_VERSION) && !defined(__ATHEOS__) && !defined(EMSCRIPTEN) && !defined(__MINT__) 
-+#if !defined(WIN32) && !defined(ANDROID) && !defined(__APPLE__) && !defined(__AMIGA__) && !defined(B_BEOS_VERSION) && !defined(__ATHEOS__) && !defined(EMSCRIPTEN) && !defined(__MINT__) && !defined(__sun)
+-#if !defined(WIN32) && !defined(__ANDROID__) && !defined(__APPLE__) && !defined(__AMIGA__) && !defined(B_BEOS_VERSION) && !defined(__ATHEOS__) && !defined(EMSCRIPTEN) && !defined(__MINT__) 
++#if !defined(WIN32) && !defined(__ANDROID__) && !defined(__APPLE__) && !defined(__AMIGA__) && !defined(B_BEOS_VERSION) && !defined(__ATHEOS__) && !defined(EMSCRIPTEN) && !defined(__MINT__) && !defined(__sun)
  #define USE_VERSIONED_SYMBOLS
  #endif
  #endif
