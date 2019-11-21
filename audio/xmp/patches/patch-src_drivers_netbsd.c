e2e2b82a89f791213c7ce325d188600f902e61dc
nia <nia@pkgsrc.org>
2019-11-21 23:59:06 +0000

    xmp: Update to 4.1.0
    Switch to native audio drivers for NetBSD and Solaris, which it has,
    somehow.
    
    4.1.0 (20160719):
    	- Requires libxmp 4.4
    	- Add option to force tracker emulation
    	- Add option to use simulated Amiga mixers
    	- Add option to set the maximum number of virtual channels
    	- Add command to switch between standard and classic mixers
    	- Add command to display current mixer type
    	- Fix default amplification factor
    	- Fix module-specific configuration file options
    	- Fix screen corruption when module uses more than 255 virtual
    	  channels (reported by Lionel Debroux)
    	- Remove option --offset-bug-emulation
    
    4.0.11 (20160306):
    	- Mark surround channels in channel list
    	- Address errors reported by Coverity Scan

--- audio/xmp/patches/patch-src_drivers_netbsd.c
+++ audio/xmp/patches/patch-src_drivers_netbsd.c
@@ -1,8 +1,17 @@
-$NetBSD: patch-src_drivers_netbsd.c,v 1.3 2014/02/27 03:40:10 rxg Exp $
+$NetBSD: patch-src_drivers_netbsd.c,v 1.4 2019/11/21 23:59:06 nia Exp $
 
---- src/sound_netbsd.c.orig	2013-04-06 13:38:02.000000000 +0000
+--- src/sound_netbsd.c.orig	2016-07-16 11:29:44.213738881 +0000
 +++ src/sound_netbsd.c
-@@ -99,7 +101,7 @@ static void play(void *b, int i)
+@@ -36,7 +36,7 @@ static int init(struct options *options)
+ 	chkparm1("buffer", bsize = strtoul(token, NULL, 0));
+ 	parm_end();
+ 
+-	if ((audio_fd = open("/dev/sound", O_WRONLY)) == -1)
++	if ((audio_fd = open("/dev/audio", O_WRONLY)) == -1)
+ 		return -1;
+ 
+ 	/* try to open audioctldevice */
+@@ -101,7 +101,7 @@ static void play(void *b, int i)
  	while (i) {
  		if ((j = write(audio_fd, b, i)) > 0) {
  			i -= j;
