a74243c8998a565ba161104ef6b51c3663ce8cda
taca <taca@pkgsrc.org>
2019-11-22 05:33:10 +0000

    lang/php72: update to 7.2.25
    Update php72 package to 7.2.25.
    
    
    21 Nov 2019, PHP 7.2.25
    
    - Core:
      . Fixed bug #78656 (Parse errors classified as highest log-level). (Erik
        Lundin)
      . Fixed bug #78752 (Segfault if GC triggered while generator stack frame is
        being destroyed). (Nikita)
      . Fixed bug #78689 (Closure::fromCallable() doesn't handle
        [Closure, '__invoke']). (Nikita)
    
    - COM:
      . Fixed bug #78694 (Appending to a variant array causes segfault). (cmb)
    
    - Date:
      . Fixed bug #70153 (\DateInterval incorrectly unserialized). (Maksim Iakunin)
      . Fixed bug #78751 (Serialising DatePeriod converts DateTimeImmutable). (cmb)
    
    - Iconv:
      . Fixed bug #78642 (Wrong libiconv version displayed). (gedas at martynas,
        cmb).
    
    - OpCache:
      . Fixed bug #78654 (Incorrectly computed opcache checksum on files with
        non-ascii characters). (mhagstrand)
      . Fixed bug #78747 (OpCache corrupts custom extension result). (Nikita)
    
    - OpenSSL:
      . Fixed bug #78775 (TLS issues from HTTP request affecting other encrypted
        connections). (Nikita)
    
    - Reflection:
      . Fixed bug #78697 (ReflectionClass::ImplementsInterface - inaccurate error
        message with traits). (villfa)
    
    - Sockets:
      . Fixed bug #78665 (Multicasting may leak memory). (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.273 2019/10/25 02:58:33 taca Exp $
+# $NetBSD: phpversion.mk,v 1.274 2019/11/22 05:33:10 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP71_VERSION=	7.1.33
-PHP72_VERSION=	7.2.24
+PHP72_VERSION=	7.2.25
 PHP73_VERSION=	7.3.11
 
 # Define initial release of major version.
