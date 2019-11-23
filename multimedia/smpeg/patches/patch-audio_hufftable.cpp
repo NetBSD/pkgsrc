a0065ecbcab5d685bb563d4143a149b4b2cf756c
nia <nia@pkgsrc.org>
2019-11-23 23:03:01 +0000

    smpeg: Update to 0.4.5
    I've had to create a distfile by checking out the latest available SVN
    tag, because there are no tarballs available. You can diff it if you
    like.
    
    Here's the latest available changelog before they stopped updating it:
    
    current:
    Ryan C. Gordon - Sun Oct 29 02:16:02 EST 2006
     * Patch from Chris Nelson to deal with SMPEG_error(NULL).
    Ryan C. Gordon - Sat Mar 18 14:57:28 EST 2006
     * Patch from Mike Frysinger to fix more asm stuff.
    Sam Lantinga - Sun Mar  5 21:08:09 PST 2006
     * Updated libtool build configuration
    Ryan C. Gordon - Fri Dec 16 22:07:22 EST 2005
     * gcc4 fixes (compliments of Gentoo Bugzilla #73579).
     * gcc4.1 fixes, too. (compliments of Robert Marmorstein).
     * Shortened filenames because "ar" apparently doesn't like them (thanks, LGP).
    Ryan C. Gordon - Wed Oct 12 18:22:02 EDT 2005
     * Patch from Jorge to allow graceful (and faster) exit of threads.
     * Patch from Mike Frysinger to disable executable stack.
     * Patch from Mike Frysinger to add sanity checks to autogen.sh
     * Patch from Mike Frysinger for MMX/asm code fixes (PIC and TEXTREL stuff).
    Ryan C. Gordon - Wed Aug 31 22:55:18 EDT 2005
     * Disable executable stack in library (thanks, Mike Frysinger!)
    Ryan C. Gordon - Wed Apr 27 11:15:01 EDT 2005
     * automake fix in smpeg.m4 by Thomas Klausner.
    Martin Köhler -  Sun Mar 20 10:48:54 EST 2005
     * PBProjects fix.
    Steven Fuller - Mon Feb  9 23:26:08 CST 2004
     * Added a --enable-video-callback-thread configure flag to disable thread
       usage in smpeg.
    Ryan C. Gordon - Fri Jan  2 22:50:11 EST 2004
     * Added some sanity checks.
    Ryan C. Gordon - Fri Jan  2 22:18:41 EST 2004
     * Removed "Patches" file...it was screwing up my greps too much.
    Ryan C. Gordon - Fri Jan  2 22:16:47 EST 2004
     * SMPEG builds on Cygwin again.
    Ryan C. Gordon - Wed Dec 31 00:07:51 EST 2003
     * Check for SDL_CreateYUVOverlay() failure (happens for OpenGL surfaces).
    Ryan C. Gordon - Tue Dec 30 23:37:07 EST 2003
     * Check that SDL_BYTEORDER is sane (it wasn't, for SDL 1.2.5 on Linux/amd64).
    Ryan C. Gordon - Fri Feb 14 21:02:00 EST 2003
     * Memory leak fix that I missed from before (thanks, Pete Shinners).
    Ryan C. Gordon - Sat Jan 25 19:20:58 EST 2003
     * Memory leak fixes from NUNOKAWA Masato and Robert Diel.
    Ryan C. Gordon - Wed Dec 18 16:47:52 EST 2002
     * Apple Project Builder support by Eric Wing.
    Ryan C. Gordon - Fri Dec  6 14:32:29 EST 2002
     * Fixed for newer automakes.
    Ryan C. Gordon - Wed Dec  4 18:00:33 EST 2002
     * Now builds again when --enable-mmx is specified at configure time.
    Ryan C. Gordon - Thu Sep 26 01:13:58 EDT 2002
     * Minor Makefile.am patch for GCC 3.2
    Ryan C. Gordon - Tue Jul  9 22:28:21 EDT 2002
     * Removed #include <unistd.h> from glmovie.c
    Ryan C. Gordon - Sun Jun 02 20:10:42 EST 2002
     * ./autogen.sh accepts aclocal commandlines via the ACLOCAL_FLAGS envr var.
    Ryan C. Gordon - Sun Jun 02 20:10:42 EST 2002
     * ./autogen.sh is now flagged as executable in CVS.
    Ryan C. Gordon - Sun Jun 02 20:10:42 EST 2002
     * ./configure.in tweaked to not complain with newer autoconf.
    Ryan C. Gordon - Sun Jun 02 20:10:42 EST 2002
     * Newer ltconfig and hacks for libtool and .S files.
    Adam Procter - Sat Jun  8 05:41:30 EDT 2002
     * Fixed potential deadlock in MPEGaudio.cpp
    Sam Lantinga - Sun Jun  9 00:24:10 EDT 2002
     * Fix for crash when an MPEG file can't be opened.
    
    0.4.5:
    Sam Lantinga - Tue Jul 17 12:27:20 PDT 2001
     * Skip MPEG audio frames with wildly varying frequencies

--- multimedia/smpeg/patches/patch-audio_hufftable.cpp
+++ multimedia/smpeg/patches/patch-audio_hufftable.cpp
@@ -1,18 +1,19 @@
-$NetBSD: patch-audio_huffmantable.cpp,v 1.1 2017/10/19 15:59:22 jperkin Exp $
+$NetBSD: patch-audio_hufftable.cpp,v 1.1 2019/11/23 23:03:01 nia Exp $
 
 Fix build with GCC >= 6.
 
---- audio/huffmantable.cpp.orig	1999-08-26 04:37:52.000000000 +0000
-+++ audio/huffmantable.cpp
-@@ -9,6 +9,7 @@
+--- audio/hufftable.cpp.orig	2019-11-23 22:30:57.000000000 +0000
++++ audio/hufftable.cpp
+@@ -9,6 +9,8 @@
  #include "config.h"
  #endif
  
 +#include <climits>
++
  #include "MPEGaudio.h"
  
  static const unsigned int
-@@ -550,11 +551,11 @@ htd33[ 31][2]={{ 16,  1},{  8,  1},{  4,
+@@ -550,11 +552,11 @@ htd33[ 31][2]={{ 16,  1},{  8,  1},{  4,
  
  const HUFFMANCODETABLE MPEGaudio::ht[HTN]=
  {
@@ -26,7 +27,7 @@ Fix build with GCC >= 6.
    { 5, 4-1, 4-1, 0, 31,htd05},
    { 6, 4-1, 4-1, 0, 31,htd06},
    { 7, 6-1, 6-1, 0, 71,htd07},
-@@ -564,7 +565,7 @@ const HUFFMANCODETABLE MPEGaudio::ht[HTN
+@@ -564,7 +566,7 @@ const HUFFMANCODETABLE MPEGaudio::ht[HTN
    {11, 8-1, 8-1, 0,127,htd11},
    {12, 8-1, 8-1, 0,127,htd12},
    {13,16-1,16-1, 0,511,htd13},
