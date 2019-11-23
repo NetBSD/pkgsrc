d436322e7a8fc3c99fa88f6d442619eb5b0ba624
gdt <gdt@pkgsrc.org>
2019-11-23 19:41:02 +0000

    games/flightgear: Ehcck for building with gcc correctly

--- games/flightgear/hacks.mk
+++ games/flightgear/hacks.mk
@@ -1,8 +1,8 @@
-# $NetBSD: hacks.mk,v 1.1 2016/07/02 14:48:36 joerg Exp $
+# $NetBSD: hacks.mk,v 1.2 2019/11/23 19:41:02 gdt Exp $
 
 .if ${OPSYS} == "NetBSD" 
 .  include "../../mk/compiler.mk"
-.  if ${PKGSRC_COMPILER} == "gcc" && \
+.  if !(${PKGSRC_COMPILER:Mgcc}) && \
       (!empty(CC_VERSION:Mgcc-4.8.*) || !empty(CC_VERSION:Mgcc-5.*))
 CONFIGURE_ARGS+=	--disable-precomp-headers
 .  endif
