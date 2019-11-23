4cc7c3af0982ba990488931d4e769b610ce6a015
gdt <gdt@pkgsrc.org>
2019-11-23 19:39:30 +0000

    editors/codeblocks: Check for compiling with gcc correctly

--- editors/codeblocks/hacks.mk
+++ editors/codeblocks/hacks.mk
@@ -1,11 +1,11 @@
-# $NetBSD: hacks.mk,v 1.2 2019/06/08 10:40:53 rillig Exp $
+# $NetBSD: hacks.mk,v 1.3 2019/11/23 19:39:30 gdt Exp $
 
 ### g++ 4.8 and 5.3 on NetBSD/i386 fails with an internal compiler error
 ### when PCH is used. Be conservative and disable on all MACHINE_ARCH
 ###
 .if ${OPSYS} == "NetBSD"
 .  include "../../mk/compiler.mk"
-.  if ${PKGSRC_COMPILER} == "gcc" && \
+.  if !empty(${PKGSRC_COMPILER:Mgcc}) && \
       (!empty(CC_VERSION:Mgcc-4.8.*) || !empty(CC_VERSION:Mgcc-5.*))
 CONFIGURE_ARGS+=	--disable-pch
 .  endif
