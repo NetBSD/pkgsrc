d5fb62152283a5a515a4af3489232b827ecadfd6
markd <markd@pkgsrc.org>
2019-11-24 03:09:19 +0000

    kross: add version 5.64.0
    Kross is a scripting bridge to embed scripting functionality
    into an application. It supports QtScript as a scripting interpreter backend.

---
+++ devel/kross/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/11/24 03:09:19 markd Exp $
+
+BUILDLINK_TREE+=	kross
+
+.if !defined(KROSS_BUILDLINK3_MK)
+KROSS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kross+=	kross>=5.64.0
+BUILDLINK_PKGSRCDIR.kross?=	../../devel/kross
+
+.include "../../devel/kparts/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtscript/buildlink3.mk"
+.endif	# KROSS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kross
