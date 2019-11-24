ed1f21141c729629645aae8204accbfb722d562a
gdt <gdt@pkgsrc.org>
2019-11-24 14:04:16 +0000

    editors/codeblocks: Fix empty/M pattern

--- editors/codeblocks/hacks.mk
+++ editors/codeblocks/hacks.mk
@@ -1,11 +1,11 @@
-# $NetBSD: hacks.mk,v 1.3 2019/11/23 19:39:30 gdt Exp $
+# $NetBSD: hacks.mk,v 1.4 2019/11/24 14:04:16 gdt Exp $
 
 ### g++ 4.8 and 5.3 on NetBSD/i386 fails with an internal compiler error
 ### when PCH is used. Be conservative and disable on all MACHINE_ARCH
 ###
 .if ${OPSYS} == "NetBSD"
 .  include "../../mk/compiler.mk"
-.  if !empty(${PKGSRC_COMPILER:Mgcc}) && \
+.  if !empty(PKGSRC_COMPILER:Mgcc) && \
       (!empty(CC_VERSION:Mgcc-4.8.*) || !empty(CC_VERSION:Mgcc-5.*))
 CONFIGURE_ARGS+=	--disable-pch
 .  endif
