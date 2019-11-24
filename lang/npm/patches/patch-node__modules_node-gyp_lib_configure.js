213683a616138c3c92f868673153b27958a1b5c0
adam <adam@pkgsrc.org>
2019-11-24 20:21:23 +0000

    npm: updated to 6.13.1
    v6.13.1:
    fix(fund): support funding string shorthand
    should not publish tap-snapshot folder
    Add preliminary WSL support for npm and npx
    print quick audit report for human output
    
    v6.13.0:
    add fund command
    delete ps1 files on package removal
    update supported node list to remove v6.0, v6.1, v9.0 - v9.2
    
    v6.12.1:
    add node v13 as a supported version
    Fix regression in lockfile repair for sub-deps
    resolve circular dependency in pack.js
    
    v6.12.0:
    Now npm ci runs prepare scripts for git dependencies, and respects the --no-optional argument. Warnings for engine mismatches are printed again. Various other fixes and cleanups.

--- lang/npm/patches/patch-node__modules_node-gyp_lib_configure.js
+++ lang/npm/patches/patch-node__modules_node-gyp_lib_configure.js
@@ -1,8 +1,8 @@
-$NetBSD: patch-node__modules_node-gyp_lib_configure.js,v 1.2 2019/08/03 07:32:55 adam Exp $
+$NetBSD: patch-node__modules_node-gyp_lib_configure.js,v 1.3 2019/11/24 20:21:23 adam Exp $
 
 Skip Python detection, version 2.7 is required and we know where it is.
 
---- node_modules/node-gyp/lib/configure.js.orig	2019-07-23 16:20:30.000000000 +0000
+--- node_modules/node-gyp/lib/configure.js.orig	2019-11-18 18:22:06.000000000 +0000
 +++ node_modules/node-gyp/lib/configure.js
 @@ -15,21 +15,14 @@ if (win) {
  }
@@ -11,7 +11,7 @@ Skip Python detection, version 2.7 is required and we know where it is.
 -  var python
 +  var python = '@PYTHONBIN@'
    var buildDir = path.resolve('build')
-   var configNames = [ 'config.gypi', 'common.gypi' ]
+   var configNames = ['config.gypi', 'common.gypi']
    var configs = []
    var nodeDir
    var release = processRelease(argv, gyp, process.version, process.release)
