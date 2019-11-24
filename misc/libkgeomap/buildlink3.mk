fc9ddf5492726b7422a1844e37c98683611cfcd1
markd <markd@pkgsrc.org>
2019-11-24 03:47:22 +0000

    libkgeomap: update to 19.08.3   qt5/kf5 version

--- misc/libkgeomap/buildlink3.mk
+++ misc/libkgeomap/buildlink3.mk
@@ -1,16 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.34 2019/08/22 12:23:34 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.35 2019/11/24 03:47:22 markd Exp $
 
 BUILDLINK_TREE+=	libkgeomap
 
 .if !defined(LIBKGEOMAP_BUILDLINK3_MK)
 LIBKGEOMAP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkgeomap+=	libkgeomap>=2.5.0
-BUILDLINK_ABI_DEPENDS.libkgeomap?=	libkgeomap>=4.13.0nb12
+BUILDLINK_API_DEPENDS.libkgeomap+=	libkgeomap>=19.08.3
 BUILDLINK_PKGSRCDIR.libkgeomap?=	../../misc/libkgeomap
 
+.include "../../devel/kio/buildlink3.mk"
+.include "../../graphics/libkexiv2/buildlink3.mk"
 .include "../../misc/marble/buildlink3.mk"
-.include "../../x11/kdelibs4/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtwebkit/buildlink3.mk"
 .endif	# LIBKGEOMAP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkgeomap
