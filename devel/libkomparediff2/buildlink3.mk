739101952dfc67a6bb4531a1246932793e19b89b
markd <markd@pkgsrc.org>
2019-11-25 09:56:34 +0000

    libkomparediff2: update to 19.08.3    qt5/kf5 version

--- devel/libkomparediff2/buildlink3.mk
+++ devel/libkomparediff2/buildlink3.mk
@@ -1,13 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/11/14 08:03:31 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/11/25 09:56:34 markd Exp $
 
 BUILDLINK_TREE+=	libkomparediff2
 
 .if !defined(LIBKOMPAREDIFF2_BUILDLINK3_MK)
 LIBKOMPAREDIFF2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkomparediff2+=	libkomparediff2>=4.14.3
+BUILDLINK_API_DEPENDS.libkomparediff2+=	libkomparediff2>=19.08.3
 BUILDLINK_PKGSRCDIR.libkomparediff2?=	../../devel/libkomparediff2
 
+.include "../../devel/kio/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# LIBKOMPAREDIFF2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkomparediff2
