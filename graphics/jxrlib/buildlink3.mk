9d6a5731f003db78002ce9623fa433660b3dbbd1
nros <nros@pkgsrc.org>
2019-11-25 21:07:18 +0000

    Fix libjxr on big endian systems and fix warnings
    Add patches from FreeBSD ports that fix build warnings.
    For the patches to apply correctly, add conversion to unix style linebreaks.
    Define _BIG__ENDIAN_ on big endian systems to make the package behave
    correctly when running on them. As pointed out in jxrlibs makefile.
    Bump pkgrevision and abi depends due to the big endian change.

--- graphics/jxrlib/buildlink3.mk
+++ graphics/jxrlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2017/04/04 13:07:07 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/11/25 21:07:18 nros Exp $
 
 BUILDLINK_TREE+=	jxrlib
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	jxrlib
 JXRLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.jxrlib+=	jxrlib>=1.1
+BUILDLINK_ABI_DEPENDS.jxrlib+=	jxrlib>=1.1nb1
 BUILDLINK_PKGSRCDIR.jxrlib?=	../../graphics/jxrlib
 BUILDLINK_DEPMETHOD.jxrlib?=	build
 .endif	# JXRLIB_BUILDLINK3_MK
