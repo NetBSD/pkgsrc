ee7506d675ca74083404badea4bc58ac2f8d8763
wiz <wiz@pkgsrc.org>
2019-11-25 09:52:14 +0000

    libXevie: remove
    The X server stopped supporting this a long time ago.

--- x11/libXevie/builtin.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: builtin.mk,v 1.4 2019/11/03 09:14:11 rillig Exp $
-
-BUILTIN_PKG:=			libXevie
-PKGCONFIG_FILE.libXevie=	${X11BASE}/lib/pkgconfig/xevie.pc
-PKGCONFIG_FILE.libXevie+=	${X11BASE}/lib${LIBABISUFFIX}/pkgconfig/xevie.pc
-
-.include "../../mk/buildlink3/pkgconfig-builtin.mk"
-.include "../../mk/x11.builtin.mk"
