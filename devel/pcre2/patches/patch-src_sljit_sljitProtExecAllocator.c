917e2c881dab80b63ab02f795d50a1954fe5055b
wiz <wiz@pkgsrc.org>
2019-11-26 10:17:55 +0000

    pcre2: update to 10.34.
    Version 10.34 21-November-2019
    ------------------------------
    
    Another release with a few enhancements as well as bugfixes and tidies. The
    main new features are:
    
    1. There is now some support for matching in invalid UTF strings.
    
    2. Non-atomic positive lookarounds are implemented in the pcre2_match()
    interpreter, but not in JIT.
    
    3. Added two new functions: pcre2_get_match_data_size() and
    pcre2_maketables_free().
    
    4. Upgraded to Unicode 12.1.0.

---
+++ devel/pcre2/patches/patch-src_sljit_sljitProtExecAllocator.c
@@ -0,0 +1,28 @@
+$NetBSD: patch-src_sljit_sljitProtExecAllocator.c,v 1.1 2019/11/26 10:17:55 wiz Exp $
+
+NetBSD does not have secure_getenv.
+
+--- src/sljit/sljitProtExecAllocator.c.orig	2017-02-15 17:24:58.000000000 +0000
++++ src/sljit/sljitProtExecAllocator.c
+@@ -97,7 +97,9 @@ struct chunk_header {
+ #endif
+ 
+ int mkostemp(char *template, int flags);
++#if HAVE_SECURE_GETENV
+ char *secure_getenv(const char *name);
++#endif
+ 
+ static SLJIT_INLINE int create_tempfile(void)
+ {
+@@ -124,7 +126,11 @@ static SLJIT_INLINE int create_tempfile(
+ 	tmp_name_len = 4;
+ #endif
+ 
++#if HAVE_SECURE_GETENV
+ 	dir = secure_getenv("TMPDIR");
++#else
++	dir = getenv("TMPDIR");
++#endif
+ 	if (dir) {
+ 		len = strlen(dir);
+ 		if (len > 0 && len < sizeof(tmp_name)) {
