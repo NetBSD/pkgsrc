5cbaa708e91b70afbbfcb24b86d73ce96c213e8b
wiz <wiz@pkgsrc.org>
2019-11-26 14:14:23 +0000

    wxsvg: update to 1.5.20.
    Version 1.5.20 <2019-08-02>
    ---------------------------
    
      * added handling of fill-rule
      * added handling of spreadMethod
      * added handling of clipPath
      * added support of dash-offset
      * added support of mask for path element
      * fixed handling of pt units
      * fixed fill pattern with opacity
      * fixed handling of referenced patterns
    
    Version 1.5.19 <2019-06-24>
    ---------------------------
    
      * fixed rendering polyline element
      * fixed drawing a cubic Bézier curve if the previous command was not an c or s
    
    Version 1.5.18 <2019-05-19>
    ---------------------------
    
      * wxFfmpegMediaDecoder: fixed support of ffmpeg 3.4.x
    
    Version 1.5.17 <2019-05-19>
    ---------------------------
    
      * wxSVGUseElement: fixed GetBBox() and GetResultBBox()
    
    Version 1.5.16 <2019-01-27>
    ---------------------------
    
      * wxFfmpegMediaDecoder.SetPosition(): added parameter seekBackward
    
    Version 1.5.15 <2018-09-15>
    ---------------------------
    
      * added skia backend (experimental) (thanks to Zicheng Yang)
    
    Version 1.5.14 <2018-07-10>
    ---------------------------
    
      * fixed calculation of bounding box if viewbox is specified
      * fixed GetResultBBox()
      * wxSVGCanvasImage: added support of data base64 href
    
    Version 1.5.13 <2018-01-29>
    ---------------------------
    
      * added support of <a> element
      * fixed wxSVGImageElement::GetBBox()
      * fixed wxSVGUseElement::GetBBox()
    
    Version 1.5.12 <2017-05-01>
    --------------------------
    
      * wxSVGSVGElement: fixed GetIntersectionList()
      * wxSVGPointList: fixed SetValueAsString()
    
    Version 1.5.11 <2016-12-28>
    --------------------------
    
      * wxSVGImage: fixed displaying of first video frame
    
    Version 1.5.10 <2016-10-23>
    --------------------------
    
      * added support of EXIF metadata
    
    Version 1.5.9 <2016-08-09>
    --------------------------
    
      * changed GetBBox() and GetResultBBox()
      * added some missing header files
    
    Version 1.5.8 <2016-06-05>
    --------------------------
    
      * fixed ApplyAnimation
    
    Version 1.5.7 <2016-05-03>
    --------------------------
    
      * wxFfmpegMediaDecoder: added GetChapters() and GetMetadata()
      * fixed SVGAnimationElement
    
    Version 1.5.6 <2016-03-11>
    --------------------------
    
      * fixed SVGAnimationElement
    
    Version 1.5.5 <2015-10-03>
    --------------------------
    
      * fixed rendering image element with mask
      * wxFfmpegMediaDecoder: added GetFormatName() and GetCodecTag()
      * wxFfmpegMediaDecoder: fixed wxSVGCanvasVideo
    
    Version 1.5.4 <2015-03-21>
    --------------------------
    
      * added support of 3-digit hex color codes (thanks to Constantine)

--- graphics/wxsvg/buildlink3.mk
+++ graphics/wxsvg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.43 2019/07/21 22:24:07 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.44 2019/11/26 14:14:23 wiz Exp $
 
 BUILDLINK_TREE+=	wxsvg
 
@@ -9,9 +9,8 @@ BUILDLINK_API_DEPENDS.wxsvg+=	wxsvg>=1.0beta5
 BUILDLINK_ABI_DEPENDS.wxsvg+=	wxsvg>=1.5.3nb14
 BUILDLINK_PKGSRCDIR.wxsvg?=	../../graphics/wxsvg
 
-.include "../../fonts/fontconfig/buildlink3.mk"
-.include "../../graphics/libart/buildlink3.mk"
-.include "../../x11/wxGTK28/buildlink3.mk"
+.include "../../graphics/libexif/buildlink3.mk"
+.include "../../x11/wxGTK30/buildlink3.mk"
 .endif # WXSVG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-wxsvg
