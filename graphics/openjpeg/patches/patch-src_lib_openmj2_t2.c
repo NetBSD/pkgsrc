0b5a1c94987944c25d7766b53e219a397b3e475d
sevan <sevan@pkgsrc.org>
2019-11-26 23:10:22 +0000

    Patch for CVE-2018-16376

---
+++ graphics/openjpeg/patches/patch-src_lib_openmj2_t2.c
@@ -0,0 +1,37 @@
+$NetBSD: patch-src_lib_openmj2_t2.c,v 1.1 2019/11/26 23:10:22 sevan Exp $
+
+CVE-2018-16376
+https://github.com/uclouvain/openjpeg/issues/1127
+https://nvd.nist.gov/vuln/detail/CVE-2018-16376
+
+--- src/lib/openmj2/t2.c.orig	2019-11-26 22:37:00.687890833 +0000
++++ src/lib/openmj2/t2.c
+@@ -166,6 +166,12 @@ static int t2_encode_packet(opj_tcd_tile
+ 
+     /* <SOP 0xff91> */
+     if (tcp->csty & J2K_CP_CSTY_SOP) {
++	if (length < 6) {
++	    if (p_t2_mode == FINAL_PASS) {
++		opj_event_msg(p_manager, EVT_ERROR,
++				"opj_t2_encode_packet(): only %u bytes remaining in "
++				"output buffer. %u needed.\n",
++				length, 6);
+         c[0] = 255;
+         c[1] = 145;
+         c[2] = 0;
+@@ -272,6 +278,15 @@ static int t2_encode_packet(opj_tcd_tile
+ 
+     /* <EPH 0xff92> */
+     if (tcp->csty & J2K_CP_CSTY_EPH) {
++	if (length < 2) {
++	    if (p_t2_mode == FINAL_PASS) {
++		opj_event_msg(p_manager, EVT_ERROR,
++				"opj_t2_encode_packet(): only %u bytes remaining in "
++				"output buffer. %u needed.\n",
++				length, 2);
++	    }
++	    return OPJ_FALSE;
++	}
+         c[0] = 255;
+         c[1] = 146;
+         c += 2;
