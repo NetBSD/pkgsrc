0de8b59aeda1747b716eac79f50315be6f834a0d
markd <markd@pkgsrc.org>
2019-11-27 21:32:09 +0000

    mlt: update to 6.18.0
    6 years worth of development.

--- multimedia/mlt/patches/patch-src_framework_mlt__property.h
+++ multimedia/mlt/patches/patch-src_framework_mlt__property.h
@@ -1,17 +1,14 @@
-$NetBSD: patch-src_framework_mlt__property.h,v 1.2 2014/03/30 19:42:53 abs Exp $
+$NetBSD: patch-src_framework_mlt__property.h,v 1.3 2019/11/27 21:32:09 markd Exp $
 
 Add conditional section for __NetBSD__
 
---- src/framework/mlt_property.h.orig	2013-06-03 03:34:35.000000000 +0000
+--- src/framework/mlt_property.h.orig	2019-11-12 14:44:42.000000000 +1300
 +++ src/framework/mlt_property.h
-@@ -32,8 +32,10 @@
- #include <sys/param.h>
- #endif
- 
-+#include <locale.h>
- #if defined(__GLIBC__) || defined(__DARWIN__) || (__FreeBSD_version >= 900506)
- #include <xlocale.h>
+@@ -37,6 +37,7 @@
+ #elif defined(__OpenBSD__)
+ /* XXX matches __nop_locale glue in libc++ */
+ typedef void* locale_t;
 +#elif defined(__NetBSD__) && defined(LC_C_LOCALE)
  #else
- typedef void* locale_t;
+ typedef char* locale_t;
  #endif
