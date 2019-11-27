7a4da4e1ce51d9a7d5d1b2f6f9adecee5cea48ee
markd <markd@pkgsrc.org>
2019-11-27 21:32:09 +0000

    mlt: update to 6.18.0
    6 years worth of development.

--- multimedia/mlt/patches/patch-src_framework_mlt__properties.c
+++ multimedia/mlt/patches/patch-src_framework_mlt__properties.c
@@ -1,24 +1,24 @@
-$NetBSD: patch-src_framework_mlt__properties.c,v 1.2 2014/03/30 19:42:53 abs Exp $
+$NetBSD: patch-src_framework_mlt__properties.c,v 1.3 2019/11/27 21:32:09 markd Exp $
 
 Add conditional section for __NetBSD__
 
---- src/framework/mlt_properties.c.orig	2013-06-03 03:34:35.000000000 +0000
+--- src/framework/mlt_properties.c.orig	2019-11-12 01:44:42.000000000 +0000
 +++ src/framework/mlt_properties.c
-@@ -144,7 +144,7 @@ int mlt_properties_set_lcnumeric( mlt_pr
+@@ -141,7 +141,7 @@ int mlt_properties_set_lcnumeric( mlt_pr
  	{
  		property_list *list = self->local;
  
--#if defined(__linux__) || defined(__DARWIN__)
-+#if defined(__linux__) || defined(__DARWIN__) || (defined(__NetBSD__) && defined(LC_C_LOCALE))
+-#if defined(__GLIBC__) || defined(__APPLE__)
++#if defined(__GLIBC__) || defined(__APPLE__) || (defined(__NetBSD__) && defined(LC_C_LOCALE))
  		if ( list->locale )
  			freelocale( list->locale );
  		list->locale = newlocale( LC_NUMERIC_MASK, locale, NULL );
-@@ -1396,7 +1396,7 @@ void mlt_properties_close( mlt_propertie
+@@ -1425,7 +1425,7 @@ void mlt_properties_close( mlt_propertie
  				free( list->name[ index ] );
  			}
  
--#if defined(__linux__) || defined(__DARWIN__)
-+#if defined(__linux__) || defined(__DARWIN__) || (defined(__NetBSD__) && defined(LC_C_LOCALE))
+-#if defined(__GLIBC__) || defined(__APPLE__)
++#if defined(__GLIBC__) || defined(__APPLE__) || (defined(__NetBSD__) && defined(LC_C_LOCALE))
  			// Cleanup locale
  			if ( list->locale )
  				freelocale( list->locale );
