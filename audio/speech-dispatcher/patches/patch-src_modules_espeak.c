7307760070a2632b6df17cb715b80713127e04bf
nia <nia@pkgsrc.org>
2019-11-28 16:51:49 +0000

    speech-dispatcher: Update to 0.9.1
    Version 0.9.1
    * Add module for the non-free IBM TTS (voxin) speech synthesis.
    * Extend licence to later versions of GPL and LGPL.
    * Update mailing list address to savannah.
    * Make generic modules fallback to existing voices.
    
    Version 0.9
    * Add modules for non-free Baratinoo (VoxyGen) and Kali speech syntheses.
    * Add configuration file for the Mary-TTS system.
    * Add configuration file for espeak-ng + mbrola.
    * Set the pulse client name when using the generic module with paplay.
    * espeak-*-mbrola-generic: Update voice list.
    * Auto-detect module availability.
    * Make generic module provide voice list.
    * Add systemd service file.
    
    Version 0.8.8
    * Add German translation, thanks to Chris Leick for the patch
    * Fix some spelling mistakes, thanks to Paul Gevers for the patch
    * Some slight code improvements
    
    Version 0.8.7
    * Further fixes to spd-conf, which should now work properly.
    * Split the espeak-ng driver code into its own source file.
    * Add a work-around to the espeak-ng driver to account for spaces in voice
      names which recently appeared in espeak-ng git master. This will properly
      be fixed in 0.9.
    * Voice names are not forced to lower case, due to espeak-ng git master now
      having multi-case voice names.
    * Fix stripped audio output from the flite module, thanks to Samuel Thibault.
    * Further code and build improvements.
    
    Version 0.8.6
    * Various internal code improvements.
    * Fix more compiler warnings.
    * Python bug fixes with thanks to Sebastian Humenda.
    
    Version 0.8.5
    * More unused code removal.
    * Fix more compiler warnings.
    * Use GLib main loop for the main server thread.
    * Implement a shutdown timer in the server, which activates after 5 seconds
      with no clients connected.
    * Add support for espeak-ng.
    * Configuration documentation for the ibmtts module.
    * Removal of unused configuration options from the ibmtts module.
    * Add command-line argument to allow for custom modules location

--- audio/speech-dispatcher/patches/patch-src_modules_espeak.c
+++ audio/speech-dispatcher/patches/patch-src_modules_espeak.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_modules_espeak.c,v 1.1 2019/11/28 16:18:54 nia Exp $
+$NetBSD: patch-src_modules_espeak.c,v 1.2 2019/11/28 16:51:49 nia Exp $
 
 Fix path to espeak header file
 
---- src/modules/espeak.c.orig	2016-04-17 22:05:32.000000000 +0000
+--- src/modules/espeak.c.orig	2019-01-01 17:32:54.000000000 +0000
 +++ src/modules/espeak.c
-@@ -37,7 +37,7 @@
+@@ -35,7 +35,7 @@
  #include <semaphore.h>
  
  /* espeak header file */
 -#include <espeak/speak_lib.h>
 +#include <speak_lib.h>
+ 
  #ifndef ESPEAK_API_REVISION
  #define ESPEAK_API_REVISION 1
- #endif
