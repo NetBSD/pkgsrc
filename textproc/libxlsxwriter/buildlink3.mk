fef8063559d705c6ec54f76e2bbeb0ae1ec927fd
sjmulder <sjmulder@pkgsrc.org>
2019-11-30 17:21:58 +0000

    textproc/libxlsxwriter: Update to 0.8.8
    Changes:
     - Added option to allow a user defined, or overridden, image
       description used with `worksheet_insert_image()`. By default it uses
       the filename as the description.
     - Added Windows portable version of `fopen` to handle utf8 filenames
       when working with images.
     - Added an option to allow chart fonts to be rotation to 270 deg to
       give a stacked orientation. Also added support for East Asian
       vertical chart fonts.
     - Refactored struct types used in pubic APIs to remove or document
       hidden fields.
    
       NOTE: This change introduces backward incompatible API changes.
       However, it should minimize any future changes of this nature.

--- textproc/libxlsxwriter/buildlink3.mk
+++ textproc/libxlsxwriter/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/01/30 12:55:13 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2019/11/30 17:21:58 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxlsxwriter
 
 .if !defined(LIBXLSXWRITER_BUILDLINK3_MK)
 LIBXLSXWRITER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=0.8.4
+BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=0.8.8
 BUILDLINK_PKGSRCDIR.libxlsxwriter?=	../../textproc/libxlsxwriter
 
 .include "../../devel/zlib/buildlink3.mk"
