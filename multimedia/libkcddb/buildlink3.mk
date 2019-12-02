e2ff521fba93f93f501287fd4eb36a9aac8a5d9f
markd <markd@pkgsrc.org>
2019-12-02 09:32:30 +0000

    libkcddb: buildlink3.mk update for last.

--- multimedia/libkcddb/buildlink3.mk
+++ multimedia/libkcddb/buildlink3.mk
@@ -1,13 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/01/07 13:04:25 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/12/02 09:32:30 markd Exp $
 
 BUILDLINK_TREE+=	libkcddb
 
 .if !defined(LIBKCDDB_BUILDLINK3_MK)
 LIBKCDDB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkcddb+=	libkcddb>=4.10.2
-BUILDLINK_PKGSRCDIR.libkcddb?=		../../multimedia/libkcddb
+BUILDLINK_API_DEPENDS.libkcddb+=	libkcddb>=19.08.3
+BUILDLINK_PKGSRCDIR.libkcddb?=	../../multimedia/libkcddb
 
+.include "../../audio/libmusicbrainz5/buildlink3.mk"
+.include "../../devel/kio/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# LIBKCDDB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libkcddb
