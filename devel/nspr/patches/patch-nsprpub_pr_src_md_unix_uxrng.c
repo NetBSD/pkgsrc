86ae60fe0b84ac642f4b5412d351394e01580673
ryoon <ryoon@pkgsrc.org>
2019-12-03 14:28:26 +0000

    Update to 4.23
    Changelog:
    NSPR 4.23 contains the following changes:
    - fixed a build failure that was introduced in 4.22
    - correctness fix for Win64 socket polling
    - whitespace in C files was cleaned up and no longer uses
      tab characters for indenting

--- devel/nspr/patches/patch-nsprpub_pr_src_md_unix_uxrng.c
+++ devel/nspr/patches/patch-nsprpub_pr_src_md_unix_uxrng.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-nsprpub_pr_src_md_unix_uxrng.c,v 1.3 2013/07/20 08:57:20 ryoon Exp $
+$NetBSD: patch-nsprpub_pr_src_md_unix_uxrng.c,v 1.4 2019/12/03 14:28:26 ryoon Exp $
 
---- nsprpub/pr/src/md/unix/uxrng.c.orig	2012-05-18 21:55:29.000000000 +0000
+--- nspr/pr/src/md/unix/uxrng.c.orig	2019-10-15 14:21:38.000000000 +0000
 +++ nspr/pr/src/md/unix/uxrng.c
-@@ -94,7 +94,7 @@ GetHighResClock(void *buf, size_t maxbyt
+@@ -64,7 +64,7 @@ GetHighResClock(void *buf, size_t maxbyt
  
  #elif (defined(LINUX) || defined(FREEBSD) || defined(__FreeBSD_kernel__) \
      || defined(NETBSD) || defined(__NetBSD_kernel__) || defined(OPENBSD) \
--    || defined(SYMBIAN) || defined(__GNU__))
-+    || defined(SYMBIAN) || defined(__GNU__)) || defined(__DragonFly__)
+-    || defined(__GNU__))
++    || defined(__GNU__)) || defined(__DragonFly__)
  #include <sys/types.h>
  #include <sys/stat.h>
  #include <fcntl.h>
