e706717d109849d1d1d616d219ef41fd91cd26ee
ryoon <ryoon@pkgsrc.org>
2019-12-03 14:21:20 +0000

    Update to 71.0
    * Remove oss option. Its patch is not usable for 71.0.
    
    Changelog:
    New
        Improvements to Lockwise, our integrated password manager:
            Firefox now recognizes subdomains and will autofill domain logins from Lockwise
            Integrated breach alerts from Firefox Monitor are now available to users with screen readers
    
        More information about Enhanced Tracking Protection in action:
            Notifications when Firefox blocks cryptominers
            A running tally of blocked trackers in the protection panel accessed by clicking the address bar shield
    
        Picture-in-picture video comes to Firefox for Windows: Select the blue icon from the right edge of a video to pop open a floating window so you can keep watching while working in other tabs. Learn how the feature works.
    
        Native MP3 decoding on Windows, Linux, and macOS
    
    Security fixes:
    Not available yet.

--- www/firefox/patches/patch-config__make_system_wrappers.py
+++ www/firefox/patches/patch-config__make_system_wrappers.py
@@ -1,13 +1,13 @@
-$NetBSD: patch-config__make_system_wrappers.py,v 1.1 2019/09/23 08:05:18 ng0 Exp $
+$NetBSD: patch-config__make_system_wrappers.py,v 1.2 2019/12/03 14:21:20 ryoon Exp $
 
 This is a workaround for building firefox with clang from base,
 with a conflict with __isinf(__x) in libc++ vs netbsd headers.
 
---- config/make-system-wrappers.py.orig 2019-01-24 18:48:39.000000000 +0000
+--- config/make-system-wrappers.py.orig	2019-12-02 12:22:52.000000000 +0000
 +++ config/make-system-wrappers.py
-@@ -6,6 +6,7 @@ import os
+@@ -7,6 +7,7 @@ import os
  from mozbuild.util import FileAvoidWrite
-
+ 
  header_template = '''#pragma GCC system_header
 +#define __isinf(x) __builtin_isinf(x)
  #pragma GCC visibility push(default)
