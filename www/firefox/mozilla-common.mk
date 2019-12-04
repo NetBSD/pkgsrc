cd471e0d6f4110b82ed7a024ef76e83dfc24035f
gutteridge <gutteridge@pkgsrc.org>
2019-12-04 05:13:56 +0000

    firefox: reflect new minimum dependencies for 71.0
    Firefox 71.0 now requires NSS>=3.47.1 and Rust >= 1.37. (I haven't
    tested a build with Rust 1.37, but I have with 1.38.)

--- www/firefox/mozilla-common.mk
+++ www/firefox/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.146 2019/12/03 14:21:20 ryoon Exp $
+# $NetBSD: mozilla-common.mk,v 1.147 2019/12/04 05:13:56 gutteridge Exp $
 #
 # common Makefile fragment for mozilla packages based on gecko 2.0.
 #
@@ -212,7 +212,7 @@ BUILDLINK_API_DEPENDS.libevent+=	libevent>=1.1
 BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.23
 .include "../../devel/nspr/buildlink3.mk"
 .include "../../textproc/icu/buildlink3.mk"
-BUILDLINK_API_DEPENDS.nss+=	nss>=3.46.1
+BUILDLINK_API_DEPENDS.nss+=	nss>=3.47.1
 .include "../../devel/nss/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 #.include "../../mk/jpeg.buildlink3.mk"
@@ -227,7 +227,7 @@ BUILDLINK_DEPMETHOD.clang=	build
 BUILDLINK_API_DEPENDS.clang+=	clang>=6.0.1nb1
 .include "../../lang/clang/buildlink3.mk"
 BUILDLINK_DEPMETHOD.rust=	build
-BUILDLINK_API_DEPENDS.rust+=	rust>=1.36.0
+BUILDLINK_API_DEPENDS.rust+=	rust>=1.37.0
 .include "../../lang/rust/buildlink3.mk"
 # webrtc option requires internal libvpx
 #BUILDLINK_API_DEPENDS.libvpx+=	libvpx>=1.3.0
