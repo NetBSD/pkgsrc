a2dd63e13b7b4b8afdff96b0167e4531bf70288a
nia <nia@pkgsrc.org>
2019-12-06 14:00:08 +0000

    gnutls: Update to 3.6.11.1
    Not sure of 3.6.11.1's specific changes - possibly fixing an incorrectly
    generated tarball?
    
    These changes from apply:
    
    * Version 3.6.11 (released 2019-12-01)
    
    ** libgnutls: Use KERN_ARND for the system random number generator on NetBSD.
       This syscall provides an endless stream of random numbers from the kernel's
       ChaCha20-based random number generator, without blocking or requiring an open file
       descriptor.
    
    ** libgnutls: Corrected issue with TLS 1.2 session ticket handling as client
       during resumption (#841).
    
    ** libgnutls: gnutls_base64_decode2() succeeds decoding the empty string to
       the empty string. This is a behavioral change of the API but it conforms
       to the RFC4648 expectations (#834).
    
    ** libgnutls: Fixed AES-CFB8 implementation, when input is shorter than
       the block size. Fix backported from nettle.
    
    ** certtool: CRL distribution points will be set in CA certificates even when
       non self-signed (#765).
    
    ** gnutls-cli/serv: added raw public-key handling capabilities (RFC7250).
       Key material can be set via the --rawpkkeyfile and --rawpkfile flags.
    
    ** API and ABI modifications:
    No changes since last version.

--- security/gnutls/patches/patch-src_libopts_compat_compat.h
+++ security/gnutls/patches/patch-src_libopts_compat_compat.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_libopts_compat_compat.h,v 1.2 2015/06/01 21:50:22 spz Exp $
+$NetBSD: patch-src_libopts_compat_compat.h,v 1.3 2019/12/06 14:00:08 nia Exp $
 
 Workaround for older OSX.
 
---- src/libopts/compat/compat.h.orig	2015-03-23 07:19:23.000000000 +0000
+--- src/libopts/compat/compat.h.orig	2019-12-01 21:25:38.000000000 +0000
 +++ src/libopts/compat/compat.h
 @@ -182,7 +182,7 @@
  #  include <unistd.h>
  #endif
  
--#ifdef HAVE_STDBOOL_H
+-#if defined(HAVE_STDBOOL_H)
 +#if defined(HAVE_STDBOOL_H) || defined(__APPLE__)
  #  include <stdbool.h>
- #else
+ #elif ! defined(bool)
     typedef enum { false = 0, true = 1 } _Bool;
