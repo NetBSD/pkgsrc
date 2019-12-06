ea8ab6d54a5e236aa178d90d72ae75a9814a1b39
nia <nia@pkgsrc.org>
2019-12-06 14:00:08 +0000

    gnutls: Update to 3.6.11.1
    Not sure of 3.6.11.1's specific changes - possibly fixing an incorrectly
    generated tarball?
    
    These changes from apply:
    
    * Version 3.6.11 (released 2019-12-01)
    
    ** libgnutls: Use KERN_ARND for the system random number generator on NetBSD.
       This syscall provides an endless stream of random numbers from the kernel's
       ChaCha20-based random number generator, without blocking or requiring an open file
       descriptor.
    
    ** libgnutls: Corrected issue with TLS 1.2 session ticket handling as client
       during resumption (#841).
    
    ** libgnutls: gnutls_base64_decode2() succeeds decoding the empty string to
       the empty string. This is a behavioral change of the API but it conforms
       to the RFC4648 expectations (#834).
    
    ** libgnutls: Fixed AES-CFB8 implementation, when input is shorter than
       the block size. Fix backported from nettle.
    
    ** certtool: CRL distribution points will be set in CA certificates even when
       non self-signed (#765).
    
    ** gnutls-cli/serv: added raw public-key handling capabilities (RFC7250).
       Key material can be set via the --rawpkkeyfile and --rawpkfile flags.
    
    ** API and ABI modifications:
    No changes since last version.

--- security/gnutls/patches/patch-src_libopts_proto.h
+++ security/gnutls/patches/patch-src_libopts_proto.h
@@ -1,26 +1,26 @@
-$NetBSD: patch-src_libopts_proto.h,v 1.2 2015/06/01 21:50:22 spz Exp $
+$NetBSD: patch-src_libopts_proto.h,v 1.3 2019/12/06 14:00:08 nia Exp $
 
 Avoid stdnoreturn.h.
 
---- src/libopts/proto.h.orig	2015-03-23 07:19:23.000000000 +0000
+--- src/libopts/proto.h.orig	2019-12-01 21:25:38.000000000 +0000
 +++ src/libopts/proto.h
-@@ -98,16 +98,16 @@ load_opt_line(tOptions * opts, tOptState
+@@ -247,16 +247,16 @@ load_opt_line(tOptions * opts, tOptState
  /*
-  *  Extracted from makeshell.c
+  * Static declarations from makeshell.c
   */
--static noreturn void
+-noreturn static void
 +static void
  option_exits(int exit_code);
  
--static noreturn void
+-noreturn static void
 +static void
  ao_bug(char const * msg);
  
  static void
  fserr_warn(char const * prog, char const * op, char const * fname);
  
--static noreturn void
+-noreturn static void
 +static void
  fserr_exit(char const * prog, char const * op, char const * fname);
  
- /*
+ static void
