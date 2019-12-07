8d5c6ab7e922387312291b933b56a8a154848ba4
gutteridge <gutteridge@pkgsrc.org>
2019-12-07 04:50:49 +0000

    wm/enlightenment: update to 1.0.21
    pkgsrc-specific changes:
     - add options for pango, a choice of audio back-end, and font
       inclusion (which may not be necessary, depending on the target OS)
     - enable more modern X11 features
     - note correct license
    
    Addresses PR pkg/52952 by Vicente Chaves.
    
    (Quoting the upstream change log would be excessive here, as there's
    been twelve years of development since this package was last updated in
    pkgsrc. Those interested should simply look at the package's ChangeLog
    file.)
    
    (TBD: this package should probably be renamed to "enlightenment16", to
    avoid confusion and conflict with x11/enlightenment. These two packages
    should be able to co-exist, if desired. This version continues to be
    relevant as it's more lightweight and has fewer dependencies.)

---
+++ wm/enlightenment/options.mk
@@ -0,0 +1,36 @@
+# $NetBSD: options.mk,v 1.1 2019/12/07 04:50:49 gutteridge Exp $
+
+# e16 is used here instead of enlightenment to avoid
+# potential conflict with x11/enlightenment.
+PKG_OPTIONS_VAR=	PKG_OPTIONS.e16
+
+PKG_SUPPORTED_OPTIONS=	pango pulseaudio vera-ttf
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mpango)
+CONFIGURE_ARGS+=	--enable-pango
+.include "../../devel/pango/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-pango
+.endif
+
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+CONFIGURE_ARGS+=	--enable-sound=pulseaudio
+CONFIGURE_ARGS+=	--with-sndldr=sndfile
+.include "../../audio/libsndfile/buildlink3.mk"
+.include "../../audio/pulseaudio/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--enable-sound=esound
+CONFIGURE_ARGS+=	--with-sndldr=audiofile
+.include "../../audio/libaudiofile/buildlink3.mk"
+.include "../../audio/esound/buildlink3.mk"
+.endif
+
+# The upstream package includes its own Vera fonts. Because these have
+# a different (non-default) license, and are also included in some base
+# OS distributions (like NetBSD's native X11), this has been made
+# optional.
+.if !empty(PKG_OPTIONS:Mvera-ttf)
+DEPENDS+=	vera-ttf>=1.10:../../fonts/vera-ttf
+.endif
