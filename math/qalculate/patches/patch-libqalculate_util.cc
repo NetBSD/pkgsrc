c01ae14ce11d48858c757fa63ec30b7a87cd2d31
nros <nros@pkgsrc.org>
2019-12-09 09:31:06 +0000

    Use ICONV_CONST from autotools instead of hardcoding OS versions
    Use ICONV_CONST instead of hardcoding osversions, this is more portable.
    Fixes build on SmartOS.

--- math/qalculate/patches/patch-libqalculate_util.cc
+++ math/qalculate/patches/patch-libqalculate_util.cc
@@ -1,47 +1,23 @@
-$NetBSD: patch-libqalculate_util.cc,v 1.1 2019/11/20 22:15:28 markd Exp $
-
-Support NetBSD iconv variant (pre-9.99.17 and newer)
-
---- libqalculate/util.cc.orig	2019-07-21 07:29:17.000000000 +0000
+$NetBSD: patch-libqalculate_util.cc,v 1.2 2019/12/09 09:31:06 nros Exp $
+* use ICONV_CONST from autotools to support const char**
+* applied upstream https://github.com/Qalculate/libqalculate/pull/168
+--- libqalculate/util.cc.orig	2019-12-08 21:53:48.021366402 +0000
 +++ libqalculate/util.cc
-@@ -20,6 +20,9 @@
- #include <iconv.h>
- #include <sys/types.h>
- #include <sys/stat.h>
-+#if defined(__NetBSD__)
-+#include <sys/param.h>
-+#endif
- #include <sstream>
- #include <fstream>
- #ifdef HAVE_LIBCURL
-@@ -686,6 +689,12 @@ bool removeDir(string dirpath) {
- #endif
- }
- 
-+#if defined(__NetBSD__) && __NetBSD_Version__ < 999001700
-+#define ICONV_CHAR_TYPE const char **
-+#else
-+#define ICONV_CHAR_TYPE char **
-+#endif
-+
- char *locale_from_utf8(const char *str) {
- 	iconv_t conv = iconv_open("", "UTF-8");
- 	if(conv == (iconv_t) -1) return NULL;
-@@ -694,7 +703,7 @@ char *locale_from_utf8(const char *str) 
+@@ -706,7 +706,7 @@ char *locale_from_utf8(const char *str) 
  	char *dest, *buffer;
  	buffer = dest = (char*) malloc((outlength + 4) * sizeof(char));
  	if(!buffer) return NULL;
 -	size_t err = iconv(conv, (char **) &str, &inlength, &buffer, &outlength);
-+	size_t err = iconv(conv, (ICONV_CHAR_TYPE) &str, &inlength, &buffer, &outlength);
++	size_t err = iconv(conv, (ICONV_CONST char **) &str, &inlength, &buffer, &outlength);
  	if(err != (size_t) -1) err = iconv(conv, NULL, &inlength, &buffer, &outlength);
  	iconv_close(conv);
  	memset(buffer, 0, 4);
-@@ -709,7 +718,7 @@ char *locale_to_utf8(const char *str) {
+@@ -721,7 +721,7 @@ char *locale_to_utf8(const char *str) {
  	char *dest, *buffer;
  	buffer = dest = (char*) malloc((outlength + 4) * sizeof(char));
  	if(!buffer) return NULL;
 -	size_t err = iconv(conv, (char**) &str, &inlength, &buffer, &outlength);
-+	size_t err = iconv(conv, (ICONV_CHAR_TYPE) &str, &inlength, &buffer, &outlength);
++	size_t err = iconv(conv, (ICONV_CONST char**) &str, &inlength, &buffer, &outlength);
  	if(err != (size_t) -1) err = iconv(conv, NULL, &inlength, &buffer, &outlength);
  	iconv_close(conv);
  	memset(buffer, 0, 4 * sizeof(char));
