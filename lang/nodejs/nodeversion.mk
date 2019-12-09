cbf1f5607d7b305401c40a12d641993826f847ac
adam <adam@pkgsrc.org>
2019-12-09 20:03:51 +0000

    nodejs: updated to 13.3.0
    Version 13.3.0:
    
    Notable Changes
    fs:
    Reworked experimental recursive rmdir()
    The maxBusyTries option is renamed to maxRetries, and its default is set to 0. The emfileWait option has been removed, and EMFILE errors use the same retry logic as other errors. The retryDelay option is now supported. ENFILE errors are now retried.
    http:
    Make maximum header size configurable per-stream or per-server
    http2:
    Make maximum tolerated rejected streams configurable
    Allow to configure maximum tolerated invalid frames
    wasi:
    Introduce initial WASI support

--- lang/nodejs/nodeversion.mk
+++ lang/nodejs/nodeversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: nodeversion.mk,v 1.5 2018/12/12 16:38:06 adam Exp $
+# $NetBSD: nodeversion.mk,v 1.6 2019/12/09 20:03:51 adam Exp $
 
 # This file determins which nodejs version is used as a dependency for
 # a package.
@@ -8,8 +8,8 @@
 # NODE_VERSION_DEFAULT
 #	The preferred node version to use.
 #
-#	Possible values: 6 8 10
-#	Default: 8
+#	Possible values: 8 10 12 13
+#	Default: 10
 #
 # === Infrastructure variables ===
 #
@@ -27,13 +27,13 @@
 #	is significant; those listed earlier are preferred over those
 #	listed later.
 #
-#	Possible values: 6 8 10
-#	Default: 8 6 10
+#	Possible values: 8 10 12 13
+#	Default: 8 10 12 13
 #
 # NODE_VERSIONS_INCOMPATIBLE
 #	The node versions that the package *cannot* build against.
 #
-#	Possible values: 6 8 10
+#	Possible values: 8 10 12 13
 #	Default: <empty>
 #
 # Keywords: node
@@ -57,8 +57,8 @@ NODE_VERSION_REQD?=	${PKGNAME_OLD:C/(^.*-|^)node([0-9])-.*/\2/}
 BUILD_DEFS+=		NODE_VERSION_DEFAULT
 BUILD_DEFS_EFFECTS+=	NODE_PACKAGE
 
-NODE_VERSION_DEFAULT?=		8
-NODE_VERSIONS_ACCEPTED?=	8 6 10
+NODE_VERSION_DEFAULT?=		10
+NODE_VERSIONS_ACCEPTED?=	8 10 12 13
 NODE_VERSIONS_INCOMPATIBLE?=	# empty
 
 # Resolve NODE_VERSIONS_INCOMPATIBLE and generate the _OK vars.
@@ -90,9 +90,11 @@ _NODE_VERSION?=		none
 
 .if ${_NODE_VERSION} == "8"
 .include "../../lang/nodejs8/buildlink3.mk"
-.elif ${_NODE_VERSION} == "6"
-.include "../../lang/nodejs6/buildlink3.mk"
 .elif ${_NODE_VERSION} == "10"
+.include "../../lang/nodejs10/buildlink3.mk"
+.elif ${_NODE_VERSION} == "12"
+.include "../../lang/nodejs12/buildlink3.mk"
+.elif ${_NODE_VERSION} == "13"
 .include "../../lang/nodejs/buildlink3.mk"
 .else
 PKG_FAIL_REASON+=	"No valid node version found"
--- lang/nodejs/patches/patch-deps_openssl_openssl__asm.gypi
+++ lang/nodejs/patches/patch-deps_openssl_openssl__asm.gypi
@@ -1,11 +1,11 @@
-$NetBSD: patch-deps_openssl_openssl__asm.gypi,v 1.3 2019/02/24 12:18:55 rin Exp $
+$NetBSD: patch-deps_openssl_openssl__asm.gypi,v 1.4 2019/12/09 20:03:51 adam Exp $
 
 Support NetBSD/arm,aarch64,i386,amd64 (and hopefully other ILP32 archs)
 
 XXX Change default to linux-elf instead of linux-x86_64 (from FreeBSD ports)
 
---- deps/openssl/openssl_asm.gypi.orig	2019-01-29 16:20:46.000000000 +0900
-+++ deps/openssl/openssl_asm.gypi	2019-02-24 12:11:23.696088048 +0900
+--- deps/openssl/openssl_asm.gypi.orig	2019-11-19 08:29:07.000000000 +0000
++++ deps/openssl/openssl_asm.gypi
 @@ -46,6 +46,12 @@
        'includes': ['config/archs/linux-elf/asm/openssl.gypi'],
      }, 'target_arch=="x64" and OS=="freebsd"', {
@@ -19,8 +19,8 @@ XXX Change default to linux-elf instead of linux-x86_64 (from FreeBSD ports)
      }, 'target_arch=="x64" and OS=="mac"', {
        'includes': ['config/archs/darwin64-x86_64-cc/asm/openssl.gypi'],
      }, 'target_arch=="x64" and OS=="solaris"', {
-@@ -75,7 +81,7 @@
-       'includes': ['config/archs/linux-x86_64/asm/openssl.gypi'],
+@@ -77,7 +83,7 @@
+       'includes': ['config/archs/linux64-mips64/asm/openssl.gypi'],
      }, {
        # Other architectures don't use assembly
 -      'includes': ['config/archs/linux-x86_64/asm/openssl.gypi'],
