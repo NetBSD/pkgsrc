c42ea3064d27c6bd13c10a369dbbb2eca3b2d5bd
adam <adam@pkgsrc.org>
2019-12-09 20:05:40 +0000

    nodejs: added version 12.13.1, removed version 6.14.3
    Moved nodejs to nodejs10 - version 10.17.0
    
    Version 12.13.1 'Erbium' (LTS):
    
    Notable changes
    Experimental support for building Node.js with Python 3 is improved.
    ICU time zone data is updated to version 2019c. This fixes the date offset in Brazil.

---
+++ lang/nodejs12/patches/patch-deps_openssl_config_bn__conf__no-asm.h
@@ -0,0 +1,30 @@
+$NetBSD: patch-deps_openssl_config_bn__conf__no-asm.h,v 1.1 2019/12/09 20:05:40 adam Exp $
+
+Support NetBSD/arm,aarch64,i386,amd64 (and hopefully other ILP32 archs)
+
+--- deps/openssl/config/bn_conf_no-asm.h.orig	2019-05-28 21:32:16.000000000 +0000
++++ deps/openssl/config/bn_conf_no-asm.h
+@@ -9,9 +9,9 @@
+ # include "./archs/linux-x32/no-asm/crypto/include/internal/bn_conf.h"
+ #elif defined(OPENSSL_LINUX) && defined(__x86_64__)
+ # include "./archs/linux-x86_64/no-asm/crypto/include/internal/bn_conf.h"
+-#elif defined(OPENSSL_LINUX) && defined(__arm__)
++#elif (defined(OPENSSL_LINUX) || defined(__NetBSD__)) && defined(__arm__)
+ # include "./archs/linux-armv4/no-asm/crypto/include/internal/bn_conf.h"
+-#elif defined(OPENSSL_LINUX) && defined(__aarch64__)
++#elif (defined(OPENSSL_LINUX) || defined(__NetBSD__)) && defined(__aarch64__)
+ # include "./archs/linux-aarch64/no-asm/crypto/include/internal/bn_conf.h"
+ #elif defined(__APPLE__) && defined(__MACH__) && defined(__i386__)
+ # include "./archs/darwin-i386-cc/no-asm/crypto/include/internal/bn_conf.h"
+@@ -23,9 +23,9 @@
+ # include "./archs/VC-WIN64A/no-asm/crypto/include/internal/bn_conf.h"
+ #elif defined(_WIN32) && defined(_M_ARM64)
+ # include "./archs/VC-WIN64-ARM/no-asm/crypto/include/internal/bn_conf.h"
+-#elif (defined(__FreeBSD__) || defined(__OpenBSD__)) && defined(__i386__)
++#elif (defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)) && defined(__i386__)
+ # include "./archs/BSD-x86/no-asm/crypto/include/internal/bn_conf.h"
+-#elif (defined(__FreeBSD__) || defined(__OpenBSD__)) && defined(__x86_64__)
++#elif (defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)) && defined(__x86_64__)
+ # include "./archs/BSD-x86_64/no-asm/crypto/include/internal/bn_conf.h"
+ #elif defined(__sun) && defined(__i386__)
+ # include "./archs/solaris-x86-gcc/no-asm/crypto/include/internal/bn_conf.h"
