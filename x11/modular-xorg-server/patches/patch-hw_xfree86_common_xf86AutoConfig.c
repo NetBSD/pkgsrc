403ff9da0a7c9bf8fd69c04773c687b7a1ae196a
nia <nia@pkgsrc.org>
2019-12-10 17:27:05 +0000

    modular-xorg-server: Sync with current NetBSD xsrc
    Match the modesetting driver on x86 and ARM NetBSD.
    
    from maya
    
    Bump PKGREVISION

--- x11/modular-xorg-server/patches/patch-hw_xfree86_common_xf86AutoConfig.c
+++ x11/modular-xorg-server/patches/patch-hw_xfree86_common_xf86AutoConfig.c
@@ -1,10 +1,23 @@
-$NetBSD: patch-hw_xfree86_common_xf86AutoConfig.c,v 1.3 2018/10/26 10:20:12 maya Exp $
+$NetBSD: patch-hw_xfree86_common_xf86AutoConfig.c,v 1.4 2019/12/10 17:27:05 nia Exp $
 
-Look for wsfb on netbsd (needed by genfb)
+https://github.com/NetBSD/xsrc/commit/38ac2ddeea11ea3a612c616447d191f685781f88.patch
+https://github.com/NetBSD/xsrc/commit/38ac2ddeea11ea3a612c616447d191f685781f88.patch
 
---- hw/xfree86/common/xf86AutoConfig.c.orig	2018-10-25 14:13:21.000000000 +0000
+--- hw/xfree86/common/xf86AutoConfig.c.orig	2019-11-22 23:44:19.000000000 +0000
 +++ hw/xfree86/common/xf86AutoConfig.c
-@@ -313,6 +313,10 @@ listPossibleVideoDrivers(XF86MatchedDriv
+@@ -307,12 +307,22 @@ listPossibleVideoDrivers(XF86MatchedDriv
+ #endif
+ #endif                          /* !__sun */
+ 
++#if defined(__NetBSD__) && \
++    (defined(__aarch64__) || defined(__arm__) || \
++     defined(__i386__) || defined(__amd64__))
++    xf86AddMatchedDriver(md, "modesetting");
++#endif
++
+     /* Fallback to platform default hardware */
+ #if defined(__i386__) || defined(__amd64__) || defined(__hurd__)
+     xf86AddMatchedDriver(md, "vesa");
  #elif defined(__sparc__) && !defined(__sun)
      xf86AddMatchedDriver(md, "sunffb");
  #endif
