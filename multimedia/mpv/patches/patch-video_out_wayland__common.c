c720fa6a0728f0aa246654df05da507be3d317cc
nia <nia@pkgsrc.org>
2019-12-10 10:45:07 +0000

    mpv: More evdev mouse button definitions now needed

--- multimedia/mpv/patches/patch-video_out_wayland__common.c
+++ multimedia/mpv/patches/patch-video_out_wayland__common.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-video_out_wayland__common.c,v 1.3 2019/10/29 22:46:39 nia Exp $
+$NetBSD: patch-video_out_wayland__common.c,v 1.4 2019/12/10 10:45:07 nia Exp $
 
 Allow building on systems without evdev.
 
 --- video/out/wayland_common.c.orig	2019-10-25 13:08:15.000000000 +0000
 +++ video/out/wayland_common.c
-@@ -19,7 +19,13 @@
+@@ -19,7 +19,15 @@
  #include <limits.h>
  #include <poll.h>
  #include <unistd.h>
@@ -14,6 +14,8 @@ Allow building on systems without evdev.
 +#define BTN_LEFT	(0x110)
 +#define BTN_RIGHT	(0x111)
 +#define BTN_MIDDLE	(0x112)
++#define BTN_SIDE	(0x113)
++#define BTN_EXTRA	(0x114)
 +#endif
  #include <time.h>
  #include "common/msg.h"
