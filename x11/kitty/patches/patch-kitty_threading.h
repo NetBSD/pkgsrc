e49c3dd6c550b2fca4cbef341f3cb6c505341ad7
nia <nia@pkgsrc.org>
2019-12-14 22:08:10 +0000

    kitty: Update to 0.15.0
    Various pkgsrc changes aiming to reduce the complexity of this package
    and make it less error-prone, in spite of the very weird homebrew build
    system.
    
    0.15.0 [2019-11-27]
    
        Add a new action detach_window that can be used to move the current window into a different tab (#1310)
    
        Add a new action launch that unifies launching of processes in new kitty windows/tabs.
    
        Add a new style powerline for tab bar rendering, see tab_bar_style (#2021)
    
        Allow changing colors by mapping a keyboard shortcut to read a kitty config file with color definitions. See the FAQ for details (#2083)
    
        hints kitten: Allow completely customizing the matching and actions performed by the kitten using your own script (#2124)
    
        Wayland: Fix key repeat not being stopped when focus leaves window. This is expected behavior on Wayland, apparently (#2014)
    
        When drawing unicode symbols that are followed by spaces, use multiple cells to avoid resized or cut-off glyphs (#1452)
    
        diff kitten: Allow diffing remote files easily via ssh (#727)
    
        unicode input kitten: Add an option kitty +kitten unicode_input --emoji-variation to control the presentation variant of selected emojis (#2139)
    
        Add specialised rendering for a few more box powerline and unicode symbols (#2074 and #2021)
    
        Add a new socket only mode for allow_remote_control. This makes it possible for programs running on the local machine to control kitty but not programs running over ssh.
    
        hints kitten: Allow using named groups in the regular expression. The named groups are passed to the invoked program for further processing.
    
        Fix a regression in 0.14.5 that caused rendering of private use glyphs with and without spaces to be identical (#2117)
    
        Wayland: Fix incorrect scale used when first creating an OS window (#2133)
    
        macOS: Disable mouse hiding by default as getting it to work robustly on Cocoa is too much effort (#2158)

--- x11/kitty/patches/patch-kitty_threading.h
+++ x11/kitty/patches/patch-kitty_threading.h
@@ -1,24 +1,24 @@
-$NetBSD: patch-kitty_threading.h,v 1.1 2018/11/28 18:48:58 nia Exp $
+$NetBSD: patch-kitty_threading.h,v 1.2 2019/12/14 22:08:10 nia Exp $
 
 Support NetBSD.
 
---- kitty/threading.h.orig	2018-09-29 03:45:40.000000000 +0000
+--- kitty/threading.h.orig	2019-11-27 04:25:00.000000000 +0000
 +++ kitty/threading.h
-@@ -14,6 +14,8 @@
- #if defined(__APPLE__)
- // I cant figure out how to get pthread.h to include this definition on macOS. MACOSX_DEPLOYMENT_TARGET does not work.
- extern int pthread_setname_np(const char *name);
-+#elif defined(__NetBSD__)
-+extern int pthread_getname_np(pthread_t, char *, size_t);
+@@ -17,6 +17,8 @@ extern int pthread_setname_np(const char
  #elif defined(FREEBSD_SET_NAME)
  // Function has a different name on FreeBSD
  void pthread_set_name_np(pthread_t tid, const char *name);
-@@ -27,6 +29,8 @@ set_thread_name(const char *name) {
-     int ret;
- #if defined(__APPLE__)
-     ret = pthread_setname_np(name);
 +#elif defined(__NetBSD__)
-+    ret = pthread_setname_np(pthread_self(), name, NULL);
++int pthread_setname_np(pthread_t thread, const char *name, void *arg);
+ #else
+ // Need _GNU_SOURCE for pthread_setname_np on linux and that causes other issues on systems with old glibc
+ extern int pthread_setname_np(pthread_t, const char *name);
+@@ -30,6 +32,8 @@ set_thread_name(const char *name) {
  #elif defined(FREEBSD_SET_NAME)
      pthread_set_name_np(pthread_self(), name);
      ret = 0;
++#elif defined(__NetBSD__)
++    ret = pthread_setname_np(pthread_self(), "%s", (char *)name);
+ #else
+     ret = pthread_setname_np(pthread_self(), name);
+ #endif
