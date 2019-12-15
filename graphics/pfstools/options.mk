3d5ecafbe0be29fa119b4a7a561a14b7454a3c68
adam <adam@pkgsrc.org>
2019-12-15 21:32:52 +0000

    pfstools: updated to 2.1.0
    2.1.0:
    This release adds support for .yuv video files encoded as PQ2020 or HLG2020 (used by the reference implementation of h265). pfsview was updated to link with Qt5 instead of Qt4. Several compilation issues on Ubuntu have been resolved.

--- graphics/pfstools/options.mk
+++ graphics/pfstools/options.mk
@@ -1,28 +1,19 @@
-# $NetBSD: options.mk,v 1.7 2019/10/28 09:42:28 kamil Exp $
+# $NetBSD: options.mk,v 1.8 2019/12/15 21:32:52 adam Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.pfstools
-PKG_SUPPORTED_OPTIONS=	fftw imagemagick octave opengl
-
+PKG_OPTIONS_VAR=		PKG_OPTIONS.pfstools
+PKG_SUPPORTED_OPTIONS=		imagemagick octave opengl qt
 PKG_SUGGESTED_OPTIONS.Darwin+=	opengl
 
-PLIST_VARS+=	fftw im octave gl
+PLIST_VARS+=	im octave gl qt
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Mfftw)
-.include "../../math/fftwf/buildlink3.mk"
-PLIST.fftw=	yes
-CMAKE_ARGS+=	-DWITH_FFTW=YES
-.else
-CMAKE_ARGS+=	-DWITH_FFTW=NO
-.endif
 
 .if !empty(PKG_OPTIONS:Mimagemagick)
 .include "../../graphics/ImageMagick6/buildlink3.mk"
 PLIST.im=	yes
-CMAKE_ARGS+=	-DWITH_ImageMagick=YES
+CMAKE_ARGS+=	-DWITH_ImageMagick=ON
 .else
-CMAKE_ARGS+=	-DWITH_ImageMagick=NO
+CMAKE_ARGS+=	-DWITH_ImageMagick=OFF
 .endif
 
 .if !empty(PKG_OPTIONS:Moctave)
@@ -33,9 +24,17 @@ REPLACE_OCTAVE+=	src/octave/pfsstat
 PLIST.octave=	yes
 PLIST_SUBST+=	OCT_LOCALVEROCTFILEDIR=${OCT_LOCALVEROCTFILEDIR:S/${BUILDLINK_PREFIX.octave}\///}
 PLIST_SUBST+=	OCT_LOCALVERFCNFILEDIR=${OCT_LOCALVERFCNFILEDIR:S/${BUILDLINK_PREFIX.octave}\///}
-CMAKE_ARGS+=	-DWITH_Octave=YES
+CMAKE_ARGS+=	-DWITH_Octave=ON
 .else
-CMAKE_ARGS+=	-DWITH_Octave=NO
+CMAKE_ARGS+=	-DWITH_Octave=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mqt)
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+PLIST.qt=	yes
+CMAKE_ARGS+=	-DWITH_QT=ON
+.else
+CMAKE_ARGS+=	-DWITH_QT=OFF
 .endif
 
 .if !empty(PKG_OPTIONS:Mopengl)
@@ -45,7 +44,7 @@ CMAKE_ARGS+=	-DWITH_Octave=NO
 # XXX nasty hack
 CXXFLAGS.NetBSD+=	-lpthread
 PLIST.gl=	yes
-CMAKE_ARGS+=	-DWITH_pfsglview=YES
+CMAKE_ARGS+=	-DWITH_pfsglview=ON
 .else
-CMAKE_ARGS+=	-DWITH_pfsglview=NO
+CMAKE_ARGS+=	-DWITH_pfsglview=OFF
 .endif
