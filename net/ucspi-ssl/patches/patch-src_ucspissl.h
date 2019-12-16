c39181ca24e73cd4a71ea3d9cfa31e749041e58f
schmonz <schmonz@pkgsrc.org>
2019-12-16 21:26:43 +0000

    Update to 0.11.1 (as 0.999.11.1). From the changelog:
    - Added compatibility with fehQlibs-13.
    - Fixed wrong behavior of sslserver/sslclient given a local or remote
      IPv4 address. sslhandle is now an own program (man sslhandle.3).
    - Code streamlined with ucspi-tcp6-1.11.0.
    - Removed parenthesis from host in https@: [$host]:$port -> $host:port.
      Tx, A.E.
    - Fixed TLSv1* macro's names in ucspissl.h to match ssl_context.c.
    - Clarified usage of 'SSL_CTX_set_ciphersuites()' in ssl_ciphers.c.

--- net/ucspi-ssl/patches/patch-src_ucspissl.h
+++ net/ucspi-ssl/patches/patch-src_ucspissl.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_ucspissl.h,v 1.1 2018/10/28 17:10:27 schmonz Exp $
+$NetBSD: patch-src_ucspissl.h,v 1.2 2019/12/16 21:26:43 schmonz Exp $
 
 Disable TLSv1.0.
 
---- src/ucspissl.h.orig	2018-10-11 12:24:52.000000000 +0000
+--- src/ucspissl.h.orig	2019-10-31 21:23:05.000000000 +0000
 +++ src/ucspissl.h
-@@ -15,7 +15,7 @@
+@@ -23,7 +23,7 @@
  
  #define SSLv2_DISABLE
  #define SSLv3_DISABLE
 -// #define TLSv1_DISABLE
 +#define TLSv1_DISABLE
- // #define TLSv1.1_DISABLE
- // #define TLSv1.2_DISABLE
- // #define TLSv1.3_DISABLE
+ // #define TLSv1_1_DISABLE
+ // #define TLSv1_2_DISABLE
+ // #define TLSv1_3_DISABLE
