128759b30192a3671c406d83887822daf5b458d5
taca <taca@pkgsrc.org>
2019-12-16 16:52:40 +0000

    www/php-ja-wordpress: update to 5.3.1
    Update php-ja-wordpress from 4.5.3 to 5.3.1.
    
    This release contains these security fixes.
    
    * Props to Daniel Bachhuber for finding an issue where an unprivileged
      user could make a post sticky via the REST API.
    
    * Props to Simon Scannell of RIPS Technologies for finding and
      disclosing an issue where cross-site scripting (XSS) could be stored
      in well-crafted links.
    
    * Props to the WordPress.org Security Team for hardening
      wp_kses_bad_protocol() to ensure that it is aware of the named colon
      attribute.
    
    * Props to Nguyen The Duc for discovering a stored XSS vulnerability
      using block editor content.
    
    For more detail about version 5.3.1, please refer
    <https://wordpress.org/news/2019/12/wordpress-5-3-1-security-and-maintenance-release/>
    
    And changes from 4.5.3 to 5.3.0, please refer HOMEPAGE and
    <https://wordpress.org/>.

--- www/php-ja-wordpress/options.mk
+++ www/php-ja-wordpress/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2014/10/19 14:14:47 ryoon Exp $
+# $NetBSD: options.mk,v 1.3 2019/12/16 16:52:40 taca Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.wordpress
 
@@ -10,14 +10,14 @@ PKG_SUGGESTED_OPTIONS=		php-fpm
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mphp-cgi)
-DEPENDS+=	php>=5.2.4:${PHPPKGSRCDIR}
+DEPENDS+=	php>=5.6.0:${PHPPKGSRCDIR}
 .endif
 
 .if !empty(PKG_OPTIONS:Map-php)
 .  include "../../mk/apache.mk"
-DEPENDS+=	${APACHE_PKG_PREFIX}-${PHP_PKG_PREFIX}>=4.3.0:../../www/ap-php
+DEPENDS+=	${APACHE_PKG_PREFIX}-${PHP_PKG_PREFIX}>=5.6.0:../../www/ap-php
 .endif
 
 .if !empty(PKG_OPTIONS:Mphp-fpm)
-DEPENDS+=	${PHP_PKG_PREFIX}-fpm>=5.2.4:../../www/php-fpm
+DEPENDS+=	${PHP_PKG_PREFIX}-fpm>=5.6.0:../../www/php-fpm
 .endif
