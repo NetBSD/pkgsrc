4e2fed29986ddcda73f1f377b0261694da29e387
mef <mef@pkgsrc.org>
2019-12-18 12:38:48 +0000

    (editors/emacs-snapshot) Updated 25.0.x to 27.0.50
    - currently almost the same as wip/emacs-git

--- editors/emacs-snapshot/files/site-init.el
+++ editors/emacs-snapshot/files/site-init.el
@@ -1,4 +1,4 @@
-;; $NetBSD: site-init.el,v 1.1 2015/06/14 15:46:22 mef Exp $
+;; $NetBSD: site-init.el,v 1.2 2019/12/18 12:38:48 mef Exp $
 
 (autoload 'send-pr "/usr/share/gnats/send-pr.el" 
   "Command to create and send a problem report." t)
--- editors/emacs-snapshot/options.mk
+++ editors/emacs-snapshot/options.mk
@@ -1,24 +1,27 @@
-# $NetBSD: options.mk,v 1.12 2019/06/08 10:14:08 rillig Exp $
-#
+# $NetBSD: options.mk,v 1.13 2019/12/18 12:38:48 mef Exp $
 
 ### Set options
-PKG_OPTIONS_VAR=	PKG_OPTIONS.emacs
-PKG_SUPPORTED_OPTIONS=	dbus gconf gnutls imagemagick svg xaw3d xft2 xml
+PKG_OPTIONS_VAR=			PKG_OPTIONS.emacs
+PKG_SUPPORTED_OPTIONS=			dbus gconf gnutls imagemagick jansson svg xaw3d xft2 xml
+PKG_SUGGESTED_OPTIONS=			jansson
 # xaw3d is only valid with tookit = xaw
 
 PKG_OPTIONS_OPTIONAL_GROUPS+=		window-system
 PKG_OPTIONS_GROUP.window-system=	x11 nextstep
+
 # tempted to have 'nox11' :-)
 
-PKG_OPTIONS_OPTIONAL_GROUPS+=	toolkit
-#  --with-x-toolkit=KIT    use an X toolkit (KIT one of: yes or gtk, gtk2,
+PKG_OPTIONS_OPTIONAL_GROUPS+=		toolkit
+PKG_SUGGESTED_OPTIONS.Darwin=		nextstep
+#  --with-x-toolkit=KIT    use an X toolkit (KIT one of: yes or  gtk2,
 #                          gtk3, lucid or athena, motif, no)
 # gtk in next line implies gtk2, xaw = athena = lucid
-PKG_OPTIONS_GROUP.toolkit=	gtk motif xaw lucid
-# gtk is default in the logic below (even not included in SUGGESTED_=
-# gconf, gtk and xft2 will be ingnored for nextstep even shown as selected.
+PKG_OPTIONS_GROUP.toolkit=		gtk gtk2 gtk3 motif xaw lucid
+# gtk2 and gtk has the same effect
+# gtk3 is default in the logic below (even not included in SUGGESTED_=
+# gconf, gtk* and xft2 will be ignored for nextstep even shown as selected.
 
-PKG_SUGGESTED_OPTIONS=	dbus gconf gnutls imagemagick svg xaw3d xft2 xml x11
+PKG_SUGGESTED_OPTIONS=	dbus gconf gnutls gtk3 xaw3d xft2 xml x11
 
 .include "../../mk/bsd.options.mk"
 
@@ -27,7 +30,7 @@ PKG_SUGGESTED_OPTIONS=	dbus gconf gnutls imagemagick svg xaw3d xft2 xml x11
 ### Support D-BUS
 ###
 .if !empty(PKG_OPTIONS:Mdbus)
-.include "../../sysutils/dbus/buildlink3.mk"
+.  include "../../sysutils/dbus/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-dbus
 .endif
@@ -36,11 +39,11 @@ CONFIGURE_ARGS+=	--without-dbus
 ### Support XML2
 ###
 .if !empty(PKG_OPTIONS:Mxml)
-USE_TOOLS+=             pkg-config
+USE_TOOLS+=		pkg-config
 BUILDLINK_API_DEPENDS.libxml2+= libxml2>=2.6.17
 .include "../../textproc/libxml2/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=        --without-xml2
+CONFIGURE_ARGS+=	--without-xml2
 .endif
 
 ###
@@ -48,17 +51,9 @@ CONFIGURE_ARGS+=        --without-xml2
 ###
 .if !empty(PKG_OPTIONS:Mgnutls)
 .include "../../security/gnutls/buildlink3.mk"
+.include "../../security/p11-kit/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-gnutls
-.endif
-
-###
-### Support ImageMagick
-###
-.if !empty(PKG_OPTIONS:Mimagemagick)
-.include "../../graphics/ImageMagick/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-imagemagick
 .endif
 
 ###
@@ -87,6 +82,25 @@ CONFIGURE_ARGS+=	--without-gconf
 .  endif
 
 ###
+### Support ImageMagick (not recognized for now, sorry)
+###
+.  if !empty(PKG_OPTIONS:Mimagemagick)
+.include "../../graphics/ImageMagick/buildlink3.mk"
+# DEPENDS+=	py[0-9]*-wand-[0-9]*:../../graphics/py-wand
+.  else
+CONFIGURE_ARGS+=	--without-imagemagick
+.  endif
+
+###
+### Support jansson (JSON library)
+###
+.  if !empty(PKG_OPTIONS:Mjansson)
+.include "../../textproc/jansson/buildlink3.mk"
+.  else
+CONFIGURE_ARGS+=	--without-json
+.  endif
+
+###
 ### Support Xaw3d (This is only valid with Lucid Toolkit)
 ###
 .  if !empty(PKG_OPTIONS:Mxaw3d)
@@ -109,15 +123,17 @@ CONFIGURE_ARGS+=	--without-xft --without-libotf --without-m17n-flt
 ###
 ### Toolkit selection
 ###
-.  if (empty(PKG_OPTIONS:Mxaw) && empty(PKG_OPTIONS:Mlucid) &&  empty(PKG_OPTIONS:Mmotif))
-# defaults to gtk
+.  if (empty(PKG_OPTIONS:Mxaw) && empty(PKG_OPTIONS:Mlucid) && \
+       empty(PKG_OPTIONS:Mgtk) && empty(PKG_OPTIONS:Mgtk2) && \
+       empty(PKG_OPTIONS:Mmotif))
+# defaults to gtk3
+USE_TOOLS+=		pkg-config
+.include "../../x11/gtk3/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-x-toolkit=gtk3
+.  elif !empty(PKG_OPTIONS:Mgtk2) || !empty(PKG_OPTIONS:Mgtk)
 USE_TOOLS+=		pkg-config
 .include "../../x11/gtk2/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-x-toolkit=gtk
-.  elif !empty(PKG_OPTIONS:Mgtk)
-USE_TOOLS+=		pkg-config
-.include "../../x11/gtk2/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-x-toolkit=gtk
+CONFIGURE_ARGS+=	--with-x-toolkit=gtk2
 .  elif !empty(PKG_OPTIONS:Mxaw)
 .include "../../mk/xaw.buildlink3.mk"
 CONFIGURE_ARGS+=	--with-x-toolkit=athena
@@ -175,6 +191,7 @@ post-install:
 		pax -rw -pp -pm Emacs.app ${DESTDIR}${PREFIX}/${APPLICATIONS_DIR}
 
 .else  # no window system
+#.if empty(PKG_OPTIONS:Mx11)
 CONFIGURE_ARGS+=	--without-x
 CONFIGURE_ARGS+=	--without-xpm
 CONFIGURE_ARGS+=	--without-jpeg
