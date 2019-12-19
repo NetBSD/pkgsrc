e196c1cccb9add0e48d997e4b9d181514f0f0ffa
joerg <joerg@pkgsrc.org>
2019-12-19 22:24:39 +0000

    Add missing errno.h

---
+++ textproc/xapian/patches/patch-common_errno__to__string.cc
@@ -0,0 +1,12 @@
+$NetBSD: patch-common_errno__to__string.cc,v 1.1 2019/12/19 22:24:39 joerg Exp $
+
+--- common/errno_to_string.cc.orig	2019-12-19 14:19:58.476027186 +0000
++++ common/errno_to_string.cc
+@@ -27,6 +27,7 @@
+ #include "errno_to_string.h"
+ 
+ // <cstring> doesn't give us strerror_r() with Sun C++ 5.9.
++#include <errno.h>
+ #include <string.h>
+ #if defined HAVE__SYS_ERRLIST_AND__SYS_NERR || \
+     defined HAVE_SYS_ERRLIST_AND_SYS_NERR
