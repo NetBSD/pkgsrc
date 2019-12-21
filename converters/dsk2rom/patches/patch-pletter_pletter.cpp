fcfaa3f4cb44a6514eef5aeeb3411707e7c8d602
joerg <joerg@pkgsrc.org>
2019-12-21 23:30:21 +0000

    Don't try to build header files. Fix C/C++ interaction.

--- converters/dsk2rom/patches/patch-pletter_pletter.cpp
+++ converters/dsk2rom/patches/patch-pletter_pletter.cpp
@@ -1,6 +1,7 @@
-$NetBSD: patch-pletter_pletter.cpp,v 1.1 2018/08/13 15:53:49 nia Exp $
+$NetBSD: patch-pletter_pletter.cpp,v 1.2 2019/12/21 23:30:21 joerg Exp $
 
 Add missing includes.
+pletter is called from C code.
 
 --- pletter/pletter.cpp.orig	2007-05-27 11:18:42.000000000 +0000
 +++ pletter/pletter.cpp
@@ -14,3 +15,12 @@ Add missing includes.
  #include <string>
  using namespace std;
  
+@@ -306,7 +309,7 @@ int main(int argc, char *argv[]) {
+ 
+ //-----------------------------------------------------------------------------
+ 
+-void pletter( unsigned char*  input,  int  input_length,
++extern "C" void pletter( unsigned char*  input,  int  input_length,
+ 	      unsigned char** output, int* output_length,
+ 	      int pletmode  )
+ {
