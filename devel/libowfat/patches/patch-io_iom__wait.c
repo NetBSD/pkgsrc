cea8b8de9d42507d3230993f1344ce98462028dc
joerg <joerg@pkgsrc.org>
2019-12-22 22:23:00 +0000

    Fix kqueue support. Bump revision.

--- devel/libowfat/patches/patch-io_iom__wait.c
+++ devel/libowfat/patches/patch-io_iom__wait.c
@@ -1,9 +1,18 @@
-$NetBSD: patch-io_iom__wait.c,v 1.1 2018/10/07 12:13:46 schmonz Exp $
+$NetBSD: patch-io_iom__wait.c,v 1.2 2019/12/22 22:23:00 joerg Exp $
 
 Fix build on systems with kqueue(2).
 
 --- io/iom_wait.c.orig	2018-04-11 14:12:50.000000000 +0000
 +++ io/iom_wait.c
+@@ -65,7 +65,7 @@ int iom_wait(iomux_t* c,int64* s,unsigne
+ #elif defined(HAVE_KQUEUE)
+       struct kevent kev[SLOTS];
+       struct timespec ts = { .tv_sec=timeout/1000, .tv_nsec=(timeout%1000)*1000000 };
+-      int r=kevent(c->ctx, 0, 0, &kev, freeslots, &ts);
++      int r=kevent(c->ctx, 0, 0, kev, freeslots, &ts);
+       if (r<=0) {
+ 	/* we ran into a timeout, so let someone else take over */
+ 	if (__sync_val_compare_and_swap(&c->working,1,0)==-2) return -2;
 @@ -76,13 +76,13 @@ int iom_wait(iomux_t* c,int64* s,unsigne
  #endif
  	return r;
