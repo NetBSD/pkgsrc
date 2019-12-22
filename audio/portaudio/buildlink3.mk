a708c15a5264619314279a4e8611845414349f58
nia <nia@pkgsrc.org>
2019-12-22 04:14:33 +0000

    portaudio: fix buildlink3 for alsa/jack

--- audio/portaudio/buildlink3.mk
+++ audio/portaudio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2019/10/20 11:10:49 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2019/12/22 04:14:33 nia Exp $
 
 BUILDLINK_TREE+=	portaudio
 
@@ -10,6 +10,18 @@ BUILDLINK_PKGSRCDIR.portaudio?=		../../audio/portaudio
 BUILDLINK_INCDIRS.portaudio?=		include/portaudio2
 BUILDLINK_LIBDIRS.portaudio+=		lib/portaudio2
 BUILDLINK_CPPFLAGS.portaudio+=		-I${BUILDLINK_PREFIX.portaudio}/include/portaudio2
+
+pkgbase:=	portaudio
+
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.portaudio:Malsa)
+.  include "../../audio/alsa-lib/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.portaudio:Mjack)
+.  include "../../audio/jack/buildlink3.mk"
+.endif
 .endif # PORTAUDIO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-portaudio
