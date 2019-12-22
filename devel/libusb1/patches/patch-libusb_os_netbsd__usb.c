15cad0ba1fe9d3454faf7a602289ad6d64cc6c58
jmcneill <jmcneill@pkgsrc.org>
2019-12-22 17:06:39 +0000

    Translate EWOULDBLOCK to LIBUSB_ERROR_TIMEOUT on NetBSD.

--- devel/libusb1/patches/patch-libusb_os_netbsd__usb.c
+++ devel/libusb1/patches/patch-libusb_os_netbsd__usb.c
@@ -1,7 +1,8 @@
-$NetBSD: patch-libusb_os_netbsd__usb.c,v 1.1 2019/12/22 16:49:05 jmcneill Exp $
+$NetBSD: patch-libusb_os_netbsd__usb.c,v 1.2 2019/12/22 17:06:39 jmcneill Exp $
 
-Do not emit log messages from netbsd_clock_gettime as it is called from
-the logger.
+* Do not emit log messages from netbsd_clock_gettime as it is called from
+  the logger.
+* Translate EWOULDBLOCK (ugen timeouts) errors to LIBUSB_ERROR_TIMEOUT.
 
 --- libusb/os/netbsd_usb.c.orig	2018-03-17 07:23:37.000000000 +0000
 +++ libusb/os/netbsd_usb.c
@@ -14,3 +15,12 @@ the logger.
  	if (clkid == USBI_CLOCK_REALTIME)
  		return clock_gettime(CLOCK_REALTIME, tp);
  
+@@ -516,6 +514,8 @@ _errno_to_libusb(int err)
+ 		return (LIBUSB_ERROR_NO_DEVICE);
+ 	case ENOMEM:
+ 		return (LIBUSB_ERROR_NO_MEM);
++	case EWOULDBLOCK:
++		return (LIBUSB_ERROR_TIMEOUT);
+ 	}
+ 
+ 	usbi_dbg("error: %s", strerror(err));
