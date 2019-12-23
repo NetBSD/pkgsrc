4efb48cdb5fafb6c7a9524e4b4b267297f30fe31
sjmulder <sjmulder@pkgsrc.org>
2019-12-23 13:43:31 +0000

    textproc/libxlsxwriter: update to 0.8.9
    Changes:
     - Added support for default hyperlink style in worksheet_write_url().
     - Added support for hyperlink in images, see
       worksheet_insert_image_opt().
     - Fixed several worksheet_write_url() edge cases.

--- textproc/libxlsxwriter/buildlink3.mk
+++ textproc/libxlsxwriter/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/11/30 17:21:58 sjmulder Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/12/23 13:43:31 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxlsxwriter
 
 .if !defined(LIBXLSXWRITER_BUILDLINK3_MK)
 LIBXLSXWRITER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=0.8.8
+BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=0.8.9
 BUILDLINK_PKGSRCDIR.libxlsxwriter?=	../../textproc/libxlsxwriter
 
 .include "../../devel/zlib/buildlink3.mk"
