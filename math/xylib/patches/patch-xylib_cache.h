fbdb7d393fc49074742978793c562cd623115078
nros <nros@pkgsrc.org>
2019-12-23 19:54:51 +0000

    Fix build of math/xylib on gcc 6 and above
    gcc 6 and above inmplicity uses c++14 so use std::shared_ptr.

--- math/xylib/patches/patch-xylib_cache.h
+++ math/xylib/patches/patch-xylib_cache.h
@@ -1,13 +1,21 @@
-$NetBSD: patch-xylib_cache.h,v 1.1 2013/04/30 22:26:29 joerg Exp $
+$NetBSD: patch-xylib_cache.h,v 1.2 2019/12/23 19:54:51 nros Exp $
 
---- xylib/cache.h.orig	2013-04-29 18:52:26.000000000 +0000
+* use std::shared_prt on gcc 6 and above since they implicitly use c++14
+
+--- xylib/cache.h.orig	2012-07-25 22:25:14.000000000 +0000
 +++ xylib/cache.h
-@@ -28,7 +28,10 @@
+@@ -28,7 +28,16 @@
  #define XYLIB_USE_TR1_MEMORY 1
  #endif
  
 -#if XYLIB_USE_TR1_MEMORY
-+#if defined(_LIBCPP_VERSION)
++#ifdef __GNUC__
++#if __GNUC__ >= 6
++#define __GNU_V6__
++#endif
++#endif
++
++#if defined(_LIBCPP_VERSION) || defined(__GNU_V6__)
 +# include <memory>
 +using std::shared_ptr;
 +#elif XYLIB_USE_TR1_MEMORY
