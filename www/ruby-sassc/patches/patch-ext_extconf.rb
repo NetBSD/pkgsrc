a2000b712befcddf6eced73bc6e790e49e67580c
joerg <joerg@pkgsrc.org>
2019-12-25 00:14:15 +0000

    Don't force LTO. At least with Clang, the linker also needs to support it.

--- www/ruby-sassc/patches/patch-ext_extconf.rb
+++ www/ruby-sassc/patches/patch-ext_extconf.rb
@@ -1,8 +1,11 @@
-$NetBSD: patch-ext_extconf.rb,v 1.1 2019/10/01 15:34:50 ryoon Exp $
+$NetBSD: patch-ext_extconf.rb,v 1.2 2019/12/25 00:14:15 joerg Exp $
+
+Don't enable host-specific optimisations.
+Don't enable LTO.
 
 --- ext/extconf.rb.orig	2019-09-27 13:30:47.910318310 +0000
 +++ ext/extconf.rb
-@@ -19,12 +19,6 @@ if enable_config('static-stdlib', false)
+@@ -19,18 +19,6 @@ if enable_config('static-stdlib', false)
    $LDFLAGS << ' -static-libgcc -static-libstdc++'
  end
  
@@ -12,6 +15,12 @@ $NetBSD: patch-ext_extconf.rb,v 1.1 2019/10/01 15:34:50 ryoon Exp $
 -  $CXXFLAGS << ' -march=native -mtune=native'
 -end
 -
- if enable_config('lto', true)
-   $CFLAGS << ' -flto'
-   $CXXFLAGS << ' -flto'
+-if enable_config('lto', true)
+-  $CFLAGS << ' -flto'
+-  $CXXFLAGS << ' -flto'
+-  $LDFLAGS << ' -flto'
+-end
+-
+ # Disable noisy compilation warnings.
+ $warnflags = ''
+ $CFLAGS.gsub!(/[\s+](-ansi|-std=[^\s]+)/, '')
