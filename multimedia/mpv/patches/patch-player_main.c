c3dfb00e31cdbdfd02df0803dbf04de9d65f4a8f
leot <leot@pkgsrc.org>
2019-12-28 17:20:32 +0000

    mpv: Update to 0.31.0
    Changes:
    0.31.0
    ======
    Features
    --------
    Added
    ~~~~~
    - vo_gpu/d3d11: allow configurable swapchain color space for native HDR output
    - player: add mechanisms for better logging as preperation for console.lua
    - audio: add ao_audiotrack for Android
    - input: introduce a pseudo key name that grabs all text input
    - wayland: add grab zone for resizing window with mouse
    - osc: implement pseudo client side decorations via OSC
    - vf_gpu: add video filter using vo_gpu's renderer
    - added console.lua: interactive REPL for input commands
    - osc: display Santa hat for idle logo in December
    - vd_lavc: add option to autoselect known good hwdec implementations
    - mac: replace old event tap for media key support with MediaPlayer
    
    Changed
    ~~~~~~~
    - input: disable gamepad code by default
    - vo_gpu: allow vdpau usage under EGL
    - stream, demux: redo origin policy
    
    Removed
    ~~~~~~~
    - mac: remove Apple Remote support
    - client API: deprecate tick event
    
    Options and Commands
    --------------------
    Added
    ~~~~~
    - vo_gpu/d3d11: add --d3d11-output-csp for configuring swap chain color space
    - command: add command-list property
    - command, input: add input-bindings property
    - command: add --window-maximized and make --window-minimized settable
    - command: add property returning hidpi scale
    - command, lua: add a way to share data between scripts
    - client API, lua: add new API for setting OSD overlays
    
    Changed
    ~~~~~~~
    - add a builtin "sw-fast" profile, which restores performance settings
      for software video conversion. These were switched to higher quality.
      This affects video outputs like vo_x11 and vo_drm, and screenshots.
    - options: make --show-profile without parameters list all profiles
    - wayland: support for --hidpi-window-scale option
    - stream_lavf: set --network-timeout to 60 seconds by default
    - x11: scale window-scale by DPI
    
    Deprecated
    ~~~~~~~~~~
    - deprecate --input-file
    - deprecate --video-sync=display-adrop
    - deprecate all input section commands
    - deprecate --display-fps, replaced by --override-display-fps
    
    Removed
    ~~~~~~~
    - remove deprecated --playlist-pos alias
    
    Fixes and Minor Enhancements
    ----------------------------
    - vo_drm, vo_x11: allow use of zimg
    - vo_sdl: Support mouse wheel input, Improve mouse button input
    - video: fix track switching work for external images
    - vo_gpu: fix infinite scaler reinit spam
    - f_output_chain: fix possible crash when changing filters
    - vo_gpu: opengl: add hack for ancient Mesa/GLX
    - demux_lavf: export demuxer_id for more formats which have it
    - lua: make later key bindings always have higher priority
    - drm_common: fix display FPS estimation for interlaced modes
    - vo_drm: fix incorrect pixel format possibly being picked for 30-bit
    - zsh completion: fix pcre usage to really be optional
    - options: increase consistency between list options and document them
    - osc: reduce unnecesary CPU usage by using property notifications

--- multimedia/mpv/patches/patch-player_main.c
+++ multimedia/mpv/patches/patch-player_main.c
@@ -1,15 +1,18 @@
-$NetBSD: patch-player_main.c,v 1.5 2019/10/25 14:35:56 leot Exp $
+$NetBSD: patch-player_main.c,v 1.6 2019/12/28 17:20:33 leot Exp $
 
 Avoid to check mismatched built vs running libraries version.
 The use in pkgsrc can be considered a legitimate one.
 
---- player/main.c.orig	2019-10-25 13:08:15.000000000 +0000
+--- player/main.c.orig	2019-12-28 11:07:07.000000000 +0000
 +++ player/main.c
-@@ -384,15 +384,6 @@ int mp_initialize(struct MPContext *mpct
+@@ -387,18 +387,6 @@ int mp_initialize(struct MPContext *mpct
      if (handle_help_options(mpctx))
          return 1; // help
  
 -    if (!print_libav_versions(mp_null_log, 0)) {
+-        // This happens only if the runtime FFmpeg version is lower than the
+-        // build version, which will not work according to FFmpeg's ABI rules.
+-        // This does not happen if runtime FFmpeg is newer, which is compatible.
 -        print_libav_versions(mpctx->log, MSGL_FATAL);
 -        MP_FATAL(mpctx, "\nmpv was compiled against an incompatible version of "
 -                 "FFmpeg/Libav than the shared\nlibrary it is linked against. "
@@ -18,6 +21,6 @@ The use in pkgsrc can be considered a legitimate one.
 -        return -1;
 -    }
 -
-     if (!mpctx->playlist->first && !opts->player_idle_mode) {
-         // nothing to play
-         mp_print_version(mpctx->log, true);
+ #if HAVE_TESTS
+     if (opts->test_mode && opts->test_mode[0])
+         return run_tests(mpctx) ? 1 : -1;
