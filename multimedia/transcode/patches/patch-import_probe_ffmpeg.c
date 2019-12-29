32ba16d58e333a6bcc370c9542d6e62125b0b8ac
markd <markd@pkgsrc.org>
2019-12-29 10:23:53 +0000

    transcode: use ffmpeg4

--- multimedia/transcode/patches/patch-import_probe_ffmpeg.c
+++ multimedia/transcode/patches/patch-import_probe_ffmpeg.c
@@ -1,9 +1,20 @@
-$NetBSD: patch-import_probe_ffmpeg.c,v 1.2 2015/11/21 17:14:26 adam Exp $
+$NetBSD: patch-import_probe_ffmpeg.c,v 1.3 2019/12/29 10:23:53 markd Exp $
 
-Fixes building against FFmpeg version >= 2.0.0 (gentoo patch).
+Fixes building against FFmpeg version >= 4.0.0 (gentoo patch).
 
 --- import/probe_ffmpeg.c.orig	2011-11-19 16:50:27.000000000 +0000
 +++ import/probe_ffmpeg.c
+@@ -51,8 +51,8 @@ static void translate_info(const AVForma
+             info->bitrate = st->codec->bit_rate / 1000;
+             info->width = st->codec->width;
+             info->height = st->codec->height;
+-            if (st->r_frame_rate.num > 0 && st->r_frame_rate.den > 0) {
+-                info->fps = av_q2d(st->r_frame_rate);
++            if (st->avg_frame_rate.num > 0 && st->avg_frame_rate.den > 0) {
++                info->fps = av_q2d(st->avg_frame_rate);
+             } else {
+                 /* watch out here */
+                 info->fps = 1.0/av_q2d(st->codec->time_base);
 @@ -99,8 +99,8 @@ void probe_ffmpeg(info_t *ipipe)
  
      TC_INIT_LIBAVCODEC;
@@ -24,12 +35,15 @@ Fixes building against FFmpeg version >= 2.0.0 (gentoo patch).
      if (ret < 0) {
          tc_log_error(__FILE__, "unable to fetch informations from '%s'"
                                 " (libavformat failure)",
-@@ -120,7 +120,7 @@ void probe_ffmpeg(info_t *ipipe)
+@@ -120,7 +120,11 @@ void probe_ffmpeg(info_t *ipipe)
  
      translate_info(lavf_dmx_context, ipipe->probe_info);
  
--    av_close_input_file(lavf_dmx_context);
++#if LIBAVFORMAT_VERSION_INT > AV_VERSION_INT(53,25,0)
 +    avformat_close_input(&lavf_dmx_context);
++#else
+     av_close_input_file(lavf_dmx_context);
++#endif
      return;
  }
  
