2c0b1c42368f97ab6e996c9f378255dd2152029f
pho <pho@pkgsrc.org>
2019-12-29 23:20:52 +0000

    Update to zlib-0.6.2.1
    0.6.2.1
     * Add support for GHC 8.8 / base-4.13
    
    0.6.2
     * New cabal flag 'pkg-config' for discovering 'zlib` via pkg-config(1) (#16)
     * Use CApiFFI where available for cross-compile friendliness (#14)
     * Change the window bits range from 8..15 to 9..15 (#11)
    
    0.6.1.2
     * Fix a segfault when reading the stream multithreaded, #7
     * New experimental cabal flag 'non-blocking-ffi' for 'safe' ffi calls
    
    0.6.1.1
     * Fixed building with GHC 7.0 and 7.2
    
    0.6.0.2
     * Fixed building with GHC 7.0 and 7.2
    
    0.6.1.0
     * Support for concatenated gzip files (multiple back-to-back streams)
    
    0.6.0.1
     * Fixed building with older GHC
     * Fixed warnings with new GHC
     * Fixed building on Windows
     * Fixed testsuite
    
    0.6.0.0
     * New incremental interface for compression and decompression
     * Provide access to unconsumed trailing data
     * Simplified structured error type, and instance of Exception
     * Updated bundled zlib C code to 1.2.8 (used on Windows)
     * Fixed memory leak of zlib z_stream C structure
     * More derivied instances (Eq, Show, Typeable, Generic)
    
    0.5.4.2
     * Builds with GHC 7.10

--- archivers/hs-zlib/buildlink3.mk
+++ archivers/hs-zlib/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:38 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/12/29 23:20:52 pho Exp $
 
 BUILDLINK_TREE+=	hs-zlib
 
 .if !defined(HS_ZLIB_BUILDLINK3_MK)
 HS_ZLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-zlib+=	hs-zlib>=0.5.4
-BUILDLINK_ABI_DEPENDS.hs-zlib+=	hs-zlib>=0.5.4.1
+BUILDLINK_API_DEPENDS.hs-zlib+=	hs-zlib>=0.6.2
+BUILDLINK_ABI_DEPENDS.hs-zlib+=	hs-zlib>=0.6.2.1
 BUILDLINK_PKGSRCDIR.hs-zlib?=	../../archivers/hs-zlib
 
 .include "../../devel/zlib/buildlink3.mk"
