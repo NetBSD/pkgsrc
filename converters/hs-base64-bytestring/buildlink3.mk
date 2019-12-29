5293cda22725b5a97120d0c01cf96b7ec117fd8e
pho <pho@pkgsrc.org>
2019-12-29 23:26:43 +0000

    Update to base64-bytestring-1.0.0.2
    * Fixed a write past allocated memory in joinWith (potential security
      issue).

--- converters/hs-base64-bytestring/buildlink3.mk
+++ converters/hs-base64-bytestring/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:38 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/12/29 23:26:43 pho Exp $
 
 BUILDLINK_TREE+=	hs-base64-bytestring
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-base64-bytestring
 HS_BASE64_BYTESTRING_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=1.0.0
-BUILDLINK_ABI_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=1.0.0.1
+BUILDLINK_ABI_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=1.0.0.2
 BUILDLINK_PKGSRCDIR.hs-base64-bytestring?=	../../converters/hs-base64-bytestring
 .endif	# HS_BASE64_BYTESTRING_BUILDLINK3_MK
 
