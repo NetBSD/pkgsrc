253380b8827465b08a8c0cfc2919e0a92c359499
adam <adam@pkgsrc.org>
2019-12-29 15:31:21 +0000

    llvm: updated to 9.0.1
    9.0.1 is a bug-fix release.

--- lang/compiler-rt/buildlink3.mk
+++ lang/compiler-rt/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/06/02 08:35:56 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/12/29 15:31:21 adam Exp $
 
 BUILDLINK_TREE+=	compiler-rt
 
 .if !defined(COMPILER_RT_BUILDLINK3_MK)
 COMPILER_RT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.compiler-rt+=	compiler-rt>=8.0.0
+BUILDLINK_API_DEPENDS.compiler-rt+=	compiler-rt>=9.0.1
 BUILDLINK_PKGSRCDIR.compiler-rt?=	../../lang/compiler-rt
 
 .include "../../lang/llvm/buildlink3.mk"
