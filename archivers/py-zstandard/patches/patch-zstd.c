608e2d2dd74709ac67372a7dd145028b7c5bd219
adam <adam@pkgsrc.org>
2019-12-29 16:53:13 +0000

    py-zstandard: updated to 0.13.0
    Changes 0.13.0:
    * ``pytest-xdist`` ``pytest`` extension is now installed so tests can be
      run in parallel.
    * CI now builds ``manylinux2010`` and ``manylinux2014`` binary wheels
      instead of a mix of ``manylinux2010`` and ``manylinux1``.
    * Official support for Python 3.8 has been added.
    * Bundled zstandard library upgraded from 1.4.3 to 1.4.4.
    * Python code has been reformatted with black.

--- archivers/py-zstandard/patches/patch-zstd.c
+++ archivers/py-zstandard/patches/patch-zstd.c
@@ -1,14 +1,14 @@
-$NetBSD: patch-zstd.c,v 1.3 2019/09/19 16:50:57 adam Exp $
+$NetBSD: patch-zstd.c,v 1.4 2019/12/29 16:53:13 adam Exp $
 
 Allow newer zstd.
 
---- zstd.c.orig	2019-09-16 01:27:27.000000000 +0000
+--- zstd.c.orig	2019-12-28 17:43:58.000000000 +0000
 +++ zstd.c
 @@ -210,10 +210,6 @@ void zstd_module_init(PyObject* m) {
  	   We detect this mismatch here and refuse to load the module if this
  	   scenario is detected.
  	*/
--	if (ZSTD_VERSION_NUMBER != 10403 || ZSTD_versionNumber() != 10403) {
+-	if (ZSTD_VERSION_NUMBER != 10404 || ZSTD_versionNumber() != 10404) {
 -		PyErr_SetString(PyExc_ImportError, "zstd C API mismatch; Python bindings not compiled against expected zstd version");
 -		return;
 -	}
