8b870af1276a6d5f1da6cd19a495bf92d6949262
adam <adam@pkgsrc.org>
2019-12-29 15:31:21 +0000

    llvm: updated to 9.0.1
    9.0.1 is a bug-fix release.

--- devel/lld/buildlink3.mk
+++ devel/lld/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/06/02 08:37:39 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/12/29 15:31:21 adam Exp $
 
 BUILDLINK_TREE+=	lld
 
 .if !defined(LLD_BUILDLINK3_MK)
 LLD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lld+=	lld>=8.0.0
+BUILDLINK_API_DEPENDS.lld+=	lld>=9.0.1
 BUILDLINK_PKGSRCDIR.lld?=	../../devel/lld
 
 .include "../../lang/llvm/buildlink3.mk"
--- lang/clang/buildlink3.mk
+++ lang/clang/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.11 2019/10/19 13:54:29 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2019/12/29 15:31:22 adam Exp $
 
 BUILDLINK_TREE+=	clang
 
 .if !defined(CLANG_BUILDLINK3_MK)
 CLANG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clang+=	clang>=9.0.0
+BUILDLINK_API_DEPENDS.clang+=	clang>=9.0.1
 BUILDLINK_PKGSRCDIR.clang?=	../../lang/clang
 
 .include "../../textproc/libxml2/buildlink3.mk"
