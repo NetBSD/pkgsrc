cb59908df96c5d626c13bf64d20013513f25fffa
sjmulder <sjmulder@pkgsrc.org>
2019-12-30 12:35:42 +0000

    libxlsxwriter: Update to 0.91
    Changes in 0.91:
     - Fix to missing MD5 linkage in Cocoapod file.
    
    Changes in 0.90:
     - Fix to avoid duplicate images being copied to an libxlsxwriter
       file. Excel uses an optimization where it only stores one copy of
       a repeated/duplicate image in a workbook. Libxlsxwriter didn't do
       this which meant that the file size would increase when then was a
       large number of repeated images. This release fixes that issue and
       replicates Excel's behavior.

--- textproc/libxlsxwriter/buildlink3.mk
+++ textproc/libxlsxwriter/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2019/12/23 13:43:31 sjmulder Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2019/12/30 12:35:42 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxlsxwriter
 
 .if !defined(LIBXLSXWRITER_BUILDLINK3_MK)
 LIBXLSXWRITER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=0.8.9
+BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=0.9.1
 BUILDLINK_PKGSRCDIR.libxlsxwriter?=	../../textproc/libxlsxwriter
 
 .include "../../devel/zlib/buildlink3.mk"
