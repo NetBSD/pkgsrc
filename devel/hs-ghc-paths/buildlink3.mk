a69d2531fad51b865eff372ece114c6a25900156
pho <pho@pkgsrc.org>
2019-12-31 08:12:48 +0000

    Update to ghc-paths-0.1.0.12
    No changelogs are provided by the upstream.

--- devel/hs-ghc-paths/buildlink3.mk
+++ devel/hs-ghc-paths/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:39 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/12/31 08:12:48 pho Exp $
 
 BUILDLINK_TREE+=	hs-ghc-paths
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-ghc-paths
 HS_GHC_PATHS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-ghc-paths+=	hs-ghc-paths>=0.1.0
-BUILDLINK_ABI_DEPENDS.hs-ghc-paths+=	hs-ghc-paths>=0.1.0.9
+BUILDLINK_ABI_DEPENDS.hs-ghc-paths+=	hs-ghc-paths>=0.1.0.12
 BUILDLINK_PKGSRCDIR.hs-ghc-paths?=	../../devel/hs-ghc-paths
 .endif	# HS_GHC_PATHS_BUILDLINK3_MK
 
