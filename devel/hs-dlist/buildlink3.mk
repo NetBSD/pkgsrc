ed9a5645d05876524b8e7cc24ad1587a623c6b39
pho <pho@pkgsrc.org>
2019-12-31 07:33:17 +0000

    Update to dlist-0.8.0.7
    Version 0.8.0.7 (2019-08-05) Independence Day in Burkina Faso
    * Add MonadFail instance for GHC >= 8.8 (Vanessa McHale)
    * Change deepseq upper bound to 1.5 (Herbert Valerio Riedel)
    
    Version 0.8.0.6 (2019-03-29) Martyrs' Day in Madagascar
    * Change QuickCheck upper bound from 2.13 to 2.14
    
    Version 0.8.0.5 (2018-09-13) Day of the Programmer
    * Change QuickCheck upper bound from 2.12 to 2.13
    
    Version 0.8.0.4 (2018-01-19) Kokborok Day
    * Change QuickCheck upper bound from 2.11 to 2.12
    * Make Data.DList trustworthy (Bertram Felgenhauer)
    * Remove quickcheck-instances dependency for tests
    
    Version 0.8.0.3 (2017-07-04) Independence Day in the United States
    * Change QuickCheck upper bound from 2.10 to 2.11 and import the
      Arbitrary NonEmpty instance from quickcheck-instances for 2.10
    * Fix stimes property in test suite (Oleg Grenrus)
    
    Version 0.8.0.2 (2016-09-04) World Sexual Health Day
    * Fix test suite: add missing module OverloadedStrings (Sergei
      Trofimovich)
    
    Version 0.8.0.1 (2016-07-29) 58th Anniversary of the Creation of NASA
    * Change QuickCheck lower bound to 2.9 for GHC >= 8 (base >= 4.9)
      (Adam Bergmark)
    
    Version 0.8 (2016-07-17) Constitution Day in South Korea
    * Add pattern synonyms Nil and Cons for GHC >= 7.8
    * Add Semigroup instance for GHC >= 8 (base >= 4.9)
    * Use inflexible instance for IsString to improve support for
      overloaded strings (Baldur Blöndal)
    * Change QuickCheck upper bound from 2.9 to 2.10
    * Add -Wall -Werror testing
    * Add testing for GHC 8.0.1 to Travis-CI

--- devel/hs-dlist/buildlink3.mk
+++ devel/hs-dlist/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2018/01/07 13:04:03 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2019/12/31 07:33:17 pho Exp $
 
 BUILDLINK_TREE+=	hs-dlist
 
 .if !defined(HS_DLIST_BUILDLINK3_MK)
 HS_DLIST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-dlist+=	hs-dlist>=0.7.1
-BUILDLINK_ABI_DEPENDS.hs-dlist+=	hs-dlist>=0.7.1.2
+BUILDLINK_API_DEPENDS.hs-dlist+=	hs-dlist>=0.8.0
+BUILDLINK_ABI_DEPENDS.hs-dlist+=	hs-dlist>=0.8.0.7
 BUILDLINK_PKGSRCDIR.hs-dlist?=		../../devel/hs-dlist
 .endif	# HS_DLIST_BUILDLINK3_MK
 
