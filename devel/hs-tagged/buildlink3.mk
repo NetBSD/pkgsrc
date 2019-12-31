a84bec938a01b9bc7039a47305d671712a211f62
pho <pho@pkgsrc.org>
2019-12-31 18:51:27 +0000

    Remove dependency on devel/hs-transformers

--- devel/hs-tagged/buildlink3.mk
+++ devel/hs-tagged/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2019/12/31 14:47:06 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2019/12/31 18:52:58 pho Exp $
 
 BUILDLINK_TREE+=	hs-tagged
 
@@ -8,8 +8,6 @@ HS_TAGGED_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-tagged+=	hs-tagged>=0.8.6
 BUILDLINK_ABI_DEPENDS.hs-tagged+=	hs-tagged>=0.8.6
 BUILDLINK_PKGSRCDIR.hs-tagged?=		../../devel/hs-tagged
-
-.include "../../devel/hs-transformers/buildlink3.mk"
 .endif	# HS_TAGGED_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-tagged
