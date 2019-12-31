cf01410f210e17732c92c85892f59c6dd9fce8bb
pho <pho@pkgsrc.org>
2019-12-31 18:51:27 +0000

    Remove dependency on devel/hs-transformers

--- math/hs-semigroups/buildlink3.mk
+++ math/hs-semigroups/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2019/12/31 16:36:07 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2019/12/31 18:57:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-semigroups
 
@@ -12,7 +12,6 @@ BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../math/hs-semigroups
 .include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
 .include "../../devel/hs-text/buildlink3.mk"
-.include "../../devel/hs-transformers/buildlink3.mk"
 .include "../../devel/hs-transformers-compat/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .endif	# HS_SEMIGROUPS_BUILDLINK3_MK
