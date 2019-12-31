fb9077346968aad5b28eb03c6d33474663d9761b
pho <pho@pkgsrc.org>
2019-12-31 12:47:44 +0000

    Update to hashable-1.3.0.0
    Version 1.3.0.0
    * Semantic change of Hashable Arg instance to not hash the second
      argument of Arg in order to be consistent with Eq Arg (#171)
    * Semantic change of Hashable Float and Hashable Double instances to
      hash -0.0 and 0.0 to the same value (#173)
    * Add Hashable instance for Fingerprint (#156)
    * Add new Data.Hashable.Generic module providing the default
      implementations genericHashWithSalt and genericLiftHashWithSalt
      together with other Generics support helpers (#148, #178)
    * Bump minimum version requirement of base to base-4.5 (i.e. GHC >=
      7.4)
    
    Version 1.2.7.0
    * Add Hashable and Hashable1 instances for Complex
    * Fix undefined behavior in hashable_fn_hash() implementation due to
      signed integer overflow (#152)
    * Mark Data.Hashable.Lifted as Trustworthy (re SafeHaskell)
    * Support GHC 8.4
    
    Version 1.2.6.1
    * Use typeRepFingerprint from Type.Reflection.Unsafe
    * Bump minimum version of base to 4.4.
    
    Version 1.2.6.0
    * Add support for type-indexed Typeable.
    * Rework the Generic hashable for sums.
    
    Version 1.2.5.0
    * Add Hashable1 and Hashable2
    * Add instances for: Eq1, Ord1, Show1, Ptr, FunPtr, IntPtr, WordPtr
    * Add Hashed type for caching the hash function result.
    
    Version 1.2.4.0
    * Add instances for: Unique, Version, Fixed, NonEmpty, Min, Max, Arg,
      First, Last, WrappedMonoid, Option
    * Support GHC 8.0

--- devel/hs-hashable/buildlink3.mk
+++ devel/hs-hashable/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2016/01/10 11:48:24 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2019/12/31 12:47:44 pho Exp $
 
 BUILDLINK_TREE+=	hs-hashable
 
 .if !defined(HS_HASHABLE_BUILDLINK3_MK)
 HS_HASHABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.2.3
-BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.2.3.3
+BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.3.0
+BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.3.0.0
 BUILDLINK_PKGSRCDIR.hs-hashable?=	../../devel/hs-hashable
 
 .include "../../devel/hs-text/buildlink3.mk"
