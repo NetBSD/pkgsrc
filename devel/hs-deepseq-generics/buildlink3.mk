7e90cecbae3e47c1112badf385d66e051c7c0f9a
pho <pho@pkgsrc.org>
2019-12-31 07:52:47 +0000

    Update to deepseq-generics-0.2.0.0
    0.2.0.0 January 2016
    * Add support for GHC 8.0
    * Remove re-exports from deepseq:Control.DeepSeq
    * Synchronise implementation to deepseq-1.4.0.0's code
    
    0.1.1.2 December 2014
    * Add support for GHC 7.10 and deepseq-1.4.0.0

--- devel/hs-deepseq-generics/buildlink3.mk
+++ devel/hs-deepseq-generics/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:39 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/12/31 07:52:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-deepseq-generics
 
 .if !defined(HS_DEEPSEQ_GENERICS_BUILDLINK3_MK)
 HS_DEEPSEQ_GENERICS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-deepseq-generics+=	hs-deepseq-generics>=0.1.1
-BUILDLINK_ABI_DEPENDS.hs-deepseq-generics+=	hs-deepseq-generics>=0.1.1.1
+BUILDLINK_API_DEPENDS.hs-deepseq-generics+=	hs-deepseq-generics>=0.2.0
+BUILDLINK_ABI_DEPENDS.hs-deepseq-generics+=	hs-deepseq-generics>=0.2.0.0
 BUILDLINK_PKGSRCDIR.hs-deepseq-generics?=	../../devel/hs-deepseq-generics
 .endif	# HS_DEEPSEQ_GENERICS_BUILDLINK3_MK
 
