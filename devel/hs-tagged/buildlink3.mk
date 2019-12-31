f931b53babd593b9d9a9052e8c725d54982e07dc
pho <pho@pkgsrc.org>
2019-12-31 14:47:06 +0000

    Update to tagged-0.8.6
    0.8.6 [2018.07.02]
    * Make the Read(1) instances for Proxy ignore the precedence argument,
      mirroring similar changes to base here.
    * Fix a bug in the Floating instance for Tagged in which logBase was
      defined in terms of (**).
    * Avoid incurring some dependencies when using recent GHCs.
    
    0.8.5
    * Support Data.Bifoldable/Data.Bitraversable in base for GHC 8.1+.
    * Backport the Eq1, Ord1, Read1, and Show1 instances for Proxy from
      base-4.9
    * Add Eq1/2, Ord1/2, Read1/2, and Show1/2 instances for Tagged
    
    0.8.4
    * Backport the Alternative, MonadPlus, and MonadZip instances for
      Proxy from base-4.9
    * Add Bits, FiniteBits, IsString, and Storable instances for Tagged
    
    0.8.3
    * Manual Generic1 support to work around a bug in GHC 7.6
    * Invert the dependency to supply the Semigroup instance ourselves
      when building on GHC 8
    
    0.8.2
    * deepseq support.
    * Widened template-haskell dependency bounds.
    
    0.8.1
    * Add KProxy to the backwards compatibility Data.Proxy module.
    * Add a Generic instance to Proxy.
    
    0.8.0.1
    * Fix builds on GHC 7.4.
    
    0.8
    * Added Data.Proxy.TH, based on the code from Frames by Anthony Cowley.
    * Removed reproxy from Data.Proxy. This is a bad API decision, but it
      isn't present in GHC's Data.Proxy, and this makes the API more
      stable.
    
    0.7.3
    * Support Data.Bifunctor in base for GHC 7.9+.

--- devel/hs-tagged/buildlink3.mk
+++ devel/hs-tagged/buildlink3.mk
@@ -1,13 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/01/07 13:04:04 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2019/12/31 14:47:06 pho Exp $
 
 BUILDLINK_TREE+=	hs-tagged
 
 .if !defined(HS_TAGGED_BUILDLINK3_MK)
 HS_TAGGED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tagged+=	hs-tagged>=0.7.2
-BUILDLINK_ABI_DEPENDS.hs-tagged+=	hs-tagged>=0.7.2
+BUILDLINK_API_DEPENDS.hs-tagged+=	hs-tagged>=0.8.6
+BUILDLINK_ABI_DEPENDS.hs-tagged+=	hs-tagged>=0.8.6
 BUILDLINK_PKGSRCDIR.hs-tagged?=		../../devel/hs-tagged
+
+.include "../../devel/hs-transformers/buildlink3.mk"
 .endif	# HS_TAGGED_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-tagged
