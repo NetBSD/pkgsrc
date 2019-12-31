b2dc961ae311ac0a149001f9fef4619487f58b43
pho <pho@pkgsrc.org>
2019-12-31 07:52:08 +0000

    Add dependency on devel/hs-old-locale

--- devel/hs-data-default-instances-old-locale/buildlink3.mk
+++ devel/hs-data-default-instances-old-locale/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:39 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2019/12/31 07:52:08 pho Exp $
 
 BUILDLINK_TREE+=	hs-data-default-instances-old-locale
 
@@ -10,6 +10,7 @@ BUILDLINK_ABI_DEPENDS.hs-data-default-instances-old-locale+=	hs-data-default-ins
 BUILDLINK_PKGSRCDIR.hs-data-default-instances-old-locale?=	../../devel/hs-data-default-instances-old-locale
 
 .include "../../devel/hs-data-default-class/buildlink3.mk"
+.include "../../devel/hs-old-locale/buildlink3.mk"
 .endif	# HS_DATA_DEFAULT_INSTANCES_OLD_LOCALE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-data-default-instances-old-locale
