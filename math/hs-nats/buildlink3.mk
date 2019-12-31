0370f5c4887e3afa54a84d5f21f1bc6e00ca72ee
pho <pho@pkgsrc.org>
2019-12-31 13:55:47 +0000

    Update to nats-1.1.2
    1.1.2 [2018-02-06]
    * Don't enable Safe on GHC 7.2.
    
    1.1.1
    * Backported a more efficient Binary instance for Nat
    * Allow binary-0.8
    
    1.1
    * Backported Lift instances.
    
    1.0
    * Make nats a compat-package since Numeric.Natural moved to base-4.8.0.0.
    
    0.2.1
    * Better readsPrec handling when you try to feed it a negative
      number. Note it is a failed parse rather than an error.

--- math/hs-nats/buildlink3.mk
+++ math/hs-nats/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2016/01/10 11:49:10 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2019/12/31 13:55:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-nats
 
 .if !defined(HS_NATS_BUILDLINK3_MK)
 HS_NATS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-nats+=	hs-nats>=0.2
-BUILDLINK_ABI_DEPENDS.hs-nats+=	hs-nats>=0.2nb8
+BUILDLINK_API_DEPENDS.hs-nats+=	hs-nats>=1.1.2
+BUILDLINK_ABI_DEPENDS.hs-nats+=	hs-nats>=1.1.2
 BUILDLINK_PKGSRCDIR.hs-nats?=	../../math/hs-nats
 
 .include "../../devel/hs-hashable/buildlink3.mk"
