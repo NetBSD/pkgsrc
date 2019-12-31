90ca098bd8d9ad37cc7bf99498bb4c49c445b2a1
pho <pho@pkgsrc.org>
2019-12-31 08:59:52 +0000

    Update to text-1.2.4.0
    1.2.4.0
    * Add TH Lift instances for Data.Text.Text and Data.Text.Lazy.Text
      (gh-232)
    * Update Haddock documentation to better reflect fusion eligibility;
      improve fusion rules for takeWhileEnd and length (gh-241, ghc-202)
    * Optimise Data.Text.replicate from O(n) to O(log n) (gh-209)
    * Support base-4.13.0.0
    
    1.2.3.1
    * Make decodeUtf8With fail explicitly for unsupported non-BMP
      replacement characters instead silent undefined behaviour (gh-213)
    * Fix termination condition for file reads via Data.Text.IO operations
      (gh-223)
    * A serious correctness issue affecting uses of take and drop with
      negative counts has been fixed (gh-227)
    * A bug in the case-mapping functions resulting in unreasonably large
      allocations with large arguments has been fixed (gh-221)
    
    1.2.3.0
    * Spec compliance: toCaseFold now follows the Unicode 9.0 spec
      (updated from 8.0).
    * Bug fix: the lazy takeWhileEnd function violated the lazy text
      invariant (gh-184).
    * Bug fix: Fixed usage of size hints causing incorrect behavior
      (gh-197).
    * New function: unsnoc (gh-173).
    * Reduce memory overhead in encodeUTF8 (gh-194).
    * Improve UTF-8 decoder error-recovery (gh-182).
    * Minor documentation improvements (@since annotations, more examples,
      clarifications).
    
    1.2.2.2
    * The toTitle function now correctly handles letters that immediately
      follow punctuation. Before, "there's" would turn into
      "There'S". Now, it becomes "There's".
    * The implementation of unstreaming is faster, resulting in operations
      such as map and intersperse speeding up by up to 30%, with smaller
      code generated.
    * The optimised length comparison function is now more likely to be
      used after some rewrite rule tweaking.
    * Bug fix: an off-by-one bug in takeEnd is fixed.
    * Bug fix: a logic error in takeWord16 is fixed.
    
    1.2.2.1
    * The switch to integer-pure in 1.2.2.0 was apparently mistaken. The
      build flag has been renamed accordingly. Your army of diligent
      maintainers apologizes for the churn.
    * Spec compliance: toCaseFold now follows the Unicode 8.0 spec
      (updated from 7.0)
    * An STG lint error has been fixed

--- devel/hs-text/buildlink3.mk
+++ devel/hs-text/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2016/01/10 11:48:07 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2019/12/31 08:59:52 pho Exp $
 
 BUILDLINK_TREE+=	hs-text
 
 .if !defined(HS_TEXT_BUILDLINK3_MK)
 HS_TEXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=1.2.2
-BUILDLINK_ABI_DEPENDS.hs-text+=	hs-text>=1.2.2.0
+BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=1.2.4
+BUILDLINK_ABI_DEPENDS.hs-text+=	hs-text>=1.2.4.0
 BUILDLINK_PKGSRCDIR.hs-text?=	../../devel/hs-text
 .endif	# HS_TEXT_BUILDLINK3_MK
 
