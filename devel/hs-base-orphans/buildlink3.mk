02dd383d7c3ba1a720547277d7711bc764c3b559
pho <pho@pkgsrc.org>
2019-12-31 17:57:42 +0000

    Import base-orphans-0.8.1
    base-orphans defines orphan instances that mimic instances available
    in later versions of base to a wider (older) range of
    compilers. base-orphans does not export anything except the orphan
    instances themselves and complements base-compat. See the README for
    what instances are covered:
    https://github.com/haskell-compat/base-orphans#readme. See also the
    what is not covered section.

---
+++ devel/hs-base-orphans/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/12/31 17:57:42 pho Exp $
+
+BUILDLINK_TREE+=	hs-base-orphans
+
+.if !defined(HS_BASE_ORPHANS_BUILDLINK3_MK)
+HS_BASE_ORPHANS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.8.1
+BUILDLINK_ABI_DEPENDS.hs-base-orphans+=	hs-base-orphans>=0.8.1
+BUILDLINK_PKGSRCDIR.hs-base-orphans?=	../../devel/hs-base-orphans
+.endif	# HS_BASE_ORPHANS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-base-orphans
