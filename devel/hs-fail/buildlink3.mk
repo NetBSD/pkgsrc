4756c70c0f53ac19bb535f266130ce33d7193218
pho <pho@pkgsrc.org>
2019-12-31 14:24:57 +0000

    Import fail-4.9.0.0
    This package contains the Control.Monad.Fail module providing the
    MonadFail class that became available in base-4.9.0.0 for older base
    package versions.
    
    This package turns into an empty package when used with GHC versions
    which already provide the Control.Monad.Fail module to make way for
    GHC's own Control.Monad.Fail module.

---
+++ devel/hs-fail/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2019/12/31 14:24:57 pho Exp $
+
+BUILDLINK_TREE+=	hs-fail
+
+.if !defined(HS_FAIL_BUILDLINK3_MK)
+HS_FAIL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-fail+=	hs-fail>=4.9.0
+BUILDLINK_ABI_DEPENDS.hs-fail+=	hs-fail>=4.9.0.0
+BUILDLINK_PKGSRCDIR.hs-fail?=	../../devel/hs-fail
+.endif	# HS_FAIL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-fail
