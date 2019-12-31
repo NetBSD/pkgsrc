80a2bc22f0b8860cf685b1bb3f7d311571fbb66d
pho <pho@pkgsrc.org>
2019-12-31 17:33:18 +0000

    Update to attoparsec-0.13.2.3
    0.13.2.1
    * Improved performance of Data.Attoparsec.Text.asciiCI
    
    0.13.2.0
    * pure is now strict in Position
    
    0.13.1.0
    * runScanner now correctly returns the final state
      (https://github.com/bos/attoparsec/issues/105).
    * Parser, ZeptoT, Buffer, and More now expose Semigroup instances.
    * Parser, and ZeptoT now expose MonadFail instances.
    
    0.13.0.2
    * Restore the fast specialised character set implementation for Text
    * Move testsuite from test-framework to tasty
    * Performance optimization of takeWhile and takeWhile1
    
    0.13.0.1
    * Fixed a bug in the implementations of inClass and notInClass for
      Text (https://github.com/bos/attoparsec/issues/103)
    
    0.13.0.0
    * Made the parser type in the Zepto module a monad transformer (needed
      by aeson's string unescaping parser).
    
    0.12.1.6
    * Fixed a case folding bug in the ByteString version of stringCI.
    
    0.12.1.5
    * Fixed an indexing bug in the new Text implementation of string,
      reported by Michel Boucey.
    
    0.12.1.4
    * Fixed a case where the string parser would consume an unnecessary
      amount of input before failing a match, when it could bail much
      earlier (https://github.com/bos/attoparsec/issues/97)
    * Added more context to error messages
      (https://github.com/bos/attoparsec/pull/79)
    
    0.12.1.3
    * Fixed incorrect tracking of Text lengths
      (https://github.com/bos/attoparsec/issues/80)

--- textproc/hs-attoparsec/buildlink3.mk
+++ textproc/hs-attoparsec/buildlink3.mk
@@ -1,16 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.10 2016/01/10 11:49:10 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2019/12/31 17:33:18 pho Exp $
 
 BUILDLINK_TREE+=	hs-attoparsec
 
 .if !defined(HS_ATTOPARSEC_BUILDLINK3_MK)
 HS_ATTOPARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.12.1
-BUILDLINK_ABI_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.12.1.2nb9
+BUILDLINK_API_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.13.2
+BUILDLINK_ABI_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.13.2.3
 BUILDLINK_PKGSRCDIR.hs-attoparsec?=	../../textproc/hs-attoparsec
 
+.include "../../devel/hs-fail/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
 .include "../../devel/hs-text/buildlink3.mk"
+.include "../../devel/hs-transformers/buildlink3.mk"
 .endif	# HS_ATTOPARSEC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-attoparsec
