d030eb83d7ace8931f36b2832b15c88202caead9
pho <pho@pkgsrc.org>
2019-12-31 14:36:26 +0000

    Update to transformers-0.5.6.2
    0.5.6.2 Ross Paterson Feb 2019
    * Further backward compatability fix
    
    0.5.6.1 Ross Paterson Feb 2019
    * Backward compatability fix for MonadFix ListT instance
    
    0.5.6.0 Ross Paterson Feb 2019
    * Generalized type of except
    * Added Control.Monad.Trans.Writer.CPS and Control.Monad.Trans.RWS.CPS
    * Added Contravariant instances
    * Added MonadFix instance for ListT
    
    0.5.5.0 Ross Paterson Oct 2017
    * Added mapSelect and mapSelectT
    * Renamed selectToCont to selectToContT for consistency
    * Defined explicit method definitions to fix space leaks
    * Added missing Semigroup instance to `Constant` functor
    
    0.5.4.0 Ross Paterson Feb 2017
    * Migrate Bifoldable and Bitraversable instances for Constant
    
    0.5.3.1 Ross Paterson Feb 2017
    * Fixed for pre-AMP environments
    
    0.5.3.0 Ross Paterson Feb 2017
    * Added AccumT and SelectT monad transformers
    * Deprecated ListT
    * Added Monad (and related) instances for Reverse
    * Added elimLift and eitherToErrors
    * Added specialized definitions of several methods for efficiency
    * Removed specialized definition of sequenceA for Reverse
    * Backported Eq1/Ord1/Read1/Show1 instances for Proxy
    
    0.5.2.0 Ross Paterson Feb 2016
    * Re-added orphan instances for Either to deprecated module
    * Added lots of INLINE pragmas
    
    0.5.1.0 Ross Paterson Jan 2016
    * Bump minor version number, required by added instances
    
    0.5.0.2 Ross Paterson Jan 2016
    * Backported extra instances for Identity
    
    0.5.0.1 Ross Paterson Jan 2016
    * Tightened GHC bounds for PolyKinds and DeriveDataTypeable
    
    0.5.0.0 Ross Paterson Dec 2015
    * Control.Monad.IO.Class in base for GHC >= 8.0
    * Data.Functor.{Classes,Compose,Product,Sum} in base for GHC >= 8.0
    * Added PolyKinds for GHC >= 7.4
    * Added instances of base classes MonadZip and MonadFail
    * Changed liftings of Prelude classes to use explicit dictionaries
    
    0.4.3.0 Ross Paterson Mar 2015
    * Added Eq1, Ord1, Show1 and Read1 instances for Const

--- devel/hs-transformers/buildlink3.mk
+++ devel/hs-transformers/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/11/23 12:51:34 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2019/12/31 14:36:26 pho Exp $
 
 BUILDLINK_TREE+=	hs-transformers
 
 .if !defined(HS_TRANSFORMERS_BUILDLINK3_MK)
 HS_TRANSFORMERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-transformers+=	hs-transformers>=0.4.2
-BUILDLINK_ABI_DEPENDS.hs-transformers+=	hs-transformers>=0.4.2.0
+BUILDLINK_API_DEPENDS.hs-transformers+=	hs-transformers>=0.5.6
+BUILDLINK_ABI_DEPENDS.hs-transformers+=	hs-transformers>=0.5.6.2
 BUILDLINK_PKGSRCDIR.hs-transformers?=	../../devel/hs-transformers
 .endif	# HS_TRANSFORMERS_BUILDLINK3_MK
 
