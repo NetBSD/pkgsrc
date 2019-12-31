7d784e908176a495942289176be0458e92a1cbd6
pho <pho@pkgsrc.org>
2019-12-31 07:48:16 +0000

    Update to data-default-0.7.1.1
    No changelogs are provided by the upstream.

--- devel/hs-data-default/buildlink3.mk
+++ devel/hs-data-default/buildlink3.mk
@@ -1,16 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.4 2016/01/04 12:24:24 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2019/12/31 07:48:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-data-default
 
 .if !defined(HS_DATA_DEFAULT_BUILDLINK3_MK)
 HS_DATA_DEFAULT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-data-default+=	hs-data-default>=0.5.3
-BUILDLINK_ABI_DEPENDS.hs-data-default+=	hs-data-default>=0.5.3nb3
+BUILDLINK_API_DEPENDS.hs-data-default+=	hs-data-default>=0.7.1
+BUILDLINK_ABI_DEPENDS.hs-data-default+=	hs-data-default>=0.7.1.1
 BUILDLINK_PKGSRCDIR.hs-data-default?=	../../devel/hs-data-default
 
 .include "../../devel/hs-data-default-class/buildlink3.mk"
-.include "../../devel/hs-data-default-instances-base/buildlink3.mk"
 .include "../../devel/hs-data-default-instances-containers/buildlink3.mk"
 .include "../../devel/hs-data-default-instances-dlist/buildlink3.mk"
 .include "../../devel/hs-data-default-instances-old-locale/buildlink3.mk"
