54e977f30a5b5e5b27d6175f4bbd9c402cb6c4a2
pho <pho@pkgsrc.org>
2020-01-01 05:15:20 +0000

    Update to resourcet-1.1.11
    1.1.11
    * runResourceTChecked, which checks if any of the cleanup actions
      threw exceptions and, if so, rethrows them. NOTE This is probably a
      much better choice of function than runResourceT, and in the next
      major version release, will become the new behavior of runResourceT.
    
    1.1.10
    * Added MonadUnliftIO instances and UnliftIO.Resource
    
    1.1.9
    * Add generalized version of resourceForkIO
    
    1.1.8.1
    * Allocation actions should be masked
    
    1.1.8
    * Add instance MonadFix ResourceT #281
    
    1.1.7.5
    * Inline the tutorial from SoH
    
    1.1.7.4
    * Make test suite slightly more robust
    
    1.1.7.3
    * Doc tweak
    
    1.1.7.2
    * Remove upper bound on transformers #249
    
    1.1.7.1
    * transformers-compat 0.5
    
    1.1.7
    * Canonicalise Monad instances #237
    
    1.1.6
    * Safe/Trustworthy for resourcet #220
    
    1.1.5
    * Add pass-through instances for Alternative and MonadPlus #214
    
    1.1.4.1
    * Allow older exceptions version again
    
    1.1.4
    * Add MonadResource ExceptT instance #198
    
    1.1.3.2
    * monad-control-1.0 support #191

--- devel/hs-resourcet/buildlink3.mk
+++ devel/hs-resourcet/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/12/11 12:24:45 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/01/01 05:15:20 pho Exp $
 
 BUILDLINK_TREE+=	hs-resourcet
 
 .if !defined(HS_RESOURCET_BUILDLINK3_MK)
 HS_RESOURCET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=1.1.3
-BUILDLINK_ABI_DEPENDS.hs-resourcet+=	hs-resourcet>=1.1.3.1
+BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=1.1.11
+BUILDLINK_ABI_DEPENDS.hs-resourcet+=	hs-resourcet>=1.1.11
 BUILDLINK_PKGSRCDIR.hs-resourcet?=	../../devel/hs-resourcet
 
 .include "../../devel/hs-exceptions/buildlink3.mk"
@@ -14,8 +14,9 @@ BUILDLINK_PKGSRCDIR.hs-resourcet?=	../../devel/hs-resourcet
 .include "../../devel/hs-mmorph/buildlink3.mk"
 .include "../../devel/hs-monad-control/buildlink3.mk"
 .include "../../devel/hs-mtl/buildlink3.mk"
-.include "../../devel/hs-transformers/buildlink3.mk"
 .include "../../devel/hs-transformers-base/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
+.include "../../devel/hs-unliftio-core/buildlink3.mk"
 .endif	# HS_RESOURCET_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-resourcet
