9061f7333555d1cf438596d45b926c629137a334
adam <adam@pkgsrc.org>
2020-01-01 21:15:06 +0000

    meson: add test target

--- devel/meson/build.mk
+++ devel/meson/build.mk
@@ -1,12 +1,13 @@
-# $NetBSD: build.mk,v 1.3 2019/10/24 09:19:02 tnn Exp $
+# $NetBSD: build.mk,v 1.4 2020/01/01 21:15:06 adam Exp $
 
 BUILD_DEPENDS+=	meson-[0-9]*:../../devel/meson
 
 CONFIGURE_DIRS?=	.
 BUILD_DIRS?=		${CONFIGURE_DIRS}
 INSTALL_DIRS?=		${CONFIGURE_DIRS}
+TEST_DIRS?=		${CONFIGURE_DIRS}
 
-.PHONY: meson-configure meson-build meson-install
+.PHONY: meson-configure meson-build meson-install meson-test
 
 # pkgsrc contains multiple llvm-config executables at multiple locations.
 .if !empty(LLVM_CONFIG_PATH)
@@ -38,6 +39,12 @@ meson-install:
 		${CHMOD} +x ${WRKSRC}/meson_post_install.py;	\
 	fi
 	cd ${WRKSRC} && cd ${d} && ${SETENV} ${INSTALL_ENV} ${MAKE_ENV} ninja -C output install
+.endfor
+
+do-test: meson-test
+meson-test:
+.for d in ${TEST_DIRS}
+	cd ${WRKSRC} && cd ${d} && ${SETENV} ${TEST_ENV} ninja -C output test
 .endfor
 
 .include "../../lang/python/application.mk"
