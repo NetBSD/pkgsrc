a2c5fd451142ff576c639450304ff578337e52be
pho <pho@pkgsrc.org>
2020-01-01 04:51:21 +0000

    Update to monad-control-1.0.2.3
    1.0.2.3
    * Correct spelling mistake. Courtesy of Edward Betts.
    * Support transformers-compat-0.6.
    
    1.0.2.2
    * Added some good documentation. Courtesy of Franz Thoma.
    
    1.0.2.1
    * Refer to Michael Snoyman's excellent tutorial on monad-control.
    
    1.0.2.0
    * Improve documentation by including type equalities in the Haddock
      documentation.
    * Add helpers to define MonadTransControl for stack of two:
      RunDefault2, defaultLiftWith2, defaultRestoreT2
    
    1.0.1.0
    * Added the functions:
    
      liftThrough
        :: (MonadTransControl t, Monad (t m), Monad m)
        => (m (StT t a) -> m (StT t b)) -- ^
        -> t m a -> t m b
    
      captureT :: (MonadTransControl t, Monad (t m), Monad m) => t m (StT t ())
      captureM :: MonadBaseControl b m => m (StM m ())
    
    * Added Travis-CI integration
    
    1.0.0.5
    * Support transformers-0.5 & ransformers-compat-0.5.*.
    
    1.0.0.4
    * Support transformers-compat-0.4.*.
    
    1.0.0.3
    * Unconditionally add ExceptT instances using transformers-compat.
      Courtesy of Adam Bergmark.
    
    1.0.0.2
    * Add a base >= 4.5 constraint because monad-control only builds on
      GHC >= 7.4.
    
    1.0.0.1
    * Use Safe instead of Trustworthy. This requires a dependency on stm.
    
    1.0.0.0
    * Switch the associated data types StT and StM to associated type
      synonyms.
    
      This is an API breaking change. To fix your MonadTransControl or
      MonadBaseControl instances simply remove the StT or StM constructors
      and deconstructors for your monad transformers or monad.
    
    * Add the embed, embed_ and liftBaseOpDiscard functions.
    
    0.3.3.1
    * Unconditionally add ExceptT instances using transformers-compat.
      Courtesy of Adam Bergmark.

--- devel/hs-monad-control/buildlink3.mk
+++ devel/hs-monad-control/buildlink3.mk
@@ -1,16 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/01/07 13:04:03 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/01/01 04:51:21 pho Exp $
 
 BUILDLINK_TREE+=	hs-monad-control
 
 .if !defined(HS_MONAD_CONTROL_BUILDLINK3_MK)
 HS_MONAD_CONTROL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-monad-control+=	hs-monad-control>=0.3.3
-BUILDLINK_ABI_DEPENDS.hs-monad-control+=	hs-monad-control>=0.3.3.0nb2
+BUILDLINK_API_DEPENDS.hs-monad-control+=	hs-monad-control>=1.0.2
+BUILDLINK_ABI_DEPENDS.hs-monad-control+=	hs-monad-control>=1.0.2.3
 BUILDLINK_PKGSRCDIR.hs-monad-control?=		../../devel/hs-monad-control
 
-.include "../../devel/hs-transformers/buildlink3.mk"
+.include "../../devel/hs-stm/buildlink3.mk"
 .include "../../devel/hs-transformers-base/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
 .endif	# HS_MONAD_CONTROL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-monad-control
