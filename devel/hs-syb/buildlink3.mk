743d8f1c7dda003f4f9498fd8051b01ec1cf23a5
pho <pho@pkgsrc.org>
2020-01-01 03:13:39 +0000

    Update to syb-0.7.1
    Define recursive traversals in two parts, non-recursive wrapper and
    recursive local helper to facilitate inlining and avoid passing the
    same argument to all recursive calls

--- devel/hs-syb/buildlink3.mk
+++ devel/hs-syb/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/08/29 14:08:39 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/01/01 03:13:39 pho Exp $
 
 BUILDLINK_TREE+=	hs-syb
 
 .if !defined(HS_SYB_BUILDLINK3_MK)
 HS_SYB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-syb+=	hs-syb>=0.4.2
-BUILDLINK_ABI_DEPENDS.hs-syb+=	hs-syb>=0.4.2
+BUILDLINK_API_DEPENDS.hs-syb+=	hs-syb>=0.7.1
+BUILDLINK_ABI_DEPENDS.hs-syb+=	hs-syb>=0.7.1
 BUILDLINK_PKGSRCDIR.hs-syb?=	../../devel/hs-syb
 .endif	# HS_SYB_BUILDLINK3_MK
 
