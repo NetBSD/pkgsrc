809025a0fda10e2c15a324b77a77cfb8e72110a1
pho <pho@pkgsrc.org>
2020-01-01 02:18:07 +0000

    Update to stm-2.5.0.0
    2.5.0.0 Sep 2018
    * Removed alwaysSucceeds and always, GHC's invariant checking
      primitives. (GHC #14324)
    * Add lengthTBQueue to Control.Concurrent.STM.TBQueue (gh-9)
    * Add stateTVar :: TVar s -> (s -> (a, s)) -> STM a combinator (gh-14)
    * Switched newTBQueue and newTBQueueIO to accept Natural as size
      (gh-17)
    * Switched signalTSemN and newTSem to accept Natural and Integer
      respectively (gh-17)
    
    2.4.5.1 Sep 2018
    * Fix incorrect bookkeeping of write capacity in flushTBQueue (gh-9)
    * Avoid redundant writeTVars in flushTQueue to avoid unncessarily
      invalidating other transactions (gh-6)
    
    2.4.5.0 Feb 2018
    * Fix space leak in TBQueue (gh-2, GHC#14494)
    * Make signalTSem resilient against Int overflows (gh-4)
    * Make definition of readTQueue consistent with readTBQueue (gh-3,
      GHC#9539)
    * Add flushTQueue to Control.Concurrent.STM.TQueue (gh-1)
    * Add flushTBQueue to Control.Concurrent.STM.TBQueue (gh-1)
    * Add signalTSemN operation (gh-5)
    
    2.4.4.1 Dec 2015
    * Add support for base-4.9.0.0
    * Drop support for GHC 6.12 / base-4.2
    
    2.4.4 Dec 2014
    * Add support for base-4.8.0.0
    * Tighten Safe Haskell bounds
    * Add mkWeakTMVar to Control.Concurrent.STM.TMVar
    * Add @since-annotations

--- devel/hs-stm/buildlink3.mk
+++ devel/hs-stm/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:39 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/01/01 02:18:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-stm
 
 .if !defined(HS_STM_BUILDLINK3_MK)
 HS_STM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-stm+=	hs-stm>=2.4.3
-BUILDLINK_ABI_DEPENDS.hs-stm+=	hs-stm>=2.4.3
+BUILDLINK_API_DEPENDS.hs-stm+=	hs-stm>=2.5.0
+BUILDLINK_ABI_DEPENDS.hs-stm+=	hs-stm>=2.5.0.0
 BUILDLINK_PKGSRCDIR.hs-stm?=	../../devel/hs-stm
 .endif	# HS_STM_BUILDLINK3_MK
 
