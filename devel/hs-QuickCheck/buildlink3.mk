f94fe77d45fd7c5c4e5d8085d23659ca2791fe25
pho <pho@pkgsrc.org>
2020-01-01 03:44:06 +0000

    Update to QuickCheck-2.13.2
    Changes:
    http://hackage.haskell.org/package/QuickCheck-2.13.2/changelog

--- devel/hs-QuickCheck/buildlink3.mk
+++ devel/hs-QuickCheck/buildlink3.mk
@@ -1,17 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.4 2015/12/13 14:14:35 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/01/01 03:44:06 pho Exp $
 
 BUILDLINK_TREE+=	hs-QuickCheck
 
 .if !defined(HS_QUICKCHECK_BUILDLINK3_MK)
 HS_QUICKCHECK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.7.6
-BUILDLINK_ABI_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.7.6nb3
+BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.13.2
+BUILDLINK_ABI_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.13.2
 BUILDLINK_PKGSRCDIR.hs-QuickCheck?=	../../devel/hs-QuickCheck
 
 .include "../../devel/hs-random/buildlink3.mk"
-.include "../../devel/hs-tf-random/buildlink3.mk"
-.include "../../devel/hs-transformers/buildlink3.mk"
+.include "../../devel/hs-splitmix/buildlink3.mk"
 .endif	# HS_QUICKCHECK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-QuickCheck
