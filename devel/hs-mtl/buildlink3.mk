39dbf87c3e0fea3fc2c69ff682570fb6c6c3f905
pho <pho@pkgsrc.org>
2020-01-01 02:55:02 +0000

    Update to mtl-2.2.2
    * Control.Monad.Identity now re-exports Control.Monad.Trans.Identity
    * Fix a bug in which Control.Monad.State.Class.modify' was not as
      strict in the new state as its counterparts in transformers
    * Add a MonadError () Maybe instance
    * Add liftEither :: MonadError e m => Either e a -> m a to
      Control.Monad.Except{.Class}
    * Add a MonadWriter w ((,) w) instance (when built against base-4.9 or
      later)

--- devel/hs-mtl/buildlink3.mk
+++ devel/hs-mtl/buildlink3.mk
@@ -1,15 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/11/23 12:52:32 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/01/01 02:55:02 pho Exp $
 
 BUILDLINK_TREE+=	hs-mtl
 
 .if !defined(HS_MTL_BUILDLINK3_MK)
 HS_MTL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mtl+=	hs-mtl>=2.2.1
-BUILDLINK_ABI_DEPENDS.hs-mtl+=	hs-mtl>=2.2.1nb1
+BUILDLINK_API_DEPENDS.hs-mtl+=	hs-mtl>=2.2.2
+BUILDLINK_ABI_DEPENDS.hs-mtl+=	hs-mtl>=2.2.2
 BUILDLINK_PKGSRCDIR.hs-mtl?=	../../devel/hs-mtl
-
-.include "../../devel/hs-transformers/buildlink3.mk"
 .endif	# HS_MTL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-mtl
