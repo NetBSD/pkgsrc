ba277217e598867a0b373b1743d2ae203da06b16
pho <pho@pkgsrc.org>
2020-01-01 02:43:35 +0000

    Update to vector-0.12.0.3
    Changes in version 0.12.0.3
    * Add support for ghc >=8.8 monad fail
    
    Changes in version 0.12.0.2
    * Fixes issue #220, compact heap operations crashing on boxed vectors
      constructed using traverse.
    * remove usage of Data.Primitive.Address and clarify the memset Prim
      Storable smuggling trick in Vector.Storable.Mutable
    * backport injective type family support
    
    Changes in version 0.12.0.1
    * Make sure `length` can be inlined
    * Include modules that test-suites depend on in other-modules
    
    Changes in version 0.12.0.0
    * Documentation fixes/additions
    * New functions: createT, iscanl/r, iterateNM, unfoldrM, uniq
    * New instances for various vector types: Semigroup, MonadZip
    * Made `Storable` vectors respect memory alignment
    * Changed some macros to ConstraintKinds
      * Dropped compatibility with old GHCs to support this
    * Add `Eq1`, `Ord1`, `Show1`, and `Read1` `Vector` instances, and
      related helper functions.
    * Relax context for `Unbox (Complex a)`.

--- devel/hs-vector/buildlink3.mk
+++ devel/hs-vector/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2018/01/07 13:04:04 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/01/01 02:43:35 pho Exp $
 
 BUILDLINK_TREE+=	hs-vector
 
@@ -10,6 +10,8 @@ BUILDLINK_ABI_DEPENDS.hs-vector+=	hs-vector>=0.11.0.0
 BUILDLINK_PKGSRCDIR.hs-vector?=		../../devel/hs-vector
 
 .include "../../devel/hs-primitive/buildlink3.mk"
+.include "../../devel/hs-fail/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_VECTOR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-vector
