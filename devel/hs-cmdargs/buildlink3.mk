210362f34d24db166d2c0bf86b075d05df48b163
pho <pho@pkgsrc.org>
2020-01-01 03:01:16 +0000

    Update to cmdargs-0.10.20
    0.10.20, released 2018-01-22
        #54, use the getopt data types from base
    0.10.19, released 2018-01-01
        #47, ensure Semigroup instance on all GHC versions
    0.10.18, released 2017-09-24
        #47, GHC 8.4 compatibility
    0.10.17, released 2017-03-31
        Add processValueIO for more controlled error messages
        #529, don't include the stack trace in processValue
    0.10.16, released 2017-03-22
        Minor improvement to error messages
    0.10.15, released 2017-03-06
        #43, GHC 8.2 compatibility
    0.10.14, released 2016-02-16
        #39, ensure correct line breaks in HTML help output
        #18, preserve manual \n in help messages
        #25, reformat the README

--- devel/hs-cmdargs/buildlink3.mk
+++ devel/hs-cmdargs/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.6 2015/06/04 13:06:12 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/01/01 03:01:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-cmdargs
 
 .if !defined(HS_CMDARGS_BUILDLINK3_MK)
 HS_CMDARGS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.13
-BUILDLINK_ABI_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.13
+BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.20
+BUILDLINK_ABI_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.20
 BUILDLINK_PKGSRCDIR.hs-cmdargs?=	../../devel/hs-cmdargs
 
-.include "../../devel/hs-transformers/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_CMDARGS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-cmdargs
