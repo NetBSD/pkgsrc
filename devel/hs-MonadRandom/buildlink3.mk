f5b2580a911d9d39097547bd094e1b4e2b907326
pho <pho@pkgsrc.org>
2020-01-01 03:33:07 +0000

    Update to MonadRandom-0.5.1.1
    0.5.1.1 (21 May 2018)
    * Only depend on fail package when GHC < 8.0.
    
    0.5.1 (9 February 2017)
    * Re-export System.Random from Control.Monad.Random.{Lazy,Strict} and
      hence also from Control.Monad.Random.
    * Hackage r1: allow transformers-compat-0.6.x.
    
    0.5 (3 January 2017)
    * This release has quite a few small additions as well as a big module
      reorganization. However, thanks to module re-exports, most existing
      code using the library should continue to work with no changes; the
      major version bump reflects the large reorganization and my
      inability to 100% guarantee that existing user code will not break.
    * The biggest changes that may be of interest to users of the library
      include new lazy vs strict variants of the Rand monad; a new
      MonadInterleave class which is a big improvement over MonadSplit;
      new PrimMonad instances; and new random selection functions like
      weighted, weightedMay, uniformMay, etc.. See the list below for full
      details.
    * Although there was some discussion of generalizing MonadRandom to
      work for a wider range of underlying generators (see #26, #31, and
      comments on this blog post), I decided to punt on that for now. It
      seems rather complicated and there are already good alternatives so
      I decided to keep things simple for this release. I'm still open to
      proposals for generalizing future releases.
    
    Changes in 0.5 include:
    * Refactor to reflect structure of mtl and transformers libraries.
    * Add lazy and strict variants of RandT.
    * Add MonadRandom and MonadSplit instances for ListT.
    * Add (but do not export) unRandT field to RandT.
    * Add MonadCont, MonadError, MonadRWS, PrimMonad, and MonadFail
      instances for RandT.
    * Add evalRandTIO operation.
    * Move fromList and uniform operations to Control.Monad.Random.Class.
    * fromList now raises an error when the total weight of elements is
      zero.
    * Generalize the type of uniform to work over any Foldable.
    * Add new operations weighted, weightedMay, fromListMay, and
      uniformMay. weighted is like fromList but generalized to work over
      any Foldable. The May variants return a Maybe result instead of
      raising an error.
    * New MonadInterleave class for random monads which can interleave
      random generation using split. In some ways this is similar to
      MonadSplit but much more useful.
    * Improved documentation.
    
    0.4.2.3 (21 April 2016)
    * Mark Control.Monad.Random as Trustworthy.
    
    0.4.2.2 (18 January 2016)
    * Allow transformers-0.5.
    
    0.4.2.1 (16 January 2016)
    * Allow transformers-compat-0.5.
    
    0.4.2 (16 January 2016)
    * Add MonadPlus and Alternative instances for RandT.
    
    0.4.1 (20 November 2015)
    * Remove unnecessary Monad m constraint from liftRandT and runRandT.
      This should again technically require a major version bump, but I'm
      not doing it this time in the interest of not being super
      annoying. If this breaks something for you, just yell, and I will
      deprecate this version and do a proper 0.5 release.
    
    0.4 (12 May 2015)
    * Remove unnecessary RandomGen g constraints from liftRandT, liftRand,
      evalRandT, evalRand, runRandT, runRand.
    * A major version bump is required by the PVP since the types of all
      the above methods have changed, but this release is again very
      unlikely to break any client code.
    
    0.3.0.2 (30 March 2015)
    * Add transformers-compat to allow building with newer mtl
    
    0.3.0.1 (24 November 2014)
    * Improve documentation: ranges are exclusive at the upper bound

--- devel/hs-MonadRandom/buildlink3.mk
+++ devel/hs-MonadRandom/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/11/23 12:52:32 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/01/01 03:33:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-MonadRandom
 
@@ -10,8 +10,10 @@ BUILDLINK_ABI_DEPENDS.hs-MonadRandom+=	hs-MonadRandom>=0.3nb1
 BUILDLINK_PKGSRCDIR.hs-MonadRandom?=	../../devel/hs-MonadRandom
 
 .include "../../devel/hs-mtl/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../devel/hs-random/buildlink3.mk"
-.include "../../devel/hs-transformers/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
+.include "../../devel/hs-fail/buildlink3.mk"
 .endif	# HS_MONADRANDOM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-MonadRandom
