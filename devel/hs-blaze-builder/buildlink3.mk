9add8d174f8bdc152e7a2227a98fe579d1c213ad
pho <pho@pkgsrc.org>
2020-01-01 06:44:06 +0000

    Update to blaze-builder-0.4.1.0
    * 0.4.1.0
    - Gain compatibility with the Semigroup/Monoid proposal
    - Add Word8 HTML escaping builders
    - Speed up `fromHtmlEscapedText` and `fromHtmlEscapedLazyText`
    
    * 0.4.0.2
    - Fixed warnings on GHC 7.10, courtesy of Mikhail Glushenkov.
    
    * 0.4.0.1
    - Tightened the version constraints on the bytestring package for GHC
      7.8
    
    * 0.4.0.0
    - This is now a compatibility shim for the new bytestring builder.
      Most of the old internal modules are gone.  See this blog post for
      more information:
    
      <http://blog.melding-monads.com/2015/02/12/announcing-blaze-builder-0-4/>
    
    - The 'Blaze.ByteString.Builder.Html.Utf8.fromHtmlEscaped*' functions
      now strip out any ASCII control characters present in their inputs.
      See <https://github.com/lpsmith/blaze-builder/issues/1> for more
      information.

--- devel/hs-blaze-builder/buildlink3.mk
+++ devel/hs-blaze-builder/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.9 2018/01/07 13:04:03 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/01/01 06:44:06 pho Exp $
 
 BUILDLINK_TREE+=	hs-blaze-builder
 
 .if !defined(HS_BLAZE_BUILDER_BUILDLINK3_MK)
 HS_BLAZE_BUILDER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.3.3
-BUILDLINK_ABI_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.3.3.3nb5
+BUILDLINK_API_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.4.1
+BUILDLINK_ABI_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.4.1.0
 BUILDLINK_PKGSRCDIR.hs-blaze-builder?=		../../devel/hs-blaze-builder
 
 .include "../../devel/hs-text/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_BLAZE_BUILDER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-blaze-builder
