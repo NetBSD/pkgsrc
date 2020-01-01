b9acdd8cdbf7ef0f62a1182e020351a6759fdd4e
pho <pho@pkgsrc.org>
2020-01-01 02:50:57 +0000

    Update to aeson-1.4.6.0
    The list of changes from previous aeson-0.7.0.6 is too long to paste here.
    See http://hackage.haskell.org/package/aeson-1.4.6.0/changelog

--- converters/hs-aeson/buildlink3.mk
+++ converters/hs-aeson/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2018/01/07 13:03:58 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2020/01/01 02:50:57 pho Exp $
 
 BUILDLINK_TREE+=	hs-aeson
 
@@ -10,14 +10,22 @@ BUILDLINK_ABI_DEPENDS.hs-aeson+=	hs-aeson>=0.7.0.6nb16
 BUILDLINK_PKGSRCDIR.hs-aeson?=		../../converters/hs-aeson
 
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../devel/hs-base-compat/buildlink3.mk"
 .include "../../devel/hs-dlist/buildlink3.mk"
 .include "../../devel/hs-hashable/buildlink3.mk"
-.include "../../devel/hs-mtl/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
-.include "../../devel/hs-syb/buildlink3.mk"
+.include "../../devel/hs-tagged/buildlink3.mk"
 .include "../../devel/hs-text/buildlink3.mk"
+.include "../../devel/hs-th-abstraction/buildlink3.mk"
+.include "../../time/hs-time-compat/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-uuid-types/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
+.include "../../math/hs-contravariant/buildlink3.mk"
+.include "../../devel/hs-fail/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
 .endif	# HS_AESON_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-aeson
