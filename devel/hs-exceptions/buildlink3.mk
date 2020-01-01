8847783b3fb9858fcdbfd7c923a4ca66d4215b20
pho <pho@pkgsrc.org>
2020-01-01 04:23:34 +0000

    Update to exceptions-0.10.4
    0.10.4 [2019.12.26]
    * Allow building with template-haskell-2.16.*.
    * Only depend on transformers-compat on old versions of GHC.
    
    0.10.3 [2019.08.27]
    * MonadThrow instance for the strict ST monad.
    
    0.10.2 [2019.05.02]
    * Allow building with base-4.13/template-haskell-2.15.
    
    0.10.1 [2019.03.26]
    * Define a MonadFail instance for CatchT.
    * Allow QuickCheck-2.13 in the test suite.
    
    0.10.0
    * Fix a regression in 0.9.0 whereby the non-IO effects in bracket's
      use action were not visible to the release action, and the non-IO
      effects in the release action were not visible after the bracket
      call.
    * The type of generalBracket was changed in order to restore those
      non-IO effects, so if you are a library author that provides a
      MonadMask instance, you will need to update your implementation of
      this method.
    * Add MonadMask instance for MaybeT
    * Add onError function whose action also runs on errors which are not
      exceptions, such as a Nothing or a Left.
    
    0.9.0
    * Add generalBracket to the MonadMask typeclass, allowing more valid
      instances.
    * Note that functions such as bracket and finally are now based off of
      generalBracket, so if you are a library author that provides a
      MonadMask instance, you will need to provide an implementation of
      this method.
    * Add MonadMask instances for ExceptT and ErrorT
    
    0.8.3
    * MonadCatch and MonadMask instances for Either SomeException
    
    0.8.1
    * Support for throwing in the template-haskell Q monad
    * Support for transformers 0.5
    
    0.8.0.1
    * Resolved warnings on GHC 7.10 and with transformers 0.4.
    
    0.8
    * Use transformers-compat to allow support for ExceptT even on older
      transformers versions.
    
    0.7
    * stm support

--- devel/hs-exceptions/buildlink3.mk
+++ devel/hs-exceptions/buildlink3.mk
@@ -1,16 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/11/23 12:52:32 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/01/01 04:23:34 pho Exp $
 
 BUILDLINK_TREE+=	hs-exceptions
 
 .if !defined(HS_EXCEPTIONS_BUILDLINK3_MK)
 HS_EXCEPTIONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-exceptions+=	hs-exceptions>=0.6.1
-BUILDLINK_ABI_DEPENDS.hs-exceptions+=	hs-exceptions>=0.6.1nb2
+BUILDLINK_API_DEPENDS.hs-exceptions+=	hs-exceptions>=0.10.4
+BUILDLINK_ABI_DEPENDS.hs-exceptions+=	hs-exceptions>=0.10.4
 BUILDLINK_PKGSRCDIR.hs-exceptions?=	../../devel/hs-exceptions
 
 .include "../../devel/hs-mtl/buildlink3.mk"
-.include "../../devel/hs-transformers/buildlink3.mk"
+.include "../../devel/hs-stm/buildlink3.mk"
+.include "../../devel/hs-fail/buildlink3.mk"
 .endif	# HS_EXCEPTIONS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-exceptions
