4d673ba0b0be4db1b08595f6cd92f616fb6c00ed
pho <pho@pkgsrc.org>
2020-01-01 05:24:54 +0000

    Update to temporary-1.3
    1.3
    * Generated directory names are now based on random hex strings rather
      than PIDs.
    * This got a major version bump as a courtesy to users who may depend
      on the specific form of generated names, but that form is not part
      of the API contract and should not be depended upon.
    
    1.2.1.1
    * Improve the docs
    
    1.2.1
    * Limit support to GHC 7.0+
    * Add new functions: writeTempFile, writeSystemTempFile,
      emptyTempFile, emptySystemTempFile
    * Make sure that system* functions return canonicalized paths
    * Modernize the code base, add tests and documentation
    
    1.2.0.4
    * Update maintainership information
    * Fix the docs

--- sysutils/hs-temporary/buildlink3.mk
+++ sysutils/hs-temporary/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/11/23 12:52:33 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/01/01 05:24:54 pho Exp $
 
 BUILDLINK_TREE+=	hs-temporary
 
 .if !defined(HS_TEMPORARY_BUILDLINK3_MK)
 HS_TEMPORARY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-temporary+=	hs-temporary>=1.2.0
-BUILDLINK_ABI_DEPENDS.hs-temporary+=	hs-temporary>=1.2.0.3nb1
+BUILDLINK_API_DEPENDS.hs-temporary+=	hs-temporary>=1.3
+BUILDLINK_ABI_DEPENDS.hs-temporary+=	hs-temporary>=1.3
 BUILDLINK_PKGSRCDIR.hs-temporary?=	../../sysutils/hs-temporary
 
 .include "../../devel/hs-exceptions/buildlink3.mk"
-.include "../../devel/hs-transformers/buildlink3.mk"
+.include "../../devel/hs-random/buildlink3.mk"
 .endif	# HS_TEMPORARY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-temporary
