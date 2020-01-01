c145c9eb7e6c63d9a2373193a2b96952c35d5349
pho <pho@pkgsrc.org>
2020-01-01 04:35:58 +0000

    Update to mmorph-1.1.3
    1.1.3
    * Add an MFunctor instance for ComposeT for GHC >= 8.6
    * Add GHC 8.8 support
    
    1.1.2
    * Conditionally disable Polykinds to support older versions of GHC
    
    1.1.1
    * Increase upper bound on transformers-compat
    
    1.1.0
    BREAKING CHANGE: Enable PolyKinds
      This should in theory be a non-breaking change, but due to a bug in
      GHC 8.0.1 and kind inference ambiguities I'm marking this as a
      breaking change

--- devel/hs-mmorph/buildlink3.mk
+++ devel/hs-mmorph/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/01/07 13:04:03 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/01/01 04:35:58 pho Exp $
 
 BUILDLINK_TREE+=	hs-mmorph
 
 .if !defined(HS_MMORPH_BUILDLINK3_MK)
 HS_MMORPH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.4
-BUILDLINK_ABI_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.4nb1
+BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.1.3
+BUILDLINK_ABI_DEPENDS.hs-mmorph+=	hs-mmorph>=1.1.3
 BUILDLINK_PKGSRCDIR.hs-mmorph?=		../../devel/hs-mmorph
 
-.include "../../devel/hs-transformers/buildlink3.mk"
+.include "../../devel/hs-mtl/buildlink3.mk"
+.include "../../devel/hs-transformers-compat/buildlink3.mk"
 .endif	# HS_MMORPH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-mmorph
