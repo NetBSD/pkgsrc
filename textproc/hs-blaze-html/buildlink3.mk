17a6568a3cf70cddfa80e9b9b552349af828a0ed
pho <pho@pkgsrc.org>
2020-01-02 11:51:27 +0000

    Update to blaze-html-0.9.1.2
    - 0.9.1.2 (2019-10-02)
        * Bump QuickCheck dependency to 2.13
        * Bump containers dependency to 0.6
        * Check in generated code
        * Regenerate .travis.yml using haskell-ci
    
    - 0.9.1.1 (2018-06-12)
        * Fix generated source code on Hackage
    
    - 0.9.1.0
        * Add `u` element to HTML5
        * Add `role` attribute to HTML5
        * Bump HUnit dependency to 1.6
        * Bump QuickCheck dependency to 2.11
    
    - 0.9.0.1
        * Fix CHANGELOG
    
    - 0.9.0.0
        * Bump blaze-html dependency to 0.8.  This has a few consequences:
            - Make `MarkupM` finally adhere to the Monad laws
            - Stricten the `IsString` instance to only work with `MarkupM ()` and
              not `MarkupM a`
            - Change the type of `contents` to `MarkupM a -> MarkupM a`
            - Add a `Semigroup` instance for `MarkupM`
    
    - 0.7.1.1
    
    - 0.8.1.3
        * Bump HUnit dependency to 1.5
    
    - 0.8.1.2
        * Bump QuickCheck dependency to 2.9
    
    - 0.8.1.1
        * Bump HUnit dependency to 1.3
    
    - 0.8.1.0
        * Add `<main>` element to HTML5
    
    - 0.8.0.2
        * Relax blaze-builder dependency to include 0.3 again
    
    - 0.8.0.1
        * Bump QuickCheck dependency to 2.8
    
    - 0.8.0.0
        * Bump blaze-builder dependency to 0.4
    
    - 0.7.1.0
        * Add `itemscope` and `itemprop` attributes
        * Constraint `blaze-markup` dependency

--- textproc/hs-blaze-html/buildlink3.mk
+++ textproc/hs-blaze-html/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2016/01/10 11:49:10 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/01/02 11:51:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-blaze-html
 
 .if !defined(HS_BLAZE_HTML_BUILDLINK3_MK)
 HS_BLAZE_HTML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-blaze-html+=	hs-blaze-html>=0.7.0
-BUILDLINK_ABI_DEPENDS.hs-blaze-html+=	hs-blaze-html>=0.7.0.3nb5
+BUILDLINK_API_DEPENDS.hs-blaze-html+=	hs-blaze-html>=0.9.1
+BUILDLINK_ABI_DEPENDS.hs-blaze-html+=	hs-blaze-html>=0.9.1.2
 BUILDLINK_PKGSRCDIR.hs-blaze-html?=	../../textproc/hs-blaze-html
 
 .include "../../devel/hs-blaze-builder/buildlink3.mk"
