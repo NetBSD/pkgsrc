ac9980a5a744ecd2a3eb8dbac7c34ffd12f8f0b2
pho <pho@pkgsrc.org>
2020-01-02 10:38:57 +0000

    Update to void-0.7.3
    0.7.3 [2019.05.10]
    * Backport the Lift Void instance introduced in
      template-haskell-2.15.0.0.
    
    0.7.2
    * Only depend on deepseq, hashable, and semigroups if using GHC 7.8 or
      earlier.
    * Cleaned up spurious "redundant constraint" warnings on GHC 8+
    
    0.7.1
    * Support semigroups 0.17 on older GHCs
    * Backported NFData's semigroup instance to older GHCs.
    
    0.7
    * adapt to Data.Void being moved into base-4.8
    * vacuousM removed

--- devel/hs-void/buildlink3.mk
+++ devel/hs-void/buildlink3.mk
@@ -1,16 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.9 2016/01/10 11:49:10 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/01/02 10:38:57 pho Exp $
 
 BUILDLINK_TREE+=	hs-void
 
 .if !defined(HS_VOID_BUILDLINK3_MK)
 HS_VOID_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-void+=	hs-void>=0.6.1
-BUILDLINK_ABI_DEPENDS.hs-void+=	hs-void>=0.6.1nb9
+BUILDLINK_API_DEPENDS.hs-void+=	hs-void>=0.7.3
+BUILDLINK_ABI_DEPENDS.hs-void+=	hs-void>=0.7.3
 BUILDLINK_PKGSRCDIR.hs-void?=	../../devel/hs-void
-
-.include "../../devel/hs-hashable/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_VOID_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-void
