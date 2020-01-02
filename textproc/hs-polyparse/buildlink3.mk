eea46afaf9d4a010cc965867e3194308a8e46df9
pho <pho@pkgsrc.org>
2020-01-02 03:21:09 +0000

    Update to polyparse-1.13
    1.13
    * GHC-8.8 compatibility
    * PolyParse has MonadFail as a superclass.
    
    1.12.1
    * GHC-8.6 compatibility
      * MonadFail instances

--- textproc/hs-polyparse/buildlink3.mk
+++ textproc/hs-polyparse/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.8 2016/01/10 11:49:11 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/01/02 03:21:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-polyparse
 
 .if !defined(HS_POLYPARSE_BUILDLINK3_MK)
 HS_POLYPARSE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-polyparse+=	hs-polyparse>=1.11
-BUILDLINK_ABI_DEPENDS.hs-polyparse+=	hs-polyparse>=1.11nb1
+BUILDLINK_API_DEPENDS.hs-polyparse+=	hs-polyparse>=1.13
+BUILDLINK_ABI_DEPENDS.hs-polyparse+=	hs-polyparse>=1.13
 BUILDLINK_PKGSRCDIR.hs-polyparse?=	../../textproc/hs-polyparse
 
+.include "../../devel/hs-fail/buildlink3.mk"
 .include "../../devel/hs-text/buildlink3.mk"
 .endif	# HS_POLYPARSE_BUILDLINK3_MK
 
