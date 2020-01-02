b4fb2fd7be2c1d85ae0a9ac9ca4fb6ef029389b8
pho <pho@pkgsrc.org>
2020-01-02 15:03:19 +0000

    Update to regex-posix-0.96.0.0
    * Update to regex-base-0.94.0.0 API
    * Compatibility with base-4.13.0
    * Remove internal regex C implementation

--- textproc/hs-regex-posix/buildlink3.mk
+++ textproc/hs-regex-posix/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/11/23 12:52:33 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/01/02 15:03:19 pho Exp $
 
 BUILDLINK_TREE+=	hs-regex-posix
 
 .if !defined(HS_REGEX_POSIX_BUILDLINK3_MK)
 HS_REGEX_POSIX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-regex-posix+=	hs-regex-posix>=0.95.2
-BUILDLINK_ABI_DEPENDS.hs-regex-posix+=	hs-regex-posix>=0.95.2nb1
+BUILDLINK_API_DEPENDS.hs-regex-posix+=	hs-regex-posix>=0.96.0
+BUILDLINK_ABI_DEPENDS.hs-regex-posix+=	hs-regex-posix>=0.96.0.0
 BUILDLINK_PKGSRCDIR.hs-regex-posix?=	../../textproc/hs-regex-posix
 
 .include "../../textproc/hs-regex-base/buildlink3.mk"
