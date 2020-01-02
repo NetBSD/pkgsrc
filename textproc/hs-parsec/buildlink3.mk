5a2057a41990721638fd9e3dfb38b04f3ea92e12
pho <pho@pkgsrc.org>
2020-01-02 14:49:43 +0000

    Update to parsec-3.1.14.0
    3.1.14.0
    * Add parseFromFile to Text.Parsec.Text.Lazy and Text.Parsec.Text
      (#103, #104).
    * Clarify Haddock documentation in various places (#105,#101,#102).
    * Add support for base-4.13.
    
    3.1.13.0
    
    * Add official support for SafeHaskell.
    
      NOTE: This is the first version whose SafeHaskell properties have
      become an intentional part of the API contract; previous versions
      were merely accidentally safe-inferred (or not depending on various
      factors; in other words, this was a fragile property). If you rely
      on SafeHaskell to consider module imports from parsec safe, this is
      the first version of parsec which actually guarantees a well-defined
      state; you can declare this requirement by either specifying
    
        build-depends: parsec >= 3.1.13.0 && < 3.2
    
      or, starting with cabal-version:2.0, via
    
        build-depends: parsec ^>= 3.1.13.0
    
    * Drop support for GHC 7.0, GHC 7.2, and GHC 7.4.1; support window
      starts with GHC 7.4.2.
    
    3.1.12.0
    * Support limited to GHC 7.0 & GHC 7.2 only
    * Add MonadFail instance for ParsecT
    * Add Semigroup/Monoid instances for ParsecT (#80,#82)
    * Fix space leak in Applicative/Monad interface (#37)
    * Add parserTrace and parserTraced combinators for debugging.
    
    3.1.11
    * Include README.md in package.
    
    3.1.10
    * Most types now have a Typeable instance. Some instances are dropped
      from older versions of GHC (sorry about that!).
    * The token-parser now rejects Unicode numeric escape sequences for
      characters outside the Unicode range.
    * The token-parser now loses less precision when parsing literal
      doubles.
    * Documentation fixes and corrections.
    * We no longer test parsec builds on GHC 7.4.

--- textproc/hs-parsec/buildlink3.mk
+++ textproc/hs-parsec/buildlink3.mk
@@ -1,16 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.12 2018/01/07 13:04:33 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2020/01/02 14:49:43 pho Exp $
 
 BUILDLINK_TREE+=	hs-parsec
 
 .if !defined(HS_PARSEC_BUILDLINK3_MK)
 HS_PARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-parsec+=	hs-parsec>=3.1.9
-BUILDLINK_ABI_DEPENDS.hs-parsec+=	hs-parsec>=3.1.9nb2
+BUILDLINK_API_DEPENDS.hs-parsec+=	hs-parsec>=3.1.14
+BUILDLINK_ABI_DEPENDS.hs-parsec+=	hs-parsec>=3.1.14.0
 BUILDLINK_PKGSRCDIR.hs-parsec?=		../../textproc/hs-parsec
 
 .include "../../devel/hs-mtl/buildlink3.mk"
 .include "../../devel/hs-text/buildlink3.mk"
+.include "../../devel/hs-fail/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_PARSEC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-parsec
