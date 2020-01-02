92d86c05c80ecb24080fc94b1740b7b861d39934
pho <pho@pkgsrc.org>
2020-01-02 07:19:23 +0000

    Update to safe-0.3.18
    0.3.18, released 2019-12-04
        #27, deprecate maximumDef and friends, fold*1Def
        #27, add maximumBounded and friends
        Stop supporting GHC 7.4 to 7.8
    0.3.17, released 2018-03-09
        Improve the display of errors, less internal callstack
        Add a few missing Partial constraints
    0.3.16, released 2018-01-06
        #22, add Safe index
    0.3.15, released 2017-06-18
        Support QuickCheck 2.10
    0.3.14, released 2017-02-15
        #20, fix for GHC 7.10.1
    0.3.13, released 2017-02-09
        #20, require GHC 7.4 or above
    0.3.12, released 2017-02-05
        #19, add Safe.Partial exposing a Partial typeclass
        #19, add support for GHC call stacks
    0.3.11, released 2017-01-22
        #16, add Safe succ and pred
        #16, add readEitherSafe for better errors than readEither
        #14, add Safe zip3Exact
    0.3.10, released 2016-11-08
        #15, add Safe cycle
    0.3.9, released 2015-05-09
        #9, add Safe toEnum
    0.3.8, released 2014-08-10
        #8, remove unnecessary Ord constraints from Foldable functions

--- devel/hs-safe/buildlink3.mk
+++ devel/hs-safe/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:39 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/01/02 07:19:23 pho Exp $
 
 BUILDLINK_TREE+=	hs-safe
 
 .if !defined(HS_SAFE_BUILDLINK3_MK)
 HS_SAFE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-safe+=	hs-safe>=0.3.7
-BUILDLINK_ABI_DEPENDS.hs-safe+=	hs-safe>=0.3.7
+BUILDLINK_API_DEPENDS.hs-safe+=	hs-safe>=0.3.18
+BUILDLINK_ABI_DEPENDS.hs-safe+=	hs-safe>=0.3.18
 BUILDLINK_PKGSRCDIR.hs-safe?=	../../devel/hs-safe
 .endif	# HS_SAFE_BUILDLINK3_MK
 
