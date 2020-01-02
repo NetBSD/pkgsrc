c0230d3395e5718df5a7eebb21df19da706bd2bc
pho <pho@pkgsrc.org>
2020-01-02 10:28:24 +0000

    Update to vector-algorithms-0.8.0.3
    Version 0.8.0.3 (2019-12-02)
        Fix out-of-bounds access in Timsort.
    
    Version 0.8.0.2 (2019-11-28)
        Bump upper bounds on primitive and QuickCheck.
        Expose 'terminate' function from 'AmericanFlag' module.
        Fix an off-by-one error in Data.Vector.Algorithms.Heaps.heapInsert.

--- devel/hs-vector-algorithms/buildlink3.mk
+++ devel/hs-vector-algorithms/buildlink3.mk
@@ -1,16 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.6 2015/12/13 14:11:25 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/01/02 10:28:24 pho Exp $
 
 BUILDLINK_TREE+=	hs-vector-algorithms
 
 .if !defined(HS_VECTOR_ALGORITHMS_BUILDLINK3_MK)
 HS_VECTOR_ALGORITHMS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vector-algorithms+=	hs-vector-algorithms>=0.7.0
-BUILDLINK_ABI_DEPENDS.hs-vector-algorithms+=	hs-vector-algorithms>=0.7.0.1
+BUILDLINK_API_DEPENDS.hs-vector-algorithms+=	hs-vector-algorithms>=0.8.0
+BUILDLINK_ABI_DEPENDS.hs-vector-algorithms+=	hs-vector-algorithms>=0.8.0.3
 BUILDLINK_PKGSRCDIR.hs-vector-algorithms?=	../../devel/hs-vector-algorithms
 
-.include "../../devel/hs-mtl/buildlink3.mk"
-.include "../../math/hs-mwc-random/buildlink3.mk"
 .include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
 .endif	# HS_VECTOR_ALGORITHMS_BUILDLINK3_MK
