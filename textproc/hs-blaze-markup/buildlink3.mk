52b33db01022e5ab05f86713ab655b44cdbd0f1d
pho <pho@pkgsrc.org>
2020-01-02 11:47:47 +0000

    Update to blaze-markup-0.8.2.3
    - 0.8.2.3 (2019-10-02)
        * Bump `base` to 4.13
        * Bump `tasty` to 1.2
        * Bump `QuickCheck` to 2.13
    
    - 0.8.2.2 (2018-09-25)
        * Bump `base` to 4.12
        * Bump `containers` to 0.6
        * Bump `tasty` to 1.1
    
    - 0.8.2.1 (2018-04-09)
        * Bump `QuickCheck` dependency to allow 2.11
        * Bump `tasty` dependency to allow 1.0
        * Bump `tasty-hunit` dependency to allow 0.10
        * Bump `tasty-quickcheck` dependency to allow 0.10
    
    - 0.8.2.0 (2018-01-09)
        * Define `ToMarkup` instance for `Natural`.
    
    - 0.8.1.0 (2017-09-16)
        * Compatibility with Semigroup/Monoid proposal
        * Switch to `tasty` for running tests
    
    - 0.8.0.0 (2017-01-30)
        * Make `MarkupM` finally adhere to the Monad laws
        * Stricten the `IsString` instance to only work with `MarkupM ()` and not
          `MarkupM a`
        * Change the type of `contents` to `MarkupM a -> MarkupM a`
        * Add a `Semigroup` instance for `MarkupM`
    
    - 0.7.1.1
        * Bump `HUnit` dependency to allow 1.5
    
    - 0.7.1.0
        * Relax `QuickCheck` dependency to allow 2.9
        * Add text builder instances
    
    - 0.7.0.3
        * Relax `HUnit` dependency to allow 1.3
    
    - 0.7.0.2
        * Relax `blaze-builder` dependency to allow 0.3
    
    - 0.7.0.1
        * Bump `QuickCheck` dependency to allow 2.8
    
    - 0.7.0.0
        * Depend on blaze-builder 0.4
    
    - 0.6.3.0
        * Add combinators to insert HTML comments
    
    - 0.6.2.0
        * Add `Applicative` instance for `MarkupM`

--- textproc/hs-blaze-markup/buildlink3.mk
+++ textproc/hs-blaze-markup/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2016/01/10 11:49:10 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/01/02 11:47:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-blaze-markup
 
 .if !defined(HS_BLAZE_MARKUP_BUILDLINK3_MK)
 HS_BLAZE_MARKUP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-blaze-markup+=	hs-blaze-markup>=0.6.1
-BUILDLINK_ABI_DEPENDS.hs-blaze-markup+=	hs-blaze-markup>=0.6.1.1nb5
+BUILDLINK_API_DEPENDS.hs-blaze-markup+=	hs-blaze-markup>=0.8.2
+BUILDLINK_ABI_DEPENDS.hs-blaze-markup+=	hs-blaze-markup>=0.8.2.3
 BUILDLINK_PKGSRCDIR.hs-blaze-markup?=	../../textproc/hs-blaze-markup
 
 .include "../../devel/hs-blaze-builder/buildlink3.mk"
