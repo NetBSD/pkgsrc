7699c86d322cfa6de84c3073e138ed7bc568c3de
pho <pho@pkgsrc.org>
2020-01-02 09:31:47 +0000

    Update to vault-0.3.1.3
    0.3.1.3
        Bump dependencies to allow hashable-1.3.
        Compatibility with GHC-8.8
            Bump dependencies to allow base >= 4.5 && < 4.14
    
    0.3.1.2
        Bump dependencies to allow containers-0.6.
        Compatibility with GHC-8.6
            Bump dependencies to allow base >= 4.5 && < 4.13.
    
    0.3.1.1
        Rename source files to allow building the package with Bazel.
    
    0.3.1.0
        Compatibility with GHC-8.4:
            Bump dependencies to allow base >= 4.5 && < 4.12.
            Add Semigroup instances.
    
    0.3.0.7
        Bump dependencies to allow base >= 4.5 && < 4.11.
    
    0.3.0.6
        Bump dependencies to allow base >= 4.5 && < 4.10.
    
    0.3.0.5
        Add support for GHC type roles extension, using the RoleAnnotations language pragma.
    
    0.3.0.4
        Bump upper version bound for the base dependency.

--- devel/hs-vault/buildlink3.mk
+++ devel/hs-vault/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.11 2018/01/07 13:04:04 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2020/01/02 09:31:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-vault
 
 .if !defined(HS_VAULT_BUILDLINK3_MK)
 HS_VAULT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vault+=	hs-vault>=0.3.0
-BUILDLINK_ABI_DEPENDS.hs-vault+=	hs-vault>=0.3.0.3nb9
+BUILDLINK_API_DEPENDS.hs-vault+=	hs-vault>=0.3.1
+BUILDLINK_ABI_DEPENDS.hs-vault+=	hs-vault>=0.3.1.3
 BUILDLINK_PKGSRCDIR.hs-vault?=		../../devel/hs-vault
 
 .include "../../devel/hs-hashable/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .endif	# HS_VAULT_BUILDLINK3_MK
 
