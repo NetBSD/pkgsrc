262f9dd8d2095489b7bcc62bea43b103be2d3102
pho <pho@pkgsrc.org>
2020-01-02 04:58:07 +0000

    Update to hint-0.7.0
    0.7.0
    * Support for GHC 8.2
    * Support use in a dynamically-linked executable
    * Add normalizeType, like ghci's :kind!
    * Drop support for GHC 7.6
    
    0.6.0
    * Support for GHC 8.0
    * Add displayException to InterpreterError
    
    0.5.2
    * Add runInterpreter variant that takes a GHC libdir at runtime
    * Add missing negated extensions to the Extension type
    * Do not throw GHC warnings as errors
    
    0.5.1
    * Expose unsafeInterpret in Language.Haskell.Interpreter.Unsafe
    
    0.5.0
    * Drop support for GHC 7.4
    * Remove deprecated functions and modules:
      * glasgowExtensions
      * setUseLanguageExtensions
      * setInstalledModsAreInScopeQualified
      * Language.Haskell.Interpreter.GHC
      * Language.Haskell.Interpreter.GHC.Unsafe
    * Drop dependencies on ghc-mtl and extensible-exceptions
    
    0.4.3
    * New maintainer and source code repo

--- devel/hs-hint/buildlink3.mk
+++ devel/hs-hint/buildlink3.mk
@@ -1,17 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.4 2016/01/04 12:23:05 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/01/02 04:58:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-hint
 
 .if !defined(HS_HINT_BUILDLINK3_MK)
 HS_HINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hint+=	hs-hint>=0.4.2
-BUILDLINK_ABI_DEPENDS.hs-hint+=	hs-hint>=0.4.2.3
+BUILDLINK_API_DEPENDS.hs-hint+=	hs-hint>=0.7.0
+BUILDLINK_ABI_DEPENDS.hs-hint+=	hs-hint>=0.7.0
 BUILDLINK_PKGSRCDIR.hs-hint?=	../../devel/hs-hint
 
 .include "../../devel/hs-exceptions/buildlink3.mk"
-.include "../../devel/hs-extensible-exceptions/buildlink3.mk"
-.include "../../devel/hs-ghc-mtl/buildlink3.mk"
 .include "../../devel/hs-ghc-paths/buildlink3.mk"
 .include "../../devel/hs-mtl/buildlink3.mk"
 .include "../../devel/hs-random/buildlink3.mk"
