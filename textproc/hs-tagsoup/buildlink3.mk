6d284999c47997ccd8adc4fd9268ce9f82092fa3
pho <pho@pkgsrc.org>
2020-01-02 15:19:00 +0000

    Update to tagsoup-0.14.8
    0.14.8, released 2019-05-01
        #78, add strMap
    0.14.7, released 2018-09-18
        #75, escape single quote (') characters as &#39;
        #72, update some dead URLs
    0.14.6, released 2018-02-27
        Fix up source positions on bogus <! markers
    0.14.5, released 2018-02-27
        Fix up source positions on bogus <? markers
    0.14.4, released 2018-02-26
        #71, fix up source positions on bogus comments
    0.14.3, released 2018-01-21
        #70, fix up source positions on comments
    0.14.2, released 2017-11-30
        #66, make sure positions are correct for lone & characters
    0.14.1, released 2017-02-25
        #63, add maybeAttrib
    0.14, released 2016-06-11
        #14, eliminate Text.HTML.Download
    0.13.10, released 2016-05-06
        #51, improve the Haddock documentation
        #52, fix some > 16bit HTML entities

--- textproc/hs-tagsoup/buildlink3.mk
+++ textproc/hs-tagsoup/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2016/03/15 13:45:17 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/01/02 15:19:00 pho Exp $
 
 BUILDLINK_TREE+=	hs-tagsoup
 
 .if !defined(HS_TAGSOUP_BUILDLINK3_MK)
 HS_TAGSOUP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.9
-BUILDLINK_ABI_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.9
+BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.14.8
+BUILDLINK_ABI_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.14.8
 BUILDLINK_PKGSRCDIR.hs-tagsoup?=	../../textproc/hs-tagsoup
 
 .include "../../devel/hs-text/buildlink3.mk"
