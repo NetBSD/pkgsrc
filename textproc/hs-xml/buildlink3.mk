e160f46889adf3d679993d26ddc88d6c51423e64
pho <pho@pkgsrc.org>
2020-01-02 14:22:53 +0000

    Update to xml-1.3.14
    No changelogs are provided by the upstream.

--- textproc/hs-xml/buildlink3.mk
+++ textproc/hs-xml/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2016/01/10 11:49:11 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/01/02 14:22:53 pho Exp $
 
 BUILDLINK_TREE+=	hs-xml
 
 .if !defined(HS_XML_BUILDLINK3_MK)
 HS_XML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-xml+=	hs-xml>=1.3.13
-BUILDLINK_ABI_DEPENDS.hs-xml+=	hs-xml>=1.3.13nb7
+BUILDLINK_API_DEPENDS.hs-xml+=	hs-xml>=1.3.14
+BUILDLINK_ABI_DEPENDS.hs-xml+=	hs-xml>=1.3.14
 BUILDLINK_PKGSRCDIR.hs-xml?=	../../textproc/hs-xml
 
 .include "../../devel/hs-text/buildlink3.mk"
