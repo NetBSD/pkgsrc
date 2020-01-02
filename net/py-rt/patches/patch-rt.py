8b4f20be6c9c2d3e2dae935884333593f6e7cd8d
leot <leot@pkgsrc.org>
2020-01-02 15:46:11 +0000

    py-rt: Update to 1.0.12
    Changes:
    1.0.12
    ------
    - Travis CI Docker tests
    - RT 4.4 fixes
    - Support multiline CF values in create_ticket and edit_ticket.
    - Fix support for custom field names containing colons
    - In search(), replace splitlines() with lines array split on \n.
    - Add debug_mode flag for response logging
    - Add platform independent url joining / Allow testing on Windows
    - Add numerical_id to get_ticket result

--- net/py-rt/patches/patch-rt.py
+++ net/py-rt/patches/patch-rt.py
@@ -1,37 +1,13 @@
-$NetBSD: patch-rt.py,v 1.2 2019/06/19 10:51:54 leot Exp $
+$NetBSD: patch-rt.py,v 1.3 2020/01/02 15:46:11 leot Exp $
 
-- Backport upstream commit bb07009c4f62dd2ac393aab82fded6424eead82f
-  to ignore ignore possible empty lines.
-- RT 4 when a ticket is not modified return an empty response, treat it as
-  a successfull operation.
+In RT 4 when a ticket is not modified an empty response is returned.
+Treat it as a successfull operation.
 
---- rt.py.orig	2018-07-16 12:47:27.000000000 +0000
+--- rt.py.orig	2019-10-25 12:58:58.000000000 +0000
 +++ rt.py
-@@ -518,15 +518,19 @@ class Rt:
-             return items
-         elif Format == 's':
-             items = []
--            msgs = msg.splitlines()[2:]
-+            msgs = lines[2:]
-             for msg in msgs:
-+                if "" == msg: # Ignore blank line at the end
-+                   continue
-                 ticket_id, subject = msg.split(': ', 1)
-                 items.append({'id': 'ticket/' + ticket_id, 'Subject': subject})
-             return items
-         elif Format == 'i':
-             items = []
--            msgs = msg.splitlines()[2:]
-+            msgs = lines[2:]
-             for msg in msgs:
-+                if "" == msg: # Ignore blank line at the end
-+                   continue
-                 _, ticket_id = msg.split('/', 1)
-                 items.append({'id': 'ticket/' + ticket_id})
-             return items
-@@ -685,6 +689,8 @@ class Rt:
-             else:
-                 post_data += "CF.{{{}}}: {}\n".format(key[3:], value)
+@@ -736,6 +736,8 @@ class Rt:
+         """
+         post_data = self.__ticket_post_data(kwargs)
          msg = self.__request('ticket/{}/edit'.format(str(ticket_id)), post_data={'content': post_data})
 +        if "" == msg: # Ticket not modified
 +            return True
