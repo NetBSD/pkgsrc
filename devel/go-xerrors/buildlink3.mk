c226466b5203e8ae43f4834f47d9ed57969ff49a
wiz <wiz@pkgsrc.org>
2020-01-02 22:57:40 +0000

    go-xerrors: fix path in buildlink3.mk

--- devel/go-xerrors/buildlink3.mk
+++ devel/go-xerrors/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/01 17:42:12 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/02 22:57:40 wiz Exp $
 
 BUILDLINK_TREE+=	go-xerrors
 
@@ -9,7 +9,7 @@ BUILDLINK_CONTENTS_FILTER.go-xerrors=	${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-xerrors?=	build
 
 BUILDLINK_API_DEPENDS.go-xerrors+=	go-xerrors>=20190719
-BUILDLINK_PKGSRCDIR.go-xerrors?=	../../wip/go-xerrors
+BUILDLINK_PKGSRCDIR.go-xerrors?=	../../devel/go-xerrors
 .endif	# GO_XERRORS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-go-xerrors
