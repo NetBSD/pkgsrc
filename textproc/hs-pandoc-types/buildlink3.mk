544150fa1b33e198ea59bca8d39ddabd89c7a1c6
pho <pho@pkgsrc.org>
2020-01-02 14:41:22 +0000

    Update to pandoc-types-1.17.6.1
    The changelog from 1.12.4.1 is too long to paste here. See
    http://hackage.haskell.org/package/pandoc-types-1.20/changelog

--- textproc/hs-pandoc-types/buildlink3.mk
+++ textproc/hs-pandoc-types/buildlink3.mk
@@ -1,17 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.17 2016/01/10 11:49:11 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2020/01/02 14:41:22 pho Exp $
 
 BUILDLINK_TREE+=	hs-pandoc-types
 
 .if !defined(HS_PANDOC_TYPES_BUILDLINK3_MK)
 HS_PANDOC_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.4
-BUILDLINK_ABI_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.4.1nb16
+BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.17.6
+BUILDLINK_ABI_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.17.6.1
 BUILDLINK_PKGSRCDIR.hs-pandoc-types?=	../../textproc/hs-pandoc-types
 
 .include "../../converters/hs-aeson/buildlink3.mk"
-.include "../../devel/hs-deepseq-generics/buildlink3.mk"
+.include "../../devel/hs-QuickCheck/buildlink3.mk"
 .include "../../devel/hs-syb/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_PANDOC_TYPES_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-pandoc-types
