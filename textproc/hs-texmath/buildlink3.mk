401a2f82685f8b4b22ca14b81fe7226953bc0b3e
pho <pho@pkgsrc.org>
2020-01-02 15:37:08 +0000

    Update to texmath-0.11.3
    Changelog from 0.8 is too long to paste here. See
    http://hackage.haskell.org/package/texmath-0.11.3/changelog

--- textproc/hs-texmath/buildlink3.mk
+++ textproc/hs-texmath/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.19 2016/01/10 11:49:11 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2020/01/02 15:37:09 pho Exp $
 
 BUILDLINK_TREE+=	hs-texmath
 
 .if !defined(HS_TEXMATH_BUILDLINK3_MK)
 HS_TEXMATH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.8
-BUILDLINK_ABI_DEPENDS.hs-texmath+=	hs-texmath>=0.8nb18
+BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.11.3
+BUILDLINK_ABI_DEPENDS.hs-texmath+=	hs-texmath>=0.11.3
 BUILDLINK_PKGSRCDIR.hs-texmath?=	../../textproc/hs-texmath
 
 .include "../../devel/hs-mtl/buildlink3.mk"
