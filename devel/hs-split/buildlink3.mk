f19abee26ea6f825831a74f71143727db45559fc
pho <pho@pkgsrc.org>
2020-01-02 09:11:33 +0000

    Update to split-0.2.3.3
    * 0.2.3.3 (23 Jan 2018)
      - Update for GHC-8.4, test with GHC 7.0-8.4
    
    * 0.2.3.2 (15 May 2017)
      - Move to github.
    
    * 0.2.3.1 (2 May 2016)
      - Fix a test case which was causing occasional spurious test suite
        failures due to too many discarded tests.  Thanks to Doug
        Beardsley and Peter Simons for reporting the issue.
    
    * 0.2.3 (12 January 2016)
      - New function 'divvy' (with associated tests) from Tim Washington.
    
    * 0.2.2r2 (7 Jan 2016)
      - allow base-4.9
    
    * 0.2.2r1 (12 Dec 2014)
      - allow base-4.8

--- devel/hs-split/buildlink3.mk
+++ devel/hs-split/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/01/07 13:04:04 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/01/02 09:11:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-split
 
 .if !defined(HS_SPLIT_BUILDLINK3_MK)
 HS_SPLIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.2.2
-BUILDLINK_ABI_DEPENDS.hs-split+=	hs-split>=0.2.2
+BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.2.3
+BUILDLINK_ABI_DEPENDS.hs-split+=	hs-split>=0.2.3.3
 BUILDLINK_PKGSRCDIR.hs-split?=		../../devel/hs-split
 .endif	# HS_SPLIT_BUILDLINK3_MK
 
