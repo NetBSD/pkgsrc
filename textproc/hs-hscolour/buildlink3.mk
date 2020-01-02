2ac4e9c54fd74b7db58ebadfe2913c8d9abd17e4
pho <pho@pkgsrc.org>
2020-01-02 14:25:33 +0000

    Update to hscolour-1.24.4
    No changelogs are provided by the upstream.

--- textproc/hs-hscolour/buildlink3.mk
+++ textproc/hs-hscolour/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:08:41 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/01/02 14:25:33 pho Exp $
 
 BUILDLINK_TREE+=	hs-hscolour
 
 .if !defined(HS_HSCOLOUR_BUILDLINK3_MK)
 HS_HSCOLOUR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hscolour+=	hs-hscolour>=1.20.3
-BUILDLINK_ABI_DEPENDS.hs-hscolour+=	hs-hscolour>=1.20.3
+BUILDLINK_API_DEPENDS.hs-hscolour+=	hs-hscolour>=1.24.4
+BUILDLINK_ABI_DEPENDS.hs-hscolour+=	hs-hscolour>=1.24.4
 BUILDLINK_PKGSRCDIR.hs-hscolour?=	../../textproc/hs-hscolour
 .endif	# HS_HSCOLOUR_BUILDLINK3_MK
 
