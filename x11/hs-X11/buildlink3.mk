59ce6ed4076bb4bdec73ae8c7bbbeb5a02e55d2c
pho <pho@pkgsrc.org>
2020-01-02 16:48:30 +0000

    Update to X11-1.9.1
    No changelogs are provided by the upstream.

--- x11/hs-X11/buildlink3.mk
+++ x11/hs-X11/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2016/01/04 12:24:25 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/01/02 16:48:30 pho Exp $
 
 BUILDLINK_TREE+=	hs-X11
 
 .if !defined(HS_X11_BUILDLINK3_MK)
 HS_X11_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-X11+=	hs-X11>=1.6.1
-BUILDLINK_ABI_DEPENDS.hs-X11+=	hs-X11>=1.6.1.2nb3
+BUILDLINK_API_DEPENDS.hs-X11+=	hs-X11>=1.9.1
+BUILDLINK_ABI_DEPENDS.hs-X11+=	hs-X11>=1.9.1
 BUILDLINK_PKGSRCDIR.hs-X11?=	../../x11/hs-X11
 
 .include "../../devel/hs-data-default/buildlink3.mk"
