3206219fde28f2471b7f04fa21a736ef0d239ddd
pho <pho@pkgsrc.org>
2020-01-02 11:40:05 +0000

    Add dependency on devel/zlib

--- security/hs-digest/buildlink3.mk
+++ security/hs-digest/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/01/07 13:04:28 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/01/02 11:40:05 pho Exp $
 
 BUILDLINK_TREE+=	hs-digest
 
@@ -8,6 +8,8 @@ HS_DIGEST_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-digest+=	hs-digest>=0.0.1
 BUILDLINK_ABI_DEPENDS.hs-digest+=	hs-digest>=0.0.1.2
 BUILDLINK_PKGSRCDIR.hs-digest?=		../../security/hs-digest
+
+.include "../../devel/zlib/buildlink3.mk"
 .endif	# HS_DIGEST_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-digest
