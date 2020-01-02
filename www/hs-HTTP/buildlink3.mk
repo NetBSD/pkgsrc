0815a7a180d4d94dc6bcbd77174cfd73aaef541c
pho <pho@pkgsrc.org>
2020-01-02 15:50:01 +0000

    Update to HTTP-4000.3.14
    * If the URI contains "user:pass@" part, use it for Basic Authorization
    * Add a test harness.
    * Don't leak a socket when getHostAddr throws an exception.
    * Send cookies in request format, not response format.
    * Moved BrowserAction to be a StateT IO, with instances for
      Applicative, MonadIO, MonadState.
    * Add method to control size of connection pool.
    * Consider both host and port when reusing connections.
    * Handle response code 304 "not modified" properly.
    * Fix digest authentication by fixing md5 output string rep.
    * Make the default user agent string follow the package version.
    * Document lack of HTTPS support and fail when clients try
      to use it instead of silently falling back to HTTP.
    * Add helper to set the request type and body.

--- www/hs-HTTP/buildlink3.mk
+++ www/hs-HTTP/buildlink3.mk
@@ -1,17 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.11 2016/01/10 11:49:11 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2020/01/02 15:50:01 pho Exp $
 
 BUILDLINK_TREE+=	hs-HTTP
 
 .if !defined(HS_HTTP_BUILDLINK3_MK)
 HS_HTTP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.17
-BUILDLINK_ABI_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.17nb10
+BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.3.14
+BUILDLINK_ABI_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.3.14
 BUILDLINK_PKGSRCDIR.hs-HTTP?=	../../www/hs-HTTP
 
+.include "../../textproc/hs-parsec/buildlink3.mk"
 .include "../../devel/hs-mtl/buildlink3.mk"
 .include "../../net/hs-network/buildlink3.mk"
-.include "../../textproc/hs-parsec/buildlink3.mk"
+.include "../../net/hs-network-uri/buildlink3.mk"
 .endif	# HS_HTTP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-HTTP
