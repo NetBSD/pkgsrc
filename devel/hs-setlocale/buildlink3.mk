48803c6ca5231143da466b1ed43cb5dffd32bfc0
pho <pho@pkgsrc.org>
2020-01-02 09:07:04 +0000

    Update to setlocale-1.0.0.9
    No changelogs provided by the upstream.

--- devel/hs-setlocale/buildlink3.mk
+++ devel/hs-setlocale/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/01/04 12:25:08 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/02 09:07:04 pho Exp $
 
 BUILDLINK_TREE+=	hs-setlocale
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-setlocale
 HS_SETLOCALE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-setlocale+=	hs-setlocale>=1.0.0
-BUILDLINK_ABI_DEPENDS.hs-setlocale+=	hs-setlocale>=1.0.0.3
+BUILDLINK_ABI_DEPENDS.hs-setlocale+=	hs-setlocale>=1.0.0.9
 BUILDLINK_PKGSRCDIR.hs-setlocale?=	../../devel/hs-setlocale
 .endif	# HS_SETLOCALE_BUILDLINK3_MK
 
