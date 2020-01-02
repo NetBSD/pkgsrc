12418d027432ff14467d236aefbe7b899478d4bb
pho <pho@pkgsrc.org>
2020-01-02 14:56:10 +0000

    Update to regex-base-0.94.0.0
    * Breaking change: Switch RegExp API from the previously used
      Monad(fail) to MonadFail(fail) to denote matching failures
    * Define Extract Text instances for strict and lazy Text types
    * Compatibility with base-4.13.0
    * Explicitly declare all modules Safe under SafeHaskell for GHC 7.4
      and higher

--- textproc/hs-regex-base/buildlink3.mk
+++ textproc/hs-regex-base/buildlink3.mk
@@ -1,15 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/11/23 12:52:33 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/01/02 14:56:10 pho Exp $
 
 BUILDLINK_TREE+=	hs-regex-base
 
 .if !defined(HS_REGEX_BASE_BUILDLINK3_MK)
 HS_REGEX_BASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-regex-base+=	hs-regex-base>=0.93.2
-BUILDLINK_ABI_DEPENDS.hs-regex-base+=	hs-regex-base>=0.93.2nb1
+BUILDLINK_API_DEPENDS.hs-regex-base+=	hs-regex-base>=0.94.0
+BUILDLINK_ABI_DEPENDS.hs-regex-base+=	hs-regex-base>=0.94.0.0
 BUILDLINK_PKGSRCDIR.hs-regex-base?=	../../textproc/hs-regex-base
 
 .include "../../devel/hs-mtl/buildlink3.mk"
+.include "../../devel/hs-text/buildlink3.mk"
+.include "../../devel/hs-fail/buildlink3.mk"
 .endif	# HS_REGEX_BASE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-regex-base
