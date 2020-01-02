2724b08396f0150236473024753a84933b6341dd
pho <pho@pkgsrc.org>
2020-01-02 07:16:27 +0000

    Update to reflection-2.1.5
    2.1.5 [2019.08.27]
    * Fix a bug in which reifyNat would yield incorrect results for very
      large Integers on GHC 8.2 or later.
    
    2.1.4 [2018.07.02]
    * Allow building with GHC 8.6.
    
    2.1.3 [2018.01.18]
    * Add Semigroup instance for ReflectedMonoid.
    
    2.1.2
    * Support cross-compilation and unregistered GHC builds.
    
    2.1.1.1
    * Fixed typos in the documentation.
    
    2.1.1
    * Fixed support for GHC 7.0
    
    2.1
    * Added ReifiedMonoid and ReifiedApplicative.
    
    2
    * Added reifyNat and reifySymbol for GHC 7.8+, capable of reflecting
      into the KnownNat and KnownSymbol classes respectively for use with
      other APIs.
    * Back-ported reifyTypeable from lens. This enables us to perform a
      (less efficient) form of Typeable reflection.
    
    1.5.2
    * Renamed the flag for disabling the use of template-haskell, to
      -f-template-haskell for consistency with my other packages.
    
    1.5.1.2
    * Builds warning-free on GHC 7.10.
    * Added a dynamic FromJSON example.
    
    1.5.1.1
    * Updated the link to the paper.
    * More examples.

--- devel/hs-reflection/buildlink3.mk
+++ devel/hs-reflection/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/11/07 13:07:56 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/02 07:16:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-reflection
 
 .if !defined(HS_REFLECTION_BUILDLINK3_MK)
 HS_REFLECTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-reflection+=	hs-reflection>=1.5.1
-BUILDLINK_ABI_DEPENDS.hs-reflection+=	hs-reflection>=1.5.1
+BUILDLINK_API_DEPENDS.hs-reflection+=	hs-reflection>=2.1.5
+BUILDLINK_ABI_DEPENDS.hs-reflection+=	hs-reflection>=2.1.5
 BUILDLINK_PKGSRCDIR.hs-reflection?=	../../devel/hs-reflection
 
-.include "../../devel/hs-tagged/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_REFLECTION_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-reflection
