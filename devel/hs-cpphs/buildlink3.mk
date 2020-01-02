f352500cee1af8575bf3d7e16188e336b5d1e94d
pho <pho@pkgsrc.org>
2020-01-02 03:27:12 +0000

    Update to cpphs-1.20.8
    Version 1.20
    ------------
      * bugfixes for #if defined(FOO) && FOO(a,b)
      * (1.20.1): fix version number
      * (1.20.2): ensure all input/output is UTF8, regardless of locale
      * (1.20.3): detect an absolute windows path with a drive letter in a #include
      * (1.20.4): more windows path handling
      * (1.20.5): revert change in 1.20.4
      * (1.20.6): minor bugfix for crash in obscure corner case
      * (1.20.7): bugfix for windows drive letter in #include
      * (1.20.8): another bugfix for windows drive letter in #include
    
    Version 1.19
    ------------
      * (1.19.3): bugfix for hlint ticket #161 - interaction of --unlit/--linepragma

--- devel/hs-cpphs/buildlink3.mk
+++ devel/hs-cpphs/buildlink3.mk
@@ -1,14 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.11 2018/01/07 13:04:03 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2020/01/02 03:27:12 pho Exp $
 
 BUILDLINK_TREE+=	hs-cpphs
 
 .if !defined(HS_CPPHS_BUILDLINK3_MK)
 HS_CPPHS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.19.2
-BUILDLINK_ABI_DEPENDS.hs-cpphs+=	hs-cpphs>=1.19.2nb1
+BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.20.8
+BUILDLINK_ABI_DEPENDS.hs-cpphs+=	hs-cpphs>=1.20.8
 BUILDLINK_PKGSRCDIR.hs-cpphs?=		../../devel/hs-cpphs
 
+.include "../../devel/hs-old-locale/buildlink3.mk"
+.include "../../time/hs-old-time/buildlink3.mk"
 .include "../../textproc/hs-polyparse/buildlink3.mk"
 .endif	# HS_CPPHS_BUILDLINK3_MK
 
