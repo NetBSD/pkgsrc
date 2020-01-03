fb392b132855891c5fcf9a651d1063440ab94031
tsutsui <tsutsui@pkgsrc.org>
2020-01-03 02:05:23 +0000

    tremor: tremor's vorbisidec.pc explicitly requires libogg.

--- audio/tremor/buildlink3.mk
+++ audio/tremor/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:23:59 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/01/03 02:05:23 tsutsui Exp $
 
 BUILDLINK_TREE+=	tremor
 
@@ -9,5 +9,7 @@ BUILDLINK_API_DEPENDS.tremor+=	tremor>=1.0.2
 BUILDLINK_ABI_DEPENDS.tremor+=	tremor>=1.0.2nb5
 BUILDLINK_PKGSRCDIR.tremor?=	../../audio/tremor
 .endif # TREMOR_BUILDLINK3_MK
+
+.include "../../multimedia/libogg/buildlink3.mk"
 
 BUILDLINK_TREE+=	-tremor
