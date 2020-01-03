04e2b804340c59bde2b318cdc738e7c175f8d9e1
pho <pho@pkgsrc.org>
2020-01-03 03:59:19 +0000

    Update to case-insensitive-1.2.1.0
    1.2.1.0
    * Add traverse
    
    1.2.0.11
    * Dropped upper version bounds of dependencies because it's to much
      work to maintain.
    
    1.2.0.10
    * Support criterion-1.2
    
    1.2.0.9
    * Support base-4.10
    
    1.2.0.8
    * Support HUnit-1.5.
    
    1.2.0.7
    * Fix build of benchmarks when getting the sources from Hackage.
    
    1.2.0.6
    * Add Travis CI support
    * Add Semigroup s => Semigroup (CI s) instance
    
    1.2.0.5
    * Support HUnit-1.3
    * Support criterion-1.1
    * Updated .nix files
    
    1.2.0.4
    * Fixed #17: https://github.com/basvandijk/case-insensitive/issues/17
    
    1.2.0.3
    * Add CHANGELOG to extra-source files so that it shows up on Hackage
    
    1.2.0.2
    * Bump base and deepseq upper bounds for GHC 7.10 compatibility
    * Add case-insensitive.nix and default.nix
    * Fix #14: Build failure with GHC 7.2.2
    * Fix #8: Don't unnecessarily use NoImplicitPrelude

--- textproc/hs-case-insensitive/buildlink3.mk
+++ textproc/hs-case-insensitive/buildlink3.mk
@@ -1,16 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.8 2016/01/10 11:49:10 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/01/03 03:59:19 pho Exp $
 
 BUILDLINK_TREE+=	hs-case-insensitive
 
 .if !defined(HS_CASE_INSENSITIVE_BUILDLINK3_MK)
 HS_CASE_INSENSITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.2.0
-BUILDLINK_ABI_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.2.0.1nb6
+BUILDLINK_API_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.2.1
+BUILDLINK_ABI_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.2.1.0
 BUILDLINK_PKGSRCDIR.hs-case-insensitive?=	../../textproc/hs-case-insensitive
 
 .include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../devel/hs-text/buildlink3.mk"
+.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_CASE_INSENSITIVE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-case-insensitive
