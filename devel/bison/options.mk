d6f6014c66cbb191016c86abbba65f8f8d02477f
jperkin <jperkin@pkgsrc.org>
2020-01-03 19:23:27 +0000

    bison: gettext-tools is required with nls for libtextstyle.

--- devel/bison/options.mk
+++ devel/bison/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2019/10/25 12:32:54 triaxx Exp $
+# $NetBSD: options.mk,v 1.4 2020/01/03 19:23:27 jperkin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.bison
 PKG_SUPPORTED_OPTIONS=	nls
@@ -17,6 +17,7 @@ USE_TOOLS+=		msgfmt
 CONFIGURE_ARGS+=	--enable-nls
 CONFIGURE_ENV+=		gt_cv_func_gnugettext1_libintl=yes
 .include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/gettext-tools/buildlink3.mk"
 PLIST.nls=		yes
 .else
 CONFIGURE_ARGS+=	--disable-nls
