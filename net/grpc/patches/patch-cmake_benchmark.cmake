7e9f3799a3bbc6070509281bd32f6cca2a3027f9
adam <adam@pkgsrc.org>
2020-01-03 08:21:36 +0000

    grpc: updated to 1.26.0
    Release v1.26.0
    This release contains refinements, improvements, and bug fixes, with highlights listed below.
    
    Core
    Fix compression filter crash on empty payload.
    Ensure awake pollset_work threads exist on Windows.
    Disable client_idle_filter.
    Remove gpr_get/set_allocation_functions.
    Security audit response.
    
    C++
    Automatically disable testing frameworks if gRPC_BUILD_TESTS=OFF.
    Do not build channelz when gRPC_USE_PROTO_LITE.
    Add options for all codegen plugins.
    gRPC-C++ podspec follows gRPC versioning.
    Issue 19208: Fix pollset_set_del_fd to cleanup all fd references.
    De-duplicate .proto file processing.
    cmake: Add VERSION and SOVERSION properties to libraries.
    
    Python
    Release Python3.8 wheels for Windows.
    Release Python3.8 wheel on macOS.
    Fix issue with exception being out of scope in Python 3.
    [AIO] Implement the shutdown process for AIO server and completion queue.
    Attempt to drop support for Python 3.4.
    AIO Unified call interface.
    Make sure Core aware of gevent Cython objects.
    [bazel] Add an ability to call an optional custom plugin for py_proto_library and py_grpc_library.

--- net/grpc/patches/patch-cmake_benchmark.cmake
+++ net/grpc/patches/patch-cmake_benchmark.cmake
@@ -1,13 +1,13 @@
-$NetBSD: patch-cmake_benchmark.cmake,v 1.1 2018/04/27 17:47:27 minskim Exp $
+$NetBSD: patch-cmake_benchmark.cmake,v 1.2 2020/01/03 08:21:36 adam Exp $
 
 Find benchmark from pkgsrc.
 
---- cmake/benchmark.cmake.orig	2018-04-13 18:08:11.000000000 +0000
+--- cmake/benchmark.cmake.orig	2019-12-18 01:29:27.000000000 +0000
 +++ cmake/benchmark.cmake
-@@ -26,8 +26,12 @@ if("${gRPC_BENCHMARK_PROVIDER}" STREQUAL
-       message(WARNING "gRPC_BENCHMARK_PROVIDER is \"module\" but BENCHMARK_ROOT_DIR is wrong")
+@@ -26,8 +26,12 @@ if(gRPC_BENCHMARK_PROVIDER STREQUAL "mod
+     message(WARNING "gRPC_BENCHMARK_PROVIDER is \"module\" but BENCHMARK_ROOT_DIR is wrong")
    endif()
- elseif("${gRPC_BENCHMARK_PROVIDER}" STREQUAL "package")
+ elseif(gRPC_BENCHMARK_PROVIDER STREQUAL "package")
 -  # Use "CONFIG" as there is no built-in cmake module for benchmark.
 -  find_package(benchmark REQUIRED CONFIG)
 +  find_path(BENCHMARK_INCLUDE_DIR benchmark/benchmark.h)
@@ -18,4 +18,4 @@ Find benchmark from pkgsrc.
 +    IMPORTED_LOCATION "${BENCHMARK_LIB}")
    if(TARGET benchmark::benchmark)
      set(_gRPC_BENCHMARK_LIBRARIES benchmark::benchmark)
-     # extract the include dir from target's properties
+   endif()
