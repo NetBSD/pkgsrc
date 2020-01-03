04ba252012ad5c137b30397518da0471b8ebd13d
pho <pho@pkgsrc.org>
2020-01-03 06:48:10 +0000

    Import unix-compat-0.5.2 from wip
    This package provides portable implementations of parts of the unix
    package. This package re-exports the unix package when available. When
    it isn't available, portable implementations are used.

---
+++ devel/hs-unix-compat/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/03 06:48:10 pho Exp $
+
+BUILDLINK_TREE+=	hs-unix-compat
+
+.if !defined(HS_UNIX_COMPAT_BUILDLINK3_MK)
+HS_UNIX_COMPAT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.5.2
+BUILDLINK_ABI_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.5.2
+BUILDLINK_PKGSRCDIR.hs-unix-compat?=	../../devel/hs-unix-compat
+.endif	# HS_UNIX_COMPAT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-unix-compat
