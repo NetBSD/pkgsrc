80afc8d161421086ec30b86c9d25e942e2265987
nia <nia@pkgsrc.org>
2020-01-04 01:53:55 +0000

    MesaLib: probably makes more sense to check wayland here

--- graphics/MesaLib/options.mk
+++ graphics/MesaLib/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.78 2020/01/04 01:50:32 nia Exp $
+# $NetBSD: options.mk,v 1.79 2020/01/04 01:53:55 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.MesaLib
 
@@ -8,15 +8,13 @@ PKG_SUPPORTED_OPTIONS+=		llvm vulkan x11
 
 .if ${MESALIB_SUPPORTS_DRI} == "yes"
 PKG_SUPPORTED_OPTIONS+=		wayland
+.  include "../../devel/wayland/platform.mk"
+.  if ${PLATFORM_SUPPORTS_WAYLAND} == "yes"
+PKG_SUGGESTED_OPTIONS+=		wayland
+.  endif
 .endif
 
 PKG_SUGGESTED_OPTIONS+=		x11
-
-.include "../../devel/wayland/platform.mk"
-
-.if ${PLATFORM_SUPPORTS_WAYLAND} == "yes"
-PKG_SUGGESTED_OPTIONS+=		wayland
-.endif
 
 # The LLVM option enables JIT accelerated software rendering and is also
 # required to support the latest RADEON GPUs.
