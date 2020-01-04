dbbd5cc99c0678105e34a58d2c423959e621cfd5
wiz <wiz@pkgsrc.org>
2020-01-04 20:28:08 +0000

    mercurial: remove outdated comments

--- devel/py-mercurial/version.mk
+++ devel/py-mercurial/version.mk
@@ -1,6 +1,3 @@
-# $NetBSD: version.mk,v 1.8 2020/01/04 20:21:15 wiz Exp $
+# $NetBSD: version.mk,v 1.9 2020/01/04 20:28:08 wiz Exp $
 
 VERSION=	5.2.1
-
-# see https://www.mercurial-scm.org/wiki/Python3
-# set HGPYTHON3=1 in the environment during install
