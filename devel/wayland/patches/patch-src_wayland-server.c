9eb1b9c46a738d4cbd1c7ca29479b7918ba45bec
nia <nia@pkgsrc.org>
2020-01-05 19:30:48 +0000

    wayland: Simplify patches.
    Bump PKGREVISION

--- devel/wayland/patches/patch-src_wayland-server.c
+++ devel/wayland/patches/patch-src_wayland-server.c
@@ -1,22 +1,15 @@
-$NetBSD: patch-src_wayland-server.c,v 1.1 2019/08/18 16:05:12 nia Exp $
+$NetBSD: patch-src_wayland-server.c,v 1.2 2020/01/05 19:30:48 nia Exp $
 
-BSD support from FreeBSD
+NetBSD support
 
 --- src/wayland-server.c.orig	2019-03-21 00:55:25.000000000 +0000
 +++ src/wayland-server.c
-@@ -25,6 +25,8 @@
- 
- #define _GNU_SOURCE
- 
-+#include "../config.h"
-+
- #include <stdlib.h>
- #include <stdint.h>
- #include <stddef.h>
-@@ -43,6 +45,11 @@
+@@ -43,6 +43,13 @@
  #include <sys/file.h>
  #include <sys/stat.h>
  
++#include "../config.h"
++
 +#ifdef HAVE_SYS_UCRED_H
 +#include <sys/types.h>
 +#include <sys/ucred.h>
@@ -31,10 +24,10 @@ BSD support from FreeBSD
  	struct wl_priv_signal destroy_signal;
 +#ifdef HAVE_SYS_UCRED_H
 +#ifdef __NetBSD__
-+	struct uucred xucred;
++	struct uucred ucred;
 +#else
 +	/* FreeBSD */
-+	struct xucred xucred;
++	struct xucred ucred;
 +#endif
 +#else
 +	/* Linux */
@@ -43,49 +36,42 @@ BSD support from FreeBSD
  	int error;
  	struct wl_priv_signal resource_created_signal;
  };
-@@ -312,7 +329,8 @@ wl_resource_post_error(struct wl_resourc
+@@ -312,7 +329,11 @@ wl_resource_post_error(struct wl_resourc
  static void
  destroy_client_with_error(struct wl_client *client, const char *reason)
  {
--	wl_log("%s (pid %u)\n", reason, client->ucred.pid);
++#ifdef __linux__
+ 	wl_log("%s (pid %u)\n", reason, client->ucred.pid);
++#else
 +	wl_log("Destroying with error: %s\n", reason);
-+	//wl_log("%s (pid %u)\n", reason, client->ucred.pid);
++#endif
  	wl_client_destroy(client);
  }
  
-@@ -526,10 +544,20 @@ wl_client_create(struct wl_display *disp
- 	if (!client->source)
+@@ -527,9 +548,11 @@ wl_client_create(struct wl_display *disp
  		goto err_client;
  
-+#if defined(SO_PEERCRED)
-+	/* Linux */
  	len = sizeof client->ucred;
++#ifdef SO_PEERCRED
  	if (getsockopt(fd, SOL_SOCKET, SO_PEERCRED,
  		       &client->ucred, &len) < 0)
  		goto err_source;
-+#elif defined(LOCAL_PEERCRED)
-+	/* FreeBSD */
-+	len = sizeof client->xucred;
-+	if (getsockopt(fd, SOL_SOCKET, LOCAL_PEERCRED,
-+		       &client->xucred, &len) < 0 ||
-+		       client->xucred.cr_version != XUCRED_VERSION)
-+		goto err_source;
 +#endif
  
  	client->connection = wl_connection_create(fd);
  	if (client->connection == NULL)
-@@ -583,12 +611,23 @@ WL_EXPORT void
+@@ -583,12 +606,23 @@ WL_EXPORT void
  wl_client_get_credentials(struct wl_client *client,
  			  pid_t *pid, uid_t *uid, gid_t *gid)
  {
 +#ifdef HAVE_SYS_UCRED_H
-+	/* FreeBSD */
++	/* BSD */
 +	if (pid)
-+		*pid = 0; /* FIXME: not defined on FreeBSD */
++		*pid = 0; /* FIXME: should be set*/
 +	if (uid)
-+		*uid = client->xucred.cr_uid;
++		*uid = client->ucred.cr_uid;
 +	if (gid)
-+		*gid = client->xucred.cr_gid;
++		*gid = client->ucred.cr_gid;
 +#else
 +	/* Linux */
  	if (pid)
