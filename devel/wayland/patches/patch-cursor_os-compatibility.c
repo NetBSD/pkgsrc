ed0f31bcad23235d4bc9021db7160c8a3838a7d9
nia <nia@pkgsrc.org>
2020-01-05 19:30:48 +0000

    wayland: Simplify patches.
    Bump PKGREVISION

--- devel/wayland/patches/patch-cursor_os-compatibility.c
+++ devel/wayland/patches/patch-cursor_os-compatibility.c
@@ -1,7 +1,7 @@
-$NetBSD: patch-cursor_os-compatibility.c,v 1.1 2019/08/19 12:50:23 nia Exp $
+$NetBSD: patch-cursor_os-compatibility.c,v 1.2 2020/01/05 19:30:48 nia Exp $
 
-fallocate is currently a no-op on NetBSD.
-Make sure ftruncate is used instead.
+NetBSD support: no filesystems currently support posix_fallocate,
+so it always fails
 
 --- cursor/os-compatibility.c.orig	2019-03-21 00:55:25.000000000 +0000
 +++ cursor/os-compatibility.c
