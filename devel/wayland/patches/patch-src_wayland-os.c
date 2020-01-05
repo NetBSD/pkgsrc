1cd7ec8f99f4c47f1997243f24bae3cce25a933b
nia <nia@pkgsrc.org>
2020-01-05 19:30:48 +0000

    wayland: Simplify patches.
    Bump PKGREVISION

--- devel/wayland/patches/patch-src_wayland-os.c
+++ devel/wayland/patches/patch-src_wayland-os.c
@@ -1,96 +1,29 @@
-$NetBSD: patch-src_wayland-os.c,v 1.2 2019/08/28 10:35:40 nia Exp $
-
-BSD support from FreeBSD
+$NetBSD: patch-src_wayland-os.c,v 1.3 2020/01/05 19:30:48 nia Exp $
 
 https://lists.freedesktop.org/archives/wayland-devel/2019-February/040024.html
 
 --- src/wayland-os.c.orig	2019-03-21 00:55:25.000000000 +0000
 +++ src/wayland-os.c
-@@ -25,14 +25,20 @@
- 
- #define _GNU_SOURCE
- 
-+#include "../config.h"
-+
- #include <sys/types.h>
- #include <sys/socket.h>
+@@ -30,9 +30,17 @@
  #include <unistd.h>
  #include <fcntl.h>
  #include <errno.h>
+-#include <sys/epoll.h>
+ 
+ #include "../config.h"
++
 +#ifdef HAVE_SYS_EPOLL_H
- #include <sys/epoll.h>
++#include <sys/epoll.h>
 +#endif
++
 +#ifdef HAVE_SYS_EVENT_H
 +#include <sys/event.h>
 +#endif
- 
--#include "../config.h"
++
  #include "wayland-os.h"
  
  static int
-@@ -62,26 +68,50 @@ wl_os_socket_cloexec(int domain, int typ
- {
- 	int fd;
- 
-+#ifdef SOCK_CLOEXEC
- 	fd = socket(domain, type | SOCK_CLOEXEC, protocol);
- 	if (fd >= 0)
- 		return fd;
- 	if (errno != EINVAL)
- 		return -1;
-+#endif
- 
- 	fd = socket(domain, type, protocol);
- 	return set_cloexec_or_close(fd);
- }
- 
- int
-+wl_os_socketpair_cloexec(int domain, int type, int protocol, int sv[2])
-+{
-+       int retval;
-+
-+#ifdef SOCK_CLOEXEC
-+       retval = socketpair(domain, type | SOCK_CLOEXEC, protocol, sv);
-+       if (retval >= 0)
-+               return retval;
-+       if (errno != EINVAL)
-+               return -1;
-+#endif
-+
-+       retval = socketpair(domain, type, protocol, sv);
-+       if (set_cloexec_or_close(sv[0]) < 0 || set_cloexec_or_close(sv[1]) < 0)
-+               retval = -1;
-+
-+       return retval;
-+}
-+
-+int
- wl_os_dupfd_cloexec(int fd, long minfd)
- {
- 	int newfd;
- 
-+#ifdef F_DUPFD_CLOEXEC
- 	newfd = fcntl(fd, F_DUPFD_CLOEXEC, minfd);
- 	if (newfd >= 0)
- 		return newfd;
- 	if (errno != EINVAL)
- 		return -1;
-+#endif
- 
- 	newfd = fcntl(fd, F_DUPFD, minfd);
- 	return set_cloexec_or_close(newfd);
-@@ -123,15 +153,18 @@ wl_os_recvmsg_cloexec(int sockfd, struct
- {
- 	ssize_t len;
- 
-+#ifdef MSG_CMSG_CLOEXEC
- 	len = recvmsg(sockfd, msg, flags | MSG_CMSG_CLOEXEC);
- 	if (len >= 0)
- 		return len;
- 	if (errno != EINVAL)
- 		return -1;
-+#endif
- 
+@@ -132,6 +140,7 @@ wl_os_recvmsg_cloexec(int sockfd, struct
  	return recvmsg_cloexec_fallback(sockfd, msg, flags);
  }
  
@@ -98,7 +31,7 @@ https://lists.freedesktop.org/archives/wayland-devel/2019-February/040024.html
  int
  wl_os_epoll_create_cloexec(void)
  {
-@@ -148,6 +181,18 @@ wl_os_epoll_create_cloexec(void)
+@@ -148,6 +157,18 @@ wl_os_epoll_create_cloexec(void)
  	fd = epoll_create(1);
  	return set_cloexec_or_close(fd);
  }
--- devel/wayland/patches/patch-tests_test-runner.c
+++ devel/wayland/patches/patch-tests_test-runner.c
@@ -1,30 +1,23 @@
-$NetBSD: patch-tests_test-runner.c,v 1.1 2019/08/18 16:05:12 nia Exp $
+$NetBSD: patch-tests_test-runner.c,v 1.2 2020/01/05 19:30:48 nia Exp $
 
-BSD support from FreeBSD
+NetBSD support
 
 --- tests/test-runner.c.orig	2019-03-21 00:55:25.000000000 +0000
 +++ tests/test-runner.c
-@@ -25,6 +25,12 @@
- 
- #define _GNU_SOURCE
- 
-+#include "../config.h"
-+
-+#ifdef HAVE_SYS_PARAM_H
-+#include <sys/param.h>
-+#endif
-+
- #include <unistd.h>
- #include <stdio.h>
- #include <stdlib.h>
-@@ -37,13 +43,28 @@
+@@ -31,19 +31,34 @@
+ #include <sys/types.h>
+ #include <sys/wait.h>
+ #include <sys/stat.h>
++#include <signal.h>
+ #include <string.h>
+ #include <assert.h>
+ #include <dlfcn.h>
  #include <errno.h>
  #include <limits.h>
  #include <sys/ptrace.h>
-+#ifdef HAVE_SYS_PRCTL_H
++#if defined(HAVE_PRCTL)
  #include <sys/prctl.h>
 +#endif
-+#include <signal.h>
  #ifndef PR_SET_PTRACER
  # define PR_SET_PTRACER 0x59616d61
  #endif
@@ -46,25 +39,7 @@ BSD support from FreeBSD
  /* when set to 1, check if tests are not leaking opened files.
   * It is turned on by default. It can be turned off by
   * WAYLAND_TEST_NO_LEAK_CHECK environment variable. */
-@@ -51,7 +72,7 @@ int fd_leak_check_enabled;
- 
- /* when this var is set to 0, every call to test_set_timeout() is
-  * suppressed - handy when debugging the test. Can be set by
-- * WAYLAND_TEST_NO_TIMEOUTS environment variable. */
-+ * WAYLAND_TESTS_NO_TIMEOUTS evnironment var */
- static int timeouts_enabled = 1;
- 
- /* set to one if the output goes to the terminal */
-@@ -239,6 +260,8 @@ is_debugger_attached(void)
- 		return 0;
- 	}
- 
-+
-+// xxx start here
- 	pid = fork();
- 	if (pid == -1) {
- 		perror("fork");
-@@ -259,13 +282,14 @@ is_debugger_attached(void)
+@@ -259,12 +274,13 @@ is_debugger_attached(void)
  			_exit(1);
  		if (!waitpid(-1, NULL, 0))
  			_exit(1);
@@ -75,12 +50,11 @@ BSD support from FreeBSD
  	} else {
  		close(pipefd[0]);
  
- 		/* Enable child to ptrace the parent process */
 +#if defined(HAVE_PRCTL)
+ 		/* Enable child to ptrace the parent process */
  		rc = prctl(PR_SET_PTRACER, pid);
  		if (rc != 0 && errno != EINVAL) {
- 			/* An error prevents us from telling if a debugger is attached.
-@@ -275,7 +299,9 @@ is_debugger_attached(void)
+@@ -275,7 +291,9 @@ is_debugger_attached(void)
  			 */
  			perror("prctl");
  			write(pipefd[1], "-", 1);
@@ -91,49 +65,3 @@ BSD support from FreeBSD
  			/* Signal to client that parent is ready by passing '+' */
  			write(pipefd[1], "+", 1);
  		}
-@@ -295,9 +321,6 @@ int main(int argc, char *argv[])
- 	int total, pass;
- 	siginfo_t info;
- 
--	if (isatty(fileno(stderr)))
--		is_atty = 1;
--
- 	if (is_debugger_attached()) {
- 		fd_leak_check_enabled = 0;
- 		timeouts_enabled = 0;
-@@ -336,7 +359,8 @@ int main(int argc, char *argv[])
- 		if (pid == 0)
- 			run_test(t); /* never returns */
- 
--		if (waitid(P_PID, pid, &info, WEXITED)) {
-+#ifdef HAVE_WAITID
-+		if (waitid(P_PID, 0, &info, WEXITED)) {
- 			stderr_set_color(RED);
- 			fprintf(stderr, "waitid failed: %m\n");
- 			stderr_reset_color();
-@@ -367,6 +391,25 @@ int main(int argc, char *argv[])
- 
- 			break;
- 		}
-+#else
-+               if (waitpid(-1, &status, 0) == -1) {
-+                       fprintf(stderr, "waitpid failed: %s\n",
-+                               strerror(errno));
-+                       abort();
-+               }
-+
-+               fprintf(stderr, "test \"%s\":\t", t->name);
-+               if (WIFEXITED(status)) {
-+                       fprintf(stderr, "exit status %d", WEXITSTATUS(status));
-+                       if (WEXITSTATUS(status) == EXIT_SUCCESS)
-+                               success = 1;
-+               } else if (WIFSIGNALED(status)) {
-+                       fprintf(stderr, "signal %d", WTERMSIG(status));
-+               }
-+#endif
-+
-+		if (t->must_fail)
-+			success = !success;
- 
- 		if (success) {
- 			pass++;
