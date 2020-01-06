e0cea37d8e17e4b14a8da00f1c03267dd1e3b107
ng0 <ng0@pkgsrc.org>
2020-01-06 17:35:31 +0000

    Add devel/remake version 4.2.1+dbg-1.4
    remake is an enhanced version of GNU Make that adds improved error
    reporting, better tracing, profiling and a debugger.

---
+++ devel/remake/options.mk
@@ -0,0 +1,32 @@
+# $NetBSD: options.mk,v 1.1 2020/01/06 17:35:31 ng0 Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.remake
+PKG_SUPPORTED_OPTIONS=	nls readline
+PKG_SUGGESTED_OPTIONS=	nls readline
+
+PLIST_VARS+=		nls
+
+.include "../../mk/bsd.options.mk"
+
+# Build NLS support only if a native implementation of gettext is available
+# to avoid a circular dependency (gmake->gettext-tools->ncurses->gmake).
+.include "../../devel/gettext-lib/builtin.mk"
+.if !empty(PKG_OPTIONS:Mnls) && \
+    !empty(USE_BUILTIN.gettext:M[yY][eE][sS])
+USE_PKGLOCALEDIR=	yes
+USE_TOOLS+=		msgfmt
+.  include "../../devel/gettext-lib/buildlink3.mk"
+PLIST.nls=		yes
+.else
+CONFIGURE_ARGS+=	--without-libintl-prefix
+CONFIGURE_ARGS+=	--without-libiconv-prefix
+CONFIGURE_ARGS+=	--disable-nls
+.endif
+
+# readline
+.if !empty(PKG_OPTIONS:Mreadline)
+CONFIGURE_ARGS+= --enable-gpl
+.include "../../mk/readline.buildlink3.mk"
+.else
+CONFIGURE_ARGS+= --disable-readline
+.endif
