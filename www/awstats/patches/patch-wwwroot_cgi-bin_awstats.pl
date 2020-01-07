d2f8e123e35430f4e662e4123a509402006c2b1b
adam <adam@pkgsrc.org>
2020-01-07 18:21:02 +0000

    awstats: updated to 7.7
    7.7:
    Security fix: CVE-2017-1000501
    Security fix: Missing sanitizing of parameters
    Fix LogFormat=4 with url containing spaces.
    Fix to window.opener vulnerability in external referral site links.
    Add methodurlprot in key to define log format.
    Add Dynamic DNS Lookup.
    Fix edge support.

--- www/awstats/patches/patch-wwwroot_cgi-bin_awstats.pl
+++ www/awstats/patches/patch-wwwroot_cgi-bin_awstats.pl
@@ -1,10 +1,10 @@
-$NetBSD: patch-aa,v 1.12 2013/04/27 11:19:38 adam Exp $
+$NetBSD: patch-wwwroot_cgi-bin_awstats.pl,v 1.1 2020/01/07 18:21:02 adam Exp $
 
 Check for configdir.
 
---- wwwroot/cgi-bin/awstats.pl.orig	2013-03-08 20:16:52.000000000 +0000
+--- wwwroot/cgi-bin/awstats.pl.orig	2018-01-07 14:36:46.000000000 +0000
 +++ wwwroot/cgi-bin/awstats.pl
-@@ -1713,7 +1713,7 @@ sub Read_Config {
+@@ -1723,7 +1723,7 @@ sub Read_Config {
  			"/etc/opt/awstats"
  		); 
  
