e3f65eddf67c4fed6e5ef1de0d1f068d71a6fc73
sevan <sevan@pkgsrc.org>
2020-01-07 20:25:14 +0000

    Update to giflib/giflib-util 5.2.1

--- graphics/giflib/buildlink3.mk
+++ graphics/giflib/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2014/05/31 23:44:19 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/01/07 20:25:14 sevan Exp $
 
 BUILDLINK_TREE+=	giflib
 
 .if !defined(GIFLIB_BUILDLINK3_MK)
 GIFLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.giflib+=	giflib>=5.1.0
+BUILDLINK_API_DEPENDS.giflib+=	giflib>=5.2.1
 BUILDLINK_PKGSRCDIR.giflib?=	../../graphics/giflib
 .endif	# GIFLIB_BUILDLINK3_MK
 
