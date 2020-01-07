e2baff583bda8eac373158e13db44eb0f34d941c
kim <kim@pkgsrc.org>
2020-01-07 11:08:14 +0000

    Update net/tcptraceroute6 to 1.0.4
    Notable changes:
    - Portability fixes.
    - A C11 compiler is now necessary (instead of GNU extensions).

--- net/tcptraceroute6/patches/patch-rdnssd_rdnssd.h
+++ net/tcptraceroute6/patches/patch-rdnssd_rdnssd.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-rdnssd_rdnssd.h,v 1.1 2015/10/23 09:52:35 he Exp $
+$NetBSD: patch-rdnssd_rdnssd.h,v 1.2 2020/01/07 11:08:14 kim Exp $
 
 Don't define nd_opt_rdnss on NetBSD.
 
---- rdnssd/rdnssd.h.orig	2011-10-17 15:35:11.000000000 +0000
-+++ rdnssd/rdnssd.h
+--- rdnss/rdnssd.h.orig	2011-10-17 15:35:11.000000000 +0000
++++ rdnss/rdnssd.h
 @@ -32,6 +32,7 @@ extern const rdnss_src_t rdnss_netlink, 
  #define ND_OPT_RDNSS 25
  #define ND_OPT_DNSSL 31
