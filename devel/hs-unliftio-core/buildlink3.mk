d87147a2da3f06fa99233437df43a5232844d2d6
pho <pho@pkgsrc.org>
2020-01-08 18:57:25 +0000

    Fix build with GHC 8.8

--- devel/hs-unliftio-core/buildlink3.mk
+++ devel/hs-unliftio-core/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/01 05:03:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/08 18:57:25 pho Exp $
 
 BUILDLINK_TREE+=	hs-unliftio-core
 
@@ -7,7 +7,7 @@ HS_UNLIFTIO_CORE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-unliftio-core+=	hs-unliftio-core>=0.1.2
 BUILDLINK_ABI_DEPENDS.hs-unliftio-core+=	hs-unliftio-core>=0.1.2.0
-BUILDLINK_PKGSRCDIR.hs-unliftio-core?=	../../devel/hs-unliftio-core
+BUILDLINK_PKGSRCDIR.hs-unliftio-core?=		../../devel/hs-unliftio-core
 .endif	# HS_UNLIFTIO_CORE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-unliftio-core
