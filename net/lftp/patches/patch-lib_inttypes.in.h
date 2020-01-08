cd1abd10ae5650bdf711b15aea456dd5620f22eb
nia <nia@pkgsrc.org>
2020-01-08 12:38:08 +0000

    lftp: Update to 4.9.0
    Version 4.9.0 - 2019-12-21
    
    * ftp: add support for CEPR command.
    * ftp: enabled TLS by default.
    * ftp: fixed OPTS MLST format.
    * ftps: apply rfc1738 rules.
    * ftp: stop sending ALLO commands by default.
    * http: fixed redirection to absolute path.
    * http: improved index parsing performance.
    * http: revert to original URL when temporary redirects led to an error.
    * mirror --no-empty-dirs: exclude empty dirs so that chmod is not done on them.
    * net: disable inet6 if there is no public ipv6 address.
    * net: lowered reconnect intervals.
    * sftp, fish: fixed yes/no/[fingerprint] and passphrase recognition.
    * sftp: fixed a bug with truncated files when packets are reordered.
    * torrent: fixed a coredump when all dht ports are busy.
    * fixed file list sorting when some file names start with tilde.
    * fixed retry counting when a site has multiple addresses.
    * fixed "version" command for certain openssl versions.

--- net/lftp/patches/patch-lib_inttypes.in.h
+++ net/lftp/patches/patch-lib_inttypes.in.h
@@ -1,16 +1,17 @@
-$NetBSD: patch-lib_inttypes.in.h,v 1.2 2015/07/25 03:29:57 ryoon Exp $
+$NetBSD: patch-lib_inttypes.in.h,v 1.3 2020/01/08 12:38:08 nia Exp $
 
 Poratbility obstacle.
 
---- lib/inttypes.in.h.orig	2015-05-27 08:37:01.000000000 +0000
+--- lib/inttypes.in.h.orig	2019-12-21 07:27:17.000000000 +0000
 +++ lib/inttypes.in.h
-@@ -56,9 +56,6 @@
+@@ -56,10 +56,6 @@
  # include <stdio.h>
  #endif
  
--#if !(INT_MIN == INT32_MIN && INT_MAX == INT32_MAX)
--# error "This file assumes that 'int' has exactly 32 bits. Please report your platform and compiler to <bug-gnulib@gnu.org>."
+-#if !(INT_MAX == 0x7fffffff && INT_MIN + INT_MAX == -1)
+-# error "This file assumes that 'int' is 32-bit two's complement. Please report your platform and compiler to <bug-gnulib@gnu.org>."
 -#endif
- 
+-
  /* The definitions of _GL_FUNCDECL_RPL etc. are copied here.  */
  
+ /* The definition of _GL_ARG_NONNULL is copied here.  */
