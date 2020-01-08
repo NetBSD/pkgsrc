d9c9f1e29df5440eb3a33d4a1681d589d599669d
adam <adam@pkgsrc.org>
2020-01-08 22:29:02 +0000

    nodejs12: updated to 12.14.1
    Version 12.14.1 'Erbium' (LTS):
    Notable changes
    * crypto: fix key requirements in asymmetric cipher
    * deps:
      - update llhttp to 2.0.1
      - update nghttp2 to 1.40.0
    * v8: mark serdes API as stable

--- lang/nodejs12/patches/patch-tools_gyp_pylib_gyp_common.py
+++ lang/nodejs12/patches/patch-tools_gyp_pylib_gyp_common.py
@@ -1,6 +1,7 @@
-$NetBSD: patch-tools_gyp_pylib_gyp_common.py,v 1.1 2019/12/09 20:05:40 adam Exp $
+$NetBSD: patch-tools_gyp_pylib_gyp_common.py,v 1.2 2020/01/08 22:29:03 adam Exp $
 
 Add support for DragonFly.
+
 --- tools/gyp/pylib/gyp/common.py.orig	2017-05-03 02:00:16.000000000 +0000
 +++ tools/gyp/pylib/gyp/common.py
 @@ -427,6 +427,8 @@ def GetFlavor(params):
