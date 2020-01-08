a308b104e2f40037b7784a5436bb92453830be89
adam <adam@pkgsrc.org>
2020-01-08 22:30:38 +0000

    nodejs: updated to 13.6.0
    Version 13.6.0 (Current):
    
    Notable Changes
    * assert:
      - Implement assert.match() and assert.doesNotMatch()
    * events:
      - Add EventEmitter.on to async iterate over events
      - Allow monitoring error events
    * fs:
      - Allow overriding fs for streams
    * perf_hooks:
      - Move perf_hooks out of experimental
    * repl:
      - Implement ZSH-like reverse-i-search
    * tls:
      - Add PSK (pre-shared key) support

--- lang/nodejs/patches/patch-tools_gyp_pylib_gyp_common.py
+++ lang/nodejs/patches/patch-tools_gyp_pylib_gyp_common.py
@@ -1,6 +1,7 @@
-$NetBSD: patch-tools_gyp_pylib_gyp_common.py,v 1.3 2019/01/10 13:02:21 ryoon Exp $
+$NetBSD: patch-tools_gyp_pylib_gyp_common.py,v 1.4 2020/01/08 22:30:38 adam Exp $
 
 Add support for DragonFly.
+
 --- tools/gyp/pylib/gyp/common.py.orig	2017-05-03 02:00:16.000000000 +0000
 +++ tools/gyp/pylib/gyp/common.py
 @@ -427,6 +427,8 @@ def GetFlavor(params):
