e7ca29f142fc585e2d67dca3ecec687395b58cc9
wiz <wiz@pkgsrc.org>
2020-01-09 14:02:31 +0000

    py-angr: update py-angr and dependencies (all from the angr project) to 8.20.1.7
    This now only supports python 3.x.
    
    Other changes not documented.

--- devel/py-pyvex/vex.mk
+++ devel/py-pyvex/vex.mk
@@ -1,9 +1,9 @@
-# $NetBSD: vex.mk,v 1.1 2018/03/15 02:49:25 khorben Exp $
+# $NetBSD: vex.mk,v 1.2 2020/01/09 14:02:32 wiz Exp $
 
 DISTNAME=	vex-0.0.0
 CATEGORIES=	devel
 MASTER_SITES=	${MASTER_SITE_GITHUB:=angr/}
-GITHUB_TAG=	7394e917fc86c8f042d8ce51a609810a97c20fd7
+GITHUB_TAG=	0e76ee25dc2793d23ec5f9695d38a2ca76780bbc
 
 MAINTAINER=	khorben@defora.org
 HOMEPAGE=	https://github.com/angr/vex/
