4afdc1f52e01a1f2a3e9cccf655fec35c8672985
ryoon <ryoon@pkgsrc.org>
2020-01-09 15:26:36 +0000

    mono6: Update to 6.6.0.161
    Changelog:
    WebAssembly
    CoreFX integration
    .NET 4.8 reference assemblies

--- lang/mono6/patches/patch-mcs_class_corlib_Test_Mono_MonoNativePlatformType.cs
+++ lang/mono6/patches/patch-mcs_class_corlib_Test_Mono_MonoNativePlatformType.cs
@@ -1,14 +1,14 @@
-$NetBSD: patch-mcs_class_corlib_Test_Mono_MonoNativePlatformType.cs,v 1.2 2019/10/09 22:46:04 maya Exp $
+$NetBSD: patch-mcs_class_corlib_Test_Mono_MonoNativePlatformType.cs,v 1.3 2020/01/09 15:26:36 ryoon Exp $
 
 Add netbsd support
 https://github.com/mono/mono/pull/15938
 
---- mcs/class/corlib/Test/Mono/MonoNativePlatformType.cs.orig	2019-09-19 07:46:06.000000000 +0000
+--- mcs/class/corlib/Test/Mono/MonoNativePlatformType.cs.orig	2019-12-10 07:50:31.000000000 +0000
 +++ mcs/class/corlib/Test/Mono/MonoNativePlatformType.cs
-@@ -38,6 +38,7 @@ namespace Mono
- 		MONO_NATIVE_PLATFORM_TYPE_AIX		= 4,
+@@ -39,6 +39,7 @@ namespace Mono
  		MONO_NATIVE_PLATFORM_TYPE_ANDROID	= 5,
  		MONO_NATIVE_PLATFORM_TYPE_FREEBSD	= 6,
+ 		MONO_NATIVE_PLATFORM_TYPE_HAIKU		= 7,
 +		MONO_NATIVE_PLATFORM_TYPE_NETBSD	= 8,
  
  		MONO_NATIVE_PLATFORM_TYPE_IPHONE	= 0x100,
