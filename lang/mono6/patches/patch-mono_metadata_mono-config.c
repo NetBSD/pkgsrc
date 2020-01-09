e9bcca43965340d4b372e662d3ef641b171654f4
ryoon <ryoon@pkgsrc.org>
2020-01-09 15:26:36 +0000

    mono6: Update to 6.6.0.161
    Changelog:
    WebAssembly
    CoreFX integration
    .NET 4.8 reference assemblies

--- lang/mono6/patches/patch-mono_metadata_mono-config.c
+++ lang/mono6/patches/patch-mono_metadata_mono-config.c
@@ -1,16 +1,16 @@
-$NetBSD: patch-mono_metadata_mono-config.c,v 1.2 2019/10/09 22:46:04 maya Exp $
+$NetBSD: patch-mono_metadata_mono-config.c,v 1.3 2020/01/09 15:26:36 ryoon Exp $
 
 Look for our dllmap in /usr/pkg/etc/mono/config
 as opposed to /usr/pkg/etc/mono/mono/config
 
---- mono/metadata/mono-config.c.orig	2019-09-19 07:46:07.000000000 +0000
+--- mono/metadata/mono-config.c.orig	2019-12-10 07:50:32.000000000 +0000
 +++ mono/metadata/mono-config.c
-@@ -682,7 +682,7 @@ mono_config_parse (const char *filename)
+@@ -676,7 +676,7 @@ mono_config_parse (const char *filename)
  
  	const char *cfg_dir = mono_get_config_dir ();
  	if (cfg_dir) {
--		mono_cfg = g_build_filename (cfg_dir, "mono", "config", NULL);
-+		mono_cfg = g_build_filename (cfg_dir, "config", NULL);
+-		char *mono_cfg = g_build_filename (cfg_dir, "mono", "config", (const char *)NULL);
++		char *mono_cfg = g_build_filename (cfg_dir, "config", (const char *)NULL);
  		mono_config_parse_file (mono_cfg);
  		g_free (mono_cfg);
  	}
