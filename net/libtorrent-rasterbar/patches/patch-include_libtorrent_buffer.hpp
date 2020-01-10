4aec2e98c9561717901d306bf0fc2f173b77a2b8
nia <nia@pkgsrc.org>
2020-01-10 11:52:05 +0000

    libtorrent-rasterbar: Let's not peek into internal malloc properties.

--- net/libtorrent-rasterbar/patches/patch-include_libtorrent_buffer.hpp
+++ net/libtorrent-rasterbar/patches/patch-include_libtorrent_buffer.hpp
@@ -1,24 +1,24 @@
-$NetBSD: patch-include_libtorrent_buffer.hpp,v 1.1 2019/12/16 18:00:51 nia Exp $
+$NetBSD: patch-include_libtorrent_buffer.hpp,v 1.2 2020/01/10 11:52:05 nia Exp $
 
-Support NetBSD.
+On second thought, let's not peek into internal malloc properties.
 
---- include/libtorrent/buffer.hpp.orig	2019-09-24 09:52:01.000000000 +0000
+--- include/libtorrent/buffer.hpp.orig	2019-12-19 00:51:16.000000000 +0000
 +++ include/libtorrent/buffer.hpp
-@@ -47,6 +47,8 @@ POSSIBILITY OF SUCH DAMAGE.
+@@ -49,8 +49,6 @@ POSSIBILITY OF SUCH DAMAGE.
  #include <malloc.h>
- #elif defined _MSC_VER
- #include <malloc.h>
-+#elif defined __NetBSD__
-+#include <malloc.h>
  #elif defined __FreeBSD__
  #include <malloc_np.h>
- #elif defined TORRENT_BSD
-@@ -85,7 +87,7 @@ public:
+-#elif defined TORRENT_BSD
+-#include <malloc/malloc.h>
+ #endif
  
- 		// the actual allocation may be larger than we requested. If so, let the
- 		// user take advantage of every single byte
--#if (defined __GLIBC__ && !defined __UCLIBC__) || defined __FreeBSD__
-+#if (defined __GLIBC__ && !defined __UCLIBC__) || defined __FreeBSD__ || defined __NetBSD__
+ namespace libtorrent {
+@@ -89,8 +87,6 @@ public:
  		m_size = static_cast<difference_type>(::malloc_usable_size(m_begin));
  #elif defined _MSC_VER
  		m_size = static_cast<difference_type>(::_msize(m_begin));
+-#elif defined TORRENT_BSD
+-		m_size = static_cast<difference_type>(::malloc_size(m_begin));
+ #else
+ 		m_size = size;
+ #endif
