7efb0624f86875408eb1901b5fc4ff476f28a778
khorben <khorben@pkgsrc.org>
2020-01-10 22:53:13 +0000

    deforaos-libdesktop: update to 0.4.0
    Changes since 0.2.2:
    - Defaults to Gtk+ 3
    - Improvements to the compatibility layer
    - New MimeHandler API

--- x11/deforaos-libdesktop/buildlink3.mk
+++ x11/deforaos-libdesktop/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2019/07/21 22:24:14 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2020/01/10 22:53:13 khorben Exp $
 #
 
 BUILDLINK_TREE+=	deforaos-libdesktop
@@ -6,12 +6,12 @@ BUILDLINK_TREE+=	deforaos-libdesktop
 .if !defined(DEFORAOS_LIBDESKTOP_BUILDLINK3_MK)
 DEFORAOS_LIBDESKTOP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.2.1nb1
-BUILDLINK_ABI_DEPENDS.deforaos-libdesktop?=	deforaos-libdesktop>=0.2.2nb6
+BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.4.0
+BUILDLINK_ABI_DEPENDS.deforaos-libdesktop?=	deforaos-libdesktop>=0.4.0
 BUILDLINK_PKGSRCDIR.deforaos-libdesktop?=	../../x11/deforaos-libdesktop
 
 .include "../../devel/deforaos-libsystem/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
 .endif	# DEFORAOS_LIBDESKTOP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-deforaos-libdesktop
