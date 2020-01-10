27da222be39c714de444178337af60497ee6cae2
joerg <joerg@pkgsrc.org>
2020-01-10 21:16:03 +0000

    Drop SSLv3-only support. Don't look into internals of SSL structures.

--- mail/qpopper/patches/patch-popper_pop__tls__openssl.c
+++ mail/qpopper/patches/patch-popper_pop__tls__openssl.c
@@ -1,10 +1,18 @@
-$NetBSD: patch-popper_pop__tls__openssl.c,v 1.1 2016/03/26 23:52:37 joerg Exp $
+$NetBSD: patch-popper_pop__tls__openssl.c,v 1.2 2020/01/10 21:16:03 joerg Exp $
 
-Disable SSLv2.
+Disable SSLv2 and SSLv3. Don't look into the internals of SSL objects.
 
---- popper/pop_tls_openssl.c.orig	2016-03-26 21:49:05.471963098 +0000
+--- popper/pop_tls_openssl.c.orig	2011-05-30 19:13:40.000000000 +0000
 +++ popper/pop_tls_openssl.c
-@@ -318,10 +318,12 @@ openssl_init ( pop_tls *pTLS, POP *pPOP 
+@@ -51,6 +51,7 @@
+ #include <fcntl.h>
+ #include <setjmp.h>
+ #include <signal.h>
++#include <string.h>
+ 
+ #ifdef HAVE_UNISTD_H
+ #  include <unistd.h>
+@@ -318,6 +319,7 @@ openssl_init ( pop_tls *pTLS, POP *pPOP 
              pTLS->m_OpenSSLmeth = SSLv23_server_method();
              break;
  
@@ -12,12 +20,15 @@ Disable SSLv2.
          case QPOP_SSLv2:       /* SSL version 2 only */
              DEBUG_LOG0 ( pPOP, "...setting method to SSLv2_server_method" );
              pTLS->m_OpenSSLmeth = SSLv2_server_method();
+@@ -327,6 +329,7 @@ openssl_init ( pop_tls *pTLS, POP *pPOP 
+             DEBUG_LOG0 ( pPOP, "...setting method to SSLv3_server_method" );
+             pTLS->m_OpenSSLmeth = SSLv3_server_method();
              break;
 +#endif
  
-         case QPOP_SSLv3:       /* SSL version 3 only */
-             DEBUG_LOG0 ( pPOP, "...setting method to SSLv3_server_method" );
-@@ -350,6 +352,7 @@ openssl_init ( pop_tls *pTLS, POP *pPOP 
+         case QPOP_TLSv1:       /* TLS version 1 only */
+             DEBUG_LOG0 ( pPOP, "...setting method to TLSv1_server_method" );
+@@ -350,6 +353,7 @@ openssl_init ( pop_tls *pTLS, POP *pPOP 
          log_openssl_err ( pPOP, HERE, "Unable to allocate SSL_CTX" );
          goto Done;
      }
@@ -25,3 +36,12 @@ Disable SSLv2.
  
      /*
       * Set desired options
+@@ -537,7 +541,7 @@ openssl_handshake ( pop_tls *pTLS )
+                           "%s session-id; cipher: %s (%s), %d bits",
+                           VERSION, SSL_CIPHER_get_version(ciph),
+                           pTLS->m_pPOP->client, pTLS->m_pPOP->ipaddr,
+-                          ( pTLS->m_OpenSSLconn->hit ? "reused" : "new" ),
++                          ( SSL_session_reused(pTLS->m_OpenSSLconn) ? "reused" : "new" ),
+                           ( ciph_name != NULL ? ciph_name : "(none)" ),
+                           get_cipher_description ( ciph, buf, sizeof(buf) ),
+                           SSL_CIPHER_get_bits    ( ciph, &al_bits ) );
