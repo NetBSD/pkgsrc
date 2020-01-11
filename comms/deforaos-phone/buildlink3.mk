aee8a0463ae863eb6dd273ea9bc759b5d575c445
khorben <khorben@pkgsrc.org>
2020-01-11 01:34:11 +0000

    deforaos-phone: update to 0.6.0
    Changes since 0.5.1:
    - Defaults to Gtk+ 3 (like libDesktop)
    - Re-licensed to 2-clause BSD
    - Extended telephony API
    - More translated content
    - Preferences for additional telephony providers (APN, USSD)
    - Build fix for non-NetBSD platforms

--- comms/deforaos-phone/buildlink3.mk
+++ comms/deforaos-phone/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2019/07/21 22:23:58 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/01/11 01:34:11 khorben Exp $
 
 BUILDLINK_TREE+=	deforaos-phone
 
 .if !defined(DEFORAOS_PHONE_BUILDLINK3_MK)
 DEFORAOS_PHONE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-phone+=	deforaos-phone>=0.4.3
-BUILDLINK_ABI_DEPENDS.deforaos-phone?=	deforaos-phone>=0.5.1nb5
+BUILDLINK_API_DEPENDS.deforaos-phone+=	deforaos-phone>=0.6.0
+BUILDLINK_ABI_DEPENDS.deforaos-phone?=	deforaos-phone>=0.6.0
 BUILDLINK_PKGSRCDIR.deforaos-phone?=	../../comms/deforaos-phone
 
 .include "../../x11/deforaos-libdesktop/buildlink3.mk"
