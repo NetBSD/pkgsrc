1e77aae8c85f8c6dc3fd55ba99db2e1864c2fcd1
pho <pho@pkgsrc.org>
2020-01-11 08:05:40 +0000

    Remove dependency on devel/hs-mtl
    mtl is now a part of GHC.
    Also hs-fail is no longer required as of GHC 8.

--- devel/hs-MonadRandom/buildlink3.mk
+++ devel/hs-MonadRandom/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/01/02 02:47:31 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/01/11 08:05:40 pho Exp $
 
 BUILDLINK_TREE+=	hs-MonadRandom
 
@@ -9,11 +9,9 @@ BUILDLINK_API_DEPENDS.hs-MonadRandom+=	hs-MonadRandom>=0.5.1
 BUILDLINK_ABI_DEPENDS.hs-MonadRandom+=	hs-MonadRandom>=0.5.1.1
 BUILDLINK_PKGSRCDIR.hs-MonadRandom?=	../../devel/hs-MonadRandom
 
-.include "../../devel/hs-mtl/buildlink3.mk"
 .include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../devel/hs-random/buildlink3.mk"
 .include "../../devel/hs-transformers-compat/buildlink3.mk"
-.include "../../devel/hs-fail/buildlink3.mk"
 .endif	# HS_MONADRANDOM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-MonadRandom
