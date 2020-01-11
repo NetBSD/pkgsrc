1ac8cc02b36256371e8ef102ecf3d096438e8d30
pho <pho@pkgsrc.org>
2020-01-11 08:15:58 +0000

    Update to hint-0.9.0.2
    0.9.0.2
    * Support GHC 8.8
    * Drop support for GHC 8.2
    
    0.9.0.1
    * Make tests pass with stack 2.1.1
    
    0.9.0
    * Support GHC 8.6
    * Drop support for GHC 8.0
    
    0.8.0
    * Support GHC 8.4
    * Drop support for GHC 7.8 and 7.10
    * Add runStmt to execute statements in the IO monad and bind new names
    * Internal changes of temporary files for phantom modules
      * The files are now called M<nnn>.hs instead of <nnn>
      * Improved cleanup of phantom module source files
      * ghc 8.4 only: phantom modules are put into a temporary directory
    * Add typeChecksWithDetails to obtain type-checking errors
    * Stop GHC from overwriting the Ctrl-C signal handler
    * Add SetImportsF to allow finer imports control

--- devel/hs-hint/buildlink3.mk
+++ devel/hs-hint/buildlink3.mk
@@ -1,17 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/01/02 04:58:07 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/01/11 08:15:58 pho Exp $
 
 BUILDLINK_TREE+=	hs-hint
 
 .if !defined(HS_HINT_BUILDLINK3_MK)
 HS_HINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hint+=	hs-hint>=0.7.0
-BUILDLINK_ABI_DEPENDS.hs-hint+=	hs-hint>=0.7.0
+BUILDLINK_API_DEPENDS.hs-hint+=	hs-hint>=0.9.0
+BUILDLINK_ABI_DEPENDS.hs-hint+=	hs-hint>=0.9.0.2
 BUILDLINK_PKGSRCDIR.hs-hint?=	../../devel/hs-hint
 
 .include "../../devel/hs-exceptions/buildlink3.mk"
 .include "../../devel/hs-ghc-paths/buildlink3.mk"
-.include "../../devel/hs-mtl/buildlink3.mk"
 .include "../../devel/hs-random/buildlink3.mk"
 .endif	# HS_HINT_BUILDLINK3_MK
 
