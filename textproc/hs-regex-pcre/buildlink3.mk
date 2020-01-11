7a7ea1a5e76c9c04c6534171bec1ce2f8040fd19
pho <pho@pkgsrc.org>
2020-01-11 09:22:13 +0000

    Remove dependency on devel/hs-fail
    It's no longer required as of GHC 8.

--- textproc/hs-regex-pcre/buildlink3.mk
+++ textproc/hs-regex-pcre/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/03 06:18:21 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/11 09:22:13 pho Exp $
 
 BUILDLINK_TREE+=	hs-regex-pcre
 
@@ -10,7 +10,6 @@ BUILDLINK_ABI_DEPENDS.hs-regex-pcre+=	hs-regex-pcre>=0.95.0.0
 BUILDLINK_PKGSRCDIR.hs-regex-pcre?=	../../textproc/hs-regex-pcre
 
 .include "../../textproc/hs-regex-base/buildlink3.mk"
-.include "../../devel/hs-fail/buildlink3.mk"
 .include "../../devel/pcre/buildlink3.mk"
 .endif	# HS_REGEX_PCRE_BUILDLINK3_MK
 
