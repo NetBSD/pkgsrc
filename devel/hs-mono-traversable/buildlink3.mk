f09b75d03d763e48d1438fb8db83da8335252f0d
pho <pho@pkgsrc.org>
2020-01-11 08:56:16 +0000

    Update to mono-traversable-1.0.15.1
    1.0.15.1
    * Remove whitespace after @ in as-patterns for GHC HEAD #186
    
    1.0.15.0
    * Added toNonEmpty to Data.NonNull #185
    
    1.0.14.0
    * Added WrappedMono to Data.MonoTraversable #182
    
    1.0.13.0
    * Added WrappedPoly to Data.MonoTraversable #180
    
    1.0.12.0
    * Added filterSet to Data.Containers
    * Use container specific implementations for filterSet and filterMap
      #178
    
    1.0.11.0
    * Adding monomorphic instances for GHC.Generics and Data.Proxy types
      #175
    
    1.0.10.0
    * Make index work on negative indices #172 #114
    
    1.0.9.0
    * Added filterMap to Data.Containers #167

--- devel/hs-mono-traversable/buildlink3.mk
+++ devel/hs-mono-traversable/buildlink3.mk
@@ -1,21 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/01/03 07:46:30 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/01/11 08:56:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-mono-traversable
 
 .if !defined(HS_MONO_TRAVERSABLE_BUILDLINK3_MK)
 HS_MONO_TRAVERSABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mono-traversable+=	hs-mono-traversable>=1.0.8
-BUILDLINK_ABI_DEPENDS.hs-mono-traversable+=	hs-mono-traversable>=1.0.8.1
+BUILDLINK_API_DEPENDS.hs-mono-traversable+=	hs-mono-traversable>=1.0.15
+BUILDLINK_ABI_DEPENDS.hs-mono-traversable+=	hs-mono-traversable>=1.0.15.1
 BUILDLINK_PKGSRCDIR.hs-mono-traversable?=	../../devel/hs-mono-traversable
 
 .include "../../devel/hs-hashable/buildlink3.mk"
 .include "../../devel/hs-split/buildlink3.mk"
-.include "../../devel/hs-text/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
 .include "../../devel/hs-vector-algorithms/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_MONO_TRAVERSABLE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-mono-traversable
