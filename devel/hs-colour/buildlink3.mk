0123f0d300b84f7d65ea1e1720dbeb02bc7c1633
pho <pho@pkgsrc.org>
2020-01-11 11:44:27 +0000

    Update to colour-2.3.5
    New in version 2.3.5:
    - Support for MonadFail Proposal.
    - Documentation updates.
    
    New in version 2.3.4:
    - Support for Semigroup (as superclass of) Monoid Proposal.
    - Add test-suite to cabal file.

--- devel/hs-colour/buildlink3.mk
+++ devel/hs-colour/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/12/29 23:57:51 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/11 11:44:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-colour
 
 .if !defined(HS_COLOUR_BUILDLINK3_MK)
 HS_COLOUR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-colour+=	hs-colour>=2.3.3
-BUILDLINK_ABI_DEPENDS.hs-colour+=	hs-colour>=2.3.3
-BUILDLINK_PKGSRCDIR.hs-colour?=	../../devel/hs-colour
+BUILDLINK_API_DEPENDS.hs-colour+=	hs-colour>=2.3.5
+BUILDLINK_ABI_DEPENDS.hs-colour+=	hs-colour>=2.3.5
+BUILDLINK_PKGSRCDIR.hs-colour?=		../../devel/hs-colour
 .endif	# HS_COLOUR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-colour
