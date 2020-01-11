e69293d1e3f1080b6c65e1bd2a52a159ad35baf3
pho <pho@pkgsrc.org>
2020-01-11 10:38:32 +0000

    Remove dependency on devel/hs-stm
    It's now part of GHC.

--- devel/hs-monad-control/buildlink3.mk
+++ devel/hs-monad-control/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/01/01 04:51:21 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/01/11 10:39:25 pho Exp $
 
 BUILDLINK_TREE+=	hs-monad-control
 
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.hs-monad-control+=	hs-monad-control>=1.0.2
 BUILDLINK_ABI_DEPENDS.hs-monad-control+=	hs-monad-control>=1.0.2.3
 BUILDLINK_PKGSRCDIR.hs-monad-control?=		../../devel/hs-monad-control
 
-.include "../../devel/hs-stm/buildlink3.mk"
 .include "../../devel/hs-transformers-base/buildlink3.mk"
 .include "../../devel/hs-transformers-compat/buildlink3.mk"
 .endif	# HS_MONAD_CONTROL_BUILDLINK3_MK
