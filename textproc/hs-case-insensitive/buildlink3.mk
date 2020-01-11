6154d0480c33c1e79bcd52a5b7c32785d21a2ef4
pho <pho@pkgsrc.org>
2020-01-11 11:15:53 +0000

    Remove dependency on devel/hs-text
    It's now part of GHC.
    Also contravariant, fail, and semigroups are no longer required as of GHC 8.

--- textproc/hs-case-insensitive/buildlink3.mk
+++ textproc/hs-case-insensitive/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2020/01/03 03:59:19 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/01/11 11:19:03 pho Exp $
 
 BUILDLINK_TREE+=	hs-case-insensitive
 
@@ -10,8 +10,6 @@ BUILDLINK_ABI_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.2.1.0
 BUILDLINK_PKGSRCDIR.hs-case-insensitive?=	../../textproc/hs-case-insensitive
 
 .include "../../devel/hs-hashable/buildlink3.mk"
-.include "../../devel/hs-text/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_CASE_INSENSITIVE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-case-insensitive
