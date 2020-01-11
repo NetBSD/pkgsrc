7123e93224728e378dff4063222d4aa22d21857a
pho <pho@pkgsrc.org>
2020-01-11 11:15:53 +0000

    Remove dependency on devel/hs-text
    It's now part of GHC.
    Also contravariant, fail, and semigroups are no longer required as of GHC 8.

--- textproc/hs-blaze-html/buildlink3.mk
+++ textproc/hs-blaze-html/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2020/01/02 11:51:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/01/11 11:17:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-blaze-html
 
@@ -11,7 +11,6 @@ BUILDLINK_PKGSRCDIR.hs-blaze-html?=	../../textproc/hs-blaze-html
 
 .include "../../devel/hs-blaze-builder/buildlink3.mk"
 .include "../../textproc/hs-blaze-markup/buildlink3.mk"
-.include "../../devel/hs-text/buildlink3.mk"
 .endif	# HS_BLAZE_HTML_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-blaze-html
