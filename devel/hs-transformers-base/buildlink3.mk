b6f79e9bfbc53c221959069b07f43e67d00b888c
pho <pho@pkgsrc.org>
2020-01-11 10:38:32 +0000

    Remove dependency on devel/hs-stm
    It's now part of GHC.

--- devel/hs-transformers-base/buildlink3.mk
+++ devel/hs-transformers-base/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/01/01 04:47:40 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/01/11 10:38:32 pho Exp $
 
 BUILDLINK_TREE+=	hs-transformers-base
 
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.hs-transformers-base+=	hs-transformers-base>=0.4.5
 BUILDLINK_ABI_DEPENDS.hs-transformers-base+=	hs-transformers-base>=0.4.5.2
 BUILDLINK_PKGSRCDIR.hs-transformers-base?=	../../devel/hs-transformers-base
 
-.include "../../devel/hs-stm/buildlink3.mk"
 .include "../../devel/hs-transformers-compat/buildlink3.mk"
 .include "../../devel/hs-base-orphans/buildlink3.mk"
 .endif	# HS_TRANSFORMERS_BASE_BUILDLINK3_MK
