b24c1d24f719a8ab9679c4a3d5cd3baa3dbe549e
pho <pho@pkgsrc.org>
2020-01-11 14:01:07 +0000

    Update to xmonad-0.15
    * Reimplement sendMessage to deal properly with windowset changes made
      during handling.
    * Add new library functions windowBracket and modifyWindowSet to
      XMonad.Operations.

--- wm/xmonad/buildlink3.mk
+++ wm/xmonad/buildlink3.mk
@@ -1,19 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.6 2020/01/02 17:11:37 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/01/11 14:01:07 pho Exp $
 
 BUILDLINK_TREE+=	xmonad
 
 .if !defined(XMONAD_BUILDLINK3_MK)
 XMONAD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xmonad+=	xmonad>=0.14
-BUILDLINK_ABI_DEPENDS.xmonad+=	xmonad>=0.14
+BUILDLINK_API_DEPENDS.xmonad+=	xmonad>=0.15
+BUILDLINK_ABI_DEPENDS.xmonad+=	xmonad>=0.15
 BUILDLINK_PKGSRCDIR.xmonad?=	../../wm/xmonad
 
-.include "../../x11/libX11/buildlink3.mk"
 .include "../../devel/hs-data-default/buildlink3.mk"
 .include "../../devel/hs-extensible-exceptions/buildlink3.mk"
-.include "../../devel/hs-mtl/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .include "../../devel/hs-setlocale/buildlink3.mk"
 .include "../../devel/hs-utf8-string/buildlink3.mk"
 .include "../../x11/hs-X11/buildlink3.mk"
