6833cf010c3a0bdf2ef462cdd46a5ed130d8157a
pho <pho@pkgsrc.org>
2020-01-11 10:25:05 +0000

    Remove dependency on devel/hs-mtl and devel/hs-parsec
    They are now part of GHC.

--- www/hs-HTTP/buildlink3.mk
+++ www/hs-HTTP/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2020/01/02 15:50:01 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2020/01/11 10:25:05 pho Exp $
 
 BUILDLINK_TREE+=	hs-HTTP
 
@@ -9,8 +9,6 @@ BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.3.14
 BUILDLINK_ABI_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.3.14
 BUILDLINK_PKGSRCDIR.hs-HTTP?=	../../www/hs-HTTP
 
-.include "../../textproc/hs-parsec/buildlink3.mk"
-.include "../../devel/hs-mtl/buildlink3.mk"
 .include "../../net/hs-network/buildlink3.mk"
 .include "../../net/hs-network-uri/buildlink3.mk"
 .endif	# HS_HTTP_BUILDLINK3_MK
