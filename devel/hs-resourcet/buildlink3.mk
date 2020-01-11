0d87c778d3ed498b55813f146b1b054b95d770d9
pho <pho@pkgsrc.org>
2020-01-11 08:26:22 +0000

    Update to resourcet-1.2.2
    1.2.2
    * Add MonadFail instance for ResourceT.
    
    1.2.1
    * Support exceptions-0.10.
    
    1.2.0
    * Drop monad-control and mmorph dependencies
    * Change behavior of runResourceT to match runResourceTChecked

--- devel/hs-resourcet/buildlink3.mk
+++ devel/hs-resourcet/buildlink3.mk
@@ -1,21 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/01/01 05:15:20 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/01/11 08:26:22 pho Exp $
 
 BUILDLINK_TREE+=	hs-resourcet
 
 .if !defined(HS_RESOURCET_BUILDLINK3_MK)
 HS_RESOURCET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=1.1.11
-BUILDLINK_ABI_DEPENDS.hs-resourcet+=	hs-resourcet>=1.1.11
+BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=1.2.2
+BUILDLINK_ABI_DEPENDS.hs-resourcet+=	hs-resourcet>=1.2.2
 BUILDLINK_PKGSRCDIR.hs-resourcet?=	../../devel/hs-resourcet
 
 .include "../../devel/hs-exceptions/buildlink3.mk"
-.include "../../devel/hs-lifted-base/buildlink3.mk"
-.include "../../devel/hs-mmorph/buildlink3.mk"
-.include "../../devel/hs-monad-control/buildlink3.mk"
-.include "../../devel/hs-mtl/buildlink3.mk"
-.include "../../devel/hs-transformers-base/buildlink3.mk"
-.include "../../devel/hs-transformers-compat/buildlink3.mk"
+.include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../devel/hs-unliftio-core/buildlink3.mk"
 .endif	# HS_RESOURCET_BUILDLINK3_MK
 
