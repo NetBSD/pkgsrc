6621b7cb5c95d21ad2ef3ce3e87975386285999b
pho <pho@pkgsrc.org>
2020-01-11 10:21:27 +0000

    Update to texmath-0.12
    * Use Text instead of String in data types and functions
      (Christian Despres) [API change].  Note that there are still a few
      places where we unpack Text to String with a view pattern:
      performance could likely be increased with further rewriting.
    * Avoid use of !! with negative index (jgm/pandoc#5853).

--- textproc/hs-texmath/buildlink3.mk
+++ textproc/hs-texmath/buildlink3.mk
@@ -1,19 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.20 2020/01/02 15:37:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2020/01/11 10:21:27 pho Exp $
 
 BUILDLINK_TREE+=	hs-texmath
 
 .if !defined(HS_TEXMATH_BUILDLINK3_MK)
 HS_TEXMATH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.11.3
-BUILDLINK_ABI_DEPENDS.hs-texmath+=	hs-texmath>=0.11.3
+BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.12
+BUILDLINK_ABI_DEPENDS.hs-texmath+=	hs-texmath>=0.12
 BUILDLINK_PKGSRCDIR.hs-texmath?=	../../textproc/hs-texmath
 
-.include "../../devel/hs-mtl/buildlink3.mk"
 .include "../../textproc/hs-pandoc-types/buildlink3.mk"
-.include "../../textproc/hs-parsec/buildlink3.mk"
 .include "../../devel/hs-syb/buildlink3.mk"
 .include "../../textproc/hs-xml/buildlink3.mk"
+.include "../../converters/hs-aeson/buildlink3.mk"
 .endif	# HS_TEXMATH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-texmath
