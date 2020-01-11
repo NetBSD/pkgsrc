bf91b4c1abe94c388144b91c28f7f5bd37362f52
pho <pho@pkgsrc.org>
2020-01-11 09:10:32 +0000

    Remove dependency on devel/hs-mtl and devel/hs-text
    They are now part of GHC.
    Also hs-fail is no longer required as of GHC 8.

--- textproc/hs-regex-base/buildlink3.mk
+++ textproc/hs-regex-base/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/01/02 14:56:10 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/01/11 09:10:32 pho Exp $
 
 BUILDLINK_TREE+=	hs-regex-base
 
@@ -8,10 +8,6 @@ HS_REGEX_BASE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-regex-base+=	hs-regex-base>=0.94.0
 BUILDLINK_ABI_DEPENDS.hs-regex-base+=	hs-regex-base>=0.94.0.0
 BUILDLINK_PKGSRCDIR.hs-regex-base?=	../../textproc/hs-regex-base
-
-.include "../../devel/hs-mtl/buildlink3.mk"
-.include "../../devel/hs-text/buildlink3.mk"
-.include "../../devel/hs-fail/buildlink3.mk"
 .endif	# HS_REGEX_BASE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-regex-base
