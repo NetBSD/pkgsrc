040e53c6766545d90d0749a2cec05c260f0b1893
pho <pho@pkgsrc.org>
2020-01-11 10:38:32 +0000

    Remove dependency on devel/hs-stm
    It's now part of GHC.

--- devel/hs-StateVar/buildlink3.mk
+++ devel/hs-StateVar/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/01 02:22:40 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/11 10:41:29 pho Exp $
 
 BUILDLINK_TREE+=	hs-StateVar
 
@@ -8,8 +8,6 @@ HS_STATEVAR_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-StateVar+=	hs-StateVar>=1.2
 BUILDLINK_ABI_DEPENDS.hs-StateVar+=	hs-StateVar>=1.2
 BUILDLINK_PKGSRCDIR.hs-StateVar?=	../../devel/hs-StateVar
-
-.include "../../devel/hs-stm/buildlink3.mk"
 .endif	# HS_STATEVAR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-StateVar
