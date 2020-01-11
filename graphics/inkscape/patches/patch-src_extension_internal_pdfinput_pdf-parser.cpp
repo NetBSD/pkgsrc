37ca44ce4eac7283871f21becf660f2c9c53d52a
martin <martin@pkgsrc.org>
2020-01-11 09:59:05 +0000

    Make the pdf-input extension buildable with gcc 8.3.

--- graphics/inkscape/patches/patch-src_extension_internal_pdfinput_pdf-parser.cpp
+++ graphics/inkscape/patches/patch-src_extension_internal_pdfinput_pdf-parser.cpp
@@ -1,11 +1,12 @@
-$NetBSD: patch-src_extension_internal_pdfinput_pdf-parser.cpp,v 1.12 2019/11/23 09:23:22 martin Exp $
+$NetBSD: patch-src_extension_internal_pdfinput_pdf-parser.cpp,v 1.13 2020/01/11 09:59:06 martin Exp $
 
 Object.h is included in pdf-parser.h -- see patch for pdf-parser.h.
 Fix building with poppler 0.76.x.
 Work around constness bugs (addChar should take a const arg).
+Add some needed const casts for GfxPath objects.
 
---- src/extension/internal/pdfinput/pdf-parser.cpp.orig	2019-01-15 05:29:27.000000000 +0100
-+++ src/extension/internal/pdfinput/pdf-parser.cpp	2019-11-23 10:14:30.693904450 +0100
+--- src/extension/internal/pdfinput/pdf-parser.cpp.orig	2020-01-11 10:11:43.316590715 +0100
++++ src/extension/internal/pdfinput/pdf-parser.cpp	2020-01-11 10:22:11.403079322 +0100
 @@ -41,7 +41,6 @@
  #include "goo/GooString.h"
  #include "GlobalParams.h"
@@ -14,6 +15,15 @@ Work around constness bugs (addChar should take a const arg).
  #include "Array.h"
  #include "Dict.h"
  #include "Stream.h"
+@@ -342,7 +341,7 @@
+         state->lineTo(cropBox->x1, cropBox->y2);
+         state->closePath();
+         state->clip();
+-        clipHistory->setClip(state->getPath(), clipNormal);
++        clipHistory->setClip(const_cast<GfxPath*>(state->getPath()), clipNormal);
+         builder->setClipPath(state);
+         state->clearPath();
+     }
 @@ -426,7 +425,7 @@
  	error(errInternal, -1, "Weird page contents");
      	return;
@@ -23,6 +33,19 @@ Work around constness bugs (addChar should take a const arg).
    go(topLevel);
    delete parser;
    parser = NULL;
+@@ -2143,10 +2142,10 @@
+   if (state->isCurPt() && clip != clipNone) {
+     state->clip();
+     if (clip == clipNormal) {
+-      clipHistory->setClip(state->getPath(), clipNormal);
++      clipHistory->setClip(const_cast<GfxPath*>(state->getPath()), clipNormal);
+       builder->clip(state);
+     } else {
+-      clipHistory->setClip(state->getPath(), clipEO);
++      clipHistory->setClip(const_cast<GfxPath*>(state->getPath()), clipEO);
+       builder->clip(state, true);
+     }
+   }
 @@ -2414,7 +2413,7 @@
    int wMode;
    double riseX, riseY;
@@ -42,3 +65,12 @@ Work around constness bugs (addChar should take a const arg).
        state->shift(tdx, tdy);
        p += n;
        len -= n;
+@@ -3112,7 +3112,7 @@
+   state->lineTo(bbox[0], bbox[3]);
+   state->closePath();
+   state->clip();
+-  clipHistory->setClip(state->getPath());
++  clipHistory->setClip(const_cast<GfxPath*>(state->getPath()));
+   builder->clip(state);
+   state->clearPath();
+ 
