568947cbb2ea0a2fb55e49b1de35ae8195a35a9e
pho <pho@pkgsrc.org>
2020-01-11 09:59:02 +0000

    Remove dependency on devel/hs-text
    It's now part of GHC.
    Also contravariant, fail, and semigroups are no longer required as of GHC 8.

--- converters/hs-aeson/buildlink3.mk
+++ converters/hs-aeson/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.19 2020/01/01 02:50:57 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2020/01/11 09:59:02 pho Exp $
 
 BUILDLINK_TREE+=	hs-aeson
 
 .if !defined(HS_AESON_BUILDLINK3_MK)
 HS_AESON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-aeson+=	hs-aeson>=0.7.0
-BUILDLINK_ABI_DEPENDS.hs-aeson+=	hs-aeson>=0.7.0.6nb16
+BUILDLINK_API_DEPENDS.hs-aeson+=	hs-aeson>=1.4.6
+BUILDLINK_ABI_DEPENDS.hs-aeson+=	hs-aeson>=1.4.6.0
 BUILDLINK_PKGSRCDIR.hs-aeson?=		../../converters/hs-aeson
 
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
@@ -16,16 +16,11 @@ BUILDLINK_PKGSRCDIR.hs-aeson?=		../../converters/hs-aeson
 .include "../../devel/hs-primitive/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
-.include "../../devel/hs-text/buildlink3.mk"
 .include "../../devel/hs-th-abstraction/buildlink3.mk"
 .include "../../time/hs-time-compat/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .include "../../devel/hs-uuid-types/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
-.include "../../math/hs-contravariant/buildlink3.mk"
-.include "../../devel/hs-fail/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
-.include "../../devel/hs-transformers-compat/buildlink3.mk"
 .endif	# HS_AESON_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-aeson
