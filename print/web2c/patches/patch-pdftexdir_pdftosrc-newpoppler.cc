5964f7723863c59f238e3b2bf0cc2a6c76a01414
ryoon <ryoon@pkgsrc.org>
2020-01-11 00:31:05 +0000

    web2c: Fix build with poppler-0.84.0

--- print/web2c/patches/patch-pdftexdir_pdftosrc-newpoppler.cc
+++ print/web2c/patches/patch-pdftexdir_pdftosrc-newpoppler.cc
@@ -1,19 +1,40 @@
-$NetBSD: patch-pdftexdir_pdftosrc-newpoppler.cc,v 1.6 2019/05/14 13:23:10 adam Exp $
+$NetBSD: patch-pdftexdir_pdftosrc-newpoppler.cc,v 1.7 2020/01/11 00:31:05 ryoon Exp $
 
 Fix building.
 
 --- pdftexdir/pdftosrc-newpoppler.cc.orig	2018-12-06 23:31:33.000000000 +0000
 +++ pdftexdir/pdftosrc-newpoppler.cc
-@@ -69,7 +69,7 @@ int main(int argc, char *argv[])
-     Stream *s;
-     Object srcStream, srcName, catalogDict;
-     FILE *outfile;
--    char *outname;
-+    const char *outname;
-     int objnum = 0, objgen = 0;
-     bool extract_xref_table = false;
-     int c;
-@@ -110,7 +110,7 @@ int main(int argc, char *argv[])
+@@ -20,8 +20,7 @@ with this program.  If not, see <http://
+ /*
+ This is based on the patch texlive-poppler-0.59.patch <2017-09-19> at
+ https://git.archlinux.org/svntogit/packages.git/plain/texlive-bin/trunk
+-by Arch Linux. The poppler should be 0.59.0 or newer versions.
+-It is tested upto the poppler 0.70.1.
++by Arch Linux. The poppler should be 0.83.0 or newer versions.
+ POPPLER_VERSION should be defined.
+ */
+ 
+@@ -80,7 +79,7 @@ int main(int argc, char *argv[])
+         exit(1);
+     }
+     fileName = new GString(argv[1]);
+-    globalParams = new GlobalParams();
++    globalParams = std::make_unique<GlobalParams>();
+     doc = new PDFDoc(fileName);
+     if (!doc->isOk()) {
+         fprintf(stderr, "Invalid PDF file\n");
+@@ -100,8 +99,8 @@ int main(int argc, char *argv[])
+     srcStream = Object(objNull);
+     if (objnum == 0) {
+         srcStream = catalogDict.dictLookup("SourceObject");
+-        static char const_SourceFile[] = "SourceFile";
+-        if (!srcStream.isStream(const_SourceFile)) {
++        static const char *const_SourceFile = "SourceFile";
++        if (!srcStream.isDict(const_SourceFile)) {
+             fprintf(stderr, "No SourceObject found\n");
+             exit(1);
+         }
+@@ -110,7 +109,7 @@ int main(int argc, char *argv[])
              fprintf(stderr, "No SourceName found\n");
              exit(1);
          }
@@ -22,7 +43,7 @@ Fix building.
          // We cannot free srcName, as objname shares its string.
          // srcName.free();
      } else if (objnum > 0) {
-@@ -119,7 +119,7 @@ int main(int argc, char *argv[])
+@@ -119,7 +118,7 @@ int main(int argc, char *argv[])
              fprintf(stderr, "Not a Stream object\n");
              exit(1);
          }
@@ -31,7 +52,7 @@ Fix building.
          if ((p = strrchr(buf, '.')) == 0)
              p = strchr(buf, 0);
          if (objgen == 0)
-@@ -129,7 +129,7 @@ int main(int argc, char *argv[])
+@@ -129,7 +128,7 @@ int main(int argc, char *argv[])
          outname = buf;
      } else {                    // objnum < 0 means we are extracting the XRef table
          extract_xref_table = true;
@@ -40,12 +61,7 @@ Fix building.
          if ((p = strrchr(buf, '.')) == 0)
              p = strchr(buf, 0);
          sprintf(p, ".xref");
-@@ -157,12 +157,11 @@ int main(int argc, char *argv[])
-                         (e->type == xrefEntryFree ? "f" : "n"));
-             else {              // e->offset is the object number of the object stream
-                 Stream *str;
--                Lexer *lexer;
-                 Parser *parser;
+@@ -162,7 +161,7 @@ int main(int argc, char *argv[])
                  Object objStr, obj1, obj2;
                  int nObjects, first, n;
                  int localOffset = 0;
@@ -66,3 +82,9 @@ Fix building.
                  for (n = 0; n < nObjects; ++n) {
                      obj1 = parser->getObj();
                      obj2 = parser->getObj();
+@@ -204,5 +202,4 @@ int main(int argc, char *argv[])
+         fprintf(stderr, "Cross-reference table extracted to %s\n", outname);
+     fclose(outfile);
+     delete doc;
+-    delete globalParams;
+ }
