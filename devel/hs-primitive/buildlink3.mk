95f928b58b8c310614612102522cd5c7c067b56d
pho <pho@pkgsrc.org>
2020-01-11 07:54:04 +0000

    Remove dependency on devel/hs-mtl
    mtl is now a part of GHC.

--- devel/hs-primitive/buildlink3.mk
+++ devel/hs-primitive/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2019/12/31 18:51:27 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/01/11 08:00:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-primitive
 
 .if !defined(HS_PRIMITIVE_BUILDLINK3_MK)
 HS_PRIMITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.6.4
-BUILDLINK_ABI_DEPENDS.hs-primitive+=	hs-primitive>=0.6.4.0
+BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.7.0
+BUILDLINK_ABI_DEPENDS.hs-primitive+=	hs-primitive>=0.7.0.0
 BUILDLINK_PKGSRCDIR.hs-primitive?=	../../devel/hs-primitive
 .endif	# HS_PRIMITIVE_BUILDLINK3_MK
 
