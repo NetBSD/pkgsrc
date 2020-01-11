66ba8eb49e5b991b0003d304044e4beb34a6160d
pho <pho@pkgsrc.org>
2020-01-11 13:50:07 +0000

    Remove unnecessary .include

--- x11/hs-X11-xft/buildlink3.mk
+++ x11/hs-X11-xft/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2018/03/12 11:15:51 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/01/11 13:50:07 pho Exp $
 
 BUILDLINK_TREE+=	hs-X11-xft
 
@@ -9,9 +9,9 @@ BUILDLINK_API_DEPENDS.hs-X11-xft+=	hs-X11-xft>=0.3.1
 BUILDLINK_ABI_DEPENDS.hs-X11-xft+=	hs-X11-xft>=0.3.1nb4
 BUILDLINK_PKGSRCDIR.hs-X11-xft?=	../../x11/hs-X11-xft
 
-.include "../../x11/libXft/buildlink3.mk"
 .include "../../devel/hs-utf8-string/buildlink3.mk"
 .include "../../x11/hs-X11/buildlink3.mk"
+.include "../../x11/libXft/buildlink3.mk"
 .endif	# HS_X11_XFT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-X11-xft
