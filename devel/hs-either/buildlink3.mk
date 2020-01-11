f2bfaf1f95420e5275d809eba485fdf549737661
pho <pho@pkgsrc.org>
2020-01-11 08:32:25 +0000

    Remove dependency on devel/hs-mtl
    mtl is now a part of GHC.
    Also hs-semigroups is no longer required as of GHC 8.

--- devel/hs-either/buildlink3.mk
+++ devel/hs-either/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/03 05:21:34 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/11 08:32:25 pho Exp $
 
 BUILDLINK_TREE+=	hs-either
 
@@ -7,13 +7,11 @@ HS_EITHER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-either+=	hs-either>=5.0.1
 BUILDLINK_ABI_DEPENDS.hs-either+=	hs-either>=5.0.1.1
-BUILDLINK_PKGSRCDIR.hs-either?=	../../devel/hs-either
+BUILDLINK_PKGSRCDIR.hs-either?=		../../devel/hs-either
 
 .include "../../math/hs-bifunctors/buildlink3.mk"
-.include "../../devel/hs-mtl/buildlink3.mk"
 .include "../../math/hs-profunctors/buildlink3.mk"
 .include "../../math/hs-semigroupoids/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .endif	# HS_EITHER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-either
