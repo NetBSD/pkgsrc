4454f433a8806c84277529c9d142b255a53a2f23
pho <pho@pkgsrc.org>
2020-01-11 11:15:53 +0000

    Remove dependency on devel/hs-text
    It's now part of GHC.
    Also contravariant, fail, and semigroups are no longer required as of GHC 8.

--- textproc/hs-polyparse/buildlink3.mk
+++ textproc/hs-polyparse/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2020/01/02 03:21:09 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/01/11 11:21:14 pho Exp $
 
 BUILDLINK_TREE+=	hs-polyparse
 
@@ -8,9 +8,6 @@ HS_POLYPARSE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-polyparse+=	hs-polyparse>=1.13
 BUILDLINK_ABI_DEPENDS.hs-polyparse+=	hs-polyparse>=1.13
 BUILDLINK_PKGSRCDIR.hs-polyparse?=	../../textproc/hs-polyparse
-
-.include "../../devel/hs-fail/buildlink3.mk"
-.include "../../devel/hs-text/buildlink3.mk"
 .endif	# HS_POLYPARSE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-polyparse
--- textproc/hs-tagsoup/buildlink3.mk
+++ textproc/hs-tagsoup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2020/01/02 15:19:00 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2020/01/11 11:23:40 pho Exp $
 
 BUILDLINK_TREE+=	hs-tagsoup
 
@@ -8,8 +8,6 @@ HS_TAGSOUP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.14.8
 BUILDLINK_ABI_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.14.8
 BUILDLINK_PKGSRCDIR.hs-tagsoup?=	../../textproc/hs-tagsoup
-
-.include "../../devel/hs-text/buildlink3.mk"
 .endif	# HS_TAGSOUP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-tagsoup
