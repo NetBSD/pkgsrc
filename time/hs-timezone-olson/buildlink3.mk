cf3158fb909ca656bb74fbdfae9f8143a07dc507
pho <pho@pkgsrc.org>
2020-01-11 11:52:48 +0000

    Update to timezone-olson-0.1.9
    No changelogs are provided by upstream.

--- time/hs-timezone-olson/buildlink3.mk
+++ time/hs-timezone-olson/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/01/07 13:04:36 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/01/11 11:52:48 pho Exp $
 
 BUILDLINK_TREE+=	hs-timezone-olson
 
 .if !defined(HS_TIMEZONE_OLSON_BUILDLINK3_MK)
 HS_TIMEZONE_OLSON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-timezone-olson+=	hs-timezone-olson>=0.1.7
-BUILDLINK_ABI_DEPENDS.hs-timezone-olson+=	hs-timezone-olson>=0.1.7
+BUILDLINK_API_DEPENDS.hs-timezone-olson+=	hs-timezone-olson>=0.1.9
+BUILDLINK_ABI_DEPENDS.hs-timezone-olson+=	hs-timezone-olson>=0.1.9
 BUILDLINK_PKGSRCDIR.hs-timezone-olson?=		../../time/hs-timezone-olson
 
 .include "../../devel/hs-extensible-exceptions/buildlink3.mk"
