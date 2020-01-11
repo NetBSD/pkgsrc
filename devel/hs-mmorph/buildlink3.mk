b7b7c6fa4d293fe403e2b5ca6bf7170d73b7cd2b
pho <pho@pkgsrc.org>
2020-01-11 08:20:36 +0000

    Remove dependency on devel/hs-mtl
    mtl is now a part of GHC.

--- devel/hs-mmorph/buildlink3.mk
+++ devel/hs-mmorph/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2020/01/01 04:35:58 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/01/11 08:20:36 pho Exp $
 
 BUILDLINK_TREE+=	hs-mmorph
 
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.1.3
 BUILDLINK_ABI_DEPENDS.hs-mmorph+=	hs-mmorph>=1.1.3
 BUILDLINK_PKGSRCDIR.hs-mmorph?=		../../devel/hs-mmorph
 
-.include "../../devel/hs-mtl/buildlink3.mk"
 .include "../../devel/hs-transformers-compat/buildlink3.mk"
 .endif	# HS_MMORPH_BUILDLINK3_MK
 
