a1de53e00c0d8deb11e4d310465360827328d6d0
pho <pho@pkgsrc.org>
2020-01-11 09:00:30 +0000

    Remove dependency on deve/hs-stm
    It's now part of GHC.

--- devel/hs-exceptions/buildlink3.mk
+++ devel/hs-exceptions/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/01/11 08:09:43 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/01/11 09:00:30 pho Exp $
 
 BUILDLINK_TREE+=	hs-exceptions
 
@@ -8,8 +8,6 @@ HS_EXCEPTIONS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-exceptions+=	hs-exceptions>=0.10.4
 BUILDLINK_ABI_DEPENDS.hs-exceptions+=	hs-exceptions>=0.10.4
 BUILDLINK_PKGSRCDIR.hs-exceptions?=	../../devel/hs-exceptions
-
-.include "../../devel/hs-stm/buildlink3.mk"
 .endif	# HS_EXCEPTIONS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-exceptions
