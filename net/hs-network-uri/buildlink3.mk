a71e16e5b8a469c98945a7e3cf6c510128436f98
pho <pho@pkgsrc.org>
2020-01-11 10:08:28 +0000

    Remove dependency on devel/hs-parsec
    It's now part of GHC.

--- net/hs-network-uri/buildlink3.mk
+++ net/hs-network-uri/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/02 15:27:23 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/11 10:08:28 pho Exp $
 
 BUILDLINK_TREE+=	hs-network-uri
 
@@ -8,8 +8,6 @@ HS_NETWORK_URI_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-network-uri+=	hs-network-uri>=2.6.1
 BUILDLINK_ABI_DEPENDS.hs-network-uri+=	hs-network-uri>=2.6.1.0
 BUILDLINK_PKGSRCDIR.hs-network-uri?=	../../net/hs-network-uri
-
-.include "../../textproc/hs-parsec/buildlink3.mk"
 .endif	# HS_NETWORK_URI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-network-uri
