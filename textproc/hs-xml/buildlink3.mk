61af2699d4ca5349c3293993b6e26982798bda33
pho <pho@pkgsrc.org>
2020-01-11 09:19:08 +0000

    Remove dependency on devel/hs-text
    It's now part of GHC.

--- textproc/hs-xml/buildlink3.mk
+++ textproc/hs-xml/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2020/01/02 14:22:53 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/01/11 09:19:08 pho Exp $
 
 BUILDLINK_TREE+=	hs-xml
 
@@ -8,8 +8,6 @@ HS_XML_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-xml+=	hs-xml>=1.3.14
 BUILDLINK_ABI_DEPENDS.hs-xml+=	hs-xml>=1.3.14
 BUILDLINK_PKGSRCDIR.hs-xml?=	../../textproc/hs-xml
-
-.include "../../devel/hs-text/buildlink3.mk"
 .endif	# HS_XML_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-xml
