180e4ce97111fb865b57ba40fb66bb31f635415a
pho <pho@pkgsrc.org>
2020-01-11 13:08:35 +0000

    Add missing dependency on devel/hs-colour

--- devel/hs-ansi-terminal/buildlink3.mk
+++ devel/hs-ansi-terminal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/01/07 13:04:03 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/01/11 13:08:35 pho Exp $
 
 BUILDLINK_TREE+=	hs-ansi-terminal
 
@@ -8,6 +8,8 @@ HS_ANSI_TERMINAL_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.6.2
 BUILDLINK_ABI_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.6.2.1
 BUILDLINK_PKGSRCDIR.hs-ansi-terminal?=		../../devel/hs-ansi-terminal
+
+.include "../../devel/hs-colour/buildlink3.mk"
 .endif	# HS_ANSI_TERMINAL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-ansi-terminal
