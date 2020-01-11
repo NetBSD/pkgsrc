ac53cad86e5253fc6ae78d25597c59f6b2cd7130
pho <pho@pkgsrc.org>
2020-01-11 09:31:04 +0000

    Remove dependency on devel/hs-text
    It's now part of GHC.
    Also fail and semigroups are no longer required as of GHC 8.

--- devel/hs-hashable/buildlink3.mk
+++ devel/hs-hashable/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2019/12/31 12:47:44 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2020/01/11 09:31:04 pho Exp $
 
 BUILDLINK_TREE+=	hs-hashable
 
@@ -8,8 +8,6 @@ HS_HASHABLE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.3.0
 BUILDLINK_ABI_DEPENDS.hs-hashable+=	hs-hashable>=1.3.0.0
 BUILDLINK_PKGSRCDIR.hs-hashable?=	../../devel/hs-hashable
-
-.include "../../devel/hs-text/buildlink3.mk"
 .endif	# HS_HASHABLE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-hashable
