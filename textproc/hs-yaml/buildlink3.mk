ff00f8fbd883867561d38f151699830ebb749655
pho <pho@pkgsrc.org>
2020-01-11 11:08:49 +0000

    Update to yaml-0.11.2.0
    0.11.2.0
    * Reduces some of the code duplication between the encode and
      encodePretty functions
    * The output of encodePretty has been improved:
      * Multiline strings now use Literal style instead of SingleQuoted
      * Special keys are now quoted in mappings #179
    * Support for complex keys in mappings: #182
      * Adds complexMapping function to Data.Yaml.Builder
      * Decode functions now return a NonStringKey error when attempting
        to decode a mapping with a complex key as it is not possible to
        decode these to an Aeson Value
    * Adds missing ToYaml instances
    
    0.11.1.2
    * Compiles with GHC 8.8.1 (MonadFail split)
    
    0.11.1.1
    * Use the appropriate Scientific rendering function to avoid a memory
      overflow when rendering. The previously used function from aeson
      would not use scientific notation, and could use large amounts of
      memory for values such as 1e9999999999999.
    
    0.11.1.0
    * Better error messages in the Data.Yaml.Config module #168
    * Add LoadSettingsException exception and remove error printing from
      loadYamlSettings #172
    
    0.11.0.0
    * Split out the libyaml and Text.Libyaml code into its own
      package. #145
    
    0.10.4.0
    * Add decodeMarked and decodeFileMarked functions to Text.Libyaml, and
      extend native bindings to extract mark information. #157
    
    0.10.3.0
    * Add support for anchors and aliases to Data.Yaml.Builder #155
    * Fix test suite for 32 bit machines #158
    
    0.10.2.0
    * Add EncodeOptions and FormatOptions to control the style of the
      encoded YAML. #153
    * Default to using literal style for multiline strings #152
    
    0.10.1.1
    * Correctly declare libyaml dependency on system-libyaml flag #151
    
    0.10.1
    * Avoid incurring a semigroups dependency on recent GHCs.
    * Fix a space leak that was introduced with 0.10.0 #147
    
    0.10.0
    * Add decodeFileWithWarnings which returns warnings for duplicate
      fields
    
    0.9.0
    * Expose style and tags on mappings and sequences in Text.Libyaml #141
    
    0.8.32
    
    * Escape keys as necessary #137
    * Support hexadecimal and octal number values #135
    * More resilient isNumeric (should reduce cases of unneeded quoting)
    * hpackify
    * src subdir
    
    0.8.31.1
    * Add a workaround for a cabal bug haskell-infra/hackage-trustees#165
    
    0.8.31
    * Add decodeThrow and decodeFileThrow convenience functions.
    * Upgrade libyaml versions
    * Deprecate decode and decodeEither

--- textproc/hs-yaml/buildlink3.mk
+++ textproc/hs-yaml/buildlink3.mk
@@ -1,24 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/03 08:03:05 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/11 11:08:49 pho Exp $
 
 BUILDLINK_TREE+=	hs-yaml
 
 .if !defined(HS_YAML_BUILDLINK3_MK)
 HS_YAML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-yaml+=	hs-yaml>=0.8.30
-BUILDLINK_ABI_DEPENDS.hs-yaml+=	hs-yaml>=0.8.30
+BUILDLINK_API_DEPENDS.hs-yaml+=	hs-yaml>=0.11.2
+BUILDLINK_ABI_DEPENDS.hs-yaml+=	hs-yaml>=0.11.2.0
 BUILDLINK_PKGSRCDIR.hs-yaml?=	../../textproc/hs-yaml
 
 .include "../../converters/hs-aeson/buildlink3.mk"
 .include "../../textproc/hs-attoparsec/buildlink3.mk"
 .include "../../devel/hs-conduit/buildlink3.mk"
+.include "../../textproc/hs-libyaml/buildlink3.mk"
 .include "../../devel/hs-resourcet/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
-.include "../../devel/hs-text/buildlink3.mk"
-.include "../../devel/hs-unordered-containers/buildlink3.mk"
 .include "../../devel/hs-vector/buildlink3.mk"
-.include "../../textproc/libyaml/buildlink3.mk"
 .endif	# HS_YAML_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-yaml
