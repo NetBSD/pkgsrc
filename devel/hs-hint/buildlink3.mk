56077152fd1aada1dcec92c69fab459178f88f5f
pho <pho@pkgsrc.org>
2020-01-11 08:18:47 +0000

    Add missing dependency on sysutils/hs-temporary

--- devel/hs-hint/buildlink3.mk
+++ devel/hs-hint/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2020/01/11 08:15:58 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/01/11 08:18:47 pho Exp $
 
 BUILDLINK_TREE+=	hs-hint
 
@@ -12,6 +12,7 @@ BUILDLINK_PKGSRCDIR.hs-hint?=	../../devel/hs-hint
 .include "../../devel/hs-exceptions/buildlink3.mk"
 .include "../../devel/hs-ghc-paths/buildlink3.mk"
 .include "../../devel/hs-random/buildlink3.mk"
+.include "../../sysutils/hs-temporary/buildlink3.mk"
 .endif	# HS_HINT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-hint
