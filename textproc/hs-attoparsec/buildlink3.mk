6ca375a84b1c0806143e91189b9bf095488b5c6f
pho <pho@pkgsrc.org>
2020-01-11 09:31:04 +0000

    Remove dependency on devel/hs-text
    It's now part of GHC.
    Also fail and semigroups are no longer required as of GHC 8.

--- textproc/hs-attoparsec/buildlink3.mk
+++ textproc/hs-attoparsec/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2019/12/31 19:00:39 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2020/01/11 09:34:21 pho Exp $
 
 BUILDLINK_TREE+=	hs-attoparsec
 
@@ -9,10 +9,7 @@ BUILDLINK_API_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.13.2
 BUILDLINK_ABI_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.13.2.3
 BUILDLINK_PKGSRCDIR.hs-attoparsec?=	../../textproc/hs-attoparsec
 
-.include "../../devel/hs-fail/buildlink3.mk"
-.include "../../math/hs-semigroups/buildlink3.mk"
 .include "../../math/hs-scientific/buildlink3.mk"
-.include "../../devel/hs-text/buildlink3.mk"
 .endif	# HS_ATTOPARSEC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-attoparsec
