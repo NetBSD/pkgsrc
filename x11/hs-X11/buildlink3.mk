75481979177009e4eacbf5690220499ec488e619
pho <pho@pkgsrc.org>
2020-01-11 13:45:37 +0000

    Add missing dependencies on several X11 libraries

--- x11/hs-X11/buildlink3.mk
+++ x11/hs-X11/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/01/02 16:48:30 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/01/11 13:45:37 pho Exp $
 
 BUILDLINK_TREE+=	hs-X11
 
@@ -10,6 +10,11 @@ BUILDLINK_ABI_DEPENDS.hs-X11+=	hs-X11>=1.9.1
 BUILDLINK_PKGSRCDIR.hs-X11?=	../../x11/hs-X11
 
 .include "../../devel/hs-data-default/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXrandr/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXScrnSaver/buildlink3.mk"
+.include "../../x11/libXinerama/buildlink3.mk"
 .endif	# HS_X11_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-X11
