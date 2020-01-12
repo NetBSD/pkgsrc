efd9abac6954cdfabd98cafba4db77d18dfe13fc
pho <pho@pkgsrc.org>
2020-01-12 08:13:40 +0000

    Fix a build failure that happens when alex is not installed
    Also added a bookit for Darwin/x86_64.

--- lang/ghc88/bootstrap.mk
+++ lang/ghc88/bootstrap.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bootstrap.mk,v 1.1 2020/01/09 12:32:24 pho Exp $
+# $NetBSD: bootstrap.mk,v 1.2 2020/01/12 08:13:40 pho Exp $
 # -----------------------------------------------------------------------------
 # Select a bindist of bootstrapping compiler on a per-platform basis.
 #
@@ -30,6 +30,12 @@
 #BOOT_VERSION:=	8.4.4
 #BOOT_ARCHIVE:=	ghc-${BOOT_VERSION}-boot-powerpc-apple-darwin.tar.xz
 #DISTFILES:=	${DISTFILES} ${BOOT_ARCHIVE} # Available in LOCAL_PORTS
+.endif
+
+.if !empty(MACHINE_PLATFORM:MDarwin-*-x86_64) || make(distinfo) || make (makesum) || make(mdi)
+BOOT_VERSION:=	8.4.4
+BOOT_ARCHIVE:=	ghc-${BOOT_VERSION}-boot-x86_64-apple-darwin.tar.xz
+DISTFILES:=	${DISTFILES} ${BOOT_ARCHIVE} # Available in LOCAL_PORTS
 .endif
 
 .if !empty(MACHINE_PLATFORM:MFreeBSD-*-i386) || make(distinfo) || make (makesum) || make(mdi)
