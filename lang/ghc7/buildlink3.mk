242c0e8d5aeeba778fdea72197c302491b28ae49
pho <pho@pkgsrc.org>
2020-01-13 01:06:52 +0000

    Update the bootkit for FreeBSD/i386
    It's now built on FreeBSD 12.1.

--- lang/ghc7/buildlink3.mk
+++ lang/ghc7/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2019/12/29 16:59:08 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/01/13 01:06:52 pho Exp $
 
 BUILDLINK_TREE+=	ghc
 
@@ -8,12 +8,6 @@ GHC_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.ghc+=	ghc>=7.10.3
 BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=7.10.3
 BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc7
-
-# On FreeBSD we need pkgsrc libiconv. See bootstrap.mk for details.
-.include "../../mk/bsd.fast.prefs.mk"
-.if ${OPSYS} == "FreeBSD"
-USE_BUILTIN.iconv=	no
-.endif
 
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/libffi/buildlink3.mk"
