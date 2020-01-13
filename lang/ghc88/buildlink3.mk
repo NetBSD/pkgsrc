df150d757240180304f4141e9cd814ccea9a7600
pho <pho@pkgsrc.org>
2020-01-13 15:42:47 +0000

    Use 8.4.4 bootkit for FreeBSD/i386

--- lang/ghc88/buildlink3.mk
+++ lang/ghc88/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/09 12:32:24 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/13 15:42:47 pho Exp $
 
 BUILDLINK_TREE+=	ghc
 
@@ -8,12 +8,6 @@ GHC_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.ghc+=	ghc>=8.8.1
 BUILDLINK_ABI_DEPENDS.ghc+=	ghc>=8.8.1
 BUILDLINK_PKGSRCDIR.ghc?=	../../lang/ghc88
-
-# On FreeBSD we need pkgsrc libiconv. See bootstrap.mk for details.
-.include "../../mk/bsd.fast.prefs.mk"
-.if ${OPSYS} == "FreeBSD"
-USE_BUILTIN.iconv=	no
-.endif
 
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/libffi/buildlink3.mk"
