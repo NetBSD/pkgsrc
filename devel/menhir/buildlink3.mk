c8ac2492a07a26cbaf29810a61200c00fd8fc605
jaapb <jaapb@pkgsrc.org>
2020-01-14 22:04:26 +0000

    Updated devel/menhir to version 20190924.
    It mostly contains minor changes and bugfixes.

--- devel/menhir/buildlink3.mk
+++ devel/menhir/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/21 12:18:56 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/14 22:04:26 jaapb Exp $
 
 BUILDLINK_TREE+=	menhir
 
 .if !defined(MENHIR_BUILDLINK3_MK)
 MENHIR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.menhir+=	menhir>=20180530
+BUILDLINK_API_DEPENDS.menhir+=	menhir>=20190626
+BUILDLINK_ABI_DEPENDS.menhir+=	menhir>=20190924
 BUILDLINK_PKGSRCDIR.menhir?=	../../devel/menhir
 
 .endif	# MENHIR_BUILDLINK3_MK
