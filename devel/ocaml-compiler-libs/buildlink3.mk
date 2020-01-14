061b927039b6af809436cc0d2d20032eb2ce2ce0
jaapb <jaapb@pkgsrc.org>
2020-01-14 22:14:42 +0000

    Updated devel/ocaml-compiler-libs to version 0.12.0.
    This mostly involves changing the build system from jbuilder to dune.

--- devel/ocaml-compiler-libs/buildlink3.mk
+++ devel/ocaml-compiler-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2019/03/05 17:38:30 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/01/14 22:14:42 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-compiler-libs
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-compiler-libs
 OCAML_COMPILER_LIBS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-compiler-libs+=	ocaml-compiler-libs>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-compiler-libs+=	ocaml-compiler-libs>=0.11.0nb4
+BUILDLINK_ABI_DEPENDS.ocaml-compiler-libs+=	ocaml-compiler-libs>=0.12.1
 BUILDLINK_PKGSRCDIR.ocaml-compiler-libs?=	../../devel/ocaml-compiler-libs
 .endif	# OCAML_COMPILER_LIBS_BUILDLINK3_MK
 
