5fd76195ff581d280b0848eb3ffeb8f85fac1219
jaapb <jaapb@pkgsrc.org>
2020-01-14 20:29:07 +0000

    Update databases/pgocaml to version 4.2.
    Changes include:
    - support for type hints
    - support for custom converters
    - package uses Dune now
    - camlp4 replaced by ppx

--- databases/pgocaml/buildlink3.mk
+++ databases/pgocaml/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2019/07/04 09:18:37 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/01/14 20:29:07 jaapb Exp $
 
 BUILDLINK_TREE+=	pgocaml
 
 .if !defined(PGOCAML_BUILDLINK3_MK)
 PGOCAML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.pgocaml+=	pgocaml>=3.0
-BUILDLINK_ABI_DEPENDS.pgocaml+=	pgocaml>=3.2nb1
+BUILDLINK_API_DEPENDS.pgocaml+=	pgocaml>=4.0
+BUILDLINK_ABI_DEPENDS.pgocaml+=	pgocaml>=4.2
 BUILDLINK_PKGSRCDIR.pgocaml?=	../../databases/pgocaml
 
 .include "../../devel/ocaml-re/buildlink3.mk"
