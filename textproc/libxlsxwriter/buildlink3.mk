e480a5a941842eede348ed0bcdf0f6083ffba639
sjmulder <sjmulder@pkgsrc.org>
2020-01-15 14:36:07 +0000

    libxlsxwriter: Update to 0.9.3
    Changes in 0.9.3:
     - Fix Xcode/Cocoapods build warnings.
    
    Changes in 0.9.2:
     - Added support for writing cell comments, see working_with_comments.
    
    Changes in 0.9.2 but unlisted in changelog:
     - Makefile now respects DESTDIR and PREFIX, including when setting
       rpath on macOS.
     - Removal of deprecated functions.
     - Naming changes in bundled MD5 code to avoid conflicts with OpenSSL.
     - Warning fixes (int sizes etc).
     - __atribute__((deprecated)) not used on GCC < 5.
     - Fix for vba_name allowing illegal worksheet codename.
    
    Package changes:
     - No longer set now unused INSTALL_DIR since DESTDIR and PREFIX are
       now respected.
     - The macOS $(PREFIX) patch has been merged upstream.

--- textproc/libxlsxwriter/buildlink3.mk
+++ textproc/libxlsxwriter/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2019/12/30 12:35:42 sjmulder Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/01/15 14:36:07 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxlsxwriter
 
 .if !defined(LIBXLSXWRITER_BUILDLINK3_MK)
 LIBXLSXWRITER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=0.9.1
+BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=0.9.3
 BUILDLINK_PKGSRCDIR.libxlsxwriter?=	../../textproc/libxlsxwriter
 
 .include "../../devel/zlib/buildlink3.mk"
