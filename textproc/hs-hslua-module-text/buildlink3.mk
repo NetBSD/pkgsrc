0ae5920dc31f723e68bc1035cd89af0e80717498
pho <pho@pkgsrc.org>
2020-01-15 15:51:22 +0000

    Import hslua-module-text-0.2.1
    UTF-8 aware subset of Lua's string module.

---
+++ textproc/hs-hslua-module-text/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/15 15:51:22 pho Exp $
+
+BUILDLINK_TREE+=	hs-hslua-module-text
+
+.if !defined(HS_HSLUA_MODULE_TEXT_BUILDLINK3_MK)
+HS_HSLUA_MODULE_TEXT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hslua-module-text+=	hs-hslua-module-text>=0.2.1
+BUILDLINK_ABI_DEPENDS.hs-hslua-module-text+=	hs-hslua-module-text>=0.2.1
+BUILDLINK_PKGSRCDIR.hs-hslua-module-text?=	../../textproc/hs-hslua-module-text
+
+.include "../../lang/hs-hslua/buildlink3.mk"
+.endif	# HS_HSLUA_MODULE_TEXT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hslua-module-text
