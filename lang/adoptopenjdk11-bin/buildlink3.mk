f0f0852c3e7d8d6b47e65c1bd672c5cfdebffa16
ryoon <ryoon@pkgsrc.org>
2020-01-15 15:35:35 +0000

    adoptopenjdk11-bin: Regenerate buildlink3.mk
    * Fix side effect of my mistake in PKGNAME.

--- lang/adoptopenjdk11-bin/buildlink3.mk
+++ lang/adoptopenjdk11-bin/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/13 01:38:42 ryoon Exp $
-BUILDLINK_TREE+=	adoptopenjdk11
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/15 15:35:35 ryoon Exp $
 
-.if !defined(ADOPTOPENJDK11_BUILDLINK3_MK)
-ADOPTOPENJDK11_BUILDLINK3_MK:=
+BUILDLINK_TREE+=	adoptopenjdk11-bin
 
-BUILDLINK_API_DEPENDS.adoptopenjdk11+=	adoptopenjdk11>=11.0.5.10
-BUILDLINK_PKGSRCDIR.adoptopenjdk11?=	../../lang/adoptopenjdk11-bin
+.if !defined(ADOPTOPENJDK11_BIN_BUILDLINK3_MK)
+ADOPTOPENJDK11_BIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.adoptopenjdk11-bin+=	adoptopenjdk11-bin>=11.0.5.10
+BUILDLINK_PKGSRCDIR.adoptopenjdk11-bin?=	../../lang/adoptopenjdk11-bin
 
 BUILDLINK_JAVA_PREFIX.adoptopenjdk11-bin=	${PREFIX}/java/adoptopenjdk11-bin
-.endif	# ADOPTOPENJDK11_BUILDLINK3_MK
+.endif	# ADOPTOPENJDK11_BIN_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-adoptopenjdk11
+BUILDLINK_TREE+=	-adoptopenjdk11-bin
