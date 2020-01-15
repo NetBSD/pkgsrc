d7472334a6a25d7476618d1eaca66f95cdc4eb7f
pho <pho@pkgsrc.org>
2020-01-15 06:04:21 +0000

    Import aeson-pretty-0.8.8
    A JSON pretty-printing library compatible with aeson as well as a
    command-line tool to improve readabilty of streams of JSON data.
    
    The library provides the function "encodePretty". It is a drop-in
    replacement for aeson's "encode" function, producing JSON-ByteStrings
    for human readers.
    
    The command-line tool reads JSON from stdin and writes prettified JSON
    to stdout. It also offers a complementary "compact"-mode, essentially
    the opposite of pretty-printing.

---
+++ converters/hs-aeson-pretty/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/15 06:04:21 pho Exp $
+
+BUILDLINK_TREE+=	hs-aeson-pretty
+
+.if !defined(HS_AESON_PRETTY_BUILDLINK3_MK)
+HS_AESON_PRETTY_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-aeson-pretty+=	hs-aeson-pretty>=0.8.8
+BUILDLINK_ABI_DEPENDS.hs-aeson-pretty+=	hs-aeson-pretty>=0.8.8
+BUILDLINK_PKGSRCDIR.hs-aeson-pretty?=	../../converters/hs-aeson-pretty
+
+.include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-base-compat/buildlink3.mk"
+.include "../../math/hs-scientific/buildlink3.mk"
+.include "../../devel/hs-unordered-containers/buildlink3.mk"
+.include "../../devel/hs-vector/buildlink3.mk"
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../devel/hs-cmdargs/buildlink3.mk"
+.endif	# HS_AESON_PRETTY_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-aeson-pretty
