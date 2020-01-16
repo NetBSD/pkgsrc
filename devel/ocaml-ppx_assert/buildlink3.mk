5eb1116550d4ef7491ceafead949d249cc8286bd
jaapb <jaapb@pkgsrc.org>
2020-01-16 13:23:05 +0000

    Updated devel/ocaml-ppx_assert to version 0.13.0.
    The changelog does not seem to have been updated recently, but it does
    not look like there have been any major changes.

--- devel/ocaml-ppx_assert/buildlink3.mk
+++ devel/ocaml-ppx_assert/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/11/03 10:39:24 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/01/16 13:23:05 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_assert
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_assert
 OCAML_PPX_ASSERT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_assert+=	ocaml-ppx_assert>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_assert+=	ocaml-ppx_assert>=0.13.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_assert?=		../../devel/ocaml-ppx_assert
 
 .endif	# OCAML_PPX_ASSERT_BUILDLINK3_MK
