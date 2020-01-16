7d4109c43a2b39729be7de9e3ac2f8b8dd9863c8
jaapb <jaapb@pkgsrc.org>
2020-01-16 14:28:31 +0000

    Updated devel/ocaml-ppx_module_timer to version 0.13.0.
    The changelog has not been updated, but there do not seem to have been
    any major changes.

--- devel/ocaml-ppx_module_timer/buildlink3.mk
+++ devel/ocaml-ppx_module_timer/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/13 11:53:56 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/16 14:28:31 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_module_timer
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_module_timer
 OCAML_PPX_MODULE_TIMER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_module_timer+=	ocaml-ppx_module_timer>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_module_timer+=	ocaml-ppx_module_timer>=0.13.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_module_timer?=	../../devel/ocaml-ppx_module_timer
 
 .endif	# OCAML_PPX_MODULE_TIMER_BUILDLINK3_MK
