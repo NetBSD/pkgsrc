cede9693eef3c35f93d697d9e527353625121fac
jaapb <jaapb@pkgsrc.org>
2020-01-16 13:37:46 +0000

    Updated devel/ocaml-ppx_variants_conv to version 0.13.0.
    The changelog has not been updated, but this version definitely adds
    support for OCaml 4.09.

--- devel/ocaml-ppx_variants_conv/buildlink3.mk
+++ devel/ocaml-ppx_variants_conv/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/13 11:03:16 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/16 13:37:46 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_variants_conv
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_variants_conv
 OCAML_PPX_VARIANTS_CONV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_variants_conv+=	ocaml-ppx_variants_conv>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_variants_conv+=	ocaml-ppx_variants_conv>=0.13.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_variants_conv?=	../../devel/ocaml-ppx_variants_conv
 
 .endif	# OCAML_PPX_VARIANTS_CONV_BUILDLINK3_MK
