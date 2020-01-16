af94bc90c284d805d2fa284a352ff807a3a9ca4d
jaapb <jaapb@pkgsrc.org>
2020-01-16 13:27:56 +0000

    Updated devel/ocaml-splittable_random to version 0.13.0.
    The changelog has not been updated, but it does not look like there have
    been any major changes.

--- devel/ocaml-splittable_random/buildlink3.mk
+++ devel/ocaml-splittable_random/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/12 18:55:23 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/16 13:27:56 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-splittable_random
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-splittable_random
 OCAML_SPLITTABLE_RANDOM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-splittable_random+=	ocaml-splittable_random>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-splittable_random+=	ocaml-splittable_random>=0.13.0
 BUILDLINK_PKGSRCDIR.ocaml-splittable_random?=	../../devel/ocaml-splittable_random
 
 .endif	# OCAML_SPLITTABLE_RANDOM_BUILDLINK3_MK
