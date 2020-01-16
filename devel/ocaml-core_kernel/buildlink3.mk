bdce7555ec175ad7d8ca9234ff2c337775b8bde9
jaapb <jaapb@pkgsrc.org>
2020-01-16 15:31:15 +0000

    Updated devel/ocaml-core_kernel to version 0.13.0.
    This version mostly includes minor improvements and bugfixes. See the
    changelog for more details.

--- devel/ocaml-core_kernel/buildlink3.mk
+++ devel/ocaml-core_kernel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2019/11/03 10:39:22 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/01/16 15:31:15 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-core_kernel
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-core_kernel
 OCAML_CORE_KERNEL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-core_kernel+=	ocaml-core_kernel>=0.12.2
+BUILDLINK_ABI_DEPENDS.ocaml-core_kernel+=	ocaml-core_kernel>=0.13.0
 BUILDLINK_PKGSRCDIR.ocaml-core_kernel?=		../../devel/ocaml-core_kernel
 
 .endif	# OCAML_CORE_KERNEL_BUILDLINK3_MK
