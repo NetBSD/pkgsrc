b30d701cf27eb0ef180143ec2edc9f309882c583
pho <pho@pkgsrc.org>
2020-01-16 11:44:45 +0000

    Import mime-types-0.1.0.9
    Basic mime-type handling types and functions

---
+++ net/hs-mime-types/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/16 11:44:45 pho Exp $
+
+BUILDLINK_TREE+=	hs-mime-types
+
+.if !defined(HS_MIME_TYPES_BUILDLINK3_MK)
+HS_MIME_TYPES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-mime-types+=	hs-mime-types>=0.1.0
+BUILDLINK_ABI_DEPENDS.hs-mime-types+=	hs-mime-types>=0.1.0.9
+BUILDLINK_PKGSRCDIR.hs-mime-types?=	../../net/hs-mime-types
+.endif	# HS_MIME_TYPES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-mime-types
