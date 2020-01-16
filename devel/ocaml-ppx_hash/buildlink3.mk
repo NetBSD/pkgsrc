5a6447b7a071b57ec290f702e5b75123e2f75e2b
jaapb <jaapb@pkgsrc.org>
2020-01-16 10:07:40 +0000

    Updated devel/ocaml-ppx_hash to version 0.13.0.
    The changelog doesn't seem to have been updated recently, but this
    version definitely adds support for OCaml 4.09.

--- devel/ocaml-ppx_hash/buildlink3.mk
+++ devel/ocaml-ppx_hash/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2019/03/05 18:08:00 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/01/16 10:07:40 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_hash
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_hash
 OCAML_PPX_HASH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_hash+=	ocaml-ppx_hash>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_hash+=	ocaml-ppx_hash>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_hash+=	ocaml-ppx_hash>=0.13.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_hash?=	../../devel/ocaml-ppx_hash
 
 .endif	# OCAML_PPX_HASH_BUILDLINK3_MK
