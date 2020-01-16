325faf8c94317028893b30eccf14781cc989af7f
jperkin <jperkin@pkgsrc.org>
2020-01-16 13:33:50 +0000

    *: Remove USE_OLD_DES_API.
    OpenSSL 1.1.1d no longer ships des_old.h, and the time for this being
    necessary appears to be behind us.

--- net/tcpdump/options.mk
+++ net/tcpdump/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2017/09/08 11:10:41 wiz Exp $
+# $NetBSD: options.mk,v 1.5 2020/01/16 13:33:51 jperkin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tcpdump
 PKG_SUPPORTED_OPTIONS=	ssl libsmi
@@ -7,7 +7,6 @@ PKG_SUGGESTED_OPTIONS=	ssl
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mssl)
-USE_OLD_DES_API=	yes
 .include "../../security/openssl/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-crypto
