adead5c80add861665c0708b2d1f8fedc4d5d6d7
jaapb <jaapb@pkgsrc.org>
2020-01-16 09:09:20 +0000

    Updated devel/ocaml-ppx_compare to version 0.13.0.
    The changelog doesn't seem to have been updated in a while, but this
    version definitely adds support for OCaml 4.09.

--- devel/ocaml-ppx_compare/buildlink3.mk
+++ devel/ocaml-ppx_compare/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2019/03/05 17:59:40 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2020/01/16 09:09:20 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_compare
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_compare
 OCAML_PPX_COMPARE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_compare+=	ocaml-ppx_compare>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_compare+=	ocaml-ppx_compare>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_compare+=	ocaml-ppx_compare>=0.13.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_compare?=		../../devel/ocaml-ppx_compare
 .endif	# OCAML_PPX_COMPARE_BUILDLINK3_MK
 
