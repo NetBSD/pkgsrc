c72fb0b966eb3723c256076e8fc636382f45c213
jaapb <jaapb@pkgsrc.org>
2020-01-16 12:44:03 +0000

    Updated devel/ocaml-stdio to version 0.13.0.
    The changelog doesn't seem to have been updated recently, but there do
    not seem to have been anything but minor improvements and bugfixes.

--- devel/ocaml-stdio/buildlink3.mk
+++ devel/ocaml-stdio/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2019/03/05 16:21:31 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/01/16 12:44:03 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-stdio
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-stdio
 OCAML_STDIO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-stdio+=	ocaml-stdio>=0.9.0
-BUILDLINK_ABI_DEPENDS.ocaml-stdio+=	ocaml-stdio>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-stdio+=	ocaml-stdio>=0.13.0
 BUILDLINK_PKGSRCDIR.ocaml-stdio?=	../../devel/ocaml-stdio
 .endif	# OCAML_STDIO_BUILDLINK3_MK
 
