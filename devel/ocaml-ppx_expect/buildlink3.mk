1d6fc8b55e97db43ffe06cbe1daaf799cbbdeb2e
jaapb <jaapb@pkgsrc.org>
2020-01-16 14:20:56 +0000

    Updated devel/ocaml-ppx_expect to version 0.13.0.
    The changelog has not been updated; there do not seem to have been any
    major changes.

--- devel/ocaml-ppx_expect/buildlink3.mk
+++ devel/ocaml-ppx_expect/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/11/03 10:39:24 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/01/16 14:20:56 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_expect
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_expect
 OCAML_PPX_EXPECT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_expect+=	ocaml-ppx_expect>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_expect+=	ocaml-ppx_expect>=0.13.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_expect?=		../../devel/ocaml-ppx_expect
 
 .endif	# OCAML_PPX_EXPECT_BUILDLINK3_MK
