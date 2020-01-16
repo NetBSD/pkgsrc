85568e1ceea78ac0548e948d76436a34222610c2
adam <adam@pkgsrc.org>
2020-01-16 08:20:01 +0000

    py-pypiserver: updated to 1.3.2
    1.3.2:
    - ENH: The Dockerfile used for the official Docker images now uses Python 3.6
      rather than Python 2.7
    - ENH: The `welcome.html` page has been updated to provide more metadata
      and be more HTML-standards compliant
    - FIX: the `pypi-server -U` command no longer fails when run inside the
      Docker container
    - FIX: The `remove_pkg` API action now removes any extant instances of a
      package name-version combination, not just the first one found. This means
      that now, for example, if a `.whl` and `.tar.gz` file exist for the
      requested package name and version, both will be removed
    - FIX: include missing `simple/` path on a URL in the example pip commands
      on the `welcome.html` page
    - DOC: more consistent and accurate documentation for pip commands provided
      on the `welcome.html` page
    - DOC: fixes to the README to make it easier for people to use pypiserver
      behind an apache webserver

--- net/py-pypiserver/patches/patch-setup.py
+++ net/py-pypiserver/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.1 2019/05/29 20:13:09 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2020/01/16 08:20:02 adam Exp $
 
 Do not require setuptools-git for building.
 
---- setup.py.orig	2019-03-10 15:33:57.000000000 +0000
+--- setup.py.orig	2019-09-17 18:56:54.000000000 +0000
 +++ setup.py
-@@ -14,7 +14,7 @@ tests_require = ['pytest>=2.3', 'tox', '
+@@ -20,7 +20,7 @@ tests_require = [
  if sys.version_info == (2, 7):
-     tests_require.append('mock')
+     tests_require.append("mock")
  
--setup_requires = ['setuptools', 'setuptools-git >= 0.3']
-+setup_requires = ['setuptools']
+-setup_requires = ["setuptools", "setuptools-git >= 0.3"]
++setup_requires = ["setuptools"]
  if sys.version_info >= (3, 5):
-     setup_requires.append('wheel >= 0.25.0')  # earlier wheels fail in 3.5
+     setup_requires.append("wheel >= 0.25.0")  # earlier wheels fail in 3.5
  else:
