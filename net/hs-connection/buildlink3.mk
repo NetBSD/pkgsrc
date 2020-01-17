f39ebb67c6226baaef7cb3e71c1658608a3bd767
pho <pho@pkgsrc.org>
2020-01-17 14:59:02 +0000

    Import connection-0.3.1
    Simple network library for all your connection need.
    
    Features: Really simple to use, SSL/TLS, SOCKS.
    
    This library provides a very simple api to create sockets to a
    destination with the choice of SSL/TLS, and SOCKS.

---
+++ net/hs-connection/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 14:59:02 pho Exp $
+
+BUILDLINK_TREE+=	hs-connection
+
+.if !defined(HS_CONNECTION_BUILDLINK3_MK)
+HS_CONNECTION_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-connection+=	hs-connection>=0.3.1
+BUILDLINK_ABI_DEPENDS.hs-connection+=	hs-connection>=0.3.1
+BUILDLINK_PKGSRCDIR.hs-connection?=	../../net/hs-connection
+
+.include "../../devel/hs-basement/buildlink3.mk"
+.include "../../devel/hs-data-default-class/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.include "../../net/hs-socks/buildlink3.mk"
+.include "../../security/hs-tls/buildlink3.mk"
+.include "../../security/hs-x509/buildlink3.mk"
+.include "../../security/hs-x509-store/buildlink3.mk"
+.include "../../security/hs-x509-system/buildlink3.mk"
+.include "../../security/hs-x509-validation/buildlink3.mk"
+.endif	# HS_CONNECTION_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-connection
