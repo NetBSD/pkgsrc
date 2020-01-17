fb9edd7612fd8e450a274e58ec729092bce05682
pho <pho@pkgsrc.org>
2020-01-17 19:07:16 +0000

    Correct PKGSRCDIR

--- converters/hs-emojis/buildlink3.mk
+++ converters/hs-emojis/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/15 11:53:16 pho Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/17 19:07:16 pho Exp $
 
 BUILDLINK_TREE+=	hs-emojis
 
@@ -7,7 +7,7 @@ HS_EMOJIS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-emojis+=	hs-emojis>=0.1
 BUILDLINK_ABI_DEPENDS.hs-emojis+=	hs-emojis>=0.1
-BUILDLINK_PKGSRCDIR.hs-emojis?=		../../converters/hs-emoji
+BUILDLINK_PKGSRCDIR.hs-emojis?=		../../converters/hs-emojis
 .endif	# HS_EMOJIS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-emojis
