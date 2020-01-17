12d920401e7ead9619b2fdbfeb2e19f46047d442
pho <pho@pkgsrc.org>
2020-01-17 17:18:15 +0000

    Import haskell-lexer-1.0.2 from wip
    A fully compliant Haskell 98 lexer.

---
+++ devel/hs-haskell-lexer/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 17:18:15 pho Exp $
+
+BUILDLINK_TREE+=	hs-haskell-lexer
+
+.if !defined(HS_HASKELL_LEXER_BUILDLINK3_MK)
+HS_HASKELL_LEXER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-haskell-lexer+=	hs-haskell-lexer>=1.0.2
+BUILDLINK_ABI_DEPENDS.hs-haskell-lexer+=	hs-haskell-lexer>=1.0.2
+BUILDLINK_PKGSRCDIR.hs-haskell-lexer?=		../../devel/hs-haskell-lexer
+.endif	# HS_HASKELL_LEXER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-haskell-lexer
