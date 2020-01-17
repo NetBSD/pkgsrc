8608c4c03c66042cb8de9f9d1006b9cf05b4c962
pho <pho@pkgsrc.org>
2020-01-17 19:01:49 +0000

    Import zip-archive-0.4.1 from wip
    The zip-archive library provides functions for creating, modifying,
    and extracting files from zip archives. The zip archive format is
    documented in http://www.pkware.com/documents/casestudies/APPNOTE.TXT.
    
    Certain simplifying assumptions are made about the zip archives: in
    particular, there is no support for strong encryption, zip files that
    span multiple disks, ZIP64, OS-specific file attributes, or
    compression methods other than Deflate. However, the library should be
    able to read the most common zip archives, and the archives it
    produces should be readable by all standard unzip programs.

---
+++ archivers/hs-zip-archive/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 19:01:49 pho Exp $
+
+BUILDLINK_TREE+=	hs-zip-archive
+
+.if !defined(HS_ZIP_ARCHIVE_BUILDLINK3_MK)
+HS_ZIP_ARCHIVE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.4.1
+BUILDLINK_ABI_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.4.1
+BUILDLINK_PKGSRCDIR.hs-zip-archive?=	../../archivers/hs-zip-archive
+
+.include "../../security/hs-digest/buildlink3.mk"
+.include "../../archivers/hs-zlib/buildlink3.mk"
+.endif	# HS_ZIP_ARCHIVE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-zip-archive
