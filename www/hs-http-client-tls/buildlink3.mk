d67ad9500e132482e7b37b42ff4eb8958bc8ed96
pho <pho@pkgsrc.org>
2020-01-17 15:17:55 +0000

    Import http-client-tls-0.3.5.3
    Support for making connections via the connection package and, in
    turn, the tls package suite.

---
+++ www/hs-http-client-tls/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 15:17:55 pho Exp $
+
+BUILDLINK_TREE+=	hs-http-client-tls
+
+.if !defined(HS_HTTP_CLIENT_TLS_BUILDLINK3_MK)
+HS_HTTP_CLIENT_TLS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-http-client-tls+=	hs-http-client-tls>=0.3.5.3
+BUILDLINK_ABI_DEPENDS.hs-http-client-tls+=	hs-http-client-tls>=0.3.5.3
+BUILDLINK_PKGSRCDIR.hs-http-client-tls?=	../../www/hs-http-client-tls
+
+.include "../../textproc/hs-case-insensitive/buildlink3.mk"
+.include "../../net/hs-connection/buildlink3.mk"
+.include "../../security/hs-cryptonite/buildlink3.mk"
+.include "../../devel/hs-data-default-class/buildlink3.mk"
+.include "../../devel/hs-exceptions/buildlink3.mk"
+.include "../../www/hs-http-client/buildlink3.mk"
+.include "../../www/hs-http-types/buildlink3.mk"
+.include "../../devel/hs-memory/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.include "../../net/hs-network-uri/buildlink3.mk"
+.include "../../security/hs-tls/buildlink3.mk"
+.endif	# HS_HTTP_CLIENT_TLS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-http-client-tls
