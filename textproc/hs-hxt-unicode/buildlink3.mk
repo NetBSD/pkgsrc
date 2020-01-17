5fb6bf630f55cab3954bcba03fa7f48eb7ef3269
pho <pho@pkgsrc.org>
2020-01-17 15:59:25 +0000

    Import hxt-unicode-9.2.0.4 from wip
    Unicode encoding and decoding functions for utf8, iso-latin-* and
    somes other encodings, used in the Haskell XML Toolbox. ISO Latin 1 -
    16, utf8, utf16, ASCII are supported. Decoding is done with lasy
    functions, errors may be detected or ignored.

---
+++ textproc/hs-hxt-unicode/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 15:59:25 pho Exp $
+
+BUILDLINK_TREE+=	hs-hxt-unicode
+
+.if !defined(HS_HXT_UNICODE_BUILDLINK3_MK)
+HS_HXT_UNICODE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hxt-unicode+=	hs-hxt-unicode>=9.0.2
+BUILDLINK_ABI_DEPENDS.hs-hxt-unicode+=	hs-hxt-unicode>=9.0.2.4
+BUILDLINK_PKGSRCDIR.hs-hxt-unicode?=	../../textproc/hs-hxt-unicode
+
+.include "../../textproc/hs-hxt-charproperties/buildlink3.mk"
+.endif	# HS_HXT_UNICODE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hxt-unicode
