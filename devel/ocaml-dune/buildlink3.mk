2ef214d19bdeb19ddf3d0ac975c9c4f16bac6771
jaapb <jaapb@pkgsrc.org>
2020-01-17 10:29:34 +0000

    Updated devel/ocaml-dune to version 2.1.3nb1.
    This also compiles all the other packages included in the distribution;
    they do not add any dependencies, so I decided against making them
    optional.

--- devel/ocaml-dune/buildlink3.mk
+++ devel/ocaml-dune/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/01/17 09:18:08 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/01/17 10:29:34 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-dune
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-dune
 OCAML_DUNE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-dune+=	ocaml-dune>=2.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-dune+=	ocaml-dune>=2.1.3
+BUILDLINK_ABI_DEPENDS.ocaml-dune+=	ocaml-dune>=2.1.3nb1
 BUILDLINK_PKGSRCDIR.ocaml-dune?=	../../devel/ocaml-dune
 .endif	# OCAML_DUNE_BUILDLINK3_MK
 
