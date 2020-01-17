e39b9ea281bc631f880064e57c5cfaa922ff2ee3
jaapb <jaapb@pkgsrc.org>
2020-01-17 09:37:32 +0000

    Updated devel/ocaml-ppx_derivers to version 1.2.1.
    This converts the package from jbuilder to dune.

--- devel/ocaml-ppx_derivers/buildlink3.mk
+++ devel/ocaml-ppx_derivers/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2019/03/05 16:30:39 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/01/17 09:37:32 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_derivers
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_derivers
 OCAML_PPX_DERIVERS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_derivers+=	ocaml-ppx_derivers>=1.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_derivers+=	ocaml-ppx_derivers>=1.2nb4
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_derivers+=	ocaml-ppx_derivers>=1.2.1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_derivers?=	../../devel/ocaml-ppx_derivers
 .endif	# OCAML_PPX_DERIVERS_BUILDLINK3_MK
 
