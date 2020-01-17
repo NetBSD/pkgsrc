1716e2e3c3addfd71bd40c5799de39b35bef3b97
jaapb <jaapb@pkgsrc.org>
2020-01-17 09:07:25 +0000

    Updated devel/ocaml-jane-street-headers to version 0.13.0.
    This does change the type of some of the functions in the header files,
    hence the API update in the buildlink3.mk file.

--- devel/ocaml-jane-street-headers/buildlink3.mk
+++ devel/ocaml-jane-street-headers/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/11/03 10:39:23 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/01/17 09:07:25 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-jane-street-headers
 
 .if !defined(OCAML_JANE_STREET_HEADERS_BUILDLINK3_MK)
 OCAML_JANE_STREET_HEADERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-jane-street-headers+=	ocaml-jane-street-headers>=0.12.0
+BUILDLINK_API_DEPENDS.ocaml-jane-street-headers+=	ocaml-jane-street-headers>=0.13.0
 BUILDLINK_PKGSRCDIR.ocaml-jane-street-headers?=		../../devel/ocaml-jane-street-headers
 .endif	# OCAML_JANE_STREET_HEADERS_BUILDLINK3_MK
 
