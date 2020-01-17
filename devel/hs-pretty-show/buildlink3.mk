87b174e92644e9c1996d804110bacbe6e760628e
pho <pho@pkgsrc.org>
2020-01-17 17:24:36 +0000

    Import pretty-show-1.9.5
    A library and an executable for working with derived Show
    instances. By using the library, we can parse derived Show instances
    into a generic data structure. The ppsh tool uses the library to
    produce human-readable versions of Show instances, which can be quite
    handy for debugging Haskell programs. We can also render complex
    generic values into an interactive Html page, for easier examination.

---
+++ devel/hs-pretty-show/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 17:24:36 pho Exp $
+
+BUILDLINK_TREE+=	hs-pretty-show
+
+.if !defined(HS_PRETTY_SHOW_BUILDLINK3_MK)
+HS_PRETTY_SHOW_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-pretty-show+=	hs-pretty-show>=1.9.5
+BUILDLINK_ABI_DEPENDS.hs-pretty-show+=	hs-pretty-show>=1.9.5
+BUILDLINK_PKGSRCDIR.hs-pretty-show?=	../../devel/hs-pretty-show
+
+.include "../../devel/hs-haskell-lexer/buildlink3.mk"
+.endif	# HS_PRETTY_SHOW_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-pretty-show
