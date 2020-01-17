66d958df6bcbc97a5f92b10e65924a34127ea1be
jaapb <jaapb@pkgsrc.org>
2020-01-17 09:18:08 +0000

    Updated devel/ocaml-dune to version 2.1.3.
    The list of changes is quite large, details can be found in the CHANGES.md
    file in the distribution.
    
    For our purposes, the main change is that .install files are now put in a
    different location by default; I've updated mk/ocaml.mk to take that into
    account.

--- devel/ocaml-dune/buildlink3.mk
+++ devel/ocaml-dune/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2019/05/07 11:29:34 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/01/17 09:18:08 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-dune
 
 .if !defined(OCAML_DUNE_BUILDLINK3_MK)
 OCAML_DUNE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-dune+=	ocaml-dune>=1.7.0
-BUILDLINK_ABI_DEPENDS.ocaml-dune+=	ocaml-dune>=1.9.3
+BUILDLINK_API_DEPENDS.ocaml-dune+=	ocaml-dune>=2.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-dune+=	ocaml-dune>=2.1.3
 BUILDLINK_PKGSRCDIR.ocaml-dune?=	../../devel/ocaml-dune
 .endif	# OCAML_DUNE_BUILDLINK3_MK
 
