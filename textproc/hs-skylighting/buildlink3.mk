1ede0c7c96d1328f68206477dee049ab3dfb14c7
pho <pho@pkgsrc.org>
2020-01-17 17:32:25 +0000

    Import skylighting-0.8.3
    Skylighting is a syntax highlighting library with support for over one
    hundred languages. It derives its tokenizers from XML syntax
    definitions used by KDE's KSyntaxHighlighting framework, so any syntax
    supported by that framework can be added. An optional command-line
    program is provided. Skylighting is intended to be the successor to
    highlighting-kate. This package provides generated syntax modules
    based on the KDE XML definitions provided by the skylighting-core
    package. As a result this package is licensed under the GPL.

---
+++ textproc/hs-skylighting/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 17:32:25 pho Exp $
+
+BUILDLINK_TREE+=	hs-skylighting
+
+.if !defined(HS_SKYLIGHTING_BUILDLINK3_MK)
+HS_SKYLIGHTING_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-skylighting+=	hs-skylighting>=0.8.3
+BUILDLINK_ABI_DEPENDS.hs-skylighting+=	hs-skylighting>=0.8.3
+BUILDLINK_PKGSRCDIR.hs-skylighting?=	../../textproc/hs-skylighting
+
+.include "../../textproc/hs-skylighting-core/buildlink3.mk"
+.endif	# HS_SKYLIGHTING_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-skylighting
