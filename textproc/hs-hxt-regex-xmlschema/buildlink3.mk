929f79a3228b44ceefcc5804e1666367f1dacb5b
pho <pho@pkgsrc.org>
2020-01-17 15:52:41 +0000

    Import hxt-regex-xmlschema-9.2.0.3
    This library supports full W3C XML Schema regular expressions
    inclusive all Unicode character sets and blocks. The complete grammar
    can be found under http://www.w3.org/TR/xmlschema11-2/#regexs. It is
    implemented by the technique of derivations of regular expressions.
    
    The W3C syntax is extended to support not only union of regular sets,
    but also intersection, set difference, exor. Matching of
    subexpressions is also supported.
    
    The library can be used for constricting lightweight scanners and
    tokenizers. It is a standalone library, no external regex libraries
    are used.

---
+++ textproc/hs-hxt-regex-xmlschema/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 15:52:41 pho Exp $
+
+BUILDLINK_TREE+=	hs-hxt-regex-xmlschema
+
+.if !defined(HS_HXT_REGEX_XMLSCHEMA_BUILDLINK3_MK)
+HS_HXT_REGEX_XMLSCHEMA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hxt-regex-xmlschema+=	hs-hxt-regex-xmlschema>=9.2.0
+BUILDLINK_ABI_DEPENDS.hs-hxt-regex-xmlschema+=	hs-hxt-regex-xmlschema>=9.2.0.3
+BUILDLINK_PKGSRCDIR.hs-hxt-regex-xmlschema?=	../../textproc/hs-hxt-regex-xmlschema
+
+.include "../../textproc/hs-hxt-charproperties/buildlink3.mk"
+.endif	# HS_HXT_REGEX_XMLSCHEMA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hxt-regex-xmlschema
