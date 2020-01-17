6773f96f355cde48f43794bb86745ee557f589e2
pho <pho@pkgsrc.org>
2020-01-17 13:38:00 +0000

    Import tls-1.5.3
    Native Haskell TLS and SSL protocol implementation for server and
    client.
    
    This provides a high-level implementation of a sensitive security
    protocol, eliminating a common set of security issues through the use
    of the advanced type system, high level constructions and common
    Haskell features.
    
    Currently implement the SSL3.0, TLS1.0, TLS1.1, TLS1.2 and TLS 1.3
    protocol, and support RSA and Ephemeral (Elliptic curve and regular)
    Diffie Hellman key exchanges, and many extensions.

---
+++ security/hs-tls/buildlink3.mk
@@ -0,0 +1,26 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/17 13:38:00 pho Exp $
+
+BUILDLINK_TREE+=	hs-tls
+
+.if !defined(HS_TLS_BUILDLINK3_MK)
+HS_TLS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-tls+=	hs-tls>=1.5.3
+BUILDLINK_ABI_DEPENDS.hs-tls+=	hs-tls>=1.5.3
+BUILDLINK_PKGSRCDIR.hs-tls?=	../../security/hs-tls
+
+.include "../../devel/hs-asn1-encoding/buildlink3.mk"
+.include "../../devel/hs-asn1-types/buildlink3.mk"
+.include "../../devel/hs-async/buildlink3.mk"
+.include "../../devel/hs-cereal/buildlink3.mk"
+.include "../../security/hs-cryptonite/buildlink3.mk"
+.include "../../devel/hs-data-default-class/buildlink3.mk"
+.include "../../time/hs-hourglass/buildlink3.mk"
+.include "../../devel/hs-memory/buildlink3.mk"
+.include "../../security/hs-x509/buildlink3.mk"
+.include "../../security/hs-x509-store/buildlink3.mk"
+.include "../../security/hs-x509-validation/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.endif	# HS_TLS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-tls
