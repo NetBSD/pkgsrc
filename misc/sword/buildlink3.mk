13943f3046ec802b487341457fa4c9b383c5a5d6
jperkin <jperkin@pkgsrc.org>
2020-01-18 21:48:10 +0000

    *: Recursive revision bump for openssl 1.1.1.

--- misc/sword/buildlink3.mk
+++ misc/sword/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2020/01/12 20:19:57 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.27 2020/01/18 21:48:18 jperkin Exp $
 
 BUILDLINK_TREE+=	sword
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	sword
 SWORD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.sword+=	sword>=1.8.1
-BUILDLINK_ABI_DEPENDS.sword?=	sword>=1.8.1nb1
+BUILDLINK_ABI_DEPENDS.sword?=	sword>=1.8.1nb2
 BUILDLINK_PKGSRCDIR.sword?=	../../misc/sword
 
 .include "../../textproc/icu/buildlink3.mk"
