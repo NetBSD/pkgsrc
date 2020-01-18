416737c3f36f01a5fb2d1b7ac687bc76acb1e946
nia <nia@pkgsrc.org>
2020-01-18 15:32:40 +0000

    firefox: Remove remaining traces of OSS support.
    We no longer patch this in but it's still searching for the files if
    you're using something FreeBSDish or Linuxish. This should resolve build
    problems on these platforms.
    
    On NetBSD this problem never appeared because it's been using native audio
    instead of OSS for a while now.
    
    from Michael Forney in PR pkg/54868

--- www/firefox/patches/patch-dom_media_CubebUtils.cpp
+++ www/firefox/patches/patch-dom_media_CubebUtils.cpp
@@ -1,13 +1,13 @@
-$NetBSD: patch-dom_media_CubebUtils.cpp,v 1.4 2019/07/11 11:32:40 ryoon Exp $
+$NetBSD: patch-dom_media_CubebUtils.cpp,v 1.5 2020/01/18 15:32:40 nia Exp $
 
---- dom/media/CubebUtils.cpp.orig	2019-07-06 01:48:31.000000000 +0000
+--- dom/media/CubebUtils.cpp.orig	Wed Jan  8 01:23:31 2020
 +++ dom/media/CubebUtils.cpp
-@@ -163,7 +163,7 @@ const char kBrandBundleURL[] = "chrome:/
+@@ -140,7 +140,7 @@ const char kBrandBundleURL[] = "chrome://branding/loca
  
  const char* AUDIOSTREAM_BACKEND_ID_STR[] = {
      "jack",  "pulse",       "alsa",  "audiounit", "audioqueue", "wasapi",
 -    "winmm", "directsound", "sndio", "opensl",    "audiotrack", "kai"};
-+    "winmm", "directsound", "sndio", "opensl",    "audiotrack", "kai", "oss", "sun"};
++    "winmm", "directsound", "sndio", "opensl",    "audiotrack", "kai", "sun"};
  /* Index for failures to create an audio stream the first time. */
  const int CUBEB_BACKEND_INIT_FAILURE_FIRST =
      ArrayLength(AUDIOSTREAM_BACKEND_ID_STR);
