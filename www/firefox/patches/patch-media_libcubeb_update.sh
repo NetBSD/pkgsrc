15a1adfa8f1edff6be390e45a159a04ac42677a0
nia <nia@pkgsrc.org>
2020-01-18 15:32:40 +0000

    firefox: Remove remaining traces of OSS support.
    We no longer patch this in but it's still searching for the files if
    you're using something FreeBSDish or Linuxish. This should resolve build
    problems on these platforms.
    
    On NetBSD this problem never appeared because it's been using native audio
    instead of OSS for a while now.
    
    from Michael Forney in PR pkg/54868

--- www/firefox/patches/patch-media_libcubeb_update.sh
+++ www/firefox/patches/patch-media_libcubeb_update.sh
@@ -1,12 +1,11 @@
-$NetBSD: patch-media_libcubeb_update.sh,v 1.6 2019/06/13 22:05:24 nia Exp $
+$NetBSD: patch-media_libcubeb_update.sh,v 1.7 2020/01/18 15:32:40 nia Exp $
 
---- media/libcubeb/update.sh.orig	2019-05-29 15:39:11.000000000 +0000
+--- media/libcubeb/update.sh.orig	Wed Jan  8 01:23:35 2020
 +++ media/libcubeb/update.sh
-@@ -25,6 +25,8 @@ cp $1/src/cubeb_log.h src
+@@ -25,6 +25,7 @@ cp $1/src/cubeb_log.h src
  cp $1/src/cubeb_mixer.cpp src
  cp $1/src/cubeb_mixer.h src
  cp $1/src/cubeb_opensl.c src
-+cp $1/src/cubeb_oss.c src
 +cp $1/src/cubeb_sun.c src
  cp $1/src/cubeb-jni.cpp src
  cp $1/src/cubeb-jni.h src
