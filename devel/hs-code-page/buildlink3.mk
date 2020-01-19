26ebc508baeebe6e28c07b69f09172678f37f3cc
pho <pho@pkgsrc.org>
2020-01-19 12:37:21 +0000

    Import code-page-0.2
    This library provides two modules:
    
    * System.IO.CodePage: a cross-platform module that exports functions
      which adjust code pages on Windows, and do nothing on other
      operating systems.
    
    * System.Win32.CodePage: On Windows, this exports functions for
      getting, setting, and analyzing code pages. On other operating
      systems, this module exports nothing.

---
+++ devel/hs-code-page/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/19 12:37:21 pho Exp $
+
+BUILDLINK_TREE+=	hs-code-page
+
+.if !defined(HS_CODE_PAGE_BUILDLINK3_MK)
+HS_CODE_PAGE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-code-page+=	hs-code-page>=0.2
+BUILDLINK_PKGSRCDIR.hs-code-page?=	../../devel/hs-code-page
+.endif	# HS_CODE_PAGE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-code-page
