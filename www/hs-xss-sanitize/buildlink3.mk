e0ecf80126d3732bb7d6d6c370d0dbc702f79e6c
pho <pho@pkgsrc.org>
2020-01-19 10:49:46 +0000

    Import xss-sanitize-0.3.6
    Run untrusted HTML through Text.HTML.SanitizeXSS.sanitizeXSS to
    prevent XSS attacks.

---
+++ www/hs-xss-sanitize/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/19 10:49:46 pho Exp $
+
+BUILDLINK_TREE+=	hs-xss-sanitize
+
+.if !defined(HS_XSS_SANITIZE_BUILDLINK3_MK)
+HS_XSS_SANITIZE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-xss-sanitize+=	hs-xss-sanitize>=0.3.6
+BUILDLINK_ABI_DEPENDS.hs-xss-sanitize+=	hs-xss-sanitize>=0.3.6
+BUILDLINK_PKGSRCDIR.hs-xss-sanitize?=	../../www/hs-xss-sanitize
+
+.include "../../textproc/hs-attoparsec/buildlink3.mk"
+.include "../../textproc/hs-css-text/buildlink3.mk"
+.include "../../net/hs-network-uri/buildlink3.mk"
+.include "../../textproc/hs-tagsoup/buildlink3.mk"
+.include "../../devel/hs-utf8-string/buildlink3.mk"
+.endif	# HS_XSS_SANITIZE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-xss-sanitize
