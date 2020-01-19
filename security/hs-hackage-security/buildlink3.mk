5a1fc92d10eeccb90790c9e4ea3581fdb0d67e52
pho <pho@pkgsrc.org>
2020-01-19 01:54:46 +0000

    Import hackage-security-0.6.0.0
    The hackage security library provides both server and client utilities
    for securing the Hackage package server
    (http://hackage.haskell.org/). It is based on The Update Framework
    (http://theupdateframework.com/), a set of recommendations developed
    by security researchers at various universities in the US as well as
    developers on the Tor project (https://www.torproject.org/).
    
    The current implementation supports only index signing, thereby
    enabling untrusted mirrors. It does not yet provide facilities for
    author package signing.

---
+++ security/hs-hackage-security/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/19 01:54:46 pho Exp $
+
+BUILDLINK_TREE+=	hs-hackage-security
+
+.if !defined(HS_HACKAGE_SECURITY_BUILDLINK3_MK)
+HS_HACKAGE_SECURITY_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hackage-security+=	hs-hackage-security>=0.6.0
+BUILDLINK_ABI_DEPENDS.hs-hackage-security+=	hs-hackage-security>=0.6.0.0
+BUILDLINK_PKGSRCDIR.hs-hackage-security?=	../../security/hs-hackage-security
+
+.include "../../converters/hs-base16-bytestring/buildlink3.mk"
+.include "../../converters/hs-base64-bytestring/buildlink3.mk"
+.include "../../security/hs-cryptohash-sha256/buildlink3.mk"
+.include "../../security/hs-ed25519/buildlink3.mk"
+.include "../../archivers/hs-tar/buildlink3.mk"
+.include "../../archivers/hs-zlib/buildlink3.mk"
+.include "../../devel/hs-lukko/buildlink3.mk"
+.include "../../net/hs-network/buildlink3.mk"
+.include "../../net/hs-network-uri/buildlink3.mk"
+.endif	# HS_HACKAGE_SECURITY_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hackage-security
