60192e8baf3ccc7aa006f32861a982ff823a2b76
pho <pho@pkgsrc.org>
2020-01-19 01:11:35 +0000

    Import lukko-0.1.1.1
    This package provides access to platform dependent file locking APIs:
    
    * Open file descriptor locking on Linux (Lukko.OFD)
    
    * flock locking on unix platforms (Lukko.FLock)
    
    * Windows locking LockFileEx (Lukko.Windows)
    
    * No-op locking, which throws exceptions (Lukko.NoOp)
    
    Lukko module exports the best option for the target platform with
    uniform API.

---
+++ devel/hs-lukko/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/19 01:11:35 pho Exp $
+
+BUILDLINK_TREE+=	hs-lukko
+
+.if !defined(HS_LUKKO_BUILDLINK3_MK)
+HS_LUKKO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-lukko+=	hs-lukko>=0.1.1
+BUILDLINK_ABI_DEPENDS.hs-lukko+=	hs-lukko>=0.1.1.1
+BUILDLINK_PKGSRCDIR.hs-lukko?=		../../devel/hs-lukko
+.endif	# HS_LUKKO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-lukko
