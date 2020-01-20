56f76995d6deee2198c6cdcde21f099a485f28e5
nia <nia@pkgsrc.org>
2020-01-20 01:26:16 +0000

    libmediaart: Add BUILDLINK_INCDIRS

--- multimedia/libmediaart/buildlink3.mk
+++ multimedia/libmediaart/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/10/18 16:55:50 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/20 01:26:16 nia Exp $
 
 BUILDLINK_TREE+=	libmediaart
 
@@ -7,6 +7,7 @@ LIBMEDIAART_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libmediaart+=	libmediaart>=1.9.4
 BUILDLINK_PKGSRCDIR.libmediaart?=	../../multimedia/libmediaart
+BUILDLINK_INCDIRS.libmediaart+=		include/libmediaart-2.0
 
 .include "../../devel/glib2/buildlink3.mk"
 .endif	# LIBMEDIAART_BUILDLINK3_MK
