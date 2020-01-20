7b9a44034569b42242e151e242eefaf19215b447
nia <nia@pkgsrc.org>
2020-01-20 21:26:52 +0000

    libmikmod: Remove esound support; add alsa and pulse support instead.
    Bump PKGREVISION because this changes default options (on Linux only)

--- audio/libmikmod/options.mk
+++ audio/libmikmod/options.mk
@@ -1,16 +1,26 @@
-# $NetBSD: options.mk,v 1.2 2006/03/09 21:04:40 jlam Exp $
+# $NetBSD: options.mk,v 1.3 2020/01/20 21:26:52 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libmikmod
-PKG_SUPPORTED_OPTIONS=	esound oss
+PKG_SUPPORTED_OPTIONS=	pulseaudio
+
+.include "../../mk/bsd.fast.prefs.mk"
+.if ${OPSYS} == "Linux"
+PKG_SUPPORTED_OPTIONS+=	alsa
+PKG_SUGGESTED_OPTIONS+=	alsa
+.endif
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mesound)
-.include "../../audio/esound/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Malsa)
+CONFIGURE_ARGS+=	--enable-alsa
+.include "../../audio/alsa-lib/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=	--disable-esd
+CONFIGURE_ARGS+=	--disable-alsa
 .endif
 
-.if !empty(PKG_OPTIONS:Moss)
-.  include "../../mk/oss.buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+CONFIGURE_ARGS+=	--enable-pulseaudio
+.include "../../audio/pulseaudio/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-pulseaudio
 .endif
