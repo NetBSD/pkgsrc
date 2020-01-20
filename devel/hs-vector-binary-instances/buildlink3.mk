65d81c36c38313a83805f127e5c0fbfcecf5e5ba
pho <pho@pkgsrc.org>
2020-01-20 01:04:20 +0000

    Import vector-binary-instances-0.2.5.1
    Instances for Binary for the types defined in the vector package,
    making it easy to serialize vectors to and from disk. We use the
    generic interface to vectors, so all vector types are
    supported. Specific instances are provided for unboxed, boxed and
    storable vectors.

---
+++ devel/hs-vector-binary-instances/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/20 01:04:20 pho Exp $
+
+BUILDLINK_TREE+=	hs-vector-binary-instances
+
+.if !defined(HS_VECTOR_BINARY_INSTANCES_BUILDLINK3_MK)
+HS_VECTOR_BINARY_INSTANCES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-vector-binary-instances+=	hs-vector-binary-instances>=0.2.5
+BUILDLINK_ABI_DEPENDS.hs-vector-binary-instances+=	hs-vector-binary-instances>=0.2.5.1
+BUILDLINK_PKGSRCDIR.hs-vector-binary-instances?=	../../devel/hs-vector-binary-instances
+
+.include "../../devel/hs-vector/buildlink3.mk"
+.endif	# HS_VECTOR_BINARY_INSTANCES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-vector-binary-instances
