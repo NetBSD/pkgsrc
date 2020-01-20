da627d4a808097324ca8f068a4f3dcd9670cbab7
nia <nia@pkgsrc.org>
2020-01-20 01:32:17 +0000

    grilo: Update to 0.3.11
    Needed for several newer GNOME package updates.

--- net/grilo/buildlink3.mk
+++ net/grilo/buildlink3.mk
@@ -1,17 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.21 2019/04/03 00:32:57 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2020/01/20 01:32:17 nia Exp $
 
 BUILDLINK_TREE+=	grilo
 
 .if !defined(GRILO_BUILDLINK3_MK)
 GRILO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.grilo+=	grilo>=0.2.2
-BUILDLINK_ABI_DEPENDS.grilo?=	grilo>=0.2.7nb15
+BUILDLINK_API_DEPENDS.grilo+=	grilo>=0.3
 BUILDLINK_PKGSRCDIR.grilo?=	../../net/grilo
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../devel/gobject-introspection/buildlink3.mk"
+.include "../../multimedia/totem-pl-parser/buildlink3.mk"
 .include "../../net/libsoup/buildlink3.mk"
+.include "../../security/liboauth/buildlink3.mk"
 .endif	# GRILO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-grilo
