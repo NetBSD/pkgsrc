f3b0be43d38f2e0ec0acfc287a0268417d8dadb7
nia <nia@pkgsrc.org>
2020-01-20 21:31:02 +0000

    mp3blaster: Remove esound support; use sdl on all non-OSS platforms.
    Remove OSS option too because it does absolutely nothing, OSS support
    is autodetected if the OS supports it.
    
    Bump PKGREVISION because this changes default options on platforms that
    don't support OSS.

--- audio/mp3blaster/options.mk
+++ audio/mp3blaster/options.mk
@@ -1,24 +1,14 @@
-# $NetBSD: options.mk,v 1.2 2015/12/29 04:54:35 dholland Exp $
+# $NetBSD: options.mk,v 1.3 2020/01/20 21:31:02 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mp3blaster
-PKG_SUPPORTED_OPTIONS=	esound oss sdl
-PKG_SUGGESTED_OPTIONS=	oss
+PKG_SUPPORTED_OPTIONS=	sdl
 
-.if ${OPSYS} == "Darwin"
+.include "../../mk/oss.buildlink3.mk"
+.if ${OSS_TYPE} == "none"
 PKG_SUGGESTED_OPTIONS=	sdl
 .endif
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Mesound)
-CONFIGURE_ARGS+=	--with-esd=yes
-.include "../../audio/esound/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Moss)
-CONFIGURE_ARGS+=	--with-oss=yes
-.include "../../mk/oss.buildlink3.mk"
-.endif
 
 .if !empty(PKG_OPTIONS:Msdl)
 CONFIGURE_ARGS+=	--with-sdl=yes
