37b90b1537f804f7c91c926ee2e2666596749d6f
nia <nia@pkgsrc.org>
2020-01-20 01:41:52 +0000

    totem: Update to 3.34.1
    Updated to GNOME 3 / gstreamer1 / gtk3 version.

--- multimedia/totem-pl-parser/buildlink3.mk
+++ multimedia/totem-pl-parser/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.48 2019/04/03 00:32:56 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.49 2020/01/20 01:41:52 nia Exp $
 
 BUILDLINK_TREE+=	totem-pl-parser
 
@@ -6,12 +6,11 @@ BUILDLINK_TREE+=	totem-pl-parser
 TOTEM_PL_PARSER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.totem-pl-parser+=	totem-pl-parser>=2.22.1
-BUILDLINK_ABI_DEPENDS.totem-pl-parser+=	totem-pl-parser>=3.4.5nb14
+BUILDLINK_ABI_DEPENDS.totem-pl-parser+=	totem-pl-parser>=3.4.5nb1
 BUILDLINK_PKGSRCDIR.totem-pl-parser?=	../../multimedia/totem-pl-parser
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../mail/gmime/buildlink3.mk"
-.include "../../net/libsoup/buildlink3.mk"
+.include "../../archivers/libarchive/buildlink3.mk"
 .include "../../security/libgcrypt/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .endif # TOTEM_PL_PARSER_BUILDLINK3_MK
