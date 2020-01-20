5dbbaba34b08cf918f1356d97cae7a372d2f0430
nia <nia@pkgsrc.org>
2020-01-20 01:55:47 +0000

    gnome-desktop3: Update buildlink3 to match .pc file

--- x11/gnome-desktop3/buildlink3.mk
+++ x11/gnome-desktop3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.27 2019/07/21 22:24:15 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2020/01/20 01:55:47 nia Exp $
 
 BUILDLINK_TREE+=	gnome-desktop3
 
@@ -9,8 +9,9 @@ BUILDLINK_API_DEPENDS.gnome-desktop3+=	gnome-desktop3>=3.4.2
 BUILDLINK_ABI_DEPENDS.gnome-desktop3+=	gnome-desktop3>=3.24.2nb6
 BUILDLINK_PKGSRCDIR.gnome-desktop3?=	../../x11/gnome-desktop3
 
+BUILDLINK_API_DEPENDS.gsettings-desktop-schemas+=	gsettings-desktop-schemas>=3.5.91
+.include "../../sysutils/gsettings-desktop-schemas/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
-.include "../../x11/libxkbfile/buildlink3.mk"
 .endif # GNOME_DESKTOP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnome-desktop3
