6125216d7a2e671b7f1ebb32590edb0c1a25d1d2
leot <leot@pkgsrc.org>
2020-01-20 10:33:04 +0000

    gallery-dl: Update to 1.12.3
    1.12.3
    ------
    ### Additions
    - [hentaifoundry] extract more metadata (#565)
    - [twitter] add option to extract TwitPic embeds (#579)
    - implement a post-processor module to compare file versions (#530)
    
    ### Fixes
    - [hitomi] update image URL generation
    - [mangadex] revert domain to `mangadex.org`
    - [pinterest] improve detection of invalid pin.it links
    - [pixiv] update URL patterns for user profiles and bookmarks (#568)
    - [twitter] Fix stop before real end (#573)
    - remove temp files before downloading from fallback URLs
    
    ### Removals
    - [erolord] remove extractor

--- net/gallery-dl/patches/patch-gallery__dl_version.py
+++ net/gallery-dl/patches/patch-gallery__dl_version.py
@@ -1,4 +1,4 @@
-$NetBSD: patch-gallery__dl_version.py,v 1.3 2019/02/05 21:18:04 leot Exp $
+$NetBSD: patch-gallery__dl_version.py,v 1.4 2020/01/20 10:33:04 leot Exp $
 
 Avoid a UnicodeDecodeError.
 
@@ -7,8 +7,8 @@ Avoid a UnicodeDecodeError.
 @@ -1,6 +1,6 @@
  # -*- coding: utf-8 -*-
  
--# Copyright 2016-2019 Mike Fährmann
-+# Copyright 2016-2019 Mike Fahrmann
+-# Copyright 2016-2020 Mike Fährmann
++# Copyright 2016-2020 Mike Fahrmann
  #
  # This program is free software; you can redistribute it and/or modify
  # it under the terms of the GNU General Public License version 2 as
