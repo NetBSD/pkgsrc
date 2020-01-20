e030cb76594d7cebfccdfd9882fa0eb6772dcb88
nia <nia@pkgsrc.org>
2020-01-20 21:32:59 +0000

    allegro: Remove (for wrong build system) esound support. Add alsa and jack.
    Bump PKGREVISION because this changes default options (on Linux only)

--- devel/allegro/options.mk
+++ devel/allegro/options.mk
@@ -1,14 +1,26 @@
-# $NetBSD: options.mk,v 1.2 2008/04/12 22:42:59 jlam Exp $
+# $NetBSD: options.mk,v 1.3 2020/01/20 21:32:59 nia Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.allegro
-PKG_SUPPORTED_OPTIONS=	esound
+PKG_OPTIONS_VAR=		PKG_OPTIONS.allegro
+PKG_SUPPORTED_OPTIONS=		alsa jack
+PKG_SUGGESTED_OPTIONS.Linux=	alsa
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		esound
+PLIST_SRC+=	PLIST
 
-.if !empty(PKG_OPTIONS:Mesound)
-CONFIGURE_ARGS+=	--enable-esddigi
-PLIST.esound=		yes
-.include "../../audio/esound/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Malsa)
+PLIST_SRC+=	PLIST.alsa
+CFLAGS.NetBSD+=	-DESTRPIPE=EPIPE
+CMAKE_ARGS+=	-DWANT_ALSA=1
+.include "../../audio/alsa-lib/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DWANT_ALSA=0
+.endif
+
+PLIST_SRC+=	PLIST.jack
+.if !empty(PKG_OPTIONS:Mjack)
+CMAKE_ARGS+=	-DWANT_JACK=1
+.include "../../audio/jack/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DWANT_JACK=0
 .endif
