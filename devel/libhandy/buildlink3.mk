fc3639c90e4d4b689c80f03ce6a57a5431c4f622
nia <nia@pkgsrc.org>
2020-01-21 14:58:09 +0000

    devel: Add libhandy.
    Needed for epiphany update.
    
    The aim of the Handy library is to help with developing UI for mobile devices
    using GTK/GNOME.

---
+++ devel/libhandy/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/21 14:58:09 nia Exp $
+
+BUILDLINK_TREE+=	libhandy
+
+.if !defined(LIBHANDY_BUILDLINK3_MK)
+LIBHANDY_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libhandy+=	libhandy>=0.0.13
+BUILDLINK_PKGSRCDIR.libhandy?=		../../devel/libhandy
+BUILDLINK_INCDIRS.libhandy+=		include/libhandy-0.0
+
+.include "../../x11/gtk3/buildlink3.mk"
+.endif	# LIBHANDY_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libhandy
