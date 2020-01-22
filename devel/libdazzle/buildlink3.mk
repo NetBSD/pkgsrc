58932155445e21b883713c3995f69c2e59d4c2fa
nia <nia@pkgsrc.org>
2020-01-22 09:44:41 +0000

    libdazzle: Downgrade to release from stable branch.
    Doesn't seem to cause any ABI breakage, epiphany still works and that's
    the only thing using this.
    
    Pointed out by wiz, downgrading ok'd by wiz

--- devel/libdazzle/buildlink3.mk
+++ devel/libdazzle/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/01/21 14:56:41 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/22 09:44:41 nia Exp $
 
 BUILDLINK_TREE+=	libdazzle
 
 .if !defined(LIBDAZZLE_BUILDLINK3_MK)
 LIBDAZZLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libdazzle+=	libdazzle>=3.35.3
+BUILDLINK_API_DEPENDS.libdazzle+=	libdazzle>=3.34
 BUILDLINK_PKGSRCDIR.libdazzle?=		../../devel/libdazzle
 BUILDLINK_INCDIRS.libdazzle+=		include/libdazzle-1.0
 
