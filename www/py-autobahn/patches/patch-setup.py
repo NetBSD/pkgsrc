9e7ed5e250826de9a7b4cdd2e7fc6522fa208e22
adam <adam@pkgsrc.org>
2020-01-23 14:33:18 +0000

    py-autobahn: updated to 20.1.2
    20.1.2
    fix: add python_requires>=3.5 to prevent installation on python 2
    
    20.1.1
    IMPORTANT: beginning release v20.1.1, Autobahn|Python only supports Python 3.5 or later.
    fix: first part of cleaning up code, dropping Python 2 support

--- www/py-autobahn/patches/patch-setup.py
+++ www/py-autobahn/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.1 2019/05/30 09:10:13 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2020/01/23 14:33:18 adam Exp $
 
 Relax test requirements.
 
---- setup.py.orig	2018-08-19 10:45:51.000000000 +0000
+--- setup.py.orig	2020-01-14 15:21:43.000000000 +0000
 +++ setup.py
-@@ -167,7 +167,7 @@ if PY3:
+@@ -155,7 +155,7 @@ extras_require_dev = [
  
- # for testing by users with "python setup.py test" (not Tox, which we use)
- test_requirements = [
+     # pytest 3.3.0 has dropped support for Python 3.3
+     # https://docs.pytest.org/en/latest/changelog.html#pytest-3-3-0-2017-11-23
 -    "pytest>=2.8.6,<3.3.0",             # MIT license
 +    "pytest>=2.8.6",                    # MIT license
-     "mock>=1.3.0",                      # BSD license
- ]
  
+     "twine>=1.6.5",                     # Apache 2.0
+     'sphinx>=1.2.3',                    # BSD
