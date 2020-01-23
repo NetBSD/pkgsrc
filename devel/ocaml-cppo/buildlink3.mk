ca8d545ac9323bfbc1bb1a366f8801665b88c0e9
jaapb <jaapb@pkgsrc.org>
2020-01-23 13:11:14 +0000

    Updated devel/ocaml-cppo to version 1.6.6.
    This updates the build system to dune (2.0), and includes a minor bugfix
    to remove compiler warnings.

--- devel/ocaml-cppo/buildlink3.mk
+++ devel/ocaml-cppo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2019/03/05 16:53:49 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/01/23 13:11:14 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cppo
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-cppo
 OCAML_CPPO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.3.1
-BUILDLINK_ABI_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.6.5nb1
+BUILDLINK_ABI_DEPENDS.ocaml-cppo+=	ocaml-cppo>=1.6.6
 BUILDLINK_PKGSRCDIR.ocaml-cppo?=	../../devel/ocaml-cppo
 .endif	# OCAML_CPPO_BUILDLINK3_MK
 
