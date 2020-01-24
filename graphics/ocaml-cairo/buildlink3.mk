ecb5e4576681add4fa945a3eeab37e600ef9ad6a
jaapb <jaapb@pkgsrc.org>
2020-01-24 12:57:08 +0000

    Added graphics/ocaml-cairo, OCaml bindings for cairo

---
+++ graphics/ocaml-cairo/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/01/24 12:57:08 jaapb Exp $
+
+BUILDLINK_TREE+=	ocaml-cairo
+
+.if !defined(OCAML_CAIRO_BUILDLINK3_MK)
+OCAML_CAIRO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-cairo+=	ocaml-cairo>=0.6.1
+BUILDLINK_PKGSRCDIR.ocaml-cairo?=	../../graphics/ocaml-cairo
+
+.include "../../graphics/cairo/buildlink3.mk"
+.endif	# OCAML_CAIRO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-cairo
