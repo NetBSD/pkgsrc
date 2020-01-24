77d3979eb9dbfe6594e3795c21374d0a3889e221
jaapb <jaapb@pkgsrc.org>
2020-01-24 16:04:51 +0000

    Added depenency to devel/ocaml-re and revbump.
    The ocaml-seq package is needed by ocaml-re; added this as a dependency.

--- devel/ocaml-re/buildlink3.mk
+++ devel/ocaml-re/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2020/01/24 09:14:10 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2020/01/24 16:04:51 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-re
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-re
 OCAML_RE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-re+=	ocaml-re>=1.6.0
-BUILDLINK_ABI_DEPENDS.ocaml-re+=	ocaml-re>=1.9.0
+BUILDLINK_ABI_DEPENDS.ocaml-re+=	ocaml-re>=1.9.0nb1
 BUILDLINK_PKGSRCDIR.ocaml-re?=		../../devel/ocaml-re
 .endif	# OCAML_RE_BUILDLINK3_MK
 
