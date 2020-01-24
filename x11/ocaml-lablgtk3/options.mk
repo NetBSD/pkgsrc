4d94a903a3b782a9dde5e47241dc3a13df913e42
jaapb <jaapb@pkgsrc.org>
2020-01-24 14:50:42 +0000

    Added options to x11/ocaml-lablgtk3

--- x11/ocaml-lablgtk3/options.mk
+++ x11/ocaml-lablgtk3/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2020/01/24 14:07:02 jaapb Exp $
+# $NetBSD: options.mk,v 1.2 2020/01/24 14:50:42 jaapb Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.lablgtk3
 PKG_SUPPORTED_OPTIONS=	gtksourceview3 gtkspell
@@ -11,6 +11,9 @@ PLIST_VARS+=		gtkspell gtksourceview
 .include "../../textproc/gtkspell3/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-gtkspell
 PLIST.gtkspell=	yes
+OCAML_FINDLIB_DIRS+=	lablgtk3-gtkspell3
+OPAM_INSTALL_FILES+=	lablgtk3-gtkspell3
+DUNE_BUILD_PACKAGES+=	lablgtk3-gtkspell3
 .else
 CONFIGURE_ARGS+=	--without-gtkspell
 .endif
@@ -19,6 +22,9 @@ CONFIGURE_ARGS+=	--without-gtkspell
 .include "../../x11/gtksourceview3/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-gtksourceview3
 PLIST.gtksourceview=	yes
+OCAML_FINDLIB_DIRS+=	lablgtk3-sourceview3
+OPAM_INSTALL_FILES+=	lablgtk3-sourceview3
+DUNE_BUILD_PACKAGES+=	lablgtk3-sourceview3
 .else
 CONFIGURE_ARGS+=	--without-gtksourceview3
 .endif
