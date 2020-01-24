8f2c14688e326582a0129db520d71a525310eae2
jaapb <jaapb@pkgsrc.org>
2020-01-24 09:14:10 +0000

    Updated devel/ocaml-re to version 1.9.0.
    This version introduces some minor bugfixes, and works with Dune 2.0.

--- devel/ocaml-re/buildlink3.mk
+++ devel/ocaml-re/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2019/11/03 10:39:25 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2020/01/24 09:14:10 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-re
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-re
 OCAML_RE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-re+=	ocaml-re>=1.6.0
-BUILDLINK_ABI_DEPENDS.ocaml-re+=	ocaml-re>=1.8.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-re+=	ocaml-re>=1.9.0
 BUILDLINK_PKGSRCDIR.ocaml-re?=		../../devel/ocaml-re
 .endif	# OCAML_RE_BUILDLINK3_MK
 
