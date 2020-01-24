eb1ce63c717e87ac6692afd5034d854f7cf43048
jaapb <jaapb@pkgsrc.org>
2020-01-24 16:27:41 +0000

    Updated devel/ocaml-stringext to version 1.6.0.
    This changes the build system to dune 2.0.

--- devel/ocaml-stringext/buildlink3.mk
+++ devel/ocaml-stringext/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/19 14:24:26 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/01/24 16:27:41 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-stringext
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-stringext
 OCAML_STRINGEXT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-stringext+=	ocaml-stringext>=1.5.0
+BUILDLINK_ABI_DEPENDS.ocaml-stringext+=	ocaml-stringext>=1.6.0
 BUILDLINK_PKGSRCDIR.ocaml-stringext?=	../../devel/ocaml-stringext
 .endif	# OCAML_STRINGEXT_BUILDLINK3_MK
 
