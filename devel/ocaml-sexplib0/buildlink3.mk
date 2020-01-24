2a3421dd3cfd54f4266fe4a4a6a1f47099a8eb8e
jaapb <jaapb@pkgsrc.org>
2020-01-24 16:15:30 +0000

    Updated devel/ocaml-sexplib0 to version 0.13.0.
    There does not seem to be a changelog, but it looks like changes are minor.

--- devel/ocaml-sexplib0/buildlink3.mk
+++ devel/ocaml-sexplib0/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2019/03/05 16:13:26 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/01/24 16:15:30 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-sexplib0
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-sexplib0
 OCAML_SEXPLIB0_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-sexplib0+=	ocaml-sexplib0>=0.11.0
-BUILDLINK_ABI_DEPENDS.ocaml-sexplib0+=	ocaml-sexplib0>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-sexplib0+=	ocaml-sexplib0>=0.13.0
 BUILDLINK_PKGSRCDIR.ocaml-sexplib0?=	../../devel/ocaml-sexplib0
 
 .endif	# OCAML_SEXPLIB0_BUILDLINK3_MK
