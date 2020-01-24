39fa176c28b9edbc7401734516fef7eb1b418e19
jaapb <jaapb@pkgsrc.org>
2020-01-24 16:07:38 +0000

    Updated misc/ocaml-opam to version 2.0.6.
    Minor improvements and bugfixes only.

--- misc/ocaml-opam/buildlink3.mk
+++ misc/ocaml-opam/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2019/03/05 18:36:24 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/01/24 16:07:38 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-opam
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-opam
 OCAML_OPAM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-opam+=	ocaml-opam>=2.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-opam+=	ocaml-opam>=2.0.3
+BUILDLINK_ABI_DEPENDS.ocaml-opam+=	ocaml-opam>=2.0.6
 BUILDLINK_PKGSRCDIR.ocaml-opam?=	../../misc/ocaml-opam
 
 .endif	# OCAML_OPAM_BUILDLINK3_MK
