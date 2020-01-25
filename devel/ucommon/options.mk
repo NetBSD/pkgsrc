8e5648a517752753b62a269ad91fd3df28a7e951
jperkin <jperkin@pkgsrc.org>
2020-01-25 10:45:10 +0000

    *: Remove obsolete BUILDLINK_API_DEPENDS.openssl.

--- devel/ucommon/options.mk
+++ devel/ucommon/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2019/01/13 18:21:24 adam Exp $
+# $NetBSD: options.mk,v 1.4 2020/01/25 10:45:10 jperkin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ucommon
 PKG_SUPPORTED_OPTIONS=	gnutls openssl tests
@@ -12,7 +12,6 @@ BUILDLINK_API_DEPENDS.gnutls+=	gnutls>=3.0.0
 .endif
 
 .if !empty(PKG_OPTIONS:Mopenssl)
-BUILDLINK_API_DEPENDS.openssl+=	openssl>=1.0.0
 .include "../../security/openssl/buildlink3.mk"
 CMAKE_ARGS+=	-DCRYPTO_OPENSSL=ON
 .else
