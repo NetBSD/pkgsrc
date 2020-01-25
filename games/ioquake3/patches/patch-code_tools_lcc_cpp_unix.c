8417777857e7ecddf09516d7582df74485ee53d8
jmcneill <jmcneill@pkgsrc.org>
2020-01-25 20:13:49 +0000

    Update ioquake3 to 1.36.20200125 (latest from Github).
    The project seems to no longer do official releases.

--- games/ioquake3/patches/patch-code_tools_lcc_cpp_unix.c
+++ games/ioquake3/patches/patch-code_tools_lcc_cpp_unix.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-code_tools_lcc_cpp_unix.c,v 1.1 2020/01/25 16:07:15 jmcneill Exp $
+$NetBSD: patch-code_tools_lcc_cpp_unix.c,v 1.2 2020/01/25 20:13:50 jmcneill Exp $
 
 Use built-in memmove on NetBSD.
 
---- code/tools/lcc/cpp/unix.c.orig	2008-04-23 17:39:39.000000000 +0000
+--- code/tools/lcc/cpp/unix.c.orig	2020-01-25 19:19:26.823146950 +0000
 +++ code/tools/lcc/cpp/unix.c
-@@ -99,7 +99,7 @@ char *basepath( char *fname )
- /* memmove is defined here because some vendors don't provide it at
+@@ -106,7 +106,7 @@ char *basepath( char *fname )
     all and others do a terrible job (like calling malloc) */
  // -- ouch, that hurts -- ln
--#ifndef MACOS_X   /* always use the system memmove() on Mac OS X. --ryan. */
-+#if !defined(MACOS_X) && !defined(__NetBSD__)   /* always use the system memmove() */
+ /* always use the system memmove() on Mac OS X. --ryan. */
+-#if !defined(__APPLE__) && !defined(_MSC_VER)
++#if !defined(__APPLE__) && !defined(_MSC_VER) && !defined(__NetBSD__)
  #ifdef memmove
  #undef memmove
  #endif
