8f7483aafc5403d1208aeed4856690e7e6200cbf
fox <fox@pkgsrc.org>
2020-01-25 23:48:31 +0000

    chat/ircd-hybrid: Adds option for supporting GnuTLS.
    Thanks to leot@ for reviewing.

--- chat/ircd-hybrid/options.mk
+++ chat/ircd-hybrid/options.mk
@@ -1,17 +1,23 @@
-# $NetBSD: options.mk,v 1.5 2020/01/23 10:06:22 fox Exp $
+# $NetBSD: options.mk,v 1.6 2020/01/25 23:48:31 fox Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ircd-hybrid
-PKG_SUPPORTED_OPTIONS=	ssl
+
+PKG_OPTIONS_OPTIONAL_GROUPS=	ssl
+PKG_OPTIONS_GROUP.ssl=		gnutls ssl
+
 PKG_SUGGESTED_OPTIONS=	ssl
 
 .include "../../mk/bsd.options.mk"
 
 ###
-### Enable OpenSSL support
+### SSL support
 ###
 .if !empty(PKG_OPTIONS:Mssl)
-.include "../../security/openssl/buildlink3.mk"
+.  include "../../security/openssl/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-tls=openssl
+.elif !empty(PKG_OPTIONS:Mgnutls)
+.  include "../../security/gnutls/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-tls=gnutls
 .else
 CONFIGURE_ARGS+=	--with-tls=none
 .endif
