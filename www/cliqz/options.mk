7a4efd6b2e75c2d8f13185029755440c9e5da6b4
fox <fox@pkgsrc.org>
2020-01-25 17:12:17 +0000

    www/cliqz: Adds support for wayland.
    Should fix packaging issues due to missing wayland library.
    
    Thanks to nia@

--- www/cliqz/options.mk
+++ www/cliqz/options.mk
@@ -1,10 +1,15 @@
-# $NetBSD: options.mk,v 1.1 2019/04/19 14:02:03 fox Exp $
+# $NetBSD: options.mk,v 1.2 2020/01/25 17:12:17 fox Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.cliqz
 
-PKG_SUPPORTED_OPTIONS=	alsa pulseaudio dbus
+PKG_SUPPORTED_OPTIONS=	alsa pulseaudio dbus wayland
 
 PKG_SUGGESTED_OPTIONS+=	pulseaudio dbus
+
+.include "../../devel/wayland/platform.mk"
+.if ${PLATFORM_SUPPORTS_WAYLAND} == "yes"
+PKG_SUGGESTED_OPTIONS+= wayland
+.endif
 
 .include "../../mk/bsd.options.mk"
 
@@ -27,4 +32,10 @@ MOZ_CONFIGURE_ARGS+=	"ac_add_options --disable-pulseaudio"
 MOZ_CONFIGURE_ARGS+=	"ac_add_options --enable-dbus"
 .else
 MOZ_CONFIGURE_ARGS+=	"ac_add_options --disable-dbus"
+.endif
+
+PLIST_VARS+=		wayland
+.if !empty(PKG_OPTIONS:Mwayland)
+# \todo Instead of using an option, determine if gtk3 was built with wayland.
+PLIST.wayland=		yes
 .endif
