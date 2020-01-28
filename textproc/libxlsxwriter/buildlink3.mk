2f580b161e2e0929e7620fc6a59caa4363302a65
sjmulder <sjmulder@pkgsrc.org>
2020-01-28 14:32:48 +0000

    libxlsxwriter: Update to 0.9.4
    - Added option to specify worksheet "object positioning" for images and
      charts to define how they move or size with underlying cells. See
      ww_object_position_options and working_with_object_positioning.

--- textproc/libxlsxwriter/buildlink3.mk
+++ textproc/libxlsxwriter/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/01/15 14:36:07 sjmulder Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/01/28 14:32:48 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxlsxwriter
 
 .if !defined(LIBXLSXWRITER_BUILDLINK3_MK)
 LIBXLSXWRITER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=0.9.3
+BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=0.9.4
 BUILDLINK_PKGSRCDIR.libxlsxwriter?=	../../textproc/libxlsxwriter
 
 .include "../../devel/zlib/buildlink3.mk"
