2efd118b8aed11712cb7145b16e2f16d60613101
nia <nia@pkgsrc.org>
2020-01-28 13:31:34 +0000

    sublib: Remove missed reference to mono2

--- textproc/sublib/buildlink3.mk
+++ textproc/sublib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2019/04/03 00:33:09 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2020/01/28 13:31:34 nia Exp $
 
 BUILDLINK_TREE+=	sublib
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.sublib+=	sublib>=0.9
 BUILDLINK_ABI_DEPENDS.sublib+=	sublib>=0.9nb22
 BUILDLINK_PKGSRCDIR.sublib?=	../../textproc/sublib
 
-.include "../../lang/mono2/buildlink3.mk"
+.include "../../lang/mono/buildlink3.mk"
 .endif # SUBLIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-sublib
