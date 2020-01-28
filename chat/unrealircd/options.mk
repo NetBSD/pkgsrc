3f15b79023d0832b181dcf9b5c9f4cef8243133a
nia <nia@pkgsrc.org>
2020-01-28 13:03:36 +0000

    unrealircd: Update to 5.0.2
    Upgrade notes (seem like there are very few breaking changes):
    
         https://www.unrealircd.org/docs/Upgrading_from_4.x
    
    What's new in UnrealIRCd 5:
    
         * Channel history. You can now see the last couple of lines that have
           been said on channels when you JOIN. For this you need to set channel
           mode +H, eg: eg: /MODE #chan +H 15:1440
    
         * More IRCv3 features. Additional details are communicated to clients
           which may help with displaying information. Implemented specs are:
           account-tag, message-ids, time, echo-message, labeled-response and
           BATCH.
    
         * Ban exceptions (/ELINE). You can now exempt users dynamically on IRC
           from *LINES, spamfilter, throttling, blacklist checking, connection
           floods, bypassing antirandom, etc. Just type /ELINE on IRC to see
           details.
    
         * *LINES and Spamfilters are remembered: All of these are saved to a
           file every few minutes and saved across reboots. This uses the new
           tkldb module (loaded by default). No need for services for that
           anymore.
    
         * Persistent channels are remembered: For channels which have mode +P
           set we now save all channel settings across reboots (topic, regular
           modes and +beI lists). This via the channeldb module (loaded by
           default).
    
         * Anti connect-flood measures. In the last few 4.2.x versions we
           introduced Connthrottle and reputation. In 5.x these modules are now
           loaded by default for increased security.
    
         * Easily restrict commands to fight drones. You can now disable any
           command or impose restrictions, such as: command can only be executed
           after being connected for XX seconds, or if you are identified to
           services, etc. See the example for how to restrict LIST, INVITE and
           messaging.
    
         * Module manager for managing 3rd party modules easily. Install and
           update modules with a single command.
    
         * Condition configuration. You can have condition configuration where
           you e.g. @define $IP "203.0.113.1" and can use $IP everywhere in the
           configuration file. Similarly, support for @if-blocks. This is
           especially useful for advanced users who like to use the same
           configuration file on multiple machines, usually with the help of
           remote includes.
    
         * Improved Channel Mode +L now kicks in for any rejected join, so not
           just for +l but also for +b, +i, +O, +z, +R and +k. If, for example,
           the channel is +L #insecure and also +z then, when an insecure user
           ties to join they will be redirected to #insecure.
    
         * Ban forwards. New extended ban ~f to forward users to the specified
           channel if the ban matches. Example: MODE #chan +b
           ~f:#badisp:*!*@*.isp.org
    
         * Improved WebSocket support. We already supported websockets, but now
           we support websocket type 'text', which is compatible with web IRC
           clients such as Kiwi IRC.
    
         * Code cleanups. The biggest effort of all went into cleaning up old
           code and making the code much more readable. This also means that
           UnrealIRCd 5 will not be able to link with really older servers or
           services, like UnrealIRCd 3.2.x.
    
         * Windows version is 64-bits. This should allow for increased address
           space and security. This also means UnrealIRCd 5 will not run on
           32-bits Windows (should be rare nowadays, anyway)

--- chat/unrealircd/options.mk
+++ chat/unrealircd/options.mk
@@ -1,12 +1,11 @@
-# $NetBSD: options.mk,v 1.17 2019/02/09 15:23:15 nia Exp $
+# $NetBSD: options.mk,v 1.18 2020/01/28 13:03:36 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.unrealircd
 
 PKG_SUPPORTED_OPTIONS+=	unrealircd-remoteinc
 PKG_SUPPORTED_OPTIONS+=	unrealircd-showlistmodes
-PKG_SUPPORTED_OPTIONS+=	unrealircd-topicisnuhost unrealircd-shunnotices
 PKG_SUPPORTED_OPTIONS+=	unrealircd-operoverride-verify
-PKG_SUPPORTED_OPTIONS+=	unrealircd-no-operoverride unrealircd-disableusermod
+PKG_SUPPORTED_OPTIONS+=	unrealircd-no-operoverride
 PKG_SUGGESTED_OPTIONS=	unrealircd-showlistmodes
 
 .include "../../mk/bsd.options.mk"
@@ -27,32 +26,10 @@ CONFIGURE_ARGS+=		--with-showlistmodes
 .endif
 
 ###
-### /topic command to show the nick!user@host of the person
-### who set the topic, rather than just the nickname.
-###
-.if !empty(PKG_OPTIONS:Munrealircd-topicisnuhost)
-CONFIGURE_ARGS+=		--with-topicisnuhost
-.endif
-
-###
-### Notify a user when they are no longer shunned.
-###
-.if !empty(PKG_OPTIONS:Munrealircd-shunnotices)
-CONFIGURE_ARGS+=		--with-shunnotices
-.endif
-
-###
 ### Disable oper override.
 ###
 .if !empty(PKG_OPTIONS:Munrealircd-no-operoverride)
 CONFIGURE_ARGS+=		--with-no-operoverride
-.endif
-
-###
-### Disable /sethost, /setident, /chgname, /chghost, and /chgident.
-###
-.if !empty(PKG_OPTIONS:Munrealircd-disableusermod)
-CONFIGURE_ARGS+=		--with-disableusermod
 .endif
 
 ###
