f511e50f14aaa224fdfd0201a336e30786fe73eb
bsiegert <bsiegert@pkgsrc.org>
2020-01-29 13:39:33 +0000

    Pullup ticket #6129 - requested by taca lang/php72: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.286
    - lang/php72/Makefile                                           1.23-1.24
    - lang/php72/distinfo                                           1.51
    
    ---
       Module Name:	pkgsrc
       Committed By:	jperkin
       Date:		Sat Jan 18 21:51:16 UTC 2020
    
       Modified Files:
       	pkgsrc/lang/php72: Makefile
    
       Log Message:
       *: Recursive revision bump for openssl 1.1.1.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sat Jan 25 17:24:03 UTC 2020
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php72: Makefile distinfo
    
       Log Message:
       lang/php72: update to 7.2.27
    
       Update php72 to 7.2.27 (PHP 7.2.27).
    
       23 Jan 2020, PHP 7.2.27
    
       - Mbstring:
         . Fixed bug #79037 (global buffer-overflow in `mbfl_filt_conv_big5_wchar`).
           (CVE-2020-7060) (Nikita)
    
       - Session:
         . Fixed bug #79091 (heap use-after-free in session_create_id()). (cmb, Nikita)
    
       - Standard:
         . Fixed bug #79099 (OOB read in php_strip_tags_ex). (CVE-2020-7059). (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.283.4.2 2020/01/29 13:34:46 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.283.4.3 2020/01/29 13:39:33 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -88,7 +88,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
-PHP72_VERSION=	7.2.26
+PHP72_VERSION=	7.2.27
 PHP73_VERSION=	7.3.14
 PHP74_VERSION=	7.4.2
 
