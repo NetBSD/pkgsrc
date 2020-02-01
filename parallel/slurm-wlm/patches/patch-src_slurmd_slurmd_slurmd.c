66e791298eeb65348e94d23f2fff8201767e4d06
bacon <bacon@pkgsrc.org>
2020-02-01 00:15:01 +0000

    parallel/slurm-wlm: Upgrade to 19.05.5
    CVE-2019-6438 resolved upstream
    Numerous enhancements and bug fixes
    Remove some old patches and hacks

--- parallel/slurm-wlm/patches/patch-src_slurmd_slurmd_slurmd.c
+++ parallel/slurm-wlm/patches/patch-src_slurmd_slurmd_slurmd.c
@@ -1,29 +1,30 @@
-$NetBSD: patch-src_slurmd_slurmd_slurmd.c,v 1.1 2018/04/22 21:31:09 bacon Exp $
+$NetBSD: patch-src_slurmd_slurmd_slurmd.c,v 1.2 2020/02/01 00:15:02 bacon Exp $
 
-# cpuset functions not standardized across platforms
+# NetBSD syntax
 
---- src/slurmd/slurmd/slurmd.c.orig	2018-01-04 21:15:01.000000000 +0000
+--- src/slurmd/slurmd/slurmd.c.orig	2020-01-19 02:36:16.111141994 +0000
 +++ src/slurmd/slurmd/slurmd.c
-@@ -2067,7 +2067,13 @@ static int _core_spec_init(void)
+@@ -2295,7 +2295,13 @@ static int _core_spec_init(void)
  	uint32_t task_params;
  	bool slurmd_off_spec;
  	bitstr_t *res_mac_bitmap;
+-	cpu_set_t mask;
 +#ifdef __NetBSD__
 +	cpuset_t *mask = cpuset_create();
 +#define	CPU_SET_SIZE	cpuset_size(mask)
 +#else
- 	cpu_set_t mask;
++ 	cpu_set_t mask;
 +#define	CPU_SET_SIZE	sizeof(cpu_set_t)
 +#endif
  
  	if ((conf->core_spec_cnt == 0) && (conf->cpu_spec_list == NULL)) {
  		debug("Resource spec: No specialized cores configured by "
-@@ -2165,7 +2171,7 @@ static int _core_spec_init(void)
+@@ -2393,7 +2399,7 @@ static int _core_spec_init(void)
  		rval = cpuset_setaffinity(CPU_LEVEL_WHICH, CPU_WHICH_PID,
  					  pid, sizeof(cpu_set_t), &mask);
- #elif defined(SCHED_GETAFFINITY_THREE_ARGS)
+ #else
 -		rval = sched_setaffinity(pid, sizeof(cpu_set_t), &mask);
 +		rval = sched_setaffinity(pid, CPU_SET_SIZE, &mask);
- #else
- 		rval = sched_setaffinity(pid, &mask);
  #endif
+ 
+ 		if (rval != 0) {
--- parallel/slurm-wlm/patches/patch-src_slurmd_slurmstepd_task.c
+++ parallel/slurm-wlm/patches/patch-src_slurmd_slurmstepd_task.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_slurmd_slurmstepd_task.c,v 1.1 2018/04/22 21:31:09 bacon Exp $
+$NetBSD: patch-src_slurmd_slurmstepd_task.c,v 1.2 2020/02/01 00:15:02 bacon Exp $
 
 # Linux compat
 
---- src/slurmd/slurmstepd/task.c.orig	2016-05-03 22:41:59.000000000 +0000
+--- src/slurmd/slurmstepd/task.c.orig	2019-12-20 20:44:51.000000000 +0000
 +++ src/slurmd/slurmstepd/task.c
-@@ -558,6 +558,18 @@ _make_tmpdir(stepd_step_rec_t *job)
+@@ -573,6 +573,12 @@ _make_tmpdir(stepd_step_rec_t *job)
  		 * still work with older systems we include this check.
  		 */
  
@@ -14,12 +14,6 @@ $NetBSD: patch-src_slurmd_slurmstepd_task.c,v 1.1 2018/04/22 21:31:09 bacon Exp 
 +#define HAVE_EACCESS 1
 +#endif
 +  
-+// Unsure about the reason for this patch, but it dates back to SLURM 2.6.4
-+//#if defined(__FreeBSD__) || defined(__NetBSD__)
-+//#define __GLIBC__               (1) 
-+//#define __GLIBC_PREREQ(a,b)     (1) 
-+//#endif
-+
  #if defined(HAVE_FACCESSAT)
  		else if (faccessat(AT_FDCWD, tmpdir, X_OK|W_OK, AT_EACCESS))
  #elif defined(HAVE_EACCESS)
