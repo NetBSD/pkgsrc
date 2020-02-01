a9b8aeac8242867e663cb2a162202f55821ac193
bacon <bacon@pkgsrc.org>
2020-02-01 00:15:01 +0000

    parallel/slurm-wlm: Upgrade to 19.05.5
    CVE-2019-6438 resolved upstream
    Numerous enhancements and bug fixes
    Remove some old patches and hacks

--- parallel/slurm-wlm/patches/patch-src_slurmd_common_task__plugin.c
+++ parallel/slurm-wlm/patches/patch-src_slurmd_common_task__plugin.c
@@ -1,29 +1,31 @@
-$NetBSD: patch-src_slurmd_common_task__plugin.c,v 1.1 2018/04/22 21:31:09 bacon Exp $
+$NetBSD: patch-src_slurmd_common_task__plugin.c,v 1.2 2020/02/01 00:15:02 bacon Exp $
 
-# cpuset functions not standardized across platforms
+# NetBSD specifics
 
---- src/slurmd/common/task_plugin.c.orig	2018-01-05 18:03:10.000000000 +0000
+--- src/slurmd/common/task_plugin.c.orig	2020-01-19 02:41:02.593115935 +0000
 +++ src/slurmd/common/task_plugin.c
-@@ -542,7 +542,11 @@ extern void task_slurm_chkaffinity(cpu_s
- 				   int statval)
- {
+@@ -514,7 +514,11 @@ extern void task_slurm_chkaffinity(cpu_s
+ 	fatal("%s: not supported on macOS", __func__);
+ #else
  	char *bind_type, *action, *status, *units;
+-	char mstr[1 + CPU_SETSIZE / 4];
 +#ifdef __NetBSD__
 +	char mstr[1 + cpuset_size(mask) / 4];
 +#else
- 	char mstr[1 + CPU_SETSIZE / 4];
++ 	char mstr[1 + CPU_SETSIZE / 4];
 +#endif
  	int task_gid = job->envtp->procid;
  	int task_lid = job->envtp->localid;
  	pid_t mypid = job->envtp->task_pid;
-@@ -609,7 +613,11 @@ extern char *task_cpuset_to_str(const cp
+@@ -585,7 +589,11 @@ extern char *task_cpuset_to_str(const cp
  	char *ptr = str;
  	char *ret = NULL;
  
+-	for (base = CPU_SETSIZE - 4; base >= 0; base -= 4) {
 +#ifdef __NetBSD__
 +	for (base = cpuset_size(mask) - 4; base >= 0; base -= 4) {
 +#else
- 	for (base = CPU_SETSIZE - 4; base >= 0; base -= 4) {
++ 	for (base = CPU_SETSIZE - 4; base >= 0; base -= 4) {
 +#endif
  		char val = 0;
  		if (CPU_ISSET(base, mask))
