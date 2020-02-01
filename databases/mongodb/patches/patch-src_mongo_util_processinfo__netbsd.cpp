a9ffeb6156db69607ff6cfbcf4e85748bf884dfc
adam <adam@pkgsrc.org>
2020-02-01 20:00:08 +0000

    mongodb4: updated to 4.2.3
    4.2.3:
    Issues fixed:
    SERVER-42565: Aggregations and find commands sort missing fields differently’
    SERVER-44174: $push and $addToSet should restrict memory usage
    SERVER-40435: A clearJumboFlag command to clear the jumbo flag
    SERVER-45270: Increased vulnerability to slow DNS
    TOOLS-1952: Use –forceTableScan by default when running against WiredTiger nodes
    TOOLS-2453: Index keys not escaped correctly
    SERVER-45396: fix the “me” field in isMaster responses when using splithorizon
    SERVER-45309: Ensure bind credentials live longer than LDAP operations
    WT-5120: Checkpoint hangs when reconciliation doesn’t release the eviction generation
    
    4.2.2:
    Issues fixed:
    SERVER-31083: Allow passing primary shard to “enableSharding” command for a new database
    SERVER-33272: The DatabaseHolder::close() function no longer requires a global write lock and neither does the dropDatabase command
    SERVER-44050: Arrays along ‘hashed’ index key path are not correctly rejected
    SERVER-43882: Building indexes for startup recovery uses unowned RecordData after yielding its cursor
    SERVER-44617: $regexFind crash when one of the capture group doesn’t match the input but pattern matches
    SERVER-44721: Shell KMS AWS support cannot decrypt responses
    WT-4961: Checkpoints with cache overflow must keep history for reads
    
    4.2.1:
    Issues fixed:
    SERVER-37768: Platform Support: Add Community & Enterprise Debian 10 x64
    SERVER-37772: Platform Support: Add Community & Enterprise RHEL 8 x64
    SERVER-41506: Track metrics associated with a node calling an election
    SERVER-41499: Track number of elections called for each reason in serverStatus
    SERVER-42518: Wildcard index plans miss results when the query path has multiple subsequent array indexes
    SERVER-42856: Transactions with write can be sent to the wrong shard

--- databases/mongodb/patches/patch-src_mongo_util_processinfo__netbsd.cpp
+++ databases/mongodb/patches/patch-src_mongo_util_processinfo__netbsd.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_mongo_util_processinfo__netbsd.cpp,v 1.3 2019/03/05 19:35:58 adam Exp $
+$NetBSD: patch-src_mongo_util_processinfo__netbsd.cpp,v 1.4 2020/02/01 20:00:08 adam Exp $
 
-NetBSD support.
+Add NetBSD support.
 
---- src/mongo/util/processinfo_netbsd.cpp.orig	2019-03-04 22:59:41.311405065 +0000
+--- src/mongo/util/processinfo_netbsd.cpp.orig	2020-01-27 09:41:08.452841019 +0000
 +++ src/mongo/util/processinfo_netbsd.cpp
-@@ -27,6 +27,7 @@
+@@ -26,6 +26,7 @@
   *    exception statement from all source files in the program, then also delete
   *    it in the license file.
   */
@@ -12,7 +12,7 @@ NetBSD support.
  
  #define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kControl
  
-@@ -39,7 +40,6 @@
+@@ -38,7 +39,6 @@
  #include <sys/param.h>
  #include <sys/sysctl.h>
  #include <sys/types.h>
@@ -20,7 +20,16 @@ NetBSD support.
  #include <sys/vmmeter.h>
  #include <unistd.h>
  
-@@ -112,7 +112,7 @@ int ProcessInfo::getVirtualMemorySize() 
+@@ -85,7 +85,7 @@ template <>
+ int getSysctlByIDWithDefault<std::string>(const int* sysctlID,
+                                           const int idLen,
+                                           const std::string& defaultValue,
+-                                          string* result) {
++                                          std::string* result) {
+     char value[256] = {0};
+     size_t len = sizeof(value);
+     if (sysctl(sysctlID, idLen, &value, &len, NULL, 0) == -1) {
+@@ -109,7 +109,7 @@ int ProcessInfo::getVirtualMemorySize() 
          return -1;
      }
  
@@ -29,7 +38,7 @@ NetBSD support.
      int vss = ((task->p_vm_dsize + task->p_vm_ssize + task->p_vm_tsize) * sysconf(_SC_PAGESIZE)) /
          1048576;
      kvm_close(kd);
-@@ -127,7 +127,7 @@ int ProcessInfo::getResidentSize() {
+@@ -124,7 +124,7 @@ int ProcessInfo::getResidentSize() {
          log() << "Unable to get res mem size: " << err;
          return -1;
      }
@@ -38,3 +47,12 @@ NetBSD support.
      int rss = (task->p_vm_rssize * sysconf(_SC_PAGESIZE)) / 1048576;  // convert from pages to MB
      kvm_close(kd);
      return rss;
+@@ -136,7 +136,7 @@ double ProcessInfo::getSystemMemoryPress
+ 
+ void ProcessInfo::SystemInfo::collectSystemInfo() {
+     osType = "BSD";
+-    osName = "OpenBSD";
++    osName = "NetBSD";
+     int mib[2];
+ 
+     mib[0] = CTL_KERN;
