3b56aa9e96b7cf5b932a363694c32d72459c0f7e
triaxx <triaxx@pkgsrc.org>
2020-02-03 09:12:32 +0000

    collectd-dns: fix PR pkg/54304
    pkgsrc changes:
    ---------------
      * Update patch to undef DLT_IEEE802_15_4 when already defined in
        /usr/include/net/dlt.h.
      * Bump revision.

--- sysutils/collectd/patches/patch-src_utils__dns.c
+++ sysutils/collectd/patches/patch-src_utils__dns.c
@@ -1,14 +1,18 @@
-$NetBSD: patch-src_utils__dns.c,v 1.2 2016/12/07 17:28:39 fhajny Exp $
+$NetBSD: patch-src_utils__dns.c,v 1.3 2020/02/03 09:12:32 triaxx Exp $
 
-Resolve conflict with pkgsrc PCAP on NetBSD 7.
---- src/utils_dns.c.orig	2016-11-30 08:52:01.328910078 +0000
+Resolve conflict with pkgsrc PCAP on NetBSD 7 and 8.
+
+--- src/utils_dns.c.orig	2017-11-18 09:03:27.370749644 +0000
 +++ src/utils_dns.c
-@@ -91,6 +91,9 @@
+@@ -91,6 +91,12 @@
  #endif
  
  #if HAVE_PCAP_H
 +# if defined(DLT_MATCHING_MAX)
 +#  undef DLT_MATCHING_MAX
++# endif
++# if defined(DLT_IEEE802_15_4)
++#  undef DLT_IEEE802_15_4
 +# endif
  #include <pcap.h>
  #endif
