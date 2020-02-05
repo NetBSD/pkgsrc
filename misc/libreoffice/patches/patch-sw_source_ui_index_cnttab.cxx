ebe3f90fc742f1b33b7e1c7116e6dc11afc07757
ryoon <ryoon@pkgsrc.org>
2020-02-05 14:10:35 +0000

    libreoffice: Update to 6.4.0.3
    * Do not use PDFium's internal freetype2.
      Not tested with X11_TYPE=native yet.
    * GTK2 VCL is removed.
    
    Changelog:
    # Writer
    Added option to mark comments as resolved.
    Fix missing change tracking layout of numbers and bullets of lists.
    The btLr text direction is now available.
    It is now possible to comment on Writer images and charts.
    It is now possible to automatically avoid overlapping shapes with a new wrap option.
    Improved performance when importing files with many bookmarks.
    Selection of drawing objects anchored at-paragraph works more consistently and user-friendly now.
    
    ## Tables
    Important improvements in table handling.
    Improved drag & drop mouse operations to move table data.
    
    # Calc
    Improved selecting cells which contain hyperlinks.
    n MM:SS or [MM]:SS or MM:SS.00 or [MM]:SS.00 pre-formatted cells a two digit groups input like 12:34 is now accepted as minutes:seconds value instead of the usual hours:minutes:00 value; to force the usual hours:minutes:seconds input on such cells enter three digit groups like 12:34:00.
    Improved scalability of formula-groups computation on CPUs with large number of cores by potentially threading multiple independent formula-groups together.
    Implemented a parallelized version of super scalar sample sort algorithm to speed up sorting. This is now used in pivot-table creation where some sorting is required.
    Shading of columns/rows headers is removed; the headers are drawn flat now.
    Deleted a 15 symbols limit to password length for XLSX files.
    Performance improvements.
    Full-Sheet Previews.
    
    # Impress & Draw
    Added 'Remove Hyperlink' to context menu in Impress.
    Added 'Consolidate Text' which combines multiple selected textboxes into one (primarily to simplify editing fragmented content from imported PDFs).
    Interaction now only executed in presentation mode (when clicking on an object). For edit mode, a context menu "Execute interaction" was added.
    Image Maps now respect the setting to require Ctrl+Click to follow the hyperlink.
    Hyperlink in Image Maps now work in Slideshow too.
    Performance improvements.
    
    # Base
    Access2Base callable from Python.
    
    # BASIC
    Fix hex number conversion.
    Improvements in VBA handling.
    Fix handling for function that does not has closing parenthesis.
    
    # Core / General
    Internal Paths are now displayed in the UI.
    Add checkbox to enable/disable sending crash reports to TDF.
    Hyperlink context menus were unified. Every hyperlink now has the following context menu entries.
    Generate QR Code feature is added in LO.

--- misc/libreoffice/patches/patch-sw_source_ui_index_cnttab.cxx
+++ misc/libreoffice/patches/patch-sw_source_ui_index_cnttab.cxx
@@ -1,13 +1,13 @@
-$NetBSD: patch-sw_source_ui_index_cnttab.cxx,v 1.1 2019/08/10 11:41:29 ryoon Exp $
+$NetBSD: patch-sw_source_ui_index_cnttab.cxx,v 1.2 2020/02/05 14:10:35 ryoon Exp $
 
---- sw/source/ui/index/cnttab.cxx.orig	2019-08-06 17:26:35.000000000 +0000
+--- sw/source/ui/index/cnttab.cxx.orig	2020-01-22 23:14:16.000000000 +0000
 +++ sw/source/ui/index/cnttab.cxx
-@@ -1554,7 +1554,7 @@ void SwTOXEdit::AdjustSize()
+@@ -1545,7 +1545,7 @@ void SwTOXEdit::AdjustSize()
  {
      auto nWidth = m_xEntry->get_pixel_size(GetText()).Width();
      float fChars = nWidth / m_xEntry->get_approximate_digit_width();
--    m_xEntry->set_width_chars(std::max(1.0f, ceil(fChars)));
-+    m_xEntry->set_width_chars(std::max((double)1.0f, (double)ceil(fChars)));
+-    m_xEntry->set_width_chars(std::max(1.0f, std::ceil(fChars)));
++    m_xEntry->set_width_chars(std::max((double)1.0f, (double)(std::ceil(fChars))));
  }
  
  class SwTOXButton : public SwTOXWidget
