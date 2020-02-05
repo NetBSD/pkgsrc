507bcf76fbe2a121d2a7459cfd2bcd1ad3c03cae
ryoon <ryoon@pkgsrc.org>
2020-02-05 13:26:09 +0000

    mdds: Update to 1.5.0
    Changelog:
    mdds 1.5.0
    
    * documentation
    
      * moved the documentation hosting to readthedocs.io, and adjusted the build
        steps.
    
      * moved the API incompatibility notes from README to the rst doc.
    
      * added the overview section for flat_segment_tree.
    
    * multi_type_vector
    
      * fixed the static get(const const_position_type& pos) method for the
        boolean_element_block, by adding specialization for it to work around the
        issue with std::vector<bool> not having the at() method.
    
      * fixed an issue with the const position() method not returning a valid end
        position the same way the non-const variant does.
    
      * added steps to traverse blocks backward from the postiion specified in the
        position hint.  This may result in improved performance in some
        situations.
    
      * the standard integer blocks now use fixed size integer types i.e.
    
        * (u)int8_t
    
        * (u)int16_t
    
        * (u)int32_t
    
        * (u)int64_t
    
      * The numeric_element_block has been renamed to double_element_block.
    
      * added new block type to store float element values.
    
    * general
    
      * added gdb pretty printers that prints the contents of the data structures.

--- devel/mdds/buildlink3.mk
+++ devel/mdds/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2016/02/13 08:40:22 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/02/05 13:26:09 ryoon Exp $
 
 BUILDLINK_TREE+=	mdds
 
 .if !defined(MDDS_BUILDLINK3_MK)
 MDDS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mdds+=	mdds>=1.0.0
+BUILDLINK_API_DEPENDS.mdds+=	mdds>=1.5.0
 BUILDLINK_PKGSRCDIR.mdds?=	../../devel/mdds
 # only contains header files
 BUILDLINK_DEPMETHOD.mdds?=	build
