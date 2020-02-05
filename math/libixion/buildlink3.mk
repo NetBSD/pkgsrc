2aaca68fafcd70166ca81bcb149fbc2f9223147e
ryoon <ryoon@pkgsrc.org>
2020-02-05 13:46:13 +0000

    libixion, py-libixion: Update to 0.15.0
    Changelog:
    ixion 0.15.0
    
    * fill_down_cells() method has been added to model_context, to fill duplicate
      values downward from an arbitrary cell position.  For now, it always
      duplicate the value of the source cell; cell value enumerations are not
      supported.  Also, filling down of formula cells is not yet supported.
    
    * cell iterator has been aded to model_context, for efficient horizontal and
      vertical iterations of cell values in an arbitrary range within a sheet.
    
    * improved the cmake-based build, with all test cases integrated.
    
    * switched to spdlog for compile-time debug log outputs.
    
    * improved performance by removing repeated fetching of each formula
      cell prior to interpretation.
    
    * reworked topological sorting of formula cells to avoid having to
      unnecessarily build dependency relations twice; once during the
      dirty cell determination and twice prior to interpretation of
      formula cells.  The new code performs topological sorting during the
      dirty cell determination phase.
    
    * reworked the cell reference tracker to use R-tree.  This allows more
      accurate tracking of references involving grouped formulas.
    
    * fixed a resource leak with dynamic loading of compute engine modules.

--- math/libixion/buildlink3.mk
+++ math/libixion/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2019/02/22 06:15:50 gutteridge Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/02/05 13:46:13 ryoon Exp $
 
 BUILDLINK_TREE+=	libixion
 
 .if !defined(LIBIXION_BUILDLINK3_MK)
 LIBIXION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libixion+=	libixion>=0.14.0
+BUILDLINK_API_DEPENDS.libixion+=	libixion>=0.15.0
 BUILDLINK_PKGSRCDIR.libixion?=		../../math/libixion
 
 .include "../../devel/mdds/buildlink3.mk"
