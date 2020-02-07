b3e048297b84d430a3f938085eaa16db1b0ce64e
ryoon <ryoon@pkgsrc.org>
2020-02-07 12:18:59 +0000

    screen: Update to 4.8.0
    Changelog:
    Version 4.8.0 (05/02/2020)
      * Improve startup time by only polling for files to close
      Fixes:
            - Fix for segfault if termcap doesn't have Km entry
            - Make screen exit code be 0 when checking --version
            - Fix potential memory corruption when using OSC 49

--- misc/screen/patches/patch-tty.sh
+++ misc/screen/patches/patch-tty.sh
@@ -1,23 +1,24 @@
-$NetBSD: patch-tty.sh,v 1.4 2017/07/18 23:02:18 kim Exp $
+$NetBSD: patch-tty.sh,v 1.5 2020/02/07 12:19:00 ryoon Exp $
 
 Change condition for using TIOCSCTTY.
 
 NetBSD on the netbsd-5 branch insists on a non-NULL second argument
 to the realpath() function.  Make it so.
 
---- tty.sh.orig	2017-07-10 19:26:25.000000000 +0000
-+++ tty.sh	2017-07-18 22:40:56.000000000 +0000
-@@ -813,7 +813,8 @@
+--- tty.sh.orig	2020-02-05 20:09:38.000000000 +0000
++++ tty.sh
+@@ -784,7 +784,9 @@ int fgtty(int fd)
    /*
     * Under BSD we have to set the controlling terminal again explicitly.
     */
--# if (defined(__FreeBSD_kernel__) || defined(__DragonFly__) || defined(__GNU__)) && defined(TIOCSCTTY)
+-# if (defined(__FreeBSD_kernel__) || defined(__DragonFly__) || defined(__GNU__) || defined(__OpenBSD__)) && defined(TIOCSCTTY)
 +# if (defined(__FreeBSD_kernel__) || defined(__DragonFly__) \
-+      || defined(__GNU__) || (BSD >= 199103)) && defined(TIOCSCTTY)
++	|| defined(__GNU__) || (BSD >= 199103) || defined(__OpenBSD__)) \
++	&& defined(TIOCSCTTY)
    ioctl(fd, TIOCSCTTY, (char *)0);
  # endif
  
-@@ -1521,8 +1522,10 @@
+@@ -1485,8 +1487,10 @@ int CheckTtyname (char *tty)
    char realbuf[PATH_MAX];
    const char *real;
    int rc;
