bc8b895cd62dc3691c69a0119b2c902df2144b65
nia <nia@pkgsrc.org>
2020-02-08 16:34:55 +0000

    libmysofa: Update to 1.0
    - Fixed CVE-2020-6860
    - Support for netcdf 4.7.2 generated files
    - Support for user-defined variables

--- audio/libmysofa/buildlink3.mk
+++ audio/libmysofa/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/11/30 13:35:42 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/02/08 16:34:55 nia Exp $
 
 BUILDLINK_TREE+=	libmysofa
 
 .if !defined(LIBMYSOFA_BUILDLINK3_MK)
 LIBMYSOFA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libmysofa+=	libmysofa>=0.9
+BUILDLINK_API_DEPENDS.libmysofa+=	libmysofa>=1.0
 BUILDLINK_PKGSRCDIR.libmysofa?=		../../audio/libmysofa
 
 .include "../../devel/zlib/buildlink3.mk"
