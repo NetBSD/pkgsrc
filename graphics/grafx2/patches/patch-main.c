fff7edf021ad0e6a5e7031dcff6ca63c179fa802
ryoon <ryoon@pkgsrc.org>
2020-02-10 13:08:10 +0000

    grafx2: Update to 2.7.2950
    Changelog:
    Not available.

--- graphics/grafx2/patches/patch-main.c
+++ graphics/grafx2/patches/patch-main.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-main.c,v 1.1 2018/07/17 13:07:50 ryoon Exp $
+$NetBSD: patch-main.c,v 1.2 2020/02/10 13:08:10 ryoon Exp $
 
 * Enable non-absolute path execute
 
---- main.c.orig	2018-05-15 16:51:04.000000000 +0000
+--- main.c.orig	2020-02-06 21:01:27.000000000 +0000
 +++ main.c
-@@ -544,7 +544,7 @@ int Init_program(int argc,char * argv[])
+@@ -654,7 +654,7 @@ int Init_program(int argc,char * argv[])
    Init_list_of_pages(Spare.backups);
  
    // Determine the executable directory
--  Set_program_directory(argv[0],program_directory);
-+  Set_program_directory("@PREFIX@/bin/grafx2",program_directory);
+-  program_directory = Get_program_directory(argv[0]);
++  program_directory = Get_program_directory("@PREFIX@/bin/grafx2-sdl");
    // Choose directory for data (read only)
-   Set_data_directory(program_directory,Data_directory);
+   Data_directory = Get_data_directory(program_directory);
    // Choose directory for settings (read/write)
