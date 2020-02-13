08654b98931b7d92141705ae0483e9c50532153f
adam <adam@pkgsrc.org>
2020-02-13 08:35:18 +0000

    py-bsddb3: updated to 6.2.7
    6.2.7:
    * Update copyright notices.
    * https links.
    * Fix Python 3 deprecation warning.
    * Fix compilation Python 3.8 deprecation warnings.
    * Fix compilation Python 3.9 deprecation warnings.
    * Python 3.8 and 3.9 are explicitly supported.

--- databases/py-bsddb3/buildlink3.mk
+++ databases/py-bsddb3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2010/06/02 12:22:59 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2020/02/13 08:35:18 adam Exp $
 
 BUILDLINK_TREE+=	py-bsddb3
 
@@ -12,7 +12,6 @@ BUILDLINK_API_DEPENDS.py-bsddb3+=	${PYPKGPREFIX}-bsddb3>=4.7.2
 BUILDLINK_ABI_DEPENDS.py-bsddb3+=	${PYPKGPREFIX}-bsddb3>=4.7.2
 BUILDLINK_PKGSRCDIR.py-bsddb3?=		../../databases/py-bsddb3
 
-BDB_ACCEPTED?=		db4 db5
 .include "../../mk/bdb.buildlink3.mk"
 .endif # PY_BSDDB3_BUILDLINK3_MK
 
