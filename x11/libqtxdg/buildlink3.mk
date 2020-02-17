1d2b3e29050d59b4fd9759ffe753d660945784a0
gutteridge <gutteridge@pkgsrc.org>
2020-02-17 19:50:34 +0000

    libqtxdg: import new package, version 3.4.0
    This is a library providing freedesktop.org specs implementations for
    Qt.
    
    Initial packaging in pkgsrc-wip by pin, as part of a broader effort
    toward providing all of LXQt and associated utility applications.
    (Additional build dependency and portability fixes by myself.)

---
+++ x11/libqtxdg/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/02/17 19:50:34 gutteridge Exp $
+
+BUILDLINK_TREE+=	libqtxdg
+
+.if !defined(LIBQTXDG_BUILDLINK3_MK)
+LIBQTXDG_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libqtxdg+=	libqtxdg>=3.4.0
+BUILDLINK_PKGSRCDIR.libqtxdg?=		../../x11/libqtxdg
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtsvg/buildlink3.mk"
+.endif	# LIBQTXDG_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libqtxdg
