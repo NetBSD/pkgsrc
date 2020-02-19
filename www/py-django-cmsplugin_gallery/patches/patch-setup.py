b23d0cc4f35986d07f638904b8e951572a3b091b
adam <adam@pkgsrc.org>
2020-02-19 11:09:52 +0000

    py-django-cmsplugin_gallery: updated to 1.1.7
    1.1.7:
    Unknown changes
    
    1.1.4:
    Add filer support and migration for existing plugins to filer based ecosystem.
    Add better support for Python3
    Fix all tests for the plugins

--- www/py-django-cmsplugin_gallery/patches/patch-setup.py
+++ www/py-django-cmsplugin_gallery/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.2 2018/04/07 20:09:37 adam Exp $
+$NetBSD: patch-setup.py,v 1.3 2020/02/19 11:09:52 adam Exp $
 
 Do not install tests.
 
---- setup.py.orig	2018-03-14 18:01:43.000000000 +0000
+--- setup.py.orig	2018-05-03 10:53:37.000000000 +0000
 +++ setup.py
 @@ -11,7 +11,7 @@ setup(
      description = 'DjangoCMS image gallery plugin with drag&drop '
                    'reordering in admin, support for thumbnails and '
                    'jQueryTOOLS overlay.',
 -    packages=find_packages(),
-+    packages=['cmsplugin_gallery'],
++    packages=find_packages(exclude=['tests']),
      provides=['cmsplugin_gallery', ],
      include_package_data=True,
-     install_requires = ['django-cms>=3.2.0', 'django-inline-ordering>=0.1.1', 'easy-thumbnails',]
+     install_requires = ['django-cms>=3.2.0', 'django-inline-ordering>=0.1.1',
