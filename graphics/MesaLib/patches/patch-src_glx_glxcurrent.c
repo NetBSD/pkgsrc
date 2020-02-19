4d4c22f72029804f0f1d182a74915cb0e5ea02e5
tnn <tnn@pkgsrc.org>
2020-02-19 21:03:41 +0000

    MesaLib: fix issues in TLS dispatch code on NetBSD/amd64. Bump.
    - workaround for toolchain/50277 had bitrotted after meson switch
    - make tls dispatch stub template behave correct (with input from joerg@)
    - double alignment of stubs to accommodate slightly larger code size
    
    Fixes among other things Xorg crash in glamor_egl_init().

--- graphics/MesaLib/patches/patch-src_glx_glxcurrent.c
+++ graphics/MesaLib/patches/patch-src_glx_glxcurrent.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-src_glx_glxcurrent.c,v 1.3 2019/08/21 13:35:28 nia Exp $
+$NetBSD: patch-src_glx_glxcurrent.c,v 1.4 2020/02/19 21:03:41 tnn Exp $
 
 Interim fix for toolchain/50277.
 
 NetBSD only supports zero-initialized initial-exec tls variables in conjuction
 with dlopen(3) at the moment.
 
---- src/glx/glxcurrent.c.orig	2018-02-09 02:17:59.000000000 +0000
+--- src/glx/glxcurrent.c.orig	2019-12-04 22:10:12.000000000 +0000
 +++ src/glx/glxcurrent.c
-@@ -40,6 +40,18 @@
+@@ -40,6 +40,20 @@
  #include "glx_error.h"
  
  /*
@@ -15,10 +15,12 @@ with dlopen(3) at the moment.
 + * We need these to not be extern in libGL.so because of
 + * PR toolchain/50277
 + */
-+#if defined(GLX_USE_TLS) && defined(__NetBSD__)
-+_X_EXPORT __thread struct _glapi_table * _glapi_tls_Dispatch
++#if defined(USE_ELF_TLS) && defined(__NetBSD__)
++#define u_current_table _glapi_tls_Dispatch
++#define u_current_context _glapi_tls_Context
++_X_EXPORT __thread struct _glapi_table *u_current_table
 +    __attribute__((tls_model("initial-exec"))) = NULL;
-+_X_EXPORT __thread void * _glapi_tls_Context
++_X_EXPORT __thread void * u_current_context
 +    __attribute__((tls_model("initial-exec")));
 +#endif
 +
@@ -26,7 +28,7 @@ with dlopen(3) at the moment.
  ** We setup some dummy structures here so that the API can be used
  ** even if no context is current.
  */
-@@ -77,7 +89,11 @@ _X_HIDDEN pthread_mutex_t __glXmutex = P
+@@ -77,7 +91,11 @@ _X_HIDDEN pthread_mutex_t __glXmutex = P
   * \c __glXGetCurrentContext can be implemented as trivial macro.
   */
  __thread void *__glX_tls_Context __attribute__ ((tls_model("initial-exec")))
