821298c3106fce55aad2a6b05591e990572984a2
gutteridge <gutteridge@pkgsrc.org>
2020-02-20 05:31:20 +0000

    liblxqt: import new package, version 0.14.1
    This is the core utility library for all LXQt components.
    
    Packaged in pkgsrc-wip by pin, as part of a broader effort toward
    providing all of LXQt and associated utility applications. General
    reviews, feedback, and testing of such by gdt@, ng0@, and myself.

---
+++ x11/liblxqt/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/02/20 05:31:20 gutteridge Exp $
+
+BUILDLINK_TREE+=	liblxqt
+
+.if !defined(LIBLXQT_BUILDLINK3_MK)
+LIBLXQT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=0.14.1
+BUILDLINK_PKGSRCDIR.liblxqt?=	../../x11/liblxqt
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtx11extras/buildlink3.mk"
+.include "../../x11/kwindowsystem/buildlink3.mk"
+.include "../../x11/libqtxdg/buildlink3.mk"
+.include "../../x11/libXScrnSaver/buildlink3.mk"
+.include "../../security/polkit-qt5/buildlink3.mk"
+.endif	# LIBLXQT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-liblxqt
