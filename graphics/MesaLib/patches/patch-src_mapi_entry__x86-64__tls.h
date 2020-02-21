ea1f592c5cddbeac830e831fb821477df268aa8a
tnn <tnn@pkgsrc.org>
2020-02-21 21:52:24 +0000

    MesaLib: assorted fixes
    - must use march=i586 on i386 for 64-bit atomic CAS
    - call through @plt from position independent code
    - put back the stock STUB_ASM_CODE for non-NetBSD

--- graphics/MesaLib/patches/patch-src_mapi_entry__x86-64__tls.h
+++ graphics/MesaLib/patches/patch-src_mapi_entry__x86-64__tls.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_mapi_entry__x86-64__tls.h,v 1.4 2020/02/19 21:03:41 tnn Exp $
+$NetBSD: patch-src_mapi_entry__x86-64__tls.h,v 1.5 2020/02/21 21:52:24 tnn Exp $
 
 NetBSD only supports zero-initialized initial-exec tls variables in conjuction
 with dlopen(3) at the moment.
@@ -8,7 +8,7 @@ es1api and es2api are not going to be patched for NetBSD.
 
 --- src/mapi/entry_x86-64_tls.h.orig	2019-12-04 22:10:14.000000000 +0000
 +++ src/mapi/entry_x86-64_tls.h
-@@ -32,13 +32,13 @@
+@@ -32,21 +32,33 @@
  #endif
  
  __asm__(".text\n"
@@ -24,21 +24,27 @@ es1api and es2api are not going to be patched for NetBSD.
     func ":"
  
  #ifndef __ILP32__
-@@ -46,7 +46,12 @@ __asm__(".text\n"
- #define STUB_ASM_CODE(slot)                              \
-    "movq " ENTRY_CURRENT_TABLE "@GOTTPOFF(%rip), %rax\n\t"  \
-    "movq %fs:(%rax), %r11\n\t"                           \
--   "jmp *(8 * " slot ")(%r11)"
+ 
++#if defined(__NetBSD__)
++#define STUB_ASM_CODE(slot)                              \
++   "movq " ENTRY_CURRENT_TABLE "@GOTTPOFF(%rip), %rax\n\t"  \
++   "movq %fs:(%rax), %r11\n\t"                           \
 +   "testq %r11, %r11\n\t"                                \
 +   "je 1f\n\t"                                           \
 +   "jmp *(8 * " slot ")(%r11)\n\t"                       \
 +   "1:\n\t"                                              \
-+   "call " ENTRY_CURRENT_TABLE_GET "\n\t"                \
++   "callq " ENTRY_CURRENT_TABLE_GET "@PLT\n\t"           \
 +   "jmp *(8 * " slot ")(%rax)"
++#else
+ #define STUB_ASM_CODE(slot)                              \
+    "movq " ENTRY_CURRENT_TABLE "@GOTTPOFF(%rip), %rax\n\t"  \
+    "movq %fs:(%rax), %r11\n\t"                           \
+    "jmp *(8 * " slot ")(%r11)"
++#endif
  
  #else
  
-@@ -77,7 +82,7 @@ x86_64_entry_start[] HIDDEN;
+@@ -77,7 +89,7 @@ x86_64_entry_start[] HIDDEN;
  mapi_func
  entry_get_public(int slot)
  {
