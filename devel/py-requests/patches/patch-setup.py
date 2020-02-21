1ec4b431a7d3ab6272214fe2360697a659b387f5
adam <adam@pkgsrc.org>
2020-02-21 06:42:59 +0000

    py-requests: updated to 2.23.0
    2.23.0:
    **Improvements**
    - Remove defunct reference to `prefetch` in Session `__attrs__`
    
    **Bugfixes**
    - Requests no longer outputs password in basic auth usage warning.
    
    **Dependencies**
    - Pinning for `chardet` and `idna` now uses major version instead of minor.
      This hopefully reduces the need for releases everytime a dependency is updated.

--- devel/py-requests/patches/patch-setup.py
+++ devel/py-requests/patches/patch-setup.py
@@ -1,20 +1,10 @@
-$NetBSD: patch-setup.py,v 1.8 2019/05/17 08:34:38 adam Exp $
+$NetBSD: patch-setup.py,v 1.9 2020/02/21 06:42:59 adam Exp $
 
-Allow newer versions of dependencies.
+Allow newer test dependencies.
 
---- setup.py.orig	2019-05-16 14:22:45.000000000 +0000
+--- setup.py.orig	2020-02-18 22:55:51.000000000 +0000
 +++ setup.py
-@@ -42,14 +42,14 @@ if sys.argv[-1] == 'publish':
- packages = ['requests']
- 
- requires = [
--    'chardet>=3.0.2,<3.1.0',
--    'idna>=2.5,<2.9',
--    'urllib3>=1.21.1,<1.26,!=1.25.0,!=1.25.1',
-+    'chardet>=3.0.2',
-+    'idna>=2.5',
-+    'urllib3>=1.21.1,!=1.25.0,!=1.25.1',
-     'certifi>=2017.4.17'
+@@ -49,7 +49,7 @@ requires = [
  
  ]
  test_requirements = [
