242c305c8c25dd8b7c75338ca333e6a68f81aab4
tsutsui <tsutsui@pkgsrc.org>
2020-02-21 02:53:19 +0000

    miutter: update to 4.0.4.
    pkgsrc changes:
    
    * update DESCR and MESSAGE to reflect twitter plugin removal etc.
    * sort DEPENDS by package (i.e. gem) names without categories for
      maintainability
    * adjust EXTRACT_DIR per upstream tarball changes
      (probably this will soon be changed again on upstream)
    * set DISABLE_BUNDLER_SETUP=1 to disable bundler's auto download on startup
    
    Upstream changes:
    
    mikutter 4.0.4
    
    * crash on adding Mastdon accounts
     * thanks Ahiru Iegamo
    * error on Mastodon User Profile tab
     * thanks Ahiru Iegamo
    * delayer 1.0.2
    
    mikutter 4.0.3
    
    * support Ruby 2.7 changes
    * clear image loading window by bg color after loading images and
      before drawing Pixbuf
     * thanks Shibafu Midorino
    
    mikutter 4.0.2
    
    * support thumbnails of YouTube and Niconico-video
     * thanks Shibafu Midorino
    * appimage: fix crash/freeze
     * thanks Yuto Tokunaga
    * adjust selected region including custom emoji cases on strings copy
     * thanks Shibafu Midorino
    * remove irb from Gemfile
    
    mikutter 4.0.1
    
    * happy new year
    * fix appimage
     * thanks hinaloe k
    * remove unnecessary code executed per MiraclePainter updates
     * thanks Shibafu Midorino
    
    mikutter 4.0
    
    Major version up after five and half years after prevous update,
    for 10 years aniversary of mikutter, with some incompatible changes.
    
    * make Twitter plugins third party plugins
     * Twitter plugins are no longer included
     * users can still use Twitter on mikutter by installing Twitter plugins
       https://github.com/mikutter/twitter_bootstrap
    * remove Service module
     * Abstraction by Service was not sufficient and it has been replaced
       by World plugin
    * deprecate boot events
     * initialization should be implemented in plugin context or
       Delayer.new {...} should be used if it need to be executed after
       load is conplete
    * remove miquire
     * miquire was introduce to avoid (no-longer-recallable) problems
       on ruby 1.8 and to switch loaded files by command line options,
       but both of them no longer exist
    * use own implementation to handle event loop
     * mikutter used Gtk mailloop and Delayer queue but it caused FiberError
       exceptions and the FiberError was hard to fix per specification
    * make it possible to change, preserve and restore order of general
      Model viewer tabs
     * orders of tabs in Mastdon user prifile tabs are recoreded and
       preserved on the next open even on other similar tabs
    * support Ruby 2.5 and later (drop support for 2.4)
     * ruby 2.4 will be EOLed on end of March 2020
       https://www.ruby-lang.org/ja/news/2019/10/02/ruby-2-4-9-released/
    * drop bundling gems in release tar ball
     * nowadays bundler is common enough and plugins with Gemfile made
       things more complicated
    * all other things forgotten to be written
     * all things I missed

--- net/mikutter/patches/patch-mikutter.rb
+++ net/mikutter/patches/patch-mikutter.rb
@@ -1,9 +1,9 @@
-$NetBSD: patch-mikutter.rb,v 1.7 2019/02/12 17:21:18 tsutsui Exp $
+$NetBSD: patch-mikutter.rb,v 1.8 2020/02/21 02:53:19 tsutsui Exp $
 
 - pkgsrc can handle location of ruby binary
   https://dev.mikutter.hachune.net/issues/889
 
---- mikutter.rb.orig	2019-02-11 07:32:53.000000000 +0000
+--- mikutter.rb.orig	2020-02-01 07:51:58.000000000 +0000
 +++ mikutter.rb
 @@ -1,7 +1,5 @@
 -#!/bin/sh
@@ -12,5 +12,5 @@ $NetBSD: patch-mikutter.rb,v 1.7 2019/02/12 17:21:18 tsutsui Exp $
 -exec ruby -x "$0" "$@"
 -#!ruby
  =begin rdoc
- = mikutter - the moest twitter client
- Copyright (C) 2009-2019 Toshiaki Asai
+ = mikutter - simple, powerful and moeful Mastodon client
+ Copyright (C) 2009-2020 Toshiaki Asai
