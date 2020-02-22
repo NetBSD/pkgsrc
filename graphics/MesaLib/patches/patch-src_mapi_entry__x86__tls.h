feeedd281fe22d8b7adebec0380f509708117a14
tnn <tnn@pkgsrc.org>
2020-02-22 09:25:22 +0000

    optimize a tail-call in previous

--- graphics/MesaLib/patches/patch-src_mapi_entry__x86__tls.h
+++ graphics/MesaLib/patches/patch-src_mapi_entry__x86__tls.h
@@ -1,11 +1,11 @@
-$NetBSD: patch-src_mapi_entry__x86__tls.h,v 1.4 2020/02/22 07:52:01 tnn Exp $
+$NetBSD: patch-src_mapi_entry__x86__tls.h,v 1.5 2020/02/22 09:25:22 tnn Exp $
 
 NetBSD only supports zero-initialized initial-exec tls variables in conjuction
 with dlopen(3) at the moment.
 
 --- src/mapi/entry_x86_tls.h.orig	2019-12-04 22:10:14.000000000 +0000
 +++ src/mapi/entry_x86_tls.h
-@@ -43,6 +43,21 @@ __asm__("x86_current_tls:\n\t"
+@@ -43,6 +43,20 @@ __asm__("x86_current_tls:\n\t"
  	"movl " ENTRY_CURRENT_TABLE "@GOTNTPOFF(%eax), %eax\n\t"
  	"ret");
  
@@ -20,14 +20,13 @@ with dlopen(3) at the moment.
 +	"2:\n\t"                      \
 +	"popl %eax\n\t"               \
 +	"addl $_GLOBAL_OFFSET_TABLE_+[.-2b], %eax\n\t" \
-+	"call *" ENTRY_CURRENT_TABLE_GET "@GOT(%eax)\n\t" \
-+	"ret");
++	"jmp *" ENTRY_CURRENT_TABLE_GET "@GOT(%eax)");
 +#endif
 +
  #ifndef GLX_X86_READONLY_TEXT
  __asm__(".section wtext, \"awx\", @progbits");
  #endif /* GLX_X86_READONLY_TEXT */
-@@ -56,10 +71,17 @@ __asm__(".balign 16\n"
+@@ -56,10 +70,17 @@ __asm__(".balign 16\n"
     ".balign 16\n"                \
     func ":"
  
