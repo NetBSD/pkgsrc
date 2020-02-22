23a81ac1437157cbabb179cceeac65f7888f6295
tnn <tnn@pkgsrc.org>
2020-02-22 07:52:01 +0000

    MesaLib: port x86_64 tls dispatch changes to i386 also

--- graphics/MesaLib/patches/patch-src_mapi_entry__x86__tls.h
+++ graphics/MesaLib/patches/patch-src_mapi_entry__x86__tls.h
@@ -1,39 +1,46 @@
-$NetBSD: patch-src_mapi_entry__x86__tls.h,v 1.3 2019/08/21 13:35:28 nia Exp $
+$NetBSD: patch-src_mapi_entry__x86__tls.h,v 1.4 2020/02/22 07:52:01 tnn Exp $
 
 NetBSD only supports zero-initialized initial-exec tls variables in conjuction
 with dlopen(3) at the moment.
 
-Fix --enable-glx-tls with clang. From FreeBSD.
-
-table_noop_array is only defined for shared-glapi.
-es1api and es2api are not going to be patched for NetBSD.
-
---- src/mapi/entry_x86_tls.h.orig	2018-01-18 21:30:29.000000000 +0000
+--- src/mapi/entry_x86_tls.h.orig	2019-12-04 22:10:14.000000000 +0000
 +++ src/mapi/entry_x86_tls.h
-@@ -56,10 +56,27 @@ __asm__(".balign 16\n"
+@@ -43,6 +43,21 @@ __asm__("x86_current_tls:\n\t"
+ 	"movl " ENTRY_CURRENT_TABLE "@GOTNTPOFF(%eax), %eax\n\t"
+ 	"ret");
+ 
++#if defined(__NetBSD__)
++__asm__("x86_current_table_helper:\n\t"
++	"movl %gs:(%eax), %eax\n\t"   \
++	"testl %eax, %eax\n\t"        \
++	"je 1f\n\t"                   \
++	"ret\n\t"                     \
++	"1:\n\t"                      \
++	"call 2f\n\t"                 \
++	"2:\n\t"                      \
++	"popl %eax\n\t"               \
++	"addl $_GLOBAL_OFFSET_TABLE_+[.-2b], %eax\n\t" \
++	"call *" ENTRY_CURRENT_TABLE_GET "@GOT(%eax)\n\t" \
++	"ret");
++#endif
++
+ #ifndef GLX_X86_READONLY_TEXT
+ __asm__(".section wtext, \"awx\", @progbits");
+ #endif /* GLX_X86_READONLY_TEXT */
+@@ -56,10 +71,17 @@ __asm__(".balign 16\n"
     ".balign 16\n"                \
     func ":"
  
-+#if defined(__NetBSD__) && defined(MAPI_MODE_GLAPI)
-+extern const mapi_func table_noop_array[];
++#if defined(__NetBSD__)
++#define STUB_ASM_CODE(slot)      \
++   "call x86_current_tls\n\t"    \
++   "call x86_current_table_helper\n\t"    \
++   "jmp *(4 * " slot ")(%eax)"
++#else
  #define STUB_ASM_CODE(slot)      \
     "call x86_current_tls\n\t"    \
     "movl %gs:(%eax), %eax\n\t"   \
-+   "testl %eax, %eax\n\t"        \
-+   "je 1f\n\t"                   \
-+   "jmp *(4 * " slot ")(%eax)\n\t" \
-+   "1:\n\t"                      \
-+   "call 2f\n"                   \
-+   "2:\n\t"                      \
-+   "popl %eax\n\t"               \
-+   "addl $_GLOBAL_OFFSET_TABLE_+[.-1b], %eax\n\t" \
-+   "movl table_noop_array@GOT(%eax), %eax\n\t" \
     "jmp *(4 * " slot ")(%eax)"
-+#else
-+#define STUB_ASM_CODE(slot)      \
-+   "call x86_current_tls\n\t"    \
-+   "movl %gs:(%eax), %eax\n\t"   \
-+   "jmp *(4 * " slot ")(%eax)"
 +#endif
  
  #define MAPI_TMP_STUB_ASM_GCC
