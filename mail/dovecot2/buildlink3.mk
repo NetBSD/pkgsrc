bf95044602b9abbf4db85415e880b421d0787ec7
bsiegert <bsiegert@pkgsrc.org>
2020-02-22 19:37:30 +0000

    Pullup ticket #6136 - requested by taca mail/dovecot2: security fix
    Revisions pulled up:
    - mail/dovecot2-ldap/Makefile                                   1.5-1.6
    - mail/dovecot2-pigeonhole/Makefile                             1.55
    - mail/dovecot2/Makefile                                        1.102-1.103
    - mail/dovecot2/Makefile.common                                 1.38
    - mail/dovecot2/buildlink3.mk                                   1.33
    - mail/dovecot2/distinfo                                        1.102
    
    ---
       Module Name:	pkgsrc
       Committed By:	jperkin
       Date:		Sat Jan 18 21:51:16 UTC 2020
    
       Modified Files:
       	pkgsrc/mail/dovecot2: Makefile buildlink3.mk
       	pkgsrc/mail/dovecot2-ldap: Makefile
       	pkgsrc/mail/dovecot2-pigeonhole: Makefile
    
       Log Message:
       *: Recursive revision bump for openssl 1.1.1.
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Wed Feb 12 14:01:59 UTC 2020
    
       Modified Files:
       	pkgsrc/mail/dovecot2: Makefile Makefile.common distinfo
       	pkgsrc/mail/dovecot2-ldap: Makefile
    
       Log Message:
       mail/dovecot2: update to 2.3.9.3
    
       Update dovecot2 to 2.3.9.3, security release.
    
       v2.3.9.3 2019-02-12  Aki Tuomi <aki.tuomi@open-xchange.com>
    
       	* CVE-2020-7046: Truncated UTF-8 can be used to DoS
       	  submission-login and lmtp processes.
       	* CVE-2020-7957: Specially crafted mail can crash snippet generation.

--- mail/dovecot2/buildlink3.mk
+++ mail/dovecot2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.32 2019/07/18 13:38:18 taca Exp $
+# $NetBSD: buildlink3.mk,v 1.32.6.1 2020/02/22 19:37:30 bsiegert Exp $
 
 BUILDLINK_TREE+=	dovecot
 
@@ -7,7 +7,7 @@ DOVECOT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.dovecot+=		dovecot>=2.2.0
 # must match current package version for plugins to load
-BUILDLINK_ABI_DEPENDS.dovecot+=		dovecot>=2.3.7
+BUILDLINK_ABI_DEPENDS.dovecot+=		dovecot>=2.3.9.2nb1
 BUILDLINK_PKGSRCDIR.dovecot?=		../../mail/dovecot2
 
 pkgbase:=	dovecot
