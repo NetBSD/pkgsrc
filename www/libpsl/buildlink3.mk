ab7e30847d324faab1c7e241caca15ab44919a1b
adam <adam@pkgsrc.org>
2020-02-24 19:46:23 +0000

    libpsl: updated to 0.21.0
    Release V0.21.0
    * Add -b/--batch to 'psl' to suppress printing the domain
    * Add support for Meson build system
    * Improve build system
    * Improve Windows compatibility
    * Remove NLS / gettext
    * Several cleanups and cosmetics

--- www/libpsl/buildlink3.mk
+++ www/libpsl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2019/04/03 00:33:12 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/02/24 19:46:23 adam Exp $
 
 BUILDLINK_TREE+=	libpsl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libpsl
 LIBPSL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libpsl+=	libpsl>=0.20.2
-BUILDLINK_ABI_DEPENDS.libpsl?=	libpsl>=0.20.2nb3
+BUILDLINK_ABI_DEPENDS.libpsl+=	libpsl>=0.20.2nb3
 BUILDLINK_PKGSRCDIR.libpsl?=	../../www/libpsl
 
 .include "../../textproc/icu/buildlink3.mk"
