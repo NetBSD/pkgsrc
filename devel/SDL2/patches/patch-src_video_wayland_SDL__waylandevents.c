7e716020e0d4b0ab2926304772730291e624ffac
nia <nia@pkgsrc.org>
2020-02-24 11:55:36 +0000

    SDL2: Apply upstream patch for wayland protocol error

--- devel/SDL2/patches/patch-src_video_wayland_SDL__waylandevents.c
+++ devel/SDL2/patches/patch-src_video_wayland_SDL__waylandevents.c
@@ -1,6 +1,8 @@
-$NetBSD: patch-src_video_wayland_SDL__waylandevents.c,v 1.2 2019/10/29 22:46:39 nia Exp $
+$NetBSD: patch-src_video_wayland_SDL__waylandevents.c,v 1.3 2020/02/24 11:55:36 nia Exp $
 
 Allow building with Wayland on operating systems without evdev.
+
+https://bugzilla.libsdl.org/show_bug.cgi?id=4924
 
 --- src/video/wayland/SDL_waylandevents.c.orig	2019-07-25 04:32:36.000000000 +0000
 +++ src/video/wayland/SDL_waylandevents.c
@@ -20,3 +22,16 @@ Allow building with Wayland on operating systems without evdev.
  #include <sys/select.h>
  #include <sys/mman.h>
  #include <poll.h>
+@@ -788,8 +796,10 @@ data_device_handle_enter(void *data, str
+         if (has_mime == SDL_TRUE) {
+             dnd_action = WL_DATA_DEVICE_MANAGER_DND_ACTION_COPY;
+         }
+-        wl_data_offer_set_actions(data_device->drag_offer->offer,
+-                                  dnd_action, dnd_action);
++        if (wl_data_offer_get_version(data_device->drag_offer->offer) >= 3) {
++            wl_data_offer_set_actions(data_device->drag_offer->offer,
++                                      dnd_action, dnd_action);
++        }
+     }
+ }
+ 
