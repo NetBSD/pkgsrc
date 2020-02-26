50eb2a15f37d743e9ca291b1156f4c66f28e1f90
adam <adam@pkgsrc.org>
2020-02-26 19:15:44 +0000

    libffi: updated to 3.3
    libffi v3.3
    * Add RISC-V support.
    * New API in support of GO closures.
    * Add IEEE754 binary128 long double support for 64-bit Power
    * Default to Microsoft's 64-bit long double ABI with Visual C++.
    * GNU compiler uses 80 bits (128 in memory) FFI_GNUW64 ABI.
    * Add Windows on ARM64 (WOA) support.
    * Add Windows 32-bit ARM support.
    * Raw java (gcj) API deprecated.
    * Add pre-built PDF documentation to source distribution.
    * Many new test cases and bug fixes.

--- devel/libffi/patches/patch-testsuite_libffi.call_float2.c
+++ devel/libffi/patches/patch-testsuite_libffi.call_float2.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-ag,v 1.2 2016/01/25 17:56:39 jperkin Exp $
+$NetBSD: patch-testsuite_libffi.call_float2.c,v 1.1 2020/02/26 19:15:44 adam Exp $
 
 The %Lf printf format expects "long double", so cast args accordingly.
 
---- testsuite/libffi.call/float2.c.orig	2014-11-08 12:47:24.000000000 +0000
+--- testsuite/libffi.call/float2.c.orig	2019-10-31 14:49:54.000000000 +0000
 +++ testsuite/libffi.call/float2.c
-@@ -45,7 +45,7 @@ int main (void)
-   /* This is ifdef'd out for now. long double support under SunOS/gcc
-      is pretty much non-existent.  You'll get the odd bus error in library
-      routines like printf().  */
+@@ -47,7 +47,7 @@ int main (void)
+   /* long double support under SunOS/gcc is pretty much non-existent.
+      You'll get the odd bus error in library routines like printf() */
+ #else
 -  printf ("%Lf, %Lf, %Lf, %Lf\n", ld, ldblit(f), ld - ldblit(f), LDBL_EPSILON);
 +  printf ("%Lf, %Lf, %Lf, %Lf\n", (long double)ld, (long double)ldblit(f), (long double)(ld - ldblit(f)), (long double)LDBL_EPSILON);
  #endif
