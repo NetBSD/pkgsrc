11a227cf76fff9f08cb9262eeb7a5776b702c870
nia <nia@pkgsrc.org>
2020-02-26 17:55:51 +0000

    srb2: Update to 2.2.2
    Changes in 2.2:
    
    General
    
        Slopes have been implemented into almost the entire campaign, including support for launching off of quarterpipes. No, before you ask, loops still aren’t possible.
        An enormous number of graphics and textures have been updated or redone. Highlights include the title screen, Sonic and Tails, with separate sprites of Tails’s tails for optimum mofumofu.
        Practically the entire soundtrack has been redone.
        Character sprites now face the direction the player’s control inputs point instead of in the direction the camera is facing.
        Automatic braking, a new assist feature, has been added. While enabled, releasing the controls will cause the player’s character to attempt to stop instead of coasting forward.
        Tails’s AI has been significantly improved in Sonic & Tails mode, including allowing him to be commanded to fly you without using player 2’s controls.
        The attraction, elemental, and force shields now have a jump-spin ability like whirlwind and armageddon already did.
        Continuing the game after getting a game over now starts the player with more lives for each continue used.
        The intro cutscene has been revamped with brand-new art, and the game now has a short ending sequence.
    
    Levels
    
        Arid Canyon Zone Acts 2 and 3 now exist.
        Almost the entire rest of the campaign has been remade from scratch or significantly updated.
        Several bosses have had their arena and behavior updated.
        The final battles have been adjusted to make losing not kick the player all the way back to the beginning of Eggrock.
        A short, optional tutorial stage has been added.
        Two stages previously from the OLDC have been included as unlockables.
        Cooperative mode now uses the old 2.0 special stages, which have been slightly updated to be more multiplayer-friendly.
    
    Interface
    
        The menus have been massively revamped to both look better and be easier to understand.
        Controls, menus, and various other things have been renamed to make them easier to understand.
        Record attack now has HUD elements to display the buttons being pressed both during gameplay and while watching replays.
        Multiple accessibility features have been added, including closed-captioning and the ability to adjust the palette at runtime to add contrast to aid colorblind players.
    
    Engine/Editing
    
        The palette has changed again to provide slightly more diverse color options
        Music no longer restarts from the beginning after an interruption, such as getting an extra life.
        Plugging in a controller during gameplay will allow that controller to be used instead of requiring the game to be restarted to recognize it.
        Added support for paper sprites, which allow the sprite to be rendered as if it’s on an upright piece of paper, becoming thinner when viewed at an angle and disappearing entirely when viewed from the side. Think Paper Mario.
        Textures can now be used as flats (but not vice-versa).
        Skybox rendering has been significantly optimized.
        PNG images can now be used as graphics, at any resolution (but not too high or you’ll run out of memory).
        Sprite rotation is now supported.
        libopenmpt support
        Added support for the MD3 model format.
        So many Lua changes we couldn’t possibly hope to list them all here.

--- games/srb2/patches/patch-src_sdl_i__system.c
+++ games/srb2/patches/patch-src_sdl_i__system.c
@@ -1,21 +1,21 @@
-$NetBSD: patch-src_sdl_i__system.c,v 1.1 2019/10/01 12:30:32 nia Exp $
+$NetBSD: patch-src_sdl_i__system.c,v 1.2 2020/02/26 17:55:51 nia Exp $
 
 Hunk 1: Support searching PREFIX for data files.
 
 Hunk 2: Support NetBSD's spelling of statfs.
 
---- src/sdl/i_system.c.orig	2019-08-17 18:07:04.000000000 +0000
+--- src/sdl/i_system.c.orig	2020-02-22 22:27:19.000000000 +0000
 +++ src/sdl/i_system.c
-@@ -133,7 +133,7 @@ typedef LPVOID (WINAPI *p_MapViewOfFile)
+@@ -139,7 +139,7 @@ typedef LPVOID (WINAPI *p_MapViewOfFile)
  
- // Locations for searching the srb2.srb
+ // Locations for searching the srb2.pk3
  #if defined (__unix__) || defined(__APPLE__) || defined (UNIXCOMMON)
 -#define DEFAULTWADLOCATION1 "/usr/local/share/games/SRB2"
 +#define DEFAULTWADLOCATION1 "@PREFIX@/share/srb2"
  #define DEFAULTWADLOCATION2 "/usr/local/games/SRB2"
  #define DEFAULTWADLOCATION3 "/usr/share/games/SRB2"
  #define DEFAULTWADLOCATION4 "/usr/games/SRB2"
-@@ -2375,8 +2375,13 @@ void I_GetDiskFreeSpace(INT64 *freespace
+@@ -2565,8 +2565,13 @@ void I_GetDiskFreeSpace(INT64 *freespace
  	*freespace = INT32_MAX;
  	return;
  #else // Both Linux and BSD have this, apparently.
