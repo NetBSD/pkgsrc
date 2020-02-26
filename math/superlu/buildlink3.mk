001132b29507b980ec2ec0f21145cebfb6c1571b
wiz <wiz@pkgsrc.org>
2020-02-26 16:29:43 +0000

    superlu: update to 5.2.1.
    Changes not found, previous version was from 2003.

--- math/superlu/buildlink3.mk
+++ math/superlu/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2019/08/11 13:17:49 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/02/26 16:29:43 wiz Exp $
 
 BUILDLINK_TREE+=	superlu
 
 .if !defined(SUPERLU_BUILDLINK3_MK)
 SUPERLU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.superlu+=	superlu>=2.0nb2
-BUILDLINK_ABI_DEPENDS.superlu?=	superlu>=3.0.1nb7
+BUILDLINK_API_DEPENDS.superlu+=	superlu>=5.0
+
 BUILDLINK_PKGSRCDIR.superlu?=	../../math/superlu
 BUILDLINK_DEPMETHOD.superlu?=	build
 .endif # SUPERLU_BUILDLINK3_MK
