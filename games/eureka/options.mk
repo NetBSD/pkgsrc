93949424ce7e77be6cd928741cb79702399647bc
micha <micha@pkgsrc.org>
2020-02-26 12:28:02 +0000

    eureka: Update to 1.27.1
    Version 1.27.1 is used for the package with the latest macOS patches. They were
    added to the source archive by upstream after the 1.27 release without changing
    its name.
    
    The new OpenGL renderer can be disabled with the new package option "opengl"
    (recommended on machines without hardware acceleration for OpenGL).
    The option "opengl" is ignored if the FLTK package was compiled without OpenGL
    support.
    
    
    CHANGES IN Eureka 1.27
    ======================
    
    (Since version 1.24)
    
    + use OpenGL for faster rendering of 2D and 3D views
    + improved non-GL drawing of 2D view, much faster in Linux
    + support mixing textures and flats for ports that allow it
    + improved 3D mode, can move things and sector surfaces
    + ratio lock, makes producing lines at certain angles easier
    
    - a single status bar for both 2D and 3D views
    - new popup menus to set edit mode, browser mode, scaling
    - render sectors in the 2D view with both lighting and texturing
    - create void islands by drawing lines anti-clockwise in a sector
    - snap position indicator for 2D view (a light-blue cross)
    - added scale '-' and '+' buttons, show scale as percentage
    - ability to highlight/select railings in 3D view
    - beginning of support for UDMF maps (not usable yet...)
    
    - render BOOM's deep water effect (linetype 242)
      [pkgrc note: Legacy linetype 301 "Translucent 3D water" is now visible
                   with the OpenGL renderer too]
    - render 3D Floors for EDGE, LEGACY and ZDOOM ports
      [pkgrc note: 3D floors are only visible with the OpenGL renderer]
    - render basic slopes for EDGE, ETERNITY, ODAMEX and ZDOOM
      [pkgrc note: Slopes are only visible with the OpenGL renderer]
    - render things with a NULL sprite as a blue triangle
    - render Hexen map-spots as a green triangle
    - render unknown things with a smaller '?' sprite
    
    - preference to combine flats and textures in a single browser
    - preference to show all textures on a one-sided linedef
    - preference for linedef information (Length, Angle, Ratio)
    
    - support ESC key to cancel a current action (e.g. dragging)
    - support SHIFT and CTRL when using mouse-wheel to move in 3D view
    - support the FreeDM iwad
    - support TNT and Plutonia textures in FreeDoom2 iwad
    - support Hexen map format in the ETERNITY port
    - support PNG, JPEG and TGA images in sprite/flat/patch lumps
    - added portal specials 156 and 107 to the ZDOOM port
    - added missing specials 105, 106 and 130 to the ZDOOM port
    - improvements to the Makefile
    - improvements to the MacOS build files
    
    - fixed mode menu disappearing into Window's task bar when maximized
    - fixed not remembering test-in-game EXE when port is "vanilla"
    - fixed the 'Online Docs' command in the Help menu
    - fixed window title not showing correct map name
    - fixed Re-bind in key binding dialog to show new key as valid
    - fixed grid getting out-of-sync with the "hide in FREE mode"
    - fixed needless line flips when drawing clockwise within a sector

---
+++ games/eureka/options.mk
@@ -0,0 +1,29 @@
+# $NetBSD: options.mk,v 1.1 2020/02/26 12:28:02 micha Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.eureka
+PKG_SUPPORTED_OPTIONS=	opengl xdg-utils
+PKG_SUGGESTED_OPTIONS=	opengl xdg-utils
+
+.include "../../mk/bsd.options.mk"
+
+.include "../../x11/fltk13/buildlink3.mk"
+
+# opengl: Use OpenGL for rendering
+# If FLTK is compiled without opengl option, it is always disabled here too.
+# If FLTK is compiled with opengl option, disabling it here allows to force
+# usage of the software renderer (recommended without hardware acceleration).
+.if empty(PKG_OPTIONS.fltk13:Mopengl) || empty(PKG_OPTIONS:Mopengl)
+SUBST_CLASSES+=		opengl
+SUBST_STAGE.opengl=	do-configure
+SUBST_MESSAGE.opengl=	Disable OpenGL renderer in Makefile ...
+SUBST_FILES.opengl=	Makefile
+SUBST_SED.opengl=	-e 's,\# CXXFLAGS += -DNO_OPENGL,CXXFLAGS += -DNO_OPENGL,'
+SUBST_SED.opengl+=	-e 's, --use-gl,,'
+SUBST_SED.opengl+=	-e 's,LIBS += -lGLU -lGL,\#LIBS += -lGLU -lGL,'
+.endif
+
+# xdg-utils: Create dependency for xdg-utils (Portland project)
+# Can be used by FLTK for fl_open_uri() on X11.
+.if !empty(PKG_OPTIONS:Mxdg-utils)
+DEPENDS+=	xdg-utils>=1.1:../../misc/xdg-utils
+.endif
