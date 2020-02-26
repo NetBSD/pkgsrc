e55985ffc1a7e4e907ed01b372d56fe887a1ce99
maya <maya@pkgsrc.org>
2020-02-26 17:48:58 +0000

    firefox: limit what is noted as being a hack for NetBSD to NetBSD.
    This is causing problems on fedora 31 which gets a function prototype
    mismatch from this somehow.

--- www/firefox/patches/patch-config__make_system_wrappers.py
+++ www/firefox/patches/patch-config__make_system_wrappers.py
@@ -1,15 +1,17 @@
-$NetBSD: patch-config__make_system_wrappers.py,v 1.2 2019/12/03 14:21:20 ryoon Exp $
+$NetBSD: patch-config__make_system_wrappers.py,v 1.3 2020/02/26 17:48:58 maya Exp $
 
 This is a workaround for building firefox with clang from base,
 with a conflict with __isinf(__x) in libc++ vs netbsd headers.
 
 --- config/make-system-wrappers.py.orig	2019-12-02 12:22:52.000000000 +0000
 +++ config/make-system-wrappers.py
-@@ -7,6 +7,7 @@ import os
+@@ -7,6 +7,9 @@ import os
  from mozbuild.util import FileAvoidWrite
  
  header_template = '''#pragma GCC system_header
++#ifdef __NetBSD__
 +#define __isinf(x) __builtin_isinf(x)
++#endif
  #pragma GCC visibility push(default)
  {includes}
  #pragma GCC visibility pop
