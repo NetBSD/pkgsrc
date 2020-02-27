9f47e38ec8889fc11d2ee4d25519d6c9c4db3fc5
bsiegert <bsiegert@pkgsrc.org>
2020-02-27 14:32:57 +0000

    Add a package for Go 1.14.
    The default will remain at 1.13 for the next branch.
    
    The latest Go release, version 1.14, arrives six months after Go 1.13. Most of
    its changes are in the implementation of the toolchain, runtime, and libraries.
    As always, the release maintains the Go 1 promise of compatibility. We expect
    almost all Go programs to continue to compile and run as before.
    
    See the release notes at https://golang.org/doc/go1.14.

---
+++ lang/go114/patches/patch-src_crypto_x509_root__bsd.go
@@ -0,0 +1,10 @@
+$NetBSD: patch-src_crypto_x509_root__bsd.go,v 1.1 2020/02/27 14:32:57 bsiegert Exp $
+
+--- src/crypto/x509/root_bsd.go.orig	2017-07-24 19:38:15.000000000 +0000
++++ src/crypto/x509/root_bsd.go
+@@ -12,4 +12,5 @@ var certFiles = []string{
+ 	"/etc/ssl/cert.pem",                      // OpenBSD
+ 	"/usr/local/share/certs/ca-root-nss.crt", // DragonFly
+ 	"/etc/openssl/certs/ca-certificates.crt", // NetBSD
++	"/etc/ssl/certs/ca-certificates.crt",     // NetBSD
+ }
