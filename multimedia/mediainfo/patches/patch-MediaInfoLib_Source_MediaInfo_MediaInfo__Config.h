869cab2c3190b620605f860786bcd5157b32c1e7
bsiegert <bsiegert@pkgsrc.org>
2020-02-28 12:16:45 +0000

    Update mediainfo and libmediainfo to 19.09.
    No changelog from upstream as far as I can see.
    
    From Joern Clausen in PR pkg/54832.

--- multimedia/mediainfo/patches/patch-MediaInfoLib_Source_MediaInfo_MediaInfo__Config.h
+++ multimedia/mediainfo/patches/patch-MediaInfoLib_Source_MediaInfo_MediaInfo__Config.h
@@ -1,12 +1,12 @@
-$NetBSD: patch-MediaInfoLib_Source_MediaInfo_MediaInfo__Config.h,v 1.1 2015/05/14 13:20:06 jperkin Exp $
+$NetBSD: patch-MediaInfoLib_Source_MediaInfo_MediaInfo__Config.h,v 1.2 2020/02/28 12:16:45 bsiegert Exp $
 
 Avoid CS pollution from sys/regset.h on SunOS.
 
---- MediaInfoLib/Source/MediaInfo/MediaInfo_Config.h.orig	2013-10-11 14:18:26.000000000 +0000
+--- MediaInfoLib/Source/MediaInfo/MediaInfo_Config.h.orig	2020-02-28 11:50:05.265436338 +0000
 +++ MediaInfoLib/Source/MediaInfo/MediaInfo_Config.h
-@@ -29,6 +29,9 @@
- #include "ZenLib/Translation.h"
+@@ -30,6 +30,9 @@
  #include "ZenLib/InfoMap.h"
+ #include <set>
  #include <bitset>
 +#ifdef __sun
 +#undef CS
