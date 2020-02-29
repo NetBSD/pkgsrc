00a05cd1672ba2fd55e5f4d8892b83498c4c11ec
nia <nia@pkgsrc.org>
2020-02-29 11:49:57 +0000

    libgit2: Correct buildlink3

--- devel/libgit2/buildlink3.mk
+++ devel/libgit2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2020/02/29 11:47:09 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/02/29 11:49:57 nia Exp $
 
 BUILDLINK_TREE+=	libgit2
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libgit2
 LIBGIT2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libgit2+=	libgit2>=0.28.0
-BUILDLINK_ABI_DEPENDS.libgit2+=	libgit2>=0.99.0nb1
+BUILDLINK_ABI_DEPENDS.libgit2+=	libgit2>=0.99.0
 BUILDLINK_PKGSRCDIR.libgit2?=	../../devel/libgit2
 
 .include "../../devel/zlib/buildlink3.mk"
