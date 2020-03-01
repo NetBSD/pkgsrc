e85540af12f4e6f4287b668e23d4773a2ecc6f5b
nia <nia@pkgsrc.org>
2020-03-01 15:36:01 +0000

    opencv: Restore optionality of jasper
    bump PKGREVISION

--- graphics/opencv/options.mk
+++ graphics/opencv/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.10 2020/02/13 09:23:46 adam Exp $
+# $NetBSD: options.mk,v 1.11 2020/03/01 15:36:01 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.opencv
-PKG_SUPPORTED_OPTIONS=	ffmpeg
+PKG_SUPPORTED_OPTIONS=	ffmpeg jasper
 
 .include "../../mk/bsd.options.mk"
 
@@ -12,6 +12,15 @@ CMAKE_ARGS+=	-DFFMPEG_LIB_DIR=${PREFIX}/lib/ffmpeg4
 .include "../../multimedia/ffmpeg4/buildlink3.mk"
 .else
 CMAKE_ARGS+=	-DWITH_FFMPEG=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mjasper)
+CMAKE_ARGS+=	-DWITH_JASPER=ON
+# jasper uses SIZE_MAX and friends in its headers.
+CXXFLAGS+=	-D__STDC_LIMIT_MACROS
+.include "../../graphics/jasper/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DWITH_JASPER=OFF
 .endif
 
 # FIXME: should be option.mk'ed instead
