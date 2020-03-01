9cce1a9b91fe754698a502df246ba6b163af9e40
markd <markd@pkgsrc.org>
2020-03-01 11:18:58 +0000

    poppler*: reenable splash backend - needed to display output in poppler-qt based backends. also add nss dependency and be explicit in the tiff dependency.

--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.77 2019/11/07 12:38:04 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.78 2020/03/01 11:18:58 markd Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -11,12 +11,14 @@ BUILDLINK_PKGSRCDIR.poppler?=	../../print/poppler
 
 GCC_REQD+=	5
 
+.include "../../devel/nss/buildlink3.mk"
 .include "../../graphics/cairo/buildlink3.mk"
 .include "../../graphics/lcms2/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../graphics/openjpeg/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
 .endif # POPPLER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-poppler
