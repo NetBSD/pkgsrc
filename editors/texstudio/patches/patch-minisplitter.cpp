12cb6e679c7e6c9a0beefae0600b31cd9cface62
markd <markd@pkgsrc.org>
2020-03-01 12:48:13 +0000

    texstudio: update to 2.12.22
    2020-01-18: 2.12.22
    It fixes garbled symbols in OSX, crash when changing magic language
    comment and pdf search path handling.
    
    2020-01-13: 2.12.20
    It fixes a problem with replacing when search highlight is activated.
    
    2019-12-26: 2.12.18
    This is mainly a bug fix release. Most notably change is better support
    of regexp in search (Qt5 version only).

---
+++ editors/texstudio/patches/patch-minisplitter.cpp
@@ -0,0 +1,14 @@
+$NetBSD: patch-minisplitter.cpp,v 1.1 2020/03/01 12:48:13 markd Exp $
+
+fs54269
+
+--- src/minisplitter.cpp.orig	2019-12-24 11:57:23.000000000 +0000
++++ src/minisplitter.cpp
+@@ -72,7 +72,6 @@ void MiniSplitterHandle::resizeEvent(QRe
+     else
+         setContentsMargins(0, 2, 0, 2);
+     setMask(QRegion(contentsRect()));
+-    QSplitterHandle::resizeEvent(event);
+ }
+ 
+ void MiniSplitterHandle::paintEvent(QPaintEvent *event)
