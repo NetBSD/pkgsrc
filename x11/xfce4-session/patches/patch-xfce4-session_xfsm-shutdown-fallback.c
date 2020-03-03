50fff8489fd02c2e22394af3998f8e9049dbe79e
gutteridge <gutteridge@pkgsrc.org>
2020-03-03 05:06:20 +0000

    xfce4-session: fix botch in patch

--- x11/xfce4-session/patches/patch-xfce4-session_xfsm-shutdown-fallback.c
+++ x11/xfce4-session/patches/patch-xfce4-session_xfsm-shutdown-fallback.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-xfce4-session_xfsm-shutdown-fallback.c,v 1.1 2020/03/01 19:47:23 gutteridge Exp $
+$NetBSD: patch-xfce4-session_xfsm-shutdown-fallback.c,v 1.2 2020/03/03 05:06:20 gutteridge Exp $
 
 Fix compilation on OSes other than Linux and the *BSDs.
 
@@ -14,7 +14,19 @@ Fix compilation on OSes other than Linux and the *BSDs.
    gboolean ret = FALSE;
    gint exit_status = 0;
  #ifdef HAVE_POLKIT
-@@ -390,21 +392,27 @@ xfsm_shutdown_fallback_try_action (XfsmS
+@@ -382,29 +384,39 @@ xfsm_shutdown_fallback_try_action (XfsmS
+   {
+     case XFSM_SHUTDOWN_SHUTDOWN:
+       xfsm_helper_action = "shutdown";
++#ifdef __BACKEND_TYPE_BSD__
+       cmd = POWEROFF_CMD;
++#endif
+       break;
+     case XFSM_SHUTDOWN_RESTART:
+       xfsm_helper_action = "restart";
++#ifdef __BACKEND_TYPE_BSD__
+       cmd = REBOOT_CMD;
++#endif
        break;
      case XFSM_SHUTDOWN_SUSPEND:
        xfsm_helper_action = "suspend";
