13bff02885794619f97508c5e18fece8ca49ea42
nia <nia@pkgsrc.org>
2020-03-04 10:54:25 +0000

    libical: Add links to PRs

--- time/libical/patches/patch-src_libical_icalcomponent.c
+++ time/libical/patches/patch-src_libical_icalcomponent.c
@@ -1,6 +1,8 @@
-$NetBSD: patch-src_libical_icalcomponent.c,v 1.1 2018/05/14 08:34:56 jperkin Exp $
+$NetBSD: patch-src_libical_icalcomponent.c,v 1.2 2020/03/04 10:54:25 nia Exp $
 
 Need limits.h for *_MAX
+
+https://github.com/libical/libical/pull/420
 
 --- src/libical/icalcomponent.c.orig	2018-02-27 15:34:25.000000000 +0000
 +++ src/libical/icalcomponent.c
