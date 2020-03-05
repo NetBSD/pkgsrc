290f54696868fddcaadfdaa7b4b04a07634cd534
schmonz <schmonz@pkgsrc.org>
2020-03-05 13:19:32 +0000

    Avoid xsltproc attempting network fetch.

--- textproc/po4a/patches/patch-Po4aBuilder.pm
+++ textproc/po4a/patches/patch-Po4aBuilder.pm
@@ -1,13 +1,14 @@
-$NetBSD: patch-Po4aBuilder.pm,v 1.2 2015/03/18 02:25:23 mef Exp $
+$NetBSD: patch-Po4aBuilder.pm,v 1.3 2020/03/05 13:19:32 schmonz Exp $
 
 (1) ryoon: remove gzip at initial import
 (2) mef: Add PerlIO::F_UTF8 () macro
 (3) mef: to avoid --previous option is said unknown to msgmerge
     (but sounds strange)
+(4) schmonz: avoid xsltproc attempting network fetch
 
---- Po4aBuilder.pm.orig	2013-08-22 05:11:04.000000000 +0900
-+++ Po4aBuilder.pm	2015-03-18 11:10:49.000000000 +0900
-@@ -8,6 +8,8 @@ use File::stat;
+--- Po4aBuilder.pm.orig	2019-10-25 10:32:20.000000000 +0000
++++ Po4aBuilder.pm
+@@ -9,6 +9,8 @@ use File::stat;
  
  @ISA = qw(Module::Build);
  
@@ -16,7 +17,7 @@ $NetBSD: patch-Po4aBuilder.pm,v 1.2 2015/03/18 02:25:23 mef Exp $
  sub ACTION_build {
      my $self = shift;
      $self->depends_on('code');
-@@ -104,7 +106,7 @@ sub ACTION_binpo {
+@@ -81,7 +83,7 @@ sub ACTION_binpo {
          my $lang = fileparse($_, qw{.po});
          unless ($self->up_to_date("po/bin/po4a.pot", $_)) {
              print "XX Sync $_: ";
@@ -25,7 +26,7 @@ $NetBSD: patch-Po4aBuilder.pm,v 1.2 2015/03/18 02:25:23 mef Exp $
              # Typically all that changes was a date. I'd
              # prefer not to commit such changes, so detect
              # and ignore them.
-@@ -253,8 +255,6 @@ sub ACTION_man {
+@@ -233,8 +235,6 @@ sub ACTION_man {
          }
          $parser->parse_from_file ($file, $out);
  
@@ -34,11 +35,13 @@ $NetBSD: patch-Po4aBuilder.pm,v 1.2 2015/03/18 02:25:23 mef Exp $
      }
  
      # Install the manpages written in XML DocBook
-@@ -265,9 +265,7 @@ sub ACTION_man {
+@@ -244,10 +244,8 @@ sub ACTION_man {
+     foreach $file (@{$self->rscan_dir($manpath, qr{\.xml$})}) {
          if ($file =~ m,(.*/man(.))/([^/]*)\.xml$,) {
              my ($outdir, $section, $outfile) = ($1, $2, $3);
-             system("xsltproc -o $outdir/$outfile.$section --nonet http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl $file") and die;
+-            system("xsltproc -o $outdir/$outfile.$section --nonet http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl $file") and die;
 -            system ("gzip -9 -f $outdir/$outfile.$section") and die;
++            system("xsltproc -o $outdir/$outfile.$section --nonet @PREFIX@/share/xsl/docbook/manpages/docbook.xsl $file") and die;
          }
 -        unlink "$file" || die;
      }
