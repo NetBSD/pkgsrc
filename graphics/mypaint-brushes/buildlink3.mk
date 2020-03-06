0939f082fc1829ffbbc15792c14ae9e9aad42fc0
tnn <tnn@pkgsrc.org>
2020-03-06 13:46:27 +0000

    bump bl3.mk for pkg-config filename change

--- graphics/mypaint-brushes/buildlink3.mk
+++ graphics/mypaint-brushes/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/04/30 20:57:04 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/03/06 13:46:27 tnn Exp $
 
 BUILDLINK_TREE+=	mypaint-brushes
 
 .if !defined(MYPAINT_BRUSHES_BUILDLINK3_MK)
 MYPAINT_BRUSHES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mypaint-brushes+=	mypaint-brushes>=1.3.0
+BUILDLINK_API_DEPENDS.mypaint-brushes+=	mypaint-brushes>=2.0
 BUILDLINK_PKGSRCDIR.mypaint-brushes?=	../../graphics/mypaint-brushes
 .endif	# MYPAINT_BRUSHES_BUILDLINK3_MK
 
