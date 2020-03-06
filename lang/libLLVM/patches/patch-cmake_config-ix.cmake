9c5214875593796f5fdae2ff15a1574470420190
tnn <tnn@pkgsrc.org>
2020-03-06 22:44:18 +0000

    libLLVM: update to 9.0.1
    sync w/ lang/llvm. needed for mesa 20 update.

--- lang/libLLVM/patches/patch-cmake_config-ix.cmake
+++ lang/libLLVM/patches/patch-cmake_config-ix.cmake
@@ -1,14 +1,14 @@
-$NetBSD: patch-cmake_config-ix.cmake,v 1.1 2019/01/26 21:17:20 tnn Exp $
+$NetBSD: patch-cmake_config-ix.cmake,v 1.2 2020/03/06 22:44:18 tnn Exp $
 
 Do not generate invalid llvm-config in pkgsrc.
 
---- cmake/config-ix.cmake.orig	2018-01-02 17:53:08.000000000 +0000
+--- cmake/config-ix.cmake.orig	2019-12-11 19:15:30.000000000 +0000
 +++ cmake/config-ix.cmake
-@@ -153,7 +153,7 @@ if(NOT LLVM_USE_SANITIZER MATCHES "Memor
+@@ -141,7 +141,7 @@ if(NOT LLVM_USE_SANITIZER MATCHES "Memor
      endif()
      if(LLVM_ENABLE_TERMINFO)
        set(HAVE_TERMINFO 0)
--      foreach(library tinfo terminfo curses ncurses ncursesw)
+-      foreach(library terminfo tinfo curses ncurses ncursesw)
 +      foreach(library terminfo curses ncurses ncursesw tinfo)
          string(TOUPPER ${library} library_suffix)
          check_library_exists(${library} setupterm "" HAVE_TERMINFO_${library_suffix})
