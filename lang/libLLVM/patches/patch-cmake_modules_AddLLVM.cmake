8b2d14a9d4a123fa5ebf1ff4662080229c8d53bb
tnn <tnn@pkgsrc.org>
2020-03-06 22:44:18 +0000

    libLLVM: update to 9.0.1
    sync w/ lang/llvm. needed for mesa 20 update.

--- lang/libLLVM/patches/patch-cmake_modules_AddLLVM.cmake
+++ lang/libLLVM/patches/patch-cmake_modules_AddLLVM.cmake
@@ -1,46 +1,36 @@
-$NetBSD: patch-cmake_modules_AddLLVM.cmake,v 1.8 2019/01/27 00:07:32 tnn Exp $
+$NetBSD: patch-cmake_modules_AddLLVM.cmake,v 1.9 2020/03/06 22:44:18 tnn Exp $
 
 Disable library install rules. Handled manually.
 Make sure llvm-config goes in libexec/libLLVM to avoid conflict.
 Don't use non-portable -z discard-unused on SunOS.
 Don't use relative @rpath in llvm-config on Darwin.
 
---- cmake/modules/AddLLVM.cmake.orig	2018-08-01 07:51:55.000000000 +0000
+--- cmake/modules/AddLLVM.cmake.orig	2019-12-11 19:15:30.000000000 +0000
 +++ cmake/modules/AddLLVM.cmake
-@@ -218,9 +218,6 @@ function(add_link_opts target_name)
+@@ -222,9 +222,6 @@ function(add_link_opts target_name)
          # ld64's implementation of -dead_strip breaks tools that use plugins.
          set_property(TARGET ${target_name} APPEND_STRING PROPERTY
                       LINK_FLAGS " -Wl,-dead_strip")
 -      elseif(${CMAKE_SYSTEM_NAME} MATCHES "SunOS")
 -        set_property(TARGET ${target_name} APPEND_STRING PROPERTY
 -                     LINK_FLAGS " -Wl,-z -Wl,discard-unused=sections")
-       elseif(NOT WIN32 AND NOT LLVM_LINKER_IS_GOLD AND NOT ${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD")
+       elseif(NOT WIN32 AND NOT LLVM_LINKER_IS_GOLD AND
+              NOT ${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD|AIX")
          # Object files are compiled with -ffunction-data-sections.
-         # Versions of bfd ld < 2.23.1 have a bug in --gc-sections that breaks
-@@ -654,11 +651,6 @@ macro(add_llvm_library name)
+@@ -695,12 +692,6 @@ macro(add_llvm_library name)
          set_property(GLOBAL PROPERTY LLVM_HAS_EXPORTS True)
        endif()
  
 -      install(TARGETS ${name}
 -              ${export_to_llvmexports}
--              ${install_type} DESTINATION ${install_dir}
--              COMPONENT ${name})
+-              LIBRARY DESTINATION lib${LLVM_LIBDIR_SUFFIX} COMPONENT ${name}
+-              ARCHIVE DESTINATION lib${LLVM_LIBDIR_SUFFIX} COMPONENT ${name}
+-              RUNTIME DESTINATION bin COMPONENT ${name})
 -
-       if (NOT CMAKE_CONFIGURATION_TYPES)
+       if (NOT LLVM_ENABLE_IDE)
          add_llvm_install_targets(install-${name}
                                   DEPENDS ${name}
-@@ -693,10 +685,6 @@ macro(add_llvm_loadable_module name)
-           set_property(GLOBAL PROPERTY LLVM_HAS_EXPORTS True)
-         endif()
- 
--        install(TARGETS ${name}
--                ${export_to_llvmexports}
--                LIBRARY DESTINATION ${dlldir}
--                ARCHIVE DESTINATION lib${LLVM_LIBDIR_SUFFIX})
-       endif()
-       set_property(GLOBAL APPEND PROPERTY LLVM_EXPORTS ${name})
-     endif()
-@@ -887,7 +875,7 @@ macro(add_llvm_tool name)
+@@ -907,7 +898,7 @@ macro(add_llvm_tool name)
  
        install(TARGETS ${name}
                ${export_to_llvmexports}
@@ -48,8 +38,8 @@ Don't use relative @rpath in llvm-config on Darwin.
 +              RUNTIME DESTINATION libexec/libLLVM
                COMPONENT ${name})
  
-       if (NOT CMAKE_CONFIGURATION_TYPES)
-@@ -1632,7 +1620,7 @@ function(llvm_setup_rpath name)
+       if (NOT LLVM_ENABLE_IDE)
+@@ -1727,7 +1718,7 @@ function(llvm_setup_rpath name)
    endif()
  
    if (APPLE)
