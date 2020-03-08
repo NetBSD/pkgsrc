3af3e6b3eac11e3263e6436857cb238233eb7725
bsiegert <bsiegert@pkgsrc.org>
2020-03-08 18:22:44 +0000

    Update nqp to 2020.02.1.
    No changelog from upstream.

--- lang/nqp/buildlink3.mk
+++ lang/nqp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2020/03/08 16:47:52 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2020/03/08 18:22:44 bsiegert Exp $
 
 BUILDLINK_TREE+=	nqp
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	nqp
 NQP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.nqp+=		nqp>=2015.11
-BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2019.07.1nb2
+BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2020.02
 BUILDLINK_PKGSRCDIR.nqp?=		../../lang/nqp
 
 .include "../../devel/MoarVM/buildlink3.mk"
