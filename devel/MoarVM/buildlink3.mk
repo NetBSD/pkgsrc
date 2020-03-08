9704b1915c5ab289ce0135a18aa9bcf37bedf4fd
bsiegert <bsiegert@pkgsrc.org>
2020-03-08 15:47:54 +0000

    Update MoarVM to 2020.02.1.
    No release notes available, only a very long changelog at
    http://moarvm.org/releases.html.

--- devel/MoarVM/buildlink3.mk
+++ devel/MoarVM/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2019/02/16 19:37:48 bsiegert Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/03/08 15:47:54 bsiegert Exp $
 
 BUILDLINK_TREE+=	MoarVM
 
 .if !defined(MOARVM_BUILDLINK3_MK)
 MOARVM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.MoarVM+=	MoarVM>=2018.12
+BUILDLINK_API_DEPENDS.MoarVM+=	MoarVM>=2020.02
 BUILDLINK_PKGSRCDIR.MoarVM?=	../../devel/MoarVM
 
 .include "../../devel/libatomic_ops/buildlink3.mk"
