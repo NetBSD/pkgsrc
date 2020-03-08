7047dd108e024873ddb51c25927bb487592ddc76
bsiegert <bsiegert@pkgsrc.org>
2020-03-08 19:47:47 +0000

    Update rakudo to 2020.02.1.
    Upstream changelog is very long, but the language has been renamed from
    Perl 6 to Raku, so bin/raku is now the main interpreter binary.

--- lang/rakudo/buildlink3.mk
+++ lang/rakudo/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/03/08 16:47:52 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/03/08 19:47:47 bsiegert Exp $
 
 BUILDLINK_TREE+=	rakudo
 
 .if !defined(RAKUDO_BUILDLINK3_MK)
 RAKUDO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rakudo+=		rakudo>=2018.06
-BUILDLINK_ABI_DEPENDS.rakudo+=		rakudo>=2019.07.1nb2
+BUILDLINK_API_DEPENDS.rakudo+=		rakudo>=2020.02
+BUILDLINK_ABI_DEPENDS.rakudo+=		rakudo>=2020.02
 BUILDLINK_PKGSRCDIR.rakudo?=		../../lang/rakudo
 
 .include "../../lang/nqp/buildlink3.mk"
