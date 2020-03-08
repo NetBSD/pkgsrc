dbe767b5d7bb9833d4a5b74a17afda5ff2934e53
bsiegert <bsiegert@pkgsrc.org>
2020-03-08 10:29:22 +0000

    Pullup ticket #6143 - requested by taca lang/php72: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.289
    - lang/php72/distinfo                                           1.52
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Thu Feb 20 14:47:55 UTC 2020
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php72: distinfo
    
       Log Message:
       lang/php72: update to 7.2.28
    
       Update php72 to 7.2.28 (PHP 7.2.28).
    
       20 Feb 2020, PHP 7.2.28
    
       - DOM:
         . Fixed bug #77569: (Write Access Violation in DomImplementation). (Nikita,
           cmb)
    
       - Phar:
         . Fixed bug #79082 (Files added to tar with Phar::buildFromIterator have
           all-access permissions). (CVE-2020-7063) (stas)
    
       - Session:
         . Fixed bug #79221 (Null Pointer Dereference in PHP Session Upload Progress).
           (CVE-2020-7062) (stas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.283.4.5 2020/03/08 10:24:19 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.283.4.6 2020/03/08 10:29:22 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -88,7 +88,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
-PHP72_VERSION=	7.2.27
+PHP72_VERSION=	7.2.28
 PHP73_VERSION=	7.3.15
 PHP74_VERSION=	7.4.3
 
