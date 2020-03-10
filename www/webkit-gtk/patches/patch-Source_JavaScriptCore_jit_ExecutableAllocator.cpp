7b97c6b8ad537a0071f58e0eea74cc9d04023a2e
leot <leot@pkgsrc.org>
2020-03-10 18:14:04 +0000

    webkit-gtk: Update to 2.28.0
    Changes:
    WebKitGTK 2.28.0
    ================
      - Enable JIT in MIPS platform.
      - Do not limit the surrounding text for input methods to current paragraph.
      - Fix the build with OpenGL disabled.
      - Fix the build when Wayland target is disabled.
      - Fix the build on ppc64le if __unix is undefined.
      - Fix several crashes and rendering issues.
      - Translation updates: Polish.
    
    WebKitGTK 2.27.91
    =================
      - Update user agent quirks to fix the unsupported browser message in several google services.
      - Fix several compile warnings with GCC 10.
      - Fix the build with GCC 10.
      - Fix several crashes and rendering issues.
      - Translation updates: Chinese
    
    WebKitGTK 2.27.90
    =================
      - Add support for same-site cookies.
      - Add flatpak sandbox support.
      - Enable WebAudio and WebGL by default in WebKitSettings.
      - Add a setting to disallow top level navigation to a data URI.
      - Add support for the -webkit-font-smoothing CSS property.
      - Always use a light theme for rendering form controls.
      - Stop making the Web Inspector windows transient.
      - Ensure mouse cursor is hidden during fullscreen video playback.
      - Add support for inspecting service workers to the remote inspector.
      - Fix several crashes and rendering issues.
    
    WebKitGTK 2.27.4
    ================
      - Add API for input methods.
      - Add API to serialize/deserialize a JSCValue to/from a JSON string.
      - Add support for strict secure cookies.
      - Add support for saving data from remote inspector.
      - Make ondemand hardware acceleration policy never leave accelerated compositing mode.
      - Fix rendering of conic gradients in high resolution displays.
      - Fix special combination characters not respecting the keystroke order when high CPU load.
      - Honor the IndexedDB directory set in WebsiteDataManager.
      - Fix rendering of text when there's an initial advance in the text run.
      - Fix web process crash when displaying a KaTeX formula.
      - Fix network process crash with PSON enabled.
      - Fix several crashes and rendering issues.
    
    WebKitGTK 2.27.3
    ================
      - Add support for Pointer Lock API.
      - Improve performance when falling back to system fonts.
      - Stop using DBus for the remote inspector implementation to improve the performance of both
        WebDriver and remote inspector.
      - Implement support for new ARIA roles: code, strong, emphasis, generic.
      - Fix handling of content type with new custom protocols implementation.
      - Make image decoders fully thread safe.
      - Add support for get page source command in WebDriver.
      - Add support for network proxy capabilities in WebDriver.
      - Add support for new window command in WebDriver.
      - Fix several crashes and rendering issues.
      - Translation updates: Brazilian Portuguese, Ukrainian.
    
    WebKitGTK 2.27.2
    ================
      - Add user messages API for the communication with the web extension.
      - Enable service workers by default.
      - Add support for saving data in Web Inspector.
      - More navigation gesture improvement.
      - Fix the build with WebDriver disabled.
      - Show also client EGL extensions in about:gpu.
      - Disable accelerated compositing when we fail to initialize the EGL dispaly under Wayland.
      - Fix several crashes and rendering issues.
    
    WebKitGTK 2.27.1
    ================
      - Enable async scrolling when accelerating compositing policy is 'always'.
      - Add about:gpu to show information about the graphics stack.
      - Add API to enable Process Swap on (Cross-site) Navigation, that is now disabled by default.
      - Add WebKitWebView:page-id property.
      - Improve swipe navigation gesture style.
      - Fix several crashes and rendering issues.

--- www/webkit-gtk/patches/patch-Source_JavaScriptCore_jit_ExecutableAllocator.cpp
+++ www/webkit-gtk/patches/patch-Source_JavaScriptCore_jit_ExecutableAllocator.cpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-Source_JavaScriptCore_jit_ExecutableAllocator.cpp,v 1.3 2019/03/13 20:49:40 leot Exp $
+$NetBSD: patch-Source_JavaScriptCore_jit_ExecutableAllocator.cpp,v 1.4 2020/03/10 18:14:04 leot Exp $
 
 Avoid to (pre)allocate 1GB of memory on OpenBSD:
 
@@ -6,14 +6,14 @@ Avoid to (pre)allocate 1GB of memory on OpenBSD:
 
 From OpenBSD ports.
 
---- Source/JavaScriptCore/jit/ExecutableAllocator.cpp.orig	2019-02-12 11:21:03.000000000 +0000
+--- Source/JavaScriptCore/jit/ExecutableAllocator.cpp.orig	2020-02-04 10:23:59.000000000 +0000
 +++ Source/JavaScriptCore/jit/ExecutableAllocator.cpp
-@@ -92,7 +92,7 @@ static const size_t fixedExecutableMemor
- static const size_t fixedExecutableMemoryPoolSize = 16 * 1024 * 1024;
+@@ -97,7 +97,7 @@ static constexpr size_t fixedExecutableM
+ static constexpr size_t fixedExecutableMemoryPoolSize = 16 * 1024 * 1024;
  #elif CPU(ARM64)
- static const size_t fixedExecutableMemoryPoolSize = 128 * 1024 * 1024;
+ static constexpr size_t fixedExecutableMemoryPoolSize = 128 * 1024 * 1024;
 -#elif CPU(X86_64)
 +#elif CPU(X86_64) && !OS(OPENBSD)
- static const size_t fixedExecutableMemoryPoolSize = 1024 * 1024 * 1024;
+ static constexpr size_t fixedExecutableMemoryPoolSize = 1024 * 1024 * 1024;
  #else
- static const size_t fixedExecutableMemoryPoolSize = 32 * 1024 * 1024;
+ static constexpr size_t fixedExecutableMemoryPoolSize = 32 * 1024 * 1024;
