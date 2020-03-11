782e8aa98ef359a41b5e42fc5d977db09973a377
wiz <wiz@pkgsrc.org>
2020-03-11 11:05:53 +0000

    geeqie: add upstream bug report URL

--- graphics/geeqie/patches/patch-src_layout__util.c
+++ graphics/geeqie/patches/patch-src_layout__util.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_layout__util.c,v 1.2 2019/07/30 13:03:18 ryoon Exp $
+$NetBSD: patch-src_layout__util.c,v 1.3 2020/03/11 11:05:53 wiz Exp $
 
 For WEXITSTATUS().
+https://github.com/BestImageViewer/geeqie/pull/759
 
 --- src/layout_util.c.orig	2017-12-31 12:31:21.000000000 +0000
 +++ src/layout_util.c
