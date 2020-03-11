0cd2548f3385c1af7ba444a71303c675a482f507
gdt <gdt@pkgsrc.org>
2020-03-11 12:52:06 +0000

    geography/geos: Update to 3.8.1
    Changes in 3.8.1
    2020-xx-xx
    
    - Bug fixes / improvements
      - Stack allocate line segments in OverlapUnion (Paul Ramsey)
      - Avoid returning non-empty CoordinateSequence from empty Point
        (#1001, Dan Baston)
      - Avoid assertion failure with MSVC 2017 / 2019 (#1002, Dan Baston)
      - Remove whitespace from end of GEOSversion() output (azhi)
      - Improve performance of GEOSisValid (#1008, Dan Baston)
      - Avoid changing MultiLineString component order in GEOSReverse
        (#1013, Dan Baston)
      - Fix missing vtable for LineString and CoordinateArraySequenceFactory
        (#299 and #1016, Evgen Bodunov)
      - Reduce performance regression in GEOSBuffer (#1020)
    
    Changes in 3.8.0
    2019-10-10
    
    - New things:
      - CAPI: GEOSBuildArea (#952, Even Rouault)
      - CAPI: GEOSMakeValid (#952, Even Rouault)
      - CAPI: GEOSPolygonize_valid (#727, Dan Baston)
      - CAPI: GEOSCoverageUnion (Dan Baston)
      - CAPI: GEOSCoordSeq_setXY, GEOSCoordSeq_setXYZ,
              GEOSCoordSeq_getXY, GEOSCoordSeq_getXYZ (Dan Baston)
      - CAPI: GEOSMinimumBoundingCircle (#735)
      - CAPI: GEOSGeom_createPointFromXY (Dan Baston)
    
    - Improvements:
      - Improve overall performance by reducing of heap allocations (Dan Baston)
      - Improve performance and robustness of GEOSPointOnSurface (Martin Davis)
      - Improve performance of GEOSPolygonize for cases with many potential
        holes (#748, Dan Baston)
      - Support extended precision calculations (ttmath) and port JTS
        improvements related to extended precision (Paul Ramsey, Mateusz Loskot)
      - Improve performance of GEOSPolygonize for cases with many or complex
        shells (Dan Baston, Martin Davis)
      - Improve performance of Delaunay triangulations / Voronoi Diagrams
        (Dan Baston)
      - Improve performance of prepared geometry operations (Dan Baston)
      - Improve robustness of Delaunay triangulations (Paul Ramsey, Martin Davis)
      - Improve performance of unary union for lines (Dan Baston)
      - Improve general predicate, overlay, and buffer performance (Dan Baston, Paul Ramsey)
      - Improve cascaded union performance (Paul Ramsey, Martin Davis)
      - Allocate default GeometryFactory singleton on the stack (Sandro Mani)
      - Harmonize XML tests with JTS and harmonize cmake/autoconf
        test running (Paul Ramsey)
      - CMake modernization (Mateusz Loskot, Paul Ramsey, Dan Baston)
      - Return unique_ptr from most methods that produce a new geometry (Dan Baston)
    
    - Changes:
      - Constructive geometry functions in CAPI now preserve SRID
        of input arguments in output (#896)

--- geography/geos/patches/patch-capi_geos__ts__c.cpp
+++ geography/geos/patches/patch-capi_geos__ts__c.cpp
@@ -1,45 +1,46 @@
-$NetBSD: patch-capi_geos__ts__c.cpp,v 1.2 2018/09/11 16:13:29 gdt Exp $
+$NetBSD: patch-capi_geos__ts__c.cpp,v 1.3 2020/03/11 12:52:07 gdt Exp $
 
-\todo Document this patch.   It appears to be that passing non-POD types
-to variadic functions is UB.
+Passing non-POD types like std::string to variadic functions is UB.
+To avoid this, change functions to take const char * rather than
+std::string.
+  
+\todo File a bug upstream
 
-\todo File a bug upstream, if appropriate.
-
---- capi/geos_ts_c.cpp.orig	2015-09-08 16:31:02.000000000 +0000
+--- capi/geos_ts_c.cpp.orig	2020-03-10 17:19:40.000000000 +0000
 +++ capi/geos_ts_c.cpp
-@@ -214,7 +214,7 @@ typedef struct GEOSContextHandle_HS
+@@ -233,7 +233,7 @@ typedef struct GEOSContextHandle_HS {
      }
  
      void
 -    NOTICE_MESSAGE(string fmt, ...)
 +    NOTICE_MESSAGE(const char *fmt, ...)
      {
-       if (NULL == noticeMessageOld && NULL == noticeMessageNew) {
-         return;
-@@ -222,7 +222,7 @@ typedef struct GEOSContextHandle_HS
+         if(NULL == noticeMessageOld && NULL == noticeMessageNew) {
+             return;
+@@ -241,7 +241,7 @@ typedef struct GEOSContextHandle_HS {
  
-       va_list args;
-       va_start(args, fmt);
--      int result = vsnprintf(msgBuffer, sizeof(msgBuffer) - 1, fmt.c_str(), args);
-+      int result = vsnprintf(msgBuffer, sizeof(msgBuffer) - 1, fmt, args);
-       va_end(args);
+         va_list args;
+         va_start(args, fmt);
+-        int result = vsnprintf(msgBuffer, sizeof(msgBuffer) - 1, fmt.c_str(), args);
++        int result = vsnprintf(msgBuffer, sizeof(msgBuffer) - 1, fmt, args);
+         va_end(args);
  
-       if (result > 0) {
-@@ -235,7 +235,7 @@ typedef struct GEOSContextHandle_HS
+         if(result > 0) {
+@@ -255,7 +255,7 @@ typedef struct GEOSContextHandle_HS {
      }
  
      void
 -    ERROR_MESSAGE(string fmt, ...)
 +    ERROR_MESSAGE(const char *fmt, ...)
      {
-       if (NULL == errorMessageOld && NULL == errorMessageNew) {
-         return;
-@@ -243,7 +243,7 @@ typedef struct GEOSContextHandle_HS
+         if(NULL == errorMessageOld && NULL == errorMessageNew) {
+             return;
+@@ -263,7 +263,7 @@ typedef struct GEOSContextHandle_HS {
  
-       va_list args;
-       va_start(args, fmt);
--      int result = vsnprintf(msgBuffer, sizeof(msgBuffer) - 1, fmt.c_str(), args);
-+      int result = vsnprintf(msgBuffer, sizeof(msgBuffer) - 1, fmt, args);
-       va_end(args);
+         va_list args;
+         va_start(args, fmt);
+-        int result = vsnprintf(msgBuffer, sizeof(msgBuffer) - 1, fmt.c_str(), args);
++        int result = vsnprintf(msgBuffer, sizeof(msgBuffer) - 1, fmt, args);
+         va_end(args);
  
-       if (result > 0) {
+         if(result > 0) {
