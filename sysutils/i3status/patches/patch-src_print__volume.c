e1bfd2e4ecaf94c849b686f66a0607f476f27754
nia <nia@pkgsrc.org>
2020-03-11 23:28:21 +0000

    i3status: Fix up sunaudio support and link to my PR

--- sysutils/i3status/patches/patch-src_print__volume.c
+++ sysutils/i3status/patches/patch-src_print__volume.c
@@ -1,6 +1,8 @@
-$NetBSD: patch-src_print__volume.c,v 1.1 2020/03/11 22:46:36 nia Exp $
+$NetBSD: patch-src_print__volume.c,v 1.2 2020/03/11 23:28:21 nia Exp $
 
 Use sunaudio on NetBSD.
+
+https://github.com/i3/i3status/pull/391
 
 --- src/print_volume.c.orig	2019-01-23 08:03:56.000000000 +0000
 +++ src/print_volume.c
@@ -22,6 +24,15 @@ Use sunaudio on NetBSD.
      /* Try PulseAudio first */
  
      /* If the device name has the format "pulse[:N]" where N is the
+@@ -248,7 +248,7 @@ void print_volume(yajl_gen json_gen, cha
+     snd_mixer_selem_id_free(sid);
+ 
+ #endif
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
++#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+     char *mixerpath;
+     char defaultmixer[] = "/dev/mixer";
+     int mixfd, vol, devmask = 0;
 @@ -272,7 +272,7 @@ void print_volume(yajl_gen json_gen, cha
      if (mixer_idx > 0)
          free(mixerpath);
@@ -31,3 +42,29 @@ Use sunaudio on NetBSD.
      int oclass_idx = -1, master_idx = -1, master_mute_idx = -1;
      int master_next = AUDIO_MIXER_LAST;
      mixer_devinfo_t devinfo, devinfo2;
+@@ -327,15 +327,17 @@ void print_volume(yajl_gen json_gen, cha
+         vol = (int)vinfo.un.value.level[AUDIO_MIXER_LEVEL_MONO];
+     }
+ 
+-    vinfo.dev = master_mute_idx;
+-    vinfo.type = AUDIO_MIXER_ENUM;
+-    if (ioctl(mixfd, AUDIO_MIXER_READ, &vinfo) == -1)
+-        goto out;
++    if (master_mute_idx != -1) {
++        vinfo.dev = master_mute_idx;
++        vinfo.type = AUDIO_MIXER_ENUM;
++        if (ioctl(mixfd, AUDIO_MIXER_READ, &vinfo) == -1)
++            goto out;
+ 
+-    if (master_mute_idx != -1 && vinfo.un.ord) {
+-        START_COLOR("color_degraded");
+-        fmt = fmt_muted;
+-        pbval = 0;
++        if (vinfo.un.ord) {
++            START_COLOR("color_degraded");
++            fmt = fmt_muted;
++            pbval = 0;
++        }
+     }
+ 
+ #else
