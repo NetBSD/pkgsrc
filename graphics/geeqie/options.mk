d0d15a8a4d4bd405923674f8b03de77991a8305c
wiz <wiz@pkgsrc.org>
2020-03-11 14:37:24 +0000

    geeqie: fix option names

--- graphics/geeqie/options.mk
+++ graphics/geeqie/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2020/03/11 10:54:43 wiz Exp $
+# $NetBSD: options.mk,v 1.7 2020/03/11 14:37:24 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.geeqie
 PKG_OPTIONS_OPTIONAL_GROUPS=	gtk
@@ -9,12 +9,12 @@ PKG_SUGGESTED_OPTIONS=		gtk3
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mlibchamplain)
-CONFIGURE_ARGS+=	--enable-gps
+CONFIGURE_ARGS+=	--enable-map
 CONFIGURE_ARGS+=	--enable-clutter
 .include "../../geography/libchamplain012/buildlink3.mk"
 .include "../../graphics/clutter/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=	--disable-gps
+CONFIGURE_ARGS+=	--disable-map
 .endif
 
 .if !empty(PKG_OPTIONS:Mgtk2)
