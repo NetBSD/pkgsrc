aa26249f359caadc7135e24b469dfdb2fe695947
pho <pho@pkgsrc.org>
2020-03-11 15:01:53 +0000

    Update to hengband-2.2.1r
    The game is supposed to be able to automatically convert old save
    files, although the converter may not be fully tested. The list of
    changes from 1.6.2 is too long to paste here:
    
    * 1.7.2: https://hengband.osdn.jp/history/history1.7.2.html
    * 2.0.0: https://hengband.osdn.jp/history/history2.0.0.html
    * 2.1.0: https://hengband.osdn.jp/history/history2.1.0.html
    * 2.1.1: https://hengband.osdn.jp/history/history2.1.1.html
    * 2.1.2: https://hengband.osdn.jp/history/history2.1.2.html
    * 2.1.3: https://hengband.osdn.jp/history/history2.1.3.html
    * 2.1.4: https://hengband.osdn.jp/history/history2.1.4.html
    * 2.1.5: https://hengband.osdn.jp/history/history2.1.5.html
    * 2.2.0: https://hengband.osdn.jp/history/history2.2.0.html
    * 2.2.1: https://hengband.osdn.jp/history/history2.2.1.html

--- games/hengband/patches/patch-src_cmd3.c
+++ games/hengband/patches/patch-src_cmd3.c
@@ -1,40 +1,15 @@
-$NetBSD: patch-src_cmd3.c,v 1.1 2020/01/30 13:56:12 pho Exp $
+$NetBSD: patch-src_cmd3.c,v 1.2 2020/03/11 15:01:54 pho Exp $
 
-Use the correct format specifier.
+Fix an error on 64-bits platforms.
 
-Local Variables:
-coding: euc-jp
-End:
-
---- src/cmd3.c.orig	2004-03-28 05:05:36.000000000 +0000
+--- src/cmd3.c.orig	2020-03-11 14:36:27.328921818 +0000
 +++ src/cmd3.c
-@@ -45,12 +45,12 @@ void do_cmd_inven(void)
- 	item_tester_full = FALSE;
- 
- #ifdef JP
--	sprintf(out_val, "持ち物： 合計 %3d.%1d kg (限界の%ld%%) コマンド: ",
-+	sprintf(out_val, "持ち物： 合計 %3d.%1d kg (限界の%d%%) コマンド: ",
- 	    lbtokg1(p_ptr->total_weight) , lbtokg2(p_ptr->total_weight) ,
- 	    (p_ptr->total_weight * 100) / ((adj_str_wgt[p_ptr->stat_ind[A_STR]] * (p_ptr->pclass == CLASS_BERSERKER ? 150 : 100)) 
- / 2));
+@@ -53,7 +53,7 @@ void do_cmd_inven(void)
  #else
--	sprintf(out_val, "Inventory: carrying %d.%d pounds (%ld%% of capacity). Command: ",
-+	sprintf(out_val, "Inventory: carrying %d.%d pounds (%d%% of capacity). Command: ",
+ 	sprintf(out_val, "Inventory: carrying %d.%d pounds (%ld%% of capacity). Command: ",
  	    (int)(p_ptr->total_weight / 10), (int)(p_ptr->total_weight % 10),
- 	    (p_ptr->total_weight * 100) / ((adj_str_wgt[p_ptr->stat_ind[A_STR]] * (p_ptr->pclass == CLASS_BERSERKER ? 150 : 100)) / 2));
+-	    (p_ptr->total_weight * 100) / weight_limit());
++	    (long int)((p_ptr->total_weight * 100) / weight_limit()));
  #endif
-@@ -120,12 +120,12 @@ void do_cmd_equip(void)
  
- 	/* Build a prompt */
- #ifdef JP
--	sprintf(out_val, "装備： 合計 %3d.%1d kg (限界の%ld%%) コマンド: ",
-+	sprintf(out_val, "装備： 合計 %3d.%1d kg (限界の%d%%) コマンド: ",
- 	    lbtokg1(p_ptr->total_weight) , lbtokg2(p_ptr->total_weight) ,
- 	    (p_ptr->total_weight * 100) / ((adj_str_wgt[p_ptr->stat_ind[A_STR]] * (p_ptr->pclass == CLASS_BERSERKER ? 150 : 100)) 
- / 2));
- #else
--	sprintf(out_val, "Equipment: carrying %d.%d pounds (%ld%% of capacity). Command: ",
-+	sprintf(out_val, "Equipment: carrying %d.%d pounds (%d%% of capacity). Command: ",
- 	    (int)(p_ptr->total_weight / 10), (int)(p_ptr->total_weight % 10),
- 	    (p_ptr->total_weight * 100) / ((adj_str_wgt[p_ptr->stat_ind[A_STR]] * (p_ptr->pclass == CLASS_BERSERKER ? 150 : 100)) / 2));
- #endif
+ 
