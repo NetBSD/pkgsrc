aef4c1d953e71403377ecb3f86fec73ce44030fb
wiz <wiz@pkgsrc.org>
2020-03-11 10:54:43 +0000

    geeqie: switch libchamplain dependency to 012
    This is detected and compiles fine.
    I have not enabled the option by default though.
    
    Fix typo in patch comment.

--- graphics/geeqie/options.mk
+++ graphics/geeqie/options.mk
@@ -1,11 +1,9 @@
-# $NetBSD: options.mk,v 1.5 2019/09/16 21:55:46 nia Exp $
+# $NetBSD: options.mk,v 1.6 2020/03/11 10:54:43 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.geeqie
 PKG_OPTIONS_OPTIONAL_GROUPS=	gtk
 PKG_OPTIONS_GROUP.gtk=		gtk2 gtk3
 PKG_SUPPORTED_OPTIONS=		libchamplain
-# The libchamplain option doesn't actually work, so leave it off
-# to avoid the pain of dependencies with no benefit, until debugged.
 PKG_SUGGESTED_OPTIONS=		gtk3
 
 .include "../../mk/bsd.options.mk"
@@ -13,7 +11,7 @@ PKG_SUGGESTED_OPTIONS=		gtk3
 .if !empty(PKG_OPTIONS:Mlibchamplain)
 CONFIGURE_ARGS+=	--enable-gps
 CONFIGURE_ARGS+=	--enable-clutter
-.include "../../geography/libchamplain04/buildlink3.mk"
+.include "../../geography/libchamplain012/buildlink3.mk"
 .include "../../graphics/clutter/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-gps
