eb1eca69f013a082c05659cc98d0733a2ad7fde5
wiz <wiz@pkgsrc.org>
2020-03-13 17:59:27 +0000

    tor-browser: update to 9.0.6
    This version is now based on firefox68-esr and builds with the current
    rust in pkgsrc.

--- security/tor-browser/patches/patch-browser_app_profile_firefox.js
+++ security/tor-browser/patches/patch-browser_app_profile_firefox.js
@@ -1,14 +1,20 @@
-$NetBSD: patch-browser_app_profile_firefox.js,v 1.1 2019/02/25 15:32:24 wiz Exp $
+$NetBSD: patch-browser_app_profile_firefox.js,v 1.2 2020/03/13 17:59:27 wiz Exp $
 
---- browser/app/profile/firefox.js.orig	2018-05-16 05:38:23.000000000 +0000
+--- browser/app/profile/firefox.js.orig	2019-07-06 01:48:29.000000000 +0000
 +++ browser/app/profile/firefox.js
-@@ -1747,3 +1747,9 @@ pref("app.shield.optoutstudies.enabled",
- #else
- pref("app.shield.optoutstudies.enabled", false);
- #endif
-+
+@@ -1865,6 +1865,15 @@ pref("prio.publicKeyB", "26E6674E65425B8
+ pref("toolkit.coverage.enabled", false);
+ pref("toolkit.coverage.endpoint.base", "https://coverage.mozilla.org");
+ 
 +// Select UI locale from LANG/LC_MESSAGE environmental variables
 +pref("intl.locale.requested", "");
 +
++// Enable system addons, for example langpacks from www/firefox-l10n
++pref("extensions.autoDisableScopes", 11);
++
 +// Disable multiprocess window support. Workaround for PR 53273.
 +pref("browser.tabs.remote.autostart", false);
++
+ // Discovery prefs
+ pref("browser.discovery.enabled", true);
+ pref("browser.discovery.containers.enabled", true);
--- security/tor-browser/patches/patch-gfx_gl_GLContextProviderGLX.cpp
+++ security/tor-browser/patches/patch-gfx_gl_GLContextProviderGLX.cpp
@@ -1,16 +1,17 @@
-$NetBSD: patch-gfx_gl_GLContextProviderGLX.cpp,v 1.3 2019/06/01 20:24:03 wiz Exp $
+$NetBSD: patch-gfx_gl_GLContextProviderGLX.cpp,v 1.4 2020/03/13 17:59:27 wiz Exp $
 
 NetBSD doesn't ship libGL.so.1 due to a major bump. Look for the unversioned
 name.
+https://hg.mozilla.org/integration/mozilla-inbound/rev/8bcc51aaa91e
 
---- gfx/gl/GLContextProviderGLX.cpp.orig	2019-05-14 19:13:16.000000000 +0000
+--- gfx/gl/GLContextProviderGLX.cpp.orig	2019-05-17 00:33:26.000000000 +0000
 +++ gfx/gl/GLContextProviderGLX.cpp
 @@ -80,7 +80,7 @@ bool GLXLibrary::EnsureInitialized() {
-     // implementation requires an executable stack, which causes mprotect calls,
      // which trigger glibc bug
      // http://sourceware.org/bugzilla/show_bug.cgi?id=12225
+     const char* libGLfilename = "libGL.so.1";
 -#ifdef __OpenBSD__
 +#if defined(__OpenBSD__) || defined(__NetBSD__)
      libGLfilename = "libGL.so";
- #else
-     libGLfilename = "libGL.so.1";
+ #endif
+ 
