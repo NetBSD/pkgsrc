32b76f4e2443f1e2686c9c75189f2cd05cb2ebff
wiz <wiz@pkgsrc.org>
2020-03-13 17:59:27 +0000

    tor-browser: update to 9.0.6
    This version is now based on firefox68-esr and builds with the current
    rust in pkgsrc.

--- security/tor-browser/patches/patch-ipc_chromium_src_base_platform__thread__posix.cc
+++ security/tor-browser/patches/patch-ipc_chromium_src_base_platform__thread__posix.cc
@@ -1,26 +1,18 @@
-$NetBSD: patch-ipc_chromium_src_base_platform__thread__posix.cc,v 1.3 2019/02/25 15:32:24 wiz Exp $
+$NetBSD: patch-ipc_chromium_src_base_platform__thread__posix.cc,v 1.4 2020/03/13 17:59:27 wiz Exp $
 
 * Support NetBSD
 * Support Solaris (we can't rely on pthread_setname_np so ignore it).
 
---- ipc/chromium/src/base/platform_thread_posix.cc.orig	2019-02-23 20:00:48.000000000 +0000
+--- ipc/chromium/src/base/platform_thread_posix.cc.orig	2019-03-07 16:53:35.000000000 +0000
 +++ ipc/chromium/src/base/platform_thread_posix.cc
 @@ -12,7 +12,9 @@
  #if defined(OS_MACOSX)
- #include <mach/mach.h>
+ #  include <mach/mach.h>
  #elif defined(OS_NETBSD)
+-#  include <lwp.h>
 +_Pragma("GCC visibility push(default)")
- #include <lwp.h>
++#include <lwp.h>
 +_Pragma("GCC visibility pop")
  #elif defined(OS_LINUX)
- #include <sys/syscall.h>
- #include <sys/prctl.h>
-@@ -100,7 +102,7 @@ void PlatformThread::SetName(const char*
-   pthread_setname_np(pthread_self(), "%s", (void*)name);
- #elif defined(OS_BSD) && !defined(__GLIBC__)
-   pthread_set_name_np(pthread_self(), name);
--#elif defined(OS_SOLARIS)
-+#elif defined(notOS_SOLARIS)
-   pthread_setname_np(pthread_self(), name);
- #else
- #endif
+ #  include <sys/syscall.h>
+ #  include <sys/prctl.h>
