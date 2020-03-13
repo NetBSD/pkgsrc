e8ea2d2ddbe4328fcb69f8ac01a45af882a1507c
wiz <wiz@pkgsrc.org>
2020-03-13 17:59:27 +0000

    tor-browser: update to 9.0.6
    This version is now based on firefox68-esr and builds with the current
    rust in pkgsrc.

--- security/tor-browser/patches/patch-dom_media_CubebUtils.cpp
+++ security/tor-browser/patches/patch-dom_media_CubebUtils.cpp
@@ -1,13 +1,15 @@
-$NetBSD: patch-dom_media_CubebUtils.cpp,v 1.1 2019/02/25 15:32:24 wiz Exp $
+$NetBSD: patch-dom_media_CubebUtils.cpp,v 1.2 2020/03/13 17:59:27 wiz Exp $
 
---- dom/media/CubebUtils.cpp.orig	2019-02-23 20:00:40.000000000 +0000
+Add Sun audio support.
+
+--- dom/media/CubebUtils.cpp.orig	2019-07-06 01:48:31.000000000 +0000
 +++ dom/media/CubebUtils.cpp
-@@ -143,7 +143,7 @@ const char kBrandBundleURL[] = "chrome:/
+@@ -163,7 +163,7 @@ const char kBrandBundleURL[] = "chrome:/
  
  const char* AUDIOSTREAM_BACKEND_ID_STR[] = {
      "jack",  "pulse",       "alsa",  "audiounit", "audioqueue", "wasapi",
 -    "winmm", "directsound", "sndio", "opensl",    "audiotrack", "kai"};
-+    "winmm", "directsound", "sndio", "opensl",    "audiotrack", "kai", "oss"};
++    "winmm", "directsound", "sndio", "opensl",    "audiotrack", "kai", "sun"};
  /* Index for failures to create an audio stream the first time. */
  const int CUBEB_BACKEND_INIT_FAILURE_FIRST =
      ArrayLength(AUDIOSTREAM_BACKEND_ID_STR);
--- security/tor-browser/patches/patch-ipc_chromium_src_base_lock__impl__posix.cc
+++ security/tor-browser/patches/patch-ipc_chromium_src_base_lock__impl__posix.cc
@@ -1,15 +1,15 @@
-$NetBSD: patch-ipc_chromium_src_base_lock__impl__posix.cc,v 1.1 2019/02/25 15:32:24 wiz Exp $
+$NetBSD: patch-ipc_chromium_src_base_lock__impl__posix.cc,v 1.2 2020/03/13 17:59:27 wiz Exp $
 
 - Fix build on NetBSD 7.1 (which doesn't have PTHREAD_PRIO_INHERIT)
 
---- ipc/chromium/src/base/lock_impl_posix.cc.orig	2017-06-05 20:45:20.000000000 +0000
+--- ipc/chromium/src/base/lock_impl_posix.cc.orig	2019-03-07 16:53:35.000000000 +0000
 +++ ipc/chromium/src/base/lock_impl_posix.cc
-@@ -20,7 +20,7 @@ namespace internal {
+@@ -22,7 +22,7 @@ namespace internal {
  // Lock::PriorityInheritanceAvailable still must be checked as the code may
  // compile but the underlying platform still may not correctly support priority
  // inheritance locks.
 -#if defined(OS_NACL) || defined(OS_ANDROID)
 +#if defined(OS_NACL) || defined(OS_ANDROID) || (defined(__NetBSD__) && !defined(PTHREAD_PRIO_INHERIT))
- #define PRIORITY_INHERITANCE_LOCKS_POSSIBLE() 0
+ #  define PRIORITY_INHERITANCE_LOCKS_POSSIBLE() 0
  #else
- #define PRIORITY_INHERITANCE_LOCKS_POSSIBLE() 1
+ #  define PRIORITY_INHERITANCE_LOCKS_POSSIBLE() 1
