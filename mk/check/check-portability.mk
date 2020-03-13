f62222697c1a42a441ad9766b066f10b004c538a
rillig <rillig@pkgsrc.org>
2020-03-13 08:11:36 +0000

    mk/check: revert collateral damage from previous commit
    Killing the editor does not stop cvs from committing; to achieve that, it
    is necessary to remove the file containing the commit message being
    edited.

--- mk/check/check-portability.mk
+++ mk/check/check-portability.mk
@@ -1,39 +1,40 @@
-# $NetBSD: check-portability.mk,v 1.13 2020/03/13 08:04:08 rillig Exp $
+# $NetBSD: check-portability.mk,v 1.14 2020/03/13 08:11:36 rillig Exp $
 #
-# This file checks that the extracted shell programs don't contain
-# bashisms and other constructs that only work on some platforms.
+# This file contains some checks that are applied to the configure
+# scripts to check for certain constructs that are known to cause
+# problems on some platforms. The detailed checks are in
+# check-portability.sh.
 #
 # User-settable variables:
 #
 # CHECK_PORTABILITY
-#	Whether to enable the portability checks.
+#	Whether to enable some portability checks for the configure
+#	scripts before they are run.
 #
 #	Default value: yes for PKG_DEVELOPERs, no otherwise.
 #
 # Package-settable variables:
 #
 # CHECK_PORTABILITY_SKIP
-#	The shell patterns that should not be checked.
-#	Note that a * in a pattern also matches a slash in a pathname.
+#	The list of files that should be skipped in the portability
+#	check.
 #
 #	Default value: ${REPLACE_BASH}
-#	Examples: debian/* test/* *.bash
+#	Example: debian/*
 
 _VARGROUPS+=			check-portability
 _USER_VARS.check-portability=	CHECK_PORTABILITY
 _PKG_VARS.check-portability=	CHECK_PORTABILITY_SKIP
 
-.if ${PKG_DEVELOPER:Uno} != no
+.if ${PKG_DEVELOPER:Uno} != "no"
 CHECK_PORTABILITY?=		yes
 .endif
 CHECK_PORTABILITY?=		no
 CHECK_PORTABILITY_SKIP?=	${REPLACE_BASH}
 
 .if ${CHECK_PORTABILITY:tl} == yes && ${CHECK_PORTABILITY_SKIP} != "*"
-TOOL_DEPENDS+=	check-portability>=19.4.1:../../pkgtools/check-portability
 pre-configure-checks-hook: _check-portability
 .endif
-
 .PHONY: _check-portability
 _check-portability:
 	@${STEP_MSG} "Checking for portability problems in extracted files"
