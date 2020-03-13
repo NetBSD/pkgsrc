a444b3ca5254261a12f3cca0d244a4730b353353
tnn <tnn@pkgsrc.org>
2020-03-13 08:55:37 +0000

    nmap: assorted fixes
    - use pkgsrc libssh2 rather than bundled
    - Check portability skip for bundled libssh2
    - ipv6 is no longer optional, so remove it
    - bump rev

--- net/nmap/options.mk
+++ net/nmap/options.mk
@@ -1,19 +1,12 @@
-# $NetBSD: options.mk,v 1.20 2018/04/14 13:04:33 adam Exp $
+# $NetBSD: options.mk,v 1.21 2020/03/13 08:55:37 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nmap
 
-PKG_SUPPORTED_OPTIONS=	inet6 lua
-PKG_SUGGESTED_OPTIONS=	inet6
+PKG_SUPPORTED_OPTIONS=	lua
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=	lua
-
-.if !empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGS+=	--enable-ipv6
-.else
-CONFIGURE_ARGS+=	--disable-ipv6
-.endif
 
 .if !empty(PKG_OPTIONS:Mlua)
 LUA_VERSIONS_ACCEPTED=	53	# needs lua_isyieldable, introduced in Lua 5.3
