89aa244e33035281343f48a427424640a7a7b0e9
tnn <tnn@pkgsrc.org>
2020-03-14 13:28:57 +0000

    gmake: try harder to unbreak circular dependency
    Having builtin gettext (the lib) doesn't always imply that we have a
    builtin msgfmt (the tool).

--- devel/gmake/options.mk
+++ devel/gmake/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.8 2019/10/31 15:05:52 jperkin Exp $
+# $NetBSD: options.mk,v 1.9 2020/03/14 13:28:57 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gmake
 PKG_SUPPORTED_OPTIONS=	nls
@@ -12,7 +12,8 @@ PLIST_VARS+=		nls
 # to avoid a circular dependency (gmake->gettext-tools->ncurses->gmake).
 .include "../../devel/gettext-lib/builtin.mk"
 .if !empty(PKG_OPTIONS:Mnls) && \
-    !empty(USE_BUILTIN.gettext:M[yY][eE][sS])
+    !empty(USE_BUILTIN.gettext:M[yY][eE][sS]) && \
+    !empty(TOOLS_PLATFORM.msgfmt:U)
 USE_PKGLOCALEDIR=	yes
 USE_TOOLS+=		msgfmt
 .  include "../../devel/gettext-lib/buildlink3.mk"
