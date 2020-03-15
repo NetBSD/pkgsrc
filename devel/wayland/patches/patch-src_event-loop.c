f0610c85c18b3b0c9d269e69b70e64e5e3a64d20
nia <nia@pkgsrc.org>
2020-03-15 16:03:26 +0000

    wayland: Update to 1.18.0
    Based on a patch from wiz@, thanks! The main change I made was tricking
    autoconf into thinking NetBSD doesn't have posix_fallocate.
    
    The main new features in this release are:
    
    - Add support for the Meson build system (autotools is still supported
      but will be removed in a future release)
    - Add API to tag proxy objects to allow applications and toolkits to
      share the same Wayland connection
    - Track wayland-server timers in user-space to prevent creating too
      many FDs
    - Add wl_global_remove, a new function to mitigate race conditions with
      globals

--- devel/wayland/patches/patch-src_event-loop.c
+++ devel/wayland/patches/patch-src_event-loop.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_event-loop.c,v 1.2 2020/01/05 19:30:48 nia Exp $
+$NetBSD: patch-src_event-loop.c,v 1.3 2020/03/15 16:03:26 nia Exp $
 
 https://lists.freedesktop.org/archives/wayland-devel/2019-February/040024.html
 
---- src/event-loop.c.orig	2019-03-21 00:55:25.000000000 +0000
+--- src/event-loop.c.orig	2020-02-11 23:46:03.000000000 +0000
 +++ src/event-loop.c
 @@ -23,6 +23,8 @@
   * SOFTWARE.
@@ -10,10 +10,10 @@ https://lists.freedesktop.org/archives/wayland-devel/2019-February/040024.html
  
 +#include "config.h"
 +#ifdef HAVE_SYS_EPOLL_H
+ #include <assert.h>
  #include <stddef.h>
  #include <stdio.h>
- #include <errno.h>
-@@ -702,3 +704,4 @@ wl_event_loop_get_destroy_listener(struc
+@@ -1088,3 +1090,4 @@ wl_event_loop_get_destroy_listener(struc
  {
  	return wl_signal_get(&loop->destroy_signal, notify);
  }
