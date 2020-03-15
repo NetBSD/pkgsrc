de5817f4b17d3cf626998214700fc1553f2de63b
tnn <tnn@pkgsrc.org>
2020-03-15 21:41:27 +0000

    proj: buildlink3 sqlite3
    Needed because it is expressed in the installed libtool file:
    libproj.la:dependency_libs='-lsqlite3 -ldl -lpthread'

--- geography/proj/buildlink3.mk
+++ geography/proj/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/03/02 01:13:10 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/03/15 21:41:27 tnn Exp $
 
 BUILDLINK_TREE+=	proj
 
@@ -8,6 +8,7 @@ PROJ_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.proj+=	proj>=5.0.0
 BUILDLINK_ABI_DEPENDS.proj+=	proj>=5.0.0
 BUILDLINK_PKGSRCDIR.proj?=	../../geography/proj
+.include "../../databases/sqlite3/buildlink3.mk"
 .endif # PROJ_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-proj
