17c707dcbc02e907cd8b4688da73d87ba7255221
taca <taca@pkgsrc.org>
2020-03-15 16:00:55 +0000

    devel/ruby-redmine: update to 4.0.6
    Update ruby-redmine to 4.0.6 with the same packaging scheme with 3.3.2.
    
    pkgsrc change:
    
    * Support ruby25 and ruby26, no more ruby22.
    
    Changes from previous release are too many to write here, please refer
    <https://redmine.org/>.

--- devel/ruby-redmine/files/unicorn.rb
+++ devel/ruby-redmine/files/unicorn.rb
@@ -2,7 +2,7 @@ working_directory "@PREFIX@/share/@RUBY_PKGPREFIX@-redmine/app"
 pid "@PREFIX@/share/@RUBY_PKGPREFIX@-redmine/unicorn.pid"
 stderr_path "@PREFIX@/share/@RUBY_PKGPREFIX@-redmine/unicorn.log"
 stdout_path "@PREFIX@/share/@RUBY_PKGPREFIX@-redmine/unicorn.log"
- 
+
 worker_processes 3
 listen "@PREFIX@/share/@RUBY_PKGPREFIX@-redmine/unicorn.redmine.sock"
 timeout 60
--- devel/ruby-redmine/options.mk
+++ devel/ruby-redmine/options.mk
@@ -1,52 +1,58 @@
-# $NetBSD: options.mk,v 1.6 2019/05/23 19:22:59 rillig Exp $
+# $NetBSD: options.mk,v 1.7 2020/03/15 16:00:55 taca Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.redmine
 
 PKG_OPTIONS_REQUIRED_GROUPS=	db
-PKG_OPTIONS_GROUP.db=		mysql pgsql sqlite3
+PKG_OPTIONS_GROUP.db=		mssql mysql pgsql sqlite3
 PKG_SUPPORTED_OPTIONS+=		unicorn
 
 PKG_SUGGESTED_OPTIONS=	mysql unicorn
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	mysql pgsql sqlite3
+PLIST_VARS+=		mssql mysql pgsql sqlite3 unicorn
 
 ###
 ### Use mysql, pgsql, or sqlite3 backend
 ###
-MYSQL_DISTFILE=		mysql2-0.3.21.gem
-PGSQL_DISTFILE=		pg-0.18.4.gem
-SQLITE3_DISTFILE=	sqlite3-1.3.12.gem
+MSSQL_DISTFILES=	activerecord-sqlserver-adapter-5.2.0.gem tiny_tds-2.1.2.gem
+MYSQL_DISTFILES=	mysql2-0.5.3.gem
+PGSQL_DISTFILES=	pg-1.1.4.gem
+SQLITE3_DISTFILES=	sqlite3-1.3.13.gem
 
 .if make (distinfo) || make (mdi) # for checksum generation only
-GEMS_DISTFILES+=	${MYSQL_DISTFILE}
-GEMS_DISTFILES+=	${PGSQL_DISTFILE}
-GEMS_DISTFILES+=	${SQLITE3_DISTFILE}
+GEMS_DISTFILES+=	${MSSQL_DISTFILES}
+GEMS_DISTFILES+=	${MYSQL_DISTFILES}
+GEMS_DISTFILES+=	${PGSQL_DISTFILES}
+GEMS_DISTFILES+=	${SQLITE3_DISTFILES}
+.elif !empty(PKG_OPTIONS:Mmssql)
+GEMS_DISTFILES+=	${MSSQL_DISTFILES}
+.include "../../databases/freetds/buildlink3.mk"
+PLIST.mssql=		yes
 .elif !empty(PKG_OPTIONS:Mmysql)
-GEMS_DISTFILES+=	${MYSQL_DISTFILE}
+GEMS_DISTFILES+=	${MYSQL_DISTFILES}
 .include "../../mk/mysql.buildlink3.mk"
-RM_PLIST_SRC+=		PLIST.mysql
+PLIST.mysql=		yes
 .elif !empty(PKG_OPTIONS:Mpgsql)
-GEMS_DISTFILES+=	${PGSQL_DISTFILE}
+GEMS_DISTFILES+=	${PGSQL_DISTFILES}
 .include "../../mk/pgsql.buildlink3.mk"
 CHECK_INTERPRETER_SKIP+=	${RM_DIR}/gems/gems/pg-*/spec/*
 CHECK_INTERPRETER_SKIP+=	${RM_DIR}/gems/gems/pg-*/spec/pg/*
-RM_PLIST_SRC+=		PLIST.pgsql
+PLIST.pgsql=		yes
 .elif !empty(PKG_OPTIONS:Msqlite3)
-GEMS_DISTFILES+=	${SQLITE3_DISTFILE}
+GEMS_DISTFILES+=	${SQLITE3_DISTFILES}
 .include "../../databases/sqlite3/buildlink3.mk"
-RM_PLIST_SRC+=		PLIST.sqlite3
+PLIST.sqlite3=		yes
 .endif
 
 ###
 ### Use Unicorn web server
 ###
 .if !empty(PKG_OPTIONS:Municorn) || make (distinfo) || make (mdi)
-RM_PLIST_SRC+=		PLIST.unicorn
-GEMS_DISTFILES+=	kgio-2.11.0.gem \
-			raindrops-0.17.0.gem \
-			unicorn-5.2.0.gem
+
+GEMS_DISTFILES+=	kgio-2.11.3.gem raindrops-0.19.1.gem unicorn-5.5.3.gem
+
+PLIST.unicorn=		yes
 
 SUBST_CLASSES+=			prefix
 SUBST_STAGE.prefix=		pre-configure
@@ -60,8 +66,6 @@ RCD_SCRIPT_SRC.redmine_unicorn${RUBY_SUFFIX}=	${FILESDIR}/redmine_unicorn.sh
 
 CONF_FILES+=	${EGDIR}/unicorn.rb.example \
 		${PREFIX}/${RM_DIR}/app/config/unicorn.rb
-
-PLIST_SRC=	${PLIST_SRC_DFLT} ${RM_PLIST_SRC}
 
 post-extract:
 	${CP} ${FILESDIR}/unicorn.rb ${WRKDIR}/unicorn.rb
