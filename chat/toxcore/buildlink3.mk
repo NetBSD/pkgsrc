e2914da085d5cadd53cea08fd8ad63296ccc416a
mef <mef@pkgsrc.org>
2020-03-15 14:03:44 +0000

    (chat/toxcore) Update buildlink3.mk for lib/libtoxcore.so.2.10.0 bump

--- chat/toxcore/buildlink3.mk
+++ chat/toxcore/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/07/30 18:58:43 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/03/15 14:03:44 mef Exp $
 
 BUILDLINK_TREE+=	toxcore
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	toxcore
 TOXCORE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.toxcore+=	toxcore>=0.2.4
+BUILDLINK_ABI_DEPENDS.toxcore?=	toxcore>=0.2.10nb1
 BUILDLINK_PKGSRCDIR.toxcore?=	../../chat/toxcore
 
 .include "../../audio/libopus/buildlink3.mk"
