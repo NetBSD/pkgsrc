2bd9045674814212a8026da5b40e6f2b949eac8e
taca <taca@pkgsrc.org>
2020-03-15 15:26:22 +0000

    lang/ruby27-base: add ruby-base version 2.7.0
    Add ruby27-base 2.7.0 package.  This package is Ruby 2.7 release
    minimum base package.
    
    
    Ruby is the interpreted scripting language for quick and
    easy object-oriented programming.  It has many features to
    process text files and to do system management tasks (as in
    Perl).  It is simple, straight-forward, and extensible.
    
    Features of Ruby are shown below.
    
      + Simple Syntax
      + *Normal* Object-Oriented features (ex. class, method calls)
      + *Advanced* Object-Oriented features (ex. Mix-in, Singleton-method)
      + Operator Overloading
      + Exception Handling
      + Iterators and Closures
      + Garbage Collection
      + Dynamic Loading of Object files (on some architecture)
      + Highly Portable (works on many UNIX machines, and on DOS, Windows,
        Mac, etc.)
    
    Ruby 2.7 introduces a number of new features and performance
    improvements, most notably:
    
      * Pattern Matching (Experimental)
      * REPL improvement
      * Compaction GC
      * Separation of positional and keyword arguments

---
+++ lang/ruby27-base/patches/patch-lib_rubygems_commands_setup__command.rb
@@ -0,0 +1,41 @@
+$NetBSD: patch-lib_rubygems_commands_setup__command.rb,v 1.1 2020/03/15 15:26:22 taca Exp $
+
+* Make sure to setup under DESTDIR.
+
+--- lib/rubygems/commands/setup_command.rb.orig	2018-12-23 00:20:49.000000000 +0000
++++ lib/rubygems/commands/setup_command.rb
+@@ -177,7 +177,7 @@ By default, this RubyGems will install g
+ 
+     uninstall_old_gemcutter
+ 
+-    documentation_success = install_rdoc
++    documentation_success = install_rdoc install_destdir
+ 
+     say
+     if @verbose
+@@ -341,11 +341,15 @@ By default, this RubyGems will install g
+     end
+   end
+ 
+-  def install_rdoc
++  def install_rdoc(install_destdir)
+     gem_doc_dir = File.join Gem.dir, 'doc'
+     rubygems_name = "rubygems-#{Gem::VERSION}"
+     rubygems_doc_dir = File.join gem_doc_dir, rubygems_name
+ 
++    unless install_destdir.empty?
++      FileUtils.mkdir_p gem_doc_dir unless File.exist?(gem_doc_dir)
++    end
++
+     begin
+       Gem.ensure_gem_subdirectories Gem.dir
+     rescue SystemCallError
+@@ -356,7 +360,7 @@ By default, this RubyGems will install g
+        (not File.exist? rubygems_doc_dir or
+         File.writable? rubygems_doc_dir)
+       say "Removing old RubyGems RDoc and ri" if @verbose
+-      Dir[File.join(Gem.dir, 'doc', 'rubygems-[0-9]*')].each do |dir|
++      Dir[File.join(gem_doc_dir, 'rubygems-[0-9]*')].each do |dir|
+         rm_rf dir
+       end
+ 
