66d9b3d08e51d3a6744831f3ea02073634604ffd
taca <taca@pkgsrc.org>
2020-03-15 15:26:22 +0000

    lang/ruby27-base: add ruby-base version 2.7.0
    Add ruby27-base 2.7.0 package.  This package is Ruby 2.7 release
    minimum base package.
    
    
    Ruby is the interpreted scripting language for quick and
    easy object-oriented programming.  It has many features to
    process text files and to do system management tasks (as in
    Perl).  It is simple, straight-forward, and extensible.
    
    Features of Ruby are shown below.
    
      + Simple Syntax
      + *Normal* Object-Oriented features (ex. class, method calls)
      + *Advanced* Object-Oriented features (ex. Mix-in, Singleton-method)
      + Operator Overloading
      + Exception Handling
      + Iterators and Closures
      + Garbage Collection
      + Dynamic Loading of Object files (on some architecture)
      + Highly Portable (works on many UNIX machines, and on DOS, Windows,
        Mac, etc.)
    
    Ruby 2.7 introduces a number of new features and performance
    improvements, most notably:
    
      * Pattern Matching (Experimental)
      * REPL improvement
      * Compaction GC
      * Separation of positional and keyword arguments

---
+++ lang/ruby27-base/patches/patch-lib_rdoc_encoding.rb
@@ -0,0 +1,15 @@
+$NetBSD: patch-lib_rdoc_encoding.rb,v 1.1 2020/03/15 15:26:22 taca Exp $
+
+* Always convert "\r\n" to "\n" since it cause rdoc processing error.
+
+--- lib/rdoc/encoding.rb.orig	2018-03-26 05:56:26.000000000 +0000
++++ lib/rdoc/encoding.rb
+@@ -31,7 +31,7 @@ module RDoc::Encoding
+ 
+   def self.read_file filename, encoding, force_transcode = false
+     content = File.open filename, "rb" do |f| f.read end
+-    content.gsub!("\r\n", "\n") if RUBY_PLATFORM =~ /mswin|mingw/
++    content.gsub!("\r\n", "\n")
+ 
+     utf8 = content.sub!(/\A\xef\xbb\xbf/, '')
+ 
