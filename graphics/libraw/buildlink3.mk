4dbb3f4d011d4ac7f047ae5360955c0024284199
rillig <rillig@pkgsrc.org>
2020-03-15 10:31:59 +0000

    graphics/libraw: fix determining PKG_BUILD_OPTIONS.libraw

--- graphics/libraw/buildlink3.mk
+++ graphics/libraw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2019/07/16 15:45:03 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2020/03/15 10:31:59 rillig Exp $
 
 BUILDLINK_TREE+=	libraw
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.libraw+=	libraw>=0.16.0
 BUILDLINK_ABI_DEPENDS.libraw+=	libraw>=0.19.3
 BUILDLINK_PKGSRCDIR.libraw?=	../../graphics/libraw
 
+pkgbase := libraw
 .include "../../mk/pkg-build-options.mk"
 .if !empty(PKG_BUILD_OPTIONS.libraw:Mjasper)
 .  include "../../graphics/jasper/buildlink3.mk"
