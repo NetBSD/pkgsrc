9bdd5b73948465078c77b8f7ff9c99826afed53a
nia <nia@pkgsrc.org>
2020-03-16 11:17:22 +0000

    boehm-gc: Enable threads. Needed for neko-vm.

--- devel/boehm-gc/options.mk
+++ devel/boehm-gc/options.mk
@@ -1,10 +1,8 @@
-# $NetBSD: options.mk,v 1.5 2018/07/27 10:23:12 jperkin Exp $
+# $NetBSD: options.mk,v 1.6 2020/03/16 11:17:22 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.boehm-gc
 PKG_SUPPORTED_OPTIONS=	threads
-
-PKG_SUGGESTED_OPTIONS.Darwin+=	threads
-PKG_SUGGESTED_OPTIONS.SunOS+=	threads
+PKG_SUGGESTED_OPTIONS=	threads
 
 .include "../../mk/bsd.options.mk"
 
