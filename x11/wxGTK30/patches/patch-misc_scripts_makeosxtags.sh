ae45d29f95d2f4e2825b1fbc42de8e7a59408929
gdt <gdt@pkgsrc.org>
2020-03-17 23:39:15 +0000

    wxGTK30: Add bashism remediation patch
    New checks found a "test [[" bug.  Fix that, and regenerate another
    patch whose line numbers were far off.
    
    (No functional change.)

---
+++ x11/wxGTK30/patches/patch-misc_scripts_makeosxtags.sh
@@ -0,0 +1,17 @@
+$NetBSD: patch-misc_scripts_makeosxtags.sh,v 1.1 2020/03/17 23:39:15 gdt Exp $
+
+Remediate bashism.
+
+Not yet filed uptream.
+
+--- misc/scripts/makeosxtags.sh.orig	2018-03-07 16:55:38.000000000 +0000
++++ misc/scripts/makeosxtags.sh
+@@ -3,7 +3,7 @@
+ create_tags osx
+ 
+ osx_port=${1-cocoa}
+-if [[ $osx_port = carbon ]]; then
++if [ $osx_port = carbon ]; then
+     ext=cpp
+ else
+     ext=mm
