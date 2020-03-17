231ba02d2ec1ff6fa0666c414e00ead8b48acda3
adam <adam@pkgsrc.org>
2020-03-17 19:04:49 +0000

    powerdns: updated to 4.2.1
    4.2.1
    This release fixes several bugs and makes a few features more robust or intuitive. It also contains a few performance improvements for API users.
    
    New Features
    Add SLAVE-RENOTIFY zone metadata support
    Add configurable timeout for inbound AXFR
    Add CentOS 8 as builder target
    gmysql backend, add an option to send the SSL capability flag
    
    Improvements
    API: reduce number of database connections
    Register a few known RR types and remove an unknown one
    bindbackend: use metadata for also-notifies as well
    pdnsutil increase-serial: under SOA-EDIT=INCEPTION-EPOCH, bump as if it is EPOCH
    API: optionally do not return dnssec info in domain list
    Basic validation of $GENERATE parameters
    
    Bug Fixes
    LUA view: do not crash on empty IP list
    API: Accept headers without spaces
    Avoid database state-related SERVFAILs after a LUA error
    Just before 4.2.0, some SQL-related fixes broke edit-zone and other features with the LMDB backend. This has been fixed now.
    rfc2136, pdnsutil: somewhat improve duplicate record handling
    
    
    4.2.0
    Compared to the last release candidate, one more bug has been fixed.
    
    The LMDB backend is incomplete in this version. Slaving zones works, loading zones with pdnsutil works, but more fine grained edits (using edit-zone, or the REST API) fail. We hope to fix this soon in a 4.2.x release.
    
    For an overview of features new since 4.1.x, please see the 4.2.0 announcement blog post.
    
    Bug Fixes
    bind getAllDomains: ignore per-zone exceptions

--- net/powerdns/patches/patch-pdns_version.cc
+++ net/powerdns/patches/patch-pdns_version.cc
@@ -1,20 +1,15 @@
-$NetBSD: patch-pdns_version.cc,v 1.1 2018/01/13 15:30:54 adam Exp $
+$NetBSD: patch-pdns_version.cc,v 1.2 2020/03/17 19:04:49 adam Exp $
 
-Do not store configure arguments; they have references to the build directory.
+Do not store configure args, as they contain workdir references.
 
---- pdns/version.cc.orig	2018-01-13 15:13:31.000000000 +0000
+--- pdns/version.cc.orig	2020-02-29 22:23:50.000000000 +0000
 +++ pdns/version.cc
-@@ -117,13 +117,6 @@ void showBuildConfiguration()
+@@ -145,7 +145,7 @@ void showBuildConfiguration()
    // Auth only
-   theL()<<Logger::Warning<<"Built-in modules: "<<PDNS_MODULES<<endl;
+   g_log<<Logger::Warning<<"Built-in modules: "<<PDNS_MODULES<<endl;
  #endif
 -#ifdef PDNS_CONFIG_ARGS
--#define double_escape(s) #s
--#define escape_quotes(s) double_escape(s)
--  theL()<<Logger::Warning<<"Configured with: "<<escape_quotes(PDNS_CONFIG_ARGS)<<endl;
--#undef escape_quotes
--#undef double_escape
--#endif
- }
- 
- string fullVersionString()
++#if 0
+ #define double_escape(s) #s
+ #define escape_quotes(s) double_escape(s)
+   g_log<<Logger::Warning<<"Configured with: "<<escape_quotes(PDNS_CONFIG_ARGS)<<endl;
