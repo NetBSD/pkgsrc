8e8bb91893314b83e73865216f23e2d4d233e04f
adam <adam@pkgsrc.org>
2020-03-17 18:17:06 +0000

    unixodbc: fix buildlinking

--- databases/unixodbc/buildlink3.mk
+++ databases/unixodbc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2018/01/07 13:04:00 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2020/03/17 18:17:06 adam Exp $
 
 BUILDLINK_TREE+=	unixodbc
 
@@ -8,6 +8,8 @@ UNIXODBC_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.unixodbc+=	unixodbc>=2.3.2
 BUILDLINK_ABI_DEPENDS.unixodbc+=	unixodbc>=2.3.2
 BUILDLINK_PKGSRCDIR.unixodbc?=		../../databases/unixodbc
+
+.include "../../devel/libltdl/buildlink3.mk"
 .endif # UNIXODBC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-unixodbc
