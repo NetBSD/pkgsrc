9d05197e2ef2c0752d915eda74224857b10c4bb2
gdt <gdt@pkgsrc.org>
2020-03-18 00:02:46 +0000

    geography/viking: Update to 1.8
    Viking 1.8 (2020-02-02)
    New features since 1.7
    * New GeoClue Layer to Track Position from the GeoClue service
    ** Not available on Windows
    * Per Year Track Analysis
    * Track Timing Speed Splits Analysis
    * Subsecond Timing Precision for Tracks & Waypoints
    * Track Area Coverage Feature
    * Native Read Support of Zip & bzip2 Files
    ** This attempts to load supported files found within the compressed file
    * Native Basic TCX Read Support
    * Embedded (Elevation & Speed) Graphs in the Main Window
    * Improved icons for Aggregate and Maps layers
    * Scrolling Options for Better Usage with Touchpads
    * Right Click Menu Options on the Calendar
    * Various Keyboard Enhancements:
    ** Left & Right arrow keys selects the previous or next trackpoint respectively
    ** Keypad '+' & Keypad '-' inserts a trackpoint after or before of the current trackpoint
    ** Menu key opens the track context menu
    * SF Features#117: Area Calculation Tool
    * Large Basque language translation update
    * Many other small fixes - see the ChangeLog for the full details.

--- geography/viking/patches/patch-src_gpx.c
+++ geography/viking/patches/patch-src_gpx.c
@@ -1,15 +1,16 @@
-$NetBSD: patch-src_gpx.c,v 1.2 2019/01/22 15:28:45 gdt Exp $
+$NetBSD: patch-src_gpx.c,v 1.3 2020/03/18 00:02:46 gdt Exp $
 
 This is a build fix for SunOS.
 \todo Explain why, and if the new __sun way is correct in general.
 \todo File upstream.
 
---- src/gpx.c.orig	2015-02-03 22:44:42.000000000 +0000
+--- src/gpx.c.orig	2020-01-30 08:34:43.000000000 +0000
 +++ src/gpx.c
-@@ -30,7 +30,11 @@
- #include "config.h"
- #endif
- 
+@@ -26,7 +26,12 @@
+  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  *
+  */
++
 +#if defined(__sun)
 +#define _XOPEN_SOURCE 600
 +#else
