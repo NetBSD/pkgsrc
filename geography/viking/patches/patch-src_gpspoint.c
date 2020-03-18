b251584c5195168b9feb97927ab4f71e54458747
gdt <gdt@pkgsrc.org>
2020-03-18 00:02:46 +0000

    geography/viking: Update to 1.8
    Viking 1.8 (2020-02-02)
    New features since 1.7
    * New GeoClue Layer to Track Position from the GeoClue service
    ** Not available on Windows
    * Per Year Track Analysis
    * Track Timing Speed Splits Analysis
    * Subsecond Timing Precision for Tracks & Waypoints
    * Track Area Coverage Feature
    * Native Read Support of Zip & bzip2 Files
    ** This attempts to load supported files found within the compressed file
    * Native Basic TCX Read Support
    * Embedded (Elevation & Speed) Graphs in the Main Window
    * Improved icons for Aggregate and Maps layers
    * Scrolling Options for Better Usage with Touchpads
    * Right Click Menu Options on the Calendar
    * Various Keyboard Enhancements:
    ** Left & Right arrow keys selects the previous or next trackpoint respectively
    ** Keypad '+' & Keypad '-' inserts a trackpoint after or before of the current trackpoint
    ** Menu key opens the track context menu
    * SF Features#117: Area Calculation Tool
    * Large Basque language translation update
    * Many other small fixes - see the ChangeLog for the full details.

--- geography/viking/patches/patch-src_gpspoint.c
+++ geography/viking/patches/patch-src_gpspoint.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_gpspoint.c,v 1.1 2019/01/22 15:28:45 gdt Exp $
+$NetBSD: patch-src_gpspoint.c,v 1.2 2020/03/18 00:02:46 gdt Exp $
 
 Work around a bug in NetBSD.  See PR standards/40695.  Bug is fixed in
 -current but not -5; revisit if the workaround causes problems.
@@ -6,9 +6,9 @@ While 5 is very old, keep the patch because somebody might be using it
 and the pach is not problematic.
 (Not filed upstream because this is a NetBSD bug.)
 
---- src/gpspoint.c.orig	2018-12-23 10:48:06.000000000 +0000
+--- src/gpspoint.c.orig	2020-01-30 08:34:43.000000000 +0000
 +++ src/gpspoint.c
-@@ -94,8 +94,25 @@ static gdouble line_altitude = VIK_DEFAU
+@@ -79,8 +79,25 @@ static gdouble line_altitude = NAN;
  static gboolean line_visible = TRUE;
  
  static gboolean line_extended = FALSE;
@@ -33,4 +33,4 @@ and the pach is not problematic.
 +#endif /* NetBSD */
  static gint line_sat = 0;
  static gint line_fix = 0;
- static gdouble line_hdop = VIK_DEFAULT_DOP;
+ static gdouble line_hdop = NAN;
