357a22dca5dba68d7fbc1650bcc56eeab09eb2ae
joerg <joerg@pkgsrc.org>
2020-03-18 17:52:39 +0000

    Fix obvious code bug. Bump revision.

--- devel/picasm/patches/patch-picasm.c
+++ devel/picasm/patches/patch-picasm.c
@@ -1,9 +1,19 @@
-$NetBSD: patch-picasm.c,v 1.1 2016/06/12 20:03:45 kamil Exp $
+$NetBSD: patch-picasm.c,v 1.2 2020/03/18 17:52:39 joerg Exp $
 
+Fix tautological check.
 Remove unused variable.
 
 --- picasm.c.orig	2005-06-29 07:05:34.000000000 +0000
 +++ picasm.c
+@@ -895,7 +895,7 @@ gen_byte_c(int instr_code)
+ 	sym = lookup_symbol(token_string, symtype);
+ 	if(sym != NULL && sym->type != SYM_FORWARD)
+ 	{
+-	    if(sym->type != SYM_SET || sym->type != SYM_DEFINED)
++	    if(sym->type != SYM_SET && sym->type != SYM_DEFINED)
+ 	    {
+ 		error(1, "Invalid symbol type");
+ 		return FAIL;
 @@ -1866,7 +1866,7 @@ int
  main(int argc, char *argv[])
  {
