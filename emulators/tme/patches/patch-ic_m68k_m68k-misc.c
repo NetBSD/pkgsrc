d373ac0d2537ac4e5d0fc01fd7dba3ffc16c8090
rin <rin@pkgsrc.org>
2020-03-19 02:27:21 +0000

    Fix logic to determine whether destination EA for cas lies b/w page boundary or not; casl was mistakenly treated as an illegal instruction when destination is the highest long word in a page. This results in kernel panic in supervisor mode.
    Bump revision.
    
    Now, NetBSD/sun3 boots multiuser again. Have fun!

--- emulators/tme/patches/patch-ic_m68k_m68k-misc.c
+++ emulators/tme/patches/patch-ic_m68k_m68k-misc.c
@@ -1,10 +1,13 @@
-$NetBSD: patch-ic_m68k_m68k-misc.c,v 1.1 2013/12/28 19:16:48 martin Exp $
+$NetBSD: patch-ic_m68k_m68k-misc.c,v 1.2 2020/03/19 02:27:21 rin Exp $
 
-Fix from upstream CVS: fix sequencing and fault handling for read-modify-write
-instructions.
+- Fix from upstream CVS: fix sequencing and fault handling for
+  read-modify-write instructions.
 
---- ic/m68k/m68k-misc.c.orig	2009-08-29 21:47:52.000000000 +0200
-+++ ic/m68k/m68k-misc.c	2013-12-23 13:12:54.000000000 +0100
+- Fix logic to determine whether destination EA for cas lies b/w page
+  boundary or not.
+
+--- ic/m68k/m68k-misc.c.orig	2009-08-30 04:47:52.000000000 +0900
++++ ic/m68k/m68k-misc.c	2020-03-18 23:15:56.877341698 +0900
 @@ -1441,6 +1441,9 @@ tme_m68k_rmw_start(struct tme_m68k *ic,
    tme_uint32_t *buffer_reg;
    int supported;
@@ -15,6 +18,15 @@ instructions.
    /* if the user reran the cycle: */
    if (TME_M68K_SEQUENCE_RESTARTING
        && (ic->_tme_m68k_group0_buffer_read_softrr > 0
+@@ -1690,7 +1693,7 @@ tme_m68k_rmw_start(struct tme_m68k *ic,
+       tlb = rmw->tme_m68k_rmw_tlbs[address_i];
+ 
+       /* if this TLB entry doesn't cover the entire operand: */
+-      if ((((tme_bus_addr32_t) tlb->tme_m68k_tlb_linear_last) - address) < rmw->tme_m68k_rmw_size) {
++      if ((((tme_bus_addr32_t) tlb->tme_m68k_tlb_linear_last + 1) - address) < rmw->tme_m68k_rmw_size) {
+ 
+ 	/* we can't support this instruction on this memory, because
+ 	   we can't split an atomic operation across TLB entries.  on
 @@ -1754,9 +1757,6 @@ tme_m68k_rmw_start(struct tme_m68k *ic,
  	  tlbs_busy[!tlb_i] = FALSE;
  	}
