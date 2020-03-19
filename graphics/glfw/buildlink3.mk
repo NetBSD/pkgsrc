1e3a73ab2659dc4f8229ee0630d43f5ac4fd5115
nia <nia@pkgsrc.org>
2020-03-19 11:23:53 +0000

    glfw: Update to 3.3.2
    This is a primarily a bug fix release. It improves Vulkan support on macOS with VK_EXT_metal_surface support and better run-time loading, and adds fixes for issues on Windows, macOS and X11.

--- graphics/glfw/buildlink3.mk
+++ graphics/glfw/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/02/04 11:38:10 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/03/19 11:23:53 nia Exp $
 
 BUILDLINK_TREE+=	glfw
 
@@ -8,16 +8,22 @@ GLFW_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.glfw+=	glfw>=3.1.2
 BUILDLINK_PKGSRCDIR.glfw?=	../../graphics/glfw
 
-.include "../../graphics/glu/buildlink3.mk"
-.include "../../graphics/MesaLib/buildlink3.mk"
-.include "../../x11/libICE/buildlink3.mk"
-.include "../../x11/libX11/buildlink3.mk"
-.include "../../x11/libXext/buildlink3.mk"
-.include "../../x11/libXinerama/buildlink3.mk"
-.include "../../x11/libXrandr/buildlink3.mk"
-.include "../../x11/libxkbcommon/buildlink3.mk"
-.include "../../mk/dlopen.buildlink3.mk"
-.include "../../mk/pthread.buildlink3.mk"
+pkgbase:= glfw
+
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.glfw:Mx11)
+.  include "../../x11/libX11/buildlink3.mk"
+.  include "../../x11/libXrandr/buildlink3.mk"
+.endif
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} != "Darwin"
+.  include "../../graphics/MesaLib/buildlink3.mk"
+.  include "../../graphics/glu/buildlink3.mk"
+.endif
+
 .endif	# GLFW_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-glfw
