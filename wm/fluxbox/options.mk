0cd72bdfe9f9ed07f45596d6f181290e5f15f56c
rillig <rillig@pkgsrc.org>
2020-03-21 15:58:49 +0000

    wm/fluxbox: remove unknown configure options

--- wm/fluxbox/options.mk
+++ wm/fluxbox/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.6 2012/04/17 17:13:01 bsiegert Exp $
+# $NetBSD: options.mk,v 1.7 2020/03/21 15:58:49 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fluxbox
-PKG_SUPPORTED_OPTIONS=	gnome kde imlib2 xft xrender nls
+PKG_SUPPORTED_OPTIONS=	imlib2 xft xrender nls
 PKG_SUGGESTED_OPTIONS=	xrender
 
 .include "../../mk/bsd.options.mk"
@@ -11,18 +11,6 @@ PKG_SUGGESTED_OPTIONS+=	nls
 .endif
 
 PLIST_VARS+=		nls
-
-.if !empty(PKG_OPTIONS:Mgnome)
-CONFIGURE_ARGS+=	--enable-gnome
-.else
-CONFIGURE_ARGS+=	--disable-gnome
-.endif
-
-.if !empty(PKG_OPTIONS:Mkde)
-CONFIGURE_ARGS+=	--enable-kde
-.else
-CONFIGURE_ARGS+=	--disable-kde
-.endif
 
 .if !empty(PKG_OPTIONS:Mimlib2)
 CONFIGURE_ARGS+=	--enable-imlib2
@@ -48,7 +36,7 @@ CONFIGURE_ARGS+=	--disable-xrender
 .if !empty(PKG_OPTIONS:Mnls)
 CONFIGURE_ARGS+=	--enable-nls
 .include "../../converters/libiconv/buildlink3.mk"
-PLIST.nls=		Yes
+PLIST.nls=		yes
 .else
 CONFIGURE_ARGS+=	--disable-nls
 .endif
