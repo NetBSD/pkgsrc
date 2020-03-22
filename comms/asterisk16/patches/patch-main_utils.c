d2a97887abb605e65b50871490a0b63530521f8c
tnn <tnn@pkgsrc.org>
2020-03-22 23:09:24 +0000

    asterisk16: fix L§inux packaging issues

--- comms/asterisk16/patches/patch-main_utils.c
+++ comms/asterisk16/patches/patch-main_utils.c
@@ -1,19 +1,21 @@
-$NetBSD: patch-main_utils.c,v 1.1 2019/08/20 13:47:42 ryoon Exp $
+$NetBSD: patch-main_utils.c,v 1.2 2020/03/22 23:09:24 tnn Exp $
 
---- main/utils.c.orig	2018-05-01 20:12:26.000000000 +0000
+--- main/utils.c.orig	2019-12-23 16:43:56.000000000 +0000
 +++ main/utils.c
-@@ -39,6 +39,10 @@
+@@ -39,6 +39,12 @@
  #elif defined(HAVE_SYS_THR_H)
  #include <sys/thr.h>
  #endif
 +#if defined(HAVE_GETIFADDRS)
 +#include <ifaddrs.h>
++#if !defined(__linux__)
 +#include <net/if_dl.h>
++#endif
 +#endif
  
  #include "asterisk/network.h"
  #include "asterisk/ast_version.h"
-@@ -612,7 +616,7 @@ const char *ast_inet_ntoa(struct in_addr
+@@ -612,7 +618,7 @@ const char *ast_inet_ntoa(struct in_addr
  
  static int dev_urandom_fd = -1;
  
