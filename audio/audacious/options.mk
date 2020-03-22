5bc027da97258d080c6a6f9c0ced996b80ae45da
nia <nia@pkgsrc.org>
2020-03-22 08:39:48 +0000

    audacious: Update to 4.0
    This release switches to Qt 5 by default. The Qt UI has become quite polished by now. It will be quite familiar to users of the GTK2 UI, but it also brings a few nice-to-have improvements, such as a playlist view that is easier to navigate and sort.
    
    GTK2 remains available and supported as a build option, but new features will only be added to the Qt UI going forward.
    
    Some things that we wanted to get finished didn't make it into this release. In particular, the Qt port of the Winamp-like UI is still missing some key features such the Jump to Song window. Users of the Winamp-like UI may want to continue using GTK2 for now. The Windows builds are also still using GTK2, until some Windows-specific font size issues (on displays greater than 96 DPI) can be worked out in the Qt version.
    
    New features and improvements:
    
        Clicking on playlist column headers sorts the playlist (#8)
        Dragging playlist column headers changes the column order (#10)
        Application-wide settings for volume and time step sizes (#330)
        New option to hide playlist tabs (#547)
        Sorting playlist by path now sorts folders after files (#843)
        Implemented additional MPRIS calls for compatibility with KDE 5.16+ (#900)
        New OpenMPT-based tracker module plugin (contributed by Chris Spiegel)
        New VU Meter visualization plugin (contributed by Marc Sánchez)
        Added option to use a SOCKS network proxy (contributed by Róbert Čerňanský)
        The Song Change plugin now works on Windows (contributed by Domen Mori)
        New “Next Album” and “Previous Album” commands
        The tag editor in Qt UI can now edit multiple files at once
        Implemented equalizer presets window for Qt UI
        Lyrics plugin gained the ability to save and load lyrics locally
        Blur Scope and Spectrum Analyzer visualizations ported to Qt
        MIDI plugin soundfont selection ported to Qt
        JACK output plugin gained some new options
        Added option to endlessly loop PSF files
    
    Bug fixes since 3.10.1:
    
        Fixed blurry application icon on Windows (#863)
        Cuesheet entries in an .m3u playlist are now added correctly (#878)
        Various minor fixes in handling of output stream
    
    Other:
    
        Experimental support for building with meson

--- audio/audacious/options.mk
+++ audio/audacious/options.mk
@@ -1,14 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2019/07/06 13:17:00 nia Exp $
+# $NetBSD: options.mk,v 1.2 2020/03/22 08:39:48 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.audacious
-PKG_OPTIONS_OPTIONAL_GROUPS=	ui
-PKG_OPTIONS_GROUP.ui=		qt5 gtk2
 PKG_SUPPORTED_OPTIONS+=		dbus
-PKG_SUGGESTED_OPTIONS+=		dbus qt5 gtk2
+PKG_SUGGESTED_OPTIONS+=		dbus
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	dbus gtk qt
+PLIST_VARS+=	dbus
 
 .if !empty(PKG_OPTIONS:Mdbus)
 PLIST.dbus=	yes
@@ -16,17 +14,4 @@ TOOL_DEPENDS+=	gdbus-codegen-[0-9]*:../../devel/gdbus-codegen
 .include "../../sysutils/dbus/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-dbus
-.endif
-
-.if !empty(PKG_OPTIONS:Mqt5)
-PLIST.qt=	yes
-CONFIGURE_ARGS+=	--enable-qt
-.include "../../x11/qt5-qtbase/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Mgtk2)
-PLIST.gtk=	yes
-.include "../../x11/gtk2/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-gtk
 .endif
