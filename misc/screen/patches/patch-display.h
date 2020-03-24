8c0d69a7efe58919334560ae7f4bf8f87d45e6e7
rillig <rillig@pkgsrc.org>
2020-03-24 06:55:34 +0000

    misc/screen: fix paths in example files, fix array subscript
    The warnings in the patches are no longer necessary since pkglint 19.4.13
    reports an error for these cases.

---
+++ misc/screen/patches/patch-display.h
@@ -0,0 +1,33 @@
+$NetBSD: patch-display.h,v 1.1 2020/03/24 06:55:34 rillig Exp $
+
+display.c: In function 'disp_readev_fn':
+display.c:3590:39: error: array subscript has type 'char' [-Werror=char-subscripts]
+                   D_mouse_parse.params[D_mouse_parse.state] = 0;
+                                       ^
+display.c:3605:43: error: array subscript has type 'char' [-Werror=char-subscripts]
+                       D_mouse_parse.params[D_mouse_parse.state] *= 10;
+                                           ^
+display.c:3606:43: error: array subscript has type 'char' [-Werror=char-subscripts]
+                       D_mouse_parse.params[D_mouse_parse.state] += c - '0';
+                                           ^
+display.c:3618:43: error: array subscript has type 'char' [-Werror=char-subscripts]
+                       D_mouse_parse.params[D_mouse_parse.state] = 0;
+                                           ^
+display.c:3626:39: error: array subscript has type 'char' [-Werror=char-subscripts]
+                   D_mouse_parse.params[D_mouse_parse.state++] = c;
+                                       ^
+
+The constants for the state are from an anonymous enum at the top of
+display.c. They are all small nonnegative numbers.
+
+--- display.h.orig	2020-02-05 20:09:38.000000000 +0000
++++ display.h
+@@ -61,7 +61,7 @@ struct win;			/* forward declaration */
+ struct mouse_parse
+ {
+   char sgrmode;                 /* non-zero if parsing an SGR sequence */
+-  char state;                   /* current state of parsing */
++  int state;                    /* current state of parsing */
+   int params[3];                /* parsed params: button, x, y */
+ };
+ 
