17c505b8c5a86373ffe2982122877fc2ca45d78d
adam <adam@pkgsrc.org>
2020-03-24 16:34:45 +0000

    ghostscript-agpl: updated to 9.52
    Version 9.52:
    
    Highlights in this release include:
    
    The 9.52 release replaces the 9.51 release after a problem was reported with 9.51 which warranted the quick turnaround. Thus, like 9.51, 9.52 is primarily a maintenance release, consolidating the changes we introduced in 9.50.
    
    We have continued our work on code hygiene for this release, with a focus on the static analysis tool Coverity (from Synopsys, Inc) and we have now adopted a policy of maintaining zero Coverity issues in the Ghostscript/GhostPDL source base.
    
    IMPORTANT: In consultation with a representative of (OpenPrinting) it is our intention to deprecate and, in the not distant future, remove the OpenPrinting Vector/Raster Printer Drivers (that is, the opvp and oprp devices).
    
    If you rely on either of these devices, please get in touch with us, so we can discuss your use case, and revise our plans accordingly.
    
    IMPORTANT: We have forked LittleCMS2 into LittleCMS2mt (the "mt" indicating "multi-thread"). LCMS2 is not thread-safe, and cannot be made thread-safe without breaking the ABI. Our fork will be thread-safe and include performance enhancements (these changes have all be been offered and rejected upstream). We will maintain compatibility between Ghostscript and LCMS2 for a time, but not in perpetuity. If there is sufficient interest, our fork will be available as its own package separately from Ghostscript (and MuPDF).
    
    The usual round of bug fixes, compatibility changes, and incremental improvements.

--- print/ghostscript-agpl/patches/patch-base_mkromfs.c
+++ print/ghostscript-agpl/patches/patch-base_mkromfs.c
@@ -1,16 +1,11 @@
-$NetBSD: patch-base_mkromfs.c,v 1.1 2017/03/31 07:26:23 adam Exp $
+$NetBSD: patch-base_mkromfs.c,v 1.2 2020/03/24 16:34:45 adam Exp $
 
-chunk 1:
-fix build with _FORTIFY_SOURCE=2; it seems the arguments sometimes
+Fix build with _FORTIFY_SOURCE=2; it seems the arguments sometimes
 overlap.
 
-chunk 2:
-Replace BSD/Linux specific "qd" printf format specifier by "lld",
-should fix build problem on Solaris reported by Joern Clausen per PR pkg/40664
-
---- base/mkromfs.c.orig	2017-03-16 10:12:02.000000000 +0000
+--- base/mkromfs.c.orig	2020-03-19 08:21:42.000000000 +0000
 +++ base/mkromfs.c
-@@ -2014,7 +2014,7 @@ ws(const byte *str, int len)
+@@ -2137,7 +2137,7 @@ ws(const byte *str, int len)
      if (len >= LINE_SIZE)
          exit(1);
  
@@ -19,12 +14,3 @@ should fix build problem on Solaris reported by Joern Clausen per PR pkg/40664
      flush_line_buf(len);
  }
  
-@@ -2368,7 +2368,7 @@ main(int argc, char *argv[])
-     }
-     if (!buildtime)
-         buildtime = time(NULL);
--    fprintf(out,"    time_t gs_romfs_buildtime = %ld;\n\n", buildtime);
-+    fprintf(out,"    time_t gs_romfs_buildtime = %lld;\n\n", (long long)buildtime);
- 
-     /* process the remaining arguments (options interspersed with paths) */
-     for (; atarg < argc; atarg++) {
