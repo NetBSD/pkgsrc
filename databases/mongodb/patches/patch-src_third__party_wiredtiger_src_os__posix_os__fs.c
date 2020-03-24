965ebc4980a5ba2b91863373180a574362464d18
adam <adam@pkgsrc.org>
2020-03-24 14:34:06 +0000

    mongodb: updated to 4.2.5
    4.2.5:
    Unknown changes
    
    4.2.4:
    Issues fixed:
    SERVER-44915: Extend $indexStats output to include full index options and shard name
    SERVER-46121: mongos crashes with invariant error after changing taskExecutorPoolSize
    SERVER-45137: Increasing memory allocation in Top::record with high rate of collection creates and drops
    SERVER-44904: Startup recovery should not delete corrupt documents while rebuilding unfinished indexes
    SERVER-44260: Transaction can conflict with previous transaction on the session if the all committed point is held back
    SERVER-35050: Don’t abort collection clone due to negative document count
    SERVER-39112: Primary drain mode can be unnecessarily slow

--- databases/mongodb/patches/patch-src_third__party_wiredtiger_src_os__posix_os__fs.c
+++ databases/mongodb/patches/patch-src_third__party_wiredtiger_src_os__posix_os__fs.c
@@ -1,15 +1,22 @@
-$NetBSD: patch-src_third__party_wiredtiger_src_os__posix_os__fs.c,v 1.1 2020/02/01 20:01:16 adam Exp $
+$NetBSD: patch-src_third__party_wiredtiger_src_os__posix_os__fs.c,v 1.2 2020/03/24 14:34:06 adam Exp $
 
-On NetBSD, fdatasync() fails with "Bad file descriptor".
+The fdatasync call is specified by POSIX, and the definition has changed:
+  https://pubs.opengroup.org/onlinepubs/009695399/functions/fdatasync.html
+  https://pubs.opengroup.org/onlinepubs/9699919799/functions/fdatasync.html  
 
---- src/third_party/wiredtiger/src/os_posix/os_fs.c.orig	2020-01-29 21:12:06.697276371 +0000
+NetBSD's fdatasync follows the earlier specification.  For now, simply avoid
+crashing on EBADF, at the possible expense of sync safety.
+
+\todo Change NetBSD to follow the current standard.
+
+--- src/third_party/wiredtiger/src/os_posix/os_fs.c.orig	2020-03-19 16:16:51.000000000 +0000
 +++ src/third_party/wiredtiger/src/os_posix/os_fs.c
-@@ -91,7 +91,7 @@ __posix_sync(WT_SESSION_IMPL *session, i
-         WT_PANIC_RET(session, ret, "%s: %s: fcntl(F_FULLFSYNC)", name, func);
-     }
- #endif
--#if defined(HAVE_FDATASYNC)
-+#if defined(HAVE_FDATASYNC) && !defined(__NetBSD__)
+@@ -94,7 +94,7 @@ __posix_sync(WT_SESSION_IMPL *session, i
+ #if defined(HAVE_FDATASYNC)
      /* See comment in __posix_sync(): sync cannot be retried or fail. */
      WT_SYSCALL(fdatasync(fd), ret);
-     if (ret == 0)
+-    if (ret == 0)
++    if (ret == 0 || errno == EBADF)
+         return (0);
+     WT_PANIC_RET(session, ret, "%s: %s: fdatasync", name, func);
+ #else
