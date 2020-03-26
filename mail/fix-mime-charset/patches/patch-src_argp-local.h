94adcb8acc148e3535e5a6b774b096910fdcdccf
joerg <joerg@pkgsrc.org>
2020-03-26 02:30:49 +0000

    Fix const correctness with C++ compliant string.h

--- mail/fix-mime-charset/patches/patch-src_argp-local.h
+++ mail/fix-mime-charset/patches/patch-src_argp-local.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_argp-local.h,v 1.2 2011/12/16 16:55:25 hans Exp $
+$NetBSD: patch-src_argp-local.h,v 1.3 2020/03/26 02:30:49 joerg Exp $
 
---- src/argp-local.h.orig	2011-11-24 00:53:41.000000000 +0000
+--- src/argp-local.h.orig	2003-11-25 23:51:41.000000000 +0000
 +++ src/argp-local.h
 @@ -10,6 +10,7 @@
  //
@@ -9,6 +9,15 @@ $NetBSD: patch-src_argp-local.h,v 1.2 2011/12/16 16:55:25 hans Exp $
 +#include <stdlib.h>
  #include <string.h>
  #include <iostream>
+ 
+@@ -65,7 +66,7 @@ const argp_option _argp_default_options[
+ 
+ string _argp_progname(const char *arg)
+ {
+-char *p=strrchr(arg,'/');
++const char *p=strrchr(arg,'/');
+ return p==NULL ? arg : p+1;
+ }
  
 @@ -196,7 +197,11 @@ for(p=_argp->options;p!=_argp_default_op
     if(!p->name && !p->key)
