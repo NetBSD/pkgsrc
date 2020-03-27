8268b6036996d95427973617e9cb9b0c33c7e195
nia <nia@pkgsrc.org>
2020-03-27 17:46:53 +0000

    libftdi1: Update libusb reference in buildlink3

--- devel/libftdi1/buildlink3.mk
+++ devel/libftdi1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2018/02/10 13:53:47 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/03/27 17:46:53 nia Exp $
 
 BUILDLINK_TREE+=	libftdi1
 
@@ -8,7 +8,7 @@ LIBFTDI1_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libftdi1+=	libftdi1>=1.0
 BUILDLINK_PKGSRCDIR.libftdi1?=		../../devel/libftdi1
 
-.include "../../mk/libusb.buildlink3.mk"
+.include "../../devel/libusb1/buildlink3.mk"
 .endif	# LIBFTDI1_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libftdi1
