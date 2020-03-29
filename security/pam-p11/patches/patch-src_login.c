ab78168935ec53b888ba76f10f523ab201779137
manu <manu@pkgsrc.org>
2020-03-29 02:18:55 +0000

    Update pam-p11 to 0.3.1
    Changes since pam-p11-0.1.5 from the NEWS file:
    
    New in 0.3.1; 2019-09-11; Frank Morgner
    * CVE-2019-16058: Fixed buffer overflow when creating signatures longer than 256
     bytes
    
    New in 0.3.0; 2019-04-24; Frank Morgner
    * Add Italian translation
    * Add support for matching the PIN-input with a regular expression
    * Add support for macOS
    * Add support for building with OpenSSL 1.1.1
    * Add support for nistp256/384/521 keys in authorized_keys file
    
    New in 0.2.0; 2018-05-16; Frank Morgner
    * Add user documentation in Readme.md
    * Add support for PIN pad readers
    * Add support for changing/unblocking PIN (use with passwd)
    * Add support for localized user feedback
    * Add support for cards without certificates (e.g. OpenPGP card)
    * Add support for PKCS#11 modules with multiple slots
    * Add support for building with OpenSSL 1.1
    * Merged opensc and openssh module into pam_p11.so
    * Fixed memory leaks, coverity issues, compiler warnings
    * Created `test-passwd` and `test-login` for testing standard use cases
    
    New in 0.1.6; 2017-03-06; Alon Bar-Lev
    * Build system rewritten (NOTICE: configure options was modified).

---
+++ security/pam-p11/patches/patch-src_login.c
@@ -0,0 +1,16 @@
+$NetBSD: patch-src_login.c,v 1.1 2020/03/29 02:18:56 manu Exp $
+--- src/login.c.orig	2020-03-28 02:25:09.912642023 +0000
++++ src/login.c	2020-03-28 02:25:31.170971060 +0000
+@@ -19,8 +19,12 @@
+ #ifdef HAVE_CONFIG_H
+ #include "config.h"
+ #endif
+ 
++#ifdef __NetBSD__
++#define NO_STATIC_MODULES
++#endif
++
+ #include <security/pam_modules.h>
+ 
+ extern int pam_sm_test(pam_handle_t *pamh, int flags, int argc, const char **argv);
+ 
