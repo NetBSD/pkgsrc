823156bdc0b01b5642f79fa0f6235ff8e8ac4e26
adam <adam@pkgsrc.org>
2020-03-30 13:54:29 +0000

    Added www/nghttp2-tools (to avoid circular dependency)

--- www/nghttp2/options.mk
+++ www/nghttp2/options.mk
@@ -1,11 +1,11 @@
-# $NetBSD: options.mk,v 1.7 2019/11/20 16:38:22 adam Exp $
+# $NetBSD: options.mk,v 1.8 2020/03/30 13:54:29 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nghttp2
-PKG_SUPPORTED_OPTIONS=	nghttp2-asio nghttp2-tools
+PKG_SUPPORTED_OPTIONS=	nghttp2-asio
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	asio tools
+PLIST_VARS+=	asio
 
 ###
 ### Build the ASIO C++ library
@@ -24,24 +24,4 @@ PLIST.asio=		yes
 .include "../../security/openssl/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--with-boost=no
-.endif
-
-###
-### Build apps and tools
-###
-.if !empty(PKG_OPTIONS:Mnghttp2-tools)
-# Upstream documents C++14 and gcc>=6 or clang>=6
-USE_LANGUAGES+=		c++14
-GCC_REQD+=		6
-CONFIGURE_ARGS+=	--enable-app
-CONFIGURE_ARGS+=	--enable-hpack-tools
-PLIST.tools=		yes
-.include "../../devel/libev/buildlink3.mk"
-.include "../../devel/zlib/buildlink3.mk"
-.include "../../net/libcares/buildlink3.mk"
-.include "../../security/openssl/buildlink3.mk"
-.include "../../textproc/jansson/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-app
-CONFIGURE_ARGS+=	--disable-hpack-tools
 .endif
