4d45ecc5a708fda73c2deb176fac3aa375906812
nia <nia@pkgsrc.org>
2020-03-30 11:07:18 +0000

    weechat: Drop patches that are no longer needed.

--- chat/weechat/patches/patch-src_core_weechat.c
+++ chat/weechat/patches/patch-src_core_weechat.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_core_weechat.c,v 1.1 2019/03/07 18:09:24 jperkin Exp $
+$NetBSD: patch-src_core_weechat.c,v 1.2 2020/03/30 11:07:18 nia Exp $
 
-Need unistd.h for mkdtemp(3).
+Need unistd.h for mkdtemp(3) (on what platform?)
 
 --- src/core/weechat.c.orig	2019-02-17 07:20:07.000000000 +0000
 +++ src/core/weechat.c
