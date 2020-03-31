ac98f07768097895abaf7c399141bddd44dd0f9c
gdt <gdt@pkgsrc.org>
2020-03-31 14:34:41 +0000

    mongodb3: Update patch comments
    Several patches have been filed upstream.  Note URLs, and
    revise/expand comments.   This is a comment-only change.
    
    Thanks to Paul Ripke for filing issues upstream.

--- databases/mongodb3/patches/patch-src_third__party_asio-master_asio_include_asio_detail_impl_kqueue__reactor.ipp
+++ databases/mongodb3/patches/patch-src_third__party_asio-master_asio_include_asio_detail_impl_kqueue__reactor.ipp
@@ -1,6 +1,8 @@
-$NetBSD: patch-src_third__party_asio-master_asio_include_asio_detail_impl_kqueue__reactor.ipp,v 1.1 2020/02/01 20:05:54 adam Exp $
+$NetBSD: patch-src_third__party_asio-master_asio_include_asio_detail_impl_kqueue__reactor.ipp,v 1.2 2020/03/31 14:34:41 gdt Exp $
 
-Fix build with recent NetBSD 9.99.
+In NetBSD 9.99.15 and higher, adapt to new EV_SET types.
+
+Filed upstream as https://jira.mongodb.org/browse/SERVER-47145
 
 --- src/third_party/asio-master/asio/include/asio/detail/impl/kqueue_reactor.ipp.orig	2020-01-26 17:31:51.341577994 +0000
 +++ src/third_party/asio-master/asio/include/asio/detail/impl/kqueue_reactor.ipp
--- databases/mongodb3/patches/patch-src_third__party_wiredtiger_src_os__posix_os__fs.c
+++ databases/mongodb3/patches/patch-src_third__party_wiredtiger_src_os__posix_os__fs.c
@@ -1,13 +1,16 @@
-$NetBSD: patch-src_third__party_wiredtiger_src_os__posix_os__fs.c,v 1.4 2020/03/12 16:36:53 gdt Exp $
+$NetBSD: patch-src_third__party_wiredtiger_src_os__posix_os__fs.c,v 1.5 2020/03/31 14:34:41 gdt Exp $
 
 The fdatasync call is specified by POSIX, and the definition has changed:
   https://pubs.opengroup.org/onlinepubs/009695399/functions/fdatasync.html
   https://pubs.opengroup.org/onlinepubs/9699919799/functions/fdatasync.html  
 
-NetBSD's fdatasync follows the earlier specification.  For now, simply avoid
-crashing on EBADF, at the possible expense of sync safety.
+NetBSD's fdatasync followed the earlier specification.  Current has been
+updated, with pullups expected.  However, it will be a long time until
+all NetBSD systems can be presumed to have the fix.  For now, simply
+avoid crashing on EBADF, at the possible expense of sync safety.
 
-\todo Change NetBSD to follow the current standard.
+Not filed upstream, because this is working around a NetBSD bug and
+upstream would close it due to 3.4 being EOL anyway.
 
 --- src/third_party/wiredtiger/src/os_posix/os_fs.c.orig	2020-01-08 16:30:41.000000000 +0000
 +++ src/third_party/wiredtiger/src/os_posix/os_fs.c
