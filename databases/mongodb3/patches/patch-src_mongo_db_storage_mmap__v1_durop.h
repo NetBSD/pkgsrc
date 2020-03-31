23675892773289677eb3303ced5b1bf804ea8f72
gdt <gdt@pkgsrc.org>
2020-03-31 14:34:41 +0000

    mongodb3: Update patch comments
    Several patches have been filed upstream.  Note URLs, and
    revise/expand comments.   This is a comment-only change.
    
    Thanks to Paul Ripke for filing issues upstream.

--- databases/mongodb3/patches/patch-src_mongo_db_storage_mmap__v1_durop.h
+++ databases/mongodb3/patches/patch-src_mongo_db_storage_mmap__v1_durop.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_mongo_db_storage_mmap__v1_durop.h,v 1.2 2020/03/06 14:22:46 gdt Exp $
+$NetBSD: patch-src_mongo_db_storage_mmap__v1_durop.h,v 1.3 2020/03/31 14:34:41 gdt Exp $
 
 This file uses std::shared_ptr, but did not include the memory header as required.
 
-\todo File upstream and add upstream bugtracker URL.
+Filed upstream as ttps://jira.mongodb.org/browse/SERVER-47172
 
 --- src/mongo/db/storage/mmap_v1/durop.h.orig	2020-01-08 16:30:41.000000000 +0000
 +++ src/mongo/db/storage/mmap_v1/durop.h
