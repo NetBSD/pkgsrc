848794deb9f40e4c69bc10479c7d832a6d10b5a7
wiz <wiz@pkgsrc.org>
2020-04-01 14:51:05 +0000

    meson: update to 0.54.0.
    New features
    
    Emscripten (emcc) now supports threads
    Introduce dataonly for the pkgconfig module
    Consistently report file locations relative to cwd
    dependency() consistency
    Override dependency()
    Simplified dependency() fallback
    Backend agnostic compile command
    Native (build machine) compilers not always required
    Summary improvements
    Add a system type dependency for zlib
    Added 'name' method
    New option --quiet to meson install
    Property support emscripten's wasm-ld
    Skip sanity tests when cross compiling
    Support for overiding the linker with ldc and gdc
    Native file properties
    Changed the signal used to terminate a test process (group)
    Dynamic Linker environment variables actually match docs
    Per subproject default_library and werror options
    Environment Variables with Cross Builds
    Added 'pkg_config_libdir' property
    More new sample Meson templates for (Java, Cuda, and more)
    Ninja version requirement bumped to 1.7
    Added -C argument to meson init command
    More than one argument to message() and warning()
    Added has_tools method to qt module
    The MSI installer is only available in 64 bit version
    Uninstalled pkg-config files
    CMake find_package COMPONENTS support
    Added Microchip XC16 C compiler support
    Added Texas Instruments C2000 C/C++ compiler support
    Unity file block size is configurable
    
    More details:
    https://mesonbuild.com/Release-notes-for-0-54-0.html

--- devel/meson/patches/patch-mesonbuild_dependencies_dev.py
+++ devel/meson/patches/patch-mesonbuild_dependencies_dev.py
@@ -1,13 +1,13 @@
-$NetBSD: patch-mesonbuild_dependencies_dev.py,v 1.3 2020/03/17 10:25:39 adam Exp $
+$NetBSD: patch-mesonbuild_dependencies_dev.py,v 1.4 2020/04/01 14:51:05 wiz Exp $
 
 Allow handling different versions of LLVM in pkgsrc using LLVM_CONFIG_PATH.
 
---- mesonbuild/dependencies/dev.py.orig	2020-01-23 12:51:19.000000000 +0000
+--- mesonbuild/dependencies/dev.py.orig	2020-03-23 17:22:09.000000000 +0000
 +++ mesonbuild/dependencies/dev.py
-@@ -205,7 +205,10 @@ class LLVMDependencyConfigTool(ConfigToo
+@@ -201,7 +201,10 @@ class LLVMDependencyConfigTool(ConfigToo
      __cpp_blacklist = {'-DNDEBUG'}
  
-     def __init__(self, environment, kwargs):
+     def __init__(self, name: str, environment, kwargs):
 -        self.tools = get_llvm_tool_names('llvm-config')
 +        if 'LLVM_CONFIG_PATH' in os.environ:
 +            self.tools = [os.environ['LLVM_CONFIG_PATH']]
