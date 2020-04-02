06c07e9fad3333d03ab775612935b79d001af513
nia <nia@pkgsrc.org>
2020-04-02 15:52:46 +0000

    xine-lib: Update to 1.2.10
    xine-lib (1.2.10) 2019-12-13
      * Add first Android support.
      * Add (xcb)xv yuy2 emulation.
      * Add libavcodec v58 compatibility.
      * Add avio seek support.
      * Make libpostproc optional.
      * Add libdav1d, libaom and lavc AV1 video decoders.
      * Add libvpx multithreading.
      * Add libpng decoder.
      * Add a52 double and fixed point modes.
      * Add Opus audio support to ogg demuxer.
      * Add AV1 video support to matroska demuxer.
      * Add ivf demuxer.
      * Add mpeg-ts split payload support.
      * Add TLS support using gnutls or OpenSSL.
      * Add ftp input plugin (ftp://) with TLS support (ftpes://)
      * Add tls:// input plugin (raw TLS over TCP).
      * Add libnfs NFS input plugin.
      * Add ftp/http seek support.
      * Add scp forward seek support.
      * Add mp4 http streaming support (plain and fragment modes).
      * Add HLS streaming support.
      * Add HTTP 1.1 support.
      * Add OpenGL EGL and Wayland support.
      * Add generic bitrate estimation.
      * Add side stream feature.
      * Optimize demux_qt.
      * Optimize OSD.
      * Optimize output layers.
      * Optimize decoder threads.
      * Optimize event handling.
      * Optimize stream info.
      * Optimize TCP/TLS/HTTP network input.
      * Optimize network buffering control.
      * Alsa/oss startup optimization.
      * Optimize input_stdin_fifo.
      * Optimize internal liba52.
      * Optimize user seek.
      * Build optimizations.
      * Simply user config.
      * Better support for audio out drivers that cannot resume after pause.
      * Better support for mpeg pts jumps.
      * Better bluray seek.
      * XML parser fixes.
      * Fix midstream audio mode switch.
      * Fix FLAC audio playback via ffmpeg.
      * Fix ffmpeg mpeg1/2 video.
      * Fix C++ build.
      * Fix build on clang only systems.
      * Fix/optimize mpeg, mpeg-ts, qt, flv, matroska, real and asf demuxers.
      * Fix network seek.
      * Fix/optimize audio CD.
      * Fix DVD (occasional crashes, damaged video after a DVD had been played).
      * Fix opengl2 freeze after X server failure.
      * Fix tvtime deinterlacer crash.
      * Fix/optimize overlay.
      * Fix/optimize old VDR plugin.
      * Fix xine-ui freeze when opening a playlist while paused.
      * Security fixes.
      * Build fixes (C99 mode, vaapi, ImageMagick, libmvec, less warnings).
      * Many small fixes.
      * More error handling instead of aborting.
      * Update german translation.

--- multimedia/xine-lib/patches/patch-src-post-planar-noise.c
+++ multimedia/xine-lib/patches/patch-src-post-planar-noise.c
@@ -1,30 +1,12 @@
-$NetBSD: patch-src-post-planar-noise.c,v 1.5 2018/11/19 12:48:32 jperkin Exp $
+$NetBSD: patch-src-post-planar-noise.c,v 1.6 2020/04/02 15:52:46 nia Exp $
 
 https://bugs.xine-project.org/show_bug.cgi?id=524
 
 Disable MMX sections on SunOS.
 
---- src/post/planar/noise.c.orig	2018-01-11 12:49:47.000000000 +0000
+--- src/post/planar/noise.c.orig	2019-12-13 20:47:50.000000000 +0000
 +++ src/post/planar/noise.c
-@@ -141,7 +141,7 @@ static inline void lineNoise_C(uint8_t *
-     }
- }
- 
--#ifdef ARCH_X86
-+#if defined(ARCH_X86) && !defined(__sun)
- 
- #if defined(ARCH_X86_64)
- #  define TYPEA int64_t
-@@ -231,7 +231,7 @@ static inline void lineNoiseAvg_C(uint8_
-     }
- }
- 
--#ifdef ARCH_X86
-+#if defined(ARCH_X86) && !defined(__sun)
- 
- static inline void lineNoiseAvg_MMX(uint8_t *dst, uint8_t *src, int len, int8_t **shift){
-   TYPEA mmx_len = len & (~7);
-@@ -340,15 +340,20 @@ static const char *const enum_quality[]
+@@ -220,15 +220,20 @@ static const char *const enum_quality[] 
   * description of params struct
   */
  START_PARAM_DESCR( noise_parameters_t )
@@ -50,16 +32,7 @@ Disable MMX sections on SunOS.
      "Mix random noise with a (semi)regular pattern" )
  END_PARAM_DESCR( param_descr )
  
-@@ -467,7 +472,7 @@ void *noise_init_plugin(xine_t *xine, vo
-     class->description     = N_("Adds noise");
-     class->dispose         = default_post_class_dispose;
- 
--#ifdef ARCH_X86
-+#if defined(ARCH_X86) && !defined(__sun)
-     if (xine_mm_accel() & MM_ACCEL_X86_MMX) {
-         lineNoise = lineNoise_MMX;
-         lineNoiseAvg = lineNoiseAvg_MMX;
-@@ -587,7 +592,7 @@ static int noise_draw(vo_frame_t *frame,
+@@ -372,7 +377,7 @@ static int noise_draw(vo_frame_t *frame,
                frame->width * 2, frame->height, &this->params[0]);
      }
  
@@ -68,3 +41,12 @@ Disable MMX sections on SunOS.
      if (xine_mm_accel() & MM_ACCEL_X86_MMX)
          __asm__ __volatile__ ("emms\n\t");
      if (xine_mm_accel() & MM_ACCEL_X86_MMXEXT)
+@@ -447,7 +452,7 @@ static post_plugin_t *noise_open_plugin(
+ 
+     this->params[0].lineNoise = lineNoise_C;
+     this->params[0].lineNoiseAvg = lineNoiseAvg_C;
+-#ifdef ARCH_X86
++#if defined(ARCH_X86) && !defined(__sun)
+     if (xine_mm_accel() & MM_ACCEL_X86_MMX) {
+         this->params[0].lineNoise = lineNoise_MMX;
+         this->params[0].lineNoiseAvg = lineNoiseAvg_MMX;
