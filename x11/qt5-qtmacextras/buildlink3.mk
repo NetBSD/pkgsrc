d68b2a1a0089d016d941b5c5bfd2080f4cd7225a
adam <adam@pkgsrc.org>
2020-04-02 22:05:57 +0000

    qt5: updated to 5.14.2
    Qt 5.14.2:
    As usual this second patch release to Qt 5.14 series doesn't bring any new features but provide several bug fixes and other improvements. Compared to Qt 5.14.1 there are more than 200 bug fixes included in this release. For details of the most important changes please check the Changes files for Qt 5.14.2.

--- x11/qt5-qtmacextras/buildlink3.mk
+++ x11/qt5-qtmacextras/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.25 2020/03/10 22:09:13 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2020/04/02 22:05:58 adam Exp $
 
 BUILDLINK_TREE+=	qt5-qtmacextras
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	qt5-qtmacextras
 QT5_QTMACEXTRAS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.qt5-qtmacextras+=	qt5-qtmacextras>=5.9.1
-BUILDLINK_ABI_DEPENDS.qt5-qtmacextras+=	qt5-qtmacextras>=5.14.1nb2
+BUILDLINK_ABI_DEPENDS.qt5-qtmacextras+=	qt5-qtmacextras>=5.14.2
 BUILDLINK_PKGSRCDIR.qt5-qtmacextras?=	../../x11/qt5-qtmacextras
 
 BUILDLINK_INCDIRS.qt5-qtmacextras+=	qt5/include
