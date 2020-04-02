0da326535d30cd25e06d5cadfea926b629c3eb77
bsiegert <bsiegert@pkgsrc.org>
2020-04-02 11:38:49 +0000

    Update upower to 0.99.11.
    Version 0.99.11
    ~~~~~~~~~~~~~~~
    Released: 2019-09-03
    
    New Features:
     - Add code of conduct document
     - build: Migrate from intltool to gettext
     - rules: Split off HID++ udev rules
     - Harden systemd service
     - Let systemd create /var/lib/upower
     - Move D-Bus policy file to /usr/share/dbus-1/system.d/
    
    Bug fixes:
     - Fix endless loop burning 100% CPU on keyboard plugout with external
       backlight
     - linux: Start polling for unknown device batteries too
     - linux: Retry to get a battery type if it's unknown
     - linux: Don't treat device batteries like laptop batteries
     - Replace use of G_TYPE_INSTANCE_GET_PRIVATE and g_type_class_add_private()
    
    Version 0.99.10
    ~~~~~~~~~~~~~~~
    Released: 2019-02-20
    
    Bugfixes:
     - Set 'pending-charge' for DisplayDevice if at least one
       battery is in the 'pending-charge' state
     - Map pending-charge to fully-charged when charge is 100%
    
    Version 0.99.9
    ~~~~~~~~~~~~~~
    Released: 2018-10-25
    
    Bugfixes:
     - Fix lack of update after AC status changes, and broken keyboard
       backlight, following the daemon lockdown added in 0.99.8
     - Multiple API documentation fixes
     - Out-of-tree build fixes
    
    Version 0.99.8
    ~~~~~~~~~~~~~~
    Released: 2018-06-18
    
    New Features:
     - Lock down systemd service file
     - Add support for "Unknown" capacity level, and clarify handling
       of devices with coarse battery levels
     - Add a new version of up_client_get_devices() which unrefs contents
    
    Bugfixes:
     - Fix warnings when D-Bus related properties change
     - Prevent crash after attaching an Apple TV, and support newer
       versions of iOS
     - Lower severity of "unhandled action" messages
     - Fix battery status on MacBooks after a plug or unplug event
     - Fix double-close on exit
    
    Version 0.99.7
    ~~~~~~~~~~~~~~
    Released: 2017-11-28
    
    New Features:
     - Add support for Bluetooth LE device batteries (Bastien Nocera)
     - Allow to be replaced via --replace,-r (Christian Kellner)
    
    Bugfixes:
     - Fix critical action after resume from hibernate (Miroslav Sustek)
     - Fix compilation with libimobiledevice git (Bastien Nocera)
    
    Version 0.99.6
    ~~~~~~~~~~~~~~
    Released: 2017-09-11
    
    New Features:
     - Add UP_DEVICE_KIND_GAMING_INPUT for gaming devices (Bastien Nocera)
     - Detect joysticks as gaming input devices (Bastien Nocera)
    
    Bugfixes:
     - Correctly close inhibitor FD (Benjamin Berg)
     - Fix crash when '@' is present in the device name (oleid, Bastien Nocera)
     - Fix lid detection on FreeBSD (Alberto Villa)
     - Grab the model name from device if unavailable from battery (Bastien Nocera)
    
    Version 0.99.5
    ~~~~~~~~~~~~~~
    Released: 2017-07-24
    
    New Features:
     - Add a more complete self test for HID++ devices (Bastien Nocera)
     - Add BatteryLevel property for devices with a finite number of power levels (Bastien Nocera)
     - Add support for pausing and resuming of the daemon poll (Christian Kellner, Bastien Nocera)
     - Get a serial number for device batteries (Bastien Nocera)
     - Refresh devices after waking up from sleep (Christian Kellner)
    
    Bugfixes:
     - Add proper error and cancellable handling to UpClient constructor (Martin Pitt)
     - Do not spin in a loop when /proc/timer_stats cannot be written (Richard Hughes)
     - Exit early from up-tool when connecting to upower fails (Martin Pitt)
     - Expand the integration-tests to run in more environments (Bastien Nocera, Christian Kellner)
     - Fix reading and writing the keyboard brightness level (Hans de Goede, Marco Trevisan)
     - Fix -Wformat-y2k compilation errors (Bastien Nocera)
     - Lower initial power usage when iDevice isn't accessible (Bastien Nocera)
     - Simplify string checks in upower-glib (Bastien Nocera)

--- sysutils/upower/buildlink3.mk
+++ sysutils/upower/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/03/08 16:48:13 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/04/02 11:38:49 bsiegert Exp $
 
 BUILDLINK_TREE+=	upower
 
 .if !defined(UPOWER_BUILDLINK3_MK)
 UPOWER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.upower+=	upower>=0.99.2
-BUILDLINK_ABI_DEPENDS.upower?=	upower>=0.99.4nb5
+BUILDLINK_API_DEPENDS.upower+=	upower>=0.99.8
+BUILDLINK_ABI_DEPENDS.upower?=	upower>=0.99.11
 BUILDLINK_PKGSRCDIR.upower?=	../../sysutils/upower
 
 .include "../../devel/glib2/buildlink3.mk"
