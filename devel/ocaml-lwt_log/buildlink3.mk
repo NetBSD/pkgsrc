2460a267b0767cbd71bfa096317887712584dae7
jaapb <jaapb@pkgsrc.org>
2020-04-03 09:16:54 +0000

    Updated devel/ocaml-lwt_log to version 1.1.1.
    This upgrades the build system to dune, and fixes license headers.

--- devel/ocaml-lwt_log/buildlink3.mk
+++ devel/ocaml-lwt_log/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2019/05/30 10:26:03 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/04/03 09:16:54 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt_log
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-lwt_log
 OCAML_LWT_LOG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-lwt_log+=	ocaml-lwt_log>=1.1.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt_log?=	ocaml-lwt_log>=1.1.0nb5
+BUILDLINK_ABI_DEPENDS.ocaml-lwt_log?=	ocaml-lwt_log>=1.1.1
 BUILDLINK_PKGSRCDIR.ocaml-lwt_log?=	../../devel/ocaml-lwt_log
 
 .include "../../devel/ocaml-lwt/buildlink3.mk"
