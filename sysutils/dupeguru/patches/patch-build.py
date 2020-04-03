5c6326a179c5e189a6ac82d76db8c1df324557d5
wiz <wiz@pkgsrc.org>
2020-04-03 21:59:48 +0000

    dupeguru: update to 4.0.4rc1.
    Fixes build.
    
    Changes Since Last Release
    
        Update qt/platform.py to support other Unix style OSes (#444)
        Fix font size scaling issue in properties dialog [qt] (#504)
        Updates to support Python 3.7
        Fix issue with result window appearing partially off-screen [qt] (#521)
        Fix translation error for Simplified Chinese
        Updates to language files for German (#479)
        Fix error with multiple close calls to the progress window [qt] (#460, #449)
        Add Travis CI Builds
        Un-recurse methods get_files() and get_state() to improve stability (#421)
        Updates to language files for Italian (#445, #446, #447, #448)
        Fix issue with cache_shelve (#402, #439)
        Updated Windows packaging and builds (#438, #456, #461, #491, #474, #490, #565)
        Handle OS termination signals (#425)
        Make documentation installation optional
        Move cocoa UI to dupeguru-cocoa [cocoa]

--- sysutils/dupeguru/patches/patch-build.py
+++ sysutils/dupeguru/patches/patch-build.py
@@ -1,16 +1,16 @@
-$NetBSD: patch-build.py,v 1.1 2020/01/26 12:55:00 rhialto Exp $
+$NetBSD: patch-build.py,v 1.2 2020/04/03 21:59:48 wiz Exp $
 
 pyrcc5 is python-version-specific.
 
---- build.py.orig	2016-11-25 01:04:31.000000000 +0000
+--- build.py.orig	2019-05-14 01:43:47.000000000 +0000
 +++ build.py
-@@ -164,7 +164,8 @@ def build_qt(dev):
+@@ -125,7 +125,8 @@ def build_normal():
      print("Building localizations")
-     build_localizations('qt')
+     build_localizations()
      print("Building Qt stuff")
 -    print_and_do("pyrcc5 {0} > {1}".format(op.join('qt', 'dg.qrc'), op.join('qt', 'dg_rc.py')))
 +    pyrcc5 = "pyrcc5-" + os.environ["PYVERSSUFFIX"]
 +    print_and_do(pyrcc5 +" {0} > {1}".format(op.join('qt', 'dg.qrc'), op.join('qt', 'dg_rc.py')))
      fix_qt_resource_file(op.join('qt', 'dg_rc.py'))
      build_help()
-     print("Creating the run.py file")
+ 
