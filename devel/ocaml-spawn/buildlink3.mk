a9115925787106dce1fa5f1d103ebedc550a99dc
jaapb <jaapb@pkgsrc.org>
2020-04-03 10:56:09 +0000

    Updated devel/ocaml-spawn to version 0.13.0.
    This introduces a breaking change on Windows, and updates the build system
    to dune.

--- devel/ocaml-spawn/buildlink3.mk
+++ devel/ocaml-spawn/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/25 09:07:07 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/04/03 10:56:09 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-spawn
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-spawn
 OCAML_SPAWN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-spawn+=	ocaml-spawn>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-spawn+=	ocaml-spawn>=0.13.0
 BUILDLINK_PKGSRCDIR.ocaml-spawn?=	../../devel/ocaml-spawn
 
 .endif	# OCAML_SPAWN_BUILDLINK3_MK
