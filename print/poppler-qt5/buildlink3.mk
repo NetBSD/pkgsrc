307b1027c51b309f723cea9a3142544b29127483
wiz <wiz@pkgsrc.org>
2020-04-03 10:30:30 +0000

    *: recursive bump for poppler-0.87

--- print/poppler-qt5/buildlink3.mk
+++ print/poppler-qt5/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.35 2020/03/04 22:07:24 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.36 2020/04/03 10:30:31 wiz Exp $
 
 BUILDLINK_TREE+=	poppler-qt5
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	poppler-qt5
 POPPLER_QT5_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.poppler-qt5+=	poppler-qt5>=0.26.0
-BUILDLINK_ABI_DEPENDS.poppler-qt5+=	poppler-qt5>=0.86.1
+BUILDLINK_ABI_DEPENDS.poppler-qt5+=	poppler-qt5>=0.87.0
 BUILDLINK_PKGSRCDIR.poppler-qt5?=	../../print/poppler-qt5
 
 .include "../../print/poppler/buildlink3.mk"
