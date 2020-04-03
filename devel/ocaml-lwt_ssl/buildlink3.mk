8a776af1fff1b66bc2075fa4e126fb22e9fb3b8a
jaapb <jaapb@pkgsrc.org>
2020-04-03 14:36:44 +0000

    Update devel/ocaml-lwt_ssl to version 1.1.3.
    This switches the build system from jbuilder to dune.

--- devel/ocaml-lwt_ssl/buildlink3.mk
+++ devel/ocaml-lwt_ssl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2020/01/18 21:48:09 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2020/04/03 14:36:44 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt_ssl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-lwt_ssl
 OCAML_LWT_SSL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-lwt_ssl+=	ocaml-lwt_ssl>=1.1.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt_ssl?=	ocaml-lwt_ssl>=1.1.2nb5
+BUILDLINK_ABI_DEPENDS.ocaml-lwt_ssl?=	ocaml-lwt_ssl>=1.1.3
 BUILDLINK_PKGSRCDIR.ocaml-lwt_ssl?=	../../devel/ocaml-lwt_ssl
 
 .include "../../devel/ocaml-lwt/buildlink3.mk"
