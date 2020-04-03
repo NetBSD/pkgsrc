76b9476033f5fd7e7eadfd9109d0decf20788a10
jaapb <jaapb@pkgsrc.org>
2020-04-03 09:14:57 +0000

    Updated devel/ocaml-ppx_tools_versioned to version 5.3.0.
    This adds support for OCaml 4.10.

--- devel/ocaml-ppx_tools_versioned/buildlink3.mk
+++ devel/ocaml-ppx_tools_versioned/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2019/07/31 09:15:50 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2020/04/03 09:14:57 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_tools_versioned
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_tools_versioned
 OCAML_PPX_TOOLS_VERSIONED_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_tools_versioned+=	ocaml-ppx_tools_versioned>=5.0.1
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_tools_versioned+=	ocaml-ppx_tools_versioned>=5.2.
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_tools_versioned+=	ocaml-ppx_tools_versioned>=5.3.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_tools_versioned?=		../../devel/ocaml-ppx_tools_versioned
 .endif	# OCAML_PPX_TOOLS_VERSIONED_BUILDLINK3_MK
 
