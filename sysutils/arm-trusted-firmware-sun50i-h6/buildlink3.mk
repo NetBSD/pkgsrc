8eb6867122f675d13d0638c238590408b7fd8caa
thorpej <thorpej@pkgsrc.org>
2020-04-04 15:12:17 +0000

    Use R_WDOG to reset H6 SoCs; some of them have non-working WDOG blocks.

--- sysutils/arm-trusted-firmware-sun50i-h6/buildlink3.mk
+++ sysutils/arm-trusted-firmware-sun50i-h6/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/05/26 17:51:33 jmcneill Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/04/04 15:12:17 thorpej Exp $
 
 BUILDLINK_DEPMETHOD.arm-trusted-firmware-sun50i-h6?=	build
 
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	arm-trusted-firmware-sun50i-h6
 .if !defined(ARM_TRUSTED_FIRMWARE_SUN50I_H6_BUILDLINK3_MK)
 ARM_TRUSTED_FIRMWARE_SUN50I_H6_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.arm-trusted-firmware-sun50i-h6+=	arm-trusted-firmware-sun50i-h6>=20180207
+BUILDLINK_API_DEPENDS.arm-trusted-firmware-sun50i-h6+=	arm-trusted-firmware-sun50i-h6>=20180207nb1
 BUILDLINK_PKGSRCDIR.arm-trusted-firmware-sun50i-h6?=	../../sysutils/arm-trusted-firmware-sun50i-h6
 .endif	# ARM_TRUSTED_FIRMWARE_SUN50I_H6_BUILDLINK3_MK
 
