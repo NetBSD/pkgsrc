044974ac71ecbc64cb4bbb60b95aed3fd9f9f232
bsiegert <bsiegert@pkgsrc.org>
2020-04-05 16:05:39 +0000

    Update all MATE packages (except system-monitor) to 1.24.0.
    This release contains plenty of new features, bug-fixes, and general
    improvements. Some of the most important highlights include:
    
      * We did it again, the MATE desktop environment is easier to use than before,
        once the user starts the session. Do you want to hide applications startup?
        Now you can set which applications to show on startup.
      * Engrampa now has support for a handful of extra formats, as well as fixed
        support for passwords and unicode characters in some of them.
      * Eye of MATE now has support for Wayland and we’ve added support for
        embedded color profiles.
          * The thumbnail generation has been reworked and fixed in several places.
          * Added support for webp files.
      * Our window manager, marco, has gotten quite a few changes:
          * We’ve brought a bunch of window decorations from the past to feed
            your nostalgia.
          * Finally added invisible resize borders. No more struggling to find a
            border to grab with your mouse!
          * All window controls (you know, the min, max, close buttons) are now
            rendered in HiDPI.
          * The Alt+Tab and Workspace Switcher popups have been entirely reworked.
            Now they render in beautiful OSD style, are more configurable, and can
            respond to keyboard arrows.
          * Tiling windows with the keyboard now allows you to cycle through
            different window sizes. You no longer need to feel constrained by only
            half of your screen.
      * The System Monitor panel applet now has support for NVMe drives.
      * Calculator now supports using either “pi” or “π”.
          * Scientific notation has been improved.
          * Some fixes for supporting pre-defined physical constants.
      * The Control Center now displays its icons correctly on HiDPI displays.
      * A brand new Time And Date Manager app has been added.
      * The Mouse app now supports acceleration profiles.
      * The Preferred Applications app has been improved for accessibility, as well
        as better support for integration with IM clients.
      * The Indicator Applet has slightly better interaction with
        oddly-sized icons.
      * Speaking of icons, the network manager applet icons in our own themes have
        been entirely redesigned and can now be enjoyed on HiDPI displays.
      * If you’re the type of person that does not like to be disturbed when busy,
        or giving a presentation, or watching a movie, you’ll be happy to know that
        the notification daemon now supports a Do-Not-Disturb mode.
      * The MATE Panel had several bugs that caused crashes in the past when
        changing layouts. Those are now fixed!
          * Support for Wayland compatibility has improved considerably.
          * Status icons (a.k.a. notification area, or system tray) have support
            for HiDPI displays.
          * Wanda the Fish got a make-over and now you can enjoy her in full
            HiDPI glory.
          * The window list applet now supports window thumbnails on hover.
          * Various accessibility improvements throughout the panel and its
            core applets.
      * If your system doesn’t, uh, support systemd you might be interested in
        knowing that we’ve added support for elogind to both the MATE Screensaver
        and the MATE Session.
      * We’ve also added a brand new MATE Disk Image Mounter utility.
      * Mozo, the menu editor, now supports Undo and Redo actions.
      * Pluma plugins have now fully switched to Python 3.
      * Pluma no longer has to envy anything from other complex editors, since it
        can now show the formatting marks.
      * i18n: All applications have been migrated from intltools to gettext.

--- x11/mate-settings-daemon/buildlink3.mk
+++ x11/mate-settings-daemon/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2020/03/08 16:48:27 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/04/05 16:05:44 bsiegert Exp $
 #
 
 BUILDLINK_TREE+=	mate-settings-daemon
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	mate-settings-daemon
 .if !defined(MATE_SETTINGS_DAEMON_BUILDLINK3_MK)
 MATE_SETTINGS_DAEMON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mate-settings-daemon+=	mate-settings-daemon>=1.8.1
-BUILDLINK_ABI_DEPENDS.mate-settings-daemon+=	mate-settings-daemon>=1.22.1nb1
+BUILDLINK_API_DEPENDS.mate-settings-daemon+=	mate-settings-daemon>=1.24
+BUILDLINK_ABI_DEPENDS.mate-settings-daemon+=	mate-settings-daemon>=1.24
 BUILDLINK_PKGSRCDIR.mate-settings-daemon?=	../../x11/mate-settings-daemon
 
 .include "../../x11/libmatekbd/buildlink3.mk"
