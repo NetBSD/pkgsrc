0144c164557f5c2e905377ef0ff055c7b42d8bea
wiz <wiz@pkgsrc.org>
2020-04-06 05:33:02 +0000

    mozjs60: follow packaging recommendations
    Disable nspr.
    
    See https://github.com/mozilla-spidermonkey/spidermonkey-embedding-examples/blob/esr60/docs/Building%20SpiderMonkey.md
    
    Bump PKGREVISION.

--- lang/mozjs60/buildlink3.mk
+++ lang/mozjs60/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/04/05 06:10:28 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/04/06 05:33:02 wiz Exp $
 
 BUILDLINK_TREE+=	mozjs60
 
@@ -6,11 +6,11 @@ BUILDLINK_TREE+=	mozjs60
 MOZJS60_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mozjs60+=	mozjs60>=60.8.0
+BUILDLINK_ABI_DEPENDS.mozjs60+=	mozjs60>=60.8.0nb1
 BUILDLINK_PKGSRCDIR.mozjs60?=	../../lang/mozjs60
 
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../textproc/icu/buildlink3.mk"
-.include "../../devel/nspr/buildlink3.mk"
 .endif	# MOZJS60_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mozjs60
