ae79822e7eefe3b2f847aeb1a270665e5c834ea5
wiz <wiz@pkgsrc.org>
2020-04-06 09:11:13 +0000

    libsigc++3: update to 3.0.3.
    3.0.3 (stable)
    
    Build:
    * docs/docs/reference/meson.build: Check if perl is found
      (Kjell Ahlstedt) Issue #53 (Rasmus Thomsen)
    * README.md: Describe building with Meson
      (Kjell Ahlstedt)
    
    Examples:
    * Add example of using libsigc++ with Qt
      (Robert Middleton) Pull request #35
    
    Tests:
    * Replace C-style casts with static_cast<>
      (Lennard Berger)
    
    
    3.0.2 (stable)
    
    * Build: Meson build improvements.
      (Kjell Ahlstedt, Chun-Wei Fan)
    
    3.0.1 (stable)
    
    * Build:
      - Add meson build, alongside the existing autotools and CMake builds.
      (Kjell Ahlstedt, Chun-Wei Fan)
      - CMake: Fix the MSVC build, enable warnings, and disable unnecessary
      warnings, with MVSC.
      (Stuart Dootson)
    * connection: Allow copying of empty connections.
      (Aigrind)
    * sigc++/adaptors/bind.h: Make bind_functor::bound_ public.
      (Used by sigc::visitor::do_visit_each().
      (Kjell Ahlstedt) Bug #26
    * test_limit_reference: Avoid virtual-move-assign warning
      (seen with g++ 9.2)
      (Murray Cumming)
    * signal: slot_iterator_buf: Remove unused T_result default type
      (Murray Cumming)
    * tests: Slight cleanups
      (Murray Cumming)
    * Remove unused #ifdefed code.
      (Murray Cumming)

--- devel/libsigc++3/buildlink3.mk
+++ devel/libsigc++3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/11/07 13:17:42 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/04/06 09:11:13 wiz Exp $
 
 BUILDLINK_TREE+=	libsigcpp3
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libsigcpp3
 LIBSIGCPP3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libsigcpp3+=	libsigc++3>=3.0.0
-BUILDLINK_PKGSRCDIR.libsigcpp3?=		../../devel/libsigc++3
+BUILDLINK_PKGSRCDIR.libsigcpp3?=	../../devel/libsigc++3
 
 # needs C++11
 GCC_REQD+=	4.8
