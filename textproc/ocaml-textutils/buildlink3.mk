00565248e1bffc9aad9f25c232793afed069add1
jaapb <jaapb@pkgsrc.org>
2020-04-08 11:41:44 +0000

    Updated textproc/ocaml-textutils to version 0.13.0.
    The CHANGES.md file hasn't been updated, but changes seem minor.

--- textproc/ocaml-textutils/buildlink3.mk
+++ textproc/ocaml-textutils/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/19 10:56:03 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/04/08 11:41:44 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-textutils
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-textutils
 OCAML_TEXTUTILS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-textutils+=	ocaml-textutils>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-textutils+=	ocaml-textutils>=0.13.0
 BUILDLINK_PKGSRCDIR.ocaml-textutils?=	../../textproc/ocaml-textutils
 
 .endif	# OCAML_TEXTUTILS_BUILDLINK3_MK
