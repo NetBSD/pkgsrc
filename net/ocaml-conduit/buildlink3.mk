c249d416941af065bbdb9bce7cb04f7336ef3ad7
jaapb <jaapb@pkgsrc.org>
2020-04-08 12:46:24 +0000

    Updated net/ocaml-conduit to version 2.1.0.
    This introduces a fairly extensive list of changes, some of which break
    compatibility. See CHANGES.md for more details.

--- net/ocaml-conduit/buildlink3.mk
+++ net/ocaml-conduit/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/07/01 16:01:36 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/04/08 12:46:24 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-conduit
 
 .if !defined(OCAML_CONDUIT_BUILDLINK3_MK)
 OCAML_CONDUIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-conduit+=	ocaml-conduit>=1.4.0
+BUILDLINK_API_DEPENDS.ocaml-conduit+=	ocaml-conduit>=2.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-conduit+=	ocaml-conduit>=2.1.0
 BUILDLINK_PKGSRCDIR.ocaml-conduit?=	../../net/ocaml-conduit
 
 .endif	# OCAML_CONDUIT_BUILDLINK3_MK
