c1106d538a9526247dc9e4854ed211d74460ecce
jaapb <jaapb@pkgsrc.org>
2020-04-08 09:34:28 +0000

    Updated net/ocaml-protocol_version_header to version 0.13.0.
    There is no CHANGES file, but changes seem minor.

--- net/ocaml-protocol_version_header/buildlink3.mk
+++ net/ocaml-protocol_version_header/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/25 09:19:32 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/04/08 09:34:28 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-protocol_version_header
 
@@ -6,6 +6,8 @@ BUILDLINK_TREE+=	ocaml-protocol_version_header
 OCAML_PROTOCOL_VERSION_HEADER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-protocol_version_header+=	ocaml-protocol_version_header>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-protocol_version_header+=	ocaml-protocol_version_header>=0.13.0
+
 BUILDLINK_PKGSRCDIR.ocaml-protocol_version_header?=	../../net/ocaml-protocol_version_header
 
 .endif	# OCAML_PROTOCOL_VERSION_HEADER_BUILDLINK3_MK
