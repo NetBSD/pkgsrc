18c1c621522f54114ee48c3a56c501dc6733d193
jaapb <jaapb@pkgsrc.org>
2020-04-08 08:56:37 +0000

    Updated devel/ocaml-ppx_tools to version 6.0.
    This switches the build system to dune.

--- devel/ocaml-ppx_tools/buildlink3.mk
+++ devel/ocaml-ppx_tools/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2019/07/31 09:13:50 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/04/08 08:56:37 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_tools
 
 .if !defined(OCAML_PPX_TOOLS_BUILDLINK3_MK)
 OCAML_PPX_TOOLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ppx_tools+=	ocaml-ppx_tools>=0.99.3
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_tools+=	ocaml-ppx_tools>=5.3
+BUILDLINK_API_DEPENDS.ocaml-ppx_tools+=	ocaml-ppx_tools>=6.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_tools+=	ocaml-ppx_tools>=6.0
 BUILDLINK_PKGSRCDIR.ocaml-ppx_tools?=	../../devel/ocaml-ppx_tools
 .endif	# OCAML_PPX_TOOLS_BUILDLINK3_MK
 
