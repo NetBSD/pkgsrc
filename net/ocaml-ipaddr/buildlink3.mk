eb58747d2a6535a5ca30750c590efd4f61cc8305
jaapb <jaapb@pkgsrc.org>
2020-04-08 12:31:01 +0000

    Updated net/ocaml-ipaddr to version 4.0.0.
    Apart from some minor changes, this version adds separate packages that
    use the sexp and cstruct libraries.

--- net/ocaml-ipaddr/buildlink3.mk
+++ net/ocaml-ipaddr/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.18 2019/03/05 18:53:15 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2020/04/08 12:31:01 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ipaddr
 
 .if !defined(OCAML_IPADDR_BUILDLINK3_MK)
 OCAML_IPADDR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-ipaddr+=	ocaml-ipaddr>=3.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-ipaddr?=	ocaml-ipaddr>=3.1.0
+BUILDLINK_API_DEPENDS.ocaml-ipaddr+=	ocaml-ipaddr>=4.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-ipaddr?=	ocaml-ipaddr>=4.0.0
 BUILDLINK_PKGSRCDIR.ocaml-ipaddr?=	../../net/ocaml-ipaddr
 BUILDLINK_FILES.ocaml-ipaddr+=		lib/ocaml/site-lib/*
 
