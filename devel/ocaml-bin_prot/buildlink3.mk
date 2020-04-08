3a8912adbafcc79c6b8dd8d28b0beafbe62baf0a
jaapb <jaapb@pkgsrc.org>
2020-04-08 09:19:42 +0000

    Updated devel/ocaml-bin_prot to version 0.13.0.
    The CHANGES.md file does not seem to have been updated, but it looks like
    changes are minor.

--- devel/ocaml-bin_prot/buildlink3.mk
+++ devel/ocaml-bin_prot/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/03/13 11:06:00 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/04/08 09:19:42 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-bin_prot
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-bin_prot
 OCAML_BIN_PROT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-bin_prot+=	ocaml-bin_prot>=0.12.0
+BUILDLINK_ABI_DEPENDS.ocaml-bin_prot+=	ocaml-bin_prot>=0.13.0
 BUILDLINK_PKGSRCDIR.ocaml-bin_prot?=	../../devel/ocaml-bin_prot
 
 .endif	# OCAML_BIN_PROT_BUILDLINK3_MK
