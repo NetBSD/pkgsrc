e7cd702d81bce4db738ac071fdaac4d66330afe9
khorben <khorben@pkgsrc.org>
2020-04-11 03:57:10 +0000

    deforaos-libdatabase: update to version 0.0.2
    Changes since the previous release:
    - additional documentation
    - updated API (backends, prepared statements)
    - new generic database(1) tool

--- databases/deforaos-libdatabase/buildlink3.mk
+++ databases/deforaos-libdatabase/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/08/31 22:24:05 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/04/11 03:57:10 khorben Exp $
 
 BUILDLINK_TREE+=	deforaos-libdatabase
 
 .if !defined(DEFORAOS_LIBDATABASE_BUILDLINK3_MK)
 DEFORAOS_LIBDATABASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-libdatabase+=	deforaos-libdatabase>=0.0.1
+BUILDLINK_API_DEPENDS.deforaos-libdatabase+=	deforaos-libdatabase>=0.0.2
 BUILDLINK_PKGSRCDIR.deforaos-libdatabase?=	../../databases/deforaos-libdatabase
 
 .include "../../devel/deforaos-libsystem/buildlink3.mk"
