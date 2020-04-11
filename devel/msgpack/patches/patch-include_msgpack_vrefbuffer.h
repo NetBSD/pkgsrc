d69c4dc18a0474da5c7f2cebdaf1373cbb5b1dc3
adam <adam@pkgsrc.org>
2020-04-11 13:39:29 +0000

    msgpack: updated to 3.2.1
    version 3.2.1
    * Fix snprintf return value checking
    * Remove some warnings
    * Fix fbuffer result checking
    * Fix temporary object handling
    * Improve cmake support
    * Fix invalid `int main` parameter
    * Improve supporting platform
    * Fix ZLIB error handling
    * Remove unused variable
    * Improve integer overflow checking
    
    version 3.2.0
    * Fix invalid include
    * Add timespec support
    * Fix unchecked fnprintf on C
    * Improve integer overflow checking on C
    * Fix warnings on `-Wconversion`
    * Fix invalid passed by value on aligned_zone_size_visitor
    * Improve windows support
    * Fix msgpack::object size caluclation error
    * Fix memory error on example code
    * Fix redundant memory allocation on C
    * Fix msgpack::type::tuple base class conversion

--- devel/msgpack/patches/patch-include_msgpack_vrefbuffer.h
+++ devel/msgpack/patches/patch-include_msgpack_vrefbuffer.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-include_msgpack_vrefbuffer.h,v 1.2 2018/06/01 07:55:43 adam Exp $
+$NetBSD: patch-include_msgpack_vrefbuffer.h,v 1.3 2020/04/11 13:39:29 adam Exp $
 
 FreeBSD fix.
 
---- include/msgpack/vrefbuffer.h.orig	2018-04-09 13:56:00.000000000 +0000
+--- include/msgpack/vrefbuffer.h.orig	2019-12-10 03:33:32.000000000 +0000
 +++ include/msgpack/vrefbuffer.h
 @@ -13,7 +13,7 @@
  #include "zone.h"
  #include <stdlib.h>
  
--#if defined(unix) || defined(__unix) || defined(__APPLE__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__QNX__) || defined(__QNXTO__)
-+#if defined(unix) || defined(__unix) || defined(__APPLE__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__QNX__) || defined(__QNXTO__) || defined(__FreeBSD__)
+-#if defined(unix) || defined(__unix) || defined(__linux__) || defined(__APPLE__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__QNX__) || defined(__QNXTO__) || defined(__HAIKU__)
++#if defined(unix) || defined(__unix) || defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__QNX__) || defined(__QNXTO__) || defined(__HAIKU__)
  #include <sys/uio.h>
  #else
  struct iovec {
