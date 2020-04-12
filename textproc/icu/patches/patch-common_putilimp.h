52552027c9a367ecc3382e756fed1d237e7878b1
adam <adam@pkgsrc.org>
2020-04-12 07:06:43 +0000

    icu: updated to 66.1
    ICU 66
    It updates to Unicode 13 & CLDR 36.1. New, extra Q1 releases for low-risk integration of Unicode 13.
    
    ICU 65
    It updates to CLDR 36 locale data with many additions and corrections, and some new measurement units. The Java LocaleMatcher API is improved, and ported to C++. For building ICU data, there are new filtering options, and new tracing support for data loading in ICU4C.

--- textproc/icu/patches/patch-common_putilimp.h
+++ textproc/icu/patches/patch-common_putilimp.h
@@ -1,8 +1,8 @@
-$NetBSD: patch-common_putilimp.h,v 1.4 2013/11/05 15:25:37 ryoon Exp $
+$NetBSD: patch-common_putilimp.h,v 1.5 2020/04/12 07:06:43 adam Exp $
 
-* GCC for arm does not have __sync_add_and_fetch() etc.
+MirBSD support.
 
---- common/putilimp.h.orig	2013-10-04 20:49:30.000000000 +0000
+--- common/putilimp.h.orig	2020-03-11 18:16:11.000000000 +0000
 +++ common/putilimp.h
 @@ -128,6 +128,8 @@ typedef size_t uintptr_t;
     /* not defined */
@@ -13,12 +13,3 @@ $NetBSD: patch-common_putilimp.h,v 1.4 2013/11/05 15:25:37 ryoon Exp $
  #else
  #   define U_TIMEZONE timezone
  #endif
-@@ -187,7 +189,7 @@ typedef size_t uintptr_t;
-     /* Use the predefined value. */
- #elif U_PLATFORM == U_PF_MINGW
-     #define U_HAVE_GCC_ATOMICS 0
--#elif U_GCC_MAJOR_MINOR >= 404 || defined(__clang__)
-+#elif (U_GCC_MAJOR_MINOR >= 404 && !defined(__arm__)) || defined(__clang__)
-     /* TODO: Intel icc and IBM xlc on AIX also support gcc atomics.  (Intel originated them.)
-      *       Add them for these compilers.
-      * Note: Clang sets __GNUC__ defines for version 4.2, so misses the 4.4 test here.
