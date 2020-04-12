8bac2655e7bc6efdf6c9415d38218a54bee80828
adam <adam@pkgsrc.org>
2020-04-12 07:21:04 +0000

    libxml2: add icu option

--- textproc/libxml2/buildlink3.mk
+++ textproc/libxml2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2012/06/14 07:39:37 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2020/04/12 07:21:04 adam Exp $
 
 BUILDLINK_TREE+=	libxml2
 
@@ -12,6 +12,12 @@ BUILDLINK_PKGSRCDIR.libxml2?=	../../textproc/libxml2
 BUILDLINK_FILES.libxml2+=	bin/xml2-config
 
 USE_FEATURES+=		glob
+
+pkgbase := libxml2
+.include "../../mk/pkg-build-options.mk"
+.if ${PKG_BUILD_OPTIONS.libxml2:Micu}
+.include "../../textproc/icu/buildlink3.mk"
+.endif
 
 .include "../../archivers/xz/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
