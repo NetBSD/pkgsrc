4083d12325372a2d40e5d5343eeb2c1198e4b79e
adam <adam@pkgsrc.org>
2020-04-12 07:21:04 +0000

    libxml2: add icu option

--- textproc/libxml2/options.mk
+++ textproc/libxml2/options.mk
@@ -1,10 +1,17 @@
-# $NetBSD: options.mk,v 1.2 2012/06/12 15:46:03 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2020/04/12 07:21:04 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libxml2
-PKG_SUPPORTED_OPTIONS+=	inet6
+PKG_SUPPORTED_OPTIONS+=	icu inet6
 PKG_SUGGESTED_OPTIONS=	inet6
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Micu)
+.include "../../textproc/icu/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-icu
+.else
+CONFIGURE_ARGS+=	--without-icu
+.endif
 
 .if !empty(PKG_OPTIONS:Minet6)
 CONFIGURE_ARGS+=	--enable-ipv6
