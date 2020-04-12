368f95dff1d26d2162a4fab2d221cacf89c249e6
ryoon <ryoon@pkgsrc.org>
2020-04-12 07:50:24 +0000

    qpdf: Update to 10.0.1
    * Bump forgotten BUILDLINK_API_DEPENDS.
    
    Changelog:
    This is qpdf version 10.0.1. This release includes some small bug
    fixes over 10.0.0 including a fix to one regression in
    QPDFObjectHandle::getStreamData. If you use that method in your
    code, you should definitely take 10.0.1 over 10.0.0.

--- print/qpdf/buildlink3.mk
+++ print/qpdf/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2019/09/15 00:11:29 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/04/12 07:50:24 ryoon Exp $
 
 BUILDLINK_TREE+=	qpdf
 
 .if !defined(QPDF_BUILDLINK3_MK)
 QPDF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qpdf+=	qpdf>=9.0.0
+BUILDLINK_API_DEPENDS.qpdf+=	qpdf>=10.0.0
 BUILDLINK_PKGSRCDIR.qpdf?=	../../print/qpdf
 
 .endif	# QPDF_BUILDLINK3_MK
