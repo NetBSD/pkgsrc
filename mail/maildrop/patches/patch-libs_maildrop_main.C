1fbffe671b924f9f6b9fa89248b842fc5f5de6b9
manu <manu@pkgsrc.org>
2020-04-16 12:49:34 +0000

    Build fix for auth_mkhomedir() that is missing from courrier-authlib

--- mail/maildrop/patches/patch-libs_maildrop_main.C
+++ mail/maildrop/patches/patch-libs_maildrop_main.C
@@ -1,10 +1,27 @@
-$NetBSD: patch-libs_maildrop_main.C,v 1.1 2019/05/24 13:17:58 hauke Exp $
+$NetBSD: patch-libs_maildrop_main.C,v 1.2 2020/04/16 12:49:35 manu Exp $
 
 Make this maildrop install usable by both Courier and other MTAs.
 
---- libs/maildrop/main.C.orig	2018-09-13 23:22:17.000000000 +0000
-+++ libs/maildrop/main.C
-@@ -754,7 +754,7 @@ uid_t	my_u=getuid();
+--- libs/maildrop/main.C.orig	2018-09-14 01:22:17.000000000 +0200
++++ libs/maildrop/main.C	2020-04-10 03:32:11.795122838 +0200
+@@ -278,13 +278,15 @@
+ 			    void *void_arg)
+ {
+ 	Maildrop &maildrop=*(Maildrop *)void_arg;
+ 
++#if 0 /* auth_mkhomedir does not exist in courrier-authlib */
+ 	if (auth_mkhomedir(auth))
+ 	{
+ 		perror(auth->homedir);
+ 		exit(1);
+ 	}
++#endif
+ 
+ 	if (VerboseLevel() > 1)
+ 	{
+ 		Buffer b;
+@@ -753,9 +755,9 @@
+ 
  	if (!found)
  	{
  #if HAVE_COURIER
@@ -13,7 +30,9 @@ Make this maildrop install usable by both Courier and other MTAs.
  #endif
  		{
  			my_pw=getpwuid(my_u);
-@@ -814,6 +814,14 @@ Buffer	value;
+ 			if (!my_pw)
+@@ -813,8 +815,16 @@
+ 	if (deliverymode && orig_uid == getuid())
  	{
  	const char *p;
  
@@ -28,7 +47,9 @@ Make this maildrop install usable by both Courier and other MTAs.
  		if ((p=getenv("HOME")) && *p)
  			maildrop.init_home=p;
  
-@@ -838,6 +846,8 @@ Buffer	value;
+ 		if ((p=getenv("LOGNAME")) && *p)
+@@ -837,8 +847,10 @@
+ 		maildrop.init_default=p;
  
  		if ((p=getenv("MAILDIRQUOTA")) && *p)
  			maildrop.init_quota=p;
@@ -37,3 +58,4 @@ Make this maildrop install usable by both Courier and other MTAs.
  	}
  #endif
  
+ 	if (deliverymode)
