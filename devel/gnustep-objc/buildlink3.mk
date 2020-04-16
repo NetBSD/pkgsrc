65b1740a9bbab1a9f8b174de4288adba1e7e8d6a
manu <manu@pkgsrc.org>
2020-04-16 09:28:39 +0000

    Update gnustep-objc to 1.8.1
    This is required to fix the build of gnustep-make on NetBSD 9.0
    
    Changes since previous version in pkgsrc:
    
    Version 1.8
    
    - Added API for tracing, allowing interposition on all message sends matching a
      given selector.
    
    - Numerous bug fixes and stability improvements.
    
    Version 1.7
    
    - A new CMake-based build system.  This makes all of the configurable options
      available via a clean interface.  CPack is supported for building RPM and DEB
      packages out of the box.
    
    - A new CTest-based test suite, replacing the old ad-hoc tests.
    
    - Build a single libobjc with support for Objective-C++ on platforms where a
      C++ ABI library (libcxxrt or libsupc++) is installed as a shared library.
    
    - Added specialised property accessor functions and support for atomic
      properties with C++ non-POD types.
    
    - Significant improvements in property introspection and an exhaustive test
      suite.
    
    - Improved integration with libdispatch.  The runtime will correctly register
      work queues with the garbage collector or create autorelease pools around
      block invocations.
    
    - A new exception implementation providing better integration with foreign
      exceptions (e.g. C++ exceptions).  The new ABI is supported by clang 3.3 when
      compiling with -fobjc-runtime=gnustep-1.7 (or higher).  The old ABI is still
      supported and both can be used within the same program, however code compiled
      with the old ABI remains unreliable in the presence of foreign exceptions.
      It is strongly recommended that anyone using exceptions with Objective-C++
      switches to the new version.
    
    - MIPS64 support in the assembly routines.  Currently these are only tested
      with the n64 ABI.  They are believed to work with n32 and o32, but should be
      considered unsupported on these platforms.
    
    - Small algorithmic improvement to the objc_msgSend() implementation, giving
      approximately a 10% speedup (architecture-dependent) on message sends.
    
    - Updated optimisation passes to work with LLVM 3.2 and recent LLVM trunk.

--- devel/gnustep-objc/buildlink3.mk
+++ devel/gnustep-objc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.11 2017/12/31 11:20:03 he Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2020/04/16 09:28:39 manu Exp $
 
 BUILDLINK_TREE+=	gnustep-objc
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.gnustep-objc+=	gnustep-objc>=1.3.0
 BUILDLINK_ABI_DEPENDS.gnustep-objc+=	gnustep-objc>=1.3.0nb1
 BUILDLINK_PKGSRCDIR.gnustep-objc?=	../../devel/gnustep-objc
 
+BUILDLINK_CFLAGS.gnustep-objc+=	-I${BUILDLINK_PREFIX.gnustep-objc}/include/objc
 BUILDLINK_LDFLAGS.gnustep-objc+=	-lobjc -lpthread
 .if ${OPSYS} == "NetBSD"
 # Missing _Unwind_* symbols in gnustep objc.so, needs to come from somewhere...
