3a81bb07a24eb2200c6863300e0bead78ebd34e8
ryoon <ryoon@pkgsrc.org>
2020-04-16 11:51:26 +0000

    openjdk11: Update to 1.11.0.7.10
    Changelog:
    Updated BSD port of JDK 11
    
    Additional features include:
    
        Update to 11.0.7 GA
        Fixes for NetBSD (build, swing character input)
    
    
    Securiy fixes:
    CVE-2020-2803
    CVE-2020-2805
    CVE-2019-18197
    CVE-2020-2816
    CVE-2020-2781
    CVE-2020-2830
    CVE-2020-2767
    CVE-2020-2800
    CVE-2020-2778
    CVE-2020-2764
    CVE-2020-2754
    CVE-2020-2755
    CVE-2020-2773
    CVE-2020-2756
    CVE-2020-2757

--- lang/openjdk11/patches/patch-src_hotspot_os_bsd_os__bsd.cpp
+++ lang/openjdk11/patches/patch-src_hotspot_os_bsd_os__bsd.cpp
@@ -1,7 +1,4 @@
-$NetBSD: patch-src_hotspot_os_bsd_os__bsd.cpp,v 1.3 2020/02/11 02:30:42 ryoon Exp $
-
-sigaction part: https://github.com/battleblow/openjdk-jdk11u/pull/97
-Last chunk: https://github.com/battleblow/openjdk-jdk11u/pull/96
+$NetBSD: patch-src_hotspot_os_bsd_os__bsd.cpp,v 1.4 2020/04/16 11:51:26 ryoon Exp $
 
 --- src/hotspot/os/bsd/os_bsd.cpp.orig	2020-01-15 18:29:57.000000000 +0000
 +++ src/hotspot/os/bsd/os_bsd.cpp
@@ -14,24 +11,3 @@ Last chunk: https://github.com/battleblow/openjdk-jdk11u/pull/96
  #else
    #define DEFAULT_LIBPATH "/usr/lib:/usr/local/lib"
  #endif
-@@ -3377,7 +3377,11 @@ void os::Bsd::check_signal_handler(int s
-   struct sigaction act;
-   if (os_sigaction == NULL) {
-     // only trust the default sigaction, in case it has been interposed
-+#if defined(__NetBSD__)
-+    os_sigaction = (os_sigaction_t)dlsym(RTLD_DEFAULT, "__sigaction14");
-+#else
-     os_sigaction = (os_sigaction_t)dlsym(RTLD_DEFAULT, "sigaction");
-+#endif
-     if (os_sigaction == NULL) return;
-   }
- 
-@@ -3632,7 +3636,7 @@ void os::set_native_thread_name(const ch
- #elif defined(__FreeBSD__) || defined(__OpenBSD__)
-     pthread_set_name_np(pthread_self(), name);
- #elif defined(__NetBSD__)
--    pthread_setname_np(pthread_self(), "%s", name);
-+    pthread_setname_np(pthread_self(), "%s", const_cast<char *>(name));
- #endif
-   }
- }
