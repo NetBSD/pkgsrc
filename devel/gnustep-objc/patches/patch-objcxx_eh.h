26dc4d455a47374c7c8c697342582b77039be114
joerg <joerg@pkgsrc.org>
2020-04-17 13:32:23 +0000

    Use <typeinfo> in all cases

--- devel/gnustep-objc/patches/patch-objcxx_eh.h
+++ devel/gnustep-objc/patches/patch-objcxx_eh.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-objcxx_eh.h,v 1.1 2020/04/16 09:28:39 manu Exp $
+$NetBSD: patch-objcxx_eh.h,v 1.2 2020/04/17 13:32:23 joerg Exp $
 
 Workaround build failure due to redefinition
 
---- objcxx_eh.h.orig	2015-08-07 13:33:41.000000000 +0200
-+++ objcxx_eh.h	2020-04-14 11:19:51.950615777 +0200
-@@ -1,7 +1,12 @@
+--- objcxx_eh.h.orig	2015-08-07 11:33:41.000000000 +0000
++++ objcxx_eh.h
+@@ -1,6 +1,11 @@
  #ifdef __cplusplus
  extern "C" {
  #endif
@@ -16,9 +16,7 @@ Workaround build failure due to redefinition
  /**
   * Allocates a C++ exception.  This function is part of the Itanium C++ ABI and
   * is provided externally.
-  */
-@@ -24,9 +29,9 @@
-  * Frees an exception object allocated by __cxa_allocate_exception().  Part of
+@@ -25,7 +30,7 @@ __attribute__((weak)) DECLARE_PERSONALIT
   * the Itanium C++ ABI.
   */
  __attribute__((weak))
@@ -27,4 +25,3 @@ Workaround build failure due to redefinition
  /**
   * Tests whether a C++ exception contains an Objective-C object, and returns if
   * if it does.  The second argument is a pointer to a boolean value indicating
-  * whether this is a valid object.
