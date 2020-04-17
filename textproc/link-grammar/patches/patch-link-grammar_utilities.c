3adbc4ab84e420f8f813070f21993a0a3d5d24ab
gutteridge <gutteridge@pkgsrc.org>
2020-04-17 01:43:41 +0000

    link-grammar: add the upstream pull request to a patch

--- textproc/link-grammar/patches/patch-link-grammar_utilities.c
+++ textproc/link-grammar/patches/patch-link-grammar_utilities.c
@@ -1,6 +1,7 @@
-$NetBSD: patch-link-grammar_utilities.c,v 1.1 2020/04/16 14:45:15 gutteridge Exp $
+$NetBSD: patch-link-grammar_utilities.c,v 1.2 2020/04/17 01:43:41 gutteridge Exp $
 
 Fix SunOS build.
+https://github.com/opencog/link-grammar/pull/1093
 
 --- link-grammar/utilities.c.orig	2020-02-29 01:44:12.000000000 +0000
 +++ link-grammar/utilities.c
