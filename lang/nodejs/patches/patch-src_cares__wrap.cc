333dd367b634e3c6d433c7805cd21a7107da3925
ryoon <ryoon@pkgsrc.org>
2020-04-17 10:57:02 +0000

    nodejs: Fix build under NetBSD

--- lang/nodejs/patches/patch-src_cares__wrap.cc
+++ lang/nodejs/patches/patch-src_cares__wrap.cc
@@ -1,15 +1,17 @@
-$NetBSD: patch-src_cares__wrap.cc,v 1.2 2017/06/05 21:14:04 fhajny Exp $
+$NetBSD: patch-src_cares__wrap.cc,v 1.3 2020/04/17 10:57:02 ryoon Exp $
 
 NetBSD has no AI_V4MAPPED.
 
---- src/cares_wrap.cc.orig	2017-05-30 17:32:13.000000000 +0000
+--- src/cares_wrap.cc.orig	2020-04-14 16:40:37.000000000 +0000
 +++ src/cares_wrap.cc
-@@ -47,7 +47,7 @@
+@@ -49,8 +49,9 @@
  # include <arpa/nameser.h>
  #endif
  
 -#if defined(__OpenBSD__)
 +#if defined(__OpenBSD__) || defined(__NetBSD__)
  # define AI_V4MAPPED 0
++# define AI_ALL 0
  #endif
  
+ namespace node {
