f3f7b14c1b4ce8957ff72853129f73363c90240c
taca <taca@pkgsrc.org>
2020-04-18 04:59:40 +0000

    lang/php73: update to 7.3.17.
    Update php73 to 7.3.17.
    
    
    16 Apr 2020, PHP 7.3.17
    
    - Core:
      . Fixed bug #79364 (When copy empty array, next key is unspecified). (cmb)
      . Fixed bug #78210 (Invalid pointer address). (cmb, Nikita)
    
    - CURL:
      . Fixed bug #79199 (curl_copy_handle() memory leak). (cmb)
    
    - Date:
      . Fixed bug #79396 (DateTime hour incorrect during DST jump forward). (Nate
        Brunette)
    
    - Iconv:
      . Fixed bug #79200 (Some iconv functions cut Windows-1258). (cmb)
    
    - OPcache:
      . Fixed bug #79412 (Opcache chokes and uses 100% CPU on specific script).
        (Dmitry)
    
    - Session:
      . Fixed bug #79413 (session_create_id() fails for active sessions). (cmb)
    
    - Shmop:
      . Fixed bug #79427 (Integer Overflow in shmop_open()). (cmb)
    
    - SimpleXML:
      . Fixed bug #61597 (SXE properties may lack attributes and content). (cmb)
    
    - Spl:
      . Fixed bug #75673 (SplStack::unserialize() behavior). (cmb)
      . Fixed bug #79393 (Null coalescing operator failing with SplFixedArray).
        (cmb)
    
    - Standard:
      . Fixed bug #79330 (shell_exec() silently truncates after a null byte). (stas)
      . Fixed bug #79465 (OOB Read in urldecode()). (stas)
      . Fixed bug #79410 (system() swallows last chunk if it is exactly 4095 bytes
        without newline). (Christian Schneider)
    
    - Zip:
      . Fixed Bug #79296 (ZipArchive::open fails on empty file). (Remi)
      . Fixed bug #79424 (php_zip_glob uses gl_pathc after call to globfree).
        (Max Rees)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.292 2020/03/20 08:14:44 taca Exp $
+# $NetBSD: phpversion.mk,v 1.293 2020/04/18 04:59:40 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP72_VERSION=	7.2.29
-PHP73_VERSION=	7.3.16
+PHP73_VERSION=	7.3.17
 PHP74_VERSION=	7.4.4
 
 # Define initial release of major version.
