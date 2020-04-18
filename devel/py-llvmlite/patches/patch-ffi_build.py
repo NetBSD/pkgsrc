25af15aad77d7a200bd853dbe2a0950dde71e6f7
adam <adam@pkgsrc.org>
2020-04-18 08:11:53 +0000

    py-llvmlite: updated to 0.32.0
    v0.32.0:
    * llvmlite wheel building fixes
    * ENH: Better error message
    * update install docs
    * binding: Allow empty features list
    * travis: Cleanup
    * azure-pipelines: Bump VM images.
    * Add paragraph on installing from sdist and on non-traditional platforms.
    * Remove python 2, python < 3.6, fix up, add flake8
    * Miscalled method and missing parameter in the documentation
    * Permit building on Visual Studio 2017
    * Update error message in LLVM version check.
    * update to final release date for 0.31.0

--- devel/py-llvmlite/patches/patch-ffi_build.py
+++ devel/py-llvmlite/patches/patch-ffi_build.py
@@ -1,19 +1,19 @@
-$NetBSD: patch-ffi_build.py,v 1.5 2019/10/19 14:10:00 adam Exp $
+$NetBSD: patch-ffi_build.py,v 1.6 2020/04/18 08:11:53 adam Exp $
 
-Allow building with LLVM 9.0.x.
+Allow building with LLVM 10.0.x and 9.0.x.
 Add NetBSD support.
 
---- ffi/build.py.orig	2019-10-10 19:15:38.000000000 +0000
+--- ffi/build.py.orig	2020-04-17 14:49:06.000000000 +0000
 +++ ffi/build.py
-@@ -109,7 +109,7 @@ def main_posix(kind, library_ext):
+@@ -110,7 +110,7 @@ def main_posix(kind, library_ext):
  
      out = out.decode('latin1')
      print(out)
 -    if not (out.startswith('8.0.') or out.startswith('7.0.')
-+    if not (out.startswith('9.0.') or out.startswith('8.0.') or out.startswith('7.0.')
++    if not (out.startswith('10.0.') or out.startswith('9.0.') or out.startswith('8.0.') or out.startswith('7.0.')
              or out.startswith('7.1.')):
          msg = (
-             "Building llvmlite requires LLVM 7.0+ Be sure to "
+             "Building llvmlite requires LLVM 7.0.x, 7.1.x or 8.0.x, got {!r}. "
 @@ -158,6 +158,8 @@ def main():
          main_posix('linux', '.so')
      elif sys.platform.startswith(('freebsd','openbsd')):
