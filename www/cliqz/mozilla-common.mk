19661f6b8669d62f884dd134b8cb2d4a52cc5fa2
fox <fox@pkgsrc.org>
2020-04-18 07:58:36 +0000

    www/cliqz: Updates to 1.35.0
    * Bumps version dependencies.
    * Adds python3 based SQLite3 library.
    * Imports a couple of patches from www/firefox.
    
    Changes since 1.34.1:
    
    Improvements
    
       * Cliqz got updated to Firefox 75.0 with various improvements and fixes.
       * The built-in security add-on HTTPS Everywhere got updated to the latest
         version 2020.3.16.
       * The Cliqz Browser now supports eight content processes by default. This
         should make it run even faster on powerful machines.
       * The Enterprise version of the Cliqz Browser offers a new setting to disable
         MyOffrz via enterprise policy.
    
    Fixes
    
       * Minor fixes for Cliqz Tab improve performance and stability.
       * Some websites that use third-party cookies were no longer working
         properly. These problems should now be fixed.
    
    Miscellaneous
    
       * We removed the "Do Not Track" option from the browser settings as it often
         has no effect and gives you a false sense of security. Thanks to the
         Anti-Tracking technology built into the Cliqz Browser, you are fully
         protected from data collectors anyway. (Power users can still configure Do
         Not Track via about:config.)

--- www/cliqz/mozilla-common.mk
+++ www/cliqz/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.9 2020/03/31 15:40:54 fox Exp $
+# $NetBSD: mozilla-common.mk,v 1.10 2020/04/18 07:58:36 fox Exp $
 #
 # Common Makefile fragment for mozilla packages based on gecko 2.0.
 # derived from www/firefox
@@ -74,13 +74,13 @@ BUILDLINK_API_DEPENDS.libevent+=       libevent>=1.1
 BUILDLINK_API_DEPENDS.nspr+=   nspr>=4.25
 .include "../../devel/nspr/buildlink3.mk"
 .include "../../textproc/icu/buildlink3.mk"
-BUILDLINK_API_DEPENDS.nss+=     nss>=3.50
+BUILDLINK_API_DEPENDS.nss+=     nss>=3.51
 .include "../../devel/nss/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../graphics/MesaLib/buildlink3.mk"
 BUILDLINK_API_DEPENDS.libwebp+=	libwebp>=1.0.2
 .include "../../graphics/libwebp/buildlink3.mk"
-BUILDLINK_API_DEPENDS.libvpx+= libvpx>=1.3.0
+BUILDLINK_API_DEPENDS.libvpx+= libvpx>=1.6.0
 .include "../../multimedia/libvpx/buildlink3.mk"
 .include "../../net/libIDL/buildlink3.mk"
 .include "../../multimedia/ffmpeg4/buildlink3.mk"
