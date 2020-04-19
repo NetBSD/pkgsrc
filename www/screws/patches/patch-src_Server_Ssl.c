db3c773f85fa4b0dd10573bd2e2e1b68cd3f0e28
plunky <plunky@pkgsrc.org>
2020-04-19 06:06:00 +0000

    repair build:
    add COMPILER_RPATH_FLAG to make env
    
    drop SSl3 support as breaks with newer OpenSSL

--- www/screws/patches/patch-src_Server_Ssl.c
+++ www/screws/patches/patch-src_Server_Ssl.c
@@ -1,14 +1,19 @@
-$NetBSD: patch-src_Server_Ssl.c,v 1.1 2016/04/03 20:24:27 joerg Exp $
+$NetBSD: patch-src_Server_Ssl.c,v 1.2 2020/04/19 06:06:00 plunky Exp $
 
---- src/Server/Ssl.c.orig	2016-04-03 16:00:51.049640130 +0000
+remove deprecatd methods
+
+--- src/Server/Ssl.c.orig	2004-09-16 22:05:58.000000000 +0000
 +++ src/Server/Ssl.c
-@@ -82,9 +82,6 @@ SslInit(n,ctx)
+@@ -82,12 +82,6 @@ SslInit(n,ctx)
  	
  	switch(Svr.enc)
  	{
 -	case ENC_SSL2:
 -		sm=SSLv2_server_method();
 -		break;
- 	case ENC_SSL3:
- 		sm=SSLv3_server_method();
+-	case ENC_SSL3:
+-		sm=SSLv3_server_method();
+-		break;
+ 	case ENC_SSL23:
+ 		sm=SSLv23_server_method();
  		break;
