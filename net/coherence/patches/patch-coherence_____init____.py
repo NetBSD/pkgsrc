60b6a62104bf47a45bbfee7a317e04f733e02c79
joerg <joerg@pkgsrc.org>
2020-04-20 00:42:52 +0000

    Don't use a Twisted version that no longer exist.

--- net/coherence/patches/patch-coherence_____init____.py
+++ net/coherence/patches/patch-coherence_____init____.py
@@ -1,11 +1,21 @@
-$NetBSD: patch-coherence_____init____.py,v 1.1 2016/04/17 10:15:21 wiz Exp $
+$NetBSD: patch-coherence_____init____.py,v 1.2 2020/04/20 00:42:52 joerg Exp $
 
 twisted 16.0.0 detection is broken, see
 https://github.com/coherence-project/Coherence/issues/25
 
 --- coherence/__init__.py.orig	2010-01-02 15:35:20.000000000 +0000
 +++ coherence/__init__.py
-@@ -24,8 +24,6 @@ try:
+@@ -11,8 +11,7 @@ SERVER_ID = ','.join([platform.system(),
+ 
+ 
+ try:
+-    from twisted import version as twisted_version
+-    from twisted.web import version as twisted_web_version
++    from twisted import __version__ as twisted_version
+     from twisted.python.versions import Version
+ except ImportError, exc:
+     # log error to stderr, might be useful for debugging purpose
+@@ -24,8 +23,6 @@ try:
      if twisted_version < Version("twisted", 2, 5, 0):
          raise ImportError("Twisted >= 2.5 is required. Please install it.")
  
