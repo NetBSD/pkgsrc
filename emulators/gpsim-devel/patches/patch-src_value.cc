06df6ea6795ff8b1068654a0c10976087d474941
joerg <joerg@pkgsrc.org>
2020-04-20 00:36:45 +0000

    Don't modify const strings. Honor length arguments. Bump revision.

--- emulators/gpsim-devel/patches/patch-src_value.cc
+++ emulators/gpsim-devel/patches/patch-src_value.cc
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_value.cc,v 1.1 2011/12/19 15:58:40 wiz Exp $
+$NetBSD: patch-src_value.cc,v 1.2 2020/04/20 00:36:45 joerg Exp $
 
 --- src/value.cc.orig	2005-08-31 14:48:21.000000000 +0000
 +++ src/value.cc
@@ -10,3 +10,12 @@ $NetBSD: patch-src_value.cc,v 1.1 2011/12/19 15:58:40 wiz Exp $
  
  #include "processor.h"
  
+@@ -1126,7 +1127,7 @@ void String::set(const char *s,int len)
+   if(value)
+     free(value);
+   if(s)
+-    value = strdup(s);
++    value = strndup(s, len);
+   else
+     value = 0;
+ }
