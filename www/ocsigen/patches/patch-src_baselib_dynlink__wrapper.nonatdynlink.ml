70947f80eb09a1e985e82b172d6b583f549bfd67
jaapb <jaapb@pkgsrc.org>
2020-04-21 11:51:02 +0000

    Updated www/ocsigen to version 2.16.0.
    This adds support for Lwt 5.

--- www/ocsigen/patches/patch-src_baselib_dynlink__wrapper.nonatdynlink.ml
+++ www/ocsigen/patches/patch-src_baselib_dynlink__wrapper.nonatdynlink.ml
@@ -1,6 +1,7 @@
-$NetBSD: patch-src_baselib_dynlink__wrapper.nonatdynlink.ml,v 1.1 2019/08/24 11:13:48 jaapb Exp $
+$NetBSD: patch-src_baselib_dynlink__wrapper.nonatdynlink.ml,v 1.2 2020/04/21 11:51:02 jaapb Exp $
 
 Changes for OCaml 4.08 (patch from upstream)
+
 --- src/baselib/dynlink_wrapper.nonatdynlink.ml.orig	2018-12-19 10:54:19.000000000 +0000
 +++ src/baselib/dynlink_wrapper.nonatdynlink.ml
 @@ -26,8 +26,6 @@ let loadfile _ = failwith message
