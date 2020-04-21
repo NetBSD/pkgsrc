641c203f88230006777dc8d223ed509de3d5cd04
jaapb <jaapb@pkgsrc.org>
2020-04-21 12:56:17 +0000

    Updated www/ocsigen-toolkit to version 2.5.0.
    Minor changes, bugfixes and compatibility updates.

--- www/ocsigen-toolkit/buildlink3.mk
+++ www/ocsigen-toolkit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2019/05/13 11:59:04 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/04/21 12:56:17 jaapb Exp $
 
 BUILDLINK_TREE+=	ocsigen-toolkit
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocsigen-toolkit
 OCSIGEN_TOOLKIT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocsigen-toolkit+=	ocsigen-toolkit>=2.0.0
-BUILDLINK_ABI_DEPENDS.ocsigen-toolkit+=	ocsigen-toolkit>=2.1.0
+BUILDLINK_ABI_DEPENDS.ocsigen-toolkit+=	ocsigen-toolkit>=2.5.0
 BUILDLINK_PKGSRCDIR.ocsigen-toolkit?=	../../www/ocsigen-toolkit
 
 .endif	# OCSIGEN_TOOLKIT_BUILDLINK3_MK
