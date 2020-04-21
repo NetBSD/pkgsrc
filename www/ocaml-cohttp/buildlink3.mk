9b53de39e4bef62d6fab5f777f890e45d2b1404a
jaapb <jaapb@pkgsrc.org>
2020-04-21 11:25:18 +0000

    Updated www/ocaml-cohttp to version 2.5.1.
    Many changes since the last version; mostly they are minor though. See the
    CHANGES.md file in the distribution for details.

--- www/ocaml-cohttp/buildlink3.mk
+++ www/ocaml-cohttp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/07/01 15:48:53 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/04/21 11:25:18 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-cohttp
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ocaml-cohttp
 OCAML_COHTTP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-cohttp+=	ocaml-cohttp>=2.1.2
+BUILDLINK_ABI_DEPENDS.ocaml-cohttp+=	ocaml-cohttp>=2.5.1
 BUILDLINK_PKGSRCDIR.ocaml-cohttp?=	../../www/ocaml-cohttp
 
 .endif	# OCAML_COHTTP_BUILDLINK3_MK
