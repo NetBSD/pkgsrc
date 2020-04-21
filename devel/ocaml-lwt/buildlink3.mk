c0118102cf66a5f9ba0f7531582529d2b685d59c
jaapb <jaapb@pkgsrc.org>
2020-04-21 11:29:59 +0000

    Updated devel/ocaml-lwt to version 5.2.0.
    This adds several updates and bugfixes, some of them breaking. For more
    details see the CHANGES file in the distribution.

--- devel/ocaml-lwt/buildlink3.mk
+++ devel/ocaml-lwt/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2019/05/13 11:38:45 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2020/04/21 11:29:59 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-lwt
 
 .if !defined(OCAML_LWT_BUILDLINK3_MK)
 OCAML_LWT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-lwt+=	ocaml-lwt>=4.0.0
-BUILDLINK_ABI_DEPENDS.ocaml-lwt+=	ocaml-lwt>=4.2.1
+BUILDLINK_API_DEPENDS.ocaml-lwt+=	ocaml-lwt>=5.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-lwt+=	ocaml-lwt>=5.2.0
 BUILDLINK_PKGSRCDIR.ocaml-lwt?=		../../devel/ocaml-lwt
 
 #.include "../../devel/libev/buildlink3.mk"
--- devel/ocaml-lwt/patches/patch-src_unix_unix__c_unix__stat__job__utils.c
+++ devel/ocaml-lwt/patches/patch-src_unix_unix__c_unix__stat__job__utils.c
@@ -1,11 +1,10 @@
-$NetBSD: patch-src_unix_unix__c_unix__stat__job__utils.c,v 1.1 2018/04/05 11:04:40 jaapb Exp $
+$NetBSD: patch-src_unix_unix__c_unix__stat__job__utils.c,v 1.2 2020/04/21 11:29:59 jaapb Exp $
 
 Need limits.h for IOV_MAX.
-Avoid system NANOSEC definition.
 
---- src/unix/unix_c/unix_stat_job_utils.c.orig	2018-01-11 16:40:12.000000000 +0000
+--- src/unix/unix_c/unix_stat_job_utils.c.orig	2020-03-09 04:17:06.000000000 +0000
 +++ src/unix/unix_c/unix_stat_job_utils.c
-@@ -28,6 +28,7 @@
+@@ -10,6 +10,7 @@
  #include <caml/alloc.h>
  #include <caml/memory.h>
  #include <errno.h>
@@ -13,18 +12,3 @@ Avoid system NANOSEC definition.
  
  #include "unix_stat_job_utils.h"
  
-@@ -37,11 +38,11 @@ value copy_stat(int use_64, struct stat 
-     CAMLlocal5(atime, mtime, ctime, offset, v);
- 
-     atime = caml_copy_double((double)buf->st_atime +
--                             (NANOSEC(buf, a) / 1000000000.0));
-+                             (OCNANOSEC(buf, a) / 1000000000.0));
-     mtime = caml_copy_double((double)buf->st_mtime +
--                             (NANOSEC(buf, m) / 1000000000.0));
-+                             (OCNANOSEC(buf, m) / 1000000000.0));
-     ctime = caml_copy_double((double)buf->st_ctime +
--                             (NANOSEC(buf, c) / 1000000000.0));
-+                             (OCNANOSEC(buf, c) / 1000000000.0));
-     offset = use_64 ? caml_copy_int64(buf->st_size) : Val_int(buf->st_size);
-     v = caml_alloc_small(12, 0);
-     Field(v, 0) = Val_int(buf->st_dev);
