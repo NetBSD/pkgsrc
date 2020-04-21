333184caa111ce06e3e1b8f5f132e0f38aa9ea7c
jaapb <jaapb@pkgsrc.org>
2020-04-21 13:02:02 +0000

    Updated www/ocsigen-i18n to version 3.5.0.
    This adds a dependency on ppx_tools.

--- www/ocsigen-i18n/buildlink3.mk
+++ www/ocsigen-i18n/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2019/05/13 17:18:43 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/04/21 13:02:02 jaapb Exp $
 
 BUILDLINK_TREE+=	ocsigen-i18n
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocsigen-i18n
 OCSIGEN_I18N_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocsigen-i18n+=	ocsigen-i18n>=3.1.0
-BUILDLINK_ABI_DEPENDS.ocsigen-i18n+=	ocsigen-i18n>=3.4.0
+BUILDLINK_ABI_DEPENDS.ocsigen-i18n+=	ocsigen-i18n>=3.5.0
 BUILDLINK_PKGSRCDIR.ocsigen-i18n?=	../../www/ocsigen-i18n
 .endif	# OCSIGEN_I18N_BUILDLINK3_MK
 
