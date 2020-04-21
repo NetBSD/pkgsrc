196dc992a942e698667410b54a4e129693c799cd
jaapb <jaapb@pkgsrc.org>
2020-04-21 11:21:47 +0000

    Updated www/ocaml-uri to version 3.1.0.
    This version splits out the uri and uri-sexp packages, and has some other
    minor improvements and updates.

--- www/ocaml-uri/buildlink3.mk
+++ www/ocaml-uri/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2019/11/04 22:09:58 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/04/21 11:21:47 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-uri
 
 .if !defined(OCAML_URI_BUILDLINK3_MK)
 OCAML_URI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-uri+=	ocaml-uri>=2.2.0
+BUILDLINK_API_DEPENDS.ocaml-uri+=	ocaml-uri>=3.0.0
+BUILDLINK_ABI_DEPENDS.ocaml-uri+=	ocaml-uri>=3.1.0
 BUILDLINK_PKGSRCDIR.ocaml-uri?=		../../www/ocaml-uri
 
 .endif	# OCAML_URI_BUILDLINK3_MK
