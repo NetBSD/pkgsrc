6f4685aa1c294cf1d2e340dcf58ed823ac60a64b
jaapb <jaapb@pkgsrc.org>
2020-04-21 12:52:06 +0000

    Updated www/eliom to version 6.11.0.
    The CHANGES file in the distribution has not been updated fully, but
    changes include several improvements and adaptations to new versions of
    dependencies and the OCaml language.

--- www/eliom/patches/patch-pkg_build.ml
+++ www/eliom/patches/patch-pkg_build.ml
@@ -1,29 +1,14 @@
-$NetBSD: patch-pkg_build.ml,v 1.3 2019/08/24 11:07:34 jaapb Exp $
+$NetBSD: patch-pkg_build.ml,v 1.4 2020/04/21 12:52:06 jaapb Exp $
 
 Do not install mime types
-Do not build syntax package (patch from upstream)
---- pkg/build.ml.orig	2019-02-08 11:31:12.000000000 +0000
+
+--- pkg/build.ml.orig	2020-03-11 10:55:00.000000000 +0000
 +++ pkg/build.ml
-@@ -75,12 +75,6 @@ let () =
-     Pkg.bin ~auto:true ~dst:"eliom-distillery" "src/tools/distillery";
-     Pkg.bin ~auto:true "src/ocamlbuild/eliombuild";
- 
--    (* SYNTAXES *)
--    Pkg.lib ~exts:exts_syntax ~dst:"syntax/pa_eliom_seed" "src/syntax/pa_eliom_seed";
--    Pkg.lib ~exts:exts_syntax ~dst:"syntax/pa_eliom_client_client" "src/syntax/pa_eliom_client_client";
--    Pkg.lib ~exts:exts_syntax ~dst:"syntax/pa_eliom_client_server" "src/syntax/pa_eliom_client_server";
--    Pkg.lib ~exts:exts_syntax ~dst:"syntax/pa_eliom_type_filter" "src/syntax/pa_eliom_type_filter";
--
-     Pkg.lib ~exts:exts_modlib ~dst:"ocamlbuild/ocamlbuild_eliom" "src/ocamlbuild/ocamlbuild_eliom";
- 
-     (* PPX *)
-@@ -109,8 +103,7 @@ let () =
-     (* MISC *)
+@@ -104,7 +104,6 @@ let () =
  
      Pkg.doc "README.md";
--    Pkg.doc "CHANGES";
+     Pkg.doc "CHANGES";
 -    Pkg.etc "pkg/etc/mime.types"
-+    Pkg.doc "CHANGES"
    ] @ (
      List.flatten (
        List.map (fun (name,files) ->
