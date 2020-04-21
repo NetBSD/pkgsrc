ec27f31b90aaad64985edbef4af2ea3230700aea
jaapb <jaapb@pkgsrc.org>
2020-04-21 10:48:30 +0000

    Updated devel/ocaml-resource-pooling to version 1.1.
    No CHANGES file, so it's hard to see what changes are, but the updated
    version of ocsigen-start will not run with the old version.

--- devel/ocaml-resource-pooling/buildlink3.mk
+++ devel/ocaml-resource-pooling/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/01/29 14:20:26 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/04/21 10:48:30 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-resource-pooling
 
 .if !defined(OCAML_RESOURCE_POOLING_BUILDLINK3_MK)
 OCAML_RESOURCE_POOLING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-resource-pooling+=	ocaml-resource-pooling>=0.6
+BUILDLINK_API_DEPENDS.ocaml-resource-pooling+=	ocaml-resource-pooling>=1.0
+BUILDLINK_API_DEPENDS.ocaml-resource-pooling+=	ocaml-resource-pooling>=1.1
 BUILDLINK_PKGSRCDIR.ocaml-resource-pooling?=	../../devel/ocaml-resource-pooling
 
 .endif	# OCAML_RESOURCE_POOLING_BUILDLINK3_MK
