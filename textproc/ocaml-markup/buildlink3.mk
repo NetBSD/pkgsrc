56f92013f3be78d2840f181234861693612fc891
jaapb <jaapb@pkgsrc.org>
2020-04-21 11:00:32 +0000

    Updated textproc/ocaml-markup to version 0.8.2.
    Changes are minor, plus a license change to MIT.

--- textproc/ocaml-markup/buildlink3.mk
+++ textproc/ocaml-markup/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2019/03/05 17:08:08 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2020/04/21 11:00:32 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-markup
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-markup
 OCAML_MARKUP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-markup+=	ocaml-markup>=0.7.2
-BUILDLINK_ABI_DEPENDS.ocaml-markup+=	ocaml-markup>=0.8.0nb1
+BUILDLINK_ABI_DEPENDS.ocaml-markup+=	ocaml-markup>=0.8.2
 BUILDLINK_PKGSRCDIR.ocaml-markup?=	../../textproc/ocaml-markup
 
 .endif	# OCAML_MARKUP_BUILDLINK3_MK
