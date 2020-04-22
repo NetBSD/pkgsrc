82f64f67e612513723ad42cecd23906dd9d9bc97
nia <nia@pkgsrc.org>
2020-04-22 08:13:39 +0000

    caprice32: Update to 4.6.0
    Changes:
    * Support CAP32_DELAY in autorun commands
    * Support German keyboard (physical layout only)
    * Better freedesktop integration
    * Better logic for finding configuration file
    * Dynamic loading of CAPS (IPF) library
    * Nix packaging support

--- emulators/caprice32/patches/patch-src_cap32.cpp
+++ emulators/caprice32/patches/patch-src_cap32.cpp
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_cap32.cpp,v 1.1 2020/03/06 12:23:01 nia Exp $
+$NetBSD: patch-src_cap32.cpp,v 1.2 2020/04/22 08:13:40 nia Exp $
 
 Fix paths.
 
---- src/cap32.cpp.orig	2019-01-04 19:01:29.000000000 +0000
+--- src/cap32.cpp.orig	2020-03-14 14:36:08.000000000 +0000
 +++ src/cap32.cpp
-@@ -1578,7 +1578,7 @@ std::string getConfigurationFilename(boo
-         configFilename = std::string(getenv("HOME")) + "/.cap32.cfg";
-         // If still not found, look for cap32.cfg in /etc
-         if (!forWrite && access(configFilename.c_str(), F_OK) != 0) {
--           configFilename = "/etc/cap32.cfg";
-+           configFilename = "@PKG_SYSCONFDIR@/cap32.cfg";
-         }
-      }
-   }
+@@ -1577,7 +1577,7 @@ std::string getConfigurationFilename(boo
+     { getenv("XDG_CONFIG_HOME"), "/cap32.cfg" },
+     { getenv("HOME"), "/.config/cap32.cfg" },
+     { getenv("HOME"), "/.cap32.cfg" },
+-    { PATH_OK, "/etc/cap32.cfg"}
++    { PATH_OK, "@PKG_SYSCONFDIR@/cap32.cfg"}
+   };
+ 
+   for(const auto& p: configPaths){
