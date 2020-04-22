eef36b0651517467d0fc134ce1748ea48441d3f6
nia <nia@pkgsrc.org>
2020-04-22 05:01:51 +0000

    balsa: Update to 2.6.0
    * Balsa-2.6.0 release. Release date 2020-04-11
    
    This release switches to gmime3 library for MIME processing.

--- mail/balsa/options.mk
+++ mail/balsa/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.4 2019/09/14 18:57:10 nia Exp $
+# $NetBSD: options.mk,v 1.5 2020/04/22 05:01:51 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.balsa
-PKG_SUPPORTED_OPTIONS=	compface ldap sqlite
+PKG_SUPPORTED_OPTIONS=	compface ldap
 
 .include "../../mk/bsd.options.mk"
 
@@ -13,9 +13,4 @@ CONFIGURE_ARGS+=	--with-compface
 .if !empty(PKG_OPTIONS:Mldap)
 CONFIGURE_ARGS+=	--with-ldap
 .  include "../../databases/openldap-client/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Msqlite)
-CONFIGURE_ARGS+=	--with-sqlite
-.  include "../../databases/sqlite3/buildlink3.mk"
 .endif
