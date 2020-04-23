fb092ddd139ac046cf035bfaf3824246f57e7b51
adam <adam@pkgsrc.org>
2020-04-23 10:54:51 +0000

    libtorrent-rasterbar: updated to 1.2.6
    1.2.6:
    fix peer timeout logic
    simplify proxy handling. A proxy now overrides listen_interfaces
    fix issues when configured to use a non-default choking algorithm
    fix issue in reading resume data
    revert NXDOMAIN change from 1.2.4
    don't open any listen sockets if listen_interfaces is empty or misconfigured
    fix bug in auto disk cache size logic
    fix issue with outgoing_interfaces setting, where bind() would be called twice
    add build option to disable share-mode
    support validation of HTTPS trackers
    deprecate strict super seeding mode
    make UPnP port-mapping lease duration configurable
    deprecate the bittyrant choking algorithm
    add build option to disable streaming

--- net/libtorrent-rasterbar/buildlink3.mk
+++ net/libtorrent-rasterbar/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/01/18 21:48:19 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/04/23 10:54:51 adam Exp $
 
 BUILDLINK_TREE+=	libtorrent-rasterbar
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libtorrent-rasterbar
 LIBTORRENT_RASTERBAR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libtorrent-rasterbar+=	libtorrent-rasterbar>=1.2.2
-BUILDLINK_ABI_DEPENDS.libtorrent-rasterbar?=	libtorrent-rasterbar>=1.2.3nb3
+BUILDLINK_ABI_DEPENDS.libtorrent-rasterbar+=	libtorrent-rasterbar>=1.2.3nb3
 BUILDLINK_PKGSRCDIR.libtorrent-rasterbar?=	../../net/libtorrent-rasterbar
 
 .include "../../devel/boost-libs/buildlink3.mk"
