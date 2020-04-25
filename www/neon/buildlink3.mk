ec90fb13997cc1f6760d9ee92236b2cc5eb680be
nia <nia@pkgsrc.org>
2020-04-25 12:45:43 +0000

    neon: Add gnutls support.
    This probably makes more sense as a default given that neon is GPLv2.

--- www/neon/buildlink3.mk
+++ www/neon/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2020/01/18 21:48:30 jperkin Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2020/04/25 12:45:43 nia Exp $
 
 BUILDLINK_TREE+=	neon
 
@@ -9,8 +9,18 @@ BUILDLINK_API_DEPENDS.neon+=	neon>=0.24.4
 BUILDLINK_ABI_DEPENDS.neon+=	neon>=0.30.2nb3
 BUILDLINK_PKGSRCDIR.neon?=	../../www/neon
 
-.include "../../devel/zlib/buildlink3.mk"
+pkgbase := neon
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.neon:Mgnutls}
+.include "../../security/gnutls/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.neon:Mopenssl}
 .include "../../security/openssl/buildlink3.mk"
+.endif
+
+.include "../../devel/zlib/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
 .endif # NEON_BUILDLINK3_MK
 
