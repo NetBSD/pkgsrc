c964a36a6aa9d188e8066a24e413928ecbb67307
nia <nia@pkgsrc.org>
2020-04-25 14:04:59 +0000

    libdlna: Switch to ffmpeg4

--- net/libdlna/buildlink3.mk
+++ net/libdlna/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2018/02/26 08:19:26 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2020/04/25 14:04:59 nia Exp $
 #
 
 BUILDLINK_TREE+=	libdlna
@@ -10,7 +10,7 @@ BUILDLINK_API_DEPENDS.libdlna+=	libdlna>=0.2.3
 BUILDLINK_ABI_DEPENDS.libdlna+=	libdlna>=0.2.4nb8
 BUILDLINK_PKGSRCDIR.libdlna?=	../../net/libdlna
 
-.include "../../multimedia/ffmpeg1/buildlink3.mk"
+.include "../../multimedia/ffmpeg4/buildlink3.mk"
 .endif	# LIBDLNA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libdlna
