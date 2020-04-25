9b815988fab28d41d64b0a14299d827bbcee0262
nia <nia@pkgsrc.org>
2020-04-25 10:51:42 +0000

    dia: Disable GNOME 2 integration

--- graphics/dia/options.mk
+++ graphics/dia/options.mk
@@ -1,23 +1,12 @@
-# $NetBSD: options.mk,v 1.8 2009/12/09 10:22:04 drochner Exp $
+# $NetBSD: options.mk,v 1.9 2020/04/25 10:51:42 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dia
-PKG_SUPPORTED_OPTIONS=	gnome cairo
-PKG_SUGGESTED_OPTIONS=	gnome cairo
+PKG_SUPPORTED_OPTIONS=	cairo
+PKG_SUGGESTED_OPTIONS=	cairo
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		cairo gnome
-
-.if !empty(PKG_OPTIONS:Mgnome)
-CONFIGURE_ARGS+=	--enable-gnome
-.  include "../../devel/libgnome/buildlink3.mk"
-.  include "../../devel/libgnomeui/buildlink3.mk"
-PLIST.gnome=		yes
-PLIST_SUBST+=		DIA_HELPDIR=share/gnome/help/dia
-.else
-CONFIGURE_ARGS+=	--disable-gnome
-PLIST_SUBST+=		DIA_HELPDIR=share/dia/help
-.endif
+PLIST_VARS+=		cairo
 
 .if !empty(PKG_OPTIONS:Mcairo)
 CONFIGURE_ARGS+=	--with-cairo
