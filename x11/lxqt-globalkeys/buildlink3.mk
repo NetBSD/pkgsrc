0e40a8b172789eace20297d588d127095174f29a
gutteridge <gutteridge@pkgsrc.org>
2020-04-25 01:34:53 +0000

    lxqt-globalkeys: add new package, version 0.14.3
    LXQt daemon and library for global keyboard shortcuts registration.
    (Packaged in wip by pin@, final review by myself.)

---
+++ x11/lxqt-globalkeys/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/04/25 01:34:53 gutteridge Exp $
+
+BUILDLINK_TREE+=	lxqt-globalkeys
+
+.if !defined(LXQT_GLOBALKEYS_BUILDLINK3_MK)
+LXQT_GLOBALKEYS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=0.14.3
+BUILDLINK_PKGSRCDIR.lxqt-globalkeys?=	../../x11/lxqt-globalkeys
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/liblxqt/buildlink3.mk"
+.endif	# LXQT_GLOBALKEYS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-lxqt-globalkeys
