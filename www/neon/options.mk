f407ce41120b11048b0b0dccda5a45f491b8c0b7
nia <nia@pkgsrc.org>
2020-04-25 12:45:43 +0000

    neon: Add gnutls support.
    This probably makes more sense as a default given that neon is GPLv2.

--- www/neon/options.mk
+++ www/neon/options.mk
@@ -1,7 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2006/01/31 04:15:29 epg Exp $
+# $NetBSD: options.mk,v 1.2 2020/04/25 12:45:43 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.neon
+
+PKG_OPTIONS_REQUIRED_GROUPS=	ssl
+PKG_OPTIONS_GROUP.ssl=		gnutls openssl
+
 PKG_SUPPORTED_OPTIONS=	gssapi
+PKG_SUGGESTED_OPTIONS=	gnutls
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,4 +15,12 @@ CONFIGURE_ARGS+=	--with-gssapi
 .  include "../../mk/krb5.buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-gssapi
+.endif
+
+.if !empty(PKG_OPTIONS:Mopenssl)
+.  include "../../security/openssl/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-ssl=openssl
+.elif !empty(PKG_OPTIONS:Mgnutls)
+.  include "../../security/gnutls/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-ssl=gnutls
 .endif
