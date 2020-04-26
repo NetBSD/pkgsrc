da57656b7ed7cc3ec93c1b5cde51738a9f19adf3
nia <nia@pkgsrc.org>
2020-04-26 14:06:32 +0000

    xournalpp: Update to 1.0.18. Patch from Yorick Hardy.
    ## 1.0.18
    
    * Fixed a crash occurring when recent file entries are invalid (#1730, thanks to
      @iczero)
    * Fixed translations not being built correctly, causing packaging issues (#1596)
    * Fixed background PDF outlines not being saved in exported PDF (only available
      when compiled with Cairo 1.16 or newer)
    * Fixed a deadlock occurring when a second PDF with an outline is opened (#1582).
    * Fixed the settings file being written to when it is parsed (#1074, thanks to
      @Guldoman)
    * Fixed dark mode icons not loading properly (#1767, thanks to @badshah400)
    * Added missing dark mode icons (#1765, thanks to @badshah400)
    * Fixed crash in `Export As ...` on some page range options (#1790)
    * Fixed crash caused by custom colors in toolbar being "too close" (#1659)
    * Windows: Fixed the LaTeX tool always failing to find kpsewhich (#1738). Note
      that to make this work properly, a console window will now flash briefly
      before Xournal++ starts.

--- editors/xournalpp/patches/patch-src_control_tools_StrokeHandler.cpp
+++ editors/xournalpp/patches/patch-src_control_tools_StrokeHandler.cpp
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_control_tools_StrokeHandler.cpp,v 1.1 2019/11/02 14:44:15 nia Exp $
+$NetBSD: patch-src_control_tools_StrokeHandler.cpp,v 1.2 2020/04/26 14:06:32 nia Exp $
 
 On NetBSD, absolute coordinate changes are reported as an X value followed by a Y value,
 which (when moving quickly) leads to a "stepping" effect. Using a Bezier curve fitting
 for the points yields a smooth curve in most cases, and does not seem to adversely affect
 the user experience (even with a conventional mouse).
 
---- ./src/control/tools/StrokeHandler.cpp.orig	2019-09-10 05:47:36.000000000 +0000
-+++ ./src/control/tools/StrokeHandler.cpp
+--- src/control/tools/StrokeHandler.cpp.orig	2020-04-15 19:03:39.000000000 +0000
++++ src/control/tools/StrokeHandler.cpp
 @@ -115,9 +115,36 @@ bool StrokeHandler::onMotionNotifyEvent(
  	}
  	else
@@ -44,3 +44,35 @@ the user experience (even with a conventional mouse).
  
  			Stroke lastSegment;
  
+@@ -154,6 +181,15 @@ void StrokeHandler::onButtonReleaseEvent
+ 	Control* control = xournal->getControl();
+ 	Settings* settings = control->getSettings();
+ 
++#if defined(__NetBSD__) || defined(__OpenBSD__)
++	// Since we omit the last point (treat it as a control point),
++	// the stroke may be empty -- i.e. has only one point --
++	// and we can make it "non-empty" by duplicating the point.
++	if(stroke->getPointCount() == 1) {
++		stroke->addPoint(stroke->getPoint(0));
++        }
++#endif
++
+ 	if (settings->getStrokeFilterEnabled())  // Note: For shape tools see BaseStrokeHandler which has a slightly
+ 	                                         // different version of this filter. See //!
+ 	{
+@@ -192,6 +228,15 @@ void StrokeHandler::onButtonReleaseEvent
+ 		this->lastStrokeTime = pos.timestamp;
+ 	}
+ 
++#if defined(__NetBSD__) || defined(__OpenBSD__)
++	if (stroke->getPointCount() > 0)
++	{
++		// The last segment was never drawn, so it will "pop up" if we repaint.
++		// Visually it was never there, so remove it.
++		stroke->deletePoint(stroke->getPointCount()-1);
++	}
++#endif
++
+ 	// Backward compatibility and also easier to handle for me;-)
+ 	// I cannot draw a line with one point, to draw a visible line I need two points,
+ 	// twice the same Point is also OK
