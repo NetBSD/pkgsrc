c90ac97c0d97a36f906c814273c0079f0122331f
adam <adam@pkgsrc.org>
2020-04-27 16:52:55 +0000

    Added py-numpy16 - the last version to support Python 2.7

---
+++ math/py-numpy16/patches/patch-numpy_linalg_lapack__litemodule.c
@@ -0,0 +1,15 @@
+$NetBSD: patch-numpy_linalg_lapack__litemodule.c,v 1.1 2020/04/27 16:52:56 adam Exp $
+
+Add function definition for FNAME(MAIN_).
+
+--- numpy/linalg/lapack_litemodule.c.orig	2015-04-16 14:14:55.000000000 +0000
++++ numpy/linalg/lapack_litemodule.c
+@@ -17,6 +17,8 @@ typedef struct { float r, i; } f2c_compl
+ typedef struct { double r, i; } f2c_doublecomplex;
+ /* typedef long int (*L_fp)(); */
+ 
++int FNAME(MAIN_)() { return 0; }
++
+ extern int FNAME(dgelsd)(int *m, int *n, int *nrhs,
+                           double a[], int *lda, double b[], int *ldb,
+                           double s[], double *rcond, int *rank,
