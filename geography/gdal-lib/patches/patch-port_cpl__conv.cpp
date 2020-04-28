2fd8f2d97b0472dc6773b1eaf7523e4d0d3c6fb6
gdt <gdt@pkgsrc.org>
2020-04-28 00:24:48 +0000

    gdal: Simplify patch dealing with visibility defines
    Rather than adding a definition of _NETBSD_SOURCE, avoiding defining
    _XOPEN_SOURCE in the first place.  What used to be extra is now
    normal, and these defines hide rather than expose symbols.

--- geography/gdal-lib/patches/patch-port_cpl__conv.cpp
+++ geography/gdal-lib/patches/patch-port_cpl__conv.cpp
@@ -1,17 +1,23 @@
-$NetBSD: patch-port_cpl__conv.cpp,v 1.2 2019/03/09 01:54:59 gdt Exp $
+$NetBSD: patch-port_cpl__conv.cpp,v 1.3 2020/04/28 00:24:48 gdt Exp $
 
-Fix build under NetBSD
-\todo Explain what is wrong.
-\todo File ticket and get this fixed upstream.
+Avoid visibility define that masks usage that is apparently beyond XOPEN or C99.
 
---- port/cpl_conv.cpp.orig	2017-11-20 10:45:32.000000000 +0000
+Not yet net upstream; waiting until we are caught up.
+
+--- port/cpl_conv.cpp.orig	2020-01-08 11:49:05.000000000 +0000
 +++ port/cpl_conv.cpp
-@@ -45,7 +45,7 @@
+@@ -38,14 +38,7 @@
+ #define _XOPEN_SOURCE 600
  #endif
- 
- // For atoll (at least for NetBSD)
+ #else
+-#ifdef _XOPEN_SOURCE
+-#undef _XOPEN_SOURCE
+ #endif
+-#define _XOPEN_SOURCE 700
+-#endif
+-
+-// For atoll (at least for NetBSD)
 -#define _ISOC99_SOURCE
-+#define _NETBSD_SOURCE
  
  #ifdef MSVC_USE_VLD
  #include <vld.h>
