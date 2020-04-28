ff4960f7db5751e1f4adbd2fd22aa2cf5be3bd64
riastradh <riastradh@pkgsrc.org>
2020-04-28 23:01:26 +0000

    security/openpam: define NO_STATIC_MODULES on NetBSD
    This is a hack to work around a mistake in the NetBSD openpam build
    which leaked into the public header files.  We will fix this in the
    NetBSD build but it's been in the public header files for nearly a
    decade now, with each individual pam module sometimes having this
    workaround, so let's apply the workaround uniformly for now.
    
    PR security/39313
    PR security/55216

--- security/openpam/builtin.mk
+++ security/openpam/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.11 2019/11/04 21:12:56 rillig Exp $
+# $NetBSD: builtin.mk,v 1.12 2020/04/28 23:01:26 riastradh Exp $
 
 BUILTIN_PKG:=	openpam
 
@@ -59,3 +59,7 @@ USE_BUILTIN.openpam!=							\
 .  endif  # PREFER.openpam
 .endif
 MAKEVARS+=		USE_BUILTIN.openpam
+
+.if !empty(USE_BUILTIN.openpam:M[yY][eE][sS])
+CPPFLAGS.NetBSD+=	-DNO_STATIC_MODULES
+.endif
