70dc9996646a6a447c5b7c48a1fe5b9fa28aef6a
nia <nia@pkgsrc.org>
2020-04-29 15:11:09 +0000

    ffmpeg*: Add speex support. Further clarify license mess.

--- multimedia/ffmpeg3/options.mk
+++ multimedia/ffmpeg3/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.19 2020/04/29 14:17:48 nia Exp $
+# $NetBSD: options.mk,v 1.20 2020/04/29 15:11:10 nia Exp $
 
 # Global and legacy options
 
@@ -8,10 +8,10 @@ PKG_OPTIONS_OPTIONAL_GROUPS=	ssl
 PKG_OPTIONS_GROUP.ssl=		gnutls openssl
 
 PKG_SUPPORTED_OPTIONS=	ass bluray doc fdk-aac fontconfig freetype jack \
-			lame libvpx opencore-amr opus pulseaudio rpi \
-			rtmp tesseract theora vorbis x11 x264 x265 xvid
+			lame libvpx opencore-amr opus pulseaudio rpi rtmp \
+			speex tesseract theora vorbis x11 x264 x265 xvid
 PKG_SUGGESTED_OPTIONS=	lame ass bluray freetype fontconfig gnutls libvpx \
-			opus theora vorbis x11 x264 x265 xvid
+			opus speex theora vorbis x11 x264 x265 xvid
 
 PKG_OPTIONS_LEGACY_OPTS+=	xcb:x11
 
@@ -88,11 +88,7 @@ CONFIGURE_ARGS+=	--disable-gnutls
 .if !empty(PKG_OPTIONS:Mopencore-amr)
 CONFIGURE_ARGS+=	--enable-libopencore-amrnb
 CONFIGURE_ARGS+=	--enable-libopencore-amrwb
-# "The OpenCORE external libraries are under the Apache License
-# 2.0. That license is incompatible with the LGPL v2.1 and the GPL
-# v2, but not with version 3 of those licenses. So to combine the
-# OpenCORE libraries with FFmpeg, the license version needs to be
-# upgraded by passing --enable-version3 to configure."
+# Apache License 2.0 is incompatible with (L)GPL versions before 3
 CONFIGURE_ARGS+=	--enable-version3
 # TODO: LICENSE
 .include "../../audio/opencore-amr/buildlink3.mk"
@@ -164,6 +160,12 @@ CONFIGURE_ARGS+=	--enable-libmp3lame
 .if !empty(PKG_OPTIONS:Mopus)
 CONFIGURE_ARGS+=	--enable-libopus
 .include "../../audio/libopus/buildlink3.mk"
+.endif
+
+# Speex support
+.if !empty(PKG_OPTIONS:Mspeex)
+CONFIGURE_ARGS+=	--enable-libspeex
+.include "../../audio/speex/buildlink3.mk"
 .endif
 
 # Raspberry Pi support
