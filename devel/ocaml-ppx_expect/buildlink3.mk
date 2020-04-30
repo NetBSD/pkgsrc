7a86c1e7c49e216a92a3a0d38991a1b1d31d2a10
jaapb <jaapb@pkgsrc.org>
2020-04-30 08:11:24 +0000

    Added OCaml 4.10 patch to devel/ocaml-ppx_expect. Revbump.
    No upstream changes.

--- devel/ocaml-ppx_expect/buildlink3.mk
+++ devel/ocaml-ppx_expect/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/01/16 14:20:56 jaapb Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/04/30 08:11:24 jaapb Exp $
 
 BUILDLINK_TREE+=	ocaml-ppx_expect
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-ppx_expect
 OCAML_PPX_EXPECT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-ppx_expect+=	ocaml-ppx_expect>=0.12.0
-BUILDLINK_ABI_DEPENDS.ocaml-ppx_expect+=	ocaml-ppx_expect>=0.13.0
+BUILDLINK_ABI_DEPENDS.ocaml-ppx_expect+=	ocaml-ppx_expect>=0.13.0nb1
 BUILDLINK_PKGSRCDIR.ocaml-ppx_expect?=		../../devel/ocaml-ppx_expect
 
 .endif	# OCAML_PPX_EXPECT_BUILDLINK3_MK
