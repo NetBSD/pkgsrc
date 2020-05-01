f628533ddeb18dd65c7b08cca3ae2909ae3cad86
joerg <joerg@pkgsrc.org>
2020-05-01 00:14:17 +0000

    Update embryo to 1.7.10. No relevant changes.

--- lang/embryo/buildlink3.mk
+++ lang/embryo/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2013/06/16 18:56:05 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/05/01 00:14:17 joerg Exp $
 
 BUILDLINK_TREE+=	embryo
 
 .if !defined(EMBRYO_BUILDLINK3_MK)
 EMBRYO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.embryo+=	embryo>=1.7.7
+BUILDLINK_API_DEPENDS.embryo+=	embryo>=1.7.10
 BUILDLINK_PKGSRCDIR.embryo?=	../../lang/embryo
 
 .include "../../devel/eina/buildlink3.mk"
