d01764b591a30530563871d50d5c111ce2d7eef0
tnn <tnn@pkgsrc.org>
2020-05-01 14:39:37 +0000

    go/bootstrap.mk: allow user to set GOROOT_BOOTSTRAP in mk.conf

--- lang/go/bootstrap.mk
+++ lang/go/bootstrap.mk
@@ -1,4 +1,6 @@
-# $NetBSD: bootstrap.mk,v 1.1 2020/05/01 14:09:14 tnn Exp $
+# $NetBSD: bootstrap.mk,v 1.2 2020/05/01 14:39:37 tnn Exp $
 
+.if !defined(GOROOT_BOOTSTRAP) || !exists(${GOROOT_BOOTSTRAP}/bin/go)
 BUILD_DEPENDS+=		go14-1.4*:../../lang/go14
 GOROOT_BOOTSTRAP=	${PREFIX}/go14
+.endif
