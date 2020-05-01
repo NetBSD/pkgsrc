acab9e5d0b45e70fc5d4c42d7513b4bd5a11f5a4
riastradh <riastradh@pkgsrc.org>
2020-05-01 11:48:26 +0000

    finance/ledger: Update to 3.2.0
    - Port Python support to Python 3
    
    - Entities are no longer regarded as defined due to being part of a
      cleared transaction. `--explicit` is effectively enabled by default
      and is now a no-op (PR #1819)
    
    - Add `--average-lot-prices` to show the average of lot prices
    
    - Add support for `%F` date format specifier (bug #1775)
    
    - Add `commodity_price(NAME, DATE)` function
    
    - Add `set_commodity_price(NAME, DATE)` function
    
    - Fix buffer overflow when evaluating date
    
    - Fix balance assertions on accounts with virtual posts (bug #543)
    
    - Fix segfault with `ledger print` (bug #1850)
    
    - Ensure that `apply` directives (like `apply account`) have the
      required argument (bug #553)
    
    - Format annotations using a date format that can be parsed
    
    - Change `--invert` to invert displayed amounts and totals, not amounts
      (bug #1803)
    
    - Correct the way parens are parsed in query expressions
    
    - Compare price annotations using their textual rendering
    
    - Fix build failure with utfcpp 3.0 (bug #1816)
    
    - Fix build failure due to ambiguous type (bug #1833)

--- finance/ledger/options.mk
+++ finance/ledger/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2017/09/06 21:08:09 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2020/05/01 11:48:26 riastradh Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ledger
 PKG_SUPPORTED_OPTIONS+=	python
@@ -12,6 +12,5 @@ PLIST_VARS+=		python
 CMAKE_ARGS+=		-DUSE_PYTHON:BOOL=TRUE
 PLIST.python=		yes
 PLIST_SUBST+=		PYSITELIB=${PYSITELIB:Q}
-PYTHON_VERSIONS_ACCEPTED=	27 # as of 3.1.1
 .include "../../devel/py-boost/buildlink3.mk"
 .endif
