d204f4d399596a3252e9ed9322bced093033ecc9
joerg <joerg@pkgsrc.org>
2020-05-01 00:01:32 +0000

    Use https. Bump API requirement.

--- devel/eina/buildlink3.mk
+++ devel/eina/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/06/16 18:56:04 sno Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/05/01 00:01:32 joerg Exp $
 
 BUILDLINK_TREE+=	eina
 
 .if !defined(EINA_BUILDLINK3_MK)
 EINA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.eina+=	eina>=1.7.7
+BUILDLINK_API_DEPENDS.eina+=	eina>=1.7.10
 BUILDLINK_PKGSRCDIR.eina?=	../../devel/eina
 .endif	# EINA_BUILDLINK3_MK
 
