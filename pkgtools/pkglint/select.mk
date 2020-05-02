4655b07de14a1a69991263deb4767898ee3a12ab
tnn <tnn@pkgsrc.org>
2020-05-02 10:43:44 +0000

    don't need a wildcard here

--- pkgtools/pkglint/select.mk
+++ pkgtools/pkglint/select.mk
@@ -1,4 +1,4 @@
-# $NetBSD: select.mk,v 1.6 2020/05/02 10:33:15 tnn Exp $
+# $NetBSD: select.mk,v 1.7 2020/05/02 10:43:44 tnn Exp $
 #
 # Selects the proper version of pkglint, depending on whether the
 # platform supports the Go programming language.
@@ -11,7 +11,7 @@
 .if ${MACHINE_ARCH:Ni386:Nx86_64:Nevbarm:Naarch64} || \
     ${MACHINE_PLATFORM:MSunOS-*-i386} || \
     ${MACHINE_PLATFORM:MNetBSD-[1-5].*-*} || \
-    ${MACHINE_PLATFORM:MNetBSD-9.0*-aarch64}
+    ${MACHINE_PLATFORM} == "NetBSD-9.0-aarch64"
 DEPENDS+=	pkglint4>=4.82<5:../../pkgtools/pkglint4
 .else
 DEPENDS+=	pkglint>=5:../../pkgtools/pkglint
