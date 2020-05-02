bd3fb54890fddc351a32f04c8666e65321d6cd27
triaxx <triaxx@pkgsrc.org>
2020-05-02 16:53:54 +0000

    grub2: update to 2.04
    upstream changes:
    -----------------
    * GCC 8 and 9 support.
    * Gnulib integration overhaul.
    * RISC-V support.
    * Xen PVH support.
    * Native UEFI secure boot support.
    * UEFI TPM driver.
    * New IEEE 1275 obdisk driver.
    * Btrfs RAID 5 and RIAD 6 support.
    * PARTUUID support.
    * VLAN support.
    * Native DHCP support.
    * Many ARM and ARM64 fixes.
    * Many SPARC fixes.
    * Many IEEE 1275 fixes.
    * ...and tons of other fixes and cleanups...

--- sysutils/grub2/patches/patch-grub-core_osdep_bsd_hostdisk.c
+++ sysutils/grub2/patches/patch-grub-core_osdep_bsd_hostdisk.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-grub-core_osdep_bsd_hostdisk.c,v 1.1 2018/05/23 07:56:53 triaxx Exp $
+$NetBSD: patch-grub-core_osdep_bsd_hostdisk.c,v 1.2 2020/05/02 16:53:54 triaxx Exp $
 
 Fix build error for NetBSD:
  * undefined RAW_FLOPPY_MAJOR macro
  * undefined fd variable
 
---- grub-core/osdep/bsd/hostdisk.c.orig    2015-05-21 15:50:29.000000000 +0000
+--- grub-core/osdep/bsd/hostdisk.c.orig	2018-11-24 17:13:02.000000000 +0000
 +++ grub-core/osdep/bsd/hostdisk.c
 @@ -50,6 +50,9 @@
  # endif /* HAVE_GETRAWPARTITION */
@@ -24,20 +24,20 @@ Fix build error for NetBSD:
 +grub_hostdisk_configure_device_driver (grub_util_fd_t fd)
  {
    struct stat st;
-
-@@ -81,7 +84,7 @@ configure_device_driver (grub_util_fd_t
+ 
+@@ -81,7 +84,7 @@ configure_device_driver (grub_util_fd_t 
  grub_util_fd_t
  grub_util_fd_open (const char *os_dev, int flags)
  {
 -  grub_util_fd_t ret;
 +  grub_util_fd_t fd;
-
+ 
  #ifdef O_LARGEFILE
    flags |= O_LARGEFILE;
 @@ -90,10 +93,10 @@ grub_util_fd_open (const char *os_dev, i
    flags |= O_BINARY;
  #endif
-
+ 
 -  ret = open (os_dev, flags, S_IROTH | S_IRGRP | S_IRUSR | S_IWUSR);
 -  if (ret >= 0)
 -    configure_device_driver (fd);
@@ -47,5 +47,5 @@ Fix build error for NetBSD:
 +    grub_hostdisk_configure_device_driver (fd);
 +  return fd;
  }
-
+ 
  #endif
