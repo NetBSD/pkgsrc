98c6368c13cdbae725027eafbae5e028e86e710a
tnn <tnn@pkgsrc.org>
2020-05-02 10:33:15 +0000

    pkglint/select.mk: prefer golang pkglint on NetBSD/aarch64 9.1 or later

--- pkgtools/pkglint/select.mk
+++ pkgtools/pkglint/select.mk
@@ -1,4 +1,4 @@
-# $NetBSD: select.mk,v 1.5 2018/10/03 22:27:53 rillig Exp $
+# $NetBSD: select.mk,v 1.6 2020/05/02 10:33:15 tnn Exp $
 #
 # Selects the proper version of pkglint, depending on whether the
 # platform supports the Go programming language.
@@ -8,7 +8,10 @@
 
 # See lang/go/version.mk
 # While it's wrong in the above, go14 does not build on NetBSD 5.
-.if ${MACHINE_ARCH:Ni386:Nx86_64:Nevbarm} || ${MACHINE_PLATFORM:MSunOS-*-i386} || ${MACHINE_PLATFORM:MNetBSD-[1-5].*-*}
+.if ${MACHINE_ARCH:Ni386:Nx86_64:Nevbarm:Naarch64} || \
+    ${MACHINE_PLATFORM:MSunOS-*-i386} || \
+    ${MACHINE_PLATFORM:MNetBSD-[1-5].*-*} || \
+    ${MACHINE_PLATFORM:MNetBSD-9.0*-aarch64}
 DEPENDS+=	pkglint4>=4.82<5:../../pkgtools/pkglint4
 .else
 DEPENDS+=	pkglint>=5:../../pkgtools/pkglint
