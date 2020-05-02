3659d2abe125891fcf47f66ea7cf38f4dd596423
tnn <tnn@pkgsrc.org>
2020-05-02 11:23:04 +0000

    picom: update to version 8.
    pkgsrc changes:
    - Restore patch-src_backend_gl_gl__common.c from the previous "compton"
      package. It is smaller and better explains why the patch is needed.
    - drop dependency on libxdg-basedir, dropped by upstream.
    
    Upstream new feature & improvements
    - You can now use the time variable in window shaders to get a timestamp.
    - --max-brightness, which allows you to dim windows that are too bright.
    - Automatic configuration file reloading after it's changed.
    - A lot of the artifacts seen with blur and use-damage enabled are fixed.
    - _NET_WM_BYPASS_COMPOSITOR property are now handled.
    - Completely transparent windows won't have a blurred background anymore.
    - Improved fullscreen window detection.
    - Various improvments to the inverter.sh script
    - wintype notify has been renamed to notification to match the EWMH names
    - (also various bug fixes)

--- x11/picom/patches/patch-src_backend_gl_gl__common.c
+++ x11/picom/patches/patch-src_backend_gl_gl__common.c
@@ -1,58 +1,42 @@
-$NetBSD: patch-src_backend_gl_gl__common.c,v 1.1 2019/12/15 14:05:47 nia Exp $
+$NetBSD: patch-src_backend_gl_gl__common.c,v 1.2 2020/05/02 11:23:05 tnn Exp $
 
-error: macro "memcpy" passed 19 arguments, but takes just 3
+Parentheses are required around macro argument containing braced initializer
+list.
+memcpy(3) is a macro when using e.g. -D_FORTIFY_SOURCE=2.
 
---- src/backend/gl/gl_common.c.orig	2019-11-18 21:59:30.000000000 +0000
+--- src/backend/gl/gl_common.c.orig	2019-08-18 21:40:33.000000000 +0000
 +++ src/backend/gl/gl_common.c
-@@ -315,22 +315,23 @@ static void x_rect_to_coords(int nrects,
- 		// log_trace("Rect %d: %f, %f, %f, %f -> %d, %d, %d, %d",
+@@ -316,7 +316,7 @@ static void x_rect_to_coords(int nrects,
  		//          ri, rx, ry, rxe, rye, rdx, rdy, rdxe, rdye);
  
--		memcpy(&coord[i * 16],
+ 		memcpy(&coord[i * 16],
 -		       (GLint[][2]){
--		           {vx1, vy1},
--		           {texture_x1, texture_y1},
--		           {vx2, vy1},
--		           {texture_x2, texture_y1},
--		           {vx2, vy2},
--		           {texture_x2, texture_y2},
--		           {vx1, vy2},
--		           {texture_x1, texture_y2},
++		       ((GLint[][2]){
+ 		           {vx1, vy1},
+ 		           {texture_x1, texture_y1},
+ 		           {vx2, vy1},
+@@ -325,11 +325,11 @@ static void x_rect_to_coords(int nrects,
+ 		           {texture_x2, texture_y2},
+ 		           {vx1, vy2},
+ 		           {texture_x1, texture_y2},
 -		       },
--		       sizeof(GLint[2]) * 8);
-+		GLint temp1[8][2] = {
-+		   {vx1, vy1},
-+		   {texture_x1, texture_y1},
-+		   {vx2, vy1},
-+		   {texture_x2, texture_y1},
-+		   {vx2, vy2},
-+		   {texture_x2, texture_y2},
-+		   {vx1, vy2},
-+		   {texture_x1, texture_y2},
-+		};
-+
-+		memcpy(&coord[i * 16], temp1, sizeof(GLint[2]) * 8);
++		       }),
+ 		       sizeof(GLint[2]) * 8);
  
  		GLuint u = (GLuint)(i * 4);
 -		memcpy(&indices[i * 6], (GLuint[]){u + 0, u + 1, u + 2, u + 2, u + 3, u + 0},
--		       sizeof(GLuint) * 6);
-+		GLuint temp2[] = {u + 0, u + 1, u + 2, u + 2, u + 3, u + 0};
-+
-+		memcpy(&indices[i * 6], temp2, sizeof(GLuint) * 6);
++		memcpy(&indices[i * 6], ((GLuint[]){u + 0, u + 1, u + 2, u + 2, u + 3, u + 0}),
+ 		       sizeof(GLuint) * 6);
  	}
  }
- 
-@@ -672,10 +673,9 @@ _gl_fill(backend_t *base, struct color c
- 	for (int i = 0; i < nrects; i++) {
+@@ -673,8 +673,8 @@ _gl_fill(backend_t *base, struct color c
  		GLint y1 = y_inverted ? height - rect[i].y2 : rect[i].y1,
  		      y2 = y_inverted ? height - rect[i].y1 : rect[i].y2;
--		memcpy(&coord[i * 8],
+ 		memcpy(&coord[i * 8],
 -		       (GLint[][2]){
 -		           {rect[i].x1, y1}, {rect[i].x2, y1}, {rect[i].x2, y2}, {rect[i].x1, y2}},
--		       sizeof(GLint[2]) * 4);
-+		GLint temp1[4][2] = {
-+		{rect[i].x1, y1}, {rect[i].x2, y1}, {rect[i].x2, y2}, {rect[i].x1, y2}};
-+		memcpy(&coord[i * 8], temp1, sizeof(GLint[2]) * 4);
++		       ((GLint[][2]){
++		           {rect[i].x1, y1}, {rect[i].x2, y1}, {rect[i].x2, y2}, {rect[i].x1, y2}}),
+ 		       sizeof(GLint[2]) * 4);
  		indices[i * 6 + 0] = (GLuint)i * 4 + 0;
  		indices[i * 6 + 1] = (GLuint)i * 4 + 1;
- 		indices[i * 6 + 2] = (GLuint)i * 4 + 2;
