dd49e51a98b66ed4e6e528d5fb288857f1932cfa
maya <maya@pkgsrc.org>
2020-05-02 22:24:24 +0000

    firefox: force disable WebGL. It is currently broken unless multiprocess windows are enabled, and we disable them unconditionally.
    Upstream: https://bugzilla.mozilla.org/show_bug.cgi?id=1610395
    
    PKGREVISION++

--- www/firefox/patches/patch-browser_app_profile_firefox.js
+++ www/firefox/patches/patch-browser_app_profile_firefox.js
@@ -1,8 +1,8 @@
-$NetBSD: patch-browser_app_profile_firefox.js,v 1.13 2019/07/11 11:32:40 ryoon Exp $
+$NetBSD: patch-browser_app_profile_firefox.js,v 1.14 2020/05/02 22:24:24 maya Exp $
 
 --- browser/app/profile/firefox.js.orig	2019-07-06 01:48:29.000000000 +0000
 +++ browser/app/profile/firefox.js
-@@ -1865,6 +1865,15 @@ pref("prio.publicKeyB", "26E6674E65425B8
+@@ -1851,6 +1851,17 @@ pref("fission.frontend.simulate-messages
  pref("toolkit.coverage.enabled", false);
  pref("toolkit.coverage.endpoint.base", "https://coverage.mozilla.org");
  
@@ -14,6 +14,8 @@ $NetBSD: patch-browser_app_profile_firefox.js,v 1.13 2019/07/11 11:32:40 ryoon E
 +
 +// Disable multiprocess window support. Workaround for PR 53273.
 +pref("browser.tabs.remote.autostart", false);
++// WebGL broken without multiprocess window: https://bugzilla.mozilla.org/show_bug.cgi?id=1610395
++pref("webgl.disabled", true);
 +
  // Discovery prefs
  pref("browser.discovery.enabled", true);
