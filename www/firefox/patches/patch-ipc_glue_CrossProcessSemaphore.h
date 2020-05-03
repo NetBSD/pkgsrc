194937a5b474f6e4ff306aa762e2242ac018afee
maya <maya@pkgsrc.org>
2020-05-03 18:45:30 +0000

    firefox: limit disbling multiprocess firefox to netbsd-8.
    The patches might be safe to remove altogether (disabling multiprocess
    should be enough), but it's not necessary for netbsd-9 in general, as
    it has working pshared semaphores.
    
    Fixes a lot of issues with Firefox, like WebGL not working.
    PKGREVISION++

--- www/firefox/patches/patch-ipc_glue_CrossProcessSemaphore.h
+++ www/firefox/patches/patch-ipc_glue_CrossProcessSemaphore.h
@@ -1,12 +1,23 @@
-$NetBSD: patch-ipc_glue_CrossProcessSemaphore.h,v 1.2 2018/11/04 00:38:44 ryoon Exp $
+$NetBSD: patch-ipc_glue_CrossProcessSemaphore.h,v 1.3 2020/05/03 18:45:30 maya Exp $
 
 --- ipc/glue/CrossProcessSemaphore.h.orig	2018-10-18 20:06:06.000000000 +0000
 +++ ipc/glue/CrossProcessSemaphore.h
-@@ -93,7 +93,13 @@ private:
+@@ -18,6 +18,10 @@
+ #  include "mozilla/Atomics.h"
+ #endif
+ 
++#ifdef __NetBSD__ // For version check only.
++#include <sys/param.h>
++#endif
++
+ namespace IPC {
+ template <typename T>
+ struct ParamTraits;
+@@ -105,7 +109,13 @@ class CrossProcessSemaphore {
    HANDLE mSemaphore;
  #elif !defined(OS_MACOSX)
    RefPtr<mozilla::ipc::SharedMemoryBasic> mSharedBuffer;
-+#if defined(__NetBSD__)
++#if defined(__NetBSD__) && (__NetBSD_Version__ < 900000000)
 +  pthread_mutex_t* mMutex;
 +  pthread_cond_t* mNotZero;
 +  uint32_t* mValue;
