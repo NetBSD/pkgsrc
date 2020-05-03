e1f0bfb9807a3b06a78e32782a81bc3b80cdfe8a
maya <maya@pkgsrc.org>
2020-05-03 18:45:30 +0000

    firefox: limit disbling multiprocess firefox to netbsd-8.
    The patches might be safe to remove altogether (disabling multiprocess
    should be enough), but it's not necessary for netbsd-9 in general, as
    it has working pshared semaphores.
    
    Fixes a lot of issues with Firefox, like WebGL not working.
    PKGREVISION++

--- www/firefox/patches/patch-browser_app_profile_firefox.js
+++ www/firefox/patches/patch-browser_app_profile_firefox.js
@@ -1,8 +1,8 @@
-$NetBSD: patch-browser_app_profile_firefox.js,v 1.14 2020/05/02 22:24:24 maya Exp $
+$NetBSD: patch-browser_app_profile_firefox.js,v 1.15 2020/05/03 18:45:30 maya Exp $
 
 --- browser/app/profile/firefox.js.orig	2019-07-06 01:48:29.000000000 +0000
 +++ browser/app/profile/firefox.js
-@@ -1851,6 +1851,17 @@ pref("fission.frontend.simulate-messages
+@@ -1851,6 +1851,12 @@ pref("fission.frontend.simulate-messages
  pref("toolkit.coverage.enabled", false);
  pref("toolkit.coverage.endpoint.base", "https://coverage.mozilla.org");
  
@@ -11,11 +11,6 @@ $NetBSD: patch-browser_app_profile_firefox.js,v 1.14 2020/05/02 22:24:24 maya Ex
 +
 +// Enable system addons, for example langpacks from www/firefox-l10n
 +pref("extensions.autoDisableScopes", 11);
-+
-+// Disable multiprocess window support. Workaround for PR 53273.
-+pref("browser.tabs.remote.autostart", false);
-+// WebGL broken without multiprocess window: https://bugzilla.mozilla.org/show_bug.cgi?id=1610395
-+pref("webgl.disabled", true);
 +
  // Discovery prefs
  pref("browser.discovery.enabled", true);
