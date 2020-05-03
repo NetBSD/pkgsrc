d30f45636385c89beb0477b74b9ac39b64e86759
gutteridge <gutteridge@pkgsrc.org>
2020-05-03 20:48:57 +0000

    xfce4-session: handle a Polkit disabling edge case
    Allow Polkit support to be disabled even if Polkit is found in the
    build environment (an atypical use case, indeed, but one I just tripped
    over).

--- x11/xfce4-session/options.mk
+++ x11/xfce4-session/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2018/06/04 10:51:56 youri Exp $
+# $NetBSD: options.mk,v 1.4 2020/05/03 20:48:57 gutteridge Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xfce4-session
 PKG_SUPPORTED_OPTIONS=	polkit
@@ -11,4 +11,8 @@ PLIST_VARS=	polkit
 .if !empty(PKG_OPTIONS:Mpolkit)
 .include "../../security/mate-polkit/buildlink3.mk"
 PLIST.polkit=	yes
+.else
+# For the edge case when Polkit is installed on the system, but isn't
+# intended to be enabled, we must explicitly disable it.
+CONFIGURE_ARGS+=	--disable-polkit
 .endif
