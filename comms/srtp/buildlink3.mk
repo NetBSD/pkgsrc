ecaa2b1bf473be6c42f35448c5165c40bfd3795f
adam <adam@pkgsrc.org>
2020-05-05 17:54:39 +0000

    srtp: updated to 2.3.0
    libsrtp 2.3.0
    Major changes in this release are a fuzzer for libsrtp, NSS as optional crypto back end and cmake support for building. For more details and a complete list of changes please see the CHANGES file.
    
    libsrtp 2.2.0
    First release in the 2.2 series.
    
    The major change with this release is that the all the code has been reformatted to be consistent and this consistency can be enforced with the include .clang-format file. This resulted in a lot of none functional changes but was considered worth it to simplify maintenance in the future. There are numerous other minor fixes, see the CHANGES file for more details.
    
    libsrtp 2.1.0
    First release in the 2.1 series.
    
    libsrtp 2.0.0
    Initial libsrtp 2.0 release.

--- comms/srtp/buildlink3.mk
+++ comms/srtp/buildlink3.mk
@@ -1,12 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/09/06 14:02:08 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/05/05 17:54:39 adam Exp $
 
 BUILDLINK_TREE+=	srtp
 
 .if !defined(SRTP_BUILDLINK3_MK)
 SRTP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.srtp+=	srtp>=1.4.4
+BUILDLINK_API_DEPENDS.srtp+=	srtp>=2.3.0
 BUILDLINK_PKGSRCDIR.srtp?=	../../comms/srtp
+.include "../../security/openssl/buildlink3.mk"
 .endif	# SRTP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-srtp
