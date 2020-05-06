f2df6422af533dcff6be806cbd36fb7446f4bc36
ryoon <ryoon@pkgsrc.org>
2020-05-06 01:42:28 +0000

    inkscape: Update to 1.0
    Changelog:
    Inkscape 1.0
    
    Release highlights
    
        Theming support and more new customization options
        Better HiDPI (high resolution) screen support
        Native support for macOS with a signed and notarized .dmg file
        Coordinate origin in top left corner by default
        Canvas rotation and mirroring
        On-Canvas alignment of objects
        Split view and X-Ray modes
        PowerPencil for drawing editable, variable width strokes with a pressure sensitive graphics tablet
        New PNG export options
        Integrated centerline tracing for vectorization of line drawings
        Searchable Symbols dialog
        New Live Path Effect (LPE) selection dialog
        New Corners (Fillet/chamfer) LPE, (lossless) Boolean Operation LPE (experimental), Offset LPE and Measure Segments LPE (and more!)
        Path operations, deselection of a large number of paths as well as grouping/ungrouping are much faster now
        Much improved text line-height settings
        Variable fonts support (only if compiled with pango library version >= 1.41.1)
        Browser-compatible flowed text
        Extensions programming interface updated, with many new options
        Python 3 support for extensions

--- graphics/inkscape/patches/patch-CMakeScripts_Pod2man.cmake
+++ graphics/inkscape/patches/patch-CMakeScripts_Pod2man.cmake
@@ -1,15 +1,22 @@
-$NetBSD: patch-CMakeScripts_Pod2man.cmake,v 1.2 2017/08/24 09:07:12 jperkin Exp $
+$NetBSD: patch-CMakeScripts_Pod2man.cmake,v 1.3 2020/05/06 01:42:28 ryoon Exp $
 
 Fix man installation path.
 
---- CMakeScripts/Pod2man.cmake.orig	2017-02-08 19:36:52.000000000 +0000
+--- CMakeScripts/Pod2man.cmake.orig	2020-05-01 13:17:41.000000000 +0000
 +++ CMakeScripts/Pod2man.cmake
-@@ -48,7 +48,7 @@ macro(pod2man PODFILE_FULL RELEASE SECTI
-         )
-         install(
-             FILES ${MANFILE_FULL}
--            DESTINATION ${CMAKE_INSTALL_PREFIX}/${SHARE_INSTALL}/man/man${SECTION}
-+            DESTINATION ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_MANDIR}/man${SECTION}
-         )
-     endif()
- endmacro(pod2man PODFILE NAME SECTION CENTER)
+@@ -34,13 +34,13 @@ macro(pod2man PODFILE_FULL RELEASE SECTI
+             set(MANFILE_TEMP "${CMAKE_CURRENT_BINARY_DIR}/${NAME}.${LANG}.tmp")
+             set(MANFILE_FULL "${CMAKE_CURRENT_BINARY_DIR}/${NAME}.${LANG}.${SECTION}")
+             set(MANFILE_FULL_GZ "${CMAKE_CURRENT_BINARY_DIR}/${NAME}.${LANG}.${SECTION}.gz")
+-            set(MANFILE_DEST "${SHARE_INSTALL}/man/${LANG}/man${SECTION}")
++            set(MANFILE_DEST "${CMAKE_INSTALL_MANDIR}/${LANG}/man${SECTION}")
+         else()
+             set(MANPAGE_TARGET "man-${NAME}")
+             set(MANFILE_TEMP "${CMAKE_CURRENT_BINARY_DIR}/${NAME}.tmp")
+             set(MANFILE_FULL "${CMAKE_CURRENT_BINARY_DIR}/${NAME}.${SECTION}")
+             set(MANFILE_FULL_GZ "${CMAKE_CURRENT_BINARY_DIR}/${NAME}.${SECTION}.gz")
+-            set(MANFILE_DEST "${SHARE_INSTALL}/man/man${SECTION}")
++            set(MANFILE_DEST "${CMAKE_INSTALL_MANDIR}/man${SECTION}")
+         endif()
+         add_custom_command(
+             OUTPUT ${MANFILE_TEMP}
