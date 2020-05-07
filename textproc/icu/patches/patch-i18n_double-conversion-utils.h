a8413757b6f1a70f3349a8d9fa95b8cfb319bb71
tsutsui <tsutsui@pkgsrc.org>
2020-05-07 16:01:33 +0000

    icu: fix build error on sh3.  Also remove now duplicated part for ARMEB.

--- textproc/icu/patches/patch-i18n_double-conversion-utils.h
+++ textproc/icu/patches/patch-i18n_double-conversion-utils.h
@@ -1,15 +1,15 @@
-$NetBSD: patch-i18n_double-conversion-utils.h,v 1.5 2019/07/24 17:02:07 rin Exp $
+$NetBSD: patch-i18n_double-conversion-utils.h,v 1.6 2020/05/07 16:01:33 tsutsui Exp $
 
-Add support for ARMEB.
+Add support for sh3.
 
---- i18n/double-conversion-utils.h.orig	2019-07-24 21:04:19.876508903 +0900
-+++ i18n/double-conversion-utils.h	2019-07-24 21:05:44.988164937 +0900
-@@ -92,7 +92,7 @@ int main(int argc, char** argv) {
- //
- // If it prints "correct" then the architecture should be here, in the "correct" section.
- #if defined(_M_X64) || defined(__x86_64__) || \
--    defined(__ARMEL__) || defined(__avr32__) || defined(_M_ARM) || defined(_M_ARM64) || \
-+    defined(__ARMEL__) || defined(__ARMEB__) || defined(__avr32__) || defined(_M_ARM) || defined(_M_ARM64) || \
-     defined(__hppa__) || defined(__ia64__) || \
-     defined(__mips__) || \
+--- i18n/double-conversion-utils.h.orig	2020-03-11 18:16:11.000000000 +0000
++++ i18n/double-conversion-utils.h
+@@ -107,7 +107,7 @@ int main(int argc, char** argv) {
      defined(__powerpc__) || defined(__ppc__) || defined(__ppc64__) || \
+     defined(_POWER) || defined(_ARCH_PPC) || defined(_ARCH_PPC64) || \
+     defined(__sparc__) || defined(__sparc) || defined(__s390__) || \
+-    defined(__SH4__) || defined(__alpha__) || \
++    defined(__SH4__) || defined(__sh__) || defined(__alpha__) || \
+     defined(_MIPS_ARCH_MIPS32R2) || defined(__ARMEB__) ||\
+     defined(__AARCH64EL__) || defined(__aarch64__) || defined(__AARCH64EB__) || \
+     defined(__riscv) || defined(__e2k__) || \
