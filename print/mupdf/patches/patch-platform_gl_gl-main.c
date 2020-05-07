00494e876cf3f08b9ef2d60e8598efe58b593654
leot <leot@pkgsrc.org>
2020-05-07 20:42:56 +0000

    mupdf: Update to 1.17.0
    pkgsrc changes:
     - Remove patches/patch-platform_x11_pdfapp.c, no longer needed
    
    Changes:
    1.17.0
    ------
    api: Improved accessors for markup/ink/polygon annotation data.
    api: Chapter based API for faster EPUB loading.
    api: Add more documentation to header files.
    api: Improved digital signature signing and verification.
    api: Validate changes in a signed PDF file.
    
    build: Moved windows build to VS2019 solution.
    
    pdf: Redaction now works on images and links as well as text.
    pdf: Greek, Cyrillic, Chinese, Japanese, and Korean scripts in forms and annotations.
    pdf: File attachment annotations.
    pdf: Use CCITT Fax compression for 1-bit images when creating PDF files.
    
    epub: More forgiving XHTML parsing.
    epub: Accelerator files to cache chapter data for faster EPUB loading.
    epub: Optimized memory use.
    
    mutool run: Edit Markup, Ink, and Polygon annotation data.
    mutool run: Fill out form fields.
    viewer: Ask for confirmation before closing a PDF with unsaved changes.
    viewer: Embed and extract file attachment annotations.
    
    library: Miscellaneous bugfixes.
    library: Fixes to store operation.
    
    thirdparty: Updated Harfbuzz to 2.6.4.
    thirdparty: Updated jbig2dec to 0.18.
    thirdparty: Updated MuJS to 1.0.7.
    thirdparty: Updated OpenJPEG to 2.3.1.
    thirdparty: Updated cURL to 7.66.0 (windows only).

--- print/mupdf/patches/patch-platform_gl_gl-main.c
+++ print/mupdf/patches/patch-platform_gl_gl-main.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-platform_gl_gl-main.c,v 1.2 2018/10/04 19:02:33 leot Exp $
+$NetBSD: patch-platform_gl_gl-main.c,v 1.3 2020/05/07 20:42:57 leot Exp $
 
 glutLeaveMainLoop() is present only on freeglut, define it otherwise.
 
---- platform/gl/gl-main.c.orig	2018-10-03 11:29:14.000000000 +0000
+--- platform/gl/gl-main.c.orig	2020-05-05 11:29:09.000000000 +0000
 +++ platform/gl/gl-main.c
-@@ -16,7 +16,7 @@
- char *realpath(const char *path, char *resolved_path); /* in gl-file.c */
+@@ -26,7 +26,7 @@
+ #include <direct.h> /* for getcwd */
  #endif
  
 -#ifdef __APPLE__
