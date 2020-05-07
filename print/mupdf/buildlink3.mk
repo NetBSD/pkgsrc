72d9d36e2860cc98f4b39a570439d0d734840c9f
leot <leot@pkgsrc.org>
2020-05-07 20:42:56 +0000

    mupdf: Update to 1.17.0
    pkgsrc changes:
     - Remove patches/patch-platform_x11_pdfapp.c, no longer needed
    
    Changes:
    1.17.0
    ------
    api: Improved accessors for markup/ink/polygon annotation data.
    api: Chapter based API for faster EPUB loading.
    api: Add more documentation to header files.
    api: Improved digital signature signing and verification.
    api: Validate changes in a signed PDF file.
    
    build: Moved windows build to VS2019 solution.
    
    pdf: Redaction now works on images and links as well as text.
    pdf: Greek, Cyrillic, Chinese, Japanese, and Korean scripts in forms and annotations.
    pdf: File attachment annotations.
    pdf: Use CCITT Fax compression for 1-bit images when creating PDF files.
    
    epub: More forgiving XHTML parsing.
    epub: Accelerator files to cache chapter data for faster EPUB loading.
    epub: Optimized memory use.
    
    mutool run: Edit Markup, Ink, and Polygon annotation data.
    mutool run: Fill out form fields.
    viewer: Ask for confirmation before closing a PDF with unsaved changes.
    viewer: Embed and extract file attachment annotations.
    
    library: Miscellaneous bugfixes.
    library: Fixes to store operation.
    
    thirdparty: Updated Harfbuzz to 2.6.4.
    thirdparty: Updated jbig2dec to 0.18.
    thirdparty: Updated MuJS to 1.0.7.
    thirdparty: Updated OpenJPEG to 2.3.1.
    thirdparty: Updated cURL to 7.66.0 (windows only).

--- print/mupdf/buildlink3.mk
+++ print/mupdf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.37 2020/05/06 14:04:12 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.38 2020/05/07 20:42:56 leot Exp $
 
 BUILDLINK_TREE+=	mupdf
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	mupdf
 MUPDF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mupdf+=	mupdf>=1.14.0
-BUILDLINK_ABI_DEPENDS.mupdf?=	mupdf>=1.16.1nb9
+BUILDLINK_ABI_DEPENDS.mupdf?=	mupdf>=1.17.0
 BUILDLINK_PKGSRCDIR.mupdf?=	../../print/mupdf
 
 pkgbase := mupdf
--- print/mupdf/patches/patch-platform_gl_gl-app.h
+++ print/mupdf/patches/patch-platform_gl_gl-app.h
@@ -1,20 +1,19 @@
-$NetBSD: patch-platform_gl_gl-app.h,v 1.3 2019/09/23 12:17:04 nia Exp $
+$NetBSD: patch-platform_gl_gl-app.h,v 1.4 2020/05/07 20:42:57 leot Exp $
 
 - Do not force freeglut GLUT implementation to every non-APPLE platforms.
 - Adjust the glut.h include.
 
---- platform/gl/gl-app.h.orig	2018-09-25 12:39:17.000000000 +0000
+--- platform/gl/gl-app.h.orig	2020-05-05 11:29:09.000000000 +0000
 +++ platform/gl/gl-app.h
 @@ -7,11 +7,7 @@ void win_install(void);
  #include "mupdf/ucdn.h"
  #include "mupdf/pdf.h" /* for pdf specifics and forms */
  
 -#ifndef __APPLE__
--#include <GL/freeglut.h>
+ #include <GL/freeglut.h>
 -#else
 -#include <GLUT/glut.h>
 -#endif
-+#include <GL/freeglut.h>
  
  /* UI */
  
