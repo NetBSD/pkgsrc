2f1b140114d856c80908d0fd9d691d8a6834e997
leot <leot@pkgsrc.org>
2020-05-07 20:42:56 +0000

    mupdf: Update to 1.17.0
    pkgsrc changes:
     - Remove patches/patch-platform_x11_pdfapp.c, no longer needed
    
    Changes:
    1.17.0
    ------
    api: Improved accessors for markup/ink/polygon annotation data.
    api: Chapter based API for faster EPUB loading.
    api: Add more documentation to header files.
    api: Improved digital signature signing and verification.
    api: Validate changes in a signed PDF file.
    
    build: Moved windows build to VS2019 solution.
    
    pdf: Redaction now works on images and links as well as text.
    pdf: Greek, Cyrillic, Chinese, Japanese, and Korean scripts in forms and annotations.
    pdf: File attachment annotations.
    pdf: Use CCITT Fax compression for 1-bit images when creating PDF files.
    
    epub: More forgiving XHTML parsing.
    epub: Accelerator files to cache chapter data for faster EPUB loading.
    epub: Optimized memory use.
    
    mutool run: Edit Markup, Ink, and Polygon annotation data.
    mutool run: Fill out form fields.
    viewer: Ask for confirmation before closing a PDF with unsaved changes.
    viewer: Embed and extract file attachment annotations.
    
    library: Miscellaneous bugfixes.
    library: Fixes to store operation.
    
    thirdparty: Updated Harfbuzz to 2.6.4.
    thirdparty: Updated jbig2dec to 0.18.
    thirdparty: Updated MuJS to 1.0.7.
    thirdparty: Updated OpenJPEG to 2.3.1.
    thirdparty: Updated cURL to 7.66.0 (windows only).

--- print/mupdf/patches/patch-source_fitz_load-jpx.c
+++ print/mupdf/patches/patch-source_fitz_load-jpx.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-source_fitz_load-jpx.c,v 1.9 2018/10/04 19:02:33 leot Exp $
+$NetBSD: patch-source_fitz_load-jpx.c,v 1.10 2020/05/07 20:42:57 leot Exp $
 
 - MuPDF does some locking around its allocation calls; it overrides openjpeg's
   allocators to do this locking too. However mupdf tries to manually align things
@@ -13,14 +13,14 @@ $NetBSD: patch-source_fitz_load-jpx.c,v 1.9 2018/10/04 19:02:33 leot Exp $
 
   From OpenBSD ports textproc/mupdf/patch-source_fitz_load-jpx_c,v 1.5.
 
---- source/fitz/load-jpx.c.orig	2018-09-25 12:39:17.000000000 +0000
+--- source/fitz/load-jpx.c.orig	2020-05-05 11:29:09.000000000 +0000
 +++ source/fitz/load-jpx.c
-@@ -577,19 +577,7 @@ void opj_free(void *ptr)
+@@ -584,19 +584,7 @@ void opj_free(void *ptr)
  
  static void * opj_aligned_malloc_n(size_t alignment, size_t size)
  {
 -	uint8_t *ptr;
--	int off;
+-	size_t off;
 -
 -	if (size == 0)
 -		return NULL;
@@ -30,13 +30,13 @@ $NetBSD: patch-source_fitz_load-jpx.c,v 1.9 2018/10/04 19:02:33 leot Exp $
 -	if (ptr == NULL)
 -		return NULL;
 -	off = alignment-(((int)(intptr_t)ptr) & (alignment - 1));
--	ptr[off-1] = off;
+-	ptr[off-1] = (uint8_t)off;
 -	return ptr + off;
 +	return opj_malloc(size);
  }
  
  void * opj_aligned_malloc(size_t size)
-@@ -604,13 +592,7 @@ void * opj_aligned_32_malloc(size_t size
+@@ -611,13 +599,7 @@ void * opj_aligned_32_malloc(size_t size
  
  void opj_aligned_free(void* ptr_)
  {
