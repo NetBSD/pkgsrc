1e3469f651a6e5889a9ba5b53044f661f479abd4
skrll <skrll@pkgsrc.org>
2020-05-10 11:33:23 +0000

    Two fixes - make qemu on aarch64 work (needs fixes in base OS) - don't assert hppa lasi imr, but simply store the know interrupt bits
    bump PKGREVISION

--- emulators/qemu/patches/patch-accel_tcg_user-exec.c
+++ emulators/qemu/patches/patch-accel_tcg_user-exec.c
@@ -1,11 +1,11 @@
-$NetBSD: patch-accel_tcg_user-exec.c,v 1.1 2019/07/03 14:38:13 thorpej Exp $
+$NetBSD: patch-accel_tcg_user-exec.c,v 1.2 2020/05/10 11:33:23 skrll Exp $
 
 Fix building on NetBSD/arm by extracting the FSR value from the
 correct siginfo_t field.
 
---- accel/tcg/user-exec.c.orig	2019-07-03 13:09:53.530028621 +0000
-+++ accel/tcg/user-exec.c	2019-07-03 13:22:13.179596536 +0000
-@@ -448,6 +448,7 @@ int cpu_signal_handler(int host_signum, 
+--- accel/tcg/user-exec.c.orig	2020-04-28 16:49:24.000000000 +0000
++++ accel/tcg/user-exec.c
+@@ -492,6 +492,7 @@ int cpu_signal_handler(int host_signum, 
  
  #if defined(__NetBSD__)
  #include <ucontext.h>
@@ -13,7 +13,7 @@ correct siginfo_t field.
  #endif
  
  int cpu_signal_handler(int host_signum, void *pinfo,
-@@ -456,6 +457,7 @@ int cpu_signal_handler(int host_signum, 
+@@ -500,6 +501,7 @@ int cpu_signal_handler(int host_signum, 
      siginfo_t *info = pinfo;
  #if defined(__NetBSD__)
      ucontext_t *uc = puc;
@@ -21,7 +21,7 @@ correct siginfo_t field.
  #else
      ucontext_t *uc = puc;
  #endif
-@@ -470,10 +472,18 @@ int cpu_signal_handler(int host_signum, 
+@@ -514,15 +516,47 @@ int cpu_signal_handler(int host_signum, 
      pc = uc->uc_mcontext.arm_pc;
  #endif
  
@@ -39,4 +39,41 @@ correct siginfo_t field.
 +#endif
      return handle_cpu_signal(pc, info, is_write, &uc->uc_sigmask);
  }
+ 
+ #elif defined(__aarch64__)
+ 
++#if defined(__NetBSD__)
++
++#include <ucontext.h>
++#include <sys/siginfo.h>
++
++int cpu_signal_handler(int host_signum, void *pinfo, void *puc)
++{
++    ucontext_t *uc = puc;
++    siginfo_t *si = pinfo;
++    unsigned long pc;
++    int is_write;
++    uint32_t esr;
++
++    pc = uc->uc_mcontext.__gregs[_REG_PC];
++    esr = si->si_trap;
++
++    /* siginfo_t::si_trap is the ESR value, for data aborts ESR.EC
++     * is 0b10010x: then bit 6 is the WnR bit
++     */
++    is_write = extract32(esr, 27, 5) == 0x12 && extract32(esr, 6, 1) == 1;
++    return handle_cpu_signal(pc, si, is_write, &uc->uc_sigmask);
++}
++
++#else
+ #ifndef ESR_MAGIC
+ /* Pre-3.16 kernel headers don't have these, so provide fallback definitions */
+ #define ESR_MAGIC 0x45535201
+@@ -585,6 +619,7 @@ int cpu_signal_handler(int host_signum, 
+     }
+     return handle_cpu_signal(pc, info, is_write, &uc->uc_sigmask);
+ }
++#endif
+ 
+ #elif defined(__s390__)
  
