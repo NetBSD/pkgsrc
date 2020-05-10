2cf327a82040451a12c2ab6abc25b3a191cddc37
rillig <rillig@pkgsrc.org>
2020-05-10 10:17:05 +0000

    lang/quickjs: update to 2020-04-12
    Changes since 2020-01-19:
    
    2020-04-12:
    
    - added cross realm support
    - added AggregateError and Promise.any
    - added env, uid and gid options in os.exec()
    - misc bug fixes
    
    2020-03-16:
    
    - reworked error handling in std and os libraries: suppressed I/O
      exceptions in std FILE functions and return a positive errno value
      when it is explicit
    - output exception messages to stderr
    - added std.loadFile(), std.strerror(), std.FILE.prototype.tello()
    - added JS_GetRuntimeOpaque(), JS_SetRuntimeOpaque(), JS_NewUint32()
    - updated to Unicode 13.0.0
    - misc bug fixes

--- lang/quickjs/patches/patch-quickjs-libc.c
+++ lang/quickjs/patches/patch-quickjs-libc.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-quickjs-libc.c,v 1.1 2019/11/14 21:20:04 rillig Exp $
+$NetBSD: patch-quickjs-libc.c,v 1.2 2020/05/10 10:17:06 rillig Exp $
 
 Portability patch for NetBSD.
 
---- quickjs-libc.c.orig	2019-10-27 10:55:35.000000000 +0000
+--- quickjs-libc.c.orig	2020-04-12 13:18:13.000000000 +0000
 +++ quickjs-libc.c
-@@ -1652,7 +1652,7 @@ static void os_signal_handler(int sig_nu
+@@ -1712,7 +1712,7 @@ static void os_signal_handler(int sig_nu
      os_pending_signals |= ((uint64_t)1 << sig_num);
  }
  
@@ -13,3 +13,12 @@ Portability patch for NetBSD.
  typedef void (*sighandler_t)(int sig_num);
  #endif
  
+@@ -2442,6 +2442,8 @@ static int my_execvpe(const char *filena
+     return -1;
+ }
+ 
++extern char **environ; /* Needed at least for NetBSD-8.0-x86_64. */
++
+ /* exec(args[, options]) -> exitcode */
+ static JSValue js_os_exec(JSContext *ctx, JSValueConst this_val,
+                           int argc, JSValueConst *argv)
