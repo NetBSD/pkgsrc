5f5d542895cb46f15e4da15eb20fc7db84afa039
adam <adam@pkgsrc.org>
2020-05-12 18:40:35 +0000

    py-onionbalance: updated to 0.2.0
    0.2.0
    Allow migration from Tor to Onionbalance by reading tor private keys directly using the ‘key’ directive in the YAML config file. Also update onionbalance-config to support that.
    Improve onionbalance-config for v3 onions. Simplify the output directory (and change docs to reflect so) and the wizard suggestions.
    
    0.1.9
    Initial support for v3 onions!

--- net/py-onionbalance/patches/patch-setup.py
+++ net/py-onionbalance/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.1 2019/09/19 21:25:52 adam Exp $
+$NetBSD: patch-setup.py,v 1.2 2020/05/12 18:40:35 adam Exp $
 
 Replace pycrypto with pycryptodome.
 
---- setup.py.orig	2019-09-19 21:01:51.000000000 +0000
+--- setup.py.orig	2020-04-14 22:49:04.000000000 +0000
 +++ setup.py
-@@ -43,7 +43,7 @@ setup(
+@@ -46,7 +46,7 @@ setup(
          'setuptools',
-         'stem>=1.4.0-dev',
-         'PyYAML>=3.11',
+         'stem>=1.8',
+         'PyYAML>=4.2b1',
 -        'pycrypto>=2.6.1',
 +        'pycryptodome',
          'future>=0.14.0',
          'setproctitle',
-         ],
+         'cryptography>=2.5',
