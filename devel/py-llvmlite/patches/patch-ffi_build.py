3a97ee40f3853888b7f948e63c1a4369caedcddb
adam <adam@pkgsrc.org>
2020-05-12 08:08:08 +0000

    py-llvmlite: updated to 0.32.1
    v0.32.1:
    Trove classifiers may be out of date.
    Add FAQ entry on LLVM version support.
    Adds override for LLVM version check, re-formats docs.

--- devel/py-llvmlite/patches/patch-ffi_build.py
+++ devel/py-llvmlite/patches/patch-ffi_build.py
@@ -1,20 +1,10 @@
-$NetBSD: patch-ffi_build.py,v 1.6 2020/04/18 08:11:53 adam Exp $
+$NetBSD: patch-ffi_build.py,v 1.7 2020/05/12 08:08:08 adam Exp $
 
-Allow building with LLVM 10.0.x and 9.0.x.
 Add NetBSD support.
 
---- ffi/build.py.orig	2020-04-17 14:49:06.000000000 +0000
+--- ffi/build.py.orig	2020-05-08 14:22:24.000000000 +0000
 +++ ffi/build.py
-@@ -110,7 +110,7 @@ def main_posix(kind, library_ext):
- 
-     out = out.decode('latin1')
-     print(out)
--    if not (out.startswith('8.0.') or out.startswith('7.0.')
-+    if not (out.startswith('10.0.') or out.startswith('9.0.') or out.startswith('8.0.') or out.startswith('7.0.')
-             or out.startswith('7.1.')):
-         msg = (
-             "Building llvmlite requires LLVM 7.0.x, 7.1.x or 8.0.x, got {!r}. "
-@@ -158,6 +158,8 @@ def main():
+@@ -182,6 +182,8 @@ def main():
          main_posix('linux', '.so')
      elif sys.platform.startswith(('freebsd','openbsd')):
          main_posix('freebsd', '.so')
