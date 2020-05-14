7481e75f8e4d5eba83a9ba5f92f18555b6818a78
joerg <joerg@pkgsrc.org>
2020-05-14 19:35:59 +0000

    Consistently build as C++11 source, but ignore narrow issues. Don't depend on ordering of pointers relative to zero. Avoid UDL.

---
+++ emulators/libretro-mame2010/patches/patch-src_emu_romload.c
@@ -0,0 +1,20 @@
+$NetBSD: patch-src_emu_romload.c,v 1.1 2020/05/14 19:35:59 joerg Exp $
+
+--- src/emu/romload.c.orig	2020-05-13 15:21:49.756367021 +0000
++++ src/emu/romload.c
+@@ -586,13 +586,13 @@ static void display_rom_load_results(rom
+ 	{
+ 		/* create the error message and exit fatally */
+ 		mame_printf_error("%s", romdata->errorstring.cstr());
+-		fatalerror_exitcode(romdata->machine, MAMERR_MISSING_FILES, "ERROR: required files are missing, the "GAMENOUN" cannot be run.");
++		fatalerror_exitcode(romdata->machine, MAMERR_MISSING_FILES, "ERROR: required files are missing, the " GAMENOUN " cannot be run.");
+ 	}
+ 
+ 	/* if we had warnings, output them, but continue */
+ 	if (romdata->warnings)
+ 	{
+-		romdata->errorstring.cat("WARNING: the "GAMENOUN" might not run correctly.");
++		romdata->errorstring.cat("WARNING: the " GAMENOUN " might not run correctly.");
+ 		mame_printf_warning("%s\n", romdata->errorstring.cstr());
+ 	}
+ }
---
+++ emulators/libretro-mame2010/patches/patch-src_emu_state.h
@@ -0,0 +1,21 @@
+$NetBSD: patch-src_emu_state.h,v 1.1 2020/05/14 19:35:59 joerg Exp $
+
+--- src/emu/state.h.orig	2020-05-13 15:02:59.769613659 +0000
++++ src/emu/state.h
+@@ -18,16 +18,8 @@
+ #ifndef __STATE_H__
+ #define __STATE_H__
+ 
+-#if defined(__MACH__) && !defined(IOS)
+ #include <type_traits>
+ #define DEF_NAMESPACE std
+-#elif defined(IOS)
+-#include <tr1/type_traits>
+-#define DEF_NAMESPACE std::tr1
+-#elif defined(__GNUC__)
+-#include <tr1/type_traits>
+-#define DEF_NAMESPACE std::tr1
+-#endif
+ 
+ 
+ /***************************************************************************
