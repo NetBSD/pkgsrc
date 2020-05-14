29790cd9f2d954b7090c316eb5eefe07ae6e96d5
joerg <joerg@pkgsrc.org>
2020-05-14 19:27:41 +0000

    Use smaller guns than "using namespace std" which creates conflicts with more modern STL implementations.

--- graphics/impress/patches/patch-font3d_build.h
+++ graphics/impress/patches/patch-font3d_build.h
@@ -1,15 +1,13 @@
-$NetBSD: patch-font3d_build.h,v 1.1 2011/11/25 21:57:36 joerg Exp $
+$NetBSD: patch-font3d_build.h,v 1.2 2020/05/14 19:27:41 joerg Exp $
 
---- font3d/build.h.orig	2011-11-25 18:27:44.000000000 +0000
+--- font3d/build.h.orig	2000-07-16 02:37:31.000000000 +0000
 +++ font3d/build.h
-@@ -31,7 +31,9 @@
+@@ -31,7 +31,7 @@
  #ifndef __BUILD_H__
  #define __BUILD_H__
  
 -#include <iostream.h>
 +#include <iostream>
-+
-+using namespace std;
  
  #include "truetype.h"
  #include "geometry.h"
