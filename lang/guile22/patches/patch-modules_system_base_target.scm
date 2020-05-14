bbcb9996b89de1b351ec04bf06b678bb5dcb1f49
nikita <nikita@pkgsrc.org>
2020-05-14 15:36:40 +0000

    guile22: include bugticket in patch

--- lang/guile22/patches/patch-modules_system_base_target.scm
+++ lang/guile22/patches/patch-modules_system_base_target.scm
@@ -1,4 +1,4 @@
-$NetBSD: patch-modules_system_base_target.scm,v 1.1 2020/05/14 14:28:04 nikita Exp $
+$NetBSD: patch-modules_system_base_target.scm,v 1.2 2020/05/14 15:36:40 nikita Exp $
 
 Crude patch to make target.scm conform to vendor field of gnu-tripplet
 being optional.
@@ -6,6 +6,8 @@ being optional.
 Tested with GnuTLS Guile bindings, where the current implementation
 prevents building on any platform which isn't conforming to having a
 vendor part.
+
+Full explanation: https://gitlab.com/gnutls/gnutls/-/issues/996
 
 --- module/system/base/target.scm.orig  2018-06-18 14:14:47.000000000 +0200
 +++ module/system/base/target.scm       2020-05-14 15:51:14.664581289 +0200
