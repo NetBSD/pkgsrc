2c6b433387d2a617f41a18c47110d674e244cd7a
nia <nia@pkgsrc.org>
2020-05-14 14:44:17 +0000

    SDL: fix CVE-2019-13616
    bump PKGREVISION

--- devel/SDL/patches/patch-src_video_SDL__bmp.c
+++ devel/SDL/patches/patch-src_video_SDL__bmp.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_video_SDL__bmp.c,v 1.1 2019/07/21 11:14:38 nia Exp $
+$NetBSD: patch-src_video_SDL__bmp.c,v 1.2 2020/05/14 14:44:17 nia Exp $
 
 CVE-2019-7635: Reject BMP images with pixel colors out the palette
 From upstream hg 12831:f1f5878be5db
@@ -6,12 +6,27 @@ From upstream hg 12831:f1f5878be5db
 CVE-2019-7636, CVE-2019-7638
 From upstream hg 12612:07c39cbbeacf
 
+CVE-2019-13616: validate image size when loading BMP files
+From upstream hg 12960:ad1bbfbca760
+
 Reject 2, 3, 5, 6, 7-bpp BMP images
 From upstreah hg 12646:4646533663ae
 
 --- src/video/SDL_bmp.c.orig	2012-01-19 06:30:06.000000000 +0000
 +++ src/video/SDL_bmp.c
-@@ -163,6 +163,14 @@ SDL_Surface * SDL_LoadBMP_RW (SDL_RWops 
+@@ -143,6 +143,11 @@ SDL_Surface * SDL_LoadBMP_RW (SDL_RWops 
+ 	(void) biYPelsPerMeter;
+ 	(void) biClrImportant;
+ 
++	if (biWidth <= 0 || biHeight == 0) {
++		SDL_SetError("BMP file with bad dimensions (%dx%d)", biWidth, biHeight);
++		was_error = SDL_TRUE;
++		goto done;
++	}
+ 	if (biHeight < 0) {
+ 		topDown = SDL_TRUE;
+ 		biHeight = -biHeight;
+@@ -163,6 +168,14 @@ SDL_Surface * SDL_LoadBMP_RW (SDL_RWops 
  			ExpandBMP = biBitCount;
  			biBitCount = 8;
  			break;
@@ -26,7 +41,7 @@ From upstreah hg 12646:4646533663ae
  		default:
  			ExpandBMP = 0;
  			break;
-@@ -233,6 +241,10 @@ SDL_Surface * SDL_LoadBMP_RW (SDL_RWops 
+@@ -233,6 +246,10 @@ SDL_Surface * SDL_LoadBMP_RW (SDL_RWops 
  	if ( palette ) {
  		if ( biClrUsed == 0 ) {
  			biClrUsed = 1 << biBitCount;
@@ -37,7 +52,7 @@ From upstreah hg 12646:4646533663ae
  		}
  		if ( biSize == 12 ) {
  			for ( i = 0; i < (int)biClrUsed; ++i ) {
-@@ -296,6 +308,12 @@ SDL_Surface * SDL_LoadBMP_RW (SDL_RWops 
+@@ -296,6 +313,12 @@ SDL_Surface * SDL_LoadBMP_RW (SDL_RWops 
  				}
  				*(bits+i) = (pixel>>shift);
  				pixel <<= ExpandBMP;
@@ -50,7 +65,7 @@ From upstreah hg 12646:4646533663ae
  			} }
  			break;
  
-@@ -306,6 +324,16 @@ SDL_Surface * SDL_LoadBMP_RW (SDL_RWops 
+@@ -306,6 +329,16 @@ SDL_Surface * SDL_LoadBMP_RW (SDL_RWops 
  				was_error = SDL_TRUE;
  				goto done;
  			}
