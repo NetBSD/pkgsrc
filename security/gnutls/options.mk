9fae6e28cc5d35273d477045cf43ec2715707ed4
nikita <nikita@pkgsrc.org>
2020-05-14 14:30:02 +0000

    security/gnutls: revbump, add support for building guile bindings

--- security/gnutls/options.mk
+++ security/gnutls/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.2 2019/10/04 17:25:53 nia Exp $
+# $NetBSD: options.mk,v 1.3 2020/05/14 14:30:02 nikita Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnutls
-PKG_SUPPORTED_OPTIONS=	dane
+PKG_SUPPORTED_OPTIONS=	dane guile
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,4 +11,12 @@ CONFIGURE_ARGS+=	--enable-libdane
 PLIST_SRC+=		PLIST.dane
 .else
 CONFIGURE_ARGS+=	--disable-libdane
+.endif
+
+.if !empty(PKG_OPTIONS:Mguile)
+.include "../../lang/guile22/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-guile
+PLIST_SRC+=		PLIST.guile
+.else
+CONFIGURE_ARGS+=	--disable-guile
 .endif
