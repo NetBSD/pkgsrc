6bd76702c8ec90e12a2dda94da011bf6a9839753
joerg <joerg@pkgsrc.org>
2020-05-14 19:09:09 +0000

    Just use C++11 std::shared_ptr instead of boost variant as dependencies do so anyway.

--- databases/libcassandra/patches/patch-libcassandra_keyspace__factory.cc
+++ databases/libcassandra/patches/patch-libcassandra_keyspace__factory.cc
@@ -1,28 +1,19 @@
-$NetBSD: patch-libcassandra_keyspace__factory.cc,v 1.1 2013/05/30 15:36:29 joerg Exp $
+$NetBSD: patch-libcassandra_keyspace__factory.cc,v 1.2 2020/05/14 19:09:10 joerg Exp $
 
---- libcassandra/keyspace_factory.cc.orig	2013-05-30 11:03:36.000000000 +0000
+--- libcassandra/keyspace_factory.cc.orig	2010-10-10 00:59:23.000000000 +0000
 +++ libcassandra/keyspace_factory.cc
-@@ -34,6 +34,16 @@ KeyspaceFactory::KeyspaceFactory() {}
+@@ -34,12 +34,11 @@ KeyspaceFactory::KeyspaceFactory() {}
  KeyspaceFactory::~KeyspaceFactory() {}
  
  
-+#if defined(_LIBCPP_VERSION) || __cplusplus >= 201103L
+-tr1::shared_ptr<Keyspace> KeyspaceFactory::create(Cassandra *client,
 +std::shared_ptr<Keyspace> KeyspaceFactory::create(Cassandra *client,
-+                                                  const string &name,
-+                                                  const map< string, map<string, string> > &descrip,
-+                                                  ConsistencyLevel level)
-+{
-+  std::shared_ptr<Keyspace> ret(new Keyspace(client, name, descrip, level));
-+  return ret;
-+}
-+#else
- tr1::shared_ptr<Keyspace> KeyspaceFactory::create(Cassandra *client,
                                                    const string &name,
                                                    const map< string, map<string, string> > &descrip,
-@@ -42,4 +52,4 @@ tr1::shared_ptr<Keyspace> KeyspaceFactor
-   tr1::shared_ptr<Keyspace> ret(new Keyspace(client, name, descrip, level));
+                                                   ConsistencyLevel level)
+ {
+-  tr1::shared_ptr<Keyspace> ret(new Keyspace(client, name, descrip, level));
++  std::shared_ptr<Keyspace> ret(new Keyspace(client, name, descrip, level));
    return ret;
  }
 -
-+#endif
-\ No newline at end of file
--- databases/libcassandra/patches/patch-libgenthrift_Cassandra.cpp
+++ databases/libcassandra/patches/patch-libgenthrift_Cassandra.cpp
@@ -1,13 +1,13 @@
-$NetBSD: patch-libgenthrift_Cassandra.cpp,v 1.1 2011/11/23 15:39:21 joerg Exp $
+$NetBSD: patch-libgenthrift_Cassandra.cpp,v 1.2 2020/05/14 19:09:10 joerg Exp $
 
---- libgenthrift/Cassandra.cpp.orig	2011-11-23 14:40:36.000000000 +0000
+--- libgenthrift/Cassandra.cpp.orig	2010-10-10 00:59:23.000000000 +0000
 +++ libgenthrift/Cassandra.cpp
 @@ -6761,7 +6761,8 @@ void CassandraClient::recv_describe_spli
    throw ::apache::thrift::TApplicationException(::apache::thrift::TApplicationException::MISSING_RESULT, "describe_splits failed: unknown result");
  }
  
 -bool CassandraProcessor::process(boost::shared_ptr< ::apache::thrift::protocol::TProtocol> piprot, boost::shared_ptr< ::apache::thrift::protocol::TProtocol> poprot) {
-+bool CassandraProcessor::process(boost::shared_ptr< ::apache::thrift::protocol::TProtocol> piprot, boost::shared_ptr< ::apache::thrift::protocol::TProtocol> poprot, void* connectionContext) {
++bool CassandraProcessor::process(std::shared_ptr< ::apache::thrift::protocol::TProtocol> piprot, std::shared_ptr< ::apache::thrift::protocol::TProtocol> poprot, void* connectionContext) {
 +  (void)&connectionContext;
  
    ::apache::thrift::protocol::TProtocol* iprot = piprot.get();
