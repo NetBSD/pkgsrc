8b353ca8f6b0d1e48b8a92567518a04c99311ed3
taca <taca@pkgsrc.org>
2020-05-14 14:24:53 +0000

    lang/php74: update to 7.4.6
    Update php74 to 7.4.6 (PHP 7.4.6).
    
    
    14 May 2020, PHP 7.4.6
    
    - Core:
      . Fixed bug #78434 (Generator yields no items after valid() call). (Nikita)
      . Fixed bug #79477 (casting object into array creates references). (Nikita)
      . Fixed bug #79514 (Memory leaks while including unexistent file). (cmb,
        Nikita)
      . Fixed bug #79470 (PHP incompatible with 3rd party file system on demand).
        (cmb)
      . Fixed bug #78784 (Unable to interact with files inside a VFS for Git
        repository). (cmb)
      . Fixed bug #78875 (Long variables cause OOM and temp files are not cleaned).
        (cmb) (CVE-2019-11048)
      . Fixed bug #78876 (Long variables cause OOM and temp files are not cleaned).
        (cmb) (CVE-2019-11048)
    
    - DOM:
      . Fixed bug #78221 (DOMNode::normalize() doesn't remove empty text nodes).
        (cmb)
    
    - EXIF:
      . Fixed bug #79336 (ext/exif/tests/bug79046.phpt fails on Big endian arch).
        (Nikita)
    
    - FCGI:
      . Fixed bug #79491 (Search for .user.ini extends up to root dir). (cmb)
    
    - MBString:
      . Fixed bug #79441 (Segfault in mb_chr() if internal encoding is unsupported).
        (Girgias)
    
    - OpenSSL:
      . Fixed bug #79497 (stream_socket_client() throws an unknown error sometimes
        with <1s timeout). (Joe Cai)
    
    - PCRE:
      . Upgraded to PCRE2 10.34. (cmb)
    
    - Phar:
      . Fixed bug #79503 (Memory leak on duplicate metadata). (cmb)
    
    - SimpleXML:
      . Fixed bug #79528 (Different object of the same xml between 7.4.5 and
        7.4.4). (cmb)
    
    - SPL:
      . Fixed bug #69264 (__debugInfo() ignored while extending SPL classes). (cmb)
      . Fixed bug #67369 (ArrayObject serialization drops the iterator class).
        (Alex Dowad)
    
    - Standard:
      . Fixed bug #79468 (SIGSEGV when closing stream handle with a stream filter
        appended). (dinosaur)
      . Fixed bug #79447 (Serializing uninitialized typed properties with __sleep
        should not throw). (nicolas-grekas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.296 2020/05/14 14:23:08 taca Exp $
+# $NetBSD: phpversion.mk,v 1.297 2020/05/14 14:24:53 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP72_VERSION=	7.2.30
 PHP73_VERSION=	7.3.18
-PHP74_VERSION=	7.4.5
+PHP74_VERSION=	7.4.6
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
