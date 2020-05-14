ed9ec26703944092e14a967d0ffdaa04ea228d12
joerg <joerg@pkgsrc.org>
2020-05-14 19:08:06 +0000

    Fix constness of strchr. Fix format string. Bump revision.

--- devel/librlog/patches/patch-rlog-RLogChannel.cpp
+++ devel/librlog/patches/patch-rlog-RLogChannel.cpp
@@ -1,6 +1,8 @@
-$NetBSD: patch-rlog-RLogChannel.cpp,v 1.1 2011/11/22 18:06:16 joerg Exp $
+$NetBSD: patch-rlog-RLogChannel.cpp,v 1.2 2020/05/14 19:08:07 joerg Exp $
 
---- rlog/RLogChannel.cpp.orig	2011-11-22 17:34:38.000000000 +0000
+Fix strchr use.
+
+--- rlog/RLogChannel.cpp.orig	2005-06-02 12:25:55.000000000 +0000
 +++ rlog/RLogChannel.cpp
 @@ -17,6 +17,7 @@
   */
@@ -10,3 +12,12 @@ $NetBSD: patch-rlog-RLogChannel.cpp,v 1.1 2011/11/22 18:06:16 joerg Exp $
  #include "RLogChannel.h"
  #include "rlog.h"
  #include "Lock.h"
+@@ -183,7 +184,7 @@ RLogChannel *rlog::GetComponentChannel( 
+ 	if((current->logLevel() == Log_Undef) && (level != Log_Undef))
+ 	    current->setLogLevel( level );
+ 
+-	char *next = strchr( path , '/' );
++	const char *next = strchr( path , '/' );
+ 	size_t len = next ? next - path : strlen( path );
+ 
+ 	if(len > 1)
