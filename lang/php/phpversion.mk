8b64d3c215257ee251f41e2240fc0222148c6ceb
bsiegert <bsiegert@pkgsrc.org>
2020-05-15 16:47:57 +0000

    Pullup ticket #6198 - requested by taca lang/php73: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.296
    - lang/php73/distinfo                                           1.22
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Thu May 14 14:23:08 UTC 2020
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php73: distinfo
    
       Log Message:
       lang/php73: update to 7.3.18
    
       Update php73 to 7.3.18 (PHP 7.3.18).
    
       14 May 2020, PHP 7.3.18
    
       - Core:
         . Fixed bug #78875 (Long filenames cause OOM and temp files are not cleaned).
           (CVE-2019-11048) (cmb)
         . Fixed bug #78876 (Long variables in multipart/form-data cause OOM and temp
           files are not cleaned). (CVE-2019-11048) (cmb)
         . Fixed bug #79434 (PHP 7.3 and PHP-7.4 crash with NULL-pointer dereference
           on !CS constant). (Nikita)
         . Fixed bug #79477 (casting object into array creates references). (Nikita)
         . Fixed bug #79470 (PHP incompatible with 3rd party file system on demand).
           (cmb)
         . Fixed bug #78784 (Unable to interact with files inside a VFS for Git
           repository). (cmb)
    
       - DOM:
         . Fixed bug #78221 (DOMNode::normalize() doesn't remove empty text nodes).
           (cmb)
    
       - FCGI:
         . Fixed bug #79491 (Search for .user.ini extends up to root dir). (cmb)
    
       - MBString:
         . Fixed bug #79441 (Segfault in mb_chr() if internal encoding is unsupported).
           (Girgias)
    
       - OpenSSL:
         . Fixed bug #79497 (stream_socket_client() throws an unknown error sometimes
           with <1s timeout). (Joe Cai)
    
       - Phar:
         . Fix bug #79503 (Memory leak on duplicate metadata). (cmb)
    
       - SimpleXML:
         . Fixed bug #79528 (Different object of the same xml between 7.4.5 and
           7.4.4). (cmb)
    
       - Standard:
         . Fixed bug #79468 (SIGSEGV when closing stream handle with a stream filter
           appended). (dinosaur)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.292.2.3 2020/04/30 07:38:46 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.292.2.4 2020/05/15 16:47:57 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP72_VERSION=	7.2.30
-PHP73_VERSION=	7.3.17
+PHP73_VERSION=	7.3.18
 PHP74_VERSION=	7.4.5
 
 # Define initial release of major version.
