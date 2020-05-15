1efd919af60fb87d221e74dc53f387c2e669da87
adam <adam@pkgsrc.org>
2020-05-15 08:32:34 +0000

    py-sanic: updated to 20.3.0
    20.3.0:
    Unknown changes

--- www/py-sanic/patches/patch-setup.py
+++ www/py-sanic/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.4 2020/01/08 21:03:21 adam Exp $
+$NetBSD: patch-setup.py,v 1.5 2020/05/15 08:32:34 adam Exp $
 
 Allow newer versions.
 
---- setup.py.orig	2020-01-02 21:35:13.000000000 +0000
+--- setup.py.orig	2020-05-14 19:06:33.000000000 +0000
 +++ setup.py
-@@ -84,7 +84,7 @@ requirements = [
+@@ -85,7 +85,7 @@ requirements = [
      "aiofiles>=0.3.0",
      "websockets>=7.0,<9.0",
      "multidict>=4.0,<5.0",
--    "httpx==0.9.3",
-+    "httpx>=0.9.3",
+-    "httpx==0.11.1",
++    "httpx>=0.11.1",
  ]
  
  tests_require = [
