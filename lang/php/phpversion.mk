7b7ef53604421a3d9aecf0b8c7f5b22993d34563
bsiegert <bsiegert@pkgsrc.org>
2020-05-15 16:57:50 +0000

    Pullup ticket #6200 - requested by taca lang/php72: security fix
    Revisions pulled up:
    - lang/php/phpversion.mk                                        1.298
    - lang/php72/distinfo                                           1.55
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Thu May 14 14:26:59 UTC 2020
    
       Modified Files:
       	pkgsrc/lang/php: phpversion.mk
       	pkgsrc/lang/php72: distinfo
    
       Log Message:
       lang/php72: update to 7.2.31
    
       Update php72 to 7.2.31 (PHP 7.2.31).
    
       14 May 2020, PHP 7.2.31
    
       - Core:
         . Fixed bug #78875 (Long filenames cause OOM and temp files are not cleaned).
           (CVE-2019-11048) (cmb)
         . Fixed bug #78876 (Long variables in multipart/form-data cause OOM and temp
           files are not cleaned). (CVE-2019-11048) (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.292.2.5 2020/05/15 16:54:48 bsiegert Exp $
+# $NetBSD: phpversion.mk,v 1.292.2.6 2020/05/15 16:57:50 bsiegert Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -88,7 +88,7 @@ PHPVERSION_MK=	defined
 
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
-PHP72_VERSION=	7.2.30
+PHP72_VERSION=	7.2.31
 PHP73_VERSION=	7.3.18
 PHP74_VERSION=	7.4.6
 
