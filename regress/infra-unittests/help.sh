ec46b008fb9b516910c74174ba58622caf64702b
rillig <rillig@pkgsrc.org>
2020-05-16 20:22:41 +0000

    regress/infra-unittests: replace double quotes with single quotes

--- regress/infra-unittests/help.sh
+++ regress/infra-unittests/help.sh
@@ -1,32 +1,32 @@
 #! /bin/sh
-# $NetBSD: help.sh,v 1.4 2020/05/03 09:51:07 rillig Exp $
+# $NetBSD: help.sh,v 1.5 2020/05/16 20:22:41 rillig Exp $
 #
 # Test cases for "bmake help", mainly implemented in mk/help/help.awk.
 #
 
 set -eu
 
-. "./test.subr"
+. './test.subr'
 
 test_case_set_up() {
 	cd "$tmpdir"
 }
 
 
-if test_case_begin "help topic at the top of a file, with debugging"; then
+if test_case_begin 'help topic at the top of a file, with debugging'; then
 
 	# The first line of a file does not count since it usually contains
 	# the CVS Id. Therefore this topic is considered too small.
 
-	create_file_lines "test.help" \
-		"# topic" \
-		"#" \
-		"# Explanation of the topic."
+	create_file_lines 'test.help' \
+		'# topic' \
+		'#' \
+		'# Explanation of the topic.'
 
-	HELP_DEBUG="yes" TOPIC=":all" awk -f "$pkgsrcdir/mk/help/help.awk" \
-		"test.help" >"out"
+	HELP_DEBUG='yes' TOPIC=':all' awk -f "$pkgsrcdir/mk/help/help.awk" \
+		'test.help' >'out'
 
-	assert_that "out" --file-is-lines \
+	assert_that 'out' --file-is-lines \
 		'test.help:1: Adding keyword "topic"' \
 		'test.help:1: Ignoring section because of too small comment.' \
 		'test.help:3: "Explanation" is no keyword because it is mixed case' \
@@ -37,23 +37,23 @@ if test_case_begin "help topic at the top of a file, with debugging"; then
 fi
 
 
-if test_case_begin "short help topic with debugging"; then
+if test_case_begin 'short help topic with debugging'; then
 
 	# The topic starts in the paragraph in line 3 and has only 3 lines.
 	# It still has enough explanation to be considered valuable.
 	# This form is often used in mk/help/undefined-reference.help.
 
-	create_file_lines "test.help" \
-		"# $""NetBSD: ...$" \
-		"" \
-		"# topic" \
-		"#" \
-		"# Explanation of the topic."
+	create_file_lines 'test.help' \
+		'# $''NetBSD: ...$' \
+		'' \
+		'# topic' \
+		'#' \
+		'# Explanation of the topic.'
 
-	HELP_DEBUG="yes" TOPIC=":all" awk -f "$pkgsrcdir/mk/help/help.awk" \
-		"test.help" >"out"
+	HELP_DEBUG='yes' TOPIC=':all' awk -f "$pkgsrcdir/mk/help/help.awk" \
+		'test.help' >'out'
 
-	assert_that "out" --file-is-lines \
+	assert_that 'out' --file-is-lines \
 		'test.help:1: "$NetBSD:" is no keyword because it is mixed case' \
 		'test.help:1: Ignoring section because of missing keywords.' \
 		'test.help:2: Ignoring section because of missing keywords.' \
@@ -68,23 +68,23 @@ if test_case_begin "short help topic with debugging"; then
 fi
 
 
-if test_case_begin "short help topic"; then
+if test_case_begin 'short help topic'; then
 
 	# The topic starts in the paragraph in line 3 and has only 3 lines.
 	# It still has enough explanation to be considered valuable.
 	# This form is often used in mk/help/undefined-reference.help.
 
-	create_file_lines "test.help" \
-		"# $""NetBSD: ...$" \
-		"" \
-		"# topic" \
-		"#" \
-		"# Explanation of the topic."
+	create_file_lines 'test.help' \
+		'# $''NetBSD: ...$' \
+		'' \
+		'# topic' \
+		'#' \
+		'# Explanation of the topic.'
 
-	TOPIC="topic" awk -f "$pkgsrcdir/mk/help/help.awk" \
-		"test.help" >"out"
+	TOPIC='topic' awk -f "$pkgsrcdir/mk/help/help.awk" \
+		'test.help' >'out'
 
-	assert_that "out" --file-is-lines \
+	assert_that 'out' --file-is-lines \
 		'===> test.help (keywords: topic):' \
 		'# topic' \
 		'#' \
@@ -94,13 +94,13 @@ if test_case_begin "short help topic"; then
 fi
 
 
-if test_case_begin "comments without keywords"; then
+if test_case_begin 'comments without keywords'; then
 
 	# In the top-level Makefile, the SUBDIR section contains a few
 	# comment lines, which are implementation comments and do not
 	# contain keywords.  These should not appear in the help.
 
-	create_file_lines "Makefile" \
+	create_file_lines 'Makefile' \
 		'SUBDIR+=	subdir1' \
 		'# Comment1' \
 		'# Comment2' \
@@ -108,7 +108,7 @@ if test_case_begin "comments without keywords"; then
 		'SUBDIR+=	subdir2'
 
 	TOPIC=':all' awk -f "$pkgsrcdir/mk/help/help.awk" \
-		'Makefile' >"out"
+		'Makefile' >'out'
 
 	assert_that 'out' --file-is-lines \
 		'No help found for :all.'
@@ -117,7 +117,7 @@ if test_case_begin "comments without keywords"; then
 fi
 
 
-if test_case_begin "commented variables with continuation lines"; then
+if test_case_begin 'commented variables with continuation lines'; then
 
 	# When a variable assignment is commented out, it can still
 	# contain continuation lines.  These are not ordinary comments
@@ -126,7 +126,7 @@ if test_case_begin "commented variables with continuation lines"; then
 	#
 	# Found in mk/defaults/mk.conf.
 
-	create_file_lines "mk.conf" \
+	create_file_lines 'mk.conf' \
 		'# Australia.' \
 		'#' \
 		'#MASTER_SITE_OPENOFFICE=	... \' \
@@ -135,7 +135,7 @@ if test_case_begin "commented variables with continuation lines"; then
 		'#'
 
 	TOPIC=':all' awk -f "$pkgsrcdir/mk/help/help.awk" \
-		'mk.conf' >"out"
+		'mk.conf' >'out'
 
 	assert_that 'out' --file-is-lines \
 		'No help found for :all.'
