73f65ba3ff65cc51a437b64ee3562cd80bc8fc42
taca <taca@pkgsrc.org>
2020-05-17 16:25:42 +0000

    www/ruby-sassc: update to 2.3.0
    Update to ruby-sassc to 2.3.0.
    
    
    - **2.3.0**
      - [Fix rake gem:native task](https://github.com/sass/sassc-ruby/pull/196)
      - [disable lto flag for darwin + nix](https://github.com/sass/sassc-ruby/pull/166)
      - [Sort input file list](https://github.com/sass/sassc-ruby/pull/178)
      - [Set appropriate encoding for source_map](https://github.com/sass/sassc-ruby/pull/152)
      - [allow passing functions directly](https://github.com/sass/sassc-ruby/pull/162)
      - [always dispose data context](https://github.com/sass/sassc-ruby/pull/161)
      - [Update libsass to 3.6.3](https://github.com/sass/sassc-ruby/pull/164)
      - [Restore strip symbols](https://github.com/sass/sassc-ruby/pull/184)
      - [Default --march-tune-native to false](https://github.com/sass/sassc-ruby/pull/158)
      - [Fix compile issue on Mac OS X](https://github.com/sass/sassc-ruby/pull/174)
      - [Test on TruffleRuby in TravisCI](https://github.com/sass/sassc-ruby/pull/171)
      - [Use RbConfig::MAKEFILE_CONFIG['DLEXT'] instead of hardcoding extensions](https://github.com/sass/sassc-ruby/pull/173)

--- www/ruby-sassc/patches/patch-ext_extconf.rb
+++ www/ruby-sassc/patches/patch-ext_extconf.rb
@@ -1,21 +1,25 @@
-$NetBSD: patch-ext_extconf.rb,v 1.2 2019/12/25 00:14:15 joerg Exp $
+$NetBSD: patch-ext_extconf.rb,v 1.3 2020/05/17 16:25:42 taca Exp $
 
 Don't enable host-specific optimisations.
 Don't enable LTO.
 
---- ext/extconf.rb.orig	2019-09-27 13:30:47.910318310 +0000
+--- ext/extconf.rb.orig	2020-04-25 02:39:24.087193330 +0000
 +++ ext/extconf.rb
-@@ -19,18 +19,6 @@ if enable_config('static-stdlib', false)
+@@ -19,22 +19,11 @@ if enable_config('static-stdlib', false)
    $LDFLAGS << ' -static-libgcc -static-libstdc++'
  end
  
--# Set to false when building binary gems
--if enable_config('march-tune-native', true)
+-if enable_config('march-tune-native', false)
 -  $CFLAGS << ' -march=native -mtune=native'
 -  $CXXFLAGS << ' -march=native -mtune=native'
 -end
 -
--if enable_config('lto', true)
+ # darwin nix clang doesn't support lto
+ # disable -lto flag for darwin + nix
+ # see: https://github.com/sass/sassc-ruby/issues/148
+ enable_lto_by_default = (Gem::Platform.local.os == "darwin" && !ENV['NIX_CC'].nil?)
+ 
+-if enable_config('lto', enable_lto_by_default)
 -  $CFLAGS << ' -flto'
 -  $CXXFLAGS << ' -flto'
 -  $LDFLAGS << ' -flto'
