3bfe20d56add4dfaab3d3980721752fe6ae89c5d
hauke <hauke@pkgsrc.org>
2020-05-18 15:39:20 +0000

    Unbreak building without ldap support - the library got built and installed, anyway, resulting in a PLIST error.
    ldb's snazzy little build system has no official configuration option
    for this, so we reach in and flick the switch by hand.

--- databases/ldb/options.mk
+++ databases/ldb/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2020/03/18 00:29:38 tnn Exp $
+# $NetBSD: options.mk,v 1.2 2020/05/18 15:39:20 hauke Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ldb
 PKG_SUPPORTED_OPTIONS=	ldap
@@ -11,4 +11,11 @@ PLIST_VARS+=		ldap
 .if !empty(PKG_OPTIONS:Mldap)
 .  include "../../databases/openldap-client/buildlink3.mk"
 PLIST.ldap=		yes
+.else
+# No official way to configure without ldap
+CFG_CACHE=	${WRKSRC}/bin/c4che/default_cache.py
+post-configure:
+	mv ${CFG_CACHE} ${CFG_CACHE}.orig
+	sed -E -e 's/(ENABLE_LDAP_BACKEND =) True/\1 False/g' \
+		${CFG_CACHE}.orig > ${CFG_CACHE}
 .endif
