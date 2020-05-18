e5e8a2ffe0453a7a7c7f00e855c346dbabe65a34
adam <adam@pkgsrc.org>
2020-05-18 10:45:19 +0000

    py-sqlalchemy-utils: updated to 0.36.5
    0.36.5:
    - Added support for dictionary input in CompositeType
    - Added new EnrichedDateTime and EnrichedDate types
    - Using String instead of LargeBinary for impl of EncryptedType
    - Added support for JSONType in EncryptedType
    
    0.36.4:
    - Added jsonb_sql function
    - Drop py27 support

--- databases/py-sqlalchemy-utils/options.mk
+++ databases/py-sqlalchemy-utils/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2017/02/20 17:00:35 wiz Exp $
+# $NetBSD: options.mk,v 1.5 2020/05/18 10:45:19 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.py-sqlalchemy-utils
 PKG_SUPPORTED_OPTIONS=	arrow babel color furl intervals ipaddress json password
@@ -44,7 +44,8 @@ DEPENDS+=	${PYPKGPREFIX}-phonenumbers>=5.9.2:../../textproc/py-phonenumbers
 .endif
 
 .if !empty(PKG_OPTIONS:Mtests)
-DEPENDS+=	${PYPKGPREFIX}-test>=2.2.3:../../devel/py-test
+PYTHON_VERSIONED_DEPENDENCIES=	test
+.include "../../lang/python/versioned_dependencies.mk"
 DEPENDS+=	${PYPKGPREFIX}-pygments>=1.2:../../textproc/py-pygments
 DEPENDS+=	${PYPKGPREFIX}-jinja2>=2.3:../../textproc/py-jinja2
 DEPENDS+=	${PYPKGPREFIX}-docutils>=0.10:../../textproc/py-docutils
