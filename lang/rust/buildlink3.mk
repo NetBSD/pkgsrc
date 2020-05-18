b3ac5c1f89e0e7977100691b523ce3169ad629d4
nia <nia@pkgsrc.org>
2020-05-18 16:17:20 +0000

    lang: Add rust-bin.
    Dumb package that selects and installs a binary rust distribution
    based on its guess of your platform (FreeBSD, NetBSD, Linux x86_64 are
    all supported). These binaries are the official ones provided by
    rust upstream and are the same as those provided by the `rustup` tool.
    
    You can choose to use a binary rust distribution by setting:
    RUST_TYPE=bin in mk.conf
    (or source distribution with RUST_TYPE=src).
    
    Currently, RUST_TYPE=bin by default ONLY for NetBSD-x86_64. This is
    because TNF has been shown to _repeatedly_ be unable and _unwilling_ to
    ensure that rust-dependent packages build properly on their
    infrastructure, and NetBSD users are all suffering for it.
    
    This was based on minskim's work in pkgsrc-wip.
    
    It was tested by building librsvg and firefox-esr with the resulting
    binaries.

--- lang/rust/buildlink3.mk
+++ lang/rust/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2017/09/30 04:42:43 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/05/18 16:17:20 nia Exp $
 
 BUILDLINK_TREE+=	rust
 
@@ -6,7 +6,13 @@ BUILDLINK_TREE+=	rust
 RUST_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.rust+=	rust>=1.20.0
+
+.include "../../lang/rust/type.mk"
+.if ${RUST_TYPE} != "bin"
 BUILDLINK_PKGSRCDIR.rust?=	../../lang/rust
+.else
+BUILDLINK_PKGSRCDIR.rust?=	../../lang/rust-bin
+.endif
 
 BUILDLINK_PASSTHRU_DIRS+=	${PREFIX}/lib/rustlib
 .endif
