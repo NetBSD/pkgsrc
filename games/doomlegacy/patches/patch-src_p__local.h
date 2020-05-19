b6583c350b96d63e536efc6ee28c19c262dac80d
micha <micha@pkgsrc.org>
2020-05-19 11:20:16 +0000

    games/doomlegacy: Update to 1.48.4
    pkgsrc changes:
    - Patches for NetBSD support removed (merged upstream)
    - Define "SOLARIS" for SunOS based operating systems
    - MESSAGE replaced by file "share/doc/doomlegacy/INSTALL.pkgsrc"
    - Man page is now installed in section 6
    - Patch to disable launcher replaced by startscript
      (upstream suggested to use an unsupported option for this purpose)
    - Patch for extended node support added (Feature request #95)
      Supported formats: DeeP V4, ZDoom uncompressed, ZDoom compressed
      Without this patch the engine may crash with unsupported node formats
    - Patch for local blockmap creation added
      Command line option "-blockmap" added for activation (no automatic mode)
    
    The extended node and blockmap patches allow to play modern PWADs like NOVA III
    and Lost Civilization.
    
    Upstream changelog since 1.47.2 is very long, refer to this page:
    http://doomlegacy.sourceforge.net/docs/whatsnew.html

---
+++ games/doomlegacy/patches/patch-src_p__local.h
@@ -0,0 +1,18 @@
+$NetBSD: patch-src_p__local.h,v 1.1 2020/05/19 11:20:16 micha Exp $
+
+Add support to internally create blockmap.
+
+--- src/p_local.h.orig	2020-05-11 09:40:33.000000000 +0000
++++ src/p_local.h
+@@ -379,8 +379,9 @@ void P_ApplyTorque(mobj_t *mo);
+ extern byte*            rejectmatrix;   // for fast sight rejection
+ // Read wad blockmap using int16_t wadblockmaplump[].
+ // Expand from 16bit wad to internal 32bit blockmap.
+-extern uint32_t*        blockmaphead;   // offsets in blockmap are from here
+-extern uint32_t*        blockmapindex;  // Big blockmap, SSNTails
++// [MB] 2020-05-13: Fix comments (blockmapindex points to offset part of lump)
++extern uint32_t*        blockmaphead;   // Big blockmap, SSNTails
++extern uint32_t*        blockmapindex;  // offsets in blockmap are from here
+ extern int              bmapwidth;
+ extern int              bmapheight;     // in mapblocks
+ extern fixed_t          bmaporgx;
