c87f0dc44e023d4f7fc27e698d74fc4d39686b8b
nia <nia@pkgsrc.org>
2020-05-19 17:05:44 +0000

    lv2: Update to 1.18.0
    lv2 (1.18.0) stable;
    
      * Improve documentation.
      * Separate extended documentation from primary data.
      * core: Add lv2:Markdown datatype.
      * core: Deprecate lv2:reportsLatency.
      * patch: Fix incorrect type of patch:sequenceNumber.
      * port-groups: Remove incorrect type of pg:letterCode.
      * port-groups: Replace broken links with detailed Ambisonic channel
        descriptions.
      * state: Add state:freePath feature.
      * ui: Add ui:requestValue feature.
      * ui: Add ui:scaleFactor, ui:foregroundColor, and ui:backgroundColor
        properties.
      * ui: Deprecate ui:binary.
      * worker: Improve documentation.

--- audio/lv2/options.mk
+++ audio/lv2/options.mk
@@ -1,9 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2015/04/19 22:54:55 rodent Exp $
+# $NetBSD: options.mk,v 1.3 2020/05/19 17:05:44 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.lv2
 PKG_SUPPORTED_OPTIONS=	debug doc tests valgrind
 PKG_SUGGESTED_OPTIONS+=	# blank
-PLIST_VARS+=		doc
 
 .include "../../mk/bsd.options.mk"
 
@@ -14,11 +13,11 @@ BUILD_DEBUG=		# blank
 .endif
 
 .if !empty(PKG_OPTIONS:Mdoc)
+PLIST_SRC+=		PLIST.doc
 BUILD_DOC=		--docs
 BUILD_DEPENDS+=		asciidoc-[0-9]*:../../textproc/asciidoc
 BUILD_DEPENDS+=		doxygen-[0-9]*:../../devel/doxygen
 BUILD_DEPENDS+=		${PYPKGPREFIX}-rdflib-[0-9]*:../../textproc/py-rdflib
-PLIST.doc=		yes
 .else
 BUILD_DOC=		# blank
 .endif
