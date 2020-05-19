46288521100edf460f87ab4b90f985f3aae4e96d
micha <micha@pkgsrc.org>
2020-05-19 11:20:16 +0000

    games/doomlegacy: Update to 1.48.4
    pkgsrc changes:
    - Patches for NetBSD support removed (merged upstream)
    - Define "SOLARIS" for SunOS based operating systems
    - MESSAGE replaced by file "share/doc/doomlegacy/INSTALL.pkgsrc"
    - Man page is now installed in section 6
    - Patch to disable launcher replaced by startscript
      (upstream suggested to use an unsupported option for this purpose)
    - Patch for extended node support added (Feature request #95)
      Supported formats: DeeP V4, ZDoom uncompressed, ZDoom compressed
      Without this patch the engine may crash with unsupported node formats
    - Patch for local blockmap creation added
      Command line option "-blockmap" added for activation (no automatic mode)
    
    The extended node and blockmap patches allow to play modern PWADs like NOVA III
    and Lost Civilization.
    
    Upstream changelog since 1.47.2 is very long, refer to this page:
    http://doomlegacy.sourceforge.net/docs/whatsnew.html

---
+++ games/doomlegacy/files/doomlegacy.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#
+# Start script for doomlegacy
+#
+# The option "-no_launcher" doesn't exist, but disables the launcher
+# (this method was suggested by upstream).
+#
+
+$PREFIX/bin/doomlegacy.bin -no_launcher $@
+
+
+# EOF
