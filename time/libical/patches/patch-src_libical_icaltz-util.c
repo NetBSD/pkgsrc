9650d775c0315fba94732303adf6da2a63fa073d
nia <nia@pkgsrc.org>
2020-05-19 15:33:10 +0000

    libical: Update to 3.0.8
    Version 3.0.8 is a patch release.
    This release is binary and source compatible with version 3.0.0.
    Note that the libical-glib library is considered unstable; therefore not binary compatible with previous releases
    
    ReleaseNotes:
    
    - Fix for icalattach_new_from_data() and the 'free_fn' argument
    - Fix if recurrencetype contains both COUNT and UNTIL (only output UNTIL in the RRULE)

--- time/libical/patches/patch-src_libical_icaltz-util.c
+++ time/libical/patches/patch-src_libical_icaltz-util.c
@@ -1,27 +1,17 @@
-$NetBSD: patch-src_libical_icaltz-util.c,v 1.3 2020/03/04 10:54:25 nia Exp $
+$NetBSD: patch-src_libical_icaltz-util.c,v 1.4 2020/05/19 15:33:10 nia Exp $
 
 Define bswap_32 on NetBSD
-Need limits.h for CHAR_BIT
 
-https://github.com/libical/libical/pull/420
-
---- src/libical/icaltz-util.c.orig	2018-02-27 15:34:25.000000000 +0000
+--- src/libical/icaltz-util.c.orig	2020-03-07 14:42:42.000000000 +0000
 +++ src/libical/icaltz-util.c
-@@ -25,6 +25,7 @@
- #include "icalerror.h"
- #include "icaltimezone.h"
- 
-+#include <limits.h>
- #include <stdlib.h>
- 
- #if defined(sun) && defined(__SVR4)
-@@ -48,6 +49,9 @@
+@@ -49,6 +49,10 @@
  #endif
  #endif
  
 +#if defined(__NetBSD__) && !defined(bswap_32)
 +#define bswap_32 bswap32
 +#endif
++
  #if defined(__OpenBSD__) && !defined(bswap_32)
  #define bswap_32 swap32
  #endif
