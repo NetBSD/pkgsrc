d3c0ebc0f864dca9f6af1c47c3986b9fde45e4d1
wiz <wiz@pkgsrc.org>
2020-05-20 16:48:41 +0000

    gnumeric112: disable python function plugin by default
    It uses python 2.x and gnumeric upstream has turned off the plugin by
    default as well.
    
    Add a "python" option for those who really need it.
    
    Update some outdated comments.
    
    Bump PKGREVISION.

--- math/gnumeric112/options.mk
+++ math/gnumeric112/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1 2013/03/14 19:00:45 drochner Exp $
+# $NetBSD: options.mk,v 1.2 2020/05/20 16:48:41 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnumeric
-PKG_SUPPORTED_OPTIONS=	psiconv
+PKG_SUPPORTED_OPTIONS=	psiconv python
 
 .include "../../mk/bsd.options.mk"
 
@@ -9,4 +9,10 @@ PKG_SUPPORTED_OPTIONS=	psiconv
 .include "../../converters/psiconv/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-psiconv
+.endif
+
+.if !empty(PKG_OPTIONS:Mpython)
+CONFIGURE_ARGS+=               --with-python
+PYTHON_VERSIONS_ACCEPTED=      27 # not yet ported as of 1.12.47
+PLIST_SRC+=		PLIST PLIST.python
 .endif
