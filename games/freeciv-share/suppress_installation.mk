26ad3e5ae062697083fc0a904cddc16bb17ab115
nia <nia@pkgsrc.org>
2020-05-21 11:14:29 +0000

    freeciv: sprinkle SUBST_NOOP_OK
    being extremely unopinionated about whether autoconf files are pregenerated
    or not, especially when the subst is transcluded

--- games/freeciv-share/suppress_installation.mk
+++ games/freeciv-share/suppress_installation.mk
@@ -1,4 +1,4 @@
-# $NetBSD: suppress_installation.mk,v 1.4 2016/04/24 06:52:44 prlw1 Exp $
+# $NetBSD: suppress_installation.mk,v 1.5 2020/05/21 11:14:29 nia Exp $
 
 # Makefile snippet to be included by freeciv-client and freeciv-server to
 # prevent files installed by freeciv-share from being installed again.
@@ -28,15 +28,18 @@ SUBST_STAGE.installmanfix3=	post-configure
 SUBST_FILES.installmanfix3=	Makefile
 SUBST_SED.installmanfix3+=	-e 's|translations doc ||g'
 SUBST_MESSAGE.installmanfix3=	Suppressing installation of man pages and localization files already in freeciv-share.
+SUBST_NOOP_OK.installmanfix3=	yes 
 
 SUBST_CLASSES+=			installmanfix4
 SUBST_STAGE.installmanfix4=	post-configure
 SUBST_FILES.installmanfix4=	Makefile
 SUBST_SED.installmanfix4+=	-e 's|install-data-am: install-docDATA|install-data-am: \# install-docDATA|'
 SUBST_MESSAGE.installmanfix4=	Suppressing installation of doc files already in freeciv-share
+SUBST_NOOP_OK.installmanfix4=	yes 
 
 SUBST_CLASSES+=			installfix2
 SUBST_STAGE.installfix2=	post-configure
 SUBST_FILES.installfix2=	common/Makefile
 SUBST_SED.installfix2+=		-e 's|^install-exec-am: install-libLTLIBRARIES|install-exec-am: \# install-libLTLIBRARIES|'
 SUBST_MESSAGE.installfix2=	Suppressing installation of libs already in freeciv-share.
+SUBST_NOOP_OK.installfix2=	yes 
