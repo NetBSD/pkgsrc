9a0fa7a009dde18a262cbb3ba2f4f224639f4c07
adam <adam@pkgsrc.org>
2020-05-21 16:30:33 +0000

    protobuf: updated to 3.12.1
    Protocol Buffers v3.12.1
    
    Ruby
    Re-add binary gems for Ruby 2.3 and 2.4. These are EOL upstream, however
    many people still use them and dropping support will require more
    coordination.
    
    
    Protocol Buffers v3.12.0
    
    Protocol Compiler
    [experimental] Singular, non-message typed fields in proto3 now support
    presence tracking. This is enabled by adding the "optional" field label and
    passing the --experimental_allow_proto3_optional flag to protoc.
    For usage info, see docs/field_presence.md.
    During this experimental phase, code generators should update to support
    proto3 presence, see docs/implementing_proto3_presence.md for instructions.
    Allow duplicate symbol names when multiple descriptor sets are passed on
    the command-line, to match the behavior when multiple .proto files are passed.
    Deterministic protoc --descriptor_set_out
    
    C++
    [experimental] Added proto3 presence support.
    New descriptor APIs to support proto3 presence.
    Fix for 7463 in -rc1 (core dump mixing optional and singular fields in proto3)
    Enable Arenas by default on all .proto files.
    Documented that users are not allowed to subclass Message or MessageLite.
    Mark generated classes as final; inheriting from protos is strongly discouraged.
    Add stack overflow protection for text format with unknown fields.
    Add accessors for map key and value FieldDescriptors.
    Add FieldMaskUtil::FromFieldNumbers().
    MessageDifferencer: use ParsePartial() on Any fields so the diff does not
    fail when there are missing required fields.
    ReflectionOps::Merge(): lookup messages in the right factory, if it can.
    Added Descriptor::WellKnownTypes enum and Descriptor::well_known_type()
    accessor as an easier way of determining if a message is a Well-Known Type.
    Optimized RepeatedField::Add() when it is used in a loop.
    Made proto move/swap more efficient.
    De-virtualize the GetArena() method in MessageLite.
    Improves performance of json_stream_parser.cc by factor 1000
    bug: 7076 undefine Windows OUT and OPTIONAL macros
    Fixed a bug in FieldDescriptor::DebugString() that would erroneously print
    an "optional" label for a field in a oneof.
    Fix bug in parsing bool extensions that assumed they are always 1 byte.
    Fix off-by-one error in FieldOptions::ByteSize() when extensions are present.
    Clarified the comments to show an example of the difference between
    Descriptor::extension and DescriptorPool::FindAllExtensions.
    Add a compiler option 'code_size' to force optimize_for=code_size on all
    protos where this is possible.

--- devel/protobuf/buildlink3.mk
+++ devel/protobuf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2019/03/21 21:26:23 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2020/05/21 16:30:33 adam Exp $
 
 BUILDLINK_TREE+=	protobuf
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	protobuf
 PROTOBUF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.protobuf+=	protobuf>=3.0.0
-BUILDLINK_ABI_DEPENDS.protobuf+=	protobuf>=3.6.0
+BUILDLINK_ABI_DEPENDS.protobuf+=	protobuf>=3.12.0
 BUILDLINK_PKGSRCDIR.protobuf?=		../../devel/protobuf
 
 .include "../../devel/zlib/buildlink3.mk"
