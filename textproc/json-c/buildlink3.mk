4bcd61f799b226b798e9fb16ed7983d96d9f5c6b
nia <nia@pkgsrc.org>
2020-05-21 10:38:14 +0000

    json-c: api/abi bump

--- textproc/json-c/buildlink3.mk
+++ textproc/json-c/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/04/20 00:26:15 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/05/21 10:38:14 nia Exp $
 
 BUILDLINK_TREE+=	json-c
 
 .if !defined(JSON_C_BUILDLINK3_MK)
 JSON_C_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.json-c+=	json-c>=0.12
-BUILDLINK_ABI_DEPENDS.json-c+=	json-c>=0.12
+BUILDLINK_API_DEPENDS.json-c+=	json-c>=0.14
+BUILDLINK_ABI_DEPENDS.json-c+=	json-c>=0.14
 BUILDLINK_PKGSRCDIR.json-c?=	../../textproc/json-c
 .endif	# JSON_C_BUILDLINK3_MK
 
