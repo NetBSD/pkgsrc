2070c89fb2d8e563a849542b07b6f804aae6a998
ryoon <ryoon@pkgsrc.org>
2020-05-21 20:53:37 +0000

    *: Recursive revbump from graphics/ilmbase

--- x11/kdelibs4/buildlink3.mk
+++ x11/kdelibs4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.60 2020/05/06 14:04:16 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.61 2020/05/21 20:53:39 ryoon Exp $
 
 BUILDLINK_TREE+=	kdelibs
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kdelibs
 KDELIBS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kdelibs+=	kdelibs4>=4.0.0
-BUILDLINK_ABI_DEPENDS.kdelibs+=	kdelibs4>=4.14.38nb12
+BUILDLINK_ABI_DEPENDS.kdelibs+=	kdelibs4>=4.14.38nb13
 BUILDLINK_PKGSRCDIR.kdelibs?=	../../x11/kdelibs4
 
 .include "../../archivers/bzip2/buildlink3.mk"
