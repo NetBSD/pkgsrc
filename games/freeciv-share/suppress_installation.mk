e106e0245680ec7a50f22d04ffc7f10d5e2466ba
rillig <rillig@pkgsrc.org>
2020-05-21 15:58:40 +0000

    games/freeciv-share: remove redundant SUBST block
    Since common/Makefile.in is already patched, common/Makefile doesn't need
    to.

--- games/freeciv-share/suppress_installation.mk
+++ games/freeciv-share/suppress_installation.mk
@@ -1,4 +1,4 @@
-# $NetBSD: suppress_installation.mk,v 1.6 2020/05/21 15:55:14 rillig Exp $
+# $NetBSD: suppress_installation.mk,v 1.7 2020/05/21 15:58:40 rillig Exp $
 
 # Makefile snippet to be included by freeciv-client and freeciv-server to
 # prevent files installed by freeciv-share from being installed again.
@@ -34,10 +34,3 @@ SUBST_FILES.installmanfix4=	Makefile
 SUBST_SED.installmanfix4+=	-e 's|install-data-am: install-docDATA|install-data-am: \# install-docDATA|'
 SUBST_MESSAGE.installmanfix4=	Suppressing installation of doc files already in freeciv-share
 SUBST_NOOP_OK.installmanfix4=	yes 
-
-SUBST_CLASSES+=			installfix2
-SUBST_STAGE.installfix2=	post-configure
-SUBST_FILES.installfix2=	common/Makefile
-SUBST_SED.installfix2+=		-e 's|^install-exec-am: install-libLTLIBRARIES|install-exec-am: \# install-libLTLIBRARIES|'
-SUBST_MESSAGE.installfix2=	Suppressing installation of libs already in freeciv-share.
-SUBST_NOOP_OK.installfix2=	yes 
