809d39ab0bcce68d79b38ca7b517f0588e2b4763
ryoon <ryoon@pkgsrc.org>
2020-05-21 20:58:32 +0000

    ilmbase, openexr: Update to 2.5.1
    Changelog:
    2.5.1:
    
    A patch release that corrects the SO version for the v2.5 release, which missed getting bumped in v2.5.0.
    
    This release also fixes an improper failure in IlmImfTest when running on ARMv7 and AAarch64.
    
    2.5.0:
    Minor release with miscellaneous bug fixes and small features
    
    ## Summary
    
    * No more build-time header generation: toFloat.h, eLut.h,
      b44ExpLogTable.h, and dwaLookups.h are now ordinary header files, no
      longer generated on the fly.
    * New StdISSTream class, an "input" stringstream version of StdOSStream
    * New Matrix22 class in Imath
    * Chromaticity comparison operator now includes white (formerly ignored)
    * Various cmake fixes
    * Bug fixes for various memory leaks
    * Bug fixes for various invalid memory accesses
    * New checks to detect damaged input files
    * OpenEXR_Viewers has been deprecated, removed from the top-level
      cmake build and documentation.

--- graphics/openexr/buildlink3.mk
+++ graphics/openexr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2014/08/22 11:24:26 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2020/05/21 20:58:32 ryoon Exp $
 
 BUILDLINK_TREE+=	openexr
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	openexr
 OPENEXR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.openexr+=	openexr>=1.6.1
-BUILDLINK_ABI_DEPENDS.openexr+=	openexr>=2.2.0nb3
+BUILDLINK_ABI_DEPENDS.openexr+=	openexr>=2.5.1
 BUILDLINK_PKGSRCDIR.openexr?=	../../graphics/openexr
 
 PTHREAD_OPTS+=	require
