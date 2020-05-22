dca01a7a6b5b7c4682570b3d6fad7133d0c73dc8
nia <nia@pkgsrc.org>
2020-05-22 17:44:41 +0000

    wine: Update to 4.0 (most recent stable version known to build on NetBSD)
    The most recent stable version (5.0) is known to be problematic due to
    the need for individual programs to link against msvcrt.
    
    Based partially on zerous's "wine32" package in pkgsrc-wip.
    
    Includes several years of changes.
    
    This package does not include WoW64 support, so you can't run 32-bit
    binaries on a 64-bit system. That's for later...

--- emulators/wine/buildlink3.mk
+++ emulators/wine/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.29 2020/03/08 16:47:43 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2020/05/22 17:44:41 nia Exp $
 
 BUILDLINK_TREE+=	wine
 
@@ -8,17 +8,6 @@ WINE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.wine+=	wine>=1.3.0
 BUILDLINK_ABI_DEPENDS.wine+=	wine>=1.6.2nb26
 BUILDLINK_PKGSRCDIR.wine?=	../../emulators/wine
-
-pkgbase:= wine
-.include "../../mk/pkg-build-options.mk"
-
-.include "../../graphics/freetype2/buildlink3.mk"
-.if !empty(PKG_BUILD_OPTIONS.wine:Mopengl)
-.include "../../graphics/glu/buildlink3.mk"
-.include "../../graphics/freeglut/buildlink3.mk"
-.endif
-.include "../../graphics/giflib/buildlink3.mk"
-.include "../../mk/jpeg.buildlink3.mk"
 .endif # WINE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-wine
