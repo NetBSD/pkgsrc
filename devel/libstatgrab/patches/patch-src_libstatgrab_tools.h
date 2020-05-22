3de9f9e11a3ade079b66d81007d20838e1c43445
joerg <joerg@pkgsrc.org>
2020-05-22 13:45:11 +0000

    Use uvm/uvm_extern.h directly on NetBSD.

--- devel/libstatgrab/patches/patch-src_libstatgrab_tools.h
+++ devel/libstatgrab/patches/patch-src_libstatgrab_tools.h
@@ -1,7 +1,7 @@
-$NetBSD: patch-src_libstatgrab_tools.h,v 1.1 2017/07/05 19:43:53 wiedi Exp $
+$NetBSD: patch-src_libstatgrab_tools.h,v 1.2 2020/05/22 13:45:11 joerg Exp $
 
 Add include for LOCK_SH
---- src/libstatgrab/tools.h.orig	2014-06-19 11:41:10.000000000 +0000
+--- src/libstatgrab/tools.h.orig	2019-07-16 16:05:37.000000000 +0000
 +++ src/libstatgrab/tools.h
 @@ -36,6 +36,10 @@
  # include <sys/stat.h>
@@ -14,3 +14,13 @@ Add include for LOCK_SH
  #include <stdio.h>
  #ifdef STDC_HEADERS
  # include <assert.h>
+@@ -252,6 +256,9 @@ extern "C" strcasecmp(const char *s1, co
+ #ifdef HAVE_UVM_UVM_H
+ # include <uvm/uvm.h>
+ #endif
++#ifdef HAVE_UVM_UVM_EXTERN_H
++# include <uvm/uvm_extern.h>
++#endif
+ #ifdef HAVE_SYS_VMMETER_H
+ #include <sys/vmmeter.h>
+ #endif
