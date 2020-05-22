a387d8d77752403d915fa2fdca7d492c6e125879
plunky <plunky@pkgsrc.org>
2020-05-22 08:31:39 +0000

    because this is installed as libusb-1.0, provide INCDIRS and TRANSFORM to help packages find the correct files

--- devel/libusb1/buildlink3.mk
+++ devel/libusb1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/05/31 11:57:28 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/05/22 08:31:39 plunky Exp $
 
 BUILDLINK_TREE+=	libusb1
 
@@ -7,6 +7,9 @@ LIBUSB1_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libusb1+=	libusb1>=1.0.9
 BUILDLINK_PKGSRCDIR.libusb1?=	../../devel/libusb1
+
+BUILDLINK_INCDIRS.libusb1+=	include/libusb-1.0
+BUILDLINK_TRANSFORM+=		l:usb:usb-1.0
 .endif	# LIBUSB1_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libusb1
