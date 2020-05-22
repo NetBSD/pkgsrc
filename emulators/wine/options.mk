abc203069d8409826576b4e41e6a992037ff4c69
nia <nia@pkgsrc.org>
2020-05-22 17:44:41 +0000

    wine: Update to 4.0 (most recent stable version known to build on NetBSD)
    The most recent stable version (5.0) is known to be problematic due to
    the need for individual programs to link against msvcrt.
    
    Based partially on zerous's "wine32" package in pkgsrc-wip.
    
    Includes several years of changes.
    
    This package does not include WoW64 support, so you can't run 32-bit
    binaries on a 64-bit system. That's for later...

--- emulators/wine/options.mk
+++ emulators/wine/options.mk
@@ -1,71 +1,59 @@
-# $NetBSD: options.mk,v 1.19 2020/01/20 21:16:43 nia Exp $
+# $NetBSD: options.mk,v 1.20 2020/05/22 17:44:41 nia Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.wine
-PKG_SUPPORTED_OPTIONS=	cups dbus hal ldap opengl sane ssl x11
-PKG_SUGGESTED_OPTIONS=	opengl ssl x11
+PKG_OPTIONS_VAR=		PKG_OPTIONS.wine
+PKG_SUPPORTED_OPTIONS=		alsa cups dbus pulseaudio sane x11
+PKG_SUGGESTED_OPTIONS=		cups dbus sane x11
+PKG_SUGGESTED_OPTIONS.Linux=	alsa
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		opengl x11
+PLIST_VARS+=	alsa
+.if !empty(PKG_OPTIONS:Malsa)
+PLIST.alsa=		yes
+.  include "../../audio/alsa-lib/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-alsa
+.endif
 
 .if !empty(PKG_OPTIONS:Mcups)
-.include "../../print/cups-base/buildlink3.mk"
+.  include "../../print/libcups/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-cups
 .endif
 
 .if !empty(PKG_OPTIONS:Mdbus)
-.include "../../sysutils/dbus/buildlink3.mk"
+.  include "../../sysutils/dbus/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-dbus
 .endif
 
-.if !empty(PKG_OPTIONS:Mhal)
-.include "../../sysutils/hal/buildlink3.mk"
+PLIST_VARS+=	pulse
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+PLIST.pulse=		yes
+.  include "../../audio/pulseaudio/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=	--without-hal
-.endif
-
-.if !empty(PKG_OPTIONS:Mldap)
-.include "../../databases/openldap-client/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-ldap
-.endif
-
-.if !empty(PKG_OPTIONS:Mssl)
-.include "../../security/openssl/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-openssl
-.endif
-
-.if !empty(PKG_OPTIONS:Mx11)
-PLIST.x11=		yes
-.include "../../fonts/fontconfig/buildlink3.mk"
-.include "../../graphics/freetype2/buildlink3.mk"
-.include "../../x11/libICE/buildlink3.mk"
-.include "../../x11/libSM/buildlink3.mk"
-.include "../../x11/libX11/buildlink3.mk"
-.include "../../x11/libXcomposite/buildlink3.mk"
-.include "../../x11/libXcursor/buildlink3.mk"
-.include "../../x11/libXext/buildlink3.mk"
-.include "../../x11/libXi/buildlink3.mk"
-.include "../../x11/libXinerama/buildlink3.mk"
-.include "../../x11/libXrandr/buildlink3.mk"
-.include "../../x11/libXrender/buildlink3.mk"
-.include "../../x11/libXxf86vm/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-freetype
-CONFIGURE_ARGS+=	--without-x
-.endif
-
-.if !empty(PKG_OPTIONS:Mopengl) && !empty(PKG_OPTIONS:Mx11)
-PLIST.opengl=		yes
-.include "../../graphics/glu/buildlink3.mk"
-.include "../../graphics/freeglut/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-opengl
+CONFIGURE_ARGS+=	--without-pulse
 .endif
 
 .if !empty(PKG_OPTIONS:Msane)
-.include "../../graphics/sane-backends/buildlink3.mk"
+.  include "../../graphics/sane-backends/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-sane
+.endif
+
+PLIST_VARS+=	x11
+.if !empty(PKG_OPTIONS:Mx11)
+PLIST.x11=		yes
+.  include "../../x11/libX11/buildlink3.mk"
+.  include "../../x11/libICE/buildlink3.mk"
+.  include "../../x11/libXcursor/buildlink3.mk"
+.  include "../../x11/libXext/buildlink3.mk"
+.  include "../../x11/libXfixes/buildlink3.mk"
+.  include "../../x11/libXi/buildlink3.mk"
+.  include "../../x11/libXinerama/buildlink3.mk"
+.  include "../../x11/libXrandr/buildlink3.mk"
+.  include "../../x11/libXrender/buildlink3.mk"
+.  include "../../x11/libXxf86vm/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-x
 .endif
