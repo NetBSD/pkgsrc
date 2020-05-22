27da04371e492c2904837146f0066e46065433ed
adam <adam@pkgsrc.org>
2020-05-22 12:43:52 +0000

    revbump for multimedia/libdvdread; requested by leot@

--- multimedia/libdvdplay/buildlink3.mk
+++ multimedia/libdvdplay/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2018/01/07 13:04:24 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/05/22 12:43:52 adam Exp $
 
 BUILDLINK_TREE+=	libdvdplay
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libdvdplay
 LIBDVDPLAY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libdvdplay+=	libdvdplay>=1.0.1
-BUILDLINK_ABI_DEPENDS.libdvdplay+=	libdvdplay>=1.0.1nb3
+BUILDLINK_ABI_DEPENDS.libdvdplay+=	libdvdplay>=1.0.1nb4
 BUILDLINK_PKGSRCDIR.libdvdplay?=	../../multimedia/libdvdplay
 
 .include "../../multimedia/libdvdread/buildlink3.mk"
