043c34e7b4ae54d12e6745b49832014ee48aaa6a
joerg <joerg@pkgsrc.org>
2020-05-22 01:15:01 +0000

    Sync with base package

--- print/libspectre/buildlink3.mk
+++ print/libspectre/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.31 2020/03/10 22:08:58 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.32 2020/05/22 01:15:01 joerg Exp $
 
 BUILDLINK_TREE+=	libspectre
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.libspectre+=	libspectre>=0.2.1
 BUILDLINK_ABI_DEPENDS.libspectre+=	libspectre>=0.2.8nb12
 BUILDLINK_PKGSRCDIR.libspectre?=	../../print/libspectre
 
-.include "../../print/ghostscript/buildlink3.mk"
+.include "../../print/ghostscript-agpl/buildlink3.mk"
 .endif # LIBSPECTRE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libspectre
