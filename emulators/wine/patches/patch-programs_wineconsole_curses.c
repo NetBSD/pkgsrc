cc1e80388d45634f3ffcef746187ac181ae06a1a
adam <adam@pkgsrc.org>
2020-05-23 21:20:38 +0000

    wine: make it build on Darwin

--- emulators/wine/patches/patch-programs_wineconsole_curses.c
+++ emulators/wine/patches/patch-programs_wineconsole_curses.c
@@ -1,24 +1,30 @@
-$NetBSD: patch-programs_wineconsole_curses.c,v 1.1 2020/05/22 17:44:42 nia Exp $
+$NetBSD: patch-programs_wineconsole_curses.c,v 1.2 2020/05/23 21:20:39 adam Exp $
 
 _acs_map in NetBSD curses is the equivalent of acs_map in ncurses.
 
---- programs/wineconsole/curses.c.orig	2019-03-15 20:18:31.000000000 +0000
+--- programs/wineconsole/curses.c.orig	2019-11-25 15:10:40.000000000 +0000
 +++ programs/wineconsole/curses.c
-@@ -125,7 +125,7 @@ MAKE_FUNCPTR(getmouse)
+@@ -125,7 +125,11 @@ MAKE_FUNCPTR(getmouse)
  MAKE_FUNCPTR(mouseinterval)
  MAKE_FUNCPTR(mousemask)
  #endif
--MAKE_FUNCPTR(acs_map)
++#ifdef __NetBSD__
 +MAKE_FUNCPTR(_acs_map)
++#else
+ MAKE_FUNCPTR(acs_map)
++#endif
  
  #undef MAKE_FUNCPTR
  
-@@ -189,7 +189,7 @@ static BOOL WCCURSES_bind_libcurses(void
+@@ -189,7 +193,11 @@ static BOOL WCCURSES_bind_libcurses(void
      LOAD_FUNCPTR(mouseinterval)
      LOAD_FUNCPTR(mousemask)
  #endif
--    LOAD_FUNCPTR(acs_map)
++#ifdef __NetBSD__
 +    LOAD_FUNCPTR(_acs_map)
++#else
+     LOAD_FUNCPTR(acs_map)
++#endif
  
  #undef LOAD_FUNCPTR
  
