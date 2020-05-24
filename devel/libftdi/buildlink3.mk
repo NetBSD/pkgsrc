42bb1831e6c9d13f4bef7e4b2a7f53e79627e071
joerg <joerg@pkgsrc.org>
2020-05-24 23:03:17 +0000

    Don't allow the newer libftdi version to match as it also depends on a different version of libusb.

--- devel/libftdi/buildlink3.mk
+++ devel/libftdi/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/02/10 13:53:47 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/05/24 23:03:17 joerg Exp $
 
 BUILDLINK_TREE+=	libftdi
 
 .if !defined(LIBFTDI_BUILDLINK3_MK)
 LIBFTDI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libftdi+=	libftdi>=0.18
+BUILDLINK_API_DEPENDS.libftdi+=	libftdi>=0.18<1
 BUILDLINK_PKGSRCDIR.libftdi?=	../../devel/libftdi
 
 .include "../../mk/libusb.buildlink3.mk"
