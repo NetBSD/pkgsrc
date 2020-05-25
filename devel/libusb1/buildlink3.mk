d5e94277a710e5172d98e7c7f9d3a333ccab168d
plunky <plunky@pkgsrc.org>
2020-05-25 06:47:40 +0000

    revert previous addition of BUILDLINK_TRANSFORM as it causes problem with libusb-compat which includes this file but provides libusb-0.1

--- devel/libusb1/buildlink3.mk
+++ devel/libusb1/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2020/05/22 08:31:39 plunky Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/05/25 06:47:40 plunky Exp $
 
 BUILDLINK_TREE+=	libusb1
 
@@ -7,9 +7,6 @@ LIBUSB1_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libusb1+=	libusb1>=1.0.9
 BUILDLINK_PKGSRCDIR.libusb1?=	../../devel/libusb1
-
-BUILDLINK_INCDIRS.libusb1+=	include/libusb-1.0
-BUILDLINK_TRANSFORM+=		l:usb:usb-1.0
 .endif	# LIBUSB1_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libusb1
