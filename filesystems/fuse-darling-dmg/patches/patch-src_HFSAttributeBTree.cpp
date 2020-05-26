388e184fc89fcad3fdba08abbc9cca1b4931ad84
joerg <joerg@pkgsrc.org>
2020-05-26 11:29:17 +0000

    Use unversioned icu namespace.

--- filesystems/fuse-darling-dmg/patches/patch-src_HFSAttributeBTree.cpp
+++ filesystems/fuse-darling-dmg/patches/patch-src_HFSAttributeBTree.cpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_HFSAttributeBTree.cpp,v 1.1 2020/03/26 02:34:52 joerg Exp $
+$NetBSD: patch-src_HFSAttributeBTree.cpp,v 1.2 2020/05/26 11:29:17 joerg Exp $
 
 --- src/HFSAttributeBTree.cpp.orig	2020-03-25 15:29:52.355921486 +0000
 +++ src/HFSAttributeBTree.cpp
@@ -6,7 +6,7 @@ $NetBSD: patch-src_HFSAttributeBTree.cpp,v 1.1 2020/03/26 02:34:52 joerg Exp $
  #include <unicode/unistr.h>
  #include "unichar.h"
  
-+using icu_64::UnicodeString;
++using icu::UnicodeString;
 +
  HFSAttributeBTree::HFSAttributeBTree(std::shared_ptr<HFSFork> fork, CacheZone* zone)
  : HFSBTree(fork, zone, "Attribute")
