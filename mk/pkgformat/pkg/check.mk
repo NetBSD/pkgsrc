2fe0a48d2dfee45d7ce7670f1c14881b34790499
maya <maya@pkgsrc.org>
2020-05-28 16:22:58 +0000

    Flip the switch on ALLOW_VULNERABLE_PACKAGES to allow them by default.
    Right now, users who install the pkg-vulnerabilities database find that
    the vast majority of packages fail to build, penalizing them too severely.
    Package auditing can still be done via "pkg_admin audit".
    
    Alternatively, the previous behaviour can be restored with
    ALLOW_VULNERABLE_PACKAGES=no in mk.conf.
    
    Additionally, bmake-ify the check.mk logic. It was easier to do this,
    as the package relied on a single long ${RUN} command.
    
    Proposed on tech-pkg, with no objections to the idea of changing the
    default, just the method of doing so.

--- mk/pkgformat/pkg/check.mk
+++ mk/pkgformat/pkg/check.mk
@@ -1,25 +1,32 @@
-# $NetBSD: check.mk,v 1.1 2011/10/15 00:23:09 reed Exp $
+# $NetBSD: check.mk,v 1.2 2020/05/28 16:22:58 maya Exp $
 #
 
 # _pkgformat-check-vulnerable:
 #	Checks for known vulnerabilities in the package if a vulnerability
 #	file exists.
 #
+
+ALLOW_VULNERABLE_PACKAGES?=	yes
+
 .if defined(NO_PKGTOOLS_REQD_CHECK)
 _pkgformat-check-vulnerable: .PHONY
 	${RUN}${DO_NADA}
 .else
+
+_VULNFILE:=	${_EXTRACT_PKGVULNDIR:sh}/pkg-vulnerabilities
 _pkgformat-check-vulnerable: .PHONY
-	${RUN}\
-	_PKGVULNDIR=`${_EXTRACT_PKGVULNDIR}`; \
-	vulnfile=$$_PKGVULNDIR/pkg-vulnerabilities;		\
-	if ${TEST} ! -f "$$vulnfile"; then 			\
-		${PHASE_MSG} "Skipping vulnerability checks.";	\
-		${WARNING_MSG} "No $$vulnfile file found.";	\
-		${WARNING_MSG} "To fix run: \`${DOWNLOAD_VULN_LIST}'."; \
-		exit 0;						\
-	fi;							\
-	${PHASE_MSG} "Checking for vulnerabilities in ${PKGNAME}"; \
-	${AUDIT_PACKAGES} ${_AUDIT_PACKAGES_CMD} ${AUDIT_PACKAGES_FLAGS} ${PKGNAME} \
+.  if exists(${_VULNFILE})
+	@${PHASE_MSG} "Checking for vulnerabilities in ${PKGNAME}"
+.    if !empty(ALLOW_VULNERABLE_PACKAGES:tl:Mno)
+	@${AUDIT_PACKAGES} ${_AUDIT_PACKAGES_CMD} ${AUDIT_PACKAGES_FLAGS} ${PKGNAME} \
 	|| ${FAIL_MSG} "Define ALLOW_VULNERABLE_PACKAGES in mk.conf or ${_AUDIT_CONFIG_OPTION} in ${_AUDIT_CONFIG_FILE}(5) if this package is absolutely essential."
+.    else
+	@${AUDIT_PACKAGES} ${_AUDIT_PACKAGES_CMD} ${AUDIT_PACKAGES_FLAGS} ${PKGNAME} || ${TRUE}
+.    endif
+.  else
+	@${PHASE_MSG} "Skipping vulnerability checks."
+	@${WARNING_MSG} "No ${_VULNFILE} file found."
+	@${WARNING_MSG} "To fix run: \`${DOWNLOAD_VULN_LIST}'."
+.  endif
+
 .endif
