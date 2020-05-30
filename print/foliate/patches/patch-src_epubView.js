937a299d8ab7f3c4405d6aac70a63782b06fee8a
fox <fox@pkgsrc.org>
2020-05-30 00:01:58 +0000

    print/foliate: Updates to 2.2.0
    - Updates a patch.
    
    Changes since 2.1.1:
    
    Library:
    
      * A library view showing recent books and reading progress (#119)
      * Search books by metadata
      * E-book discovery with OPDS (#6, #253)
    
    New supported formats:
    
      * FictionBook (.fb2, .fb2.zip) (#128)
      * Comic book archive (.cbr, .cbz, .cbt, .cb7) (#254)
      * Plain text (.txt) (#226)
      * Unpacked EPUB files
    
    Layout:
    
      * Option to set maximum page width (#192)
      * The "Automatic" layout will now show four columns when the page width is
        wide (#257)
    
    Image viewer:
    
      * Improved image viewer, with new shortcuts and the ability to rotate (#299)
        and invert images
      * Option to disable image viewer, or to open images on double, middle, or
        right click (#315)
    
    Text-to-speech:
    
      * Improved text-to-speech configuration UI
      * Ability to set text-to-speech voice based on book language (#134)
    
    Annotations
    
      * Import annotations from JSON
      * Search in annotations
      * Annotations are now sorted in the same order as they appear in the book
        (#318)
    
    Security:
    
      * "Allow Unsafe Content" now only enables JavaScript; external content will no
        longer be loaded. This is an important security fix. If you are running an
        old version of Foliate and cannot upgrade, you should disable "Allow Unsafe
        Content".
      * WebKit processes are now sandboxed
    
    Other featues and fixes:
    
      * Improved support for vertical and right-to-left books
      * Support for StarDict dictionaries (#302) (requires new optional dependency
        sdcv)
      * Fixed current location not preserved when resizing (#152, #204)
      * Fixed auto-hiding header bar under some themes (#316)
      * Command-line options for showing version and adding books to library
      * Option to clear or disable cache
      * When an HTML page contains invalid self-closing anchor tags, it is now
        parsed as XHTML (#275, #288)
    
    Technical changes:
    
      * Foliate now stores file locations in
        ~/.local/share/com.github.johnfactotum.Foliate/library/uri-store.json, and
        saves book covers to ~/.cache/com.github.johnfactotum.Foliate. Both can be
        disabled.
      * New optional dependency tracker (gir1.2-tracker-2.0 on Debian based
        distributions), which can be used to get file locations.
      * New optional dependency libhandy (gir1.2-handy-0.0). It's not absolutely
        required, but installing it would make the experience better on larger as
        well as smaller screens.
      * To open remote files, Foliate now optionally depends on libsoup
        (gir1.2-soup-2.4).
    
    Foliate now bundles two additional libraries:
    
      * libarchivejs, which is MIT licensed. It is a WASM port of the popular
        libarchive C library.
      * crypto-js, which is MIT licensed. The MD5 module is used to generate
        identifiers for files that don't have unique identifiers.

--- print/foliate/patches/patch-src_epubView.js
+++ print/foliate/patches/patch-src_epubView.js
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_epubView.js,v 1.1 2020/04/06 14:15:47 fox Exp $
+$NetBSD: patch-src_epubView.js,v 1.2 2020/05/30 00:01:58 fox Exp $
 
 Adds variable to help substitute the correct python version suffix.
 
 --- src/epubView.js.orig	2020-04-06 13:02:45.032535873 +0000
 +++ src/epubView.js
-@@ -21,7 +21,7 @@ const {
-     mimetypes, execCommand, recursivelyDeleteDir
- } = imports.utils
+@@ -36,7 +36,7 @@ const {
+     mimetypes.kindleAlias,
+ ]
  
 -const python = GLib.find_program_in_path('python') || GLib.find_program_in_path('python3')
 +const python = GLib.find_program_in_path('python@PYVERSSUFFIX@')
