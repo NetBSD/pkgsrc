635bb61da91a08627b8f02334782586d66044cd5
rillig <rillig@pkgsrc.org>
2020-05-31 15:47:40 +0000

    security/mate-polkit: remove unknown configure option

--- security/mate-polkit/options.mk
+++ security/mate-polkit/options.mk
@@ -1,19 +1,13 @@
-# $NetBSD: options.mk,v 1.2 2016/06/13 23:28:36 youri Exp $
+# $NetBSD: options.mk,v 1.3 2020/05/31 15:47:40 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mate-polkit
 PKG_SUPPORTED_OPTIONS=	introspection
-PKG_SUGGESTED_OPTIONS=
+PKG_SUGGESTED_OPTIONS=	# none
 
 .include "../../mk/bsd.options.mk"
-
-PLIST_VARS+=	introspection
 
 .if !empty(PKG_OPTIONS:Mintrospection)
 BUILDLINK_DEPMETHOD.gobject-introspection+=	build
 .include "../../devel/gobject-introspection/buildlink3.mk"
 .include "../../graphics/cairo-gobject/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-introspection
-PLIST.introspection=	yes
-.else
-CONFIGURE_ARGS+=	--disable-introspection
 .endif
