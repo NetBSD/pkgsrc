fe5935b02d4022265cb234ae857ce3259e701a52
adam <adam@pkgsrc.org>
2020-05-31 11:34:15 +0000

    mkvtoolnix: updated to 47.0.0
    Version 47.0.0 "Black Flag"
    
    New features and enhancements
    
    * mkvmerge: chapters: mkvmerge can now read chapters from DVDs if the user
      specifies the path to a DVD folder structure via the `--chapters …`
      parameter. By default chapters from the first title will be imported. This
      can be changed by append `:<title number>` to the file/directory name in the
      `--chapters …` argument, e.g. `--chapters /srv/dvds/BigBuckBunny/VIDEO_TS:3`
      This feature requires mkvmerge to have been built with the `libdvdread`
      library.
    * mkvmerge: added `--engage append_and_split_flac` that enables mkvmerge to
      append and split FLAC tracks, restoring pre-v45 behavior.  The resulting
      tracks will be broken: the official FLAC tools will not be able to decode
      them and seeking will not work as expected.
    * MKVToolNix GUI: multiplexer: added support for mkvmerge's new support for
      reading chapters from DVDs if both have been built with the `libdvdread`
      library.
    * MKVToolNix GUI: multiplexer: when deriving languages from file names the GUI
      will now look for simplified language names instead of the full ones
      (e.g. instead of looking for "Greek, Modern (1453-)" it would simply look
      for "Greek").
    * MKVToolNix GUI: multiplexer: the options in the "additional command-line
      options" dialog are now sorted alphabetically. Additionally the
      `--append-mode` option has been added as one of the only missing global
      options.
    * MKVToolNix GUI: chapter editor: the chapter editor can now read chapters
      from DVDs if MKVToolNix has been build with the `libdvdread` library.
    * MKVToolNix GUI: header editor: added an option in the preferences for
      displaying all date & time values in UTC instead of the local time
      zone.
    
    Bug fixes
    
    * all: fixed a crash when using the `MTX_LOGGER=file` syntax for logging debug
      messages without specifying a file name to log to. It will now log to a file
      called `mkvtoolnix-debug.log` in the system's default temporary directory,
      as initially intended.
    
    Build system changes
    
    * The [`libdvdread`](https://www.videolan.org/developers/libdvdnav.html)
      library will be used if found via `pkg-config`. If it is found, support for
      reading chapters from DVDs will be enabled in `mkvmerge` and the MKVToolNix
      GUI.
    * Boost's Date/Time, Lexical Cast, Range, Range Adaptors, Tri-Bool, Variant
      libraries are not used anymore.
    * MKVToolNix now requires a C++ compiler & standard library that support the
      following features of the C++17 standard: "`std::variant`", "`std::gcd`".
      For the GNU Compiler Collection (gcc) this means v7 or newer; for clang it
      means v4 or newer — the same versions required by earlier MKVToolNix
      versions.
    * MKVToolNix now requires version 6.1.0 of fmtlib or later for the
      `fmt::to_string` function and bugfixes to the formatting of floating point
      numbers. If a system-wide version is installed that's older, the bundled
      copy will be used.
    * The bundled version of fmtlib has been updated to release 6.2.1.

--- multimedia/mkvtoolnix/patches/patch-src_common_locale.cpp
+++ multimedia/mkvtoolnix/patches/patch-src_common_locale.cpp
@@ -1,19 +1,19 @@
-$NetBSD: patch-src_common_locale.cpp,v 1.1 2018/12/03 12:15:03 jperkin Exp $
+$NetBSD: patch-src_common_locale.cpp,v 1.2 2020/05/31 11:34:15 adam Exp $
 
 Remove broken SunOS section that is no longer needed.
 https://gitlab.com/mbunkus/mkvtoolnix/issues/2450
 
---- src/common/locale.cpp.orig	2018-12-01 11:15:36.000000000 +0000
+--- src/common/locale.cpp.orig	2020-05-31 10:56:36.000000000 +0000
 +++ src/common/locale.cpp
-@@ -292,12 +292,6 @@ get_local_charset() {
+@@ -283,12 +283,6 @@ get_local_charset() {
    setlocale(LC_CTYPE, "");
  #if defined(COMP_MINGW) || defined(COMP_MSC)
-   lc_charset = "CP" + to_string(GetACP());
+   lc_charset = fmt::format("CP{0}", GetACP());
 -#elif defined(SYS_SOLARIS)
 -  int i;
 -
 -  lc_charset = nl_langinfo(CODESET);
--  if (parse_number(lc_charset, i))
+-  if (mtx::string::parse_number(lc_charset, i))
 -    lc_charset = "ISO"s + lc_charset + "-US"s;
  #elif HAVE_NL_LANGINFO
    lc_charset = nl_langinfo(CODESET);
