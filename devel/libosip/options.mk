7e6662322d70146b9576922a57abb67b459d6032
rillig <rillig@pkgsrc.org>
2020-05-31 15:34:32 +0000

    devel/libosip: remove unknown configure option

--- devel/libosip/options.mk
+++ devel/libosip/options.mk
@@ -1,13 +1,13 @@
-# $NetBSD: options.mk,v 1.1 2013/07/14 14:55:16 rodent Exp $
+# $NetBSD: options.mk,v 1.2 2020/05/31 15:34:32 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libosip
 PKG_SUPPORTED_OPTIONS=	debug
-PKG_SUGGESTED_OPTIONS+=	# blank
+PKG_SUGGESTED_OPTIONS=	# none
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdebug)
-CONFIGURE_ARGS+=	--enable-debug --enable-trace
+CONFIGURE_ARGS+=	--enable-trace
 .else
-CONFIGURE_ARGS+=	--disable-debug --disable-trace
+CONFIGURE_ARGS+=	--disable-trace
 .endif
