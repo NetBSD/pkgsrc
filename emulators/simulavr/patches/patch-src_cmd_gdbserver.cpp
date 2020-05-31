b968f750f0653e413529ed58668c19ec92ea8af5
mef <mef@pkgsrc.org>
2020-05-31 14:11:42 +0000

    (emulators/simulavr) long standing no regen on patch, and resulting SUBST issue. Fix them (I hope)

--- emulators/simulavr/patches/patch-src_cmd_gdbserver.cpp
+++ emulators/simulavr/patches/patch-src_cmd_gdbserver.cpp
@@ -1,4 +1,6 @@
-$NetBSD: patch-src_cmd_gdbserver.cpp,v 1.1 2020/05/26 11:28:50 joerg Exp $
+$NetBSD: patch-src_cmd_gdbserver.cpp,v 1.2 2020/05/31 14:11:42 mef Exp $
+
+Use BSD bind, not STL bind.
 
 --- src/cmd/gdbserver.cpp.orig	2020-05-26 01:36:49.070961229 +0000
 +++ src/cmd/gdbserver.cpp
