2a246cc659b402e86bfc16dc311a502d25d2a020
nia <nia@pkgsrc.org>
2020-05-31 09:51:35 +0000

    xxhash: Update to 0.7.3. Fix installation of libs.
    v0.7.3
    - perf: improved speed for large inputs (~+20%)
    - perf: improved latency for small inputs (~10%)
    - perf: s390x Vectorial code, by @easyaspi314
    - cli: improved support for Unicode filenames on Windows, thanks to @easyaspi314 and @t-mat
    - api: `xxhash.h` can now be included in any order, with and without `XXH_STATIC_LINKING_ONLY` and `XXH_INLINE_ALL`
    - build: xxHash's implementation transferred into `xxhash.h`. No more need to have `xxhash.c` in the `/include` directory for `XXH_INLINE_ALL` to work
    - install: created pkg-config file, by @bket
    - install: VCpkg installation instructions, by @LilyWangL
    - doc: Highly improved code documentation, by @easyaspi314
    - misc: New test tool in `/tests/collisions`: brute force collision tester for 64-bit hashes
    
    v0.7.2
    - Fixed collision ratio of `XXH128` for some specific input lengths, reported by @svpv
    - Improved `VSX` and `NEON` variants, by @easyaspi314
    - Improved performance of scalar code path (`XXH_VECTOR=0`), by @easyaspi314
    - `xxhsum`: can generate 128-bit hashes with the `-H2` option (note: for experimental purposes only! `XXH128` is not yet frozen)
    - `xxhsum`: option `-q` removes status notifications
    
    v0.7.1
    - Secret first: the algorithm computation can be altered by providing a "secret", which is any blob of bytes, of size >= `XXH3_SECRET_SIZE_MIN`.
    - `seed` is still available, and acts as a secret generator
    - updated `ARM NEON` variant by @easyaspi314
    - Streaming implementation is available
    - Improve compatibility and performance with Visual Studio, with help from @aras-p
    - Better integration when using `XXH_INLINE_ALL`: do not pollute host namespace, use its own macros, such as `XXH_ASSERT()`, `XXH_ALIGN`, etc.
    - 128-bit variant provides helper functions for comparison of hashes.
    - Better `clang` generation of `rotl` instruction, thanks to @easyaspi314
    - `XXH_REROLL` build macro to reduce binary size, by @easyaspi314
    - Improved `cmake` script, by @Mezozoysky
    - Full benchmark program provided in `/tests/bench`

--- devel/xxhash/buildlink3.mk
+++ devel/xxhash/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/09/10 13:47:31 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/05/31 09:51:35 nia Exp $
 
 BUILDLINK_TREE+=	xxhash
 
 .if !defined(XXHASH_BUILDLINK3_MK)
 XXHASH_BUILDLINK3_MK:=
 
-BUILDLINK_ABI_DEPENDS.xxhash+=	xxhash>=0.6.0
-BUILDLINK_API_DEPENDS.xxhash+=	xxhash>=0.6.0
+BUILDLINK_ABI_DEPENDS.xxhash+=	xxhash>=0.7.3
+BUILDLINK_API_DEPENDS.xxhash+=	xxhash>=0.7.3
 BUILDLINK_PKGSRCDIR.xxhash?=	../../devel/xxhash
 .endif # XXHASH_BUILDLINK3_MK
 
