6a26e927331ede506ee44a47817a18a2ea8a9b8d
gutteridge <gutteridge@pkgsrc.org>
2020-05-31 23:44:49 +0000

    mate-power-manager: remove unused PLIST.keyring variable

--- sysutils/mate-power-manager/options.mk
+++ sysutils/mate-power-manager/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2016/05/21 19:13:18 youri Exp $
+# $NetBSD: options.mk,v 1.2 2020/05/31 23:44:49 gutteridge Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mate-power-manager
 PKG_SUPPORTED_OPTIONS=	keyring
@@ -6,12 +6,9 @@ PKG_SUGGESTED_OPTIONS=	keyring
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	keyring
-
 .if !empty(PKG_OPTIONS:Mkeyring)
 .include "../../security/libgnome-keyring/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-keyring
-PLIST.keyring=yes
 .else
 CONFIGURE_ARGS+=	--without-keyring
 .endif
