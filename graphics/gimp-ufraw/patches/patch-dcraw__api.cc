f60a1c931e208ad86b83a4bdde1daff1f0f6098a
gdt <gdt@pkgsrc.org>
2020-06-01 00:14:21 +0000

    gimp-ufraw: Recover from patches that broke NetBSD 8
    Replace patches that broke NetBSD 8 (added without MAINTAINER
    consultation, and for which queries went unanswered), replacing them
    with something that seems likely to work in multiple situations.
    
    The basic issue is that OPENMP is unstable, and it does not seem to be
    possible to write non-ifdefed code that compiles cleanly on old and
    new versions:
      https://gcc.gnu.org/gcc-9/porting_to.html#ompdatasharing

--- graphics/gimp-ufraw/patches/patch-dcraw__api.cc
+++ graphics/gimp-ufraw/patches/patch-dcraw__api.cc
@@ -1,22 +1,25 @@
-$NetBSD: patch-dcraw__api.cc,v 1.1 2020/04/20 00:38:36 joerg Exp $
+$NetBSD: patch-dcraw__api.cc,v 1.2 2020/06/01 00:14:21 gdt Exp $
 
---- dcraw_api.cc.orig	2020-04-19 19:59:33.951610818 +0000
+Drop default(none), because the OPENMP specification is unstable;
+some versions prohibit declaring constants as shared and some require it.
+
+--- dcraw_api.cc.orig	2015-06-16 03:58:38.000000000 +0000
 +++ dcraw_api.cc
-@@ -687,7 +687,7 @@ extern "C" {
+@@ -686,7 +686,7 @@ extern "C" {
+             rgbWB[3] = rgbWB[1];
          if (dark) {
  #ifdef _OPENMP
-             #pragma omp parallel for schedule(static) default(none) \
--            shared(h,dark,rgbWB)
-+            shared(h,dark,rgbWB,pixels,black)
+-            #pragma omp parallel for schedule(static) default(none) \
++            #pragma omp parallel for schedule(static) \
+             shared(h,dark,rgbWB)
  #endif
              for (int i = 0; i < pixels; i++) {
-                 int cc;
-@@ -700,7 +700,7 @@ extern "C" {
+@@ -699,7 +699,7 @@ extern "C" {
+             }
          } else {
  #ifdef _OPENMP
-             #pragma omp parallel for schedule(static) default(none) \
--            shared(h,dark,rgbWB)
-+            shared(h,dark,rgbWB,pixels,black)
+-            #pragma omp parallel for schedule(static) default(none) \
++            #pragma omp parallel for schedule(static) \
+             shared(h,dark,rgbWB)
  #endif
              for (int i = 0; i < pixels; i++) {
-                 int cc;
