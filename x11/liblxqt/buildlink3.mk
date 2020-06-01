b856244b235e249e7ef1ae001f2f6f3b64af7b8b
pin <pin@pkgsrc.org>
2020-06-01 10:30:37 +0000

    x11/liblxqt: update to 0.15.1
    liblxqt-0.15.1 / 2020-05-28
    ===========================
      * Bumped version to 0.15.1.
      * Removed dead codes.
      * Removed Xdg Dirs Fallback workaround.
      * Let Qt handle dialog position.
      * Prevent crash when closing screen saver error message.
    
    liblxqt-0.15.0 / 2020-04-22
    ===========================
      * Bumped version to 0.15.0.
      * Fixed licenses.
      * Removed message handler from lxqtapplication.
      * Set default button to Yes in leave dialogs.
      * C++11 code updates.
      * Used braced initializer list in returns.
      * Allow spacing to be added between items in GridLayout.
      * Set size hint of selection widget correctly.
      * Make macOS build possible.
      * Fixed compilation error on the Hurd.
      * Fixed error messages for screensavers.
      * Fixed the wrong arguments in dbusCall.
      * Allow custom lock command in lxqtscreensaver.
      * Improved error messages in lxqtscreensaver.
      * Fixed LinuxBackend backlightChanged signal.
      * Remove deprecated QDesktopWidget.
      * Remove (duplicated) string casts definitions.
      * Don't use Qt keywords "emit", "signal" and "slot" (they may collide with glib).
      * Fixed memory leak in backlight brightness driver.
      * lxqtnotification: async API for querying serverInfo.

--- x11/liblxqt/buildlink3.mk
+++ x11/liblxqt/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/04/12 08:28:13 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/06/01 10:30:37 pin Exp $
 
 BUILDLINK_TREE+=	liblxqt
 
 .if !defined(LIBLXQT_BUILDLINK3_MK)
 LIBLXQT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=0.14.1
-BUILDLINK_ABI_DEPENDS.liblxqt?=	liblxqt>=0.14.1nb3
+BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=0.15.0
 BUILDLINK_PKGSRCDIR.liblxqt?=	../../x11/liblxqt
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
