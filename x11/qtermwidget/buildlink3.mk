3bd61718b6f548b3c3537a93a628fd038e5d9fa8
pin <pin@pkgsrc.org>
2020-06-01 10:53:09 +0000

    x11/qtermwidget: update to 0.15.0
    qtermwidget-0.15.0 / 2020-04-24
    ===============================
    
      * Bumped version to 0.15.0.
      * Replaced Q_WS_MAC with Q_OS_MACOS for Qt5 compatibility.
      * KPty: Don't conditionalize chownpty existence on HAVE_OPENPTY.
      * cmake: set CMAKE_BUILD_WITH_INSTALL_NAME_DIR ON for macOS.
      * Fixed the default font on macOS.
      * pyqt: Fixes deprecation warning (CMP0002).
      * Fixed the PyQt5 binding with Qt  5.11.
      * Swap Qt's ControlModifier and MetaModifier on macOS.
      * C++11 code updates.
      * Use braced initializer list in returns.
      * Fixed the memory access violation in TerminalDisplay's method `getCharacterPosition`.
      * Completed the support for transient scrollbars.
      * Added `saveHistory` to QTermWidget.
      * Use vanilla Qt instead of patched one.
      * Don't set the selection clipboard if it's unsupported.
      * Announce truecolor support via COLORTERM.
      * Fixed numpad handling and added entries for numpad 5.
      * Allow to disable drawing line chars.
      * Use QRectF for cursor drawing and fix artifacts in hidpi.
      * Fixed compilation on NetBSD.
      * Added sendKeyEvent() API.
      * Fixed flickering on font change.
      * Select all text when opening search bar.
      * Removed some debug outputs.
      * Removed (duplicated) string casts definitions.
      * Removed obsolete qCopy.
      * Fix SearchBar light text over white background with dark themes.
      * pyqt: also check for sip 5.x path.
      * Prevented a c++11 range-loop might detach Qt container.
      * Dropped the deprecated QString method `sprintf()`.
      * Avoid buffer overflows exploits.
      * Fixed build with LLVM/clang.
      * Explicitly mark exported classes.
      * Fixed QCharRef's deprecated behavior.
      * Correctly initialize sigsets.

--- x11/qtermwidget/buildlink3.mk
+++ x11/qtermwidget/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/02/20 05:47:44 gutteridge Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/06/01 10:53:09 pin Exp $
 
 BUILDLINK_TREE+=	qtermwidget
 
 .if !defined(QTERMWIDGET_BUILDLINK3_MK)
 QTERMWIDGET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=0.14.1
+BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=0.15.0
 BUILDLINK_PKGSRCDIR.qtermwidget?=	../../x11/qtermwidget
 
 #.include "../../x11/qt5-qtbase/buildlink3.mk"
