030d5f1c610e1282ec6a6f56390e68f5ba49dcc3
gdt <gdt@pkgsrc.org>
2020-06-01 00:14:21 +0000

    gimp-ufraw: Recover from patches that broke NetBSD 8
    Replace patches that broke NetBSD 8 (added without MAINTAINER
    consultation, and for which queries went unanswered), replacing them
    with something that seems likely to work in multiple situations.
    
    The basic issue is that OPENMP is unstable, and it does not seem to be
    possible to write non-ifdefed code that compiles cleanly on old and
    new versions:
      https://gcc.gnu.org/gcc-9/porting_to.html#ompdatasharing

--- graphics/gimp-ufraw/patches/patch-dcraw__indi.c
+++ graphics/gimp-ufraw/patches/patch-dcraw__indi.c
@@ -1,31 +1,31 @@
-$NetBSD: patch-dcraw__indi.c,v 1.1 2020/04/20 00:38:36 joerg Exp $
+$NetBSD: patch-dcraw__indi.c,v 1.2 2020/06/01 00:14:21 gdt Exp $
 
---- dcraw_indi.c.orig	2020-04-19 19:58:30.067650344 +0000
+Drop default(none), because the OPENMP specification is unstable;
+some versions prohibit declaring constants as shared and some require it.
+
+--- dcraw_indi.c.orig	2015-06-16 03:58:38.000000000 +0000
 +++ dcraw_indi.c
-@@ -143,7 +143,7 @@ void CLASS wavelet_denoise_INDI(ushort(*
+@@ -142,7 +142,6 @@ void CLASS wavelet_denoise_INDI(ushort(*
+     private(c,i,hpass,lev,lpass,row,col,thold,fimg,temp)
  #else
      #pragma omp parallel for				\
-     default(none)					\
--    shared(nc,image,size)				\
-+    shared(nc,image,size,iwidth,iheight,threshold,noise) \
+-    default(none)					\
+     shared(nc,image,size)				\
      private(c,i,hpass,lev,lpass,row,col,thold,fimg,temp)
  #endif
- #endif
-@@ -414,7 +414,7 @@ void CLASS vng_interpolate_INDI(ushort(*
+@@ -413,7 +412,6 @@ void CLASS vng_interpolate_INDI(ushort(*
+     progress(PROGRESS_INTERPOLATE, -height);
  #ifdef _OPENMP
      #pragma omp parallel				\
-     default(none)					\
--    shared(image,code,prow,pcol,h)			\
-+    shared(image,code,prow,pcol,h,colors,width,filters,height)		\
+-    default(none)					\
+     shared(image,code,prow,pcol,h)			\
      private(row,col,g,brow,rowtmp,pix,ip,gval,diff,gmin,gmax,thold,sum,color,num,c,t)
  #endif
-     {
-@@ -497,7 +497,7 @@ void CLASS ppg_interpolate_INDI(ushort(*
+@@ -496,7 +494,6 @@ void CLASS ppg_interpolate_INDI(ushort(*
+ 
  #ifdef _OPENMP
      #pragma omp parallel				\
-     default(none)					\
--    shared(image,dir,diff)				\
-+    shared(image,dir,diff,height,width,filters)		\
+-    default(none)					\
+     shared(image,dir,diff)				\
      private(row,col,i,d,c,pix,guess)
  #endif
-     {
