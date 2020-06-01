5fbfc21c3e541946d06196de75899c3e7fdaf6b3
pin <pin@pkgsrc.org>
2020-06-01 12:01:42 +0000

    x11/lxqt-globalkeys: update to 0.15.0
    lxqt-globalkeys-0.15.0 / 2020-04-22
    ===================================
      * Removed (duplicated) string casts definitions.
      * Removed obsolete `qBinaryFind`.
      * Added some translation context to `main_window.ui`.
      * Don't mix const and non-const iterators.

--- x11/lxqt-globalkeys/buildlink3.mk
+++ x11/lxqt-globalkeys/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/04/25 01:34:53 gutteridge Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/06/01 12:01:42 pin Exp $
 
 BUILDLINK_TREE+=	lxqt-globalkeys
 
 .if !defined(LXQT_GLOBALKEYS_BUILDLINK3_MK)
 LXQT_GLOBALKEYS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=0.14.3
+BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=0.15.0
 BUILDLINK_PKGSRCDIR.lxqt-globalkeys?=	../../x11/lxqt-globalkeys
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
