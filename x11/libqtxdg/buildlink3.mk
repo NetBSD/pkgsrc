cedfbcd4b4535e70ce2d08d24c2aaaea5c5ce960
pin <pin@pkgsrc.org>
2020-06-01 10:25:53 +0000

    x11/libqtxdg: update to 3.5.0
    libqtxdg-3.5.0 / 2020-04-22
    ===========================
      * Removed dead Qt4 Mac code from XdgIconLoader.
      * Removed obsolete qSwap.
      * Silenced clazy warnings
      * Sharp SVG icons with scale factors > 1.

--- x11/libqtxdg/buildlink3.mk
+++ x11/libqtxdg/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/04/12 08:28:13 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/06/01 10:25:53 pin Exp $
 
 BUILDLINK_TREE+=	libqtxdg
 
 .if !defined(LIBQTXDG_BUILDLINK3_MK)
 LIBQTXDG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libqtxdg+=	libqtxdg>=3.4.0
-BUILDLINK_ABI_DEPENDS.libqtxdg?=		libqtxdg>=3.4.0nb3
-BUILDLINK_PKGSRCDIR.libqtxdg?=		../../x11/libqtxdg
+BUILDLINK_API_DEPENDS.libqtxdg+=	libqtxdg>=3.5.0
+BUILDLINK_PKGSRCDIR.libqtxdg?=		../../wip/libqtxdg
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
