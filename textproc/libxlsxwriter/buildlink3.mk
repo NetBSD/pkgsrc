5f98c4ca7205d55d312236c18a95d113485725b6
sjmulder <sjmulder@pkgsrc.org>
2020-06-02 10:28:40 +0000

    libxlsxwriter: Update to 0.9.5
    From the changelog:
    
     - Fix for issue where hyperlinks urls and tips were ignored for
       `worksheet_insert_image_buffer_opt()` images.
    
       https://github.com/jmcnamara/libxlsxwriter/issues/292
    
     - Added #LXW_CHART_LINE_STACKED and #LXW_CHART_LINE_STACKED_PERCENT
       line charts subtypes.
    
     - Removed LXW_ERROR_SHEETNAME_RESERVED warning which was used with the
       reserved worksheet name "History". However, this is an allowed
       worksheet name in some Excel variants so the warning has been turned
       into a documentation note instead.

--- textproc/libxlsxwriter/buildlink3.mk
+++ textproc/libxlsxwriter/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2020/01/28 14:32:48 sjmulder Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/06/02 10:28:40 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxlsxwriter
 
 .if !defined(LIBXLSXWRITER_BUILDLINK3_MK)
 LIBXLSXWRITER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=0.9.4
+BUILDLINK_API_DEPENDS.libxlsxwriter+=	libxlsxwriter>=0.9.5
 BUILDLINK_PKGSRCDIR.libxlsxwriter?=	../../textproc/libxlsxwriter
 
 .include "../../devel/zlib/buildlink3.mk"
