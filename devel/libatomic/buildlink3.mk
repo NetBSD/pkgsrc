5ea1cd50352f83b90b2e041a1f8f70b26159bafa
maya <maya@pkgsrc.org>
2020-06-02 21:27:05 +0000

    libatomic: remove wip reference

--- devel/libatomic/buildlink3.mk
+++ devel/libatomic/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/06/02 21:26:03 maya Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/06/02 21:27:05 maya Exp $
 
 BUILDLINK_TREE+=	libatomic
 
@@ -7,7 +7,7 @@ LIBATOMIC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libatomic+=	libatomic>=10.0
 BUILDLINK_ABI_DEPENDS.libatomic+=	libatomic>=10.0
-BUILDLINK_PKGSRCDIR.libatomic?=		../../wip/libatomic
+BUILDLINK_PKGSRCDIR.libatomic?=		../../devel/libatomic
 
 .include "../../mk/pthread.buildlink3.mk"
 .endif
