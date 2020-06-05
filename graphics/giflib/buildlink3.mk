1481743049ad6c7e89892a8a914d30bc52bc0b72
jperkin <jperkin@pkgsrc.org>
2020-06-05 12:18:32 +0000

    giflib: Bump API depends for libgifutil fix.

--- graphics/giflib/buildlink3.mk
+++ graphics/giflib/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2020/01/07 20:25:14 sevan Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2020/06/05 12:18:32 jperkin Exp $
 
 BUILDLINK_TREE+=	giflib
 
 .if !defined(GIFLIB_BUILDLINK3_MK)
 GIFLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.giflib+=	giflib>=5.2.1
+BUILDLINK_API_DEPENDS.giflib+=	giflib>=5.2.1nb3
 BUILDLINK_PKGSRCDIR.giflib?=	../../graphics/giflib
 .endif	# GIFLIB_BUILDLINK3_MK
 
