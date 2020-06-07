96a992cbc4572de749a8d7ed56fb12bbe4483310
markd <markd@pkgsrc.org>
2020-06-07 11:27:01 +0000

    kwayland: update to 5.70.0
    5.68
      Add application menu dbus paths to org_kde_plasma_window interface
      Registry: don't destroy the callback on globalsync
      [surface] Fix buffer offset when attaching buffers to surfaces
    5.69
      [xdgoutput] Only send initial name and description if set
      Add XdgOutputV1 version 2
      Broadcast application menu to resources when registering them
      Provide an implementation for the tablet interface
      [server] Don't make assumptions about the order of damage_buffer and
      attach requests
      Pass a dedicated fd to each keyboard for the xkb  keymap
      [server] Introduce SurfaceInterface::boundingRect()
    5.70
      [Wayland] Add to PlasmaWindowManagement protocol windows stacking order
      [server] Add some sub-surface life cycle signals

--- devel/kwayland/buildlink3.mk
+++ devel/kwayland/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2020/06/02 08:22:38 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2020/06/07 11:27:01 markd Exp $
 
 BUILDLINK_TREE+=	kwayland
 
@@ -9,9 +9,8 @@ BUILDLINK_API_DEPENDS.kwayland+=	kwayland>=5.6.3
 BUILDLINK_ABI_DEPENDS.kwayland?=		kwayland>=5.66.0nb4
 BUILDLINK_PKGSRCDIR.kwayland?=		../../devel/kwayland
 
-.include "../../devel/wayland/buildlink3.mk"
-.include "../../devel/wayland-protocols/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtwayland/buildlink3.mk"
 .endif	# KWAYLAND_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kwayland
