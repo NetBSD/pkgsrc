75153fb9c3edaf2807342a7dfb1068b2aba4051e
wiz <wiz@pkgsrc.org>
2020-06-08 13:47:43 +0000

    HElib: add upstream bug report URL

--- security/HElib/patches/patch-timing.cpp
+++ security/HElib/patches/patch-timing.cpp
@@ -1,6 +1,7 @@
-$NetBSD: patch-timing.cpp,v 1.2 2020/06/08 13:45:35 wiz Exp $
+$NetBSD: patch-timing.cpp,v 1.3 2020/06/08 13:47:44 wiz Exp $
 
 NetBSD provides CLOCK_MONOTONIC, but not CLOCK_MONOTONIC_RAW.
+https://github.com/homenc/HElib/pull/355
 
 --- timing.cpp.orig	2020-05-04 18:09:21.000000000 +0000
 +++ timing.cpp
