0d8872459e142001627f261565d3fafad1ed3787
he <he@pkgsrc.org>
2020-06-08 11:19:33 +0000

    Back out the update to version 1.44.0, revert to 1.43.1nb1.
    This is primarily due to
      https://github.com/rust-lang/rust/issues/73117
    (build failure on NetBSD/i386 8.0) which is reportedly seen also
    on NetBSD/amd64 9.99.64 and 9.99.65.

--- lang/rust/patches/patch-src_bootstrap_builder.rs
+++ lang/rust/patches/patch-src_bootstrap_builder.rs
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_bootstrap_builder.rs,v 1.9 2020/06/07 22:44:39 he Exp $
+$NetBSD: patch-src_bootstrap_builder.rs,v 1.10 2020/06/08 11:19:33 he Exp $
 
 Do not install 'src'.
 Use @PREFIX@, not $ORIGIN in rpath.
@@ -7,15 +7,15 @@ Fix RPATH for pkgsrc.
 
 --- src/bootstrap/builder.rs.orig	2020-01-27 15:34:02.000000000 +0000
 +++ src/bootstrap/builder.rs
-@@ -454,7 +454,6 @@ impl<'a> Builder<'a> {
+@@ -465,7 +465,6 @@ impl<'a> Builder<'a> {
                  install::Clippy,
                  install::Miri,
                  install::Analysis,
 -                install::Src,
                  install::Rustc
              ),
-             Kind::Run => describe!(run::ExpandYamlAnchors,),
-@@ -956,7 +955,7 @@ impl<'a> Builder<'a> {
+         }
+@@ -970,7 +969,7 @@ impl<'a> Builder<'a> {
                  rustflags.arg("-Zosx-rpath-install-name");
                  Some("-Wl,-rpath,@loader_path/../lib")
              } else if !target.contains("windows") {
--- lang/rust/patches/patch-src_libstd_build.rs
+++ lang/rust/patches/patch-src_libstd_build.rs
@@ -1,12 +1,13 @@
-$NetBSD: patch-src_libstd_build.rs,v 1.6 2020/06/07 22:44:39 he Exp $
+$NetBSD: patch-src_libstd_build.rs,v 1.7 2020/06/08 11:19:33 he Exp $
 
 SunOS support:
  - Support PKGSRC_USE_SSP (ugly for now).
+ - Add libumem support.
  - Add libexecinfo for backtrace().
 
 --- src/libstd/build.rs.orig	2019-08-13 06:27:22.000000000 +0000
 +++ src/libstd/build.rs
-@@ -16,6 +16,7 @@ fn main() {
+@@ -18,6 +18,7 @@ fn main() {
          println!("cargo:rustc-link-lib=execinfo");
          println!("cargo:rustc-link-lib=pthread");
      } else if target.contains("netbsd") {
@@ -14,12 +15,14 @@ SunOS support:
          println!("cargo:rustc-link-lib=pthread");
          println!("cargo:rustc-link-lib=rt");
      } else if target.contains("dragonfly") || target.contains("openbsd") {
-@@ -31,6 +32,8 @@ fn main() {
+@@ -27,6 +28,10 @@ fn main() {
+         println!("cargo:rustc-link-lib=posix4");
          println!("cargo:rustc-link-lib=pthread");
          println!("cargo:rustc-link-lib=resolv");
-         println!("cargo:rustc-link-lib=nsl");
-+	// pkgsrc hack until I can figure out how to pass it through properly
-+	println!("cargo:rustc-link-lib=ssp");
-         // Use libumem for the (malloc-compatible) allocator
-         println!("cargo:rustc-link-lib=umem");
++        println!("cargo:rustc-link-lib=nsl");
++        // pkgsrc hack until I can figure out how to pass it through properly
++        println!("cargo:rustc-link-lib=ssp");
++        println!("cargo:rustc-link-lib=umem");
      } else if target.contains("apple-darwin") {
+         println!("cargo:rustc-link-lib=System");
+ 
