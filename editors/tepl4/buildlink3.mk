a65ddfa6da77b8b830123cb5f06c7341fe9b8979
nia <nia@pkgsrc.org>
2020-06-09 14:44:56 +0000

    tepl4: missing uchardet

--- editors/tepl4/buildlink3.mk
+++ editors/tepl4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/06/09 14:31:27 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/06/09 14:44:56 nia Exp $
 
 BUILDLINK_TREE+=	tepl4
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.tepl4+=	tepl4>=4.4.0
 BUILDLINK_PKGSRCDIR.tepl4?=	../../editors/tepl4
 
 .include "../../devel/amtk/buildlink3.mk"
+.include "../../textproc/uchardet/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
 .include "../../x11/gtksourceview4/buildlink3.mk"
 .endif	# TEPL4_BUILDLINK3_MK
