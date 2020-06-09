2c1b5ac8e3d41a0a495d89a7c8e9f84bfbcc2070
nia <nia@pkgsrc.org>
2020-06-09 14:30:00 +0000

    editors: Add tepl4
    Tepl is a library that eases the development of GtkSourceView-based text
    editors and IDEs. Tepl is the acronym for "Text editor product line". It
    serves as an incubator for GtkSourceView.
    
    Tepl was previously named Gtef (GTK+ text editor framework). The project
    has been renamed in June 2017 to have a more beautiful name. The end of Tepl
    is pronounced like in "apple".

---
+++ editors/tepl4/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/06/09 14:30:00 nia Exp $
+
+BUILDLINK_TREE+=	tepl4
+
+.if !defined(TEPL4_BUILDLINK3_MK)
+TEPL4_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.tepl4+=	tepl4>=4.4.0
+BUILDLINK_PKGSRCDIR.tepl4?=	../../wip/tepl4
+
+.include "../../devel/amtk/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.include "../../x11/gtksourceview4/buildlink3.mk"
+.endif	# TEPL4_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-tepl4
