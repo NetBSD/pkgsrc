68a5cb693bb2c59bd28062d4315fc8fe85793678
nia <nia@pkgsrc.org>
2020-06-09 11:32:43 +0000

    syncthing: Update to 1.6.1
       v1.6.1
    
       This is a fixup release on 1.6.0 in order to restore the auto upgrade
       critera broken in 1.5.0.
    
       Bugfixes:
    
         * #6701: Syncthing 1.5.0+ auto upgrades even with auto upgrades disabled
           in config
    
       v1.6.0
    
       This release performs a database schema migration, and adds the
       BlockPullOrder, DisableFsync and MaxConcurrentWrites folder
       options to the configuration schema.
    
       The LocalChangeDetected event no longer has the action set to
       added for new files, instead showing modified for all local file
       changes.
    
       Bugfixes:
    
         * #3876: Removing and re-adding a folder may cause data loss
         * #5731: 100% CPU every minute on watch setup retry
         * #6268: Out of sync panel layout overflows
         * #6557: Folder error repeatedly set and unset
         * #6559: Deadlock on folder unsubscribe
         * #6576: Versioning params in config flip flop in order
         * #6578: Allow rescan at folder state "Local Additions"
         * #6583: Distributed deadlock on request
         * #6604: Docker healthcheck fails when run in host network
    
       Enhancements:
    
         * #5224: Accept new connections in place of old ones when appropriate
         * #6530: Add "Pause All"/"Resume All" button for devices
         * #6541: Limit number of concurrent writes while syncing
    
       Other issues:
    
         * #6575: Incomprehensible error message: 'directory is not empty; files
           within are probably ignored on connected devices only'
         * #6584: A field in a structure is sometimes protected by Mutex, but
           sometimes unprotected.

--- net/syncthing/patches/patch-vendor_golang.org_x_sys_cpu_cpu__arm64.go
+++ net/syncthing/patches/patch-vendor_golang.org_x_sys_cpu_cpu__arm64.go
@@ -1,22 +1,20 @@
-$NetBSD: patch-vendor_golang.org_x_sys_cpu_cpu__arm64.go,v 1.1 2020/05/01 21:09:03 tnn Exp $
+$NetBSD: patch-vendor_golang.org_x_sys_cpu_cpu__arm64.go,v 1.2 2020/06/09 11:32:43 nia Exp $
 
 copypasta of devel/go-sys/patches/patch-cpu_cpu__arm64.go
 
---- vendor/golang.org/x/sys/cpu/cpu_arm64.go.orig	2020-04-08 07:13:30.000000000 +0000
+--- vendor/golang.org/x/sys/cpu/cpu_arm64.go.orig	2020-06-02 09:59:11.000000000 +0000
 +++ vendor/golang.org/x/sys/cpu/cpu_arm64.go
-@@ -10,8 +10,14 @@ const cacheLineSize = 64
+@@ -10,8 +10,12 @@ const cacheLineSize = 64
  
  func init() {
  	switch runtime.GOOS {
--	case "darwin":
-+	case "darwin", "netbsd":
- 		// iOS does not seem to allow reading these registers
+-	case "android", "darwin":
++	case "android", "darwin", "netbsd":
+ 		// Android and iOS don't seem to allow reading these registers.
 +		// NetBSD:
 +		// ID_AA64ISAR0_EL1 is a privileged register and cannot be read from EL0.
 +		// It can be read via sysctl(3). Example for future implementers:
 +		// https://nxr.netbsd.org/xref/src/usr.sbin/cpuctl/arch/aarch64.c
-+		ARM64.HasASIMD = true
-+		ARM64.HasFP = true
- 	case "android", "linux":
- 		doinit()
- 	default:
+ 		// Fake the minimal features expected by
+ 		// TestARM64minimalFeatures.
+ 		ARM64.HasASIMD = true
