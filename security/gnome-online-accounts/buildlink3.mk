b0817951dc3aa28b77363be4e2af56a04704045e
nia <nia@pkgsrc.org>
2020-06-10 19:50:42 +0000

    gnome-online-accounts: remove reference to wip

--- security/gnome-online-accounts/buildlink3.mk
+++ security/gnome-online-accounts/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/06/10 19:48:26 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/06/10 19:50:42 nia Exp $
 
 BUILDLINK_TREE+=	gnome-online-accounts
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	gnome-online-accounts
 GNOME_ONLINE_ACCOUNTS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.gnome-online-accounts+=	gnome-online-accounts>=3.36.0
-BUILDLINK_PKGSRCDIR.gnome-online-accounts?=	../../wip/gnome-online-accounts
+BUILDLINK_PKGSRCDIR.gnome-online-accounts?=	../../security/gnome-online-accounts
 
 .include "../../x11/gtk3/buildlink3.mk"
 .endif	# GNOME_ONLINE_ACCOUNTS_BUILDLINK3_MK
