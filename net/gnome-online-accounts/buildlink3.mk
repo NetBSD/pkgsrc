ce70b84d29d57190a3a8384441b2bf9762f929c7
nia <nia@pkgsrc.org>
2020-06-10 20:13:01 +0000

    gnome-online-accounts: Update to  3.36.0
    Overview of changes in 3.36.0
    =============================
    
    * Updated translations:
     Finnish
     Kurdish (Sorani)
    
    
    Overview of changes in 3.35.90
    ==============================
    
    * Bugs fixed:
     !41 Polish some rough edges in the Meson build
     !43 build: Remove AC_ISC_POSIX
     !44 build: Remove AC_HEADER_STDC
     !45 kerberos-identity-manager: Remove unused signal handler ID
     #43 daemon, dbus: Enable checking if a provider type is supported
    
    * Updated translations:
     Croatian
     English (British)
     Japanese
     Malay
    
    
    Overview of changes in 3.35.3
    =============================
    
    * Remove Pocket (!18)
    
    * Bugs fixed:
     !36 Drop the priv pointer from the GObject instance structures
     !37 Port away from deprecated GLib API like g_memove and g_get_current_time
     !39 webview: Port to JSCValue
     !40 Use the G_DECLARE_* macros to reduce GObject boilerplate
     #83 Add Exchange and Last.fm icons
    
    * Updated translations:
     Galician
     Russian
    
    
    Overview of changes in 3.35.1
    =============================
    
    * Bugs fixed:
     #8 icons: Unbreak providers without any visual identity
    
    * Updated translations:
     Basque
     Catalan
     Traditional Chinese (Taiwan)
     Czech
     Danish
     Dutch
     Finnish
     French
     Friulian
     Galician
     German
     Greek
     Hungarian
     Indonesian
     Italian
     Japanese
     Korean
     Latvian
     Lithuanian
     Persian
     Polish
     Portuguese (Brazilian)
     Punjabi
     Romanian
     Serbian
     Slovak
     Slovenian
     Spanish
     Swedish
     Turkish
    
    
    Overview of changes in 3.33.91
    ==============================
    
    * Support Fedora Account System (#26)
    
    * Bugs fixed:
        !21 owncloud: Use "Nextcloud", not "ownCloud", in user-visible errors
        !23 build: Allow building with gettext ≥ 0.20
        !26 webview: Make it more flexible in terms of its size
        !28 kerberos: Use GTask's task data slot to leverage the compiler
        !29 daemon: Return a D-Bus error if AddAccount fails to list all providers
        !30 kerberos: Use GTask's task data slot to leverage the compiler
        #73 build: Support the use of g_autolist with GoaObject
     764157 Port to GTask from GSimpleAsyncResult
    
    * Updated translations:
     Basque
     Czech
     German
     Indonesian
     Karbi
     Lithuanian
     Polish
     Romanian
     Spanish
     Swedish
    
    
    Overview of changes in 3.32.0
    =============================
    
    * Updated translations:
     Basque
     Finnish
     Gaelic (Scottish)
     Icelandic
    
    
    Overview of changes in 3.31.90
    ==============================
    
    * Bugs fixed:
        !17 icons: Replace PNGs with SVGs and add symbolics
        !19 google: Only request the email field when getting the user's identity
     796095 alarm: Refresh Kerberos tickets after a suspended system is resumed
    
    * Updated translations:
     Afrikaans
     French
     Japanese
     Kabyle
    
    
    Overview of changes in 3.31.3
    =============================
    
    * Remove Todoist
    
    * Bugs fixed:
        !14 build: Don't define _POSIX_PTHREAD_SEMANTICS and _REENTRANT
        !15 identity: Port away from deprecated g_type_class_add_private
     764157 Port to GTask from GSimpleAsyncResult
    
    * Updated translations:
     Belarusian
     Esperanto
     Estonian
     Malayalam
    
    
    Overview of changes in 3.30.0
    =============================
    
    * Updated translations:
     Arabic
    
    
    Overview of changes in 3.29.91
    ==============================
    
    * Bugs fixed:
      !2 build: Fix #FooObject style links
      !8 Remove additional unnecessary GDestroyNotify casts
     #10 build: Silence warning about _FORTIFY_SOURCE requiring optimization
    
    
    Overview of changes in 3.29.4
    =============================
    
    * Bugs fixed:
     !2 Port to meson build system
    
    * Updated translations:
     Occitan
    
    
    Overview of changes in 3.29.1
    =============================
    
    * Remove Telepathy support (795322)
    
    * Updated translations:
     Simplified Chinese
     Czech

--- net/gnome-online-accounts/buildlink3.mk
+++ net/gnome-online-accounts/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2020/06/02 08:22:51 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2020/06/10 20:13:01 nia Exp $
 
 BUILDLINK_TREE+=	gnome-online-accounts
 
@@ -9,11 +9,7 @@ BUILDLINK_API_DEPENDS.gnome-online-accounts+=	gnome-online-accounts>=3.29.1
 BUILDLINK_ABI_DEPENDS.gnome-online-accounts?=	gnome-online-accounts>=3.28.0nb14
 BUILDLINK_PKGSRCDIR.gnome-online-accounts?=	../../net/gnome-online-accounts
 
-.include "../../security/libgcrypt/buildlink3.mk"
-.include "../../security/libsecret/buildlink3.mk"
-.include "../../textproc/json-glib/buildlink3.mk"
-.include "../../www/librest07/buildlink3.mk"
-.include "../../www/webkit-gtk/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
 .endif	# GNOME_ONLINE_ACCOUNTS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnome-online-accounts
