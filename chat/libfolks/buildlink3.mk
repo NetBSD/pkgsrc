21e99f89cb304f783dfab1c0f8106ec2eb16ad95
nia <nia@pkgsrc.org>
2020-06-10 18:08:21 +0000

    libfolks: Update to 0.14.0

--- chat/libfolks/buildlink3.mk
+++ chat/libfolks/buildlink3.mk
@@ -1,19 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.10 2020/03/08 16:47:27 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2020/06/10 18:08:21 nia Exp $
 
 BUILDLINK_TREE+=	libfolks
 
 .if !defined(LIBFOLKS_BUILDLINK3_MK)
 LIBFOLKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libfolks+=	libfolks>=0.3.6
-BUILDLINK_ABI_DEPENDS.libfolks+=	libfolks>=0.4.2nb6
+BUILDLINK_API_DEPENDS.libfolks+=	libfolks>=0.14.0
+BUILDLINK_ABI_DEPENDS.libfolks+=	libfolks>=0.14.0
 BUILDLINK_PKGSRCDIR.libfolks?=		../../chat/libfolks
 
 .include "../../devel/glib2/buildlink3.mk"
-#.include "../../devel/gobject-introspection/buildlink3.mk"
-#.include "../../sysutils/dbus-glib/buildlink3.mk"
-.include "../../devel/libgee0.6/buildlink3.mk"
-.include "../../chat/telepathy-glib/buildlink3.mk"
+.include "../../devel/libgee/buildlink3.mk"
 .endif	# LIBFOLKS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libfolks
