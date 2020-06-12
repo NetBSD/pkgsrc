02400136c6a7819c5cba053324aaed43928ea8bf
sevan <sevan@pkgsrc.org>
2020-06-12 15:22:17 +0000

    Fix path in patch Update PLIST

--- devel/gmake/patches/patch-src_hash.c
+++ devel/gmake/patches/patch-src_hash.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-src_hash.c,v 1.1 2020/06/12 14:48:38 sevan Exp $
+$NetBSD: patch-src_hash.c,v 1.2 2020/06/12 15:22:18 sevan Exp $
 
 PCC says: hash.c:326: error: Constant "4294967295" is out of range
 
---- work/make-4.3/src/hash.c.orig	2020-06-12 14:40:24.838680043 +0000
-+++ work/make-4.3/src/hash.c
+--- src/hash.c.orig	2020-06-12 14:40:24.838680043 +0000
++++ src/hash.c
 @@ -321,7 +321,7 @@ round_up_2 (unsigned long n)
    n |= (n >> 8);
    n |= (n >> 16);
