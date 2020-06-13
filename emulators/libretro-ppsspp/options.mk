e08fc440d874a5ee6d83a00d4cc9a9e6f7ee489b
nia <nia@pkgsrc.org>
2020-06-13 10:45:19 +0000

    libretro-ppsspp: Don't assume ARM is rpi, bringing in line with other cores.

--- emulators/libretro-ppsspp/options.mk
+++ emulators/libretro-ppsspp/options.mk
@@ -1,24 +1,20 @@
-# $NetBSD: options.mk,v 1.1 2018/08/14 12:55:41 nia Exp $
+# $NetBSD: options.mk,v 1.2 2020/06/13 10:45:19 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.libretro-ppsspp
-PKG_OPTIONS_REQUIRED_GROUPS=	graphics
-PKG_OPTIONS_GROUP.graphics=	opengl
+PKG_OPTIONS_REQUIRED_GROUPS=	gl
+PKG_OPTIONS_GROUP.gl=		opengl
 
 .include "../../mk/bsd.fast.prefs.mk"
 
-.if !empty(MACHINE_ARCH:Mearm*)
-PKG_OPTIONS_GROUP.graphics+=	rpi
-.endif
-
 .if !empty(MACHINE_ARCH:Mearm*) && ${OPSYS} == "NetBSD"
-PKG_SUGGESTED_OPTIONS+=		rpi
-.else
-PKG_SUGGESTED_OPTIONS+=		opengl
+PKG_OPTIONS_GROUP.gl+=	rpi
 .endif
 
-.if ${MACHINE_ARCH} == "earmv7hf" || ${MACHINE_ARCH} == "earmv7hfeb"
-PKG_SUPPORTED_OPTIONS+=		simd
-PKG_SUGGESTED_OPTIONS+=		simd
+PKG_SUGGESTED_OPTIONS+=	opengl
+
+.if ${MACHINE_ARCH} == "earmv7hf"
+PKG_SUPPORTED_OPTIONS+=	simd
+PKG_SUGGESTED_OPTIONS+=	simd
 .endif
 
 .include "../../mk/bsd.options.mk"
@@ -29,12 +25,8 @@ MAKE_FLAGS+=	HAVE_NEON=1
 .endif
 
 .if !empty(PKG_OPTIONS:Mrpi)
-BUILD_MAKE_FLAGS+=	platform=rpi
-SUBST_CLASSES+=		vc
-SUBST_STAGE.vc=		pre-configure
-SUBST_MESSAGE.vc=	Fixing path to VideoCore libraries.
-SUBST_FILES.vc=		libretro/Makefile
-SUBST_SED.vc+=		-e 's;/opt/vc;${PREFIX};g'
+MAKE_FLAGS+=	GLES=1
+MAKE_FLAGS+=	GL_LIB="-lbrcmGLESv2"
 .include "../../misc/raspberrypi-userland/buildlink3.mk"
 .elif !empty(PKG_OPTIONS:Mopengl)
 .include "../../graphics/MesaLib/buildlink3.mk"
