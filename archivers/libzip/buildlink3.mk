3496e1b1b6d2dc5cd931358f09dda7eef384bfe3
wiz <wiz@pkgsrc.org>
2020-06-14 22:18:54 +0000

    libzip: update bl3.mk
    Stop linking lib/libzip/include, does not exist since 1.3.1; and depend
    on that version (from 2017).
    
    Link bin/zipcmp for the cmake infrastructure (the cmake support
    files are looking for zipcmp in the same place as the headers/library).
    
    The latter fixes a build problem in mgba reported by nia.

--- archivers/libzip/buildlink3.mk
+++ archivers/libzip/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.22 2020/06/05 10:44:48 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2020/06/14 22:18:54 wiz Exp $
 
 BUILDLINK_TREE+=	libzip
 
 .if !defined(LIBZIP_BUILDLINK3_MK)
 LIBZIP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libzip+=	libzip>=0.7.1
+BUILDLINK_API_DEPENDS.libzip+=	libzip>=1.3.1
 BUILDLINK_ABI_DEPENDS.libzip+=	libzip>=1.6.1nb2
 BUILDLINK_PKGSRCDIR.libzip?=	../../archivers/libzip
 
-BUILDLINK_INCDIRS.libzip=	include lib/libzip/include
+BUILDLINK_FILES.libzip+=               bin/zipcmp
 
 .include "../../archivers/bzip2/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
