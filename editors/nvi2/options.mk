8e1af156c8f83b1211d70ce25cb3ce59e7420e3c
nia <nia@pkgsrc.org>
2020-06-14 11:44:02 +0000

    editors: Add nvi2.
    A multibyte fork of the nvi editor for BSD

---
+++ editors/nvi2/options.mk
@@ -0,0 +1,25 @@
+# $NetBSD: options.mk,v 1.1 2020/06/14 11:44:02 nia Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.nvi2
+PKG_SUPPORTED_OPTIONS=	iconv wide-curses
+PKG_SUGGESTED_OPTIONS=	iconv wide-curses
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Miconv)
+.  include "../../converters/libiconv/buildlink3.mk"
+CMAKE_ARGS+=	-DUSE_ICONV=ON
+.else
+CMAKE_ARGS+=	-DUSE_ICONV=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mwide-curses)
+.  include "../../mk/curses.buildlink3.mk"
+.  if ${CURSES_TYPE} != "ncurses" # XXX: Why is this necessary!?
+CMAKE_ARGS+=	-DUSE_WIDECHAR=ON
+.  else
+CMAKE_ARGS+=	-DUSE_WIDECHAR=OFF
+.  endif
+.else
+CMAKE_ARGS+=	-DUSE_WIDECHAR=OFF
+.endif
