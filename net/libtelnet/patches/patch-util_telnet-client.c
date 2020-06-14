2ce1921689aed8ce9eb2a3dcf3cb5b27f714045c
nia <nia@pkgsrc.org>
2020-06-14 10:21:10 +0000

    libtelnet: Update to 0.23
    Version 0.23
    ============
    
    * Fixed release errors
    
    Version 0.22
    ============
    
    * EOL handling fixes
    * allocation fixes to Q option states
    * portability fixes

--- net/libtelnet/patches/patch-util_telnet-client.c
+++ net/libtelnet/patches/patch-util_telnet-client.c
@@ -1,27 +1,10 @@
-$NetBSD: patch-util_telnet-client.c,v 1.3 2019/03/20 16:02:10 jperkin Exp $
+$NetBSD: patch-util_telnet-client.c,v 1.4 2020/06/14 10:21:10 nia Exp $
 
 Provide workaround for a non-POSIX cfmakeraw call.
-Uses getaddrinfo.
 
---- util/telnet-client.c.orig	2012-03-18 21:07:49.000000000 +0000
+--- util/telnet-client.c.orig	2018-10-18 05:55:23.000000000 +0000
 +++ util/telnet-client.c
-@@ -9,9 +9,15 @@
-  * all present and future rights to this code under copyright law. 
-  */
- 
-+#ifdef __NetBSD__
-+#define _NETBSD_SOURCE
-+#endif
-+#if !defined(__sun) || __STDC_VERSION__-0 < 199901L
-+#define _XOPEN_SOURCE 520
- #if !defined(_POSIX_SOURCE)
- #	define _POSIX_SOURCE
- #endif
-+#endif
- #if !defined(_BSD_SOURCE)
- #	define _BSD_SOURCE
- #endif
-@@ -199,7 +205,15 @@ int main(int argc, char **argv) {
+@@ -204,7 +204,15 @@ int main(int argc, char **argv) {
  	tcgetattr(STDOUT_FILENO, &orig_tios);
  	atexit(_cleanup);
  	tios = orig_tios;
