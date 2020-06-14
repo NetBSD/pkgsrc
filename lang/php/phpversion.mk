4aab3039fc14d63bf482a904ac3eabc2ff6f2a2d
taca <taca@pkgsrc.org>
2020-06-14 05:59:17 +0000

    lang/php74: update to 7.4.7
    Update update to 7.4.7.
    
    
    11 Jun 2020, PHP 7.4.7
    
    - Core:
      . Fixed bug #79599 (coredump in set_error_handler). (Laruence)
      . Fixed bug #79566 (Private SHM is not private on Windows). (cmb)
      . Fixed bug #79489 (.user.ini does not inherit). (cmb)
      . Fixed bug #79600 (Regression in 7.4.6 when yielding an array based
        generator). (Nikita)
      . Fixed bug #79657 ("yield from" hangs when invalid value encountered).
        (Nikita)
    
    - FFI:
      . Fixed bug #79571 (FFI: var_dumping unions may segfault). (cmb)
    
    - GD:
      . Fixed bug #79615 (Wrong GIF header written in GD GIFEncode). (sageptr, cmb)
    
    - Opcache:
      . Fixed bug #79588 (Boolean opcache settings ignore on/off values). (cmb)
      . Fixed bug #79548 (Preloading segfault with inherited method using static
        variable). (Nikita)
      . Fixed bug #79603 (RTD collision with opcache). (Nikita)
    
    - Standard:
      . Fixed bug #79561 (dns_get_record() fails with DNS_ALL). (cmb)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.299 2020/06/14 05:56:51 taca Exp $
+# $NetBSD: phpversion.mk,v 1.300 2020/06/14 05:59:17 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -90,7 +90,7 @@ PHPVERSION_MK=	defined
 PHP56_VERSION=	5.6.40
 PHP72_VERSION=	7.2.31
 PHP73_VERSION=	7.3.19
-PHP74_VERSION=	7.4.6
+PHP74_VERSION=	7.4.7
 
 # Define initial release of major version.
 PHP56_RELDATE=	20140828
