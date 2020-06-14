045e634456d2cf71370fa13722f3ae5cac3597c8
nia <nia@pkgsrc.org>
2020-06-14 15:33:27 +0000

    Rename rust-bin's PKGNAME to rust-bin. Add rust.mk for rust packages.
    This allows rust-bin and rust to coexist in bulk builds (for testing, etc),
    but the packages still may not be installed at the same time.
    
    rust.mk as a solution for picking the correct rust variant was suggested
    by gdt@. It is intended to be included directly by packages that do not
    use cargo.mk, and indirectly by packages that do use cargo.mk.
    
    rust.mk provides one user-settable variable:
    
    RUST_TYPE
    	as before, whether to bootstrap rust from source or use
    	official binaries. may be "src" or "bin"
    
    And two package-settable variables:
    
    RUST_REQ
    	the minimum version of Rust required by the package.
    	defaults to "1.20.0"
    
    RUST_RUNTIME
    	whether Rust is a runtime dependency, may be "yes" or "no"

--- www/seamonkey/mozilla-common.mk
+++ www/seamonkey/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.2 2020/06/08 12:39:19 ryoon Exp $
+# $NetBSD: mozilla-common.mk,v 1.3 2020/06/14 15:33:28 nia Exp $
 #
 # common Makefile fragment for mozilla packages based on gecko 2.0.
 #
@@ -201,9 +201,8 @@ BUILDLINK_API_DEPENDS.nss+=	nss>=3.35
 #.include "../../graphics/cairo/buildlink3.mk"
 BUILDLINK_DEPMETHOD.clang=	build
 .include "../../lang/clang/buildlink3.mk"
-BUILDLINK_API_DEPENDS.rust+=	rust>=1.23.0
-BUILDLINK_DEPMETHOD.rust=	build
-.include "../../lang/rust/buildlink3.mk"
+RUST_REQ=	1.23.0
+.include "../../lang/rust/rust.mk"
 #BUILDLINK_API_DEPENDS.libvpx+=	libvpx>=1.3.0
 #.include "../../multimedia/libvpx/buildlink3.mk"
 .include "../../net/libIDL/buildlink3.mk"
