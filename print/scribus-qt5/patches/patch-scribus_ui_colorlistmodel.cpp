b3352063a52397db8eac9312aeb828bc71f3c34a
ryoon <ryoon@pkgsrc.org>
2020-06-14 07:35:58 +0000

    print/scribus-qt5: import scribus-1.5.6pre20200611
    Scribus is a publishing layout program for X using the QT interface.
    It can be used to create stationery, small posters and brochures
    and to layout newsletters and other documents. It can export in
    Postscript and PDF formats; and the data is saved in XML format.
    
    Scribus aims to be similar to Adobe PageMaker and Quark XPress.
    It can do typical tasks like kerning text and placing and rotating
    text and images.
    
    This package provides preview release of Scribus (Qt5 port).

---
+++ print/scribus-qt5/patches/patch-scribus_ui_colorlistmodel.cpp
@@ -0,0 +1,15 @@
+$NetBSD: patch-scribus_ui_colorlistmodel.cpp,v 1.1 2020/06/14 07:35:58 ryoon Exp $
+
+* Fix segfault.
+
+--- scribus/ui/colorlistmodel.cpp.orig	2020-06-11 09:56:15.000000000 +0000
++++ scribus/ui/colorlistmodel.cpp
+@@ -31,8 +31,6 @@ ColorPixmapValue::ColorPixmapValue( cons
+ 	m_name = colName;
+ }
+ 
+-ColorPixmapValue ColorListModel::m_NoneColor(ScColor(), nullptr, CommonStrings::None);
+-
+ ColorListModel::ColorListModel(QObject *parent)
+ 	          : QAbstractItemModel(parent)
+ {
---
+++ print/scribus-qt5/patches/patch-scribus_ui_colorlistmodel.h
@@ -0,0 +1,21 @@
+$NetBSD: patch-scribus_ui_colorlistmodel.h,v 1.1 2020/06/14 07:35:58 ryoon Exp $
+
+* Fix segfault.
+
+--- scribus/ui/colorlistmodel.h.orig	2020-06-11 09:56:15.000000000 +0000
++++ scribus/ui/colorlistmodel.h
+@@ -103,12 +103,9 @@ protected:
+ 	// SortRule m_sortRule
+ 	SortRule m_sortRule;
+ 
+-	//! Dummy object used to represent None Color
+-	static ColorPixmapValue m_NoneColor;
+-
+ 	static bool compareColorNames(const ColorPixmapValue& v1, const ColorPixmapValue& v2);
+ 	static bool compareColorValues(const ColorPixmapValue& v1, const ColorPixmapValue& v2);
+ 	static bool compareColorTypes(const ColorPixmapValue& v1, const ColorPixmapValue& v2);
+ };
+ 
+-#endif // COLORLISTMODEL_H
+\ No newline at end of file
++#endif // COLORLISTMODEL_H
