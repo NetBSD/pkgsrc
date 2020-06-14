9826c114659521f4163afd40640b9c6b928ef97e
nia <nia@pkgsrc.org>
2020-06-14 15:38:18 +0000

    rust: More rust.mk explanation.

--- lang/rust/buildlink3.mk
+++ lang/rust/buildlink3.mk
@@ -1,4 +1,8 @@
-# $NetBSD: buildlink3.mk,v 1.4 2020/05/18 16:17:20 nia Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/06/14 15:38:18 nia Exp $
+
+#
+# DO NOT include this directly! Use rust.mk instead.
+#
 
 BUILDLINK_TREE+=	rust
 
@@ -6,14 +10,7 @@ BUILDLINK_TREE+=	rust
 RUST_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.rust+=	rust>=1.20.0
-
-.include "../../lang/rust/type.mk"
-.if ${RUST_TYPE} != "bin"
 BUILDLINK_PKGSRCDIR.rust?=	../../lang/rust
-.else
-BUILDLINK_PKGSRCDIR.rust?=	../../lang/rust-bin
-.endif
-
 BUILDLINK_PASSTHRU_DIRS+=	${PREFIX}/lib/rustlib
 .endif
 
