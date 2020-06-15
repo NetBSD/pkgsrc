a579f3cbd2d13d35c2737f054f005bd5e38a66d8
adam <adam@pkgsrc.org>
2020-06-15 16:54:38 +0000

    ledger: updated to 3.2.1
    changes in 3.2.1:
    
    - Fix regression with expression evaluation by reverting commit
     `Correction to the way parens are parsed in query expressions`
    
    - Fix --invert breakage by reverting commit `Change --invert to invert
     displayed amounts and totals, not amounts`
    
    - Fix performance regression by reverting commit `Compare price
     annotations using their textual rendering`
    
    - Fix library path issue
    
    - Allow specifying the Python version
    
    - Some documentation fixes

--- finance/ledger/options.mk
+++ finance/ledger/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2020/05/01 11:48:26 riastradh Exp $
+# $NetBSD: options.mk,v 1.4 2020/06/15 16:54:38 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ledger
 PKG_SUPPORTED_OPTIONS+=	python
@@ -13,4 +13,18 @@ CMAKE_ARGS+=		-DUSE_PYTHON:BOOL=TRUE
 PLIST.python=		yes
 PLIST_SUBST+=		PYSITELIB=${PYSITELIB:Q}
 .include "../../devel/py-boost/buildlink3.mk"
+
+.  include "../../mk/bsd.prefs.mk"
+.  if ${OPSYS} == "Darwin"
+SOEXT=dylib
+.  else
+SOEXT=so
+.  endif
+
+# Remove when src/CMakeLists.txt (see FIXME there) stops copying
+# uninstalled library as Python module. Make symlink here to
+# avoid problem with install_name on Darwin and save disk-space.
+post-install:
+	cd ${DESTDIR}${PREFIX}/${PYSITELIB} && \
+	${LN} -s -f ${DESTDIR}${PREFIX}/lib/libledger.${SOEXT} ledger.so
 .endif
