4f924a23a6419db6246df9d5de15234bea9336bb
nia <nia@pkgsrc.org>
2020-06-15 15:44:22 +0000

    thunderbird: Sync with firefox68
    - Re-enable multiprocess mode
    - Drop hacks for crossprocess semaphores on NetBSD
    - Drop OSS support
    - Drop unused gnome option
    
    Bump PKGREVISION

--- mail/thunderbird/patches/patch-browser_app_profile_firefox.js
+++ mail/thunderbird/patches/patch-browser_app_profile_firefox.js
@@ -1,8 +1,8 @@
-$NetBSD: patch-browser_app_profile_firefox.js,v 1.2 2019/09/21 10:55:17 ryoon Exp $
+$NetBSD: patch-browser_app_profile_firefox.js,v 1.3 2020/06/15 15:44:22 nia Exp $
 
---- browser/app/profile/firefox.js.orig	2019-09-09 23:43:23.000000000 +0000
+--- browser/app/profile/firefox.js.orig	2020-06-02 14:07:51.000000000 +0000
 +++ browser/app/profile/firefox.js
-@@ -1862,6 +1862,15 @@ pref("prio.publicKeyB", "26E6674E65425B8
+@@ -1862,6 +1862,12 @@ pref("prio.publicKeyB", "26E6674E65425B8
  pref("toolkit.coverage.enabled", false);
  pref("toolkit.coverage.endpoint.base", "https://coverage.mozilla.org");
  
@@ -11,9 +11,6 @@ $NetBSD: patch-browser_app_profile_firefox.js,v 1.2 2019/09/21 10:55:17 ryoon Ex
 +
 +// Enable system addons, for example langpacks from www/firefox-l10n
 +pref("extensions.autoDisableScopes", 11);
-+
-+// Disable multiprocess window support. Workaround for PR 53273.
-+pref("browser.tabs.remote.autostart", false);
 +
  // Discovery prefs
  pref("browser.discovery.enabled", true);
--- mail/thunderbird/patches/patch-media_libcubeb_src_cubeb.c
+++ mail/thunderbird/patches/patch-media_libcubeb_src_cubeb.c
@@ -1,6 +1,6 @@
-$NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.2 2019/09/21 10:55:17 ryoon Exp $
+$NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.3 2020/06/15 15:44:22 nia Exp $
 
---- media/libcubeb/src/cubeb.c.orig	2019-09-09 23:43:34.000000000 +0000
+--- media/libcubeb/src/cubeb.c.orig	2020-06-02 14:07:57.000000000 +0000
 +++ media/libcubeb/src/cubeb.c
 @@ -51,6 +51,9 @@ int wasapi_init(cubeb ** context, char c
  #if defined(USE_SNDIO)
@@ -12,28 +12,7 @@ $NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.2 2019/09/21 10:55:17 ryoon Exp $
  #if defined(USE_OPENSL)
  int opensl_init(cubeb ** context, char const * context_name);
  #endif
-@@ -60,6 +63,9 @@ int audiotrack_init(cubeb ** context, ch
- #if defined(USE_KAI)
- int kai_init(cubeb ** context, char const * context_name);
- #endif
-+#if defined(USE_OSS)
-+int oss_init(cubeb ** context, char const * context_name);
-+#endif
- 
- static int
- validate_stream_params(cubeb_stream_params * input_stream_params,
-@@ -160,6 +166,10 @@ cubeb_init(cubeb ** context, char const 
- #if defined(USE_KAI)
-       init_oneshot = kai_init;
- #endif
-+    } else if (!strcmp(backend_name, "oss")) {
-+#if defined(USE_OSS)
-+      init_oneshot = oss_init;
-+#endif
-     } else {
-       /* Already set */
-     }
-@@ -195,6 +205,9 @@ cubeb_init(cubeb ** context, char const 
+@@ -195,6 +198,9 @@ cubeb_init(cubeb ** context, char const 
  #if defined(USE_SNDIO)
      sndio_init,
  #endif
@@ -43,13 +22,3 @@ $NetBSD: patch-media_libcubeb_src_cubeb.c,v 1.2 2019/09/21 10:55:17 ryoon Exp $
  #if defined(USE_OPENSL)
      opensl_init,
  #endif
-@@ -204,6 +217,9 @@ cubeb_init(cubeb ** context, char const 
- #if defined(USE_KAI)
-     kai_init,
- #endif
-+#if defined(USE_OSS)
-+    oss_init,
-+#endif
-   };
-   int i;
- 
