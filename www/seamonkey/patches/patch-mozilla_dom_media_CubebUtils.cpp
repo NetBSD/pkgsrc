2935f395cf4ea6448e3d7bcbebfbae8e7029f99e
nia <nia@pkgsrc.org>
2020-06-16 09:09:02 +0000

    seamonkey: Remove hacks to disable multiprocess mode and remains of OSS

--- www/seamonkey/patches/patch-mozilla_dom_media_CubebUtils.cpp
+++ www/seamonkey/patches/patch-mozilla_dom_media_CubebUtils.cpp
@@ -1,14 +1,12 @@
-$NetBSD: patch-mozilla_dom_media_CubebUtils.cpp,v 1.1 2020/05/06 14:34:53 ryoon Exp $
+$NetBSD: patch-mozilla_dom_media_CubebUtils.cpp,v 1.2 2020/06/16 09:09:02 nia Exp $
 
 --- mozilla/dom/media/CubebUtils.cpp.orig	2020-04-01 11:01:50.000000000 +0000
 +++ mozilla/dom/media/CubebUtils.cpp
-@@ -95,7 +95,8 @@ const char* AUDIOSTREAM_BACKEND_ID_STR[]
+@@ -93,6 +93,7 @@ const char* AUDIOSTREAM_BACKEND_ID_STR[]
+   "winmm",
+   "directsound",
    "sndio",
++  "sun",
    "opensl",
    "audiotrack",
--  "kai"
-+  "kai",
-+  "oss",
- };
- /* Index for failures to create an audio stream the first time. */
- const int CUBEB_BACKEND_INIT_FAILURE_FIRST =
+   "kai"
