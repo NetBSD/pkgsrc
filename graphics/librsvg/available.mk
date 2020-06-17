21b712d80003e59b51ec2ad2af224d4109d2ab9b
nia <nia@pkgsrc.org>
2020-06-17 10:13:25 +0000

    librsvg: Disable rust version on NetBSD/arm (32-bit)
    It doesn't have the address space to build a rust compiler so this
    is currently broken.

--- graphics/librsvg/available.mk
+++ graphics/librsvg/available.mk
@@ -1,4 +1,4 @@
-# $NetBSD: available.mk,v 1.1 2019/06/08 19:51:38 leot Exp $
+# $NetBSD: available.mk,v 1.2 2020/06/17 10:13:25 nia Exp $
 #
 # This mk fragment define LIBRSVG_TYPE variable used in other librsvg mk
 # fragments.
@@ -18,7 +18,9 @@ LIBRSVG_AVAILABLE_MK=	defined
 .include "../../mk/bsd.prefs.mk"
 
 .include "../../lang/rust/platform.mk"
-.if !empty(PLATFORM_SUPPORTS_RUST:M[Yy][Ee][Ss])
+# https://mail-index.netbsd.org/tech-pkg/2020/06/16/msg023380.html
+.if !empty(PLATFORM_SUPPORTS_RUST:M[Yy][Ee][Ss]) && \
+    empty(MACHINE_PLATFORM:MNetBSD-*-earm*)
 LIBRSVG_TYPE?=	rust
 .else
 LIBRSVG_TYPE?=	c
