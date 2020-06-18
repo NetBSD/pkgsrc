27a6b1efa420ae552f35f5e99a8e44f095883c45
nia <nia@pkgsrc.org>
2020-06-18 14:52:12 +0000

    olive-editor: fix Qt 5.15 fallout

---
+++ multimedia/olive-editor/patches/patch-ui_graphview.cpp
@@ -0,0 +1,14 @@
+$NetBSD: patch-ui_graphview.cpp,v 1.1 2020/06/18 14:52:12 nia Exp $
+
+Fix Qt 5.15 build issues
+
+--- ui/graphview.cpp.orig	2019-07-09 15:16:17.000000000 +0000
++++ ui/graphview.cpp
+@@ -21,6 +21,7 @@
+ #include "graphview.h"
+ 
+ #include <QPainter>
++#include <QPainterPath>
+ #include <QMouseEvent>
+ #include <QtMath>
+ #include <cfloat>
