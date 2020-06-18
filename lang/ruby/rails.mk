10b1a280d3517c0cbe87acde2d6ffcb150c87a74
taca <taca@pkgsrc.org>
2020-06-18 13:38:45 +0000

    lang/rails60: update to 6.0.3.2
    Update Ruby on Rails to 6.0.3.2.
    
    www/ruby-actionpack60 is the really updated package and other packages
    have no change except version.
    
    CHANGELOG of www/ruby-actionpack60 is here:
    
    ## Rails 6.0.3.2 (June 17, 2020) ##
    
    * [CVE-2020-8185] Only allow ActionableErrors if
      show_detailed_exceptions is enabled

--- lang/ruby/rails.mk
+++ lang/ruby/rails.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rails.mk,v 1.86 2020/05/19 17:09:49 taca Exp $
+# $NetBSD: rails.mk,v 1.87 2020/06/18 13:38:45 taca Exp $
 
 .if !defined(_RUBY_RAILS_MK)
 _RUBY_RAILS_MK=	# defined
@@ -49,7 +49,7 @@ _RUBY_RAILS_MK=	# defined
 #
 RUBY_RAILS51_VERSION?=	5.1.7
 RUBY_RAILS52_VERSION?=	5.2.4.3
-RUBY_RAILS60_VERSION?=	6.0.3.1
+RUBY_RAILS60_VERSION?=	6.0.3.2
 
 RUBY_RAILS_ACCEPTED?=	# defined
 RUBY_RAILS_DEFAULT?=	52
