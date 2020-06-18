857f0d76f97a5d4a943de05cdefb570637ec8fef
gdt <gdt@pkgsrc.org>
2020-06-18 15:00:21 +0000

    rust/platform.mk: Note PR about earmv7, and requirement for PR links
    Following discussion on tech-net, note that disabling rust on a
    platform where it ought to work requires a link to an open PR.
    
    Add a link to the PR about NetBSD/earmv7hf.

--- lang/rust/platform.mk
+++ lang/rust/platform.mk
@@ -1,10 +1,17 @@
-# $NetBSD: platform.mk,v 1.3 2020/05/19 22:29:05 nia Exp $
+# $NetBSD: platform.mk,v 1.4 2020/06/18 15:00:21 gdt Exp $
+
+# This file encodes whether a given platform has support for rust.
+
+# Platforms where rust ought to work but does not require a link to an
+# open PR.
 
 .if !defined(PLATFORM_SUPPORTS_RUST)
 
 # Rust needs NetBSD>7
 .  for _rust_arch in aarch64 armv7 i386 powerpc sparc64 x86_64
 .    for _rust_os in Darwin FreeBSD Linux NetBSD SunOS
+# rust fails to build on NetBSD/earmv7
+# http://gnats.netbsd.org/cgi-bin/query-pr-single.pl?number=54621
 .      if ${OPSYS} != "NetBSD" || empty(OS_VERSION:M[0-7].*)
 RUST_PLATFORMS+=	${_rust_os}-*-${_rust_arch}
 .      endif
