e6b2f8a818848be7402e56b14afd7f274c308196
scole <scole@pkgsrc.org>
2020-06-19 21:17:46 +0000

    Welcome to Squashfs 4.4.  This is the first release in over 5 years, and there are substantial improvements: reproducible builds, new compressors, CVE fixes, security hardening and new options for Mksquashfs/Unsquashfs.
    Please see the INSTALL file for instructions on installing the tools,
    and the USAGE file for documentation on how to use the tools.
    
    Summary of changes
    ---------------------------------------
    
    1. Mksquashfs now generates reproducible images by default.  Mkfs time and
       file timestamps can also be specified.
    
    2. Support for the Zstandard (ZSTD) compression algorithm has been added.
    
    3. Pseudo files now support symbolic links.
    
    4. CVE-2015-4645 and CVE-2015-4646 have been fixed.
    
    5. Unsquashfs has been further hardened against corrupted filestems.
    
    6. Unsquashfs is now more strict about error handling.
    
    7. Miscellaneous new options and major bug fixes for Mksquashfs.
    
    8. Miscellaneous new options and major bug fixes for Unsquashfs.
    
    9. Squashfs-tools 4.4 is compatible with all earlier 4.x filesystems
       and releases.
    
    For pkgsrc changes, now GitHub is now the main repository and using libtool

--- filesystems/squashfs/options.mk
+++ filesystems/squashfs/options.mk
@@ -1,20 +1,18 @@
-# $NetBSD: options.mk,v 1.3 2019/11/03 19:26:22 rillig Exp $
+# $NetBSD: options.mk,v 1.4 2020/06/19 21:17:46 scole Exp $
 
+# xxx unsquashfs hung for me without any error message when extracting
+# an xattr archive and xattr was an included as a package option
 PKG_OPTIONS_VAR=	PKG_OPTIONS.squashfs
-PKG_SUPPORTED_OPTIONS=	zlib lz4 lzo xz xattr
-PKG_SUGGESTED_OPTIONS=	zlib lzo xz
+PKG_SUPPORTED_OPTIONS=	zlib xz lzo lz4 zstd xattr reproducible
+PKG_SUGGESTED_OPTIONS=	zlib xz lzo lz4 zstd xattr reproducible
 
 .include "../../mk/bsd.options.mk"
-
-# XXX any other OS support xattr?
-.if ${OPSYS} == "Linux"
-PKG_SUGGESTED_OPTIONS+=	xattr
-.endif
 
 .if empty(PKG_OPTIONS:Mzlib) && \
     empty(PKG_OPTIONS:Mlz4) && \
     empty(PKG_OPTIONS:Mlzo) && \
-    empty(PKG_OPTIONS:Mxz)
+    empty(PKG_OPTIONS:Mxz) && \
+    empty(PKG_OPTIONS:Mzstd)
 PKG_FAIL_REASON=	"need at least one compression algorithm selected"
 .endif
 
@@ -29,7 +27,7 @@ BUILD_MAKE_FLAGS+=	GZIP_SUPPORT=0
 .if !empty(PKG_OPTIONS:Mlz4)
 .include "../../archivers/lz4/buildlink3.mk"
 BUILD_MAKE_FLAGS+=	LZ4_SUPPORT=1
-SQUASH_COMPRESSORS+=	lz4	
+SQUASH_COMPRESSORS+=	lz4
 .else
 BUILD_MAKE_FLAGS+=	LZ4_SUPPORT=0
 .endif
@@ -50,6 +48,14 @@ SQUASH_COMPRESSORS+=	xz
 BUILD_MAKE_FLAGS+=	XZ_SUPPORT=0
 .endif
 
+.if !empty(PKG_OPTIONS:Mzstd)
+.include "../../archivers/zstd/buildlink3.mk"
+BUILD_MAKE_FLAGS+=	ZSTD_SUPPORT=1
+SQUASH_COMPRESSORS+=	zstd
+.else
+BUILD_MAKE_FLAGS+=	ZSTD_SUPPORT=0
+.endif
+
 .if !empty(PKG_OPTIONS:Mxattr)
 BUILD_MAKE_FLAGS+=	XATTR_SUPPORT=1
 .else
@@ -65,4 +71,10 @@ BUILD_MAKE_FLAGS+=	COMP_DEFAULT=gzip
 .  else
 BUILD_MAKE_FLAGS+=	COMP_DEFAULT=${SQUASH_COMPRESSORS:Nxattr:[1]}
 .  endif
+.endif
+
+.if !empty(PKG_OPTIONS:Mreproducible)
+BUILD_MAKE_FLAGS+=	REPRODUCIBLE_DEFAULT=1
+.else
+BUILD_MAKE_FLAGS+=	REPRODUCIBLE_DEFAULT=0
 .endif
