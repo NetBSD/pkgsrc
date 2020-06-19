4d74b0deb1f336e73172a6d48a54e2668bc3ed61
hauke <hauke@pkgsrc.org>
2020-06-19 10:05:07 +0000

    PKG_SYSCONFDIR fixes.
    Install a sample config file. Mention, but do not install lldpd.d
    directory.
    
    The config file is not mandatory, adjust rc.d script.

--- net/lldpd/files/lldpd.sh
+++ net/lldpd/files/lldpd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: lldpd.sh,v 1.1 2013/05/09 01:39:43 mef Exp $
+# $NetBSD: lldpd.sh,v 1.2 2020/06/19 10:05:07 hauke Exp $
 #
 
 # PROVIDE: lldpd
@@ -12,7 +12,6 @@
 name="lldpd"
 rcvar="lldpd"
 command="@PREFIX@/sbin/${name}"
-#required_files="@PKG_SYSCONFDIR@/lldpd.conf"
 
 load_rc_config $name
 run_rc_command "$1"
