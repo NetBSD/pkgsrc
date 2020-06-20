8ed1635a93821d877597e932e0db7567ee605d16
ryoon <ryoon@pkgsrc.org>
2020-06-20 00:10:24 +0000

    vte3: Update to 0.60.3
    * Use meson to build.
    * Enable vala and gobject-introspection.
    
    Changelog:
    Not available.

--- x11/vte3/patches/patch-src_pty.cc
+++ x11/vte3/patches/patch-src_pty.cc
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_pty.cc,v 1.3 2018/06/22 14:34:43 jperkin Exp $
+$NetBSD: patch-src_pty.cc,v 1.4 2020/06/20 00:10:24 ryoon Exp $
 
 NetBSD fix
 Use correct includes on SunOS.
 Don't use packet mode on SunOS.
 
---- src/pty.cc.orig	2018-05-21 19:30:33.000000000 +0000
+--- src/pty.cc.orig	2020-05-13 09:51:07.000000000 +0000
 +++ src/pty.cc
-@@ -60,7 +60,9 @@
+@@ -64,7 +64,9 @@
  #ifdef HAVE_PTY_H
  #include <pty.h>
  #endif
@@ -17,7 +17,7 @@ Don't use packet mode on SunOS.
  #include <stropts.h>
  #endif
  #include <glib.h>
-@@ -605,12 +607,14 @@ fd_setup(int fd)
+@@ -638,12 +640,14 @@ fd_setup(int fd)
                  return -1;
          }
  
@@ -32,7 +32,7 @@ Don't use packet mode on SunOS.
  
          return 0;
  }
-@@ -632,7 +636,12 @@ _vte_pty_open_posix(void)
+@@ -665,7 +669,12 @@ _vte_pty_open_posix(void)
          fd = posix_openpt(O_RDWR | O_NOCTTY | O_NONBLOCK | O_CLOEXEC);
  #ifndef __linux__
          /* Other kernels may not support CLOEXEC or NONBLOCK above, so try to fall back */
@@ -46,7 +46,7 @@ Don't use packet mode on SunOS.
          if (fd == -1 && errno == EINVAL) {
                  /* Try without NONBLOCK and apply the flag afterward */
                  need_nonblocking = true;
-@@ -668,12 +677,14 @@ _vte_pty_open_posix(void)
+@@ -701,12 +710,14 @@ _vte_pty_open_posix(void)
          }
  #endif /* !linux */
  
@@ -59,5 +59,5 @@ Don't use packet mode on SunOS.
          }
 +#endif
  
- 	_vte_debug_print(VTE_DEBUG_PTY, "Allocated pty on fd %d.\n", (int)fd);
- 
+         if (grantpt(fd) != 0) {
+                 vte::util::restore_errno errsv;
