f2cdc860cf72fb97d791c96f739870d0037ed571
leot <leot@pkgsrc.org>
2020-06-20 10:43:43 +0000

    zbar: Simplify PKG_BUILD_OPTIONS check in bl3
    Pointed out by pkglint.

--- graphics/zbar/buildlink3.mk
+++ graphics/zbar/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/12/04 11:17:32 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/06/20 10:43:43 leot Exp $
 
 BUILDLINK_TREE+=	zbar
 
@@ -11,7 +11,7 @@ BUILDLINK_PKGSRCDIR.zbar?=	../../graphics/zbar
 pkgbase:=	zbar
 .include "../../mk/pkg-build-options.mk"
 
-.if !empty(PKG_BUILD_OPTIONS.zbar:Mx11)
+.if ${PKG_BUILD_OPTIONS.zbar:Mx11}
 .include "../../x11/libICE/buildlink3.mk"
 .include "../../x11/libSM/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
