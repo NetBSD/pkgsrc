f9b99c2df5e3ac911cdd4896432db434dbcf5bba
thorpej <thorpej@pkgsrc.org>
2020-06-20 17:42:34 +0000

    Update to u-boot-2020.04.

--- sysutils/u-boot-bananapi-r2/patches/patch-include_configs_mt7623.h
+++ sysutils/u-boot-bananapi-r2/patches/patch-include_configs_mt7623.h
@@ -1,12 +1,12 @@
-$NetBSD: patch-include_configs_mt7623.h,v 1.1 2020/06/20 17:19:37 thorpej Exp $
+$NetBSD: patch-include_configs_mt7623.h,v 1.2 2020/06/20 17:42:34 thorpej Exp $
 
 Set the bootm_size, kernel_addr_r, fdt_addr_r, scriptaddr, pxefile_addr_r,
 ramdisk_addr_r, and fdtfile environment varibles.
 
---- include/configs/mt7623.h.orig	2019-05-20 21:48:27.989085653 -0700
-+++ include/configs/mt7623.h	2019-05-20 21:59:17.210366023 -0700
-@@ -45,14 +45,39 @@
- #define CONFIG_SUPPORT_EMMC_BOOT
+--- include/configs/mt7623.h.orig	2020-04-13 15:02:18.000000000 +0000
++++ include/configs/mt7623.h	2020-06-20 17:33:23.538928996 +0000
+@@ -42,14 +42,39 @@
+ #define MMC_SUPPORTS_TUNING
  
  /* DRAM */
 +#define SDRAM_OFFSET(x) 0x8##x
@@ -15,7 +15,7 @@ ramdisk_addr_r, and fdtfile environment varibles.
  /* This is needed for kernel booting */
  #define FDT_HIGH			"fdt_high=0xac000000\0"
  
-+/*      
++/*
 + * 128M RAM (256M minimum minus 64MB heap + 64MB for u-boot, stack, fb, etc.
 + * 32M uncompressed kernel, 16M compressed kernel, 1M fdt,
 + * 1M script, 1M pxe and the ramdisk at the end.
