22a044752be743a9db8945259996e37fab4cd8a5
ryoon <ryoon@pkgsrc.org>
2020-06-20 00:10:24 +0000

    vte3: Update to 0.60.3
    * Use meson to build.
    * Enable vala and gobject-introspection.
    
    Changelog:
    Not available.

--- x11/vte3/patches/patch-src_vte.cc
+++ x11/vte3/patches/patch-src_vte.cc
@@ -1,21 +1,11 @@
-$NetBSD: patch-src_vte.cc,v 1.2 2018/06/22 14:34:43 jperkin Exp $
+$NetBSD: patch-src_vte.cc,v 1.3 2020/06/20 00:10:24 ryoon Exp $
 
 Use correct includes on SunOS.
 Don't use packet mode on SunOS.
 
---- src/vte.cc.orig	2018-05-21 19:31:53.000000000 +0000
+--- src/vte.cc.orig	2020-05-13 09:51:07.000000000 +0000
 +++ src/vte.cc
-@@ -25,6 +25,9 @@
- #include <string.h>
- #include <sys/ioctl.h>
- #include <sys/param.h> /* howmany() */
-+#ifdef __sun
-+#include <sys/ptyvar.h>
-+#endif
- #include <errno.h>
- #include <fcntl.h>
- #include <math.h>
-@@ -3970,6 +3973,9 @@ VteTerminalPrivate::pty_io_read(GIOChann
+@@ -3966,6 +3966,9 @@ Terminal::pty_io_read(int const fd,
  			bp = chunk->data + chunk->len;
  			len = 0;
  			do {
@@ -25,7 +15,7 @@ Don't use packet mode on SunOS.
                                  /* We'd like to read (fd, bp, rem); but due to TIOCPKT mode
                                   * there's an extra input byte returned at the beginning.
                                   * We need to see what that byte is, but otherwise drop it
-@@ -3980,6 +3986,7 @@ VteTerminalPrivate::pty_io_read(GIOChann
+@@ -3977,6 +3980,7 @@ Terminal::pty_io_read(int const fd,
                                  int ret = read (fd, bp - 1, rem + 1);
                                  pkt_header = bp[-1];
                                  bp[-1] = save;
@@ -33,19 +23,28 @@ Don't use packet mode on SunOS.
  				switch (ret){
  					case -1:
  						err = errno;
-@@ -3988,6 +3995,7 @@ VteTerminalPrivate::pty_io_read(GIOChann
- 						eof = TRUE;
+@@ -3985,12 +3989,15 @@ Terminal::pty_io_read(int const fd,
+ 						eos = true;
  						goto out;
  					default:
 +#ifndef __sun
                                                  ret--;
- 
-                                                 if (pkt_header & TIOCPKT_IOCTL) {
-@@ -4008,6 +4016,7 @@ VteTerminalPrivate::pty_io_read(GIOChann
-                                                 } else if (pkt_header & TIOCPKT_START) {
-                                                         pty_scroll_lock_changed(false);
+-
+                                                 if (pkt_header == TIOCPKT_DATA) {
++#endif
+                                                         bp += ret;
+                                                         rem -= ret;
+                                                         len += ret;
++#ifndef __sun
++                                                ret--;
+                                                 } else {
+                                                         if (pkt_header & TIOCPKT_IOCTL) {
+                                                                 /* We'd like to always be informed when the termios change,
+@@ -4012,6 +4019,7 @@ Terminal::pty_io_read(int const fd,
+                                                                 pty_scroll_lock_changed(false);
+                                                         }
                                                  }
 +#endif
- 
- 						bp += ret;
- 						rem -= ret;
+ 						break;
+ 				}
+ 			} while (rem);
