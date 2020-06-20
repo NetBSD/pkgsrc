b0e4ea5d84c91a1f0a39bd251171a7b32288dc78
ryoon <ryoon@pkgsrc.org>
2020-06-20 00:10:24 +0000

    vte3: Update to 0.60.3
    * Use meson to build.
    * Enable vala and gobject-introspection.
    
    Changelog:
    Not available.

--- x11/vte3/buildlink3.mk
+++ x11/vte3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2020/05/22 10:55:53 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2020/06/20 00:10:24 ryoon Exp $
 
 BUILDLINK_TREE+=	vte3
 
@@ -9,10 +9,10 @@ BUILDLINK_API_DEPENDS.vte3+=	vte3>=0.52.2
 BUILDLINK_ABI_DEPENDS.vte3+=	vte3>=0.54.3nb5
 BUILDLINK_PKGSRCDIR.vte3?=	../../x11/vte3
 
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
 .include "../../devel/pcre2/buildlink3.mk"
-.include "../../security/gnutls/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
-.include "../../mk/termcap.buildlink3.mk"
 .endif # VTE3_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-vte3
