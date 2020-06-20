0554c906c1f6974d127975e6f3dad7d6a6a021e5
thorpej <thorpej@pkgsrc.org>
2020-06-20 14:33:35 +0000

    mipsel-none-elf-gcc: Import version 8.3.0
    Copied mainly from cross/powerpc-none-elf-gcc.

---
+++ cross/mipsel-none-elf-gcc/patches/patch-libcc1_connection.cc
@@ -0,0 +1,12 @@
+$NetBSD: patch-libcc1_connection.cc,v 1.1 2020/06/20 14:33:35 thorpej Exp $
+
+--- libcc1/connection.cc.orig	2014-11-13 10:22:22.000000000 +0000
++++ libcc1/connection.cc
+@@ -23,6 +23,7 @@ along with GCC; see the file COPYING3.  
+ #include <sys/types.h>
+ #include <string.h>
+ #include <errno.h>
++#include <sys/select.h>
+ #include "marshall.hh"
+ #include "connection.hh"
+ #include "rpc.hh"
