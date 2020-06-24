f36618b18e6b3a4c0c0cac44242721d85c2f66c3
hauke <hauke@pkgsrc.org>
2020-06-24 16:10:31 +0000

    Disable generation of PIE code for clang, too, unbreaking the xemacs build on Mac OS X (10.12 here).
    Note that xemacs-current does not yet build on this platform:
    src/vdb-mach.c blindly assumes a powerpc cpu.

--- editors/xemacs-current/hacks.mk
+++ editors/xemacs-current/hacks.mk
@@ -1,16 +1,20 @@
-# $NetBSD: hacks.mk,v 1.3 2019/04/11 16:05:24 hauke Exp $
+# $NetBSD: hacks.mk,v 1.4 2020/06/24 16:10:31 hauke Exp $
 
 .if !defined(XEMACS_CURRENT_HACKS_MK)
 XEMACS_CURRENT_HACKS_MK=	defined
 
 .include "../../mk/compiler.mk"
 
-### Position-independent code does not rhyme well with
-### dumped emacsen.
+### Position-independent code does not rhyme well with dumped emacsen.
 ###
 .if !empty(CC_VERSION:Mgcc-[6789].*)
 PKG_HACKS+=		disable-gcc-pie
 CFLAGS+=		-no-pie
+.endif
+
+.if !empty(CC_VERSION:Mclang-*)
+PKG_HACKS+=		disable-clang-pie
+CFLAGS+=		-fno-pie
 .endif
 
 .endif  # XEMACS_CURRENT_HACKS_MK
