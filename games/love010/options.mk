57a7d7be8cd521244d5a359e823e16c7476c7f01
nia <nia@pkgsrc.org>
2020-06-24 13:32:09 +0000

    Move luajit out of SUPPORTED_OPTIONS where unsupported.

--- games/love010/options.mk
+++ games/love010/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.1 2020/06/24 11:24:45 nia Exp $
+# $NetBSD: options.mk,v 1.2 2020/06/24 13:32:09 nia Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.love
-PKG_SUPPORTED_OPTIONS=	luajit
 
 .include "../../lang/LuaJIT2/platform.mk"
 .if !empty(PLATFORM_SUPPORTS_LUAJIT:M[Yy][Ee][Ss])
+PKG_SUPPORTED_OPTIONS=	luajit
 PKG_SUGGESTED_OPTIONS=	luajit
 .endif
 
