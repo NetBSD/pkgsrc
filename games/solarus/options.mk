ee23c5640f0313e358f8fe9408bae2997111e782
nia <nia@pkgsrc.org>
2020-06-24 13:28:26 +0000

    solarus: Make luajit optional. Should help aarch64 builds.
    qt5-qttools is a build dependency, not a runtime dependency.

---
+++ games/solarus/options.mk
@@ -0,0 +1,19 @@
+# $NetBSD: options.mk,v 1.1 2020/06/24 13:28:26 nia Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.solarus
+PKG_SUPPORTED_OPTIONS=	luajit
+
+.include "../../lang/LuaJIT2/platform.mk"
+.if !empty(PLATFORM_SUPPORTS_LUAJIT:M[Yy][Ee][Ss])
+PKG_SUGGESTED_OPTIONS=	luajit
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mluajit)
+CMAKE_ARGS+=	-DSOLARUS_USE_LUAJIT=ON
+.  include "../../lang/LuaJIT2/buildlink3.mk"
+.else
+CMAKE_ARGS+=	-DSOLARUS_USE_LUAJIT=OFF
+.  include "../../lang/lua51/buildlink3.mk"
+.endif
