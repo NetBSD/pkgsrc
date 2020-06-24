90cddce88ce47505fb2a95d1c4d621bfa7a8efb1
nikita <nikita@pkgsrc.org>
2020-06-24 22:58:07 +0000

    go-chroma: fix bl3.

--- textproc/go-chroma/buildlink3.mk
+++ textproc/go-chroma/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2018/11/18 08:22:23 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/06/24 22:58:07 nikita Exp $
 
 BUILDLINK_TREE+=	go-chroma
 
@@ -11,6 +11,12 @@ BUILDLINK_DEPMETHOD.go-chroma?=		build
 BUILDLINK_API_DEPENDS.go-chroma+=	go-chroma>=0.5.0
 BUILDLINK_PKGSRCDIR.go-chroma?=		../../textproc/go-chroma
 
+.include "../../textproc/go-regexp2/buildlink3.mk"
+.include "../../devel/go-fnmatch/buildlink3.mk"
+.include "../../devel/go-colorable/buildlink3.mk"
+.include "../../devel/go-kingpin.v3-unstable/buildlink3.mk"
+.include "../../math/go-units/buildlink3.mk"
+.include "../../devel/go-i18n/buildlink3.mk"
 .endif	# GO_CHROMA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-go-chroma
