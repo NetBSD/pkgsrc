95c1a8bbf845edb2bfdb919f05dd97249970d2f4
gdt <gdt@pkgsrc.org>
2020-06-24 21:49:20 +0000

    geography/qgis: Update MAINTAINER, comment on python option.
    (This is a comment-only change.)
    
    Set primary MAINTAINER to gdt, following discussion among the
    maintainer group.
    
    State a plan to delete the python option (with python support being
    always on) after the branch.

--- geography/qgis/options.mk
+++ geography/qgis/options.mk
@@ -1,10 +1,12 @@
-# $NetBSD: options.mk,v 1.17 2020/06/21 13:51:49 gdt Exp $
+# $NetBSD: options.mk,v 1.18 2020/06/24 21:49:20 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.qgis
 PKG_SUPPORTED_OPTIONS=	python
-# \todo Explain why there is even an option; python is so central and
-# qgis is so huge that it is hard to imagine someone wanting to build
-# qgis without python.
+# \todo After 2020Q2, drop python option (making it on).  Python
+# within qgis is so huge that it is hard to imagine someone wanting to
+# build qgis without python, and the cost of that theoretical person
+# putting up with a few more bits is smaller than the maintenance pain
+# of having an option.
 PKG_SUGGESTED_OPTIONS+=	python
 
 .include "../../mk/bsd.options.mk"
