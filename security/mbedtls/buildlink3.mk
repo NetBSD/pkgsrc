19461bf74ac0d55aa283e6ee991058f38a47cbdd
nia <nia@pkgsrc.org>
2020-06-29 12:39:36 +0000

    mbedtls: Add KERN_ARND support.
    Motivation: the default behaviour of reopening /dev/urandom repeatedly
    for every 128 bytes of entropy required is _exceedingly_ slow on NetBSD.
    Not helped is using fread(), which assumes a long-lived file and buffers
    excessively. This change makes the standard gen_entropy tool run in
    milliseconds instead of seconds when it generates 48K of randomness.
    
    Not only that, but sysctl is a lot more robust in e.g. chroots, resource
    limited processes, etc.
    
    Risk: On NetBSD, the security properties of the previous and current
    behaviour are identical.
    
    Upstreamed: https://github.com/ARMmbed/mbedtls/pull/3423
    
    Bump PKGREVISION.

--- security/mbedtls/buildlink3.mk
+++ security/mbedtls/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/06/12 09:05:05 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/06/29 12:39:36 nia Exp $
 
 BUILDLINK_TREE+=	mbedtls
 
@@ -11,7 +11,7 @@ BUILDLINK_PKGSRCDIR.mbedtls?=	../../security/mbedtls
 pkgbase := mbedtls
 .include "../../mk/pkg-build-options.mk"
 
-.if !empty(PKG_BUILD_OPTIONS.mbedtls:Mzlib)
+.if ${PKG_BUILD_OPTIONS.mbedtls:Mzlib}
 .include "../../devel/zlib/buildlink3.mk"
 .endif
 
