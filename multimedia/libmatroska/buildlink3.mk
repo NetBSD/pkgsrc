14ecf931c1b44a08452f6cfa6d866bd3cbaed456
adam <adam@pkgsrc.org>
2020-06-29 15:09:54 +0000

    libmatroska: updated to 1.6.0
    Released v1.6.0.
    
    * libEBML v1.4.0 is now required.
    
    * Due to breaking ABI the soname version has been bumped to 7.0.0.
    
    * Default symbol visibility is now "hidden", reducing binary size.
    
    * Converted some things pointed out by cppcheck & clang-tidy to
    C++11.
    
    * Fixed a lot of issues pointed out by clang-tidy.
    
    * A C++11 compliant compiler is now required. Several of its
    features are now used in the source.
    
    * "KaxCueData" class: added "PositionSet" overrides taking
    "KaxSimpleBlock" arguments.
    
    * "KaxSeekHead" class: the "IndexThis" function now returns a
    pointer to the newly allocated "KaxSeek" instance.
    
    * "KaxBlockData" class: the "SetReferencedTimecode" function is
    now public.
    
    * Fixed classes for updates in the current specification:
    KaxTrackTimecodeScale (can be written again), TimeSlice,
    SliceLaceNumber, TrackAttachmentLink (these cannot be written
    anymore), KaxContentEncAlgo (it's mandatory), KaxChapLanguageIETF
    (can occur multiple times), KaxFileUsedStartTime,
    KaxFileUsedEndTime (DivX-related elements, cannot be written
    anymore)
    
    * Added classes for new elements from the current specification:
    KaxBlockAdditionMapping, KaxBlockAddIDValue, KaxBlockAddIDName,
    KaxBlockAddIDType, KaxBlockAddIDExtraData,
    KaxContentEncAESSettings, KaxContentEncAESSettings.
    
    * The MATROSKA_VERSION pre-processor definition has been
    removed. You used to be able toe restrict compiling with only
    those classes for elements contained in Matroska v1, but that
    hasn't worked since 2010, so… yeah.
    
    * Build system: more fixes for building libMatroska as a DLL on
    Windows wrt. symbol visibility.

--- multimedia/libmatroska/buildlink3.mk
+++ multimedia/libmatroska/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2015/01/06 14:59:18 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2020/06/29 15:09:54 adam Exp $
 
 BUILDLINK_TREE+=	libmatroska
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libmatroska
 LIBMATROSKA_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libmatroska+=	libmatroska>=0.8.0
-BUILDLINK_ABI_DEPENDS.libmatroska+=	libmatroska>=1.4.2
+BUILDLINK_ABI_DEPENDS.libmatroska+=	libmatroska>=1.6.0
 BUILDLINK_PKGSRCDIR.libmatroska?=	../../multimedia/libmatroska
 
 .include "../../devel/libebml/buildlink3.mk"
