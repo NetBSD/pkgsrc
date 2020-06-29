066b2f7952e05c3da2329480914d8c0c4b5d52c6
adam <adam@pkgsrc.org>
2020-06-29 14:29:20 +0000

    py-sanic: updated to 20.6.3
    Version 20.6.3
    
    Bugfixes
    * Revert change to multiprocessing mode
    
    
    Version 20.6.2
    
    Features
    * Socket binding implemented properly for IPv6 and UNIX sockets
    
    
    Version 20.6.1
    
    Features
    * Add version parameter to websocket routes
    * Add sanic as an entry point command
    * Add handler names for websockets for url_for usage
    
    Bugfixes
    * Bug fix for host parameter issue with lists
    * Fix static _handler pickling error
    * Fix reloader on OSX py38 and Windows
    * Reverse named_response_middlware execution order, to match normal response middleware execution order
    * Fix pickle error when attempting to pickle an application which contains websocket routes
    
    Deprecations and Removals
    * Deprecate body_bytes to merge into body
    
    Developer infrastructure
    * Fix naming of CI test env on Python nightlies
    * Adjust websockets version to setup.py
    * Wrap run()'s "protocol" type annotation in Optional[]
    
    Improved Documentation
    * Update docs to clarify response middleware execution order
    * Fixing rst format issue that was hiding documentation

--- www/py-sanic/patches/patch-setup.py
+++ www/py-sanic/patches/patch-setup.py
@@ -1,12 +1,12 @@
-$NetBSD: patch-setup.py,v 1.5 2020/05/15 08:32:34 adam Exp $
+$NetBSD: patch-setup.py,v 1.6 2020/06/29 14:29:20 adam Exp $
 
 Allow newer versions.
 
---- setup.py.orig	2020-05-14 19:06:33.000000000 +0000
+--- setup.py.orig	2020-06-28 10:14:53.000000000 +0000
 +++ setup.py
-@@ -85,7 +85,7 @@ requirements = [
+@@ -81,7 +81,7 @@ requirements = [
      "aiofiles>=0.3.0",
-     "websockets>=7.0,<9.0",
+     "websockets>=8.1,<9.0",
      "multidict>=4.0,<5.0",
 -    "httpx==0.11.1",
 +    "httpx>=0.11.1",
