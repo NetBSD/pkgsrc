a9e9c6c0c42cfd1e8a85cbf3169aa59dc43b5ac4
gutteridge <gutteridge@pkgsrc.org>
2020-06-29 23:35:51 +0000

    loudmouth: very belatedly add a GitHub pull reference to a patch

--- chat/loudmouth/patches/patch-loudmouth_lm-resolver.c
+++ chat/loudmouth/patches/patch-loudmouth_lm-resolver.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-loudmouth_lm-resolver.c,v 1.1 2019/01/16 01:36:12 gutteridge Exp $
+$NetBSD: patch-loudmouth_lm-resolver.c,v 1.2 2020/06/29 23:35:51 gutteridge Exp $
 
 Fix a build breakage on non-x86 NetBSD architectures where arpa/
 nameser_compat.h was being unnecessarily included (without additional
 dependent macros being defined, as nameser.h takes care of when it's
-included first).
+included first). https://github.com/mcabber/loudmouth/pull/39
 
 --- loudmouth/lm-resolver.c.orig	2016-02-14 17:05:11.000000000 +0000
 +++ loudmouth/lm-resolver.c
