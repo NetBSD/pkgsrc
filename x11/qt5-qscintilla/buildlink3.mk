3cd5ec0bacdc7e185674d293e7e64d0052b49e99
adam <adam@pkgsrc.org>
2020-06-29 13:25:57 +0000

    qt5-qscintilla py-qt5-qscintilla: updated to 2.11.5
    QScintilla v2.11.5 has been released. This is a minor bug-fix release.

--- x11/qt5-qscintilla/buildlink3.mk
+++ x11/qt5-qscintilla/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2020/04/12 08:28:14 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/06/29 13:25:57 adam Exp $
 
 BUILDLINK_TREE+=	qt5-qscintilla
 
@@ -9,6 +9,11 @@ BUILDLINK_API_DEPENDS.qt5-qscintilla+=	qt5-qscintilla>=2.11.2
 BUILDLINK_ABI_DEPENDS.qt5-qscintilla+=	qt5-qscintilla>=2.11.2nb5
 BUILDLINK_PKGSRCDIR.qt5-qscintilla?=	../../x11/qt5-qscintilla
 
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} == "Darwin"
+.include "../../x11/qt5-qtmacextras/buildlink3.mk"
+.endif
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# QT5_QSCINTILLA_BUILDLINK3_MK
 
