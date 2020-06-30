0623a90116ffe44c0fc1ddd21137c8e134d28d60
nia <nia@pkgsrc.org>
2020-06-30 13:16:14 +0000

    lang: Add lua54.
    Note: this is slightly different than other Lua packages and, among
    other things, implements its own install target. This is becuase I
    wanted to minimize the amount of patching to Makefiles to make it
    fit into the pkgsrc world.
    
    Lua is a powerful, fast, lightweight, embeddable scripting language.
    
    Lua combines simple procedural syntax with powerful data description
    constructs based on associative arrays and extensible semantics. Lua is
    dynamically typed, runs by interpreting bytecode for a register-based
    virtual machine, and has automatic memory management with incremental
    garbage collection, making it ideal for configuration, scripting, and
    rapid prototyping.
    
    This version is a preferred choice for new Lua modules.

---
+++ lang/lua54/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/06/30 13:16:14 nia Exp $
+
+BUILDLINK_TREE+=	lua54
+
+.if !defined(LUA54_BUILDLINK3_MK)
+LUA54_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.lua54+=	lua54>=5.4.0<5.4
+BUILDLINK_PKGSRCDIR.lua54?=	../../lang/lua54
+
+.if defined(BUILDLINK_DEPMETHOD.lua)
+BUILDLINK_DEPMETHOD.lua54?=	${BUILDLINK_DEPMETHOD.lua}
+.endif
+
+# -llua -> -llua5.4
+BUILDLINK_TRANSFORM+=		l:lua:lua5.4
+BUILDLINK_INCDIRS.lua54+=	include/lua-5.4
+
+.include "../../mk/readline.buildlink3.mk"
+.endif # LUA54_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-lua54
