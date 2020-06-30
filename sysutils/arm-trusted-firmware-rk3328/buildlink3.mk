4e71a1f65c88cdcaa7ff44ad998955150a2c02bd
thorpej <thorpej@pkgsrc.org>
2020-06-30 14:33:14 +0000

    - Re-factor the arm-trusted-firmware packages into common definitions and   build infrastructure. - Update arm-trusted-firmware to v2.3.

--- sysutils/arm-trusted-firmware-rk3328/buildlink3.mk
+++ sysutils/arm-trusted-firmware-rk3328/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2020/04/04 06:21:54 mrg Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/06/30 14:33:15 thorpej Exp $
 
 BUILDLINK_DEPMETHOD.arm-trusted-firmware-rk3328?=	build
 
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	arm-trusted-firmware-rk3328
 .if !defined(ARM_TRUSTED_FIRMWARE_RK3328_BUILDLINK3_MK)
 ARM_TRUSTED_FIRMWARE_RK3328_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.arm-trusted-firmware-rk3328+=	arm-trusted-firmware-rk3328>2.0
+BUILDLINK_API_DEPENDS.arm-trusted-firmware-rk3328+=	arm-trusted-firmware-rk3328>=2.3
 BUILDLINK_PKGSRCDIR.arm-trusted-firmware-rk3328?=	../../sysutils/arm-trusted-firmware-rk3328
 .endif	# ARM_TRUSTED_FIRMWARE_RK3328_BUILDLINK3_MK
 
