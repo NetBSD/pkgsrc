50a8b8c4764a6a423c22aa79eeeead3e5d9fee29
nia <nia@pkgsrc.org>
2020-06-30 18:49:56 +0000

    vim-share: Fix lua option.
    Instead of the first unversioned lua interpreter being detected
    (on NetBSD usually /usr/bin/lua), make sure the correct version
    as selected by pkgsrc infrastructure gets used.

--- editors/vim-share/options.mk
+++ editors/vim-share/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.8 2019/11/02 19:57:01 tnn Exp $
+# $NetBSD: options.mk,v 1.9 2020/06/30 18:49:56 nia Exp $
 
 PKG_SUPPORTED_OPTIONS+=	ruby python perl lua luajit
 PKG_OPTIONS_VAR=	PKG_OPTIONS.vim
@@ -29,11 +29,19 @@ CONFIGURE_ENV+=		vi_cv_path_perl=${PERL5:Q}
 .endif
 
 .if !empty(PKG_OPTIONS:Mlua)
+LUA_VERSIONS_ACCEPTED=	53 52 51
 .include "../../lang/lua/buildlink3.mk"
-CONFIGURE_ARGS+=   --enable-luainterp
+CONFIGURE_ARGS+=	--enable-luainterp
+CONFIGURE_ARGS+=	--with-lua-prefix=${BUILDLINK_PREFIX.lua}
+CONFIGURE_ENV+=		vi_cv_path_plain_lua=${LUA_INTERPRETER}
+# pkgsrc has include/lua-5.x instead of include/lua5.x
+SUBST_CLASSES+=		lua
+SUBST_FILES.lua=	auto/configure
+SUBST_STAGE.lua=	pre-configure
+SUBST_SED.lua=		-e "s,include/lua,include/lua-,g"
 .endif
 
 .if !empty(PKG_OPTIONS:Mluajit)
 .include "../../lang/LuaJIT2/buildlink3.mk"
-CONFIGURE_ARGS+=   --enable-luainterp --with-luajit
+CONFIGURE_ARGS+=	--enable-luainterp --with-luajit
 .endif
