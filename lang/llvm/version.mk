ad32e26a3052cf38bc8ee70eced56afce1d13a4a
adam <adam@pkgsrc.org>
2020-07-01 14:46:36 +0000

    clang: build with static analizer (default and needed for clang-tools-extra); remove clang-static-analyzer

--- lang/llvm/version.mk
+++ lang/llvm/version.mk
@@ -1,9 +1,8 @@
-# $NetBSD: version.mk,v 1.2 2020/04/18 07:50:18 adam Exp $
+# $NetBSD: version.mk,v 1.3 2020/07/01 14:46:36 adam Exp $
 # used by devel/lld
 # used by devel/lldb
 # used by devel/polly
 # used by lang/clang
-# used by lang/clang-static-analyzer
 # used by lang/clang-tools-extra
 # used by lang/compiler-rt
 # used by lang/libcxx
