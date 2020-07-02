bce6f79d2a918ab882d15476af12b8fa0d1a7061
nia <nia@pkgsrc.org>
2020-07-02 21:42:23 +0000

    lua: move cmake bits to per-interpreter buildlink3s

--- lang/lua51/buildlink3.mk
+++ lang/lua51/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2015/07/20 11:30:54 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2020/07/02 21:42:23 nia Exp $
 
 BUILDLINK_TREE+=	lua51
 
@@ -15,6 +15,14 @@ BUILDLINK_DEPMETHOD.lua51?=	${BUILDLINK_DEPMETHOD.lua}
 # -llua -> -llua5.1
 BUILDLINK_TRANSFORM+=		l:lua:lua5.1
 BUILDLINK_INCDIRS.lua51+=	include/lua-5.1
+
+.if defined(USE_CMAKE)
+# used by FindLua.cmake
+CMAKE_ARGS+=	-DLua_FIND_VERSION_EXACT=ON
+CMAKE_ARGS+=	-DLua_FIND_VERSION_COUNT=2
+CMAKE_ARGS+=	-DLua_FIND_VERSION_MAJOR=5
+CMAKE_ARGS+=	-DLua_FIND_VERSION_MINOR=1
+.endif
 
 .include "../../mk/readline.buildlink3.mk"
 .endif # LUA51_BUILDLINK3_MK
