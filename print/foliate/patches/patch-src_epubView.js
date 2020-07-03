8c2b9c9e87b623baceff09d574f68791fb2a61a7
fox <fox@pkgsrc.org>
2020-07-03 12:18:12 +0000

    print/foliate: Updates to 2.4.0
    Changes since 2.3.0:
    
    Changes:
    
      * Improved metadata display
      * Improved FictionBook rendering
      * Improved UI for acquiring books from OPDS (#404, #405)
      * Fixed unique identifier extraction for EPUB files
      * Fixed missing window icon (#456)
      * Fixed TTS environment variables not set when using Flatpak
      * Fixed eSpeak NG voice not selected when testing TTS configuration
      * Fixed wrong __ibooks_internal_theme attribute when "Invert" is
        enabled
    
    Technical changes:
    
      * Foliate no longer depends on libsoup (gir1.2-soup-2.4 on Debian based
        distributions). Previously, it was an optional dependency for opening
        remote files.

--- print/foliate/patches/patch-src_epubView.js
+++ print/foliate/patches/patch-src_epubView.js
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_epubView.js,v 1.2 2020/05/30 00:01:58 fox Exp $
+$NetBSD: patch-src_epubView.js,v 1.3 2020/07/03 12:18:12 fox Exp $
 
 Adds variable to help substitute the correct python version suffix.
 
---- src/epubView.js.orig	2020-04-06 13:02:45.032535873 +0000
+--- src/epubView.js.orig	2020-07-02 07:05:56.000000000 +0000
 +++ src/epubView.js
-@@ -36,7 +36,7 @@ const {
-     mimetypes.kindleAlias,
- ]
+@@ -25,7 +25,7 @@ const {
+     mimetypes, mimetypeIs, execCommand, recursivelyDeleteDir
+ } = imports.utils
  
 -const python = GLib.find_program_in_path('python') || GLib.find_program_in_path('python3')
 +const python = GLib.find_program_in_path('python@PYVERSSUFFIX@')
