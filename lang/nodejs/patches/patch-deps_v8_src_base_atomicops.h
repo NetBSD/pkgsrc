5274fb1d41539d76ce4a73f9a763fc96e20135df
adam <adam@pkgsrc.org>
2020-07-03 19:00:15 +0000

    nodejs: updated to 14.5.0
    Version 14.5.0 (Current)
    
    Notable Changes
    
    V8 engine is updated to version 8.3
    Initial experimental implementation of EventTarget

--- lang/nodejs/patches/patch-deps_v8_src_base_atomicops.h
+++ lang/nodejs/patches/patch-deps_v8_src_base_atomicops.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-deps_v8_src_base_atomicops.h,v 1.3 2019/12/09 20:03:51 adam Exp $
+$NetBSD: patch-deps_v8_src_base_atomicops.h,v 1.4 2020/07/03 19:00:15 adam Exp $
 
---- deps/v8/src/base/atomicops.h.orig	2019-11-19 08:29:07.000000000 +0000
+--- deps/v8/src/base/atomicops.h.orig	2020-07-03 17:37:00.000000000 +0000
 +++ deps/v8/src/base/atomicops.h
 @@ -52,9 +52,13 @@ using Atomic64 = intptr_t;
  #endif  // defined(__ILP32__)
@@ -15,4 +15,4 @@ $NetBSD: patch-deps_v8_src_base_atomicops.h,v 1.3 2019/12/09 20:03:51 adam Exp $
 +#endif
  
  // Atomically execute:
- //      result = *ptr;
+ //   result = *ptr;
