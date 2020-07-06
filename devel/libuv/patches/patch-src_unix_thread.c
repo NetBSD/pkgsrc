81a77fd460813b80cf45232eddfa83784a821beb
adam <adam@pkgsrc.org>
2020-07-06 10:07:17 +0000

    libuv: updated to 1.38.1
    Changes since version 1.38.0:
    * test: use last matching qemu version
    * win, util: rearrange uv_hrtime
    * test: skip signal_multiple_loops test on QEMU
    * build: add android build to CI
    * test: extend fs_event_error_reporting timeout
    * build: link libkvm on netbsd only
    * linux: refactor /proc file reader logic
    * linux: read load average from /proc/loadavg
    * android: remove patch code for below 21
    * win: fix visual studio 2008 build
    * win,tty: fix deadlock caused by inconsistent state
    * unix: use relaxed loads/stores for feature checks
    * build: don't .gitignore m4/ax_pthread.m4
    * unix: fix gcc atomics feature check
    * darwin: work around clock jumping back in time
    * udp: fix write_queue cleanup on sendmmsg error
    * src: build fix for Android

--- devel/libuv/patches/patch-src_unix_thread.c
+++ devel/libuv/patches/patch-src_unix_thread.c
@@ -1,16 +1,19 @@
-$NetBSD: patch-src_unix_thread.c,v 1.7 2019/08/22 10:28:25 wiz Exp $
+$NetBSD: patch-src_unix_thread.c,v 1.8 2020/07/06 10:07:17 adam Exp $
 
 Use feature test for pthread_condattr_setclock, absent in netbsd-6-0
 https://github.com/libuv/libuv/pull/2436
 
---- src/unix/thread.c.orig	2019-02-10 16:44:47.000000000 +0000
+--- src/unix/thread.c.orig	2020-07-03 16:51:27.000000000 +0000
 +++ src/unix/thread.c
-@@ -687,7 +689,7 @@ int uv_cond_init(uv_cond_t* cond) {
+@@ -709,9 +709,11 @@ int uv_cond_init(uv_cond_t* cond) {
    if (err)
      return UV__ERR(err);
  
--#if !(defined(__ANDROID_API__) && __ANDROID_API__ < 21)
 +#if defined(HAVE_PTHREAD_CONDATTR_SETCLOCK)
    err = pthread_condattr_setclock(&attr, CLOCK_MONOTONIC);
    if (err)
      goto error2;
++#endif
+ 
+   err = pthread_cond_init(cond, &attr);
+   if (err)
