46da4fd1afb737ce4655d23029acc47600ef94f8
schmonz <schmonz@pkgsrc.org>
2020-07-07 10:31:31 +0000

    Update to 2020.05.04. From the changelog:
    - The setstate command from idtools is now included in fdtools.
    - Compatibility with the latest skalibs.
    - Silencing modern compiler warnings.

--- sysutils/fdtools/patches/patch-src_.mkmed-data_src_prjlibs-c_standards.h
+++ sysutils/fdtools/patches/patch-src_.mkmed-data_src_prjlibs-c_standards.h
@@ -1,16 +1,18 @@
-$NetBSD: patch-src_.mkmed-data_src_prjlibs-c_standards.h,v 1.1 2018/12/17 17:04:38 schmonz Exp $
+$NetBSD: patch-src_.mkmed-data_src_prjlibs-c_standards.h,v 1.2 2020/07/07 10:31:31 schmonz Exp $
 
 Handle _XOPEN_SOURCE on SunOS.
 
---- src/.mkmed-data/src/prjlibs-c/standards.h.orig	2015-03-28 22:14:51.231617623 +0000
+--- src/.mkmed-data/src/prjlibs-c/standards.h.orig	2020-05-04 08:10:42.000000000 +0000
 +++ src/.mkmed-data/src/prjlibs-c/standards.h
-@@ -7,13 +7,16 @@
+@@ -7,7 +7,6 @@
  #endif
  #endif
  #endif
 -#define _XOPEN_SOURCE_EXTENDED 1
- #ifndef _BSD_SOURCE
- #define _BSD_SOURCE 1
+ #ifndef _DEFAULT_SOURCE
+ #define _DEFAULT_SOURCE 1
+ #endif
+@@ -16,7 +15,11 @@
  #endif
  #ifdef __sun
  #undef _XOPEN_SOURCE
