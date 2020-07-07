f33a2e1b81847ce89d73efaee2e940711753fb29
adam <adam@pkgsrc.org>
2020-07-07 06:54:03 +0000

    rsync: updated to 3.2.2
    NEWS for rsync 3.2.2
    
    BUG FIXES:
    
    - Avoid a crash when a daemon module enables `transfer logging` without
      setting a `log format` value.
    
    - Fixed installing rsync-ssl script from an alternate build dir.
    
    - Fixed the updating of configure.sh from an alternate build dir.
    
    - Apple requires the asm function name to begin with an underscore.
    
    - Avoid a test failure in the daemon test when --atimes is disabled.
    
    ENHANCEMENTS:
    
    - Allow the server side to restrict checksum & compression choices via the
      same environment variables the client uses.  The env vars can be divided
      into "client list & server list" by the "`&`" char or the same list can
      apply to both.
    
    - Simplify how the negotiation environment variables apply when interacting
      with an older rsync and also when a list contains only invalid names.
    
    - Do not allow a negotiated checksum or compression choice of "none" unless
      the user authorized it via an environment variable or command-line option.
    
    - Added the `--max-alloc=SIZE` option to be able to override the memory
      allocator's sanity-check limit.  It defaults to 1G (as before) but the error
      message when exceeding it specifically mentions the new option so that you
      can differentiate an out-of-memory error from a failure of this limit.  It
      also allows you to specify the value via the RSYNC_MAX_ALLOC environment
      variable.
    
    - Add the "open atime" daemon parameter to allow a daemon to always enable or
      disable the use of O_NOATIME (the default is to let the user control it).
    
    - The default systemd config was changed to remove the `ProtectHome=on`
      setting since rsync is often used to serve files in /home and /root and this
      seemed a bit too strict.  Feel free to use `systemctl edit rsync` to add
      that restriction (or maybe `ProtectHome=read-only`), if you like.  See the
      3.2.0 NEWS for the other restrictions that were added compared to 3.1.3.
    
    - The memory allocation functions now automatically check for a failure and
      die when out of memory.  This eliminated some caller-side check-and-die
      code and added some missing sanity-checking of allocations.
    
    - Put optimizations into their own list in the `--version` output.
    
    - Improved the man page a bit more.
    
    PACKAGING RELATED:
    
    - Prepared the checksum code for an upcoming xxHash release that provides new
      XXH3 (64-bit) & XXH128 (128-bit) checksum routines.  These will not be
      compiled into rsync until the xxhash v0.8.0 include files are installed on
      the build host, and that release is a few weeks away at the time this was
      written.  So, if it's now the future and you have packaged and installed
      xxhash-0.8.0-devel, a fresh rebuild of rsync 3.2.2 will give you the new
      checksum routines.  Just make sure that the new rsync package depends on
      xxhash >= 0.8.0.
    
    DEVELOPER RELATED:
    
    - Moved the version number out of configure.ac into its own version.h file so
      that we don't need to reconfigure just because the version number changes.
    
    - Moved the daemon parameter list into daemon-parm.txt so that an awk script
      can create the interrelated structs and accessors that loadparm.c needs.

--- net/rsync/patches/patch-authenticate.c
+++ net/rsync/patches/patch-authenticate.c
@@ -1,21 +1,21 @@
-$NetBSD: patch-authenticate.c,v 1.3 2017/11/10 06:59:16 spz Exp $
+$NetBSD: patch-authenticate.c,v 1.4 2020/07/07 06:54:03 adam Exp $
 
 3.1.2 is not vulnerable to CVE-2017-15994, the code is different,
 but not allowing fallback to MD4 for passwords is a good idea by now.
 Patch from
 https://git.samba.org/?p=rsync.git;a=blobdiff;f=authenticate.c;h=a106b0f60a8cb88e37080bc5e2a58ce28c66f379;hp=d60ee20b6b53a9351efbdf175f36525ead220de6;hb=9a480deec4d20277d8e20bc55515ef0640ca1e55;hpb=c252546ceeb0925eb8a4061315e3ff0a8c55b48b
 
---- authenticate.c.orig	2015-08-24 18:54:00.000000000 +0000
+--- authenticate.c.orig	2020-06-26 03:54:21.000000000 +0000
 +++ authenticate.c
-@@ -22,6 +22,7 @@
- #include "itypes.h"
+@@ -23,6 +23,7 @@
+ #include "ifuncs.h"
  
  extern int read_only;
 +extern int protocol_version;
  extern char *password_file;
  
  /***************************************************************************
-@@ -237,6 +238,11 @@ char *auth_server(int f_in, int f_out, i
+@@ -238,6 +239,11 @@ char *auth_server(int f_in, int f_out, i
  	if (!users || !*users)
  		return "";
  
