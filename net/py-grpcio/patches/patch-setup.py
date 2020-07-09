d806d61a9fd23248f8506d0b922a48b84aa1aa53
adam <adam@pkgsrc.org>
2020-07-09 07:23:43 +0000

    grpc: updated to 1.30.1
    Release v1.30.1
    This release is a patch specifically for the Ruby bindings
    
    Release v1.30.0
    Core
    This release adds an xDS URI scheme called xds. This is the stable version of the scheme xds-experimental that was introduced in v1.28.0. xds-experimental scheme will be removed in subsequent releases so you must switch to xds scheme instead. xds scheme is a client side implementation of xDSv2 APIs. This allows a gRPC client written in C++, Python, Ruby, PHP and C# to receive configuration from an xDSv2 API compatible server and use that configuration to load balance RPCs. In this release, only the virtual host matching, default path (“” or “/”) matching and cluster route action are supported. The features supported in a given release are documented here.
    Remove unnamed typedef structs in src/core.
    Support xDS via both xds and xds-experimental URI schemes.
    Fix sorting of gRPCLB addresses when resolved via DNS.
    Support local creds in grpc_cli.
    Add some additional delay when sending pings without there being activity on receive side.
    Added GRPC_TSAN_SUPPRESSED and GRPC_ASAN_SUPPRESSED.
    Fail decompression when the gzip trailer is missing.
    Include source address in tcp posix async connect errors.
    Fix HTTP status conversion inconsistencies.
    Add GRPC_ARG_HTTP_PROXY channel argument.
    Include the query type and name in all c-ares DNS error messages.
    Include the destination address in synchronous TCP connect errors.
    Use aligned calculation to determine transport stream from call data.
    Fixing bug with END_STREAM if header has continuations.
    Fail writes when End of stream has been received.
    
    C++
    Fix missing include for std::string.
    Don't override cmake cxx standard when already set by the user.
    Grpc.Tools: Fix cpp paths in tools to match actual codegen.
    TlsCredentialsOption API optimization.
    Fixed MinGW 7.3.0 shared library compile and link issues.
    Fix interceptor batch method FailHijackedRecvMessage for async APIs.
    
    Python
    Add Aio stream stream client interceptor support.
    [Aio] Add AsyncIO support for Channelz.
    Stop memory leak when Python channel is deallocated without invoking "close".
    Expose ALTS client/server credentials in Python API.
    [Aio] Stream Unary client interceptor.
    [Aio] Make sync handlers runnable in AsyncIO server.
    [Aio] Add AsyncIO support to grpcio-status.
    [Aio] Implement the Unary Stream client interceptor.

--- net/py-grpcio/patches/patch-setup.py
+++ net/py-grpcio/patches/patch-setup.py
@@ -1,55 +1,62 @@
-$NetBSD: patch-setup.py,v 1.6 2020/04/04 09:30:37 adam Exp $
+$NetBSD: patch-setup.py,v 1.7 2020/07/09 07:23:44 adam Exp $
 
 Fix libatomic detection.
 Use dependencies in pkgsrc.
 
---- setup.py.orig	2020-04-03 01:18:49.000000000 +0000
+--- setup.py.orig	2020-06-22 21:42:24.000000000 +0000
 +++ setup.py
-@@ -146,7 +146,7 @@ def check_linker_need_libatomic():
-   """Test if linker on system needs libatomic."""
-   code_test = (b'#include <atomic>\n' +
-                b'int main() { return std::atomic<int64_t>{}; }')
--  cc_test = subprocess.Popen(['cc', '-x', 'c++', '-std=c++11', '-'],
-+  cc_test = subprocess.Popen(['c++', '-x', 'c++', '-std=c++11', '-'],
-                              stdin=PIPE,
-                              stdout=PIPE,
-                              stderr=PIPE)
-@@ -235,19 +235,7 @@ EXTENSION_INCLUDE_DIRECTORIES = (
-     UPB_GRPC_GENERATED_INCLUDE +
-     ZLIB_INCLUDE)
+@@ -150,7 +150,7 @@ def check_linker_need_libatomic():
+     """Test if linker on system needs libatomic."""
+     code_test = (b'#include <atomic>\n' +
+                  b'int main() { return std::atomic<int64_t>{}; }')
+-    cc_test = subprocess.Popen(['cc', '-x', 'c++', '-std=c++11', '-'],
++    cc_test = subprocess.Popen(['c++', '-x', 'c++', '-std=c++11', '-'],
+                                stdin=PIPE,
+                                stdout=PIPE,
+                                stderr=PIPE)
+@@ -235,26 +235,7 @@ EXTENSION_INCLUDE_DIRECTORIES = ((PYTHON
+                                  SSL_INCLUDE + UPB_INCLUDE +
+                                  UPB_GRPC_GENERATED_INCLUDE + ZLIB_INCLUDE)
  
 -EXTENSION_LIBRARIES = ()
 -if "linux" in sys.platform:
--  EXTENSION_LIBRARIES += ('rt',)
+-    EXTENSION_LIBRARIES += ('rt',)
 -if not "win32" in sys.platform:
--  EXTENSION_LIBRARIES += ('m',)
+-    EXTENSION_LIBRARIES += ('m',)
 -if "win32" in sys.platform:
--  EXTENSION_LIBRARIES += ('advapi32', 'ws2_32', 'dbghelp',)
+-    EXTENSION_LIBRARIES += (
+-        'advapi32',
+-        'ws2_32',
+-        'dbghelp',
+-    )
 -if BUILD_WITH_SYSTEM_OPENSSL:
--  EXTENSION_LIBRARIES += ('ssl', 'crypto',)
+-    EXTENSION_LIBRARIES += (
+-        'ssl',
+-        'crypto',
+-    )
 -if BUILD_WITH_SYSTEM_ZLIB:
--  EXTENSION_LIBRARIES += ('z',)
+-    EXTENSION_LIBRARIES += ('z',)
 -if BUILD_WITH_SYSTEM_CARES:
--  EXTENSION_LIBRARIES += ('cares',)
+-    EXTENSION_LIBRARIES += ('cares',)
 +EXTENSION_LIBRARIES = ('grpc',)
  
  DEFINE_MACROS = (('OPENSSL_NO_ASM', 1), ('_WIN32_WINNT', 0x600))
  if not DISABLE_LIBC_COMPATIBILITY:
-@@ -295,7 +283,7 @@ def cython_extensions_and_necessity():
-                   for name in CYTHON_EXTENSION_MODULE_NAMES]
-   config = os.environ.get('CONFIG', 'opt')
-   prefix = 'libs/' + config + '/'
--  if USE_PREBUILT_GRPC_CORE:
-+  if False:
-     extra_objects = [prefix + 'libares.a',
-                      prefix + 'libboringssl.a',
-                      prefix + 'libgpr.a',
-@@ -307,7 +295,7 @@ def cython_extensions_and_necessity():
-   extensions = [
-       _extension.Extension(
-           name=module_name,
--          sources=[module_file] + list(CYTHON_HELPER_C_FILES) + core_c_files,
-+          sources=[module_file] + list(CYTHON_HELPER_C_FILES),
-           include_dirs=list(EXTENSION_INCLUDE_DIRECTORIES),
-           libraries=list(EXTENSION_LIBRARIES),
-           define_macros=list(DEFINE_MACROS),
+@@ -311,7 +292,7 @@ def cython_extensions_and_necessity():
+     ]
+     config = os.environ.get('CONFIG', 'opt')
+     prefix = 'libs/' + config + '/'
+-    if USE_PREBUILT_GRPC_CORE:
++    if False:
+         extra_objects = [
+             prefix + 'libares.a', prefix + 'libboringssl.a',
+             prefix + 'libgpr.a', prefix + 'libgrpc.a'
+@@ -323,7 +304,7 @@ def cython_extensions_and_necessity():
+     extensions = [
+         _extension.Extension(
+             name=module_name,
+-            sources=[module_file] + list(CYTHON_HELPER_C_FILES) + core_c_files,
++            sources=[module_file] + list(CYTHON_HELPER_C_FILES),
+             include_dirs=list(EXTENSION_INCLUDE_DIRECTORIES),
+             libraries=list(EXTENSION_LIBRARIES),
+             define_macros=list(DEFINE_MACROS),
