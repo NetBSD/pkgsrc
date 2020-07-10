4b6cf43af8d3fde6fe5d0d26461e8c5b3483aa8b
adam <adam@pkgsrc.org>
2020-07-10 10:24:21 +0000

    py-acme,py-certbot: updated to 1.6.0
    1.6.0
    
    Added
    
    Certbot snaps are now available for the arm64 and armhf architectures.
    Add minimal code to run Nginx plugin on NetBSD.
    Make Certbot snap find externally snapped plugins
    Function certbot.compat.filesystem.umask is a drop-in replacement for os.umask implementing umask for both UNIX and Windows systems.
    Support for alternative certificate chains in the acme module.
    Added --preferred-chain <issuer CN>. If a CA offers multiple certificate chains, it may be used to indicate to Certbot which chain should be preferred.
    e.g. --preferred-chain "DST Root CA X3"
    
    Changed
    
    Allow session tickets to be disabled in Apache when mod_ssl is statically linked.
    Generalize UI warning message on renewal rate limits
    Certbot behaves similarly on Windows to on UNIX systems regarding umask, and the umask 022 is applied by default: all files/directories are not writable by anyone other than the user running Certbot and the system/admin users.
    Read acmev1 Let's Encrypt server URL from renewal config as acmev2 URL to prepare for impending acmev1 deprecation.
    
    Fixed
    
    Cloudflare API Tokens may now be restricted to individual zones.
    Don't use StrictVersion, but LooseVersion to check version requirements with setuptools, to fix some packaging issues with libraries respecting PEP404 for version string, with doesn't match StrictVersion requirements.
    Certbot output doesn't refer to SSL Labs due to confusing scoring behavior.
    Fix paths when calling to programs outside of the Certbot Snap, fixing the apache and nginx plugins on, e.g., CentOS 7.

--- security/py-certbot-nginx/patches/patch-certbot__nginx___internal_constants.py
+++ security/py-certbot-nginx/patches/patch-certbot__nginx___internal_constants.py
@@ -1,17 +1,27 @@
-$NetBSD: patch-certbot__nginx___internal_constants.py,v 1.1 2020/06/21 08:00:03 plunky Exp $
+$NetBSD: patch-certbot__nginx___internal_constants.py,v 1.2 2020/07/10 10:24:23 adam Exp $
 
 Look for nginx.conf in PKG_SYSCONFDIR
 
---- certbot_nginx/_internal/constants.py.orig	2020-02-04 21:46:57.000000000 +0000
+--- certbot_nginx/_internal/constants.py.orig	2020-07-07 17:13:20.000000000 +0000
 +++ certbot_nginx/_internal/constants.py
-@@ -1,8 +1,8 @@
+@@ -1,19 +1,8 @@
  """nginx plugin constants."""
  import platform
  
 -FREEBSD_DARWIN_SERVER_ROOT = "/usr/local/etc/nginx"
 -LINUX_SERVER_ROOT = "/etc/nginx"
-+FREEBSD_DARWIN_SERVER_ROOT = "@PKG_SYSCONFDIR@/nginx"
-+LINUX_SERVER_ROOT = "@PKG_SYSCONFDIR@/nginx"
- 
- if platform.system() in ('FreeBSD', 'Darwin'):
-     server_root_tmp = FREEBSD_DARWIN_SERVER_ROOT
+-PKGSRC_SERVER_ROOT = "/usr/pkg/etc/nginx"
+-
+-if platform.system() in ('FreeBSD', 'Darwin'):
+-    server_root_tmp = FREEBSD_DARWIN_SERVER_ROOT
+-elif platform.system() in ('NetBSD',):
+-    server_root_tmp = PKGSRC_SERVER_ROOT
+-else:
+-    server_root_tmp = LINUX_SERVER_ROOT
+-
+ CLI_DEFAULTS = dict(
+-    server_root=server_root_tmp,
++    server_root="@PKG_SYSCONFDIR@/nginx",
+     ctl="nginx",
+ )
+ """CLI defaults."""
