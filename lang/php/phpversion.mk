56acdea514c7b9858b6288626a025bd632f9c35b
taca <taca@pkgsrc.org>
2020-07-11 03:59:46 +0000

    lang/php73: update to 7.3.20
    Update php73 to 7.3.20.
    
    
    09 Jul 2020, PHP 7.3.20
    
    - Core:
      . Fixed bug #79650 (php-win.exe 100% cpu lockup). (cmb)
      . Fixed bug #79668 (get_defined_functions(true) may miss functions). (cmb,
        Nikita)
      . Fixed possibly unsupported timercmp() usage. (cmb)
    
    - Exif:
      . Fixed bug #79687 (Sony picture - PHP Warning - Make, Model, MakerNotes).
        (cmb)
    
    - Filter:
      . Fixed bug #73527 (Invalid memory access in php_filter_strip). (cmb)
    
    - GD:
      . Fixed bug #79676 (imagescale adds black border with IMG_BICUBIC). (cmb)
    
    - OpenSSL:
      . Fixed bug #62890 (default_socket_timeout=-1 causes connection to timeout).
        (cmb)
    
    - PDO SQLite:
      . Fixed bug #79664 (PDOStatement::getColumnMeta fails on empty result set).
        (cmb)
    
    - SPL:
      . Fixed bug #79710 (Reproducible segfault in error_handler during GC
        involved an SplFileObject). (Nikita)
    
    - Standard:
      . Fixed bug #74267 (segfault with streams and invalid data). (cmb)
      . Fixed bug #79579 (ZTS build of PHP 7.3.17 doesn't handle ERANGE for
        posix_getgrgid and others). (Böszörményi Zoltán)
    09 Jul 2020, PHP 7.3.20
    
    - Core:
      . Fixed bug #79650 (php-win.exe 100% cpu lockup). (cmb)
      . Fixed bug #79668 (get_defined_functions(true) may miss functions). (cmb,
        Nikita)
      . Fixed possibly unsupported timercmp() usage. (cmb)
    
    - Exif:
      . Fixed bug #79687 (Sony picture - PHP Warning - Make, Model, MakerNotes).
        (cmb)
    
    - Filter:
      . Fixed bug #73527 (Invalid memory access in php_filter_strip). (cmb)
    
    - GD:
      . Fixed bug #79676 (imagescale adds black border with IMG_BICUBIC). (cmb)
    
    - OpenSSL:
      . Fixed bug #62890 (default_socket_timeout=-1 causes connection to timeout).
        (cmb)
    
    - PDO SQLite:
      . Fixed bug #79664 (PDOStatement::getColumnMeta fails on empty result set).
        (cmb)
    
    - SPL:
      . Fixed bug #79710 (Reproducible segfault in error_handler during GC
        involved an SplFileObject). (Nikita)
    
    - Standard:
      . Fixed bug #74267 (segfault with streams and invalid data). (cmb)
      . Fixed bug #79579 (ZTS build of PHP 7.3.17 doesn't handle ERANGE for
        posix_getgrgid and others). (Böszörményi Zoltán)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.300 2020/06/14 05:59:17 taca Exp $
+# $NetBSD: phpversion.mk,v 1.301 2020/07/11 03:59:46 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -89,7 +89,7 @@ PHPVERSION_MK=	defined
 # Define each PHP's version.
 PHP56_VERSION=	5.6.40
 PHP72_VERSION=	7.2.31
-PHP73_VERSION=	7.3.19
+PHP73_VERSION=	7.3.20
 PHP74_VERSION=	7.4.7
 
 # Define initial release of major version.
