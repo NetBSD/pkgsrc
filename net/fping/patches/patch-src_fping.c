3c4f432f8b4d50df60cf11735f1fc6fd1d40c266
adam <adam@pkgsrc.org>
2020-07-12 19:31:03 +0000

    fping: updated to 4.3
    fping 4.3
    
    New features
    
    Linux unprivileged ping support
    Add SIGQUIT summary support similar to ping
    
    Bugfixes and other changes
    
    Corrected long option name of -s to --stats
    Do not fail if using fping6 with -6 flag
    Fail if interface binding (-I) does not work
    Fix using option -4 when fping is compiled IPv4-only
    Add Azure pipeline test build
    GCC 10 compatibility fixes
    Macos build fix
    Fix xmt stats in Netdata output
    Only increase num_alive if response is not a duplicate
    Use line buffering for stdout

--- net/fping/patches/patch-src_fping.c
+++ net/fping/patches/patch-src_fping.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_fping.c,v 1.1 2017/05/20 11:25:50 adam Exp $
+$NetBSD: patch-src_fping.c,v 1.2 2020/07/12 19:31:03 adam Exp $
 
 Fix for NetBSD-alpha.
 
---- src/fping.c.orig	2017-04-23 16:03:08.000000000 +0000
+--- src/fping.c.orig	2020-07-11 14:27:36.000000000 +0000
 +++ src/fping.c
-@@ -1787,7 +1787,7 @@ int decode_icmp_ipv4(
-     struct icmp* icp;
-     int hlen = 0;
+@@ -1877,7 +1877,7 @@ int decode_icmp_ipv4(
+     if (!using_sock_dgram4) {
+         struct ip* ip = (struct ip*)reply_buf;
  
 -#if defined(__alpha__) && __STDC__ && !defined(__GLIBC__)
 +#if defined(__alpha__) && __STDC__ && !defined(__GLIBC__) && !defined(__NetBSD__)
-     /* The alpha headers are decidedly broken.
-      * Using an ANSI compiler, it provides ip_vhl instead of ip_hl and
-      * ip_v.  So, to get ip_hl, we mask off the bottom four bits.
+         /* The alpha headers are decidedly broken.
+          * Using an ANSI compiler, it provides ip_vhl instead of ip_hl and
+          * ip_v.  So, to get ip_hl, we mask off the bottom four bits.
