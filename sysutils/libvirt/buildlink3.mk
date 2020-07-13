40788e2212f613c3e36f13efcb2608d5b96dfca4
wiz <wiz@pkgsrc.org>
2020-07-13 18:41:53 +0000

    libvirt: add buildlink3.mk
    From Edgar Fuß in PR 55479

---
+++ sysutils/libvirt/buildlink3.mk
@@ -0,0 +1,41 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/07/13 18:41:53 wiz Exp $
+
+BUILDLINK_TREE+=	libvirt
+
+.if !defined(LIBVIRT_BUILDLINK3_MK)
+LIBVIRT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libvirt+=	libvirt>=1.2.9nb27
+BUILDLINK_PKGSRCDIR.libvirt?=	../../sysutils/libvirt
+
+.include "../../devel/yajl/buildlink3.mk"
+.include "../../security/gnutls/buildlink3.mk"
+.include "../../security/cyrus-sasl/buildlink3.mk"
+.include "../../security/libgcrypt/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+pkgbase := libvirt
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.libvirt:Mxen}
+.include "../../sysutils/xentools42/buildlink3.mk"
+.endif
+.if ${PKG_BUILD_OPTIONS.libvirt:Mlibssh2}
+.include "../../security/libssh2/buildlink3.mk"
+.endif
+.if ${PKG_BUILD_OPTIONS.libvirt:Mhal}
+.include "../../sysutils/hal/buildlink3.mk"
+.endif
+.if ${PKG_BUILD_OPTIONS.libvirt:Mdbus}
+.include "../../sysutils/dbus/buildlink3.mk"
+.endif
+.if ${PKG_BUILD_OPTIONS.libvirt:Mavahi}
+.include "../../net/avahi/buildlink3.mk"
+.endif
+
+.endif	# LIBVIRT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libvirt
