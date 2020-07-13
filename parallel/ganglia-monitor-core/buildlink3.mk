a688924e6ff4abf084643c4b262bb4c98ffe442f
wiz <wiz@pkgsrc.org>
2020-07-13 18:40:47 +0000

    ganglia-monitor-core: add buildlink3.mk
    From Edgar Fuß in PR 55479.
    
    Some pkglint cleanup while here.

---
+++ parallel/ganglia-monitor-core/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2020/07/13 18:40:47 wiz Exp $
+
+BUILDLINK_TREE+=	ganglia-monitor-core
+
+.if !defined(GANGLIA_MONITOR_CORE_BUILDLINK3_MK)
+GANGLIA_MONITOR_CORE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ganglia-monitor-core+=	ganglia-monitor-core>=3.7.2nb8
+BUILDLINK_PKGSRCDIR.ganglia-monitor-core?=	../../parallel/ganglia-monitor-core
+
+.include "../../devel/apr/buildlink3.mk"
+.include "../../devel/confuse/buildlink3.mk"
+.include "../../devel/pcre/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../textproc/expat/buildlink3.mk"
+.endif	# GANGLIA_MONITOR_CORE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ganglia-monitor-core
