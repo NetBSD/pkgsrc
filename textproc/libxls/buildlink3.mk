24e9e48f388721d868eb582dacfd4915e14a8af0
sjmulder <sjmulder@pkgsrc.org>
2020-07-14 12:48:58 +0000

    libxls: Update to 1.5.3
    Changes:
     - Allow truncated XLS files #55 #60 #76 tidyverse/readxl#619
     - Fix long-standing "extra column" bug #73
     - Support for RSTRING records (rich-text cells in older BIFF5 files)
       tidyverse/readxl#611
    
    Minimum version updated in bl3 due to header changes.

--- textproc/libxls/buildlink3.mk
+++ textproc/libxls/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2019/02/11 21:55:27 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/07/14 12:48:58 sjmulder Exp $
 
 BUILDLINK_TREE+=	libxls
 
 .if !defined(LIBXLS_BUILDLINK3_MK)
 LIBXLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxls+=	libxls>=1.5.0
+BUILDLINK_API_DEPENDS.libxls+=	libxls>=1.5.3
 BUILDLINK_PKGSRCDIR.libxls?=	../../textproc/libxls
 
 .include "../../converters/libiconv/buildlink3.mk"
