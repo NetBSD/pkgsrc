8cc07a768fe54e6d2d4dc59ebc9b62a524f60b6a
perseant <perseant@pkgsrc.org>
2020-07-16 14:49:13 +0000

    Update to Shibboleth SP 3.1.0.  Fixes PR pkg/54639.

--- www/shibboleth-sp/patches/patch-shibsp_attribute_resolver_impl_ChainingAttributeExtractor.cpp
+++ www/shibboleth-sp/patches/patch-shibsp_attribute_resolver_impl_ChainingAttributeExtractor.cpp
@@ -1,13 +1,11 @@
-$NetBSD: patch-shibsp_attribute_resolver_impl_ChainingAttributeExtractor.cpp,v 1.2 2015/10/23 07:35:08 pettai Exp $
-
---- shibsp/attribute/resolver/impl/ChainingAttributeExtractor.cpp.orig	2015-10-19 17:27:55.000000000 -0700
-+++ shibsp/attribute/resolver/impl/ChainingAttributeExtractor.cpp	2015-10-19 17:28:27.000000000 -0700
-@@ -170,7 +170,7 @@
+--- shibsp/attribute/resolver/impl/ChainingAttributeExtractor.cpp.orig	2018-07-09 18:17:23.000000000 -0700
++++ shibsp/attribute/resolver/impl/ChainingAttributeExtractor.cpp	2020-07-07 12:26:19.008490542 -0700
+@@ -139,7 +139,7 @@
                  m_extractors.push_back(np.get());
                  np.release();
              }
--            catch (exception& ex) {
-+            catch (std::exception& ex) {
+-            catch (const exception& ex) {
++            catch (const std::exception& ex) {
                  Category::getInstance(SHIBSP_LOGCAT ".AttributeExtractor.Chaining").error(
                      "caught exception processing embedded AttributeExtractor element: %s", ex.what()
                      );
