6a393576938752d30375e00142c01a25a8faa2ae
perseant <perseant@pkgsrc.org>
2020-07-16 14:49:13 +0000

    Update to Shibboleth SP 3.1.0.  Fixes PR pkg/54639.

--- www/shibboleth-sp/patches/patch-shibsp_attribute_filtering_impl_XMLAttributeFilter.cpp
+++ www/shibboleth-sp/patches/patch-shibsp_attribute_filtering_impl_XMLAttributeFilter.cpp
@@ -1,13 +1,11 @@
-$NetBSD: patch-shibsp_attribute_filtering_impl_XMLAttributeFilter.cpp,v 1.1 2013/09/29 10:19:47 joerg Exp $
-
---- shibsp/attribute/filtering/impl/XMLAttributeFilter.cpp.orig	2013-09-29 08:53:37.000000000 +0000
-+++ shibsp/attribute/filtering/impl/XMLAttributeFilter.cpp
-@@ -249,7 +249,7 @@ MatchFunctor* XMLFilterImpl::buildFuncto
+--- shibsp/attribute/filtering/impl/XMLAttributeFilter.cpp.orig	2020-04-01 16:27:44.000000000 -0700
++++ shibsp/attribute/filtering/impl/XMLAttributeFilter.cpp	2020-07-07 12:23:32.612919412 -0700
+@@ -259,7 +259,7 @@
              functorMap.getMatchFunctors().insert(multimap<string,MatchFunctor*>::value_type(id, func.get()));
              return func.release();
          }
--        catch (exception& ex) {
-+        catch (std::exception& ex) {
+-        catch (const exception& ex) {
++        catch (const std::exception& ex) {
              m_log.error("error building %s with type (%s): %s", logname, type->toString().c_str(), ex.what());
          }
      }
--- www/shibboleth-sp/patches/patch-shibsp_attribute_resolver_impl_QueryAttributeResolver.cpp
+++ www/shibboleth-sp/patches/patch-shibsp_attribute_resolver_impl_QueryAttributeResolver.cpp
@@ -1,85 +1,83 @@
-$NetBSD: patch-shibsp_attribute_resolver_impl_QueryAttributeResolver.cpp,v 1.1 2013/09/29 10:19:47 joerg Exp $
-
---- shibsp/attribute/resolver/impl/QueryAttributeResolver.cpp.orig	2013-09-29 08:58:22.000000000 +0000
-+++ shibsp/attribute/resolver/impl/QueryAttributeResolver.cpp
-@@ -277,7 +277,7 @@ QueryResolver::QueryResolver(const DOMEl
+--- shibsp/attribute/resolver/impl/QueryAttributeResolver.cpp.orig	2020-03-18 11:45:13.000000000 -0700
++++ shibsp/attribute/resolver/impl/QueryAttributeResolver.cpp	2020-07-07 12:26:37.773204823 -0700
+@@ -266,7 +266,7 @@
                  }
              }
          }
--        catch (exception& ex) {
-+        catch (std::exception& ex) {
+-        catch (const exception& ex) {
++        catch (const std::exception& ex) {
              m_log.error("exception loading attribute designator: %s", ex.what());
          }
          child = XMLHelper::getNextSiblingElement(child);
-@@ -347,7 +347,7 @@ void QueryResolver::SAML1Query(QueryCont
+@@ -342,7 +342,7 @@
              client.sendSAML(request, application.getId(), mcc, loc.get());
              response.reset(client.receiveSAML());
          }
--        catch (exception& ex) {
-+        catch (std::exception& ex) {
+-        catch (const exception& ex) {
++        catch (const std::exception& ex) {
              m_log.error("exception during SAML query to %s: %s", loc.get(), ex.what());
              soaper.reset();
          }
-@@ -393,7 +393,7 @@ void QueryResolver::SAML1Query(QueryCont
+@@ -395,7 +395,7 @@
          if (!policy->isAuthenticated())
              throw SecurityPolicyException("Security of SAML 1.x query result not established.");
      }
--    catch (exception& ex) {
-+    catch (std::exception& ex) {
+-    catch (const exception& ex) {
++    catch (const std::exception& ex) {
          m_log.error("assertion failed policy validation: %s", ex.what());
          throw;
      }
-@@ -435,7 +435,7 @@ void QueryResolver::SAML1Query(QueryCont
+@@ -437,7 +437,7 @@
              filter->filterAttributes(fc, ctx.getResolvedAttributes());
          }
      }
--    catch (exception& ex) {
-+    catch (std::exception& ex) {
+-    catch (const exception& ex) {
++    catch (const std::exception& ex) {
          m_log.error("caught exception extracting/filtering attributes from query result: %s", ex.what());
          for_each(ctx.getResolvedAttributes().begin(), ctx.getResolvedAttributes().end(), xmltooling::cleanup<shibsp::Attribute>());
          ctx.getResolvedAttributes().clear();
-@@ -517,7 +517,7 @@ void QueryResolver::SAML2Query(QueryCont
+@@ -534,7 +534,7 @@
              client.sendSAML(query, application.getId(), mcc, loc.get());
              srt.reset(client.receiveSAML());
          }
--        catch (exception& ex) {
-+        catch (std::exception& ex) {
+-        catch (const exception& ex) {
++        catch (const std::exception& ex) {
              m_log.error("exception during SAML query to %s: %s", loc.get(), ex.what());
              soaper.reset();
          }
-@@ -581,7 +581,7 @@ void QueryResolver::SAML2Query(QueryCont
+@@ -605,7 +605,7 @@
                      m_log.debugStream() << "decrypted assertion: " << *newtoken << logging::eol;
              }
          }
--        catch (exception& ex) {
-+        catch (std::exception& ex) {
+-        catch (const exception& ex) {
++        catch (const std::exception& ex) {
              m_log.error("failed to decrypt assertion: %s", ex.what());
              throw;
          }
-@@ -648,7 +648,7 @@ void QueryResolver::SAML2Query(QueryCont
+@@ -672,7 +672,7 @@
              }
          }
      }
--    catch (exception& ex) {
-+    catch (std::exception& ex) {
+-    catch (const exception& ex) {
++    catch (const std::exception& ex) {
          m_log.error("assertion failed policy validation: %s", ex.what());
          throw;
      }
-@@ -677,7 +677,7 @@ void QueryResolver::SAML2Query(QueryCont
+@@ -701,7 +701,7 @@
              filter->filterAttributes(fc, ctx.getResolvedAttributes());
          }
      }
--    catch (exception& ex) {
-+    catch (std::exception& ex) {
+-    catch (const exception& ex) {
++    catch (const std::exception& ex) {
          m_log.error("caught exception extracting/filtering attributes from query result: %s", ex.what());
          for_each(ctx.getResolvedAttributes().begin(), ctx.getResolvedAttributes().end(), xmltooling::cleanup<shibsp::Attribute>());
          ctx.getResolvedAttributes().clear();
-@@ -716,7 +716,7 @@ void QueryResolver::resolveAttributes(Re
+@@ -742,7 +742,7 @@
              m_log.warn("can't attempt attribute query, either no NameID or no metadata to use");
          }
      }
--    catch (exception& ex) {
-+    catch (std::exception& ex) {
+-    catch (const exception& ex) {
++    catch (const std::exception& ex) {
          // Already logged.
          if (!m_exceptionId.empty()) {
              auto_ptr<SimpleAttribute> attr(new SimpleAttribute(m_exceptionId));
