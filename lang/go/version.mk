5d31284fb328c9546134790425fbb84f26926be1
spz <spz@pkgsrc.org>
2020-07-20 14:59:01 +0000

    Pullup ticket #6279 - requested by bsiegert lang/go114: security update
    Revisions pulled up:
    - lang/go/version.mk                                            1.94
    - lang/go114/PLIST                                              1.5
    - lang/go114/distinfo                                           1.8
    
    -------------------------------------------------------------------
       Module Name:    pkgsrc
       Committed By:   bsiegert
       Date:           Fri Jul 17 17:20:06 UTC 2020
    
       Modified Files:
               pkgsrc/lang/go: version.mk
               pkgsrc/lang/go114: PLIST distinfo
    
       Log Message:
       Update go114 to 1.14.6.
    
       go1.14.5 (released 2020/07/14) includes security fixes to the crypto/x509
       and
       net/http packages. See the Go 1.14.5 milestone on our issue tracker for
       details.
    
       go1.14.6 (released 2020/07/16) includes fixes to the go command, the
       compiler,
       the linker, vet, and the database/sql, encoding/json, net/http, reflect, and
       testing packages. See the Go 1.14.6 milestone on our issue tracker for
       details.
    
    
       To generate a diff of this commit:
       cvs rdiff -u -r1.93 -r1.94 pkgsrc/lang/go/version.mk
       cvs rdiff -u -r1.4 -r1.5 pkgsrc/lang/go114/PLIST
       cvs rdiff -u -r1.7 -r1.8 pkgsrc/lang/go114/distinfo

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.92.2.1 2020/07/20 14:52:20 spz Exp $
+# $NetBSD: version.mk,v 1.92.2.2 2020/07/20 14:59:01 spz Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -6,7 +6,7 @@
 #
 .include "go-vars.mk"
 
-GO114_VERSION=	1.14.4
+GO114_VERSION=	1.14.6
 GO113_VERSION=	1.13.14
 GO110_VERSION=	1.10.8
 GO19_VERSION=	1.9.7
