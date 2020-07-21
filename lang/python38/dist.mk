5fa2ad4785e6de590cd17e2ae0fa0d3f026b2cef
adam <adam@pkgsrc.org>
2020-07-21 16:31:15 +0000

    python38 py38-html-docs: updated to 3.8.5
    Python 3.8.5 final
    
    Security
    bpo-41304: Fixes python3x._pth being ignored on Windows, caused by the fix for bpo-29778 (CVE-2020-15801).
    bpo-39603: Prevent http header injection by rejecting control characters in http.client.putrequest(…).
    
    Core and Builtins
    bpo-41295: Resolve a regression in CPython 3.8.4 where defining “__setattr__” in a multi-inheritance setup and calling up the hierarchy chain could fail if builtins/extension types were involved in the base types.
    
    Library
    bpo-41288: Unpickling invalid NEWOBJ_EX opcode with the C implementation raises now UnpicklingError instead of crashing.
    bpo-39017: Avoid infinite loop when reading specially crafted TAR files using the tarfile module (CVE-2019-20907).
    
    Documentation
    bpo-37703: Updated Documentation to comprehensively elaborate on the behaviour of gather.cancel()
    
    Build
    bpo-41302: Enable building Python 3.8 with libmpdec-2.5.0 to ease maintenance for Linux distributions. Patch by Felix Yan.
    
    macOS
    bpo-40741: Update macOS installer to use SQLite 3.32.3.
    
    IDLE
    bpo-41300: Save files with non-ascii chars. Fix regression released in 3.9.0b4 and 3.8.4.

--- lang/python38/dist.mk
+++ lang/python38/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.5 2020/07/14 19:12:31 adam Exp $
+# $NetBSD: dist.mk,v 1.6 2020/07/21 16:31:15 adam Exp $
 
-PY_DISTVERSION=	3.8.4
+PY_DISTVERSION=	3.8.5
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python38/distinfo
