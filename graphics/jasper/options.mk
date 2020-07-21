ab096a9110bf6c4782ff2438db0cbccf06748b7a
adam <adam@pkgsrc.org>
2020-07-21 07:31:28 +0000

    jasper: updated to 2.0.18
    2.0.18:
    Unknown changes.

--- graphics/jasper/options.mk
+++ graphics/jasper/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2017/02/21 18:24:20 adam Exp $
+# $NetBSD: options.mk,v 1.2 2020/07/21 07:31:28 adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.jasper
 PKG_SUPPORTED_OPTIONS=	opengl
@@ -7,12 +7,12 @@ PKG_SUPPORTED_OPTIONS=	opengl
 
 PLIST_VARS+=		opengl
 
-.if empty(PKG_OPTIONS:Mopengl)
-CMAKE_ARGS+=		-DJAS_ENABLE_OPENGL=OFF
-.else
-CMAKE_ARGS+=		-DJAS_ENABLE_OPENGL=ON
-PLIST.opengl=		yes
+.if !empty(PKG_OPTIONS:Mopengl)
+CMAKE_ARGS+=	-DJAS_ENABLE_OPENGL=ON
+PLIST.opengl=	yes
 .  if ${OPSYS} != "Darwin"
-.    include "../../graphics/glut/buildlink3.mk"
+.    include "../../graphics/freeglut/buildlink3.mk"
 .  endif
+.else
+CMAKE_ARGS+=	-DJAS_ENABLE_OPENGL=OFF
 .endif
