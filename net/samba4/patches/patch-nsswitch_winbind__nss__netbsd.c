0e6a36c5cf34e3a5461682e821fffda4a474429a
christos <christos@pkgsrc.org>
2020-07-21 18:42:25 +0000

    Fix arguments to getgroupmembership

--- net/samba4/patches/patch-nsswitch_winbind__nss__netbsd.c
+++ net/samba4/patches/patch-nsswitch_winbind__nss__netbsd.c
@@ -1,10 +1,19 @@
-$NetBSD: patch-nsswitch_winbind__nss__netbsd.c,v 1.1 2019/03/20 19:09:10 adam Exp $
+$NetBSD: patch-nsswitch_winbind__nss__netbsd.c,v 1.2 2020/07/21 18:42:25 christos Exp $
 
 Syntax error fix.
+Add primary group support
 
---- nsswitch/winbind_nss_netbsd.c.orig	2019-03-20 11:23:40.911995594 +0000
-+++ nsswitch/winbind_nss_netbsd.c
-@@ -185,7 +185,7 @@ netbsdwinbind_getgroupmembership(void *n
+--- nsswitch/winbind_nss_netbsd.c.orig	2020-01-31 05:25:36.000000000 -0500
++++ nsswitch/winbind_nss_netbsd.c	2020-07-21 10:24:19.651265315 -0400
+@@ -176,6 +176,7 @@
+ {
+ 	int		*result	= va_arg(ap, int *);
+ 	const char 	*uname	= va_arg(ap, const char *);
++	gid_t		 agroup	= va_arg(ap, gid_t);
+ 	gid_t		*groups	= va_arg(ap, gid_t *);
+ 	int		 maxgrp	= va_arg(ap, int);
+ 	int		*groupc	= va_arg(ap, int *);
+@@ -185,10 +186,17 @@
  	};
  	struct winbindd_response response = {
  		.length = 0,
@@ -13,3 +22,13 @@ Syntax error fix.
  	gid_t	*wblistv;
  	int	wblistc, i, isdup, dupc;
  
++	/* add the primary group */
++	if (*groupc < maxgrp)
++		groups[*groupc] = agroup;
++	else
++		*result = -1;
++	(*groupc)++;
++
+ 	strncpy(request.data.username, uname,
+ 				sizeof(request.data.username) - 1);
+ 	i = winbindd_request_response(NULL, WINBINDD_GETGROUPS,
