e2c6fe596ef20fc89904036dabf21a20f9f3d9a5
bacon <bacon@pkgsrc.org>
2020-07-22 14:50:14 +0000

    biology/htslib: Upgrade to 1.10.2
    Numerous enhancements and bug fixes since 1.9, including:
    
    Elimited refernce file size limits
    SAM header API
    On-the-fly indexing
    Improved Amazon S3 interface
    
    This package contains a patch for recent libcurl not in the upstream release

--- biology/htslib/buildlink3.mk
+++ biology/htslib/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/05/01 13:20:44 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/07/22 14:50:14 bacon Exp $
 
 BUILDLINK_TREE+=	htslib
 
 .if !defined(HTSLIB_BUILDLINK3_MK)
 HTSLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.8
+BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.10.2
 BUILDLINK_PKGSRCDIR.htslib?=	../../biology/htslib
 
 .endif	# HTSLIB_BUILDLINK3_MK
