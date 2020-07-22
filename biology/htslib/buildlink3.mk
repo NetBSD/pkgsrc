5bcef207cb68f678a805d9679249fbb29d8772a0
bacon <bacon@pkgsrc.org>
2020-07-22 15:36:55 +0000

    bioloty/htslib: Correct bl2 (ABI bump, not API)
    Also add -if to autoreconf

--- biology/htslib/buildlink3.mk
+++ biology/htslib/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2020/07/22 14:50:14 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2020/07/22 15:36:55 bacon Exp $
 
 BUILDLINK_TREE+=	htslib
 
 .if !defined(HTSLIB_BUILDLINK3_MK)
 HTSLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.10.2
+BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.8
+BUILDLINK_ABI_DEPENDS.htslib+=	htslib>=1.10.2
 BUILDLINK_PKGSRCDIR.htslib?=	../../biology/htslib
 
 .endif	# HTSLIB_BUILDLINK3_MK
