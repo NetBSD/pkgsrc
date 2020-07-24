93b22f03d0768b8f2bc101759ee8daf6e1214b90
fox <fox@pkgsrc.org>
2020-07-24 07:29:32 +0000

    www/cliqz: Updates to 1.38.0
    - Removes some of the obsolete patches.
    - Bumps versions of dependencies.
    
    Changes since 1.36.0:
    
    Merge with Firefox 78.0.2

--- www/cliqz/patches/patch-mozilla-release_js_src_jsfriendapi.h
+++ www/cliqz/patches/patch-mozilla-release_js_src_jsfriendapi.h
@@ -1,13 +1,13 @@
-$NetBSD: patch-mozilla-release_js_src_jsfriendapi.h,v 1.1 2020/04/18 07:58:36 fox Exp $
+$NetBSD: patch-mozilla-release_js_src_jsfriendapi.h,v 1.2 2020/07/24 07:29:32 fox Exp $
 
 * Fix va_list error.
 
 Taken from www/firefox
 
---- mozilla-release/js/src/jsfriendapi.h.orig	2020-04-03 19:34:47.000000000 +0000
+--- mozilla-release/js/src/jsfriendapi.h.orig	2020-05-22 02:11:19.000000000 +0000
 +++ mozilla-release/js/src/jsfriendapi.h
-@@ -13,6 +13,7 @@
- #include "mozilla/MemoryReporting.h"
+@@ -14,6 +14,7 @@
+ #include "mozilla/PodOperations.h"
  #include "mozilla/UniquePtr.h"
  
 +#include <stdarg.h>
