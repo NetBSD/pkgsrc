f55caff0a2577d5cb5fc1f93aa60a7876d67e9a2
fox <fox@pkgsrc.org>
2020-07-24 07:29:32 +0000

    www/cliqz: Updates to 1.38.0
    - Removes some of the obsolete patches.
    - Bumps versions of dependencies.
    
    Changes since 1.36.0:
    
    Merge with Firefox 78.0.2

---
+++ www/cliqz/patches/patch-mozilla-release_third__party_rust_getrandom_src_lib.rs
@@ -0,0 +1,26 @@
+$NetBSD: patch-mozilla-release_third__party_rust_getrandom_src_lib.rs,v 1.1 2020/07/24 07:29:32 fox Exp $
+
+https://github.com/rust-random/getrandom/pull/115
+
+Taken from www/firefox
+
+--- mozilla-release/third_party/rust/getrandom/src/lib.rs.orig	2020-06-02 23:37:31.000000000 +0000
++++ mozilla-release/third_party/rust/getrandom/src/lib.rs
+@@ -17,7 +17,7 @@
+ //! | macOS, iOS       | [`SecRandomCopyBytes`][4]
+ //! | FreeBSD          | [`kern.arandom`][5]
+ //! | OpenBSD, Bitrig  | [`getentropy`][6]
+-//! | NetBSD           | [`/dev/urandom`][7] after reading from `/dev/random` once
++//! | NetBSD           | [`kern.arandom`][5]
+ //! | Dragonfly BSD    | [`/dev/random`][8]
+ //! | Solaris, illumos | [`getrandom`][9] system call if available, otherwise [`/dev/random`][10]
+ //! | Fuchsia OS       | [`cprng_draw`][11]
+@@ -184,7 +184,7 @@ mod_use!(cfg(target_os = "illumos"), sol
+ mod_use!(cfg(target_os = "ios"), macos);
+ mod_use!(cfg(target_os = "linux"), linux_android);
+ mod_use!(cfg(target_os = "macos"), macos);
+-mod_use!(cfg(target_os = "netbsd"), use_file);
++mod_use!(cfg(target_os = "netbsd"), freebsd);
+ mod_use!(cfg(target_os = "openbsd"), openbsd_bitrig);
+ mod_use!(cfg(target_os = "redox"), use_file);
+ mod_use!(cfg(target_os = "solaris"), solaris_illumos);
