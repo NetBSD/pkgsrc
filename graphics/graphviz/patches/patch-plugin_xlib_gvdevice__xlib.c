8ff245d1cedffe56312ef57f21428e15dc0ac187
adam <adam@pkgsrc.org>
2020-07-27 20:44:15 +0000

    graphviz: updated to 2.44.1
    2.44.1
    
    Added
    
    applied RH patches (from graphviz-2.42.2-8.fc32.src.rpm)
    
    graphviz-2.42.2-coverity-scan-fixes.patch
    graphviz-2.42.2-dotty-menu-fix.patch
    graphviz-2.42.2-ocaml-allow-const-cast.patch
    
    some allocation failures that could previously allow memory corruption now exit
    lab_gamut.3.pdf is no longer included in release archives
    
    Changed
    Windows binaries available at https://www2.graphviz.org/Packages/ instead of
    https://ci.appveyor.com/project/ellson/graphviz-pl238
    Retarget Windows builds to Visual Studio 2019 and toolset v142
    
    Fixed
    Released Ubuntu packages does not contain language bindings for Python3
    Neato's hier mode is broken since v2.44.0
    Segmentation fault (core dumped)
    
    
    2.44.0
    
    Added
    New SGD mode in neato
    Add pkg-config files
    tred: add feature to output removed edges to stderr upon request
    Workaround: avoid creating a virtual edge loop.
    Add riscv64 to host_cpu configure.ac
    lib/cgraph: include empty malloc.h from subdir include
    lib/gvpr: compile mkdefs with $(HOSTCC) rather than $(CC)
    lib/vpsc: rename bcopy->b_copy
    
    Fixed
    MSB4018 The NativeCodeAnalysis task failed unexpectedly.

--- graphics/graphviz/patches/patch-plugin_xlib_gvdevice__xlib.c
+++ graphics/graphviz/patches/patch-plugin_xlib_gvdevice__xlib.c
@@ -1,10 +1,10 @@
-$NetBSD: patch-plugin_xlib_gvdevice__xlib.c,v 1.1 2015/01/31 23:25:26 jperkin Exp $
+$NetBSD: patch-plugin_xlib_gvdevice__xlib.c,v 1.2 2020/07/27 20:44:15 adam Exp $
 
 SunOS inotify requires sys/filio.h for FIONREAD.
 
---- plugin/xlib/gvdevice_xlib.c.orig	2014-04-13 20:40:25.000000000 +0000
+--- plugin/xlib/gvdevice_xlib.c.orig	2020-04-08 07:51:58.000000000 +0000
 +++ plugin/xlib/gvdevice_xlib.c
-@@ -39,6 +39,9 @@
+@@ -32,6 +32,9 @@
  #endif
  #ifdef HAVE_SYS_INOTIFY_H
  #include <sys/inotify.h>
@@ -12,5 +12,5 @@ SunOS inotify requires sys/filio.h for FIONREAD.
 +#include <sys/filio.h>
 +#endif
  #endif
- #ifdef HAVE_ERRNO_H
  #include <errno.h>
+ #ifdef HAVE_FCNTL_H
