d232e3d6c3e59c15f749050abfd77d1d8bf63e2c
adam <adam@pkgsrc.org>
2020-07-27 16:08:13 +0000

    meson: updated to 0.55.0
    0.55.0:
    rpath removal now more careful
    Added ability to specify targets in meson compile
    Test protocol for gtest
    meson.add_*_script methods accept new types
    Machine file constants
    Configure CMake subprojects with meson.subproject_options
    find_program: Fixes when the program has been overridden by executable
    Response files enabled on Linux, reined in on Windows
    unstable-kconfig module renamed to unstable-keyval
    Fatal warnings in gnome.generate_gir()
    b_ndebug support for D language compilers
    Meson test now produces JUnit xml from results
    Config tool based dependencies no longer search PATH for cross compiling
    Rename has_exe_wrapper -> can_run_host_binaries
    String concatenation in meson_options.txt
    Wrap fallback URL
    Clang coverage support
    Local wrap source and patch files
    Local wrap patch directory
    Patch on all wrap types
    link_language argument added to all targets
    meson dist --no-tests
    Force fallback for
    Implicit dependency fallback
    Wrap file provide section
    find_program() fallback
    Test scripts are given the exe wrapper if needed
    Added ability to specify backend arguments in meson compile
    Introspection API changes

--- devel/meson/patches/patch-mesonbuild_scripts_depfixer.py
+++ devel/meson/patches/patch-mesonbuild_scripts_depfixer.py
@@ -1,14 +1,14 @@
-$NetBSD: patch-mesonbuild_scripts_depfixer.py,v 1.1 2019/10/04 14:06:19 prlw1 Exp $
+$NetBSD: patch-mesonbuild_scripts_depfixer.py,v 1.2 2020/07/27 16:08:13 adam Exp $
 
 Do not touch rpath for ELF.
 
---- mesonbuild/scripts/depfixer.py.orig	2019-06-06 05:17:20.000000000 +0000
+--- mesonbuild/scripts/depfixer.py.orig	2020-06-29 17:00:15.000000000 +0000
 +++ mesonbuild/scripts/depfixer.py
-@@ -439,8 +439,6 @@ def fix_rpath(fname, new_rpath, final_pa
+@@ -457,8 +457,6 @@ def fix_rpath(fname, rpath_dirs_to_remov
          if fname.endswith('.jar'):
              fix_jar(fname)
              return
--        fix_elf(fname, new_rpath, verbose)
+-        fix_elf(fname, rpath_dirs_to_remove, new_rpath, verbose)
 -        return
      except SystemExit as e:
          if isinstance(e.code, int) and e.code == 0:
