1a2f99ff3226b298b6b9214066542697ab2549d9
tnn <tnn@pkgsrc.org>
2020-07-28 22:43:01 +0000

    meson: fix C++ compiler detection fallout from last update. Bump.

---
+++ devel/meson/patches/patch-mesonbuild_environment.py
@@ -0,0 +1,34 @@
+$NetBSD: patch-mesonbuild_environment.py,v 1.3 2020/07/28 22:43:01 tnn Exp $
+
+Specify c++ language when making CXX parse standard input.
+Fixes problem where graphics/MesaLib fails to build due to:
+
+> clang++ -std=c++14 -dM -E -
+> error: invalid argument '-std=c++14' not allowed with 'C'
+
+--- mesonbuild/environment.py.orig	2020-07-05 20:13:14.000000000 +0000
++++ mesonbuild/environment.py
+@@ -708,7 +708,10 @@ class Environment:
+         """
+         # Arguments to output compiler pre-processor defines to stdout
+         # gcc, g++, and gfortran all support these arguments
+-        args = compiler + ['-E', '-dM', '-']
++        args = compiler + ['-E', '-dM' ]
++        if "++" in compiler[0]:
++          args += [ '-x', 'c++' ]
++        args += [ '-' ]
+         p, output, error = Popen_safe(args, write='', stdin=subprocess.PIPE)
+         if p.returncode != 0:
+             raise EnvironmentException('Unable to detect GNU compiler type:\n' + output + error)
+@@ -750,7 +753,10 @@ class Environment:
+         """
+         Get the list of Clang pre-processor defines
+         """
+-        args = compiler + ['-E', '-dM', '-']
++        args = compiler + ['-E', '-dM' ]
++        if "++" in compiler[0]:
++          args += [ '-x', 'c++' ]
++        args += [ '-' ]
+         p, output, error = Popen_safe(args, write='', stdin=subprocess.PIPE)
+         if p.returncode != 0:
+             raise EnvironmentException('Unable to get clang pre-processor defines:\n' + output + error)
