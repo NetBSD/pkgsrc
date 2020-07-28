32dc3162dc0ab8a763b3fbda8552b65b1ba8097b
gutteridge <gutteridge@pkgsrc.org>
2020-07-28 00:06:09 +0000

    libfm-extra: fix typo in comment reported by pkglint

--- sysutils/libfm-extra/buildlink3.mk
+++ sysutils/libfm-extra/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2016/05/22 22:08:40 youri Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/07/28 00:06:09 gutteridge Exp $
 
 BUILDLINK_TREE+=	libfm-extra
 
@@ -8,6 +8,6 @@ LIBFM_EXTRA_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libfm-extra+=	libfm-extra>=1.2.4
 BUILDLINK_PKGSRCDIR.libfm-extra?=	../../sysutils/libfm-extra
 
-.endif	# LIBFM-EXTRA_BUILDLINK3_MK
+.endif	# LIBFM_EXTRA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libfm-extra
