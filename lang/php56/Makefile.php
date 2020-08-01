3a6d1cfa373c5b3ab7023acf085ac2d059216026
wiz <wiz@pkgsrc.org>
2020-08-01 09:52:51 +0000

    php56: add default-off php-embed option to install embed SAPI
    Requested by Sergey Osokin in PR 55508.
    
    While here, split off option handling into options.mk.

--- lang/php56/Makefile.php
+++ lang/php56/Makefile.php
@@ -1,4 +1,4 @@
-# $NetBSD: Makefile.php,v 1.7 2019/03/31 20:48:18 wiz Exp $
+# $NetBSD: Makefile.php,v 1.8 2020/08/01 09:52:51 wiz Exp $
 # used by lang/php56/Makefile
 # used by www/ap-php/Makefile
 # used by www/php-fpm/Makefile
@@ -42,60 +42,7 @@ CONFIGURE_ARGS+=	--enable-xml
 CONFIGURE_ARGS+=	--with-libxml-dir=${PREFIX}
 .include "../../textproc/libxml2/buildlink3.mk"
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.${PHP_PKG_PREFIX}
-PKG_SUPPORTED_OPTIONS+=	inet6 ssl maintainer-zts readline disable-filter-url
-PKG_SUGGESTED_OPTIONS+=	inet6 ssl
-
-.if ${OPSYS} == "SunOS" || ${OPSYS} == "Darwin" || ${OPSYS} == "FreeBSD"
-PKG_SUPPORTED_OPTIONS+=	dtrace
-.endif
-
-.include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGS+=	--enable-ipv6
-.else
-CONFIGURE_ARGS+=	--disable-ipv6
-.endif
-
-.if !empty(PKG_OPTIONS:Mssl)
-.  include "../../security/openssl/buildlink3.mk"
-.  if ${OPSYS} == "SunOS"
-CONFIGURE_ARGS+=	--with-openssl=yes
-LIBS.SunOS+=		-lcrypto
-.  else
-CONFIGURE_ARGS+=	--with-openssl=${BUILDLINK_PREFIX.openssl}
-.  endif
-PATCH_SITES+=		http://zettasystem.com/
-PATCHFILES+=		PHP-5.6.31-OpenSSL-1.1.0-compatibility-20170801.patch
-PATCH_DIST_STRIP=	-p1
-.else
-CONFIGURE_ARGS+=	--without-openssl
-.endif
-
-.if !empty(PKG_OPTIONS:Mmaintainer-zts)
-CONFIGURE_ARGS+=	--enable-maintainer-zts
-.endif
-
-.if !empty(PKG_OPTIONS:Mreadline)
-USE_GNU_READLINE=	yes
-.include "../../devel/readline/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-readline=${BUILDLINK_PREFIX.readline}
-.else
-CONFIGURE_ARGS+=	--without-readline
-.endif
-
-.if !empty(PKG_OPTIONS:Mdtrace)
-PLIST.dtrace=		yes
-CONFIGURE_ARGS+=	--enable-dtrace
-
-# See https://bugs.php.net/bug.php?id=61268
-INSTALL_MAKE_FLAGS+=	-r
-.endif
-
-.if !empty(PKG_OPTIONS:Mdisable-filter-url)
-CFLAGS+=		-DDISABLE_FILTER_URL
-.endif
+.include "options.mk"
 
 DL_AUTO_VARS=		yes
 .include "../../mk/dlopen.buildlink3.mk"
