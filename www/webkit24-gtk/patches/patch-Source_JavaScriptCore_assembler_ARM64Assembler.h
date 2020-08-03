4470280346cff5d903bf1ce3d9b1e166befdddce
jmcneill <jmcneill@pkgsrc.org>
2020-08-03 08:36:23 +0000

    Include <limits.h> for INT_MIN definition.

--- www/webkit24-gtk/patches/patch-Source_JavaScriptCore_assembler_ARM64Assembler.h
+++ www/webkit24-gtk/patches/patch-Source_JavaScriptCore_assembler_ARM64Assembler.h
@@ -1,8 +1,16 @@
-$NetBSD: patch-Source_JavaScriptCore_assembler_ARM64Assembler.h,v 1.1 2020/08/02 17:41:41 jmcneill Exp $
+$NetBSD: patch-Source_JavaScriptCore_assembler_ARM64Assembler.h,v 1.2 2020/08/03 08:36:23 jmcneill Exp $
 
 --- Source/JavaScriptCore/assembler/ARM64Assembler.h.orig	2016-04-10 06:48:36.000000000 +0000
 +++ Source/JavaScriptCore/assembler/ARM64Assembler.h
-@@ -2756,6 +2756,9 @@ public:
+@@ -32,6 +32,7 @@
+ #include <wtf/Assertions.h>
+ #include <wtf/Vector.h>
+ #include <stdint.h>
++#include <limits.h>
+ 
+ #define CHECK_DATASIZE_OF(datasize) ASSERT(datasize == 32 || datasize == 64)
+ #define DATASIZE_OF(datasize) ((datasize == 64) ? Datasize_64 : Datasize_32)
+@@ -2756,6 +2757,9 @@ public:
      {
  #if OS(IOS)
          sys_cache_control(kCacheFunctionPrepareForExecution, code, size);
