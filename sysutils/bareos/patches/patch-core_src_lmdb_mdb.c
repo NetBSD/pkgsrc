24a83f9f4d1fa02facc59e10ebc4cb41c27bde3f
kardel <kardel@pkgsrc.org>
2020-08-04 12:56:15 +0000

    improve lmdb compile patch

--- sysutils/bareos/patches/patch-core_src_lmdb_mdb.c
+++ sysutils/bareos/patches/patch-core_src_lmdb_mdb.c
@@ -1,25 +1,19 @@
-$NetBSD: patch-core_src_lmdb_mdb.c,v 1.1 2020/07/28 06:36:29 kardel Exp $
+$NetBSD: patch-core_src_lmdb_mdb.c,v 1.2 2020/08/04 12:56:15 kardel Exp $
 
-	Fix compilation on NetBSD
+	use POSIX semaphores on NetBSD
 
---- core/src/lmdb/mdb.c.orig	2018-12-20 08:51:51.000000000 +0000
+--- core/src/lmdb/mdb.c.orig	2020-08-04 12:17:56.368741881 +0000
 +++ core/src/lmdb/mdb.c
-@@ -150,7 +150,8 @@ typedef SSIZE_T	ssize_t;
+@@ -150,7 +150,11 @@ typedef SSIZE_T	ssize_t;
  
  #if defined(__APPLE__) || defined (BSD)
  # if !(defined(MDB_USE_POSIX_MUTEX) || defined(MDB_USE_POSIX_SEM))
 -# define MDB_USE_SYSV_SEM	1
-+/* # define MDB_USE_SYSV_SEM	1 */
-+# define MDB_USE_POSIX_SEM	1
++#   if defined(__NetBSD__)
++#     define MDB_USE_POSIX_SEM	1
++#   else
++#     define MDB_USE_SYSV_SEM	1
++#   endif
  # endif
  # define MDB_FDATASYNC		fsync
  #elif defined(ANDROID)
-@@ -4829,7 +4830,7 @@ mdb_env_setup_locks(MDB_env *env, char *
- #endif
- #ifdef MDB_USE_SYSV_SEM
- 	int semid;
--	union semun semu;
-+	// XXXkd: union semun semu;
- #endif
- 	int rc;
- 	off_t size, rsize;
