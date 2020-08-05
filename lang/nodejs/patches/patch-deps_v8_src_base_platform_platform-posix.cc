19868099171daac2b5efd1c3b9c8664694301f24
maya <maya@pkgsrc.org>
2020-08-05 21:49:18 +0000

    nodejs: workaround issue for netbsd/aarch64 in PR port-arm/55533
    NetBSD mmap might fail depending on the choice of hint addr given, so don't
    give a hint at all.
    
    bump PKGREVISION.

--- lang/nodejs/patches/patch-deps_v8_src_base_platform_platform-posix.cc
+++ lang/nodejs/patches/patch-deps_v8_src_base_platform_platform-posix.cc
@@ -1,11 +1,25 @@
-$NetBSD: patch-deps_v8_src_base_platform_platform-posix.cc,v 1.5 2018/05/03 21:19:16 fhajny Exp $
+$NetBSD: patch-deps_v8_src_base_platform_platform-posix.cc,v 1.6 2020/08/05 21:49:18 maya Exp $
 
 Use sysconf(_SC_THREAD_STACK_MIN) instead of PTHREAD_STACK_MIN.
 Cast explicitly.
 
---- deps/v8/src/base/platform/platform-posix.cc.orig	2018-04-24 14:41:24.000000000 +0000
+Avoid using a random hint, some low numbers cause spurious ENOMEM on netbsd
+(PR port-arm/55533)
+
+--- deps/v8/src/base/platform/platform-posix.cc.orig	2020-07-20 22:18:45.000000000 +0000
 +++ deps/v8/src/base/platform/platform-posix.cc
-@@ -480,6 +480,8 @@ int OS::GetCurrentThreadId() {
+@@ -323,6 +323,10 @@ void* OS::GetRandomMmapAddr() {
+ #endif
+ #endif
+ #endif
++
++#ifdef __NetBSD__ && V8_TARGET_ARCH_ARM64
++  raw_addr = 0;
++#endif
+   return reinterpret_cast<void*>(raw_addr);
+ }
+ 
+@@ -564,6 +568,8 @@ int OS::GetCurrentThreadId() {
    return static_cast<int>(syscall(__NR_gettid));
  #elif V8_OS_ANDROID
    return static_cast<int>(gettid());
@@ -14,7 +28,7 @@ Cast explicitly.
  #elif V8_OS_AIX
    return static_cast<int>(thread_self());
  #elif V8_OS_FUCHSIA
-@@ -670,8 +672,13 @@ Thread::Thread(const Options& options)
+@@ -756,8 +762,13 @@ Thread::Thread(const Options& options)
      : data_(new PlatformData),
        stack_size_(options.stack_size()),
        start_semaphore_(nullptr) {
@@ -28,7 +42,7 @@ Cast explicitly.
    }
    set_name(options.name());
  }
-@@ -687,7 +694,7 @@ static void SetThreadName(const char* na
+@@ -773,7 +784,7 @@ static void SetThreadName(const char* na
    pthread_set_name_np(pthread_self(), name);
  #elif V8_OS_NETBSD
    STATIC_ASSERT(Thread::kMaxThreadNameLength <= PTHREAD_MAX_NAMELEN_NP);
