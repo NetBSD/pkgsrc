b55a6ebd0895e2fc7e8b1e8397b6a5ee0cf65609
adam <adam@pkgsrc.org>
2020-08-06 16:28:03 +0000

    dnsdist: updated to 1.5.0
    1.5.0
    
    Improvements
    Use explicit flag for the specific version of c++ we are targeting.
    Prevent a copy of a pool’s backends when selecting a server.
    
    Bug Fixes
    Fix compilation with h2o_socket_get_ssl_server_name().
    Prevent a possible overflow via large Proxy Protocol values. (Valentei Sergey)
    Avoid name clashes on Solaris derived systems.
    Resize hostname to final size in getCarbonHostname(). (Aki Tuomi)
    Fix compilation on OpenBSD/amd64.
    Handle calling PacketCache methods on a nil object.
    
    
    1.4.0
    
    Improvements
    Fix the default value of setMaxUDPOutstanding in the console’s help (phonedph1)
    Add bindings for the noerrors and drops members of StatNode
    Fix -Wshadow warnings (Aki Tuomi)
    Fix typo: settting to setting (Chris Hofstaedtler)
    
    Bug Fixes
    Lowercase the name blocked by a SMT dynamic block
    
    misc
    Prefer the cipher suite from the server by default (DoH, DoT)

--- net/dnsdist/patches/patch-dnsdist-console.cc
+++ net/dnsdist/patches/patch-dnsdist-console.cc
@@ -1,8 +1,8 @@
-$NetBSD: patch-dnsdist-console.cc,v 1.4 2020/04/23 07:43:30 jperkin Exp $
+$NetBSD: patch-dnsdist-console.cc,v 1.5 2020/08/06 16:28:03 adam Exp $
 
 Always use the pkgsrc editline layout.
 
---- dnsdist-console.cc.orig	2018-11-08 10:15:21.000000000 +0000
+--- dnsdist-console.cc.orig	2020-07-28 07:39:28.000000000 +0000
 +++ dnsdist-console.cc
 @@ -24,7 +24,7 @@
  #include <pwd.h>
@@ -10,6 +10,6 @@ Always use the pkgsrc editline layout.
  
 -#if defined (__OpenBSD__) || defined(__NetBSD__)
 +#if 1
+ // If this is not undeffed, __attribute__ wil be redefined by /usr/include/readline/rlstdc.h
+ #undef __STRICT_ANSI__
  #include <readline/readline.h>
- #include <readline/history.h>
- #else
