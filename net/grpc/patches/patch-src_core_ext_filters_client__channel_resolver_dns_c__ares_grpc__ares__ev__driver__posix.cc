0cfab54a48132b02dfdbf324536616c2193ddc59
adam <adam@pkgsrc.org>
2020-08-07 06:45:57 +0000

    grpc: updated to 1.31.0
    Release v1.31.0
    
    Core
    
    The following new xDS functionality is added in this release:
    
    Requests matching based on path (prefix, full path and safe regex) and headers.
    Requests routing to multiple clusters based on weights.
    The features supported in a given release are documented here.
    Other changes:
    
    Remove MAX_EPOLL_EVENTS_HANDLED_EACH_POLL_CALL to ensure timely processing of events.
    Include the target name in top-level DNS error messages.
    Remove xds-experimental URI scheme.
    fix memory leak of grpc_resource_user_quota.
    Store ref to the ExternalConnectivityWatcher in external_watchers_ map.
    Update grpclb configuration with field "service_name".
    Fix possible deadlock in RemoveExternalConnectivityWatcher.
    Enable TLS 1.3 in the C-core and all wrapped languages.
    Add message-size check before message decompression with ordering change.
    Fix race condition caused by simultaneous updates on SSL server handshaker.
    Add missing reset for ping clocks to avoid mistakenly sending GOAWAY frames due to 'too_many_pings'.
    
    C++
    
    Simplify makefile: Get rid of "install" rules with pure make, recommend cmake and bazel instead.
    Replaced grpc::string with std::string.
    Fix wrong version in gRPCConfigVersion.cmake and grpc++*.pc.
    
    Python
    
    [Aio] Support tuple and aio.Metadata interaction.
    [Aio] Allows poller to bind to ephemeral loops in multiple threads.
    [Aio] Hide init_grpc_aio and guard async API outside of AsyncIO context.
    [Aio] Implement methods to access auth context and peer info.
    Add protobuf as an "extras" dependency to grpcio package.
    [Aio] Use Metadata type.
    Avoid attribute error in del of _ChannelCallState.
    Default wait_for_ready to True in simple stubs.
    Propagate contextvars to auxiliary threads.
    Simplify channel credentials in simple stubs.

--- net/grpc/patches/patch-src_core_ext_filters_client__channel_resolver_dns_c__ares_grpc__ares__ev__driver__posix.cc
+++ net/grpc/patches/patch-src_core_ext_filters_client__channel_resolver_dns_c__ares_grpc__ares__ev__driver__posix.cc
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_core_ext_filters_client__channel_resolver_dns_c__ares_grpc__ares__ev__driver__posix.cc,v 1.2 2019/03/22 08:25:51 adam Exp $
+$NetBSD: patch-src_core_ext_filters_client__channel_resolver_dns_c__ares_grpc__ares__ev__driver__posix.cc,v 1.3 2020/08/07 06:45:57 adam Exp $
 
 Add SunOS support.
 
---- src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_ev_driver_posix.cc.orig	2019-03-08 20:07:26.000000000 +0000
+--- src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_ev_driver_posix.cc.orig	2020-08-04 20:02:23.000000000 +0000
 +++ src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_ev_driver_posix.cc
 @@ -23,6 +23,11 @@
  #include <ares.h>
@@ -14,5 +14,5 @@ Add SunOS support.
 +#include <stropts.h>
 +#endif
  
- #include "src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_ev_driver.h"
+ #include "absl/strings/str_cat.h"
  
