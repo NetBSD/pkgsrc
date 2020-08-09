c8f40312c9b05319946f465007b43cdbe6142ef7
joerg <joerg@pkgsrc.org>
2020-08-09 00:53:12 +0000

    Fix ICU option. Disable benchmarking as it requires git and external downloads. Require gtk-doc for documentation to match PLIST.

--- fonts/harfbuzz/options.mk
+++ fonts/harfbuzz/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.5 2020/08/08 19:11:13 nia Exp $
+# $NetBSD: options.mk,v 1.6 2020/08/09 00:53:12 joerg Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.harfbuzz
 PKG_SUPPORTED_OPTIONS+=	icu
@@ -11,6 +11,7 @@ PLIST_VARS+=		icu
 .if !empty(PKG_OPTIONS:Micu)
 .include "../../textproc/icu/buildlink3.mk"
 MESON_ARGS+=	-Dicu=enabled
+PLIST.icu=		yes
 .else
 MESON_ARGS+=	-Dicu=disabled
 .endif
