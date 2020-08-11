d5e7aaeb18a7c0db0b8e436fdf5250d1a421eef5
ryoon <ryoon@pkgsrc.org>
2020-08-11 16:07:38 +0000

    libreoffice: Update to 7.0.0.3
    * Support Python 3.8.
    
    Changelog:
    Summary of Other New Features
    
    GENERAL
    
    - New icon theme, the default on macOS: Sukapura
    - New shapes galleries: arrows, diagrams, icons and more...
    - Glow and soft edge effects for objects
    
    WRITER
    
    - Navigator is easier to use, with more context menus
    - Semi-transparent text is now supported
    - Bookmarks can now be displayed in-line in text
    - Padded numbering in lists, for consistency
    - Better handling of quotation marks and apostrophes
    
    CALC
    
    - New functions for non-volatile random number generation
    - Keyboard shortcut added for autosum
    
    IMPRESS & DRAW
    
    - Semi-transparent text is supported here too
    - Subscripts now return to the default of 8%
    - PDFs larger than 500 cm can now be generated

--- misc/libreoffice/patches/patch-sc_source_core_tool_math.cxx
+++ misc/libreoffice/patches/patch-sc_source_core_tool_math.cxx
@@ -1,10 +1,10 @@
-$NetBSD: patch-sc_source_core_tool_math.cxx,v 1.1 2019/08/10 11:41:29 ryoon Exp $
+$NetBSD: patch-sc_source_core_tool_math.cxx,v 1.2 2020/08/11 16:07:39 ryoon Exp $
 
 NetBSD has no math_errhandling support yet.
 
---- sc/source/core/tool/math.cxx.orig	2019-08-06 17:26:35.000000000 +0000
+--- sc/source/core/tool/math.cxx.orig	2020-07-29 19:29:17.000000000 +0000
 +++ sc/source/core/tool/math.cxx
-@@ -18,7 +18,9 @@ static double err_pow( const double& fVa
+@@ -20,7 +20,9 @@ static double err_pow( const double& fVa
  {
      // pow() is expected to set domain error or pole error or range error (or
      // flag them via exceptions) or return NaN or Inf.
@@ -14,7 +14,7 @@ NetBSD has no math_errhandling support yet.
      std::feclearexcept(FE_ALL_EXCEPT);
      errno = 0;
      return pow( fVal1, fVal2);
-@@ -49,10 +51,15 @@ double power( const double& fVal1, const
+@@ -52,10 +54,15 @@ double power( const double& fVal1, const
          fPow = err_pow( fVal1, fVal2);
      }
      // The pow() call must had been the most recent call to check errno or exception.
@@ -25,7 +25,7 @@ NetBSD has no math_errhandling support yet.
      if ((((math_errhandling & MATH_ERRNO) != 0) && (errno == EDOM || errno == ERANGE))
              || (((math_errhandling & MATH_ERREXCEPT) != 0)
                  && std::fetestexcept( FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW | FE_UNDERFLOW))
-             || !rtl::math::isFinite(fPow))
+             || !std::isfinite(fPow))
 +#endif
      {
          fPow = CreateDoubleError( FormulaError::IllegalFPOperation);
