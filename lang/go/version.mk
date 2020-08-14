4677ea248d0ef687821d49ef8f123889a53f6c3d
bsiegert <bsiegert@pkgsrc.org>
2020-08-14 18:28:29 +0000

    Update go113 to 1.13.15.
    go1.13.15 (released 2020/08/06) includes security fixes to the encoding/binary
    package. See the Go 1.13.15 milestone on our issue tracker for details.

--- lang/go/version.mk
+++ lang/go/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.94 2020/07/17 17:20:05 bsiegert Exp $
+# $NetBSD: version.mk,v 1.95 2020/08/14 18:28:29 bsiegert Exp $
 
 #
 # If bsd.prefs.mk is included before go-package.mk in a package, then this
@@ -7,7 +7,7 @@
 .include "go-vars.mk"
 
 GO114_VERSION=	1.14.6
-GO113_VERSION=	1.13.14
+GO113_VERSION=	1.13.15
 GO110_VERSION=	1.10.8
 GO19_VERSION=	1.9.7
 GO14_VERSION=	1.4.3
