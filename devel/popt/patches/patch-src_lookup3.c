1fee5780786a994a720fe53edabc886419889082
tnn <tnn@pkgsrc.org>
2020-08-15 01:02:42 +0000

    popt: update to 1.18
    Replace HOMEPAGE and MASTER_SITE, the old rpm5.org site seems dead & broken
    
    Changes from popt 1.16
      fix an ugly and ancient security issue with popt failing to
        drop privileges on alias exec from a SUID/SGID program
      perform rudimentary sanity checks when reading in popt config files
      collect accumulated misc fixes (memleaks etc) from distros
      convert translations to utf-8 encoding
      convert old postscript documentation to pdf
      dust off ten years worth of autotools sediment
      reorganize and clean up the source tree for clarity
      remove the obnoxious splint annotations from the sources

--- devel/popt/patches/patch-src_lookup3.c
+++ devel/popt/patches/patch-src_lookup3.c
@@ -1,9 +1,9 @@
-$NetBSD: patch-lookup3.c,v 1.1 2011/04/01 21:28:32 tez Exp $
+$NetBSD: patch-src_lookup3.c,v 1.1 2020/08/15 01:02:42 tnn Exp $
 
 Don't include stdint.h if it does not exist.  Fixes PR#44770
 
---- lookup3.c.orig	2011-04-01 21:06:28.047391300 +0000
-+++ lookup3.c
+--- src/lookup3.c.orig	2011-04-01 21:06:28.047391300 +0000
++++ src/lookup3.c
 @@ -34,7 +34,9 @@
  */
  /* -------------------------------------------------------------------- */
--- devel/popt/patches/patch-src_poptint.h
+++ devel/popt/patches/patch-src_poptint.h
@@ -1,9 +1,9 @@
-$NetBSD: patch-poptint.h,v 1.2 2019/05/05 23:16:06 rjs Exp $
+$NetBSD: patch-src_poptint.h,v 1.1 2020/08/15 01:02:42 tnn Exp $
 
 Don't include stdint.h if it does not exist.  Fixes PR#44770
 
---- poptint.h.orig	2010-01-19 00:39:10.000000000 +0000
-+++ poptint.h
+--- src/poptint.h.orig	2010-01-19 00:39:10.000000000 +0000
++++ src/poptint.h
 @@ -9,7 +9,13 @@
  #ifndef H_POPTINT
  #define H_POPTINT
