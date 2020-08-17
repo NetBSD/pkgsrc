e4addcc75bf9b0d8510d802a9f337664d5556e55
leot <leot@pkgsrc.org>
2020-08-17 15:23:28 +0000

    fontconfig: remove libuuid bl3 inclusion in buildlink3.mk
    libuuid is marked as a private dependency and it is an implementation
    detail.
    
    On some platforms (e.g. NetBSD) by including libuuid bl3 fonts/fontconfig was
    unconditionally installed due that.
    
    Problem originally analyzed by <wiz> in:
    
     <https://mail-index.NetBSD.org/tech-pkg/2019/06/19/msg021438.html>
    
    And change proposed on tech-pkg@:
    
     <https://mail-index.NetBSD.org/tech-pkg/2020/07/13/msg023550.html>
    
    Thanks to <wiz> also for reviews, suggestions and discussions!

--- fonts/fontconfig/buildlink3.mk
+++ fonts/fontconfig/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.37 2019/09/09 12:22:03 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.38 2020/08/17 15:23:28 leot Exp $
 
 BUILDLINK_TREE+=	fontconfig
 
@@ -12,7 +12,6 @@ BUILDLINK_PKGSRCDIR.fontconfig?=	../../fonts/fontconfig
 .include "../../archivers/bzip2/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
-.include "../../devel/libuuid/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
 .endif # FONTCONFIG_BUILDLINK3_MK
