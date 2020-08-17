485bbd5564e79ea4d45db70dc73ba084039bea52
tnn <tnn@pkgsrc.org>
2020-08-17 05:25:48 +0000

    libsndfile: needs to buildlink libopus due to visible in .pc file
    XXX recursive revbump should be performed once the dust settles

--- audio/libsndfile/buildlink3.mk
+++ audio/libsndfile/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/03/20 19:23:57 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2020/08/17 05:25:48 tnn Exp $
 
 BUILDLINK_TREE+=	libsndfile
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.libsndfile+=	libsndfile>=1.0.0
 BUILDLINK_ABI_DEPENDS.libsndfile+=	libsndfile>=1.0.18
 BUILDLINK_PKGSRCDIR.libsndfile?=	../../audio/libsndfile
 
+.include "../../audio/libopus/buildlink3.mk"
 .include "../../audio/libvorbis/buildlink3.mk"
 .include "../../audio/flac/buildlink3.mk"
 .include "../../multimedia/libogg/buildlink3.mk"
