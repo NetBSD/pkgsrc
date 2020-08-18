8d7fa440df41c7bef75b49056c5a86a3476f472c
tnn <tnn@pkgsrc.org>
2020-08-18 09:27:38 +0000

    also need an implementation of uuid_compare() for the test suite

--- fonts/fontconfig/files/uuid.h
+++ fonts/fontconfig/files/uuid.h
@@ -228,4 +228,21 @@ static void uuid_generate_random(uuid_t out)
 	__uuid_generate_random(out, &num);
 }
 
+#define UUCMP(u1,u2) if (u1 != u2) return((u1 < u2) ? -1 : 1);
+
+static int uuid_compare(const uuid_t uu1, const uuid_t uu2)
+{
+	struct uuid	uuid1, uuid2;
+
+	uuid_unpack(uu1, &uuid1);
+	uuid_unpack(uu2, &uuid2);
+
+	UUCMP(uuid1.time_low, uuid2.time_low);
+	UUCMP(uuid1.time_mid, uuid2.time_mid);
+	UUCMP(uuid1.time_hi_and_version, uuid2.time_hi_and_version);
+	UUCMP(uuid1.clock_seq, uuid2.clock_seq);
+	return memcmp(uuid1.node, uuid2.node, 6);
+}
+
+
 #endif
