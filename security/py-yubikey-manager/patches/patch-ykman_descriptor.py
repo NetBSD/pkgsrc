aa08e24c4bf10bad58e8efefc1ce8a8dc0269a7a
riastradh <riastradh@pkgsrc.org>
2020-08-18 13:37:50 +0000

    security/py-yubikey-manager: Avoid crash on NetBSD for non-CCID keys.

--- security/py-yubikey-manager/patches/patch-ykman_descriptor.py
+++ security/py-yubikey-manager/patches/patch-ykman_descriptor.py
@@ -1,10 +1,29 @@
-$NetBSD: patch-ykman_descriptor.py,v 1.1 2020/08/17 07:02:25 riastradh Exp $
+$NetBSD: patch-ykman_descriptor.py,v 1.2 2020/08/18 13:37:50 riastradh Exp $
 
 Enumerate uhidevs in case they are not available as ugen.
 
---- ykman/descriptor.py.orig	2020-08-17 06:36:13.378751569 +0000
+--- ykman/descriptor.py.orig	2020-01-29 07:32:03.000000000 +0000
 +++ ykman/descriptor.py
-@@ -145,6 +145,13 @@ def _gen_descriptors():
+@@ -75,6 +75,8 @@ class Descriptor(object):
+ 
+     @property
+     def name(self):
++        if self.version is None:
++            return self.key_type.value
+         if self.key_type == YUBIKEY.SKY and self.version < (5, 0, 0):
+             return 'FIDO U2F Security Key'
+         elif self.key_type == YUBIKEY.YK4 and self.version >= (5, 0, 0):
+@@ -132,7 +134,8 @@ class Descriptor(object):
+     @classmethod
+     def from_driver(cls, driver):
+         fp = (driver.key_type, driver.mode)
+-        return cls(driver.key_type, driver.mode, None, fp)
++        version = driver.read_version()
++        return cls(driver.key_type, driver.mode, version, fp)
+ 
+ 
+ def _gen_descriptors():
+@@ -145,6 +148,13 @@ def _gen_descriptors():
              desc = Descriptor.from_usb(dev, backend)
              if desc:
                  yield desc
