f4c5b670381b67f9d2083cf3c93cf392719e14b7
riastradh <riastradh@pkgsrc.org>
2020-08-18 20:24:57 +0000

    Update py-angr to 8.20.7.27, along with associated packages.
    Version is consolidated into devel/py-angr/version.mk now so the next
    person to come along will know what else needs to be updated.
    
    devel/py-pyvex distfiles are still kinda kludgey, and devel/py-angr's
    tests don't run without binary samples that I got bored trying to
    incorporate.

---
+++ devel/py-angr/version.mk
@@ -0,0 +1,9 @@
+#	$NetBSD: version.mk,v 1.1 2020/08/18 20:24:57 riastradh Exp $
+
+# used by devel/py-ailment/Makefile
+# used by devel/py-archinfo/Makefile
+# used by devel/py-cle/Makefile
+# used by devel/py-pyvex/Makefile
+# used by math/py-claripy/Makefile
+
+ANGR_VERSION=	8.20.7.27
