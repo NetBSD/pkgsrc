862383cab42881da9aef052fa550987754b712b3
bouyer <bouyer@pkgsrc.org>
2020-08-19 10:39:22 +0000

    Remove xenkernel and xentools packages older than 4.11. They're not maintained anymore upstream, and don't build on supported NetBSD releases.

--- sysutils/xentools46/version.mk
+++
@@ -1,6 +0,0 @@
-# $NetBSD: version.mk,v 1.3 2017/10/17 10:57:34 bouyer Exp $
-#
-# Version number is used by xentools46 and xenstoretools
-
-VERSION=	4.6.6
-VERSION_IPXE=	9a93db3f0947484e30e753bbd61a10b17336e20e
