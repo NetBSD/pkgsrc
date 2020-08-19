cc4567be1adf9176c33ec904a07ef8d5c2e0b5b9
adam <adam@pkgsrc.org>
2020-08-19 07:08:34 +0000

    python37 py37-html-docs: updated to 3.7.9
    Python 3.7.9 final
    
    Security
    bpo-41304: Fixes python3x._pth being ignored on Windows, caused by the fix for bpo-29778 (CVE-2020-15801).
    bpo-29778: Ensure python3.dll is loaded from correct locations when Python is embedded (CVE-2020-15523).
    bpo-41004: CVE-2020-14422: The __hash__() methods of ipaddress.IPv4Interface and ipaddress.IPv6Interface incorrectly generated constant hash values of 32 and 128 respectively. This resulted in always causing hash collisions. The fix uses hash() to generate hash values for the tuple of (address, mask length, network address).
    bpo-39603: Prevent http header injection by rejecting control characters in http.client.putrequest(…).
    Core and Builtins
    bpo-33786: Fix asynchronous generators to handle GeneratorExit in athrow() correctly
    
    Library
    bpo-41288: Unpickling invalid NEWOBJ_EX opcode with the C implementation raises now UnpicklingError instead of crashing.
    bpo-39017: Avoid infinite loop when reading specially crafted TAR files using the tarfile module (CVE-2019-20907).
    bpo-41235: Fix the error handling in ssl.SSLContext.load_dh_params().
    
    macOS
    bpo-41100: Additional fixes for testing on macOS 11 Big Sur Intel. Note: macOS 11 is not yet released, this release of Python is not fully supported on 11.0, and not all tests pass.

--- lang/python37/dist.mk
+++ lang/python37/dist.mk
@@ -1,6 +1,6 @@
-# $NetBSD: dist.mk,v 1.9 2020/06/30 05:56:02 adam Exp $
+# $NetBSD: dist.mk,v 1.10 2020/08/19 07:08:34 adam Exp $
 
-PY_DISTVERSION=	3.7.8
+PY_DISTVERSION=	3.7.9
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.xz
 DISTINFO_FILE=	${.CURDIR}/../../lang/python37/distinfo
