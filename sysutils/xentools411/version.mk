92fca3ca2c43aefb97b3d02d3e334600631f0ba2
bouyer <bouyer@pkgsrc.org>
2020-08-20 11:30:25 +0000

    Add back sysutils/xenstoretools, based on xentools411

---
+++ sysutils/xentools411/version.mk
@@ -0,0 +1,6 @@
+# $NetBSD: version.mk,v 1.1 2020/08/20 11:30:25 bouyer Exp $
+#
+# Version number is used by xentools411 and xenstoretools
+
+VERSION=	4.11.3
+VERSION_IPXE=	356f6c1b64d7a97746d1816cef8ca22bdd8d0b5d
