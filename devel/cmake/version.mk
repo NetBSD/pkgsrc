fe348876afeb8dd5f77159846ec8a1f5fdc1e28b
adam <adam@pkgsrc.org>
2020-08-21 07:26:51 +0000

    cmake: updated to 3.18.2
    Changes made since CMake 3.18.1:
    * AutoGen: Fix over-specified direct dependencies of custom command
    * AutoGen: Add test to check for correct AutoMoc dependencies
    * FindMPI: Fix regression in pthread guard
    * FPHSA: detect inclusion between find modules
    * Tests: Isolate RunCMake.FindPkgConfig from caller environment
    * FindRuby: Restore compatibility variable RUBY_INCLUDE_PATH
    * Xcode: Explicitly turn off signing in try_compile projects
    * PCH: Avoid Apple-specific architecture flags on other platforms
    * gitlab-ci: add 'cmake' tag to all jobs
    * Tests: Fix RunCMake.try_compile test for C standards with MSVC 19.27
    * gitlab-ci: Update Windows builds to MSVC 19.27
    * ExternalProject: Stop patch target from always appearing out-of-date
    * ExternalProject: Stop configure target from always appearing out-of-date
    * PCH: Avoid unnecessary 30s delay on MSBuild Generator with REUSE_FROM
    * FindRuby: Add documentation for deprecated variables
    * Autogen: Turn off moc path prefix generation by default
    * Help: Add 3.18.2 release note about AUTOMOC_PATH_PREFIX default change
    * Ninja: Restore shorter path to response files
    * GoogleTest: Restore support for list arguments in TEST_EXECUTOR
    * FindPython: ensure user's policies are respected
    * FindPython: manage SOABI for all Python versions
    * FindPython: enhance robustness of version extraction from library name
    * MSVC: Record support for C11 and c_restrict
    * CMakeFindBinUtils: Always consider plain binutils when not cross-compiling
    * FindHDF5: fix compiler detection when HL is disabled
    * llvm-rc: Fix quoting of path to cmake in CMAKE_RC_COMPILE_OBJECT

--- devel/cmake/version.mk
+++ devel/cmake/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.1 2020/07/31 09:02:31 wiz Exp $
+# $NetBSD: version.mk,v 1.2 2020/08/21 07:26:51 adam Exp $
 # used by devel/cmake/Makefile.common
 # used by devel/cmake-fedora/Makefile
 
-CMAKE_VERSION=	3.18.1
+CMAKE_VERSION=	3.18.2
 CMAKE_API=	${CMAKE_VERSION:R}
