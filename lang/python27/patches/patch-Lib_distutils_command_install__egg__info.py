b0d4a57b9d896e801fb0d2dff828bd54408c54ba
schmonz <schmonz@pkgsrc.org>
2020-08-22 13:01:42 +0000

    We only know the build version better than python does if we're building 'Python". For all other distributions built with distutils, we sure don't (and, mind you, it's very unlikely to be the exact same version as Python itself). This should fix a whole bunch of py27-* packages broken with the previous commit. Thanks wiz@ for the heads up.

--- lang/python27/patches/patch-Lib_distutils_command_install__egg__info.py
+++ lang/python27/patches/patch-Lib_distutils_command_install__egg__info.py
@@ -1,4 +1,4 @@
-$NetBSD: patch-Lib_distutils_command_install__egg__info.py,v 1.1 2020/08/21 19:39:28 schmonz Exp $
+$NetBSD: patch-Lib_distutils_command_install__egg__info.py,v 1.2 2020/08/22 13:01:42 schmonz Exp $
 
 On systems where both of the conditions
 
@@ -20,14 +20,18 @@ builds on all platforms. This patch is needed because of that patch.
 
 --- Lib/distutils/command/install_egg_info.py.orig	2020-04-19 21:13:39.000000000 +0000
 +++ Lib/distutils/command/install_egg_info.py
-@@ -21,9 +21,10 @@ class install_egg_info(Command):
+@@ -21,9 +21,13 @@ class install_egg_info(Command):
  
      def finalize_options(self):
          self.set_undefined_options('install_lib',('install_dir','install_dir'))
-+        distribution_version = "@PKGVERSION_NOREV@"
++        distribution_name = self.distribution.get_name()
++        distribution_version = self.distribution.get_version()
++        if distribution_name == "Python":
++            distribution_version = "@PKGVERSION_NOREV@"
          basename = "%s-%s-py%s.egg-info" % (
-             to_filename(safe_name(self.distribution.get_name())),
+-            to_filename(safe_name(self.distribution.get_name())),
 -            to_filename(safe_version(self.distribution.get_version())),
++            to_filename(safe_name(distribution_name)),
 +            to_filename(safe_version(distribution_version)),
              sys.version[:3]
          )
