26bfcf1233464448eaacf35aa70516769ca78ffb
joerg <joerg@pkgsrc.org>
2020-08-22 00:28:29 +0000

    Progress for the SmartOS build.

--- x11/qt5-qtdeclarative/patches/patch-src_qml_jit_qv4assemblercommon__p.h
+++ x11/qt5-qtdeclarative/patches/patch-src_qml_jit_qv4assemblercommon__p.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-src_qml_jit_qv4assemblercommon__p.h,v 1.1 2020/01/21 21:39:09 adam Exp $
+$NetBSD: patch-src_qml_jit_qv4assemblercommon__p.h,v 1.2 2020/08/22 00:28:30 joerg Exp $
 
-Add NetBSD support.
+Add NetBSD & Solaris support.
 
 --- src/qml/jit/qv4assemblercommon_p.h.orig	2020-01-21 16:34:07.735772770 +0000
 +++ src/qml/jit/qv4assemblercommon_p.h
@@ -9,7 +9,7 @@ Add NetBSD support.
  
  #if defined(Q_PROCESSOR_X86_64) || defined(ENABLE_ALL_ASSEMBLERS_FOR_REFACTORING_PURPOSES)
 -#if defined(Q_OS_LINUX) || defined(Q_OS_QNX) || defined(Q_OS_FREEBSD) || defined(Q_OS_DARWIN)
-+#if defined(Q_OS_LINUX) || defined(Q_OS_QNX) || defined(Q_OS_FREEBSD) || defined(Q_OS_DARWIN) || defined(Q_OS_NETBSD)
++#if defined(Q_OS_LINUX) || defined(Q_OS_QNX) || defined(Q_OS_FREEBSD) || defined(Q_OS_DARWIN) || defined(Q_OS_NETBSD) || defined(Q_OS_SOLARIS)
  
  class PlatformAssembler_X86_64_SysV : public JSC::MacroAssembler<JSC::MacroAssemblerX86_64>
  {
