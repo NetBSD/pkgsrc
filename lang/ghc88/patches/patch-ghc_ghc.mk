3ea71a0f5125102d9d623560c2d3f755270ae388
pho <pho@pkgsrc.org>
2020-08-24 17:23:11 +0000

    Update to GHC 8.8.4
    The full change log is too long to paste here.
    
    Changes from 8.8.1 to 8.8.2:
    https://downloads.haskell.org/~ghc/8.8.2/docs/html/users_guide/8.8.2-notes.html
    
    Changes from 8.8.2 to 8.8.3:
    https://downloads.haskell.org/~ghc/8.8.3/docs/html/users_guide/8.8.3-notes.html
    
    Changes from 8.8.3 to 8.8.4:
    https://downloads.haskell.org/~ghc/8.8.4/docs/html/users_guide/8.8.4-notes.html

--- lang/ghc88/patches/patch-ghc_ghc.mk
+++ lang/ghc88/patches/patch-ghc_ghc.mk
@@ -1,13 +1,13 @@
-$NetBSD: patch-ghc_ghc.mk,v 1.2 2020/01/18 11:40:04 pho Exp $
+$NetBSD: patch-ghc_ghc.mk,v 1.3 2020/08/24 17:23:12 pho Exp $
 
 Fix building bootkits: the ghc-bin package is threaded by default, but
 the build system fails to opt out from it when threaded RTS is
 disabled:
 https://gitlab.haskell.org/ghc/ghc/issues/17692
 
---- ghc/ghc.mk.orig	2019-08-25 12:03:36.000000000 +0000
+--- ghc/ghc.mk.orig	2020-08-24 10:13:02.192874900 +0000
 +++ ghc/ghc.mk
-@@ -61,7 +61,13 @@ ifeq "$(GhcThreaded)" "YES"
+@@ -61,6 +61,10 @@ ifeq "$(GhcThreaded)" "YES"
  # Use threaded RTS with GHCi, so threads don't get blocked at the prompt.
  ghc_stage2_MORE_HC_OPTS += -threaded
  ghc_stage3_MORE_HC_OPTS += -threaded
@@ -16,8 +16,5 @@ https://gitlab.haskell.org/ghc/ghc/issues/17692
 +ghc_stage2_CONFIGURE_OPTS += -f-threaded
 +ghc_stage3_CONFIGURE_OPTS += -f-threaded
  endif
-+# Stage-0 compiler isn't guaranteed to have a threaded RTS.
-+ghc_stage1_CONFIGURE_OPTS += -f-threaded
  
- ifeq "$(GhcProfiled)" "YES"
- ghc_stage2_PROGRAM_WAY = p
+ # If stage 0 supplies a threaded RTS, we can use it for stage 1.
