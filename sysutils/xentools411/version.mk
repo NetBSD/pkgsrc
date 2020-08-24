7857894265d6387794011f913c1d39634631f1c2
bouyer <bouyer@pkgsrc.org>
2020-08-24 10:33:27 +0000

    update to 4.11.4. Changes: mosly bug fixes and improvements; better support for newer AMD CPUs. full changelog at https://xenproject.org/downloads/xen-project-archives/xen-project-4-11-series/xen-project-4-11-4/

--- sysutils/xentools411/version.mk
+++ sysutils/xentools411/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD: version.mk,v 1.1 2020/08/20 11:30:25 bouyer Exp $
+# $NetBSD: version.mk,v 1.2 2020/08/24 10:33:27 bouyer Exp $
 #
 # Version number is used by xentools411 and xenstoretools
 
-VERSION=	4.11.3
+VERSION=	4.11.4
 VERSION_IPXE=	356f6c1b64d7a97746d1816cef8ca22bdd8d0b5d
