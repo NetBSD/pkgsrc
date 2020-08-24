0271d54a3f2cd3a437a59758511f43979ddc272f
wiz <wiz@pkgsrc.org>
2020-08-24 08:35:07 +0000

    libvpx: update to 1.9.0.
    2020-07-29 v1.9.0 "Quacking Duck"
      This release adds support for NV12, a separate library for rate control, as
      well as incremental improvements.
    
      - Upgrading:
        NV12 support is added to this release.
        A new interface is added for VP9 rate control. The new library libvp9rc.a
        must be linked by applications.
        Googletest is updated to v1.10.0.
        simple_encode.cc is compiled into a new library libsimple_encode.a with
        CONFIG_RATE_CTRL.
    
      - Enhancement:
        Various changes to improve VP9 SVC, rate control, quality and speed to real
        time encoding.
    
      - Bug fixes:
        Fix key frame update refresh simulcast flexible svc.
        Fix to disable_16x16part speed feature for real time encoding.
        Fix some signed integer overflows for VP9 rate control.
        Fix initialization of delta_q_uv.
        Fix condition in regulate_q for cyclic refresh.
        Various fixes to dynamic resizing for VP9 SVC.

--- multimedia/libvpx/patches/patch-libs.mk
+++ multimedia/libvpx/patches/patch-libs.mk
@@ -1,19 +1,19 @@
-$NetBSD: patch-libs.mk,v 1.3 2020/01/28 19:20:09 adam Exp $
+$NetBSD: patch-libs.mk,v 1.4 2020/08/24 08:35:07 wiz Exp $
 
 Do not install debug library.
 
---- libs.mk.orig	2019-12-09 23:09:20.000000000 +0000
+--- libs.mk.orig	2020-07-30 19:59:15.000000000 +0000
 +++ libs.mk
-@@ -162,7 +162,6 @@ INSTALL-LIBS-$(CONFIG_SHARED) += $(forea
+@@ -170,7 +170,6 @@ INSTALL-LIBS-$(CONFIG_SHARED) += $(forea
  endif
  else
  INSTALL-LIBS-$(CONFIG_STATIC) += $(LIBSUBDIR)/libvpx.a
 -INSTALL-LIBS-$(CONFIG_DEBUG_LIBS) += $(LIBSUBDIR)/libvpx_g.a
  endif
  
- CODEC_SRCS=$(call enabled,CODEC_SRCS)
-@@ -229,8 +228,8 @@ endif
- else
+ ifeq ($(CONFIG_VP9_ENCODER)$(CONFIG_RATE_CTRL),yesyes)
+@@ -278,8 +277,8 @@ endif # ifeq ($(CONFIG_MSVS),yes)
+ else # ifeq ($(CONFIG_EXTERNAL_BUILD),yes)
  LIBVPX_OBJS=$(call objs, $(filter-out $(ASM_INCLUDES), $(CODEC_SRCS)))
  OBJS-yes += $(LIBVPX_OBJS)
 -LIBS-$(if yes,$(CONFIG_STATIC)) += $(BUILD_PFX)libvpx.a $(BUILD_PFX)libvpx_g.a
@@ -22,4 +22,4 @@ Do not install debug library.
 +$(BUILD_PFX)libvpx.a: $(LIBVPX_OBJS)
  
  SO_VERSION_MAJOR := 6
- SO_VERSION_MINOR := 2
+ SO_VERSION_MINOR := 3
