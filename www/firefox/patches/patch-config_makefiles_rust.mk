e296a8deb2f23698c244776258e59fea7d268c59
ryoon <ryoon@pkgsrc.org>
2020-08-25 14:35:24 +0000

    firefox: Update to 80.0
    Changelog:
    New
        Firefox can now be set as the default system PDF viewer.
    
        The name reported by accessibility tools for items in multi-tiered
        tree controls no longer incorrectly includes information from
        items at deeper levels, providing users with the correct level
        of content when using a screen reader.
    
    Fixed
        Various security fixes.
    
        Several crashes while using a screen reader were fixed including
        a frequently encountered crash when using the JAWS screen
        reader.
    
        Firefox Developer Tools received significant fixes allowing
        screen reader users to benefit from some of the tools that were
        previously inaccessible.
    
        SVG title and desc elements (labels and descriptions) are now
        correctly exposed to assistive technology products such as
        screen readers.
    
    Changed
        For users with reduced motion settings, we've reduced a number
        of animations such as tab loading to reduce motion for users
        with migraines and epilepsy.
    
        The new add-ons blocklist has been enabled to improve performance
        and scalability.
    
    Enterprise
        A number of bug fixes and new policies have been implemented
        in the latest version of Firefox. You can see more details in
        the Firefox for Enterprise 80 Release Notes.
    
        Today's release is the final scheduled for Firefox 68 ESR
        (68.12) unless there is a critical security issue found prior
        to the release of Firefox ESR 78.3 on September 22, 2020. Users
        of Firefox 68 ESR will be automatically upgraded to the Firefox
        78 ESR series with the release of 78.3.
    
    Developer
        We've shipped an experimental sidebar panel in the inspector
        to Firefox Developer Edition that helps developers more quickly
        identify potential browser compatibility problems based on MDN
        data.
    
        In the Network Monitor request list, a turtle icon is shown
        for "slow" requests that exceed a threshold for the waiting
        time.
    
        Firefox now supports RTX and Transport-cc for improved call
        quality in poor network conditions and better bandwidth
        estimation. These features also provide better compatibility
        with many websites using WebRTC.
    
    Security fixes:
    #CVE-2020-15663: Downgrade attack on the Mozilla Maintenance Service could have resulted in escalation of privilege
    #CVE-2020-15664: Attacker-induced prompt for extension installation
    #CVE-2020-12401: Timing-attack on ECDSA signature generation
    #CVE-2020-6829: P-384 and P-521 vulnerable to an electro-magnetic side channel attack on signature generation
    #CVE-2020-12400: P-384 and P-521 vulnerable to a side channel attack on modular inversion
    #CVE-2020-15665: Address bar not reset when choosing to stay on a page after the beforeunload dialog is shown
    #CVE-2020-15666: MediaError message property leaks cross-origin response status
    #CVE-2020-15667: Heap overflow when processing an update file
    #CVE-2020-15668: Data Race when reading certificate information
    #CVE-2020-15670: Memory safety bugs fixed in Firefox 80 and Firefox ESR 78.2

--- www/firefox/patches/patch-config_makefiles_rust.mk
+++ www/firefox/patches/patch-config_makefiles_rust.mk
@@ -1,12 +1,6 @@
-$NetBSD: patch-config_makefiles_rust.mk,v 1.2 2020/08/07 08:29:42 maya Exp $
+$NetBSD: patch-config_makefiles_rust.mk,v 1.3 2020/08/25 14:35:24 ryoon Exp $
 
-NetBSD doesn't get along with parallel rust builds (it causes issues
-with ld.so) which are the default. Force -j1.
-
-Set -Cembed-bitcode=yes when using LTO to fix build with rust >= 1.45.
-Upstream bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1640982
-
---- config/makefiles/rust.mk.orig	2020-04-03 19:34:34.000000000 +0000
+--- config/makefiles/rust.mk.orig	2020-08-19 02:08:50.000000000 +0000
 +++ config/makefiles/rust.mk
 @@ -52,6 +52,9 @@ endif
  ifeq (1,$(MOZ_PARALLEL_BUILD))
@@ -18,15 +12,15 @@ Upstream bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1640982
  
  # These flags are passed via `cargo rustc` and only apply to the final rustc
  # invocation (i.e., only the top-level crate, not its dependencies).
-@@ -62,6 +65,11 @@ ifndef MOZ_DEBUG_RUST
- # gkrust_gtest.
- ifeq (,$(findstring gkrust_gtest,$(RUST_LIBRARY_FILE)))
- cargo_rustc_flags += -Clto
-+endif
+@@ -68,6 +71,11 @@ endif
+ ifeq (,$(filter 1.38.% 1.39.% 1.40.% 1.41.% 1.42.% 1.43.% 1.44.%,$(RUSTC_VERSION)))
+ RUSTFLAGS += -Cembed-bitcode=yes
+ endif
 +# Versions of rust >= 1.45 need -Cembed-bitcode=yes for all crates when
 +# using -Clto.
 +ifeq (,$(filter 1.38.% 1.39.% 1.40.% 1.41.% 1.42.% 1.43.% 1.44.%,$(RUSTC_VERSION)))
 +RUSTFLAGS += -Cembed-bitcode=yes
++endif
  endif
  endif
- endif
+ 
