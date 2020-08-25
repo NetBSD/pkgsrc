f89cf69de0bc2c1041bc5ed6e6e0c376e7c317d0
gdt <gdt@pkgsrc.org>
2020-08-25 13:45:52 +0000

    ham/chirp: Update to 20200807 (switching to daily builds)
    Upstream does not have release notes (or releases).
    
    chirp now tracks upstreams "chirp-daily" pseudoreleases, labeled by
    date.  The package remains "chirp"; upstream only calls it chirp-daily
    due to some GNU/Linux packaging issues.
    
    This is a huge update as 0.4.1 is from many years ago.

--- ham/chirp/patches/patch-setup.py
+++ ham/chirp/patches/patch-setup.py
@@ -1,16 +1,17 @@
-$NetBSD: patch-setup.py,v 1.1 2014/07/21 07:57:21 dbj Exp $
+$NetBSD: patch-setup.py,v 1.2 2020/08/25 13:45:53 gdt Exp $
+
 This software contains darwin specific tweaks which do not apply to pkgsrc
 
---- setup.py.orig	2012-12-24 08:00:57.000000000 +0000
-+++ setup.py
-@@ -141,9 +141,7 @@ def nuke_manifest(*files):
-         print >>f, fn
+--- setup.py.orig	2020-07-03 15:51:11.931206711 +1000
++++ setup.py	2020-07-03 15:51:24.114023817 +1000
+@@ -150,9 +150,7 @@
      f.close()
-                     
+ 
+ 
 -if sys.platform == "darwin":
 -    macos_build()
 -elif sys.platform == "win32":
 +if sys.platform == "win32":
      win32_build()
  else:
-     if os.path.exists("rpttool"):
+     default_build()
