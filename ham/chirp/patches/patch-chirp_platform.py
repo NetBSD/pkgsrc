2a25028c38378823718f8d031e72ba61f381c0e5
gdt <gdt@pkgsrc.org>
2020-08-25 13:45:52 +0000

    ham/chirp: Update to 20200807 (switching to daily builds)
    Upstream does not have release notes (or releases).
    
    chirp now tracks upstreams "chirp-daily" pseudoreleases, labeled by
    date.  The package remains "chirp"; upstream only calls it chirp-daily
    due to some GNU/Linux packaging issues.
    
    This is a huge update as 0.4.1 is from many years ago.

--- ham/chirp/patches/patch-chirp_platform.py
+++ ham/chirp/patches/patch-chirp_platform.py
@@ -1,18 +1,19 @@
-$NetBSD: patch-chirp_platform.py,v 1.1 2014/07/21 07:57:21 dbj Exp $
+$NetBSD: patch-chirp_platform.py,v 1.2 2020/08/25 13:45:53 gdt Exp $
+
 This software contains darwin specific tweaks which do not apply to pkgsrc
 
---- chirp/platform.py.orig	2014-04-29 03:55:43.000000000 +0000
-+++ chirp/platform.py
-@@ -249,16 +249,6 @@ class UnixPlatform(Platform):
+--- chirp/platform.py.orig	2017-10-12 18:01:04.000000000 +1100
++++ chirp/platform.py	2020-07-03 15:33:43.225703607 +1000
+@@ -283,16 +283,6 @@
  
          Platform.__init__(self, basepath)
  
--	# This is a hack that needs to be properly fixed by importing the
--	# latest changes to this module from d-rats.  In the interest of
--	# time, however, I'll throw it here
+-        # This is a hack that needs to be properly fixed by importing the
+-        # latest changes to this module from d-rats.  In the interest of
+-        # time, however, I'll throw it here
 -        if sys.platform == "darwin":
--            if not os.environ.has_key("DISPLAY"):
--                print "Forcing DISPLAY for MacOS"
+-            if "DISPLAY" not in os.environ:
+-                LOG.info("Forcing DISPLAY for MacOS")
 -                os.environ["DISPLAY"] = ":0"
 -
 -            os.environ["PANGO_RC_FILE"] = "../Resources/etc/pango/pangorc"
