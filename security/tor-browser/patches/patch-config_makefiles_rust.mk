8994621700adfa30af8c094890fe0afc05d2bc80
wiz <wiz@pkgsrc.org>
2020-08-26 20:55:19 +0000

    tor-browser: update to 9.5.4.
    This release updates Firefox to 68.12.0esr.
    
    Also, this release features important security updates to Firefox.

--- security/tor-browser/patches/patch-config_makefiles_rust.mk
+++ security/tor-browser/patches/patch-config_makefiles_rust.mk
@@ -1,24 +1,11 @@
-$NetBSD: patch-config_makefiles_rust.mk,v 1.1 2020/08/22 23:12:34 wiz Exp $
+$NetBSD: patch-config_makefiles_rust.mk,v 1.2 2020/08/26 20:55:19 wiz Exp $
 
 NetBSD<10 doesn't get along with parallel rust builds (it causes issues
 with ld.so) which are the default. Force -j1.
 
-Set -Cembed-bitcode=yes when using LTO to fix build with rust >= 1.45.
-Upstream bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1640982
-
---- config/makefiles/rust.mk.orig	2020-07-20 21:02:39.000000000 +0000
+--- config/makefiles/rust.mk.orig	2020-08-18 09:41:09.000000000 +0000
 +++ config/makefiles/rust.mk
-@@ -49,11 +49,19 @@ ifndef MOZ_DEBUG_RUST
- # Enable link-time optimization for release builds.
- cargo_rustc_flags += -C lto
- endif
-+# Versions of rust >= 1.45 need -Cembed-bitcode=yes for all crates when
-+# using -Clto.
-+ifeq (,$(filter 1.38.% 1.39.% 1.40.% 1.41.% 1.42.% 1.43.% 1.44.%,$(RUSTC_VERSION)))
-+RUSTFLAGS += -Cembed-bitcode=yes
-+endif
- endif
- 
+@@ -59,6 +59,9 @@ endif
  ifdef CARGO_INCREMENTAL
  export CARGO_INCREMENTAL
  endif
