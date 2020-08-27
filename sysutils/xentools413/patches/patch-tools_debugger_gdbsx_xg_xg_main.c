a4f33cb3b74e232f519a7ba6239d07fed574be43
bouyer <bouyer@pkgsrc.org>
2020-08-27 08:22:33 +0000

    Always use system-supplied IOCTL definitions, remove xen-provided include files. Build seabios and ipxe, they're needed by qemu-xen (ipxe not strictly needed but it's convenient to have) Switch default device model from qemu-xen-traditional to qemu-xen (the former being unmaintained) Add upstream patch for XSA335 security issue. Bump PKGREVISION.

--- sysutils/xentools413/patches/patch-tools_debugger_gdbsx_xg_xg_main.c
+++ sysutils/xentools413/patches/patch-tools_debugger_gdbsx_xg_xg_main.c
@@ -1,8 +1,20 @@
-$NetBSD: patch-tools_debugger_gdbsx_xg_xg_main.c,v 1.1 2020/05/26 11:12:11 bouyer Exp $
+$NetBSD: patch-tools_debugger_gdbsx_xg_xg_main.c,v 1.2 2020/08/27 08:22:33 bouyer Exp $
 
---- tools/debugger/gdbsx/xg/xg_main.c.orig	2020-04-30 09:57:45.000000000 +0200
-+++ tools/debugger/gdbsx/xg/xg_main.c	2020-04-30 09:58:43.000000000 +0200
-@@ -126,12 +126,19 @@
+--- tools/debugger/gdbsx/xg/xg_main.c.orig	2020-05-14 14:19:32.000000000 +0200
++++ tools/debugger/gdbsx/xg/xg_main.c	2020-08-26 23:12:04.349806717 +0200
+@@ -49,7 +49,11 @@
+ #include "xg_public.h"
+ #include <xen/version.h>
+ #include <xen/domctl.h>
++#ifdef __NetBSD__
++#include <xen/xenio.h>
++#else
+ #include <xen/sys/privcmd.h>
++#endif
+ #include <xen/foreign/x86_32.h>
+ #include <xen/foreign/x86_64.h>
+ 
+@@ -126,12 +130,19 @@
      int flags, saved_errno;
  
      XGTRC("E\n");
