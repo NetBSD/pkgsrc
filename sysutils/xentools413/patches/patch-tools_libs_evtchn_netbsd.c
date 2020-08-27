650e89d47c67a69dc467c3fe0c9965f71d1fe31c
bouyer <bouyer@pkgsrc.org>
2020-08-27 08:22:33 +0000

    Always use system-supplied IOCTL definitions, remove xen-provided include files. Build seabios and ipxe, they're needed by qemu-xen (ipxe not strictly needed but it's convenient to have) Switch default device model from qemu-xen-traditional to qemu-xen (the former being unmaintained) Add upstream patch for XSA335 security issue. Bump PKGREVISION.

--- sysutils/xentools413/patches/patch-tools_libs_evtchn_netbsd.c
+++ sysutils/xentools413/patches/patch-tools_libs_evtchn_netbsd.c
@@ -1,8 +1,21 @@
-$NetBSD: patch-tools_libs_evtchn_netbsd.c,v 1.1 2020/05/26 11:12:11 bouyer Exp $
+$NetBSD: patch-tools_libs_evtchn_netbsd.c,v 1.2 2020/08/27 08:22:33 bouyer Exp $
 
---- tools/libs/evtchn/netbsd.c.orig	2017-03-24 18:23:27.000000000 +0100
-+++ tools/libs/evtchn/netbsd.c	2017-03-24 18:23:40.000000000 +0100
-@@ -124,7 +124,7 @@
+--- tools/libs/evtchn/netbsd.c.orig	2020-05-14 14:19:32.000000000 +0200
++++ tools/libs/evtchn/netbsd.c	2020-05-27 14:07:41.348188034 +0200
+@@ -25,10 +25,10 @@
+ 
+ #include <sys/ioctl.h>
+ 
+-#include <xen/sys/evtchn.h>
+-
+ #include "private.h"
+ 
++#include <xen/xenio3.h>
++
+ #define EVTCHN_DEV_NAME  "/dev/xenevt"
+ 
+ int osdep_evtchn_open(xenevtchn_handle *xce)
+@@ -131,7 +131,7 @@
      int fd = xce->fd;
      evtchn_port_t port;
  
@@ -11,7 +24,7 @@ $NetBSD: patch-tools_libs_evtchn_netbsd.c,v 1.1 2020/05/26 11:12:11 bouyer Exp $
          return -1;
  
      return port;
-@@ -133,7 +133,7 @@
+@@ -140,7 +140,7 @@
  int xenevtchn_unmask(xenevtchn_handle *xce, evtchn_port_t port)
  {
      int fd = xce->fd;
