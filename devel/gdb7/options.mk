6d98ddc0d0ae203169518ba93597ccf4eaa5d00f
micha <micha@pkgsrc.org>
2020-08-28 11:02:23 +0000

    devel/gdb7: Fix package with disabled python option
    The common PLIST must be reduced in this case.
    
    No bump of PKGREVISION because the package with enabled python option
    is unchanged.
    
    While here, fix some pkglint warnings.

--- devel/gdb7/options.mk
+++ devel/gdb7/options.mk
@@ -1,8 +1,9 @@
-# $NetBSD: options.mk,v 1.4 2019/09/02 13:19:41 adam Exp $
+# $NetBSD: options.mk,v 1.5 2020/08/28 11:02:23 micha Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gdb
 PKG_SUPPORTED_OPTIONS=	python
 PKG_SUGGESTED_OPTIONS=	python
+PLIST_VARS+=		python
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,4 +11,5 @@ PKG_SUGGESTED_OPTIONS=	python
 CONFIGURE_ARGS+=	--with-python=${PYTHONBIN}
 PYTHON_VERSIONS_ACCEPTED=	 27
 .include "../../lang/python/application.mk"
+PLIST.python=	yes
 .endif
