5c568a874c2bebcb49720c0a23f683f78d4b2f58
ryoon <ryoon@pkgsrc.org>
2020-08-29 22:18:41 +0000

    graphics/libv4l1: import libv4l-1.18.1 from wip/libv4l
    libv4l is a collection of libraries which adds a thin abstraction layer on
    top of video4linux2 devices. The purpose of this (thin) layer is to make it
    easy for application writers to support a wide variety of devices without
    having to write seperate code for different devices in the same class.

---
+++ graphics/libv4l1/patches/patch-lib_libv4lconvert_libv4lsyscall-priv.h
@@ -0,0 +1,28 @@
+$NetBSD: patch-lib_libv4lconvert_libv4lsyscall-priv.h,v 1.1 2020/08/29 22:18:42 ryoon Exp $
+
+--- lib/libv4lconvert/libv4lsyscall-priv.h.orig	2017-01-22 17:33:34.000000000 +0000
++++ lib/libv4lconvert/libv4lsyscall-priv.h
+@@ -59,11 +59,10 @@
+ #define	_IOC_NR(cmd) ((cmd) & 0xFF)
+ #define	_IOC_TYPE(cmd) IOCGROUP(cmd)
+ #define	_IOC_SIZE(cmd) IOCPARM_LEN(cmd)
+-#define	MAP_ANONYMOUS MAP_ANON
+ #define	MMAP2_PAGE_SHIFT 0
+ #endif
+ 
+-#if defined(__OpenBSD__)
++#if defined(__OpenBSD__) || defined(__NetBSD__)
+ #include <sys/syscall.h>
+ #include <sys/types.h>
+ #include <sys/ioctl.h>
+@@ -111,6 +110,10 @@ register_t __syscall(quad_t, ...);
+ #define SYS_MMAP(addr, len, prot, flags, fd, offset) \
+ 	__syscall((quad_t)SYS_mmap, (void *)(addr), (size_t)(len), \
+ 			(int)(prot), (int)(flags), (int)(fd), 0, (off_t)(offset))
++#elif defined(__NetBSD__)
++#define SYS_MMAP(addr, len, prot, flags, fd, offset) \
++	__syscall((quad_t)SYS_mmap, (void *)(addr), (size_t)(len), \
++			(int)(prot), (int)(flags), (int)(fd), 0, (off_t)(offset))
+ #else
+ #define SYS_MMAP(addr, len, prot, flags, fd, off) \
+ 	syscall(SYS_mmap2, (void *)(addr), (size_t)(len), \
