1eebb4d595b5b1751c8faae2d1dd99bf5407e9f9
js <js@pkgsrc.org>
2020-08-29 21:39:14 +0000

    devel/m4: Fix compilation on QNX

---
+++ devel/m4/patches/patch-lib_strerror_override.c
@@ -0,0 +1,18 @@
+$NetBSD: patch-lib_strerror_override.c,v 1.1 2020/08/29 21:39:14 js Exp $
+
+--- lib/strerror-override.c.orig	2016-12-31 13:54:42.000000000 +0000
++++ lib/strerror-override.c
+@@ -29,6 +29,8 @@
+ # endif
+ #endif
+ 
++/* strerror_override.h might define it to NULL. */
++#ifndef strerror_override
+ /* If ERRNUM maps to an errno value defined by gnulib, return a string
+    describing the error.  Otherwise return NULL.  */
+ const char *
+@@ -300,3 +302,4 @@ strerror_override (int errnum)
+       return NULL;
+     }
+ }
++#endif
