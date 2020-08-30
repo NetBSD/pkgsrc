ac4d2b562cab574429077d829f4b92539ccb1dd3
tnn <tnn@pkgsrc.org>
2020-08-30 21:12:55 +0000

    gnuradio-vocoder: update to 3.8.1.0

--- ham/gnuradio-vocoder/buildlink3.mk
+++ ham/gnuradio-vocoder/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/01/07 13:04:17 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/08/30 21:12:55 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-vocoder
 
 .if !defined(GNURADIO_VOCODER_BUILDLINK3_MK)
 GNURADIO_VOCODER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.7.5
+BUILDLINK_API_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.8.1.0
 BUILDLINK_PKGSRCDIR.gnuradio-vocoder?=		../../ham/gnuradio-vocoder
 .endif # GNURADIO_VOCODER_BUILDLINK3_MK
 
