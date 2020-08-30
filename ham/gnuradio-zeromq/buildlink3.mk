cc9c3f2d33777cc325bd8f1a8e4f14540b2aa8e8
tnn <tnn@pkgsrc.org>
2020-08-30 21:16:02 +0000

    gnuradio-zeromq: update to 3.8.1.0

--- ham/gnuradio-zeromq/buildlink3.mk
+++ ham/gnuradio-zeromq/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/10/13 12:51:10 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/08/30 21:16:02 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-zeromq
 
 .if !defined(GNURADIO_ZEROMQ_BUILDLINK3_MK)
 GNURADIO_ZEROMQ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-zeromq+=	gnuradio-zeromq>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-zeromq+=	gnuradio-zeromq>=3.7.5
+BUILDLINK_API_DEPENDS.gnuradio-zeromq+=	gnuradio-zeromq>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-zeromq+=	gnuradio-zeromq>=3.8.1.0
 BUILDLINK_PKGSRCDIR.gnuradio-zeromq?=	../../ham/gnuradio-zeromq
 .endif # GNURADIO_ZEROMQ_BUILDLINK3_MK
 
