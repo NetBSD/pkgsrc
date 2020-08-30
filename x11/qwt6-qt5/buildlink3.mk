2ede41f77edaae808aa1e0a99a0c59efc8ed43f5
tnn <tnn@pkgsrc.org>
2020-08-30 08:05:57 +0000

    qwt6-qt5: update to 6.1.5
    Maintenance
    - Avoid using some of the deprecated Qt APIs
    - Missing includes of QPainterPath ( Qt 5.15 ) added
    - QwtPlotSpectroCurve: missing public inheritance
    - QwtPlotTradingCurve: missing public inheritance
    
    Bug fixes
    - QwtLinearColorMap: handling of alpha values in color stops fixed
    - QwtPlotRasterItem: missing color table initialization for
      QImage::Format_Indexed8
    - QwtScaleWidget: missing QEvent::LayoutRequest added
    - QwtSetSample: bound rectangle fixed for empty sets

--- x11/qwt6-qt5/buildlink3.mk
+++ x11/qwt6-qt5/buildlink3.mk
@@ -1,18 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.9 2020/08/17 20:18:00 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2020/08/30 08:05:57 tnn Exp $
 
 BUILDLINK_TREE+=	qwt6-qt5
 
 .if !defined(QWT6_QT5_BUILDLINK3_MK)
 QWT6_QT5_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qwt6-qt5+=	qwt6-qt5>=6.1.4
-BUILDLINK_ABI_DEPENDS.qwt6-qt5+=	qwt6-qt5>=6.1.4nb6
+BUILDLINK_API_DEPENDS.qwt6-qt5+=	qwt6-qt5>=6.1.5
 BUILDLINK_PKGSRCDIR.qwt6-qt5?=		../../x11/qwt6-qt5
 
-QWT_DIR=			${BUILDLINK_PREFIX.qwt6-qt5}/qwt-6.1.4
-BUILDLINK_INCDIRS.qwt6-qt5+=	qwt-6.1.4/include
-BUILDLINK_LIBDIRS.qwt6-qt5+=	qwt-6.1.4/lib
-BUILDLINK_RPATHDIRS.qwt6-qt5+=	qwt-6.1.4/lib
+QWT_DIR=			${BUILDLINK_PREFIX.qwt6-qt5}/qwt-6.1.5
+BUILDLINK_INCDIRS.qwt6-qt5+=	qwt-6.1.5/include
+BUILDLINK_LIBDIRS.qwt6-qt5+=	qwt-6.1.5/lib
+BUILDLINK_RPATHDIRS.qwt6-qt5+=	qwt-6.1.5/lib
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .include "../../x11/qt5-qtsvg/buildlink3.mk"
