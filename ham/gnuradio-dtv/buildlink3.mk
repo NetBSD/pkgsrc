dd179a92ab50bca9aef96934d6cf3a3bf8c5e621
tnn <tnn@pkgsrc.org>
2020-08-30 20:59:52 +0000

    gnuradio-dtv: update to 3.8.1.0

--- ham/gnuradio-dtv/buildlink3.mk
+++ ham/gnuradio-dtv/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/10/13 12:51:09 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/08/30 20:59:52 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-dtv
 
 .if !defined(GNURADIO_DTV_BUILDLINK3_MK)
 GNURADIO_DTV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-dtv+=	gnuradio-dtv>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-dtv+=	gnuradio-dtv>=3.7.5
+BUILDLINK_API_DEPENDS.gnuradio-dtv+=	gnuradio-dtv>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-dtv+=	gnuradio-dtv>=3.8.1.0
 BUILDLINK_PKGSRCDIR.gnuradio-dtv?=	../../ham/gnuradio-dtv
 .endif # GNURADIO_DTV_BUILDLINK3_MK
 
