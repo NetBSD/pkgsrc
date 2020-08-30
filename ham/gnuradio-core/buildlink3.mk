e49e6e8b1bd1d52b8e03fcae992c815de2d86b8a
tnn <tnn@pkgsrc.org>
2020-08-30 20:43:46 +0000

    gnuradio-core: update to 3.8.1.0
    Here is a very high level list of changes:
    
    ## Changed
    ### Project Scope
    - C++11
    - merged the wholeness of the `next` branch
    - Dependency version bumps: CMake, GCC, MSVC, Swig, Boost
    - New dependencies: MPIR/GMP, Qt5, gsm, codec2
    - Removed dependencies: libusb, Qt4, CppUnit
    - Python: Python 2 & Python 3 compatible. 3.8 will be the last Py2k-compatible
      release series
    - gengen was replaced by templates (if you don't know gengen, don't do any
      research; save yourself that sorrow)
    - Modern CMake (as far as feasible at this point)
    - VOLK version updated to v2.0.0
    - .clang-format file now dictates coding style
    - clang-format'ed the whole tree.
    - installed CMake files now tell about configuration
    
    ### gnuradio-runtime
    - reworked fractional tag time handling, especially in the context of resamplers
    
    ### GRC
    - C++ generation as option
    - YAML instead of XML
    - removed `blks2`
    - much better canvas tooling
    - consistent gobject usage
    - ROUNDED ARROWS
    
    ### gr-qtgui
    - moving from Qt4 to Qt5
    
    ### gr-utils
    - `gr_modtool` now vastly improved
    
    ### gr-vocoder
    - improved versatility
    - removed in-tree libgsm, libcodec2, use system-wide libs
    
    ## Removed
    ### Project Scope
    - Modules `gr-comedi`, `gr-fcd` and `gr-wxgui` are gone
    ### gr-comedi
    - nobody could remember who used this, or for what. It has seen 0 active code
      contributions in the 3.7 lifecycle
    ### gr-digital
    - python-based `packet_encoder` and related tools: Bugs that were sporadic and
      never fixed, so after long deprecation, we're removing it
    ### gr-fcd
    - since it's currently untestable by the CI, it's being removed, as there was no
      code contributions. Generally, we strive to include all batteries with GNU
      Radio. Re-integration within a more general SDR interface would be desirable.
    ### gr-utils
    - removed PyQwt (dead) based tools
    ### gr-wxgui
    - Unmaintained, breaks on increasingly many systems, always was slower than
      Qtgui. We've been starting to tell people to migrate to Qt since at
      least 2015. Now, we're finally removing it.

--- ham/gnuradio-core/buildlink3.mk
+++ ham/gnuradio-core/buildlink3.mk
@@ -1,13 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.29 2017/03/15 14:07:12 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.30 2020/08/30 20:43:46 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-core
 
 .if !defined(GNURADIO_CORE_BUILDLINK3_MK)
 GNURADIO_CORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-core+=	gnuradio-core>=3.7.11
-BUILDLINK_ABI_DEPENDS.gnuradio-core+=	gnuradio-core>=3.7.11
+BUILDLINK_API_DEPENDS.gnuradio-core+=	gnuradio-core>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-core+=	gnuradio-core>=3.8.1.0
 BUILDLINK_PKGSRCDIR.gnuradio-core?=	../../ham/gnuradio-core
+
+.include "../../devel/gmp/buildlink3.mk"
+.include "../../textproc/log4cpp/buildlink3.mk"
+.include "../../math/volk/buildlink3.mk"
 .endif # GNURADIO_CORE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnuradio-core
