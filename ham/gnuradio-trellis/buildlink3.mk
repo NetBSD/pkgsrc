1860715d0eff0e7e66d3c368931f03b12d70482d
tnn <tnn@pkgsrc.org>
2020-08-30 21:05:13 +0000

    gnuradio-trellis: update to 3.8.1.0

--- ham/gnuradio-trellis/buildlink3.mk
+++ ham/gnuradio-trellis/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/01/07 13:04:17 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/08/30 21:05:13 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-trellis
 
 .if !defined(GNURADIO_TRELLIS_BUILDLINK3_MK)
 GNURADIO_TRELLIS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-trellis+=	gnuradio-trellis>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-trellis+=	gnuradio-trellis>=3.7.5
+BUILDLINK_API_DEPENDS.gnuradio-trellis+=	gnuradio-trellis>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-trellis+=	gnuradio-trellis>=3.8.1.0
 BUILDLINK_PKGSRCDIR.gnuradio-trellis?=		../../ham/gnuradio-trellis
 .endif # GNURADIO_TRELLIS_BUILDLINK3_MK
 
