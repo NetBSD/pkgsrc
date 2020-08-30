ffabed7cf54947df73447b669ac25c496ebdaa9b
tnn <tnn@pkgsrc.org>
2020-08-30 21:14:07 +0000

    gnuradio-wavelet: update to 3.8.1.0

--- ham/gnuradio-wavelet/buildlink3.mk
+++ ham/gnuradio-wavelet/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/01/07 13:04:17 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/08/30 21:14:07 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-wavelet
 
 .if !defined(GNURADIO_WAVELET_BUILDLINK3_MK)
 GNURADIO_WAVELET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-wavelet+=	gnuradio-wavelet>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-wavelet+=	gnuradio-wavelet>=3.7.5
+BUILDLINK_API_DEPENDS.gnuradio-wavelet+=	gnuradio-wavelet>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-wavelet+=	gnuradio-wavelet>=3.8.1.0
 BUILDLINK_PKGSRCDIR.gnuradio-wavelet?=		../../ham/gnuradio-wavelet
 .endif # GNURADIO_WAVELET_BUILDLINK3_MK
 
