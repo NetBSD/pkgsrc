786a2f53a52f77622f48496cb9012f535386ddda
tnn <tnn@pkgsrc.org>
2020-08-30 21:11:08 +0000

    gnuradio-video-sdl: update to 3.8.1.0

--- ham/gnuradio-video-sdl/buildlink3.mk
+++ ham/gnuradio-video-sdl/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/10/13 12:51:10 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/08/30 21:11:08 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-video-sdl
 
 .if !defined(GNURADIO_VIDEO_SDL_BUILDLINK3_MK)
 GNURADIO_VIDEO_SDL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-video-sdl+=	gnuradio-video-sdl>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-video-sdl+=	gnuradio-video-sdl>=3.7.5
+BUILDLINK_API_DEPENDS.gnuradio-video-sdl+=	gnuradio-video-sdl>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-video-sdl+=	gnuradio-video-sdl>=3.8.1.0
 BUILDLINK_PKGSRCDIR.gnuradio-video-sdl?=	../../ham/gnuradio-video-sdl
 .endif # GNURADIO_VIDEO_SDL_BUILDLINK3_MK
 
