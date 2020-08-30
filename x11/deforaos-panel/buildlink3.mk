5bf492dc224377cffda82f320a7e1462087abfa4
khorben <khorben@pkgsrc.org>
2020-08-30 22:06:22 +0000

    deforaos-panel: fix wrong dependency on Gtk+ 2
    This should fix building meta-pkgs/deforaos-desktop.
    
    Thanks joerg@ for the heads up!

--- x11/deforaos-panel/buildlink3.mk
+++ x11/deforaos-panel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2020/08/17 20:17:48 leot Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2020/08/30 22:06:22 khorben Exp $
 
 BUILDLINK_TREE+=	deforaos-panel
 
@@ -9,8 +9,7 @@ BUILDLINK_API_DEPENDS.deforaos-panel+=	deforaos-panel>=0.3.6
 BUILDLINK_ABI_DEPENDS.deforaos-panel?=	deforaos-panel>=0.4.0nb3
 BUILDLINK_PKGSRCDIR.deforaos-panel?=	../../x11/deforaos-panel
 
-.include "../../devel/deforaos-libsystem/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/deforaos-libdesktop/buildlink3.mk"
 .endif	# DEFORAOS_PANEL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-deforaos-panel
