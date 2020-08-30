9e912952d80ff59586b94c6414c33f3ca0f6c124
tnn <tnn@pkgsrc.org>
2020-08-30 20:53:11 +0000

    gnuradio-digital: update to 3.8.1.0

--- ham/gnuradio-digital/buildlink3.mk
+++ ham/gnuradio-digital/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2018/01/07 13:04:17 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2020/08/30 20:53:11 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-digital
 
 .if !defined(GNURADIO_DIGITAL_BUILDLINK3_MK)
 GNURADIO_DIGITAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-digital+=	gnuradio-digital>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-digital+=	gnuradio-digital>=3.7.5
+BUILDLINK_API_DEPENDS.gnuradio-digital+=	gnuradio-digital>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-digital+=	gnuradio-digital>=3.8.1.0
 BUILDLINK_PKGSRCDIR.gnuradio-digital?=		../../ham/gnuradio-digital
 .endif # GNURADIO_DIGITAL_BUILDLINK3_MK
 
