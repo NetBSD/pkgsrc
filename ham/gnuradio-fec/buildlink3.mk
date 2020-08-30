59cf63e8b3b1cbb8b58367aab85748542967cf04
tnn <tnn@pkgsrc.org>
2020-08-30 21:01:28 +0000

    gnuradio-fec: update to 3.8.1.0

--- ham/gnuradio-fec/buildlink3.mk
+++ ham/gnuradio-fec/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/10/13 12:51:09 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/08/30 21:01:28 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-fec
 
 .if !defined(GNURADIO_FEC_BUILDLINK3_MK)
 GNURADIO_FEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-fec+=	gnuradio-fec>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-fec+=	gnuradio-fec>=3.7.5
+BUILDLINK_API_DEPENDS.gnuradio-fec+=	gnuradio-fec>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-fec+=	gnuradio-fec>=3.8.1.0
 BUILDLINK_PKGSRCDIR.gnuradio-fec?=	../../ham/gnuradio-fec
 .endif # GNURADIO_FEC_BUILDLINK3_MK
 
