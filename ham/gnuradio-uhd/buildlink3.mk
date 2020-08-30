ca34526ebd64fe5efbc9083d069a37f85e3c9641
tnn <tnn@pkgsrc.org>
2020-08-30 21:07:13 +0000

    gnuradio-uhd: update to 3.8.1.0

--- ham/gnuradio-uhd/buildlink3.mk
+++ ham/gnuradio-uhd/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/10/13 12:51:10 mef Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2020/08/30 21:07:13 tnn Exp $
 
 BUILDLINK_TREE+=	gnuradio-uhd
 
 .if !defined(GNURADIO_UHD_BUILDLINK3_MK)
 GNURADIO_UHD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-uhd+=	gnuradio-uhd>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-uhd+=	gnuradio-uhd>=3.7.5
+BUILDLINK_API_DEPENDS.gnuradio-uhd+=	gnuradio-uhd>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-uhd+=	gnuradio-uhd>=3.8.1.0
 BUILDLINK_PKGSRCDIR.gnuradio-uhd?=	../../ham/gnuradio-uhd
 .endif # GNURADIO_UHD_BUILDLINK3_MK
 
