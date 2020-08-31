1b77f890921f8d15a529d696339415c085886821
wiz <wiz@pkgsrc.org>
2020-08-31 18:06:29 +0000

    *: bump PKGREVISION for perl-5.32.

--- security/libpreludedb-perl/buildlink3.mk
+++ security/libpreludedb-perl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2019/11/04 21:12:54 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2020/08/31 18:06:30 wiz Exp $
 
 BUILDLINK_TREE+=	libpreludedb-perl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libpreludedb-perl
 LIBPRELUDEDB_PERL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libpreludedb-perl+=	libpreludedb-perl>=0.9.15.3
-BUILDLINK_ABI_DEPENDS.libpreludedb-perl?=	libpreludedb-perl>=0.9.15.3nb18
+BUILDLINK_ABI_DEPENDS.libpreludedb-perl?=	libpreludedb-perl>=0.9.15.3nb21
 BUILDLINK_PKGSRCDIR.libpreludedb-perl?=		../../security/libpreludedb-perl
 
 .endif	# LIBPRELUDEDB_PERL_BUILDLINK3_MK
