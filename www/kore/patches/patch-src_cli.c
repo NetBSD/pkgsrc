601cf28a1db3f7310cfb66c35a1a4b600191488e
fcambus <fcambus@pkgsrc.org>
2020-09-01 19:25:00 +0000

    kore: update to 4.0.0.
    ChangeLog:
    
    - Fully priviledge seperated ACME support with Let's Encrypt or any
      compatible ACME provider.
    - A better way of writing Kore Python applications.
    - Full seccomp sandboxing for acme, keymgr and worker processes on Linux.
    - Ability to mix TLS and non-TLS servers in a single configuration.
    - Many improvements to the Python asynchronous runtime.
    - Configuration based redirection.
    - A complete builtin and easy to use JSON parser.
    - Several bug fixes and speed improvements.

--- www/kore/patches/patch-src_cli.c
+++ www/kore/patches/patch-src_cli.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_cli.c,v 1.1 2020/01/12 23:01:06 joerg Exp $
+$NetBSD: patch-src_cli.c,v 1.2 2020/09/01 19:25:00 fcambus Exp $
 
---- src/cli.c.orig	2020-01-11 18:45:39.562950385 +0000
+--- src/cli.c.orig	2020-08-26 07:20:35.000000000 +0000
 +++ src/cli.c
-@@ -985,7 +985,7 @@ cli_build_asset(char *fpath, struct dire
+@@ -1120,7 +1120,7 @@ cli_build_asset(char *fpath, struct dire
  
  	/* Replace dots, spaces, etc etc with underscores. */
  	for (p = name; *p != '\0'; p++) {
