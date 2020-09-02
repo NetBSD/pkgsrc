e339b065a0d7151312b46421bc4a7a6829b699e8
gdt <gdt@pkgsrc.org>
2020-09-02 00:24:26 +0000

    geography/merkaator: Update to 0.18.4
    Update originally by Ben Gergely, with modifications by gdt, from PR
    pkg/55592.
    
    pkgsrc changes:
      - Adjust dependencies to qt5
      - Require gcc6, due to gcc5 lossage with -Wno-registers
    
    Upstream NEWS:
    
      - Support for qt5
      - Lots of bugfixes and minor improvements

--- geography/merkaartor/patches/patch-src_Features_Feature.h
+++ geography/merkaartor/patches/patch-src_Features_Feature.h
@@ -1,32 +1,13 @@
-$NetBSD: patch-src_Features_Feature.h,v 1.1 2013/02/26 11:15:19 joerg Exp $
+$NetBSD: patch-src_Features_Feature.h,v 1.2 2020/09/02 00:24:26 gdt Exp $
 
 --- src/Features/Feature.h.orig	2011-02-10 12:00:35.000000000 +0000
 +++ src/Features/Feature.h
-@@ -9,6 +9,14 @@
+@@ -10,6 +10,7 @@ class Feature;
+
  #include <QtCore/QString>
  #include <QList>
- 
-+class Feature;
-+
-+namespace boost
-+{
-+    void intrusive_ptr_add_ref(Feature * p);
-+    void intrusive_ptr_release(Feature * p);
-+}
-+
- #include <boost/intrusive_ptr.hpp>
++#include <QPainterPath>
  
  #define CAST_FEATURE(x) (dynamic_cast<Feature*>(x))
-@@ -79,12 +87,6 @@ class RenderPriority
-         int theLayer;
- };
- 
--namespace boost
--{
--    void intrusive_ptr_add_ref(Feature * p);
--    void intrusive_ptr_release(Feature * p);
--}
--
- /// Used to store objects of the map
- class Feature : public IFeature
- {
+ #define CAST_NODE(x) (dynamic_cast<Node*>(x))
+
