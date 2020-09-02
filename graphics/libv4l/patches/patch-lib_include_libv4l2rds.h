7b990b397e20e67fd4296fbcb81647214150e24b
ryoon <ryoon@pkgsrc.org>
2020-09-02 09:54:33 +0000

    libv4l, libv4l1: Update graphics/libv4l with libv4l1
    * Copy graphics/libv4l1 to graphics/libv4l.
    * Remove graphics/libv4l1.

--- graphics/libv4l/patches/patch-lib_include_libv4l2rds.h
+++ graphics/libv4l/patches/patch-lib_include_libv4l2rds.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-lib_include_libv4l2rds.h,v 1.1 2020/08/29 22:18:42 ryoon Exp $
+$NetBSD: patch-lib_include_libv4l2rds.h,v 1.1 2020/09/02 09:54:33 ryoon Exp $
 
 --- lib/include/libv4l2rds.h.orig	2017-01-22 17:33:34.000000000 +0000
 +++ lib/include/libv4l2rds.h
